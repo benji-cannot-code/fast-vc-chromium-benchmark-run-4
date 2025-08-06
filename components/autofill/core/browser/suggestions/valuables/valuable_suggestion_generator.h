e69ad65b0fdbfb85a4f6837e76bbea13067b5dae
Karol Sygiet <sygiet@google.com>
2025-08-06 02:32:32 -0700

    [RSG] Make GetSuggestionsForLoyaltyCards take form, field and client
    This change is needed in order to allow for the legacy interface to
    wrap the new LoyaltyCardSuggestionGenerator that is introduced in a
    followup CL.
    
    Bug: 409962888
    Change-Id: I1fc90016fbfc35f7c12b1573a7cbeb988396c10a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6818437
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Cr-Commit-Position: refs/heads/main@{#1497450}

--- components/autofill/core/browser/suggestions/valuables/valuable_suggestion_generator.h
+++ components/autofill/core/browser/suggestions/valuables/valuable_suggestion_generator.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "components/autofill/core/browser/foundations/autofill_client.h"
 #include "components/autofill/core/browser/suggestions/suggestion.h"
 
 class GURL;
@@ -16,12 +17,17 @@ namespace autofill {
 
 class ValuablesDataManager;
 
-// Generates loyalty card suggestions for a given `url`. Loyalty cards are
-// extracted from the `valuables_manager`.
+// Generates loyalty card suggestions for the value of trigger `field` and the
+// last committed primary main frame URL obtained from `client`. Loyalty cards
+// are extracted from the `ValuablesDataManager` using `client`.
+// TODO(crbug.com/409962888): Remove after new suggestion generation logic is
+// launched.
 std::vector<Suggestion> GetSuggestionsForLoyaltyCards(
-    const ValuablesDataManager& valuables_manager,
-    const GURL& url,
-    bool trigger_field_is_autofilled);
+    const FormData& form,
+    const FormStructure* form_structure,
+    const FormFieldData& field,
+    const AutofillField* autofill_field,
+    const AutofillClient& client);
 
 // Extends `email_suggestions` with loyalty cards suggestions.
 void ExtendEmailSuggestionsWithLoyaltyCardSuggestions(
