67abc1dfa1650e7fcbc6b23769d1dc79bf3c12ab
Sourav <sinhasourav@google.com>
2025-08-06 12:19:29 -0700

    [iOSImportPasswordsSafari] Ensure `OnPasswordsImported` is always called
    This CL fixes an issue where the callback was not being called when the
    password importer was not in the `kUserInteractionRequired` state.
    
    Bug: 407587751
    Change-Id: Id394ea2c2ac6d3ffca57af9eb85fed34326f544b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6824348
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Commit-Queue: Sourav Uttam Sinha <sinhasourav@google.com>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1497771}

--- components/user_data_importer/utility/safari_data_importer_unittest.cc
+++ components/user_data_importer/utility/safari_data_importer_unittest.cc
@@ -866,7 +866,10 @@ TEST_F(SafariDataImporterTest, ImportSingleBookmark) {
 
   EXPECT_CALL(client_, OnBookmarksImported(1u));
   EXPECT_CALL(client_, OnHistoryImported(0));
-  EXPECT_CALL(client_, OnPaymentCardsImported(0));
+  EXPECT_CALL(client_, OnPaymentCardsImported(0u));
+  EXPECT_CALL(client_, OnPasswordsImported(
+                           AllOf(Field(&ImportResults::number_imported, 0u),
+                                 Field(&ImportResults::number_to_import, 0u))));
 
   CompleteImport({});
 
@@ -891,6 +894,9 @@ TEST_F(SafariDataImporterTest, ImportsMultipleBookmarks) {
   EXPECT_CALL(client_, OnBookmarksImported(2u));
   EXPECT_CALL(client_, OnHistoryImported(0));
   EXPECT_CALL(client_, OnPaymentCardsImported(0));
+  EXPECT_CALL(client_, OnPasswordsImported(
+                           AllOf(Field(&ImportResults::number_imported, 0u),
+                                 Field(&ImportResults::number_to_import, 0u))));
   CompleteImport({});
 
   const bookmarks::BookmarkNode* other_node = GetOtherBookmarkNode();
@@ -922,6 +928,9 @@ TEST_F(SafariDataImporterTest, ImportsNestedBookmark) {
   EXPECT_CALL(client_, OnBookmarksImported(1u));
   EXPECT_CALL(client_, OnHistoryImported(0));
   EXPECT_CALL(client_, OnPaymentCardsImported(0));
+  EXPECT_CALL(client_, OnPasswordsImported(
+                           AllOf(Field(&ImportResults::number_imported, 0u),
+                                 Field(&ImportResults::number_to_import, 0u))));
   CompleteImport({});
 
   const bookmarks::BookmarkNode* other_node = GetOtherBookmarkNode();
@@ -950,6 +959,9 @@ TEST_F(SafariDataImporterTest, ImportsEmptyFolder) {
   EXPECT_CALL(client_, OnBookmarksImported(0u));
   EXPECT_CALL(client_, OnHistoryImported(0));
   EXPECT_CALL(client_, OnPaymentCardsImported(0));
+  EXPECT_CALL(client_, OnPasswordsImported(
+                           AllOf(Field(&ImportResults::number_imported, 0u),
+                                 Field(&ImportResults::number_to_import, 0u))));
   CompleteImport({});
 
   const bookmarks::BookmarkNode* other_node = GetOtherBookmarkNode();
@@ -979,6 +991,9 @@ TEST_F(SafariDataImporterTest, ImportsMultipleReadingListItems) {
   EXPECT_CALL(client_, OnBookmarksImported(3u));
   EXPECT_CALL(client_, OnHistoryImported(0));
   EXPECT_CALL(client_, OnPaymentCardsImported(0));
+  EXPECT_CALL(client_, OnPasswordsImported(
+                           AllOf(Field(&ImportResults::number_imported, 0u),
+                                 Field(&ImportResults::number_to_import, 0u))));
   CompleteImport({});
 
   const ReadingListModel* model = GetReadingListModel();
@@ -1040,6 +1055,9 @@ TEST_F(SafariDataImporterTest, DuplicateBookmarkFolders) {
 
   EXPECT_CALL(client_, OnHistoryImported(0));
   EXPECT_CALL(client_, OnPaymentCardsImported(0));
+  EXPECT_CALL(client_, OnPasswordsImported(
+                           AllOf(Field(&ImportResults::number_imported, 0u),
+                                 Field(&ImportResults::number_to_import, 0u))));
   CompleteImport({});
 
   const bookmarks::BookmarkNode* import_folder =
