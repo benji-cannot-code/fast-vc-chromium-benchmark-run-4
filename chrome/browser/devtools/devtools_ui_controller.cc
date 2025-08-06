4698ca3cbe6cd1e7997639c405e4ad357487b51a
Eshwar Stalin <estalin@chromium.org>
2025-08-06 14:24:29 -0700

    [SxS] Fixing tracked DevToolsDockedPlacement state
    The DevToolsDockedPlacement needs to be computed after the layout for
    the ContentsContainerView is completed. This also results in a update to
    the rounded corner logic. This state is needed for computing the GLIC
    border rounded corners as well.
    
    Change-Id: I4856acdc0a54cb0073a5737720df32a529641d9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6825105
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1497849}

--- chrome/browser/devtools/devtools_ui_controller.cc
+++ chrome/browser/devtools/devtools_ui_controller.cc
@@ -131,7 +131,6 @@ bool DevtoolsUIController::DevtoolsWebViewController::UpdateDevtools(
     }
   }
 
-  contents_container_view_->UpdateDevToolsDockedPlacement();
   return devtools_layout_updated;
 }
 
