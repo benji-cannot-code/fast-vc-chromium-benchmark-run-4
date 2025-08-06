a68b6d60d16f4935a35c91a0799d59cecba4fca0
Elly <ellyjones@chromium.org>
2025-08-06 11:43:35 -0700

    components/ownership: remove RSAPrivateKey support from mock ownership
    It's unused as of d60babe063b3f37fe37cfb8f3ee1c6d128ac6f54, which
    migrated all call sites to the crypto/keypair variants instead.
    
    Bug: 372283556
    Change-Id: Ie287a5759f7ac895fc59e7ee0fe381e852fc415d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6824725
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1497717}

--- components/ownership/mock_owner_key_util.h
+++ components/ownership/mock_owner_key_util.h
@@ -17,10 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/ownership/ownership_export.h"
 #include "crypto/keypair.h"
 
-namespace crypto {
-class RSAPrivateKey;
-}
-
 namespace ownership {
 
 // Implementation of OwnerKeyUtil which should be used only for
@@ -48,21 +44,15 @@ class OWNERSHIP_EXPORT MockOwnerKeyUtil : public OwnerKeyUtil {
 
   // Sets the public key to use from the given private key, but doesn't
   // configure the private key.
-  void SetPublicKeyFromPrivateKey(const crypto::RSAPrivateKey& key);
   void SetPublicKeyFromPrivateKey(crypto::keypair::PrivateKey key);
 
   // Imports the private key into NSS, so it can be found later.
   // Also extracts the public key and sets it for this mock object (equivalent
   // to calling `SetPublicKeyFromPrivateKey`).
-  void ImportPrivateKeyAndSetPublicKey(
-      std::unique_ptr<crypto::RSAPrivateKey> key);
   void ImportPrivateKeyAndSetPublicKey(crypto::keypair::PrivateKey key);
 
   // Same as ImportPrivateKeyAndSetPublicKey, but remembers in which slot the
   // key is supposed to be. FindPrivateKeyInSlot will take this into account.
-  void ImportPrivateKeyInSlotAndSetPublicKey(
-      std::unique_ptr<crypto::RSAPrivateKey> key,
-      PK11SlotInfo* slot);
   void ImportPrivateKeyInSlotAndSetPublicKey(crypto::keypair::PrivateKey key,
                                              PK11SlotInfo* slot);
 
@@ -73,9 +63,6 @@ class OWNERSHIP_EXPORT MockOwnerKeyUtil : public OwnerKeyUtil {
  private:
   ~MockOwnerKeyUtil() override;
 
-  void ImportPrivateKeyAndSetPublicKeyImpl(
-      std::unique_ptr<crypto::RSAPrivateKey> key,
-      PK11SlotInfo* slot);
   void ImportPrivateKeyAndSetPublicKeyImpl(crypto::keypair::PrivateKey key,
                                            PK11SlotInfo* slot);
 
