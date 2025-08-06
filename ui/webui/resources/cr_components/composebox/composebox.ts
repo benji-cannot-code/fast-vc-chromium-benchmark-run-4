b970b7bb9a863607ec9b0c8ff6925e8d7f4cc1d5
Fred Shih <ffred@chromium.org>
2025-08-05 21:38:19 -0700

    Add a default case for all switch statements in ts
    Starting with ts, because the change is already quite substantial.
    
    The styleguide requires that all switch statements *must* have a default case.
    We would like to turn on the linter for this styleguide, but need to fix all
    the missing cases first. Styleguide:
    
    https://google.github.io/styleguide/tsguide.html#switch-statements
    
    For cases where it was just falling through, we used:
      default:
        break;
    
    For cases where it was returning something (that cannot be undefined),
    we used:
      default:
        assertNotReached();
    
    This change should be a noop change.
    
    NO_IFTTT=styleguide change, no modification to underlying enum
    
    This CL was uploaded by git cl split.
    
    R=ender@google.com
    
    Bug: 436274263
    Change-Id: I810956bddb3ecbd6bc2e8ad0332f5d2be11a1a96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6822759
    Auto-Submit: Fred Shih <ffred@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1497343}

--- ui/webui/resources/cr_components/composebox/composebox.ts
+++ ui/webui/resources/cr_components/composebox/composebox.ts
@@ -180,6 +180,8 @@ export class ComposeboxElement extends I18nMixinLit
                     this.errorMessage_ =
                         this.i18n('composeboxFileUploadExpired');
                     break;
+                  default:
+                    break;
                 }
                 this.showErrorScrim_ = true;
               } else {
