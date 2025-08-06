1e44e542de554767c87e623d4947a94cdd548390
Christian Dullweber <dullweber@chromium.org>
2025-08-06 08:46:04 -0700

    SiteSettings: Fix crash for categories without default setting
    Avoid refreshing the default value for changed content settings
    if the setting is not displayed in settings. This otherwise
    leads to crashes for settings that have no default setting,
    e.g. USB chooser data.
    
    Bug: 435597217
    Change-Id: Ia22530f56a45876b9f4b4db0a18cc02844b7b7a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6823437
    Reviewed-by: Filipa Senra <fsenra@google.com>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1497623}

--- chrome/browser/resources/settings/site_settings_page/site_settings_list.ts
+++ chrome/browser/resources/settings/site_settings_page/site_settings_list.ts
@@ -69,6 +69,11 @@ class SettingsSiteSettingsListElement extends
     return {
       categoryList: Array,
 
+      categoryMap_: {
+        type: Map,
+        computed: 'computeCategoryMap(categoryList)',
+      },
+
       focusConfig: {
         type: Object,
         observer: 'focusConfigChanged_',
@@ -90,6 +95,7 @@ class SettingsSiteSettingsListElement extends
 
   declare categoryList: CategoryListItem[];
   declare focusConfig: FocusConfig;
+  declare private categoryMap_: Map<ContentSettingsTypes, number>;
   private browserProxy_: SiteSettingsPrefsBrowserProxy =
       SiteSettingsPrefsBrowserProxyImpl.getInstance();
 
@@ -111,6 +117,11 @@ class SettingsSiteSettingsListElement extends
     }
   }
 
+  private computeCategoryMap(categoryList: CategoryListItem[]):
+      Map<ContentSettingsTypes, number> {
+    return new Map(categoryList.map((e, index) => [e.id, index]));
+  }
+
   override ready() {
     super.ready();
 
@@ -126,9 +137,8 @@ class SettingsSiteSettingsListElement extends
         (category: ContentSettingsTypes) =>
             this.refreshDefaultValueLabel_(category));
 
-    const hasProtocolHandlers = this.categoryList.some(item => {
-      return item.id === ContentSettingsTypes.PROTOCOL_HANDLERS;
-    });
+    const hasProtocolHandlers =
+        this.categoryMap_.has(ContentSettingsTypes.PROTOCOL_HANDLERS);
 
     if (hasProtocolHandlers) {
       // The protocol handlers have a separate enabled/disabled notifier.
@@ -167,11 +177,14 @@ class SettingsSiteSettingsListElement extends
     }
 
     if (category === ContentSettingsTypes.PERFORMANCE) {
-      const index = this.categoryList.map(e => e.id).indexOf(
-          ContentSettingsTypes.PERFORMANCE);
+      const index = this.categoryMap_.get(ContentSettingsTypes.PERFORMANCE);
       this.set(
           `categoryList.${index}.subLabel`,
           this.i18n('siteSettingsPerformanceSublabel'));
+      return Promise.resolve();
+    }
+
+    if (!this.categoryMap_.has(category)) {
       return Promise.resolve();
     }
 
@@ -221,8 +234,7 @@ class SettingsSiteSettingsListElement extends
    */
   private updateLocationLabel_() {
     const state = this.getPref('generated.geolocation').value;
-    const index = this.categoryList.map(e => e.id).indexOf(
-        ContentSettingsTypes.GEOLOCATION);
+    const index = this.categoryMap_.get(ContentSettingsTypes.GEOLOCATION);
 
     // The location row might not be part of the current site-settings-list
     // but the class always observes the preference.
@@ -248,8 +260,7 @@ class SettingsSiteSettingsListElement extends
    */
   private updateNotificationsLabel_() {
     const state = this.getPref('generated.notification').value;
-    const index = this.categoryList.map(e => e.id).indexOf(
-        ContentSettingsTypes.NOTIFICATIONS);
+    const index = this.categoryMap_.get(ContentSettingsTypes.NOTIFICATIONS);
 
     // The notification row might not be part of the current site-settings-list
     // but the class always observes the preference.
@@ -275,8 +286,7 @@ class SettingsSiteSettingsListElement extends
   private updateSiteDataLabel_() {
     const state =
         this.getPref('generated.cookie_default_content_setting').value;
-    const index = this.categoryList.map(e => e.id).indexOf(
-        ContentSettingsTypes.SITE_DATA);
+    const index = this.categoryMap_.get(ContentSettingsTypes.SITE_DATA);
 
     // The site data row might not be part of the current site-settings-list
     // but the class always observes the preference.
@@ -300,8 +310,7 @@ class SettingsSiteSettingsListElement extends
    * Update the third-party cookies link row label when the pref changes.
    */
   private updateThirdPartyCookiesLabel_() {
-    const index =
-        this.categoryList.map(e => e.id).indexOf(ContentSettingsTypes.COOKIES);
+    const index = this.categoryMap_.get(ContentSettingsTypes.COOKIES);
     // The third-party cookies might not be part of the current
     // site-settings-list but the class always observes the preference.
     if (index === -1) {
@@ -339,8 +348,8 @@ class SettingsSiteSettingsListElement extends
     }
 
     const enabled = this.getPref('compose.proactive_nudge_enabled').value;
-    const index = this.categoryList.map(e => e.id).indexOf(
-        ContentSettingsTypes.OFFER_WRITING_HELP);
+    const index =
+        this.categoryMap_.get(ContentSettingsTypes.OFFER_WRITING_HELP);
 
     // The writing help data row might not be part of the current
     // site-settings-list but the class always observes the preference.
