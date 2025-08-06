7f1d5f44a01efae86c883ab52263755adf612fe8
Jérôme Lebel <jlebel@chromium.org>
2025-08-06 05:18:39 -0700

    [iOS] Remove LogoVendor protocol
    LogoVendor protocol is not used anymore after LogoController was
    removed:
    crrev.com/i/8459977
    
    And remove all unneeded dependency on:
    //ios/chrome/browser/ntp/ui_bundled:logo
    
    Bug: 40820398, 423883582
    Change-Id: I9c0fc7f5b73541839fbde7a47100c092ad721e8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6818238
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1497509}

--- ios/chrome/browser/browser_view/ui_bundled/browser_view_controller.mm
+++ ios/chrome/browser/browser_view/ui_bundled/browser_view_controller.mm
@@ -2827,12 +2827,6 @@ const CGFloat kTopDynamicIslandInset = 24;
   }
 }
 
-#pragma mark - LogoAnimationControllerOwnerOwner (Public)
-
-- (id<LogoAnimationControllerOwner>)logoAnimationControllerOwner {
-  return nil;
-}
-
 #pragma mark - FindBarPresentationDelegate
 
 - (void)setHeadersForFindBarCoordinator:
