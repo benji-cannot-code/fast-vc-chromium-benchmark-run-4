26806d9f7f4014d52d584ab743ea78f5e2954d8d
Kelsen Liu <kelsenliu@chromium.org>
2025-08-05 19:29:23 -0700

    Spanify some functions in gpu/command_buffer/client
    Bug: 40285824
    Change-Id: Id1d5dd97ccf440d04e2951886bf1b7ff9cf2587b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6813630
    Owners-Override: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: kelsen liu <kelsenliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1497320}

--- gpu/command_buffer/tests/gl_helper_unittest.cc
+++ gpu/command_buffer/tests/gl_helper_unittest.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/compiler_specific.h"
 #include "base/functional/bind.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/ref_counted_memory.h"
@@ -671,7 +672,8 @@ class GLHelperTest : public testing::Test {
 
     EXPECT_TRUE(ReadBackTexture(
         dst_texture, gfx::Rect(scaled_size),
-        static_cast<unsigned char*>(output_pixels.getPixels()),
+        UNSAFE_TODO(base::span(static_cast<uint8_t*>(output_pixels.getPixels()),
+                               output_pixels.computeByteSize())),
         output_pixels.rowBytes(), flip_output, kRGBA_8888_SkColorType));
 
     // If the bitmap shouldn't have changed - compare against input.
@@ -756,7 +758,8 @@ class GLHelperTest : public testing::Test {
 
     EXPECT_TRUE(ReadBackTexture(
         dst_texture, gfx::Rect(entire_output_size),
-        static_cast<unsigned char*>(entire_output.getPixels()),
+        UNSAFE_TODO(base::span(static_cast<uint8_t*>(entire_output.getPixels()),
+                               entire_output.computeByteSize())),
         entire_output.rowBytes(), /*flip_y=*/false, kRGBA_8888_SkColorType));
 
     const std::string human_readable_test_params = base::StringPrintf(
@@ -799,7 +802,9 @@ class GLHelperTest : public testing::Test {
                               kRGBA_8888_SkColorType, kPremul_SkAlphaType));
         EXPECT_TRUE(ReadBackTexture(
             dst_texture, gfx::Rect(patch_size),
-            static_cast<unsigned char*>(patch_output.getPixels()),
+            UNSAFE_TODO(
+                base::span(static_cast<uint8_t*>(patch_output.getPixels()),
+                           patch_output.computeByteSize())),
             patch_output.rowBytes(), /*flip_y=*/false, kRGBA_8888_SkColorType));
         SkBitmap expected;
         SkIRect expected_subrect{patch_rect.x(), patch_rect.y(),
@@ -843,7 +848,9 @@ class GLHelperTest : public testing::Test {
           gl_->DeleteTextures(1, &src_subset_texture);
           EXPECT_TRUE(ReadBackTexture(
               dst_texture, gfx::Rect(patch_size),
-              static_cast<unsigned char*>(patch_output.getPixels()),
+              UNSAFE_TODO(
+                  base::span(static_cast<uint8_t*>(patch_output.getPixels()),
+                             patch_output.computeByteSize())),
               patch_output.rowBytes(), /*flip_y=*/false,
               kRGBA_8888_SkColorType));
           Compare(&expected, &patch_output, 2, test_bitmap.get(), stages,
@@ -996,7 +1003,7 @@ class GLHelperTest : public testing::Test {
 
   bool ReadBackTexture(GLuint src_texture,
                        const gfx::Rect& src_rect,
-                       unsigned char* pixels,
+                       base::span<uint8_t> pixels,
                        size_t pixels_stride,
                        bool flip_y,
                        SkColorType color_type) {
@@ -1042,7 +1049,9 @@ class GLHelperTest : public testing::Test {
     // Initialize the output bitmap with Green color.
     // When the readback is over output bitmap should have the red color.
     output_pixels.eraseColor(SK_ColorGREEN);
-    uint8_t* pixels = static_cast<uint8_t*>(output_pixels.getPixels());
+    auto pixels =
+        UNSAFE_TODO(base::span(static_cast<uint8_t*>(output_pixels.getPixels()),
+                               output_pixels.computeByteSize()));
     if (!ReadBackTexture(src_texture, gfx::Rect(src_size), pixels,
                          output_pixels.rowBytes(), /*flip_y=*/false,
                          color_type) ||
