8f1ebdab1799b982edf38adca567faad4ce36074
Hailey Wang <haileywang@google.com>
2025-08-06 13:31:52 -0700

    [Comments] Add public apis for personal collaboration data service
    Bug: 433329690
    Change-Id: I3c62b9e24e686c0963e23c1d1891bea3854e1adc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6797890
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1497813}

--- components/data_sharing/internal/personal_collaboration_data/personal_collaboration_data_service_impl.cc
+++ components/data_sharing/internal/personal_collaboration_data/personal_collaboration_data_service_impl.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/data_sharing/internal/personal_collaboration_data/personal_collaboration_data_service_impl.h"
 
+#include "base/notreached.h"
+
 namespace data_sharing::personal_collaboration_data {
 
 PersonalCollaborationDataServiceImpl::PersonalCollaborationDataServiceImpl() =
@@ -13,8 +15,41 @@ PersonalCollaborationDataServiceImpl::PersonalCollaborationDataServiceImpl() =
 PersonalCollaborationDataServiceImpl::~PersonalCollaborationDataServiceImpl() =
     default;
 
+void PersonalCollaborationDataServiceImpl::AddObserver(Observer* observer) {
+  observers_.AddObserver(observer);
+}
+
+void PersonalCollaborationDataServiceImpl::RemoveObserver(Observer* observer) {
+  observers_.RemoveObserver(observer);
+}
+
+std::optional<sync_pb::SharedTabGroupAccountDataSpecifics>
+PersonalCollaborationDataServiceImpl::GetSpecifics(
+    SpecificsType specifics_type,
+    const std::string& storage_key) {
+  // TODO(haileywang): Implement actual logic.
+  return sync_pb::SharedTabGroupAccountDataSpecifics();
+}
+
+void PersonalCollaborationDataServiceImpl::CreateOrUpdateSpecifics(
+    SpecificsType specifics_type,
+    const std::string& storage_key,
+    const sync_pb::SharedTabGroupAccountDataSpecifics& specifics) {
+  // TODO(haileywang): Implement actual logic.
+  NOTREACHED();
+}
+
+void PersonalCollaborationDataServiceImpl::DeleteSpecifics(
+    SpecificsType specifics_type,
+    const std::string& storage_key) {
+  // TODO(haileywang): Implement actual logic.
+  NOTREACHED();
+}
+
 bool PersonalCollaborationDataServiceImpl::IsInitialized() const {
-  return false;
+  // TODO(haileywang): Implement actual logic to check if the service has
+  // finished loading initial data or is ready for use.
+  return is_initialized_;
 }
 
 }  // namespace data_sharing::personal_collaboration_data
