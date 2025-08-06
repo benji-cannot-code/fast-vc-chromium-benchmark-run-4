f80b728b99754ec823679d35ad7d6b3215fcf124
Nina Satragno <nsatragno@chromium.org>
2025-08-06 15:56:13 -0700

    [webauthn] Roll enclave
    Bug: 399818721
    Change-Id: Id74b3bc2ee6b2667544a2cba1e632c59965f8b91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6821415
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1497903}

--- third_party/cloud_authenticator/processor/src/passkeys.rs
+++ third_party/cloud_authenticator/processor/src/passkeys.rs
@@ -483,6 +483,8 @@ fn validate_pin(
     Ok(())
 }
 
+/// This is intended to be removed in favour of recovery_key_store.rs'
+/// do_wrap_pin_and_secret.
 pub(crate) fn do_wrap_pin(
     metrics: &mut MetricsUpdate,
     auth: &Authentication,
