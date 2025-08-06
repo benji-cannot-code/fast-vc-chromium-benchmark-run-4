2ff78d13483138ba8196032a6f183279147b5ade
Xiaohui Chen <xiaohuic@google.com>
2025-08-06 09:59:27 -0700

    assistant: cleanup code 11 of N
    * remove assistant optin page and related OOBE code
    
    NO_IFTTT=removing metrics code, but keeping histogram xml to annotate existing data.
    
    Bug: 388361414
    Change-Id: I527d922f6d90f267fb915831d041cc271b8102f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6819543
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Yuki Awano <yawano@google.com>
    Cr-Commit-Position: refs/heads/main@{#1497672}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -2139,11 +2139,6 @@ BASE_FEATURE(kFeatureManagementOobeSimon,
              "FeatureManagementOobeSimon",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-// Enables Skipping the assistant setup screen in OOBE.
-BASE_FEATURE(kOobeSkipAssistant,
-             "OobeSkipAssistant",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 // Enables or disables the OOBE QuickStart flow on the login screen.
 BASE_FEATURE(kOobeQuickStartOnLoginScreen,
              "OobeQuickStartOnLoginScreen",
@@ -4135,10 +4130,6 @@ bool IsBootAnimationEnabled() {
 
 bool IsOobeAddUserDuringEnrollmentEnabled() {
   return base::FeatureList::IsEnabled(kOobeAddUserDuringEnrollment);
-}
-
-bool IsOobeSkipAssistantEnabled() {
-  return base::FeatureList::IsEnabled(kOobeSkipAssistant);
 }
 
 bool IsOobeChoobeEnabled() {
