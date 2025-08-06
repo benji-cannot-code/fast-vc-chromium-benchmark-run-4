399bdb603cfbdd8a4cb0bb2bdec1e9a286876d27
Elly <ellyjones@chromium.org>
2025-08-06 12:13:29 -0700

    extensions: drop unused RSAPrivateKey includes
    Bug: None
    Change-Id: I64f785ee5b604f5856cbe38b261bd763213a85b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6823153
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1497766}

--- extensions/browser/extension_creator_unittest.cc
+++ extensions/browser/extension_creator_unittest.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/scoped_temp_dir.h"
 #include "base/path_service.h"
 #include "base/values.h"
-#include "crypto/rsa_private_key.h"
 #include "extensions/common/constants.h"
 #include "extensions/common/extension_paths.h"
 #include "extensions/strings/grit/extensions_strings.h"
