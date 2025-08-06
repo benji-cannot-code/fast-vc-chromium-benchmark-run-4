b5b4aafb9dc4bf971cc6c0f36466fde6b1f9150a
Sylvain Defresne <sdefresne@chromium.org>
2025-08-06 09:02:53 -0700

    [ios] Add PrerenderTabHelper and PrerenderTabHelperDelegate
    In preparation for convertion PrerenderService into a BrowserAgent,
    introduce PrerenderTabHelper and PrerenderTabHelperDelegate. This
    will allow code to check whether a tab is used for pre-rendering
    without having to know for which Browser it is used (e.g. download
    needs to be cancelled for pre-rendered tabs but the logic to do so
    is not aware of Browser).
    
    Bug: 40726702
    Change-Id: Icf710cae90d3379c8c1b51d3cc31ef1a0cfe8050
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6823518
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1497633}

---
+++ ios/chrome/browser/prerender/model/prerender_tab_helper_delegate.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_PRERENDER_MODEL_PRERENDER_TAB_HELPER_DELEGATE_H_
+#define IOS_CHROME_BROWSER_PRERENDER_MODEL_PRERENDER_TAB_HELPER_DELEGATE_H_
+
+// Delegate for the PrerenderTabHelper.
+class PrerenderTabHelperDelegate {
+ public:
+  PrerenderTabHelperDelegate() = default;
+
+  PrerenderTabHelperDelegate(const PrerenderTabHelperDelegate&) = delete;
+  PrerenderTabHelperDelegate& operator=(const PrerenderTabHelperDelegate&) =
+      delete;
+
+  virtual ~PrerenderTabHelperDelegate() = default;
+
+  // Cancel the prerender.
+  virtual void CancelPrerender() = 0;
+};
+
+#endif  // IOS_CHROME_BROWSER_PRERENDER_MODEL_PRERENDER_TAB_HELPER_DELEGATE_H_
