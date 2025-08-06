3b0a727f2f9c959472395c35c1ac877cd1094a85
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2025-08-06 11:00:00 -0700

    Revert "Add keyboard shortcut to reorder focused tab strip item"
    This reverts commit ebea59174ec8df586cfe8da91357d9ffd628ecce.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5659844137713664
    
    Sample build with failed test: https://ci.chromium.org/b/8707272724644913633
    Affected test(s):
    [ninja://chrome/android:chrome_public_test_apk/org.chromium.chrome.browser.compositor.PerformancePTTest#zeroCompositorFramesWhileScrollingBrowserControls](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Fandroid:chrome_public_test_apk%2Forg.chromium.chrome.browser.compositor.PerformancePTTest%23zeroCompositorFramesWhileScrollingBrowserControls?q=VHash%3A32b68f5b6a345446)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5659844137713664&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F6792438&type=BUG
    
    Original change's description:
    > Add keyboard shortcut to reorder focused tab strip item
    >
    > Demo: https://screencast.googleplex.com/cast/NTM2ODQ4NDM3OTc1NDQ5NnwxM2FjNGVkMS01Yg
    >
    > Demo with slow reordering animations: https://screencast.googleplex.com/cast/NDY2ODg5OTMwMjgzNDE3NnwxYjBjYzYwOS00OQ
    >
    > NO_IFTTT=Changing label only to mark as implemented, not changing buckets
    >
    > Bug: 402775002, 40916028
    > Change-Id: I30926eb29a9712b26ba8542e49de4ee4e99954e4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6792438
    > Reviewed-by: Neil Coronado <nemco@google.com>
    > Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    > Commit-Queue: Jenna Himawan <jhimawan@google.com>
    > Reviewed-by: Evan Liu <evliu@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1497605}
    >
    
    Bug: 402775002, 40916028
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I5e2a4614fde38abff396109e31f286db6e8cd071
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6823357
    Owners-Override: Noah Rose Ledesma <noahrose@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Cr-Commit-Position: refs/heads/main@{#1497704}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelperManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelperManager.java
@@ -550,7 +550,6 @@ public class StripLayoutHelperManager
         mNormalHelper =
                 new StripLayoutHelper(
                         context,
-                        this,
                         managerHost,
                         updateHost,
                         renderHost,
@@ -569,7 +568,6 @@ public class StripLayoutHelperManager
         mIncognitoHelper =
                 new StripLayoutHelper(
                         context,
-                        this,
                         managerHost,
                         updateHost,
                         renderHost,
@@ -1715,17 +1713,6 @@ public class StripLayoutHelperManager
      */
     public boolean openKeyboardFocusedContextMenu() {
         return getActiveStripLayoutHelper().openKeyboardFocusedContextMenu();
-    }
-
-    /**
-     * Reorders the currently keyboard-focused item, if applicable.
-     *
-     * @param toLeft Whether the focused item should be reordered to the left (note: this is still
-     *     left in RTL).
-     * @return Whether the item was successfully reordered.
-     */
-    public boolean reorderKeyboardFocusedItem(boolean toLeft) {
-        return getActiveStripLayoutHelper().moveSelectedStripView(toLeft);
     }
 
     private boolean isActivityInXrFullSpaceModeNow() {
