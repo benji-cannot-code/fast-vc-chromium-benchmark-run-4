ff1e4c1d5556b4bdbdf3998c32a82db9872f7e77
Liza Bipin <mlbipin@google.com>
2025-08-06 08:53:51 -0700

    [Signin][Android]Remove duplicated code in SystemAccountManagerDelegate
    Rename the class to be more reflective of current implementation. See
    crbug.com/429143376#comment3 for more context.
    
    Bug: 429143376
    Change-Id: I7883533dd7edc61516f24064bb457eb23752e10e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6818502
    Commit-Queue: Liza Bipin <mlbipin@google.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Liza Bipin <mlbipin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1497626}

--- components/signin/public/android/java/src/org/chromium/components/signin/AccountManagerFacadeProvider.java
+++ components/signin/public/android/java/src/org/chromium/components/signin/AccountManagerFacadeProvider.java
@@ -28,7 +28,7 @@ public class AccountManagerFacadeProvider {
             AccountManagerDelegate delegate =
                     ServiceLoaderUtil.maybeCreate(AccountManagerDelegate.class);
             if (delegate == null) {
-                delegate = new SystemAccountManagerDelegate();
+                delegate = new NullAccountManagerDelegate();
             }
             INSTANCE = new AccountManagerFacadeImpl(delegate);
         }
