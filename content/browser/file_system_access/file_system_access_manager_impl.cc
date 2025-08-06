07e88e2eac52002aa05ddc6c8aed78487c95546e
Ming-Ying Chung <mych@chromium.org>
2025-08-05 22:04:52 -0700

    [FSA] Update FileSystemFileAccessHandle.CreateFileWriter() to write only permission.
    - It powers the Web API `FileSystemFileHandle.createWritable()`, and
      currently requests `readwrite` permission.
    - This CL updates it to `write`-only permission after the feature
      `kFileSystemAccessWriteMode` is enabled. This flag currently off so
      there should be no impact at the moment.
    - The call path now of the method now only checks write status instead
      of read&write status.
    
    
    Bug: 40276567
    Change-Id: I2e04d0a665169c84536e76dd857ec1d18b96c2ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6803157
    Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1497354}

--- content/browser/file_system_access/file_system_access_manager_impl.cc
+++ content/browser/file_system_access/file_system_access_manager_impl.cc
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/thread_pool.h"
 #include "base/types/expected_macros.h"
 #include "base/unguessable_token.h"
-#include "base/uuid.h"
 #include "build/build_config.h"
 #include "components/services/storage/public/cpp/buckets/bucket_id.h"
 #include "components/services/storage/public/cpp/buckets/bucket_locator.h"
@@ -64,6 +63,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "storage/browser/file_system/file_system_url.h"
 #include "storage/browser/quota/quota_manager_proxy.h"
 #include "storage/common/file_system/file_system_types.h"
+#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/common/storage_key/storage_key.h"
 #include "third_party/blink/public/mojom/file_system_access/file_system_access_data_transfer_token.mojom.h"
 #include "third_party/blink/public/mojom/file_system_access/file_system_access_error.mojom.h"
@@ -2069,6 +2069,15 @@ base::WeakPtr<FileSystemAccessManagerImpl>
 FileSystemAccessManagerImpl::AsWeakPtr() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   return weak_factory_.GetWeakPtr();
+}
+
+// static
+blink::mojom::FileSystemAccessPermissionMode
+FileSystemAccessManagerImpl::GetEffectiveWritePermissionMode() {
+  return base::FeatureList::IsEnabled(
+             blink::features::kFileSystemAccessWriteMode)
+             ? blink::mojom::FileSystemAccessPermissionMode::kWrite
+             : blink::mojom::FileSystemAccessPermissionMode::kReadWrite;
 }
 
 bool FileSystemAccessManagerImpl::IsSafePathComponent(
--- content/browser/file_system_access/file_system_access_manager_impl.h
+++ content/browser/file_system_access/file_system_access_manager_impl.h
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/file_system_access/file_system_access_file_writer.mojom.h"
 #include "third_party/blink/public/mojom/file_system_access/file_system_access_manager.mojom.h"
 #include "third_party/blink/public/mojom/file_system_access/file_system_access_observer_host.mojom.h"
+#include "third_party/blink/public/mojom/file_system_access/file_system_access_permission_mode.mojom.h"
 #include "third_party/blink/public/mojom/permissions/permission_status.mojom.h"
 
 namespace blink {
@@ -389,6 +390,13 @@ class CONTENT_EXPORT FileSystemAccessManagerImpl
   // correspond to a "real" file path and not a virtual path in a sandboxed file
   // system.
   storage::FileSystemURL CreateFileSystemURLFromPath(const PathInfo& path_info);
+
+  // Returns the effective permission mode for operations that require write
+  // access. This currently returns kReadWrite, but will switch to kWrite only
+  // when the FileSystemAccessWriteMode feature is fully rolled out.
+  // See https://crbug.com/40276567.
+  static blink::mojom::FileSystemAccessPermissionMode
+  GetEffectiveWritePermissionMode();
 
   void Shutdown();
 
