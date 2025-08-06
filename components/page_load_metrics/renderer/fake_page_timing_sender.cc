828f8a196c58d278685891240aafa05258dd764c
Johannes Henkel <johannes@chromium.org>
2025-08-06 11:50:50 -0700

    [soft navs] DebugString(...) for PageLoadMetrics mojom struct.
    This adds simple DebugString routines that I find valuable for
    debugging the UKM reporting; the change adds one invocation
    in the fake_page_timing_sender.cc, which is used in the unittests,
    but it's super easy to temporarily add
    LOG(INFO) << DebugString(*soft_navigation_metrics);
    wherever it's needed / useful.
    
    Bug: 382245480
    
    Change-Id: I7a09d82977bf0a62ec811904814995c5e18d4f2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6823314
    Commit-Queue: Johannes Henkel <johannes@chromium.org>
    Reviewed-by: Michal Mocny <mmocny@chromium.org>
    Reviewed-by: Scott Haseley <shaseley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1497729}

--- components/page_load_metrics/renderer/fake_page_timing_sender.cc
+++ components/page_load_metrics/renderer/fake_page_timing_sender.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
+#include "components/page_load_metrics/common/page_load_metrics_debug_string.h"
 #include "testing/gmock/include/gmock/gmock-matchers.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -88,8 +89,12 @@ void FakePageTimingSender::PageTimingValidator::
             *expected_soft_navigation_metrics_.at(i))) {
       continue;
     }
-    ADD_FAILURE() << "Observed soft navigation metric != expected one at index "
-                  << i;
+    ADD_FAILURE() << "Actual soft navigation metric != expected at index "
+                  << i << "\n"
+                  << "Actual: "
+                  << DebugString(*actual_soft_navigation_metrics_.at(i))
+                  << "\nExpected: "
+                  << DebugString(*expected_soft_navigation_metrics_.at(i));
   }
 }
 
