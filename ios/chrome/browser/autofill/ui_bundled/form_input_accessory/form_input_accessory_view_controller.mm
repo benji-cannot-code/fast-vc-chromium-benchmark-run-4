53987a91397d946aa6cc1a76d09f0783a5919e6b
Alexis Hetu <sugoi@chromium.org>
2025-08-06 12:28:58 -0700

    [iOS] Fix keyboard accessory layout issues
    A few KA issues sometimes happened:
    - Bad max width on payments suggestions
    - Extra space between bottom omnibox and KA
    These seem to be fixed by a well timed "layoutIfNeeded" call.
    
    Bug: 422513902
    Change-Id: I6c78f514bf29f18385361b711166241cc587a104
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6824903
    Reviewed-by: Noémie St-Onge <noemies@google.com>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1497774}

--- ios/chrome/browser/autofill/ui_bundled/form_input_accessory/form_input_accessory_view_controller.mm
+++ ios/chrome/browser/autofill/ui_bundled/form_input_accessory/form_input_accessory_view_controller.mm
@@ -209,6 +209,12 @@ void LogManualFallbackEntryThroughExpandIcon(ManualFillDataType data_type,
   _keyboardWasClosed = NO;
 }
 
+- (void)viewWillAppear:(BOOL)animated {
+  [super viewWillAppear:animated];
+
+  [self.formInputAccessoryView layoutIfNeeded];
+}
+
 - (void)viewDidDisappear:(BOOL)animated {
   [super viewDidDisappear:animated];
 
