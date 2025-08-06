d2830d6cdfed639486100669e6a142dd9ac493d3
Piotr Domaradzki <doland@google.com>
2025-08-06 04:29:32 -0700

    Remove an unnecessary copy.
    The signature had a const std::u16string parameter which sounds like a
    typo.
    
    Bug: 40100455
    Change-Id: I9a930c8194ddfbaf6634c02f07b992b0f5b2a4fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6819964
    Commit-Queue: Piotr Domaradzki <doland@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Karol Sygiet <sygiet@google.com>
    Cr-Commit-Position: refs/heads/main@{#1497490}

--- components/autofill/core/browser/data_model/addresses/autofill_structured_address_utils.cc
+++ components/autofill/core/browser/data_model/addresses/autofill_structured_address_utils.cc
@@ -409,7 +409,7 @@ bool AreSortedTokensEqual(const std::vector<AddressToken>& first,
          SortedTokenComparisonStatus::kMatch;
 }
 
-std::vector<AddressToken> TokenizeValue(const std::u16string value) {
+std::vector<AddressToken> TokenizeValue(std::u16string_view value) {
   std::vector<AddressToken> tokens;
   int index = 0;
 
@@ -423,7 +423,7 @@ std::vector<AddressToken> TokenizeValue(const std::u16string value) {
         SplitByRegex(value, **regex, value.size());
     if (!parts.has_value()) {
       return {AddressToken{
-          .value = value,
+          .value = std::u16string(value),
           .normalized_value =
               AutofillProfileComparator::NormalizeForComparison(value),
           .position = 0}};
