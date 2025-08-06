515509fc8d8d0eeb85233c855548a37ad3ff2684
Ron Sailer <rsailer@google.com>
2025-08-06 07:42:30 -0700

    Fix missing fingerprinting protection pref registration on iOS.
    Ensure tracking_protection_3pcd_enabled is not being accessed if not
    registered, as previously it was immediately accessed during the
    construction of the TrackingProtectionSettings.
    
    Bug: 428754923, 434992778
    Change-Id: Ie8fdae0e04f1e272a52a5a24dd4680f37da7203f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6817468
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Ron Sailer <rsailer@chromium.org>
    Reviewed-by: Fiona Macintosh <fmacintosh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1497577}

--- ios/chrome/browser/profile/model/keyed_service_factories.mm
+++ ios/chrome/browser/profile/model/keyed_service_factories.mm
@@ -109,6 +109,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/power_bookmarks/model/power_bookmark_service_factory.h"
 #import "ios/chrome/browser/prerender/model/prerender_service_factory.h"
 #import "ios/chrome/browser/price_insights/model/price_insights_model_factory.h"
+#import "ios/chrome/browser/privacy_sandbox/tracking_protection_settings_factory.h"
 #import "ios/chrome/browser/promos_manager/model/promos_manager_factory.h"
 #import "ios/chrome/browser/push_notification/model/provisional_push_notification_service_factory.h"
 #import "ios/chrome/browser/push_notification/model/push_notification_profile_service_factory.h"
@@ -364,6 +365,7 @@ void EnsureProfileKeyedServiceFactoriesBuilt() {
   TextClassifierModelServiceFactory::GetInstance();
   TextToSpeechPlaybackControllerFactory::GetInstance();
   TipsManagerIOSFactory::GetInstance();
+  TrackingProtectionSettingsFactory::GetInstance();
   LanguageDetectionModelServiceFactory::GetInstance();
   TrustedVaultClientBackendFactory::GetInstance();
   UnifiedConsentServiceFactory::GetInstance();
