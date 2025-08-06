68a4f40f45095e8425bbdd100c3e2c7b30bbe491
Michael Wilson <mjwilson@chromium.org>
2025-08-06 15:48:33 -0700

    More miscellaneous cleanup in WebAudio codebase
    This should cause no functional change.
    
    This CL performs some follow-up fixes promised in previous CLs:
    - crrev.com/c/3565058
    - crrev.com/c/3583279
    - crrev.com/c/3577934
    
    Fixes include:
    - Convert DCHECK(0) to NOTREACHED()
    - Change declaration order
    - Remove comment that is no longer applicable
    
    Change-Id: I95e3a86e64fffbd566f209a77a6b921f36e80cae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6825622
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1497902}

--- third_party/blink/renderer/modules/webaudio/audio_param_handler.h
+++ third_party/blink/renderer/modules/webaudio/audio_param_handler.h
@@ -97,13 +97,6 @@ class AudioParamHandler final : public ThreadSafeRefCounted<AudioParamHandler>,
     kVariable
   };
 
-  AudioParamType GetParamType() const { return param_type_; }
-  void SetParamType(AudioParamType);
-  // Set the parameter name for an AudioWorklet.
-  void SetCustomParamName(const String name);
-  // Return a nice name for the AudioParam.
-  String GetParamName() const;
-
   static scoped_refptr<AudioParamHandler> Create(BaseAudioContext& context,
                                                  AudioParamType param_type,
                                                  double default_value,
@@ -160,6 +153,17 @@ class AudioParamHandler final : public ThreadSafeRefCounted<AudioParamHandler>,
     automation_rate_ = automation_rate;
   }
 
+  float DefaultValue() const { return default_value_; }
+  float MinValue() const { return min_value_; }
+  float MaxValue() const { return max_value_; }
+
+  AudioParamType GetParamType() const { return param_type_; }
+  void SetParamType(AudioParamType);
+  // Set the parameter name for an AudioWorklet.
+  void SetCustomParamName(const String name);
+  // Return a nice name for the AudioParam.
+  String GetParamName() const;
+
   bool IsAutomationRateFixed() const {
     return rate_mode_ == AutomationRateMode::kFixed;
   }
@@ -168,10 +172,6 @@ class AudioParamHandler final : public ThreadSafeRefCounted<AudioParamHandler>,
   // calculateSampleAccurateValues() for a-rate.
   // Must be called in the audio thread.
   float FinalValue();
-
-  float DefaultValue() const { return default_value_; }
-  float MinValue() const { return min_value_; }
-  float MaxValue() const { return max_value_; }
 
   // An AudioParam needs sample accurate processing if there are
   // automations scheduled or if there are connections.
@@ -388,6 +388,8 @@ class AudioParamHandler final : public ThreadSafeRefCounted<AudioParamHandler>,
     raw_ptr<const ParamEvent> event;
     const int event_index;
   };
+
+  friend class AudioNodeWiring;
 
   AudioParamHandler(BaseAudioContext&,
                     AudioParamType,
@@ -620,8 +622,6 @@ class AudioParamHandler final : public ThreadSafeRefCounted<AudioParamHandler>,
 
   // Audio bus to sum in any connections to the AudioParam.
   scoped_refptr<AudioBus> summing_bus_;
-
-  friend class AudioNodeWiring;
 };
 
 }  // namespace blink
