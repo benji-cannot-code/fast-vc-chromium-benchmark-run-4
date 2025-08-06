1e7290699bc9725d8cdb1eda07c500261d66b961
Ivan Šandrk <isandrk@chromium.org>
2025-08-06 04:56:18 -0700

    PPDP: Show full path for custom URI PPD
    Implementing feedback from UX for enabling managed PPD settings for printers:
    https://docs.google.com/presentation/d/19qrMDtCY1L_yqtcCG6VaCRWnwTuf3POZovyUaVdA0l0/edit?slide=id.g3438b61cadf_1_47&pli=1#slide=id.g3438b61cadf_1_47
    
    Edit printer dialog screenshot:
    https://drive.google.com/file/d/1HbkAhAXuI55BESv5LnOkH8tR70DL2FG4/view?resourcekey=0-MUXXMrj_W_B2YoyNgFehJg
    
    View printer dialog screenshot:
    https://drive.google.com/file/d/1MAOJ5GJYvuRbMv3MFVKa-uiFuP0R-a7l/view?resourcekey=0-Dw690WkBgFxQcvRFElWvYA
    
    Bug: b:435189109
    Change-Id: I2d9b65680e83d100bd58a051ee92b4e3593fbeb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6819060
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Ivan Šandrk <isandrk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1497502}

--- chrome/browser/resources/ash/settings/os_printing_page/cups_edit_printer_dialog.ts
+++ chrome/browser/resources/ash/settings/os_printing_page/cups_edit_printer_dialog.ts
@@ -34,7 +34,7 @@ import {cast} from '../assert_extras.js';
 import type {Constructor} from '../common/types.js';
 
 import {getTemplate} from './cups_edit_printer_dialog.html.js';
-import {getBaseName, getErrorText, isNameAndAddressValid, isNetworkProtocol, isPPDInfoValid} from './cups_printer_dialog_util.js';
+import {getDisplayPpd, getErrorText, isNameAndAddressValid, isNetworkProtocol, isPPDInfoValid} from './cups_printer_dialog_util.js';
 import type {CupsPrinterInfo, CupsPrintersBrowserProxy, ManufacturersInfo, ModelsInfo, PrinterPpdMakeModel, PrinterSetupResult} from './cups_printers_browser_proxy.js';
 import {CupsPrintersBrowserProxyImpl} from './cups_printers_browser_proxy.js';
 
@@ -233,7 +233,7 @@ export class SettingsCupsEditPrinterDialogElement extends
             this.onGetPrinterPpdManufacturerAndModelFailed_.bind(this));
     this.browserProxy_.getCupsPrinterManufacturersList().then(
         this.manufacturerListChanged_.bind(this));
-    this.userPPD_ = getBaseName(this.pendingPrinter_.printerPPDPath);
+    this.userPPD_ = getDisplayPpd(this.pendingPrinter_.printerPPDPath);
   }
 
   override onActiveNetworksChanged(networks: NetworkStateProperties[]): void {
@@ -463,7 +463,7 @@ export class SettingsCupsEditPrinterDialogElement extends
       // A new valid PPD file should be treated as a saveable change.
       this.onPrinterInfoChange_();
     }
-    this.userPPD_ = getBaseName(path);
+    this.userPPD_ = getDisplayPpd(path);
   }
 
   /**
