7862ea809c1d9782fb0b0fa4e91be7765304124d
Gauthier Ambard <gambard@chromium.org>
2025-08-06 01:53:51 -0700

    [iOS] Remove colorful tab group flag
    The flag was never used.
    
    Fixed: 427494354
    Change-Id: Id589eca09513fafe6009f77af8d317c4814eeb7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6818333
    Commit-Queue: Louis Romero <lpromero@google.com>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1497427}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -1242,14 +1242,6 @@ bool IsContainedTabGroupEnabled() {
   return base::FeatureList::IsEnabled(kContainedTabGroup);
 }
 
-BASE_FEATURE(kColorfulTabGroup,
-             "ColorfulTabGroup",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
-bool IsColorfulTabGroupEnabled() {
-  return base::FeatureList::IsEnabled(kColorfulTabGroup);
-}
-
 BASE_FEATURE(kBestOfAppFRE, "BestOfAppFRE", base::FEATURE_DISABLED_BY_DEFAULT);
 
 bool IsBestOfAppFREEnabled() {
