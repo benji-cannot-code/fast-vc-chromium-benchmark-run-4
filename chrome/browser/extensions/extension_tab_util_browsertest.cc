19b446843f8c2b4a2d1ecbe32f8aab6605808e44
Achuith Bhandarkar <achuith@chromium.org>
2025-08-07 14:47:28 -0700

    extensions: Replace NavigateToURLWithDisposition with NavigateToURLInNewTab
    BUG=436871366
    
    Change-Id: I702233cb6fd990f66e2fe3ffe4509605c6983de2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6825806
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1498485}

--- chrome/browser/extensions/extension_tab_util_browsertest.cc
+++ chrome/browser/extensions/extension_tab_util_browsertest.cc
@@ -205,9 +205,7 @@ IN_PROC_BROWSER_TEST_F(ExtensionTabUtilBrowserTest,
 
   // If the user navigates to the options page e.g. by typing in the url, it
   // should not override the currently-open tab.
-  ui_test_utils::NavigateToURLWithDisposition(
-      browser(), options_url, WindowOpenDisposition::NEW_FOREGROUND_TAB,
-      ui_test_utils::BROWSER_TEST_WAIT_FOR_LOAD_STOP);
+  NavigateToURLInNewTab(options_url);
   EXPECT_EQ(4, browser()->tab_strip_model()->count());
   EXPECT_EQ(options_url, GetActiveUrl(browser()));
 
@@ -487,10 +485,7 @@ class SharedTabGroupExtensionsTabUtilTest : public ExtensionTabUtilBrowserTest {
   // Adds tab navigated to |url| in the given |browser|.
   tabs::TabInterface* AddTab(const GURL& url) {
     return browser()->tab_strip_model()->GetTabForWebContents(
-        content::WebContents::FromRenderFrameHost(
-            ui_test_utils::NavigateToURLWithDisposition(
-                browser(), url, WindowOpenDisposition::NEW_FOREGROUND_TAB,
-                ui_test_utils::BROWSER_TEST_WAIT_FOR_LOAD_STOP)));
+        content::WebContents::FromRenderFrameHost(NavigateToURLInNewTab(url)));
   }
 
   tab_groups::TabGroupId CreateTabGroup() {
