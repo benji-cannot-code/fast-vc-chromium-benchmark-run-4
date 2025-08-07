25b708d7220621a4860786b2dd549bbed6577fa3
Xinghui Lu <xinghuilu@chromium.org>
2025-08-07 13:42:03 -0700

    [CLAMS] Add an additional wrapper to handle JNI for AiCoreModelDownloader.
    The idea is very similar to https://crrev.com/c/6820301. Although we
    don't need to cast any object in this case, adding this layer
    preemptively before the downstream repo has implemented the interface.
    It will be a lot harder to restructure the interface without breaking
    downstream going forward.
    
    Bug: 425408635
    Change-Id: I1edc4c342a2f52dbd51fd790c989389df05b7d93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6825329
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1498445}

---
+++ services/on_device_model/android/java/src/org/chromium/on_device_model/DownloaderResponder.java
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.on_device_model;
+
+import org.chromium.build.annotations.NullMarked;
+
+/** A responder for the backend to send download status. */
+@NullMarked
+public interface DownloaderResponder {
+    /**
+     * Called when the download has completed and the status has become available. Called at most
+     * once. Not called if onUnavailable is called.
+     *
+     * <p>TODO(crbug.com/425408635): Return the base model name and version.
+     */
+    void onAvailable();
+
+    /**
+     * Called when the model is unavailable. Called at most once. Not called if onAvailable is
+     * called.
+     *
+     * <p>TODO(crbug.com/425408635): Return the error reason.
+     */
+    void onUnavailable();
+}
--- services/on_device_model/android/model_downloader_android.cc
+++ services/on_device_model/android/model_downloader_android.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/on_device_model/android/on_device_model_bridge.h"
 
 // Must come after all headers that specialize FromJniType() / ToJniType().
-#include "services/on_device_model/android/jni_headers/AiCoreModelDownloader_jni.h"
+#include "services/on_device_model/android/jni_headers/AiCoreModelDownloaderWrapper_jni.h"
 
 namespace on_device_model {
 
@@ -19,7 +19,7 @@ ModelDownloaderAndroid::ModelDownloaderAndroid(
 
 ModelDownloaderAndroid::~ModelDownloaderAndroid() {
   JNIEnv* env = base::android::AttachCurrentThread();
-  Java_AiCoreModelDownloader_onNativeDestroyed(env, java_downloader_);
+  Java_AiCoreModelDownloaderWrapper_onNativeDestroyed(env, java_downloader_);
 }
 
 void ModelDownloaderAndroid::StartDownload(
@@ -28,8 +28,8 @@ void ModelDownloaderAndroid::StartDownload(
       << "StartDownload() can only be called once.";
   on_download_complete_callback_ = std::move(on_download_complete_callback);
   JNIEnv* env = base::android::AttachCurrentThread();
-  Java_AiCoreModelDownloader_startDownload(env, java_downloader_,
-                                           reinterpret_cast<intptr_t>(this));
+  Java_AiCoreModelDownloaderWrapper_startDownload(
+      env, java_downloader_, reinterpret_cast<intptr_t>(this));
 }
 
 void ModelDownloaderAndroid::OnAvailable() {
@@ -40,14 +40,16 @@ void ModelDownloaderAndroid::OnUnavailable() {
   std::move(on_download_complete_callback_).Run(false);
 }
 
-void JNI_AiCoreModelDownloader_OnAvailable(JNIEnv* env,
-                                           jlong model_downloader_android) {
+void JNI_AiCoreModelDownloaderWrapper_OnAvailable(
+    JNIEnv* env,
+    jlong model_downloader_android) {
   reinterpret_cast<ModelDownloaderAndroid*>(model_downloader_android)
       ->OnAvailable();
 }
 
-void JNI_AiCoreModelDownloader_OnUnavailable(JNIEnv* env,
-                                             jlong model_downloader_android) {
+void JNI_AiCoreModelDownloaderWrapper_OnUnavailable(
+    JNIEnv* env,
+    jlong model_downloader_android) {
   reinterpret_cast<ModelDownloaderAndroid*>(model_downloader_android)
       ->OnUnavailable();
 }
