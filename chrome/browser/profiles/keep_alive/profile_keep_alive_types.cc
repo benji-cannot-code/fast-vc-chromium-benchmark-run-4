6580c3372144007e05044f42bf59b96a53258aa1
Atharv Maan <atharvmaan@chromium.org>
2025-08-07 07:11:45 -0700

    [First Run Bookmark Import] Add ProfileKeepAliveOrigin member
    This CL adds an kWaitingForBookmarksImport member to
    ProfileKeepAliveOrigin enum that will be used to keep Profile alive
    until bookmarks are imported from Initial Preferences on first run.
    
    Fixed: 435990892
    Change-Id: Ib634b45cde83f5bfdcb5c2d085a383e046b2cd2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6816156
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Reviewed-by: Daniel Soromou <koretadaniel@chromium.org>
    Commit-Queue: Atharv Maan <atharvmaan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1498201}

--- chrome/browser/profiles/keep_alive/profile_keep_alive_types.cc
+++ chrome/browser/profiles/keep_alive/profile_keep_alive_types.cc
@@ -84,6 +84,8 @@ std::ostream& operator<<(std::ostream& out,
       return out << "kGlicView";
     case ProfileKeepAliveOrigin::kWaitingForGlicView:
       return out << "kWaitingForGlicView";
+    case ProfileKeepAliveOrigin::kWaitingForBookmarksImportOnFirstRun:
+      return out << "kWaitingForBookmarksImportOnFirstRun";
   }
   NOTREACHED();
 }
