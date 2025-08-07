c9bbdbb59d77ba3cd41a5c54d00e41c75ab89f1d
David Tseng <dtseng@google.com>
2025-08-07 14:12:03 -0700

    android-a11y: ensures scrolls always gets the right node id
    This change:
    
    - re-adds the flagging which seems to have been removed by this
      change[1]. As of that change, the linked issue should be resolved.
    
    - corrects the calls to scrollToMakeNodeVisible such that it will always
      have the right a11y focus id
    
    R=mschillaci@google.com
    
    1. https://chromium-review.googlesource.com/c/chromium/src/+/6640274
    
    Bug: 435748442
    Change-Id: I2fd1d303ec609607b87bcbf9103074acdb584c83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6828523
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1498468}

--- content/public/android/java/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityImpl.java
@@ -1416,11 +1416,11 @@ public class WebContentsAccessibilityImpl extends AccessibilityNodeProviderCompa
                             .getIdForElementAfterElementHostingAutofillPopup(mNativeObj);
             if (id == 0) return;
             if (ContentFeatureList.sAccessibilityDeprecateJavaNodeCacheOptimizeScroll.getValue()) {
-                scrollToMakeNodeVisible(mAccessibilityFocusId);
+                scrollToMakeNodeVisible(id);
                 moveAccessibilityFocusToId(id);
             } else {
                 moveAccessibilityFocusToId(id);
-                scrollToMakeNodeVisible(mAccessibilityFocusId);
+                scrollToMakeNodeVisible(id);
             }
         }
     }
@@ -1466,8 +1466,13 @@ public class WebContentsAccessibilityImpl extends AccessibilityNodeProviderCompa
     @Override
     public void restoreFocus() {
         if (isAccessibilityEnabled() && mLastAccessibilityFocusId != View.NO_ID) {
-            moveAccessibilityFocusToId(mLastAccessibilityFocusId);
-            scrollToMakeNodeVisible(mLastAccessibilityFocusId);
+            if (ContentFeatureList.sAccessibilityDeprecateJavaNodeCacheOptimizeScroll.getValue()) {
+                scrollToMakeNodeVisible(mLastAccessibilityFocusId);
+                moveAccessibilityFocusToId(mLastAccessibilityFocusId);
+            } else {
+                moveAccessibilityFocusToId(mLastAccessibilityFocusId);
+                scrollToMakeNodeVisible(mLastAccessibilityFocusId);
+            }
         }
     }
 
@@ -1518,8 +1523,13 @@ public class WebContentsAccessibilityImpl extends AccessibilityNodeProviderCompa
             WebContentsAccessibilityImplJni.get().setSequentialFocusStartingPoint(mNativeObj, id);
         }
 
-        moveAccessibilityFocusToId(id);
-        scrollToMakeNodeVisible(mAccessibilityFocusId);
+        if (ContentFeatureList.sAccessibilityDeprecateJavaNodeCacheOptimizeScroll.getValue()) {
+            scrollToMakeNodeVisible(id);
+            moveAccessibilityFocusToId(id);
+        } else {
+            moveAccessibilityFocusToId(id);
+            scrollToMakeNodeVisible(mAccessibilityFocusId);
+        }
         return true;
     }
 
