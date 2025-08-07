37171c8df73952e35d144fe55ca240deeadbc4ce
Manish Goregaokar <manishearth@google.com>
2025-08-06 21:16:59 -0700

    Import temporal_rs 0.0.12
    This will require test262 expectation changes once it gets rolled into
    v8; I plan to get approval for this now but only merge this after
    tomorrow's DEPS roll and make a separate CL that manually updates v8 to
    this CL, which contains the other fixes.
    
    Bug: 401065166,436134592
    Change-Id: Iff618914c40ea1e35fff2a1424280575624fe1aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6823634
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Manish Goregaokar <manishearth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1498009}

---
+++ third_party/rust/chromium_crates_io/vendor/temporal_capi-v0_0_12/bindings/c/ParsedDateTime.d.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#ifndef ParsedDateTime_D_H
+#define ParsedDateTime_D_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include "diplomat_runtime.h"
+
+
+
+
+
+typedef struct ParsedDateTime ParsedDateTime;
+
+
+
+
+#endif // ParsedDateTime_D_H
