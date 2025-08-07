bf84e585744323b1cdec105513a7763f325060ad
Ekaterina Kulikova <kate.kulikova@lge.com>
2025-08-07 02:51:00 -0700

    GCC: Add missing include of bound_session_oauth_multilogin_delegate.h in signin_client.cc
    In the CL https://crrev.com/c/6727156 added
    SigninClient::CreateBoundSessionOAuthMultiloginDelegate() but its
    return type of
    std::unique_ptr<signin::BoundSessionOAuthMultiLoginDelegate> requires
    the include in GCC compiler.
    
    Bug: 41455655
    Change-Id: Idab22099af91a16a9e81145dff889c8ddd8857a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6818578
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Ernest Nguyen Hung <ernn@google.com>
    Reviewed-by: Ernest Nguyen Hung <ernn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1498090}

--- components/signin/public/base/signin_client.cc
+++ components/signin/public/base/signin_client.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/signin/public/base/signin_client.h"
 
+#include "components/signin/public/base/bound_session_oauth_multilogin_delegate.h"
+
 void SigninClient::PreSignOut(
     base::OnceCallback<void(SignoutDecision)> on_signout_decision_reached,
     signin_metrics::ProfileSignout signout_source_metric,
