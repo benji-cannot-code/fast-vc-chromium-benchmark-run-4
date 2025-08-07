ca3ce2224a8d54c8682f648bb728172dacb66d64
Greg Thompson <grt@chromium.org>
2025-08-07 00:15:30 -0700

    [A11yPerformance] Enable the UiaProvider by default
    Bug: 41390439, 40065516
    Change-Id: I813c47c1eaa975a2fd4e94af04715d99cfbd1d8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6818985
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1498045}

--- chrome/browser/chrome_content_browser_client_unittest.cc
+++ chrome/browser/chrome_content_browser_client_unittest.cc
@@ -159,6 +159,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IS_WIN)
 #include "base/test/mock_entropy_provider.h"
 #include "chrome/test/base/scoped_metrics_service_for_synthetic_trials.h"
+#include "ui/accessibility/accessibility_features.h"
 #endif
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
@@ -1824,6 +1825,13 @@ class ChromeContentBrowserClientFieldTrialTest
     base::MockEntropyProvider entropy_provider(0.9);
     trial_ = base::FieldTrialList::FactoryGetFieldTrial(
         "UiaProviderWin", 100, "Default_1234", entropy_provider);
+  }
+
+  void SetUp() override {
+    if (features::kUiaProvider.default_state ==
+        base::FEATURE_ENABLED_BY_DEFAULT) {
+      GTEST_SKIP() << "UiaProvider is enabled by default";
+    }
   }
 
   ChromeContentBrowserClient& client() { return client_; }
