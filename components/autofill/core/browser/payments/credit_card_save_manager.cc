36a6b9e3518109a241122dd0f261f9ca0293610c
yiwenqian <yiwenqian@google.com>
2025-08-07 13:10:48 -0700

    [IOS] Add a helper function for save card bottom sheet showing logic
    This CL is the follow-up of comment: https://chromium-review.googlesource.com/c/chromium/src/+/6770736/comment/52e9ef7d_090a7430/ and https://chromium-review.googlesource.com/c/chromium/src/+/6770736/comment/036ec932_ca579ad2/
    
    Change-Id: I36e7921d1feb74c28cedfd9fb733162ef54a5058
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6822817
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Olivia Saul <osaul@google.com>
    Commit-Queue: Yiwen Qian <yiwenqian@google.com>
    Cr-Commit-Position: refs/heads/main@{#1498430}

--- components/autofill/core/browser/payments/credit_card_save_manager.cc
+++ components/autofill/core/browser/payments/credit_card_save_manager.cc
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/metrics/autofill_metrics.h"
 #include "components/autofill/core/browser/metrics/payments/credit_card_save_metrics.h"
 #include "components/autofill/core/browser/payments/autofill_payments_feature_availability.h"
+#include "components/autofill/core/browser/payments/autofill_save_card_ui_info.h"
 #include "components/autofill/core/browser/payments/client_behavior_constants.h"
 #include "components/autofill/core/browser/payments/payments_autofill_client.h"
 #include "components/autofill/core/browser/payments/payments_network_interface.h"
@@ -1631,12 +1632,10 @@ bool CreditCardSaveManager::ShouldRequestCvcInclusiveLegalMessage() const {
 
   int num_strikes = GetCreditCardSaveStrikeDatabase()->GetStrikes(
       base::UTF16ToUTF8(upload_request_.card.LastFourDigits()));
-  bool will_show_bottom_sheet =
-      num_strikes == 0 && !should_request_name_from_user_ &&
-      !should_request_expiration_date_from_user_ &&
-      base::FeatureList::IsEnabled(features::kAutofillSaveCardBottomSheet);
-
-  return !will_show_bottom_sheet;
+  return !autofill::ShouldShowSaveCardBottomSheet(
+             num_strikes, should_request_name_from_user_,
+             should_request_expiration_date_from_user_) ||
+         !base::FeatureList::IsEnabled(features::kAutofillSaveCardBottomSheet);
 #else
   // For other platforms, we only request the CVC-inclusive message if a CVC
   // was present in the form.
