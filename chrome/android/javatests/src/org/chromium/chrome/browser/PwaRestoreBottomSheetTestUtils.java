0a0ba8aaf8e792d73db80e6680944b02df6b8e00
Momoko Hattori <momohatt@google.com>
2025-08-07 03:19:37 -0700

    Remove unused lastUsedInDays from setAppListForTesting JNI method
    This CL removes the unused lastUsedInDays argument from
    setAppListForTesting JNI method. It was introduced in CL:5217232 to test
    the restore UI that splits apps into two groups based on whether they
    are used in the last 30 days, but the UI feature was removed in
    CL:5513003.
    
    Bug: 40281671
    Change-Id: Ia76f263ab486a59b33f3404d8285dd4ad03f704c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6777860
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Glenn Hartmann <hartmanng@chromium.org>
    Commit-Queue: Momoko Hattori <momohatt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1498103}

--- chrome/android/javatests/src/org/chromium/chrome/browser/PwaRestoreBottomSheetTestUtils.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/PwaRestoreBottomSheetTestUtils.java
@@ -33,15 +33,12 @@ public class PwaRestoreBottomSheetTestUtils {
     }
 
     /** Set the app list to use for testing. */
-    public static void setAppListForRestoring(String[][] appList, int[] lastUsedInDays)
-            throws Exception {
+    public static void setAppListForRestoring(String[][] appList) throws Exception {
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
                     PwaRestoreBottomSheetTestUtilsJni.get()
                             .setAppListForRestoring(
-                                    appList,
-                                    lastUsedInDays,
-                                    ProfileManager.getLastUsedRegularProfile());
+                                    appList, ProfileManager.getLastUsedRegularProfile());
                 });
     }
 
@@ -54,7 +51,6 @@ public class PwaRestoreBottomSheetTestUtils {
     interface Natives {
         void waitForWebApkDatabaseInitialization(@JniType("Profile*") Profile profile);
 
-        void setAppListForRestoring(
-                String[][] appList, int[] lastUsedInDays, @JniType("Profile*") Profile profile);
+        void setAppListForRestoring(String[][] appList, @JniType("Profile*") Profile profile);
     }
 }
