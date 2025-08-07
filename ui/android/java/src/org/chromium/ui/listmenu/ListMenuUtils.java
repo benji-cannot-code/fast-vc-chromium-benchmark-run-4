513f38209e2737bb6c685ca4ff62bca0c7ea247e
Jenna Himawan <jhimawan@google.com>
2025-08-07 12:34:09 -0700

    Handle nulls for TITLE field
    Bug: 418807464
    Change-Id: I286e97cd117325c344d4abbd18b9c5f2245363a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6825376
    Reviewed-by: Lijin Shen <lazzzis@google.com>
    Auto-Submit: Jenna Himawan <jhimawan@google.com>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1498408}

--- ui/android/java/src/org/chromium/ui/listmenu/ListMenuUtils.java
+++ ui/android/java/src/org/chromium/ui/listmenu/ListMenuUtils.java
@@ -283,7 +283,8 @@ public class ListMenuUtils {
                 firstItem = mContentModelList.get(0);
             }
             if (firstItem instanceof ListItem firstListItem && firstListItem.model != null) {
-                if (firstListItem.model.containsKey(TITLE)) {
+                if (firstListItem.model.containsKey(TITLE)
+                        && firstListItem.model.get(TITLE) != null) {
                     CharSequence title = firstListItem.model.get(TITLE);
                     if (title.length() != 0) {
                         accessibilityPaneTitle = String.valueOf(title);
