a1924a6d0fcdcacb7ecf02e16876db98dbc2cdae
Justin Lulejian <jlulejian@chromium.org>
2025-08-07 09:53:00 -0700

    [Extensions] Remove `ScopedMultiWorkerCrashKeys` and related debug code
    This change removes the `debug::ScopedMultiWorkerCrashKeys` class and
    all associated helper functions and crash key definitions from
    `worker_id_set.cc` and `worker_id_set.h`. This functionality was used to
    log crash keys when multiple service workers were detected for a single
    extension, but it no longer used.
    
    Also it adds an include to read_anything_service.cc since it was
    transitively relying on the string_number_conversions.h include in
    worker_id_set.cc.
    
    Bug: 40936639
    Fixed: 436883654
    Change-Id: I5a8e32b2114610ddeb920ec3637b2e6bf6eb83d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6824126
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1498303}

--- chrome/browser/ui/views/side_panel/read_anything/read_anything_service.cc
+++ chrome/browser/ui/views/side_panel/read_anything/read_anything_service.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check_is_test.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/strings/string_number_conversions.h"
 #include "chrome/browser/accessibility/embedded_a11y_extension_loader.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/extensions/extension_service.h"
