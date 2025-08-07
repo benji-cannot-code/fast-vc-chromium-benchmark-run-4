697795d66d703ffee99f6546a7c8eb56f2660aa0
Christine Hollingsworth <christinesm@chromium.org>
2025-08-07 14:36:44 -0700

    [Mac] Add fuzzer for applying the serialized sandbox policy
    Add a new fuzzer to specifically target the `ApplySerializedPolicy`
    code path.
    
    This is intended to address and better pinpont a crash that's
    caused by the destruction of a `std::string` object after a call to
    `ApplySerializedPolicy`.
    
    The existing fuzzer tests the deserialization of a policy,
    while the newly added fuzzer tests the application of a serialized
    policy.
    
    Bug: 40066531
    Change-Id: I18b95cc95c0f2ef368229cee29d98a0d12542b5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6823772
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Christine Hollingsworth <christinesm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1498479}

--- sandbox/mac/sandbox_serializer_fuzztest.cc
+++ sandbox/mac/sandbox_serializer_fuzztest.cc
@@ -8,6 +8,32 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace sandbox {
 
+// Tests for the specific crash in the `ApplySerializedPolicy` path.
+// Fuzz for an input that, when processed, causes the crash
+// in `std::string`'s destructor (crbug.com/40066531).
+static void CanApplySerializedPolicyWithoutCrashing(std::string input) {
+  std::string error;
+  {
+    SandboxSerializer serializer(SandboxSerializer::Target::kSource);
+    serializer.SetProfile(input);
+    std::string serialized;
+    if (serializer.SerializePolicy(serialized, error)) {
+      std::ignore = serializer.ApplySerializedPolicy(serialized);
+    }
+  }
+
+  {
+    SandboxSerializer serializer(SandboxSerializer::Target::kCompiled);
+    serializer.SetProfile(input);
+    std::string serialized;
+    if (serializer.SerializePolicy(serialized, error)) {
+      std::ignore = serializer.ApplySerializedPolicy(serialized);
+    }
+  }
+}
+
+FUZZ_TEST(SandboxSerializerFuzzTest, CanApplySerializedPolicyWithoutCrashing);
+
 static void CanDeserializeWithoutCrashing(std::string input) {
   std::string error;
   {
