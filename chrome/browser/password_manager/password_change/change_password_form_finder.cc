b9a4be8a4008cdf61a91fdaeeed00d54ca5c3d66
Anna Tsvirchkova <atsvirchkova@google.com>
2025-08-07 03:52:38 -0700

    [APC] Remove the timeout after clicking to open password change form
    Remove the timeout when waiting for the form to appear after a click in
    settings. This will prevent the flow from failing on some websites, e.
    g. paypal.com.
    It also moves the creation of the form_waiter to happen earlier. It may
    happen that the form would be detected earlier than the button click
    result would arrive.
    
    Bug: 432436431
    Change-Id: Ibb7a7f9aaf0b568a4d5751362bd1ce396d1c307f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6823512
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Reviewed-by: Anna Tsvirchkova <atsvirchkova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1498113}

--- chrome/browser/password_manager/password_change/change_password_form_finder.cc
+++ chrome/browser/password_manager/password_change/change_password_form_finder.cc
@@ -199,6 +199,11 @@ void ChangePasswordFormFinder::OnExecutionResponseCallback(
       web_contents_, dom_node_id,
       base::BindOnce(&ChangePasswordFormFinder::OnButtonClicked,
                      weak_ptr_factory_.GetWeakPtr()));
+  form_waiter_ = std::make_unique<ChangePasswordFormWaiter>(
+      web_contents_, client_,
+      base::BindOnce(&ChangePasswordFormFinder::OnSubsequentFormWaitingResult,
+                     weak_ptr_factory_.GetWeakPtr()),
+      base::TimeDelta::Max());
 }
 
 void ChangePasswordFormFinder::OnButtonClicked(bool result) {
@@ -217,11 +222,6 @@ void ChangePasswordFormFinder::OnButtonClicked(bool result) {
     std::move(callback_).Run(nullptr);
     return;
   }
-
-  form_waiter_ = std::make_unique<ChangePasswordFormWaiter>(
-      web_contents_, client_,
-      base::BindOnce(&ChangePasswordFormFinder::OnSubsequentFormWaitingResult,
-                     weak_ptr_factory_.GetWeakPtr()));
 }
 
 void ChangePasswordFormFinder::OnSubsequentFormWaitingResult(
--- chrome/browser/password_manager/password_change/change_password_form_finder_unittest.cc
+++ chrome/browser/password_manager/password_change/change_password_form_finder_unittest.cc
@@ -292,7 +292,6 @@ TEST_F(ChangePasswordFormFinderTest, ButtonClickRequestedButFailed) {
   // Since ExecuteModel() call was successful, `form_finder` is now attempting
   // to click an underlying button.
   EXPECT_TRUE(form_finder->click_helper());
-  EXPECT_FALSE(form_finder->form_waiter());
 
   EXPECT_CALL(completion_callback, Run(nullptr));
   form_finder->click_helper()->SimulateClickResult(/*result=*/false);
@@ -357,7 +356,6 @@ TEST_F(ChangePasswordFormFinderTest, ButtonClickRequestedAndSucceeded) {
   // Since ExecuteModel() call was successful, `form_finder` is now attempting
   // to click an underlying button.
   EXPECT_TRUE(form_finder->click_helper());
-  EXPECT_FALSE(form_finder->form_waiter());
 
   form_finder->click_helper()->SimulateClickResult(/*result=*/true);
   EXPECT_FALSE(form_finder->click_helper());
