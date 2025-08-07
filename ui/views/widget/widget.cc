e8fd75c0e804d55f671af4efb14d3cc5b5b22d41
Thomas Lukaszewicz <tluk@chromium.org>
2025-08-06 23:30:30 -0700

    [bedrock] Add Widget::ForEachOwnedWidget()
    Currently Widget::GetAllOwnedWidgets() returns a set of Widget
    pointers. This is dangerous for clients that iterate over this
    collection and perform operations that could result in the
    destruction of any of the widgets in the collection.
    
    This CL introduces a new ForEachOwnedWidget() API and
    WidgetEnumerator class that allows safe iteration over owned
    Widgets.
    
    Bug: 417766643
    Change-Id: I7ce4ad34a96f7f98dd48bda3afc37c1793b2a7f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6825272
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1498035}

--- ui/views/widget/widget.cc
+++ ui/views/widget/widget.cc
@@ -60,6 +60,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/widget/tooltip_manager.h"
 #include "ui/views/widget/widget_delegate.h"
 #include "ui/views/widget/widget_deletion_observer.h"
+#include "ui/views/widget/widget_enumerator.h"
 #include "ui/views/widget/widget_observer.h"
 #include "ui/views/widget/widget_removals_observer.h"
 #include "ui/views/window/dialog_delegate.h"
@@ -390,6 +391,16 @@ Widget::Widgets Widget::GetAllOwnedWidgets(gfx::NativeView native_view) {
   return native_view
              ? internal::NativeWidgetPrivate::GetAllOwnedWidgets(native_view)
              : Widget::Widgets();
+}
+
+// static
+void Widget::ForEachOwnedWidget(gfx::NativeView native_view,
+                                base::FunctionRef<void(Widget*)> on_widget) {
+  WidgetEnumerator widget_iterator(
+      internal::NativeWidgetPrivate::GetAllOwnedWidgets(native_view));
+  while (!widget_iterator.IsEmpty()) {
+    on_widget(widget_iterator.Next());
+  }
 }
 
 // static
