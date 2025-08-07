734b07cef3ca629ddce7cc30213b217752703a4f
Calder Kitagawa <ckitagawa@chromium.org>
2025-08-07 06:35:42 -0700

    Add JniType annotations to TabModelObserverJniBridge
    Test to see if gemini can add JniType annotations. This was originally
    done on a71e715abb40ea264ba4190eed3ace89d8621aae before a JniType
    annotation ever existed in this file. I've rebased it to be able to
    merge and cleaned up some raw_ptr type related issues.
    
    Bug: N/A
    Change-Id: Id08dcd149bb01c07d41ba09617e743abf08d7b47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6824769
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1498176}

--- chrome/browser/ui/android/tab_model/tab_model_observer.cc
+++ chrome/browser/ui/android/tab_model/tab_model_observer.cc
@@ -22,7 +22,7 @@ void TabModelObserver::OnFinishingTabClosure(
     TabModel::TabClosingSource source) {}
 
 void TabModelObserver::OnFinishingMultipleTabClosure(
-    const std::vector<raw_ptr<TabAndroid, VectorExperimental>>& tabs,
+    const std::vector<TabAndroid*>& tabs,
     bool canRestore) {}
 
 void TabModelObserver::WillAddTab(TabAndroid* tab,
@@ -40,8 +40,7 @@ void TabModelObserver::OnTabClosePending(const std::vector<TabAndroid*>& tabs,
 
 void TabModelObserver::TabClosureUndone(TabAndroid* tab) {}
 
-void TabModelObserver::OnTabCloseUndone(
-    const std::vector<raw_ptr<TabAndroid, VectorExperimental>>& tabs) {}
+void TabModelObserver::OnTabCloseUndone(const std::vector<TabAndroid*>& tabs) {}
 
 void TabModelObserver::TabClosureCommitted(TabAndroid* tab) {}
 
