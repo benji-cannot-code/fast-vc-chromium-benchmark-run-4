5cb5f3225674c89b6ee6e34f5604de6eea18f283
Calder Kitagawa <ckitagawa@chromium.org>
2025-08-07 12:00:24 -0700

    [Tab Group] Refactor tab group visual data storage
    Precursor work to migrating to TabGroupId based storage.
    
    Bug: 436240845
    Change-Id: I481981969ce0a1011d75cc10823e01a789338de7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6824675
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1498391}

--- chrome/browser/tabmodel/android/java/src/org/chromium/chrome/browser/tabmodel/TabGroupTitleUtilsUnitTest.java
+++ chrome/browser/tabmodel/android/java/src/org/chromium/chrome/browser/tabmodel/TabGroupTitleUtilsUnitTest.java
@@ -78,7 +78,7 @@ public class TabGroupTitleUtilsUnitTest {
 
     @Test
     public void testDeleteTabGroupTitle() {
-        TabGroupTitleUtils.deleteTabGroupTitle(TAB_ID);
+        TabGroupVisualDataStore.deleteTabGroupTitle(TAB_ID);
 
         verify(mEditor).remove(eq(String.valueOf(TAB_ID)));
         verify(mRemoveEditor).apply();
@@ -89,12 +89,12 @@ public class TabGroupTitleUtilsUnitTest {
         // Mock that we have a stored tab group title with reference to TAB_ID.
         when(mSharedPreferences.getString(String.valueOf(TAB_ID), null)).thenReturn(TAB_TITLE);
 
-        assertThat(TabGroupTitleUtils.getTabGroupTitle(TAB_ID), equalTo(TAB_TITLE));
+        assertThat(TabGroupVisualDataStore.getTabGroupTitle(TAB_ID), equalTo(TAB_TITLE));
     }
 
     @Test
     public void testStoreTabGroupTitle() {
-        TabGroupTitleUtils.storeTabGroupTitle(TAB_ID, TAB_TITLE);
+        TabGroupVisualDataStore.storeTabGroupTitle(TAB_ID, TAB_TITLE);
 
         verify(mEditor).putString(eq(String.valueOf(TAB_ID)), eq(TAB_TITLE));
         verify(mPutStringEditor).apply();
@@ -102,7 +102,7 @@ public class TabGroupTitleUtilsUnitTest {
 
     @Test
     public void testStoreTabGroupTitle_Empty() {
-        TabGroupTitleUtils.storeTabGroupTitle(TAB_ID, "");
+        TabGroupVisualDataStore.storeTabGroupTitle(TAB_ID, "");
 
         verify(mEditor).remove(eq(String.valueOf(TAB_ID)));
         verify(mRemoveEditor).apply();
@@ -110,7 +110,7 @@ public class TabGroupTitleUtilsUnitTest {
 
     @Test
     public void testStoreTabGroupTitle_Null() {
-        TabGroupTitleUtils.storeTabGroupTitle(TAB_ID, null);
+        TabGroupVisualDataStore.storeTabGroupTitle(TAB_ID, null);
 
         verify(mEditor).remove(eq(String.valueOf(TAB_ID)));
         verify(mRemoveEditor).apply();
