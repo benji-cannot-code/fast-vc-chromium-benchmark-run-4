2cceb11ff129220c7e558a02994cad93e757e035
Daniil Hrybchuk <renkens@google.com>
2025-08-07 05:33:17 -0700

    window management api: histogram for the allowed and blocked usages
    Add a histogram that counts the number of allowed and blocked usages of
    the Window Management API on Android.
    
    Test: out/{build_directory}/bin/run_components_unittests --gtest_filter='*PermissionManagerTest*'
    Bug: 435637755
    Change-Id: I9a16384fd9fed19c7ebc757593c0ea4378af12fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6813490
    Reviewed-by: Judith Hemp <hempjudith@google.com>
    Commit-Queue: Daniil Hrybchuk <renkens@google.com>
    Reviewed-by: Florian Jacky <fjacky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1498150}

--- components/permissions/permission_manager_unittest.cc
+++ components/permissions/permission_manager_unittest.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_ANDROID)
 #include "base/feature_list.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "ui/android/ui_android_features.h"
 #endif  // IS_ANDROID
@@ -48,6 +49,11 @@ using network::mojom::PermissionsPolicyFeature;
 
 namespace permissions {
 namespace {
+
+#if BUILDFLAG(IS_ANDROID)
+constexpr char kWindowManagementHistogramName[] =
+    "Permissions.WindowManagementApi.Android.Allowed";
+#endif  // IS_ANDROID
 
 class ScopedPartitionedOriginBrowserClient
     : public content::ContentBrowserClient {
@@ -381,11 +387,29 @@ TEST_F(PermissionManagerTest, AndroidWindowManagementPermission) {
   scoped_feature_list.InitWithFeatureState(ui::kAndroidWindowManagementWebApi,
                                            true);
 
-  CheckPermissionStatus(PermissionType::WINDOW_MANAGEMENT,
-                        PermissionStatus::ASK);
-  SetPermission(PermissionType::WINDOW_MANAGEMENT, PermissionStatus::GRANTED);
-  CheckPermissionStatus(PermissionType::WINDOW_MANAGEMENT,
-                        PermissionStatus::GRANTED);
+  {
+    base::HistogramTester histogram_tester;
+
+    CheckPermissionStatus(PermissionType::WINDOW_MANAGEMENT,
+                          PermissionStatus::ASK);
+    SetPermission(PermissionType::WINDOW_MANAGEMENT, PermissionStatus::GRANTED);
+    CheckPermissionStatus(PermissionType::WINDOW_MANAGEMENT,
+                          PermissionStatus::GRANTED);
+
+    histogram_tester.ExpectUniqueSample(kWindowManagementHistogramName, true,
+                                        1);
+  }
+
+  {
+    base::HistogramTester histogram_tester;
+
+    SetPermission(PermissionType::WINDOW_MANAGEMENT, PermissionStatus::DENIED);
+    CheckPermissionStatus(PermissionType::WINDOW_MANAGEMENT,
+                          PermissionStatus::DENIED);
+
+    histogram_tester.ExpectUniqueSample(kWindowManagementHistogramName, false,
+                                        1);
+  }
 }
 #endif
 
