703caddb6e72097e3580a37810956ca90809aed7
Nohemi Fernandez <fernandex@google.com>
2025-08-07 07:12:14 -0700

    Remove setTimeout/clearTimeout overrides in readability_triggering.js
    Follow-up to issue fixed by crrev.com/c/6797985, the timeout
    manipulation had been copied over from previous scripts and causes
    other Javascript features (Translate, Autofill) to never return.
    
    Bug: 437077200
    Change-Id: I137498855d8144577c8bfe055b011e0377979d8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6827126
    Reviewed-by: Quentin Pubert <qpubert@google.com>
    Commit-Queue: Nohemi Fernandez <fernandex@google.com>
    Cr-Commit-Position: refs/heads/main@{#1498202}

--- components/dom_distiller/core/javascript/readability_triggering.js
+++ components/dom_distiller/core/javascript/readability_triggering.js
@@ -12,8 +12,6 @@ function initialize() {
   // clang-format on
   window.isProbablyReaderable = isProbablyReaderable;
 }
-window.setTimeout = function() {};
-window.clearTimeout = function() {};
 initialize();
 
 return isProbablyReaderable(document);
