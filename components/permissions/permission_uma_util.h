15a0b197c33cb618f941b2bbfba0748678d40028
Elias Klim <elklm@chromium.org>
2025-08-07 04:46:02 -0700

    [PermissionsAI] Prevent concurrent requests
    PermissionsAI contains multiple async blocks but on the top level it
    does not support concurrent permission requests. It is unclear if it is
    even possible to send multiple request for the verification, hence this
    CL adds a new histogram to track it.
    
    NO_IFTTT=Added kMaxValue.
    
    Bug: 436802385
    Change-Id: I4f095e4ce22443f24a29ee3c8568a3055a8e9278
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6820529
    Commit-Queue: Elias Klim <elklm@chromium.org>
    Reviewed-by: Judith Hemp <hempjudith@google.com>
    Cr-Commit-Position: refs/heads/main@{#1498128}

--- components/permissions/permission_uma_util.h
+++ components/permissions/permission_uma_util.h
@@ -747,6 +747,9 @@ class PermissionUmaUtil {
   static void RecordDSEEffectiveSetting(ContentSettingsType permission_type,
                                         ContentSetting setting);
 
+  static void RecordPermissionPredictionConcurrentRequests(
+      RequestType request_type);
+
   static void RecordPermissionPredictionSource(
       PermissionPredictionSource prediction_source,
       RequestType request_type);
