991f5352f46e15fc60378e7426bc335a19718745
Devlin Cronin <rdevlin.cronin@chromium.org>
2025-08-07 07:55:04 -0700

    [Browser UI] Add a cross-platform method to create a new browser window
    Add a cross-browser method to create a new browser window, providing an
    alternative to Browser::Create() (which is unavailable on android
    platforms).
    
    For now, this is only implemented on classic desktop platforms, but
    support will be added for Android in the future.
    
    As part of this, we need to move BrowserWindowInterface::Type to be
    part of the public interface on Android, as well.
    
    Bug: 434215969
    Change-Id: If975ceca7fd65225acb4f86aa3284fb6adad2a53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6795465
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1498228}

---
+++ chrome/browser/ui/browser_window/internal/create_browser_window.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/browser_window/public/create_browser_window.h"
+
+BrowserWindowCreateParams::BrowserWindowCreateParams(
+    BrowserWindowInterface::Type type,
+    Profile& profile,
+    bool from_user_gesture)
+    : type(type), from_user_gesture(from_user_gesture), profile(profile) {}
+
+BrowserWindowCreateParams::BrowserWindowCreateParams(Profile& profile,
+                                                     bool from_user_gesture)
+    : BrowserWindowCreateParams(BrowserWindowInterface::TYPE_NORMAL,
+                                profile,
+                                from_user_gesture) {}
+
+BrowserWindowCreateParams::BrowserWindowCreateParams(
+    BrowserWindowCreateParams&&) = default;
+
+BrowserWindowCreateParams& BrowserWindowCreateParams::operator=(
+    BrowserWindowCreateParams&&) = default;
+
+BrowserWindowCreateParams::~BrowserWindowCreateParams() = default;
