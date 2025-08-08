b8426d027830ca8f20d0edd522c476513c58a10a
dpapad <dpapad@chromium.org>
2025-08-08 13:08:20 -0700

    Reland "Settings: Fix blank landing page on CrOS guest mode."
    Original CL was reverted due to a merge conflict with
    https://crrev.com/c/6825102, which is addressed here.
    
    Fixed: 437052925
    Change-Id: Id2ccff5c6a8a3fed083c57d278428b06a525683c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6832023
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1498989}

--- chrome/test/data/webui/settings/privacy_page_index_test.ts
+++ chrome/test/data/webui/settings/privacy_page_index_test.ts
@@ -19,6 +19,7 @@ suite('PrivacyPageIndex', function() {
 
     loadTimeData.overrideValues(Object.assign(
         {
+          isGuest: false,
           enableSecurityKeysSubpage: false,
           isPrivacySandboxRestricted: false,
           isPrivacySandboxRestrictedNoticeEnabled: false,
@@ -148,6 +149,15 @@ suite('PrivacyPageIndex', function() {
     assertTrue(!!index.$.viewManager.querySelector(
         `#securityKeys[slot=view][data-parent-view-id=old]`));
   });
+
+  // <if expr="is_chromeos">
+  test('RoutingGuestMode', async function() {
+    assertFalse(loadTimeData.getBoolean('isGuest'));
+    assertEquals(routes.BASIC, Router.getInstance().getCurrentRoute());
+    await createPrivacyPageIndex({isGuest: true});
+    assertTrue(!!index.$.viewManager.querySelector('#old.active[slot=view]'));
+  });
+  // </if>
 
   // Minimal (non-exhaustive) tests to ensure SearchableViewContainerMixin is
   // inherited correctly.
