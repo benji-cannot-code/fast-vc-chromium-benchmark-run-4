b4f9559492dd0ad78a236582450c94f46fcdc4e4
Anders Hartvoll Ruud <andruud@chromium.org>
2025-08-08 01:28:46 -0700

    Fix float-cast-overflow UBSAN issues in media_query_evaluator.cc
    Fixed: 435228815
    Change-Id: I354d031cb9530e84d12c3f2074743bd5cdeeb713
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6828783
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1498650}

--- third_party/blink/renderer/core/css/media_query_evaluator.cc
+++ third_party/blink/renderer/core/css/media_query_evaluator.cc
@@ -328,7 +328,7 @@ static bool ColorMediaFeatureEval(const MediaQueryExpValue& value,
                                bits_per_component);
   if (value.IsValid()) {
     return NumberValue(value, number, media_values) &&
-           CompareValue(bits_per_component, static_cast<int>(number), op);
+           CompareValue(bits_per_component, ClampTo<int>(number), op);
   }
 
   return bits_per_component != 0;
@@ -348,7 +348,7 @@ static bool ColorIndexMediaFeatureEval(const MediaQueryExpValue& value,
   // value is zero.
   float number;
   return NumberValue(value, number, media_values) &&
-         CompareValue(0, static_cast<int>(number), op);
+         CompareValue(0, ClampTo<int>(number), op);
 }
 
 static bool MonochromeMediaFeatureEval(const MediaQueryExpValue& value,
@@ -361,7 +361,7 @@ static bool MonochromeMediaFeatureEval(const MediaQueryExpValue& value,
       bits_per_component);
   if (value.IsValid()) {
     return NumberValue(value, number, media_values) &&
-           CompareValue(bits_per_component, static_cast<int>(number), op);
+           CompareValue(bits_per_component, ClampTo<int>(number), op);
   }
   return bits_per_component != 0;
 }
@@ -641,7 +641,7 @@ static bool GridMediaFeatureEval(const MediaQueryExpValue& value,
   // assume we have bitmap device
   float number;
   if (value.IsValid() && NumberValue(value, number, media_values)) {
-    return CompareValue(static_cast<int>(number), 0, op);
+    return CompareValue(ClampTo<int>(number), 0, op);
   }
   return false;
 }
@@ -950,7 +950,7 @@ static bool Transform3dMediaFeatureEval(const MediaQueryExpValue& value,
   if (value.IsValid()) {
     float number;
     return NumberValue(value, number, media_values) &&
-           CompareValue(have3d_rendering, static_cast<int>(number), op);
+           CompareValue(have3d_rendering, ClampTo<int>(number), op);
   }
   return return_value_if_no_parameter;
 }
@@ -1337,8 +1337,7 @@ static bool HorizontalViewportSegmentsMediaFeatureEval(
 
   float number;
   return NumberValue(value, number, media_values) &&
-         CompareValue(horizontal_viewport_segments, static_cast<int>(number),
-                      op);
+         CompareValue(horizontal_viewport_segments, ClampTo<int>(number), op);
 }
 
 static bool VerticalViewportSegmentsMediaFeatureEval(
@@ -1361,7 +1360,7 @@ static bool VerticalViewportSegmentsMediaFeatureEval(
 
   float number;
   return NumberValue(value, number, media_values) &&
-         CompareValue(vertical_viewport_segments, static_cast<int>(number), op);
+         CompareValue(vertical_viewport_segments, ClampTo<int>(number), op);
 }
 
 static bool OverflowInlineMediaFeatureEval(const MediaQueryExpValue& value,
