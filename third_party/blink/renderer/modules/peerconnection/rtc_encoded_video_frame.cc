7da5773431337682d9b7059f9c6f8f19057fa937
Philipp Hancke <philipp.hancke@googlemail.com>
2025-08-08 08:03:02 -0700

    webrtc: update RTCEncodedVideoFrameMetadata and make types match spec
    for spatialIndex and temporalIndex. Also pick up the spec change to have audio and video derived from a generic frame object which mostly consists of the RTP properties.
    
    Also remove RTCVideoCodecType and RTCDecodeTargetIndication which did not ship in that form.
    
    Bug: webrtc:411452026
    
    Change-Id: Ib43782eecce94644ad7ca88da2cc383b0e3e96e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6470848
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Philipp Hancke <philipp.hancke@googlemail.com>
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1498798}

--- third_party/blink/renderer/modules/peerconnection/rtc_encoded_video_frame.cc
+++ third_party/blink/renderer/modules/peerconnection/rtc_encoded_video_frame.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/unguessable_token.h"
 #include "third_party/blink/public/common/features.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_rtc_codec_specifics_vp_8.h"
-#include "third_party/blink/renderer/bindings/modules/v8/v8_rtc_decode_target_indication.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_rtc_encoded_video_frame_metadata.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_rtc_encoded_video_frame_options.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
