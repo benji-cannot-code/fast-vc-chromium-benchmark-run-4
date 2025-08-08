99136261649f83e2c14287e408479aee1135e0f7
Brandon Wylie <wylieb@chromium.org>
2025-08-08 12:09:49 -0700

    [reader mode] Expand the prefs bottomsheet even if it's already peeked
    Bug: 434943491
    Change-Id: I976400cc41f1cbbb340abd43dcb45b4cbf36a8ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6825803
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Reviewed-by: Brandon Fong <bjfong@google.com>
    Auto-Submit: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1498956}

--- chrome/android/java/src/org/chromium/chrome/browser/dom_distiller/ReaderModeBottomSheetCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/dom_distiller/ReaderModeBottomSheetCoordinator.java
@@ -10,7 +10,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 
 import androidx.annotation.StringRes;
-import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.lifetime.DestroyChecker;
 import org.chromium.build.annotations.NullMarked;
@@ -76,9 +75,18 @@ public class ReaderModeBottomSheetCoordinator {
      */
     public void show(boolean showFullSheet) {
         mDestroyChecker.checkNotDestroyed();
-        boolean success =
-                mBottomSheetController.requestShowContent(mBottomSheetContent, /* animate= */ true);
-        if (success && showFullSheet) {
+        // Only try to show the bottom sheet if it's not already showing. BottomSheetController
+        // makes a copy of the sheet content, so equals comparison isn't useful here.
+        boolean showing =
+                mBottomSheetController.getCurrentSheetContent()
+                        instanceof ReaderModeBottomSheetContent;
+        if (!showing) {
+            showing =
+                    mBottomSheetController.requestShowContent(
+                            mBottomSheetContent, /* animate= */ true);
+        }
+
+        if (showing && showFullSheet) {
             mBottomSheetController.expandSheet();
         }
     }
@@ -170,8 +178,11 @@ public class ReaderModeBottomSheetCoordinator {
 
     // For testing methods.
 
-    @VisibleForTesting
-    View getView() {
+    View getViewForTesting() {
         return mReaderModeBottomSheetView;
+    }
+
+    BottomSheetContent getBottomSheetContentForTesting() {
+        return mBottomSheetContent;
     }
 }
