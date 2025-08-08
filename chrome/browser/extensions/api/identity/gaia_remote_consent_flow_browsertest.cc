b69e48d599086270759c4e1c7c784f815cfddf3c
Achuith Bhandarkar <achuith@chromium.org>
2025-08-08 16:15:12 -0700

    extensions: replace browser()->profile with GetProfile()
    Replace in GaiaRemoteConsentFlowParamBrowserTest
    
    BUG=436871366
    
    Change-Id: I1814d0a2fc9f475b6616e207468f6b8420e1d3e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6832864
    Commit-Queue: James Cook <jamescook@chromium.org>
    Auto-Submit: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1499091}

--- chrome/browser/extensions/api/identity/gaia_remote_consent_flow_browsertest.cc
+++ chrome/browser/extensions/api/identity/gaia_remote_consent_flow_browsertest.cc
@@ -131,7 +131,7 @@ class GaiaRemoteConsentFlowParamBrowserTest : public InProcessBrowserTest {
 
   CoreAccountInfo CreateFakeAccountInfoAndSetAsPrimary() {
     signin::IdentityManager* identity_manager =
-        IdentityManagerFactory::GetForProfile(profile());
+        IdentityManagerFactory::GetForProfile(GetProfile());
     CoreAccountInfo account_info = SetPrimaryAccount(
         identity_manager, kTestEmail, signin::ConsentLevel::kSync);
     SetRefreshTokenForPrimaryAccount(identity_manager, kFakeRefreshToken);
@@ -153,7 +153,7 @@ class GaiaRemoteConsentFlowParamBrowserTest : public InProcessBrowserTest {
     resolution_data.url = url;
     resolution_data.cookies = resolution_cookies;
 
-    flow_ = std::make_unique<GaiaRemoteConsentFlow>(&mock(), profile(),
+    flow_ = std::make_unique<GaiaRemoteConsentFlow>(&mock(), GetProfile(),
                                                     token_key, resolution_data,
                                                     /*user_gesture=*/true);
   }
@@ -190,8 +190,6 @@ class GaiaRemoteConsentFlowParamBrowserTest : public InProcessBrowserTest {
   net::EmbeddedTestServer* fake_gaia_test_server() {
     return &fake_gaia_test_server_;
   }
-
-  Profile* profile() { return browser()->profile(); }
 
   GaiaRemoteConsentFlow* flow() { return flow_.get(); }
 
