9177601539b53deb51b7247d3796ad4282a26318
Daniel Lehmann <dlehmann@chromium.org>
2025-08-08 05:26:29 -0700

    [cleanup][v8] Remove unused/launched feature flag
    ...for Intel JCC erratum mitigation, which is enabled by default in V8
    since almost a year, see https://crrev.com/c/5898673.
    
    Bug: 42204161
    Change-Id: I9d5ffb3e455a9873f9173e941f3338ac57896b57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6828985
    Reviewed-by: Andreas Haas <ahaas@chromium.org>
    Commit-Queue: Daniel Lehmann <dlehmann@chromium.org>
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1498714}

--- gin/v8_initializer.cc
+++ gin/v8_initializer.cc
@@ -491,8 +491,6 @@ void SetFeatureFlags() {
   bool any_slow_histograms_alias =
       base::FeatureList::IsEnabled(
           features::kV8SlowHistogramsCodeMemoryWriteProtection) ||
-      base::FeatureList::IsEnabled(
-          features::kV8SlowHistogramsIntelJCCErratumMitigation) ||
       base::FeatureList::IsEnabled(features::kV8SlowHistogramsSparkplug) ||
       base::FeatureList::IsEnabled(
           features::kV8SlowHistogramsSparkplugAndroid) ||
@@ -507,10 +505,6 @@ void SetFeatureFlags() {
   SetV8FlagsIfOverridden(features::kV8IgnitionElideRedundantTdzChecks,
                          "--ignition-elide-redundant-tdz-checks",
                          "--no-ignition-elide-redundant-tdz-checks");
-
-  SetV8FlagsIfOverridden(features::kV8IntelJCCErratumMitigation,
-                         "--intel-jcc-erratum-mitigation",
-                         "--no-intel-jcc-erratum-mitigation");
 
   SetV8FlagsIfOverridden(features::kV8UseLibmTrigFunctions,
                          "--use-libm-trig-functions",
