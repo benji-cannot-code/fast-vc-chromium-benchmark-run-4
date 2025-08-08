728fa16d2069b531b215afbfcf8292ad0d6cd29e
Maya Jelonkiewicz <mjel@google.com>
2025-08-08 01:04:29 -0700

    Use sample rates reported by Android for streams with specified devices
    Test: run_media_unittests --gtest_filter="AudioAndroid*"
    Bug: 417397476
    Change-Id: Iba871406fb2f73629127453b9a8080f4a12d0472
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6648924
    Commit-Queue: Maya Jelonkiewicz <mjel@google.com>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Eliot Courtney <edcourtney@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1498639}

--- media/base/android/java/src/org/chromium/media/AudioManagerAndroid.java
+++ media/base/android/java/src/org/chromium/media/AudioManagerAndroid.java
@@ -78,10 +78,14 @@ class AudioManagerAndroid {
         private final @Nullable String mName;
         private final int mType;
 
-        public AudioDevice(int id, @Nullable String name, int type) {
+        // Empty if arbitrary sample rates are supported.
+        private final int[] mSampleRates;
+
+        public AudioDevice(int id, @Nullable String name, int type, int[] sampleRates) {
             mId = id;
             mName = name;
             mType = type;
+            mSampleRates = sampleRates;
         }
 
         @CalledByNative("AudioDevice")
@@ -97,6 +101,11 @@ class AudioManagerAndroid {
         @CalledByNative("AudioDevice")
         private int type() {
             return mType;
+        }
+
+        @CalledByNative("AudioDevice")
+        private @JniType("std::vector<int>") int[] sampleRates() {
+            return mSampleRates;
         }
     }
 
@@ -345,7 +354,8 @@ class AudioManagerAndroid {
                 // `android.os.Build.MODEL` to facilitate providing a custom fallback name instead.
                 name = null;
             }
-            devices.add(new AudioDevice(id, name, type));
+            int[] sampleRates = deviceInfo.getSampleRates();
+            devices.add(new AudioDevice(id, name, type, sampleRates));
         }
         return devices.toArray(new AudioDevice[0]);
     }
