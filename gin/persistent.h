6a7cd0ce81555a7fbf3f9469935fd1e68be33d16
Andreas Haas <ahaas@chromium.org>
2025-08-08 05:09:00 -0700

    [gin] Move parts of blink::Persistent and blink::WeakCell to gin
    By making gin::Wrappable an oilpan object, oilpan infrastructure is
    needed for gin::Wrappable users. This CL moves some infrastructure to
    gin to make it usable for gin::Wrappable users.
    
    Bug: 345640553
    Change-Id: Iaad5df2dc893e78652d019df4613f86a473e2d6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6811186
    Commit-Queue: Andreas Haas <ahaas@chromium.org>
    Reviewed-by: Omer Katz <omerkatz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1498685}

---
+++ gin/persistent.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef GIN_PERSISTENT_H_
+#define GIN_PERSISTENT_H_
+
+#include "v8/include/cppgc/persistent.h"
+#include "v8/include/cppgc/source-location.h"
+
+namespace gin {
+
+// Wraps a raw pointer into a cppgc::Persistent. This is a simplified version
+// of blink::WrapPersistent, which doesn't support location tracking.
+template <typename T>
+cppgc::Persistent<T> WrapPersistent(
+    T* value,
+    const cppgc::SourceLocation& loc = cppgc::SourceLocation()) {
+  return cppgc::Persistent<T>(value, loc);
+}
+
+}  // namespace gin
+
+#endif  // GIN_PERSISTENT_H_
--- third_party/blink/renderer/platform/wtf/hash_functions.h
+++ third_party/blink/renderer/platform/wtf/hash_functions.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_WTF_HASH_FUNCTIONS_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_WTF_HASH_FUNCTIONS_H_
 
+#include <cstddef>
 #include <stdint.h>
 
 #include <concepts>
