cb014f9f6f4cd2e03f5a50219a15fa6c952ddf2b
Vasiliy Telezhnikov <vasilyt@chromium.org>
2025-08-08 11:46:08 -0700

    Remove enable_grcontext from ContextCreationAttribs
    It's always false.
    
    Bug: 391648152
    Change-Id: I94c42adfb1a22c8c519b66bd0310e395763caa67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6829185
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1498943}

--- content/browser/renderer_host/compositor_impl_android.cc
+++ content/browser/renderer_host/compositor_impl_android.cc
@@ -108,7 +108,6 @@ gpu::ContextCreationAttribs GetCompositorContextAttributes() {
   gpu::ContextCreationAttribs attributes;
   attributes.enable_raster_interface = true;
   attributes.enable_gles2_interface = false;
-  attributes.enable_grcontext = false;
 
   return attributes;
 }
