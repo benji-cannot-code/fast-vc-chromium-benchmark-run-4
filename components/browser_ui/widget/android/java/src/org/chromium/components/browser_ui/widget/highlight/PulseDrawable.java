0bc52469651bbdfd255c78240cd83020087daf36
Henrique Nakashima <hnakashima@chromium.org>
2025-08-08 09:03:21 -0700

    [Android] Migrate ToolbarButtonIphTest to Public Transit
    Also fix flakiness in testTabSwitcherButtonIph() by reducing frame rate
    of PulseDrawable animation updates (applies to all
    ChromeActivityTestRule tests).
    
    Could also disable updates altogether but then we wouldn't exercise the
    custom animation code.
    
    These posted updates keep the UI thread busy and any Espresso
    ViewInteractions become flaky.
    
    Bug: 399877997,342240475,40728023
    Change-Id: I3681b354dc53db2d648d2c5ea2ec8d30712c5adf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6824603
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1498836}

--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/highlight/PulseDrawable.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/highlight/PulseDrawable.java
@@ -22,6 +22,7 @@ import androidx.annotation.Px;
 import androidx.core.view.animation.PathInterpolatorCompat;
 
 import org.chromium.base.MathUtils;
+import org.chromium.base.ResettersForTesting;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.components.browser_ui.styles.SemanticColorUtils;
@@ -37,6 +38,7 @@ import org.chromium.ui.interpolators.Interpolators;
 public class PulseDrawable extends Drawable implements Animatable {
     private static final long PULSE_DURATION_MS = 2500;
     private static final long FRAME_RATE = 60;
+    private static @Nullable Long sFrameRateForTesting;
 
     /**
      * Informs the PulseDrawable about whether it can continue pulsing, and specifies a callback to
@@ -256,7 +258,7 @@ public class PulseDrawable extends Drawable implements Animatable {
                 public void run() {
                     stepPulse();
                     if (mRunning) {
-                        scheduleSelf(mNextFrame, SystemClock.uptimeMillis() + 1000 / FRAME_RATE);
+                        scheduleSelf(mNextFrame, calculateNextFrameTime());
                     }
                 }
             };
@@ -327,7 +329,7 @@ public class PulseDrawable extends Drawable implements Animatable {
     public void start() {
         if (mRunning) {
             unscheduleSelf(mNextFrame);
-            scheduleSelf(mNextFrame, SystemClock.uptimeMillis() + 1000 / FRAME_RATE);
+            scheduleSelf(mNextFrame, calculateNextFrameTime());
         } else {
             mRunning = true;
             if (mState.startTime == 0) {
@@ -486,5 +488,17 @@ public class PulseDrawable extends Drawable implements Animatable {
         public int getChangingConfigurations() {
             return 0;
         }
+    }
+
+    public static void setFrameRateForTesting(long frameRateForTesting) {
+        sFrameRateForTesting = frameRateForTesting;
+        ResettersForTesting.register(() -> sFrameRateForTesting = null);
+    }
+
+    private static long calculateNextFrameTime() {
+        if (sFrameRateForTesting != null) {
+            return SystemClock.uptimeMillis() + 1000 / sFrameRateForTesting;
+        }
+        return SystemClock.uptimeMillis() + 1000 / FRAME_RATE;
     }
 }
