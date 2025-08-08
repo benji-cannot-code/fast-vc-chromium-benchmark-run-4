9ceb50691547f0898ba93a9c9a1b47c27152f726
Kartar Singh <kartarsingh@google.com>
2025-08-08 08:00:59 -0700

    [InputVizard] Progress fling when fling starts on Viz.
    FlingController uses `NeedsBeginFrameForFlingProgress` for
    deciding whether to progress fling on start or not. Viz side fling
    scheduler does use begin frames for scheduling fling progress, but
    would also want to progress fling on start.
    
    In this change we are renaming NeedsBeginFrameForFlingProgress
    to ProgressFlingOnFlingStart to better tell what's it being used for,
    and updates the current implementations to return just the negated
    values from fling scheduler clients except on Viz where
    `ProgressFlingOnFlingStart` keeps returning true.
    
    Bug: 434175924
    Change-Id: I10188214ce094817d1ed2a0ff4db7ecff9f5216a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6829323
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Kartar Singh <kartarsingh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1498796}

--- content/browser/renderer_host/input/mock_input_router_client.cc
+++ content/browser/renderer_host/input/mock_input_router_client.cc
@@ -120,8 +120,8 @@ MockInputRouterClient::GetAndResetCompositorAllowedTouchAction() {
   return allowed;
 }
 
-bool MockInputRouterClient::NeedsBeginFrameForFlingProgress() {
-  return false;
+bool MockInputRouterClient::ProgressFlingOnFlingStart() {
+  return true;
 }
 
 bool MockInputRouterClient::ShouldUseMobileFlingCurve() {
