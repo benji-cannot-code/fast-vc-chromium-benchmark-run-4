bbc9693eeb1850bad652c3e82c927471a48fca1c
Saifuddin Hitawala <hitawala@chromium.org>
2025-08-08 13:22:53 -0700

    [gpu] Use SharedImageFormat in ImageDecodeAcceleratorWorker
    Update ImageDecodeAcceleratorWorker::DecodeResult to use
    SharedImageFormat instead of BufferFormat. Also update relevant
    callsites to use the same.
    
    Bug: 356649879
    Change-Id: I2fdeaae6a0d9af14f4c61bcfc3a9fba0ce92c904
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6828631
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1498997}

--- gpu/ipc/service/image_decode_accelerator_worker.h
+++ gpu/ipc/service/image_decode_accelerator_worker.h
@@ -13,9 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/functional/callback.h"
+#include "components/viz/common/resources/shared_image_format.h"
 #include "gpu/config/gpu_info.h"
 #include "third_party/skia/include/core/SkImageInfo.h"
-#include "ui/gfx/buffer_types.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/gpu_memory_buffer_handle.h"
 
@@ -31,7 +31,7 @@ class ImageDecodeAcceleratorWorker {
   struct DecodeResult {
     gfx::GpuMemoryBufferHandle handle;
     gfx::Size visible_size;
-    gfx::BufferFormat buffer_format;
+    viz::SharedImageFormat si_format;
     size_t buffer_byte_size;
     SkYUVColorSpace yuv_color_space;
   };
