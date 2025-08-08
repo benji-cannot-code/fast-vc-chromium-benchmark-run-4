a051f554947986e4ef023ed6642eda1d72697861
Trevor Perrier <perrier@google.com>
2025-08-08 11:39:50 -0700

    Enable DisableGeoLanguageModel by default
    Bug: 379097685
    Change-Id: I6053daf89fd70f3a304b09df41d8e52e3fd870e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6770349
    Reviewed-by: Megan Jablonski <megjablon@chromium.org>
    Auto-Submit: Trevor Perrier <perrier@chromium.org>
    Commit-Queue: Megan Jablonski <megjablon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1498940}

--- components/language/core/common/language_experiments.cc
+++ components/language/core/common/language_experiments.cc
@@ -25,6 +25,6 @@ BASE_FEATURE(kTranslateOpenSettings,
 
 BASE_FEATURE(kDisableGeoLanguageModel,
              "DisableGeoLanguageModel",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace language
