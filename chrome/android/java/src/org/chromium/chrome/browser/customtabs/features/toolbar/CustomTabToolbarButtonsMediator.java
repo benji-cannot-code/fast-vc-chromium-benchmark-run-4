febc4474ec15e3e2397ce445f4db13b56ac50445
Jinsuk Kim <jinsukkim@chromium.org>
2025-08-08 15:23:36 -0700

    [CCT][ToolbarRefactor] Fix optional button icon/bg state image position
    The button icon was 2dp off the center wrt the background image, and
    the pressed-state image was not positioned/scaled correctly. This CL
    fixes that.
    
    Note: pressed-state ListDrawable is set after the optional button is
    first updated. It had to be moved accordingly.
    
    Before: https://drive.google.com/file/d/1EN_gIVZHzs2IjZmfGJCTv3qBsAnuIpW8/view?usp=sharing
    After: https://drive.google.com/file/d/1QhDoINKuzyFxgs3Ac3ccF6nrdDllAoB9/view?usp=sharing
    
    Bug: 428261559
    Change-Id: I8722f6ee1dff01526e9f4e44cb9c7edf5f185037
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6828360
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1499067}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/toolbar/CustomTabToolbarButtonsMediator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/toolbar/CustomTabToolbarButtonsMediator.java
@@ -184,6 +184,7 @@ class CustomTabToolbarButtonsMediator
             mOptionalButtonCoordinator.updateButton(buttonData, mModel.get(IS_INCOGNITO));
             updateOptionalButtonColors(
                     mView.getBackground().getColor(), mView.getBrandedColorScheme());
+            mView.setOptionalButtonBackgroundInset();
             RecordHistogram.recordEnumeratedHistogram(
                     "CustomTabs.AdaptiveToolbarButton.Shown",
                     buttonVariant,
