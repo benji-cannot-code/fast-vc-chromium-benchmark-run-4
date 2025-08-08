06f4ca1a1f2f0fadce09c5229bbb1dfbd091633b
Henrique Nakashima <hnakashima@chromium.org>
2025-08-08 15:18:02 -0700

    [Android] Fix shouldShowImeWithHardwareKeyboard() in Android 16
    This should fix tests that are failing because they don't expect the
    soft keyboard to appear in Android 16 (but it does even when
    show_ime_with_hard_keyboard is 0). This causes flakes when UI elements
    move in the middle of an interaction, or get hidden under the soft
    keyboard.
    
    Cq-Include-Trybots: luci.chromium.try:android-16-x64-dbg,android-16-x64-rel
    Bug: 424223727
    Change-Id: I6dc070b3aaf6fa1f69f7441227d5db9f7d8b60f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6828627
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1499060}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/transit/hub/NewTabGroupDialogFacility.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/transit/hub/NewTabGroupDialogFacility.java
@@ -26,6 +26,7 @@ import androidx.annotation.Nullable;
 
 import org.hamcrest.Matcher;
 
+import org.chromium.base.Log;
 import org.chromium.base.Token;
 import org.chromium.base.test.transit.Element;
 import org.chromium.base.test.transit.Facility;
@@ -64,6 +65,8 @@ public class NewTabGroupDialogFacility<
     public ViewElement<View> doneButtonElement;
     private @Nullable String mTitle;
     private @Nullable List<Integer> mTabIdsToGroup;
+
+    private static final String TAG = "TransitLayer";
 
     /** Constructor. Expects no particular title or selected color. */
     public NewTabGroupDialogFacility(SoftKeyboardFacility softKeyboard) {
@@ -272,8 +275,13 @@ public class NewTabGroupDialogFacility<
 
     private void ensureSoftKeyboardClosed() {
         if (mSoftKeyboard.getPhase() == Phase.ACTIVE) {
+            Log.i(TAG, "SoftKeyboardFacility active, try to close soft keyboard.");
             mSoftKeyboard.close(dialogElement);
         } else if (mSoftKeyboard.getPhase() == Phase.FINISHED) {
+            Log.i(
+                    TAG,
+                    "SoftKeyboardFacility already finished, won't try to close soft keyboard"
+                            + " again.");
             // Do nothing as the soft keyboard has already been closed
         } else {
             throw new IllegalArgumentException(
