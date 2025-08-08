33a3bda4d320d0a109856c4e24711721218f14ad
Yiren Wang <yrw@chromium.org>
2025-08-08 13:16:04 -0700

    Add a unit test for MediaTranscriptProvider
    Bug: 409427125
    Change-Id: I2d2aca6450f7b5d44f5317e1837f9e890f86fa68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6832859
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Commit-Queue: Yiren Wang <yrw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1498992}

--- chrome/browser/glic/media/glic_media_integration.cc
+++ chrome/browser/glic/media/glic_media_integration.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/live_caption/caption_util.h"
 #include "components/live_caption/live_caption_controller.h"
 #include "components/live_caption/pref_names.h"
-#include "components/optimization_guide/content/browser/media_transcript_provider.h"
 #include "components/optimization_guide/proto/features/common_quality_data.pb.h"
 #include "components/prefs/pref_service.h"
 #include "content/public/browser/peer_connection_tracker_host_observer.h"
