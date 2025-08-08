278f193aead9d8fb8750bcb3dab1589c40c2415e
Jonathan Lee <jonathanjlee@google.com>
2025-08-08 13:44:16 -0700

    Import wpt@146144f19e30bb5282a25360c69cf8b88a0da220
    https://github.com/web-platform-tests/wpt/compare/fc4f94d9a...146144f19
    
    Using wpt-import in Chromium 7c5bca454d1dd698c1c9fde449b89325f7c367db.
    With Chromium commits locally applied on WPT:
      302d2fbacb "[webaudio-testharness] Migrate audioparam-close.html [...]
      952b83299e "Treat SVG documents in <object>/<embed> as replaced [...]
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: Ic0b1404dd4f88fc2b8fc0ab1488f2d07617145c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6830311
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1499008}

---
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/network/get_data/collector.py
@@ -0,0 +1,41 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import pytest
+import webdriver.bidi.error as error
+
+from .. import PAGE_EMPTY_TEXT
+
+pytestmark = pytest.mark.asyncio
+
+
+async def test_single_collector(bidi_session, url, setup_collected_response):
+    [request, collector] = await setup_collected_response(
+        fetch_url=url(PAGE_EMPTY_TEXT)
+    )
+    data = await bidi_session.network.get_data(
+        request=request, data_type="response", collector=collector
+    )
+
+    assert data["type"] == "string"
+    assert data["value"] == "empty\n"
+
+
+async def test_several_collectors(
+    bidi_session, url, add_data_collector, setup_collected_response
+):
+    collector = await add_data_collector(
+        collector_type="blob", data_types=["response"], max_encoded_data_size=1000
+    )
+    [request, other_collector] = await setup_collected_response(
+        fetch_url=url(PAGE_EMPTY_TEXT)
+    )
+
+    data = await bidi_session.network.get_data(
+        request=request, data_type="response", collector=collector
+    )
+    assert data["type"] == "string"
+    assert data["value"] == "empty\n"
+
+    data_from_other_collector = await bidi_session.network.get_data(
+        request=request, data_type="response", collector=other_collector
+    )
+    assert data_from_other_collector == data
