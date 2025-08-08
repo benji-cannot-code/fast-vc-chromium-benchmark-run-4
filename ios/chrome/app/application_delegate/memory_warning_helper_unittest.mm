d0c3ab5cdbf44d50eb13b7cacf9e202c1482e003
Patrick Monette <pmonette@chromium.org>
2025-08-08 07:17:54 -0700

    Use SyncMemoryPressureListener in MemoryWarningHelperTest
    This simplifies the test by making the notifications synchronous.
    
    Bug: 436324601
    Change-Id: I2e4c0cb81d4e382b4cfdc1023c57833b2a4928cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6824470
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1498771}

--- ios/chrome/app/application_delegate/memory_warning_helper_unittest.mm
+++ ios/chrome/app/application_delegate/memory_warning_helper_unittest.mm
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/functional/bind.h"
 #import "base/memory/memory_pressure_listener.h"
-#import "base/run_loop.h"
 #import "base/test/task_environment.h"
 #import "base/threading/thread.h"
 #import "components/previous_session_info/previous_session_info.h"
@@ -26,8 +25,7 @@ class MemoryWarningHelperTest : public PlatformTest {
     // Set up `memory_pressure_listener_` to invoke `OnMemoryPressure` which
     // will store the memory pressure level sent to the callback in
     // `memory_pressure_level_` so that tests can verify the level is correct.
-    memory_pressure_listener_.reset(new base::MemoryPressureListener(
-        FROM_HERE,
+    memory_pressure_listener_.reset(new base::SyncMemoryPressureListener(
         base::BindRepeating(&MemoryWarningHelperTest::OnMemoryPressure,
                             base::Unretained(this))));
     memory_pressure_level_ =
@@ -49,16 +47,12 @@ class MemoryWarningHelperTest : public PlatformTest {
   void OnMemoryPressure(
       base::MemoryPressureListener::MemoryPressureLevel memory_pressure_level) {
     memory_pressure_level_ = memory_pressure_level;
-    run_loop_.QuitWhenIdle();
   }
-
-  void RunMessageLoop() { run_loop_.Run(); }
 
  private:
   base::test::SingleThreadTaskEnvironment task_environment_;
-  base::RunLoop run_loop_;
   base::MemoryPressureListener::MemoryPressureLevel memory_pressure_level_;
-  std::unique_ptr<base::MemoryPressureListener> memory_pressure_listener_;
+  std::unique_ptr<base::SyncMemoryPressureListener> memory_pressure_listener_;
   MemoryWarningHelper* memory_helper_;
 };
 
@@ -80,7 +74,6 @@ TEST_F(MemoryWarningHelperTest, VerifyForegroundMemoryWarningCountReset) {
 // callback (i.e. MainControllerTest::OnMemoryPressure) is invoked.
 TEST_F(MemoryWarningHelperTest, VerifyApplicationDidReceiveMemoryWarning) {
   [GetMemoryHelper() handleMemoryPressure];
-  RunMessageLoop();
   EXPECT_EQ(base::MemoryPressureListener::MEMORY_PRESSURE_LEVEL_CRITICAL,
             GetMemoryPressureLevel());
 }
