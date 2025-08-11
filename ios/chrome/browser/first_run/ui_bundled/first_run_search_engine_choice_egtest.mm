62cfa60d5174aafc0f17c539c5db835ef8db5abb
Asami Doi <asamidoi@chromium.org>
2025-08-11 05:59:16 -0700

    [ios][gardener] Mark testIncognitoForcedByPolicy as flaky
    Bug: 427943675
    Change-Id: I0a5deea3a49612da53f54b75d0d2ce2ac802e0a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6836208
    Auto-Submit: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Owners-Override: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1499503}

--- ios/chrome/browser/first_run/ui_bundled/first_run_search_engine_choice_egtest.mm
+++ ios/chrome/browser/first_run/ui_bundled/first_run_search_engine_choice_egtest.mm
@@ -281,7 +281,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 // Tests that incognito can be forced through the FRE with search engine screen.
-- (void)testIncognitoForcedByPolicy {
+// TODO(crbug.com/427943675): Test is flaky on simulator. Reenable the test.
+#if TARGET_OS_SIMULATOR
+#define MAYBE_testIncognitoForcedByPolicy FLAKY_testIncognitoForcedByPolicy
+#else
+#define MAYBE_testIncognitoForcedByPolicy testIncognitoForcedByPolicy
+#endif
+- (void)MAYBE_testIncognitoForcedByPolicy {
   // Configure the policy to force sign-in.
   [self relaunchAppWithPolicyKey:policy::key::kIncognitoModeAvailability
                   xmlPolicyValue:"<integer>2</integer>"];
