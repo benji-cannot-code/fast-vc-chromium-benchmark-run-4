cafb59b46ba69957f9bb9cfd6e15f63337bf91be
Dave Tapuska <dtapuska@chromium.org>
2025-08-11 14:33:18 -0700

    [actor] Fix crash when no geometry
    It is possible there is no geometry. Log a TOU validation error in those
    situations.
    
    Bug: b:437969134
    Change-Id: I8f9687fa00c8d05453abf37ab76a8f0e10b56f8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6838917
    Reviewed-by: Rodney Ding <rodneyding@google.com>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1499830}

--- chrome/renderer/actor/tool_base.cc
+++ chrome/renderer/actor/tool_base.cc
@@ -151,6 +151,20 @@ ToolBase::ValidateTimeOfUse(const ResolvedTarget& resolved_target) const {
                         base::NumberToString(target_node.GetDomNodeId())}));
       // TODO(crbug.com/418280472): return error after retry for failed task is
       // landed.
+      return resolved_target;
+    }
+
+    if (!observed_target_->node_attribute->geometry) {
+      journal_->Log(
+          task_id_, "TimeOfUseValidation",
+          base::StrCat(
+              {"No geometry for node:",
+               base::NumberToString(
+                   *observed_target_->node_attribute->dom_node_id),
+               base::ToString(gfx::ToFlooredPoint(resolved_target.point))}));
+      // TODO(crbug.com/418280472): return error after retry for failed task is
+      // landed.
+      return resolved_target;
     }
 
     // Check that the interaction point is inside the observed target bounding
@@ -166,6 +180,7 @@ ToolBase::ValidateTimeOfUse(const ResolvedTarget& resolved_target) const {
                " Observed bounding box:", base::ToString(observed_bounds)}));
       // TODO(crbug.com/418280472): return error after retry for failed task is
       // landed.
+      return resolved_target;
     }
   }
 
