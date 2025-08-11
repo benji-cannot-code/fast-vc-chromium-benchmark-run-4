8929501d10950c2b38dd932f62f85178b6ae03cd
Johannes Henkel <johannes@chromium.org>
2025-08-11 10:35:41 -0700

    Remove unnecessary timeout in performance-entry-navigation-id.js
    The `setTimeout` was added to wait for trace events to be collected,
    but it appears that (1) it didn't actually help; (2) a few days
    later, the flakiness dropped anyway, most likely due to some
    other change. So I don't see a need to keep this timeout.
    
    Test History for ninja://:blink_web_tests/http/tests/inspector-protocol/tracing/performance-entry-navigation-id.js:
    https://luci-milo.appspot.com/ui/test/chromium/ninja%3A%2F%2F%3Ablink_web_tests%2Fhttp%2Ftests%2Finspector-protocol%2Ftracing%2Fperformance-entry-navigation-id.js
    
    Sequence of events:
    1) Flakiness increases around 2025-07-17 for linux-rel
    and other bots. Inspecting individual failures shows that
    the last trace event is sometimes not emitted, and sometimes
    the last two (rarely) or (very rarely) three aren't emitted.
    2) My attempt to fix this is to introduce a delay just before
    stopping to record the trace. https://crrev.com/c/6776081, which
    landed on 2025-07-22. This did not seem to change the number
    of flakes for linux-rel, unfortunately.
    3) Around 2025-07-30, the flakes drop from 564 to 4 on linux-rel.
    This is without any changes to the test.
    
    So, still not sure what happened / why the problem occurred,
    but I'd like to try to remove this delay since it's not clear
    it made a difference.
    
    Bug: 431553197, 433588348
    Change-Id: I9f6cf4789cc4b33102aca7ef4502d5648393e1b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6833716
    Commit-Queue: Johannes Henkel <johannes@chromium.org>
    Reviewed-by: Michal Mocny <mmocny@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1499667}

--- third_party/blink/web_tests/http/tests/inspector-protocol/tracing/performance-entry-navigation-id.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/tracing/performance-entry-navigation-id.js
@@ -177,10 +177,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     name: 'hard-lcp (entry)'
   });
 
-  // There's no principled way to wait for the trace events to arrive,
-  // so we give it a second here, before we stop tracing.
-  await new Promise(resolve => setTimeout(resolve, 1000));
-
   const unfilteredEvents = await tracingHelper.stopTracing();
 
   const traceEntries = [];
