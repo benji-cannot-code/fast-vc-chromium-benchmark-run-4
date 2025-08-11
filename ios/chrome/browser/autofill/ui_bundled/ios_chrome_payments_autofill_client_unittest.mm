8a6998f5940fc647ede867f4087682d3f2f9c7a0
Stephen McGruer <smcgruer@google.com>
2025-08-11 12:35:00 -0700

    [iOS] Enable AutofillSaveCardBottomSheet by default
    This feature has been approved for full launch, so enabling by default
    and removing from fieldtesting trial config.
    
    launch/4383266
    
    Bug: 427492497
    Change-Id: I73f613cf05f16078a19a9a7b97db95f27c681d0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6828628
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1499754}

--- ios/chrome/browser/autofill/ui_bundled/ios_chrome_payments_autofill_client_unittest.mm
+++ ios/chrome/browser/autofill/ui_bundled/ios_chrome_payments_autofill_client_unittest.mm
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/autofill/core/common/autofill_payments_features.h"
 #import "components/autofill/ios/browser/autofill_agent.h"
 #import "components/autofill/ios/browser/test_autofill_client_ios.h"
+#import "components/strings/grit/components_strings.h"
 #import "ios/chrome/browser/autofill/model/bottom_sheet/autofill_bottom_sheet_tab_helper.h"
 #import "ios/chrome/browser/autofill/ui_bundled/chrome_autofill_client_ios.h"
 #import "ios/chrome/browser/infobars/model/infobar_ios.h"
@@ -32,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "testing/platform_test.h"
 #import "third_party/ocmock/OCMock/OCMock.h"
 #import "third_party/ocmock/gtest_support.h"
+#import "ui/base/l10n/l10n_util.h"
 
 @interface FakeAutofillCommands : NSObject <AutofillCommands>
 
@@ -355,8 +357,10 @@ TEST_F(
 TEST_F(IOSChromePaymentsAutofillClientTest,
        UsesSaveCvcInfobarTypeForUploadSaveCvc) {
   // Set up the save options for a CVC-only save.
-  base::test::ScopedFeatureList features(
-      features::kAutofillEnableCvcStorageAndFilling);
+  base::test::ScopedFeatureList features;
+  features.InitWithFeatures(
+      /*enable_features=*/{features::kAutofillEnableCvcStorageAndFilling},
+      /*disable_features=*/{features::kAutofillSaveCardBottomSheet});
   payments::PaymentsAutofillClient::SaveCreditCardOptions options;
   options.card_save_type =
       payments::PaymentsAutofillClient::CardSaveType::kCvcSaveOnly;
@@ -638,6 +642,30 @@ TEST_F(IOSChromePaymentsAutofillClientWithSaveCardBottomSheetTest,
   const std::optional<AutofillErrorDialogContext>& error_context =
       [autofill_commands() autofillErrorDialogContext];
   EXPECT_FALSE(error_context.has_value());
+}
+
+// Test that a save CVC Bottomsheet is shown when saving only a CVC for upload
+// save.
+TEST_F(IOSChromePaymentsAutofillClientWithSaveCardBottomSheetTest,
+       UsesSaveCvcBottomsheetForUploadSaveCvc) {
+  // Set up the save options for a CVC-only save.
+  base::test::ScopedFeatureList features(
+      features::kAutofillEnableCvcStorageAndFilling);
+  payments::PaymentsAutofillClient::SaveCreditCardOptions options;
+  options.card_save_type =
+      payments::PaymentsAutofillClient::CardSaveType::kCvcSaveOnly;
+  options.show_prompt = true;
+
+  payments_client()->ShowSaveCreditCardToCloud(autofill::test::GetCreditCard(),
+                                               LegalMessageLines(), options,
+                                               base::DoNothing());
+
+  // Make sure that a bottomsheet was shown and is specifically for the CVC-only
+  // prompt.
+  EXPECT_TRUE([autofill_commands() showSaveCardBottomSheetCalled]);
+  EXPECT_EQ(
+      bottomsheet_tab_helper_->GetSaveCardBottomSheetModel()->title(),
+      l10n_util::GetStringUTF16(IDS_AUTOFILL_SAVE_CVC_PROMPT_TITLE_TO_CLOUD));
 }
 
 class IOSChromePaymentsAutofillClientWithLocalSaveCardBottomSheetTest
