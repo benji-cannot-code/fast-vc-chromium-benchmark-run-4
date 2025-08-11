70bbe5c10c1da76c4048858cf572004c23ce0427
Florian Leimgruber <fleimgruber@google.com>
2025-08-11 02:45:13 -0700

    Fix manual test import race condition
    Using command line flags, the code in manual_testing_import.h can import
    addresses and credit cards for manual testing. By design, the data
    imported this way overwrites any existing data. This is done to make
    manual testing of features (especially during testing reviews) easier,
    by getting Chrome into some predefined state.
    
    However, the "overwrites any existing data" bit is currently broken due
    to a race condition. Specifically, the manual testing import is
    triggered in the PDM's constructor, before the initial loading of data
    has finished. As a result, depending on whether loading or the import
    finishes first, the remove operations at [1] might be a no-op.
    
    This CL fixes this by tracking whether the initial load has finished
    inside the PDM. It's a bit unfortunate that additional state in the PDM
    is needed, since the ADM and PayDM already track this separately. But
    since the PDM itself only relies on an observer, there is no easy way
    to determine whether it's the first notification where both loads have
    finished.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:components/autofill/core/browser/manual_testing_import.cc;l=188,194-195;drc=f2b23a1b93cb06d057c8ddaea858bc3678cc9238
    
    Bug: 437826196
    Change-Id: I2e3197b287961b16b5feae9733426cfda9f69fc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6830066
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Piotr Kotynia <piotrkotynia@google.com>
    Cr-Commit-Position: refs/heads/main@{#1499453}

--- components/autofill/core/browser/data_manager/personal_data_manager.h
+++ components/autofill/core/browser/data_manager/personal_data_manager.h
@@ -167,6 +167,8 @@ class PersonalDataManager : public KeyedService,
   base::ScopedObservation<history::HistoryService, HistoryServiceObserver>
       history_service_observation_{this};
 
+  bool has_initial_load_finished_ = false;
+
   base::WeakPtrFactory<PersonalDataManager> weak_factory_{this};
 };
 
