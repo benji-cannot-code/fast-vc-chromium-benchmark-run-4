1bace709c7474b4156e5dd542b6409f5c551a1d9
Wenyu Fu <wenyufu@chromium.org>
2025-08-11 15:01:56 -0700

    [ViewState] Fix incognito GTS tab message card color
    Previously the baseline version of resource is using the light mode
    colors; for incognito's use case, the baseline resource should use dark
    colors.
    
    Also make card_hover_press_overlay_color_list_baseline_dark reference
    the correct baseline colors.
    
    Fixed: 437286219
    Change-Id: Ia45c35bf2ff69660291de0cf32b06bbba1df4d33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6828521
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1499846}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabUiThemeProvider.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabUiThemeProvider.java
@@ -330,7 +330,7 @@ public class TabUiThemeProvider {
      */
     public static int getMessageCardBackgroundResourceId(boolean isIncognito) {
         return isIncognito
-                ? R.drawable.card_background_corners_16dp_baseline
+                ? R.drawable.card_background_corners_16dp_baseline_dark
                 : R.drawable.card_background_corners_16dp;
     }
 
