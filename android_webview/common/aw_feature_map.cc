b1bc7c1adecb75e4e7943adc1ea8302b5d23582d
Brian Sheedy <bsheedy@chromium.org>
2025-08-11 16:20:25 -0700

    Revert "Adds 2 new flags to improve the freshness of app's copy of finch seed"
    This reverts commit b2cc8c95a443e42b520f11ecef26d2874596d916.
    
    Reason for revert: Breaks WebView startup crbug.com/437952021
    
    Bug: 414578477
    Original change's description:
    > Adds 2 new flags to improve the freshness of app's copy of finch seed
    >
    > These flags half the expiration of the seed and the minimum time between
    > requesting new seeds respectively. They'll be rolled out together to see
    > if there's a positive impact on app seed freshness.
    >
    > This CL also introduces a new native API for CachedFlags which is used
    > to record a synthetic field trial.
    >
    > Bug: 414578477
    > Change-Id: Id835dc7cd1b18c3944ebf0d697850314844c8f2b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6633976
    > Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    > Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    > Commit-Queue: Alex Mitra <alexmitra@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1499780}
    
    Bug: 414578477
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I6b1cf90542a2b788d389f056940be8c31365e1c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6839341
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1499876}

--- android_webview/common/aw_feature_map.cc
+++ android_webview/common/aw_feature_map.cc
@@ -55,8 +55,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &features::kWebViewQuicConnectionTimeout,
     &features::kWebViewRecordAppCacheHistograms,
     &features::kWebViewReduceUAAndroidVersionDeviceModel,
-    &features::kWebViewReducedSeedExpiration,
-    &features::kWebViewReducedSeedRequestPeriod,
     &features::kWebViewReportImeInsets,
     &features::kWebViewSafeAreaIncludesSystemBars,
     &features::kWebViewShortCircuitShouldInterceptRequest,
