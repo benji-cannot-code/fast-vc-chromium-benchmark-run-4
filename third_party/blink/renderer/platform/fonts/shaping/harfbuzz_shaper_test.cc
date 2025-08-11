38caf21b37638a53382cff0e32a2653f61f1ba52
Dominik Röttsches <drott@chromium.org>
2025-08-11 10:59:56 -0700

    [Fontations] Remove Fontations suffix from font names
    We've now fully switched to using Fontations, we no longer work in a
    hybrid mode where Fontations and FreeType typefaces get
    instantiated. Remove the suffix from tests and from the font family name
    retrieval logic, in which Skia would previously add it.
    
    Fixed: skia:307906051
    Change-Id: Ied3e606271b08da1d72ea1f1ac1cc1baec107c64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6835930
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Kyungjun Lee <kyungjunlee@google.com>
    Reviewed-by: Ben Wagner <bungeman@google.com>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1499692}

--- third_party/blink/renderer/platform/fonts/shaping/harfbuzz_shaper_test.cc
+++ third_party/blink/renderer/platform/fonts/shaping/harfbuzz_shaper_test.cc
@@ -182,15 +182,6 @@ class HarfBuzzShaperTest : public FontTestBase {
     return run_font_data[0].font_data_->PlatformData().FontFamilyName();
   }
 
-  StringView MaybeStripFontationsSuffix(const String& font_name) {
-    wtf_size_t found_index = font_name.ReverseFind(" (Fontations)");
-    if (found_index != kNotFound) {
-      return StringView(font_name, 0, found_index);
-    } else {
-      return font_name;
-    }
-  }
-
   const ShapeResult* SplitRun(ShapeResult* shape_result, unsigned offset) {
     unsigned length = shape_result->NumCharacters();
     const ShapeResult* run2 = shape_result->SubRange(offset, length);
@@ -823,18 +814,16 @@ TEST_F(HarfBuzzShaperTest, SystemEmojiVS15) {
       u"\u2614"
       u"\ufe0e");
   for (String text : {text_default, emoji_default}) {
-    EXPECT_EQ(MaybeStripFontationsSuffix(
-                  GetShapedFontFamilyNameForEmojiVS(*mono_font, text)),
-              StringView(kNotoEmojiFontName));
+    EXPECT_EQ(GetShapedFontFamilyNameForEmojiVS(*mono_font, text),
+              kNotoEmojiFontName);
     const char* system_mono_font_name = kSystemMonoEmojiFont;
 #if BUILDFLAG(IS_MAC)
     if (text == text_default) {
       system_mono_font_name = kSystemMonoTextDefaultEmojiFont;
     }
 #endif
-    EXPECT_EQ(MaybeStripFontationsSuffix(
-                  GetShapedFontFamilyNameForEmojiVS(*color_font, text)),
-              StringView(system_mono_font_name));
+    EXPECT_EQ(GetShapedFontFamilyNameForEmojiVS(*color_font, text),
+              system_mono_font_name);
   }
 }
 
@@ -852,12 +841,10 @@ TEST_F(HarfBuzzShaperTest, SystemEmojiVS16) {
       u"\u2614"
       u"\ufe0f");
   for (String text : {text_default, emoji_default}) {
-    EXPECT_EQ(MaybeStripFontationsSuffix(
-                  GetShapedFontFamilyNameForEmojiVS(*mono_font, text)),
-              StringView(kSystemColorEmojiFont));
-    EXPECT_EQ(MaybeStripFontationsSuffix(
-                  GetShapedFontFamilyNameForEmojiVS(*color_font, text)),
-              StringView(kNotoColorEmojiFontName));
+    EXPECT_EQ(GetShapedFontFamilyNameForEmojiVS(*mono_font, text),
+              kSystemColorEmojiFont);
+    EXPECT_EQ(GetShapedFontFamilyNameForEmojiVS(*color_font, text),
+              kNotoColorEmojiFontName);
   }
 }
 
@@ -903,12 +890,10 @@ TEST_P(FontVariantEmojiTest, FontVariantEmojiSystemFallback) {
     }
 #endif
 
-    EXPECT_EQ(MaybeStripFontationsSuffix(
-                  GetShapedFontFamilyNameForEmojiVS(*mono_font, text)),
-              StringView(expected_name_for_mono_requested_font));
-    EXPECT_EQ(MaybeStripFontationsSuffix(
-                  GetShapedFontFamilyNameForEmojiVS(*color_font, text)),
-              StringView(expected_name_for_color_requested_font));
+    EXPECT_EQ(GetShapedFontFamilyNameForEmojiVS(*mono_font, text),
+              expected_name_for_mono_requested_font);
+    EXPECT_EQ(GetShapedFontFamilyNameForEmojiVS(*color_font, text),
+              expected_name_for_color_requested_font);
   }
 }
 
@@ -926,15 +911,12 @@ TEST_F(HarfBuzzShaperTest, VSOverrideFontVariantEmoji) {
   const ShapeResult* result = shaper.Shape(font, TextDirection::kLtr);
   result->GetRunFontData(&run_font_data);
   EXPECT_EQ(run_font_data.size(), 3u);
-  EXPECT_EQ(MaybeStripFontationsSuffix(
-                run_font_data[0].font_data_->PlatformData().FontFamilyName()),
-            StringView(kSystemColorEmojiFont));
-  EXPECT_EQ(MaybeStripFontationsSuffix(
-                run_font_data[1].font_data_->PlatformData().FontFamilyName()),
-            StringView(kSystemMonoEmojiFont));
-  EXPECT_EQ(MaybeStripFontationsSuffix(
-                run_font_data[2].font_data_->PlatformData().FontFamilyName()),
-            StringView(kSystemColorEmojiFont));
+  EXPECT_EQ(run_font_data[0].font_data_->PlatformData().FontFamilyName(),
+            kSystemColorEmojiFont);
+  EXPECT_EQ(run_font_data[1].font_data_->PlatformData().FontFamilyName(),
+            kSystemMonoEmojiFont);
+  EXPECT_EQ(run_font_data[2].font_data_->PlatformData().FontFamilyName(),
+            kSystemColorEmojiFont);
 }
 
 TEST_F(HarfBuzzShaperTest, FontVariantEmojiTextSystemFallback) {
@@ -950,9 +932,8 @@ TEST_F(HarfBuzzShaperTest, FontVariantEmojiTextSystemFallback) {
 #endif
   String text(u"\u26CE");
   Font* color_font = CreateNotoColorEmoji(FontVariantEmoji::kTextVariantEmoji);
-  EXPECT_EQ(MaybeStripFontationsSuffix(
-                GetShapedFontFamilyNameForEmojiVS(*color_font, text)),
-            StringView(mono_font_name));
+  EXPECT_EQ(GetShapedFontFamilyNameForEmojiVS(*color_font, text),
+            mono_font_name);
 }
 
 #endif
