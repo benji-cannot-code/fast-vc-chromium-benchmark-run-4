1eec6b05526805696ab33cd8da9399dbd667283e
Muhammad Salmaan <musalmaan@chromium.org>
2025-08-11 12:06:04 -0700

    Add session restore infobar feature
    This CL is only adding the feature flag with a todo for where the
    implementation will be.
    
    Bug: 431828875
    Change-Id: I860a9278fb32a04fe3e8c2b6ea0f61edabc80ecb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6838237
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Muhammad Salmaan <musalmaan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1499736}

--- chrome/browser/ui/startup/infobar_utils.cc
+++ chrome/browser/ui/startup/infobar_utils.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/startup/startup_browser_creator.h"
 #include "chrome/browser/ui/startup/startup_types.h"
 #include "chrome/browser/ui/startup/test_third_party_cookie_phaseout_infobar_delegate.h"
+#include "chrome/browser/ui/ui_features.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 #include "components/infobars/content/content_infobar_manager.h"
@@ -42,7 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window/public/browser_window_features.h"
 #include "chrome/browser/ui/pdf/infobar/pdf_infobar_controller.h"
 #include "chrome/browser/ui/startup/default_browser_prompt/pin_infobar/pin_infobar_controller.h"
-#include "chrome/browser/ui/ui_features.h"
 #endif
 
 #if BUILDFLAG(IS_WIN) && BUILDFLAG(GOOGLE_CHROME_BRANDING)
@@ -189,6 +189,13 @@ void AddInfoBarsIfNecessary(Browser* browser,
     if (auto* controller = g_browser_process->GetFeatures()
                                ->installer_downloader_controller()) {
       controller->MaybeShowInfoBar();
+    }
+#endif
+
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
+    if (base::FeatureList::IsEnabled(features::kSessionRestoreInfobar)) {
+      // TODO(crbug.com/431828875): Instantiate and initialize the session
+      // restore controller.
     }
 #endif
 
