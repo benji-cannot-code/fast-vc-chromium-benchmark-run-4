ab342c5f3f846526d5fdea65bb877288b1d393b9
Alison Gale <agale@chromium.org>
2025-08-11 11:08:17 -0700

    [SxS] Only show light scrim when omnibox dropdown shows
    This signal better matches the user intent to use the omnibox. There are
    many situations where the omnibox can be focused while the user isn't
    interacting with it at all so scrimming in those scenarios is
    distracting.
    
    Bug: 434985237
    Change-Id: I11cd7486ec043553f8e1d251975ddb64bc36b00d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6825308
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Reviewed-by: Steven Luong <stluong@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1499694}

--- chrome/browser/ui/tabs/split_tab_scrim_controller.cc
+++ chrome/browser/ui/tabs/split_tab_scrim_controller.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/location_bar/location_bar_view.h"
 #include "chrome/browser/ui/views/page_info/page_info_bubble_view.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_view.h"
-#include "components/omnibox/common/omnibox_focus_state.h"
 #include "components/tabs/public/tab_interface.h"
 
 namespace split_tabs {
@@ -39,17 +38,13 @@ SplitTabScrimController::SplitTabScrimController(BrowserView* browser_view)
 SplitTabScrimController::~SplitTabScrimController() = default;
 
 bool SplitTabScrimController::ShouldShowScrim() {
-  tabs::TabInterface* const active_tab =
-      browser_window_interface_->GetActiveTabInterface();
-  return (active_tab &&
-          (OmniboxTabHelper::FromWebContents(active_tab->GetContents())
-               ->focus_state() != OmniboxFocusState::OMNIBOX_FOCUS_NONE)) ||
-         is_permission_prompt_showing_ || is_page_info_bubble_showing_;
+  return is_omnibox_popup_showing_ || is_permission_prompt_showing_ ||
+         is_page_info_bubble_showing_;
 }
 
-void SplitTabScrimController::OnOmniboxFocusChanged(
-    OmniboxFocusState state,
-    OmniboxFocusChangeReason reason) {
+void SplitTabScrimController::OnOmniboxPopupVisibilityChanged(
+    bool popup_is_open) {
+  is_omnibox_popup_showing_ = popup_is_open;
   UpdateScrimVisibility();
 }
 
