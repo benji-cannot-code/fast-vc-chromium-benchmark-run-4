c89656d90a9722165cd65e119623290b04bba072
Dana Fried <dfried@chromium.org>
2025-08-11 08:57:14 -0700

    [Browser Window] Easy ways to fetch element context, elements, views
    This moves the responsibility for providing ElementContext for a Browser
    to an UnownedUserData.
    
    BrowserElements (and its subclass BrowserElementsViews) will make it
    unnecessary to use Browser/BrowserWindow to get the element context, and
    also provide convenience methods for finding elements and sending custom
    events, which in turn, will remove many more verbose calls to
    ElementTracker[Views].
    
    You can use whichever is appropriate; in c/b/ui, use BrowserElements; in
    c/b/ui/views and some tests, use BrowserElementsViews.
    
    Implementation examples (as well as removing
    `BrowserWindow::GetElementContext()`) will happen in a follow-up CL.
    
    Change-Id: I047305502659dd0cb0b1573c0e35007396ae3ce0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6825843
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1499595}

---
+++ chrome/browser/ui/interaction/browser_elements.cc
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/interaction/browser_elements.h"
+
+#include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
+#include "ui/base/interaction/element_tracker.h"
+#include "ui/base/unowned_user_data/scoped_unowned_user_data.h"
+
+DEFINE_USER_DATA(BrowserElements);
+
+BrowserElements::BrowserElements(BrowserWindowInterface& browser)
+    : scoped_data_holder_(browser.GetUnownedUserDataHost(), *this) {}
+
+BrowserElements::~BrowserElements() = default;
+
+// static
+BrowserElements* BrowserElements::From(BrowserWindowInterface* browser) {
+  return Get(browser->GetUnownedUserDataHost());
+}
+
+ui::TrackedElement* BrowserElements::GetElement(ui::ElementIdentifier id) {
+  return ui::ElementTracker::GetElementTracker()->GetFirstMatchingElement(
+      id, GetContext());
+}
+
+BrowserElements::ElementList BrowserElements::GetAllElements(
+    ui::ElementIdentifier id) {
+  return ui::ElementTracker::GetElementTracker()->GetAllMatchingElements(
+      id, GetContext());
+}
+
+bool BrowserElements::NotifyEvent(ui::ElementIdentifier id,
+                                  ui::CustomElementEventType event_type) {
+  auto* const element = GetElement(id);
+  if (!element) {
+    return false;
+  }
+  ui::ElementTracker::GetFrameworkDelegate()->NotifyCustomEvent(element,
+                                                                event_type);
+  return true;
+}
