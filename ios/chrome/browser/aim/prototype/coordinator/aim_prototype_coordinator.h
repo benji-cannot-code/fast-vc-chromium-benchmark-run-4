991ce9c5e181936c1092762e2f273047aecb021c
Christian Xu <christianxu@google.com>
2025-08-11 08:31:17 -0700

    [iOS][AIM] Add custom presentation and dismissal animation
    This change replaces the default modal presentation of the AIM prototype
    with a custom animation.
    
    The new animation consists of:
    - A fade-in for the main view.
    - A slide-up animation for the input area upon presentation.
    - The reverse for dismissal (fade-out and slide-down).
    
    Key implementation details:
    - Created `AIMPrototypePresentAnimator` and `AIMPrototypeDismissAnimator` to handle the custom transition logic.
    - The `AIMPrototypeCoordinator` now acts as a `UIViewControllerTransitioningDelegate` to provide these custom animators.
    - Introduced the `AIMPrototypeAnimationContextProvider` protocol to safely provide the necessary views from the `AIMPrototypeViewController` to the animators, decoupling the components.
    
    Bug: None
    Change-Id: Id8807471295a0f9f26e237a5dc9d9c2deaa6ce74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6831462
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1499580}

--- ios/chrome/browser/aim/prototype/coordinator/aim_prototype_coordinator.h
+++ ios/chrome/browser/aim/prototype/coordinator/aim_prototype_coordinator.h
@@ -13,18 +13,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @class AIMPrototypeCoordinator;
 
-// Delegate for the AIM prototype coordinator.
+/// Delegate for the AIM prototype coordinator.
 @protocol AIMPrototypeCoordinatorDelegate
+/// Called when the coordinator is finished and should be stopped.
 - (void)aimPrototypeCoordinatorDidFinish:(AIMPrototypeCoordinator*)coordinator;
 @end
 
-// LensOverlayCoordinator presents the public interface for the Lens Overlay.
+/// AIMPrototypeCoordinator presents the public interface for the Lens Overlay.
 @interface AIMPrototypeCoordinator
     : ChromeCoordinator <AIMPrototypeViewControllerDelegate,
                          PHPickerViewControllerDelegate,
                          UIImagePickerControllerDelegate,
                          UINavigationControllerDelegate>
 
+/// The delegate for this coordinator.
 @property(nonatomic, weak) id<AIMPrototypeCoordinatorDelegate> delegate;
 
 @end
---
+++ ios/chrome/browser/aim/prototype/ui/aim_prototype_animation_context_provider.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_AIM_PROTOTYPE_UI_AIM_PROTOTYPE_ANIMATION_CONTEXT_PROVIDER_H_
+#define IOS_CHROME_BROWSER_AIM_PROTOTYPE_UI_AIM_PROTOTYPE_ANIMATION_CONTEXT_PROVIDER_H_
+
+#import <UIKit/UIKit.h>
+
+// Provides the views needed for the custom dismissal animation.
+@protocol AIMPrototypeAnimationContextProvider
+// The main view of the view controller to be faded out.
+@property(nonatomic, readonly) UIView* mainViewForAnimation;
+// The input plate view to be animated downwards.
+@property(nonatomic, readonly) UIView* inputPlateViewForAnimation;
+// The text view to be focused after the presentation animation.
+@property(nonatomic, readonly) UITextView* textViewForAnimation;
+@end
+
+#endif  // IOS_CHROME_BROWSER_AIM_PROTOTYPE_UI_AIM_PROTOTYPE_ANIMATION_CONTEXT_PROVIDER_H_
