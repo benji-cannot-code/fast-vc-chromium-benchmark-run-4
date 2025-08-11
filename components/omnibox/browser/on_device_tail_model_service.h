c66111af91cf86062f1cce513a9badca3b9fc0ca
Ce Chen <cechen@google.com>
2025-08-11 14:23:51 -0700

    [omnibox] add histograms for on device brain model performance analysis.
    These histograms can help us better understand the performance of our current RNN model in real world environment, and decide if we should move to latest Gemini Nano V3, especially given the decoding speed of V3 becomes slower per http://screenshot/6FFdNd8XfJthCoH.
    
    Bug: 40241602
    Change-Id: Ie67bfe6e7dd37e46262059325f8cbe9391a3471e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6831745
    Commit-Queue: Ce Chen <cechen@google.com>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1499815}

--- components/omnibox/browser/on_device_tail_model_service.h
+++ components/omnibox/browser/on_device_tail_model_service.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/memory_pressure_listener.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/task/sequenced_task_runner.h"
-#include "base/timer/timer.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/omnibox/browser/on_device_tail_model_executor.h"
 #include "components/optimization_guide/core/delivery/optimization_guide_model_provider.h"
