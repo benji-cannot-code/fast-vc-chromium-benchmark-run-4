67a451bb4d8a0c91ea52afe469b756268f01911b
Judith Hemp <hempjudith@google.com>
2025-08-11 07:35:00 -0700

    [PermissionsAI] Setup PassageEmbedder
    We strictly depend on the passage embedder for our feature to work.
    Lets set the model observer up such that the embedder model will get
    setup correctly for our feature. This is done similar to this code
    piece by just calling GetForProfile:
    
    https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/passage_embeddings/page_embeddings_service_factory.cc;drc=a1c9f45bef3cb866292f4737654f038c21f3bd58;l=68
    
    Bug: 437872774
    Change-Id: I41fb1a8a5adf4c32ef29792ca0b542f49eca7c68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6835872
    Reviewed-by: Elias Klim <elklm@chromium.org>
    Commit-Queue: Judith Hemp <hempjudith@google.com>
    Cr-Commit-Position: refs/heads/main@{#1499546}

--- chrome/browser/permissions/prediction_service/prediction_model_handler_provider.cc
+++ chrome/browser/permissions/prediction_service/prediction_model_handler_provider.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
 #include "chrome/browser/optimization_guide/optimization_guide_keyed_service.h"
-#include "chrome/browser/passage_embeddings/chrome_passage_embeddings_service_controller.h"
 #include "chrome/browser/permissions/prediction_service/permissions_aiv1_handler.h"
 #include "components/optimization_guide/core/delivery/optimization_guide_model_provider.h"
 #include "components/permissions/features.h"
@@ -26,7 +25,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace permissions {
 
 PredictionModelHandlerProvider::PredictionModelHandlerProvider(
-    OptimizationGuideKeyedService* optimization_guide) {
+    OptimizationGuideKeyedService* optimization_guide,
+    passage_embeddings::Embedder* passage_embedder)
+    : passage_embedder_(passage_embedder) {
   VLOG(1) << "[PermissionsAI] PredictionModelHandlerProvider ctor";
   // We set up model handlers if necessary in order of preference:
   // Aiv4, Aiv3, Aiv1
@@ -164,7 +165,8 @@ void PredictionModelHandlerProvider::set_permissions_aiv4_handler_for_testing(
 
 void PredictionModelHandlerProvider::set_passage_embedder_for_testing(
     passage_embeddings::Embedder* passage_embedder) {
-  passage_embedder_for_testing = passage_embedder;
+  CHECK_IS_TEST();
+  passage_embedder_ = passage_embedder;
 }
 
 bool PredictionModelHandlerProvider::IsAiv4ModelAvailable() {
@@ -173,15 +175,7 @@ bool PredictionModelHandlerProvider::IsAiv4ModelAvailable() {
 
 passage_embeddings::Embedder*
 PredictionModelHandlerProvider::GetPassageEmbedder() {
-  if (passage_embedder_for_testing.has_value()) {
-    CHECK_IS_TEST();
-    return passage_embedder_for_testing.value();
-  }
-  if (auto* passage_embeddings_service_controller =
-          passage_embeddings::ChromePassageEmbeddingsServiceController::Get()) {
-    return passage_embeddings_service_controller->GetEmbedder();
-  }
-  return nullptr;
+  return passage_embedder_;
 }
 
 #endif  // BUILDFLAG(BUILD_WITH_TFLITE_LIB)
