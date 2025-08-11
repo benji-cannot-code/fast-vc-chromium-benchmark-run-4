9fa7bbff27f917d7f762e1d26e7e39a2c439ad10
Elmehdi Rahmaoui <erahmaoui@google.com>
2025-08-11 07:18:18 -0700

    [iOS] Allow screenshots of fullpage in reader mode
    The aim of this CL is to allow the screenshot of pages in reader mode,
    by replacing the active `webState` with reader mode's.
    
    Bug: 430900237
    Change-Id: I885cf61303cd0ea6e1b1ee698e99710b0081ecda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6835932
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Elmehdi Rahmaoui <erahmaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1499533}

--- ios/chrome/browser/screenshot/model/screenshot_delegate.mm
+++ ios/chrome/browser/screenshot/model/screenshot_delegate.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/screenshot/model/screenshot_delegate.h"
 
+#import "ios/chrome/browser/reader_mode/model/features.h"
+#import "ios/chrome/browser/reader_mode/model/reader_mode_tab_helper.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
 #import "ios/chrome/browser/shared/model/browser/browser_provider.h"
 #import "ios/chrome/browser/shared/model/browser/browser_provider_interface.h"
@@ -63,6 +65,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       base::BindOnce(^(NSData* pdfDoumentData) {
         completionHandler(pdfDoumentData, 0, webViewFrame);
       });
+
+  if (IsReaderModeAvailable()) {
+    ReaderModeTabHelper* tabHelper =
+        ReaderModeTabHelper::FromWebState(webState);
+    if (tabHelper) {
+      web::WebState* readerModeWebState = tabHelper->GetReaderModeWebState();
+      if (readerModeWebState) {
+        webState = readerModeWebState;
+      }
+    }
+  }
 
   webState->CreateFullPagePdf(std::move(callback));
 }
