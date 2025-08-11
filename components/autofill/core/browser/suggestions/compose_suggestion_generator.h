78319aae2e4e322420a20d48d59a7ce5f995e3ec
Karol Sygiet <sygiet@google.com>
2025-08-11 02:41:57 -0700

    [RSG] Introduce ComposeSuggestionGenerator
    This CL introduces a SuggestionGenerator implementation for compose.
    Compared to the other suggestion generators, compose does not rely on
    any suggestion data in order to produce suggestions, so
    FetchSuggestionData() returns always with an empty vector and the
    GenerateSuggestions() method does the heavy lifting of checking for
    the field requirements.
    
    Bug: 409962888
    Change-Id: I090def184489c7f374c69e3406c9aef3705b9f3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6827221
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1499452}

---
+++ components/autofill/core/browser/suggestions/compose_suggestion_generator.h
@@ -0,0 +1,71 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_AUTOFILL_CORE_BROWSER_SUGGESTIONS_COMPOSE_SUGGESTION_GENERATOR_H_
+#define COMPONENTS_AUTOFILL_CORE_BROWSER_SUGGESTIONS_COMPOSE_SUGGESTION_GENERATOR_H_
+
+#include "components/autofill/core/browser/integrators/compose/autofill_compose_delegate.h"
+#include "components/autofill/core/browser/suggestions/suggestion_generator.h"
+
+namespace autofill {
+
+class ComposeSuggestionGenerator : public SuggestionGenerator {
+ public:
+  ComposeSuggestionGenerator(AutofillComposeDelegate* delegate,
+                             AutofillSuggestionTriggerSource trigger_source);
+  ~ComposeSuggestionGenerator() override;
+
+  void FetchSuggestionData(
+      const FormData& form_data,
+      const FormFieldData& field_data,
+      const FormStructure* form,
+      const AutofillField* field,
+      const AutofillClient& client,
+      base::OnceCallback<
+          void(std::pair<FillingProduct,
+                         std::vector<SuggestionGenerator::SuggestionData>>)>
+          callback) override;
+
+  void GenerateSuggestions(
+      const FormData& form_data,
+      const FormFieldData& field_data,
+      const FormStructure* form,
+      const AutofillField* field,
+      const std::vector<std::pair<FillingProduct, std::vector<SuggestionData>>>&
+          all_suggestion_data,
+      base::OnceCallback<void(ReturnedSuggestions)> callback) override;
+
+  // Like SuggestionGenerator override, but takes a base::FunctionRef instead of
+  // a base::OnceCallback. Calls that callback exactly once.
+  void FetchSuggestionData(
+      const FormData& form_data,
+      const FormFieldData& field_data,
+      const FormStructure* form,
+      const AutofillField* field,
+      const AutofillClient& client,
+      base::FunctionRef<
+          void(std::pair<FillingProduct,
+                         std::vector<SuggestionGenerator::SuggestionData>>)>
+          callback);
+
+  // Like SuggestionGenerator override, but takes a base::FunctionRef instead of
+  // a base::OnceCallback. Calls that callback exactly once.
+  void GenerateSuggestions(
+      const FormData& form_data,
+      const FormFieldData& field_data,
+      const FormStructure* form,
+      const AutofillField* field,
+      const std::vector<std::pair<FillingProduct, std::vector<SuggestionData>>>&
+          all_suggestion_data,
+      base::FunctionRef<void(ReturnedSuggestions)> callback);
+
+ private:
+  raw_ptr<AutofillComposeDelegate> compose_delegate_;
+  AutofillSuggestionTriggerSource trigger_source_;
+};
+
+}  // namespace autofill
+
+#endif  // COMPONENTS_AUTOFILL_CORE_BROWSER_SUGGESTIONS_COMPOSE_SUGGESTION_GENERATOR_H_
