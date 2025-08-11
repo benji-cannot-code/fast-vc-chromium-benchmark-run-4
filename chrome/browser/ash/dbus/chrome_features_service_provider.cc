50a60fa600d306516bdfc28ad952dfb70e04a28b
Mitsuru Oshima <oshima@chromium.org>
2025-08-11 12:42:34 -0700

    Remove kDeferConciergeStartup feature flag
    Bug: 434082728
    
    Change-Id: I8dfdfc892ff220205625905933d188055ba38a59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6835224
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1499756}

--- chrome/browser/ash/dbus/chrome_features_service_provider.cc
+++ chrome/browser/ash/dbus/chrome_features_service_provider.cc
@@ -203,7 +203,7 @@ void ChromeFeaturesServiceProvider::IsFeatureEnabled(
       &features::kSessionManagerLongKillTimeout,
       &features::kSessionManagerLivenessCheck,
       &features::kBorealisProvision,
-      &features::kDeferConciergeStartup,
+
   };
 
   dbus::MessageReader reader(method_call);
