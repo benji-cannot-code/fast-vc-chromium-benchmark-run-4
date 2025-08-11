c1e1b8b90580499017086aea8353c697c7aec8f8
Caroline Rising <corising@chromium.org>
2025-08-11 14:33:42 -0700

    [Vertical Tabs] Add VerticalTabStripRegionView class
    Add placeholder chid views, set up layout, and update separator
    visibility based on collapsed state.
    
    Bug: 434014566
    Change-Id: I55b225f3159d04f814d285ca07bfe1a2b4c0cee6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6824077
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1499831}

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -136,6 +136,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/top_container_loading_bar.h"
 #include "chrome/browser/ui/views/frame/top_container_view.h"
 #include "chrome/browser/ui/views/frame/top_controls_slide_controller.h"
+#include "chrome/browser/ui/views/frame/vertical_tab_strip_region_view.h"
 #include "chrome/browser/ui/views/frame/web_contents_close_handler.h"
 #include "chrome/browser/ui/views/fullscreen_control/fullscreen_control_host.h"
 #include "chrome/browser/ui/views/global_media_controls/media_toolbar_button_view.h"
@@ -1014,9 +1015,9 @@ BrowserView::BrowserView(std::unique_ptr<Browser> browser)
   set_contents_view(contents_container_);
 
   if (tabs::AreVerticalTabsEnabled()) {
-    auto vertical_tab_strip_container = std::make_unique<views::View>();
-    vertical_tab_strip_container->SetBackground(
-        views::CreateSolidBackground(ui::kColorFrameActive));
+    auto vertical_tab_strip_container =
+        std::make_unique<VerticalTabStripRegionView>(
+            browser_->GetFeatures().vertical_tab_strip_state_controller());
     vertical_tab_strip_container_ =
         AddChildView(std::move(vertical_tab_strip_container));
   }
