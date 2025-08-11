7794cae1a26a1abcfdba2f0d6445de0f2519bc23
Harsh Singh <harshsinghiitism@gmail.com>
2025-08-11 14:30:57 -0700

    [Extensions] Fix build failure in extension_side_panel_utils.h.
    This is a follow-up to CL 6638129, which introduced a dependency on
    std::optional without including the <optional> header. This caused
    build failures on LUCI bots that enforce module includes.
    
    This change adds the missing include to fix the build.
    
    Change-Id: I1d4f08a2b4b02911afd008c5847112b310b8d6da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6835436
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Oliver Dunk <oliverdunk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1499826}

--- chrome/browser/ui/extensions/extension_side_panel_utils.h
+++ chrome/browser/ui/extensions/extension_side_panel_utils.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_EXTENSIONS_EXTENSION_SIDE_PANEL_UTILS_H_
 #define CHROME_BROWSER_UI_EXTENSIONS_EXTENSION_SIDE_PANEL_UTILS_H_
 
+#include <optional>
+
 #include "extensions/common/extension_id.h"
 
 class Browser;
