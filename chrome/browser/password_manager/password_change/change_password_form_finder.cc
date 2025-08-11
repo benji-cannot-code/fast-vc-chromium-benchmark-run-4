5c72d9b5c6d27ad35cdcea7d1000ef2b3fc2cb15
Oleksandr Tara <otara@google.com>
2025-08-11 09:25:31 -0700

    Fix race condition between clicking button and finding new form
    It can happen, that after we click the button to open password change
    form, we fill and try to submit the form before we actually get a
    successful response from the button clicker. This causes the clicker
    tool to respond with "busy" when we try to submit the change form.
    
    Bug: 437825220
    Change-Id: I13819926543d14c79bffdd58c59f68f65dd1a802
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6836254
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Auto-Submit: Oleksandr Tara <otara@google.com>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1499618}

--- chrome/browser/password_manager/password_change/change_password_form_finder.cc
+++ chrome/browser/password_manager/password_change/change_password_form_finder.cc
@@ -5,7 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/password_manager/password_change/change_password_form_finder.h"
 
+#include "base/barrier_closure.h"
+#include "base/check.h"
 #include "base/functional/bind.h"
+#include "base/functional/callback_forward.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/task/single_thread_task_runner.h"
 #include "chrome/browser/optimization_guide/optimization_guide_keyed_service_factory.h"
@@ -195,14 +198,21 @@ void ChangePasswordFormFinder::OnExecutionResponseCallback(
     return;
   }
 
+  const auto button_clicked_or_form_found_cb = base::BarrierClosure(
+      /*num_closures=*/2,
+      base::BindOnce(
+          &ChangePasswordFormFinder::OnButtonClickedAndSubsequentFormFound,
+          weak_ptr_factory_.GetWeakPtr()));
   click_helper_ = std::make_unique<ButtonClickHelper>(
       web_contents_, dom_node_id,
       base::BindOnce(&ChangePasswordFormFinder::OnButtonClicked,
-                     weak_ptr_factory_.GetWeakPtr()));
+                     weak_ptr_factory_.GetWeakPtr())
+          .Then(button_clicked_or_form_found_cb));
   form_waiter_ = std::make_unique<ChangePasswordFormWaiter>(
       web_contents_, client_,
       base::BindOnce(&ChangePasswordFormFinder::OnSubsequentFormWaitingResult,
-                     weak_ptr_factory_.GetWeakPtr()),
+                     weak_ptr_factory_.GetWeakPtr())
+          .Then(button_clicked_or_form_found_cb),
       base::TimeDelta::Max());
 }
 
@@ -233,9 +243,10 @@ void ChangePasswordFormFinder::OnSubsequentFormWaitingResult(
   }
   if (!form_manager) {
     logs_uploader_->FormNotDetectedAfterOpening();
+    std::move(callback_).Run(nullptr);
+    return;
   }
-  CHECK(callback_);
-  std::move(callback_).Run(form_manager);
+  change_password_form_manager_ = form_manager;
 }
 
 void ChangePasswordFormFinder::OnFormNotFound() {
@@ -244,4 +255,9 @@ void ChangePasswordFormFinder::OnFormNotFound() {
   }
   CHECK(callback_);
   std::move(callback_).Run(nullptr);
+}
+
+void ChangePasswordFormFinder::OnButtonClickedAndSubsequentFormFound() {
+  CHECK(callback_);
+  std::move(callback_).Run(change_password_form_manager_);
 }
