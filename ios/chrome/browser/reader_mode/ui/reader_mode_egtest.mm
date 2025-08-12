c4f1e4e4894e19ceaef17708fb446693f5b017cf
ginnnnnnny <ginnyhuang@chromium.org>
2025-08-12 15:32:24 -0700

    [iOS][gardening] Reader mode egtest fix
    Failing on iPad when kLensOverlayEnableIPadCompatibility is not enabled.
    Forcing this feature on.
    
    Fixed: 436469457
    Change-Id: I51ba08d34fd79171dbc8123b49a4a3a818fb2712
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6843596
    Reviewed-by: Briana McClure <bmcclure@google.com>
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Owners-Override: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1500459}

--- ios/chrome/browser/reader_mode/ui/reader_mode_egtest.mm
+++ ios/chrome/browser/reader_mode/ui/reader_mode_egtest.mm
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/reader_mode/test/reader_mode_app_interface.h"
 #import "ios/chrome/browser/reader_mode/ui/constants.h"
 #import "ios/chrome/browser/shared/model/prefs/pref_names.h"
+#import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/signin/model/fake_system_identity.h"
 #import "ios/chrome/browser/text_zoom/ui_bundled/text_zoom_constants.h"
 #import "ios/chrome/grit/ios_strings.h"
@@ -152,6 +153,8 @@ id<GREYMatcher> VisibleContextMenuItem(int message_id) {
   if ([self isRunningTest:@selector(testTurnOnReaderModeViaPageActionMenu)] ||
       [self isRunningTest:@selector(testReaderModeChipShowsAIHubIfAvailable)]) {
     config.features_enabled_and_params.push_back({kPageActionMenu, {}});
+    config.features_enabled_and_params.push_back(
+        {kLensOverlayEnableIPadCompatibility, {}});
   } else {
     config.features_disabled.push_back(kPageActionMenu);
   }
