6543371da21dda5e4d01fb11ed46930ba599c739
Jonathan Ross <jonross@chromium.org>
2025-08-12 09:47:23 -0700

    viz: Refactor CompositorFrameSink mojom to use SetParams
    This change refactors the `viz.mojom.CompositorFrameSink` interface
    to simplify its settings mechanism and reduce API churn.
    
    A new `CompositorFrameSinkParams` struct is introduced to bundle
    various configuration options. Multiple individual setter methods
    have been replaced with a single
    `SetParams(CompositorFrameSinkParams)` method.
    
    This unifies SetWantsAnimateOnlyBeginFrames and
    SetAutoNeedsBeginFrame. In a follow up we will add a setting for
    NoCompositorFrameAcks
    
    As a bonus, the unused method `SetWantsBeginFrameAcks` has been
    removed. Its implementation was removed a while ago. We missed some
    mocks though.
    
    Bug: 40900977
    Change-Id: I23d6d3bd66fae3ee453b0315148750031f41d431
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6824347
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1500227}

--- components/viz/service/frame_sinks/compositor_frame_sink_impl.h
+++ components/viz/service/frame_sinks/compositor_frame_sink_impl.h
@@ -45,9 +45,8 @@ class CompositorFrameSinkImpl : public mojom::CompositorFrameSink {
   ~CompositorFrameSinkImpl() override;
 
   // mojom::CompositorFrameSink:
+  void SetParams(mojom::CompositorFrameSinkParamsPtr params) override;
   void SetNeedsBeginFrame(bool needs_begin_frame) override;
-  void SetWantsAnimateOnlyBeginFrames() override;
-  void SetAutoNeedsBeginFrame() override;
   void SubmitCompositorFrame(
       const LocalSurfaceId& local_surface_id,
       CompositorFrame frame,
@@ -74,6 +73,8 @@ class CompositorFrameSinkImpl : public mojom::CompositorFrameSink {
   // Must be destroyed before |compositor_frame_sink_client_|. This must never
   // change for the lifetime of CompositorFrameSinkImpl.
   const std::unique_ptr<CompositorFrameSinkSupport> support_;
+
+  bool params_set_ = false;
 };
 
 }  // namespace viz
