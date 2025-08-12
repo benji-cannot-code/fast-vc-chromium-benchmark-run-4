7e3afff4814063d610a0581a7e91649170a47b48
Duong Dac <ddac@chromium.org>
2025-08-12 08:05:09 -0700

    Enable sync datatypes for signed in users who never synced before
    This CL enables syncing Bookmarks, Reading List, and Settings (a.k.a
    preferences) for users who signed in but never enabled sync as part of
    the sync to signin migration.
    
    Previously, after the migration, the sync states for these datatypes are
    explicitly set to be disabled. This change removes the explicit pref
    value assignments, allowing them to take the default values defined by
    IsTypeSelectedByDefaultInTransportMode.
    
    Bug: 424124636
    Change-Id: I4b86c7e97ab4b17408bad8d45510ba33877d5b5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6836131
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1500148}

--- components/sync/service/sync_prefs_unittest.cc
+++ components/sync/service/sync_prefs_unittest.cc
@@ -1187,7 +1187,7 @@ TEST_F(SyncPrefsMigrationTest, GlobalPrefsAreUnchanged) {
   }
 }
 
-TEST_F(SyncPrefsMigrationTest, TurnsPreferencesOff) {
+TEST_F(SyncPrefsMigrationTest, MigratesPreferencesNotOptedIn) {
   base::test::ScopedFeatureList enable_sync_to_signin(
       kReplaceSyncPromosWithSignInPromos);
 
@@ -1201,9 +1201,15 @@ TEST_F(SyncPrefsMigrationTest, TurnsPreferencesOff) {
   prefs.MaybeMigratePrefsForSyncToSigninPart1(
       SyncPrefs::SyncAccountState::kSignedInWithoutSyncConsent, gaia_id_);
 
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
+  // After migration, Preferences should still be enabled (by default).
+  EXPECT_TRUE(prefs.GetSelectedTypesForAccount(gaia_id_).Has(
+      UserSelectableType::kPreferences));
+#else
   // Preferences should've been turned off in the account-scoped settings.
   EXPECT_FALSE(prefs.GetSelectedTypesForAccount(gaia_id_).Has(
       UserSelectableType::kPreferences));
+#endif
 }
 
 TEST_F(SyncPrefsMigrationTest, MigratesBookmarksOptedIn) {
@@ -1291,11 +1297,19 @@ TEST_F(SyncPrefsMigrationTest, MigratesBookmarksNotOptedIn) {
     prefs.MaybeMigratePrefsForSyncToSigninPart1(
         SyncPrefs::SyncAccountState::kSignedInWithoutSyncConsent, gaia_id_);
 
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
+    // After the migration, the types should be enabled.
+    EXPECT_TRUE(prefs.GetSelectedTypesForAccount(gaia_id_).Has(
+        UserSelectableType::kBookmarks));
+    EXPECT_TRUE(prefs.GetSelectedTypesForAccount(gaia_id_).Has(
+        UserSelectableType::kReadingList));
+#else
     // After the migration, the types should be disabled.
     EXPECT_FALSE(prefs.GetSelectedTypesForAccount(gaia_id_).Has(
         UserSelectableType::kBookmarks));
     EXPECT_FALSE(prefs.GetSelectedTypesForAccount(gaia_id_).Has(
         UserSelectableType::kReadingList));
+#endif
   }
 }
 
