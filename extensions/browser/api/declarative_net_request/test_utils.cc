a9bdafa66c7d60da06ddf8fa36f2d4fd12af4969
Dave Vandyke <kzar@chromium.org>
2025-08-12 08:00:06 -0700

    [DNR] Add the topDomains/excludedTopDomains rule conditions
    Add the the topDomains/excludedTopDomains rule conditions. Similar to
    initiatorDomains/excludedInitiatorDomains rule conditions, except that
    they are matched against the URL of the top-level frame associated with
    the request. For requests where there is no associated top-level frame
    (e.g. requests made by ServiceWorkers), the rule condition is matched
    against the request initiator's origin instead.
    
    See also:
     - https://github.com/w3c/webextensions/issues/762
     - https://docs.google.com/document/d/1EXTQM_jK5gGu7jj3lMSLSUcucjbTMxyhC3M99HQa9kQ/edit?tab=t.0#heading=h.x9snb54sjlu9
    
    Bug: 40879376
    Change-Id: Ieaa612041be7f5490ba74115e32420078248040e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4145958
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Dave Vandyke <kzar@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1500145}

--- extensions/browser/api/declarative_net_request/test_utils.cc
+++ extensions/browser/api/declarative_net_request/test_utils.cc
@@ -198,6 +198,9 @@ std::ostream& operator<<(std::ostream& output, const ParseResult& result) {
     case ParseResult::ERROR_EMPTY_REQUEST_DOMAINS_LIST:
       output << "ERROR_EMPTY_REQUEST_DOMAINS_LIST";
       break;
+    case ParseResult::ERROR_EMPTY_TOP_DOMAINS_LIST:
+      output << "ERROR_EMPTY_TOP_DOMAINS_LIST";
+      break;
     case ParseResult::ERROR_DOMAINS_AND_INITIATOR_DOMAINS_BOTH_SPECIFIED:
       output << "ERROR_DOMAINS_AND_INITIATOR_DOMAINS_BOTH_SPECIFIED";
       break;
@@ -241,6 +244,12 @@ std::ostream& operator<<(std::ostream& output, const ParseResult& result) {
       break;
     case ParseResult::ERROR_NON_ASCII_EXCLUDED_REQUEST_DOMAIN:
       output << "ERROR_NON_ASCII_EXCLUDED_REQUEST_DOMAIN";
+      break;
+    case ParseResult::ERROR_NON_ASCII_TOP_DOMAIN:
+      output << "ERROR_NON_ASCII_TOP_DOMAIN";
+      break;
+    case ParseResult::ERROR_NON_ASCII_EXCLUDED_TOP_DOMAIN:
+      output << "ERROR_NON_ASCII_EXCLUDED_TOP_DOMAIN";
       break;
     case ParseResult::ERROR_INVALID_URL_FILTER:
       output << "ERROR_INVALID_URL_FILTER";
@@ -598,7 +607,8 @@ base::flat_set<int> GetDisabledRuleIdsFromMatcherForTesting(
 RequestParams CreateRequestWithResponseHeaders(
     const GURL& url,
     const net::HttpResponseHeaders* headers) {
-  return RequestParams(url, url::Origin(), dnr_api::ResourceType::kSubFrame,
+  return RequestParams(url, url::Origin(), url::Origin(),
+                       dnr_api::ResourceType::kSubFrame,
                        dnr_api::RequestMethod::kGet, -1, headers);
 }
 
