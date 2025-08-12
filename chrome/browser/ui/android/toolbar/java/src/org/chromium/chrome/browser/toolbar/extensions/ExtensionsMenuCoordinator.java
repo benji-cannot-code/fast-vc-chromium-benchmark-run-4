b81a8e47a53ef75b34be021428501bdea1220f41
Ted Choc <tedchoc@chromium.org>
2025-08-11 21:53:52 -0700

    Move extensions enabled logic to ExtensionsMenuMediator.
    Aiming to keep as much business logic out of the menu coordinator.
    
    Bug: 427670411
    Change-Id: I4198f23990ece1fb77c5db66843c1fcc309760ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6831887
    Reviewed-by: Masa Fujita <massan@google.com>
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1499964}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/extensions/ExtensionsMenuCoordinator.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/extensions/ExtensionsMenuCoordinator.java
@@ -17,7 +17,6 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.divider.MaterialDivider;
 
-import org.chromium.base.Callback;
 import org.chromium.base.lifetime.Destroyable;
 import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.build.annotations.NullMarked;
@@ -28,7 +27,6 @@ import org.chromium.chrome.browser.tab.TabLaunchType;
 import org.chromium.chrome.browser.tabmodel.TabCreator;
 import org.chromium.chrome.browser.theme.ThemeColorProvider;
 import org.chromium.chrome.browser.toolbar.MenuBuilderHelper;
-import org.chromium.chrome.browser.ui.extensions.ExtensionActionsBridge;
 import org.chromium.chrome.browser.ui.extensions.R;
 import org.chromium.chrome.browser.ui.theme.BrandedColorScheme;
 import org.chromium.components.embedder_support.util.UrlConstants;
@@ -54,7 +52,6 @@ public class ExtensionsMenuCoordinator implements Destroyable {
     private final ListMenuButton mExtensionsMenuButton;
     private final MaterialDivider mExtensionsMenuTabSwitcherDivider;
     private final ThemeColorProvider mThemeColorProvider;
-    private final ObservableSupplier<Profile> mProfileSupplier;
     private final ObservableSupplier<Tab> mCurrentTabSupplier;
     private final TabCreator mTabCreator;
     private final View mContentView;
@@ -62,9 +59,7 @@ public class ExtensionsMenuCoordinator implements Destroyable {
     private final ModelList mExtensionModels;
 
     private final ThemeColorProvider.TintObserver mTintObserver = this::onTintChanged;
-    private final Callback<Profile> mProfileUpdatedCallback = this::onProfileUpdated;
 
-    @Nullable private Profile mProfile;
     @Nullable @VisibleForTesting ExtensionsMenuMediator mMediator;
 
     private boolean mShouldShowMenuOnInit;
@@ -103,9 +98,6 @@ public class ExtensionsMenuCoordinator implements Destroyable {
         mThemeColorProvider = themeColorProvider;
         mThemeColorProvider.addTintObserver(mTintObserver);
 
-        mProfileSupplier = profileSupplier;
-        mProfileSupplier.addObserver(mProfileUpdatedCallback);
-
         mContentView = LayoutInflater.from(mContext).inflate(R.layout.extensions_menu, null, false);
 
         PropertyModel model = createMenuPropertyModel();
@@ -136,7 +128,7 @@ public class ExtensionsMenuCoordinator implements Destroyable {
 
         mMediator =
                 new ExtensionsMenuMediator(
-                        mProfileSupplier,
+                        profileSupplier,
                         mCurrentTabSupplier,
                         mExtensionModels,
                         () -> {
@@ -158,27 +150,12 @@ public class ExtensionsMenuCoordinator implements Destroyable {
                                 mExtensionsMenuButton.showMenu();
                                 mShouldShowMenuOnInit = false;
                             }
+                        },
+                        (extensionsSupported) -> {
+                            int visibility = extensionsSupported ? View.VISIBLE : View.GONE;
+                            mExtensionsMenuButton.setVisibility(visibility);
+                            mExtensionsMenuTabSwitcherDivider.setVisibility(visibility);
                         });
-    }
-
-    private void onProfileUpdated(@Nullable Profile profile) {
-        if (profile == mProfile) {
-            return;
-        }
-
-        mProfile = profile;
-
-        // TODO(crbug.com/422307625): Remove this check once extensions are ready for dogfooding.
-        int visibility = View.GONE;
-        if (mProfile != null) {
-            ExtensionActionsBridge extensionActionsBridge = ExtensionActionsBridge.get(mProfile);
-            if (extensionActionsBridge != null && extensionActionsBridge.extensionsEnabled()) {
-                visibility = View.VISIBLE;
-            }
-        }
-
-        mExtensionsMenuButton.setVisibility(visibility);
-        mExtensionsMenuTabSwitcherDivider.setVisibility(visibility);
     }
 
     private void openUrlFromMenu(String url) {
@@ -244,8 +221,6 @@ public class ExtensionsMenuCoordinator implements Destroyable {
         }
         mExtensionsMenuButton.setOnClickListener(null);
         mThemeColorProvider.removeTintObserver(mTintObserver);
-        mProfileSupplier.removeObserver(mProfileUpdatedCallback);
-        mProfile = null;
         mChangeProcessor.destroy();
     }
 
