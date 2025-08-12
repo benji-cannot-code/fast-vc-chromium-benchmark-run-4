b033542bb1a67cb2cdbd7f7b3c8354cfb3d47c84
dljames <dljames@chromium.org>
2025-08-12 13:47:13 -0700

    [Vertical Tabs] Use TabStripViewInterface for GetDragContext calls
    Replaces calls to TabStrip::GetDragContext with calls to
    tab_strip_view()->GetDragContext() where applicable.
    
    Removes unused includes in tab_drag_controller to follow the include
    what you use methodology.
    
    Change-Id: I7d0ae0f837d5c027b82fb450e2aa2d37bbbbf286
    Bug: 436873217
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6838040
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1500397}

--- chrome/browser/ui/views/frame/multi_contents_view_tab_drag_interactive_uitest.cc
+++ chrome/browser/ui/views/frame/multi_contents_view_tab_drag_interactive_uitest.cc
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/test/test_timeouts.h"
 #include "chrome/browser/ui/browser_list.h"
+#include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/multi_contents_drop_target_view.h"
+#include "chrome/browser/ui/views/frame/tab_strip_view_interface.h"
 #include "chrome/browser/ui/views/tabs/dragging/tab_drag_controller.h"
 #include "chrome/browser/ui/views/test/split_tabs_interactive_test_mixin.h"
 #include "chrome/browser/ui/views/test/tab_strip_interactive_test_mixin.h"
@@ -98,8 +100,8 @@ void Poll(base::RepeatingCallback<bool()> condition,
 // This should be created before drag loop is started.
 class QuitTabDraggingObserver {
  public:
-  explicit QuitTabDraggingObserver(TabStrip* tab_strip) {
-    tab_strip->GetDragContext()->SetDragControllerCallbackForTesting(
+  explicit QuitTabDraggingObserver(TabStripViewInterface* tab_strip_view) {
+    tab_strip_view->GetDragContext()->SetDragControllerCallbackForTesting(
         base::BindOnce(&QuitTabDraggingObserver::OnDragControllerSet,
                        weak_ptr_factory_.GetWeakPtr()));
   }
@@ -257,10 +259,9 @@ IN_PROC_BROWSER_TEST_P(MultiContentsViewTabDragEntrypointsUiParamTest,
   SKIP_FOR_WAYLAND();
 
   BrowserView& browser_view = GetBrowserView();
-  TabStrip* tab_strip = browser_view.tabstrip();
   const auto drop_side = GetParam();
 
-  QuitTabDraggingObserver observer(tab_strip);
+  QuitTabDraggingObserver observer(browser_view.tab_strip_view());
   RunTestSequence(
       AddInstrumentedTab(kNewTab, GURL(chrome::kChromeUISettingsURL), 1),
       AddInstrumentedTab(kSecondTab, GURL(chrome::kChromeUISettingsURL), 2),
@@ -286,10 +287,9 @@ IN_PROC_BROWSER_TEST_P(MultiContentsViewTabDragEntrypointsUiParamTest,
   SKIP_FOR_WAYLAND();
 
   BrowserView& browser_view = GetBrowserView();
-  TabStrip* tab_strip = browser_view.tabstrip();
   const auto drop_side = GetParam();
 
-  QuitTabDraggingObserver observer(tab_strip);
+  QuitTabDraggingObserver observer(browser_view.tab_strip_view());
   RunTestSequence(
       AddInstrumentedTab(kNewTab, GURL(chrome::kChromeUISettingsURL), 1),
       AddInstrumentedTab(kSecondTab, GURL(chrome::kChromeUISettingsURL), 2),
