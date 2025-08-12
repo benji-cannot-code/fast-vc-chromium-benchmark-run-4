ffa1a2fd99187076429c8e1e751c4fc3bc2437d5
Matt Stark <msta@google.com>
2025-08-11 18:03:51 -0700

    Improve and simplify header creation and target building.
    * Header deps are headers, not references
    * All headers have an absolute path that exists on disk.
    * Calculate dependencies better for include_next'd targets.
    * Headers use 'required deps' instead of direct deps in most places
      * Required deps handle textual deps better
    * Replaced the clunky graph[(Directory, rel)] with something closer to
      what the compiler would see (rel -> linked list of headers)
    * Better handle symlinks in the sysroot
    
    Bug: 435303825
    
    Change-Id: I6a6a6964b0aefeabbb50b4838844b62aecccddec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6830384
    Commit-Queue: Matt Stark <msta@google.com>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Auto-Submit: Matt Stark <msta@google.com>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1499910}

--- build/modules/modularize/modularize.py
+++ build/modules/modularize/modularize.py
@@ -49,7 +49,7 @@ def main(args):
       return
 
   graph = compiler.compile_all()
-  fix_graph(graph, compiler.os, compiler.cpu)
+  fix_graph(graph, compiler)
   targets = run_build(graph)
   out_dir.mkdir(exist_ok=True, parents=False)
   # Since apple provides a modulemap, we only need to create a BUILD.gn file.
