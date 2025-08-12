bb18c4c3b55b3a4936612688b67bafd33ba81eb9
Justin Lulejian <jlulejian@chromium.org>
2025-08-12 11:28:46 -0700

    [Extensions] Fix listener responds with unserializable value.
    Before this change, when a listener sent an unserializable value back to
    the message sender, we would throw an error in the listener JS context
    and not close the message port. This would cause the port to remain open
    and eventually when the worker stopped the sender would get a response
    of `undefined` (unless another listener responded).
    
    After this change, we now immediately close the port (and channel)
    because the listener responded in an invalid way. We pass along the
    error to the sender as "Could not serialize message.". This more closely
    behaves like chromium using github.com/mozilla/webextension-polyfill
    which passes back a (different) error (though it waits until the worker
    stops).
    
    I've flagged as disabled because while I'm fairly confident this is the
    more intuitive behavior, I'd like to get other browser alignment on one
    time message response behavior before changing this globally and forcing
    the issue for developers. It appears firefox, safari, chromium, and
    chromium+webextension-polyfill all behave differently before and after
    this change so hence the flag.
    
    Low-Coverage-Reason: COVERAGE_UNDERREPORTED crbug.com/437945199
    Bug: 433214074
    Change-Id: I846ad37f5b7067e1d21d15b4124fef91b85da21d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6825702
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1500292}

---
+++ extensions/test/data/api_test/messaging/send_message_promise_polyfill_unserializable/background.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function onMessageListener(message, sender, sendResponse) {
+  switch (message) {
+      // JS functions are not JSON serializable. The below causes extensions v8
+      // C++ logic to throw a TypeError in this context.
+    case 'respond synchronously with an unserializable value':
+      chrome.test.assertThrows(
+          sendResponse, [() => {}], 'Could not serialize message.');
+      break;
+    case 'respond asynchronously with an unserializable value':
+      setTimeout(() => {
+        chrome.test.assertThrows(
+            sendResponse, [() => {}], 'Could not serialize message.');
+      }, 1);
+      return true;
+    default:
+      chrome.test.fail('Unexpected test message: ' + message);
+  }
+}
+chrome.runtime.onMessage.addListener(onMessageListener);
