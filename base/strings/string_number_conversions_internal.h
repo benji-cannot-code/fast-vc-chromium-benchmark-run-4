4c2c64ac2a7a9ac8dfeeb3c79d93c7f1cdbd8ab1
Avi Drissman <avi@chromium.org>
2025-08-12 10:23:02 -0700

    Improve base::ByteCount
    - Extend mojo-fication so that it works better in Blink and is
      available for WebUI
    - Allow the direct usage in Blink
    - Fix logging for 0B (it used to log "0GiB")
    - Fix logging for negative values
    - Extend logging for the new extended range
    - Add more tests to cover all new functionality
    
    Bug: 429140103
    Change-Id: I504738fe67fda188bc12564fccbf0dbb3610ccb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6830312
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1500250}

--- base/strings/string_number_conversions_internal.h
+++ base/strings/string_number_conversions_internal.h
@@ -20,9 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/third_party/double_conversion/double-conversion/double-conversion.h"
 
-namespace base {
-
-namespace internal {
+namespace base::internal {
 
 template <typename STR, typename INT>
 static STR IntToStringT(INT value) {
@@ -236,6 +234,14 @@ StringT DoubleToStringT(double value) {
   return ToString<StringT>(buffer, static_cast<size_t>(builder.position()));
 }
 
+template <typename StringT>
+StringT DoubleToStringFixedT(double value, int digits) {
+  char buffer[32];
+  double_conversion::StringBuilder builder(buffer, sizeof(buffer));
+  GetDoubleToStringConverter()->ToFixed(value, digits, &builder);
+  return ToString<StringT>(buffer, static_cast<size_t>(builder.position()));
+}
+
 template <typename STRING, typename CHAR>
 bool StringToDoubleImpl(STRING input, const CHAR* data, double& output) {
   static double_conversion::StringToDoubleConverter converter(
@@ -282,8 +288,6 @@ static bool HexStringToByteContainer(std::string_view input, OutIter output) {
   return true;
 }
 
-}  // namespace internal
-
-}  // namespace base
+}  // namespace base::internal
 
 #endif  // BASE_STRINGS_STRING_NUMBER_CONVERSIONS_INTERNAL_H_
--- mojo/public/cpp/base/byte_count_mojom_traits.h
+++ mojo/public/cpp/base/byte_count_mojom_traits.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace mojo {
 
 template <>
-struct COMPONENT_EXPORT(MOJO_BASE_TRAITS)
+struct COMPONENT_EXPORT(MOJO_BASE_SHARED_TRAITS)
     StructTraits<mojo_base::mojom::ByteCountDataView, base::ByteCount> {
   static int64_t count(const base::ByteCount& byte_count) {
     return byte_count.InBytes();
