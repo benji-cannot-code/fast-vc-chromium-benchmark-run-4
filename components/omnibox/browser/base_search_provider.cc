253113442bb38de04ffde048d45f7fa596942fd4
manukh <manukh@chromium.org>
2025-08-12 12:25:27 -0700

    [omnibox][ai-mode] Hide server AI matches when policy/pref is disabled
    Client-side AI matches are disabled when AI policy is disabled. But we
    never added that check for server AI matches. This CL does so.
    
    Change-Id: I9708c1ca052ba476195309dcb614dc0025cb4bc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6838044
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1500344}

--- components/omnibox/browser/base_search_provider.cc
+++ components/omnibox/browser/base_search_provider.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/autocomplete_provider_client.h"
 #include "components/omnibox/browser/autocomplete_provider_listener.h"
 #include "components/omnibox/browser/omnibox_field_trial.h"
+#include "components/omnibox/browser/omnibox_prefs.h"
 #include "components/omnibox/browser/page_classification_functions.h"
 #include "components/omnibox/browser/remote_suggestions_service.h"
 #include "components/omnibox/browser/search_scoring_signals_annotator.h"
@@ -600,7 +601,8 @@ void BaseSearchProvider::AddMatchToMap(
   if (!match.destination_url.is_valid())
     return;
   if (match.IsSearchAimSuggestion() &&
-      !omnibox_feature_configs::AiMode::Get().allow_ai_mode_matches) {
+      (!omnibox_feature_configs::AiMode::Get().allow_ai_mode_matches ||
+       !omnibox::IsAimAllowedByPolicy(client_->GetPrefs()))) {
     return;
   }
   match.RecordAdditionalInfo(kRelevanceFromServerKey,
