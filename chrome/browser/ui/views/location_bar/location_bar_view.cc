ca17247d7abd9bded3b5bf6fd0ffd1946c7c006a
Justin Donnelly <jdonnelly@chromium.org>
2025-08-11 18:27:59 -0700

    [omnibox] Replaces focus on AIM button with just showing the focus ring.
    Rather than moving keyboard focus to the AIM button in the omnibox
    when tabbing through the popup's focus selections, instead simply
    install a focus ring on it to indicate that it is the target of
    the next action (e.g., pressing Enter).
    
    Mouse event activations are still handled by the button but keyboard
    events are now handled by the omnibox view since it retains focus.
    The AI Mode button activations are handled with a new case in
    OmniboxEditModel::OpenSelection.
    
    Additionally, this moves the AI Mode URL navigations from the button
    view to the edit model, and the URL is now generated using the same
    utility method used by the realbox.
    
    Finally, this fixes the issue with the button disappearing when
    pressing tab when the popup is closed.
    
    Bug: b:437913260, b:436257080
    Change-Id: If54a9aa3297a2d70b1ace40ecec0b2470539b5c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6820330
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Nihar Majmudar <niharm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1499920}

--- chrome/browser/ui/views/location_bar/location_bar_view.cc
+++ chrome/browser/ui/views/location_bar/location_bar_view.cc
@@ -1306,14 +1306,6 @@ bool LocationBarView::RefreshContentSettingViews() {
   return visibility_changed;
 }
 
-void LocationBarView::RefreshAimPageActionIcon() {
-  PageActionIconView* aim_page_action_icon_view =
-      page_action_icon_controller_->GetIconView(PageActionIconType::kAiMode);
-  if (aim_page_action_icon_view) {
-    aim_page_action_icon_view->Update();
-  }
-}
-
 void LocationBarView::RefreshPageActionIconViews() {
   if (web_app::AppBrowserController::IsWebApp(browser_)) {
     // For web apps, the location bar is normally hidden and icons appear in
@@ -1589,9 +1581,6 @@ void LocationBarView::OnOmniboxFocused() {
   // the omnibox is intentional, snapping is better than transitioning here.
   hover_animation_.Reset();
   RefreshBackground();
-
-  // Ensure AIM page action button reacts to changes in Omnibox focus state.
-  RefreshAimPageActionIcon();
 }
 
 void LocationBarView::OnOmniboxBlurred() {
@@ -1599,9 +1588,6 @@ void LocationBarView::OnOmniboxBlurred() {
     views::FocusRing::Get(this)->SchedulePaint();
   }
   RefreshBackground();
-
-  // Ensure AIM page action button reacts to changes in Omnibox focus state.
-  RefreshAimPageActionIcon();
 }
 
 void LocationBarView::OnOmniboxHovered(bool is_hovering) {
