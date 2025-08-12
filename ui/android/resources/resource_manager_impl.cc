3c4d42c0e1b3769f687ef0d3a99f14ef2b180540
Peilin Wang <peilinwang@google.com>
2025-08-12 13:27:39 -0700

    Guard dumpWithoutCrashing behind flag
    Too many reports, silence and investigate later.
    
    Bug: 431469892, 431190834
    Change-Id: I673c1e55c7f0e07926e28aa0c920034e89fb5917
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6842463
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Commit-Queue: Peilin Wang <peilinwang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1500384}

--- ui/android/resources/resource_manager_impl.cc
+++ ui/android/resources/resource_manager_impl.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_array.h"
 #include "base/android/jni_string.h"
 #include "base/debug/dump_without_crashing.h"
+#include "base/feature_list.h"
 #include "base/memory/ptr_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/task/single_thread_task_runner.h"
@@ -23,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/trace_event.h"
 #include "cc/resources/scoped_ui_resource.h"
 #include "cc/resources/ui_resource_manager.h"
+#include "components/viz/common/features.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "third_party/skia/include/core/SkCanvas.h"
 #include "third_party/skia/include/core/SkColorFilter.h"
@@ -255,7 +257,10 @@ void ResourceManagerImpl::AssertResourceExists(JNIEnv* env,
                                                jint res_type,
                                                jint res_id) {
   if (resources_[res_type].find(res_id) == resources_[res_type].end()) {
-    base::debug::DumpWithoutCrashing();
+    if (base::FeatureList::IsEnabled(
+            features::kAndroidDumpForBadCompositedUiState)) {
+      base::debug::DumpWithoutCrashing();
+    }
   }
 }
 
