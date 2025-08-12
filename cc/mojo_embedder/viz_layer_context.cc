97ff832e4905d169fc8441ea6a6c4d2cd6cb7c82
Victor Miura <vmiura@chromium.org>
2025-08-12 15:53:50 -0700

    [trees-in-viz] Fix incremental animation updates during full sync
    When a full sync to Viz is required (needs_full_sync_), cached animation state was not being cleared. This resulted in an incorrect incremental animation update being sent instead of the required full state.
    
    This change addresses the issue by clearing pushed_animation_timelines_ when needs_full_sync_ is set, ensuring that the complete animation state is always pushed to Viz during a full sync.
    
    Bug: 437995947
    Change-Id: I440f2fdd433ce5e4d4e79a6dc22b0772bc3fb146
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6842256
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Victor Miura <vmiura@chromium.org>
    Auto-Submit: Victor Miura <vmiura@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1500475}

--- cc/mojo_embedder/viz_layer_context.cc
+++ cc/mojo_embedder/viz_layer_context.cc
@@ -1345,6 +1345,7 @@ base::TimeTicks VizLayerContext::UpdateDisplayTreeFrom(
   // diffing every time.
   if (needs_full_sync_) {
     last_committed_property_trees_.clear();
+    pushed_animation_timelines_.clear();
   }
   PropertyTrees& old_trees = last_committed_property_trees_;
   ComputePropertyTreeUpdate(
