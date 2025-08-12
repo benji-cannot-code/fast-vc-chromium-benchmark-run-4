f046eb88e4ad904c2b46a03efd252a57e2525260
Joey Arhar <jarhar@chromium.org>
2025-08-12 12:13:14 -0700

    Fix <input type=range> event handling when switching documents
    <input type=range> listens to touch/pointer events and sets up the event
    handler registry to indicate that these events should be fired on the
    page. When the element is moved between documents, it was not correctly
    telling the new document to fire these events before this patch.
    
    I spent some time trying to make a minimal repro of the repro in the bug
    with no success, so there is no test.
    
    Fixed: 436887349
    Change-Id: I788051d9384f58b1a5c4e50fbfd13f61fe5bf76d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6832847
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1500324}

--- third_party/blink/renderer/core/html/forms/slider_thumb_element.cc
+++ third_party/blink/renderer/core/html/forms/slider_thumb_element.cc
@@ -449,6 +449,7 @@ void SliderContainerElement::UpdateTouchEventHandlerRegistry() {
 }
 
 void SliderContainerElement::DidMoveToNewDocument(Document& old_document) {
+  has_touch_event_handler_ = false;
   UpdateTouchEventHandlerRegistry();
   HTMLElement::DidMoveToNewDocument(old_document);
 }
