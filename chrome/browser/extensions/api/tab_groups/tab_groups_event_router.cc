54d01646aa1774d364e689f37227a68073620add
Antonio Gomes <tonikitoo@igalia.com>
2025-08-12 03:37:08 -0700

    Do not operate over Browser* in ExtensionTabUtil::BrowserSupportsTabs()
    Instead, use BrowserWindowInterface.
    
    R=neis@chromium.org
    
    Bug: 427359111
    Change-Id: I11ae1e2c7bcae4186cca0b8f750e4a68109be526
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6836680
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1500057}

--- chrome/browser/extensions/api/tab_groups/tab_groups_event_router.cc
+++ chrome/browser/extensions/api/tab_groups/tab_groups_event_router.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/extensions/extension_tab_util.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/tabs/tab_group_model.h"
 #include "components/tab_groups/tab_group_color.h"
@@ -64,8 +63,7 @@ void TabGroupsEventRouter::OnTabGroupChanged(const TabGroupChange& change) {
 
 bool TabGroupsEventRouter::ShouldTrackBrowser(BrowserWindowInterface* browser) {
   return profile_ == browser->GetProfile() &&
-         ExtensionTabUtil::BrowserSupportsTabs(
-             browser->GetBrowserForMigrationOnly());
+         ExtensionTabUtil::BrowserSupportsTabs(browser);
 }
 
 void TabGroupsEventRouter::DispatchGroupCreated(tab_groups::TabGroupId group) {
