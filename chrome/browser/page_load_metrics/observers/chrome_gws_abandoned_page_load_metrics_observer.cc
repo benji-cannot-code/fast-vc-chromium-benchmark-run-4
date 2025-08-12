7bca0316dcf3c7e5eb05d9924feeeff861af1edf
Keita Suzuki <suzukikeita@chromium.org>
2025-08-12 00:06:52 -0700

    Remove suffixes related to RTT from AbandonedPLMO
    This commit removes the RTT related suffixes that were added to GWS PLMO
    metrics.
    
    OBSOLETE_HISTOGRAMS=Removed {RTTBucketSuffix} from GWS PLMO metrics as they are not used.
    
    Bug: 436345871
    Change-Id: I294ab8768f0261e4b4c89ba031c8560d4103644c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6819851
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Commit-Queue: Keita Suzuki <suzukikeita@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1499995}

--- chrome/browser/page_load_metrics/observers/chrome_gws_abandoned_page_load_metrics_observer.cc
+++ chrome/browser/page_load_metrics/observers/chrome_gws_abandoned_page_load_metrics_observer.cc
@@ -16,25 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace internal {
 
-const char kSuffixResponseFromCache[] = ".ResponseFromCache";
-const char kSuffixRTTBelow200[] = ".RTTBelow200";
-const char kSuffixRTT200to450[] = ".RTT200To450";
-const char kSuffixRTTAbove450[] = ".RTTAbove450";
 const char kIncognito[] = ".Incognito";
 
 }  // namespace internal
-
-const char* ChromeGWSAbandonedPageLoadMetricsObserver::GetSuffixForRTT(
-    std::optional<base::TimeDelta> rtt) {
-  if (rtt.value().InMilliseconds() < 200) {
-    return internal::kSuffixRTTBelow200;
-  }
-  if (rtt.value().InMilliseconds() <= 450) {
-    return internal::kSuffixRTT200to450;
-  }
-
-  return internal::kSuffixRTTAbove450;
-}
 
 ChromeGWSAbandonedPageLoadMetricsObserver::
     ChromeGWSAbandonedPageLoadMetricsObserver() = default;
@@ -53,25 +37,7 @@ ChromeGWSAbandonedPageLoadMetricsObserver::GetAdditionalSuffixes() const {
       suffixes.push_back(suffix + internal::kIncognito);
     }
   }
-  // Make sure each histogram logged will log a version without connection type,
-  // and a version with the connection type, to allow filtering if needed.
-  // TODO(https://crbug.com/347706997): Consider doing this for the WebView
-  // version as well.
-  std::vector<std::string> suffixes_with_rtt;
-  for (std::string& base_suffix : suffixes) {
-    suffixes_with_rtt.push_back(base_suffix);
-    if (IsResponseFromCache()) {
-      suffixes_with_rtt.push_back(base_suffix +
-                                  internal::kSuffixResponseFromCache);
-    } else {
-      std::optional<base::TimeDelta> rtt =
-          g_browser_process->network_quality_tracker()->GetHttpRTT();
-      if (rtt.has_value()) {
-        suffixes_with_rtt.push_back(base_suffix + GetSuffixForRTT(rtt));
-      }
-    }
-  }
-  return suffixes_with_rtt;
+  return suffixes;
 }
 
 void ChromeGWSAbandonedPageLoadMetricsObserver::AddSRPMetricsToUKMIfNeeded(
