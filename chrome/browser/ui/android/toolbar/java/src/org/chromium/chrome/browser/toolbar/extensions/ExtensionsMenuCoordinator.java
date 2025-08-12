b9cdc6af8abcae3736d6adeaeb751a2c70ee0f56
Ted Choc <tedchoc@chromium.org>
2025-08-11 21:41:34 -0700

    Convert extensions popup to use ListMenu capabilities.
    Now ExtensionsMenuCoordinator no longer manages/owns the
    popup and defers that to ListMenuButton/ListMenuHost.
    
    This CL adds a few bits of polish:
    1.) Sets a comfortable min width (320dp), and an arbitrary max
        width (400dp)
    2.) Aligns the popup under the puzzle piece instead of offset a bit
    3.) Changes the background to work in dark mode.
    4.) Allows dismissal of the popup when clicking outside of the popup
        (including on the puzzle piece itself).
    
    Bug: 427670411
    Change-Id: I8ff7fed5360ac86d57850aac2d2b84af53e283c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6828685
    Reviewed-by: Masa Fujita <massan@google.com>
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1499960}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/extensions/ExtensionsMenuCoordinator.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/extensions/ExtensionsMenuCoordinator.java
@@ -5,15 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.toolbar.extensions;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.res.ColorStateList;
 import android.view.LayoutInflater;
 import android.view.View;
 
-import androidx.annotation.DimenRes;
 import androidx.annotation.VisibleForTesting;
-import androidx.appcompat.content.res.AppCompatResources;
 import androidx.core.widget.ImageViewCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -30,20 +27,22 @@ import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabLaunchType;
 import org.chromium.chrome.browser.tabmodel.TabCreator;
 import org.chromium.chrome.browser.theme.ThemeColorProvider;
+import org.chromium.chrome.browser.toolbar.MenuBuilderHelper;
 import org.chromium.chrome.browser.ui.extensions.ExtensionActionsBridge;
 import org.chromium.chrome.browser.ui.extensions.R;
 import org.chromium.chrome.browser.ui.theme.BrandedColorScheme;
 import org.chromium.components.embedder_support.util.UrlConstants;
 import org.chromium.content_public.browser.LoadUrlParams;
 import org.chromium.ui.base.PageTransition;
+import org.chromium.ui.listmenu.ListMenu;
 import org.chromium.ui.listmenu.ListMenuButton;
+import org.chromium.ui.listmenu.ListMenuDelegate;
 import org.chromium.ui.modelutil.LayoutViewBuilder;
 import org.chromium.ui.modelutil.MVCListAdapter.ModelList;
 import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.ui.modelutil.PropertyModelChangeProcessor;
 import org.chromium.ui.modelutil.SimpleRecyclerViewAdapter;
-import org.chromium.ui.widget.AnchoredPopupWindow;
-import org.chromium.ui.widget.ViewRectProvider;
+import org.chromium.ui.widget.RectProvider;
 
 /**
  * Coordinator for the extensions menu, accessed from the puzzle icon in the toolbar. This class is
@@ -65,12 +64,10 @@ public class ExtensionsMenuCoordinator implements Destroyable {
     private final ThemeColorProvider.TintObserver mTintObserver = this::onTintChanged;
     private final Callback<Profile> mProfileUpdatedCallback = this::onProfileUpdated;
 
-    @Nullable private AnchoredPopupWindow mMenuWindow;
     @Nullable private Profile mProfile;
     @Nullable @VisibleForTesting ExtensionsMenuMediator mMediator;
 
-    private boolean mShouldShowMenu;
-    private boolean mExtensionDataReady;
+    private boolean mShouldShowMenuOnInit;
 
     /**
      * Constructor.
@@ -92,33 +89,14 @@ public class ExtensionsMenuCoordinator implements Destroyable {
             ObservableSupplier<Profile> profileSupplier,
             ObservableSupplier<Tab> currentTabSupplier,
             TabCreator tabCreator) {
-        this(
-                context,
-                extensionsMenuButton,
-                extensionsMenuTabSwitcherDivider,
-                themeColorProvider,
-                profileSupplier,
-                currentTabSupplier,
-                tabCreator,
-                null);
-    }
-
-    @VisibleForTesting
-    public ExtensionsMenuCoordinator(
-            Context context,
-            ListMenuButton extensionsMenuButton,
-            MaterialDivider extensionsMenuTabSwitcherDivider,
-            ThemeColorProvider themeColorProvider,
-            ObservableSupplier<Profile> profileSupplier,
-            ObservableSupplier<Tab> currentTabSupplier,
-            TabCreator tabCreator,
-            @Nullable AnchoredPopupWindow menuWindow) {
         mContext = context;
         mCurrentTabSupplier = currentTabSupplier;
         mTabCreator = tabCreator;
 
         mExtensionsMenuButton = extensionsMenuButton;
-        mExtensionsMenuButton.setOnClickListener(this::onClick);
+        mExtensionsMenuButton.setOnClickListener(view -> mShouldShowMenuOnInit = true);
+        mExtensionsMenuButton.setMenuMaxWidth(
+                context.getResources().getDimensionPixelSize(R.dimen.extension_menu_max_width));
 
         mExtensionsMenuTabSwitcherDivider = extensionsMenuTabSwitcherDivider;
 
@@ -139,7 +117,22 @@ public class ExtensionsMenuCoordinator implements Destroyable {
         mExtensionModels = new ModelList();
         setUpExtensionsRecyclerView(mContentView, mContext, mExtensionModels);
 
-        mMenuWindow = menuWindow;
+        ListMenu listMenu =
+                new ListMenu() {
+                    @Override
+                    public View getContentView() {
+                        return mContentView;
+                    }
+
+                    @Override
+                    public void addContentViewClickRunnable(Runnable runnable) {}
+
+                    @Override
+                    public int getMaxItemWidth() {
+                        assert false : "Max width item measurement not supported";
+                        return 0;
+                    }
+                };
 
         mMediator =
                 new ExtensionsMenuMediator(
@@ -147,8 +140,24 @@ public class ExtensionsMenuCoordinator implements Destroyable {
                         mCurrentTabSupplier,
                         mExtensionModels,
                         () -> {
-                            mExtensionDataReady = true;
-                            updateWindowVisibility();
+                            mExtensionsMenuButton.setDelegate(
+                                    new ListMenuDelegate() {
+                                        @Override
+                                        public RectProvider getRectProvider(
+                                                View listMenuHostingView) {
+                                            return MenuBuilderHelper.getRectProvider(
+                                                    mExtensionsMenuButton);
+                                        }
+
+                                        @Override
+                                        public ListMenu getListMenu() {
+                                            return listMenu;
+                                        }
+                                    });
+                            if (mShouldShowMenuOnInit) {
+                                mExtensionsMenuButton.showMenu();
+                                mShouldShowMenuOnInit = false;
+                            }
                         });
     }
 
@@ -172,43 +181,8 @@ public class ExtensionsMenuCoordinator implements Destroyable {
         mExtensionsMenuTabSwitcherDivider.setVisibility(visibility);
     }
 
-    void onClick(View view) {
-        showMenu();
-    }
-
-    /** Show the extensions menu (potentially async). */
-    @VisibleForTesting
-    void showMenu() {
-        mShouldShowMenu = true;
-        // The mediator has to wait for `onProfileUpdated` on creation. We don't change window
-        // visibility here directly because we want the menu to be shown only after the `Mediator`
-        // notifies us that the data is ready.
-        updateWindowVisibility();
-    }
-
-    private void closeMenu() {
-        mShouldShowMenu = false;
-        updateWindowVisibility();
-    }
-
-    private void updateWindowVisibility() {
-        if (mShouldShowMenu && mExtensionDataReady) {
-            if (mMenuWindow == null) {
-                View decorView = ((Activity) mContext).getWindow().getDecorView();
-                mMenuWindow =
-                        createPopupWindow(mContext, decorView, mExtensionsMenuButton, mContentView);
-            }
-
-            // We have to make sure that the extension data created in the mediator is ready
-            // before we can show the menu window.
-            mMenuWindow.show();
-        } else {
-            if (mMenuWindow != null) mMenuWindow.dismiss();
-        }
-    }
-
     private void openUrlFromMenu(String url) {
-        closeMenu();
+        mExtensionsMenuButton.dismiss();
 
         LoadUrlParams params = new LoadUrlParams(url, PageTransition.AUTO_TOPLEVEL);
 
@@ -233,13 +207,11 @@ public class ExtensionsMenuCoordinator implements Destroyable {
         mExtensionsMenuButton.setBackgroundResource(backgroundResource);
     }
 
-    private static int getDimensionPixelSize(Context context, @DimenRes int dimenId) {
-        return context.getResources().getDimensionPixelSize(dimenId);
-    }
-
     private PropertyModel createMenuPropertyModel() {
         return new PropertyModel.Builder(ExtensionsMenuProperties.ALL_KEYS)
-                .with(ExtensionsMenuProperties.CLOSE_CLICK_LISTENER, (view) -> closeMenu())
+                .with(
+                        ExtensionsMenuProperties.CLOSE_CLICK_LISTENER,
+                        (view) -> mExtensionsMenuButton.dismiss())
                 .with(
                         ExtensionsMenuProperties.DISCOVER_EXTENSIONS_CLICK_LISTENER,
                         (view) -> openUrlFromMenu(UrlConstants.CHROME_WEBSTORE_URL))
@@ -262,28 +234,6 @@ public class ExtensionsMenuCoordinator implements Destroyable {
 
         extensionRecyclerView.setAdapter(extensionsAdapter);
         extensionRecyclerView.setLayoutManager(new LinearLayoutManager(context));
-    }
-
-    private static AnchoredPopupWindow createPopupWindow(
-            Context context,
-            View decorView,
-            ListMenuButton extensionsMenuButton,
-            View contentView) {
-        ViewRectProvider anchoredViewRectProvider = new ViewRectProvider(extensionsMenuButton);
-        int toolbarHeight = extensionsMenuButton.getHeight();
-        int iconHeight =
-                getDimensionPixelSize(
-                        context, org.chromium.chrome.browser.toolbar.R.dimen.toolbar_icon_height);
-        int paddingVertical = (toolbarHeight - iconHeight) / 2;
-        anchoredViewRectProvider.setInsetPx(0, paddingVertical, 0, paddingVertical);
-        anchoredViewRectProvider.setIncludePadding(true);
-
-        return new AnchoredPopupWindow(
-                context,
-                decorView,
-                AppCompatResources.getDrawable(context, R.drawable.extensions_menu_bg_tinted),
-                contentView,
-                anchoredViewRectProvider);
     }
 
     @Override
