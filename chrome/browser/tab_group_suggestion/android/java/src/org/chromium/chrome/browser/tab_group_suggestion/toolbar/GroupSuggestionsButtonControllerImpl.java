b68cf191f83387d7e58eac242e9653537b9c5c92
Fiaz Muhammad <mfiaz@google.com>
2025-08-12 07:55:18 -0700

    [Suggest] Handle destroying callback on CPA destruction
    - Currently, the CPA may not handle the destruction of
    	JniOnceCallbacks on chrome closing.
    - This change destroys the last callback on destruction of the
    	CPA Controller.
    
    Bug: 428824862
    Change-Id: I6c436f44b7a0cda20d6ee8e725cc2116443d8078
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6838996
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Auto-Submit: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1500142}

--- chrome/browser/tab_group_suggestion/android/java/src/org/chromium/chrome/browser/tab_group_suggestion/toolbar/GroupSuggestionsButtonControllerImpl.java
+++ chrome/browser/tab_group_suggestion/android/java/src/org/chromium/chrome/browser/tab_group_suggestion/toolbar/GroupSuggestionsButtonControllerImpl.java
@@ -161,4 +161,10 @@ public class GroupSuggestionsButtonControllerImpl implements GroupSuggestionsBut
 
         return null;
     }
+
+    @Override
+    public void destroy() {
+        if (mCachedSuggestions == null) return;
+        mCachedSuggestions.userResponseMetadataCallback.destroy();
+    }
 }
--- components/visited_url_ranking/internal/url_grouping/android/java/src/org/chromium/components/visited_url_ranking/url_grouping/GroupSuggestionsServiceImpl.java
+++ components/visited_url_ranking/internal/url_grouping/android/java/src/org/chromium/components/visited_url_ranking/url_grouping/GroupSuggestionsServiceImpl.java
@@ -9,6 +9,7 @@ import org.jni_zero.CalledByNative;
 import org.jni_zero.JNINamespace;
 import org.jni_zero.NativeMethods;
 
+import org.chromium.base.JniOnceCallback;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.url.GURL;
@@ -90,7 +91,7 @@ public class GroupSuggestionsServiceImpl implements GroupSuggestionsService {
     public @Nullable CachedSuggestions getCachedSuggestions(int windowId) {
         if (mNativePtr == 0) {
             // Return CachedSuggestions with an empty list if the native service isn't initialized.
-            return new CachedSuggestions(null, (res) -> {});
+            return new CachedSuggestions(null, emptyJniCallback());
         }
         return GroupSuggestionsServiceImplJni.get().getCachedSuggestions(mNativePtr, windowId);
     }
@@ -98,6 +99,21 @@ public class GroupSuggestionsServiceImpl implements GroupSuggestionsService {
     @CalledByNative
     private void clearNativePtr() {
         mNativePtr = 0;
+    }
+
+    private static JniOnceCallback<UserResponseMetadata> emptyJniCallback() {
+        return new JniOnceCallback<>() {
+            @Override
+            public void destroy() {}
+
+            @Override
+            public void onResult(UserResponseMetadata result) {}
+
+            @Override
+            public Runnable bind(UserResponseMetadata result) {
+                return () -> {};
+            }
+        };
     }
 
     @NativeMethods
