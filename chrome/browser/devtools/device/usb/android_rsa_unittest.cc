f19ef29579d5fbaddd102da5cc79779218001518
Elly <ellyjones@chromium.org>
2025-08-12 00:16:02 -0700

    devtools: drop stray rsa_private_key.h include
    Bug: 406190025
    Change-Id: I41bbef8e8db20adc243c65dbc215efb8d6f71968
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6838257
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Auto-Submit: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1499999}

--- chrome/browser/devtools/device/usb/android_rsa_unittest.cc
+++ chrome/browser/devtools/device/usb/android_rsa_unittest.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_view_util.h"
 #include "crypto/hash.h"
 #include "crypto/keypair.h"
-#include "crypto/rsa_private_key.h"
 #include "crypto/sign.h"
 #include "crypto/test_support.h"
 #include "testing/gtest/include/gtest/gtest.h"
