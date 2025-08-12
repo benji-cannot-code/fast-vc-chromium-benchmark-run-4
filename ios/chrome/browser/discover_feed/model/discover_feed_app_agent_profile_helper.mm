db39c3b25cc18101115f9c2e98807193e62befe9
Boris Sazonov <bsazonov@chromium.org>
2025-08-12 10:04:46 -0700

    [Signin][iOS] Replace HasPrimaryIdentity usage in ios/c/b/discover_feed
    Existing usages of AuthenticationService::HasPrimaryIdentity should be
    replaced with IdentityManager::HasPrimaryAccount.
    
    Bug: 424237387
    Change-Id: I318205eb3f9cff4bb60a2926e0479a39e48279da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6838481
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Auto-Submit: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1500238}

--- ios/chrome/browser/discover_feed/model/discover_feed_app_agent_profile_helper.mm
+++ ios/chrome/browser/discover_feed/model/discover_feed_app_agent_profile_helper.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/search_engines/template_url_prepopulate_data.h"
 #import "components/search_engines/template_url_service.h"
 #import "components/signin/public/base/consent_level.h"
+#import "components/signin/public/identity_manager/identity_manager.h"
 #import "ios/chrome/app/profile/profile_init_stage.h"
 #import "ios/chrome/app/profile/profile_state.h"
 #import "ios/chrome/app/profile/profile_state_observer.h"
@@ -23,8 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/search_engines/model/template_url_service_factory.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
-#import "ios/chrome/browser/signin/model/authentication_service.h"
-#import "ios/chrome/browser/signin/model/authentication_service_factory.h"
+#import "ios/chrome/browser/signin/model/identity_manager_factory.h"
 
 namespace {
 
@@ -104,10 +104,10 @@ bool IsGoogleDefaultSearchEngine(ProfileIOS* profile) {
   ProfileIOS* profile = _profileState.profile;
   CHECK(profile);
 
-  AuthenticationService* authService =
-      AuthenticationServiceFactory::GetForProfile(profile);
+  signin::IdentityManager* identityManager =
+      IdentityManagerFactory::GetForProfile(profile);
   const bool isUserSignedIn =
-      authService->HasPrimaryIdentity(signin::ConsentLevel::kSignin);
+      identityManager->HasPrimaryAccount(signin::ConsentLevel::kSignin);
 
   if (isUserSignedIn) {
     if (IsWebChannelsEnabled() && IsDiscoverFeedServiceCreatedEarly()) {
