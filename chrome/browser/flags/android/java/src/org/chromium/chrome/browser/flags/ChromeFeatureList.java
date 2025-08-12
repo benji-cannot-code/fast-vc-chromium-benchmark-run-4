5276cd2182fea30fc39c9af17ef6098d79789658
Wenyu Fu <wenyufu@chromium.org>
2025-08-11 17:46:57 -0700

    [IncognitoAware] Create cached boolean param
    Add a cached param to force a light mode the theme overlay for manual testing.
    
    Bug: 416222384
    Change-Id: I20343a069337deb93a8750b3eec247b89b6bb7d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6832948
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Auto-Submit: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1499905}

--- chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
@@ -754,6 +754,8 @@ public abstract class ChromeFeatureList {
             newCachedFlag(ANDROID_TAB_SKIP_SAVE_TABS_TASK_KILLSWITCH, true, true);
     public static final CachedFlag sAndroidThemeModule =
             newCachedFlag(ANDROID_THEME_MODULE, false, /* defaultValueInTests= */ true);
+    public static final CachedFlag sAndroidThemeResourceProvider =
+            newCachedFlag(ANDROID_THEME_RESOURCE_PROVIDER, false, /* defaultValueInTests= */ false);
     public static final CachedFlag sAndroidUseAdminsForEnterpriseInfo =
             newCachedFlag(
                     ANDROID_USE_ADMINS_FOR_ENTERPRISE_INFO,
@@ -1091,6 +1093,7 @@ public abstract class ChromeFeatureList {
                     sAndroidTabGroupsColorUpdateGm3,
                     sAndroidTabSkipSaveTabsKillswitch,
                     sAndroidThemeModule,
+                    sAndroidThemeResourceProvider,
                     sAndroidUseAdminsForEnterpriseInfo,
                     sAndroidWebAppLaunchHandler,
                     sAndroidWindowPopupLargeScreen,
@@ -1337,6 +1340,9 @@ public abstract class ChromeFeatureList {
     public static final BooleanCachedFeatureParam sAndroidThemeModuleForceDependencies =
             newBooleanCachedFeatureParam(
                     ANDROID_THEME_MODULE, "force_theme_module_dependencies", false);
+    public static final BooleanCachedFeatureParam sAndroidThemeResourceProviderForceLight =
+            newBooleanCachedFeatureParam(
+                    ANDROID_THEME_RESOURCE_PROVIDER, "force_light_theme", false);
     public static final BooleanCachedFeatureParam sCctAdaptiveButtonEnableOpenInBrowser =
             newBooleanCachedFeatureParam(CCT_ADAPTIVE_BUTTON, "open_in_browser", false);
     public static final BooleanCachedFeatureParam sCctAdaptiveButtonEnableVoice =
@@ -1740,6 +1746,7 @@ public abstract class ChromeFeatureList {
                     sAndroidComposeplateSkipLocaleCheck,
                     sAndroidComposeplateHideIncognitoButton,
                     sAndroidThemeModuleForceDependencies,
+                    sAndroidThemeResourceProviderForceLight,
                     sBackgroundThreadPoolFieldTrialConfig,
                     sBatchTabRestoreBatchSize,
                     sCctAdaptiveButtonContextualOnly,
