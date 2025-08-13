06e8c0867587ea91f918bfd1bf6807345223ca27
Ali Hijazi <ahijazi@chromium.org>
2025-08-13 01:24:15 -0700

    Fix unsafe buffer usage in history_url_provider_unittest.cc
    Replaced C-style arrays with std::array.
    
    Bug: 435317390
    Bug: 436195625
    Fixed: 436195625
    Change-Id: Ia366b3880feaf05c861d021ca7c353f0b2ca76f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6842539
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Ali Hijazi <ahijazi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1500653}

--- components/omnibox/browser/history_url_provider_unittest.cc
+++ components/omnibox/browser/history_url_provider_unittest.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "components/omnibox/browser/history_url_provider.h"
 
 #include <stddef.h>
@@ -1074,7 +1069,7 @@ TEST_F(HistoryURLProviderTest, SuggestExactInput) {
     // Expected Outputs:
     const char* contents;
     // Offsets of the ACMatchClassifications, terminated by npos.
-    size_t offsets[3];
+    std::array<size_t, 3> offsets;
     // The index of the ACMatchClassification that should have the MATCH bit
     // set, npos if no ACMatchClassification should have the MATCH bit set.
     size_t match_classification_index;
@@ -1204,42 +1199,42 @@ TEST_F(HistoryURLProviderTest, HUPScoringExperiment) {
       int control_relevance;
       int experiment_relevance;
     };
-    ExpectedMatch matches[kProviderMaxMatches];
+    std::array<ExpectedMatch, kProviderMaxMatches> matches;
   };
   auto test_cases = std::to_array<TestCase>({
       // Max score 2000 -> no demotion.
       {"7.com/1",
        max_2000_no_time_decay,
-       {{"7.com/1a", 1413, 1413}, {nullptr, 0, 0}, {nullptr, 0, 0}}},
+       {{{"7.com/1a", 1413, 1413}, {nullptr, 0, 0}, {nullptr, 0, 0}}}},
 
       // Limit score to 1250/1000 and make sure that the top match is unchanged.
       {"7.com/1",
        max_1250_no_time_decay,
-       {{"7.com/1a", 1413, 1413}, {nullptr, 0, 0}, {nullptr, 0, 0}}},
+       {{{"7.com/1a", 1413, 1413}, {nullptr, 0, 0}, {nullptr, 0, 0}}}},
       {"7.com/2",
        max_1250_no_time_decay,
-       {{"7.com/2a", 1413, 1413}, {"7.com/2b", 1412, 1250}, {nullptr, 0, 0}}},
+       {{{"7.com/2a", 1413, 1413}, {"7.com/2b", 1412, 1250}, {nullptr, 0, 0}}}},
       {"7.com/4",
        max_1000_no_time_decay,
-       {{"7.com/4", 1203, 1203},
-        {"7.com/4a", 1202, 1000},
-        {"7.com/4b", 1201, 999}}},
+       {{{"7.com/4", 1203, 1203},
+         {"7.com/4a", 1202, 1000},
+         {"7.com/4b", 1201, 999}}}},
 
       // Max relevance cap is 1400 and half-life is 16 days.
       {"7.com/1",
        max_1100_with_time_decay_and_max_cap,
-       {{"7.com/1a", 1413, 1413}, {nullptr, 0, 0}, {nullptr, 0, 0}}},
+       {{{"7.com/1a", 1413, 1413}, {nullptr, 0, 0}, {nullptr, 0, 0}}}},
       {"7.com/4",
        max_1100_with_time_decay_and_max_cap,
-       {{"7.com/4", 1203, 1203},
-        {"7.com/4a", 1202, 200},
-        {"7.com/4b", 1201, 100}}},
+       {{{"7.com/4", 1203, 1203},
+         {"7.com/4a", 1202, 200},
+         {"7.com/4b", 1201, 100}}}},
 
       // Max relevance cap is 1400 and half-life is 16 days for both
       // visit/typed.
       {"7.com/5",
        max_1100_visit_typed_decays,
-       {{"7.com/5", 1203, 1203}, {"7.com/5a", 1202, 50}, {nullptr, 0, 0}}},
+       {{{"7.com/5", 1203, 1203}, {"7.com/5a", 1202, 50}, {nullptr, 0, 0}}}},
   });
   for (size_t i = 0; i < std::size(test_cases); ++i) {
     SCOPED_TRACE(test_cases[i].input);
