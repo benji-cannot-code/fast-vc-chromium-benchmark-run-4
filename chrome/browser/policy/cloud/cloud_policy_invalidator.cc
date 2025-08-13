6daac8719b3a6f98554c749362024161d8044190
Artem Sumaneev <asumaneev@google.com>
2025-08-13 05:06:30 -0700

    [invalidation] Rewrite policy invalidator tests with task env
    Instead of mixing testing clock, task runner and task environment, use
    just single test task environment with mock time.
    
    Now, to check that invalidation was handled and policy refresh is
    scheduled, we count mock calls of FetchPolicy and check the number of
    such calls after triggering invalidation task.
    
    Bug: b:341377023
    Change-Id: I6334550cf33c2d4aa9cc721abe1d04a55793e83d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6829327
    Reviewed-by: Igor <igorcov@chromium.org>
    Commit-Queue: Artem Sumaneev <asumaneev@google.com>
    Cr-Commit-Position: refs/heads/main@{#1500740}

--- chrome/browser/policy/cloud/cloud_policy_invalidator.cc
+++ chrome/browser/policy/cloud/cloud_policy_invalidator.cc
@@ -132,7 +132,7 @@ CloudPolicyInvalidator::PolicyInvalidationHandler::PolicyInvalidationHandler(
     PolicyInvalidationScope scope,
     int64_t highest_handled_invalidation_version,
     CloudPolicyCore* core,
-    base::Clock* clock,
+    const base::Clock* clock,
     scoped_refptr<base::SequencedTaskRunner> task_runner)
     : scope_(scope),
       core_(core),
@@ -161,7 +161,7 @@ CloudPolicyInvalidator::CloudPolicyInvalidator(
     invalidation::InvalidationListener* invalidation_listener,
     CloudPolicyCore* core,
     const scoped_refptr<base::SequencedTaskRunner>& task_runner,
-    base::Clock* clock,
+    const base::Clock* clock,
     int64_t highest_handled_invalidation_version)
     : CloudPolicyInvalidator(scope,
                              invalidation_listener,
@@ -176,7 +176,7 @@ CloudPolicyInvalidator::CloudPolicyInvalidator(
     invalidation::InvalidationListener* invalidation_listener,
     CloudPolicyCore* core,
     const scoped_refptr<base::SequencedTaskRunner>& task_runner,
-    base::Clock* clock,
+    const base::Clock* clock,
     int64_t highest_handled_invalidation_version,
     const std::string& device_local_account_id)
     : policy_invalidation_handler_(scope,
