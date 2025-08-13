a9355a6f2639fe9b21acf7902ca8cea4fa021bcc
Alexander Cooper <alcooper@chromium.org>
2025-08-13 12:49:43 -0700

    Refine OpenXrExtensionHandlerFactory interface
    The OpenXrExtensionHandlerFactory interface defined both a method that
    was called to determine support at the time the XrInstance was created,
    and then a method that was called to determine support whenever we were
    attempting to check if features could be supported or to create the
    factories. This really just resulted in a somewhat messy state where
    essentially two functions were responsible for determining enablement,
    but the latter didn't *actually* have to happen later. Further,
    `ProcessSystemProperties` (now `CheckAndUpdateEnabledFeatureState`), is
    run any time a new XrInstance is created, so this ensures that the
    feature state is always the most up-to-date that we could create.
    
    This change thus refines the OpenXrExtensionHandlerFactory in the
    following ways:
    
    * Renames `ProcessSystemProperties` to `CheckAndUpdateEnabledState`,
      also updates the helper set methods and bools used by the method.
    * Updates the default `CheckAndUpdateEnabledState` to match the
      behavior of the default `IsEnabled` method.
    * Converts `IsEnabled` to a non-virtual method that simply returns the
      boolean that is required to be set by `CheckAndUpdateEnabledState`.
    * Removes the `ExtensionEnumeration` from `IsEnabled`, including
      cleaning up callsites that only passed the value to pass to
      `IsEnabled`
    * Updates existing `IsEnabled` overrides to instead override and set
      the enabled state in `CheckAndUpdateEnabledState`.
    * This unlocked further refinement of `GetSupportedFeatures` described
      below.
    
    For some of the more complex `GetSupportedFeatures` code, it was
    determined that those checks should *also* be run during
    `CheckAndUpdateEnabledState`. As such, the classes with more complex
    `GetSupportedFeatures` code (the SceneUnderstandingManagers), were
    updated to compute and store a list of supported features in
    `CheckAndUpdateEnabledState`. This change removed the need for
    `GetSupportedFeatures` to take an extension enumeration and thus makes
    that call cheaper and simplifies its callers as well.
    
    The change to `CheckAndUpdateEnabledState` needing to be overridden by
    both of the SceneUnderstandingManagerFactory's children removed any
    benefit that it had, and thus it was also removed.
    
    While doing this cleanup, the `CreateAnchorManager` factory method was
    found to still be around but no longer needed due to the consolidation
    of the anchor manager into the SceneUnderstanding framework, so it was
    also removed.
    
    The final state after this change is that OpenXrExtensionHandlerFactory
    classes are updated during XrInstance creation to determine what level
    of support they have for this particular XrInstance/XrSystem, and thus
    their `IsEnabled` and `GetSupportedFeature` checks should now be cheaper
    and hopefully more work can be reused. These factories now only have a
    single method to override to update their enabled state making it easier
    to reason about.
    
    Bug: 394772465
    Change-Id: Ie79e70be94435aedf3353677db423521a606201f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6832951
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1500967}

--- device/vr/openxr/openxr_extension_handler_factory.h
+++ device/vr/openxr/openxr_extension_handler_factory.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace device {
 
-class OpenXrAnchorManager;
 class OpenXrDepthSensor;
 class OpenXrExtensionEnumeration;
 class OpenXrExtensionHelper;
@@ -62,28 +61,25 @@ class OpenXrExtensionHandlerFactory {
   // populate this list, unlike the list of extensions which are expected to
   // essentially be static, as such we do not want to enforce any data types
   // (or const& values) on this function's implementations.
-  virtual std::set<device::mojom::XRSessionFeature> GetSupportedFeatures(
-      const OpenXrExtensionEnumeration* extension_enum) const = 0;
+  virtual std::set<device::mojom::XRSessionFeature> GetSupportedFeatures()
+      const = 0;
 
   // Returns whether or not this factory is enabled (e.g. can return at least
-  // one "ExtensionHandler" type). By default checks if all RequestedExtensions
-  // are supported.
-  virtual bool IsEnabled(
-      const OpenXrExtensionEnumeration* extension_enum) const;
+  // one "ExtensionHandler" type).
+  bool IsEnabled() const;
 
-  // All currently supported extensions that want to query system properties
-  // have their own struct defined as a forced leaf-node, so all extensions that
-  // want to query xrGetSystemProperties must make the call themselves. This
-  // will be called during initialization.
-  virtual void ProcessSystemProperties(
+  // Checks if the factory should be enabled, and updates its internal state
+  // if it is. By default this checks if all RequestedExtensions are supported.
+  // However, some extensions have additional logic that they need to run, such
+  // as querying xrGetSystemProperties to check for support. Note that the
+  // `xrGetSystemProperties` calls cannot be combined as many of these
+  // extensions require appending their own struct to the default system
+  // properties and that the `next` pointer on their own struct must be null.
+  // This will be called during initialization.
+  virtual void CheckAndUpdateEnabledState(
       const OpenXrExtensionEnumeration* extension_enum,
       XrInstance instance,
       XrSystemId system);
-
-  virtual std::unique_ptr<OpenXrAnchorManager> CreateAnchorManager(
-      const OpenXrExtensionHelper& extension_helper,
-      XrSession session,
-      XrSpace mojo_space) const;
 
   virtual std::unique_ptr<OpenXrDepthSensor> CreateDepthSensor(
       const OpenXrExtensionHelper& extension_helper,
@@ -107,21 +103,19 @@ class OpenXrExtensionHandlerFactory {
                                   XrSpace mojo_space) const;
 
   virtual std::unique_ptr<OpenXrStageBoundsProvider> CreateStageBoundsProvider(
-      const OpenXrExtensionHelper& extension_helper,
       XrSession session) const;
 
   virtual std::unique_ptr<OpenXrUnboundedSpaceProvider>
-  CreateUnboundedSpaceProvider(
-      const OpenXrExtensionHelper& extension_helper) const;
+  CreateUnboundedSpaceProvider() const;
 
  protected:
   bool AreAllRequestedExtensionsSupported(
       const OpenXrExtensionEnumeration* extension_enum) const;
 
-  void SetSystemPropertiesSupport(bool supported);
+  void SetEnabled(bool enabled);
 
  private:
-  bool supported_by_system_properties_ = false;
+  bool enabled_ = false;
 };
 }  // namespace device
 
