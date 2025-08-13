72673ce4adc75d588dbbaedb9b960e33232dc7c9
chrstne <chrstne@google.com>
2025-08-13 08:53:58 -0700

    Actuation view resets task icon text
    Actuation/pause Screencast:
    https://screencast.googleplex.com/cast/NTQ4OTg0NjgzNDQyOTk1MnwxMWFlZGE4Zi0zNQ
    Conversation/pause Screencast:
    https://screencast.googleplex.com/cast/NDY3MDMyNDQyNjY3MDA4MHw3MmUyNjVlZC00Mw
    No floaty/pause Screencast: https://screencast.googleplex.com/cast/NTA3NDY2Mjk4MjY4MDU3NnxiM2ZjNDIyYy1kMw
    Actuation/complete Screencast:
    https://screencast.googleplex.com/cast/NjE0ODM4NTQ0NTc3MzMxMnw0MDA4MzljMi00NQ
    No floaty/conversation/complete Screencast: https://screencast.googleplex.com/cast/NTEzMzAzNzMyNjA0MTA4OHwwN2YyMWU3MC1lYQ
    
    Bug: 436255383
    Change-Id: I8718fe34089b2fa7bae597c49f42e77426acbc39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6828448
    Commit-Queue: Christine Ying <chrstne@google.com>
    Reviewed-by: Maggie Jennings <mjenn@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1500832}

--- chrome/browser/actor/ui/mocks/mock_actor_ui_state_manager.h
+++ chrome/browser/actor/ui/mocks/mock_actor_ui_state_manager.h
@@ -37,7 +37,7 @@ class MockActorUiStateManager : public ActorUiStateManagerInterface {
               (tabs::TabInterface * tab),
               (override));
   MOCK_METHOD(void, MaybeShowToast, (BrowserWindowInterface * bwi), (override));
-  MOCK_METHOD(UiState, GetUiState, (), (const override));
+  MOCK_METHOD(TaskIconUiState, GetTaskIconUiState, (), (const override));
 
 #if BUILDFLAG(ENABLE_GLIC)
   MOCK_METHOD(void,
@@ -46,8 +46,8 @@ class MockActorUiStateManager : public ActorUiStateManagerInterface {
                glic::mojom::CurrentView current_view),
               (override));
   MOCK_METHOD(base::CallbackListSubscription,
-              RegisterFloatyTaskStateChange,
-              (FloatyTaskStateChangeCallback callback),
+              RegisterTaskIconStateChange,
+              (TaskIconStateChangeCallback callback),
               (override));
 
 #endif
