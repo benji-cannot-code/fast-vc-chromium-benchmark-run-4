bf7c1100a6a86fc6f082fdfd72ebc7539fa215f0
Thomas Lukaszewicz <tluk@chromium.org>
2025-08-13 11:42:15 -0700

    [bedrock] Ensure window placement is saved when Browser is Closed
    Under the CLIENT_OWNS_WIDGET ownership model, the signal to save
    window placement is emitted from the views::Widget destructor.
    
    However the window placement checks require state on the
    BrowserFrame (a views::Widget subclass). By the time this signal is
    received the relevant state on BrowserFrame has been destroyed.
    
    This CL ensures window placement is saved by saving window placement
    in the BrowserFrame destructor.
    
    This CL also checks for widget_closing_ before attempting to save
    placement in Widget activation changes to avoid UAF BrowserFrame
    access after it's destructor has run.
    
    Bug: 413168662
    Change-Id: I6697d1324625c9456377ea2e26299ea8f04a5fad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6843076
    Reviewed-by: Fred Shih <ffred@chromium.org>
    Commit-Queue: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1500922}

--- ui/views/widget/widget.h
+++ ui/views/widget/widget.h
@@ -933,6 +933,9 @@ class VIEWS_EXPORT Widget : public internal::NativeWidgetDelegate,
   // Returns the reason the widget was closed, if it was specified.
   ClosedReason closed_reason() const { return closed_reason_; }
 
+  // True if the Widget is being destroyed and running its destructor.
+  bool is_destroying() const { return is_destroying_; }
+
   // Shows the widget. The widget is activated if during initialization the
   // can_activate flag in the InitParams structure is set to true.
   void Show();
@@ -1488,6 +1491,11 @@ class VIEWS_EXPORT Widget : public internal::NativeWidgetDelegate,
   // Set the native theme from which this widget gets color from.
   void SetNativeTheme(ui::NativeTheme* native_theme);
 
+  // Invokes SaveWindowPlacement() if the native widget has been initialized.
+  // This is called at times when the native widget may not have been
+  // initialized.
+  void SaveWindowPlacementIfNeeded();
+
   // The following methods are used by the property access system described in
   // the comments on views::View. They follow the required naming convention in
   // order to allow them to be visible via the metadata.
@@ -1539,11 +1547,6 @@ class VIEWS_EXPORT Widget : public internal::NativeWidgetDelegate,
   // Persists the window's restored position and "show" state using the
   // window delegate.
   void SaveWindowPlacement();
-
-  // Invokes SaveWindowPlacement() if the native widget has been initialized.
-  // This is called at times when the native widget may not have been
-  // initialized.
-  void SaveWindowPlacementIfNeeded();
 
   // Sizes and positions the window just after it is created.
   void SetInitialBounds(const gfx::Rect& bounds);
@@ -1679,6 +1682,9 @@ class VIEWS_EXPORT Widget : public internal::NativeWidgetDelegate,
 
   // Set to true if the widget is in the process of closing.
   bool widget_closed_ = false;
+
+  // Set to true if the widget is in the process of being destroyed.
+  bool is_destroying_ = false;
 
   // Set to true after OnWidgetDestroyed called.
   bool native_widget_destroyed_ = false;
