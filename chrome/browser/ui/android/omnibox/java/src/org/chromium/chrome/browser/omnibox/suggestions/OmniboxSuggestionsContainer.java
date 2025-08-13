9dd35b42542db5b73af341d76379c5c90a9c2653
Tomasz Wiszkowski <ender@google.com>
2025-08-13 16:02:20 -0700

    Swallow Omnibox Container touch events.
    This change ensures that tapping on the suggestions list does not
    dismiss the active Omnibox session.
    
    Bug: 436888404
    Screen-Before: https://screenshot.googleplex.com/C32aidtpbx3L9RY.png
    Screen-After: https://screenshot.googleplex.com/4eWeBmkafEZmpGj.png
    Change-Id: I282e856eb8a13d1d13702707f23e4430bd089704
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6846586
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1501089}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/OmniboxSuggestionsContainer.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/OmniboxSuggestionsContainer.java
@@ -5,10 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.omnibox.suggestions;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.util.AttributeSet;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewOutlineProvider;
@@ -93,6 +95,18 @@ public class OmniboxSuggestionsContainer extends FrameLayout {
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         return mDropdown.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    @SuppressLint("ClickableViewAccessibility")
+    public boolean onTouchEvent(MotionEvent event) {
+        // Swallow all touch events, especially if these were not consumed by the Dropdown.
+        // This ensures that touching the blank areas of the container does not dismiss the
+        // Omnibox.
+        // Making the container `clickable=true` achieves similar goal, but this consumes all
+        // activators, including keyboard <Enter> key.
+        super.onTouchEvent(event);
+        return true;
     }
 
     private void maybeUpdateLayoutParams(int topMargin) {
