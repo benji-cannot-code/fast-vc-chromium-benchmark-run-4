74d4207ac05224aa18cdc1968c05ea3c92c114eb
Jun Ishiguro <junis@google.com>
2025-08-13 00:05:25 -0700

    ash-refactor: Pass PrefService to DeviceDisablingManager
    This change refactors DeviceDisablingManager to take PrefService* in its
    constructor instead of directly accessing g_browser_process.
    
    This is part of a larger effort to remove dependencies on
    g_browser_process from Ash-specific code.
    
    The PrefService is stored as a const raw_ref<PrefService> local_state_.
    The lifetime of local_state (owned by BrowserProcessImpl) is guaranteed
    to outlive the DeviceDisablingManager (owned by
    BrowserProcessPlatformPartAsh), as BrowserProcessPlatformPartAsh is
    destroyed before local_state during shutdown.
    
    No functional change intended.
    
    Bug: b:404133899
    Change-Id: I5c9ab2bffc71e64831a00891f70aeae4a927f77d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6844956
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Reviewed-by: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1500632}

--- chrome/browser/ash/system/device_disabling_manager.h
+++ chrome/browser/ash/system/device_disabling_manager.h
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/policy/restriction_schedule/device_restriction_schedule_controller.h"
 #include "chromeos/ash/components/settings/cros_settings.h"
 
+class PrefService;
+
 namespace policy {
 class BrowserPolicyConnectorAsh;
 }
@@ -83,8 +85,10 @@ class DeviceDisablingManager
     virtual void ShowDeviceDisabledScreen() = 0;
   };
 
-  // |delegate| must outlive |this|.
-  DeviceDisablingManager(Delegate* delegate,
+  // `local_state` must be non-null, and must outlive `this`.
+  // `delegate` must outlive `this`.
+  DeviceDisablingManager(PrefService* local_state,
+                         Delegate* delegate,
                          CrosSettings* cros_settings,
                          user_manager::UserManager* user_manager);
 
@@ -135,6 +139,7 @@ class DeviceDisablingManager
 
   void Update();
 
+  const raw_ref<PrefService> local_state_;
   raw_ptr<Delegate> delegate_;
   raw_ptr<policy::BrowserPolicyConnectorAsh> browser_policy_connector_;
   raw_ptr<CrosSettings> cros_settings_;
