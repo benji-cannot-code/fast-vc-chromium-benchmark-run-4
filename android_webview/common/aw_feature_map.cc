4cf9e14b70f1bda00d00b2f3713ba796086249c3
Alex Mitra <alexmitra@chromium.org>
2025-08-13 09:30:22 -0700

    Reland "Adds 2 new flags to improve the freshness of app's copy of finch seed"
    This is a reland of commit b2cc8c95a443e42b520f11ecef26d2874596d916
    
    This reland adds a call to initialize WebViewCachedFlags from
    AwShellActivity and runs gpu-fyi-try-android-r-pixel-4-32 to verify that
    the previously failing tests are now fixed.
    
    Original change's description:
    > Adds 2 new flags to improve the freshness of app's copy of finch seed
    >
    > These flags half the expiration of the seed and the minimum time between
    > requesting new seeds respectively. They'll be rolled out together to see
    > if there's a positive impact on app seed freshness.
    >
    > This CL also introduces a new native API for CachedFlags which is used
    > to record a synthetic field trial.
    >
    > Bug: 414578477
    > Change-Id: Id835dc7cd1b18c3944ebf0d697850314844c8f2b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6633976
    > Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    > Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    > Commit-Queue: Alex Mitra <alexmitra@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1499780}
    
    Bug: 414578477, 437952021
    Change-Id: I9424c00926680dacf18926a959f09d27d60c8d87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6846282
    Auto-Submit: Alex Mitra <alexmitra@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Richard (Torne) Coles <torne@chromium.org>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1500850}

--- android_webview/common/aw_feature_map.cc
+++ android_webview/common/aw_feature_map.cc
@@ -55,6 +55,8 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &features::kWebViewQuicConnectionTimeout,
     &features::kWebViewRecordAppCacheHistograms,
     &features::kWebViewReduceUAAndroidVersionDeviceModel,
+    &features::kWebViewReducedSeedExpiration,
+    &features::kWebViewReducedSeedRequestPeriod,
     &features::kWebViewReportImeInsets,
     &features::kWebViewSafeAreaIncludesSystemBars,
     &features::kWebViewShortCircuitShouldInterceptRequest,
