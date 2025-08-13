969d608450b29e9479ac168c9939b31493444939
Maggie Jennings <mjenn@google.com>
2025-08-13 11:27:02 -0700

    [Cleanup] Add API test case for ViewChangeRequests
    Bug: 438164226
    Change-Id: I7582fa98ac21acf73e21d1239a3939b8a6079935
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6841622
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Commit-Queue: Maggie Jennings <mjenn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1500917}

--- chrome/test/data/webui/glic/api_test.ts
+++ chrome/test/data/webui/glic/api_test.ts
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // ash/webui/personalization_app/tools/gen_tsconfig.py --root_out_dir out/pc \
 //   --gn_target chrome/test/data/webui/glic:build_ts
 
-import {HostCapability, ScrollToErrorReason, WebClientMode} from '/glic/glic_api/glic_api.js';
-import type {FocusedTabData, GetPinCandidatesOptions, GlicBrowserHost, GlicHostRegistry, GlicWebClient, Observable, OpenPanelInfo, PanelOpeningData, ScrollToError, Subscriber, UserProfileInfo, ZeroStateSuggestionsV2} from '/glic/glic_api/glic_api.js';
+import {ClientView, HostCapability, ScrollToErrorReason, WebClientMode} from '/glic/glic_api/glic_api.js';
+import type {FocusedTabData, GetPinCandidatesOptions, GlicBrowserHost, GlicHostRegistry, GlicWebClient, Observable, OpenPanelInfo, PanelOpeningData, ScrollToError, Subscriber, UserProfileInfo, ViewChangeRequest, ZeroStateSuggestionsV2} from '/glic/glic_api/glic_api.js';
 import {ObservableValue} from '/glic/observable.js';
 
 import {createGlicHostRegistryOnLoad} from './api_boot.js';
@@ -1238,6 +1238,24 @@ class ApiTests extends ApiTestFixtureBase {
       this.host.maybeRefreshUserStatus();
       await sleep(100);
     }
+  }
+
+  async testSendsViewChangeRequestOnTaskIconOrGlicButtonToggle() {
+    assertDefined(this.host.getViewChangeRequests);
+    assertDefined(this.host.onViewChanged);
+
+    const viewChangeRequests =
+        observeSequence<ViewChangeRequest>(this.host.getViewChangeRequests());
+    const actuationChangeRequest = await viewChangeRequests.next();
+    assertDefined(actuationChangeRequest.desiredView);
+    assertEquals(actuationChangeRequest.desiredView, ClientView.ACTUATION);
+    this.host.onViewChanged({currentView: actuationChangeRequest.desiredView});
+
+    await this.advanceToNextStep();
+    const conversationChangeRequest = await viewChangeRequests.next();
+    assertDefined(conversationChangeRequest.desiredView);
+    assertEquals(
+        conversationChangeRequest.desiredView, ClientView.CONVERSATION);
   }
 
   async testJournal() {
