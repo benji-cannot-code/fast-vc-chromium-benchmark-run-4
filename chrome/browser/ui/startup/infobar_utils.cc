99bab37701762bf017d8d803a30bcfbfc2724ad5
Dylan Cutler <dylancutler@google.com>
2025-08-13 06:51:04 -0700

    Add feature to toggle info bar for --test-third-party-cookie-phaseout
    Bug: 438001109
    Change-Id: I2e74fba0c4400657558cc7fd3d342d9dcfa6d55b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6839578
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Dylan Cutler <dylancutler@google.com>
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1500771}

--- chrome/browser/ui/startup/infobar_utils.cc
+++ chrome/browser/ui/startup/infobar_utils.cc
@@ -91,6 +91,10 @@ bool IsGpuTest() {
 }
 #endif
 
+BASE_FEATURE(kShowThirdPartyCookiePhaseoutInfoBar,
+             "ShowThirdPartyCookiePhaseoutInfoBar",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
 }  // namespace
 
 void AddInfoBarsIfNecessary(Browser* browser,
@@ -126,7 +130,8 @@ void AddInfoBarsIfNecessary(Browser* browser,
     }
 
     if (base::CommandLine::ForCurrentProcess()->HasSwitch(
-            network::switches::kTestThirdPartyCookiePhaseout)) {
+            network::switches::kTestThirdPartyCookiePhaseout) &&
+        base::FeatureList::IsEnabled(kShowThirdPartyCookiePhaseoutInfoBar)) {
       TestThirdPartyCookiePhaseoutInfoBarDelegate::Create(web_contents);
     }
   }
