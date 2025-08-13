b8d9fc046087adec3ad810288911d1ab14bf7665
Marc Treib <treib@chromium.org>
2025-08-13 06:29:14 -0700

    History WebUI: Move ProfileInfoWatcher into /history subfolder
    This class is only used by HistoryLoginHandler, so it makes sense to
    live close to that.
    
    A followup will be to rename ProfileInfoWatcher to something like
    HistorySignInStateWatcher, since the current name is way too generic.
    
    AI notes: This worked reasonably well; the prompt was essentially: Move
    the files, update references, update include guards. It mostly worked,
    but failed to parse the `git status` output after staging the changes
    for commit - it thought there were still unstaged changes and tried to
    stage them again. (Also not sure why it did git stuff to begin with,
    since I never told it to stage or commit anything.) Overall, this once
    again didn't really save time, but maybe could if there were a standard
    doc with instructions.
    
    Bug: 418144047
    Change-Id: I9556ec82e4d4ce57b81d864d65a1558d1711615f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6846159
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1500758}

--- chrome/browser/ui/webui/history/profile_info_watcher.cc
+++ chrome/browser/ui/webui/history/profile_info_watcher.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/webui/profile_info_watcher.h"
+#include "chrome/browser/ui/webui/history/profile_info_watcher.h"
 
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
