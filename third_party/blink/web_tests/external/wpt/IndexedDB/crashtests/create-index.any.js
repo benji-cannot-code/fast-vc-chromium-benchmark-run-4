b7d928552112dc3f8fde72dba93555a663baf21a
Andrey Kosyakov <caseq@chromium.org>
2025-08-12 17:13:40 -0700

    Create v8::MicrotasksScope in IndexPopulator native event listener
    ... otherwise nested calls to v8 API are going to trigger a DCHECK().
    
    Bug: 434115938
    Change-Id: I855212793fccbc7c3e521eb8a13183523021a5a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6829385
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1500514}

---
+++ third_party/blink/web_tests/external/wpt/IndexedDB/crashtests/create-index.any.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=window,worker
+// META: title=IndexedDB: assure no crash when populating index
+// META: script=../resources/support.js
+// See https://crbug.com/434115938 for additional context and credits.
+
+'use_strict';
+
+promise_test(async t => {
+  const db = (await new Promise(resolve => {
+    const request = self.indexedDB.open('db', 1);
+    request.addEventListener('upgradeneeded', resolve, {once: true});
+  })).target.result;
+  const store = db.createObjectStore('store', {keyPath: 'a.b', autoIncrement: true});
+  store.put({});
+  const index = store.createIndex('index', 'keypath');
+  db.close();
+}, "Assure no crash when populating index");
