aacf1e738eea568b5f722e213f1ad11fb24c8a84
Antonio Gomes <tonikitoo@igalia.com>
2025-08-12 20:07:09 -0700

    Componentize chrome/browser/ui/passwords
    This CL adds a BUILD.gn file to drive the compilation of
    c/b/ui/passwords (and c/b/ui/passwords/settings).
    
    R=dljames@chromium.org
    
    Bug: 437811373
    Change-Id: I4f95286282e0b64e31d935507510a3d1b7239f55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6839344
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1500570}

--- chrome/browser/ui/passwords/settings/password_manager_porter.cc
+++ chrome/browser/ui/passwords/settings/password_manager_porter.cc
@@ -26,9 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/shell_dialogs/selected_file_info.h"
 
-#if BUILDFLAG(IS_WIN)
-#endif
-
 namespace {
 
 #if !BUILDFLAG(IS_ANDROID)
