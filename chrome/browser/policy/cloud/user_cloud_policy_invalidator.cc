f5b9786f7442d11fcd310b8a22a0b685ffedeed4
Artem Sumaneev <asumaneev@google.com>
2025-08-13 09:12:59 -0700

    [invalidation] Remove highest invalidation from constructor
    Initial value for the highest handled invalidation is always 0 on
    startup, there is no pref stored for it.
    
    Always initialize the highest version as 0, remove constructor
    parameter.
    
    Bug: b:341377023
    Change-Id: I5488c43490170d48afabdfd1fa0416fe884f4dfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6696152
    Reviewed-by: Roland Bock <rbock@google.com>
    Auto-Submit: Artem Sumaneev <asumaneev@google.com>
    Commit-Queue: Artem Sumaneev <asumaneev@google.com>
    Cr-Commit-Position: refs/heads/main@{#1500841}

--- chrome/browser/policy/cloud/user_cloud_policy_invalidator.cc
+++ chrome/browser/policy/cloud/user_cloud_policy_invalidator.cc
@@ -73,8 +73,7 @@ void UserCloudPolicyInvalidator::OnProfileInitializationComplete(
           policy::kPolicyInvalidationProjectNumber),
       policy_manager_->core(),
       base::SingleThreadTaskRunner::GetCurrentDefault(),
-      base::DefaultClock::GetInstance(),
-      /*highest_handled_invalidation_version=*/0);
+      base::DefaultClock::GetInstance());
 }
 
 }  // namespace policy
