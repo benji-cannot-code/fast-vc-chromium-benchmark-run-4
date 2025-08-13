071429a415a67f9e49fc14417ae86b8fe2bb6260
Sergii Bykov <sbykov@google.com>
2025-08-13 11:10:29 -0700

    kiosk: Simplify wait for page load in browser tests
    Replaces waiting for js object via TestPredicateWaiter with a browser
    test util WaitForLoadStop. Also fixes include files.
    
    Change-Id: I41bf81456834cc6a7e72985165b623b97d386f37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6838622
    Reviewed-by: Edman Anjos <edman@chromium.org>
    Commit-Queue: Sergii Bykov <sbykov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1500908}

--- chrome/browser/ash/login/app_mode/test/kiosk_enterprise_input_api_browsertest.cc
+++ chrome/browser/ash/login/app_mode/test/kiosk_enterprise_input_api_browsertest.cc
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
-#include "content/public/test/test_navigation_observer.h"
 #include "extensions/test/extension_test_message_listener.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -155,8 +154,7 @@ class KioskEnterpriseInputApiBrowserTest
     extension_ready.set_extension_id(std::string(kExtensionId));
     ASSERT_TRUE(kiosk::test::WaitKioskLaunched());
     // `chrome.runtime` only gets defined once the page finishes loading.
-    content::TestNavigationObserver(&GetAppWebContents())
-        .WaitForNavigationFinished();
+    ASSERT_TRUE(WaitForLoadStop(&GetAppWebContents()));
     ASSERT_TRUE(extension_ready.WaitUntilSatisfied());
   }
 
