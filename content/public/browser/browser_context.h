866bcdd2a03924bff3da92de851cd4172f8641b9
David Sanders <dsanders11@ucsbalum.com>
2025-08-13 00:10:47 -0700

    Remove unused include in browser_context.h
    Should save ~26 MiB of compiler input size.
    
    Adds a missing include to fix the build.
    
    Bug: 40318405
    Change-Id: Ie66c7712a6a951568d85b28e0be89dd5cae28a93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6839300
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1500635}

--- content/public/browser/browser_context.h
+++ content/public/browser/browser_context.h
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/push_messaging/push_messaging.mojom-forward.h"
 #include "third_party/blink/public/mojom/push_messaging/push_messaging_status.mojom-forward.h"
 #include "third_party/perfetto/include/perfetto/tracing/traced_value_forward.h"
-#include "url/origin.h"
 
 class GURL;
 
