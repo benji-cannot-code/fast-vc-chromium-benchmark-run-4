26954620f29234fec9a5916e8396c409b5cf3532
Asami Doi <asamidoi@chromium.org>
2025-08-13 02:56:13 -0700

    [ios] Fix DownloadManagerTestCase on iOS 26 device
    On a real device, the size of the Activity menu can be different
    depending on the contacts, installed apps, etc. This CL scrolls up to
    make sure that the target text on the Activity menu is visible.
    
    Fixed: 437314309, 437311144
    Change-Id: Ic5dc1d22d205c986e4f10bee39a0455699331bd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6842335
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1500698}

--- ios/chrome/browser/download/ui/download_manager_egtest.mm
+++ ios/chrome/browser/download/ui/download_manager_egtest.mm
@@ -36,6 +36,13 @@ using download::WaitForDownloadButton;
 using download::WaitForOpenInButton;
 using download::WaitForOpenPDFButton;
 
+namespace {
+
+// Accessibility ID of the Activity menu.
+NSString* kActivityMenuIdentifier = @"ActivityListView";
+
+}  // namespace
+
 // Helper to test critical user journeys for Download Manager.
 @interface DownloadManagerTestCaseHelper : NSObject
 
@@ -238,12 +245,13 @@ using download::WaitForOpenPDFButton;
       verifyTextVisibleInActivitySheetWithID:l10n_util::GetNSString(
                                                  IDS_IOS_OPEN_IN_DOWNLOADS)];
 
-  // Tests filename label.
-  [[EarlGrey
-      selectElementWithMatcher:grey_allOf(grey_text(@"download-example"),
-                                          grey_minimumVisiblePercent(0.65),
-                                          nil)]
-      assertWithMatcher:grey_notNil()];
+  // Scroll up to find the text if necessary. Verify that the filename label is
+  // visible.
+  [[[EarlGrey
+      selectElementWithMatcher:grey_accessibilityID(kActivityMenuIdentifier)]
+         usingSearchAction:grey_scrollInDirection(kGREYDirectionUp, 100)
+      onElementWithMatcher:grey_text(@"download-example")]
+      assertWithMatcher:grey_sufficientlyVisible()];
 }
 
 // Tests that "Open in..." works if the download ended while waiting in a
@@ -291,12 +299,14 @@ using download::WaitForOpenPDFButton;
   [ChromeEarlGrey
       verifyTextVisibleInActivitySheetWithID:l10n_util::GetNSString(
                                                  IDS_IOS_OPEN_IN_DOWNLOADS)];
-  // Tests filename label.
-  [[EarlGrey
-      selectElementWithMatcher:grey_allOf(grey_text(@"download-example"),
-                                          grey_minimumVisiblePercent(0.65),
-                                          nil)]
-      assertWithMatcher:grey_notNil()];
+
+  // Scroll up to find the text if necessary. Verify that the filename label is
+  // visible.
+  [[[EarlGrey
+      selectElementWithMatcher:grey_accessibilityID(kActivityMenuIdentifier)]
+         usingSearchAction:grey_scrollInDirection(kGREYDirectionUp, 100)
+      onElementWithMatcher:grey_text(@"download-example")]
+      assertWithMatcher:grey_sufficientlyVisible()];
 }
 
 // Tests successful blob download. This also checks that a file can be
