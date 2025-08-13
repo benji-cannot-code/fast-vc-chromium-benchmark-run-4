0f12c8930ae0314dcca4171e37a5da8bfb8c8042
Ari Chivukula <arichiv@chromium.org>
2025-08-13 02:48:19 -0700

    Launch TcpPortRandomizationWin
    This is going to 100% on M139+ so we can turn it on by default for
    M141+. The finch flag should stick around in case there are issues
    though.
    
    Bug: 40744069
    Change-Id: I5f8542c4def4b04c4758f3c77c342c95619fbec0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6834116
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Nidhi Jaju <nidhijaju@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1500696}

--- net/base/features.cc
+++ net/base/features.cc
@@ -317,7 +317,7 @@ BASE_FEATURE(kEnableGetNetworkConnectivityHintAPI,
 
 BASE_FEATURE(kTcpPortRandomizationWin,
              "TcpPortRandomizationWin",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE_PARAM(int,
                    kTcpPortRandomizationWinVersionMinimum,
