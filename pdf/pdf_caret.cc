62cde58f8f1386453638e081b274c3cc772b1882
Andy Phan <andyphan@chromium.org>
2025-08-13 16:52:34 -0700

    [PDF Ink Signatures] Rename a caret browsing method and tests
    https://crrev.com/c/6839550 will update the GetScreenRectsForChar() to
    support non-text pages. Update the name to GetScreenRectsForCaret() to
    better reflect that the caret can be placed in locations that are not
    text characters.
    
    Also update some outdated unit test names, as the method they were
    testing had also been renamed.
    
    Bug: 437807125
    Change-Id: I63e975de35369312d6897400f801572045205409
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6842116
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1501111}

--- pdf/pdf_caret.cc
+++ pdf/pdf_caret.cc
@@ -143,7 +143,7 @@ gfx::Rect PdfCaret::GetScreenRectForChar(
   }
 
   const std::vector<gfx::Rect> screen_rects =
-      client_->GetScreenRectsForChar(index);
+      client_->GetScreenRectsForCaret(index);
   return !screen_rects.empty() ? screen_rects[0] : gfx::Rect();
 }
 
