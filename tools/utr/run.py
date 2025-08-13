59cce0ddd03b7e6dbd0214ac6d4922be23683646
Ben Pastene <bpastene@chromium.org>
2025-08-13 16:13:17 -0700

    utr: Add optional --use-autoninja arg
    With https://crrev.com/c/6821386, the UTR recipe can make use of a
    supplied autoninja path. So we expose that as an optional arg.
    
    This also uncaps the siso job amount by default, which should
    hopefully lead to faster compile times for devs.
    
    Bug: 418169214
    Change-Id: I88b8afe1c3c8ad30a40cae7a2d3daae9833cdfdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6822207
    Reviewed-by: Struan Shrimpton <sshrimp@google.com>
    Commit-Queue: Ben Pastene <bpastene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1501095}

--- tools/utr/run.py
+++ tools/utr/run.py
@@ -158,6 +158,13 @@ def add_compile_args(parser):
       help='Skips instrumenting code-coverage, even if the builder is '
       'configured to instrument. Instrumentation can inflate both build sizes '
       "and runtimes. But some failures may only occur when it's enabled.")
+  parser.add_argument(
+      '--use-autoninja',
+      action='store_true',
+      help="Uses autoninja if it's detected on PATH. By default, UTR will "
+      'compile using direct siso invocations, exactly as the given builder '
+      'behaves. But this may lead to slower compiles than expected. Use this '
+      'option to instead use autoninja, which will use its own siso settings.')
 
 
 def add_test_args(parser):
@@ -299,6 +306,7 @@ def _main_impl():
         skip_coverage=not skip_compile and args.no_coverage_instrumentation,
         no_rbe=not skip_compile and args.no_rbe,
         no_siso=args.no_siso,
+        use_autoninja=not skip_compile and args.use_autoninja,
     )
     exit_code, error_msg = recipe_runner.run_recipe(
         filter_stdout=args.verbosity < 2)
