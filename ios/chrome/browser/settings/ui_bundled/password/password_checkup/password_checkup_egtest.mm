b2aea62af9f564872c24087b7bd4fb021288163d
Noémie St-Onge <noemies@google.com>
2025-08-13 06:59:56 -0700

    [iOS] De-flake testPasswordCheckupHomepageWithFailedAuth
    Fixed: 437856519
    Change-Id: I904eac7cbc6bfe959b5b725ae8da9721603da890
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6845256
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Reviewed-by: Ernesto Izquierdo Clua <eic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1500775}

--- ios/chrome/browser/settings/ui_bundled/password/password_checkup/password_checkup_egtest.mm
+++ ios/chrome/browser/settings/ui_bundled/password/password_checkup/password_checkup_egtest.mm
@@ -346,9 +346,9 @@ NSString* LeakedPasswordDescription() {
   [PasswordSettingsAppInterface mockReauthenticationModuleReturnMockedResult];
 
   // Password Manager UI should be dismissed leaving the Settings UI Visible.
+  [ChromeEarlGrey
+      waitForUIElementToDisappearWithMatcher:ReauthenticationController()];
   [[EarlGrey selectElementWithMatcher:PasswordCheckupTableView()]
-      assertWithMatcher:grey_notVisible()];
-  [[EarlGrey selectElementWithMatcher:ReauthenticationController()]
       assertWithMatcher:grey_notVisible()];
 
   [[EarlGrey selectElementWithMatcher:SettingsCollectionView()]
