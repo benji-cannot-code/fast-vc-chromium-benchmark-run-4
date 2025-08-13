c1eaebc4f9a36e4779e5eac2e9c24962d9ec19d8
Gazal <agazal@google.com>
2025-08-13 14:04:35 -0700

    Prevent crash when preferenceScreen is null
    Bug: 433576895, 436852648
    Change-Id: I1861c75fa8b2bcd5f1505e9389ed55a34ab355d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6847421
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Gazal Agarwal <agazal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1501020}

--- components/browser_ui/settings/android/java/src/org/chromium/components/browser_ui/settings/SettingsStylingController.java
+++ components/browser_ui/settings/android/java/src/org/chromium/components/browser_ui/settings/SettingsStylingController.java
@@ -70,6 +70,7 @@ public class SettingsStylingController {
     }
 
     private ArrayList<Preference> getVisiblePreferences() {
+        if (mPreferenceScreen == null) return new ArrayList<>();
         ArrayList<Preference> visiblePreferences = new ArrayList<>();
         for (int i = 0; i < mPreferenceScreen.getPreferenceCount(); i++) {
             Preference preference = mPreferenceScreen.getPreference(i);
