020d126b06e0de03ec40c8651d662fa213ceb533
Jun Ishiguro <junis@google.com>
2025-08-12 21:15:44 -0700

    ash-refactor: Pass PrefService to AutomaticRebootManager
    This is a part of an effort to remove g_browser_process dependency from
    ash specific parts.
    
    This CL modifies AutomaticRebootManager to take PrefService* in its
    constructor, and use it instead of g_browser_process->local_state().
    
    The call sites are updated to pass the local_state from
    g_browser_process for now, which will be removed in the future CLs.
    
    Bug: b:404133899
    Change-Id: Ic8435e04bde8271acf88263ec8a46fbb92a7bb5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6840938
    Reviewed-by: Georg Neis <neis@chromium.org>
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1500593}

--- chrome/browser/ash/system/automatic_reboot_manager.h
+++ chrome/browser/ash/system/automatic_reboot_manager.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 
 #include "base/callback_list.h"
+#include "base/check_deref.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
@@ -27,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/user_activity/user_activity_observer.h"
 
 class PrefRegistrySimple;
+class PrefService;
 
 namespace base {
 class TickClock;
@@ -80,7 +82,9 @@ class AutomaticRebootManager : public chromeos::PowerManagerClient::Observer,
                                public ui::UserActivityObserver,
                                public session_manager::SessionManagerObserver {
  public:
-  AutomaticRebootManager(const base::Clock* clock,
+  // `local_state` must be non-null, and must outlive `this`.
+  AutomaticRebootManager(PrefService* local_state,
+                         const base::Clock* clock,
                          const base::TickClock* tick_clock);
 
   AutomaticRebootManager(const AutomaticRebootManager&) = delete;
@@ -149,6 +153,7 @@ class AutomaticRebootManager : public chromeos::PowerManagerClient::Observer,
   const raw_ptr<const base::Clock> clock_;
   const raw_ptr<const base::TickClock> tick_clock_;
 
+  const raw_ref<PrefService> local_state_;
   PrefChangeRegistrar local_state_registrar_;
 
   base::CallbackListSubscription on_app_terminating_subscription_;
