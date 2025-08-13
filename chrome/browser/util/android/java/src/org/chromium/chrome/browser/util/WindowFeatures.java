2d8b250dd0df4a1a57f009b744334124793f17c5
Bartosz Chominski <chominskib@google.com>
2025-08-13 01:39:43 -0700

    Pass requested window features in popup-creating intent
    go/clank-window-popup-api-bounds
    
    Bug: 420598478
    Change-Id: I23da938fc0ead3f5fec61f126582c51ab725ca8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6797695
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Bartosz Chomiński <chominskib@google.com>
    Cr-Commit-Position: refs/heads/main@{#1500658}

--- chrome/browser/util/android/java/src/org/chromium/chrome/browser/util/WindowFeatures.java
+++ chrome/browser/util/android/java/src/org/chromium/chrome/browser/util/WindowFeatures.java
@@ -5,8 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.util;
 
+import android.os.Bundle;
+
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
+
+import java.util.Objects;
 
 /** Datatype for describing initial window positioning requirements used in addNewContents calls. */
 @NullMarked
@@ -15,6 +19,14 @@ public final class WindowFeatures {
     public final @Nullable Integer top;
     public final @Nullable Integer width;
     public final @Nullable Integer height;
+
+    private static final String KEY_LEFT =
+            "org.chromium.chrome.browser.util.WindowFeatures.KEY_LEFT";
+    private static final String KEY_TOP = "org.chromium.chrome.browser.util.WindowFeatures.KEY_TOP";
+    private static final String KEY_WIDTH =
+            "org.chromium.chrome.browser.util.WindowFeatures.KEY_WIDTH";
+    private static final String KEY_HEIGHT =
+            "org.chromium.chrome.browser.util.WindowFeatures.KEY_HEIGHT";
 
     public WindowFeatures() {
         this(null, null, null, null);
@@ -31,5 +43,53 @@ public final class WindowFeatures {
         this.height = height;
     }
 
-    // More methods will be added in future CLs when needed by the Window Popup feature.
+    public Bundle toBundle() {
+        final Bundle out = new Bundle();
+        if (left != null) {
+            out.putInt(KEY_LEFT, left);
+        }
+        if (top != null) {
+            out.putInt(KEY_TOP, top);
+        }
+        if (width != null) {
+            out.putInt(KEY_WIDTH, width);
+        }
+        if (height != null) {
+            out.putInt(KEY_HEIGHT, height);
+        }
+        return out;
+    }
+
+    public WindowFeatures(Bundle b) {
+        left = b.containsKey(KEY_LEFT) ? b.getInt(KEY_LEFT) : null;
+        top = b.containsKey(KEY_TOP) ? b.getInt(KEY_TOP) : null;
+        width = b.containsKey(KEY_WIDTH) ? b.getInt(KEY_WIDTH) : null;
+        height = b.containsKey(KEY_HEIGHT) ? b.getInt(KEY_HEIGHT) : null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof WindowFeatures)) {
+            return false;
+        }
+        return toString().equals(o.toString());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(toString());
+    }
+
+    @Override
+    public String toString() {
+        return "{left: "
+                + left
+                + ", top: "
+                + top
+                + ", width: "
+                + width
+                + ", height: "
+                + height
+                + "}";
+    }
 }
