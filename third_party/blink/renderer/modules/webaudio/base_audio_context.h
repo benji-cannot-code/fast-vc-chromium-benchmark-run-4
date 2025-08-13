82749d74c4c83e35935caed25596c8689eb960c3
Michael Wilson <mjwilson@chromium.org>
2025-08-13 15:21:33 -0700

    Move AsyncAudioDecoder into BaseAudioContext
    This should cause no functional change.
    
    Doing this hides more implementation details and is more efficient.
    
    Change-Id: I358a85a1c79ac0e523619d27ca1d0f14dde7d7dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6847422
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1501062}

--- third_party/blink/renderer/modules/webaudio/base_audio_context.h
+++ third_party/blink/renderer/modules/webaudio/base_audio_context.h
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/events/event_listener.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context_lifecycle_state_observer.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
-#include "third_party/blink/renderer/modules/webaudio/async_audio_decoder.h"
 #include "third_party/blink/renderer/modules/webaudio/audio_destination_node.h"
 #include "third_party/blink/renderer/modules/webaudio/deferred_task_handler.h"
 #include "third_party/blink/renderer/modules/webaudio/inspector_helper_mixin.h"
@@ -408,8 +407,6 @@ class MODULES_EXPORT BaseAudioContext
 
   // Graph locking.
   scoped_refptr<DeferredTaskHandler> deferred_task_handler_;
-
-  AsyncAudioDecoder audio_decoder_;
 
   // Vector of promises created by decodeAudioData.  This keeps the resolvers
   // alive until decodeAudioData finishes decoding and can tell the main thread
