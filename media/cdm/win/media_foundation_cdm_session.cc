3f226538d5ad85f146a57bd57b9105ffabaef7a5
Vikram Pasupathy <vpasupathy@chromium.org>
2025-08-13 20:01:16 -0700

    Remove guards on upgraded CHECKs and prolong one NotFatalUntil to M145
    Most of these upgraded CHECKs are not being reported as a crash, so we
    can remove the NotFatalUntil M140 check. However, the
    InitializeVideoDecoder has crashes reported for ChromeOS, so we should
    are prolonging the NotFatalUntil to M145.
    
    Cq-Include-Trybots: luci.chrome.try:fuchsia-ava-sherlock
    Bug: 406530495, 412213310, 424206858
    Change-Id: I8537ed5e5ef7ffa7be6d364026032110c9e2cbda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6846760
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Vikram Pasupathy <vpasupathy@chromium.org>
    Reviewed-by: Zijie He <zijiehe@google.com>
    Reviewed-by: Feras Aldahlawi <frs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1501178}

--- media/cdm/win/media_foundation_cdm_session.cc
+++ media/cdm/win/media_foundation_cdm_session.cc
@@ -203,7 +203,7 @@ HRESULT MediaFoundationCdmSession::GenerateRequest(
     const std::vector<uint8_t>& init_data,
     SessionIdCB session_id_cb) {
   DVLOG_FUNC(1);
-  CHECK((session_id_.empty() && !session_id_cb_), base::NotFatalUntil::M140);
+  CHECK((session_id_.empty() && !session_id_cb_));
 
   session_id_cb_ = std::move(session_id_cb);
 
@@ -265,7 +265,7 @@ void MediaFoundationCdmSession::OnSessionMessage(
   if (session_id_.empty() && !SetSessionId())
     return;
 
-  CHECK(!session_id_.empty(), base::NotFatalUntil::M140);
+  CHECK(!session_id_.empty());
   session_message_cb_.Run(session_id_, message_type, message);
 }
 
@@ -299,22 +299,20 @@ void MediaFoundationCdmSession::OnSessionKeysChange() {
 }
 
 bool MediaFoundationCdmSession::SetSessionId() {
-  CHECK((session_id_.empty() && session_id_cb_), base::NotFatalUntil::M140);
+  CHECK((session_id_.empty() && session_id_cb_));
 
   base::win::ScopedCoMem<wchar_t> session_id;
   HRESULT hr = mf_cdm_session_->GetSessionId(&session_id);
   if (FAILED(hr) || !session_id) {
     bool success = std::move(session_id_cb_).Run("");
-    CHECK(!success, base::NotFatalUntil::M140)
-        << "Empty session ID should not be accepted";
+    CHECK(!success) << "Empty session ID should not be accepted";
     return false;
   }
 
   auto session_id_str = base::WideToUTF8(session_id.get());
   if (session_id_str.empty()) {
     bool success = std::move(session_id_cb_).Run("");
-    CHECK(!success, base::NotFatalUntil::M140)
-        << "Empty session ID should not be accepted";
+    CHECK(!success) << "Empty session ID should not be accepted";
     return false;
   }
 
@@ -330,7 +328,7 @@ bool MediaFoundationCdmSession::SetSessionId() {
 }
 
 HRESULT MediaFoundationCdmSession::UpdateExpirationIfNeeded() {
-  CHECK(!session_id_.empty(), base::NotFatalUntil::M140);
+  CHECK(!session_id_.empty());
 
   // Media Foundation CDM follows the EME spec where Time generally represents
   // an instant in time with millisecond accuracy.
