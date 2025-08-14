1f685d5ec4c4ea9a5c00d94f7bd1e007578f4587
Maria Kazinova <kazinova@google.com>
2025-08-14 01:45:23 -0700

    [Passwords] Fix the password manager blocklist policy.
    The policy was added recently, but it's not working, because the
    factory is not registered timely, and the CHECK is hit on accessing
    the factory.
    
    Change-Id: Ib492fccb1228475f59f913af1359386f092dbdf1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6842657
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1501263}

--- chrome/browser/profiles/profile_keyed_service_browsertest.cc
+++ chrome/browser/profiles/profile_keyed_service_browsertest.cc
@@ -266,6 +266,10 @@ IN_PROC_BROWSER_TEST_F(ProfileKeyedServiceBrowserTest,
     "LocalPresentationManager",
     "OmniboxInputWatcher",
     "OmniboxSuggestionsWatcher",
+#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN) || BUILDFLAG(IS_LINUX) || \
+    BUILDFLAG(IS_CHROMEOS)
+    "PasswordManagerBlocklist",
+#endif
     "PasswordManagerInternalsService",
     "PasswordRequirementsServiceFactory",
     "PolicyBlocklist",
@@ -312,6 +316,10 @@ IN_PROC_BROWSER_TEST_F(ProfileKeyedServiceBrowserTest,
     "HasEnrolledInstrumentQuery",
     "OmniboxInputWatcher",
     "OmniboxSuggestionsWatcher",
+#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN) || BUILDFLAG(IS_LINUX) || \
+    BUILDFLAG(IS_CHROMEOS)
+    "PasswordManagerBlocklist",
+#endif
     "PolicyBlocklist",
     "PolicyClipboardRestriction",
 #if BUILDFLAG(ENTERPRISE_CONTENT_ANALYSIS)
