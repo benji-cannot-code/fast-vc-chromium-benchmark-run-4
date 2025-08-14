64a2d2d7d838938a4c6debeb2c94c1d69bc34bfd
Jerry Liu <pzliu@google.com>
2025-08-14 12:29:08 -0700

    p13n: refresh screensaver google photos albums when photos count changes
    When the photos count of an album is updated, we should update the
    album for display so that the UI always shows the correct number of
    photos in an album.
    
    Screenshot: http://go/scrcast/NDk3MDk5Njk2NzY2OTc2MHw5ZTQ2ZjY2NC1kNw
    
    BUG=b:424739954
    TEST=browser_tests --gtest-filter="*AmbientSubpage*"
    
    Change-Id: I6199619df905373be09f2eb9da53bac6591f39f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6843293
    Auto-Submit: Jerry Liu <pzliu@google.com>
    Reviewed-by: Jeffrey Young <cowmoo@google.com>
    Commit-Queue: Jeffrey Young <cowmoo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1501542}

--- ash/webui/personalization_app/resources/js/ambient/album_list_element.ts
+++ ash/webui/personalization_app/resources/js/ambient/album_list_element.ts
@@ -76,7 +76,7 @@ export class AlbumListElement extends WithPersonalizationStore {
     this.updateList(
         /*propertyPath=*/ 'albumsForDisplay_',
         /*identityGetter=*/
-        (album: AmbientModeAlbum) => album.id,
+        (album: AmbientModeAlbum) => album.id + `${album.numberOfPhotos}`,
         /*newList=*/ albums,
         /*identityBasedUpdate=*/ true,
     );
