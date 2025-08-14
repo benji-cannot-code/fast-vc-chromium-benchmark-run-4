b7aca0ed250d191e62ef55bbc44a8a34620c9667
Tom Anderson <thomasanderson@chromium.org>
2025-08-13 18:36:09 -0700

    Change PowerSaveBlocker::Delegate from ref counted to sequence bound
    R=reillyg
    
    Bug: None
    Change-Id: I6cefe0fc3975e400e06780d899f90e3affe0a6b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6844476
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1501153}

--- services/device/wake_lock/power_save_blocker/power_save_blocker_stub.cc
+++ services/device/wake_lock/power_save_blocker/power_save_blocker_stub.cc
@@ -11,17 +11,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace device {
 
-class PowerSaveBlocker::Delegate
-    : public base::RefCountedThreadSafe<PowerSaveBlocker::Delegate> {
+class PowerSaveBlocker::Delegate {
  public:
-  Delegate() {}
+  Delegate() = default;
 
   Delegate(const Delegate&) = delete;
   Delegate& operator=(const Delegate&) = delete;
 
- private:
-  friend class base::RefCountedThreadSafe<Delegate>;
-  virtual ~Delegate() {}
+  ~Delegate() = default;
 };
 
 PowerSaveBlocker::PowerSaveBlocker(
@@ -29,7 +26,7 @@ PowerSaveBlocker::PowerSaveBlocker(
     mojom::WakeLockReason reason,
     const std::string& description,
     scoped_refptr<base::SequencedTaskRunner> ui_task_runner)
-    : delegate_(new Delegate()), ui_task_runner_(ui_task_runner) {}
+    : delegate_(ui_task_runner) {}
 
 PowerSaveBlocker::~PowerSaveBlocker() {}
 
