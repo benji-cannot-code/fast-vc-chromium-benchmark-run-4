0e9337a0c92a0300b65332415d56598698f953e5
Johannes Henkel <johannes@chromium.org>
2025-08-14 14:56:00 -0700

    [Blink] Remove deprecated Document.softNavigations method
    This CL removes the deprecated and non-standard 'softNavigations'
    method from the Document interface.
    
    The method was used in tests to verify soft navigation heuristics but
    was not exposed to the web. The C++ implementation in Document.h/cc and
    the IDL definition have been removed.
    
    Existing usages in Web Platform Tests have been updated to use the
    standard 'performance.getEntriesByType('soft-navigation').length' API,
    which provides the same functionality.
    
    Bug: 415977076
    Change-Id: Ic07c56b7400a6f2e28beb32e3e9c75b06d835a87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6846370
    Reviewed-by: Michal Mocny <mmocny@chromium.org>
    Commit-Queue: Johannes Henkel <johannes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1501626}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -346,7 +346,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/svg_element_factory.h"
 #include "third_party/blink/renderer/core/svg_names.h"
 #include "third_party/blink/renderer/core/timing/render_blocking_metrics_reporter.h"
-#include "third_party/blink/renderer/core/timing/soft_navigation_heuristics.h"
 #include "third_party/blink/renderer/core/trustedtypes/trusted_html.h"
 #include "third_party/blink/renderer/core/view_transition/page_reveal_event.h"
 #include "third_party/blink/renderer/core/view_transition/view_transition_supplement.h"
@@ -2270,17 +2269,6 @@ String Document::visibilityStateAsString() const {
 
 bool Document::prerendering() const {
   return IsPrerendering();
-}
-uint32_t Document::softNavigations() const {
-  LocalDOMWindow* window = domWindow();
-  if (!window) {
-    return 0;
-  }
-  if (SoftNavigationHeuristics* heuristics =
-          window->GetSoftNavigationHeuristics()) {
-    return heuristics->SoftNavigationCount();
-  }
-  return 0;
 }
 
 bool Document::hidden() const {
--- third_party/blink/renderer/core/dom/document.h
+++ third_party/blink/renderer/core/dom/document.h
@@ -581,8 +581,6 @@ class CORE_EXPORT Document : public ContainerNode,
 
   bool prerendering() const;
 
-  uint32_t softNavigations() const;
-
   bool wasDiscarded() const;
   void SetWasDiscarded(bool);
 
