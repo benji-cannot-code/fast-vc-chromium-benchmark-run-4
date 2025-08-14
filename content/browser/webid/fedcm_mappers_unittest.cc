7da918907f4abc2d5e755d98fd22c976f2bd8b11
Christian Biesinger <cbiesinger@chromium.org>
2025-08-14 09:17:05 -0700

    [FedCM] Ship alternative identifiers
    Chromestatus: https://chromestatus.com/feature/5121180773908480
    Intent to ship:
    https://groups.google.com/a/chromium.org/g/blink-dev/c/TwBCFixr72k/m/mg1KtqXxAQAJ
    
    R=npm@chromium.org
    
    Fixed: 382086282
    Change-Id: I0759d50079993bf229a9a43e4e12fb87c0d40a3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6846362
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Auto-Submit: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1501429}

--- content/browser/webid/fedcm_mappers_unittest.cc
+++ content/browser/webid/fedcm_mappers_unittest.cc
@@ -42,7 +42,9 @@ TEST(FedCmMappersTest, GetDisclosureFieldsEmpty) {
       ElementsAre());
 }
 
-TEST(FedCmMappersTest, GetDisclosureFields) {
+TEST(FedCmMappersTest, GetDisclosureFieldsWithoutAlternativeIdentifiers) {
+  base::test::ScopedFeatureList list;
+  list.InitAndDisableFeature(features::kFedCmAlternativeIdentifiers);
   // When no fields are passed, we use the default.
   EXPECT_THAT(GetDisclosureFields(std::nullopt),
               ElementsAre(Field::kName, Field::kEmail, Field::kPicture));
