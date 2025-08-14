bf7ca6234bf3ec982f93a6928f15d019bcfa6d58
Thomas Lukaszewicz <tluk@chromium.org>
2025-08-14 15:13:07 -0700

    [bedrock] Address UAF issues in ExtensionsMenuCoordinator
    This CL addresses a dangling ViewObserver reference added in
    ExtensionsMenuCoordinator::CreateExtensionsMenuBubbleDialogDelegate()
    using a ScopedObservation on the bubble_view.
    
    Also synchronously closes the Extensions menu widget when the bubble
    when the controller is destroyed as the menu holds references to the
    ExtensionsMenuCoordinator and its Browser which may dangle if
    destroyed asynchronously.
    
    Bug: 413168662
    Change-Id: I15d8cfcaddf29be71fd7c8ae4996f646cc5e009d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6845195
    Commit-Queue: Tom Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Fred Shih <ffred@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1501636}

--- chrome/browser/ui/views/extensions/extensions_menu_coordinator.cc
+++ chrome/browser/ui/views/extensions/extensions_menu_coordinator.cc
@@ -21,7 +21,11 @@ ExtensionsMenuCoordinator::ExtensionsMenuCoordinator(Browser* browser)
     : browser_(browser) {}
 
 ExtensionsMenuCoordinator::~ExtensionsMenuCoordinator() {
-  Hide();
+  if (views::Widget* const menu = GetExtensionsMenuWidget()) {
+    // Close the menu widget synchronously as it may hold references back to the
+    // coordinator and its host Browser.
+    menu->CloseNow();
+  }
 }
 
 void ExtensionsMenuCoordinator::Show(
@@ -39,8 +43,7 @@ void ExtensionsMenuCoordinator::Show(
 void ExtensionsMenuCoordinator::Hide() {
   DCHECK(base::FeatureList::IsEnabled(
       extensions_features::kExtensionsMenuAccessControl));
-  views::Widget* const menu = GetExtensionsMenuWidget();
-  if (menu) {
+  if (views::Widget* const menu = GetExtensionsMenuWidget()) {
     menu->Close();
     // Immediately stop tracking the view. Widget will be destroyed
     // asynchronously.
@@ -86,7 +89,7 @@ ExtensionsMenuCoordinator::CreateExtensionsMenuBubbleDialogDelegate(
 
   auto* bubble_contents = bubble_delegate->SetContentsView(
       views::Builder<views::View>().SetUseDefaultFillLayout(true).Build());
-  bubble_contents->View::AddObserver(this);
+  bubble_view_observation_.Observe(bubble_contents);
   bubble_tracker_.SetView(bubble_contents);
 
   controller_ = std::make_unique<ExtensionsMenuViewController>(
@@ -98,6 +101,7 @@ ExtensionsMenuCoordinator::CreateExtensionsMenuBubbleDialogDelegate(
 
 void ExtensionsMenuCoordinator::OnViewIsDeleting(views::View* observed_view) {
   bubble_tracker_.SetView(nullptr);
+  bubble_view_observation_.Reset();
   // Reset the controller to keep 1:1 lifetime with the view.
   controller_.reset();
 }
