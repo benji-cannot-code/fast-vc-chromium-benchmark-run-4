bd8979f45adc303ca6e5922df3f4c6b00e4a9505
Daniil Sakhapov <sakhapov@chromium.org>
2025-08-14 03:35:37 -0700

    Remove runtime flag for unprefixed background-clip
    It's been in stable for about 2 years.
    
    Bug: 40229927
    Change-Id: Ieb09d9f32cf6e3a024928294499910a781fc518c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6846224
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1501294}

--- third_party/blink/renderer/core/frame/use_counter_impl_test.cc
+++ third_party/blink/renderer/core/frame/use_counter_impl_test.cc
@@ -691,11 +691,7 @@ TEST_F(UseCounterImplTest, BackgroundClip) {
   document.documentElement()->SetInnerHTMLWithoutTrustedTypes(
       "<style>html{-webkit-background-clip: border;}</style>");
   UpdateAllLifecyclePhases(document);
-  if (RuntimeEnabledFeatures::CSSBackgroundClipUnprefixEnabled()) {
-    EXPECT_FALSE(document.IsUseCounted(WebFeature::kCSSBackgroundClipBorder));
-  } else {
-    EXPECT_TRUE(document.IsUseCounted(WebFeature::kCSSBackgroundClipBorder));
-  }
+  EXPECT_FALSE(document.IsUseCounted(WebFeature::kCSSBackgroundClipBorder));
   EXPECT_FALSE(document.IsUseCounted(WebFeature::kCSSBackgroundClipContent));
   EXPECT_FALSE(document.IsUseCounted(WebFeature::kCSSBackgroundClipPadding));
 
@@ -704,11 +700,7 @@ TEST_F(UseCounterImplTest, BackgroundClip) {
       "<style>html{-webkit-background-clip: content;}</style>");
   UpdateAllLifecyclePhases(document);
   EXPECT_FALSE(document.IsUseCounted(WebFeature::kCSSBackgroundClipBorder));
-  if (RuntimeEnabledFeatures::CSSBackgroundClipUnprefixEnabled()) {
-    EXPECT_FALSE(document.IsUseCounted(WebFeature::kCSSBackgroundClipContent));
-  } else {
-    EXPECT_TRUE(document.IsUseCounted(WebFeature::kCSSBackgroundClipContent));
-  }
+  EXPECT_FALSE(document.IsUseCounted(WebFeature::kCSSBackgroundClipContent));
   EXPECT_FALSE(document.IsUseCounted(WebFeature::kCSSBackgroundClipPadding));
 
   document.ClearUseCounterForTesting(WebFeature::kCSSBackgroundClipContent);
@@ -717,11 +709,7 @@ TEST_F(UseCounterImplTest, BackgroundClip) {
   UpdateAllLifecyclePhases(document);
   EXPECT_FALSE(document.IsUseCounted(WebFeature::kCSSBackgroundClipBorder));
   EXPECT_FALSE(document.IsUseCounted(WebFeature::kCSSBackgroundClipContent));
-  if (RuntimeEnabledFeatures::CSSBackgroundClipUnprefixEnabled()) {
-    EXPECT_FALSE(document.IsUseCounted(WebFeature::kCSSBackgroundClipPadding));
-  } else {
-    EXPECT_TRUE(document.IsUseCounted(WebFeature::kCSSBackgroundClipPadding));
-  }
+  EXPECT_FALSE(document.IsUseCounted(WebFeature::kCSSBackgroundClipPadding));
 }
 
 }  // namespace blink
