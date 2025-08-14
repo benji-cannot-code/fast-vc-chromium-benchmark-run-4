4ce7dd2040249cedb53c8468ac5672ef4e38bb3b
Tom Anderson <thomasanderson@chromium.org>
2025-08-14 11:57:16 -0700

    Reland "Change PowerSaveBlocker::Delegate from ref counted to sequence bound"
    This is a reland of commit 6ab808c5184659e6e4c5fc608ffee31118860275
    
    Original change's description:
    > Change PowerSaveBlocker::Delegate from ref counted to sequence bound
    >
    > R=reillyg
    >
    > Bug: None
    > Change-Id: I6cefe0fc3975e400e06780d899f90e3affe0a6b3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6844476
    > Reviewed-by: Reilly Grant <reillyg@chromium.org>
    > Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    > Commit-Queue: Reilly Grant <reillyg@chromium.org>
    > Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1501153}
    
    Bug: None
    Change-Id: Id4d1eb5a361fbbcf72abadb665c8fc31c5e65977
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6851550
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1501518}

--- services/device/wake_lock/power_save_blocker/power_save_blocker_android.cc
+++ services/device/wake_lock/power_save_blocker/power_save_blocker_android.cc
@@ -19,27 +19,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace device {
 
 using base::android::AttachCurrentThread;
+using base::android::ScopedJavaGlobalRef;
 using base::android::ScopedJavaLocalRef;
 
-class PowerSaveBlocker::Delegate
-    : public base::RefCountedThreadSafe<PowerSaveBlocker::Delegate> {
+class PowerSaveBlocker::Delegate {
  public:
-  Delegate(scoped_refptr<base::SequencedTaskRunner> ui_task_runner);
+  explicit Delegate(scoped_refptr<base::SequencedTaskRunner> ui_task_runner);
 
   Delegate(const Delegate&) = delete;
   Delegate& operator=(const Delegate&) = delete;
 
-  // Does the actual work to apply or remove the desired power save block.
-  void ApplyBlock(ui::ViewAndroid* view_android);
-  void RemoveBlock();
-
- private:
-  friend class base::RefCountedThreadSafe<Delegate>;
   ~Delegate();
 
+  // Does the actual work to apply or remove the desired power save block.
+  void ApplyBlock(ScopedJavaGlobalRef<jobject> container_view);
+
+ private:
   base::android::ScopedJavaGlobalRef<jobject> java_power_save_blocker_;
 
   scoped_refptr<base::SequencedTaskRunner> ui_task_runner_;
+
+  size_t block_count_ = 0;
 };
 
 PowerSaveBlocker::Delegate::Delegate(
@@ -49,25 +49,26 @@ PowerSaveBlocker::Delegate::Delegate(
   java_power_save_blocker_.Reset(Java_PowerSaveBlocker_create(env));
 }
 
-PowerSaveBlocker::Delegate::~Delegate() {}
-
-void PowerSaveBlocker::Delegate::ApplyBlock(ui::ViewAndroid* view_android) {
+PowerSaveBlocker::Delegate::~Delegate() {
   DCHECK(ui_task_runner_->RunsTasksInCurrentSequence());
-  DCHECK(view_android);
 
   ScopedJavaLocalRef<jobject> obj(java_power_save_blocker_);
-  ScopedJavaLocalRef<jobject> container_view(view_android->GetContainerView());
-  if (container_view.is_null())
-    return;
-
-  Java_PowerSaveBlocker_applyBlock(AttachCurrentThread(), obj, container_view);
+  for (size_t i = 0; i < block_count_; ++i) {
+    Java_PowerSaveBlocker_removeBlock(AttachCurrentThread(), obj);
+  }
 }
 
-void PowerSaveBlocker::Delegate::RemoveBlock() {
+void PowerSaveBlocker::Delegate::ApplyBlock(
+    ScopedJavaGlobalRef<jobject> container_view) {
   DCHECK(ui_task_runner_->RunsTasksInCurrentSequence());
 
   ScopedJavaLocalRef<jobject> obj(java_power_save_blocker_);
-  Java_PowerSaveBlocker_removeBlock(AttachCurrentThread(), obj);
+  if (container_view.is_null()) {
+    return;
+  }
+
+  Java_PowerSaveBlocker_applyBlock(AttachCurrentThread(), obj, container_view);
+  block_count_++;
 }
 
 PowerSaveBlocker::PowerSaveBlocker(
@@ -75,23 +76,16 @@ PowerSaveBlocker::PowerSaveBlocker(
     mojom::WakeLockReason reason,
     const std::string& description,
     scoped_refptr<base::SequencedTaskRunner> ui_task_runner)
-    : ui_task_runner_(ui_task_runner) {
+    : delegate_(ui_task_runner, ui_task_runner) {
   // Don't support PreventAppSuspension.
 }
 
-PowerSaveBlocker::~PowerSaveBlocker() {
-  if (delegate_.get()) {
-    ui_task_runner_->PostTask(
-        FROM_HERE, base::BindOnce(&Delegate::RemoveBlock, delegate_));
-  }
-}
+PowerSaveBlocker::~PowerSaveBlocker() = default;
 
 void PowerSaveBlocker::InitDisplaySleepBlocker(ui::ViewAndroid* view_android) {
-  DCHECK(ui_task_runner_->RunsTasksInCurrentSequence());
   DCHECK(view_android);
-
-  delegate_ = new Delegate(ui_task_runner_);
-  delegate_->ApplyBlock(view_android);
+  delegate_.AsyncCall(&PowerSaveBlocker::Delegate::ApplyBlock)
+      .WithArgs(ScopedJavaGlobalRef<jobject>(view_android->GetContainerView()));
 }
 
 }  // namespace device
--- services/device/wake_lock/power_save_blocker/power_save_blocker_win.cc
+++ services/device/wake_lock/power_save_blocker/power_save_blocker_win.cc
@@ -50,8 +50,7 @@ void DeletePowerRequest(POWER_REQUEST_TYPE type, HANDLE handle) {
 
 }  // namespace
 
-class PowerSaveBlocker::Delegate
-    : public base::RefCountedThreadSafe<PowerSaveBlocker::Delegate> {
+class PowerSaveBlocker::Delegate {
  public:
   Delegate(mojom::WakeLockType type,
            const std::string& description,
@@ -63,6 +62,8 @@ class PowerSaveBlocker::Delegate
   Delegate(const Delegate&) = delete;
   Delegate& operator=(const Delegate&) = delete;
 
+  ~Delegate() = default;
+
   // Does the actual work to apply or remove the desired power save block.
   void ApplyBlock();
   void RemoveBlock();
@@ -71,9 +72,6 @@ class PowerSaveBlocker::Delegate
   POWER_REQUEST_TYPE RequestType();
 
  private:
-  friend class base::RefCountedThreadSafe<Delegate>;
-  ~Delegate() {}
-
   mojom::WakeLockType type_;
   const std::string description_;
   base::win::ScopedHandle handle_;
@@ -119,15 +117,12 @@ PowerSaveBlocker::PowerSaveBlocker(
     mojom::WakeLockReason reason,
     const std::string& description,
     scoped_refptr<base::SequencedTaskRunner> ui_task_runner)
-    : delegate_(new Delegate(type, description, ui_task_runner)),
-      ui_task_runner_(ui_task_runner) {
-  ui_task_runner_->PostTask(FROM_HERE,
-                            base::BindOnce(&Delegate::ApplyBlock, delegate_));
+    : delegate_(ui_task_runner, type, description, ui_task_runner) {
+  delegate_.AsyncCall(&Delegate::ApplyBlock);
 }
 
 PowerSaveBlocker::~PowerSaveBlocker() {
-  ui_task_runner_->PostTask(FROM_HERE,
-                            base::BindOnce(&Delegate::RemoveBlock, delegate_));
+  delegate_.AsyncCall(&Delegate::RemoveBlock);
 }
 
 }  // namespace device
