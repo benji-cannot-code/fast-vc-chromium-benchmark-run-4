bdb65e512c8c75c3adddfe3564c405d3574b9bed
Bruno Braga <brunobraga@google.com>
2025-08-14 03:08:16 -0700

    [AutofillAi] ktn expiration date should be of type date
    Bug: 438424501
    Change-Id: Id60bf4150fb4187070a03775f49e80d101dfb7a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6845747
    Auto-Submit: Bruno Braga <brunobraga@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1501283}

--- components/autofill/core/browser/field_type_utils.cc
+++ components/autofill/core/browser/field_type_utils.cc
@@ -319,7 +319,6 @@ bool IsDateFieldType(FieldType field_type) {
     case NATIONAL_ID_CARD_ISSUING_COUNTRY:
     case REDRESS_NUMBER:
     case KNOWN_TRAVELER_NUMBER:
-    case KNOWN_TRAVELER_NUMBER_EXPIRATION_DATE:
     case MAX_VALID_FIELD_TYPE:
       return false;
     case CREDIT_CARD_EXP_DATE_2_DIGIT_YEAR:
@@ -330,6 +329,7 @@ bool IsDateFieldType(FieldType field_type) {
     case DRIVERS_LICENSE_ISSUE_DATE:
     case NATIONAL_ID_CARD_EXPIRATION_DATE:
     case NATIONAL_ID_CARD_ISSUE_DATE:
+    case KNOWN_TRAVELER_NUMBER_EXPIRATION_DATE:
       return true;
   }
   NOTREACHED();
