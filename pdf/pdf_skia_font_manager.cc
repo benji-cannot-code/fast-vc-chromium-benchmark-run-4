2dd9f6fad48c6ae212bcbf7c8aeca4e54d6ab132
Lei Zhang <thestig@chromium.org>
2025-08-14 11:24:33 -0700

    [PDF] Add pdf_skia_font_manager to customize PDFium behavior
    Add a GN target that implements pdfium_skia_custom_font_manager(), which
    corresponds to what https://pdfium-review.googlesource.com/134890 calls.
    This overrides the SkFontMgr used in Skia-enabled PDFium, to avoid using
    FreeType.
    
    Change-Id: I66e9c7c65b42fba25c2e682c4b9d581f89c50956
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6828742
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Ben Wagner <bungeman@google.com>
    Cr-Commit-Position: refs/heads/main@{#1501498}

---
+++ pdf/pdf_skia_font_manager.cc
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/skia/include/core/SkFontMgr.h"
+#include "third_party/skia/include/ports/SkFontMgr_Fontations.h"
+
+sk_sp<SkFontMgr> pdfium_skia_custom_font_manager() {
+  return SkFontMgr_New_Fontations_Empty();
+}
