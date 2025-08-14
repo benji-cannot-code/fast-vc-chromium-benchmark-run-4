815d8940b33a04664214e5a8456acee5ae02d18b
Tanmoy Mollik <triploblastic@google.com>
2025-08-14 06:19:05 -0700

    Use OAuthConsumerId for PrimaryAccountAccessTokenFetcher clients - 6
    This cl is part of a series that converts
    PrimaryAccountAccessTokenFetcher clients to use OAuthConsumerId based
    constructor instead.
    
    Bug: 425896213
    Change-Id: If88beafb8a4f6e0d4d8c323f1ee5eec204c07176
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6827355
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Menghan Yang <myuu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1501346}

--- chrome/browser/webauthn/enclave_manager.cc
+++ chrome/browser/webauthn/enclave_manager.cc
@@ -1562,7 +1562,7 @@ class EnclaveManager::StateMachine {
       store_keys_args_for_joining_ = std::move(store_keys_args);
       if (!new_security_domain_secrets_.empty()) {
         state_ = State::kWaitingForEnclaveTokenForWrapping;
-        GetAccessTokenInternal(GaiaConstants::kPasskeysEnclaveOAuth2Scope);
+        GetAccessTokenInternal();
       } else if (!user_->joined() && !user_->member_public_key().empty()) {
         JoinSecurityDomain();
       }
@@ -1610,7 +1610,7 @@ class EnclaveManager::StateMachine {
       }
 
       state_ = State::kWaitingForEnclaveTokenForUnregister;
-      GetAccessTokenInternal(GaiaConstants::kPasskeysEnclaveOAuth2Scope);
+      GetAccessTokenInternal();
       return;
     }
 
@@ -1812,7 +1812,7 @@ class EnclaveManager::StateMachine {
     }
 
     state_ = State::kWaitingForEnclaveTokenForRegistration;
-    GetAccessTokenInternal(GaiaConstants::kPasskeysEnclaveOAuth2Scope);
+    GetAccessTokenInternal();
   }
 
   void DoWaitingForEnclaveTokenForRegistration(Event event) {
@@ -2127,7 +2127,7 @@ class EnclaveManager::StateMachine {
     }
 
     state_ = State::kWaitingForEnclaveTokenForPINWrapping;
-    GetAccessTokenInternal(GaiaConstants::kPasskeysEnclaveOAuth2Scope);
+    GetAccessTokenInternal();
   }
 
   void DoWaitingForEnclaveTokenForPINWrapping(Event event) {
@@ -2638,11 +2638,11 @@ class EnclaveManager::StateMachine {
                        weak_ptr_factory_.GetWeakPtr()));
   }
 
-  void GetAccessTokenInternal(const char* scope) {
+  void GetAccessTokenInternal() {
     access_token_fetcher_ =
         std::make_unique<signin::PrimaryAccountAccessTokenFetcher>(
-            "passkeys_enclave", manager_->identity_manager_,
-            signin::ScopeSet{scope},
+            signin::OAuthConsumerId::kEnclaveManager,
+            manager_->identity_manager_,
             base::BindOnce(
                 [](base::WeakPtr<StateMachine> machine,
                    GoogleServiceAuthError error,
