bdf197cbd4a33201d3250fb8cacd6b11cb8567a3
Scott Haseley <shaseley@chromium.org>
2025-08-13 17:24:44 -0700

    AbortSignal: Add debugging for crash
    AbortSignal::RunAbortSteps() occasionally crashes due to a handle in
    `abort_algorithms_` being null. This adds some debugging and one small
    (probably not root cause) fix:
     - Iterate over the dependent signals using an AbortSignal* rather
       than const auto&, which is a ref to a WeakMember. If abort reason
       is empty, this can allocate, so can potentially trigger a GC and
       collect the signal before adding it to the local copy of the signals.
    
     - I don't think reentrancy is possible due to the aborted() checks, and
       this CL adds a bool flag to check that.
    
     - This adds a loop before running algorithms to check the state before
       dispatching any algorithms, which I suspect is either causing a
       handle to be GCed and possible invalidating the iterator. This also
       adds crash keys to see if GC is changing the size.
    
    Bug: 40068730
    Change-Id: Icd641ddccbb6b8a8f26a6ab280719679eac19e11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6844276
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1501129}

--- third_party/blink/renderer/core/dom/abort_signal.cc
+++ third_party/blink/renderer/core/dom/abort_signal.cc
@@ -8,7 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 #include <utility>
 
+#include "base/auto_reset.h"
 #include "base/check_deref.h"
+#include "base/debug/crash_logging.h"
 #include "base/functional/callback.h"
 #include "base/time/time.h"
 #include "third_party/blink/public/platform/task_type.h"
@@ -202,6 +204,7 @@ AbortSignal::AlgorithmHandle* AbortSignal::AddAlgorithm(Algorithm* algorithm) {
   if (aborted() || composition_manager_->IsSettled()) {
     return nullptr;
   }
+  CHECK(!is_running_abort_steps_);
   auto* handle = MakeGarbageCollected<AlgorithmHandle>(algorithm, this);
   CHECK(!abort_algorithms_.Contains(handle));
   // This always appends since `handle` is not already in the collection.
@@ -214,6 +217,7 @@ AbortSignal::AlgorithmHandle* AbortSignal::AddAlgorithm(
   if (aborted() || composition_manager_->IsSettled()) {
     return nullptr;
   }
+  CHECK(!is_running_abort_steps_);
   auto* callback_algorithm =
       MakeGarbageCollected<OnceCallbackAlgorithm>(std::move(algorithm));
   auto* handle =
@@ -228,6 +232,7 @@ void AbortSignal::RemoveAlgorithm(AlgorithmHandle* handle) {
   if (aborted() || composition_manager_->IsSettled()) {
     return;
   }
+  CHECK(!is_running_abort_steps_);
   abort_algorithms_.erase(handle);
 }
 
@@ -286,7 +291,21 @@ void AbortSignal::SetAbortReason(ScriptState* script_state,
 }
 
 void AbortSignal::RunAbortSteps() {
+  CHECK(!is_running_abort_steps_);
+  base::AutoReset<bool> scope(&is_running_abort_steps_, true);
+  SCOPED_CRASH_KEY_NUMBER("AbortSignal", "size_before",
+                          static_cast<int>(abort_algorithms_.size()));
+
+  // TODO(crbug.com/40068730): Remove after root-causing the bug. This is meant
+  // to help determine if running the algorithms is changing the set or
+  // invalidating the iterator unexpectedly.
   for (AbortSignal::AlgorithmHandle* handle : abort_algorithms_) {
+    CHECK(handle);
+  }
+
+  for (AbortSignal::AlgorithmHandle* handle : abort_algorithms_) {
+    SCOPED_CRASH_KEY_NUMBER("AbortSignal", "current_size",
+                            static_cast<int>(abort_algorithms_.size()));
     CHECK(handle);
     CHECK(handle->GetAlgorithm());
     handle->GetAlgorithm()->Run();
@@ -315,11 +334,13 @@ void AbortSignal::DetachFromController() {
   if (aborted()) {
     return;
   }
+  CHECK(!is_running_abort_steps_);
   composition_manager_->Settle();
 }
 
 void AbortSignal::OnSignalSettled(AbortSignalCompositionType type) {
   if (type == AbortSignalCompositionType::kAbort) {
+    CHECK(!is_running_abort_steps_);
     abort_algorithms_.clear();
   }
   if (signal_type_ == SignalType::kComposite && GetExecutionContext()) {
