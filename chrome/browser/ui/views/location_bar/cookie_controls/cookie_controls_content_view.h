d1e055d5c136422154b7988d03aba503b9298746
Morten Stenshorne <mstensho@chromium.org>
2025-08-14 01:33:12 -0700

    Revert "Componentize chrome/browser/ui/views/location_bar/cookie_controls (3/x)"
    This reverts commit 5027ae1ee4e61bfea1f3d7d019e0c3f4020ed272.
    
    Reason for revert: Introduced build problem at https://ci.chromium.org/ui/p/chromium/builders/ci/Centipede%20High%20End%20Upload%20Linux%20ASan/46530/overview
    
    Bug: 434882143
    Original change's description:
    > Componentize chrome/browser/ui/views/location_bar/cookie_controls (3/x)
    >
    > Extract cookie_controls from the monolithic //chrome/browser/ui:ui
    > target into its own modularized BUILD.gn file with separate header and
    > implementation targets.
    >
    > The following GN targets are added:
    >  - c/b/ui/views/location_bar/cookie_controls:cookie_controls
    >  - c/b/ui/views/location_bar/cookie_controls:impl
    >  - c/b/ui/views/location_bar/cookie_controls:browser_tests
    >  - c/b/ui/views/location_bar/cookie_controls:interactive_ui_tests
    >  - c/b/ui/views/location_bar/cookie_controls:unit_tests
    >
    > Bug: 434882143
    > Change-Id: I8deab8a3b9069140263599de62e00d84ea1ee662
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6814031
    > Auto-Submit: Tiago Vignatti <tvignatti@igalia.com>
    > Reviewed-by: Darryl James <dljames@chromium.org>
    > Reviewed-by: Antonio Gomes <tonikitoo@igalia.com>
    > Commit-Queue: Darryl James <dljames@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1500968}
    
    Bug: 434882143
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: If052bb6a4cf95ee2457e83f43a89bb2a0d7188ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6846404
    Auto-Submit: Morten Stenshorne <mstensho@chromium.org>
    Owners-Override: Morten Stenshorne <mstensho@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1501262}

--- chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_content_view.h
+++ chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_content_view.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_VIEWS_LOCATION_BAR_COOKIE_CONTROLS_COOKIE_CONTROLS_CONTENT_VIEW_H_
 
 #include "base/memory/raw_ptr.h"
+#include "chrome/browser/ui/views/controls/rich_hover_button.h"
 #include "components/content_settings/core/common/cookie_controls_state.h"
 #include "ui/base/interaction/element_identifier.h"
 #include "ui/base/metadata/metadata_header_macros.h"
@@ -14,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/image_view.h"
 #include "ui/views/view.h"
 
-class RichHoverButton;
 class RichControlsContainerView;
 namespace views {
 class Label;
