5067b6dd45b7c44db7cc3daa146d7011aadb2197
EmiliaPaz <emiliapaz@chromium.org>
2025-08-14 16:27:24 -0700

    [Extensions UI] Change ExtensionPermissionsView constructor
    ExtensionPermissionsView holds permissions information of an
    extension. Instead of having two ways to populate it (AddItem or
    AddPermissions), have the constructor receive the permissions. This is
    preferred because:
      a. prevents having the view populated twice
      b. easier construction calls
      c. bonus - allows for easier refactor on next CL
    
    Bug: 438576283
    Change-Id: Ibd98be158b32aaa6e486d1e3c9f778aa25e6e6dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6851879
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1501682}

--- chrome/browser/ui/views/extensions/extension_permissions_view.cc
+++ chrome/browser/ui/views/extensions/extension_permissions_view.cc
@@ -13,11 +13,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/label.h"
 #include "ui/views/layout/box_layout.h"
 
-ExtensionPermissionsView::ExtensionPermissionsView() {
+ExtensionPermissionsView::ExtensionPermissionsView(
+    const extensions::InstallPromptPermissions& permissions) {
   SetLayoutManager(std::make_unique<views::BoxLayout>(
       views::BoxLayout::Orientation::kVertical, gfx::Insets(),
       ChromeLayoutProvider::Get()->GetDistanceMetric(
           views::DISTANCE_RELATED_CONTROL_VERTICAL)));
+
+  for (size_t i = 0; i < permissions.permissions.size(); ++i) {
+    AddItem(permissions.permissions.at(i), permissions.details.at(i));
+  }
 }
 
 void ExtensionPermissionsView::AddItem(
@@ -35,17 +40,6 @@ void ExtensionPermissionsView::AddItem(
     details_container.push_back(permission_details);
     AddChildView(std::make_unique<ExpandableContainerView>(details_container));
   }
-}
-
-void ExtensionPermissionsView::AddPermissions(
-    const extensions::InstallPromptPermissions& permissions) {
-  for (size_t i = 0; i < permissions.permissions.size(); ++i) {
-    AddItem(permissions.permissions[i], permissions.details[i]);
-  }
-}
-
-void ExtensionPermissionsView::ChildPreferredSizeChanged(views::View* child) {
-  PreferredSizeChanged();
 }
 
 BEGIN_METADATA(ExtensionPermissionsView)
