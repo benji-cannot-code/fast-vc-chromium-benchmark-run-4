9e95cbbff8f03bd1ab783f2587237208d7fa7c7a
Noémie St-Onge <noemies@google.com>
2025-08-15 07:24:49 -0700

    [iOSLiquidGlass] Fix expanded view header top padding
    In portrait mode, the system's view that's driving the presentation of
    the expanded view is a couple pixels taller than the expanded view on
    iOS 26, see https://screenshot.googleplex.com/AKzEgrBs5DfToAP.png.
    
    This consequently increases the padding at the top of the expanded view
    content. To counter this, this CL sets an offset to the top constraint
    of the expanded view's header.
    
    Before:
    - Portrait: https://screenshot.googleplex.com/Af7w6gtw8Barxk9.png
    - Landscape: https://screenshot.googleplex.com/8AqobgH2TFrfiUn.png
    
    After:
    - Portrait: https://screenshot.googleplex.com/8Vs9m5fRueJdxd2.png
    - Landscape: https://screenshot.googleplex.com/6rY6yHiFUwcazjk.png
    
    Mocks:
    https://www.figma.com/design/xH8lYMwzSzW6fGYEaxKJQ8/iOS-26---Bling?node-id=337-49013&t=l8gvLGJ55T7zNEZS-4
    
    Fixed: 436874801
    Change-Id: Ia84a3b914e5f93e1f01feb2fdf8c867c88214308
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6850386
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1501883}

--- ios/chrome/browser/autofill/ui_bundled/manual_fill/expanded_manual_fill_view_controller.mm
+++ ios/chrome/browser/autofill/ui_bundled/manual_fill/expanded_manual_fill_view_controller.mm
@@ -104,6 +104,19 @@ UIImageSymbolConfiguration* GetCloseButtonSymbolConfiguration() {
                            scale:UIImageSymbolScaleMedium];
 }
 
+// Returns the constant to apply to the header view top constraint.
+CGFloat GetHeaderViewTopConstraintConstant(bool is_compact_height) {
+#if defined(__IPHONE_26_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_26_0
+  if (@available(iOS 26, *)) {
+    if (!is_compact_height) {
+      return -kHeaderViewTopPadding;
+    }
+  }
+#endif
+
+  return kHeaderViewTopPadding;
+}
+
 }  // namespace
 
 @interface ExpandedManualFillViewController ()
@@ -197,9 +210,12 @@ UIImageSymbolConfiguration* GetCloseButtonSymbolConfiguration() {
       [self createSegmentedControlAndSelectDataType:_initialDataType];
   _headerViewHeightConstraint = [_headerView.heightAnchor
       constraintEqualToConstant:kHeaderViewHeightWideLayout];
+  _headerViewTopConstraint =
+      [_headerView.topAnchor constraintEqualToAnchor:self.view.topAnchor];
 
   [self setUpHeaderView:_headerView
       headerViewHeightConstraint:_headerViewHeightConstraint
+         headerViewTopConstraint:_headerViewTopConstraint
                       chromeLogo:_chromeLogo
                      closeButton:_closeButton
                 segmentedControl:_segmentedControl
@@ -213,9 +229,6 @@ UIImageSymbolConfiguration* GetCloseButtonSymbolConfiguration() {
   _headerViewTrailingConstraint = [_headerView.trailingAnchor
       constraintEqualToAnchor:self.view.safeAreaLayoutGuide.trailingAnchor
                      constant:-kHeaderViewHorizontalPadding];
-  _headerViewTopConstraint =
-      [_headerView.topAnchor constraintEqualToAnchor:self.view.topAnchor
-                                            constant:kHeaderViewTopPadding];
   _headerViewPopoverTopConstraint = [_headerView.topAnchor
       constraintEqualToAnchor:self.view.topAnchor
                      constant:kHeaderViewPopoverTopPadding];
@@ -456,13 +469,17 @@ UIImageSymbolConfiguration* GetCloseButtonSymbolConfiguration() {
 // Sets up the header view depending on the device's orientation.
 - (void)setUpHeaderView:(UIView*)headerView
     headerViewHeightConstraint:(NSLayoutConstraint*)headerViewHeightConstraint
+       headerViewTopConstraint:(NSLayoutConstraint*)headerViewTopConstraint
                     chromeLogo:(UIImageView*)chromeLogo
                    closeButton:(UIButton*)closeButton
               segmentedControl:(UISegmentedControl*)segmentedControl
                  headerTopView:(UIView*)headerTopView {
   // If the vertical size class is compact, apply the wide layout. Otherwise,
   // apply the narrow layout.
-  if (IsCompactHeight(self)) {
+  bool isCompactHeight = IsCompactHeight(self);
+  _headerViewTopConstraint.constant =
+      GetHeaderViewTopConstraintConstant(isCompactHeight);
+  if (isCompactHeight) {
     [headerView addSubview:chromeLogo];
     [headerView addSubview:closeButton];
     [headerView addSubview:segmentedControl];
@@ -547,6 +564,7 @@ UIImageSymbolConfiguration* GetCloseButtonSymbolConfiguration() {
 // Resets the header view. Called when a layout change is needed.
 - (void)resetHeaderView:(UIView*)headerView
     headerViewHeightConstraint:(NSLayoutConstraint*)headerViewHeightConstraint
+       headerViewTopConstraint:(NSLayoutConstraint*)headerViewTopConstraint
                     chromeLogo:(UIImageView*)chromeLogo
                    closeButton:(UIButton*)closeButton
               segmentedControl:(UISegmentedControl*)segmentedControl
@@ -559,6 +577,7 @@ UIImageSymbolConfiguration* GetCloseButtonSymbolConfiguration() {
 
   [self setUpHeaderView:headerView
       headerViewHeightConstraint:headerViewHeightConstraint
+         headerViewTopConstraint:headerViewTopConstraint
                       chromeLogo:chromeLogo
                      closeButton:closeButton
                 segmentedControl:segmentedControl
@@ -596,6 +615,7 @@ UIImageSymbolConfiguration* GetCloseButtonSymbolConfiguration() {
 - (void)resetHeaderViewOnTraitChange {
   [self resetHeaderView:_headerView
       headerViewHeightConstraint:_headerViewHeightConstraint
+         headerViewTopConstraint:_headerViewTopConstraint
                       chromeLogo:_chromeLogo
                      closeButton:_closeButton
                 segmentedControl:_segmentedControl
