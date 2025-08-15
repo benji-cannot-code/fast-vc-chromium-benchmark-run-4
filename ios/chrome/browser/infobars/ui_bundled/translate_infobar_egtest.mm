63bd4a5e5a5ef4cd37f663fe031ca5909b3bb19d
Noémie St-Onge <noemies@google.com>
2025-08-15 08:50:19 -0700

    [iOS] Fix testTranslateInReaderMode on ios-fieldtrial-rel
    Failure example:
    https://ci.chromium.org/ui/p/chromium/builders/ci/ios-fieldtrial-rel/48114/overview
    
    Bug: 430489596
    Change-Id: I220678fd6bb39e9975bc45f77903ea1ee7f7d0c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6849433
    Reviewed-by: Ernesto Izquierdo Clua <eic@google.com>
    Owners-Override: Noémie St-Onge <noemies@google.com>
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1501939}

--- ios/chrome/browser/infobars/ui_bundled/translate_infobar_egtest.mm
+++ ios/chrome/browser/infobars/ui_bundled/translate_infobar_egtest.mm
@@ -299,6 +299,11 @@ void TestResponseProvider::GetLanguageResponse(
 - (AppLaunchConfiguration)appConfigurationForTestCase {
   AppLaunchConfiguration config;
   config.features_enabled.push_back(kEnableReaderModeTranslation);
+
+  if ([self isRunningTest:@selector(testTranslateInReaderMode)]) {
+    config.features_enabled.push_back(kEnableReaderMode);
+  }
+
   return config;
 }
 
