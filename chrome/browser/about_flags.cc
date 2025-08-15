335bdb05a12d727620edbe033ceb0cdbe54323cb
viplavkadam <viplavkadam@google.com>
2025-08-15 14:13:13 -0700

    [BNPL][Clank] Fix to show BNPL flags
    This change fixes an issue where the BNPL feature flags for
    Clank were not visible. Although the flags were added in
    CL/6681542, they did not appear in the UI. This fix ensures
    they are now displayed correctly.
    Before:
    http://screencast/cast/NTA3Mjc0NzY2MjE0NzU4NHxkNTA0YzdlYS05YQ
    After:
    http://screencast/cast/NTgyNjgxODE5MDg2ODQ4MHxmM2M0M2FmMC0yMA
    
    Bug: b:438538911, 430575808
    Change-Id: Ia52be422a289ac57a7f63878083ce7a301d11486
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6847543
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Commit-Queue: Viplav Kadam <viplavkadam@google.com>
    Cr-Commit-Position: refs/heads/main@{#1502129}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -11614,13 +11614,14 @@ const FeatureEntry kFeatureEntries[] = {
     BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_ANDROID)
     {"autofill-enable-buy-now-pay-later",
      flag_descriptions::kAutofillEnableBuyNowPayLaterName,
-     flag_descriptions::kAutofillEnableBuyNowPayLaterDescription, kOsDesktop,
+     flag_descriptions::kAutofillEnableBuyNowPayLaterDescription,
+     kOsDesktop | kOsAndroid,
      FEATURE_VALUE_TYPE(autofill::features::kAutofillEnableBuyNowPayLater)},
 
     {"autofill-enable-buy-now-pay-later-syncing",
      flag_descriptions::kAutofillEnableBuyNowPayLaterSyncingName,
      flag_descriptions::kAutofillEnableBuyNowPayLaterSyncingDescription,
-     kOsDesktop,
+     kOsDesktop | kOsAndroid,
      FEATURE_VALUE_TYPE(
          autofill::features::kAutofillEnableBuyNowPayLaterSyncing)},
 #endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) ||
@@ -12120,7 +12121,7 @@ const FeatureEntry kFeatureEntries[] = {
     {"autofill-enable-amount-extraction-desktop",
      flag_descriptions::kAutofillEnableAmountExtractionDesktopName,
      flag_descriptions::kAutofillEnableAmountExtractionDesktopDescription,
-     kOsDesktop,
+     kOsDesktop | kOsAndroid,
      FEATURE_VALUE_TYPE(
          autofill::features::kAutofillEnableAmountExtractionDesktop)},
 #endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) ||
@@ -12142,7 +12143,7 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kAutofillEnableAmountExtractionAllowlistDesktopName,
      flag_descriptions::
          kAutofillEnableAmountExtractionAllowlistDesktopDescription,
-     kOsDesktop,
+     kOsDesktop | kOsAndroid,
      FEATURE_VALUE_TYPE(
          autofill::features::kAutofillEnableAmountExtractionAllowlistDesktop)},
 #endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) ||
@@ -12830,7 +12831,7 @@ const FeatureEntry kFeatureEntries[] = {
     {"autofill-enable-buy-now-pay-later-for-klarna",
      flag_descriptions::kAutofillEnableBuyNowPayLaterForKlarnaName,
      flag_descriptions::kAutofillEnableBuyNowPayLaterForKlarnaDescription,
-     kOsDesktop,
+     kOsDesktop | kOsAndroid,
      FEATURE_VALUE_TYPE(
          autofill::features::kAutofillEnableBuyNowPayLaterForKlarna)},
 #endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) ||
@@ -12921,7 +12922,7 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kAutofillEnableBuyNowPayLaterForExternallyLinkedName,
      flag_descriptions::
          kAutofillEnableBuyNowPayLaterForExternallyLinkedDescription,
-     kOsDesktop,
+     kOsDesktop | kOsAndroid,
      FEATURE_VALUE_TYPE(
          autofill::features::kAutofillEnableBuyNowPayLaterForExternallyLinked)},
 #endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) ||
