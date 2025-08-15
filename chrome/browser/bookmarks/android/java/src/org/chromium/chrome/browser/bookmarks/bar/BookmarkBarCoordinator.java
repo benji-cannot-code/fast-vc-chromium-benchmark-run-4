94c2f0f278392a2b858956e8d9aa30c0c2cff00e
Eleanor Lee <eleanorlee@google.com>
2025-08-14 21:08:03 -0700

    [Bookmarks Bar] Make the anchored popup window size dynamic
    After: https://screencast.googleplex.com/cast/NTkzMDgyMDk1ODE1ODg0OHw3NGI5YTQ5NC04Yw
    
    Based on the finalized UX discussions:
    
    1.
    Min height: 182 dp (or the equivalent of 2 bookmarks plus the header).
    Max height: Extend the menu as far as we can go vertically and leave a 48 dp gap with the bottom of the window bound.
    Max width = 400dp
    Min width = No set min width, width = Math.min(windowWidth - 48dp, 400dp)
    
    
    2. Added an empty state ("No bookmarks" centered vertically & horizontally) text view.
    
    Bug: 430044890
    Change-Id: I41e5f714a858c91cc7caceeaa27c65c09d7e09d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6832189
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Commit-Queue: Eleanor Lee <eleanorlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1501778}

--- chrome/browser/bookmarks/android/java/src/org/chromium/chrome/browser/bookmarks/bar/BookmarkBarCoordinator.java
+++ chrome/browser/bookmarks/android/java/src/org/chromium/chrome/browser/bookmarks/bar/BookmarkBarCoordinator.java
@@ -11,6 +11,7 @@ import static org.chromium.ui.accessibility.KeyboardFocusUtil.setFocus;
 import static org.chromium.ui.accessibility.KeyboardFocusUtil.setFocusOnFirstFocusableDescendant;
 
 import android.app.Activity;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +21,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import org.chromium.base.Callback;
 import org.chromium.base.supplier.ObservableSupplier;
+import org.chromium.base.supplier.Supplier;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.browser.bookmarks.BookmarkManagerOpener;
@@ -116,12 +118,19 @@ public class BookmarkBarCoordinator
         itemsContainer.getRecycledViewPool().setMaxRecycledViews(BookmarkBarUtils.ViewType.ITEM, 0);
         itemsContainer.setItemViewCacheSize(0);
 
+        Supplier<Pair<Integer, Integer>> controlsHeightSupplier =
+                () ->
+                        new Pair<>(
+                                mBrowserControlsStateProvider.getTopControlsHeight(),
+                                mBrowserControlsStateProvider.getBottomControlsHeight());
+
         // Bind view/model for bookmark bar and instantiate mediator.
         final var model = new PropertyModel.Builder(BookmarkBarProperties.ALL_KEYS).build();
         mMediator =
                 new BookmarkBarMediator(
                         activity,
                         allBookmarksButtonModel,
+                        controlsHeightSupplier,
                         itemsModel,
                         mBookmarkBarItemsLayoutManager.getItemsOverflowSupplier(),
                         model,
@@ -251,6 +260,9 @@ public class BookmarkBarCoordinator
         // we want to hide the Android widgets (which are controlled by the Mediator). We do not
         // also set the sceneLayer visibility here because we want that to be what is shown.
         mMediator.setVisibility(mBrowserControlsStateProvider.getTopControlOffset() == 0);
+        mMediator.onBrowserControlsChanged(
+                mBrowserControlsStateProvider.getTopControlsHeight(),
+                mBrowserControlsStateProvider.getBottomControlsHeight());
     }
 
     @Override
@@ -261,6 +273,8 @@ public class BookmarkBarCoordinator
         // when calculating top margin in order to bottom align the bookmark bar relative to other
         // top browser controls.
         mMediator.setTopMargin(topControlsHeight - getTopControlHeight());
+        mMediator.onBrowserControlsChanged(
+                topControlsHeight, mBrowserControlsStateProvider.getBottomControlsHeight());
     }
 
     // Private methods:
