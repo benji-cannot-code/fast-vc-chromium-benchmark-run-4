53804b1eeea7de410f984e155691061d8bc7bcb1
Chris Davis <chrdavis@microsoft.com>
2025-08-15 12:30:22 -0700

    Fix static initializer for lock in ort environment.cc
    Small change to fix a static initializer in ort environment.  Change to
    on demand magic static.
    
    Bug: 438884258
    Change-Id: I30537b12e9195d427481627f9297e27bf17460f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6854323
    Commit-Queue: Chris Davis <chrdavis@microsoft.com>
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1502068}

--- services/webnn/ort/environment.cc
+++ services/webnn/ort/environment.cc
@@ -168,7 +168,7 @@ void ORT_API_CALL OrtCustomLoggingFunction(void* /*param*/,
 // static
 base::expected<scoped_refptr<Environment>, std::string>
 Environment::GetInstance(const gpu::GPUInfo& gpu_info) {
-  base::AutoLock auto_lock(lock_);
+  base::AutoLock auto_lock(GetLock());
   if (instance_) {
     return base::WrapRefCounted(instance_);
   }
@@ -267,7 +267,7 @@ void Environment::AddRef() const {
 }
 
 void Environment::Release() const {
-  base::AutoLock auto_lock(lock_);
+  base::AutoLock auto_lock(GetLock());
   if (base::subtle::RefCountedThreadSafeBase::Release()) {
     ANALYZER_SKIP_THIS_PATH();
     CHECK_EQ(instance_, this);
@@ -310,7 +310,11 @@ bool Environment::IsExternalDataSupported(mojom::Device device_type) const {
   return true;
 }
 
-base::Lock Environment::lock_;
+// static
+base::Lock& Environment::GetLock() {
+  static base::NoDestructor<base::Lock> lock;
+  return *lock;
+}
 
 raw_ptr<Environment> Environment::instance_ = nullptr;
 
