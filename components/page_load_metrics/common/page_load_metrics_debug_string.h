9fe2dec3cd6521431282209f5f7a44e91d386b2b
Johannes Henkel <johannes@chromium.org>
2025-08-15 09:21:16 -0700

    [soft navs] Add DebugString routines for mojom::PaintTiming.
    Also a small fix (typo) for the IsEmpty routine for SoftNavigationMetrics.
    
    Also a very simple unittest.
    
    Change-Id: I75624eef2a8c9b02ecc9bde0761320260fcbe8ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6852735
    Commit-Queue: Johannes Henkel <johannes@chromium.org>
    Reviewed-by: Michal Mocny <mmocny@chromium.org>
    Reviewed-by: Scott Haseley <shaseley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1501952}

--- components/page_load_metrics/common/page_load_metrics_debug_string.h
+++ components/page_load_metrics/common/page_load_metrics_debug_string.h
@@ -12,10 +12,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace page_load_metrics {
 
-std::string DebugString(
-    const mojom::LcpResourceLoadTimings& resource_load_timings);
+std::string DebugString(const mojom::DocumentTiming& timing);
+
+std::string DebugString(const mojom::LcpResourceLoadTimings& timings);
 
 std::string DebugString(const mojom::LargestContentfulPaintTiming& lcp);
+
+std::string DebugString(const mojom::PaintTiming& timing);
+
+std::string DebugString(const mojom::ParseTiming& timing);
+
+std::string DebugString(const mojom::InteractiveTiming& timing);
+
+std::string DebugString(const mojom::DomainLookupTiming& timing);
+
+std::string DebugString(const mojom::PageLoadTiming& timing);
+
+std::string DebugString(const mojom::BackForwardCacheTiming& timing);
 
 std::string DebugString(
     const mojom::SoftNavigationMetrics& soft_navigation_metrics);
