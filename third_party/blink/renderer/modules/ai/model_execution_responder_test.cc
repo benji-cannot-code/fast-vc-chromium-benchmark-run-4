0b40475907b5ecfb27a234f852bff95134baf58c
Queenie Zhang <queeniezhang@google.com>
2025-08-14 18:04:04 -0700

    Proofreader API: Take abort signal for Proofreader.proofread()
    Change-Id: Ic5afcc4dd97ac6f456e09afd014aa4f5124db099
    Bug: 403313556
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6645150
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Commit-Queue: Mike Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1501722}

--- third_party/blink/renderer/modules/ai/model_execution_responder_test.cc
+++ third_party/blink/renderer/modules/ai/model_execution_responder_test.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/abort_controller.h"
 #include "third_party/blink/renderer/core/fetch/readable_stream_bytes_consumer.h"
 #include "third_party/blink/renderer/modules/ai/ai_metrics.h"
+#include "third_party/blink/renderer/modules/ai/ai_utils.h"
 #include "third_party/blink/renderer/platform/bindings/v8_binding.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/testing/task_environment.h"
@@ -67,20 +68,26 @@ TEST(CreateModelExecutionResponder, Simple) {
   base::RunLoop complete_runloop;
   base::RunLoop overflow_runloop;
   auto pending_remote = CreateModelExecutionResponder(
-      script_state, /*signal=*/nullptr, resolver,
+      script_state, /*signal=*/nullptr,
       blink::scheduler::GetSequencedTaskRunnerForTesting(),
       AIMetrics::AISessionType::kLanguageModel,
       /*complete_callback=*/
       blink::BindOnce(
           [](uint64_t expected_tokens, base::RunLoop* runloop,
+             ScriptPromiseResolver<IDLString>* resolver, const String& response,
              mojom::blink::ModelExecutionContextInfoPtr context_info) {
             EXPECT_TRUE(context_info);
             EXPECT_EQ(context_info->current_tokens, expected_tokens);
+            ResolvePromiseOnCompletion(resolver, response,
+                                       std::move(context_info));
             runloop->Quit();
           },
-          kTestTokenNumber, blink::Unretained(&complete_runloop)),
+          kTestTokenNumber, blink::Unretained(&complete_runloop),
+          WrapPersistent(resolver)),
       /*overflow_callback=*/overflow_runloop.QuitClosure(),
-      /*resolve_override_callback=*/base::NullCallback());
+      base::BindOnce(&RejectPromiseOnError, WrapPersistent(resolver)),
+      base::BindOnce(&RejectPromiseOnAbort, WrapPersistent(resolver), nullptr,
+                     WrapPersistent(script_state)));
 
   mojo::Remote<blink::mojom::blink::ModelStreamingResponder> responder(
       std::move(pending_remote));
@@ -113,12 +120,14 @@ TEST(CreateModelExecutionResponder, ErrorPermissionDenied) {
       MakeGarbageCollected<ScriptPromiseResolver<IDLString>>(script_state);
   auto promise = resolver->Promise();
   auto pending_remote = CreateModelExecutionResponder(
-      script_state, /*signal=*/nullptr, resolver,
+      script_state, /*signal=*/nullptr,
       blink::scheduler::GetSequencedTaskRunnerForTesting(),
       AIMetrics::AISessionType::kLanguageModel,
-      /*complete_callback=*/base::DoNothing(),
+      base::BindOnce(&ResolvePromiseOnCompletion, WrapPersistent(resolver)),
       /*overflow_callback=*/base::DoNothing(),
-      /*resolve_override_callback=*/base::NullCallback());
+      base::BindOnce(&RejectPromiseOnError, WrapPersistent(resolver)),
+      base::BindOnce(&RejectPromiseOnAbort, WrapPersistent(resolver), nullptr,
+                     WrapPersistent(script_state)));
 
   mojo::Remote<blink::mojom::blink::ModelStreamingResponder> responder(
       std::move(pending_remote));
@@ -151,12 +160,15 @@ TEST(CreateModelExecutionResponder, AbortWithoutResponse) {
       MakeGarbageCollected<ScriptPromiseResolver<IDLString>>(script_state);
   auto promise = resolver->Promise();
   auto pending_remote = CreateModelExecutionResponder(
-      script_state, controller->signal(), resolver,
+      script_state, controller->signal(),
       blink::scheduler::GetSequencedTaskRunnerForTesting(),
       AIMetrics::AISessionType::kLanguageModel,
-      /*complete_callback=*/base::DoNothing(),
+      base::BindOnce(&ResolvePromiseOnCompletion, WrapPersistent(resolver)),
       /*overflow_callback=*/base::DoNothing(),
-      /*resolve_override_callback=*/base::NullCallback());
+      base::BindOnce(&RejectPromiseOnError, WrapPersistent(resolver)),
+      base::BindOnce(&RejectPromiseOnAbort, WrapPersistent(resolver),
+                     WrapPersistent(controller->signal()),
+                     WrapPersistent(script_state)));
 
   controller->abort(scope.GetScriptState());
 
@@ -188,12 +200,15 @@ TEST(CreateModelExecutionResponder, AbortAfterResponse) {
       MakeGarbageCollected<ScriptPromiseResolver<IDLString>>(script_state);
   auto promise = resolver->Promise();
   auto pending_remote = CreateModelExecutionResponder(
-      script_state, controller->signal(), resolver,
+      script_state, controller->signal(),
       blink::scheduler::GetSequencedTaskRunnerForTesting(),
       AIMetrics::AISessionType::kLanguageModel,
-      /*complete_callback=*/base::DoNothing(),
+      base::BindOnce(&ResolvePromiseOnCompletion, WrapPersistent(resolver)),
       /*overflow_callback=*/base::DoNothing(),
-      /*resolve_override_callback=*/base::NullCallback());
+      base::BindOnce(&RejectPromiseOnError, WrapPersistent(resolver)),
+      base::BindOnce(&RejectPromiseOnAbort, WrapPersistent(resolver),
+                     WrapPersistent(controller->signal()),
+                     WrapPersistent(script_state)));
 
   mojo::Remote<blink::mojom::blink::ModelStreamingResponder> responder(
       std::move(pending_remote));
---
+++ third_party/blink/web_tests/external/wpt/ai/proofreader/proofreader-abort.tentative.https.window.js
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: title=Proofreader Abort
+// META: script=/resources/testdriver.js
+// META: script=../resources/util.js
+// META: timeout=long
+
+'use strict';
+
+promise_test(async t => {
+  await testAbortPromise(t, signal => {
+    return createProofreader({signal: signal});
+  });
+}, 'Aborting Proofreader.create()');
+
+promise_test(async t => {
+  const proofreader = await createProofreader();
+  await testAbortPromise(t, signal => {
+    return proofreader.proofread(kTestPrompt, { signal: signal });
+  });
+}, 'Aborting Proofreader.proofread()');
+
