e753c5f8943cf817c89533841843a20121d9e4a8
Qiu Jianlin <jianlin.qiu@intel.com>
2025-08-15 00:21:26 -0700

    [DX12 Enc] Reserve 1 DPB slot for H.264 short term reference.
    We use long term references for H.264 encoding exclusively in D3D12 VEA
    so from encoder perspective it seems we only need to configure
    sps.max_num_ref_frames to the number of maximum long term references we
    will be using. This usually works when on decoder side there is no frame
    dropping. However, with temporal scalability, receiver may only decode
    the base layer frames and there would be potential frame_num gap. We
    must reserve one extra DPB slot for the stream, so that the sliding
    window picture marking process can run on the non-existing pictures
    created on decoder side to fill the frame_num gap.
    
    Bug: 40275246
    Change-Id: If2dee5ef5c5ea6842f1b3d326649a791b88bdee3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6849218
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Jianlin Qiu <jianlin.qiu@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1501813}

--- media/gpu/windows/d3d12_video_encode_delegate_unittest.h
+++ media/gpu/windows/d3d12_video_encode_delegate_unittest.h
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MEDIA_GPU_WINDOWS_D3D12_VIDEO_ENCODE_DELEGATE_UNITTEST_H_
 #define MEDIA_GPU_WINDOWS_D3D12_VIDEO_ENCODE_DELEGATE_UNITTEST_H_
 
+#include <optional>
+
+#include "base/test/scoped_feature_list.h"
 #include "media/gpu/windows/d3d12_video_encode_delegate.h"
 #include "media/gpu/windows/d3d12_video_encoder_wrapper.h"
 #include "media/gpu/windows/d3d12_video_processor_wrapper.h"
@@ -84,7 +87,12 @@ class D3D12VideoEncodeDelegateTestBase : public ::testing::Test {
       const gfx::Size& size,
       VideoPixelFormat format) const;
 
+  void EnableFeature(const base::Feature& feature);
+
+  void DisableFeature(const base::Feature& feature);
+
   std::unique_ptr<D3D12VideoEncodeDelegate> encoder_delegate_;
+  std::optional<base::test::ScopedFeatureList> scoped_feature_list_;
 };
 
 }  // namespace media
