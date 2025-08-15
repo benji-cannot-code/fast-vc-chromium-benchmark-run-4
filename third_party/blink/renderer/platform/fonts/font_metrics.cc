1f60bcb3ed156c6f2e5a10d1f0a4744ed0e4eee4
Dominik Röttsches <drott@chromium.org>
2025-08-15 03:23:22 -0700

    Clamp pixel size calculation in FontMetrics.
    Use `ClampTo<int>` to an integer to ensure the value is within the valid range for `int`.
    
    Fixed: chromum:435218001
    Change-Id: I76a3c4fda999ed82fade881ee916ccd62072313a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6853818
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Auto-Submit: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1501852}

--- third_party/blink/renderer/platform/fonts/font_metrics.cc
+++ third_party/blink/renderer/platform/fonts/font_metrics.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "third_party/blink/renderer/platform/fonts/font_platform_data.h"
 #include "third_party/blink/renderer/platform/fonts/vdmx_parser.h"
+#include "third_party/blink/renderer/platform/wtf/math_extras.h"
 #include "third_party/skia/include/core/SkFont.h"
 #include "third_party/skia/include/core/SkFontMetrics.h"
 #include "third_party/skia/include/core/SkTypeface.h"
@@ -73,7 +74,7 @@ void FontMetrics::AscentDescentWithHacks(
   // using FreeType.  With DirectWrite or CoreText, no bytecode hinting is ever
   // done.  This code should be pushed into FreeType (hinted font metrics).
   static const uint32_t kVdmxTag = SkSetFourByteTag('V', 'D', 'M', 'X');
-  int pixel_size = platform_data.size() + 0.5;
+  int pixel_size = ClampTo<int>(platform_data.size() + 0.5);
   // TODO(xiaochengh): How do we support ascent/descent override with VDMX?
   if (!ascent_override && !descent_override && !font.isForceAutoHinting() &&
       (font.getHinting() == SkFontHinting::kFull ||
