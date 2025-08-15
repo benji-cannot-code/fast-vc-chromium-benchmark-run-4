21b01d98d73512bb68f336780656873bdad3e000
Dana Fried <dfried@chromium.org>
2025-08-15 13:33:09 -0700

    Fix implementation of Browser::IsBrowserClosing()
    This is a blocker for the User Education 2.5 rollout; see attached bugs.
    
    Bug: 439032383, 439030167
    Change-Id: Id052284e66e99aa6f20a904ff7bf1278ff57cffc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6852657
    Auto-Submit: Dana Fried <dfried@chromium.org>
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1502101}

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -696,6 +696,10 @@ Browser::Browser(const CreateParams& params)
   // is initialized.
   features_->InitPostWindowConstruction(this);
 
+  // All initialization is complete; after this point, the browser should be on
+  // the browser list until it is marked for destruction.
+  is_initialized_ = true;
+
   BrowserList::AddBrowser(this);
 }
 
@@ -3792,6 +3796,7 @@ bool Browser::SupportsWindowFeatureImpl(WindowFeature feature,
 bool Browser::IsBrowserClosing() const {
   BrowserList* browser_list = BrowserList::GetInstance();
   const bool removed_from_browserlist =
+      is_initialized_ &&
       std::ranges::find_if(*browser_list, [this](Browser* browser) {
         return browser == this;
       }) == browser_list->end();
