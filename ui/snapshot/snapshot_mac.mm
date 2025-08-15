75ea3fcb33e90fb080def3f6fcbbc220c29b06a9
Avi Drissman <avi@chromium.org>
2025-08-15 07:40:17 -0700

    Remove emergency switch for SCK
    It's working reliably and it's here to stay.
    
    Bug: 40275728
    Change-Id: I3d15f1504072938f6a51962a0866678b6db138bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6840630
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1501906}

--- ui/snapshot/snapshot_mac.mm
+++ ui/snapshot/snapshot_mac.mm
@@ -19,14 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/image/image.h"
 #include "ui/snapshot/snapshot_mac.h"
 
-// The API that allows an app TCC-less access to its own windows is new in macOS
-// 14.4. While this has been tested extensively on 14.4 betas, because this is a
-// new API added in an OS dot release, have a "break in case of emergency" off-
-// switch.
-BASE_FEATURE(kUseScreenCaptureKitForSnapshots,
-             "UseScreenCaptureKitForSnapshots",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 namespace ui {
 
 namespace {
@@ -197,9 +189,7 @@ void GrabViewSnapshot(gfx::NativeView view,
                       GrabSnapshotImageCallback callback) {
   SnapshotAPI api = g_snapshot_api;
   if (api == SnapshotAPI::kUnspecified) {
-    if (base::mac::MacOSVersion() >= 14'04'00 &&
-        base::FeatureList::IsEnabled(kUseScreenCaptureKitForSnapshots) &&
-        !ShouldForceOldAPIUse()) {
+    if (base::mac::MacOSVersion() >= 14'04'00 && !ShouldForceOldAPIUse()) {
       api = SnapshotAPI::kNewAPI;
     } else {
       api = SnapshotAPI::kOldAPI;
