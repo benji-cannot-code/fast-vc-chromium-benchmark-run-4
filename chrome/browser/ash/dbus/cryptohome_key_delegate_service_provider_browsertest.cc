cd55e58c3b5deceb58915594a7166cf85a1df999
Elly <ellyjones@chromium.org>
2025-08-15 09:38:11 -0700

    ash/cryptohome: use crypto/sign in browser tests
    The crypto/sign API is a bit more ergonomic and also not deprecated.
    
    Bug: 372283556
    Change-Id: I4b540335af332bb54628938ea6c995765bf526e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6848691
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1501960}

--- chrome/browser/ash/dbus/cryptohome_key_delegate_service_provider_browsertest.cc
+++ chrome/browser/ash/dbus/cryptohome_key_delegate_service_provider_browsertest.cc
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/user_manager/user_names.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/test/browser_test.h"
-#include "crypto/signature_verifier.h"
+#include "crypto/sign.h"
 #include "dbus/message.h"
 #include "dbus/object_path.h"
 #include "extensions/common/features/simple_feature.h"
@@ -175,16 +175,15 @@ class CryptohomeKeyDelegateServiceProviderTest
 
   // Returns whether the given |signature| is a valid signature of the original
   // data.
-  bool IsSignatureValid(crypto::SignatureVerifier::SignatureAlgorithm algorithm,
+  bool IsSignatureValid(crypto::sign::SignatureKind algorithm,
                         const std::vector<uint8_t>& signature) const {
     const std::string spki =
         certificate_provider_extension()->GetCertificateSpki();
-    crypto::SignatureVerifier verifier;
-    if (!verifier.VerifyInit(algorithm, signature, base::as_byte_span(spki))) {
-      return false;
-    }
-    verifier.VerifyUpdate(base::as_byte_span(kDataToSign));
-    return verifier.VerifyFinal();
+    const auto pubkey = crypto::keypair::PublicKey::FromSubjectPublicKeyInfo(
+        base::as_byte_span(spki));
+    CHECK(pubkey.has_value());
+    return crypto::sign::Verify(algorithm, *pubkey,
+                                base::as_byte_span(kDataToSign), signature);
   }
 
   TestCertificateProviderExtension* certificate_provider_extension() {
@@ -223,8 +222,7 @@ IN_PROC_BROWSER_TEST_F(CryptohomeKeyDelegateServiceProviderTest,
   std::vector<uint8_t> signature;
   EXPECT_TRUE(CallSignatureChallengeKey(
       cryptohome::CHALLENGE_RSASSA_PKCS1_V1_5_SHA256, &signature));
-  EXPECT_TRUE(
-      IsSignatureValid(crypto::SignatureVerifier::RSA_PKCS1_SHA256, signature));
+  EXPECT_TRUE(IsSignatureValid(crypto::sign::RSA_PKCS1_SHA256, signature));
 }
 
 // Verifies that the ChallengeKey request with the PKCS #1 v1.5 SHA-1 algorithm
@@ -234,8 +232,7 @@ IN_PROC_BROWSER_TEST_F(CryptohomeKeyDelegateServiceProviderTest,
   std::vector<uint8_t> signature;
   EXPECT_TRUE(CallSignatureChallengeKey(
       cryptohome::CHALLENGE_RSASSA_PKCS1_V1_5_SHA1, &signature));
-  EXPECT_TRUE(
-      IsSignatureValid(crypto::SignatureVerifier::RSA_PKCS1_SHA1, signature));
+  EXPECT_TRUE(IsSignatureValid(crypto::sign::RSA_PKCS1_SHA1, signature));
 }
 
 // Verifies that the ChallengeKey request fails when the requested algorithm
