c5cc7f3195a249b445fecb09ae76fa5e7ce0f0ac
Rakina Zata Amni <rakina@chromium.org>
2025-08-14 22:02:13 -0700

    Copy overlay color on LocalFrame swap
    With RenderDocument, we'll change LocalFrames on every cross-document
    navigation. Frame color overlays are saved in LocalFrame, and thus
    needs to be carried over to the new LocalFrame to be preserved.
    This CL makes it so that we do that by saving the previous
    LocalFrame's  overlay color in OldDocumentInfoForCommit and applying
    it to the new LocalFrame.
    
    This is needed to make this test pass with RenderDocument enabled:
    compositing/visibility/overlays-persist-on-navigation.html
    
    Bug: 40615943
    Change-Id: Iea0cb5c277f63004366487ac78f4a2598ebae931
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6728715
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1501786}

--- third_party/blink/renderer/core/frame/local_frame.h
+++ third_party/blink/renderer/core/frame/local_frame.h
@@ -702,13 +702,9 @@ class CORE_EXPORT LocalFrame final
 
   void SetReducedAcceptLanguage(const AtomicString& reduced_accept_language);
 
-  // Overlays a color on top of this LocalFrameView if it is associated with
-  // the main frame. Should not have multiple consumers.
-  void SetMainFrameColorOverlay(SkColor color);
+  // Overlays a color on top of this LocalFrameView.
+  void SetFrameColorOverlay(SkColor color);
 
-  // Overlays a color on top of this LocalFrameView if it is associated with
-  // a subframe. Should not have multiple consumers.
-  void SetSubframeColorOverlay(SkColor color);
   void UpdateFrameColorOverlayPrePaint();
 
   void PaintFrameColorOverlay(GraphicsContext&);
@@ -880,7 +876,7 @@ class CORE_EXPORT LocalFrame final
   void SetBackgroundColorPaintImageGeneratorForTesting(
       BackgroundColorPaintImageGenerator* generator);
 
-  std::optional<SkColor> GetFrameOverlayColorForTesting() const;
+  std::optional<SkColor> GetFrameOverlayColor() const;
 
   // Returns a PendingRemote resolved via this frame's BrowserInterfaceBroker
   // for use when creating the PublicUrlManager instance in threaded worklets.
@@ -1036,8 +1032,6 @@ class CORE_EXPORT LocalFrame final
   // Consumes and returns the transient user activation state this frame, after
   // updating all other frames in the frame tree.
   bool ConsumeTransientUserActivation(UserActivationUpdateSource update_source);
-
-  void SetFrameColorOverlay(SkColor color);
 
   void DidFreeze();
   void DidResume();
