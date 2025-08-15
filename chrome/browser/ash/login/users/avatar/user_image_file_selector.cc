36a70ced1450d4eab92afd0a1648293f32a1b789
Antonio Gomes <tonikitoo@igalia.com>
2025-08-14 19:49:05 -0700

    Do not operate over Browser and chrome::Find* in user_image_file_selector.cc
    Instead use content::WebContents::GetTopLevelNativeWindow() directly.
    
    R=neis@chromium.org
    
    Bug: 436427339
    Change-Id: I32b4bf850684928821ef17411cc73ece31b1a11c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6848549
    Reviewed-by: Georg Neis <neis@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1501761}

--- chrome/browser/ash/login/users/avatar/user_image_file_selector.cc
+++ chrome/browser/ash/login/users/avatar/user_image_file_selector.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "base/path_service.h"
-#include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/chrome_select_file_policy.h"
 #include "chrome/common/chrome_paths.h"
@@ -85,8 +84,7 @@ void UserImageFileSelector::SelectFile(
 }
 
 gfx::NativeWindow UserImageFileSelector::GetBrowserWindow() {
-  Browser* browser = chrome::FindBrowserWithTab(web_ui_->GetWebContents());
-  return browser ? browser->window()->GetNativeWindow() : gfx::NativeWindow();
+  return web_ui_->GetWebContents()->GetTopLevelNativeWindow();
 }
 
 void UserImageFileSelector::FileSelected(const ui::SelectedFileInfo& file,
