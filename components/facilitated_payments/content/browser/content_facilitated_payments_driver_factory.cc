75c9cc2f2f450b06c7dfd5c787ad96e67e431d9a
Longsheng Yin <longsheng@google.com>
2025-08-15 15:15:02 -0700

    [Pix] Log pix pay flow exit reason when code was copied in iframe.
    Bug: b:420729447
    Change-Id: I2794b9a3211161393db459ba215d8e8f5286a427
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6843998
    Reviewed-by: Darwin Yang <darwinyang@chromium.org>
    Reviewed-by: Siddharth Shah <siashah@chromium.org>
    Reviewed-by: Olivia Saul <osaul@google.com>
    Commit-Queue: Longsheng Yin <longsheng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1502158}

--- components/facilitated_payments/content/browser/content_facilitated_payments_driver_factory.cc
+++ components/facilitated_payments/content/browser/content_facilitated_payments_driver_factory.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/facilitated_payments/content/browser/security_checker.h"
 #include "components/facilitated_payments/core/browser/facilitated_payments_client.h"
 #include "components/facilitated_payments/core/features/features.h"
+#include "components/facilitated_payments/core/metrics/facilitated_payments_metrics.h"
 #include "content/public/browser/navigation_handle.h"
 
 namespace payments::facilitated {
@@ -82,8 +83,12 @@ void ContentFacilitatedPaymentsDriverFactory::DidFinishNavigation(
 void ContentFacilitatedPaymentsDriverFactory::OnTextCopiedToClipboard(
     content::RenderFrameHost* render_frame_host,
     const std::u16string& copied_text) {
-  if (render_frame_host != render_frame_host->GetOutermostMainFrame() ||
-      !render_frame_host->IsActive()) {
+  if (render_frame_host != render_frame_host->GetOutermostMainFrame()) {
+    LogPixFlowExitedReason(PixFlowExitedReason::kPixCodeInIFrame);
+    return;
+  }
+  if (!render_frame_host->IsActive()) {
+    LogPixFlowExitedReason(PixFlowExitedReason::kFrameNotActive);
     return;
   }
 
