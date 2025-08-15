32013ef80b8fec38f454514be9dafb5bcc79d38d
Wangyang Dai <jludwy@gmail.com>
2025-08-15 12:06:11 -0700

    Move null check for web_contents before its usage
    On Android, if the previous app session had tabs that were not closed,
    the web_contents of those tabs will be null on the next launch until the
    tab is brought to the foreground by the user.
    
    This change moves the null check for web_contents to right before its
    usage to avoid null pointer exceptions.
    
    Change-Id: If1c6092daba1e46f40ba7487ffef689450ec11d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6852542
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1502057}

--- chrome/browser/extensions/api/tabs/tabs_api.cc
+++ chrome/browser/extensions/api/tabs/tabs_api.cc
@@ -815,6 +815,10 @@ ExtensionFunction::ResponseAction TabsQueryFunction::Run() {
       CHECK(tab);
       content::WebContents* web_contents = tab->GetContents();
 
+      if (!web_contents) {
+        continue;
+      }
+
 #if BUILDFLAG(IS_ANDROID)
       Profile* tab_profile =
           Profile::FromBrowserContext(web_contents->GetBrowserContext());
@@ -822,10 +826,6 @@ ExtensionFunction::ResponseAction TabsQueryFunction::Run() {
         continue;
       }
 #endif
-
-      if (!web_contents) {
-        continue;
-      }
 
       if (!MatchesBool(params->query_info.highlighted, tab->IsSelected())) {
         continue;
