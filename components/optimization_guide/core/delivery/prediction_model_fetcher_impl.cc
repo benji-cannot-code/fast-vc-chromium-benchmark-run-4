e27a3d70bd172f310e15a0f151d8efc328d003a3
Reilly Grant <reillyg@chromium.org>
2025-08-15 17:20:40 -0700

    Don't use std::optional in ModelsFetchedCallback
    This change simplifies the type used for passing the fetched model
    response. A nullptr now indicates the absence of a response, removing
    the need for the std::optional wrapper.
    
    Change-Id: Ib1c4ce212f7b0240f7e85cd71989614e2ef33492
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6854821
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1502214}

--- components/optimization_guide/core/delivery/prediction_model_fetcher_impl.cc
+++ components/optimization_guide/core/delivery/prediction_model_fetcher_impl.cc
@@ -59,7 +59,7 @@ bool PredictionModelFetcherImpl::FetchOptimizationGuideServiceModels(
 
   // If there are no models to request, do not make a GetModelsRequest.
   if (models_request_info.empty()) {
-    std::move(models_fetched_callback).Run(std::nullopt);
+    std::move(models_fetched_callback).Run(nullptr);
     return false;
   }
 
@@ -137,9 +137,8 @@ void PredictionModelFetcherImpl::HandleResponse(
     const std::string& get_models_response_data,
     int net_status,
     int response_code) {
-  std::unique_ptr<optimization_guide::proto::GetModelsResponse>
-      get_models_response =
-          std::make_unique<optimization_guide::proto::GetModelsResponse>();
+  auto get_models_response =
+      std::make_unique<optimization_guide::proto::GetModelsResponse>();
 
   if (response_code >= 0 && response_code <= net::HTTP_VERSION_NOT_SUPPORTED) {
     UMA_HISTOGRAM_ENUMERATION(
@@ -178,7 +177,7 @@ void PredictionModelFetcherImpl::HandleResponse(
       get_models_response->ParseFromString(get_models_response_data)) {
     std::move(models_fetched_callback_).Run(std::move(get_models_response));
   } else {
-    std::move(models_fetched_callback_).Run(std::nullopt);
+    std::move(models_fetched_callback_).Run(nullptr);
   }
 }
 
