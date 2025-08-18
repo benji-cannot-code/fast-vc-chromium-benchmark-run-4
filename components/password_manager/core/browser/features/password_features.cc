8f93566dfecb419ab52d90d7f2aea8fd0785e299
Ioana Pandele <ioanap@chromium.org>
2025-08-18 09:20:00 -0700

    Enable actor login feature
    Bug: 438398825
    Change-Id: I688621545139ef9f7a965821a897807f627c2c22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6851061
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1502748}

--- components/password_manager/core/browser/features/password_features.cc
+++ components/password_manager/core/browser/features/password_features.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace password_manager::features {
 #if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)  // Desktop
-BASE_FEATURE(kActorLogin, "ActorLogin", base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kActorLogin, "ActorLogin", base::FEATURE_ENABLED_BY_DEFAULT);
 #endif  // !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
 
 #if BUILDFLAG(IS_ANDROID)
