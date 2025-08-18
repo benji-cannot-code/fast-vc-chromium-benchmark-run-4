d316ed87aa19738a1f7e3b118eadd19b66c2c894
Dan Harrington <harringtond@chromium.org>
2025-08-18 13:57:21 -0700

    Disable some flaky win-asan glic-api tests
    Bug: 439218355
    Change-Id: I7cc194349625655b81f1ffcf9cde57e24038d56d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6858616
    Reviewed-by: Ahmed Moussa <ahmedmoussa@google.com>
    Auto-Submit: Dan Harrington <harringtond@chromium.org>
    Commit-Queue: Ahmed Moussa <ahmedmoussa@google.com>
    Cr-Commit-Position: refs/heads/main@{#1502943}

--- chrome/browser/glic/host/glic_api_browsertest.cc
+++ chrome/browser/glic/host/glic_api_browsertest.cc
@@ -937,6 +937,9 @@ IN_PROC_BROWSER_TEST_F(GlicApiTestWithOneTab,
 
 IN_PROC_BROWSER_TEST_F(GlicApiTestWithOneTab,
                        testGetContextFromFocusedTabWithAllRequestedData) {
+#if BUILDFLAG(IS_WIN) && defined(ADDRESS_SANITIZER)
+  GTEST_SKIP() << "skip timeout test for slow binary";
+#endif
   ExecuteJsTest();
 }
 
@@ -1462,7 +1465,8 @@ IN_PROC_BROWSER_TEST_F(GlicApiTestWithOneTab,
 }
 
 // TODO(b/431837630): Make this work on mac.
-#if BUILDFLAG(IS_MAC)
+// Win-asan is flaky.
+#if BUILDFLAG(IS_MAC) || (BUILDFLAG(IS_WIN) && defined(ADDRESS_SANITIZER))
 #define MAYBE_testFetchInactiveTabScreenshot \
   DISABLED_testFetchInactiveTabScreenshot
 #else
@@ -1480,7 +1484,8 @@ IN_PROC_BROWSER_TEST_F(GlicApiTestWithOneTab,
 }
 
 // TODO(b/431837630): Make this work on mac.
-#if BUILDFLAG(IS_MAC)
+// Win-asan is flaky.
+#if BUILDFLAG(IS_MAC) || (BUILDFLAG(IS_WIN) && defined(ADDRESS_SANITIZER))
 #define MAYBE_testFetchInactiveTabScreenshotWhileMinimized \
   DISABLED_testFetchInactiveTabScreenshotWhileMinimized
 #else
