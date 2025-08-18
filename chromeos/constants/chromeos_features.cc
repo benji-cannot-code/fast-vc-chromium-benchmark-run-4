d7db3cc2703035ffa6e0724dbf8a2fd862ffdf2e
Zoraiz Naeem <zoraiznaeem@chromium.org>
2025-08-18 16:03:46 -0700

    Disable background blur on low end devices on ChromeOS
    This change disables the background blur effect and its required
    background opacity on systems with 4GB or less of physical memory. This
    is an optimization, as the feature has a significant performance cost
    for a minimal perceptual impact on the user experience.
    
    Bug: b:408097362
    Change-Id: I8e5e6d9c42e37b7b2fd25c9c7b64b9554f6d2cc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6814444
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1503023}

--- chromeos/constants/chromeos_features.cc
+++ chromeos/constants/chromeos_features.cc
@@ -5,8 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/constants/chromeos_features.h"
 
+#include "base/byte_count.h"
 #include "base/feature_list.h"
 #include "base/metrics/field_trial_params.h"
+#include "base/system/sys_info.h"
 #include "chromeos_features.h"
 
 namespace chromeos::features {
@@ -575,7 +577,15 @@ bool IsRoundedWindowsEnabled() {
 }
 
 bool IsSystemBlurEnabled() {
-  return !base::FeatureList::IsEnabled(kDisableSystemBlur);
+  constexpr base::ByteCount kMinimumMemoryThreshold = base::GiB(4);  // 4GB
+  bool disable_blur =
+      base::SysInfo::AmountOfPhysicalMemory() <= kMinimumMemoryThreshold;
+  if (std::optional<bool> force_disable =
+          base::FeatureList::GetStateIfOverridden(kDisableSystemBlur)) {
+    disable_blur = force_disable.value();
+  }
+
+  return !disable_blur;
 }
 
 bool IsFeatureManagementHistoryEmbeddingEnabled() {
