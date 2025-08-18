ca3aeb00cc723fb48ba259a71165eea7beddf349
Daniel Rubery <drubery@chromium.org>
2025-08-18 09:06:41 -0700

    Define new Origin Trial flag for DBSC
    Due to change in the spec after the start of the first Origin Trial,
    we'd like to run one more Origin Trial. This CL defines the new flag and
    its integration.
    
    Bug: 438906412
    Change-Id: Id773859702396ab09c47c87d25c9ec3762fb0c80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6853856
    Reviewed-by: Emily Stark <estark@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1502740}

--- third_party/blink/common/origin_trials/navigation_origin_trial_features.cc
+++ third_party/blink/common/origin_trials/navigation_origin_trial_features.cc
@@ -23,6 +23,7 @@ bool FeatureEnabledForNavigation(blink::mojom::OriginTrialFeature feature) {
       blink::mojom::OriginTrialFeature::kOriginTrialsSampleAPINavigation,
       blink::mojom::OriginTrialFeature::kTextFragmentIdentifiers,
       blink::mojom::OriginTrialFeature::kDeviceBoundSessionCredentials,
+      blink::mojom::OriginTrialFeature::kDeviceBoundSessionCredentials2,
   };
   return base::Contains(kEnabledForNavigation, feature);
 }
