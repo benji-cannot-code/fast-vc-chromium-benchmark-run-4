3f9af3b489b101d5d83ba40f66e7b0ea081d12c2
David Sanders <dsanders11@ucsbalum.com>
2025-08-17 21:59:22 -0700

    Remove unused include in idl_types.h
    Should save ~30 MiB of compiler input size. Adds a missing include to
    fix the build.
    
    `IDLDate` was removed in
    https://chromium-review.googlesource.com/c/chromium/src/+/6075966 at
    which point this became an unused include.
    
    Bug: 40318405
    Change-Id: If19e8f9062a8a4b0b5683019050eac65cf2de852
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6854939
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#1502531}

--- third_party/blink/renderer/bindings/core/v8/idl_types_base.h
+++ third_party/blink/renderer/bindings/core/v8/idl_types_base.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_BINDINGS_CORE_V8_IDL_TYPES_BASE_H_
 #define THIRD_PARTY_BLINK_RENDERER_BINDINGS_CORE_V8_IDL_TYPES_BASE_H_
 
+#include <concepts>
+
 namespace blink {
 
 // This is the base type for all Web IDL types, such as the ones defined in
