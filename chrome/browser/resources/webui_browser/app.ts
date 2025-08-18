c19f32891768483753d8d5e8fcbd7e3dc10645d3
Paul Jensen <pauljensen@chromium.org>
2025-08-18 10:59:55 -0700

    webium: add profile and app menu buttons to search bar
    This adds the HTML/CSS/TS for these two buttons.  Their implementation
    in C++ is still NOTIMPLEMENTED().
    This also fixes up the looks of the search bar.
    
    Bug: 393144165
    Change-Id: I641884f74b97ff481ef75159af7a713cae71d9e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6849951
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Auto-Submit: Paul Jensen <pauljensen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1502822}

--- chrome/browser/resources/webui_browser/app.ts
+++ chrome/browser/resources/webui_browser/app.ts
@@ -80,6 +80,10 @@ export class WebuiBrowserAppElement extends CrLitElement implements
     BrowserProxy.getPageHandler().openAppMenu();
   }
 
+  protected onAvatarClick_(_: Event) {
+    BrowserProxy.getPageHandler().openProfileMenu();
+  }
+
   protected onMinimizeClick_(_: Event) {
     BrowserProxy.getPageHandler().minimize();
   }
