6ff3f89b7c0c44de4e4a1fc8f1899690b5ed568b
Chris Fredrickson <cfredric@chromium.org>
2025-08-18 08:27:29 -0700

    [Paint Preview] Use callback to clean up in-progress capture state
    `all_document_data_` entries are supposed to be cleaned up after the
    screenshot request fails/succeeds, per the
    [comment](https://crsrc.org/c/components/paint_preview/browser/paint_preview_client.h;drc=2550a5cfb96365e9e493b34c5d74cd5e5a6faf46;l=251-253).
    However, there are two code paths that invoke the callback (signalling
    that the capture is no longer in progress) but don't clean up stale
    entries:
    
    * https://crsrc.org/c/components/paint_preview/browser/paint_preview_client.cc;drc=3b1fde9febfcc601ee133d1d590345dfa6ca0ee8;l=503
    * https://crsrc.org/c/components/paint_preview/browser/paint_preview_client.cc;drc=3b1fde9febfcc601ee133d1d590345dfa6ca0ee8;l=515
    
    This CL fixes those codepaths (and makes it impossible to forget to
    clean up in future logic) by making cleanup part of
    `document_data.callback`'s execution.
    
    Change-Id: Ibfb4067365996bce3f731ee13f0139874177a537
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6854031
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1502703}

--- components/paint_preview/browser/paint_preview_client.h
+++ components/paint_preview/browser/paint_preview_client.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/unguessable_token.h"
 #include "components/paint_preview/common/capture_result.h"
-#include "components/paint_preview/common/mojom/paint_preview_recorder.mojom-shared.h"
 #include "components/paint_preview/common/mojom/paint_preview_recorder.mojom.h"
 #include "components/paint_preview/common/proto/paint_preview.pb.h"
 #include "content/public/browser/web_contents_observer.h"
@@ -121,8 +120,8 @@ class PaintPreviewClient
     // Main frame capture time.
     base::TimeDelta main_frame_blink_recording_time;
 
-    // Callback that is invoked on completion of data. May become null if the
-    // screenshot request failed.
+    // Callback that is invoked on completion of data. Must be non-null for the
+    // lifetime of the InProgressDocumentCaptureState.
     PaintPreviewCallback callback;
 
     // All the render frames that are still required.
