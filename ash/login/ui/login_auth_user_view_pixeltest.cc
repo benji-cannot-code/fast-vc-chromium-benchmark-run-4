1c69c1f48597c3b91c45764d224f05b3db95b082
Peter Kasting <pkasting@chromium.org>
2025-08-18 12:33:55 -0700

    Misc. cleanup, ash/
    * Shorten code
    * Improve comments
    * DRY
    * Remove unnecessary #include
    * Remove unnecessary null check
    
    Bug: none
    Change-Id: I6cb0142f8de1b8a2a4403f145ecdb4b7dfe6bb8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6855753
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1502881}

--- ash/login/ui/login_auth_user_view_pixeltest.cc
+++ ash/login/ui/login_auth_user_view_pixeltest.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/login/ui/login_test_base.h"
 #include "ash/login/ui/login_test_utils.h"
 #include "ash/login/ui/login_user_view.h"
-#include "ash/style/dark_light_mode_controller_impl.h"
 #include "ash/test/pixel/ash_pixel_differ.h"
 #include "ash/test/pixel/ash_pixel_test_helper.h"
 #include "ash/test/pixel/ash_pixel_test_init_params.h"
