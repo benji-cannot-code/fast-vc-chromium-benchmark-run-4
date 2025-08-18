82cff313489c1db4b33316cb22c0b3fe891e10e0
Bryan Bernhart <bryan.bernhart@intel.com>
2025-08-18 11:13:55 -0700

    Reland "WebNN: make WebNNContextImpl use WebNNReceiverImpl"
    This is a reland of commit a694a8e3e0499cd5e4306a3231f7691011f041c4
    
    Fixed issue where WebNNContextImpl was leaked due to base::RetainedRef
    or gpu::Sequence was leaked if context creation failed.
    
    Original change's description:
    > WebNN: make WebNNContextImpl use WebNNReceiverImpl
    >
    > Converts WebNNContext to be an associated interface, since its
    > operations execute asynchronously relative to other contexts through the
    > gpu::Scheduler.
    >
    > Bug: 345352987
    > Change-Id: I9a8ddeb954e3c678ad372cbd0eec42de47e10c33
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6819737
    > Reviewed-by: Alex Gough <ajgo@chromium.org>
    > Reviewed-by: Reilly Grant <reillyg@chromium.org>
    > Commit-Queue: Bryan Bernhart <bryan.bernhart@intel.com>
    > Cr-Commit-Position: refs/heads/main@{#1498500}
    
    Bug: 345352987
    Change-Id: I44be94eb4025cb96f0fbea0313532244ca248ed8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6838538
    Commit-Queue: Bryan Bernhart <bryan.bernhart@intel.com>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1502830}

--- services/webnn/webnn_graph_impl_backend_test.cc
+++ services/webnn/webnn_graph_impl_backend_test.cc
@@ -77,7 +77,7 @@ struct TensorRemoteAndHandle {
 };
 
 TensorRemoteAndHandle CreateTensor(
-    mojo::Remote<mojom::WebNNContext>& context_remote,
+    mojo::AssociatedRemote<mojom::WebNNContext>& context_remote,
     mojom::TensorInfoPtr tensor_info) {
   mojo::AssociatedRemote<mojom::WebNNTensor> webnn_tensor_remote;
 
@@ -97,7 +97,7 @@ TensorRemoteAndHandle CreateTensor(
 }
 
 TensorRemoteAndHandle CreateTensorWithValues(
-    mojo::Remote<mojom::WebNNContext>& context_remote,
+    mojo::AssociatedRemote<mojom::WebNNContext>& context_remote,
     mojom::TensorInfoPtr tensor_info,
     base::span<const uint8_t> data) {
   auto remote_and_handle = CreateTensor(context_remote, std::move(tensor_info));
@@ -117,7 +117,7 @@ enum class BuildAndComputeExpectation { kSuccess, kCreateGraphFailure };
 template <typename InputDataType, typename OutputDataType = InputDataType>
 [[nodiscard]] base::flat_map<std::string, std::vector<OutputDataType>>
 BuildAndCompute(
-    mojo::Remote<mojom::WebNNContext>& context_remote,
+    mojo::AssociatedRemote<mojom::WebNNContext>& context_remote,
     mojo::AssociatedRemote<mojom::WebNNGraphBuilder> graph_builder_remote,
     mojom::GraphInfoPtr graph_info,
     base::flat_map<std::string, base::span<const InputDataType>> named_inputs,
@@ -302,7 +302,9 @@ class WebNNGraphImplBackendTest : public dml::TestBase {
 
   mojo::AssociatedRemote<mojom::WebNNGraphBuilder> BindNewGraphBuilderRemote();
 
-  mojo::Remote<mojom::WebNNContext>& context() { return webnn_context_; }
+  mojo::AssociatedRemote<mojom::WebNNContext>& context() {
+    return webnn_context_;
+  }
 
  protected:
   base::test::ScopedFeatureList scoped_feature_list_;
@@ -310,7 +312,7 @@ class WebNNGraphImplBackendTest : public dml::TestBase {
 
   WebNNTestEnvironment webnn_test_environment_;
   mojo::Remote<mojom::WebNNContextProvider> provider_remote_;
-  mojo::Remote<mojom::WebNNContext> webnn_context_;
+  mojo::AssociatedRemote<mojom::WebNNContext> webnn_context_;
 };
 
 void WebNNGraphImplBackendTest::SetUp() {
@@ -393,7 +395,9 @@ class WebNNGraphImplBackendTest : public testing::Test {
 
   mojo::AssociatedRemote<mojom::WebNNGraphBuilder> BindNewGraphBuilderRemote();
 
-  mojo::Remote<mojom::WebNNContext>& context() { return webnn_context_; }
+  mojo::AssociatedRemote<mojom::WebNNContext>& context() {
+    return webnn_context_;
+  }
 
  protected:
   base::test::ScopedFeatureList scoped_feature_list_;
@@ -401,7 +405,7 @@ class WebNNGraphImplBackendTest : public testing::Test {
 
   WebNNTestEnvironment webnn_test_environment_;
   mojo::Remote<mojom::WebNNContextProvider> provider_remote_;
-  mojo::Remote<mojom::WebNNContext> webnn_context_;
+  mojo::AssociatedRemote<mojom::WebNNContext> webnn_context_;
 };
 
 void WebNNGraphImplBackendTest::SetUp() {
@@ -441,7 +445,9 @@ class WebNNGraphImplBackendTest : public testing::Test {
 
   mojo::AssociatedRemote<mojom::WebNNGraphBuilder> BindNewGraphBuilderRemote();
 
-  mojo::Remote<mojom::WebNNContext>& context() { return webnn_context_; }
+  mojo::AssociatedRemote<mojom::WebNNContext>& context() {
+    return webnn_context_;
+  }
 
  protected:
   base::test::ScopedFeatureList scoped_feature_list_;
@@ -449,7 +455,7 @@ class WebNNGraphImplBackendTest : public testing::Test {
 
   WebNNTestEnvironment webnn_test_environment_;
   mojo::Remote<mojom::WebNNContextProvider> provider_remote_;
-  mojo::Remote<mojom::WebNNContext> webnn_context_;
+  mojo::AssociatedRemote<mojom::WebNNContext> webnn_context_;
 };
 
 void WebNNGraphImplBackendTest::SetUp() {
@@ -530,8 +536,9 @@ void WebNNGraphImplBackendTest::SetUpBase() {
 
 void WebNNGraphImplBackendTest::TearDown() {
   webnn_context_.reset();
-  provider_remote_.reset();
   EXPECT_TRUE(base::test::RunUntil([&]() { return true; }));
+  // Give WebNNContext a chance to run disconnect.
+  provider_remote_.reset();
 }
 
 mojo::AssociatedRemote<mojom::WebNNGraphBuilder>
