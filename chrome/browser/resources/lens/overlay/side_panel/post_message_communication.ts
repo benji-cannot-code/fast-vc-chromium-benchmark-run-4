fecd5a9c8cc3d849c030f248ea1b7b77c7079405
Duncan Mercer <mercerd@google.com>
2025-08-18 16:01:47 -0700

    [AIM] Disable remote communication when flag disabled
    Bug: 437157200
    
    Change-Id: Iba6d66df992c04f39fdddc8bc441e88ab4bc5070
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6853892
    Reviewed-by: Bryan Nguyen <nguyenbryan@google.com>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1503022}

--- chrome/browser/resources/lens/overlay/side_panel/post_message_communication.ts
+++ chrome/browser/resources/lens/overlay/side_panel/post_message_communication.ts
@@ -36,6 +36,8 @@ export class PostMessageReceiver {
   private handshakeMessage: Uint8Array =
       new TextEncoder().encode(loadTimeData.getString('handshakeMessage'));
   private handshakeIntervalId: number|null = null;
+  private isAimSearchboxEnabled: boolean =
+      loadTimeData.getBoolean('enableAimSearchbox');
 
   constructor(
       sidePanelProxy: SidePanelBrowserProxy, resultsFrame: HTMLIFrameElement) {
@@ -67,6 +69,10 @@ export class PostMessageReceiver {
   }
 
   private queueHandshake() {
+    // Only send the handshake message if the AIM searchbox is enabled.
+    if (!this.isAimSearchboxEnabled) {
+      return;
+    }
     // Wait for the iframe to load before sending the handshake message.
     // Also will send a new handshake message if the iframe is reloaded.
     this.eventTracker.add(this.resultsFrame, 'load', () => {
@@ -85,6 +91,10 @@ export class PostMessageReceiver {
   }
 
   private onMessageReceived(event: MessageEvent) {
+    // Only handle messages if the AIM searchbox is enabled.
+    if (!this.isAimSearchboxEnabled) {
+      return;
+    }
     // Only handle messages from the results frame.
     if (event.origin !== this.resultsSearchUrl.origin) {
       return;
--- chrome/test/data/webui/lens/side_panel/post_message_communication_test.ts
+++ chrome/test/data/webui/lens/side_panel/post_message_communication_test.ts
@@ -38,6 +38,7 @@ suite('PostMessageCommunication', () => {
 
     loadTimeData.overrideValues({
       'resultsSearchURL': RESULTS_SEARCH_URL,
+      'enableAimSearchbox': true,
     });
 
     document.body.innerHTML = window.trustedTypes!.emptyHTML;
