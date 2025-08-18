0294d702de7c0476a9a94ffc8534f323ea7474d4
Alex N. Jose <alexnj@chromium.org>
2025-08-18 15:42:02 -0700

    DevTools Network Response Body Durable Storage implementation
    This CL implements a response body storage mechanism that allow
    introspection of network response bodies after the renderer has been
    terminated. This reuses CDP's `NetworkHandler::enable()` with a
    new parameter and `::GetResponseBody()` methods to switch to
    the `DevtoolsDurableMessage` storage hosted in the Network Service.
    The storage is designed to be evicted based on the set limit, or as
    CDP clients are disconnected.
    
    Follow up CLs will complete the following:
     * Decoding stored messages on retrieval (gzip, brotli etc).
     * Base64 encoding binary payloads on retrieval to match current CDP
       API design.
    
    Design doc: https://docs.google.com/document/d/1CGX7k-8KLJBPUCWGUzJSNF991JPBDYGEQF_-hYGQ2hA/edit?pli=1&tab=t.0#heading=h.7nki9mck5t64
    
    Bug: 414864477
    Change-Id: Ib991fb2d2801a5ef0e17517ec0a1c719a2e1687c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6503314
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Alex N. Jose <alexnj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1503009}

--- services/network/url_loader.h
+++ services/network/url_loader.h
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "net/url_request/url_request.h"
 #include "services/network/ad_auction/event_record_request_helper.h"
+#include "services/network/devtools_durable_msg.h"
 #include "services/network/keepalive_statistics_recorder.h"
 #include "services/network/network_service.h"
 #include "services/network/observer_wrapper.h"
@@ -183,7 +184,8 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) URLLoader
       mojo::PendingRemote<mojom::AcceptCHFrameObserver>
           accept_ch_frame_observer,
       bool shared_storage_writable_eligible,
-      SharedResourceChecker& shared_resource_checker);
+      SharedResourceChecker& shared_resource_checker,
+      base::WeakPtr<DevtoolsDurableMessage> devtools_durable_message);
 
   URLLoader(const URLLoader&) = delete;
   URLLoader& operator=(const URLLoader&) = delete;
@@ -541,6 +543,11 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) URLLoader
   // starting another network transaction (such as following a redirect).
   void ResetRawHeadersForRedirect();
 
+  // If Devtools Durable Message collection is enabled, copies the response
+  // chunks into `devtools_durable_message_`. If `num_bytes` <= 0, marks the
+  // message as complete.
+  void MaybeCollectDurableMessage(size_t new_data_offset, int num_bytes);
+
   const raw_ptr<net::URLRequestContext> url_request_context_;
 
   const raw_ptr<mojom::NetworkContextClient> network_context_client_;
@@ -773,6 +780,12 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) URLLoader
 
   // Permissions policy of the request.
   const std::optional<network::PermissionsPolicy> permissions_policy_;
+
+  // DevTools Durable Message instance, if enabled.
+  base::WeakPtr<DevtoolsDurableMessage> devtools_durable_message_;
+
+  // Keeps track of raw body sizes transmitted to DevTools.
+  int64_t devtools_durable_message_raw_size_ = 0;
 
   base::WeakPtrFactory<URLLoader> weak_ptr_factory_{this};
 };
