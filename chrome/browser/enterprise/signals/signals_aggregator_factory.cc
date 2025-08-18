745b10c62f98c261462a8db235bf7a67055b9509
Sebastien Lalancette <seblalancette@chromium.org>
2025-08-18 06:25:26 -0700

    Update "detected_agents" Signal Logic
    Look for the presence of identifiers instead of relying on a different
    logic. This allows simplifying the code quite a bit.
    
    Fixed: b:436558444
    Change-Id: Id1245682143ebdb756b1ac66a901bd81746cd361
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6850598
    Auto-Submit: Sébastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Hamda Mare <hmare@google.com>
    Reviewed-by: Hamda Mare <hmare@google.com>
    Cr-Commit-Position: refs/heads/main@{#1502653}

--- chrome/browser/enterprise/signals/signals_aggregator_factory.cc
+++ chrome/browser/enterprise/signals/signals_aggregator_factory.cc
@@ -53,7 +53,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
 #include "components/device_signals/core/browser/agent_signals_collector.h"
 #include "components/device_signals/core/browser/crowdstrike_client.h"
-#include "components/device_signals/core/browser/detected_agent_client.h"
 #endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
 
 namespace enterprise_signals {
@@ -117,8 +116,7 @@ SignalsAggregatorFactory::BuildServiceInstanceForBrowserContext(
 
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
   collectors.push_back(std::make_unique<device_signals::AgentSignalsCollector>(
-      device_signals::CrowdStrikeClient::Create(),
-      device_signals::DetectedAgentClient::Create()));
+      device_signals::CrowdStrikeClient::Create()));
 #endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
 
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
