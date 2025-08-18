40ed10ed741af58fe8500569bc6e778ac3c58ff8
Verina Armanyous <averina@google.com>
2025-08-18 10:49:46 -0700

    [Save and Fill] Log strike if suggestion is shown but not selected
    Add a strike if the Save and Fill suggestion is shown but not selected
    given that the credit card form is submitted. If the form isn't
    submitted, we don't count it as a strike even if the user didn't select
    the suggestion.
    
    Design doc: go/payments-autofill-save-and-fill-desktop-design
    
    Bug: 378165003
    Change-Id: I06a55e2b241bd8c11bb67747fb0a4fd1e09ecdb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6843216
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Verina Armanyous <averina@google.com>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1502812}

--- components/autofill/core/browser/foundations/browser_autofill_manager.cc
+++ components/autofill/core/browser/foundations/browser_autofill_manager.cc
@@ -123,6 +123,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/payments/constants.h"
 #include "components/autofill/core/browser/payments/credit_card_access_manager.h"
 #include "components/autofill/core/browser/payments/iban_manager.h"
+#include "components/autofill/core/browser/payments/save_and_fill_manager.h"
 #include "components/autofill/core/browser/permissions/autofill_ai/autofill_ai_permission_utils.h"
 #include "components/autofill/core/browser/single_field_fillers/autocomplete/autocomplete_history_manager.h"
 #include "components/autofill/core/browser/single_field_fillers/payments/merchant_promo_code_manager.h"
@@ -940,6 +941,11 @@ void BrowserAutofillManager::OnFormSubmittedImpl(const FormData& form,
       /*observed_submission=*/true, GetCurrentPageLanguage(),
       metrics_->initial_interaction_timestamp, last_unlocked_credit_card_cvc_,
       driver().GetPageUkmSourceId());
+
+  if (auto* save_and_fill_manager =
+          client().GetPaymentsAutofillClient()->GetSaveAndFillManager()) {
+    save_and_fill_manager->OnCreditCardFormSubmitted();
+  }
 }
 
 void BrowserAutofillManager::UpdatePendingForm(const FormData& form) {
@@ -2328,6 +2334,11 @@ void BrowserAutofillManager::DidShowSuggestions(
   if (shown_suggestion_types.contains(
           SuggestionType::kSaveAndFillCreditCardEntry)) {
     metrics_->credit_card_form_event_logger.OnSaveAndFillSuggestionShown();
+
+    if (auto* save_and_fill_manager =
+            client().GetPaymentsAutofillClient()->GetSaveAndFillManager()) {
+      save_and_fill_manager->OnSuggestionOffered();
+    }
   }
 
   if (std::ranges::none_of(
