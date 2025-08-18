946f545f9a2b46aced9565e11c568d1c7e0214e8
Kurt Catti-Schmidt <kschmi@microsoft.com>
2025-08-18 13:56:10 -0700

    Avoid Member<>'s on stack in page.cc
    Per crbug.com/40209908 Member<>'s on the stack have extra overhead and
    should be avoided. This change converts them to Member<>&'s, which do
    not incur this overhead.
    
    No behavioral changes are expected (other than slightly less Oilpan
    overhead).
    
    Bug: 439630585
    
    Change-Id: I7c39336375497b5e5bda09f176b18c32ad036e6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6858195
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Commit-Queue: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1502941}

--- third_party/blink/renderer/core/page/page.cc
+++ third_party/blink/renderer/core/page/page.cc
@@ -519,7 +519,7 @@ void Page::TakePropertiesForLocalMainFrameSwap(Page* old_page) {
 
   // If the previous page is an opener for other pages, make sure that the
   // openees point to the new page instead.
-  for (auto page : RelatedPages()) {
+  for (auto& page : RelatedPages()) {
     if (page->opener_ == old_page) {
       page->opener_ = this;
     }
@@ -865,7 +865,7 @@ void Page::SetVisibilityState(
   if (is_initial_state)
     return;
 
-  for (auto observer : page_visibility_observer_set_) {
+  for (auto& observer : page_visibility_observer_set_) {
     observer->PageVisibilityChanged();
   }
 
@@ -1448,7 +1448,7 @@ void Page::WillBeDestroyed() {
     validation_message_client_->WillBeDestroyed();
   main_frame_ = nullptr;
 
-  for (auto observer : page_visibility_observer_set_) {
+  for (auto& observer : page_visibility_observer_set_) {
     observer->ObserverSetWillBeCleared();
   }
   page_visibility_observer_set_.clear();
