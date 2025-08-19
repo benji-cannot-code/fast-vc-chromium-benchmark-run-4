abbea8f53475c51df854919ae6524119747825d4
Keigo Oka <oka@google.com>
2025-08-18 22:38:49 -0700

    base: Support virtual document paths in FileEnumerator
    This change enables `FileEnumerator` to work with virtual document
    paths. This allows callers to enumerate files and directories
    represented by virtual document paths as if they were regular file
    paths.
    
    Low-Coverage-Reason: COVERAGE_UNDERREPORTED FileEnumerator.GetInfoOnFiles should be convering populateFileInfo but it's underreported.
    Bug: 422035179
    Change-Id: Ia385794263a3ee4ea98639ca52a38d03bac12e84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6791171
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Keigo Oka <oka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1503152}

--- base/files/file_enumerator_posix.cc
+++ base/files/file_enumerator_posix.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_ANDROID)
 #include "base/android/content_uri_utils.h"
+#include "base/files/file_util.h"
 #endif
 
 namespace base {
@@ -146,12 +147,10 @@ FileEnumerator::FileEnumerator(const FilePath& root_path,
   DCHECK(!(recursive && (INCLUDE_DOT_DOT & file_type_)));
 
 #if BUILDFLAG(IS_ANDROID)
-  // Content-URIs have limited support.
-  if (root_path.IsContentUri()) {
-    CHECK_EQ(file_type_, FileType::FILES | FileType::DIRECTORIES);
+  if (auto content_uri = base::ResolveToContentUri(root_path); content_uri) {
     // Get display-name of root path.
     FileInfo root_info;
-    internal::ContentUriGetFileInfo(root_path, &root_info);
+    internal::ContentUriGetFileInfo(*content_uri, &root_info);
     pending_subdirs_.push(
         std::vector<std::string>({root_info.GetName().value()}));
   }
@@ -191,10 +190,11 @@ FilePath FileEnumerator::Next() {
     pending_paths_.pop();
 
 #if BUILDFLAG(IS_ANDROID)
-    if (root_path_.IsContentUri()) {
+    if (auto content_uri = base::ResolveToContentUri(root_path_); content_uri) {
       subdirs_ = pending_subdirs_.top();
       pending_subdirs_.pop();
-      directory_entries_ = internal::ListContentUriDirectory(root_path_);
+      directory_entries_ =
+          internal::ListContentUriDirectory(*content_uri, file_type_);
       current_directory_entry_ = 0;
       if (directory_entries_.empty()) {
         continue;
@@ -311,7 +311,7 @@ FilePath FileEnumerator::Next() {
   }
 
 #if BUILDFLAG(IS_ANDROID)
-  if (root_path_.IsContentUri()) {
+  if (root_path_.IsContentUri() || root_path_.IsVirtualDocumentPath()) {
     return directory_entries_[current_directory_entry_].content_uri_;
   }
 #endif
