2860d1748b6ea6a093d43fb41c2a5600399db235
Mark Mentovai <mark@chromium.org>
2025-08-19 11:30:56 -0700

    net, apple: detect bogus return values from `sendto` for UDP sockets
    UDP sockets are theoretically susceptible to https://crbug.com/40064248
    just as TCP sockets are, but the UDP code was missing the `CHECK` that
    would have caught the bug. This adds the missing `CHECK` to
    net/socket/udp_socket_posix.cc, updates the bug-detecting logic to
    handle UDP as well as TCP, and enables the workaround of detecting the
    bug for UDP sockets.
    
    Bug: 40064248, 283787255
    Change-Id: I847e02e5c30ba4a8572f190fc1e61d0d9ee3e05b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6862413
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1503493}

--- net/socket/udp_socket_posix.cc
+++ net/socket/udp_socket_posix.cc
@@ -62,6 +62,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_APPLE)
 #include "net/base/apple/guarded_fd.h"
+#include "net/socket/socket_apple.h"
 #endif  // BUILDFLAG(IS_APPLE)
 
 #if BUILDFLAG(IS_MAC)
@@ -816,12 +817,26 @@ int UDPSocketPosix::InternalSendTo(IOBuffer* buf,
     }
   }
 
-  int result = HANDLE_EINTR(sendto(socket_, buf->data(), buf_len, sendto_flags_,
-                                   addr, storage.addr_len));
-  if (result < 0)
+#if !defined(WORK_AROUND_CRBUG_40064248)
+  ssize_t result = HANDLE_EINTR(sendto(socket_, buf->data(), buf_len,
+                                       sendto_flags_, addr, storage.addr_len));
+#else   // !WORK_AROUND_CRBUG_40064248
+  ssize_t result = HANDLE_EINTR(SendtoAndDetectBogusReturnValue(
+      socket_, buf->data(), buf_len, sendto_flags_, addr, storage.addr_len));
+  if (result == kSendBogusReturnValueDetected) {
+    // https://crbug.com/40064248 is known to occur as a result of certain
+    // network configuration changes.
+    result = ERR_NETWORK_CHANGED;
+  } else
+#endif  // !WORK_AROUND_CRBUG_40064248
+  if (result < 0) {
     result = MapSystemError(errno);
-  if (result != ERR_IO_PENDING)
+  } else {
+    CHECK_LE(result, buf_len);
+  }
+  if (result != ERR_IO_PENDING) {
     LogWrite(result, buf->data(), address);
+  }
   return result;
 }
 
