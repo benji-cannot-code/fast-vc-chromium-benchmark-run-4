b8267e109fbd17b49e42302d7f48cb54ed1c75e5
Joel Tan-Aristy <jtanaristy@google.com>
2025-08-19 11:27:32 -0700

    [E2E] Move E2ELayout related code to //ui/edge_to_edge
    Moved Edge to Edge layout classes to a new //ui/edge_to_edge
    directory.
    
    Bug: 428281174
    Bypass-Check-License: Refactoring directory
    Change-Id: I91fa6da9535d8f5f1956b135040a578466952900
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6804203
    Reviewed-by: Jesse McKenna <jessemckenna@google.com>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Charles Hager <clhager@google.com>
    Commit-Queue: Joel Tan-Aristy <jtanaristy@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Glenn Hartmann <hartmanng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1503489}

--- chrome/android/java/src/org/chromium/chrome/browser/init/ProcessInitializationHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/ProcessInitializationHandler.java
@@ -109,7 +109,6 @@ import org.chromium.chrome.browser.webapps.WebappRegistry;
 import org.chromium.components.background_task_scheduler.BackgroundTaskSchedulerFactory;
 import org.chromium.components.browser_ui.accessibility.PageZoomUtils;
 import org.chromium.components.browser_ui.contacts_picker.ContactsPickerDialog;
-import org.chromium.components.browser_ui.edge_to_edge.EdgeToEdgeStateProvider;
 import org.chromium.components.browser_ui.photo_picker.DecoderServiceHost;
 import org.chromium.components.browser_ui.photo_picker.PhotoPickerDelegateBase;
 import org.chromium.components.browser_ui.photo_picker.PhotoPickerDialog;
@@ -138,6 +137,7 @@ import org.chromium.ui.base.PhotoPicker;
 import org.chromium.ui.base.PhotoPickerListener;
 import org.chromium.ui.base.SelectFileDialog;
 import org.chromium.ui.base.WindowAndroid;
+import org.chromium.ui.edge_to_edge.EdgeToEdgeStateProvider;
 import org.chromium.url.GURL;
 
 import java.io.File;
--- chrome/browser/bookmarks/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkManagerCoordinator.java
+++ chrome/browser/bookmarks/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkManagerCoordinator.java
@@ -44,7 +44,6 @@ import org.chromium.chrome.browser.ui.signin.signin_promo.BookmarkSigninPromoDel
 import org.chromium.chrome.browser.ui.signin.signin_promo.SigninPromoCoordinator;
 import org.chromium.components.bookmarks.BookmarkId;
 import org.chromium.components.bookmarks.BookmarkItem;
-import org.chromium.components.browser_ui.edge_to_edge.EdgeToEdgePadAdjuster;
 import org.chromium.components.browser_ui.modaldialog.AppModalPresenter;
 import org.chromium.components.browser_ui.util.GlobalDiscardableReferencePool;
 import org.chromium.components.browser_ui.widget.dragreorder.DragReorderableRecyclerViewAdapter;
@@ -58,6 +57,7 @@ import org.chromium.components.image_fetcher.ImageFetcher;
 import org.chromium.components.image_fetcher.ImageFetcherConfig;
 import org.chromium.components.image_fetcher.ImageFetcherFactory;
 import org.chromium.ui.KeyboardVisibilityDelegate;
+import org.chromium.ui.edge_to_edge.EdgeToEdgePadAdjuster;
 import org.chromium.ui.modaldialog.ModalDialogManager;
 import org.chromium.ui.modaldialog.ModalDialogManager.ModalDialogType;
 import org.chromium.ui.modelutil.MVCListAdapter.ListItem;
