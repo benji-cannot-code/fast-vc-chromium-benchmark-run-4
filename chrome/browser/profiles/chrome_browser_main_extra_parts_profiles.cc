11f2ebd985b6f9ba7c2664823a3ec84132acd664
chrstne <chrstne@google.com>
2025-08-19 09:47:00 -0700

    Add GlicActorTaskIconManager/KeyedService
    Bug: 437161973
    Change-Id: I9a0920f517142b9a28618169cecb9d8f38a5f46e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6862853
    Commit-Queue: Christine Ying <chrstne@google.com>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1503409}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -525,6 +525,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(ENABLE_GLIC)
 #include "chrome/browser/glic/public/glic_enabling.h"
 #include "chrome/browser/glic/public/glic_keyed_service_factory.h"
+#include "chrome/browser/ui/tabs/glic_actor_task_icon_manager_factory.h"
 #endif
 
 #if BUILDFLAG(ENABLE_ON_DEVICE_TRANSLATION)
@@ -959,6 +960,7 @@ void ChromeBrowserMainExtraPartsProfiles::
 #endif
 #if BUILDFLAG(ENABLE_GLIC)
   glic::GlicKeyedServiceFactory::GetInstance();
+  tabs::GlicActorTaskIconManagerFactory::GetInstance();
 #endif
 #if !BUILDFLAG(IS_ANDROID)
   GlobalErrorServiceFactory::GetInstance();
---
+++ chrome/browser/ui/tabs/glic_actor_task_icon_manager.h
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_TABS_GLIC_ACTOR_TASK_ICON_MANAGER_H_
+#define CHROME_BROWSER_UI_TABS_GLIC_ACTOR_TASK_ICON_MANAGER_H_
+
+#include "components/keyed_service/core/keyed_service.h"
+
+namespace glic {
+class GlicWindowController;
+class Host;
+}  // namespace glic
+
+namespace actor {
+class ActorKeyedService;
+}  // namespace actor
+
+class Profile;
+
+namespace tabs {
+class GlicActorTaskIconManager : public KeyedService {
+ public:
+  GlicActorTaskIconManager(Profile* profile,
+                           actor::ActorKeyedService* actor_service,
+                           glic::GlicWindowController& window_controller,
+                           glic::Host& host);
+  ~GlicActorTaskIconManager() override;
+
+  // KeyedService:
+  void Shutdown() override;
+};
+
+}  // namespace tabs
+
+#endif  // CHROME_BROWSER_UI_TABS_GLIC_ACTOR_TASK_ICON_MANAGER_H_
