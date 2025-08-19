4ea0defd817f9508ca75e4824112604d87d0faaf
Joe Downing <joedow@google.com>
2025-08-19 12:55:44 -0700

    Adding a CorpMesagingPlayground utility
    This utility will be used to test corp messaging functionality.
    Landing the placeholder code now and will add to it as I land
    additional classes.
    
    Bug: 359620500
    Change-Id: Idada2dc413515192ca70f6327e61398cef2867c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6862855
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Auto-Submit: Joe Downing <joedow@chromium.org>
    Commit-Queue: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1503572}

---
+++ remoting/test/corp_messaging_playground.h
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef REMOTING_TEST_CORP_MESSAGING_PLAYGROUND_H_
+#define REMOTING_TEST_CORP_MESSAGING_PLAYGROUND_H_
+
+#include <memory>
+
+namespace network {
+class TransitionalURLLoaderFactoryOwner;
+}
+
+namespace remoting {
+
+class CorpMessagingPlayground {
+ public:
+  CorpMessagingPlayground();
+  ~CorpMessagingPlayground();
+
+  CorpMessagingPlayground(const CorpMessagingPlayground&) = delete;
+  CorpMessagingPlayground& operator=(const CorpMessagingPlayground&) = delete;
+
+  void Start();
+
+ private:
+  std::unique_ptr<network::TransitionalURLLoaderFactoryOwner>
+      url_loader_factory_owner_;
+};
+
+}  // namespace remoting
+
+#endif  // REMOTING_TEST_CORP_MESSAGING_PLAYGROUND_H_
