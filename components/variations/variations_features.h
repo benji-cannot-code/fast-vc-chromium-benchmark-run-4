9110abf9cd16e7d421bb4c24a9c0a158c90df493
Roger McFarlane <rogerm@chromium.org>
2025-08-19 07:47:28 -0700

    Cleanup: Use C++17 concise namespace syntax for `variations::internal`.
    This updates the namespace declaration to use the `namespace variations::internal { ... }` form.
    
    Change-Id: I0b72dcac385b68b4125e1c8b88a7ffe00c5a2d44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6862275
    Commit-Queue: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Auto-Submit: Roger McFarlane <rogerm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1503336}

--- components/variations/variations_features.h
+++ components/variations/variations_features.h
@@ -9,12 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/component_export.h"
 #include "base/metrics/field_trial.h"
 
-namespace variations {
-namespace internal {
+namespace variations::internal {
 
 // Add Variations Features here...
 
-}  // namespace internal
-}  // namespace variations
+}  // namespace variations::internal
 
 #endif  // COMPONENTS_VARIATIONS_VARIATIONS_FEATURES_H_
