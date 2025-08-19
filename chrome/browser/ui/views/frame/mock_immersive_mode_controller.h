b4a1c534eaaa31da724dab3deb20f377d21fd8d1
Michelle Abreo <michelleabreo@google.com>
2025-08-19 08:47:55 -0700

    Create gMock ImmersiveModeController for unit tests.
    I needed a mock of this controller in my target directory for unit tests
    for a new UI component (a button that hovers over the web contents).
    However, copy-pasting an existing fake controller would be brittle; any
    future changes to the base class, such as adding new pure virtual
    functions, would require manual updates in multiple locations.
    
    This CL introduces a centralized, reusable MockImmersiveModeController
    here to solve this. This mock will be immediately reused for follow-up
    changes to my UI component and will be available for any future tests
    that need to interact with an ImmersiveModeController.
    
    I've replaced the existing MockImmersiveModeController that I initially
    used as a guide with this new one as well.
    
    Change-Id: I593f42fc386d10a1030bbc619b95256e8340d185
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6857331
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Michelle Abreo <michelleabreo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1503372}

---
+++ chrome/browser/ui/views/frame/mock_immersive_mode_controller.h
@@ -0,0 +1,51 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_VIEWS_FRAME_MOCK_IMMERSIVE_MODE_CONTROLLER_H_
+#define CHROME_BROWSER_UI_VIEWS_FRAME_MOCK_IMMERSIVE_MODE_CONTROLLER_H_
+
+#include "chrome/browser/ui/views/frame/immersive_mode_controller.h"
+#include "testing/gmock/include/gmock/gmock.h"
+
+class MockImmersiveRevealedLock : public ImmersiveRevealedLock {
+ public:
+  MockImmersiveRevealedLock() = default;
+  ~MockImmersiveRevealedLock() override = default;
+};
+
+class MockImmersiveModeController : public ImmersiveModeController {
+ public:
+  MockImmersiveModeController();
+  ~MockImmersiveModeController() override;
+
+  MOCK_METHOD(void, Init, (BrowserView * browser_view), (override));
+  MOCK_METHOD(void, SetEnabled, (bool enabled), (override));
+  MOCK_METHOD(bool, IsEnabled, (), (const, override));
+  MOCK_METHOD(bool, ShouldHideTopViews, (), (const, override));
+  MOCK_METHOD(bool, IsRevealed, (), (const, override));
+  MOCK_METHOD(int,
+              GetTopContainerVerticalOffset,
+              (const gfx::Size& top_container_size),
+              (const, override));
+  MOCK_METHOD(std::unique_ptr<ImmersiveRevealedLock>,
+              GetRevealedLock,
+              (AnimateReveal animate_reveal),
+              (override));
+  MOCK_METHOD(void,
+              OnFindBarVisibleBoundsChanged,
+              (const gfx::Rect& new_visible_bounds_in_screen),
+              (override));
+  MOCK_METHOD(bool, ShouldStayImmersiveAfterExitingFullscreen, (), (override));
+  MOCK_METHOD(int, GetMinimumContentOffset, (), (const, override));
+  MOCK_METHOD(int, GetExtraInfobarOffset, (), (const, override));
+  MOCK_METHOD(void,
+              OnContentFullscreenChanged,
+              (bool is_content_fullscreen),
+              (override));
+  MOCK_METHOD(void, AddObserver, (Observer * observer), (override));
+  MOCK_METHOD(void, RemoveObserver, (Observer * observer), (override));
+};
+
+#endif  // CHROME_BROWSER_UI_VIEWS_FRAME_MOCK_IMMERSIVE_MODE_CONTROLLER_H_
