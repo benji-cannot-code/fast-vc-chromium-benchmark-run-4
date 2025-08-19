78307e972d4194ee733d3be525b1a98e1bc9443f
Shiyi Zou <shiyi.zou@intel.com>
2025-08-19 08:58:20 -0700

    webnn: work around the resample2d layout issue for OV EP
    OV CPU EP only supports resample2d with NCHW layout while ORT supports
    any layouts. This CL introduces `EpWorkarounds` which specifies the
    workarounds needed for execution provider limitations.
    
    Bug: 436345874
    Change-Id: I56f3e4599da118de1e750bbe61cd7bf3f590a37e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6818326
    Commit-Queue: Rafael Cintron <rafael.cintron@microsoft.com>
    Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1503378}

--- services/webnn/ort/environment.h
+++ services/webnn/ort/environment.h
@@ -20,6 +20,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace webnn::ort {
 
+// Describes the workarounds needed for execution provider limitations.
+// TODO(crbug.com/428740146): Remove this struct once all the execution
+// providers fix these issues.
+struct EpWorkarounds {
+  // TODO(crbug.com/429253567): Specify the minimum package version that
+  // supports these features without requiring workarounds.
+
+  bool disable_external_data = false;
+  // By default ONNX Resize op supports any axes, but some EPs may only support
+  // NCHW layout. `ContextProperties.resample_2d_axes` setting will respect to
+  // this limit.
+  bool resample2d_limit_to_nchw = false;
+
+  EpWorkarounds& operator|=(const EpWorkarounds& other) {
+    disable_external_data |= other.disable_external_data;
+    resample2d_limit_to_nchw |= other.resample2d_limit_to_nchw;
+    return *this;
+  }
+};
+
 // A wrapper of `OrtEnv` which is thread-safe and can be shared across sessions.
 // It should be kept alive until all sessions using it are destroyed.
 class Environment : public base::subtle::RefCountedThreadSafeBase {
@@ -36,9 +56,9 @@ class Environment : public base::subtle::RefCountedThreadSafeBase {
   void AddRef() const;
   void Release() const;
 
-  const OrtEnv* get() const { return env_.get(); }
+  EpWorkarounds GetEpWorkarounds(mojom::Device device_type) const;
 
-  bool IsExternalDataSupported(mojom::Device device_type) const;
+  const OrtEnv* get() const { return env_.get(); }
 
  private:
   static base::expected<scoped_refptr<Environment>, std::string> Create(
