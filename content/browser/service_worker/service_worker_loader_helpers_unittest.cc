40629e7866a0783957b6979efa106ffed7678237
Shunya Shishido <sisidovski@chromium.org>
2025-08-19 02:26:55 -0700

    [ServiceWorker] Use single URL for synthetic response feature
    The kServiceWorkerSyntheticResponseAllowedUrls feature param was
    previously a comma-separated list of URLs. This was unnecessarily
    complex, as only a single URL is needed.
    
    This CL renames the feature to kServiceWorkerSyntheticResponseAllowedUrl
    and simplifies the implementation to handle a single URL.
    
    Bug: 352578800
    Change-Id: I36086bb212fe2d131af57d0483798db744c67768
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6860825
    Commit-Queue: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1503226}

--- content/browser/service_worker/service_worker_loader_helpers_unittest.cc
+++ content/browser/service_worker/service_worker_loader_helpers_unittest.cc
@@ -29,9 +29,9 @@ bool IsPathRestrictionSatisfiedWithServiceWorkerAllowedHeader(
 }
 
 bool IsEligibleForSyntheticResponse(const GURL& client_url,
-                                    const std::string& allowed_urls) {
+                                    const std::string& allowed_url) {
   return service_worker_loader_helpers::
-      IsEligibleForSyntheticResponseForTesting(client_url, allowed_urls);
+      IsEligibleForSyntheticResponseForTesting(client_url, allowed_url);
 }
 
 }  // namespace
@@ -356,6 +356,8 @@ TEST(ServiceWorkerLoaderHelpersTest, IsEligibleForSyntheticResponse) {
   EXPECT_FALSE(
       IsEligibleForSyntheticResponse(GURL("http://example.com/foo?param=test"),
                                      "http://example.com/bar?param=test"));
+  // Empty string is not allowed.
+  EXPECT_FALSE(IsEligibleForSyntheticResponse(GURL("http://example.com/"), ""));
 }
 
 }  // namespace service_worker_loader_helpers
