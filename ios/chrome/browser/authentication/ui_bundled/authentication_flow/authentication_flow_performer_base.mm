a1c5b8cb4502174726c61f246750c1402a568f81
Arthur Milchior <arthurmilchior@google.com>
2025-08-19 06:20:01 -0700

    [iOS]Fix a crash regarding undo during shutdown
    The sign-in can be done in incognito mode, so the sign-out callback
    should also consider the incognito browser. Hence requesting the
    original profile to get the sign-in and sync services.
    
    Bug: 438409485
    Change-Id: Id834eda73c1c200f9a5bfd70dbfc8927733cbd2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6858531
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1503297}

--- ios/chrome/browser/authentication/ui_bundled/authentication_flow/authentication_flow_performer_base.mm
+++ ios/chrome/browser/authentication/ui_bundled/authentication_flow/authentication_flow_performer_base.mm
@@ -104,7 +104,7 @@ void HandleSignoutForSnackbar(
   base::RecordAction(
       base::UserMetricsAction("Mobile.Signin.SnackbarUndoTapped"));
 
-  ProfileIOS* profile = browser->GetProfile();
+  ProfileIOS* profile = browser->GetProfile()->GetOriginalProfile();
   AuthenticationService* auth_service =
       AuthenticationServiceFactory::GetForProfile(profile);
   if (!auth_service->HasPrimaryIdentity(signin::ConsentLevel::kSignin)) {
