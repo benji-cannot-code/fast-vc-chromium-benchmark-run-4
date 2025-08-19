3248e183c72f247c93603fe0ac55f475d121acf2
kylechar <kylechar@chromium.org>
2025-08-19 13:35:13 -0700

    Remove client side vertex array support
    This functionality was last used by PPAPI and isn't used outside tests now. Remove tests and most of the implementation here.
    
    Bug: 439823997
    Change-Id: I314cfb92ab32beec8d9d647b3295d8f4e7c9d7e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6860267
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1503603}

--- gpu/command_buffer/client/vertex_array_object_manager_unittest.cc
+++ gpu/command_buffer/client/vertex_array_object_manager_unittest.cc
@@ -22,16 +22,9 @@ namespace gles2 {
 class VertexArrayObjectManagerTest : public testing::Test {
  protected:
   static const GLuint kMaxAttribs = 4;
-  static const GLuint kClientSideArrayBuffer = 0x1234;
-  static const GLuint kClientSideElementArrayBuffer = 0x1235;
-  static const bool kSupportClientSideArrays = true;
 
   void SetUp() override {
-    manager_.reset(new VertexArrayObjectManager(
-        kMaxAttribs,
-        kClientSideArrayBuffer,
-        kClientSideElementArrayBuffer,
-        kSupportClientSideArrays));
+    manager_ = std::make_unique<VertexArrayObjectManager>(kMaxAttribs);
   }
   void TearDown() override {}
 
@@ -39,8 +32,6 @@ class VertexArrayObjectManagerTest : public testing::Test {
 };
 
 const GLuint VertexArrayObjectManagerTest::kMaxAttribs;
-const GLuint VertexArrayObjectManagerTest::kClientSideArrayBuffer;
-const GLuint VertexArrayObjectManagerTest::kClientSideElementArrayBuffer;
 
 TEST_F(VertexArrayObjectManagerTest, Basic) {
   EXPECT_FALSE(manager_->HaveEnabledClientSideBuffers());
@@ -270,14 +261,5 @@ TEST_F(VertexArrayObjectManagerTest, GenBindDelete) {
   EXPECT_FALSE(changed);
 }
 
-TEST_F(VertexArrayObjectManagerTest, IsReservedId) {
-  EXPECT_TRUE(manager_->IsReservedId(kClientSideArrayBuffer));
-  EXPECT_TRUE(manager_->IsReservedId(kClientSideElementArrayBuffer));
-  EXPECT_FALSE(manager_->IsReservedId(0));
-  EXPECT_FALSE(manager_->IsReservedId(1));
-  EXPECT_FALSE(manager_->IsReservedId(2));
-}
-
 }  // namespace gles2
 }  // namespace gpu
-
