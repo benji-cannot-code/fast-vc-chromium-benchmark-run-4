27fd30f8b0d2456b9d6d1083699c0d85ebc8b14f
Jun Ishiguro <junis@google.com>
2025-08-19 00:48:55 -0700

    ash-refactor: Move ClipboardHistoryControllerDelegateImpl
    This CL moves ClipboardHistoryControllerDelegateImpl from
    //chrome/browser/ui/ash/clipboard to
    //chromeos/ash/experiences/clipboard.
    
    This is part of an ongoing effort to relocate Ash-specific code from
    //chrome to //ash, enhancing the separation of concerns between the two
    components.
    
    Bug: b:426116843
    Change-Id: I41fd8f088983d8016094d9c05288827f2cd7b0e9
    Bypass-Check-License: Moving files
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6856799
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1503189}

--- chromeos/ash/experiences/clipboard/clipboard_history_controller_delegate_impl.cc
+++ chromeos/ash/experiences/clipboard/clipboard_history_controller_delegate_impl.cc
@@ -3,11 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/ash/clipboard/clipboard_history_controller_delegate_impl.h"
+#include "chromeos/ash/experiences/clipboard/clipboard_history_controller_delegate_impl.h"
 
 #include <set>
 
-#include "chrome/browser/ui/ash/clipboard/clipboard_image_model_factory_impl.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/render_widget_host.h"
