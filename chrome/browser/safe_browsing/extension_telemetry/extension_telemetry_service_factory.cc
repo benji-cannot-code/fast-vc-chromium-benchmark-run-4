2f4be0e759b372be6353903a392489dafe4973a7
Anunoy Ghosh <anunoy@chromium.org>
2025-08-19 13:41:10 -0700

    Integrate search hijacking signal into extension telemetry service
    This CL integrates the search hijacking detection heuristic signal into
    the extension telemetry service. The service owns the
    SearchHijackingDetector class, instantiates it when ESB is enabled, and
    periodically calls the heuristic check function. The service also
    includes the signal data in telemetry reports if there is a heuristic
    match.
    
    Bug: 437345485
    Change-Id: I5519f62e36ada38f21328fb04471f1002f0b32d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6855833
    Reviewed-by: thefrog <thefrog@chromium.org>
    Commit-Queue: Anunoy Ghosh <anunoy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1503606}

--- chrome/browser/safe_browsing/extension_telemetry/extension_telemetry_service_factory.cc
+++ chrome/browser/safe_browsing/extension_telemetry/extension_telemetry_service_factory.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/safe_browsing/extension_telemetry/extension_telemetry_service.h"
 #include "chrome/browser/safe_browsing/network_context_service.h"
 #include "chrome/browser/safe_browsing/network_context_service_factory.h"
+#include "chrome/browser/search_engines/template_url_service_factory.h"
 #include "components/enterprise/buildflags/buildflags.h"
 #include "components/safe_browsing/core/common/features.h"
 #include "content/public/browser/browser_context.h"
@@ -52,6 +53,7 @@ ExtensionTelemetryServiceFactory::ExtensionTelemetryServiceFactory()
   DependsOn(extensions::ExtensionRegistryFactory::GetInstance());
   DependsOn(extensions::ExtensionManagementFactory::GetInstance());
   DependsOn(extensions::ChromeExtensionSystemFactory::GetInstance());
+  DependsOn(TemplateURLServiceFactory::GetInstance());
 #if BUILDFLAG(ENTERPRISE_CLOUD_CONTENT_ANALYSIS)
   DependsOn(enterprise_connectors::ConnectorsServiceFactory::GetInstance());
   DependsOn(enterprise_connectors::ExtensionTelemetryEventRouterFactory::
