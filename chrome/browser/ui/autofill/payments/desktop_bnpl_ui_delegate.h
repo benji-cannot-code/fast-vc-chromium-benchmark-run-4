816bb6df267e9540213170f8e167e250506daca7
Fernando Ramirez <ferny@google.com>
2025-08-19 09:44:26 -0700

    [BNPL][Clank] Create BnplUiDelegate skeleton and implementations
    This CL introduces BnplUiDelegate to handle platform-specific UI for the
    Buy Now, Pay Later (BNPL) flow. This delegate is implemented by
    platform-specific classes (DesktopBnplUiDelegate and
    AndroidBnplUiDelegate). This abstracts away UI logic, allowing the core
    BNPL flow in BnplManager to remain platform-agnostic.
    
    This is part of the effort to expand the Chrome Desktop BNPL feature to
    Android. The goal is to reuse as much of the existing BnplManager as
    possible while avoiding platform-specific build flags.
    
    Design: go/autofill-clank-bnpl
    
    Low-Coverage-Reason: TRIVIAL_CHANGE This CL only adds a skeleton class
    Bug: 430575808, 438783909
    Change-Id: I0a6df704fbea2b296946c6b93deb7bf27b0213d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6825669
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Fernando Ramirez <ferny@google.com>
    Cr-Commit-Position: refs/heads/main@{#1503406}

---
+++ chrome/browser/ui/autofill/payments/desktop_bnpl_ui_delegate.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_AUTOFILL_PAYMENTS_DESKTOP_BNPL_UI_DELEGATE_H_
+#define CHROME_BROWSER_UI_AUTOFILL_PAYMENTS_DESKTOP_BNPL_UI_DELEGATE_H_
+
+#include "components/autofill/core/browser/ui/payments/bnpl_ui_delegate.h"
+
+namespace autofill::payments {
+
+// Desktop implementation of the `BnplUiDelegate` interface. This class handles
+// the UI for the BNPL autofill flow on the Desktop platform.
+class DesktopBnplUiDelegate : public BnplUiDelegate {
+ public:
+  DesktopBnplUiDelegate();
+  DesktopBnplUiDelegate(const DesktopBnplUiDelegate& other) = delete;
+  DesktopBnplUiDelegate& operator=(const DesktopBnplUiDelegate& other) = delete;
+  ~DesktopBnplUiDelegate() override;
+};
+
+}  // namespace autofill::payments
+
+#endif  // CHROME_BROWSER_UI_AUTOFILL_PAYMENTS_DESKTOP_BNPL_UI_DELEGATE_H_
