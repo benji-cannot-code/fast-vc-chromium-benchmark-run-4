8bab5124a01a11d804f1280be4c0f151725ffdce
David Maunder <davidjm@google.com>
2025-08-19 06:33:17 -0700

    Record size of Impression Limit storage in ShopCard
    Impressions of some cards in the magic stack are stored on a per-URL
    basis to ensure impressions are shown a maximum of 3 times before
    the card is auto dismissed. This CL adds in metrics for the
    size of the storage for the impressions as it is believed it
    could be contributing to a mild memory regression.
    
    Bug: 436660596
    Change-Id: I1a7fc5eca7ef1b6727bdf1e2169b69e3375be2d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6859741
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Sebastien S-G <sebsg@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1503306}

--- ios/chrome/browser/content_suggestions/ui_bundled/impression_limits/impression_limit_service.cc
+++ ios/chrome/browser/content_suggestions/ui_bundled/impression_limits/impression_limit_service.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/time_formatting.h"
 #include "base/json/values_util.h"
 #include "base/logging.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/time/time.h"
 #include "base/values.h"
@@ -157,6 +158,32 @@ void ImpressionLimitService::LogImpressionForURL(
                  << " must be registered with ImpressionLimitService";
   }
   LogImpressionForURLAtTime(url, pref_name, base::Time::Now());
+  size_t storage_size_kb =
+      pref_service_->GetDict(pref_name).EstimateMemoryUsage() / 1024;
+  if (pref_name == shop_card_prefs::kShopCardPriceDropUrlImpressions) {
+    base::UmaHistogramMemoryKB(
+        "IOS.MagicStack.ShopCard.PriceDropOnTrackedItem."
+        "ImpressionLimitStorageSize",
+        storage_size_kb);
+  } else if (pref_name ==
+             tab_resumption_prefs::kTabResumptionRegularUrlImpressions) {
+    base::UmaHistogramMemoryKB(
+        "IOS.MagicStack.ShopCard.TabResumptionRegular."
+        "ImpressionLimitStorageSize",
+        storage_size_kb);
+  } else if (pref_name ==
+             tab_resumption_prefs::kTabResumptionWithPriceDropUrlImpressions) {
+    base::UmaHistogramMemoryKB(
+        "IOS.MagicStack.ShopCard.TabResumptionWithPriceDrop."
+        "ImpressionLimitStorageSize",
+        storage_size_kb);
+  } else if (pref_name == tab_resumption_prefs::
+                              kTabResumptionWithPriceTrackableUrlImpressions) {
+    base::UmaHistogramMemoryKB(
+        "IOS.MagicStack.ShopCard.TabResumptionWithPriceTracking."
+        "ImpressionLimitStorageSize",
+        storage_size_kb);
+  }
 }
 
 std::optional<int> ImpressionLimitService::GetImpressionCount(
