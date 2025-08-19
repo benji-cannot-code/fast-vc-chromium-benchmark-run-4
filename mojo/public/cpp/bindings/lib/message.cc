e63c4423b34b54e78e7772e66a8754e0ebcfb593
Xiaohan Wang <xhwang@chromium.org>
2025-08-19 12:59:24 -0700

    mojo: Migrate BASE_FEATURE macros to 2-argument version
    This CL migrates three-argument BASE_FEATURE macros in //mojo to the
    new two-argument version where the C++ identifier and the string feature
    name match.
    
    This automated refactoring helps ensure that the C++ identifier and the
    string feature name always match, reducing potential errors.
    
    The following features could not be migrated automatically and may
    require manual intervention:
    
    *   kTaskPerMessage vs "MojoTaskPerMessage"
        *   Name has Mojo prefix.
    
    Bug: 436274260
    Change-Id: Iff778905d1290b6ba31d747e5490474905a4baea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6861702
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Xiaohan Wang <xhwang@chromium.org>
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1503578}

--- mojo/public/cpp/bindings/lib/message.cc
+++ mojo/public/cpp/bindings/lib/message.cc
@@ -36,9 +36,7 @@ namespace mojo {
 
 namespace {
 
-BASE_FEATURE(kMojoBindingsInlineSLS,
-             "MojoBindingsInlineSLS",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+BASE_FEATURE(MojoBindingsInlineSLS, base::FEATURE_ENABLED_BY_DEFAULT);
 
 base::GenericSequenceLocalStorageSlot<internal::MessageDispatchContext*>&
 GetSLSMessageDispatchContext() {
