883030b3d892f20970c5412cc3cd4908d2c29566
Jenna Himawan <jhimawan@google.com>
2025-08-19 09:42:21 -0700

    Add FromWebContents method for SelectionPopupController
    This is to enable crrev.com/c/6844383.
    
    Bug: 432007079
    Change-Id: I5b794de1c0cb66b854ed89b1cf817b951ef7d4b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6854375
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1503404}

--- content/public/android/java/src/org/chromium/content/browser/selection/SelectionPopupControllerImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/selection/SelectionPopupControllerImpl.java
@@ -254,6 +254,7 @@ public class SelectionPopupControllerImpl extends ActionModeCallbackHelper
      * @return {@link SelectionPopupController} object. {@code null} if not available because {@link
      *     #create()} is not called yet.
      */
+    @CalledByNative
     public static @Nullable SelectionPopupControllerImpl fromWebContents(WebContents webContents) {
         return webContents.getOrSetUserData(
                 SelectionPopupControllerImpl.class, UserDataFactoryLazyHolder.INSTANCE);
@@ -1972,6 +1973,11 @@ public class SelectionPopupControllerImpl extends ActionModeCallbackHelper
     @CalledByNative
     private static Rect createJavaRect(int x, int y, int right, int bottom) {
         return new Rect(x, y, right, bottom);
+    }
+
+    @CalledByNative
+    private long getNativePtr() {
+        return mNativeSelectionPopupController;
     }
 
     /**
