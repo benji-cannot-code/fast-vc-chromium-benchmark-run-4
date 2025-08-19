f67f0b5baaf088389d375d9c4784306db74f91cf
Sam Davis Omekara (from Dev Box) <samomekarajr@microsoft.com>
2025-08-19 11:27:45 -0700

    [Gap Decorations]: Add UseCounter for Disentanglement change
    This CL introduces granular use counters for the *-width CSS properties
    affected by the disentanglement work in crrev.com/c/6489213. These
    counters are property-specific to accurately measure usage and assess
    potential web compatibility risks resulting from the CSS issue
    resolution [1], which led to decoupling the *-width computed values from
    their corresponding style values.
    
    [1]:
    https://github.com/w3c/csswg-drafts/issues/11494#issuecomment-2675800489
    
    Bug: 393631108
    Change-Id: I787d348fab52f815564762ef2c0555ec01218763
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6839339
    Reviewed-by: Kevin Babbitt <kbabbitt@microsoft.com>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Commit-Queue: Sam Davis Omekara <samomekarajr@microsoft.com>
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1503490}

--- third_party/blink/renderer/core/style/computed_style.h
+++ third_party/blink/renderer/core/style/computed_style.h
@@ -293,6 +293,7 @@ class ComputedStyle final : public ComputedStyleBase {
   friend class css_longhand::BorderTopWidth;
   friend class css_longhand::ColumnRuleWidth;
   friend class css_longhand::OutlineWidth;
+  friend class ComputedStylePropertyMap;
   // Access to private Appearance() and HasAppearance().
   friend class LayoutTheme;
   friend class StyleAdjuster;
