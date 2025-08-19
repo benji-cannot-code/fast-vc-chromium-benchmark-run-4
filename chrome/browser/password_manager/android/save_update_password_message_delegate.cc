abbca38f9da413b443087abc4a6beb299fa3e328
Ioana Pandele <ioanap@chromium.org>
2025-08-19 06:23:18 -0700

    [UPMLoginDbSunset] Remove Play Services update warnings on password save
    Users with old versions of Play Services cannot save passwords anyway,
    so these warnings aren't actually shown.
    
    Bug: 433684502
    Change-Id: I9fc88b7b412fc426a7d8cb997eb9b44358e33a94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6832195
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1503300}

--- chrome/browser/password_manager/android/save_update_password_message_delegate.cc
+++ chrome/browser/password_manager/android/save_update_password_message_delegate.cc
@@ -45,9 +45,6 @@ using password_manager::PasswordForm;
 // Duration of message before timeout; 20 seconds.
 const int kMessageDismissDurationMs = 20000;
 
-constexpr base::TimeDelta kUpdateGMSCoreMessageDisplayDelay =
-    base::Milliseconds(500);
-
 }  // namespace
 
 SaveUpdatePasswordMessageDelegate::SaveUpdatePasswordMessageDelegate()
@@ -297,12 +294,6 @@ void SaveUpdatePasswordMessageDelegate::HandleSaveButtonClicked() {
 void SaveUpdatePasswordMessageDelegate::SavePassword() {
   if (!device_lock_bridge_->ShouldShowDeviceLockUi()) {
     passwords_state_.form_manager()->Save();
-    base::SequencedTaskRunner::GetCurrentDefault()->PostDelayedTask(
-        FROM_HERE,
-        base::BindOnce(
-            &SaveUpdatePasswordMessageDelegate::MaybeNudgeToUpdateGmsCore,
-            weak_ptr_factory_.GetWeakPtr()),
-        kUpdateGMSCoreMessageDisplayDelay);
     return;
   }
   device_lock_bridge_->LaunchDeviceLockUiIfNeededBeforeRunningCallback(
@@ -317,12 +308,6 @@ void SaveUpdatePasswordMessageDelegate::SavePasswordAfterDeviceLockUi(
   CHECK(device_lock_bridge_->RequiresDeviceLock());
   if (is_device_lock_requirement_met) {
     passwords_state_.form_manager()->Save();
-    base::SequencedTaskRunner::GetCurrentDefault()->PostDelayedTask(
-        FROM_HERE,
-        base::BindOnce(
-            &SaveUpdatePasswordMessageDelegate::MaybeNudgeToUpdateGmsCore,
-            weak_ptr_factory_.GetWeakPtr()),
-        kUpdateGMSCoreMessageDisplayDelay);
   }
   ClearState();
 }
@@ -500,15 +485,4 @@ SaveUpdatePasswordMessageDelegate::
       break;
   }
   return ui_dismissal_reason;
-}
-
-void SaveUpdatePasswordMessageDelegate::MaybeNudgeToUpdateGmsCore() {
-  if (passwords_state_.client()
-          ->GetPasswordFeatureManager()
-          ->ShouldUpdateGmsCore()) {
-    passwords_state_.client()->ShowPasswordManagerErrorMessage(
-        password_manager::ErrorMessageFlowType::kSaveFlow,
-        password_manager::PasswordStoreBackendErrorType::
-            kGMSCoreOutdatedSavingPossible);
-  }
 }
