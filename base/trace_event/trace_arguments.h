68dc2756b088a4113abd6fd7bdef9f8268ce6d57
Kent Tamura <tkent@chromium.org>
2025-08-18 20:16:09 -0700

    Remove `WTF::` in base/
    This CL has no behavior changes.
    
    Bug: 422768753
    Change-Id: I400edca3a79537e7c467274d5b9beb877420fc4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6857993
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1503117}

--- base/trace_event/trace_arguments.h
+++ base/trace_event/trace_arguments.h
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // identified by a name (a C string literal) and a value, which can be an
 // integer, enum, floating point, boolean, string pointer or reference, or
 // std::unique_ptr<ConvertableToTraceFormat> compatible values. Additionally,
-// custom data types need to be supported, like time values or WTF::CString.
+// custom data types need to be supported, like time values.
 //
 // TraceArguments is a helper class used to store 0 to 2 named arguments
 // corresponding to an individual trace macro call. As efficiently as possible,
@@ -131,7 +131,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Finally, it is possible to support initialization from custom values by
 // specializing the TraceValue::Helper<> template struct as described below.
 //
-// This is how values of custom types like WTF::CString can be passed directly
+// This is how values of custom types like base::Time can be passed directly
 // to trace macros.
 
 namespace base {
