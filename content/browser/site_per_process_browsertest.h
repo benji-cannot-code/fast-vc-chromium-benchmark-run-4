05a5477578cd9dbf22ba7e6c9ed407f9dcb1fcb2
Devon Loehr <dloehr@google.com>
2025-08-19 07:14:48 -0700

    Update classes to match chrome style requirements
    Recent changes to llvm have caused our style plugin to flag
    additional parts of the code that it missed before. Fix
    the warnings by adding various constructors/destructors,
    and/or moving their definitions to .cc files, as needed.
    
    This change applies to the following directories:
    content/browser
    
    This CL was uploaded by git cl split.
    
    R=boliu@chromium.org
    
    Bug: 437910658
    Change-Id: I566cd60782494a624d40388d082ccc1558c34b7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6860302
    Auto-Submit: Devon Loehr <dloehr@google.com>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Devon Loehr <dloehr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1503324}

--- content/browser/site_per_process_browsertest.h
+++ content/browser/site_per_process_browsertest.h
@@ -55,6 +55,7 @@ class SitePerProcessBrowserTest
       public ::testing::WithParamInterface<std::string> {
  public:
   SitePerProcessBrowserTest();
+  ~SitePerProcessBrowserTest() override;
 
   SitePerProcessBrowserTest(const SitePerProcessBrowserTest&) = delete;
   SitePerProcessBrowserTest& operator=(const SitePerProcessBrowserTest&) =
---
+++ content/browser/speech/speech_recognizer.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/speech/speech_recognizer.h"
+
+#include "content/public/browser/speech_recognition_event_listener.h"
+
+namespace content {
+
+SpeechRecognizer::SpeechRecognizer(SpeechRecognitionEventListener* listener,
+                                   int session_id)
+    : listener_(listener), session_id_(session_id) {
+  DCHECK(listener_);
+}
+
+}  // namespace content
