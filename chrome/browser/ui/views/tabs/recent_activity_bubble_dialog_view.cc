0be2f93470853d483c440d449fc5091d0bc8dcd4
dljames <dljames@chromium.org>
2025-08-19 10:45:12 -0700

    [Vertical Tabs] Use GetTabGroupAnchorView where applicable
    Updates callers of TabStrip::group_header to use
    TabStripViewInterface::GetTabGroupAnchorView where applicable.
    
    Remaining uses of TabStrip::group_header will need to be special cased
    in follow-up changes.
    
    Change-Id: I11502ae70b1c1f2a1ff1fe34634ae6b87be24a5d
    Bug: 436873217
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6854386
    Commit-Queue: Darryl James <dljames@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1503452}

--- chrome/browser/ui/views/tabs/recent_activity_bubble_dialog_view.cc
+++ chrome/browser/ui/views/tabs/recent_activity_bubble_dialog_view.cc
@@ -31,8 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/page_action/collaboration_messaging_page_action_icon_view.h"
 #include "chrome/browser/ui/views/tabs/tab_group_editor_bubble_view.h"
-#include "chrome/browser/ui/views/tabs/tab_group_header.h"
-#include "chrome/browser/ui/views/tabs/tab_strip.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/collaboration/public/collaboration_flow_entry_point.h"
 #include "components/collaboration/public/collaboration_service.h"
@@ -630,9 +628,10 @@ void RecentActivityRowView::OpenTabGroupEditDialog() {
     return;
   }
 
-  if (auto* tab_group_header = BrowserView::GetBrowserViewForBrowser(browser)
-                                   ->tabstrip()
-                                   ->group_header(group_id.value())) {
+  if (views::View* tab_group_header =
+          BrowserView::GetBrowserViewForBrowser(browser)
+              ->tab_strip_view()
+              ->GetTabGroupAnchorView(group_id.value())) {
     TabGroupEditorBubbleView::Show(browser, group_id.value(), tab_group_header,
                                    /*anchor_rect=*/std::nullopt,
                                    /*stop_context_menu_propagation=*/false);
