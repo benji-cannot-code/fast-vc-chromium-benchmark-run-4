ecef7881e0f4df1b72d208972b19795cd76da121
Tiago Vignatti <tvignatti@igalia.com>
2025-08-19 10:18:14 -0700

    Reland "Componentize chrome/browser/ui/views/location_bar/cookie_controls (3/x)"
    This is a reland of https://crrev.com/c/6849713, which was reverted by
    https://crrev.com/c/6859636, because
    cookie_controls_bubble_view_pixel_test.cc, from interactive_ui_tests
    target, was trying to include test_browser_dialog.h but the target
    didn't have the proper dependency. This CL fixes that.
    
    Original message:
    
    Extract cookie_controls from the monolithic //chrome/browser/ui:ui
    target into its own modularized BUILD.gn file with separate header and
    implementation targets.
    
    The following GN targets are added:
     - c/b/ui/views/location_bar/cookie_controls:cookie_controls
     - c/b/ui/views/location_bar/cookie_controls:impl
     - c/b/ui/views/location_bar/cookie_controls:browser_tests
     - c/b/ui/views/location_bar/cookie_controls:interactive_ui_tests
     - c/b/ui/views/location_bar/cookie_controls:unit_tests
    
    Bug: 434882143
    Change-Id: I8af370528ee4b02fc798356f83bd566a6c8ea956
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6859742
    Reviewed-by: Antonio Gomes <tonikitoo@igalia.com>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Auto-Submit: Tiago Vignatti <tvignatti@igalia.com>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1503430}

--- chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_bubble_coordinator.cc
+++ chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_bubble_coordinator.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/page_action/page_action_icon_type.h"
 #include "chrome/browser/ui/views/frame/toolbar_button_provider.h"
+#include "chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_bubble_view_controller.h"
+#include "chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_bubble_view_impl.h"
 #include "chrome/browser/ui/views/page_action/page_action_icon_view.h"
 #include "ui/views/bubble/bubble_dialog_delegate_view.h"
 
