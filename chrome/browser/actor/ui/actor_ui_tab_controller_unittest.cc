6660727bf02c7f4c631814e952d11c401e3464b7
Kene Okoye <kenok@google.com>
2025-08-19 15:44:43 -0700

    [ActorUiTabController] Move mock factory into a dedicated file
    Bug: 438255493
    Change-Id: I519f5daab2ed85e2a3e507218931b5f1f78e04da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6863795
    Reviewed-by: Christine Ying <chrstne@google.com>
    Commit-Queue: Kene Okoye <kenok@google.com>
    Cr-Commit-Position: refs/heads/main@{#1503668}

--- chrome/browser/actor/ui/actor_ui_tab_controller_unittest.cc
+++ chrome/browser/actor/ui/actor_ui_tab_controller_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/actor/ui/actor_ui_tab_controller_interface.h"
 #include "chrome/browser/actor/ui/mocks/mock_actor_overlay_view_controller.h"
 #include "chrome/browser/actor/ui/mocks/mock_actor_ui_state_manager.h"
+#include "chrome/browser/actor/ui/mocks/mock_actor_ui_tab_controller_factory.h"
 #include "chrome/browser/actor/ui/mocks/mock_handoff_button_controller.h"
 #include "chrome/browser/actor/ui/states/actor_overlay_state.h"
 #include "chrome/browser/actor/ui/states/handoff_button_state.h"
@@ -56,40 +57,6 @@ MockWebContents::MockWebContents(content::BrowserContext* browser_context)
     : TestWebContents(browser_context) {}
 
 MockWebContents::~MockWebContents() = default;
-
-class MockActorUiTabControllerFactory
-    : public ActorUiTabControllerFactoryInterface {
- public:
-  ~MockActorUiTabControllerFactory() override {
-    mock_overlay_view_controller_ = nullptr;
-    mock_handoff_button_controller_ = nullptr;
-  }
-
-  std::unique_ptr<HandoffButtonController> CreateHandoffButtonController(
-      tabs::TabInterface& tab) override {
-    auto controller = std::make_unique<MockHandoffButtonController>(tab);
-    mock_handoff_button_controller_ = controller.get();
-    return controller;
-  }
-  std::unique_ptr<ActorOverlayViewController> CreateActorOverlayViewController(
-      tabs::TabInterface& tab) override {
-    auto controller = std::make_unique<MockActorOverlayViewController>(tab);
-    mock_overlay_view_controller_ = controller.get();
-    return controller;
-  }
-
-  MockActorOverlayViewController* overlay_controller() {
-    return mock_overlay_view_controller_;
-  }
-
-  MockHandoffButtonController* handoff_button_controller() {
-    return mock_handoff_button_controller_;
-  }
-
- private:
-  raw_ptr<MockActorOverlayViewController> mock_overlay_view_controller_;
-  raw_ptr<MockHandoffButtonController> mock_handoff_button_controller_;
-};
 
 class ActorUiTabControllerTest : public testing::Test {
  public:
---
+++ chrome/browser/actor/ui/mocks/mock_actor_ui_tab_controller_factory.cc
@@ -0,0 +1,46 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/actor/ui/mocks/mock_actor_ui_tab_controller_factory.h"
+
+#include "chrome/browser/actor/ui/mocks/mock_actor_overlay_view_controller.h"
+#include "chrome/browser/actor/ui/mocks/mock_handoff_button_controller.h"
+
+namespace actor::ui {
+
+MockActorUiTabControllerFactory::MockActorUiTabControllerFactory() = default;
+
+MockActorUiTabControllerFactory::~MockActorUiTabControllerFactory() {
+  mock_overlay_view_controller_ = nullptr;
+  mock_handoff_button_controller_ = nullptr;
+}
+
+std::unique_ptr<HandoffButtonController>
+MockActorUiTabControllerFactory::CreateHandoffButtonController(
+    tabs::TabInterface& tab) {
+  auto controller = std::make_unique<MockHandoffButtonController>(tab);
+  mock_handoff_button_controller_ = controller.get();
+  return controller;
+}
+
+std::unique_ptr<ActorOverlayViewController>
+MockActorUiTabControllerFactory::CreateActorOverlayViewController(
+    tabs::TabInterface& tab) {
+  auto controller = std::make_unique<MockActorOverlayViewController>(tab);
+  mock_overlay_view_controller_ = controller.get();
+  return controller;
+}
+
+MockActorOverlayViewController*
+MockActorUiTabControllerFactory::overlay_controller() {
+  return mock_overlay_view_controller_;
+}
+
+MockHandoffButtonController*
+MockActorUiTabControllerFactory::handoff_button_controller() {
+  return mock_handoff_button_controller_;
+}
+
+}  // namespace actor::ui
