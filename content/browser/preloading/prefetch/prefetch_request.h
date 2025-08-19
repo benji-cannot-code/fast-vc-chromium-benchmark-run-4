ee181bee399446967a086e01494e490dcc66c229
Hiroshige Hayashizaki <hiroshige@chromium.org>
2025-08-19 11:56:02 -0700

    Move additional_headers_ to PrefetchRequest
    Bug: 437631382
    Change-Id: Ib58dabcef8ef0954ce6b55a8c5cf7a5f49239638
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6843127
    Reviewed-by: Taiyo Mizuhashi <taiyo@chromium.org>
    Reviewed-by: Ken Okada <kenoss@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1503525}

--- content/browser/preloading/prefetch/prefetch_request.h
+++ content/browser/preloading/prefetch/prefetch_request.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/content_export.h"
 #include "content/public/browser/global_routing_id.h"
 #include "net/http/http_no_vary_search_data.h"
+#include "net/http/http_request_headers.h"
 #include "services/metrics/public/cpp/ukm_source_id.h"
 #include "url/origin.h"
 
@@ -132,6 +133,7 @@ class CONTENT_EXPORT PrefetchRequest final {
       const std::optional<url::Origin>& referring_origin,
       base::WeakPtr<BrowserContext> browser_context,
       std::optional<SpeculationRulesTags> speculation_rules_tags,
+      const net::HttpRequestHeaders& additional_headers,
       std::optional<PreloadingHoldbackStatus> holdback_status_override,
       std::variant<PrefetchRendererInitiatorInfo, PrefetchBrowserInitiatorInfo>
           info);
@@ -155,6 +157,9 @@ class CONTENT_EXPORT PrefetchRequest final {
     return speculation_rules_tags_;
   }
 
+  const net::HttpRequestHeaders& additional_headers() const {
+    return additional_headers_;
+  }
   const std::optional<PreloadingHoldbackStatus>& holdback_status_override()
       const {
     return holdback_status_override_;
@@ -219,6 +224,11 @@ class CONTENT_EXPORT PrefetchRequest final {
 
   // -------- Parameters that can have non-default values only for
   // -------- browser-initiated prefetches:
+
+  // Additional headers for WebView initiated prefetch.
+  // This must be empty for non-WebView initiated prefetches.
+  // TODO(crbug.com/369859822): Revisit the semantics if needed.
+  const net::HttpRequestHeaders additional_headers_;
 
   // If set, this value is used to override holdback status derived by the
   // normal process. It is set to `attempt_` on
