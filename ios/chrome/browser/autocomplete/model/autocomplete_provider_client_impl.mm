cf52cd959199d38ce6cb008d2e99391e51aa87a5
Christian Xu <christianxu@google.com>
2025-08-19 09:26:31 -0700

    [IOS][AIM] Add an AimEligibilityService for IOS
    Add IOSChromeAimEligibilityService with its factory. The service is used
    to determine AIM eligibility for a specific profile. The iOS
    implementation provides native logic for locale and country.
    
    Bug: 437913345
    Change-Id: Idc3b39459750ed98f8b63ba7e0a1cad61c0bdd06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6851051
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Auto-Submit: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1503391}

--- ios/chrome/browser/autocomplete/model/autocomplete_provider_client_impl.mm
+++ ios/chrome/browser/autocomplete/model/autocomplete_provider_client_impl.mm
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/signin/public/identity_manager/identity_manager.h"
 #import "components/sync/service/sync_service.h"
 #import "components/unified_consent/url_keyed_data_collection_consent_helper.h"
+#import "ios/chrome/browser/aim/model/ios_chrome_aim_eligibility_service_factory.h"
 #import "ios/chrome/browser/autocomplete/model/autocomplete_classifier_factory.h"
 #import "ios/chrome/browser/autocomplete/model/autocomplete_scoring_model_service_factory.h"
 #import "ios/chrome/browser/autocomplete/model/in_memory_url_index_factory.h"
@@ -213,7 +214,7 @@ AutocompleteProviderClientImpl::GetTabGroupSyncService() const {
 
 AimEligibilityService*
 AutocompleteProviderClientImpl::GetAimEligibilityService() const {
-  return nullptr;
+  return IOSChromeAimEligibilityServiceFactory::GetForProfile(profile_);
 }
 
 std::string AutocompleteProviderClientImpl::GetAcceptLanguages() const {
--- ios/chrome/browser/shared/model/prefs/browser_prefs.mm
+++ ios/chrome/browser/shared/model/prefs/browser_prefs.mm
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/network_time/network_time_tracker.h"
 #import "components/ntp_tiles/most_visited_sites.h"
 #import "components/ntp_tiles/popular_sites_impl.h"
+#import "components/omnibox/browser/aim_eligibility_service.h"
 #import "components/omnibox/browser/omnibox_prefs.h"
 #import "components/omnibox/browser/zero_suggest_provider.h"
 #import "components/optimization_guide/core/model_execution/model_execution_prefs.h"
@@ -687,6 +688,7 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
   autofill::prefs::RegisterProfilePrefs(registry);
   collaboration::prefs::RegisterProfilePrefs(registry);
   commerce::RegisterPrefs(registry);
+  AimEligibilityService::RegisterProfilePrefs(registry);
   dom_distiller::DistilledPagePrefs::RegisterProfilePrefs(registry);
   enterprise::RegisterIdentifiersProfilePrefs(registry);
   enterprise_connectors::RegisterProfilePrefs(registry);
