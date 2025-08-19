5ed2be918bfa92adc9e5c729bf3e917ac889b8e0
Vasilii Sukhanov <vasilii@chromium.org>
2025-08-19 04:57:21 -0700

    Remove unnecessary include in chrome/browser/ui/passwords/ui_utils.cc
    Fixed: 439696328
    Change-Id: I08523f0fdc5259d0ec4efc679483284b258b3b35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6858857
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Auto-Submit: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1503264}

--- chrome/browser/ui/passwords/ui_utils.cc
+++ chrome/browser/ui/passwords/ui_utils.cc
@@ -51,7 +51,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if !BUILDFLAG(IS_ANDROID)
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/user_education/show_promo_in_page.h"
-#include "chrome/browser/ui/webui/password_manager/password_manager_ui.h"
 #endif
 
 namespace {
