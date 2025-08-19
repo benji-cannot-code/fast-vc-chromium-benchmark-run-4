8375b4cf86865d0b083787d4de1cd4f952eb8e53
Zekun Jiang <zekunjiang@google.com>
2025-08-19 11:51:07 -0700

    Add bert safety op resolver.
    Bug: 435495018
    Change-Id: I468742ffcb5665173cf16624046e1abb5d0eb314
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6862524
    Commit-Queue: Zekun Jiang <zekunjiang@google.com>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1503517}

---
+++ services/on_device_model/safety/bert_safety_op_resolver.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SERVICES_ON_DEVICE_MODEL_SAFETY_BERT_SAFETY_OP_RESOLVER_H_
+#define SERVICES_ON_DEVICE_MODEL_SAFETY_BERT_SAFETY_OP_RESOLVER_H_
+
+#include "components/optimization_guide/core/tflite_op_resolver.h"
+
+namespace ml {
+
+// TFLite Op Resolver for the Bert Safety model.
+class BertSafetyOpResolver : public optimization_guide::TFLiteOpResolver {
+ public:
+  BertSafetyOpResolver();
+};
+
+}  // namespace ml
+
+#endif  // SERVICES_ON_DEVICE_MODEL_SAFETY_BERT_SAFETY_OP_RESOLVER_H_
