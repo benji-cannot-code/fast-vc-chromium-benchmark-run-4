a599d7f0ef94f6e023d0a6f2862959c48b848194
Tanmoy Mollik <triploblastic@google.com>
2025-08-20 02:36:03 -0700

    Use OAuthConsumerId for PrimaryAccountAccessTokenFetcher clients - 7
    This cl is part of a series that converts
    PrimaryAccountAccessTokenFetcher clients to use OAuthConsumerId based
    constructor instead.
    
    Bug: 425896213
    Change-Id: I3afe74d44504f8c843e634193d061b7acc249ed5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6828796
    Reviewed-by: Menghan Yang <myuu@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1503820}

--- chrome/browser/feedback/feedback_uploader_chrome.cc
+++ chrome/browser/feedback/feedback_uploader_chrome.cc
@@ -36,8 +36,6 @@ namespace {
 constexpr char kAuthenticationErrorLogMessage[] =
     "Feedback report will be sent without authentication.";
 
-constexpr char kConsumer[] = "feedback_uploader_chrome";
-
 void QueueSingleReport(base::WeakPtr<feedback::FeedbackUploader> uploader,
                        scoped_refptr<FeedbackReport> report) {
   content::GetUIThreadTaskRunner({})->PostTask(
@@ -131,11 +129,9 @@ void FeedbackUploaderChrome::StartDispatchingReport() {
   // has its own privacy notice.
   if (identity_manager &&
       identity_manager->HasPrimaryAccount(signin::ConsentLevel::kSignin)) {
-    signin::ScopeSet scopes;
-    scopes.insert(GaiaConstants::kSupportContentOAuth2Scope);
     primary_account_token_fetcher_ =
         std::make_unique<signin::PrimaryAccountAccessTokenFetcher>(
-            kConsumer, identity_manager, scopes,
+            signin::OAuthConsumerId::kFeedbackUploader, identity_manager,
             base::BindOnce(
                 &FeedbackUploaderChrome::PrimaryAccountAccessTokenAvailable,
                 base::Unretained(this)),
@@ -157,6 +153,7 @@ void FeedbackUploaderChrome::StartDispatchingReport() {
   bool isMeetDevice =
       policy::EnrollmentRequisitionManager::IsMeetDevice();
   if (isMeetDevice && !device_identity_provider->GetActiveAccountId().empty()) {
+    char kConsumer[] = "feedback_uploader";
     OAuth2AccessTokenManager::ScopeSet scopes;
     scopes.insert(GaiaConstants::kSupportContentOAuth2Scope);
     active_account_token_fetcher_ = device_identity_provider->FetchAccessToken(
