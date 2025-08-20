ed7f14c3da65443ecabe5c5779704b08e1bc1fa2
Zoraiz Naeem <zoraiznaeem@chromium.org>
2025-08-20 16:30:14 -0700

    Reland "Remove unnecessary layers for ResizeToggleMenu bubble"
    This is a reland of commit cc4ec44edaa1a1a66cc02b0aa2441202c6d21ae7
    
    Change: Updated the ownership model of the widget
    
    Original change's description:
    > Remove unnecessary layers for ResizeToggleMenu bubble
    >
    > First of all, instead of painting a transparent bubble (and use a
    > TEXTURE layer), use NOT_DRAWN_LAYER instead of the bubble widget.
    >
    > By correctly structuring the layers/views, we can get rid of additional
    > layers used on delegate view and container view.
    >
    > This CL also migrates bubble to use BubbleDialogDelegate interface
    > instead of BubbleDialogDelegateView.
    >
    > Bug: b:436348660
    > Change-Id: I0157807098c314548b0339aebeb020055c658093
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6820120
    > Reviewed-by: Elly FJ <ellyjones@chromium.org>
    > Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1497223}
    
    Bug: b:436348660
    Change-Id: Icd1c902e4eb2ce2a0db0e8cc7f982f3c0a9c37cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6825541
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1504266}

--- ui/views/bubble/bubble_dialog_delegate_view.h
+++ ui/views/bubble/bubble_dialog_delegate_view.h
@@ -95,7 +95,6 @@ namespace arc {
 class ArcSplashScreenDialogView;
 class BaseDialogDelegateView;
 class ResizeConfirmationDialogView;
-class RoundedCornerBubbleDialogDelegateView;
 
 namespace input_overlay {
 class DeleteEditShortcut;
@@ -812,7 +811,6 @@ class VIEWS_EXPORT BubbleDialogDelegateView : public View,
   friend class ::arc::ArcSplashScreenDialogView;
   friend class ::arc::BaseDialogDelegateView;
   friend class ::arc::ResizeConfirmationDialogView;
-  friend class ::arc::RoundedCornerBubbleDialogDelegateView;
   friend class ::arc::input_overlay::DeleteEditShortcut;
   friend class ::arc::input_overlay::RichNudge;
   friend class ::ash::AnchoredNudge;
