1c6f814b66dd5b2f24269f4995d9713a87f2d85b
Ernesto Izquierdo Clua <eic@google.com>
2025-08-20 07:05:01 -0700

    Reenable testEnterpriseBlockingPage
    The test was flaky on VM bots because they are slower and loading
    Enterprise profiles takes longer. We tested with multiple timeouts for
    profile loading and 20 seconds seems to be enough.
    
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-noncq
    Bug: 429137228
    Change-Id: I1588629f52a67b92db562d725bbf7cbc7b6e82b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6829090
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Reviewed-by: Daniel White <danieltwhite@google.com>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1503916}

--- ios/chrome/test/app/chrome_test_util.mm
+++ ios/chrome/test/app/chrome_test_util.mm
@@ -63,7 +63,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-inline constexpr base::TimeDelta kProfileCreationTimeout = base::Seconds(10);
+inline constexpr base::TimeDelta kProfileCreationTimeout = base::Seconds(20);
 
 // Returns the original ProfileIOS if `incognito` is false. If
 // `incognito` is true, returns an off-the-record ProfileIOS.
