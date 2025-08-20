816c0b369883fc9459def259b7cb9c1847e6783d
Minseop Choi <minsubb13@gmail.com>
2025-08-19 20:23:34 -0700

    crostini: migrate to new crypto/hash APIs
    Migrate baguette_download to crypto/hash APIs instead of crypto/sha2 and
    crypto/secure_hash.
    
    Bug: 372283556
    Change-Id: I6d7bcd2cf833a0401a28f268d0d90072555bd599
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6851080
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Commit-Queue: Minseop Choi <minsubb13@gmail.com>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1503737}

--- chrome/browser/ash/crostini/baguette_download.cc
+++ chrome/browser/ash/crostini/baguette_download.cc
@@ -17,8 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/thread_pool.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
-#include "crypto/secure_hash.h"
-#include "crypto/sha2.h"
+#include "crypto/hash.h"
 #include "net/base/load_flags.h"
 #include "net/base/net_errors.h"
 #include "services/network/public/cpp/resource_request.h"
@@ -72,8 +71,7 @@ std::string Sha256File(const base::FilePath& path) {
     return "";
   }
 
-  std::unique_ptr<crypto::SecureHash> ctx(
-      crypto::SecureHash::Create(crypto::SecureHash::SHA256));
+  crypto::hash::Hasher ctx(crypto::hash::kSha256);
   std::array<uint8_t, 4096> buffer;
   while (true) {
     std::optional<size_t> read = file.ReadAtCurrentPos(buffer);
@@ -81,11 +79,11 @@ std::string Sha256File(const base::FilePath& path) {
     if (read.value_or(0) == 0) {
       break;
     }
-    ctx->Update(base::span(buffer).first(*read));
+    ctx.Update(base::span(buffer).first(*read));
   }
 
-  std::array<uint8_t, crypto::kSHA256Length> digest_bytes;
-  ctx->Finish(digest_bytes);
+  std::array<uint8_t, crypto::hash::kSha256Size> digest_bytes;
+  ctx.Finish(digest_bytes);
   return base::HexEncode(digest_bytes);
 }
 
