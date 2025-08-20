f71a7d22a03c8dc12af56a2bfbadea642285dd11
Shanthanu Bhardwaj <xanth@google.com>
2025-08-20 10:57:55 -0700

    Add "COMPONENT" extension source in proto path too.
    Component was added only to the legacy path and not protos in crrev.com/c/6579765. This adds it to that path and adds unit tests for the `extensionSource` field.
    
    BUG=379715001
    
    Change-Id: I19e9a2a437357e1a248e0366eac19c48151d8b30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6867449
    Reviewed-by: Haihan Chen <haihan@google.com>
    Commit-Queue: Shanthanu Bhardwaj <xanth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1504071}

--- chrome/browser/enterprise/connectors/reporting/extension_install_event_router.h
+++ chrome/browser/enterprise/connectors/reporting/extension_install_event_router.h
@@ -59,6 +59,9 @@ class ExtensionInstallEventRouter
       const extensions::Extension* extension,
       const ::chrome::cros::reporting::proto::BrowserExtensionInstallEvent::
           ExtensionAction extension_action);
+  static ::chrome::cros::reporting::proto::BrowserExtensionInstallEvent::
+      ExtensionSource
+      GetExtensionSource(const extensions::Extension* extension);
 };
 
 class ExtensionInstallEventRouterFactory
