da614f978999b471cfd26fd6ab003f4d1b878c95
Brandon Fong <bjfong@google.com>
2025-08-20 13:24:35 -0700

    [Color Picker] Null check the anchored pop up window to prevent crash
    Null check the anchored pop up window when dismissing as there is a
    possible race condition that could cause a crash.
    
    Bug: 439969452
    Change-Id: I264b5ecd82a6c3ebd66767e7fe9f41ec2fd11ca5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6867392
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1504166}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogCoordinator.java
@@ -417,9 +417,10 @@ public class TabGridDialogCoordinator implements TabGridDialogMediator.DialogCon
                         mModel.get(TabGridDialogProperties.IS_INCOGNITO),
                         ColorPickerLayoutType.DOUBLE_ROW,
                         () -> {
-                            assumeNonNull(mColorIconPopupWindow);
-                            mColorIconPopupWindow.dismiss();
-                            mColorIconPopupWindow = null;
+                            if (mColorIconPopupWindow != null) {
+                                mColorIconPopupWindow.dismiss();
+                                mColorIconPopupWindow = null;
+                            }
                             onDismissListener.onDismiss();
                         });
         mColorPickerCoordinator.setSelectedColorItem(
