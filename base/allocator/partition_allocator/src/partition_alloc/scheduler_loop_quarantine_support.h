15198d414610df36024e345ecb1076748dbc8b5a
David Sanders <dsanders11@ucsbalum.com>
2025-08-20 00:18:20 -0700

    Remove unused include in scheduler_loop_quarantine_support.h
    Bug: 40318405
    Change-Id: Ic62b45d6edf2a486bcb7437a79cf9c498c7b0acd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6820151
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#1503783}

--- base/allocator/partition_allocator/src/partition_alloc/scheduler_loop_quarantine_support.h
+++ base/allocator/partition_allocator/src/partition_alloc/scheduler_loop_quarantine_support.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef PARTITION_ALLOC_SCHEDULER_LOOP_QUARANTINE_SUPPORT_H_
 #define PARTITION_ALLOC_SCHEDULER_LOOP_QUARANTINE_SUPPORT_H_
 
-#include <map>
 #include <optional>
 #include <variant>
 
