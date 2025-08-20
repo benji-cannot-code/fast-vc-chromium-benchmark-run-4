a720fde64353220907e6d8db7035eea0290c47df
Trevor Perrier <perrier@chromium.org>
2025-08-20 14:16:46 -0700

    Reland "Add sidepanel attached mode for Glic"
    This is a reland of commit 9333841ff5eb0ad3229e70f106dc32cfad9f5fb6
    
    The diff for the fix is here: https://chromium-review.googlesource.com/c/chromium/src/+/6866765/1..2
    
    The core change is for the glic_side_panel_coordinator to be created
    even for non-normal Browser types. A check is done so that the
    side panel entery is not added if the browser is not a normal type.
    
    Original change's description:
    > Add sidepanel attached mode for Glic
    >
    > Adapt the old infrastructure in GlicWindowController for an attached
    > mode glic window to use the sidepanel. Attached mode was removed back in
    > March but the code was left in. The old code partially worked and had to
    > be reimplemented for sidepanel.
    >
    > The bulk of the change is in `glic_window_contorller_impl` which can now
    > place the glic window in either the side panel or floating window. When
    > it is in the side panel there is only a GlicView and no GlicWidget.
    >
    > Methods that no longer make sense for an attached sidepanel have been
    > removed including:
    >   * GetInitialAttachedBounds
    >   * GetTopRightPositionForAttachedGlicWindow
    >   * MovePositionToBrowserGlicButton
    >
    > Previously, there had been an assumption that if Glic is showing the
    > GlicWidget exists. Having a sidepanel breaks this assumption so most the
    > rest of the CL outside of the window controller checks that the panel is detached before accessing GlicWidget.
    >
    > See screencast: https://screencast.googleplex.com/cast/NjY5NzgwOTQwNjMyODgzMnw4NmE1MmNiNi1mNw
    >
    > Bug for adding more unit tests: b:439838289
    >
    > Bug: b:435490133
    > Change-Id: I763ef7c11855ffcbac3693dcca32be93ae504611
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6844012
    > Reviewed-by: Erik Chen <erikchen@chromium.org>
    > Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    > Commit-Queue: Trevor Perrier <perrier@chromium.org>
    > Reviewed-by: Darryl James <dljames@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1503822}
    
    Bug: b:435490133
    Change-Id: I94971e89e02d336c065eae23d737fa936095cdd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6866765
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Trevor Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1504205}

--- chrome/browser/ui/views/side_panel/side_panel_util.cc
+++ chrome/browser/ui/views/side_panel/side_panel_util.cc
@@ -31,6 +31,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/accessibility_features.h"
 #include "ui/actions/actions.h"
 
+#if BUILDFLAG(ENABLE_GLIC)
+#include "chrome/browser/glic/public/glic_enabling.h"
+#include "chrome/browser/glic/widget/glic_side_panel_coordinator.h"
+#endif
+
 // static
 void SidePanelUtil::PopulateGlobalEntries(Browser* browser,
                                           SidePanelRegistry* window_registry) {
@@ -65,6 +70,14 @@ void SidePanelUtil::PopulateGlobalEntries(Browser* browser,
         ->comments_side_panel_coordinator()
         ->CreateAndRegisterEntry(window_registry);
   }
+#if BUILDFLAG(ENABLE_GLIC)
+  if (glic::GlicEnabling::IsEnabledForProfile(browser->profile()) &&
+      browser->is_type_normal()) {
+    browser->browser_window_features()
+        ->glic_side_panel_coordinator()
+        ->CreateAndRegisterEntry(window_registry);
+  }
+#endif
 }
 
 SidePanelContentProxy* SidePanelUtil::GetSidePanelContentProxy(
