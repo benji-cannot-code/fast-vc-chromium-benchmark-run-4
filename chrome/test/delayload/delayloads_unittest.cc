2e882112186053bbfd13624f30d81a62bc9480d6
Alex Gough <ajgo@chromium.org>
2025-08-20 12:01:08 -0700

    Update delayloads unittest to reflect current delayloads
    UIAutomationCore was delayloaded at least from 121.0.6135.0 and Advapi32
    was delayloaded at least from 103.0.5030.0 so we can remove them from
    the allowlist to prevent backsliding.
    
    Bug: 439521000
    Change-Id: I083b276c7c1f182b506aa32e0402af0c02ad011a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6863176
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1504118}

--- chrome/test/delayload/delayloads_unittest.cc
+++ chrome/test/delayload/delayloads_unittest.cc
@@ -210,8 +210,9 @@ class MinimumWindowsSupportTest : public DelayloadsTest {
 
 // Run this test only in Release builds.
 //
-// These tests make sure that chrome.dll, chrome_child.dll, and chrome.exe
-// have only certain types of imports.
+// These tests make sure that chrome.dll, and chrome.exe have only certain types
+// of imports.
+//
 // In particular, we explicitly want to ensure user32.dll and its many related
 // dlls are delayloaded and not automatically brought in via some other
 // dependent dll. The primary reason for this is that the sandbox for the
@@ -245,7 +246,6 @@ TEST_F(DelayloadsTest, ChromeDllDelayloadsCheck) {
       "KERNEL32.dll",
       "chrome_elf.dll",
       "DWrite.dll",
-      "ADVAPI32.dll",
       "CRYPT32.dll",
       "dbghelp.dll",
       "dhcpcsvc.DLL",
@@ -253,7 +253,6 @@ TEST_F(DelayloadsTest, ChromeDllDelayloadsCheck) {
       "ntdll.dll",
       "OLEAUT32.dll",
       "Secur32.dll",
-      "UIAutomationCore.DLL",
       "USERENV.dll",
       "WINHTTP.dll",
       "WINMM.dll",
