0746b2b4a222260c4cf44edcbaaac3e409ed7dfc
Ewann Pelle <ewannpv@chromium.org>
2025-08-20 08:33:24 -0700

    [iOS] Cleans MDC progress view import
    Fixed: 438151254
    Change-Id: I17a28cb200bebdc038e888177ce694b8dd9ccc1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6866133
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1503967}

--- ios/chrome/browser/toolbar/ui_bundled/adaptive_toolbar_view_controller.mm
+++ ios/chrome/browser/toolbar/ui_bundled/adaptive_toolbar_view_controller.mm
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/toolbar/ui_bundled/adaptive_toolbar_view_controller.h"
 
-#import <MaterialComponents/MaterialProgressView.h>
-
 #import "base/metrics/user_metrics.h"
 #import "base/notreached.h"
 #import "base/time/time.h"
