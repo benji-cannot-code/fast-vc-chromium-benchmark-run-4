91b8f5c3369b9181aa5ca0983de159fa3c3f1a3e
Alex Ilin <alexilin@chromium.org>
2025-08-20 08:55:54 -0700

    Revert "Delete unreachable "update gms" in PasswordManagerErrorMessageDelegate"
    This reverts commit 14f71c8c59c1085573ebebc56fef8a5caefb229e.
    
    Reason for revert: PasswordGenerationFrameHelperTest failures on
    "android-automotive-12l-x64-rel-tests" (https://crbug.com/440019138)
    
    Bug: 433684502
    Original change's description:
    > Delete unreachable "update gms" in PasswordManagerErrorMessageDelegate
    >
    > This is a follow-up to crrev.com/c/6832195.
    > PasswordManagerClient::ShowPasswordManagerErrorMessage() does nothing
    > for kGMSCoreOutdatedSavingPossible and kGMSCoreOutdatedSavingDisabled
    > these days. The enum values can be deleted, along with the corresponding
    > UI code.
    > https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/password_manager/chrome_password_manager_client.cc;l=523;drc=c711efae5816b0a02668f4aca7f9bd94ad3860bb
    >
    > Semi-related, this CL also replaces
    > PasswordFeatureManager::ShouldUpdateGmsCore() with a direct call
    > to IsGmsCoreUpdateRequired(), since the latter is equally easy to
    > unit test.
    >
    > OBSOLETE_HISTOGRAMS=The "update gms" password manager message was deleted.
    >
    > Bug: 433684502
    > Change-Id: Ic9d8addef1db64adbd6de06a724a9c9b4229f270
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6862993
    > Auto-Submit: Victor Vianna <victorvianna@google.com>
    > Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    > Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    > Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1503877}
    
    Bug: 433684502, 440019138
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ife78f05c0c33cc7977036dd5806c23e6dbbe5226
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6863279
    Auto-Submit: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Alex Ilin <alexilin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1503983}

--- components/password_manager/core/browser/mock_password_feature_manager.h
+++ components/password_manager/core/browser/mock_password_feature_manager.h
@@ -28,6 +28,10 @@ class MockPasswordFeatureManager : public PasswordFeatureManager {
               IsBiometricAuthenticationBeforeFillingEnabled,
               (),
               (override, const));
+
+#if BUILDFLAG(IS_ANDROID)
+  MOCK_METHOD(bool, ShouldUpdateGmsCore, (), (override));
+#endif  // BUILDFLAG(IS_ANDROID)
 };
 
 }  // namespace password_manager
--- components/password_manager/core/browser/password_generation_frame_helper.cc
+++ components/password_manager/core/browser/password_generation_frame_helper.cc
@@ -27,10 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/common/password_manager_features.h"
 #include "url/gurl.h"
 
-#if BUILDFLAG(IS_ANDROID)
-#include "components/password_manager/core/browser/split_stores_and_local_upm.h"
-#endif  // BUILDFLAG(IS_ANDROID)
-
 using autofill::AutofillType;
 using autofill::CalculateFieldSignatureForField;
 using autofill::CalculateFormSignature;
@@ -136,7 +132,7 @@ bool PasswordGenerationFrameHelper::IsGenerationEnabled(
   }
 
 #if BUILDFLAG(IS_ANDROID)
-  if (IsGmsCoreUpdateRequired()) {
+  if (client_->GetPasswordFeatureManager()->ShouldUpdateGmsCore()) {
     if (logger) {
       logger->LogMessage(
           Logger::STRING_GENERATION_DISABLED_CHROME_DOES_NOT_SYNC_PASSWORDS);
