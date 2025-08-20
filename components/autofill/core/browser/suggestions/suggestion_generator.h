a51c771e978e111d82d9d2aa2e13a9007fc9f75e
Karol Sygiet <sygiet@google.com>
2025-08-20 01:37:13 -0700

    [RSG] Add IdentityCredential for federated identity suggestions
    This CL introduces autofill::IdentityCredential, a new struct to
    represent federated identity credentials for Autofill suggestions.
    
    This is a simplified representation of content::IdentityRequestAccount
    that lives in the //content/public/ layer and thus is not obtainable
    from //components/autofill/core/browser where the new suggestion
    generation logic will be.
    
    Additionally the interfaces of the relevant federated identity
    suggestion generating functions were modified to use this new struct in
    order to make the future CL containing the implementation of the new
    IdentityCredentialSuggestionGenerator easier.
    
    Bug: 409962888
    Change-Id: Ic537b2bb77704bd61ca79f07bf7cbb2cc8aa552c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6845968
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1503804}

--- components/autofill/core/browser/suggestions/suggestion_generator.h
+++ components/autofill/core/browser/suggestions/suggestion_generator.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/autofill/core/browser/data_model/addresses/autofill_profile.h"
 #include "components/autofill/core/browser/data_model/autofill_ai/entity_instance.h"
+#include "components/autofill/core/browser/data_model/identity_credential/identity_credential.h"
 #include "components/autofill/core/browser/data_model/payments/autofill_offer_data.h"
 #include "components/autofill/core/browser/data_model/payments/credit_card.h"
 #include "components/autofill/core/browser/data_model/payments/iban.h"
@@ -57,7 +58,8 @@ class SuggestionGenerator {
                                       Iban,
                                       AutofillOfferData,
                                       AutocompleteEntry,
-                                      LoyaltyCard>;
+                                      LoyaltyCard,
+                                      IdentityCredential>;
 
   // Obtains data that will be used to generate suggestions on a given trigger
   // `field` that belongs to `form` by calling `GenerateSuggestions` later (See
