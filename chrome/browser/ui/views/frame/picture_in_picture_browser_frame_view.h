6bf2880032719fca0b2bc400809a0a218ede4947
Benjamin Keen <bkeen@google.com>
2025-08-20 11:02:54 -0700

    Fix picture-in-picture window flickering due to resize by child dialog
    This change fixes a UI bug, where the picture-in-picture window would
    sometimes flicker, when a child dialog was opened.
    
    The flicker is caused due to a conflict between the child dialog
    animation and the picture-in-picture window resize. During the PiP
    window resize the child dialog could briefly be shown outside the PiP
    window.
    
    To fix this, whenever the PiP window may be resized for a child dialog,
    we remember the child dialog, disable the child dialog widget visibility
    change animations, wait for the PiP window to resize and, once resized
    we proceed to animate the child dialog.
    
    Additionally we must set the child dialog opacity to 0, in order to hide
    it. To prevent abuse, while the child dialog is hidden we also disable
    input event processing. The opacity and input event processing are
    restored after the PiP window is done resizing itself.
    
    This change also fixes a MacOS bug, where the picture-in-picture window
    would always resize on drag, whenever the child dialog bounds changed.
    This was due to calls to OnWidgetBoundsChanged after a
    picture-in-picture window resize taking place.
    
    To fix this issue, we now keep track of child dialog sizes. If the new
    child dialog size does not change and the picture-in-picture window
    contains the child dialog, a resize will not be performed.
    
    Bug: 427684054
    Change-Id: Ia1a8ee87ad683c5eb5e21d15dbd76e4f92609618
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6821694
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Benjamin Keen <bkeen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1504080}

--- chrome/browser/ui/views/frame/picture_in_picture_browser_frame_view.h
+++ chrome/browser/ui/views/frame/picture_in_picture_browser_frame_view.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <optional>
 
+#include "base/containers/flat_map.h"
 #include "base/containers/flat_set.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
@@ -293,6 +294,16 @@ class PictureInPictureBrowserFrameView
       kSizedToChildren,
     };
 
+    // Animates the picture-in-picture child dialogs that are waiting to be
+    // resized.
+    //
+    // When child dialogs that require a resize of the parent window are
+    // opened, we first make them transparent and non-interactive to avoid
+    // visual glitches while the parent resizes. This function is called after
+    // the parent resize is complete to fade in the child dialogs and make them
+    // interactive again.
+    void AnimateDialogsWaitingForResize();
+
     void PostResizeForChild(const gfx::Rect& new_bounds);
     void FinishPendingResizeForChild();
 
@@ -318,6 +329,19 @@ class PictureInPictureBrowserFrameView
     // The bounds that we forced the window to be in response to a child dialog
     // opening.
     gfx::Rect latest_child_dialog_forced_bounds_;
+
+    // The child dialogs that are waiting for the picture-in-picture window to
+    // resize.
+    //
+    // Used to disable visibility changed animations while child
+    // dialogs are waiting for the picture-in-picture window to resize. After
+    // the picture-in-picture window resizes, or if the user resizes the
+    // picture-in-picture window before the resize takes place, this set
+    // is used to enable visibility changed animations.
+    base::flat_set<raw_ptr<views::Widget>> child_dialogs_waiting_for_resize_;
+
+    // The sizes of the child dialogs. Used to avoid unnecessary resizes.
+    base::flat_map<raw_ptr<views::Widget>, gfx::Size> child_dialog_sizes_;
 
     // The bounds that the window would ideally be if we did not have to enlarge
     // to fit a child dialog.
