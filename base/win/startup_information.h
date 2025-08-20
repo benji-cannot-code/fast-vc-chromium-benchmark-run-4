7449f06e54ba8b5806cee579174f784736117754
Michael Tatarski <mtatarski@google.com>
2025-08-20 10:23:46 -0700

    [CodeHealth] Use `base::HeapArray` in `StartupInformation`.
    Replaces `std::unique_ptr<char[]>` with `base::HeapArray<char>` for
    managing the attribute list buffer. `base::HeapArray` is the preferred
    container for dynamically sized arrays in `base`.
    
    Bug: 40285824
    Change-Id: I4076e94cb757d1602e1a9b5d0ba2f1f600bf166d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6862664
    Auto-Submit: Michael Tatarski <mtatarski@google.com>
    Commit-Queue: Michael Tatarski <mtatarski@google.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1504053}

--- base/win/startup_information.h
+++ base/win/startup_information.h
@@ -10,9 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
-#include <memory>
-
 #include "base/base_export.h"
+#include "base/containers/heap_array.h"
 
 namespace base {
 namespace win {
@@ -45,7 +44,7 @@ class BASE_EXPORT StartupInformation {
   }
 
  private:
-  std::unique_ptr<char[]> attribute_list_;
+  base::HeapArray<char> attribute_list_;
   STARTUPINFOEXW startup_info_;
 };
 
