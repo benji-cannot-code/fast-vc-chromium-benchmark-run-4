c281401c9ea0c66311d9a6deee6525ec3bdcd462
Salma Elmahallawy <esalma@google.com>
2025-08-20 10:53:12 -0700

    Revert "Reland "Add custom layout design to Infobar for showing link text before button""
    This reverts commit 568832f468c0dcd0d013817181700421407b4ec0.
    
    Reason for revert: the CL caused tree closure. https://ci.chromium.org/ui/p/chromium/builders/ci/Win%20x64%20Builder/167344/overview
    
    Bug: 431825969
    Original change's description:
    > Reland "Add custom layout design to Infobar for showing link text before button"
    >
    > This reverts commit 1fcf77a0fa4f93073ea8abb9fdb729e7680869da.
    >
    > Reason for revert: <Removed test that was failing because it was flaky
    > in the way it was testing bounds for one element greater then the other.
    > The main test currently in the cl make sure all the elements exist which
    > is the main test.Currently infobars do not get tested on the position of
    > each element.>
    >
    > Bug: 431825969 Original change's description:
    > > Revert "Add custom layout design to Infobar for showing link text before button"
    > >
    > > This reverts commit 8cb0d10ae737274aaede914776b9fcc82f852f59.
    > >
    > > Reason for revert:
    > > ConfirmInfobarCustomLayoutBrowserTest.ShowsCustomLayoutWhenEnabled
    > > added by the original CL has been failing on win-asan:
    > > https://ci.chromium.org/ui/p/chromium/builders/ci/win-asan/36666/overview
    > >
    > > Bug: 431825969
    > > Original change's description:
    > > > Add custom layout design to Infobar for showing link text before button
    > > >
    > > > Added a custom layout design for the infobar to show the link text
    > > > before the button. Added browser test for it as well. Screenshot:
    > > > https://screenshot.googleplex.com/9Lhfpcx69KitYgS
    > > >
    > > > Bug: 431825969
    > > > Change-Id: Ie30bd68f739ac421e4f8e59424c9ff40a2d9c6f5
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6851578
    > > > Commit-Queue: Muhammad Salmaan <musalmaan@chromium.org>
    > > > Reviewed-by: Kaan Alsan <alsan@chromium.org>
    > > > Cr-Commit-Position: refs/heads/main@{#1503683}
    > >
    > > Bug: 431825969
    > > No-Presubmit: true
    > > No-Tree-Checks: true
    > > No-Try: true
    > > Change-Id: I6add2b33c56c1e1d65f16f67e63bacbedba2590f
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6864441
    > > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > > Auto-Submit: Hiroki Nakagawa <nhiroki@chromium.org>
    > > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > > Owners-Override: Hiroki Nakagawa <nhiroki@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1503765}
    >
    > Bug: 431825969
    > Change-Id: Ib57422f96fd4a9fa962cfac99ed054d396690b77
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6866890
    > Reviewed-by: Ted Choc <tedchoc@chromium.org>
    > Commit-Queue: Muhammad Salmaan <musalmaan@chromium.org>
    > Reviewed-by: Dana Fried <dfried@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1504039}
    
    Bug: 431825969 Original change's description:
    Bug: 431825969
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ibe1843136117ff2992af6c9633a0de6774c71698
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6867411
    Commit-Queue: Salma Elmahallawy <esalma@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Salma Elmahallawy <esalma@google.com>
    Cr-Commit-Position: refs/heads/main@{#1504066}

--- components/infobars/core/confirm_infobar_delegate.cc
+++ components/infobars/core/confirm_infobar_delegate.cc
@@ -26,8 +26,7 @@ void ConfirmInfoBarDelegate::InfoBarDismissed() {
   }
 }
 
-const ConfirmInfoBarDelegate* ConfirmInfoBarDelegate::AsConfirmInfoBarDelegate()
-    const {
+ConfirmInfoBarDelegate* ConfirmInfoBarDelegate::AsConfirmInfoBarDelegate() {
   return this;
 }
 
@@ -62,10 +61,6 @@ bool ConfirmInfoBarDelegate::GetButtonEnabled(InfoBarButton button) const {
 std::u16string ConfirmInfoBarDelegate::GetButtonTooltip(
     InfoBarButton button) const {
   return std::u16string();
-}
-
-bool ConfirmInfoBarDelegate::ShouldShowLinkBeforeButton() const {
-  return false;
 }
 
 #if BUILDFLAG(IS_IOS)
