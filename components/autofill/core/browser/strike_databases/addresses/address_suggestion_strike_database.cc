f59dcd3e46562d899a59105dd2c59d5c58bc1804
Jan Keitel <jkeitel@google.com>
2025-08-20 10:26:25 -0700

    Move address-related strike dbs to their own subdirectory
    No functionality change.
    
    Bypass-Check-License: Files were only moved.
    Bug: 40100455
    Change-Id: I49c393cc990d6b5103a2fa98cf87a2d59c22e7c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6862625
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1504055}

--- components/autofill/core/browser/strike_databases/addresses/address_suggestion_strike_database.cc
+++ components/autofill/core/browser/strike_databases/addresses/address_suggestion_strike_database.cc
@@ -3,13 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/autofill/core/browser/strike_databases/address_suggestion_strike_database.h"
+#include "components/autofill/core/browser/strike_databases/addresses/address_suggestion_strike_database.h"
 
 #include <string>
 
 #include "base/strings/string_number_conversions.h"
 #include "components/autofill/core/browser/proto/strike_data.pb.h"
-#include "components/autofill/core/browser/strike_databases/autofill_profile_save_strike_database.h"
+#include "components/autofill/core/browser/strike_databases/addresses/autofill_profile_save_strike_database.h"
 #include "components/autofill/core/common/signatures.h"
 
 namespace autofill {
