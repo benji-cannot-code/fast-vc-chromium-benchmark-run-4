0c669b391a8705075888f1e1238c87b174c60790
Kaan Alsan <alsan@chromium.org>
2025-08-20 06:11:41 -0700

    [SxS] Disallow opening 'javascript:' urls for SxS drag entrypoints
    URLs with the 'javascipt:' prefix will be opened to the
    'about://blank#blocked' URL, similar to how they're opened when opening
    via context menu.
    
    Change-Id: Idf14c41b2e031d8e508b22b4a5a645184381874c
    Bug: 438364204
    Fixed: 438364204
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6857508
    Reviewed-by: Alison Gale <agale@chromium.org>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1503888}

--- chrome/browser/ui/views/frame/multi_contents_view_delegate.cc
+++ chrome/browser/ui/views/frame/multi_contents_view_delegate.cc
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/multi_contents_drop_target_view.h"
 #include "components/tabs/public/split_tab_data.h"
 #include "components/tabs/public/split_tab_visual_data.h"
+#include "content/public/common/url_constants.h"
+#include "url/url_constants.h"
 
 MultiContentsViewDelegateImpl::MultiContentsViewDelegateImpl(Browser& browser)
     : browser_(browser), tab_strip_model_(*browser.tab_strip_model()) {}
@@ -68,6 +70,16 @@ void MultiContentsViewDelegateImpl::HandleLinkDrop(
   CHECK(!urls.empty());
   CHECK(!tab_strip_model_->GetActiveTab()->IsSplit());
 
+  // Disallow javascript: URLs to prevent self-XSS.
+  std::vector<GURL> filtered_urls;
+  for (const GURL& url : urls) {
+    if (url.SchemeIs(url::kJavaScriptScheme)) {
+      filtered_urls.emplace_back(content::kBlockedURL);
+    } else {
+      filtered_urls.push_back(url);
+    }
+  }
+
   // Insert the tab before or after the active tab, according to the drop side.
   const int new_tab_idx =
       tab_strip_model_->active_index() +
@@ -79,7 +91,7 @@ void MultiContentsViewDelegateImpl::HandleLinkDrop(
 
   // We currently only support creating a split with one link; i.e., the first
   // link in the provided list.
-  tab_strip_model_->delegate()->AddTabAt(urls.front(), new_tab_idx,
+  tab_strip_model_->delegate()->AddTabAt(filtered_urls.front(), new_tab_idx,
                                          /*foreground=*/true);
   // Create a split with the previously active tab, which should be before or
   // after the newly created tab.
