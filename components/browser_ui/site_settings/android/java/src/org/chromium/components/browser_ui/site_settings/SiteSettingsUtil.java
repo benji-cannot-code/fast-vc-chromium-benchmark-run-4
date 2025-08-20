4cf939a72c92cc36014bbaa9678637c0522b749f
Joel Tan-Aristy <jtanaristy@google.com>
2025-08-20 15:04:48 -0700

    Revert "[geo] Improve approximate geolocation support for SingleWebsiteSettings"
    This reverts commit 5ea2337f81591971b95074613d30b9ed3e497978.
    
    Reason for revert: Tests added here are failing automotive builders.
    https://ci.chromium.org/ui/p/chrome/builders/ci/android-automotive-12l-x64-rel-tests/12033/overview
    
    Bug: 418941286, 418936296
    Original change's description:
    > [geo] Improve approximate geolocation support for SingleWebsiteSettings
    >
    > Display approximate geolocation permissions in SingleWebsiteSettings.
    > Allow to toggle persistent permissions and reset onetime permissions.
    >
    > Bug: 418941286, 418936296
    > Change-Id: I02060894a258ac08870249785bc2dedf7266fe35
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6862654
    > Reviewed-by: Florian Jacky <fjacky@chromium.org>
    > Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1503949}
    
    Bug: 418941286, 418936296
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: If16b2da92f5809294aec42aec0f57386f5b04ba0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6868799
    Owners-Override: Joel Tan-Aristy <jtanaristy@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Joel Tan-Aristy <jtanaristy@google.com>
    Auto-Submit: Joel Tan-Aristy <jtanaristy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1504233}

--- components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/SiteSettingsUtil.java
+++ components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/SiteSettingsUtil.java
@@ -28,7 +28,6 @@ public class SiteSettingsUtil {
     public static final int[] SETTINGS_ORDER = {
         ContentSettingsType.COOKIES,
         ContentSettingsType.GEOLOCATION,
-        ContentSettingsType.GEOLOCATION_WITH_OPTIONS,
         ContentSettingsType.MEDIASTREAM_CAMERA,
         ContentSettingsType.MEDIASTREAM_MIC,
         ContentSettingsType.NOTIFICATIONS,
