6e0173a4f41eb70d24132441979ce882fe1bb9d6
Frank Liberato <liberato@chromium.org>
2025-08-20 14:47:14 -0700

    Install SODA for any captioning client.
    Rather than wait until Live Caption starts to check for SODA
    installation, do so when any captioning client registers with
    CaptionControllerBase.  This allows headless captioning to
    install SODA too.
    
    The side-effects of this CL are mostly that the UI bubble
    for Live Caption is created before soda installation rather
    than after.  This is because the captioning UI is also the
    client, so it must register itself to start SODA installation.
    Previously, the UI was created when LiveCaptionController was
    notified that SODA installation was complete.
    
    Change-Id: Id8a14b71d415956af67028525f31c91596be0d2b
    Bug: 439852131
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6851614
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Yiren Wang <yrw@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Reviewed-by: Fr <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1504222}

--- chrome/browser/glic/media/glic_media_integration_unittest.cc
+++ chrome/browser/glic/media/glic_media_integration_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/live_caption/live_caption_controller.h"
 #include "components/live_caption/pref_names.h"
 #include "components/optimization_guide/content/browser/media_transcript_provider.h"
+#include "components/soda/mock_soda_installer.h"
 #include "components/sync_preferences/testing_pref_service_syncable.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test_utils.h"
@@ -29,6 +30,14 @@ class GlicMediaIntegrationTest : public ChromeRenderViewHostTestHarness {
  public:
   void SetUp() override {
     ChromeRenderViewHostTestHarness::SetUp();
+
+    // Set up soda Installer
+    soda_installer_.NeverDownloadSodaForTesting();
+    ON_CALL(soda_installer_, Init).WillByDefault(testing::Return());
+    captions::LiveCaptionController::RegisterProfilePrefs(
+        static_cast<user_prefs::PrefRegistrySyncable*>(
+            testing_pref_service_.registry()));
+
     // Return a mock Live Caption controller.
     auto controller = CreateLiveCaptionController();
     live_caption_controller_ = controller.get();
@@ -113,6 +122,8 @@ class GlicMediaIntegrationTest : public ChromeRenderViewHostTestHarness {
   std::optional<base::test::ScopedFeatureList> scoped_feature_list_;
   raw_ptr<captions::LiveCaptionController> live_caption_controller_ = nullptr;
   raw_ptr<user_prefs::PrefRegistrySyncable> pref_registry_ = nullptr;
+  TestingPrefServiceSimple testing_pref_service_;
+  speech::MockSodaInstaller soda_installer_;
 };
 
 TEST_F(GlicMediaIntegrationTest, GetWithNullReturnsNull) {
