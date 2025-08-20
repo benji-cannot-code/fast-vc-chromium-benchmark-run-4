9023eb6616110b29b7927b0c85624fcb8d3f0951
Justin Cohen <justincohen@google.com>
2025-08-20 14:11:58 -0700

    ios: Disable failing EG tests for Xcode 26 roll
    This CL disables three EarlGrey tests that are failing after the Xcode
    26 roll. The failures appear related to swipe gestures.
     - testDeleteHistoricalSuggestion
     - testSwipingAnotherAffiliatedGroupWhenAnotherIsIn
     - testVisibleButtonsAfterSwipeDeletion
    
    Disabling them to unblock the main branch while the root cause is
    investigated.
    
    Bug: 440119404
    Change-Id: Ifcf383affceff6556f9d97709433ca4857ed4e38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6867451
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Owners-Override: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1504197}

--- ios/chrome/browser/settings/ui_bundled/password/password_manager_egtest.mm
+++ ios/chrome/browser/settings/ui_bundled/password/password_manager_egtest.mm
@@ -4092,7 +4092,9 @@ void OpenPasswordManagerWidgetPromoInstructions() {
       assertWithMatcher:grey_sufficientlyVisible()];
 }
 
-- (void)testSwipingAnotherAffiliatedGroupWhenAnotherIsInEditMode {
+// TODO(crbug.com/440119404): Re-enable after roll is complete and testers are
+// also updated.
+- (void)DISABLED_testSwipingAnotherAffiliatedGroupWhenAnotherIsInEditMode {
   // Form an affiliated group with two passwords.
   SavePasswordFormToProfileStore(/*password=*/@"password1",
                                  /*username=*/@"user1",
