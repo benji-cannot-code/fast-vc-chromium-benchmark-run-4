b090cc6c3c9257ee22169bcfa2982d87499d26cf
Rafal Godlewski <rgod@google.com>
2025-08-20 06:29:41 -0700

    Enable kCredentialProviderAutomaticPasskeyUpgrade by default
    The feature was approved for full launch.
    
    Bug: b:355666571
    Change-Id: Iab35693403a79bdd50932d58633a43d5423cebfe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6861225
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1503898}

--- ios/chrome/browser/credential_provider/model/features.mm
+++ ios/chrome/browser/credential_provider/model/features.mm
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/credential_provider/model/features.h"
 
 BASE_FEATURE(CredentialProviderAutomaticPasskeyUpgrade,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(CredentialProviderPasskeyPRF, base::FEATURE_ENABLED_BY_DEFAULT);
 
