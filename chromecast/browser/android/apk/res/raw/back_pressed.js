33a38a911a6a4292e6fafcb118b3cae3c84e7ea3
Simeon Anfinrud <sanfin@chromium.org>
2025-08-20 16:57:25 -0700

    [chromecast] Load back-pressed JavaScript as resource.
    This avoids hard-coding the back-pressed JS code as a string constant in
    CastWebContentsActivity, slightly reducing runtime runnime memory
    footprint when not handling back button events and sidestepping
    formatting issues where the default formatter preferred by chromium
    mangles the inline JS.
    
    Bug: 436916267
    Test: cast_shell_junit_tests
    Test: use back button in mediashell YouTube web app
    
    Change-Id: I9edd96777786aa6c97376e5007f44fcae633fbed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6825109
    Reviewed-by: Sandeep Vijayasekar <sandv@google.com>
    Commit-Queue: Simeon Anfinrud <sanfin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1504277}

---
+++ chromecast/browser/android/apk/res/raw/back_pressed.js
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+{
+  let getActiveElement = function() {
+    let activeElement = document.activeElement;
+    while (activeElement && activeElement.shadowRoot &&
+           activeElement.shadowRoot.activeElement) {
+      activeElement = activeElement.shadowRoot.activeElement;
+    }
+    return activeElement;
+  };
+  let backPressEvent = new KeyboardEvent(
+      'keydown',
+      {bubbles: true, key: 'BrowserBack', cancelable: true, composed: true});
+  let activeElement = getActiveElement();
+  if (activeElement) {
+    activeElement.dispatchEvent(backPressEvent);
+  } else {
+    document.dispatchEvent(backPressEvent);
+  }
+  backPressEvent.defaultPrevented;
+};
