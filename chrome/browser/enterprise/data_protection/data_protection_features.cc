e4e5bd08f480801b01e3aaacf3436d380af71665
Nasser Al-shawwa <alshawwa@chromium.org>
2025-08-20 12:14:12 -0700

    enable EnableSinglePageAppDataProtection flag by default
    Change-Id: I07a02757e09d34370bb26ba9429c0633f3ac7e12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6868309
    Commit-Queue: Nasser Al-shawwa <alshawwa@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1504127}

--- chrome/browser/enterprise/data_protection/data_protection_features.cc
+++ chrome/browser/enterprise/data_protection/data_protection_features.cc
@@ -9,6 +9,6 @@ namespace enterprise_data_protection {
 
 BASE_FEATURE(kEnableSinglePageAppDataProtection,
              "EnableSinglePageAppDataProtection",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace enterprise_data_protection
