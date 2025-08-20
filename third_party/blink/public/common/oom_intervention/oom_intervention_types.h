1b05fd5ab9cad095fd5176cfa3a24f59b3509463
Thiabaud Engelbrecht <thiabaud@google.com>
2025-08-20 11:10:55 -0700

    [blink] Migrate OomInterventionTypes to ByteCount
    Bug: 429140103
    Change-Id: I2edd11588fb568f4097946eb73ab1322adce1b7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6841821
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Thiabaud Engelbrecht <thiabaud@google.com>
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Cr-Commit-Position: refs/heads/main@{#1504093}

--- third_party/blink/public/common/oom_intervention/oom_intervention_types.h
+++ third_party/blink/public/common/oom_intervention/oom_intervention_types.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include "base/byte_count.h"
+
 namespace blink {
 
 // The struct with renderer metrics that are used to detect OOMs. This is stored
@@ -17,8 +19,8 @@ namespace blink {
 // the arm64 and renderer in the arm32.
 
 struct OomInterventionMetrics {
-  uint64_t current_available_memory_kb = 0;
-  uint64_t current_swap_free_kb = 0;
+  base::ByteCount current_available_memory;
+  base::ByteCount current_swap_free;
 
   // Indicates whether the crash was because of virtual address space OOM.
   // This holds only 0 or 1 as a value but because of the reason stated above,
