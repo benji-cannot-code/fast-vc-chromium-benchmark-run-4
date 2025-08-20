b9757b71a044c55d094aa03d816a6e467c446013
Kurt Catti-Schmidt <kschmi@microsoft.com>
2025-08-19 18:09:42 -0700

    Avoid Member<>'s on stack in core/dom
    Per crbug.com/40209908 Member<>'s on the stack have extra overhead and
    should be avoided. This change converts them to Member<>&'s, which do
    not incur this overhead.
    
    In GetAttrAssociatedElements, a Member<> is copied into another
    HeapVector. For this case, the stack variable is stored as a raw pointer
    which is the preferred method to handle this situation per
    third_party/blink/renderer/platform/heap/BlinkGCAPIReference.md (see the
    section titled "Raw Pointers".
    
    This change also eliminates some unnecessary string copies
    (highlight_names and tokens).
    
    No behavioral changes are expected (other than slightly less Oilpan
    overhead).
    
    Bug: 439630585
    Change-Id: I021a924d55c513f602d9f135ca4768b1c5680867
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6858269
    Commit-Queue: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1503704}

--- third_party/blink/renderer/core/dom/slot_assignment.cc
+++ third_party/blink/renderer/core/dom/slot_assignment.cc
@@ -302,7 +302,7 @@ void SlotAssignment::RecalcAssignment() {
         }
       }
 
-      for (auto child : children_to_clear) {
+      for (auto& child : children_to_clear) {
         child->ClearFlatTreeNodeData();
         child->RemovedFromFlatTree();
       }
