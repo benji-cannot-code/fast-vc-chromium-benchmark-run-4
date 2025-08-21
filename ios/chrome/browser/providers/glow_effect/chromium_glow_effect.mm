b9b0463b3c987ba6b606e10aef23ea27308aac93
Christian Xu <christianxu@google.com>
2025-08-21 05:19:56 -0700

    [iOS][AIM] Add glow effect API
    Add the glow effect API and the public implementation.
    
    Bug: 440076059
    Change-Id: I04971f8fe0e65ba184a4578cd68852a2b8ded9e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6870551
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1504494}

---
+++ ios/chrome/browser/providers/glow_effect/chromium_glow_effect.mm
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/public/provider/chrome/browser/glow_effect/glow_effect_api.h"
+
+namespace ios {
+namespace provider {
+
+UIView<GlowEffect>* CreateGlowEffect(CGRect frame,
+                                     CGFloat cornerRadius,
+                                     CGFloat borderWidth) {
+  return nil;
+}
+
+}  // namespace provider
+}  // namespace ios
