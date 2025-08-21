09c0ca75ef4feb41054271532f20ee2bcca73ccc
Lei Zhang <thestig@chromium.org>
2025-08-21 12:37:05 -0700

    [PDF] Add/Move code to test blank bitmaps to test_helpers.h
    Move the existing IsBitmapBlank() function from
    pdfium_draw_selection_test_base.cc to test_helpers.h, so it can be
    reused in upcoming tests. Also add IsImageBlank() to check SkImage.
    
    Change-Id: I4b1f74e80024ece55f09559fc652096612fc67e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6867809
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1504679}

--- pdf/pdfium/pdfium_draw_selection_test_base.cc
+++ pdf/pdfium/pdfium_draw_selection_test_base.cc
@@ -23,21 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chrome_pdf {
 
-namespace {
-
-bool IsBitmapBlank(const SkBitmap& bitmap) {
-  for (int i = 0; i < bitmap.width(); ++i) {
-    for (int j = 0; j < bitmap.height(); ++j) {
-      if (bitmap.getColor(i, j) != SK_ColorWHITE) {
-        return false;
-      }
-    }
-  }
-  return true;
-}
-
-}  // namespace
-
 void PDFiumDrawSelectionTestBase::DrawSelectionAndCompare(
     PDFiumEngine& engine,
     int page_index,
