5561cd292d64bf56d3c67293594621aac476cc00
Tanmoy Mollik <triploblastic@google.com>
2025-08-21 05:21:39 -0700

    Use OAuthConsumerId for PrimaryAccountAccessTokenFetcher clients - 8
    This cl is part of a series that converts
    PrimaryAccountAccessTokenFetcher clients to use OAuthConsumerId based
    constructor instead.
    
    Bug: 425896213
    Change-Id: I91f03485ec2f12af7eb9dbe6e6fc248ba2a32b36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6843196
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Menghan Yang <myuu@google.com>
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1504495}

--- components/history/core/browser/web_history_service.cc
+++ components/history/core/browser/web_history_service.cc
@@ -43,8 +43,6 @@ namespace history {
 
 namespace {
 
-const char kHistoryOAuthScope[] = "https://www.googleapis.com/auth/chromesync";
-
 const char kHistoryQueryHistoryUrl[] =
     "https://history.google.com/history/api/lookup?client=chrome";
 
@@ -161,9 +159,6 @@ class RequestImpl : public WebHistoryService::Request {
 
   // Tells the request to do its thang.
   void Start() override {
-    signin::ScopeSet oauth_scopes;
-    oauth_scopes.insert(kHistoryOAuthScope);
-
     // TODO(crbug.com/40066882): Simplify this once
     // kReplaceSyncPromosWithSignInPromos has rolled out on all platforms.
     signin::ConsentLevel consent_level = signin::ConsentLevel::kSync;
@@ -174,7 +169,7 @@ class RequestImpl : public WebHistoryService::Request {
 
     access_token_fetcher_ =
         std::make_unique<signin::PrimaryAccountAccessTokenFetcher>(
-            "web_history", identity_manager_, oauth_scopes,
+            signin::OAuthConsumerId::kWebHistoryService, identity_manager_,
             base::BindOnce(&RequestImpl::OnAccessTokenFetchComplete,
                            base::Unretained(this)),
             signin::PrimaryAccountAccessTokenFetcher::Mode::kImmediate,
@@ -194,11 +189,9 @@ class RequestImpl : public WebHistoryService::Request {
     // If the response code indicates that the token might not be valid,
     // invalidate the token and try again.
     if (response_code_ == net::HTTP_UNAUTHORIZED && ++auth_retry_count_ <= 1) {
-      signin::ScopeSet oauth_scopes;
-      oauth_scopes.insert(kHistoryOAuthScope);
       identity_manager_->RemoveAccessTokenFromCache(
           identity_manager_->GetPrimaryAccountId(signin::ConsentLevel::kSignin),
-          oauth_scopes, access_token_);
+          signin::OAuthConsumerId::kWebHistoryService, access_token_);
 
       access_token_.clear();
       Start();
