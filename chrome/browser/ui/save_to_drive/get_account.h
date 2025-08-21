ca0db64a2e10077d5f2ff5e7f3b8f715d7643166
Tiffany Shen <tishen@google.com>
2025-08-21 10:34:25 -0700

    [Save PDF to Drive] Add skeleton of the function to launch the account chooser UI.
    Bug: 440292537
    
    Change-Id: I56197f0749ee7d05f4884ae97314641005ec707f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6871513
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Tiffany Shen <tishen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1504615}

---
+++ chrome/browser/ui/save_to_drive/get_account.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_SAVE_TO_DRIVE_GET_ACCOUNT_H_
+#define CHROME_BROWSER_UI_SAVE_TO_DRIVE_GET_ACCOUNT_H_
+
+#include <optional>
+
+#include "base/functional/callback_forward.h"
+#include "components/signin/public/identity_manager/account_info.h"
+
+namespace content {
+class WebContents;
+}  // namespace content
+
+namespace save_to_drive {
+
+void LaunchAccountChooserAndGetAccount(
+    content::WebContents* web_contents,
+    base::OnceCallback<void(std::optional<AccountInfo>)>
+        on_account_selected_callback);
+
+}  // namespace save_to_drive
+
+#endif  // CHROME_BROWSER_UI_SAVE_TO_DRIVE_GET_ACCOUNT_H_
---
+++ chrome/browser/ui/views/save_to_drive/get_account.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/save_to_drive/get_account.h"
+
+#include "components/signin/public/identity_manager/account_info.h"
+#include "content/public/browser/web_contents.h"
+
+namespace save_to_drive {
+
+void LaunchAccountChooserAndGetAccount(
+    content::WebContents* web_contents,
+    base::OnceCallback<void(std::optional<AccountInfo>)>
+        on_account_selected_callback) {
+  // TODO(crbug.com/440292537): Implement.
+}
+
+}  // namespace save_to_drive
