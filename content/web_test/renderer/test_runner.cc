14b9e06e01eb6cb983502c28bbc09f04488ddc0f
Rakina Zata Amni <rakina@chromium.org>
2025-08-20 19:22:31 -0700

    Update TestRunner code to expect RFH change on same-site navigations
    Bug: 40615943
    Change-Id: Ic069f4c14012b0ef441db7e3e8d21bec9a6a54b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6726516
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Mingyu Lei <leimy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1504347}

--- content/web_test/renderer/test_runner.cc
+++ content/web_test/renderer/test_runner.cc
@@ -3355,11 +3355,6 @@ void TestRunner::SetMainWindowAndTestConfiguration(
     }
   }
 
-  // This may be called for a local root in the same process as another local
-  // root, in which case we just keep the original config, which should match.
-  if (test_is_running_)
-    return;
-
   test_config_ = std::move(*config);
   SetTestIsRunning(true);
 
