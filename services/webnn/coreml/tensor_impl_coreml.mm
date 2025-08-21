461e65bafcfab561e77efaf331d14f427826f2a3
Phillis Tang <phillis@chromium.org>
2025-08-21 12:25:28 -0700

    webnn: fix representation lifecycle for coreml
    Both the representation and its scoped access need to be owned by
    TensorImplCoreml.
    
    Bug: 333392274
    Change-Id: I85031c0ca59794926775967ff2ade8283637d214
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6870950
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1504671}

--- services/webnn/coreml/tensor_impl_coreml.mm
+++ services/webnn/coreml/tensor_impl_coreml.mm
@@ -180,8 +180,8 @@ TensorImplCoreml::Create(
           std::move(buffer_content));
   return base::MakeRefCounted<TensorImplCoreml>(
       std::move(receiver), std::move(context), std::move(tensor_info),
-      std::move(buffer_state), /*representation_access=*/nullptr,
-      base::PassKey<TensorImplCoreml>());
+      std::move(buffer_state), /*representation=*/nullptr,
+      /*representation_access=*/nullptr, base::PassKey<TensorImplCoreml>());
 }
 
 // static
@@ -255,8 +255,8 @@ TensorImplCoreml::Create(
           std::move(buffer_content));
   return base::MakeRefCounted<TensorImplCoreml>(
       std::move(receiver), std::move(context), std::move(tensor_info),
-      std::move(buffer_state), std::move(representation_access),
-      base::PassKey<TensorImplCoreml>());
+      std::move(buffer_state), std::move(representation),
+      std::move(representation_access), base::PassKey<TensorImplCoreml>());
 }
 
 TensorImplCoreml::TensorImplCoreml(
@@ -264,12 +264,14 @@ TensorImplCoreml::TensorImplCoreml(
     base::WeakPtr<WebNNContextImpl> context,
     mojom::TensorInfoPtr tensor_info,
     scoped_refptr<QueueableResourceState<BufferContent>> buffer_state,
+    std::unique_ptr<gpu::WebNNTensorRepresentation> representation,
     std::unique_ptr<gpu::WebNNTensorRepresentation::ScopedAccess>
         representation_access,
     base::PassKey<TensorImplCoreml> /*pass_key*/)
     : WebNNTensorImpl(std::move(receiver),
                       std::move(context),
-                      std::move(tensor_info)),
+                      std::move(tensor_info),
+                      std::move(representation)),
       buffer_state_(std::move(buffer_state)) {
   representation_access_ = std::move(representation_access);
 }
