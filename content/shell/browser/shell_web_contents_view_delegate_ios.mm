0bf0cd64fd047a2b1b1af13c8da55b4ffded54ae
Gyuyoung Kim <gyuyoung@igalia.com>
2025-08-21 09:54:42 -0700

    [iOS Blink] Include navigation_controller.h in shell_contents_view_delegate_ios.mm
    Since https://crrev.com/c/6866209 removed unnecessary include files,
    shell_contents_view_delegate_ios.mm now needs to explicitly include
    navigation_controller.h because it calls NavigationController.
    
    Bug: 429365675
    Change-Id: Ic5f33388df7694774e7c59ad3b02d1e2d4774d45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6868971
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Raphael Kubo da Costa <kubo@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1504579}

--- content/shell/browser/shell_web_contents_view_delegate_ios.mm
+++ content/shell/browser/shell_web_contents_view_delegate_ios.mm
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notimplemented.h"
 #include "build/build_config.h"
 #include "content/public/browser/context_menu_params.h"
+#include "content/public/browser/navigation_controller.h"
 #include "content/public/browser/render_widget_host_view.h"
 #include "content/public/browser/web_contents.h"
 #include "content/shell/browser/shell_web_contents_view_delegate_creator.h"
