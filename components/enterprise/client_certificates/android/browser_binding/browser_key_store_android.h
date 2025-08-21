928de64213e1bb98a67d23dff8fa3b75643c8827
Elias Homsi <eliashomsi@google.com>
2025-08-21 10:13:53 -0700

    Introduce BrowserKey and BrowserKeyStore JNI interfaces for Key Android browser binding.
    This change adds the `BrowserKey` and `BrowserKeyStore` C++ interfaces,
    along with their Android-specific implementations (`BrowserKeyAndroid`
    and `BrowserKeyStoreAndroid`). These classes wrap Java objects to
    interact with the Android Keystore for creating, retrieving, and using
    keys for browser binding. Unit tests are included to verify the
    implementation.
    
    Bug: 432304139
    Change-Id: I4f9e67aa51296fb78dd651eb613b2eb2a9308208
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6867773
    Commit-Queue: Elias Homsi <eliashomsi@google.com>
    Reviewed-by: Hamda Mare <hmare@google.com>
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1504606}

---
+++ components/enterprise/client_certificates/android/browser_binding/browser_key_store_android.h
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_ENTERPRISE_CLIENT_CERTIFICATES_ANDROID_BROWSER_BINDING_BROWSER_KEY_STORE_ANDROID_H_
+#define COMPONENTS_ENTERPRISE_CLIENT_CERTIFICATES_ANDROID_BROWSER_BINDING_BROWSER_KEY_STORE_ANDROID_H_
+
+#include <vector>
+
+#include "components/enterprise/client_certificates/android/browser_binding/browser_key_store.h"
+#include "third_party/jni_zero/jni_zero.h"
+
+namespace client_certificates {
+
+// Implements BrowserKeyStore for Android.
+class BrowserKeyStoreAndroid : public BrowserKeyStore {
+ public:
+  explicit BrowserKeyStoreAndroid(
+      jni_zero::ScopedJavaLocalRef<jobject> java_object);
+
+  std::unique_ptr<BrowserKey> GetOrCreateBrowserKeyForCredentialId(
+      const std::vector<uint8_t>& credential_id,
+      const CredentialInfoList& allowed_credentials) override;
+
+  bool GetDeviceSupportsHardwareKeys() override;
+
+  void DeleteBrowserKey(const std::vector<uint8_t>& credential_id) override;
+
+ protected:
+  ~BrowserKeyStoreAndroid() override;
+
+ private:
+  // The implementation Java object.
+  jni_zero::ScopedJavaGlobalRef<jobject> impl_;
+};
+
+}  // namespace client_certificates
+
+#endif  // COMPONENTS_ENTERPRISE_CLIENT_CERTIFICATES_ANDROID_BROWSER_BINDING_BROWSER_KEY_STORE_ANDROID_H_
