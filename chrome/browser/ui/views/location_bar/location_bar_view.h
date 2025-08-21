b46b11063512cb47139bba33684a6e6477845561
Hannah Collins <hannahrogers@google.com>
2025-08-21 13:56:34 -0700

    [omnibox] Refresh the AIM icon view on text and focus changes
    This ensures we are properly hiding the AIM button whenever the user has
    typed any input.
    
    And ensures we hide the AIM button when the omnibox loses visibility.
    
    Also, we should verify the user text is non-empty, and not rely on the
    textbox text (which may also include suggestions not yet accepted).
    
    Before:
    https://screencast.googleplex.com/cast/NjY1NzY3MjczNTYyMTEyMHwxZGQ5MjA0Yi1lNQ
    After:
    https://screencast.googleplex.com/cast/NTkzNDgyMzQ5ODU3OTk2OHxhMWNjMTdmNy1jMg
    
    Fixed: 439678655
    Change-Id: Ife95576bc4102474ed0a06eed02e0fa6fada7544
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6863817
    Commit-Queue: Hannah Collins <hannahrogers@google.com>
    Reviewed-by: Khalid Peer <khalidpeer@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1504698}

--- chrome/browser/ui/views/location_bar/location_bar_view.h
+++ chrome/browser/ui/views/location_bar/location_bar_view.h
@@ -321,6 +321,9 @@ class LocationBarView
   // actions are available on the current page.
   void RefreshPageActionIconViews();
 
+  // Updates the visibility state of the AIM page action icon view.
+  void RefreshAiModePageActionIconView();
+
   // Updates PageActionContainerView's action controller to the active tab's
   // controller. At the same time, the page actions visibility will be set based
   // on the omnibox state.
