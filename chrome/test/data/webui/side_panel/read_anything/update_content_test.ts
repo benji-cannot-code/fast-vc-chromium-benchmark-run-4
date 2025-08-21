faaa921dee8ccc6b65a4732676eb8052979e893a
Kristi Saney <kristislee@google.com>
2025-08-21 16:44:27 -0700

    [Reading mode] Update the words seen heuristic
    This overall heuristic is as follows:
    1. Estimates the number of new words visible on the screen.
    2. Estimates the time required for a person to read that number of words
    3. Starts a timer for that duration. If the timer completes without
       interruption, the words are counted as seen.
    4. Dynamically extends the timer if new activity occurs (e.g. scrolling)
       It adjusts the total wait time to account for the new total number of
       words, ensuring the final count is saved only after the total
       estimated reading time has passed.
    The heuristic for counting the number of words on screen being read is
    the same as before. However, this CL now calculates the delay after
    which to log based on the number of words seen. With this, we need to
    ensure we account for the user scrolling a little bit very often to keep
    reading down the page. With the set delay, we'd only log the words
    visible after the delay, rather than including the words that were read
    before and then hidden with a small scroll. This heuristic should do a
    better job of handling that case.
    
    Bug: 372890165
    Change-Id: I2b5e8c938859be3301af22acb8266c67c5aa3f1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6869114
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1504764}

--- chrome/test/data/webui/side_panel/read_anything/update_content_test.ts
+++ chrome/test/data/webui/side_panel/read_anything/update_content_test.ts
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 import 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
 
-import {BrowserProxy, COUNT_WORDS_SEEN_DELAY_MS, SpeechBrowserProxyImpl, ToolbarEvent, VoiceLanguageController} from 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
+import {BrowserProxy, MIN_MS_TO_READ, SpeechBrowserProxyImpl, ToolbarEvent, VoiceLanguageController} from 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
 import type {AppElement} from 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
 import {assertEquals, assertFalse, assertTrue} from 'chrome-untrusted://webui-test/chai_assert.js';
 import {MockTimer} from 'chrome-untrusted://webui-test/mock_timer.js';
@@ -136,7 +136,7 @@ suite('UpdateContent', () => {
     };
 
     app.updateContent();
-    mockTimer.tick(COUNT_WORDS_SEEN_DELAY_MS);
+    mockTimer.tick(MIN_MS_TO_READ);
     mockTimer.uninstall();
 
     assertTrue(sentWordsSeen);
