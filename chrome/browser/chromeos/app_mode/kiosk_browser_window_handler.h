143137d3427e75b71833ac7c9efdfc2d1c881c89
Ashutosh Singhal <macinashutosh@google.com>
2025-08-21 09:55:53 -0700

    Revert "[Fix] Use WeakPtr in NavigationWaiter to prevent crash"
    This reverts commit 490ef25b449c5c0f8ff07bcc73ef88e6d3ae23e2.
    
    Reason for revert: The cl re-introduced the b:406450838.
    
    Bug: b:439563444
    Original change's description:
    > [Fix] Use WeakPtr in NavigationWaiter to prevent crash
    >
    > Replaces `base::Unretained` with a `base::WeakPtr` for the callback
    > passed to NavigationWaiter.
    > This fixes a use-after-free crash that occurred when a
    > KioskBrowserWindowHandler was destroyed before the navigation callback
    > could be run. The weak pointer ensures the callback is safely
    > invalidated.
    >
    > Bug: b:439563444
    > Change-Id: Ia75d8bb3ee5b721be74a9d9874a79b371c770515
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6859123
    > Reviewed-by: Sergii Bykov <sbykov@google.com>
    > Commit-Queue: Ashutosh Singhal <macinashutosh@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1503286}
    
    Bug: b:439563444
    Change-Id: Ib048d07cdbde13665fc322c894c203785a4ec245
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6870537
    Commit-Queue: Ashutosh Singhal <macinashutosh@google.com>
    Reviewed-by: Sergii Bykov <sbykov@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1504581}

--- chrome/browser/chromeos/app_mode/kiosk_browser_window_handler.h
+++ chrome/browser/chromeos/app_mode/kiosk_browser_window_handler.h
@@ -67,7 +67,6 @@ class KioskBrowserWindowHandler : public BrowserListObserver {
 
  private:
   void OnCompleteBrowserAdded(Browser* browser);
-  void OnBrowserNavigationStarted(Browser* browser);
   bool TriageNewBrowserWindow(Browser* browser);
   void HandleNewSettingsWindow(Browser* browser, const std::string& url_string);
 
