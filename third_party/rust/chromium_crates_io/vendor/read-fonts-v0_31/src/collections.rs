598b6e280195c760ccd084bae2c1387fd66b6a83
Dominik Röttsches <drott@chromium.org>
2025-08-21 03:48:36 -0700

    Roll Fontations crates to new versions
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * read-fonts: 0.31.3 => 0.32.0; https://docs.rs/crate/read-fonts/0.32.0
    * skrifa: 0.33.2 => 0.34.0; https://docs.rs/crate/skrifa/0.34.0
    
    Bug: chromium:438792570
    Change-Id: I8765e501cc8be665e9c83a531765b190dddc3154
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6863026
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Julia Hansbrough <flowerhack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1504462}

--- third_party/rust/chromium_crates_io/vendor/read-fonts-v0_31/src/collections.rs
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-//! Data structures useful for font work.
-
-pub mod int_set;
-pub use int_set::IntSet;
-
-mod range_set;
-pub use range_set::RangeSet;
---
+++ third_party/rust/chromium_crates_io/vendor/read-fonts-v0_32/src/collections.rs
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+//! Data structures useful for font work.
+
+pub mod int_set;
+pub use int_set::IntSet;
+pub use int_set::U32Set;
+
+mod range_set;
+pub use range_set::RangeSet;
