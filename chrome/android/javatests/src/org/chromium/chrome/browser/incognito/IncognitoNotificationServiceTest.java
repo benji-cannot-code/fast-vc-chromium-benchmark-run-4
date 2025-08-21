d52d290d3a93505321d4cf1c4728cf880d1e4f92
Joel Tan-Aristy <jtanaristy@google.com>
2025-08-20 17:54:07 -0700

    [Gardener] Disable consistently failing IncognitoNotificationServiceTest
    Bug: 440177822
    Change-Id: Ib8f445ce9dd40612b5059217db0bebbf724887ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6869119
    Reviewed-by: Shu Yang <shuyng@google.com>
    Owners-Override: Joel Tan-Aristy <jtanaristy@google.com>
    Commit-Queue: Joel Tan-Aristy <jtanaristy@google.com>
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Reviewed-by: Gazal Agarwal <agazal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1504309}

--- chrome/android/javatests/src/org/chromium/chrome/browser/incognito/IncognitoNotificationServiceTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/incognito/IncognitoNotificationServiceTest.java
@@ -238,6 +238,7 @@ public class IncognitoNotificationServiceTest {
     @Test
     @MediumTest
     @Feature("Incognito")
+    @DisabledTest(message = "https://crbug.com/440177822")
     public void testCloseAllIncognitoNotificationForIncognitoCct_DoesNotCloseCct()
             throws PendingIntent.CanceledException {
         launchIncognitoTabAndEnsureNotificationDisplayed();
