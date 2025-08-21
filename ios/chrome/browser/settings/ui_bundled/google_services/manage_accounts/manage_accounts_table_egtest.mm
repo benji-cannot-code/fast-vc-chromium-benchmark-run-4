a9aa61ca6b1f3382564923559fcf025609741040
Federica Germinario <fedegermi@google.com>
2025-08-21 10:00:00 -0700

    [ios] Disable flaky tests on ios26
    Bug: 439735103, 440058430, 436557023, 440059889, 440041762
    Change-Id: Ic582def715b39fa87736ba4278921a09f3d63107
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6862624
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1504588}

--- ios/chrome/browser/settings/ui_bundled/google_services/manage_accounts/manage_accounts_table_egtest.mm
+++ ios/chrome/browser/settings/ui_bundled/google_services/manage_accounts/manage_accounts_table_egtest.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
+#import "base/ios/ios_util.h"
 #import "base/test/ios/wait_util.h"
 #import "ios/chrome/browser/authentication/ui_bundled/signin_earl_grey.h"
 #import "ios/chrome/browser/authentication/ui_bundled/signin_earl_grey_ui_test_util.h"
@@ -218,6 +219,11 @@ using chrome_test_util::SettingsSignInRowMatcher;
 // third time to remove a second identity.
 // The goal of this test is to confirm the dialog can be opened several times.
 - (void)testRemoveAccountSeveralTime {
+  // TODO(crbug.com/436557023): Re-enable the test on iOS26.
+  if (base::ios::IsRunningOnIOS26OrLater()) {
+    EARL_GREY_TEST_DISABLED(@"Test disabled on iOS 26.");
+  }
+
   FakeSystemIdentity* fakeIdentity1 = [FakeSystemIdentity fakeIdentity1];
   FakeSystemIdentity* fakeIdentity2 = [FakeSystemIdentity fakeIdentity2];
   FakeSystemIdentity* fakeIdentity3 = [FakeSystemIdentity fakeIdentity3];
