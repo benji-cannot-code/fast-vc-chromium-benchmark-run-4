9b2ff886b2e13dc759882f868f4a0076e89c7758
Christian Dullweber <dullweber@chromium.org>
2025-08-21 05:46:50 -0700

    [geo] Remove sample data flag
    The sample data flag is not needed anymore because real permission
    requests work now.
    
    Change-Id: I9d5268553fe768d220c94a3f92a555143517665f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6870410
    Commit-Queue: Florian Jacky <fjacky@chromium.org>
    Reviewed-by: Florian Jacky <fjacky@chromium.org>
    Auto-Submit: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1504508}

--- components/permissions/android/java/src/org/chromium/components/permissions/PermissionsAndroidFeatureList.java
+++ components/permissions/android/java/src/org/chromium/components/permissions/PermissionsAndroidFeatureList.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.components.permissions;
 
 import org.chromium.build.annotations.NullMarked;
-import org.chromium.components.cached_flags.BooleanCachedFeatureParam;
 import org.chromium.components.cached_flags.CachedFeatureParam;
 import org.chromium.components.cached_flags.IntCachedFeatureParam;
 
@@ -47,10 +46,6 @@ public abstract class PermissionsAndroidFeatureList {
             "ApproximateGeolocationPermission";
 
     public static final String AUTO_PICTURE_IN_PICTURE_ANDROID = "AutoPictureInPictureAndroid";
-
-    public static final BooleanCachedFeatureParam APPROXIMATE_GEOLOCATION_SAMPLE_DATA =
-            PermissionsAndroidFeatureMap.newBooleanCachedFeatureParam(
-                    APPROXIMATE_GEOLOCATION_PERMISSION, "sample_data", false);
 
     public static final IntCachedFeatureParam APPROXIMATE_GEOLOCATION_PROMPT_ARM =
             PermissionsAndroidFeatureMap.newIntCachedFeatureParam(
