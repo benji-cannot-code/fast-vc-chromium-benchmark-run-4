659de4d292dfa2dab8638408d3fddb3d9b9a7a52
Jan Keitel <jkeitel@google.com>
2025-08-20 23:06:47 -0700

    Add separate build target for strike database core logic
    No functionality changes. The strike database just happens to be one
    of the few areas in the autofill code base that does not depend on
    (almost) any other Autofill dependencies.
    This CL makes that explicit by adding its own build target. It also
    paves the way towards potentially moving strike databases to their
    own component.
    
    Bug: 40100455
    Change-Id: I7bb2d6bb7b91ba527e3b6efe0e0ecbcda2b13c78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6862602
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1504400}

--- components/autofill/core/browser/strike_databases/history_clearable_strike_database_unittest.cc
+++ components/autofill/core/browser/strike_databases/history_clearable_strike_database_unittest.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/scoped_temp_dir.h"
 #include "base/test/task_environment.h"
 #include "base/time/time.h"
-#include "components/autofill/core/browser/strike_databases/addresses/autofill_profile_save_strike_database.h"
 #include "components/autofill/core/browser/strike_databases/strike_database.h"
 #include "components/history/core/browser/history_types.h"
 #include "components/history/core/browser/url_row.h"
