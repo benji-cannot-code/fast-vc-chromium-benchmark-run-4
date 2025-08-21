819e95512e4ecd1a700a54d9c033b9045d67eb72
Hidehiko Abe <hidehiko@chromium.org>
2025-08-21 16:39:03 -0700

    Remove PowerButtonTestBase::Initialize.
    Initialize() is to create a user session. This CL just use
    SimulateUserLogin() with moving a can_lock concept to
    AshTestHelper.
    
    BUG=425160398
    TEST=Tryjob
    
    Change-Id: Ib9258b393aa57ac27f9bd3448238c2ea561f7ca2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6857582
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1504755}

--- ash/system/power/power_button_controller_unittest.cc
+++ ash/system/power/power_button_controller_unittest.cc
@@ -156,7 +156,6 @@ class PowerButtonControllerTest : public PowerButtonTestBase {
 };
 
 TEST_F(PowerButtonControllerTest, LockScreenIfRequired) {
-  Initialize(LoginStatus::USER);
   EnableTabletMode(true);
   SetShouldLockScreenAutomatically(true);
   ASSERT_FALSE(GetLockedState());
@@ -387,7 +386,6 @@ TEST_F(PowerButtonControllerTest, HoldPowerButtonWhileMenuShownInLaptopMode) {
 // Tests press lock button and power button in sequence.
 TEST_F(PowerButtonControllerTest, PressAfterAnotherReleased) {
   // Tap power button after press lock button should still turn screen off.
-  Initialize(LoginStatus::USER);
   EnableTabletMode(true);
   PressLockButton();
   ReleaseLockButton();
@@ -409,7 +407,6 @@ TEST_F(PowerButtonControllerTest, PressAfterAnotherReleased) {
 TEST_F(PowerButtonControllerTest, PressBeforeAnotherReleased) {
   // Press lock button when power button is still being pressed will be ignored
   // and continue to turn screen off.
-  Initialize(LoginStatus::USER);
   EnableTabletMode(true);
   EXPECT_FALSE(power_manager_client()->backlights_forced_off());
   PressPowerButton();
@@ -826,7 +823,7 @@ TEST_F(PowerButtonControllerTest, MenuItemsToLoginAndLockedStatus) {
   // Should have sign out and feedback items if in guest mode (or, generally,
   // if screen locking is disabled).
   ClearLogin();
-  Initialize(LoginStatus::GUEST);
+  SimulateGuestLogin();
   OpenPowerButtonMenu();
   EXPECT_FALSE(GetLockedState());
   EXPECT_TRUE(power_button_test_api_->MenuHasSignOutItem());
@@ -1417,8 +1414,6 @@ class PowerButtonControllerLegacyTest : public PowerButtonControllerTest {
 // Tests that a power button press before the menu is fully shown will not
 // create a new menu.
 TEST_F(PowerButtonControllerLegacyTest, LegacyPowerButtonIgnoreExtraPress) {
-  Initialize(LoginStatus::USER);
-
   // Enable animations so that we can make sure that they occur.
   ui::ScopedAnimationDurationScaleMode regular_animations(
       ui::ScopedAnimationDurationScaleMode::NON_ZERO_DURATION);
--- ash/system/power/power_button_test_base.h
+++ ash/system/power/power_button_test_base.h
@@ -73,9 +73,6 @@ class PowerButtonTestBase : public AshTestBase {
   // Simulates a mouse move event.
   void GenerateMouseMoveEvent();
 
-  // Initializes login status and sets power button type.
-  void Initialize(LoginStatus status);
-
   // Triggers a lock screen operation.
   void LockScreen();
 
