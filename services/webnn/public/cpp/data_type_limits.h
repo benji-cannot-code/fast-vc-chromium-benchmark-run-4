54a92f3eb8d9e12960c1332abb3043b73c3e3fd8
Yuki Shiino <yukishiino@chromium.org>
2025-08-21 00:02:14 -0700

    Revert "[WebNN] Support roundEven operation for WebNN"
    This reverts commit 1d6016972b23a2d0742c8aa4d806360ddab61eae.
    
    Reason for revert: Seems causing test failures of:
    virtual/webnn-service-on-cpu/external/wpt/webnn/conformance_tests/round_even.https.any.html?cpu
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-blink-msan-rel/1561/overview
    
    Bug: 435317709
    Original change's description:
    > [WebNN] Support roundEven operation for WebNN
    >
    > Bug: 435317709
    > Change-Id: Idf037c804e661878c9fe8a0dbac19b98a7fd5327
    > Cq-Include-Trybots: luci.chromium.try:win11-blink-rel, mac14.arm64-blink-rel, mac14-blink-rel, mac15.arm64-blink-rel, mac15-blink-rel, linux-blink-rel
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6803093
    > Reviewed-by: Alex Gough <ajgo@chromium.org>
    > Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    > Reviewed-by: Phillis Tang <phillis@chromium.org>
    > Commit-Queue: Bin Miao <bin.miao@intel.com>
    > Reviewed-by: Robert Flack <flackr@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1504337}
    
    Bug: 435317709
    Cq-Include-Trybots: luci.chromium.try:win11-blink-rel, mac14.arm64-blink-rel, mac14-blink-rel, mac15.arm64-blink-rel, mac15-blink-rel, linux-blink-rel
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Iaffc0d7ba7c775a0d72f29075f5a63fd65febe59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6868972
    Auto-Submit: Yuki Shiino <yukishiino@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1504408}

--- services/webnn/public/cpp/data_type_limits.h
+++ services/webnn/public/cpp/data_type_limits.h
@@ -60,7 +60,6 @@ struct COMPONENT_EXPORT(WEBNN_PUBLIC_CPP) DataTypeLimits {
                  SupportedTensors log_input,
                  SupportedTensors neg_input,
                  SupportedTensors reciprocal_input,
-                 SupportedTensors round_even_input,
                  SupportedTensors sign_input,
                  SupportedTensors sin_input,
                  SupportedTensors sqrt_input,
@@ -186,7 +185,6 @@ struct COMPONENT_EXPORT(WEBNN_PUBLIC_CPP) DataTypeLimits {
   SupportedTensors log_input;
   SupportedTensors neg_input;
   SupportedTensors reciprocal_input;
-  SupportedTensors round_even_input;
   SupportedTensors sign_input;
   SupportedTensors sin_input;
   SupportedTensors sqrt_input;
@@ -305,7 +303,6 @@ inline bool operator==(const DataTypeLimits& lhs, const DataTypeLimits& rhs) {
          lhs.log_input == rhs.log_input &&
          lhs.neg_input == rhs.neg_input &&
          lhs.reciprocal_input == rhs.reciprocal_input &&
-         lhs.round_even_input == rhs.round_even_input &&
          lhs.sign_input == rhs.sign_input &&
          lhs.sin_input == rhs.sin_input &&
          lhs.sqrt_input == rhs.sqrt_input &&
--- third_party/blink/renderer/modules/ml/webnn/ml_graph_builder.h
+++ third_party/blink/renderer/modules/ml/webnn/ml_graph_builder.h
@@ -235,9 +235,6 @@ class MODULES_EXPORT MLGraphBuilder final : public ScriptWrappable {
   MLOperand* neg(MLOperand* input,
                  MLOperatorOptions* options,
                  ExceptionState& exception_state);
-  MLOperand* roundEven(MLOperand* input,
-                       MLOperatorOptions* options,
-                       ExceptionState& exception_state);
   MLOperand* sign(MLOperand* input,
                   MLOperatorOptions* options,
                   ExceptionState& exception_state);
