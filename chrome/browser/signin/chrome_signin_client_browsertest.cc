a947b893195fc9fce70d8e988470f0f5ce7df208
Duong Dac <ddac@chromium.org>
2025-08-21 04:01:41 -0700

    Deflake HatsSurveyLaunchedOnBrowserCreationAfterSignin test
    This CL
    - replaces the use of `browser()->profile()` when there's no browser,
    - adds `ScopedKeepAlive`.
    
    Bug: 433498793
    Change-Id: I6207611a59e650411133b3ba92409980c97deb4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6862709
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1504469}

--- chrome/browser/signin/chrome_signin_client_browsertest.cc
+++ chrome/browser/signin/chrome_signin_client_browsertest.cc
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/hats/mock_hats_service.h"
 #include "chrome/browser/ui/hats/survey_config.h"
 #include "chrome/test/base/in_process_browser_test.h"
+#include "components/keep_alive_registry/keep_alive_types.h"
+#include "components/keep_alive_registry/scoped_keep_alive.h"
 #include "components/prefs/pref_service.h"
 #include "components/signin/public/base/consent_level.h"
 #include "components/signin/public/base/signin_switches.h"
@@ -165,19 +167,15 @@ IN_PROC_BROWSER_TEST_F(ChromeSigninClientHatsSurveyBrowserTest,
   signin::WaitForRefreshTokensLoaded(identity_manager);
 }
 
-// TODO(crbug.com/433498793): Re-enable this flaky test on Windows.
-#if BUILDFLAG(IS_WIN)
-#define MAYBE_HatsSurveyLaunchedOnBrowserCreationAfterSignin DISABLED_HatsSurveyLaunchedOnBrowserCreationAfterSignin
-#else
-#define MAYBE_HatsSurveyLaunchedOnBrowserCreationAfterSignin HatsSurveyLaunchedOnBrowserCreationAfterSignin
-#endif
-
 // Tests that if a user signs in when no browser is open, the HaTS survey is
 // launched immediately when a browser is subsequently created for that profile.
 IN_PROC_BROWSER_TEST_F(ChromeSigninClientHatsSurveyBrowserTest,
-  MAYBE_HatsSurveyLaunchedOnBrowserCreationAfterSignin) {
-  // Keep the profile alive and close all existing browsers.
+                       HatsSurveyLaunchedOnBrowserCreationAfterSignin) {
   Profile* profile = browser()->profile();
+  // Keep the browser process running while browsers are closed.
+  ScopedKeepAlive keep_alive(KeepAliveOrigin::BROWSER,
+                             KeepAliveRestartOption::DISABLED);
+  // Keep the profile alive and close all existing browsers.
   ScopedProfileKeepAlive profile_keep_alive(
       profile, ProfileKeepAliveOrigin::kProfilePickerView);
   CloseAllBrowsers();
@@ -185,7 +183,7 @@ IN_PROC_BROWSER_TEST_F(ChromeSigninClientHatsSurveyBrowserTest,
   // Sign in to Chrome. The survey won't launch yet, as it requires an active
   // browser.
   signin::MakeAccountAvailable(
-      IdentityManagerFactory::GetForProfile(browser()->profile()),
+      IdentityManagerFactory::GetForProfile(profile),
       signin::AccountAvailabilityOptionsBuilder()
           .AsPrimary(signin::ConsentLevel::kSignin)
           .WithAccessPoint(signin_metrics::AccessPoint::kForYouFre)
@@ -208,6 +206,7 @@ IN_PROC_BROWSER_TEST_F(ChromeSigninClientHatsSurveyBrowserTest,
 
   // Create a new browser for the signed-in profile, which should now trigger
   // the survey.
-  CreateBrowser(profile);
+  Browser* new_browser = CreateBrowser(profile);
+  ASSERT_TRUE(new_browser);
 }
 #endif  // BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_WIN)
