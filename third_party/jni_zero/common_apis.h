9a1a3d92afa5b8fdfa14350ed6a94a07b928d07a
Matt Stark <msta@google.com>
2025-08-20 21:55:43 -0700

    Fix missing include in jni zero
    Bug: 40263312
    Change-Id: I6a6a696436e883614ca3ac0899524bc2a0d300f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6869451
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Auto-Submit: Matt Stark <msta@google.com>
    Owners-Override: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1504386}

--- third_party/jni_zero/common_apis.h
+++ third_party/jni_zero/common_apis.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define JNI_ZERO_COMMON_APIS_H_
 
 #include <jni.h>
+#include <stdint.h>
 
 #include "third_party/jni_zero/java_refs.h"
 #include "third_party/jni_zero/jni_export.h"
