b0f804d1746ba28495904d1b87bec1eaa74ba31c
Christian Xu <christianxu@google.com>
2025-08-21 01:30:55 -0700

    [iOS][AIM] Prewarm gallery picker
    Prewarm the gallery picker to avoid delay when tapping on the button.
    
    Bug: None
    Change-Id: Id4a311a05d10c7353692f37191b1d07c40cdbcbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6863278
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1504417}

--- ios/chrome/browser/aim/prototype/coordinator/aim_prototype_coordinator.mm
+++ ios/chrome/browser/aim/prototype/coordinator/aim_prototype_coordinator.mm
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @implementation AIMPrototypeCoordinator {
   AIMPrototypeViewController* _viewController;
   AIMPrototypeMediator* _mediator;
+  /// The prewarmed picker as it takes time to appear.
+  PHPickerViewController* _picker;
 }
 
 - (void)start {
@@ -70,6 +72,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [_viewController.presentingViewController dismissViewControllerAnimated:YES
                                                                completion:nil];
   _viewController = nil;
+  _picker = nil;
   [_mediator disconnect];
   _mediator = nil;
 }
@@ -99,14 +102,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)aimPrototypeViewControllerDidTapGalleryButton:
     (AIMPrototypeViewController*)viewController {
-  PHPickerConfiguration* config = [[PHPickerConfiguration alloc]
-      initWithPhotoLibrary:PHPhotoLibrary.sharedPhotoLibrary];
-  config.selectionLimit = 1;
-  config.filter = [PHPickerFilter imagesFilter];
-  PHPickerViewController* picker =
-      [[PHPickerViewController alloc] initWithConfiguration:config];
-  picker.delegate = self;
-  [_viewController presentViewController:picker animated:YES completion:nil];
+  if (!_picker) {
+    [self aimPrototypeViewControllerMayShowGalleryPicker:viewController];
+  }
+  [_viewController presentViewController:_picker animated:YES completion:nil];
+  _picker = nil;
 }
 
 - (void)aimPrototypeViewControllerDidTapCameraButton:
@@ -120,6 +120,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   picker.delegate = self;
   picker.sourceType = UIImagePickerControllerSourceTypeCamera;
   [_viewController presentViewController:picker animated:YES completion:nil];
+}
+
+- (void)aimPrototypeViewControllerMayShowGalleryPicker:
+    (AIMPrototypeViewController*)viewController {
+  if (_picker) {
+    return;
+  }
+  PHPickerConfiguration* config = [[PHPickerConfiguration alloc]
+      initWithPhotoLibrary:PHPhotoLibrary.sharedPhotoLibrary];
+  config.selectionLimit = 1;
+  config.filter = [PHPickerFilter imagesFilter];
+  _picker = [[PHPickerViewController alloc] initWithConfiguration:config];
+  _picker.delegate = self;
 }
 
 #pragma mark - PHPickerViewControllerDelegate
--- ios/chrome/browser/aim/prototype/ui/aim_prototype_view_controller.h
+++ ios/chrome/browser/aim/prototype/ui/aim_prototype_view_controller.h
@@ -21,6 +21,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     (AIMPrototypeViewController*)viewController;
 - (void)aimPrototypeViewControllerDidTapCameraButton:
     (AIMPrototypeViewController*)viewController;
+- (void)aimPrototypeViewControllerMayShowGalleryPicker:
+    (AIMPrototypeViewController*)viewController;
 @end
 
 // View controller for the AIM prototype.
