f0fe0d2968c871e07f7ccf7ab8ebce14a9525fcb
Duong Dac <ddac@chromium.org>
2025-08-21 10:18:41 -0700

    Update product-specific data checks for Identity HaTS tests
    On certain Windows trybots, the channel is set to "stable" instead of
    "unknown", causing tests to fail. This CL updates the HaTS tests to use
    `UnorderedElementsAre`, allowing for more flexibility.
    
    Bug: 427971911
    Change-Id: I7bb31241b7b589bb39255c1039e16f656189cf93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6870130
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1504610}

--- chrome/browser/ui/views/profiles/profile_picker_view_browsertest.cc
+++ chrome/browser/ui/views/profiles/profile_picker_view_browsertest.cc
@@ -160,6 +160,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using signin::constants::kNoHostedDomainFound;
 using testing::_;
 using testing::Eq;
+using testing::Pair;
+using testing::UnorderedElementsAre;
 
 namespace {
 const SkColor kProfileColor = SK_ColorRED;
@@ -2045,16 +2047,16 @@ IN_PROC_BROWSER_TEST_F(ProfilePickerCreationFlowBrowserTest,
 
   // Verify that a HaTS survey is launched when the user switch profile with the
   // profile picker.
-  std::map<std::string, std::string> expected_string_psd = {
-      {"Channel", "unknown"},
-      {"Chrome Version", version_info::GetVersion().GetString()},
-      {"Number of Chrome Profiles", "2"},
-      {"Number of Google Accounts", "0"},
-      {"Sign-in Status", "Signed Out"}};
-  EXPECT_CALL(*hats_service,
-              LaunchDelayedSurvey(
-                  kHatsSurveyTriggerIdentitySwitchProfileFromProfilePicker, _,
-                  _, Eq(expected_string_psd)))
+  EXPECT_CALL(
+      *hats_service,
+      LaunchDelayedSurvey(
+          kHatsSurveyTriggerIdentitySwitchProfileFromProfilePicker, _, _,
+          UnorderedElementsAre(
+              Pair("Channel", _),
+              Pair("Chrome Version", version_info::GetVersion().GetString()),
+              Pair("Number of Chrome Profiles", "2"),
+              Pair("Number of Google Accounts", "0"),
+              Pair("Sign-in Status", "Signed Out"))))
       .Times(2);
 
   // Open the picker.
