d7ff71a33213288cbfc23b3a096f34ed0d8ba0fb
Keren Zhu <kerenzhu@chromium.org>
2025-08-20 20:04:27 -0700

    IWYU: Add missing include for bubble_dialog_delegate_view.h
    Explicitly include ui/views/bubble/bubble_dialog_delegate_view.h in
    files that use BubbleDialogDelegate and BubbleDialogDelegateView. This
    fixes linker error on win-libfuzzer-asan-rel bot when the function
    signature in bubble_dialog_delegate_view.h changes (e.g.
    https://ci.chromium.org/ui/p/chromium/builders/try/win-libfuzzer-asan-rel/2288943).
    
    Bug: none
    Change-Id: I3271c4fcc4e8b0535ccd84752400a923d52c15ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6868256
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Auto-Submit: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1504361}

--- chrome/browser/ui/plus_addresses/views/plus_address_creation_dialog_delegate.cc
+++ chrome/browser/ui/plus_addresses/views/plus_address_creation_dialog_delegate.cc
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/background.h"
 #include "ui/views/bubble/bubble_border.h"
+#include "ui/views/bubble/bubble_dialog_delegate_view.h"
 #include "ui/views/controls/button/image_button.h"
 #include "ui/views/controls/button/image_button_factory.h"
 #include "ui/views/controls/button/md_text_button.h"
--- chrome/browser/ui/views/download/bubble/download_toolbar_ui_controller.cc
+++ chrome/browser/ui/views/download/bubble/download_toolbar_ui_controller.cc
@@ -61,6 +61,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/render_text.h"
 #include "ui/gfx/text_constants.h"
 #include "ui/views/accessibility/view_accessibility.h"
+#include "ui/views/bubble/bubble_dialog_delegate_view.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/controls/image_view.h"
 #include "ui/views/controls/progress_ring_utils.h"
