cbaf376fbf4d84b86cad505876b3d9a38c25f89e
Louis Romero <lpromero@google.com>
2025-08-21 00:36:50 -0700

    [iOS] Add SF Symbols to View menu commands
    This change adds SF Symbols to the commands in the View menu, improving
    the visual presentation and discoverability of these actions.
    
    The following commands have been updated with symbols:
    - Stop (xmark)
    - Reload (arrow.clockwise)
    - Go to Tab Grid (square.on.square)
    
    Screenshot: https://screenshot.googleplex.com/3bFysiUHeHeEBXJ
    
    Bug: 438114271
    Change-Id: I05f4a6b5eecf926cf91e455273b3788e39dd3b49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6860826
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1504415}

--- ios/chrome/browser/keyboard/ui_bundled/UIKeyCommand+Chrome.mm
+++ ios/chrome/browser/keyboard/ui_bundled/UIKeyCommand+Chrome.mm
@@ -181,10 +181,12 @@ const char kMobileKeyCommandClose[] = "MobileKeyCommandClose";
 }
 
 + (UIKeyCommand*)cr_reload {
+  UIImage* image = CustomSymbolWithConfiguration(kArrowClockWiseSymbol, nil);
   return [self cr_commandWithInput:@"r"
                      modifierFlags:Command
                             action:@selector(keyCommand_reload)
-                   titleIDAsString:@"IDS_IOS_KEYBOARD_RELOAD"];
+                   titleIDAsString:@"IDS_IOS_KEYBOARD_RELOAD"
+                             image:image];
 }
 
 + (UIKeyCommand*)cr_back {
@@ -257,10 +259,12 @@ const char kMobileKeyCommandClose[] = "MobileKeyCommandClose";
 }
 
 + (UIKeyCommand*)cr_stop {
+  UIImage* image = DefaultSymbolWithConfiguration(kXMarkSymbol, nil);
   return [self cr_commandWithInput:@"."
                      modifierFlags:Command
                             action:@selector(keyCommand_stop)
-                   titleIDAsString:@"IDS_IOS_KEYBOARD_STOP"];
+                   titleIDAsString:@"IDS_IOS_KEYBOARD_STOP"
+                             image:image];
 }
 
 + (UIKeyCommand*)cr_showHelp {
@@ -375,10 +379,12 @@ const char kMobileKeyCommandClose[] = "MobileKeyCommandClose";
 }
 
 + (UIKeyCommand*)cr_goToTabGrid {
+  UIImage* image = DefaultSymbolWithConfiguration(kTabsSymbol, nil);
   return [self cr_commandWithInput:@"\\"
                      modifierFlags:ShiftCommand
                             action:@selector(keyCommand_goToTabGrid)
-                   titleIDAsString:@"IDS_IOS_KEYBOARD_GO_TO_TAB_GRID"];
+                   titleIDAsString:@"IDS_IOS_KEYBOARD_GO_TO_TAB_GRID"
+                             image:image];
 }
 
 + (UIKeyCommand*)cr_clearBrowsingData {
