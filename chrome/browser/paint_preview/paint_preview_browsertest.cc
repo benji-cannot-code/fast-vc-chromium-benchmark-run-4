ba1f1afc7a2fbe1dab27742f06d1a85054b62c7b
Nate Chapin <japhet@chromium.org>
2025-08-22 15:03:38 -0700

    Revert "[Paint Preview] Extract common MockPaintPreviewRecorder class"
    This reverts commit 608fef36503d76105c5ab880522aa2043c267223.
    
    Reason for revert: closed the tree: https://ci.chromium.org/ui/p/chromium/builders/ci/android-cast-arm-rel/17570/overview
    
    Bug: b:434992768
    Original change's description:
    > [Paint Preview] Extract common MockPaintPreviewRecorder class
    >
    > There are 5 MockPaintPreviewRecorder classes that all do roughly the
    > same things; we can extract those to a single class that all the tests
    > can reuse.
    >
    > Bug: b:434992768
    > Change-Id: I5f14c6a2babd6543dee7cf5e485ed1d0abb43337
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6875225
    > Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    > Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    > Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    > Reviewed-by: Victor Vianna <victorvianna@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1505343}
    
    Bug: b:434992768
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I758b034bd333bf0e6ba5a25e1d06b994a415048c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6874311
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Owners-Override: Nate Chapin <japhet@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1505353}

--- chrome/browser/paint_preview/paint_preview_browsertest.cc
+++ chrome/browser/paint_preview/paint_preview_browsertest.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/paint_preview/browser/paint_preview_client.h"
 #include "components/paint_preview/common/file_stream.h"
-#include "components/paint_preview/common/mock_paint_preview_recorder.h"
 #include "components/paint_preview/common/mojom/paint_preview_recorder.mojom-shared.h"
 #include "components/paint_preview/common/proto/paint_preview.pb.h"
 #include "components/paint_preview/common/recording_map.h"
@@ -42,7 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace paint_preview {
 
-class NoOpPaintPreviewRecorder : public MockPaintPreviewRecorder {
+class NoOpPaintPreviewRecorder : public mojom::PaintPreviewRecorder {
  public:
   NoOpPaintPreviewRecorder() = default;
   ~NoOpPaintPreviewRecorder() override = default;
@@ -50,11 +49,27 @@ class NoOpPaintPreviewRecorder : public MockPaintPreviewRecorder {
   NoOpPaintPreviewRecorder(const NoOpPaintPreviewRecorder&) = delete;
   NoOpPaintPreviewRecorder& operator=(const NoOpPaintPreviewRecorder&) = delete;
 
- protected:
-  void CheckParams(const mojom::PaintPreviewCaptureParamsPtr& params) override {
+  void SetRequestedClosure(base::OnceClosure requested) {
+    requested_ = std::move(requested);
+  }
+
+  void CapturePaintPreview(
+      mojom::PaintPreviewCaptureParamsPtr params,
+      mojom::PaintPreviewRecorder::CapturePaintPreviewCallback callback)
+      override {
+    callback_ = std::move(callback);
+    std::move(requested_).Run();
+  }
+
+  void BindRequest(mojo::ScopedInterfaceEndpointHandle handle) {
+    binding_.Bind(mojo::PendingAssociatedReceiver<mojom::PaintPreviewRecorder>(
+        std::move(handle)));
   }
 
  private:
+  base::OnceClosure requested_;
+  mojom::PaintPreviewRecorder::CapturePaintPreviewCallback callback_;
+  mojo::AssociatedReceiver<mojom::PaintPreviewRecorder> binding_{this};
 };
 
 // Test harness for a integration test of paint previews. In this test:
@@ -335,7 +350,7 @@ IN_PROC_BROWSER_TEST_P(PaintPreviewFencedFrameBrowserTest,
   // Override remote interfaces of the fenced frame with a no-op.
   base::RunLoop started_loop;
   NoOpPaintPreviewRecorder noop_recorder;
-  noop_recorder.SetReceivedRequestClosure(started_loop.QuitClosure());
+  noop_recorder.SetRequestedClosure(started_loop.QuitClosure());
 
   OverrideInterface(&noop_recorder, fenced_rfh_wrapper.get());
 
@@ -506,7 +521,7 @@ IN_PROC_BROWSER_TEST_P(PaintPreviewBrowserTest, DontReloadInRenderProcessExit) {
   // Override remote interfaces with a no-op.
   base::RunLoop started_loop;
   NoOpPaintPreviewRecorder noop_recorder;
-  noop_recorder.SetReceivedRequestClosure(started_loop.QuitClosure());
+  noop_recorder.SetRequestedClosure(started_loop.QuitClosure());
   OverrideInterface(&noop_recorder, GetWebContents()->GetPrimaryMainFrame());
 
   CreateClient();
--- components/paint_preview/browser/paint_preview_base_service_unittest.cc
+++ components/paint_preview/browser/paint_preview_base_service_unittest.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "components/paint_preview/browser/paint_preview_base_service_test_factory.h"
 #include "components/paint_preview/browser/paint_preview_file_mixin.h"
-#include "components/paint_preview/common/mock_paint_preview_recorder.h"
 #include "components/paint_preview/common/mojom/paint_preview_recorder.mojom.h"
 #include "components/paint_preview/common/mojom/paint_preview_types.mojom.h"
 #include "components/paint_preview/common/serialized_recording.h"
@@ -76,14 +75,42 @@ base::FilePath CreateDir(scoped_refptr<FileManager> manager,
 
 }  // namespace
 
-class NoGuidMockPaintPreviewRecorder : public MockPaintPreviewRecorder {
+class MockPaintPreviewRecorder : public mojom::PaintPreviewRecorder {
  public:
-  NoGuidMockPaintPreviewRecorder() = default;
-  ~NoGuidMockPaintPreviewRecorder() override = default;
+  MockPaintPreviewRecorder() = default;
+  ~MockPaintPreviewRecorder() override = default;
 
- protected:
-  void CheckParams(
-      const mojom::PaintPreviewCaptureParamsPtr& input_params) override {
+  void CapturePaintPreview(
+      mojom::PaintPreviewCaptureParamsPtr params,
+      mojom::PaintPreviewRecorder::CapturePaintPreviewCallback callback)
+      override {
+    {
+      base::ScopedAllowBlockingForTesting scope;
+      CheckParams(std::move(params));
+      std::move(callback).Run(status_, std::move(response_));
+    }
+  }
+
+  void SetExpectedParams(mojom::PaintPreviewCaptureParamsPtr params) {
+    expected_params_ = std::move(params);
+  }
+
+  void SetResponse(mojom::PaintPreviewStatus status,
+                   mojom::PaintPreviewCaptureResponsePtr response) {
+    status_ = status;
+    response_ = std::move(response);
+  }
+
+  void BindRequest(mojo::ScopedInterfaceEndpointHandle handle) {
+    binding_.Bind(mojo::PendingAssociatedReceiver<mojom::PaintPreviewRecorder>(
+        std::move(handle)));
+  }
+
+  MockPaintPreviewRecorder(const MockPaintPreviewRecorder&) = delete;
+  MockPaintPreviewRecorder& operator=(const MockPaintPreviewRecorder&) = delete;
+
+ private:
+  void CheckParams(mojom::PaintPreviewCaptureParamsPtr input_params) {
     // Ignore GUID and File as this is internal information not known by the
     // Keyed Service API.
     EXPECT_EQ(input_params->clip_rect, expected_params_->clip_rect);
@@ -100,6 +127,11 @@ class NoGuidMockPaintPreviewRecorder : public MockPaintPreviewRecorder {
     }
     EXPECT_EQ(input_params->is_main_frame, expected_params_->is_main_frame);
   }
+
+  mojom::PaintPreviewCaptureParamsPtr expected_params_;
+  mojom::PaintPreviewStatus status_;
+  mojom::PaintPreviewCaptureResponsePtr response_;
+  mojo::AssociatedReceiver<mojom::PaintPreviewRecorder> binding_{this};
 };
 
 class PaintPreviewBaseServiceTest
@@ -131,12 +163,12 @@ class PaintPreviewBaseServiceTest
         web_contents(), GURL("https://www.chromium.org"));
   }
 
-  void OverrideInterface(NoGuidMockPaintPreviewRecorder* service) {
+  void OverrideInterface(MockPaintPreviewRecorder* service) {
     blink::AssociatedInterfaceProvider* remote_interfaces =
         main_rfh()->GetRemoteAssociatedInterfaces();
     remote_interfaces->OverrideBinderForTesting(
         mojom::PaintPreviewRecorder::Name_,
-        base::BindRepeating(&NoGuidMockPaintPreviewRecorder::BindRequest,
+        base::BindRepeating(&MockPaintPreviewRecorder::BindRequest,
                             base::Unretained(service)));
   }
 
@@ -178,7 +210,7 @@ class PaintPreviewBaseServiceTest
 };
 
 TEST_P(PaintPreviewBaseServiceTest, CaptureMainFrame) {
-  NoGuidMockPaintPreviewRecorder recorder;
+  MockPaintPreviewRecorder recorder;
   auto params = mojom::PaintPreviewCaptureParams::New();
   params->clip_rect = gfx::Rect(0, 0, 0, 0);
   params->clip_x_coord_override =
@@ -249,7 +281,7 @@ TEST_P(PaintPreviewBaseServiceTest, CaptureMainFrame) {
 }
 
 TEST_P(PaintPreviewBaseServiceTest, CaptureFailed) {
-  NoGuidMockPaintPreviewRecorder recorder;
+  MockPaintPreviewRecorder recorder;
   auto params = mojom::PaintPreviewCaptureParams::New();
   params->clip_rect = gfx::Rect(0, 0, 0, 0);
   params->clip_x_coord_override =
@@ -288,7 +320,7 @@ TEST_P(PaintPreviewBaseServiceTest, CaptureFailed) {
 }
 
 TEST_P(PaintPreviewBaseServiceTest, CaptureDisallowed) {
-  NoGuidMockPaintPreviewRecorder recorder;
+  MockPaintPreviewRecorder recorder;
   auto params = mojom::PaintPreviewCaptureParams::New();
   params->clip_rect = gfx::Rect(0, 0, 0, 0);
   params->clip_x_coord_override =
