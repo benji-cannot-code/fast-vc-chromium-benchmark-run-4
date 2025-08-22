68e6292d0be97181fe187155df1744c25a5c5e9d
Steinar H. Gunderson <sesse@chromium.org>
2025-08-22 06:24:43 -0700

    Store MixinMap persistently on StyleSheetContents.
    This is effectively a cache; it's not because it's so expensive to
    create (it's actually very cheap), but it will make it easier to do
    proper invalidation in the next patch when we know what we used to have
    and when it changed. If we ever do precise invalidation of mixins,
    it will be even more important, as we need to know what we had before.
    
    Change-Id: I4106849d7f1089617a7c940f78b9161efdf9ec0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6870710
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1505010}

---
+++ third_party/blink/renderer/core/css/mixin_map.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/core/css/mixin_map.h"
+
+#include "third_party/blink/renderer/core/css/style_rule.h"
+
+namespace blink {
+
+void MixinMap::Merge(const MixinMap& other) {
+  for (const auto& [key, value] : other.mixins) {
+    mixins.Set(key, value);
+  }
+  media_query_result_flags.Add(other.media_query_result_flags);
+  media_query_set_results.AppendVector(other.media_query_set_results);
+}
+
+}  // namespace blink
