8805cf7e82f1d4b78bdc7eafc4572c5d9302de72
Evan Luo <evanluo@google.com>
2025-08-22 13:17:19 -0700

    [reader mode] Introduce new reading mode prefs UI to bottom sheet
    Add logic into the ReaderModePrefsView to manage the various elements
    within the new prefs UI for reader mode. Previous CL was submitted where
    xml and initial UI was created in reader_mode_prefs_view. Consolidates
    metrics used by both DistilledPagePrefsView and ReaderModePrefsView into
    one metrics file.
    
    Screen recording:
    http://screencast/cast/NjUyMjY3NTY3MjM4MzQ4OHw5ZWIwNWFmOC1mNg
    
    Bug: 430372209
    Change-Id: I147bb3601fa54551bb538e20b9290045cebaca6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6872822
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Commit-Queue: Evan Luo <evanluo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1505285}

--- chrome/android/java/src/org/chromium/chrome/browser/dom_distiller/ReaderModeBottomSheetCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/dom_distiller/ReaderModeBottomSheetCoordinator.java
@@ -52,8 +52,7 @@ public class ReaderModeBottomSheetCoordinator {
         mPropertyModel = new PropertyModel(ReaderModeBottomSheetProperties.ALL_KEYS);
         mPropertyModel.set(
                 ReaderModeBottomSheetProperties.CONTENT_VIEW,
-                DistilledPagePrefsView.create(
-                        mContext, mDomDistillerService.getDistilledPagePrefs()));
+                ReaderModePrefsView.create(mContext, mDomDistillerService.getDistilledPagePrefs()));
         mReaderModeBottomSheetView =
                 (ReaderModeBottomSheetView)
                         LayoutInflater.from(mContext)
@@ -81,6 +80,7 @@ public class ReaderModeBottomSheetCoordinator {
                 mBottomSheetController.getCurrentSheetContent()
                         instanceof ReaderModeBottomSheetContent;
         if (!showing) {
+            ReaderModeMetrics.reportReaderModePrefsOpened();
             showing =
                     mBottomSheetController.requestShowContent(
                             mBottomSheetContent, /* animate= */ true);
@@ -166,8 +166,7 @@ public class ReaderModeBottomSheetCoordinator {
 
         @Override
         public int getPeekHeight() {
-            return mContentView.findViewById(R.id.drag_handle).getHeight()
-                    + mContentView.findViewById(R.id.title).getHeight();
+            return ((ReaderModeBottomSheetView) mContentView).getPeekHeight();
         }
 
         @Override
