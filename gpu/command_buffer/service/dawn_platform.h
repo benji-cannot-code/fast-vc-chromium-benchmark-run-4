2a7efb0891b705821414c813be5ff519b3268314
Le Hoang Quyen <lehoangquyen@chromium.org>
2025-08-22 07:06:34 -0700

    Graphite: Add CacheHitPercentage UMA metric for Dawn Shaders
    This CL adds a new UMA metric to track the cache hit percentage for
    Dawn shaders within 90 seconds of startup.
    
    Bug: 439845637
    Change-Id: I8f6736e29be180d63adbacec04ba860ff3085cdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6873442
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1505030}

--- gpu/command_buffer/service/dawn_platform.h
+++ gpu/command_buffer/service/dawn_platform.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/lock.h"
 #include "base/time/time.h"
 #include "gpu/command_buffer/service/dawn_caching_interface.h"
+#include "gpu/gpu_gles2_export.h"
 
 namespace gl {
 class ProgressReporter;
@@ -21,7 +22,7 @@ class ProgressReporter;
 
 namespace gpu::webgpu {
 
-class DawnPlatform : public dawn::platform::Platform {
+class GPU_GLES2_EXPORT DawnPlatform : public dawn::platform::Platform {
  public:
   explicit DawnPlatform(
       std::unique_ptr<DawnCachingInterface> dawn_caching_interface,
