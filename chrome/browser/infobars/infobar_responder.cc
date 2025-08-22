02b33eca99b44a1ce8035302890df945582a5460
Foromo Daniel Soromou <koretadaniel@chromium.org>
2025-08-21 19:01:22 -0700

    [Cleanup] (1) Update InfobarResponder to use scoped observation
    Bug: None
    Change-Id: I0abd696cfadb0f85ddd5941e325da264a2ceb6c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6839896
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Daniel Soromou <koretadaniel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1504807}

--- chrome/browser/infobars/infobar_responder.cc
+++ chrome/browser/infobars/infobar_responder.cc
@@ -15,18 +15,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 InfoBarResponder::InfoBarResponder(
     infobars::ContentInfoBarManager* infobar_manager,
     AutoResponseType response)
-    : infobar_manager_(infobar_manager), response_(response) {
-  infobar_manager_->AddObserver(this);
+    : response_(response) {
+  infobar_scoped_observation_.Observe(infobar_manager);
 }
 
-InfoBarResponder::~InfoBarResponder() {
-  // This is safe even if we were already removed as an observer in
-  // OnInfoBarAdded().
-  infobar_manager_->RemoveObserver(this);
-}
+InfoBarResponder::~InfoBarResponder() = default;
 
 void InfoBarResponder::OnInfoBarAdded(infobars::InfoBar* infobar) {
-  infobar_manager_->RemoveObserver(this);
+  infobar_scoped_observation_.Reset();
   ConfirmInfoBarDelegate* delegate =
       infobar->delegate()->AsConfirmInfoBarDelegate();
   DCHECK(delegate);
--- chrome/browser/infobars/infobar_responder.h
+++ chrome/browser/infobars/infobar_responder.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_INFOBARS_INFOBAR_RESPONDER_H_
 
 #include "base/memory/raw_ptr.h"
+#include "base/scoped_observation.h"
 #include "components/infobars/core/infobar_manager.h"
 
 namespace infobars {
@@ -42,7 +43,11 @@ class InfoBarResponder : public infobars::InfoBarManager::Observer {
  private:
   void Respond(ConfirmInfoBarDelegate* delegate);
 
-  raw_ptr<infobars::ContentInfoBarManager> infobar_manager_;
+  // Scoped observer that facilitates observing an InfoBarManager.
+  base::ScopedObservation<infobars::InfoBarManager,
+                          infobars::InfoBarManager::Observer>
+      infobar_scoped_observation_{this};
+
   AutoResponseType response_;
 };
 
