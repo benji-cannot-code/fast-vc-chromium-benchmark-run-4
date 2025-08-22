9f6c6bb0295e212849503ab8eaa43e7ca4e9d15c
Bo Liu <boliu@chromium.org>
2025-08-22 13:26:48 -0700

    viz: Remove unused FrameSinkObserver methods
    Change-Id: Ia36edc652495fd04eef4da038ce3d11c6d3fa99f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6874969
    Auto-Submit: Bo Liu <boliu@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1505295}

--- components/viz/service/frame_sinks/frame_sink_manager_impl.cc
+++ components/viz/service/frame_sinks/frame_sink_manager_impl.cc
@@ -191,9 +191,6 @@ void FrameSinkManagerImpl::RegisterFrameSinkId(const FrameSinkId& frame_sink_id,
 
   if (video_detector_)
     video_detector_->OnFrameSinkIdRegistered(frame_sink_id);
-
-  for (auto& observer : observer_list_)
-    observer.OnRegisteredFrameSinkId(frame_sink_id);
 }
 
 void FrameSinkManagerImpl::InvalidateFrameSinkId(
@@ -213,9 +210,6 @@ void FrameSinkManagerImpl::InvalidateFrameSinkId(
   root_sink_map_.erase(frame_sink_id);
 
   frame_sink_data_.erase(frame_sink_id);
-
-  for (auto& observer : observer_list_)
-    observer.OnInvalidatedFrameSinkId(frame_sink_id);
 }
 
 void FrameSinkManagerImpl::SetFrameSinkDebugLabel(
@@ -623,9 +617,6 @@ void FrameSinkManagerImpl::RegisterCompositorFrameSinkSupport(
   auto it = frame_sink_source_map_.find(frame_sink_id);
   if (it != frame_sink_source_map_.end() && it->second.source)
     support->SetBeginFrameSource(it->second.source);
-
-  for (auto& observer : observer_list_)
-    observer.OnCreatedCompositorFrameSink(frame_sink_id, support->is_root());
 
   if (global_throttle_interval_) {
     UpdateThrottlingRecursively(frame_sink_id,
