30996eb6756900a7b6fa01b5943cb06962fb5a51
Joey Arhar <jarhar@chromium.org>
2025-08-22 07:20:23 -0700

    Add isAppearanceBase to DevTools protocol
    This will be used to correctly render a strikethrough in
    -internal-auto-base() in the styles sidebar, which currently uses an
    incorrect approximation of what the browser is actually doing.
    
    -internal-auto-base() is currently only used for customizable select,
    but it will have more usage in the future for other form control
    elements.
    
    Bug: 399562763
    Change-Id: I3ede122c44005aa151b7245964caeeffba8fb949
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6779000
    Reviewed-by: Maksim Sadym <sadym@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1505042}

--- third_party/blink/renderer/core/inspector/inspector_css_agent.cc
+++ third_party/blink/renderer/core/inspector/inspector_css_agent.cc
@@ -2099,7 +2099,8 @@ protocol::Response InspectorCSSAgent::getInlineStylesForNode(
 protocol::Response InspectorCSSAgent::getComputedStyleForNode(
     int node_id,
     std::unique_ptr<protocol::Array<protocol::CSS::CSSComputedStyleProperty>>*
-        style) {
+        style,
+    std::unique_ptr<protocol::CSS::ComputedStyleExtraFields>* extra_fields) {
   protocol::Response response = AssertEnabled();
   if (!response.IsSuccess())
     return response;
@@ -2148,6 +2149,15 @@ protocol::Response InspectorCSSAgent::getComputedStyleForNode(
                                .setValue(it.value->CssText())
                                .build());
   }
+
+  bool is_appearance_base = false;
+  if (auto* computed_style = element->GetComputedStyle()) {
+    is_appearance_base = computed_style->InBaseSelectAppearance();
+  }
+  *extra_fields = protocol::CSS::ComputedStyleExtraFields::create()
+                      .setIsAppearanceBase(is_appearance_base)
+                      .build();
+
   return protocol::Response::Success();
 }
 
---
+++ third_party/blink/web_tests/inspector-protocol/css/css-is-appearance-base.js
@@ -0,0 +1,55 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(/** @type {import('test_runner').TestRunner} */ testRunner) {
+  const {page, session, dp} = await testRunner.startHTML(`
+<!DOCTYPE html>
+<style>
+.base, .base::picker(select) {
+  appearance: base-select;
+}
+</style>
+
+<select class=base>
+  <button>button</button>
+  <option>option</option>
+  <optgroup>
+    <legend>legend</legend>
+  </optgroup>
+</select>
+
+<select class=auto>
+  <button>button</button>
+  <option>option</option>
+  <optgroup>
+    <legend>legend</legend>
+  </optgroup>
+</select>
+`, 'The test verifies functionality of protocol method CSS.isAppearanceBase.');
+
+  await dp.DOM.enable();
+  await dp.CSS.enable();
+  const CSSHelper = await testRunner.loadScript('../resources/css-helper.js');
+  const cssHelper = new CSSHelper(testRunner, dp);
+
+  const documentNodeId = await cssHelper.requestDocumentNodeId();
+
+  const selectors = [
+    '.base',
+    '.base button',
+    '.base option',
+    '.base optgroup',
+    '.base legend',
+    '.auto',
+    '.auto button',
+    '.auto option',
+    '.auto optgroup',
+    '.auto legend',
+  ];
+
+  for (const selector of selectors) {
+    const nodeId = await cssHelper.requestNodeId(documentNodeId, selector);
+    const response = await dp.CSS.getComputedStyleForNode({nodeId});
+    testRunner.log(`"${selector}" isAppearanceBase: ${response.result.extraFields.isAppearanceBase}`);
+  }
+
+  testRunner.completeTest();
+});
