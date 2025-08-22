5d63f481c76969513cc89eacc72e8198146748a4
Jon Toohill <jtoohill@google.com>
2025-08-22 14:49:46 -0700

    Implement quiche::BlindSignTracingHooks
    The hooks cause trace events to be emitted to a specific
    perfetto::Track so that they can be viewed as a sequence, regardless of
    which thread they run on. (Currently all BlindSignAuth operations
    happen in a single thread, but this may not be the case for long.)
    
    Also adds a new `ip_protection` Perfetto category.
    
    These hooks will be wired up in a subsequent CL.
    
    Bug: b/438756313
    Change-Id: I0525fa155e02b52c6f23c8c183629f376794b5cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6862041
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Victor Vasiliev <vasilvv@chromium.org>
    Commit-Queue: Jon Toohill <jtoohill@google.com>
    Reviewed-by: Andrew Williams <awillia@chromium.org>
    Auto-Submit: Jon Toohill <jtoohill@google.com>
    Cr-Commit-Position: refs/heads/main@{#1505351}

---
+++ components/ip_protection/common/ip_protection_performance_hooks.cc
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/ip_protection/common/ip_protection_performance_hooks.h"
+
+#include "base/trace_event/trace_event.h"
+
+namespace ip_protection {
+
+void IpProtectionPerformanceHooks::OnGetInitialDataStart() {
+  TRACE_EVENT_BEGIN("ip_protection", "GetInitialData", track_);
+}
+
+void IpProtectionPerformanceHooks::OnGetInitialDataEnd() {
+  TRACE_EVENT_END("ip_protection", track_);
+}
+
+void IpProtectionPerformanceHooks::OnGenerateBlindedTokenRequestsStart() {
+  TRACE_EVENT_BEGIN("ip_protection", "GenerateBlindedTokenRequests", track_);
+}
+
+void IpProtectionPerformanceHooks::OnGenerateBlindedTokenRequestsEnd() {
+  TRACE_EVENT_END("ip_protection", track_);
+}
+
+void IpProtectionPerformanceHooks::OnAuthAndSignStart() {
+  TRACE_EVENT_BEGIN("ip_protection", "AuthAndSign", track_);
+}
+
+void IpProtectionPerformanceHooks::OnAuthAndSignEnd() {
+  TRACE_EVENT_END("ip_protection", track_);
+}
+
+void IpProtectionPerformanceHooks::OnUnblindTokensStart() {
+  TRACE_EVENT_BEGIN("ip_protection", "UnblindTokens", track_);
+}
+
+void IpProtectionPerformanceHooks::OnUnblindTokensEnd() {
+  TRACE_EVENT_END("ip_protection", track_);
+}
+
+}  // namespace ip_protection
