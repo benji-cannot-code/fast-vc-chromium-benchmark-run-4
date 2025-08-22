4cd98ca8a8ea27220a938d22068dd2e429fe0fb9
Kevin Graney <kmg@google.com>
2025-08-22 06:23:43 -0700

    Remove OnPreFirstAct handle
    This is now covered by ActorTask state change callbacks.
    
    Bug: 425784083
    Change-Id: I0a0d72f9b5c4e5d9e20f9bfbe55817e9be3e8dc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6761413
    Commit-Queue: Kevin Graney <kmg@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1505009}

--- chrome/browser/actor/ui/event_dispatcher.cc
+++ chrome/browser/actor/ui/event_dispatcher.cc
@@ -88,12 +88,6 @@ constexpr absl::Overload PostToolEventsFn{
     NoUiEvents<AttemptLoginToolRequest>,   NoUiEvents<ScriptToolRequest>,
     NoUiEvents<ScrollToToolRequest>};
 
-// TODO(crbug.com/425784083): Remove FirstActEventsFn once functionality moves
-// to ActorTaskChangeFn.
-constexpr absl::Overload FirstActEventsFn{
-    NoUiEvents<UiEventDispatcher::FirstActInfo>,
-};
-
 constexpr absl::Overload ActorTaskAsyncChangeFn{
     [](const UiEventDispatcher::AddTab& c) {
       return EventSequence<AsyncUiEvent>{
@@ -130,11 +124,6 @@ struct VisitorTraits<PostToolEventsFn> {
 };
 
 template <>
-struct VisitorTraits<FirstActEventsFn> {
-  static constexpr const char* phase_name = "FirstAct";
-};
-
-template <>
 struct VisitorTraits<ActorTaskAsyncChangeFn> {
   static constexpr const char* phase_name = "ActorTaskAsyncChange";
 };
@@ -159,18 +148,6 @@ struct InputTraits<ToolRequest> {
   static constexpr auto debug_info = [](const ToolRequest& tr) {
     return tr.JournalEvent();
   };
-};
-
-template <>
-struct InputTraits<UiEventDispatcher::FirstActInfo> {
-  static constexpr const char* name = "FirstActInfo";
-  static constexpr auto convert_fn = std::identity();
-  static constexpr auto debug_info =
-      [](const UiEventDispatcher::FirstActInfo& info) {
-        return absl::StrFormat("task_id=%d tab? %s",
-                               info.task_id.GetUnsafeValue(),
-                               info.tab_handle.has_value() ? "yes" : "no");
-      };
 };
 
 template <>
@@ -223,11 +200,6 @@ class UiEventDispatcherImpl : public UiEventDispatcher {
 
   void OnPostTool(const ToolRequest& tr, UiCompleteCallback callback) override {
     On<PostToolEventsFn>(tr, std::move(callback));
-  }
-
-  void OnPreFirstAct(const FirstActInfo& first_act_info,
-                     UiCompleteCallback callback) override {
-    On<FirstActEventsFn>(first_act_info, std::move(callback));
   }
 
   void OnActorTaskAsyncChange(const ActorTaskAsyncChange& change,
