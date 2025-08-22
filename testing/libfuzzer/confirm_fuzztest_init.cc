4a6c1e2e16045056e054e9f70a2338d095c8dda2
Titouan Rigoudy <titouan@chromium.org>
2025-08-22 02:22:00 -0700

    [Fuzzing] Adjust test expectations when seed corpuses are not built.
    The `archive_seed_corpus` GN argument controls whether seed corpuses
    specified in fuzzer GN target definitions are packaged into zips as
    data dependencies of their fuzzer executables. When the argument is
    false, the tests fail because they cannot find the corpus zips.
    
    Along the way:
    
    * Consolidate the buildflag header with that used for fuzztests
    * Avoid crashing with out-of-bounds accesses on test failures
    
    Bug: 435582616
    Fixed: 435582616
    Change-Id: I49aa638257b4f9edb342d07292a4286a041c0f3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6870751
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Auto-Submit: Titouan Rigoudy <titouan@chromium.org>
    Commit-Queue: Titouan Rigoudy <titouan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1504919}

--- testing/libfuzzer/confirm_fuzztest_init.cc
+++ testing/libfuzzer/confirm_fuzztest_init.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "testing/libfuzzer/confirm_fuzztest_init_buildflags.h"
+#include "testing/libfuzzer/buildflags.h"
 #include "testing/libfuzzer/fuzztest_init_helper.h"
 #include "third_party/fuzztest/src/fuzztest/init_fuzztest.h"
 
