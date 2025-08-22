a9affe720119a3e01051e8fe69c7094a58ad5286
Hitarth Kothari <hitarthkothari@google.com>
2025-08-22 07:34:24 -0700

    [Tab Highlighting] Record the number of tabs selected in multi-selection.
    Adds a histogram to track how many tabs are selected at once when using
    multi-selection.
    
    Bug: 432760135
    Change-Id: Ia77c2d0dd8ce3c535954830f98ce2523e718a878
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6866367
    Auto-Submit: Hitarth Kothari <hitarthkothari@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Hitarth Kothari <hitarthkothari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1505050}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelper.java
@@ -3019,6 +3019,7 @@ public class StripLayoutHelper
     }
 
     private List<Integer> getMultiSelectedTabIds() {
+        StripLayoutUtils.recordTabMultiSelectionTabCount(mModel);
         List<Integer> multiSelectedTabs = new ArrayList<>();
         if (mModel == null) return multiSelectedTabs;
         for (StripLayoutTab stripTab : mStripTabs) {
