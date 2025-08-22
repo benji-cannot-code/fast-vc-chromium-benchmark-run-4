794cde31b4ba13cd43a2228849080cac23db7522
Shunya Shishido <sisidovski@chromium.org>
2025-08-22 00:00:46 -0700

    Revert "NPT Promos: Finchify shown promo counts"
    This reverts commit bcc483421f8c443459ad38db8974a2623db01569.
    
    Reason for revert: Multiple tests failures in interactive_ui_tests on linux64-rel-ready.
    
    Bug: 440478789
    
    Original change's description:
    > NPT Promos: Finchify shown promo counts
    >
    > This ties the number of promos shown (eligible and completed) to
    > Finch. Note that the two-promo arm of this experiment is done by
    > choosing single-promo mode and setting the promo count to two.
    >
    > Change-Id: I6090579d5cef3f3119d8ac374e7f80580eae0c84
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6871055
    > Reviewed-by: Dana Fried <dfried@chromium.org>
    > Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1504849}
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I7c026a9e8811e3559bda3c9d64ba989005b3fb33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6874202
    Commit-Queue: Shunya Shishido <sisidovski@chromium.org>
    Owners-Override: Shunya Shishido <sisidovski@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1504880}

--- chrome/browser/resources/new_tab_page/ntp_promo/setup_list.ts
+++ chrome/browser/resources/new_tab_page/ntp_promo/setup_list.ts
@@ -20,6 +20,8 @@ import {NtpPromoProxyImpl} from './ntp_promo_proxy.js';
 import {getCss} from './setup_list.css.js';
 import {getHtml} from './setup_list.html.js';
 
+export const MAX_SETUP_LIST_ENTRIES = 5;
+
 export interface SetupListElement {
   $: {
     moduleHeaderElementV2: ModuleHeaderElement,
@@ -43,9 +45,6 @@ export class SetupListElement extends I18nMixinLit
 
   static override get properties() {
     return {
-      maxPromos: {type: Number, attribute: true, useDefault: true},
-      maxCompletedPromos: {type: Number, attribute: true, useDefault: true},
-
       completedPromos_: {type: Array},
 
       eligiblePromos_: {type: Array},
@@ -55,9 +54,6 @@ export class SetupListElement extends I18nMixinLit
       allowFaviconServerFallback_: {type: Boolean},
     };
   }
-
-  accessor maxPromos: number = 0;
-  accessor maxCompletedPromos: number = 0;
 
   protected accessor completedPromos_: Promo[] = [];
   protected accessor eligiblePromos_: Promo[] = [];
@@ -91,9 +87,10 @@ export class SetupListElement extends I18nMixinLit
   }
 
   onSetPromos(eligible: Promo[], completed: Promo[]) {
-    this.completedPromos_ = completed.slice(0, this.maxCompletedPromos);
-    const maxEligible = this.maxPromos - this.completedPromos_.length;
-    this.eligiblePromos_ = eligible.slice(0, maxEligible);
+    this.completedPromos_ = completed.slice(0, 2);
+    this.eligiblePromos_ = eligible.slice(
+        0, MAX_SETUP_LIST_ENTRIES - this.completedPromos_.length);
+
     const hasPromos =
         this.completedPromos_.length !== 0 || this.eligiblePromos_.length !== 0;
     const readyEvent = new CustomEvent('module-ready', {
