f932ed538fbaf26ea4d3628b6ad38456ae887d9a
Jan Keitel <jkeitel@google.com>
2025-08-22 05:37:18 -0700

    Encode geo-ip exclusions for entities in entity schema
    We are still pending final confirmation that the geo ip will be the
    only decision criterion for disabling national id card import.
    However, it seems clear that it will be *one* of the exclusion
    criteria. There this CL adds geo_ip as an additional parameter to
    the "enabled" method of EntityType.
    
    Bug: 437327293
    Change-Id: I242d3ca0c651e3862adc708e466088d7e45efb44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6874563
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1504986}

--- components/autofill/core/browser/data_model/autofill_ai/entity_type_unittest.cc
+++ components/autofill/core/browser/data_model/autofill_ai/entity_type_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/data_model/autofill_ai/entity_type.h"
 
 #include "base/test/scoped_feature_list.h"
+#include "components/autofill/core/browser/country_type.h"
 #include "components/autofill/core/browser/data_model/autofill_ai/entity_type_names.h"
 #include "components/autofill/core/common/autofill_features.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -101,6 +102,20 @@ TEST(AutofillEntityTypeTest, Disabled) {
   EXPECT_TRUE(EntityType(kPassport).enabled());
   EXPECT_TRUE(EntityType(kDriversLicense).enabled());
   EXPECT_TRUE(EntityType(kVehicle).enabled());
+}
+
+// Tests that specifying an "excluded geo-ip" disabled the entity in countries
+// with that geo ip.
+TEST(AutofillEntityTypeTest, EnabledWithCountryCode) {
+  EntityType e = EntityType(EntityTypeName::kNationalIdCard);
+  EXPECT_FALSE(e.enabled());
+  EXPECT_FALSE(e.enabled(GeoIpCountryCode("US")));
+
+  base::test::ScopedFeatureList feature_list{
+      features::kAutofillAiNationalIdCard};
+  EXPECT_TRUE(e.enabled(GeoIpCountryCode("US")));
+  EXPECT_TRUE(e.enabled(GeoIpCountryCode("DE")));
+  EXPECT_FALSE(e.enabled(GeoIpCountryCode("IN")));
 }
 
 TEST(AutofillEntityTypeTest, EntityGetNameForI18n) {
