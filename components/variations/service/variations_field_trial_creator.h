d15c028a418923aea54e05d8e5dce45af2a68e88
Ramon Cano Aparicio <rcanoaparicio@google.com>
2025-08-22 11:21:05 -0700

    Merge SafeSeedManagerBase into SafeSeedManager.
    Part of the changes to remove unused code from cros_evaluate_seed.
    
    Bug: 434969200
    Change-Id: I8ced160ff028c052468971caaa29c9baef5dafc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6875306
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1505213}

--- components/variations/service/variations_field_trial_creator.h
+++ components/variations/service/variations_field_trial_creator.h
@@ -103,7 +103,6 @@ enum LoadPermanentConsistencyCountryResult {
 };
 
 class PlatformFieldTrials;
-class SafeSeedManagerBase;
 class VariationsServiceClient;
 
 // Used to set up field trials based on stored variations seed data.
@@ -169,7 +168,7 @@ class VariationsFieldTrialCreator {
       std::unique_ptr<base::FeatureList> feature_list,
       metrics::MetricsStateManager* metrics_state_manager,
       PlatformFieldTrials* platform_field_trials,
-      SafeSeedManagerBase* safe_seed_manager,
+      SafeSeedManager* safe_seed_manager,
       bool add_entropy_source_to_variations_ids,
       const EntropyProviders& entropy_providers);
 
@@ -280,7 +279,7 @@ class VariationsFieldTrialCreator {
   CreateTrialsResult CreateTrialsFromSeed(
       const EntropyProviders& entropy_providers,
       base::FeatureList* feature_list,
-      SafeSeedManagerBase* safe_seed_manager,
+      SafeSeedManager* safe_seed_manager,
       std::unique_ptr<ClientFilterableState> client_state);
 
   // Reads a seed's data and signature from the file at |json_seed_path| and
