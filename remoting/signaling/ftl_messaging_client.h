7aaf47c3f2a9321fd1ab364e6502329aa8f822c0
Joe Downing <joedow@google.com>
2025-08-22 11:18:48 -0700

    Update FtlMessagingClient to use new shared classes
    Bug: 359620500
    Change-Id: Id2270e726ab616b4583d8b9caf244e1bc659598b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6870948
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1505210}

--- remoting/signaling/ftl_messaging_client.h
+++ remoting/signaling/ftl_messaging_client.h
@@ -30,8 +30,9 @@ class SharedURLLoaderFactory;
 
 namespace remoting {
 
+class FtlMessageChannelStrategy;
+class MessageChannel;
 class ProtobufHttpClient;
-class MessageReceptionChannel;
 class OAuthTokenGetter;
 class RegistrationManager;
 class ScopedProtobufHttpRequest;
@@ -70,7 +71,8 @@ class FtlMessagingClient final : public MessagingClient {
 
   FtlMessagingClient(std::unique_ptr<ProtobufHttpClient> client,
                      RegistrationManager* registration_manager,
-                     std::unique_ptr<MessageReceptionChannel> channel);
+                     SignalingTracker* signaling_tracker,
+                     std::unique_ptr<FtlMessageChannelStrategy> strategy);
 
   template <typename CallbackFunctor>
   void ExecuteRequest(const net::NetworkTrafficAnnotationTag& tag,
@@ -104,7 +106,7 @@ class FtlMessagingClient final : public MessagingClient {
 
   std::unique_ptr<ProtobufHttpClient> client_;
   raw_ptr<RegistrationManager> registration_manager_;
-  std::unique_ptr<MessageReceptionChannel> reception_channel_;
+  std::unique_ptr<MessageChannel> message_channel_;
   MessageCallbackList callback_list_;
   MessageTracker message_tracker_;
 };
