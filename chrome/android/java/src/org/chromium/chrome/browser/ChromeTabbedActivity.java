813857a5d1a05f71e886c1d5b157a5c28521b435
Zhe Li <zheliooo@google.com>
2025-08-22 12:21:12 -0700

    [PinnedTabs] Honor the user-selected drop index when reparenting pinned tabs
    Bug: 440132197
    Change-Id: Id2803b56d565df30aa965aa031d9a0a9039cc4c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6870202
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Zhe Li <zheliooo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1505259}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -1809,7 +1809,8 @@ public class ChromeTabbedActivity extends ChromeActivity {
                         IntentUtils.safeGetStringExtra(intent, Browser.EXTRA_APPLICATION_ID),
                         tabIdToBringToFront,
                         intent);
-        if (IntentHandler.getPinnedState(intent)) {
+        boolean shouldPin = IntentHandler.getPinnedState(intent);
+        if (shouldPin && !tab.getIsPinned()) {
             getTabModelSelector().getModel(tab.isIncognito()).pinTab(tab.getId());
         }
         int destTabId = IntentHandler.getDestTabId(intent);
