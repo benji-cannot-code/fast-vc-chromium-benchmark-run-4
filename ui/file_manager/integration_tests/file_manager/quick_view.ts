6bce64847f83e296f248f7f068d48522a3f5a149
Andy Phan <andyphan@chromium.org>
2025-08-22 10:11:50 -0700

    [OOPIF PDF] Remove checks dependent on PDF internals in QuickView tests
    The openQuickViewPdf.* tests are checking for a PDF iframe to create an
    inner PDF embed. This is an PDF viewer implementation detail and is
    susceptible to breaking if the PDF viewer architecture changes. This
    causes the tests to fail when OOPIF PDF is enabled, since OOPIF PDF
    hides the inner contents of the PDF iframe using a shadow DOM.
    
    The test really only needs to check that it successfully created a PDF
    iframe. Keep the checks for the PDF iframe while removing the checks for
    the inner PDF embed.
    
    Manual testing shows that the OOPIF PDF viewer still loads successfully
    in QuickView.
    
    Bug: 326487542
    Change-Id: I088289a1f12260de539c7ece494485edf7d3b1a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6871242
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1505144}

--- ui/file_manager/integration_tests/file_manager/quick_view.ts
+++ ui/file_manager/integration_tests/file_manager/quick_view.ts
@@ -963,25 +963,6 @@ export async function openQuickViewPdf() {
         'deepQueryAllElements', appId, [preview, ['display']]));
   });
 
-  // Get the preview embed type attribute.
-  function checkPdfEmbedType(type: ElementObject[]) {
-    const haveElements = Array.isArray(type) && type.length === 1;
-    if (!haveElements || !type[0]!.toString().includes('pdf')) {
-      return pending(caller, 'Waiting for plugin <embed> type.');
-    }
-    return type[0]!;
-  }
-  const type = await repeatUntil(async () => {
-    const getType =
-        contentWindowQuery + '.document.querySelector("embed").type';
-    const type = await executeJsInPreviewTagAndCatchErrors(
-                     appId, preview, getType) as ElementObject[];
-    return checkPdfEmbedType(type);
-  });
-
-  // Check: the preview embed type should be PDF mime type.
-  chrome.test.assertEq('application/pdf', type);
-
   // Check: the correct mimeType should be displayed.
   const mimeType = await getQuickViewMetadataBoxField(appId, 'Type');
   chrome.test.assertEq('application/pdf', mimeType);
@@ -1021,25 +1002,6 @@ export async function openQuickViewPdfPopup() {
     return checkPreviewPdfLoaded(await remoteCall.callRemoteTestUtil(
         'deepQueryAllElements', appId, [preview, ['display']]));
   });
-
-  // Get the preview embed type attribute.
-  function checkPdfEmbedType(type: unknown) {
-    const haveElements = Array.isArray(type) && type.length === 1;
-    if (!haveElements || !type[0].toString().includes('pdf')) {
-      return pending(caller, 'Waiting for plugin <embed> type.');
-    }
-    return type[0];
-  }
-  const type = await repeatUntil(async () => {
-    const getType =
-        contentWindowQuery + '.document.querySelector("embed").type';
-    const type =
-        await executeJsInPreviewTagAndCatchErrors(appId, preview, getType);
-    return checkPdfEmbedType(type);
-  });
-
-  // Check: the preview embed type should be PDF mime type.
-  chrome.test.assertEq('application/pdf', type);
 
   // Check: the correct mimeType should be displayed.
   const mimeType = await getQuickViewMetadataBoxField(appId, 'Type');
