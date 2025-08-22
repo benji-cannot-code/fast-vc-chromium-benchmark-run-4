287015f980655756b64cf0207ef1e809c7171093
dongguoz <dongguoz@google.com>
2025-08-22 04:04:44 -0700

    Refactor(plus_addresses): Move sync_utils to core/browser
    This CL moves the plus_addresses sync_utils folder to a core/browser
    subdirectory to better align with the layered components design.
    
    This is a pure refactoring CL and does not introduce any logic changes.
    
    The code is mainly generated by mass-rename script and build.gn is
    updated by the shell script.
    
    Bypass-Check-License: Moving files
    Bug: 365494310
    Change-Id: I071f84d076eeaf5157c5371340a62ab962334d03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6861061
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Ankush Singh <ankushkush@google.com>
    Commit-Queue: Dongguo Zhang <dongguo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1504957}

--- components/plus_addresses/core/browser/sync_utils/plus_address_data_type_controller.cc
+++ components/plus_addresses/core/browser/sync_utils/plus_address_data_type_controller.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/plus_addresses/sync_utils/plus_address_data_type_controller.h"
+#include "components/plus_addresses/core/browser/sync_utils/plus_address_data_type_controller.h"
 
 #include "base/check_deref.h"
 #include "base/functional/bind.h"
--- components/plus_addresses/core/browser/sync_utils/plus_address_data_type_controller.h
+++ components/plus_addresses/core/browser/sync_utils/plus_address_data_type_controller.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef COMPONENTS_PLUS_ADDRESSES_SYNC_UTILS_PLUS_ADDRESS_DATA_TYPE_CONTROLLER_H_
-#define COMPONENTS_PLUS_ADDRESSES_SYNC_UTILS_PLUS_ADDRESS_DATA_TYPE_CONTROLLER_H_
+#ifndef COMPONENTS_PLUS_ADDRESSES_CORE_BROWSER_SYNC_UTILS_PLUS_ADDRESS_DATA_TYPE_CONTROLLER_H_
+#define COMPONENTS_PLUS_ADDRESSES_CORE_BROWSER_SYNC_UTILS_PLUS_ADDRESS_DATA_TYPE_CONTROLLER_H_
 
 #include <memory>
 
@@ -33,7 +33,7 @@ namespace plus_addresses {
 // the user type not supported.
 // Tested by the sync integration tests.
 class PlusAddressDataTypeController : public syncer::DataTypeController,
-                                       public syncer::SyncServiceObserver {
+                                      public syncer::SyncServiceObserver {
  public:
   PlusAddressDataTypeController(
       syncer::DataType type,
@@ -68,4 +68,4 @@ class PlusAddressDataTypeController : public syncer::DataTypeController,
 
 }  // namespace plus_addresses
 
-#endif  // COMPONENTS_PLUS_ADDRESSES_SYNC_UTILS_PLUS_ADDRESS_DATA_TYPE_CONTROLLER_H_
+#endif  // COMPONENTS_PLUS_ADDRESSES_CORE_BROWSER_SYNC_UTILS_PLUS_ADDRESS_DATA_TYPE_CONTROLLER_H_
