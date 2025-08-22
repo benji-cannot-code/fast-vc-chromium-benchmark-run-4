5ab886bde86ed892a839a81f7a4030773eb825d1
Taiyo Mizuhashi <taiyo@chromium.org>
2025-08-22 05:45:48 -0700

    Prefetch, cleanup: Fix inconsistent variable name for PrefetchPotentialCandidateServingResult
    Bug: 360094997
    Change-Id: I82b6fa4bfdd8023bcb124c47c7c858c7a617069f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6875363
    Auto-Submit: Taiyo Mizuhashi <taiyo@chromium.org>
    Reviewed-by: Ken Okada <kenoss@chromium.org>
    Commit-Queue: Ken Okada <kenoss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1504989}

--- content/browser/preloading/prefetch/prefetch_match_resolver.cc
+++ content/browser/preloading/prefetch/prefetch_match_resolver.cc
@@ -235,7 +235,7 @@ void PrefetchMatchResolver::StartWaitFor(const PrefetchKey& prefetch_key,
 void PrefetchMatchResolver::UnregisterCandidate(
     const PrefetchKey& prefetch_key,
     bool is_served,
-    PrefetchPotentialCandidateServingResult matching_reuslt) {
+    PrefetchPotentialCandidateServingResult serving_result) {
   // By #prefetch-key-availability
   CHECK(candidates_.contains(prefetch_key));
   auto& candidate_data = candidates_[prefetch_key];
@@ -243,7 +243,7 @@ void PrefetchMatchResolver::UnregisterCandidate(
   PrefetchContainer& prefetch_container = *candidate_data->prefetch_container;
 
   prefetch_container.OnUnregisterCandidate(navigated_key_.url(), is_served,
-                                           matching_reuslt, is_nav_prerender_,
+                                           serving_result, is_nav_prerender_,
                                            GetBlockedDuration());
   prefetch_container.RemoveObserver(this);
   candidates_.erase(prefetch_key);
@@ -415,8 +415,8 @@ void PrefetchMatchResolver::UnblockForNoCandidates() {
 
 void PrefetchMatchResolver::MaybeUnblockForUnmatch(
     const PrefetchKey& prefetch_key,
-    PrefetchPotentialCandidateServingResult matching_result) {
-  UnregisterCandidate(prefetch_key, /*is_served=*/false, matching_result);
+    PrefetchPotentialCandidateServingResult serving_result) {
+  UnregisterCandidate(prefetch_key, /*is_served=*/false, serving_result);
 
   if (candidates_.size() == 0) {
     UnblockForNoCandidates();
