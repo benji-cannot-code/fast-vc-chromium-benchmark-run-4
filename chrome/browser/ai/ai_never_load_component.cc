c231ddbc3c6c2dff631a9638b8777c17b68fa59b
Nathan Memmott <memmott@chromium.org>
2025-08-22 09:50:47 -0700

    Writing Assistance: Don't load some percentage until completion
    Adds a component that won't load roughly 10% of the loading bar when the
    model isn't downloaded. This is to indicate that there is still work
    being done between model download and API instance creation. The
    renderer will still send 100% when the API instance is created.
    
    Enabled-by-default-reason: killswitch
    Bug: 425322243
    Change-Id: I0797a24d140c340d9ad0e2b27bf743df3e02a283
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6787415
    Reviewed-by: Brad Triebwasser <btriebw@chromium.org>
    Commit-Queue: Nathan Memmott <memmott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1505131}

---
+++ chrome/browser/ai/ai_never_load_component.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ai/ai_never_load_component.h"
+
+namespace on_device_ai {
+
+AINeverLoadComponent::AINeverLoadComponent(int64_t total_bytes) {
+  SetDownloadedBytes(0);
+  SetTotalBytes(total_bytes);
+}
+AINeverLoadComponent::~AINeverLoadComponent() = default;
+
+}  // namespace on_device_ai
