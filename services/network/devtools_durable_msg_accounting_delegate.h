a5f893b8a591b4504887e6b1d8fedf1deed67020
Alex N. Jose <alexnj@chromium.org>
2025-08-21 19:53:02 -0700

    Reland "DevTools Network Response Body Durable Storage implementation"
    This is a reland of commit 9a67277c4c1e28001785ac7117c67c8299d5eea8
    
    This CL adds an explicit destruction of the Durable Messages Collector
    map to prevent a destruction order related crash on MSAN builds.
    
    Original change's description:
    > DevTools Network Response Body Durable Storage implementation
    >
    > This CL implements a response body storage mechanism that allow
    > introspection of network response bodies after the renderer has been
    > terminated. This reuses CDP's `NetworkHandler::enable()` with a
    > new parameter and `::GetResponseBody()` methods to switch to
    > the `DevtoolsDurableMessage` storage hosted in the Network Service.
    > The storage is designed to be evicted based on the set limit, or as
    > CDP clients are disconnected.
    >
    > Follow up CLs will complete the following:
    >  * Decoding stored messages on retrieval (gzip, brotli etc).
    >  * Base64 encoding binary payloads on retrieval to match current CDP
    >    API design.
    >
    > Design doc: https://docs.google.com/document/d/1CGX7k-8KLJBPUCWGUzJSNF991JPBDYGEQF_-hYGQ2hA/edit?pli=1&tab=t.0#heading=h.7nki9mck5t64
    >
    > Bug: 414864477
    > Change-Id: Ib991fb2d2801a5ef0e17517ec0a1c719a2e1687c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6503314
    > Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    > Reviewed-by: Tom Sepez <tsepez@chromium.org>
    > Commit-Queue: Alex N. Jose <alexnj@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1503009}
    
    Bug: 414864477
    Change-Id: Ifbe215f084e049f3ea289f98aced13c357d763b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6861675
    Commit-Queue: Alex N. Jose <alexnj@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1504827}

---
+++ services/network/devtools_durable_msg_accounting_delegate.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SERVICES_NETWORK_DEVTOOLS_DURABLE_MSG_ACCOUNTING_DELEGATE_H_
+#define SERVICES_NETWORK_DEVTOOLS_DURABLE_MSG_ACCOUNTING_DELEGATE_H_
+
+#include <stdint.h>
+
+namespace network {
+class DevtoolsDurableMessage;
+
+class DevtoolsDurableMessageAccountingDelegate {
+ public:
+  virtual ~DevtoolsDurableMessageAccountingDelegate() = default;
+  virtual void WillAddBytes(DevtoolsDurableMessage& message,
+                            int64_t chunk_size) = 0;
+  virtual void WillRemoveBytes(DevtoolsDurableMessage& message) = 0;
+};
+
+}  // namespace network
+
+#endif  // SERVICES_NETWORK_DEVTOOLS_DURABLE_MSG_ACCOUNTING_DELEGATE_H_
