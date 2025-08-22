31a07b3d1e71a19505ef8d5351f79c2f5abd9e7a
Vidhan Jain <vidhanj@google.com>
2025-08-22 04:32:31 -0700

    Use BubbleManager to show offer notification bubbles
    BubbleManager uses a priority based algorithm to decide which bubble
    has precedence to show if there are multiple bubbles waiting.
    With this CL, the offer notification bubbles are made to use the bubble
    manager to show the bubble.
    
    Bug: 432429605
    Change-Id: If80136ea2f09d15e0586bf47b22644d3819b334b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6870563
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1504968}

--- chrome/browser/ui/autofill/payments/offer_notification_bubble_controller_impl.cc
+++ chrome/browser/ui/autofill/payments/offer_notification_bubble_controller_impl.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/actions/chrome_action_id.h"
 #include "chrome/browser/ui/autofill/autofill_bubble_base.h"
 #include "chrome/browser/ui/autofill/autofill_bubble_handler.h"
+#include "chrome/browser/ui/autofill/bubble_manager.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_actions.h"
 #include "chrome/browser/ui/browser_finder.h"
@@ -143,6 +144,18 @@ void OfferNotificationBubbleControllerImpl::ShowOfferNotificationIfApplicable(
     return;
   }
 
+  const bool bubble_manager_enabled = base::FeatureList::IsEnabled(
+      features::kAutofillShowBubblesBasedOnPriorities);
+
+  if (bubble_manager_enabled) {
+    auto* manager = BubbleManager::GetForWebContents(web_contents());
+    if (!manager || manager->HasPendingBubble(*this)) {
+      // Early return if a pre-existing of similar type is in the queue or the
+      // manager does not exist.
+      return;
+    }
+  }
+
   // Hides the old bubble. Sets bubble_state_ to show icon here since we are
   // going to show another bubble anyway.
   HideBubbleAndClearTimestamp(/*should_show_icon=*/true);
@@ -150,7 +163,13 @@ void OfferNotificationBubbleControllerImpl::ShowOfferNotificationIfApplicable(
   SetupOfferNotification(offer, card);
 
   if (options.show_notification_automatically) {
-    ShowBubble();
+    if (bubble_manager_enabled) {
+      if (auto* manager = BubbleManager::GetForWebContents(web_contents())) {
+        manager->RequestShowController(*this);
+      }
+    } else {
+      ShowBubble();
+    }
   } else {
     HideBubbleAndClearTimestamp(/*should_show_icon=*/true);
   }
