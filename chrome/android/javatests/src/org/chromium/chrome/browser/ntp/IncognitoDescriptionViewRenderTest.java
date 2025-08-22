4d98aea6b27eebc0d537028b6e2e0dccc0bb698d
Fiona Macintosh <fmacintosh@google.com>
2025-08-21 20:05:24 -0700

    [ACT] Updates to ACT NTP and settings strings
    Bug: b:440331109
    Change-Id: I4456aee08c403b29558dcaf298650ab85323de6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6867732
    Reviewed-by: Kevin Graney <kmg@google.com>
    Commit-Queue: Fiona Macintosh <fmacintosh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1504832}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ntp/IncognitoDescriptionViewRenderTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ntp/IncognitoDescriptionViewRenderTest.java
@@ -44,7 +44,8 @@ import java.util.List;
 @Batch(Batch.PER_CLASS)
 @EnableFeatures({
     ChromeFeatureList.ALWAYS_BLOCK_3PCS_INCOGNITO,
-    ChromeFeatureList.FINGERPRINTING_PROTECTION_UX
+    ChromeFeatureList.FINGERPRINTING_PROTECTION_UX,
+    ChromeFeatureList.IP_PROTECTION_UX
 })
 public class IncognitoDescriptionViewRenderTest {
     @ParameterAnnotations.ClassParameter
@@ -88,7 +89,8 @@ public class IncognitoDescriptionViewRenderTest {
     @Feature({"RenderTest"})
     @DisableFeatures({
         ChromeFeatureList.ALWAYS_BLOCK_3PCS_INCOGNITO,
-        ChromeFeatureList.FINGERPRINTING_PROTECTION_UX
+        ChromeFeatureList.FINGERPRINTING_PROTECTION_UX,
+        ChromeFeatureList.IP_PROTECTION_UX
     })
     public void testRender_IncognitoDescriptionView() throws IOException {
         View view = sActivity.findViewById(android.R.id.content);
@@ -108,7 +110,8 @@ public class IncognitoDescriptionViewRenderTest {
     @Feature({"RenderTest"})
     @DisableFeatures({
         ChromeFeatureList.ALWAYS_BLOCK_3PCS_INCOGNITO,
-        ChromeFeatureList.FINGERPRINTING_PROTECTION_UX
+        ChromeFeatureList.FINGERPRINTING_PROTECTION_UX,
+        ChromeFeatureList.IP_PROTECTION_UX
     })
     public void testRender_IncognitoDescriptionViewTrackingProtection() throws IOException {
         View view = sActivity.findViewById(android.R.id.content);
@@ -126,7 +129,10 @@ public class IncognitoDescriptionViewRenderTest {
     @Test
     @MediumTest
     @Feature({"RenderTest"})
-    @DisableFeatures({ChromeFeatureList.FINGERPRINTING_PROTECTION_UX})
+    @DisableFeatures({
+        ChromeFeatureList.FINGERPRINTING_PROTECTION_UX,
+        ChromeFeatureList.IP_PROTECTION_UX
+    })
     public void render_IncognitoDescriptionView_alwaysBlock3pcsIncognito() throws IOException {
         View view = sActivity.findViewById(android.R.id.content);
         ThreadUtils.runOnUiThreadBlocking(
