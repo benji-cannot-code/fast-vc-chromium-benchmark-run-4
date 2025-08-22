a5e998be45b84cfe63eab85b61ac139a3cca14bb
Alison Gale <agale@chromium.org>
2025-08-22 15:22:28 -0700

    [SxS] Ensure contents scrim is associated with specific web contents
    In split view there can be bugs when swapping between views where the
    scrim gets rendered on the wrong tab. This ensures it is always tied to
    the same web contents.
    
    Change-Id: I161c81eb67fa9a05461e5654b1ffddaf44ced638
    Bug: 439833602
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6872236
    Commit-Queue: Alison Gale <agale@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1505368}

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -1104,7 +1104,6 @@ std::vector<StatusBubble*> Browser::GetStatusBubblesForTesting() {
   return GetStatusBubbles();
 }
 
-
 views::WebView* Browser::GetWebView() {
   return window_->GetContentsWebView();
 }
@@ -1112,7 +1111,6 @@ views::WebView* Browser::GetWebView() {
 Profile* Browser::GetProfile() {
   return profile();
 }
-
 
 void Browser::OpenGURL(const GURL& gurl, WindowOpenDisposition disposition) {
   OpenURL(content::OpenURLParams(gurl, content::Referrer(), disposition,
@@ -2965,7 +2963,7 @@ void Browser::SetWebContentsBlocked(content::WebContents* web_contents,
   }
 
   if (contents_is_active) {
-    window_->SetContentScrimVisibility(/*visible=*/blocked);
+    window_->SetContentScrimVisibility(web_contents, /*visible=*/blocked);
   }
 }
 
@@ -3159,14 +3157,14 @@ void Browser::OnActiveTabChanged(WebContents* old_contents,
   BookmarkBarController::From(this)->UpdateBookmarkBarState(
       BookmarkBarController::StateChangeReason::kTabSwitch);
 
-  bool is_blocked = tab_strip_model_->IsTabBlocked(index);
-  window_->SetContentScrimVisibility(/*visible=*/is_blocked);
-
   // Let the BrowserWindow do its handling.  On e.g. views this changes the
   // focused object, which should happen before we update the toolbar below,
-  // since the omnibox expects the correct element to already be focused when it
-  // is updated.
+  // since the omnibox expects the correct element to already be focused when
+  // it is updated.
   window_->OnActiveTabChanged(old_contents, new_contents, index, reason);
+
+  bool is_blocked = tab_strip_model_->IsTabBlocked(index);
+  window_->SetContentScrimVisibility(new_contents, /*visible=*/is_blocked);
 
   browser_window_features()->exclusive_access_manager()->OnTabDetachedFromView(
       old_contents);
@@ -3783,7 +3781,6 @@ bool Browser::SupportsWindowFeatureImpl(WindowFeature feature,
                                                           check_can_support);
   }
 }
-
 
 bool Browser::IsBrowserClosing() const {
   BrowserList* browser_list = BrowserList::GetInstance();
