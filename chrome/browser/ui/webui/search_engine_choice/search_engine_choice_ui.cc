2eff279121d3a360d8b9329be1809c3c585f1774
Jérôme Lebel <jlebel@chromium.org>
2025-08-22 09:21:14 -0700

    Taiyaki: Use GetChoiceScreenDesign in desktop
    Use RegionalCapabilitiesService::GetChoiceScreenDesign() in desktop.
    
    This method was added in crrev.com/c/6875063
    
    Bug: 433501136
    Change-Id: Ibcf4de088faca6592f96434da7c1cb823cb6bccf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6874888
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1505113}

--- chrome/browser/ui/webui/search_engine_choice/search_engine_choice_ui.cc
+++ chrome/browser/ui/webui/search_engine_choice/search_engine_choice_ui.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/json_writer.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/regional_capabilities/regional_capabilities_service_factory.h"
 #include "chrome/browser/search_engine_choice/search_engine_choice_dialog_service.h"
 #include "chrome/browser/search_engine_choice/search_engine_choice_dialog_service_factory.h"
 #include "chrome/browser/search_engine_choice/search_engine_choice_service_factory.h"
@@ -20,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/search_engine_choice_resources.h"
 #include "chrome/grit/search_engine_choice_resources_map.h"
 #include "chrome/grit/signin_resources.h"
+#include "components/regional_capabilities/regional_capabilities_service.h"
 #include "components/search_engines/search_engine_choice/search_engine_choice_service.h"
 #include "components/search_engines/search_engine_choice/search_engine_choice_utils.h"
 #include "components/search_engines/search_engines_switches.h"
@@ -70,14 +72,24 @@ SearchEngineChoiceUI::SearchEngineChoiceUI(content::WebUI* web_ui)
       web_ui->GetWebContents()->GetBrowserContext(),
       chrome::kChromeUISearchEngineChoiceHost);
 
-  source->AddLocalizedString("title", IDS_SEARCH_ENGINE_CHOICE_PAGE_TITLE);
+  regional_capabilities::RegionalCapabilitiesService*
+      regional_capabilities_service = regional_capabilities::
+          RegionalCapabilitiesServiceFactory::GetForProfile(&profile_.get());
+  const std::optional<
+      regional_capabilities::RegionalCapabilitiesService::ChoiceScreenDesign>
+      choice_screen_design =
+          regional_capabilities_service->GetChoiceScreenDesign();
+  CHECK(choice_screen_design.has_value());
+  source->AddLocalizedString("title", choice_screen_design->title_string_id);
   source->AddLocalizedString("subtitle",
-                             IDS_SEARCH_ENGINE_CHOICE_PAGE_SUBTITLE);
-  source->AddLocalizedString("subtitleInfoLink",
-                             IDS_SEARCH_ENGINE_CHOICE_PAGE_SUBTITLE_INFO_LINK);
+                             choice_screen_design->subtitle_1_string_id);
+  source->AddLocalizedString(
+      "subtitleInfoLink",
+      choice_screen_design->subtitle_1_learn_more_suffix_string_id);
   source->AddLocalizedString(
       "subtitleInfoLinkA11yLabel",
-      IDS_SEARCH_ENGINE_CHOICE_PAGE_SUBTITLE_INFO_LINK_A11Y_LABEL);
+      choice_screen_design->subtitle_1_learn_more_a11y_string_id);
+  CHECK(!choice_screen_design->subtitle_2_string_id.has_value());
   source->AddLocalizedString("submitButtonText",
                              IDS_SEARCH_ENGINE_CHOICE_BUTTON_TITLE);
   source->AddLocalizedString("infoDialogTitle",
