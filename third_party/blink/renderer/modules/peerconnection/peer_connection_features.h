0a701654879a55432155afccb60859d1dd3d3c35
Guido Urdaneta <guidou@chromium.org>
2025-08-22 07:28:01 -0700

    [EncodedTransform] Remember metadata and type after neutering frames
    This is a spec compliance change.
    There is also a minor update on a WPT which was not compliant (expected a metadata field to disappear after neutering).
    
    Bug: 354881878
    Enabled-by-default-reason: killswitch
    Change-Id: I630508ec845eaee9da4205206564ebfb1c527753
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6870309
    Reviewed-by: Tony Herre <toprice@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1505048}

--- third_party/blink/renderer/modules/peerconnection/peer_connection_features.h
+++ third_party/blink/renderer/modules/peerconnection/peer_connection_features.h
@@ -9,6 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/modules_export.h"
 namespace blink {
 MODULES_EXPORT BASE_DECLARE_FEATURE(kWebRtcEncodedTransformDirectCallback);
+MODULES_EXPORT BASE_DECLARE_FEATURE(kWebRtcEncodedTransformRememberMetadata);
+MODULES_EXPORT BASE_DECLARE_FEATURE(
+    kWebRtcEncodedTransformRememberVideoFrameType);
 MODULES_EXPORT BASE_DECLARE_FEATURE(kWebRtcEncryptedRtpHeaderExtensions);
 MODULES_EXPORT BASE_DECLARE_FEATURE(
     kWebRtcRtpScriptTransformerFrameRestrictions);
