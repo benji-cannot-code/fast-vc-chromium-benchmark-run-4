fb059b5fd55c7506f0d973597d7e4715c741a92c
Vidhan Jain <vidhanj@google.com>
2025-08-22 03:24:29 -0700

    Use BubbleManager to show autofill ai bubbles
    BubbleManager uses a priority based algorithm to decide which bubble
    has precedence to show if there are multiple bubbles waiting.
    With this CL, the autofill ai bubbles are made to use the bubble manager
    to show the bubble.
    
    Bug: 432429605
    Change-Id: I9b5f112127c1a6f2be0233a461fabefa37cb98ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6871093
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1504944}

--- chrome/browser/ui/autofill/autofill_ai/save_or_update_autofill_ai_data_controller_impl_browsertest.cc
+++ chrome/browser/ui/autofill/autofill_ai/save_or_update_autofill_ai_data_controller_impl_browsertest.cc
@@ -10,11 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_helpers.h"
 #include "base/notreached.h"
 #include "base/test/metrics/histogram_tester.h"
+#include "base/test/with_feature_override.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/test/test_browser_dialog.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/autofill/core/browser/data_model/autofill_ai/entity_instance.h"
 #include "components/autofill/core/browser/test_utils/autofill_test_utils.h"
+#include "components/autofill/core/common/autofill_features.h"
 #include "content/public/test/browser_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -38,9 +40,14 @@ std::pair<EntityInstance, EntityInstance> GetUpdateEntities() {
   return std::make_pair(new_entity, old_entity);
 }
 }  // namespace
-class SaveOrUpdateAutofillAiDataControllerImplTest : public DialogBrowserTest {
+class SaveOrUpdateAutofillAiDataControllerImplTest
+    : public DialogBrowserTest,
+      public base::test::WithFeatureOverride {
  public:
-  SaveOrUpdateAutofillAiDataControllerImplTest() = default;
+  SaveOrUpdateAutofillAiDataControllerImplTest()
+      : base::test::WithFeatureOverride(
+            features::kAutofillShowBubblesBasedOnPriorities) {}
+
   SaveOrUpdateAutofillAiDataControllerImplTest(
       const SaveOrUpdateAutofillAiDataControllerImplTest&) = delete;
   SaveOrUpdateAutofillAiDataControllerImplTest& operator=(
@@ -75,13 +82,16 @@ class SaveOrUpdateAutofillAiDataControllerImplTest : public DialogBrowserTest {
     DialogBrowserTest::TearDownOnMainThread();
   }
 
+  bool IsBubbleManagerEnabled() const { return GetParam(); }
+
   SaveOrUpdateAutofillAiDataControllerImpl* controller() { return controller_; }
 
  private:
+  base::test::ScopedFeatureList scoped_features_;
   raw_ptr<SaveOrUpdateAutofillAiDataControllerImpl> controller_ = nullptr;
 };
 
-IN_PROC_BROWSER_TEST_F(SaveOrUpdateAutofillAiDataControllerImplTest,
+IN_PROC_BROWSER_TEST_P(SaveOrUpdateAutofillAiDataControllerImplTest,
                        UpdatedAttributesDetails_UpdateEntity) {
   ShowUi("UpdateEntity");
   std::vector<
@@ -112,7 +122,7 @@ IN_PROC_BROWSER_TEST_F(SaveOrUpdateAutofillAiDataControllerImplTest,
       1);
 }
 
-IN_PROC_BROWSER_TEST_F(SaveOrUpdateAutofillAiDataControllerImplTest,
+IN_PROC_BROWSER_TEST_P(SaveOrUpdateAutofillAiDataControllerImplTest,
                        UpdatedAttributesDetails_SaveNewEntity) {
   ShowUi("SaveNewEntity");
   std::vector<
@@ -139,4 +149,8 @@ IN_PROC_BROWSER_TEST_F(SaveOrUpdateAutofillAiDataControllerImplTest,
           kAccepted,
       1);
 }
+
+INSTANTIATE_FEATURE_OVERRIDE_TEST_SUITE(
+    SaveOrUpdateAutofillAiDataControllerImplTest);
+
 }  // namespace autofill
