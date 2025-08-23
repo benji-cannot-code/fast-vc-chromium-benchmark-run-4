94e87406af974645bd49ce4dd198ba64f3071355
David Sanders <dsanders11@ucsbalum.com>
2025-08-23 01:04:14 -0700

    Forward declare gfx::RectF in element.h
    Should save ~22 MiB of compiler input size.
    
    Change-Id: Id1dac059ebb71c414e7df39f1b2c9c31eb8eb2ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6868518
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1505463}

--- third_party/blink/renderer/core/dom/element.h
+++ third_party/blink/renderer/core/dom/element.h
@@ -65,10 +65,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
 #include "third_party/blink/renderer/platform/wtf/text/atomic_string_table.h"
 #include "third_party/blink/renderer/platform/wtf/wtf_size_t.h"
-#include "ui/gfx/geometry/rect_f.h"
 
 namespace gfx {
 class QuadF;
+class RectF;
 class Vector2dF;
 }  // namespace gfx
 
