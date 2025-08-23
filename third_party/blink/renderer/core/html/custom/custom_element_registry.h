b83df1cdd2a1425d52de0739b67e70de9e71cfed
Jayson Chen <jaysonchen@microsoft.com>
2025-08-22 20:55:13 -0700

    [scoped-registry] Implement Initialize logic on scoped registry
    Initialize function allows a node and its descendants with null scoped
    registry be initialized with the given registry and used in future
    upgrade/hydration.
    
    Bug: 40826514
    Change-Id: I4a5fc53a45310958919f10d9bb8d561a0df95966
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6832952
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Jayson Chen <jaysonchen@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1505439}

--- third_party/blink/renderer/core/html/custom/custom_element_registry.h
+++ third_party/blink/renderer/core/html/custom/custom_element_registry.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_custom_element_constructor.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_custom_element_constructor_hash.h"
-#include "third_party/blink/renderer/bindings/core/v8/v8_union_element_shadowroot.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/dom/element_rare_data_field.h"
 #include "third_party/blink/renderer/core/html/custom/custom_element_definition.h"
@@ -83,7 +82,7 @@ class CORE_EXPORT CustomElementRegistry final : public ScriptWrappable,
 
   void AssociatedWith(Document& document);
 
-  void initialize(V8UnionElementOrShadowRoot* element_or_shadowroot) {}
+  void initialize(Node* root, ExceptionState&);
 
   void Trace(Visitor*) const override;
 
