6951cdafc007841b292e7d045feb788c7e56667c
Jason Hu <hujasonx@google.com>
2025-08-22 17:48:47 -0700

    [lensoverlay] Add IsAimM3Enabled check using aim_eligibility_service
    Adds a check in Lens util file to check the AIM eligibility status from
    the service. Updates the checks in lens_composebox_controller and
    untrusted_ui to use this new check.
    
    Bug: 439088299, 437157200
    Change-Id: I6a2e9b4e8ac80971ced21e4cc6a1e61a8e140073
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6863196
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1505415}

--- chrome/browser/ui/lens/lens_composebox_controller.cc
+++ chrome/browser/ui/lens/lens_composebox_controller.cc
@@ -5,11 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/lens/lens_composebox_controller.h"
 
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/lens/lens_composebox_handler.h"
 #include "chrome/browser/ui/lens/lens_overlay_query_controller.h"
 #include "chrome/browser/ui/lens/lens_overlay_side_panel_coordinator.h"
 #include "chrome/browser/ui/lens/lens_search_contextualization_controller.h"
 #include "chrome/browser/ui/lens/lens_search_controller.h"
+#include "chrome/browser/ui/lens/lens_search_feature_flag_utils.h"
 #include "components/lens/lens_features.h"
 #include "components/lens/lens_overlay_mime_type.h"
 #include "third_party/lens_server_proto/aim_communication.pb.h"
@@ -31,8 +33,9 @@ lens::LensOverlayVisualInputType LensMimeTypeToVisualInputType(
 namespace lens {
 
 LensComposeboxController::LensComposeboxController(
-    LensSearchController* lens_search_controller)
-    : lens_search_controller_(lens_search_controller) {}
+    LensSearchController* lens_search_controller,
+    Profile* profile)
+    : lens_search_controller_(lens_search_controller), profile_(profile) {}
 
 LensComposeboxController::~LensComposeboxController() = default;
 
@@ -49,7 +52,7 @@ void LensComposeboxController::BindComposebox(
 
 void LensComposeboxController::IssueComposeboxQuery(
     const std::string& query_text) {
-  if (!lens::features::GetAimSearchboxEnabled()) {
+  if (!lens::IsAimM3Enabled(profile_)) {
     return;
   }
   // Can only issue a query if the remote UI supports the DEFAULT feature.
@@ -98,7 +101,7 @@ void LensComposeboxController::CloseUI() {
 void LensComposeboxController::OnAimMessage(
     const std::vector<uint8_t>& message) {
   // Ignore the message if the searchbox is disabled.
-  if (!lens::features::GetAimSearchboxEnabled()) {
+  if (!lens::IsAimM3Enabled(profile_)) {
     return;
   }
   // Try and parse the message as an AimToClientMessage. Since it is the only
--- chrome/browser/ui/lens/lens_composebox_controller.h
+++ chrome/browser/ui/lens/lens_composebox_controller.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 
 #include "base/memory/raw_ptr.h"
+#include "chrome/browser/profiles/profile.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
@@ -30,7 +31,8 @@ class LensComposeboxHandler;
 class LensComposeboxController {
  public:
   explicit LensComposeboxController(
-      LensSearchController* lens_search_controller);
+      LensSearchController* lens_search_controller,
+      Profile* profile);
   ~LensComposeboxController();
 
   // This method is used to set up communication between this instance and the
@@ -68,6 +70,9 @@ class LensComposeboxController {
 
   // Owns this.
   const raw_ptr<LensSearchController> lens_search_controller_;
+
+  // Guarantee to outlive this.
+  const raw_ptr<Profile> profile_;
 
   // The remote UI's capabilities.
   std::set<lens::FeatureCapability> remote_ui_capabilities_;
