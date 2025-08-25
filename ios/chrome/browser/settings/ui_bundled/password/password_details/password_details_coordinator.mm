f8b33c465869547f608809b2cab334e0957b23e5
Sylvain Defresne <sdefresne@chromium.org>
2025-08-25 04:01:44 -0700

    [ios] Only update the password managers of regular and otr Browsers
    The inactive Browser never realize its active WebState, so it is
    possible for the tab helpers to not exists. Additionally, the tab
    are never presented directly, they first needs to be moved to the
    regular Browser, so ignore the inactive Browser when updating the
    password managers.
    
    Bug: 432020830
    Change-Id: I6d35f3e6a533f4af012b93e9ae61244b7127b7ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6849449
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1505785}

--- ios/chrome/browser/settings/ui_bundled/password/password_details/password_details_coordinator.mm
+++ ios/chrome/browser/settings/ui_bundled/password/password_details/password_details_coordinator.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <vector>
 
 #import "base/apple/foundation_util.h"
+#import "base/check.h"
 #import "base/memory/scoped_refptr.h"
 #import "base/strings/sys_string_conversions.h"
 #import "components/password_manager/core/browser/password_manager_client.h"
@@ -433,8 +434,8 @@ const CGFloat kShareSpinnerMinTimeInSeconds = 0.5;
 - (void)updateFormManagers {
   BrowserList* browserList = BrowserListFactory::GetForProfile(self.profile);
 
-  for (Browser* browser :
-       browserList->BrowsersOfType(BrowserList::BrowserType::kAll)) {
+  for (Browser* browser : browserList->BrowsersOfType(
+           BrowserList::BrowserType::kRegularAndIncognito)) {
     [self updateFormManagersForBrowser:browser];
   }
 }
@@ -600,6 +601,7 @@ const CGFloat kShareSpinnerMinTimeInSeconds = 0.5;
   if (!webState) {
     return;
   }
+  CHECK(webState->IsRealized(), base::NotFatalUntil::M150);
   password_manager::PasswordManagerClient* passwordManagerClient =
       PasswordTabHelper::FromWebState(webState)->GetPasswordManagerClient();
   passwordManagerClient->UpdateFormManagers();
