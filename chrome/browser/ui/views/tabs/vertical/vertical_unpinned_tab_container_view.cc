fa94814d5f14173bdea19f9aa158337d015befb0
Caroline Rising <corising@chromium.org>
2025-08-25 10:35:58 -0700

    [Vertical Tabs] Move unpinned tabs container to c/b/ui/tabs/vertical
    Bug: 438494724
    Change-Id: Ic60894a76d11d67e4448c204a77b9ef2140b06cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6879087
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1505955}

--- chrome/browser/ui/views/tabs/vertical/vertical_unpinned_tab_container_view.cc
+++ chrome/browser/ui/views/tabs/vertical/vertical_unpinned_tab_container_view.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/views/frame/vertical_unpinned_tab_container_view.h"
+#include "chrome/browser/ui/views/tabs/vertical/vertical_unpinned_tab_container_view.h"
 
 #include "chrome/browser/ui/layout_constants.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
