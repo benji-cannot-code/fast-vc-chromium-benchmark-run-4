68162c3e0ea8cc327ca28b7072fbfe44cc78f493
Glenn Hartmann <hartmanng@chromium.org>
2025-08-25 09:37:39 -0700

    [bedrock] Migrate relaunch_notification_controller_platform_impl_desktop.cc to ForEachCurrentAndNewBrowserWindowInterfaceOrderedByActivation()
    This migrates from BrowserList::OrderedByActivation() to
    ForEachCurrentAndNewBrowserWindowInterfaceOrderedByActivation() as part of project
    bedrock (https://crbug.com/421758609 ).
    
    Bug: 438458102
    Change-Id: Ia3f09c75fe2db71b4177d9f05fa01bad2cc365b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6785750
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Glenn Hartmann <hartmanng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1505918}

--- chrome/browser/ui/views/relaunch_notification/relaunch_recommended_bubble_view.cc
+++ chrome/browser/ui/views/relaunch_notification/relaunch_recommended_bubble_view.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "build/build_config.h"
 #include "chrome/browser/ui/browser_list.h"
+#include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/toolbar/browser_app_menu_button.h"
@@ -47,13 +48,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // static
 views::Widget* RelaunchRecommendedBubbleView::ShowBubble(
-    Browser* browser,
+    BrowserWindowInterface* browser,
     base::Time detection_time,
     base::RepeatingClosure on_accept) {
   DCHECK(browser);
 
   // Anchor the popup to the browser's app menu.
-  auto* anchor_button = BrowserView::GetBrowserViewForBrowser(browser)
+  auto* anchor_button = BrowserView::GetBrowserViewForBrowser(
+                            browser->GetBrowserForMigrationOnly())
                             ->toolbar_button_provider()
                             ->GetAppMenuButton();
   auto* bubble_view = new RelaunchRecommendedBubbleView(
--- chrome/browser/ui/views/relaunch_notification/relaunch_recommended_bubble_view_browsertest.cc
+++ chrome/browser/ui/views/relaunch_notification/relaunch_recommended_bubble_view_browsertest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback_helpers.h"
 #include "base/time/time.h"
+#include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/test/test_browser_dialog.h"
 #include "content/public/test/browser_test.h"
 
