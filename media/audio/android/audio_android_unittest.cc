031412bb057a4a10cb1e890f0d7835649c772ebe
Shin Kawamura <kawasin@google.com>
2025-08-24 21:31:51 -0700

    Rename audio buffer size methods to use "FramesPerBuffer".
    This change renames methods such as `GetMinInputFrameSize` and
    `GetAudioLowLatencyOutputFrameSize` to use "FramesPerBuffer" instead of
    "FrameSize" to more accurately describe that they return the number of
    audio frames per buffer. Corresponding variable names and Java methods
    are also updated for consistency.
    
    Bug: 436476367
    Change-Id: I35f16330993976ea9d4d1c13762f548210a7af92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6873027
    Reviewed-by: Maya Jelonkiewicz <mjel@google.com>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Shin Kawamura <kawasin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1505693}

--- media/audio/android/audio_android_unittest.cc
+++ media/audio/android/audio_android_unittest.cc
@@ -182,7 +182,7 @@ class MockJniDelegate : public JniDelegate {
               (),
               (override));
   MOCK_METHOD(int,
-              GetMinInputFrameSize,
+              GetMinInputFramesPerBuffer,
               (int sample_rate, int channels),
               (override));
   MOCK_METHOD(bool, AcousticEchoCancelerIsAvailable, (), (override));
@@ -195,9 +195,9 @@ class MockJniDelegate : public JniDelegate {
   MOCK_METHOD(void, MaybeSetBluetoothScoState, (bool state), (override));
   MOCK_METHOD(int, GetNativeOutputSampleRate, (), (override));
   MOCK_METHOD(bool, IsAudioLowLatencySupported, (), (override));
-  MOCK_METHOD(int, GetAudioLowLatencyOutputFrameSize, (), (override));
+  MOCK_METHOD(int, GetAudioLowLatencyOutputFramesPerBuffer, (), (override));
   MOCK_METHOD(int,
-              GetMinOutputFrameSize,
+              GetMinOutputFramesPerBuffer,
               (int sample_rate, int channels),
               (override));
   MOCK_METHOD(AudioParameters::Format,
@@ -928,7 +928,7 @@ TEST_F(AudioAndroidInputTest, GetInputStreamParametersForDevice) {
       .WillRepeatedly(Return(true));
   EXPECT_CALL(jni_delegate, GetNativeOutputSampleRate())
       .WillRepeatedly(Return(90000));
-  EXPECT_CALL(jni_delegate, GetMinInputFrameSize(_, _))
+  EXPECT_CALL(jni_delegate, GetMinInputFramesPerBuffer(_, _))
       .WillRepeatedly(Return(64));
 
   // Ensure device metadata is fetched and cached.
@@ -981,7 +981,7 @@ TEST_F(AudioAndroidOutputTest, GetOutputStreamParametersForDevice) {
       .WillRepeatedly(Return(true));
   EXPECT_CALL(jni_delegate, GetNativeOutputSampleRate())
       .WillRepeatedly(Return(90000));
-  EXPECT_CALL(jni_delegate, GetAudioLowLatencyOutputFrameSize())
+  EXPECT_CALL(jni_delegate, GetAudioLowLatencyOutputFramesPerBuffer())
       .WillRepeatedly(Return(64));
   EXPECT_CALL(jni_delegate, GetHdmiOutputEncodingFormats())
       .WillRepeatedly(Return(static_cast<AudioParameters::Format>(0)));
@@ -1036,7 +1036,7 @@ TEST_F(AudioAndroidOutputTest,
                                        /*sample_rates=*/{}}}));
   EXPECT_CALL(jni_delegate, IsAudioLowLatencySupported())
       .WillRepeatedly(Return(true));
-  EXPECT_CALL(jni_delegate, GetAudioLowLatencyOutputFrameSize())
+  EXPECT_CALL(jni_delegate, GetAudioLowLatencyOutputFramesPerBuffer())
       .WillRepeatedly(Return(64));
   EXPECT_CALL(jni_delegate, GetHdmiOutputEncodingFormats())
       .WillRepeatedly(Return(static_cast<AudioParameters::Format>(0)));
--- media/base/android/java/src/org/chromium/media/AudioManagerAndroid.java
+++ media/base/android/java/src/org/chromium/media/AudioManagerAndroid.java
@@ -407,7 +407,7 @@ class AudioManagerAndroid {
      * @param channels number of channels
      */
     @CalledByNative
-    private static int getMinInputFrameSize(int sampleRate, int channels) {
+    private static int getMinInputFramesPerBuffer(int sampleRate, int channels) {
         int channelConfig;
         if (channels == 1) {
             channelConfig = AudioFormat.CHANNEL_IN_MONO;
@@ -429,7 +429,7 @@ class AudioManagerAndroid {
      * @param channels number of channels
      */
     @CalledByNative
-    private static int getMinOutputFrameSize(int sampleRate, int channels) {
+    private static int getMinOutputFramesPerBuffer(int sampleRate, int channels) {
         int channelConfig;
         if (channels == 1) {
             channelConfig = AudioFormat.CHANNEL_OUT_MONO;
@@ -452,7 +452,7 @@ class AudioManagerAndroid {
     }
 
     @CalledByNative
-    private int getAudioLowLatencyOutputFrameSize() {
+    private int getAudioLowLatencyOutputFramesPerBuffer() {
         String framesPerBuffer =
                 mAudioManager.getProperty(AudioManager.PROPERTY_OUTPUT_FRAMES_PER_BUFFER);
         return framesPerBuffer == null ? 0 : Integer.parseInt(framesPerBuffer);
