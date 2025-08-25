617e1aa43446eafada4ce863e7cf122694ad3d5a
Matt Mueller <mattm@chromium.org>
2025-08-25 10:00:55 -0700

    Refactor X509Certificate to store cert buffers in single vector
    Changes the internal storage from a separate cert_buffer_ and
    intermediate_buffers_ vector, to a single cert_buffers_ vector, and adds
    accessors that return the full vector. This simplifies some code that
    previously needed to split some operations into two steps of operating
    on cert_buffer() and then looping on intermediate_buffers().
    
    Also adds a CopyCertBuffers() helper for cases where you need to pass
    the list of all the buffers into some API that takes a reference.
    
    Bug: 433569398
    Change-Id: Ie89e1e712a247e7b9f096a5a7152d2cfbf81c465
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6862025
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1505937}

--- net/socket/ssl_server_socket_impl.cc
+++ net/socket/ssl_server_socket_impl.cc
@@ -41,21 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace net {
 
-namespace {
-
-std::vector<bssl::UniquePtr<CRYPTO_BUFFER>> ChainFromX509Certificate(
-    X509Certificate* cert) {
-  std::vector<bssl::UniquePtr<CRYPTO_BUFFER>> cert_chain;
-  cert_chain.reserve(1 + cert->intermediate_buffers().size());
-  cert_chain.push_back(bssl::UpRef(cert->cert_buffer()));
-  for (const auto& handle : cert->intermediate_buffers()) {
-    cert_chain.push_back(bssl::UpRef(handle.get()));
-  }
-  return cert_chain;
-}
-
-}  // namespace
-
 class SSLServerContextImpl::SocketImpl : public SSLServerSocket,
                                          public SocketBIOAdapter::Delegate {
  public:
@@ -771,7 +756,7 @@ std::unique_ptr<SSLServerContext> CreateSSLServerContext(
     EVP_PKEY* pkey,
     const SSLServerConfig& ssl_server_config) {
   SSLServerCredential credential;
-  credential.cert_chain = ChainFromX509Certificate(certificate);
+  credential.cert_chain = certificate->CopyCertBuffers();
   credential.pkey = bssl::UpRef(pkey);
   std::vector<SSLServerCredential> credentials;
   credentials.push_back(std::move(credential));
