f6a5fb01e814fa492bbc81f00299b4e2827b76d5
David Sanders <dsanders11@ucsbalum.com>
2025-08-25 14:36:29 -0700

    Replace include with forward decl in devtools_instrumentation.h
    Should save ~86 MiB of compiler input size.
    
    Adds missing includes and forward decls as needed to fix the build.
    
    Bug: 429365675
    Change-Id: Iaac53d26e5be1fa47230bf491374f9a5149ab317
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6876397
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#1506147}

--- content/browser/service_worker/service_worker_context_wrapper.cc
+++ content/browser/service_worker/service_worker_context_wrapper.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/single_thread_task_runner.h"
 #include "base/task/thread_pool.h"
 #include "base/time/time.h"
+#include "base/trace_event/trace_event.h"
 #include "content/browser/blob_storage/chrome_blob_storage_context.h"
 #include "content/browser/devtools/devtools_instrumentation.h"
 #include "content/browser/loader/navigation_url_loader_impl.h"
--- content/browser/service_worker/service_worker_new_script_loader.cc
+++ content/browser/service_worker/service_worker_new_script_loader.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/numerics/safe_conversions.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/single_thread_task_runner.h"
+#include "base/trace_event/trace_event.h"
 #include "content/browser/devtools/devtools_instrumentation.h"
 #include "content/browser/renderer_host/policy_container_host.h"
 #include "content/browser/service_worker/service_worker_cache_writer.h"
