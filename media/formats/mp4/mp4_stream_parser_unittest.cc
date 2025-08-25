42353fb79e41a9a88713731fb4ec40868714591f
Kalvin Lee <kdlee@chromium.org>
2025-08-24 23:00:23 -0700

    spanify: Ensafeify `mp4_stream_parser_unittest`
    The actual implementation of `mp4_stream_parser` will be spanified
    downstream in a much more intrusive CL.
    
    Bug: 439964610
    Change-Id: I3d24d0c41cf2983a483a5e1ab83e832e615c9899
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6869851
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1505705}

--- media/formats/mp4/mp4_stream_parser_unittest.cc
+++ media/formats/mp4/mp4_stream_parser_unittest.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "media/formats/mp4/mp4_stream_parser.h"
 
 #include <stddef.h>
@@ -1105,10 +1100,11 @@ TEST_P(MP4StreamParserRotationMatrixEvaluatorTest, RotationCalculation) {
   MovieHeader movie_header;
 
   // Identity matrix, with 16.16 and 2.30 fixed points.
-  uint32_t identity_matrix[9] = {1 << 16, 0, 0, 0, 1 << 16, 0, 0, 0, 1 << 30};
+  static constexpr std::array<int32_t, 9u> identity_matrix = {
+      1 << 16, 0, 0, 0, 1 << 16, 0, 0, 0, 1 << 30};
 
-  memcpy(movie_header.display_matrix, identity_matrix, sizeof(identity_matrix));
-  memcpy(track_header.display_matrix, identity_matrix, sizeof(identity_matrix));
+  base::span(movie_header.display_matrix).copy_from(identity_matrix);
+  base::span(track_header.display_matrix).copy_from(identity_matrix);
 
   MatrixRotationTestCaseParam data = GetParam();
 
