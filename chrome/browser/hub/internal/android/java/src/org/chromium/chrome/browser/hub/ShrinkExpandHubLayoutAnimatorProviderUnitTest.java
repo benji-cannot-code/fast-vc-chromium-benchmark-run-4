3b67270a9acf2f24a5ba4146bcb9fd447c775228
Calder Kitagawa <ckitagawa@chromium.org>
2025-08-25 12:09:01 -0700

    [Hub] Fix robolectric test
    When rolling robolectric to 4.15 these tests got disabled. At the time
    the theory was that the "more realistic" animation timing changes were
    the problem. This was part of the issue. However, the root cause is
    actually that views now correctly report dimensions so the scaling
    logic for the final corner radii started kicking in so the test
    expectations were wrong:
    https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/hub/android/java/src/org/chromium/chrome/browser/hub/ShrinkExpandAnimationData.java;drc=48f660fbabd7ec824f280cbdf7f33dac87ef37dc;l=128
    
    This CL gets the tests to pass again.
    
    Fixed: 424153626
    Change-Id: I73a9ed69bd67dfdfa3d107c83d7fa1ac2ef04759
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6879249
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Dan Polanco <polardz@google.com>
    Commit-Queue: Dan Polanco <polardz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1506033}

--- chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/ShrinkExpandHubLayoutAnimatorProviderUnitTest.java
+++ chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/ShrinkExpandHubLayoutAnimatorProviderUnitTest.java
@@ -54,7 +54,6 @@ import org.robolectric.shadows.ShadowLooper;
 import org.chromium.base.Callback;
 import org.chromium.base.supplier.SyncOneshotSupplierImpl;
 import org.chromium.base.test.BaseRobolectricTestRunner;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.HistogramWatcher;
@@ -110,7 +109,6 @@ public class ShrinkExpandHubLayoutAnimatorProviderUnitTest {
     }
 
     @Test
-    @DisabledTest(message = "crbug.com/424153626")
     public void testShrinkTab() {
         var watcher =
                 HistogramWatcher.newBuilder()
@@ -140,19 +138,20 @@ public class ShrinkExpandHubLayoutAnimatorProviderUnitTest {
         Rect finalRect = new Rect(50, 10, 70, 95);
         int initialTopCorner = 0;
         int initialBottomCorner = 0;
-        int finalTopCorner = 30;
-        int finalBottomCorner = 40;
+        int finalTopCornerUnscaled = 30;
+        int finalBottomCornerUnscaled = 40;
         ShrinkExpandAnimationData data =
                 ShrinkExpandAnimationData.createHubShrinkExpandAnimationData(
                         initialRect,
                         finalRect,
                         initialTopCorner,
                         initialBottomCorner,
-                        finalTopCorner,
-                        finalBottomCorner,
+                        finalTopCornerUnscaled,
+                        finalBottomCornerUnscaled,
                         thumbnailSize,
                         /* isTopToolbar= */ true,
                         /* useFallbackAnimation= */ false);
+        int[] finalCornerRadius = data.getFinalCornerRadii();
 
         HubLayoutAnimationRunner runner =
                 HubLayoutAnimationRunnerFactory.createHubLayoutAnimationRunner(animatorProvider);
@@ -167,10 +166,10 @@ public class ShrinkExpandHubLayoutAnimatorProviderUnitTest {
         runner.addListener(mListener);
         runner.runWithWaitForAnimatorTimeout(HUB_LAYOUT_TIMEOUT_MS);
 
-        thumbnailCallback.onResult(mBitmap);
         mAnimationDataSupplier.set(data);
+        thumbnailCallback.onResult(mBitmap);
 
-        ShadowLooper.runUiThreadTasks();
+        ShadowLooper.runUiThreadTasksIncludingDelayedTasks();
 
         verify(imageView, atLeastOnce())
                 .setRoundedCorners(
@@ -180,7 +179,10 @@ public class ShrinkExpandHubLayoutAnimatorProviderUnitTest {
                         initialBottomCorner);
         verify(imageView, atLeastOnce())
                 .setRoundedCorners(
-                        finalTopCorner, finalTopCorner, finalBottomCorner, finalBottomCorner);
+                        finalCornerRadius[0],
+                        finalCornerRadius[1],
+                        finalCornerRadius[2],
+                        finalCornerRadius[3]);
         verifyFinalState(animatorProvider, /* wasForcedToFinish= */ false);
         watcher.assertExpected();
     }
@@ -637,7 +639,6 @@ public class ShrinkExpandHubLayoutAnimatorProviderUnitTest {
     }
 
     @Test
-    @DisabledTest(message = "crbug.com/424153626")
     public void testShrinkTab_NoToolbarFadeForBottomToolbar() {
         var watcher =
                 HistogramWatcher.newBuilder()
@@ -667,19 +668,20 @@ public class ShrinkExpandHubLayoutAnimatorProviderUnitTest {
         Rect finalRect = new Rect(50, 10, 70, 95);
         int initialTopCorner = 0;
         int initialBottomCorner = 0;
-        int finalTopCorner = 30;
-        int finalBottomCorner = 40;
+        int finalTopCornerUnscaled = 30;
+        int finalBottomCornerUnscaled = 40;
         ShrinkExpandAnimationData data =
                 ShrinkExpandAnimationData.createHubShrinkExpandAnimationData(
                         initialRect,
                         finalRect,
                         initialTopCorner,
                         initialBottomCorner,
-                        finalTopCorner,
-                        finalBottomCorner,
+                        finalTopCornerUnscaled,
+                        finalBottomCornerUnscaled,
                         thumbnailSize,
                         /* isTopToolbar= */ false,
                         /* useFallbackAnimation= */ false);
+        int[] finalCornerRadius = data.getFinalCornerRadii();
 
         HubLayoutAnimationRunner runner =
                 HubLayoutAnimationRunnerFactory.createHubLayoutAnimationRunner(animatorProvider);
@@ -694,10 +696,10 @@ public class ShrinkExpandHubLayoutAnimatorProviderUnitTest {
         runner.addListener(mListener);
         runner.runWithWaitForAnimatorTimeout(HUB_LAYOUT_TIMEOUT_MS);
 
-        thumbnailCallback.onResult(mBitmap);
         mAnimationDataSupplier.set(data);
+        thumbnailCallback.onResult(mBitmap);
 
-        ShadowLooper.runUiThreadTasks();
+        ShadowLooper.runUiThreadTasksIncludingDelayedTasks();
 
         verify(imageView, atLeastOnce())
                 .setRoundedCorners(
@@ -707,7 +709,10 @@ public class ShrinkExpandHubLayoutAnimatorProviderUnitTest {
                         initialBottomCorner);
         verify(imageView, atLeastOnce())
                 .setRoundedCorners(
-                        finalTopCorner, finalTopCorner, finalBottomCorner, finalBottomCorner);
+                        finalCornerRadius[0],
+                        finalCornerRadius[1],
+                        finalCornerRadius[2],
+                        finalCornerRadius[3]);
         verifyFinalState(animatorProvider, /* wasForcedToFinish= */ false);
         watcher.assertExpected();
     }
