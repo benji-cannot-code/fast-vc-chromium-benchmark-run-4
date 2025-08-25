a88a8650f3eabedbb1b74b289ededef0cd6a9830
HuanPo Lin <robertlin@chromium.org>
2025-08-24 22:47:04 -0700

    preload: Introduce BookmarkBar prefetch
    This CL introduces BookmarkBar prefetch on-hover trigger.
    1. The expected order of preloading
    is preconnect => prefetch => prerender depending on the level of the
    signals.
    2. The order of 1 cannot be guaranteed as prerender is
    triggered by mouse down and it is not necessarily happens after on-hover
    trigger. If prerender is triggered without on-hover prefetch being
    triggered, prefetch will be requested before starting prerender. In this
    way, if prerender fails, it is possible to fall back to prefetch.
    
    NO_IFTTT=Updating embedder triggers requires no update in histograms.xml
    
    Bug: 413259638
    Change-Id: I9bb8e64eb5c52013c68007b87b9093eed181d36f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6477744
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Ken Okada <kenoss@chromium.org>
    Reviewed-by: Taiyo Mizuhashi <taiyo@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1505703}

--- chrome/browser/preloading/bookmarkbar_preload/bookmarkbar_preload_pipeline.h
+++ chrome/browser/preloading/bookmarkbar_preload/bookmarkbar_preload_pipeline.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_PRELOADING_BOOKMARKBAR_PRELOAD_BOOKMARKBAR_PRELOAD_PIPELINE_H_
 #define CHROME_BROWSER_PRELOADING_BOOKMARKBAR_PRELOAD_BOOKMARKBAR_PRELOAD_PIPELINE_H_
 
+#include "content/public/browser/prefetch_handle.h"
 #include "content/public/browser/preload_pipeline_info.h"
 #include "content/public/browser/prerender_handle.h"
 #include "url/gurl.h"
@@ -28,19 +29,30 @@ class BookmarkBarPreloadPipeline {
   BookmarkBarPreloadPipeline& operator=(const BookmarkBarPreloadPipeline&) =
       delete;
 
+  void StartPrefetch(content::WebContents& web_contents,
+                     content::PreloadingPredictor predictor);
+
   void StartPrerender(content::WebContents& web_contents,
                       content::PreloadingPredictor predictor);
   bool IsPrerenderValid() {
     return prerender_handle_ && prerender_handle_->IsValid();
   }
+  void ResetPrerender() { prerender_handle_.reset(); }
 
   const GURL& url() const { return url_; }
+
+  void SetOnPrefetchCompletedOrFailedCallbackForTesting(
+      base::RepeatingCallback<
+          void(const network::URLLoaderCompletionStatus& completion_status,
+               const std::optional<int>& response_code)>
+          on_prefetch_completed_or_failed);
 
  private:
   const scoped_refptr<content::PreloadPipelineInfo> pipeline_info_;
 
   const GURL url_;
 
+  std::unique_ptr<content::PrefetchHandle> prefetch_handle_;
   std::unique_ptr<content::PrerenderHandle> prerender_handle_;
 };
 
--- chrome/browser/ui/views/bookmarks/bookmark_button.h
+++ chrome/browser/ui/views/bookmarks/bookmark_button.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/timer/timer.h"
 #include "chrome/browser/preloading/chrome_preloading.h"
+#include "content/public/browser/preloading.h"
 #include "content/public/browser/prerender_handle.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
@@ -77,8 +78,9 @@ class BookmarkButton : public BookmarkButtonBase, public views::WidgetObserver {
                              const gfx::Rect& new_bounds) override;
 
  private:
+  void StartPreloading(const GURL& url, content::PreloadingType preloadingType);
   void StartPreconnecting(GURL url);
-  void StartPrerendering(GURL url);
+  void StopPreloadingTimers();
 
   void UpdateMaxTooltipWidth();
 
@@ -89,6 +91,7 @@ class BookmarkButton : public BookmarkButtonBase, public views::WidgetObserver {
   const raw_ref<const GURL> url_;
   const raw_ptr<Browser> browser_;
   base::RetainingOneShotTimer preconnect_timer_;
+  base::RetainingOneShotTimer prefetch_timer_;
 
   base::ScopedObservation<views::Widget, views::WidgetObserver>
       widget_observation_{this};
