1f3b9c637fd544301d6f06e66f36b822ae848531
Oleg Nitz <ovn@google.com>
2025-08-25 01:04:49 -0700

    Delete expired histogram Apps.PreferredApps.EntryCount.
    Fixed: 40914930
    Change-Id: Id411edbe740736c2f8232478f71be80323d12283
    
    OBSOLETE_HISTOGRAM[Apps.PreferredApps.EntryCount]=Not used
    
    Change-Id: Id411edbe740736c2f8232478f71be80323d12283
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6874451
    Auto-Submit: Oleg Nitz <ovn@google.com>
    Commit-Queue: Oleg Nitz <ovn@google.com>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1505732}

--- components/services/app_service/public/cpp/preferred_apps_impl.cc
+++ components/services/app_service/public/cpp/preferred_apps_impl.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/json/json_string_value_serializer.h"
 #include "base/logging.h"
-#include "base/metrics/histogram_functions.h"
 #include "base/sequence_checker.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
@@ -47,10 +46,6 @@ enum class PreferredAppsUpdateAction {
   kUpgraded = 3,
   kMaxValue = kUpgraded,
 };
-
-void LogPreferredAppEntryCount(int entry_count) {
-  base::UmaHistogramCounts10000("Apps.PreferredApps.EntryCount", entry_count);
-}
 
 // Performs blocking I/O. Called on another thread.
 void WriteDataBlocking(const base::FilePath& preferred_apps_file,
@@ -204,8 +199,6 @@ void PreferredAppsImpl::ReadCompleted(std::string preferred_apps_string) {
   if (!preferred_apps_upgraded) {
     WriteToJSON(profile_dir_, preferred_apps_list_);
   }
-
-  LogPreferredAppEntryCount(preferred_apps_list_.GetEntrySize());
 
   while (!pending_preferred_apps_tasks_.empty()) {
     std::move(pending_preferred_apps_tasks_.front()).Run();
