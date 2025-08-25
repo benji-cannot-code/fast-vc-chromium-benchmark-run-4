0359acd6b6ea408804b3e29a4a9ba479c39947d1
Peter Kasting <pkasting@chromium.org>
2025-08-25 16:12:39 -0700

    Trivial cleanups for PageColorsController.
    * Comment/whitespace changes
    * Remove unnecessary Init() method
    * PrefRegistry passed in need not actually be syncable
    * Put virtual method before non-static non-virtuals, keep same access
    
    Bug: 432019728
    Change-Id: I2c62fd4a524ae8a2b7497d2ac7e43ab06d2cdda6
    AX-Relnotes: n/a.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6881751
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1506196}

--- chrome/browser/accessibility/page_colors_controller_factory.cc
+++ chrome/browser/accessibility/page_colors_controller_factory.cc
@@ -25,8 +25,8 @@ PageColorsControllerFactory::PageColorsControllerFactory()
     : ProfileKeyedServiceFactory(
           "PageColorsController",
           ProfileSelections::Builder()
-              // The incognito profile shares the PageColors with its original
-              // profile.
+              // The incognito profile shares the PageColorsController with its
+              // original profile.
               .WithRegular(ProfileSelection::kRedirectedToOriginal)
               .WithGuest(ProfileSelection::kNone)
               .WithSystem(ProfileSelection::kNone)
@@ -42,8 +42,6 @@ bool PageColorsControllerFactory::ServiceIsCreatedWithBrowserContext() const {
 std::unique_ptr<KeyedService>
 PageColorsControllerFactory::BuildServiceInstanceForBrowserContext(
     content::BrowserContext* context) const {
-  auto page_colors_controller = std::make_unique<PageColorsController>(
+  return std::make_unique<PageColorsController>(
       Profile::FromBrowserContext(context)->GetPrefs());
-  page_colors_controller->Init();
-  return page_colors_controller;
 }
