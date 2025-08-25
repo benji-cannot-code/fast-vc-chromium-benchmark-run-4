09cf6d1bf771705098266f91c03319770e5696f7
Wan-Teh Chang <wtc@google.com>
2025-08-25 14:09:44 -0700

    Roll src/third_party/dav1d/libdav1d/ 716164239..af5cf2b1e (3 commits)
    https://chromium.googlesource.com/external/github.com/videolan/dav1d.git/+log/716164239ad6..af5cf2b1e7f0
    
    $ git log 716164239..af5cf2b1e --date=short --no-merges --format='%ad %ae %s'
    2025-07-17 khalid.masum.92 Readme: improve consitency of compilation steps
    2025-08-12 jb On the road to 1.5.2
    2025-03-29 jb Update COPYING years
    
    Created with:
      roll-dep src/third_party/dav1d/libdav1d
    R=dalecurtis@chromium.org,eugene@chromium.org
    
    Change-Id: Ic402fa125c34ad474974c8f2b0fce2b085656754
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6881553
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Wan-Teh Chang <wtc@google.com>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1506132}

--- third_party/dav1d/version/vcs_version.h
+++ third_party/dav1d/version/vcs_version.h
@@ -1,3 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* auto-generated, do not edit */
-#define DAV1D_VERSION "1.5.1-17-g7161642"
+#define DAV1D_VERSION "1.5.1-20-gaf5cf2b"
