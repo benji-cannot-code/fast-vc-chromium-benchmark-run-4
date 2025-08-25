cc87ac967dd5d7dabd32bd95c8cc3924356a138e
Zaina Al-Mashni <zalmashni@google.com>
2025-08-25 08:20:45 -0700

    [History] Add actor icon and tooltip to items with actor visit source
    This CL adds a tooltip icon to history items when isActorVisit is true.
    
    Screenshot: https://screenshot.googleplex.com/Ay7JxEgzG4pwt3j
    Bug: 428943987
    Change-Id: Ifbf7dd7caeebec9a3fc927e0d4a0cdc9cdb96199
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6870151
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1505875}

--- chrome/browser/resources/history/history_item.ts
+++ chrome/browser/resources/history/history_item.ts
@@ -4,8 +4,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import './searched_label.js';
+import './shared_icons.html.js';
 import '/strings.m.js';
 import 'chrome://resources/cr_elements/cr_checkbox/cr_checkbox.js';
+import 'chrome://resources/cr_elements/policy/cr_tooltip_icon.js';
 import 'chrome://resources/cr_elements/icons.html.js';
 
 import {HistoryResultType} from 'chrome://resources/cr_components/history/constants.js';
@@ -221,6 +223,11 @@ export class HistoryItemElement extends HistoryItemElementBase {
     return this.isCardStart || this.isCardEnd ?
         'title-and-domain date-accessed' :
         'title-and-domain';
+  }
+
+  protected shouldShowActorTooltip_() {
+    return loadTimeData.getBoolean('enableBrowsingHistoryActorIntegrationM1') &&
+        this.item?.isActorVisit;
   }
 
   /**
--- chrome/browser/ui/webui/cr_components/history/history_util.cc
+++ chrome/browser/ui/webui/cr_components/history/history_util.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/history_resources.h"
 #include "chrome/grit/history_resources_map.h"
 #include "chrome/grit/locale_settings.h"
+#include "components/browsing_data/core/features.h"
 #include "components/favicon_base/favicon_url_parser.h"
 #include "components/history/core/common/pref_names.h"
 #include "components/history_clusters/core/history_clusters_prefs.h"
@@ -49,6 +50,7 @@ content::WebUIDataSource* HistoryUtil::PopulateCommonSourceForHistory(
       {"entrySummary", IDS_HISTORY_ENTRY_SUMMARY},
       {"expandSessionButton", IDS_HISTORY_OTHER_SESSIONS_EXPAND_SESSION},
       {"foundSearchResults", IDS_HISTORY_FOUND_SEARCH_RESULTS},
+      {"actorTaskTooltip", IDS_ACTOR_TASK},
       {"historyMenuButton", IDS_HISTORY_HISTORY_MENU_DESCRIPTION},
       {"historyMenuItem", IDS_HISTORY_HISTORY_MENU_ITEM},
       {"itemsSelected", IDS_HISTORY_ITEMS_SELECTED},
@@ -81,6 +83,11 @@ content::WebUIDataSource* HistoryUtil::PopulateCommonSourceForHistory(
   source->AddBoolean("isGuestSession", profile->IsGuestSession());
   source->AddBoolean("isSignInAllowed",
                      prefs->GetBoolean(prefs::kSigninAllowed));
+
+  source->AddBoolean(
+      "enableBrowsingHistoryActorIntegrationM1",
+      base::FeatureList::IsEnabled(
+          browsing_data::features::kBrowsingHistoryActorIntegrationM1));
 
   signin::IdentityManager* identity_manager =
       IdentityManagerFactory::GetForProfile(profile);
