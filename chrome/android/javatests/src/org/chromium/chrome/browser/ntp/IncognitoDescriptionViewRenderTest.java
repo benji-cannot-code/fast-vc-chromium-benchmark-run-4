507d2704da89dee67833a9320cd49005cc29854d
Zainab Rizvi <rizvis@google.com>
2025-08-24 23:30:16 -0700

    Revert "[ACT] Updates to ACT NTP and settings strings"
    This reverts commit cfc15de893cd1fd98d56bfad297315001a7f2f85.
    
    Reason for revert: Changes need more approvals
    
    Bug: b:440331109
    Original change's description:
    > [ACT] Updates to ACT NTP and settings strings
    >
    > Bug: b:440331109
    > Change-Id: I4456aee08c403b29558dcaf298650ab85323de6e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6867732
    > Reviewed-by: Kevin Graney <kmg@google.com>
    > Commit-Queue: Fiona Macintosh <fmacintosh@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1504832}
    
    Bug: b:440331109
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I9f2b6b07a19afcd194d46dac67ee3e2ce905e6d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6874908
    Reviewed-by: Fiona Macintosh <fmacintosh@google.com>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Kevin Graney <kmg@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Auto-Submit: Zainab Rizvi <rizvis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1505713}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ntp/IncognitoDescriptionViewRenderTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ntp/IncognitoDescriptionViewRenderTest.java
@@ -44,8 +44,7 @@ import java.util.List;
 @Batch(Batch.PER_CLASS)
 @EnableFeatures({
     ChromeFeatureList.ALWAYS_BLOCK_3PCS_INCOGNITO,
-    ChromeFeatureList.FINGERPRINTING_PROTECTION_UX,
-    ChromeFeatureList.IP_PROTECTION_UX
+    ChromeFeatureList.FINGERPRINTING_PROTECTION_UX
 })
 public class IncognitoDescriptionViewRenderTest {
     @ParameterAnnotations.ClassParameter
@@ -89,8 +88,7 @@ public class IncognitoDescriptionViewRenderTest {
     @Feature({"RenderTest"})
     @DisableFeatures({
         ChromeFeatureList.ALWAYS_BLOCK_3PCS_INCOGNITO,
-        ChromeFeatureList.FINGERPRINTING_PROTECTION_UX,
-        ChromeFeatureList.IP_PROTECTION_UX
+        ChromeFeatureList.FINGERPRINTING_PROTECTION_UX
     })
     public void testRender_IncognitoDescriptionView() throws IOException {
         View view = sActivity.findViewById(android.R.id.content);
@@ -110,8 +108,7 @@ public class IncognitoDescriptionViewRenderTest {
     @Feature({"RenderTest"})
     @DisableFeatures({
         ChromeFeatureList.ALWAYS_BLOCK_3PCS_INCOGNITO,
-        ChromeFeatureList.FINGERPRINTING_PROTECTION_UX,
-        ChromeFeatureList.IP_PROTECTION_UX
+        ChromeFeatureList.FINGERPRINTING_PROTECTION_UX
     })
     public void testRender_IncognitoDescriptionViewTrackingProtection() throws IOException {
         View view = sActivity.findViewById(android.R.id.content);
@@ -129,10 +126,7 @@ public class IncognitoDescriptionViewRenderTest {
     @Test
     @MediumTest
     @Feature({"RenderTest"})
-    @DisableFeatures({
-        ChromeFeatureList.FINGERPRINTING_PROTECTION_UX,
-        ChromeFeatureList.IP_PROTECTION_UX
-    })
+    @DisableFeatures({ChromeFeatureList.FINGERPRINTING_PROTECTION_UX})
     public void render_IncognitoDescriptionView_alwaysBlock3pcsIncognito() throws IOException {
         View view = sActivity.findViewById(android.R.id.content);
         ThreadUtils.runOnUiThreadBlocking(
