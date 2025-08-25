d7ab29ae9f7a70b1533d28c5dc79385de7c45cd4
Greg Thompson <grt@chromium.org>
2025-08-25 01:08:17 -0700

    [A11yPerformance] Add node counts to chrome://accessibility
    Add a new "Accessible node counters" block to chrome://accessibility so
    that developers of accessibility tools for Windows can check for node
    leaks. This block is updated once per second while the page is not
    hidden. While at it, I took the liberty of applying the same treatment
    to the process-wide accessibility mode checkboxes.
    
    On Windows, applications expose accessible nodes to tools via COM
    interfaces on objects implemented by the application. When a node in the
    application's UI is removed, the application cannot release the memory
    used by its object until any accessibility tool(s) referencing the node
    have released all of their references via IUnknown::Release().
    Reference leaks in either the application itself or in an accessibility
    tool result in memory leaks.
    
    UMA metrics in Chrome indicate that users experience such reference
    leaks, resulting in excessive memory use in the browser process. This
    new data in chrome://accessibility empowers developers to be aware of
    leaks and, one hopes, work toward reducing them.
    
    Bug: 40665982
    Change-Id: Ia6861c4e845c2c0ee65714a00613721dc7bb17a2
    AX-Relnotes: n/a.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6870052
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Reviewed-by: Lucas Radaelli <lucasradaelli@google.com>
    Cr-Commit-Position: refs/heads/main@{#1505735}

--- chrome/browser/ui/webui/accessibility/accessibility_ui.h
+++ chrome/browser/ui/webui/accessibility/accessibility_ui.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/timer/timer.h"
 #include "base/values.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_ui_controller.h"
@@ -63,7 +64,8 @@ class AccessibilityUIObserver : public content::WebContentsObserver {
 };
 
 // Manages messages sent from accessibility.js via json.
-class AccessibilityUIMessageHandler : public content::WebUIMessageHandler {
+class AccessibilityUIMessageHandler : public content::WebUIMessageHandler,
+                                      public content::WebContentsObserver {
  public:
   AccessibilityUIMessageHandler();
 
@@ -98,8 +100,21 @@ class AccessibilityUIMessageHandler : public content::WebUIMessageHandler {
   // the user-set type is not supported.
   ui::AXApiType::Type GetRecordingApiType();
 
+  // content::WebContentsObserver:
+  void OnVisibilityChanged(content::Visibility visibility) override;
+
+  // Updates the UI with new data. Called periodically to keep the UI up-to-date
+  // while it is visible.
+  void OnUpdateDisplayTimer();
+
   std::vector<std::string> event_logs_;
   std::unique_ptr<AccessibilityUIObserver> observer_;
+
+  // The last data for display sent to the UI by OnUpdateDisplayTimer.
+  base::Value::Dict last_data_;
+
+  // A timer that runs while the UI is visible to call OnUpdateDisplayTimer.
+  base::RepeatingTimer update_display_timer_;
 
   base::WeakPtrFactory<AccessibilityUIMessageHandler> weak_ptr_factory_{this};
 };
