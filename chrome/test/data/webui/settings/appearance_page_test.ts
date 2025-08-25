76bb3cd71954dfdbf72f6b34760daa504e3b53df
Kaan Alsan <alsan@google.com>
2025-08-25 07:56:35 -0700

    [SxS] Add appearance setting to disable drag entrypoint
    Adds a setting to the Appearances page to disable drag entrypoints for
    split view. This setting will not appear if the `SideBySide` feature
    flag is disabled.
    
    Change-Id: Ifcd4649a7d6c530e7d806e8fc22b2e807988a0e9
    Bug: 436243734
    Fixed: 436243734
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6875979
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1505862}

--- chrome/test/data/webui/settings/appearance_page_test.ts
+++ chrome/test/data/webui/settings/appearance_page_test.ts
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
-import type {CustomizeColorSchemeModeClientRemote, SettingsAppearancePageElement, SettingsDropdownMenuElement} from 'chrome://settings/settings.js';
+import type {CustomizeColorSchemeModeClientRemote, SettingsAppearancePageElement, SettingsDropdownMenuElement, SettingsToggleButtonElement} from 'chrome://settings/settings.js';
 import {AppearanceBrowserProxyImpl, ColorSchemeMode, CustomizeColorSchemeModeBrowserProxy, CustomizeColorSchemeModeClientCallbackRouter, CustomizeColorSchemeModeHandlerRemote, SystemTheme} from 'chrome://settings/settings.js';
 import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {TestMock} from 'chrome://webui-test/test_mock.js';
@@ -358,6 +358,55 @@ suite('AppearanceHandler', function() {
     await microtasksFinished();
     assertTrue(
         !appearancePage.shadowRoot!.querySelector('#tabSearchPositionRow'));
+  });
+
+  test('show split view drag and drop options', async function() {
+    loadTimeData.overrideValues({
+      showSplitViewDragAndDropSetting: true,
+    });
+    createAppearancePage();
+    await microtasksFinished();
+    assertTrue(
+        !!appearancePage.shadowRoot!.querySelector('#splitViewDragAndDrop'));
+    assertFalse(!!appearancePage.shadowRoot!
+                      .querySelector<SettingsToggleButtonElement>(
+                          '#splitViewDragAndDrop')!.hidden);
+  });
+
+  test('hide split view drag and drop options', async function() {
+    loadTimeData.overrideValues({
+      showSplitViewDragAndDropSetting: false,
+    });
+    createAppearancePage();
+    await microtasksFinished();
+    assertTrue(
+        !!appearancePage.shadowRoot!.querySelector('#splitViewDragAndDrop'));
+    assertTrue(!!appearancePage.shadowRoot!
+                     .querySelector<SettingsToggleButtonElement>(
+                         '#splitViewDragAndDrop')!.hidden);
+  });
+
+  test('split view drag and drop toggle updates pref', async function() {
+    loadTimeData.overrideValues({
+      showSplitViewDragAndDropSetting: true,
+    });
+    createAppearancePage();
+    appearancePage.set('prefs.browser.split_view_drag_and_drop_enabled', {
+      type: chrome.settingsPrivate.PrefType.BOOLEAN,
+      value: true,
+    });
+    await microtasksFinished();
+
+    const toggle =
+        appearancePage.shadowRoot!.querySelector<SettingsToggleButtonElement>(
+            '#splitViewDragAndDrop');
+    assertTrue(!!toggle);
+    assertTrue(toggle.checked);
+
+    toggle.click();
+    await microtasksFinished();
+    assertFalse(appearancePage.get(
+        'prefs.browser.split_view_drag_and_drop_enabled.value'));
   });
 });
 
