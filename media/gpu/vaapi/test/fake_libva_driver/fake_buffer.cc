adb12126c819592e35e27a9c1168acf2822946a9
Michael Tatarski <mtatarski@google.com>
2025-08-25 16:46:58 -0700

    [CodeHealth] Use base::HeapArray in FakeBuffer.
    Replaces std::unique_ptr<uint8_t[]> with base::HeapArray<uint8_t> for
    buffer data storage in FakeBuffer, aligning with Chromium's preferred
    array types.
    
    Bug: 40285824
    Change-Id: Ida797702a0cec87de393b9747cb53b624c6553fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6870129
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Auto-Submit: Michael Tatarski <mtatarski@google.com>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1506211}

--- media/gpu/vaapi/test/fake_libva_driver/fake_buffer.cc
+++ media/gpu/vaapi/test/fake_libva_driver/fake_buffer.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string.h>
 
 #include "base/compiler_specific.h"
+#include "base/containers/span.h"
 #include "base/numerics/checked_math.h"
 
 namespace {
@@ -33,10 +34,10 @@ FakeBuffer::FakeBuffer(IdType id,
     : id_(id),
       context_(context),
       type_(type),
-      data_size_(CalculateDataSize(size_per_element, num_elements)),
-      data_(std::make_unique<uint8_t[]>(data_size_)) {
+      data_(base::HeapArray<uint8_t>::Uninit(
+          CalculateDataSize(size_per_element, num_elements))) {
   if (data) {
-    UNSAFE_TODO(memcpy(data_.get(), data, data_size_));
+    UNSAFE_TODO(memcpy(const_cast<uint8_t*>(data_.data()), data, data_.size()));
   }
 }
 
@@ -55,11 +56,11 @@ VABufferType FakeBuffer::GetType() const {
 }
 
 size_t FakeBuffer::GetDataSize() const {
-  return data_size_;
+  return data_.size();
 }
 
 void* FakeBuffer::GetData() const {
-  return data_.get();
+  return static_cast<void*>(const_cast<uint8_t*>(data_.data()));
 }
 
 }  // namespace media::internal
--- media/gpu/vaapi/test/fake_libva_driver/fake_buffer.h
+++ media/gpu/vaapi/test/fake_libva_driver/fake_buffer.h
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MEDIA_GPU_VAAPI_TEST_FAKE_LIBVA_DRIVER_FAKE_BUFFER_H_
 #define MEDIA_GPU_VAAPI_TEST_FAKE_LIBVA_DRIVER_FAKE_BUFFER_H_
 
-#include <memory>
-
 #include <va/va.h>
+
+#include "base/containers/heap_array.h"
 
 namespace media::internal {
 
@@ -44,8 +44,7 @@ class FakeBuffer {
   const IdType id_;
   const VAContextID context_;
   const VABufferType type_;
-  const size_t data_size_;
-  const std::unique_ptr<uint8_t[]> data_;
+  const base::HeapArray<uint8_t> data_;
 };
 
 }  // namespace media::internal
