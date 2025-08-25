dd3aae58b9b1278f616a6c1403ced720cc0c816b
Keigo Oka <oka@google.com>
2025-08-24 17:10:07 -0700

    extensions: enable developerPrivate.removeMultipleExtensions on desktop android
    This moves the implementation of developerPrivate.removeMultipleExtensions
    to a shared location to enable it on Android.
    
    The multi-uninstall dialog is currently disabled on desktop Android, and
    the extensions are removed directly without user confirmation.
    
    Bug: 440503572
    Change-Id: I22a7a718800e12119f2e4231d1f72f132b8253a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6875122
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Keigo Oka <oka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1505665}

--- chrome/browser/extensions/api/developer_private/developer_private_functions_android.cc
+++ chrome/browser/extensions/api/developer_private/developer_private_functions_android.cc
@@ -10,9 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 DEFINE_UNIMPLEMENTED_EXTENSION_FUNCTION(DeveloperPrivateLoadDirectoryFunction,
                                         "developerPrivate.loadDirectory")
 DEFINE_UNIMPLEMENTED_EXTENSION_FUNCTION(
-    DeveloperPrivateRemoveMultipleExtensionsFunction,
-    "developerPrivate.removeMultipleExtensions")
-DEFINE_UNIMPLEMENTED_EXTENSION_FUNCTION(
     DeveloperPrivateDismissMv2DeprecationNoticeForExtensionFunction,
     "developerPrivate.dismissMv2DeprecationNoticeForExtension")
 DEFINE_UNIMPLEMENTED_EXTENSION_FUNCTION(
--- chrome/browser/extensions/api/developer_private/developer_private_functions_android.h
+++ chrome/browser/extensions/api/developer_private/developer_private_functions_android.h
@@ -12,9 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 DECLARE_UNIMPLEMENTED_EXTENSION_FUNCTION(DeveloperPrivateLoadDirectoryFunction,
                    "developerPrivate.loadDirectory",
                    DEVELOPERPRIVATE_LOADUNPACKEDCROS);
-DECLARE_UNIMPLEMENTED_EXTENSION_FUNCTION(DeveloperPrivateRemoveMultipleExtensionsFunction,
-                   "developerPrivate.removeMultipleExtensions",
-                   DEVELOPERPRIVATE_REMOVEMULTIPLEEXTENSIONS);
 DECLARE_UNIMPLEMENTED_EXTENSION_FUNCTION(
     DeveloperPrivateDismissMv2DeprecationNoticeForExtensionFunction,
     "developerPrivate.dismissMv2DeprecationNoticeForExtension",
