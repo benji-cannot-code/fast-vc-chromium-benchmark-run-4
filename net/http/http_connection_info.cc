096d0c9254374de588968ef37b15d10a14731dbc
Keita Suzuki <suzukikeita@chromium.org>
2025-08-24 23:55:41 -0700

    Slice GWS connection metrics by connection protocol.
    This change adds a `ConnectionProtocolType` variant to several GWS page
    load metrics, including `ConnectionReuseStatus` and all `ConnectTiming`
    histograms. The connection protocol (e.g., HTTP/1, HTTP/2, HTTP/3) is
    extracted from the navigation handle and used as a suffix for these
    histograms.
    
    Bug: 440123099
    Change-Id: I6021dcf69009edd3ae29222a227e998de1384e83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6869212
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Keita Suzuki <suzukikeita@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1505717}

--- net/http/http_connection_info.cc
+++ net/http/http_connection_info.cc
@@ -103,6 +103,11 @@ std::string_view HttpConnectionInfoToString(
   }
 }
 
+// Returns a more coarse-grained description of the protocol used to fetch the
+// response.
+//
+// This is used for histograms, so the returned value must not change.
+// LINT.IfChange(HttpConnectionInfoCoarseToString)
 std::string_view HttpConnectionInfoCoarseToString(
     HttpConnectionInfoCoarse connection_info_coarse) {
   switch (connection_info_coarse) {
@@ -116,6 +121,7 @@ std::string_view HttpConnectionInfoCoarseToString(
       return "Other";
   }
 }
+// LINT.ThenChange(//tools/metrics/histograms/metadata/page/histograms.xml:ConnectionProtocolType)
 
 // Returns a more coarse-grained description of the protocol used to fetch the
 // response.
