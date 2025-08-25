908a6b9dcbb8a55faee641551320cb9f25bbcd3f
Sergii Bykov <sbykov@google.com>
2025-08-25 03:04:40 -0700

    kiosk: Remove an obsolete todo in iwa manager
    This TODO was based on a similar web kiosk functionality. But the
    install url is not used in IWAs. The update manifest URL is instead
    used as a temp app name in the 'Apps' menu before the app is installed.
    
    Bug: 361017701
    Change-Id: I34bac9be5ffb7f4fbd4923042e64147b0a81fa9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6875599
    Reviewed-by: Ashutosh Singhal <macinashutosh@google.com>
    Commit-Queue: Ashutosh Singhal <macinashutosh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1505768}

--- chrome/browser/ash/app_mode/isolated_web_app/kiosk_iwa_manager.cc
+++ chrome/browser/ash/app_mode/isolated_web_app/kiosk_iwa_manager.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/logging.h"
 #include "base/memory/raw_ref.h"
+#include "base/notreached.h"
 #include "chrome/browser/ash/app_mode/isolated_web_app/kiosk_iwa_data.h"
 #include "chrome/browser/ash/app_mode/kiosk_app_manager_base.h"
 #include "chrome/browser/ash/app_mode/kiosk_app_types.h"
@@ -84,7 +85,6 @@ KioskIwaManager::~KioskIwaManager() {
 KioskAppManagerBase::AppList KioskIwaManager::GetApps() const {
   AppList result;
   for (const auto& iwa_app_data : isolated_web_apps_) {
-    // TODO(crbug.com/361017701): fill in the install url
     result.emplace_back(*iwa_app_data);
   }
   return result;
