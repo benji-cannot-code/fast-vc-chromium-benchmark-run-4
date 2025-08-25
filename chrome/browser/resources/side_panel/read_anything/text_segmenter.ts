2d312a8cf06cf811be1821417b60e583cfd4c20f
Lauren Winston <lwinston@google.com>
2025-08-25 16:51:45 -0700

    [Read aloud] Use base language for Intl.Segmenter.
    It was difficult to find a test case where this actually makes
    a difference for word count- it should make more of a difference
    for sentence segmentation and when read aloud is actually
    separating out the words themselves.
    
    Bug: 440400392
    Change-Id: I769c5cbb00c231617e26aef775f55fd7d46f5453
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6880773
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1506212}

--- chrome/browser/resources/side_panel/read_anything/text_segmenter.ts
+++ chrome/browser/resources/side_panel/read_anything/text_segmenter.ts
@@ -6,14 +6,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Wrapper class for Intl.Segmenter that manages Intl.Segmenter instances to
 // be used to segment text.
 export class TextSegmenter {
-  private wordSegmenter_: Intl.Segmenter;
+  private wordSegmenter_!: Intl.Segmenter;
 
   constructor() {
     // If no language code has been provided, Intl.Segmenter will use the system
     // default language.
     // TODO: crbug.com/440400392- Use the page language to improve segmentation.
-    this.wordSegmenter_ = new Intl.Segmenter(/* languageCode= */ undefined,
-                                             {granularity: 'word'});
+    this.updateLanguage();
+  }
+
+  updateLanguage(lang?: string) {
+    // The try-catch is needed because Intl.Segmenter throws an error if the
+    // language code is not well-formed.
+    try {
+      this.wordSegmenter_ = new Intl.Segmenter(lang, {granularity: 'word'});
+    } catch {
+      this.wordSegmenter_ =
+          new Intl.Segmenter(undefined, {granularity: 'word'});
+    }
   }
 
   getWordCount(text: string): number {
