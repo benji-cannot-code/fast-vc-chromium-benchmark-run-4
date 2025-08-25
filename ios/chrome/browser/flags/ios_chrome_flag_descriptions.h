a56051e9851b1c9030a5568be1ae2cdb57579213
Briana McClure <bmcclure@google.com>
2025-08-25 14:57:37 -0700

    [iOS] Add feature flag for Default Browser Magic Stack
    This change adds a two arm flag for the Default Browser Magic Stack
    feature.
    
    Fixed: 440096754
    Change-Id: I58b7d61bc22489d910f7c6a0968af4abf8a55280
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6880129
    Commit-Queue: Briana McClure <bmcclure@google.com>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1506163}

--- ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
+++ ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
@@ -264,6 +264,9 @@ extern const char kDataSharingSharedDataTypesEnabledWithUi[];
 extern const char kDefaultBrowserBannerPromoName[];
 extern const char kDefaultBrowserBannerPromoDescription[];
 
+extern const char kDefaultBrowserMagicStackName[];
+extern const char kDefaultBrowserMagicStackDescription[];
+
 extern const char kDefaultBrowserOffCyclePromoName[];
 extern const char kDefaultBrowserOffCyclePromoDescription[];
 
