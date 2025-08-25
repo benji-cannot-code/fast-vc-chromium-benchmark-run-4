6833e1379b6e071fabd2d1558bf47e3e4ba65996
Ivan Pavlotsky <ivan.pavlotsky@lge.com>
2025-08-25 10:59:00 -0700

    libstdc++: Add initializers for constexpr static data members in Node
    In the CL https://crrev.com/c/6512733 static constexpr data members of
    structs ParentNodeTag and ShadowHostTag were added without initializers.
    But C++ standard requires initializer for constexpr static data member
    of LiteralType. So, added missing initializers for these structs.
    
    Without this change the build fails with GCC compiler errors like:
    ../../third_party/blink/renderer/core/dom/node.h:1261:5: error: 'constexpr' static data member 'kParentNodeTag' must have an initializer
    
    Bug: 41455655
    Change-Id: Id273e4f85b6052dd28bd36003e8a2b6ea8dd061c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6867590
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1505983}

--- third_party/blink/renderer/core/dom/node.h
+++ third_party/blink/renderer/core/dom/node.h
@@ -1249,9 +1249,9 @@ class CORE_EXPORT Node : public EventTarget {
 
  private:
   static constexpr struct ParentNodeTag {
-  } kParentNodeTag;
+  } kParentNodeTag{};
   static constexpr struct ShadowHostTag {
-  } kShadowHostTag;
+  } kShadowHostTag{};
 
   using TaggedParentOrShadowHostNode =
       subtle::TaggedUncompressedMember<Node, ParentNodeTag, ShadowHostTag>;
