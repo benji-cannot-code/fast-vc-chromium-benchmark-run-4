0823c3f4064e3c3c184749babde4a863ca48f4c9
Foromo Daniel Soromou <koretadaniel@chromium.org>
2025-08-25 09:12:07 -0700

    Ensure to reopen mandatory reauth bubble on click
    We previously assumed that clicking on the mandatory reauth page action
    should not do nothing.
    
    The legacy page action trigger the bubble through a command id. This CL
    updates the new framework code to explicitly trigger the bubble display.
    
    Bug: 435463095
    Change-Id: Ibb35e5b25a109687d054e11f1656474a4d6e6cfa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6875848
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Daniel Soromou <koretadaniel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1505909}

--- chrome/browser/ui/browser_actions.cc
+++ chrome/browser/ui/browser_actions.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/actions/chrome_actions.h"
 #include "chrome/browser/ui/autofill/address_bubbles_icon_controller.h"
 #include "chrome/browser/ui/autofill/autofill_bubble_base.h"
+#include "chrome/browser/ui/autofill/payments/mandatory_reauth_bubble_controller_impl.h"
 #include "chrome/browser/ui/autofill/payments/save_payment_icon_controller.h"
 #include "chrome/browser/ui/bookmarks/bookmark_utils.h"
 #include "chrome/browser/ui/browser.h"
@@ -423,13 +424,19 @@ void BrowserActions::InitializeBrowserActions() {
               ui::ImageModel::FromVectorIcon(vector_icons::kShoppingmodeIcon))
           .Build());
 
-  // Clicking the Mandatory Reauth page action is a no-op. This is because the
-  // icon is always shown with a dialog bubble. The expected behavior is to
-  // simply close this bubble, which happens automatically due to focus change
-  // when the user clicks the icon. Therefore, a `base::DoNothing()` callback is
-  // used.
   root_action_item_->AddChild(
-      actions::ActionItem::Builder(base::DoNothing())
+      actions::ActionItem::Builder(
+          base::BindRepeating(
+              [](BrowserWindowInterface* bwi, actions::ActionItem* item,
+                 actions::ActionInvocationContext context) {
+                auto* tab_interface = bwi->GetActiveTabInterface();
+                CHECK(tab_interface);
+
+                autofill::MandatoryReauthBubbleControllerImpl::FromWebContents(
+                    tab_interface->GetContents())
+                    ->ShowBubble();
+              },
+              bwi))
           .SetActionId(kActionAutofillMandatoryReauth)
           .SetTooltipText(l10n_util::GetStringUTF16(
               IDS_AUTOFILL_MANDATORY_REAUTH_ICON_TOOLTIP))
