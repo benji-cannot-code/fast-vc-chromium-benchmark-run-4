86a30b4770900bcde02866ad60bc0d0f286cd481
Joemer Ramos <joemerramos@google.com>
2025-08-25 12:48:55 -0700

    [iOS] Creates "New" Badge for AI Hub Entry Point
    Creates "new" badge and makes it visible if criteria is met. This CL
    doesn't account for hiding the badge, color, scaling, etc. which will
    be done in follow up CLs.
    
    UI Screenshot: screen/7ygQcWuYVrSvtKi
    
    Bug: 438956386
    Change-Id: Ib75942de89e7adcebf425bf96a96ed332b74325b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6871514
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@google.com>
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1506061}

--- ios/chrome/browser/intelligence/page_action_menu/ui/page_action_menu_entrypoint_view.h
+++ ios/chrome/browser/intelligence/page_action_menu/ui/page_action_menu_entrypoint_view.h
@@ -15,8 +15,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @interface PageActionMenuEntrypointView
     : ExtendedTouchTargetButton <PageActionMenuEntryPointCommands>
 
+- (instancetype)init NS_UNAVAILABLE;
+- (instancetype)initWithFrame:(CGRect)frame NS_UNAVAILABLE;
+- (instancetype)initWithCoder:(NSCoder*)coder NS_UNAVAILABLE;
+
+// Designated initializer.
+- (instancetype)initWithNewBadgeVisible:(BOOL)isNewBadgeVisible
+    NS_DESIGNATED_INITIALIZER;
+
 // If YES, highlights PageActionMenu entry point. Otherwise, unhighlights.
 - (void)toggleEntryPointHighlight:(BOOL)highlight;
+
+// If YES, enables "New" badge.
+- (void)setNewBadgeVisible:(BOOL)enabled;
 
 @end
 
--- ios/chrome/browser/location_bar/ui_bundled/location_bar_view_controller.mm
+++ ios/chrome/browser/location_bar/ui_bundled/location_bar_view_controller.mm
@@ -298,7 +298,8 @@ const CGFloat kShareIconBalancingHeightPadding = 1;
   }
 
   if (IsPageActionMenuEnabled()) {
-    _pageActionMenuEntrypointView = [[PageActionMenuEntrypointView alloc] init];
+    _pageActionMenuEntrypointView = [[PageActionMenuEntrypointView alloc]
+        initWithNewBadgeVisible:_isAIHubNewBadgeVisible];
     [_pageActionMenuEntrypointView
                addTarget:self
                   action:@selector(handlePageActionMenuEntrypointTapped)
