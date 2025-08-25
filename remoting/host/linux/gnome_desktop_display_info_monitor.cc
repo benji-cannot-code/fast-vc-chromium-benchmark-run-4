71eaff60c7d08e0796666bee01b348772784d51c
Yuwei Huang <yuweih@chromium.org>
2025-08-25 15:23:32 -0700

    [crd host][wayland] Support for layout mode changing and recalculation
    Gnome/mutter uses the logical layout mode by default, meaning the
    monitor offsets are in DIPs as opposed to actual screen pixels. This
    diverges from what the X11 host does and what the client supports. Also,
    this makes scale changing via ApplyMonitorsConfig difficult since the
    monitors would need to be re-layouted to close the gaps or remove
    overlaps (though arguably we still need to do the same thing for layout
    mode changes). So this CL:
    
    1. Adds the layout_mode property to GnomeDisplayConfig, which is loaded
       from the GetCurrentState output and will be set when
       ApplyMonitorsConfig is called.
    2. Implements a SetLayoutMode method for changing the layout mode and
       recalculating the offsets for the new layout mode. The algorithm
       pretty much comes from X11CrtcResizer.
    3. Adds and populates `supported_scales`. This comes from the original
       CL prior to the split. I figure it's easier to keep changes in the
       same file in the same CL.
    
    Bug: 432217140
    Change-Id: Iba8e57f7665b04221d831cf6541a627ac4ec31c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6876414
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Auto-Submit: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1506172}

--- remoting/host/linux/gnome_desktop_display_info_monitor.cc
+++ remoting/host/linux/gnome_desktop_display_info_monitor.cc
@@ -7,14 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/bind.h"
 #include "base/logging.h"
+#include "remoting/base/constants.h"
+#include "remoting/host/linux/gnome_display_config.h"
 
 namespace remoting {
-
-namespace {
-
-constexpr int kDefaultDPI = 96;
-
-}  // namespace
 
 GnomeDesktopDisplayInfoMonitor::GnomeDesktopDisplayInfoMonitor(
     base::WeakPtr<GnomeDisplayConfigDBusClient> display_config_client)
@@ -54,6 +50,8 @@ void GnomeDesktopDisplayInfoMonitor::OnGnomeDisplayConfigReceived(
     GnomeDisplayConfig config) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
+  // This is the mode that the client uses for Linux hosts.
+  config.SwitchLayoutMode(GnomeDisplayConfig::LayoutMode::kPhysical);
   DesktopDisplayInfo info;
   for (const auto& [name, monitor] : config.monitors) {
     const GnomeDisplayConfig::MonitorMode* current_mode =
@@ -67,10 +65,10 @@ void GnomeDesktopDisplayInfoMonitor::OnGnomeDisplayConfigReceived(
     // causes the client to resize the display to the actual screen resolution
     // at 1x scale when "High-DPI mode" is disabled.
     // TODO: crbug.com/431816005 - fix this bug on the host and set the the DPI
-    // to `kDefaultDPI * monitor.scale * text_scaling_factor`.
+    // to `kDefaultDpi * monitor.scale * text_scaling_factor`.
     info.AddDisplay(DisplayGeometry(
         GnomeDisplayConfig::GetScreenId(name), monitor.x, monitor.y,
-        current_mode->width, current_mode->height, kDefaultDPI * monitor.scale,
+        current_mode->width, current_mode->height, kDefaultDpi * monitor.scale,
         /*bpp=*/24, monitor.is_primary, name));
   }
   callback_list_.Notify(info);
