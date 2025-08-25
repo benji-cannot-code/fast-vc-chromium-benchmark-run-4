3af231babe4241cef85391bf91aba9277bdf69fa
Ernest Nguyen Hung <ernn@google.com>
2025-08-24 23:10:27 -0700

    Reland "[UNO-d] Clean up history sync opt-in pill experiment"
    This is a reland of commit ce5b2543a406b6a3ef2f172d3e43da43fa5126a1
    
    Most likely that was a bad revert (this already landed before and never
    caused Builder(s) to fail (was reverted because of a different reason -
    already addressed).
    
    Original change's description:
    > [UNO-d] Clean up history sync opt-in pill experiment
    >
    > This is yet another reland of crrev.com/c/6837526. Pixel tests don't
    > like cherry picking and previously were failing with
    > https://ci.chromium.org/ui/p/chromium/builders/ci/Win10%20Tests%20x64/102915/overview.
    > The diff is expected though and has been already accepted in
    > https://chrome-public-gold.skia.org/search?corpus=gtest-pixeltests&crs=gerrit&issue=6858912&patchsets=2&positive=true&untriaged=false.
    > This aims to work around reference image reverting in pixel tests while
    > cherry picking.
    >
    > Change-Id: Iff5275caa3bb4f46354afb7a7350adcfb427817b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6862655
    > Reviewed-by: Ryan Sultanem <rsult@google.com>
    > Commit-Queue: Ernest Nguyen Hung <ernn@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1503819}
    
    Change-Id: Ia528997e4b1762deaa87a21b7b1a9a1712674f26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6870310
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Commit-Queue: Ernest Nguyen Hung <ernn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1505707}

--- chrome/browser/ui/views/profiles/profile_menu_view_ui_browsertest.cc
+++ chrome/browser/ui/views/profiles/profile_menu_view_ui_browsertest.cc
@@ -232,26 +232,6 @@ const ProfileMenuViewPixelTestParam kPixelTestParams[] = {
             {{switches::kEnableHistorySyncOptinExpansionPill, {}}},
     },
     {
-        .pixel_test_param = {.test_suffix =
-                                 "HistorySyncOptinExperimentNewPromoVariant"},
-        .signin_status = SigninStatusPixelTestParam::kSignedInNoSync,
-        .extra_features_and_params =
-            {{switches::kEnableHistorySyncOptinExpansionPill,
-              {{"history-sync-optin-expansion-pill-option",
-                "browse-across-devices-new-profile-menu-promo-variant"}}}},
-    },
-    {
-        .pixel_test_param =
-            {.test_suffix =
-                 "HistorySyncOptinExperimentNewPromoVariant_DarkTheme",
-             .use_dark_theme = true},
-        .signin_status = SigninStatusPixelTestParam::kSignedInNoSync,
-        .extra_features_and_params =
-            {{switches::kEnableHistorySyncOptinExpansionPill,
-              {{"history-sync-optin-expansion-pill-option",
-                "browse-across-devices-new-profile-menu-promo-variant"}}}},
-    },
-    {
         .pixel_test_param = {.test_suffix = "BatchUploadPromoSingleLocalData"},
         .signin_status = SigninStatusPixelTestParam::kSignedInNoSync,
         .with_local_data = WithLocalData::kSingleLocalData,
@@ -262,16 +242,6 @@ const ProfileMenuViewPixelTestParam kPixelTestParams[] = {
                              .use_dark_theme = true},
         .signin_status = SigninStatusPixelTestParam::kSignedInNoSync,
         .with_local_data = WithLocalData::kMultipleLocalData,
-    },
-    {
-        .pixel_test_param = {.test_suffix =
-                                 "BatchUploadPromoAndHistorySyncOptinPromo"},
-        .signin_status = SigninStatusPixelTestParam::kSignedInNoSync,
-        .with_local_data = WithLocalData::kMultipleLocalData,
-        .extra_features_and_params =
-            {{switches::kEnableHistorySyncOptinExpansionPill,
-              {{"history-sync-optin-expansion-pill-option",
-                "browse-across-devices-new-profile-menu-promo-variant"}}}},
     },
     {
         .pixel_test_param = {.test_suffix = "AvatarSyncPromo"},
