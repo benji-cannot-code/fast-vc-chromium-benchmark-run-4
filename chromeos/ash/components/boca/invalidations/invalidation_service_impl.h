e2e322f4b0b1e49633c0a31b6277318a50e3d2a2
Ahmed Nasr <anasr@google.com>
2025-08-26 12:34:03 -0700

    Pass BocaSessionManager to InvalidationServiceImpl as the delegate.
    Use InvalidationServiceDelegate to handle upload token and OnInvalidationReceived
    implemented by BocaSessionManager. Other consumers such as boca receiver
    can provide their own delegate implementation.
    
    Bug: b:440410639
    Change-Id: Iff025bdcc9f1c3c222b9768ea3e5c3d9e917de6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6871920
    Reviewed-by: April Zhou <aprilzhou@google.com>
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1506704}

--- chromeos/ash/components/boca/invalidations/invalidation_service_impl.h
+++ chromeos/ash/components/boca/invalidations/invalidation_service_impl.h
@@ -7,9 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROMEOS_ASH_COMPONENTS_BOCA_INVALIDATIONS_INVALIDATION_SERVICE_IMPL_H_
 
 #include <memory>
+#include <string>
 
+#include "base/memory/raw_ptr.h"
 #include "base/time/time.h"
-#include "base/types/expected.h"
 #include "chromeos/ash/components/boca/invalidations/fcm_handler.h"
 #include "components/account_id/account_id.h"
 #include "google_apis/common/api_error_codes.h"
@@ -24,20 +25,19 @@ class GCMDriver;
 }
 
 namespace ash::boca {
-class BocaSessionManager;
-class SessionClientImpl;
+
+class InvalidationServiceDelegate;
+
 class InvalidationServiceImpl : public InvalidationsListener,
-                                FCMRegistrationTokenObserver {
+                                public FCMRegistrationTokenObserver {
  public:
   inline static constexpr char kSenderId[] = "947897361853";
   inline static constexpr char kApplicationId[] =
       "com.google.chrome.boca.fcm.invalidations";
+  // `delegate` should outlive the InvalidationServiceImpl instance.
   InvalidationServiceImpl(gcm::GCMDriver* gcm_driver,
                           instance_id::InstanceIDDriver* instance_id_driver,
-                          AccountId account_id,
-                          BocaSessionManager* boca_session_manager_,
-                          SessionClientImpl* session_client_impl_,
-                          std::string base_url);
+                          InvalidationServiceDelegate* delegate);
   ~InvalidationServiceImpl() override;
 
   // InvalidationsListener implementation.
@@ -47,7 +47,7 @@ class InvalidationServiceImpl : public InvalidationsListener,
   void OnFCMRegistrationTokenChanged() override;
 
   void UploadToken();
-  void OnTokenUploaded(base::expected<bool, google_apis::ApiErrorCode> result);
+  void OnTokenUploaded(bool success);
 
   virtual void ShutDown();
 
@@ -56,13 +56,10 @@ class InvalidationServiceImpl : public InvalidationsListener,
  private:
   SEQUENCE_CHECKER(sequence_checker_);
 
-  const std::string school_tools_base_url_;
   net::BackoffEntry upload_retry_backoff_;
   base::OneShotTimer token_refresh_timer_;
   std::unique_ptr<FCMHandler> fcm_handler_;
-  AccountId account_id_;
-  raw_ptr<BocaSessionManager> boca_session_manager_;
-  raw_ptr<SessionClientImpl> session_client_impl_;
+  raw_ptr<InvalidationServiceDelegate> delegate_;
   base::WeakPtrFactory<InvalidationServiceImpl> weak_factory_{this};
 };
 
