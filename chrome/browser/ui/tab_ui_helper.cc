a948c3ea2f21cccaaa501567953d592b9971a699
Linyu He <linyuh@google.com>
2025-08-26 09:37:50 -0700

    Separate public Java and C++ targets in browser/ui/browser_window
    Currently it's not obvious in
    //chrome/browser/ui/browser_window/BUILD.gn that all the public C++
    headers should be available to desktop Android, but not other Android
    builds, i.e., we can't assert(is_desktop_android).
    
    The reasons are:
    * The BUILD file contains both C++ and Java targets;
    * Java has no if-def, so non-desktop-Android also needs access to the
      public Java interfaces. We then supply different Java implementations:
      * Desktop Android gets the real implementation;
      * Non-desktop Android gets a stub.
    
    These details have caused confusion for engineers focusing on C++ code.
    For example, they may think that browser_window/BUILD.gn is universally
    available, but depending on it would break non-desktop-Android since the
    definitions for the public C++ headers are only included in desktop
    Android (browser_window/internal/BUILD.gn asserts is_desktop_android,
    which is correct).
    
    Therefore, this CL moves public Java targets to a different BUILD file
    (browser_window/public/android/BUILD.gn) and asserts is_desktop_android
    in browser_window/BUILD.gn, so that the confusion caused by
    Java-specific details can be avoided in C++.
    
    Note:
    
    An incorrect line at //chrome/browser/ui/BUILD.gn:341 is removed.
    
    The line added "//chrome/browser/ui/browser_window" as a dependency to
    satisfy a conditional #include in tab_ui_helper.cc, but without checking
    "is_desktop_android".
    
    The dependency is already correctly added at
    //chrome/browser/ui/BUILD.gn:667, and tab_ui_helper.cc should use
    "nogncheck" to prevent presubmit errors.
    
    Bug: 440107688
    Change-Id: I48ef554b00b5f12ed89a61db304a5531f9ce68f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6868932
    Commit-Queue: Linyu He <linyuh@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1506570}

--- chrome/browser/ui/tab_ui_helper.cc
+++ chrome/browser/ui/tab_ui_helper.cc
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/resources/grit/ui_resources.h"
 
 #if !BUILDFLAG(IS_ANDROID)
-#include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
+#include "chrome/browser/ui/browser_window/public/browser_window_interface.h"  // nogncheck
 #endif
 
 namespace {
