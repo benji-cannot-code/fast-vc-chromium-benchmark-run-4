04b847f4dc9229c0697c6bdb71d8bd96051431df
Matt Stark <msta@google.com>
2025-08-25 22:13:12 -0700

    Add missing include to fix modules build on android
    Bug: 425527533
    Change-Id: I6a6a696464c9bc6bd51bd273e702a2a6aed838c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6883230
    Auto-Submit: Matt Stark <msta@google.com>
    Owners-Override: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1506303}

--- android_webview/browser/android_protocol_handler.cc
+++ android_webview/browser/android_protocol_handler.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "android_webview/browser/android_protocol_handler.h"
 
 #include <memory>
+#include <string>
 #include <utility>
 
 #include "android_webview/common/url_constants.h"
