bbcfc7645e1998b7ba24af1e7d40e17b705a3641
Jun Ishiguro <junis@google.com>
2025-08-26 02:46:55 -0700

    ash-refactor: Pass local_state to RecordMetricAndClear
    This CL refactors KioskAppLaunchError::RecordMetricAndClear to take
    PrefService& as a parameter instead of directly accessing
    g_browser_process->local_state().
    
    The call sites in KioskAppMenuController and unit tests have been
    updated. A TODO is added to track the removal of the new
    g_browser_process usage in the caller.
    
    This is part of a larger effort to eliminate dependencies on
    BrowserProcess from ash-specific code.
    
    Bug: b:404129026
    Change-Id: I21b15f35543df57e9ea594d8448527f9464a795d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6883710
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1506382}

--- chrome/browser/ash/app_mode/kiosk_app_launch_error.cc
+++ chrome/browser/ash/app_mode/kiosk_app_launch_error.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notreached.h"
 #include "base/values.h"
 #include "chrome/browser/ash/app_mode/kiosk_chrome_app_manager.h"
-#include "chrome/browser/browser_process.h"
 #include "chrome/grit/generated_resources.h"
 #include "chromeos/ash/components/login/auth/public/auth_failure.h"
 #include "components/prefs/scoped_user_pref_update.h"
@@ -130,9 +129,8 @@ KioskAppLaunchError::Error KioskAppLaunchError::Get(
 }
 
 // static
-void KioskAppLaunchError::RecordMetricAndClear() {
-  PrefService* local_state = g_browser_process->local_state();
-  ScopedDictPrefUpdate dict_update(local_state,
+void KioskAppLaunchError::RecordMetricAndClear(PrefService& local_state) {
+  ScopedDictPrefUpdate dict_update(&local_state,
                                    KioskChromeAppManager::kKioskDictionaryName);
 
   std::optional<int> error = dict_update->FindInt(kKeyLaunchError);
--- chrome/browser/ash/app_mode/kiosk_app_launch_error.h
+++ chrome/browser/ash/app_mode/kiosk_app_launch_error.h
@@ -65,7 +65,7 @@ class KioskAppLaunchError {
   static Error Get(const PrefService& local_state);
 
   // Records the launch error and cryptohome auth error metric and clears them.
-  static void RecordMetricAndClear();
+  static void RecordMetricAndClear(PrefService& local_state);
 
   KioskAppLaunchError() = delete;
   KioskAppLaunchError(const KioskAppLaunchError&) = delete;
