5f49d0fbdd3ca5d15f76405a24776bfdaa0a549f
Rouslan Solomakhin <rouslan@google.com>
2025-08-26 07:59:03 -0700

    Remove the expiring histogram for measuring service binding
    This patch removes the following histogram:
    OBSOLETE_HISTOGRAM[PaymentRequest.PaymentDetailsUpdateService.Bind]=The histogram is expired and no longer needed.
    
    Fixed: 433258754
    Change-Id: I59b6cd9187f79faef64ac3cb1df1224890583ffe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6880201
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1506498}

--- components/payments/content/android/java/src/org/chromium/components/payments/PaymentDetailsUpdateService.java
+++ components/payments/content/android/java/src/org/chromium/components/payments/PaymentDetailsUpdateService.java
@@ -11,7 +11,6 @@ import android.os.Binder;
 import android.os.Bundle;
 import android.os.IBinder;
 
-import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.task.PostTask;
 import org.chromium.base.task.TaskTraits;
 import org.chromium.build.annotations.NullMarked;
@@ -80,8 +79,6 @@ public class PaymentDetailsUpdateService extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
-        RecordHistogram.recordBooleanHistogram(
-                "PaymentRequest.PaymentDetailsUpdateService.Bind", true);
         return mBinder;
     }
 
