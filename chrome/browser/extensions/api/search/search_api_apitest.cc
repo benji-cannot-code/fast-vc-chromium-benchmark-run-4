960f16a57d3ad83b2847b43d35f8a17f2a1c9b67
Zhengzheng Liu <zhzhliu@chromium.org>
2025-08-26 16:46:47 -0700

    extensions: Porting chrome.search on desktop Android - part 4
    This CL enables SearchApiTest.Normal on Mac and SearchApiTest.Incognito
    on Ozone.
    While all apitest can not be enabled on desktop Android due to the
    dependency on chrome.tabs.
    
    Bug: 434262354
    Change-Id: I20c59ee85d1ee11a1580c00e37cf6ae84dd077c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6885838
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Zhengzheng Liu <zhzhliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1506842}

--- chrome/browser/extensions/api/search/search_api_apitest.cc
+++ chrome/browser/extensions/api/search/search_api_apitest.cc
@@ -6,9 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/browser.h"
+#include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test.h"
+#include "extensions/buildflags/buildflags.h"
 #include "extensions/test/result_catcher.h"
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace extensions {
 
@@ -16,27 +19,21 @@ namespace {
 
 using SearchApiTest = ExtensionApiTest;
 
+// TODO(crbug.com/434262354):Enable this test once chrome.tabs(create, query,
+// onUpdated, etc.) is supported on desktop Android.
+#if BUILDFLAG(ENABLE_EXTENSIONS)
 // Test various scenarios, such as the use of input different parameters.
-// Disabled due to flakes on Mac testers; see https://crbug.com/394345948.
-#if BUILDFLAG(IS_MAC)
-#define MAYBE_Normal DISABLED_Normal
-#else
-#define MAYBE_Normal Normal
-#endif
-IN_PROC_BROWSER_TEST_F(SearchApiTest, MAYBE_Normal) {
+IN_PROC_BROWSER_TEST_F(SearchApiTest, Normal) {
   ASSERT_TRUE(RunExtensionTest("search/query/normal")) << message_;
 }
 
 // Test incognito browser in extension default spanning mode.
-// Disabled due to flakes on Ozone testers; see https://crbug.com/1188651.
-#if BUILDFLAG(IS_OZONE)
-#define MAYBE_Incognito DISABLED_Incognito
-#else
-#define MAYBE_Incognito Incognito
-#endif
-IN_PROC_BROWSER_TEST_F(SearchApiTest, MAYBE_Incognito) {
+IN_PROC_BROWSER_TEST_F(SearchApiTest, Incognito) {
   ResultCatcher catcher;
-  CreateIncognitoBrowser(profile());
+  auto* incognito_web_contents =
+      PlatformOpenURLOffTheRecord(profile(), GURL("about:blank"));
+  auto* incognito_context = incognito_web_contents->GetBrowserContext();
+  ASSERT_TRUE(incognito_context->IsOffTheRecord());
   ASSERT_TRUE(RunExtensionTest("search/query/incognito", {},
                                {.allow_in_incognito = true}))
       << message_;
@@ -47,11 +44,15 @@ IN_PROC_BROWSER_TEST_F(SearchApiTest, IncognitoSplit) {
   ResultCatcher catcher;
   catcher.RestrictToBrowserContext(
       profile()->GetPrimaryOTRProfile(/*create_if_needed=*/true));
-  CreateIncognitoBrowser(profile());
+  auto* incognito_web_contents =
+      PlatformOpenURLOffTheRecord(profile(), GURL("about:blank"));
+  auto* incognito_context = incognito_web_contents->GetBrowserContext();
+  ASSERT_TRUE(incognito_context->IsOffTheRecord());
   ASSERT_TRUE(RunExtensionTest("search/query/incognito_split", {},
                                {.allow_in_incognito = true}))
       << message_;
 }
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
 }  // namespace
 }  // namespace extensions
