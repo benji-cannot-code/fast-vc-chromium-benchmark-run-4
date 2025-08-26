f03bbe7218aece53bad5bd66865a5586e3868778
Yuwei Huang <yuweih@chromium.org>
2025-08-25 22:17:57 -0700

    [crd host][wayland] Display removal
    This CL updates GnomeDesktopResizer::SetVideoLayout to support display
    removal when the client's requested video layout no longer has a
    specific screen ID.
    
    It turns out simply stopping and destroying the pipewire stream would
    not remove the virtual monitor. To do so, I have to call the screencast
    Stop method.
    
    Bug: 432217140
    Change-Id: I04d359dff0df5be56634534ca5dc7c914c8bc76c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6880776
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1506307}

--- remoting/host/linux/gnome_desktop_resizer.cc
+++ remoting/host/linux/gnome_desktop_resizer.cc
@@ -5,7 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "remoting/host/linux/gnome_desktop_resizer.h"
 
+#include <functional>
+
 #include "base/check.h"
+#include "base/containers/flat_set.h"
 #include "base/functional/bind.h"
 #include "base/location.h"
 #include "base/logging.h"
@@ -20,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/host/linux/gnome_interaction_strategy.h"
 #include "remoting/host/linux/pipewire_capture_stream.h"
 #include "remoting/host/linux/pipewire_capture_stream_manager.h"
+#include "third_party/webrtc/modules/desktop_capture/desktop_capture_types.h"
 #include "third_party/webrtc/modules/desktop_capture/desktop_geometry.h"
 #include "ui/base/glib/gsettings.h"
 
@@ -135,13 +139,24 @@ void GnomeDesktopResizer::SetVideoLayout(const protocol::VideoLayout& layout) {
   if (!stream_manager_) {
     return;
   }
+  // TODO: crbug.com/432217140 - Implement support for change of primary
+  // display, monitor offsets and scales.
+  auto unseen_screen_ids = base::MakeFlatSet<webrtc::ScreenId>(
+      stream_manager_->GetActiveStreams(), std::less<>(),
+      [](const auto& kv) { return kv.first; });
   for (const auto& track : layout.video_track()) {
     if (!track.has_screen_id()) {
       stream_manager_->AddStream(
           {{track.width(), track.height()}, {track.x_dpi(), track.y_dpi()}},
           base::BindOnce(&GnomeDesktopResizer::OnAddStreamResult,
                          weak_ptr_factory_.GetWeakPtr()));
+    } else if (unseen_screen_ids.erase(track.screen_id()) == 0) {
+      LOG(ERROR) << "Found unexpected screen ID: " << track.screen_id();
     }
+  }
+  // Remove pipewire streams that are no longer in the video layout.
+  for (const auto& screen_id : unseen_screen_ids) {
+    stream_manager_->RemoveStream(screen_id);
   }
 }
 
--- remoting/host/linux/pipewire_capture_stream_manager.h
+++ remoting/host/linux/pipewire_capture_stream_manager.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "base/observer_list_types.h"
 #include "base/types/expected.h"
+#include "remoting/host/base/loggable.h"
 #include "remoting/host/base/screen_resolution.h"
 #include "remoting/host/linux/gdbus_connection_ref.h"
 #include "remoting/host/linux/gnome_display_config_dbus_client.h"
@@ -113,6 +114,16 @@ class PipewireCaptureStreamManager final {
     AddStreamCallback callback;
   };
 
+  struct StreamInfo {
+    StreamInfo();
+    StreamInfo(StreamInfo&&);
+    StreamInfo& operator=(StreamInfo&&);
+    ~StreamInfo();
+
+    std::unique_ptr<PipewireCaptureStream> stream;
+    gvariant::ObjectPath stream_path;
+  };
+
   template <typename SuccessType, typename String>
   GDBusConnectionRef::CallCallback<SuccessType> CheckAddStreamResultAndContinue(
       void (PipewireCaptureStreamManager::*success_method)(SuccessType),
@@ -133,6 +144,8 @@ class PipewireCaptureStreamManager final {
   void OnStreamCreated(std::tuple<gvariant::ObjectPath> args);
   void OnStreamParameters(GVariantRef<"a{sv}"> parameters);
   void OnStreamStarted(std::tuple<> args);
+  void OnStreamStopped(webrtc::ScreenId screen_id,
+                       base::expected<std::tuple<>, Loggable> result);
   void OnPipeWireStreamAdded(std::string mapping_id,
                              std::tuple<std::uint32_t> args);
   void QueryDisplayInfo();
@@ -156,8 +169,8 @@ class PipewireCaptureStreamManager final {
   // nullopt if not display config has been loaded yet.
   std::optional<GnomeDisplayConfig> last_seen_display_config_
       GUARDED_BY_CONTEXT(sequence_checker_);
-  base::flat_map<webrtc::ScreenId, std::unique_ptr<PipewireCaptureStream>>
-      streams_ GUARDED_BY_CONTEXT(sequence_checker_);
+  base::flat_map<webrtc::ScreenId, StreamInfo> streams_
+      GUARDED_BY_CONTEXT(sequence_checker_);
 
   // Queue to allow streams to be added one at a time, which is crucial to
   // ensure the stream is associated with the correct screen ID.
