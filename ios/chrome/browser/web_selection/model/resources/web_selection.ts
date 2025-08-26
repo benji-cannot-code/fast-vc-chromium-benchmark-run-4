3865da795e5300cb5454fc375601fad2796423af
Alexandra Pereira <alexandrasp@google.com>
2025-08-26 10:53:04 -0700

    [iOS][gCrWeb] Migrate web_selection.ts to use new gCrWeb API
    Update web selection TypeScript resource to use the new gCrWeb class
    and its API.  No functional change is intended.
    
    Bug: 399666983
    Change-Id: I335fe1ebba4c6b61f0058d2214e4812259d1f592
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6872196
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Alexandra Pereira <alexandrasp@google.com>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1506648}

--- ios/chrome/browser/web_selection/model/resources/web_selection.ts
+++ ios/chrome/browser/web_selection/model/resources/web_selection.ts
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {gCrWebLegacy} from '//ios/web/public/js_messaging/resources/gcrweb.js';
+import {CrWebApi, gCrWeb} from '//ios/web/public/js_messaging/resources/gcrweb.js';
 import {sendWebKitMessage} from '//ios/web/public/js_messaging/resources/utils.js';
 
 /**
@@ -59,8 +59,6 @@ function getSelectedTextWithOffset(offsetX: number, offsetY: number) {
   });
 }
 
-gCrWebLegacy.webSelection = {getSelectedText};
-
 window.addEventListener('message', function(message) {
   const payload = message.data;
   if (!payload ||
@@ -75,3 +73,9 @@ window.addEventListener('message', function(message) {
   const y = payload.offsetY;
   getSelectedTextWithOffset(x, y);
 });
+
+const webSelection = new CrWebApi();
+
+webSelection.addFunction('getSelectedText', getSelectedText);
+
+gCrWeb.registerApi('webSelection', webSelection);
