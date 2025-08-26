c158be9616bc304bcd0fef280dab3e22bfc33746
Robbie Gibson <rkgibson@google.com>
2025-08-26 11:48:56 -0700

    [iOS] Load user images on a background thread
    Before this CL, UserUploadedImageManager loaded images from disk
    synchronously. However, this is not great, as disk interactions could
    take some time and shouldn't block the UI.
    
    This CL modifies the method to load asynchronously. As part of this,
    it also changes the priority of the task runner to USER_VISIBLE, so
    images load quickly.
    
    Fixed: 438219461
    Change-Id: I3eda38a97e299a4cf42fec797148cca99f678932
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6879691
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1506678}

--- ios/chrome/browser/ntp/ui_bundled/new_tab_page_mediator.mm
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_mediator.mm
@@ -37,10 +37,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/discover_feed/model/discover_feed_service.h"
 #import "ios/chrome/browser/discover_feed/model/discover_feed_service_factory.h"
 #import "ios/chrome/browser/discover_feed/model/discover_feed_visibility_browser_agent.h"
+#import "ios/chrome/browser/home_customization/coordinator/home_customization_data_conversion.h"
 #import "ios/chrome/browser/home_customization/model/home_background_customization_service.h"
 #import "ios/chrome/browser/home_customization/model/home_background_customization_service_observer_bridge.h"
 #import "ios/chrome/browser/home_customization/model/home_background_data.h"
 #import "ios/chrome/browser/home_customization/model/user_uploaded_image_manager.h"
+#import "ios/chrome/browser/home_customization/ui/home_customization_framing_coordinates.h"
 #import "ios/chrome/browser/metrics/model/new_tab_page_uma.h"
 #import "ios/chrome/browser/ntp/model/new_tab_page_state.h"
 #import "ios/chrome/browser/ntp/model/new_tab_page_tab_helper.h"
@@ -415,6 +417,10 @@ void LogLensButtonNewBadgeShownHistogram(IOSNTPNewBadgeShownResult result) {
   [traitAccessor
       setBoolForNewTabPageImageBackgroundTrait:customBackground.has_value()];
   if (customBackground) {
+    // Clear background so old state doesn't show. It will be set to the new
+    // background later.
+    [self.consumer setBackgroundImage:nil];
+
     if (std::holds_alternative<sync_pb::NtpCustomBackground>(
             customBackground.value())) {
       sync_pb::NtpCustomBackground background =
@@ -440,8 +446,17 @@ void LogLensButtonNewBadgeShownHistogram(IOSNTPNewBadgeShownResult result) {
     } else {
       HomeUserUploadedBackground userBackground =
           std::get<HomeUserUploadedBackground>(customBackground.value());
-      [self handleUserUploadedBackground:userBackground.image_path
-                      framingCoordinates:userBackground.framing_coordinates];
+
+      HomeCustomizationFramingCoordinates* framingCoordinates =
+          HomeCustomizationFramingCoordinatesFromFramingCoordinates(
+              userBackground.framing_coordinates);
+      __weak __typeof(self) weakSelf = self;
+
+      _userUploadedImageManager->LoadUserUploadedImage(
+          base::FilePath(userBackground.image_path),
+          base::BindOnce(^(UIImage* image) {
+            [weakSelf applyFramingCoordinates:framingCoordinates toImage:image];
+          }));
     }
   } else {
     [self.consumer setBackgroundImage:nil];
@@ -648,11 +663,10 @@ void LogLensButtonNewBadgeShownHistogram(IOSNTPNewBadgeShownResult result) {
 
 // Helper method to handle displaying a user-uploaded background image
 // with the specified framing coordinates.
-- (void)handleUserUploadedBackground:(const std::string&)imagePath
-                  framingCoordinates:(const FramingCoordinates&)coordinates {
-  UIImage* image = _userUploadedImageManager->LoadUserUploadedImage(
-      base::FilePath(imagePath));
-  if (!image) {
+- (void)applyFramingCoordinates:
+            (HomeCustomizationFramingCoordinates*)coordinates
+                        toImage:(UIImage*)originalImage {
+  if (!originalImage) {
     // Clear the corrupted data.
     _backgroundCustomizationService->ClearCurrentUserUploadedBackground();
     _backgroundCustomizationService->StoreCurrentTheme();
@@ -660,17 +674,6 @@ void LogLensButtonNewBadgeShownHistogram(IOSNTPNewBadgeShownResult result) {
     return;
   }
 
-  // Apply framing coordinates to frame the image.
-  UIImage* framedImage = [self applyFramingCoordinates:coordinates
-                                               toImage:image];
-
-  [self.consumer setBackgroundImage:framedImage];
-}
-
-// Helper method to apply framing coordinates to position
-// the user-uploaded background image.
-- (UIImage*)applyFramingCoordinates:(const FramingCoordinates&)coordinates
-                            toImage:(UIImage*)originalImage {
   // Create a canvas the size of the view.
   CGSize canvasSize = [UIScreen mainScreen].bounds.size;
 
@@ -683,8 +686,8 @@ void LogLensButtonNewBadgeShownHistogram(IOSNTPNewBadgeShownResult result) {
   CGFloat scaledHeight = originalImage.size.height * scale;
 
   // Use negative offset to position the image so the framed area is visible.
-  CGFloat offsetX = -(coordinates.x * scale);
-  CGFloat offsetY = -(coordinates.y * scale);
+  CGFloat offsetX = -(coordinates.visibleRect.origin.x * scale);
+  CGFloat offsetY = -(coordinates.visibleRect.origin.y * scale);
 
   UIGraphicsImageRendererFormat* format =
       [[UIGraphicsImageRendererFormat alloc] init];
@@ -701,7 +704,7 @@ void LogLensButtonNewBadgeShownHistogram(IOSNTPNewBadgeShownResult result) {
             drawInRect:CGRectMake(offsetX, offsetY, scaledWidth, scaledHeight)];
       }];
 
-  return framedImage;
+  [self.consumer setBackgroundImage:framedImage];
 }
 
 @end
