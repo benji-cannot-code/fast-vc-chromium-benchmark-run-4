9cda3b311d961e6e6bc1c0537e6df480ae969a5b
Brandon Fong <bjfong@google.com>
2025-08-25 18:54:03 -0700

    [hub] Put hub search usage for tab groups pane behind a flag param
    Place hub search entrypoints for the tab groups pane behind a param.
    
    Bug: 441099542
    Change-Id: Id63433a6ddaee04ad635d59b09f1cb881f99d621
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6879651
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1506260}

--- chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubToolbarView.java
+++ chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubToolbarView.java
@@ -109,7 +109,11 @@ public class HubToolbarView extends LinearLayout {
     }
 
     void setMenuButtonVisible(boolean visible) {
-        mMenuButtonWrapper.setVisibility(visible ? View.VISIBLE : View.INVISIBLE);
+        if (OmniboxFeatures.sAndroidHubSearchEnableOnTabGroupsPane.getValue()) {
+            mMenuButtonWrapper.setVisibility(visible ? View.VISIBLE : View.INVISIBLE);
+        } else {
+            mMenuButtonContainer.setVisibility(visible ? View.VISIBLE : View.INVISIBLE);
+        }
     }
 
     void setPaneSwitcherButtonData(
--- chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubToolbarViewUnitTest.java
+++ chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubToolbarViewUnitTest.java
@@ -40,6 +40,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 import androidx.core.content.ContextCompat;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
@@ -72,6 +73,7 @@ import org.chromium.chrome.browser.hub.HubToolbarProperties.PaneButtonLookup;
 import org.chromium.chrome.browser.toolbar.menu_button.MenuButton;
 import org.chromium.components.browser_ui.styles.SemanticColorUtils;
 import org.chromium.components.omnibox.OmniboxFeatureList;
+import org.chromium.components.omnibox.OmniboxFeatures;
 import org.chromium.ui.base.TestActivity;
 import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.ui.modelutil.PropertyModelChangeProcessor;
@@ -115,6 +117,7 @@ public class HubToolbarViewUnitTest {
     private FrameLayout mToolbarContainer;
     private Button mActionButton;
     private TabLayout mPaneSwitcher;
+    private LinearLayout mMenuButtonContainer;
     private MenuButton mMenuButtonWrapper;
     private View mSearchBox;
     private View mSearchLoupe;
@@ -139,6 +142,7 @@ public class HubToolbarViewUnitTest {
         mToolbarContainer = (FrameLayout) inflater.inflate(layoutId, null, false);
         mActionButton = mToolbarContainer.findViewById(R.id.toolbar_action_button);
         mPaneSwitcher = mToolbarContainer.findViewById(R.id.pane_switcher);
+        mMenuButtonContainer = mToolbarContainer.findViewById(R.id.menu_button_container);
         mMenuButtonWrapper = mToolbarContainer.findViewById(R.id.menu_button_wrapper);
         mSearchBox = mToolbarContainer.findViewById(R.id.search_box);
         mSearchLoupe = mToolbarContainer.findViewById(R.id.search_loupe);
@@ -236,7 +240,18 @@ public class HubToolbarViewUnitTest {
     }
 
     @Test
-    public void testMenuButtonVisibility() {
+    public void testMenuButtonContainerVisibility() {
+        OmniboxFeatures.sAndroidHubSearchEnableOnTabGroupsPane.setForTesting(false);
+        mPropertyModel.set(MENU_BUTTON_VISIBLE, false);
+        assertEquals(View.INVISIBLE, mMenuButtonContainer.getVisibility());
+
+        mPropertyModel.set(MENU_BUTTON_VISIBLE, true);
+        assertEquals(View.VISIBLE, mMenuButtonContainer.getVisibility());
+        OmniboxFeatures.sAndroidHubSearchEnableOnTabGroupsPane.setForTesting(true);
+    }
+
+    @Test
+    public void testMenuButtonWrapperVisibility() {
         mPropertyModel.set(MENU_BUTTON_VISIBLE, false);
         assertEquals(View.INVISIBLE, mMenuButtonWrapper.getVisibility());
 
