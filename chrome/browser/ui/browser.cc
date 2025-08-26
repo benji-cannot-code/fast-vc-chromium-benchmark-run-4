0adcab3a5a110b7f6e019f7480b0e16fbb0864c3
Paul Jensen <pauljensen@chromium.org>
2025-08-26 10:34:41 -0700

    Remove unused PreHandleMouseEvent
    This method on Browser, BrowserWindow, WebContentsDelegate etc always
    returned false in all implementations.
    Rather than implement it for WebUIBrowserWindow (it was generating
    *a lot* of NOTIMPLEMENTED() debug messages) I figure removing it is
    better for overall code health.
    
    Bug: 393144165, 394367683
    Change-Id: I1bafa47ae78132e4d7e67d7b63afe4910e840821
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6880411
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Auto-Submit: Paul Jensen <pauljensen@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1506627}

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -1820,11 +1820,6 @@ void Browser::SetFocusToLocationBar() {
   window_->SetFocusToLocationBar(false);
 }
 
-bool Browser::PreHandleMouseEvent(content::WebContents* source,
-                                  const blink::WebMouseEvent& event) {
-  return window()->PreHandleMouseEvent(event);
-}
-
 void Browser::PreHandleDragUpdate(const content::DropData& drop_data,
                                   const gfx::PointF& client_pt) {
   window()->PreHandleDragUpdate(drop_data, client_pt);
--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -3455,10 +3455,6 @@ void BrowserView::ShowAppMenu() {
       ->Activate(nullptr);
 }
 
-bool BrowserView::PreHandleMouseEvent(const blink::WebMouseEvent& event) {
-  return false;
-}
-
 content::KeyboardEventProcessingResult BrowserView::PreHandleKeyboardEvent(
     const NativeWebKeyboardEvent& event) {
   if ((event.GetType() != blink::WebInputEvent::Type::kRawKeyDown) &&
