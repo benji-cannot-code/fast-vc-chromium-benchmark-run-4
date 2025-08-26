b1e87a03cd7495fd6da65c69367f55ddb694a940
Fiaz Muhammad <mfiaz@google.com>
2025-08-26 08:29:41 -0700

    Rename FindTabModelWithId
    This change renames FindTabModelWithId to
    FindTabModelWithWindowSessionId to be more clear.
    
    Bug: 440045719
    Change-Id: I097c55f42ad9a16294958890ce92a210a55d67c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6879408
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: David Trainor <dtrainor@chromium.org>
    Auto-Submit: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1506515}

--- chrome/browser/sync/glue/synced_tab_delegate_android.cc
+++ chrome/browser/sync/glue/synced_tab_delegate_android.cc
@@ -86,7 +86,7 @@ SyncedTabDelegateAndroid::ReadPlaceholderTabSnapshotIfItShouldSync(
     }
   }
 
-  const TabModel* tab_model = TabModelList::FindTabModelWithId(
+  const TabModel* tab_model = TabModelList::FindTabModelWithWindowSessionId(
       tab_android_data_provider_->GetWindowId());
   DCHECK(tab_model);
   Profile* profile = tab_model->GetProfile();
