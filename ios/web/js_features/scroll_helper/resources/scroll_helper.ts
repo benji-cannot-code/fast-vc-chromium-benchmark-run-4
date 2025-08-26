21555d6ffb8c7cf222ac2eec3cbea0ea7c244063
Alexandra Pereira <alexandrasp@google.com>
2025-08-26 08:55:32 -0700

    [iOS][gCrWeb] Update scroll_helper Typescript feature
    Update the scroll_helper Typescript feature so it's wrapped by and
    accessible via the new CrWeb API. No functional change is intended.
    
    Bug: 399666983
    Change-Id: Ifdd8c8f8214a17f072aa2050b9f810d02c550125
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6788535
    Commit-Queue: Alexandra Pereira <alexandrasp@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1506537}

--- ios/web/js_features/scroll_helper/resources/scroll_helper.ts
+++ ios/web/js_features/scroll_helper/resources/scroll_helper.ts
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {gCrWebLegacy} from '//ios/web/public/js_messaging/resources/gcrweb.js';
+import {CrWebApi, gCrWeb} from '//ios/web/public/js_messaging/resources/gcrweb.js';
 
 /**
  * @fileoverview APIs used for the scroll workaround. See crbug.com/554257.
@@ -11,13 +11,18 @@ import {gCrWebLegacy} from '//ios/web/public/js_messaging/resources/gcrweb.js';
 
 let webViewScrollViewIsDragging = false;
 
+const scrollHelperApi = new CrWebApi();
+
 /**
  * Tracks whether user is in the middle of scrolling/dragging. If user is
  * scrolling, ignore window.scrollTo() until user stops scrolling.
  */
-gCrWebLegacy.setWebViewScrollViewIsDragging = function(state: boolean) {
-  webViewScrollViewIsDragging = state;
-};
+scrollHelperApi.addFunction(
+    'setWebViewScrollViewIsDragging', (state: boolean) => {
+      webViewScrollViewIsDragging = state;
+    });
+
+gCrWeb.registerApi('scrollHelper', scrollHelperApi);
 
 const originalWindowScrollTo_: Function = window.scrollTo;
 
