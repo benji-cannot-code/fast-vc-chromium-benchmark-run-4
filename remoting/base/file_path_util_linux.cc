3d3c618c16110e345e4f6085c6d2f127b32919e2
Elly <ellyjones@chromium.org>
2025-08-26 16:32:10 -0700

    remoting: base/hash/md5 -> crypto/obsolete/md5
    Despite the name, crypto/obsolete/md5 is the new md5 API, and the
    base/hash one is deprecated.
    
    Bug: 406729261
    Change-Id: Ia5c1bfb9f07189ddfd171becf661964df47fccaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6886030
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1506838}

--- remoting/base/file_path_util_linux.cc
+++ remoting/base/file_path_util_linux.cc
@@ -6,8 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/base/file_path_util_linux.h"
 
 #include "base/base_paths.h"
-#include "base/hash/md5.h"
 #include "base/path_service.h"
+#include "base/strings/string_number_conversions.h"
+#include "base/strings/string_util.h"
+#include "crypto/obsolete/md5.h"
 #include "net/base/network_interfaces.h"
 
 namespace remoting {
@@ -19,7 +21,8 @@ base::FilePath GetConfigDirectoryPath() {
 }
 
 std::string GetHostHash() {
-  return "host#" + base::MD5String(net::GetHostName());
+  return "host#" + base::ToLowerASCII(base::HexEncode(
+                       crypto::obsolete::Md5::Hash(net::GetHostName())));
 }
 
 }  // namespace remoting
