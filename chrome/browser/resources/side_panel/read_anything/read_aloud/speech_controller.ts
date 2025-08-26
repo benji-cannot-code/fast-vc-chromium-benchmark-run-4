c597a0c619457f49929d21f42c5b3dbeffe2bc7f
Lauren Winston <lwinston@google.com>
2025-08-25 17:09:55 -0700

    [Read aloud] Genrify initialization methods in TS.
    This might not be the final implementation, but this
    generifies the init and isInitialized methods enough
    that they would be able to be used with both the TS and
    V8 implementations
    
    Bug: 440400392
    Change-Id: I2cffaab948f967c30f094d8359c9aeedff9a9801
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6882457
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1506218}

--- chrome/browser/resources/side_panel/read_anything/read_aloud/speech_controller.ts
+++ chrome/browser/resources/side_panel/read_anything/read_aloud/speech_controller.ts
@@ -122,7 +122,7 @@ export class SpeechController {
   }
 
   isSpeechTreeInitialized(): boolean {
-    return this.readAloudModel_.isSpeechTreeInitialized();
+    return this.readAloudModel_.isInitialized();
   }
 
   isPausedFromButton(): boolean {
@@ -175,7 +175,7 @@ export class SpeechController {
 
     // TODO: crbug.com/40927698 - This step should be skipped on migrating to
     // a non-AXPosition-based text segmentation strategy.
-    this.readAloudModel_.onFirstTextNode(new AxReadAloudNode(firstTextNode));
+    this.readAloudModel_.init(new AxReadAloudNode(firstTextNode));
   }
 
   onSelectionChange() {
