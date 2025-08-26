ea63a7ac1b1365fe4ed00acbb7d4596b4a093b08
AbdAlRahman Gad <agad@igalia.com>
2025-08-26 06:18:29 -0700

    Fix dangling pointer issues in WaylandBufferManagerHost and GLOzoneEGLWayland
    Bug: 324294360
    Change-Id: I6526fdce3dbbc2388654a0acebf509dde8f5f1dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6827269
    Reviewed-by: Max Ihlenfeldt <max@igalia.com>
    Commit-Queue: AbdAlRahman Gad <agad@igalia.com>
    Reviewed-by: Orko Garai <orko@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1506450}

--- ui/ozone/platform/wayland/wayland_buffer_manager_unittest.cc
+++ ui/ozone/platform/wayland/wayland_buffer_manager_unittest.cc
@@ -194,8 +194,12 @@ class WaylandBufferManagerTest : public WaylandTest {
                 manager_host_->SetTerminateGpuCallback(callback->Get());
 
                 auto interface_ptr = manager_host_->BindInterface();
-                // Recreate the gpu side manager (the production code does the
-                // same).
+
+                // Reset the surface factory's buffer manager to avoid a
+                // dangling pointer.
+                surface_factory_->SetBufferManagerForTesting(nullptr);
+                // Recreate the gpu side manager (the production code does
+                // the same).
                 buffer_manager_gpu_ =
                     std::make_unique<WaylandBufferManagerGpu>();
                 buffer_manager_gpu_->Initialize(
@@ -205,6 +209,8 @@ class WaylandBufferManagerTest : public WaylandTest {
                     /*supports_acquire_fence=*/false,
                     /*supports_overlays=*/true,
                     /*supports_single_pixel_buffer=*/true);
+                surface_factory_->SetBufferManagerForTesting(
+                    buffer_manager_gpu_.get());
               });
     }
   }
