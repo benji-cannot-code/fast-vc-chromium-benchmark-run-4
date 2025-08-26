96fba2914112423fc1d784df705f9fb59866877b
Ananya <ananyaseelam@google.com>
2025-08-26 11:55:08 -0700

    [context-input] Add ContextualInput and ContextualInputData structs
    This CL creates new `ContextualInput` and `ContextualInputData` structs
    which can be used across the lens and composebox query controllers. The
    `ContextualInput` struct copies over the code from `lens::PageContent`.
    The `ContextualInputData` struct contains a `ContextualInput` along with
    other useful content information that will be later be used to replace
    the `file_data` argument in the lens and composebox query controllers
    `StartFileUploadFlow` method.
    
    Bug: 441058721
    Change-Id: I0545f864a41cf9b23feb8bcc1b7c7a4914eb38e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6881528
    Commit-Queue: Ananya Seelam <ananyaseelam@google.com>
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1506680}

---
+++ components/lens/contextual_input.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/lens/contextual_input.h"
+
+namespace lens {
+
+ContextualInput::ContextualInput() : content_type_(lens::MimeType::kUnknown) {}
+ContextualInput::ContextualInput(std::vector<uint8_t> bytes,
+                                 lens::MimeType content_type)
+    : bytes_(bytes), content_type_(content_type) {}
+ContextualInput::ContextualInput(const ContextualInput& other) = default;
+ContextualInput::~ContextualInput() = default;
+
+ContextualInputData::ContextualInputData() = default;
+ContextualInputData::~ContextualInputData() = default;
+ContextualInputData::ContextualInputData(const ContextualInputData&) = default;
+
+}  // namespace lens
