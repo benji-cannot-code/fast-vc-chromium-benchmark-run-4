3f73d402fad3e76056d4e367016af30a9d3be81e
Shunya Shishido <sisidovski@chromium.org>
2025-08-26 05:42:26 -0700

    Make LoadingBehaviorFlag a typed enum and add operators
    This change improves the type safety and usability of the
    LoadingBehaviorFlag enum by:
    
    1.  Explicitly setting the underlying type to `uint32_t`.
    2.  Introducing `operator|` and `operator|=` overloads.
    
    These changes allow for direct bitwise operations on LoadingBehaviorFlag
    enum values without requiring explicit static_casts, making the code
    in `document_loader.cc` cleaner and less error-prone.
    
    This is a preliminary for synthetic response UMA fix.
    
    Change-Id: I5d2571c89b7f671dd51cf32d1d26db8e2d35fd2d
    Bug: 352578800
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6882717
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Shunya Shishido <sisidovski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1506435}

--- third_party/blink/renderer/core/loader/document_loader.cc
+++ third_party/blink/renderer/core/loader/document_loader.cc
@@ -3206,15 +3206,11 @@ void DocumentLoader::CreateParserPostCommit() {
         mojom::blink::ServiceWorkerFetchHandlerType::kNotSkippable) {
       DCHECK_NE(service_worker_network_provider_->GetFetchHandlerType(),
                 mojom::blink::ServiceWorkerFetchHandlerType::kNoHandler);
-      // LoadingBehaviorFlag is a bit stream, and `|` should work.
-      loading_behavior = static_cast<LoadingBehaviorFlag>(
-          loading_behavior |
-          kLoadingBehaviorServiceWorkerFetchHandlerSkippable);
+      loading_behavior |= kLoadingBehaviorServiceWorkerFetchHandlerSkippable;
     }
     if (!response_.WasFetchedViaServiceWorker()) {
-      loading_behavior = static_cast<LoadingBehaviorFlag>(
-          loading_behavior |
-          kLoadingBehaviorServiceWorkerMainResourceFetchFallback);
+      loading_behavior |=
+          kLoadingBehaviorServiceWorkerMainResourceFetchFallback;
     }
     if (service_worker_network_provider_->GetFetchHandlerBypassOption() ==
             mojom::blink::ServiceWorkerFetchHandlerBypassOption::
@@ -3222,12 +3218,10 @@ void DocumentLoader::CreateParserPostCommit() {
         service_worker_network_provider_->GetFetchHandlerBypassOption() ==
             mojom::blink::ServiceWorkerFetchHandlerBypassOption::
                 kRaceNetworkRequestHoldback) {
-      loading_behavior = static_cast<LoadingBehaviorFlag>(
-          loading_behavior | kLoadingBehaviorServiceWorkerRaceNetworkRequest);
+      loading_behavior |= kLoadingBehaviorServiceWorkerRaceNetworkRequest;
     }
     if (response_.FromSyntheticResponse()) {
-      loading_behavior = static_cast<LoadingBehaviorFlag>(
-          loading_behavior | kLoadingBehaviorServiceWorkerSyntheticResponse);
+      loading_behavior |= kLoadingBehaviorServiceWorkerSyntheticResponse;
     }
 
     GetLocalFrameClient().DidObserveLoadingBehavior(loading_behavior);
