b3df950be032858530b263c204084b76be0059d1
William Liu <liuwilliam@chromium.org>
2025-08-26 14:19:03 -0700

    Refactor the credential selection dialog mojo from glic:: to actor::
    This allows glic.mojom to depend on actor_webui.mojom. The big benefit
    is that we can freely handle the credential selection response in the
    actor code space instead of inside glic_page_hander.cc.
    
    There is no behavior change in this CL. The implementation of the IPC is
    in the child CL https://crrev.com/c/6867438
    
    bug: b:427817882
    
    Change-Id: If680927abb396229d206004d039a4f7735becb88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6884892
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Commit-Queue: William Liu <liuwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1506768}

--- chrome/browser/resources/glic/glic_api_impl/conversions.ts
+++ chrome/browser/resources/glic/glic_api_impl/conversions.ts
@@ -3,8 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import type * as actorWebUiMojom from '../actor_webui.mojom-webui.js';
 import type * as mojom from '../glic.mojom-webui.js';
 import type * as api from '../glic_api/glic_api.js';
+
 import type * as requestTypes from './request_types.js';
 
 
@@ -96,8 +98,8 @@ assertNever<CheckEnumCompatibility<
 assertNever<CheckEnumCompatibility<
     typeof mojom.ActorTaskStopReason, typeof api.ActorTaskStopReason>>();
 assertNever<CheckEnumCompatibility<
-    typeof mojom.UserGrantedPermissionDuration,
+    typeof actorWebUiMojom.UserGrantedPermissionDuration,
     typeof api.UserGrantedPermissionDuration>>();
 assertNever<CheckEnumCompatibility<
-    typeof mojom.SelectCredentialDialogErrorReason,
+    typeof actorWebUiMojom.SelectCredentialDialogErrorReason,
     typeof requestTypes.SelectCredentialDialogErrorReason>>();
