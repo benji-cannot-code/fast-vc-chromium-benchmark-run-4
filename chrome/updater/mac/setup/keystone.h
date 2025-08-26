6ef8b8b76902f975bc11fe6c18315563828d6ea8
Michael Herrmann <michael@herrmann.io>
2025-08-26 13:19:12 -0700

    Fix an error in CreateEmptyPlistFile
    Before this CL, a permission error was raised when CreateEmptyPlistFile
    was called with a path below ~/Library/LaunchAgents, when that directory did not exist.
    
    This commit also updates KeystoneTest to test for the new behavior.
    
    Fixed: 440454445
    Change-Id: I5237af4a54a04fbf305a5c72505fa09affef02a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6879086
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1506733}

--- chrome/updater/mac/setup/keystone.h
+++ chrome/updater/mac/setup/keystone.h
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_UPDATER_MAC_SETUP_KEYSTONE_H_
 #define CHROME_UPDATER_MAC_SETUP_KEYSTONE_H_
 
+#include <string>
+
+#include "base/files/file_path.h"
 #include "base/functional/callback_forward.h"
 #include "chrome/updater/updater_scope.h"
 
@@ -17,8 +20,15 @@ namespace updater {
 
 struct RegistrationRequest;
 
-// Create a plist file with an empty dictionary.
-bool CreateEmptyPlistFile(const base::FilePath& file_path);
+// Create a plist file that makes legacy Keystone installer believe that a
+// healthy newer version updater already exists and thus won't over-install.
+bool CreateLegacyPlistFile(UpdaterScope scope,
+                           const std::string& library_subdir,
+                           const std::string& file_suffix);
+bool CreateLegacyPlistFileForTesting(UpdaterScope scope,
+                                     const base::FilePath& library_dir,
+                                     const std::string& library_subdir,
+                                     const std::string& file_suffix);
 
 // Installs Keystone and the necessary supporting files.
 bool InstallKeystone(UpdaterScope scope);
