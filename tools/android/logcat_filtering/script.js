7a15dc251a9bce2dc4f538928aaf58aa539586ca
Martin Kong <martinkong@google.com>
2025-08-26 14:13:55 -0700

    Allow the user to paste logcat in addition to uploading a file
    This is an improvement to the logcat filtering site.
    This CL allows the user to paste logcat in addition to uploading a
    file. We added a button called "Paste Logcat". When it is clicked,
    we transform the text display area into an editable text area.
    Once the user finishes pasting the logcat, they can click on a
    button called "Finish Pasting" and it will be equivalent to
    uploading a file containing the logcat.
    
    Bug: 438718922
    Change-Id: I33d3660b78ddf7ce509413b489f2fb7c17922f2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6885424
    Auto-Submit: Martin Kong <martinkong@google.com>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1506761}

--- tools/android/logcat_filtering/script.js
+++ tools/android/logcat_filtering/script.js
@@ -33,6 +33,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 const controlsDiv = document.getElementById('controls');
 const fileUploadButton = document.getElementById('file-upload-button');
+const pasteLogcatButton = document.getElementById('paste-logcat-button');
+const pasteLogcatButtonLabel = document.querySelector(
+  'label[for="paste-logcat-button"]');
 const nextExceptionButton = document.getElementById('next-exception-button');
 const exceptionFeedbackSpan = document.getElementById('exception-feedback');
 const nextTestButton = document.getElementById('next-test-button');
@@ -59,6 +62,8 @@ const textDisplayArea = document.getElementById('text-display-area');
 // Event listeners:
 
 fileUploadButton.addEventListener('change', handleFileUpload);
+
+pasteLogcatButton.addEventListener('click', handlePasteLogcatButtonClick);
 
 nextExceptionButton.addEventListener('click', jumpToNextException);
 
@@ -255,6 +260,33 @@ function handleFileUpload(event) {
       textDisplayArea.innerHTML = 'Encountered an error when reading the file.';
     };
     reader.readAsText(file);
+  }
+
+  // Reset the value of the file input element after the file is processed.
+  if (event.target) {
+    event.target.value = '';
+  }
+}
+
+/**
+ * This function is called when the user clicks on the paste logcat button.
+ * @param {Event} event
+ */
+function handlePasteLogcatButtonClick(event) {
+  if (pasteLogcatButtonLabel.textContent.includes('Paste Logcat')) {
+    // Change the UI to allow the user to paste text.
+    pasteLogcatButtonLabel.innerHTML =
+      '<i class="material-icons">done</i> Finish Pasting';
+    textDisplayArea.innerHTML = '';
+    textDisplayArea.contentEditable = 'true';
+    textDisplayArea.focus();
+  } else {
+    // Finish the pasting process and change the UI back to normal.
+    pasteLogcatButtonLabel.innerHTML =
+      '<i class="material-icons">content_paste</i> Paste Logcat';
+    textDisplayArea.contentEditable = 'false';
+    setUpElements(textDisplayArea.innerText.split('\n'));
+    updateTextDisplayArea(false);
   }
 }
 
