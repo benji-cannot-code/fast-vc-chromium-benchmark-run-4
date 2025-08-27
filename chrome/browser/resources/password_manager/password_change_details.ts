0b09c27c6a720f5deb0f37142f5e0a00a9797e80
Rafał Godlewski <rgod@google.com>
2025-08-27 00:12:12 -0700

    Manage focus on password change page navigation
    * Adds focusing back button in password change page in settings when
    the navigation happens from the password settings page
    * Adds focusing password change row in password settings page when
    navigation to it happens from password change page
    
    Fixed: b:438521841
    Change-Id: I4ccbb7aa2d6cdbb941c6ace1c0ba9b211cee19ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6881887
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1506935}

--- chrome/browser/resources/password_manager/password_change_details.ts
+++ chrome/browser/resources/password_manager/password_change_details.ts
@@ -12,7 +12,8 @@ import {I18nMixin} from 'chrome://resources/cr_elements/i18n_mixin.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {getTemplate} from './password_change_details.html.js';
-import {Page, Router} from './router.js';
+import type {Route} from './router.js';
+import {Page, RouteObserverMixin, Router} from './router.js';
 
 export interface PasswordChangeDetailsElement {
   $: {
@@ -20,7 +21,8 @@ export interface PasswordChangeDetailsElement {
   };
 }
 
-const PasswordChangeDetailsElementBase = I18nMixin(PolymerElement);
+const PasswordChangeDetailsElementBase =
+    I18nMixin(RouteObserverMixin(PolymerElement));
 
 export class PasswordChangeDetailsElement extends
     PasswordChangeDetailsElementBase {
@@ -30,6 +32,17 @@ export class PasswordChangeDetailsElement extends
 
   static get template() {
     return getTemplate();
+  }
+
+  override currentRouteChanged(newRoute: Route, oldRoute?: Route): void {
+    if (newRoute.page !== Page.PASSWORD_CHANGE ||
+        oldRoute?.page === Page.SETTINGS) {
+      return;
+    }
+
+    setTimeout(() => {
+      this.$.back.focus();
+    }, 0);
   }
 
   private navigateBack_() {
