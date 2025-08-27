660614bf847998c8e5cf0d9d9257187e57f058ed
Cassy Chun-Crogan <cassycc@google.com>
2025-08-27 16:21:51 -0700

    Office: Correctly check for kCannotGetSourceType consistency
    Bug: 429051861
    Change-Id: I5b81447e16d14f76e50816d8e3d9008b7249347e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6884371
    Reviewed-by: Alexander Bolodurin <alexbn@chromium.org>
    Commit-Queue: Cassy Chun-Crogan <cassycc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1507427}

--- chrome/browser/ui/webui/ash/cloud_upload/cloud_open_metrics.cc
+++ chrome/browser/ui/webui/ash/cloud_upload/cloud_open_metrics.cc
@@ -69,6 +69,7 @@ bool DidEndBeforeCallingOpenOrMoveFiles(OfficeTaskResult task_result) {
     case OfficeTaskResult::kNoFilesToOpen:
     case OfficeTaskResult::kOkAtFallback:
     case OfficeTaskResult::kFileAlreadyBeingOpened:
+    case OfficeTaskResult::kCannotGetSourceType:
       return true;
     case OfficeTaskResult::kOpened:
     case OfficeTaskResult::kMoved:
@@ -82,7 +83,6 @@ bool DidEndBeforeCallingOpenOrMoveFiles(OfficeTaskResult task_result) {
     case OfficeTaskResult::kFallbackQuickOfficeAfterOpen:
     case OfficeTaskResult::kCancelledAtFallbackAfterOpen:
     case OfficeTaskResult::kCannotGetFallbackChoiceAfterOpen:
-    case OfficeTaskResult::kCannotGetSourceType:
       return false;
   }
 }
@@ -265,13 +265,7 @@ void CloudOpenMetrics::CheckForInconsistencies(
     } else {
       // CloudOpenTask::OpenOrMoveFiles() was called.
       ExpectLogged(source_volume);
-      if (task_result.value == OfficeTaskResult::kCannotGetSourceType) {
-        // Special case where an upload was required but type of upload couldn't
-        // be determined.
-        ExpectNotLogged(transfer_required);
-      } else {
-        ExpectLogged(transfer_required);
-      }
+      ExpectLogged(transfer_required);
       if (DidEndAtFallback(task_result.value)) {
         // The cloud open/upload flow was exited at the Fallback Dialog after
         // an open was attempted. OpenErrors should give a fallback reason.
