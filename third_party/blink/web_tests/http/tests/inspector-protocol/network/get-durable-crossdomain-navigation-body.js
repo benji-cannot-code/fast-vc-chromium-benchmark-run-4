c38777d7ca95e256445467e782b47c70e25a83be
Alex N. Jose <alexnj@chromium.org>
2025-08-27 08:59:36 -0700

    Reland "Reland "DevTools Network Response Body Durable Storage implementation""
    This is a reland of commit f68fb6456fb6271f81791a40e9112fd1eed9056d
    
    The most recent revert of this is unrelated to this change itself, but
    due to an issue that existed in a PHP test script. I've created a
    separate fix CL crrev.com/c/6877661 that needs to land before this can
    be relanded. I've also removed some of the expectations that may help to
    reduce any potential flakes due to platform differences.
    
    Original change's description:
    > Reland "DevTools Network Response Body Durable Storage implementation"
    >
    > This is a reland of commit 9a67277c4c1e28001785ac7117c67c8299d5eea8
    >
    > This CL adds an explicit destruction of the Durable Messages Collector
    > map to prevent a destruction order related crash on MSAN builds.
    >
    > Original change's description:
    > > DevTools Network Response Body Durable Storage implementation
    > >
    > > This CL implements a response body storage mechanism that allow
    > > introspection of network response bodies after the renderer has been
    > > terminated. This reuses CDP's `NetworkHandler::enable()` with a
    > > new parameter and `::GetResponseBody()` methods to switch to
    > > the `DevtoolsDurableMessage` storage hosted in the Network Service.
    > > The storage is designed to be evicted based on the set limit, or as
    > > CDP clients are disconnected.
    > >
    > > Follow up CLs will complete the following:
    > >  * Decoding stored messages on retrieval (gzip, brotli etc).
    > >  * Base64 encoding binary payloads on retrieval to match current CDP
    > >    API design.
    > >
    > > Design doc: https://docs.google.com/document/d/1CGX7k-8KLJBPUCWGUzJSNF991JPBDYGEQF_-hYGQ2hA/edit?pli=1&tab=t.0#heading=h.7nki9mck5t64
    > >
    > > Bug: 414864477
    > > Change-Id: Ib991fb2d2801a5ef0e17517ec0a1c719a2e1687c
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6503314
    > > Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    > > Reviewed-by: Tom Sepez <tsepez@chromium.org>
    > > Commit-Queue: Alex N. Jose <alexnj@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1503009}
    >
    > Bug: 414864477
    > Change-Id: Ifbe215f084e049f3ea289f98aced13c357d763b6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6861675
    > Commit-Queue: Alex N. Jose <alexnj@chromium.org>
    > Reviewed-by: Adam Rice <ricea@chromium.org>
    > Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    > Reviewed-by: Tom Sepez <tsepez@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1504827}
    
    Bug: 414864477
    Change-Id: I44bd30b9592866a50fd8789fc4a0a61d78ad0ebf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6876126
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Alex N. Jose <alexnj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1507116}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/network/get-durable-crossdomain-navigation-body.js
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(/** @type {import('test_runner').TestRunner} */ testRunner) {
+  const {page, session, dp} = await testRunner.startBlank(
+      `Tests that DurableMessages live after a cross-origin navigation`);
+
+  await dp.Network.enable({maxTotalBufferSize: 115025, enableDurableMessages: true});
+  await dp.Page.enable();
+
+  const resourceUrl = testRunner.url('./resources/final.js');
+  session.evaluate(`fetch("${resourceUrl}").then(r => r.text());`);
+  const requestWillBeSent = (await dp.Network.onceRequestWillBeSent()).params;
+  testRunner.log(`Request for ${requestWillBeSent.request.url}`);
+  await dp.Network.onceResponseReceived();
+  const resourceRequestId = requestWillBeSent.requestId;
+
+  testRunner.log('-- Test Page.navigate() to a cross origin URL --');
+  await session.navigate('https://second.test/resources/hello-world.html');
+
+  const data = await dp.Network.getResponseBody({requestId: resourceRequestId});
+  testRunner.log(btoa(encodeURIComponent(data.result.body)));
+
+  testRunner.completeTest();
+})
+
+
