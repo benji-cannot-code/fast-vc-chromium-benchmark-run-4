3c00d482565c5d063934b0650b2ed85a2b078a6b
Jennifer Serrano <jennserrano@google.com>
2025-08-27 08:54:56 -0700

    [ntp][enterprise] MSFT file card url improvements
    Limits the number of responses returned by the MSFT graph API for recent
    files and requests the shared files to be ordered by the last modified
    date time, as well as only retrieves the fields needed.
    
    A couple things to note: the select param is not supported by the recent
    files endpoint, the top param is not supported by the shared endpoint
    nor is ordering by the sharedDateTime.
    
    Also increases the DownloadToString max size. Theoretically MSFT graph should only return up to 100 items for the shared files, but
    because it's not guaranteed, increase the size to allow showing
    the cards when there is a large response.
    
    Change-Id: I1d600ad398a9d3a355997371ef915f5f6aed80be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6886460
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Commit-Queue: Jennifer Serrano <jennserrano@google.com>
    Cr-Commit-Position: refs/heads/main@{#1507115}

--- chrome/browser/new_tab_page/modules/file_suggestion/microsoft_files_page_handler.cc
+++ chrome/browser/new_tab_page/modules/file_suggestion/microsoft_files_page_handler.cc
@@ -79,8 +79,6 @@ constexpr net::NetworkTrafficAnnotationTag traffic_annotation =
           }
         })");
 
-const int kMaxResponseSize = 1024 * 1024;
-
 const int kNumberOfDaysPerWeek = 7;
 
 const char kFakeTrendingData[] =
@@ -154,17 +152,20 @@ const char kFakeTrendingData[] =
 constexpr base::TimeDelta kModuleDismissalDuration = base::Hours(12);
 
 const char kBatchRequestUrl[] = "https://graph.microsoft.com/v1.0/$batch";
-const char kNonInsightsRequestBody[] = R"({
+const char kNonInsightsRequestBody[] =
+    R"({
   "requests": [
   {
     "id": "recent",
     "method": "GET",
-    "url": "/me/drive/recent?orderby=fileSystemInfo/lastAccessedDateTime+desc"
+    "url": "/me/drive/recent?orderby=fileSystemInfo/lastAccessedDateTime+desc)"
+    R"(&$top=6"
   },
   {
     "id": "shared",
     "method": "GET",
-    "url": "/me/drive/sharedWithMe"
+    "url": "/me/drive/sharedWithMe?$select=id,name,webUrl,file,)"
+    R"(lastModifiedDateTime,remoteItem&$orderBy=lastModifiedDateTime+desc"
   }]})";
 
 const char kNonInsightsFakeData[] =
@@ -451,7 +452,7 @@ void MicrosoftFilesPageHandler::RequestFiles(
       url_loader_factory_.get(),
       base::BindOnce(&MicrosoftFilesPageHandler::OnJsonReceived,
                      weak_factory_.GetWeakPtr(), std::move(callback)),
-      kMaxResponseSize);
+      network::SimpleURLLoader::kMaxBoundedStringDownloadSize);
 }
 
 void MicrosoftFilesPageHandler::ParseFakeData(GetFilesCallback callback) {
