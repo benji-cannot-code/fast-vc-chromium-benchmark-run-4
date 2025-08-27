1b7628689f33c5304b45ad47d83513f932deb7c7
Yuwei Huang <yuweih@chromium.org>
2025-08-27 15:13:22 -0700

    [crd host][wayland] Store pending frames when sink is not yet set
    It turns out sometimes the PipewireDesktopCapturer sends a frame before
    the sink is set on WebrtcVideoTrackSource, which will result in the
    frame being dropped. For poll-based capturers, that is not an issue
    because a new frame will soon be received and sent to the sink, but this
    is problematic for PipewireDesktopCapturer, since the sink will not
    receive any frames until the screen is changed, or the monitor is
    resized.
    
    This CL fixes this by storing the latest captured frame when the sink is
    not ready, and sending it to the sink once it's ready.
    
    Bug: 393626535
    Change-Id: If2e5e93fc0d8547a068b21d517197faf32244a8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6888072
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Auto-Submit: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1507394}

--- remoting/protocol/webrtc_video_track_source.h
+++ remoting/protocol/webrtc_video_track_source.h
@@ -6,15 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef REMOTING_PROTOCOL_WEBRTC_VIDEO_TRACK_SOURCE_H_
 #define REMOTING_PROTOCOL_WEBRTC_VIDEO_TRACK_SOURCE_H_
 
+#include <memory>
+
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "base/task/sequenced_task_runner.h"
 #include "remoting/codec/webrtc_video_encoder.h"
 #include "third_party/webrtc/api/media_stream_interface.h"
 #include "third_party/webrtc/api/notifier.h"
 #include "third_party/webrtc/modules/desktop_capture/desktop_frame.h"
-
-#include <memory>
 
 namespace remoting::protocol {
 
@@ -60,13 +61,21 @@ class WebrtcVideoTrackSource
       std::unique_ptr<WebrtcVideoEncoder::FrameStats> frame_stats);
 
  private:
+  void SendPendingFrame();
+
   raw_ptr<webrtc::VideoSinkInterface<webrtc::VideoFrame>> sink_ = nullptr;
   AddSinkCallback add_sink_callback_;
   scoped_refptr<base::SequencedTaskRunner> main_task_runner_;
 
+  // Last frame that is received before the sink is set. Once the sink is set,
+  // the pending frame will be sent to the sink.
+  std::unique_ptr<webrtc::VideoFrame> pending_frame_;
+
   // Incrementing ID to be attached to each VideoFrame, so that the
   // encoder-wrapper can detect if a frame was dropped.
   uint16_t frame_id_ = 0;
+
+  base::WeakPtrFactory<WebrtcVideoTrackSource> weak_ptr_factory_{this};
 };
 
 }  // namespace remoting::protocol
