e77e15f007b053e7b61641ed833000609942f3b1
Zijie He <zijiehe@google.com>
2025-08-27 14:58:28 -0700

    Add missing include for `base/command_line.h` in base/feature_list.cc
    The https://crrev.com/c/6885575 broke the builders with
    DCHECK_IS_CONFIGURABLE. E.g.
    https://ci.chromium.org/ui/p/chrome/builders/official/android-x86-official-dcheck/1840/overview
    
    ```
    ../../base/feature_list.cc:632:7: error: incomplete type 'base::CommandLine' named in nested name specifier
      632 |       CommandLine::ForCurrentProcess()->HasSwitch(
          |       ^~~~~~~~~~~~~
    ```
    
    Fixed: 441558006
    Change-Id: I3640b7e9a8df1bf5cfc1458ae434667a2f919567
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6892022
    Commit-Queue: Zijie He <zijiehe@google.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1507383}

--- base/feature_list.cc
+++ base/feature_list.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base_switches.h"
 #include "base/check_is_test.h"
+#include "base/command_line.h"
 #include "base/containers/contains.h"
 #include "base/containers/span.h"
 #include "base/debug/crash_logging.h"
