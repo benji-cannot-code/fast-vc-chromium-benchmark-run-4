9dd4194a93702abf59501a526070dcddfad0eeda
Charles Meng <charlesmeng@chromium.org>
2025-08-27 10:19:49 -0700

    Split toolbar button a11y
    Sets a different label based on state, and sets the tooltip to the same
    string.
    
    Fixed: 437560061
    Bug: 406790895
    Change-Id: I5f6111a6dfe155ae4ef248c1b8fa1a745bfbce94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6886451
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Charles Meng <charlesmeng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1507180}

--- chrome/browser/ui/views/toolbar/split_tabs_button.h
+++ chrome/browser/ui/views/toolbar/split_tabs_button.h
@@ -60,6 +60,7 @@ class SplitTabsToolbarButton : public ToolbarButton,
   void UpdateButtonIcon();
   void UpdateStatusIndicator(bool show_status_indicator);
   void UpdateAccessibilityRole(bool has_menu);
+  void UpdateAccessibilityLabel(bool is_enabled);
 
   BooleanPrefMember pin_state_;
   raw_ptr<Browser> browser_;
