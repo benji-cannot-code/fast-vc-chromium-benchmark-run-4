79ba15c45b8bf2f01d55b9f922aabe86db0562ac
Nicolas Dossou-Gbete <dgn@chromium.org>
2025-08-27 03:08:32 -0700

    rcaps: Expand program resolution testing
    Change the parameterized program resolution tests to not be based on the
    context fetched by the client, which allows to exercise the full
    regional capabilities cache initialisation.
    
    In follow-ups we'll add expectations for more histograms here.
    
    Bug: 438717568
    Change-Id: I92b98bdb8ad08f52fa553aac1086cc23d750bcb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6865597
    Auto-Submit: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: James Lee <ljjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1506994}

--- components/regional_capabilities/regional_capabilities_service.cc
+++ components/regional_capabilities/regional_capabilities_service.cc
@@ -409,8 +409,9 @@ void RegionalCapabilitiesService::ClearCacheForTesting() {
   program_settings_cache_.reset();
 }
 
-Program RegionalCapabilitiesService::GetActiveProgramForTesting() {
-  return GetActiveProgramSettings().program;
+const ProgramSettings&
+RegionalCapabilitiesService::GetActiveProgramSettingsForTesting() {
+  return GetActiveProgramSettings();
 }
 
 CountryId RegionalCapabilitiesService::GetPersistedCountryId() {
--- components/regional_capabilities/regional_capabilities_service.h
+++ components/regional_capabilities/regional_capabilities_service.h
@@ -137,7 +137,7 @@ class RegionalCapabilitiesService : public KeyedService {
   // in tests.
   void ClearCacheForTesting();
 
-  Program GetActiveProgramForTesting();
+  const ProgramSettings& GetActiveProgramSettingsForTesting();
 
 #if BUILDFLAG(IS_ANDROID)
   // -- JNI Interface ---------------------------------------------------------
