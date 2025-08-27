07871b5189aa63cb0dce4b5d409a2457c056e710
Hitarth Kothari <hitarthkothari@google.com>
2025-08-27 12:04:35 -0700

    Enable MostVisitedTilesVisualDeduplication by default.
    Bug: 417414647
    Change-Id: I8a9c82f193f0121c2d35599dbc310b51a7ed8c86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6892052
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Hitarth Kothari <hitarthkothari@google.com>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Hitarth Kothari <hitarthkothari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1507262}

--- components/history/core/browser/features.cc
+++ components/history/core/browser/features.cc
@@ -94,7 +94,7 @@ BASE_FEATURE(kMostVisitedTilesNewScoring,
 // If enabled, heuristically remove possible visual duplicates from top sites.
 BASE_FEATURE(kMostVisitedTilesVisualDeduplication,
              "MostVisitedTilesVisualDeduplication",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 constexpr char kMvtScoringParamRecencyFactor_Classic[] = "default";
 constexpr char kMvtScoringParamRecencyFactor_Decay[] = "decay";
