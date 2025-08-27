354499af8a709e3cd1e2a0b58bf1740c61d5e764
Takuto Ikuta <tikuta@chromium.org>
2025-08-27 05:28:30 -0700

    components: Add missing include `<cstdint>`
    This is to fix
    https://ci.chromium.org/ui/p/chrome/builders/try/linux-chrome/8743/overview
    
    Bug: 40263312
    Change-Id: I2b0ff9c8b081d38f72944b85699ea23f13ba7918
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6888314
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1507029}

--- components/omnibox/browser/on_device_tail_tokenizer.h
+++ components/omnibox/browser/on_device_tail_tokenizer.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_OMNIBOX_BROWSER_ON_DEVICE_TAIL_TOKENIZER_H_
 #define COMPONENTS_OMNIBOX_BROWSER_ON_DEVICE_TAIL_TOKENIZER_H_
 
+#include <cstdint>
 #include <memory>
 #include <string>
 #include <utility>
