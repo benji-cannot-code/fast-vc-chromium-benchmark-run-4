2755df6210ff83effe0fcf82f1dc715e195a97df
Gyuyoung Kim <gyuyoung@igalia.com>
2025-08-27 11:26:07 -0700

    [iOS Blink] Fix build break because of base::as_string_view
    Include base/strings/string_view_util.h header file in
    //content/browser/ios/nfc_host.mm to fix the build break
    on the bot.
    
    Bug: None
    Change-Id: Ib191c754f63d5789cb402dbfea9bdd5a3de910df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6888391
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1507234}

--- content/browser/ios/nfc_host.mm
+++ content/browser/ios/nfc_host.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/apple/foundation_util.h"
+#include "base/strings/string_view_util.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/task/bind_post_task.h"
 #include "content/public/browser/browser_context.h"
