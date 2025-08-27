d4f803d4ddbe96dfd01f780c3243ad0d1c7228be
Robert Ferens <rferens@google.com>
2025-08-27 09:23:57 -0700

    iwa: Add NonAllowlistedAppInstallationRejected histogram
    The histogram will trace the session type (Kiosk, Managed Guest, or
    Managed User), whenever the installation is rejected because of app
    being not in the IWA allowlist.
    
    DD: go/iwa-allowlist-dd (section Metrics)
    Fixed: 441026491
    Change-Id: I4b3e5618f855fbd48729d8907e52f30d1036feb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6881466
    Reviewed-by: Simon Hangl <simonha@google.com>
    Commit-Queue: Robert Ferens <rferens@google.com>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1507130}

--- chrome/browser/web_applications/isolated_web_apps/policy/isolated_web_app_installer.cc
+++ chrome/browser/web_applications/isolated_web_apps/policy/isolated_web_app_installer.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_deref.h"
 #include "base/files/file_path.h"
 #include "base/functional/bind.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/no_destructor.h"
 #include "base/types/expected_macros.h"
 #include "base/types/optional_util.h"
@@ -20,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/isolated_web_apps/update_manifest/update_manifest_fetcher.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
 #include "chrome/common/chrome_features.h"
+#include "chromeos/components/kiosk/kiosk_utils.h"
 #include "components/webapps/isolated_web_apps/download/bundle_downloader.h"
 #include "components/webapps/isolated_web_apps/iwa_key_distribution_info_provider.h"
 #include "components/webapps/isolated_web_apps/types/source.h"
@@ -110,6 +112,27 @@ std::optional<UpdateManifest::VersionEntry> GetVersionWithOptions(
     return update_manifest.GetLatestVersion(install_options.update_channel());
   }
 }
+constexpr std::string_view kNonAllowlistedAppInstallationRejectedHistogramName =
+    "WebApp.Isolated.NonAllowlistedAppInstallationRejected";
+
+enum class ManagedSessionType {
+  kManagedUserSession = 0,
+  kManagedGuestSession = 1,
+  kKiosk = 2,
+  kMaxValue = kKiosk,
+};
+
+ManagedSessionType GetCurrentManagedSessionType() {
+#if BUILDFLAG(IS_CHROMEOS)
+  if (chromeos::IsManagedGuestSession()) {
+    return ManagedSessionType::kManagedGuestSession;
+  }
+  if (chromeos::IsKioskSession()) {
+    return ManagedSessionType::kKiosk;
+  }
+#endif  // BUILDFLAG(IS_CHROMEOS)
+  return ManagedSessionType::kManagedUserSession;
+}
 
 }  // namespace
 
@@ -169,6 +192,9 @@ IwaInstaller::~IwaInstaller() = default;
 void IwaInstaller::Start() {
   if (!IwaKeyDistributionInfoProvider::GetInstance().IsManagedInstallPermitted(
           install_options_.web_bundle_id().id())) {
+    base::UmaHistogramEnumeration(
+        kNonAllowlistedAppInstallationRejectedHistogramName,
+        GetCurrentManagedSessionType());
     Finish(Result(Result::Type::kErrorAppNotInAllowlist,
                   "Not in the managed allowlist."));
     return;
