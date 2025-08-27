343031f00c7a91d01d33ea93f95c92c860d38341
Christoph Schwering <schwering@google.com>
2025-08-27 04:56:12 -0700

    [Autofill] Bump NotFatalUntil in form_autofill_util.cc
    Bug: 347059988, 417307428
    Change-Id: I9714a835ca9faef9602c1e9d19d36ffcfcf4987c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6889275
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1507019}

--- components/autofill/content/renderer/form_autofill_util.cc
+++ components/autofill/content/renderer/form_autofill_util.cc
@@ -2077,7 +2077,7 @@ std::optional<FormData> ExtractFormDataWithFieldsAndFrames(
     ButtonTitlesCache* button_titles_cache,
     DenseSet<ExtractOption> extract_options) {
   CHECK(!form_element || form_element.GetDocument() == document,
-        base::NotFatalUntil::M141);
+        base::NotFatalUntil::M142);
 
   if (form_element && !IsAccessible(form_element)) {
     return std::nullopt;
@@ -2556,8 +2556,8 @@ FindFormAndFieldForFormControlElement(
   CHECK(base::Contains(GetOwnedFormControls(element.GetDocument(),
                                             element.GetOwningFormForAutofill()),
                        element),
-        base::NotFatalUntil::M141);
-  NOTREACHED(base::NotFatalUntil::M141);
+        base::NotFatalUntil::M142);
+  NOTREACHED(base::NotFatalUntil::M142);
   return std::nullopt;
 }
 
