d29ae38847d06b2eaeead7e2c279f11ab093b23f
Benjamin Williams <bwwilliams@google.com>
2025-08-27 12:30:13 -0700

    [CrossDevicePrefTracker] Introduce skeleton CrossDevicePrefTracker
    This change introduces the initial skeleton for the
    CrossDevicePrefTracker, a new KeyedService for tracking non-syncing
    Prefs across a user's devices.
    
    This CL creates the following:
    * The CrossDevicePrefTracker abstract interface.
    * A stubbed CrossDevicePrefTrackerImpl implementation.
    * The required BUILD.gn and DEPS changes for the new files.
    
    This is the first step in implementing the service described in
    go/cross-device-pref-tracker. The factories for this service will be
    added in a subsequent change.
    
    Fixed: 440620985
    Change-Id: I1418619a2a747672f91ba557a1db3fc2f9ff3c16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6886309
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Reviewed-by: Ankush Singh <ankushkush@google.com>
    Reviewed-by: Jenna Himawan <jhimawan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1507289}

---
+++ components/sync_preferences/cross_device_pref_tracker.cc
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/sync_preferences/cross_device_pref_tracker.h"
+
+namespace sync_preferences {
+
+CrossDevicePrefTracker::~CrossDevicePrefTracker() = default;
+
+}  // namespace sync_preferences
