0b75263216738da1acb17caf76dded32f3a2e0d6
Daniil Sakhapov <sakhapov@chromium.org>
2025-08-27 04:14:34 -0700

    Set aria-current=true for :target-current html anchor element
    aria-current=true
    
    AX-Relnotes: :target-current html anchor element is set to have
    Bug: 439294790
    Change-Id: If85c4dc71d03194217dba461251bc6d23df44226
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6853905
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    Reviewed-by: Lucas Radaelli <lucasradaelli@google.com>
    Cr-Commit-Position: refs/heads/main@{#1507008}

--- content/browser/accessibility/dump_accessibility_tree_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_tree_browsertest.cc
@@ -87,6 +87,10 @@ void DumpAccessibilityTreeTest::SetUpCommandLine(
   // Enable MenuElements so that the new menu elements are recognized.
   command_line->AppendSwitchASCII(switches::kEnableBlinkFeatures,
                                   "MenuElements");
+  // Enable CSSScrollTargetGroupAriaCurrent, used by
+  // TargetCurrentHtmlAnchorRole.
+  command_line->AppendSwitchASCII(switches::kEnableBlinkFeatures,
+                                  "CSSScrollTargetGroupAriaCurrent");
 }
 
 void DumpAccessibilityTreeTest::SetUpOnMainThread() {
@@ -4809,6 +4813,15 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeWithCarouselTest,
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeWithCarouselTest,
                        CarouselMarkerNames) {
   RunCSSTest(FILE_PATH_LITERAL("carousel-marker-names.html"));
+}
+
+IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest, TargetCurrentHtmlAnchorRole) {
+  RunCSSTest(FILE_PATH_LITERAL("target-current-html-anchor-role.html"));
+}
+
+IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
+                       TargetCurrentHtmlAnchorRoleDynamic) {
+  RunCSSTest(FILE_PATH_LITERAL("target-current-html-anchor-role-dynamic.html"));
 }
 
 //
