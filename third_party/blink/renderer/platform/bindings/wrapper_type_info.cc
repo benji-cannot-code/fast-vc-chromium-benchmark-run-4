06ab760d997f962636a44dc8c8c36b8eaa9247d0
David Sanders <dsanders11@ucsbalum.com>
2025-08-26 21:54:02 -0700

    Remove unnecessary includes in v8_per_isolate_data.h
    There are already forward declarations that render these includes
    unnecessary.
    
    Should save ~22 MiB of compiler input size. Adds missing includes as
    needed to fix the build.
    
    Bug: 40318405
    Change-Id: Ifee2d7f594e3707d4f9ba877e6b386fd2c938212
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6883392
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1506912}

--- third_party/blink/renderer/platform/bindings/wrapper_type_info.cc
+++ third_party/blink/renderer/platform/bindings/wrapper_type_info.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/bindings/wrapper_type_info.h"
 
+#include "base/notreached.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/bindings/v8_object_constructor.h"
 #include "third_party/blink/renderer/platform/bindings/v8_per_isolate_data.h"
