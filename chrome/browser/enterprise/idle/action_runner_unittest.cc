783c9567510e8ea32c27ba77ee0eeea6b03da355
Lei Zhang <thestig@chromium.org>
2025-08-26 18:03:44 -0700

    Replace custom gMock action with code from base/test
    Use base/test/gmock_callback_support.h instead of reinventing the wheel.
    Mention testing::InvokeArgument and base::OnceCallback on the same line
    in gmock_callback_support.h, so future developers can find that support
    code more easily.
    
    Also remove some Times(1) calls, as that is the implied cardinality for
    EXPECT_CALL().
    
    Change-Id: I49ec627e7f82f6958d8415ea24d3aefb63bcf095
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6886461
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1506872}

--- chrome/browser/enterprise/idle/action_runner_unittest.cc
+++ chrome/browser/enterprise/idle/action_runner_unittest.cc
@@ -74,12 +74,6 @@ class MockAction : public Action {
   }
 };
 
-// testing::InvokeArgument<N> does not work with base::OnceCallback, so we
-// define our own gMock action to run the 2nd argument.
-ACTION_P(RunContinuation, success) {
-  std::move(const_cast<Action::Continuation&>(arg1)).Run(success);
-}
-
 }  // namespace
 
 // TODO(crbug.com/40222234): Enable this when Android supports >1 Action.
@@ -102,9 +96,9 @@ TEST(IdleActionRunnerTest, RunsActionsInSequence) {
       std::make_unique<MockAction>(ActionType::kShowProfilePicker);
   testing::InSequence in_sequence;
   EXPECT_CALL(*close_browsers, Run(&profile, _))
-      .WillOnce(RunContinuation(true));
+      .WillOnce(base::test::RunOnceCallback<1>(true));
   EXPECT_CALL(*show_profile_picker, Run(&profile, _))
-      .WillOnce(RunContinuation(true));
+      .WillOnce(base::test::RunOnceCallback<1>(true));
 
   action_factory.Associate(ActionType::kCloseBrowsers,
                            std::move(close_browsers));
@@ -132,9 +126,9 @@ TEST(IdleActionRunnerTest, PrefOrderDoesNotMatter) {
       std::make_unique<MockAction>(ActionType::kShowProfilePicker);
   testing::InSequence in_sequence;
   EXPECT_CALL(*close_browsers, Run(&profile, _))
-      .WillOnce(RunContinuation(true));
+      .WillOnce(base::test::RunOnceCallback<1>(true));
   EXPECT_CALL(*show_profile_picker, Run(&profile, _))
-      .WillOnce(RunContinuation(true));
+      .WillOnce(base::test::RunOnceCallback<1>(true));
 
   action_factory.Associate(ActionType::kCloseBrowsers,
                            std::move(close_browsers));
@@ -165,7 +159,7 @@ TEST(IdleActionRunnerTest, OtherActionsDontRunOnFailure) {
   // "show_profile_picker" shouldn't run, because "close_browsers" fails.
   testing::InSequence in_sequence;
   EXPECT_CALL(*close_browsers, Run(&profile, _))
-      .WillOnce(RunContinuation(false));
+      .WillOnce(base::test::RunOnceCallback<1>(false));
   EXPECT_CALL(*show_profile_picker, Run(_, _)).Times(0);
 
   action_factory.Associate(ActionType::kCloseBrowsers,
@@ -219,7 +213,7 @@ TEST(IdleActionRunnerTest, JustCloseBrowsers) {
       std::make_unique<MockAction>(ActionType::kShowProfilePicker);
 
   EXPECT_CALL(*close_browsers, Run(&profile, _))
-      .WillOnce(RunContinuation(true));
+      .WillOnce(base::test::RunOnceCallback<1>(true));
   EXPECT_CALL(*show_profile_picker, Run(_, _)).Times(0);
 
   action_factory.Associate(ActionType::kCloseBrowsers,
@@ -248,7 +242,7 @@ TEST(IdleActionRunnerTest, JustShowProfilePicker) {
 
   EXPECT_CALL(*close_browsers, Run(_, _)).Times(0);
   EXPECT_CALL(*show_profile_picker, Run(&profile, _))
-      .WillOnce(RunContinuation(true));
+      .WillOnce(base::test::RunOnceCallback<1>(true));
 
   action_factory.Associate(ActionType::kCloseBrowsers,
                            std::move(close_browsers));
