8975404950ea6a64f3c7388972a606d75d90a987
Qiu Jianlin <jianlin.qiu@intel.com>
2025-08-26 17:31:58 -0700

    [DX12 Enc] Manual reference control with D3D12 VEA.
    This CL enable manual reference control with D3D12 hardware accelerators
    (H.264 & H.265). AV1 support will be added by follow-up CL.
    
    Test: start chromium with `--enable-experimental-web-platform-features
    --enable-features=D3D12VideoEncodeAccelerator`, and test with
    manual-svc.html on H.264 & H.265 HW encode.
    
    Bug: 347676170, 40275246
    Change-Id: Icea7b330142e9e9dd26359466971a2170ea45091
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6856483
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Jianlin Qiu <jianlin.qiu@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1506859}

--- media/video/fake_video_encode_accelerator.cc
+++ media/video/fake_video_encode_accelerator.cc
@@ -75,10 +75,17 @@ EncoderStatus FakeVideoEncodeAccelerator::Initialize(
 
 void FakeVideoEncodeAccelerator::Encode(scoped_refptr<VideoFrame> frame,
                                         bool force_keyframe) {
+  VideoEncoder::EncodeOptions options(/*key_frame=*/true);
+  Encode(std::move(frame), options);
+}
+
+void FakeVideoEncodeAccelerator::Encode(
+    scoped_refptr<VideoFrame> frame,
+    const VideoEncoder::EncodeOptions& options) {
   DCHECK(client_);
   FrameToEncode encode;
   encode.frame = frame;
-  encode.force_keyframe = force_keyframe;
+  encode.options = options;
   queued_frames_.push(encode);
   EncodeTask();
 }
@@ -179,7 +186,7 @@ void FakeVideoEncodeAccelerator::EncodeTask() {
     queued_frames_.pop();
 
     if (next_frame_is_first_frame_) {
-      frame_to_encode.force_keyframe = true;
+      frame_to_encode.options.key_frame = true;
       next_frame_is_first_frame_ = false;
     }
 
@@ -200,12 +207,12 @@ void FakeVideoEncodeAccelerator::DoBitstreamBufferReady(
   }
 
   BitstreamBufferMetadata metadata(kMinimumOutputBufferSize,
-                                   frame_to_encode.force_keyframe,
+                                   frame_to_encode.options.key_frame,
                                    frame_to_encode.frame->timestamp());
   metadata.encoded_size = frame_to_encode.frame->coded_size();
 
   if (!encoding_callback_.is_null()) {
-    metadata = encoding_callback_.Run(buffer, frame_to_encode.force_keyframe,
+    metadata = encoding_callback_.Run(buffer, frame_to_encode.options.key_frame,
                                       frame_to_encode.frame);
   }
 
--- media/video/fake_video_encode_accelerator.h
+++ media/video/fake_video_encode_accelerator.h
@@ -48,6 +48,8 @@ class FakeVideoEncodeAccelerator : public VideoEncodeAccelerator {
       Client* client,
       std::unique_ptr<MediaLog> media_log = nullptr) override;
   void Encode(scoped_refptr<VideoFrame> frame, bool force_keyframe) override;
+  void Encode(scoped_refptr<VideoFrame> frame,
+              const VideoEncoder::EncodeOptions& options) override;
   void UseOutputBitstreamBuffer(BitstreamBuffer buffer) override;
   void RequestEncodingParametersChange(
       const Bitrate& bitrate,
@@ -82,7 +84,7 @@ class FakeVideoEncodeAccelerator : public VideoEncodeAccelerator {
     FrameToEncode(const FrameToEncode&);
     ~FrameToEncode();
     scoped_refptr<VideoFrame> frame;
-    bool force_keyframe;
+    VideoEncoder::EncodeOptions options;
   };
 
   using EncodingCallback = base::RepeatingCallback<BitstreamBufferMetadata(
