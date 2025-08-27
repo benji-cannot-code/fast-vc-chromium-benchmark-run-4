b1a7b266f4a4f965ff430fe8c0da4efb163a2c82
Eriko Kurimoto <elkurin@google.com>
2025-08-26 21:34:41 -0700

    ServiceWorker: Resolve DanglingUntriage of disk cache entry
    `disk_cache_entry_` was freed by Entry::Close() called against itself so
    using ExtractAsDangling() will allow us to resolve DanglingUntriage.
    
    Ideally, we should make entry as std::unique_ptr and stop using `delete
    this` inside Close() method, but it would be tough considering
    disk_cache is used among the code and there are multiple impls for
    disk_cache. It is not worhwhile to reimplement whole stuff for the
    dangling ptr which has no risk of leaking.
    
    Change-Id: Ic517e24479f8851c857a53917f49549a110e47c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6883086
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1506904}

--- components/services/storage/service_worker/service_worker_disk_cache.cc
+++ components/services/storage/service_worker/service_worker_disk_cache.cc
@@ -59,7 +59,7 @@ ServiceWorkerDiskCacheEntry::ServiceWorkerDiskCacheEntry(
 
 ServiceWorkerDiskCacheEntry::~ServiceWorkerDiskCacheEntry() {
   if (disk_cache_entry_) {
-    disk_cache_entry_->Close();
+    disk_cache_entry_.ExtractAsDangling()->Close();
     cache_->RemoveOpenEntry(this);
   }
 }
@@ -96,7 +96,7 @@ int64_t ServiceWorkerDiskCacheEntry::GetSize(int index) {
 }
 
 void ServiceWorkerDiskCacheEntry::Abandon() {
-  disk_cache_entry_->Close();
+  disk_cache_entry_.ExtractAsDangling()->Close();
   disk_cache_entry_ = nullptr;
 }
 
