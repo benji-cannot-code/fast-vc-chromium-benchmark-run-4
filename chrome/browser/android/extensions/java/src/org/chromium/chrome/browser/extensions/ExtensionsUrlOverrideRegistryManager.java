51d390b1f85956ca427279130c51f22d31127807
Fiaz Muhammad <mfiaz@google.com>
2025-08-27 13:36:22 -0700

    [URL Override] Add stub classes in chrome/browser/android/extensions/
    - Creates the directory, chrome/browser/android/extensions/.
    - Create file, ExtensionsUrlOverrideRegistryManager, along with
    	Native counter-part.
    - Create module in chrome/browser/android/extensions/BUILD.gn.
    
    Bug: 439831443
    Change-Id: I534c49f2673edfc956dd14aebf5bd3378d784f26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6863229
    Auto-Submit: Fiaz Muhammad <mfiaz@google.com>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1507345}

---
+++ chrome/browser/android/extensions/java/src/org/chromium/chrome/browser/extensions/ExtensionsUrlOverrideRegistryManager.java
@@ -0,0 +1,39 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.extensions;
+
+import org.jni_zero.JNINamespace;
+import org.jni_zero.NativeMethods;
+
+import org.chromium.build.annotations.NullMarked;
+
+/**
+ * Listens to changes to the Native-level extensions URL registry and handles updates to Android
+ * classes.
+ */
+@NullMarked
+@JNINamespace("extensions")
+public class ExtensionsUrlOverrideRegistryManager {
+    private long mNativePtr;
+
+    public ExtensionsUrlOverrideRegistryManager() {
+        mNativePtr = ExtensionsUrlOverrideRegistryManagerJni.get().initialize(this);
+    }
+
+    public void destroy() {
+        if (mNativePtr != 0) {
+            ExtensionsUrlOverrideRegistryManagerJni.get().destroy(mNativePtr);
+            mNativePtr = 0;
+        }
+    }
+
+    @NativeMethods
+    interface Natives {
+        long initialize(ExtensionsUrlOverrideRegistryManager javaObject);
+
+        void destroy(long nativeExtensionsUrlOverrideRegistryManager);
+    }
+}
