6a5a809956e69e7b0adbf20b8270cafdd714ea1c
Dominique Fauteux-Chapleau <domfc@chromium.org>
2025-08-28 12:59:28 -0700

    Add enterprise referrer chain cache logic to download util
    This CL updates the caching logic so downloads use the same
    cache as pastes/file uploads/prints, and also adds logic so
    refreshing the tab doesn't clear the cache.
    
    Bug: 438212880
    Change-Id: I44914d1b89173d2154d50ced0365cb87924eb118
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6863370
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Lina Ismail <ismaill@google.com>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1507959}

--- chrome/browser/enterprise/connectors/referrer_cache_utils.h
+++ chrome/browser/enterprise/connectors/referrer_cache_utils.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ENTERPRISE_CONNECTORS_REFERRER_CACHE_UTILS_H_
 #define CHROME_BROWSER_ENTERPRISE_CONNECTORS_REFERRER_CACHE_UTILS_H_
 
+#include "base/supports_user_data.h"
 #include "components/safe_browsing/core/browser/referrer_chain_provider.h"
 
 class GURL;
@@ -13,6 +14,10 @@ class GURL;
 namespace content {
 class WebContents;
 }  // namespace content
+
+namespace download {
+class DownloadItem;
+}  // namespace download
 
 namespace enterprise_connectors {
 
@@ -22,13 +27,26 @@ safe_browsing::ReferrerChain GetReferrerChain(
     const GURL& url,
     content::WebContents& web_contents);
 
+// Create a chain containing `download_item`'s URL and whatever corresponding
+// chain can be found from the cache in its web contents.
+safe_browsing::ReferrerChain GetOrCreateReferrerChain(
+    download::DownloadItem& download_item);
+
+// Get the cached chain for the given download if it exists.
+safe_browsing::ReferrerChain GetCachedReferrerChain(
+    download::DownloadItem& download_item);
+
 // Obtains the referrer chain for the given arguments and caches it in
 // `web_contents`'s user data.
 void SetReferrerChain(const GURL& url, content::WebContents& web_contents);
 
+// Cache the given `referrer_chain` into the passed download item's user data.
+void SetReferrerChain(safe_browsing::ReferrerChain referrer_chain,
+                      download::DownloadItem& download_item);
+
 // Test-only helper to check if a value has already been cached for the existing
-// web contents.
-bool HasCachedChainForTesting(content::WebContents& web_contents);
+// web contents or download item.
+bool HasCachedChainForTesting(base::SupportsUserData& supports_user_data);
 
 }  // namespace enterprise_connectors
 
