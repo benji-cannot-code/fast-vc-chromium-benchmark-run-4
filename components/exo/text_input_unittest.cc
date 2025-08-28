c09a2cfe75e1f28c6dc190e5c105cfd92ccfa1fb
Jonathan Lee <jonathanjlee@google.com>
2025-08-28 10:05:20 -0700

    [gtest] Clean up single-arg `Invoke()`s in `//components/exo/`
    Not needed and deprecated [0]. This CL should be a no-op.
    
    [0]: https://chromium.googlesource.com/external/github.com/google/googletest.git/+/a05c0915074bcd1b82f232e081da9bb6c205c28d/googlemock/include/gmock/gmock-actions.h#2046
    
    Bug: 439838457
    Change-Id: Ie1ed3ba149b07cb89ad0c00b79c839b08f70d270
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6893564
    Commit-Queue: Peter McNeeley <petermcneeley@google.com>
    Reviewed-by: Peter McNeeley <petermcneeley@google.com>
    Auto-Submit: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1507788}

--- components/exo/text_input_unittest.cc
+++ components/exo/text_input_unittest.cc
@@ -339,12 +339,12 @@ TEST_F(TextInputTest, ShowVirtualKeyboardIfEnabled) {
   // VirtualKeyboardControllerObserver event as if it is done via
   // real VirtualKeyboardController implementation.
   EXPECT_CALL(observer, OnVirtualKeyboardVisibilityChangedIfEnabled)
-      .WillOnce(testing::Invoke([this](bool should_show) {
+      .WillOnce([this](bool should_show) {
         if (should_show)
           text_input()->OnKeyboardVisible(gfx::Rect());
         else
           text_input()->OnKeyboardHidden();
-      }));
+      });
   EXPECT_CALL(*delegate(), OnVirtualKeyboardVisibilityChanged(true)).Times(1);
   text_input()->ShowVirtualKeyboardIfEnabled();
   testing::Mock::VerifyAndClearExpectations(&observer);
@@ -371,12 +371,12 @@ TEST_F(TextInputTest, ShowVirtualKeyboardIfEnabledBeforeActivated) {
   // VirtualKeyboardControllerObserver event as if it is done via
   // real VirtualKeyboardController implementation.
   EXPECT_CALL(observer, OnVirtualKeyboardVisibilityChangedIfEnabled)
-      .WillOnce(testing::Invoke([this](bool should_show) {
+      .WillOnce([this](bool should_show) {
         if (should_show)
           text_input()->OnKeyboardVisible(gfx::Rect());
         else
           text_input()->OnKeyboardHidden();
-      }));
+      });
   EXPECT_CALL(*delegate(), Activated).Times(1);
   EXPECT_CALL(*delegate(), OnVirtualKeyboardVisibilityChanged(true)).Times(1);
   text_input()->Activate(seat(), surface(),
