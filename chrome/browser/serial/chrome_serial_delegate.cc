192a50d6df17b2b678b4c64ed08e8f272aa030ec
Antonio Gomes <tonikitoo@igalia.com>
2025-08-27 20:45:07 -0700

    Componentize chrome/browser/serial
    This CL finishes the componentization of c/b/serial, adding .cc and unit
    tests.
    No behavior change expected.
    
    R=dljames@chromium.org
    
    Bug: 353332589
    Change-Id: Ic0d1aa665168927a742cd615079a695d3f52ca2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6893054
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1507510}

--- chrome/browser/serial/chrome_serial_delegate.cc
+++ chrome/browser/serial/chrome_serial_delegate.cc
@@ -11,8 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/serial/serial_chooser_context.h"
 #include "chrome/browser/serial/serial_chooser_context_factory.h"
 #include "chrome/browser/serial/web_serial_chooser.h"
-#include "chrome/browser/ui/browser_dialogs.h"
-#include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/serial/serial_chooser_controller.h"
 #include "components/guest_view/buildflags/buildflags.h"
 #include "content/public/browser/render_frame_host.h"
