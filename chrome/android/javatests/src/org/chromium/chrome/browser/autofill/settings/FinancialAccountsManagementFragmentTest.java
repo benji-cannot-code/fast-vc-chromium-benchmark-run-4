8a9f6a9560eec23d1d97e9e7f7d46d0e2c63830a
Joshua Tan <joshuatan@google.com>
2025-08-28 07:07:59 -0700

    Adds testing config for A2A payments.
    Also disables AUTOFILL_ENABLE_SEPARATE_PIX_PREFERENCE_ITEM globally
    for tests in FinancialAccountsManagementFragmentTest; otherwise,
    the ewallet tests fail when the flag is enabled by the fieldtrial
    config.
    
    Bug: 440947759
    Change-Id: I0a0b82b1d34a905f73f96abe656e98f31bf63022
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6874286
    Reviewed-by: Junhui He <junhuihe@google.com>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Joshua Tan <joshuatan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1507716}

--- chrome/android/javatests/src/org/chromium/chrome/browser/autofill/settings/FinancialAccountsManagementFragmentTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/autofill/settings/FinancialAccountsManagementFragmentTest.java
@@ -37,6 +37,7 @@ import org.chromium.base.ContextUtils;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.HistogramWatcher;
 import org.chromium.base.test.util.RequiresRestart;
@@ -72,6 +73,7 @@ import java.util.concurrent.TimeoutException;
     ChromeFeatureList.AUTOFILL_ENABLE_SYNCING_OF_PIX_BANK_ACCOUNTS,
     ChromeFeatureList.AUTOFILL_SYNC_EWALLET_ACCOUNTS
 })
+@DisableFeatures({ChromeFeatureList.AUTOFILL_ENABLE_SEPARATE_PIX_PREFERENCE_ITEM})
 @Batch(Batch.PER_CLASS)
 public class FinancialAccountsManagementFragmentTest {
     @Rule public final AutofillTestRule rule = new AutofillTestRule();
