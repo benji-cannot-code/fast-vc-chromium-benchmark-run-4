f55d8cbc1820b2e8ad7eeb81abae6ed7e9f0cf5a
mikt <mikt@google.com>
2025-08-28 00:43:41 -0700

    [PA] Separate Scheduler-Loop quarantine branch for AMSC
    Scheduler-Loop quarantine is used by two separate features -
    PartitionAlloc with Advanced Checks (PA/AC) and Advanced Memory Safety
    Checks (AMSC). As we are running experiments in parallel, those two
    interfere with each other, making experimenting harder.
    This patch separates backing Schduler-Loop Quarantine branch to
    make those independent.
    
    Bug: 351974425
    Change-Id: I72ad615f4a0093269b1a31ff903501a7a2d2702f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6883375
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Mikihito Matsuura <mikt@google.com>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1507569}

--- base/allocator/partition_allocator/src/partition_alloc/shim/allocator_shim_default_dispatch_to_partition_alloc.cc
+++ base/allocator/partition_allocator/src/partition_alloc/shim/allocator_shim_default_dispatch_to_partition_alloc.cc
@@ -644,6 +644,8 @@ void ConfigurePartitions(
         scheduler_loop_quarantine_global_config,
     partition_alloc::internal::SchedulerLoopQuarantineConfig
         scheduler_loop_quarantine_thread_local_config,
+    partition_alloc::internal::SchedulerLoopQuarantineConfig
+        scheduler_loop_quarantine_for_advanced_memory_safety_checks_config,
     EventuallyZeroFreedMemory eventually_zero_freed_memory,
     FewerMemoryRegions fewer_memory_regions) {
   // Calling Get() is actually important, even if the return value isn't
@@ -680,6 +682,8 @@ void ConfigurePartitions(
             scheduler_loop_quarantine_global_config;
         opts.scheduler_loop_quarantine_thread_local_config =
             scheduler_loop_quarantine_thread_local_config;
+        opts.scheduler_loop_quarantine_for_advanced_memory_safety_checks_config =
+            scheduler_loop_quarantine_for_advanced_memory_safety_checks_config;
         opts.memory_tagging = {
             .enabled = enable_memory_tagging
                            ? partition_alloc::PartitionOptions::kEnabled
--- base/allocator/scheduler_loop_quarantine_config.cc
+++ base/allocator/scheduler_loop_quarantine_config.cc
@@ -25,6 +25,7 @@ constexpr char kProcessTypeWildcardStr[] = "*";
 constexpr char kBranchTypeGlobalStr[] = "global";
 constexpr char kBranchTypeThreadLocalDefaultStr[] = "*";
 constexpr char kBranchTypeMainStr[] = "main";
+constexpr char kBranchTypeAdvancedMemorySafetyChecksStr[] = "amsc";
 
 constexpr std::string_view GetSchedulerLoopQuarantineBranchTypeStr(
     SchedulerLoopQuarantineBranchType type) {
@@ -35,6 +36,8 @@ constexpr std::string_view GetSchedulerLoopQuarantineBranchTypeStr(
       return kBranchTypeThreadLocalDefaultStr;
     case SchedulerLoopQuarantineBranchType::kMain:
       return kBranchTypeMainStr;
+    case SchedulerLoopQuarantineBranchType::kAdvancedMemorySafetyChecks:
+      return kBranchTypeAdvancedMemorySafetyChecksStr;
   }
   NOTREACHED();
 }
@@ -105,7 +108,9 @@ GetSchedulerLoopQuarantineConfiguration(
 
     // Falls back to thread-local default unless global.
     if (!config_entry &&
-        branch_type != SchedulerLoopQuarantineBranchType::kGlobal) {
+        branch_type != SchedulerLoopQuarantineBranchType::kGlobal &&
+        branch_type !=
+            SchedulerLoopQuarantineBranchType::kAdvancedMemorySafetyChecks) {
       config_entry =
           config_current_process->FindDict(kBranchTypeThreadLocalDefaultStr);
     }
