2a8841db84fc246f5331d3cfee9497d966e4dfd3
Paul Adedeji <pauladedeji@google.com>
2025-08-28 13:07:06 -0700

    [ntp-composebox] De-duplicate composebox localized strings.
    Moves composebox strings from lens and ntp's UI into
    `SearchboxHandler::SetupWebUIDataSource`, which they both call already.
    
    Change-Id: I39c4184025816a4ee5fad6fa5f6d20c5d94d1186
    Bug: 441503800
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6892073
    Auto-Submit: Paul Adedeji <pauladedeji@google.com>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1507963}

--- chrome/browser/ui/lens/lens_side_panel_untrusted_ui.cc
+++ chrome/browser/ui/lens/lens_side_panel_untrusted_ui.cc
@@ -35,36 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/webui/resources/cr_components/composebox/composebox.mojom.h"
 #include "ui/webui/webui_util.h"
 
-namespace {
-static constexpr webui::LocalizedString kStrings[] = {
-    // Composebox.
-    {"composeboxCancelButtonTitle", IDS_NTP_COMPOSE_CANCEL_BUTTON_A11Y_LABEL},
-    {"composeboxCancelButtonTitleInput",
-     IDS_NTP_COMPOSE_CANCEL_BUTTON_A11Y_LABEL_INPUT},
-    {"composeboxImageUploadButtonTitle",
-     IDS_NTP_COMPOSE_IMAGE_UPLOAD_BUTTON_A11Y_LABEL},
-    {"composeboxPdfUploadButtonTitle",
-     IDS_NTP_COMPOSE_PDF_UPLOAD_BUTTON_A11Y_LABEL},
-    {"composeboxPlaceholderText", IDS_NTP_COMPOSE_PLACEHOLDER_TEXT},
-    {"composeboxSubmitButtonTitle", IDS_NTP_COMPOSE_SUBMIT_BUTTON_A11Y_LABEL},
-    {"composeboxDeleteFileTitle", IDS_NTP_COMPOSE_DELETE_FILE_A11Y_LABEL},
-    {"composeboxFileUploadStartedText",
-     IDS_NTP_COMPOSE_FILE_UPLOAD_STARTED_A11Y_TEXT},
-    {"composeboxFileUploadCompleteText",
-     IDS_NTP_COMPOSE_FILE_UPLOAD_COMPLETE_A11Y_TEXT},
-    {"composeboxFileUploadInvalidEmptySize",
-     IDS_NTP_COMPOSE_FILE_UPLOAD_INVALID_EMPTY_SIZE},
-    {"composeboxFileUploadInvalidTooLarge",
-     IDS_NTP_COMPOSE_FILE_UPLOAD_INVALID_TOO_LARGE},
-    {"composeboxFileUploadImageProcessingError",
-     IDS_NTP_COMPOSE_FILE_UPLOAD_IMAGE_PROCESSING_ERROR},
-    {"composeboxFileUploadValidationFailed",
-     IDS_NTP_COMPOSE_FILE_UPLOAD_VALIDATION_FAILED},
-    {"composeboxFileUploadFailed", IDS_NTP_COMPOSE_FILE_UPLOAD_FAILED},
-    {"composeboxFileUploadExpired", IDS_NTP_COMPOSE_FILE_UPLOAD_EXPIRED},
-};
-}  // namespace
-
 namespace lens {
 
 LensSidePanelUntrustedUI::LensSidePanelUntrustedUI(content::WebUI* web_ui)
@@ -179,8 +149,6 @@ LensSidePanelUntrustedUI::LensSidePanelUntrustedUI(content::WebUI* web_ui)
       network::mojom::CSPDirectiveName::StyleSrc,
       "style-src 'self' chrome-untrusted://resources chrome-untrusted://theme");
 
-  // ComposeBox LoadTimeData
-  html_source->AddLocalizedStrings(kStrings);
   // Support no file types.
   html_source->AddString("composeboxImageFileTypes", "");
   html_source->AddString("composeboxAttachmentFileTypes", "");
