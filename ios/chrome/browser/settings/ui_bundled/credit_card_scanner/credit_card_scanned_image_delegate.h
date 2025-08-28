544ae2c95941996e0340455d519488740d967d78
Stephen McGruer <smcgruer@google.com>
2025-08-28 11:32:54 -0700

    Reapply "[iOS] Add credit card scanner prototype (3/4)"
    This reverts commit daf553ee810c6319b94d72e3b0c794bc50e7d01a.
    
    This CL is the third in a chain of CLs that add a prototype credit card
    scanner for iOS, which can be triggered when adding cards manually via
    Payment Method settings. The prototype resurrects and improves upon a
    previous ~2019 attempt that was removed in
    https://chromium-review.googlesource.com/c/chromium/src/+/2534781
    
    This CL adds a camera scanner which is launched upon clicking the 'Use
    Camera' button. Currently it does not do anything with the images
    captured. The next CL will add processing and extracting of card data.
    
    The CL was originally reverted due to an unexpectedly added static
    initializer, caused by using CGMake() in an anonymous namespace
    declared function. This was fixed by moving to `constexpr` for the
    variables and not calling `CGMake`.
    
    Bug: 435324025
    Change-Id: I8136e2b276f75eea7d81ff91f6930d0b15dbe48a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6896806
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1507906}

---
+++ ios/chrome/browser/settings/ui_bundled/credit_card_scanner/credit_card_scanned_image_delegate.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SETTINGS_UI_BUNDLED_CREDIT_CARD_SCANNER_CREDIT_CARD_SCANNED_IMAGE_DELEGATE_H_
+#define IOS_CHROME_BROWSER_SETTINGS_UI_BUNDLED_CREDIT_CARD_SCANNER_CREDIT_CARD_SCANNED_IMAGE_DELEGATE_H_
+
+#import <CoreMedia/CoreMedia.h>
+
+// A delegate notified when there is a new credit card image.
+@protocol CreditCardScannedImageDelegate
+
+// Receives an image from the `viewport` and processes it in the implementation.
+// `viewport` defines the rectangle on the screen where a credit card should be
+// positioned to be scanned.
+- (void)processOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
+                         viewport:(CGRect)viewport;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_SETTINGS_UI_BUNDLED_CREDIT_CARD_SCANNER_CREDIT_CARD_SCANNED_IMAGE_DELEGATE_H_
