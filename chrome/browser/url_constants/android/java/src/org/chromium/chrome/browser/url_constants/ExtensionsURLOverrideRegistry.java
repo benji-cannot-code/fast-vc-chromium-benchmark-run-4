f9c85de7e4f8c3680c5c7621a8c02483e935ecc1
Fiaz Muhammad <mfiaz@google.com>
2025-08-28 09:05:44 -0700

    [Url Override] Init classes in chrome/browser/url_constants/android
    - Inits module, chrome/browser/url_constants/android.
    - Init classes, UrlConstantResolver, UrlConstantResolverFactory,
    	ExtensionsURLOverrideRegistry.
    
    Bug: 439831443
    Change-Id: I48803733c8a0a177d557be6fb1a850512757232a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6896016
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Auto-Submit: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1507786}

---
+++ chrome/browser/url_constants/android/java/src/org/chromium/chrome/browser/url_constants/ExtensionsURLOverrideRegistry.java
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.url_constants;
+
+import org.chromium.build.annotations.NullMarked;
+
+/** Used to store information on whether extensions have overridden URLs. */
+@NullMarked
+public class ExtensionsURLOverrideRegistry {
+    // Prevent instantiation.
+    private ExtensionsURLOverrideRegistry() {}
+}
---
+++ chrome/browser/url_constants/android/java/src/org/chromium/chrome/browser/url_constants/UrlConstantResolverFactory.java
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.url_constants;
+
+import org.chromium.build.annotations.NullMarked;
+
+/** Creates instances of {@link UrlConstantResolver}, keyed to profiles. */
+@NullMarked
+public class UrlConstantResolverFactory {
+    // Prevent instantiation.
+    private UrlConstantResolverFactory() {}
+}
