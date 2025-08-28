a6db3918e39a043e905b9807504f22c5a6aba180
Bryant Chandler <bryantchandler@google.com>
2025-08-28 14:11:10 -0700

    feat(glic): Add GlicSidePanelUi stub
    This change introduces a stub implementation for `GlicSidePanelUi`,
    which will be used for the side panel version of the Glic UI.
    
    All methods in the stub currently call `NOTIMPLEMENTED()`.
    
    Bug: b:441737518
    Change-Id: I6a6a696491cbb8949f68c25dcbbdeca0064c5c11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6896891
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1508001}

---
+++ chrome/browser/glic/widget/glic_side_panel_ui.cc
@@ -0,0 +1,52 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/glic/widget/glic_side_panel_ui.h"
+
+#include "base/notimplemented.h"
+
+namespace glic {
+
+GlicSidePanelUi::GlicSidePanelUi() = default;
+GlicSidePanelUi::~GlicSidePanelUi() = default;
+
+const mojom::PanelState& GlicSidePanelUi::GetPanelState() const {
+  NOTIMPLEMENTED();
+  return panel_state_;
+}
+
+void GlicSidePanelUi::Resize(const gfx::Size& size,
+                             base::TimeDelta duration,
+                             base::OnceClosure callback) {
+  NOTIMPLEMENTED();
+}
+
+void GlicSidePanelUi::SetDraggableAreas(
+    const std::vector<gfx::Rect>& draggable_areas) {
+  NOTIMPLEMENTED();
+}
+
+void GlicSidePanelUi::EnableDragResize(bool enabled) {
+  NOTIMPLEMENTED();
+}
+
+void GlicSidePanelUi::Attach() {
+  NOTIMPLEMENTED();
+}
+
+void GlicSidePanelUi::Detach() {
+  NOTIMPLEMENTED();
+}
+
+void GlicSidePanelUi::SetMinimumWidgetSize(const gfx::Size& size) {
+  NOTIMPLEMENTED();
+}
+
+bool GlicSidePanelUi::IsShowing() const {
+  NOTIMPLEMENTED();
+  return false;
+}
+
+}  // namespace glic
