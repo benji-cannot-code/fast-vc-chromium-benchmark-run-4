6737ba1c9045edf5b4e1c5f5eaad6e0d50275361
Calder Kitagawa <ckitagawa@chromium.org>
2025-08-28 16:41:25 -0700

    [Tab Switcher] Deflake modal dialog manager presence
    Use the supplier for the modal dialog manager rather than trying to
    outright get it during setup when it may still be null.
    
    Fixed: 441591498
    Change-Id: Ib1daebbfe37599f7d0357a12ef21cb09a930adc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6896469
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1508099}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherLayoutTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherLayoutTest.java
@@ -70,6 +70,7 @@ import org.junit.runner.RunWith;
 import org.chromium.base.Callback;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.Token;
+import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
@@ -141,7 +142,7 @@ public class TabSwitcherLayoutTest {
     private final List<WeakReference<Bitmap>> mAllBitmaps = new LinkedList<>();
     private final Callback<Bitmap> mBitmapListener =
             (bitmap) -> mAllBitmaps.add(new WeakReference<>(bitmap));
-    private ModalDialogManager mModalDialogManager;
+    private ObservableSupplier<ModalDialogManager> mModalDialogManagerSupplier;
     private RegularNewTabPageStation mNtp;
 
     @Before
@@ -158,7 +159,8 @@ public class TabSwitcherLayoutTest {
         cta.getTabContentManager().setCaptureMinRequestTimeForTesting(0);
 
         CriteriaHelper.pollUiThread(cta.getTabModelSelector()::isTabStateInitialized);
-        mModalDialogManager = ThreadUtils.runOnUiThreadBlocking(cta::getModalDialogManager);
+        mModalDialogManagerSupplier =
+                ThreadUtils.runOnUiThreadBlocking(cta::getModalDialogManagerSupplier);
     }
 
     @After
@@ -1362,21 +1364,28 @@ public class TabSwitcherLayoutTest {
     private void verifyModalDialogShowingAnimationCompleteInTabSwitcher() {
         CriteriaHelper.pollUiThread(
                 () -> {
-                    Criteria.checkThat(mModalDialogManager.isShowing(), Matchers.is(true));
+                    ModalDialogManager modalDialogManager = mModalDialogManagerSupplier.get();
+                    Criteria.checkThat(modalDialogManager, Matchers.notNullValue());
+                    Criteria.checkThat(modalDialogManager.isShowing(), Matchers.is(true));
                 });
     }
 
     private void verifyModalDialogHidingAnimationCompleteInTabSwitcher() {
         CriteriaHelper.pollUiThread(
                 () -> {
-                    Criteria.checkThat(mModalDialogManager.isShowing(), Matchers.is(false));
+                    ModalDialogManager modalDialogManager = mModalDialogManagerSupplier.get();
+                    Criteria.checkThat(modalDialogManager, Matchers.notNullValue());
+                    Criteria.checkThat(modalDialogManager.isShowing(), Matchers.is(false));
                 });
     }
 
     private void dismissAllModalDialogs() {
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
-                    mModalDialogManager.dismissAllDialogs(DialogDismissalCause.UNKNOWN);
+                    ModalDialogManager modalDialogManager = mModalDialogManagerSupplier.get();
+                    if (modalDialogManager == null) return;
+
+                    modalDialogManager.dismissAllDialogs(DialogDismissalCause.UNKNOWN);
                 });
     }
 
