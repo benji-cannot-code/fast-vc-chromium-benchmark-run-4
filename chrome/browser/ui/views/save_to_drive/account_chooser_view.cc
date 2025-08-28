0908b00731265b872ae890fc9327bbc2535f4eba
Tiffany Shen <tishen@google.com>
2025-08-28 10:13:56 -0700

    [Save PDF to Drive] Restructure build files.
    Modularizes c/b/u/v/save_to_drive build file to have save_to_drive, test
    support, and unit tests.
    
    Change-Id: I477776f71071ff8cc14b5e4411ed66c47b88a6c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6891179
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Tiffany Shen <tishen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1507803}

--- chrome/browser/ui/views/save_to_drive/account_chooser_view.cc
+++ chrome/browser/ui/views/save_to_drive/account_chooser_view.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/save_to_drive/account_chooser_view.h"
 
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
-#include "chrome/browser/ui/views/controls/hover_button.h"
 #include "chrome/browser/ui/views/save_to_drive/account_chooser_radio_group_view.h"
 #include "chrome/browser/ui/views/save_to_drive/account_chooser_util.h"
 #include "chrome/grit/generated_resources.h"
