bc154c0691e6c14af3bdd87eb7168f77e61bfb10
Louis Romero <lpromero@google.com>
2025-08-28 07:48:43 -0700

    [iOS] Add SF Symbols to Help menu commands
    This change adds SF Symbols to the Help menu commands to improve
    visual consistency with the rest of the application.
    
    The following commands have been updated with symbols:
    - Show Help (questionmark.circle)
    - Report an Issue (exclamationmark.bubble)
    
    Screenshot: https://screenshot.googleplex.com/7qEbH75jeBjAF4F
    
    Bug: 438114271
    Change-Id: Ic21e83a12d0316aa964807f4c569c2675cd1bc0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6865810
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1507742}

--- ios/chrome/browser/keyboard/ui_bundled/UIKeyCommand+Chrome.mm
+++ ios/chrome/browser/keyboard/ui_bundled/UIKeyCommand+Chrome.mm
@@ -290,10 +290,12 @@ const char kMobileKeyCommandClose[] = "MobileKeyCommandClose";
 }
 
 + (UIKeyCommand*)cr_showHelp {
+  UIImage* image = DefaultSymbolWithConfiguration(kHelpSymbol, nil);
   return [self cr_commandWithInput:@"?"
                      modifierFlags:AltCommand
                             action:@selector(keyCommand_showHelp)
-                   titleIDAsString:@"IDS_IOS_KEYBOARD_SHOW_HELP"];
+                   titleIDAsString:@"IDS_IOS_KEYBOARD_SHOW_HELP"
+                             image:image];
 }
 
 + (UIKeyCommand*)cr_showDownloads {
@@ -383,10 +385,13 @@ const char kMobileKeyCommandClose[] = "MobileKeyCommandClose";
 }
 
 + (UIKeyCommand*)cr_reportAnIssue {
+  UIImage* image =
+      DefaultSymbolWithConfiguration(kExclamationMarkBubbleSymbol, nil);
   return [self cr_commandWithInput:@"i"
                      modifierFlags:ShiftCommand
                             action:@selector(keyCommand_reportAnIssue)
-                   titleIDAsString:@"IDS_IOS_KEYBOARD_REPORT_AN_ISSUE"];
+                   titleIDAsString:@"IDS_IOS_KEYBOARD_REPORT_AN_ISSUE"
+                             image:image];
 }
 
 + (UIKeyCommand*)cr_reportAnIssue_2 {
