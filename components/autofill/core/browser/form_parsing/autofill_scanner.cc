fe5b8181cb7aa9b8fff3439146a20a08a50ff417
Christoph Schwering <schwering@google.com>
2025-08-28 01:56:53 -0700

    [Autofill] Remove VectorExperimental in form parsing
    The VectorExperimental annotation suppresses dangling-pointer checks.
    
    It complicates boilerplate code in the followup CLs because we sometimes
    have std::vector<raw_ptr<AutofillField>> and sometimes
     std::vector<raw_ptr<AutofillField, VectorExperimental>>.
    
    Bug: 40100455
    Change-Id: I68cafbc87ecc97e0f6858eb3abe506f2c407c9b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6891014
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1507595}

--- components/autofill/core/browser/form_parsing/autofill_scanner.cc
+++ components/autofill/core/browser/form_parsing/autofill_scanner.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace autofill {
 
 AutofillScanner::AutofillScanner(
-    const std::vector<raw_ptr<AutofillField, VectorExperimental>>& fields) {
+    const std::vector<raw_ptr<AutofillField>>& fields) {
   Init(fields);
 }
 
@@ -65,8 +65,7 @@ size_t AutofillScanner::CursorPosition() {
   return static_cast<size_t>(cursor_ - begin_);
 }
 
-void AutofillScanner::Init(
-    const std::vector<raw_ptr<AutofillField, VectorExperimental>>& fields) {
+void AutofillScanner::Init(const std::vector<raw_ptr<AutofillField>>& fields) {
   cursor_ = fields.begin();
   saved_cursor_ = fields.begin();
   begin_ = fields.begin();
