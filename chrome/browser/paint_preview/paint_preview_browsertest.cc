46a529cd3854d8740b4a35d703f89dacf97e1cd8
Chris Fredrickson <cfredric@chromium.org>
2025-08-28 05:38:13 -0700

    [Paint Preview] Make document_guid member private and non-const
    This is another way of preventing client code from mutating this field,
    but this way the struct's move-ctor and move-assignment operator can be
    defaulted if needed.
    
    Change-Id: Id7fe1c3a926f374039ac2b674b41cebf8dc4ff7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6893687
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1507673}

--- chrome/browser/paint_preview/paint_preview_browsertest.cc
+++ chrome/browser/paint_preview/paint_preview_browsertest.cc
@@ -184,7 +184,7 @@ IN_PROC_BROWSER_TEST_P(PaintPreviewBrowserTest, CaptureFrame) {
   client->CapturePaintPreview(params, GetWebContents()->GetPrimaryMainFrame(),
                               future.GetCallback());
   auto [guid, status, result] = future.Take();
-  EXPECT_EQ(guid, params.inner.document_guid);
+  EXPECT_EQ(guid, params.inner.get_document_guid());
   EXPECT_EQ(status, mojom::PaintPreviewStatus::kOk);
   EXPECT_TRUE(result->proto.has_root_frame());
   EXPECT_EQ(result->proto.subframes_size(), 0);
@@ -219,7 +219,7 @@ IN_PROC_BROWSER_TEST_P(PaintPreviewBrowserTest,
                               future.GetCallback());
 
   auto [guid, status, result] = future.Take();
-  EXPECT_EQ(guid, params.inner.document_guid);
+  EXPECT_EQ(guid, params.inner.get_document_guid());
   EXPECT_EQ(status, mojom::PaintPreviewStatus::kOk);
   EXPECT_TRUE(result->proto.has_root_frame());
   EXPECT_EQ(result->proto.subframes_size(), 1);
@@ -275,7 +275,7 @@ IN_PROC_BROWSER_TEST_P(PaintPreviewFencedFrameBrowserTest,
   auto [guid, status, result] = future.Take();
   // This callback should have a success result without any DCHECK
   // error.
-  EXPECT_EQ(guid, params.inner.document_guid);
+  EXPECT_EQ(guid, params.inner.get_document_guid());
   EXPECT_EQ(status, mojom::PaintPreviewStatus::kOk);
   EXPECT_TRUE(result->proto.has_root_frame());
   EXPECT_EQ(result->proto.subframes_size(), 2);
@@ -338,7 +338,7 @@ IN_PROC_BROWSER_TEST_P(PaintPreviewFencedFrameBrowserTest,
   auto [guid, status, result] = future.Take();
   // This callback should have a partial success result since the
   // fenced frame has been removed during running the capture.
-  EXPECT_EQ(guid, params.inner.document_guid);
+  EXPECT_EQ(guid, params.inner.get_document_guid());
   EXPECT_EQ(status, mojom::PaintPreviewStatus::kPartialSuccess);
   EXPECT_TRUE(result->proto.has_root_frame());
   EXPECT_EQ(result->proto.subframes_size(), 1);
@@ -383,7 +383,7 @@ IN_PROC_BROWSER_TEST_P(PaintPreviewBrowserTest,
   client->CapturePaintPreview(params, GetWebContents()->GetPrimaryMainFrame(),
                               future.GetCallback());
   auto [guid, status, result] = future.Take();
-  EXPECT_EQ(guid, params.inner.document_guid);
+  EXPECT_EQ(guid, params.inner.get_document_guid());
   EXPECT_EQ(status, mojom::PaintPreviewStatus::kOk);
   EXPECT_TRUE(result->proto.has_root_frame());
   EXPECT_EQ(result->proto.subframes_size(), 1);
@@ -429,7 +429,7 @@ IN_PROC_BROWSER_TEST_P(PaintPreviewBrowserTest,
   client->CapturePaintPreview(params, GetWebContents()->GetPrimaryMainFrame(),
                               future.GetCallback());
   auto [guid, status, result] = future.Take();
-  EXPECT_EQ(guid, params.inner.document_guid);
+  EXPECT_EQ(guid, params.inner.get_document_guid());
   EXPECT_EQ(status, mojom::PaintPreviewStatus::kOk);
   EXPECT_TRUE(result->proto.has_root_frame());
   EXPECT_EQ(result->proto.subframes_size(), 0);
--- components/paint_preview/common/capture_result.cc
+++ components/paint_preview/common/capture_result.cc
@@ -8,10 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace paint_preview {
 
 RecordingParams::RecordingParams(const base::UnguessableToken& document_guid)
-    : document_guid(document_guid),
-      is_main_frame(false),
+    : is_main_frame(false),
       capture_links(true),
-      max_capture_size(0) {}
+      max_capture_size(0),
+      document_guid(document_guid) {}
 
 CaptureResult::CaptureResult(RecordingPersistence persistence)
     : persistence(persistence) {}
