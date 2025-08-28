049347c4bdeab71e49e7a3e52e67adff91d01145
Jonathan Lee <jonathanjlee@google.com>
2025-08-28 10:31:48 -0700

    [gtest] Clean up single-arg `Invoke()`s in `//c/saved_tab_groups/`
    Not needed and deprecated [0]. This CL should be a no-op.
    
    [0]: https://chromium.googlesource.com/external/github.com/google/googletest.git/+/a05c0915074bcd1b82f232e081da9bb6c205c28d/googlemock/include/gmock/gmock-actions.h#2046
    
    Bug: 439838457
    Change-Id: I47ab90e382479c1a3d2591200b57b9fbe434caaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6892302
    Auto-Submit: Jonathan Lee <jonathanjlee@google.com>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1507833}

--- components/saved_tab_groups/internal/shared_tab_group_account_data_sync_bridge_unittest.cc
+++ components/saved_tab_groups/internal/shared_tab_group_account_data_sync_bridge_unittest.cc
@@ -32,7 +32,6 @@ using base::test::EqualsProto;
 using testing::_;
 using testing::DefaultValue;
 using testing::Eq;
-using testing::Invoke;
 using testing::Matcher;
 using testing::Not;
 using testing::Return;
@@ -213,9 +212,9 @@ class SharedTabGroupAccountDataSyncBridgeTest : public testing::Test {
 
     base::RunLoop run_loop;
     base::RepeatingClosure quit_closure = run_loop.QuitClosure();
-    EXPECT_CALL(processor_, ModelReadyToSync).WillOnce(Invoke([&]() {
+    EXPECT_CALL(processor_, ModelReadyToSync).WillOnce([&]() {
       quit_closure.Run();
-    }));
+    });
 
     if (!model_) {
       model_ = std::make_unique<SavedTabGroupModel>();
