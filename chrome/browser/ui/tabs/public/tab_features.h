9519a997183312d7952932cac4289dc12aa63ab6
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2025-08-28 12:54:02 -0700

    Revert "[context-input] Add tab_contextualization_controller as a tab feature"
    This reverts commit 72701822ba0707d70c5f4eb157e9133b99283f21.
    
    Reason for revert:
    LUCI Bisection has identified this change as the culprit of a build failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/compile-analysis/b/8705270643597048497
    
    Sample failed build: https://ci.chromium.org/b/8705270643597048497
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F6884052&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Fcompile-analysis%2Fb%2F8705270643597048497&type=BUG
    
    Original change's description:
    > [context-input] Add tab_contextualization_controller as a tab feature
    >
    > This CL adds boilerplate code for the tab contextualization controller
    > which will be used to retrieve page content from a tab attached to the
    > composebox as context input. The tab contextualization controller is
    > also added as a member of `TabFeatures`.
    >
    > Bug: 441081554
    > Change-Id: I0538aede14fe1650e1bff996a25c7a0051aeba06
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6884052
    > Reviewed-by: Dana Fried <dfried@chromium.org>
    > Commit-Queue: Ananya Seelam <ananyaseelam@google.com>
    > Reviewed-by: Jason Hu <hujasonx@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1507802}
    >
    
    Bug: 441081554
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I695b6bfe674a06167f5e3e8ab07b81b9cc0cb0ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6897562
    Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1507955}

--- chrome/browser/ui/tabs/public/tab_features.h
+++ chrome/browser/ui/tabs/public/tab_features.h
@@ -110,10 +110,6 @@ namespace tab_groups {
 class CollaborationMessagingTabData;
 }  // namespace tab_groups
 
-namespace lens {
-class TabContextualizationController;
-}  // namespace lens
-
 namespace tabs {
 
 class TabAlertController;
@@ -418,9 +414,6 @@ class TabFeatures {
       ask_before_http_dialog_controller_;
 
   std::unique_ptr<actor::ActorTabData> actor_tab_data_;
-
-  std::unique_ptr<lens::TabContextualizationController>
-      tab_contextualization_controller_;
 
   // Must be the last member.
   base::WeakPtrFactory<TabFeatures> weak_factory_{this};
