200368606795fc27c4a429b4f437ff94e58d24fc
Abhishek Shanthkumar <abhishek.shanthkumar@microsoft.com>
2025-08-28 06:46:39 -0700

    IDB: SQLite - store primary key in index_references
    This optimization no-ops the ORDER BY clause of the IndexRecordIterator
    query, leading to a 70x reduction in the average run time of the
    owp_storage.mail-client-read.html Blink perf test (reduced from 5739 ms
    to 81 ms).
    
    The index iterator query is also simplified for the
    first_primary_keys_only case:
    
    1. The object_store_position and target_primary_key constraints are
    omitted as per
    https://w3c.github.io/IndexedDB/#cursor-iteration-operation
    
    2. The retained constraints are checked before the GROUP BY clause so
    that the summarization operation (MIN(primary_key)) occurs on a reducing
    set of rows as the iterator advances.
    
    Detailed rationale is in the optimization doc:
    https://docs.google.com/document/d/1swWIwHDgLr0NpP8XxrM93HabpS6Mz7x9kG-bK0bM7NE/edit?tab=t.0#heading=h.vz9iw1w8y5ym
    
    Bug: 433318798
    Change-Id: I3c23837302630b9a0dbcffe9e7fe84883e4390ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6764828
    Commit-Queue: Abhishek Shanthkumar <abhishek.shanthkumar@microsoft.com>
    Reviewed-by: Evan Stade <evanstade@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1507701}

--- content/browser/indexed_db/instance/backing_store.h
+++ content/browser/indexed_db/instance/backing_store.h
@@ -55,7 +55,7 @@ class BackingStore {
     // ignore them.
     // SQLite: a row id. LevelDB: a version.
     int64_t number;
-    // SQLite: unused. LevelDB: the *encoded* primary key bytes.
+    // SQLite and LevelDB: the *encoded* primary key bytes.
     std::string data;
   };
 
