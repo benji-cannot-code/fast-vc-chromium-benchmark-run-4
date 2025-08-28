2b22b587d4a596c3e97da3f62111d8e6c7feb013
dongguoz <dongguoz@google.com>
2025-08-28 01:11:02 -0700

    refactor(plus_addresses): Move plus_address_cache to core/browser
    This CL moves the plus_address_cache to `/core/browser`.
    This is to better align with the layered component design.
    
    This is a pure refactoring CL and does not introduce any logic changes.
    
    The code is mainly generated by mass-rename script and build.gn is
    updated by the shell script.
    
    Bypass-Check-License: Moving files
    Bug: 365494310
    Change-Id: I622f0393fe58d6411a39ca65b834d4700e68820d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6882796
    Commit-Queue: Dongguo Zhang <dongguo@google.com>
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1507581}

--- components/plus_addresses/core/browser/plus_address_cache.h
+++ components/plus_addresses/core/browser/plus_address_cache.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef COMPONENTS_PLUS_ADDRESSES_PLUS_ADDRESS_CACHE_H_
-#define COMPONENTS_PLUS_ADDRESSES_PLUS_ADDRESS_CACHE_H_
+#ifndef COMPONENTS_PLUS_ADDRESSES_CORE_BROWSER_PLUS_ADDRESS_CACHE_H_
+#define COMPONENTS_PLUS_ADDRESSES_CORE_BROWSER_PLUS_ADDRESS_CACHE_H_
 
 #include <memory>
 #include <string>
@@ -66,4 +66,4 @@ class PlusAddressCache {
 
 }  // namespace plus_addresses
 
-#endif  // COMPONENTS_PLUS_ADDRESSES_PLUS_ADDRESS_CACHE_H_
+#endif  // COMPONENTS_PLUS_ADDRESSES_CORE_BROWSER_PLUS_ADDRESS_CACHE_H_
