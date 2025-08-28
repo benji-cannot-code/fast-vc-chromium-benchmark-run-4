73b08f5ad39e50d04ddb5364e4dad47f92190db4
Alex Ilin <alexilin@chromium.org>
2025-08-28 03:36:55 -0700

    [signin] Change the fetch account capabilities URL on Desktop
    This CL migrates the AccountCapabilitiesFetcherGaia to a new API for
    fetching account capabilities.
    
    Previously used API, "batchGet", has one important shortcoming: the
    whole request fails if the server fails to evaluate any of the requested
    account capabilities, meaning that Chrome will not receive any of the
    capability values.
    
    Such a scenario may occur when a new capability is added to Chrome
    before it's been fully rolled out on the server. Another potential
    scenario is when a capability gets deprecated and removed on the server
    but old Chrome clients keep requesting it.
    
    The new "getAllVisible" API returns instead the list of all account
    capabilities exposed to Chrome in the server configuration. Given that
    Chrome was always requesting the full capabilities list with the old
    API, the new approach doesn't change the amount of transferred data while
    ensuring that Chrome receives all available capability values.
    
    Enabled-by-default-reason: killswitch
    Bug: 436151197
    Change-Id: I7ddb1f8ed0073d10a91a1bb755d34045b8e49b90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6859058
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1507632}

--- google_apis/gaia/gaia_features.cc
+++ google_apis/gaia/gaia_features.cc
@@ -5,12 +5,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "google_apis/gaia/gaia_features.h"
 
+#include "base/feature_list.h"
+
 namespace gaia::features {
 
 // Enables binary format parsing in the /ListAccounts Gaia call. The endpoint
 // response depends on the presence of laf=b64bin parameter in the called url.
 BASE_FEATURE(kListAccountsUsesBinaryFormat,
              "ListAccountsUsesBinaryFormat",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
+// Switches account capabilities fetch to the "getAllVisible" URL that fetches
+// all capabilities visible to Chrome instead of asking for a hardcoded list of
+// capabilities that might be only partially available.
+COMPONENT_EXPORT(GOOGLE_APIS)
+BASE_FEATURE(kGetAccountCapabilitiesUsesGetAllVisibleUrl,
+             "GetAccountCapabilitiesUsesGetAllVisibleUrl",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace gaia::features
