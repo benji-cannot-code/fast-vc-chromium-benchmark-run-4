cee9b67d238a3275bbbb88a2a405cfbac34dd826
Paulina Gacek <paulinagacek@google.com>
2025-08-28 05:26:40 -0700

    iwa: Use IwaVersion in InstallIsolatedWebAppCommandSuccess
    Bug: 437038363
    Change-Id: I82b79808b0fd4089048a0600437465a64b4751ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6881050
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Commit-Queue: Paulina Gacek <paulinagacek@google.com>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1507668}

--- chrome/browser/web_applications/isolated_web_apps/policy/isolated_web_app_installer.h
+++ chrome/browser/web_applications/isolated_web_apps/policy/isolated_web_app_installer.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_forward.h"
 #include "base/memory/raw_ptr.h"
 #include "base/values.h"
-#include "base/version.h"
 #include "chrome/browser/web_applications/isolated_web_apps/isolated_web_app_install_source.h"
 #include "chrome/browser/web_applications/isolated_web_apps/isolated_web_app_url_info.h"
 #include "chrome/browser/web_applications/isolated_web_apps/policy/isolated_web_app_external_install_options.h"
