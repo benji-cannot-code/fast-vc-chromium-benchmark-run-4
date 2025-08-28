f5bac622c74717cdde6e5534a72aa953b65546eb
Mohamed Amir Yosef <mamir@chromium.org>
2025-08-28 14:51:23 -0700

    [DC] Ship Digital Credentials API (presentation support)
    The API has been approved for shipping.
    https://chromestatus.com/feature/5166035265650688
    
    I2S: https://groups.google.com/a/chromium.org/g/blink-dev/c/cPLAQLj8nV0
    
    Bug: 398031907, 385174612
    Change-Id: Ib9f523d9fa048852b31891c32d2b8089dd931966
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6894996
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1508034}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -288,7 +288,7 @@ void SetRuntimeFeaturesFromChromiumFeatures() {
            kSetOnlyIfOverridden},
           {wf::EnableWebIdentityDigitalCredentials,
            raw_ref(features::kWebIdentityDigitalCredentials),
-           kSetOnlyIfOverridden},
+           kDefault},
           {wf::EnableWebIdentityDigitalCredentialsCreation,
            raw_ref(features::kWebIdentityDigitalCredentialsCreation),
            kSetOnlyIfOverridden},
