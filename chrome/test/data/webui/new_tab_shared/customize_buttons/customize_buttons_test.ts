67c5ffa48b1a9c9ba9450c49fe497a650b4eb6a8
Paul Adedeji <pauladedeji@google.com>
2025-08-28 10:16:05 -0700

    [ntp footer] Remove shadow from customize chrome button.
    Also adds browser test suite for customize chrome button.
    
    Change-Id: I33a15e99fd4a230db85e2bf2f482ee98180dc6dc
    Bug: 423030388
    Fixed: 423030388
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6634719
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1507806}

---
+++ chrome/test/data/webui/new_tab_shared/customize_buttons/customize_buttons_test.ts
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import type {CustomizeButtonsElement} from 'chrome://newtab-shared/customize_buttons/customize_buttons.js';
+import {assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
+import {hasStyle, microtasksFinished} from 'chrome://webui-test/test_util.js';
+
+suite('CustomizeButtons', () => {
+  let element: CustomizeButtonsElement;
+
+  setup(async () => {
+    document.body.innerHTML = window.trustedTypes!.emptyHTML;
+    element = document.createElement('ntp-customize-buttons');
+    document.body.appendChild(element);
+    await microtasksFinished();
+  });
+
+  test('shows shadow according to property', async () => {
+    const button = element.$.customizeButton;
+    assertTrue(hasStyle(button, 'box-shadow', 'none'));
+
+    element.showShadow = true;
+    await microtasksFinished();
+    assertFalse(hasStyle(button, 'box-shadow', 'none'));
+
+    element.showBackgroundImage = true;
+    await microtasksFinished();
+    assertTrue(hasStyle(button, 'box-shadow', 'none'));
+  });
+});
