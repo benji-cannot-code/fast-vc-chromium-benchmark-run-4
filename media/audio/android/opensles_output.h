44b433ed193a5cd36cc60c859f6d59c1ba3b4ce1
Matt Stark <msta@google.com>
2025-08-27 23:44:20 -0700

    Fix missing include required for android
    Bug: 40263312
    Change-Id: I6a6a69644d342a9ca4f9cf7f776ddfa1be9a711f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6893626
    Auto-Submit: Matt Stark <msta@google.com>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Owners-Override: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1507553}

--- media/audio/android/opensles_output.h
+++ media/audio/android/opensles_output.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
+#include <array>
 #include <memory>
 
 #include "base/compiler_specific.h"
