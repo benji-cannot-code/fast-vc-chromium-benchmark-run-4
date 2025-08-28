7f7549839aab4f5f6c42fb8be02cee569bc4e2e0
dongguoz <dongguoz@google.com>
2025-08-28 05:20:35 -0700

    refactor(plus_addresses): Move test_environment to core/browser
    This CL moves the PlusAddressTestEnvironment to `/core/browser`.
    This is to better align with the layered component design.
    
    This is a pure refactoring CL and does not introduce any logic changes.
    
    The code is mainly generated by mass-rename script and build.gn is
    updated by the shell script.
    
    Bypass-Check-License: Moving files
    Bug: 365494310
    Change-Id: Iced2ab9197373aae3147d78f615d88f3034a483b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6883163
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Dongguo Zhang <dongguo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1507666}

--- components/plus_addresses/core/browser/plus_address_test_environment.cc
+++ components/plus_addresses/core/browser/plus_address_test_environment.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/plus_addresses/plus_address_test_environment.h"
+#include "components/plus_addresses/core/browser/plus_address_test_environment.h"
 
 #include "components/plus_addresses/core/common/plus_address_prefs.h"
 
