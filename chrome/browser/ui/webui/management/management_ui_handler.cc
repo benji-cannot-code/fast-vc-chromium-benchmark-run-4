775abbd043c8416720f7a29a6c37ffa6d846a8be
Simon Hangl <simonha@google.com>
2025-08-28 11:09:15 -0700

    Move check if any app can use multi capture to data service
    So far, the `capture_policy::IsMultiScreenCaptureAllowed` function
    was used to determine whether an entry on chrome://management should
    be shown about multi capture. This function will be deprecated soon
    and is replaced with
    `MultiCaptureDataService::IsMultiCaptureAllowedForAnyApp`.
    
    This CL
    * introduces `MultiCaptureDataService::IsMultiCaptureAllowedForAnyApp`
    * calls the function directly where it is needed (in
    `ManagementUIHandler::GetThreatProtectionInfo`)
    * removes a pre-existing unused function (`ManagementUIHandlerChromeOS::CheckGetAllScreensMediaAllowedForAnyOriginResultReceived`)
    
    Bug: 440298134
    Change-Id: Ida72da5536ea2fd6580c156de7b6cf682bd57fcd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6870552
    Commit-Queue: Simon Hangl <simonha@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1507890}

--- chrome/browser/ui/webui/management/management_ui_handler.cc
+++ chrome/browser/ui/webui/management/management_ui_handler.cc
@@ -34,6 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/enterprise/browser_management/management_service_factory.h"
 #include "chrome/browser/enterprise/identifiers/profile_id_service_factory.h"
 #include "chrome/browser/enterprise/reporting/prefs.h"
+#include "chrome/browser/media/webrtc/multi_capture/multi_capture_data_service.h"
+#include "chrome/browser/media/webrtc/multi_capture/multi_capture_data_service_factory.h"
 #include "chrome/browser/policy/chrome_browser_policy_connector.h"
 #include "chrome/browser/policy/policy_ui_utils.h"
 #include "chrome/browser/policy/profile_policy_connector.h"
@@ -503,7 +505,11 @@ base::Value::Dict ManagementUIHandler::GetThreatProtectionInfo(
   }
 
 #if BUILDFLAG(IS_CHROMEOS)
-  if (is_get_all_screens_media_allowed_for_any_origin_) {
+  const auto* multi_capture_data_service =
+      multi_capture::MultiCaptureDataServiceFactory::GetForBrowserContext(
+          profile);
+  if (multi_capture_data_service &&
+      multi_capture_data_service->IsMultiCaptureAllowedForAnyApp()) {
     AddThreatProtectionPermission(kManagementScreenCaptureEvent,
                                   kManagementScreenCaptureData, &info);
   }
