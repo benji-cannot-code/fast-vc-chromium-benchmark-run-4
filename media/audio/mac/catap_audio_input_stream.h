e67cc7872a697cd176cc14e82efd54b59bb4eeed
Olov Brändström <brandstrom@chromium.org>
2025-08-28 08:57:04 -0700

    Add an AudioGlitchInfo to AudioInputCallback in catap input stream.
    Compare the progress of capture times in the catap audio input stream,
    and the number of captured buffers, to detect any discrepancies. Report
    missing audio duration to the AudioInputCallback (i.e. sink_->OnData()).
    Also Report the missing audio to UMA histograms via
    SystemGlitchReporter.
    
    This CL is inspired by the glitch detection in the mic capture logic.
    https://source.chromium.org/chromium/chromium/src/+/main:media/audio/apple/audio_low_latency_input.cc;drc=43776165d1d46d532f5e0a184045fb62e959af6e;l=1235
    
    Change-Id: I0a737b62e91754b38a04e7712f5edc51ce179adf
    Bug: 441332583
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6874578
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Henrik Andreasson <henrika@chromium.org>
    Commit-Queue: Olov Brändström <brandstrom@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1507781}

--- media/audio/mac/catap_audio_input_stream.h
+++ media/audio/mac/catap_audio_input_stream.h
@@ -15,8 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "media/audio/agc_audio_stream.h"
+#include "media/audio/apple/glitch_helper.h"
 #include "media/audio/audio_io.h"
 #include "media/audio/mac/audio_manager_mac.h"
+#include "media/base/audio_glitch_info.h"
 #include "media/base/audio_parameters.h"
 
 @class NSError;
@@ -129,6 +131,9 @@ class MEDIA_EXPORT API_AVAILABLE(macos(14.2)) CatapAudioInputStream
   // Send log messages to the stream creator.
   void SendLogMessage(const char* format, ...);
 
+  // Called from the dtor and when the stream is reset.
+  void ReportAndResetStats();
+
   // Interface used to access the CoreAudio framework.
   const std::unique_ptr<CatapApi> catap_api_;
 
@@ -137,6 +142,9 @@ class MEDIA_EXPORT API_AVAILABLE(macos(14.2)) CatapAudioInputStream
 
   // The length of time covered by the audio data in a single audio buffer.
   const base::TimeDelta buffer_frames_duration_;
+
+  // Used to detect and report glitches.
+  GlitchHelper glitch_helper_;
 
   // One of AudioDeviceDescription::kLoopback*.
   const std::string device_id_;
--- media/base/audio_glitch_info.h
+++ media/base/audio_glitch_info.h
@@ -30,7 +30,7 @@ struct MEDIA_EXPORT AudioGlitchInfo {
 
   class MEDIA_EXPORT Accumulator;
 
-  enum class Direction { kRender, kCapture };
+  enum class Direction { kRender, kCapture, kLoopback };
 
  private:
   friend class AUHALStream;
@@ -40,6 +40,8 @@ struct MEDIA_EXPORT AudioGlitchInfo {
   friend class WASAPIAudioInputStream;
   friend class WASAPIAudioOutputStream;
   friend class AudioGlitchInfoTester;
+  friend class CatapAudioInputStream;
+  friend class GlitchHelper;
 
   // Creates a glitch with duration clamped to between 0 and 1 seconds. Also
   // logs the glitch duration to a UMA-histogram. This should only be used in OS
