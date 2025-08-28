4a17e9f02bead53fdb96f30a7a4190bdf843adde
Benjamin Williams <bwwilliams@google.com>
2025-08-28 15:42:28 -0700

    [CrossDevicePrefTracker] Introduce TimestampedPrefValue
    This change refactors CrossDevicePrefTracker to use TimestampedPrefValue
    instead of base::Value for GetValues(), GetMostRecentValue(), and
    CrossDevicePrefTracker::Observer::OnRemotePrefChanged().
    
    This provides clients with more context for each Pref value, including
    its modification timestamp.
    
    Fixed: 441332360
    Change-Id: Ifc9bb96bb5e49e2255412c3b5b192f1598b309e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6892732
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1508064}

--- components/sync_preferences/cross_device_pref_tracker_impl.h
+++ components/sync_preferences/cross_device_pref_tracker_impl.h
@@ -8,9 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <optional>
 #include <string>
+#include <string_view>
 #include <vector>
 
-#include "base/values.h"
 #include "components/sync_device_info/device_info_tracker.h"
 #include "components/sync_preferences/cross_device_pref_tracker.h"
 
@@ -34,11 +34,12 @@ class CrossDevicePrefTrackerImpl : public CrossDevicePrefTracker,
   // `CrossDevicePrefTracker` overrides
   void AddObserver(CrossDevicePrefTracker::Observer* observer) override;
   void RemoveObserver(CrossDevicePrefTracker::Observer* observer) override;
-  std::vector<base::Value> GetValues(const std::string& pref_name,
-                                     const DeviceFilter& filter) const override;
-  std::optional<base::Value> GetMostRecentValue(
-      const std::string& pref_name,
+  std::vector<CrossDevicePrefTracker::TimestampedPrefValue> GetValues(
+      std::string_view pref_name,
       const DeviceFilter& filter) const override;
+  std::optional<CrossDevicePrefTracker::TimestampedPrefValue>
+  GetMostRecentValue(std::string_view pref_name,
+                     const DeviceFilter& filter) const override;
 
   // `KeyedService` override
   void Shutdown() override;
