58a36b8bffe0b56d82ac80f8d9e0ba67c2ac5181
Lei Zhang <thestig@chromium.org>
2025-08-28 11:00:06 -0700

    Fix DCHECK_IS_CONFIGURABLE build, part 2
    Add another missing include for base/command_line.h.
    
    Bug: 441558006
    Change-Id: I2c06d75ff6f76d35af37bbb0ff0b8c05aa7ca255
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6896649
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Auto-Submit: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1507880}

--- base/check_unittest.cc
+++ base/check_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check_deref.h"
 #include "base/check_version_internal.h"
+#include "base/command_line.h"
 #include "base/dcheck_is_on.h"
 #include "base/debug/dump_without_crashing.h"
 #include "base/functional/bind.h"
