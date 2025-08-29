5f01daf72d13a2643a1621e09f781436a49f2b2d
henrika <henrika@google.com>
2025-08-29 08:13:30 -0700

    [MediaStream] Adds UMAs for GetUserMediaEchoCancellationModes
    See https://paste.googleplex.com/6232489286434816 for new logs.
    
    Bug: 428856440
    Change-Id: I2097d0e0790b0d38afe3ed1096e0fccbfdc544d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6899450
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Auto-Submit: Henrik Andreasson <henrika@chromium.org>
    Commit-Queue: Johannes Kron <kron@chromium.org>
    Reviewed-by: Johannes Kron <kron@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1508383}

--- third_party/blink/renderer/platform/mediastream/media_stream_audio_processor_options.h
+++ third_party/blink/renderer/platform/mediastream/media_stream_audio_processor_options.h
@@ -12,17 +12,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
 enum class EchoCancellationMode {
-  kDisabled,
-  kBrowserDecides,
-  kRemoteOnly,
-  kAll
+  kDisabled = 0,
+  kBrowserDecides = 1,
+  kRemoteOnly = 2,
+  kAll = 3,
+  kMaxValue = kAll,
 };
 
 extern PLATFORM_EXPORT const char kEchoCancellationModeAll[];
 extern PLATFORM_EXPORT const char kEchoCancellationModeRemoteOnly[];
 
-const char* EchoCancellationModeToString(EchoCancellationMode);
+PLATFORM_EXPORT const char* EchoCancellationModeToString(EchoCancellationMode);
 
 // The result of parsing media stream constraints.
 struct PLATFORM_EXPORT AudioProcessingProperties {
