f223fafe7eb43eebc8e9b6a6b182111ae980653f
Jason Hu <hujasonx@google.com>
2025-08-29 13:09:07 -0700

    [ntp-composebox] Attach all ContextualInputs to upload payload for non-image requests
    The query controller previously only uploaded the first ContextualInput
    for PDFs (to match the old functionality). This change makes it so that
    all ContextualInputs are attached similarly to LensOverlay.
    
    Replaces CreatePDFFileUploadPayload with
    CreateContentextualDataUploadPayload, which accepts page title and url
    as well as the data, and compressed pdf data (outside of Android). This
    change should enable support for PDF tab context.
    
    Bug: 441142997
    Change-Id: I2b060f6c85fd0f6b07ed3d7d481b509602268fee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6892437
    Reviewed-by: Nihar Majmudar <niharm@google.com>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Cr-Commit-Position: refs/heads/main@{#1508580}

---
+++ components/lens/lens_payload_construction.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_LENS_LENS_PAYLOAD_CONSTRUCTION_H_
+#define COMPONENTS_LENS_LENS_PAYLOAD_CONSTRUCTION_H_
+
+#include <string>
+
+#include "base/containers/span.h"
+#include "components/lens/lens_overlay_mime_type.h"
+#include "third_party/lens_server_proto/lens_overlay_service_deps.pb.h"
+
+namespace lens {
+
+lens::ContentData::ContentType MimeTypeToContentType(
+    lens::MimeType content_type);
+
+// Compresses the given bytes using Zstd and store them into `dst_bytes`.
+// Returns true if the compression is successful.
+bool ZstdCompressBytes(base::span<const uint8_t> src_bytes,
+                       std::string* dst_bytes);
+
+}  // namespace lens
+
+#endif  // COMPONENTS_LENS_LENS_PAYLOAD_CONSTRUCTION_H_
