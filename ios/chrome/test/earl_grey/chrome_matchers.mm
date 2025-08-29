09ff1d539950a929f5a993405cdff938d2ed0dd0
Gauthier Ambard <gambard@chromium.org>
2025-08-29 02:16:18 -0700

    [iOS] Fix iOS 26 tests for buttons
    The button background color has changed, matcher should be updated.
    
    Change-Id: I7cefa84bfed92611859251a0eb3a319ee619d49e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6898674
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1508241}

--- ios/chrome/test/earl_grey/chrome_matchers.mm
+++ ios/chrome/test/earl_grey/chrome_matchers.mm
@@ -66,7 +66,7 @@ id<GREYMatcher> ButtonWithEqualWeightColor() {
 }
 
 id<GREYMatcher> ButtonWithBackgroundColor(NSString* colorName) {
-  return [ChromeMatchersAppInterface buttonWithBackgroundColor:colorName];
+  return [ChromeMatchersAppInterface buttonWithBackgroundColorNamed:colorName];
 }
 
 id<GREYMatcher> ContextMenuItemWithAccessibilityLabel(NSString* label) {
