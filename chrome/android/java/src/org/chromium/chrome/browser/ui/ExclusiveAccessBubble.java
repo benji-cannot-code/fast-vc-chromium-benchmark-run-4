e32f2b70668d65eec3f289512fac8cbbc523594f
Abdelrahman Eed <abdoeed@google.com>
2025-08-29 11:55:31 -0700

    Adjust exclusive access bubble text based on keyboard presence.
    This change introduces a check for whether a physical keyboard is
    connected when updating the exclusive access bubble on Android. The
    bubble text will now be generated differently depending on whether
    `DeviceInput.supportsAlphabeticKeyboard()` is true.
    
    Bug: b:424139102
    Change-Id: I429c72980c88af9da47f031933f9a5fd60b018c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6870149
    Reviewed-by: Muyao Xu <muyaoxu@google.com>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Abdelrahman Eed <abdoeed@google.com>
    Cr-Commit-Position: refs/heads/main@{#1508534}

--- chrome/android/java/src/org/chromium/chrome/browser/ui/ExclusiveAccessBubble.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ui/ExclusiveAccessBubble.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.ui;
 
 import android.view.Gravity;
 
+import org.chromium.ui.base.DeviceInput;
 import org.jni_zero.CalledByNative;
 
 import org.chromium.build.annotations.NullMarked;
@@ -62,5 +63,10 @@ public class ExclusiveAccessBubble {
     @CalledByNative
     public boolean isVisible() {
         return mNotificationToast != null;
+    }
+
+    @CalledByNative
+    public boolean isKeyboardConnected() {
+        return DeviceInput.supportsAlphabeticKeyboard();
     }
 }
