cc643a1caf48e3bc0a22779275c75caffc7ff2c0
Nicolas MacBeth <nicolasmacbeth@google.com>
2025-08-29 12:59:51 -0700

    [iOS][iPad] Support BWG multiwindow by dismissing BWG in other windows
    Dismisses BWG in all other browsers/windows than the current one before
    starting the experience. This effectively enables support for iPad by
    having only one instance alive at a time.
    
    Fixed: 419069147
    Change-Id: If4a5774c8893fe933c6454b9efbdb8b6512e56f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6897131
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1508565}

--- ios/chrome/browser/browser_view/ui_bundled/browser_coordinator.mm
+++ ios/chrome/browser/browser_view/ui_bundled/browser_coordinator.mm
@@ -3080,6 +3080,11 @@ enum class ToolbarKind {
 }
 
 - (void)dismissBWGFlowWithCompletion:(ProceduralBlock)completion {
+  if (!_BWGCoordinator) {
+    completion();
+    return;
+  }
+
   [_BWGCoordinator stopWithCompletion:completion];
   _BWGCoordinator = nil;
 }
