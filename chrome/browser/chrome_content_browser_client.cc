d13e8785bbd2de06c156e3d9577da2dc82a82ad2
Robert Lin <robertlin@chromium.org>
2025-08-29 08:22:38 -0700

    Enable NewTabPage preloading to use PrefetchPrerenderIntegration
    It is desired to allow NewTabPage preloading to use
    PrefetchPrerenderIntegration, and this CL adds the feature flag
    `features::kNewTabPageTriggerForPrefetch` to
    `ContentBrowserClient::UsePrefetchPrerenderIntegration` to achieve that.
    
    Bug: 440123102
    Change-Id: I80fafe8b51a7ef0a71c3ae97d8ed0d797dc96e0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6869311
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Reviewed-by: Ken Okada <kenoss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1508390}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -8940,5 +8940,6 @@ bool ChromeContentBrowserClient::ShouldEnableCanvasNoise(
 }
 
 bool ChromeContentBrowserClient::UsePrefetchPrerenderIntegration() {
-  return base::FeatureList::IsEnabled(features::kBookmarkTriggerForPrefetch);
+  return base::FeatureList::IsEnabled(features::kBookmarkTriggerForPrefetch) ||
+         base::FeatureList::IsEnabled(features::kNewTabPageTriggerForPrefetch);
 }
