addb786f4e57954d7acec256b766ef3ae6099ab9
Byungwoo Lee <blee@igalia.com>
2025-08-28 20:07:18 -0700

    Fix crash when kPhaseMayBegin mouse wheel event on slotted text
    Add missing null check in HitTestResult::GetScrollableArea() to avoid
    crash when kPhaseMayBegin mouse wheel event on slotted text.
    
    MouseWheelEventManager::FindTargetNode() returns parent node of the
    hit-test result inner node for the mouse wheel event on text node.
    
    In case that the text node is a slotted node, the method returns the
    slot element which doesn't have LayoutObject. This lead to crash in
    HitTestResult::GetScrollableArea() due to the missing null check.
    
    Bug: 40260134
    Change-Id: If4eca201e77842e048bde5b33224100cf51fd3e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6888432
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Byungwoo Lee <blee@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1508151}

--- third_party/blink/renderer/core/layout/hit_test_result.cc
+++ third_party/blink/renderer/core/layout/hit_test_result.cc
@@ -298,7 +298,7 @@ CompositorElementId HitTestResult::GetScrollableContainer() const {
 
 // static
 ScrollableArea* HitTestResult::GetScrollableArea(const Node* node) {
-  if (!node) {
+  if (!node || !node->GetLayoutObject()) {
     return nullptr;
   }
 
