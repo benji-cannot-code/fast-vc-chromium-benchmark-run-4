4b3dbac6a6dd67a4fe93a248e188d108a6643e82
Robbie Gibson <rkgibson@google.com>
2025-08-29 14:46:47 -0700

    [iOS] Make sure discover feed loads with correct background
    The root cause here was that the feed container can be added after other
    setup. Before, theme changes were applied when the full view loads and
    then when customization changes. This misses the feed container adding
    in after initial load.
    
    This CL fixes the issue by also styling the feed container when it is
    initialized.
    
    Fixed: 441529243
    Change-Id: I381fa473e8a3d9b4dc1600e82ec547aca1a21d97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6900918
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1508631}

--- ios/chrome/browser/ntp/ui_bundled/new_tab_page_view_controller.mm
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_view_controller.mm
@@ -505,6 +505,7 @@ CGFloat SpaceBetweenModules() {
           .translatesAutoresizingMaskIntoConstraints = NO;
       [_feedContainer addSubview:_feedVisualEffectBackgroundView];
       AddSameConstraints(_feedContainer, _feedVisualEffectBackgroundView);
+      [self applyBackgroundThemeToFeedContainer];
     } else {
       _feedContainer.backgroundColor = [UIColor colorNamed:kBackgroundColor];
     }
@@ -647,6 +648,7 @@ CGFloat SpaceBetweenModules() {
   if (_feedContainer) {
     [_feedContainer removeFromSuperview];
     _feedContainer = nil;
+    _feedVisualEffectBackgroundView = nil;
   }
 
   [self removeFromViewHierarchy:self.feedWrapperViewController];
@@ -1123,11 +1125,11 @@ CGFloat SpaceBetweenModules() {
 // Sets the background using the current color palette, or defaults if none is
 // set.
 - (void)applyBackgroundTheme {
+  [self applyBackgroundThemeToFeedContainer];
+
   BOOL hasImageBackground =
       [self.traitCollection boolForNewTabPageImageBackgroundTrait];
   if (hasImageBackground) {
-    _feedContainer.backgroundColor = UIColor.clearColor;
-    _feedVisualEffectBackgroundView.hidden = NO;
     _backgroundGradientView.hidden = YES;
     self.view.backgroundColor = [UIColor colorNamed:@"ntp_background_color"];
     return;
@@ -1135,7 +1137,6 @@ CGFloat SpaceBetweenModules() {
 
   _backgroundGradientView.hidden =
       self.traitCollection.userInterfaceStyle == UIUserInterfaceStyleLight;
-  _feedVisualEffectBackgroundView.hidden = YES;
 
   NewTabPageColorPalette* colorPalette =
       [self.traitCollection objectForNewTabPageTrait];
@@ -1144,14 +1145,32 @@ CGFloat SpaceBetweenModules() {
     self.view.backgroundColor = colorPalette.primaryColor;
     [_backgroundGradientView setStartColor:colorPalette.secondaryColor
                                   endColor:colorPalette.primaryColor];
-    _feedContainer.backgroundColor = colorPalette.secondaryCellColor;
   } else {
     self.view.backgroundColor = [UIColor colorNamed:@"ntp_background_color"];
     [_backgroundGradientView
         setStartColor:[UIColor colorNamed:kSecondaryBackgroundColor]
              endColor:[UIColor colorNamed:kPrimaryBackgroundColor]];
-    _feedContainer.backgroundColor = [UIColor colorNamed:kBackgroundColor];
   }
+}
+
+// Sets the feed container's background views correctly based on the current
+// theme settings.
+- (void)applyBackgroundThemeToFeedContainer {
+  BOOL hasImageBackground =
+      [self.traitCollection boolForNewTabPageImageBackgroundTrait];
+  if (hasImageBackground) {
+    _feedContainer.backgroundColor = UIColor.clearColor;
+    _feedVisualEffectBackgroundView.hidden = NO;
+    return;
+  }
+  _feedVisualEffectBackgroundView.hidden = YES;
+
+  NewTabPageColorPalette* colorPalette =
+      [self.traitCollection objectForNewTabPageTrait];
+
+  _feedContainer.backgroundColor = colorPalette
+                                       ? colorPalette.secondaryCellColor
+                                       : [UIColor colorNamed:kBackgroundColor];
 }
 
 - (void)setNTPShortcutsHandler:
