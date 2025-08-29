d0822a35a31ca39916120ee05cd43c8d9790d440
Faizur Rahman <faizur@chromium.org>
2025-08-29 11:44:08 -0700

    [Save to Drive] Instantiate the Drive uploader based on file size
    This is the continuation of the save to Drive implementation. This
    CL:
    - Uses primary account until the account chooser is ready. Added a TODO
    - Calls the content reader to get the size of the PDF
    - Creates Multipart or Resumable uploader based on the content size
    - Sends upload progress updates
    
    The uploader's implementations are just stubs for now. This will be
    implemented in the following CLs.
    
    BYPASS_LARGE_CHANGE_WARNING=A chunk of the code are stubs for the
    uploaders.
    
    Bug: 435142523
    Change-Id: I353a1a86ab7c61af481b5e70081d706a267f6d07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6801735
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Faizur Rahman <faizur@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1508519}

---
+++ chrome/browser/save_to_drive/multipart_drive_uploader.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/save_to_drive/multipart_drive_uploader.h"
+
+namespace save_to_drive {
+
+MultipartDriveUploader::MultipartDriveUploader(
+    std::string title,
+    AccountInfo account_info,
+    ProgressCallback progress_callback)
+    : DriveUploader(DriveUploaderType::kMultipart,
+                    std::move(title),
+                    std::move(account_info),
+                    std::move(progress_callback)) {}
+
+MultipartDriveUploader::~MultipartDriveUploader() = default;
+
+void MultipartDriveUploader::UploadFile() {
+  // TODO(crbug.com/424208776): Implement MultipartDriveUploader.
+}
+
+}  // namespace save_to_drive
