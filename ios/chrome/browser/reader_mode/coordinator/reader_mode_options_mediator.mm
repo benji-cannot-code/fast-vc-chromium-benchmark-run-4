fe5d2f5421969567127038e5c32900d9e70a51f7
Nohemi Fernandez <fernandex@google.com>
2025-08-29 01:56:53 -0700

    [iOS] Fix a race condition on dismissal of Reading Mode options view.
    The page action menu provides two mechanisms to dismiss Reader Mode
    options, which may result in the mediator attempting to disconnect
    twice. Add a condition to ensure that this does not cause a crash.
    
    Bug: 441281289
    Change-Id: Ia3d82cb464720442e08cb487ec60d7462ac4c18c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6891658
    Auto-Submit: Nohemi Fernandez <fernandex@google.com>
    Reviewed-by: Quentin Pubert <qpubert@google.com>
    Commit-Queue: Nohemi Fernandez <fernandex@google.com>
    Cr-Commit-Position: refs/heads/main@{#1508234}

--- ios/chrome/browser/reader_mode/coordinator/reader_mode_options_mediator.mm
+++ ios/chrome/browser/reader_mode/coordinator/reader_mode_options_mediator.mm
@@ -75,7 +75,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark - Public
 
 - (void)disconnect {
-  _distilledPagePrefs->RemoveObserver(_prefsObserverBridge.get());
+  if (_distilledPagePrefs) {
+    _distilledPagePrefs->RemoveObserver(_prefsObserverBridge.get());
+  }
   _prefsObserverBridge.reset();
   _distilledPagePrefs = nullptr;
   _webStateList = nullptr;
