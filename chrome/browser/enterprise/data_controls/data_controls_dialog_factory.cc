168dcc290d122b91fdc6e4cb7dbc93fabf8bce92
Xu Li <mxlg@google.com>
2025-08-29 09:49:13 -0700

    Move DataControlsDialogFactory from //component/ to //chrome/.
    The code under //component should be platform-agnostic, but the
    DataControlsDialogFactory depends on the content::Webcontents and can't
    be used for iOS.
    
    Bypass-Check-License: Moved existing file
    Change-Id: Iab0ba6b6e35ea2736899c2a54230c35b1ef68735
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6897528
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Xu Li <mxlg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1508442}

--- chrome/browser/enterprise/data_controls/data_controls_dialog_factory.cc
+++ chrome/browser/enterprise/data_controls/data_controls_dialog_factory.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/enterprise/data_controls/content/browser/data_controls_dialog_factory.h"
+#include "chrome/browser/enterprise/data_controls/data_controls_dialog_factory.h"
 
 #include <map>
 
--- chrome/browser/enterprise/data_controls/data_controls_dialog_factory.h
+++ chrome/browser/enterprise/data_controls/data_controls_dialog_factory.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef COMPONENTS_ENTERPRISE_DATA_CONTROLS_CONTENT_BROWSER_DATA_CONTROLS_DIALOG_FACTORY_H_
-#define COMPONENTS_ENTERPRISE_DATA_CONTROLS_CONTENT_BROWSER_DATA_CONTROLS_DIALOG_FACTORY_H_
+#ifndef CHROME_BROWSER_ENTERPRISE_DATA_CONTROLS_DATA_CONTROLS_DIALOG_FACTORY_H_
+#define CHROME_BROWSER_ENTERPRISE_DATA_CONTROLS_DATA_CONTROLS_DIALOG_FACTORY_H_
 
 #include "base/functional/callback.h"
 #include "components/enterprise/data_controls/core/browser/data_controls_dialog.h"
@@ -39,4 +39,4 @@ class DataControlsDialogFactory {
 
 }  // namespace data_controls
 
-#endif  // COMPONENTS_ENTERPRISE_DATA_CONTROLS_CONTENT_BROWSER_DATA_CONTROLS_DIALOG_FACTORY_H_
+#endif  // CHROME_BROWSER_ENTERPRISE_DATA_CONTROLS_DATA_CONTROLS_DIALOG_FACTORY_H_
