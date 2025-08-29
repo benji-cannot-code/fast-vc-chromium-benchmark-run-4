cd7f9436ff01caca01724c46c6d60d8fa310355e
Friedrich Horschig <friedrichh@chromium.org>
2025-08-29 03:57:17 -0700

    [HybridInAutofill] Add generator to support new suggestion generation
    CL crrev.com/c/6870589 introduced the dropdown item only in the legacy
    suggestion list construction. This CL ensure the passkey item is also
    part of the suggestion list when the AutofillNewSuggestionGeneration
    feature is active.
    
    The generator for password autofill items reuses the delegate. It's
    unclear whether fetching passkeys may even be an advantage over the
    legacy way that relies on a populated cache.
    
    Screenshot: http://screen/7RacahewbUDhBVc
    Feature: AutofillReintroduceHybridPasskeyDropdownItem
    Bug: 399124614
    Change-Id: If655bc372dd9f06c851c09c833b8b1d1747595c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6871098
    Commit-Queue: Friedrich Hauser <friedrichh@chromium.org>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Reviewed-by: Karol Sygiet <sygiet@google.com>
    Cr-Commit-Position: refs/heads/main@{#1508275}

--- components/autofill/core/browser/foundations/browser_autofill_manager.cc
+++ components/autofill/core/browser/foundations/browser_autofill_manager.cc
@@ -132,6 +132,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/suggestions/autofill_ai/autofill_ai_suggestion_generator.h"
 #include "components/autofill/core/browser/suggestions/compose_suggestion_generator.h"
 #include "components/autofill/core/browser/suggestions/one_time_passwords/otp_suggestion_generator.h"
+#include "components/autofill/core/browser/suggestions/passkeys/passkey_autofill_suggestion_generator.h"
 #include "components/autofill/core/browser/suggestions/payments/iban_suggestion_generator.h"
 #include "components/autofill/core/browser/suggestions/payments/merchant_promo_code_suggestion_generator.h"
 #include "components/autofill/core/browser/suggestions/payments/payments_suggestion_generator.h"
@@ -1274,6 +1275,12 @@ void BrowserAutofillManager::OnAskForValuesToFillImpl(
             delegate->GetIdentityCredentialSuggestionGenerator()) {
       suggestion_generators_.push_back(std::move(suggestion_generator));
     }
+  }
+  if (PasswordManagerDelegate* password_delegate =
+          client().GetPasswordManagerDelegate(field.global_id())) {
+    suggestion_generators_.push_back(
+        std::make_unique<PasskeyAutofillSuggestionGenerator>(
+            *password_delegate));
   }
 
   SuggestionsContext context = BuildSuggestionsContext(
--- components/autofill/core/browser/suggestions/suggestion_generator.h
+++ components/autofill/core/browser/suggestions/suggestion_generator.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/data_model/valuables/loyalty_card.h"
 #include "components/autofill/core/browser/filling/filling_product.h"
 #include "components/autofill/core/browser/foundations/autofill_client.h"
+#include "components/autofill/core/browser/suggestions/passkeys/hybrid_passkey_availability.h"
 #include "components/autofill/core/browser/suggestions/suggestion.h"
 #include "components/autofill/core/browser/webdata/autocomplete/autocomplete_entry.h"
 
@@ -59,7 +60,8 @@ class SuggestionGenerator {
                                       AutofillOfferData,
                                       AutocompleteEntry,
                                       LoyaltyCard,
-                                      IdentityCredential>;
+                                      IdentityCredential,
+                                      HybridPasskeyAvailability>;
 
   // Obtains data that will be used to generate suggestions on a given trigger
   // `field` that belongs to `form` by calling `GenerateSuggestions` later (See
