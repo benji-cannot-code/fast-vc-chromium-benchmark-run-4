ced83c3d01b53d505a1174e62e6290fcafe7e771
Fiaz Muhammad <mfiaz@google.com>
2025-08-29 15:17:40 -0700

    [Reader Mode] Make bottom sheet contents swappable
    - Since the reader mode bottom sheet doesn't block the usage of
    	the chrome UI, the user is able to trigger other bottom
    	sheets, which are blocked by the reader mode bottom sheet.
    - This reduces the priority of the reader mode bottom sheet and
    	allows its contents to be swappable.
    
    Bug: 441481778
    Change-Id: I0851a3e716af93a7ea8b54c305b9d1a68cc61d90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6901136
    Auto-Submit: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1508646}

--- chrome/android/junit/src/org/chromium/chrome/browser/dom_distiller/ReaderModeBottomSheetCoordinatorTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/dom_distiller/ReaderModeBottomSheetCoordinatorTest.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.dom_distiller;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.anyBoolean;
 import static org.mockito.Mockito.eq;
@@ -29,6 +31,8 @@ import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.UserActionTester;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.components.browser_ui.bottomsheet.BottomSheetContent;
+import org.chromium.components.browser_ui.bottomsheet.BottomSheetContent.ContentPriority;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
 import org.chromium.components.dom_distiller.core.DistilledPagePrefs;
 import org.chromium.components.dom_distiller.core.DomDistillerService;
@@ -103,5 +107,16 @@ public class ReaderModeBottomSheetCoordinatorTest {
         Assert.assertEquals(
                 0,
                 mUserActionTester.getActionCount("DomDistiller.Android.DistilledPagePrefsOpened"));
+    }
+
+    @Test
+    public void testBottomSheetContentsAreSwappable() {
+        mCoordinator.show(/* showFullSheet= */ false);
+
+        verify(mBottomSheetController).requestShowContent(any(), eq(true));
+        BottomSheetContent bottomSheetContent = mCoordinator.getBottomSheetContentForTesting();
+
+        assertEquals(ContentPriority.LOW, bottomSheetContent.getPriority());
+        assertTrue(bottomSheetContent.canSuppressInAnyState());
     }
 }
