456b5b726bae0c2c6193f92265fd7aa5771f15f0
Raj T <rajendrant@chromium.org>
2025-08-29 11:01:00 -0700

    Refactor: Use raw_ref for local_state in PredictionModelStore
    Bug: 440098411
    Change-Id: I61b92c76989e9ea3e1a8542538dbc6f522f8be61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6897874
    Commit-Queue: Raj T <rajendrant@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1508492}

--- components/optimization_guide/core/delivery/prediction_model_store.h
+++ components/optimization_guide/core/delivery/prediction_model_store.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_OPTIMIZATION_GUIDE_CORE_DELIVERY_PREDICTION_MODEL_STORE_H_
 
 #include "base/files/file_path.h"
-#include "base/memory/raw_ptr.h"
+#include "base/memory/raw_ref.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/sequence_checker.h"
@@ -35,7 +35,7 @@ class PredictionModelStore {
   using PredictionModelLoadedCallback =
       base::OnceCallback<void(std::unique_ptr<proto::PredictionModel>)>;
 
-  explicit PredictionModelStore(PrefService* local_state);
+  explicit PredictionModelStore(PrefService& local_state);
 
   // Initializes the model store with |base_store_dir|. Model store will be
   // usable only after it is initialized.
@@ -150,7 +150,7 @@ class PredictionModelStore {
   // The base dir where the prediction model dirs are saved.
   base::FilePath base_store_dir_ GUARDED_BY_CONTEXT(sequence_checker_);
 
-  const raw_ptr<PrefService> local_state_ GUARDED_BY_CONTEXT(sequence_checker_);
+  const raw_ref<PrefService> local_state_ GUARDED_BY_CONTEXT(sequence_checker_);
 
   SEQUENCE_CHECKER(sequence_checker_);
 
