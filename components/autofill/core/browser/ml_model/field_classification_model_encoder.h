690118c25b0a05e6be7793645d958da02e662bf7
Christoph Schwering <schwering@google.com>
2025-08-29 10:48:07 -0700

    [Autofill] Migrate local ML predictions to FormFieldData
    This CL replaces FormStructure and AutofillField in
    core/browser/ml_model with FormData and FormFieldData.
    
    This is a necessary step towards migrating asynchronous form parsing
    more functional: rather than mutating FormStructures, it should read
    FormDatas and return an assignment.
    
    Bug: 427787155, 40232021
    Change-Id: If3b161edda3d741c373eba9d61ced11f70ab3fcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6895487
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1508484}

--- components/autofill/core/browser/ml_model/field_classification_model_encoder.h
+++ components/autofill/core/browser/ml_model/field_classification_model_encoder.h
@@ -18,8 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 
-class AutofillField;
-class FormStructure;
+class FormFieldData;
+class FormData;
 
 // The Encoder performs vectorization for on-device field type prediction ML
 // model. It changes the string input for preprocessing by standardizing and
@@ -67,17 +67,17 @@ class FieldClassificationModelEncoder {
   // Encodes the `form` into the `ModelInput` representation understood by the
   // `FieldClassificationModelExecutor`. This is done by encoding the attributes
   // of the form's fields.
-  ModelInput EncodeForm(const FormStructure& form) const;
+  ModelInput EncodeForm(const FormData& form) const;
 
   // Constructs from `field` the input for Field Classification ML model using
   // field attributes. More specifically, handles the attributes encoding and
   // prepares the final input.
-  std::vector<TokenId> EncodeField(const AutofillField& field) const;
+  std::vector<TokenId> EncodeField(const FormFieldData& field) const;
 
   // Constructs the input for Field Classification ML model using
   // form level attributes.
   std::vector<FieldClassificationModelEncoder::TokenId> EncodeFormFeatures(
-      const FormStructure& form) const;
+      const FormData& form) const;
 
   // Tokenizes the specific `input` to a vector of size
   // `max_tokens_per_feature`. The token IDs are looked up in token_to_id_
