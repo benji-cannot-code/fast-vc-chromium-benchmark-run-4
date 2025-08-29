314047c374c9626a19c07838c59a25ef8a7bfff8
Philip Rogers <pdr@chromium.org>
2025-08-29 13:06:47 -0700

    [html-in-canvas] Don't ignore a11y canvas fallback shadow elements
    Prior to this change, the checks for whether to ignore ax nodes in
    canvas fallback content used `parentElement()`, which returns false for
    elements in shadow DOM (e.g., descendants of <input>). The fix for this
    is to use `ParentOrShadowHostElement()` instead, which fixes
    `canvas-fallback-content-2.html`. A unit test has also been added.
    
    This change revealed that we were already excluding `display: contents`
    content under canvas due to the `!GetLayoutObject()` check in
    `AXNodeObject::ComputeIsIgnored`. To avoid a difference to this
    behavior, a temporary TODO has been added for `display: contents` under
    canvas.
    
    AX-Relnotes: Minor change to ignore fewer elements in canvas fallback
    content. The effect should be small because the majority of canvas
    fallback content is just text, rather than more complex elements:
      CanvasFallbackContent: ~0.5%, https://chromestatus.com/metrics/feature/timeline/popularity/5608
      CanvasFallbackElementContent: ~0.06%, https://chromestatus.com/metrics/feature/timeline/popularity/5609
    
    Fixed: 441128439
    Change-Id: I0ff355c71dc1b3e72f0419799de2685bfdf66e52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6887226
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1508576}

--- chrome/browser/accessibility/media_app/ax_media_app_untrusted_service_browsertest.cc
+++ chrome/browser/accessibility/media_app/ax_media_app_untrusted_service_browsertest.cc
@@ -1205,7 +1205,8 @@ IN_PROC_BROWSER_TEST_F(AXMediaAppUntrustedServiceTest, StitchDocumentTree) {
       "++++genericContainer\n"
       "++++++canvas htmlTag='canvas'\n"
       "++++++++staticText name='<newline>          '\n"
-      "++++++++staticText name='Text that is not replaced by child tree.'\n"
+      "++++++++paragraph htmlTag='p'\n"
+      "++++++++++staticText name='Text that is not replaced by child tree.'\n"
       "++++++++staticText name='<newline>        '\n"
       "++++++graphicsDocument htmlTag='div' name='graphics-document'\n"
       "++++++++paragraph htmlTag='p'\n"
@@ -1235,7 +1236,8 @@ IN_PROC_BROWSER_TEST_F(AXMediaAppUntrustedServiceTest, StitchDocumentTree) {
       "++++genericContainer\n"
       "++++++canvas htmlTag='canvas'\n"
       "++++++++staticText name='<newline>          '\n"
-      "++++++++staticText name='Text that is not replaced by child tree.'\n"
+      "++++++++paragraph htmlTag='p'\n"
+      "++++++++++staticText name='Text that is not replaced by child tree.'\n"
       "++++++++staticText name='<newline>        '\n"
       "++++++graphicsDocument htmlTag='div' name='graphics-document'\n",
       browser()
--- third_party/blink/renderer/modules/accessibility/ax_node_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_node_object.cc
@@ -1259,8 +1259,8 @@ bool AXNodeObject::ComputeIsIgnored(IgnoredReasons* ignored_reasons) const {
     }
 
     // Fallback elements inside of a <canvas> are invisible, but are not ignored
-    if (IsHiddenViaStyle() || !node || !node->parentElement() ||
-        !node->parentElement()->IsCanvasOrInCanvasSubtree()) {
+    if (IsHiddenViaStyle() || !node || !node->ParentOrShadowHostElement() ||
+        !node->ParentOrShadowHostElement()->IsCanvasOrInCanvasSubtree()) {
       return true;
     }
   }
@@ -1279,6 +1279,15 @@ bool AXNodeObject::ComputeIsIgnored(IgnoredReasons* ignored_reasons) const {
     // explicitly hidden, e.g. is in a <canvas> fallback or is display locked.
     if (IsA<Text>(node)) {
       return false;
+    }
+    // Similarly, elements in <canvas> fallback should not be ignored.
+    if (node->ParentOrShadowHostElement() &&
+        node->ParentOrShadowHostElement()->IsCanvasOrInCanvasSubtree()) {
+      // TODO: We should not exclude display: contents (crbug.com/41384724) in
+      // canvas.
+      if (!GetElement() || !GetElement()->HasDisplayContentsStyle()) {
+        return false;
+      }
     }
     if (ignored_reasons) {
       ignored_reasons->push_back(IgnoredReason(kAXUninteresting));
@@ -5538,8 +5547,8 @@ void AXNodeObject::GetRelativeBounds(AXObject** out_container,
   Element* element = GetElement();
   // If it's in a canvas but doesn't have an explicit rect, or has display:
   // contents set, get the bounding rect of its children.
-  if ((GetNode()->parentElement() &&
-       GetNode()->parentElement()->IsCanvasOrInCanvasSubtree()) ||
+  if ((GetNode()->ParentOrShadowHostElement() &&
+       GetNode()->ParentOrShadowHostElement()->IsCanvasOrInCanvasSubtree()) ||
       (element && element->HasDisplayContentsStyle())) {
     Vector<gfx::RectF> rects;
     for (Node& child : NodeTraversal::ChildrenOf(*GetNode())) {
