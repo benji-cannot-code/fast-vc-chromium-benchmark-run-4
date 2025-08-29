c558328846cfe3236c4941f9f0c76e0055f6aefd
Alex Gough <ajgo@chromium.org>
2025-08-29 16:31:46 -0700

    use result<> for ParseXmlForPrinterCapabilities
    No functional changes.
    
    Bug: 40841428
    Change-Id: Iebad9b41ef670ae336efdc983152156adc08dcd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6900664
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1508674}

--- chrome/browser/printing/printer_xml_parser_impl.cc
+++ chrome/browser/printing/printer_xml_parser_impl.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/functional/callback.h"
+#include "base/types/expected.h"
 #include "base/values.h"
 #include "chrome/services/printing/public/mojom/printer_xml_parser.mojom.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
@@ -21,18 +22,17 @@ namespace printing {
 
 namespace {
 
+using ParseXmlCallback =
+    mojom::PrinterXmlParser::ParseXmlForPrinterCapabilitiesCallback;
+
 void XmlPrinterCapabilitiesParsed(
-    PrinterXmlParserImpl::ParseXmlCallback callback,
+    ParseXmlCallback callback,
     data_decoder::DataDecoder::ValueOrError value_or_error) {
   if (!value_or_error.has_value()) {
-    std::move(callback).Run(
-        mojom::PrinterCapabilitiesValueResult::NewResultCode(
-            mojom::ResultCode::kFailed));
+    std::move(callback).Run(base::unexpected(mojom::ResultCode::kFailed));
     return;
   }
-  std::move(callback).Run(
-      mojom::PrinterCapabilitiesValueResult::NewCapabilities(
-          std::move(value_or_error.value())));
+  std::move(callback).Run(base::ok(std::move(value_or_error.value())));
 }
 
 }  // namespace
