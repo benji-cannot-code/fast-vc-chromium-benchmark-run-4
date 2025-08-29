52c1ac24efe5e41cbbfd899a7573c98b738cbf9e
Duncan Mercer <mercerd@google.com>
2025-08-29 16:25:13 -0700

    Add a fieldtrial testing entry for ChromnientAimM3
    Change-Id: I3498d926084819087bbfe4d2311e89752fbf9718
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6898136
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Auto-Submit: Duncan Mercer <mercerd@google.com>
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1508672}

--- chrome/test/data/webui/lens/side_panel/searchbox_back_button_test.ts
+++ chrome/test/data/webui/lens/side_panel/searchbox_back_button_test.ts
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome-untrusted://lens/side_panel/side_panel_app.js';
 
+import {loadTimeData} from '//resources/js/load_time_data.js';
 import type {LensSidePanelAppElement} from 'chrome-untrusted://lens/side_panel/side_panel_app.js';
 import {SidePanelBrowserProxyImpl} from 'chrome-untrusted://lens/side_panel/side_panel_browser_proxy.js';
 import {assertFalse, assertTrue} from 'chrome-untrusted://webui-test/chai_assert.js';
@@ -20,6 +21,10 @@ suite('SearchboxBackButton', () => {
   setup(() => {
     testBrowserProxy = new TestLensSidePanelBrowserProxy();
     SidePanelBrowserProxyImpl.setInstance(testBrowserProxy);
+
+    // This test can only be run with the AIM searchbox disabled, since the
+    // back button is removed when the AIM searchbox is enabled.
+    loadTimeData.overrideValues({enableAimSearchbox: false});
 
     document.body.innerHTML = window.trustedTypes!.emptyHTML;
     lensSidePanelElement = document.createElement('lens-side-panel-app');
