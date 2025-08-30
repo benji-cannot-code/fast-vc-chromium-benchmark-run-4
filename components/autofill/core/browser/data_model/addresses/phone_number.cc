9ca70026183b29d266572dd54c64018c5b0a4e31
Jan Keitel <jkeitel@google.com>
2025-08-30 16:47:45 -0700

    Use std::string_view in phone number data model
    No functionality changes.
    
    Bug: 40100455
    Change-Id: I7acd96251c09c2fa29d93c992e3b4ca69c3b7292
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6903218
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1508822}

--- components/autofill/core/browser/data_model/addresses/phone_number.cc
+++ components/autofill/core/browser/data_model/addresses/phone_number.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <limits.h>
 
 #include <algorithm>
+#include <string_view>
 
 #include "base/check_op.h"
 #include "base/feature_list.h"
@@ -29,11 +30,11 @@ namespace autofill {
 namespace {
 
 // Returns the region code for this phone number, which is an ISO 3166 2-letter
-// country code.  The returned value is based on the |profile|; if the |profile|
+// country code.  The returned value is based on the `profile`; if the `profile`
 // does not have a country code associated with it, falls back to the country
-// code corresponding to the |app_locale|.
+// code corresponding to the `app_locale`.
 std::string GetRegion(const AutofillProfile& profile,
-                      const std::string& app_locale) {
+                      std::string_view app_locale) {
   std::u16string country_code = profile.GetRawInfo(ADDRESS_HOME_COUNTRY);
   if (!country_code.empty())
     return base::UTF16ToASCII(country_code);
@@ -114,7 +115,7 @@ void PhoneNumber::GetMatchingTypes(const std::u16string& text,
                                    const std::string& app_locale,
                                    FieldTypeSet* matching_types) const {
   // Strip the common phone number non numerical characters before calling the
-  // base matching type function. For example, the |text| "(514) 121-1523"
+  // base matching type function. For example, the `text` "(514) 121-1523"
   // would become the stripped text "5141211523". Since the base matching
   // function only does simple canonicalization to match against the stored
   // data, some domain specific cases will be covered below.
@@ -125,7 +126,7 @@ void PhoneNumber::GetMatchingTypes(const std::u16string& text,
   // TODO(crbug.com/41236729): Investigate the use of PhoneNumberUtil when
   // matching phone numbers for upload.
   // If there is not already a match for PHONE_HOME_WHOLE_NUMBER, normalize the
-  // |text| based on the app_locale before comparing it to the whole number. For
+  // `text` based on the app_locale before comparing it to the whole number. For
   // example, the France number "33 2 49 19 70 70" would be normalized to
   // "+33249197070" whereas the US number "+1 (234) 567-8901" would be
   // normalized to "12345678901".
@@ -169,7 +170,7 @@ void PhoneNumber::GetMatchingTypes(const std::u16string& text,
   }
 }
 
-// Normalize phones if |type| is a whole number:
+// Normalize phones if `type` is a whole number:
 //   (650)2345678 -> 6502345678
 //   1-800-FLOWERS -> 18003569377
 // If the phone cannot be normalized, returns the stored value verbatim.
@@ -315,7 +316,7 @@ VerificationStatus PhoneNumber::GetVerificationStatus(FieldType type) const {
   return VerificationStatus::kNoStatus;
 }
 
-void PhoneNumber::UpdateCacheIfNeeded(const std::string& app_locale) const {
+void PhoneNumber::UpdateCacheIfNeeded(std::string_view app_locale) const {
   std::string region = GetRegion(*profile_, app_locale);
   if (!number_.empty() && cached_parsed_phone_.region() != region) {
     // To enable filling of country calling codes for nationally formatted
