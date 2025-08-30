874969c4ccb5e1812234698f78a7a8ad05709bcd
Sirisha Kavuluru <skavuluru@google.com>
2025-08-29 17:11:39 -0700

    Request desktop site on external display for browser and renderer initiated navigations
    Changes behind FF.
    
    Bug: 423390518
    Change-Id: I51da46b46f32f788a4e13582133caaf75b4082dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6896269
    Reviewed-by: Shu Yang <shuyng@google.com>
    Commit-Queue: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1508688}

--- chrome/browser/content_settings/request_desktop_site_web_contents_observer_android.cc
+++ chrome/browser/content_settings/request_desktop_site_web_contents_observer_android.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/device_info.h"
 #include "base/command_line.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
+#include "chrome/browser/flags/android/chrome_feature_list.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/chrome_switches.h"
 #include "components/content_settings/core/common/content_settings_utils.h"
@@ -16,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/common/content_features.h"
 #include "services/metrics/public/cpp/ukm_builders.h"
+#include "ui/display/screen.h"
 
 namespace rds_web_contents_observer {
 // Keep in sync with UserAgentRequestType in tools/metrics/histograms/enums.xml.
@@ -76,6 +78,16 @@ void RequestDesktopSiteWebContentsObserverAndroid::DidStartNavigation(
       desktop_mode = false;
     }
   }
+
+  // Always request desktop site on external displays irrespective of the
+  // content setting.
+  bool is_on_external_display =
+      base::FeatureList::IsEnabled(
+          chrome::android::kDesktopUAOnConnectedDisplay) &&
+      (display::Screen::GetScreen()
+           ->GetDisplayNearestView(web_contents()->GetNativeView())
+           .id() != display::kDefaultDisplayId);
+  desktop_mode |= is_on_external_display;
 
   // Override UA for renderer initiated navigation only. UA override for browser
   // initiated navigation is handled on Java side. This is to workaround known
