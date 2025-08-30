9fcdb60319618a4aaed6186ab8b37d408e0df6ae
David Sanders <dsanders11@ucsbalum.com>
2025-08-29 17:33:03 -0700

    Remove unused includes in //media/base/media_resource.h
    Bug: 40318405
    Change-Id: Id3989744e415e5063eeb9c8c10513d5c0db8d41a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6899123
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#1508698}

--- media/base/media_resource.h
+++ media/base/media_resource.h
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
-#include "base/memory/raw_ptr.h"
-#include "base/time/time.h"
 #include "media/base/demuxer_stream.h"
 #include "media/base/media_export.h"
 
