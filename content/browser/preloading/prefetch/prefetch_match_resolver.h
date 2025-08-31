bcbdfe05e82134ef2fb41fb7b2410079dd40a90d
kenoss <kenoss@chromium.org>
2025-08-31 07:20:35 -0700

    PreloadServingMetrics: Store WeakPtr<NavigationRequest> in PrefetchMatchResolver
    This CL is a preparation of `PreloadServingMetrics`, especially for
    https://crrev.com/c/6884265 .
    
    This CL makes `NavigationRequest` be stored in `PrefetchMatchResolver`,
    which is going to be used to store logs.
    
    Bug: 360094997
    Change-Id: Ieab3570035e499dba8d6377a1b7717cde581ea03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6884092
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Taiyo Mizuhashi <taiyo@chromium.org>
    Commit-Queue: Ken Okada <kenoss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1508889}

--- content/browser/preloading/prefetch/prefetch_match_resolver.h
+++ content/browser/preloading/prefetch/prefetch_match_resolver.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/preloading/prefetch/prefetch_params.h"
 #include "content/browser/preloading/prefetch/prefetch_servable_state.h"
 #include "content/browser/preloading/prefetch/prefetch_serving_handle.h"
+#include "content/browser/renderer_host/frame_tree_node.h"
 #include "content/common/content_export.h"
 #include "content/public/browser/global_routing_id.h"
 
@@ -124,13 +125,21 @@ class CONTENT_EXPORT PrefetchMatchResolver final
   // Matches prefetches only if its final PrefetchServiceWorkerState is
   // `expected_service_worker_state` (either `kControlled` or `kDisallowed`).
   static void FindPrefetch(
+      FrameTreeNodeId frame_tree_node_id,
+      PrefetchService& prefetch_service,
       PrefetchKey navigated_key,
       PrefetchServiceWorkerState expected_service_worker_state,
-      bool is_nav_prerender,
-      PrefetchService& prefetch_service,
       base::WeakPtr<PrefetchServingPageMetricsContainer>
           serving_page_metrics_container,
       Callback callback);
+  static void FindPrefetchForTesting(
+      PrefetchService& prefetch_service,
+      PrefetchKey navigated_key,
+      PrefetchServiceWorkerState expected_service_worker_state,
+      base::WeakPtr<PrefetchServingPageMetricsContainer>
+          serving_page_metrics_container,
+      Callback callback,
+      bool is_nav_prerender);
 
  private:
   struct CandidateData final {
@@ -142,10 +151,11 @@ class CONTENT_EXPORT PrefetchMatchResolver final
   };
 
   explicit PrefetchMatchResolver(
+      base::WeakPtr<NavigationRequest> navigation_request,
+      base::WeakPtr<PrefetchService> prefetch_service,
       PrefetchKey navigated_key,
       PrefetchServiceWorkerState expected_service_worker_state,
       bool is_nav_prerender,
-      base::WeakPtr<PrefetchService> prefetch_service,
       Callback callback);
 
   // Returns blocked duration. Returns null iff it's not blocked yet.
@@ -213,9 +223,14 @@ class CONTENT_EXPORT PrefetchMatchResolver final
   // A would be enough.
   std::unique_ptr<PrefetchMatchResolver> self_;
 
+  // `NavigationRequest` associated to `this`.
+  //
+  // It is used only for metrics purpose.
+  base::WeakPtr<NavigationRequest> navigation_request_for_metrics_;
+  base::WeakPtr<PrefetchService> prefetch_service_;
+
   const PrefetchKey navigated_key_;
   const PrefetchServiceWorkerState expected_service_worker_state_;
-  base::WeakPtr<PrefetchService> prefetch_service_;
   Callback callback_;
   const bool is_nav_prerender_;
   std::map<PrefetchKey, std::unique_ptr<CandidateData>> candidates_;
