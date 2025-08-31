d6adbb160f59cea00fed2df06f831c33de156e70
Moe Ahmadi <mahmadi@chromium.org>
2025-08-31 11:10:06 -0700

    [omnibox] Adds fieldtrial_testing_config for AIMEligibilityService
    Also changes the Observer pattern to a CallbackList for simplicity and
    gates eligibility change notifications behind a default-disabled feature.
    
    Bug: 436225340
    Change-Id: I3c8ea37e40dd45c6c2eb3cdf7c56f5f830bed764
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6896911
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1508911}

--- chrome/test/data/webui/new_tab_page/new_tab_page_browsertest.cc
+++ chrome/test/data/webui/new_tab_page/new_tab_page_browsertest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/test/base/web_ui_mocha_browser_test.h"
 #include "components/history_clusters/core/features.h"
+#include "components/omnibox/browser/aim_eligibility_service.h"
 #include "components/search/ntp_features.h"
 #include "content/public/test/browser_test.h"
 
@@ -15,7 +16,13 @@ class NewTabPageBrowserTest : public WebUIMochaBrowserTest {
  protected:
   NewTabPageBrowserTest() {
     set_test_loader_host(chrome::kChromeUINewTabPageHost);
+    scoped_feature_list_.InitWithFeatures(
+        /*enabled_features=*/{},
+        /*disabled_features=*/{kAimServerEligibilityEnabledEn});
   }
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 using NewTabPageTest = NewTabPageBrowserTest;
