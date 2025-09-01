78f2f3a6a968fc195063e9c557a7e6e2f0b02512
kenoss <kenoss@chromium.org>
2025-09-01 03:56:44 -0700

    PreloadServingMetrics: Introduce PrefetchMatchMetrics
    This CL introduces `PrefetchMatchMetrics`, which holds serving metrics
    of prefetch matching.
    
    No behavioral change if the feature is disabled.
    
    This CL is just a plumbing. Further extensions, metrics, and tests will
    follow.
    
    Bug: 360094997
    Change-Id: If1e67b70cbcb9a070a15f798a0228a4d3dc65995
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6884265
    Reviewed-by: Taiyo Mizuhashi <taiyo@chromium.org>
    Commit-Queue: Ken Okada <kenoss@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1509111}

--- content/browser/preloading/prefetch/prefetch_match_resolver.cc
+++ content/browser/preloading/prefetch/prefetch_match_resolver.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/preloading/prefetch/prefetch_request.h"
 #include "content/browser/preloading/prefetch/prefetch_service.h"
 #include "content/browser/preloading/prefetch/prefetch_serving_handle.h"
+#include "content/browser/preloading/preload_serving_metrics_holder.h"
 #include "content/browser/preloading/prerender/prerender_features.h"
 #include "content/browser/renderer_host/frame_tree.h"
 
@@ -54,7 +55,8 @@ PrefetchMatchResolver::PrefetchMatchResolver(
       navigated_key_(std::move(navigated_key)),
       expected_service_worker_state_(expected_service_worker_state),
       callback_(std::move(callback)),
-      is_nav_prerender_(is_nav_prerender) {
+      is_nav_prerender_(is_nav_prerender),
+      prefetch_match_metrics_(std::make_unique<PrefetchMatchMetrics>()) {
   switch (expected_service_worker_state_) {
     case PrefetchServiceWorkerState::kAllowed:
       NOTREACHED();
@@ -459,12 +461,14 @@ void PrefetchMatchResolver::UnblockForMatch(const PrefetchKey& prefetch_key) {
 
 void PrefetchMatchResolver::UnblockForNoCandidates() {
   TRACE_EVENT0("loading", "PrefetchMatchResolver::UnblockForNoCandidates");
-  UnblockInternal({});
+
   if (prefetch_service_ && expected_service_worker_state_ ==
                                PrefetchServiceWorkerState::kDisallowed) {
     prefetch_service_->AddRecentUnmatchedNavigatedKeysForMetrics(
         navigated_key_);
   }
+
+  UnblockInternal({});
 }
 
 void PrefetchMatchResolver::MaybeUnblockForUnmatch(
@@ -507,6 +511,23 @@ void PrefetchMatchResolver::UnblockInternal(
   // Postcondition: This resolver waits for no `PrefetchContainer`s when it has
   // been unblocking.
   CHECK_EQ(candidates_.size(), 0u);
+
+  if (PreloadServingMetrics::IsEnabled()) {
+    PrefetchContainer* prefetch_container =
+        serving_handle.GetPrefetchContainer();
+    prefetch_match_metrics_->prefetch_container_metrics =
+        prefetch_container
+            ? std::make_unique<PrefetchContainerMetrics>(
+                  prefetch_container->GetPrefetchContainerMetrics())
+            : std::unique_ptr<PrefetchContainerMetrics>(nullptr);
+    if (navigation_request_for_metrics_) {
+      auto& preload_serving_metrics_holder =
+          *PreloadServingMetricsHolder::GetOrCreateForNavigationHandle(
+              *navigation_request_for_metrics_.get());
+      preload_serving_metrics_holder.AddPrefetchMatchMetrics(
+          std::move(prefetch_match_metrics_));
+    }
+  }
 
   auto callback = std::move(callback_);
 
