a215242aba46a1682cf67fc340a5c89b8ab57533
Hidehiko Abe <hidehiko@chromium.org>
2025-09-01 00:48:19 -0700

    Rename publisher_host.{h,cc} to publisher_host_impl.{h,cc}
    This is preparation to extract an interface of publisher host.
    The name of the class is kept PublisherHost in this CL,
    and will be renamed to PublisherHostImpl in a following CL
    with introducing new PublisherHost interface.
    
    BUG=441649482
    TEST=Tryjob
    
    Bypass-Check-License: Rename files.
    Change-Id: Iec765a5ada23cea536af62dabcb6b69e9c916884
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6903325
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Jun Ishiguro <junis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1509024}

--- chrome/browser/apps/app_service/publisher_host_impl.cc
+++ chrome/browser/apps/app_service/publisher_host_impl.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/apps/app_service/publisher_host.h"
+#include "chrome/browser/apps/app_service/publisher_host_impl.h"
 
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/publishers/extension_apps.h"
--- chrome/browser/apps/app_service/publisher_host_impl.h
+++ chrome/browser/apps/app_service/publisher_host_impl.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_APPS_APP_SERVICE_PUBLISHER_HOST_H_
-#define CHROME_BROWSER_APPS_APP_SERVICE_PUBLISHER_HOST_H_
+#ifndef CHROME_BROWSER_APPS_APP_SERVICE_PUBLISHER_HOST_IMPL_H_
+#define CHROME_BROWSER_APPS_APP_SERVICE_PUBLISHER_HOST_IMPL_H_
 
 #include <memory>
 
@@ -96,4 +96,4 @@ class ScopedOmitPluginVmAppsForTesting {
 
 }  // namespace apps
 
-#endif  // CHROME_BROWSER_APPS_APP_SERVICE_PUBLISHER_HOST_H_
+#endif  // CHROME_BROWSER_APPS_APP_SERVICE_PUBLISHER_HOST_IMPL_H_
