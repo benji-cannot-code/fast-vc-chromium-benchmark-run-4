39b66156f3a4bcc1f95b8012b97d8508f6d1a154
Christoph Schwering <schwering@google.com>
2025-09-01 02:40:46 -0700

    [Autofill] Call LogDetermineHeuristicTypesMetrics synchronously
    This CL moves LogDetermineHeuristicTypesMetrics() out of
    DetermineHeuristic(), which will be migrated from FormStructure
    to FormData.
    
    Bug: 427787155, 40232021
    Change-Id: Iaded2112d03713beb33ccc9561d9857be7bf279b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6905442
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1509068}

--- components/autofill/core/browser/form_structure.cc
+++ components/autofill/core/browser/form_structure.cc
@@ -241,7 +241,6 @@ void FormStructure::DetermineHeuristicTypes(
   AssignBestFieldTypes(regex_predictions, HeuristicSource::kRegexes);
   RationalizeAndAssignSections(client_country, current_page_language,
                                log_manager);
-  LogDetermineHeuristicTypesMetrics();
 }
 
 void FormStructure::RationalizeAndAssignSections(
@@ -692,7 +691,7 @@ void FormStructure::RetrieveFromCache(const FormStructure& cached_form,
   may_run_autofill_ai_model_ = cached_form.may_run_autofill_ai_model_;
 }
 
-void FormStructure::LogDetermineHeuristicTypesMetrics() {
+void FormStructure::LogDeveloperEngagementMetric() {
   developer_engagement_metrics_ = 0;
   if (IsAutofillable()) {
     AutofillMetrics::DeveloperEngagementMetric metric =
