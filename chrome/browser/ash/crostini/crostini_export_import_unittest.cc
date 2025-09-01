11e8520754f37180bd32ad84a0f013aef2b1dd14
Dominic Battré <battre@chromium.org>
2025-09-01 06:47:15 -0700

    Disable flaky Crostini export/import unit tests.
    The tests `TestExportFail` and `TestExportDoneBeforeCancelled`
    in `crostini_export_import_unittest.cc` are disabled due to
    flakiness.
    
    Bug: 440792198, 441657411
    Change-Id: I531c034515ea5701f906a88324d76b568a97b5df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6904241
    Auto-Submit: Dominic Battré <battre@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Owners-Override: Dominic Battré <battre@chromium.org>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1509175}

--- chrome/browser/ash/crostini/crostini_export_import_unittest.cc
+++ chrome/browser/ash/crostini/crostini_export_import_unittest.cc
@@ -884,7 +884,8 @@ TEST_F(CrostiniExportImportTest, TestExportCustomVmContainerSuccess) {
       [&]() -> bool { return base::PathExists(tarball_); }));
 }
 
-TEST_F(CrostiniExportImportTest, TestExportFail) {
+// TODO(crbug.com/440792198): Disabled for flakiness.
+TEST_F(CrostiniExportImportTest, DISABLED_TestExportFail) {
   crostini_export_import_->FillOperationData(ExportImportType::EXPORT);
   crostini_export_import_->FileSelected(ui::SelectedFileInfo(tarball_), 0);
 
@@ -1001,7 +1002,8 @@ TEST_F(CrostiniExportImportTest, DISABLED_TestExportCancelled) {
       [&]() -> bool { return !base::PathExists(tarball_); }));
 }
 
-TEST_F(CrostiniExportImportTest, TestExportDoneBeforeCancelled) {
+// TODO(crbug.com/440792198): Disabled for flakiness.
+TEST_F(CrostiniExportImportTest, DISABLED_TestExportDoneBeforeCancelled) {
   crostini_export_import_->FillOperationData(ExportImportType::EXPORT);
   crostini_export_import_->FileSelected(ui::SelectedFileInfo(tarball_), 0);
 
