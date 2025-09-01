40b4cf86432764b6ae1d9bf0a0eaae919d3f7581
Colin Blundell <blundell@chromium.org>
2025-09-01 04:20:39 -0700

    [//gpu] Remove GpuMemoryBufferImplSharedMemory::CreateForTesting()
    This method is used only in two places:
    
    * ClientSharedImage::CreateForTesting(), where it can be replaced by
      allocating a handle and then creating the GMB from the handle
    * GpuMemoryBufferImplCreateTest, which is not a meaningful test and can
      simply be removed as it is instantiated only for GMBImplSharedMemory
    
    Bug: 404905709
    Change-Id: Iab0ee4604195a2af7cc2e3721cefaa763544786a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6811894
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1509118}

--- gpu/ipc/common/gpu_memory_buffer_impl_shared_memory_unittest.cc
+++ gpu/ipc/common/gpu_memory_buffer_impl_shared_memory_unittest.cc
@@ -15,9 +15,5 @@ INSTANTIATE_TYPED_TEST_SUITE_P(GpuMemoryBufferImplSharedMemory,
                                GpuMemoryBufferImplTest,
                                GpuMemoryBufferImplSharedMemory);
 
-INSTANTIATE_TYPED_TEST_SUITE_P(GpuMemoryBufferImplSharedMemory,
-                               GpuMemoryBufferImplCreateTest,
-                               GpuMemoryBufferImplSharedMemory);
-
 }  // namespace
 }  // namespace gpu
