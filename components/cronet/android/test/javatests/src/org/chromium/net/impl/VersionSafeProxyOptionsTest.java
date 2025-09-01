6a91ede0f4e897ecbef25f24581c21198f922e6b
Stefano Duo <stefanoduo@google.com>
2025-09-01 07:51:35 -0700

    Throw if null is present, but is not the last element in a proxy list
    Bug: b:421175342
    Change-Id: I9846d2649ab46943ac9fdc9bec0b97c1c9f01861
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6898278
    Commit-Queue: Etienne Dechamps <edechamps@google.com>
    Auto-Submit: Stefano Duo <stefanoduo@google.com>
    Reviewed-by: Etienne Dechamps <edechamps@google.com>
    Cr-Commit-Position: refs/heads/main@{#1509197}

--- components/cronet/android/test/javatests/src/org/chromium/net/impl/VersionSafeProxyOptionsTest.java
+++ components/cronet/android/test/javatests/src/org/chromium/net/impl/VersionSafeProxyOptionsTest.java
@@ -181,12 +181,12 @@ public class VersionSafeProxyOptionsTest {
                                         "not-existing-hostname",
                                         8080,
                                         httpsProxyCallback),
-                                (Proxy) null,
                                 new Proxy(
                                         Proxy.HTTP,
                                         "not-existing-hostname",
                                         8080,
-                                        httpProxyCallback)));
+                                        httpProxyCallback),
+                                (Proxy) null));
         VersionSafeProxyOptions safeProxyOptions = new VersionSafeProxyOptions(proxyOptions);
         org.chromium.net.impl.proto.ProxyOptions proxyOptionsProto =
                 safeProxyOptions.createProxyOptionsProto();
@@ -200,8 +200,8 @@ public class VersionSafeProxyOptionsTest {
                                 .collect(Collectors.toList()))
                 .containsExactly(
                         org.chromium.net.impl.proto.ProxyScheme.HTTPS,
-                        org.chromium.net.impl.proto.ProxyScheme.DIRECT,
-                        org.chromium.net.impl.proto.ProxyScheme.HTTP)
+                        org.chromium.net.impl.proto.ProxyScheme.HTTP,
+                        org.chromium.net.impl.proto.ProxyScheme.DIRECT)
                 .inOrder();
         // Confirm that the original order within ProxyOptions#getProxyList is maintained for
         // Callback's proto.
@@ -213,8 +213,8 @@ public class VersionSafeProxyOptionsTest {
         safeProxyCallbacks.get(0).onBeforeTunnelRequest(any());
         Mockito.verify(httpsProxyCallback, times(1)).onBeforeTunnelRequest(any());
         Mockito.verify(httpProxyCallback, never()).onBeforeTunnelRequest(any());
-        assertThat(safeProxyCallbacks.get(1)).isNull();
-        safeProxyCallbacks.get(2).onBeforeTunnelRequest(any());
+        assertThat(safeProxyCallbacks.get(2)).isNull();
+        safeProxyCallbacks.get(1).onBeforeTunnelRequest(any());
         Mockito.verify(httpProxyCallback, times(1)).onBeforeTunnelRequest(any());
     }
 }
