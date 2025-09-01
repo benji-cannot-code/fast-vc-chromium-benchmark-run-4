1d7221259f87b211b0f206179fea0d0e90c3d69d
Keita Suzuki <suzukikeita@chromium.org>
2025-09-01 06:43:32 -0700

    Enable DSEPrewarm testing config for Android
    This CL enables the DSEPrewarm testing config for Android, since the
    feature is implemented for Android as well.
    
    Bug: 434845358
    Change-Id: I35282176a2efca757de25bfb9259a58e289a806c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6884557
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Jiacheng Guo <gjc@google.com>
    Commit-Queue: Keita Suzuki <suzukikeita@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1509170}

--- chrome/browser/preloading/prefetch/prefetch_browsertest.cc
+++ chrome/browser/preloading/prefetch/prefetch_browsertest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/timer/elapsed_timer.h"
 #include "chrome/browser/preloading/chrome_preloading.h"
 #include "chrome/browser/preloading/prefetch/chrome_prefetch_manager.h"
+#include "chrome/browser/preloading/scoped_prewarm_feature_list.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/test/base/chrome_test_utils.h"
 #include "chrome/test/base/platform_browser_test.h"
@@ -91,6 +92,11 @@ class PrefetchBrowserTest : public PlatformBrowserTest {
   base::ScopedMockElapsedTimersForTest test_timer_;
   net::test_server::EmbeddedTestServer ssl_server_{
       net::test_server::EmbeddedTestServer::TYPE_HTTPS};
+  // TODO(https://crbug.com/423465927): Explore a better approach to make the
+  // existing tests run with the prewarm feature enabled.
+  ::test::ScopedPrewarmFeatureList prewarm_feature_list_{
+      ::test::ScopedPrewarmFeatureList::PrewarmState::kDisabled};
+
   base::test::ScopedFeatureList feature_list_;
 
   std::unique_ptr<ukm::TestAutoSetUkmRecorder> test_ukm_recorder_;
@@ -149,6 +155,11 @@ class CCTPrerenderBrowserTestWithHoldback : public PrefetchBrowserTest {
   }
 
  private:
+  // TODO(https://crbug.com/423465927): Explore a better approach to make the
+  // existing tests run with the prewarm feature enabled.
+  ::test::ScopedPrewarmFeatureList prewarm_feature_list_{
+      ::test::ScopedPrewarmFeatureList::PrewarmState::kDisabled};
+
   base::test::ScopedFeatureList feature_list_;
 };
 
