79a3db24bac20ffe6562f4634a40feb1aabc707e
Christoph Schwering <schwering@google.com>
2025-09-01 01:26:31 -0700

    [Autofill] Clean up AutofillUseDeepEqualInsteadOfSameFieldAs
    Bug: 40183094
    Change-Id: I46e8c18a5404e16a84df887c4cb33a6620aec1a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6904543
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1509040}

--- components/autofill/core/common/autofill_features.h
+++ components/autofill/core/common/autofill_features.h
@@ -160,8 +160,6 @@ extern const base::FeatureParam<bool>
 COMPONENT_EXPORT(AUTOFILL)
 BASE_DECLARE_FEATURE(kAutofillNewSuggestionGeneration);
 COMPONENT_EXPORT(AUTOFILL)
-BASE_DECLARE_FEATURE(kAutofillUseDeepEqualInsteadOfSameFieldAs);
-COMPONENT_EXPORT(AUTOFILL)
 BASE_DECLARE_FEATURE(kAutofillSupportPhoneticNameForJP);
 COMPONENT_EXPORT(AUTOFILL)
 BASE_DECLARE_FEATURE(kAutofillSupportLastNamePrefix);
