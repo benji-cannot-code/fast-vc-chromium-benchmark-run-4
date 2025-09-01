3ce659867eebda279c6fca89f378777bd0e7aac8
kenoss <kenoss@chromium.org>
2025-09-01 03:56:07 -0700

    PreloadServingMetrics: Introduce PrefechContainerMetrics
    This CL introduces `PrefetchContainerMetrics`, which contains timing
    information that can be used for both `PrefetchContainer`-internal
    metrics and serving metrics.
    
    No behavioral change.
    
    Bug: 360094997
    Change-Id: I93a53057aeeb82b371ba80f7f70018d2f994235f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6884361
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Commit-Queue: Ken Okada <kenoss@chromium.org>
    Reviewed-by: Taiyo Mizuhashi <taiyo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1509110}

--- content/browser/preloading/preload_serving_metrics.cc
+++ content/browser/preloading/preload_serving_metrics.cc
@@ -13,6 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
+PrefetchContainerMetrics::PrefetchContainerMetrics() = default;
+
+PrefetchContainerMetrics::~PrefetchContainerMetrics() = default;
+
 // static
 bool PreloadServingMetrics::IsEnabled() {
   return features::kPrerender2FallbackUsePreloadServingMetrics.Get();
