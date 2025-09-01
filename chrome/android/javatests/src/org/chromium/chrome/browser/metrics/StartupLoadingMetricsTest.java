2644053c10ea58410939bf48755f3589ddb7adb5
Egor Pasko <pasko@chromium.org>
2025-09-01 10:25:39 -0700

    android: Disable flaky StartupLoadingMetricsTest#testWebApkStartRecorded
    Bug: 442398236
    Change-Id: Id019657ce56ae2feeec35c2ccd41ea8497a4464e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6905393
    Owners-Override: Egor Pasko <pasko@chromium.org>
    Auto-Submit: Egor Pasko <pasko@chromium.org>
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1509253}

--- chrome/android/javatests/src/org/chromium/chrome/browser/metrics/StartupLoadingMetricsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/metrics/StartupLoadingMetricsTest.java
@@ -353,6 +353,7 @@ public class StartupLoadingMetricsTest {
      */
     @Test
     @LargeTest
+    @DisabledTest(message = "crbug.com/442398236")
     public void testWebApkStartRecorded() throws Exception {
         HistogramWatcher ntpColdStartWatcher = createNtpColdStartHistogramWatcher(0);
         runAndWaitForPageLoadMetricsRecorded(
