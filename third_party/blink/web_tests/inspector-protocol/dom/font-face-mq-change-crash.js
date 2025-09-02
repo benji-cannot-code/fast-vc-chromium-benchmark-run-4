89c81c2266b4eb7228c6940c9966d1f27c58b47f
Steinar H. Gunderson <sesse@chromium.org>
2025-09-02 05:43:59 -0700

    Fix a DCHECK/wrong style when devtools is active.
    When a media query causes changes in @font-face, we fire off a probe
    to Devtools to inform about changed fonts. However, if Devtools has
    pending invalidations at this point, it will process them first of all,
    causing a callback into the same RuleSet code that we are already
    in the process of running. This code was not prepared for reentrancy
    and could then mess up old vs. new stylesheets, leaing to a DCHECK
    in debug mode and unstyled elements in release mode.
    
    Test with lots of help from futhark@ and pfaffe@.
    
    Fixed: 441271797
    Change-Id: I342daf37f216c0ce6c38b3897f198cc5e676f981
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6905953
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Philip Pfaffe <pfaffe@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1509503}

---
+++ third_party/blink/web_tests/inspector-protocol/dom/font-face-mq-change-crash.js
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(/** @type {import('test_runner').TestRunner} */ testRunner) {
+  const { page, session, dp } = await testRunner.startHTML(`
+<!DOCTYPE html>
+<style>body { color: red; }</style>
+<style>@font-face {}</style>
+<body>
+<script>
+  function crashme() {
+    document.styleSheets[0].disabled = true;
+    document.body.offsetTop;
+    document.styleSheets[1].media = "print";
+    document.body.offsetTop;
+  }
+</script>
+`, 'Verify that modifying active @font-face with pending style invalidations does not crash via Devtools');
+
+  await dp.DOM.enable();
+  await dp.CSS.enable();
+  await session.evaluate('crashme();');
+  testRunner.completeTest();
+});
