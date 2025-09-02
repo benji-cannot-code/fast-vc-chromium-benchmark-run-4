32681de6c97d65c99e2a5cc67313c51442cf737c
Peter Kasting <pkasting@chromium.org>
2025-09-01 17:17:00 -0700

    Inline implementation of SysColorChangeListener into the lone user.
    This can be further improved -- NativeThemeWin can treat the color and
    high contrast changes separately -- but that will come later.
    
    Bug: 431953872
    Change-Id: I5cd7294f0d49d229e954e877ff3cbcb937e9c161
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6905083
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1509318}

--- ui/views/controls/button/label_button_border.cc
+++ ui/views/controls/button/label_button_border.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/skia_conversions.h"
-#include "ui/gfx/sys_color_change_listener.h"
 #include "ui/native_theme/native_theme.h"
 #include "ui/views/border.h"
 #include "ui/views/controls/button/label_button.h"
