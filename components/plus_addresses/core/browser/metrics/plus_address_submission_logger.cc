0f22b74240dc2236b417391cf5447fcd2394bf73
Dominic Battre <battre@chromium.org>
2025-09-02 08:12:31 -0700

    Use OnAfterFormSubmitted in PlusAddressSubmissionLogger
    This is a trivial change in PlusAddressSubmissionLogger to use the
    OnAfterFormSubmitted signal instead of the OnBeforeFormSubmitted signal
    for metric recording. It should not have any visible effect.
    
    Bug: 434794640
    Change-Id: I1779ed9ab416e4d1f7a4d88bcab70202d76d6618
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6905727
    Auto-Submit: Dominic Battré <battre@chromium.org>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1509583}

--- components/plus_addresses/core/browser/metrics/plus_address_submission_logger.cc
+++ components/plus_addresses/core/browser/metrics/plus_address_submission_logger.cc
@@ -179,7 +179,7 @@ void PlusAddressSubmissionLogger::OnAutofillManagerStateChanged(
   }
 }
 
-void PlusAddressSubmissionLogger::OnBeforeFormSubmitted(
+void PlusAddressSubmissionLogger::OnAfterFormSubmitted(
     autofill::AutofillManager& manager,
     const autofill::FormData& form) {
   const CoreAccountInfo core_account_info =
