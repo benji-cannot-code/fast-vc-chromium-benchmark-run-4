c19abc85f3a66304f44ca2f42e49f849430c7dc2
Jian Li <jianli@chromium.org>
2025-09-02 07:53:22 -0700

    Enable FeedHeaderRemoval and WebFeedKillSwitch by default
    Enabled-by-default-reason: launching to 100%
    Bug: none
    Change-Id: Ic6f496d3f6e3478ed502c7397adb9d35053f2144
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6902079
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Auto-Submit: Jian Li <jianli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1509570}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceCoordinator.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceCoordinator.java
@@ -557,7 +557,7 @@ public class FeedSurfaceCoordinator
                     LayoutInflater.from(mActivity)
                             .inflate(R.layout.new_tab_page_feed_header, null, false);
             mHeaderView.setAccessibilityLiveRegion(View.ACCESSIBILITY_LIVE_REGION_POLITE);
-            if (treatment.equals("none")) {
+            if (!treatment.equals("label")) {
                 mHeaderView.setVisibility(View.GONE);
             }
         } else {
