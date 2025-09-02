f6e6c5c336fcf0cf16b256f071fa7cb39cf998e7
Mitsuru Oshima <oshima@chromium.org>
2025-09-02 03:42:57 -0700

    Mark an event generated by display config change IS_SYNTHESIZED
    This will keep the mouse invisible and prevent being used by an app.
    This CL rename GenerateMouseMove to SynthesizeMouseMove to be more
    explicit.
    
    Bug: 438837665, 440508983
    
    Change-Id: Ic2c573d7022ae64247cc4507311590c2d6a8a138
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6904020
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1509456}

--- ui/ozone/platform/drm/host/drm_window_host.h
+++ ui/ozone/platform/drm/host/drm_window_host.h
@@ -93,7 +93,7 @@ class DrmWindowHost : public PlatformWindow,
   void SizeConstraintsChanged() override;
 
   void OnCursorUpdate();
-  void GenerateMouseMove(const gfx::PointF& location);
+  void SynthesizeMouseMove(const gfx::PointF& location);
 
   // PlatformEventDispatcher:
   bool CanDispatchEvent(const PlatformEvent& event) override;
