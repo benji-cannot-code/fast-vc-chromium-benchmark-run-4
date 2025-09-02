25d7434ce4d311074a3c0bf07c22d01ae0d30d99
Mason Freed <masonf@chromium.org>
2025-09-02 14:07:07 -0700

    Add tweaks to behavior of losing interest
    This makes it possible to fire non-cancelable `loseinterest`
    events, and have the loss of interest not close popovers. (The
    default is still to have cancelable events and close popovers.)
    
    One case is changed: when `HidePopover` is called, it will
    (already) automatically lose interest in any upstream interest
    invoker. But now, in this case, that call will not also try to
    recursively close the popover again. Doing that sometimes
    generated a console warning that was a bit confusing.
    
    Bug: 326681249
    Change-Id: If9e4eec6855cee5783a1b4f49477112768871609
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6901319
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1509840}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -1720,7 +1720,9 @@ bool Element::InterestGained(Element* target) {
   return true;
 }
 
-bool Element::InterestLost(Element* target) {
+bool Element::InterestLost(Element* target,
+                           InterestLostCancelable cancelable,
+                           InterestLostPopoverBehavior behavior) {
   CHECK(RuntimeEnabledFeatures::HTMLInterestForAttributeEnabled(
       GetDocument().GetExecutionContext()));
 
@@ -1728,8 +1730,11 @@ bool Element::InterestLost(Element* target) {
     return false;
   }
 
-  Event* lose_interest_event = InterestEvent::Create(
-      event_type_names::kLoseinterest, this, Event::Cancelable::kYes);
+  Event* lose_interest_event =
+      InterestEvent::Create(event_type_names::kLoseinterest, this,
+                            cancelable == InterestLostCancelable::kCancelable
+                                ? Event::Cancelable::kYes
+                                : Event::Cancelable::kNo);
   target->DispatchEvent(*lose_interest_event);
   if (lose_interest_event->defaultPrevented()) {
     return false;
@@ -1742,16 +1747,18 @@ bool Element::InterestLost(Element* target) {
   }
 
   // If the target is a popover, hide it.
-  if (auto* popover = DynamicTo<HTMLElement>(target);
-      popover && popover->PopoverType() != PopoverValueType::kNone) {
-    if (popover->IsPopoverReady(PopoverTriggerAction::kHide,
-                                /*exception_state=*/nullptr,
-                                /*include_event_handler_text=*/true,
-                                &GetDocument())) {
-      popover->HidePopoverInternal(
-          /*invoker=*/this, HidePopoverFocusBehavior::kFocusPreviousElement,
-          HidePopoverTransitionBehavior::kFireEventsAndWaitForTransitions,
-          /*exception_state=*/nullptr);
+  if (behavior == InterestLostPopoverBehavior::kClosePopovers) {
+    if (auto* popover = DynamicTo<HTMLElement>(target);
+        popover && popover->PopoverType() != PopoverValueType::kNone) {
+      if (popover->IsPopoverReady(PopoverTriggerAction::kHide,
+                                  /*exception_state=*/nullptr,
+                                  /*include_event_handler_text=*/true,
+                                  &GetDocument())) {
+        popover->HidePopoverInternal(
+            /*invoker=*/this, HidePopoverFocusBehavior::kFocusPreviousElement,
+            HidePopoverTransitionBehavior::kFireEventsAndWaitForTransitions,
+            /*exception_state=*/nullptr);
+      }
     }
   }
   return true;
@@ -7840,13 +7847,14 @@ void Element::ShowInterestNow() {
   InterestGained(InterestForElement());
 }
 
-void Element::LoseInterestNow() {
+void Element::LoseInterestNow(InterestLostCancelable cancelable,
+                              InterestLostPopoverBehavior behavior) {
   DCHECK(RuntimeEnabledFeatures::HTMLInterestForAttributeEnabled(
       GetDocument().GetExecutionContext()));
   Element* target = InterestForElement();
   DCHECK_EQ(GetInvokerData()->ActiveInterestTarget(), target);
   DCHECK_EQ(GetInterestState(), InterestState::kFullInterest);
-  InterestLost(target);
+  InterestLost(target, cancelable, behavior);
 }
 
 bool Element::IsKeyboardFocusableSlow(UpdateBehavior update_behavior) const {
--- third_party/blink/renderer/core/html/html_element.cc
+++ third_party/blink/renderer/core/html/html_element.cc
@@ -1996,7 +1996,10 @@ PopoverHideResult HTMLElement::HidePopoverInternal(
       DCHECK_EQ(upstream_invoker->InterestForElement(), this);
       DCHECK_NE(upstream_invoker->GetInvokerData()->GetInterestState(),
                 InterestState::kNoInterest);
-      upstream_invoker->LoseInterestNow();
+      // We're already closing this popover, so don't try to close it again.
+      upstream_invoker->LoseInterestNow(
+          InterestLostCancelable::kCancelable,
+          InterestLostPopoverBehavior::kDontClosePopovers);
     }
 
     // The 'loseinterest' event handler could have changed this popover, e.g. by
@@ -2577,7 +2580,8 @@ bool HTMLElement::HandleCommandInternal(HTMLElement& invoker,
       ShowInterestNow();
     } else {
       CHECK_EQ(GetInterestState(), InterestState::kFullInterest);
-      LoseInterestNow();
+      LoseInterestNow(InterestLostCancelable::kCancelable,
+                      InterestLostPopoverBehavior::kClosePopovers);
     }
     return true;
   } else if (command == CommandEventType::kToggleFullscreen) {
