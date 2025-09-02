13089ae026db59262071d047bdfed6c91b5d7199
Ananya <ananyaseelam@google.com>
2025-09-02 11:12:24 -0700

    [context-input] Implement page context eligibility check
    This CL implements the method for checking eligibility of a tab in the
    `tab_contextualization_controller`.
    
    Note: There will be a fast follow CL for adding a browser test for the
    `tab_contextualization_controller`.
    
    Bug: 439597165
    Change-Id: Ifb678d776ac9d811eb3a258301b48ab17d05bc91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6907927
    Commit-Queue: Ananya Seelam <ananyaseelam@google.com>
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1509699}

--- components/lens/tab_contextualization_controller.cc
+++ components/lens/tab_contextualization_controller.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/lens/tab_contextualization_controller.h"
 
+#include "content/public/browser/navigation_handle.h"
 #include "ui/base/unowned_user_data/scoped_unowned_user_data.h"
 
 namespace lens {
@@ -13,13 +14,49 @@ DEFINE_USER_DATA(TabContextualizationController);
 
 TabContextualizationController::TabContextualizationController(
     tabs::TabInterface* tab)
-    : scoped_unowned_user_data_(tab->GetUnownedUserDataHost(), *this) {}
+    : content::WebContentsObserver(tab->GetContents()),
+      scoped_unowned_user_data_(tab->GetUnownedUserDataHost(), *this),
+      tab_(tab) {
+  tab_subscription_ = tab->RegisterWillDiscardContents(
+      base::BindRepeating(&TabContextualizationController::WillDiscardContents,
+                          base::Unretained(this)));
+}
 
 TabContextualizationController::~TabContextualizationController() = default;
+
+void TabContextualizationController::WillDiscardContents(
+    tabs::TabInterface* tab,
+    content::WebContents* old_contents,
+    content::WebContents* new_contents) {
+  Observe(new_contents);
+}
 
 TabContextualizationController* TabContextualizationController::From(
     tabs::TabInterface* tab) {
   return tab ? Get(tab->GetUnownedUserDataHost()) : nullptr;
+}
+
+void TabContextualizationController::PrimaryPageChanged(content::Page& page) {
+  is_page_context_eligible_ = false;
+}
+
+void TabContextualizationController::DidFinishNavigation(
+    content::NavigationHandle* navigation_handle) {
+  if (!navigation_handle->IsInPrimaryMainFrame() ||
+      !navigation_handle->HasCommitted() ||
+      navigation_handle->IsSameDocument()) {
+    return;
+  }
+
+  // Call the asynchronous eligibility check.
+  GetInitialPageContextEligibility(
+      base::BindOnce(&TabContextualizationController::OnEligibilityChecked,
+                     base::Unretained(this)));
+}
+
+void TabContextualizationController::OnEligibilityChecked(
+    bool is_page_context_eligible) {
+  is_page_context_eligible_ = is_page_context_eligible;
 }
 
 // TODO(crbug.com/439595898): Get contextual page content.
@@ -30,9 +67,8 @@ void TabContextualizationController::GetPageContext(
 void TabContextualizationController::GetInitialPageContextEligibility(
     GetPageContextEligibilityCallback callback) {}
 
-// TODO(crbug.com/439597165): Check tab eligibility.
 bool TabContextualizationController::GetCurrentPageContextEligibility() {
-  return false;
+  return is_page_context_eligible_;
 }
 
 }  // namespace lens
