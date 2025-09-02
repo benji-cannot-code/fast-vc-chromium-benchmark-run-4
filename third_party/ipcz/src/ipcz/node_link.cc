040f2257fe6547a1fa4b36fce7dcadbb3f7523b2
Egor Pasko <pasko@chromium.org>
2025-09-02 05:18:37 -0700

    ipcz: Add log messages in router and node_link
    Router is a central abstraction in ipcz. It is important to know about
    all these objects when investigating ipcz bugs. Lifetime of Router is
    also closely related to lifetime of Sublink.
    
    Add a few log messages to make it easier to track lifetimes of Router
    and RemoteRouterLink.
    
    Bug: None
    Change-Id: If2a9a7eca7dec553480905156c083f25008b26f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6905431
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1509482}

--- third_party/ipcz/src/ipcz/node_link.cc
+++ third_party/ipcz/src/ipcz/node_link.cc
@@ -130,6 +130,7 @@ Ref<RemoteRouterLink> NodeLink::AddRemoteRouterLink(
     return nullptr;
   }
 
+  DVLOG(4) << "Adding sublink " << sublink;
   auto [it, added] = sublinks_.try_emplace(
       sublink, Sublink(std::move(link), std::move(router)));
   if (!added) {
@@ -142,6 +143,7 @@ Ref<RemoteRouterLink> NodeLink::AddRemoteRouterLink(
 
 void NodeLink::RemoveRemoteRouterLink(SublinkId sublink) {
   absl::MutexLock lock(&mutex_);
+  DVLOG(4) << "Removing RemoteRouterLink for sublink " << sublink;
   sublinks_.erase(sublink);
   deleted_sublinks_.Insert(sublink);
 }
