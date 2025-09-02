e3cd329e3c326b6a638e97cef1c73a23ac45123d
Paul Adedeji <pauladedeji@google.com>
2025-09-02 15:07:57 -0700

    [ntp-composebox] Show context menu on click.
    This CL adds the baseline styling/logic for the context menu. Followup
    CL(s) will add more behavior to it.
    
    The only outstanding style changes should be for the tabs section, which
    is tracked by crbug.com/442575942.
    
    - Adds menu with image and file upload icons.
    - Adds new test folder for composebox TS tests.
    - Adds placeholder code for tabs logic.
    
    https://screen/72QZH7zMuV2BnrG
    
    Change-Id: Ida1ec0d516f0dc1358a530f566b826a51b2886b1
    Bug: 441491936, 439618274, 442575942
    Fixed: 441491936
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6907140
    Commit-Queue: Ananya Seelam <ananyaseelam@google.com>
    Reviewed-by: Ananya Seelam <ananyaseelam@google.com>
    Auto-Submit: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1509888}

--- ui/webui/resources/cr_components/composebox/context_menu_entrypoint.ts
+++ ui/webui/resources/cr_components/composebox/context_menu_entrypoint.ts
@@ -3,14 +3,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import './icons.html.js';
 import '//resources/cr_elements/icons.html.js';
 import '//resources/cr_elements/cr_icon/cr_icon.js';
+import '//resources/cr_elements/cr_action_menu/cr_action_menu.js';
 import '//resources/cr_elements/cr_button/cr_button.js';
 
+import {AnchorAlignment} from '//resources/cr_elements/cr_action_menu/cr_action_menu.js';
+import type {CrActionMenuElement} from '//resources/cr_elements/cr_action_menu/cr_action_menu.js';
 import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
 
 import {getCss} from './context_menu_entrypoint.css.js';
 import {getHtml} from './context_menu_entrypoint.html.js';
+
+export interface ContextMenuEntrypointElement {
+  $: {
+    entrypoint: HTMLElement,
+    entrypointIcon: HTMLElement,
+    menu: CrActionMenuElement,
+  };
+}
 
 export class ContextMenuEntrypointElement extends CrLitElement {
   static get is() {
@@ -23,6 +35,36 @@ export class ContextMenuEntrypointElement extends CrLitElement {
 
   override render() {
     return getHtml.bind(this)();
+  }
+
+  static override get properties() {
+    return {
+      hasTabSuggestions_: {type: Boolean},
+    };
+  }
+
+  // TODO(crbug.com/442575942): Set `hasTabSuggestions_` to false by default,
+  // and add actual logic for setting it true.
+  protected accessor hasTabSuggestions_: boolean = true;
+
+  protected onEntrypointClick_() {
+    this.$.menu.showAt(this.$.entrypointIcon, {
+      top: this.$.entrypointIcon.getBoundingClientRect().bottom,
+      width: 190,
+      anchorAlignmentX: AnchorAlignment['AFTER_START'],
+    });
+  }
+
+  protected openImageUpload_() {
+    // TODO(crbug.com/439618274):  Integrate existing file and photo context
+    // behavior into new context menu.
+    this.$.menu.close();
+  }
+
+  protected openFileUpload_() {
+    // TODO(crbug.com/439618274):  Integrate existing file and photo context
+    // behavior into new context menu.
+    this.$.menu.close();
   }
 }
 
