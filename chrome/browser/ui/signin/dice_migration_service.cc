db0ee7bb8336088d035d72e9ebd1193d643dfd97
Ankush Singh <ankushkush@google.com>
2025-09-02 01:27:00 -0700

    Update the help center URL for DICe migration dialog
    The help center URL now includes a p-link parameter to direct users to a
    more specific section of the help page.
    
    Unfortunately there are no corresponding tests because the link exists
    as part of the dialog paragraph and there's no easy mechanism in the
    tests to trigger a click on that part of the paragraph.
    
    Note: The p-link does not actually exist yet but will be added asap. See
    https://crbug.com/425312284#comment5 (sorry, Googlers-only).
    
    Bug: 442382553
    Change-Id: I9ae6aff6b514d9b1a6dc0a531838d5e711ae1436
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6905372
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Auto-Submit: Ankush Singh <ankushkush@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1509411}

--- chrome/browser/ui/signin/dice_migration_service.cc
+++ chrome/browser/ui/signin/dice_migration_service.cc
@@ -45,7 +45,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 constexpr char kHelpCenterUrl[] =
-    "https://support.google.com/chrome/answer/185277";
+    "https://support.google.com/chrome/answer/"
+    "185277?p=manage_autofill_settings";
 
 constexpr base::TimeDelta kForcedSigninToastDelay = base::Seconds(5);
 
