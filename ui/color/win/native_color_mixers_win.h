185e945ad2667ce8f3057890eef854e9c768ed33
Peter Kasting <pkasting@chromium.org>
2025-09-01 17:23:14 -0700

    Reduce duplication in fetching Windows native colors.
    We only need call GetSysColor in one place, and some of the existing
    colors are never used.
    
    Bug: 431953872
    Change-Id: I784b67221e4a87818ee8856c0608b2fc0e264f8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6904637
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1509319}

---
+++ ui/color/win/native_color_mixers_win.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_COLOR_WIN_NATIVE_COLOR_MIXERS_WIN_H_
+#define UI_COLOR_WIN_NATIVE_COLOR_MIXERS_WIN_H_
+
+#include "base/component_export.h"
+#include "base/containers/flat_map.h"
+#include "third_party/skia/include/core/SkColor.h"
+#include "ui/color/color_id.h"
+
+namespace ui {
+
+// Returns a map from the Windows-specific native color IDs to their current
+// corresponding system colors, as given by the OS `GetSysColor()` function.
+COMPONENT_EXPORT(COLOR) base::flat_map<ColorId, SkColor> GetCurrentSysColors();
+
+}  // namespace ui
+
+#endif  // UI_COLOR_WIN_NATIVE_COLOR_MIXERS_WIN_H_
