57d25bfbc67db31af78d24eae21e9c75a447e92d
David Sanders <dsanders11@ucsbalum.com>
2025-09-02 16:16:17 -0700

    Remove unused include in origin_agent_cluster_isolation_state.h
    Should save ~51 MiB of compiler input size.
    
    Bug: 40318405
    Change-Id: I8db612dfa52e09e89d53e56c5713d8eca63e4f60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6903217
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#1509927}

--- content/browser/origin_agent_cluster_isolation_state.h
+++ content/browser/origin_agent_cluster_isolation_state.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_BROWSER_ORIGIN_AGENT_CLUSTER_ISOLATION_STATE_H_
 #define CONTENT_BROWSER_ORIGIN_AGENT_CLUSTER_ISOLATION_STATE_H_
 
-#include "base/tracing/protos/chrome_track_event.pbzero.h"
 #include "content/browser/agent_cluster_key.h"
 #include "content/common/content_export.h"
 
