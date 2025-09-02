45e0cf6c094cf8ee3e7f656ecf93106c9d000c0b
Kent Tamura <tkent@chromium.org>
2025-09-01 23:32:33 -0700

    FitText: Pass a pair of scaling factors from MeasurePerBlockScale() to LineFitter.
    It's necessary for the `font-size` scaling method.
    This CL has no behavior changes.
    
    Bug: 417306102
    Change-Id: I0c8d6cdf946a21f82569c61d64f1c56935279434
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6904062
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1509372}

--- third_party/blink/renderer/core/layout/inline/inline_child_layout_context.h
+++ third_party/blink/renderer/core/layout/inline/inline_child_layout_context.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 
 #include "third_party/blink/renderer/core/core_export.h"
+#include "third_party/blink/renderer/core/layout/inline/fit_text_utils.h"
 #include "third_party/blink/renderer/core/layout/inline/fragment_items_builder.h"
 #include "third_party/blink/renderer/core/layout/inline/inline_box_state.h"
 #include "third_party/blink/renderer/core/layout/inline/logical_line_item.h"
@@ -74,11 +75,11 @@ class CORE_EXPORT InlineChildLayoutContext {
     balanced_available_width_ = value;
   }
 
-  // FitText: Enable the measuring mode if `paragraph_scale` is std::nullopt.
-  void EnableMeasuringModeIfNecessary(std::optional<float> paragraph_scale);
+  // FitText: Enable the measuring mode if `paragraph_scale` is nullptr.
+  void EnableMeasuringModeIfNecessary(const ParagraphScale* paragraph_scale);
   bool IsMeasuringScale() const { return is_measuring_scale_; }
   // FitText: Returns the minimum scale handled in the measuring mode.
-  float MeasuredScale() const;
+  ParagraphScale MeasuredScale() const;
 
  protected:
   InlineChildLayoutContext(const InlineNode& node,
@@ -110,7 +111,7 @@ class CORE_EXPORT InlineChildLayoutContext {
 
   // FitText: In the measuring mode, this field is updated for every line.
   // Otherwise, this holds the paragraph scale which is applied to every line.
-  float minimum_scale_ = 1.0f;
+  ParagraphScale minimum_scale_;
   // FitText: True if in the measuring mode.
   bool is_measuring_scale_ = false;
 };
