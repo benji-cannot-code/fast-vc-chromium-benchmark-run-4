5c2050f01f3418ff4e2aa4ee164770575b3038f5
Robbie Gibson <rkgibson@google.com>
2025-09-02 13:48:46 -0700

    [iOS] Don't update theme on cancel unless changed
    Before this, cancelling the gallery picker when the current background
    is a gallery item caused background to flicker as the image was re-
    loaded.
    
    Bug: 440133415
    Change-Id: If457569ac22ee36e32fd13a86613ad0c17e7bc39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6908867
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1509817}

--- ios/chrome/browser/home_customization/coordinator/home_customization_background_picker_action_sheet_mediator.mm
+++ ios/chrome/browser/home_customization/coordinator/home_customization_background_picker_action_sheet_mediator.mm
@@ -86,8 +86,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)cancelThemeSelection {
-  self.themeHasChanged = NO;
-  _homeBackgroundCustomizationService->RestoreCurrentTheme();
+  if (self.themeHasChanged) {
+    _homeBackgroundCustomizationService->RestoreCurrentTheme();
+    self.themeHasChanged = NO;
+  }
 }
 
 #pragma mark - HomeBackgroundCustomizationServiceObserving
