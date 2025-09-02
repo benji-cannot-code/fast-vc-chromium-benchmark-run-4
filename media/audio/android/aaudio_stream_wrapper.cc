3a782b7916509c266f1da564f8d1c4afcf45b43b
Thomas Guilbert <tguilbert@chromium.org>
2025-09-02 16:16:52 -0700

    Default to low latency mode for AAudio inputs
    Most applications which use a microphone benefit from lower latency.
    
    This CL changes AAudio input streams to use low latency mode by default,
    unless there is an explicit `latency_tag`.
    
    Using this AAUDIO_PERFORMANCE_MODE_LOW_LATENCY configures AAudio to use
    smaller buffer sizes, and enables MMAP.
    
    Enabled-by-default-reason: killswitch
    Bug: 438088221
    Change-Id: I1d31baee1cb24992880d86ae828666a0587179d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6896943
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1509928}

--- media/audio/android/aaudio_stream_wrapper.cc
+++ media/audio/android/aaudio_stream_wrapper.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <array>
 #include <optional>
 
+#include "base/feature_list.h"
 #include "base/logging.h"
 #include "base/memory/raw_ptr.h"
 #include "base/metrics/histogram_functions.h"
@@ -24,6 +25,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // AAudioStreamBuilder_setChannelMask was not introduced until API version 32.
 #define AAUDIO_CHANNEL_MASK_MIN_API 32
+
+BASE_FEATURE(AAudioInputLowLatencyModeByDefault,
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 namespace media {
 
@@ -176,6 +180,14 @@ AAudioStreamWrapper::AAudioStreamWrapper(DataCallback* callback,
   CHECK(params.IsValid());
   CHECK(callback_);
 
+  if (stream_type_ == StreamType::kInput &&
+      base::FeatureList::IsEnabled(kAAudioInputLowLatencyModeByDefault)) {
+    // Default to low latency for input streams.
+    performance_mode_ = AAUDIO_PERFORMANCE_MODE_LOW_LATENCY;
+  } else {
+    performance_mode_ = AAUDIO_PERFORMANCE_MODE_NONE;
+  }
+
   switch (params.latency_tag()) {
     case AudioLatency::Type::kExactMS:
     case AudioLatency::Type::kInteractive:
@@ -186,7 +198,8 @@ AAudioStreamWrapper::AAudioStreamWrapper(DataCallback* callback,
       performance_mode_ = AAUDIO_PERFORMANCE_MODE_POWER_SAVING;
       break;
     case AudioLatency::Type::kUnknown:
-      performance_mode_ = AAUDIO_PERFORMANCE_MODE_NONE;
+      // The default value should be set above.
+      break;
   }
 
   TRACE_EVENT2("audio", "AAudioStreamWrapper::AAudioStreamWrapper",
