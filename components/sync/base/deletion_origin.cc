b7b795c368a0d93370ef01c909b8d3354af273a6
Michael Tatarski <mtatarski@google.com>
2025-09-02 05:34:32 -0700

    Refactor string truncation and vector initialization
    In `deletion_origin.cc`, use `string::replace` for truncating filenames with an ellipsis. In `nigori_state.cc`, use a range constructor to initialize `std::vector` from a proto's repeated field.
    
    Bug: 441464169
    Change-Id: I8d78fb13129d7e3de75d8eccf6eebb9effa9c05f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6907567
    Auto-Submit: Michael Tatarski <mtatarski@google.com>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Michael Tatarski <mtatarski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1509489}

--- components/sync/base/deletion_origin.cc
+++ components/sync/base/deletion_origin.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace syncer {
 namespace {
 
+constexpr std::string_view kEllipsis = "...";
 constexpr size_t kMaxFileNameBeforeTruncation = 30;
 
 // Truncates the filename to a maximum size by stripping, if needed, the
@@ -30,9 +31,7 @@ std::string MaybeTruncateFileName(std::string_view file_name) {
 
   std::string result(
       file_name.substr(file_name.size() - kMaxFileNameBeforeTruncation));
-  for (int i = 0; i < 3; i++) {
-    result[i] = '.';
-  }
+  result.replace(0, kEllipsis.size(), kEllipsis);
   return result;
 }
 
