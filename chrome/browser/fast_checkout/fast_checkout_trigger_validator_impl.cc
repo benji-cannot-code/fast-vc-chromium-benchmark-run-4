f765aa64a312be59e709a27d91f75d266ab080f1
Christoph Schwering <schwering@google.com>
2025-09-01 23:47:08 -0700

    [Autofill] Add missing namespace for country_type.h
    This CL wraps the contetn of country_code.h in
      namespace autofill { ... }
    and adds the necessary namespace qualifications.
    
    Bug: 40100455
    Change-Id: Id84fe842aa7e21ed812b3bc62085db5ef1e6e87f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6897967
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1509375}

--- chrome/browser/fast_checkout/fast_checkout_trigger_validator_impl.cc
+++ chrome/browser/fast_checkout/fast_checkout_trigger_validator_impl.cc
@@ -55,7 +55,7 @@ FastCheckoutTriggerOutcome FastCheckoutTriggerValidatorImpl::ShouldRun(
   }
 
   if (autofill_client_->GetVariationConfigCountryCode() !=
-      GeoIpCountryCode("US")) {
+      autofill::GeoIpCountryCode("US")) {
     return FastCheckoutTriggerOutcome::kUnsupportedCountry;
   }
 
--- ios/chrome/browser/autofill/ui_bundled/bottom_sheet/settings_autofill_edit_profile_bottom_sheet_handler.mm
+++ ios/chrome/browser/autofill/ui_bundled/bottom_sheet/settings_autofill_edit_profile_bottom_sheet_handler.mm
@@ -49,7 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (std::unique_ptr<autofill::AutofillProfile>)autofillProfile {
   // Since this is creating a new (empty) address, use the app's locale country
   // code as the default value.
-  AddressCountryCode countryCode =
+  autofill::AddressCountryCode countryCode =
       _addressDataManager->GetDefaultCountryCodeForNewAddress();
 
   std::unique_ptr<autofill::AutofillProfile> autofillProfile =
