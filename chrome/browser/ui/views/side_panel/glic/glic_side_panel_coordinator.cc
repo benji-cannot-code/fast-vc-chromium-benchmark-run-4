4f4ca66199d3c60a5851f3084202799682ee0f1d
Anthony Cui <cuianthony@google.com>
2025-09-02 11:17:30 -0700

    Move GlicSidePanelCoordinator and implement SidePanelEntryObserver
    GlicSidePanelCoordinator moved from initial location in order to prevent circular dependencies
    
    Bug: b:438806750
    Change-Id: I27e8c87078b30eab009b02423fdbc112c5000298
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6886520
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Anthony Cui <cuianthony@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1509708}

--- chrome/browser/ui/views/side_panel/glic/glic_side_panel_coordinator.cc
+++ chrome/browser/ui/views/side_panel/glic/glic_side_panel_coordinator.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/glic/widget/glic_side_panel_coordinator.h"
+#include "chrome/browser/ui/views/side_panel/glic/glic_side_panel_coordinator.h"
 
 #include "base/functional/callback.h"
 #include "chrome/browser/glic/host/glic_ui.h"
@@ -20,22 +20,32 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace glic {
 
-GlicSidePanelCoordinator::GlicSidePanelCoordinator() = default;
+GlicSidePanelCoordinator::GlicSidePanelCoordinator(Profile* profile)
+    : glic_service_(GlicKeyedServiceFactory::GetGlicKeyedService(profile)) {}
 
 void GlicSidePanelCoordinator::CreateAndRegisterEntry(
     SidePanelRegistry* global_registry) {
-  global_registry->Register(std::make_unique<SidePanelEntry>(
+  auto entry = std::make_unique<SidePanelEntry>(
       SidePanelEntry::Key(SidePanelEntry::Id::kGlic),
       base::BindRepeating(&GlicSidePanelCoordinator::CreateGlicWebView,
                           base::Unretained(this)),
-      /*default_content_width_callback=*/base::NullCallback()));
+      /*default_content_width_callback=*/base::NullCallback());
+  entry->AddObserver(this);
+  global_registry->Register(std::move(entry));
+}
+
+void GlicSidePanelCoordinator::OnEntryHidden(SidePanelEntry* entry) {
+  if (glic_service_) {
+    glic_service_->ClosePanel();
+  }
 }
 
 std::unique_ptr<views::View> GlicSidePanelCoordinator::CreateGlicWebView(
     SidePanelEntryScope& scope) {
-  auto* const glic_service = GlicKeyedServiceFactory::GetGlicKeyedService(
-      scope.GetBrowserWindowInterface().GetProfile());
-  return glic_service->window_controller().CreateGlicViewForSidePanel();
+  if (!glic_service_) {
+    return nullptr;
+  }
+  return glic_service_->window_controller().CreateGlicViewForSidePanel();
 }
 
 }  // namespace glic
