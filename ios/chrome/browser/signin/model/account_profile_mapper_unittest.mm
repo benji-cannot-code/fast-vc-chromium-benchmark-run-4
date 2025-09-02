5aa0cfe202e5c23be9a589643d3930238a6ed7d6
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2025-09-02 08:44:18 -0700

    [ios] Add ProfileManagerIOS::GetProfilePath()
    This lets us know a Profile's path, even if it's not currently loaded in
    memory. This will be used for the enterprise profile reporting feature.
    
    Bug: 385175028
    Change-Id: I928f89240396bf45974af0c7aebd7a5ffdbcf26c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6862415
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1509598}

--- ios/chrome/browser/signin/model/account_profile_mapper_unittest.mm
+++ ios/chrome/browser/signin/model/account_profile_mapper_unittest.mm
@@ -299,6 +299,10 @@ class FakeProfileManagerIOS : public ProfileManagerIOS {
     return &profile_attributes_storage_;
   }
 
+  base::FilePath GetProfilePath(std::string_view name) override {
+    NOTREACHED();
+  }
+
  private:
   ScopedProfileKeepAliveIOS CreateScopedProfileKeepAlive(ProfileIOS* profile) {
     return ScopedProfileKeepAliveIOS(CreatePassKey(), profile, {});
