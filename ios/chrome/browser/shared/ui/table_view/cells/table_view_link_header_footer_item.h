d742ca41fa98f150fe645cfe864e30c7a9794cd7
Max Ihlenfeldt <max@igalia.com>
2025-09-02 03:00:30 -0700

    [iOS] Allow setting font for TableViewLinkHeaderFooterView
    The view name suggests it can be used as both a header or a footer, but
    the font is currently hardcoded to the preferred font for
    UIFontTextStyleFootnote, which is unsuitable for headers.
    
    This CL adds another setText: method that allows specifying the font.
    
    Bug: None
    Change-Id: I744548c2a7eb28798f93cdbdcc96f8cbcdbc4299
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6899287
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Max Ihlenfeldt <max@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1509446}

--- ios/chrome/browser/shared/ui/table_view/cells/table_view_link_header_footer_item.h
+++ ios/chrome/browser/shared/ui/table_view/cells/table_view_link_header_footer_item.h
@@ -58,11 +58,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Sets the `text` displayed by this cell. If the `text` contains a link, the
 // link is appropriately colored. The text is coolored with `color`. If specific
 // `textAlignment` is provided, apply that as well, otherwise it is
-// NSTextAlignmentNatural.
+// NSTextAlignmentNatural. The default font is the preferred font for
+// UIFontTextStyleFootnote.
 - (void)setText:(NSString*)text withColor:(UIColor*)color;
 - (void)setText:(NSString*)text
         withColor:(UIColor*)color
     textAlignment:(NSTextAlignment)textAlignment;
+- (void)setText:(NSString*)text
+        withColor:(UIColor*)color
+    textAlignment:(NSTextAlignment)textAlignment
+             font:(UIFont*)font;
 
 // If forceIndents is YES, activates the constraint to align the header/footer
 // with section items.
