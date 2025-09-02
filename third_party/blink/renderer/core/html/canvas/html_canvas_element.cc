85f9ddeab5320fac07794adbe4d436ef16963048
Colin Blundell <blundell@chromium.org>
2025-09-02 06:46:52 -0700

    [Blink] Remove expired metric
    Change-Id: I260988d14571e306f149080fbca9f27b6c4ef9c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6905946
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1509532}

--- third_party/blink/renderer/core/html/canvas/html_canvas_element.cc
+++ third_party/blink/renderer/core/html/canvas/html_canvas_element.cc
@@ -1219,8 +1219,6 @@ void HTMLCanvasElement::PaintInternal(GraphicsContext& context,
       UMA_HISTOGRAM_BOOLEAN("Blink.Canvas.2DPrintingAsVector", true);
       return;
     }
-    UMA_HISTOGRAM_ENUMERATION("Blink.Canvas.VectorPrintFallbackReason",
-                              provider->printing_fallback_reason());
     UMA_HISTOGRAM_BOOLEAN("Blink.Canvas.2DPrintingAsVector", false);
   }
 
--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
@@ -328,8 +328,6 @@ class PLATFORM_EXPORT CanvasResourceProvider
   // are modified externally from the provider's SkSurface.
   virtual void NotifyTexParamsModified(const CanvasResource* resource) {}
 
-  FlushReason printing_fallback_reason() { return printing_fallback_reason_; }
-
   void RestoreBackBuffer(const cc::PaintImage&);
 
   ResourceProviderType GetType() const { return type_; }
@@ -499,7 +497,6 @@ class PLATFORM_EXPORT CanvasResourceProvider
 
   bool clear_frame_ = true;
   FlushReason last_flush_reason_ = FlushReason::kNone;
-  FlushReason printing_fallback_reason_ = FlushReason::kNone;
   std::optional<cc::PaintRecord> last_recording_;
 
   base::WeakPtrFactory<CanvasResourceProvider> weak_ptr_factory_{this};
