4327c6558bd224a6e2ccc10ae93381ce5f6ccded
Andrew Grieve <agrieve@chromium.org>
2025-09-02 14:18:18 -0700

    network_traffic_annotation.h: Use consteval for compile-time guarantee
    Noticed that a lot of annotation hashes were being computed at runtime
    rather than at compile time.
    
    Saves ~17kb for arm32, and 30kb for arm64
    
    Also changes an anonymous namespace to an "internal" one, since
    anonymous within a header file is... not anonymous.
    
    Change-Id: I6b8641c6e3f0dd3fc894a268ba2bd6252c98a400
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6898814
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1509847}

--- services/network/resource_scheduler/resource_scheduler_unittest.cc
+++ services/network/resource_scheduler/resource_scheduler_unittest.cc
@@ -862,7 +862,7 @@ TEST_F(ResourceSchedulerTest,
     net::NetworkTrafficAnnotationTag tag = net::DefineNetworkTrafficAnnotation(
         "metrics_report_uma",
         "Traffic annotation for unit, browser and other tests");
-    // (COMPUTE_NETWORK_TRAFFIC_ANNOTATION_ID_HASH(""));
+    // (ComputeAnnotationHash(""));
     std::unique_ptr<TestRequest> lows = (NewTrustedRequest(
         url.c_str(), net::LOWEST, tag));  //"metrics_report_uma"));
     EXPECT_EQ(test.expected_browser_initiated_traffic_started, lows->started())
@@ -906,7 +906,7 @@ TEST_F(ResourceSchedulerTest, P2PConnectionWentAway) {
     net::NetworkTrafficAnnotationTag tag = net::DefineNetworkTrafficAnnotation(
         "metrics_report_uma",
         "Traffic annotation for unit, browser and other tests");
-    // (COMPUTE_NETWORK_TRAFFIC_ANNOTATION_ID_HASH(""));
+    // (ComputeAnnotationHash(""));
     std::unique_ptr<TestRequest> lows = (NewTrustedRequest(
         url.c_str(), net::LOWEST, tag));  //"metrics_report_uma"));
     EXPECT_FALSE(lows->started());
@@ -946,7 +946,7 @@ TEST_F(ResourceSchedulerTest,
   net::NetworkTrafficAnnotationTag tag = net::DefineNetworkTrafficAnnotation(
       "metrics_report_uma",
       "Traffic annotation for unit, browser and other tests");
-  // (COMPUTE_NETWORK_TRAFFIC_ANNOTATION_ID_HASH(""));
+  // (ComputeAnnotationHash(""));
   std::unique_ptr<TestRequest> lows = (NewTrustedRequest(
       url.c_str(), net::LOWEST, tag));  //"metrics_report_uma"));
   EXPECT_FALSE(lows->started());
