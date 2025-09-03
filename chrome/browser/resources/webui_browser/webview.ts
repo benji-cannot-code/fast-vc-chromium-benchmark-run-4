8a672b97148ef9bd707d7f8edfff3ce0abf946eb
Paul Jensen <pauljensen@chromium.org>
2025-09-03 09:50:07 -0700

    webium: refactor GuestContents-specific PageHandler pieces into GuestHandler
    This stops PageHandler from becoming an interface multiplexed on the
    guest_id which generally simplifies the code by not having to pass
    around and check the guest_id.
    
    Bug: 393144165
    Change-Id: I9491d0e2d2132f10e213e109e75993daabc0f3b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6896337
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Auto-Submit: Paul Jensen <pauljensen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1510329}

--- chrome/browser/resources/webui_browser/webview.ts
+++ chrome/browser/resources/webui_browser/webview.ts
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {assert} from '//resources/js/assert.js';
 import {CrLitElement, html} from '//resources/lit/v3_0/lit.rollup.js';
 
+import {GuestHandlerRemote} from './browser.mojom-webui.js';
 import {BrowserProxy} from './browser_proxy.js';
 import {getCss} from './webview.css.js';
 
@@ -36,6 +37,7 @@ export class WebviewElement extends CrLitElement {
 
   accessor guestId: number = -1;
   private attached: boolean = false;
+  guestHandler?: GuestHandlerRemote;
 
   override async connectedCallback() {
     super.connectedCallback();
@@ -77,30 +79,40 @@ export class WebviewElement extends CrLitElement {
   }
 
   goBack() {
-    BrowserProxy.getPageHandler().goBack(this.guestId);
+    assert(this.guestHandler);
+    this.guestHandler.goBack();
   }
 
   goForward() {
-    BrowserProxy.getPageHandler().goForward(this.guestId);
+    assert(this.guestHandler);
+    this.guestHandler.goForward();
   }
 
   reload() {
-    BrowserProxy.getPageHandler().reload(this.guestId);
+    if (this.guestHandler) {
+      this.guestHandler.reload();
+    }
   }
 
   stopLoading() {
-    BrowserProxy.getPageHandler().stopLoading(this.guestId);
+    if (this.guestHandler) {
+      this.guestHandler.stopLoading();
+    }
   }
 
   async canGoBack(): Promise<boolean> {
-    const {canGoBack} =
-        await BrowserProxy.getPageHandler().canGoBack(this.guestId);
+    if (!this.guestHandler) {
+      return false;
+    }
+    const {canGoBack} = await this.guestHandler.canGoBack();
     return canGoBack;
   }
 
   async canGoForward(): Promise<boolean> {
-    const {canGoForward} =
-        await BrowserProxy.getPageHandler().canGoForward(this.guestId);
+    if (!this.guestHandler) {
+      return false;
+    }
+    const {canGoForward} = await this.guestHandler.canGoForward();
     return canGoForward;
   }
 }
@@ -128,10 +140,12 @@ export class TabWebviewElement extends WebviewElement {
   }
 
   private attachTabContents() {
+    const handler = new GuestHandlerRemote();
     BrowserProxy.getPageHandler()
-        .getGuestIdForTabId(this.tabId)
+        .getGuestIdForTabId(this.tabId, handler.$.bindNewPipeAndPassReceiver())
         .then(({guestId}) => {
           this.guestId = guestId;
+          this.guestHandler = handler;
           this.tryToAttach();
         });
   }
