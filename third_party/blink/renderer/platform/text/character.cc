340a34bf37a66d017cfd17cedc9bfad2a33eaf51
Minseong Kim <jja08111@gmail.com>
2025-09-03 01:58:37 -0700

    [text-emphasis] Allow specific punctuation characters
    This CL implements an exception list that allows a specific set of
    punctuation characters to receive CSS `text-emphasis`.
    
    This CL cannot fix
    `css/css-text-decor/text-emphasis-punctuation-3.html`, because Blink
    renders text-emphasis differently from ruby, which the test uses as its
    reference. Related issue is https://crbug.com/40851160
    
    Spec:
    https://drafts.csswg.org/css-text-decor/#text-emphasis-style-property
    
    Bug: 359420112
    Change-Id: I1b92b79e5f42acb1ce4240f49daab5675f6a5a9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6904710
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Minseong Kim <jja08111@gmail.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1510097}

--- third_party/blink/renderer/platform/text/character.cc
+++ third_party/blink/renderer/platform/text/character.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 
 #include "base/synchronization/lock.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/text/character_property_data.h"
 #include "third_party/blink/renderer/platform/text/icu_error.h"
 #include "third_party/blink/renderer/platform/wtf/std_lib_extras.h"
@@ -261,6 +262,41 @@ bool Character::CanReceiveTextEmphasis(UChar32 c) {
     return false;
   }
 
+  if (RuntimeEnabledFeatures::TextEmphasisPunctuationExceptionsEnabled()) {
+    // A set of exceptions for punctuation.
+    switch (c) {
+      // List from
+      // https://drafts.csswg.org/css-text-decor/#text-emphasis-style-property
+      case uchar::kNumberSign:
+      case uchar::kPercentSign:
+      case uchar::kAmpersand:
+      case uchar::kCommercialAt:
+      case uchar::kSectionSign:
+      case uchar::kPilcrowSign:
+      case uchar::kArabicIndicPerMilleSign:
+      case uchar::kArabicIndicPerTenThousandSign:
+      case uchar::kArabicPercentSign:
+      case uchar::kPerMilleSign:
+      case uchar::kPerTenThousandSign:
+      case uchar::kTironianSignEt:
+      case uchar::kReversedPilcrowSign:
+      case uchar::kSwungDash:
+      case uchar::kPartAlternationMark:
+      // Characters with NFKD equivalence to the above.
+      case uchar::kSmallNumberSign:
+      case uchar::kSmallAmpersand:
+      case uchar::kSmallPercentSign:
+      case uchar::kSmallCommercialAt:
+      case uchar::kFullwidthNumberSign:
+      case uchar::kFullwidthPercentSign:
+      case uchar::kFullwidthAmpersand:
+      case uchar::kFullwidthCommercialAt:
+        return true;
+      default:
+        break;
+    }
+  }
+
   // Punctuation
   if (category &
       (unicode::kPunctuation_Dash | unicode::kPunctuation_Open |
@@ -269,9 +305,6 @@ bool Character::CanReceiveTextEmphasis(UChar32 c) {
        unicode::kPunctuation_FinalQuote)) {
     return false;
   }
-  // TODO(layout-dev): css/css-text-decor/text-emphasis-punctuation-3.html
-  // requires implementation for the following rule in the specification:
-  // > do not NFKD normalize to any of the following symbols:
 
   return true;
 }
