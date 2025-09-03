8afb1ce3d7f14c68fd5c404964924fc7894a0572
Bryan Nguyen <nguyenbryan@google.com>
2025-09-03 10:52:06 -0700

    [lensoverlay] Add field trial config for straight to SRP.
    Bug: 442642702
    Change-Id: I5ccec99f78b2be27cbe172b3c8996a9cd485aeb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6909612
    Commit-Queue: Bryan Nguyen <nguyenbryan@google.com>
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1510390}

--- chrome/browser/ui/lens/lens_overlay_controller_interactive_uitest.cc
+++ chrome/browser/ui/lens/lens_overlay_controller_interactive_uitest.cc
@@ -1418,7 +1418,7 @@ class LensOverlayControllerEduActionChipTest
             lens::features::kLensOverlayEduActionChip,
             {{"url-allow-filters", "[\"*\"]"},
              {"url-path-match-allow-filters", "[\"select\"]"}})},
-        {});
+        {lens::features::kLensOverlayStraightToSrp});
   }
 };
 
