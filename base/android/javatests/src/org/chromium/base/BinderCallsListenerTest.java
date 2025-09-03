d893ed0099043f5a2f28355b6fee8bcba4d59a33
Jack Thiesen <jthiesen@chromium.org>
2025-09-03 13:47:59 -0700

    [Deprecate SDK_INT < Q] BinderCallsListenerTest.java
    Bug: 425948259
    Bug: 402822704
    Change-Id: Ibe5251564d0c2334fa776b42dbf28a4f59992dee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6909926
    Commit-Queue: Jack Thiesen <jthiesen@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1510523}

--- base/android/javatests/src/org/chromium/base/BinderCallsListenerTest.java
+++ base/android/javatests/src/org/chromium/base/BinderCallsListenerTest.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.base;
 
 import android.app.ActivityManager;
-import android.os.Build;
 import android.util.Pair;
 
 import androidx.test.annotation.UiThreadTest;
@@ -45,12 +44,6 @@ public class BinderCallsListenerTest {
         BinderCallsListener listener = BinderCallsListener.getInstance();
         boolean success = listener.installListener();
 
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
-            // The API was added in Android 10, but don't skip the test in earlier releases, to
-            // check that we don't cause crashes there.
-            Assert.assertFalse(success);
-            return;
-        }
         Assert.assertTrue(success);
         List<Pair<String, String>> actions = new ArrayList<>();
         listener.setBinderCallListenerObserverForTesting(
