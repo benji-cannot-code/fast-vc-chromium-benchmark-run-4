299b68cada1c370a18912a7e51d7645dcc58b51e
Sylvain Defresne <sdefresne@chromium.org>
2025-09-03 09:49:09 -0700

    [ios] Rename RegisterBrowserStatePrefs() to RegisterProfilePrefs()
    This convert files in //ios/chrome/browser/home_customization/model.
    
    Bug: 436484783
    Change-Id: I531b3b47e1ad6d1f418fe4b0403d7f50cd7e6a9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6905408
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1510328}

--- ios/chrome/browser/home_customization/model/home_background_customization_service_factory.mm
+++ ios/chrome/browser/home_customization/model/home_background_customization_service_factory.mm
@@ -47,7 +47,7 @@ HomeBackgroundCustomizationServiceFactory::BuildServiceInstanceFor(
       HomeBackgroundImageServiceFactory::GetForProfile(profile));
 }
 
-void HomeBackgroundCustomizationServiceFactory::RegisterBrowserStatePrefs(
+void HomeBackgroundCustomizationServiceFactory::RegisterProfilePrefs(
     user_prefs::PrefRegistrySyncable* registry) {
   HomeBackgroundCustomizationService::RegisterProfilePrefs(registry);
 }
