9dd5cb4937b2c69088921a10ca0f34b2b39dcf7b
dljames <dljames@chromium.org>
2025-09-03 13:38:28 -0700

    [Vertical Tabs] Remove BrowserView::tabstrip from FrameView ChromeOS
    Removes usages of BrowserView::tabstrip from
    BrowserNonClientFrameViewChromeOS. To provide the same behavior we can
    now use the GetDescendantByElementId function on Views to target the
    element we want to retrieve. In this case the TabStrip
    (vertical or horizontal).
    
    Change-Id: Ibbb82ccde268c730e926e10834ae3035372f5988
    Bug: 436873217
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6909759
    Commit-Queue: Darryl James <dljames@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1510513}

--- chrome/browser/ui/views/frame/browser_non_client_frame_view_chromeos.cc
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_chromeos.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ash/session/session_util.h"
 #include "chrome/browser/ui/ash/system_web_apps/system_web_app_ui_utils.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_features.h"
 #include "chrome/browser/ui/color/chrome_color_id.h"
 #include "chrome/browser/ui/exclusive_access/exclusive_access_manager.h"
@@ -297,7 +298,7 @@ gfx::Rect BrowserNonClientFrameViewChromeOS::GetBoundsForWebAppFrameToolbar(
   const int available_width = caption_button_container_->x() - x;
   int painted_height = GetTopInset(false);
   if (browser_view()->GetTabStripVisible()) {
-    painted_height += browser_view()->tabstrip()->GetPreferredSize().height();
+    painted_height += browser_view()->GetTabStripHeight();
   }
   return gfx::Rect(x, 0, std::max(0, available_width), painted_height);
 }
@@ -437,7 +438,11 @@ int BrowserNonClientFrameViewChromeOS::NonClientHitTest(
     // should reside in the TabStrip class.
     gfx::Point client_point(point);
     View::ConvertPointToTarget(this, frame()->client_view(), &client_point);
-    gfx::Rect tabstrip_shadow_bounds(browser_view()->tabstrip()->bounds());
+    gfx::Rect tabstrip_shadow_bounds(
+        browser_view()
+            ->tab_strip_view()
+            ->GetViewByElementId(kTabStripElementId)
+            ->bounds());
     constexpr int kTabShadowHeight = 4;
     tabstrip_shadow_bounds.set_height(kTabShadowHeight);
     if (tabstrip_shadow_bounds.Contains(client_point)) {
@@ -510,7 +515,7 @@ void BrowserNonClientFrameViewChromeOS::Layout(PassKey) {
 
   int painted_height = GetTopInset(false);
   if (browser_view()->GetTabStripVisible()) {
-    painted_height += browser_view()->tabstrip()->GetPreferredSize().height();
+    painted_height += browser_view()->GetTabStripHeight();
   }
 
   if (frame_header_) {
