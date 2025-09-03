4c4b2466451e3e49645f43171f348bc713c80599
Luke Warlow <lwarlow@igalia.com>
2025-09-03 14:08:39 -0700

    Export wpt tests for preference override API
    Previously these were in the wpt_internal folder but these should be
    exported to WPT, now that this API is in the mediaqueries-5 draft.
    
    Bug: 40947206
    Change-Id: I4431c335ce53fdb0eb406e8c984eb38747d2584f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6909535
    Commit-Queue: Luke <lwarlow@igalia.com>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1510533}

--- third_party/blink/web_tests/external/wpt/css/mediaqueries/resources/preferences-helper.js
+++ third_party/blink/web_tests/external/wpt/css/mediaqueries/resources/preferences-helper.js
@@ -1,11 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-window.changeEventPromise = function changeEventPromise(preference) {
+window.changeEventPromise = function changeEventPromise(preference, t) {
     return Promise.race([
         new Promise(resolve => {
             navigator.preferences[preference].onchange = resolve;
         }),
         new Promise((resolve, reject) => {
-            setTimeout(() => {
+            t.step_timeout(() => {
                 reject(`Change event for ${preference} preference not fired.`);
             }, 500);
         })
