91bae9e3ec0373a32ae0ec9aa671f95d5621cdb3
Muhammad Salmaan <musalmaan@google.com>
2025-09-03 12:12:02 -0700

    Fix installer downloader get active web contents
    Fixed installer downloader for getting active web contents by adding a
    check to make sure that there is an active tab in the browser.
    
    Bug: 431281518
    Change-Id: I55b395dcb543bcae6c6a1276078e62e1e24aff23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6871044
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Commit-Queue: Muhammad Salmaan <musalmaan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1510461}

--- chrome/browser/win/installer_downloader/installer_downloader_controller.cc
+++ chrome/browser/win/installer_downloader/installer_downloader_controller.cc
@@ -39,25 +39,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/download_manager.h"
 #include "content/public/browser/web_contents.h"
+#include "ui/base/base_window.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "url/gurl.h"
 
 namespace installer_downloader {
 
 namespace {
-
-content::WebContents* GetActiveWebContents() {
-  for (BrowserWindowInterface* browser : GetAllBrowserWindowInterfaces()) {
-    if (!browser->IsActive() ||
-        browser->GetType() != BrowserWindowInterface::Type::TYPE_NORMAL) {
-      continue;
-    }
-
-    return CHECK_DEREF(browser->GetActiveTabInterface()).GetContents();
-  }
-
-  return nullptr;
-}
 
 std::optional<GURL> BuildInstallerDownloadUrl(bool is_metrics_enabled) {
   std::string installer_url_template = kInstallerUrlTemplateParam.Get();
@@ -93,8 +81,9 @@ InstallerDownloaderController::InstallerDownloaderController(
       show_infobar_callback_(std::move(show_infobar_callback)),
       model_(std::make_unique<InstallerDownloaderModelImpl>(
           std::make_unique<SystemInfoProviderImpl>())),
-      get_active_web_contents_callback_(
-          base::BindRepeating(&GetActiveWebContents)),
+      get_active_web_contents_callback_(base::BindRepeating(
+          &InstallerDownloaderController::GetActiveWebContents,
+          base::Unretained(this))),
       should_show_infobar_for_profile_callback_(base::BindRepeating(
           &InstallerDownloaderController::ShouldShowInfobarForCurrentProfile,
           base::Unretained(this))) {
@@ -108,8 +97,9 @@ InstallerDownloaderController::InstallerDownloaderController(
     : is_metrics_enabled_callback_(std::move(is_metrics_enabled_callback)),
       show_infobar_callback_(std::move(show_infobar_callback)),
       model_(std::move(model)),
-      get_active_web_contents_callback_(
-          base::BindRepeating(&GetActiveWebContents)),
+      get_active_web_contents_callback_(base::BindRepeating(
+          &InstallerDownloaderController::GetActiveWebContents,
+          base::Unretained(this))),
       should_show_infobar_for_profile_callback_(base::BindRepeating(
           &InstallerDownloaderController::ShouldShowInfobarForCurrentProfile,
           base::Unretained(this))) {
@@ -126,6 +116,25 @@ void InstallerDownloaderController::RegisterBrowserWindowEvents() {
       window_tracker_.RegisterRemovedWindowCallback(base::BindRepeating(
           &InstallerDownloaderController::OnRemovedBrowserWindow,
           base::Unretained(this)));
+}
+
+content::WebContents* InstallerDownloaderController::GetActiveWebContents() {
+  BrowserWindowInterface* last_active_window =
+      window_tracker_.get_last_active_window();
+  if (!last_active_window) {
+    return nullptr;
+  }
+
+  tabs::TabInterface* active_tab = last_active_window->GetActiveTabInterface();
+  if (!active_tab) {
+    return nullptr;
+  }
+
+  content::WebContents* web_contents = active_tab->GetContents();
+  if (!web_contents || web_contents->IsBeingDestroyed()) {
+    return nullptr;
+  }
+  return web_contents;
 }
 
 InstallerDownloaderController::~InstallerDownloaderController() = default;
