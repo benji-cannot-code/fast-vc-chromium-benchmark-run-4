0d9ece67c6a2eb84d7e4af0ca135206ffe9e68a2
Nico Weber <thakis@chromium.org>
2025-09-03 13:16:13 -0700

    Roll libc++ from 23b5bc93867b to 2b317cc190e9 (8 revisions)
    Manually updated def files. Manually edited tab_group_range to
    add an operator== to an iterator there.
    
    (This now is needed because the #154703 change adds
    
        if (__first == __last)
          return;
    
    to include/__tree:1026. See also try results for patch set 2.)
    
    https://chromium.googlesource.com/external/github.com/llvm/llvm-project/libcxx.git/+log/23b5bc93867b..2b317cc190e9
    
    2025-09-03 hghristov.rmm@gmail.com [libc++][ranges] LWG4083: `views::as_rvalue` should reject non-input ranges (#155156)
    2025-09-03 nikolasklauser@berlin.de [libc++] Refactor remaining __find_equal calls (#156594)
    2025-09-03 nikolasklauser@berlin.de [libc++][C++03] Backport #111127, #112843 and #121620 (#155571)
    2025-09-03 nikolasklauser@berlin.de [libc++] Add thread safety annotations for std::lock (#154078)
    2025-09-03 nikolasklauser@berlin.de [libc++] Simplify std::function implementation further (#145153)
    2025-09-03 nikolasklauser@berlin.de [libc++] Refactor __tree::__find_equal to not have an out parameter (#147345)
    2025-09-03 nikolasklauser@berlin.de [libc++] Optimize {map,set}::insert(InputIterator, InputIterator) (#154703)
    2025-09-02 gutenev@gmail.com [libcxx][test] Avoid warnings about unused variables and typedefs if `_LIBCPP_VERSION` is not defined (#155679)
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/libcxx-chromium
    Please CC hans@chromium.org,jwata@google.com,thakis@chromium.org on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in Chromium: https://bugs.chromium.org/p/chromium/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Change-Id: I8a1cde4e97f7e1fce28ce5e31cb41a40a914a62a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6911750
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Devon Loehr <dloehr@google.com>
    Owners-Override: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1510499}

--- ios/chrome/browser/shared/model/web_state_list/tab_group_range.h
+++ ios/chrome/browser/shared/model/web_state_list/tab_group_range.h
@@ -91,6 +91,9 @@ class TabGroupRange {
 
     int operator*() const { return current_; }
     void operator++() { ++current_; }
+    bool operator==(const iterator& other) const {
+      return current_ == other.current_;
+    }
     bool operator!=(const iterator& other) const {
       return current_ != other.current_;
     }
