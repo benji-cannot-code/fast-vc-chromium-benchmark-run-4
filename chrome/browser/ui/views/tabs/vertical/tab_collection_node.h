ddd829f52902bd329cef093777bb314264822122
David Pennington <dpenning@chromium.org>
2025-09-03 14:41:57 -0700

    [Vertical Tabs] Add GetDirectChildren for CollectionNode
    this function is used by children to get the views in the correct
    order for layout.
    
    Change-Id: I13fe2a5cdea425e7c826bccd8d5b408a610e188b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6908543
    Auto-Submit: David Pennington <dpenning@chromium.org>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1510548}

--- chrome/browser/ui/views/tabs/vertical/tab_collection_node.h
+++ chrome/browser/ui/views/tabs/vertical/tab_collection_node.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_VIEWS_TABS_VERTICAL_TAB_COLLECTION_NODE_H_
 
 #include <memory>
+#include <vector>
 
 #include "base/functional/callback_forward.h"
 #include "base/memory/raw_ptr.h"
@@ -41,6 +42,7 @@ class TabCollectionNode {
 
   const tabs_api::mojom::DataPtr& data() const { return data_; }
   const Children& children() const { return children_; }
+  std::vector<views::View*> GetDirectChildren() const;
 
   Type GetType() const { return data_->which(); }
 
