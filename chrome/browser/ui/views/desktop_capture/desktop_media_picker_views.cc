afb9e1e51cf1ecf1ea9d59a7fef50638d984d18f
Mitsuru Oshima <oshima@chromium.org>
2025-09-02 19:13:03 -0700

    Rename native_window_types.h to native_ui_types.h
    in //ui/views, //c/b/ui/views
    
    We decided to rename to native_ui_types.h instead. Otherwise, this is
    same as original CLs that removed to native_window_types.h
    
    AX-Relnotes: n/a.
    Bug: 440644556
    Change-Id: I25d83ece287fd3f83f8d288efc00b778e78a3a25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6909760
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1509992}

--- chrome/browser/ui/views/desktop_capture/desktop_media_picker_views.cc
+++ chrome/browser/ui/views/desktop_capture/desktop_media_picker_views.cc
@@ -59,7 +59,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/keycodes/keyboard_codes.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/geometry/insets.h"
-#include "ui/gfx/native_window_types.h"
+#include "ui/gfx/native_ui_types.h"
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/views/background.h"
 #include "ui/views/bubble/bubble_frame_view.h"
--- ui/views/examples/colored_dialog_example.cc
+++ ui/views/examples/colored_dialog_example.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/mojom/dialog_button.mojom.h"
 #include "ui/base/mojom/ui_base_types.mojom-shared.h"
 #include "ui/base/ui_base_types.h"
-#include "ui/gfx/native_window_types.h"
+#include "ui/gfx/native_ui_types.h"
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/gfx/vector_icon_types.h"
 #include "ui/native_theme/native_theme.h"
