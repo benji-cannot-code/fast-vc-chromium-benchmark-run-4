30e0d6e2675e01ce6abdce914567872b49a1313e
Qiu Jianlin <jianlin.qiu@intel.com>
2025-09-02 18:53:43 -0700

    [DX12 Enc] Notify shared image encode support via VideoEncoderInfo.
    GetSupportedProfiles may contain both profile entries that supports or
    do not support shared image encode, especially when on Windows we have
    two VEAs. So make sure the active VEA updates VideoEncoderInfo before VEA adapter finishes initialization.
    
    Bug: 441011637
    Change-Id: Iff103d9d442daa41c3f898361187af112585854e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6897205
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Jianlin Qiu <jianlin.qiu@intel.com>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1509985}

--- media/gpu/windows/d3d12_video_encode_accelerator.h
+++ media/gpu/windows/d3d12_video_encode_accelerator.h
@@ -95,7 +95,7 @@ class MEDIA_GPU_EXPORT D3D12VideoEncodeAccelerator
   size_t GetSharedHandleCacheSizeForTesting() const;
 
  private:
-  void InitializeTask(const Config& config);
+  void InitializeTask(const Config& config, const SupportedProfiles& profile);
 
   void UseOutputBitstreamBufferTask(BitstreamBuffer buffer);
 
