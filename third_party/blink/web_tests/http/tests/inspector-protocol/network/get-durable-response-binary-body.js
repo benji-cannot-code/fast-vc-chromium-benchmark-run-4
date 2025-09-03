53d74bf57850185b0f3b445125ceddde0bdfa0a7
Alex N. Jose <alexnj@chromium.org>
2025-09-03 08:58:23 -0700

    Deflake multi-chunk response collection test
    Current implementation isn't waiting until all chunks have been
    received, causing a flake. This CL attempts to deflake the test by
    waiting for loading to finish, before requesting body collection.
    
    Bug: 442384994
    Change-Id: I621712185ec1f6cf484a5ceeb159077b348c53a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6910422
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Auto-Submit: Alex N. Jose <alexnj@chromium.org>
    Commit-Queue: Alex N. Jose <alexnj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1510283}

--- third_party/blink/web_tests/http/tests/inspector-protocol/network/get-durable-response-binary-body.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/network/get-durable-response-binary-body.js
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   dp.Runtime.evaluate({expression: `fetch('${url}').then(r => r.text())`});
 
   const requestWillBeSent = (await dp.Network.onceRequestWillBeSent()).params;
-  await dp.Network.onceResponseReceived();
+  await dp.Network.onceLoadingFinished();
   const {result} = await dp.Network.getResponseBody({requestId: requestWillBeSent.requestId});
 
   testRunner.log(`Body is base64 encoded: ${result.base64Encoded}`);
