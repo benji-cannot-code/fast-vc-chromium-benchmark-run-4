e6e574f4aa53b3e9df9fbf0fcfe833892e237d1b
Brandon Wylie <wylieb@google.com>
2025-09-03 15:20:14 -0700

    Disable `testPasswordSheetDisplaysProvidedItems` in `PasswordAccessoryIntegrationTest`.
    Bug: 442895429
    Change-Id: I2fe2ef9aeec2641d68d5944725b593f93653b180
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6913638
    Auto-Submit: Brandon Wylie <wylieb@google.com>
    Owners-Override: Brandon Wylie <wylieb@google.com>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1510572}

--- chrome/android/features/keyboard_accessory/javatests/src/org/chromium/chrome/browser/keyboard_accessory/sheet_tabs/PasswordAccessoryIntegrationTest.java
+++ chrome/android/features/keyboard_accessory/javatests/src/org/chromium/chrome/browser/keyboard_accessory/sheet_tabs/PasswordAccessoryIntegrationTest.java
@@ -95,6 +95,7 @@ public class PasswordAccessoryIntegrationTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/442895429")
     public void testPasswordSheetDisplaysProvidedItems() throws TimeoutException {
         preparePasswordBridge();
         ThreadUtils.runOnUiThreadBlocking(
