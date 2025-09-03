38a02f39dd4b88b21da59f9d0739ef5a598e49d4
Ahmed Nasr <anasr@google.com>
2025-09-03 08:54:08 -0700

    Add BocaRequest class to be used for server requests.
    This will be used for boca receiver requests, and we can migrate
    other boca requests to it gradually to reduce code duplication.
    
    Bug: b:439661794
    Change-Id: I98214e20ed1ef7aa203d3931ac12e1195aa004a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6892061
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Reviewed-by: April Zhou <aprilzhou@google.com>
    Cr-Commit-Position: refs/heads/main@{#1510278}

---
+++ chromeos/ash/components/boca/util.cc
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/ash/components/boca/util.h"
+
+#include <string>
+
+#include "base/command_line.h"
+#include "base/version_info/channel.h"
+#include "chromeos/ash/components/channel/channel_info.h"
+
+namespace ash::boca {
+
+namespace {
+
+inline constexpr char kSchoolToolsApiBaseProdUrl[] =
+    "https://schooltools-pa.googleapis.com";
+inline constexpr char kSchoolToolsServerSwitch[] = "st-server";
+
+}  // namespace
+
+std::string GetSchoolToolsUrl() {
+  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
+  if (ash::GetChannel() != version_info::Channel::STABLE &&
+      ash::GetChannel() != version_info::Channel::BETA &&
+      command_line->HasSwitch(kSchoolToolsServerSwitch)) {
+    return command_line->GetSwitchValueASCII(kSchoolToolsServerSwitch);
+  }
+  return kSchoolToolsApiBaseProdUrl;
+}
+
+}  // namespace ash::boca
---
+++ chromeos/ash/components/boca/util.h
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_ASH_COMPONENTS_BOCA_UTIL_H_
+#define CHROMEOS_ASH_COMPONENTS_BOCA_UTIL_H_
+
+#include <string>
+
+namespace ash::boca {
+
+std::string GetSchoolToolsUrl();
+
+}
+
+#endif  // CHROMEOS_ASH_COMPONENTS_BOCA_UTIL_H_
