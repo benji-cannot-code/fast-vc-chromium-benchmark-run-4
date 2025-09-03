a738151db975acb9edb2fb10e615fc24fa1f570a
Kene Okoye <kenok@google.com>
2025-09-03 09:20:33 -0700

    Rename `actor_ui_state_manager_fake` to `actor_ui_state_manager`
    - To better reflect the replacement of ActorUiStateManagerFake in
       crrev.com/c/6846501.
    
    Change-Id: I15c404fc20f0867cd19d5de1a17418652e342806
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6907184
    Auto-Submit: Kene Okoye <kenok@google.com>
    Reviewed-by: Christine Ying <chrstne@google.com>
    Commit-Queue: Christine Ying <chrstne@google.com>
    Cr-Commit-Position: refs/heads/main@{#1510303}

--- chrome/browser/actor/ui/actor_ui_state_manager_unittest.cc
+++ chrome/browser/actor/ui/actor_ui_state_manager_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/actor/actor_keyed_service_factory.h"
 #include "chrome/browser/actor/actor_keyed_service_fake.h"
 #include "chrome/browser/actor/ui/actor_ui_tab_controller.h"
+#include "chrome/browser/actor/ui/actor_ui_state_manager_interface.h"
 #include "chrome/browser/actor/ui/mocks/mock_actor_ui_tab_controller.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
@@ -78,11 +79,6 @@ class ActorUiStateManagerTest : public testing::Test {
     auto actor_keyed_service = std::make_unique<ActorKeyedServiceFake>(profile);
     actor_keyed_service_fake_ = actor_keyed_service.get();
 
-    auto actor_ui_state_manager_fake =
-        std::make_unique<ActorUiStateManager>(*actor_keyed_service);
-    actor_ui_state_manager_fake_ = actor_ui_state_manager_fake.get();
-    actor_keyed_service->SetActorUiStateManagerForTesting(
-        std::move(actor_ui_state_manager_fake));
     return std::move(actor_keyed_service);
   }
 
@@ -105,8 +101,8 @@ class ActorUiStateManagerTest : public testing::Test {
             });
   }
 
-  ActorUiStateManager* actor_ui_state_manager() {
-    return actor_ui_state_manager_fake_;
+  ActorUiStateManagerInterface* actor_ui_state_manager() {
+    return actor_keyed_service_fake_->GetActorUiStateManager();
   }
 
   ActorKeyedServiceFake* actor_keyed_service() {
@@ -161,7 +157,6 @@ class ActorUiStateManagerTest : public testing::Test {
   base::test::ScopedFeatureList scoped_feature_list_;
   std::unique_ptr<TestingProfile> profile_;
   raw_ptr<ActorKeyedServiceFake> actor_keyed_service_fake_;
-  raw_ptr<ActorUiStateManager> actor_ui_state_manager_fake_;
   ::ui::UnownedUserDataHost user_data_host_;
   MockBrowserWindowInterface mock_browser_window_interface_;
   MockTabInterface mock_tab_;
