fa63048d04e04574435b3e44757e238213765cde
Hiroki Nakagawa <nhiroki@chromium.org>
2025-09-02 18:02:23 -0700

    Prerender: Remove enable-speculation-rules-prerendering-target-hint from chrome://flags
    This feature has been enabled by default:
    https://chromestatus.com/feature/5162540351094784
    
    Bug: 40234240
    Change-Id: I739f15951201d2f7274240b3684d94cb509dad0f
    Fixed: 394280911
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6910077
    Reviewed-by: Huanpo Lin <robertlin@chromium.org>
    Auto-Submit: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1509973}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1972,9 +1972,6 @@ extern const char kAnnotatedPageContentsForVirtualStructureDescription[];
 extern const char kPowerBookmarkBackendName[];
 extern const char kPowerBookmarkBackendDescription[];
 
-extern const char kSpeculationRulesPrerenderingTargetHintName[];
-extern const char kSpeculationRulesPrerenderingTargetHintDescription[];
-
 extern const char kSubframeProcessReuseThresholds[];
 extern const char kSubframeProcessReuseThresholdsDescription[];
 
