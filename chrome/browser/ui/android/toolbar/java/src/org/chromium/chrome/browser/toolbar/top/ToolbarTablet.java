8523036916303f0def656dd3be7281c32eb1a05d
Charles Hager <clhager@google.com>
2025-09-03 11:10:17 -0700

    Fix optional button showing incorrectly
    If the optional button is explicitly hidden using the
    hideOptionalButton() method, it should not be allowed to consume from
    the toolbar width to show again.
    
    Bug: 427260734
    Change-Id: Ide78b43417ac79e1e702618828a5abd1d7b0aec9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6896624
    Commit-Queue: Charles Hager <clhager@google.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1510408}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarTablet.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarTablet.java
@@ -83,6 +83,7 @@ public class ToolbarTablet extends ToolbarLayout {
 
     private boolean mIsInTabSwitcherMode;
     private boolean mToolbarButtonsVisible;
+    private boolean mOptionalButtonForciblyHidden;
     private @Nullable ImageButton mOptionalButton;
     private boolean mOptionalButtonUsesTint;
 
@@ -558,12 +559,14 @@ public class ToolbarTablet extends ToolbarLayout {
                 mOptionalButton.getPaddingBottom());
 
         mOptionalButton.setContentDescription(buttonSpec.getContentDescription());
+        mOptionalButtonForciblyHidden = false;
         setOptionalButtonVisibility(/* isVisible= */ true);
         mOptionalButton.setEnabled(buttonData.isEnabled());
     }
 
     @Override
     protected void hideOptionalButton() {
+        mOptionalButtonForciblyHidden = true;
         setOptionalButtonVisibility(/* isVisible= */ false);
     }
 
@@ -576,6 +579,10 @@ public class ToolbarTablet extends ToolbarLayout {
         @Override
         public int updateVisibility(int availableWidth) {
             assert isToolbarTabletResizeRefactorEnabled();
+            if (mOptionalButtonForciblyHidden) {
+                setOptionalButtonVisibility(false);
+                return 0;
+            }
 
             int width = getResources().getDimensionPixelSize(R.dimen.toolbar_button_width);
             if (availableWidth >= width) {
