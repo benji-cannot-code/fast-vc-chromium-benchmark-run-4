043c15456f2a33c1f5fe3aa83ece3d31183d747a
Nina Satragno <nsatragno@chromium.org>
2025-09-03 08:52:59 -0700

    [webauthn] Wire `create_new_vault` mode to enclave
    Add a feature flag that controls using the new `create_new_vault` when
    refreshing a PIN with the enclave, and a regression test to verify that
    the change actually works.
    
    This adds some features to our fake implementation of Keychain to
    support multiple cohorts, needed for the tests.
    
    Bug: 399818721
    Change-Id: I71cc3586ca5f530a13818789b560d9010830efe0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6896661
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1510276}

--- device/fido/features.cc
+++ device/fido/features.cc
@@ -198,4 +198,9 @@ BASE_FEATURE(kWebAuthnWrapCohortData,
 BASE_FEATURE(AuthenticatorPasswordsOnlyImmediateRequests,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+// Not yet enabled by default.
+BASE_FEATURE(kWebAuthnNewRefreshFlow,
+             "WebAuthenticationNewRefreshFlow",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 }  // namespace device
