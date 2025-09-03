664d284807a8c56dac6172d46248419d251b68ac
Minseong Kim <jja08111@gmail.com>
2025-09-03 01:39:04 -0700

    [tab-size] Fallback font if space glyph doesn't exists
    This CL fixes a font fallback logic by ensuring the font contains the
    space glyph when computing tab-size.
    
    Bug: 442623894
    Change-Id: Ia7863084f9b0f83477e7efcdcefaa2c9b9864a53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6903719
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Minseong Kim <jja08111@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1510092}

--- third_party/blink/renderer/platform/fonts/font_fallback_list.h
+++ third_party/blink/renderer/platform/fonts/font_fallback_list.h
@@ -113,6 +113,17 @@ class PLATFORM_EXPORT FontFallbackList
     return cached_primary_simple_font_data_with_cjk_water_;
   }
 
+  const SimpleFontData* PrimarySimpleFontDataForTabSize(
+      const FontDescription& font_description) {
+    if (!cached_primary_simple_font_data_for_tab_size_) {
+      cached_primary_simple_font_data_for_tab_size_ =
+          DeterminePrimarySimpleFontData(font_description, uchar::kSpace,
+                                         /*should_contain_glyph=*/true);
+      DCHECK(cached_primary_simple_font_data_for_tab_size_);
+    }
+    return cached_primary_simple_font_data_for_tab_size_;
+  }
+
   const FontData* FontDataAt(const FontDescription&, unsigned index);
 
   base::span<const FontFeatureRange> GetFontFeatures(const FontDescription&);
@@ -138,10 +149,12 @@ class PLATFORM_EXPORT FontFallbackList
 
   const SimpleFontData* DeterminePrimarySimpleFontData(
       const FontDescription&,
-      UChar32 lookup_character = uchar::kSpace);
+      UChar32 lookup_character = uchar::kSpace,
+      bool should_contain_glyph = false);
   const SimpleFontData* DeterminePrimarySimpleFontDataCore(
       const FontDescription&,
-      UChar32 lookup_character = uchar::kSpace);
+      UChar32 lookup_character = uchar::kSpace,
+      bool should_contain_glyph = false);
 
   void ComputeFontFeatures(const FontDescription&);
   bool ComputeCanShapeWordByWord(const FontDescription&);
@@ -150,6 +163,7 @@ class PLATFORM_EXPORT FontFallbackList
   Member<const SimpleFontData> cached_primary_simple_font_data_with_space_;
   Member<const SimpleFontData> cached_primary_simple_font_data_with_digit_zero_;
   Member<const SimpleFontData> cached_primary_simple_font_data_with_cjk_water_;
+  Member<const SimpleFontData> cached_primary_simple_font_data_for_tab_size_;
   const Member<FontSelector> font_selector_;
   int family_index_ = 0;
   const uint16_t generation_;
