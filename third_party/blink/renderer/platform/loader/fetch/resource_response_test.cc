0f5e7e1054a9fc8247caec5057ed4f5cc61d2286
Fredrik Söderquist <fs@opera.com>
2025-09-03 16:04:51 -0700

    Remove "ParseDateUsesBaseTimeFromUtcString" flag
    Shipped since M139 with no known issues filed against it.
    
    This reverts most of commit 12fd5bd9fa99495f82e5275c41b20e10ad3d35a0
    which added the UseCounters, and marks the use counters obsolete.
    
    The now unused ParseDateFromNullTerminatedCharacters() will be removed
    in a follow-up CL.
    
    Bug: 375343405
    Change-Id: I0f971bfd3e5af06ca118ff35b3028225a4f3190b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6909875
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1510590}

--- third_party/blink/renderer/platform/loader/fetch/resource_response_test.cc
+++ third_party/blink/renderer/platform/loader/fetch/resource_response_test.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
 #include "third_party/blink/renderer/platform/network/http_names.h"
 #include "third_party/blink/renderer/platform/scheduler/public/non_main_thread.h"
 #include "third_party/blink/renderer/platform/scheduler/public/post_cross_thread_task.h"
@@ -35,14 +34,6 @@ ResourceResponse CreateTestResponse() {
                               AtomicString("attachment; filename=a.txt"));
   return response;
 }
-
-class FakeUseCounter : public GarbageCollected<FakeUseCounter>,
-                       public UseCounter {
- private:
-  void CountUse(mojom::WebFeature feature) override {}
-  void CountDeprecation(mojom::WebFeature feature) override {}
-  void CountWebDXFeature(WebDXFeature feature) override {}
-};
 
 }  // namespace
 
@@ -84,8 +75,7 @@ TEST(ResourceResponseTest, TreatExpiresZeroAsExpired) {
 
   response.SetHttpHeaderField(http_names::kExpires, AtomicString("0"));
 
-  std::optional<base::Time> expires =
-      response.Expires(*MakeGarbageCollected<FakeUseCounter>());
+  std::optional<base::Time> expires = response.Expires();
   EXPECT_EQ(base::Time::Min(), expires);
 
   base::Time creation_time = base::Time::UnixEpoch();
