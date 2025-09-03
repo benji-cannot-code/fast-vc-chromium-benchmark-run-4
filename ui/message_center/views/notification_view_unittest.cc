7f980cfa75b75633f5099a5e6b9238dd72130814
Peter Kasting <pkasting@chromium.org>
2025-09-03 10:16:12 -0700

    Miscellaneous small cleanups
    * Trivial temp removals/additions in prep for future reworking of
      NativeTheme::ShouldUseDarkColors()
    * common_theme.cc no longer exists
    * Remove test boilerplate
    * Method overrides should match superclass access control
    * Other test methods can be private
    
    Bug: 431953872
    Change-Id: Id1704b352f15ca5bb3d9972191fca9d3bf7e8142
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6905789
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1510358}

--- ui/message_center/views/notification_view_unittest.cc
+++ ui/message_center/views/notification_view_unittest.cc
@@ -97,11 +97,6 @@ class NotificationViewTest : public views::ViewObserver,
                              public message_center::MessageCenterObserver,
                              public views::InkDropObserver {
  public:
-  NotificationViewTest() = default;
-  NotificationViewTest(const NotificationViewTest&) = delete;
-  NotificationViewTest& operator=(const NotificationViewTest&) = delete;
-  ~NotificationViewTest() override = default;
-
   // views::ViewsTestBase:
   void SetUp() override {
     views::ViewsTestBase::SetUp();
@@ -124,6 +119,32 @@ class NotificationViewTest : public views::ViewObserver,
     views::ViewsTestBase::TearDown();
   }
 
+  // views::ViewObserver:
+  void OnViewPreferredSizeChanged(views::View* observed_view) override {
+    EXPECT_EQ(observed_view, notification_view());
+    notification_view_->GetWidget()->SetSize(
+        notification_view()->GetPreferredSize({}));
+  }
+
+  void OnNotificationRemoved(const std::string& notification_id,
+                             bool by_user) override {
+    if (delete_on_notification_removed_) {
+      views::InkDrop::Get(notification_view_)
+          ->SetMode(views::InkDropHost::InkDropMode::OFF);
+      notification_view_.ExtractAsDangling()->GetWidget()->CloseNow();
+      return;
+    }
+  }
+
+  // views::InkDropObserver:
+  void InkDropAnimationStarted() override {}
+
+  void InkDropRippleAnimationEnded(
+      views::InkDropState ink_drop_state) override {
+    ink_drop_stopped_ = true;
+  }
+
+ protected:
   std::unique_ptr<Notification> CreateSimpleNotification() const {
     RichNotificationData data;
     data.settings_button_handler = SettingsButtonHandler::INLINE;
@@ -221,7 +242,6 @@ class NotificationViewTest : public views::ViewObserver,
     notification_view_->ToggleInlineSettings(ui::test::TestEvent());
   }
 
- protected:
   NotificationView* notification_view() { return notification_view_; }
   NotificationHeaderView* header_row() {
     return notification_view_->header_row();
@@ -252,31 +272,6 @@ class NotificationViewTest : public views::ViewObserver,
   scoped_refptr<NotificationTestDelegate> delegate_;
 
  private:
-  // views::ViewObserver:
-  void OnViewPreferredSizeChanged(views::View* observed_view) override {
-    EXPECT_EQ(observed_view, notification_view());
-    notification_view_->GetWidget()->SetSize(
-        notification_view()->GetPreferredSize({}));
-  }
-
-  void OnNotificationRemoved(const std::string& notification_id,
-                             bool by_user) override {
-    if (delete_on_notification_removed_) {
-      views::InkDrop::Get(notification_view_)
-          ->SetMode(views::InkDropHost::InkDropMode::OFF);
-      notification_view_.ExtractAsDangling()->GetWidget()->CloseNow();
-      return;
-    }
-  }
-
-  // views::InkDropObserver:
-  void InkDropAnimationStarted() override {}
-
-  void InkDropRippleAnimationEnded(
-      views::InkDropState ink_drop_state) override {
-    ink_drop_stopped_ = true;
-  }
-
   raw_ptr<NotificationView> notification_view_ = nullptr;
   bool delete_on_notification_removed_ = false;
   bool ink_drop_stopped_ = false;
