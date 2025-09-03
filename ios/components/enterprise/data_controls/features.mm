d12457e6a0b80b2983b4310fc03777fd445f2adb
Ernesto Izquierdo Clua <eic@google.com>
2025-09-03 07:05:19 -0700

    [ios] Move Data Controls features to ios/components
    The implementation of Clipboard Data Controls requires changes in
    ios/web/. This CL moves the existing feature flag to ios/components to
    make it available to ios/web/ and ios/chrome/.
    
    Bug: 346533036
    Change-Id: I75678eac1b3a4a45870178867482962c2524ac61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6907180
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1510197}

--- ios/components/enterprise/data_controls/features.mm
+++ ios/components/enterprise/data_controls/features.mm
@@ -3,9 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/enterprise/data_controls/features.h"
+#import "ios/components/enterprise/data_controls/features.h"
 
 namespace data_controls {
 
 BASE_FEATURE(EnableClipboardDataControlsIOS, base::FEATURE_DISABLED_BY_DEFAULT);
+
 }
