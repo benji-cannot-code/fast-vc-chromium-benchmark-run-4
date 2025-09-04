603d44c29f517aabb390f2a39c8623d3eeead176
Trevor Perrier <perrier@chromium.org>
2025-09-04 10:16:15 -0700

    [Glic] Don't close if detaching
    A SidePanelEntryObserver was added in https://crrev.com/c/6886520 to
    close the Glic panel when the side panel is closed.  This meant that
    when detaching we were closing the panel and could not show the panel
    in the floating window.
    
    This CL prevents the panel from closing if it is currently detaching.
    
    Bug: b:442839727
    Change-Id: Ib41dee2f505a79fb639b2b53e50addecdbf6aef5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6914405
    Reviewed-by: Basia Zimirska <basiaz@google.com>
    Commit-Queue: Trevor Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1510982}

--- chrome/browser/glic/widget/glic_window_controller_impl.cc
+++ chrome/browser/glic/widget/glic_window_controller_impl.cc
@@ -1125,7 +1125,7 @@ bool GlicWindowControllerImpl::ActivateBrowser() {
 }
 
 void GlicWindowControllerImpl::Close() {
-  if (state_ == State::kClosed) {
+  if (state_ == State::kClosed || state_ == State::kDetaching) {
     return;
   }
   window_config_.SetLastCloseTime();
