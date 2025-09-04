2684442187997c00e5ee63a409911f0922c14650
kylechar <kylechar@chromium.org>
2025-09-04 10:29:47 -0700

    Remove bind_generates_resoure from GPU service
    The feature is no longer used and is being deleted.
    
    Bug: 429382005
    Change-Id: I2338810472270827abbd275f264b7078ad765e35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6876120
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1510984}

--- gpu/command_buffer/service/context_group_unittest.cc
+++ gpu/command_buffer/service/context_group_unittest.cc
@@ -37,8 +37,6 @@ namespace gles2 {
 
 class ContextGroupTest : public GpuServiceTest {
  public:
-  static const bool kBindGeneratesResource = false;
-
   ContextGroupTest() : discardable_manager_(gpu_preferences_) {}
 
  protected:
@@ -51,8 +49,7 @@ class ContextGroupTest : public GpuServiceTest {
         gpu_preferences_, /*memory_tracker=*/nullptr,
         /*shader_translator_cache=*/nullptr,
         /*framebuffer_completeness_cache=*/nullptr, feature_info,
-        kBindGeneratesResource, /*progress_reporter=*/nullptr, GpuFeatureInfo(),
-        &discardable_manager_,
+        /*progress_reporter=*/nullptr, GpuFeatureInfo(), &discardable_manager_,
         /*passthrough_discardable_manager=*/nullptr, &shared_image_manager_));
   }
 
@@ -84,9 +81,9 @@ TEST_F(ContextGroupTest, Basic) {
 }
 
 TEST_F(ContextGroupTest, InitializeNoExtensions) {
-  TestHelper::SetupContextGroupInitExpectations(
-      gl_.get(), DisallowedFeatures(), "ANGLE", "OpenGL ES 2.0",
-      CONTEXT_TYPE_OPENGLES2, kBindGeneratesResource);
+  TestHelper::SetupContextGroupInitExpectations(gl_.get(), DisallowedFeatures(),
+                                                "ANGLE", "OpenGL ES 2.0",
+                                                CONTEXT_TYPE_OPENGLES2);
   group_->Initialize(decoder_.get(), CONTEXT_TYPE_OPENGLES2,
                      DisallowedFeatures());
   EXPECT_EQ(static_cast<uint32_t>(TestHelper::kNumVertexAttribs),
@@ -123,9 +120,9 @@ TEST_F(ContextGroupTest, MultipleContexts) {
   TraceOutputter outputter;
   std::unique_ptr<MockGLES2Decoder> decoder2_(
       new MockGLES2Decoder(&client2, &command_buffer_service2, &outputter));
-  TestHelper::SetupContextGroupInitExpectations(
-      gl_.get(), DisallowedFeatures(), "ANGLE", "OpenGL ES 2.0",
-      CONTEXT_TYPE_OPENGLES2, kBindGeneratesResource);
+  TestHelper::SetupContextGroupInitExpectations(gl_.get(), DisallowedFeatures(),
+                                                "ANGLE", "OpenGL ES 2.0",
+                                                CONTEXT_TYPE_OPENGLES2);
   EXPECT_EQ(group_->Initialize(decoder_.get(), CONTEXT_TYPE_OPENGLES2,
                                DisallowedFeatures()),
             gpu::ContextResult::kSuccess);
