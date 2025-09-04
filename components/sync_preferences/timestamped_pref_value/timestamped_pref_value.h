4e77159e0eccb00c206a5c95abc830da400b2fb8
Jenna Himawan <jhimawan@google.com>
2025-09-04 08:51:41 -0700

    Refactor TimestampedPrefValue to its own directory and file
    Bug: 442902926
    Change-Id: I2378bdceeaa35222b64b55490b79177d528e7bd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6912478
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1510942}

---
+++ components/sync_preferences/timestamped_pref_value/timestamped_pref_value.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_SYNC_PREFERENCES_TIMESTAMPED_PREF_VALUE_TIMESTAMPED_PREF_VALUE_H_
+#define COMPONENTS_SYNC_PREFERENCES_TIMESTAMPED_PREF_VALUE_TIMESTAMPED_PREF_VALUE_H_
+
+#include "base/time/time.h"
+#include "base/values.h"
+
+namespace sync_preferences {
+
+// Holds a Pref's `value` and the time of its last observed change.
+//
+// The timestamp is synced across devices. For a Pref that was not previously
+// synced, the timestamp is taken from the first recorded change after the user
+// started syncing Prefs. If no change has been recorded, the timestamp will be
+// unset.
+struct TimestampedPrefValue {
+  base::Value value;
+  base::Time last_observed_change_time;
+};
+
+}  // namespace sync_preferences
+
+#endif  // COMPONENTS_SYNC_PREFERENCES_TIMESTAMPED_PREF_VALUE_TIMESTAMPED_PREF_VALUE_H_
