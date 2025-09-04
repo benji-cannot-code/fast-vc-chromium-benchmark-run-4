08d18bd614670e626846c571fee30d6dcdb09cf1
Jan Keitel <jkeitel@google.com>
2025-09-04 03:59:05 -0700

    Pass StrikeDatabaseBase to AutofillAiManager
    StrikeDatabaseBase is the interface and StrikeDatabase is the
    implementation. We should be passing pointers to the interface
    where that is all that is needed.
    
    Bug: 40100455
    Change-Id: I30c2923d0199e34db2cf653d00aa4fe2b25feabd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6904571
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1510821}

--- chrome/browser/ui/autofill/chrome_autofill_client.cc
+++ chrome/browser/ui/autofill/chrome_autofill_client.cc
@@ -125,6 +125,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/base/signin_metrics.h"
 #include "components/signin/public/identity_manager/account_info.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
+#include "components/strike_database/strike_database.h"
 #include "components/sync/service/sync_service.h"
 #include "components/translate/core/browser/translate_manager.h"
 #include "components/unified_consent/pref_names.h"
