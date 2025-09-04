726a0f61a80693621a9e494bc554cef989021d7c
Maria Kazinova <kazinova@google.com>
2025-09-04 14:08:28 -0700

    Record triggering clientside models in autofill-internals.
    I'd like to have this log to confirm that manual raters are reaching
    this stage in their testing. We have log coverage in other logs and
    other moments, but not here exactly.
    
    Change-Id: I5f8ff10583b68c38591bfeff85d3b683bf6a40cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6912876
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1511149}

--- components/autofill/core/common/autofill_internals/log_message.h
+++ components/autofill/core/common/autofill_internals/log_message.h
@@ -64,7 +64,9 @@ class LogBuffer;
     "field: ")                                                                 \
   T(FastCheckout, "Fast Checkout: ")                                           \
   T(TouchToFill, "Touch To Fill: ")                                            \
-  T(AutofillAi, "Autofill with AI: ")
+  T(AutofillAi, "Autofill with AI: ")                                          \
+  T(TriggeringClientsideModelFor,                                              \
+    "Triggered clientside classification model for : ")
 
 // Log messages for chrome://autofill-internals.
 
