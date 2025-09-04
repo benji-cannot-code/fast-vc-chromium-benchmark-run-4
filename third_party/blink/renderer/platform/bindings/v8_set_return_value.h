ecb2b60a94152c801da7687741827252abddfab7
Andrey Kosyakov <caseq@chromium.org>
2025-09-04 13:50:26 -0700

    Remove unused V8SetReturnValue() overloads for WebString
    Change-Id: I89d2c39c15c7b00ec375829105dd2bee59140c61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6913849
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Auto-Submit: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511128}

--- third_party/blink/renderer/platform/bindings/v8_set_return_value.h
+++ third_party/blink/renderer/platform/bindings/v8_set_return_value.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 #include <type_traits>
 
-#include "third_party/blink/public/platform/web_string.h"
 #include "third_party/blink/renderer/platform/bindings/dom_data_store.h"
 #include "third_party/blink/renderer/platform/bindings/dom_wrapper_world.h"
 #include "third_party/blink/renderer/platform/bindings/enumeration_base.h"
@@ -260,20 +259,6 @@ void V8SetReturnValue(const CallbackInfo& info,
 
 template <FunctionCallbackInfoOrPropertyCallbackInfo CallbackInfo>
 void V8SetReturnValue(const CallbackInfo& info,
-                      const WebString& string,
-                      v8::Isolate* isolate,
-                      V8ReturnValue::NonNullable) {
-  if (string.IsEmpty()) {
-    info.GetReturnValue().SetEmptyString();
-    return;
-  }
-  DCHECK(!string.IsNull());  // Null strings are empty.
-  V8PerIsolateData::From(isolate)->GetStringCache()->SetReturnValueFromString(
-      info.GetReturnValue(), static_cast<String>(string).Impl());
-}
-
-template <FunctionCallbackInfoOrPropertyCallbackInfo CallbackInfo>
-void V8SetReturnValue(const CallbackInfo& info,
                       const AtomicString& string,
                       v8::Isolate* isolate,
                       V8ReturnValue::Nullable) {
@@ -302,22 +287,6 @@ void V8SetReturnValue(const CallbackInfo& info,
   }
   V8PerIsolateData::From(isolate)->GetStringCache()->SetReturnValueFromString(
       info.GetReturnValue(), string.Impl());
-}
-
-template <FunctionCallbackInfoOrPropertyCallbackInfo CallbackInfo>
-void V8SetReturnValue(const CallbackInfo& info,
-                      const WebString& string,
-                      v8::Isolate* isolate,
-                      V8ReturnValue::Nullable) {
-  if (string.IsNull()) {
-    info.GetReturnValue().SetNull();
-    return;
-  } else if (string.IsEmpty()) {
-    info.GetReturnValue().SetEmptyString();
-    return;
-  }
-  V8PerIsolateData::From(isolate)->GetStringCache()->SetReturnValueFromString(
-      info.GetReturnValue(), static_cast<String>(string).Impl());
 }
 
 // ScriptWrappable
