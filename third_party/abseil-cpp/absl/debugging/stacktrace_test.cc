72d5ed6fafabad147b2c9b39c09615481dae8408
Mirko Bonadei <mbonadei@chromium.org>
2025-09-04 08:55:55 -0700

    Roll abseil_revision 5ad0bfb7ab..d5ccaa212c
    Change Log:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+log/5ad0bfb7ab..d5ccaa212c
    Full diff:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+/5ad0bfb7ab..d5ccaa212c
    
    Bug: None
    Change-Id: Ib48732fdfdd7150fa2119b93abf5b7ccefd3892b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6907203
    Commit-Queue: Mirko Bonadei <mbonadei@chromium.org>
    Commit-Queue: Danil Chapovalov <danilchap@chromium.org>
    Reviewed-by: Danil Chapovalov <danilchap@chromium.org>
    Auto-Submit: Mirko Bonadei <mbonadei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1510944}

--- third_party/abseil-cpp/absl/debugging/stacktrace_test.cc
+++ third_party/abseil-cpp/absl/debugging/stacktrace_test.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gtest/gtest.h"
 #include "absl/base/attributes.h"
 #include "absl/base/config.h"
+#include "absl/base/internal/errno_saver.h"
 #include "absl/base/optimization.h"
 #include "absl/types/span.h"
 
@@ -415,6 +416,7 @@ const void* g_return_address = nullptr;
 bool g_sigusr2_raised = false;
 
 void SigUsr2Handler(int, siginfo_t*, void* uc) {
+  absl::base_internal::ErrnoSaver errno_saver;
   // Many platforms don't support this by default.
   bool support_is_expected = false;
   constexpr int kMaxStackDepth = 64;
