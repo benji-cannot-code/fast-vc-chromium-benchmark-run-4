cff02b70ab4abd85f7852d6407b6a232be55d7e5
Patrick Noland <pnoland@google.com>
2025-09-04 13:57:15 -0700

    Reset active gesture count on tab change or crash
    The active gesture count suppresses captures until the gesture ends.
    This value can become invalid if we don't receive a balanced number of
    acks for gesture begins and ends. Two ways that can happen are changing
    tabs between acks or crashes; regardless, it's safe to reset the value
    in both these cases.
    
    Bug: 442636157, 434751393
    Change-Id: Idb6be4ed8b53bdc2e2f594db01813c2e13c52801
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6917456
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511136}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/CompositorViewHolder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/CompositorViewHolder.java
@@ -319,6 +319,11 @@ public class CompositorViewHolder extends FrameLayout
                 }
 
                 @Override
+                public void onCrash(Tab tab) {
+                    mNumGestureActiveTouches = 0;
+                }
+
+                @Override
                 public void onDidFinishNavigationInPrimaryMainFrame(
                         Tab tab, NavigationHandle navigation) {
                     if (!navigation.isSameDocument() && navigation.hasCommitted()) {
@@ -338,7 +343,7 @@ public class CompositorViewHolder extends FrameLayout
 
                 @Override
                 public void onGestureEnd() {
-                    mNumGestureActiveTouches--;
+                    mNumGestureActiveTouches = Math.max(mNumGestureActiveTouches - 1, 0);
                     updateInMotion();
                 }
             };
@@ -1542,6 +1547,7 @@ public class CompositorViewHolder extends FrameLayout
             mHasKeyboardGeometryChangeFired = false;
             if (mTabVisible != null) mTabVisible.removeObserver(mTabObserver);
             if (tab != null) {
+                mNumGestureActiveTouches = 0;
                 tab.addObserver(mTabObserver);
                 mCompositorView.onTabChanged();
             }
