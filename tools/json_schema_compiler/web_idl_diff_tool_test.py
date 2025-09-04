d72a91a9305fda7d724a7838a89c9a5f81dc6cf3
Tim Judkins <tjudkins@chromium.org>
2025-09-04 10:39:41 -0700

    [Extensions WebIDL] Convert cecPrivate to WebIDL
    This conversion was largely done using a detailed description of the
    conversion process passed to Gemini CLI. To double check this work, the
    file has also been copied into the converted schemas test to verify no
    functional difference in output.
    
    Note: A small correction was made to the original IDL file to add a
    colon after the `|callback|` in two comments. This was likely never
    noticed previously because this is a private API and we don't actually
    generate docs for it that anyone looks at.
    
    Bug: 436624834
    Change-Id: Ic6b2e5d9879b41160da00cda3d1ebdcb56233fa3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6912190
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1510990}

--- tools/json_schema_compiler/web_idl_diff_tool_test.py
+++ tools/json_schema_compiler/web_idl_diff_tool_test.py
@@ -20,6 +20,7 @@ class WebIdlDiffToolTest(unittest.TestCase):
         ('alarms.idl', 'alarms.webidl'),
         ('bluetooth.idl', 'bluetooth.webidl'),
         ('audio.idl', 'audio.webidl'),
+        ('cec_private.idl', 'cec_private.webidl'),
     ]
     # LoadAndReturnUnifiedDiff expects file paths relative to the repo root.
     converted_schema_path = 'tools/json_schema_compiler/test/converted_schemas/'
