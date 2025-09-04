159c80b5788ffadf0902cb34656a84d8c8c2ac76
Andrey Kosyakov <caseq@chromium.org>
2025-09-04 16:56:06 -0700

    Revert "Rename display::Screen::GetScreen() to display::Screen::Get()"
    This reverts commit 5cf40a29688a48f98df8e5e86d28ce4b213d7765.
    
    Reason for revert: broke waterfall builds: https://ci.chromium.org/ui/p/chromium/builders/ci/linux-archive-rel/157483/overview
    
    Original change's description:
    > Rename display::Screen::GetScreen() to display::Screen::Get()
    >
    > This is the last change and removes GetScreen() as well.
    >
    > Fixed: 439672783
    > Change-Id: Ie747ec3a3f5eeb63192d9e27550da86045409abd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6860363
    > Reviewed-by: Dana Fried <dfried@chromium.org>
    > Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    > Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    > Reviewed-by: Reilly Grant <reillyg@chromium.org>
    > Reviewed-by: Rick Byers <rbyers@chromium.org>
    > Reviewed-by: David Song <wintermelons@google.com>
    > Owners-Override: Rick Byers <rbyers@chromium.org>
    > Reviewed-by: Shu Yang <shuyng@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1511205}
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ia803e411ffe085eb6c6279c719c057fafb055c69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6917491
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Owners-Override: Wanda Mora <morawand@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Wanda Mora <morawand@google.com>
    Commit-Queue: Ben Pastene <bpastene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511217}

--- ui/display/screen.cc
+++ ui/display/screen.cc
@@ -33,6 +33,11 @@ Screen::Screen() : display_id_for_new_windows_(kInvalidDisplayId) {}
 Screen::~Screen() = default;
 
 // static
+Screen* Screen::GetScreen() {
+  return g_screen;
+}
+
+// static
 Screen* Screen::Get() {
   return g_screen;
 }
@@ -253,7 +258,7 @@ ScopedNativeScreen::ScopedNativeScreen(const base::Location& location) {
 
 ScopedNativeScreen::~ScopedNativeScreen() {
   if (screen_) {
-    DCHECK_EQ(screen_.get(), Screen::Get());
+    DCHECK_EQ(screen_.get(), Screen::GetScreen());
     Screen::SetScreenInstance(nullptr);
     screen_.reset();
   }
