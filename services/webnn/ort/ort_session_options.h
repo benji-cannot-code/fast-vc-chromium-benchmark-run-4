7e5c018141e380b65eccfb80fa40c8ba567d0536
mingmingtasd <mingming1.xu@intel.com>
2025-09-03 19:22:17 -0700

    WebNN: Implement EP selection policy delegate for ORT
    This CL implements the EP selection policy delegate which allows
    providing custom `OrtEpDevice` selection logic. This delegate is
    called during session creation to select OrtEpDevice instances from
    the sorted list of available devices.
    
    With the custom EP selection delegate, now the GPU device will be
    selected when users request a NPU but NPU EP or device is not
    available.
    
    Bug: 437178792
    Change-Id: Ib7e49c40ce505efd00a1363b2852dbd79d13d50b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6831830
    Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Commit-Queue: Mingming1 Xu <mingming1.xu@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1510664}

--- services/webnn/ort/ort_session_options.h
+++ services/webnn/ort/ort_session_options.h
@@ -26,7 +26,8 @@ class SessionOptions final : public base::RefCountedThreadSafe<SessionOptions> {
                                               scoped_refptr<Environment> env);
 
   SessionOptions(base::PassKey<SessionOptions>,
-                 ScopedOrtSessionOptions session_options);
+                 ScopedOrtSessionOptions session_options,
+                 mojom::Device device_type);
 
   SessionOptions(const SessionOptions&) = delete;
   SessionOptions& operator=(const SessionOptions&) = delete;
@@ -39,6 +40,7 @@ class SessionOptions final : public base::RefCountedThreadSafe<SessionOptions> {
   ~SessionOptions();
 
   ScopedOrtSessionOptions session_options_;
+  const mojom::Device device_type_;
 };
 
 }  // namespace webnn::ort
