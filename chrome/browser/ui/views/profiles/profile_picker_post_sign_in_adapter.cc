7596804245205f4ea3a878e8bd540279f229249d
anthie@google.com <anthie@google.com>
2025-09-04 11:07:43 -0700

    Show managed account screen before history sync screen and fetch policies
    This change applies to the profile picker flows. Subsequent changes
    will enforce this and follow a different path for ensuring management
    when a browser already exists.
    
    This change adds a `PolicyHelper` class which, for users that
    are subject to `IsManaged` account property, registers for
    policies to determine if the user is managed and for managed
    user fetches the applicable policies.
    
    Managed users are then shown the account management screen.
    Accepting this screen, marks the management as approved, and
    proceeds to the history sync optin screen. If the user rejects
    management we finalize the flow (follow up CLs to specify if the
    user remains signed in or not).
    
    The relevant tests are also added for the history sync optin
    helper.
    
    Demo: https://screencast.googleplex.com/cast/NjI0Mjk2Nzc3MjcyNTI0OHxhMDk2OWNlOC1iMA
    
    Bug: 434964019
    Change-Id: I793a12b79cd0055491ba569054a7f194f7dafa2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6895417
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Anthi Orfanou <anthie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1511016}

--- chrome/browser/ui/views/profiles/profile_picker_post_sign_in_adapter.cc
+++ chrome/browser/ui/views/profiles/profile_picker_post_sign_in_adapter.cc
@@ -108,6 +108,10 @@ void ProfilePickerPostSignInAdapter::Init(
       << "A profile with a valid account must be passed in.";
   email_ = account_info.email;
 
+  on_sync_screen_closed_closure_ =
+      base::BindOnce(&ProfilePickerPostSignInAdapter::FinishAndOpenBrowser,
+                     weak_ptr_factory_.GetWeakPtr(), PostHostClearedCallback());
+
   if (base::FeatureList::IsEnabled(switches::kEnableHistorySyncOptin)) {
     history_sync_optin_helper_ = std::make_unique<HistorySyncOptinHelper>(
         identity_manager, profile_, account_info, /*delegate=*/this);
@@ -115,10 +119,6 @@ void ProfilePickerPostSignInAdapter::Init(
     history_sync_optin_helper_->StartHistorySyncOptinFlow();
     return;
   }
-
-  base::OnceClosure sync_consent_completed_closure =
-      base::BindOnce(&ProfilePickerPostSignInAdapter::FinishAndOpenBrowser,
-                     weak_ptr_factory_.GetWeakPtr(), PostHostClearedCallback());
 
   // TurnSyncOnHelper deletes itself once done.
   new TurnSyncOnHelper(
@@ -128,7 +128,7 @@ void ProfilePickerPostSignInAdapter::Init(
       TurnSyncOnHelper::SigninAbortedMode::KEEP_ACCOUNT,
       std::make_unique<ProfilePickerTurnSyncOnDelegate>(
           weak_ptr_factory_.GetWeakPtr(), profile_),
-      std::move(sync_consent_completed_closure));
+      std::move(on_sync_screen_closed_closure_));
 }
 
 void ProfilePickerPostSignInAdapter::ShowHistorySyncOptinScreen() {
@@ -142,6 +142,18 @@ void ProfilePickerPostSignInAdapter::ShowHistorySyncOptinScreen() {
           // Unretained is enough as the callback is
           // called by the owner of this instance.
           base::Unretained(this)));
+}
+
+void ProfilePickerPostSignInAdapter::ShowAccountManagementScreen(
+    signin::SigninChoiceCallback on_account_management_screen_closed) {
+  SwitchToManagedUserProfileNotice(
+      ManagedUserProfileNoticeUI::ScreenType::kEnterpriseAccountCreation,
+      std::move(on_account_management_screen_closed));
+}
+
+void ProfilePickerPostSignInAdapter::FinishFlowWithoutHistorySyncOptin() {
+  CHECK(!on_sync_screen_closed_closure_.is_null());
+  std::move(on_sync_screen_closed_closure_).Run();
 }
 
 void ProfilePickerPostSignInAdapter::Cancel() {}
@@ -270,12 +282,9 @@ void ProfilePickerPostSignInAdapter::SwitchToHistorySyncOptinFinished() {
   // Initialize the WebUI page once we know it's committed.
   HistorySyncOptinUI* history_sync_optin_ui =
       static_cast<HistorySyncOptinUI*>(contents()->GetWebUI()->GetController());
-
+  CHECK(!on_sync_screen_closed_closure_.is_null());
   history_sync_optin_ui->Initialize(
-      /*browser=*/nullptr,
-      base::BindOnce(&ProfilePickerPostSignInAdapter::FinishAndOpenBrowser,
-                     weak_ptr_factory_.GetWeakPtr(),
-                     PostHostClearedCallback()));
+      /*browser=*/nullptr, std::move(on_sync_screen_closed_closure_));
 }
 
 void ProfilePickerPostSignInAdapter::SwitchToManagedUserProfileNoticeFinished(
