dbabf809ddbc66ae6d9d655c93d738d31fca420f
Ahmed Moussa <ahmedmoussa@google.com>
2025-09-03 17:26:12 -0700

    Revert "[CVC ios] Add deletion dialog to support CVC deletion"
    This reverts commit d68d435bbc681a123c8c60f469c6eb1b477a5e01.
    
    Reason for revert: Causing test failures on bots https://ci.chromium.org/ui/p/chromium/builders/ci/ios-simulator-noncq/65913/test-results
    
    Bug: 441505362
    Original change's description:
    > [CVC ios] Add deletion dialog to support CVC deletion
    >
    > Screenshot: https://screenshot.googleplex.com/5qWjxy9dZxBR6YT
    > Mock: https://screenshot.googleplex.com/C2T25HCWhE2E9rg
    >
    > Bug: 441505362
    > Change-Id: I62a11d49cab82e8fae76aeff351195fc370671bb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6892295
    > Reviewed-by: Tommy Martino <tmartino@chromium.org>
    > Commit-Queue: Qihui Zhao <qihuizhao@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1510564}
    
    Bug: 441505362
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ib39ab8c83b2b90b5ce54849e585f376925c35b0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6913854
    Commit-Queue: Jiacheng Guo <gjc@google.com>
    Reviewed-by: Jiacheng Guo <gjc@google.com>
    Owners-Override: Jiacheng Guo <gjc@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1510620}

--- ios/chrome/browser/settings/ui_bundled/autofill/autofill_cvc_storage_view_mediator.mm
+++ ios/chrome/browser/settings/ui_bundled/autofill/autofill_cvc_storage_view_mediator.mm
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/check.h"
 #import "components/autofill/core/browser/data_manager/payments/payments_data_manager.h"
 #import "components/autofill/core/browser/data_manager/personal_data_manager.h"
-#import "components/autofill/core/browser/data_model/payments/credit_card.h"
 #import "components/autofill/core/common/autofill_prefs.h"
 #import "ios/chrome/browser/shared/model/prefs/pref_backed_boolean.h"
 #import "ios/chrome/browser/shared/model/utils/observable_boolean.h"
@@ -62,14 +61,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   _cvcStorageEnabled.value = isOn;
 }
 
-- (void)deleteAllSavedCvcsForViewController:
-    (AutofillCvcStorageViewController*)controller {
-  CHECK(_personalDataManager);
-  _personalDataManager->payments_data_manager().ClearLocalCvcs();
-  _personalDataManager->payments_data_manager().ClearServerCvcs();
-  self.consumer.hasSavedCvcs = NO;
-}
-
 #pragma mark - BooleanObserver
 
 - (void)booleanDidChange:(PrefBackedBoolean*)boolean {
@@ -81,11 +72,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (BOOL)hasSavedCvcs {
   CHECK(_personalDataManager);
-  const std::vector<const autofill::CreditCard*> cards =
-      _personalDataManager->payments_data_manager().GetCreditCards();
-  return std::ranges::any_of(cards, [&](const autofill::CreditCard* card) {
-    return !card->cvc().empty();
-  });
+  // TODO(crbug.com/40266992): This should check if there are any saved CVCs.
+  return true;
 }
 
 @end
