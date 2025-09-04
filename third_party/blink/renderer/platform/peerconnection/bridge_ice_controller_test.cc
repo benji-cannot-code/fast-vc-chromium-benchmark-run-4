008f54d1b3b9f39240b6665344151cf119c16314
Danil Chapovalov <danilchap@chromium.org>
2025-09-04 14:36:05 -0700

    Use default constructor for webrtc::MockIceController
    Single parameter constructor ignores that parameter anyway. This removes
    dependency on IceControllerFactoryArgs struct and allows WebRTC to
    modify it.
    
    Bug: webrtc:42223992
    Change-Id: Id0611a917efa7852fe0e6bbfb3150ff9f500d625
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6905237
    Auto-Submit: Danil Chapovalov <danilchap@chromium.org>
    Commit-Queue: Harald Alvestrand <hta@chromium.org>
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511170}

--- third_party/blink/renderer/platform/peerconnection/bridge_ice_controller_test.cc
+++ third_party/blink/renderer/platform/peerconnection/bridge_ice_controller_test.cc
@@ -46,7 +46,6 @@ using ::blink::IceSwitchProposal;
 using ::webrtc::Candidate;
 using ::webrtc::Connection;
 using ::webrtc::IceConfig;
-using ::webrtc::IceControllerFactoryArgs;
 using ::webrtc::IceControllerInterface;
 using ::webrtc::IceMode;
 using ::webrtc::IceRecheckEvent;
@@ -187,8 +186,7 @@ TEST_F(BridgeIceControllerTest, ObserverAttached) {
   MockIceAgent agent;
   MockIceControllerObserver observer1;
   MockIceControllerObserver observer2;
-  std::unique_ptr<MockIceController> will_move =
-      std::make_unique<MockIceController>(IceControllerFactoryArgs{});
+  auto will_move = std::make_unique<MockIceController>();
 
   scoped_refptr<IceInteractionInterface> interaction_agent = nullptr;
   EXPECT_CALL(observer1, OnObserverAttached).WillOnce(WithArgs<0>([&](auto ia) {
@@ -210,8 +208,7 @@ TEST_F(BridgeIceControllerTest, PassthroughIceControllerInterface) {
   MockIceAgent agent;
   MockIceControllerObserver observer1;
   MockIceControllerObserver observer2;
-  std::unique_ptr<MockIceController> will_move =
-      std::make_unique<MockIceController>(IceControllerFactoryArgs{});
+  auto will_move = std::make_unique<MockIceController>();
   MockIceController* wrapped = will_move.get();
 
   EXPECT_CALL(observer1, OnObserverAttached(_));
@@ -293,8 +290,7 @@ TEST_F(BridgeIceControllerTest, PassthroughIceControllerInterface) {
 TEST_F(BridgeIceControllerTest, HandlesImmediateSwitchRequest) {
   NiceMock<MockIceAgent> agent;
   MockIceControllerObserver observer;
-  std::unique_ptr<MockIceController> will_move =
-      std::make_unique<MockIceController>(IceControllerFactoryArgs{});
+  auto will_move = std::make_unique<MockIceController>();
   MockIceController* wrapped = will_move.get();
 
   scoped_refptr<IceInteractionInterface> interaction_agent = nullptr;
@@ -370,8 +366,7 @@ TEST_F(BridgeIceControllerTest, HandlesImmediateSwitchRequest) {
 TEST_P(BridgeIceControllerProposalTest, HandlesImmediateSortAndSwitchRequest) {
   NiceMock<MockIceAgent> agent;
   MockIceControllerObserver observer;
-  std::unique_ptr<MockIceController> will_move =
-      std::make_unique<MockIceController>(IceControllerFactoryArgs{});
+  auto will_move = std::make_unique<MockIceController>();
   MockIceController* wrapped = will_move.get();
 
   scoped_refptr<IceInteractionInterface> interaction_agent = nullptr;
@@ -480,8 +475,7 @@ TEST_P(BridgeIceControllerProposalTest, HandlesImmediateSortAndSwitchRequest) {
 TEST_P(BridgeIceControllerProposalTest, HandlesSortAndSwitchRequest) {
   NiceMock<MockIceAgent> agent;
   MockIceControllerObserver observer;
-  std::unique_ptr<MockIceController> will_move =
-      std::make_unique<MockIceController>(IceControllerFactoryArgs{});
+  auto will_move = std::make_unique<MockIceController>();
   MockIceController* wrapped = will_move.get();
 
   scoped_refptr<IceInteractionInterface> interaction_agent = nullptr;
@@ -557,8 +551,7 @@ TEST_P(BridgeIceControllerProposalTest, HandlesSortAndSwitchRequest) {
 TEST_P(BridgeIceControllerProposalTest, StartPingingAfterSortAndSwitch) {
   NiceMock<MockIceAgent> agent;
   MockIceControllerObserver observer;
-  std::unique_ptr<MockIceController> will_move =
-      std::make_unique<MockIceController>(IceControllerFactoryArgs{});
+  auto will_move = std::make_unique<MockIceController>();
   MockIceController* wrapped = will_move.get();
 
   scoped_refptr<IceInteractionInterface> interaction_agent = nullptr;
@@ -671,9 +664,7 @@ class BridgeIceControllerInvalidProposalTest : public BridgeIceControllerTest {
   BridgeIceControllerInvalidProposalTest()
       : recheck_event(IceSwitchReason::ICE_CONTROLLER_RECHECK,
                       recheck_delay_ms) {
-    std::unique_ptr<StrictMock<MockIceController>> will_move =
-        std::make_unique<StrictMock<MockIceController>>(
-            IceControllerFactoryArgs{});
+    auto will_move = std::make_unique<StrictMock<MockIceController>>();
     wrapped_controller = will_move.get();
 
     EXPECT_CALL(observer, OnObserverAttached(_))
@@ -822,8 +813,7 @@ TEST_F(BridgeIceControllerInvalidProposalTest, AcceptUnknownPruneProposal) {
 TEST_F(BridgeIceControllerTest, HandlesPingRequest) {
   NiceMock<MockIceAgent> agent;
   MockIceControllerObserver observer;
-  std::unique_ptr<MockIceController> will_move =
-      std::make_unique<MockIceController>(IceControllerFactoryArgs{});
+  auto will_move = std::make_unique<MockIceController>();
   MockIceController* wrapped = will_move.get();
 
   scoped_refptr<IceInteractionInterface> interaction_agent = nullptr;
@@ -856,8 +846,7 @@ TEST_F(BridgeIceControllerTest, HandlesPingRequest) {
 TEST_F(BridgeIceControllerTest, HandlesSwitchRequest) {
   NiceMock<MockIceAgent> agent;
   MockIceControllerObserver observer;
-  std::unique_ptr<MockIceController> will_move =
-      std::make_unique<MockIceController>(IceControllerFactoryArgs{});
+  auto will_move = std::make_unique<MockIceController>();
   MockIceController* wrapped = will_move.get();
 
   scoped_refptr<IceInteractionInterface> interaction_agent = nullptr;
@@ -892,8 +881,7 @@ TEST_F(BridgeIceControllerTest, HandlesSwitchRequest) {
 TEST_F(BridgeIceControllerTest, HandlesPruneRequest) {
   NiceMock<MockIceAgent> agent;
   MockIceControllerObserver observer;
-  std::unique_ptr<MockIceController> will_move =
-      std::make_unique<MockIceController>(IceControllerFactoryArgs{});
+  auto will_move = std::make_unique<MockIceController>();
   MockIceController* wrapped = will_move.get();
 
   scoped_refptr<IceInteractionInterface> interaction_agent = nullptr;
