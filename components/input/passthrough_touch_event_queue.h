e534d01d4ff64f82147abe3ff75e3c87d2dd076c
Kartar Singh <kartarsingh@google.com>
2025-09-04 10:57:07 -0700

    Reland "Send touch moves async immediately after scroll starts."
    This reverts commit b280a10d745d26ea9922a7ab49c74ecb83d13fd1.
    
    Reason for revert: Try relanding after instantiating SyntheticGestureController with `renderer_known_to_be_initialized` set to true.
    
    Bug: 433304196
    Original change's description:
    > Revert "Send touch moves async immediately after scroll starts."
    >
    > This reverts commit 783730fde55ea98720f2dce48402f6d925619f18.
    >
    > Reason for revert: suspect that an earlier CL (upon which this depends) is causing failures - https://ci.chromium.org/ui/p/chromium/builders/ci/linux-blink-msan-rel . Thanks to Matthew Warton for pointing this out.
    >
    > Bug: 433304196
    > Original change's description:
    > > Send touch moves async immediately after scroll starts.
    > >
    > > As per the current logic the touch moves are marked async only after
    > > the ack for a GestureScrollUpdate(GSU) comes back as `kConsumed`. In
    > > cases where a touch move comes in before the ack of GSU, it gets sent as
    > > blocking to Renderer as well delaying the generation of GSU.
    > >
    > > In this change we start sending the touch moves async immediately after
    > > the scroll has started. This doesn't break the web spec since the
    > > websites are only allowed to cancel the default behavior on touch start
    > > or first touch move.
    > > From https://www.w3.org/TR/touch-events/#the-touchmove-event: "If the
    > > preventDefault method is called on the first touchmove event of an
    > > active touch point, it should prevent any default action caused by any
    > > touchmove event associated with the same active touch point, such as
    > > scrolling."
    > >
    > > The behavior is guarded behind a finch feature flag to allow monitored
    > > rollout and study performance impacts.
    > >
    > > Bug: 433304196
    > > Change-Id: Ibb281ace48766461dff015f7d8f8f01bd7504ce2
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6874533
    > > Commit-Queue: Kartar Singh <kartarsingh@google.com>
    > > Reviewed-by: Jonathan Ross <jonross@chromium.org>
    > > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1509717}
    >
    > Bug: 433304196
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: Idc313c1560fcad1f4d8063ce4ed2d40fcf99e21e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6908034
    > Reviewed-by: Jonathan Ross <jonross@chromium.org>
    > Commit-Queue: Ian Vollick <vollick@chromium.org>
    > Owners-Override: Ian Vollick <vollick@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1509853}
    
    Bug: 433304196
    Change-Id: If243ae375ebbd472c7643e7fd1dbd1d76f0ef356
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6911185
    Commit-Queue: Kartar Singh <kartarsingh@google.com>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511005}

--- components/input/passthrough_touch_event_queue.h
+++ components/input/passthrough_touch_event_queue.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_INPUT_PASSTHROUGH_TOUCH_EVENT_QUEUE_H_
 #define COMPONENTS_INPUT_PASSTHROUGH_TOUCH_EVENT_QUEUE_H_
 
+#include <optional>
 #include <set>
 #include <string>
 
@@ -107,7 +108,7 @@ class COMPONENT_EXPORT(INPUT) PassthroughTouchEventQueue {
   void QueueEvent(const TouchEventWithLatencyInfo& event,
                   DispatchToRendererCallback& dispatch_callback);
 
-  void PrependTouchScrollNotification();
+  void PrependTouchScrollNotification(uint32_t primary_unique_touch_event_id);
 
   void ProcessTouchAck(blink::mojom::InputEventResultSource ack_source,
                        blink::mojom::InputEventResultState ack_result,
@@ -230,6 +231,9 @@ class COMPONENT_EXPORT(INPUT) PassthroughTouchEventQueue {
   PreFilterResult FilterBeforeForwardingImpl(const blink::WebTouchEvent& event);
   bool ShouldFilterForEvent(const blink::WebTouchEvent& event);
 
+  void SetAckStateForPendingTouchMovesFromSequence(
+      uint32_t primary_unique_touch_event_id);
+
   void AckTouchEventToClient(
       const TouchEventWithLatencyInfo& acked_event,
       blink::mojom::InputEventResultSource ack_source,
@@ -278,6 +282,8 @@ class COMPONENT_EXPORT(INPUT) PassthroughTouchEventQueue {
   std::set<TouchEventWithLatencyInfoAndAckState,
            TouchEventWithLatencyInfoAndAckStateComparator>
       outstanding_touches_;
+
+  std::optional<uint32_t> curr_sequence_down_event_id_ = std::nullopt;
 
   // Whether we should allow events to bypass normal queue filter rules.
   const bool skip_touch_filter_;
--- content/browser/renderer_host/render_widget_host_impl.h
+++ content/browser/renderer_host/render_widget_host_impl.h
@@ -1028,6 +1028,10 @@ class CONTENT_EXPORT RenderWidgetHostImpl
   // cpu-priority boosted to run discard logic.
   void SetIsDiscarding(bool is_discarding);
 
+  SyntheticGestureController* SyntheticGestureControllerForTesting() {
+    return synthetic_gesture_controller_.get();
+  }
+
  protected:
   // |routing_id| must not be IPC::mojom::kRoutingIdNone.
   // If this object outlives |delegate|, DetachDelegate() must be called when
@@ -1080,6 +1084,8 @@ class CONTENT_EXPORT RenderWidgetHostImpl
                            AddAndRemoveImeInputEventObserver);
   FRIEND_TEST_ALL_PREFIXES(RenderWidgetHostTest,
                            InputRouterReceivesHasTouchEventHandlers);
+  FRIEND_TEST_ALL_PREFIXES(OOPIFScrollBubblingTest,
+                           ScrollBubblingWithTouchMoveInjection);
   FRIEND_TEST_ALL_PREFIXES(RenderWidgetHostTest, EventDispatchPostDetach);
   FRIEND_TEST_ALL_PREFIXES(RenderWidgetHostTest, InputEventRWHLatencyComponent);
   FRIEND_TEST_ALL_PREFIXES(DevToolsAgentHostImplTest,
