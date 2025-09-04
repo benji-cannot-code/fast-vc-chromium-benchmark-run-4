ab1aeb25984ec9992ea7373ce2d97e88d0d144ff
Juan Mojica <juanmojica@google.com>
2025-09-04 16:02:12 -0700

    Add chrome://flag entry to enable contextual suggest UI improvements.
    Bug: 443126160
    Change-Id: I7134e08914caaa2eeac780df50c06e750bdfe0c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6916662
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1511209}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1772,6 +1772,9 @@ extern const char kContextualSearchOpenLensActionUsesThumbnailDescription[];
 extern const char kContextualSuggestionsAblateOthersWhenPresentName[];
 extern const char kContextualSuggestionsAblateOthersWhenPresentDescription[];
 
+extern const char kContextualSuggestionsUiImprovementsName[];
+extern const char kContextualSuggestionsUiImprovementsDescription[];
+
 extern const char kOmniboxContextualSearchOnFocusSuggestionsName[];
 extern const char kOmniboxContextualSearchOnFocusSuggestionsDescription[];
 
