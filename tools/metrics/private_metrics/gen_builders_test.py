aac89df1062b8d5060bf47772b5c1dac37574824
Kamil Jarosz <kjarosz@google.com>
2025-09-04 11:14:45 -0700

    pmc: Move tools/metrics/dwa/ to tools/metrics/private_metrics/
    DWA is a part of Private Metrics. Move DWA-related tools to a general
    Private-Metrics-related tools directory.
    
    In the future, tools for other types of private metrics will be added
    there.
    
    Bypass-Check-License: moved files
    Bug: 433728659
    Change-Id: I5a5e755c072ac005b117f8d11d975563a64647e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6899529
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Jay Zhou <zhouzj@google.com>
    Reviewed-by: Matt Dembski <dembski@google.com>
    Commit-Queue: Kamil Jarosz <kjarosz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1511027}

--- tools/metrics/private_metrics/gen_builders_test.py
+++ tools/metrics/private_metrics/gen_builders_test.py
@@ -360,5 +360,6 @@ std::map<uint64_t, EntryDecoder> CreateDecodeMap() {
 }  // namespace dwa
 """)
 
+
 if __name__ == '__main__':
   unittest.main()
--- tools/metrics/private_metrics/validate_format.py
+++ tools/metrics/private_metrics/validate_format.py
@@ -14,7 +14,7 @@ import path_util
 
 from xml.dom import minidom
 
-_DWA_XML = path_util.GetInputFile('tools/metrics/dwa/dwa.xml')
+_DWA_XML = path_util.GetInputFile('tools/metrics/private_metrics/dwa.xml')
 
 
 def main():
