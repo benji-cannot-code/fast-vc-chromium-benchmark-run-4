c0e6269e560f123f21fe7e7e42ea050b5d48bd3f
Minseop Choi <minsubb13@gmail.com>
2025-09-04 14:11:27 -0700

    fido/cable: migrate to new crypto/hash API
    Migrate fido_cable_handshake_handler.cc from crypto/sha2 API to new
    crypto/hash API.
    
    Bug: 374310081
    Change-Id: I59aa635c37b543e2792dcd98ee6ffe25f64f205d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6914275
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Minseop Choi <minsubb13@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1511152}

--- device/fido/cable/fido_cable_handshake_handler_unittest.cc
+++ device/fido/cable/fido_cable_handshake_handler_unittest.cc
@@ -27,9 +27,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_future.h"
 #include "components/cbor/reader.h"
 #include "components/cbor/values.h"
+#include "crypto/hash.h"
 #include "crypto/hkdf.h"
 #include "crypto/hmac.h"
-#include "crypto/sha2.h"
 #include "device/bluetooth/test/bluetooth_test.h"
 #include "device/bluetooth/test/mock_bluetooth_adapter.h"
 #include "device/fido/cable/fido_ble_frames.h"
@@ -133,7 +133,7 @@ std::array<uint8_t, 32> GetExpectedEncryptionKey(
   fido_parsing_utils::Append(&nonce_message, client_random_nonce);
   fido_parsing_utils::Append(&nonce_message, kAuthenticatorSessionRandom);
   return crypto::HkdfSha256<32>(kTestSessionPreKey,
-                                crypto::SHA256Hash(nonce_message),
+                                crypto::hash::Sha256(nonce_message),
                                 kCableDeviceEncryptionKeyInfo);
 }
 
