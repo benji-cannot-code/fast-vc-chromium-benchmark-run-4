efcd1e4cea33e9987bbbbeb59bd880460c5529f4
Gyuyoung Kim <gyuyoung@igalia.com>
2025-09-04 06:39:38 -0700

    [iOS Blink] Add missing color_scheme.mojom-shared.h include in layout_theme_ios.mm
    After https://crrev.com/c/6909611 removed unused header files,
    the iOS Blink port broke due to a missing dependency. This CL
    fixes the issue by including color_scheme.mojom-shared.h in
    layout_theme_ios.mm.
    
    Bug: 431953872
    Change-Id: If1b062c705369a9a5fff026c545ed695d01ffa0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6914235
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1510868}

--- third_party/blink/renderer/core/layout/layout_theme_ios.mm
+++ third_party/blink/renderer/core/layout/layout_theme_ios.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/layout/layout_theme_ios.h"
 
+#include "third_party/blink/public/mojom/frame/color_scheme.mojom-shared.h"
 #include "third_party/blink/renderer/platform/wtf/std_lib_extras.h"
 #include "ui/base/ui_base_features.h"
 
