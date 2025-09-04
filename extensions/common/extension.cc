be71fe02facd2142f272b0c345432fc38e08ade3
Xiaohan Wang <xhwang@chromium.org>
2025-09-04 13:56:05 -0700

    extensions: Migrate to new 2-param BASE_FEATURE macro
    This is part of the migration to the new 2-param BASE_FEATURE macro
    to fix code search issue and make naming more consistent:
    - Old: BASE_FEATURE(MyFeature, base::FEATURE_ENABLED_BY_DEFAULT);
    - New: BASE_FEATURE(kMyFeature, base::FEATURE_ENABLED_BY_DEFAULT);
    
    This CL removes the `TODO_BASE_FEATURE_MACROS_NEED_MIGRATION` define
    and update the files to use the new 2-param BASE_FEATURE macro format.
    
    See the bug for details about the migration plan.
    
    Enabled-by-default-reason: refactoring
    
    Bug: 436274260
    Change-Id: Ifc1ec626b6999b0e933e606e30086da460d97a14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6916859
    Auto-Submit: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511134}

--- extensions/common/extension.cc
+++ extensions/common/extension.cc
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#define TODO_BASE_FEATURE_MACROS_NEED_MIGRATION
-
 #include "extensions/common/extension.h"
 
 #include <stddef.h>
@@ -61,7 +59,7 @@ namespace errors = manifest_errors;
 
 namespace {
 
-BASE_FEATURE(ValidateGetResourceURLPath, base::FEATURE_ENABLED_BY_DEFAULT);
+BASE_FEATURE(kValidateGetResourceURLPath, base::FEATURE_ENABLED_BY_DEFAULT);
 
 constexpr int kMinimumSupportedManifestVersion = 2;
 constexpr int kMaximumSupportedManifestVersion = 3;
--- extensions/renderer/dispatcher.cc
+++ extensions/renderer/dispatcher.cc
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#define TODO_BASE_FEATURE_MACROS_NEED_MIGRATION
-
 #include "extensions/renderer/dispatcher.h"
 
 #include <stddef.h>
@@ -130,10 +128,10 @@ namespace extensions {
 namespace {
 
 // A feature flag for the crash issue in crbug.com/389971360.
-BASE_FEATURE(SpeculativeFixForServiceWorkerDataInDidStartServiceWorkerContext,
+BASE_FEATURE(kSpeculativeFixForServiceWorkerDataInDidStartServiceWorkerContext,
              base::FEATURE_ENABLED_BY_DEFAULT);
 // A feature flag for the crash issue in crbug.com/424476776.
-BASE_FEATURE(SpeculativeFixForNoExtensionInDidStartServiceWorkerContext,
+BASE_FEATURE(kSpeculativeFixForNoExtensionInDidStartServiceWorkerContext,
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 static const char kOnSuspendEvent[] = "runtime.onSuspend";
