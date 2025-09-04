27ee41e89ced4e018acb1131607206d8d118e082
Vidhan Jain <vidhanj@google.com>
2025-09-04 03:28:38 -0700

    Use BubbleManager to show mandatory reauth bubbles
    BubbleManager uses a priority based algorithm to decide which bubble
    has precedence to show if there are multiple bubbles waiting.
    With this CL, the mandatory reauth bubbles are made to use the bubble
    manager to show the bubble.
    
    Bug: 432429605
    Change-Id: I7d00b0e9f81e3a561013837a6bff7eaf1dba4008
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6870056
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1510812}

--- chrome/browser/ui/autofill/payments/mandatory_reauth_bubble_controller_impl.cc
+++ chrome/browser/ui/autofill/payments/mandatory_reauth_bubble_controller_impl.cc
@@ -50,7 +50,7 @@ void MandatoryReauthBubbleControllerImpl::SetupAndShowBubble(
     base::OnceClosure accept_mandatory_reauth_callback,
     base::OnceClosure cancel_mandatory_reauth_callback,
     base::RepeatingClosure close_mandatory_reauth_callback) {
-  if (bubble_view()) {
+  if (bubble_view() || !MaySetUpBubble()) {
     return;
   }
 
@@ -60,7 +60,8 @@ void MandatoryReauthBubbleControllerImpl::SetupAndShowBubble(
   autofill_metrics::LogMandatoryReauthOptInBubbleOffer(
       autofill_metrics::MandatoryReauthOptInBubbleOffer::kShown,
       /*is_reshow=*/false);
-  ShowBubble();
+
+  QueueOrShowBubble();
 }
 
 void MandatoryReauthBubbleControllerImpl::SetupBubble(
@@ -100,7 +101,7 @@ void MandatoryReauthBubbleControllerImpl::ReshowBubble() {
         autofill_metrics::MandatoryReauthOptInConfirmationBubbleMetric::kShown);
   }
 
-  ShowBubble();
+  QueueOrShowBubble(/*force_show=*/true);
 }
 
 std::u16string MandatoryReauthBubbleControllerImpl::GetWindowTitle() const {
