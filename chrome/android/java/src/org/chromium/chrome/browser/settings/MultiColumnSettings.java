5c06aa0f87082bcc18152e3d2863280c54eabc54
Hidehiko Abe <hidehiko@chromium.org>
2025-09-04 00:07:11 -0700

    Initial implementation of multi-column mode.
    This CL introduces MultiColumnSettings, which implements multi-column mode
    of the settings page, specifically for the wider window.
    
    This is the first step, so there are many KIs. They will be addressed
    separately. Major KIs:
    - Back button is not handled as expected yet.
    - Title of the detailed pane is not yet supported.
    - Single <-> Multi column mode switch is done at the default value
      of the library.
    - Very large window (1200dp+) size is not yet supported.
    
    BUG=404074032
    TEST=Tryjob
    
    Binary-Size: Size increase is for the new library inclusion.
    Change-Id: I26837218aecb4005b96b7189ee8f15b455a4990e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6911408
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1510736}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/MultiColumnSettings.java
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.settings;
+
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceHeaderFragmentCompat;
+
+import org.chromium.build.annotations.NullMarked;
+
+/** Preference container implementation for SettingsActivity in multi-column mode. */
+@NullMarked
+public class MultiColumnSettings extends PreferenceHeaderFragmentCompat {
+    @Override
+    public PreferenceFragmentCompat onCreatePreferenceHeader() {
+        // Main menu, which is the first page in one column mode (i.e. window is
+        // small enough), or shown at left side pane in two column mode.
+        return new MainSettings();
+    }
+}
