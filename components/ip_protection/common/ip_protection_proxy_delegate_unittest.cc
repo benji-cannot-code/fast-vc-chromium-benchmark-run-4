4e58a0e76454f604cee4b88b6ee3fe86fc802fbb
clarej <clarej@google.com>
2025-09-04 08:44:01 -0700

    Fix feature parameter name kIpPrivacyEnableIppPanelInDevTools
    Rename kIpPrivacyEnableIppPanelInDevTools feature name to align with
    similar flags.
    
    Bug: 442636135
    Change-Id: I4fc4033d6e2ff0d1bff3872f26e4e4b38e050f93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6913725
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Aaron Selya <selya@google.com>
    Commit-Queue: Clare Jin <clarej@google.com>
    Cr-Commit-Position: refs/heads/main@{#1510935}

--- components/ip_protection/common/ip_protection_proxy_delegate_unittest.cc
+++ components/ip_protection/common/ip_protection_proxy_delegate_unittest.cc
@@ -1890,7 +1890,7 @@ TEST_F(IpProtectionProxyDelegateTest, OnResolveProxyBypassesWhenSet) {
   scoped_feature_list.InitWithFeaturesAndParameters(
       {{net::features::kEnableIpProtectionProxy,
         {{"IpPrivacyEnableUserBypass", "true"},
-         {"kIpPrivacyEnableIppPanelInDevTools", "true"}}}},
+         {"IpPrivacyEnableIppPanelInDevTools", "true"}}}},
       {});
 
   std::map<std::string, std::set<std::string>> first_party_map;
