4cf55c15b0a98fef88b2daeaeece35b9f2440d4d
Matt Mueller <mattm@chromium.org>
2025-09-04 11:12:32 -0700

    Make testing DomainReliability mojo methods async
    Speculative fix for flaky test timeouts in
    DomainReliabilityBrowserTest.RequestAtShutdown
    DomainReliabilityBrowserTest.Upload
    DomainReliabilityBrowserTest.UploadAtShutdown
    
    Sync mojo methods were responsible for similar flaky browser test
    timeouts in other tests, so try switching these mojo methods to async to
    see if it fixes these tests.
    
    Bug: 440694414,440371016
    Change-Id: Ib21bd74028d3107f971f410a03d98dd3af5318bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6912474
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511024}

--- chrome/browser/domain_reliability/browsertest.cc
+++ chrome/browser/domain_reliability/browsertest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/json/json_reader.h"
 #include "base/run_loop.h"
+#include "base/test/test_future.h"
 #include "base/test/values_test_util.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/domain_reliability/service_factory.h"
@@ -25,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/storage_partition.h"
 #include "content/public/test/browser_test.h"
-#include "mojo/public/cpp/bindings/sync_call_restrictions.h"
 #include "net/base/net_errors.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "net/test/embedded_test_server/http_request.h"
@@ -223,9 +223,10 @@ IN_PROC_BROWSER_TEST_F(DomainReliabilityBrowserTest, Upload) {
   GURL upload_url = test_server()->GetURL(kUploadPath);
 
   {
-    mojo::ScopedAllowSyncCallForTesting allow_sync_call;
+    base::test::TestFuture<void> future;
     GetNetworkContext()->AddDomainReliabilityContextForTesting(  // IN-TEST
-        test_server()->GetOrigin(), upload_url);
+        test_server()->GetOrigin(), upload_url, future.GetCallback());
+    EXPECT_TRUE(future.Wait());
   }
 
   // Trigger an error.
@@ -233,8 +234,10 @@ IN_PROC_BROWSER_TEST_F(DomainReliabilityBrowserTest, Upload) {
   ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), error_url));
 
   {
-    mojo::ScopedAllowSyncCallForTesting allow_sync_call;
-    GetNetworkContext()->ForceDomainReliabilityUploadsForTesting();
+    base::test::TestFuture<void> future;
+    GetNetworkContext()->ForceDomainReliabilityUploadsForTesting(
+        future.GetCallback());
+    EXPECT_TRUE(future.Wait());
   }
 
   run_loop.Run();
@@ -263,17 +266,21 @@ IN_PROC_BROWSER_TEST_F(DomainReliabilityBrowserTest, UploadAtShutdown) {
 
   GURL upload_url = test_server()->GetURL("/hung");
   {
-    mojo::ScopedAllowSyncCallForTesting allow_sync_call;
+    base::test::TestFuture<void> future;
     GetNetworkContext()->AddDomainReliabilityContextForTesting(  // IN-TEST
-        url::Origin::Create(GURL("https://localhost/")), upload_url);
+        url::Origin::Create(GURL("https://localhost/")), upload_url,
+        future.GetCallback());
+    EXPECT_TRUE(future.Wait());
   }
 
   ASSERT_TRUE(
       ui_test_utils::NavigateToURL(browser(), GURL("https://localhost/")));
 
   {
-    mojo::ScopedAllowSyncCallForTesting allow_sync_call;
-    GetNetworkContext()->ForceDomainReliabilityUploadsForTesting();
+    base::test::TestFuture<void> future;
+    GetNetworkContext()->ForceDomainReliabilityUploadsForTesting(
+        future.GetCallback());
+    EXPECT_TRUE(future.Wait());
   }
 
   // At this point, there is an upload pending. If everything goes well, the
@@ -289,9 +296,10 @@ IN_PROC_BROWSER_TEST_F(DomainReliabilityBrowserTest, RequestAtShutdown) {
 
   GURL hung_url = test_server()->GetURL("/hung");
   {
-    mojo::ScopedAllowSyncCallForTesting allow_sync_call;
+    base::test::TestFuture<void> future;
     GetNetworkContext()->AddDomainReliabilityContextForTesting(  // IN-TEST
-        url::Origin::Create(hung_url), hung_url);
+        url::Origin::Create(hung_url), hung_url, future.GetCallback());
+    EXPECT_TRUE(future.Wait());
   }
 
   // Use a SimpleURLLoader so we can leak the mojo pipe, ensuring that URLLoader
