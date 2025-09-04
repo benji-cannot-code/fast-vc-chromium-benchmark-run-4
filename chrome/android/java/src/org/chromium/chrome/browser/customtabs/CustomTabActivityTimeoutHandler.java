80a457d5f9f67bedee4e8ef1452618be0bb3c6c6
Pasha Kostyukov <kostyukovp@google.com>
2025-09-04 12:24:37 -0700

    Add support for sending a PendingIntent on Custom Tab timeout.
    This change introduces a `PendingIntent` to `CustomTabActivityTimeoutHandler`. When a timeout condition is met, if a `PendingIntent` is provided, it will be sent instead of finishing the Custom Tab activity. This implementation might not be used for the reset timeout and we might use existing implementation for finishing the activity. This CL would give us an opportunity choose the implementation.
    
    Change-Id: I5ebc328fe976d6b8a559e5c052c5f4d0d9389b55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6907595
    Commit-Queue: Pasha Kostyukov <kostyukovp@google.com>
    Reviewed-by: Edmund Wright <edmundw@google.com>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511063}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivityTimeoutHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivityTimeoutHandler.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.customtabs;
 
 import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -13,6 +14,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import org.chromium.base.IntentUtils;
+import org.chromium.base.Log;
 import org.chromium.base.TimeUtils;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
@@ -27,6 +29,8 @@ import java.util.concurrent.TimeUnit;
 @NullMarked
 class CustomTabActivityTimeoutHandler {
 
+    private static final String TAG = "CustomTabTimeout";
+
     /**
      * An extra that can be used to provide a timeout in minutes. If the user leaves the app and
      * returns after the timeout has elapsed, the activity will be finished.
@@ -34,11 +38,16 @@ class CustomTabActivityTimeoutHandler {
     static final String EXTRA_TIMEOUT_MINUTES =
             "org.chromium.chrome.browser.customtabs.EXTRA_TIMEOUT_MINUTES";
 
+    static final String EXTRA_TIMEOUT_PENDING_INTENT =
+            "org.chromium.chrome.browser.customtabs.EXTRA_TIMEOUT_PENDING_INTENT";
+
     static final String KEY_LEAVE_TIMESTAMP = "CustomTabActivity.leave_timestamp";
 
     private final Runnable mFinishActivityRunnable;
     private final Intent mIntent;
     private final boolean mIsTimeoutEnabled;
+
+    @Nullable private final PendingIntent mEmbedderClosingIntent;
 
     // Timestamp of when the user left the activity, used for timeout logic.
     private long mLeaveTimestamp = -1;
@@ -46,6 +55,12 @@ class CustomTabActivityTimeoutHandler {
     CustomTabActivityTimeoutHandler(Runnable finishActivityRunnable, Intent intent) {
         mFinishActivityRunnable = finishActivityRunnable;
         mIntent = intent;
+        if (IntentUtils.safeGetParcelableExtra(intent, EXTRA_TIMEOUT_PENDING_INTENT)
+                instanceof PendingIntent pendingIntent) {
+            mEmbedderClosingIntent = pendingIntent;
+        } else {
+            mEmbedderClosingIntent = null;
+        }
         mIsTimeoutEnabled =
                 IntentUtils.safeHasExtra(intent, EXTRA_TIMEOUT_MINUTES)
                         && ChromeFeatureList.isEnabled(ChromeFeatureList.CCT_RESET_TIMEOUT_ENABLED);
@@ -82,8 +97,18 @@ class CustomTabActivityTimeoutHandler {
             long elapsedTime = TimeUtils.elapsedRealtimeMillis() - mLeaveTimestamp;
 
             if (elapsedTime > timeoutMillis) {
-                // Finish the activity if the timeout has elapsed.
-                mFinishActivityRunnable.run();
+                // Finish the activity if the timeout has elapsed. If an embedder closing intent is
+                // specified, send it, otherwise finish the activity.
+                if (mEmbedderClosingIntent != null) {
+                    try {
+                        mEmbedderClosingIntent.send();
+                    } catch (PendingIntent.CanceledException e) {
+                        Log.e(TAG, "Failed to send embedder intent: %s", e);
+                    }
+                    return;
+                } else {
+                    mFinishActivityRunnable.run();
+                }
             }
         }
         // Reset the timestamp after checking to ensure the timeout logic only runs once per leave.
