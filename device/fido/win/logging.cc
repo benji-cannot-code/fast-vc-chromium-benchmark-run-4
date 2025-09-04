6492760ad8f147bc5a2f6273fc0d9ed4b8c8eea4
Nina Satragno <nsatragno@chromium.org>
2025-09-04 12:25:51 -0700

    [webauthn] Redact sensitive info from win log
    Redact the following fields from the device log for the Windows API:
    * Signature
    * largeBlob field
    * largeBlob extension
    * prf extension
    
    Bug: 438773761
    Change-Id: I49b0277c1c554f4738ea5d9954819161a4aa8637
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6916002
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511065}

--- device/fido/win/logging.cc
+++ device/fido/win/logging.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <string_view>
 
+#include "base/containers/contains.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "components/device_event_log/device_event_log.h"
@@ -107,8 +108,14 @@ std::ostream& operator<<(std::ostream& out,
 }
 
 std::ostream& operator<<(std::ostream& out, const WEBAUTHN_EXTENSION& in) {
-  return out << "{" << Quoted(in.pwszExtensionIdentifier) << kSep
-             << base::HexEncode(in.pvExtension, in.cbExtension) << "}";
+  constexpr const wchar_t* kRedactedExtensions[] = {L"largeBlob", L"prf"};
+  out << "{" << Quoted(in.pwszExtensionIdentifier) << kSep;
+  if (base::Contains(kRedactedExtensions, in.pwszExtensionIdentifier)) {
+    out << "[redacted]";
+  } else {
+    out << base::HexEncode(in.pvExtension, in.cbExtension);
+  }
+  return out << "}";
 }
 
 std::ostream& operator<<(std::ostream& out, const WEBAUTHN_EXTENSIONS& in) {
@@ -176,7 +183,7 @@ std::ostream& operator<<(
   }
   out << kSep << in.dwCredLargeBlobOperation << "(";
   if (in.cbCredLargeBlob) {
-    out << base::HexEncode(in.pbCredLargeBlob, in.cbCredLargeBlob) << ")";
+    out << "[redacted large blob]" << ")";
   } else {
     out << "null)";
   }
@@ -244,15 +251,15 @@ std::ostream& operator<<(std::ostream& out,
 std::ostream& operator<<(std::ostream& out, const WEBAUTHN_ASSERTION& in) {
   out << "{" << in.dwVersion << kSep
       << base::HexEncode(in.pbAuthenticatorData, in.cbAuthenticatorData) << kSep
-      << base::HexEncode(in.pbSignature, in.cbSignature) << kSep
-      << in.Credential << kSep << base::HexEncode(in.pbUserId, in.cbUserId);
+      << "[redacted signature]" << kSep << in.Credential << kSep
+      << base::HexEncode(in.pbUserId, in.cbUserId);
   if (in.dwVersion < WEBAUTHN_ASSERTION_VERSION_2) {
     return out << "}";
   }
   out << in.Extensions;
   out << kSep << in.dwCredLargeBlobStatus << " (";
   if (in.pbCredLargeBlob) {
-    out << base::HexEncode(in.pbCredLargeBlob, in.cbCredLargeBlob) << ")";
+    out << "[redacted large blob]" << ")";
   } else {
     out << "null)";
   }
