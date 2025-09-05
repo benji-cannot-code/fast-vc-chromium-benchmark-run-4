78f5c90da94d1349d884469d4f204556c1528b6c
Matt Menke <mmenke@chromium.org>
2025-09-04 21:01:37 -0700

    Attempt to fix lifetime issue with HttpStreamPool.
    Make HttpStreamPool::JobController own pending streams instead of
    binding them to a callback, to ensure the streams are destroyed before
    the rest of the network stack is.
    
    Bug: 442566938
    Change-Id: I3b54eb02d6de5b039da420c695d8bc21d7336d8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6913563
    Commit-Queue: mmenke <mmenke@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511308}

--- net/http/http_stream_pool_job_controller.cc
+++ net/http/http_stream_pool_job_controller.cc
@@ -44,12 +44,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace net {
 
-HttpStreamPool::JobController::StreamWithProtocol::StreamWithProtocol(
+HttpStreamPool::JobController::PendingStream::PendingStream(
     std::unique_ptr<HttpStream> stream,
-    NextProto negotiated_protocol)
-    : stream(std::move(stream)), negotiated_protocol(negotiated_protocol) {}
+    NextProto negotiated_protocol,
+    std::optional<SessionSource> session_source)
+    : stream(std::move(stream)),
+      negotiated_protocol(negotiated_protocol),
+      session_source(session_source) {}
 
-HttpStreamPool::JobController::StreamWithProtocol::~StreamWithProtocol() =
+HttpStreamPool::JobController::PendingStream::PendingStream(PendingStream&&) =
+    default;
+
+HttpStreamPool::JobController::PendingStream::~PendingStream() = default;
+
+HttpStreamPool::JobController::PendingStream&
+HttpStreamPool::JobController::PendingStream::operator=(PendingStream&&) =
     default;
 
 // static
@@ -183,9 +192,9 @@ void HttpStreamPool::JobController::HandleStreamRequest(
     return;
   }
 
-  auto stream_with_protocol = MaybeCreateStreamFromExistingSession();
-  if (stream_with_protocol) {
-    if (stream_with_protocol->negotiated_protocol != NextProto::kProtoQUIC &&
+  pending_stream_ = MaybeCreateStreamFromExistingSession();
+  if (pending_stream_) {
+    if (pending_stream_->negotiated_protocol != NextProto::kProtoQUIC &&
         origin_quic_version_.IsKnown()) {
       StartAltSvcQuicPreconnect();
     }
@@ -193,10 +202,7 @@ void HttpStreamPool::JobController::HandleStreamRequest(
         FROM_HERE,
         base::BindOnce(
             &HttpStreamPool::JobController::CallRequestCompleteAndStreamReady,
-            weak_ptr_factory_.GetWeakPtr(),
-            std::move(stream_with_protocol->stream),
-            stream_with_protocol->negotiated_protocol,
-            SessionSource::kExisting));
+            weak_ptr_factory_.GetWeakPtr()));
     return;
   }
 
@@ -315,14 +321,23 @@ void HttpStreamPool::JobController::OnStreamReady(
     NextProto negotiated_protocol,
     std::optional<SessionSource> session_source) {
   SetJobResult(job, OK);
+
+  // If there's already a `pending_stream_` or the callback has already been
+  // invoked, nothing more to do.
+  if (pending_stream_) {
+    return;
+  }
+
+  pending_stream_.emplace(std::move(stream), negotiated_protocol,
+                          session_source);
+
   // Use PostTask to align the behavior with HttpStreamFactory::Job, see
   // https://crrev.com/2827533002.
   // TODO(crbug.com/346835898): Avoid using PostTask here if possible.
   base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
       FROM_HERE,
       base::BindOnce(&JobController::CallRequestCompleteAndStreamReady,
-                     weak_ptr_factory_.GetWeakPtr(), std::move(stream),
-                     negotiated_protocol, session_source));
+                     weak_ptr_factory_.GetWeakPtr()));
 }
 
 void HttpStreamPool::JobController::OnStreamFailed(
@@ -440,7 +455,7 @@ SpdySessionPool* HttpStreamPool::JobController::spdy_session_pool() {
   return pool_->http_network_session()->spdy_session_pool();
 }
 
-std::optional<HttpStreamPool::JobController::StreamWithProtocol>
+std::optional<HttpStreamPool::JobController::PendingStream>
 HttpStreamPool::JobController::MaybeCreateStreamFromExistingSession() {
   // Check QUIC session first.
   std::unique_ptr<HttpStream> quic_http_stream =
@@ -449,8 +464,9 @@ HttpStreamPool::JobController::MaybeCreateStreamFromExistingSession() {
     net_log_.AddEvent(
         NetLogEventType::
             HTTP_STREAM_POOL_JOB_CONTROLLER_FOUND_EXISTING_QUIC_SESSION);
-    return std::optional<StreamWithProtocol>(
-        std::in_place, std::move(quic_http_stream), NextProto::kProtoQUIC);
+    return std::optional<PendingStream>(
+        std::in_place, std::move(quic_http_stream), NextProto::kProtoQUIC,
+        SessionSource::kExisting);
   }
 
   // Check SPDY session next.
@@ -466,8 +482,9 @@ HttpStreamPool::JobController::MaybeCreateStreamFromExistingSession() {
     auto http_stream = std::make_unique<SpdyHttpStream>(
         spdy_session, stream_request_->net_log().source(),
         spdy_session_pool()->GetDnsAliasesForSessionKey(spdy_session_key));
-    return std::optional<StreamWithProtocol>(
-        std::in_place, std::move(http_stream), NextProto::kProtoHTTP2);
+    return std::optional<PendingStream>(std::in_place, std::move(http_stream),
+                                        NextProto::kProtoHTTP2,
+                                        SessionSource::kExisting);
   }
 
   // Check idle HTTP/1.1 stream.
@@ -485,8 +502,9 @@ HttpStreamPool::JobController::MaybeCreateStreamFromExistingSession() {
         origin_group.CreateTextBasedStream(std::move(idle_stream_socket),
                                            reuse_type,
                                            LoadTimingInfo::ConnectTiming());
-    return std::optional<StreamWithProtocol>(
-        std::in_place, std::move(http_stream), negotiated_protocol);
+    return std::optional<PendingStream>(std::in_place, std::move(http_stream),
+                                        negotiated_protocol,
+                                        SessionSource::kExisting);
   }
 
   return std::nullopt;
@@ -570,16 +588,15 @@ void HttpStreamPool::JobController::StartAltSvcQuicPreconnect() {
   origin_job_->Start();
 }
 
-void HttpStreamPool::JobController::CallRequestCompleteAndStreamReady(
-    std::unique_ptr<HttpStream> stream,
-    NextProto negotiated_protocol,
-    std::optional<SessionSource> session_source) {
+void HttpStreamPool::JobController::CallRequestCompleteAndStreamReady() {
   CHECK(stream_request_);
   CHECK(delegate_);
-  stream_request_->Complete({negotiated_protocol,
+  CHECK(pending_stream_);
+
+  stream_request_->Complete({pending_stream_->negotiated_protocol,
                              ALTERNATE_PROTOCOL_USAGE_UNSPECIFIED_REASON,
-                             session_source});
-  delegate_->OnStreamReady(proxy_info_, std::move(stream));
+                             pending_stream_->session_source});
+  delegate_->OnStreamReady(proxy_info_, std::move(pending_stream_->stream));
 }
 
 void HttpStreamPool::JobController::CallOnStreamFailed(
