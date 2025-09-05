0c5311fda8296de2e3c9a5971250000beb533d0e
Guillaume Jenkins <gujen@google.com>
2025-09-05 13:54:59 -0700

    [iOS] Enable TailoredNonModalDBPromo by default
    Enables the kTailoredNonModalDBPromo feature by default after all
    approvals have been received.
    
    Also removes the corresponding field trial testing config entry.
    
    Bug: 408243236
    Enabled-by-default-reason: Launching
    Change-Id: Ica1aac3da5fe9bfd3eb39ba3b8f8ba1c0be9c558
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6916301
    Auto-Submit: Guillaume Jenkins <gujen@google.com>
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Reviewed-by: Sebastien S-G <sebsg@chromium.org>
    Commit-Queue: Sebastien S-G <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511854}

--- ios/chrome/browser/default_browser/model/features.mm
+++ ios/chrome/browser/default_browser/model/features.mm
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/default_browser/model/features.h"
 
-BASE_FEATURE(TailoredNonModalDBPromo, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(TailoredNonModalDBPromo, base::FEATURE_ENABLED_BY_DEFAULT);
 
 bool IsTailoredNonModalDBPromoEnabled() {
   return base::FeatureList::IsEnabled(kTailoredNonModalDBPromo);
