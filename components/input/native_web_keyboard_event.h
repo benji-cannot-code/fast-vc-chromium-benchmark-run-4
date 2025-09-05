1b7bd13ae5e2f085ca086dfdad8b6c985938652a
Mitsuru Oshima <oshima@chromium.org>
2025-09-05 15:47:23 -0700

    Rename native_window_types.h to native_ui_types.h
    in //components
    
    We decided to rename to native_ui_types.h instead. Otherwise, this
    is same as the original CL that renamed to native_window_types.h
    
    AX-Relnotes: n/a.
    Bug: 440644556
    Change-Id: Id7e61de633cf5e975afbb7fe782be6521b8baf52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6915354
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511911}

--- components/input/native_web_keyboard_event.h
+++ components/input/native_web_keyboard_event.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "build/build_config.h"
 #include "third_party/blink/public/common/input/web_keyboard_event.h"
-#include "ui/gfx/native_window_types.h"
+#include "ui/gfx/native_ui_types.h"
 
 #if BUILDFLAG(IS_ANDROID)
 #include "base/android/scoped_java_ref.h"
--- components/remote_cocoa/app_shim/override_ns_next_step_frame_hit_test.h
+++ components/remote_cocoa/app_shim/override_ns_next_step_frame_hit_test.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_REMOTE_COCOA_APP_SHIM_OVERRIDE_NS_NEXT_STEP_FRAME_HIT_TEST_H_
 #define COMPONENTS_REMOTE_COCOA_APP_SHIM_OVERRIDE_NS_NEXT_STEP_FRAME_HIT_TEST_H_
 
-#include "ui/gfx/native_window_types.h"
+#include "ui/gfx/native_ui_types.h"
 
 namespace remote_cocoa {
 
