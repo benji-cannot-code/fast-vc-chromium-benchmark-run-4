d4674e42f999668bd45966fae9da007cf239ab90
Alexei Svitkine <asvitkine@chromium.org>
2025-09-05 07:05:32 -0700

    Reset `ScopedFeatureList` in `BlinkTestEnvironment` destructor.
    This change ensures that the `ScopedFeatureList` instance managed by
    `FeatureListScopedToEachTest` is reset when `BlinkTestEnvironment` is
    destroyed. This is necessary because if `exit()` is called within a test
    (e.g., in fuzz tests), `BlinkTestEnvironment`'s destructor can run while
    `FeatureListScopedToEachTest` is still active, potentially leading to
    incorrect destruction order of `ScopedFeatureList` instances and
    triggering CHECKs. A static instance pointer and reset method are added
    to `FeatureListScopedToEachTest` to allow this forced reset.
    
    Note: It may seem redundant that there's two levels of
    ScopedFeatureLists (one in FeatureListScopedToEachTest and one in
    BlinkTestEnvironment), but per the CL desc of this CL that added it,
    it's apparently necessary: https://crrev.com/c/5739000
    
    Bug: 441543203
    Change-Id: If5c13040963d5c09d570ca02af67d89853172cfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6915483
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511540}

--- base/test/test_suite.cc
+++ base/test/test_suite.cc
@@ -160,8 +160,12 @@ class ResetCommandLineBetweenTests : public testing::EmptyTestEventListener {
 // to initialize them manually.
 class FeatureListScopedToEachTest : public testing::EmptyTestEventListener {
  public:
-  FeatureListScopedToEachTest() = default;
-  ~FeatureListScopedToEachTest() override = default;
+  FeatureListScopedToEachTest() {
+    instance_ = this;
+  }
+  ~FeatureListScopedToEachTest() override {
+    instance_ = nullptr;
+  }
 
   FeatureListScopedToEachTest(const FeatureListScopedToEachTest&) = delete;
   FeatureListScopedToEachTest& operator=(const FeatureListScopedToEachTest&) =
@@ -183,9 +187,19 @@ class FeatureListScopedToEachTest : public testing::EmptyTestEventListener {
     scoped_feature_list_.Reset();
   }
 
+  static void Reset() {
+    if (instance_) {
+      instance_->scoped_feature_list_.Reset();
+    }
+  }
+
  private:
   test::ScopedFeatureList scoped_feature_list_;
+
+  static FeatureListScopedToEachTest* instance_;
 };
+
+FeatureListScopedToEachTest* FeatureListScopedToEachTest::instance_ = nullptr;
 
 class CheckForLeakedGlobals : public testing::EmptyTestEventListener {
  public:
@@ -443,6 +457,10 @@ void TestSuite::DisableCheckForThreadAndProcessPriority() {
 void TestSuite::DisableCheckForLeakedGlobals() {
   DCHECK(!is_initialized_);
   check_for_leaked_globals_ = false;
+}
+
+void TestSuite::ResetScopedFeatureListInstance() {
+  FeatureListScopedToEachTest::Reset();
 }
 
 void TestSuite::UnitTestAssertHandler(const char* file,
