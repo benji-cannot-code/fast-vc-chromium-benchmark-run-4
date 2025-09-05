8ea5e3d10192209e33efea9413a7205473da02e1
Javier Garcia Visiedo <visiedo@chromium.org>
2025-09-04 22:59:32 -0700

    Add WebDxFeature UseCounter to readable-byte-streams
    The Web Platform Telemetry project aims to provide better usage data to
    teams working on the WebPlatform in Chrome. This change adds a new
    kReadableByteStreams WebDXFeature use counter to account for the use
    of readable-byte-streams feature
    (https://webstatus.dev/features/readable-byte-streams) as part of this
    initiative.
    
    Bug: 411544193
    Change-Id: Ib5e25f23503665f422999ec51e805d01588e2ff7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6902673
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511324}

--- components/page_load_metrics/browser/observers/use_counter/webdx_feature_maps.cc
+++ components/page_load_metrics/browser/observers/use_counter/webdx_feature_maps.cc
@@ -448,7 +448,9 @@ UseCounterMetricsRecorder::GetWebFeatureToWebDXFeatureMap() {
           {WebFeature::kV8WasmCustomDescriptors,
            WebDXFeature::kDRAFT_WasmCustomDescriptors},
           {WebFeature::kFetchUploadStreaming,
-           WebDXFeature::kFetchRequestStreams}
+           WebDXFeature::kFetchRequestStreams},
+          {WebFeature::kReadableStreamWithByteSource,
+           WebDXFeature::kReadableByteStreams},
           // Add new features above this line.
       }};
 
