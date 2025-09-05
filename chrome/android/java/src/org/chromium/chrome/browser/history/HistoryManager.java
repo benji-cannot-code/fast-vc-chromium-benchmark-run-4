1d8f4bfb3caa3ea96f6748bfb09fcf233d64b06f
Fiona Macintosh <fmacintosh@google.com>
2025-09-05 08:44:06 -0700

    Delete Android.HistoryPage.ClearBrowsingData.PerProfileType
    This histogram is expiring and per UMA data only one profile type is
    recorded so suggestion is to delete it. Also removing associated logic
    
    OBSOLETE_HISTOGRAM[Android.HistoryPage.ClearBrowsingData.PerProfileType]=Expiring soon and not needed going forward
    
    Bug: 436260374
    Change-Id: Icff5900ac3d155d7875011dacbb90bdcb9d08bf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6907409
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Commit-Queue: Roger McFarlane <rogerm@chromium.org>
    Auto-Submit: Fiona Macintosh <fmacintosh@google.com>
    Commit-Queue: Fiona Macintosh <fmacintosh@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511634}

--- chrome/android/java/src/org/chromium/chrome/browser/history/HistoryManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/history/HistoryManager.java
@@ -610,7 +610,7 @@ public class HistoryManager
     // HistoryContentManager.Observer
     @Override
     public void onClearBrowsingDataClicked() {
-        mUmaRecorder.recordClearBrowsingData(mIsIncognito);
+        mUmaRecorder.recordClearBrowsingData();
         // Opens the clear browsing data preference.
         SettingsNavigation settingsNavigation =
                 SettingsNavigationFactory.createSettingsNavigation();
