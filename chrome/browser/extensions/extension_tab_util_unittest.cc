2c8765b7d0219ec0aa734cb54da791ba13f89974
Zhengzheng Liu <zhzhliu@chromium.org>
2025-09-05 13:02:58 -0700

    extensions: Refactor NavigateToURL to take disposition param
    -- Add CHECK of the disposition param for NavigateToURL to make sure it
    is supported on the specific platform.
    
    -- Add simple browsertest for NavigateToURL method.
    
    -- Enable extension_tab_util_unittest on desktop Android.
    
    Bug: 440173000
    Change-Id: If93303e0d781d6cf2e19072822578c8d60225fa9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6913722
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Zhengzheng Liu <zhzhliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511813}

--- chrome/browser/extensions/extension_tab_util_unittest.cc
+++ chrome/browser/extensions/extension_tab_util_unittest.cc
@@ -13,10 +13,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_util.h"
 #include "components/sync_preferences/testing_pref_service_syncable.h"
 #include "extensions/browser/pref_names.h"
+#include "extensions/buildflags/buildflags.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/extension_builder.h"
 #include "extensions/common/mojom/context_type.mojom.h"
 #include "testing/gtest/include/gtest/gtest.h"
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace extensions {
 
