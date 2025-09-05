72de26acd75b64fa9a6239d8d40480a0c6204a9d
Kevin McNee <mcnee@chromium.org>
2025-09-05 16:21:13 -0700

    Revert "fix close tab during actuation crash"
    This reverts commit d502577278a11b670363452017efb31f0c3fde59.
    
    Reason for revert: Crash in PageTool::OnRenderFrameHostChanged in internal e2e tests
    
    Bug: 436886189
    Original change's description:
    > fix close tab during actuation crash
    >
    > - A use-after-free crash in `ObservationDelayController` when a tab was closed mid-actuation
    > - Differentiate between navigation and tab closed by checking webcontents.
    >
    > Bug: 436886189
    > Change-Id: If342b1b175efb606e4c09e211cf2335444ab289f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6880355
    > Commit-Queue: Rodney Ding <rodneyding@google.com>
    > Reviewed-by: David Bokan <bokan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1511858}
    
    Bug: 436886189
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I313e250ea9c96bc4abb6b1eb1553dba9e454bf40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6916569
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1511925}

--- chrome/browser/actor/tools/page_tool.h
+++ chrome/browser/actor/tools/page_tool.h
@@ -53,7 +53,6 @@ class PageTool : public Tool {
                               InvokeCallback callback) const override;
 
  private:
-  void OnRenderFrameHostChanged();
   void FinishInvoke(mojom::ActionResultPtr result);
 
   void PostFinishInvoke(mojom::ActionResultCode result_code);
