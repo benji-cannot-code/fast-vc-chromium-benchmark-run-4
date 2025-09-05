a933357712ff33851361ca7cdb2fbe4f668ca991
Yihong Guo <yhguo@google.com>
2025-09-05 13:39:47 -0700

    [Save to Drive] Display time remaining in progress file metadata
    This change adds functionality to track upload progress, calculate the
    upload speed, and estimate the remaining time for a file being saved to
    Google Drive. The time calculation is all done as int and not rounded,
    since the final product is user facing and is just an estimation. It is
    only included in the file metadata string if upload is in progress.
    
    Change-Id: I71a1f08f0f6d93cbde938a590496480cff869327
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6892953
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Commit-Queue: Yihong Guo <yhguo@google.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511848}

--- chrome/browser/save_to_drive/save_to_drive_flow_unittest.cc
+++ chrome/browser/save_to_drive/save_to_drive_flow_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/save_to_drive/multipart_drive_uploader.h"
 #include "chrome/browser/save_to_drive/resumable_drive_uploader.h"
 #include "chrome/browser/save_to_drive/save_to_drive_event_dispatcher.h"
+#include "chrome/browser/save_to_drive/time_remaining_calculator.h"
 #include "chrome/browser/signin/identity_test_environment_profile_adaptor.h"
 #include "chrome/common/extensions/api/pdf_viewer_private.h"
 #include "chrome/test/base/testing_profile.h"
@@ -48,9 +49,13 @@ AccountInfo CreateAccountInfo() {
 
 class MockSaveToDriveEventDispatcher : public SaveToDriveEventDispatcher {
  public:
-  MockSaveToDriveEventDispatcher(content::RenderFrameHost* render_frame_host,
-                                 const GURL& stream_url)
-      : SaveToDriveEventDispatcher(render_frame_host, stream_url) {}
+  MockSaveToDriveEventDispatcher(
+      content::RenderFrameHost* render_frame_host,
+      const GURL& stream_url,
+      std::unique_ptr<TimeRemainingCalculator> time_remaining_calculator)
+      : SaveToDriveEventDispatcher(render_frame_host,
+                                   stream_url,
+                                   std::move(time_remaining_calculator)) {}
   ~MockSaveToDriveEventDispatcher() override = default;
 
   MOCK_METHOD(void, Notify, (SaveToDriveProgress progress), (const, override));
@@ -75,7 +80,8 @@ class SaveToDriveFlowTest : public testing::Test {
     web_contents_ = web_contents_factory_.CreateWebContents(profile_.get());
     auto event_dispatcher =
         std::make_unique<testing::StrictMock<MockSaveToDriveEventDispatcher>>(
-            rfh(), GURL("https://example.com/stream"));
+            rfh(), GURL("https://example.com/stream"),
+            std::make_unique<TimeRemainingCalculator>());
     auto content_reader =
         std::make_unique<testing::StrictMock<MockContentReader>>();
 
