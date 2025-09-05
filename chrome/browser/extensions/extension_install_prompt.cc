c25bc7ce144d972d1a587d637635dce66d38e42a
Foromo Daniel Soromou <koretadaniel@chromium.org>
2025-09-04 23:40:13 -0700

    [Clean up] Move extensions strings in chrome/chromium grd
    Bug: None
    Change-Id: I7864ed6c512b15f55917fc5893e64ada70f06fca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6915000
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511328}

--- chrome/browser/extensions/extension_install_prompt.cc
+++ chrome/browser/extensions/extension_install_prompt.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/extensions/extension_install_ui.h"
 #include "chrome/common/buildflags.h"
+#include "chrome/grit/branded_strings.h"
 #include "chrome/grit/generated_resources.h"
 #include "chrome/grit/theme_resources.h"
 #include "components/strings/grit/components_strings.h"
