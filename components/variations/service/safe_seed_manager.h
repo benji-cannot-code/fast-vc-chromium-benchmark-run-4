558e8f373bffed8af595cb366bd7723ccd5f3a00
Ekaterina Kulikova <kate.kulikova@lge.com>
2025-09-05 06:07:28 -0700

    IWYU: Add missing include for std::optional usage in safe_seed_manager.h
    In the CL https://crrev.com/c/6875306 used std::optional in the
    components/variations/service/safe_seed_manager.h, but corresponding
    include was missed.
    Added missing include for std::optional usage.
    
    Bug: 41455655
    Change-Id: I6081130e8f18e0f95d39e2748de6ac8055828797
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6916048
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Kate Kulikova <kate.kulikova@lge.com>
    Cr-Commit-Position: refs/heads/main@{#1511500}

--- components/variations/service/safe_seed_manager.h
+++ components/variations/service/safe_seed_manager.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_VARIATIONS_SERVICE_SAFE_SEED_MANAGER_H_
 #define COMPONENTS_VARIATIONS_SERVICE_SAFE_SEED_MANAGER_H_
 
+#include <optional>
+
 #include "base/memory/raw_ptr.h"
 #include "base/time/time.h"
 
