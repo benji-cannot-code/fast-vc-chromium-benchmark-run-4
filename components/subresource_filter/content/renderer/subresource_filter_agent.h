432857e3a01ecf2572956c9776815294045974dc
Charlie Harrison <csharrison@chromium.org>
2025-09-05 14:39:47 -0700

    [subresource_filter] Experiment with prewarming on ReadyToCommit
    The subresource filter utilizes a memory mapped file for its ruleset.
    This means that the first reads in a process always need to go to disk.
    Since the first reads are nearly always in the critical path, this
    CL "prewarms" the file by querying it outside of the critical path.
    
    Change-Id: Ie582dce2a0b0f820be88bde7de88ce0bd0b76db3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6915627
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511882}

--- components/subresource_filter/content/renderer/subresource_filter_agent.h
+++ components/subresource_filter/content/renderer/subresource_filter_agent.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/feature_list.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "components/subresource_filter/content/mojom/subresource_filter.mojom.h"
@@ -24,6 +25,8 @@ class WebDocumentSubresourceFilter;
 }  // namespace blink
 
 namespace subresource_filter {
+
+BASE_DECLARE_FEATURE(kSubresourceFilterPrewarm);
 
 class UnverifiedRulesetDealer;
 class WebDocumentSubresourceFilterImpl;
@@ -107,6 +110,7 @@ class SubresourceFilterAgent
 
   // mojom::SubresourceFilterAgent:
   void ActivateForNextCommittedLoad(
+      const GURL& url,
       mojom::ActivationStatePtr activation_state,
       const std::optional<blink::FrameAdEvidence>& ad_evidence) override;
 
@@ -119,14 +123,18 @@ class SubresourceFilterAgent
       content::RenderFrame* render_frame);
 
   void RecordHistogramsOnFilterCreation(
-      const mojom::ActivationState& activation_state);
+      const mojom::ActivationState& activation_state,
+      const GURL& url);
   void ResetInfoForNextDocument();
 
   virtual const mojom::ActivationState
   GetInheritedActivationStateForNewDocument();
 
-  void ConstructFilter(const mojom::ActivationState activation_state,
-                       const GURL& url);
+  std::unique_ptr<WebDocumentSubresourceFilterImpl> ConstructFilter(
+      const mojom::ActivationState& activation_state,
+      const GURL& url);
+  void ConstructAndSetFilter(const mojom::ActivationState& activation_state,
+                             const GURL& url);
 
   mojom::SubresourceFilterHost* GetSubresourceFilterHost();
 
@@ -149,6 +157,8 @@ class SubresourceFilterAgent
 
   mojom::ActivationState activation_state_for_next_document_;
 
+  std::unique_ptr<WebDocumentSubresourceFilterImpl> filter_for_next_document_;
+
   // Use associated interface to make sure mojo messages are ordered with regard
   // to legacy IPC messages.
   mojo::AssociatedRemote<mojom::SubresourceFilterHost> subresource_filter_host_;
@@ -157,6 +167,8 @@ class SubresourceFilterAgent
 
   base::WeakPtr<WebDocumentSubresourceFilterImpl>
       filter_for_last_created_document_;
+
+  bool any_filter_created_ = false;
   base::WeakPtrFactory<SubresourceFilterAgent> weak_ptr_factory_{this};
 };
 
