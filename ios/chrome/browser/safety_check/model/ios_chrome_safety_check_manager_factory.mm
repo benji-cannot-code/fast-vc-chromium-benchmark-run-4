8c35f23af1864cf58a5199a3f76b4d00bddcdd8c
Sylvain Defresne <sdefresne@chromium.org>
2025-09-05 11:02:31 -0700

    [ios] Convert factories to BuildServiceInstanceFor(ProfileIOS*)
    {Refcounted,}ProfileKeyedServiceFactoryIOS exposes a method
    BuildServiceInstanceFor(ProfileIOS*) so convert factories
    to override this method instead of the deprecated method
    BuildServiceInstanceFor(web::BrowserState*).
    
    This convert factories in //ios/chrome/browser/safety_check/model.
    
    Bug: 436484783
    Change-Id: Ib48eba1eb618c8d2ff064d730a5822a438aa3e55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6912875
    Reviewed-by: Benjamin Williams <bwwilliams@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511736}

--- ios/chrome/browser/safety_check/model/ios_chrome_safety_check_manager_factory.mm
+++ ios/chrome/browser/safety_check/model/ios_chrome_safety_check_manager_factory.mm
@@ -16,11 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-std::unique_ptr<KeyedService> BuildServiceInstance(web::BrowserState* context) {
+std::unique_ptr<KeyedService> BuildServiceInstance(ProfileIOS* profile) {
   CHECK(IsSafetyCheckMagicStackEnabled());
-
-  ProfileIOS* profile = ProfileIOS::FromBrowserState(context);
-
   const scoped_refptr<base::SequencedTaskRunner> task_runner =
       base::SequencedTaskRunner::GetCurrentDefault();
 
@@ -47,9 +44,9 @@ IOSChromeSafetyCheckManagerFactory::GetInstance() {
 }
 
 // static
-IOSChromeSafetyCheckManagerFactory::TestingFactory
+ProfileKeyedServiceFactoryIOS::ProfileTestingFactory
 IOSChromeSafetyCheckManagerFactory::GetDefaultFactory() {
-  return base::BindRepeating(&BuildServiceInstance);
+  return base::BindOnce(&BuildServiceInstance);
 }
 
 IOSChromeSafetyCheckManagerFactory::IOSChromeSafetyCheckManagerFactory()
@@ -63,6 +60,6 @@ IOSChromeSafetyCheckManagerFactory::~IOSChromeSafetyCheckManagerFactory() =
 
 std::unique_ptr<KeyedService>
 IOSChromeSafetyCheckManagerFactory::BuildServiceInstanceFor(
-    web::BrowserState* context) const {
-  return BuildServiceInstance(context);
+    ProfileIOS* profile) const {
+  return BuildServiceInstance(profile);
 }
