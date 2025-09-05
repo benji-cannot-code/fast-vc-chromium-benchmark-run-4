14752157767848b22bf2544419b0ce4940eb2b93
Sylvain Defresne <sdefresne@chromium.org>
2025-09-05 04:50:23 -0700

    [ios] Convert factories to BuildServiceInstanceFor(ProfileIOS*)
    {Refcounted,}ProfileKeyedServiceFactoryIOS exposes a method
    BuildServiceInstanceFor(ProfileIOS*) so convert factories
    to override this method instead of the deprecated method
    BuildServiceInstanceFor(web::BrowserState*).
    
    This convert factories in //ios/chrome/browser/web/model/java_script_console.
    
    Bug: 436484783
    Change-Id: Iddcc4aea1afb356ec3c6a7a72d84bd13c3d6609d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6916482
    Reviewed-by: Louis Romero <lpromero@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511463}

--- ios/chrome/browser/web/model/java_script_console/java_script_console_feature_factory.mm
+++ ios/chrome/browser/web/model/java_script_console/java_script_console_feature_factory.mm
@@ -32,6 +32,6 @@ JavaScriptConsoleFeatureFactory::~JavaScriptConsoleFeatureFactory() = default;
 
 std::unique_ptr<KeyedService>
 JavaScriptConsoleFeatureFactory::BuildServiceInstanceFor(
-    web::BrowserState* context) const {
+    ProfileIOS* profile) const {
   return std::make_unique<JavaScriptConsoleFeature>();
 }
