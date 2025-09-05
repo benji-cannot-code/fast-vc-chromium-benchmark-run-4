076d926268061e4ea9037d8d17605dcec9484532
Alison Gale <agale@chromium.org>
2025-09-05 09:38:36 -0700

    [SxS] Fix 3-dot menu color in custom theme
    Use a different token when custom themes are used. Follows the pattern
    used for the side panel buttons which are the same color.
    
    Bug: 437089466
    Change-Id: I39d60bfae64be8a168e764b234645ea5f07d5f77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6919927
    Auto-Submit: Alison Gale <agale@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511668}

--- chrome/browser/ui/color/material_chrome_color_mixer.cc
+++ chrome/browser/ui/color/material_chrome_color_mixer.cc
@@ -175,8 +175,7 @@ void AddMaterialChromeColorMixer(ui::ColorProvider* provider,
   // Split View colors.
   mixer[kColorSplitViewBackground] = {ui::kColorSysSurface2};
   mixer[kColorSplitViewScrim] = ui::SetAlpha(ui::kColorRefNeutral99, 0x99);
-  mixer[kColorMulitContentsViewMiniToolbarForeground] = {
-      ui::kColorSysOnSurfaceSubtle};
+  mixer[kColorMulitContentsViewMiniToolbarForeground] = {kColorToolbarText};
 
   // Side Panel colors.
   mixer[kColorSidePanelBackground] = {ui::kColorSysBaseContainer};
@@ -327,6 +326,10 @@ void AddMaterialChromeColorMixer(ui::ColorProvider* provider,
   mixer[kColorMediaRouterIconActive] =
       ui::PickGoogleColor(ui::kColorSysPrimary, kColorToolbar,
                           color_utils::kMinimumVisibleContrastRatio);
+
+  mixer[kColorMulitContentsViewMiniToolbarForeground] = {
+      ui::kColorSysOnSurfaceSubtle};
+
   mixer[kColorNewTabButtonFocusRing] = ui::PickGoogleColorTwoBackgrounds(
       ui::kColorSysStateFocusRing,
       ui::GetResultingPaintColor(kColorNewTabButtonBackgroundFrameActive,
