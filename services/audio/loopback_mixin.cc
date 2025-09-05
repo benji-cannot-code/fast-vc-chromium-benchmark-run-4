571a7fe6a8f2defeedf25594f972f38b6349e78d
Xiaohan Wang <xhwang@chromium.org>
2025-09-05 00:18:36 -0700

    services: Migrate to new 2-param BASE_FEATURE macro
    This is part of the migration to the new 2-param BASE_FEATURE macro
    to fix code search issue and make naming more consistent:
    - Old: BASE_FEATURE(MyFeature, base::FEATURE_ENABLED_BY_DEFAULT);
    - New: BASE_FEATURE(kMyFeature, base::FEATURE_ENABLED_BY_DEFAULT);
    
    This CL removes the `TODO_BASE_FEATURE_MACROS_NEED_MIGRATION` define
    and update the files to use the new 2-param BASE_FEATURE macro format.
    
    See the bug for details about the migration plan.
    
    Enabled-by-default-reason: refactoring
    
    Bug: 436274260
    Change-Id: I39b6a21de4a420f404a2069da70e960acd2375a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6916521
    Auto-Submit: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511342}

--- services/audio/loopback_mixin.cc
+++ services/audio/loopback_mixin.cc
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#define TODO_BASE_FEATURE_MACROS_NEED_MIGRATION
-
 #include "services/audio/loopback_mixin.h"
 
 #include "base/memory/ptr_util.h"
@@ -20,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace audio {
 
-BASE_FEATURE(RestrictOwnAudioAddChromiumBack,
+BASE_FEATURE(kRestrictOwnAudioAddChromiumBack,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 // static
