6b255e1ebc0dce07b2bc650c23fbb65a90fcd17b
Fiorella Barrientos Villalta <fiorellab@google.com>
2025-09-05 10:25:20 -0700

    [APC] Log server error codes for the login check step
    This change adds a distinct log entry for server errors and empty
    response failures, which were previously undifferentiated. This will
    allow faster and more accurate debugging.
    
    Bug: 442992978
    Change-Id: If5158b6b44899ded8a28d5f254e5bcfb7e40ae20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6919396
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Fiorella Barrientos Villalta <fiorellab@google.com>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1511676}

--- chrome/browser/password_manager/password_change/login_state_checker.cc
+++ chrome/browser/password_manager/password_change/login_state_checker.cc
@@ -49,6 +49,16 @@ void LogBoolean(password_manager::PasswordManagerClient* client,
   }
 }
 
+void LogNumber(password_manager::PasswordManagerClient* client,
+               autofill::SavePasswordProgressLogger::StringID message_id,
+               int error_enum) {
+  if (client && client->GetCurrentLogManager() &&
+      client->GetCurrentLogManager()->IsLoggingActive()) {
+    BrowserSavePasswordProgressLogger(client->GetCurrentLogManager())
+        .LogNumber(message_id, error_enum);
+  }
+}
+
 }  // namespace
 
 LoginStateChecker::LoginStateChecker(
@@ -151,8 +161,9 @@ void LoginStateChecker::OnExecutionResponseCallback(
       client_,
       SavePasswordProgressLogger::STRING_LOGIN_STATE_CHECK_RESPONSE_RECEIVED);
   if (!execution_result.response.has_value()) {
-    LogMessage(client_,
-               SavePasswordProgressLogger::STRING_LOGIN_STATE_CHECK_FAILURE);
+    LogNumber(client_,
+              SavePasswordProgressLogger::STRING_LOGIN_STATE_CHECK_SERVER_ERROR,
+              static_cast<int>(execution_result.response.error().error()));
     TerminateLoginChecks();
     return;
   }
