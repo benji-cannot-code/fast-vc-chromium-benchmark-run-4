e95ca73db4007556405be92bb95605736da99474
Steinar H. Gunderson <sesse@chromium.org>
2025-09-05 07:23:54 -0700

    Use concurrent bitfield reads in CSSSelector::Trace().
    Evidently, using ConcurrentlyReadBitField on its own is not enough;
    you need to explicitly ask for get_concurrently(). (set() is atomic,
    though.)
    
    Fixed: 435225413
    Change-Id: Iab76c9994bc478cf073d16f2391eaab69ba47cf1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6919097
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511549}

--- third_party/blink/renderer/core/css/css_selector.h
+++ third_party/blink/renderer/core/css/css_selector.h
@@ -406,6 +406,9 @@ class CORE_EXPORT CSSSelector {
   PseudoType GetPseudoType() const {
     return static_cast<PseudoType>(bits_.get<PseudoTypeField>());
   }
+  PseudoType GetPseudoTypeForOilpan() const {
+    return static_cast<PseudoType>(bits_.get_concurrently<PseudoTypeField>());
+  }
 
   void UpdatePseudoType(const AtomicString&,
                         const CSSParserContext&,
@@ -565,6 +568,9 @@ class CORE_EXPORT CSSSelector {
   MatchType Match() const {
     return static_cast<MatchType>(bits_.get<MatchField>());
   }
+  MatchType MatchForOilpan() const {
+    return static_cast<MatchType>(bits_.get_concurrently<MatchField>());
+  }
   void SetMatch(MatchType match) {
     bits_.set<MatchField>(match);
     DCHECK_EQ(Match(), match);  // using a bitfield.
@@ -600,6 +606,9 @@ class CORE_EXPORT CSSSelector {
   bool HasVisited() const;
 
   bool HasRareData() const { return bits_.get<HasRareDataField>(); }
+  bool HasRareDataForOilpan() const {
+    return bits_.get_concurrently<HasRareDataField>();
+  }
 
   bool IsForPage() const { return bits_.get<IsForPageField>(); }
   void SetForPage() { bits_.set<IsForPageField>(true); }
@@ -647,7 +656,7 @@ class CORE_EXPORT CSSSelector {
   // RuleData bucketing sets is_covered_by_bucketing,
   // and these could happen concurrently. This trips up TSan,
   // even though the race is benign, so use an atomic read
-  // instead of C++ bitfields.
+  // while in the Oilpan thread, instead of C++ bitfields.
   using BitField = ConcurrentlyReadBitField<uint32_t>;
   using RelationField =
       BitField::DefineFirstValue<uint32_t, 4>;  // RelationType
