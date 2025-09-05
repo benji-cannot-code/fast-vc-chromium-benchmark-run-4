b337090307cf4ef95dd01e98fe4d0336592d21b3
Sylvain Defresne <sdefresne@chromium.org>
2025-09-05 05:39:59 -0700

    [ios] Convert factories to BuildServiceInstanceFor(ProfileIOS*)
    {Refcounted,}ProfileKeyedServiceFactoryIOS exposes a method
    BuildServiceInstanceFor(ProfileIOS*) so convert factories
    to override this method instead of the deprecated method
    BuildServiceInstanceFor(web::BrowserState*).
    
    This convert factories in //ios/chrome/browser/voice/ui_bundled.
    
    Bug: 436484783
    Change-Id: Iaa53d8c0f808c3b0bf87698ad4efd939b1c47236
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6912801
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511479}

--- ios/chrome/browser/voice/ui_bundled/text_to_speech_playback_controller_factory.mm
+++ ios/chrome/browser/voice/ui_bundled/text_to_speech_playback_controller_factory.mm
@@ -32,6 +32,6 @@ TextToSpeechPlaybackControllerFactory::TextToSpeechPlaybackControllerFactory()
 
 std::unique_ptr<KeyedService>
 TextToSpeechPlaybackControllerFactory::BuildServiceInstanceFor(
-    web::BrowserState* context) const {
+    ProfileIOS* profile) const {
   return std::make_unique<TextToSpeechPlaybackController>();
 }
