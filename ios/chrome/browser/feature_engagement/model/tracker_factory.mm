cb3d46cbbe66712a2780ea5b58ec9761f7d02aa1
Sylvain Defresne <sdefresne@chromium.org>
2025-09-05 06:05:02 -0700

    [ios] Convert factories to BuildServiceInstanceFor(ProfileIOS*)
    {Refcounted,}ProfileKeyedServiceFactoryIOS exposes a method
    BuildServiceInstanceFor(ProfileIOS*) so convert factories
    to override this method instead of the deprecated method
    BuildServiceInstanceFor(web::BrowserState*).
    
    This convert factories in //ios/chrome/browser/feature_engagement/model.
    
    Bug: 436484783
    Change-Id: Ide7b982d3592741f20effc2333d727e4893925ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6915721
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511496}

--- ios/chrome/browser/feature_engagement/model/tracker_factory.mm
+++ ios/chrome/browser/feature_engagement/model/tracker_factory.mm
@@ -11,6 +11,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
 
 namespace feature_engagement {
+namespace {
+
+// static
+std::unique_ptr<KeyedService> BuildServiceInstance(ProfileIOS* profile) {
+  return CreateFeatureEngagementTracker(profile);
+}
+
+}  // namespace
 
 // static
 feature_engagement::Tracker* TrackerFactory::GetForProfile(
@@ -33,20 +41,14 @@ TrackerFactory::TrackerFactory()
 TrackerFactory::~TrackerFactory() = default;
 
 // static
-TrackerFactory::TestingFactory TrackerFactory::GetDefaultFactory() {
-  return base::BindRepeating(&BuildServiceInstance);
+ProfileKeyedServiceFactoryIOS::ProfileTestingFactory
+TrackerFactory::GetDefaultFactory() {
+  return base::BindOnce(&BuildServiceInstance);
 }
 
 std::unique_ptr<KeyedService> TrackerFactory::BuildServiceInstanceFor(
-    web::BrowserState* context) const {
-  return CreateFeatureEngagementTracker(ProfileIOS::FromBrowserState(context));
-}
-
-// static
-std::unique_ptr<KeyedService> TrackerFactory::BuildServiceInstance(
-    web::BrowserState* context) {
-  ProfileIOS* profile = ProfileIOS::FromBrowserState(context);
-  return feature_engagement::CreateFeatureEngagementTracker(profile);
+    ProfileIOS* profile) const {
+  return BuildServiceInstance(profile);
 }
 
 }  // namespace feature_engagement
