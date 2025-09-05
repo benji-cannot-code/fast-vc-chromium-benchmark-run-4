f68ee8bce0f9684ac07c4976a7bff8ed7c00795f
Jérôme Lebel <jlebel@chromium.org>
2025-09-05 06:37:57 -0700

    [iOS] Avoid reentry in add account coordinator
    It is unclear how it possible, but while there is a way to have a
    stack of coordinator, the user is able to sign-in using another UI:
    - SigninAndHistorySyncCoordinator
    - InstantSigninCoordinator
    - AddAccountSigninCoordinator
    
    In the bug, AddAccountSigninCoordinator is stuck by displaying the
    alert dialog (to say that it is not possible to open to add account
    at the same time).
    
    Since the primary account is set, InstantSigninCoordinator stops itself
    and shutdown AddAccountSigninCoordinator. While the add account
    coordinator stops, it also stops the alert dialog.
    By stopping the alert dialog, this triggers a second stop of the add
    account coordinator.
    
    To avoid a reentry, when before stopping the alert dialog,
    `alertCoordinator.noInteractionAction` needs to be set to nil.
    
    This patch should be cherry-picked in M141, and maybe in M140.
    
    Fixed: 441677851
    Change-Id: I6511ea7c447d853a5e578e1d6dfd36106b2d1fa8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6917619
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511520}

--- ios/chrome/browser/authentication/ui_bundled/signin/add_account_signin/add_account_signin_coordinator.mm
+++ ios/chrome/browser/authentication/ui_bundled/signin/add_account_signin/add_account_signin_coordinator.mm
@@ -266,6 +266,8 @@ using signin_metrics::PromoAction;
 #pragma mark - Private
 
 - (void)stopAlertCoordinator {
+  // To avoid reentry, `noInteractionAction` is set to nil.
+  self.alertCoordinator.noInteractionAction = nil;
   [self.alertCoordinator stop];
   self.alertCoordinator = nil;
 }
