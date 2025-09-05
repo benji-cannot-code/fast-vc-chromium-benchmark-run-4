11fcf7522010c66f7258172922f7dca9fba8f8c8
Kartar Singh <kartarsingh@google.com>
2025-09-05 07:47:21 -0700

    Cancel ongoing touch sequence upon navigation on Aura and Mac
    The change is same as https://crrev.com/c/6836138, except we are now
    doing it for other platforms, and we add a browser test to not break the
    behavior in future.
    
    Bug: 435778464, 419912177
    Change-Id: I8a31de5fbbb1147b26776b76d41b280561eb8fb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6846305
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Kartar Singh <kartarsingh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1511569}

--- content/browser/renderer_host/render_widget_host_view_aura.cc
+++ content/browser/renderer_host/render_widget_host_view_aura.cc
@@ -3091,6 +3091,7 @@ void RenderWidgetHostViewAura::OnOldViewDidNavigatePreCommit() {
   // Invalidate the surface so that we don't attempt to evict it multiple times.
   window_->InvalidateLocalSurfaceId();
   delegated_frame_host_->DidNavigateMainFramePreCommit();
+  CancelActiveTouches();
 }
 
 void RenderWidgetHostViewAura::OnNewViewDidNavigatePostCommit() {
