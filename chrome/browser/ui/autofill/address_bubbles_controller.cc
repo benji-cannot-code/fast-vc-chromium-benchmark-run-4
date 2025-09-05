a3b27647035814cb8ff95aab032e9196b298a801
Vidhan Jain <vidhanj@google.com>
2025-09-05 04:04:05 -0700

    Use QueueOrShowBubble instead of ShowBubble
    This CL updates the remaining usages of ShowBubble in classes extending
    from AutofillBubbleControllerBase. However, calls to show bubbles on tab
    visibility changed are skipped.
    
    Bug: 432429605
    Change-Id: Idab600a1ab02eca2eae850f1ef9aca6cb0a6e08d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6915257
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1511446}

--- chrome/browser/ui/autofill/address_bubbles_controller.cc
+++ chrome/browser/ui/autofill/address_bubbles_controller.cc
@@ -163,7 +163,7 @@ void AddressBubblesController::OnUserDecision(
   if (decision == AutofillClient::AddressPromptUserDecision::kEditDeclined) {
     // Reopen this bubble if the user canceled editing.
     shown_by_user_gesture_ = false;
-    ShowBubble();
+    QueueOrShowBubble(/*force_show=*/true);
     return;
   }
   if (address_profile_save_prompt_callback_) {
@@ -197,7 +197,7 @@ void AddressBubblesController::OnIconClicked() {
     return;
   }
   shown_by_user_gesture_ = true;
-  ShowBubble();
+  QueueOrShowBubble(/*force_show=*/true);
 }
 
 bool AddressBubblesController::IsBubbleActive() const {
