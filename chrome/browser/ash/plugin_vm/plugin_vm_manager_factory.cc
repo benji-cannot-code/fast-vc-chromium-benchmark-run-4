44fb967358257d2c624bb010bac830b0ccb49411
Jun Ishiguro <junis@google.com>
2025-09-04 22:48:10 -0700

    ash-refactor: Inject ApplicationLocaleStorage dependency
    PluginVmManagerImpl is updated to receive ApplicationLocaleStorage as a
    parameter instead of calling into the g_browser_process.
    
    Fixed: b:404133194
    Change-Id: Icac1c932ad03b99c5dd29fee5eafe0def89af0a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6915374
    Reviewed-by: Georg Neis <neis@chromium.org>
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1511322}

--- chrome/browser/ash/plugin_vm/plugin_vm_manager_factory.cc
+++ chrome/browser/ash/plugin_vm/plugin_vm_manager_factory.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/no_destructor.h"
 #include "chrome/browser/ash/plugin_vm/plugin_vm_manager_impl.h"
+#include "chrome/browser/browser_process.h"
+#include "chrome/browser/global_features.h"
 #include "chrome/browser/profiles/profile.h"
 
 namespace plugin_vm {
@@ -37,8 +39,11 @@ PluginVmManagerFactory::~PluginVmManagerFactory() = default;
 std::unique_ptr<KeyedService>
 PluginVmManagerFactory::BuildServiceInstanceForBrowserContext(
     content::BrowserContext* context) const {
+  auto* application_locale_storage =
+      g_browser_process->GetFeatures()->application_locale_storage();
   Profile* profile = Profile::FromBrowserContext(context);
-  return std::make_unique<PluginVmManagerImpl>(profile);
+  return std::make_unique<PluginVmManagerImpl>(application_locale_storage,
+                                               profile);
 }
 
 }  // namespace plugin_vm
--- chrome/browser/ash/plugin_vm/plugin_vm_manager_impl.h
+++ chrome/browser/ash/plugin_vm/plugin_vm_manager_impl.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/memory/raw_ptr.h"
+#include "base/memory/raw_ref.h"
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
 #include "chrome/browser/ash/guest_os/guest_os_dlc_helper.h"
@@ -23,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/dbus/vm_plugin_dispatcher/vm_plugin_dispatcher.pb.h"
 #include "chromeos/ash/components/dbus/vm_plugin_dispatcher/vm_plugin_dispatcher_client.h"
 
+class ApplicationLocaleStorage;
 class Profile;
 
 namespace plugin_vm {
@@ -50,7 +52,10 @@ class PluginVmManagerImpl : public PluginVmManager,
  public:
   using LaunchPluginVmCallback = base::OnceCallback<void(bool success)>;
 
-  explicit PluginVmManagerImpl(Profile* profile);
+  // `application_locale_storage` must be non-null, and must outlive `this`.
+  PluginVmManagerImpl(
+      const ApplicationLocaleStorage* application_locale_storage,
+      Profile* profile);
 
   PluginVmManagerImpl(const PluginVmManagerImpl&) = delete;
   PluginVmManagerImpl& operator=(const PluginVmManagerImpl&) = delete;
@@ -149,6 +154,8 @@ class PluginVmManagerImpl : public PluginVmManager,
   // Called when Plugin VM changes availability e.g. installed, uninstalled,
   // policy changes.
   void OnAvailabilityChanged(bool is_allowed, bool is_configured);
+
+  const raw_ref<const ApplicationLocaleStorage> application_locale_storage_;
 
   raw_ptr<Profile> profile_;
   std::string owner_id_;
