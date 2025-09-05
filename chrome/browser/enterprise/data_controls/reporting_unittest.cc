846bad54fca08709266e1086729588b29191cf1f
Dominique Fauteux-Chapleau <domfc@chromium.org>
2025-09-05 13:58:30 -0700

    Enable `EnterpriseActiveUserDetection` by default
    Enabled-by-default-reason: Launching experiment, see cl/803461996
    Bug: 293360492
    Change-Id: I9eab9eca172361923ec5c6f696190047760fef8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6919807
    Commit-Queue: Nancy Xiao <nancylanxiao@google.com>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1511856}

--- chrome/browser/enterprise/data_controls/reporting_unittest.cc
+++ chrome/browser/enterprise/data_controls/reporting_unittest.cc
@@ -23,6 +23,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/data_transfer_policy/data_transfer_endpoint.h"
 
+#if BUILDFLAG(IS_CHROMEOS)
+#include "chromeos/ash/components/network/network_handler.h"
+#include "chromeos/ash/services/network_config/public/cpp/cros_network_config_test_helper.h"
+#endif
+
 namespace data_controls {
 
 namespace {
@@ -44,11 +49,21 @@ class DataControlsReportingTest : public testing::Test {
     helper_ = std::make_unique<
         enterprise_connectors::test::EventReportValidatorHelper>(
         managed_profile_);
+
+#if BUILDFLAG(IS_CHROMEOS)
+    network_config_helper_ =
+        std::make_unique<ash::network_config::CrosNetworkConfigTestHelper>();
+    ash::NetworkHandler::Initialize();
+#endif
   }
 
   void TearDown() override {
     managed_profile_->GetPrefs()->ClearPref(kDataControlsRulesScopePref);
     helper_.reset();
+
+#if BUILDFLAG(IS_CHROMEOS)
+    ash::NetworkHandler::Shutdown();
+#endif
   }
 
   Profile* incognito_managed_profile() {
@@ -154,6 +169,10 @@ class DataControlsReportingTest : public testing::Test {
   std::unique_ptr<content::WebContents> guest_contents_;
   std::unique_ptr<enterprise_connectors::test::EventReportValidatorHelper>
       helper_;
+#if BUILDFLAG(IS_CHROMEOS)
+  std::unique_ptr<ash::network_config::CrosNetworkConfigTestHelper>
+      network_config_helper_;
+#endif
 };
 
 }  // namespace
--- chrome/browser/enterprise/data_protection/paste_allowed_request_unittest.cc
+++ chrome/browser/enterprise/data_protection/paste_allowed_request_unittest.cc
@@ -31,6 +31,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/clipboard/scoped_clipboard_writer.h"
 #include "ui/base/clipboard/test/test_clipboard.h"
 
+#if BUILDFLAG(IS_CHROMEOS)
+#include "chromeos/ash/components/network/network_handler.h"
+#include "chromeos/ash/services/network_config/public/cpp/cros_network_config_test_helper.h"
+#endif
+
 namespace enterprise_data_protection {
 
 namespace {
@@ -117,6 +122,18 @@ class PasteAllowedRequestTest : public testing::Test {
   void SetUp() override {
     PasteAllowedRequest::CleanupRequestsForTesting();
     ui::TestClipboard::CreateForCurrentThread();
+
+#if BUILDFLAG(IS_CHROMEOS)
+    network_config_helper_ =
+        std::make_unique<ash::network_config::CrosNetworkConfigTestHelper>();
+    ash::NetworkHandler::Initialize();
+#endif
+  }
+
+  void TearDown() override {
+#if BUILDFLAG(IS_CHROMEOS)
+    ash::NetworkHandler::Shutdown();
+#endif
   }
 
   content::WebContents* main_web_contents() {
@@ -168,6 +185,10 @@ class PasteAllowedRequestTest : public testing::Test {
   raw_ptr<TestingProfile> profile_;
   std::unique_ptr<content::WebContents> main_web_contents_;
   std::unique_ptr<content::WebContents> secondary_web_contents_;
+#if BUILDFLAG(IS_CHROMEOS)
+  std::unique_ptr<ash::network_config::CrosNetworkConfigTestHelper>
+      network_config_helper_;
+#endif
 };
 
 class PasteAllowedRequestScanningTest : public PasteAllowedRequestTest {
