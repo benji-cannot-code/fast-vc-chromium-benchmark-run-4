77eb08a164efb9dfef7c994b3086640f6a770a38
Daniel Cheng <dcheng@chromium.org>
2025-09-04 21:37:05 -0700

    Migrate deprecated PerformRequest() calls in gif_tenor_api_fetcher.cc
    `PerformRequest()` is deprecated. Since the fetcher itself is
    constructed with an auth type of `NO_AUTH` (this is the builder
    default), and the fetcher is called with a null key, this is a
    straightforward migration to just call `Fetch()` instead.
    
    Bug: 284531303
    Change-Id: Ia13e6c5cd1e520d5127379e1ccded5072a9f697a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6897494
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511309}

--- chromeos/ash/components/emoji/gif_tenor_api_fetcher.cc
+++ chromeos/ash/components/emoji/gif_tenor_api_fetcher.cc
@@ -408,10 +408,9 @@ void GifTenorApiFetcher::FetchCategories(
                                 kClientKeyName, kClientKeyValue),
       kTrafficAnnotation);
   auto* const endpoint_fetcher_ptr = endpoint_fetcher.get();
-  endpoint_fetcher_ptr->PerformRequest(
-      base::BindOnce(&FetchCategoriesResponseHandler, std::move(callback),
-                     std::move(endpoint_fetcher)),
-      nullptr);
+  endpoint_fetcher_ptr->Fetch(base::BindOnce(&FetchCategoriesResponseHandler,
+                                             std::move(callback),
+                                             std::move(endpoint_fetcher)));
 }
 
 void GifTenorApiFetcher::FetchFeaturedGifs(
@@ -460,10 +459,9 @@ void GifTenorApiFetcher::FetchFeaturedGifs(
       CreateEndpointFetcher(std::move(url_loader_factory),
                             GetUrl(kFeaturedApi, pos), kTrafficAnnotation);
   auto* const endpoint_fetcher_ptr = endpoint_fetcher.get();
-  endpoint_fetcher_ptr->PerformRequest(
-      base::BindOnce(&TenorGifsApiResponseHandler, std::move(callback),
-                     std::move(endpoint_fetcher)),
-      nullptr);
+  endpoint_fetcher_ptr->Fetch(base::BindOnce(&TenorGifsApiResponseHandler,
+                                             std::move(callback),
+                                             std::move(endpoint_fetcher)));
 }
 
 void GifTenorApiFetcher::FetchGifSearch(
@@ -481,10 +479,9 @@ void GifTenorApiFetcher::FetchGifSearch(
   auto endpoint_fetcher = CreateEndpointFetcher(std::move(url_loader_factory),
                                                 url, kSearchTrafficAnnotation);
   auto* const endpoint_fetcher_ptr = endpoint_fetcher.get();
-  endpoint_fetcher_ptr->PerformRequest(
-      base::BindOnce(&TenorGifsApiResponseHandler, std::move(callback),
-                     std::move(endpoint_fetcher)),
-      nullptr);
+  endpoint_fetcher_ptr->Fetch(base::BindOnce(&TenorGifsApiResponseHandler,
+                                             std::move(callback),
+                                             std::move(endpoint_fetcher)));
 }
 
 std::unique_ptr<EndpointFetcher> GifTenorApiFetcher::FetchGifSearchCancellable(
@@ -501,11 +498,9 @@ std::unique_ptr<EndpointFetcher> GifTenorApiFetcher::FetchGifSearchCancellable(
 
   std::unique_ptr<EndpointFetcher> endpoint_fetcher = CreateEndpointFetcher(
       std::move(url_loader_factory), url, kSearchTrafficAnnotation);
-  CHECK_DEREF(endpoint_fetcher.get())
-      .PerformRequest(
-          base::BindOnce(&TenorGifsApiResponseHandler, std::move(callback),
-                         /*endpoint_fetcher=*/nullptr),
-          nullptr);
+  endpoint_fetcher.get()->Fetch(base::BindOnce(&TenorGifsApiResponseHandler,
+                                               std::move(callback),
+                                               /*endpoint_fetcher=*/nullptr));
   return endpoint_fetcher;
 }
 
@@ -558,10 +553,9 @@ void GifTenorApiFetcher::FetchGifsByIds(
           "ids", base::JoinString(ids, ",")),
       kTrafficAnnotation);
   auto* const endpoint_fetcher_ptr = endpoint_fetcher.get();
-  endpoint_fetcher_ptr->PerformRequest(
-      base::BindOnce(&FetchGifsByIdsResponseHandler, std::move(callback),
-                     std::move(endpoint_fetcher)),
-      nullptr);
+  endpoint_fetcher_ptr->Fetch(base::BindOnce(&FetchGifsByIdsResponseHandler,
+                                             std::move(callback),
+                                             std::move(endpoint_fetcher)));
 }
 
 }  // namespace ash
