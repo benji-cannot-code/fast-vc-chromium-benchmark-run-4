493b42c4f91da7ef5093bfb740d30f389c04c51a
Kuo-Hsin Yang <vovoy@google.com>
2025-09-05 08:45:17 -0700

    [resource coordinator] Remove unused utility methods
    Remove SerializeOriginIntoDatabaseKey and
    URLShouldBeStoredInLocalDatabase.
    
    Bug: 394889323
    Change-Id: I6c42514701492c29bc1ad7691c5ed823f32613a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6918617
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Vovo Yang <vovoy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511636}

--- chrome/browser/resource_coordinator/utils.cc
+++ chrome/browser/resource_coordinator/utils.cc
@@ -5,20 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/resource_coordinator/utils.h"
 
-#include "base/hash/md5.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/resource_coordinator/resource_coordinator_parts.h"
 
 namespace resource_coordinator {
-
-std::string SerializeOriginIntoDatabaseKey(const url::Origin& origin) {
-  return base::MD5String(origin.host());
-}
-
-bool URLShouldBeStoredInLocalDatabase(const GURL& url) {
-  // Only store information for the HTTP(S) sites for now.
-  return url.SchemeIsHTTPOrHTTPS();
-}
 
 TabLifecycleUnitSource* GetTabLifecycleUnitSource() {
   DCHECK(g_browser_process);
