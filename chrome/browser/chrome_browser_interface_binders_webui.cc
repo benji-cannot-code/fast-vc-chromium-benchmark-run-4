db846fae7876d2a2bfc3ea188358d7f35393c89c
Paul Jensen <pauljensen@chromium.org>
2025-09-05 14:15:13 -0700

    webium: Get color theme changes propagating
    This change gets color changes coming from "Customize Chrome"
    propagating to all parts of the Chrome UI.  The tricky part is making
    sure the WebUIBrowserWindow (which is a ui::ColorProviderSource) is
    set as the ColorProviderSource on all WebContents, namely the
    browser frame's, the side-panel's, and any rendering tab contents.
    This CL does not add OS theme tracking.  Linux functionality is
    subject to crbug.com/442745067
    
    Bug: 393144165
    Change-Id: I4d85c3cbf8e3a4301d9aee858de52a726edb00ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6910481
    Auto-Submit: Paul Jensen <pauljensen@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511872}

--- chrome/browser/chrome_browser_interface_binders_webui.cc
+++ chrome/browser/chrome_browser_interface_binders_webui.cc
@@ -1460,6 +1460,7 @@ void PopulateChromeWebUIFrameInterfaceBrokers(
 
   if (webui_browser::IsWebUIBrowserEnabled()) {
     registry.ForWebUI<WebUIBrowserUI>()
+        .Add<color_change_listener::mojom::PageHandler>()
         .Add<webui_browser::mojom::PageHandlerFactory>()
         .Add<bookmark_bar::mojom::PageHandlerFactory>()
         .Add<searchbox::mojom::PageHandler>()
