64cd61c7f4467399dee2fd476086e8f77f65450b
Evan Luo <evanluo@google.com>
2025-09-05 13:07:28 -0700

    [reader mode] Fix reader mode prefs UI responsiveness
    Allow the reader mode prefs UI to gracefully adjust to larger zoom/font
    settings. Includes changes to font style button size, font size, capping
    width of the slider to not span the entire width of the bottomsheet.
    
    Also fixed font style not properly applying on the font style buttons.
    
    Screenshots
    Phone on max zoom/font size:
    https://screenshot.googleplex.com/5HtLWHWUeMVpRrg.png
    
    Tablet default state:
    https://screenshot.googleplex.com/cEU6kF4FFMjpMgc.png
    
    Bug: 441485016
    Change-Id: I8b3c2a7f7817516bad8493e0080a1e34fa71c86b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6913584
    Reviewed-by: Brandon Fong <bjfong@google.com>
    Commit-Queue: Evan Luo <evanluo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1511820}

--- chrome/android/java/src/org/chromium/chrome/browser/dom_distiller/ReaderModePrefsView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/dom_distiller/ReaderModePrefsView.java
@@ -116,7 +116,7 @@ public class ReaderModePrefsView extends LinearLayout
         initializeFontButton(R.id.font_serif, FontFamily.SERIF);
         initializeFontButton(R.id.font_monospace, FontFamily.MONOSPACE);
 
-        mFontScalingSlider = findViewById(R.id.font_size);
+        mFontScalingSlider = findViewById(R.id.font_size_slider);
         mFontScalingSlider.setValueFrom(FONT_SCALE_LOWER_BOUND);
         mFontScalingSlider.setValueTo(FONT_SCALE_UPPER_BOUND);
         mFontScalingSlider.setStepSize(FONT_SCALE_STEP_SIZE);
@@ -151,10 +151,6 @@ public class ReaderModePrefsView extends LinearLayout
         SpannableString fontStyleNameString = new SpannableString(line2);
 
         fontStyleSignifierString.setSpan(
-                new TypefaceSpan(typeface), 0, line1.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        fontStyleNameString.setSpan(
-                new TypefaceSpan(typeface), 0, line2.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        fontStyleSignifierString.setSpan(
                 new TextAppearanceSpan(
                         getContext(), R.style.TextAppearance_ReaderModePrefsFontStyleSignifier),
                 0,
@@ -166,6 +162,11 @@ public class ReaderModePrefsView extends LinearLayout
                 0,
                 line2.length(),
                 Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        // Maintain TypefaceSpan initialization after TextAppearance or it will be overridden.
+        fontStyleSignifierString.setSpan(
+                new TypefaceSpan(typeface), 0, line1.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        fontStyleNameString.setSpan(
+                new TypefaceSpan(typeface), 0, line2.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
 
         // Get font metrics for first line using mock TextPaint to set suitable line height.
         TextPaint mockTextPaint = new TextPaint();
