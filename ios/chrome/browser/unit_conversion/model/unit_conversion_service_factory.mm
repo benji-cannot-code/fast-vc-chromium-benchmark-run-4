6c103c10540a2b49513c949152d3923bcaa59ea4
Sylvain Defresne <sdefresne@chromium.org>
2025-09-05 09:45:20 -0700

    [ios] Convert factories to BuildServiceInstanceFor(ProfileIOS*)
    {Refcounted,}ProfileKeyedServiceFactoryIOS exposes a method
    BuildServiceInstanceFor(ProfileIOS*) so convert factories
    to override this method instead of the deprecated method
    BuildServiceInstanceFor(web::BrowserState*).
    
    This convert factories in //ios/chrome/browser/unit_conversion/model.
    
    Bug: 436484783
    Change-Id: I4f8509a26e9160988f288c6f9730ebcb15db57cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6916044
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511673}

--- ios/chrome/browser/unit_conversion/model/unit_conversion_service_factory.mm
+++ ios/chrome/browser/unit_conversion/model/unit_conversion_service_factory.mm
@@ -31,6 +31,6 @@ UnitConversionServiceFactory::~UnitConversionServiceFactory() {}
 
 std::unique_ptr<KeyedService>
 UnitConversionServiceFactory::BuildServiceInstanceFor(
-    web::BrowserState* context) const {
+    ProfileIOS* profile) const {
   return std::make_unique<UnitConversionService>();
 }
