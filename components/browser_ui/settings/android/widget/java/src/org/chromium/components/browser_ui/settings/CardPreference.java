95021f809e40df6c4ed5e848652a46bb56da57ee
Gazal <agazal@google.com>
2025-09-05 13:14:16 -0700

    [Settings containment] Add custom background color to Safety check
    Before: https://screenshot.googleplex.com/9LJ7oj9ju99h5zH.png
    After: https://screenshot.googleplex.com/5oM7XEhspGCxh5K.png
    
    Bug: 433576895, 439725777
    
    Change-Id: Ida611c0b494fc139381eaf2d6103bd1caae8afd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6914515
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Gazal Agarwal <agazal@google.com>
    Auto-Submit: Gazal Agarwal <agazal@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511828}

--- components/browser_ui/settings/android/widget/java/src/org/chromium/components/browser_ui/settings/CardPreference.java
+++ components/browser_ui/settings/android/widget/java/src/org/chromium/components/browser_ui/settings/CardPreference.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.components.browser_ui.settings;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
@@ -37,12 +38,21 @@ public class CardPreference extends TextMessagePreference {
     private @Nullable ChromeImageView mIcon;
     private @Nullable ChromeImageView mCloseIcon;
     private boolean mShouldCenterIcon;
+    private final int mBackgroundStyle;
+    private final int mBackgroundColor;
 
     /** Constructor for inflating from XML. */
     public CardPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
         setLayoutResource(R.layout.card_preference);
         setSelectable(false);
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.ChromeBasePreference);
+        mBackgroundStyle =
+                a.getInt(
+                        R.styleable.ChromeBasePreference_backgroundStyle, BackgroundStyle.STANDARD);
+        mBackgroundColor =
+                a.getInt(R.styleable.ChromeBasePreference_backgroundColor, DEFAULT_COLOR);
+        a.recycle();
     }
 
     @Override
@@ -117,5 +127,22 @@ public class CardPreference extends TextMessagePreference {
      */
     public void setShouldCenterIcon(boolean shouldCenterIcon) {
         mShouldCenterIcon = shouldCenterIcon;
+    }
+
+    @Override
+    public @BackgroundStyle int getCustomBackgroundStyle() {
+        return mBackgroundStyle;
+    }
+
+    @Override
+    public int getCustomBackgroundColor() {
+        return mBackgroundColor;
+    }
+
+    @Override
+    public int getCustomHorizontalMargin() {
+        // TODO (crbug.com/439911511): Remove the custom horizontal margin along with
+        // android:layout_marginHorizontal value in card_preference.xml.
+        return 0;
     }
 }
--- components/browser_ui/settings/android/widget/java/src/org/chromium/components/browser_ui/settings/ChromeBasePreference.java
+++ components/browser_ui/settings/android/widget/java/src/org/chromium/components/browser_ui/settings/ChromeBasePreference.java
@@ -35,6 +35,7 @@ import org.chromium.build.annotations.Nullable;
 public class ChromeBasePreference extends Preference implements CustomStyledPreference {
     private final @Nullable ColorStateList mIconTint;
     private final int mBackgroundStyle;
+    private final int mBackgroundColor;
     private @Nullable ManagedPreferenceDelegate mManagedPrefDelegate;
 
     /** Indicates if the preference uses a custom layout. */
@@ -63,6 +64,8 @@ public class ChromeBasePreference extends Preference implements CustomStyledPref
         mBackgroundStyle =
                 a.getInt(
                         R.styleable.ChromeBasePreference_backgroundStyle, BackgroundStyle.STANDARD);
+        mBackgroundColor =
+                a.getInt(R.styleable.ChromeBasePreference_backgroundColor, DEFAULT_COLOR);
         a.recycle();
 
         mHasCustomLayout = ManagedPreferencesUtils.isCustomLayoutApplied(context, attrs);
@@ -118,5 +121,10 @@ public class ChromeBasePreference extends Preference implements CustomStyledPref
     @Override
     public int getCustomBackgroundStyle() {
         return mBackgroundStyle;
+    }
+
+    @Override
+    public int getCustomBackgroundColor() {
+        return mBackgroundColor;
     }
 }
