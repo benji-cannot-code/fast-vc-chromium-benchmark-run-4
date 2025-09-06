8d37f54e7c8798619a913fed4771abdaa110db1c
Xiqi Ruan <xiqiruan@chromium.org>
2025-09-06 14:49:57 -0700

    Revert "extensions: Fix a race adding component extensions multiple times"
    This reverts commit c708d3922cbb162c0fe10898ec8eb9d2df4d474b.
    
    Reason for revert: Fails the browser test at https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/53371/overview
    
    Bug: 442902361
    Original change's description:
    > extensions: Fix a race adding component extensions multiple times
    >
    > - Make `AddComponentFromDirWithManifestFilename` and
    >   `FinishAddComponentFromDir` track pending extension loads on the file
    >   task runner;
    > - Update `AddChromeOsSpeechSynthesisExtensions` and a couple other
    >   callers to check `IsPendingAdd` in addition to `Exists`;
    > - Add unit tests to verify the pending load is tracked as expected;
    > - Minor clean-ups;
    >
    > Bug: 442902361
    > Change-Id: I3deaed45449feddce2e8426a8bb2d1c7c1e46ee9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6916700
    > Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    > Reviewed-by: James Cook <jamescook@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1511591}
    
    Bug: 442902361
    Change-Id: I3b3d17f3043ecb942480176263d2642c0bef19a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6921052
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Xiqi Ruan <xiqiruan@chromium.org>
    Commit-Queue: Xiqi Ruan <xiqiruan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1512085}

--- chrome/browser/ash/file_manager/file_manager_test_util.cc
+++ chrome/browser/ash/file_manager/file_manager_test_util.cc
@@ -150,7 +150,7 @@ void AddDefaultComponentExtensionsOnMainThread(Profile* profile) {
       data_dir.Append("chromeos/file_manager/quickoffice"),
       extension_misc::kQuickOfficeComponentExtensionId,
       extensions::kManifestFilename, extensions::kManifestFilename,
-      run_loop.QuitClosure(), {});
+      run_loop.QuitClosure());
   run_loop.Run();
 #endif
   // AddDefaultComponentExtensions() is normally invoked during
