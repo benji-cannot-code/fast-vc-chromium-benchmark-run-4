4da994611b807737df9350318d3fc9636e2b9698
Daniel Cheng <dcheng@chromium.org>
2025-09-05 17:55:28 -0700

    Roll log: 0.4.27 => 0.4.28 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * log: 0.4.27 => 0.4.28; https://docs.rs/crate/log/0.4.28
    
    Bug: None
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Change-Id: I9e0da6e78d00c5ae5d7ae91bd22f5dd8e7143f44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6918273
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511955}

--- third_party/rust/chromium_crates_io/vendor/log-v0_4/src/kv/mod.rs
+++ third_party/rust/chromium_crates_io/vendor/log-v0_4/src/kv/mod.rs
@@ -226,7 +226,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //!
 //! ```
 //! # use log::kv::Key;
-//! # #[derive(Debug)]
+//! #[derive(Debug)]
 //! struct Data {
 //!     a: i32,
 //!     b: bool,
