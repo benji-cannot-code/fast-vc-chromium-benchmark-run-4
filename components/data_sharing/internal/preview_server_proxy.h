06f7c95fb2d881340a7056d3e084bf781e8ec48c
Daniel Cheng <dcheng@chromium.org>
2025-09-05 17:52:46 -0700

    Remove unneeded call to `ParseJsonIsolated()` in PreviewServerProxy
    base::JSONReader is Rust-based and can be used to decode untrustworthy
    data. Also migrate to use ReadDict() since the caller only wants a
    dictionary.
    
    Bug: 414842425
    Change-Id: Id230528b2250379725e582da2bc0c5080058497a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6914125
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511953}

--- components/data_sharing/internal/preview_server_proxy.h
+++ components/data_sharing/internal/preview_server_proxy.h
@@ -6,16 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_DATA_SHARING_INTERNAL_PREVIEW_SERVER_PROXY_H_
 #define COMPONENTS_DATA_SHARING_INTERNAL_PREVIEW_SERVER_PROXY_H_
 
+#include <optional>
 #include <vector>
 
 #include "base/functional/callback.h"
 #include "base/memory/weak_ptr.h"
+#include "base/values.h"
 #include "base/version_info/channel.h"
 #include "components/data_sharing/public/data_sharing_service.h"
 #include "components/data_sharing/public/group_data.h"
 #include "components/endpoint_fetcher/endpoint_fetcher.h"
 #include "components/sync/base/data_type.h"
-#include "services/data_decoder/public/cpp/data_decoder.h"
 #include "url/gurl.h"
 
 namespace network {
@@ -65,7 +66,7 @@ class PreviewServerProxy {
       base::OnceCallback<
           void(const DataSharingService::SharedDataPreviewOrFailureOutcome&)>
           callback,
-      data_decoder::DataDecoder::ValueOrError result);
+      std::optional<base::Value::Dict> result);
 
   std::string GetPreviewServerURLString() const;
 
