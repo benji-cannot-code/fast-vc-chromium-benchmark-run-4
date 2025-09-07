ecb678154985091ca8da4f6a3b3f8f4f5eda6d1c
Thomas Lukaszewicz <tluk@chromium.org>
2025-09-07 12:11:02 -0700

    [bedrock] Eliminate BrowserList from SessionRestorePolicy
    Eliminates use of the deprecated BrowserList API and migrates code
    to the approprirate browser_window_interface_iterator API
    ForEachCurrentBrowserWindowInterfaceOrderedByActivation().
    
    Bug: 431672609
    Change-Id: I3fdce977a3c4b7e62372ae8452fabe47cfca7fc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6916641
    Commit-Queue: Tom Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1512175}

--- chrome/browser/resource_coordinator/session_restore_policy.cc
+++ chrome/browser/resource_coordinator/session_restore_policy.cc
@@ -39,8 +39,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/permissions/permission_utils.h"
 
 #if !BUILDFLAG(IS_ANDROID)
-#include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/browser_list.h"
+// TODO(crbug.com/407105162): Remove nogncheck when crbug.com/40147906 is fixed.
+#include "chrome/browser/ui/browser_window/public/browser_window_interface.h"  // nogncheck
+#include "chrome/browser/ui/browser_window/public/browser_window_interface_iterator.h"  // nogncheck
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #endif
 
@@ -125,16 +126,16 @@ float SessionRestorePolicy::AddTabForScoring(content::WebContents* contents) {
   // this!
   // In theory all tabs should belong to a tab-strip, but in tests this isn't
   // necessarily true.
-  auto* browser_list = BrowserList::GetInstance();
-  for (size_t i = 0; i < browser_list->size(); ++i) {
-    auto* browser = browser_list->get(i);
-    auto* tab_strip = browser->tab_strip_model();
-    int tab_index = tab_strip->GetIndexOfWebContents(contents);
-    if (tab_index == TabStripModel::kNoTab)
-      continue;
-    tab_data->is_pinned = tab_strip->IsTabPinned(tab_index);
-    break;
-  }
+  ForEachCurrentBrowserWindowInterfaceOrderedByActivation(
+      [&](BrowserWindowInterface* browser) {
+        auto* const tab_strip = browser->GetTabStripModel();
+        const int tab_index = tab_strip->GetIndexOfWebContents(contents);
+        if (tab_index == TabStripModel::kNoTab) {
+          return true;  // continue iteration.
+        }
+        tab_data->is_pinned = tab_strip->IsTabPinned(tab_index);
+        return false;  // end iteration.
+      });
 #endif  // !BUILDFLAG(IS_ANDROID)
 
   // Cache a handful of other properties.
