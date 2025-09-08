e744406b062dbe8c4c5c870a7b8a8f3afff479b7
Zainab Rizvi <rizvis@google.com>
2025-09-08 10:55:58 -0700

    [IPP] Remove unused functions from IpProtectionTokenManager
    IsAuthTokenAvailable() and GetAuthToken() are current geo specific
    functions that are never used and can be removed.
    
    Bug: 435139159
    Change-Id: Ie68128eebe47d1caaacb35470ec8e7d2cc417ec8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6924394
    Commit-Queue: Zainab Rizvi <rizvis@google.com>
    Reviewed-by: Andrew Williams <awillia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1512539}

--- components/ip_protection/common/ip_protection_token_manager_impl.cc
+++ components/ip_protection/common/ip_protection_token_manager_impl.cc
@@ -128,10 +128,6 @@ void IpProtectionTokenManagerImpl::ProcessInitialTokens(
   }
 }
 
-bool IpProtectionTokenManagerImpl::IsAuthTokenAvailable() {
-  return IsAuthTokenAvailable(current_geo_id_);
-}
-
 bool IpProtectionTokenManagerImpl::IsAuthTokenAvailable(
     const std::string& geo_id) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
@@ -406,11 +402,6 @@ void IpProtectionTokenManagerImpl::OnGotAuthTokens(
   }
 
   ScheduleMaybeRefillCache();
-}
-
-std::optional<BlindSignedAuthToken>
-IpProtectionTokenManagerImpl::GetAuthToken() {
-  return GetAuthToken(current_geo_id_);
 }
 
 std::optional<BlindSignedAuthToken> IpProtectionTokenManagerImpl::GetAuthToken(
