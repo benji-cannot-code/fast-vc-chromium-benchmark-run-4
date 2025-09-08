7c34cd7fd0528334c957fa4d7bbbc5e61d489f1e
Philip Jägenstedt <foolip@chromium.org>
2025-09-08 03:29:44 -0700

    Add WebDX use counter for device posture
    Bug: 366475997
    Change-Id: I3a7ccc86b70b4ae168a91d2d57bcc2e9e6c0a2e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6867569
    Reviewed-by: Alexis Menard <alexis.menard@intel.com>
    Commit-Queue: Philip Jägenstedt <foolip@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1512299}

--- components/page_load_metrics/browser/observers/use_counter/webdx_feature_maps.cc
+++ components/page_load_metrics/browser/observers/use_counter/webdx_feature_maps.cc
@@ -451,6 +451,9 @@ UseCounterMetricsRecorder::GetWebFeatureToWebDXFeatureMap() {
            WebDXFeature::kFetchRequestStreams},
           {WebFeature::kReadableStreamWithByteSource,
            WebDXFeature::kReadableByteStreams},
+          {WebFeature::kDevicePostureMediaFeature,
+           WebDXFeature::kDevicePosture},
+          {WebFeature::kDevicePosture, WebDXFeature::kDevicePosture},
           // Add new features above this line.
       }};
 
