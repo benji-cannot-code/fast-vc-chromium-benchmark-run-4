a92143cf280ec44d4b79750816ddc462b63b6180
Tibor Goldschwendt <tiborg@chromium.org>
2025-09-08 13:16:18 -0700

    [ntp] Check chrome.metricsPrivate exists before logging
    In rare cases, chrome.metricsPrivate is not available. Logging anyway
    causes a crash report. Gate all calls to chrome.metricsPrivate behind a
    check to avoid the crash report.
    
    Bug: 40162029
    Change-Id: Ib709f450e4974118db3e53d74515f594b33823b3
    Fixed: 442835995
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6923654
    Auto-Submit: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1512629}

--- chrome/test/data/webui/new_tab_page/metrics_utils_test.ts
+++ chrome/test/data/webui/new_tab_page/metrics_utils_test.ts
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {recordBoolean, recordDuration, recordEnumeration, recordLoadDuration, recordOccurrence, recordPerdecage, recordSmallCount, recordSparseValueWithPersistentHash, recordValue} from 'chrome://new-tab-page/new_tab_page.js';
+import {recordBoolean, recordDuration, recordEnumeration, recordLinearValue, recordLoadDuration, recordLogValue, recordOccurrence, recordPerdecage, recordSmallCount, recordSparseValueWithPersistentHash} from 'chrome://new-tab-page/new_tab_page.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import {assertEquals} from 'chrome://webui-test/chai_assert.js';
 import type {MetricsTracker} from 'chrome://webui-test/metrics_test_support.js';
@@ -49,20 +49,6 @@ suite('NewTabPageMetricsUtilsTest', () => {
     assertEquals(1, metrics.count('foo.metric', 5));
   });
 
-  test('recordValue', () => {
-    recordValue(
-        {
-          metricName: 'foo.metric',
-          type: chrome.metricsPrivate.MetricTypeType.HISTOGRAM_LINEAR,
-          min: 1,
-          max: 10,
-          buckets: 11,
-        },
-        5);
-    assertEquals(1, metrics.count('foo.metric'));
-    assertEquals(1, metrics.count('foo.metric', 5));
-  });
-
   test('recordEnumeration', () => {
     recordEnumeration('foo.metric', 5, 10);
     assertEquals(1, metrics.count('foo.metric'));
@@ -79,5 +65,27 @@ suite('NewTabPageMetricsUtilsTest', () => {
     recordBoolean('foo.metric', true);
     assertEquals(1, metrics.count('foo.metric'));
     assertEquals(1, metrics.count('foo.metric', true));
+  });
+
+  test('recordLogValue', () => {
+    recordLogValue(
+        'foo.metric',
+        /*min=*/ 1,
+        /*max=*/ 10,
+        /*buckets=*/ 11,
+        /*value=*/ 5);
+    assertEquals(1, metrics.count('foo.metric'));
+    assertEquals(1, metrics.count('foo.metric', 5));
+  });
+
+  test('recordLinearValue', () => {
+    recordLinearValue(
+        'foo.metric',
+        /*min=*/ 1,
+        /*max=*/ 10,
+        /*buckets=*/ 11,
+        /*value=*/ 5);
+    assertEquals(1, metrics.count('foo.metric'));
+    assertEquals(1, metrics.count('foo.metric', 5));
   });
 });
