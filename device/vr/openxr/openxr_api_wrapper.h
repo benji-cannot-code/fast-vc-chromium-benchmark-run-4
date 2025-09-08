c59a4390ebf8b8c04f521d89950b566363b55d97
Yong Li <yyonglii@xwf.google.com>
2025-09-08 06:23:47 -0700

    device/vr/openxr: Refactor for layers support
    This is a refactor adding an OpenXrCompositionLayer class
    and using it for the projection layer.
    
    BYPASS_LARGE_CHANGE_WARNING=a big refactor anyway
    BYPASS_RECITATION_REASON=false alarm see bug 441738495
    
    Change-Id: I9bcc247db72310106df9c8ec0d0d090dcefb79b3
    Bug: 409255534
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6792586
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Yong Li (xWF) <yyonglii@google.com>
    Cr-Commit-Position: refs/heads/main@{#1512343}

--- device/vr/openxr/openxr_api_wrapper.h
+++ device/vr/openxr/openxr_api_wrapper.h
@@ -135,6 +135,8 @@ class OpenXrApiWrapper {
   void PollFuture(XrFutureEXT future,
                   base::OnceCallback<void(XrFutureEXT)> on_ready_callback);
 
+  uint32_t GetRecommendedSwapchainSampleCount() const;
+
   static void DEVICE_VR_EXPORT SetTestHook(VRTestHook* hook);
 
  private:
@@ -173,10 +175,8 @@ class OpenXrApiWrapper {
   bool HasSystem() const;
   bool HasBlendMode() const;
   bool HasSession() const;
-  bool HasColorSwapChain() const;
   bool HasSpace(XrReferenceSpaceType type) const;
 
-  uint32_t GetRecommendedSwapchainSampleCount() const;
   void UpdateStageBounds();
   std::optional<gfx::Transform> GetLocalFromStage();
   std::optional<gfx::Transform> GetBaseSpaceFromSpace(
@@ -195,7 +195,6 @@ class OpenXrApiWrapper {
 
   bool ShouldCreateSharedImages() const;
   void CreateSharedMailboxes();
-  void ReleaseColorSwapchainImages();
 
   void SetXrSessionState(XrSessionState new_state);
 
@@ -241,10 +240,6 @@ class OpenXrApiWrapper {
 
   bool received_initial_valid_primary_views_ = false;
   uint64_t frames_before_initial_valid_primary_views_ = 0;
-
-  // The swapchain is initializd when a session begins and is re-created when
-  // the state of a secondary view configuration changes.
-  XrSwapchain color_swapchain_;
 
   // The rest of these objects store information about the current frame and are
   // updated each frame.
