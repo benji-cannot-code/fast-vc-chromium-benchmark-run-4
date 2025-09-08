294e2d72a705d6237b8a059087aa1c3e6db7a030
Tony Herre <toprice@chromium.org>
2025-09-08 02:48:23 -0700

    Remove unused encoded_insertable_streams_ ctr params
    Missed in crrev.com/c/5545619
    
    Bug: 40943169
    Change-Id: I71836b582541ce22d775be50042745e98a2b2a3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6921168
    Auto-Submit: Tony Herre <toprice@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Tony Herre <toprice@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1512290}

--- third_party/blink/renderer/modules/peerconnection/rtc_rtp_receiver_impl.cc
+++ third_party/blink/renderer/modules/peerconnection/rtc_rtp_receiver_impl.cc
@@ -146,7 +146,6 @@ class RTCRtpReceiverImpl::RTCRtpReceiverInternal
   RTCRtpReceiverInternal(webrtc::scoped_refptr<webrtc::PeerConnectionInterface>
                              native_peer_connection,
                          RtpReceiverState state,
-                         bool require_encoded_insertable_streams,
                          std::unique_ptr<webrtc::Metronome> decode_metronome)
       : native_peer_connection_(std::move(native_peer_connection)),
         main_task_runner_(state.main_task_runner()),
@@ -305,12 +304,10 @@ RTCRtpReceiverImpl::RTCRtpReceiverImpl(
     webrtc::scoped_refptr<webrtc::PeerConnectionInterface>
         native_peer_connection,
     RtpReceiverState state,
-    bool require_encoded_insertable_streams,
     std::unique_ptr<webrtc::Metronome> decode_metronome)
     : internal_(base::MakeRefCounted<RTCRtpReceiverInternal>(
           std::move(native_peer_connection),
           std::move(state),
-          require_encoded_insertable_streams,
           std::move(decode_metronome))) {}
 
 RTCRtpReceiverImpl::RTCRtpReceiverImpl(const RTCRtpReceiverImpl& other)
