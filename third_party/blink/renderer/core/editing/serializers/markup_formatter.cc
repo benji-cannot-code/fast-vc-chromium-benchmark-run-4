8521635a76b8ee643bb237ab0e0123938a9c8667
Michał Bentkowski <securitymb@google.com>
2025-09-08 16:25:09 -0700

    Reland "Cleanup after EscapeLtGtInAttributes experiment"
    This reverts commit 548fe0c403fcd7ef5614af3a1a3a60010f8546f9.
    
    Reason for reland: Enough time has passed, we didn't get any new complaints.
    
    Bug: 40747109
    Original change's description:
    > Revert "Cleanup after EscapeLtGtInAttributes experiment"
    >
    > This reverts commit 8d717b4f75bbbdb0f26a5aee9f431159d7a28070.
    >
    > Reason for revert: Because I failed to merge the original change in M138, it actually landed in M139. So I'm reverting this one to make sure that the cleanup will happen at M141 at the earliest.
    >
    > Bug: 40747109
    > Original change's description:
    > > Cleanup after EscapeLtGtInAttributes experiment
    > >
    > > The experiment to escape "<" and ">" in attributes has been successful
    > > and after https://crrev.com/c/6597054 it's been launched to everyone (in
    > > M138) and is a part of the spec (see
    > > https://github.com/whatwg/html/pull/6362).
    > >
    > > This CL does a cleanup changes the following: 1) Removes the use
    > > counter which is no longer necessary. 2) Updates markup_formatter.{cc,h}
    > > so that "<>" are always escaped. 3) Removes EscapeLtGtInAttributes as a
    > > runtime enabled feature
    > >
    > > Note that removing the usecounter forced change to the signature of
    > > `AppendAttributeValue` method -- it no longer needs to accept
    > > `Document`. So this CL changes more files so all call sites are changed
    > > accordingly.
    > >
    > > Because this has been launched in M138, it's now safe to remove in M140.
    > >
    > > Bug: 40747109
    > > Change-Id: If14151908eb74643fcbd2fa1d461f6002da3ccd7
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6714366
    > > Reviewed-by: Mason Freed <masonf@chromium.org>
    > > Reviewed-by: Mike West <mkwst@chromium.org>
    > > Commit-Queue: Michał Bentkowski <securitymb@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1484826}
    >
    > Bug: 40747109
    > Change-Id: I617c4a1159c33f4d30d52eea11eca52b58e64d26
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6769766
    > Reviewed-by: Mason Freed <masonf@chromium.org>
    > Commit-Queue: Michał Bentkowski <securitymb@google.com>
    > Reviewed-by: Mike West <mkwst@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1489988}
    
    Bug: 40747109
    Change-Id: I5995b31c1f82dc48fb44e289b9d0d6444b294dbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6905283
    Reviewed-by: Daniel Vogelheim <vogelheim@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Michał Bentkowski <securitymb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1512746}

--- third_party/blink/renderer/core/editing/serializers/markup_formatter.cc
+++ third_party/blink/renderer/core/editing/serializers/markup_formatter.cc
@@ -28,13 +28,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/editing/serializers/markup_formatter.h"
 
-#include "third_party/blink/public/mojom/use_counter/metrics/web_feature.mojom-shared.h"
 #include "third_party/blink/renderer/core/dom/cdata_section.h"
 #include "third_party/blink/renderer/core/dom/comment.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/document_fragment.h"
 #include "third_party/blink/renderer/core/dom/document_type.h"
-#include "third_party/blink/renderer/core/dom/node.h"
 #include "third_party/blink/renderer/core/dom/processing_instruction.h"
 #include "third_party/blink/renderer/core/editing/editing_utilities.h"
 #include "third_party/blink/renderer/core/editing/editor.h"
@@ -45,7 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/xlink_names.h"
 #include "third_party/blink/renderer/core/xml_names.h"
 #include "third_party/blink/renderer/core/xmlns_names.h"
-#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/weborigin/kurl.h"
 #include "third_party/blink/renderer/platform/wtf/text/character_names.h"
 #include "third_party/blink/renderer/platform/wtf/text/character_visitor.h"
@@ -211,19 +208,9 @@ void MarkupFormatter::AppendEndMarkup(StringBuilder& result,
 
 void MarkupFormatter::AppendAttributeValue(StringBuilder& result,
                                            const String& attribute,
-                                           bool document_is_html,
-                                           const Document& document) {
-  if (attribute.Contains('<') || attribute.Contains('>')) {
-    document.CountUse(mojom::blink::WebFeature::kAttributeValueContainsLtOrGt);
-  }
-
-  EntityMask entity_mask =
-      document_is_html
-          ? (RuntimeEnabledFeatures::EscapeLtGtInAttributesEnabled()
-                 ? kEntityExperimentalMaskInHTMLAttributeValue
-                 : kEntityMaskInHTMLAttributeValue)
-          : kEntityMaskInAttributeValue;
-
+                                           bool document_is_html) {
+  EntityMask entity_mask = document_is_html ? kEntityMaskInHTMLAttributeValue
+                                            : kEntityMaskInAttributeValue;
   AppendCharactersReplacingEntities(result, attribute, entity_mask);
 }
 
@@ -231,8 +218,7 @@ void MarkupFormatter::AppendAttribute(StringBuilder& result,
                                       const AtomicString& prefix,
                                       const AtomicString& local_name,
                                       const String& value,
-                                      bool document_is_html,
-                                      const Document& document) {
+                                      bool document_is_html) {
   result.Append(' ');
   if (!prefix.empty()) {
     result.Append(prefix);
@@ -240,7 +226,7 @@ void MarkupFormatter::AppendAttribute(StringBuilder& result,
   }
   result.Append(local_name);
   result.Append("=\"");
-  AppendAttributeValue(result, value, document_is_html, document);
+  AppendAttributeValue(result, value, document_is_html);
   result.Append('"');
 }
 
@@ -345,8 +331,7 @@ void MarkupFormatter::AppendStartTagClose(StringBuilder& result,
 
 void MarkupFormatter::AppendAttributeAsHTML(StringBuilder& result,
                                             const Attribute& attribute,
-                                            const String& value,
-                                            const Document& document) {
+                                            const String& value) {
   // https://html.spec.whatwg.org/C/#attribute's-serialised-name
   QualifiedName prefixed_name = attribute.GetName();
   if (attribute.NamespaceURI() == xmlns_names::kNamespaceURI) {
@@ -358,14 +343,13 @@ void MarkupFormatter::AppendAttributeAsHTML(StringBuilder& result,
     prefixed_name.SetPrefix(g_xlink_atom);
   }
   AppendAttribute(result, prefixed_name.Prefix(), prefixed_name.LocalName(),
-                  value, true, document);
+                  value, true);
 }
 
 void MarkupFormatter::AppendAttributeAsXMLWithoutNamespace(
     StringBuilder& result,
     const Attribute& attribute,
-    const String& value,
-    const Document& document) {
+    const String& value) {
   const AtomicString& attribute_namespace = attribute.NamespaceURI();
   AtomicString candidate_prefix = attribute.Prefix();
   if (attribute_namespace == xmlns_names::kNamespaceURI) {
@@ -378,8 +362,8 @@ void MarkupFormatter::AppendAttributeAsXMLWithoutNamespace(
     if (!candidate_prefix)
       candidate_prefix = g_xlink_atom;
   }
-  AppendAttribute(result, candidate_prefix, attribute.LocalName(), value, false,
-                  document);
+  AppendAttribute(result, candidate_prefix, attribute.LocalName(), value,
+                  false);
 }
 
 void MarkupFormatter::AppendCDATASection(StringBuilder& result,
