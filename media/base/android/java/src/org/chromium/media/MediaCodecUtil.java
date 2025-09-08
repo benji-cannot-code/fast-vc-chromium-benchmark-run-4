abf0a8b8eab4f0f27bd1f03779caf999681ccc1e
Jack Thiesen <jthiesen@chromium.org>
2025-09-08 13:55:33 -0700

    [Unnecessary SDK_INT >= Q] MediaCodecUtil.java
    Bug: 425948259
    Bug: 402822704
    Change-Id: I15ed0ff338baba1a83422911e00f7210a22e9906
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6920479
    Reviewed-by: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Commit-Queue: Jack Thiesen <jthiesen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1512646}

--- media/base/android/java/src/org/chromium/media/MediaCodecUtil.java
+++ media/base/android/java/src/org/chromium/media/MediaCodecUtil.java
@@ -57,11 +57,10 @@ class MediaCodecUtil {
     }
 
     /**
-     * Class to abstract platform version API differences for interacting with
-     * the MediaCodecList.
+     * Class to abstract platform version API differences for interacting with the MediaCodecList.
      */
     private static class MediaCodecListHelper implements Iterable<MediaCodecInfo> {
-        public MediaCodecListHelper() {
+        MediaCodecListHelper() {
             try {
                 mCodecList = new MediaCodecList(MediaCodecList.ALL_CODECS).getCodecInfos();
             } catch (Throwable e) {
@@ -120,9 +119,7 @@ class MediaCodecUtil {
 
     /** Return true if and only if info is a software codec. */
     public static boolean isSoftwareCodec(MediaCodecInfo info) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            if (!info.isHardwareAccelerated() || info.isSoftwareOnly()) return true;
-        }
+        if (!info.isHardwareAccelerated() || info.isSoftwareOnly()) return true;
 
         String name = info.getName().toLowerCase(Locale.ROOT);
 
