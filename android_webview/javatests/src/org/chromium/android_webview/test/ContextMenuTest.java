252785725cef0bd17f99cd528dafc774d001c10e
Jenna Himawan <jhimawan@google.com>
2025-09-08 11:53:17 -0700

    Don't use JNICallback in MenuModelBridge
    This fixes an error where the JNICallbacks were not being destroyed ("org.chromium.base.lifetime.LifetimeAssert$LifetimeAssertException: Object of type org.chromium.base.JniCallbackImpl was GC'ed without cleanup").
    
    Bug: 442958969
    Change-Id: I2b1ba500a4bb9358f3729044d4ccee23f5012fe7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6919190
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1512569}

--- android_webview/javatests/src/org/chromium/android_webview/test/ContextMenuTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/ContextMenuTest.java
@@ -606,7 +606,7 @@ public class ContextMenuTest extends AwParameterizedTest {
         final ContextMenuParams params =
                 new ContextMenuParams(
                         /* nativePtr= */ 0,
-                        new MenuModelBridge(),
+                        new MenuModelBridge(0L),
                         ContextMenuDataMediaType.NONE,
                         ContextMenuDataMediaFlags.MEDIA_NONE,
                         /* pageUrl= */ GURL.emptyGURL(),
@@ -897,7 +897,7 @@ public class ContextMenuTest extends AwParameterizedTest {
 
         return new ContextMenuParams(
                 0,
-                new MenuModelBridge(),
+                new MenuModelBridge(0L),
                 mediaType,
                 ContextMenuDataMediaFlags.MEDIA_NONE,
                 mPageUrl,
