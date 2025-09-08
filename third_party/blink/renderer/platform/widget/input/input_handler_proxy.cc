2b75dd37dc74d9348baea18ab6014e35afc9a358
Oksana Zhuravlova <oksamyt@chromium.org>
2025-09-08 08:34:42 -0700

    Add a param to skip 0 GSUs to SendEmptyGestureScrollUpdate
    This CL adds a `filter_out_empty_updates` field trial parameter for the
    SendEmptyGestureScrollUpdate feature, which, if set to true, causes the
    empty GestureScrollUpdates to be filtered out on the renderer side. This
    will allow us to estimate how the empty updates might affect the
    predictor output quality.
    
    Also adds back the field trial testing config.
    
    Bug: 402144902
    Change-Id: I59d28447e3f72b8cbdc663e1cd65b3166bc7aaba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6907874
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1512441}

--- third_party/blink/renderer/platform/widget/input/input_handler_proxy.cc
+++ third_party/blink/renderer/platform/widget/input/input_handler_proxy.cc
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/widget/input/input_handler_proxy_client.h"
 #include "third_party/blink/renderer/platform/widget/input/input_metrics.h"
 #include "third_party/blink/renderer/platform/widget/input/scroll_predictor.h"
+#include "ui/base/ui_base_features.h"
 #include "ui/events/types/scroll_input_type.h"
 #include "ui/gfx/geometry/point_conversions.h"
 #include "ui/latency/latency_info.h"
@@ -401,7 +402,16 @@ void InputHandlerProxy::HandleInputEventWithLatencyInfo(
       // dispatching if `event_with_callback` is too old, and if we expect a
       // newer input event to still arrive in time.
       enqueue_scroll_events_ = true;
-      if (scroll_predictor_) {
+
+      // To estimate the impact of empty GestureScrollUpdates on predictor
+      // output quality, some experiment arms will skip them.
+      const bool should_filter_out_event =
+          (::features::kSendEmptyGestureScrollUpdateFilterOutEmptyUpdates
+               .Get() &&
+           gesture_event.data.scroll_update.delta_x == 0 &&
+           gesture_event.data.scroll_update.delta_y == 0);
+
+      if (scroll_predictor_ && !should_filter_out_event) {
         std::unique_ptr<EventWithCallback> event_to_dispatch =
             scroll_predictor_->ResampleScrollEvents(
                 std::move(event_with_callback),
