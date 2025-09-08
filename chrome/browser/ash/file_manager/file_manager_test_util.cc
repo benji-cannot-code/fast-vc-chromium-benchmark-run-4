ebcfe482781d252bef446b91b5f54672f902650e
Xiyuan Xia <xiyuan@chromium.org>
2025-09-08 10:51:31 -0700

    Reland "extensions: Fix a race adding component extensions multiple times"
    This is a reland of commit c708d3922cbb162c0fe10898ec8eb9d2df4d474b
    
    Changes:
    Make `AddDefaultComponentExtensionsOnMainThread` wait for the
    pending kQuickOfficeComponentExtensionId install finish before
    adding it again.
    
    Original change's description:
    > extensions: Fix a race adding component extensions multiple times
    >
    > - Make `AddComponentFromDirWithManifestFilename` and
    >   `FinishAddComponentFromDir` track pending extension loads on the file
    >   task runner;
    > - Update `AddChromeOsSpeechSynthesisExtensions` and a couple other
    >   callers to check `IsPendingAdd` in addition to `Exists`;
    > - Add unit tests to verify the pending load is tracked as expected;
    > - Minor clean-ups;
    >
    > Bug: 442902361
    > Change-Id: I3deaed45449feddce2e8426a8bb2d1c7c1e46ee9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6916700
    > Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    > Reviewed-by: James Cook <jamescook@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1511591}
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Bug: 442902361
    Change-Id: I39a22fc2e41446f2d7547eecbb6e7cace6171ddb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6924413
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1512534}

--- chrome/browser/ash/file_manager/file_manager_test_util.cc
+++ chrome/browser/ash/file_manager/file_manager_test_util.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/to_string.h"
 #include "base/test/bind.h"
+#include "base/test/run_until.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_ash.h"
 #include "chrome/browser/ash/file_manager/file_tasks.h"
 #include "chrome/browser/ash/file_manager/fileapi_util.h"
@@ -142,7 +143,13 @@ void AddDefaultComponentExtensionsOnMainThread(Profile* profile) {
   component_loader->AddDefaultComponentExtensions(false);
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
   // QuickOffice loads from rootfs at /usr/share/chromeos-assets/quickoffce
-  // which does not exist on bots for tests, so load test version.
+  // which does not exist on bots for tests, so load test version after the
+  // pending load finishes (fail or not).
+  ASSERT_TRUE(base::test::RunUntil([&]() {
+    return !component_loader->IsPendingAdd(
+        extension_misc::kQuickOfficeComponentExtensionId);
+  }));
+
   base::FilePath data_dir;
   CHECK(base::PathService::Get(chrome::DIR_TEST_DATA, &data_dir));
   base::RunLoop run_loop;
@@ -150,7 +157,7 @@ void AddDefaultComponentExtensionsOnMainThread(Profile* profile) {
       data_dir.Append("chromeos/file_manager/quickoffice"),
       extension_misc::kQuickOfficeComponentExtensionId,
       extensions::kManifestFilename, extensions::kManifestFilename,
-      run_loop.QuitClosure());
+      run_loop.QuitClosure(), {});
   run_loop.Run();
 #endif
   // AddDefaultComponentExtensions() is normally invoked during
