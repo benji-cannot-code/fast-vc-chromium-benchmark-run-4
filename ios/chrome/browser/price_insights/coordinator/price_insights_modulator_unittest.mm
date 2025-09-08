adf3195b6618e715b914e0ed434684e8ecb9442c
Sylvain Defresne <sdefresne@chromium.org>
2025-09-08 08:49:16 -0700

    [ios] Convert tests to ProfileTestingFactory
    {Refcounted,}ProfileKeyedServiceFactoryIOS exposes a testing
    factory accepting a ProfileIOS* parameter, so convert tests
    to use this new API.
    
    This convert tests in //ios/chrome/browser/price_insights/coordinator.
    
    Bug: 436484783
    Change-Id: I6b5fae9dbb0aff533955371d667af349123212b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6919777
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Gayane Petrosyan <gayane@google.com>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Gayane Petrosyan <gayane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1512455}

--- ios/chrome/browser/price_insights/coordinator/price_insights_modulator_unittest.mm
+++ ios/chrome/browser/price_insights/coordinator/price_insights_modulator_unittest.mm
@@ -55,7 +55,7 @@ class PriceInsightsModulatorTest : public PlatformTest {
     builder.AddTestingFactory(
         commerce::ShoppingServiceFactory::GetInstance(),
         base::BindRepeating(
-            [](web::BrowserState*) -> std::unique_ptr<KeyedService> {
+            [](ProfileIOS* profile) -> std::unique_ptr<KeyedService> {
               return commerce::MockShoppingService::Build();
             }));
 
