37d306df0bd22ca77049d8820527cc09ee941f16
Sylvain Defresne <sdefresne@chromium.org>
2025-09-08 08:29:23 -0700

    [ios] Convert tests to ProfileTestingFactory
    {Refcounted,}ProfileKeyedServiceFactoryIOS exposes a testing
    factory accepting a ProfileIOS* parameter, so convert tests
    to use this new API.
    
    This convert tests in //ios/chrome/browser/sync/model.
    
    Bug: 436484783
    Change-Id: I6038cc04d878a53ff3ce564fa176fef11dcef595
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6923489
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1512433}

--- ios/chrome/browser/sync/model/mock_sync_service_utils.cc
+++ ios/chrome/browser/sync/model/mock_sync_service_utils.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ios/chrome/browser/shared/model/profile/profile_ios.h"
 
-std::unique_ptr<KeyedService> CreateMockSyncService(
-    web::BrowserState* context) {
+std::unique_ptr<KeyedService> CreateMockSyncService(ProfileIOS* profile) {
   return std::make_unique<testing::NiceMock<syncer::MockSyncService>>();
 }
--- ios/chrome/browser/sync/model/mock_sync_service_utils.h
+++ ios/chrome/browser/sync/model/mock_sync_service_utils.h
@@ -10,6 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/shared/model/profile/profile_ios.h"
 
 // Returns a basic MockSyncService.
-std::unique_ptr<KeyedService> CreateMockSyncService(web::BrowserState* context);
+std::unique_ptr<KeyedService> CreateMockSyncService(ProfileIOS* profile);
 
 #endif  // IOS_CHROME_BROWSER_SYNC_MODEL_MOCK_SYNC_SERVICE_UTILS_H_
