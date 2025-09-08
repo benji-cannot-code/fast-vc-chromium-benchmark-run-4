e514966b286758d18cf75f58f4fdc5ab6d84cb2a
Sylvain Defresne <sdefresne@chromium.org>
2025-09-08 08:35:53 -0700

    [ios] Convert tests to ProfileTestingFactory
    {Refcounted,}ProfileKeyedServiceFactoryIOS exposes a testing
    factory accepting a ProfileIOS* parameter, so convert tests
    to use this new API.
    
    This convert tests in //ios/chrome/browser/saved_tab_groups/model.
    
    Bug: 436484783
    Change-Id: I988a6e2fdd4ca7d3ee8718e77444bd57e651d9d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6917253
    Commit-Queue: Louis Romero <lpromero@google.com>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1512443}

--- ios/chrome/browser/saved_tab_groups/model/ios_tab_group_sync_util_unittest.mm
+++ ios/chrome/browser/saved_tab_groups/model/ios_tab_group_sync_util_unittest.mm
@@ -42,8 +42,7 @@ namespace utils {
 namespace {
 
 // Creates a MockTabGroupSyncService.
-std::unique_ptr<KeyedService> CreateMockSyncService(
-    web::BrowserState* context) {
+std::unique_ptr<KeyedService> CreateMockSyncService(ProfileIOS* profile) {
   return std::make_unique<::testing::NiceMock<MockTabGroupSyncService>>();
 }
 
