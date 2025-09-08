cf91701528ff80f7ddef847f6911fb3f1d793ed8
Keren Zhu <kerenzhu@chromium.org>
2025-09-08 10:10:00 -0700

    mac: remove hit test target overriding on TabTitlebarView removal
    There are events arriving at TabTitlebarView even thought it is not in
    the fullscreen toolbar window (NSToolbarFullScreenWindow). This triggers
    a CHECK failure. This might be due to a race condition between event
    dispatch and title bar view removal.
    
    This CL addresses the issue by resetting the hit testing overriding when
    the title bar view is removed from the fullscreen toolbar window.
    
    Bug: 443291277
    Change-Id: I25e0b1cdf809f475ceaf297d00798316badadffe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6918879
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1512507}

--- components/remote_cocoa/app_shim/immersive_mode_tabbed_controller_cocoa.mm
+++ components/remote_cocoa/app_shim/immersive_mode_tabbed_controller_cocoa.mm
@@ -20,12 +20,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @end
 
 @implementation TabTitlebarView
+- (NSView*)tabContentView {
+  CHECK(self.subviews.count > 0);
+  return base::apple::ObjCCastStrict<BridgedContentView>(self.subviews[0]);
+}
+
 - (void)viewDidMoveToWindow {
+  // During transition from regular to fullscreen, the TabTitlebarView is first
+  // added to the main browser's NSWindow, then moved to
+  // NSToolbarFullscreenWindow. Only NSToolbarFullscreenWindow needs hit test
+  // overriding.
   if (remote_cocoa::IsNSToolbarFullScreenWindow(self.window)) {
-    CHECK(self.subviews.count > 0);
-    NSView* tab_content_view =
-        base::apple::ObjCCastStrict<BridgedContentView>(self.subviews[0]);
-    remote_cocoa::SetNSNextStepFrameHitTestTargetView(tab_content_view);
+    remote_cocoa::SetNSNextStepFrameHitTestTargetView(self.window,
+                                                      self.tabContentView);
+  }
+}
+
+- (void)viewWillMoveToWindow:(NSWindow*)newWindow {
+  // Removing from NSToolbarFullscreenWindow, hence reset hit test overriding.
+  if (!newWindow && remote_cocoa::IsNSToolbarFullScreenWindow(self.window)) {
+    remote_cocoa::SetNSNextStepFrameHitTestTargetView(self.window, nil);
   }
 }
 @end
