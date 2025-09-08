a19ded06448df35acb5860cd4494d66313c57e6d
Jon Toohill <jtoohill@google.com>
2025-09-08 01:14:03 -0700

    Log TokenEvents for recycled tokens
    Adds a separate event type to measure previously orphaned tokens that
    are successfully recycled into a new cache. The delta between the
    number of orphaned tokens and the number of recycled tokens tells us
    how many tokens are lost due to browser shutdown.
    
    Bug: 435240740
    Change-Id: I6a6a6964f75b797a0164f7bf0f554a628f0304e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6898194
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Auto-Submit: Jon Toohill <jtoohill@google.com>
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1512260}

--- components/ip_protection/common/ip_protection_token_manager_impl_unittest.cc
+++ components/ip_protection/common/ip_protection_token_manager_impl_unittest.cc
@@ -41,8 +41,10 @@ namespace ip_protection {
 
 namespace {
 
+using ::base::Bucket;
 using ::testing::ElementsAre;
 using ::testing::Pair;
+using ::testing::UnorderedElementsAre;
 
 constexpr char kGeoChangeTokenPresence[] =
     "NetworkService.IpProtection.GeoChangeTokenPresence";
@@ -74,6 +76,8 @@ constexpr char kProxyBTokenCountExpiredHistogram[] =
     "NetworkService.IpProtection.ProxyB.TokenCount.Expired";
 constexpr char kProxyATokenCountOrphanedHistogram[] =
     "NetworkService.IpProtection.ProxyA.TokenCount.Orphaned";
+constexpr char kProxyATokenCountRecycledHistogram[] =
+    "NetworkService.IpProtection.ProxyA.TokenCount.Recycled";
 
 constexpr base::TimeDelta kTokenLimitExceededDelay = base::Minutes(10);
 constexpr base::TimeDelta kTokenRateMeasurementInterval = base::Minutes(5);
@@ -1378,6 +1382,13 @@ TEST_F(IpProtectionTokenManagerImplTest, InitialTokens) {
 
   EXPECT_TRUE(token_manager->WasTokenCacheEverFilled());
   EXPECT_TRUE(token_manager->IsAuthTokenAvailable(kMountainViewGeoId));
+  EXPECT_THAT(
+      histogram_tester_.GetAllSamplesForPrefix(
+          "NetworkService.IpProtection.ProxyA.TokenCount."),
+      UnorderedElementsAre(
+          Pair(kProxyATokenCountRecycledHistogram, BucketsAre(Bucket(1, 1))),
+          Pair(kProxyATokenCountExpiredHistogram, BucketsAre(Bucket(1, 1)))));
+
   auto token = token_manager->GetAuthToken(kMountainViewGeoId);
   ASSERT_TRUE(token);
   EXPECT_EQ(token->token, "good-token");
@@ -1398,6 +1409,10 @@ TEST_F(IpProtectionTokenManagerImplTest, InitialTokensAllExpired) {
 
   EXPECT_FALSE(token_manager->WasTokenCacheEverFilled());
   EXPECT_FALSE(token_manager->IsAuthTokenAvailable(kMountainViewGeoId));
+  EXPECT_THAT(histogram_tester_.GetAllSamplesForPrefix(
+                  "NetworkService.IpProtection.ProxyA.TokenCount."),
+              UnorderedElementsAre(Pair(kProxyATokenCountExpiredHistogram,
+                                        BucketsAre(Bucket(2, 1)))));
 }
 
 TEST_F(IpProtectionTokenManagerImplTest, InitialTokensSkipsPrefill) {
