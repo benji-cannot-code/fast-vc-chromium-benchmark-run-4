681afe85248c99827cd806e7f8eee773bd8c89ee
Michael Wu <mklw@google.com>
2025-09-08 13:14:21 -0700

    Update context menu from web content to show Incognito window
    Currently, long-pressing a link or image will trigger a context menu
    where you can open the resource in an incognito tab. We change this
    entry point from an incognito tab to an incognito window. This change is
    part of a larger effort to create "Desktop-like Incognito Windows on
    Clank".
    
    NO_IFTTT=Change was added to enums.xml and linted file
    
    Screencast: http://shortn/_i8uw1FFJ2O
    Bug: 435489282
    Change-Id: I6e77178a1a7fa1b47d251f9f29aff464c771f9e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6881842
    Commit-Queue: Michael Wu <mklw@google.com>
    Reviewed-by: Shu Yang <shuyng@google.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1512626}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ChromeContextMenuPopulatorTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ChromeContextMenuPopulatorTest.java
@@ -331,6 +331,7 @@ public class ChromeContextMenuPopulatorTest {
     @Test
     @SmallTest
     @UiThreadTest
+    @DisableFeatures(ChromeFeatureList.ANDROID_OPEN_INCOGNITO_AS_WINDOW)
     public void testHttpLink() {
         FirstRunStatus.setFirstRunFlowComplete(false);
         ContextMenuParams params = getHttpLinkParams();
@@ -419,6 +420,7 @@ public class ChromeContextMenuPopulatorTest {
     @Test
     @SmallTest
     @UiThreadTest
+    @DisableFeatures(ChromeFeatureList.ANDROID_OPEN_INCOGNITO_AS_WINDOW)
     public void testHttpLinkWithDownloadBlockedByPolicy() {
         FirstRunStatus.setFirstRunFlowComplete(true);
         DownloadUtils.setIsDownloadRestrictedByPolicyForTesting(true);
@@ -495,6 +497,7 @@ public class ChromeContextMenuPopulatorTest {
     @Test
     @SmallTest
     @UiThreadTest
+    @DisableFeatures(ChromeFeatureList.ANDROID_OPEN_INCOGNITO_AS_WINDOW)
     public void testHttpLinkWithPreviewTabEnabled() {
         ContextMenuParams params =
                 new ContextMenuParams(
@@ -572,6 +575,7 @@ public class ChromeContextMenuPopulatorTest {
     @Test
     @SmallTest
     @UiThreadTest
+    @DisableFeatures(ChromeFeatureList.ANDROID_OPEN_INCOGNITO_AS_WINDOW)
     public void testDataUrlDisablesPreviewTab() {
         ContextMenuParams params =
                 new ContextMenuParams(
@@ -808,6 +812,7 @@ public class ChromeContextMenuPopulatorTest {
     @Test
     @SmallTest
     @UiThreadTest
+    @DisableFeatures(ChromeFeatureList.ANDROID_OPEN_INCOGNITO_AS_WINDOW)
     public void testVideoLink() {
         FirstRunStatus.setFirstRunFlowComplete(false);
         GURL sourceUrl = new GURL("http://www.blah.com/");
@@ -918,6 +923,7 @@ public class ChromeContextMenuPopulatorTest {
     @Test
     @SmallTest
     @UiThreadTest
+    @DisableFeatures(ChromeFeatureList.ANDROID_OPEN_INCOGNITO_AS_WINDOW)
     public void testVideoLinkWithDownloadBlockedByPolicy() {
         FirstRunStatus.setFirstRunFlowComplete(true);
         DownloadUtils.setIsDownloadRestrictedByPolicyForTesting(true);
@@ -1164,6 +1170,7 @@ public class ChromeContextMenuPopulatorTest {
     @Test
     @SmallTest
     @UiThreadTest
+    @DisableFeatures(ChromeFeatureList.ANDROID_OPEN_INCOGNITO_AS_WINDOW)
     public void testHttpLinkWithImageHiFi() {
         FirstRunStatus.setFirstRunFlowComplete(false);
         ContextMenuParams params =
@@ -1340,6 +1347,7 @@ public class ChromeContextMenuPopulatorTest {
     @Test
     @SmallTest
     @UiThreadTest
+    @DisableFeatures(ChromeFeatureList.ANDROID_OPEN_INCOGNITO_AS_WINDOW)
     public void testReadLater() {
         FirstRunStatus.setFirstRunFlowComplete(true);
 
@@ -1504,6 +1512,7 @@ public class ChromeContextMenuPopulatorTest {
     @Test
     @SmallTest
     @UiThreadTest
+    @DisableFeatures(ChromeFeatureList.ANDROID_OPEN_INCOGNITO_AS_WINDOW)
     public void testOpenInOtherWindow() {
         FirstRunStatus.setFirstRunFlowComplete(true);
 
@@ -1559,47 +1568,17 @@ public class ChromeContextMenuPopulatorTest {
     @Test
     @SmallTest
     @UiThreadTest
+    @DisableFeatures(ChromeFeatureList.ANDROID_OPEN_INCOGNITO_AS_WINDOW)
     public void testOpenInNewWindow() {
-        FirstRunStatus.setFirstRunFlowComplete(true);
+        checkOpenInNewWindowItems(/* isIncognitoWindowFeatureEnabled= */ false);
+    }
 
-        ContextMenuParams params =
-                new ContextMenuParams(
-                        0,
-                        mMenuModelBridge,
-                        ContextMenuDataMediaType.NONE,
-                        ContextMenuDataMediaFlags.MEDIA_NONE,
-                        new GURL(PAGE_URL),
-                        new GURL(LINK_URL),
-                        LINK_TEXT,
-                        GURL.emptyGURL(),
-                        GURL.emptyGURL(),
-                        "",
-                        null,
-                        false,
-                        0,
-                        0,
-                        MenuSourceType.TOUCH,
-                        false,
-                        /* openedFromInterestFor= */ false,
-                        /* interestForNodeID= */ 0,
-                        /* additionalNavigationParams= */ null);
-
-        when(mItemDelegate.canEnterMultiWindowMode()).thenReturn(true);
-        initializePopulator(ChromeContextMenuPopulator.ContextMenuMode.NORMAL, params);
-        doReturn(true).when(mPopulator).isTabletScreen();
-        int[] expectedMultiWindow = {
-            R.id.contextmenu_open_in_new_tab,
-            R.id.contextmenu_open_in_new_tab_in_group,
-            R.id.contextmenu_open_in_incognito_tab,
-            R.id.contextmenu_open_in_new_window,
-            R.id.contextmenu_open_in_ephemeral_tab,
-            R.id.contextmenu_copy_link_address,
-            R.id.contextmenu_copy_link_text,
-            R.id.contextmenu_save_link_as,
-            R.id.contextmenu_read_later,
-            R.id.contextmenu_share_link
-        };
-        checkMenuOptions(expectedMultiWindow);
+    @Test
+    @SmallTest
+    @UiThreadTest
+    @EnableFeatures(ChromeFeatureList.ANDROID_OPEN_INCOGNITO_AS_WINDOW)
+    public void testOpenInNewWindow_incognitoWindowEnabled() {
+        checkOpenInNewWindowItems(/* isIncognitoWindowFeatureEnabled= */ true);
     }
 
     @Test
@@ -2573,5 +2552,38 @@ public class ChromeContextMenuPopulatorTest {
             }
         }
         return null;
+    }
+
+    private void checkOpenInNewWindowItems(boolean isIncognitoWindowFeatureEnabled) {
+        FirstRunStatus.setFirstRunFlowComplete(true);
+        ContextMenuParams params = getHttpLinkParams();
+
+        when(mItemDelegate.isIncognito()).thenReturn(false);
+        when(mItemDelegate.isIncognitoSupported()).thenReturn(true);
+        when(mItemDelegate.canEnterMultiWindowMode()).thenReturn(true);
+        initializePopulator(ChromeContextMenuPopulator.ContextMenuMode.NORMAL, params);
+        doReturn(true).when(mPopulator).isTabletScreen();
+
+        List<Integer> expectedItems = new ArrayList<>();
+        expectedItems.add(R.id.contextmenu_open_in_new_tab);
+        expectedItems.add(R.id.contextmenu_open_in_new_tab_in_group);
+
+        if (isIncognitoWindowFeatureEnabled) {
+            expectedItems.add(R.id.contextmenu_open_in_new_window);
+            expectedItems.add(R.id.contextmenu_open_in_incognito_window);
+        } else {
+            expectedItems.add(R.id.contextmenu_open_in_incognito_tab);
+            expectedItems.add(R.id.contextmenu_open_in_new_window);
+        }
+
+        expectedItems.add(R.id.contextmenu_open_in_ephemeral_tab);
+        expectedItems.add(R.id.contextmenu_copy_link_address);
+        expectedItems.add(R.id.contextmenu_copy_link_text);
+        expectedItems.add(R.id.contextmenu_save_link_as);
+        expectedItems.add(R.id.contextmenu_read_later);
+        expectedItems.add(R.id.contextmenu_share_link);
+
+        int[] expectedItemsArray = expectedItems.stream().mapToInt(Integer::intValue).toArray();
+        checkMenuOptions(expectedItemsArray);
     }
 }
