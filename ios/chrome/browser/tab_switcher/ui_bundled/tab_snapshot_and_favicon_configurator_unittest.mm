d113911cfc2a8052e3104144d5a18df71b578542
Sylvain Defresne <sdefresne@chromium.org>
2025-09-08 08:28:00 -0700

    [ios] Convert tests to ProfileTestingFactory
    {Refcounted,}ProfileKeyedServiceFactoryIOS exposes a testing
    factory accepting a ProfileIOS* parameter, so convert tests
    to use this new API.
    
    This convert tests in //ios/chrome/browser/tab_switcher/ui_bundled.
    
    Bug: 436484783
    Change-Id: I13f9cd04f01ffabf7714cffc4b686647d5f89097
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6919779
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1512431}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_snapshot_and_favicon_configurator_unittest.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_snapshot_and_favicon_configurator_unittest.mm
@@ -31,8 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // Builds a `TestFaviconLoader`.
-std::unique_ptr<KeyedService> BuildTestFaviconLoader(
-    web::BrowserState* context) {
+std::unique_ptr<KeyedService> BuildTestFaviconLoader(ProfileIOS* profile) {
   return std::make_unique<TestFaviconLoader>();
 }
 
