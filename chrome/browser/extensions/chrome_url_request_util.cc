eb32f0f2c347c60b02b2d34ec29d22e36bdbd956
Devlin Cronin <rdevlin.cronin@chromium.org>
2025-09-08 13:23:29 -0700

    [Extensions] Fix devtools accesible resources issue
    There's a bug where resources of devtools extensions are made
    improperly available. Fix it, and add a regression test.
    
    Bug: 428397712
    Change-Id: Ib39ecbe64c571340ec365aa118faca43b643cd8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6901326
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1512635}

--- chrome/browser/extensions/chrome_url_request_util.cc
+++ chrome/browser/extensions/chrome_url_request_util.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_view_util.h"
 #include "base/task/thread_pool.h"
 #include "chrome/common/chrome_paths.h"
+#include "content/public/common/url_constants.h"
 #include "extensions/browser/component_extension_resource_manager.h"
 #include "extensions/browser/extension_protocols.h"
 #include "extensions/browser/extensions_browser_client.h"
@@ -267,13 +268,16 @@ bool AllowCrossRendererResourceLoad(
     return true;
   }
 
-  // If there aren't any explicitly marked web accessible resources, the
-  // load should be allowed only if it is by DevTools. A close approximation is
-  // checking if the extension contains a DevTools page.
   if (extension &&
       !chrome_manifest_urls::GetDevToolsPage(extension).is_empty()) {
-    *allowed = true;
-    return true;
+    // Allow the load if the initiator is either a devtools origin, or if
+    // there is no initiator (in which case it was likely a browser-initiated
+    // request).
+    if (!request.request_initiator ||
+        request.request_initiator->scheme() == content::kChromeDevToolsScheme) {
+      *allowed = true;
+      return true;
+    }
   }
 
   // Couldn't determine if the resource is allowed or not.
