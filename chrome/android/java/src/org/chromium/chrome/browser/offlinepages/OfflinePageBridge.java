d4d53333550441e23d050610469c4ca5cefd3c76
Jack Thiesen <jthiesen@chromium.org>
2025-09-08 15:13:16 -0700

    [Unnecessary SDK_INT >= Q] Update File Access Requests
    This change cleans up unnecessary references to old versions. From doing so, it is clear that some of the logic exists only for pre-Q APIs and those are cleaned up as well.
    
    Bug: 425948259
    Bug: 402822704
    Change-Id: Ib695c5a7c279972f4b73e2f987e8f543032ec3ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6914599
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Jack Thiesen <jthiesen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1512692}

--- chrome/android/java/src/org/chromium/chrome/browser/offlinepages/OfflinePageBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/offlinepages/OfflinePageBridge.java
@@ -585,17 +585,6 @@ public class OfflinePageBridge {
                 .isShowingTrustedOfflinePage(mNativeOfflinePageBridge, webContents);
     }
 
-    /**
-     * Tries to acquire the storage access permssion if not yet.
-     *
-     * @param webContents Contents of the page to check.
-     * @param callback Callback to notify the result.
-     */
-    public void acquireFileAccessPermission(WebContents webContents, Callback<Boolean> callback) {
-        org.chromium.chrome.browser.offlinepages.OfflinePageBridgeJni.get()
-                .acquireFileAccessPermission(mNativeOfflinePageBridge, webContents, callback);
-    }
-
     @CalledByNative
     protected void offlinePageModelLoaded() {
         mIsNativeOfflinePageModelLoaded = true;
@@ -727,7 +716,7 @@ public class OfflinePageBridge {
         void getAllPages(
                 long nativeOfflinePageBridge,
                 List<OfflinePageItem> offlinePages,
-                final Callback<List<OfflinePageItem>> callback);
+                Callback<List<OfflinePageItem>> callback);
 
         void willCloseTab(long nativeOfflinePageBridge, WebContents webContents);
 
@@ -830,8 +819,5 @@ public class OfflinePageBridge {
                 long nativeOfflinePageBridge,
                 @JniType("std::string") String url,
                 Callback<LoadUrlParams> callback);
-
-        void acquireFileAccessPermission(
-                long nativeOfflinePageBridge, WebContents webContents, Callback<Boolean> callback);
     }
 }
--- chrome/browser/download/android/download_controller.h
+++ chrome/browser/download/android/download_controller.h
@@ -42,9 +42,6 @@ class DownloadController : public DownloadControllerBase {
   DownloadController& operator=(const DownloadController&) = delete;
 
   // DownloadControllerBase implementation.
-  void AcquireFileAccessPermission(
-      const content::WebContents::Getter& wc_getter,
-      AcquireFileAccessPermissionCallback callback) override;
   void CreateAndroidDownload(const content::WebContents::Getter& wc_getter,
                              const DownloadInfo& info) override;
 
@@ -100,10 +97,6 @@ class DownloadController : public DownloadControllerBase {
   // Helper methods to start android download on UI thread.
   void StartAndroidDownload(const content::WebContents::Getter& wc_getter,
                             const DownloadInfo& info);
-  void StartAndroidDownloadInternal(
-      const content::WebContents::Getter& wc_getter,
-      const DownloadInfo& info,
-      bool allowed);
 
   // Get profile key from download item.
   ProfileKey* GetProfileKey(download::DownloadItem* download_item);
