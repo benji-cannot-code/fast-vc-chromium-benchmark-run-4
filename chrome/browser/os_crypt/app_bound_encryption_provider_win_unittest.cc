b3b08f291c4eac2c5aeff26afe7d7e1262003fa6
Will Harris <wfh@chromium.org>
2025-09-08 11:55:44 -0700

    Fix flaky AppBoundEncryptionProviderTest.Basic test
    This was caused by another test in the same fixture not correctly
    resetting the overrides.
    
    This glitch is fixed and to avoid this happening again a ScopedOverrides
    helper class is created.
    
    BUG=443701345
    
    Change-Id: I8607739c4d643d4ff15d0d452b8435d9d257b38c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6924383
    Commit-Queue: Will Harris <wfh@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1512572}

--- chrome/browser/os_crypt/app_bound_encryption_provider_win_unittest.cc
+++ chrome/browser/os_crypt/app_bound_encryption_provider_win_unittest.cc
@@ -54,6 +54,16 @@ class MockAppBoundEncryptionOverrides
               (override));
 };
 
+class ScopedOverridesForTesting {
+ public:
+  explicit ScopedOverridesForTesting(
+      AppBoundEncryptionOverridesForTesting& overrides) {
+    SetOverridesForTesting(&overrides);
+  }
+
+  ~ScopedOverridesForTesting() { SetOverridesForTesting(nullptr); }
+};
+
 HRESULT DefaultEncrypt(ProtectionLevel level,
                        const std::string& plaintext,
                        std::string& ciphertext,
@@ -143,7 +153,7 @@ TEST_F(AppBoundEncryptionProviderTest, InvalidKeyRegenerated) {
   base::test::TaskEnvironment env;
   ::testing::StrictMock<MockAppBoundEncryptionOverrides> mock_app_bound;
 
-  SetOverridesForTesting(&mock_app_bound);
+  ScopedOverridesForTesting overrides(mock_app_bound);
 
   ON_CALL(mock_app_bound, EncryptAppBoundString)
       .WillByDefault(::testing::Invoke(DefaultEncrypt));
@@ -205,7 +215,7 @@ TEST_F(AppBoundEncryptionProviderTest, Basic) {
   base::test::TaskEnvironment env;
   ::testing::StrictMock<MockAppBoundEncryptionOverrides> mock_app_bound;
 
-  SetOverridesForTesting(&mock_app_bound);
+  ScopedOverridesForTesting overrides(mock_app_bound);
 
   ON_CALL(mock_app_bound, EncryptAppBoundString)
       .WillByDefault(::testing::Invoke(DefaultEncrypt));
@@ -424,8 +434,6 @@ TEST_F(AppBoundEncryptionProviderTest, Basic) {
     // The key returned should be same as the new one generated in part 4.
     EXPECT_EQ(*key, *new_encryption_key);
   }
-
-  SetOverridesForTesting(nullptr);
 }
 
 }  // namespace os_crypt
