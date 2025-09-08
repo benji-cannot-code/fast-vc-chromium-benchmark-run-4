d2c04b87702125c2b9449951cd554966c15c7953
Marke Hallowell <wry@chromium.org>
2025-09-08 14:20:25 -0700

    Add empty focused tab manager implementation.
    Implements an empty focused tab manager for cases when no auto
    focus behavior is desired.
    
    Bug: 441552043
    Change-Id: I03e01e02c83b5b47fce178fb9ffb4becccdb321d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6925222
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Marke Hallowell <wry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1512658}

---
+++ chrome/browser/glic/host/context/glic_empty_focused_tab_manager.cc
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/glic/host/context/glic_empty_focused_tab_manager.h"
+
+namespace glic {
+
+GlicEmptyFocusedTabManager::GlicEmptyFocusedTabManager() = default;
+
+GlicEmptyFocusedTabManager::~GlicEmptyFocusedTabManager() = default;
+
+base::CallbackListSubscription
+GlicEmptyFocusedTabManager::AddFocusedTabChangedCallback(
+    FocusedTabChangedCallback callback) {
+  return {};
+}
+
+base::CallbackListSubscription
+GlicEmptyFocusedTabManager::AddFocusedTabDataChangedCallback(
+    FocusedTabDataChangedCallback callback) {
+  return {};
+}
+
+bool GlicEmptyFocusedTabManager::IsTabFocused(
+    tabs::TabHandle tab_handle) const {
+  return false;
+}
+
+FocusedTabData GlicEmptyFocusedTabManager::GetFocusedTabData() {
+  return FocusedTabData(std::string("no focusable tab"), nullptr);
+}
+
+}  // namespace glic
