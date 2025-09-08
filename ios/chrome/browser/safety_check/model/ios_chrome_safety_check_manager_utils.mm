21287e17ff7d090c386c39286cd1b58254e270a2
Filipa Senra <fsenra@google.com>
2025-09-08 06:31:54 -0700

    [iOS][SH] Make interval between background checks finch configurable
    We will start experimenting with shorter internals between background
    checks for Safety Hub. This CL makes the interval finch configurable in
    iOS.
    
    Bug: 383479015
    Change-Id: Id034ec37f0511e79a2e2aaed612bbef92161c49a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6696009
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Filipa Senra <fsenra@google.com>
    Reviewed-by: Side YILMAZ <sideyilmaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1512346}

--- ios/chrome/browser/safety_check/model/ios_chrome_safety_check_manager_utils.mm
+++ ios/chrome/browser/safety_check/model/ios_chrome_safety_check_manager_utils.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/safety_check/model/ios_chrome_safety_check_manager_utils.h"
 
 #import "base/check.h"
+#import "components/safety_check/features.h"
 #import "ios/chrome/browser/passwords/model/ios_chrome_password_check_manager.h"
 #import "ios/chrome/browser/safety_check/model/ios_chrome_safety_check_manager_constants.h"
 #import "ios/chrome/browser/shared/model/prefs/pref_names.h"
@@ -102,8 +103,10 @@ bool CanAutomaticallyRunSafetyCheck(std::optional<base::Time> last_run_time) {
   }
 
   base::TimeDelta last_run_age = base::Time::Now() - last_run_time.value();
+  const base::TimeDelta update_interval_in_days = base::Days(
+      safety_check::features::kBackgroundPasswordCheckInterval.Get().InDays());
 
-  return last_run_age > kSafetyCheckAutorunDelay;
+  return last_run_age > update_interval_in_days;
 }
 
 std::optional<base::Time> GetLatestSafetyCheckRunTimeAcrossAllEntrypoints(
