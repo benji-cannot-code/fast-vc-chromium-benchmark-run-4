f09072726044ea3b5b3a397ef093f95dfc94e746
Adem Derinel <derinel@google.com>
2025-09-08 09:10:32 -0700

    Remove unused forward declaration of EnclaveManagerObserver
    Change-Id: Ib208039671877df1be311d2b46ae922b52b4593b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6923727
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Auto-Submit: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1512473}

--- chrome/browser/webauthn/chrome_authenticator_request_delegate.h
+++ chrome/browser/webauthn/chrome_authenticator_request_delegate.h
@@ -220,8 +220,6 @@ class ChromeAuthenticatorRequestDelegate
   FRIEND_TEST_ALL_PREFIXES(ChromeAuthenticatorRequestDelegatePrivateTest,
                            ShouldCreateInICloudKeychain);
 
-  class EnclaveManagerObserver;
-
   content::BrowserContext* GetBrowserContext() const;
   Profile* profile() const;
 
