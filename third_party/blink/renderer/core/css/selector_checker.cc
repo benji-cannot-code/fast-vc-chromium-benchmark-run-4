64c02389d7f868d6d4e4fc65dd7d3c4ac9ed172b
Steve Kobes <skobes@chromium.org>
2025-09-09 11:58:56 -0700

    ScopedVT: Support :active-view-transition[-type] on scope.
    Bug: 443707076
    Change-Id: I6c29fdb7fee5a8d17c2c826d38495b119b6c19a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6927610
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513215}

--- third_party/blink/renderer/core/css/selector_checker.cc
+++ third_party/blink/renderer/core/css/selector_checker.cc
@@ -492,6 +492,13 @@ bool NeedsScopeActivation(
                                  context.selector->IsLastInComplexSelector());
 }
 
+ViewTransition* GetTransitionForScope(const Element& element) {
+  if (element.IsPseudoElement()) {
+    return nullptr;
+  }
+  return ViewTransitionUtils::GetTransition(element);
+}
+
 }  // namespace
 
 SelectorChecker::FeaturelessMatch
@@ -2870,14 +2877,8 @@ bool SelectorChecker::CheckPseudoClass(const SelectorCheckingContext& context,
       DCHECK(context.relative_anchor_element);
       return context.relative_anchor_element == &element;
     case CSSSelector::kPseudoActiveViewTransition: {
-      // :active-view-transition is only valid on the document element.
-      if (!element.IsDocumentElement()) {
-        return false;
-      }
-
       // The pseudo is only valid if there is a transition.
-      auto* transition =
-          ViewTransitionUtils::GetTransition(element.GetDocument());
+      auto* transition = GetTransitionForScope(element);
       if (!transition) {
         return false;
       }
@@ -2886,14 +2887,8 @@ bool SelectorChecker::CheckPseudoClass(const SelectorCheckingContext& context,
       return transition->MatchForActiveViewTransition();
     }
     case CSSSelector::kPseudoActiveViewTransitionType: {
-      // :active-view-transition-type is only valid on the document element.
-      if (!element.IsDocumentElement()) {
-        return false;
-      }
-
       // The pseudo is only valid if there is a transition.
-      auto* transition =
-          ViewTransitionUtils::GetTransition(element.GetDocument());
+      auto* transition = GetTransitionForScope(element);
       if (!transition) {
         return false;
       }
