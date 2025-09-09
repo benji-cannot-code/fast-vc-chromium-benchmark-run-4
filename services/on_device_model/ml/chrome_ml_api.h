83d1d38cdadcba1ea61628f1fa29f9c5d24bbe0a
Nathan Memmott <memmott@chromium.org>
2025-09-09 10:40:34 -0700

    Add speculative decoding fields to shim header
    Adds speculative decoding fields to ChromeMLModelDescriptor and
    ChromeMLAdaptationDescriptor.
    
    Bug: 432789459
    Change-Id: Id2019cd9ce29b6c485153abc992189c6247214ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6926100
    Commit-Queue: Nathan Memmott <memmott@chromium.org>
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513164}

--- services/on_device_model/ml/chrome_ml_api.h
+++ services/on_device_model/ml/chrome_ml_api.h
@@ -102,6 +102,9 @@ struct ChromeMLModelDescriptor {
   float temperature;
   int top_k;
 
+  // Speculative decoding
+  int num_draft_tokens;
+
   // Packed TS data.
   const void* ts_data;
   size_t ts_size;
@@ -133,6 +136,9 @@ struct ChromeMLAdaptationDescriptor {
   // Parameters which control the output sampling.
   uint32_t top_k;
   float temperature;
+
+  // Whether or not the mode will use speculative decoding.
+  bool enable_speculative_decoding;
 
   // Whether this model will handle InputPieces containing images.
   bool enable_image_input;
