65be9bfb9ae3db0c4aa68b2a8e3cc9cb2ff81548
Jenna Himawan <jhimawan@google.com>
2025-09-09 07:44:13 -0700

    Move timestamped_pref_value_android -> cross_device_pref_tracker/android
    And move timestamped_pref_value.h to cross_device_pref_tracker
    
    Bug: 442902926
    Change-Id: I1023a025280453f53de9584afa84cff46beefc0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6924020
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1513068}

--- components/sync_preferences/cross_device_pref_tracker/cross_device_pref_tracker.h
+++ components/sync_preferences/cross_device_pref_tracker/cross_device_pref_tracker.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list_types.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/sync_device_info/device_info.h"
-#include "components/sync_preferences/cross_device_pref_tracker/timestamped_pref_value/timestamped_pref_value.h"
+#include "components/sync_preferences/cross_device_pref_tracker/timestamped_pref_value.h"
 
 namespace sync_preferences {
 
