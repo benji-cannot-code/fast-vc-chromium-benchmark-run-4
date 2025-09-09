5068ca57acedf8597655479db6167794131ca5bd
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2025-09-09 08:04:29 -0700

    LNA: Add feature param to only prompt for loopback addresses
    If we find the prompt to be too disruptive as we rollout the feature,
    this param allows us to limit the permission prompt to only loopback
    addresses.
    
    Testing locally that the killswitch works and the surrounding code is
    tested by the following Web Tests:
    
    fast/peerconnection/RTCPeerConnection-local-network-access-with-public-address.https.html
    fast/peerconnection/RTCPeerConnection-local-network-access-with-loopback-address.html
    
    Bug: 421223919
    Change-Id: Iaea406f015f3d88a5091392f508d63c414a15ccb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6794604
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Reviewed-by: mmenke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513081}

--- third_party/blink/renderer/modules/peerconnection/peer_connection_dependency_factory.cc
+++ third_party/blink/renderer/modules/peerconnection/peer_connection_dependency_factory.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/video/gpu_video_accelerator_factories.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "net/net_buildflags.h"
+#include "services/network/public/cpp/features.h"
 #include "services/network/public/cpp/ip_address_space_util.h"
 #include "services/network/public/mojom/ip_address_space.mojom-shared.h"
 #include "third_party/blink/public/common/features.h"
@@ -199,8 +200,14 @@ class LocalNetworkAccessPermission final
     DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
     CHECK(RuntimeEnabledFeatures::LocalNetworkAccessWebRTCEnabled());
 
-    return network::IsLessPublicAddressSpace(
+    const bool is_less_public = network::IsLessPublicAddressSpace(
         FromSocketAddress(candidate_address), originator_address_space_);
+
+    if (network::features::kLocalNetworkAccessChecksWebRTCLoopbackOnly.Get()) {
+      return candidate_address.IsLoopbackIP() && is_less_public;
+    }
+
+    return is_less_public;
   }
 
   void RequestPermission(
