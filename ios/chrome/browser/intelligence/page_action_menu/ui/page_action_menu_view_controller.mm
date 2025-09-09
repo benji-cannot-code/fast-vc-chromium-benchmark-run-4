b41386a03f31bc1d92d2ac801d5a3461f9dcd11a
Joemer Ramos <joemerramos@google.com>
2025-09-08 20:11:16 -0700

    [iOS] Refactor AI Hub "Ask Gemini" Button to use Shared Primary Button
    Primary button should use the shared UI component as described in the
    bug below. This CL refactors the button and reflects the changes
    respectively on iOS 26.
    
    UI Screenshot: crbug.com/441278010#comment11
    
    Fixed: 441278010
    Change-Id: I7a4785d0db899bcb2766440b2f228ec80f1bcdeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6926771
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Auto-Submit: Joemer Ramos <joemerramos@google.com>
    Commit-Queue: Joemer Ramos <joemerramos@google.com>
    Cr-Commit-Position: refs/heads/main@{#1512826}

--- ios/chrome/browser/intelligence/page_action_menu/ui/page_action_menu_view_controller.mm
+++ ios/chrome/browser/intelligence/page_action_menu/ui/page_action_menu_view_controller.mm
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
+#import "ios/chrome/common/ui/util/button_util.h"
+#import "ios/chrome/common/ui/util/chrome_button.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ui/base/l10n/l10n_util.h"
@@ -478,15 +480,14 @@ const CGFloat kReaderModeContentStackVerticalPadding = 10;
 
 // Creates a large button for the BWG entry point.
 - (UIButton*)createBWGButton {
+  ChromeButton* button = PrimaryActionButton();
+
   // Create the background config.
-  UIBackgroundConfiguration* backgroundConfig =
-      [UIBackgroundConfiguration clearConfiguration];
+  UIBackgroundConfiguration* backgroundConfig = button.configuration.background;
   backgroundConfig.backgroundColor = [UIColor colorNamed:kBlue600Color];
-  backgroundConfig.cornerRadius = kButtonsCornerRadius;
 
   // Create the button config.
-  UIButtonConfiguration* buttonConfiguration =
-      [UIButtonConfiguration filledButtonConfiguration];
+  UIButtonConfiguration* buttonConfiguration = button.configuration;
   buttonConfiguration.background = backgroundConfig;
   buttonConfiguration.image = [self askGeminiIcon];
   buttonConfiguration.imagePlacement = NSDirectionalRectEdgeLeading;
@@ -503,9 +504,8 @@ const CGFloat kReaderModeContentStackVerticalPadding = 10;
       initWithString:l10n_util::GetNSString(IDS_IOS_AI_HUB_GEMINI_LABEL)];
   [string addAttributes:titleAttributes range:NSMakeRange(0, string.length)];
   buttonConfiguration.attributedTitle = string;
+  button.configuration = buttonConfiguration;
 
-  UIButton* button = [UIButton buttonWithConfiguration:buttonConfiguration
-                                         primaryAction:nil];
   button.translatesAutoresizingMaskIntoConstraints = NO;
   [button addTarget:self
                 action:@selector(handleBWGTapped:)
@@ -639,6 +639,7 @@ const CGFloat kReaderModeContentStackVerticalPadding = 10;
   // image.
   button.userInteractionEnabled = enabled;
   button.alpha = enabled ? 1.0 : 0.5;
+  button.enabled = enabled;
 }
 
 @end
