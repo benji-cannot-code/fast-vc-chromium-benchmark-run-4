94a3a933db76fbcd8d430933b64b2298c18de477
Jiahe Zhang <jiahe.zhang@intel.com>
2025-09-09 09:55:09 -0700

    Add BoostCompositorThreadsPriorityWhenIdle feature for experiment
    If enabled, BoostCompositorThreadsPriorityWhenIdle will boost
    compositor threads to kInteractive when not in input or loading
    scenarios.
    
    Bug: 423313079
    Change-Id: I4841de6b33cad1701e3e74402d4d042aa5b2cc34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6698680
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513134}

--- components/viz/service/main/viz_compositor_thread_runner_impl.h
+++ components/viz/service/main/viz_compositor_thread_runner_impl.h
@@ -7,10 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_VIZ_SERVICE_MAIN_VIZ_COMPOSITOR_THREAD_RUNNER_IMPL_H_
 
 #include <memory>
+#include <optional>
 
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/task/single_thread_task_runner.h"
+#include "base/threading/scoped_thread_priority.h"
 #include "build/build_config.h"
 #include "components/viz/service/main/viz_compositor_thread_runner.h"
 #include "gpu/command_buffer/service/shared_context_state.h"
@@ -71,6 +73,8 @@ class VizCompositorThreadRunnerImpl : public VizCompositorThreadRunner {
   void RequestBeginFrameForGpuServiceOnCompositorThread(bool toggle);
   void TearDownOnCompositorThread();
 
+  std::optional<base::TaskMonitoringScopedBoostPriority>
+      scenario_priority_boost_;
   std::unique_ptr<VizCompositorThreadType> thread_;
   scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
 
