466d21447473904e93a81f7ae72859d3bab959b0
Philipp Hancke <philipp.hancke@googlemail.com>
2025-09-09 05:47:56 -0700

    webrtc-internals: grey out inactive outbound-rtp
    Bug: chromium:417262733
    Change-Id: I1f512c2be521f81936b24a18bb1cef3447153ef2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6917509
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513007}

--- content/browser/webrtc/resources/stats_table.js
+++ content/browser/webrtc/resources/stats_table.js
@@ -94,6 +94,7 @@ export class StatsTable {
    * @private
    */
   ensureStatsTable_(peerConnectionElement, report) {
+    const detailsId = peerConnectionElement.id + '-details-' + report.id;
     const tableId = peerConnectionElement.id + '-table-' + report.id;
     // Disable getElementById restriction here, since |tableId| is not
     // always a valid selector.
@@ -102,6 +103,7 @@ export class StatsTable {
     if (!table) {
       const container = this.ensureStatsTableContainer_(peerConnectionElement);
       const details = document.createElement('details');
+      details.id = detailsId;
       details.attributes['data-statsType'] = report.type;
       container.appendChild(details);
 
