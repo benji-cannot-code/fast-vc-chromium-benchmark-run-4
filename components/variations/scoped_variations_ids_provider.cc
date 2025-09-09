5031ceca947f8cd8468d61841111ff0386b512b3
Roger McFarlane <rogerm@chromium.org>
2025-09-09 15:35:57 -0700

    Refactor `ScopedVariationsIdsProvider` to restore the previous instance.
    This allows `ScopedVariationsIdsProvider` to be reliably used in nested
    scopes and to behave well when used with content and browser tests that
    instantiate more/less of the browser context, which may include creating
    the global `VariationsIdsProvider` instance.
    
    Bug: 422445605
    Bug: 442849530
    Change-Id: If5ad9737eb0b1e7862bb258447a536fe5647c925
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6916583
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Roger McFarlane <rogerm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513371}

--- components/variations/scoped_variations_ids_provider.cc
+++ components/variations/scoped_variations_ids_provider.cc
@@ -8,12 +8,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace variations::test {
 
 ScopedVariationsIdsProvider::ScopedVariationsIdsProvider(
-    VariationsIdsProvider::Mode mode) {
-  VariationsIdsProvider::CreateInstanceForTesting(mode);
+    VariationsIdsProvider::Mode mode)
+    : previous_instance_(VariationsIdsProvider::CreateInstanceForTesting(mode)),
+      current_instance_(VariationsIdsProvider::GetInstance()) {
 }
 
 ScopedVariationsIdsProvider::~ScopedVariationsIdsProvider() {
-  VariationsIdsProvider::DestroyInstanceForTesting();
+  CHECK_EQ(current_instance_, VariationsIdsProvider::GetInstance());
+  // Release our pointer to the current instance before destroying it, as
+  // `DestroyInstanceForTesting()` will delete the global instance and reset
+  // the global instance pointer to the previous instance.
+  current_instance_ = nullptr;
+  VariationsIdsProvider::DestroyInstanceForTesting(previous_instance_);
 }
 
 }  // namespace variations::test
