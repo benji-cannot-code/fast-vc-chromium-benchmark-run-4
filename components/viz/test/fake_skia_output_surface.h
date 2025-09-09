1b46b5c80e9931f4c519ac8b97f67f1650ab572a
Colin Blundell <blundell@chromium.org>
2025-09-08 23:50:40 -0700

    [Viz] Have FakeSkiaOutputSurface create GrDirectContext internally
    Eliminates dependency on TestContextProvider::GetGrContext() returning
    a non-null context.
    
    Bug: 391648152
    Change-Id: Ic71759a6d2f4e010e9a3dcfb84dd461762ee587a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6919214
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1512867}

--- components/viz/test/fake_skia_output_surface.h
+++ components/viz/test/fake_skia_output_surface.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/common/shared_image_usage.h"
 #include "gpu/command_buffer/common/sync_token.h"
 #include "media/gpu/buildflags.h"
+#include "third_party/skia/include/gpu/ganesh/GrDirectContext.h"
 
 namespace viz {
 
@@ -176,7 +177,12 @@ class FakeSkiaOutputSurface : public SkiaOutputSurface {
 
  private:
   ContextProvider* context_provider() { return context_provider_.get(); }
-  GrDirectContext* gr_context() { return context_provider()->GrContext(); }
+  GrDirectContext* gr_context() {
+    if (!gr_context_) {
+      gr_context_ = GrDirectContext::MakeMock(nullptr);
+    }
+    return gr_context_.get();
+  }
 
   gpu::SyncToken GenerateSyncToken();
 
@@ -215,6 +221,8 @@ class FakeSkiaOutputSurface : public SkiaOutputSurface {
   SharedImagePurgeableCallback set_purgeable_callback_;
 
   THREAD_CHECKER(thread_checker_);
+
+  sk_sp<GrDirectContext> gr_context_;
 
   base::WeakPtrFactory<FakeSkiaOutputSurface> weak_ptr_factory_{this};
 };
