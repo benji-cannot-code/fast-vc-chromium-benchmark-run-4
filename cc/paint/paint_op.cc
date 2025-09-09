ccf44c78cf716778f7e8155289f88d60cca95b1a
Xiaohan Wang <xhwang@chromium.org>
2025-09-08 22:33:04 -0700

    cc: Migrate BASE_FEATURE macros to 2-argument version
    This CL migrates three-argument BASE_FEATURE macros in //cc to the new
    two-argument version where the C++ identifier and the string feature
    name match.
    
    This automated refactoring helps ensure that the C++ identifier and the
    string feature name always match, reducing potential errors.
    
    The following features could not be migrated automatically and may
    require manual intervention:
    
    * kTreeAnimationsInViz vs "kTreeAnimationsInViz"
        * The string feature name has a "k" prefix.
    
    Enabled-by-default-reason: refactor
    
    Bug: 436274260
    Change-Id: I3a3f683ef7bdc7d04b2485dd9a9678646759b788
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6918267
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1512851}

--- cc/paint/paint_op.cc
+++ cc/paint/paint_op.cc
@@ -55,9 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace cc {
 namespace {
 
-BASE_FEATURE(kUseLitePaintOps,
-             "UseLitePaintOps",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+BASE_FEATURE(kUseLitePaintOps, base::FEATURE_ENABLED_BY_DEFAULT);
 
 // In a future CL, convert DrawImage to explicitly take sampling instead of
 // quality
