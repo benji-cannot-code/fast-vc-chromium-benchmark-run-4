2ccebc2920cea4a04d9c8468b8644ea3e2865a19
Gazal <agazal@google.com>
2025-09-09 14:27:58 -0700

    [Settings containment] Restyle SignOut button
    Before: https://screenshot.googleplex.com/AcScQWiybtd7kYp.png
    After: https://screenshot.googleplex.com/4rhVTpHZxWrw6Az.png
    
    Bug: 433576895, 439725777
    Change-Id: I87ebcd954d11307fed9398b2afba0f7e1c0b0e99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6923009
    Commit-Queue: Gazal Agarwal <agazal@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513322}

--- chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/SignoutButtonPreference.java
+++ chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/SignoutButtonPreference.java
@@ -20,7 +20,6 @@ import org.chromium.base.supplier.OneshotSupplier;
 import org.chromium.build.annotations.Initializer;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
-import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.signin.services.IdentityServicesProvider;
 import org.chromium.chrome.browser.ui.messages.snackbar.SnackbarManager;
@@ -41,13 +40,7 @@ public class SignoutButtonPreference extends Preference implements CustomStyledP
 
     public SignoutButtonPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
-
-        if (ChromeFeatureList.sAndroidSettingsContainment.isEnabled()) {
-            setLayoutResource(R.layout.signout_button_view_containment);
-        } else {
-            // TODO (crbug.com/439911511): Remove unused resource
-            setLayoutResource(R.layout.signout_button_view);
-        }
+        setLayoutResource(R.layout.signout_button_view);
     }
 
     @Initializer
@@ -98,22 +91,6 @@ public class SignoutButtonPreference extends Preference implements CustomStyledP
 
     @Override
     public @BackgroundStyle int getCustomBackgroundStyle() {
-        return BackgroundStyle.CARD;
-    }
-
-    /**
-     * @return The custom top margin for the preference in pixels.
-     */
-    @Override
-    public int getCustomTopMargin() {
-        return mContext.getResources().getDimensionPixelSize(R.dimen.signout_button_top_margin);
-    }
-
-    /**
-     * @return The custom bottom margin for the preference in pixels.
-     */
-    @Override
-    public int getCustomBottomMargin() {
-        return mContext.getResources().getDimensionPixelSize(R.dimen.signout_button_bottom_margin);
+        return BackgroundStyle.NONE;
     }
 }
