dc52aaa57bab6b43c4d3b2668e3ec4edb6705133
Steve Becker <stevebe@microsoft.com>
2025-09-09 10:56:33 -0700

    Split RTCDataChannel-send-close.html web test to avoid timeouts
    Addresses flakes by dividing the test into three new tests. Adds one
    test for each type of data sent: Blob, string, and ArrayBuffer. Adds
    `RTCDataChannel-send-close-helper.js` for shared test code.
    
    The original test ran for ~45 seconds on my powerful developer machine.
    Each new tests takes ~15 seconds.
    
    Re-enables the test on win-arm and Linux. MacOS still fails with the
    following network errors, that lead to a timeout:
    
    ```
    services/network/p2p/socket_manager.cc:137 Failed to resolve address for
    9218b16a-a784-40f0-9384-0dec7712202d.local., errorcode: -105
    
    third_party/webrtc/p2p/base/p2p_transport_channel.cc:1274 Failed to
    resolve ICE candidate hostname
    9218b16a-a784-40f0-9384-0dec7712202d.local with error -1
    ```
    
    Bug: 377072733 414687402
    Change-Id: I2e367bdcb9185437d8db4840b5b3ccc70143de7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6913845
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Steve Becker <stevebe@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1513170}

---
+++ third_party/blink/web_tests/external/wpt/webrtc/RTCDataChannel-send-close-array-buffer.window.js
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: title=RTCDataChannel.prototype.send with large ArrayBuffer
+// META: script=RTCPeerConnection-helper.js
+// META: script=RTCDataChannel-send-close-helper.js
+// META: timeout=long
+
+'use strict';
+
+const largeString = ' '.repeat(largeSendDataLength);
+const largeArrayBuffer = new TextEncoder('utf-8').encode(largeString);
+rtc_data_channel_send_close_test(/*sendData=*/ largeArrayBuffer);
---
+++ third_party/blink/web_tests/external/wpt/webrtc/RTCDataChannel-send-close-string.window.js
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: title=RTCDataChannel.prototype.send with large string
+// META: script=RTCPeerConnection-helper.js
+// META: script=RTCDataChannel-send-close-helper.js
+// META: timeout=long
+
+'use strict';
+
+const largeString = ' '.repeat(largeSendDataLength);
+rtc_data_channel_send_close_test(/*sendData=*/ largeString);
