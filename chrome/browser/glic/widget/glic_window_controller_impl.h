f95309523b638ed22b7d7eb6a38c5c1f89302701
Andrea Gomez <andreaxg@google.com>
2025-09-09 10:29:29 -0700

    Create GlicInstanceComponents, a per instance class
    Initially moving GlicSharingManager and GlicZeroStateSuggestionsManager
    
    Bug: 441737907
    Change-Id: Iac52c334aff14f545dd23ce02b9466cbfdd7d991
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6908681
    Auto-Submit: Andrea Gomez <andreaxg@google.com>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Andrea Gomez <andreaxg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1513156}

--- chrome/browser/glic/widget/glic_window_controller_impl.h
+++ chrome/browser/glic/widget/glic_window_controller_impl.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/glic/host/glic_web_client_access.h"
 #include "chrome/browser/glic/host/host.h"
 #include "chrome/browser/glic/public/glic_enabling.h"
+#include "chrome/browser/glic/public/glic_instance_components.h"
 #include "chrome/browser/glic/widget/glic_window_config.h"
 #include "chrome/browser/glic/widget/glic_window_controller.h"
 #include "chrome/browser/glic/widget/local_hotkey_manager.h"
@@ -65,10 +66,12 @@ class GlicWindowControllerImpl
   GlicWindowControllerImpl(const GlicWindowControllerImpl&) = delete;
   GlicWindowControllerImpl& operator=(const GlicWindowControllerImpl&) = delete;
 
-  GlicWindowControllerImpl(Profile* profile,
-                           signin::IdentityManager* identity_manager,
-                           GlicKeyedService* service,
-                           GlicEnabling* enabling);
+  GlicWindowControllerImpl(
+      Profile* profile,
+      signin::IdentityManager* identity_manager,
+      GlicKeyedService* service,
+      GlicEnabling* enabling,
+      contextual_cueing::ContextualCueingService* contextual_cueing_service);
   ~GlicWindowControllerImpl() override;
 
   // GlicWindowController implementation
@@ -149,6 +152,8 @@ class GlicWindowControllerImpl
   Host& host() override;
   HostManager& host_manager() override;
   Host* GetHostForTab(tabs::TabInterface* tab) override;
+
+  GlicInstanceComponents& glic_instance_components();
 
  private:
   // Sets the floating attributes of the glic window.
@@ -393,6 +398,7 @@ class GlicWindowControllerImpl
 
   raw_ptr<GlicKeyedService> glic_service_;  // Owns this.
   raw_ptr<GlicEnabling> enabling_;
+  GlicInstanceComponents glic_instance_components_;
   base::ScopedObservation<Host, Host::Observer> host_observation_{this};
 
   base::WeakPtrFactory<GlicWindowControllerImpl> weak_ptr_factory_{this};
