03b8dc3122c9f37d3a6174d377de9643aeaa4d06
Daniel Rubery <drubery@chromium.org>
2025-09-09 13:07:57 -0700

    Remove "Enabled - Standard" option from #enable-standard-device-bound-session-credentials
    This is exactly the same as "Enabled", which is confusing. The new
    options are:
    - Default
    - Enabled
    - Enabled - For developers
    - Disabled
    
    Which is much more clear.
    
    Change-Id: I5efe2e10d795a9e6f42f337bf3f6b31c89bbc24e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6931891
    Reviewed-by: thefrog <thefrog@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513266}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -4455,11 +4455,6 @@ const FeatureEntry::FeatureVariation kHistoryOptInEducationalTipVariations[] = {
      std::size(kHistoryOptInEducationalTipContinue), nullptr}};
 #endif  // BUILDFLAG(IS_ANDROID)
 
-const FeatureEntry::FeatureParam kStandardBoundSessionCredentialsStandard[] = {
-    {"RequireOriginTrialTokens", "true"},
-    {"RefreshQuota", "true"},
-    {"CheckSubdomainRegistration", "true"},
-    {"CheckFederatedRegistration", "true"}};
 const FeatureEntry::FeatureParam
     kStandardBoundSessionCredentialsForDevelopers[] = {
         {"RequireOriginTrialTokens", "false"},
@@ -4469,8 +4464,6 @@ const FeatureEntry::FeatureParam
 
 const FeatureEntry::FeatureVariation
     kStandardBoundSessionCredentialsVariations[] = {
-        {"- Standard", kStandardBoundSessionCredentialsStandard,
-         std::size(kStandardBoundSessionCredentialsStandard), nullptr},
         {"- For developers", kStandardBoundSessionCredentialsForDevelopers,
          std::size(kStandardBoundSessionCredentialsForDevelopers), nullptr}};
 
