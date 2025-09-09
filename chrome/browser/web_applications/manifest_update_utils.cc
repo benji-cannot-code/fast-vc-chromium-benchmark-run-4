b8439cf1d7a35e0b0e35b6066c3fa2936b204067
Dan Murphy <dmurph@chromium.org>
2025-09-09 06:53:45 -0700

    [PWA] Refactoring scope into a separate class with encapsulated logic.
    This change encapsulates all logic reasoning about urls and scopes into
    a dedicated class that is self-contained (doesn't fetch anything). It
    then updates the places that had this logic to call this class instead.
    
    There are a few options introduced to accommodate the current behavior,
    but those can change in the future.
    
    Some other changes here:
    - A change that allows all chrome://-url installed pwas to
      silently update their identity, as those are trusted. This made
      the following test no longer flaky:
    ProfileMenuClickTest_PasswordManagerWebApp.ProfileMenuClickTest_PasswordManagerWebApp/0
    - The scope extensions score number was based on the origin, but this is
      no longer correct after the addition of the 'scope' member. This as
      fixed.
    
    Some caveats:
    - Behavior around in-scope checks that intend to exclude extended scope
      but consider it for CrOS experiments was maintained from before.
      Hopefully the need for this will go away after we use extensions in
      most places.
    
    Bug: 434956941
    Change-Id: I74e3b934e0e572eba071e94748496a50213a255d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6916346
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513027}

--- chrome/browser/web_applications/manifest_update_utils.cc
+++ chrome/browser/web_applications/manifest_update_utils.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_registrar.h"
 #include "chrome/common/chrome_features.h"
 #include "components/webapps/common/web_app_id.h"
+#include "content/public/common/url_constants.h"
 #include "third_party/blink/public/common/features.h"
 #include "ui/gfx/skia_util.h"
 
@@ -169,6 +170,9 @@ bool CanWebAppSilentlyUpdateIdentity(const WebApp& web_app) {
   if (web_app.IsPolicyInstalledApp() &&
       base::FeatureList::IsEnabled(
           features::kWebAppManifestPolicyAppIdentityUpdate)) {
+    return true;
+  }
+  if (web_app.scope().SchemeIs(content::kChromeUIScheme)) {
     return true;
   }
 
--- chrome/browser/web_applications/web_app.h
+++ chrome/browser/web_applications/web_app.h
@@ -56,6 +56,7 @@ enum class WebappInstallSource;
 
 namespace web_app {
 class UrlPatternWithRegexMatcher;
+class WebAppScope;
 
 class WebApp {
  public:
@@ -245,6 +246,8 @@ class WebApp {
   const base::flat_set<ScopeExtensionInfo>& validated_scope_extensions() const {
     return validated_scope_extensions_;
   }
+
+  WebAppScope GetScope() const;
 
   RunOnOsLoginMode run_on_os_login_mode() const {
     return run_on_os_login_mode_;
