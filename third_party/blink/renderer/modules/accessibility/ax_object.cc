dca21051fb6a245c95b1fd2d3ab105acd26eb95b
Jacques Newman <janewman@microsoft.com>
2025-09-09 15:36:43 -0700

    [AriaNotify] Serialize aria notifications called on ignored elements.
    Similar issue to CL:6676264, authors want to be able to call arianotify
    on the body, but there are some cases where we consider the body to be
    ignored for the purposes of accessibility. In these cases, we should
    still allow for the event to be fired.
    
    Once verified in canary, this will need to be cherry-picked to 141
    
    Bug: 326277796
    Change-Id: I0db763006297b8982cbbd8efec2c79e79d1bc5ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6932925
    Auto-Submit: Jacques Newman <janewman@microsoft.com>
    Commit-Queue: Jacques Newman <janewman@microsoft.com>
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1513372}

--- third_party/blink/renderer/modules/accessibility/ax_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object.cc
@@ -1400,6 +1400,9 @@ void AXObject::Serialize(ui::AXNodeData* node_data,
   // Always try to serialize child tree ids.
   SerializeChildTreeID(node_data);
 
+  SerializeAriaNotificationAttributes(
+      AXObjectCache().RetrieveAriaNotifications(this), node_data);
+
   // Return early. The following attributes are unnecessary for ignored nodes.
   // Exception: focusable ignored nodes are fully serialized, so that reasonable
   // verbalizations can be made if they actually receive focus.
@@ -1442,8 +1445,6 @@ void AXObject::Serialize(ui::AXNodeData* node_data,
   }
   SerializeOtherScreenReaderAttributes(node_data);
   SerializeMathContent(node_data);
-  SerializeAriaNotificationAttributes(
-      AXObjectCache().RetrieveAriaNotifications(this), node_data);
 }
 
 void AXObject::SerializeBoundingBoxAttributes(ui::AXNodeData& dst) const {
