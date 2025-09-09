e1ad99fa1a7c6646a72f30fc7b6cf2ab3ab2f8ce
Rakina Zata Amni <rakina@chromium.org>
2025-09-09 00:52:47 -0700

    Update tests to expect RenderFrameHost change on same-site navigations
    These updates some tests that fail when RenderDocument is enabled and
    BFCache is disabled (running on linux-bfcache-rel bots). These tests
    didn't expect RFH changing due to same-site navs, causing e.g.
    RenderFrameSubmission destructor to fail. The fix is basically
    reordering some actions / changing lifetimes / etc.
    
    Bug: 40615943
    Change-Id: Iefa470c2be0986fd12263c661bf72d5d4518ff24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6929322
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1512890}

--- content/browser/text_fragment_browsertest.cc
+++ content/browser/text_fragment_browsertest.cc
@@ -78,7 +78,6 @@ class TextFragmentAnchorBrowserTest : public ContentBrowserTest {
 
     SimulateMouseClickAt(web_contents, 0, blink::WebMouseEvent::Button::kLeft,
                          gfx::Point(x, y));
-    RunUntilInputProcessed(GetWidgetHost());
   }
 
   void WaitForPageLoad(WebContents* contents) {
@@ -882,6 +881,7 @@ IN_PROC_BROWSER_TEST_F(ForceLoadAtTopBrowserTest, SameDocumentNavigation) {
   // Click on a link with a fragment id. Ensure we scroll to the targeted
   // element.
   ClickElementWithId(main_contents, "link");
+  RunUntilInputProcessed(GetWidgetHost());
 
   EXPECT_DID_SCROLL(true);
 }
