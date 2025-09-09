3f9dcf458e1fbf0ae40347c860c7b454a297881f
Olivier Li <olivierli@chromium.org>
2025-09-09 14:10:26 -0700

    Experiment with alternative map types.
    This experiment replaces uses of std::map with absl::flat_hash_map
    in a transparent way. Using the new wrapper uses finch automatically
    to evaluate performance differences.
    
    Bug: 433462519
    
    Change-Id: I2046e4d610c66e9d5cb203be37ce4886d062953d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6652483
    Commit-Queue: Olivier Li <olivierli@chromium.org>
    Auto-Submit: Olivier Li <olivierli@chromium.org>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513306}

--- mojo/public/cpp/bindings/binder_map.h
+++ mojo/public/cpp/bindings/binder_map.h
@@ -13,9 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/component_export.h"
 #include "base/containers/contains.h"
+#include "base/containers/variant_map.h"
 #include "base/functional/callback.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/task/sequenced_task_runner.h"
+#include "base/types/pass_key.h"
 #include "mojo/public/cpp/bindings/generic_pending_receiver.h"
 #include "mojo/public/cpp/bindings/lib/binder_map_internal.h"
 
@@ -40,6 +42,8 @@ namespace mojo {
 template <typename ContextType>
 class BinderMapWithContext {
  public:
+  using PassKey = base::PassKey<BinderMapWithContext>;
+
   using Traits = internal::BinderContextTraits<ContextType>;
   using ContextValueType = typename Traits::ValueType;
   using GenericBinderType = typename Traits::GenericBinderType;
@@ -50,7 +54,8 @@ class BinderMapWithContext {
   template <typename Interface>
   using FuncType = typename Traits::template FuncType<Interface>;
 
-  BinderMapWithContext() = default;
+  BinderMapWithContext() : binders_(PassKey()) {}
+
   BinderMapWithContext(const BinderMapWithContext&) = default;
   BinderMapWithContext(BinderMapWithContext&&) = default;
   ~BinderMapWithContext() = default;
@@ -170,8 +175,8 @@ class BinderMapWithContext {
     binders_.try_emplace(key, std::move(binder));
   }
 
-  std::map<std::string_view,
-           internal::GenericCallbackBinderWithContext<ContextType>>
+  base::VariantMap<std::string_view,
+                   internal::GenericCallbackBinderWithContext<ContextType>>
       binders_;
   DefaultBinder default_binder_;
 };
