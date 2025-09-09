a883307533123eb30a1b7c4d8919896bdc6bc6c8
Vidhan Jain <vidhanj@google.com>
2025-09-09 02:59:54 -0700

    Use QueueOrShowBubble in filled card info bubble to show bubble
    There is a slight behavior change for this class to show the bubble when
    the bubble manager is enabled.
    
    1. The bubble won't be created/presented if the bubble manager does not
    exist which is the case for non-tab features.
    
    2. If the filled card information bubble is to be shown to the user, and
    a lower priority bubble is currently shown and hovered by the user, the
    filled card information bubble is added to the queue even though it has
    the highest priority. Once the user takes an action on the lower
    priority bubble, the filled card information bubble would automatically
    popup. If the lower priority bubble was not hovered, the lower priority
    bubble will be hidden and filled card information bubble is shown.
    
    Bug: 432429605
    Change-Id: Idaa123985ad32c02137e9a35b42028681d5f8c76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6875312
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Cr-Commit-Position: refs/heads/main@{#1512945}

--- chrome/browser/ui/autofill/payments/filled_card_information_bubble_controller_impl.cc
+++ chrome/browser/ui/autofill/payments/filled_card_information_bubble_controller_impl.cc
@@ -5,11 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/autofill/payments/filled_card_information_bubble_controller_impl.h"
 
-#include "base/feature_list.h"
 #include "base/task/single_thread_task_runner.h"
 #include "chrome/browser/ui/autofill/autofill_bubble_base.h"
 #include "chrome/browser/ui/autofill/autofill_bubble_handler.h"
-#include "chrome/browser/ui/autofill/bubble_manager.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_window.h"
@@ -18,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/payments/bnpl_manager.h"
 #include "components/autofill/core/browser/payments/constants.h"
 #include "components/autofill/core/browser/payments/payments_service_url.h"
-#include "components/autofill/core/common/autofill_features.h"
 #include "components/autofill/core/common/credit_card_number_validation.h"
 #include "components/grit/components_scaled_resources.h"
 #include "components/strings/grit/components_strings.h"
@@ -69,14 +66,10 @@ void FilledCardInformationBubbleControllerImpl::SetupAndShowBubble(
     HideBubble();
   }
 
-  if (base::FeatureList::IsEnabled(
-          features::kAutofillShowBubblesBasedOnPriorities)) {
-    auto* manager = BubbleManager::GetForWebContents(web_contents());
-    if (!manager || manager->HasPendingBubble(*this)) {
-      // Early return if a pre-existing of similar type is in the queue or the
-      // manager does not exist.
-      return;
-    }
+  if (!MaySetUpBubble()) {
+    // This will early return when bubble manager is enabled but doesn't exist
+    // for the tab.
+    return;
   }
 
   SetupBubbleState(options);
@@ -87,20 +80,9 @@ void FilledCardInformationBubbleControllerImpl::SetupAndShowBubble(
   base::SingleThreadTaskRunner::GetCurrentDefault()->PostDelayedTask(
       FROM_HERE,
       base::BindOnce(
-          &FilledCardInformationBubbleControllerImpl::RequestShowBubble,
-          weak_ptr_factory_.GetWeakPtr()),
+          &FilledCardInformationBubbleControllerImpl::QueueOrShowBubble,
+          weak_ptr_factory_.GetWeakPtr(), /*force_show=*/false),
       kFilledCardInformationBubbleDelay);
-}
-
-void FilledCardInformationBubbleControllerImpl::RequestShowBubble() {
-  if (base::FeatureList::IsEnabled(
-          features::kAutofillShowBubblesBasedOnPriorities)) {
-    if (auto* manager = BubbleManager::GetForWebContents(web_contents())) {
-      manager->RequestShowController(*this, /*force_show=*/false);
-    }
-  } else {
-    ShowBubble();
-  }
 }
 
 void FilledCardInformationBubbleControllerImpl::SetupBubbleState(
@@ -119,7 +101,7 @@ void FilledCardInformationBubbleControllerImpl::ReshowBubble() {
 
   is_user_gesture_ = true;
   should_icon_be_visible_ = true;
-  ShowBubble();
+  QueueOrShowBubble(/*force_show=*/true);
 }
 
 AutofillBubbleBase* FilledCardInformationBubbleControllerImpl::GetBubble()
