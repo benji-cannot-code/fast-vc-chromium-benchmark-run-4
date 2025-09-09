5bced842ddf009f38d7a97f2c751bb266422b1e6
Mark Schillaci <mschillaci@google.com>
2025-09-09 15:55:53 -0700

    [Bookmarks Bar] Record any time a bookmark has been opened within Clank
    This CL adds an additional metric that is of interest for the Bookmark
    Bar feature. This metric tracks the number of bookmarks that have been
    opened within the app as a whole. This can be compared to the number of
    bookmarks opened from within the bookmark bar, or tracked long-term to
    see if bookmark usage is changing.
    
    Bug: 430057288
    Change-Id: If78f35f8ad0544c621c4a7ca01eeac4e214580a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6927231
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Eleanor Lee <eleanorlee@google.com>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1513386}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkOpenerImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkOpenerImpl.java
@@ -148,6 +148,7 @@ public class BookmarkOpenerImpl implements BookmarkOpener {
         RecordUserAction.record("MobileBookmarkManagerEntryOpened");
         recordTypeOpened(item, "Bookmarks.OpenBookmarkType");
         recordTimeSinceAdded(item, "Bookmarks.OpenBookmarkTimeInterval2.");
+        recordBookmarkURLOpened();
     }
 
     private void recordMetricsForOpenBookmarksInNewTabs(List<BookmarkItem> items) {
@@ -156,6 +157,7 @@ public class BookmarkOpenerImpl implements BookmarkOpener {
         for (BookmarkItem item : items) {
             recordTypeOpened(item, "Bookmarks.MultipleOpened.OpenBookmarkType");
             recordTimeSinceAdded(item, "Bookmarks.MultipleOpened.OpenBookmarkTimeInterval2.");
+            recordBookmarkURLOpened();
         }
     }
 
@@ -184,5 +186,9 @@ public class BookmarkOpenerImpl implements BookmarkOpener {
                 1,
                 DateUtils.DAY_IN_MILLIS * 30,
                 50);
+    }
+
+    private void recordBookmarkURLOpened() {
+        RecordHistogram.recordBooleanHistogram("Bookmarks.MobileBookmarkManager.OpenedURL", true);
     }
 }
