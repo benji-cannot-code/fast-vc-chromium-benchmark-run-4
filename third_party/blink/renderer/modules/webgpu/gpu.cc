7407be45b40e9772a9d32edf578e499eb33cc69e
Olivier Li <olivierli@chromium.org>
2025-09-09 09:17:55 -0700

    Log success of requesting WebGPU adapter
    Bug: 40208065
    Change-Id: Id3a946fd25d0aa6731f87c4193ebd0d2f03b107b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6908176
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1513114}

--- third_party/blink/renderer/modules/webgpu/gpu.cc
+++ third_party/blink/renderer/modules/webgpu/gpu.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/feature_list.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/notreached.h"
 #include "base/synchronization/waitable_event.h"
 #include "gpu/command_buffer/client/webgpu_interface.h"
@@ -40,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/webgpu/wgsl_language_features.h"
 #include "third_party/blink/renderer/platform/graphics/gpu/dawn_control_client_holder.h"
 #include "third_party/blink/renderer/platform/graphics/gpu/webgpu_callback.h"
+#include "third_party/blink/renderer/platform/graphics/gpu/webgpu_cpp.h"
 #include "third_party/blink/renderer/platform/graphics/web_graphics_context_3d_provider_util.h"
 #include "third_party/blink/renderer/platform/heap/cross_thread_handle.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
@@ -206,6 +208,12 @@ void GPU::OnRequestAdapterCallback(
     wgpu::Adapter adapter,
     wgpu::StringView error_message) {
   GPUAdapter* gpu_adapter = nullptr;
+
+  // wgpu::RequestAdapterStatus is part of the stable API, so is safe to log to histograms.
+  // The macro + `to_underlying` converts the enum to an int to calculate the max range.
+  UMA_HISTOGRAM_ENUMERATION("GPU.RequestAdapterStatus.WebGPU", status,
+                            base::to_underlying(wgpu::RequestAdapterStatus::Error) + 1);
+
   switch (status) {
     case wgpu::RequestAdapterStatus::Success:
       gpu_adapter = MakeGarbageCollected<GPUAdapter>(
