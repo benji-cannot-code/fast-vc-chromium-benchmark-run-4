d51f5ec2697dde405dbcc2d83480bbb57a84f0d6
Fiaz Muhammad <mfiaz@google.com>
2025-09-09 06:39:05 -0700

    [Suggest] Don't allow pinned tabs in group suggestions
    - Check for pinned tabs. Don't show suggestion if tab is pinned.
    - Add test.
    
    Bug: 443284594
    Change-Id: I41b81af9de556bff295f8a1dd148d58bb6bca19e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6925540
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Madhav Pruthi <madhavpruthi@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Auto-Submit: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1513020}

--- chrome/browser/tab_ui/android/junit/src/org/chromium/chrome/browser/tab_ui/TabSwitcherGroupSuggestionServiceUnitTest.java
+++ chrome/browser/tab_ui/android/junit/src/org/chromium/chrome/browser/tab_ui/TabSwitcherGroupSuggestionServiceUnitTest.java
@@ -211,6 +211,21 @@ public class TabSwitcherGroupSuggestionServiceUnitTest {
     }
 
     @Test
+    public void testMaybeShowSuggestions_oneTabPinned() {
+        int[] tabIds = {1, 2};
+        GroupSuggestion suggestion = new GroupSuggestion(tabIds, 10, 0, "", "", "");
+        setupCachedSuggestion(suggestion);
+        mockTab(1, 0, true);
+        Tab pinnedTab = mockTab(2, 1, false);
+        when(pinnedTab.getIsPinned()).thenReturn(true);
+        when(mTabModel.getCount()).thenReturn(2);
+
+        mService.maybeShowSuggestions();
+        verify(mSuggestionLifecycleObserverHandler, never()).onShowSuggestion(any());
+        verify(mUserResponseCallback).onResult(any());
+    }
+
+    @Test
     public void testClearSuggestions_callsHandler() {
         mService.clearSuggestions();
         verify(mSuggestionLifecycleObserverHandler).onSuggestionIgnored();
@@ -279,7 +294,7 @@ public class TabSwitcherGroupSuggestionServiceUnitTest {
         verify(mSuggestionLifecycleObserverHandler).onSuggestionIgnored();
     }
 
-    private void mockTab(int tabId, int index, boolean isActive) {
+    private Tab mockTab(int tabId, int index, boolean isActive) {
         Tab tab = mock();
 
         when(tab.getId()).thenReturn(tabId);
@@ -291,6 +306,7 @@ public class TabSwitcherGroupSuggestionServiceUnitTest {
         when(mTabModel.indexOf(tab)).thenReturn(index);
         when(mTabModel.getTabAt(index)).thenReturn(tab);
         mTabs.add(index, tab);
+        return tab;
     }
 
     private void setupCachedSuggestion(GroupSuggestion suggestion) {
