76ce4c3a4d480623a771e3a4cb6f2bad7be31c18
Masa Fujita <massan@google.com>
2025-09-09 15:13:35 -0700

    flyout: Disregard user action when hover is too short
    Currently, even when the user briefly hovers over an item with submenu,
    the flyout switches to the new item. We utilize `ACTION_HOVER_EXIT` and
    cancel the planned action if the timer has not finished.
    
    Bug: 433410990
    Change-Id: I00a01f358a25230878efda9a84914b48111adf60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6918564
    Commit-Queue: Masa Fujita <massan@google.com>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1513354}

--- ui/android/java/src/org/chromium/ui/listmenu/ListMenuUtils.java
+++ ui/android/java/src/org/chromium/ui/listmenu/ListMenuUtils.java
@@ -35,6 +35,7 @@ import org.chromium.ui.modelutil.MVCListAdapter.ModelList;
 import org.chromium.ui.modelutil.ModelListAdapter;
 import org.chromium.ui.modelutil.PropertyModel;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -43,6 +44,7 @@ import java.util.Set;
 @NullMarked
 public class ListMenuUtils {
     private static @Nullable Runnable sFlyoutAfterDelayRunnable;
+    private static @Nullable WeakReference<View> sPendingFlyoutParentView;
 
     /**
      * Defines a contract for managing a series of flyout popups, typically used for nested context
@@ -257,6 +259,7 @@ public class ListMenuUtils {
                 () -> {
                     onFlyoutAfterDelay(item, view, flyoutHandler, levelOfHoveredItem);
                 };
+        sPendingFlyoutParentView = new WeakReference<>(view);
         Handler handler = view.getHandler();
         assert handler != null;
         handler.postDelayed(
@@ -290,12 +293,15 @@ public class ListMenuUtils {
     }
 
     private static void cancelFlyoutDelay(View view) {
-        if (sFlyoutAfterDelayRunnable != null) {
+        View pendingView =
+                (sPendingFlyoutParentView == null) ? null : sPendingFlyoutParentView.get();
+        if (sFlyoutAfterDelayRunnable != null && pendingView == view) {
             Handler handler = view.getHandler();
             if (handler != null) {
                 handler.removeCallbacks(sFlyoutAfterDelayRunnable);
             }
             sFlyoutAfterDelayRunnable = null;
+            sPendingFlyoutParentView = null;
         }
     }
 
@@ -357,6 +363,7 @@ public class ListMenuUtils {
                                         drillDownOverrideValue);
                                 break;
                             case MotionEvent.ACTION_HOVER_EXIT:
+                                cancelFlyoutDelay(view);
                                 // We only want to remove the flyout popups when the user hovers
                                 // over another item. We don't close the flyout popup even when the
                                 // item itself loses hover.
