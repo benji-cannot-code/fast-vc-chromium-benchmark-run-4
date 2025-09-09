10491019549c018e59f40955c436385a4901ff30
Takashi Sakamoto <tasak@google.com>
2025-09-08 22:37:42 -0700

    Unbind PermissionService after running ClipboardPromiseReadText.
    Without the `unbind`, some blink unittests, e.g.
    AudioContextAutoplayTest/AudioContextAutoplayTest.AutoplayMetrics_NodeStartNoGestureThenSuccess_Main/0,
    fail because of:
    [FATAL:third_party/blink/renderer/modules/clipboard/mock_clipboard_permission_service.cc:18]
    DCHECK failed: !receiver_.is_bound().
    
    Change-Id: I6462640ec447dcfa9bfa343bc4a42f5ab4a92e03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6921811
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1512852}

--- third_party/blink/renderer/modules/clipboard/clipboard_unittest.cc
+++ third_party/blink/renderer/modules/clipboard/clipboard_unittest.cc
@@ -83,6 +83,9 @@ TEST_F(ClipboardTest, ClipboardPromiseReadText) {
   String promise_returned_string;
   promise_tester.Value().ToString(promise_returned_string);
   EXPECT_EQ(promise_returned_string, testing_string);
+
+  executionContext->GetBrowserInterfaceBroker().SetBinderForTesting(
+      mojom::blink::PermissionService::Name_, {});
 }
 
 }  // namespace blink
