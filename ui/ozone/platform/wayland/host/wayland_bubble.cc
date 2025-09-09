d36002fe4fe9de96d46cc4fb950f0dcae81a3859
Kramer Ge <fangzhoug@chromium.org>
2025-09-09 11:23:30 -0700

    [Ozone/Wayland]Apply ui_scale before bubble set_position
    _dip is from chromium UI layers perspective, when passing it to wayland,
    we need to up-scale by the ui_scale because wayland is agnostic of it.
    
    Change-Id: Id2e64052d8c316b3343485aab07a6d3ab562365c
    Bug: 369213517, 439508621
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6923333
    Commit-Queue: Kramer Ge <fangzhoug@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513193}

--- ui/ozone/platform/wayland/host/wayland_bubble.cc
+++ ui/ozone/platform/wayland/host/wayland_bubble.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/platform/wayland/host/wayland_buffer_manager_host.h"
 #include "ui/ozone/platform/wayland/host/wayland_connection.h"
 #include "ui/ozone/platform/wayland/host/wayland_data_drag_controller.h"
+#include "ui/ozone/platform/wayland/host/wayland_window.h"
 #include "ui/ozone/platform/wayland/host/wayland_window_manager.h"
 #include "ui/platform_window/platform_window_init_properties.h"
 
@@ -154,11 +155,15 @@ void WaylandBubble::AddToParentAsSubsurface() {
 }
 
 void WaylandBubble::SetSubsurfacePosition() {
-  // TODO(crbug.com/369213517): Handle ui scale before enabling this on Linux.
-  const auto bounds_dip_in_parent =
-      wl::TranslateWindowBoundsToParentDIP(this, parent_window());
-  wl_subsurface_set_position(subsurface_.get(), bounds_dip_in_parent.x(),
-                             bounds_dip_in_parent.y());
+  // bounds_dip here are in Chromium UI coordinates space (i.e ui_scale'd), as
+  // they have just been provided by upper UI layers. Since they are going to be
+  // used to issue Wayland requests, they must be reverse-transformed to Wayland
+  // DIP coordinates space.
+  const auto bounds_in_parent = gfx::ScaleToEnclosingRectIgnoringError(
+      wl::TranslateWindowBoundsToParentDIP(this, parent_window()),
+      applied_state().ui_scale);
+  wl_subsurface_set_position(subsurface_.get(), bounds_in_parent.x(),
+                             bounds_in_parent.y());
   parent_window()->root_surface()->Commit();
 }
 
@@ -170,6 +175,7 @@ bool WaylandBubble::OnInitialize(PlatformWindowInitProperties properties,
   state->window_state = PlatformWindowState::kNormal;
 
   state->window_scale = parent_window()->applied_state().window_scale;
+  state->ui_scale = parent_window()->applied_state().ui_scale;
   activatable_ = properties.activatable;
   accept_events_ = properties.accept_events;
 
