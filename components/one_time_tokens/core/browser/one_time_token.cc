9fe1d425cbc9ee10a657f599b90c7539c01624ae
Dominic Battre <battre@chromium.org>
2025-09-09 01:11:09 -0700

    Introduce one_time_token component
    This CL introduces a basic one_time_token component with the vary basic
    data types of OneTimeToken and OneTimeTokenType.
    
    This is part of a refactoring that moves the one time token fetching
    logic from components/password_manager/core/browser/one_time_passwords/
    into an isolated component/one_time_token/ and prepares it for being
    extended in functionality.
    
    Bug: 415273270
    Change-Id: I5fa94cd5ec56ffd53e2f796f3cc324030888508f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6919147
    Reviewed-by: Matthias Körber <koerber@google.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1512900}

---
+++ components/one_time_tokens/core/browser/one_time_token.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/one_time_tokens/core/browser/one_time_token.h"
+
+namespace one_time_tokens {
+
+OneTimeToken::OneTimeToken(OneTimeTokenType type,
+                           const std::string& value,
+                           base::Time on_device_arrival_time)
+    : type_(type),
+      value_(value),
+      on_device_arrival_time_(on_device_arrival_time) {}
+
+OneTimeToken::OneTimeToken(const OneTimeToken&) = default;
+OneTimeToken& OneTimeToken::operator=(const OneTimeToken&) = default;
+
+OneTimeToken::OneTimeToken(OneTimeToken&&) = default;
+OneTimeToken& OneTimeToken::operator=(OneTimeToken&&) = default;
+
+OneTimeToken::~OneTimeToken() = default;
+
+}  // namespace one_time_tokens
---
+++ components/one_time_tokens/core/browser/one_time_token_unittest.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/one_time_tokens/core/browser/one_time_token.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace one_time_tokens {
+
+TEST(OneTimeToken, Constructor) {
+  base::Time now = base::Time::Now();
+  OneTimeToken token(OneTimeTokenType::kSmsOtp, "value", now);
+  EXPECT_EQ(token.type(), OneTimeTokenType::kSmsOtp);
+  EXPECT_EQ(token.value(), "value");
+  EXPECT_EQ(token.on_device_arrival_time(), now);
+}
+
+}  // namespace one_time_tokens
