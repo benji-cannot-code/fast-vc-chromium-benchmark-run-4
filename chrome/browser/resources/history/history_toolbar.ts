1633574bff1efb00d23338568183bd6fe5f8a500
John Lee <johntlee@chromium.org>
2025-09-09 10:00:08 -0700

    History WebUI: Check-in Lit version of shared_style.css
    Bug: 421235224
    Change-Id: Ieaa1d0e0812774717d6e6c9b4b97b5b010a17d0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6926901
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513139}

--- chrome/browser/resources/history/history_toolbar.ts
+++ chrome/browser/resources/history/history_toolbar.ts
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import './shared_style_lit.css.js';
 import '/strings.m.js';
 import 'chrome://resources/cr_components/history_embeddings/icons.html.js';
 import 'chrome://resources/cr_elements/cr_toolbar/cr_toolbar.js';
