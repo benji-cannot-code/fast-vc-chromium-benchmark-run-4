6dee7d556d0c2594d3d4a86aa3c0b02066c7b700
Aviv Kiss <avivkiss@google.com>
2025-09-09 01:26:51 -0700

    [iOS][AIM] Add a context menu item to attach tabs to the compose plate.
    Fixed: 442564519
    Change-Id: Ife622c39d23b60d16a82ab1aa18c3228c966d5db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6919966
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Auto-Submit: Aviv Kiss <avivkiss@google.com>
    Cr-Commit-Position: refs/heads/main@{#1512908}

--- ios/chrome/browser/aim/prototype/ui/aim_prototype_mutator.h
+++ ios/chrome/browser/aim/prototype/ui/aim_prototype_mutator.h
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)sendText:(NSString*)text;
 /// Sets `enabled` state for AIM.
 - (void)setAIModeEnabled:(BOOL)enabled;
+/// Adds tab content.
+- (void)attachCurrentTabContent;
 
 @end
 
