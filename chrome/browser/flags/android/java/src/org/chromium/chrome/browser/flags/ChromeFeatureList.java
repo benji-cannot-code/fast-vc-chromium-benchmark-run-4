0267dcf3545496d03f59f71e83682a62306716c8
Charles Hager <clhager@google.com>
2025-09-09 13:41:06 -0700

    Change ToolbarTabletResizeRefactor to a cached flag
    The ToolbarTabletResizeRefactor is checked multiple times before native
    is loaded, and the lacked of cached value is causing some issues.
    
    Bug: 427260734
    Change-Id: If7e92df33cb50bc2a3f6c65c1a9ed776392a07d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6926873
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Commit-Queue: Charles Hager <clhager@google.com>
    Cr-Commit-Position: refs/heads/main@{#1513285}

--- chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
@@ -1097,6 +1097,8 @@ public abstract class ChromeFeatureList {
                     TOOLBAR_SNAPSHOT_REFACTOR,
                     /* defaultValue= */ false,
                     /* defaultValueInTests= */ true);
+    public static final CachedFlag sToolbarTabletResizeRefactor =
+            newCachedFlag(TOOLBAR_TABLET_RESIZE_REFACTOR, /* defaultValue= */ false);
     public static final CachedFlag sTopControlsRefactor =
             newCachedFlag(TOP_CONTROLS_REFACTOR, true);
     public static final CachedFlag sTouchToSearchCallout =
@@ -1266,6 +1268,7 @@ public abstract class ChromeFeatureList {
                     sTabletTabStripAnimation,
                     sToolbarPhoneAnimationRefactor,
                     sToolbarSnapshotRefactor,
+                    sToolbarTabletResizeRefactor,
                     sTopControlsRefactor,
                     sTouchToSearchCallout,
                     sTraceBinderIpc,
@@ -1395,8 +1398,6 @@ public abstract class ChromeFeatureList {
             newMutableFlagWithSafeDefault(TAB_SWITCHER_GROUP_SUGGESTIONS_TEST_MODE_ANDROID, false);
     public static final MutableFlagWithSafeDefault sToolbarScrollAblation =
             newMutableFlagWithSafeDefault(TOOLBAR_SCROLL_ABLATION, false);
-    public static final MutableFlagWithSafeDefault sToolbarTabletResizeRefactor =
-            newMutableFlagWithSafeDefault(TOOLBAR_TABLET_RESIZE_REFACTOR, false);
     // keep-sorted end
 
     // CachedFeatureParam instances.
