a9faa0406f244ebd65013c4bbf23918aad2d0415
Peter Wen <wnwen@chromium.org>
2025-09-10 12:17:30 -0700

    build_speed: Add cta_test_sig to the timeall script
    This allows my local cron job to pick it up as one of the potential
    benchmarks.
    
    Also ensure that a test target is used for test benchmarks.
    
    R=hnakashima@chromium.org
    
    Bug: 391374982
    Change-Id: Ice0381ffe6ec3af60989b5683d8351154eaddc5e
    Fixed: 391374982
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6936785
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Auto-Submit: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513870}

--- tools/android/build_speed/timeall.py
+++ tools/android/build_speed/timeall.py
@@ -68,6 +68,10 @@ class Options:
 
 def run_benchmark(options: Options):
     outdir_name = f"out/Debug"
+    if '_test_' in options.benchmark:
+        target = "chrome_test_apk"
+    else:
+        target = "chrome_apk"
     cmd = [
         "tools/android/build_speed/benchmark.py",
         "-vv",
@@ -75,7 +79,7 @@ def run_benchmark(options: Options):
         "-C",
         outdir_name,
         "--target",
-        "chrome_apk",
+        target,
     ]
     if options.e:
         cmd.extend(["--emulator", options.e])
@@ -114,6 +118,7 @@ def main():
         "chrome_nosig",
         "base_sig",
         "module_internal_nosig",
+        "cta_test_sig",
     ]
 
     if args.debug:
