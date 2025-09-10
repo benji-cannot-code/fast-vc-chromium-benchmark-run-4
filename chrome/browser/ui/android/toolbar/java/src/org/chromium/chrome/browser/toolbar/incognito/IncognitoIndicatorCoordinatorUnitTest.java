f0ae6356c0dae92b9519e83aacd2147987578690
Charles Hager <clhager@google.com>
2025-09-10 11:15:24 -0700

    Add Incognito Indicator to tablet ranking
    This change adds the incognito indicator to the dynamic tablet ranking.
    As the indicator contains a text view, whose final width cannot be
    determined with certainty before being measured, ToolbarTablet will
    verify the width after the initial onMeasure pass and potentially
    re-measure things if the predicted width doesn't match the final width.
    This should at most trigger one "double" measure pass per session per
    language.
    
    https://screencast.googleplex.com/cast/NjMyODYxMTEzMTg4MzUyMHxkNmZiZTgwMS04ZQ
    
    https://screencast.googleplex.com/cast/NDgyMzM4OTg0Njg5NjY0MHxjOGQ5ZWJiZi01MQ
    
    Bug: 427260734
    Change-Id: I43c1a154eb7cfb631ee88f394d186360b9aee7c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6897192
    Commit-Queue: Charles Hager <clhager@google.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1513845}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/incognito/IncognitoIndicatorCoordinatorUnitTest.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/incognito/IncognitoIndicatorCoordinatorUnitTest.java
@@ -5,13 +5,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.toolbar.incognito;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.clearInvocations;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.view.View;
 import android.view.ViewStub;
 
@@ -25,6 +32,7 @@ import org.mockito.junit.MockitoRule;
 import org.robolectric.annotation.Config;
 
 import org.chromium.base.test.BaseRobolectricTestRunner;
+import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.tabmodel.IncognitoStateProvider;
@@ -37,6 +45,8 @@ import org.chromium.chrome.browser.toolbar.top.ToolbarLayout;
 @Config(manifest = Config.NONE)
 @EnableFeatures(ChromeFeatureList.TAB_STRIP_INCOGNITO_MIGRATION)
 public class IncognitoIndicatorCoordinatorUnitTest {
+    private static final int BUTTON_WIDTH = 40;
+
     @Rule public MockitoRule mMockitoRule = MockitoJUnit.rule();
 
     @Mock private ToolbarLayout mParentToolbar;
@@ -44,6 +54,8 @@ public class IncognitoIndicatorCoordinatorUnitTest {
     @Mock private IncognitoStateProvider mIncognitoStateProvider;
     @Mock private ViewStub mIncognitoIndicatorStub;
     @Mock private View mIncognitoIndicatorView;
+    @Mock private Context mContext;
+    @Mock private Resources mResources;
 
     private IncognitoIndicatorCoordinator mCoordinator;
 
@@ -52,6 +64,9 @@ public class IncognitoIndicatorCoordinatorUnitTest {
         when(mParentToolbar.findViewById(eq(R.id.incognito_indicator_stub)))
                 .thenReturn(mIncognitoIndicatorStub);
         when(mIncognitoIndicatorStub.inflate()).thenReturn(mIncognitoIndicatorView);
+        when(mParentToolbar.getContext()).thenReturn(mContext);
+        when(mContext.getResources()).thenReturn(mResources);
+        when(mResources.getDimensionPixelSize(anyInt())).thenReturn(BUTTON_WIDTH);
 
         mCoordinator =
                 new IncognitoIndicatorCoordinator(
@@ -61,7 +76,7 @@ public class IncognitoIndicatorCoordinatorUnitTest {
                         /* visible= */ false);
         assertNull(
                 "Indicator should not be inflated initially.",
-                mCoordinator.getIncognitoIndicatorViewForTesting());
+                mCoordinator.getIncognitoIndicatorView());
     }
 
     @Test
@@ -72,13 +87,11 @@ public class IncognitoIndicatorCoordinatorUnitTest {
         mCoordinator.onIncognitoStateChanged(/* isIncognito= */ false);
         assertNull(
                 "Indicator should not be inflated when not in incognito.",
-                mCoordinator.getIncognitoIndicatorViewForTesting());
+                mCoordinator.getIncognitoIndicatorView());
 
         // Transition to incognito.
         mCoordinator.onIncognitoStateChanged(/* isIncognito= */ true);
-        assertNotNull(
-                "Indicator should be inflated.",
-                mCoordinator.getIncognitoIndicatorViewForTesting());
+        assertNotNull("Indicator should be inflated.", mCoordinator.getIncognitoIndicatorView());
         verify(mIncognitoIndicatorView).setVisibility(View.VISIBLE);
         clearInvocations(mIncognitoIndicatorView);
 
@@ -88,26 +101,82 @@ public class IncognitoIndicatorCoordinatorUnitTest {
     }
 
     @Test
-    public void testUpdateVisibility_TogglesVisibility() {
+    public void testSetVisibility_TogglesVisibility() {
         // Start in incognito.
         mCoordinator.onIncognitoStateChanged(/* isIncognito= */ true);
-        assertNotNull(
-                "Indicator should be inflated.",
-                mCoordinator.getIncognitoIndicatorViewForTesting());
+        assertNotNull("Indicator should be inflated.", mCoordinator.getIncognitoIndicatorView());
         verify(mIncognitoIndicatorView).setVisibility(View.GONE);
         clearInvocations(mIncognitoIndicatorView);
 
         // Show toolbar buttons.
         mCoordinator.setVisibility(/* visible= */ true);
-        assertNotNull(
-                "Indicator should be inflated.",
-                mCoordinator.getIncognitoIndicatorViewForTesting());
+        assertNotNull("Indicator should be inflated.", mCoordinator.getIncognitoIndicatorView());
         verify(mIncognitoIndicatorView).setVisibility(View.VISIBLE);
         clearInvocations(mIncognitoIndicatorView);
 
         // Hide toolbar buttons.
         mCoordinator.setVisibility(/* visible= */ false);
         verify(mIncognitoIndicatorView).setVisibility(View.GONE);
+        clearInvocations(mIncognitoIndicatorView);
+    }
+
+    @Test
+    @EnableFeatures(ChromeFeatureList.TOOLBAR_TABLET_RESIZE_REFACTOR)
+    @DisableFeatures(ChromeFeatureList.TAB_STRIP_INCOGNITO_MIGRATION)
+    public void testUpdateVisibility_TabStripMigrationDisabled() {
+        assertEquals(0, mCoordinator.updateVisibility(300));
+        assertNull("Indicator should not be inflated.", mCoordinator.getIncognitoIndicatorView());
+    }
+
+    @Test
+    @EnableFeatures(ChromeFeatureList.TOOLBAR_TABLET_RESIZE_REFACTOR)
+    public void testUpdateVisibility_ToggleIncognito() {
+        doReturn(0).when(mIncognitoIndicatorView).getMeasuredWidth();
+
+        // Start not in incognito.
+        mCoordinator.onIncognitoStateChanged(/* isIncognito= */ false);
+        assertEquals(0, mCoordinator.updateVisibility(500));
+        assertNull("Indicator should not be inflated.", mCoordinator.getIncognitoIndicatorView());
+
+        // Switch to incognito.
+        mCoordinator.onIncognitoStateChanged(/* isIncognito= */ true);
+        assertEquals(
+                "The coordinator should have consumed 3 times the button width by default.",
+                120,
+                mCoordinator.updateVisibility(500));
+        assertNotNull("Indicator should be inflated.", mCoordinator.getIncognitoIndicatorView());
+        verify(mIncognitoIndicatorView).setVisibility(View.VISIBLE);
+        assertTrue(mCoordinator.needsUpdateBeforeShowing());
+        clearInvocations(mIncognitoIndicatorView);
+
+        assertEquals(
+                "The coordinator should still consume 3 times the button width.",
+                120,
+                mCoordinator.updateVisibility(500));
+        verify(mIncognitoIndicatorView).setVisibility(View.VISIBLE);
+        assertTrue(mCoordinator.needsUpdateBeforeShowing());
+        clearInvocations(mIncognitoIndicatorView);
+
+        // Update the indicator's width measured width.
+        doReturn(100).when(mIncognitoIndicatorView).getMeasuredWidth();
+        assertTrue(mCoordinator.needsUpdateBeforeShowing());
+
+        assertEquals(
+                "The coordinator should now consume the previously measured width of the"
+                        + " indicator.",
+                100,
+                mCoordinator.updateVisibility(500));
+        verify(mIncognitoIndicatorView).setVisibility(View.VISIBLE);
+        assertFalse(mCoordinator.needsUpdateBeforeShowing());
+        clearInvocations(mIncognitoIndicatorView);
+
+        // Hide the indicator when there isn't enough available width.
+        assertEquals(
+                "The coordinator should consume the remaining width, but not show.",
+                50,
+                mCoordinator.updateVisibility(50));
+        verify(mIncognitoIndicatorView).setVisibility(View.GONE);
+        assertFalse(mCoordinator.needsUpdateBeforeShowing());
         clearInvocations(mIncognitoIndicatorView);
     }
 }
