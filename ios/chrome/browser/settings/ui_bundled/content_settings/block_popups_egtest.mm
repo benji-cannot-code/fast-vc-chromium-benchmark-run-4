2b7d0f2464088fe3bc68b329e690be3b4507d83a
Federica Germinario <fedegermi@google.com>
2025-09-10 05:32:38 -0700

    [ios] Disable test testAccessibilityOfBlockPopupSettings on iOS26
    Bug: 444168578
    Change-Id: I8fc6ff3f78363fd4fa46a9a42f43b5e92a5d7209
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6935468
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Nohemi Fernandez <fernandex@google.com>
    Reviewed-by: Nohemi Fernandez <fernandex@google.com>
    Cr-Commit-Position: refs/heads/main@{#1513640}

--- ios/chrome/browser/settings/ui_bundled/content_settings/block_popups_egtest.mm
+++ ios/chrome/browser/settings/ui_bundled/content_settings/block_popups_egtest.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
+#import "base/ios/ios_util.h"
 #import "base/strings/sys_string_conversions.h"
 #import "ios/chrome/browser/settings/ui_bundled/content_settings/block_popups_app_interface.h"
 #import "ios/chrome/grit/ios_strings.h"
@@ -83,6 +84,10 @@ class ScopedBlockPopupsException {
 // Opens the block popups settings page and verifies that accessibility is set
 // up properly.
 - (void)testAccessibilityOfBlockPopupSettings {
+  // TODO(crbug.com/444168578): Re-enable the test.
+  if (base::ios::IsRunningOnIOS26OrLater()) {
+    EARL_GREY_TEST_DISABLED(@"Test disabled on iOS 26.");
+  }
   [ChromeEarlGreyUI openSettingsMenu];
   [ChromeEarlGreyUI tapSettingsMenuButton:ContentSettingsButton()];
   [[EarlGrey selectElementWithMatcher:BlockPopupsSettingsButton()]
