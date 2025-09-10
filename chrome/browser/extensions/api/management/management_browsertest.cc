98714295c2540c3d422870ebc95c103a3d8ab5ea
James Cook <jamescook@chromium.org>
2025-09-10 13:45:52 -0700

    extensions: Port management API tests to desktop Android, part 1
    All the tests in management_api.cc pass on desktop Android without
    modification, so just add them to the build.
    
    There are additional tests spread across multiple files in
    //chrome/browser/extensions/api/management - those will come in
    later CLs.
    
    Bug: 371332103
    Change-Id: I7215140adbe65b5cea8f94fe6ce413dab452ef72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6934071
    Reviewed-by: Zhengzheng Liu <zhzhliu@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513908}

--- chrome/browser/extensions/api/management/management_browsertest.cc
+++ chrome/browser/extensions/api/management/management_browsertest.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/updater/extension_updater.h"
 #include "chrome/browser/policy/policy_test_utils.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/browser.h"
 #include "chrome/common/url_constants.h"
 #include "components/policy/core/browser/browser_policy_connector.h"
 #include "components/policy/core/common/mock_configuration_policy_provider.h"
@@ -47,9 +46,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/pending_extension_manager.h"
 #include "extensions/browser/test_extension_registry_observer.h"
 #include "extensions/browser/updater/extension_downloader.h"
+#include "extensions/buildflags/buildflags.h"
 #include "extensions/common/mojom/view_type.mojom.h"
 #include "extensions/test/extension_test_message_listener.h"
 #include "testing/gmock/include/gmock/gmock.h"
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 using content::BrowserThread;
 using extensions::Extension;
