0b1b2eea3759f31224e2108d096669cfb7f356b0
Tony Herre <toprice@chromium.org>
2025-09-10 01:02:09 -0700

    Initial interface for RTCTransport
    Add an initial empty interface for prototyping on RTCTransport,
    previously discussed as RTCRtpTransport, hidden behind the same feature.
    
    Based on philipel's crrev.com/c/6870531.
    
    Bug: 443019066
    Change-Id: I2bf4e283d3ed089024202761de8ad4be524c521b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6916045
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Tony Herre <toprice@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513547}

---
+++ third_party/blink/renderer/modules/peerconnection/rtc_transport/rtc_received_packet.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/modules/peerconnection/rtc_transport/rtc_received_packet.h"
+
+namespace blink {
+
+DOMArrayBuffer* RtcReceivedPacket::data() {
+  return data_.Get();
+}
+
+void RtcReceivedPacket::Trace(Visitor* visitor) const {
+  ScriptWrappable::Trace(visitor);
+  visitor->Trace(data_);
+}
+
+}  // namespace blink
