27a2f19ad2387d6d9c8e90ab630357e6deb25e2c
Fred Shih <ffred@chromium.org>
2025-09-10 14:19:06 -0700

    [TabStrip API] Formalize an API session object
    Previously we relied on the MutationSession object to record and replay
    events.
    
    However, now that we have a synchronous API, we'd actually like to
    prevent re-entrancy as well. That is, when there is an ongoing API
    session, we do not want to allow a call back into the API. To
    accomplish this, we will need to keep track of ongoing session state
    in the API handler.
    
    A new session controller is introduced on the tab strip service impl.
    It creates session objects and maintains the RAII pattern for replaying
    events on dtor.
    
    Change-Id: I0cf4d621b20fc5f78ba750ba71e9880b37a2ba1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6933759
    Reviewed-by: David Yeung <dayeung@chromium.org>
    Commit-Queue: Fred Shih <ffred@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513922}

--- chrome/browser/ui/tabs/tab_strip_api/tab_strip_service_impl.cc
+++ chrome/browser/ui/tabs/tab_strip_api/tab_strip_service_impl.cc
@@ -25,22 +25,52 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace tabs_api {
 
-// Starts a mutation session that suppresses incoming messages to prevent
-// re-entrancy and replays all recorded mutations on session destruction.
-class MutationSession {
+// Starts a session that suppresses incoming messages to prevent re-entrancy and
+// replays all recorded mutations on session destruction.
+class ApiSession : public TabStripServiceImpl::Session {
  public:
-  explicit MutationSession(tabs_api::events::TabStripEventRecorder* recorder)
-      : recorder_(recorder) {
-    recorder_->StopNotificationAndStartRecording();
-  }
+  using OnApiSessionEndCb = base::OnceCallback<void()>;
 
-  ~MutationSession() { recorder_->PlayRecordingsAndStartNotification(); }
+  explicit ApiSession(OnApiSessionEndCb cb)
+      : on_session_ended_(std::move(cb)) {}
+
+  ~ApiSession() override { std::move(on_session_ended_).Run(); }
 
   // Disallow copy and assign.
-  MutationSession(const MutationSession&) = delete;
-  MutationSession& operator=(const MutationSession&) = delete;
+  ApiSession(const ApiSession&) = delete;
+  ApiSession& operator=(const ApiSession&) = delete;
 
  private:
+  base::OnceCallback<void()> on_session_ended_;
+};
+
+class SessionControllerImpl : public TabStripServiceImpl::SessionController {
+ public:
+  explicit SessionControllerImpl(
+      tabs_api::events::TabStripEventRecorder* recorder)
+      : recorder_(recorder) {}
+
+  ~SessionControllerImpl() override = default;
+
+  std::unique_ptr<TabStripServiceImpl::Session> CreateSession() override {
+    CHECK(!session_in_progress_)
+        << "re-entrancy into tab strip service is not allowed";
+
+    recorder_->StopNotificationAndStartRecording();
+    auto session = std::make_unique<ApiSession>(base::BindOnce(
+        &SessionControllerImpl::EndSession, base::Unretained(this)));
+    session_in_progress_ = true;
+
+    return std::move(session);
+  }
+
+  void EndSession() {
+    recorder_->PlayRecordingsAndStartNotification();
+    session_in_progress_ = false;
+  }
+
+ private:
+  bool session_in_progress_ = false;
   raw_ptr<tabs_api::events::TabStripEventRecorder> recorder_;
 };
 
@@ -60,6 +90,9 @@ TabStripServiceImpl::TabStripServiceImpl(
       tab_strip_model_adapter_.get(),
       base::BindRepeating(&TabStripServiceImpl::BroadcastEvents,
                           base::Unretained(this)));
+  session_controller_ =
+      std::make_unique<SessionControllerImpl>(recorder_.get());
+
   tab_strip_model_adapter_->AddObserver(recorder_.get());
 }
 
@@ -72,11 +105,15 @@ void TabStripServiceImpl::BroadcastEvents(
 }
 
 TabStripService::GetTabsResult TabStripServiceImpl::GetTabs() {
+  auto session = session_controller_->CreateSession();
+
   return tab_strip_model_adapter_->GetTabStripTopology();
 }
 
 mojom::TabStripService::GetTabResult TabStripServiceImpl::GetTab(
     const tabs_api::NodeId& tab_mojom_id) {
+  auto session = session_controller_->CreateSession();
+
   if (tab_mojom_id.Type() != tabs_api::NodeId::Type::kContent) {
     return base::unexpected(
         mojo_base::mojom::Error::New(mojo_base::mojom::Code::kInvalidArgument,
@@ -91,7 +128,7 @@ mojom::TabStripService::GetTabResult TabStripServiceImpl::GetTab(
 
   tabs_api::mojom::TabPtr tab_result;
   // TODO (crbug.com/412709270) TabStripModel or TabCollections should have an
-  // api that can fetch id without of relying on indexes.
+  // api that can fetch id without of relying  on indices.
   auto tabs = tab_strip_model_adapter_->GetTabs();
   for (unsigned int i = 0; i < tabs.size(); ++i) {
     auto& handle = tabs.at(i);
@@ -116,7 +153,7 @@ mojom::TabStripService::GetTabResult TabStripServiceImpl::GetTab(
 mojom::TabStripService::CreateTabAtResult TabStripServiceImpl::CreateTabAt(
     const std::optional<tabs_api::Position>& pos,
     const std::optional<GURL>& url) {
-  MutationSession recorder_session(recorder_.get());
+  auto session = session_controller_->CreateSession();
 
   GURL target_url;
   if (url.has_value()) {
@@ -158,7 +195,7 @@ mojom::TabStripService::CreateTabAtResult TabStripServiceImpl::CreateTabAt(
 
 mojom::TabStripService::CloseTabsResult TabStripServiceImpl::CloseTabs(
     const std::vector<tabs_api::NodeId>& ids) {
-  MutationSession recorder_session(recorder_.get());
+  auto session = session_controller_->CreateSession();
 
   std::vector<int32_t> tab_content_targets;
   for (const auto& id : ids) {
@@ -199,7 +236,7 @@ mojom::TabStripService::CloseTabsResult TabStripServiceImpl::CloseTabs(
 
 mojom::TabStripService::ActivateTabResult TabStripServiceImpl::ActivateTab(
     const tabs_api::NodeId& id) {
-  MutationSession recorder_session(recorder_.get());
+  auto session = session_controller_->CreateSession();
 
   if (id.Type() != tabs_api::NodeId::Type::kContent) {
     return base::unexpected(
@@ -228,7 +265,7 @@ mojom::TabStripService::SetSelectedTabsResult
 TabStripServiceImpl::SetSelectedTabs(
     const std::vector<tabs_api::NodeId>& selection,
     const tabs_api::NodeId& tab_to_activate) {
-  MutationSession recorder_session(recorder_.get());
+  auto session = session_controller_->CreateSession();
 
   if (std::find(selection.begin(), selection.end(), tab_to_activate) ==
       selection.end()) {
@@ -272,7 +309,7 @@ TabStripServiceImpl::SetSelectedTabs(
 mojom::TabStripService::MoveTabResult TabStripServiceImpl::MoveTab(
     const tabs_api::NodeId& id,
     const tabs_api::Position& position) {
-  MutationSession recorder_session(recorder_.get());
+  auto session = session_controller_->CreateSession();
 
   if (position.index() >= tab_strip_model_adapter_->GetTabs().size()) {
     return base::unexpected(
@@ -313,6 +350,8 @@ mojom::TabStripExperimentService::UpdateTabGroupVisualResult
 TabStripServiceImpl::UpdateTabGroupVisual(
     const tabs_api::NodeId& id,
     const tab_groups::TabGroupVisualData& visual_data) {
+  auto session = session_controller_->CreateSession();
+
   if (id.Type() != tabs_api::NodeId::Type::kCollection) {
     return base::unexpected(mojo_base::mojom::Error::New(
         mojo_base::mojom::Code::kInvalidArgument, "id must be a collection"));
