79d904c9dd6bc5b5d21a95bcad9a9228b0df94a7
Bryant Chandler <bryantchandler@chromium.org>
2025-09-10 10:06:06 -0700

    Revert "Create GlicInstanceComponents, a per instance class"
    This reverts commit 1e836a8f65b25b4a73fbaf4b29893abd687771c5.
    
    Reason for revert: Unsafe static casts are causing crashes when GlicMultiInstance is enabled.
    
    Original change's description:
    > Create GlicInstanceComponents, a per instance class
    >
    > Initially moving GlicSharingManager and GlicZeroStateSuggestionsManager
    >
    > Bug: 441737907
    > Change-Id: Iac52c334aff14f545dd23ce02b9466cbfdd7d991
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6908681
    > Auto-Submit: Andrea Gomez <andreaxg@google.com>
    > Reviewed-by: Ian Wells <iwells@chromium.org>
    > Commit-Queue: Andrea Gomez <andreaxg@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1513156}
    
    Bug: 441737907
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I6c5e1a5e1d4515d8211418b7ac5e958794bcedb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6933828
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Auto-Submit: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513792}

--- chrome/browser/glic/widget/glic_window_controller_impl.h
+++ chrome/browser/glic/widget/glic_window_controller_impl.h
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/glic/host/glic_web_client_access.h"
 #include "chrome/browser/glic/host/host.h"
 #include "chrome/browser/glic/public/glic_enabling.h"
-#include "chrome/browser/glic/public/glic_instance_components.h"
 #include "chrome/browser/glic/widget/glic_window_config.h"
 #include "chrome/browser/glic/widget/glic_window_controller.h"
 #include "chrome/browser/glic/widget/local_hotkey_manager.h"
@@ -66,12 +65,10 @@ class GlicWindowControllerImpl
   GlicWindowControllerImpl(const GlicWindowControllerImpl&) = delete;
   GlicWindowControllerImpl& operator=(const GlicWindowControllerImpl&) = delete;
 
-  GlicWindowControllerImpl(
-      Profile* profile,
-      signin::IdentityManager* identity_manager,
-      GlicKeyedService* service,
-      GlicEnabling* enabling,
-      contextual_cueing::ContextualCueingService* contextual_cueing_service);
+  GlicWindowControllerImpl(Profile* profile,
+                           signin::IdentityManager* identity_manager,
+                           GlicKeyedService* service,
+                           GlicEnabling* enabling);
   ~GlicWindowControllerImpl() override;
 
   // GlicWindowController implementation
@@ -153,8 +150,6 @@ class GlicWindowControllerImpl
   HostManager& host_manager() override;
   std::vector<Host*> GetHosts() override;
   Host* GetHostForTab(tabs::TabInterface* tab) override;
-
-  GlicInstanceComponents& glic_instance_components();
 
  private:
   // Sets the floating attributes of the glic window.
@@ -399,7 +394,6 @@ class GlicWindowControllerImpl
 
   raw_ptr<GlicKeyedService> glic_service_;  // Owns this.
   raw_ptr<GlicEnabling> enabling_;
-  GlicInstanceComponents glic_instance_components_;
   base::ScopedObservation<Host, Host::Observer> host_observation_{this};
 
   base::WeakPtrFactory<GlicWindowControllerImpl> weak_ptr_factory_{this};
