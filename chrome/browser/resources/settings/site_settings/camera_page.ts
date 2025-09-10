c2c65cc61d280fcb0740fceccf91116a2efabc92
dpapad <dpapad@chromium.org>
2025-09-10 02:11:00 -0700

    Settings re-architecture: Migrate Privacy section, part 12.
    Promote several site settings subpages to a standalone views in
    <settings-privacy-page-index>.
    
    As part of the transition to the new architecture also making these
    views searchable.
    
    Bug: 424223101
    Change-Id: I752710f82c6d514249d6c6a58559b9d34b14b00f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6929393
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513573}

--- chrome/browser/resources/settings/site_settings/camera_page.ts
+++ chrome/browser/resources/settings/site_settings/camera_page.ts
@@ -3,19 +3,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import '../settings_shared.css.js';
 import './category_setting_exceptions.js';
 import './media_picker.js';
 import './settings_category_default_radio_group.js';
 import './site_settings_shared.css.js';
+import '../settings_page/settings_subpage.js';
+import '../settings_shared.css.js';
 
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
+import {SettingsViewMixin} from '../settings_page/settings_view_mixin.js';
 import {ContentSettingsTypes} from '../site_settings/constants.js';
 
 import {getTemplate} from './camera_page.html.js';
 
-export class CameraPageElement extends PolymerElement {
+const CameraPageElementBase = SettingsViewMixin(PolymerElement);
+
+export class CameraPageElement extends CameraPageElementBase {
   static get is() {
     return 'settings-camera-page';
   }
@@ -37,6 +41,11 @@ export class CameraPageElement extends PolymerElement {
   }
 
   declare searchTerm: string;
+
+  // SettingsViewMixin implementation.
+  override focusBackButton() {
+    this.shadowRoot!.querySelector('settings-subpage')!.focusBackButton();
+  }
 }
 
 declare global {
--- chrome/browser/resources/settings/site_settings/insecure_content_page.ts
+++ chrome/browser/resources/settings/site_settings/insecure_content_page.ts
@@ -5,15 +5,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import './category_setting_exceptions.js';
 import './site_settings_shared.css.js';
+import '../settings_page/settings_subpage.js';
 import '../settings_shared.css.js';
 
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
+import {SettingsViewMixin} from '../settings_page/settings_view_mixin.js';
 import {ContentSettingsTypes} from '../site_settings/constants.js';
 
 import {getTemplate} from './insecure_content_page.html.js';
 
-export class InsecureContentPageElement extends PolymerElement {
+const InsecureContentPageElementBase = SettingsViewMixin(PolymerElement);
+
+export class InsecureContentPageElement extends InsecureContentPageElementBase {
   static get is() {
     return 'settings-insecure-content-page';
   }
@@ -35,6 +39,11 @@ export class InsecureContentPageElement extends PolymerElement {
   }
 
   declare searchTerm: string;
+
+  // SettingsViewMixin implementation.
+  override focusBackButton() {
+    this.shadowRoot!.querySelector('settings-subpage')!.focusBackButton();
+  }
 }
 
 declare global {
