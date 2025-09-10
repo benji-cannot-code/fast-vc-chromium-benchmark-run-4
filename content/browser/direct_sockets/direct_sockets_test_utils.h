fc247a6ac61bbe4147d626946e56e159a767ea8e
Vlad Krot <vkrot@google.com>
2025-09-10 05:13:59 -0700

    Multicast support in Direct Sockets #3
    This CL adds mojo interfaces for MulticastController and its
    implementation with respective browser tests and unit tests.
    
    I have added Low-Coverage-Reason because the pipeline complains about
    low coverage for udp_socket.cc and network_context.cc. For udp_socket.cc
    there's no unit tests as for now. And both tests are tested with browser
    tests. I believe my change was trivial for both of those classes.
    
    Explainer
    https://github.com/explainers-by-googlers/multicast-in-direct-sockets.
    Chrome Entry https://chromestatus.com/feature/5073740211814400 Intent to
    prototype
    https://groups.google.com/a/chromium.org/g/blink-dev/c/ADV4FZtN4nE
    
    Bug: 398934282
    Low-Coverage-Reason: TRIVIAL_CHANGE
    Change-Id: I1d8236bc2f495d6c4bf09ac995d5f1ab03f78f5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6905387
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Vlad Krot <vkrot@google.com>
    Cr-Commit-Position: refs/heads/main@{#1513631}

--- content/browser/direct_sockets/direct_sockets_test_utils.h
+++ content/browser/direct_sockets/direct_sockets_test_utils.h
@@ -109,6 +109,7 @@ class MockNetworkContext : public network::TestNetworkContextWithHostResolver {
       network::mojom::RestrictedUDPSocketParamsPtr params,
       mojo::PendingReceiver<network::mojom::RestrictedUDPSocket> receiver,
       mojo::PendingRemote<network::mojom::UDPSocketListener> listener,
+      bool allow_multicast,
       CreateRestrictedUDPSocketCallback callback) override;
 
   MockUDPSocket* get_udp_socket() {
