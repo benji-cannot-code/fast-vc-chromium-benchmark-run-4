5d31375e69eee3cd68c63f7ecd58f9c4562b38fb
Tommy Steimel <steimel@chromium.org>
2025-09-10 15:38:20 -0700

    [video pip] Handle gesture events on the replay and forward 10s buttons
    This CL adds handling for gesture taps on the replay 10 seconds and
    forward 10 seconds buttons on the video picture-in-picture window for
    the updated UI.
    
    Bug: 435372472
    Change-Id: I414c1e6603ab41d49fb1e887645df1d86215a7ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6931186
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Fr <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1513981}

--- chrome/browser/ui/views/overlay/video_overlay_window_views.cc
+++ chrome/browser/ui/views/overlay/video_overlay_window_views.cc
@@ -2451,6 +2451,12 @@ void VideoOverlayWindowViews::OnGestureEvent(ui::GestureEvent* event) {
   } else if (GetHangUpButtonBounds().Contains(event->location())) {
     controller_->HangUp();
     event->SetHandled();
+  } else if (GetReplay10SecondsButtonBounds().Contains(event->location())) {
+    Replay10Seconds();
+    event->SetHandled();
+  } else if (GetForward10SecondsButtonBounds().Contains(event->location())) {
+    Forward10Seconds();
+    event->SetHandled();
   } else if (GetLiveCaptionButtonBounds().Contains(event->location())) {
     OnLiveCaptionButtonPressed();
     event->SetHandled();
