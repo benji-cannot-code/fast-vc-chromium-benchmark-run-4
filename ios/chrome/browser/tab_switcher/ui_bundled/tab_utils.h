758c1c0774a37a6c7dd338ae8f3968b7be52627e
Chris Lu <thegreenfrog@google.com>
2025-09-10 12:24:50 -0700

    [ios] Refactor out helper methods in GridLayout
    ItemAspectRatio() and ColumnsCount() are refactored out of GridLayout
    so that it can be used in the EmptyThumbnail layout logic, since
    which layout configuration to apply will depend on how the grid is
    laying out the cells.
    
    The input parameters for both methods are changed to allow for a more
    general use case.
    
    Bug: 443976867
    Change-Id: Ica77675225837b3cb2277dd35ba1259bdf4b71ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6901325
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1513872}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_utils.h
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_utils.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_CHROME_BROWSER_TAB_SWITCHER_UI_BUNDLED_TAB_UTILS_H_
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 #import "ios/chrome/browser/shared/model/web_state_list/tab_utils.h"
 #import "ios/web/public/web_state_id.h"
@@ -17,6 +18,15 @@ class BrowserList;
 @class TabItem;
 @class TabSwitcherItem;
 class WebStateList;
+
+// Returns the aspect ratio (height / width) of an item based on the container
+// `size`.
+CGFloat TabGridItemAspectRatio(CGSize size);
+
+// Returns the number of columns based on based on the container `size` and
+// `content_size_category`.
+NSInteger TabGridColumnsCount(CGSize size,
+                              UIContentSizeCategory content_size_category);
 
 // Returns the TabItem object representing the tab with the given `criteria`.
 // Returns `nil` if the tab is not found.
