c55de97cb91fa30db951e87d17bbf59266b6db24
Dibyajyoti Pal <dibyapal@google.com>
2025-09-10 09:34:05 -0700

    [PWA/Trusted Icons] Final changes to callsites using manifest icons
    This CL is a result of manually going through all callsites of
    WebAppInstallInfo::icon_bitmaps and changing them if required to
    instead use trusted_icon_bitmaps instead. The changes that needed an update are:
    1. Kiosk app installs now use the trusted icon bitmap.
    2. The GeneratedIconFix architecture will also attempt to fix trusted
       icon bitmaps.
    3. The WebInstallService implementation now supports masking.
    
    Some call sites, like the ApkWebAppInstaller and SystemWebApp related
    code has not been updated, since they do not follow the current
    manifest update process, and shouldn't rely on trusted icons.
    
    Fixed: 430552045
    Change-Id: I5059cee754d10dd3523cc23ed66715d5f08f8368
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6931510
    Reviewed-by: Sergii Bykov <sbykov@google.com>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513766}

--- chrome/browser/ash/app_mode/web_app/kiosk_web_app_data.h
+++ chrome/browser/ash/app_mode/web_app/kiosk_web_app_data.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/values.h"
 #include "chrome/browser/ash/app_mode/kiosk_app_data_base.h"
+#include "chrome/browser/web_applications/web_app_icon_generator.h"
 #include "chrome/browser/web_applications/web_app_install_info.h"
 #include "components/account_id/account_id.h"
 #include "ui/gfx/image/image_skia.h"
@@ -77,7 +78,7 @@ class KioskWebAppData : public KioskAppDataBase {
 
   void UpdateAppInfo(const std::string& title,
                      const GURL& start_url,
-                     const web_app::IconBitmaps& icon_bitmaps);
+                     const web_app::SizeToBitmap& icon_bitmaps);
 
   void SetOnLoadedCallbackForTesting(base::OnceClosure callback);
 
