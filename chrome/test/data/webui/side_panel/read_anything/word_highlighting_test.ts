6f6524e28c2a8e1865951e6b33ea558cda4adab3
Lauren Winston <lwinston@google.com>
2025-09-10 13:20:55 -0700

    [Read aloud] Allow word highlighting to work with TS segmentation.
    This does not address all word highlighting edge cases and is intended
    to be a start.
    
    Bug: 440400392,444029398
    Change-Id: I0688c9055c478d19a1752ef19e5dccf533963c52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6933758
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1513894}

--- chrome/test/data/webui/side_panel/read_anything/word_highlighting_test.ts
+++ chrome/test/data/webui/side_panel/read_anything/word_highlighting_test.ts
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import type {AppElement} from 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
-import {ContentController, ReadAloudHighlighter, SpeechBrowserProxyImpl, SpeechController, ToolbarEvent, VoiceLanguageController, WordBoundaries} from 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
+import {ContentController, ReadAloudHighlighter, setInstance, SpeechBrowserProxyImpl, SpeechController, ToolbarEvent, VoiceLanguageController, WordBoundaries} from 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
 import {assertEquals, assertFalse, assertLE, assertTrue} from 'chrome-untrusted://webui-test/chai_assert.js';
 
 import {createApp, createSpeechSynthesisVoice, emitEvent, playFromSelectionWithMockTimer, setSimpleAxTreeWithText} from './common.js';
@@ -73,6 +73,9 @@ suite('WordHighlighting', () => {
     speechController = new SpeechController();
     SpeechController.setInstance(speechController);
     ContentController.setInstance(new ContentController());
+
+    // Reset the browser proxy.
+    setInstance(null);
 
     app = await createApp();
     chrome.readingMode.setContentForTesting(axTree, [2, 4]);
@@ -146,7 +149,12 @@ suite('WordHighlighting', () => {
     const currentHighlight =
         app.$.container.querySelector('.current-read-highlight');
     assertTrue(!!currentHighlight);
-    assertEquals('4:', currentHighlight.textContent);
+    let expectedHighlight = '4';
+    if (!chrome.readingMode.isTsTextSegmentationEnabled) {
+      // The V8 model appends the colon to the highlight.
+      expectedHighlight += ':';
+    }
+    assertEquals(expectedHighlight, currentHighlight.textContent);
   });
 
   test('word highlighting date across nodes with charLength', () => {
@@ -302,7 +310,12 @@ suite('WordHighlighting', () => {
     // Verify that we're highlighting from the selected point.
     assertTrue(!!currentHighlight);
     assertTrue(!!currentHighlight.textContent);
-    assertEquals('This ', currentHighlight.textContent);
+    let expectedHighlight = 'This';
+    if (!chrome.readingMode.isTsTextSegmentationEnabled) {
+      // The V8 model appends a space in the word highlight.
+      expectedHighlight += ' ';
+    }
+    assertEquals(expectedHighlight, currentHighlight.textContent);
     // Verify that the word boundary state has been reset.
     assertFalse(wordBoundaries.hasBoundaries());
   });
