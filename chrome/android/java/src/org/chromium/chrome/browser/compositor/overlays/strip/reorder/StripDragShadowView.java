e7b31df898ecb52d85eb1af44b7e3ea7ea4277c7
Hitarth Kothari <hitarthkothari@google.com>
2025-09-10 09:49:47 -0700

    [Tab Highlighting] Change background color for dragged tabs shadow view.
    Bug: 444007395
    Change-Id: If5d529047eee07c97ca30711b2c35cd80909d805
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6931437
    Auto-Submit: Hitarth Kothari <hitarthkothari@google.com>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513781}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/reorder/StripDragShadowView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/reorder/StripDragShadowView.java
@@ -191,8 +191,7 @@ public class StripDragShadowView extends FrameLayout {
 
         // Background color
         mCardView.setBackgroundTintList(
-                ColorStateList.valueOf(
-                        TabUiThemeUtil.getTabStripSelectedTabColor(context, isIncognito)));
+                ColorStateList.valueOf(TabUiThemeUtil.getDraggedTabBackgroundColor(context)));
 
         // Title text
         LayerTitleCache layerTitleCache = mLayerTitleCacheSupplier.get();
@@ -238,8 +237,7 @@ public class StripDragShadowView extends FrameLayout {
 
         // Background color
         mCardView.setBackgroundTintList(
-                ColorStateList.valueOf(
-                        TabUiThemeUtil.getTabStripSelectedTabColor(context, isIncognito)));
+                ColorStateList.valueOf(TabUiThemeUtil.getDraggedTabBackgroundColor(context)));
 
         // Multi tab title text
         int numberOfSelectedTabs = multiSelectedTabs.size();
--- chrome/android/junit/src/org/chromium/chrome/browser/compositor/overlays/strip/reorder/StripDragShadowViewUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/compositor/overlays/strip/reorder/StripDragShadowViewUnitTest.java
@@ -328,8 +328,7 @@ public class StripDragShadowViewUnitTest {
 
         // Verify card color
         @ColorRes
-        int expectedBackgroundColor =
-                TabUiThemeUtil.getTabStripSelectedTabColor(mActivity, incognito);
+        int expectedBackgroundColor = TabUiThemeUtil.getDraggedTabBackgroundColor(mActivity);
         assertEquals(
                 "Unexpected card color.",
                 expectedBackgroundColor,
