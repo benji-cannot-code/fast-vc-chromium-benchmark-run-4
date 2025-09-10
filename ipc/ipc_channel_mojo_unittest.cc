16b5957d58dbab6f88cd0a0f7223440a7d40b69e
Tom Sepez <tsepez@chromium.org>
2025-09-10 10:23:53 -0700

    Avoid stray includes of ipc_mojo_message_helper.h
    None of these files reference it directly.
    
    Change-Id: I17c825ab41118ad78f6cdcf48c7653ae39fe3813
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6933181
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Auto-Submit: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513808}

--- ipc/ipc_channel_mojo_unittest.cc
+++ ipc/ipc_channel_mojo_unittest.cc
@@ -52,7 +52,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ipc/ipc_message.h"
 #include "ipc/ipc_message_utils.h"
 #include "ipc/ipc_mojo_handle_attachment.h"
-#include "ipc/ipc_mojo_message_helper.h"
 #include "ipc/ipc_mojo_param_traits.h"
 #include "ipc/ipc_sync_channel.h"
 #include "ipc/ipc_sync_message.h"
