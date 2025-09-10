45854e09b54a054a43c45e31d5600b7cd30df713
Antonio Sartori <antoniosartori@chromium.org>
2025-09-10 06:38:02 -0700

    Revert "Avoid redundant call to NotifyNavigationStateChanged."
    This reverts commit 7d1910ab3a702eb2b7a8ac8eb69e13da59a0a5b0.
    
    Reason for revert: Causes flakiness (test fails almost always) of ExtensionURLRewriteBrowserTest.NewTabPageURLOverride on linux-wayland-mutter-rel-tests, see for example the first failure https://ci.chromium.org/ui/p/chromium/builders/ci/linux-wayland-mutter-rel-tests/7790/overview. Can be reproduced.
    
    Original change's description:
    > Avoid redundant call to NotifyNavigationStateChanged.
    >
    > During RendererDidNavigate, there are often two identical calls to
    > NotifyNavigationStateChanged, both of which are time consuming. This CL
    > ensures that exactly 1 call will be made when the function returns. It
    > accomplishes this via a class scoped to RendererDidNavigate that invokes
    > the call to NotifyNavigationStateChanged in its destructor. Where the
    > function used to be called unconditionally, it now "arms" the new
    > ScopedDeferNotifyNavigationStateChanged class (if the class was passed
    > in as a parameter to the function). The class is only passed in to
    > helper functions from RendererDidNavigate; all other code paths will not
    > have this passed in and will continue to unconditionally send the
    > notification.
    >
    > This change is guarded behind the following killswitch:
    > SkipRedundantNavigationStateNotification
    >
    > This CL also fixes an issue on Android where the tab URL tracked by the
    > LocationBarModel and the CustomTabToolbar can get out of sync for
    > trusted CDN pages. This is a bug that was exposed when the extra
    > NotifyNavigationStateChanged call was removed.
    >
    > Change-Id: I7f17af7dacd87e3fb42fb13a97e949479d2e803f
    > Enabled-by-default-reason: killswitch
    > Bug: 441518347
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6538910
    > Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    > Reviewed-by: Matthew Jones <mdjones@chromium.org>
    > Commit-Queue: Liam Brady <lbrady@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1511173}
    
    Bug: 441518347
    Change-Id: I0bd8aa96a17d969c22c6e9b66cacbffccbd5601a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6935469
    Owners-Override: Antonio Sartori <antoniosartori@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513665}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
@@ -848,7 +848,6 @@ public class ToolbarManager
         mToolbarLayout = mActivity.findViewById(R.id.toolbar);
         mToolbarPositionSupplier = new ObservableSupplierImpl<>(ControlsPosition.NONE);
         mNtpDelegate = createNewTabPageDelegate();
-        mIsCustomTab = mToolbarLayout instanceof CustomTabToolbar;
 
         mLocationBarModel =
                 new LocationBarModel(
@@ -869,8 +868,7 @@ public class ToolbarManager
                                 return ret;
                             }
                         },
-                        mToolbarPositionSupplier,
-                        /* matchTrustedCdnUrl= */ mIsCustomTab);
+                        mToolbarPositionSupplier);
         mControlContainer = controlContainer;
         mToolbarHairline = mControlContainer.findViewById(R.id.toolbar_hairline);
 
@@ -948,6 +946,7 @@ public class ToolbarManager
         ThemeColorProvider overviewModeThemeColorProvider = mAppThemeColorProvider;
 
         Runnable requestFocusRunnable = compositorViewHolder::requestFocus;
+        mIsCustomTab = mToolbarLayout instanceof CustomTabToolbar;
         ThemeColorProvider menuButtonThemeColorProvider =
                 mIsCustomTab ? mCustomTabThemeColorProvider : browsingModeThemeColorProvider;
 
