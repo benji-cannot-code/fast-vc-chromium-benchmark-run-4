48b2f2e0ea9746bd361f2033cf7fd00772ad34c0
William Liu <liuwilliam@chromium.org>
2025-09-10 11:01:33 -0700

    Adjust default programmatic scroll animation cruve based on platform.
    We actually never ran the ProgrammaticScrollAnimationOverride experiment
    on Android so all the curve change should be guarded against Android.
    
    bug: b:414847508
    Change-Id: I5b5f54d733d78bc886a10fea1fd9afabbfdfa98c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6934086
    Commit-Queue: William Liu <liuwilliam@chromium.org>
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Auto-Submit: William Liu <liuwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513838}

--- third_party/blink/renderer/core/page/scrolling/scroll_into_view_test.cc
+++ third_party/blink/renderer/core/page/scrolling/scroll_into_view_test.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/test/scoped_feature_list.h"
+#include "cc/base/features.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/features_generated.h"
 #include "third_party/blink/public/mojom/frame/find_in_page.mojom-blink.h"
@@ -37,7 +38,30 @@ namespace blink {
 
 namespace {
 
-class ScrollIntoViewTest : public SimTest {};
+class ScrollIntoViewTest : public SimTest {
+ public:
+  ScrollIntoViewTest() {
+    std::vector<base::test::FeatureRefAndParams> enabled_features = {
+        {
+            ::features::kProgrammaticScrollAnimationOverride,
+            {
+                {"cubic_bezier_x1", "0.4"},          //
+                {"cubic_bezier_y1", "0.0"},          //
+                {"cubic_bezier_x2", "0.0"},          //
+                {"cubic_bezier_y2", "1.0"},          //
+                {"max_animation_duration", "1.5s"},  //
+            }  //
+        }  //
+    };
+    scoped_feature_list_.InitWithFeaturesAndParameters(
+        /*enabled_features=*/
+        std::move(enabled_features),
+        /*disabled_features=*/{});
+  }
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
+};
 
 TEST_F(ScrollIntoViewTest, InstantScroll) {
   v8::HandleScope HandleScope(
