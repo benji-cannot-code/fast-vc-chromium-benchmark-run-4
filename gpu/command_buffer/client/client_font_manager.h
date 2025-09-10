57883e1b9edfc8ad809dae289e7003682674a1b6
Kelsen Liu <kelsenliu@chromium.org>
2025-09-09 19:28:01 -0700

    Spanify Serializer to avoid unsafe buffer warnings
    This CL spanifies the `Serializer` class, and enables
    unsafe-buffer-usage warnings in `mapped_memory.h`.
    
    Bug: 40285824
    Change-Id: I86b58e2661221c89cc6e925f3ca3fbae0a50014a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6904058
    Commit-Queue: kelsen liu <kelsenliu@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513419}

--- gpu/command_buffer/client/client_font_manager.h
+++ gpu/command_buffer/client/client_font_manager.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/flat_map.h"
 #include "base/containers/flat_set.h"
+#include "base/containers/span.h"
 #include "base/memory/raw_ptr.h"
 #include "gpu/command_buffer/client/client_discardable_manager.h"
 #include "gpu/raster_export.h"
@@ -24,7 +25,7 @@ class RASTER_EXPORT ClientFontManager
    public:
     virtual ~Client() {}
 
-    virtual void* MapFontBuffer(uint32_t size) = 0;
+    virtual base::span<uint8_t> MapFontBuffer(uint32_t size) = 0;
   };
 
   ClientFontManager(Client* client, CommandBuffer* command_buffer);
