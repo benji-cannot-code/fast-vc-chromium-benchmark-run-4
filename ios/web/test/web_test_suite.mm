f4f72122f8394d94fcb70aee279f86f18cb6cae5
Greg Thompson <grt@chromium.org>
2025-09-10 08:28:38 -0700

    [ios] Only load locale pak files in Chrome tests
    Non-Chrome tests do not have locale pak files, so they should load only
    the test-only resources.pak file. This change moves loading of locales
    up into IOSChromeUnitTestSuite (which has and needs them). In so doing,
    I found that //ios/web/test:packed_resources was missing a dependency on
    //ui/strings:app_locale_settings and that
    //ios/components:ios_components_unittests was missing a dependency on
    the test-only resources.pak.
    
    Bug: 432395473
    Change-Id: Ib10c9cd85d6e9443fa9c20c92f4f095829ce4915
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6769904
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513727}

--- ios/web/test/web_test_suite.mm
+++ ios/web/test/web_test_suite.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/public/test/web_test_suite.h"
 
 #import "base/check.h"
+#import "base/files/file_path.h"
 #import "base/memory/ptr_util.h"
 #import "base/path_service.h"
 #import "components/crash/core/common/objc_zombie.h"
@@ -37,16 +38,14 @@ void WebTestSuite::Initialize() {
 
   RegisterWebSchemes();
 
-  // Force unittests to run using en-US so if testing string output will work
-  // regardless of the system language.
-  ui::ResourceBundle::InitSharedInstanceWithLocale(
-      "en-US", nullptr, ui::ResourceBundle::LOAD_COMMON_RESOURCES);
-  base::FilePath resources_pack_path;
-  base::PathService::Get(base::DIR_ASSETS, &resources_pack_path);
-  resources_pack_path =
-      resources_pack_path.Append(FILE_PATH_LITERAL("resources.pak"));
-  ui::ResourceBundle::GetSharedInstance().AddDataPackFromPath(
-      resources_pack_path, ui::kScaleFactorNone);
+  // Load the resources produced by //ios/web/test:packed_resources if the test
+  // has not already loaded other resources (e.g., by //ios/chrome tests).
+  if (!ui::ResourceBundle::HasSharedInstance()) {
+    base::FilePath resources_pack_path =
+        base::PathService::CheckedGet(base::DIR_ASSETS)
+            .Append(FILE_PATH_LITERAL("resources.pak"));
+    ui::ResourceBundle::InitSharedInstanceWithPakPath(resources_pack_path);
+  }
 }
 
 void WebTestSuite::Shutdown() {
