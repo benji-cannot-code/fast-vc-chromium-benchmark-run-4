77d93ba73996f6daafe3b34058f64d3ffcc98023
Min Qin <qinmin@chromium.org>
2025-09-10 09:53:08 -0700

    Don't attach a session ID to task Id if task doesn't exist
    This will leave a dangling session and not cleaned up later.
    
    Change-Id: I3d6750f912380609e46b38f7b99d68d6337e8b35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6934098
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513783}

--- components/contextual_tasks/internal/contextual_tasks_service_impl.cc
+++ components/contextual_tasks/internal/contextual_tasks_service_impl.cc
@@ -96,9 +96,9 @@ void ContextualTasksServiceImpl::DetachUrlFromTask(const base::Uuid& task_id,
 void ContextualTasksServiceImpl::AttachSessionIdToTask(
     const base::Uuid& task_id,
     SessionID session_id) {
-  session_to_task_[session_id] = task_id;
   auto it = tasks_.find(task_id);
   if (it != tasks_.end()) {
+    session_to_task_[session_id] = task_id;
     it->second.AddSessionId(session_id);
   }
 }
@@ -124,6 +124,10 @@ ContextualTasksServiceImpl::GetMostRecentContextualTaskForSessionID(
     }
   }
   return std::nullopt;
+}
+
+size_t ContextualTasksServiceImpl::GetSessionIdMapSizeForTesting() const {
+  return session_to_task_.size();
 }
 
 }  // namespace contextual_tasks
