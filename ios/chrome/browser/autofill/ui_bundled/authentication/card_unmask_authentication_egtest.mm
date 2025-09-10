bedc020946be9acd8fd55f5204572975ed71ab01
Gauthier Ambard <gambard@chromium.org>
2025-09-10 10:12:45 -0700

    [iOS][Gardener] Flaky test CardUnmaskAuthenticationSelectionEgtest/testCardUnmaskAuthenticationSelectionShowsActivityIndicatorView
    Bug: 444090218
    Change-Id: If388204d273b1765ea39509b0b0e121aeb9b4779
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6935749
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513800}

--- ios/chrome/browser/autofill/ui_bundled/authentication/card_unmask_authentication_egtest.mm
+++ ios/chrome/browser/autofill/ui_bundled/authentication/card_unmask_authentication_egtest.mm
@@ -253,8 +253,7 @@ id<GREYMatcher> ActivityIndicatorMatcher() {
 }
 
 // TODO(crbug.com/444090218): Test is flaky.
-- (void)
-    DISABLED_testCardUnmaskAuthenticationSelectionShowsActivityIndicatorView {
+- (void)FLAKY_testCardUnmaskAuthenticationSelectionShowsActivityIndicatorView {
   [self showAuthenticationSelection];
 
   // Select the text message otp challenge option.
