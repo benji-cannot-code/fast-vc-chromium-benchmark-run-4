3e44b8a0ad52ed1de14531f44731df541d4dd535
Francis McCabe <fgm@chromium.org>
2025-09-09 22:25:52 -0700

    Remove JSPI flag from chrome
    Bug: 427473924
    Change-Id: I7df91fd7d9fb1038ea0a5620f7e537fbc7572bfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6931180
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Francis McCabe <fgm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513499}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1152,9 +1152,6 @@ extern const char kEnableZeroCopyTabCaptureDescription[];
 extern const char kExperimentalWebAssemblyFeaturesName[];
 extern const char kExperimentalWebAssemblyFeaturesDescription[];
 
-extern const char kExperimentalWebAssemblyJSPIName[];
-extern const char kExperimentalWebAssemblyJSPIDescription[];
-
 extern const char kExperimentalWebAssemblySharedEverythingName[];
 extern const char kExperimentalWebAssemblySharedEverythingDescription[];
 
