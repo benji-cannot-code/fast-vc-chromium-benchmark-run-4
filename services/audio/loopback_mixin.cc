ec9d82fcfc3f36db85059bbb882a693a8ab0d086
Olga Sharonova <olka@chromium.org>
2025-09-10 07:41:00 -0700

    gDM restrictOwnAudio: enable RestrictOwnAudioAddChromiumBack by default
    Works fine in local testing, keeping the feature flag as a kill switch.
    Enabled-by-default-reason: killswitch
    
    Bug: 437005988
    Change-Id: Ie3586bbef2dd91dbd82a46f12044f794e523f933
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6935390
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Fredrik Hernqvist <fhernqvist@google.com>
    Reviewed-by: Fredrik Hernqvist <fhernqvist@google.com>
    Auto-Submit: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513697}

--- services/audio/loopback_mixin.cc
+++ services/audio/loopback_mixin.cc
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace audio {
 
 BASE_FEATURE(kRestrictOwnAudioAddChromiumBack,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // static
 std::unique_ptr<LoopbackMixin>
