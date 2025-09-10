27ea1ccadb23e392a223a9243d32d9fe17c3ca00
Federica Germinario <fedegermi@google.com>
2025-09-10 08:26:10 -0700

    [ios] Disable tests failing in PasswordManagerTestCase on iOS26
    Tests are disabled only on device on iOS26
    
    Bug: 444185069
    Change-Id: I352fb3bb4eef6e475993f22771e364a121dbd2e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6935517
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513725}

--- ios/chrome/browser/settings/ui_bundled/password/password_manager_egtest.mm
+++ ios/chrome/browser/settings/ui_bundled/password/password_manager_egtest.mm
@@ -1510,6 +1510,13 @@ void OpenPasswordManagerWidgetPromoInstructions() {
 // Checks the order of the elements in the detail view layout for a federated
 // credential.
 - (void)testLayoutFederated {
+  // TODO(crbug.com/444185069): Re-enable the test.
+#if !TARGET_OS_SIMULATOR
+  if (base::ios::IsRunningOnIOS26OrLater()) {
+    EARL_GREY_TEST_DISABLED(@"Test disabled on iOS 26.");
+  }
+#endif
+
   GREYAssert(
       [PasswordSettingsAppInterface
           saveExampleFederatedOriginToProfileStore:
@@ -1925,6 +1932,13 @@ void OpenPasswordManagerWidgetPromoInstructions() {
 // Test that when user types text in search field, passwords and blocked
 // items are filtered out correctly.
 - (void)testSearchPasswords {
+  // TODO(crbug.com/444185069): Re-enable the test.
+#if !TARGET_OS_SIMULATOR
+  if (base::ios::IsRunningOnIOS26OrLater()) {
+    EARL_GREY_TEST_DISABLED(@"Test disabled on iOS 26.");
+  }
+#endif
+
   SaveExamplePasswordForms();
   SaveExampleBlockedFormsToProfileStore();
 
@@ -1962,6 +1976,13 @@ void OpenPasswordManagerWidgetPromoInstructions() {
 
 // Test search and delete all passwords and blocked items.
 - (void)testSearchAndDeleteAllPasswords {
+  // TODO(crbug.com/444185069): Re-enable the test.
+#if !TARGET_OS_SIMULATOR
+  if (base::ios::IsRunningOnIOS26OrLater()) {
+    EARL_GREY_TEST_DISABLED(@"Test disabled on iOS 26.");
+  }
+#endif
+
   SaveExamplePasswordForms();
   SaveExampleBlockedFormsToProfileStore();
 
@@ -2029,6 +2050,13 @@ void OpenPasswordManagerWidgetPromoInstructions() {
 
 // Test that the user can edit a password that is part of search results.
 - (void)testCanEditPasswordsFromASearch {
+  // TODO(crbug.com/444185069): Re-enable the test.
+#if !TARGET_OS_SIMULATOR
+  if (base::ios::IsRunningOnIOS26OrLater()) {
+    EARL_GREY_TEST_DISABLED(@"Test disabled on iOS 26.");
+  }
+#endif
+
   SaveExamplePasswordForms();
   OpenPasswordManager();
 
@@ -3720,6 +3748,13 @@ void OpenPasswordManagerWidgetPromoInstructions() {
 // Tests that the Password Manager is opened is search mode when opened from the
 // Search Passwords widget.
 - (void)testOpenSearchPasswordsWidget {
+  // TODO(crbug.com/444185069): Re-enable the test.
+#if !TARGET_OS_SIMULATOR
+  if (base::ios::IsRunningOnIOS26OrLater()) {
+    EARL_GREY_TEST_DISABLED(@"Test disabled on iOS 26.");
+  }
+#endif
+
   // Add a saved password to not get the Password Manager's empty state.
   SavePasswordFormToProfileStore();
 
@@ -3751,6 +3786,13 @@ void OpenPasswordManagerWidgetPromoInstructions() {
 // get automatically enabled when going back to the Password Manager when the
 // Password Manager was initially opened with the Search Passwords widget.
 - (void)testGoingBackAfterOpeningInSearchMode {
+  // TODO(crbug.com/444185069): Re-enable the test.
+#if !TARGET_OS_SIMULATOR
+  if (base::ios::IsRunningOnIOS26OrLater()) {
+    EARL_GREY_TEST_DISABLED(@"Test disabled on iOS 26.");
+  }
+#endif
+
   // Add a saved password to not get the Password Manager's empty state.
   SavePasswordFormToProfileStore();
 
