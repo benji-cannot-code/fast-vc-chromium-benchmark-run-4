a0eb0c39e44ed5b5f829a9ddbf55ff552b130c27
Levi Zim <rsworktech@outlook.com>
2025-09-09 19:36:30 -0700

    [dav1d] Enable asm for linux riscv64
    dav1d has riscv64 RVV extension support for a while now.
    This CL enables RVV optimized assembly for riscv64.
    
    dav1d has built-in cpu detection via HWCAPS so it will gracefully fall
    back to generic code when RVV is not available.
    
    config/linux/riscv64/cpu-renamed.c is created to avoid object name
    collision with cpu.S.
    
    I have tested this with a spacemit k1-x deb1 board that has RVV.
    
    Bug: none
    Change-Id: Ib76cf256eaef939115df4f3ccc93b3e756583a52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6923766
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Levi Zim <rsworktech@outlook.com>
    Cr-Commit-Position: refs/heads/main@{#1513433}

---
+++ third_party/dav1d/config/linux/riscv64/cpu-renamed.c
@@ -0,0 +1,4 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Renamed from libdav1d/src/riscv/cpu.c to avoid object name conflict with
+// libdav1d/src/riscv/64/cpu.S
+#include "../../../libdav1d/src/riscv/cpu.c"
