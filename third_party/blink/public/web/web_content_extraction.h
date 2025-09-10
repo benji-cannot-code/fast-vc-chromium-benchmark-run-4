65b43088d9d1d2cacf89551398d43bf143b2074c
Aaron Leventhal <aleventhal@google.com>
2025-09-10 13:09:43 -0700

    [Content Extraction] Add fragment_bounding_boxes
    When a layout node is fragmented (e.g. across multiple lines, columns,
    or pages), it may have multiple fragments. This happens for wrapped
    inline elements for example. This change adds support to communicate
    the bounding boxes for each fragment of a given node. This is useful
    to identify where a node is broken across multiple lines.
    
    In addition:
    - Add DCHECKs (this actually caught an issue with pseudo element text)
    - Add web test capabilities
    - Refactor and introduce comments to help code clarity. All rect
      variable names now say what coordinate system they are in.
    
    Bug: 419149271
    
    Change-Id: Id175f4ee8e32bb0aca9ba4be777721a756e7a99e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6794851
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513888}

--- third_party/blink/public/web/web_content_extraction.h
+++ third_party/blink/public/web/web_content_extraction.h
@@ -7,11 +7,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class Node;
 class WebLocalFrame;
 
 // Retrieves a dump of the content node tree for the given frame
 // and its local frame descendants.
 BLINK_EXPORT WebString DumpContentNodeTreeForTest(WebLocalFrame* frame);
+
+// Retrieves a dump of a single content node for `node`.
+BLINK_EXPORT WebString DumpContentNodeForTest(WebLocalFrame* frame, Node* node);
 
 }  // namespace blink
 
--- third_party/blink/renderer/core/testing/internals.h
+++ third_party/blink/renderer/core/testing/internals.h
@@ -365,6 +365,7 @@ class Internals final : public ScriptWrappable {
   String layerTreeAsText(Document*, ExceptionState&) const;
 
   String dumpContentNodeTree(Document*, ExceptionState&) const;
+  String dumpContentNode(Node*, ExceptionState&) const;
 
   String mainThreadScrollingReasons(Document*, ExceptionState&) const;
 
