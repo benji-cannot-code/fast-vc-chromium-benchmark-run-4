c2e15d7b7f0f744691416d5b8231fb58336e6f53
Ming-Ying Chung <mych@chromium.org>
2025-09-09 19:41:13 -0700

    [fetch-later] Fix quota allocation for sandbox iframes.
    When the [`reserve deferred-fetch quota`][1] step for a iframe, i.e. `GetContainerDeferredFetchPolicyOnNavigation()`, is triggered, it
    requires a `originToNavigateTo` of the target url. If the iframe is
    sandboxed, its final url origin after navigation would be "null" in
    terms of parent's view. However, it is not avaiable at this step which
    is under `FrameLoader::StartNavigation()`.
    
    This CL manually provides such info using `sandbox_flags` from the
    owner (iframe)'s frame policy, and pass that into
    `GetContainerDeferredFetchPolicyOnNavigation()`.
    
    
    [1]: https://fetch.spec.whatwg.org/#reserve-deferred-fetch-quota
    
    Bug: 410528357
    Change-Id: Ia1041e486f55e56b1cced64affd124de63a264a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6905117
    Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513441}

--- third_party/blink/web_tests/external/wpt/fetch/fetch-later/resources/fetch-later-helper.js
+++ third_party/blink/web_tests/external/wpt/fetch/fetch-later/resources/fetch-later-helper.js
@@ -307,6 +307,11 @@ class FetchLaterIframeOptions {
     this.allowDeferredFetch;
 
     /**
+     * @type {string=} The sandbox attribute to apply to the iframe.
+     */
+    this.sandbox;
+
+    /**
      * @type {FetchLaterIframeExpectation=} The expectation on the iframe's
      * behavior.
      */
@@ -458,6 +463,7 @@ async function loadFetchLaterIframe(origin, {
   bodyType = undefined,
   bodySize = undefined,
   allowDeferredFetch = false,
+  sandbox = undefined,
   expect = undefined
 } = {}) {
   if (uuid && targetUrl && !targetUrl.includes(uuid)) {
@@ -486,6 +492,9 @@ async function loadFetchLaterIframe(origin, {
   const iframe = document.createElement('iframe');
   if (allowDeferredFetch) {
     iframe.allow = 'deferred-fetch';
+  }
+  if (sandbox) {
+    iframe.sandbox = sandbox;
   }
   iframe.src = url;
 
