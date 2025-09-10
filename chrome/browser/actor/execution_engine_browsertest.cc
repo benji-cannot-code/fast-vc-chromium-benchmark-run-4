9b63344f1f79ed3d9ecc9e01d6be0b7be4d53991
Dave Tapuska <dtapuska@chromium.org>
2025-09-10 07:06:18 -0700

    [actor] Remove last usages of BrowserAction
    BrowserAction is no longer used in chrome.
    
    Bug: b:411462297
    Change-Id: I7c338bc10343deedbd78fd37469afad8937563a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6931150
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513679}

--- chrome/browser/actor/execution_engine_browsertest.cc
+++ chrome/browser/actor/execution_engine_browsertest.cc
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/dns/mock_host_resolver.h"
 
 using ::base::test::TestFuture;
-using ::optimization_guide::proto::BrowserAction;
 using ::optimization_guide::proto::ClickAction;
 
 namespace actor {
