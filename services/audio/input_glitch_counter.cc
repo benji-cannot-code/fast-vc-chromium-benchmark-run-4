5a6a0f5887519d4ddfdb1f74b51cc696155c924c
henrika <henrika@google.com>
2025-09-11 07:47:11 -0700

    Improves logging for the Audio Input Sync Writer (AISW)
    Bug: None
    Change-Id: Ia87d8c960198a1abce36676863ca5cfba43f592d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6939370
    Commit-Queue: Henrik Andreasson <henrika@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1514266}

--- services/audio/input_glitch_counter.cc
+++ services/audio/input_glitch_counter.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 #include "services/audio/input_glitch_counter.h"
+
+#include <cinttypes>
 #include <cstddef>
 #include <utility>
 
@@ -57,8 +59,12 @@ InputGlitchCounter::~InputGlitchCounter() {
                                     : AudioGlitchResult::kGlitches);
 
   std::string log_string = base::StringPrintf(
-      "AISW: number of detected audio glitches: %" PRIuS " out of %" PRIuS,
-      global_sample_.dropped_data_count_, write_count_);
+      "AISW::%s => (number of detected audio glitches: %" PRIu64
+      " out of %" PRIu64
+      ") "
+      " [this=0x%" PRIXPTR "]",
+      __func__, global_sample_.dropped_data_count_, write_count_,
+      reinterpret_cast<uintptr_t>(this));
   log_callback_.Run(log_string);
 
   if (write_count_ < kSampleInterval) {
