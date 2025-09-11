fefc25f8f11991787288509764645d0ffa776547
Ewann Pelle <ewannpv@chromium.org>
2025-09-11 02:15:09 -0700

    [iOS] Add illustrated empty view for download list
    Implements empty state UI for the download list using
    TableViewIllustratedEmptyView.
    
    Changes:
    - Added download_list_empty image assets with
      light/dark variants
    - Updated DownloadListTableViewController to use
      TableViewIllustratedEmptyView
    
    Screenshots:
    https://drive.google.com/file/d/1_cYtbZRSAg9LI716Ojsnd2bztCrE2ZxS/view
    https://drive.google.com/file/d/1Pwa2KE1h-g3z-WAPe2wCyw8SJO61nSZf/view
    
    Change-Id: I4fbe5a458fb7140e1852c64bdc2754d67ba03718
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6935084
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: yuan yin <yuanyin@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1514164}

--- ios/chrome/browser/download/ui/download_list/download_list_table_view_controller.mm
+++ ios/chrome/browser/download/ui/download_list/download_list_table_view_controller.mm
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/download/ui/download_list/download_list_mutator.h"
 #import "ios/chrome/browser/shared/ui/table_view/cells/table_view_detail_icon_item.h"
 #import "ios/chrome/browser/shared/ui/table_view/cells/table_view_text_header_footer_item.h"
+#import "ios/chrome/browser/shared/ui/table_view/table_view_illustrated_empty_view.h"
 #import "ios/chrome/browser/shared/ui/table_view/table_view_model.h"
 #import "ios/chrome/browser/shared/ui/table_view/table_view_utils.h"
 #import "ios/chrome/grit/ios_strings.h"
@@ -175,13 +176,26 @@ typedef NSDiffableDataSourceSnapshot<DownloadListGroupItem*, DownloadListItem*>
 
 - (void)setEmptyState:(BOOL)empty {
   if (empty) {
-    // Empty downloads: show small title.
+    // Empty downloads: show small title and empty view.
     self.navigationItem.largeTitleDisplayMode =
         UINavigationItemLargeTitleDisplayModeNever;
+    if (!self.tableView.backgroundView) {
+      UIImage* emptyImage = [UIImage imageNamed:@"download_list_empty"];
+      TableViewIllustratedEmptyView* emptyView =
+          [[TableViewIllustratedEmptyView alloc]
+              initWithFrame:self.view.bounds
+                      image:emptyImage
+                      title:l10n_util::GetNSString(
+                                IDS_IOS_DOWNLOAD_LIST_NO_ENTRIES_TITLE)
+                   subtitle:l10n_util::GetNSString(
+                                IDS_IOS_DOWNLOAD_LIST_NO_ENTRIES_MESSAGE)];
+      self.tableView.backgroundView = emptyView;
+    }
   } else {
-    // Non-empty downloads: show large title initially.
+    // Non-empty downloads: show large title initially and hide empty view.
     self.navigationItem.largeTitleDisplayMode =
         UINavigationItemLargeTitleDisplayModeAlways;
+    self.tableView.backgroundView = nil;
   }
 }
 
