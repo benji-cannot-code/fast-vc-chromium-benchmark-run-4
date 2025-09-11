9a5c8f856dc4eff26f95bff9f38ec18ff9296abb
Atharv Maan <atharvmaan@chromium.org>
2025-09-11 10:19:00 -0700

    Fix confirmation bubble not appearing after successful card upload
    This CL fixes a bug that prevented confirmation bubble to show after a
    successful card upload, by using WebContents::CheckVisibility instead of
    relying on chrome::FindBrowserWithActiveWindow().
    
    Fixed: 444201063
    Change-Id: I692a7010be8e959a62891c2bbca2d0f386742204
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6936894
    Commit-Queue: Atharv Maan <atharvmaan@chromium.org>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1514358}

--- chrome/browser/ui/autofill/payments/save_card_bubble_controller_impl.cc
+++ chrome/browser/ui/autofill/payments/save_card_bubble_controller_impl.cc
@@ -53,6 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/strings/grit/components_strings.h"
 #include "components/sync/service/sync_service.h"
 #include "content/public/browser/navigation_handle.h"
+#include "content/public/browser/visibility.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
 
@@ -907,10 +908,10 @@ bool SaveCardBubbleControllerImpl::IsWebContentsActive() {
     return true;
   }
 
-  Browser* active_browser = chrome::FindBrowserWithActiveWindow();
-  return active_browser &&
-         active_browser->tab_strip_model()->GetActiveWebContents() ==
-             web_contents();
+  // Return false if the tab is inactive, occluded by another window, or out
+  // of screen bounds.
+  return web_contents() &&
+         web_contents()->GetVisibility() == content::Visibility::VISIBLE;
 }
 
 void SaveCardBubbleControllerImpl::EndSaveCardPromptFlow() {
