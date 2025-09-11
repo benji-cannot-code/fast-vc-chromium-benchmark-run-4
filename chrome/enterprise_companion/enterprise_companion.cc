cc409f82f2cae111e48b21e6003c5e4061b84d18
Sorin Jianu <sorin@chromium.org>
2025-09-11 13:18:37 -0700

    ceca: make the process more resilient for memory allocations
    This CL introduces some of the improvements present in the
    updater code.
    
    Change-Id: Ib000df0c1886efd6e7846b64dad1e1a49ec3301b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6937296
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Noah Rose Ledesma <noahrose@google.com>
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1514485}

--- chrome/enterprise_companion/enterprise_companion.cc
+++ chrome/enterprise_companion/enterprise_companion.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/logging.h"
+#include "base/process/memory.h"
 #include "base/process/process_handle.h"
 #include "base/system/sys_info.h"
 #include "base/task/single_thread_task_executor.h"
@@ -35,6 +36,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/win/windows_version.h"
 #include "chrome/enterprise_companion/installer.h"
+#include "chrome/updater/util/win_util.h"
+#include "partition_alloc/page_allocator.h"
 #endif  // BUILDFLAG(IS_WIN)
 
 namespace enterprise_companion {
@@ -143,6 +146,19 @@ std::string OperatingSystemVersion() {
 }  // namespace
 
 int EnterpriseCompanionMain(int argc, const char* const* argv) {
+#if BUILDFLAG(IS_WIN)
+  CHECK(updater::EnableSecureDllLoading());
+#endif
+
+  // Make the process more resilient to memory allocation issues.
+#if BUILDFLAG(IS_WIN)
+  updater::EnableProcessHeapMetadataProtection();
+  partition_alloc::SetRetryOnCommitFailure(true);
+#endif
+  base::EnableTerminationOnHeapCorruption();
+  base::EnableTerminationOnOutOfMemory();
+  logging::RegisterAbslAbortHook();
+
   base::CommandLine::Init(argc, argv);
   base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
   InitLogging();
