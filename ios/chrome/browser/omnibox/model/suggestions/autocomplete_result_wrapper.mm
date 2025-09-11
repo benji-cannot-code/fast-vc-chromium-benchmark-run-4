e9765bd3650c337256656262557535913323281f
Radu Nitescu <radunitescu@google.com>
2025-09-11 03:42:58 -0700

    [iOS] Fix tile Nav check handling the else branch instead of skipping
    The check that was meant to prevent the second tile nav suggest from
    being handled was instead causing a fallback to the else branch. This
    would later cascade and incorrectly inflate the count of carousel items
    exceeding the `kCarouselCapacity.
    
    Bug: 442827618
    Change-Id: I18709d82afacb3f778931111362ef07844c2447f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6934805
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1514184}

--- ios/chrome/browser/omnibox/model/suggestions/autocomplete_result_wrapper.mm
+++ ios/chrome/browser/omnibox/model/suggestions/autocomplete_result_wrapper.mm
@@ -198,8 +198,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   BOOL tileNavSuggestHandled = NO;
   for (size_t i = 0; i < autocompleteResult.size(); i++) {
     const AutocompleteMatch& match = autocompleteResult.match_at((NSUInteger)i);
-    if (match.type == AutocompleteMatchType::TILE_NAVSUGGEST &&
-        !tileNavSuggestHandled) {
+    if (match.type == AutocompleteMatchType::TILE_NAVSUGGEST) {
+      if (tileNavSuggestHandled) {
+        continue;
+      }
       tileNavSuggestHandled = YES;
       DCHECK(match.type == AutocompleteMatchType::TILE_NAVSUGGEST);
       for (const AutocompleteMatch::SuggestTile& tile : match.suggest_tiles) {
