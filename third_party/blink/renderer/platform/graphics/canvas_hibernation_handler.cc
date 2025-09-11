1efbe783a59e4b1c568edd7689225ae01745b4c3
Benoît Lizé <lizeb@chromium.org>
2025-09-11 11:53:32 -0700

    [canvas] Use MOCK_TIME in canvas 2D tests
    This allows the removal of several "ForTesting()" methods.
    
    Change-Id: I628459dba57213fc14d78ac8e047cd35efc89a4d
    Bug: 443022228
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6930282
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1514435}

--- third_party/blink/renderer/platform/graphics/canvas_hibernation_handler.cc
+++ third_party/blink/renderer/platform/graphics/canvas_hibernation_handler.cc
@@ -178,7 +178,7 @@ void CanvasHibernationHandler::SaveForHibernation(
       task_runner, FROM_HERE,
       base::BindOnce(&CanvasHibernationHandler::OnAfterHibernation,
                      weak_ptr_factory_.GetWeakPtr(), epoch_),
-      before_compression_delay_);
+      kBeforeCompressionDelay);
 }
 
 void CanvasHibernationHandler::OnAfterHibernation(uint64_t epoch) {
@@ -220,10 +220,6 @@ void CanvasHibernationHandler::OnEncoded(
     DCHECK_EQ(image_.get(), params->image.get());
     encoded_ = encoded;
     image_ = nullptr;
-  }
-
-  if (on_encoded_callback_for_testing_) {
-    on_encoded_callback_for_testing_.Run();
   }
 }
 
