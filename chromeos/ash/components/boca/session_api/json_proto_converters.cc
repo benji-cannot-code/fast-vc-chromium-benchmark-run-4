60b3ead051cbd16ef78f7b416ec32ef2d7bdc7d8
Ahmed Nasr <anasr@google.com>
2025-09-11 11:24:45 -0700

    Extract Boca UserIdentity proto conversion for receiver requests.
    Bug: b:442592057
    Change-Id: I57559ed728e565af2b6d339a8ebe606db67e3096
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6937132
    Reviewed-by: Benjamin Zielinski <bzielinski@google.com>
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1514411}

---
+++ chromeos/ash/components/boca/session_api/json_proto_converters.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/ash/components/boca/session_api/json_proto_converters.h"
+
+#include "base/values.h"
+#include "chromeos/ash/components/boca/proto/roster.pb.h"
+#include "chromeos/ash/components/boca/session_api/constants.h"
+
+namespace ash::boca {
+
+::boca::UserIdentity ConvertUserIdentityJsonToProto(
+    const base::Value::Dict* dict) {
+  ::boca::UserIdentity user_identity;
+  if (auto* email = dict->FindString(kEmail)) {
+    user_identity.set_email(*email);
+  }
+  if (auto* gaia_id = dict->FindString(kGaiaId)) {
+    user_identity.set_gaia_id(*gaia_id);
+  }
+  if (auto* full_name = dict->FindString(kFullName)) {
+    user_identity.set_full_name(*full_name);
+  }
+  if (auto* photo_url = dict->FindString(kPhotoUrl)) {
+    user_identity.set_photo_url(*photo_url);
+  }
+  return user_identity;
+}
+
+}  // namespace ash::boca
---
+++ chromeos/ash/components/boca/session_api/json_proto_converters.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_ASH_COMPONENTS_BOCA_SESSION_API_JSON_PROTO_CONVERTERS_H_
+#define CHROMEOS_ASH_COMPONENTS_BOCA_SESSION_API_JSON_PROTO_CONVERTERS_H_
+
+#include "base/values.h"
+
+namespace boca {
+class UserIdentity;
+}  // namespace boca
+
+namespace ash::boca {
+
+::boca::UserIdentity ConvertUserIdentityJsonToProto(
+    const base::Value::Dict* dict);
+
+}  // namespace ash::boca
+
+#endif  // CHROMEOS_ASH_COMPONENTS_BOCA_SESSION_API_JSON_PROTO_CONVERTERS_H_
