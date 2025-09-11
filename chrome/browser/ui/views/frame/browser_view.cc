d253a79bb54aaa78930497a6e415461c51ea9694
Keren Zhu <kerenzhu@chromium.org>
2025-09-11 13:17:03 -0700

    Cleanup kScrimForBrowserWindowModal and kScrimForTabModal
    These features were enabled by default.
    
    Bug: 391903216
    Change-Id: Ib78823de3ee9cdacdfc18bda194c9bb9772af200
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6940398
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1514483}

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -2384,10 +2384,7 @@ void BrowserView::UpdateCustomTabBarVisibility(bool visible, bool animate) {
 }
 
 void BrowserView::SetDevToolsScrimVisibility(bool visible) {
-  if (base::FeatureList::IsEnabled(features::kScrimForTabModal)) {
-    GetActiveContentsContainerView()->devtools_scrim_view()->SetVisible(
-        visible);
-  }
+  GetActiveContentsContainerView()->devtools_scrim_view()->SetVisible(visible);
 }
 
 void BrowserView::ResetToolbarTabState(content::WebContents* contents) {
@@ -3049,10 +3046,6 @@ void BrowserView::OnWidgetShowStateChanged(views::Widget* widget) {
 
 void BrowserView::OnWidgetWindowModalVisibilityChanged(views::Widget* widget,
                                                        bool visible) {
-  if (!base::FeatureList::IsEnabled(features::kScrimForBrowserWindowModal)) {
-    return;
-  }
-
 #if !BUILDFLAG(IS_MAC)
   // MacOS does not need views window scrim. We use sheets to show window modals
   // (-[NSWindow beginSheet:]), which natively draw a scrim.
--- chrome/browser/ui/views/frame/browser_view_browsertest.cc
+++ chrome/browser/ui/views/frame/browser_view_browsertest.cc
@@ -629,10 +629,6 @@ IN_PROC_BROWSER_TEST_F(BrowserViewTest, GetAccessibleTabModalDialogTree) {
 
 // Tests that a content area scrim is shown when a tab modal dialog is active.
 IN_PROC_BROWSER_TEST_F(BrowserViewTest, ScrimForTabModal) {
-  if (!base::FeatureList::IsEnabled(features::kScrimForTabModal)) {
-    GTEST_SKIP();
-  }
-
   content::WebContents* contents = browser_view()->GetActiveWebContents();
   auto delegate = std::make_unique<TestTabModalConfirmDialogDelegate>(contents);
 
@@ -661,10 +657,6 @@ IN_PROC_BROWSER_TEST_F(BrowserViewTest, ScrimForTabModal) {
 // (-[NSWindow beginSheet:]), which natively draws a scrim.
 #if !BUILDFLAG(IS_MAC)
 IN_PROC_BROWSER_TEST_F(BrowserViewTest, ScrimForBrowserWindowModal) {
-  if (!base::FeatureList::IsEnabled(features::kScrimForBrowserWindowModal)) {
-    GTEST_SKIP();
-  }
-
   auto child_widget_delegate = std::make_unique<views::WidgetDelegate>();
   auto child_widget = std::make_unique<views::Widget>();
   child_widget_delegate->SetModalType(ui::mojom::ModalType::kWindow);
@@ -882,10 +874,6 @@ IN_PROC_BROWSER_TEST_F(SideBySideBrowserViewTest,
 }
 
 IN_PROC_BROWSER_TEST_F(SideBySideBrowserViewTest, ScrimForTabModalInSplitView) {
-  if (!base::FeatureList::IsEnabled(features::kScrimForTabModal)) {
-    GTEST_SKIP();
-  }
-
   // Create a split view with two tabs followed by a third that will show the
   // scrim.
   chrome::AddTabAt(browser(), GURL(), -1, true);
