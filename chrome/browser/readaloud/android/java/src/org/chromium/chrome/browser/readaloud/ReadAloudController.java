99d960bdc914737c0c1b567230b0f612b74788b0
Shlomi Ben-Shimon <shlomib@google.com>
2025-09-11 09:20:27 -0700

    Consider language during readability check to avoid showing the button when overview is readable in non-EN pages
    Bug: 444398128
    
    Change-Id: I7f00b06256b53802acb6087b48c3489ad9e7de82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6940113
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Auto-Submit: Shlomi Ben Shimon <shlomib@google.com>
    Cr-Commit-Position: refs/heads/main@{#1514322}

--- chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/ReadAloudController.java
+++ chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/ReadAloudController.java
@@ -272,6 +272,14 @@ public class ReadAloudController
                   || (isAudioOverviewsAllowed() && isReadable(PlaybackArgs.PlaybackMode.OVERVIEW));
       }
 
+      boolean isReadable(String tabLanguage) {
+        // A better version of readability that accounts for the page language.
+          return isReadable(PlaybackArgs.PlaybackMode.CLASSIC)
+                  || (isAudioOverviewsAllowed()
+                      && isReadable(PlaybackArgs.PlaybackMode.OVERVIEW)
+                      && isLanguageSupportedForOverview(tabLanguage));
+      }
+
       boolean isReadable(PlaybackArgs.PlaybackMode mode) {
         return getReadabilityResultForMode(mode).readable;
       }
@@ -942,7 +950,10 @@ public class ReadAloudController
             int sanitizedUrlHash = urlToHash(stripUserData(nonNullTab.getUrl()).getSpec());
             ReadabilityInfo info = getReadabilityInfoIfUnexpired(sanitizedUrlHash);
             if (info != null) {
-                return info.isReadable();
+              if (ReadAloudFeatures.shouldConsiderLanguageInOverviewReadability()) {
+                return info.isReadable(tabLanguageStatus.mLanguage);
+              }
+              return info.isReadable();
             }
         }
         return false;
@@ -961,7 +972,7 @@ public class ReadAloudController
         if (tabLanguageStatus.mSupported && isAvailable()) {
             int sanitizedUrlHash = urlToHash(stripUserData(nonNullTab.getUrl()).getSpec());
             ReadabilityInfo info = getReadabilityInfoIfUnexpired(sanitizedUrlHash);
-            if (info != null && info.isReadable()) {
+            if (info != null && (ReadAloudFeatures.shouldConsiderLanguageInOverviewReadability() ? info.isReadable(tabLanguageStatus.mLanguage) : info.isReadable())) {
               List<PlaybackMode> playbackModes = getPlaybackModesForNewPlayback(info, tabLanguageStatus.mLanguage);
               return playbackModes.size() > 0 ? playbackModes.get(0) : PlaybackMode.UNSPECIFIED;
             }
@@ -1448,7 +1459,7 @@ public class ReadAloudController
                 /* clearPassword= */ true);
     }
 
-    private boolean isLanguageSupportedForOverview(String language) {
+    private static boolean isLanguageSupportedForOverview(String language) {
         return language.equals("en");
     }
 
