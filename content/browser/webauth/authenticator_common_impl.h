3a51abdfb8f7e840b2067837b6b48ffacb90b04d
Alexis Hetu <sugoi@chromium.org>
2025-09-11 12:17:00 -0700

    Remove payment related fields from ClientDataJsonParams
    This CL removes payment related fields from ClientDataJsonParams
    and adds BuildClientDataJsonWithPayment which receives the payments
    options. BuildClientDataJson and BuildClientDataJsonWithPayment now
    both exist and call ::BuildClientDataJson with or without payment info.
    
    The goal of this CL is to eventually move ClientDataJsonParams to
    components while leaving the mojom related code in content/.
    
    Bug: 385174410
    Change-Id: I8845096308a20c14da989dfaacf21ef6bc5ff93a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6936421
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1514444}

--- content/browser/webauth/authenticator_common_impl.h
+++ content/browser/webauth/authenticator_common_impl.h
@@ -380,6 +380,8 @@ class CONTENT_EXPORT AuthenticatorCommonImpl : public AuthenticatorCommon {
 
   void UpdateChallengeFromUrl(
       ClientDataJsonParams params,
+      blink::mojom::PaymentOptionsPtr payment_options,
+      std::string payment_rp,
       std::optional<base::span<const uint8_t>> challenge);
 
   // Get an identifier for the current request. Callbacks that might span a
