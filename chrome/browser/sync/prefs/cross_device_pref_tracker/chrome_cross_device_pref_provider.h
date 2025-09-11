5daa94e22ab76e176a69e0c02d3e29c11b71c976
Benjamin Williams <bwwilliams@google.com>
2025-09-11 06:08:38 -0700

    [CrossDevicePrefTracker] Introduce ChromeCrossDevicePrefProvider
    This change introduces ChromeCrossDevicePrefProvider, the implementation
    of the CrossDevicePrefProvider interface for Chrome (desktop and
    Android).
    
    This provider is responsible for supplying the CrossDevicePrefTracker
    with a list of all Prefs to be tracked. It works by combining the
    common, cross-platform Prefs from CommonCrossDevicePrefProvider with
    those that are specific to Chrome.
    
    This CL creates the following:
    * The ChromeCrossDevicePrefProvider class.
    * The required BUILD.gn changes for the new files.
    
    This is another step in implementing the service described in
    go/cross-device-pref-tracker.
    
    Fixed: 444059110
    Change-Id: Ic907c96aa406d4366e9de56374b67cb89aabaf60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6933918
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1514227}

---
+++ chrome/browser/sync/prefs/cross_device_pref_tracker/chrome_cross_device_pref_provider.h
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_SYNC_PREFS_CROSS_DEVICE_PREF_TRACKER_CHROME_CROSS_DEVICE_PREF_PROVIDER_H_
+#define CHROME_BROWSER_SYNC_PREFS_CROSS_DEVICE_PREF_TRACKER_CHROME_CROSS_DEVICE_PREF_PROVIDER_H_
+
+#include <string_view>
+
+#include "base/containers/flat_set.h"
+#include "components/sync_preferences/cross_device_pref_tracker/common_cross_device_pref_provider.h"
+#include "components/sync_preferences/cross_device_pref_tracker/cross_device_pref_provider.h"
+
+// Chrome implementation of `CrossDevicePrefProvider`.
+class ChromeCrossDevicePrefProvider
+    : public sync_preferences::CrossDevicePrefProvider {
+ public:
+  ChromeCrossDevicePrefProvider();
+  ~ChromeCrossDevicePrefProvider() override;
+
+  // `CrossDevicePrefProvider` overrides:
+  const base::flat_set<std::string_view>& GetProfilePrefs() const override;
+  const base::flat_set<std::string_view>& GetLocalStatePrefs() const override;
+
+ private:
+  // This defines the list of prefs that are tracked across all platforms.
+  sync_preferences::CommonCrossDevicePrefProvider
+      common_cross_device_pref_provider_;
+  // A list of all profile prefs to be tracked on Chrome.
+  const base::flat_set<std::string_view> profile_prefs_;
+  // A list of all local-state prefs to be tracked on Chrome.
+  const base::flat_set<std::string_view> local_state_prefs_;
+};
+
+#endif  // CHROME_BROWSER_SYNC_PREFS_CROSS_DEVICE_PREF_TRACKER_CHROME_CROSS_DEVICE_PREF_PROVIDER_H_
