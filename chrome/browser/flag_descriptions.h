c160d4493aeed488799781946949859436fc779a
Tim <tjudkins@chromium.org>
2025-09-11 13:07:12 -0700

    Revert "Cleanup kAlignSurfaceLayerImplToPixelGrid"
    This reverts commit e0265ff7765b9e00fc4588cd0eaadeefa5ee4107.
    
    Reason for revert: Caused a consistent failure of
    `http/tests/html/scaled-oopif-text-not-blurry.html` on the WebKit Win10
    builder. First failure can be seen here:
    https://ci.chromium.org/ui/p/chromium/builders/ci/WebKit%20Win10/161557/overview
    
    Original change's description:
    > Cleanup kAlignSurfaceLayerImplToPixelGrid
    >
    > This feature has launched and we can clean up the code.
    >
    > scaled-oopif-text-not-blurry.html and
    > translated-oopif-text-not-blurry.html had bugs which caused them to
    > fail. These tests have been updated to work. I verified the updated
    > tests fail with the kAlignSurfaceLayerImplToPixelGrid feature disabled.
    >
    > The in-process scrollbar expectations on MacOS seem incorrect and these
    > tests have been marked as failing on that platform.
    >
    > Fixed: 442383400, 359279545
    > Change-Id: I2aa6632f140e69510a92f99658540ff7db51fa15
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6937205
    > Reviewed-by: Jonathan Ross <jonross@chromium.org>
    > Commit-Queue: Philip Rogers <pdr@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1514367}
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I889e3ee9adcb77c816cfb728bf9cffb7888f73b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6941603
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Tim Judkins <tjudkins@google.com>
    Auto-Submit: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1514477}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -61,6 +61,9 @@ extern const char kAiModeOmniboxEntryPointDescription[];
 extern const char kOmniboxAimServerEligibilityName[];
 extern const char kOmniboxAimServerEligibilityDescription[];
 
+extern const char kAlignSurfaceLayerImplToPixelGridName[];
+extern const char kAlignSurfaceLayerImplToPixelGridDescription[];
+
 extern const char kAlignWakeUpsName[];
 extern const char kAlignWakeUpsDescription[];
 
