3eb66941ccf05f32913e70ac7805abe078279e84
Colin Blundell <blundell@chromium.org>
2025-09-11 00:02:06 -0700

    [//gpu] Streamline GpuMemoryBufferFactoryNativePixmap
    Collapse unneeded helper methods into CreateNativeGmbHandle().
    
    Bug: 404905709
    Change-Id: I6e794713b293784a4783b774a93c4b1ebba19ac1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6935451
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1514117}

--- gpu/ipc/service/gpu_memory_buffer_factory_native_pixmap.h
+++ gpu/ipc/service/gpu_memory_buffer_factory_native_pixmap.h
@@ -6,15 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef GPU_IPC_SERVICE_GPU_MEMORY_BUFFER_FACTORY_NATIVE_PIXMAP_H_
 #define GPU_IPC_SERVICE_GPU_MEMORY_BUFFER_FACTORY_NATIVE_PIXMAP_H_
 
-#include <vulkan/vulkan_core.h>
-
 #include "gpu/ipc/service/gpu_ipc_service_export.h"
 #include "gpu/ipc/service/gpu_memory_buffer_factory.h"
 #include "ui/gfx/native_pixmap.h"
 
 namespace gpu {
-
-class VulkanDeviceQueue;
 
 class GPU_IPC_SERVICE_EXPORT GpuMemoryBufferFactoryNativePixmap
     : public GpuMemoryBufferFactory {
@@ -37,14 +33,6 @@ class GPU_IPC_SERVICE_EXPORT GpuMemoryBufferFactoryNativePixmap
       gfx::BufferUsage usage) override;
 
  private:
-  gfx::GpuMemoryBufferHandle CreateNativeGmbHandleFromNativePixmap(
-      const gfx::Size& size,
-      viz::SharedImageFormat format,
-      gfx::BufferUsage usage,
-      scoped_refptr<gfx::NativePixmap> pixmap);
-
-  VulkanDeviceQueue* GetVulkanDeviceQueue();
-
   scoped_refptr<viz::VulkanContextProvider> vulkan_context_provider_;
 
   base::WeakPtrFactory<GpuMemoryBufferFactoryNativePixmap> weak_factory_{this};
