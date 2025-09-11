30cac8a3221a29496e9b28f0e3cd79812a7593ea
Thomas Lukaszewicz <tluk@chromium.org>
2025-09-11 14:53:58 -0700

    Reland "Reland "[bedrock] Update ui_test_utils to take BrowserWindowInterface inputs""
    This is a reland of commit e944500a6cdf7c4b0180b450f900ed3a5af84a4d
    
    See diff vs patchset 1, scopes browser.h to desktop platforms.
    
    Original change's description:
    > Reland "[bedrock] Update ui_test_utils to take BrowserWindowInterface inputs"
    >
    > This is a reland of commit ae249ae85d824b6c4b06e355ed15929a3c6a2948
    >
    > Original change's description:
    > > [bedrock] Update ui_test_utils to take BrowserWindowInterface inputs
    > >
    > > There are no intended behavior changes in this CL.
    > >
    > > Input params for test utilities in ui_test_utils.h/cc have been
    > > updated to take a BrowserWindowInterface param instead of a
    > > Browser. Output params are left as Browser to allow clients
    > > to be updated to use BrowserWindowInterface gradually.
    > >
    > > In most cases a BrowserWindowInterface can already be used in place
    > > of a Browser. In cases where this is not yet possible
    > > GetBrowserForMigrationOnly() is used.
    > >
    > > chrome/browser/ui/browser.h is included in the test header file
    > > to allow existing clients that deal with forward declared Browser*
    > > pointers to continue calling into these API without modification.
    > > These clients will be migrated to use BrowserWindowInterface and
    > > this include will eventually no longer be necessary.
    > >
    > > InProcessBrowserTest CloseBrowser APIs have been updated to take
    > > a BrowserWindowInterface param.
    > >
    > > Bug: 431672609
    > > Change-Id: I4799aa3cbee4d792a10fc758846fe914253e2f93
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6922486
    > > Reviewed-by: David Pennington <dpenning@chromium.org>
    > > Commit-Queue: Tom Lukaszewicz <tluk@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1513574}
    >
    > Bug: 431672609
    > Change-Id: I7afa2ab595ce34e135c9339b5de5b404d433fd6d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6934664
    > Reviewed-by: David Pennington <dpenning@chromium.org>
    > Commit-Queue: Tom Lukaszewicz <tluk@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1514145}
    
    Bug: 431672609
    Change-Id: Ifa7156339677b8b2068c712ca12c34a2ebd89094
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6937629
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1514538}

--- chrome/browser/download/download_browsertest.cc
+++ chrome/browser/download/download_browsertest.cc
@@ -71,6 +71,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_tabstrip.h"
 #include "chrome/browser/ui/browser_window.h"
+#include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/web_applications/test/web_app_browsertest_util.h"
@@ -5245,10 +5246,7 @@ IN_PROC_BROWSER_TEST_F(DownloadTest, MAYBE_NewWindow) {
   EXPECT_FALSE(IsDownloadDetailedUiVisible(browser()->window()));
 
   // The download surface SHOULD be visible in the second window.
-  std::set<Browser*> original_browsers;
-  original_browsers.insert(browser());
-  Browser* download_browser =
-      ui_test_utils::GetBrowserNotInSet(original_browsers);
+  Browser* download_browser = ui_test_utils::GetBrowserNotInSet({browser()});
   ASSERT_TRUE(download_browser);
   EXPECT_NE(download_browser, browser());
   EXPECT_EQ(1, download_browser->tab_strip_model()->count());
