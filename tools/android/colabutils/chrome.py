1ca2eec31c0f54b705a72f9190136a457b0849aa
Anand Ravi <anandrv@google.com>
2025-09-11 10:50:05 -0700

    [tools/colabutils] Add support for Web Page Replay
    This change introduces Web Page Replay (WPR) support to colabutils,
    enabling the recording and replaying of network traffic to eliminate
    network variability in tests. A new wpr.WebPageReplayArchive class
    provides async context managers for easily managing WPR sessions.
    
    To support this, the following improvements were made:
    - command_line.run is now more robust, using process groups to ensure
      subprocesses are terminated correctly. It also now supports graceful
      exit through interruption signals.
    - chrome.py has a new context manager to temporarily add command-line
      flags
    - trace.py is refactored to use a context manager for recording traces,
      allowing for nesting with WPR record/replay
    
    Bug: 443799489
    Change-Id: Ifec431a7c1c4c75712464205b064bbc735f340c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6940682
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Anand Ravi <anandrv@google.com>
    Cr-Commit-Position: refs/heads/main@{#1514386}

--- tools/android/colabutils/chrome.py
+++ tools/android/colabutils/chrome.py
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 import shlex
+import contextlib
 from enum import Enum, StrEnum
 
 from . import command_line
@@ -41,3 +42,26 @@ class App:
         if url:
             command_parts.extend(["-d", url])
         await command_line.adb_shell(shlex.join(command_parts))
+
+
+@contextlib.asynccontextmanager
+async def additional_command_line_flags(*args):
+    """A context manager to temporarily add flags to chrome-command-line."""
+    # Ensure the file exists before reading it.
+    await command_line.adb_shell("touch",
+                                 "/data/local/tmp/chrome-command-line")
+    old_flags_str = (await command_line.adb_shell(
+        "cat", "/data/local/tmp/chrome-command-line")).stdout.strip()
+
+    try:
+        # The command-line file must start with a placeholder token. If the file
+        # is empty or doesn't start with one, we add it.
+        old_flags_list = shlex.split(old_flags_str) if old_flags_str else ['_']
+        new_flags_list = old_flags_list + list(args)
+        new_flags_str = shlex.join(new_flags_list)
+        await command_line.adb_shell(
+            "cat > /data/local/tmp/chrome-command-line", input=new_flags_str)
+        yield
+    finally:
+        await command_line.adb_shell(
+            "cat > /data/local/tmp/chrome-command-line", input=old_flags_str)
