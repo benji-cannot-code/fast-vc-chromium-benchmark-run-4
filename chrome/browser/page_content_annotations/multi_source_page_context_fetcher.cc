f2022a08080b2239a346d27000ca9fd1b6269906
Chris Fredrickson <cfredric@chromium.org>
2025-09-11 12:51:39 -0700

    Pass timers instead of raw TimeTicks values
    As of https://crrev.com/c/6896002, these timers are now copyable, so we
    don't have to repeat the math by hand anymore.
    
    Change-Id: I804d235b5a1c574a7fa9d5784070378b1f5df7e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6941262
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1514463}

--- chrome/browser/page_content_annotations/multi_source_page_context_fetcher.cc
+++ chrome/browser/page_content_annotations/multi_source_page_context_fetcher.cc
@@ -53,11 +53,10 @@ namespace {
 template <typename T, typename E>
 // Conditionally emits to a given timing histogram, given the start_time.
 base::expected<T, E> EmitTimingHistogram(const std::string& histogram_name,
-                                         base::TimeTicks start_time,
+                                         base::ElapsedTimer timer,
                                          base::expected<T, E> result) {
   if (result.has_value()) {
-    base::UmaHistogramTimes(histogram_name,
-                            base::TimeTicks::Now() - start_time);
+    base::UmaHistogramTimes(histogram_name, timer.Elapsed());
   }
   return std::move(result);
 }
@@ -330,7 +329,7 @@ class PageContextFetcher : public content::WebContentsObserver {
           base::BindOnce(
               EmitTimingHistogram<const SkBitmap*, std::string>,
               "Glic.PageContextFetcher.GetScreenshot.TimeoutAgnostic",
-              elapsed_timer_.start_time())
+              elapsed_timer_)
               .Then(base::BindOnce(
                   &PageContextFetcher::ReceivedViewportBitmapOrError,
                   GetWeakPtr())));
@@ -396,7 +395,7 @@ class PageContextFetcher : public content::WebContentsObserver {
           base::BindOnce(
               EmitTimingHistogram<std::vector<uint8_t>, std::string>,
               "Glic.PageContextFetcher.GetEncodedScreenshot.TimeoutAgnostic",
-              elapsed_timer_.start_time())
+              elapsed_timer_)
               .Then(base::BindOnce(&PageContextFetcher::ReceivedJpegScreenshot,
                                    GetWeakPtr())));
     } else {
