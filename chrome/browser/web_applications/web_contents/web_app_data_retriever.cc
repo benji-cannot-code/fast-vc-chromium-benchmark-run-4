1176c394ac309a22c263c42d53ab497009747ec2
Dan Murphy <dmurph@chromium.org>
2025-09-11 12:44:23 -0700

    [PWA] Adding README.md for web_contents dependency
    R=dibyapal@chromium.org
    
    Change-Id: If72493ea33827793222d1bbc96ca5cf551829564
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6938102
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1514452}

--- chrome/browser/web_applications/web_contents/web_app_data_retriever.cc
+++ chrome/browser/web_applications/web_contents/web_app_data_retriever.cc
@@ -186,7 +186,7 @@ void WebAppDataRetriever::CheckInstallabilityAndRetrieveManifest(
 }
 
 void WebAppDataRetriever::GetIcons(content::WebContents* web_contents,
-                                   const IconUrlSizeSet& extra_icon_urls,
+                                   const IconUrlSizeSet& icon_urls,
                                    bool download_page_favicons,
                                    bool fail_all_if_any_fail,
                                    GetIconsCallback callback) {
@@ -211,7 +211,7 @@ void WebAppDataRetriever::GetIcons(content::WebContents* web_contents,
       .fail_all_if_any_fail = fail_all_if_any_fail};
   icon_downloader_ = std::make_unique<WebAppIconDownloader>();
   icon_downloader_->Start(
-      web_contents, extra_icon_urls,
+      web_contents, icon_urls,
       base::BindOnce(&WebAppDataRetriever::OnIconsDownloaded,
                      weak_ptr_factory_.GetWeakPtr()),
       options);
