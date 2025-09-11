2303503608450413258574ebda673405327e7557
dongguoz <dongguoz@google.com>
2025-09-11 05:37:40 -0700

    Add WalletablePassBarcodeDetector interface
    This CL introduces the `WalletablePassBarcodeDetector` component, which
    will be responsible for detecting images of barcodes within a web page.
    
    This change is an interface-only introduction. The public `Detect()`
    interface takes a `content::WebContents` object and a `base::OnceCallback` to handle the detection results.
    
    This CL is based on the work of minjiaw@google.com
    in Gerrit change: https://chromium-review.googlesource.com/c/chromium/src/+/6840397
    
    Bug: 438364540
    Change-Id: I2d12a7d1827530069bf1c93ac5ddb37be65eef9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6880052
    Commit-Queue: Dongguo Zhang <dongguo@google.com>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1514218}

---
+++ components/wallet/content/browser/walletable_pass_barcode_detector_impl.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/wallet/content/browser/walletable_pass_barcode_detector_impl.h"
+
+#include "base/functional/callback.h"
+
+namespace wallet {
+
+WalletablePassBarcodeDetectorImpl::WalletablePassBarcodeDetectorImpl() =
+    default;
+
+WalletablePassBarcodeDetectorImpl::~WalletablePassBarcodeDetectorImpl() =
+    default;
+
+WalletBarcodeFormat WalletablePassBarcodeDetectorImpl::MojoToBarcodeFormat(
+    shape_detection::mojom::BarcodeFormat format) {
+  // TODO(crbug.com/438364540): Implement the conversion logic from Mojo format
+  // to WalletBarcodeFormat.
+  return WalletBarcodeFormat::UNKNOWN;
+}
+
+void WalletablePassBarcodeDetectorImpl::Detect(
+    content::WebContents* web_contents,
+    WalletBarcodeDetectionDetectCallback callback) {
+  // TODO(crbug.com/438364540): Implement the actual detection logic here.
+  std::move(callback).Run({});
+}
+
+}  // namespace wallet
