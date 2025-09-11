48cf5b169d33f43df9fc03d69373af26368f58bb
Zifan Zhang <zifanzhang@google.com>
2025-09-11 09:40:24 -0700

    boca: Implement API for GetKioskReceiver from class tools server
    Bug: 439661365
    Change-Id: Ib3a2e54976ab919824ba418de41c477926d3bfdc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6916863
    Reviewed-by: Ahmed Nasr <anasr@google.com>
    Reviewed-by: April Zhou <aprilzhou@google.com>
    Commit-Queue: Zifan Zhang <zifanzhang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1514333}

---
+++ chromeos/ash/components/boca/receiver/kiosk_receiver_parser.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_ASH_COMPONENTS_BOCA_RECEIVER_KIOSK_RECEIVER_PARSER_H_
+#define CHROMEOS_ASH_COMPONENTS_BOCA_RECEIVER_KIOSK_RECEIVER_PARSER_H_
+
+#include <string>
+
+#include "chromeos/ash/components/boca/proto/receiver.pb.h"
+
+namespace ash::boca_receiver {
+::boca::ReceiverConnectionState ReceiverConnectionStateProtoFromJson(
+    const std::string& state);
+}  // namespace ash::boca_receiver
+
+#endif  // CHROMEOS_ASH_COMPONENTS_BOCA_RECEIVER_KIOSK_RECEIVER_PARSER_H_
--- chromeos/ash/components/boca/session_api/constants.h
+++ chromeos/ash/components/boca/session_api/constants.h
@@ -63,6 +63,12 @@ inline constexpr char kStudentHeartbeatUrlTemplate[] =
 inline constexpr char kNotifyGetActiveSession[] =
     "v1/teachers/$1/sessions/$2/students:notifyGetActiveSession";
 
+inline constexpr char kGetKioskReceiverWithoutConnectionIdUrlTemplate[] =
+    "/v1/receiverId/$1/kioskReceivers:get";
+
+inline constexpr char kGetKioskReceiverUrlTemplate[] =
+    "/v1/receiverId/$1/kioskReceivers:get?connectionId=$2";
+
 inline constexpr char kSessionId[] = "sessionId";
 inline constexpr char kTeacher[] = "teacher";
 inline constexpr char kRoster[] = "roster";
@@ -87,6 +93,7 @@ inline constexpr char kTranslationsEnabled[] = "translationsEnabled";
 inline constexpr char kStudentStatus[] = "studentStatuses";
 inline constexpr char kStudentStatusState[] = "state";
 inline constexpr char kDeviceStatusState[] = "state";
+inline constexpr char kReceiverState[] = "state";
 inline constexpr char kUrl[] = "url";
 inline constexpr char kTitle[] = "title";
 inline constexpr char kFavIcon[] = "faviconUrl";
@@ -123,6 +130,7 @@ inline constexpr char kServiceAccount[] = "serviceAccount";
 inline constexpr char kStudentGroupId[] = "studentGroupId";
 inline constexpr char kViewScreenConfig[] = "viewScreenConfig";
 inline constexpr char kViewScreenState[] = "viewScreenState";
+inline constexpr char kRobotEmail[] = "robotEmail";
 
 inline constexpr net::NetworkTrafficAnnotationTag kTrafficAnnotation =
     net::DefineNetworkTrafficAnnotation("boca_server_integration", R"(
