6ed1823b3b1d588756e37e9bf31106807d5db253
Omar Bahgat <omarbahgat@google.com>
2025-09-11 12:21:13 -0700

    AutoSignOut: Rename all Floating Workspace sign-in timestamp identifiers
    The automatic sign-out functionality found in FWS will now be built as a
    standalone component. Accordingly, the naming prefix for all related
    code has been changed from floating_workspace to auto_sign_out.
    
    This CL doesn’t introduce any functional changes.
    
    Bug: b:433240810
    Change-Id: Ia60cdb9ec4e0a51896b52c97e4079be9d21b83d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6929588
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Andrey Davydov <andreydav@google.com>
    Commit-Queue: Omar Bahgat <omarbahgat@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1514447}

--- chromeos/ash/components/auto_sign_out/auto_sign_out_service_unittest.cc
+++ chromeos/ash/components/auto_sign_out/auto_sign_out_service_unittest.cc
@@ -47,7 +47,7 @@ std::unique_ptr<syncer::DeviceInfo> CreateFakeDeviceInfo(
       /*paask_info=*/std::nullopt,
       /*fcm_registration_token=*/"token",
       /*interested_data_types=*/syncer::DataTypeSet(),
-      /*floating_workspace_last_signin_timestamp=*/signin_timestamp);
+      /*auto_sign_out_last_signin_timestamp=*/signin_timestamp);
 }
 
 }  // namespace
@@ -136,16 +136,14 @@ class AutoSignOutTest : public testing::Test {
 
 // Verifies that local device info timestamp is updated when service is created.
 TEST_F(AutoSignOutTest, TimestampUpdatedAfterServiceCreation) {
-  EXPECT_FALSE(local_device_info()
-                   ->floating_workspace_last_signin_timestamp()
-                   .has_value());
+  EXPECT_FALSE(
+      local_device_info()->auto_sign_out_last_signin_timestamp().has_value());
 
   AutoSignOutService auto_sign_out_service(
       fake_device_info_sync_service(), test_sync_service(), session_manager());
 
-  EXPECT_TRUE(local_device_info()
-                  ->floating_workspace_last_signin_timestamp()
-                  .has_value());
+  EXPECT_TRUE(
+      local_device_info()->auto_sign_out_last_signin_timestamp().has_value());
 }
 
 // Verifies that a sign-out is triggered when a newer device signs in.
@@ -215,7 +213,7 @@ TEST_F(AutoSignOutTest, TimestampUpdatedOnUnlockAfterWakeUpFromSleep) {
       fake_device_info_sync_service(), test_sync_service(), session_manager());
 
   const base::Time timestamp_before_sleep =
-      local_device_info()->floating_workspace_last_signin_timestamp().value();
+      local_device_info()->auto_sign_out_last_signin_timestamp().value();
 
   // Simulate sleep.
   fake_power_manager_client()->SendSuspendImminent(
@@ -227,7 +225,7 @@ TEST_F(AutoSignOutTest, TimestampUpdatedOnUnlockAfterWakeUpFromSleep) {
   fake_power_manager_client()->SendSuspendDone();
 
   const base::Time timestamp_after_wakeup =
-      local_device_info()->floating_workspace_last_signin_timestamp().value();
+      local_device_info()->auto_sign_out_last_signin_timestamp().value();
 
   EXPECT_EQ(timestamp_after_wakeup, timestamp_before_sleep);
 
@@ -235,7 +233,7 @@ TEST_F(AutoSignOutTest, TimestampUpdatedOnUnlockAfterWakeUpFromSleep) {
       /*success=*/true, /*UnlockType=*/session_manager::UnlockType::PASSWORD);
 
   const base::Time timestamp_after_unlock =
-      local_device_info()->floating_workspace_last_signin_timestamp().value();
+      local_device_info()->auto_sign_out_last_signin_timestamp().value();
 
   EXPECT_GT(timestamp_after_unlock, timestamp_after_wakeup);
 }
@@ -247,7 +245,7 @@ TEST_F(AutoSignOutTest, TimestampNotUpdatedOnUnlockWithoutPreviousSleep) {
       fake_device_info_sync_service(), test_sync_service(), session_manager());
 
   const base::Time timestamp_before_unlock =
-      local_device_info()->floating_workspace_last_signin_timestamp().value();
+      local_device_info()->auto_sign_out_last_signin_timestamp().value();
 
   task_environment().FastForwardBy(base::Seconds(10));
 
@@ -255,7 +253,7 @@ TEST_F(AutoSignOutTest, TimestampNotUpdatedOnUnlockWithoutPreviousSleep) {
       /*success=*/true, /*UnlockType=*/session_manager::UnlockType::PASSWORD);
 
   const base::Time timestamp_after_unlock =
-      local_device_info()->floating_workspace_last_signin_timestamp().value();
+      local_device_info()->auto_sign_out_last_signin_timestamp().value();
 
   EXPECT_EQ(timestamp_after_unlock, timestamp_before_unlock);
 }
