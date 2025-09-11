3484e192f3903f9094770e3e3cd21c1a06dcec98
Jenna Himawan <jhimawan@google.com>
2025-09-11 09:50:45 -0700

    Add JNI bridge for CrossDevicePrefTrackerFactory
    Bug: 442902926
    Change-Id: I633c3882bc4dfd2295c6ebbab16ce4ae25933891
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6937174
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1514342}

--- chrome/browser/sync/prefs/cross_device_pref_tracker/cross_device_pref_tracker_factory.cc
+++ chrome/browser/sync/prefs/cross_device_pref_tracker/cross_device_pref_tracker_factory.cc
@@ -5,13 +5,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/sync/prefs/cross_device_pref_tracker/cross_device_pref_tracker_factory.h"
 
+#include "base/check.h"
 #include "base/feature_list.h"
+#include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sync/device_info_sync_service_factory.h"
 #include "chrome/browser/sync/prefs/cross_device_pref_tracker/chrome_cross_device_pref_provider.h"
 #include "components/sync_preferences/cross_device_pref_tracker/cross_device_pref_tracker_impl.h"
 #include "components/sync_preferences/features.h"
+
+#if BUILDFLAG(IS_ANDROID)
+#include "base/android/scoped_java_ref.h"
+
+// Must come after all headers that specialize FromJniType() / ToJniType().
+#include "chrome/browser/sync/android/jni_headers/CrossDevicePrefTrackerFactory_jni.h"
+#endif  // BUILDFLAG(IS_ANDROID)
 
 CrossDevicePrefTrackerFactory::CrossDevicePrefTrackerFactory()
     : ProfileKeyedServiceFactory(
@@ -53,3 +62,17 @@ CrossDevicePrefTrackerFactory::BuildServiceInstanceForBrowserContext(
       DeviceInfoSyncServiceFactory::GetForProfile(profile),
       std::move(pref_provider));
 }
+
+#if BUILDFLAG(IS_ANDROID)
+base::android::ScopedJavaLocalRef<jobject>
+JNI_CrossDevicePrefTrackerFactory_GetForProfile(JNIEnv* env, Profile* profile) {
+  DCHECK(profile);
+
+  sync_preferences::CrossDevicePrefTracker* pref_tracker =
+      CrossDevicePrefTrackerFactory::GetForProfile(profile);
+  if (!pref_tracker) {
+    return base::android::ScopedJavaLocalRef<jobject>();
+  }
+  return pref_tracker->GetJavaObject();
+}
+#endif  // BUILDFLAG(IS_ANDROID)
