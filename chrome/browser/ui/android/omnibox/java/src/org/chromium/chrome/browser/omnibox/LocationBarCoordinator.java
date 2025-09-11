269af7b4d58da7b138b7f93161edfd498875e7c9
Tomasz Wiszkowski <ender@google.com>
2025-09-11 12:52:19 -0700

    Restrict multimodal omnibox to NTP and WEB
    Bug: 436888404
    Change-Id: Ib2f5d64d94d42b2df2e46cb31f64149dcf67a747
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6940857
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Jian Li <jianli@chromium.org>
    Reviewed-by: Fred Mello <fredmello@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1514464}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarCoordinator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarCoordinator.java
@@ -260,7 +260,11 @@ public class LocationBarCoordinator
                 incognitoStateProvider != null && incognitoStateProvider.isIncognitoSelected();
         mNavigationAttachmentsCoordinator =
                 new NavigationAttachmentsCoordinator(
-                        context, windowAndroid, mLocationBarLayout, profileObservableSupplier);
+                        context,
+                        windowAndroid,
+                        mLocationBarLayout,
+                        profileObservableSupplier,
+                        locationBarDataProvider);
         mUrlCoordinator =
                 new UrlBarCoordinator(
                         context,
