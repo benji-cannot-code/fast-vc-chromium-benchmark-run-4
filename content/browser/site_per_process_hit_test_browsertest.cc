e82a04b3f22783d2714d8451f8d90eb25df07308
Joel Hockey <joelhockey@chromium.org>
2025-09-11 14:39:38 -0700

    Disable the Tooltips feature by default on Android
    Disable while investigating crashes
    
    Bug: 441235003
    Change-Id: I5fb7b8fc329cdbf2828049715171cc33859c97a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6937651
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1514528}

--- content/browser/site_per_process_hit_test_browsertest.cc
+++ content/browser/site_per_process_hit_test_browsertest.cc
@@ -847,7 +847,11 @@ std::unique_ptr<ui::MotionEventAndroid> GetMotionEventAndroid(
 
 class SitePerProcessHitTestBrowserTest : public SitePerProcessBrowserTestBase {
  public:
-  SitePerProcessHitTestBrowserTest() {}
+  SitePerProcessHitTestBrowserTest() {
+#if BUILDFLAG(IS_ANDROID)
+    feature_list_.InitWithFeatures({kTooltips}, {});
+#endif
+  }
 
 #if defined(USE_AURA)
   void PreRunTestOnMainThread() override {
@@ -870,6 +874,9 @@ class SitePerProcessHitTestBrowserTest : public SitePerProcessBrowserTestBase {
 
 #if defined(USE_AURA)
   SystemEventRewriter event_rewriter_;
+#endif
+#if BUILDFLAG(IS_ANDROID)
+  base::test::ScopedFeatureList feature_list_;
 #endif
 };
 
