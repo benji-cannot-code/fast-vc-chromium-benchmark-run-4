2bf1bf136e6b3a8e66af95c6ba364f895ef8900c
Tom Sepez <tsepez@chromium.org>
2025-09-11 16:08:46 -0700

    Remove ipc/ipc_channel_mojo_unittest.test-mojom
    The test which required this is no longer present.
    
    Bug: 40621529
    Change-Id: I2b4a9150d3811965d772f118436fe6873b4b1bce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6941415
    Auto-Submit: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1514571}

--- ipc/ipc_channel_mojo_unittest.cc
+++ ipc/ipc_channel_mojo_unittest.cc
@@ -48,7 +48,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_timeouts.h"
 #include "base/threading/thread.h"
 #include "build/build_config.h"
-#include "ipc/ipc_channel_mojo_unittest.test-mojom.h"
 #include "ipc/ipc_message.h"
 #include "ipc/ipc_message_utils.h"
 #include "ipc/ipc_mojo_handle_attachment.h"
