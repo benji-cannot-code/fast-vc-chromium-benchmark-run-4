333ddbb128dab77ea312556dfbf0926f4bdd36d6
Scott Yoder <scottyoder@google.com>
2025-09-11 06:13:23 -0700

    [ios] Add CrossPlatformPromosService
    This commit introduces the CrossPlatformPromosService to track user
    activity for cross-platform feature promos. It records active days and
    determines the 16th most recent active day, which will be used with the
    CrossDevicePrefTracker to allow Desktop to determine whether the user is
    active on iOS.
    
    Bug: 442562612
    Change-Id: Ibd99fd6d9ea047b2d64fcf125ed82c0db0f00e44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6924089
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1514230}

--- ios/chrome/app/profile/profile_controller.mm
+++ ios/chrome/app/profile/profile_controller.mm
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/language/core/browser/language_usage_metrics.h"
 #import "components/language/core/browser/pref_names.h"
 #import "components/prefs/pref_service.h"
+#import "components/sharing_message/features.h"
 #import "components/translate/core/browser/translate_metrics_logger_impl.h"
 #import "ios/chrome/app/application_delegate/app_state.h"
 #import "ios/chrome/app/application_delegate/metrics_mediator.h"
@@ -54,6 +55,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/app/tests_hook.h"
 #import "ios/chrome/browser/content_settings/model/host_content_settings_map_factory.h"
 #import "ios/chrome/browser/credential_provider/model/credential_provider_buildflags.h"
+#import "ios/chrome/browser/cross_platform_promos/model/cross_platform_promos_service.h"
+#import "ios/chrome/browser/cross_platform_promos/model/cross_platform_promos_service_factory.h"
 #import "ios/chrome/browser/enterprise/model/idle/idle_service.h"
 #import "ios/chrome/browser/enterprise/model/idle/idle_service_factory.h"
 #import "ios/chrome/browser/external_files/model/external_file_remover.h"
@@ -540,6 +543,11 @@ void RecordDiscardedSceneConnectedAfterBeingPurged(
   enterprise_idle::IdleServiceFactory::GetForProfile(profile)
       ->OnApplicationWillEnterForeground();
 
+  if (IsMobilePromoOnDesktopNotificationsEnabled()) {
+    CrossPlatformPromosServiceFactory::GetForProfile(profile)
+        ->OnApplicationWillEnterForeground();
+  }
+
   // Send the "Chrome opened" event to the feature engagement tracker on a
   // warm start.
   [self sendChromeOpenedEvent];
@@ -776,6 +784,10 @@ void RecordDiscardedSceneConnectedAfterBeingPurged(
   DCHECK(_state.profile);
   enterprise_idle::IdleServiceFactory::GetForProfile(_state.profile)
       ->OnApplicationWillEnterForeground();
+  if (IsMobilePromoOnDesktopNotificationsEnabled()) {
+    CrossPlatformPromosServiceFactory::GetForProfile(_state.profile)
+        ->OnApplicationWillEnterForeground();
+  }
 }
 
 - (void)sendChromeOpenedEvent {
--- ios/chrome/browser/shared/model/prefs/browser_prefs.mm
+++ ios/chrome/browser/shared/model/prefs/browser_prefs.mm
@@ -103,6 +103,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/content_suggestions/ui_bundled/safety_check/safety_check_prefs.h"
 #import "ios/chrome/browser/content_suggestions/ui_bundled/shop_card/shop_card_prefs.h"
 #import "ios/chrome/browser/content_suggestions/ui_bundled/tips/tips_prefs.h"
+#import "ios/chrome/browser/cross_platform_promos/model/cross_platform_promos_service.h"
 #import "ios/chrome/browser/download/model/auto_deletion/auto_deletion_service.h"
 #import "ios/chrome/browser/drive/model/drive_policy.h"
 #import "ios/chrome/browser/first_run/model/first_run.h"
@@ -654,6 +655,7 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
   collaboration::prefs::RegisterProfilePrefs(registry);
   commerce::RegisterPrefs(registry);
   AimEligibilityService::RegisterProfilePrefs(registry);
+  CrossPlatformPromosService::RegisterProfilePrefs(registry);
   data_controls::RegisterProfilePrefs(registry);
   dom_distiller::DistilledPagePrefs::RegisterProfilePrefs(registry);
   enterprise::RegisterIdentifiersProfilePrefs(registry);
