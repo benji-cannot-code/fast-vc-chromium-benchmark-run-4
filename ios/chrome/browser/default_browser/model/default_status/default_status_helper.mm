96726f3eb2ed6f9835f800d1c9ae2d5a84ecc9b2
Gauthier Ambard <gambard@chromium.org>
2025-09-11 08:23:50 -0700

    [iOS] Remove unnecessary compile-time check
    Now that we build with iOS 26, this CL removes the unnecessary compile-
    time checks.
    
    CL done with Gemini agent.
    
    This CL was uploaded by git cl split.
    
    R=gayane@google.com
    
    Bug: 444407825
    Change-Id: Ic34bd2e55dc7cc4c03abb08f880086020fd1014a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6940135
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gayane Petrosyan <gayane@google.com>
    Commit-Queue: Gayane Petrosyan <gayane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1514288}

--- ios/chrome/browser/default_browser/model/default_status/default_status_helper.mm
+++ ios/chrome/browser/default_browser/model/default_status/default_status_helper.mm
@@ -33,7 +33,7 @@ const base::TimeDelta kMinimumNewClientFirstRunAge = base::Days(28);
 
 // Gets the system call to use/override for testing. Uses a function so the
 // static variable can be contained inside the function.
-#if defined(__IPHONE_18_2) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_18_2
+
 base::OnceCallback<UIApplicationCategoryDefaultStatus(NSError**)>&
 GetSystemCallForTesting() API_AVAILABLE(ios(18.4)) {
   static base::NoDestructor<
@@ -41,11 +41,10 @@ GetSystemCallForTesting() API_AVAILABLE(ios(18.4)) {
       g_system_call_for_testing;
   return *g_system_call_for_testing;
 }
-#endif
 
 // Performs the actual system call to the default status check API. Wrapped in
 // its own function to facilitate mocking in tests.
-#if defined(__IPHONE_18_2) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_18_2
+
 UIApplicationCategoryDefaultStatus MakeSystemCall(NSError** error)
     API_AVAILABLE(ios(18.4)) {
   if (GetSystemCallForTesting()) {
@@ -56,7 +55,6 @@ UIApplicationCategoryDefaultStatus MakeSystemCall(NSError** error)
       defaultStatusForCategory:UIApplicationCategoryWebBrowser
                          error:error];
 }
-#endif
 
 }  // namespace
 
@@ -146,7 +144,6 @@ void AssignNewCohortIfNeeded(PrefService* local_state) {
   local_state->SetTime(kDefaultStatusAPINextRetry, next_retry);
 }
 
-#if defined(__IPHONE_18_2) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_18_2
 DefaultStatusAPIResult SystemToLocalEnum(
     UIApplicationCategoryDefaultStatus system_enum) API_AVAILABLE(ios(18.4)) {
   switch (system_enum) {
@@ -162,7 +159,6 @@ DefaultStatusAPIResult SystemToLocalEnum(
       return DefaultStatusAPIResult::kUnknown;
   }
 }
-#endif
 
 DefaultStatusRetention DetermineRetentionStatus(
     DefaultStatusAPIResult previous,
@@ -185,7 +181,6 @@ DefaultStatusRetention DetermineRetentionStatus(
 }
 
 void QueryDefaultStatusIfReadyAndLogResults(PrefService* local_state) {
-#if defined(__IPHONE_18_2) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_18_2
   if (@available(iOS 18.4, *)) {
     int cohort_number = local_state->GetInteger(kDefaultStatusAPICohort);
     DUMP_WILL_BE_CHECK(cohort_number > 0 && cohort_number <= kCohortCount);
@@ -253,17 +248,14 @@ void QueryDefaultStatusIfReadyAndLogResults(PrefService* local_state) {
       local_state->SetTime(kDefaultStatusAPINextRetry, next_retry);
     }
   }
-#endif
 }
 
-#if defined(__IPHONE_18_2) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_18_2
 void OverrideSystemCallForTesting(  // IN-TEST
     base::OnceCallback<UIApplicationCategoryDefaultStatus(NSError**)> cb)
     API_AVAILABLE(ios(18.4)) {
   CHECK_IS_TEST();
   internal::GetSystemCallForTesting() = std::move(cb);
 }
-#endif
 
 }  // namespace internal
 
--- ios/chrome/browser/default_browser/model/default_status/default_status_helper_unittest.mm
+++ ios/chrome/browser/default_browser/model/default_status/default_status_helper_unittest.mm
@@ -23,7 +23,6 @@ namespace default_status {
 
 namespace {
 
-#if defined(__IPHONE_18_2) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_18_2
 void ExpectSuccessHistogramsToHaveNoSamples(
     const base::HistogramTester& histogram_tester) {
   histogram_tester.ExpectTotalCount(
@@ -56,7 +55,6 @@ void ExpectSuccessHistogramsToHaveNoSamples(
   histogram_tester.ExpectTotalCount(
       "IOS.DefaultStatusAPI.HeuristicAssessment42", 0);
 }
-#endif
 
 }  // namespace
 
@@ -529,7 +527,7 @@ TEST_F(DefaultStatusHelperTest, AssignNewCohortIfNeeded) {
 }
 
 // Tests SystemToLocalEnum.
-#if defined(__IPHONE_18_2) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_18_2
+
 TEST_F(DefaultStatusHelperTest, SystemToLocalEnum) API_AVAILABLE(ios(18.4)) {
   DefaultStatusAPIResult result =
       internal::SystemToLocalEnum(UIApplicationCategoryDefaultStatusIsDefault);
@@ -541,7 +539,6 @@ TEST_F(DefaultStatusHelperTest, SystemToLocalEnum) API_AVAILABLE(ios(18.4)) {
       internal::SystemToLocalEnum((UIApplicationCategoryDefaultStatus)1000);
   EXPECT_EQ(result, DefaultStatusAPIResult::kUnknown);
 }
-#endif
 
 // Tests DetermineRetentionStatus.
 TEST_F(DefaultStatusHelperTest, DetermineRetentionStatus) {
@@ -565,7 +562,7 @@ TEST_F(DefaultStatusHelperTest, DetermineRetentionStatus) {
 
 // Tests that QueryDefaultStatusIfReadyAndLogResults doesn't do anything if the
 // system isn't running on the minimum version.
-#if defined(__IPHONE_18_2) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_18_2
+
 TEST_F(DefaultStatusHelperTest, QueryDefaultStatusAPINotAvailable)
 API_AVAILABLE(ios(18.4)) {
   if (@available(iOS 18.4, *)) {
@@ -589,11 +586,10 @@ API_AVAILABLE(ios(18.4)) {
       "IOS.DefaultStatusAPI.CooldownError.DaysLeft", 0);
   ExpectSuccessHistogramsToHaveNoSamples(histogram_tester);
 }
-#endif
 
 // Test that QueryDefaultStatusIfReadyAndLogResults does not do anything if
 // the client is still on cooldown according to local prefs.
-#if defined(__IPHONE_18_2) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_18_2
+
 TEST_F(DefaultStatusHelperTest, QueryDefaultStatusAPIOnLocalCooldown)
 API_AVAILABLE(ios(18.4)) {
   if (!@available(iOS 18.4, *)) {
@@ -619,11 +615,10 @@ API_AVAILABLE(ios(18.4)) {
       "IOS.DefaultStatusAPI.CooldownError.DaysLeft", 0);
   ExpectSuccessHistogramsToHaveNoSamples(histogram_tester);
 }
-#endif
 
 // Test that QueryDefaultStatusIfReadyAndLogResults correctly handles cooldown
 // errors returned by the default status system API.
-#if defined(__IPHONE_18_2) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_18_2
+
 TEST_F(DefaultStatusHelperTest, QueryDefaultStatusAPISystemCooldownError)
 API_AVAILABLE(ios(18.4)) {
   if (!@available(iOS 18.4, *)) {
@@ -663,11 +658,10 @@ API_AVAILABLE(ios(18.4)) {
       "IOS.DefaultStatusAPI.CooldownError.DaysLeft", 100, 1);
   ExpectSuccessHistogramsToHaveNoSamples(histogram_tester);
 }
-#endif
 
 // Test that QueryDefaultStatusIfReadyAndLogResults can handle unknown error
 // types returned by the default status system API.
-#if defined(__IPHONE_18_2) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_18_2
+
 TEST_F(DefaultStatusHelperTest, QueryDefaultStatusAPISystemUnknownError)
 API_AVAILABLE(ios(18.4)) {
   if (!@available(iOS 18.4, *)) {
@@ -699,7 +693,6 @@ API_AVAILABLE(ios(18.4)) {
       "IOS.DefaultStatusAPI.CooldownError.DaysLeft", 0);
   ExpectSuccessHistogramsToHaveNoSamples(histogram_tester);
 }
-#endif
 
 // Tests the success case of QueryDefaultStatusIfReadyAndLogResults for the
 // following scenario:
@@ -707,7 +700,7 @@ API_AVAILABLE(ios(18.4)) {
 //    - Within cohort 1 reporting window
 //    - Is default
 //    - No previous default status API result
-#if defined(__IPHONE_18_2) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_18_2
+
 TEST_F(DefaultStatusHelperTest,
        QueryDefaultStatusAPIStrictCohort1IsDefaultFirstTime)
 API_AVAILABLE(ios(18.4)) {
@@ -778,7 +771,6 @@ API_AVAILABLE(ios(18.4)) {
   histogram_tester.ExpectTotalCount(
       "IOS.DefaultStatusAPI.HeuristicAssessment42", 1);
 }
-#endif
 
 // Tests the success case of QueryDefaultStatusIfReadyAndLogResults for the
 // following scenario:
@@ -786,7 +778,7 @@ API_AVAILABLE(ios(18.4)) {
 //    - Outside cohort 2 reporting window
 //    - Is not default
 //    - Has a previous default status API result of "is default"
-#if defined(__IPHONE_18_2) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_18_2
+
 TEST_F(DefaultStatusHelperTest,
        QueryDefaultStatusAPICohort2IsNotDefaultRecurring)
 API_AVAILABLE(ios(18.4)) {
@@ -861,7 +853,6 @@ API_AVAILABLE(ios(18.4)) {
   histogram_tester.ExpectTotalCount(
       "IOS.DefaultStatusAPI.HeuristicAssessment42", 1);
 }
-#endif
 
 // Tests the success case of QueryDefaultStatusIfReadyAndLogResults for the
 // following scenario:
@@ -869,7 +860,7 @@ API_AVAILABLE(ios(18.4)) {
 //    - Inside cohort 3 reporting window
 //    - Is default
 //    - Has a previous default status API result of "is default"
-#if defined(__IPHONE_18_2) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_18_2
+
 TEST_F(DefaultStatusHelperTest,
        QueryDefaultStatusAPIStrictCohort3IsDefaultRecurring)
 API_AVAILABLE(ios(18.4)) {
@@ -946,6 +937,5 @@ API_AVAILABLE(ios(18.4)) {
   histogram_tester.ExpectTotalCount(
       "IOS.DefaultStatusAPI.HeuristicAssessment42", 1);
 }
-#endif
 
 }  // namespace default_status
