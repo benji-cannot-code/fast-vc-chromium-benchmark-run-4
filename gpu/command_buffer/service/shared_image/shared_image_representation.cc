6a7dbbfa1ea3eb011a47805472c96b8509c64093
Le Hoang Quyen <lehoangquyen@chromium.org>
2025-09-12 07:45:22 -0700

    Graphite: Add a feature param to enable/disable deferred submits
    If deferred submit param is disabled, Graphite's submit() will have to
    be called before any SI's EndAccess(). It overrides the deferred
    submits' support from backings.
    
    Bug: 443902292
    Change-Id: I68e17917a92c96a41e495c41d3e4ef4fab7a8210
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6942454
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Quyen Le <lehoangquyen@chromium.org>
    Auto-Submit: Quyen Le <lehoangquyen@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1514853}

--- gpu/command_buffer/service/shared_image/shared_image_representation.cc
+++ gpu/command_buffer/service/shared_image/shared_image_representation.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/service/shared_image/shared_image_format_service_utils.h"
 #include "gpu/command_buffer/service/shared_image/shared_image_manager.h"
 #include "gpu/command_buffer/service/texture_manager.h"
+#include "gpu/config/gpu_finch_features.h"
 #include "third_party/skia/include/core/SkColorSpace.h"
 #include "third_party/skia/include/core/SkImage.h"
 #include "third_party/skia/include/gpu/MutableTextureState.h"
@@ -223,6 +224,21 @@ bool SkiaImageRepresentation::SupportsMultipleConcurrentReadAccess() {
   return false;
 }
 
+bool SkiaImageRepresentation::SupportsDeferredGraphiteSubmit() {
+  return false;
+}
+
+bool SkiaImageRepresentation::NeedGraphiteContextSubmitBeforeEndAccess() {
+  if (!features::kSkiaGraphiteEnableDeferredSubmit.Get()) {
+    // If deferred submit is disabled, then a submit is always required.
+    return true;
+  }
+
+  //  If the backing can support deferred submissions, then we don't need to
+  //  submit before EndAccess().
+  return !SupportsDeferredGraphiteSubmit();
+}
+
 SkiaImageRepresentation::ScopedWriteAccess::ScopedWriteAccess(
     SkiaImageRepresentation* representation,
     std::vector<sk_sp<SkSurface>> surfaces)
@@ -296,11 +312,6 @@ SkiaGaneshImageRepresentation::SkiaGaneshImageRepresentation(
     MemoryTypeTracker* tracker)
     : SkiaImageRepresentation(manager, backing, tracker),
       gr_context_(gr_context) {}
-
-bool SkiaGaneshImageRepresentation::NeedGraphiteContextSubmitBeforeEndAccess() {
-  // Ganesh shouldn't need a Graphite context submit.
-  return false;
-}
 
 SkiaGaneshImageRepresentation::ScopedGaneshWriteAccess::ScopedGaneshWriteAccess(
     base::PassKey<SkiaGaneshImageRepresentation> /* pass_key */,
@@ -575,12 +586,6 @@ SkiaGraphiteImageRepresentation::SkiaGraphiteImageRepresentation(
     SharedImageBacking* backing,
     MemoryTypeTracker* tracker)
     : SkiaImageRepresentation(manager, backing, tracker) {}
-
-bool SkiaGraphiteImageRepresentation::
-    NeedGraphiteContextSubmitBeforeEndAccess() {
-  // As default, assume Graphite context submit is needed.
-  return true;
-}
 
 SkiaGraphiteImageRepresentation::ScopedGraphiteWriteAccess::
     ScopedGraphiteWriteAccess(
