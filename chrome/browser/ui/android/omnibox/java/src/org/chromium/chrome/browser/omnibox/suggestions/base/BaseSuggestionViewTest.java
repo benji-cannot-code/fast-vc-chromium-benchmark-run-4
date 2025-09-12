c05dc80618cc2bae0c13e5db2ad5e0b732ee9d52
Brandon Fong <bjfong@google.com>
2025-09-12 16:47:39 -0700

    [hub] Add padding for hub search suggestion dropdowns in place of button
    Add padding for hub search suggestions in the omnibox dropdown as there
    is no action / refinement button to act as a buffer.
    
    Bug: 437987575
    Change-Id: Ieb30aeb12ca1c9ce4dddb46289d1cd56ddf0bcfe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6940887
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1515152}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/BaseSuggestionViewTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/BaseSuggestionViewTest.java
@@ -44,6 +44,7 @@ public class BaseSuggestionViewTest {
     private int mCompactSuggestionViewHeight;
     private int mDecorationIconWidthPx;
     private int mLargeDecorationIconWidthPx;
+    private int mSuggestionEndPaddingNoActionButtonPx;
 
     private BaseSuggestionViewForTest mView;
     private Activity mActivity;
@@ -117,6 +118,12 @@ public class BaseSuggestionViewTest {
                 mActivity
                         .getResources()
                         .getDimensionPixelSize(R.dimen.omnibox_suggestion_icon_area_size_large);
+
+        mSuggestionEndPaddingNoActionButtonPx =
+                mActivity
+                        .getResources()
+                        .getDimensionPixelSize(
+                                R.dimen.omnibox_suggestion_end_padding_no_action_button);
     }
 
     /**
@@ -375,7 +382,8 @@ public class BaseSuggestionViewTest {
         final int paddingEnd = 22;
 
         final int expectedContentLeft = paddingStart + mDecorationIconWidthPx;
-        final int expectedContentRight = giveSuggestionWidth - paddingEnd;
+        final int expectedContentRight =
+                giveSuggestionWidth - paddingEnd - mSuggestionEndPaddingNoActionButtonPx;
 
         mView.setPaddingRelative(paddingStart, 0, paddingEnd, 0);
         executeLayoutTest(giveSuggestionWidth, giveContentHeight, View.LAYOUT_DIRECTION_LTR);
@@ -403,7 +411,7 @@ public class BaseSuggestionViewTest {
         final int paddingStart = 57;
         final int paddingEnd = 31;
 
-        final int expectedContentLeft = paddingEnd;
+        final int expectedContentLeft = paddingEnd + mSuggestionEndPaddingNoActionButtonPx;
         final int expectedContentRight =
                 giveSuggestionWidth - paddingStart - mDecorationIconWidthPx;
 
--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/SuggestionLayout.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/SuggestionLayout.java
@@ -39,6 +39,7 @@ class SuggestionLayout extends ViewGroup {
     private final @Px int mActionButtonWidthPx;
     private final @Px int mContentPaddingPx;
     private final @Px int mMinimumContentPadding;
+    private final @Px int mSuggestionEndPaddingNoActionButtonPx;
     private boolean mUseLargeDecoration;
     private boolean mShowDecoration;
 
@@ -171,6 +172,9 @@ class SuggestionLayout extends ViewGroup {
 
         mContentPaddingPx = res.getDimensionPixelSize(R.dimen.omnibox_suggestion_content_padding);
         mMinimumContentPadding = res.getDimensionPixelSize(R.dimen.omnibox_simple_card_leadin);
+
+        mSuggestionEndPaddingNoActionButtonPx =
+                res.getDimensionPixelSize(R.dimen.omnibox_suggestion_end_padding_no_action_button);
 
         mOutlineProvider =
                 new RoundedCornerOutlineProvider(
@@ -332,6 +336,7 @@ class SuggestionLayout extends ViewGroup {
         // Reserve space for the decoration view if it's present. Otherwise, ensure a minimal
         // padding.
         var contentWidthPx = suggestionWidthPx - getContentStart();
+        int additionalPaddingEnd = mSuggestionEndPaddingNoActionButtonPx;
 
         // Measure all other views surrounding the CONTENT area. Currently these are only
         // ACTION_BUTTONs.
@@ -342,9 +347,10 @@ class SuggestionLayout extends ViewGroup {
             var params = (LayoutParams) view.getLayoutParams();
             if (params.getViewType() == LayoutParams.SuggestionViewType.ACTION_BUTTON) {
                 contentWidthPx -= mActionButtonWidthPx;
+                additionalPaddingEnd = 0;
             }
         }
-        return contentWidthPx;
+        return contentWidthPx - additionalPaddingEnd;
     }
 
     /**
