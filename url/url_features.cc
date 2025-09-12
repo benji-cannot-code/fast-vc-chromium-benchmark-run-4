0ee594e62434f464d97c5fdbb64e0cabd02aa692
Xiaohan Wang <xhwang@chromium.org>
2025-09-12 11:43:57 -0700

    url: Migrate BASE_FEATURE macros to 2-argument version
    This CL migrates three-argument BASE_FEATURE macros in //url to the
    new two-argument version where the C++ identifier and the string feature
    name match.
    
    This automated refactoring helps ensure that the C++ identifier and the
    string feature name always match, reducing potential errors.
    
    Enabled-by-default-reason: refactor
    
    Bug: 436274260
    Change-Id: I61e6c056d11d1c8bf6765b7675dcad87a970ee87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6938988
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1514997}

--- url/url_features.cc
+++ url/url_features.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace url {
 
 BASE_FEATURE(kDisallowSpaceCharacterInURLHostParsing,
-             "DisallowSpaceCharacterInURLHostParsing",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 bool IsDisallowingSpaceCharacterInURLHostParsing() {
