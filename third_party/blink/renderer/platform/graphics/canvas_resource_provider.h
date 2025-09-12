7e80ade312c7f6b61f358d2f0c00ee098a6dc2b5
Colin Blundell <blundell@chromium.org>
2025-09-11 23:53:09 -0700

    [Blink] Change DragImage to hold CanvasResourceProviderBitmap explicitly
    This is possible now that this class is publicly exposed.
    
    Bug: 352263194
    Change-Id: Idcafc9f8270041e628c2517b55193adc03da87c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6939371
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1514688}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
@@ -67,6 +67,7 @@ PLATFORM_EXPORT BASE_DECLARE_FEATURE(kUseCRPSIForLowLatencyOnWindows);
 
 class CanvasResource;
 class CanvasResourceSharedImage;
+class CanvasResourceProviderBitmap;
 class CanvasResourceProviderSharedImage;
 class ExternalCanvasResource;
 class MemoryManagedPaintCanvas;
@@ -134,7 +135,7 @@ class PLATFORM_EXPORT CanvasResourceProvider
   // Used to determine if the provider is going to be initialized or not.
   enum class ShouldInitialize { kNo, kCallClear };
 
-  static std::unique_ptr<CanvasResourceProvider> CreateBitmapProvider(
+  static std::unique_ptr<CanvasResourceProviderBitmap> CreateBitmapProvider(
       gfx::Size size,
       viz::SharedImageFormat format,
       SkAlphaType alpha_type,
@@ -528,6 +529,9 @@ class PLATFORM_EXPORT CanvasResourceProviderBitmap
       override {
     draw_callback(Canvas());
   }
+  scoped_refptr<StaticBitmapImage> Snapshot(
+      FlushReason reason,
+      ImageOrientation = ImageOrientationEnum::kDefault) override;
 
  private:
   scoped_refptr<CanvasResource> ProduceCanvasResource(FlushReason) override {
@@ -535,9 +539,6 @@ class PLATFORM_EXPORT CanvasResourceProviderBitmap
     // not supported by this class.
     return nullptr;
   }
-  scoped_refptr<StaticBitmapImage> Snapshot(
-      FlushReason reason,
-      ImageOrientation orientation) override;
   sk_sp<SkSurface> CreateSkSurface() const override;
 };
 
