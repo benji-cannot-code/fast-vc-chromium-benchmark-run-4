251d8d54ca2aa3b6b70e07d1a2871fa7a45a75a4
Zainab Rizvi <rizvis@google.com>
2025-09-12 16:45:39 -0700

    [IPP] Add `move_only` to BlindSignedAuthToken
    Changes requisite signatures to pass by value.
    
    Change-Id: Ia171fba6244151fdf8f9d02a9a9b1e18f062aecc
    Fixed: 440448421
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6935005
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Auto-Submit: Zainab Rizvi <rizvis@google.com>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515150}

--- android_webview/browser/ip_protection/aw_ip_protection_core_host.cc
+++ android_webview/browser/ip_protection/aw_ip_protection_core_host.cc
@@ -164,7 +164,7 @@ void AwIpProtectionCoreHost::TryGetAuthTokens(
 
 void AwIpProtectionCoreHost::RecycleTokens(
     ip_protection::ProxyLayer proxy_layer,
-    const std::vector<ip_protection::BlindSignedAuthToken>& tokens) {
+    std::vector<ip_protection::BlindSignedAuthToken> tokens) {
   // Not yet implemented for WebView.
   NOTIMPLEMENTED();
 }
