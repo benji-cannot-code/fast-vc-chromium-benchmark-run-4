1383f2a6ae5feca2bf0d1f56e3910e6a2a3bb8f2
David Maunder <davidjm@google.com>
2025-09-12 11:02:40 -0700

    Add auth type to remaining EndpointFetcher usage in SecondDeviceAuthBroker
    crrev.com/c/6940585 added in authentication to one EndpointFetcher
    invocation in SecondDeviceAuthBroker. This adds in the remaining.
    
    Bug: 439911463, 444110669
    Change-Id: Ie7b68db283df6df6c47794d0082d3e1fa5eef758
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6943710
    Reviewed-by: Brittany Hartmire <bhartmire@google.com>
    Commit-Queue: Brittany Hartmire <bhartmire@google.com>
    Cr-Commit-Position: refs/heads/main@{#1514965}

--- chrome/browser/ash/login/oobe_quick_start/second_device_auth_broker.cc
+++ chrome/browser/ash/login/oobe_quick_start/second_device_auth_broker.cc
@@ -592,6 +592,7 @@ void SecondDeviceAuthBroker::FetchAuthCode(
       /*identity_manager=*/nullptr,
       EndpointFetcher::RequestParams::Builder(kHttpPost,
                                               kStartSessionAnnotation)
+          .SetAuthType(endpoint_fetcher::CHROME_API_KEY)
           .SetChannel(ash::GetChannel())
           .SetContentType(kHttpContentType)
           .SetPostData(
