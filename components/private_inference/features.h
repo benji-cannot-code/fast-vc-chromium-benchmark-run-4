79b00bd8a94f78aa57aaa42446e1e5042db384bb
Christian Dullweber <dullweber@chromium.org>
2025-09-12 03:43:53 -0700

    PI: Add folders and feature for private inference
    Add folder structure and a feature with parameters for the endpoint and
    the api key.
    
    Bug: 444195208
    Change-Id: I41a391a021fdedc0d3638cc16650f4add7137567
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6940197
    Reviewed-by: Mohamad Arab <arabm@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1514759}

---
+++ components/private_inference/features.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_PRIVATE_INFERENCE_FEATURES_H_
+#define COMPONENTS_PRIVATE_INFERENCE_FEATURES_H_
+
+#include "base/features.h"
+#include "base/metrics/field_trial_params.h"
+
+namespace private_inference {
+
+// The feature for private inference.
+BASE_DECLARE_FEATURE(kPrivateInference);
+
+// The API key for private inference.
+extern const base::FeatureParam<std::string> kPrivateInferenceApiKey;
+
+// Endpoint for private inference
+extern const base::FeatureParam<std::string> kPrivateInferenceUrl;
+
+}  // namespace private_inference
+
+#endif  // COMPONENTS_PRIVATE_INFERENCE_FEATURES_H_
