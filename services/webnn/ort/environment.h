d52987534da4ca7e1abd882797367a142b8ab7ff
mingmingtasd <mingming1.xu@intel.com>
2025-09-12 02:09:25 -0700

    WebNN: Introduce custom EP devices sorting method for ORT backend
    This CL refactors the EP device selection logic into a reusable method
    that uses WebNN's custom sorting. The sorting logic closely mimics
    ORT's approach, but prioritizes the WebGPU EP over the DML EP
    specifically for GPU devices.
    
    Additionally, this CL optimizes to only apply these workarounds and
    configuration entries for the EPs that will be selected according to
    the given device type.
    
    Bug: 439708046, 429859159, 439972928
    Change-Id: I1c4fe2d5b030f0ec5d340d9971ad74fa9e9dffaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6914406
    Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Commit-Queue: Mingming1 Xu <mingming1.xu@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1514731}

--- services/webnn/ort/environment.h
+++ services/webnn/ort/environment.h
@@ -56,6 +56,8 @@ class Environment : public base::subtle::RefCountedThreadSafeBase {
   void AddRef() const;
   void Release() const;
 
+  // Get combined EP workarounds for the EPs that will be selected according to
+  // the given device type.
   EpWorkarounds GetEpWorkarounds(mojom::Device device_type) const;
 
   const OrtEnv* get() const { return env_.get(); }
@@ -65,8 +67,8 @@ class Environment : public base::subtle::RefCountedThreadSafeBase {
     base::cstring_view value;
   };
 
-  // Get all EP-specific session configuration entries for the given device
-  // type.
+  // Get all EP-specific session configuration entries for the EPs that will be
+  // selected according to the given device type.
   std::vector<SessionConfigEntry> GetEpConfigEntries(
       mojom::Device device_type) const;
 
