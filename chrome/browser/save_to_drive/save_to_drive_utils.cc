9937301726f325bcedadd151d2b2d4e81ce5f44d
Faizur Rahman <faizur@chromium.org>
2025-09-12 13:44:56 -0700

    [Save to Drive] Add a utility function to get StreamContainer from RFH
    Refactored the code to get the `StreamContainer` from
    `RenderFrameHost` into a utility function. This function will be reused
    in a subsequent CL in `SaveToDriveFlow`.
    
    Bug: 435142523
    
    Change-Id: Ibe8fb8bc93ff9b7f44f886e534f63bdcc6580eca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6941254
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Auto-Submit: Faizur Rahman <faizur@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Faizur Rahman <faizur@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515074}

---
+++ chrome/browser/save_to_drive/save_to_drive_utils.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/save_to_drive/save_to_drive_utils.h"
+
+#include "base/memory/weak_ptr.h"
+#include "chrome/browser/pdf/pdf_viewer_stream_manager.h"
+#include "extensions/browser/guest_view/mime_handler_view/mime_handler_view_guest.h"
+#include "pdf/pdf_features.h"
+
+namespace save_to_drive {
+
+base::WeakPtr<extensions::StreamContainer> GetStreamWeakPtr(
+    content::RenderFrameHost* render_frame_host) {
+  if (!chrome_pdf::features::IsOopifPdfEnabled()) {
+    auto* guest_view = extensions::MimeHandlerViewGuest::FromRenderFrameHost(
+        render_frame_host);
+    return guest_view ? guest_view->GetStreamWeakPtr() : nullptr;
+  }
+  if (!render_frame_host) {
+    return nullptr;
+  }
+  content::RenderFrameHost* embedder_host = render_frame_host->GetParent();
+  auto* manager =
+      pdf::PdfViewerStreamManager::FromRenderFrameHost(embedder_host);
+  return manager ? manager->GetStreamContainer(embedder_host) : nullptr;
+}
+
+}  // namespace save_to_drive
