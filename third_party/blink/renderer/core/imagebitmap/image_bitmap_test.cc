0f8d64ba4c4ea8b26d2c72a98dbc9a7a4a20dcab
Colin Blundell <blundell@chromium.org>
2025-09-12 00:00:10 -0700

    [Blink] Have ImageBitmap test use OOP-R
    These tests create CanvasResourceProviderSharedImage instances, which
    we're hardening to require OOP-R.
    
    Bug: 391648152
    Change-Id: I70c58e9a47027cd789823242f395e6d257ba8589
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6940098
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1514693}

--- third_party/blink/renderer/core/imagebitmap/image_bitmap_test.cc
+++ third_party/blink/renderer/core/imagebitmap/image_bitmap_test.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/imagebitmap/image_bitmap.h"
 
 #include "build/build_config.h"
+#include "components/viz/test/test_raster_interface.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/platform/scheduler/test/renderer_scheduler_test_support.h"
@@ -85,8 +86,10 @@ class ImageBitmapTest : public testing::Test {
         ReplaceMemoryCacheForTesting(MakeGarbageCollected<MemoryCache>(
             blink::scheduler::GetSingleThreadTaskRunnerForTesting()));
 
-    test_context_provider_ = viz::TestContextProvider::Create();
-    InitializeSharedGpuContextGLES2(test_context_provider_.get());
+    test_context_provider_ = viz::TestContextProvider::CreateRaster();
+    test_context_provider_->UnboundTestRasterInterface()->set_gpu_rasterization(
+        true);
+    InitializeSharedGpuContextRaster(test_context_provider_.get());
   }
 
   void TearDown() override {
