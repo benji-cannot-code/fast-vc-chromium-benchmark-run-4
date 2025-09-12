b113c9a7183a3a527e1c2fe011cc9dabc7099675
Christoph Schwering <schwering@google.com>
2025-09-12 07:49:42 -0700

    [Autofill] Include NO_SERVER_DATA, kUnrecognized in kAll[Html]FieldTypes
    This change is a no-op because no current production-coder reference
    to kAll[Html]FieldTypes depends whether NO_SERVER_DATA or
    kUnrecognized is present in the set.
    
    Bug: 40100455
    Change-Id: I35d83f397cc95591840627aab79c9d64ca0b7ca2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6939756
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Karol Sygiet <sygiet@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1514856}

--- components/autofill/core/browser/field_types.h
+++ components/autofill/core/browser/field_types.h
@@ -596,8 +596,9 @@ template <>
 struct DenseSetTraits<FieldType>
     : EnumDenseSetTraits<FieldType, NO_SERVER_DATA, MAX_VALID_FIELD_TYPE> {
   static constexpr bool is_valid(FieldType x) {
-    return ToSafeFieldType(base::to_underlying(x), NO_SERVER_DATA) !=
-           NO_SERVER_DATA;
+    return x == NO_SERVER_DATA ||
+           ToSafeFieldType(base::to_underlying(x), NO_SERVER_DATA) !=
+               NO_SERVER_DATA;
   }
 };
 
@@ -607,9 +608,10 @@ struct DenseSetTraits<HtmlFieldType>
                          HtmlFieldType(0),
                          HtmlFieldType::kMaxValue> {
   static constexpr bool is_valid(HtmlFieldType x) {
-    return ToSafeHtmlFieldType(base::to_underlying(x),
+    return x == HtmlFieldType::kUnrecognized ||
+           ToSafeHtmlFieldType(base::to_underlying(x),
                                HtmlFieldType::kUnrecognized) !=
-           HtmlFieldType::kUnrecognized;
+               HtmlFieldType::kUnrecognized;
   }
 };
 
