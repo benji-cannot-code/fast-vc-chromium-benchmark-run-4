cdc5fa823e4613552d2fb45a39a7f3b4137fd042
Alexei Svitkine <asvitkine@chromium.org>
2025-09-12 11:54:14 -0700

    Restrict `AssociateGoogleVariationID` using `base::PassKey`.
    The `AssociateGoogleVariationID` functions are now restricted to be called only from `VariationsSeedProcessor` and `SyntheticTrialRegistry` using `base::PassKey` for privacy reasons. Tests now use a new `AssociateGoogleVariationIDForTesting` function.
    
    Additionally, some clean ups on the APIs are performed, such as
    making the other params from the two passkey callsites consistent.
    
    Note: This CL is not sufficient to fully restrict things as there are
    additional APIs that need to be tightened up, that will be done in
    future CLs.
    
    Bug: 315411418
    Change-Id: Iec8025e3a1b36abcb38bb823da32a65418faf56c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6941003
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Olivia Saul <osaul@google.com>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515004}

--- components/autofill/core/browser/payments/payments_network_interface_test_base.cc
+++ components/autofill/core/browser/payments/payments_network_interface_test_base.cc
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2024 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
 #include "components/autofill/core/browser/payments/payments_network_interface_test_base.h"
 
 #include <string>
@@ -53,7 +54,7 @@ void PaymentsNetworkInterfaceTestBase::CreateFieldTrialWithId(
     const std::string& trial_name,
     const std::string& group_name,
     int variation_id) {
-  variations::AssociateGoogleVariationID(
+  variations::AssociateGoogleVariationIDForTesting(
       variations::GOOGLE_WEB_PROPERTIES_ANY_CONTEXT, trial_name, group_name,
       static_cast<variations::VariationID>(variation_id));
   base::FieldTrialList::CreateFieldTrial(trial_name, group_name)->Activate();
