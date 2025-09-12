7e493c11d3cb1930c27f781693ebd8d311ae4cac
Foromo Daniel Soromou <koretadaniel@chromium.org>
2025-09-12 05:52:56 -0700

    [Refactor Branded Strings] IDS_HISTORY_HISTORY_MENU_ITEM
    See: https://docs.google.com/document/d/1gK_4bAsyWtVHl8spYHumozi4k9vl4Yl3V9MratFK8f0
    
    Bug: 443700721
    Change-Id: Iad25147ab7a481501c4ce48da38f2e85966a632b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6940890
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Daniel Soromou <koretadaniel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1514818}

--- chrome/browser/ui/webui/cr_components/history/history_util.cc
+++ chrome/browser/ui/webui/cr_components/history/history_util.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/cr_components/history_clusters/history_clusters_util.h"
 #include "chrome/browser/ui/webui/favicon_source.h"
 #include "chrome/browser/ui/webui/history/history_sign_in_state_watcher.h"
+#include "chrome/grit/branded_strings.h"
 #include "chrome/grit/generated_resources.h"
 #include "chrome/grit/history_resources.h"
 #include "chrome/grit/history_resources_map.h"
