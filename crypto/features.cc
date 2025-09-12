4d436a4bcb84b418197d2b6f5d1de0513edd5219
Xiaohan Wang <xhwang@chromium.org>
2025-09-12 12:15:05 -0700

    crypto: Migrate BASE_FEATURE macros to 2-argument version
    This CL migrates three-argument BASE_FEATURE macros in //crypto to the
    new two-argument version where the C++ identifier and the string feature
    name match.
    
    This automated refactoring helps ensure that the C++ identifier and the
    string feature name always match, reducing potential errors.
    
    Enabled-by-default-reason: refactor
    
    Bug: 436274260
    Change-Id: I21c66dd67f4e9f2673f376af15e4231f9dc036c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6937523
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515019}

--- crypto/features.cc
+++ crypto/features.cc
@@ -9,16 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace crypto::features {
 
-BASE_FEATURE(kProcessBoundStringEncryption,
-             "ProcessBoundStringEncryption",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+BASE_FEATURE(kProcessBoundStringEncryption, base::FEATURE_ENABLED_BY_DEFAULT);
 
-BASE_FEATURE(kIsHardwareBackedFixEnabled,
-             "IsHardwareBackedFixEnabled",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+BASE_FEATURE(kIsHardwareBackedFixEnabled, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kMigrateIOSKeychainAccessibility,
-             "MigrateIOSKeychainAccessibility",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 }  // namespace crypto::features
