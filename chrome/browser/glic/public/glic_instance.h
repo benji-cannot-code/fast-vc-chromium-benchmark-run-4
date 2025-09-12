5ea7f897950f3e2c54e46c1e44d2d1a7b65977c7
Ian Wells <iwells@chromium.org>
2025-09-12 11:26:28 -0700

    Glic multi instance: create GlicInstance public interface
    The next CL will make the existing GlicInstance into GlicInstanceImpl
    which implements GlicInstance.
    
    Bug: b/444685015
    Change-Id: I9525918feddbdb213fba523857e08ffe626e36eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6944134
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Auto-Submit: Ian Wells <iwells@chromium.org>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1514984}

---
+++ chrome/browser/glic/public/glic_instance.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_GLIC_PUBLIC_GLIC_INSTANCE_H_
+#define CHROME_BROWSER_GLIC_PUBLIC_GLIC_INSTANCE_H_
+
+#include "base/uuid.h"
+
+namespace glic {
+
+class Host;
+
+// A type alias for the Glic instance identifier.
+using InstanceId = base::Uuid;
+
+// Public interface for one instance of the glic web client.
+class GlicInstance {
+ public:
+  virtual ~GlicInstance() = default;
+
+  // Get this instance's Host which manages the chrome://glic WebContents.
+  virtual Host& host() = 0;
+
+  // Get this instance's unique identifier.
+  virtual const InstanceId& id() const = 0;
+};
+
+}  // namespace glic
+
+#endif  // CHROME_BROWSER_GLIC_PUBLIC_GLIC_INSTANCE_H_
