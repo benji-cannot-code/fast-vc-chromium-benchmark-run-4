d204983fd3e3eee39059204d422bcc7674157b26
Scott Yoder <scottyoder@google.com>
2025-09-12 07:41:54 -0700

    [ios] Fix flaky HistorySystemTestCase/testPartialDeletion
    This test was failing due to a timeout while waiting for the clear
    browsing data confirmation dialog to disappear. The deletion is an
    asynchronous operation, and the test was probably not waiting long
    enough for it to complete.
    
    Fixed: 443106016
    Change-Id: I7993d7ab141f9fcee091e44a69a86a59ee3f19eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6941182
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1514852}

--- ios/chrome/browser/history/ui_bundled/history_system_egtest.mm
+++ ios/chrome/browser/history/ui_bundled/history_system_egtest.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/strings/stringprintf.h"
 #import "base/strings/sys_string_conversions.h"
 #import "base/strings/utf_string_conversions.h"
+#import "base/test/ios/wait_util.h"
 #import "components/browsing_data/core/browsing_data_utils.h"
 #import "components/browsing_data/core/pref_names.h"
 #import "components/sync/base/command_line_switches.h"
@@ -37,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "net/test/embedded_test_server/http_request.h"
 #import "net/test/embedded_test_server/http_response.h"
 
+using base::test::ios::kWaitForClearBrowsingDataTimeout;
 using chrome_test_util::BrowsingDataButtonMatcher;
 using chrome_test_util::ClearBrowsingDataButton;
 using chrome_test_util::ClearBrowsingDataView;
@@ -180,7 +182,8 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
 
   // Wait for the browsing data button to disappear.
   [ChromeEarlGrey
-      waitForUIElementToDisappearWithMatcher:BrowsingDataButtonMatcher()];
+      waitForUIElementToDisappearWithMatcher:BrowsingDataButtonMatcher()
+                                     timeout:kWaitForClearBrowsingDataTimeout];
 }
 
 #pragma mark Tests
@@ -339,13 +342,7 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
 
 // Tests that if only some of the history entries are deleted from Delete
 // Browsing Data, then the history view is updated to reflect those deletions.
-// TODO(crbug.com/443700732): Test disabled on simulator.
-#if TARGET_OS_SIMULATOR
-#define MAYBE_testPartialDeletion DISABLED_testPartialDeletion
-#else
-#define MAYBE_testPartialDeletion testPartialDeletion
-#endif
-- (void)MAYBE_testPartialDeletion {
+- (void)testPartialDeletion {
   const char olderURLString[] = "https://example.com";
   const GURL olderURL = GURL(olderURLString);
 
