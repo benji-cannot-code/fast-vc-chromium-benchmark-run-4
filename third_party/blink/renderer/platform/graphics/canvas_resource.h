c2268111a7432e479db8716c4c31f1082a52895e
Mingjing Zhang <mjzhang@chromium.org>
2025-09-12 14:20:29 -0700

    Split GetSyncTokenWithOptionalVerification()
    As part of the effort of applying scoped SyncToken access in
    CanvasResourceProvider, this CL splits the member function
    GetSyncTokenWithOptionalVerification() into GetSyncToken()
    and VerifySyncToken() in order to better organize the code logic
    in a way that makes it easier to apply the scoped access refactoring.
    
    Bug: 40286368
    Change-Id: I1e78026b96e08c8ad32a9f3409ef9263c5328c3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6936184
    Commit-Queue: Mingjing Zhang <mjzhang@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515095}

--- third_party/blink/renderer/platform/graphics/canvas_resource.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource.h
@@ -107,9 +107,7 @@ class PLATFORM_EXPORT CanvasResource
   // Returns the sync token to indicate when all writes to the current resource
   // are finished on the GPU thread. Note that the token is not guaranteed to be
   // verified at the time of calling this method.
-  const gpu::SyncToken GetSyncToken() {
-    return GetSyncTokenWithOptionalVerification(false);
-  }
+  virtual const gpu::SyncToken GetSyncToken() = 0;
 
   // Provides a TransferableResource representation of this resource to share it
   // with the compositor.
@@ -179,15 +177,12 @@ class PLATFORM_EXPORT CanvasResource
   // Returns true if the resource is rastered via the GPU.
   virtual bool UsesAcceleratedRaster() const = 0;
 
-  // Returns the sync token to indicate when all writes to the current resource
-  // are finished on the GPU thread. Note that in some subclasses the token is
-  // not guaranteed to be verified at the time of calling this method. Passing
-  // true for `needs_verified_token` ensures that the returned token will be
-  // verified.
-  virtual const gpu::SyncToken GetSyncTokenWithOptionalVerification(
-      bool needs_verified_token) {
-    NOTREACHED();
-  }
+  // Verify the sync token that indicates when all writes to the current
+  // resource are finished on the GPU thread. Note that in some subclasses the
+  // token is already verified by GetSyncToken() so this function is no-op for
+  // those classes.
+  virtual void VerifySyncToken() {}
+  virtual const gpu::SyncToken& sync_token() const = 0;
 
   bool is_origin_clean_ = true;
   HighEntropyCanvasOpType high_entropy_canvas_op_types_ =
@@ -223,6 +218,8 @@ class PLATFORM_EXPORT CanvasResourceSharedImage final : public CanvasResource {
   bool IsValid() const final;
   scoped_refptr<StaticBitmapImage> Bitmap() final;
   void Transfer() final;
+
+  const gpu::SyncToken GetSyncToken() override;
 
   void NotifyResourceLost() final;
   void BeginWriteAccess();
@@ -282,8 +279,7 @@ class PLATFORM_EXPORT CanvasResourceSharedImage final : public CanvasResource {
 
   base::WeakPtr<WebGraphicsContext3DProviderWrapper> ContextProviderWrapper()
       const override;
-  const gpu::SyncToken GetSyncTokenWithOptionalVerification(
-      bool needs_verified_token) override;
+  void VerifySyncToken() override;
   bool UsesAcceleratedRaster() const final { return is_accelerated_; }
 
   CanvasResourceProvider* Provider() override;
@@ -320,7 +316,7 @@ class PLATFORM_EXPORT CanvasResourceSharedImage final : public CanvasResource {
   bool mailbox_needs_new_sync_token() const {
     return owning_thread_data_.mailbox_needs_new_sync_token;
   }
-  const gpu::SyncToken& sync_token() const {
+  const gpu::SyncToken& sync_token() const override {
     return owning_thread_data_.sync_token;
   }
 
@@ -359,6 +355,7 @@ class PLATFORM_EXPORT ExternalCanvasResource final : public CanvasResource {
       const final {
     return client_si_;
   }
+  const gpu::SyncToken GetSyncToken() override;
 
   scoped_refptr<StaticBitmapImage> Bitmap() override;
 
@@ -369,8 +366,6 @@ class PLATFORM_EXPORT ExternalCanvasResource final : public CanvasResource {
     return resource_source_;
   }
   bool UsesAcceleratedRaster() const final { return true; }
-  const gpu::SyncToken GetSyncTokenWithOptionalVerification(
-      bool needs_verified_token) override;
   base::WeakPtr<WebGraphicsContext3DProviderWrapper> ContextProviderWrapper()
       const override;
 
@@ -383,6 +378,7 @@ class PLATFORM_EXPORT ExternalCanvasResource final : public CanvasResource {
       base::WeakPtr<WebGraphicsContext3DProviderWrapper>);
 
   SkAlphaType GetAlphaType() const { return alpha_type_; }
+  const gpu::SyncToken& sync_token() const override { return sync_token_; }
 
   scoped_refptr<gpu::ClientSharedImage> client_si_;
   const base::WeakPtr<WebGraphicsContext3DProviderWrapper>
@@ -423,11 +419,10 @@ class PLATFORM_EXPORT CanvasResourceSwapChain final : public CanvasResource {
   void PresentSwapChain();
   const scoped_refptr<gpu::ClientSharedImage>& GetClientSharedImage()
       const override;
+  const gpu::SyncToken GetSyncToken() override;
 
  private:
   bool UsesAcceleratedRaster() const final { return true; }
-  const gpu::SyncToken GetSyncTokenWithOptionalVerification(
-      bool needs_verified_token) override;
   base::WeakPtr<WebGraphicsContext3DProviderWrapper> ContextProviderWrapper()
       const override;
 
@@ -438,6 +433,7 @@ class PLATFORM_EXPORT CanvasResourceSwapChain final : public CanvasResource {
                           base::WeakPtr<WebGraphicsContext3DProviderWrapper>,
                           base::WeakPtr<CanvasResourceProvider>);
   SkAlphaType GetAlphaType() const { return alpha_type_; }
+  const gpu::SyncToken& sync_token() const override { return sync_token_; }
 
   const base::WeakPtr<WebGraphicsContext3DProviderWrapper>
       context_provider_wrapper_;
