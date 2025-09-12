32a5a233dd4b7ce0c1d5e705a2a9f11f0724ae63
Nate Chapin <japhet@chromium.org>
2025-09-12 12:22:48 -0700

    Make CHECK(keyboard_locked_) an early-exit in RenderWidgetHostViewAndroid::LockKeyboard
    Platform-independent content/ logic expects us to be resilient to
    LockKeyboard() being called while the keyboard is already locked.
    
    Fixed: 440603649
    Change-Id: I6481658a3069277a6911c817dda4c7d8496f47da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6944764
    Commit-Queue: Bo Liu <boliu@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Auto-Submit: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515027}

--- content/browser/renderer_host/render_widget_host_view_android.cc
+++ content/browser/renderer_host/render_widget_host_view_android.cc
@@ -2521,7 +2521,9 @@ void RenderWidgetHostViewAndroid::OnPointerLockRelease() {
 
 bool RenderWidgetHostViewAndroid::LockKeyboard(
     std::optional<base::flat_set<ui::DomCode>> codes) {
-  CHECK(!keyboard_locked_);
+  if (keyboard_locked_) {
+    return true;
+  }
 
   if (!base::FeatureList::IsEnabled(features::kKeyboardLockApiOnAndroid)) {
     return false;
