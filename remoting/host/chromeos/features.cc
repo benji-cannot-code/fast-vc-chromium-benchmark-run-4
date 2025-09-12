ef9b1185a44b01ccced1ec7ffe1b9ece7ed70482
Xiaohan Wang <xhwang@chromium.org>
2025-09-12 11:14:55 -0700

    remoting: Migrate BASE_FEATURE macros to 2-argument version
    This CL migrates three-argument BASE_FEATURE macros in //remoting to the
    new two-argument version where the C++ identifier and the string feature
    name match.
    
    This automated refactoring helps ensure that the C++ identifier and the
    string feature name always match, reducing potential errors.
    
    The following features could not be migrated automatically and may
    require manual intervention:
    
       * kAutoApproveEnterpriseSharedSessions vs "EnableAutoApproveEnterpriseSharedSessions"
           * Name has "Enable" prefix.
    
    Enabled-by-default-reason: refactor
    
    Bug: 436274260
    Change-Id: If1e09de35a68dcfde7cf7925b8afaf4308b89ce7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6939204
    Auto-Submit: Xiaohan Wang <xhwang@chromium.org>
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1514978}

--- remoting/host/chromeos/features.cc
+++ remoting/host/chromeos/features.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace remoting::features {
 
 BASE_FEATURE(kEnableCrdSharedSessionToUnattendedDevice,
-             "EnableCrdSharedSessionToUnattendedDevice",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kAutoApproveEnterpriseSharedSessions,
