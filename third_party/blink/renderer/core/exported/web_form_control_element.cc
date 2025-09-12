fbd9a79fc375481ab0e439eeab0e59f95b94ce1e
Sam Goto <goto@chromium.org>
2025-09-12 13:42:38 -0700

    [EVP] Add 'verified' event for form controls
    https://github.com/dickhardt/email-verification-protocol
    
    This change introduces a new 'verified' event that can be dispatched
    on HTMLFormControlElement. The event includes a 'code' attribute that
    can be used to pass a verification code to the element.
    
    This is intended to be used by features like autofill, where a
    verification code is received and needs to be communicated to the web
    page. The page can then listen for this event and handle the
    verification code accordingly.
    
    Bug: 380367784
    Change-Id: Ia76f5b40351cb683149e01e239204ad7e825abd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6902199
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Sam Goto <goto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515073}

--- third_party/blink/renderer/core/exported/web_form_control_element.cc
+++ third_party/blink/renderer/core/exported/web_form_control_element.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/input/focus_type.mojom-blink.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/events/event.h"
+#include "third_party/blink/renderer/core/events/email_verified_event.h"
 #include "third_party/blink/renderer/core/events/keyboard_event.h"
 #include "third_party/blink/renderer/core/html/forms/html_form_control_element.h"
 #include "third_party/blink/renderer/core/html/forms/html_form_control_element_with_state.h"
@@ -154,6 +155,18 @@ void WebFormControlElement::DispatchFocusEvent() {
 void WebFormControlElement::DispatchBlurEvent() {
   Unwrap<Element>()->DispatchBlurEvent(
       nullptr, mojom::blink::FocusType::kForward, nullptr);
+}
+
+void WebFormControlElement::DispatchEmailVerifiedEvent(
+    const WebString& presentation_token) {
+  if (IsNull()) {
+    return;
+  }
+
+  // Create and dispatch the event.
+  EmailVerifiedEvent* event = EmailVerifiedEvent::Create(
+      AtomicString("emailverified"), presentation_token);
+  Unwrap<HTMLFormControlElement>()->DispatchEvent(*event);
 }
 
 void WebFormControlElement::SetAutofillValue(const WebString& value,
