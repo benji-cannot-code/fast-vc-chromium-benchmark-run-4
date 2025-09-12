1bd9d7c02e83d36632f49cf069ae6d9bbf557980
Henrique Nakashima <hnakashima@chromium.org>
2025-09-12 12:21:19 -0700

    [Android] Add @NullMarked to .c.b.webapps files in chrome_java (part 7)
    Bug: 389129271
    Change-Id: Ie8773b4b3f4a996e93796d22f28684abf9f64d6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6941252
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515026}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/WebApkActivityLifecycleUmaTracker.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/WebApkActivityLifecycleUmaTracker.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.webapps;
 
+import static org.chromium.build.NullUtil.assumeNonNull;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,6 +16,7 @@ import org.chromium.base.ActivityState;
 import org.chromium.base.ApplicationStatus;
 import org.chromium.base.ApplicationStatus.ActivityStateListener;
 import org.chromium.base.library_loader.LibraryLoader;
+import org.chromium.build.annotations.NullMarked;
 import org.chromium.chrome.browser.browserservices.intents.BrowserServicesIntentDataProvider;
 import org.chromium.chrome.browser.browserservices.intents.WebApkExtras;
 import org.chromium.chrome.browser.browserservices.intents.WebappIntentUtils;
@@ -31,6 +34,7 @@ import org.chromium.chrome.browser.metrics.WebApkSplashscreenMetrics;
 import java.util.function.Supplier;
 
 /** Handles recording user metrics for WebAPK activities. */
+@NullMarked
 public class WebApkActivityLifecycleUmaTracker
         implements ActivityStateListener, InflationObserver, PauseResumeWithNativeObserver {
     private final Activity mActivity;
@@ -71,6 +75,7 @@ public class WebApkActivityLifecycleUmaTracker
                     }
 
                     WebApkExtras webApkExtras = mIntentDataProvider.getWebApkExtras();
+                    assumeNonNull(webApkExtras);
                     WebApkUmaRecorder.recordShellApkVersion(
                             webApkExtras.shellApkVersion, webApkExtras.distributor);
                 });
@@ -110,12 +115,13 @@ public class WebApkActivityLifecycleUmaTracker
     public void onPostInflationStartup() {}
 
     @Override
-    public void onResumeWithNative() {
-    }
+    public void onResumeWithNative() {}
 
     @Override
     public void onPauseWithNative() {
         WebApkExtras webApkExtras = mIntentDataProvider.getWebApkExtras();
+        assumeNonNull(webApkExtras);
+
         long sessionDuration = SystemClock.elapsedRealtime() - mStartTime;
         WebApkUmaRecorder.recordWebApkSessionDuration(webApkExtras.distributor, sessionDuration);
         WebApkUkmRecorder.recordWebApkSessionDuration(
