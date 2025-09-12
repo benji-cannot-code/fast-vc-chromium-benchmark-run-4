ce4d83658b26ed658233a1eb76c86dcdf29f3557
Colin Blundell <blundell@chromium.org>
2025-09-11 23:50:45 -0700

    [Blink] Hoist CRPSIImpl methods into CanvasResourceProviderSI (2 / N)
    Incrementally eliminating CanvasResourceProviderSIImpl.
    
    Bug: 352263194
    Change-Id: I678598ae5443a627f4a75b11261c0c430eb2cd5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6939124
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1514684}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
@@ -530,6 +530,7 @@ class PLATFORM_EXPORT CanvasResourceProviderSharedImage
 
   bool IsAccelerated() const final { return is_accelerated_; }
   bool SupportsDirectCompositing() const override { return true; }
+  bool UseOopRasterization() final { return use_oop_rasterization_; }
   bool unused_resources_reclaim_timer_is_running_for_testing() const override {
     return unused_resources_reclaim_timer_.IsRunning();
   }
@@ -537,7 +538,11 @@ class PLATFORM_EXPORT CanvasResourceProviderSharedImage
     return num_inflight_resources_;
   }
 
+  void NotifyTexParamsModified(const CanvasResource* resource) override;
+
  protected:
+  scoped_refptr<CanvasResourceSharedImage> CreateResource();
+
   // The maximum number of in-flight resources waiting to be used for
   // recycling.
   static constexpr int kMaxRecycledCanvasResources = 3;
@@ -580,6 +585,9 @@ class PLATFORM_EXPORT CanvasResourceProviderSharedImage
       cc::PaintImage::kInvalidContentId;
 
   bool notified_context_lost_ = false;
+
+ private:
+  virtual base::WeakPtr<CanvasResourceProviderSharedImage> CreateWeakPtr() = 0;
 };
 
 }  // namespace blink
