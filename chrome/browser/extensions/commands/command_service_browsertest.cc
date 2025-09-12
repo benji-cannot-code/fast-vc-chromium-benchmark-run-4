1a451af2a9977ee8fdf4440e856e0ed8e8ca78e1
Zhengzheng Liu <zhzhliu@chromium.org>
2025-09-12 11:48:33 -0700

    extensions :Port chrome.commands on desktop Android - part 1
    --Add assert check for commands API files which has been ported.
    
    --Port all browsertest for command service.
    
    --A follow up CL will be created to support tab param for
    chrome.commands.
    
    --CommandApiTest will be ported in a separate CL.
    
    Bug: 444361917
    Change-Id: Ie67fc0c0838f5d3b3e1b309a61a799e77c36a223
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6941336
    Commit-Queue: Zhengzheng Liu <zhzhliu@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515002}

--- chrome/browser/extensions/commands/command_service_browsertest.cc
+++ chrome/browser/extensions/commands/command_service_browsertest.cc
@@ -5,10 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/commands/command_service.h"
 
-#include <memory>
 #include <utility>
 
-#include "base/files/file_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/threading/thread_restrictions.h"
 #include "base/values.h"
@@ -19,8 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "components/prefs/scoped_user_pref_update.h"
 #include "content/public/test/browser_test.h"
-#include "content/public/test/test_utils.h"
 #include "extensions/browser/extension_registry.h"
+#include "extensions/buildflags/buildflags.h"
 #include "extensions/common/api/extension_action/action_info.h"
 #include "extensions/common/manifest_constants.h"
 #include "extensions/test/test_extension_dir.h"
@@ -29,17 +27,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/accelerators/command_constants.h"
 #include "ui/events/keycodes/keyboard_codes.h"
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
-#include "chrome/browser/ui/browser.h"
-#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace {
-#if BUILDFLAG(ENABLE_EXTENSIONS)
 const char kBasicBrowserActionKeybinding[] = "Ctrl+Shift+F";
 const char kBasicNamedKeybinding[] = "Ctrl+Shift+Y";
 const char kBasicAlternateKeybinding[] = "Ctrl+Shift+G";
 const char kBasicNamedCommand[] = "toggle-feature";
-#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 constexpr char kAltNKeybinding[] = "Alt+N";
 constexpr char kAltZKeybinding[] = "Alt+Z";
 constexpr char kExtensionId[] = "pgoakhfeplldmjheffidklpoklkppipp";
@@ -54,7 +48,6 @@ constexpr char kManifestTemplate[] = R"({
     },
     "%s": {}})";
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
 // Get another command platform, which is used for simulating a command has been
 // assigned with a shortcut on another platform.
 std::string GetAnotherCommandPlatform() {
@@ -70,7 +63,6 @@ std::string GetAnotherCommandPlatform() {
   return "";
 #endif
 }
-#endif
 
 // Parameters passed to CommandServiceMv3UpgradeTest parameterized tests.
 struct ManifestCommandTestParameters {
@@ -464,6 +456,7 @@ IN_PROC_BROWSER_TEST_F(CommandServiceTest, RemoveShortcutSurvivesUpdate) {
           .accelerator();
   EXPECT_EQ(ui::VKEY_UNKNOWN, accelerator.key_code());
 }
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
 IN_PROC_BROWSER_TEST_F(CommandServiceTest,
                        RemoveKeybindingPrefsShouldBePlatformSpecific) {
@@ -507,7 +500,7 @@ IN_PROC_BROWSER_TEST_F(CommandServiceTest,
     Command command;
     bool active = false;
     EXPECT_TRUE(command_service->GetExtensionActionCommand(
-        extension->id(), ActionInfo::Type::kBrowser, CommandService::ALL,
+        extension->id(), ActionInfo::Type::kAction, CommandService::ALL,
         &command, &active));
 
     EXPECT_EQ(kBasicBrowserActionKeybinding,
@@ -515,15 +508,15 @@ IN_PROC_BROWSER_TEST_F(CommandServiceTest,
     EXPECT_TRUE(active);
   }
 
-  command_service->UpdateKeybindingPrefs(
-      extension->id(), manifest_values::kBrowserActionCommandEvent,
-      kBasicAlternateKeybinding);
+  command_service->UpdateKeybindingPrefs(extension->id(),
+                                         manifest_values::kActionCommandEvent,
+                                         kBasicAlternateKeybinding);
 
   {
     Command command;
     bool active = false;
     EXPECT_TRUE(command_service->GetExtensionActionCommand(
-        extension->id(), ActionInfo::Type::kBrowser, CommandService::ALL,
+        extension->id(), ActionInfo::Type::kAction, CommandService::ALL,
         &command, &active));
 
     EXPECT_EQ(kBasicAlternateKeybinding,
@@ -531,14 +524,14 @@ IN_PROC_BROWSER_TEST_F(CommandServiceTest,
     EXPECT_TRUE(active);
   }
 
-  command_service->RemoveKeybindingPrefs(
-      extension->id(), manifest_values::kBrowserActionCommandEvent);
+  command_service->RemoveKeybindingPrefs(extension->id(),
+                                         manifest_values::kActionCommandEvent);
 
   {
     Command command;
     bool active = true;
     EXPECT_TRUE(command_service->GetExtensionActionCommand(
-        extension->id(), ActionInfo::Type::kBrowser, CommandService::ALL,
+        extension->id(), ActionInfo::Type::kAction, CommandService::ALL,
         &command, &active));
 
     EXPECT_EQ(kBasicBrowserActionKeybinding,
@@ -546,15 +539,15 @@ IN_PROC_BROWSER_TEST_F(CommandServiceTest,
     EXPECT_FALSE(active);
   }
 
-  command_service->UpdateKeybindingPrefs(
-      extension->id(), manifest_values::kBrowserActionCommandEvent,
-      /*keystroke=*/"");
+  command_service->UpdateKeybindingPrefs(extension->id(),
+                                         manifest_values::kActionCommandEvent,
+                                         /*keystroke=*/"");
 
   {
     Command command;
     bool active = true;
     EXPECT_TRUE(command_service->GetExtensionActionCommand(
-        extension->id(), ActionInfo::Type::kBrowser, CommandService::ALL,
+        extension->id(), ActionInfo::Type::kAction, CommandService::ALL,
         &command, &active));
 
     EXPECT_EQ(ui::VKEY_UNKNOWN, command.accelerator().key_code());
@@ -575,7 +568,7 @@ IN_PROC_BROWSER_TEST_F(CommandServiceTest,
     Command command;
     bool active = false;
     EXPECT_TRUE(command_service->GetExtensionActionCommand(
-        extension->id(), ActionInfo::Type::kBrowser, CommandService::ACTIVE,
+        extension->id(), ActionInfo::Type::kAction, CommandService::ACTIVE,
         &command, &active));
 
     EXPECT_EQ(kBasicBrowserActionKeybinding,
@@ -583,15 +576,15 @@ IN_PROC_BROWSER_TEST_F(CommandServiceTest,
     EXPECT_TRUE(active);
   }
 
-  command_service->UpdateKeybindingPrefs(
-      extension->id(), manifest_values::kBrowserActionCommandEvent,
-      kBasicAlternateKeybinding);
+  command_service->UpdateKeybindingPrefs(extension->id(),
+                                         manifest_values::kActionCommandEvent,
+                                         kBasicAlternateKeybinding);
 
   {
     Command command;
     bool active = false;
     EXPECT_TRUE(command_service->GetExtensionActionCommand(
-        extension->id(), ActionInfo::Type::kBrowser, CommandService::ACTIVE,
+        extension->id(), ActionInfo::Type::kAction, CommandService::ACTIVE,
         &command, &active));
 
     EXPECT_EQ(kBasicAlternateKeybinding,
@@ -599,14 +592,14 @@ IN_PROC_BROWSER_TEST_F(CommandServiceTest,
     EXPECT_TRUE(active);
   }
 
-  command_service->RemoveKeybindingPrefs(
-      extension->id(), manifest_values::kBrowserActionCommandEvent);
+  command_service->RemoveKeybindingPrefs(extension->id(),
+                                         manifest_values::kActionCommandEvent);
 
   {
     Command command;
     bool active = false;
     EXPECT_FALSE(command_service->GetExtensionActionCommand(
-        extension->id(), ActionInfo::Type::kBrowser, CommandService::ACTIVE,
+        extension->id(), ActionInfo::Type::kAction, CommandService::ACTIVE,
         &command, &active));
   }
 }
@@ -722,7 +715,6 @@ IN_PROC_BROWSER_TEST_F(CommandServiceTest, GetNamedCommandsQueryActive) {
     EXPECT_EQ(0u, command_map.count(kBasicNamedCommand));
   }
 }
-#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
 // Test both browser and page actions for these tests.
 INSTANTIATE_TEST_SUITE_P(BrowserAction,
