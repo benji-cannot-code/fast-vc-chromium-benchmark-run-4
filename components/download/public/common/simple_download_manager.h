340e58c0a06eb4476c0c1c86b8ee6fd79796e1e1
Chang Liu <changliu1@microsoft.com>
2025-09-13 00:46:55 -0700

    Fix crash in SimpleDownloadManager observer removal during widget destruction
    Make SimpleDownloadManager::Observer inherit from CheckedObserver and
    use checked ObserverList to prevent use-after-free crashes during
    destruction.
    
    The crash occurs when BrowserView::RemovedFromWidget() is called during
    widget teardown, which triggers SimpleDownloadManager observer cleanup.
    The observer list iterator crashes when trying to remove observers from
    a corrupted or partially destroyed observer list.
    
    base::CheckedObserver uses WeakPtr semantics to avoid use-after-free
    bugs. If an observer is destroyed without being removed from the list,
    the ObserverList will not call it during notification.
    
    Bug: 444481362
    Change-Id: I85201e7be9727a65de29f72bb789d8929cd26e24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6941576
    Commit-Queue: Chang Liu <changliu1@microsoft.com>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515203}

--- components/download/public/common/simple_download_manager.h
+++ components/download/public/common/simple_download_manager.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "base/observer_list.h"
+#include "base/observer_list_types.h"
 #include "components/download/public/common/download_export.h"
 #include "components/download/public/common/download_url_parameters.h"
 
@@ -23,14 +24,14 @@ class DownloadItem;
 // out a download task.
 class COMPONENTS_DOWNLOAD_EXPORT SimpleDownloadManager {
  public:
-  class Observer {
+  class Observer : public base::CheckedObserver {
    public:
     Observer() = default;
 
     Observer(const Observer&) = delete;
     Observer& operator=(const Observer&) = delete;
 
-    virtual ~Observer() = default;
+    ~Observer() override = default;
 
     virtual void OnDownloadsInitialized() {}
     virtual void OnManagerGoingDown() {}
@@ -85,7 +86,7 @@ class COMPONENTS_DOWNLOAD_EXPORT SimpleDownloadManager {
   bool initialized_ = false;
 
   // Observers that want to be notified of changes to the set of downloads.
-  base::ObserverList<Observer>::Unchecked simple_download_manager_observers_;
+  base::ObserverList<Observer> simple_download_manager_observers_;
 };
 
 }  // namespace download
