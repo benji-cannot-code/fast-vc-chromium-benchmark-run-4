2dbb1e2b424920521318590760f45c93272ec01f
AnnaSato <annasato@chromium.org>
2025-09-12 20:10:03 -0700

    Fix crash when reporting Web Bluetooth BFCache blocking
    This CL fixes a crash that occurs when reporting why a page using Web
    Bluetooth was blocked from BFCache.
    
    WebBluetooth was originally introduced as a non BFCache-blocking feature
    in https://crrev.com/c/6044286, and later changed to a blocking feature
    in https://crrev.com/c/6190377. When reporting the BFCache block reason
    kWebBluetooth, the code path would hit `NOTREACHED()`, causing a crash:
    https://crash.corp.google.com/browse?q=product_name%3D%22Chrome%22+AND+EXISTS+%28SELECT+1+FROM+UNNEST%28CrashedStackTrace.StackFrame%29%0A++WHERE+FunctionName+LIKE+%27%25MapBlocklistedFeatureToType%25%27%29#samplereports:20,productname:1000,productversion:100,magicsignature:10,magicsignature2:50,stablesignature:50,+clientid
    
    Bug: 443275589
    Change-Id: I02bd3142eaf17eb4252c5cc7041def06f6f9de80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6929174
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Anna Sato <annasato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515185}

--- content/browser/devtools/protocol/page_handler.cc
+++ content/browser/devtools/protocol/page_handler.cc
@@ -2002,7 +2002,6 @@ Page::BackForwardCacheNotRestoredReason BlocklistedFeatureToProtocol(
       return Page::BackForwardCacheNotRestoredReasonEnum::
           JsNetworkRequestReceivedCacheControlNoStoreResource;
     case WebSchedulerTrackedFeature::kWebSerial:
-    case WebSchedulerTrackedFeature::kWebBluetooth:
       // These features only disable aggressive throttling.
       NOTREACHED();
     case WebSchedulerTrackedFeature::kSmartCard:
@@ -2018,6 +2017,8 @@ Page::BackForwardCacheNotRestoredReason BlocklistedFeatureToProtocol(
           ContentWebAuthenticationAPI;
     case WebSchedulerTrackedFeature::kSharedWorkerMessage:
       return Page::BackForwardCacheNotRestoredReasonEnum::SharedWorkerMessage;
+    case WebSchedulerTrackedFeature::kWebBluetooth:
+      return Page::BackForwardCacheNotRestoredReasonEnum::WebBluetooth;
   }
 }
 
@@ -2243,6 +2244,7 @@ Page::BackForwardCacheNotRestoredReasonType MapBlocklistedFeatureToType(
     case WebSchedulerTrackedFeature::kWebSocket:
     case WebSchedulerTrackedFeature::kKeepaliveRequest:
     case WebSchedulerTrackedFeature::kWebAuthentication:
+    case WebSchedulerTrackedFeature::kWebBluetooth:
       return Page::BackForwardCacheNotRestoredReasonTypeEnum::SupportPending;
     case WebSchedulerTrackedFeature::kMainResourceHasCacheControlNoStore:
     case WebSchedulerTrackedFeature::kMainResourceHasCacheControlNoCache:
@@ -2259,7 +2261,6 @@ Page::BackForwardCacheNotRestoredReasonType MapBlocklistedFeatureToType(
     case WebSchedulerTrackedFeature::kWebSocketSticky:
       return Page::BackForwardCacheNotRestoredReasonTypeEnum::Circumstantial;
     case WebSchedulerTrackedFeature::kWebSerial:
-    case WebSchedulerTrackedFeature::kWebBluetooth:
       NOTREACHED();
   }
 }
