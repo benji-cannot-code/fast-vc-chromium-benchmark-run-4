55042bf8f8d9f34e0b22f07a4dd952735a47652e
Justin Cohen <justincohen@google.com>
2025-09-15 15:19:11 -0700

    ios: Annotate pre-existing DanglingUntriaged raw_ptr fields
    Second round, attempting to bring up downstream bots.
    
    Bug: 424376811
    Change-Id: Ie1a242e53202e1a7c22d85627e49eb2f702b1a95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6943602
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515737}

--- ios/chrome/browser/credential_provider/model/credential_provider_migrator.mm
+++ ios/chrome/browser/credential_provider/model/credential_provider_migrator.mm
@@ -30,7 +30,7 @@ static constexpr char kPasskeysIOSMigration[] = "Passkeys.IOSMigration";
 
 @interface CredentialProviderMigrator () {
   // Passkey store.
-  raw_ptr<webauthn::PasskeyModel> _passkeyStore;
+  raw_ptr<webauthn::PasskeyModel, DanglingUntriaged> _passkeyStore;
 }
 
 // Key used to retrieve the temporal storage.
