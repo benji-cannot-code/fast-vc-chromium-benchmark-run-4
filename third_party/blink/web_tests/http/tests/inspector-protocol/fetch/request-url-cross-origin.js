d79afcfc969cdc19b5b1f76281bae198cec2bd44
Andrey Kosyakov <caseq@chromium.org>
2025-09-15 13:20:03 -0700

    devtools interception: reset referrer if client updates url
    A check in URLRequest::StartJob() may result in request being blocked by
    client in case of a mismathcing referrer. It's easy to trigger this check by providing an override for the request URL, so reset the referrer if it would have triggered this check.
    
    We also reset site_for_cookies and isolation_info when the URL is updated to a cross-origin one.
    
    Change-Id: If7975188d554ea1461c30ec0d43554173595de12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6945075
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Peter Kvitek <kvitekp@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515688}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/fetch/request-url-cross-origin.js
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(/** @type {import('test_runner').TestRunner} */ testRunner) {
+  const {session, dp} = await testRunner.startBlank(`Tests updating request URL to a cross-origin one.`);
+
+  dp.Fetch.enable({});
+  await dp.Network.enable();
+  dp.Network.onLoadingFailed(e => {
+    testRunner.log(e.params, 'FAIL: ');
+  });
+
+  const path = 'inspector-protocol/resources/image.html';
+  const origin1 = 'http://domain1.test:8080/';
+  const origin2 = 'http://domain2.test:8080/';
+  testRunner.log('Initial navigation');
+  const navigationPromise = session.navigate(new URL(path, origin1).href);
+  dp.Fetch.onceRequestPaused(event => {
+    const {requestId, request} = event.params;
+    const url = new URL(new URL(request.url).pathname, origin2).href;
+    testRunner.log(request.url + ' => ' + url);
+    dp.Fetch.continueRequest({requestId, url});
+  });
+
+  await navigationPromise;
+  dp.Page.enable();
+  testRunner.log('Anchor navigation');
+  session.evaluate(() => {
+    const a = document.createElement('a');
+    a.href = 'http://domain1.test:8080/inspector-protocol/resource/empty.html';
+    a.textContent = 'Click Me!';
+    document.body.appendChild(a);
+    a.click();
+  });
+
+  await dp.Page.onceLoadEventFired();
+  testRunner.completeTest();
+})
