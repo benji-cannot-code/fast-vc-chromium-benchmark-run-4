4ffb550ca01e0238dd63156c7d8a8d98266b78e6
Shibalik Mohapatra <shibalik@chromium.org>
2025-09-15 11:40:08 -0700

    [Vertical Tabs] Add an initial vertical tab view.
    Also, this enables scroll with layers for both the pinned and unpinned containers.
    
    Bug: 442567140
    Change-Id: Ifb58fb4ffe82c8b3dee80a4d35ab98fe1bc1c5a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6937227
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Shibalik Mohapatra <shibalik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515641}

--- chrome/browser/ui/views/tabs/vertical/tab_collection_node.cc
+++ chrome/browser/ui/views/tabs/vertical/tab_collection_node.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/tab_strip_api/tab_strip_api_types.mojom.h"
 #include "chrome/browser/ui/views/tabs/vertical/vertical_pinned_tab_container_view.h"
 #include "chrome/browser/ui/views/tabs/vertical/vertical_tab_strip_view.h"
+#include "chrome/browser/ui/views/tabs/vertical/vertical_tab_view.h"
 #include "chrome/browser/ui/views/tabs/vertical/vertical_unpinned_tab_container_view.h"
 #include "ui/views/view.h"
 #include "vertical_unpinned_tab_container_view.h"
@@ -67,8 +68,7 @@ std::unique_ptr<views::View> TabCollectionNode::CreateViewForNode(
       // TODO(crbug.com/442567916): support tab groups.
       break;
     case Type::kTab:
-      // TODO(crbug.com/442567140): support tabs.
-      break;
+      return std::make_unique<VerticalTabView>(node_for_view);
   }
   return std::make_unique<CollectionTestViewImpl>(node_for_view);
 }
