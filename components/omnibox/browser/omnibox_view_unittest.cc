5f022f04634783081942aec90f2a3bb2068f1258
Lei Zhang <thestig@chromium.org>
2025-09-15 10:44:00 -0700

    Delete iOS-specific code in some Omnibox unit tests
    Since OmniboxEditModel and OmniboxView are not used on iOS, their unit
    tests do not need to test for iOS-specific behavior. Add static_assert()
    calls to make sure these files are not used on iOS. Then delete any
    other code that mentions iOS.
    
    With the above asserts in place, stop compiling these test files on iOS.
    
    Bug: 432423532
    Change-Id: I674b4ee33f5c60481ba8fb23d6d1627225335de0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6945343
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515601}

--- components/omnibox/browser/omnibox_view_unittest.cc
+++ components/omnibox/browser/omnibox_view_unittest.cc
@@ -38,10 +38,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/image/image_unittest_util.h"
 #include "ui/gfx/paint_vector_icon.h"
 
-#if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
+#if !BUILDFLAG(IS_ANDROID)
 #include "components/omnibox/browser/vector_icons.h"  // nogncheck
 #include "components/vector_icons/vector_icons.h"     // nogncheck
 #endif
+
+static_assert(!BUILDFLAG(IS_IOS));
 
 using base::ASCIIToUTF16;
 using testing::_;
@@ -114,7 +116,7 @@ class OmniboxViewPopupTest : public testing::Test {
 };
 }  // namespace
 
-#if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
+#if !BUILDFLAG(IS_ANDROID)
 // Tests GetIcon returns the default search icon when the match is a search
 // query.
 TEST_F(OmniboxViewTest, DISABLED_GetIcon_Default) {
@@ -267,7 +269,7 @@ TEST_F(OmniboxViewPopupTest, GetIcon_IconUrl) {
   gfx::test::CheckColors(bitmap.getColor(0, 0),
                          image.GetImage().ToSkBitmap()->getColor(0, 0));
 }
-#endif  // !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
+#endif  // !BUILDFLAG(IS_ANDROID)
 
 // Tests GetStateChanges correctly determines if text was deleted.
 TEST_F(OmniboxViewTest, GetStateChanges_DeletedText) {
