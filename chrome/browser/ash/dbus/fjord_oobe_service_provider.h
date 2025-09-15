901703e4e6211bf89e641453f0395bb3ae7edf4d
courtneywong <courtneywong@google.com>
2025-09-15 10:27:05 -0700

    fjord-oobe: Create dbus implementation for FjordOobeService
    This service implements the message ExitTcSetup, which uses the
    WizardController to advance from the TouchControllerSetupScreen.
    
    This is only for the Fjord variant of OOBE. In addition, if the
    WizardController does not exist or the touch controller setup screen is
    not showing, nothing happens.
    
    Bug:b:415052190
    Test:Tested that sending dbus message ExitTcSetup
    causes the OOBE screen to advance. Also tested cases in other parts of
    OOBE and outside of OOBE flow.
    
    Change-Id: If315c6450bc9707a9590fbe23a17ab923a011f7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6932939
    Reviewed-by: Osama Fathy <osamafathy@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Courtney Wong <courtneywong@chromium.org>
    Reviewed-by: Behnood Momenzadeh <behnoodm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515589}

---
+++ chrome/browser/ash/dbus/fjord_oobe_service_provider.h
@@ -0,0 +1,53 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_DBUS_FJORD_OOBE_SERVICE_PROVIDER_H_
+#define CHROME_BROWSER_ASH_DBUS_FJORD_OOBE_SERVICE_PROVIDER_H_
+
+#include <string>
+
+#include "base/memory/scoped_refptr.h"
+#include "base/memory/weak_ptr.h"
+#include "chromeos/ash/components/dbus/services/cros_dbus_service.h"
+#include "dbus/exported_object.h"
+
+namespace dbus {
+class MethodCall;
+}
+
+namespace ash {
+
+// FjordOobeServiceProvider implements the org.chromium.FjordOobeService D-Bus
+// interface. It is used by the Fjord variant of OOBE and allows for IPC to
+// control parts of OOBE that are specific to Fjord OOBE.
+class FjordOobeServiceProvider
+    : public CrosDBusService::ServiceProviderInterface {
+ public:
+  FjordOobeServiceProvider();
+
+  FjordOobeServiceProvider(const FjordOobeServiceProvider&) = delete;
+  FjordOobeServiceProvider& operator=(const FjordOobeServiceProvider&) = delete;
+
+  ~FjordOobeServiceProvider() override;
+
+  // CrosDBusService::ServiceProviderInterface overrides:
+  void Start(scoped_refptr<dbus::ExportedObject> exported_object) override;
+
+ private:
+  // Called from ExportedObject when a handler is exported as a D-Bus
+  // method or failed to be exported.
+  void OnExported(const std::string& interface_name,
+                  const std::string& method_name,
+                  bool success);
+
+  void ExitTouchControllerScreen(
+      dbus::MethodCall* method_call,
+      dbus::ExportedObject::ResponseSender response_sender);
+
+  base::WeakPtrFactory<FjordOobeServiceProvider> weak_ptr_factory_{this};
+};
+}  // namespace ash
+
+#endif  // CHROME_BROWSER_ASH_DBUS_FJORD_OOBE_SERVICE_PROVIDER_H_
