662c1cd1644650fcf82fdb5b2aeb4452e3e9a4d7
Colin Blundell <blundell@chromium.org>
2025-09-15 00:14:00 -0700

    [Blink] Have BlinkTransferableMessageStructTraitsTest use OOP-R
    These tests create CanvasResourceProviderSharedImage instances, which
    we're hardening to require OOP-R.
    
    Bug: 391648152
    Change-Id: I5fc07e081c358e94b0a873a521f51bb55a28bf31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6939760
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515359}

--- third_party/blink/renderer/core/messaging/blink_transferable_message_mojom_traits_test.cc
+++ third_party/blink/renderer/core/messaging/blink_transferable_message_mojom_traits_test.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/test/bind.h"
 #include "base/test/null_task_runner.h"
+#include "components/viz/test/test_raster_interface.h"
 #include "mojo/public/cpp/base/big_buffer_mojom_traits.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/messaging/message_port_channel.h"
@@ -227,14 +228,13 @@ TEST(BlinkTransferableMessageStructTraitsTest,
 class BlinkTransferableMessageStructTraitsWithFakeGpuTest : public Test {
  public:
   void SetUp() override {
-    auto sii = base::MakeRefCounted<gpu::TestSharedImageInterface>();
-    sii_ = sii.get();
-    context_provider_ = viz::TestContextProvider::Create(std::move(sii));
-    InitializeSharedGpuContextGLES2(context_provider_.get());
+    context_provider_ = viz::TestContextProvider::CreateRaster();
+    context_provider_->UnboundTestRasterInterface()->set_gpu_rasterization(
+        true);
+    InitializeSharedGpuContextRaster(context_provider_.get());
   }
 
   void TearDown() override {
-    sii_ = nullptr;
     SharedGpuContext::Reset();
   }
 
@@ -265,7 +265,6 @@ class BlinkTransferableMessageStructTraitsWithFakeGpuTest : public Test {
   }
 
  protected:
-  gpu::TestSharedImageInterface* sii_;
   scoped_refptr<viz::TestContextProvider> context_provider_;
 
   bool image_destroyed_ = false;
