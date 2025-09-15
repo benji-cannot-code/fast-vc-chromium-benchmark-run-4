df15ff9a0c88a1c6f4fc5f3b964ac562722c85c7
Fabio Muratori <muratori@google.com>
2025-09-15 13:40:31 -0700

    [Clank Nav Cap] Update PWA related CUJs for HTTP/S schemes navigations only
    Recent development reworked some navigation capturing CUJs to better
    align desktop windowing Clank to desktop Chrome.
    
    Some of these CUJs are being rolled out on non-desktop windowing mode
    Clank to also align it across windowing modes where possible.
    
    The scope of the CUJ changes was aimed at better integrating/aligning
    navigation flows around PWAs.
    
    Experiment is currently in beta channel and we noticed an alarming rate
    of changes in how many navigation are affected for metrics:
    - Android.Intent.IntentLaunchFromTabCreation;
    - Android.Intent.MainFrameIntentLaunch;
    - Android: Android.TabNavigationInterceptResult.*.
    UMA dashboard at [1].
    
    It makes sense to only affect CUJs for HTTP/s URL scheme based
    navigations.
    
    [1]
    https://uma.googleplex.com/p/chrome/variations?sid=a99daa183bbf72ff8862dbdb733dd8d6
    
    Bug: 443665869
    Change-Id: I155b5a010829f7bf76817c6930c9d5a84fa80d4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6917111
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Fabio Muratori <muratori@google.com>
    Cr-Commit-Position: refs/heads/main@{#1515698}

--- chrome/android/java/src/org/chromium/chrome/browser/app/tab_activity_glue/ActivityTabWebContentsDelegateAndroid.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/tab_activity_glue/ActivityTabWebContentsDelegateAndroid.java
@@ -294,7 +294,7 @@ public class ActivityTabWebContentsDelegateAndroid extends TabWebContentsDelegat
                 navigationTabHelper != null
                         && navigationTabHelper
                                 .getInterceptNavigationDelegate()
-                                .shouldReparentTab(webContents);
+                                .shouldReparentTab(webContents, url);
 
         Tab tab =
                 tabCreator.createTabWithWebContents(
