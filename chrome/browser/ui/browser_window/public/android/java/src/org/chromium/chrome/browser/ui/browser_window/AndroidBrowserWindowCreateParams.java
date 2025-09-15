70f1edb113e7f9f82dc89ef76ab9fe6f248bfa6e
Aishwarya Rajesh <aishwaryarj@google.com>
2025-09-15 14:42:13 -0700

    [EWBC] Create AndroidBrowserWindowCreateParams
    Bug: 444525241
    Change-Id: I9540bf6f21fdcb3de85501f7524cff3f549d6d02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6941349
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Reviewed-by: Linyu He <linyuh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1515721}

---
+++ chrome/browser/ui/browser_window/public/android/java/src/org/chromium/chrome/browser/ui/browser_window/AndroidBrowserWindowCreateParams.java
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.ui.browser_window;
+
+import android.graphics.Rect;
+
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.ui.mojom.WindowShowState;
+
+/** Interface defining parameters for creating a browser window on Android. */
+@NullMarked
+interface AndroidBrowserWindowCreateParams {
+    /** Returns the browser window type. */
+    @BrowserWindowType
+    int getWindowType();
+
+    /** Returns the profile associated with this window. */
+    Profile getProfile();
+
+    /** Returns the initial bounds of the window. */
+    Rect getInitialBounds();
+
+    /** Returns the initial show state of the window. */
+    @WindowShowState.EnumType
+    int getInitialShowState();
+}
