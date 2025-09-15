8ff13d005f11ea816f6c86a4992ddaed1c2c76dd
Steinar H. Gunderson <sesse@chromium.org>
2025-09-15 05:11:01 -0700

    Simplify StyleScope with implicit roots.
    We don't actually need to store the StyleSheetContents anymore;
    we can just test on from_ being nullptr or not.
    
    Change-Id: Ie8aea60815b0a400972649e8dad1719ee4ed6a21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6947767
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515429}

--- third_party/blink/renderer/core/css/style_scope.cc
+++ third_party/blink/renderer/core/css/style_scope.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "third_party/blink/renderer/core/css/style_scope.h"
+
 #include "third_party/blink/renderer/core/css/parser/css_selector_parser.h"
 #include "third_party/blink/renderer/core/css/properties/css_parsing_utils.h"
 #include "third_party/blink/renderer/core/css/style_rule.h"
@@ -16,12 +17,8 @@ namespace blink {
 StyleScope::StyleScope(StyleRule* from, CSSSelectorList* to)
     : from_(from), to_(to) {}
 
-StyleScope::StyleScope(StyleSheetContents* contents, CSSSelectorList* to)
-    : contents_(contents), to_(to) {}
-
 StyleScope::StyleScope(const StyleScope& other)
-    : contents_(other.contents_),
-      from_(other.from_ ? other.from_->Copy() : nullptr),
+    : from_(other.from_ ? other.from_->Copy() : nullptr),
       to_(other.to_ ? other.to_->Copy() : nullptr),
       parent_(other.parent_) {}
 
@@ -40,8 +37,6 @@ const StyleScope* StyleScope::Renest(StyleRule* new_parent) const {
   // Note that for the "to" selector, any '&' selectors must point
   // to the "from" selector.
   CSSSelectorList* reparented_to = to_ ? to_->Renest(reparented_from) : nullptr;
-  // For implicit scopes, we should have exited early due to from_==nullptr.
-  CHECK(!contents_);
   // The `parent_` member should only be populated via calls to CopyWithParent
   // (RuleSet-time), and this StyleScope should not be one such copy.
   CHECK(!parent_);
@@ -122,14 +117,13 @@ StyleScope* StyleScope::Parse(CSSParserTokenStream& stream,
 
   if (from.empty()) {
     // Implicitly rooted.
-    return MakeGarbageCollected<StyleScope>(style_sheet, to_list);
+    return MakeGarbageCollected<StyleScope>(/*from=*/nullptr, to_list);
   }
 
   return MakeGarbageCollected<StyleScope>(from_rule, to_list);
 }
 
 void StyleScope::Trace(blink::Visitor* visitor) const {
-  visitor->Trace(contents_);
   visitor->Trace(from_);
   visitor->Trace(to_);
   visitor->Trace(parent_);
