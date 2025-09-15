1e4dc250b1bfbff2266ec3815da83280d13bc536
Rohit Rao <rohitrao@chromium.org>
2025-09-15 08:45:11 -0700

    Strips comments from the bookmark_parser.js file before bundling in the app.
    These javascript comments have no function in the shipping binary and
    can safely be stripped out.
    
    Bug: None
    Change-Id: I93bcdee1f21711996bec95c7c026e594a5a39daf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6944264
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515513}

---
+++ components/user_data_importer/ios/strip_js_comments.py
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2025 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Strip comments from a JS file."""
+
+import argparse
+import os
+import shutil
+import subprocess
+import sys
+
+
+def parse_args(args):
+  """Parses the command-line."""
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--input', required=True, help='Path to input file')
+  parser.add_argument('--output', required=True, help='Path to output file')
+  return parser.parse_args(args)
+
+
+def main(args):
+  parsed = parse_args(args)
+
+  with open(parsed.input, 'r') as f:
+    with open(parsed.output, 'w') as g:
+      for line in f:
+        if not line.startswith('//'):
+          g.write(line)
+
+
+if __name__ == '__main__':
+  main(sys.argv[1:])
