412604732f9c73c26c92f26dd95f2daf4e3278b6
Wenyu Fu <wenyufu@chromium.org>
2025-09-15 13:05:10 -0700

    [E2E] Enable EdgeToEdgeEverywhere by default
    Bug: 369841366
    Enabled-by-default-reason: Launching
    Change-Id: I4acf895f9f3dcaedaa44ad864553f9fdca3b9e6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6942868
    Reviewed-by: Charles Hager <clhager@google.com>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515682}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -845,7 +845,7 @@ BASE_FEATURE(kEdgeToEdgeBottomChin, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kEdgeToEdgeMonitorConfigurations,
              base::FEATURE_ENABLED_BY_DEFAULT);
 
-BASE_FEATURE(kEdgeToEdgeEverywhere, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kEdgeToEdgeEverywhere, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kEdgeToEdgeTablet, base::FEATURE_DISABLED_BY_DEFAULT);
 
