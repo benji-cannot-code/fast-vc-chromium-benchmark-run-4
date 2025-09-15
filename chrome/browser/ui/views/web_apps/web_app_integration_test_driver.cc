a4ff5f41d6bb99d26e28253afb3e677f9af438b4
Dibyajyoti Pal <dibyapal@google.com>
2025-09-15 14:56:36 -0700

    [PWA/Trusted Icons] Launch architecture enabled by default
    This CL fixes all existing tests that were failing because of the
    trusted icons architecture being enabled, and launches it in
    Chromium.
    
    web apps system as a bug fix.
    
    Enabled-by-default-reason: Launching trusted icons architecture in
    Fixed: 427566193
    Change-Id: I2b6f81625bb7cf623d96137405d6b91b2f62d2df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6916260
    Reviewed-by: May Siem <msiem@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515725}

--- chrome/browser/ui/views/web_apps/web_app_integration_test_driver.cc
+++ chrome/browser/ui/views/web_apps/web_app_integration_test_driver.cc
@@ -160,6 +160,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/manifest/display_mode.mojom-shared.h"
 #include "third_party/boringssl/src/include/openssl/curve25519.h"
 #include "third_party/re2/src/re2/re2.h"
+#include "third_party/skia/include/core/SkColor.h"
 #include "ui/accessibility/ax_action_data.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/controls/button/image_button.h"
@@ -2278,11 +2279,8 @@ void WebAppIntegrationTestDriver::ManifestUpdateIcon(
           views::test::AnyWidgetTestPasskey{},
           "WebAppIdentityUpdateConfirmationView");
 
-  // The kLauncherIcon size is used here, as it is guaranteed to be written to
-  // the shortcut on all platforms, as opposed to kInstallIconSize, for example,
-  // which, on ChromeOS, is not written to the shortcut because it is not within
-  // the intersection between `kDesiredIconSizesForShortcut` (which is platform-
-  // dependent) and `SizesToGenerate()` (which is fixed on all platforms).
+  // After launching the trusted icon architecture, the icon of largest size of
+  // purpose any is going to be preferred.
   GURL url = GetUrlForSite(
       site, base::StringPrintf("?manifest=manifest_icon_red_%u.json",
                                kLauncherIconSize));
@@ -4779,6 +4777,7 @@ WebAppIntegrationTest::WebAppIntegrationTest() : helper_(this) {
   enabled_features.push_back(features::kIsolatedWebApps);
   enabled_features.push_back(features::kPwaUpdateDialogForIcon);
   enabled_features.push_back(features::kRecordWebAppDebugInfo);
+  enabled_features.push_back(features::kWebAppUsePrimaryIcon);
 #if !BUILDFLAG(IS_CHROMEOS)
   // TODO(b/313492499): Update test driver to work with new intent picker UI.
   enabled_features.push_back(features::kPwaNavigationCapturing);
--- chrome/browser/web_applications/commands/install_app_from_verified_manifest_command_browsertest.cc
+++ chrome/browser/web_applications/commands/install_app_from_verified_manifest_command_browsertest.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_install_params.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
 #include "chrome/browser/web_applications/web_app_registrar.h"
+#include "chrome/common/chrome_features.h"
 #include "components/webapps/browser/install_result_code.h"
 #include "components/webapps/browser/installable/installable_metrics.h"
 #include "components/webapps/common/web_app_id.h"
@@ -287,9 +288,15 @@ IN_PROC_BROWSER_TEST_F(InstallAppFromVerifiedManifestCommandTest,
 
   EXPECT_TRUE(webapps::IsSuccess(result_code));
 
+  // Post trusted icons launch, all icons use the same color (chosen from the
+  // one of the largest size).
+  SkColor expected_color =
+      base::FeatureList::IsEnabled(features::kWebAppUsePrimaryIcon)
+          ? SK_ColorGREEN
+          : SK_ColorRED;
   SkColor small_icon_color =
       IconManagerReadAppIconPixel(provider().icon_manager(), result_id, 96);
-  EXPECT_EQ(small_icon_color, SK_ColorRED);
+  EXPECT_EQ(small_icon_color, expected_color);
 
   SkColor large_icon_color =
       IconManagerReadAppIconPixel(provider().icon_manager(), result_id, 192);
@@ -520,9 +527,15 @@ IN_PROC_BROWSER_TEST_F(InstallAppFromVerifiedManifestCommandTest,
 
   EXPECT_TRUE(webapps::IsSuccess(result_code));
 
+  // Post trusted icons launch, all icons use the same color (chosen from the
+  // one of the largest size).
+  SkColor expected_color =
+      base::FeatureList::IsEnabled(features::kWebAppUsePrimaryIcon)
+          ? SK_ColorGREEN
+          : SK_ColorRED;
   SkColor small_icon_color =
       IconManagerReadAppIconPixel(provider().icon_manager(), result_id, 96);
-  EXPECT_EQ(small_icon_color, SK_ColorRED);
+  EXPECT_EQ(small_icon_color, expected_color);
 
   SkColor large_icon_color =
       IconManagerReadAppIconPixel(provider().icon_manager(), result_id, 192);
