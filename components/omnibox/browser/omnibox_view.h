c37f91a3f03a7ae641f0577d8cc5335f99e8ff26
Lei Zhang <thestig@chromium.org>
2025-09-15 10:42:50 -0700

    Tidy OmniboxView
    - Change GetState() to return the state instead of returning via an
      out-parameter while having a void return type.
    - Just don't define OmniboxView::GetIcon() on mobile platforms, instead
      of defining it and marking it as NOTREACHED().
    - Make OmniboxView::GetStateChanges() static, since it does not interact
      with any OmniboxView instance.
    - Format the code.
    - Change an enum so its values are named kFoo.
    - Remove a bunch of unused includes.
    
    Change-Id: I4ed744dc5c9885856fc3f1d2e51990439a5deb75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6944144
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515599}

--- components/omnibox/browser/omnibox_view.h
+++ components/omnibox/browser/omnibox_view.h
@@ -17,9 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
-#include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
-#include "base/strings/utf_string_conversions.h"
+#include "build/build_config.h"
 #include "components/omnibox/browser/autocomplete_match.h"
 #include "components/omnibox/browser/omnibox_client.h"
 #include "third_party/skia/include/core/SkColor.h"
@@ -52,9 +51,9 @@ class OmniboxView {
     bool just_deleted_text;
   };
 
-  virtual ~OmniboxView();
   OmniboxView(const OmniboxView&) = delete;
   OmniboxView& operator=(const OmniboxView&) = delete;
+  virtual ~OmniboxView();
 
   OmniboxEditModel* model();
   const OmniboxEditModel* model() const;
@@ -74,6 +73,7 @@ class OmniboxView {
   // the field is empty.
   bool IsEditingOrEmpty() const;
 
+#if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
   // Returns the icon to display as the location icon. If a favicon is
   // available, `on_icon_fetched` may be called later asynchronously.
   // `color_current_page_icon` is used for the page icon (i.e. when the popup is
@@ -94,6 +94,7 @@ class OmniboxView {
                          SkColor color_vectors_with_background,
                          IconFetchedCallback on_icon_fetched,
                          bool dark_mode) const;
+#endif
 
   // The user text is the text the user has manually keyed in.  When present,
   // this is shown in preference to the permanent text; hitting escape will
@@ -253,11 +254,11 @@ class OmniboxView {
 
   explicit OmniboxView(std::unique_ptr<OmniboxClient> client);
 
-  // Fills |state| with the current text state.
-  void GetState(State* state);
+  // Returns the current text state.
+  State GetState() const;
 
   // Returns the delta between |before| and |after|.
-  StateChanges GetStateChanges(const State& before, const State& after);
+  static StateChanges GetStateChanges(const State& before, const State& after);
 
   // Internally invoked whenever the text changes in some way.
   virtual void TextChanged();
@@ -293,7 +294,7 @@ class OmniboxView {
   friend class OmniboxViewMacTest;
   friend class TestOmniboxView;
 
-  std::unique_ptr<OmniboxController> controller_;
+  const std::unique_ptr<OmniboxController> controller_;
 };
 
 #endif  // COMPONENTS_OMNIBOX_BROWSER_OMNIBOX_VIEW_H_
--- components/omnibox/browser/omnibox_view_unittest.cc
+++ components/omnibox/browser/omnibox_view_unittest.cc
@@ -276,7 +276,8 @@ TEST_F(OmniboxViewTest, GetStateChanges_DeletedText) {
     auto state_before =
         TestOmniboxView::CreateState("google.com", 10, 3);  // goo[gle.com]
     auto state_after = TestOmniboxView::CreateState("goog", 4, 4);  // goog|
-    auto state_changes = view()->GetStateChanges(state_before, state_after);
+    auto state_changes =
+        TestOmniboxView::GetStateChanges(state_before, state_after);
     EXPECT_FALSE(state_changes.just_deleted_text);
   }
   {
@@ -284,7 +285,8 @@ TEST_F(OmniboxViewTest, GetStateChanges_DeletedText) {
     auto state_before =
         TestOmniboxView::CreateState("google.com", 1, 10);  // g[oogle.com]
     auto state_after = TestOmniboxView::CreateState("gi", 2, 2);  // gi|
-    auto state_changes = view()->GetStateChanges(state_before, state_after);
+    auto state_changes =
+        TestOmniboxView::GetStateChanges(state_before, state_after);
     EXPECT_FALSE(state_changes.just_deleted_text);
   }
   {
@@ -292,7 +294,8 @@ TEST_F(OmniboxViewTest, GetStateChanges_DeletedText) {
     auto state_before =
         TestOmniboxView::CreateState("google.com", 1, 10);       // g[oogle.com]
     auto state_after = TestOmniboxView::CreateState("g", 1, 1);  // g|
-    auto state_changes = view()->GetStateChanges(state_before, state_after);
+    auto state_changes =
+        TestOmniboxView::GetStateChanges(state_before, state_after);
     EXPECT_TRUE(state_changes.just_deleted_text);
   }
   {
@@ -301,7 +304,8 @@ TEST_F(OmniboxViewTest, GetStateChanges_DeletedText) {
         TestOmniboxView::CreateState("goole.com", 3, 3);  // goo|le.com
     auto state_after =
         TestOmniboxView::CreateState("google.com", 4, 4);  // goog|le.com
-    auto state_changes = view()->GetStateChanges(state_before, state_after);
+    auto state_changes =
+        TestOmniboxView::GetStateChanges(state_before, state_after);
     EXPECT_FALSE(state_changes.just_deleted_text);
   }
   {
@@ -310,7 +314,8 @@ TEST_F(OmniboxViewTest, GetStateChanges_DeletedText) {
         TestOmniboxView::CreateState("googgle.com", 5, 5);  // googg|le.com
     auto state_after =
         TestOmniboxView::CreateState("google.com", 4, 4);  // goog|le.com
-    auto state_changes = view()->GetStateChanges(state_before, state_after);
+    auto state_changes =
+        TestOmniboxView::GetStateChanges(state_before, state_after);
     EXPECT_TRUE(state_changes.just_deleted_text);
   }
   {
@@ -319,7 +324,8 @@ TEST_F(OmniboxViewTest, GetStateChanges_DeletedText) {
         TestOmniboxView::CreateState("goojle.com", 3, 4);  // goo[j]le.com
     auto state_after =
         TestOmniboxView::CreateState("google.com", 4, 4);  // goog|le.com
-    auto state_changes = view()->GetStateChanges(state_before, state_after);
+    auto state_changes =
+        TestOmniboxView::GetStateChanges(state_before, state_after);
     EXPECT_FALSE(state_changes.just_deleted_text);
   }
 }
