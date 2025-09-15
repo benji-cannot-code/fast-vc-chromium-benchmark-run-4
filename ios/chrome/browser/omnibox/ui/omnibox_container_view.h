ef894c1e41cdcfc96b2a9e4c8ae7046490a863bd
Christian Xu <christianxu@google.com>
2025-09-15 02:42:04 -0700

    [iOS][Omnibox] Use OmniboxTextInput in OmniboxContainerView
    Use OmniboxTextInput protocol instead of OmniboxTextFieldIOS in
    preparation for multiline UITextView.
    
    Bug: None
    Change-Id: I463955a36b23860c2187f52a6bfb74538e28416e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6939698
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515395}

--- ios/chrome/browser/omnibox/ui/omnibox_container_view.h
+++ ios/chrome/browser/omnibox/ui/omnibox_container_view.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/omnibox/ui/text_field_view_containing.h"
 
 @class LayoutGuideCenter;
-@class OmniboxTextFieldIOS;
+@protocol OmniboxTextInput;
 
 /// The omnibox container view is the view that is shown in the location bar's
 /// edit state. It contains the omnibox textfield and the buttons on the left
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @interface OmniboxContainerView : UIView <TextFieldViewContaining>
 
 /// The contained omnibox textfield.
-@property(nonatomic, strong, readonly) OmniboxTextFieldIOS* textField;
+@property(nonatomic, strong, readonly) id<OmniboxTextInput> textInput;
 
 /// The contained clear button. Hide with `setClearButtonHidden`.
 @property(nonatomic, strong, readonly) UIButton* clearButton;
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /// color for omnibox.
 - (instancetype)initWithFrame:(CGRect)frame
                     textColor:(UIColor*)textColor
-                textFieldTint:(UIColor*)textFieldTint
+                textInputTint:(UIColor*)textInputTint
                      iconTint:(UIColor*)iconTint
                 isLensOverlay:(BOOL)isLensOverlay NS_DESIGNATED_INITIALIZER;
 
