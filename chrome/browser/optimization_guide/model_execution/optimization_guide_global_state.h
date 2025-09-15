8176fcffd4bbecd446706d8ee8651437be7f9f5a
rajendrant <rajendrant@chromium.org>
2025-09-15 11:25:49 -0700

    Integrate `ChromeProfileDownloadServiceTracker` into `OptimizationGuideGlobalState`.
    This change adds `ChromeProfileDownloadServiceTracker` to the global state on both Chrome and iOS. It also refines profile filtering in the tracker on Chrome to exclude system profiles and those not allowing browser windows, and improves robustness in the iOS implementation by adding null checks and resetting observations.
    
    Change-Id: I9652d9510f215c80c397a7a52c63a697820f5789
    Bug: 423972751
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6942916
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Raj T <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515630}

--- chrome/browser/optimization_guide/model_execution/optimization_guide_global_state.h
+++ chrome/browser/optimization_guide/model_execution/optimization_guide_global_state.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
+#include "chrome/browser/optimization_guide/prediction/chrome_profile_download_service_tracker.h"
 #include "components/optimization_guide/core/delivery/optimization_guide_model_provider.h"
 #include "components/optimization_guide/core/delivery/prediction_model_store.h"
 #include "components/optimization_guide/core/model_execution/model_broker_state.h"
@@ -69,6 +70,8 @@ class OptimizationGuideGlobalState final
   ModelBrokerState model_broker_state_;
 
   PredictionModelStore prediction_model_store_;
+
+  ChromeProfileDownloadServiceTracker profile_download_service_tracker_;
 
   std::unique_ptr<ChromeModelComponentStateManagerObserver>
       component_state_manager_observer_;
