86e3c0ebf95ee2f3dbe9aa738409abf315deb8b5
Robbie Gibson <rkgibson@google.com>
2025-09-15 08:15:06 -0700

    [iOS] Close customization menu when tapping on incognito NTP button
    Fixed: 433906729
    Change-Id: I7bd232e0b42e6165d7a0bf5b2a60db6853c1c2e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6945125
    Auto-Submit: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1515495}

--- ios/chrome/browser/ntp/ui_bundled/new_tab_page_coordinator.mm
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_coordinator.mm
@@ -1995,6 +1995,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)openIncognitoSearch {
   [self.NTPMetricsRecorder recordIncognitoTapped];
+  [self dismissCustomizationMenu];
+
   OpenNewTabCommand* command = [OpenNewTabCommand commandWithIncognito:YES];
   command.shouldFocusOmnibox = YES;
   id<ApplicationCommands> applicationHandler = HandlerForProtocol(
