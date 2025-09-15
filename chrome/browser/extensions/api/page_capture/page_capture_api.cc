91b9cd52b4e76bc65baa6de9f91dfadd973f3cd8
Zhengzheng Liu <zhzhliu@chromium.org>
2025-09-15 11:12:33 -0700

    extensions: Add static assert for search and pageCapture API
    This CL adds Add static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE)) to
    files under chrome/browser/extensions/api/search and
    chrome/browser/extensions/api/page_capture/ which have already been
    ported.
    
    Bug: 443984428
    Change-Id: I602f545f34374ad8eb0804b5d0c465b9d9488244
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6946714
    Commit-Queue: Zhengzheng Liu <zhzhliu@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515623}

--- chrome/browser/extensions/api/page_capture/page_capture_api.cc
+++ chrome/browser/extensions/api/page_capture/page_capture_api.cc
@@ -22,8 +22,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/mhtml_generation_params.h"
 #include "extensions/browser/extension_api_frame_id_map.h"
 #include "extensions/browser/extension_util.h"
+#include "extensions/buildflags/buildflags.h"
 #include "extensions/common/permissions/permissions_data.h"
 #include "url/origin.h"
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 using content::BrowserThread;
 using content::ChildProcessSecurityPolicy;
