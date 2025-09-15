9a931f83bc45d31fd957dcce8ca60934aa244e07
Piotr Kotynia <piotrkotynia@google.com>
2025-09-15 07:44:35 -0700

    [Android] Delete unused popup logic
    Bug: 40100455
    Change-Id: I39e60522d4449bc80e0b6dc323837d62b4cb81b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6943098
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Commit-Queue: Piotr Kotynia <piotrkotynia@google.com>
    Cr-Commit-Position: refs/heads/main@{#1515472}

--- chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/ManualFillingCoordinator.java
+++ chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/ManualFillingCoordinator.java
@@ -28,7 +28,6 @@ import org.chromium.components.autofill.AutofillSuggestion;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.ui.AsyncViewStub;
-import org.chromium.ui.DropdownPopupWindow;
 import org.chromium.ui.base.WindowAndroid;
 import org.chromium.ui.insets.InsetObserver;
 
@@ -149,11 +148,6 @@ class ManualFillingCoordinator implements ManualFillingComponent {
     @Override
     public void dismiss() {
         mMediator.dismiss();
-    }
-
-    @Override
-    public void notifyPopupAvailable(DropdownPopupWindow popup) {
-        mMediator.notifyPopupOpened(popup);
     }
 
     @Override
--- chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/ManualFillingMediator.java
+++ chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/ManualFillingMediator.java
@@ -74,7 +74,6 @@ import org.chromium.components.browser_ui.widget.gesture.BackPressHandler;
 import org.chromium.content_public.browser.NavigationHandle;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.content_public.browser.WebContentsAccessibility;
-import org.chromium.ui.DropdownPopupWindow;
 import org.chromium.ui.base.ApplicationViewportInsetSupplier;
 import org.chromium.ui.base.DeviceInput;
 import org.chromium.ui.base.ViewUtils;
@@ -119,7 +118,6 @@ class ManualFillingMediator
     private AccessorySheetCoordinator mAccessorySheet;
     private ChromeActivity mActivity; // Used to control the keyboard.
     private TabModelSelectorTabModelObserver mTabModelObserver;
-    private DropdownPopupWindow mPopup;
     private BottomSheetController mBottomSheetController;
     private ManualFillingComponent.SoftKeyboardDelegate mSoftKeyboardDelegate;
     private ConfirmationDialogHelper mConfirmationHelper;
@@ -398,10 +396,6 @@ class ManualFillingMediator
         if (!isInitialized()) return;
         pause();
         hideSoftKeyboard();
-    }
-
-    void notifyPopupOpened(DropdownPopupWindow popup) {
-        mPopup = popup;
     }
 
     void show(boolean waitForKeyboard, boolean isCredentialFieldOrHasAutofillSuggestions) {
@@ -711,7 +705,6 @@ class ManualFillingMediator
     public void onChangeAccessorySheet(int tabIndex) {
         if (!isInitialized()) return;
         mAccessorySheet.setActiveTab(tabIndex);
-        if (mPopup != null && mPopup.isShowing()) mPopup.dismiss();
         if (is(EXTENDING_KEYBOARD)) {
             mModel.set(KEYBOARD_EXTENSION_STATE, REPLACING_KEYBOARD);
         } else if (is(FLOATING_BAR)) {
