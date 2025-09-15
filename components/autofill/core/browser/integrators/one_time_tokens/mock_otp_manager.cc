7f3e8e748789e62b5dedbb7ddfa230bf32ba2ae3
Dominic Battre <battre@chromium.org>
2025-09-15 09:26:58 -0700

    Implement OtpManager in autofill.
    This CL implements an OtpManager in components/autofill that will
    replace the copy in.
    components/password_manager/core/browser/one_time_passwords/
    
    The OtpManager in Autofill is much simpler because it does not need to
    track form structures like the old version. It can use
    AutofillManager::Observer::OnFieldTypesDetermined() to learn when OTPs
    need to be fetched and integrate into suggestion generation easily
    because the fully classified FormStructure is available in Autofill.
    
    Bug: 415273270
    Change-Id: I6bb9be9fc46078f289eaf5d48587854573b9702a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6943295
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1515550}

---
+++ components/autofill/core/browser/integrators/one_time_tokens/mock_otp_manager.cc
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/autofill/core/browser/integrators/one_time_tokens/mock_otp_manager.h"
+
+namespace autofill {
+
+MockOtpManager::MockOtpManager() = default;
+MockOtpManager::~MockOtpManager() = default;
+
+}  // namespace autofill
---
+++ components/autofill/core/browser/integrators/one_time_tokens/mock_otp_manager.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_AUTOFILL_CORE_BROWSER_INTEGRATORS_ONE_TIME_TOKENS_MOCK_OTP_MANAGER_H_
+#define COMPONENTS_AUTOFILL_CORE_BROWSER_INTEGRATORS_ONE_TIME_TOKENS_MOCK_OTP_MANAGER_H_
+
+#include "components/autofill/core/browser/autofill_field.h"
+#include "components/autofill/core/browser/form_structure.h"
+#include "components/autofill/core/browser/integrators/one_time_tokens/otp_manager.h"
+#include "testing/gmock/include/gmock/gmock.h"
+
+namespace autofill {
+
+class MockOtpManager : public OtpManager {
+ public:
+  MockOtpManager();
+  MockOtpManager(const MockOtpManager&) = delete;
+  MockOtpManager& operator=(const MockOtpManager&) = delete;
+  ~MockOtpManager() override;
+
+  MOCK_METHOD(void,
+              GetOtpSuggestions,
+              (OtpManager::GetOtpSuggestionsCallback callback),
+              (override));
+};
+
+}  // namespace autofill
+
+#endif  // COMPONENTS_AUTOFILL_CORE_BROWSER_INTEGRATORS_ONE_TIME_TOKENS_MOCK_OTP_MANAGER_H_
