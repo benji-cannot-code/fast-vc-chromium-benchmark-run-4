27c529516664e0f356bd78a3d1217d194172f777
Henrique Nakashima <hnakashima@chromium.org>
2025-09-15 11:57:42 -0700

    [Android] Add @NullMarked to .c.b.webapps files in chrome_java (part 6)
    Bug: 389129271
    Change-Id: I2f3a3c5993ddf4ec649192ba6e4f819a402a80b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6940455
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515653}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/WebApkUpdateTask.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/WebApkUpdateTask.java
@@ -5,8 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.webapps;
 
+import static org.chromium.build.NullUtil.assumeNonNull;
+
 import android.content.Context;
 
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.browser.customtabs.BaseCustomTabActivity;
 import org.chromium.chrome.browser.customtabs.CustomTabLocator;
 import org.chromium.components.background_task_scheduler.NativeBackgroundTask;
@@ -21,11 +25,12 @@ import java.util.List;
  * Handles servicing of background WebAPK update requests coming via background_task_scheduler
  * component. Will update multiple WebAPKs if there are multiple WebAPKs pending update.
  */
+@NullMarked
 public class WebApkUpdateTask extends NativeBackgroundTask {
     /** The WebappDataStorage for the WebAPK to update. */
-    private WebappDataStorage mStorageToUpdate;
+    private @Nullable WebappDataStorage mStorageToUpdate;
 
-    /** Whether there are more WebAPKs to update than just {@link mStorageToUpdate}. */
+    /** Whether there are more WebAPKs to update than just {@link #mStorageToUpdate}. */
     private boolean mMoreToUpdate;
 
     @Override
@@ -40,6 +45,8 @@ public class WebApkUpdateTask extends NativeBackgroundTask {
         List<String> ids = WebappRegistry.getInstance().findWebApksWithPendingUpdate();
         for (String id : ids) {
             WebappDataStorage storage = WebappRegistry.getInstance().getWebappDataStorage(id);
+            assumeNonNull(storage);
+
             WeakReference<BaseCustomTabActivity> activity =
                     CustomTabLocator.findRunningWebappActivityWithId(storage.getId());
             if (activity == null || activity.get() == null) {
@@ -55,6 +62,7 @@ public class WebApkUpdateTask extends NativeBackgroundTask {
     protected void onStartTaskWithNative(
             Context context, TaskParameters taskParameters, final TaskFinishedCallback callback) {
         assert taskParameters.getTaskId() == TaskIds.WEBAPK_UPDATE_JOB_ID;
+        assert mStorageToUpdate != null;
 
         WebApkUpdateManager.updateWhileNotRunning(
                 mStorageToUpdate, () -> callback.taskFinished(mMoreToUpdate));
