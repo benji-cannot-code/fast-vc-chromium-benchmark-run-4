feb24294f70370f8cf65ecba1be142f1e159a377
Sophie Chang <sophiechang@chromium.org>
2025-09-15 09:53:55 -0700

    Update sensitivity persisted tab data to also persist the sensitivity score
    Bug: 444739656
    Change-Id: I7a5ddbbfa2719c169814b311042811a817a6d81a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6942989
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: David Maunder <davidjm@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515569}

--- chrome/browser/android/persisted_tab_data/sensitivity_persisted_tab_data_android.cc
+++ chrome/browser/android/persisted_tab_data/sensitivity_persisted_tab_data_android.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/persisted_tab_data/sensitivity_persisted_tab_data_android.h"
 
 #include "chrome/browser/android/persisted_tab_data/sensitivity_data.pb.h"
-#include "components/search_engines/template_url_service.h"
 
 SensitivityPersistedTabDataAndroid::SensitivityPersistedTabDataAndroid(
     TabAndroid* tab_android)
@@ -53,6 +52,7 @@ std::unique_ptr<const std::vector<uint8_t>>
 SensitivityPersistedTabDataAndroid::Serialize() {
   sensitivity::SensitivityData sensitivity_data;
   sensitivity_data.set_is_sensitive(is_sensitive_);
+  sensitivity_data.set_sensitivity_score(sensitivity_score_);
   std::unique_ptr<std::vector<uint8_t>> data =
       std::make_unique<std::vector<uint8_t>>(sensitivity_data.ByteSizeLong());
   sensitivity_data.SerializeToArray(data->data(), data->size());
@@ -66,6 +66,9 @@ void SensitivityPersistedTabDataAndroid::Deserialize(
     sensitivity_data.Clear();
   }
   is_sensitive_ = sensitivity_data.is_sensitive();
+  sensitivity_score_ = sensitivity_data.has_sensitivity_score()
+                           ? sensitivity_data.sensitivity_score()
+                           : -1.0;
 }
 
 void SensitivityPersistedTabDataAndroid::OnPageContentAnnotated(
@@ -74,10 +77,7 @@ void SensitivityPersistedTabDataAndroid::OnPageContentAnnotated(
   if (tab_->GetURL() != url) {
     return;
   }
-  // Setting the cutoff value to 0.5 for binary classification of data
-  // sensitivity. This value ensures that we neither overclassify nor
-  // underclassify sensitive data
-  set_is_sensitive(result.GetContentVisibilityScore() < 0.5);
+  set_sensitivity_score(result.GetContentVisibilityScore());
 }
 
 void SensitivityPersistedTabDataAndroid::ExistsForTesting(
