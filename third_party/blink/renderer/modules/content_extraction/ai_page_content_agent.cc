56d1f0493d21b9d2924829888aa47a2caa20a953
Khushal Sagar <khushalsagar@chromium.org>
2025-09-15 16:27:48 -0700

    blink: Add trace events for AIPageContentAgent + assert layout clean
    Add a trace event to know when we're doing a layout walk for APC. Also
    assert layout is clean when checking focusability.
    
    Change-Id: Ie0af2f96a6afd4b144f23e44760513d43c9596ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6952052
    Auto-Submit: Khushal Sagar <khushalsagar@chromium.org>
    Commit-Queue: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515754}

--- third_party/blink/renderer/modules/content_extraction/ai_page_content_agent.cc
+++ third_party/blink/renderer/modules/content_extraction/ai_page_content_agent.cc
@@ -978,6 +978,7 @@ AIPageContentAgent::ContentBuilder::~ContentBuilder() = default;
 
 mojom::blink::AIPageContentPtr AIPageContentAgent::ContentBuilder::Build(
     LocalFrame& frame) {
+  TRACE_EVENT0("blink", "AIPageContentAgent::ContentBuilder::Build");
   auto& document = *frame.GetDocument();
 
   mojom::blink::AIPageContentPtr page_content =
@@ -1804,7 +1805,8 @@ void AIPageContentAgent::ContentBuilder::AddNodeInteractionInfo(
     // TODO(khushalsagar): Remove is_clickability.
     node_interaction_info->is_clickable =
         !node_interaction_info->clickability_reasons.empty();
-    node_interaction_info->is_focusable = element->IsFocusable();
+    node_interaction_info->is_focusable =
+        element->IsFocusable(Element::UpdateBehavior::kAssertNoLayoutUpdates);
   }
 
   const bool needs_interaction_info =
