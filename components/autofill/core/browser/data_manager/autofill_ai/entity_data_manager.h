de0c95b2b561f1637527f567ee0358e618fc804a
Norge Vizcay <vizcay@google.com>
2025-09-16 05:10:49 -0700

    Observe valuable sync changes in entity data manager
    The entity data manager needs to reload its cached entities when a
    change is synced for the `AUTOFILL_VALUABLE` data type. This specific
    data type is used for syncing valuable data like Wallet public passes,
    which are managed by the entity data manager.
    
    This CL adds support in the entity data manager to observe these sync
    changes.
    
    The changes include:
    * Make `EntityDataManager` an `AutofillWebDataServiceObserverOnUISequence`.
    * Observe the `AutofillWebDataService` only if any of the Wallet public pass features are enabled.
    * Implement `OnAutofillChangedBySync` to trigger a `LoadEntities` operation when the data type is `AUTOFILL_VALUABLE` and the relevant features are enabled.
    * Update the unit test to confirm the cache reload is triggered only for `AUTOFILL_VALUABLE` sync changes.
    
    Bug: 436175248
    Change-Id: Iee7fbbdd986537aea020e2d13f197bb602bfb5b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6950632
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1515974}

--- components/autofill/core/browser/data_manager/autofill_ai/entity_data_manager.h
+++ components/autofill/core/browser/data_manager/autofill_ai/entity_data_manager.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/types/optional_ref.h"
 #include "components/autofill/core/browser/data_model/autofill_ai/entity_instance.h"
 #include "components/autofill/core/browser/webdata/autofill_webdata_service.h"
+#include "components/autofill/core/browser/webdata/autofill_webdata_service_observer.h"
 #include "components/history/core/browser/history_service.h"
 #include "components/history/core/browser/history_service_observer.h"
 #include "components/keyed_service/core/keyed_service.h"
@@ -43,7 +44,9 @@ class AutofillAiSaveStrikeDatabaseByHost;
 // their own EntityDataManager instance, they use the same underlying database.
 // Therefore, it is the responsibility of the callers to ensure that no data
 // from an incognito session is persisted unintentionally.
-class EntityDataManager : public KeyedService, history::HistoryServiceObserver {
+class EntityDataManager : public KeyedService,
+                          public AutofillWebDataServiceObserverOnUISequence,
+                          history::HistoryServiceObserver {
  public:
   class Observer : public base::CheckedObserver {
    public:
@@ -89,6 +92,9 @@ class EntityDataManager : public KeyedService, history::HistoryServiceObserver {
   base::optional_ref<const EntityInstance> GetEntityInstance(
       const EntityInstance::EntityId& guid) const LIFETIME_BOUND;
 
+  // AutofillWebDataServiceObserver:
+  void OnAutofillChangedBySync(syncer::DataType data_type) override;
+
   // history::HistoryServiceObserver:
   void OnHistoryDeletions(history::HistoryService*,
                           const history::DeletionInfo& deletion_info) override;
@@ -123,6 +129,10 @@ class EntityDataManager : public KeyedService, history::HistoryServiceObserver {
   // The result of the last successful LoadEntities() query.
   // All entries are identifiable by their EntityInstance::guid().
   base::flat_set<EntityInstance, EntityInstance::CompareByGuid> entities_;
+
+  base::ScopedObservation<AutofillWebDataService,
+                          AutofillWebDataServiceObserverOnUISequence>
+      webdata_service_observation_{this};
 
   base::ScopedObservation<history::HistoryService, HistoryServiceObserver>
       history_service_observation_{this};
