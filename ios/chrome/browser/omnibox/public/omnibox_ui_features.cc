dd9c049f3d75c6931e205c60247174ce16330c80
Christian Xu <christianxu@google.com>
2025-09-16 04:52:26 -0700

    [iOS][Omnibox] Add flag for multiline omnibox
    Add feature flag to enable multiline omnibox in AIM prototype.
    
    Bug: None
    Change-Id: I03f3c06211e7395b3ac517742f781c94a7d961b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6949171
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515964}

--- ios/chrome/browser/omnibox/public/omnibox_ui_features.cc
+++ ios/chrome/browser/omnibox/public/omnibox_ui_features.cc
@@ -13,5 +13,7 @@ bool IsRichAutocompletionEnabled() {
   return base::FeatureList::IsEnabled(omnibox::kRichAutocompletion);
 }
 
+BASE_FEATURE(kIOSOmniboxUseTextView, base::FEATURE_DISABLED_BY_DEFAULT);
+
 BASE_FEATURE(kBeginCursorAtPointTentativeFix,
              base::FEATURE_DISABLED_BY_DEFAULT);
