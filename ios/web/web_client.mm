3d14053a2a85ab9f9417ffd457a5f40353d265b0
Quentin Pubert <qpubert@google.com>
2025-09-16 05:42:05 -0700

    [iOS] Add OverrideOpenPanel() method to WebClient
    This CL adds an OverrideOpenPanel() method to web::WebClient which is
    called to determine whether the embedder of the web layer wants to
    override the behaviour of the open panel (also called upload panel). If
    the client returns false, then CRWWKUIHandler will return NO when asked
    whether it responds to selector
    -webView:runOpenPanelWithParameters:initiatedByFrame:completionHandler:,
    which means that whenever a page requests to show an open panel, the
    default behaviour will be invoked instead of calling this selector on
    CRWWKUIHandler.
    
    Bug: 441659098
    Change-Id: I25b53b989d18ef5e4f2d8e646b8a266fdae91287
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6950593
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Quentin Pubert <qpubert@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515981}

--- ios/web/web_client.mm
+++ ios/web/web_client.mm
@@ -99,6 +99,10 @@ bool WebClient::EnableWebInspector(BrowserState* browser_state) const {
   return false;
 }
 
+bool WebClient::OverrideOpenPanel() const {
+  return false;
+}
+
 void WebClient::CleanupNativeRestoreURLs(web::WebState* web_state) const {}
 
 void WebClient::WillDisplayMediaCapturePermissionPrompt(
