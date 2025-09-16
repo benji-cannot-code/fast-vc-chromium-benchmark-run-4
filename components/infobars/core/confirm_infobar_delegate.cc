60def5272c6c9a6ab7e85980aac0e72762743ef8
Muhammad Salmaan <musalmaan@chromium.org>
2025-09-16 16:34:43 -0700

    Add link spacing before button for Infobar
    Added custom spacing for having the link before the button and after the
    message text on the infobar.
    
    Bug: 431825969
    Change-Id: I7bd7695919f95bdb47b544b3904f0fdf89282441
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6956522
    Commit-Queue: Muhammad Salmaan <musalmaan@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1516324}

--- components/infobars/core/confirm_infobar_delegate.cc
+++ components/infobars/core/confirm_infobar_delegate.cc
@@ -68,6 +68,10 @@ bool ConfirmInfoBarDelegate::ShouldShowLinkBeforeButton() const {
   return false;
 }
 
+int ConfirmInfoBarDelegate::GetLinkSpacingWhenPositionedBeforeButton() const {
+  return 0;
+}
+
 #if BUILDFLAG(IS_IOS)
 bool ConfirmInfoBarDelegate::UseIconBackgroundTint() const {
   return true;
