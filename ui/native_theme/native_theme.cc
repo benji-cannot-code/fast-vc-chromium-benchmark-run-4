602cafbb12597e4ce48bb7005212b6daeb8b18f7
Peter Kasting <pkasting@chromium.org>
2025-09-16 10:55:25 -0700

    Add no-op implementations of GetPartSize() and the nine-patch methods.
    These (especially the nine-patch ones) are what multiple subclasses
    want, so supply them to avoid repetitious boilerplate.
    
    Bug: 431953872
    Change-Id: Id26df6b49258e27ecac6392644772ff2f11b9d9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6951776
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1516153}

--- ui/native_theme/native_theme.cc
+++ ui/native_theme/native_theme.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/color/color_provider_manager.h"
 #include "ui/color/system_theme.h"
 #include "ui/gfx/canvas.h"
+#include "ui/gfx/geometry/size.h"
 #include "ui/gfx/geometry/skia_conversions.h"
 #include "ui/gfx/scoped_canvas.h"
 #include "ui/native_theme/features/native_theme_features.h"
@@ -145,6 +146,12 @@ float NativeTheme::AdjustBorderRadiusByZoom(Part part,
              : border_radius;
 }
 
+gfx::Size NativeTheme::GetPartSize(Part part,
+                                   State state,
+                                   const ExtraParams& extra_params) const {
+  return {};
+}
+
 int NativeTheme::GetPaintedScrollbarTrackInset() const {
   return 0;
 }
@@ -157,6 +164,18 @@ float NativeTheme::GetBorderRadiusForPart(Part part,
                                           float width,
                                           float height) const {
   return 0;
+}
+
+bool NativeTheme::SupportsNinePatch(Part part) const {
+  return false;
+}
+
+gfx::Size NativeTheme::GetNinePatchCanvasSize(Part part) const {
+  NOTREACHED();
+}
+
+gfx::Rect NativeTheme::GetNinePatchAperture(Part part) const {
+  NOTREACHED();
 }
 
 SkColor NativeTheme::GetScrollbarThumbColor(
