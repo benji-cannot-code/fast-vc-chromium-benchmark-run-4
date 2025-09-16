a9579e8026afbb0e6588de82c072e56baebaf170
Tobiasz Ciepliński <tobson@google.com>
2025-09-16 07:12:19 -0700

    Make flight reservation string shorter
    This CL aligns the string with the one that is used in Directional
    Review slides.
    
    Bug: 436175248
    Change-Id: I0d7e7fba8bb65236195fef162174d8c2a3b79681
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6954328
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Auto-Submit: Tobiasz Ciepliński <tobson@google.com>
    Commit-Queue: Tobiasz Ciepliński <tobson@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1516014}

--- components/autofill/core/browser/suggestions/autofill_ai/autofill_ai_suggestion_generator_unittest.cc
+++ components/autofill/core/browser/suggestions/autofill_ai/autofill_ai_suggestion_generator_unittest.cc
@@ -749,7 +749,7 @@ TEST_F(AutofillAiSuggestionGeneratorTest,
   SetEntities({test::GetFlightReservationEntityInstance()});
   SetForm({NAME_FULL, FLIGHT_RESERVATION_TICKET_NUMBER});
   EXPECT_THAT(CreateAutofillAiFillingSuggestions(field(0)),
-              SuggestionsAre(HasLabel(u"Flight reservation · MUC–BEY")));
+              SuggestionsAre(HasLabel(u"Flight · MUC–BEY")));
 }
 
 }  // namespace
