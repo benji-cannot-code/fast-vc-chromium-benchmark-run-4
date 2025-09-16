575b9da0fd475610d9c776e55b7911734857f51b
Michael Checo <michaelcheco@google.com>
2025-09-16 09:30:02 -0700

    boca: Add request/response logic for fetching course work material
    - This change fetches a complete list of student assignments by
      querying both the courseWork and courseWorkMaterials APIs.
      This new, two-step logic is enabled by the BocaCourseWorkMaterialApi
      feature flag. When disabled, the original behavior of calling only
      the courseWork API is preserved.
    
    https://developers.google.com/
    workspace/classroom/reference/rest/v1/courses.courseWorkMaterials/
    workspace/classroom/reference/rest/v1/courses.courseWorkMaterials/list
    
    Bug: 408518787
    Test: ash_webui_unittests, google_apis_unittests
    Change-Id: I06442d65e916ebea0361b0880cb4ad16a0b407ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6925661
    Reviewed-by: April Zhou <aprilzhou@google.com>
    Reviewed-by: Artsiom Mitrokhin <amitrokhin@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1516102}

--- google_apis/gaia/gaia_constants.h
+++ google_apis/gaia/gaia_constants.h
@@ -124,6 +124,8 @@ COMPONENT_EXPORT(GOOGLE_APIS)
 extern const char kClassroomProfileEmailOauth2Scope[];
 COMPONENT_EXPORT(GOOGLE_APIS)
 extern const char kClassroomProfilePhotoUrlScope[];
+COMPONENT_EXPORT(GOOGLE_APIS)
+extern const char kClassroomCourseWorkMaterialsOAuthScope[];
 
 // OAuth2 scopes for Optimization Guide.
 COMPONENT_EXPORT(GOOGLE_APIS)
