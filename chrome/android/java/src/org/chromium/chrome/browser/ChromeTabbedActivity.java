964a43e056a2c9f5767b35af62db21a0367d9ca6
Michael Thiessen <mthiesse@chromium.org>
2025-09-16 11:41:23 -0700

    Reland "Flush persistent state when entering app switcher."
    Re-land: Art bug worked around in mockito roll: https://chromium-review.googlesource.com/c/chromium/src/+/6953110
    
    Also found an API to properly check if a dialog is showing, so we don't unnecessarily flush state.
    
    Original change description:
    
    When the user swipes Chrome's task away from the app switcher, Android
    doesn't give us enough time to save state on slower devices. In order
    to give us more time to save state, heuristically detect when we're
    entering the app switcher, and flush any persistent state then.
    
    The heuristic I use is checking for Window focus changing without losing
    topResumed state, and without having a dialog present (according to
    our modalDialogManger).
    
    Note that we will persist state unnecessarily in some cases (like Alert
    Dialogs getting shown) as Android does not provide an API to check if
    another app window has focus, and I don't think it's worth it to try
    to hook into all places we use legacy dialogs.
    
    Bug: 400975101, 444399087
    Change-Id: I6ef0528456f74a5db8ba10303cf33b898d22066e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6955827
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1516182}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -1589,7 +1589,8 @@ public class ChromeTabbedActivity extends ChromeActivity {
     public void onPauseWithNative() {
         mTabModelSelector.commitAllTabClosures();
 
-        if (mIncognitoCookiesFetcher != null) {
+        if (!ChromeFeatureList.isEnabled(ChromeFeatureList.UMA_SESSION_CORRECTNESS_FIXES)
+                && mIncognitoCookiesFetcher != null) {
             mIncognitoCookiesFetcher.persistCookies();
         }
 
@@ -1608,6 +1609,15 @@ public class ChromeTabbedActivity extends ChromeActivity {
     public void onStopWithNative() {
         super.onStopWithNative();
 
+        if (!ChromeFeatureList.isEnabled(ChromeFeatureList.UMA_SESSION_CORRECTNESS_FIXES)) {
+            saveState();
+        }
+    }
+
+    @Override
+    public void flushPersistentState() {
+        super.flushPersistentState();
+        mIncognitoCookiesFetcher.persistCookies();
         saveState();
     }
 
