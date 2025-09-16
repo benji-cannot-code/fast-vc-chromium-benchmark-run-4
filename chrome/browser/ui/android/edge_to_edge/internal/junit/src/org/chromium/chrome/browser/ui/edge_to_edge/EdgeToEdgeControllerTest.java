73ac318afbe849a51329947773c803615bb440cd
Lijin Shen <lazzzis@google.com>
2025-09-15 19:11:59 -0700

    Enable EdgeToEdgeTablet by default.
    Enabled-by-default-reason: launch EdgeToEdgeTablet
    Bug: 419082417
    Change-Id: Iee3b80a74a8be7da604b6dffedc890968fc163d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6944141
    Auto-Submit: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1515808}

--- chrome/browser/ui/android/edge_to_edge/internal/junit/src/org/chromium/chrome/browser/ui/edge_to_edge/EdgeToEdgeControllerTest.java
+++ chrome/browser/ui/android/edge_to_edge/internal/junit/src/org/chromium/chrome/browser/ui/edge_to_edge/EdgeToEdgeControllerTest.java
@@ -739,7 +739,7 @@ public class EdgeToEdgeControllerTest {
 
     @Test
     @Config(qualifiers = "xlarge")
-    @EnableFeatures(ChromeFeatureList.EDGE_TO_EDGE_TABLET)
+    @EnableFeatures(ChromeFeatureList.EDGE_TO_EDGE_TABLET + ":e2e_tablet_width_threshold/-1")
     public void enabledOnTabletWhenFeatureEnabled() {
         // Even these always-draw flags do not override the device abilities.
         EdgeToEdgeUtils.setAlwaysDrawWebEdgeToEdgeForTesting(true);
@@ -785,7 +785,7 @@ public class EdgeToEdgeControllerTest {
 
     @Test
     @Config(qualifiers = "sw600dp")
-    @EnableFeatures(ChromeFeatureList.EDGE_TO_EDGE_TABLET)
+    @EnableFeatures(ChromeFeatureList.EDGE_TO_EDGE_TABLET + ":e2e_tablet_width_threshold/-1")
     public void supportFormFactor() {
         assertTrue(DeviceFormFactor.isNonMultiDisplayContextOnTablet(mActivity));
         assertTrue(
