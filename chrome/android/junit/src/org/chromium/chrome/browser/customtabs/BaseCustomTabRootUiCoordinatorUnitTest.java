d4332553faebba921a4d0110af12530374949418
Samar <samarchehade@google.com>
2025-09-16 08:24:26 -0700

    [MIM][CCT] Delete expired CctSigninPrompt flag.
    Fixed: 341948846
    Change-Id: Id042cbd6499916c4bac0649b5a899486a33ee194
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6929972
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Samar Chehade-Lepleux <samarchehade@google.com>
    Cr-Commit-Position: refs/heads/main@{#1516059}

--- chrome/android/junit/src/org/chromium/chrome/browser/customtabs/BaseCustomTabRootUiCoordinatorUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/customtabs/BaseCustomTabRootUiCoordinatorUnitTest.java
@@ -36,8 +36,6 @@ import org.robolectric.annotation.Config;
 
 import org.chromium.base.CallbackUtils;
 import org.chromium.base.FakeTimeTestRule;
-import org.chromium.base.FeatureList;
-import org.chromium.base.FeatureOverrides;
 import org.chromium.base.TimeUtils;
 import org.chromium.base.UnownedUserDataHost;
 import org.chromium.base.supplier.ObservableSupplier;
@@ -86,7 +84,6 @@ import org.chromium.components.browser_ui.widget.MenuOrKeyboardActionController;
 import org.chromium.components.commerce.core.CommerceFeatureUtils;
 import org.chromium.components.commerce.core.CommerceFeatureUtilsJni;
 import org.chromium.components.commerce.core.ShoppingService;
-import org.chromium.components.signin.SigninFeatures;
 import org.chromium.components.signin.identitymanager.IdentityManager;
 import org.chromium.ui.base.ActivityWindowAndroid;
 import org.chromium.ui.base.IntentRequestTracker;
@@ -308,7 +305,6 @@ public final class BaseCustomTabRootUiCoordinatorUnitTest {
     }
 
     @Test
-    @EnableFeatures({SigninFeatures.CCT_SIGN_IN_PROMPT})
     public void testCreateMismatchNotificationChecker() {
         HistogramWatcher freCompletedRecentlyWatcher =
                 HistogramWatcher.newSingleRecordWatcher(
@@ -323,8 +319,6 @@ public final class BaseCustomTabRootUiCoordinatorUnitTest {
                                 MismatchNotificationController.SuppressedReason
                                         .CCT_IS_OFF_THE_RECORD)
                         .build();
-        FeatureList.setDisableNativeForTesting(true);
-        FeatureOverrides.enable(SigninFeatures.CCT_SIGN_IN_PROMPT);
         CustomTabsConnection connection = Mockito.mock(CustomTabsConnection.class);
         CustomTabsConnection.setInstanceForTesting(connection);
         when(connection.isAppForAccountMismatchNotification(any())).thenReturn(true);
--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2873,9 +2873,6 @@ extern const char kCCTResizableForThirdPartiesDescription[];
 extern const char kCCTRevampedBrandingName[];
 extern const char kCCTRevampedBrandingDescription[];
 
-extern const char kCCTSignInPromptName[];
-extern const char kCCTSignInPromptDescription[];
-
 extern const char kCCTToolbarRefactorName[];
 extern const char kCCTToolbarRefactorDescription[];
 
