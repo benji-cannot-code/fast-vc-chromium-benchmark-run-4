1d37c284cdd2fc54d4c206075b8cff7985796f9a
Xiaohan Wang <xhwang@chromium.org>
2025-09-15 23:32:00 -0700

    media: Fix typo in feature name kUseSequencedTaskRunnerForVEA
    The feature name was misspelled as `kUSeSequencedTaskRunnerForVEA`. This
    commit corrects the capitalization to `kUseSequencedTaskRunnerForVEA` in
    its definition, declaration, and usage.
    
    With this change, we can also use the new 2-param BASE_FEATURE macro.
    
    Bug: 436274260
    Change-Id: I0d8fc0d5a64c9272afe16d6a88770cac97e9385e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6952851
    Auto-Submit: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Phil Yan <philyan@chromium.org>
    Reviewed-by: Phil Yan <philyan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515867}

--- media/gpu/v4l2/v4l2_video_encode_accelerator.cc
+++ media/gpu/v4l2/v4l2_video_encode_accelerator.cc
@@ -124,7 +124,7 @@ std::optional<VideoFrameLayout> AsMultiPlanarLayout(
 }
 
 scoped_refptr<base::SequencedTaskRunner> CreateEncoderTaskRunner() {
-  if (base::FeatureList::IsEnabled(kUSeSequencedTaskRunnerForVEA)) {
+  if (base::FeatureList::IsEnabled(kUseSequencedTaskRunnerForVEA)) {
     return base::ThreadPool::CreateSequencedTaskRunner(
         {base::WithBaseSyncPrimitives(), base::TaskPriority::USER_VISIBLE,
          base::MayBlock()});
