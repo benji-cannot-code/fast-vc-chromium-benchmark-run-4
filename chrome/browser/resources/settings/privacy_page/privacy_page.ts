0910ef1232e85d356f965c10d6e281ef8bcbf55d
dpapad <dpapad@chromium.org>
2025-09-16 09:05:37 -0700

    Settings: Refactor site_settings UIs to dedicated elements, part 6.
    This is in preparation of moving these UIs from privacy_page.html to
    privacy_page_index.html as part of the ongoing Settings re-architecture.
    
    The refactoring has been facilitated by using a local script (see
    crrev.com/c/4492155).
    
    Fixed: 40719916
    Change-Id: I67f101b45687a9306299bc1c661c42cd58a0ff34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6950673
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1516086}

--- chrome/browser/resources/settings/privacy_page/privacy_page.ts
+++ chrome/browser/resources/settings/privacy_page/privacy_page.ts
@@ -106,18 +106,6 @@ export class SettingsPrivacyPageElement extends SettingsPrivacyPageElementBase {
             loadTimeData.getBoolean('isPrivacySandboxRestrictedNoticeEnabled'),
       },
 
-      /**
-       * Whether the File System Access Persistent Permissions UI should be
-       * displayed.
-       */
-      showPersistentPermissions_: {
-        type: Boolean,
-        readOnly: true,
-        value: function() {
-          return loadTimeData.getBoolean('showPersistentPermissions');
-        },
-      },
-
       focusConfig_: {
         type: Object,
         value() {
@@ -187,7 +175,6 @@ export class SettingsPrivacyPageElement extends SettingsPrivacyPageElementBase {
   }
 
   declare private isGuest_: boolean;
-  declare private showPersistentPermissions_: boolean;
   declare private showClearBrowsingDataDialog_: boolean;
   declare private showPrivacyGuideDialog_: boolean;
   declare private enableDeleteBrowsingDataRevamp_: boolean;
