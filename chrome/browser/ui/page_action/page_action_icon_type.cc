7ad388cf17d7cd379a669a0c1979009e97708170
Muhammad Salmaan <musalmaan@chromium.org>
2025-09-16 15:18:43 -0700

    Remove sharing hub page action
    This CL removes the sharing hub page action as part of the page
    action migration.
    
    Bug: 376283512
    Change-Id: I62cd63094cda3ed529338c9708203f8e18c1444d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6955174
    Commit-Queue: Muhammad Salmaan <musalmaan@chromium.org>
    Reviewed-by: Daniel Soromou <koretadaniel@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1516288}

--- chrome/browser/ui/page_action/page_action_icon_type.cc
+++ chrome/browser/ui/page_action/page_action_icon_type.cc
@@ -49,6 +49,8 @@ const base::FeatureParam<bool>* GetPageActionMigrationParam(
       return &features::kPageActionsMigrationAutofillMandatoryReauth;
     case PageActionIconType::kClickToCall:
       return &features::kPageActionsMigrationClickToCall;
+    case PageActionIconType::kSharingHub:
+      return &features::kPageActionsMigrationSharingHub;
     default:
       return nullptr;
   }
