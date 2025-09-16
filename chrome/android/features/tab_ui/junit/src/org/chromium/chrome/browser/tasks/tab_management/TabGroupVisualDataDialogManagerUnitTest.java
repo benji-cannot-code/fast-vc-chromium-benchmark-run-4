2b2edee1f571a7ff567b732813ac3d4a653cc0ad
Sky Malice <skym@chromium.org>
2025-09-16 16:11:32 -0700

    Verify tab groups exist before setting metadata.
    Some UI call sites could be opened before a deletion came in from sync
    and the collections code paths will crash if they try to update the
    metadata for a group that no longer exists. This fixes the problem by
    always double checking the group still exists.
    
    It may be preferable to also auto close all of these when the underlying
    group is deleted, however that wouldn't solve this problem 100% of the
    time. While the UI events and sync both run on the UI thread, it doesn't
    seem possible to guarantee that we never interleave them perfectly. So
    this defensive approach is necessary regardless.
    
    Bug: 443545547
    Change-Id: I5d7978faf21d032bb55df2902194a37142436451
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6956517
    Reviewed-by: Zhe Li <zheliooo@google.com>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1516316}

--- chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupVisualDataDialogManagerUnitTest.java
+++ chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupVisualDataDialogManagerUnitTest.java
@@ -151,7 +151,7 @@ public class TabGroupVisualDataDialogManagerUnitTest {
         Assert.assertEquals(View.GONE, description.getVisibility());
         verify(mTracker, never()).notifyEvent(eq(TAB_GROUP_CREATION_DIALOG_SHOWN));
 
-        mTabGroupVisualDataDialogManager.hideDialog();
+        mTabGroupVisualDataDialogManager.onHideDialog();
         verify(mTracker, never()).dismissed(eq(TAB_GROUP_CREATION_DIALOG_SYNC_TEXT_FEATURE));
     }
 
@@ -179,7 +179,7 @@ public class TabGroupVisualDataDialogManagerUnitTest {
                 description.getText());
         verify(mTracker).notifyEvent(eq(TAB_GROUP_CREATION_DIALOG_SHOWN));
 
-        mTabGroupVisualDataDialogManager.hideDialog();
+        mTabGroupVisualDataDialogManager.onHideDialog();
         verify(mTracker).dismissed(eq(TAB_GROUP_CREATION_DIALOG_SYNC_TEXT_FEATURE));
     }
 
@@ -208,7 +208,7 @@ public class TabGroupVisualDataDialogManagerUnitTest {
                 description.getText());
         verify(mTracker).notifyEvent(eq(TAB_GROUP_CREATION_DIALOG_SHOWN));
 
-        mTabGroupVisualDataDialogManager.hideDialog();
+        mTabGroupVisualDataDialogManager.onHideDialog();
         verify(mTracker).dismissed(eq(TAB_GROUP_CREATION_DIALOG_SYNC_TEXT_FEATURE));
     }
 }
