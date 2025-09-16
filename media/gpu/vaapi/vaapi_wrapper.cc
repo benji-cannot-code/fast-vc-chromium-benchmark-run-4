49dfe84fb30bb443d8f444ff0676c636fb1747c3
Eugene Zemtsov <eugene@chromium.org>
2025-09-15 19:19:00 -0700

    vaapi: Add logging to make it easier to troubleshoot VAAPI playback issues
    A common reason for VAAPI playback issues on Linux is version mismatch.
    This change helps motivated users to understand that they need to update
    VA API.
    
    Change-Id: I9e93c9a6273746b5cee0711d097ecc44a4895b39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6952249
    Reviewed-by: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515810}

--- media/gpu/vaapi/vaapi_wrapper.cc
+++ media/gpu/vaapi/vaapi_wrapper.cc
@@ -1629,7 +1629,7 @@ bool VADisplayStateSingleton::Initialize() {
   int major_version, minor_version;
   VAStatus va_res = vaInitialize(va_display, &major_version, &minor_version);
   if (va_res != VA_STATUS_SUCCESS) {
-    VLOGF(1) << "vaInitialize failed: " << vaErrorStr(va_res);
+    LOG(ERROR) << "vaInitialize failed: " << vaErrorStr(va_res);
     return false;
   }
 
@@ -1651,6 +1651,9 @@ bool VADisplayStateSingleton::Initialize() {
   const base::Version build_time_version({VA_MAJOR_VERSION, VA_MINOR_VERSION});
   CHECK(build_time_version.IsValid());
   if (!IsLibVACompatible(runtime_version, build_time_version)) {
+    LOG(ERROR) << "Installed VAAPI version is too old."
+               << " min supported version: " << build_time_version
+               << " installed version: " << runtime_version;
     return false;
   }
 
