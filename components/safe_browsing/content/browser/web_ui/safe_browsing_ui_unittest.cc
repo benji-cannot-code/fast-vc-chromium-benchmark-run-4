210eeac3f2e1d442d9c10786f64bf906f0203607
Daniel White <danieltwhite@google.com>
2025-09-16 12:18:56 -0700

    [Safe Browsing] Migrate SafeBrowsingUIHandler from SafeBrowsingUI
    This CL migrates the SafeBrowsingUIHandler class from the
    SafeBrowsingUI class. This change is part of the effort to
    componentize the SafeBrowsing WebUI to work across platforms. This
    change will make it easier for componentization to occur.
    
    Bug: 427719727
    
    Change-Id: Ic41ac6b01875c917a8b5a71c548c0f60c055103f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6927051
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Reviewed-by: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1516203}

--- components/safe_browsing/content/browser/web_ui/safe_browsing_ui_unittest.cc
+++ components/safe_browsing/content/browser/web_ui/safe_browsing_ui_unittest.cc
@@ -4,7 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "components/safe_browsing/content/browser/web_ui/safe_browsing_ui.h"
+
 #include "base/test/values_test_util.h"
+#include "components/safe_browsing/content/browser/web_ui/safe_browsing_ui_handler.h"
 #include "components/safe_browsing/core/common/proto/safebrowsingv5.pb.h"
 #include "content/public/test/browser_task_environment.h"
 #include "content/public/test/test_browser_context.h"
--- components/safe_browsing/content/browser/web_ui/web_ui_info_singleton.cc
+++ components/safe_browsing/content/browser/web_ui/web_ui_info_singleton.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "components/safe_browsing/content/browser/web_ui/safe_browsing_ui.h"
+#include "components/safe_browsing/content/browser/web_ui/safe_browsing_ui_handler.h"
 #include "components/sync/protocol/user_event_specifics.pb.h"
 #include "content/public/browser/browser_context.h"
 
