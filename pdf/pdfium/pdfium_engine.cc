8827c7d85a9a8f997084b9773653000079a02bc2
Lei Zhang <thestig@chromium.org>
2025-09-16 10:18:05 -0700

    [PDF] Improve index checks in PDFiumEngine::ExtendSelection() callers
    Make sure the page index values are in bounds, and not just
    non-negative.
    
    Change-Id: Ib42208c1f03292d4678d86717a86ae292c16bf1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6951999
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1516128}

--- pdf/pdfium/pdfium_engine.cc
+++ pdf/pdfium/pdfium_engine.cc
@@ -1823,7 +1823,7 @@ bool PDFiumEngine::OnMouseMove(const blink::WebMouseEvent& event) {
 
   // We're selecting but right now we're not over text, so don't change the
   // current selection.
-  if (point_data.page_index < 0 || point_data.char_index < 0) {
+  if (!PageIndexInBounds(point_data.page_index) || point_data.char_index < 0) {
     return false;
   }
 
@@ -1891,7 +1891,7 @@ void PDFiumEngine::OnMouseEnter(const blink::WebMouseEvent& event) {
 }
 
 bool PDFiumEngine::ExtendSelection(const PointData& point_data) {
-  DCHECK_GE(point_data.page_index, 0);
+  DCHECK(PageIndexInBounds(point_data.page_index));
   DCHECK_GE(point_data.char_index, 0);
 
   const int page_index = point_data.page_index;
@@ -4383,7 +4383,7 @@ void PDFiumEngine::SetCaretPosition(const gfx::Point& position) {
 
 void PDFiumEngine::MoveRangeSelectionExtent(const gfx::Point& extent) {
   auto point_data = GetPointData(gfx::PointF(extent));
-  if (point_data.page_index < 0 || point_data.char_index < 0) {
+  if (!PageIndexInBounds(point_data.page_index) || point_data.char_index < 0) {
     return;
   }
 
@@ -4920,7 +4920,7 @@ void PDFiumEngine::RegenerateContents() {
 
 bool PDFiumEngine::ExtendSelectionByPoint(const gfx::PointF& point) {
   auto point_data = GetPointData(point);
-  if (point_data.page_index < 0 || point_data.char_index < 0) {
+  if (!PageIndexInBounds(point_data.page_index) || point_data.char_index < 0) {
     return false;
   }
 
