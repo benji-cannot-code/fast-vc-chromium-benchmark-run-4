7f49d0365833e72f6d858e2d4474b1ce98266e17
James Cook <jamescook@chromium.org>
2025-09-17 10:29:08 -0700

    extensions: Add static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE)), part 7
    Add to files we have ported to desktop Android, in this case the
    preference API and sessions API.
    
    Bug: 443984428
    Change-Id: Ifc2ebfc7f7cac4de4a41dbe880e2d0923ea2ec94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6956853
    Reviewed-by: Zhengzheng Liu <zhzhliu@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1516746}

--- chrome/browser/extensions/api/preference/network_prediction_transformer.cc
+++ chrome/browser/extensions/api/preference/network_prediction_transformer.cc
@@ -10,6 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/values.h"
 #include "chrome/browser/preloading/preloading_prefs.h"
+#include "extensions/buildflags/buildflags.h"
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace extensions {
 
