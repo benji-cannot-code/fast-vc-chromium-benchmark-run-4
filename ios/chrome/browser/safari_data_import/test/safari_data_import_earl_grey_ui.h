40f8e8297bce7f88ada762dbee4a0f8538fd37f8
ginnnnnnny <ginnyhuang@chromium.org>
2025-09-17 12:21:34 -0700

    Reland "[iOS] Fixing Safari data import egtests"
    This CL relands crrev.com/c/6956467 and crrev.com/c/6955327. Also fixes
    ios-simulator-noncq failure by adding `ScopedSynchronizationDisabler`.
    
    Bug: 441124865
    Change-Id: Ie2689a2a3ae145c73ef089f0c7d01f77dff31ebe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6960834
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Benjamin Williams <bwwilliams@google.com>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1516821}

--- ios/chrome/browser/safari_data_import/test/safari_data_import_earl_grey_ui.h
+++ ios/chrome/browser/safari_data_import/test/safari_data_import_earl_grey_ui.h
@@ -12,4 +12,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /// verification.
 void DismissSafariDataImportEntryPoint(bool verify_visibility);
 
+/// Visibility of Safari data import entry point.
+bool IsSafariDataImportEntryPointVisible();
+
+/// Tap "Import" on the Safari data import entry point displayed.
+void StartImportOnSafariDataImportEntryPoint();
+
+/// Tap "Remind Me Later" on Safari data import entry point displayed.
+void SetReminderOnSafariDataImportEntryPoint();
+
 #endif  // IOS_CHROME_BROWSER_SAFARI_DATA_IMPORT_TEST_SAFARI_DATA_IMPORT_EARL_GREY_UI_H_
