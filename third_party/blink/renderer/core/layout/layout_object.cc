4529d1f30c15479135393a9c434a0d6fa65d7dd0
Morten Stenshorne <mstensho@chromium.org>
2025-09-17 09:40:07 -0700

    Mark new spanners for layout when an ancestor no longer prevents them.
    There are a few requirements for a descendant of a multicol container to
    become a spanner. Having `column-span:all` is just one thing.
    Additionally, all ancestors of the spanner candidate need to participate
    in the block formatting context established by the columns.
    Additionally, things like transforms prevent spanners inside. If a box
    initially establishes a transform, but then a style change removes the
    transform, a descendant may suddenly become a spanner. However, such new
    spanners were not marked for layout when this happened. Fix this.
    
    This bug caused layout correctness issues and layout inconsistency.
    Additionally, we could get false cache hits on an ancestor of the
    spanner, and end up re-using an old fragment after a child in the tree
    was detached, which would lead to a crash when walking the fragment tree
    afterwards.
    
    Split IsValidColumnSpanner() into separate functions, so that the new
    code that checks the tree for new spanners can use them.
    
    Bug: 445210793
    Change-Id: Id9f0869d5aaed20da8e3ba1275faf029c8988008
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6955055
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1516713}

--- third_party/blink/renderer/core/layout/layout_object.cc
+++ third_party/blink/renderer/core/layout/layout_object.cc
@@ -3237,8 +3237,8 @@ void LayoutObject::StyleDidChange(
         }
       }
     } else if (IsBox() &&
-               To<LayoutBox>(this)->IsValidColumnSpanner(*old_style) !=
-                   To<LayoutBox>(this)->IsValidColumnSpanner(*style_)) {
+               To<LayoutBox>(this)->IsValidColumnSpannerInTree(*old_style) !=
+                   To<LayoutBox>(this)->IsValidColumnSpannerInTree(*style_)) {
       MarkParentForSpannerOrOutOfFlowPositionedChange();
     }
 
--- third_party/blink/renderer/core/layout/layout_object.h
+++ third_party/blink/renderer/core/layout/layout_object.h
@@ -1696,7 +1696,7 @@ class CORE_EXPORT LayoutObject : public GarbageCollected<LayoutObject>,
   // spanner. For one, it needs to be a block-level box that's inside a multicol
   // container, and it also needs to be in the block formatting context
   // established by the columns.
-  virtual bool IsValidColumnSpanner() const {
+  virtual bool IsValidColumnSpannerInTree() const {
     NOT_DESTROYED();
     return false;
   }
@@ -1705,7 +1705,7 @@ class CORE_EXPORT LayoutObject : public GarbageCollected<LayoutObject>,
     NOT_DESTROYED();
     // May be called before style is set.
     return Style() && Style()->GetColumnSpan() == EColumnSpan::kAll &&
-           IsValidColumnSpanner();
+           IsValidColumnSpannerInTree();
   }
 
   // We include LayoutButton in this check, because buttons are
@@ -3411,6 +3411,10 @@ class CORE_EXPORT LayoutObject : public GarbageCollected<LayoutObject>,
   struct StyleChangeContext {
     // An object ceased to be floating or out-of-flow.
     bool became_normal_flow = false;
+
+    // An object prevented descendants from becoming column spanners (before
+    // style change).
+    bool did_prevent_spanner_descendants = false;
   };
 
   // Overrides should call the superclass at the end. style_ will be 0 the
