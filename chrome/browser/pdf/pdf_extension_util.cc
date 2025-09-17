b66126a26cdfafaf9d551d908641d9eed7ac641b
Yihong Guo <yhguo@google.com>
2025-09-17 16:14:05 -0700

    [Save to Drive] Disable the feature in Incognito mode
    This change prevents the "Save to Drive" feature from being enabled in
    the PDF viewer when browsing in an incognito window. It does so by not
    rendering the Save to Drive button in the viewer.
    
    Test will be separately added.
    
    Bug: 435142523
    Change-Id: I0bddbda60b0a6d384ab9ecc5eb7f06e494f5bc72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6961105
    Commit-Queue: Yihong Guo <yhguo@google.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1516917}

--- chrome/browser/pdf/pdf_extension_util.cc
+++ chrome/browser/pdf/pdf_extension_util.cc
@@ -44,10 +44,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
 #if BUILDFLAG(ENABLE_PDF_INK2)
-#include "chrome/browser/profiles/profile.h"
 #include "chrome/common/pref_names.h"
 #include "components/prefs/pref_service.h"
 #endif  // BUILDFLAG(ENABLE_PDF_INK2)
+
+#if BUILDFLAG(ENABLE_PDF_INK2) || BUILDFLAG(ENABLE_PDF_SAVE_TO_DRIVE)
+#include "chrome/browser/profiles/profile.h"
+#endif  // BUILDFLAG(ENABLE_PDF_INK2) || BUILDFLAG(ENABLE_PDF_SAVE_TO_DRIVE)
 
 namespace pdf_extension_util {
 
@@ -316,8 +319,10 @@ base::Value::Dict GetAdditionalData(content::BrowserContext* context) {
       base::FeatureList::IsEnabled(chrome_pdf::features::kPdfSearchifySave));
 
 #if BUILDFLAG(ENABLE_PDF_SAVE_TO_DRIVE)
-  dict.Set("pdfSaveToDrive",
-           base::FeatureList::IsEnabled(chrome_pdf::features::kPdfSaveToDrive));
+  const bool save_to_drive_enabled =
+      base::FeatureList::IsEnabled(chrome_pdf::features::kPdfSaveToDrive) &&
+      !Profile::FromBrowserContext(context)->IsIncognitoProfile();
+  dict.Set("pdfSaveToDrive", save_to_drive_enabled);
   dict.Set("pdfSaveToDriveHelpCenterURL",
            chrome::kPdfViewerSaveToDriveHelpCenterURL);
 #endif
