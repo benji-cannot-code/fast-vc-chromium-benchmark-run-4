1cb855588004eb9ea9d32b974e2d57731f3a03c7
Tomasz Wiszkowski <ender@google.com>
2025-09-17 09:28:15 -0700

    Allow the suggestions list to be stacked top-down or bottom-up.
    Bug: 445227077
    Change-Id: I7264e4ca67804a7ea9d7102a0ce6fb012d19a4cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6956917
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Jian Li <jianli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1516709}

--- chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
@@ -1480,6 +1480,11 @@ public abstract class ChromeFeatureList {
     public static final BooleanCachedFeatureParam sAndroidBottomToolbarDefaultToTop =
             newBooleanCachedFeatureParam(ANDROID_BOTTOM_TOOLBAR, "default_to_top", true);
 
+    public static final BooleanCachedFeatureParam
+            sAndroidBottomToolbarV2ReverseOrderSuggestionsList =
+                    newBooleanCachedFeatureParam(
+                            ANDROID_BOTTOM_TOOLBAR_V2, "reverse_order_suggestions_list", false);
+
     public static final IntCachedFeatureParam sBackgroundThreadPoolFieldTrialConfig =
             newIntCachedFeatureParam(BACKGROUND_THREAD_POOL_FIELD_TRIAL, "config", 4);
 
@@ -1826,6 +1831,7 @@ public abstract class ChromeFeatureList {
                     sAndroidAppIntegrationWithFaviconZeroStateFaviconNumber,
                     sAndroidBookmarkBarShowBookmarkBar,
                     sAndroidBottomToolbarDefaultToTop,
+                    sAndroidBottomToolbarV2ReverseOrderSuggestionsList,
                     sAndroidComposeplateHideIncognitoButton,
                     sAndroidComposeplateSkipLocaleCheck,
                     sAndroidComposeplateV2Enabled,
