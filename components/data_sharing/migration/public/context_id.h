b4b2bbd3f767b3dae5490ead6326b75b2386b557
Hailey Wang <haileywang@google.com>
2025-09-17 15:26:56 -0700

    [ContextId] Add the definition of ContextId
    Change-Id: I6bf06ece67aa3be1c2333f777a772e83d6350085
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6956854
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1516901}

---
+++ components/data_sharing/migration/public/context_id.h
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_DATA_SHARING_MIGRATION_PUBLIC_CONTEXT_ID_H_
+#define COMPONENTS_DATA_SHARING_MIGRATION_PUBLIC_CONTEXT_ID_H_
+
+#include <string>
+
+#include "base/types/strong_alias.h"
+
+namespace data_sharing {
+
+// A stable ID that provides a reliable key to link private and shared data
+// sets.
+struct ContextId : public base::StrongAlias<class ContextIdTag, std::string> {
+  using base::StrongAlias<class ContextIdTag, std::string>::StrongAlias;
+
+  // Converts the ContextId to a string.
+  const std::string& AsString() const { return value(); }
+
+  // Creates a ContextId from a string.
+  static ContextId FromString(const std::string& str) { return ContextId(str); }
+};
+
+}  // namespace data_sharing
+
+#endif  // COMPONENTS_DATA_SHARING_MIGRATION_PUBLIC_CONTEXT_ID_H_
