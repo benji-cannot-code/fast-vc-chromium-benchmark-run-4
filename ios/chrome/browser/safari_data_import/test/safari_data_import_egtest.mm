56bacffdeeb308b657e8e05897e03dbe7b9a7028
ginnnnnnny <ginnyhuang@chromium.org>
2025-09-17 16:00:04 -0700

    [iOS] Explicitly enable Safari data import for EG tests
    ...so that they pass on ios-fieldtrial-rel too.
    
    Bug: N/A
    Change-Id: I78ff1c1250b800e25a18b9bfcc455357d5d2c79e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6962715
    Reviewed-by: Hira Mahmood <hiramahmood@google.com>
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1516912}

--- ios/chrome/browser/safari_data_import/test/safari_data_import_egtest.mm
+++ ios/chrome/browser/safari_data_import/test/safari_data_import_egtest.mm
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#import "ios/chrome/browser/passwords/model/features.h"
 #import "ios/chrome/browser/safari_data_import/test/safari_data_import_earl_grey_ui.h"
 #import "ios/chrome/browser/settings/ui_bundled/settings_table_view_controller_constants.h"
 #import "ios/chrome/common/ui/confirmation_alert/constants.h"
@@ -22,12 +23,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation SafariDataImportTestCase
 
-- (AppLaunchConfiguration)appConfigurationForTestCase {
+/// Helper method creating an instance of AppLaunchConfiguration that enables
+/// the Safari import feature by default. No other behavior is overridden.
+- (AppLaunchConfiguration)appConfigurationNoOverrideBehavior {
   AppLaunchConfiguration config = [super appConfigurationForTestCase];
+  config.features_enabled.push_back(kImportPasswordsFromSafari);
+  config.relaunch_policy = ForceRelaunchByCleanShutdown;
+  return config;
+}
+
+/// App configuration for this test suite, which ensures that the Safari import
+/// entry point displays on app startup in non-first-run launches. This reduces
+/// the time it takes for each test case by removing the need to go through FRE
+/// screens or go into Settings.
+- (AppLaunchConfiguration)appConfigurationForTestCase {
+  AppLaunchConfiguration config = [self appConfigurationNoOverrideBehavior];
   config.additional_args.push_back("-NextPromoForDisplayOverride");
   config.additional_args.push_back(
       "promos_manager::Promo::SafariImportRemindMeLater");
-  config.relaunch_policy = ForceRelaunchByCleanShutdown;
   return config;
 }
 
@@ -36,7 +49,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)testFirstRunAndReminder {
   if (@available(iOS 18.2, *)) {
     [[self class] testForStartup];
-    AppLaunchConfiguration firstRunConfig = [super appConfigurationForTestCase];
+    AppLaunchConfiguration firstRunConfig =
+        [self appConfigurationNoOverrideBehavior];
     /// Show the First Run UI at startup.
     firstRunConfig.additional_args.push_back("-FirstRunForceEnabled");
     firstRunConfig.additional_args.push_back("true");
@@ -63,7 +77,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     /// Restart in IPH demo mode. If the reminder is successfully registered, it
     /// will be displayed on restart.
-    AppLaunchConfiguration iphConfig = [super appConfigurationForTestCase];
+    AppLaunchConfiguration iphConfig =
+        [self appConfigurationNoOverrideBehavior];
     iphConfig.iph_feature_enabled = "IPH_iOSSafariImportFeature";
     iphConfig.relaunch_policy = ForceRelaunchByCleanShutdown;
     [[AppLaunchManager sharedManager]
@@ -79,7 +94,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     ScopedSynchronizationDisabler disabler;
     /// Clean restart without experimental settings.
     [[AppLaunchManager sharedManager]
-        ensureAppLaunchedWithConfiguration:[super appConfigurationForTestCase]];
+        ensureAppLaunchedWithConfiguration:
+            [self appConfigurationNoOverrideBehavior]];
     [ChromeEarlGreyUI openSettingsMenu];
     [[[EarlGrey selectElementWithMatcher:
                     grey_allOf(grey_accessibilityID(
