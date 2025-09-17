9b81ee639fc10bca991bef075ecc9c2e215389a5
Dominik Röttsches <drott@chromium.org>
2025-09-17 02:35:40 -0700

    Revert "Disable Fontations FontConfig indexing and disregard old caches"
    This reverts commit 206882f864fa01256761a755a89054ac61ac3848.
    
    Re-enable Fontations indexing. Indexing issues have been addressed by a
    fix in FontConfig. For fonts without family name a name is generated
    from the filename. Fonts with incorrect CFF tables are reject, same as
    in FreeType indexing.
    
    From 206882f864fa0:
    
    > Note: When reverting this, we need to make sure that if FontConfig cache
    > version has not been changed since this was broken, the code then also
    > needs to be able to handle cache entries that may have been incorrectly
    > added, for example for the HEJI2Text.otf font, see bug.
    
    The FontConfig cache version has been increased to 11 in the previous
    FontConfig roll [1], so we no longer need a suffix for our cache files.
    
    [1] https://crrev.com/c/6956169
    
    Enabled-by-default-reason: killswitch
    Bug: 442747781
    Change-Id: If8e92ad7498cf376c024f871e8778db9d064621e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6954652
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1516516}

--- ui/gfx/linux/fontconfig_util.cc
+++ ui/gfx/linux/fontconfig_util.cc
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
 namespace features {
-BASE_FEATURE(kFontConfigFontationsIndexing, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kFontConfigFontationsIndexing, base::FEATURE_ENABLED_BY_DEFAULT);
 }
 
 namespace gfx {
