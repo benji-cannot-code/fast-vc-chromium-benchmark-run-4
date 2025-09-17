b42bc3dfe4eadc353ab0141ce557db8c67067107
Alison Gale <agale@chromium.org>
2025-09-17 11:36:48 -0700

    [SxS] Add what's new module for split view
    Adding a browser command to be triggered from the whats new page.
    This command will open a new split view.
    
    To locally test:
    - Build patch and run Chrome (branded build).
    - Navigate to chrome://whats-new, open devtools in iframe, and run:
    
    window.top.postMessage(
      {'data': {'event': 'browser_command', 'commandId': 19, 'clickInfo': {}}},
      'chrome://whats-new'
    );
    
    Bug: 436910548
    Change-Id: I34f4e48b85d48bce4c9bf19f4df66419ccf76c59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6954955
    Reviewed-by: Mickey Burks <mickeyburks@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1516799}

--- chrome/browser/ui/webui/browser_command/browser_command_handler_unittest.cc
+++ chrome/browser/ui/webui/browser_command/browser_command_handler_unittest.cc
@@ -56,6 +56,7 @@ std::vector<Command> supported_commands = {
     Command::kOpenGlic,
     Command::kOpenGlicSettings,
     Command::kPrewarmGlicFre,
+    Command::kOpenSplitView,
 };
 
 class TestCommandHandler : public BrowserCommandHandler {
@@ -225,6 +226,8 @@ class MockCommandHandler : public TestCommandHandler {
   MOCK_METHOD(void, OpenGlicSettings, ());
 
   MOCK_METHOD(void, PrewarmGlicFre, ());
+
+  MOCK_METHOD(void, OpenSplitView, ());
 };
 
 class MockCommandUpdater : public CommandUpdaterImpl {
@@ -706,4 +709,11 @@ TEST_F(BrowserCommandHandlerTest, PrewarmGlicFreCommand) {
   info->meta_key = true;
   EXPECT_CALL(*command_handler_, PrewarmGlicFre());
   EXPECT_TRUE(ExecuteCommand(Command::kPrewarmGlicFre, std::move(info)));
+}
+
+TEST_F(BrowserCommandHandlerTest, OpenSplitViewCommand) {
+  EXPECT_TRUE(CanExecuteCommand(Command::kOpenSplitView));
+  ClickInfoPtr info = ClickInfo::New();
+  EXPECT_CALL(*command_handler_, OpenSplitView());
+  EXPECT_TRUE(ExecuteCommand(Command::kOpenSplitView, std::move(info)));
 }
