c0cd8669407ca405567bc5bfb4cc4b93aa4baece
Chris Thompson <cthomp@chromium.org>
2025-09-17 07:24:21 -0700

    [LNA] Add missing early return to LNA permission check
    This change adds a missing early return for the case where the context
    is a shared or service worker with an opaque origin, which could cause
    the Mojo callback to be run twice and crash.
    
    This has been seen in crash reports, but we have not been able to
    reproduce locally or write a regression test yet so this fix is still
    somewhat speculative.
    
    Bug: 445115519
    Change-Id: I2814e9b1f57ed9b4de7412a0730cac2ef896d997
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6957378
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Auto-Submit: Chris Thompson <cthomp@chromium.org>
    Commit-Queue: Chris Thompson <cthomp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1516622}

--- content/browser/storage_partition_impl.cc
+++ content/browser/storage_partition_impl.cc
@@ -2333,6 +2333,7 @@ void StoragePartitionImpl::OnLocalNetworkAccessPermissionRequired(
     CHECK(context.worker_origin());
     if (context.worker_origin()->opaque()) {
       std::move(callback).Run(false);
+      return;
     }
 
     PermissionController& permission_controller =
