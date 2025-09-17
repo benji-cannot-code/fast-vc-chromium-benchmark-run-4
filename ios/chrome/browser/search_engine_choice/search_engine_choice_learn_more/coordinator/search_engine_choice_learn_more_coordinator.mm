89d4633f6bc33ebeb935f1b7a7f83d14e087b60e
Jérôme Lebel <jlebel@chromium.org>
2025-09-17 03:05:59 -0700

    [iOS] Move 'learn more' files out of ui_bundled
    Bypass-Check-License: Files were moved.
    Change-Id: I63e99f9cf28577fcba46dd2994bba2283b073357
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6955968
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1516525}

--- ios/chrome/browser/search_engine_choice/search_engine_choice_learn_more/coordinator/search_engine_choice_learn_more_coordinator.mm
+++ ios/chrome/browser/search_engine_choice/search_engine_choice_learn_more/coordinator/search_engine_choice_learn_more_coordinator.mm
@@ -3,13 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/search_engine_choice/ui_bundled/search_engine_choice_learn_more/search_engine_choice_learn_more_coordinator.h"
+#import "ios/chrome/browser/search_engine_choice/search_engine_choice_learn_more/coordinator/search_engine_choice_learn_more_coordinator.h"
 
+#import "ios/chrome/browser/search_engine_choice/search_engine_choice_learn_more/ui/search_engine_choice_learn_more_view_controller.h"
 #import "ios/chrome/browser/search_engine_choice/ui_bundled/search_engine_choice_constants.h"
-#import "ios/chrome/browser/search_engine_choice/ui_bundled/search_engine_choice_learn_more/search_engine_choice_learn_more_view_controller.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
 #import "ios/chrome/browser/shared/public/commands/command_dispatcher.h"
-#import "ios/chrome/browser/shared/ui/table_view/table_view_utils.h"
+// #import "ios/chrome/browser/shared/ui/table_view/table_view_utils.h"
 #import "ui/base/device_form_factor.h"
 
 @interface SearchEngineChoiceLearnMoreCoordinator () <
--- ios/chrome/browser/search_engine_choice/search_engine_choice_learn_more/ui/search_engine_choice_learn_more_view_controller.mm
+++ ios/chrome/browser/search_engine_choice/search_engine_choice_learn_more/ui/search_engine_choice_learn_more_view_controller.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/search_engine_choice/ui_bundled/search_engine_choice_learn_more/search_engine_choice_learn_more_view_controller.h"
+#import "ios/chrome/browser/search_engine_choice/search_engine_choice_learn_more/ui/search_engine_choice_learn_more_view_controller.h"
 
 #import <UIKit/UIKit.h>
 
