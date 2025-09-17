c755beb23202e6e6efc4a5f6aafc324ae2d80164
Antonio Gomes <tonikitoo@igalia.com>
2025-09-17 11:20:58 -0700

    Move arc_vm_data_migration_confirmation_dialog.* to chromeos/
    ... more specifically to
    chromeos/ash/experiences/arc/vm_data_migration/.
    
    Both files being moved have no dependencies to //chrome/browser.
    
    R=neis@chromium.org
    Bug: 443618250
    
    Bypass-Check-License: File being moved.
    Change-Id: I2dfa5bd8e35c5e33a6745b23edc5c38507cd48b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6955907
    Reviewed-by: Georg Neis <neis@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1516783}

--- chrome/browser/ui/ash/arc/arc_vm_data_migration_confirmation_dialog_unittest.cc
+++ chrome/browser/ui/ash/arc/arc_vm_data_migration_confirmation_dialog_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/ash/arc/arc_vm_data_migration_confirmation_dialog.h"
+#include "chromeos/ash/experiences/arc/vm_data_migration/arc_vm_data_migration_confirmation_dialog.h"
 
 #include "base/test/bind.h"
 #include "base/time/time.h"
