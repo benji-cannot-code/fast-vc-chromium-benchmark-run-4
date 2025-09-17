b0ee0f5382372a2d4258bad4bdfe1c26f1c7eb17
Elad Alon <eladalon@chromium.org>
2025-09-17 06:27:09 -0700

    [DSSC][8] Non-increasing sub-capture-version is non-catastrophic
    When share-this-tab-instead and sub-capture were mutually exclusive,
    stronger guarantees could be made about a monotonically increasing
    sub-capture-version, and non-increasing values could be treated as
    malicious values from the render process. But now that these two
    features can be used conjointly, such guarantees cannot be made, and the
    non-increasing sub-capture-versions should be handled with soft failure.
    
    In a follow-up, ApplySubCaptureTarget() will start using the
    CaptureVersion (rather than just the sub-capture version), which
    will allow detecting and rejecting mismatches better.
    
    Bug: 394794490
    Change-Id: I77cdb76a4cfd156525bec0c7bf0b2fca32466e0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6954334
    Reviewed-by: Jordan Bayles <jophba@chromium.org>
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1516591}

--- components/viz/service/frame_sinks/video_capture/frame_sink_video_capturer_impl_unittest.cc
+++ components/viz/service/frame_sinks/video_capture/frame_sink_video_capturer_impl_unittest.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notimplemented.h"
 #include "base/run_loop.h"
 #include "base/test/bind.h"
+#include "base/test/gtest_util.h"
 #include "base/test/test_mock_time_task_runner.h"
 #include "base/time/time.h"
 #include "base/token.h"
