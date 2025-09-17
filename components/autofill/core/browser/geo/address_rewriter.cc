eebbc5ce0a929e8fa840eee134112ee3f4c2d5d7
Piotr Domaradzki <doland@google.com>
2025-09-17 06:36:27 -0700

    Replace void pointer in AddressRewriter with a typed one.
    In order to achieve this, a series of changes were made:
    
    1. using declaration were moved to the private scope of class definition
    so that they can be used.
    
    2. Cache was made nested class so that it can use those (out from
    anonymous namespace)
    
    3. CompileRulesFromData became private static member
    
    Bug: 40100455
    Change-Id: I6d0383a4eb1103bffa7a7aba7e07154d45baae03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6954653
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Piotr Domaradzki <doland@google.com>
    Reviewed-by: Karol Sygiet <sygiet@google.com>
    Cr-Commit-Position: refs/heads/main@{#1516594}

--- components/autofill/core/browser/geo/address_rewriter.cc
+++ components/autofill/core/browser/geo/address_rewriter.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <string_view>
-#include <unordered_map>
 
 #include "base/i18n/case_conversion.h"
 #include "base/no_destructor.h"
@@ -21,11 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 namespace {
-
-// Aliases for the types used by the compiled rules cache.
-using CompiledRule = std::pair<std::unique_ptr<re2::RE2>, std::string>;
-using CompiledRuleVector = std::vector<CompiledRule>;
-using CompiledRuleCache = std::unordered_map<std::string, CompiledRuleVector>;
 
 // Helper function to convert region to mapping key string.
 std::string GetMapKey(const std::string& region) {
@@ -50,9 +44,12 @@ std::string ExtractRegionRulesData(const std::string& region) {
   return std::string();
 }
 
+}  // namespace
+
 // Helper function to populate |compiled_rules| by parsing |data_string|.
-void CompileRulesFromData(const std::string& data_string,
-                          CompiledRuleVector* compiled_rules) {
+// static
+void AddressRewriter::CompileRulesFromData(const std::string& data_string,
+                                           CompiledRuleVector* compiled_rules) {
   std::string_view data = data_string;
   re2::RE2::Options options;
   options.set_encoding(RE2::Options::EncodingUTF8);
@@ -75,7 +72,7 @@ void CompileRulesFromData(const std::string& data_string,
 // The cache of compiled string replacement rules, keyed by region. This class
 // is a singleton that compiles the rules for a given region the first time
 // they are requested.
-class Cache {
+class AddressRewriter::Cache {
  public:
   // Return the singleton instance of the cache.
   static Cache* GetInstance() {
@@ -140,8 +137,6 @@ class Cache {
   friend class base::NoDestructor<Cache>;
 };
 
-}  // namespace
-
 // static
 std::u16string AddressRewriter::RewriteForCountryCode(
     const AddressCountryCode& country_code,
@@ -180,7 +175,7 @@ std::u16string AddressRewriter::Rewrite(const std::u16string& text) const {
   // whitespace during these passes because the patterns are all whitespace
   // tolerant regular expressions.
   std::string utf8_text = base::UTF16ToUTF8(text);
-  for (const auto& rule : *static_cast<const CompiledRuleVector*>(impl_)) {
+  for (const CompiledRule& rule : *impl_) {
     RE2::GlobalReplace(&utf8_text, *rule.first, rule.second);
   }
 
