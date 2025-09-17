90371e82ca32a68baf5196ed9b404f3ab7dfeade
Piotr Domaradzki <doland@google.com>
2025-09-17 09:00:00 -0700

    Rename `impl_` to `compiled_rules_` and mark the pointer as const.
    This requires adding the constructor as well.
    
    Bug: 40100455
    Change-Id: I13b93fd98ba78b09f6a4412accd1a66791a5bfe1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6958869
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Piotr Domaradzki <doland@google.com>
    Reviewed-by: Karol Sygiet <sygiet@google.com>
    Cr-Commit-Position: refs/heads/main@{#1516690}

--- components/autofill/core/browser/geo/address_rewriter.cc
+++ components/autofill/core/browser/geo/address_rewriter.cc
@@ -137,6 +137,9 @@ class AddressRewriter::Cache {
   friend class base::NoDestructor<Cache>;
 };
 
+AddressRewriter::AddressRewriter(const CompiledRuleVector* compiled_rules)
+    : compiled_rules_(compiled_rules) {}
+
 // static
 std::u16string AddressRewriter::RewriteForCountryCode(
     const AddressCountryCode& country_code,
@@ -151,9 +154,7 @@ AddressRewriter AddressRewriter::ForCountryCode(
   const std::string region = base::ToUpperASCII(country_code.value());
   const CompiledRuleVector* rules =
       Cache::GetInstance()->GetRulesForRegion(region);
-  AddressRewriter rewriter;
-  rewriter.impl_ = rules;
-  return rewriter;
+  return AddressRewriter(rules);
 }
 
 // static
@@ -161,13 +162,11 @@ AddressRewriter AddressRewriter::ForCustomRules(
     const std::string& custom_rules) {
   const CompiledRuleVector* rules =
       Cache::GetInstance()->CreateRulesForData(custom_rules);
-  AddressRewriter rewriter;
-  rewriter.impl_ = rules;
-  return rewriter;
+  return AddressRewriter(rules);
 }
 
 std::u16string AddressRewriter::Rewrite(const std::u16string& text) const {
-  if (impl_ == nullptr) {
+  if (compiled_rules_ == nullptr) {
     return base::CollapseWhitespace(text, true);
   }
 
@@ -175,7 +174,7 @@ std::u16string AddressRewriter::Rewrite(const std::u16string& text) const {
   // whitespace during these passes because the patterns are all whitespace
   // tolerant regular expressions.
   std::string utf8_text = base::UTF16ToUTF8(text);
-  for (const CompiledRule& rule : *impl_) {
+  for (const CompiledRule& rule : *compiled_rules_) {
     RE2::GlobalReplace(&utf8_text, *rule.first, rule.second);
   }
 
