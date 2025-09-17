749c2669b671788a2597600fa30411d42fe24523
Mirko Bonadei <mbonadei@chromium.org>
2025-09-17 01:31:41 -0700

    Roll abseil_revision dc242cce41..768f75440f
    Change Log:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+log/dc242cce41..768f75440f
    Full diff:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+/dc242cce41..768f75440f
    
    Bug: None
    Change-Id: I56d9af109e4fdaae9c55d02ce2ff6cc19a952abf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6954293
    Commit-Queue: Mirko Bonadei <mbonadei@chromium.org>
    Reviewed-by: Danil Chapovalov <danilchap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1516495}

--- third_party/abseil-cpp/absl/synchronization/mutex.h
+++ third_party/abseil-cpp/absl/synchronization/mutex.h
@@ -62,17 +62,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <atomic>
 #include <cstdint>
 #include <cstring>
-#include <iterator>
-#include <string>
 
 #include "absl/base/attributes.h"
+#include "absl/base/config.h"
 #include "absl/base/const_init.h"
 #include "absl/base/internal/identity.h"
-#include "absl/base/internal/low_level_alloc.h"
 #include "absl/base/internal/thread_identity.h"
 #include "absl/base/internal/tsan_mutex_interface.h"
+#include "absl/base/macros.h"
 #include "absl/base/nullability.h"
-#include "absl/base/port.h"
 #include "absl/base/thread_annotations.h"
 #include "absl/synchronization/internal/kernel_timeout.h"
 #include "absl/synchronization/internal/per_thread_sem.h"
@@ -562,10 +560,10 @@ class ABSL_LOCKABLE ABSL_ATTRIBUTE_WARN_UNUSED Mutex {
       base_internal::PerThreadSynch* absl_nonnull w);
   void Dtor();
 
-  friend class CondVar;   // for access to Trans()/Fer().
+  friend class CondVar;                // for access to Trans()/Fer().
   void Trans(MuHow absl_nonnull how);  // used for CondVar->Mutex transfer
   void Fer(base_internal::PerThreadSynch* absl_nonnull
-           w);  // used for CondVar->Mutex transfer
+               w);  // used for CondVar->Mutex transfer
 
   // Catch the error of writing Mutex when intending MutexLock.
   explicit Mutex(const volatile Mutex* absl_nullable /*ignored*/) {}
@@ -798,7 +796,7 @@ class Condition {
   Condition(
       bool (*absl_nonnull func)(T* absl_nullability_unknown),
       typename absl::internal::type_identity<T>::type* absl_nullability_unknown
-      arg);
+          arg);
 
   // Templated version for invoking a method that returns a `bool`.
   //
@@ -1187,7 +1185,7 @@ template <typename T, typename>
 inline Condition::Condition(
     bool (*absl_nonnull func)(T* absl_nullability_unknown),
     typename absl::internal::type_identity<T>::type* absl_nullability_unknown
-    arg)
+        arg)
     // Just delegate to the overload above.
     : Condition(func, arg) {}
 
