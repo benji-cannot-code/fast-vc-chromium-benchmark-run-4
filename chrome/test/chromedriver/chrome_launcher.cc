ab8846ec24f3cc3f0397436d9e522473305559e3
Maksim Sadym <sadym@chromium.org>
2025-09-17 01:15:45 -0700

    Disable IgnoreDuplicateNavs feature in ChromeDriver.
    Alternative to reverting https://crrev.com/c/6853243. Disable the
    `IgnoreDuplicateNavs` feature for ChromeDriver sessions. Addressing
    regression mentioned in https://crbug.com/366060351#comment21.
    
    To verify the change, re-enable WPT wdspec tests.
    
    Apparently in ChromeDriver the capability switches override the existing
    switches, which is not the expected behavior. If the user needs the
    default switches to be removed, they can use `excludeSwitches`
    capability. I fixed the `Switches::SetFromSwitches` and added missing
    unit tests.
    
    Bug: 445332809
    Change-Id: Ia885f474f70dac31c299fd63dd67418c7206ead6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6949166
    Commit-Queue: Maksim Sadym <sadym@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1516493}

--- chrome/test/chromedriver/chrome_launcher.cc
+++ chrome/test/chromedriver/chrome_launcher.cc
@@ -89,6 +89,8 @@ const char* const kCommonSwitches[] = {
     embedder_support::kDisablePopupBlocking,
     "enable-automation",
     "allow-pre-commit-input",
+    // https://crbug.com/445332809.
+    "disable-features=IgnoreDuplicateNavs",
 };
 
 const char* const kDesktopSwitches[] = {
