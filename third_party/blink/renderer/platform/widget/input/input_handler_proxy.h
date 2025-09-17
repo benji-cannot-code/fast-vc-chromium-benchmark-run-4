2553807a8c81a9953d85f486b16f3c9009ab0917
Oksana Zhuravlova <oksamyt@chromium.org>
2025-09-17 11:23:04 -0700

    Improve prediction at deadline
    This CL makes the following updates to the code related to generating
    synthetic predictions at deadline:
    
    - adds a check to ensure no prediction is generated after the deadline ratio;
    - adds a timer to run DeliverInputForDeadline around the deadline ratio time in case the scheduler doesn't call it by then.
    
    Bug: 437112108
    Change-Id: I22675866a0cc44a306a18fb0e94711a5cdc50e6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6871240
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1516786}

--- third_party/blink/renderer/platform/widget/input/input_handler_proxy.h
+++ third_party/blink/renderer/platform/widget/input/input_handler_proxy.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "base/time/time.h"
+#include "base/timer/timer.h"
 #include "base/types/optional_ref.h"
 #include "cc/input/browser_controls_offset_tag_modifications.h"
 #include "cc/input/browser_controls_state.h"
@@ -474,6 +475,12 @@ class PLATFORM_EXPORT InputHandlerProxy : public cc::InputHandlerClient,
       cc::InputHandlerClient::ScrollEventDispatchMode::kEnqueueScrollEvents;
 
   double scroll_deadline_ratio_ = 0.333;
+
+  // Used to guard against re-entrant calls to DeliverInputForDeadline.
+  viz::BeginFrameId last_deadline_call_for_frame_id_;
+
+  // Timer to ensure DeliverInputForDeadline is called.
+  base::DeadlineTimer deadline_timer_;
 };
 
 }  // namespace blink
