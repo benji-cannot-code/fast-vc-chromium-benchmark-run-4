dbb4df7e9f207dc6e60dc38dca6d6ed8d296d954
Victor Hugo Vianna Silva <victorvianna@google.com>
2025-09-17 01:55:59 -0700

    [sync] Bring back presubmit warning to update proto_visitors.h
    Today I wanted to use that presubmit as inspiration for a new one
    I'm writing and then... I noticed I accidentally removed it in
    crrev.com/c/5853863. Bring it back.
    
    Bug: None
    Change-Id: I3a04f2709192986c9dd10f6557fc38792639d132
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6935344
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1516499}

---
+++ components/sync/PRESUBMIT.py
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2025 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""
+See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
+for more details about the presubmit API built into depot_tools.
+"""
+
+PRESUBMIT_VERSION = "2.0.0"
+
+
+def CheckProtoVisitorChange(input_api, output_api):
+    files = input_api.LocalPaths()
+    if (any(f.endswith(".proto") for f in files)
+            and all(not f.endswith("proto_visitors.h") for f in files)):
+        return [
+            output_api.PresubmitPromptWarning(
+                "You changed proto files, but didn\'t change proto_visitors.h")
+        ]
+
+    return []
---
+++ components/sync/PRESUBMIT_test.py
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env vpython3
+# Copyright 2025 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from pathlib import Path
+import sys
+import unittest
+
+import PRESUBMIT
+
+sys.path.append(
+    str(Path(__file__).resolve().parent / "../../third_party/depot_tools"))
+
+from testing_support.presubmit_canned_checks_test_mocks import MockInputApi, MockOutputApi, MockAffectedFile
+
+
+class CheckProtoVisitorChangeTest(unittest.TestCase):
+
+    def test_no_warning(self):
+        input_api = MockInputApi()
+        input_api.files = [
+            MockAffectedFile("components/sync/protocol/proto_visitors.h", ""),
+            MockAffectedFile("components/sync/protocol/entity_specifics.proto",
+                             ""),
+        ]
+        self.assertFalse(
+            PRESUBMIT.CheckProtoVisitorChange(input_api, MockOutputApi()))
+
+    def test_warning(self):
+        input_api = MockInputApi()
+        input_api.files = [
+            MockAffectedFile("components/sync/protocol/entity_specifics.proto",
+                             ""),
+        ]
+        self.assertTrue(
+            PRESUBMIT.CheckProtoVisitorChange(input_api, MockOutputApi()))
+
+
+if __name__ == "__main__":
+    unittest.main()
