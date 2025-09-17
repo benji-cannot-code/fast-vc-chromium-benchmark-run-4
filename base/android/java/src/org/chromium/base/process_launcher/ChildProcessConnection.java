35044635e3c91746b25dfaca3ff48c6c06c8dcd7
Mikihito Matsuura <mikt@google.com>
2025-09-16 17:58:11 -0700

    Revert "Introduce EffectiveBindingState feature for child process bindings."
    This reverts commit 980c24036ede221908a0aa682c948b7f42215ab7.
    
    Reason for revert: Compile failure on android-x86-rel
    https://ci.chromium.org/ui/p/chromium/builders/ci/android-x86-rel/123773/overview
    
    Original change's description:
    > Introduce EffectiveBindingState feature for child process bindings.
    >
    > With `EffectiveBindingState` feature enabled,
    > LegacyChildServiceConnectionController holds at most 2 bindings: waived
    > binding and effective binding set by `setEffectiveBindingState()`.
    >
    > This is to make the abstraction of ChildServiceConnectionController
    > simpler and let LegacyChildServiceConnectionController have
    > setEffectiveBindingState() instead of set/unset binding methods.
    >
    > LegacyChildServiceConnectionController continue to keep the waived
    > binding which represents the lifetime of the child process and need to
    > be alive for CountOnServiceConnectedDecorator.
    >
    > Bug: 436110326
    > Change-Id: Ibd9709a9482b3431f8bb22dbec9ed27c14ac99d9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6941421
    > Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    > Commit-Queue: Shin Kawamura <kawasin@google.com>
    > Reviewed-by: Bo Liu <boliu@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1516348}
    
    Bug: 436110326
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I516c15ef40b3fd299ed2e7e4658656ffe3720cc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6957554
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Mikihito Matsuura <mikt@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Mikihito Matsuura <mikt@google.com>
    Cr-Commit-Position: refs/heads/main@{#1516351}

--- base/android/java/src/org/chromium/base/process_launcher/ChildProcessConnection.java
+++ base/android/java/src/org/chromium/base/process_launcher/ChildProcessConnection.java
@@ -22,7 +22,6 @@ import androidx.annotation.IntDef;
 import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.ApkInfo;
-import org.chromium.base.BaseFeatureList;
 import org.chromium.base.ChildBindingState;
 import org.chromium.base.Log;
 import org.chromium.base.MemoryPressureLevel;
@@ -314,12 +313,6 @@ public class ChildProcessConnection {
     // Whether the process exited cleanly or not.
     @GuardedBy("mProcessStateLock")
     private boolean mCleanExit;
-
-    // Whether the EffectiveBindingState feature is enabled.
-    //
-    // The feature status has to stay consistent throughout the lifetime of this object, and can't
-    // have it flip half way in the middle.
-    private @Nullable Boolean mIsEffectiveBindingStateEnabled;
 
     public ChildProcessConnection(
             Context context,
@@ -971,12 +964,10 @@ public class ChildProcessConnection {
             Log.w(TAG, "The connection is not bound for %d", getPid());
             return;
         }
-        mStrongBindingCount++;
-        if (isEffectiveBindingStateEnabled()) {
-            applyEffectiveBindingState();
-        } else if (mStrongBindingCount == 1) {
+        if (mStrongBindingCount == 0) {
             mConnectionController.setStrongBinding();
         }
+        mStrongBindingCount++;
     }
 
     public void removeStrongBinding() {
@@ -986,9 +977,7 @@ public class ChildProcessConnection {
         }
         assert mStrongBindingCount > 0;
         mStrongBindingCount--;
-        if (isEffectiveBindingStateEnabled()) {
-            applyEffectiveBindingState();
-        } else if (mStrongBindingCount == 0) {
+        if (mStrongBindingCount == 0) {
             mConnectionController.unsetStrongBinding();
         }
     }
@@ -1009,12 +998,10 @@ public class ChildProcessConnection {
             Log.w(TAG, "The connection is not bound for %d", getPid());
             return;
         }
-        mVisibleBindingCount++;
-        if (isEffectiveBindingStateEnabled()) {
-            applyEffectiveBindingState();
-        } else if (mVisibleBindingCount == 1) {
+        if (mVisibleBindingCount == 0) {
             mConnectionController.setVisibleBinding();
         }
+        mVisibleBindingCount++;
     }
 
     public void removeVisibleBinding() {
@@ -1024,9 +1011,7 @@ public class ChildProcessConnection {
         }
         assert mVisibleBindingCount > 0;
         mVisibleBindingCount--;
-        if (isEffectiveBindingStateEnabled()) {
-            applyEffectiveBindingState();
-        } else if (mVisibleBindingCount == 0) {
+        if (mVisibleBindingCount == 0) {
             mConnectionController.unsetVisibleBinding();
         }
     }
@@ -1042,12 +1027,10 @@ public class ChildProcessConnection {
             Log.w(TAG, "The connection is not bound for %d", getPid());
             return;
         }
-        mNotPerceptibleBindingCount++;
-        if (isEffectiveBindingStateEnabled()) {
-            applyEffectiveBindingState();
-        } else if (mNotPerceptibleBindingCount == 1) {
+        if (mNotPerceptibleBindingCount == 0) {
             mConnectionController.setNotPerceptibleBinding();
         }
+        mNotPerceptibleBindingCount++;
     }
 
     public void removeNotPerceptibleBinding() {
@@ -1057,25 +1040,9 @@ public class ChildProcessConnection {
         }
         assert mNotPerceptibleBindingCount > 0;
         mNotPerceptibleBindingCount--;
-        if (isEffectiveBindingStateEnabled()) {
-            applyEffectiveBindingState();
-        } else if (mNotPerceptibleBindingCount == 0) {
+        if (mNotPerceptibleBindingCount == 0) {
             mConnectionController.unsetNotPerceptibleBinding();
         }
-    }
-
-    private void applyEffectiveBindingState() {
-        assert isRunningOnLauncherThread();
-        assert isConnected();
-        @ChildBindingState int effectiveBindingState = ChildBindingState.WAIVED;
-        if (mStrongBindingCount > 0) {
-            effectiveBindingState = ChildBindingState.STRONG;
-        } else if (mVisibleBindingCount > 0) {
-            effectiveBindingState = ChildBindingState.VISIBLE;
-        } else if (mNotPerceptibleBindingCount > 0) {
-            effectiveBindingState = ChildBindingState.NOT_PERCEPTIBLE;
-        }
-        mConnectionController.setEffectiveBindingState(effectiveBindingState);
     }
 
     /**
@@ -1188,13 +1155,5 @@ public class ChildProcessConnection {
         } catch (RemoteException ex) {
             // Ignore
         }
-    }
-
-    private boolean isEffectiveBindingStateEnabled() {
-        if (mIsEffectiveBindingStateEnabled != null) {
-            return mIsEffectiveBindingStateEnabled;
-        }
-        mIsEffectiveBindingStateEnabled = BaseFeatureList.sEffectiveBindingState.isEnabled();
-        return mIsEffectiveBindingStateEnabled;
     }
 }
