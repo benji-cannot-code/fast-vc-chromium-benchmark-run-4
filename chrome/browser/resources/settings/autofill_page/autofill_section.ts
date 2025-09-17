3b0bab5a8f2306f86fde051e951afe5dd0b9f114
Karol Sygiet <sygiet@google.com>
2025-09-17 00:54:29 -0700

    [AccountNameEmail] Redirect on edit in settings to name change page
    This CL is one of many that will extend H/W behaviour in desktop
    settings to work also for AccountNameEmail profile.
    
    This CL makes the edit button redirect to the Google Account personal
    info page.
    
    Mock: https://screenshot.googleplex.com/8PhZHiWKE2vf8hz
    Implementation: https://screenshot.googleplex.com/8vJQA42HVaSGvWT
    
    Recording:
    https://screencast.googleplex.com/cast/NTQxMjgxMjk3MjA5NzUzNnwyZmM0NDEwMy0zOA
    
    Bug: 441659093
    Change-Id: I1bb2bbfc2d3953f662f4c2c9b1ab3c06ceb906b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6954266
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Cr-Commit-Position: refs/heads/main@{#1516481}

--- chrome/browser/resources/settings/autofill_page/autofill_section.ts
+++ chrome/browser/resources/settings/autofill_page/autofill_section.ts
@@ -107,9 +107,9 @@ export class SettingsAutofillSectionElement extends
       showAddressDialog_: Boolean,
       showAddressRemoveConfirmationDialog_: Boolean,
 
-      isHomeOrWorkAddress: {
+      isGoogleProfileAddress: {
         type: Boolean,
-        computed: 'computeIsHomeOrWorkAddress_(activeAddress)',
+        computed: 'computeIsGoogleProfileAddress_(activeAddress)',
       },
 
       isPlusAddressEnabled_: {
@@ -125,7 +125,7 @@ export class SettingsAutofillSectionElement extends
   declare private accountInfo_: chrome.autofillPrivate.AccountInfo|null;
   declare private showAddressDialog_: boolean;
   declare private showAddressRemoveConfirmationDialog_: boolean;
-  declare private isHomeOrWorkAddress: boolean;
+  declare private isGoogleProfileAddress: boolean;
   declare private isPlusAddressEnabled_: boolean;
   private autofillManager_: AutofillManagerProxy =
       AutofillManagerImpl.getInstance();
@@ -188,11 +188,12 @@ export class SettingsAutofillSectionElement extends
    */
   private getMenuRemoveAddressLabel_(
       address: chrome.autofillPrivate.AddressEntry): string {
-    const isHomeOrWorkAddress = this.isAccountHomeAddress_(address) ||
-        this.isAccountWorkAddress_(address);
+    const isGoogleProfileAddress = this.isAccountHomeAddress_(address) ||
+        this.isAccountWorkAddress_(address) ||
+        this.isAccountNameEmailAddress_(address);
 
     return this.i18n(
-        isHomeOrWorkAddress ? 'removeFromChrome' : 'removeAddress');
+        isGoogleProfileAddress ? 'removeFromChrome' : 'removeAddress');
   }
 
   /**
@@ -231,6 +232,8 @@ export class SettingsAutofillSectionElement extends
       this.onAccountHomeAddressClick_();
     } else if (this.isAccountWorkAddress_(this.activeAddress!)) {
       this.onAccountWorkAddressClick_();
+    } else if (this.isAccountNameEmailAddress_(this.activeAddress!)) {
+      this.onAccountNameEmailAddressClick_();
     } else {
       this.showAddressDialog_ = true;
     }
@@ -308,14 +311,21 @@ export class SettingsAutofillSectionElement extends
         chrome.autofillPrivate.AddressRecordType.ACCOUNT_WORK;
   }
 
-  private computeIsHomeOrWorkAddress_(
+  private isAccountNameEmailAddress_(
+      address: chrome.autofillPrivate.AddressEntry) {
+    return address.metadata?.recordType ===
+        chrome.autofillPrivate.AddressRecordType.ACCOUNT_NAME_EMAIL;
+  }
+
+  private computeIsGoogleProfileAddress_(
       address: chrome.autofillPrivate.AddressEntry): boolean {
     if (!address) {
       return false;
     }
 
     return this.isAccountHomeAddress_(address) ||
-        this.isAccountWorkAddress_(address);
+        this.isAccountWorkAddress_(address) ||
+        this.isAccountNameEmailAddress_(address);
   }
 
   private onAccountHomeAddressClick_() {
@@ -326,6 +336,11 @@ export class SettingsAutofillSectionElement extends
   private onAccountWorkAddressClick_() {
     OpenWindowProxyImpl.getInstance().openUrl(
         this.i18n('googleAccountWorkAddressUrl'));
+  }
+
+  private onAccountNameEmailAddressClick_() {
+    OpenWindowProxyImpl.getInstance().openUrl(
+        this.i18n('googleAccountNameEmailAddressEditUrl'));
   }
 
   private isCloudOffVisible_(
