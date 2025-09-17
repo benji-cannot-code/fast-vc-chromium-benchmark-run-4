3aeba291ccdd68f3e803fd3f52f76ee49f466768
Christian Xu <christianxu@google.com>
2025-09-17 02:49:16 -0700

    [iOS][Omnibox] Add OmniboxTextViewIOS
    Add OmniboxTextViewIOS which is mostly a copy of OmniboxTextFieldIOS.
    Diff with OmniboxTextFieldIOS:
    https://diff.googleplex.com/#key=YHftx2m0wqlz The text view is used in
    AIM prototype.
    
    BYPASS_RECITATION_REASON=OmniboxTextViewIOS is a mostly a copy of OmniboxTextFieldIOS that is probably in the training data.
    
    Bug: 442370399
    Change-Id: Ia9f33406b3013df57cc38da442f9b521796024ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6949172
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1516519}

--- ios/chrome/browser/omnibox/ui/omnibox_text_input.h
+++ ios/chrome/browser/omnibox/ui/omnibox_text_input.h
@@ -50,6 +50,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /// Returns the underlying view of the text input.
 - (UIView*)view;
 
+/// Returns the view used for aligning the icon, thumbnail and clear button.
+- (UIView*)viewForVerticalAlignment;
+
 /// Returns the text input responder for Scribble.
 - (UIResponder<UITextInput>*)scribbleInput;
 
