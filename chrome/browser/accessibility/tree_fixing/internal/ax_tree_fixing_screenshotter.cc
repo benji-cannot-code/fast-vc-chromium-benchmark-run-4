a5bf2b1bc7e251d8ae35eaf3ffa755c7bbaec745
Lukasz Anforowicz <lukasza@chromium.org>
2025-09-17 08:53:01 -0700

    [rust png] Remove unused `SkStream.h` and `SkPngEncoder.h` `#include`s.
    This CL is part of a bigger change to stop using `libpng`-based Skia
    APIs (e.g. `SkPngDecoder` or `SkPngEncoder`) and instead use equivalent
    Rust-based Skia APIs (e.g. `SkPngRustDecoder` or `SkPngRustEncoder`).
    
    This CL has been created by `git cl split`.  The original, not-yet-split
    CL can be found at https://crrev.com/c/6931171.
    
    This CL was uploaded by git cl split.
    
    R=benjamin.beaudry@microsoft.com
    
    Bug: 443128323
    Change-Id: Ic748501c93e4a1458e00de16dbfc03844bc48cb3
    AX-Relnotes: n/a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6944415
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1516678}

--- chrome/browser/accessibility/tree_fixing/internal/ax_tree_fixing_screenshotter.cc
+++ chrome/browser/accessibility/tree_fixing/internal/ax_tree_fixing_screenshotter.cc
@@ -17,8 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/paint_preview/browser/compositor_utils.h"
 #include "components/paint_preview/browser/paint_preview_base_service.h"
 #include "components/paint_preview/common/recording_map.h"
-#include "third_party/skia/include/core/SkStream.h"
-#include "third_party/skia/include/encode/SkPngEncoder.h"
 #include "ui/gfx/geometry/rect.h"
 
 constexpr size_t kMaxPerCaptureSizeBytes = 50 * 1000L * 1000L;  // 50 MB.
