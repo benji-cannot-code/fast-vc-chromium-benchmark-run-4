d06d92f5d91a3131d30e524083d3085794164698
Luchen Peng <luchenpeng@google.com>
2025-09-17 03:30:38 -0700

    [Cleanup] Cleanup AutofillFixSplitCreditCardImport flag.
    Bug: 392179445
    Change-Id: I62872e77c054ef0f638681cfc6a3445159a949ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6954287
    Commit-Queue: Luchen Peng <luchenpeng@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1516533}

--- components/autofill/core/browser/form_import/form_data_importer_unittest.cc
+++ components/autofill/core/browser/form_import/form_data_importer_unittest.cc
@@ -4074,9 +4074,6 @@ class FormDataImporterTest_ExtractCreditCardFromForm
 
   FormStructure form_{/*form=*/{}};
 
- private:
-  base::test::ScopedFeatureList scoped_feature_list_{
-      features::kAutofillFixSplitCreditCardImport};
 };
 
 // Tests that inconsistent values from different priority classes do not prevent
