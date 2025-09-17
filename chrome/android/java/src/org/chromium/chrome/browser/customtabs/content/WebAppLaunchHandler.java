bfaa3cc5ab48fc4d88e724cc69cfef8b734e5dec
Oleksii Tkachenko <tkachenkoo@google.com>
2025-09-17 08:55:21 -0700

    Verify web contents is not destroyed before notify launch queue JNI call
    Bug: 425420782
    Change-Id: I166d52d6abd88d79ee030534d091a494029801bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6954765
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Serhii Selyvon <sselyvon@google.com>
    Commit-Queue: Oleksii Tkachenko <tkachenkoo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1516682}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/content/WebAppLaunchHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/content/WebAppLaunchHandler.java
@@ -307,6 +307,11 @@ public class WebAppLaunchHandler extends WebContentsObserver {
                                 return;
                             }
 
+                            if (mWebContents.isDestroyed()) {
+                                Log.w(TAG, "Web contents was destroyed.");
+                                return;
+                            }
+
                             WebAppLaunchHandlerJni.get()
                                     .notifyLaunchQueue(
                                             mWebContents,
