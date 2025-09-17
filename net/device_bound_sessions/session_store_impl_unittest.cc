b10e695f48dba55fc82117268440be6356c83bce
thefrog <thefrog@chromium.org>
2025-09-17 13:07:56 -0700

    Increment persisted sessions schema version for Origin Trial 2
    This prevents reuse of OT 1 sessions during OT 2. Note that the OT 1
    sessions will be wiped upon database read.
    
    Fixed: 444058245
    Change-Id: I293ce144baea9568f8c61f98d1821a3f4c2ffa96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6959295
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1516838}

--- net/device_bound_sessions/session_store_impl_unittest.cc
+++ net/device_bound_sessions/session_store_impl_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/scoped_temp_dir.h"
 #include "base/strings/string_util_internal.h"
 #include "base/test/bind.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
 #include "base/test/test_future.h"
 #include "base/types/expected.h"
@@ -19,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/unexportable_keys/unexportable_key_task_manager.h"
 #include "crypto/scoped_fake_unexportable_key_provider.h"
 #include "crypto/unexportable_key.h"
+#include "net/base/features.h"
 #include "net/base/schemeful_site.h"
 #include "net/device_bound_sessions/proto/storage.pb.h"
 #include "net/dns/public/secure_dns_mode.h"
@@ -200,6 +202,9 @@ class SessionStoreImplTest : public testing::Test {
             }));
     run_loop.Run();
   }
+
+ protected:
+  base::test::ScopedFeatureList feature_list_;
 
  private:
   base::test::TaskEnvironment task_environment_;
@@ -408,12 +413,57 @@ TEST_F(SessionStoreImplTest, LoadSavedSessions) {
   MimicRestart();
 
   SessionStore::SessionsMap loaded_sessions = LoadSessions();
+  EXPECT_FALSE(loaded_sessions.empty());
   // Restore the binding keys in the store session objects.
   for (auto& [key, session] : loaded_sessions) {
     RestoreSessionBindingKey(key.site, session.get());
   }
 
   EXPECT_TRUE(SessionMapsAreEqual(saved_sessions, loaded_sessions));
+}
+
+TEST_F(SessionStoreImplTest, DropLowerSchemaVersionSessions) {
+  feature_list_.InitAndDisableFeature(
+      features::kDeviceBoundSessionsOriginTrialFeedback);
+  CreateStoreAndLoadSessions();
+  SessionCfgList cfgs = {
+      {"https://a.foo.test/index.html", "session0", "https://foo.test"},
+      {"https://b.foo.test/index.html", "session1", "https://foo.test"},
+      {"https://c.bar.test/index.html", "session2", "https://bar.test"},
+  };
+
+  SessionStore::SessionsMap saved_sessions =
+      CreateAndSaveSessions(cfgs, unexportable_key_service(), store());
+
+  feature_list_.Reset();
+  feature_list_.InitAndEnableFeature(
+      features::kDeviceBoundSessionsOriginTrialFeedback);
+  MimicRestart();
+
+  SessionStore::SessionsMap loaded_sessions = LoadSessions();
+  EXPECT_TRUE(loaded_sessions.empty());
+}
+
+TEST_F(SessionStoreImplTest, DropHigherSchemaVersionSessions) {
+  feature_list_.InitAndEnableFeature(
+      features::kDeviceBoundSessionsOriginTrialFeedback);
+  CreateStoreAndLoadSessions();
+  SessionCfgList cfgs = {
+      {"https://a.foo.test/index.html", "session0", "https://foo.test"},
+      {"https://b.foo.test/index.html", "session1", "https://foo.test"},
+      {"https://c.bar.test/index.html", "session2", "https://bar.test"},
+  };
+
+  SessionStore::SessionsMap saved_sessions =
+      CreateAndSaveSessions(cfgs, unexportable_key_service(), store());
+
+  feature_list_.Reset();
+  feature_list_.InitAndDisableFeature(
+      features::kDeviceBoundSessionsOriginTrialFeedback);
+  MimicRestart();
+
+  SessionStore::SessionsMap loaded_sessions = LoadSessions();
+  EXPECT_TRUE(loaded_sessions.empty());
 }
 
 TEST_F(SessionStoreImplTest, PruneLoadedEntryWithInvalidSite) {
