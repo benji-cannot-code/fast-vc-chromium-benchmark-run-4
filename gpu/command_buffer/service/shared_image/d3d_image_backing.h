cb2df6913d190a2b0182fc5727057373af7ed020
vikas soni <vikassoni@chromium.org>
2025-09-17 15:23:16 -0700

    [DCSI] Implement D3D11ImageSameAdapterCopyStrategy.
    Implement a copy strategy to copy between 2 D3DImageBackings which have
    different D3D device but are on same GPU/adapter. This uses direct 1
    copy mechanism to do copy on the gpu.
    
    Refactor existing code in D3D11VideoImageCopyRepresentation to reuse the
    methods from D3DImageBackingCopyStrategy to avoid duplicate logic.
    
    Note that to copy between different GPU, we will do copy as
    gpu=>staging=>gpu, i.e., copy will be done via staging buffer (and not
    via readback/upload). That strategy will be implemented later.
    
    Bug: 434215964
    Change-Id: I38971a12e1a0dc0e43814219f555dfad2296f9ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6954674
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1516898}

--- gpu/command_buffer/service/shared_image/d3d_image_backing.h
+++ gpu/command_buffer/service/shared_image/d3d_image_backing.h
@@ -40,6 +40,7 @@ class ColorSpace;
 }  // namespace gfx
 
 namespace gpu {
+class D3D11ImageSameAdapterCopyStrategy;
 class SharedContextState;
 struct Mailbox;
 
@@ -49,6 +50,7 @@ struct Mailbox;
 class GPU_GLES2_EXPORT D3DImageBacking final
     : public ClearTrackingSharedImageBacking {
  public:
+  friend class D3D11ImageSameAdapterCopyStrategy;
   // Create a backing wrapping given D3D11 texture, optionally with a shared
   // handle and keyed mutex state. Array slice is used to specify index in
   // texture array used by video decoder.
