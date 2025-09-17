304d9104f7f3618538a84a5678ccc374e7e900a8
Matthew Denton <mpdenton@chromium.org>
2025-09-16 18:51:21 -0700

    Linux sandbox: allow MFD_NOEXEC_SEAL
    This flags sets the NX bit on the memfd region and seals it so it can
    never be set to executable. This is a security feature, and depending
    on the value of the vm.memfd_noexec sysctl, can even be the default.
    The risk of allowing it is extremely small, so just allow it.
    
    This should prevent some crashes with Pulseaudio and other third party
    libraries relying on it. So, the temporary memfd_create() allowance
    is dropped from the audio sandbox.
    
    Bug: 443300816
    Change-Id: Icd22900b3976f7c7f1f685b7b02bd4b309aa2039
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6916671
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Commit-Queue: Matthew Denton <mpdenton@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1516372}

--- sandbox/linux/seccomp-bpf-helpers/syscall_parameters_restrictions.cc
+++ sandbox/linux/seccomp-bpf-helpers/syscall_parameters_restrictions.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sandbox/linux/seccomp-bpf-helpers/sigsys_handlers.h"
 #include "sandbox/linux/seccomp-bpf/sandbox_bpf.h"
 #include "sandbox/linux/system_headers/linux_futex.h"
+#include "sandbox/linux/system_headers/linux_memfd.h"
 #include "sandbox/linux/system_headers/linux_prctl.h"
 #include "sandbox/linux/system_headers/linux_ptrace.h"
 #include "sandbox/linux/system_headers/linux_syscalls.h"
@@ -559,7 +560,13 @@ SANDBOX_EXPORT bpf_dsl::ResultExpr RestrictSockSendFlags(int sysno) {
 
 SANDBOX_EXPORT bpf_dsl::ResultExpr RestrictMemfdCreate() {
   const Arg<int> flags(1);
-  return If((flags & ~(MFD_CLOEXEC | MFD_ALLOW_SEALING)) == 0, Allow())
+  // Allow MFD_NOEXEC_SEAL since it's a security feature and in fact it's the
+  // default depending on the value of the vm.memfd_noexec sysctl. Do not allow
+  // MFD_EXEC (which allows executable mappings) or MFD_HUGETLB (which allows
+  // access to huge pages, which are a complex kernel feature with some previous
+  // security bugs).
+  return If((flags & ~(MFD_CLOEXEC | MFD_ALLOW_SEALING | MFD_NOEXEC_SEAL)) == 0,
+            Allow())
       // ChromeOS uses ~0 as the flags to check if memfd_create exists (will
       // return -EINVAL).
       // https://source.chromium.org/chromium/chromium/src/+/main:mojo/core/channel_linux.cc;drc=c4987dbe36be309f8db36cba174310cb8a23e989;l=918
