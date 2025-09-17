4d33645a331230123d6e74db6906fa7b200e56e2
Maks Orlovich <morlovich@chromium.org>
2025-09-16 20:07:23 -0700

    Webium: some small extensions UI fixes
    1) Implement disabled buttons, and their fallback to showing context
    menu. Since they're not actually expected to be properly disabled, turns
    out we don't actually need to wire up the enabled bit to the WebUI, just
    the proper icon.
    
    Also it turns out that the extension icon computation is
    super-complicated, so my attempt at caching its rasterization didn't
    work. So remove it, but avoid computing it for buttons that are not
    visible.
    
    2) Implement hiding the puzzle piece button if there are no extensions
    installed.
    
    Bug: 393144165, 444237074
    Change-Id: I89e86595ef3c1dcf302fce510467b0d77e085c89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6941304
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1516395}

--- chrome/browser/resources/webui_browser/extensions_bar.ts
+++ chrome/browser/resources/webui_browser/extensions_bar.ts
@@ -26,6 +26,14 @@ export class ExtensionsBar extends CrLitElement {
     return getCss();
   }
 
+  static override get properties() {
+    return {
+      visible: {type: Boolean, reflect: true},
+    };
+  }
+
+  accessor visible: boolean = false;
+
   private callbackRouter: PageCallbackRouter = new PageCallbackRouter();
   private handler: PageHandlerRemote = new PageHandlerRemote();
 
@@ -86,6 +94,7 @@ export class ExtensionsBar extends CrLitElement {
 
       extensionButton.requestUpdate();
     }
+    this.updateVisibility();
   }
 
   private actionRemoved(actionId: string) {
@@ -93,12 +102,17 @@ export class ExtensionsBar extends CrLitElement {
     assert(extensionButton);
     this.buttons.delete(actionId);
     extensionButton.remove();
+    this.updateVisibility();
   }
 
   private actionPoppedOut() {
     // TODO(webium): If we have an animation (which we ought to); this should
     // probably consider its timing.
     return {};
+  }
+
+  private updateVisibility() {
+    this.visible = (this.buttons.size !== 0);
   }
 
   onClick(id: string) {
