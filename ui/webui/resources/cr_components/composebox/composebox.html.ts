d111bf55b1391df81bdabc1b8f66a55299d7f931
Ananya <ananyaseelam@google.com>
2025-09-17 17:43:29 -0700

    [context-input] Place context chip carousel below text input
    Before: https://screenshot.googleplex.com/C9izaGbXCmKZpyh
    After: https://screenshot.googleplex.com/9G334p5dc4zqjFu
    
    Bug: 445459443
    Change-Id: Ic8b26019e26f3842e1e06facfd452b4fb5a9ff6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6961777
    Reviewed-by: Marlon Facey <mfacey@chromium.org>
    Commit-Queue: Ananya Seelam <ananyaseelam@google.com>
    Cr-Commit-Position: refs/heads/main@{#1516962}

--- ui/webui/resources/cr_components/composebox/composebox.html.ts
+++ ui/webui/resources/cr_components/composebox/composebox.html.ts
@@ -30,11 +30,6 @@ export function getHtml(this: ComposeboxElement) {
       @focusin=${this.handleComposeboxFocusIn_}
       @focusout=${this.handleComposeboxFocusOut_}>
     <div id="inputContainer">
-      <ntp-composebox-file-carousel
-          id="carousel"
-          .files=${Array.from(this.files_.values())}
-          @delete-file=${this.onDeleteFile_}>
-      </ntp-composebox-file-carousel>
       <div id="textContainer" part="text-container">
         <div id="iconContainer" part="icon-container">
           <cr-icon id="aimIcon" icon="composebox:searchSpark"></cr-icon>
@@ -57,6 +52,11 @@ export function getHtml(this: ComposeboxElement) {
           `: ''}
         </div>
       </div>
+      <ntp-composebox-file-carousel
+          id="carousel"
+          .files=${Array.from(this.files_.values())}
+          @delete-file=${this.onDeleteFile_}>
+      </ntp-composebox-file-carousel>
       <ntp-composebox-dropdown
           id="matches"
           role="listbox"
