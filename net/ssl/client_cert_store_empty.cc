c65bc450086688fc669a43d37a37be2f31c64403
Anatoli Hancharou <antoli@google.com>
2025-09-18 02:15:09 -0700

    Enable enterprise client certificate provisioning infrastructure for managed profiles on Android.
    This change introduces a stub `net::ClientCertStoreAndroid` which
    currently returns no certificates. This stub is integrated into
    `ProfileNetworkContextService` on Android, allowing the enterprise
    certificate provisioning service to wrap it.
    
    Bug: 432304139
    DD: go/cbe-cep-client-certs-clank-dd
    
    Change-Id: I75de7fb10e0a3ff4b4cae4990699a6f2f6f4509c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6862623
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Auto-Submit: Anatoli Hancharou <antoli@google.com>
    Commit-Queue: Anatoli Hancharou <antoli@google.com>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1517132}

---
+++ net/ssl/client_cert_store_empty.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "net/ssl/client_cert_store_empty.h"
+
+#include <utility>
+
+#include "net/cert/x509_certificate.h"
+#include "net/ssl/ssl_private_key.h"
+
+namespace net {
+
+ClientCertStoreEmpty::ClientCertStoreEmpty() = default;
+
+ClientCertStoreEmpty::~ClientCertStoreEmpty() = default;
+
+void ClientCertStoreEmpty::GetClientCerts(
+    scoped_refptr<const SSLCertRequestInfo> cert_request_info,
+    ClientCertListCallback callback) {
+  std::move(callback).Run(ClientCertIdentityList());
+}
+
+}  // namespace net
