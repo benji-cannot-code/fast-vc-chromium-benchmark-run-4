dafb8064b78d639ffa965117505c700553f7a5cd
Daniil Hrybchuk <renkens@google.com>
2025-09-18 13:29:20 -0700

    window management api: coordinates translation
    Introduce a new function for coordinates translation:
    
    1. Match coordinates to display through jni function.
     If no display is found return (null, null).
    2. Convert global dip coordinates to local pixel coordinates
     with display's density and local coordinates.
    
    `convertGlobalDipToLocalPxCoordinates` will be used instead of
    `getLocalCoordinatesPx`.
    
    Only JNI function was added to display_android_manager.cc.
    
    Bug: 400450442
    Change-Id: Idcf8e35ff2f2564f3eea86706ee295a68d2e7909
    Low-Coverage-Reason: TRIVIAL_CHANGE.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6842816
    Commit-Queue: Daniil Hrybchuk <renkens@google.com>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1517533}

--- ui/android/display_android_manager.cc
+++ ui/android/display_android_manager.cc
@@ -251,4 +251,12 @@ void DisplayAndroidManager::SetPrimaryDisplayId(
   primary_display_id_ = sdkDisplayId;
 }
 
+jint DisplayAndroidManager::GetDisplaySdkMatching(JNIEnv* env,
+                                                  jint x,
+                                                  jint y,
+                                                  jint width,
+                                                  jint height) {
+  return GetDisplayMatching(gfx::Rect(x, y, width, height)).id();
+}
+
 }  // namespace ui
