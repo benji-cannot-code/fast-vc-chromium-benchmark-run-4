1044ec57c6bdf442a733f21a786c7219e2f0fc2f
thomas lukaszewicz <tluk@chromium.org>
2025-09-18 11:13:22 -0700

    [mac] Ensure CloseWindowNow() is always delivered to the app-shim
    The current expectation of the app-shim process hosting remote mac
    windows is that the chrome client explicitly close all open windows
    before resetting its ApplicationHost remote - which initiates the
    termination of the app-shim process. This expectation is captured
    in DCHECKs such as [1].
    
    There are currently circumstances where chrome is not guaranteed
    to close the remote window during app teardown. An example of such
    a case is as follows.
    
    1. Clients instantiate a mac app
        This creates an associated app-shim process that will be used
        to host the app's windows
    
    2. Clients instantiate one or many views::Widgets for the app
        This NativeWidgetMac / NativeWidgetMacNSWindowHosts are created
        for each views::Widget
        Note: These widgets may be instantiated with the
        CLIENT_OWNS_WIDGET ownership model (current recommendation)
    
    3. The final Browser Widget for a given app is closed and the browser
       process begins cleaning up state associated with the app-shim
       process [2]
        Note: under CLIENT_OWNS_WIDGET destruction of the Browser does
        not guarantee destruction of its NativeWidget
    
    4. NativeWidgetMacNSWindowHost::OnApplicationHostDestroying() [3] is
       invoked as part of the cleanup, which simulates events that
       would otherwise have been propagated if the NSWindow was closed
       in the app-shim.
         Note: This terminates the remote to the window in the app-shim
         without sending an explicit window-close message to the app-shim
    
    Step #4 triggers a DCHECK crash since it violates the expectation that
    Chrome closes all remote windows before terminating the app-shim.
    
    This CL ensures that any native widgets cleaned up in
    OnApplicationHostDestroying() propagate the expected CloseWindowNow()
    message before the app-shim remote is finally reset.
    
    Note: This addresses the currently observed issues acutely. Longer
    term the expectation that browser client should close all windows in
    the app-shim should be eliminated since it works counter to how
    CLIENT_OWNS_WIDGET is expected to work.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:components/remote_cocoa/app_shim/native_widget_ns_window_bridge.mm;l=385;drc=16e216bfbd7e86012e4d15f8ed2b4be4526c3df9
    
    [2] https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/apps/app_shim/app_shim_manager_mac.cc;l=1737;drc=6431d6c5df2665c8f749afc4d9244b61226fe317
    
    [3] https://source.chromium.org/chromium/chromium/src/+/main:ui/views/cocoa/native_widget_mac_ns_window_host.mm;l=1028;drc=4dfba5e700151c6274b5cb74cac69fbda954d52d
    
    To test open and close a chrome app window - the app should close
    cleanly with no app-shim process crashes.
    
    Bug: 443168930
    Change-Id: Idb51da262638b92fc9ef97f2e154d09a0b15c977
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6963238
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1517435}

--- ui/views/cocoa/native_widget_mac_ns_window_host.mm
+++ ui/views/cocoa/native_widget_mac_ns_window_host.mm
@@ -1042,6 +1042,12 @@ void NativeWidgetMacNSWindowHost::OnApplicationHostDestroying(
   while (!children_.empty()) {
     children_.front()->OnApplicationHostDestroying(host);
   }
+  // Allow the process hosting this window to clean away any window state in its
+  // environment before the mojo remote is destroyed and the host process is
+  // terminated.
+  if (GetNSWindowMojo()) {
+    GetNSWindowMojo()->CloseWindowNow();
+  }
   OnWindowHasClosed();
 }
 
