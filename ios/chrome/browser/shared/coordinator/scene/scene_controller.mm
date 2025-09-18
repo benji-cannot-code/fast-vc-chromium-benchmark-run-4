ba961c1fc4117b54350428b32c4ff367c3054c90
Federica Germinario <fedegermi@google.com>
2025-09-18 04:29:56 -0700

    [ios] Make sure no code is executed after switching profile
    Code after handleExternalIntents should not be run if a profile switch
    happened (and the scene was disconnected).
    
    Fixed: 443728200
    Change-Id: I3d43ad0612741aa11a8017e976e8524bb4af5d04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6954770
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1517176}

--- ios/chrome/browser/shared/coordinator/scene/scene_controller.mm
+++ ios/chrome/browser/shared/coordinator/scene/scene_controller.mm
@@ -679,9 +679,9 @@ void OnListFamilyMembersResponse(
   }
 }
 
-- (void)handleExternalIntents {
+- (BOOL)handleExternalIntents {
   if (![self canHandleIntents]) {
-    return;
+    return NO;
   }
   UserActivityBrowserAgent* userActivityBrowserAgent =
       UserActivityBrowserAgent::FromBrowser(self.currentInterface.browser);
@@ -707,7 +707,7 @@ void OnListFamilyMembersResponse(
     WidgetContext* context = [self findContextRequiringAccountChange:contexts];
     // Perform profile switching if needed.
     if ([self changeProfileForContext:context contexts:contexts openURL:NO]) {
-      return;
+      return YES;
     }
   }
 
@@ -786,6 +786,7 @@ void OnListFamilyMembersResponse(
       [self showToastWhenOpenExternalIntentInUnexpectedMode];
     }
   }
+  return NO;
 }
 
 // Handles a tab move activity as part of an intent when launching a
@@ -1253,9 +1254,11 @@ void OnListFamilyMembersResponse(
       [self tryPresentSigninUpgradePromo];
     }
 
-    // TODO(crbug.com/443728200): handleExternalIntents may change profile, code
-    // below should not be executed if profile was changed.
-    [self handleExternalIntents];
+    if ([self handleExternalIntents]) {
+      // handleExternalIntents may change profile, don't execute code below if
+      // profile was changed.
+      return;
+    }
 
     if (!initializingUIInColdStart &&
         transitionedToForegroundActiveFromBackground &&
