53a29292b84abab5d5538587a6966d7ac3cff0bd
David Awogbemila <awogbemila@chromium.org>
2025-09-18 12:49:46 -0700

    [animation-trigger] Remove obsolete trigger_names_list_ field
    CSSAnimationData::trigger_names_list_ is now obsolete because the
    format for animation-trigger is no longer a list of names (dashed
    idents).
    
    Instead, animation-trigger now specifies which triggers an animation
    should be attached to via trigger()s function which are represented by
    CSSAnimationData::trigger_attachments_list_.
    
    Bug: 441408561
    Change-Id: I41a7d0fe1c4ed18ad9e87f3fb2e3eadff9a40f27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6960429
    Commit-Queue: David Awogbemila <awogbemila@chromium.org>
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1517507}

--- third_party/blink/renderer/core/animation/css/css_animation_data.h
+++ third_party/blink/renderer/core/animation/css/css_animation_data.h
@@ -21,7 +21,6 @@ namespace blink {
 
 class CORE_EXPORT CSSAnimationData final : public CSSTimingData {
  public:
-  using TriggerNamesListType = Vector<std::optional<Vector<AtomicString>>>;
   using TriggerAttachmentsListType =
       HeapVector<Member<const StyleTriggerAttachmentVector>>;
 
@@ -94,9 +93,6 @@ class CORE_EXPORT CSSAnimationData final : public CSSTimingData {
   }
   const StyleTimeline& GetTimelineTriggerTimeline(size_t index) const;
 
-  const TriggerNamesListType& TriggerNamesList() const {
-    return trigger_names_list_;
-  }
   const TriggerAttachmentsListType& TriggerAttachmentsList() const {
     return trigger_attachments_list_;
   }
@@ -147,7 +143,6 @@ class CORE_EXPORT CSSAnimationData final : public CSSTimingData {
   Vector<StyleTimeline>& TimelineTriggerTimelineList() {
     return timeline_trigger_timeline_list_;
   }
-  TriggerNamesListType& TriggerNamesList() { return trigger_names_list_; }
   TriggerAttachmentsListType& TriggerAttachmentsList() {
     return trigger_attachments_list_;
   }
@@ -200,9 +195,6 @@ class CORE_EXPORT CSSAnimationData final : public CSSTimingData {
     return TimelineOffsetOrAuto();
   }
   static const StyleTimeline& InitialTimelineTriggerTimeline();
-  static std::optional<Vector<AtomicString>> InitialTriggerNames() {
-    return std::nullopt;
-  }
   static Member<StyleTriggerAttachmentVector> InitialTriggerAttachments() {
     return nullptr;
   }
@@ -225,11 +217,6 @@ class CORE_EXPORT CSSAnimationData final : public CSSTimingData {
   Vector<TimelineOffsetOrAuto> timeline_trigger_exit_range_start_list_;
   Vector<TimelineOffsetOrAuto> timeline_trigger_exit_range_end_list_;
   Vector<StyleTimeline> timeline_trigger_timeline_list_;
-
-  // Note that this is a list of a list of names as animation-trigger specifies
-  // a comma-separated list of space-separated lists of dashed idents.
-  // TODO: Delete this deprecated member and associated methods.
-  TriggerNamesListType trigger_names_list_;
 
   TriggerAttachmentsListType trigger_attachments_list_;
 };
