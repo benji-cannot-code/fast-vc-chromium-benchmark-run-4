3a7c347a011fff8430df450843b8833fa9537acb
Joemer Ramos <joemerramos@google.com>
2025-09-18 09:05:39 -0700

    [iOS] Base Model-View-Coordinator For Location Bar Badge
    Creates a component to consolidate location bar badge and badge chip use
    from different features such as Reader Mode, Contextual Panel, and now
    AI Hub. This refactor is being made so that the badge and badge chips
    have a central component for communication. It also will remove bloat
    from delegate layer calls in LocationBarSteadyView.
    
    Bug: 445719031
    Change-Id: I5717ce86194a27d0b00a6decee495ec4870ea21a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6961107
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@google.com>
    Cr-Commit-Position: refs/heads/main@{#1517339}

---
+++ ios/chrome/browser/location_bar/ui/badge/location_bar_badge_coordinator.mm
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/location_bar/ui/badge/location_bar_badge_coordinator.h"
+
+#import "ios/chrome/browser/location_bar/ui/badge/location_bar_badge_mediator.h"
+#import "ios/chrome/browser/location_bar/ui/badge/location_bar_badge_view_controller.h"
+
+@implementation LocationBarBadgeCoordinator {
+  LocationBarBadgeViewController* _viewController;
+  LocationBarBadgeMediator* _mediator;
+}
+
+- (void)start {
+  _viewController = [[LocationBarBadgeViewController alloc] init];
+  _mediator = [[LocationBarBadgeMediator alloc] init];
+}
+
+- (void)stop {
+  _viewController = nil;
+  _mediator = nil;
+}
+
+@end
