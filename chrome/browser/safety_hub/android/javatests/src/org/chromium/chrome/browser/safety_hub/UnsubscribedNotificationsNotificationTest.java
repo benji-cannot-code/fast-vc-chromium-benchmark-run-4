e84f9b2114d0699c3d9c5a0ec1b56c72657945e4
Antonio Sartori <antoniosartori@chromium.org>
2025-09-18 04:26:29 -0700

    [DNS] Restrict flaky UI tests to new Android versions
    For some reason, these tests are flaky on older Android versions.
    
    Bug: 441276761
    Change-Id: Id893d962711b98d3cbdb5b7f5be57c86fa6d2804
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6964793
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Zaina Al-Mashni <zalmashni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1517174}

--- chrome/browser/safety_hub/android/javatests/src/org/chromium/chrome/browser/safety_hub/UnsubscribedNotificationsNotificationTest.java
+++ chrome/browser/safety_hub/android/javatests/src/org/chromium/chrome/browser/safety_hub/UnsubscribedNotificationsNotificationTest.java
@@ -10,6 +10,7 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import android.app.Notification;
+import android.os.Build;
 
 import androidx.test.filters.SmallTest;
 import androidx.test.runner.lifecycle.Stage;
@@ -24,6 +25,7 @@ import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features;
+import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
@@ -46,6 +48,8 @@ import java.util.List;
 })
 @Batch(Batch.PER_CLASS)
 @Restriction(DeviceRestriction.RESTRICTION_TYPE_NON_AUTO)
+// For some reason these tests are flaky on earlier versions of Android.
+@MinAndroidSdkLevel(Build.VERSION_CODES.S)
 public final class UnsubscribedNotificationsNotificationTest {
     @Rule public NotificationTestRule mNotificationTestRule = new NotificationTestRule();
 
