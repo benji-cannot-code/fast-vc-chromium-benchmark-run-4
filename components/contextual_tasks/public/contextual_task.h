bcc216dd15bdb62f4118503891edcd7af5334d1f
Min Qin <qinmin@chromium.org>
2025-09-18 12:52:25 -0700

    Rename chat to Thread in ContextualTask
    Change-Id: I5c78ab3bc2e775753b85b4976bacf04f64bc9102
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6955656
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1517510}

--- components/contextual_tasks/public/contextual_task.h
+++ components/contextual_tasks/public/contextual_task.h
@@ -15,20 +15,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace contextual_tasks {
 
-enum class ChatType {
+enum class ThreadType {
   kAiMode,
 };
 
 // Represents a server-side conversation that is part of a `ContextualTask`.
-struct Chat {
-  Chat(ChatType type, const std::string& server_id, const std::string& title);
-  ~Chat();
+struct Thread {
+  Thread(ThreadType type,
+         const std::string& server_id,
+         const std::string& title);
+  ~Thread();
 
   // The type of conversation.
-  ChatType type;
+  ThreadType type;
   // The server-side ID of the conversation.
   std::string server_id;
-  // Title of the chat that will be displayed to user.
+  // Title of the thread that will be displayed to user.
   std::string title;
 };
 
@@ -48,17 +50,17 @@ class ContextualTask {
   // Returns the unique ID of the task.
   const base::Uuid& GetTaskId() const;
 
-  // Adds the server-side conversation to the task. If a task already has a chat
-  // attached to it, it will be overwritten.
-  void AddChat(ChatType type,
-               const std::string& server_id,
-               const std::string& title);
+  // Adds the server-side conversation to the task. If a task already has a
+  // thread attached to it, it will be overwritten.
+  void AddThread(ThreadType type,
+                 const std::string& server_id,
+                 const std::string& title);
 
   // Removes the server-side conversation from the task.
-  void RemoveChat(ChatType type, const std::string& server_id);
+  void RemoveThread(ThreadType type, const std::string& server_id);
 
   // Returns the server-side conversation associated with the task.
-  std::optional<Chat> GetChat() const;
+  std::optional<Thread> GetThread() const;
 
   // Adds a URL to the task. If the URL already exists, this method does
   // nothing.
@@ -85,8 +87,8 @@ class ContextualTask {
   base::Uuid task_id_;
 
   // The server-side conversation associated with the task.
-  // When we persist this, we need to ensure we support up to N Chats.
-  std::optional<Chat> chat_;
+  // When we persist this, we need to ensure we support up to N Threads.
+  std::optional<Thread> thread_;
 
   // URLs relevant to the task.
   std::vector<GURL> urls_;
