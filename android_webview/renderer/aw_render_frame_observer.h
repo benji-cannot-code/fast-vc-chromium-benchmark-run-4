2525f44f365a49e52a5671f8ced588ccc583c744
Ziad Youssef <ziadyoussef@chromium.org>
2025-09-18 13:35:50 -0700

    Delay orderfile recording for WebView Renderer Process
    We optimize for pageload time by starting the address recording for the
    orderfile as soon as navigation starts instead of as soon as the library
    is loaded. We tried this approach during orderfile field experiment
    using https://chromium-review.googlesource.com/c/chromium/src/+/5756622
    
    This change is no-op if orderfile instrumentation is disabled. orderfile
    instrumentation is enabled only to generate the orderfile during the
    build process.
    
    Currently, this doesn't affect the used orderfile because the current
    orderfile is only generated from Clank benchmarks. This change will feed
    into the work to let WebView use its own orderfiles generated from
    WebView benchmarks.
    
    Bug: 444069309
    Change-Id: I894fde2a1ff6df4e91ca6dd4f0327590ca6ff051
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6931251
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Commit-Queue: Ziad Youssef <ziadyoussef@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1517540}

---
+++ android_webview/renderer/aw_render_frame_observer.h
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ANDROID_WEBVIEW_RENDERER_AW_RENDER_FRAME_OBSERVER_H_
+#define ANDROID_WEBVIEW_RENDERER_AW_RENDER_FRAME_OBSERVER_H_
+
+#include "content/public/renderer/render_frame_observer.h"
+
+namespace android_webview {
+
+class AwRenderFrameObserver : public content::RenderFrameObserver {
+ public:
+  explicit AwRenderFrameObserver(content::RenderFrame* render_frame);
+  ~AwRenderFrameObserver() override;
+
+ private:
+  // RenderFrameObserver implementation.
+  void DidStartNavigation(
+      const GURL& url,
+      std::optional<blink::WebNavigationType> navigation_type) override;
+  void OnDestruct() override;
+};
+
+}  // namespace android_webview
+
+#endif  // ANDROID_WEBVIEW_RENDERER_AW_RENDER_FRAME_OBSERVER_H_
