366f02c04a37f1b9fc469b2c31d9cff1d7f16fb8
Jason Hu <hujasonx@google.com>
2025-09-18 12:42:46 -0700

    [context-input] Create request id update mode for multi-context input
    Adds a new request id update mode to LensOverlayRequestIdGenerator that
    regens the UUID, and sets the sequence values to a constant value. Also
    updates the query controller to use this mode when multiple-file upload
    is enabled.
    
    Bug:445185648
    
    Change-Id: I8bc6652376ad27f9ac14fd2a1b3177f073ff2b6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6960963
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1517499}

--- components/lens/lens_overlay_request_id_generator.h
+++ components/lens/lens_overlay_request_id_generator.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/lens_server_proto/lens_overlay_server.pb.h"
 #include "third_party/lens_server_proto/lens_overlay_service_deps.pb.h"
 
-
 namespace lens {
 
 class TestLensOverlayQueryController;
@@ -50,6 +49,11 @@ enum class RequestIdUpdateMode {
   // request with a viewport screenshot, i.e. incrementing the sequence id,
   // image sequence id, long context id, and creating a new analytics id.
   kPageContentWithViewportRequest = 7,
+  // Indicates that the request id should be modified for a new context upload
+  // in a multi-context upload flow, i.e. incrementing the sequence id, image
+  // sequence id, and creating a new uuid and analytics id, regardless of the
+  // context upload mime type.
+  kMultiContextUploadRequest = 8,
 };
 
 // Manages creating lens overlay request IDs. Owned by a single Lens overlay
