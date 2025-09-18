f13106884cffa9c8e90567571fbf59d1a2c0c1dd
Colin Blundell <blundell@chromium.org>
2025-09-17 23:54:53 -0700

    [Blink] Eliminate CanvasResourceProviderSIImpl
    Moves the body of the constructors/destructor and the WeakPtrFactory
    ivar into CanvasResourceProviderSI.
    
    Bug: 352263194
    Change-Id: I26545cc0b6b5bfa0e1ebfee14acd32f3cf9a3695
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6950647
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1517084}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
@@ -557,7 +557,7 @@ class PLATFORM_EXPORT CanvasResourceProviderSharedImage
                                     const gfx::ColorSpace&,
                                     WebGraphicsSharedImageInterfaceProvider*,
                                     Delegate*);
-  ~CanvasResourceProviderSharedImage() override = default;
+  ~CanvasResourceProviderSharedImage() override;
 
   bool IsAccelerated() const final { return is_accelerated_; }
   bool SupportsDirectCompositing() const override { return true; }
@@ -635,6 +635,8 @@ class PLATFORM_EXPORT CanvasResourceProviderSharedImage
       cc::PaintImage::kInvalidContentId;
 
   bool notified_context_lost_ = false;
+  base::WeakPtrFactory<CanvasResourceProviderSharedImage> weak_ptr_factory_{
+      this};
 
   void ClearUnusedResources() override { unused_resources_.clear(); }
   void RegisterUnusedResource(
@@ -690,7 +692,7 @@ class PLATFORM_EXPORT CanvasResourceProviderSharedImage
   void MaybePostUnusedResourcesReclaimTask();
   void ClearOldUnusedResources();
 
-  virtual base::WeakPtr<CanvasResourceProviderSharedImage> CreateWeakPtr() = 0;
+  base::WeakPtr<CanvasResourceProviderSharedImage> CreateWeakPtr();
 };
 
 }  // namespace blink
