3195b75f29923065c1bb19c4bd81e24ed9227bd2
Tsuyoshi Horo <horo@chromium.org>
2025-09-17 20:47:29 -0700

    Revert "[Read aloud] Queue language requests on ChromeOS"
    This reverts commit 0e90204a9494e4d75413a7a657012ca71b3db89f.
    
    Reason for revert: Some ReadAnythingUntrustedPageHandlerTests are failing on MSan bots
    
    Bug: 445805639
    
    Original change's description:
    > [Read aloud] Queue language requests on ChromeOS
    >
    > See b/419510202 for more context. This solves 2 issues that were causing
    > languages not to be downloaded on ChromeOS unless you pressed play:
    >   1. The TTS engine gets put to sleep after inactivity, so we have to
    >      wake the engine at every language request.
    >   2. The DLC cannot reliably handle more than one language request at
    >      a time, so we queue up the language requests and only request the
    >      next one when the previous one returns.
    >
    > Fixed: b/419510202
    > Change-Id: I131f63d00c2048ae241e4b97c51d861630041b48
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6952833
    > Commit-Queue: Kristi Saney <kristislee@google.com>
    > Reviewed-by: Lauren Winston <lwinston@google.com>
    > Reviewed-by: Nasko Oskov <nasko@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1517004}
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I4ffb7261431693b68a1a5f8c76455ff84146d8ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6963880
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Owners-Override: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1517041}

--- chrome/test/data/webui/side_panel/read_anything/voice_language_conversions_test.ts
+++ chrome/test/data/webui/side_panel/read_anything/voice_language_conversions_test.ts
@@ -42,9 +42,6 @@ suite('voice and language conversions', () => {
     assertEquals(
         VoicePackServerStatusErrorCode.UNSUPPORTED_PLATFORM,
         mojoVoicePackStatusToVoicePackStatusEnum('kUnsupportedPlatform').code);
-    assertEquals(
-        VoicePackServerStatusErrorCode.NOT_REACHED,
-        mojoVoicePackStatusToVoicePackStatusEnum('kNotReached').code);
   });
 
   test('convertLangOrLocaleForVoicePackManager', () => {
