8857c1dbd2a1ac9c585e9e5e6afb3e5d5df8d00c
Louis Romero <lpromero@google.com>
2025-09-18 06:42:15 -0700

    Fix typo in Lens Overlay delegate method names.
    The methods `lensOverlayMediatorOpenURLInNewTabRequsted` and
    `lensResultPageOpenURLInNewTabRequsted` have been renamed to
    `lensOverlayMediatorOpenURLInNewTabRequested` and
    `lensResultPageOpenURLInNewTabRequested` respectively, correcting a
    common misspelling.
    
    Change-Id: I17f18f1e820faf0f3d1de9b0ab09e2f985e657ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6965697
    Commit-Queue: Louis Romero <lpromero@google.com>
    Reviewed-by: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1517238}

--- ios/chrome/browser/lens_overlay/coordinator/lens_overlay_mediator.mm
+++ ios/chrome/browser/lens_overlay/coordinator/lens_overlay_mediator.mm
@@ -161,11 +161,11 @@ typedef NS_ENUM(NSUInteger, LensOverlayFilterState) {
                                      totalAnimationDuration * NSEC_PER_SEC),
                        dispatch_get_main_queue(), ^{
                          [weakSelf.delegate
-                             lensOverlayMediatorOpenURLInNewTabRequsted:URL];
+                             lensOverlayMediatorOpenURLInNewTabRequested:URL];
                        });
       } else {
         [self.delegate
-            lensOverlayMediatorOpenURLInNewTabRequsted:destinationURL];
+            lensOverlayMediatorOpenURLInNewTabRequested:destinationURL];
       }
 
       [self.metricsRecorder recordNewTabGeneratedWithSource:
@@ -444,8 +444,8 @@ typedef NS_ENUM(NSUInteger, LensOverlayFilterState) {
   [self.metricsRecorder recordNewTabGeneratedWithSource:newTabSource];
 }
 
-- (void)lensResultPageOpenURLInNewTabRequsted:(GURL)URL {
-  [self.delegate lensOverlayMediatorOpenURLInNewTabRequsted:URL];
+- (void)lensResultPageOpenURLInNewTabRequested:(GURL)URL {
+  [self.delegate lensOverlayMediatorOpenURLInNewTabRequested:URL];
 }
 
 #pragma mark - Private
--- ios/chrome/browser/lens_overlay/coordinator/lens_result_page_mediator_unittest.mm
+++ ios/chrome/browser/lens_overlay/coordinator/lens_result_page_mediator_unittest.mm
@@ -53,7 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // NO-OP
 }
 
-- (void)lensResultPageOpenURLInNewTabRequsted:(GURL)URL {
+- (void)lensResultPageOpenURLInNewTabRequested:(GURL)URL {
   _openInNewTabRequested = YES;
 }
 
