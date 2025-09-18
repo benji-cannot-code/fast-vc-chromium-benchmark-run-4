f121c6cfad11ecb2af8b9159dd5ffe663cddeba2
Andres Ricardo Perez <andresrperez@google.com>
2025-09-18 14:08:01 -0700

    Add metrics to failing GetFileHandle() in FontDataService
    FontDataServiceImpl::CreateMatchFamilyNameResult() is ending in an
    unexpected code path when GetFileHandle() fails.
    
    This CL adds metrics to capture the last Windows error when this
    happens.
    
    Bug: 40083285
    Change-Id: I56682ec9d09df14567ae8a323139b9c5ab67806d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6915463
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Andres Ricardo Perez <andresrperez@chromium.org>
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1517559}

--- components/services/font_data/font_data_service_impl.cc
+++ components/services/font_data/font_data_service_impl.cc
@@ -5,11 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/services/font_data/font_data_service_impl.h"
 
+#if BUILDFLAG(IS_WIN)
+#include <windows.h>
+#endif  // BUILDFLAG(IS_WIN)
+
 #include <algorithm>
 #include <utility>
 
 #include "base/check.h"
 #include "base/containers/heap_array.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/no_destructor.h"
 #include "base/notreached.h"
 #include "base/task/thread_pool.h"
@@ -85,6 +90,10 @@ base::File FontDataServiceImpl::GetFileHandle(SkTypeface& typeface) {
   SkString font_path;
   typeface.getResourceName(&font_path);
   if (font_path.isEmpty()) {
+#if BUILDFLAG(IS_WIN)
+    base::UmaHistogramSparse("Chrome.FontDataService.WinLastError",
+                             ::GetLastError());
+#endif  // BUILDFLAG(IS_WIN)
     return {};
   }
 
