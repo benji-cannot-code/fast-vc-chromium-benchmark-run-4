20fe674880ee5e4c1b4b530f817ab21dea6e808b
Arthur Milchior <arthurmilchior@google.com>
2025-09-18 06:37:29 -0700

    [iOS]Notes that InactiveTabsCoordinator expects the inactive browser
    This helps the reader to know that this is not the regular browser. It
    become relevant in particular when, in another CL, the settings check
    that it’s opened with the regular browser.
    
    Bug: None
    Change-Id: I151cc0e1d35ec3e358a34fd126312c54335f69e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6963113
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1517230}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/inactive_tabs/inactive_tabs_coordinator.h
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/inactive_tabs/inactive_tabs_coordinator.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(nonatomic, weak) id<TabContextMenuDelegate> tabContextMenuDelegate;
 
 // Init the inactive tabs coordinator, all parameters should *not* be nil.
+// `browser` should be the inactive browser
 - (instancetype)initWithBaseViewController:(UIViewController*)viewController
                                    browser:(Browser*)browser
                                   delegate:(id<InactiveTabsCoordinatorDelegate>)
