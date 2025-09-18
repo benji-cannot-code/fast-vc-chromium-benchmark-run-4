199898d18d706b29c1904376c2d80c6276c359bb
Henrique Nakashima <hnakashima@chromium.org>
2025-09-18 13:26:59 -0700

    [Android] Re-enable some cases in FullscreenManagerTest
    The bug causing the test failures was fixed by:
    https://chromium-review.googlesource.com/c/chromium/src/+/6944844
    
    Bug: 442852478
    Change-Id: I76606cc51a486785236a7a5336d591cd12c55b2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6966378
    Auto-Submit: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1517531}

--- chrome/android/javatests/src/org/chromium/chrome/browser/fullscreen/FullscreenManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/fullscreen/FullscreenManagerTest.java
@@ -589,7 +589,6 @@ public class FullscreenManagerTest {
         ChromeFeatureList.FULLSCREEN_INSETS_API_MIGRATION_ON_AUTOMOTIVE,
         ChromeFeatureList.EDGE_TO_EDGE_BOTTOM_CHIN
     })
-    @DisabledTest(message = "https://crbug.com/442852478")
     public void testHidingBrowserControlsPreservesScrollOffsetLegacy() throws TimeoutException {
         FullscreenManagerTestUtils.disableBrowserOverrides();
         WebPageStation page = mActivityTestRule.startOnUrl(SCROLL_OFFSET_TEST_PAGE);
@@ -639,7 +638,6 @@ public class FullscreenManagerTest {
         ChromeFeatureList.FULLSCREEN_INSETS_API_MIGRATION_ON_AUTOMOTIVE
     })
     @DisableFeatures(ChromeFeatureList.EDGE_TO_EDGE_BOTTOM_CHIN)
-    @DisabledTest(message = "https://crbug.com/442852478")
     public void testHidingBrowserControlsPreservesScrollOffset() throws TimeoutException {
         FullscreenManagerTestUtils.disableBrowserOverrides();
         WebPageStation page = mActivityTestRule.startOnUrl(SCROLL_OFFSET_TEST_PAGE);
