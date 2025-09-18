790c0c2088dcbb3de11315c61b844aabca390f54
Steve Becker <stevebe@microsoft.com>
2025-09-18 09:57:31 -0700

    Remove unused callback from MediaStreamVideoSourceCallbacks
    `state_update_cb` is always empty.
    
    Bug: 399692462
    Change-Id: I86f4be434001b8004f5df0e291cedc421b54680f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6962514
    Commit-Queue: Steve Becker <stevebe@microsoft.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1517376}

--- third_party/blink/public/web/modules/mediastream/media_stream_video_source.h
+++ third_party/blink/public/web/modules/mediastream/media_stream_video_source.h
@@ -54,7 +54,6 @@ class VideoTrackAdapterSettings;
 // `encoded_frame_cb` is called when an encoded video frame is available.
 // `settings_cb` is called when the video track settings are updated.
 // `format_cb` is called when the video track format is updated.
-// `state_update_cb` is called when the video capturer state is updated.
 struct MediaStreamVideoSourceCallbacks {
   VideoCaptureDeliverFrameCB deliver_frame_cb;
   VideoCaptureNotifyFrameDroppedCB frame_dropped_cb;
@@ -62,7 +61,6 @@ struct MediaStreamVideoSourceCallbacks {
   EncodedVideoFrameCB encoded_frame_cb;
   VideoTrackSettingsCallback settings_cb;
   VideoTrackFormatCallback format_cb;
-  VideoCaptureStateUpdateCB state_update_cb;
 };
 
 // MediaStreamVideoSource is an interface used for sending video frames to a
