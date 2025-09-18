17fdb737cdc37b0eb840827059a64a81c89bf0eb
Sylvain Defresne <sdefresne@chromium.org>
2025-09-18 06:49:18 -0700

    [ios] Remove unused import of MaterialComponents
    Bug: 443947340
    Change-Id: I61bede66705af04cc28f6eb27ba1a490a1672057
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6964801
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1517246}

--- ios/chrome/browser/authentication/ui_bundled/cells/signin_promo_view_unittest.mm
+++ ios/chrome/browser/authentication/ui_bundled/cells/signin_promo_view_unittest.mm
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/authentication/ui_bundled/cells/signin_promo_view.h"
 
-#import <MaterialComponents/MaterialOverlayWindow.h>
-
 #import "ios/chrome/browser/authentication/ui_bundled/cells/signin_promo_view_constants.h"
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
 #import "ios/chrome/browser/signin/model/signin_util.h"
