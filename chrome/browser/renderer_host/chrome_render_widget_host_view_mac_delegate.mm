b1368d673ed742f057f681baef40c49295a5764a
Keren Zhu <kerenzhu@chromium.org>
2025-09-18 08:08:50 -0700

    mac: allow DevTools to inspect elements in inactive window
    On macOS the DevTools inspection mode cannot highlight elements in an
    inactive window due to missing mouse move events. This CL fixes that by
    allowing mouse move events to go through if the content is being
    inspected. This aligns macOS with other platforms.
    
    Bug: 445624501
    Change-Id: I4ee3776502b5bc816cb756bfab1986fdbbc8127b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6959282
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1517302}

--- chrome/browser/renderer_host/chrome_render_widget_host_view_mac_delegate.mm
+++ chrome/browser/renderer_host/chrome_render_widget_host_view_mac_delegate.mm
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/spellcheck/common/spellcheck_panel.mojom.h"
 #include "components/tabs/public/tab_interface.h"
 #include "components/web_modal/web_contents_modal_dialog_manager.h"
+#include "content/public/browser/devtools_agent_host.h"
 #include "content/public/browser/preloading.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
@@ -410,6 +411,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   content::WebContents* webContents = self.webContents;
   if (!webContents) {
     return AcceptMouseEvents::kWhenInActiveWindow;
+  }
+
+  // Allow mouse move events in inactive windows when inspecting.
+  if (content::DevToolsAgentHost::IsDebuggerAttached(webContents)) {
+    return AcceptMouseEvents::kWhenInActiveApp;
   }
 
   // If this web contents is in a tab, and the tab wants to accept mouse events
