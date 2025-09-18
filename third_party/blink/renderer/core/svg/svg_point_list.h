0e3c19003d368ebbb57c35fd21ff774ccb2e20bf
Fredrik Söderquist <fs@opera.com>
2025-09-18 15:44:10 -0700

    spanification: Spanify SVGPointList::Parse()
    Bug: 351564777, 356658478
    Change-Id: I6453d07aff19cc9958cedcbd1cead63ed2a2a225
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6965076
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1517615}

--- third_party/blink/renderer/core/svg/svg_point_list.h
+++ third_party/blink/renderer/core/svg/svg_point_list.h
@@ -68,7 +68,7 @@ class SVGPointList final
 
  private:
   template <typename CharType>
-  SVGParsingError Parse(const CharType* ptr, const CharType* end);
+  SVGParsingError Parse(base::span<const CharType> span);
 };
 
 template <>
