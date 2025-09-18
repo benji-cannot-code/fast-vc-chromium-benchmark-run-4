4b2d615931449f1b8c87529ce3ccf40d9e38c037
Juan Mojica <juanmojica@google.com>
2025-09-17 19:19:17 -0700

    [lensoverlay] Remove start query flow optimization feature.
    This is a partial cleanup of the `LensOverlayLatencyOptimizations`
    feature. This is being done to remove complexity from the implementation
    to reinvoke the live page.
    
    This also removes `LensOverlayLatencyOptimizations` from the field trial
    configuration, as well as other expired studies related to the Lens
    overlay.
    
    Bug: 445698141
    Change-Id: I4695c0bf897e39e9204051763885ea4e3379fb14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6963043
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1517015}

--- components/lens/lens_features.cc
+++ components/lens/lens_features.cc
@@ -277,10 +277,6 @@ constexpr base::FeatureParam<bool> kEnableEarlyInteractionOptimization{
     &kLensOverlayLatencyOptimizations, "enable-early-interaction-optimization",
     true};
 
-constexpr base::FeatureParam<bool> kEnableEarlyStartQueryFlowOptimization{
-    &kLensOverlayLatencyOptimizations,
-    "enable-early-start-query-flow-optimization", true};
-
 constexpr base::FeatureParam<bool> kUseInnerTextAsContext{
     &kLensOverlayContextualSearchbox, "use-inner-text-as-context", true};
 
@@ -858,11 +854,6 @@ bool IsLensOverlayClusterInfoOptimizationEnabled() {
 bool IsLensOverlayEarlyInteractionOptimizationEnabled() {
   return base::FeatureList::IsEnabled(kLensOverlayLatencyOptimizations) &&
          kEnableEarlyInteractionOptimization.Get();
-}
-
-bool IsLensOverlayEarlyStartQueryFlowOptimizationEnabled() {
-  return base::FeatureList::IsEnabled(kLensOverlayLatencyOptimizations) &&
-         kEnableEarlyStartQueryFlowOptimization.Get();
 }
 
 base::TimeDelta GetLensOverlaySurveyResultsTime() {
