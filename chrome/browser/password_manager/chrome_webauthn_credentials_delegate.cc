5b658e5927214403f52f67274ff9c813a8f78711
Adem Derinel <derinel@google.com>
2025-09-18 01:27:44 -0700

    Move scheduler bits to ChromeAuthenticatorRequestDelegate
    Change-Id: Ib964ba9e5f4fcb40f1748a63090dbf2699bdcd0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6947905
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1517114}

--- chrome/browser/password_manager/chrome_webauthn_credentials_delegate.cc
+++ chrome/browser/password_manager/chrome_webauthn_credentials_delegate.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if !BUILDFLAG(IS_ANDROID)
 #include "chrome/browser/webauthn/authenticator_request_dialog_controller.h"
-#include "chrome/browser/webauthn/authenticator_request_scheduler.h"
 #include "chrome/browser/webauthn/chrome_authenticator_request_delegate.h"
 #endif  // !BUILDFLAG(IS_ANDROID)
 
@@ -61,7 +60,7 @@ ChromeWebAuthnCredentialsDelegate::~ChromeWebAuthnCredentialsDelegate() =
 void ChromeWebAuthnCredentialsDelegate::LaunchSecurityKeyOrHybridFlow() {
 #if !BUILDFLAG(IS_ANDROID)
   ChromeAuthenticatorRequestDelegate* authenticator_delegate =
-      AuthenticatorRequestScheduler::GetRequestDelegate(web_contents_);
+      ChromeAuthenticatorRequestDelegate::GetRequestDelegate(web_contents_);
   if (!authenticator_delegate) {
     return;
   }
@@ -94,7 +93,7 @@ void ChromeWebAuthnCredentialsDelegate::SelectPasskey(
   request_delegate->OnWebAuthnAccountSelected(*selected_credential_id);
 #else
   ChromeAuthenticatorRequestDelegate* authenticator_delegate =
-      AuthenticatorRequestScheduler::GetRequestDelegate(web_contents_);
+      ChromeAuthenticatorRequestDelegate::GetRequestDelegate(web_contents_);
   if (!authenticator_delegate) {
     std::move(callback).Run();
     return;
--- chrome/browser/password_manager/chrome_webauthn_credentials_delegate_unittest.cc
+++ chrome/browser/password_manager/chrome_webauthn_credentials_delegate_unittest.cc
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "chrome/browser/webauthn/authenticator_request_dialog_controller.h"
 #include "chrome/browser/webauthn/authenticator_request_dialog_model.h"
-#include "chrome/browser/webauthn/authenticator_request_scheduler.h"
 #include "chrome/browser/webauthn/chrome_authenticator_request_delegate.h"
 #include "device/fido/fido_request_handler_base.h"
 #endif  // !BUILDFLAG(IS_ANDROID)
@@ -118,7 +117,7 @@ class ChromeWebAuthnCredentialsDelegateTest
 
 #if !BUILDFLAG(IS_ANDROID)
     authenticator_request_delegate_ =
-        AuthenticatorRequestScheduler::CreateRequestDelegate(
+        ChromeAuthenticatorRequestDelegate::CreateRequestDelegate(
             web_contents()->GetPrimaryMainFrame());
     // Setting the RPID creates the dialog model.
     authenticator_request_delegate_->SetRelyingPartyId("rpId");
