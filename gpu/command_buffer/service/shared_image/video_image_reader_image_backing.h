9ac3e010627ca43ef0c6c3bef9b81452cd83f102
Eugene Zemtsov <eugene@chromium.org>
2025-09-18 10:55:32 -0700

    media: Implement ProduceVideo for VideoImageReaderImageBacking
    This make it possible for NDK VEA (in surface input mode) to consume
    frames produced by MediaCodecVideoDecoder without readback.
    
    This change depends on the VEA thread to allow basic sync primitives.
    
    Bug: 434977616
    Change-Id: I6eb5c9a41c4573359d76d84b0e46a515be32bb47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6951742
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1517415}

--- gpu/command_buffer/service/shared_image/video_image_reader_image_backing.h
+++ gpu/command_buffer/service/shared_image/video_image_reader_image_backing.h
@@ -74,6 +74,11 @@ class GPU_GLES2_EXPORT VideoImageReaderImageBacking
       gpu::SharedImageManager* manager,
       gpu::MemoryTypeTracker* tracker) override;
 
+  std::unique_ptr<VideoImageRepresentation> ProduceVideo(
+      SharedImageManager* manager,
+      MemoryTypeTracker* tracker,
+      VideoDevice device) override;
+
  private:
   // Helper class for observing SharedContext loss on gpu main thread and
   // cleaning up resources accordingly.
@@ -103,6 +108,7 @@ class GPU_GLES2_EXPORT VideoImageReaderImageBacking
   class SkiaVkVideoImageRepresentation;
   class OverlayVideoImageRepresentation;
   class LegacyOverlayVideoImageRepresentation;
+  class VideoRepresentation;
 
   std::unique_ptr<ContextLostObserverHelper> context_lost_helper_;
   scoped_refptr<StreamTextureSharedImageInterface> stream_texture_sii_;
