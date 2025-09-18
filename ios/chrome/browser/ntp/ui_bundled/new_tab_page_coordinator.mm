5b78d228725a53b7dff2015980bdea9a0e79024f
Federica Germinario <fedegermi@google.com>
2025-09-18 07:12:17 -0700

    [ios] Avoid dismissing the omnibox in ntp discover feed
    It seems that with new implementation calling cancelOmniboxEdit is not
    needed anymore.
    
    This was causing an issue sometimes when focusing the omnibox from
    widgets (with an account change) because the focus was dismissed just
    after being shown.
    
    Fixed: 445900711
    Change-Id: Ifa028fa6b667ba11910ce7ec74a8bc4e15d9262d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6964296
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1517267}

--- ios/chrome/browser/ntp/ui_bundled/new_tab_page_coordinator.mm
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_coordinator.mm
@@ -1281,7 +1281,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)updateModuleVisibility {
   [_customizationCoordinator updateMenuData];
   [self handleChangeInModules];
-  [self cancelOmniboxEdit];
   [self setContentOffsetToTop];
   [self.feedHeaderViewController updateForFeedVisibilityChanged];
   UIAccessibilityPostNotification(UIAccessibilityScreenChangedNotification,
