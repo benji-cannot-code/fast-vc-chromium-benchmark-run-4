2cadfdd9f415eca3de08eceef47d22da33a69264
Tom Sepez <tsepez@chromium.org>
2025-09-18 15:35:50 -0700

    Remove dependence on IPC::Message from url_param_traits_unittest.cc.
    Although ParamTraits will remain for serializing native classes, the
    top-level IPC message is no longer strictly necessary in Chrome. So
    re-write these tests in terms of pickles and traits.
    
    Bug: 40621529
    Change-Id: I980bbb6f7385e1f7060a2bd9d440f1862996dbab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6966028
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1517606}

--- url/ipc/url_param_traits_unittest.cc
+++ url/ipc/url_param_traits_unittest.cc
@@ -3,18 +3,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "url/ipc/url_param_traits.h"
+
 #include <string>
 
-#include "ipc/ipc_message.h"
-#include "ipc/ipc_message_utils.h"
+#include "base/pickle.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
-#include "url/ipc/url_param_traits.h"
 
 namespace {
 
 GURL BounceUrl(const GURL& input) {
-  IPC::Message msg(1, 2, IPC::Message::PRIORITY_NORMAL);
+  base::Pickle msg;
   IPC::ParamTraits<GURL>::Write(&msg, input);
 
   GURL output;
@@ -70,8 +70,9 @@ TEST(IPCMessageTest, SerializeGurl_ExcessivelyLong) {
 
 // Test of an invalid GURL.
 TEST(IPCMessageTest, SerializeGurl_InvalidUrl) {
-  IPC::Message msg;
+  base::Pickle msg;
   msg.WriteString("#inva://idurl/");
+
   GURL output;
   base::PickleIterator iter(msg);
   EXPECT_FALSE(IPC::ParamTraits<GURL>::Read(&msg, &iter, &output));
@@ -79,8 +80,9 @@ TEST(IPCMessageTest, SerializeGurl_InvalidUrl) {
 
 // Test of a corrupt deserialization input.
 TEST(IPCMessageTest, SerializeGurl_CorruptPayload) {
-  IPC::Message msg(1, 2, IPC::Message::PRIORITY_NORMAL);
+  base::Pickle msg;
   msg.WriteInt(99);
+
   GURL output;
   base::PickleIterator iter(msg);
   EXPECT_FALSE(IPC::ParamTraits<GURL>::Read(&msg, &iter, &output));
