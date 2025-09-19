d371a70fe2cc72c869b2b8c6e0a93c4bf281dfcf
Mikołaj Wałachowski <mwalachowski@google.com>
2025-09-19 07:31:45 -0700

    Add IncognitoModeBlocklist and IncognitoModeAllowlist policies.
    This change only introduces new policies and their corresponding prefs,
    they are not yet applied or supported on any platform. These policies
    will allow administrators to block or allow access to specific URLs
    within Incognito mode. `IncognitoModeBlocklist` prevents loading of
    specified URLs, while `IncognitoModeAllowlist` provides exceptions to
    the blocklist. The policies are added for all desktop platforms,
    ChromeOS, Android and iOS.
    
    Bug: 442891187
    Change-Id: I194451aa7583b4f5e2a5627cafec4f8a85c39e3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6935369
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    Commit-Queue: Mikołaj Wałachowski <mwalachowski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1517914}

--- ios/chrome/browser/shared/model/prefs/browser_prefs.mm
+++ ios/chrome/browser/shared/model/prefs/browser_prefs.mm
@@ -1025,6 +1025,9 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
 
   registry->RegisterIntegerPref(prefs::kGeminiEnabledByPolicy, 0);
 
+  registry->RegisterListPref(policy::policy_prefs::kIncognitoModeBlocklist);
+  registry->RegisterListPref(policy::policy_prefs::kIncognitoModeAllowlist);
+
   // Deprecated 11/2024.
   registry->RegisterBooleanPref(kEnableDoNotTrackIos, false);
 
