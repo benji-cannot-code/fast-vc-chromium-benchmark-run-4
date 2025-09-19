3afb737876558da85864ea70e7a533865284bb09
Peter Kasting <pkasting@chromium.org>
2025-09-18 22:13:03 -0700

    Misc. NativeTheme cleanup.
    * Shorten implementations
    * Remove AdjustCheckboxRadioRectForPadding(), which is never called
    * Make GetArrowRect() virtual so Fluent scrollbar drawing can reuse
      base code for triangular arrow case
    * Collapse color computation functions into a small number of
      functions that take color arrays. Not only is this simpler, it fixes
      more cases where callers failed to properly take custom accent
      colors into account and the like.
    * Improve comments
    * Make more of NativeThemeBase private
    * Missed yet another `extra_params` case
    * NativeThemeBaseTest should not inherit from NativeThemeBase (WTF)
    * Inline NativeThemeFluent::PaintRoundedButton() into the lone caller
      as a lambda, simplifying the signature
    * Modify Fluent typeface getter code so tests can use a proper setter
    * Float constants should have '0' after '.' (style guide)
    
    Bug: 431953872
    Change-Id: Ibab2e4891f87c3677186d1eff443e77fb8f98e46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6967631
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1517729}

--- ui/native_theme/native_theme_mobile.h
+++ ui/native_theme/native_theme_mobile.h
@@ -23,7 +23,6 @@ class NativeThemeMobile : public NativeThemeBase {
 
  protected:
   // NativeThemeBase:
-  void AdjustCheckboxRadioRectForPadding(SkRect* rect) const override;
   SkColor GetControlColor(ControlColorId color_id,
                           bool dark_mode,
                           PreferredContrast contrast,
--- ui/native_theme/native_theme_unittest.cc
+++ ui/native_theme/native_theme_unittest.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/native_theme/native_theme_observer.h"
 
 namespace ui {
-
 namespace {
 
 class NativeThemeTest : public ::testing::Test {
