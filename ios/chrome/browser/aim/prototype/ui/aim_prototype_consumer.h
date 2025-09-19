9f926ecbee767107fc82df61f596f3fe6ea632f2
Ameur Hosni <ameurhosni@google.com>
2025-09-19 08:34:10 -0700

    [IOS][AIM] Add the attach tab action only on non-ntp webpages
    Change-Id: I3a06dba58d537e3b12c93aae8bb39f9b1dc4e539
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6964834
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Auto-Submit: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1517973}

--- ios/chrome/browser/aim/prototype/ui/aim_prototype_consumer.h
+++ ios/chrome/browser/aim/prototype/ui/aim_prototype_consumer.h
@@ -23,6 +23,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Updates the mic button visibility.
 - (void)hideMicButton:(BOOL)hidden;
 
+// Sets whether the "Attach current tab" action is enabled.
+- (void)setCanAttachTabAction:(BOOL)canAttachTabAction;
+
 // Sets whether AI mode is enabled.
 - (void)setAIModeEnabled:(BOOL)AIModeEnabled;
 
