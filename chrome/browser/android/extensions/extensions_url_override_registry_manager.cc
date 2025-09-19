f7672b1ad26d0c78bf03dd9c3a13fb05dd7c570d
Fiaz Muhammad <mfiaz@google.com>
2025-09-19 07:35:31 -0700

    [URL Override] Unit Tests for State Tracker
    - This also fixes the bad BUILD.gn setup done for the tracker files.
    - New unit tests for the state tracker.
    
    Bug: 445719491
    Change-Id: Iee0175ce14b85fb8f15a60c7be5130e100794fd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6966517
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Auto-Submit: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1517921}

--- chrome/browser/android/extensions/extensions_url_override_registry_manager.cc
+++ chrome/browser/android/extensions/extensions_url_override_registry_manager.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/numerics/safe_conversions.h"
 #include "extensions/buildflags/buildflags.h"
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
+#if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 #include "chrome/browser/android/extensions/extensions_url_override_state_tracker_impl.h"
 #endif
 
@@ -40,7 +40,7 @@ void ExtensionsUrlOverrideRegistryManager::OnUrlOverrideDisabled(
 
 ExtensionsUrlOverrideRegistryManager::ExtensionsUrlOverrideRegistryManager(
     Profile* profile) {
-#if BUILDFLAG(ENABLE_EXTENSIONS)
+#if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
   state_tracker_ =
       std::make_unique<ExtensionUrlOverrideStateTrackerImpl>(profile, this);
 #endif
