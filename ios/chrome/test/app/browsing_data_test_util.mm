44702b81df46578c55835ede21bffc8aca075e52
Svend Larsen <svend@chromium.org>
2025-09-19 08:02:22 -0700

    [VL] Let callers specify 404 policy in GetHistoryCount()
    404 visits have historically not been eligible for History, so callers
    of History querying methods have not needed to consider whether 404
    visits are relevant for their feature or not. In the future, when
    history::kVisitedLinksOn404 is enabled, 404s will become eligible for
    History, and therefore be countable by VisitDatabase::GetHistoryCount(),
    and by extension the HistoryBackend and HistoryService methods of the
    same name. Some callers of GetHistoryCount() do not want to receive 404
    visits, so this CL adds a policy_for_404_visits param to the method to
    allow callers to specify whether they want 404 visits to count.
    
    This CL is part of a larger project to make links `:visited` after a 404
    visit. Project links:
    - Google-internal project doc: go/stop-filtering-history-by-status-code
    - Issue tree / work status: https://crbug.com/427225439/dependencies
    
    Fixed: 441157315
    Change-Id: Ic9f5791339c68432a8a16b7acfc1027c4b07c88b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6962734
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Ankush Singh <ankushkush@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Svend Larsen <svend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1517953}

--- ios/chrome/test/app/browsing_data_test_util.mm
+++ ios/chrome/test/app/browsing_data_test_util.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/test/ios/wait_util.h"
 #import "components/browsing_data/core/browsing_data_utils.h"
 #import "components/history/core/browser/history_service.h"
+#import "components/history/core/browser/history_types.h"
 #import "components/keyed_service/core/service_access_type.h"
 #import "ios/chrome/app/main_controller.h"
 #import "ios/chrome/browser/browsing_data/model/browsing_data_remove_mask.h"
@@ -108,6 +109,7 @@ int GetBrowsingHistoryEntryCount(NSError** error) {
   base::CancelableTaskTracker task_tracker;
   history_service->GetHistoryCount(
       base::Time::Min(), base::Time::Max(),
+      history::VisitQuery404sPolicy::kInclude404s,
       base::BindOnce(^(history::HistoryCountResult result) {
         if (result.success) {
           count = result.count;
