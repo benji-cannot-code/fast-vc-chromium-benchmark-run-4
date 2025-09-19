9ea449b83acdda06c212bfffc647cb42314d322b
courtneywong <courtneywong@google.com>
2025-09-19 16:31:52 -0700

    fjord-oobe: Update dbus message to respond with a success bool
    Test: Tested with dbus-send CLI that the correct response is returned
    Bug: b:427577944
    Change-Id: I18e30e48d5aacfbe32f75c3e6cb067bf2bab21f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6968167
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Courtney Wong <courtneywong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1518236}

--- chrome/browser/ash/login/wizard_controller_browsertest.cc
+++ chrome/browser/ash/login/wizard_controller_browsertest.cc
@@ -1469,7 +1469,7 @@ IN_PROC_BROWSER_TEST_F(WizardControllerFjordOOBETest,
   EXPECT_TRUE(StartupUtils::IsOobeCompleted());
   CheckCurrentScreen(FjordTouchControllerScreenView::kScreenId);
 
-  wizard_controller->ExitFjordTouchControllerScreen();
+  EXPECT_TRUE(wizard_controller->ExitFjordTouchControllerScreen());
   CheckCurrentScreen(FjordStationSetupScreenView::kScreenId);
 }
 
@@ -1496,7 +1496,7 @@ IN_PROC_BROWSER_TEST_F(WizardControllerFjordOOBETest,
   CheckCurrentScreen(EnrollmentScreenView::kScreenId);
 
   // Expect that Exit has no affect since TC setup screen is not showing.
-  wizard_controller->ExitFjordTouchControllerScreen();
+  EXPECT_FALSE(wizard_controller->ExitFjordTouchControllerScreen());
   CheckCurrentScreen(EnrollmentScreenView::kScreenId);
 }
 
