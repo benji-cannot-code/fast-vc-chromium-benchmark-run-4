6e247dde09d6233a66b037f3a83f1a9dfe5e4238
Qiu Jianlin <jianlin.qiu@intel.com>
2025-09-18 17:24:52 -0700

    [DX12 Enc] Rely on BRC reconfigure for AV1 per-frame-qp encoding.
    base_q_idx is not intented for configuring per-frame-qp. Drivers
    basically first overlook the BRC configuration. At the same time
    we configure base_q_idx in picture param to make sure if some IHV
    drivers are also looking at it.
    
    Bug: 40275246, 430338394
    Change-Id: I17b7e16916bd2ed49ce0476574ddd9fac790d822
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6963006
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Jianlin Qiu <jianlin.qiu@intel.com>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1517669}

--- media/gpu/windows/d3d12_video_encode_av1_delegate.cc
+++ media/gpu/windows/d3d12_video_encode_av1_delegate.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 #include <bit>
+#include <optional>
 
 #include "base/containers/fixed_flat_map.h"
 #include "base/numerics/safe_conversions.h"
@@ -807,6 +808,7 @@ EncoderStatus D3D12VideoEncodeAV1Delegate::InitializeVideoEncoder(
       FillAV1BuilderSequenceHeader(GetNumTemporalLayers(), profile, input_size_,
                                    tier_level, enabled_features_);
   picture_id_ = -1;
+  current_rate_control_ = rate_control_;
 
   return EncoderStatus::Codes::kOk;
 }
@@ -926,7 +928,7 @@ void D3D12VideoEncodeAV1Delegate::FillPictureControlParams(
         request_keyframe ? 0xFF : 1 << (libgav1::kReferenceFrameLast - 1);
   }
 
-  int qindex = AV1QPtoQindex(kAV1MaxQuantizer);
+  std::optional<int> qindex;
   if (software_brc_) {
     aom::AV1FrameParamsRTC frame_params{
         .frame_type = request_keyframe ? aom::kKeyFrame : aom::kInterFrame,
@@ -940,10 +942,32 @@ void D3D12VideoEncodeAV1Delegate::FillPictureControlParams(
         std::clamp(static_cast<uint8_t>(options.quantizer.value()),
                    kAV1MinQuantizer, kAV1MaxQuantizer));
   }
-  picture_params_.Quantization.BaseQIndex = qindex;
+  const int base_q_idx =
+      std::clamp(qindex.value_or(AV1QPtoQindex(kAV1MaxQuantizer)), 0, 255);
+  picture_params_.Quantization.BaseQIndex = base_q_idx;
   DVLOG(4) << base::StringPrintf(
       "Encoding picture: %d, is_keyframe = %d, QP = %d", picture_id_,
-      request_keyframe, qindex);
+      request_keyframe, base_q_idx);
+
+  if (qindex.has_value()) {
+    CHECK_EQ(input_arguments_.SequenceControlDesc.RateControl.Mode,
+             D3D12_VIDEO_ENCODER_RATE_CONTROL_MODE_CQP);
+    current_rate_control_.SetCQP(
+        request_keyframe ? D3D12VideoEncoderRateControl::FrameType::kIntra
+                         : D3D12VideoEncoderRateControl::FrameType::kInterPrev,
+        qindex.value());
+    input_arguments_.SequenceControlDesc.RateControl =
+        current_rate_control_.GetD3D12VideoEncoderRateControl();
+  } else if (rate_control_ != current_rate_control_) {
+    if (rate_control_.GetMode() != current_rate_control_.GetMode()) {
+      CHECK(SupportsRateControlReconfiguration());
+      input_arguments_.SequenceControlDesc.Flags |=
+          D3D12_VIDEO_ENCODER_SEQUENCE_CONTROL_FLAG_RATE_CONTROL_CHANGE;
+    }
+    current_rate_control_ = rate_control_;
+    input_arguments_.SequenceControlDesc.RateControl =
+        current_rate_control_.GetD3D12VideoEncoderRateControl();
+  }
 
   // Enable SCC tools will turn off CDEF, loop filter, etc on I-frame.
   if (!picture_ctrl_.allow_intrabc) {
@@ -963,7 +987,7 @@ void D3D12VideoEncodeAV1Delegate::FillPictureControlParams(
       // Keyframes: filt_guess = q * 0.06699 - 1.60817
       // Other frames: filt_guess = q * inter_frame_multiplier + 2.48225
       int filter_level = 0;
-      const int q = kAcQuantizerLookup[qindex];
+      const int q = kAcQuantizerLookup[base_q_idx];
       int inter_frame_multiplier =
           input_size_.Width * input_size_.Height > 352 * 288 ? 12034 : 6017;
       // Convert to fixed point: 0.06699 ≈ 17563/262144, -1.60817 ≈
