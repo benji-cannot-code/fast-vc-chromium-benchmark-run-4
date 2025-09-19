4a13850d783aa6cc6e52b4f9ec420c870a22b972
anthie@google.com <anthie@google.com>
2025-09-19 03:19:40 -0700

    Temporarily skip SyncConsent screen in signin interception
    If ReplaceSyncPromosWithSignInPromos is enabled sync the soon to
    be deprecated sync consent screen until proper replacement is built.
    
    Demo: sync screen skipped
    https://screencast.googleplex.com/cast/NTA1NTkxMjMyOTg3MTM2MHxkODI1MGM0MC01NA
    
    Bug: 418143300
    Change-Id: Iae17f2ac81bd482e8bee580e8253ce42fe5ba97f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6965704
    Commit-Queue: Anthi Orfanou <anthie@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1517833}

--- chrome/browser/ui/profiles/signin_intercept_first_run_experience_dialog.cc
+++ chrome/browser/ui/profiles/signin_intercept_first_run_experience_dialog.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "components/signin/public/base/signin_metrics.h"
 #include "components/signin/public/base/signin_switches.h"
+#include "components/sync/base/features.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_observer.h"
@@ -93,7 +94,10 @@ class SigninInterceptFirstRunExperienceDialog::InterceptTurnSyncOnHelperDelegate
 SigninInterceptFirstRunExperienceDialog::InterceptTurnSyncOnHelperDelegate::
     InterceptTurnSyncOnHelperDelegate(
         base::WeakPtr<SigninInterceptFirstRunExperienceDialog> dialog)
-    : dialog_(std::move(dialog)), browser_(dialog_->browser_->AsWeakPtr()) {}
+    : dialog_(std::move(dialog)), browser_(dialog_->browser_->AsWeakPtr()) {
+  CHECK(!base::FeatureList::IsEnabled(
+      syncer::kReplaceSyncPromosWithSignInPromos));
+}
 SigninInterceptFirstRunExperienceDialog::InterceptTurnSyncOnHelperDelegate::
     ~InterceptTurnSyncOnHelperDelegate() = default;
 
@@ -285,9 +289,24 @@ void SigninInterceptFirstRunExperienceDialog::DoNextStep(
     case Step::kStart:
       NOTREACHED();
     case Step::kTurnOnSync:
+      if (base::FeatureList::IsEnabled(
+              syncer::kReplaceSyncPromosWithSignInPromos)) {
+        // TODO(crbug.com/418143300): Until we implement the proper flow for the
+        // History Sync optin screen, skip entirely the replaced steps
+        // Step::kTurnOnSync and Step::kSyncConfirmation.
+        DoNextStep(Step::kTurnOnSync, Step::kWaitForSyncedTheme);
+        return;
+      }
       DoTurnOnSync();
       return;
     case Step::kSyncConfirmation:
+      if (base::FeatureList::IsEnabled(
+              syncer::kReplaceSyncPromosWithSignInPromos)) {
+        // TODO(crbug.com/418143300): Until we implement the proper flow for the
+        // History Sync optin screen, skip entirely the replaced steps
+        // Step::kTurnOnSync and Step::kSyncConfirmation.
+        NOTREACHED();
+      }
       DoSyncConfirmation();
       return;
     case Step::kWaitForSyncedTheme:
