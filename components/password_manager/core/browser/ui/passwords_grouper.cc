d04377fabdd5dee077f9b2ded50c57bd9526d2db
Sylvain Defresne <sdefresne@chromium.org>
2025-09-19 07:42:23 -0700

    Add missing include to //components/password_manager
    The include currently comes transitively if use_partition_alloc=true,
    but is not accessible if use_partition_alloc=false. It is better to
    include what is needed.
    
    This is a dependency of ChromeWebView which wants to be build as an
    extension safe framework (and thus build without partition alloc).
    
    Bug: 40226740
    Change-Id: I9ac4f115d8f2435f7b9a47dce86f61df9bf07f35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6965593
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1517932}

--- components/password_manager/core/browser/ui/passwords_grouper.cc
+++ components/password_manager/core/browser/ui/passwords_grouper.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check_op.h"
 #include "base/containers/flat_set.h"
+#include "base/features.h"
 #include "base/memory/raw_span.h"
 #include "base/memory/safety_checks.h"
 #include "base/strings/escape.h"
