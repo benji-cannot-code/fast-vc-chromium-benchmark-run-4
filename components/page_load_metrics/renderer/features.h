dd1ccc09e782dc1aab652ef28a305f50d3719dcd
Shunya Shishido <sisidovski@chromium.org>
2025-09-18 23:12:53 -0700

    Optimize subresource load metric dispatch
    To improve performance, this change changes the mechanism for
    dispatching subresource load notifications to page load metrics.
    Previously, these notifications were broadcast to all
    `RenderFrameObserver` instances, which could be inefficient.
    
    A new `SetSubresourceLoadCallback` method is introduced on `RenderFrame`
    to allow for a direct callback. `MetricsRenderFrameObserver` is updated
    to use this new method, bypassing the observer iteration.
    
    This is same as what we did for `DidObserveNewFeatureUsage()` before.
    This optimization is controlled by the
    `kDidObserveSubresourceLoadImprovement` feature flag.
    
    We may want to reduce the number of `DidObserveSubresourceLoad()` calls
    itself in the future. For now, this CL focuses on reducing the observer
    iteration.
    
    Bug: 404425954
    Change-Id: Ie61ecd0f0d798a391a4999f0d8200ff3fbee6ba9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6967197
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Shunya Shishido <sisidovski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1517745}

--- components/page_load_metrics/renderer/features.h
+++ components/page_load_metrics/renderer/features.h
@@ -10,5 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace page_load_metrics::features {
 BASE_DECLARE_FEATURE(kDidObserveNewFeatureUsageImprovement);
+BASE_DECLARE_FEATURE(kDidObserveSubresourceLoadImprovement);
 }
 #endif  // COMPONENTS_PAGE_LOAD_METRICS_RENDERER_FEATURES_H_
--- content/public/renderer/render_frame.h
+++ content/public/renderer/render_frame.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/content_export.h"
 #include "content/public/common/bindings_policy.h"
 #include "services/service_manager/public/cpp/binder_registry.h"
+#include "third_party/blink/public/common/subresource_load_metrics.h"
 #include "third_party/blink/public/common/use_counter/use_counter_feature.h"
 #include "third_party/blink/public/mojom/devtools/console_message.mojom.h"
 #include "third_party/blink/public/mojom/frame/triggering_event_info.mojom-shared.h"
@@ -224,6 +225,12 @@ class CONTENT_EXPORT RenderFrame :
   using NewFeatureUsageCallback =
       base::RepeatingCallback<void(const blink::UseCounterFeature&)>;
   virtual void SetNewFeatureUsageCallback(NewFeatureUsageCallback callback) = 0;
+
+  // Sets the callback which is called when the renderer observes a new
+  // subresource load. This is used for subresource loading metrics.
+  using SubresourceLoadCallback =
+      base::RepeatingCallback<void(const blink::SubresourceLoadMetrics&)>;
+  virtual void SetSubresourceLoadCallback(SubresourceLoadCallback callback) = 0;
 
  protected:
   ~RenderFrame() override {}
