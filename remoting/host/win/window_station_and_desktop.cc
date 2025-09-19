eb58cdf018f947650a60d0ea368de3f3171cc23f
Matt Stark <msta@google.com>
2025-09-18 20:46:49 -0700

    Fix a few targets for clang modules on windows.
    Bug: 425537956
    Change-Id: I6a6a69647e8a5025c07a573aaa87b292e8e69a6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6967198
    Auto-Submit: Matt Stark <msta@google.com>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Owners-Override: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1517712}

--- remoting/host/win/window_station_and_desktop.cc
+++ remoting/host/win/window_station_and_desktop.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/host/win/window_station_and_desktop.h"
 
 #include <algorithm>
+#include <utility>
 
 namespace remoting {
 
