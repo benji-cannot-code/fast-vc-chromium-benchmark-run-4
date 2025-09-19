1b4ed7376095f68a8991cc3283a067e6b4a27c28
Jean-Philippe Gravel <jpgravel@chromium.org>
2025-09-19 00:56:59 -0700

    Correctly report canvas de-hibernation caused by background rendering
    If a page continues rendering in the background while the page isn't
    visible, the canvas has to come out of hibernation. This is supposed to
    be logged via the kHibernationEndedWithSwitchToBackgroundRendering UMA
    histogram. That histogram however is emitted only if the canvas wakes up
    in CPU rendering. Any hibernation that ends with a GPU surface is
    assumed to because the page became visible.
    
    This condition orinates from the very first hibernation implementation,
    from https://codereview.chromium.org/1507863005. Back then, background
    page would always get CPU canvases. In other words, if the canvas
    de-hibernated in GPU mode, the page was obligatory visible.
    
    This condition was removed in https://crrev.com/c/2238364. Background
    page can now have GPU canvases. Thus, it's no longer sufficient to
    check that the canvas is accelerated to conclude that the hibernation
    was successfully concluded.
    
    Bug: 443022228
    Change-Id: Ia7f5a97759b545b6a781bfddbe2ab49960e9f6dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6968030
    Auto-Submit: Jean-Philippe Gravel <jpgravel@chromium.org>
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1517780}

--- third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.cc
@@ -1470,14 +1470,15 @@ CanvasRenderingContext2D::RecreateCanvasResourceProviderForCanvas2D() {
     return resource_provider_.get();
   }
 
-  if (resource_provider_->IsAccelerated()) {
+  if (!canvas()->IsPageVisible()) {
     CanvasHibernationHandler::ReportHibernationEvent(
-        CanvasHibernationHandler::HibernationEvent::kHibernationEndedNormally);
+        CanvasHibernationHandler::HibernationEvent::
+            kHibernationEndedWithSwitchToBackgroundRendering);
   } else {
-    if (!canvas()->IsPageVisible()) {
+    if (resource_provider_->IsAccelerated()) {
       CanvasHibernationHandler::ReportHibernationEvent(
           CanvasHibernationHandler::HibernationEvent::
-              kHibernationEndedWithSwitchToBackgroundRendering);
+              kHibernationEndedNormally);
     } else {
       CanvasHibernationHandler::ReportHibernationEvent(
           CanvasHibernationHandler::HibernationEvent::
