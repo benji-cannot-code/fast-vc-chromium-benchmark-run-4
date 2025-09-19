28f5f668dfb7a17f57cfdf0e73326b6e5942a8ca
etiennep-chromium <etiennep@chromium.org>
2025-09-19 04:51:46 -0700

    [GC] Use context size in external memory
    When adding stricter limits on external v8 memory, I hit test failure:
    https://chromium-review.googlesource.com/c/v8/v8/+/6942837
    (reporting 200Gb memory)
    
    This is because HTMLCanvasElement::UpdateMemoryUsage isn't accurate
    for webgl context.
    This CL makes a few incremental steps to make UpdateMemoryUsage more accurate:
    - Use DrawingBufferSize instead of host's size; ultimately this is what fixes the issue above for webgl.
    - It also moves UpdateMemoryUsage around to makes sure it's
    invoked after context_->Reshape().
    - Consider memory=0 when there's no context.
    - Every type of context is included in HTMLCanvasElement: this aligns with OffscreenCanvas: this uses a default impl AllocatedBufferCountPerPixel() == 1 which is a no-op in OffscreenCanvas; the caveat is that this isn't accurate for all context.
    
    Other changes that will be done in follow-up:
    - Merge OffscreenCanvas and HTMLCanvasElement impl of UpdateMemoryUsage
    - Improve AllocatedBufferCountPerPixel for more context (e.g. OffscreenCanvasRenderingContext2D should get the same behavior as CanvasRenderingContext2D through their common base class)
    
    Bug: 361124432
    Change-Id: I03dcf8a13f293e2fda7ad28164b230b3f4967622
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6950799
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Jean-Philippe Gravel <jpgravel@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1517859}

--- third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
+++ third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
@@ -9277,10 +9277,7 @@ void WebGLRenderingContextBase::Trace(Visitor* visitor) const {
   CanvasRenderingContext::Trace(visitor);
 }
 
-int WebGLRenderingContextBase::AllocatedBufferCountPerPixel() {
-  if (isContextLost())
-    return 0;
-
+int WebGLRenderingContextBase::AllocatedBufferCountPerPixel() const {
   int buffer_count = 1;
   buffer_count *= 2;  // WebGL's front and back color buffers.
   int samples = GetDrawingBuffer() ? GetDrawingBuffer()->SampleCount() : 0;
@@ -9297,10 +9294,6 @@ int WebGLRenderingContextBase::AllocatedBufferCountPerPixel() {
     } else if (attribs->depth() || attribs->stencil()) {
       buffer_count += 1;  // regular depth/stencil buffer
     }
-  }
-
-  if (!Host()) {
-    return buffer_count;
   }
 
   auto* provider = resource_provider_.get();
