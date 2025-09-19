5c7a903edf97c50bc3c09b39b48587c5c1c1dfa4
Joonghun Park <pjh0718@chromium.org>
2025-09-19 07:27:57 -0700

    Notify AwContents::mDisplayObserver of the current DIP Scale.
    AwDisplayAndroidObserver gets onDIPScaleChanged callback
    whenever fold device which have different DIPScales
    for internal/external displays is folded/unfolded.
    
    But there is a timing issue on such device that
    onAttachedToWindow() which adds the observer above
    is not guaranteed to be called
    before DisplayListenerBackend::onDisplayChanged().
    
    So if all the observers are added before the onDisplayChanged()
    call, it works as expected, otherwise DIPScale isn't set properly.
    
    Because if there is no value change in display attribute
    passed to the DisplayListenerBackend::onDisplayChanged()
    compared to the previous one,
    the display attribute changed callbacks on the observers above
    are not invoked for the newly added observer currently,
    this CL ensures that the new observer
    (i.e. AwContents::mDisplayObserver)
    gets notified of the current DIP Scale value
    from DisplayListenerBackend at the attached time
    even if there is no value change,
    by directly invoking AwDisplayAndroidObserver::onDIPScaleChanged()
    in AwContents::onAttachedToWindow().
    
    Bug: None
    Change-Id: Ide69c83aea1c27d0e39090ee0a31aee611a17ac2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6952712
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Joonghun Park <pjh0718@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1517909}

--- android_webview/java/src/org/chromium/android_webview/AwContents.java
+++ android_webview/java/src/org/chromium/android_webview/AwContents.java
@@ -143,6 +143,7 @@ import org.chromium.ui.base.PageTransition;
 import org.chromium.ui.base.ViewAndroidDelegate;
 import org.chromium.ui.base.ViewUtils;
 import org.chromium.ui.base.WindowAndroid;
+import org.chromium.ui.display.DisplayAndroid;
 import org.chromium.ui.display.DisplayAndroid.DisplayAndroidObserver;
 import org.chromium.url.GURL;
 
@@ -4532,7 +4533,9 @@ public class AwContents implements SmartClipProvider {
             mIsAttachedToWindow = true;
             mTemporarilyDetached = false;
 
-            mWindowAndroid.getWindowAndroid().getDisplay().addObserver(mDisplayObserver);
+            DisplayAndroid display = mWindowAndroid.getWindowAndroid().getDisplay();
+            mDisplayObserver.onDIPScaleChanged(display.getDipScale());
+            display.addObserver(mDisplayObserver);
 
             mViewEventSink.onAttachedToWindow();
             AwContentsJni.get()
