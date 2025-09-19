c945159ddeb6364df13a2c967274198b059da825
Jessica Chen <peiche@microsoft.com>
2025-09-19 12:12:08 -0700

    webium: Implement RenderWidgetHostViewChildFrame::GetNativeViewAccessible
    Exposing the root view's NativeViewAccessible from
    RenderWidgetHostViewChildFrame so it's accessible by AutofillPopupControllerImpl::GetRootAXPlatformNodeForWebContents().
    
    In webium, the "main frame" is an inner WebContents nested in the outer
    browser UI WebContents.
    
    Bug: 393144165
    Change-Id: I537aa9ae90112b72711b169f445b0b9ba31a1bf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6967404
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Jessica Chen <peiche@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1518110}

--- content/browser/renderer_host/render_widget_host_view_child_frame.cc
+++ content/browser/renderer_host/render_widget_host_view_child_frame.cc
@@ -350,7 +350,10 @@ gfx::NativeView RenderWidgetHostViewChildFrame::GetNativeView() {
 
 gfx::NativeViewAccessible
 RenderWidgetHostViewChildFrame::GetNativeViewAccessible() {
-  NOTREACHED();
+  if (!GetRootView()) {
+    return gfx::NativeViewAccessible();
+  }
+  return GetRootView()->GetNativeViewAccessible();
 }
 
 void RenderWidgetHostViewChildFrame::UpdateFrameSinkIdRegistration() {
