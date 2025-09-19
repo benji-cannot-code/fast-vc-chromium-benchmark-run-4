519efecff9c7a233005b60cc67a8fd8ff915fe4e
Ana Ercegovac <anaercegovac@google.com>
2025-09-19 02:54:25 -0700

    Add skeleton for `AnalysisServiceSettingsBase`
    This CL introduces the basic structure for a new class
    `AnalysisServiceSettingsBase`. The
    class is currently empty and will be populated in subsequent CLs.
    
    Change-Id: Ieca7fdbbcb0132bc5051ef86a712a7125a6a6e9c
    Bug: 444237640
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6943375
    Commit-Queue: Ana Ercegovac <anaercegovac@google.com>
    Reviewed-by: Lina Ismail <ismaill@google.com>
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1517825}

---
+++ components/enterprise/connectors/core/analysis_service_settings_base.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_ENTERPRISE_CONNECTORS_CORE_ANALYSIS_SERVICE_SETTINGS_BASE_H_
+#define COMPONENTS_ENTERPRISE_CONNECTORS_CORE_ANALYSIS_SERVICE_SETTINGS_BASE_H_
+
+namespace enterprise_connectors {
+
+class AnalysisServiceSettingsBase {
+ public:
+  virtual ~AnalysisServiceSettingsBase() = default;
+};
+
+}  // namespace enterprise_connectors
+
+#endif  // COMPONENTS_ENTERPRISE_CONNECTORS_CORE_ANALYSIS_SERVICE_SETTINGS_BASE_H_
