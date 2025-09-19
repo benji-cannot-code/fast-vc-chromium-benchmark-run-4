85066774fc01f33fa36f32fc5c0597a52cb8375d
Eleanor Lee <eleanorlee@google.com>
2025-09-19 09:01:47 -0700

    [Bookmarks Bar] Implement IPH part 2 : Add Iph controller
    https://screencast.googleplex.com/cast/NjQ5NTE4ODk1NTQzMDkxMnw3Y2VkNGQ1ZS1lZg
    
    Triggers:
    
    a. User has 1+ bookmarks
    
    -> We would only count existing bookmarks/webpages (and not empty folders) in the bookmarks bar (and not in mobile bookmarks or reading list).
    
    b. User bookmarks a page
    
    -> We would only count a bookmark/webpage (and not when an empty folder is added) either in the bookmarks bar, mobile bookmarks, or reading list.
    
    
    Bug: 430056284
    Change-Id: I447709bbad2aecb17723ed15504fe4c04804fb29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6923166
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Commit-Queue: Eleanor Lee <eleanorlee@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1517992}

--- chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
@@ -52,6 +52,7 @@ import org.chromium.chrome.browser.bookmarks.BookmarkOpener;
 import org.chromium.chrome.browser.bookmarks.BookmarkOpenerImpl;
 import org.chromium.chrome.browser.bookmarks.TabBookmarker;
 import org.chromium.chrome.browser.bookmarks.bar.BookmarkBarCoordinator;
+import org.chromium.chrome.browser.bookmarks.bar.BookmarkBarIphController;
 import org.chromium.chrome.browser.bookmarks.bar.BookmarkBarUtils;
 import org.chromium.chrome.browser.bookmarks.bar.BookmarkBarVisibilityProvider;
 import org.chromium.chrome.browser.bookmarks.bar.BookmarkBarVisibilityProvider.BookmarkBarVisibilityObserver;
@@ -280,6 +281,7 @@ public class TabbedRootUiCoordinator extends RootUiCoordinator {
     private final @NonNull EdgeToEdgeManager mEdgeToEdgeManager;
     protected @Nullable InstantMessageDelegateImpl mInstantMessageDelegateImpl;
     private @Nullable BookmarkBarCoordinator mBookmarkBarCoordinator;
+    private @Nullable BookmarkBarIphController mBookmarkBarIphController;
     private @Nullable BookmarkBarVisibilityProvider mBookmarkBarVisibilityProvider;
     private @Nullable BookmarkBarVisibilityObserver mBookmarkBarVisibilityObserver;
     private @Nullable Supplier<Integer> mBookmarkBarHeightSupplier;
@@ -722,6 +724,11 @@ public class TabbedRootUiCoordinator extends RootUiCoordinator {
         if (mAdvancedProtectionCoordinator != null) {
             mAdvancedProtectionCoordinator.destroy();
             mAdvancedProtectionCoordinator = null;
+        }
+
+        if (mBookmarkBarIphController != null) {
+            mBookmarkBarIphController.destroy();
+            mBookmarkBarIphController = null;
         }
 
         super.onDestroy();
@@ -1291,6 +1298,16 @@ public class TabbedRootUiCoordinator extends RootUiCoordinator {
                                 mToolbarManager.getMenuButtonView());
                 mPageZoomIphController.showColdStartIph();
             }
+        }
+
+        if (BookmarkBarUtils.isDeviceBookmarkBarCompatible(mActivity)) {
+            mBookmarkBarIphController =
+                    new BookmarkBarIphController(
+                            mActivity,
+                            profile,
+                            mAppMenuCoordinator.getAppMenuHandler(),
+                            mToolbarManager.getMenuButtonView(),
+                            mBookmarkModelSupplier.get());
         }
     }
 
