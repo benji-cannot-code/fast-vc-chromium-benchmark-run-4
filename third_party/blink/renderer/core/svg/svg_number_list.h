0e16d596f741d5068555c95426a723e100666a14
Fredrik Söderquist <fs@opera.com>
2025-09-19 01:18:30 -0700

    spanification: Spanify SVGNumberList::Parse()
    Pass the span by-value since there's only a single callsite, and it
    doesn't need the mutated span.
    
    Bug: 351564777, 356658478
    Change-Id: I3dff7ea542baeb1251699d1b21f3c38a238c3114
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6965215
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1517786}

--- third_party/blink/renderer/core/svg/svg_number_list.h
+++ third_party/blink/renderer/core/svg/svg_number_list.h
@@ -70,7 +70,7 @@ class SVGNumberList final
 
  private:
   template <typename CharType>
-  SVGParsingError Parse(const CharType*& ptr, const CharType* end);
+  SVGParsingError Parse(base::span<const CharType>);
 };
 
 template <>
