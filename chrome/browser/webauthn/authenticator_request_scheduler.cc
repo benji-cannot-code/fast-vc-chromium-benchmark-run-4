b6e845094d35730cd18d1cf04e78ce6a1dee7251
Adem Derinel <derinel@google.com>
2025-09-19 08:02:07 -0700

    Revert "Move scheduler bits to ChromeAuthenticatorRequestDelegate"
    This reverts commit fbaffc494a4b8da9aa16f1d02d16a94aae976c27.
    
    Reason for revert: To clean revert crrev.com/c/6930314 which breaks
    webauthn on page navigations
    
    Original change's description:
    > Move scheduler bits to ChromeAuthenticatorRequestDelegate
    >
    > Change-Id: Ib964ba9e5f4fcb40f1748a63090dbf2699bdcd0f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6947905
    > Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    > Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    > Commit-Queue: Adem Derinel <derinel@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1517114}
    
    Bypass-Check-License: Revert
    Change-Id: I498ab5445b68deb9c93f1a98967754d472c28d23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6965710
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1517952}

---
+++ chrome/browser/webauthn/authenticator_request_scheduler.cc
@@ -0,0 +1,41 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/webauthn/authenticator_request_scheduler.h"
+
+#include "base/memory/weak_ptr.h"
+#include "chrome/browser/webauthn/chrome_authenticator_request_delegate.h"
+#include "content/public/browser/web_contents.h"
+#include "content/public/browser/web_contents_user_data.h"
+
+// static
+ChromeAuthenticatorRequestDelegate*
+AuthenticatorRequestScheduler::CreateRequestDelegate(
+    content::RenderFrameHost* render_frame_host) {
+  // RenderFrameHosts which are not exposed to the user can't create
+  // authenticator request delegate.
+  if (!render_frame_host->IsActive())
+    return nullptr;
+
+  auto* const web_contents =
+      content::WebContents::FromRenderFrameHost(render_frame_host);
+  if (!web_contents) {
+    return nullptr;
+  }
+  if (ChromeAuthenticatorRequestDelegate::FromWebContents(web_contents)) {
+    return nullptr;
+  }
+
+  ChromeAuthenticatorRequestDelegate::CreateForWebContents(web_contents,
+                                                           render_frame_host);
+  return ChromeAuthenticatorRequestDelegate::FromWebContents(web_contents);
+}
+
+// static
+ChromeAuthenticatorRequestDelegate*
+AuthenticatorRequestScheduler::GetRequestDelegate(
+    content::WebContents* web_contents) {
+  return ChromeAuthenticatorRequestDelegate::FromWebContents(web_contents);
+}
--- chrome/browser/webauthn/webauthn_focus_interactive_uitest.cc
+++ chrome/browser/webauthn/webauthn_focus_interactive_uitest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface_iterator.h"
+#include "chrome/browser/webauthn/authenticator_request_scheduler.h"
 #include "chrome/browser/webauthn/chrome_authenticator_request_delegate.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/interactive_test_utils.h"
