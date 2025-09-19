fb1c6f623ca864acc855a0a8910e961f61eaeb74
Jack Thiesen <jthiesen@chromium.org>
2025-09-19 08:21:50 -0700

    LoadURLParamsFromNavigateParams Conversion Function
    Cleans up repeated code and creates a shared conversion for converting NavigateParams into LoadURLParams. Importantly, this moves this helper function to an area in the code that is Android Desktop accessible.
    
    Bug: 441594986
    Change-Id: Ie3ab42757bc6a741c879c8249dc0b5ac8ab8cb33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6962612
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Jack Thiesen <jthiesen@chromium.org>
    Reviewed-by: Linyu He <linyuh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1517970}

---
+++ chrome/browser/ui/browser_navigator_params_utils.h
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_BROWSER_NAVIGATOR_PARAMS_UTILS_H_
+#define CHROME_BROWSER_UI_BROWSER_NAVIGATOR_PARAMS_UTILS_H_
+
+#include "content/public/browser/navigation_controller.h"
+
+namespace content {
+class WebContents;
+}  // namespace content
+
+struct NavigateParams;
+
+// Creates a content::NavigationController::LoadURLParams object from a
+// NavigateParams object.
+content::NavigationController::LoadURLParams LoadURLParamsFromNavigateParams(
+    NavigateParams* params);
+
+#if !BUILDFLAG(IS_ANDROID)
+// Same as previous but sets navigation UI data for main frame navigations.
+content::NavigationController::LoadURLParams LoadURLParamsFromNavigateParams(
+    content::WebContents* target_contents,
+    NavigateParams* params);
+#endif
+
+#endif  // CHROME_BROWSER_UI_BROWSER_NAVIGATOR_PARAMS_UTILS_H_
