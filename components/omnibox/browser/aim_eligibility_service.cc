21c819d36c5f75a2e1770aa72b89bc7be1bdd530
manukh <manukh@chromium.org>
2025-09-19 20:20:45 -0700

    [omnibox][ai-mode] Sync features that already use kill switch approach
    This CL updates the AI features that already use the kill switch
    approach to use the helper `GenericKillSwitchFeatureCheck()`:
    
    - NTP compose entry
    - NTP compose box
    - Omnibox RHS AI button
    - Mobile AI match actions
    
    This CL also upgrades `GenericKillSwitchFeatureCheck()` to support
    entrypoints that have launched in en-US only (e.g. NTP compose entry).
    
    This helps a) ensure they're all implemented exactly the same, b) reduce
    effort & bug-risk when they need to launch in the en-US, and c) reduce
    effort when they later switch to the && approach when we launch
    eligibility service.
    
    A followup CL will upgrade the AI features that don't already use the
    kill switch approach.
    
    Details:
    https://docs.google.com/document/d/1BXuGAybW1GXRymDBOrjzTefj503quP25-eZyhGx7kQ4/
    
    Bug: 436225340
    Change-Id: Ifb5e5ef57b9ecd4648c658afae4a593cb703d4f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6967819
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1518296}

--- components/omnibox/browser/aim_eligibility_service.cc
+++ components/omnibox/browser/aim_eligibility_service.cc
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/omnibox/browser/aim_eligibility_service.h"
 
+#include <functional>
 #include <memory>
+#include <optional>
 #include <string>
 
 #include "base/base64.h"
@@ -153,7 +155,9 @@ bool GetResponseFromPrefs(const PrefService* prefs,
 // static
 bool AimEligibilityService::GenericKillSwitchFeatureCheck(
     const AimEligibilityService* aim_eligibility_service,
-    const base::Feature& feature) {
+    const base::Feature& feature,
+    const std::optional<std::reference_wrapper<const base::Feature>>
+        feature_en_us) {
   // If the generic feature is overridden to be false, return false.
   auto* feature_list = base::FeatureList::GetInstance();
   if (feature_list && feature_list->IsFeatureOverridden(feature.name) &&
@@ -178,7 +182,11 @@ bool AimEligibilityService::GenericKillSwitchFeatureCheck(
   }
 
   // Otherwise, check the generic entrypoint feature.
-  return base::FeatureList::IsEnabled(feature);
+  return base::FeatureList::IsEnabled(feature) ||
+         (feature_en_us &&
+          base::FeatureList::IsEnabled(feature_en_us.value()) &&
+          aim_eligibility_service->IsLanguage("en") &&
+          aim_eligibility_service->IsCountry("us"));
 }
 
 // static
--- components/omnibox/browser/aim_eligibility_service.h
+++ components/omnibox/browser/aim_eligibility_service.h
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_OMNIBOX_BROWSER_AIM_ELIGIBILITY_SERVICE_H_
 #define COMPONENTS_OMNIBOX_BROWSER_AIM_ELIGIBILITY_SERVICE_H_
 
+#include <functional>
 #include <memory>
+#include <optional>
 #include <string>
 
 #include "base/callback_list.h"
@@ -42,11 +44,13 @@ class AimEligibilityService : public KeyedService,
  public:
   // Helper that individual AIM features can use to check if they should be
   // enabled. Unlike most chrome features, which simply check if the
-  // `base::Feature` is enabled, AIM features should use this because we want to
+  // `base::Feature` is enabled, AIM features should use this so that they
   // auto-launch them when the eligibility service launches.
   static bool GenericKillSwitchFeatureCheck(
       const AimEligibilityService* aim_eligibility_service,
-      const base::Feature& feature);
+      const base::Feature& feature,
+      const std::optional<std::reference_wrapper<const base::Feature>>
+          feature_en_us = std::nullopt);
   // See comment for `WriteToPref()`.
   static void RegisterProfilePrefs(PrefRegistrySimple* registry);
   // Returns true if the AIM is allowed per the policy.
