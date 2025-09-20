0c928bb4b0fafa6ed9fd340c17f7e637d6007607
Minoru Chikamune <chikamune@chromium.org>
2025-09-20 05:51:52 -0700

    Enable kServiceWorkerBackgroundUpdateForRegisteredStorageKeys by default
    https://crrev.com/c/6606856 introduced
    kServiceWorkerBackgroundUpdateForRegisteredStorageKeys, and it didn't
    cause any regressions or crashes. Let me enable this feature by default.
    For WebView, we already enabled this feature by default.
    
    Enabled-by-default-reason: Launching the feature
    Bug: 421530699
    Change-Id: I2e834bff3deda1b10e12845029bd2d1a4c237870
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6869474
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1518348}

--- android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
+++ android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
@@ -1039,8 +1039,6 @@ public final class ProductionSupportedFlagList {
         Flag.baseFeature("ServiceWorkerBackgroundUpdateForServiceWorkerScopeCache"),
         Flag.baseFeature("ServiceWorkerBackgroundUpdateForFindRegistrationForClientUrl"),
         Flag.baseFeature(
-                "ServiceWorkerBackgroundUpdateForRegisteredStorageKeysFieldTrialControlled"),
-        Flag.baseFeature(
                 AwFeatures.WEBVIEW_CONNECT_TO_COMPONENT_PROVIDER_IN_BACKGROUND,
                 "Connect to the non-embedded components provider from a background thread."),
         Flag.baseFeature("PrefetchUseContentRefactor"),
