74f3429804429a6f4e3a456422639f63657f0883
Ryan Thomas <rathomas@google.com>
2025-09-19 18:07:18 -0700

    Conditionally override openAdjacently when moving tabs to a new window.
    When moving tabs or tab groups to a new window, this change
    conditionally overrides the openAdjacently parameter. If the
    ROBUST_WINDOW_MANAGEMENT_EXPERIMENTAL feature is enabled, the value of
    openAdjacently is determined by
    MultiWindowUtils.shouldOpenInAdjacentWindow() if the source is not in
    multi-window mode.
    
    This logic is applied in moveTabsToNewWindow and
    moveTabGroupToNewWindow.
    
    (openAdjacently):
    http://screencast/cast/NTcxMjE0NTA4MDY0NzY4MHwxNjJkNzJlNS1kOA
    (!openAdjacently):
    http://screencast/cast/NTIxODIxNTU4NzIxNzQwOHwzZTEwYWIzZi05NQ
    
    Bug: 436898003
    Change-Id: Ie77e222ad9abccf4a8845b2ca6300f48fa67c364
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6944695
    Commit-Queue: Ryan Thomas <rathomas@google.com>
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1518271}

--- chrome/android/java/src/org/chromium/chrome/browser/multiwindow/MultiInstanceManagerApi31.java
+++ chrome/android/java/src/org/chromium/chrome/browser/multiwindow/MultiInstanceManagerApi31.java
@@ -1404,25 +1404,29 @@ class MultiInstanceManagerApi31 extends MultiInstanceManagerImpl implements Acti
 
     @Override
     public void moveTabsToNewWindow(List<Tab> tabs) {
+        boolean openAdjacently =
+                !mActivity.isInMultiWindowMode() && MultiWindowUtils.shouldOpenInAdjacentWindow();
         moveToNewWindowIfPossible(
                 () ->
                         moveAndReparentTabsToNewWindow(
                                 tabs,
                                 INVALID_WINDOW_ID,
                                 /* preferNew= */ true,
-                                /* openAdjacently= */ false,
+                                openAdjacently,
                                 /* addTrustedIntentExtras= */ true));
     }
 
     @Override
     public void moveTabGroupToNewWindow(TabGroupMetadata tabGroupMetadata) {
+        boolean openAdjacently =
+                !mActivity.isInMultiWindowMode() && MultiWindowUtils.shouldOpenInAdjacentWindow();
         moveToNewWindowIfPossible(
                 () ->
                         moveAndReparentTabGroupToNewWindow(
                                 tabGroupMetadata,
                                 INVALID_WINDOW_ID,
                                 /* preferNew= */ true,
-                                /* openAdjacently= */ false,
+                                openAdjacently,
                                 /* addTrustedIntentExtras= */ true));
     }
 
