1235fde635a85a0e7884b7d80fb4197d2837d628
Brandon Fong <bjfong@google.com>
2025-09-19 23:19:27 -0700

    [tips] Generate NotificationData for each feature tip
    Generate NotificationData for each feature tip when scheduling tips
    notifications.
    
    Bug: 445996109
    Change-Id: I5e5a903b51bb57857e4ffc3704788de2b74198c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6970108
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1518314}

--- chrome/browser/notifications/scheduler/public/notification_scheduler_constant.h
+++ chrome/browser/notifications/scheduler/public/notification_scheduler_constant.h
@@ -12,6 +12,8 @@ inline constexpr char kDefaultHelpfulButtonId[] =
     "NOTIFICATION_SCHEDULER_DEFAULT_HELPFUL_BUTTON_ID";
 inline constexpr char kDefaultUnhelpfulButtonId[] =
     "NOTIFICATION_SCHEDULER_DEFAULT_UNHELPFUL_BUTTON_ID";
+inline constexpr char kTipsNotificationsFeatureType[] =
+    "TIPS_NOTIFICATIONS_FEATURE_TYPE";
 
 }  // namespace notifications
 
