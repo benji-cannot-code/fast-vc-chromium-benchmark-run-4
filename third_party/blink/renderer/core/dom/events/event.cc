8a8f5cd8aa89f56f1fadb295e3f9d4cda1d68051
Yeonghan Kim <soosungp33@gmail.com>
2025-09-19 19:55:13 -0700

    Fix composedPath() to return an empty list when the event path is empty
    According to the spec[1], if the event path is empty, then
    composedPath() should return an empty list as well. The event path is
    populated during the dispatching phase. Therefore, if an event is not
    being dispatched, its path is empty and composedPath() should return an
    empty list.
    However, the current implementation always returns a list containing at
    least one Window object as composed path even if the event is not being
    dispatched.
    So, this patch fixes the bug by adding a check if the event is being
    dispatched.
    
    [1] https://dom.spec.whatwg.org/#dom-event-composedpath
    
    Bug: 444932649
    Change-Id: I9c2344cc33774660af9c17ed49160f1edda39d00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6949466
    Commit-Queue: Jinho Bang <zino@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Reviewed-by: Jinho Bang <zino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1518290}

--- third_party/blink/renderer/core/dom/events/event.cc
+++ third_party/blink/renderer/core/dom/events/event.cc
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/workers/worker_global_scope.h"
 #include "third_party/blink/renderer/platform/bindings/script_state.h"
 #include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
 namespace blink {
 
@@ -367,6 +368,10 @@ HeapVector<Member<EventTarget>> Event::composedPath(
           event_path_->TopNodeEventContext()
               .GetTreeScopeEventContext()
               .EnsureEventPath(*event_path_));
+    }
+    if (RuntimeEnabledFeatures::ComposedPathEmptyAfterDispatchEnabled() &&
+        !IsBeingDispatched()) {
+      return HeapVector<Member<EventTarget>>();
     }
     return HeapVector<Member<EventTarget>>(1, window);
   }
