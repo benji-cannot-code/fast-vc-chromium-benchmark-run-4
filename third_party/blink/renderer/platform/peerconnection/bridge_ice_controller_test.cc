f80571b2a4485ee28b9064532d62dfccf76cc212
Danil Chapovalov <danilchap@chromium.org>
2025-09-22 02:06:48 -0700

    Update BridgeIceController to use webrtc Timestamp&TimeDelta
    Call webrtc function that return and consume Timestamp instead of raw int representing milliseconds.
    That would allow to remove old api and also avoid loose of precision when
    WebRTC would stop rounding current time in milliseconds in Connection
    
    Bug: webrtc:42223979
    Change-Id: I3e5d1696c6bee437109b067604a6b02a2e39ef4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6943151
    Reviewed-by: Tomas Gunnarsson <tommi@chromium.org>
    Auto-Submit: Danil Chapovalov <danilchap@chromium.org>
    Commit-Queue: Tomas Gunnarsson <tommi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1518572}

--- third_party/blink/renderer/platform/peerconnection/bridge_ice_controller_test.cc
+++ third_party/blink/renderer/platform/peerconnection/bridge_ice_controller_test.cc
@@ -16,15 +16,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
-
 #include "third_party/blink/renderer/platform/peerconnection/fake_connection_test_base.h"
 #include "third_party/blink/renderer/platform/peerconnection/webrtc_connection_matchers.h"
-
+#include "third_party/webrtc/api/units/time_delta.h"
+#include "third_party/webrtc/api/units/timestamp.h"
 #include "third_party/webrtc/p2p/base/ice_controller_interface.h"
 #include "third_party/webrtc/p2p/base/ice_switch_reason.h"
 #include "third_party/webrtc/p2p/base/mock_ice_agent.h"
 #include "third_party/webrtc/p2p/base/mock_ice_controller.h"
-
 #include "third_party/webrtc_overrides/p2p/base/fake_connection_factory.h"
 #include "third_party/webrtc_overrides/p2p/base/ice_connection.h"
 #include "third_party/webrtc_overrides/p2p/base/ice_interaction_interface.h"
@@ -571,7 +570,7 @@ TEST_P(BridgeIceControllerProposalTest, StartPingingAfterSortAndSwitch) {
 
   // Pinging does not start automatically, unless triggered through a sort.
   EXPECT_CALL(*wrapped, HasPingableConnection()).Times(0);
-  EXPECT_CALL(*wrapped, SelectConnectionToPing(_)).Times(0);
+  EXPECT_CALL(*wrapped, GetConnectionToPing).Times(0);
   EXPECT_CALL(observer, OnPingProposal(_)).Times(0);
   EXPECT_CALL(agent, OnStartedPinging()).Times(0);
 
@@ -588,7 +587,7 @@ TEST_P(BridgeIceControllerProposalTest, StartPingingAfterSortAndSwitch) {
   EXPECT_CALL(observer, OnPruneProposal).Times(0);
   EXPECT_CALL(agent, PruneConnections(IsEmpty()));
   EXPECT_CALL(*wrapped, HasPingableConnection()).WillOnce(Return(false));
-  EXPECT_CALL(*wrapped, SelectConnectionToPing(_)).Times(0);
+  EXPECT_CALL(*wrapped, GetConnectionToPing).Times(0);
   EXPECT_CALL(observer, OnPingProposal(_)).Times(0);
   EXPECT_CALL(agent, OnStartedPinging()).Times(0);
 
@@ -596,9 +595,10 @@ TEST_P(BridgeIceControllerProposalTest, StartPingingAfterSortAndSwitch) {
   env.FastForwardBy(kTick);
 
   const int recheck_delay_ms = 10;
-  const IceControllerInterface::PingResult ping_result(conn, recheck_delay_ms);
-  const IceControllerInterface::PingResult empty_ping_result(nullptr,
-                                                             recheck_delay_ms);
+  const IceControllerInterface::PingResult ping_result(
+      conn, webrtc::TimeDelta::Millis(recheck_delay_ms));
+  const IceControllerInterface::PingResult empty_ping_result(
+      nullptr, webrtc::TimeDelta::Millis(recheck_delay_ms));
 
   // Pinging starts when there is a pingable connection.
   Sequence start_pinging;
@@ -619,10 +619,10 @@ TEST_P(BridgeIceControllerProposalTest, StartPingingAfterSortAndSwitch) {
       .InSequence(start_pinging)
       .WillOnce(Return(true));
   EXPECT_CALL(agent, OnStartedPinging()).InSequence(start_pinging);
-  EXPECT_CALL(agent, GetLastPingSentMs())
+  EXPECT_CALL(agent, GetLastPingSent)
       .InSequence(start_pinging)
-      .WillOnce(Return(123));
-  EXPECT_CALL(*wrapped, SelectConnectionToPing(123))
+      .WillOnce(Return(webrtc::Timestamp::Millis(123)));
+  EXPECT_CALL(*wrapped, GetConnectionToPing(webrtc::Timestamp::Millis(123)))
       .InSequence(start_pinging)
       .WillOnce(Return(ping_result));
   EXPECT_CALL(observer, OnPingProposal(_))
@@ -647,8 +647,9 @@ TEST_P(BridgeIceControllerProposalTest, StartPingingAfterSortAndSwitch) {
 
   // ICE controller should recheck and ping after the recheck delay.
   // No ping should be sent if no connection selected to ping.
-  EXPECT_CALL(agent, GetLastPingSentMs()).WillOnce(Return(456));
-  EXPECT_CALL(*wrapped, SelectConnectionToPing(456))
+  EXPECT_CALL(agent, GetLastPingSent)
+      .WillOnce(Return(webrtc::Timestamp::Millis(456)));
+  EXPECT_CALL(*wrapped, GetConnectionToPing(webrtc::Timestamp::Millis(456)))
       .WillOnce(Return(empty_ping_result));
   EXPECT_CALL(observer,
               OnPingProposal(PingProposalEq(empty_ping_result,
@@ -713,30 +714,32 @@ using BridgeIceControllerDeathTest = BridgeIceControllerInvalidProposalTest;
 using BridgeIceControllerNoopTest = BridgeIceControllerInvalidProposalTest;
 
 TEST_F(BridgeIceControllerDeathTest, AcceptUnsolicitedPingProposal) {
-  const IceControllerInterface::PingResult ping_result(conn, recheck_delay_ms);
+  const IceControllerInterface::PingResult ping_result(
+      conn, webrtc::TimeDelta::Millis(recheck_delay_ms));
   const IcePingProposal proposal(ping_result, /*reply_expected=*/false);
   EXPECT_DCHECK_DEATH_WITH(interaction_agent->AcceptPingProposal(proposal),
                            "unsolicited");
 }
 
 TEST_F(BridgeIceControllerDeathTest, RejectUnsolicitedPingProposal) {
-  const IceControllerInterface::PingResult ping_result(conn, recheck_delay_ms);
+  const IceControllerInterface::PingResult ping_result(
+      conn, webrtc::TimeDelta::Millis(recheck_delay_ms));
   const IcePingProposal proposal(ping_result, /*reply_expected=*/false);
   EXPECT_DCHECK_DEATH_WITH(interaction_agent->RejectPingProposal(proposal),
                            "unsolicited");
 }
 
 TEST_F(BridgeIceControllerDeathTest, AcceptEmptyPingProposal) {
-  const IceControllerInterface::PingResult null_ping_result(nullptr,
-                                                            recheck_delay_ms);
+  const IceControllerInterface::PingResult null_ping_result(
+      nullptr, webrtc::TimeDelta::Millis(recheck_delay_ms));
   const IcePingProposal proposal(null_ping_result, /*reply_expected=*/true);
   EXPECT_DCHECK_DEATH_WITH(interaction_agent->AcceptPingProposal(proposal),
                            "without a connection");
 }
 
 TEST_F(BridgeIceControllerNoopTest, AcceptUnknownPingProposal) {
-  const IceControllerInterface::PingResult ping_result(conn_two,
-                                                       recheck_delay_ms);
+  const IceControllerInterface::PingResult ping_result(
+      conn_two, webrtc::TimeDelta::Millis(recheck_delay_ms));
   const IcePingProposal proposal(ping_result, /*reply_expected=*/true);
   interaction_agent->AcceptPingProposal(proposal);
   Recheck();
