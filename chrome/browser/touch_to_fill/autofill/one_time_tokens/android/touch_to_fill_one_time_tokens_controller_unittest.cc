9602c6c90e794d18417ffdfe19d7e097211ea05a
trang mai <trangm@google.com>
2025-09-22 06:47:31 -0700

    [OTP] Create skeleton for one_time_token bottom sheet
    Bug: 444612868
    Change-Id: Ib7c177f311692f57d5f119c8b68e14ba6ccc482a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6954265
    Reviewed-by: Luchen Peng <luchenpeng@google.com>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Commit-Queue: Trang Mai <trangm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1518662}

---
+++ chrome/browser/touch_to_fill/autofill/one_time_tokens/android/touch_to_fill_one_time_tokens_controller_unittest.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/touch_to_fill/autofill/one_time_tokens/android/touch_to_fill_one_time_tokens_controller.h"
+
+#include "base/test/mock_callback.h"
+#include "chrome/browser/touch_to_fill/autofill/one_time_tokens/android/mock_touch_to_fill_one_time_tokens_bridge.h"
+#include "chrome/test/base/chrome_render_view_host_test_harness.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+using ::testing::_;
+using ::testing::Eq;
+
+class TouchToFillOneTimeTokensControllerTest
+    : public ChromeRenderViewHostTestHarness {};
+
+TEST_F(TouchToFillOneTimeTokensControllerTest, ShowCallsBridge) {
+  auto bridge = std::make_unique<MockTouchToFillOneTimeTokensBridge>();
+  MockTouchToFillOneTimeTokensBridge* bridge_ptr = bridge.get();
+
+  TouchToFillOneTimeTokensController controller(std::move(bridge));
+
+  const std::u16string test_token = u"123456";
+  EXPECT_CALL(*bridge_ptr, Show(_, _, Eq(test_token)))
+      .WillOnce(testing::Return(true));
+
+  controller.Show(web_contents(), test_token);
+}
