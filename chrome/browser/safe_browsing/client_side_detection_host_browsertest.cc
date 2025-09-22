26398ac39379772b32df5eef0eea583135782fb6
Brian Begnoche <bcb@chromium.org>
2025-09-22 09:04:18 -0700

    Disable newly added flaky tests in client_side_detection_host_browsertest.cc.
    These new tests were introduced in crrev.com/c/6911850. Flakiness is skewed heavily but not entirely to Windows.
    
    This should be OK to do since these test new behavior that is flag-guarded and not yet enabled. These tests should be deflaked and re-enabled before enabling the feature.
    
    Note also that some other test failures were flagged in crbug.com/446269560. I don't have reason to believe that crrev.com/c/6911850 introduced flakiness in these other tests or made them worse. My suspicion is actually that these browser tests have some ambient flakiness at a low rate, and that these new tests are similarly missing the proper mechanism to wait on async behavior unique to it that is more likely to take longer.
    
    Bug: 446269560,446359645,443098659
    Change-Id: If918c63135e8bc8d43e87f980bce9510e7f01e4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6973976
    Reviewed-by: thefrog <thefrog@chromium.org>
    Commit-Queue: Brian Begnoche <bcb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1518741}

--- chrome/browser/safe_browsing/client_side_detection_host_browsertest.cc
+++ chrome/browser/safe_browsing/client_side_detection_host_browsertest.cc
@@ -1325,8 +1325,10 @@ class ClientSideDetectionHostCreditCardFormTest : public InProcessBrowserTest {
   std::string flatbuffer_model_str_;
 };
 
+// TODO(crbug.com/443098659): Deflake and re-enable this test before enabling
+// credit card form pings feature flag.
 IN_PROC_BROWSER_TEST_F(ClientSideDetectionHostCreditCardFormTest,
-                       CreditCardFormTriggersPreclassificationCheck) {
+                       DISABLED_CreditCardFormTriggersPreclassificationCheck) {
   if (base::FeatureList::IsEnabled(kClientSideDetectionKillswitch)) {
     GTEST_SKIP();
   }
@@ -1361,8 +1363,10 @@ IN_PROC_BROWSER_TEST_F(ClientSideDetectionHostCreditCardFormTest,
       "SBClientPhishing.PreClassificationCheckResult.CreditCardForm", 1);
 }
 
+// TODO(crbug.com/443098659): Deflake and re-enable this test before enabling
+// credit card form pings feature flag.
 IN_PROC_BROWSER_TEST_F(ClientSideDetectionHostCreditCardFormTest,
-                       CreditCardFormClassificationTriggersCSDPing) {
+                       DISABLED_CreditCardFormClassificationTriggersCSDPing) {
   if (base::FeatureList::IsEnabled(kClientSideDetectionKillswitch)) {
     GTEST_SKIP();
   }
