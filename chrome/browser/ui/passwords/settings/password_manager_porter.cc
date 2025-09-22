8dfabe11bace609b89780673334d492861c717f3
Foromo Daniel Soromou <koretadaniel@chromium.org>
2025-09-22 05:52:44 -0700

    [Refactor Branded Strings] IDS_PASSWORD_MANAGER_*_DIALOG_TITLE
    See:
    https://docs.google.com/document/d/1gK_4bAsyWtVHl8spYHumozi4k9vl4Yl3V9MratFK8f0
    
    Bug: 443700721
    Change-Id: Ica8d19a5b4c15851ab9416f45fb47f7ca36c395a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6964884
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Daniel Soromou <koretadaniel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1518647}

--- chrome/browser/ui/passwords/settings/password_manager_porter.cc
+++ chrome/browser/ui/passwords/settings/password_manager_porter.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/chrome_select_file_policy.h"
 #include "chrome/common/chrome_paths.h"
+#include "chrome/grit/branded_strings.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/password_manager/core/browser/export/password_manager_exporter.h"
 #include "components/password_manager/core/browser/import/csv_password_sequence.h"
