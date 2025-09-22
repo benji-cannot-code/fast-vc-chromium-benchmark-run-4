7034a2633c9164c481c994bb39533650ecdd35db
Aliona DANGLA <alionadangla@google.com>
2025-09-22 08:14:41 -0700

    [iOS] Remove implementation of Obscured inset in crc_web_view_proxy
    In ios/web/content/web_state/crc_web_view_proxy_impl.* the content
    view is a UIScrollView which does not handle obscured content inset.
    This CL removes the implementation of it to make the bot build.
    
    This been added in this CL: https://crrev.com/c/6973896
    
    Bug: None
    Change-Id: Id740e34841d7e00066360b9651241de56681a131
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6973541
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1518707}

--- ios/web/content/web_state/crc_web_view_proxy_impl.mm
+++ ios/web/content/web_state/crc_web_view_proxy_impl.mm
@@ -122,11 +122,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (UIEdgeInsets)obscuredInsets {
-  return _contentView.obscuredInsets;
+  return UIEdgeInsetsZero;
 }
 
 - (void)setObscuredInsets:(UIEdgeInsets)obscuredInsets {
-  [_contentView setObscuredInsets:obscuredInsets];
 }
 
 @end
