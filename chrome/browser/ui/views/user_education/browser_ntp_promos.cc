0c84aed984aff43531522e0675d53511f0fd0c0b
Christopher Grant <cjgrant@chromium.org>
2025-09-22 13:37:10 -0700

    NTP Promos: Make accessibility labels match caption
    Per accessibility review, these should match. Originally, a hover-label
    style abbreviated version was used.
    
    Bug: 446178975
    Change-Id: I42234dab0f3f57a23c83c375ee9a9fda748216d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6973045
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1518920}

--- chrome/browser/ui/views/user_education/browser_ntp_promos.cc
+++ chrome/browser/ui/views/user_education/browser_ntp_promos.cc
@@ -168,7 +168,7 @@ void MaybeRegisterNtpPromos(user_education::NtpPromoRegistry& registry) {
   registry.AddPromo(NtpPromoSpecification(
       kNtpSignInPromoId,
       NtpPromoContent("account_circle", IDS_NTP_SIGN_IN_PROMO_WITH_BOOKMARKS,
-                      IDS_NTP_SIGN_IN_PROMO_ACTION_BUTTON),
+                      IDS_NTP_SIGN_IN_PROMO_WITH_BOOKMARKS),
       base::BindRepeating(&CheckSignInPromoEligibility),
       base::BindRepeating(&SignInPromoShown),
       base::BindRepeating(&InvokeSignInPromo),
@@ -180,7 +180,7 @@ void MaybeRegisterNtpPromos(user_education::NtpPromoRegistry& registry) {
   registry.AddPromo(NtpPromoSpecification(
       kNtpCustomizationPromoId,
       NtpPromoContent("palette", IDS_NTP_CUSTOMIZATION_PROMO,
-                      IDS_NTP_CUSTOMIZATION_PROMO_ACTION_BUTTON),
+                      IDS_NTP_CUSTOMIZATION_PROMO),
       base::BindRepeating(&CheckCustomizationPromoEligibility),
       /*show_callback=*/base::DoNothing(),
       base::BindRepeating(&InvokeCustomizationPromo),
@@ -191,7 +191,7 @@ void MaybeRegisterNtpPromos(user_education::NtpPromoRegistry& registry) {
   registry.AddPromo(NtpPromoSpecification(
       kNtpExtensionsPromoId,
       NtpPromoContent("my_extensions", IDS_NTP_EXTENSIONS_PROMO,
-                      IDS_NTP_EXTENSIONS_PROMO_ACTION_BUTTON),
+                      IDS_NTP_EXTENSIONS_PROMO),
       base::BindRepeating(&CheckExtensionsPromoEligibility),
       /*show_callback=*/base::DoNothing(),
       base::BindRepeating(&InvokeExtensionsPromo),
