779abcaf84511f1ce701f1a7c43fd765cc02b637
Gazal <agazal@google.com>
2025-09-22 15:22:19 -0700

    [Settings containment] Apply containment to all radio buttons
    Refactor several radio button group preferences to use the new
    `ContainedRadioButtonGroupPreference` base class. This standardizes the
    implementation of radio button group preferences, reducing code
    duplication.
    
    The new base class removes the outer container's background, allowing
    the individual radio buttons to be styled directly.
    
    Address bar: https://screenshot.googleplex.com/6SNakREKsjgMvFj.png
    Preload page: https://screenshot.googleplex.com/6yVGFQ3ccWBUu3j.png Safe
    Browsing: https://screenshot.googleplex.com/7GadBndKtw8Dbr7.png Toolbar
    shortcut: https://screenshot.googleplex.com/9Z42rqKxV4pnasx.png
    
    Bug: 433576895, 439725777
    Change-Id: Ibb8d0c3961055e7dfe0c567369129cedc612f59c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6968649
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Gazal Agarwal <agazal@google.com>
    Auto-Submit: Gazal Agarwal <agazal@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1518974}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/adaptive/settings/RadioButtonGroupAdaptiveToolbarPreference.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/adaptive/settings/RadioButtonGroupAdaptiveToolbarPreference.java
@@ -14,7 +14,6 @@ import android.widget.RadioGroup;
 
 import androidx.annotation.StringRes;
 import androidx.annotation.VisibleForTesting;
-import androidx.preference.Preference;
 import androidx.preference.PreferenceViewHolder;
 
 import org.chromium.base.metrics.RecordUserAction;
@@ -26,12 +25,13 @@ import org.chromium.chrome.browser.toolbar.adaptive.AdaptiveToolbarButtonVariant
 import org.chromium.chrome.browser.toolbar.adaptive.AdaptiveToolbarStatePredictor;
 import org.chromium.chrome.browser.toolbar.adaptive.AdaptiveToolbarStatePredictor.UiState;
 import org.chromium.chrome.browser.toolbar.adaptive.AdaptiveToolbarStats;
+import org.chromium.components.browser_ui.settings.ContainedRadioButtonGroupPreference;
 import org.chromium.components.browser_ui.widget.RadioButtonWithDescription;
 import org.chromium.components.browser_ui.widget.RadioButtonWithDescriptionLayout;
 
 /** Fragment that allows the user to configure toolbar shortcut preferences. */
 @NullMarked
-public class RadioButtonGroupAdaptiveToolbarPreference extends Preference
+public class RadioButtonGroupAdaptiveToolbarPreference extends ContainedRadioButtonGroupPreference
         implements RadioGroup.OnCheckedChangeListener {
     private @Nullable RadioButtonWithDescriptionLayout mGroup;
     private @Nullable RadioButtonWithDescription mAutoButton;
