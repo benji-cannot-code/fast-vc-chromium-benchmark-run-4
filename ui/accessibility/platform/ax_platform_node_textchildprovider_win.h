d44336a814d34f22cabad4ce71492c33b9017da9
Greg Thompson <grt@chromium.org>
2025-09-22 12:21:03 -0700

    Reland "[A11yPerformance] Enable fast rundown for accessibility objects"
    This is a reland of commit 723fef8a40d4e3a3c32a3d065ae230fea3bb77ee.
    LeakedNodeDetectionBrowsertest.TerminateClient is disabled due to
    failures on some bots.
    
    Original change's description:
    > [A11yPerformance] Enable fast rundown for accessibility objects
    >
    > Chrome vends accessibility objects to an assistive technology tool via
    > COM interface pointers. COM machinery is responsible for releasing
    > outstanding references held by an out-of-process client in the event
    > that said client terminates. This "rundown" processing may take on the
    > order of minutes.
    >
    > In this CL, we add IFastRundown to the set of interfaces implemented by
    > our COM classes. This is a signal to COM that we would like rundown to
    > happen more quickly. Local testing shows that rundown happens after
    > approximately ten seconds rather than six minutes.
    >
    > BYPASS_RECITATION_REASON=False positive; only indentation is changing.
    >
    > AX-Relnotes: n/a.
    > Bug: 443107137
    > Change-Id: Ifacacc3e69fa34a296be513a71528529fe40095c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6917710
    > Auto-Submit: Greg Thompson <grt@chromium.org>
    > Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    > Commit-Queue: Greg Thompson <grt@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1514777}
    
    BYPASS_RECITATION_REASON=False positive; only indentation is changing.
    
    AX-Relnotes: n/a.
    Bug: 443107137
    Change-Id: Id45749d9f73ce4e327bc73e6c7b89c00fa50eb3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6943297
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1518866}

--- ui/accessibility/platform/ax_platform_node_textchildprovider_win.h
+++ ui/accessibility/platform/ax_platform_node_textchildprovider_win.h
@@ -15,10 +15,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ui {
 class COMPONENT_EXPORT(AX_PLATFORM) AXPlatformNodeTextChildProviderWin
     : public SequenceAffineComObjectRoot,
-      public ITextChildProvider {
+      public ITextChildProvider,
+      public IFastRundown {
  public:
   BEGIN_COM_MAP(AXPlatformNodeTextChildProviderWin)
   COM_INTERFACE_ENTRY(ITextChildProvider)
+  COM_INTERFACE_ENTRY(IFastRundown)
   END_COM_MAP()
 
   AXPlatformNodeTextChildProviderWin();
--- ui/accessibility/platform/ax_platform_node_textrangeprovider_win.h
+++ ui/accessibility/platform/ax_platform_node_textrangeprovider_win.h
@@ -20,13 +20,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/platform/sequence_affine_com_object_root_win.h"
 
 namespace ui {
-class COMPONENT_EXPORT(AX_PLATFORM) __declspec(uuid(
-    "3071e40d-a10d-45ff-a59f-6e8e1138e2c1")) AXPlatformNodeTextRangeProviderWin
-    : public SequenceAffineComObjectRoot,
-      public ITextRangeProvider {
+class COMPONENT_EXPORT(AX_PLATFORM)
+    __declspec(uuid("3071e40d-a10d-45ff-a59f-6e8e1138e2c1"))
+    AXPlatformNodeTextRangeProviderWin : public SequenceAffineComObjectRoot,
+                                         public ITextRangeProvider,
+                                         public IFastRundown {
  public:
   BEGIN_COM_MAP(AXPlatformNodeTextRangeProviderWin)
   COM_INTERFACE_ENTRY(ITextRangeProvider)
+  COM_INTERFACE_ENTRY(IFastRundown)
   COM_INTERFACE_ENTRY(AXPlatformNodeTextRangeProviderWin)
   END_COM_MAP()
 
