e942b93041e885dd2d12ff0de930485ce6edf5e3
Gastón Rodríguez <gastonr@microsoft.com>
2025-09-22 14:33:54 -0700

    Fix GestureManager status for in-progress drags
    This CL changes the return value of the HandleDrag function in
    MouseEventManager to explicitly tell its callers what happened with the
    drag attempt. With this change, the GestureManager will now properly
    know if a drag is in progress or not; before it incorrectly assumed that
    a true return from HandleDrag meant that a drag had started.
    
    Currently, MouseEventManager::HandleDrag returns a bool value that only
    represents whether the attempt was handled or not, irrespective of
    whether a drag started or not. A programmer might read the function's
    signature and assume the function returns whether a drag started.
    
    This CL introduces a new enum for this function to return the result of
    the drag attempt, letting callers know if (a) a drag started and (b) if
    the attempt was handled or they need to take further action.
    
    Bug: 435174491
    Change-Id: I2109875da9ae325de2bc653b53bc2c132d6c18ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6937047
    Reviewed-by: Mustaq Ahmed <mustaq@chromium.org>
    Reviewed-by: Olga Gerchikov <gerchiko@microsoft.com>
    Commit-Queue: Gaston Rodriguez <gastonr@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1518947}

--- third_party/blink/renderer/core/input/gesture_manager.cc
+++ third_party/blink/renderer/core/input/gesture_manager.cc
@@ -419,7 +419,8 @@ WebInputEventResult GestureManager::HandleGestureShortPress(
   if (frame_->GetSettings() &&
       frame_->GetSettings()->GetTouchDragDropEnabled() &&
       RuntimeEnabledFeatures::TouchDragOnShortPressEnabled() &&
-      HandleDragDropIfPossible(targeted_event)) {
+      HandleDragDropIfPossible(targeted_event) !=
+          DragHandlingResult::kNotHandled) {
     return WebInputEventResult::kHandledSystem;
   }
   return WebInputEventResult::kNotHandled;
@@ -462,7 +463,8 @@ WebInputEventResult GestureManager::HandleGestureLongPress(
          !hit_test_result.AbsoluteImageURL().IsNull() ||
          !hit_test_result.AbsoluteMediaURL().IsNull());
     if (!should_open_context_menu_now &&
-        HandleDragDropIfPossible(targeted_event)) {
+        HandleDragDropIfPossible(targeted_event) !=
+            DragHandlingResult::kNotHandled) {
       gesture_context_menu_deferred_ = true;
       return WebInputEventResult::kHandledSystem;
     }
@@ -647,11 +649,14 @@ PointerId GestureManager::GetPointerIdFromWebGestureEvent(
       gesture_event.primary_unique_touch_event_id);
 }
 
-bool GestureManager::HandleDragDropIfPossible(
+DragHandlingResult GestureManager::HandleDragDropIfPossible(
     const GestureEventWithHitTestResults& targeted_event) {
-  return drag_in_progress_ = mouse_event_manager_->HandleDragDropIfPossible(
-             targeted_event,
-             GetPointerIdFromWebGestureEvent(targeted_event.Event()));
+  const DragHandlingResult result =
+      mouse_event_manager_->HandleDragDropIfPossible(
+          targeted_event,
+          GetPointerIdFromWebGestureEvent(targeted_event.Event()));
+  drag_in_progress_ = result == DragHandlingResult::kHandledDragStarted;
+  return result;
 }
 
 bool GestureManager::DragEndOpensContextMenu() {
