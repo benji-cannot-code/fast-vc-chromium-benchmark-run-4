5adf1cbc4118a56d75d76cbfa9ef966e843c189e
anthie@google.com <anthie@google.com>
2025-09-22 02:59:43 -0700

    Check history sync optin screen eligibility conditions before displaying
    Also remove early calls to ShouldShowHistorySyncOptinScreen.
    Two former calls of this method happen too early, before the code
    that ensures management or the sync service's startup happen.
    These are no longer necessary as the check is done before the impression
    of the screen and when all necessary decision parameters are known.
    This aligns better with the existing code for the sync consent screen.
    
    Bug: 419741847
    Change-Id: I3101c8029c4c222a94648b7e189141962e53c4b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6965285
    Reviewed-by: David Roger <droger@chromium.org>
    Auto-Submit: Anthi Orfanou <anthie@google.com>
    Reviewed-by: Amelie Schneider <amelies@google.com>
    Commit-Queue: Anthi Orfanou <anthie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1518597}

--- chrome/browser/ui/webui/signin/history_sync_optin_helper_browsertest.cc
+++ chrome/browser/ui/webui/signin/history_sync_optin_helper_browsertest.cc
@@ -99,7 +99,7 @@ class HistorySyncOptinHelperBrowserTest
         /*disabled_features=*/{});
   }
 
-  AccountInfo MakeAccountInfoAvailable() {
+  AccountInfo MakeAccountInfoAvailableAndSignIn() {
     AccountInfo account_info =
         identity_test_env()->MakeAccountAvailable("test@example.com");
     // Fill the account info, in particular for the hosted_domain field.
@@ -108,6 +108,9 @@ class HistorySyncOptinHelperBrowserTest
     account_info.locale = "en";
     account_info.picture_url = "https://example.com";
     identity_test_env()->UpdateAccountInfoForAccount(account_info);
+
+    identity_test_env()->SetPrimaryAccount(account_info.email,
+                                           signin::ConsentLevel::kSignin);
     return account_info;
   }
 
@@ -160,7 +163,10 @@ class HistorySyncOptinHelperBrowserTest
 IN_PROC_BROWSER_TEST_P(
     HistorySyncOptinHelperBrowserTest,
     TriggersHistorySyncScreenWhenAccountInfoFetchedForConsumerAccount) {
-  AccountInfo account_info = MakeAccountInfoAvailable();
+  GetTestSyncService()->GetUserSettings()->SetSelectedTypes(
+      /*sync_everything=*/false, syncer::UserSelectableTypeSet());
+
+  AccountInfo account_info = MakeAccountInfoAvailableAndSignIn();
   MockHistorySyncOptinHelperDelegate delegate;
 
   EXPECT_CALL(delegate, ShowHistorySyncOptinScreen(GetProfile(), testing::_))
@@ -181,7 +187,10 @@ IN_PROC_BROWSER_TEST_P(
 IN_PROC_BROWSER_TEST_P(
     HistorySyncOptinHelperBrowserTest,
     TriggersManagedAccountScreenThenHistorySyncOptinScreenForManagedAccount) {
-  AccountInfo account_info = MakeAccountInfoAvailable();
+  GetTestSyncService()->GetUserSettings()->SetSelectedTypes(
+      /*sync_everything=*/false, syncer::UserSelectableTypeSet());
+
+  AccountInfo account_info = MakeAccountInfoAvailableAndSignIn();
   MockHistorySyncOptinHelperDelegate delegate;
 
   base::test::TestFuture<Profile*> future;
@@ -231,7 +240,7 @@ IN_PROC_BROWSER_TEST_P(
 IN_PROC_BROWSER_TEST_P(
     HistorySyncOptinHelperBrowserTest,
     SkipsHistorySyncOptinScreenWhenUserRejectsManagementForManagedAccount) {
-  AccountInfo account_info = MakeAccountInfoAvailable();
+  AccountInfo account_info = MakeAccountInfoAvailableAndSignIn();
   MockHistorySyncOptinHelperDelegate delegate;
 
   base::test::TestFuture<void> future;
@@ -277,7 +286,11 @@ IN_PROC_BROWSER_TEST_P(
 IN_PROC_BROWSER_TEST_P(
     HistorySyncOptinHelperBrowserTest,
     TriggersHistorySyncScreenWhenAccountInfoFetchingTimesOut) {
-  AccountInfo account_info = MakeAccountInfoAvailable();
+  GetTestSyncService()->GetUserSettings()->SetSelectedTypes(
+      /*sync_everything=*/false, syncer::UserSelectableTypeSet());
+
+  AccountInfo account_info = MakeAccountInfoAvailableAndSignIn();
+
   MockHistorySyncOptinHelperDelegate delegate;
 
   EXPECT_CALL(delegate, ShowHistorySyncOptinScreen).Times(0);
@@ -299,12 +312,15 @@ IN_PROC_BROWSER_TEST_P(
 
 IN_PROC_BROWSER_TEST_P(HistorySyncOptinHelperBrowserTest,
                        WaitsForSyncServiceBeforeTriggeringHistorySyncScreen) {
+  GetTestSyncService()->GetUserSettings()->SetSelectedTypes(
+      /*sync_everything=*/false, syncer::UserSelectableTypeSet());
+
   // Set the sync service in pending state.
   GetTestSyncService()->SetMaxTransportState(
       syncer::SyncService::TransportState::INITIALIZING);
 
-  AccountInfo account_info = MakeAccountInfoAvailable();
-  UpdateAccountManagementInfo(account_info, /*is_managed=*/false);
+  AccountInfo account_info = MakeAccountInfoAvailableAndSignIn();
+  UpdateAccountManagementInfo(account_info, false);
   MockHistorySyncOptinHelperDelegate delegate;
 
   auto history_sync_optin_helper = HistorySyncOptinHelper::Create(
@@ -333,8 +349,8 @@ IN_PROC_BROWSER_TEST_P(HistorySyncOptinHelperBrowserTest,
   // Disable the sync service.
   GetTestSyncService()->SetAllowedByEnterprisePolicy(false);
 
-  AccountInfo account_info = MakeAccountInfoAvailable();
-  UpdateAccountManagementInfo(account_info, /*is_managed=*/false);
+  AccountInfo account_info = MakeAccountInfoAvailableAndSignIn();
+  UpdateAccountManagementInfo(account_info, false);
   MockHistorySyncOptinHelperDelegate delegate;
 
   EXPECT_CALL(delegate, ShowHistorySyncOptinScreen).Times(0);
--- chrome/browser/ui/webui/signin/history_sync_optin_service_unittest.cc
+++ chrome/browser/ui/webui/signin/history_sync_optin_service_unittest.cc
@@ -79,6 +79,11 @@ class HistorySyncOptinServiceTest : public testing::Test {
 
 TEST_F(HistorySyncOptinServiceTest, StartFlow) {
   AccountInfo account_info = MakePrimaryAccountAvailable();
+  SyncServiceFactory::GetForProfile(profile_.get())
+      ->GetUserSettings()
+      ->SetSelectedTypes(
+          /*sync_everything=*/false, syncer::UserSelectableTypeSet());
+
   auto delegate = std::make_unique<MockHistorySyncOptinHelperDelegate>();
   auto* delegate_ptr = delegate.get();
   EXPECT_CALL(*delegate_ptr,
@@ -91,6 +96,11 @@ TEST_F(HistorySyncOptinServiceTest, StartFlow) {
 
 TEST_F(HistorySyncOptinServiceTest, AbortFlowIfOneInProgress) {
   AccountInfo account_info = MakePrimaryAccountAvailable();
+  SyncServiceFactory::GetForProfile(profile_.get())
+      ->GetUserSettings()
+      ->SetSelectedTypes(
+          /*sync_everything=*/false, syncer::UserSelectableTypeSet());
+
   auto delegate = std::make_unique<MockHistorySyncOptinHelperDelegate>();
   auto* delegate_ptr = delegate.get();
 
