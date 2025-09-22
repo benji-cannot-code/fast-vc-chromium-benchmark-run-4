7dc5c8fbb1b3c884b77d06cac229a72d33033588
Gauthier Ambard <gambard@chromium.org>
2025-09-22 10:24:33 -0700

    [iOS] Add a ContentView for an image
    This CL adds a content view for an image with a custom size/content
    mode.
    This is used in downloads to display the type of the downloaded file.
    
    Bug: 443034511
    Change-Id: I2c67bb52cf794c366c456648216ad133658fa201
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6964721
    Reviewed-by: Quentin Pubert <qpubert@google.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1518786}

---
+++ ios/chrome/browser/shared/ui/table_view/cells/image_content_configuration.mm
@@ -0,0 +1,43 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/shared/ui/table_view/cells/image_content_configuration.h"
+
+#import "ios/chrome/browser/shared/ui/table_view/cells/image_content_view.h"
+
+@implementation ImageContentConfiguration
+
+- (instancetype)init {
+  self = [super init];
+  if (self) {
+    _imageContentMode = UIViewContentModeScaleAspectFit;
+  }
+  return self;
+}
+
+#pragma mark - UIContentConfiguration
+
+- (UIView<UIContentView>*)makeContentView {
+  return [[ImageContentView alloc] initWithConfiguration:self];
+}
+
+- (instancetype)updatedConfigurationForState:(id<UIConfigurationState>)state {
+  // No state changes supported for now.
+  return self;
+}
+
+#pragma mark - NSCopying
+
+- (id)copyWithZone:(NSZone*)zone {
+  ImageContentConfiguration* copy = [[ImageContentConfiguration alloc] init];
+  // LINT.IfChange(Copy)
+  copy.image = self.image;
+  copy.imageSize = self.imageSize;
+  copy.imageContentMode = self.imageContentMode;
+  // LINT.ThenChange(image_content_configuration.h:Copy)
+  return copy;
+}
+
+@end
