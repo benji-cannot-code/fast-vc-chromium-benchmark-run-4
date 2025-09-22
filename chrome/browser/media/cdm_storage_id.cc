9e6a747d1b2c0ffd2368c0b16cc5993ea4e4f52e
Elly <ellyjones@chromium.org>
2025-09-22 08:23:05 -0700

    media: use new hash API in cdm_storage_id
    The new API is functionally equivalent but not deprecated (and takes
    spans instead of pointers/length pairs).
    
    Bug: 364687923
    Change-Id: I0ddaa8bc7e8d3b7d6253881e562cce1e0f8ec6bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6931187
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1518710}

--- chrome/browser/media/cdm_storage_id.cc
+++ chrome/browser/media/cdm_storage_id.cc
@@ -10,8 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "chrome/browser/media/cdm_storage_id_key.h"
 #include "chrome/browser/media/media_storage_id_salt.h"
-#include "crypto/secure_hash.h"
-#include "crypto/sha2.h"
+#include "crypto/hash.h"
 #include "media/media_buildflags.h"
 #include "rlz/buildflags/buildflags.h"
 #include "url/origin.h"
@@ -69,15 +68,14 @@ std::vector<uint8_t> CalculateStorageId(
   // Build the identifier as follows:
   // SHA256(machine_id + origin + storage_id_key + profile_salt)
   std::string origin_str = origin.Serialize();
-  std::unique_ptr<crypto::SecureHash> sha256 =
-      crypto::SecureHash::Create(crypto::SecureHash::SHA256);
-  sha256->Update(machine_id.data(), machine_id.length());
-  sha256->Update(origin_str.data(), origin_str.length());
-  sha256->Update(storage_id_key.data(), storage_id_key.length());
-  sha256->Update(profile_salt.data(), profile_salt.size());
+  crypto::hash::Hasher sha256(crypto::hash::kSha256);
+  sha256.Update(machine_id);
+  sha256.Update(origin_str);
+  sha256.Update(storage_id_key);
+  sha256.Update(profile_salt);
 
-  std::vector<uint8_t> result(crypto::kSHA256Length);
-  sha256->Finish(result.data(), result.size());
+  std::vector<uint8_t> result(crypto::hash::kSha256Size);
+  sha256.Finish(result);
   return result;
 }
 
