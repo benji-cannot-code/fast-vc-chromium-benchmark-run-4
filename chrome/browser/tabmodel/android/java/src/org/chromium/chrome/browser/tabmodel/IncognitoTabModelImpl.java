763e95f1d39176b063277392195ca03206d82e3a
Dan Polanco <polardz@google.com>
2025-09-22 11:00:05 -0700

    [Media Indicator] Add context menu option for mute/unmute
    This CL
    -Adds mute site setting method in Tab Model.
    -Mute/unmute item list in tab context menu (LFF).
    
    http://go/scrcast/NTgxNDQ1NjM2OTAyMDkyOHw3ZTVhNjc4MS0xNA
    
    Bug: 430072416
    Change-Id: I8aed943286e9a65d4d3269f74326be5c8e43c9d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6941502
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Auto-Submit: Dan Polanco <polardz@google.com>
    Commit-Queue: Dan Polanco <polardz@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1518803}

--- chrome/browser/tabmodel/android/java/src/org/chromium/chrome/browser/tabmodel/IncognitoTabModelImpl.java
+++ chrome/browser/tabmodel/android/java/src/org/chromium/chrome/browser/tabmodel/IncognitoTabModelImpl.java
@@ -19,6 +19,7 @@ import org.chromium.chrome.browser.tab.TabLaunchType;
 import org.chromium.chrome.browser.tab.TabSelectionType;
 
 import java.util.Iterator;
+import java.util.List;
 import java.util.OptionalInt;
 import java.util.Set;
 
@@ -423,5 +424,15 @@ class IncognitoTabModelImpl implements IncognitoTabModelInternal {
     @Override
     public OptionalInt getNativeSessionIdForTesting() {
         return mDelegateModel.getNativeSessionIdForTesting();
+    }
+
+    @Override
+    public void setMuteSetting(List<Tab> tabs, boolean mute) {
+        mDelegateModel.setMuteSetting(tabs, mute);
+    }
+
+    @Override
+    public boolean isMuted(Tab tab) {
+        return mDelegateModel.isMuted(tab);
     }
 }
