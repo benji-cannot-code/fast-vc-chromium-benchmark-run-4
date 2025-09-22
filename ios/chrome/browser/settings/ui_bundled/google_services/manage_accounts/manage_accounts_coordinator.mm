07c7d6c29ac9b321bb2082fb41b957044668e2ba
Jérôme Lebel <jlebel@chromium.org>
2025-09-22 11:04:43 -0700

    [iOS] dimiss alert dialog in ManageAccountsCoordinator
    Change-Id: I83a4609faada303c26a7096761e9082bc8600e1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6973540
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1518810}

--- ios/chrome/browser/settings/ui_bundled/google_services/manage_accounts/manage_accounts_coordinator.mm
+++ ios/chrome/browser/settings/ui_bundled/google_services/manage_accounts/manage_accounts_coordinator.mm
@@ -145,6 +145,7 @@ using signin_metrics::PromoAction;
   _mediator.consumer = nil;
   [_mediator disconnect];
   _mediator = nil;
+  [self stopConfirmRemoveIdentityAlertCoordinator];
   _UIBlocker.reset();
 }
 
@@ -274,7 +275,7 @@ using signin_metrics::PromoAction;
 // current scene is blocked or the identity is not present on the device
 // anymore.
 - (void)removeAccountDialogConfirmedWithIdentity:(id<SystemIdentity>)identity {
-  [self dismissConfirmRemoveIdentityAlertCoordinator];
+  [self stopConfirmRemoveIdentityAlertCoordinator];
 
   NSArray<id<SystemIdentity>>* identitiesOnDevice =
       signin::GetIdentitiesOnDevice(self.profile);
@@ -333,7 +334,7 @@ using signin_metrics::PromoAction;
   _errorAlertCoordinator = nil;
 }
 
-- (void)dismissConfirmRemoveIdentityAlertCoordinator {
+- (void)stopConfirmRemoveIdentityAlertCoordinator {
   [_confirmRemoveIdentityAlertCoordinator stop];
   _confirmRemoveIdentityAlertCoordinator = nil;
 }
