604e1d71348939d2bc57d81384efced41188ae34
Jun Ishiguro <junis@google.com>
2025-09-22 02:10:55 -0700

    Split ArcApps initialization into two parts
    ArcApps is a AppService publisher. However, unlike other publisher, it
    is a KeyedService and is created/registered lazily.
    
    To make the registration timing be aligned with other publishers, this
    CL splits the initialization into two parts.
    
    1) Initialize() to register ArcApps to AppServiceProxy as a publisher.
       This is currently called when an instance is created by the factory.
       In a future CL, this timing will be moved to an earlier timing
       similar to other publishers.
    
    2) OnInitialized() to complete the rest of the initialization tasks.
       This is called when ArcSessionManager is initialized. The timing
       should be close enough to the current behavior.
    
    In next CL, we'll change ArcApps not to be a KeyedService, and create
    and register the instance at the same timing as other publisher.
    
    Bug: b:442761233
    Change-Id: Ica00224a80e8e79903f223c2010fb52133523787
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6954708
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1518576}

--- chrome/browser/apps/app_service/publishers/arc_apps.h
+++ chrome/browser/apps/app_service/publishers/arc_apps.h
@@ -32,6 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/app_list/arc/arc_app_list_prefs.h"
 #include "chrome/browser/ash/arc/app_shortcuts/arc_app_shortcuts_request.h"
 #include "chrome/browser/ash/arc/privacy_items/arc_privacy_items_bridge.h"
+#include "chrome/browser/ash/arc/session/arc_session_manager.h"
+#include "chrome/browser/ash/arc/session/arc_session_manager_observer.h"
 #include "chromeos/ash/experiences/arc/intent_helper/arc_intent_helper_bridge.h"
 #include "chromeos/ash/experiences/arc/intent_helper/arc_intent_helper_observer.h"
 #include "chromeos/ash/experiences/arc/mojom/app_permissions.mojom.h"
@@ -59,6 +61,7 @@ struct AppLaunchParams;
 // See components/services/app_service/README.md.
 class ArcApps : public KeyedService,
                 public AppPublisher,
+                public arc::ArcSessionManagerObserver,
                 public ArcAppListPrefs::Observer,
                 public arc::ArcIntentHelperObserver,
                 public ash::ArcNotificationManagerBase::Observer,
@@ -88,9 +91,6 @@ class ArcApps : public KeyedService,
   using TaskIdToAppId = std::map<int, std::string>;
 
   void Initialize();
-
-  // KeyedService overrides.
-  void Shutdown() override;
 
   // apps::AppPublisher overrides.
   void GetCompressedIconData(const std::string& app_id,
@@ -136,6 +136,10 @@ class ArcApps : public KeyedService,
   void OpenNativeSettings(const std::string& app_id) override;
   void OnSupportedLinksPreferenceChanged(const std::string& app_id,
                                          bool open_in_app) override;
+
+  // ArcSessionManagerObserver oveerrides:
+  void OnInitialized() override;
+  void OnShutdown() override;
 
   // ArcAppListPrefs::Observer overrides.
   void OnAppRegistered(const std::string& app_id,
@@ -254,6 +258,10 @@ class ArcApps : public KeyedService,
   std::unique_ptr<arc::ArcAppShortcutsRequest> arc_app_shortcuts_request_;
 
   std::unique_ptr<apps::WebApkManager> web_apk_manager_;
+
+  base::ScopedObservation<arc::ArcSessionManager,
+                          arc::ArcSessionManagerObserver>
+      arc_session_manager_observation_{this};
 
   base::ScopedObservation<ArcAppListPrefs, ArcAppListPrefs::Observer>
       arc_app_list_prefs_observation_{this};
