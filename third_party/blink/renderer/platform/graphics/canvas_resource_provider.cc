d55c636fc0e88721f3ae2ab0e2116f78ae3d8fd5
Colin Blundell <blundell@chromium.org>
2025-09-22 00:53:30 -0700

    [Blink] Streamline CRPSI::WillDrawInternal() post-OOP-C
    `is_accelerated_` now implies `use_oop_rasterization_`.
    
    Bug: 391648152
    Change-Id: I18d10bc97e92dfa8978e13c3aa80f0dd45fc0c1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6964724
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1518549}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
@@ -530,10 +530,6 @@ void CanvasResourceProviderSharedImage::WillDrawInternal(
     resource_ = NewOrRecycledResource();
     DCHECK(IsResourceUsable(resource_.get()));
 
-    if (!use_oop_rasterization_) {
-      TearDownSkSurface();
-    }
-
     if (mode_ == SkSurface::kRetain_ContentChangeMode) {
       auto old_mailbox =
           old_resource_shared_image->GetClientSharedImage()->mailbox();
@@ -547,13 +543,6 @@ void CanvasResourceProviderSharedImage::WillDrawInternal(
       is_cleared_ = false;
     }
 
-    // In non-OOPR mode we need to update the client side SkSurface with the
-    // copied texture. Recreating SkSurface here matches the GPU process
-    // behaviour that will happen in OOPR mode.
-    if (!use_oop_rasterization_) {
-      EnsureWriteAccess();
-      GetSkSurface();
-    }
     UMA_HISTOGRAM_BOOLEAN("Blink.Canvas.ContentChangeMode",
                           mode_ == SkSurface::kRetain_ContentChangeMode);
     mode_ = SkSurface::kRetain_ContentChangeMode;
@@ -1951,11 +1940,6 @@ void CanvasResourceProvider::RestoreBackBuffer(const cc::PaintImage& image) {
   // PaintImage::GetSwSkImage above
   sk_image->peekPixels(&map);
   WritePixels(map.info(), map.addr(), map.rowBytes(), /*x=*/0, /*y=*/0);
-}
-
-void CanvasResourceProvider::TearDownSkSurface() {
-  skia_canvas_ = nullptr;
-  surface_ = nullptr;
 }
 
 size_t CanvasResourceProvider::ComputeSurfaceSize() const {
