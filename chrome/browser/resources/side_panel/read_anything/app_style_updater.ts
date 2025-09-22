8894d76c1e6affb2ee69a16a11e4c47598f257c2
Martin GL <martinglopez@google.com>
2025-09-22 09:00:50 -0700

    Adding new themes to reading mode for audiences with different visual needs:
    - High contrast
    - Low contrast
    - Sepia light
    - Sepia dark dark
    
    Additional context and screenshots of new themes in slides:
    https://docs.google.com/presentation/d/1XtEL9sGiahmOFkkFsquZUclFv0NNZvkZy0A0PnisPZ4/edit?usp=sharing&resourcekey=0-518-ELiIvkW0_rNbFywvmQ
    
    The new themes will align Chrome reading mode with the Android
    reading mode.
    
    The colors for the new themes prefer user research findings for
    certain user needs over general accessibility contrast guidelines.
    
    Color changes were approved in Chrome UX and Chrome A11y office
    hours.
    
    This CL also adds the new themes to the "color theme" test, and the
    shas of the screenshots of the new options in the theme menu for
    translation.
    
    Bug: 401513825
    Change-Id: I8b83a84ecb6150d489b8a4c4264fb5e465ea308b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6859039
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Martín Gómez <martinglopez@google.com>
    Reviewed-by: Evan Liu <evliu@google.com>
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1518737}

--- chrome/browser/resources/side_panel/read_anything/app_style_updater.ts
+++ chrome/browser/resources/side_panel/read_anything/app_style_updater.ts
@@ -46,6 +46,10 @@ enum ColorSuffix {
   LIGHT = '-light',
   YELLOW = '-yellow',
   BLUE = '-blue',
+  HIGH_CONTRAST = '-high-contrast',
+  LOW_CONTRAST = '-low-contrast',
+  SEPIA_LIGHT = '-sepia-light',
+  SEPIA_DARK = '-sepia-dark',
 }
 
 // Handles updating the visual styles for the Reading mode content panel.
@@ -154,6 +158,14 @@ export class AppStyleUpdater {
         return ColorSuffix.YELLOW;
       case chrome.readingMode.blueTheme:
         return ColorSuffix.BLUE;
+      case chrome.readingMode.highContrastTheme:
+        return ColorSuffix.HIGH_CONTRAST;
+      case chrome.readingMode.lowContrastTheme:
+        return ColorSuffix.LOW_CONTRAST;
+      case chrome.readingMode.sepiaLightTheme:
+        return ColorSuffix.SEPIA_LIGHT;
+      case chrome.readingMode.sepiaDarkTheme:
+        return ColorSuffix.SEPIA_DARK;
       default:
         return ColorSuffix.DEFAULT;
     }
