cdbbafc52bde8edb8bd2a2fd8306f760f4af1bed
James Cook <jamescook@chromium.org>
2025-09-22 12:23:12 -0700

    extensions: Port webNavigation API to desktop Android, part 3
    This CL refactors code and enables the compilation of some web
    navigation files on desktop Android. It does not bring up any new
    functionality - that will come later.
    
    In particular, WebNavigationEventRouter gains a "TabHelper" class to
    abstract away the TabStripModel used by Win/Mac/Linux. A future CL
    will add a TabHelper implementation that works with Android. I
    didn't add it in this CL to keep the CL from becoming too large.
    
    Once we have a working WebNavigationEventRouter on Android we can
    start bringing up more tests, so those will be in later CLs too.
    
    Bug: 371432404
    Change-Id: I29098e632b512af6bd6623c7b56353c88b40b381
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6969971
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1518868}

--- chrome/browser/extensions/api/web_navigation/web_navigation_tab_observer.cc
+++ chrome/browser/extensions/api/web_navigation/web_navigation_tab_observer.cc
@@ -22,8 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_errors.h"
 #include "url/gurl.h"
 
-class TabStripModel;
-
 namespace extensions {
 
 namespace web_navigation = api::web_navigation;
@@ -217,10 +215,14 @@ void WebNavigationTabObserver::DidOpenRequestedURL(
     return;
   }
 
-  TabStripModel* ignored_tab_strip_model = nullptr;
+  bool new_contents_is_present_in_tabstrip = false;
+  TabListInterface* ignored_tab_list_interface = nullptr;
   int ignored_tab_index = -1;
-  bool new_contents_is_present_in_tabstrip = ExtensionTabUtil::GetTabStripModel(
-      new_contents, &ignored_tab_strip_model, &ignored_tab_index);
+  // It's possible `new_contents` could be null.
+  if (new_contents) {
+    new_contents_is_present_in_tabstrip = ExtensionTabUtil::GetTabListInterface(
+        *new_contents, &ignored_tab_list_interface, &ignored_tab_index);
+  }
   router->RecordNewWebContents(
       web_contents(), source_render_frame_host->GetProcess()->GetDeprecatedID(),
       source_render_frame_host->GetRoutingID(), url, new_contents,
