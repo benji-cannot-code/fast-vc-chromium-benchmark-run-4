924fe4bea13780edb2aa41453555c303b58ea9f8
Vinny Persky <vinnypersky@google.com>
2025-09-22 16:45:40 -0700

    Remove amount extraction allowlist flag
    This killswitch has been enabled by default since M135 and can be
    removed.
    
    Bug: 443823882
    Change-Id: I3132c3f7eeda9e6a507727b4c386b6f4efbc740f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6974328
    Reviewed-by: Olivia Saul <osaul@google.com>
    Commit-Queue: Vinny Persky <vinnypersky@google.com>
    Cr-Commit-Position: refs/heads/main@{#1519014}

--- components/autofill/core/common/autofill_payments_features.h
+++ components/autofill/core/common/autofill_payments_features.h
@@ -28,8 +28,6 @@ BASE_DECLARE_FEATURE(kAutofillEnableAllowlistForBmoCardCategoryBenefits);
 COMPONENT_EXPORT(AUTOFILL)
 BASE_DECLARE_FEATURE(kAutofillEnableAmountExtraction);
 COMPONENT_EXPORT(AUTOFILL)
-BASE_DECLARE_FEATURE(kAutofillEnableAmountExtractionAllowlist);
-COMPONENT_EXPORT(AUTOFILL)
 BASE_DECLARE_FEATURE(kAutofillEnableAmountExtractionTesting);
 COMPONENT_EXPORT(AUTOFILL)
 BASE_DECLARE_FEATURE(kAutofillEnableBuyNowPayLater);
