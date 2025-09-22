5cd57007b38a884de4e01f4963fcd8c48534eac8
Colin Blundell <blundell@chromium.org>
2025-09-22 00:55:52 -0700

    [Blink] Have BaseRC2D hold CRP from transfer as CRPSI
    This is guaranteed to be a CRPSI [1].
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/6965151
    
    Bug: 352263194
    Change-Id: Iafe2e58d89f39100ffd634be1e321b35dc615da1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6966120
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1518550}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
@@ -592,6 +592,7 @@ class PLATFORM_EXPORT CanvasResourceProviderSharedImage
                    size_t row_bytes,
                    int x,
                    int y) override;
+  void ClearUnusedResources() override { unused_resources_.clear(); }
 
  protected:
   scoped_refptr<CanvasResourceSharedImage> CreateResource();
@@ -608,7 +609,6 @@ class PLATFORM_EXPORT CanvasResourceProviderSharedImage
     scoped_refptr<CanvasResourceSharedImage> resource;
   };
 
-  void ClearUnusedResources() override { unused_resources_.clear(); }
   void RegisterUnusedResource(
       scoped_refptr<CanvasResourceSharedImage>&& resource);
   scoped_refptr<CanvasResourceSharedImage> NewOrRecycledResource();
