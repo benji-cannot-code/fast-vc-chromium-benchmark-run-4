e1467feedb0ad8002d879b1af59387a087482ac5
Joe Mason <joenotcharles@google.com>
2025-09-22 09:02:43 -0700

    Add Resource Attribution query timing metrics
    These will be used to measure performance differences when the map
    implementation in Resource Attribution changes. They're set to expire
    in M145, the same time frame that VariantMap will be removed.
    
    R=olivierli
    
    Bug: 433462519
    Change-Id: Ia6b354e250a583246ab5dad805d9a6f4d9479add
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6966377
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Olivier Li <olivierli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1518739}

--- components/performance_manager/resource_attribution/memory_measurement_provider.cc
+++ components/performance_manager/resource_attribution/memory_measurement_provider.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/contains.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/time/time.h"
 #include "components/performance_manager/public/features.h"
@@ -100,6 +101,10 @@ void MemoryMeasurementProvider::OnMemorySummary(
       MemoryMeasurementDelegate::MemorySummaryMeasurement;
 
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  base::ScopedUmaHistogramTimer histogram_timer(
+      "PerformanceManager.ResourceQueryTime.MemoryMeasurementProvider",
+      base::ScopedUmaHistogramTimer::ScopedHistogramTiming::kMicrosecondTimes);
+
   QueryResultMap results;
 
   // Adds the memory from `summary` to a MemorySummaryResult for `context`.
