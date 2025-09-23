07b70a05b97fe101fd0b4c75c76686e7c9294d0b
Peter Kvitek <kvitekp@chromium.org>
2025-09-23 11:10:33 -0700

    [headless] Add SetZoomedWindowBounds test
    This CL adds a test to verify result of Browser.setWindowBounds() on a
    target that is in maximized or full screen state.
    
    Bug: 446247998
    Change-Id: I8234e6edb7aa04a305b9e7a76faa855be986b742
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6969712
    Reviewed-by: Alex N. Jose <alexnj@chromium.org>
    Commit-Queue: Alex N. Jose <alexnj@chromium.org>
    Auto-Submit: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1519475}

--- chrome/browser/headless/headless_mode_protocol_browsertest.cc
+++ chrome/browser/headless/headless_mode_protocol_browsertest.cc
@@ -521,4 +521,7 @@ HEADLESS_MODE_PROTOCOL_TEST(RemoveScreenGetScreenDetails,
 
 HEADLESS_MODE_PROTOCOL_TEST(AddRemoveScreen, "shared/add-remove-screen.js")
 
+HEADLESS_MODE_PROTOCOL_TEST(SetZoomedWindowBounds,
+                            "shared/set-zoomed-window-bounds.js")
+
 }  // namespace headless
