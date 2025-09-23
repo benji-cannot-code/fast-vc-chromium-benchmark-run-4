eb6768180b1f53b6ef70d4789f90e1d070ccac2a
Colin Blundell <blundell@chromium.org>
2025-09-23 00:07:57 -0700

    [Blink] Remove CanvasResourceProvider::UseHardwareDecodeCache()
    Inline its body at callsites to make it more obvious what the conditions
    for call WebGraphicsContext3DProvider::ImageDecodeCache() are.
    
    Bug: 391648152
    Change-Id: I3d58bea73bb98dc3387f342efeffb1ffeba691b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6972936
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1519148}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
@@ -1657,7 +1657,9 @@ CanvasResourceProvider::GetOrCreateCanvasImageProvider() {
     }
 
     auto raster_mode = cc::PlaybackImageProvider::RasterMode::kSoftware;
-    if (UseHardwareDecodeCache()) {
+
+    // Adjust the raster mode if we will be able to use an accelerated cache.
+    if (IsAccelerated() && context_provider_wrapper_) {
       raster_mode = UseOopRasterization()
                         ? cc::PlaybackImageProvider::RasterMode::kOop
                         : cc::PlaybackImageProvider::RasterMode::kGpu;
@@ -1911,7 +1913,7 @@ uint32_t CanvasResourceProvider::ContentUniqueID() const {
 }
 
 cc::ImageDecodeCache* CanvasResourceProvider::ImageDecodeCacheRGBA8() {
-  if (UseHardwareDecodeCache()) {
+  if (IsAccelerated() && context_provider_wrapper_) {
     return context_provider_wrapper_->ContextProvider().ImageDecodeCache(
         kN32_SkColorType);
   }
@@ -1920,7 +1922,7 @@ cc::ImageDecodeCache* CanvasResourceProvider::ImageDecodeCacheRGBA8() {
 }
 
 cc::ImageDecodeCache* CanvasResourceProvider::ImageDecodeCacheF16() {
-  if (UseHardwareDecodeCache()) {
+  if (IsAccelerated() && context_provider_wrapper_) {
     return context_provider_wrapper_->ContextProvider().ImageDecodeCache(
         kRGBA_F16_SkColorType);
   }
