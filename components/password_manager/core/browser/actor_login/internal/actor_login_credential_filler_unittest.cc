803abf6c916efa50f5b45c1e9ba94e0d49120164
Ioana Pandele <ioanap@chromium.org>
2025-09-22 23:36:32 -0700

    Tear down OSCryptMocker in ActorLoginCredentialFillerTest
    Change-Id: I8a64c0faa5ccedecaa9d37dcb2a57445b3a26e6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6973783
    Auto-Submit: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Oleksandr Tara <otara@google.com>
    Commit-Queue: Oleksandr Tara <otara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1519124}

--- components/password_manager/core/browser/actor_login/internal/actor_login_credential_filler_unittest.cc
+++ components/password_manager/core/browser/actor_login/internal/actor_login_credential_filler_unittest.cc
@@ -148,6 +148,8 @@ class ActorLoginCredentialFillerTest : public ::testing::Test {
         .WillByDefault(Return(false));
   }
 
+  void TearDown() override { OSCryptMocker::TearDown(); }
+
   std::unique_ptr<PasswordFormManager> CreateFormManagerWithParsedForm(
       const url::Origin& origin,
       const autofill::FormData& form_data) {
