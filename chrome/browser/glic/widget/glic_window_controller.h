68c76e136aa1c1b7233f1639b2c8017275ebe6dd
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2025-09-23 01:40:06 -0700

    Revert "Glic multi instance: remove GetHost* methods, use GetInstance*"
    This reverts commit 5ea96695cd9edaf11895f2fce932d7baa37f71bb.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/4794301725802496
    
    Sample build with failed test: https://ci.chromium.org/b/8702970132281692769
    Affected test(s):
    [ninja://chrome/test:browser_tests/ActorTabManagementToolBrowserTest.TabManagementTool_RecordActingOnTask](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Ftest:browser_tests%2FActorTabManagementToolBrowserTest.TabManagementTool_RecordActingOnTask?q=VHash%3A974ca52e51a57973)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F4794301725802496&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F6951964&type=BUG
    
    Original change's description:
    > Glic multi instance: remove GetHost* methods, use GetInstance*
    >
    > Some external callers will need access to glic UI properties that will
    > come from GlicInstance and not Host. Callers that need Host can access
    > it through GlicInstance.
    >
    > Also start looking up the GlicInstance based on the last acted tab in
    > GlicActorTaskIconManager.
    >
    > Bug: b:440969040
    > Change-Id: I1b2a0c246101f322ad795885542044587425b100
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6951964
    > Reviewed-by: Tommy Steimel <steimel@chromium.org>
    > Commit-Queue: Ian Wells <iwells@chromium.org>
    > Reviewed-by: Alison Gale <agale@chromium.org>
    > Reviewed-by: Trevor Perrier <perrier@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1519057}
    >
    
    Bug: b:440969040
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ic71ae94c4cdae21783f66f9198cce500c8fd066e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6974273
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1519190}

--- chrome/browser/glic/widget/glic_window_controller.h
+++ chrome/browser/glic/widget/glic_window_controller.h
@@ -50,6 +50,7 @@ class GlicWidget;
 class GlicKeyedService;
 class GlicView;
 class GlicWindowAnimator;
+class Host;
 enum class AttachChangeReason;
 
 // This class owns and manages the glic window. This class has the same lifetime
@@ -77,6 +78,10 @@ class GlicWindowController : public GlicInstance::UIDelegate {
   GlicWindowController() = default;
 
   virtual HostManager& host_manager() = 0;
+
+  virtual std::vector<Host*> GetHosts() = 0;
+  virtual Host* GetHostForTab(tabs::TabInterface* tab) = 0;
+  // TODO(refactor): remove GetHosts and GetHostForTab and use these instead.
   virtual std::vector<GlicInstance*> GetInstances() = 0;
   virtual GlicInstance* GetInstanceForTab(tabs::TabInterface* tab) = 0;
 
--- chrome/browser/ui/tabs/glic_actor_task_icon_controller.cc
+++ chrome/browser/ui/tabs/glic_actor_task_icon_controller.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_features.h"
 
 namespace tabs {
-using glic::GlicInstance;
 using glic::GlicKeyedService;
 using glic::GlicWindowController;
 using glic::Host;
@@ -59,11 +58,8 @@ void GlicActorTaskIconController::UpdateCurrentTaskIconUiState() {
     auto* glic_service = GlicKeyedService::Get(profile_);
 
     CurrentView current_view = CurrentView::kConversation;
-    // TODO(crbug.com/446734119): Instead ActorTask should hold a glic
-    // InstanceId and use that to retrieve the instance.
-    if (GlicInstance* instance =
-            glic_service->GetInstanceForActiveTab(browser_)) {
-      current_view = instance->host().GetPrimaryCurrentView();
+    if (Host* host = glic_service->GetHostForActiveTab(browser_)) {
+      current_view = host->GetPrimaryCurrentView();
     }
 
     OnStateUpdate(glic_service->window_controller().state(), current_view,
