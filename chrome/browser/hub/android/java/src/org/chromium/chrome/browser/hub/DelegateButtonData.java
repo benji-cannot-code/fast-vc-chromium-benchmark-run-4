c6d8064516f98e8562f18e0cd4cd0f7f18ff3d1a
Jingping Sun <jings@microsoft.com>
2025-09-23 08:46:07 -0700

    [Hub] Add hub pane switcher update method
    Add the pane switcher updating method for hub toolbar. The current
    update mechanism for the pane switcher button seems heavy, since it
    rebuilds completely even when only the content of one pane changes.
    The new updating method can optimize the UI update logic to enhance
    performance.
    
    Bug: 441413529
    Change-Id: Iddfa2a38c79bbd3ac338ca2b3aad01901ca362a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6928885
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Jingping Sun <jings@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1519377}

--- chrome/browser/hub/android/java/src/org/chromium/chrome/browser/hub/DelegateButtonData.java
+++ chrome/browser/hub/android/java/src/org/chromium/chrome/browser/hub/DelegateButtonData.java
@@ -51,4 +51,15 @@ public class DelegateButtonData implements FullButtonData {
     public @Nullable Runnable getOnPressRunnable() {
         return mOnPress;
     }
+
+    @Override
+    public boolean buttonDataEquals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o instanceof DelegateButtonData that) {
+            return mDelegateButtonData.equals(that.mDelegateButtonData);
+        }
+        return false;
+    }
 }
