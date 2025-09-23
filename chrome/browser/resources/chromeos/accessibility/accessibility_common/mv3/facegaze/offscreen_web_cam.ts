72a0e3920712327c6f85b5074781371464684051
Akihiro Ota <akihiroota@chromium.org>
2025-09-23 08:54:11 -0700

    FaceGaze: Fix flaky mv3 test
    This test is flaking on msan and debug builds due to teardown
    issues in the video capture service. Note that facegaze tests
    do not need to start the video service since we mock all results
    from the face landmarker in tests. As a result, we can call directly
    into the method under test and avoid starting the video service.
    
    Fixed: 445894005
    Change-Id: If64ce58dac3476a7bacea2120efc32582b064002
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6972606
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1519381}

--- chrome/browser/resources/chromeos/accessibility/accessibility_common/mv3/facegaze/offscreen_web_cam.ts
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/mv3/facegaze/offscreen_web_cam.ts
@@ -42,6 +42,9 @@ class TestSupport {
     this.owner_ = owner;
 
     Messenger.registerHandler(
+        OffscreenCommandType.FACEGAZE_CREATE_FACE_LANDMARKER_FOR_TEST,
+        () => this.createFaceLandmarker());
+    Messenger.registerHandler(
         OffscreenCommandType.FACEGAZE_MOCK_NO_CAMERA_FOR_TEST,
         () => this.mockNoCamera_());
     Messenger.registerHandler(
@@ -113,6 +116,11 @@ class TestSupport {
   setWebCamRetriesRemaining(message: {retries: number}): void {
     // @ts-ignore Private member access.
     this.owner_.connectToWebCamRetriesRemaining_ = message.retries;
+  }
+
+  async createFaceLandmarker(): Promise<void> {
+    // @ts-ignore Private member access.
+    return this.owner_.createFaceLandmarker_();
   }
 
   hasFaceLandmarker(): boolean {
