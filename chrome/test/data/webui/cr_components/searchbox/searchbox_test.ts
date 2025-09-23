c658286d414031a81e9846f4a2d1a997a1929536
dpapad <dpapad@chromium.org>
2025-09-22 17:57:51 -0700

    WebUI Searchbox: Restore onResultRepaint_ invocation in the Lit version.
    The invocation was accidentally removed in the Polymer->Lit migration CL
    at https://crrev.com/c/6968638.
    
    Having said that, previously this was invoked on Polymer 'dom-change'
    events which don't have a direct equivalent in Lit. The closest
    equivalent is the 'updated' lifecycle callback which executes after each
    asynchronous rendering cycle of a Lit element, so the recorded metrics
    are likely to be a bit different than before.
    
    Bug: 445727122
    Change-Id: Ic2d1bcece3a84f5f333d4811dd86a33c9cd48415
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6974987
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1519056}

--- chrome/test/data/webui/cr_components/searchbox/searchbox_test.ts
+++ chrome/test/data/webui/cr_components/searchbox/searchbox_test.ts
@@ -2035,8 +2035,7 @@ suite('NewTabPageRealboxTest', () => {
   // Test Responsiveness Metrics
   //============================================================================
 
-  // TODO fix before submitting.
-  test.skip('responsiveness metrics are being recorded', async () => {
+  test('responsiveness metrics are being recorded', async () => {
     realbox.$.input.value = 'he';
     realbox.$.input.dispatchEvent(new InputEvent('input'));
 
