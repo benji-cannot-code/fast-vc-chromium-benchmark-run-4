1a5030b725d2b5367105c5a6262b6d0c19f39a0d
dongguoz <dongguoz@google.com>
2025-09-23 08:27:41 -0700

    Wire up image extraction for barcode detection
    This change wires up the `WalletablePassBarcodeDetector` to extract
    images from a web page using a mojo `ImageExtractor` interface.
    
    It also adds browser tests to verify the image extraction process and to
    ensure that the detector handles disconnection of the `ImageExtractor`
    remote gracefully.
    
    This CL is based on the work of minjiaw@google.com in Gerrit change:
    https://chromium-review.googlesource.com/c/chromium/src/+/6845677
    
    Bug: 438657493
    Change-Id: I0289b2c32fe28af058a7dad8989feb5f8682501e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6965478
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Hanfeng Zhu <hanfeng@google.com>
    Commit-Queue: Dongguo Zhang <dongguo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1519359}

--- components/wallet/content/browser/walletable_pass_barcode_detector_impl.h
+++ components/wallet/content/browser/walletable_pass_barcode_detector_impl.h
@@ -6,9 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_WALLET_CONTENT_BROWSER_WALLETABLE_PASS_BARCODE_DETECTOR_IMPL_H_
 #define COMPONENTS_WALLET_CONTENT_BROWSER_WALLETABLE_PASS_BARCODE_DETECTOR_IMPL_H_
 
+#include <vector>
+
 #include "base/memory/weak_ptr.h"
 #include "components/wallet/content/browser/walletable_pass_barcode_detector.h"
+#include "components/wallet/content/common/mojom/image_extractor.mojom.h"
+#include "mojo/public/cpp/bindings/remote.h"
 #include "services/shape_detection/public/mojom/barcodedetection.mojom.h"
+#include "third_party/skia/include/core/SkBitmap.h"
 
 namespace wallet {
 
@@ -22,8 +27,22 @@ class WalletablePassBarcodeDetectorImpl : public WalletablePassBarcodeDetector {
               WalletBarcodeDetectionDetectCallback callback) override;
 
  private:
+  mojo::Remote<mojom::ImageExtractor> CreateAndBindImageExtractorRemote(
+      content::WebContents* web_contents);
+
+  void ExtractImageWithRemote(mojo::Remote<mojom::ImageExtractor> remote,
+                              WalletBarcodeDetectionDetectCallback callback);
+
+  void OnImagesExtracted(WalletBarcodeDetectionDetectCallback callback,
+                         const std::vector<SkBitmap>& images);
+
+  void OnRemoteDisconnected(WalletBarcodeDetectionDetectCallback callback);
+
   WalletBarcodeFormat MojoToBarcodeFormat(
       shape_detection::mojom::BarcodeFormat format);
+
+  mojo::Remote<mojom::ImageExtractor> image_extractor_remote_;
+
   base::WeakPtrFactory<WalletablePassBarcodeDetectorImpl> weak_ptr_factory_{
       this};
 };
