96ca55966635c16eef972ce2fdd3019d98229d3d
Andy Phan <andyphan@chromium.org>
2025-09-23 15:59:59 -0700

    [PDF Ink Signatures] Avoid unnecessary rect invalidation pt. 1
    PdfCaret::SetChar() invalidates the new caret screen rect, which is not
    necessary when text selecting, as PDFiumEngine will already invalidate
    it. Add a new SetCharAndDraw() method and move the new caret screen rect
    invalidation into it. Then, adjust callers to use either one, depending
    on if invalidation is required.
    
    Note that SetChar() still handles invalidating the original caret screen
    rect, otherwise it would leave an old caret drawing.
    
    Bug: 427236731
    Change-Id: I8243264109b1c5b79a43aaa1e851eeea0f176736
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6967449
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1519649}

--- pdf/pdf_caret.cc
+++ pdf/pdf_caret.cc
@@ -68,6 +68,10 @@ void PdfCaret::SetChar(const PageCharacterIndex& next_char) {
   if (!old_screen_rect.IsEmpty() && old_screen_rect != caret_screen_rect_) {
     client_->InvalidateRect(old_screen_rect);
   }
+}
+
+void PdfCaret::SetCharAndDraw(const PageCharacterIndex& next_char) {
+  SetChar(next_char);
   if (is_visible_) {
     RefreshDisplayState();
   }
@@ -225,7 +229,7 @@ void PdfCaret::MoveToChar(const PageCharacterIndex& new_index,
 
   if (!should_select || (!client_->IsSelecting() &&
                          !StartSelection(/*move_right=*/index_ < new_index))) {
-    SetChar(new_index);
+    SetCharAndDraw(new_index);
     return;
   }
 
