4445507c02f1b578a092dc71062be83f3c72a7ab
Raj T <rajendrant@chromium.org>
2025-09-23 15:35:27 -0700

    Refactor OptimizationGuideModelProviderObservation to allow dynamic observation.
    This change modifies `OptimizationGuideModelProviderObservation` to support observing different optimization targets or resetting the observation. The `OnDeviceModelAdaptationLoader` is updated to use this refactored class, simplifying its observer management.
    
    This CL also splits the observe registration in OptimizationGuideModelProviderObservation from the constructor.
    
    Change-Id: If0625c15bd768639a86c8460ed23c7e5c652ec8a
    Bug: 445936181
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6976334
    Commit-Queue: Raj T <rajendrant@chromium.org>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1519635}

--- components/optimization_guide/core/model_execution/on_device_model_adaptation_loader.h
+++ components/optimization_guide/core/model_execution/on_device_model_adaptation_loader.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_observation.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/types/expected.h"
-#include "components/optimization_guide/core/delivery/optimization_target_model_observer.h"
+#include "components/optimization_guide/core/delivery/optimization_guide_model_provider.h"
 #include "components/optimization_guide/core/model_execution/feature_keys.h"
 #include "components/optimization_guide/core/model_execution/on_device_model_component.h"
 #include "components/optimization_guide/core/model_execution/on_device_model_feature_adapter.h"
@@ -160,7 +160,7 @@ class OnDeviceModelAdaptationLoader final
   proto::OptimizationTarget target_;
 
   // The model provider to observe for updates to model adaptations.
-  raw_ref<OptimizationGuideModelProvider> model_provider_;
+  OptimizationGuideModelProviderObservation model_provider_observation_;
   OnLoadFn on_load_fn_;
 
   // The compatibility spec that we've registered for adaptations with.
