273381ec8703e2a9022e2d82550ff36b6b56dba6
Ernesto Izquierdo Clua <eic@google.com>
2025-09-23 08:33:04 -0700

    [ios] Add iOS to DataControlsRules policy supported platforms
    Bug: 346533036
    Change-Id: Idb16edec65731af2d2508abead0a8659e27f450d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6966625
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Auto-Submit: Ernesto Izquierdo Clua <eic@google.com>
    Reviewed-by: Sébastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1519364}

--- ios/chrome/browser/policy/model/configuration_policy_handler_list_factory.mm
+++ ios/chrome/browser/policy/model/configuration_policy_handler_list_factory.mm
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/enterprise/browser/reporting/common_pref_names.h"
 #import "components/enterprise/connectors/core/connectors_prefs.h"
 #import "components/enterprise/connectors/core/enterprise_connectors_policy_handler.h"
+#import "components/enterprise/data_controls/core/browser/data_controls_policy_handler.h"
+#import "components/enterprise/data_controls/core/browser/prefs.h"
 #import "components/enterprise/idle/idle_timeout_policy_handler.h"
 #import "components/history/core/common/pref_names.h"
 #import "components/lens/lens_overlay_permission_utils.h"
@@ -307,6 +309,11 @@ std::unique_ptr<policy::ConfigurationPolicyHandlerList> BuildPolicyHandlerList(
 
   handlers->AddHandler(std::make_unique<policy::DataRegionPolicyHandler>(
       policy::key::kChromeDataRegionSetting, prefs::kChromeDataRegionSetting));
+
+  handlers->AddHandler(
+      std::make_unique<data_controls::DataControlsPolicyHandler>(
+          policy::key::kDataControlsRules,
+          data_controls::kDataControlsRulesPref, chrome_schema));
 
   return handlers;
 }
