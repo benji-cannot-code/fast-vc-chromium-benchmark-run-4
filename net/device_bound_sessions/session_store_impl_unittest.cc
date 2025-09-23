6be611d269a2daafdf01b2707619d30f9f76e9cf
thefrog <thefrog@chromium.org>
2025-09-23 07:28:30 -0700

    Introduce DBSC schema version feature parameter
    This controls the database schema version for stored session. When the
    database is read, data from any non-matching version is wiped.
    
    Fixed: 444058245, 445432528
    Change-Id: I887e46f2863017e560f69567b6902ceb7c5dc24e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6966681
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: thefrog <thefrog@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1519319}

--- net/device_bound_sessions/session_store_impl_unittest.cc
+++ net/device_bound_sessions/session_store_impl_unittest.cc
@@ -423,8 +423,9 @@ TEST_F(SessionStoreImplTest, LoadSavedSessions) {
 }
 
 TEST_F(SessionStoreImplTest, DropLowerSchemaVersionSessions) {
-  feature_list_.InitAndDisableFeature(
-      features::kDeviceBoundSessionsOriginTrialFeedback);
+  feature_list_.InitAndEnableFeatureWithParameters(
+      features::kDeviceBoundSessions,
+      {{features::kDeviceBoundSessionsSchemaVersion.name, "1"}});
   CreateStoreAndLoadSessions();
   SessionCfgList cfgs = {
       {"https://a.foo.test/index.html", "session0", "https://foo.test"},
@@ -436,8 +437,9 @@ TEST_F(SessionStoreImplTest, DropLowerSchemaVersionSessions) {
       CreateAndSaveSessions(cfgs, unexportable_key_service(), store());
 
   feature_list_.Reset();
-  feature_list_.InitAndEnableFeature(
-      features::kDeviceBoundSessionsOriginTrialFeedback);
+  feature_list_.InitAndEnableFeatureWithParameters(
+      features::kDeviceBoundSessions,
+      {{features::kDeviceBoundSessionsSchemaVersion.name, "2"}});
   MimicRestart();
 
   SessionStore::SessionsMap loaded_sessions = LoadSessions();
@@ -445,8 +447,9 @@ TEST_F(SessionStoreImplTest, DropLowerSchemaVersionSessions) {
 }
 
 TEST_F(SessionStoreImplTest, DropHigherSchemaVersionSessions) {
-  feature_list_.InitAndEnableFeature(
-      features::kDeviceBoundSessionsOriginTrialFeedback);
+  feature_list_.InitAndEnableFeatureWithParameters(
+      features::kDeviceBoundSessions,
+      {{features::kDeviceBoundSessionsSchemaVersion.name, "2"}});
   CreateStoreAndLoadSessions();
   SessionCfgList cfgs = {
       {"https://a.foo.test/index.html", "session0", "https://foo.test"},
@@ -458,8 +461,9 @@ TEST_F(SessionStoreImplTest, DropHigherSchemaVersionSessions) {
       CreateAndSaveSessions(cfgs, unexportable_key_service(), store());
 
   feature_list_.Reset();
-  feature_list_.InitAndDisableFeature(
-      features::kDeviceBoundSessionsOriginTrialFeedback);
+  feature_list_.InitAndEnableFeatureWithParameters(
+      features::kDeviceBoundSessions,
+      {{features::kDeviceBoundSessionsSchemaVersion.name, "1"}});
   MimicRestart();
 
   SessionStore::SessionsMap loaded_sessions = LoadSessions();
