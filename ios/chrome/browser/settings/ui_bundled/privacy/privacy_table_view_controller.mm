2e0077289a5b6d3f3f721994c45e730d726fc810
Fiona Macintosh <fmacintosh@google.com>
2025-09-23 05:41:22 -0700

    [SB][iOS] Add Incognito section to privacy and security settings page
    Entrypoint to Incognito tracking protection settings page will be added
    in a followup, this is just reorganizing the existing settings per mocks
    
    Local screens
    Without feature: https://screenshot.googleplex.com/7wYdeC5HYb9Xk3R.png
    With feature: https://screenshot.googleplex.com/67svqKnz7LiwmTF.png
    
    Mock: https://screenshot.googleplex.com/5YA4npu4KMzaoFC.png
    
    Bug: b:442799337
    Change-Id: Ie8d83ffb091ffb5dc88b300d5eb3fa60313024ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6972505
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Fiona Macintosh <fmacintosh@google.com>
    Commit-Queue: Fiona Macintosh <fmacintosh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1519269}

--- ios/chrome/browser/settings/ui_bundled/privacy/privacy_table_view_controller.mm
+++ ios/chrome/browser/settings/ui_bundled/privacy/privacy_table_view_controller.mm
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/prefs/ios/pref_observer_bridge.h"
 #import "components/prefs/pref_change_registrar.h"
 #import "components/prefs/pref_service.h"
+#import "components/privacy_sandbox/privacy_sandbox_features.h"
 #import "components/safe_browsing/core/common/safe_browsing_prefs.h"
 #import "components/signin/public/identity_manager/account_info.h"
 #import "components/strings/grit/components_strings.h"
@@ -85,6 +86,7 @@ typedef NS_ENUM(NSInteger, SectionIdentifier) {
   SectionIdentifierIncognitoInterstitial,
   SectionIdentifierLockdownMode,
   SectionIdentifierPrivacyGuide,
+  SectionIdentifierIncognito,
 };
 
 typedef NS_ENUM(NSInteger, ItemType) {
@@ -102,6 +104,7 @@ typedef NS_ENUM(NSInteger, ItemType) {
   ItemTypeIncognitoInterstitialDisabled,
   ItemTypeLockdownMode,
   ItemTypePrivacyGuide,
+  ItemTypeIncognitoHeader,
 };
 
 // Used to open the Sync and Google Services settings.
@@ -133,11 +136,14 @@ const char kSyncSettingsURL[] = "settings://open_sync";
   TableViewDetailIconItem* _safeBrowsingDetailItem;
   // Incognito Lock item.
   TableViewDetailIconItem* _incognitoLockItem;
-  // Locdown Mode item.
+  // Lockdown Mode item.
   TableViewDetailIconItem* _lockdownModeDetailItem;
 
   // Whether Settings have been dismissed.
   BOOL _settingsAreDismissed;
+
+  // Whether the Incognito tracking protections entrypoint should be shown.
+  BOOL _showIncognitoTrackingProtections;
 
   // Registrar for local pref changes notifications.
   PrefChangeRegistrar _localStateChangeRegistrar;
@@ -224,6 +230,8 @@ const char kSyncSettingsURL[] = "settings://open_sync";
         initWithPrefService:GetApplicationContext()->GetLocalState()
                    prefName:prefs::kIncognitoInterstitialEnabled];
     [_incognitoInterstitialPref setObserver:self];
+    _showIncognitoTrackingProtections = base::FeatureList::IsEnabled(
+        privacy_sandbox::kFingerprintingProtectionUx);
   }
   return self;
 }
@@ -300,9 +308,14 @@ const char kSyncSettingsURL[] = "settings://open_sync";
       toSectionWithIdentifier:SectionIdentifierHTTPSOnlyMode];
 
   [model addSectionWithIdentifier:SectionIdentifierWebServices];
-  [model addSectionWithIdentifier:SectionIdentifierIncognitoAuth];
-  [model addSectionWithIdentifier:SectionIdentifierIncognitoInterstitial];
-  [model addSectionWithIdentifier:SectionIdentifierLockdownMode];
+  if (_showIncognitoTrackingProtections) {
+    [model addSectionWithIdentifier:SectionIdentifierLockdownMode];
+    [model addSectionWithIdentifier:SectionIdentifierIncognito];
+  } else {
+    [model addSectionWithIdentifier:SectionIdentifierIncognitoAuth];
+    [model addSectionWithIdentifier:SectionIdentifierIncognitoInterstitial];
+    [model addSectionWithIdentifier:SectionIdentifierLockdownMode];
+  }
 
   // Clear Browsing item.
   [model addItem:[self clearBrowsingDetailItem]
@@ -322,6 +335,24 @@ const char kSyncSettingsURL[] = "settings://open_sync";
   [model addItem:[self handoffDetailItem]
       toSectionWithIdentifier:SectionIdentifierWebServices];
 
+  SectionIdentifier incognitoAuthSection = SectionIdentifierIncognitoAuth;
+  SectionIdentifier incognitoInterstitialSection =
+      SectionIdentifierIncognitoInterstitial;
+  SectionIdentifier sectionWithPrivacyFooter = SectionIdentifierLockdownMode;
+  // Incognito section header item.
+  if (_showIncognitoTrackingProtections) {
+    TableViewTextHeaderFooterItem* headerItem =
+        [[TableViewTextHeaderFooterItem alloc]
+            initWithType:ItemTypeIncognitoHeader];
+    headerItem.text =
+        l10n_util::GetNSString(IDS_IOS_INCOGNITO_SETTINGS_SECTION_HEADER);
+    [model setHeader:headerItem
+        forSectionWithIdentifier:SectionIdentifierIncognito];
+    incognitoAuthSection = SectionIdentifierIncognito;
+    incognitoInterstitialSection = SectionIdentifierIncognito;
+    sectionWithPrivacyFooter = SectionIdentifierIncognito;
+  }
+
   if (IsIOSSoftLockEnabled()) {
     // Incognito Lock item.
     TableViewItem* incognitoLockItem =
@@ -329,7 +360,7 @@ const char kSyncSettingsURL[] = "settings://open_sync";
             ? self.incognitoLockItemDisabled
             : self.incognitoLockItem;
     [model addItem:incognitoLockItem
-        toSectionWithIdentifier:SectionIdentifierIncognitoAuth];
+        toSectionWithIdentifier:incognitoAuthSection];
   } else {
     // Incognito reauth item is added. If Incognito mode is disabled, or device
     // authentication is not supported, a disabled version is shown instead with
@@ -340,7 +371,7 @@ const char kSyncSettingsURL[] = "settings://open_sync";
             ? self.incognitoReauthItemDisabled
             : self.incognitoReauthItem;
     [model addItem:incognitoReauthItem
-        toSectionWithIdentifier:SectionIdentifierIncognitoAuth];
+        toSectionWithIdentifier:incognitoAuthSection];
   }
 
   // Show "Ask to Open Links from Other Apps in Incognito" setting.
@@ -353,13 +384,13 @@ const char kSyncSettingsURL[] = "settings://open_sync";
           ? self.incognitoInterstitialItemDisabled
           : self.incognitoInterstitialItem;
   [model addItem:incognitoInterstitialItem
-      toSectionWithIdentifier:SectionIdentifierIncognitoInterstitial];
+      toSectionWithIdentifier:incognitoInterstitialSection];
 
   // Lockdown Mode item.
   [model addItem:[self lockdownModeDetailItem]
       toSectionWithIdentifier:SectionIdentifierLockdownMode];
   [model setFooter:[self showPrivacyFooterItem]
-      forSectionWithIdentifier:SectionIdentifierLockdownMode];
+      forSectionWithIdentifier:sectionWithPrivacyFooter];
 }
 
 #pragma mark - Model Objects
