265b8e62f536c9ab52e9a8415daceab3750f15b5
S. Ganesh <ganesh@chromium.org>
2025-09-23 14:42:02 -0700

    update_client: delete empty temp parent directories on failures
    Bug: 445972566
    Change-Id: I164c610e68ea52a5e460923b50c353c70a0e4ebe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6977057
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1519602}

--- components/update_client/op_zucchini.cc
+++ components/update_client/op_zucchini.cc
@@ -72,7 +72,7 @@ void VerifyAndCleanUp(
     int result) {
   RetryFileOperation(&base::DeleteFile, patch_file);
   if (result) {
-    RetryFileOperation(&base::DeleteFile, new_file);
+    DeleteFileAndEmptyParentDirectory(new_file);
     std::move(callback).Run(base::unexpected<CategorizedError>(
         {.category = ErrorCategory::kUnpack,
          .code = static_cast<int>(UnpackerError::kDeltaOperationFailure),
@@ -81,7 +81,7 @@ void VerifyAndCleanUp(
   }
 
   if (!VerifyFileHash256(new_file, output_hash)) {
-    RetryFileOperation(&base::DeleteFile, new_file);
+    DeleteFileAndEmptyParentDirectory(new_file);
     std::move(callback).Run(base::unexpected<CategorizedError>(
         {.category = ErrorCategory::kUnpack,
          .code = static_cast<int>(UnpackerError::kPatchOutHashMismatch)}));
@@ -127,7 +127,7 @@ void CacheLookupDone(
         FROM_HERE, kTaskTraits,
         base::BindOnce(
             [](const base::FilePath& patch_file) {
-              RetryFileOperation(&base::DeleteFile, patch_file);
+              DeleteFileAndEmptyParentDirectory(patch_file);
             },
             patch_file),
         base::BindOnce(std::move(callback),
