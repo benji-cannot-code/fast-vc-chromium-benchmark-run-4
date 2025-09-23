26c2d8fd3d78394e334afc12b07bb3fd857ab3da
Fahad Mansoor <fahadmansoor@google.com>
2025-09-23 12:42:52 -0700

    Add new enrollment metric and update enum file path.
    This change adds a new `MetricEnrollment` value for when the
    organization unit initial enrollment limit is exceeded. It also
    updates comments in `enterprise_metrics.h` to point to the correct
    location of the `enums.xml` file and fixes a typo.
    
    NO_IFTTT=Adding these tags for the time.
    
    Change-Id: I0e5ce1c6ab88d207e77e167b49b01b4af6adabc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6975974
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Fahad Mansoor <fahadmansoor@google.com>
    Cr-Commit-Position: refs/heads/main@{#1519532}

--- components/policy/core/common/cloud/enterprise_metrics.h
+++ components/policy/core/common/cloud/enterprise_metrics.h
@@ -19,7 +19,9 @@ namespace policy {
 // Hence,
 //   (a) existing enumerated constants should never be deleted or reordered, and
 //   (b) new constants should only be appended at the end of the enumeration
-//       (update tools/metrics/histograms/enums.xml as well).
+//       (update tools/metrics/histograms/metadata/enterprise/enums.xml as
+//       well).
+// LINT.IfChange(MetricEnrollment)
 enum MetricEnrollment {
   // User pressed 'Cancel' during the enrollment process.
   kMetricEnrollmentCancelled = 0,
@@ -140,7 +142,7 @@ enum MetricEnrollment {
   kMetricEnrollmentRegisterEnterpriseAccountIsNotEligibleToEnroll = 60,
   // Enrollment failed: Enterprise TOS has not been accepted.
   kMetricEnrollmentRegisterEnterpriseTosHasNotBeenAccepted = 61,
-  // Too many requests are uploadede within a short time.
+  // Too many requests are uploaded within a short time.
   kMetricEnrollmentTooManyRequests = 62,
   // Enrollment failed: illegal account for packaged EDU license.
   kMetricEnrollmentIllegalAccountForPackagedEDULicense = 63,
@@ -163,13 +165,16 @@ enum MetricEnrollment {
   // Max value for use with enumeration histogram UMA functions.
   kMaxValue = kMetricEnrollmentOrgUnitEnrollmentLimitExceeded
 };
+// LINT.ThenChange(//tools/metrics/histograms/metadata/enterprise/enums.xml:EnterpriseEnrollmentType)
 
 // Events related to policy refresh.
 // This enum is used to define the buckets for an enumerated UMA histogram.
 // Hence,
 //   (a) existing enumerated constants should never be deleted or reordered, and
 //   (b) new constants should only be appended at the end of the enumeration
-//       (update tools/metrics/histograms/enums.xml as well).
+//       (update tools/metrics/histograms/metadata/enterprise/enums.xml as
+//       well).
+// LINT.IfChange(MetricPolicyRefresh)
 enum MetricPolicyRefresh {
   // A refresh occurred while the policy was not invalidated and the policy was
   // changed. Invalidations were enabled.
@@ -189,13 +194,16 @@ enum MetricPolicyRefresh {
 
   METRIC_POLICY_REFRESH_SIZE  // Must be the last.
 };
+// LINT.ThenChange(//tools/metrics/histograms/metadata/enterprise/enums.xml:EnterprisePolicyRefresh)
 
 // Types of policy invalidations.
 // This enum is used to define the buckets for an enumerated UMA histogram.
 // Hence,
 //   (a) existing enumerated constants should never be deleted or reordered, and
 //   (b) new constants should only be appended at the end of the enumeration
-//       (update tools/metrics/histograms/enums.xml as well).
+//       (update tools/metrics/histograms/metadata/enterprise/enums.xml as
+//       well).
+// LINT.IfChange(PolicyInvalidationType)
 enum PolicyInvalidationType {
   // The invalidation contained no payload.
   POLICY_INVALIDATION_TYPE_NO_PAYLOAD = 0,
@@ -208,10 +216,12 @@ enum PolicyInvalidationType {
 
   POLICY_INVALIDATION_TYPE_SIZE  // Must be the last.
 };
+// LINT.ThenChange(//tools/metrics/histograms/metadata/enterprise/enums.xml:EnterprisePolicyInvalidations)
 
 // Result of the Device ID field validation in policy protobufs.
 // These values are persisted to logs. Entries should not be renumbered and
 // numeric values should never be reused.
+// LINT.IfChange(PolicyDeviceIdValidity)
 enum class PolicyDeviceIdValidity {
   kValid = 0,
   kActualIdUnknown = 1,
@@ -219,6 +229,7 @@ enum class PolicyDeviceIdValidity {
   kInvalid = 3,
   kMaxValue = kInvalid,  // Must be the last.
 };
+// LINT.ThenChange(//tools/metrics/histograms/metadata/enterprise/enums.xml:EnterprisePolicyDeviceIdValidity)
 
 // These values are persisted to logs. Entries should not be renumbered and
 // numeric values should never be reused.
