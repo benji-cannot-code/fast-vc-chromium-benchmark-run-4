e112d52a868b22b3c4505f589ad8815960b127b7
Andy Phan <andyphan@chromium.org>
2025-09-23 16:11:27 -0700

    [PDF] Remove redundant test constants
    PDFiumEngineInkTextSelectionTest and PDFiumEngineSelectionTest have
    duplicate constants. Remove them and define the same constants only once
    in the anonymous namespace.
    
    Change-Id: I1c88c067561e6210d61ab0bdc37e9c6ae573a9a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6976336
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1519656}

--- pdf/pdfium/pdfium_engine_unittest.cc
+++ pdf/pdfium/pdfium_engine_unittest.cc
@@ -83,6 +83,9 @@ using ::testing::StrictMock;
 constexpr char kSelectTextExpectedText[] =
     "Hello, world!\nGoodbye, world!\nHello, world!\nGoodbye, world!";
 
+constexpr gfx::PointF kHelloWorldStartPosition{35.0f, 110.0f};
+constexpr gfx::PointF kHelloWorldEndPosition{100.0f, 110.0f};
+
 MATCHER_P2(LayoutWithSize, width, height, "") {
   return arg.size() == gfx::Size(width, height);
 }
@@ -1016,9 +1019,6 @@ INSTANTIATE_TEST_SUITE_P(All, PDFiumEngineTest, testing::Bool());
 
 class PDFiumEngineSelectionTest : public PDFiumEngineTest {
  public:
-  static constexpr gfx::PointF kHelloWorldStartPosition{35.0f, 110.0f};
-  static constexpr gfx::PointF kHelloWorldEndPosition{100.0f, 110.0f};
-
   void TearDown() override {
     // Reset `engine_` before PDFium gets uninitialized.
     engine_.reset();
@@ -2389,7 +2389,6 @@ class PDFiumEngineInkTextSelectionTest : public PDFiumEngineInkTest {
   static constexpr PdfRect kGoodbyeWorldExpectedRectPage0{20.0f, 96.592f,
                                                           136.496f, 111.792f};
 #endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
-  static constexpr gfx::PointF kStartTextPositionPage0{35.0f, 110.0f};
   static constexpr gfx::PointF kNonTextPositionPage0{5.0f, 5.0f};
 
   void TearDown() override {
@@ -2431,10 +2430,9 @@ TEST_P(PDFiumEngineInkTextSelectionTest, ExtendSelectionByPoint) {
   PDFiumEngine* engine = CreateEngine(FILE_PATH_LITERAL("hello_world2.pdf"));
   ASSERT_TRUE(engine);
 
-  engine->OnTextOrLinkAreaClick(kStartTextPositionPage0, /*click_count=*/1);
+  engine->OnTextOrLinkAreaClick(kHelloWorldStartPosition, /*click_count=*/1);
 
-  constexpr gfx::PointF kEndPosition(100.0f, 110.0f);
-  EXPECT_TRUE(engine->ExtendSelectionByPoint(kEndPosition));
+  EXPECT_TRUE(engine->ExtendSelectionByPoint(kHelloWorldEndPosition));
 
   EXPECT_EQ("Goodb", engine->GetSelectedText());
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
@@ -2450,7 +2448,7 @@ TEST_P(PDFiumEngineInkTextSelectionTest, ExtendSelectionByPointMultiPage) {
   PDFiumEngine* engine = CreateEngine(FILE_PATH_LITERAL("hello_world2.pdf"));
   ASSERT_TRUE(engine);
 
-  engine->OnTextOrLinkAreaClick(kStartTextPositionPage0, /*click_count=*/1);
+  engine->OnTextOrLinkAreaClick(kHelloWorldStartPosition, /*click_count=*/1);
 
   constexpr gfx::PointF kEndPosition(75.0f, 480.0f);
   EXPECT_TRUE(engine->ExtendSelectionByPoint(kEndPosition));
@@ -2473,7 +2471,7 @@ TEST_P(PDFiumEngineInkTextSelectionTest, OnTextOrLinkAreaClickWithDoubleClick) {
   PDFiumEngine* engine = CreateEngine(FILE_PATH_LITERAL("hello_world2.pdf"));
   ASSERT_TRUE(engine);
 
-  engine->OnTextOrLinkAreaClick(kStartTextPositionPage0, /*click_count=*/2);
+  engine->OnTextOrLinkAreaClick(kHelloWorldStartPosition, /*click_count=*/2);
 
   EXPECT_EQ("Goodbye", engine->GetSelectedText());
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
@@ -2512,7 +2510,7 @@ TEST_P(PDFiumEngineInkTextSelectionTest, OnTextOrLinkAreaClickWithTripleClick) {
   PDFiumEngine* engine = CreateEngine(FILE_PATH_LITERAL("hello_world2.pdf"));
   ASSERT_TRUE(engine);
 
-  engine->OnTextOrLinkAreaClick(kStartTextPositionPage0, /*click_count=*/3);
+  engine->OnTextOrLinkAreaClick(kHelloWorldStartPosition, /*click_count=*/3);
 
   EXPECT_EQ("Goodbye, world!", engine->GetSelectedText());
   EXPECT_THAT(
