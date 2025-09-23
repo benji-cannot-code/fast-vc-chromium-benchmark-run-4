fb48b7531432ae1f2fd913a0d5dbfff26774aa7d
Liza Bipin <mlbipin@google.com>
2025-09-23 04:49:18 -0700

    [Signin]Use OAuthConsumerId for ProfileDownloader
    Bug: 425896213
    Change-Id: Iee6b89c24ffe13b0809556e86e75f7fdc7da5893
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6973905
    Commit-Queue: Liza Bipin <mlbipin@google.com>
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1519249}

--- components/signin/internal/identity_manager/oauth_consumer_registry.cc
+++ components/signin/internal/identity_manager/oauth_consumer_registry.cc
@@ -93,6 +93,7 @@ constexpr char kNearbyPresenceServerClientName[] =
     "nearby_presence_server_client";
 constexpr char kCryptAuthClientName[] = "crypt_auth_client";
 constexpr char kAmbientModeName[] = "ambient_mode";
+constexpr char kProfileDownloaderName[] = "profile_downloader";
 
 }  // namespace
 
@@ -392,6 +393,11 @@ OAuthConsumer GetOAuthConsumerFromId(OAuthConsumerId oauth_consumer_id) {
           /*name=*/kAmbientModeName,
           /*scopes=*/{GaiaConstants::kPhotosOAuth2Scope,
                       GaiaConstants::kCastBackdropOAuth2Scope});
+    case OAuthConsumerId::kProfileDownloader:
+      return OAuthConsumer(
+          /*name=*/kProfileDownloaderName,
+          /*scopes=*/{GaiaConstants::kGoogleUserInfoProfile,
+                      GaiaConstants::kGoogleUserInfoEmail});
   }
   NOTREACHED();
 }
