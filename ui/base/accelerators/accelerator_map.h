632fde86f7701b2fc4e3453261f754e86d88cddb
Mitsuru Oshima <oshima@chromium.org>
2025-09-23 15:38:03 -0700

    Use unordered_map in AcceleratorMap
    Also changed the erase code, which also showed improvements.
    
    On my cloud top, this reduced the wall time of
    Widget::HandleWidgetDestroy around 50% (200~ns to ~100ns)
    for browser window. (release, no dcheck)
    
    Bug: 443993719
    
    Change-Id: Ie2a41a96ecbbb33958bd866764d32207bd67c4d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6974085
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1519637}

--- ui/base/accelerators/accelerator_map.h
+++ ui/base/accelerators/accelerator_map.h
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_BASE_ACCELERATORS_ACCELERATOR_MAP_H_
 #define UI_BASE_ACCELERATORS_ACCELERATOR_MAP_H_
 
+// TODO(oshima): Remove this.
 #include <map>
+#include <unordered_map>
 #include <utility>
 #include <vector>
 
@@ -19,10 +21,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/keycodes/dom/keycode_converter.h"
 #endif
 
+namespace std {
+template <>
+struct std::hash<ui::Accelerator> {
+  size_t operator()(const ui::Accelerator& a) const { return a.hash(); }
+};
+
+}  // namespace std
+
 namespace ui {
 
-// This is a wrapper around an internal std::map of type
-// |std::map<Accelerator, V>| where |V| is the mapped value.
+// This is a wrapper around an internal std::unordered_map of type
+// |std::unordered_map<Accelerator, V>| where |V| is the mapped value.
 //
 // Accelerators in Chrome on all platforms are specified with the |key_code|,
 // aka VKEY, however certain keys (eg. brackets, comma, period, plus, minus),
@@ -49,8 +59,9 @@ class COMPONENT_EXPORT(UI_BASE) AcceleratorMap {
   AcceleratorMap() = default;
   ~AcceleratorMap() = default;
 
-  using iterator = typename std::map<Accelerator, V>::iterator;
-  using const_iterator = typename std::map<Accelerator, V>::const_iterator;
+  using iterator = typename std::unordered_map<Accelerator, V>::iterator;
+  using const_iterator =
+      typename std::unordered_map<Accelerator, V>::const_iterator;
 
   // Lookup an accelerator and return a pointer to the value. If the
   // accelerator is not in the map, this returns nullptr.
@@ -137,7 +148,7 @@ class COMPONENT_EXPORT(UI_BASE) AcceleratorMap {
 #endif
 
  private:
-  std::map<Accelerator, V> map_;
+  std::unordered_map<Accelerator, V> map_;
 
 #if BUILDFLAG(IS_CHROMEOS)
   bool use_positional_lookup_ = false;
