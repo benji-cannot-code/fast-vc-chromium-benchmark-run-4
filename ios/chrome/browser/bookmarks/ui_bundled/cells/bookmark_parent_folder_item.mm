ef72f5b7b58034f38232f12a5c01fc9c069618f1
Daniel White <danieltwhite@google.com>
2025-09-23 05:51:02 -0700

    [iOS] iOS 16 deprecation code cleanup
    Each year the Chrome for iOS team increases the application's minimum
    supported iOS version. During the process, run/compile-time flags are
    added throughout the codebase to transitions areas of the code using
    deprecated APIs to the newer variants. Once the minimum supported
    version is bumped, these guards are no longer needed.
    
    This CL is part of a large-scale effort to remove the remaining
    obsolete guards from the application's codebase.
    
    This CL was uploaded by git cl split.
    
    R=noyau@chromium.org
    
    Bug: 403260493
    Change-Id: Icd0dd270a884acda34e85620ec3ba1bac60b2e53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6974118
    Auto-Submit: Daniel White <danieltwhite@google.com>
    Commit-Queue: Éric Noyau <noyau@chromium.org>
    Reviewed-by: Éric Noyau <noyau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1519276}

--- ios/chrome/browser/bookmarks/ui_bundled/cells/bookmark_parent_folder_item.mm
+++ ios/chrome/browser/bookmarks/ui_bundled/cells/bookmark_parent_folder_item.mm
@@ -124,12 +124,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                                   kBookmarkCellHorizontalAccessoryViewSpacing));
   [self applyContentSizeCategoryStyles];
 
-  if (@available(iOS 17, *)) {
-    NSArray<UITrait>* traits = TraitCollectionSetForTraits(
-        @[ UITraitPreferredContentSizeCategory.class ]);
-    [self registerForTraitChanges:traits
-                       withAction:@selector(applyContentSizeCategoryStyles)];
-  }
+  NSArray<UITrait>* traits = TraitCollectionSetForTraits(
+      @[ UITraitPreferredContentSizeCategory.class ]);
+  [self registerForTraitChanges:traits
+                     withAction:@selector(applyContentSizeCategoryStyles)];
 
   return self;
 }
@@ -139,20 +137,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   self.parentFolderNameLabel.text = nil;
   self.cloudSlashedView.hidden = YES;
 }
-
-#if !defined(__IPHONE_17_0) || __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_17_0
-- (void)traitCollectionDidChange:(UITraitCollection*)previousTraitCollection {
-  [super traitCollectionDidChange:previousTraitCollection];
-  if (@available(iOS 17, *)) {
-    return;
-  }
-
-  if (self.traitCollection.preferredContentSizeCategory !=
-      previousTraitCollection.preferredContentSizeCategory) {
-    [self applyContentSizeCategoryStyles];
-  }
-}
-#endif
 
 - (void)applyContentSizeCategoryStyles {
   if (UIContentSizeCategoryIsAccessibilityCategory(
--- ios/chrome/browser/bookmarks/ui_bundled/home/bookmarks_home_view_controller.mm
+++ ios/chrome/browser/bookmarks/ui_bundled/home/bookmarks_home_view_controller.mm
@@ -441,10 +441,8 @@ BookmarkNodeIDSet GetBookmarkNodeIDSet(
     [self showLoadingSpinnerBackground];
   }
 
-  if (@available(iOS 17, *)) {
-    [self registerForTraitChanges:TraitCollectionSetForTraits(nil)
-                       withAction:@selector(stopEdittingBookmarkOnTraitChange)];
-  }
+  [self registerForTraitChanges:TraitCollectionSetForTraits(nil)
+                     withAction:@selector(stopEdittingBookmarkOnTraitChange)];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -510,17 +508,6 @@ BookmarkNodeIDSet GetBookmarkNodeIDSet(
 - (BOOL)prefersStatusBarHidden {
   return NO;
 }
-
-#if !defined(__IPHONE_17_0) || __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_17_0
-- (void)traitCollectionDidChange:(UITraitCollection*)previousTraitCollection {
-  [super traitCollectionDidChange:previousTraitCollection];
-  if (@available(iOS 17, *)) {
-    return;
-  }
-
-  [self stopEdittingBookmarkOnTraitChange];
-}
-#endif
 
 - (UIStatusBarStyle)preferredStatusBarStyle {
   return UIStatusBarStyleDefault;
