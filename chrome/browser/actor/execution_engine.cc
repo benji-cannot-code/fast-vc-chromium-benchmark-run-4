08b2491178fd4e2cdaa2f991bed8e240b26dd6e7
Karthika Pai <karthikapai@google.com>
2025-09-23 12:08:51 -0700

    Sync feature protos.
    Change-Id: I8a0eae2d474ff5ec69a1b28a3a882a6390e38d58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6974814
    Commit-Queue: Karthika Pai <karthikapai@google.com>
    Reviewed-by: Simon Pelchat <spelchat@chromium.org>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1519505}

--- chrome/browser/actor/execution_engine.cc
+++ chrome/browser/actor/execution_engine.cc
@@ -62,7 +62,6 @@ using optimization_guide::proto::Action;
 using optimization_guide::proto::Actions;
 using optimization_guide::proto::ActionTarget;
 using optimization_guide::proto::AnnotatedPageContent;
-using optimization_guide::proto::BrowserAction;
 using tabs::TabInterface;
 
 namespace actor {
