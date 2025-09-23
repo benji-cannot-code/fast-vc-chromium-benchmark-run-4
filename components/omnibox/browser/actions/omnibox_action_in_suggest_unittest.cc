add9145567c3e9393a8bf8d568e667051e8f45f6
Jian Li <jianli@chromium.org>
2025-09-23 16:07:48 -0700

    Add new action type for chrome tab switching
    Bug: 409339245
    Change-Id: I5dcaf47fac12fb8f868dd49b592949533c9f4179
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6976343
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Jian Li <jianli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1519652}

--- components/omnibox/browser/actions/omnibox_action_in_suggest_unittest.cc
+++ components/omnibox/browser/actions/omnibox_action_in_suggest_unittest.cc
@@ -45,6 +45,9 @@ const char* ToString(ActionType type) {
       return "Reviews";
     case omnibox::SuggestTemplateInfo_TemplateAction_ActionType_CHROME_AIM:
       return "Aim";
+    case omnibox::
+        SuggestTemplateInfo_TemplateAction_ActionType_CHROME_TAB_SWITCH:
+      return "TabSwitch";
     default:
       NOTREACHED();
   }
@@ -101,6 +104,14 @@ TEST_F(OmniboxActionInSuggestTest, CheckLabelsArePresentForKnownTypes) {
           IDS_OMNIBOX_ACTION_IN_SUGGEST_AIM_CONTENTS,
           IDS_ACC_OMNIBOX_ACTION_IN_SUGGEST_SUFFIX,
           IDS_OMNIBOX_ACTION_IN_SUGGEST_AIM_CONTENTS,
+      },
+      {
+          omnibox::
+              SuggestTemplateInfo_TemplateAction_ActionType_CHROME_TAB_SWITCH,
+          IDS_OMNIBOX_ACTION_IN_SUGGEST_TAB_SWITCH_HINT,
+          IDS_OMNIBOX_ACTION_IN_SUGGEST_TAB_SWITCH_CONTENTS,
+          IDS_ACC_OMNIBOX_ACTION_IN_SUGGEST_SUFFIX,
+          IDS_OMNIBOX_ACTION_IN_SUGGEST_TAB_SWITCH_CONTENTS,
       }};
 
   for (const auto& test_case : test_cases) {
@@ -135,7 +146,8 @@ TEST_F(OmniboxActionInSuggestTest, ConversionFromAction) {
       omnibox::SuggestTemplateInfo_TemplateAction_ActionType_CALL,
       omnibox::SuggestTemplateInfo_TemplateAction_ActionType_DIRECTIONS,
       omnibox::SuggestTemplateInfo_TemplateAction_ActionType_REVIEWS,
-      omnibox::SuggestTemplateInfo_TemplateAction_ActionType_CHROME_AIM};
+      omnibox::SuggestTemplateInfo_TemplateAction_ActionType_CHROME_AIM,
+      omnibox::SuggestTemplateInfo_TemplateAction_ActionType_CHROME_TAB_SWITCH};
 
   for (auto test_case : test_cases) {
     TemplateAction template_action;
@@ -211,6 +223,8 @@ TEST_F(OmniboxActionInSuggestTest, HistogramsRecording) {
     kWebsite,
     kReviews,
     kAim,
+    kLens,
+    kTabSwitch,
   };
 
   // Correlation between ActionType and UMA-recorded bucket.
@@ -228,6 +242,9 @@ TEST_F(OmniboxActionInSuggestTest, HistogramsRecording) {
        UmaTypeForTest::kReviews, "Omnibox.ActionInSuggest.UsageByType.Reviews"},
       {omnibox::SuggestTemplateInfo_TemplateAction_ActionType_CHROME_AIM,
        UmaTypeForTest::kAim, "Omnibox.ActionInSuggest.UsageByType.AIM"},
+      {omnibox::SuggestTemplateInfo_TemplateAction_ActionType_CHROME_TAB_SWITCH,
+       UmaTypeForTest::kTabSwitch,
+       "Omnibox.ActionInSuggest.UsageByType.TabSwitch"},
   };
 
   for (const auto& test_case : test_cases) {
