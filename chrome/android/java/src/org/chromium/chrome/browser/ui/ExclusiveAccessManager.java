9d9eff8485b8b882ee608eb5d85534c2a4847405
Wojciech Zwonarz <wzwonarz@google.com>
2025-09-23 06:34:57 -0700

    Android EAM add observers for the tab events
    The EAM callbacks:
     1. OnTabDeactivated(content::WebContents* web_contents);
     2. OnTabDetachedFromView(content::WebContents* web_contents);
     3. OnTabClosing(content::WebContents* web_contents);
    need to be supported. Currently most of the handling is done in the
    FullscreenManager on the Java side.
    
    The callbacks are registered in the EAM Java side and forwarded to the native object.
    
    Bug: 434899091
    Change-Id: I0ccfab009927764e1dce49c7fb07ff3850021468
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6797806
    Reviewed-by: Abdelrahman Eed <abdoeed@google.com>
    Commit-Queue: Wojciech Zwonarz <wzwonarz@google.com>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1519292}

--- chrome/android/java/src/org/chromium/chrome/browser/ui/ExclusiveAccessManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ui/ExclusiveAccessManager.java
@@ -16,6 +16,10 @@ import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.browser.ActivityTabProvider;
 import org.chromium.chrome.browser.fullscreen.FullscreenManager;
 import org.chromium.chrome.browser.fullscreen.FullscreenOptions;
+import org.chromium.chrome.browser.tab.Tab;
+import org.chromium.chrome.browser.tabmodel.TabModel;
+import org.chromium.chrome.browser.tabmodel.TabModelObserver;
+import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.components.browser_ui.desktop_windowing.DesktopWindowStateManager;
 import org.chromium.content_public.browser.WebContents;
 
@@ -34,6 +38,8 @@ public class ExclusiveAccessManager
     private long mExclusiveAccessManagerAndroidNativePointer;
     private final FullscreenManager mFullscreenManager;
     private @MonotonicNonNull DesktopWindowStateManager mDesktopWindowStateManager;
+    @Nullable private TabModelSelector mTabModelSelector;
+    private final TabModelObserver mTabModelObserver;
 
     public ExclusiveAccessManager(
             Activity activity,
@@ -48,6 +54,45 @@ public class ExclusiveAccessManager
             mDesktopWindowStateManager = desktopWindowStateManager;
             mDesktopWindowStateManager.addObserver(this);
         }
+        mFullscreenManager.setFullscreenManagerDelegate(
+                new FullscreenManager.FullscreenManagerDelegate() {
+                    @Override
+                    public void onExitFullscreen(@Nullable Tab tab) {
+                        if (tab == null) {
+                            ExclusiveAccessManagerJni.get()
+                                    .exitExclusiveAccess(
+                                            mExclusiveAccessManagerAndroidNativePointer);
+                        } else {
+                            deactivateTab(tab);
+                        }
+                    }
+                });
+        mTabModelObserver =
+                new TabModelObserver() {
+                    @Override
+                    public void willCloseTab(Tab tab, boolean didCloseAlone) {
+                        ExclusiveAccessManagerJni.get()
+                                .onTabClosing(
+                                        mExclusiveAccessManagerAndroidNativePointer,
+                                        tab.getWebContents());
+                    }
+                };
+    }
+
+    public void initialize(TabModelSelector modelSelector) {
+        mTabModelSelector = modelSelector;
+        for (TabModel model : modelSelector.getModels()) {
+            model.addObserver(mTabModelObserver);
+        }
+    }
+
+    private void deactivateTab(Tab tab) {
+        ExclusiveAccessManagerJni.get()
+                .onTabDeactivated(
+                        mExclusiveAccessManagerAndroidNativePointer, tab.getWebContents());
+        ExclusiveAccessManagerJni.get()
+                .onTabDetachedFromView(
+                        mExclusiveAccessManagerAndroidNativePointer, tab.getWebContents());
     }
 
     /**
@@ -83,6 +128,9 @@ public class ExclusiveAccessManager
      * @return is currently in fullscreen
      */
     public boolean isFullscreenForTabOrPending(WebContents webContents) {
+        if (webContents == null) {
+            return false;
+        }
         return ExclusiveAccessManagerJni.get()
                 .isFullscreenForTabOrPending(
                         mExclusiveAccessManagerAndroidNativePointer, webContents);
@@ -155,6 +203,11 @@ public class ExclusiveAccessManager
             mExclusiveAccessManagerAndroidNativePointer = 0;
         }
         if (mDesktopWindowStateManager != null) mDesktopWindowStateManager.removeObserver(this);
+        if (mTabModelSelector != null) {
+            for (TabModel model : mTabModelSelector.getModels()) {
+                model.removeObserver(mTabModelObserver);
+            }
+        }
     }
 
     @NativeMethods
@@ -198,6 +251,15 @@ public class ExclusiveAccessManager
         void lostPointerLock(long nativeExclusiveAccessManagerAndroid);
 
         void exitExclusiveAccess(long nativeExclusiveAccessManagerAndroid);
+
+        void onTabDeactivated(
+                long nativeExclusiveAccessManagerAndroid, @Nullable WebContents webContents);
+
+        void onTabDetachedFromView(
+                long nativeExclusiveAccessManagerAndroid, @Nullable WebContents webContents);
+
+        void onTabClosing(
+                long nativeExclusiveAccessManagerAndroid, @Nullable WebContents webContents);
 
         void destroy(long nativeExclusiveAccessManagerAndroid);
     }
--- chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
@@ -978,6 +978,10 @@ public class RootUiCoordinator
                     mCallbackController.makeCancelable(this::initProfileDependentFeatures));
         }
 
+        if (ChromeFeatureList.sEnableExclusiveAccessManager.isEnabled()) {
+            mExclusiveAccessManager.initialize(mTabModelSelectorSupplier.get());
+        }
+
         initMessagesInfra();
         initScrollCapture();
         mAdaptiveToolbarUiCoordinator.onFinishNativeInitialization();
