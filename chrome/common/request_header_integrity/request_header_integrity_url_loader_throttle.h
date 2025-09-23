58434e1d935e148bb9b3d53a86501504b920b5b1
Robert Liao <robliao@chromium.org>
2025-09-22 18:11:25 -0700

    Add Redirect Support for the RequestHeaderIntegrityURLLoaderThrottle
    Bug: 445439417
    Change-Id: I569894e3f9bdd62e1050d33ff9a362554a1b32da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6967563
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Auto-Submit: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1519060}

--- chrome/common/request_header_integrity/request_header_integrity_url_loader_throttle.h
+++ chrome/common/request_header_integrity/request_header_integrity_url_loader_throttle.h
@@ -8,6 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/public/common/loader/url_loader_throttle.h"
 
+namespace net {
+class HttpRequestHeaders;
+}
+
 namespace network::mojom {
 class NetworkContextParams;
 }
@@ -29,6 +33,13 @@ class RequestHeaderIntegrityURLLoaderThrottle
   void DetachFromCurrentSequence() override;
   void WillStartRequest(network::ResourceRequest* request,
                         bool* defer) override;
+  void WillRedirectRequest(
+      net::RedirectInfo* redirect_info,
+      const network::mojom::URLResponseHead& response_head,
+      bool* defer,
+      std::vector<std::string>* to_be_removed_request_headers,
+      net::HttpRequestHeaders* modified_request_headers,
+      net::HttpRequestHeaders* modified_cors_exempt_request_headers) override;
 
   static bool IsFeatureEnabled();
   static void UpdateCorsExemptHeaders(
