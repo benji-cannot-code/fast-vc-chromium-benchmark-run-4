74ad0eb1f5f141697d7b9e56589c7170c686fcfb
Quentin Pubert <qpubert@google.com>
2025-09-24 04:28:31 -0700

    [iOS] Fix Reader mode WebState dependency installation
    This CL updates the ReaderModeBrowserAgent so it observes all WebStates
    and ReaderModeTabHelper instances in a given browser. This allows it to
    invoke `web_state_delegate_` to install/uninstall dependencies of the
    Reader mode WebState correctly when the Reader Mode WebState becomes
    available/unavailable.
    
    Bug: 438125409
    Change-Id: I99dc5785da204a06e742c74f85372a4cd0b55b73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6965757
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Auto-Submit: Quentin Pubert <qpubert@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1519904}

--- ios/chrome/browser/reader_mode/model/reader_mode_browser_agent.h
+++ ios/chrome/browser/reader_mode/model/reader_mode_browser_agent.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_READER_MODE_MODEL_READER_MODE_BROWSER_AGENT_H_
 #define IOS_CHROME_BROWSER_READER_MODE_MODEL_READER_MODE_BROWSER_AGENT_H_
 
+#import "base/scoped_multi_source_observation.h"
 #import "base/scoped_observation.h"
 #import "ios/chrome/browser/reader_mode/model/reader_mode_browser_agent_delegate.h"
 #import "ios/chrome/browser/reader_mode/model/reader_mode_browser_agent_web_state_delegate.h"
@@ -25,6 +26,7 @@ class WebStateList;
 // currently active WebState.
 class ReaderModeBrowserAgent : public BrowserUserData<ReaderModeBrowserAgent>,
                                public WebStateListObserver,
+                               public web::WebStateObserver,
                                public ReaderModeTabHelper::Observer {
  public:
   ReaderModeBrowserAgent(const ReaderModeBrowserAgent&) = delete;
@@ -41,6 +43,19 @@ class ReaderModeBrowserAgent : public BrowserUserData<ReaderModeBrowserAgent>,
 
   explicit ReaderModeBrowserAgent(Browser* browser);
 
+  // Helper returning the active WebState in the browser.
+  web::WebState* GetActiveWebState();
+  // Attaches a WebState to this browser agent.
+  void AttachWebState(web::WebState* web_state);
+  // Detaches a WebState from this browser agent.
+  void DetachWebState(web::WebState* web_state);
+  // Returns the ReaderModeTabHelper of the active WebState.
+  ReaderModeTabHelper* GetActiveReaderModeTabHelper();
+  // Attaches a ReaderModeTabHelper to this browser agent.
+  void AttachReaderModeTabHelper(ReaderModeTabHelper* tab_helper);
+  // Detaches a ReaderModeTabHelper from this browser agent.
+  void DetachReaderModeTabHelper(ReaderModeTabHelper* tab_helper);
+
   // Show/hide the Reader mode UI.
   void ShowReaderModeUI(BOOL animated);
   void HideReaderModeUI(BOOL animated);
@@ -55,6 +70,10 @@ class ReaderModeBrowserAgent : public BrowserUserData<ReaderModeBrowserAgent>,
                              const WebStateListStatus& status) override;
   void WebStateListDestroyed(WebStateList* web_state_list) override;
 
+  // web::WebStateObserver methods.
+  void WebStateRealized(web::WebState* web_state) override;
+  void WebStateDestroyed(web::WebState* web_state) override;
+
   // ReaderModeTabHelper::Observer methods.
   void ReaderModeWebStateDidLoadContent(ReaderModeTabHelper* tab_helper,
                                         web::WebState* web_state) override;
@@ -67,6 +86,11 @@ class ReaderModeBrowserAgent : public BrowserUserData<ReaderModeBrowserAgent>,
 
   base::ScopedObservation<WebStateList, WebStateListObserver>
       web_state_list_scoped_observation_{this};
+  base::ScopedMultiSourceObservation<web::WebState, web::WebStateObserver>
+      web_state_scoped_observation_{this};
+  base::ScopedMultiSourceObservation<ReaderModeTabHelper,
+                                     ReaderModeTabHelper::Observer>
+      reader_mode_tab_helper_scoped_observation_{this};
 
   // The delegate for this agent.
   id<ReaderModeBrowserAgentDelegate> delegate_;
