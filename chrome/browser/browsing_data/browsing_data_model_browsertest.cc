c636875e46a7f355ff1747a88e25728b88bf419e
Olivier Li <olivierli@chromium.org>
2025-09-24 09:23:38 -0700

    Reduce best effort related flakiness in BrowsingDataModelBrowserTest
    The while loop waiting for results can be replaced with RunUntil for
    simplicity and reproducability.
    
    Additionally the results waited on are sensitive to how quickly some
    best effort tasks are run so the test is not compatible with delaying
    them through EnableBestEffortTaskInhibitingPolicy.
    
    Bug: 441949788
    Change-Id: Ic9a3e76c0d386bf7eaf1f76d626cd7d2b294e63d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6966235
    Commit-Queue: Olivier Li <olivierli@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1520042}

--- chrome/browser/browsing_data/browsing_data_model_browsertest.cc
+++ chrome/browser/browsing_data/browsing_data_model_browsertest.cc
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/browser/page_specific_content_settings.h"
 #include "components/content_settings/core/browser/cookie_settings.h"
 #include "components/network_session_configurator/common/network_switches.h"
+#include "components/performance_manager/public/features.h"
 #include "components/privacy_sandbox/privacy_sandbox_attestations/privacy_sandbox_attestations.h"
 #include "components/privacy_sandbox/privacy_sandbox_settings.h"
 #include "components/services/storage/public/mojom/local_storage_control.mojom.h"
@@ -445,12 +446,8 @@ void AddLocalStorageUsage(content::RenderFrameHost* render_frame_host,
 }
 
 void WaitForModelUpdate(BrowsingDataModel* model, size_t expected_size) {
-  while (model->size() != expected_size) {
-    base::RunLoop run_loop;
-    base::SingleThreadTaskRunner::GetCurrentDefault()->PostDelayedTask(
-        FROM_HERE, run_loop.QuitClosure(), TestTimeouts::tiny_timeout());
-    run_loop.Run();
-  }
+  ASSERT_TRUE(
+      base::test::RunUntil([&]() { return model->size() == expected_size; }));
 }
 
 void RemoveBrowsingDataForDataOwner(BrowsingDataModel* model,
@@ -503,7 +500,12 @@ class BrowsingDataModelBrowserTest
         // Need to disable kCompressionDictionaryTransportRequireKnownRootCert
         // because EmbeddedTestServer's certificate is not rooted at a standard
         // CA root.
-        net::features::kCompressionDictionaryTransportRequireKnownRootCert};
+        net::features::kCompressionDictionaryTransportRequireKnownRootCert,
+
+        // Model update success is sensitive to how quickly some best effort
+        // tasks are run so the test is not compatible with delaying
+        // them through EnableBestEffortTaskInhibitingPolicy.
+        performance_manager::features::kEnableBestEffortTaskInhibitingPolicy};
 
 #if BUILDFLAG(ENABLE_LIBRARY_CDMS)
     enabled_features.push_back({media::kExternalClearKeyForTesting, {}});
