96e367e18db684e01d224d8d711d99e8d21c649d
Linyu He <linyuh@google.com>
2025-09-24 09:21:01 -0700

    Relay window focus change event to extension internals
    This CL relays window focus changes received by
    ExtensionWindowControllerBridge to extensions::WindowController, which
    completes the E2E flow for reporting window focus changes.
    
    An integration test is added to verify the logic on an emulator.
    
    Change-Id: I0020256e02ec3dcb528c0f634d961f26871b90ef
    Bug: 424857039
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6973935
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Linyu He <linyuh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1520038}

--- chrome/browser/ui/android/extensions/windowing/internal/window_controller_list_observer_for_testing.h
+++ chrome/browser/ui/android/extensions/windowing/internal/window_controller_list_observer_for_testing.h
@@ -44,6 +44,8 @@ class WindowControllerListObserverForTesting final
       extensions::WindowController* window_controller) override;
   void OnWindowBoundsChanged(
       extensions::WindowController* window_controller) override;
+  void OnWindowFocusChanged(extensions::WindowController* window_controller,
+                            bool has_focus) override;
 };
 
 // Events to be relayed by |WindowControllerListObserverForTesting| so that
@@ -54,6 +56,8 @@ class WindowControllerListObserverForTesting final
 enum class ExtensionInternalWindowEventForTesting {
   UNKNOWN,
   BOUNDS_CHANGED,
+  FOCUS_OBTAINED,
+  FOCUS_LOST,
   CREATED,
   REMOVED
 };
