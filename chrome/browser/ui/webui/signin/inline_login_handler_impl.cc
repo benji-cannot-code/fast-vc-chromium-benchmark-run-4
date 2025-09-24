34bd9bc3edfa4869a21dc8e07c16a4e63272c135
Amelie Schneider <amelies@google.com>
2025-09-24 07:37:31 -0700

    [UNO] Don't show sync optin screen in GCPW flow
    This change is behind the flag `ReplaceSyncPromosWithSignInPromos`.
    
    Bug: 411329294
    Change-Id: I068d3605f070f87f7a071facba3ddb498ddc5923
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6973402
    Commit-Queue: Amelie Schneider <amelies@google.com>
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1519976}

--- chrome/browser/ui/webui/signin/inline_login_handler_impl.cc
+++ chrome/browser/ui/webui/signin/inline_login_handler_impl.cc
@@ -75,6 +75,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/identity_manager/accounts_mutator.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/signin/public/identity_manager/primary_account_mutator.h"
+#include "components/sync/base/features.h"
 #include "content/public/browser/storage_partition.h"
 #include "content/public/browser/web_ui.h"
 #include "google_apis/gaia/gaia_auth_fetcher.h"
@@ -461,6 +462,13 @@ void InlineSigninHelper::CreateSyncStarter(const std::string& refresh_token) {
           signin_metrics::AccessPoint::kForcedSignin,
           signin_metrics::SourceForRefreshTokenOperation::
               kInlineLoginHandler_Signin);
+
+  // TODO(crbug.com/419539610): Reconsider if we want to show the history sync
+  // promo instead of stopping here.
+  if (base::FeatureList::IsEnabled(
+          syncer::kReplaceSyncPromosWithSignInPromos)) {
+    return;
+  }
 
   std::unique_ptr<TurnSyncOnHelper::Delegate> delegate =
       std::make_unique<ForcedSigninTurnSyncOnHelperDelegate>(browser);
