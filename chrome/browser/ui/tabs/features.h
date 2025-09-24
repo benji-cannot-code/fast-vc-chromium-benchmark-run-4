5f13c65c7f01d14aa9d3b6ba17916bbf4d5ba065
Kunal Daftari <kunaldaftari@google.com>
2025-09-24 08:03:39 -0700

    Reland "Vertical Tabs: Differentiate Vertical Tabs Pref vs Feature"
    This is a reland of commit 531f473a7c665b767832627cf230f7a49837fc68
    
    A call site within browser_view.cc was missed, causing the issue.
    
    Original change's description:
    > Vertical Tabs: Differentiate Vertical Tabs Pref vs Feature
    >
    > This CL works to differentiate the functions: IsVerticalTabsEnabled vs
    > AreVerticalTabsEnabled. It does so by clarifying whether the function
    > refers to the preference or the feature flag.
    >
    > Change-Id: Ia82ab7445e31c06de5456c84318ef37f9fe1b263
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6976332
    > Commit-Queue: Kunal Daftari <kunaldaftari@google.com>
    > Reviewed-by: Caroline Rising <corising@chromium.org>
    > Reviewed-by: Darryl James <dljames@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1519541}
    
    Change-Id: Ib4b4dfaa9e93a77bb8e7ea2682f69a78980a5531
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6977059
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Kunal Daftari <kunaldaftari@google.com>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1519986}

--- chrome/browser/ui/tabs/features.h
+++ chrome/browser/ui/tabs/features.h
@@ -37,7 +37,7 @@ BASE_DECLARE_FEATURE(kTabSelectionByPointer);
 
 bool CanShowTabSearchPositionSetting();
 bool AreTabGroupShortcutsEnabled();
-bool AreVerticalTabsEnabled();
+bool IsVerticalTabsFeatureEnabled();
 
 }  // namespace tabs
 
