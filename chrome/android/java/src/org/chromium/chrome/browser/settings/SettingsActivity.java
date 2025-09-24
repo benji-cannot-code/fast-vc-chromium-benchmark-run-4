cef3cce0e30e58c1a70a5264579c9d4b23e3ded0
Gazal Agarwal <agazal@google.com>
2025-09-24 15:29:57 -0700

    [Settings containment] Apply containment in multi-column mode
    Makes the FragmentLifecycleCallbacks for AndroidSettingsContainment
    recursive to ensure containment is applied when MultiColumnSettings is
    enabled.
    
    Screenshot: https://screenshot.googleplex.com/93RDp3SPtfKgeY4.png
    
    Bug: 433576895, 439725777
    Change-Id: I76695aa3fb9e324ea9846610476c33172198787f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6977492
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Auto-Submit: Gazal Agarwal <agazal@google.com>
    Commit-Queue: Gazal Agarwal <agazal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1520131}

--- chrome/android/java/src/org/chromium/chrome/browser/settings/SettingsActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/SettingsActivity.java
@@ -197,6 +197,9 @@ public class SettingsActivity extends ChromeBaseAppCompatActivity
         }
 
         if (ChromeFeatureList.sAndroidSettingsContainment.isEnabled()) {
+            // In multi-column mode, the main settings fragment is a child of the
+            // MultiColumnSettings fragment, so the callbacks must be registered recursively.
+            boolean recursive = true;
             fragmentManager.registerFragmentLifecycleCallbacks(
                     new FragmentManager.FragmentLifecycleCallbacks() {
                         @Override
@@ -236,7 +239,7 @@ public class SettingsActivity extends ChromeBaseAppCompatActivity
                             }
                         }
                     },
-                    false /* recursive */);
+                    recursive);
         }
 
         super.onCreate(savedInstanceState);
