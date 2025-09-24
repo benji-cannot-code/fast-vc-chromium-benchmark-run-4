8dced18be37b6c3f689dbc135cd97ee278e5888b
Lei Zhang <thestig@chromium.org>
2025-09-24 09:13:16 -0700

    Delete dead code in omnibox_view.h
    Delete some virtual methods that have no callers. Then delete the
    production and test overrides.
    
    Change-Id: I79543d9f249523bde0c8d8201e3344a2cf503e2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6977117
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1520034}

--- chrome/browser/ui/omnibox/test_omnibox_view.cc
+++ chrome/browser/ui/omnibox/test_omnibox_view.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/omnibox/omnibox_controller.h"
 #include "chrome/browser/ui/omnibox/test_omnibox_edit_model.h"
 #include "components/omnibox/browser/test_omnibox_client.h"
-#include "ui/gfx/native_ui_types.h"
 
 // static
 OmniboxView::State TestOmniboxView::CreateState(std::string text,
@@ -94,18 +93,6 @@ void TestOmniboxView::OnRevertTemporaryText(const std::u16string& display_text,
 }
 
 bool TestOmniboxView::OnAfterPossibleChange(bool allow_keyword_ui_change) {
-  return false;
-}
-
-gfx::NativeView TestOmniboxView::GetNativeView() const {
-  return gfx::NativeView();
-}
-
-gfx::NativeView TestOmniboxView::GetRelativeWindowForPopup() const {
-  return gfx::NativeView();
-}
-
-bool TestOmniboxView::IsImeComposing() const {
   return false;
 }
 
