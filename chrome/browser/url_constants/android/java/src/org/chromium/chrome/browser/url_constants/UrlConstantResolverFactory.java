46ca73f0511456258f664b1a8250d40ebc6d4b46
Fiaz Muhammad <mfiaz@google.com>
2025-09-24 09:55:59 -0700

    [Url Override] Initialize Resolver Class and Factory
    - Initialize the Resolver class and the Factory.
    - The factory class keeps two instances of UrlResolvers. One for
    	incognito branded profiles, while another is for the original
    	profile.
    - The UrlResolver class allows us to register override objects that
    	conditionally override the default URL string.
    
    Bug: 439831018
    Change-Id: Idc9041d7c40f84c2a7ee7bf84bbd9fcc3a110805
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6979524
    Auto-Submit: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1520057}

--- chrome/browser/url_constants/android/java/src/org/chromium/chrome/browser/url_constants/UrlConstantResolverFactory.java
+++ chrome/browser/url_constants/android/java/src/org/chromium/chrome/browser/url_constants/UrlConstantResolverFactory.java
@@ -5,11 +5,98 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.url_constants;
 
+import org.chromium.base.ResettersForTesting;
 import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
+import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.components.embedder_support.util.UrlConstants;
 
-/** Creates instances of {@link UrlConstantResolver}, keyed to profiles. */
+/**
+ * This factory creates and keeps a single ExtensionsUrlOverrideRegistryManager for incognito
+ * profiles and non-incognito profiles respectively.
+ */
 @NullMarked
 public class UrlConstantResolverFactory {
+    private static @Nullable UrlConstantResolver sOriginalResolver;
+    private static @Nullable UrlConstantResolver sIncognitoResolver;
+    private static @Nullable UrlConstantResolver sResolverForTesting;
+
     // Prevent instantiation.
     private UrlConstantResolverFactory() {}
+
+    public UrlConstantResolver getForProfile(@Nullable Profile profile) {
+        if (sResolverForTesting != null) {
+            return sResolverForTesting;
+        }
+
+        if (profile == null || !profile.isIncognitoBranded()) {
+            return getOriginalResolver();
+        }
+
+        return getIncognitoResolver();
+    }
+
+    private static UrlConstantResolver getOriginalResolver() {
+        if (sOriginalResolver == null) {
+            sOriginalResolver = buildOriginalResolver();
+        }
+        return sOriginalResolver;
+    }
+
+    private static UrlConstantResolver getIncognitoResolver() {
+        if (sIncognitoResolver == null) {
+            sIncognitoResolver = buildIncognitoResolver();
+        }
+        return sIncognitoResolver;
+    }
+
+    private static UrlConstantResolver buildOriginalResolver() {
+        UrlConstantResolver resolver = new UrlConstantResolver();
+        if (!ChromeFeatureList.sChromeNativeUrlOverriding.isEnabled()) return resolver;
+
+        resolver.registerOverride(
+                UrlConstants.NTP_URL,
+                () ->
+                        ExtensionsUrlOverrideRegistry.getNtpOverrideEnabled()
+                                ? UrlConstants.NTP_NON_NATIVE_URL
+                                : null);
+        resolver.registerOverride(
+                UrlConstants.BOOKMARKS_NATIVE_URL,
+                () ->
+                        ExtensionsUrlOverrideRegistry.getBookmarksPageOverrideEnabled()
+                                ? UrlConstants.BOOKMARKS_URL
+                                : null);
+        resolver.registerOverride(
+                UrlConstants.NATIVE_HISTORY_URL,
+                () ->
+                        ExtensionsUrlOverrideRegistry.getHistoryPageOverrideEnabled()
+                                ? UrlConstants.HISTORY_URL
+                                : null);
+        return resolver;
+    }
+
+    private static UrlConstantResolver buildIncognitoResolver() {
+        UrlConstantResolver resolver = new UrlConstantResolver();
+        if (!ChromeFeatureList.sChromeNativeUrlOverriding.isEnabled()) return resolver;
+
+        resolver.registerOverride(
+                UrlConstants.NTP_URL,
+                () ->
+                        ExtensionsUrlOverrideRegistry.getIncognitoNtpOverrideEnabled()
+                                ? UrlConstants.NTP_NON_NATIVE_URL
+                                : null);
+        resolver.registerOverride(
+                UrlConstants.BOOKMARKS_NATIVE_URL,
+                () ->
+                        ExtensionsUrlOverrideRegistry.getIncognitoBookmarksPageOverrideEnabled()
+                                ? UrlConstants.BOOKMARKS_URL
+                                : null);
+        return resolver;
+    }
+
+    public static void setForTesting(UrlConstantResolver resolver) {
+        sResolverForTesting = resolver;
+        ResettersForTesting.register(() -> sResolverForTesting = null);
+    }
 }
