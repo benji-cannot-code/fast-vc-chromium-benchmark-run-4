7f2494cd3db9ef0af1dd1104bfb775a926f11fc7
Aishwarya Rajesh <aishwaryarj@google.com>
2025-09-23 18:22:45 -0700

    [EWBC] Create a new JNI Java class method to create an Android browser window synchronously
    Bug: 444743737
    Change-Id: Ice1238f87b9a2dbfda83f12ddd505b0db2b244b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6962157
    Reviewed-by: Linyu He <linyuh@google.com>
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1519722}

---
+++ chrome/browser/ui/browser_window/internal/android/java/src/org/chromium/chrome/browser/ui/browser_window/BrowserWindowCreatorBridge.java
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.ui.browser_window;
+
+import androidx.annotation.VisibleForTesting;
+
+import org.jni_zero.CalledByNative;
+
+import org.chromium.build.annotations.NullMarked;
+
+/** Java class that will be used by the native {@code #CreateBrowserWindow()} function. */
+@NullMarked
+final class BrowserWindowCreatorBridge {
+    private BrowserWindowCreatorBridge() {}
+
+    @CalledByNative
+    @VisibleForTesting
+    static long createBrowserWindow(AndroidBrowserWindowCreateParams createParams) {
+        ChromeAndroidTaskTracker tracker = ChromeAndroidTaskTrackerFactory.getInstance();
+        assert tracker != null;
+        ChromeAndroidTask task = tracker.createPendingTask(createParams);
+        return task.getOrCreateNativeBrowserWindowPtr();
+    }
+}
