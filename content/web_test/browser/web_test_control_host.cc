11c5bb114fe0794a26ce97d302f78117c717553a
Elly <ellyjones@chromium.org>
2025-09-24 08:01:50 -0700

    content: stop using base/hash/md5 in web test host
    Instead, use crypto/obsolete/md5 (which despite the name is the
    replacement for base/hash/md5).
    
    Bug: 406729261
    Change-Id: I0d4a29c92369df3a0c842274502d80469faa7a9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6977634
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1519985}

--- content/web_test/browser/web_test_control_host.cc
+++ content/web_test/browser/web_test_control_host.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/functional/callback_helpers.h"
-#include "base/hash/md5.h"
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/memory/raw_ptr.h"
@@ -95,6 +94,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/web_test/common/web_test_constants.h"
 #include "content/web_test/common/web_test_string_util.h"
 #include "content/web_test/common/web_test_switches.h"
+#include "crypto/obsolete/md5.h"
 #include "mojo/public/cpp/bindings/sync_call_restrictions.h"
 #include "net/cookies/cookie_util.h"
 #include "services/device/public/cpp/compute_pressure/buildflags.h"
@@ -123,6 +123,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace content {
+
+std::string Md5OfPixelsAsHexForWebTests(base::span<const uint8_t> pixels) {
+  return base::ToLowerASCII(
+      base::HexEncode(crypto::obsolete::Md5::Hash(pixels)));
+}
 
 namespace {
 
@@ -741,7 +746,6 @@ void WebTestControlHost::ResetBrowserAfterWebTest() {
   layout_dump_.reset();
   waiting_for_layout_dumps_ = 0;
   pixel_dump_.reset();
-  actual_pixel_hash_ = "";
   waiting_for_pixel_results_ = false;
   composite_all_frames_node_queue_ =
       std::queue<raw_ptr<Node, CtnExperimental>>();
@@ -1408,14 +1412,11 @@ void WebTestControlHost::ReportResults() {
     // can't track initializedness across processes, we must assure it that the
     // pixels are in fact initialized.
     MSAN_UNPOISON(pixel_dump_->getPixels(), pixel_dump_->computeByteSize());
-    base::MD5Digest digest;
     auto bytes = UNSAFE_TODO(
         base::span(static_cast<const uint8_t*>(pixel_dump_->getPixels()),
                    pixel_dump_->computeByteSize()));
-    base::MD5Sum(bytes, &digest);
-    actual_pixel_hash_ = base::MD5DigestToBase16(digest);
 
-    OnImageDump(actual_pixel_hash_, *pixel_dump_);
+    OnImageDump(Md5OfPixelsAsHexForWebTests(bytes), *pixel_dump_);
   } else if (!renderer_dump_result_->actual_pixel_hash.empty()) {
     OnImageDump(renderer_dump_result_->actual_pixel_hash,
                 renderer_dump_result_->pixels);
