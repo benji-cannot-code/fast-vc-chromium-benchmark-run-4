1ff2ff77754ab48ad3e002a5634e30b6aa9b5745
David Sanders <dsanders11@ucsbalum.com>
2025-09-23 22:25:23 -0700

    Remove unused include in file_backed_blob_factory_dispatcher.h
    Should save ~18 MiB of compiler input size.
    
    Bug: 40318405
    Change-Id: I6c99ee189ad50dfc2a724fd7254b16c1c68d9b30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6975204
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1519776}

--- third_party/blink/renderer/core/fileapi/file_backed_blob_factory_dispatcher.h
+++ third_party/blink/renderer/core/fileapi/file_backed_blob_factory_dispatcher.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/blob/file_backed_blob_factory.mojom-blink.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context_lifecycle_observer.h"
-#include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_associated_remote.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
 #include "third_party/blink/renderer/platform/supplementable.h"
