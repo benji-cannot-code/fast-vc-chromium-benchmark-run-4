c1a037a5863e73b62f9e5c9b0e442dfc0724437f
Karol Sygiet <sygiet@google.com>
2025-09-24 01:42:57 -0700

    [AccountName] Record user decision on profile import
    This CL adds metric recording the user decision during NameEmailSuperset
    and HomeWorkAndNameEmailMerge imports.
    
    Bug: 356845298
    Change-Id: I34990678ec7459199a1655e1206ad0c0f84957e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6975980
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Cr-Commit-Position: refs/heads/main@{#1519836}

--- components/autofill/core/browser/metrics/profile_import_metrics.cc
+++ components/autofill/core/browser/metrics/profile_import_metrics.cc
@@ -239,6 +239,12 @@ void LogNewProfileImportDecision(
   }
 }
 
+void LogHomeWorkNameEmailMergeImportDecision(
+    AutofillClient::AddressPromptUserDecision decision) {
+  base::UmaHistogramEnumeration(
+      "Autofill.ProfileImport.HomeOrWorkAndNameEmailMergeDecision", decision);
+}
+
 void LogNewProfileStorageLocation(const AutofillProfile& import_candidate) {
   base::UmaHistogramEnumeration(
       "Autofill.ProfileImport.StorageNewAddressIsSavedTo",
@@ -263,6 +269,12 @@ void LogProfileUpdateImportDecision(
     base::UmaHistogramEnumeration(
         base::StrCat({kNameBase, "UserHasQuasiDuplicateProfile"}), decision);
   }
+}
+
+void LogNameEmailSupersetImportDecision(
+    AutofillClient::AddressPromptUserDecision decision) {
+  base::UmaHistogramEnumeration(
+      "Autofill.ProfileImport.NameEmailSupersetProfileDecision", decision);
 }
 
 void LogHomeAndWorkSupersetImportDecision(
