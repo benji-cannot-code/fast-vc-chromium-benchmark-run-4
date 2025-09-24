1ba5756e927e081148a77790de17354ce53ff313
Manish Goregaokar <manishearth@google.com>
2025-09-23 17:10:10 -0700

    Update temporal_rs to 0.1.0
    Change-Id: I8a0240908d3c27bd06d7bcd86a52ebf59b0c2b43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6976801
    Commit-Queue: Manish Goregaokar <manishearth@google.com>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1519687}

---
+++ third_party/rust/chromium_crates_io/vendor/temporal_capi-v0_1/bindings/c/TimeZone.d.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#ifndef TimeZone_D_H
+#define TimeZone_D_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include "diplomat_runtime.h"
+
+
+
+
+
+typedef struct TimeZone {
+  int16_t offset_minutes;
+  size_t resolved_id;
+  size_t normalized_id;
+  bool is_iana_id;
+} TimeZone;
+
+typedef struct TimeZone_option {union { TimeZone ok; }; bool is_ok; } TimeZone_option;
+
+
+
+#endif // TimeZone_D_H
---
+++ third_party/rust/chromium_crates_io/vendor/temporal_rs-v0_1/src/utils.rs
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+//! Utility date and time equations for Temporal
+
+pub(crate) use timezone_provider::utils::epoch_days_from_gregorian_date;
+
+// NOTE: Potentially add more of tests.
+
+// ==== Begin Date Equations ====
+
+pub(crate) const MS_PER_HOUR: i64 = 3_600_000;
+pub(crate) const MS_PER_MINUTE: i64 = 60_000;
+
+pub(crate) use timezone_provider::utils::{
+    epoch_days_to_epoch_ms, iso_days_in_month, ymd_from_epoch_milliseconds,
+};
+
+// ==== End Calendar Equations ====
