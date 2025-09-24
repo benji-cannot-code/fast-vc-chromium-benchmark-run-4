f3656186a09ae72c22a301122adb69225d365bec
Colin Blundell <blundell@chromium.org>
2025-09-24 02:00:16 -0700

    [Blink] Remove CRP::UseOopRasterization()
    At the one callsite of this method, it is guaranteed to return true.
    
    Bug: 391648152
    Change-Id: I5013043f66c9d498bb56664d9a4ef50d19ae7266
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6973562
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1519843}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
@@ -377,7 +377,6 @@ class PLATFORM_EXPORT CanvasResourceProvider
   friend class FlushForImageListener;
 
   virtual sk_sp<SkSurface> CreateSkSurface() const = 0;
-  virtual bool UseOopRasterization() { return false; }
   // Notifies before any drawing will be done on the resource used by this
   // provider.
   virtual void WillDraw() {}
@@ -500,7 +499,6 @@ class PLATFORM_EXPORT CanvasResourceProviderSharedImage
   }
   bool IsAccelerated() const final { return is_accelerated_; }
   bool SupportsDirectCompositing() const override { return true; }
-  bool UseOopRasterization() final { return IsAccelerated(); }
   bool unused_resources_reclaim_timer_is_running_for_testing() const {
     return unused_resources_reclaim_timer_.IsRunning();
   }
