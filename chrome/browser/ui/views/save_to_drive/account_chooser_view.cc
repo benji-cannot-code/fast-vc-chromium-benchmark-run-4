aceae9911283faae69815689af0a3bdd2076e214
Tiffany Shen <tishen@google.com>
2025-09-24 05:48:27 -0700

    [Save PDF to Drive] Add accessible text for AccountChooserView.
    According to the
    [mocks](https://screenshot.googleplex.com/AG5Xf8nTGriT8Vz), some of the
    accessibility text differs from the display text; these strings need to
    be added and translated.
    
    Bug: 422125538
    Change-Id: If28532c0a6b49fc19f3b46b086a8b03b67cee1d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6969556
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Auto-Submit: Tiffany Shen <tishen@google.com>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1519923}

--- chrome/browser/ui/views/save_to_drive/account_chooser_view.cc
+++ chrome/browser/ui/views/save_to_drive/account_chooser_view.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/vector_icons/vector_icons.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
+#include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/controls/button/md_text_button.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/controls/scroll_view.h"
@@ -219,6 +220,8 @@ std::unique_ptr<views::Label> AccountChooserView::CreateTitleLabel(
       views::style::STYLE_HEADLINE_4);
   title_label->SetEnabledColor(ui::kColorSysOnSurface);
   SetLabelProperties(title_label.get());
+  title_label->GetViewAccessibility().SetName(l10n_util::GetStringUTF16(
+      IDS_ACCOUNT_CHOOSER_HEADER_ACCESSIBILITY_LABEL));
   return title_label;
 }
 
@@ -251,6 +254,9 @@ std::unique_ptr<views::View> AccountChooserView::CreateTitleView(
     const std::vector<AccountInfo>& accounts) {
   auto title_view = std::make_unique<views::FlexLayoutView>();
   title_view->SetCrossAxisAlignment(views::LayoutAlignment::kCenter);
+  title_view->GetViewAccessibility().SetRole(ax::mojom::Role::kRegion);
+  title_view->GetViewAccessibility().SetName(l10n_util::GetStringUTF16(
+      IDS_ACCOUNT_CHOOSER_HEADER_ACCESSIBILITY_LABEL));
 
   auto title_container = std::make_unique<views::FlexLayoutView>();
   title_container->SetProperty(
