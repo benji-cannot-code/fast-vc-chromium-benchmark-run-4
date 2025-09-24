4339bf29d0c965257832a77cbe128ea67efc7372
David Sanders <dsanders11@ucsbalum.com>
2025-09-24 02:19:33 -0700

    Remove unused include in //ui/gfx/image/image.h
    Bug: 40318405
    Change-Id: I8913fc28d4f12ddfdf8145427823967b940d3bcb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6977496
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#1519862}

--- ui/gfx/image/image.h
+++ ui/gfx/image/image.h
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/component_export.h"
 #include "base/containers/span.h"
-#include "base/memory/scoped_policy.h"
 #include "base/memory/scoped_refptr.h"
 #include "build/build_config.h"
 #include "ui/gfx/native_ui_types.h"
