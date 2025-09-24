60a615d1b955441b25b6af8eb04f7102fae713f1
dpapad <dpapad@chromium.org>
2025-09-24 10:16:20 -0700

    NewTabPage: Remove last unnecessary imports of Polymer.
    Now that ui/webui/resources/cr_components/searchbox/ has been migrated
    to Lit, there should be no Polymer dependency in the NTP. However, it
    was discovered via DevTools that Polymer was still pulled in as a result
    of unnecessary Polymer imports in a few places.
    
    This reduces the amount of JS code loaded by chrome://new-tab-page by
    90kb.
    
    Bug: 445727122
    Change-Id: I856529d312b744090b1ee51f3d05948f049f32e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6974330
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1520075}

--- ui/webui/resources/cr_components/composebox/composebox_match.html.ts
+++ ui/webui/resources/cr_components/composebox/composebox_match.html.ts
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import '//resources/cr_elements/cr_icons.css.js';
-
 import {html} from '//resources/lit/v3_0/lit.rollup.js';
 
 import type {ComposeboxMatchElement} from './composebox_match.js';
