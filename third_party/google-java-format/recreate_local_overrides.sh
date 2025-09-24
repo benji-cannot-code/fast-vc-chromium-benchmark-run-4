ef52063a43f8d939e4935caeafe40b8d03e516db
Andrew Grieve <agrieve@chromium.org>
2025-09-24 09:01:55 -0700

    Reland "Roll google-java-format 1.17 -> 1.28"
    Reason for reland: Updated chromium-overrides.jar
    
    Changelog states this has better support for text blocks.
    
    Original change's description:
    > Revert "Roll google-java-format AQn4F5N... -> vwy9yNE..."
    >
    > This reverts commit 1f3c45cf34cc85216fd57016f162994d8d932651.
    >
    > Reason for revert: Fails to recognize switch expressions
    >
    > chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabsConnection.java:333:34: error: expected token: 'b'; generated ) instead
    >                 if (o instanceof Bundle b) {
    >
    > Original change's description:
    > > Roll google-java-format AQn4F5N... -> vwy9yNE...
    > >
    > > Changelog doesn't list any meaningful changes that I can tell.
    > >
    > > Change-Id: I6a2b701ab2172a8b7c83667adfe458e2ff5f3340
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6974728
    > > Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    > > Commit-Queue: Brian Ryner <bryner@google.com>
    > > Reviewed-by: Brian Ryner <bryner@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1519526}
    >
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: Ie218741d744a73d7b8b19633fb390f8321b8b974
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6977293
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1519737}
    
    Change-Id: I4d47fa943165e197f3f48623e36bbfb794db5521
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6978184
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Brian Ryner <bryner@google.com>
    Cr-Commit-Position: refs/heads/main@{#1520025}

---
+++ third_party/google-java-format/recreate_local_overrides.sh
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/bash
+set -x
+set -e
+cd $(dirname "$0")
+diff -r -u originals local_modifications > changes.patch && true
+for f in $(cd originals; find java -name "*.java"); do
+  echo "https://raw.githubusercontent.com/google/google-java-format/refs/heads/master/core/src/main/$f"
+  curl -o originals/$f "https://raw.githubusercontent.com/google/google-java-format/refs/heads/master/core/src/main/$f"
+  cp originals/$f local_modifications/$f
+done
+(cd local_modifications; patch -p1 < ../changes.patch)
