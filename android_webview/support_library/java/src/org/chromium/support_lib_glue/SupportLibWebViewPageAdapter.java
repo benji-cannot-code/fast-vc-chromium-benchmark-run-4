70af226fdd642fbda0d6b9e58f08578aec44d5bc
Rakina Zata Amni <rakina@chromium.org>
2025-09-23 18:09:16 -0700

    Expose isPrerendering in WebViewPageBoundaryInterface
    This was never exposed somehow, even though we already have it
    plumbed to AwPage.
    
    Bug: 437115164
    Change-Id: I5509ebaf759a0815e3f346447d67968f272ff1a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6958669
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1519710}

--- android_webview/support_library/java/src/org/chromium/support_lib_glue/SupportLibWebViewPageAdapter.java
+++ android_webview/support_library/java/src/org/chromium/support_lib_glue/SupportLibWebViewPageAdapter.java
@@ -5,10 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.support_lib_glue;
 
+import static org.chromium.support_lib_glue.SupportLibWebViewChromiumFactory.recordApiCall;
+
 import org.chromium.android_webview.AwPage;
 import org.chromium.android_webview.AwSupportLibIsomorphic;
 import org.chromium.android_webview.common.Lifetime;
+import org.chromium.base.TraceEvent;
 import org.chromium.support_lib_boundary.WebViewPageBoundaryInterface;
+import org.chromium.support_lib_glue.SupportLibWebViewChromiumFactory.ApiCall;
 
 /**
  * Adapter between WebViewPageBoundaryInterface and Page.
@@ -27,5 +31,14 @@ class SupportLibWebViewPageAdapter extends IsomorphicAdapter
     @Override
     AwSupportLibIsomorphic getPeeredObject() {
         return mPage;
+    }
+
+    @Override
+    public boolean isPrerendering() {
+        try (TraceEvent event =
+                TraceEvent.scoped("WebView.APICall.AndroidX.PAGE_IS_PRERENDERING")) {
+            recordApiCall(ApiCall.PAGE_IS_PRERENDERING);
+            return mPage.isPrerendering();
+        }
     }
 }
