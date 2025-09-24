f621d8a0647b4d378553e16c67a0bdb296f1b9e3
Charles Hager <clhager@google.com>
2025-09-24 14:14:43 -0700

    Move ToolbarWidthConsumer interface up
    This change moves the ToolbarWidthConsumer up to make it more broadly
    accessible.
    
    Bug: 427260734
    Change-Id: Ie152fc3e874ceb13572cd724d15abfddb83057b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6956787
    Commit-Queue: Charles Hager <clhager@google.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1520118}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarUtils.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarUtils.java
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 package org.chromium.chrome.browser.toolbar.top;
 
-import android.animation.Animator;
 import android.animation.ObjectAnimator;
 
 import androidx.annotation.IntDef;
@@ -15,7 +14,6 @@ import org.chromium.ui.interpolators.Interpolators;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.util.Collection;
 
 @NullMarked
 public class ToolbarUtils {
@@ -31,41 +29,6 @@ public class ToolbarUtils {
 
     public static boolean isToolbarTabletResizeRefactorEnabled() {
         return ChromeFeatureList.sToolbarTabletResizeRefactor.isEnabled();
-    }
-
-    /**
-     * This interface should be implemented by toolbar components to consume available width from
-     * the toolbar to display themselves.
-     */
-    public interface ToolbarWidthConsumer {
-        /**
-         * Takes in the remaining width available in the toolbar for displaying {@link ToolbarChild}
-         * components. This ToolbarChild will display itself using the available width, if
-         * appropriate, returning the width it has consumed for itself. Returning 0 indicates that
-         * this ToolbarChild is not showing, or cannot be shown.
-         *
-         * @param availableWidth The available width in the toolbar for the button to display
-         *     itself.
-         * @return The width used to display this ToolbarChild.
-         */
-        int updateVisibility(int availableWidth);
-
-        /**
-         * Takes in the remaining width available in the toolbar for displaying {@link ToolbarChild}
-         * components. This ToolbarChild will display itself using the available width, if
-         * appropriate, returning the width it has consumed for itself. Returning 0 indicates that
-         * this ToolbarChild is not showing, or cannot be shown.
-         *
-         * <p>This ToolbarChild will build a new animation for its visibility change, if applicable,
-         * and add it to the supplied list of animators.
-         *
-         * @param availableWidth The available width in the toolbar for the button to display
-         *     itself.
-         * @param animators The collection of {@link Animator}s used to animate a change in the
-         *     toolbar.
-         * @return The width used to display this ToolbarChild.
-         */
-        int updateVisibilityWithAnimation(int availableWidth, Collection<Animator> animators);
     }
 
     public static final @ToolbarComponentId int[] RANKED_TOOLBAR_COMPONENTS =
