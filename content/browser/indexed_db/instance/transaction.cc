401652818b4fb38f1578438615ac4d8e63ff6a10
François Doray <fdoray@chromium.org>
2025-09-23 23:41:07 -0700

    [indexeddb] Fix histogram name in code: IndexedDB.TransactionTimeout.IsConnected
    It previously didn't match the value in histograms.xml.
    
    Bug: 381086791
    Change-Id: I23bdcde56383a086aa32162436f681c48c615da4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6973228
    Commit-Queue: Mingyu Lei <leimy@chromium.org>
    Auto-Submit: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Mingyu Lei <leimy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1519793}

--- content/browser/indexed_db/instance/transaction.cc
+++ content/browser/indexed_db/instance/transaction.cc
@@ -1174,7 +1174,7 @@ void Transaction::TimeoutFired() {
   base::UmaHistogramBoolean("IndexedDB.TransactionTimeout.HasConnection",
                             has_connection);
   if (has_connection) {
-    base::UmaHistogramBoolean("IndexedDB.TransactionTimeout.Connected",
+    base::UmaHistogramBoolean("IndexedDB.TransactionTimeout.IsConnected",
                               connection_->IsConnected());
     base::UmaHistogramCounts10000(
         "IndexedDB.TransactionTimeout.NumTransactionsInConnection",
