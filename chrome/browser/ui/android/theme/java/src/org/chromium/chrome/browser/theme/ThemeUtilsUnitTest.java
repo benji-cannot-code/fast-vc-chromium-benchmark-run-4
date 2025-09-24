bf9b7274be4bdef38fbc15a161314568eb0176b1
Joel Tan-Aristy <jtanaristy@google.com>
2025-09-24 15:28:33 -0700

    [SurfaceColor] Cleanup omnibox surface colors
    Cleans up omnibox coloring from SurfaceColorUpdateUtils.
    
    Bug: 419311173
    Change-Id: Ief3cf3f08cacf69a2e32b2df5df6d6705167f7c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6980628
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Joel Tan-Aristy <jtanaristy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1520128}

--- chrome/browser/ui/android/theme/java/src/org/chromium/chrome/browser/theme/ThemeUtilsUnitTest.java
+++ chrome/browser/ui/android/theme/java/src/org/chromium/chrome/browser/theme/ThemeUtilsUnitTest.java
@@ -11,6 +11,8 @@ import android.content.Context;
 import android.graphics.Color;
 import android.view.ContextThemeWrapper;
 
+import androidx.core.content.ContextCompat;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -90,9 +92,7 @@ public class ThemeUtilsUnitTest {
 
     @Test
     public void getTextBoxColorForToolbarBackgroundInNonNativePage_anyDefault() {
-        int expectedColor =
-                SurfaceColorUpdateUtils.getOmniboxBackgroundColor(
-                        mContext, /* isIncognito= */ false);
+        int expectedColor = ContextCompat.getColor(mContext, R.color.toolbar_text_box_bg_color);
 
         int themeColor =
                 ThemeUtils.getTextBoxColorForToolbarBackgroundInNonNativePage(
