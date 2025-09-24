b00b820b58c2c769f133354a70a17717d720a831
Kent Tamura <tkent@chromium.org>
2025-09-24 00:59:12 -0700

    Spanify SVGPathByteStreamBuilder::CoalescingBuffer
    This CL has no behavior changes.
    
    Bug: 351564777, 356658478
    Change-Id: I283820f3240f4d9432a6d51cba586aa50a0413e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6977381
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1519820}

--- third_party/blink/renderer/core/svg/svg_path_byte_stream.h
+++ third_party/blink/renderer/core/svg/svg_path_byte_stream.h
@@ -29,12 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-template <typename DataType>
-union ByteType {
-  DataType value;
-  unsigned char bytes[sizeof(DataType)];
-};
-
 class SVGPathByteStream {
   USING_FAST_MALLOC(SVGPathByteStream);
 
