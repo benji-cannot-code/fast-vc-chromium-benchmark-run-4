ab9ae7731687ca71654875d9c3ed4d2c56d84e4c
Andy Phan <andyphan@chromium.org>
2025-09-25 12:35:22 -0700

    [PDF Ink Signatures] Separate find selection from text selection
    In PDFiumEngine, separate find selection from text selection. This has
    two benefits:
    1. It fixes unusual selection/invalidation bugs with Ink text
       highlighting. This occurs because Ink text highlighting uses
       PDFiumEngine's text selection.
    2. This behavior matches Blink's. The user is able to find text and
       select text, and both will be highlighted.
    
    Bug: 410777432
    Change-Id: Ic9129fb832246b50c4be6c8e5f2de4dc271597a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6576161
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1520813}

--- pdf/pdfium/findtext_unittest.cc
+++ pdf/pdfium/findtext_unittest.cc
@@ -250,15 +250,13 @@ TEST_P(FindTextDrawSelectionTest, DrawFindResult) {
   engine->StartFind(u"o", /*case_sensitive=*/false);
   EXPECT_TRUE(engine->SelectFindResult(/*forward=*/true));
 
-  // TODO(crbug.com/410777432): Should be empty.
-  EXPECT_EQ("o", engine->GetSelectedText());
+  EXPECT_THAT(engine->GetSelectedText(), testing::IsEmpty());
   DrawSelectionAndCompareWithPlatformExpectations(
       *engine, kPageIndex, "hello_world_draw_find_result_0.png");
 
   EXPECT_TRUE(engine->SelectFindResult(/*forward=*/true));
 
-  // TODO(crbug.com/410777432): Should be empty.
-  EXPECT_EQ("o", engine->GetSelectedText());
+  EXPECT_THAT(engine->GetSelectedText(), testing::IsEmpty());
   DrawSelectionAndCompareWithPlatformExpectations(
       *engine, kPageIndex, "hello_world_draw_find_result_1.png");
 
@@ -266,10 +264,8 @@ TEST_P(FindTextDrawSelectionTest, DrawFindResult) {
                /*end_page_index=*/kPageIndex, /*end_char_index=*/2);
 
   EXPECT_EQ("e", engine->GetSelectedText());
-  // TODO(crbug.com/410777432): Both the find result and the text selection
-  // should be highlighted.
   DrawSelectionAndCompareWithPlatformExpectations(
-      *engine, kPageIndex, "hello_world_selection_1.png");
+      *engine, kPageIndex, "hello_world_draw_find_result_2.png");
 }
 
 #if BUILDFLAG(ENABLE_PDF_INK2)
@@ -291,8 +287,7 @@ TEST_P(FindTextDrawSelectionTest, DrawFindResultInAnnotationMode) {
   engine->StartFind(u"o", /*case_sensitive=*/false);
   EXPECT_TRUE(engine->SelectFindResult(/*forward=*/true));
 
-  // TODO(crbug.com/410777432): Should be empty.
-  EXPECT_EQ("o", engine->GetSelectedText());
+  EXPECT_THAT(engine->GetSelectedText(), testing::IsEmpty());
   DrawSelectionAndCompareWithPlatformExpectations(
       *engine, kPageIndex, "hello_world_draw_find_result_0.png");
 
@@ -301,9 +296,8 @@ TEST_P(FindTextDrawSelectionTest, DrawFindResultInAnnotationMode) {
                /*end_page_index=*/kPageIndex, /*end_char_index=*/2);
 
   EXPECT_EQ("e", engine->GetSelectedText());
-  // TODO(crbug.com/410777432): Only the find result should be highlighted.
   DrawSelectionAndCompareWithPlatformExpectations(
-      *engine, kPageIndex, "hello_world_selection_1.png");
+      *engine, kPageIndex, "hello_world_draw_find_result_0.png");
 }
 #endif  // BUILDFLAG(ENABLE_PDF_INK2)
 
