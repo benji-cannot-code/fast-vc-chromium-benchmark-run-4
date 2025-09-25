b3aebbfba2220baeedeb72b8bc60b25f69abc93e
Colin Blundell <blundell@chromium.org>
2025-09-25 00:26:02 -0700

    [Blink] Remove AcceleratedStaticBitmapImage::ReleaseContext
    This is no longer used.
    
    Bug: 391648152
    Change-Id: Ic6851dc43d8284458a3c7aa91f7ec1007e0456d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6979446
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1520407}

--- third_party/blink/renderer/platform/graphics/accelerated_static_bitmap_image.cc
+++ third_party/blink/renderer/platform/graphics/accelerated_static_bitmap_image.cc
@@ -44,21 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 // static
-void AcceleratedStaticBitmapImage::ReleaseTexture(void* ctx) {
-  auto* release_ctx = static_cast<ReleaseContext*>(ctx);
-  if (release_ctx->context_provider_wrapper) {
-    if (release_ctx->texture_id) {
-      auto* ri = release_ctx->context_provider_wrapper->ContextProvider()
-                     .RasterInterface();
-      ri->EndSharedImageAccessDirectCHROMIUM(release_ctx->texture_id);
-      ri->DeleteGpuRasterTexture(release_ctx->texture_id);
-    }
-  }
-
-  delete release_ctx;
-}
-
-// static
 scoped_refptr<AcceleratedStaticBitmapImage>
 AcceleratedStaticBitmapImage::CreateFromCanvasSharedImage(
     scoped_refptr<gpu::ClientSharedImage> shared_image,
