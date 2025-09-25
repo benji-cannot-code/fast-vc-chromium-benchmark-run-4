5b15a2528f678900ced93f7d4c1eb34adfaf9483
Qikai Zhong <qikaizhong@microsoft.com>
2025-09-24 23:25:32 -0700

    [bedrock] Migrate ForEachCurrentBrowser and ForEachCurrentAndNewBrowser to BrowserWindowInterface APIs
    This CL migrates all usages of BrowserList::ForEachCurrentBrowser() and
    BrowserList::ForEachCurrentAndNewBrowser() to the corresponding
    BrowserWindowInterface iterator APIs.
    
    API migrations:
    - ForEachCurrentBrowser() → ForEachCurrentBrowserWindowInterfaceOrderedByActivation()
    - ForEachCurrentAndNewBrowser() → ForEachCurrentAndNewBrowserWindowInterfaceOrderedByActivation()
    
    Will remove ForEachCurrentBrowser() and ForEachCurrentAndNewBrowser() after 2 weeks
    
    Files changed:
    - chrome/browser/ui/browser_tab_strip_tracker.h
    - chrome/browser/ui/browser_tab_strip_tracker.cc
    - chrome/browser/lifetime/browser_close_manager.cc
    - chrome/browser/resource_coordinator/tab_lifecycle_unit_source.cc
    
    This is part of the ongoing effort to decouple clients from direct
    Browser dependencies and enable them to access Browser-scoped features
    through BrowserWindowInterface handles instead.
    
    Fixed a use-after-free caused by migrating from ForEachCurrentBrowser to ForEachCurrentBrowserWindowInterfaceOrderedByActivation.
    
    The migration maintains identical iteration behavior:
    - Same enumeration order (browsers_ordered_by_activation)
    - Same handling of browsers added during iteration (kEnumerateNewBrowser flag)
    - Callbacks updated to return bool and access Browser via GetBrowserForMigrationOnly()
    
    Bug: 431671320
    Change-Id: Iadc8d89e50e0cacc63d7167ac5fd35a3506b8b0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6952651
    Commit-Queue: Qikai Zhong <qikaizhong@microsoft.com>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1520383}

--- chrome/browser/ui/browser_tab_strip_tracker.cc
+++ chrome/browser/ui/browser_tab_strip_tracker.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_tab_strip_tracker_delegate.h"
+#include "chrome/browser/ui/browser_window/public/browser_window_interface_iterator.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 
 BrowserTabStripTracker::BrowserTabStripTracker(
@@ -22,28 +23,35 @@ BrowserTabStripTracker::BrowserTabStripTracker(
 
 BrowserTabStripTracker::~BrowserTabStripTracker() {
   BrowserList::RemoveObserver(this);
-  BrowserList::GetInstance()->ForEachCurrentBrowser([this](Browser* browser) {
-    browser->tab_strip_model()->RemoveObserver(tab_strip_model_observer_);
-  });
+  ForEachCurrentBrowserWindowInterfaceOrderedByActivation(
+      [this](BrowserWindowInterface* browser) {
+        browser->GetTabStripModel()->RemoveObserver(tab_strip_model_observer_);
+        return true;
+      });
 }
 
 void BrowserTabStripTracker::Init() {
   base::AutoReset<bool> resetter(&is_processing_initial_browsers_, true);
-  BrowserList::GetInstance()->ForEachCurrentAndNewBrowser(
-      [this](Browser* browser) { MaybeTrackBrowser(browser); });
+  ForEachCurrentAndNewBrowserWindowInterfaceOrderedByActivation(
+      [this](BrowserWindowInterface* browser) {
+        MaybeTrackBrowser(browser);
+        return true;
+      });
   BrowserList::AddObserver(this);
 }
 
-bool BrowserTabStripTracker::ShouldTrackBrowser(Browser* browser) {
+bool BrowserTabStripTracker::ShouldTrackBrowser(
+    BrowserWindowInterface* browser) {
   return !delegate_ || delegate_->ShouldTrackBrowser(browser);
 }
 
-void BrowserTabStripTracker::MaybeTrackBrowser(Browser* browser) {
+void BrowserTabStripTracker::MaybeTrackBrowser(
+    BrowserWindowInterface* browser) {
   if (!ShouldTrackBrowser(browser)) {
     return;
   }
 
-  TabStripModel* tab_strip_model = browser->tab_strip_model();
+  TabStripModel* const tab_strip_model = browser->GetTabStripModel();
   tab_strip_model->AddObserver(tab_strip_model_observer_);
 
   TabStripModelChange::Insert insert;
@@ -66,5 +74,5 @@ void BrowserTabStripTracker::OnBrowserAdded(Browser* browser) {
 void BrowserTabStripTracker::OnBrowserRemoved(Browser* browser) {
   // Per ObserverList::RemoveObserver() documentation, this does nothing if the
   // observer is not in the ObserverList (i.e. if |browser| is not tracked).
-  browser->tab_strip_model()->RemoveObserver(tab_strip_model_observer_);
+  browser->GetTabStripModel()->RemoveObserver(tab_strip_model_observer_);
 }
--- chrome/browser/ui/browser_tab_strip_tracker.h
+++ chrome/browser/ui/browser_tab_strip_tracker.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_list_observer.h"
 
 class BrowserTabStripTrackerDelegate;
+class BrowserWindowInterface;
 class TabStripModelObserver;
 
 // BrowserTabStripTracker attaches a TabStripModelObserver to a subset of
@@ -53,11 +54,11 @@ class BrowserTabStripTracker : public BrowserListObserver {
 
  private:
   // Returns true if a TabStripModelObserver should be added to |browser|.
-  bool ShouldTrackBrowser(Browser* browser);
+  bool ShouldTrackBrowser(BrowserWindowInterface* browser);
 
   // If ShouldTrackBrowser() returns true for |browser| then a
   // TabStripModelObserver is attached.
-  void MaybeTrackBrowser(Browser* browser);
+  void MaybeTrackBrowser(BrowserWindowInterface* browser);
 
   // BrowserListObserver:
   void OnBrowserAdded(Browser* browser) override;
