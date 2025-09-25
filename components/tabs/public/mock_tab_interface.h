c7a83bb6cd9ff65e029aaf57e63abb27bc668538
Sky Malice <skym@chromium.org>
2025-09-25 08:32:52 -0700

    [Persist] Adjust MockTabInterface to match TabInterface's selective compilation flags.
    Bug: 427254267
    Change-Id: I9ba4a7bb35f3c43a0bc45c13ecfc1fd05cbe36ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6981667
    Reviewed-by: Darryl James <dljames@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1520626}

--- components/tabs/public/mock_tab_interface.h
+++ components/tabs/public/mock_tab_interface.h
@@ -68,6 +68,7 @@ class MockTabInterface : public testing::NiceMock<TabInterface> {
               (TabInterfaceCallback),
               (override));
   MOCK_METHOD(bool, IsInNormalWindow, (), (const override));
+#if !BUILDFLAG(IS_ANDROID)
   MOCK_METHOD(BrowserWindowInterface*,
               GetBrowserWindowInterface,
               (),
@@ -76,6 +77,7 @@ class MockTabInterface : public testing::NiceMock<TabInterface> {
               GetBrowserWindowInterface,
               (),
               (const override));
+#endif  // !BUILDFLAG(IS_ANDROID)
   MOCK_METHOD(TabFeatures*, GetTabFeatures, (), (override));
   MOCK_METHOD(const TabFeatures*, GetTabFeatures, (), (const override));
   MOCK_METHOD(bool, IsPinned, (), (const override));
