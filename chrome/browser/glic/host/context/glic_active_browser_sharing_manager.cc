ef5981a47f20dd91f406f4530ae126d4d3a32a09
Marke Hallowell <wry@chromium.org>
2025-09-25 12:13:28 -0700

    Wire up active browser sharing manager for GKS in multi-instance mode.
    Turns on browserlist observing in the sharing manager and refactors the
    GlicKeyedService to use sharing manager interface. Instantiates and
    hands out a cross-instance active browser sharing manager on GKS.
    
    Bug: 444463509
    Change-Id: Ia69eb4e07c4ccead84258746feaea7b4becfaaff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6981752
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Marke Hallowell <wry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1520798}

--- chrome/browser/glic/host/context/glic_active_browser_sharing_manager.cc
+++ chrome/browser/glic/host/context/glic_active_browser_sharing_manager.cc
@@ -9,13 +9,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/glic/public/glic_keyed_service.h"
 #include "chrome/browser/glic/public/glic_keyed_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/browser_list.h"
 
 namespace glic {
 
 GlicActiveBrowserSharingManager::GlicActiveBrowserSharingManager(
     Profile* profile)
-    : profile_(profile) {}
-GlicActiveBrowserSharingManager::~GlicActiveBrowserSharingManager() = default;
+    : profile_(profile) {
+  BrowserList::AddObserver(this);
+}
+
+GlicActiveBrowserSharingManager::~GlicActiveBrowserSharingManager() {
+  BrowserList::RemoveObserver(this);
+}
 
 void GlicActiveBrowserSharingManager::OnBrowserSetLastActive(Browser* browser) {
   if (browser->profile() != profile_) {
--- chrome/browser/glic/public/glic_keyed_service.cc
+++ chrome/browser/glic/public/glic_keyed_service.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/glic/glic_pref_names.h"
 #include "chrome/browser/glic/glic_profile_manager.h"
 #include "chrome/browser/glic/host/auth_controller.h"
+#include "chrome/browser/glic/host/context/glic_active_browser_sharing_manager.h"
 #include "chrome/browser/glic/host/context/glic_page_context_fetcher.h"
 #include "chrome/browser/glic/host/context/glic_screenshot_capturer.h"
 #include "chrome/browser/glic/host/context/glic_sharing_manager_impl.h"
@@ -108,6 +109,18 @@ std::unique_ptr<GlicWindowController> CreateWindowController(
       profile, identity_manager, glic_service, glic_enabling);
 }
 
+std::unique_ptr<GlicSharingManager> CreateSharingManager(
+    Profile* profile,
+    GlicWindowController* window_controller,
+    GlicMetrics* metrics) {
+  if (UseDefaultWindowController()) {
+    return std::make_unique<GlicSharingManagerImpl>(profile, window_controller,
+                                                    metrics);
+  }
+
+  return std::make_unique<GlicActiveBrowserSharingManager>(profile);
+}
+
 }  // namespace
 
 GlicKeyedService::GlicKeyedService(
@@ -129,9 +142,7 @@ GlicKeyedService::GlicKeyedService(
                                                 this,
                                                 enabling_.get())),
       sharing_manager_(
-          std::make_unique<GlicSharingManagerImpl>(profile,
-                                                   &window_controller(),
-                                                   metrics_.get())),
+          CreateSharingManager(profile, &window_controller(), metrics_.get())),
       screenshot_capturer_(std::make_unique<GlicScreenshotCapturer>()),
       auth_controller_(std::make_unique<AuthController>(profile,
                                                         identity_manager,
