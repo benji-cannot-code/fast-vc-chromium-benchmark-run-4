21899c341b1ae8c70a4912e696edb6756208bb0c
Arthur Sonzogni <arthursonzogni@chromium.org>
2025-09-25 03:02:15 -0700

    Enable field trial testing config in fuzzers.
    When rolling out a new feature using field trials, Chrome engineers have
    to edit the file testing/variations/fieldtrial_testing_config.json so
    that their feature is somewhat properly tested before reaching
    Dev/Beta/Stable users.
    
    Currently, the field trial testing config is properly wired into
    content_shell based tests like web tests, content_browsertest,
    components_browsertests, etc...
    
    However, coverage is missing:
    - Most libfuzzer fuzzers: bug 406567072
    - Most unittests: bug 40105939
    
    This patch call the previously extracted function to do the
    proper setup.
    
    Bug: 40105939
    Bug: 406567072
    Fixed: 406567072
    Change-Id: I01f289818f46569f2060ae27337cfa7a3649bfe4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6961055
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1520473}

--- third_party/blink/renderer/platform/testing/blink_fuzzer_test_support.cc
+++ third_party/blink/renderer/platform/testing/blink_fuzzer_test_support.cc
@@ -9,8 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/icu_util.h"
 #include "base/test/test_timeouts.h"
 #include "content/public/test/blink_test_environment.h"
+#include "content/public/test/setup_field_trials.h"
 #include "third_party/blink/renderer/platform/heap/thread_state.h"
-
 namespace blink {
 
 BlinkFuzzerTestSupport::BlinkFuzzerTestSupport()
@@ -25,6 +25,12 @@ BlinkFuzzerTestSupport::BlinkFuzzerTestSupport(int argc, char** argv) {
   base::CommandLine::Init(argc, argv);
 
   TestTimeouts::Initialize();
+
+  // In unbranded builds, enable the features listed in
+  // testing/variations/fieldtrial_testing_config.json
+  // This allows fuzzing of features shipping to Beta/Dev/Stable users via a
+  // field trial experiment.
+  content::SetupFieldTrials();
 
   test_environment_ = std::make_unique<content::BlinkTestEnvironment>();
   test_environment_->SetUp();
