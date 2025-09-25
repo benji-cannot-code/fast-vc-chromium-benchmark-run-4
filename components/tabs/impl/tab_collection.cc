5aa29d11334932f4aa23854093724727264d5f5b
Sky Malice <skym@chromium.org>
2025-09-25 09:25:51 -0700

    [Persist] Create DirectChildWalker for tab collections.
    Bug: 427254267
    Change-Id: I5508097b13e5219b99f361e1ed7691d46994c347
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6982069
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1520673}

--- components/tabs/impl/tab_collection.cc
+++ components/tabs/impl/tab_collection.cc
@@ -283,4 +283,9 @@ void TabCollection::OnReparented(TabCollection* new_parent) {
   }
 }
 
+const ChildrenVector& TabCollection::GetChildren(
+    base::PassKey<DirectChildWalker> pass_key) const {
+  return GetChildren();
+}
+
 }  // namespace tabs
