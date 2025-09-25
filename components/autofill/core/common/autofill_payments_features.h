9bdb95a245b7711eecd916de00895be1dc924ba6
Vinny Persky <vinnypersky@google.com>
2025-09-25 16:14:19 -0700

    Add finch flag for second line string experiment in BNPL suggestion
    Bug: 447431582
    Change-Id: I52ccb8d8670a611169ddddd24b59b74db9fb2b77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6986064
    Commit-Queue: Vinny Persky <vinnypersky@google.com>
    Reviewed-by: Olivia Saul <osaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1520945}

--- components/autofill/core/common/autofill_payments_features.h
+++ components/autofill/core/common/autofill_payments_features.h
@@ -35,6 +35,9 @@ BASE_DECLARE_FEATURE(kAutofillEnableBuyNowPayLaterForKlarna);
 COMPONENT_EXPORT(AUTOFILL)
 BASE_DECLARE_FEATURE(kAutofillEnableBuyNowPayLaterSyncing);
 COMPONENT_EXPORT(AUTOFILL)
+BASE_DECLARE_FEATURE(
+    kAutofillEnableBuyNowPayLaterUpdatedSuggestionSecondLineString);
+COMPONENT_EXPORT(AUTOFILL)
 BASE_DECLARE_FEATURE(kAutofillEnableCardBenefitsForAmericanExpress);
 COMPONENT_EXPORT(AUTOFILL)
 BASE_DECLARE_FEATURE(kAutofillEnableCardBenefitsForBmo);
