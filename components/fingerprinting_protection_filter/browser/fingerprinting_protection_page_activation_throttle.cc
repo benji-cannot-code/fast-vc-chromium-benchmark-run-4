324286a16c72ae350c20309366f60b29c85a55d5
Adam Abeshouse <aabeshouse@google.com>
2025-09-24 19:21:37 -0700

    Call ThrottleManager directly from activation throttle
    Until now, page activation was computed as follows:
    
    (1) ThrottleManager adds an activation throttle. (2) The activation
    throttle computes the page activation in WillProcessResponse and passes
    it to a callback on the web contents helper. (3) The web contents helper
    passes it to a callback on ThrottleManager.
    
    This CL cuts out the unnecessary (2) step, so now it is just:
    
    (1) ThrottleManager adds an activation throttle. (2) The activation
    throttle computes the page activation in WillProcessResponse and passes
    it to a callback on ThrottleManager.
    
    Despite that the activation throttle doesn't actually throttle in
    WillProcessResponse, it needs to be implemented as a NavigationThrottle
    because the WillProcessResponse hook allows us to compute activation
    only for navigation requests that successfully received a response.
    
    Fixed: 346777548
    Change-Id: Ib0c3b801459951e1e4300453fbaf2087d26a516b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6975520
    Commit-Queue: Adam Abeshouse <aabeshouse@google.com>
    Reviewed-by: Martin Pan-Verde <thesalsa@google.com>
    Cr-Commit-Position: refs/heads/main@{#1520176}

--- components/fingerprinting_protection_filter/browser/fingerprinting_protection_page_activation_throttle.cc
+++ components/fingerprinting_protection_filter/browser/fingerprinting_protection_page_activation_throttle.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/common/content_settings.h"
 #include "components/content_settings/core/common/pref_names.h"
 #include "components/fingerprinting_protection_filter/browser/fingerprinting_protection_web_contents_helper.h"
+#include "components/fingerprinting_protection_filter/browser/throttle_manager.h"
 #include "components/fingerprinting_protection_filter/common/fingerprinting_protection_breakage_exception.h"
 #include "components/fingerprinting_protection_filter/common/fingerprinting_protection_filter_constants.h"
 #include "components/fingerprinting_protection_filter/common/fingerprinting_protection_filter_features.h"
@@ -241,13 +242,13 @@ FingerprintingProtectionPageActivationThrottle::GetActivation() const {
 void FingerprintingProtectionPageActivationThrottle::
     NotifyPageActivationComputed(ActivationState activation_state,
                                  ActivationDecision activation_decision) {
-  auto* web_contents_helper =
-      FingerprintingProtectionWebContentsHelper::FromWebContents(
-          navigation_handle()->GetWebContents());
-  // Making sure the WebContentsHelper exists is outside the scope of this
+  ThrottleManager* throttle_manager =
+      FingerprintingProtectionWebContentsHelper::GetThrottleManager(
+          *navigation_handle());
+  // Making sure the ThrottleManager exists is outside the scope of this
   // class.
-  if (web_contents_helper) {
-    web_contents_helper->NotifyPageActivationComputed(
+  if (throttle_manager) {
+    throttle_manager->OnPageActivationComputed(
         navigation_handle(), activation_state, activation_decision);
   }
 }
