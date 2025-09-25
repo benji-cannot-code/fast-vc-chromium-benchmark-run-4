c4e292ae0fd8314c0926b21067b90705ec30253e
Ibrahim Kanouche <kanouche@google.com>
2025-09-25 06:38:54 -0700

    [iOS] Add PageMenuActionFeature model for the AI hub Page Tools.
    Introduces data model for representing features that can appear as
    rows in the Page Action Menu (Translate, Pop-ups, etc). Includes enums
    for feature types and action types. Foundation for implementing dynamic
    feature rows in the proactive suggestions framework.
    
    Bug: 447129008
    Change-Id: I5d15ee822bc4297581a564ada7eadd5cff07ddbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6973229
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Auto-Submit: Ibrahim Kanouche <kanouche@google.com>
    Commit-Queue: Ibrahim Kanouche <kanouche@google.com>
    Cr-Commit-Position: refs/heads/main@{#1520547}

---
+++ ios/chrome/browser/intelligence/page_action_menu/ui/page_action_menu_feature.mm
@@ -0,0 +1,43 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/intelligence/page_action_menu/ui/page_action_menu_feature.h"
+
+#import "base/check.h"
+
+@implementation PageActionMenuFeature
+
+- (instancetype)initWithFeatureType:(PageActionMenuFeatureType)featureType
+                              title:(NSString*)title
+                               icon:(UIImage*)icon
+                         actionType:
+                             (PageActionMenuFeatureActionType)actionType {
+  DCHECK(title);
+  DCHECK(icon);
+
+  self = [super init];
+  if (self) {
+    _featureType = featureType;
+    _title = [title copy];
+    _icon = icon;
+    _actionType = actionType;
+    _toggleState = NO;
+  }
+  return self;
+}
+
+- (void)setSubtitle:(NSString*)subtitle {
+  _subtitle = [subtitle copy];
+}
+
+- (void)setActionText:(NSString*)actionText {
+  _actionText = [actionText copy];
+}
+
+- (void)setAccessibilityLabel:(NSString*)accessibilityLabel {
+  _accessibilityLabel = [accessibilityLabel copy];
+}
+
+@end
