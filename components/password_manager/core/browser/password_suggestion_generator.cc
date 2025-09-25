c137201c51cf5cf347355154fa5a51c509cc9d62
Anna Tsvirchkova <atsvirchkova@google.com>
2025-09-25 06:03:54 -0700

    [FillRecoveryPwd] Trouble signing in suggestion must not be displayed on Android
    Currently "Trouble signing in" suggestion is displayed in Android
    keyboard accessory, but it should not be, according to the design.
    
    Bug: 417943331
    Change-Id: Id3ef299b2f9dc61a5b1c00e9cb0c5ac8cb510291
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6983163
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1520531}

--- components/password_manager/core/browser/password_suggestion_generator.cc
+++ components/password_manager/core/browser/password_suggestion_generator.cc
@@ -211,6 +211,7 @@ void AppendSuggestionIfMatching(
   }
 }
 
+#if !BUILDFLAG(IS_ANDROID)
 void AppendTroubleSigningInSuggestion(
     const autofill::PasswordAndMetadata& credential,
     std::vector<Suggestion>* suggestions) {
@@ -251,6 +252,7 @@ void MaybeAppendTroubleSigningInSuggestion(
     }
   }
 }
+#endif  // BUILDFLAG(IS_ANDROID)
 
 // This function attempts to fill |suggestions| from |fill_data| based on
 // |current_username| that is the current value of the field.
@@ -275,8 +277,10 @@ void GetSuggestions(
             [](const Suggestion& a, const Suggestion& b) {
               return a.main_text.value < b.main_text.value;
             });
+#if !BUILDFLAG(IS_ANDROID)
   MaybeAppendTroubleSigningInSuggestion(undo_password_change_controller,
                                         fill_data, suggestions);
+#endif  // !BUILDFLAG(IS_ANDROID)
 }
 
 Suggestion CreateFillPasswordChildSuggestion(
