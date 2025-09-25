6da2e7bf60b8df460cb6a181ae3cfa23a2653eb4
Xiyuan Xia <xiyuan@chromium.org>
2025-09-25 09:14:13 -0700

    cros: Fix `ScanStackForNextFrame` crash
    Add a heuristic fix to `ScanStackForNextFrame` and put it behind
    a `kStackScanMaxFramePointerToStackEndGap` feature. The feature is
    enabled by default on ChromeOS to fix the stack scanning crashes.
    
    Enabled-by-default-reason: fix ChromeOS crashes and also a killswitch
    
    Bug: 402542102
    Change-Id: I8603d94b28986468147014047912f8bd60525dec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6950964
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1520666}

--- base/features.cc
+++ base/features.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <atomic>
 
+#include "base/debug/stack_trace.h"
 #include "base/files/file_path.h"
 #include "base/task/sequence_manager/sequence_manager_impl.h"
 #include "base/threading/platform_thread.h"
@@ -85,6 +86,27 @@ BASE_FEATURE_PARAM(int,
                    LOW_MEMORY_DEVICE_THRESHOLD_MB);
 
 BASE_FEATURE(kReducePPMs, FEATURE_DISABLED_BY_DEFAULT);
+
+// Whether to restrict the max gap between the frame pointer and the stack end
+// for stack scanning. If the gap is beyond the given gap threshold, the stack
+// end is treated as unreliable. Stack scanning stops when that happens.
+// This feature is only in effect when BUILDFLAG(CAN_UNWIND_WITH_FRAME_POINTERS)
+// is on and `TraceStackFramePointers` would run stack scanning. Default gap
+// threshold is an absurdly large 100MB.
+// The feature is enabled by default on ChromeOS where crashes caused by
+// unreliable stack end are found. See https://crbug.com/402542102
+BASE_FEATURE(kStackScanMaxFramePointerToStackEndGap,
+#if BUILDFLAG(IS_CHROMEOS)
+             FEATURE_ENABLED_BY_DEFAULT
+#else
+             FEATURE_DISABLED_BY_DEFAULT
+#endif
+);
+BASE_FEATURE_PARAM(int,
+                   kStackScanMaxFramePointerToStackEndGapThresholdMB,
+                   &kStackScanMaxFramePointerToStackEndGap,
+                   "StackScanMaxFramePointerToStackEndGapThresholdMB",
+                   100);
 
 #if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_CHROMEOS)
 // Force to enable LowEndDeviceMode partially on Android 3Gb devices.
@@ -171,6 +193,7 @@ void Init(EmitThreadControllerProfilerMetadata
   sequence_manager::internal::ThreadController::InitializeFeatures(
       emit_thread_controller_profiler_metadata);
 
+  debug::StackTrace::InitializeFeatures();
   FilePath::InitializeFeatures();
 
 #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_ANDROID)
