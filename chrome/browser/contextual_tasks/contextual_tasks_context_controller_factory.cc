d3346053d0b802b7988f8ec30a42ab0dd9329ed5
Tommy Nyquist <nyquist@chromium.org>
2025-09-25 11:58:08 -0700

    [contextual_tasks] Do not create KeyedServices if feature flag is disabled.
    This CL changes the functionality of the KeyedService factories for
    ContextualTasksContextController and ContextualTasksService to not create
    the services if the feature flag is disabled.
    
    Tests are updated to test this new behavior.
    
    Bug: b/445841780
    Change-Id: Icbcdb331d7599c59964bb2a5635e9f24d123574e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6985245
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1520788}

--- chrome/browser/contextual_tasks/contextual_tasks_context_controller_factory.cc
+++ chrome/browser/contextual_tasks/contextual_tasks_context_controller_factory.cc
@@ -5,10 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/contextual_tasks/contextual_tasks_context_controller_factory.h"
 
+#include "base/feature_list.h"
 #include "chrome/browser/contextual_tasks/contextual_tasks_context_controller_impl.h"
 #include "chrome/browser/contextual_tasks/contextual_tasks_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "components/contextual_tasks/public/contextual_tasks_service.h"
+#include "components/contextual_tasks/public/features.h"
 
 namespace contextual_tasks {
 
@@ -43,6 +45,10 @@ ContextualTasksContextControllerFactory::
 std::unique_ptr<KeyedService>
 ContextualTasksContextControllerFactory::BuildServiceInstanceForBrowserContext(
     content::BrowserContext* context) const {
+  if (!base::FeatureList::IsEnabled(kContextualTasks)) {
+    return nullptr;
+  }
+
   Profile* profile = Profile::FromBrowserContext(context);
   contextual_tasks::ContextualTasksService* contextual_tasks_service =
       ContextualTasksServiceFactory::GetForProfile(profile);
--- chrome/browser/contextual_tasks/contextual_tasks_context_controller_factory_unittest.cc
+++ chrome/browser/contextual_tasks/contextual_tasks_context_controller_factory_unittest.cc
@@ -7,8 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/test/scoped_feature_list.h"
 #include "chrome/browser/contextual_tasks/contextual_tasks_context_controller.h"
 #include "chrome/test/base/testing_profile.h"
+#include "components/contextual_tasks/public/features.h"
 #include "content/public/test/browser_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -19,10 +21,13 @@ class ContextualTasksContextControllerFactoryTest : public testing::Test {
   ContextualTasksContextControllerFactoryTest() = default;
   ~ContextualTasksContextControllerFactoryTest() override = default;
 
+ protected:
   content::BrowserTaskEnvironment task_environment_;
+  base::test::ScopedFeatureList feature_list_;
 };
 
 TEST_F(ContextualTasksContextControllerFactoryTest, CreatesServiceForProfile) {
+  feature_list_.InitAndEnableFeature(kContextualTasks);
   std::unique_ptr<TestingProfile> profile = TestingProfile::Builder().Build();
   ContextualTasksContextController* controller =
       ContextualTasksContextControllerFactory::GetForProfile(profile.get());
@@ -30,7 +35,17 @@ TEST_F(ContextualTasksContextControllerFactoryTest, CreatesServiceForProfile) {
 }
 
 TEST_F(ContextualTasksContextControllerFactoryTest,
+       DoesNotCreateServiceIfFeatureDisabled) {
+  feature_list_.InitAndDisableFeature(kContextualTasks);
+  std::unique_ptr<TestingProfile> profile = TestingProfile::Builder().Build();
+  ContextualTasksContextController* controller =
+      ContextualTasksContextControllerFactory::GetForProfile(profile.get());
+  EXPECT_EQ(nullptr, controller);
+}
+
+TEST_F(ContextualTasksContextControllerFactoryTest,
        DoesNotCreateServiceForIncognito) {
+  feature_list_.InitAndEnableFeature(kContextualTasks);
   std::unique_ptr<TestingProfile> profile = TestingProfile::Builder().Build();
   Profile* otr_profile = profile->GetOffTheRecordProfile(
       Profile::OTRProfileID::PrimaryID(), /*create_if_needed=*/true);
