095b262f3b58acf4595d311bff407b9f1c850006
Tsuyoshi Horo <horo@chromium.org>
2025-09-25 01:17:06 -0700

    Enable DeviceBoundSessionAccessObserverSharedRemote
    Experiments have shown that this feature improves performance as
    expected. See https://crbug.com/407680127#comment6
    
    Enabled-by-default-reason: Observed performance improvement
    Bug: 407680127
    Change-Id: Ie10646937b821d9ccad7405b11e91ffa26f40509
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6982549
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1520421}

--- services/network/public/cpp/features.cc
+++ services/network/public/cpp/features.cc
@@ -546,7 +546,7 @@ BASE_FEATURE(kStorageAccessHeadersRespectPermissionsPolicy,
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kDeviceBoundSessionAccessObserverSharedRemote,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kCSPScriptSrcV2, "ScriptSrcV2", base::FEATURE_DISABLED_BY_DEFAULT);
 
