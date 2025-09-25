bbd51c5bd0f7102b78de757728e084fcebbbdb9b
Stefan Zager <szager@chromium.org>
2025-09-24 22:18:10 -0700

    Dial in criteria for speculatively decoding images
    In manual experimentation on a Pixel 9, images with an intrinsic size
    above the 10,000 pixel threshold generally take at least 1ms to
    decode.
    
    Bug: chromium:378623805
    Change-Id: I14889b8fe2d55e47a23bb5313590415b05a897be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6974945
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1520286}

--- third_party/blink/renderer/core/loader/resource/image_resource.h
+++ third_party/blink/renderer/core/loader/resource/image_resource.h
@@ -71,9 +71,9 @@ class CORE_EXPORT ImageResource final
                                const DOMWrapperWorld* world);
   static ImageResource* CreateForTest(const KURL&);
 
-  // This will exclude ~65% of images on the web, based on:
-  //   https://almanac.httparchive.org/en/2024/media#image-dimensions
-  static constexpr int kSpeculativeDecodeMinImageSize = 100000;
+  // This restricts speculative decoding to images that are relatively expensive
+  // to decode.
+  static constexpr int kSpeculativeDecodeMinImageSize = 10000;
 
   ImageResource(const ResourceRequest&,
                 const ResourceLoaderOptions&,
@@ -107,6 +107,7 @@ class CORE_EXPORT ImageResource final
   void UpdateResourceInfoFromObservers() override;
   std::pair<ResourcePriority, ResourcePriority> PriorityFromObservers()
       const override;
+  bool HasNonDegenerateContentSize() const override;
   bool IsAboveSpeculativeDecodeSizeThreshold() const override;
 
   // MultipartImageResourceParser::Client
