4c64de3de8f36ade82bb30165c823387eaf260a4
James Cook <jamescook@chromium.org>
2025-09-24 23:05:14 -0700

    extensions: Port cookies API unit tests to desktop Android
    These were somehow missed when the API was ported. They pass without
    modification.
    
    Bug: 441370154
    Change-Id: Id6d7629e3ca617055ad7a562c55755f505bd5003
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6980732
    Reviewed-by: Zhengzheng Liu <zhzhliu@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1520352}

--- chrome/browser/extensions/api/cookies/cookies_helpers_unittest.cc
+++ chrome/browser/extensions/api/cookies/cookies_helpers_unittest.cc
@@ -8,8 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <limits>
 #include <memory>
 
+#include "extensions/buildflags/buildflags.h"
 #include "net/cookies/canonical_cookie.h"
 #include "testing/gtest/include/gtest/gtest.h"
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace extensions {
 
