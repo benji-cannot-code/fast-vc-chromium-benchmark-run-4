fe4dc8e63de3104f4bfb6e43ba4cd69a95866a6f
Sky Malice <skym@chromium.org>
2025-09-25 10:23:29 -0700

    [Persist] Maintain an in memory map of handles to storage ids.
    Bug: 427254267
    Change-Id: I1882c7b84e747531e046509461f67cd6c0c400e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6965952
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1520708}

--- chrome/browser/tab/tab_state_storage_service.h
+++ chrome/browser/tab/tab_state_storage_service.h
@@ -19,6 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/keyed_service/core/keyed_service.h"
 
 namespace tabs {
+class TabCollection;
+}  // namespace tabs
+
+namespace tabs {
 
 class TabStateStorageService : public KeyedService,
                                public base::SupportsUserData {
@@ -45,6 +49,8 @@ class TabStateStorageService : public KeyedService,
                bool tab_has_sensitive_content,
                bool is_pinned);
 
+  void Save(const tabs::TabCollection* tab_collection);
+
   void LoadAllTabs(LoadAllTabsCallback callback);
 
   // Returns a Java object of the type TabStateStorageService. This is
@@ -53,9 +59,17 @@ class TabStateStorageService : public KeyedService,
       TabStateStorageService* tab_state_storage_service);
 
  private:
+  class SaveProcessor;
+
+  template <typename T>
+  int GetOrCreateStorageId(T* object, std::map<int32_t, int>& handle_map);
+
   void OnAllTabsLoaded(LoadAllTabsCallback callback,
                        std::vector<NodeState> entries);
   std::unique_ptr<TabStateStorageBackend> tab_backend_;
+  int next_storage_id_ = 1;
+  std::map<int32_t, int> tab_handle_to_storage_id_;
+  std::map<int32_t, int> collection_handle_to_storage_id_;
 };
 
 }  // namespace tabs
