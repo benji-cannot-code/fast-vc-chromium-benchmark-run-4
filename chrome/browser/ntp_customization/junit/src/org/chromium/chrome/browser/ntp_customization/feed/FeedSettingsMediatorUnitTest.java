ad89781a8fe1b4d8e1bbe4c5a2919636299c46ab
Xinyi Ji <xinyiji@google.com>
2025-09-24 21:17:37 -0700

    [NTP Customization] Update the accessibility string of the feed switch button in the feed settings bottom sheet
    This CL updates the accessibility string of the feed switch button in the feed settings bottom sheet. After this CL, the readout should be: On, Turns on the discover feed, get suggested articles on the New Tab Page, $[selected/unselected] state so it first reads the title, the function, then the description, before the switch's state.
    
    Bug: 445557167
    Skip-Translation-Screenshots-Check: True
    Change-Id: I2dab8f374425a8368b54f82b8bfd2a09865a5806
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6975555
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Xinyi Ji <xinyiji@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1520186}

--- chrome/browser/ntp_customization/junit/src/org/chromium/chrome/browser/ntp_customization/feed/FeedSettingsMediatorUnitTest.java
+++ chrome/browser/ntp_customization/junit/src/org/chromium/chrome/browser/ntp_customization/feed/FeedSettingsMediatorUnitTest.java
@@ -97,6 +97,7 @@ public class FeedSettingsMediatorUnitTest {
         FeedSettingsMediator.setPrefForTesting(mPrefChangeRegistrar, mPrefService);
         mFeedSettingsMediator =
                 new FeedSettingsMediator(
+                        mContext,
                         mContainerPropertyModel,
                         mBottomSheetPropertyModel,
                         mFeedSettingsPropertyModel,
@@ -125,6 +126,7 @@ public class FeedSettingsMediatorUnitTest {
         // handler should be set to null.
         when(mDelegate.shouldShowAlone()).thenReturn(true);
         new FeedSettingsMediator(
+                mContext,
                 mContainerPropertyModel,
                 mBottomSheetPropertyModel,
                 mFeedSettingsPropertyModel,
@@ -138,6 +140,7 @@ public class FeedSettingsMediatorUnitTest {
         clearInvocations(mBottomSheetPropertyModel);
         when(mDelegate.shouldShowAlone()).thenReturn(false);
         new FeedSettingsMediator(
+                mContext,
                 mContainerPropertyModel,
                 mBottomSheetPropertyModel,
                 mFeedSettingsPropertyModel,
