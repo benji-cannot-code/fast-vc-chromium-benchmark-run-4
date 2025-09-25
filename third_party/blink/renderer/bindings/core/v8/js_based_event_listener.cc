dd3e5d6afc41e8da62aeed0ac6e1dc0eb1dea550
Yeonghan Kim <soosungp33@gmail.com>
2025-09-24 23:21:51 -0700

    Make invocationTargetInShadowTree determined before listener invocation
    According to spec[1], invocationTargetInShadowTree should be determined
    before calling the listener and then used during the invoke step[2].
    Chrome does not comply with the spec. Instead, it uses
    IsInShadowTree() to check whether the event is inside a shadow tree,
    and this check is performed during the invoke step.
    But this does not behave correctly when a node moves during the
    listener call, as the shadow tree check is performed based on the moved
    node.
    So, this patch fixes the bug by saving invocationTargetInShadowTree
    before calling the listener and using it.
    
    [1] https://dom.spec.whatwg.org/#concept-event-path-append
    [2] https://dom.spec.whatwg.org/#concept-event-listener-inner-invoke
    
    Bug: 41408360
    Change-Id: I4893440e76adb45949fa998edc5e8ff975d811f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6966287
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: YeongHan Kim <soosungp33@gmail.com>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1520380}

--- third_party/blink/renderer/bindings/core/v8/js_based_event_listener.cc
+++ third_party/blink/renderer/bindings/core/v8/js_based_event_listener.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/probe/core_probes.h"
 #include "third_party/blink/renderer/platform/bindings/source_location.h"
 #include "third_party/blink/renderer/platform/instrumentation/instance_counters.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
 namespace blink {
 
@@ -144,8 +145,14 @@ void JSBasedEventListener::Invoke(
     // event's target is in a shadow tree), then set |global|’s current
     // event to event.
     Node* target_node = event->RawTarget()->ToNode();
-    if (!(target_node && target_node->IsInShadowTree()))
+    bool in_shadow_tree =
+        RuntimeEnabledFeatures::TargetInShadowDeterminedBeforeListenerEnabled()
+            ? event->invocationTargetInShadowTree()
+            : (target_node && target_node->IsInShadowTree());
+
+    if (!in_shadow_tree) {
       window->SetCurrentEvent(event);
+    }
   }
 
   {
