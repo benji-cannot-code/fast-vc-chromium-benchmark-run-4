fa63d7603903d6816b8f780e2a08d34966d974d0
Olivier ROBIN <olivierrobin@google.com>
2025-09-25 03:20:28 -0700

    Hide incognito blocker on disconnection.
    If profile change before the incognito blocker is removed, the blocker
    will continue to block the window indefinitely.
    Remove it on disconnection to fix this issue.
    
    Fixed: 447305309
    Change-Id: If391813d659e8dc93e2a260e11b5a7eb56b92795
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6979589
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1520478}

--- ios/chrome/browser/main/ui_bundled/incognito_blocker_scene_agent.mm
+++ ios/chrome/browser/main/ui_bundled/incognito_blocker_scene_agent.mm
@@ -63,7 +63,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [self showOverlay];
   }
 
-  if (level >= SceneActivationLevelForegroundInactive) {
+  if (level >= SceneActivationLevelForegroundInactive ||
+      level == SceneActivationLevelDisconnected) {
     [self hideOverlay];
   }
 }
