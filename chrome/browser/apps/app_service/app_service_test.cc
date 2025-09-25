25c6bed72c7ed10022d72aaee18bee8c6bdd3bad
Hidehiko Abe <hidehiko@chromium.org>
2025-09-24 23:02:50 -0700

    Introduce chrome/browser/apps/app_service/publishers target.
    This CL extracts concrete publisher implementation classes,
    and move them into a new build target.
    
    This is a step towards resolving circular dependency.
    
    BUG=441649482
    TEST=Tryjob
    
    Change-Id: I81064394e3e7f8dd5fc2c43040c469a98706c46c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6965343
    Reviewed-by: Jun Ishiguro <junis@google.com>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1520348}

--- chrome/browser/apps/app_service/app_service_test.cc
+++ chrome/browser/apps/app_service/app_service_test.cc
@@ -14,8 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/image/image_unittest_util.h"
 
 #if BUILDFLAG(IS_CHROMEOS)
-#include "chrome/browser/apps/app_service/publishers/arc_apps.h"
-#include "chrome/browser/apps/app_service/publishers/arc_apps_factory.h"
 #include "components/services/app_service/public/cpp/features.h"
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
