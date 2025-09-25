0641fd93fd50e5f45f06195364a7821b90ffd7c8
Diana OuYang <dianaou@google.com>
2025-09-25 08:23:02 -0700

    Add Suggest WebUI to AIM M3 Side Panel
    Demo: https://screencast.googleplex.com/cast/NjMxNzg0NjkzNzIwNjc4NHxkOTNjYmVlNi1jOQ
    
    DOES:
    - Suggestions panel pops up above the composebox when ZPS suggestions available + composebox is focused in
    - Covers the results page or the AIM chatbox when active.
    - Suggestions should NOT show up when ghost loader is active.
    - Dropdown disappears when ZPS is not available
    - Uses same autocomplete backend as the Chrome Composebox
    - Gated behind kLensOverlayComposeboxShowSuggestionsDropdown
    
    DOES NOT YET:
    - Take in page context + give page-relevant suggestions.
    
    Bug: 444463452
    Change-Id: I08d086f43b266096aa141c2278690695d248b057
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6933101
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Diana OuYang <dianaou@google.com>
    Reviewed-by: Nihar Majmudar <niharm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1520620}

--- components/lens/lens_features.cc
+++ components/lens/lens_features.cc
@@ -104,6 +104,8 @@ BASE_FEATURE(kLensOverlayForceEmptyCsbQuery, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kLensSidePanelEnableWebviewResults,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kLensAimSuggestions, base::FEATURE_DISABLED_BY_DEFAULT);
+
 const base::FeatureParam<int> kLensOverlayMinRamMb{&kLensOverlay, "min_ram_mb",
                                                    /*default=value=*/-1};
 const base::FeatureParam<std::string> kActivityUrl{
@@ -1026,6 +1028,10 @@ bool GetSidePanelGhostLoaderDisabledForAim() {
 bool GetShouldComposeboxContextualizeOnFocus() {
   return base::FeatureList::IsEnabled(kLensSearchAimM3) &&
          kContextualizeOnFocus.Get();
+}
+
+bool GetAimSuggestionsEnabled() {
+  return base::FeatureList::IsEnabled(kLensAimSuggestions);
 }
 
 bool ShouldCloseOverlayOnAimTransition() {
