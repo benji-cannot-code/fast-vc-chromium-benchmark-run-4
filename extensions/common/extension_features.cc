25958ce1ee1a87b8b4d6cef8c9e7fb7ea1934161
Justin Lulejian <jlulejian@chromium.org>
2025-09-25 17:33:28 -0700

    [Extensions] String names for browser namespace and polyfill features
    This allows `kExtensionBrowserNamespaceAlternative` and
    `kRuntimeOnMessageWebExtensionPolyfillSupport` to be controlled via
    command-line flags.
    
    Bug:401226626,40753031,439644930
    
    Change-Id: Ic0264627243b443e16b1f31fa290fc1b853b52bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6986323
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1520978}

--- extensions/common/extension_features.cc
+++ extensions/common/extension_features.cc
@@ -154,6 +154,7 @@ BASE_FEATURE(kDebuggerAPIRestrictedToDevMode,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kExtensionBrowserNamespaceAlternative,
+             "ExtensionBrowserNamespaceAlternative",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kOptimizeServiceWorkerStartRequests,
@@ -166,6 +167,7 @@ BASE_FEATURE(kContentVerifyJobUseJobVersionForHashing,
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kRuntimeOnMessageWebExtensionPolyfillSupport,
+             "RuntimeOnMessageWebExtensionPolyfillSupport",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 }  // namespace extensions_features
