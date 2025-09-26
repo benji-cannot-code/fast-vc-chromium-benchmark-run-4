af76ee370575b4e726bd2dfb33ae746317e7774d
Timofey Chudakov <tchudakov@google.com>
2025-09-26 06:44:34 -0700

    [KA] Apply remaining styling to 2 line KA chips.
    Change chip start and end padding. Correct the secondary text view start
    padding. Change the chip corner radius. Reinstate the border around the
    chips.
    
    Mocks: screen/QgQX2PimvsALVBG
    Before: screen/BtUtmG3gMbaATQs
    After: screen/9YQ65E7UcoW7jJo
    
    Bug: 385172647
    Change-Id: I8c9387b2e901c28862505de8647470bf40c0539e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6973690
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1521244}

--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/chips/ChipView.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/chips/ChipView.java
@@ -71,6 +71,7 @@ public class ChipView extends LinearLayout {
     private final ChromeImageView mStartIcon;
     private final boolean mUseRoundedStartIcon;
     private final LoadingView mLoadingView;
+    private final @Px int mTextStartPadding;
     private final @StyleRes int mSecondaryTextAppearanceId;
     private final boolean mTextAlignStart;
     private final int mEndIconWidth;
@@ -192,8 +193,7 @@ public class ChipView extends LinearLayout {
                                 .getDimensionPixelSize(
                                         R.dimen.chip_text_multiline_vertical_padding));
         mTextAlignStart = a.getBoolean(R.styleable.ChipView_textAlignStart, false);
-        @Px
-        int textStartPadding =
+        mTextStartPadding =
                 a.getDimensionPixelSize(
                         R.styleable.ChipView_primaryTextStartPadding,
                         getResources()
@@ -254,7 +254,7 @@ public class ChipView extends LinearLayout {
             mPrimaryText.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_START);
         }
         mPrimaryText.setPaddingRelative(
-                textStartPadding,
+                mTextStartPadding,
                 mPrimaryText.getPaddingTop(),
                 mPrimaryText.getPaddingEnd(),
                 mPrimaryText.getPaddingBottom());
@@ -469,6 +469,13 @@ public class ChipView extends LinearLayout {
                 if (mTextAlignStart) {
                     mSecondaryText.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_START);
                 }
+                // Align secondary text view with the primary text view if they are stacked
+                // vertically.
+                mSecondaryText.setPaddingRelative(
+                        mTextStartPadding,
+                        mSecondaryText.getPaddingTop(),
+                        mSecondaryText.getPaddingEnd(),
+                        mSecondaryText.getPaddingBottom());
                 mTextViewsWrapper.addView(mSecondaryText);
             } else {
                 addView(mSecondaryText);
