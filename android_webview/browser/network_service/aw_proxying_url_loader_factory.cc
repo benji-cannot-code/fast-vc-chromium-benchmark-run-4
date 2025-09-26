91c291cec735a21187f7cdbbfb546a29bfda10c3
Peter Conn <peconn@chromium.org>
2025-09-26 07:28:31 -0700

    Slightly simplify control flow in AwProxyingURLLoaderFactory.
    This inlines some function calls instead of taking callbacks which
    should hopefully make the code slightly easier to follow.
    
    Bug: 447580987
    Change-Id: I074601f6747c8c2be4708e3e9df1693540d60197
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6988371
    Reviewed-by: Peter Pakkenberg <pbirk@chromium.org>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1521270}

--- android_webview/browser/network_service/aw_proxying_url_loader_factory.cc
+++ android_webview/browser/network_service/aw_proxying_url_loader_factory.cc
@@ -180,7 +180,6 @@ class InterceptedRequest : public network::mojom::URLLoader,
                            base::OnceClosure complete);
 
   void InterceptWithCookieHeader(
-      AwContentsIoThreadClient::ShouldInterceptRequestResponseCallback callback,
       std::string cookie);
 
   // Applies the `AwOriginMatchedHeaders` that match the current
@@ -392,7 +391,6 @@ InterceptedRequest::~InterceptedRequest() {
 }
 
 void InterceptedRequest::InterceptWithCookieHeader(
-    AwContentsIoThreadClient::ShouldInterceptRequestResponseCallback callback,
     std::string cookie) {
   if (cookie != "") {
     request_.headers.SetHeader(net::HttpRequestHeaders::kCookie, cookie);
@@ -406,7 +404,9 @@ void InterceptedRequest::InterceptWithCookieHeader(
     // before network request is intercepted (i.e. if that's possible and
     // whether it can result in any issues).
     io_thread_client->ShouldInterceptRequestAsync(
-        AwWebResourceRequest(request_), std::move(callback));
+        AwWebResourceRequest(request_),
+        base::BindOnce(&InterceptedRequest::InterceptResponseReceived,
+                       weak_factory_.GetWeakPtr()));
   } else {
     SendNoIntercept();
   }
@@ -497,13 +497,7 @@ void InterceptedRequest::Restart() {
                                  request_.referrer.spec());
     }
 
-    auto done = base::BindOnce(
-        &InterceptedRequest::InterceptWithCookieHeader,
-        weak_factory_.GetWeakPtr(),
-        base::BindOnce(&InterceptedRequest::InterceptResponseReceived,
-                       weak_factory_.GetWeakPtr()));
-
-    if (get_cookie_header_.has_value() && io_thread_client &&
+    if (get_cookie_header_.has_value() &&
         io_thread_client->ShouldAcceptCookies(
             io_thread_client_call_duration_)) {
       bool accept_third_party_cookies =
@@ -511,9 +505,14 @@ void InterceptedRequest::Restart() {
               io_thread_client_call_duration_);
 
       std::move(get_cookie_header_)
-          ->Run(accept_third_party_cookies, request_, std::move(done));
+          ->Run(accept_third_party_cookies, request_,
+                base::BindOnce(&InterceptedRequest::InterceptWithCookieHeader,
+                               weak_factory_.GetWeakPtr()));
     } else {
-      std::move(done).Run("");
+      io_thread_client->ShouldInterceptRequestAsync(
+          AwWebResourceRequest(request_),
+          base::BindOnce(&InterceptedRequest::InterceptResponseReceived,
+                         weak_factory_.GetWeakPtr()));
     }
   }
 }
