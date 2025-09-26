0d67c39b7f59a3fd544c9b22fa1d12245865f20e
Pier-Alexandre Bouchard <pabouchard@google.com>
2025-09-26 13:49:26 -0700

    [iOS] Fix contrast issues for customization and sign-in button backgrounds in dark theme
    In dark theme, the background colors of the customization and sign-in buttons did not provide sufficient contrast, creating an accessibility issue. This update fixes the problem by:
    
    - Using Grey770 (#464A4E) as the default background color when no background is set.
    - Applying Secondary30 when a color palette is defined specifically for dark mode.
    
    Default dark theme:
    https://screenshot.googleplex.com/86so39Z5AB2nsGt
    
    background color (old experience):
    https://screenshot.googleplex.com/9XbWGYHJPDMYm9d
    
    background color (new experience):
    https://screenshot.googleplex.com/8eJ9fTEk7CnbEdb
    
    Fixed: 444008805
    
    Change-Id: I7cdb6db82bc96e1d325c9598de4b9239760ecf12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6991010
    Commit-Queue: Pier-Alexandre Bouchard <pabouchard@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1521536}

--- ios/chrome/browser/ntp/ui_bundled/new_tab_page_color_palette.h
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_color_palette.h
@@ -53,6 +53,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // The color used for the monograms.
 @property(nonatomic, strong) UIColor* monogramColor;
 
+// The color used for the header buttons.
+@property(nonatomic, strong) UIColor* headerButtonColor;
+
 // The color scheme variant that was used along with the seed color to generate
 // the palette.
 @property(nonatomic, assign) ui::ColorProviderKey::SchemeVariant variant;
