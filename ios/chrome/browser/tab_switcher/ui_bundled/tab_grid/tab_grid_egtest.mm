710b877a229c492b34753bc6d50c557a33411d72
Chris Lu <thegreenfrog@google.com>
2025-09-26 13:15:20 -0700

    [ios] Re-enable TabGrid tests for ios26
    The tests were just flaking for unclear reasons and pass iOS26 CQ bots
    
    Fixed: 440041762, 441293287, 442326188, 445665552
    Change-Id: Icba1adcf579eccf2834dda1acaf2e13c8885083e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6990330
    Reviewed-by: Louis Romero <lpromero@google.com>
    Auto-Submit: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1521512}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_grid_egtest.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_grid_egtest.mm
@@ -1998,11 +1998,6 @@ void EchoURLDefaultSearchEngineResponseProvider::GetResponseHeadersAndBody(
 // Tests that add to reading list action works successfully from the long press
 // context menu on search results.
 - (void)testSearchOpenTabsContextMenuAddToReadingList {
-  // TODO(crbug.com/440041762): Re-enable the test on iOS26.
-  if (base::ios::IsRunningOnIOS26OrLater()) {
-    EARL_GREY_TEST_DISABLED(@"Test disabled on iOS 26.");
-  }
-
   // Clear the Reading List.
   GREYAssertNil([ReadingListAppInterface clearEntries],
                 @"Unable to clear Reading List entries");
@@ -2266,11 +2261,6 @@ void EchoURLDefaultSearchEngineResponseProvider::GetResponseHeadersAndBody(
 // Tests that interacting with the Tab Grid search UI shows the correct header
 // at each step.
 - (void)testSearchHeaderWithInactiveTabs {
-  // TODO(crbug.com/441293287): Re-enable the test on iOS26.
-  if (base::ios::IsRunningOnIOS26OrLater()) {
-    EARL_GREY_TEST_DISABLED(@"Test disabled on iOS 26.");
-  }
-
   [self loadTestURLsInNewTabs];
   [self relaunchAppWithInactiveTabsTestMode];
 
@@ -2425,11 +2415,6 @@ void EchoURLDefaultSearchEngineResponseProvider::GetResponseHeadersAndBody(
 // Ensures that when users tap on a tab from tab search result and this tab is
 // in another window currently displaying tab grid, the tab is opened.
 - (void)testOpenSearchedTabFromAnotherWindowWhenTabGridIsVisible {
-  // TODO(crbug.com/442326188): Re-enable this flaky test on iOS26.
-  if (base::ios::IsRunningOnIOS26OrLater()) {
-    EARL_GREY_TEST_DISABLED(@"Test disabled on iOS 26.");
-  }
-
   if (![ChromeEarlGrey areMultipleWindowsSupported]) {
     EARL_GREY_TEST_DISABLED(@"Multiple windows can't be opened.");
   }
