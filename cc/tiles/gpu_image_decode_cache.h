6d5bc6ec2444c665e32ce788bc4b1df176912dc8
Colin Blundell <blundell@chromium.org>
2025-09-26 01:02:09 -0700

    [//cc] Remove GpuImageDecodeCache::UploadedImageData::SetImage*
    These are never called post-OOP-C.
    
    Bug: 391648152
    Change-Id: Ic1b40561333819cf973abde5a5f09314d3bd95c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6983083
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1521100}

--- cc/tiles/gpu_image_decode_cache.h
+++ cc/tiles/gpu_image_decode_cache.h
@@ -155,9 +155,6 @@ class CC_EXPORT GpuImageDecodeCache
                                RasterDarkModeFilter* const dark_mode_filter);
   ~GpuImageDecodeCache() override;
 
-  // Returns the GL texture ID backing the given SkImage.
-  static GrGLuint GlIdFromSkImage(const SkImage* image);
-
   static base::TimeDelta get_purge_interval();
   static base::TimeDelta get_max_purge_age();
 
@@ -424,12 +421,6 @@ class CC_EXPORT GpuImageDecodeCache
     UploadedImageData();
     ~UploadedImageData();
 
-    // If |represents_yuv_image| is true, the method knows not to check for a
-    // texture ID for |image|, which would inadvertently flatten it to RGB.
-    void SetImage(sk_sp<SkImage> image, bool represents_yuv_image = false);
-    void SetYuvImage(sk_sp<SkImage> y_image_input,
-                     sk_sp<SkImage> u_image_input,
-                     sk_sp<SkImage> v_image_input);
     void SetTransferCacheId(uint32_t id);
     void Reset();
 
