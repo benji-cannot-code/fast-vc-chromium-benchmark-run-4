a12dfba81a72d884290b95b6a0218f549dd135d7
Dave Tapuska <dtapuska@chromium.org>
2025-09-26 13:36:23 -0700

    Disable signin promo for interactive ui test
    Similar to http://crrev.com/58903059
    
    The signin flow is looking for a specifical dialog. With UnoPhase2Desktop
    this dialog changes. Disable it while people update the generic signin
    code test harness to support this.
    
    Change-Id: Ie029fc6e2c289187d83fc76e0c5b624ad02d2dc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6991289
    Reviewed-by: Marco Georgaklis <mgeorgaklis@google.com>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1521525}

--- chrome/browser/glic/e2e_test/glic_e2e_test.cc
+++ chrome/browser/glic/e2e_test/glic_e2e_test.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/signin/public/identity_manager/test_accounts.h"
+#include "components/sync/base/features.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/test_devtools_protocol_client.h"
 #include "net/dns/mock_host_resolver.h"
@@ -86,7 +87,7 @@ GlicE2ETest::GlicE2ETest() {
                             features::kGlicRollout,
                             contextual_cueing::kContextualCueing,
                             mojom::features::kZeroStateSuggestionsV2},
-      /*disabled_features=*/{});
+      /*disabled_features=*/{syncer::kReplaceSyncPromosWithSignInPromos});
 }
 
 GlicE2ETest::~GlicE2ETest() = default;
