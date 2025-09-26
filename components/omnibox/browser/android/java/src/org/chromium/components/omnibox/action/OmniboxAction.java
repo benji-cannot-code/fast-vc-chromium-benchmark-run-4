992f7592b83b581b182a85782ba0280ad4fcfc82
Jian Li <jianli@chromium.org>
2025-09-26 15:40:34 -0700

    Support both chip icon and action icon in OmniboxAction
    Bug: 409339245
    Change-Id: I860ba61a58170d3177f788c433f83a0ab1043780
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6987332
    Commit-Queue: Jian Li <jianli@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1521614}

--- components/omnibox/browser/android/java/src/org/chromium/components/omnibox/action/OmniboxAction.java
+++ components/omnibox/browser/android/java/src/org/chromium/components/omnibox/action/OmniboxAction.java
@@ -23,23 +23,28 @@ import org.chromium.components.omnibox.R;
 @NullMarked
 public abstract class OmniboxAction {
     /** Describes the ChipView decoration. */
-    public static final class ChipIcon {
-        public final @DrawableRes int iconRes;
-        public final @DrawableRes int incognitoIconRes;
+    public static final class ActionIcon {
+        public final @DrawableRes int chipIconRes;
         public final boolean tintWithTextColor;
 
+        public final @DrawableRes int buttonIconRes;
+        public final @DrawableRes int incognitoButtonIconRes;
+
         /**
-         * @param iconRes The resource Id of the icon to be shown beside the text.
-         * @param incognitoIconRes The resource Id of the icon to be shown beside the text in
-         *     incognito.
+         * @param chipIconRes The resource Id of the icon to be shown beside the text.
+         * @param buttonIconRes The resource Id of the icon to be shown for the action button.
+         * @param incognitoButtonIconRes The resource Id of the icon to be shown for the action
+         *     button in incognito.
          * @param tintWithTextColor Whether to tint the icon using primary text color.
          */
-        public ChipIcon(
-                @DrawableRes int iconRes,
-                @DrawableRes int incognitoIconRes,
+        public ActionIcon(
+                @DrawableRes int chipIconRes,
+                @DrawableRes int buttonIconRes,
+                @DrawableRes int incognitoButtonIconRes,
                 boolean tintWithTextColor) {
-            this.iconRes = iconRes;
-            this.incognitoIconRes = incognitoIconRes;
+            this.chipIconRes = chipIconRes;
+            this.buttonIconRes = buttonIconRes;
+            this.incognitoButtonIconRes = incognitoButtonIconRes;
             this.tintWithTextColor = tintWithTextColor;
         }
 
@@ -47,19 +52,19 @@ public abstract class OmniboxAction {
          * @param iconRes The resource Id of the icon to be shown beside the text.
          * @param tintWithTextColor Whether to tint the icon using primary text color.
          */
-        public ChipIcon(@DrawableRes int iconRes, boolean tintWithTextColor) {
-            this(iconRes, iconRes, tintWithTextColor);
+        public ActionIcon(@DrawableRes int iconRes, boolean tintWithTextColor) {
+            this(iconRes, iconRes, iconRes, tintWithTextColor);
         }
     }
 
     /** The default action icon. */
     @VisibleForTesting(otherwise = VisibleForTesting.PROTECTED)
-    public static final ChipIcon DEFAULT_ICON =
-            new ChipIcon(R.drawable.action_default, /* tintWithTextColor= */ false);
+    public static final ActionIcon DEFAULT_ICON =
+            new ActionIcon(R.drawable.action_default, /* tintWithTextColor= */ false);
 
-    /** ChipIcon instance specifying no icon should be shown. */
-    protected static final ChipIcon NO_ICON =
-            new ChipIcon(ChipView.INVALID_ICON_ID, /* tintWithTextColor= */ false);
+    /** ActionIcon instance specifying no icon should be shown. */
+    protected static final ActionIcon NO_ICON =
+            new ActionIcon(ChipView.INVALID_ICON_ID, /* tintWithTextColor= */ false);
 
     /** The type of an underlying action. */
     public final @OmniboxActionId int actionId;
@@ -71,7 +76,7 @@ public abstract class OmniboxAction {
     public final String accessibilityHint;
 
     /** The icon to use to decorate the Action chip. */
-    public final ChipIcon icon;
+    public final ActionIcon icon;
 
     public final int primaryTextAppearance;
 
@@ -86,7 +91,7 @@ public abstract class OmniboxAction {
             long nativeInstance,
             String hint,
             String accessibilityHint,
-            ChipIcon icon,
+            ActionIcon icon,
             int primaryTextAppearance,
             boolean showAsActionButton) {
         assert !TextUtils.isEmpty(hint);
