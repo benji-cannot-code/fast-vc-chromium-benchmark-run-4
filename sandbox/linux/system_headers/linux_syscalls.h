e151b0b34caad320aa881932b58e1fe17a406144
Matt Stark <msta@google.com>
2025-09-25 21:04:08 -0700

    Reland "Put the syscall header file in the modulemap."
    This reverts commit f2dc75d08b131dd53b9f4b946cf67b6cb55dd65b.
    
    Reason for revert: Updated with fix
    
    Original change's description:
    > Revert "Put the syscall header file in the modulemap."
    >
    > This reverts commit 38fa95c52142968d51681c747de9b3913b02e433.
    >
    > Reason for revert:
    > LUCI Bisection has identified this change as the culprit of a build failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/compile-analysis/b/8702882351645789857
    >
    > Sample failed build: https://ci.chromium.org/b/8702882351645789857
    >
    > If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F6975305&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Fcompile-analysis%2Fb%2F8702882351645789857&type=BUG
    >
    > Original change's description:
    > > Put the syscall header file in the modulemap.
    > >
    > > This fixes https://ci.chromium.org/ui/p/chromium/builders/findit/gofindit-culprit-verification/84473/overview
    > >
    > > Bug: 435303825
    > > Change-Id: I6a6a696450b2f5f4628a0b34747775db37215d55
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6975305
    > > Auto-Submit: Matt Stark <msta@google.com>
    > > Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    > > Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1519746}
    > >
    >
    > Bug: 435303825
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: Iffdad1242e8f41c0a2ac64050c7aeca9508e505e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6975003
    > Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    > Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    > Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1519750}
    
    Bug: 435303825
    Change-Id: I62fef691e605f42ec446ecc92d1cde5dd899921d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6977893
    Commit-Queue: Matt Stark <msta@google.com>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1521041}

--- sandbox/linux/system_headers/linux_syscalls.h
+++ sandbox/linux/system_headers/linux_syscalls.h
@@ -10,6 +10,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SANDBOX_LINUX_SYSTEM_HEADERS_LINUX_SYSCALLS_H_
 #define SANDBOX_LINUX_SYSTEM_HEADERS_LINUX_SYSCALLS_H_
 
+// It is unsafe to include this file before sys/syscall.h when clang modules
+// are enabled. This is because it goes:
+// 1) Compile sys/syscalls.h, it exports __NR_stat64
+// 2) include this file. We check for #if defined(__NR_stat64), and because it
+//    isn't yet available, we define it.
+// 3) Include sys/syscalls.h, which exports a second copy of __NR_stat64
+// This now results in an ambiguous definition error.
+#include <sys/syscall.h>
+
 #include "build/build_config.h"
 
 #if defined(__x86_64__)
@@ -37,4 +46,3 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #endif  // SANDBOX_LINUX_SYSTEM_HEADERS_LINUX_SYSCALLS_H_
-
