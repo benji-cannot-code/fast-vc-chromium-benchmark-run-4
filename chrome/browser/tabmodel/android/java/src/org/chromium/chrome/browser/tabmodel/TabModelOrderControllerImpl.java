490d3b74692cd9a534bd9dfc3339b5a7c059f0ff
Madhav Pruthi <madhavpruthi@google.com>
2025-09-26 10:40:49 -0700

    [PinnedTabs] Skip modifying pinned index if tab is added through restore mechanism
    There is a separate bug(maybe feature) where selected tab index is not restored.
    
    Demo: https://screencast.googleplex.com/cast/NjI1OTMzMDU1NzkzNTYxNnxkMjc1OWI3NC1jMQ
    
    
    Bug: 443142452
    Change-Id: I0725232f4c808abe4435328a0ec5f4ce7daa5623
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6985884
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Madhav Pruthi <madhavpruthi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1521401}

--- chrome/browser/tabmodel/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelOrderControllerImpl.java
+++ chrome/browser/tabmodel/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelOrderControllerImpl.java
@@ -36,7 +36,7 @@ class TabModelOrderControllerImpl implements TabModelOrderController {
         if (type == TabLaunchType.FROM_BROWSER_ACTIONS || type == TabLaunchType.FROM_RECENT_TABS) {
             return TabList.INVALID_TAB_INDEX;
         }
-        if (newTab.getIsPinned()) {
+        if (newTab.getIsPinned() && type != TabLaunchType.FROM_RESTORE) {
             TabModel tabModel = mTabModelSelector.getCurrentModel();
 
             if (type == TabLaunchType.FROM_TAB_LIST_INTERFACE) {
@@ -44,9 +44,10 @@ class TabModelOrderControllerImpl implements TabModelOrderController {
                 @Nullable Tab parentTab = tabModel.getTabById(parentId);
                 int index = tabModel.indexOf(parentTab);
                 if (parentTab != null
-                    && index != TabList.INVALID_TAB_INDEX
-                    && parentTab.getIsPinned())
-                return index + 1;
+                        && index != TabList.INVALID_TAB_INDEX
+                        && parentTab.getIsPinned()) {
+                    return index + 1;
+                }
             }
 
             // Use the `position` when its in valid range; otherwise defer to TabModel
