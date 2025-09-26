77de8331616ab86be0ea24c5860b696d33912c38
Rainhard Findling <rainhard@chromium.org>
2025-09-26 04:19:56 -0700

    DBD dialog: Add missing user action to linkout in 'other data'
    Seems we never came around to add the user action for clicks on the new
    linkout in the 'other data' tab. This CL adds it.
    
    Fixed: 447538056
    Bug: 429984946
    Change-Id: I66715ab36f9025a072b2c997c8936a139acca89a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6988907
    Reviewed-by: Sean Harrison <harrisonsean@chromium.org>
    Reviewed-by: Zaina Al-Mashni <zalmashni@google.com>
    Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1521177}

--- chrome/browser/resources/settings/clear_browsing_data_dialog/other_google_data_dialog.ts
+++ chrome/browser/resources/settings/clear_browsing_data_dialog/other_google_data_dialog.ts
@@ -143,6 +143,9 @@ export class SettingsOtherGoogleDataDialogElement extends
   private onGeminiAppsActivityClick_() {
     OpenWindowProxyImpl.getInstance().openUrl(
         loadTimeData.getString('myActivityGeminiAppsUrl'));
+
+    this.metricsBrowserProxy_.recordAction(
+        'Settings.DeleteBrowsingData.GeminiAppsActivityLinkClick');
   }
 
   private onGeminiPersonalContextClick_() {
