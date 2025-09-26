cad6f316b47cb924f5ea0346aadbd61be3081447
Aliona Dangla <alionadangla@chromium.org>
2025-09-26 07:52:37 -0700

    Reland "[iOS] Add web view resizing type enum"
    This reverts commit 30b4641b0fdf8735f0a41292f57ea2161ff181fa.
    
    Reason for revert: This time https://crrev.com/i/8609417 is merged.
    
    Original change's description:
    > Revert "[iOS] Add web view resizing type enum"
    >
    > This reverts commit 9d6a54fd99ef40212ea0bd35792667c785228604.
    >
    > Reason for revert: compile failing at https://ci.chromium.org/ui/p/chrome/builders/luci.chrome.ci/simulator-builder-arm64
    >
    > Original change's description:
    > > [iOS] Add web view resizing type enum
    > >
    > > This change add the `WebViewResizingType` enum to define different
    > > resizing behaviors for the web view. This allows for more precise
    > > control over how the viewport adapts to UI changes, such as the
    > > appearance of toolbars or other elements.
    > >
    > > This enum is a necessary component for the "Bling Fullscreen
    > > refactoring" on iOS 26. It provides the infrastructure to handle various
    > > fullscreen scenarios and ensures that content is resized correctly when
    > > parts of the viewport are obscured.
    > >
    > > See go/bling-fullscreen-refactoring-part1 for more details
    > >
    > > This is part of multiples changes
    > > * https://crrev.com/c/6839620 | [iOS] Implement fullscreen with obscured inset in iOS 26
    > > * ➔ https://crrev.com/c/6853827 | [iOS] Add fullscreen resizing type enum (Current CL)
    > > * https://crrev.com/c/6973698 | [iOS] Add web view resizing enum
    > > * https://crrev.com/c/6827602 | [iOS] Add obscured inset property
    > >
    > > Bug: 438144976
    > > Change-Id: I310cc99ec1521bcd8929594a15d1e36187835911
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6853827
    > > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > > Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1520655}
    >
    > Bug: 438144976
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: If2a63d0301e582a94d15d10f5ee114f52a0a77c5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6982988
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Auto-Submit: Mike Dougherty <michaeldo@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1520691}
    
    Bug: 438144976
    Change-Id: I7b0a90a25b3c096af0a7c958f8fd29551d7b74dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6988313
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1521289}

--- ios/web/content/web_state/crc_web_viewport_container_view.mm
+++ ios/web/content/web_state/crc_web_viewport_container_view.mm
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @synthesize viewportEdgesAffectedBySafeArea = _viewportEdgesAffectedBySafeArea;
 @synthesize minViewportInsets = _minViewportInsets;
 @synthesize maxViewportInsets = _maxViewportInsets;
+@synthesize webViewResizingType = _webViewResizingType;
 
 - (id)init {
   if ((self = [super init])) {
