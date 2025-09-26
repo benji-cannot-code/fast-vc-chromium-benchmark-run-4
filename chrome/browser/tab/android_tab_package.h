0af28476ab4ca2f13200f2d92415b4496559a74f
Fiaz Muhammad <mfiaz@google.com>
2025-09-26 12:16:12 -0700

    [Persist] Initialize data packages for Tab Storage
    - Create data packages for Tab Storage.
    - Separate android and non-android specific data.
    
    Bug: 447180791
    Change-Id: I30191408e6ac81826a122b033c8b061d5e2f1c57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6990109
    Auto-Submit: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1521467}

---
+++ chrome/browser/tab/android_tab_package.h
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_TAB_ANDROID_TAB_PACKAGE_H_
+#define CHROME_BROWSER_TAB_ANDROID_TAB_PACKAGE_H_
+
+#include <memory>
+#include <string>
+
+namespace tabs {
+
+// C++ representation of platform-specific tab data for Android.
+struct AndroidTabPackage {
+ public:
+  AndroidTabPackage(int version,
+                    long timestamp_millis,
+                    std::unique_ptr<std::string> web_contents_state_bytes,
+                    std::unique_ptr<std::string> opener_app_id,
+                    int theme_color,
+                    long last_navigation_committed_timestamp_millis,
+                    bool tab_has_sensitive_content,
+                    int launch_type_at_creation);
+  ~AndroidTabPackage();
+
+  AndroidTabPackage(const AndroidTabPackage&) = delete;
+  AndroidTabPackage& operator=(const AndroidTabPackage&) = delete;
+
+  const int version_;
+  const long timestamp_millis_;
+  const std::unique_ptr<std::string> web_contents_state_bytes_;
+  const std::unique_ptr<std::string> opener_app_id_;
+  const int theme_color_;
+  const long last_navigation_committed_timestamp_millis_;
+  const bool tab_has_sensitive_content_;
+  const int launch_type_at_creation_;
+};
+
+}  // namespace tabs
+
+#endif  // CHROME_BROWSER_TAB_ANDROID_TAB_PACKAGE_H_
