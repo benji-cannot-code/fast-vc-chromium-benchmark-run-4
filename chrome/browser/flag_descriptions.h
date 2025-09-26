df78310bab9f17b6850910911c8777fe05342bae
Lily Chen <chlily@chromium.org>
2025-09-26 12:20:12 -0700

    Add chrome://flags entry for CNSA cryptography compliance
    This adds a flag that enables the same behavior as
    CryptographyCompliance enterprise policies, when set to comply with
    CNSA.
    
    This is temporary for testing. The enterprise policies are meant to be
    used in the long term.
    
    Bug: 445967223, 436889584
    Change-Id: Icd21be575aa2b469d985295b2ba3ac8a9657a04b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6983936
    Auto-Submit: Lily Chen <chlily@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1521473}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -247,6 +247,9 @@ extern const char kCrosSwitcherName[];
 extern const char kCrosSwitcherDescription[];
 #endif  // IS_CHROMEOS
 
+extern const char kCryptographyComplianceCnsaName[];
+extern const char kCryptographyComplianceCnsaDescription[];
+
 extern const char kCssGamutMappingName[];
 extern const char kCssGamutMappingDescription[];
 
