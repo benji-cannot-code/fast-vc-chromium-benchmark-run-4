53b8c63f766789e9c5da60ee84df6852ece234be
ahmedmoussa <ahmedmoussa@google.com>
2025-09-25 22:52:28 -0700

    Fully remove VideoCaptureEffectsProcessor
    Also remove VideoEffectsContext
    
    Bug: b:443177498
    Change-Id: If35e68938144bf9aa32af04adc7699df51bc321c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6980417
    Commit-Queue: Ahmed Moussa <ahmedmoussa@google.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1521058}

--- content/browser/renderer_host/media/in_process_video_capture_device_launcher.cc
+++ content/browser/renderer_host/media/in_process_video_capture_device_launcher.cc
@@ -394,7 +394,7 @@ InProcessVideoCaptureDeviceLauncher::CreateDeviceClient(
                               receiver_on_io_thread)));
 #else
   return std::make_unique<media::VideoCaptureDeviceClient>(
-      std::move(receiver), std::move(buffer_pool), std::nullopt);
+      std::move(receiver), std::move(buffer_pool));
 #endif  // BUILDFLAG(IS_CHROMEOS)
 }
 
