02fd122ffc2b27e653c3e4dc09b19a625f5a962f
Zack Han <zackhan@chromium.org>
2025-09-26 13:19:37 -0700

    [Synced ESB][Bug Fix] Disable notification for managed users
    The MaybeShowEnhancedProtectionSettingChangeNotification handler was
    being triggered by the policy application on startup. This was
    incorrectly handled as a remote sync change, causing an intrusive and
    un-actionable notification for users who cannot change the setting.
    
    We add a check to MaybeShowEnhancedProtectionSettingChangeNotification
    (for desktop platforms) to return early if the kSafeBrowsingEnhanced
    preference is managed by policy.
    
    Bug: 444058244, 378888301
    Change-Id: I7a63c2ac2d20488aa248e9ffd89bc6f8e9f43918
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6956867
    Commit-Queue: Zack Han <zackhan@chromium.org>
    Reviewed-by: Javier Castro <jacastro@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1521514}

--- chrome/browser/safe_browsing/safe_browsing_pref_change_handler.h
+++ chrome/browser/safe_browsing/safe_browsing_pref_change_handler.h
@@ -20,6 +20,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/android/tab_model/tab_model_observer.h"
 #endif
 
+#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_WIN) || \
+    BUILDFLAG(IS_MAC)
+#include "chrome/browser/ui/toasts/toast_controller.h"
+#endif
+
 class Profile;
 
 namespace safe_browsing {
@@ -58,9 +63,20 @@ class SafeBrowsingPrefChangeHandler {
   // settings page. Virtual for tests.
   virtual void MaybeShowEnhancedProtectionSettingChangeNotification();
 
+#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_WIN) || \
+    BUILDFLAG(IS_MAC)
+  void SetToastControllerForTesting(ToastController* controller);
+#endif
+
  private:
   // Member variable to store the Profile*.
   raw_ptr<Profile> profile_;
+
+#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_WIN) || \
+    BUILDFLAG(IS_MAC)
+  raw_ptr<ToastController> toast_controller_for_testing_ = nullptr;
+#endif
+
 #if BUILDFLAG(IS_ANDROID)
   // Called when the consented modal is dismissed.
   void ConsentedMessageDismissed();
