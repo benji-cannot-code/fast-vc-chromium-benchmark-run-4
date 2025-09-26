2dfc83359e46a056b463803517aefbffccd945d5
Colin Blundell <blundell@chromium.org>
2025-09-26 01:34:16 -0700

    [//cc] Remove unneeded Skia includes from gpu_image_decode_cache.*
    No longer needed post-OOP-C.
    
    Bug: 391648152
    Change-Id: I0034b2b867da705dd3bd2eb4ff8b172b5d785fa2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6983263
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1521116}

--- cc/tiles/gpu_image_decode_cache.h
+++ cc/tiles/gpu_image_decode_cache.h
@@ -31,10 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/cc_export.h"
 #include "cc/paint/image_transfer_cache_entry.h"
 #include "cc/tiles/image_decode_cache.h"
-#include "third_party/skia/include/core/SkImage.h"
-#include "third_party/skia/include/core/SkRefCnt.h"
-#include "third_party/skia/include/core/SkYUVAInfo.h"
-#include "third_party/skia/include/gpu/ganesh/gl/GrGLTypes.h"
 
 namespace viz {
 class RasterContextProvider;
