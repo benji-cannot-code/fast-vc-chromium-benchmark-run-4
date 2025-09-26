4c5ba524945485b4a1515cfae230324d8e02a1f0
Daniil Sakhapov <sakhapov@chromium.org>
2025-09-26 06:08:01 -0700

    Fix category calculation for complex-typed expressions
    Category can only be determined for a math node if a final sum of all
    powers is either 0 (then it's a number), or 1 (then it's that 1 type).
    But there has been a problem that powers weren't summed up in abs
    values, leading to e.g. (1px / 1Hz) resulting in number, since the final
    sum ended up being 0. So, this CL reworks category calculation code a
    bit by checking that if we have more than one non-null type powers, the
    category should be kCalcIntermediate.
    
    But there is still a problem left with kPercent and percent_hint:
    Currently, we don't supply percent hint context (info about what
    percents in an expression would be resolved against, e.g. kCalcLength),
    we rather check at a call side that category of a parsed expression is
    the same as we expect. But we can end up in situation like: (1% * 1%) /
    1px - which would have two powers (+2 for percent, -1 for length). To
    solve this, this CL does a trick in which if we have only two non-null
    type powers, once of which is percent, we simulate ApplyPercentHint to
    the expression in Category() call. With this, we don't need to supply
    the percent hint context info, but solve the problem of not knowing it.
    Note also that this solves CSSOM problem, since there we don't know
    percent hint context at all during parse time.
    
    Also this CL adds printing for CSSMathType to ease future debugging.
    
    Fixed: 445824614
    Change-Id: I4fd179efea371a93ce671027b2e66e9ef564d1c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6973396
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1521219}

--- third_party/blink/renderer/core/css/css_math_expression_node.h
+++ third_party/blink/renderer/core/css/css_math_expression_node.h
@@ -100,14 +100,16 @@ class CSSMathType final {
   DISALLOW_NEW();
 
   // https://drafts.css-houdini.org/css-typed-om-1/#cssnumericvalue-base-type
+  // kPercent should always be the last one as it's used to do a percent hint
+  // trick in Category().
   enum BaseType : uint8_t {
-    kPercent,
     kLength,
     kAngle,
     kTime,
     kFrequency,
     kResolution,
     kFlex,
+    kPercent,
     kNumTypes
   };
 
@@ -140,6 +142,9 @@ class CSSMathType final {
   CORE_EXPORT friend CSSMathType operator/(CSSMathType type1,
                                            CSSMathType type2);
   CORE_EXPORT CSSMathType operator-() const;
+#if DCHECK_IS_ON()
+  friend std::ostream& operator<<(std::ostream& os, const CSSMathType& type);
+#endif
 
  private:
   using BaseTypePowers =
