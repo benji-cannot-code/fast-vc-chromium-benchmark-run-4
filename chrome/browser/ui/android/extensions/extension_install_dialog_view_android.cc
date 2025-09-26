772fb3f29e900e8c8e83d7389e3c100801109e7e
EmiliaPaz <emiliapaz@chromium.org>
2025-09-25 17:37:23 -0700

    [Extensions UI] Add title and icon to install dialog on android
    Screenshot: crbug.com/445811132#attachment69636592
    
    Bug: 445811132
    Change-Id: I29f2b75544afb04feb1f83e78821183bb229ecbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6981116
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Jack Thiesen <jthiesen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1520982}

--- chrome/browser/ui/android/extensions/extension_install_dialog_view_android.cc
+++ chrome/browser/ui/android/extensions/extension_install_dialog_view_android.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "ui/android/view_android.h"
 #include "ui/android/window_android.h"
+#include "ui/gfx/android/java_bitmap.h"
 
 // Must come after all headers that specialize FromJniType() / ToJniType().
 #include "chrome/browser/ui/android/extensions/jni_headers/ExtensionInstallDialogBridge_jni.h"
@@ -74,6 +75,8 @@ void ExtensionInstallDialogViewAndroid::ShowDialog(
 
   Java_ExtensionInstallDialogBridge_showDialog(
       env, java_object,
+      ConvertUTF16ToJavaString(env, prompt_->GetDialogTitle()),
+      gfx::ConvertToJavaBitmap(prompt_->icon().AsBitmap()),
       ConvertUTF16ToJavaString(env, prompt_->GetAcceptButtonLabel()),
       ConvertUTF16ToJavaString(env, prompt_->GetAbortButtonLabel()));
 }
