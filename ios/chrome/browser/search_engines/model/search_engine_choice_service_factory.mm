2c2c8b73c85cec1660898e85b9649c355587603e
James Lee <ljjlee@google.com>
2025-09-26 06:40:25 -0700

    Fix management eligibility checks
    There are two changes:
    * Correctly check device-scope management, by passing the platform
    management service SearchEngineChoiceService
    * Rely on the account capability for account-scope management
    
    NO_IFTTT=Comment-only change to enums.xml
    Bug: 446137903
    
    Change-Id: I1572bd869187da21d90dc12b69e4d8ce48362788
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6976089
    Auto-Submit: James Lee <ljjlee@google.com>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: James Lee <ljjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1521241}

--- ios/chrome/browser/search_engines/model/search_engine_choice_service_factory.mm
+++ ios/chrome/browser/search_engines/model/search_engine_choice_service_factory.mm
@@ -56,7 +56,7 @@ SearchEngineChoiceServiceFactory::BuildServiceInstanceFor(
       CHECK_DEREF(ios::TemplateURLPrepopulateDataResolverFactory::GetForProfile(
           profile)),
       CHECK_DEREF(IdentityManagerFactory::GetForProfile(profile)),
-      CHECK_DEREF(policy::ManagementServiceIOSFactory::GetForProfile(profile)));
+      CHECK_DEREF(policy::ManagementServiceIOSFactory::GetForPlatform()));
 
   service->Init();
   return service;
