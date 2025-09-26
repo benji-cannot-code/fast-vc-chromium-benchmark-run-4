42e97eb9b556f2b387219fb8d6df696d5a2f9785
Kramer Ge <fangzhoug@chromium.org>
2025-09-26 12:57:02 -0700

    [Ozone/Wayland]gbm_pixmap_wayland fallback to implicit modifier
    linux_dmabuf extension specifies that DRM_FORMAT_MOD_INVALID is allowed
    for server to indicate support for implicit modifier.
    
    wlroots-based compositor such as sway appears to always announce
    MOD_INVALID and MOD_LINEAR. However, in reality some hardware driver
    does not render to MOD_LINEAR, so we need to perform this fallback. As
    long as the device selection matches the compositor, driver should know
    about the modifiers internally.
    
    Add MOD_INVALID to the list to indicate implicit modifier support.
    
    Bug: 446395287
    Change-Id: I3b9b9e66c17b3968213007d5e1643b59a6987052
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6972623
    Commit-Queue: Kramer Ge <fangzhoug@chromium.org>
    Reviewed-by: Peter McNeeley <petermcneeley@google.com>
    Cr-Commit-Position: refs/heads/main@{#1521503}

--- ui/ozone/platform/wayland/gpu/wayland_buffer_manager_gpu.cc
+++ ui/ozone/platform/wayland/gpu/wayland_buffer_manager_gpu.cc
@@ -5,8 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/ozone/platform/wayland/gpu/wayland_buffer_manager_gpu.h"
 
+#include <drm_fourcc.h>
+
 #include <utility>
 
+#include "base/containers/contains.h"
 #include "base/functional/bind.h"
 #include "base/process/process.h"
 #include "base/task/current_thread.h"
@@ -359,6 +362,15 @@ WaylandBufferManagerGpu::GetModifiersForBufferFormat(
     return it->second;
   }
   return {};
+}
+
+bool WaylandBufferManagerGpu::AllowsImplicitModifierForBufferFormat(
+    gfx::BufferFormat buffer_format) const {
+  auto it = supported_buffer_formats_with_modifiers_.find(buffer_format);
+  if (it != supported_buffer_formats_with_modifiers_.end()) {
+    return base::Contains(it->second, DRM_FORMAT_MOD_INVALID);
+  }
+  return false;
 }
 
 uint32_t WaylandBufferManagerGpu::AllocateBufferID() {
