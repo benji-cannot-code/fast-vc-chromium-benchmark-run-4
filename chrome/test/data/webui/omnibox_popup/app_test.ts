0ecceaa9109e933101d8cb7e315631b880a6f2b0
dpapad <dpapad@chromium.org>
2025-09-26 16:24:57 -0700

    WebUI Omnibox Popup: Don't show context menu on right click.
    Except when the 'debug' URL parameter exists.
    
    Also adding a new chrome/test/data/webui/omnibox_popup/ folder to host
    tests for this UI, and adding initial tests for the context menu.
    
    Fixed: b/447645394
    Change-Id: I7c3c5873905800fa9d1b0339a5c8c48a81371e73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6990534
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1521637}

---
+++ chrome/test/data/webui/omnibox_popup/app_test.ts
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'chrome://omnibox-popup.top-chrome/omnibox_popup.js';
+
+import {assertTrue} from 'chrome://webui-test/chai_assert.js';
+import {eventToPromise} from 'chrome://webui-test/test_util.js';
+
+suite('AppTest', function() {
+  test('ContextMenuPrevented', async function() {
+    const app = document.createElement('omnibox-popup-app');
+    document.body.appendChild(app);
+    const whenFired = eventToPromise('contextmenu', document.documentElement);
+    document.documentElement.dispatchEvent(
+        new Event('contextmenu', {cancelable: true}));
+    const e = await whenFired;
+    assertTrue(e.defaultPrevented);
+  });
+});
