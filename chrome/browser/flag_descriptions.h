a6a4cb313eb7eb5b02a004b8589c06fd93d9df75
Ryan Sultanem <rsult@google.com>
2025-09-26 12:51:12 -0700

    [Uno-D] Integrate History sync experiment in Uno Phase 2
    Migrated the `switches::kEnableHistorySyncOptinExpansionPill` to
    `syncer::kReplaceSyncPromosWithSignInPromos` as the feature will now
    be used as part of this launch.
    
    Also inverted the flag priority between
    `switches::IsAvatarSyncPromoFeatureEnabled()` and
    `syncer::kReplaceSyncPromosWithSignInPromos` so that the latter has
    priority when both are enabled (since sync would not be an option
    anymore). Followup CLs will take care of the specific behavior for
    Windows users.
    
    Bug: 447048341
    Fixed: 444617947, 418145883
    Change-Id: Ica0434df370c2ca896c453a1c41dab6d91c0e291
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6983027
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1521497}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -4868,9 +4868,6 @@ extern const char kProfilesReorderingDescription[];
 extern const char kEnableChromeRefreshTokenBindingName[];
 extern const char kEnableChromeRefreshTokenBindingDescription[];
 
-extern const char kEnableHistorySyncOptinExpansionPillName[];
-extern const char kEnableHistorySyncOptinExpansionPillDescription[];
-
 extern const char kEnableOAuthMultiloginCookiesBindingName[];
 extern const char kEnableOAuthMultiloginCookiesBindingDescription[];
 
--- chrome/browser/signin/signin_promo_unittest.cc
+++ chrome/browser/signin/signin_promo_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/strings/to_string.h"
+#include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
 #include "chrome/browser/autofill/personal_data_manager_factory.h"
 #include "chrome/browser/extensions/sync/extension_sync_util.h"
@@ -754,6 +755,9 @@ class SyncPromoIdentityPillManagerTest : public testing::Test {
   content::BrowserTaskEnvironment task_environment_;
   signin::IdentityTestEnvironment identity_test_environment_;
   TestingPrefServiceSimple pref_service_;
+
+  base::test::ScopedFeatureList scoped_feature_list_{
+      syncer::kReplaceSyncPromosWithSignInPromos};
 };
 
 TEST_F(SyncPromoIdentityPillManagerTest, MaxShownCount) {
