544622e316a24c21451dabfc328b3e46672464b1
Jonathan Lee <jonathanjlee@google.com>
2025-09-26 12:53:32 -0700

    [remoting] Clean up `HostFrameStats::GetForVideoPacket()`
    This has no callers and no coverage in any test suite [0].
    
    [0]: https://analysis.chromium.org/coverage/p/chromium/file?host=chromium.googlesource.com&project=chromium/src&ref=refs/heads/main&revision=c37f119c698fbb959eb84939a52ed54df114240e&path=//remoting/protocol/frame_stats.cc&platform=linux&test_suite_type=any&modifier_id=0
    
    Bug: None
    Change-Id: I7564ce929aceee5d2672c78474870f952f032fd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6990414
    Auto-Submit: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1521499}

--- remoting/protocol/frame_stats.h
+++ remoting/protocol/frame_stats.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace remoting {
 
-class VideoPacket;
 class FrameStatsMessage;
 
 namespace protocol {
@@ -22,9 +21,6 @@ struct HostFrameStats {
   HostFrameStats();
   HostFrameStats(const HostFrameStats&);
   ~HostFrameStats();
-
-  // Extracts timing fields from the |packet|.
-  static HostFrameStats GetForVideoPacket(const VideoPacket& packet);
 
   // Converts FrameStatsMessage protobuf message to HostFrameStats.
   static HostFrameStats FromFrameStatsMessage(const FrameStatsMessage& message);
