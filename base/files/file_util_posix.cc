db28dd124aa56656ddd2ae4447dc8b9f9dafb3fb
S. Ganesh <ganesh@chromium.org>
2025-09-26 15:47:17 -0700

    base: custom prefixes for posix CreateNewTempDirectory and ScopedTempDir
    This is to allow different projects to have custom prefixes. The prefix
    makes it easier to clean up temporary directories created by specific
    projects or components.
    
    It also allows for a zero-length prefix, which is useful for specific
    cases where the total path length has constraints. For instance, the
    maximum length of `sun_path` within the `sockaddr_un` structure has a
    limit of only 104 characters (including the null terminator).
    
    Fixed: 447234785,447459820
    Change-Id: Ie4043e1e584fcfc7c640e0cec3f1ae47d62fb6df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6985223
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1521617}

--- base/files/file_util_posix.cc
+++ base/files/file_util_posix.cc
@@ -128,8 +128,9 @@ bool VerifySpecificPathControlledByUser(const FilePath& path,
 }
 #endif
 
-base::FilePath GetTempTemplate() {
-  return FormatTemporaryFileName("XXXXXX");
+base::FilePath GetTempTemplate(FilePath::StringViewType prefix = "") {
+  return FormatTemporaryFileName(
+      StrCat({prefix, prefix.empty() ? "" : ".", "XXXXXX"}));
 }
 
 bool AdvanceEnumeratorWithStat(FileEnumerator* traversal,
@@ -936,7 +937,8 @@ bool CreateNewTempDirectory(const FilePath::StringType& prefix,
     return false;
   }
 
-  return CreateTemporaryDirInDirImpl(tmpdir, GetTempTemplate(), new_temp_path);
+  return CreateTemporaryDirInDirImpl(tmpdir, GetTempTemplate(prefix),
+                                     new_temp_path);
 }
 
 bool CreateDirectoryAndGetError(const FilePath& full_path, File::Error* error) {
