521b2f23e444449ebc8dc363b4ee4f9c469e916c
Peter Pakkenberg <pbirk@chromium.org>
2025-09-26 08:15:01 -0700

    Add experiment to cache boundary interface methods
    This experiment only caches methods for Chromium-side objects, but will
    still be able to give us data on whether there are any performance
    benefits to the cache. A similar cache can be added in AndroidX, but
    should just be enabled by default if a performance experiment shows that
    it makes a difference.
    
    Bug: b:447115247
    Change-Id: I4bcbd7233572e899197be531d74d6099d64f5c7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6979544
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Ashley Newson <ashleynewson@chromium.org>
    Commit-Queue: Peter Pakkenberg <pbirk@chromium.org>
    Auto-Submit: Peter Pakkenberg <pbirk@chromium.org>
    Commit-Queue: Ashley Newson <ashleynewson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1521308}

--- android_webview/common/aw_features.h
+++ android_webview/common/aw_features.h
@@ -71,6 +71,7 @@ BASE_DECLARE_FEATURE(kWebViewReducedSeedExpiration);
 BASE_DECLARE_FEATURE(kWebViewReducedSeedRequestPeriod);
 BASE_DECLARE_FEATURE(kWebViewEarlyStartupTracing);
 BASE_DECLARE_FEATURE(kWebViewEarlyPerfettoInit);
+BASE_DECLARE_FEATURE(kWebViewCacheBoundaryInterfaceMethods);
 }  // namespace android_webview::features
 
 #endif  // ANDROID_WEBVIEW_COMMON_AW_FEATURES_H_
