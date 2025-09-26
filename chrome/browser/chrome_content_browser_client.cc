5d142cde867cff0ae41c21447ec4b7186b254aaf
Lei Zhang <thestig@chromium.org>
2025-09-26 14:12:27 -0700

    Change ChromeContentBrowserClient::AddExtraPart() to use std::unique_ptr
    Make pointer ownership clearer.
    
    Change-Id: Ib73c0fad695eb86ba55479e43f2cb52bfd48b2db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6987644
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Auto-Submit: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1521561}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -7139,8 +7139,8 @@ bool ChromeContentBrowserClient::HandleWebUIReverse(
 }
 
 void ChromeContentBrowserClient::AddExtraPart(
-    ChromeContentBrowserClientParts* part) {
-  extra_parts_.push_back(base::WrapUnique(part));
+    std::unique_ptr<ChromeContentBrowserClientParts> part) {
+  extra_parts_.push_back(std::move(part));
 }
 
 std::unique_ptr<HttpAuthCoordinator>
@@ -8697,8 +8697,7 @@ void ChromeContentBrowserClient::SetSamplingProfiler(
 
 void ChromeContentBrowserClient::AddExtraPartForTesting(
     std::unique_ptr<ChromeContentBrowserClientParts> part) {
-  // AddExtraPart takes ownership of a raw pointer.
-  AddExtraPart(part.release());
+  AddExtraPart(std::move(part));
 }
 
 bool ChromeContentBrowserClient::ShouldDispatchPagehideDuringCommit(
