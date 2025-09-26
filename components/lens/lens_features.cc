49fb5977fd11cfbcf91137c211ed2f44e6725123
Juan Mojica <juanmojica@google.com>
2025-09-26 12:42:21 -0700

    Add updated feedback entrypoint feature.
    This is going to be added to chrome://flags and the field trial testing config before branch cut.
    
    Bug: 447450244
    Change-Id: If91dbfcda38b21980c15a3317c5a971ab3c218e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6990353
    Reviewed-by: Bryan Nguyen <nguyenbryan@google.com>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1521493}

--- components/lens/lens_features.cc
+++ components/lens/lens_features.cc
@@ -110,6 +110,9 @@ BASE_FEATURE(kLensSearchZeroStateCsb, base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kLensVideoCitations, base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kLensUpdatedFeedbackEntrypoint,
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 const base::FeatureParam<int> kLensOverlayMinRamMb{&kLensOverlay, "min_ram_mb",
                                                    /*default=value=*/-1};
 const base::FeatureParam<std::string> kActivityUrl{
@@ -1198,6 +1201,10 @@ std::string GetZeroStateCsbQuery() {
 
 bool IsLensVideoCitationsEnabled() {
   return base::FeatureList::IsEnabled(kLensVideoCitations);
+}
+
+bool IsLensUpdatedFeedbackEnabled() {
+  return base::FeatureList::IsEnabled(kLensUpdatedFeedbackEntrypoint);
 }
 
 }  // namespace lens::features
