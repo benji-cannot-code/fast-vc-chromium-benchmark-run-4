03f4cccb44a1bd85eec6b85bfeaef304e3d8aa4d
Lukasz Anforowicz <lukasza@chromium.org>
2025-09-26 12:40:01 -0700

    Remove `#pragma allow_unsafe_buffers` from `base/files/file_path.cc`.
    Bug: 40284755
    Change-Id: I65bb0889d896a9e10be846fd043736506a709f5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6973923
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1521490}

--- base/files/file_path.h
+++ base/files/file_path.h
@@ -111,6 +111,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base_export.h"
 #include "base/compiler_specific.h"
+#include "base/containers/span_forward_internal.h"
 #include "base/trace_event/base_tracing_forward.h"
 #include "build/build_config.h"
 
@@ -472,7 +473,8 @@ class BASE_EXPORT FilePath {
 
   // Normalize all path separators to given type on Windows
   // (if FILE_PATH_USES_WIN_SEPARATORS is true), or do nothing on POSIX systems.
-  [[nodiscard]] FilePath NormalizePathSeparatorsTo(CharType separator) const;
+  [[nodiscard]] FilePath NormalizePathSeparatorsTo(
+      CharType normalized_separator) const;
 
   // Compare two strings in the same way the file system does.
   // Note that these always ignore case, even on file systems that are case-
@@ -539,6 +541,9 @@ class BASE_EXPORT FilePath {
   // separators is never stripped, to support alternate roots.  This is used to
   // support UNC paths on Windows.
   void StripTrailingSeparatorsInternal();
+
+  // Returns `kSeparators` as a `span` (without the terminating NUL character).
+  static span<const CharType> SeparatorsAsSpan();
 
   bool IsParentFast(const FilePath& child) const;
   bool IsParentSlow(const FilePath& child) const;
