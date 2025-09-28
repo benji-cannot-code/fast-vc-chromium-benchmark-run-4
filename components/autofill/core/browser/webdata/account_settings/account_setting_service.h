5fc4e9a89e53af4fdbbad676dccd73017b6f45f2
Jihad Hanna <jihadghanna@google.com>
2025-09-28 15:45:59 -0700

    Add AccountSettingService
    Adds a new keyed service AccountSettingService, which will be
    responsible for managing settings coming from a user's Google account.
    
    In particular, this class will:
    - Own AccountSettingSyncBridge, which controls the ACCOUNT_SETTING sync
      data type.
    - Provide an API to read settings.
    
    Low-Coverage-Reason: TRIVIAL_CHANGE This CL only adds an empty new keyed
    Bug: 441735283
    Change-Id: I34b48b300c6e6ab3b413e933861a96a552d90060
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6989109
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1521933}

---
+++ components/autofill/core/browser/webdata/account_settings/account_setting_service.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_AUTOFILL_CORE_BROWSER_WEBDATA_ACCOUNT_SETTINGS_ACCOUNT_SETTING_SERVICE_H_
+#define COMPONENTS_AUTOFILL_CORE_BROWSER_WEBDATA_ACCOUNT_SETTINGS_ACCOUNT_SETTING_SERVICE_H_
+
+#include "components/keyed_service/core/keyed_service.h"
+
+namespace autofill {
+
+// Manages settings stored in your google account. These settings differ from
+// regular prefs, since they originate from the user's account and are available
+// beyond Chrome.
+class AccountSettingService : public KeyedService {
+ public:
+  AccountSettingService() = default;
+  ~AccountSettingService() override = default;
+};
+
+}  // namespace autofill
+
+#endif  // COMPONENTS_AUTOFILL_CORE_BROWSER_WEBDATA_ACCOUNT_SETTINGS_ACCOUNT_SETTING_SERVICE_H_
