078f1fcf14befb84d6c4b093b0a8fade07826c95
Federica Germinario <fedegermi@google.com>
2025-09-28 14:13:06 -0700

    [ios] Remove NotFatalUntil::M138 from authentication_flow_in_profile
    base::NotFatalUntil::M138 is not needed anymore because we're in M142.
    
    Bug: none
    Change-Id: I9522031ada492d6da07a8ed35ffc4df4e9a4feb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6988707
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1521928}

--- ios/chrome/browser/authentication/ui_bundled/authentication_flow/authentication_flow_in_profile.mm
+++ ios/chrome/browser/authentication/ui_bundled/authentication_flow/authentication_flow_in_profile.mm
@@ -114,8 +114,7 @@ enum class AuthenticationFlowInProfileState {
 
 - (void)startSignInWithCompletion:
     (signin_ui::SigninCompletionCallback)completion {
-  CHECK_EQ(_state, AuthenticationFlowInProfileState::kBegin,
-           base::NotFatalUntil::M138);
+  CHECK_EQ(_state, AuthenticationFlowInProfileState::kBegin);
   CHECK(!_signInCompletion) << "startSignInWithCompletion was called twice.";
   CHECK(completion);
   _selfRetainer = self;
@@ -322,8 +321,7 @@ enum class AuthenticationFlowInProfileState {
                 currentProfile:profile];
     _didSignIn = YES;
   } else {
-    CHECK([currentIdentity isEqual:_identityToSignIn],
-          base::NotFatalUntil::M138);
+    CHECK([currentIdentity isEqual:_identityToSignIn]);
   }
   [self continueFlow];
 }
@@ -432,7 +430,7 @@ enum class AuthenticationFlowInProfileState {
   _accountSwitchingBatchClosureRunner.RunAndReset();
   // Clean up asynchronously to ensure that `self` does not die while
   // the flow is running.
-  CHECK([NSThread isMainThread], base::NotFatalUntil::M138);
+  CHECK([NSThread isMainThread]);
   dispatch_async(dispatch_get_main_queue(), ^{
     self->_selfRetainer = nil;
   });
@@ -442,8 +440,7 @@ enum class AuthenticationFlowInProfileState {
 #pragma mark - AuthenticationFlowPerformerDelegate
 
 - (void)didSignOutForAccountSwitch {
-  CHECK_EQ(AuthenticationFlowInProfileState::kSignOutIfNeeded, _state,
-           base::NotFatalUntil::M138);
+  CHECK_EQ(AuthenticationFlowInProfileState::kSignOutIfNeeded, _state);
   [self continueFlow];
 }
 
@@ -462,7 +459,7 @@ enum class AuthenticationFlowInProfileState {
                          userAffiliationIDs:
                              (NSArray<NSString*>*)userAffiliationIDs {
   CHECK_EQ(AuthenticationFlowInProfileState::kRegisterForUserPolicyIfNeeded,
-           _state, base::NotFatalUntil::M138);
+           _state);
   _dmToken = dmToken;
   _clientID = clientID;
   _userAffiliationIDs = userAffiliationIDs;
