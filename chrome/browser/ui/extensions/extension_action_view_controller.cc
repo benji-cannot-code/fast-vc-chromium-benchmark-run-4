68f8d93019a587154093238803296a13027566e2
EmiliaPaz <emiliapaz@chromium.org>
2025-09-28 18:22:01 -0700

    [Extensions] Use BrowserWindowInterface in ExtensionActionViewController
    This CL refactors ExtensionActionViewController to use
    BrowserWindowInterface instead of Browser. This change is part of
    a larger effort to decouple extensions from the Browser object.
    
    We use browser_->GetBrowserForMigrationOnly() for the few instances that
    are not yet supported by BrowserWindowInterface.
    
    is hit.
    
    Bypass-Check-License: Edits are on existent files. Unsure why presubmit
    Bug: 437774758
    Change-Id: Ibf79357850670025fd7b180595c26f6e9c3a6d56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6967562
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1521946}

--- chrome/browser/ui/extensions/extension_action_view_controller.cc
+++ chrome/browser/ui/extensions/extension_action_view_controller.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/permissions/site_permissions_helper.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/extensions/extension_action_platform_delegate.h"
 #include "chrome/browser/ui/extensions/extension_popup_types.h"
 #include "chrome/browser/ui/extensions/extension_side_panel_utils.h"
@@ -136,18 +137,19 @@ GetHoverCardPolicyState(Profile& profile,
 std::unique_ptr<ExtensionActionViewController>
 ExtensionActionViewController::Create(
     const extensions::ExtensionId& extension_id,
-    Browser* browser,
+    BrowserWindowInterface* browser,
     ExtensionsContainer* extensions_container) {
   DCHECK(browser);
   DCHECK(extensions_container);
 
-  auto* registry = extensions::ExtensionRegistry::Get(browser->profile());
+  Profile* profile = browser->GetProfile();
+  auto* registry = extensions::ExtensionRegistry::Get(profile);
   scoped_refptr<const extensions::Extension> extension =
       registry->enabled_extensions().GetByID(extension_id);
   DCHECK(extension);
   extensions::ExtensionAction* extension_action =
-      extensions::ExtensionActionManager::Get(browser->profile())
-          ->GetExtensionAction(*extension);
+      extensions::ExtensionActionManager::Get(profile)->GetExtensionAction(
+          *extension);
   DCHECK(extension_action);
 
   // WrapUnique() because the constructor is private.
@@ -171,13 +173,13 @@ bool ExtensionActionViewController::AnyActionHasCurrentSiteAccess(
 
 ExtensionActionViewController::ExtensionActionViewController(
     scoped_refptr<const extensions::Extension> extension,
-    Browser* browser,
+    BrowserWindowInterface* browser,
     extensions::ExtensionAction* extension_action,
     extensions::ExtensionRegistry* extension_registry,
     ExtensionsContainer* extensions_container)
     : extension_(std::move(extension)),
       browser_(browser),
-      profile_(browser->profile()),
+      profile_(browser->GetProfile()),
       extension_action_(extension_action),
       extensions_container_(extensions_container),
       popup_host_(nullptr),
@@ -644,8 +646,8 @@ void ExtensionActionViewController::TriggerPopup(PopupShowAction show_action,
   const GURL popup_url = extension_action_->GetPopupUrl(tab_id);
 
   std::unique_ptr<extensions::ExtensionViewHost> host =
-      extensions::ExtensionViewHostFactory::CreatePopupHost(popup_url,
-                                                            browser_);
+      extensions::ExtensionViewHostFactory::CreatePopupHost(
+          popup_url, browser_->GetBrowserForMigrationOnly());
   // Creating a host should never fail in this case, since the extension is
   // valid and has a valid popup URL.
   CHECK(host);
