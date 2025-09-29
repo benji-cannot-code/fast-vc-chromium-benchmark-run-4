aff57bacae6ad5b97967d54d9b5695a714ce38f3
Tarek Makkouk <tarekmakkouk@google.com>
2025-09-29 08:05:02 -0700

    [iOS] Reorder background options in customization menu
    This change updates the display order of background customization
    options within the main customization menu.
    
    The new order is now:
    1. Default
    2. + (Add new button)
    3. Recently used carousel
    
    Bug: 445889454
    Change-Id: I530a6d61cd7447db4fe41f6295af098cb2b4a1fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6966998
    Reviewed-by: Pier-Alexandre Bouchard <pabouchard@google.com>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Commit-Queue: Tarek Makkouk <tarekmakkouk@google.com>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1522211}

--- ios/chrome/browser/home_customization/ui/home_customization_main_view_controller.mm
+++ ios/chrome/browser/home_customization/ui/home_customization_main_view_controller.mm
@@ -165,8 +165,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         appendSectionsWithIdentifiers:@[ kCustomizationSectionBackground ]];
     [snapshot appendItemsWithIdentifiers:[self identifiersForBackgroundCells]
                intoSectionWithIdentifier:kCustomizationSectionBackground];
-    [snapshot appendItemsWithIdentifiers:@[ kBackgroundPickerCellIdentifier ]
-               intoSectionWithIdentifier:kCustomizationSectionBackground];
   }
 
   // Create toggles section and add items to it.
@@ -460,12 +458,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // by the snapshot.
 - (NSArray<NSString*>*)identifiersForBackgroundCells {
   NSMutableArray<NSString*>* identifiers = [[NSMutableArray alloc] init];
+
+  NSUInteger indexAfterDefault = 0;
+
   for (NSString* key in _backgroundCollectionConfiguration.configurationOrder) {
-    if (![_backgroundCollectionConfiguration.configurations objectForKey:key]) {
+    id<BackgroundCustomizationConfiguration> configuration =
+        _backgroundCollectionConfiguration.configurations[key];
+    if (!configuration) {
       continue;
     }
+
     [identifiers addObject:key];
+
+    if (configuration.backgroundStyle ==
+        HomeCustomizationBackgroundStyle::kDefault) {
+      indexAfterDefault = identifiers.count;
+    }
   }
+
+  [identifiers insertObject:kBackgroundPickerCellIdentifier
+                    atIndex:indexAfterDefault];
 
   return [identifiers copy];
 }
