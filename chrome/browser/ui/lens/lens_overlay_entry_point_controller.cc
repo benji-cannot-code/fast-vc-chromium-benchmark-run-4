dc670254ed2cb06a8f282146bce2503f092fe016
Juan Mojica <juanmojica@google.com>
2025-09-29 15:12:55 -0700

    [AIM] Do not allow reinvoking overlay when feature disabled.
    Inadvertently, all entrypoints were allowed to reinvoke the overlay as
    they were not marked as disabled due to the side panel state no longer
    being accounted for in `IsOverlayActive`. This should only be allowed if
    the feature is enabled and there should no change to the visibility of
    the Lens entrypoints when the side panel is open.
    
    Bug: 448131721
    Change-Id: I42c44de1ad188ee73ad0f063c3d357273c8efce9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6994634
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1522519}

--- chrome/browser/ui/lens/lens_overlay_entry_point_controller.cc
+++ chrome/browser/ui/lens/lens_overlay_entry_point_controller.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/exclusive_access/exclusive_access_context.h"
 #include "chrome/browser/ui/exclusive_access/exclusive_access_manager.h"
 #include "chrome/browser/ui/lens/lens_overlay_controller.h"
+#include "chrome/browser/ui/lens/lens_overlay_side_panel_coordinator.h"
 #include "chrome/browser/ui/lens/lens_search_controller.h"
 #include "chrome/browser/ui/lens/lens_url_matcher.h"
 #include "chrome/browser/ui/page_action/page_action_icon_type.h"
@@ -208,8 +209,6 @@ void LensOverlayEntryPointController::InvokeAction(
     const actions::ActionInvocationContext& context) {
   LensSearchController* search_controller =
       LensSearchController::From(active_tab);
-  LensOverlayController* overlay_controller =
-      active_tab->GetTabFeatures()->lens_overlay_controller();
 
   std::underlying_type_t<page_actions::PageActionTrigger> page_action_trigger =
       context.GetProperty(page_actions::kPageActionTriggerKey);
@@ -242,7 +241,11 @@ void LensOverlayEntryPointController::InvokeAction(
 
   // Toggle the Lens overlay. There's no need to show or hide the side
   // panel as the overlay controller will handle that.
-  if (overlay_controller->IsOverlayActive()) {
+  const auto* entry_point_controller =
+      active_tab->GetBrowserWindowInterface()
+          ->GetFeatures()
+          .lens_overlay_entry_point_controller();
+  if (entry_point_controller->IsOverlayActive()) {
     search_controller->CloseLensAsync(
         lens::LensOverlayDismissalSource::kToolbar);
   } else {
@@ -341,12 +344,22 @@ void LensOverlayEntryPointController::UpdatePageActionState() {
 }
 
 bool LensOverlayEntryPointController::IsOverlayActive() const {
-  const auto* active_tab = browser_window_interface_->GetActiveTabInterface();
+  // TODO(crbug.com/404941800): Rename this function to make it clear that it
+  // checks both the overlay and the side panel being active.
+  auto* active_tab = browser_window_interface_->GetActiveTabInterface();
   if (!active_tab) {
     return false;
   }
-  const auto* controller =
-      active_tab->GetTabFeatures()->lens_overlay_controller();
+
+  LensSearchController* search_controller =
+      LensSearchController::From(active_tab);
+  LensOverlaySidePanelCoordinator* side_panel_coordinator =
+      search_controller->lens_overlay_side_panel_coordinator();
+  if (side_panel_coordinator && side_panel_coordinator->IsEntryShowing()) {
+    return true;
+  }
+
+  const auto* controller = search_controller->lens_overlay_controller();
   return controller && controller->IsOverlayActive();
 }
 
