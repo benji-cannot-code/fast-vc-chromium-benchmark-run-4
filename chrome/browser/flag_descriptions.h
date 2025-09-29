e226ca53e7c563213682b968a42b7baf5113ce24
Duong Dac <ddac@chromium.org>
2025-09-29 07:01:51 -0700

    Remove undefined signin flags and update comments
    Follow up of crrev.com/c/6966396
    
    Change-Id: I0cb21ffb3584edc82cdb7f39454d7bb98bbac24d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6994240
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1522174}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -4934,9 +4934,6 @@ extern const char kSupervisedUserLocalWebApprovalsDescription[];
 #endif  // #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN)
 
 #if BUILDFLAG(IS_ANDROID)
-extern const char kHistoryPageHistorySyncPromoName[];
-extern const char kHistoryPageHistorySyncPromoDescription[];
-
 extern const char kHistoryOptInEducationalTipName[];
 extern const char kHistoryOptInEducationalTipDescription[];
 #endif  // BUILDFLAG(IS_ANDROID)
