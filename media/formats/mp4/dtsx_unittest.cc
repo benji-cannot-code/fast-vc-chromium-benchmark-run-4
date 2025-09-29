c7bb25e91ce42b72edc85eb8c82d7ce158d5e06b
Ho Cheung <hocheung@chromium.org>
2025-09-29 16:58:16 -0700

    [media] Fix DTS/AC4 parsing and passthrough
    1. Refactor BitReader::ReadBits and SkipBits usage to require unsigned
    types and CHECK all critical parsing steps for robustness.
    
    2. Update DTSStreamParser and related classes to match base virtual
    function signatures.
    
    3. Remove legacy int* and const uint8_t* usage in favor of size_t*
    and span.
    
    4. Replace DecoderBuffer::data() with direct span or buffer usage for
    safer access.
    
    5. Fix some other type mismatch issues.
    
    Fixed: 447307172
    Change-Id: I93f7b7caeb1a4c4e101aad9d3abd08ec1969b013
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6983184
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Ho Cheung <hocheung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1522578}

--- media/formats/mp4/dtsx_unittest.cc
+++ media/formats/mp4/dtsx_unittest.cc
@@ -51,7 +51,7 @@ TEST_F(DTSXTest, NormalInputTest) {
   EXPECT_EQ(dtsx_.GetDecoderProfileCode(), 0u);
   EXPECT_EQ(dtsx_.GetFrameDuration(), 1024);
   EXPECT_EQ(dtsx_.GetMaxPayload(), 4096);
-  EXPECT_EQ(dtsx_.GetNumPresentations(), 0);
+  EXPECT_EQ(dtsx_.GetNumPresentations(), 0u);
   EXPECT_EQ(dtsx_.GetChannelMask(), 63u);
   EXPECT_EQ(dtsx_.GetSamplingFrequency(), 48000);
 }
