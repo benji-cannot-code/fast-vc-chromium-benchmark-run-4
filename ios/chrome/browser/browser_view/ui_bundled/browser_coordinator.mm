58a96a434dcd2bc347033faeb1da06539b79d59a
Nohemi Fernandez <fernandex@google.com>
2025-09-29 03:51:35 -0700

    [iOS] Record share actions for Reading Mode.
    Adds a Reading Mode surface to `Mobile.Share.EntryPoints` and its
    corresponding share actions in `Mobile.Share.ShareInReaderMode.*`.
    
    Bug: 447323488
    Change-Id: I52d905f4749652a65a0f396a1183dbe8de955001
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6984407
    Commit-Queue: Nohemi Fernandez <fernandex@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1522098}

--- ios/chrome/browser/browser_view/ui_bundled/browser_coordinator.mm
+++ ios/chrome/browser/browser_view/ui_bundled/browser_coordinator.mm
@@ -181,6 +181,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/reader_mode/model/reader_mode_browser_agent.h"
 #import "ios/chrome/browser/reader_mode/model/reader_mode_browser_agent_delegate.h"
 #import "ios/chrome/browser/reader_mode/model/reader_mode_tab_helper.h"
+#import "ios/chrome/browser/reader_mode/model/reader_mode_web_state_utils.h"
 #import "ios/chrome/browser/reading_list/model/reading_list_browser_agent.h"
 #import "ios/chrome/browser/reading_list/ui_bundled/reading_list_coordinator.h"
 #import "ios/chrome/browser/reading_list/ui_bundled/reading_list_coordinator_delegate.h"
@@ -1922,8 +1923,10 @@ const char kChromeAppStoreUrl[] =
 #pragma mark - ActivityServiceCommands
 
 - (void)stopAndStartSharingCoordinator {
-  SharingParams* params =
-      [[SharingParams alloc] initWithScenario:SharingScenario::TabShareButton];
+  SharingScenario scenario = IsReaderModeActiveInWebState(self.activeWebState)
+                                 ? SharingScenario::ShareInReaderMode
+                                 : SharingScenario::TabShareButton;
+  SharingParams* params = [[SharingParams alloc] initWithScenario:scenario];
 
   // Exit fullscreen if needed to make sure that share button is visible.
   _fullscreenController->ExitFullscreen(FullscreenExitReason::kForcedByCode);
---
+++ ios/chrome/browser/reader_mode/model/reader_mode_web_state_utils.mm
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/reader_mode/model/reader_mode_web_state_utils.h"
+
+#import "ios/chrome/browser/reader_mode/model/features.h"
+#import "ios/chrome/browser/reader_mode/model/reader_mode_tab_helper.h"
+#import "ios/web/public/web_state.h"
+
+bool IsReaderModeActiveInWebState(web::WebState* web_state) {
+  if (!IsReaderModeAvailable() || !web_state) {
+    return false;
+  }
+  ReaderModeTabHelper* tab_helper =
+      ReaderModeTabHelper::FromWebState(web_state);
+  if (tab_helper) {
+    return tab_helper->IsActive();
+  }
+
+  return false;
+}
