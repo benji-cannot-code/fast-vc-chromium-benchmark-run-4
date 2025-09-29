5aa4303edaca1c51ceaf8b22f2e2723936854163
Norge Vizcay <vizcay@google.com>
2025-09-29 06:20:36 -0700

    Fix proto visitor for sync_pb::Any.
    The CL updates the VISIT_PROTO_FIELDS declaration for sync_pb::Any to
    use the proper types.
    
    Bug: 441736370
    Change-Id: I33a4f5db1bc4e338251aef24e9b286730bd3ec4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6994239
    Auto-Submit: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1522149}

--- components/sync/protocol/proto_visitors.h
+++ components/sync/protocol/proto_visitors.h
@@ -2025,11 +2025,6 @@ VISIT_PROTO_FIELDS(
   VISIT(is_collapsed);
 }
 
-VISIT_PROTO_FIELDS(const sync_pb::Any& proto) {
-  VISIT_REP(type_url);
-  VISIT_REP(value);
-}
-
 VISIT_PROTO_FIELDS(const sync_pb::AutofillValuableSpecifics& proto) {
   VISIT(id);
   VISIT(is_editable);
@@ -2037,6 +2032,11 @@ VISIT_PROTO_FIELDS(const sync_pb::AutofillValuableSpecifics& proto) {
   VISIT(vehicle_registration);
   VISIT(flight_reservation);
   VISIT(serialized_chrome_valuables_metadata);
+}
+
+VISIT_PROTO_FIELDS(const sync_pb::Any& proto) {
+  VISIT(type_url);
+  VISIT_BYTES(value);
 }
 
 VISIT_PROTO_FIELDS(const sync_pb::LoyaltyCard& proto) {
