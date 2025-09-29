970786d4fdf42b6a15332634de0ff87c437ae2ac
Benoît Lizé <lizeb@chromium.org>
2025-09-29 05:37:08 -0700

    [cc/scheduler] Add a feature to unthrottle BMF after an urgent frame
    On some platforms (for instance Android), UI animations are throttled
    except when within some time of a touch event. This CL adds a feature to
    get the same behavior. More specifically, together with
    kThrottleMainFrameTo60Hz and kUrgentMainFrameForInput, this makes input
    events tagged as "urgent", and unthrottles main frame production for the
    next 1.5s.
    
    Bug: 379678455
    Change-Id: I0bbf0b4e983be590e87b1c68bcd29f4f6cde22ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6988747
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1522134}

--- cc/scheduler/scheduler_state_machine.cc
+++ cc/scheduler/scheduler_state_machine.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 
 #include "base/check_op.h"
+#include "base/feature_list.h"
 #include "base/format_macros.h"
 #include "base/notreached.h"
 #include "base/time/time.h"
@@ -667,6 +668,7 @@ bool SchedulerStateMachine::ShouldSendBeginMainFrame() const {
 bool SchedulerStateMachine::ShouldThrottleSendBeginMainFrame() const {
   bool result = false;
   auto throttled_interval = MainFrameThrottledInterval();
+
   if (throttled_interval.is_positive() &&
       last_begin_impl_frame_time_ - last_sent_begin_main_frame_time_ <
           throttled_interval) {
@@ -678,6 +680,15 @@ bool SchedulerStateMachine::ShouldThrottleSendBeginMainFrame() const {
   // throttle. This is more expensive, but is required to reach perceptual
   // visual parity between throttled and non-throttled scrolling.
   if (is_current_scroll_main_painted_) {
+    result = false;
+  }
+
+  // Only evaluate the condition if we would be throttling, this is important
+  // for experiment targeting (not querying the feature).
+  if (result &&
+      base::FeatureList::IsEnabled(
+          features::kBoostFrameRateForUrgentMainFrame) &&
+      (Now() - last_urgent_main_frame_request_) < kUrgentBoostDuration) {
     result = false;
   }
 
@@ -1699,6 +1710,7 @@ void SchedulerStateMachine::SetNeedsBeginMainFrame(bool now) {
 
   if (now) {
     last_sent_begin_main_frame_time_ = base::TimeTicks();
+    last_urgent_main_frame_request_ = Now();
   }
 }
 
@@ -1858,6 +1870,10 @@ void SchedulerStateMachine::SetShouldThrottleFrameRate(bool flag) {
   if (base::FeatureList::IsEnabled(features::kRenderThrottleFrameRate)) {
     throttle_frame_rate_ = flag;
   }
+}
+
+base::TimeTicks SchedulerStateMachine::Now() const {
+  return base::TimeTicks::Now();
 }
 
 base::TimeDelta SchedulerStateMachine::MainFrameThrottledInterval() const {
--- cc/scheduler/scheduler_state_machine.h
+++ cc/scheduler/scheduler_state_machine.h
@@ -118,6 +118,11 @@ class CC_EXPORT SchedulerStateMachine {
       ForcedRedrawOnTimeoutStateToProtozeroEnum(
           ForcedRedrawOnTimeoutState state);
 
+  // Public for testing.
+  // Semi-arbitrary delay, chosen to be similar to Android's platform behavior.
+  static constexpr base::TimeDelta kUrgentBoostDuration =
+      base::Milliseconds(1500);
+
   BeginMainFrameState begin_main_frame_state() const {
     return begin_main_frame_state_;
   }
@@ -393,6 +398,9 @@ class CC_EXPORT SchedulerStateMachine {
 
   void SetShouldThrottleFrameRate(bool flag);
 
+  // Virtual for testing.
+  virtual base::TimeTicks Now() const;
+
  protected:
   bool BeginFrameRequiredForAction() const;
   bool BeginFrameNeededForVideo() const;
@@ -461,6 +469,7 @@ class CC_EXPORT SchedulerStateMachine {
   base::TimeTicks last_sent_begin_main_frame_time_;
   base::TimeDelta main_frame_throttled_interval_;
   base::TimeDelta unthrottled_frame_interval_;
+  base::TimeTicks last_urgent_main_frame_request_;
 
   // Inputs from the last impl frame that are required for decisions made in
   // this impl frame. The values from the last frame are cached before being
