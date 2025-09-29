b2a4d9740584ead4d94d775bc6bc564bfc7fbb72
Andrew Grieve <agrieve@chromium.org>
2025-09-29 10:30:55 -0700

    Android: Ensure RetailDemoMode is disabled in tests
    Bug: 442291426
    Change-Id: I6db3a71c27f7e1ed4ec41f60dbe89f1475ce9f82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6995599
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Salvador Guerrero Ramos <salg@google.com>
    Commit-Queue: Salvador Guerrero Ramos <salg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1522323}

--- chrome/android/junit/src/org/chromium/chrome/browser/firstrun/FirstRunIntegrationUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/firstrun/FirstRunIntegrationUnitTest.java
@@ -33,7 +33,6 @@ import org.robolectric.android.controller.ActivityController;
 import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ShadowApplication;
 
-import org.chromium.base.DeviceInfo;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.Features;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
@@ -71,7 +70,6 @@ public final class FirstRunIntegrationUnitTest {
         mContext = ApplicationProvider.getApplicationContext();
         mShadowApplication = shadowOf((Application) ApplicationProvider.getApplicationContext());
 
-        DeviceInfo.setIsRetailDemoModeForTesting(false);
         ChromeBrowserInitializer.setForTesting(mChromeBrowserInitializer);
 
         FirstRunStatus.setFirstRunFlowComplete(false);
--- chrome/android/junit/src/org/chromium/chrome/browser/signin/SigninManagerImplTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/signin/SigninManagerImplTest.java
@@ -37,7 +37,6 @@ import org.robolectric.shadows.ShadowLooper;
 
 import org.chromium.base.Callback;
 import org.chromium.base.CallbackUtils;
-import org.chromium.base.DeviceInfo;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.chrome.browser.bookmarks.BookmarkModel;
@@ -530,9 +529,6 @@ public class SigninManagerImplTest {
         createSigninManager();
         when(mExternalAuthUtils.canUseGooglePlayServices()).thenReturn(true);
 
-        // Make sure that the user is not a demo user.
-        DeviceInfo.setIsRetailDemoModeForTesting(false);
-
         assertTrue(mSigninManager.isSigninSupported(/* requireUpdatedPlayServices= */ true));
         assertTrue(mSigninManager.isSigninSupported(/* requireUpdatedPlayServices= */ false));
     }
@@ -541,9 +537,6 @@ public class SigninManagerImplTest {
     public void signInShouldNotBeSupportedWhenGooglePlayServicesIsRequiredAndNotAvailable() {
         createSigninManager();
         when(mExternalAuthUtils.canUseGooglePlayServices()).thenReturn(false);
-
-        // Make sure that the user is not a demo user.
-        DeviceInfo.setIsRetailDemoModeForTesting(false);
 
         assertFalse(mSigninManager.isSigninSupported(/* requireUpdatedPlayServices= */ true));
     }
