49b07197eb3e99a83ff2bd8bc0f0b85b2db25509
Omar Bahgat <omarbahgat@google.com>
2025-09-29 10:51:42 -0700

    AutoSignOut: Update sign-in timestamp only when service is enabled
    Previously, the device info sign-in timestamp was sent to Chrome Sync
    regardless of whether the AutoSignOutService was enabled.
    
    This changes only uploads the sign-in timestamp to Chrome Sync if the
    AutoSignOutService is enabled. This avoids redundant traffic when the
    feature is disabled.
    
    Bug: b:433240810
    Change-Id: I700f8afd3748c62fa54f8c77fdaf52b090287bbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6988332
    Commit-Queue: Omar Bahgat <omarbahgat@google.com>
    Reviewed-by: Andrey Davydov <andreydav@google.com>
    Reviewed-by: Felipe Andrade <fsandrade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1522336}

--- chromeos/ash/components/auto_sign_out/auto_sign_out_service.cc
+++ chromeos/ash/components/auto_sign_out/auto_sign_out_service.cc
@@ -38,7 +38,6 @@ AutoSignOutService::AutoSignOutService(
       initialization_time_(base::Time::Now()) {
   RegisterPrefListeners();
   UpdateObservations();
-  UpdateLocalDeviceInfoWhenReady();
 }
 
 AutoSignOutService::~AutoSignOutService() = default;
@@ -73,6 +72,7 @@ void AutoSignOutService::UpdateObservations() {
       power_manager_client_observation_.Observe(
           chromeos::PowerManagerClient::Get());
     }
+    UpdateLocalDeviceInfoWhenReady();
   } else {
     power_manager_client_observation_.Reset();
     session_manager_observation_.Reset();
@@ -88,8 +88,7 @@ void AutoSignOutService::UpdateLocalDeviceInfoWhenReady() {
 
   if (local_device_info_provider->GetLocalDeviceInfo()) {
     UpdateLocalDeviceInfo();
-  } else {
-    CHECK(!local_device_info_ready_subscription_);
+  } else if (!local_device_info_ready_subscription_) {
     local_device_info_ready_subscription_ =
         local_device_info_provider->RegisterOnInitializedCallback(
             base::BindRepeating(
