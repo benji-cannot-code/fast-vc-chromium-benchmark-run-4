262866aaab4870ed8fbfa42869d18062c83e305d
Zijie He <zijiehe@google.com>
2025-09-29 15:04:20 -0700

    [fuchsia] Fix tarfile mode in gcs_download
    Correct the `tarfile.open` mode from `'r|gz'` to `'r:gz'` for reading
    gzipped tar archives without excessive in-memory buffering.
    
    See the test run, https://ci.chromium.org/ui/p/chromium/builders/try/fuchsia-arm64-cast-receiver-rel/14186/overview.
    
    Fixed: 448143979
    Change-Id: I0049042ffd0189e0d11f2b760ac88bcdb7eb2fa9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6996289
    Reviewed-by: David Song <wintermelons@google.com>
    Commit-Queue: Zijie He <zijiehe@google.com>
    Auto-Submit: Zijie He <zijiehe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1522515}

--- build/fuchsia/gcs_download.py
+++ build/fuchsia/gcs_download.py
@@ -20,4 +20,4 @@ def DownloadAndUnpackFromCloudStorage(url, output_dir):
     tmp_file_location = os.path.join(tmp_d, tmp_file)
     run_gsutil(['cp', url, tmp_file_location])
     tarfile.open(name=tmp_file_location,
-                 mode='r|gz').extractall(path=output_dir)
+                 mode='r:gz').extractall(path=output_dir)
