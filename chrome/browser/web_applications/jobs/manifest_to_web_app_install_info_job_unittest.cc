50fb62b3b5cb95473b5a067dfe61b05d6fd6a9a9
Dibyajyoti Pal <dibyapal@google.com>
2025-09-29 16:37:16 -0700

    [Predictable App Update] Measure uncompressed icon sizes post download
    This CL adds a metric to measure the sizes of the icons downloaded as
    part of the update process (both old and new). Since less icons will
    be downloaded as part of predictable app updating, this can still be
    used to measure the effectiveness of the new algorithm, even though
    there are some caveats (like less icons downloaded but larger sizes)
    etc.
    
    Bug: 447149311
    Change-Id: Id93bc82c3db2f586cbce30c3161ed205fd79740f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6993805
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1522574}

--- chrome/browser/web_applications/jobs/manifest_to_web_app_install_info_job_unittest.cc
+++ chrome/browser/web_applications/jobs/manifest_to_web_app_install_info_job_unittest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/contains.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/test/bind.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/test_future.h"
 #include "build/buildflag.h"
@@ -319,6 +320,16 @@ TEST_F(ManifestToWebAppInstallInfoJobTest, EmptyNameUsesShortName) {
 
   auto web_app_info = GetWebAppInstallInfoFromJob(*manifest);
   EXPECT_EQ(u"Shorter name", web_app_info->title);
+}
+
+TEST_F(ManifestToWebAppInstallInfoJobTest,
+       UpdateIconMeasurementFlagSetMeasuresMemory) {
+  base::HistogramTester tester;
+  SetupBasicPageState();
+  auto web_app_info = GetWebAppInstallInfoFromJob(
+      *GetPageManifest(), {.record_icon_results_on_update = true});
+  EXPECT_FALSE(web_app_info->icon_bitmaps.empty());
+  tester.ExpectTotalCount("WebApp.TotalIconsMemory.DownloadedForUpdate", 1);
 }
 
 TEST_F(ManifestToWebAppInstallInfoJobTest, IconParsingCorrectly) {
