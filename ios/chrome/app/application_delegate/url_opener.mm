62a28b97fdc02f71c4266f3245af1592747d3bea
Federica Germinario <fedegermi@google.com>
2025-09-29 02:51:40 -0700

    [ios] Fix opening chrome://dino from dino widget with a profile switch
    Initial solution to fix this issue is not working when opening an
    URL after a profile switch, this change makes sure that all flows
    are covered by moving that logic in:
    webView:decidePolicyForNavigationAction:preferences:decisionHandler:
    
    Fixed: 447327039
    Change-Id: I2f88e47d3701d44c48e502c6f2a1b3f72bfb9a24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6983404
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1522066}

--- ios/chrome/app/application_delegate/url_opener.mm
+++ ios/chrome/app/application_delegate/url_opener.mm
@@ -15,9 +15,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/app/startup/chrome_app_startup_parameters.h"
 #import "ios/chrome/browser/policy/model/policy_util.h"
 #import "ios/chrome/browser/shared/coordinator/scene/connection_information.h"
+#import "ios/chrome/browser/shared/model/url/chrome_url_constants.h"
 #import "ios/chrome/browser/shared/model/url/url_util.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/url_loading/model/url_loading_params.h"
+#import "ios/components/webui/web_ui_url_constants.h"
 #import "url/gurl.h"
 
 namespace {
@@ -115,6 +117,12 @@ const char* const kUMAShowDefaultPromoFromAppsHistogram =
         gurl = [params externalURL];
       }
       UrlLoadParams urlLoadParams = UrlLoadParams::InNewTab(gurl, virtualURL);
+      if (gurl.scheme() == kChromeUIScheme &&
+          gurl.host() == kChromeUIDinoHost) {
+        urlLoadParams.web_params.transition_type =
+            ui::PAGE_TRANSITION_AUTO_BOOKMARK;
+      }
+
       BOOL dismissOmnibox = [params postOpeningAction] != FOCUS_OMNIBOX;
 
       if (base::FeatureList::IsEnabled(kChromeStartupParametersAsync)) {
