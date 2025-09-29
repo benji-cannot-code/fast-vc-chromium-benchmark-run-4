ac6d56fde40c1cd9ede17564de3bbf4abf82f5ed
Ben Scarlato <akhna@google.com>
2025-09-29 10:25:31 -0700

    Revert "Enable GPU sandbox for Desktop Android"
    This reverts commit e066205b9cfa98334adb683570892fc94c701643.
    
    Reason for revert: Maps issue reported in b/447237526
    
    Original change's description:
    > Enable GPU sandbox for Desktop Android
    >
    > This enables the Landlock-based GPU sandbox, only for Desktop Android
    > devices. On older versions of Android where Landlock is either not
    > supported or blocked by seccomp, the sandbox will not be applied.
    >
    > Bug: 432047991
    > Test: Chrome launches on Desktop Android device with Landlock enabled.
    > Change-Id: Iaf4743ab69de4e280b2204201c15c0fda15cd083
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6917061
    > Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    > Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    > Commit-Queue: Ben Scarlato <akhna@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1517578}
    
    Bug: 432047991
    Change-Id: Idb82263dbe7a1fd46ccd769c7081b75fe3978e99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6992823
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Commit-Queue: Ben Scarlato <akhna@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1522320}

--- chrome/browser/chrome_browser_field_trials.cc
+++ chrome/browser/chrome_browser_field_trials.cc
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_features.h"
 #include "media/audio/audio_features.h"
 #include "media/base/media_switches.h"
-#include "sandbox/policy/features.h"
 #endif
 
 #if BUILDFLAG(IS_CHROMEOS)
@@ -185,10 +184,6 @@ void ChromeBrowserFieldTrials::RegisterFeatureOverrides(
       chrome::android::kLockTopControlsOnLargeTablets);
   feature_overrides.EnableFeature(
       chrome::android::kLockTopControlsOnLargeTabletsV2);
-  // TODO(crbug.com/445446479): Remove when rollout is complete to all form
-  // factors.
-  feature_overrides.EnableFeature(
-      sandbox::policy::features::kAndroidGpuSandbox);
   // Bypass the WebAudio output buffer, to reduce audio latency.
   // TODO(crbug.com/436988695): Remove when the long term solution is
   // implemented.
