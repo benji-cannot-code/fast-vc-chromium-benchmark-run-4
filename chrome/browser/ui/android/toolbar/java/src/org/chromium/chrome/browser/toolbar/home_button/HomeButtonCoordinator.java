7cea1732a730ceec3f9153e294629d1d62f12b67
Charles Hager <clhager@google.com>
2025-09-30 13:51:31 -0700

    Add isVisible method to ToolbarWidthConsumer
    Bug: 427260734
    Change-Id: Ib87ab3bc4e552d20ba83642c341989c4d029aa82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6976799
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Commit-Queue: Charles Hager <clhager@google.com>
    Cr-Commit-Position: refs/heads/main@{#1523126}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/home_button/HomeButtonCoordinator.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/home_button/HomeButtonCoordinator.java
@@ -131,6 +131,11 @@ public class HomeButtonCoordinator extends ToolbarChildButton implements HomeBut
     }
 
     @Override
+    public boolean isVisible() {
+        return getVisibility() == View.VISIBLE;
+    }
+
+    @Override
     public int getVisibility() {
         return mHomeButton.getVisibility();
     }
--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/menu_button/MenuButtonCoordinator.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/menu_button/MenuButtonCoordinator.java
@@ -216,6 +216,7 @@ public class MenuButtonCoordinator extends ToolbarChildButton {
     /**
      * @return Whether the menu button is present and visible.
      */
+    @Override
     public boolean isVisible() {
         if (mVisibilityDelegate != null) {
             return mVisibilityDelegate.isMenuButtonVisible();
