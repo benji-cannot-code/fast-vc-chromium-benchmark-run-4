1200e129628a16243f542791149ae72792376b5b
Teresa Mao <temao@chromium.org>
2025-09-30 10:59:24 -0700

    [searchbox][webui] Override contextual suggestion icon in composebox.
    Composebox's contextual suggestion icon should be using the default
    search icon.
    
    See screenshots in the bug.
    
    Bug: 419077032
    Change-Id: I3e7cc9e1a6d4f03c66e672444e6c8b955ec69668
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6997652
    Commit-Queue: Teresa Mao <temao@chromium.org>
    Reviewed-by: Nihar Majmudar <niharm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1523004}

--- chrome/browser/ui/webui/searchbox/contextual_searchbox_handler.cc
+++ chrome/browser/ui/webui/searchbox/contextual_searchbox_handler.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/webui_embedding_context.h"
 #include "components/lens/contextual_input.h"
 #include "components/lens/tab_contextualization_controller.h"
+#include "components/omnibox/browser/vector_icons.h"
 
 using composebox::SessionState;
 
@@ -196,4 +197,16 @@ void ContextualSearchboxHandler::OnFileUploadStatusChanged(
                                         error_type);
   composebox_metrics_recorder_->OnFileUploadStatusChanged(
       mime_type, file_upload_status, error_type);
+}
+
+std::string ContextualSearchboxHandler::AutocompleteIconToResourceName(
+    const gfx::VectorIcon& icon) {
+  // The default icon for contextual suggestions is the subdirectory arrow right
+  // icon. For the Lens composebox and realbox, we want to stay consistent with
+  // the search loupe instead.
+  if (icon.name == omnibox::kSubdirectoryArrowRightIcon.name) {
+    return searchbox_internal::kSearchIconResourceName;
+  }
+
+  return SearchboxHandler::AutocompleteIconToResourceName(icon);
 }
