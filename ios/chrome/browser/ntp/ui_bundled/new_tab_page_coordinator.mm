12a237b4e3bfdb67581604220c4a319da288f3ce
Robbie Gibson <rkgibson@google.com>
2025-09-30 10:15:15 -0700

    [iOS] Add customization menu dissmissal to NTP clear presented state
    The customization menu wasn't directly dismissed when clearing NTP
    presented state. Instead, the view would be removed, but the coordinator
    would not be stopped.
    
    Fixed: 448383345
    Change-Id: I126a9131f48b1ea95f5b8f615a411c77f33ba69b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6997873
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Auto-Submit: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1522966}

--- ios/chrome/browser/ntp/ui_bundled/new_tab_page_coordinator.mm
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_coordinator.mm
@@ -613,6 +613,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self stopSharingCoordinator];
   [self stopAccountMenuCoordinator];
   [self stopSigninCoordinator];
+  [self dismissCustomizationMenu];
 }
 
 #pragma mark - Setters
