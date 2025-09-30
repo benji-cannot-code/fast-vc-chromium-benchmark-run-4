71efd4d3b7417b9eec57855c2f026c2b0a45ae8a
Devon Loehr <dloehr@google.com>
2025-09-30 09:59:18 -0700

    Add additional logging to build_rust.py
    When parsing the output of build_rust.py, it's difficult to tell where
    the boundaries are between the different sub-builds. This adds some
    logging messages to make it clear where one stops and the next begins.
    
    Bug: 407563488
    Change-Id: I38f59ce3903837a5e62832b6218f73698f9997f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7000567
    Commit-Queue: Devon Loehr <dloehr@google.com>
    Reviewed-by: Arthur Eubanks <aeubanks@google.com>
    Cr-Commit-Position: refs/heads/main@{#1522949}

--- tools/rust/build_rust.py
+++ tools/rust/build_rust.py
@@ -757,10 +757,10 @@ def main():
     args, rest = parser.parse_known_args()
 
     if args.entire_toolchain:
-      args.build_bindgen = True
-      args.build_vet = True
-      args.build_crubit = True
-      args.gnrt_stdlib = True
+        args.build_bindgen = True
+        args.build_vet = True
+        args.build_crubit = True
+        args.gnrt_stdlib = True
 
     if sys.platform == 'win32':
         if args.sh:
@@ -949,6 +949,7 @@ def main():
               'w',
               encoding='utf-8') as log:
         if args.build_bindgen:
+            print('Building bindgen...')
             build_cmd = [
                 sys.executable,
                 os.path.join(THIS_DIR, 'build_bindgen.py')
@@ -956,6 +957,7 @@ def main():
             TeeCmd(build_cmd, log)
 
         if args.build_vet:
+            print('Building vet...')
             build_cmd = [
                 sys.executable,
                 os.path.join(THIS_DIR, 'build_vet.py')
@@ -963,6 +965,7 @@ def main():
             TeeCmd(build_cmd, log)
 
         if args.build_crubit:
+            print('Building crubit...')
             build_cmd = [
                 sys.executable,
                 os.path.join(THIS_DIR, 'build_crubit.py')
@@ -973,7 +976,9 @@ def main():
             TeeCmd(build_cmd, log, fail_hard=False)
 
         if args.gnrt_stdlib:
+            print('Building gnrt...')
             InstallRustBetaSysroot(checkout_revision, [RustTargetTriple()])
+            print('Beta sysroot installed.')
             build_cmd = [
                 sys.executable,
                 os.path.join(THIS_DIR, 'gnrt_stdlib.py'), '--skip-prep'
