ee31a2fcc115922c5f5db32ab851559d674d478a
Vidya Balachander <vidyakbalachander@gmail.com>
2025-09-30 16:33:10 -0700

    Using the tighter USE_V4L2_CODEC to guard kHardwareVideoDecodeFrameRate
    Bug: 414429305
    Change-Id: Ic49996dbb215c1834333a1a6872b3f10575043ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6987325
    Reviewed-by: Fritz Koenig <frkoenig@chromium.org>
    Reviewed-by: Nathan Hebert <nhebert@chromium.org>
    Commit-Queue: Nathan Hebert <nhebert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1523212}

--- content/browser/gpu/gpu_process_host.cc
+++ content/browser/gpu/gpu_process_host.cc
@@ -324,7 +324,7 @@ static const char* const kSwitchNames[] = {
     ash::switches::kRevenBranding,
     switches::kSchedulerBoostUrgent,
 #endif
-#if BUILDFLAG(USE_LINUX_VIDEO_ACCELERATION)
+#if BUILDFLAG(USE_V4L2_CODEC)
     switches::kHardwareVideoDecodeFrameRate,
 #endif
 };
