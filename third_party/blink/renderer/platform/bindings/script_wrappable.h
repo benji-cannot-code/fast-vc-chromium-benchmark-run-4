246b77ff4968a1be98e3524094315fb4f0514fd2
David Sanders <dsanders11@ucsbalum.com>
2025-09-30 16:20:50 -0700

    Remove unused include in script_wrappable.h
    Bug: 40318405
    Change-Id: I6c519af28146e3463f793151037897925a24f5a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6997500
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1523206}

--- third_party/blink/renderer/platform/bindings/script_wrappable.h
+++ third_party/blink/renderer/platform/bindings/script_wrappable.h
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_BINDINGS_SCRIPT_WRAPPABLE_H_
 
 #include "build/build_config.h"
-#include "third_party/blink/renderer/platform/bindings/name_client.h"
 #include "third_party/blink/renderer/platform/bindings/trace_wrapper_v8_reference.h"
 #include "third_party/blink/renderer/platform/bindings/wrapper_type_info.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
