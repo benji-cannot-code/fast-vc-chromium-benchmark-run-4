a32311bcea44de3cfcca555d31bfbbf841e461d1
James Cook <jamescook@chromium.org>
2025-09-29 17:26:40 -0700

    extensions: Port content_capabilities_browsertest.cc to desktop Android
    This is the integration test for the "content_capabilities" manifest
    key, which allows extensions to grant certain capabilities to web
    pages. Supporting the key is needed for DocsOffline.
    
    Bug: 448098008
    Change-Id: Ia2d01b78c7bff2823b20e939abf525c96c3eb6cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6997749
    Reviewed-by: Zhengzheng Liu <zhzhliu@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1522596}

--- extensions/common/manifest_handlers/content_capabilities_manifest_unittest.cc
+++ extensions/common/manifest_handlers/content_capabilities_manifest_unittest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/command_line.h"
+#include "extensions/buildflags/buildflags.h"
 #include "extensions/common/extension_builder.h"
 #include "extensions/common/manifest_constants.h"
 #include "extensions/common/manifest_handlers/content_capabilities_handler.h"
@@ -14,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/switches.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 using extensions::mojom::APIPermissionID;
 
