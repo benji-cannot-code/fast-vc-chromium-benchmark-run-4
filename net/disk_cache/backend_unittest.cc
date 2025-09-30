d3e0f2c8ea200e7ed4cd12da3200941c7bfc89b4
Olivier Li <olivierli@chromium.org>
2025-09-30 12:33:41 -0700

    Re-enable DiskCacheBackendTest.SimpleCacheNegMaxSize
    The test, like others in the file needs to wait for the threadpool
    tasks to have executed to get results.
    
    Bug: 430656242
    Change-Id: I180ec496d4164660ad3b78839fca0bce021f643e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6986104
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Commit-Queue: Olivier Li <olivierli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1523075}

--- net/disk_cache/backend_unittest.cc
+++ net/disk_cache/backend_unittest.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/task/thread_pool.h"
+#include "base/task/thread_pool/thread_pool_instance.h"
 #include "base/test/bind.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
@@ -4307,13 +4308,7 @@ TEST_F(DiskCacheBackendTest, SimpleCacheLateDoom) {
             simple_cache_impl_->index()->init_method());
 }
 
-// TODO(crbug.com/430656242): Flaky on Android.
-#if BUILDFLAG(IS_ANDROID)
-#define MAYBE_SimpleCacheNegMaxSize DISABLED_SimpleCacheNegMaxSize
-#else
-#define MAYBE_SimpleCacheNegMaxSize SimpleCacheNegMaxSize
-#endif
-TEST_F(DiskCacheBackendTest, MAYBE_SimpleCacheNegMaxSize) {
+TEST_F(DiskCacheBackendTest, SimpleCacheNegMaxSize) {
   SetCacheType(net::GENERATED_BYTE_CODE_CACHE);
 
   SetMaxSize(-1);
@@ -4346,6 +4341,9 @@ TEST_F(DiskCacheBackendTest, MAYBE_SimpleCacheNegMaxSize) {
         field_trial_params);
 
     InitCache();
+
+    // Wait for tasks on which the init depend to have executed.
+    base::ThreadPoolInstance::Get()->FlushForTesting();
 
     uint64_t max_size_scaled = simple_cache_impl_->index()->max_size();
     uint64_t max_file_size_scaled = simple_cache_impl_->MaxFileSize();
