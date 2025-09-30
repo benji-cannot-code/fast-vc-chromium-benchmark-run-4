085270c1587ee6239383929ad6372a94f47ec014
Tomasz Moniuszko <tmoniuszko@opera.com>
2025-09-30 12:29:08 -0700

    Add missing build/build_config.h include
    IWYU fix. Required for BUILDFLAG(IS_CHROMEOS) and BUILDFLAG(IS_LINUX).
    Fixes compilation error in some build configurations.
    
    Bug: None
    Change-Id: Ie9854a302ec368f24699293b701c3405827678ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6999930
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Auto-Submit: Tomasz Moniuszko <tmoniuszko@opera.com>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1523069}

--- chrome/browser/ui/actions/chrome_action_id.h
+++ chrome/browser/ui/actions/chrome_action_id.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_ACTIONS_CHROME_ACTION_ID_H_
 
 #include "build/branding_buildflags.h"
+#include "build/build_config.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "ui/actions/action_id.h"
 
