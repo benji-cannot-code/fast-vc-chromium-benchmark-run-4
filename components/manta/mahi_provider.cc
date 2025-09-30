e62790ec590bd2434aa2565ba29d49320c974578
Yuki Awano <yawano@google.com>
2025-09-30 11:01:44 -0700

    hmr: add precondition checks
    - Checks preconditions before making a request for HMR.
    
    Fixed: b:446036490
    Test: Manually tested. No crash with using HMR.
    Change-Id: I1f7c3288fc29a0a4e9253a38758829cf9ac9103f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6977822
    Reviewed-by: Andrew Moylan <amoylan@google.com>
    Commit-Queue: Yuki Awano <yawano@google.com>
    Cr-Commit-Position: refs/heads/main@{#1523008}

--- components/manta/mahi_provider.cc
+++ components/manta/mahi_provider.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/values.h"
 #include "base/version_info/channel.h"
+#include "chromeos/components/magic_boost/public/cpp/magic_boost_state.h"
 #include "components/endpoint_fetcher/endpoint_fetcher.h"
 #include "components/manta/base_provider.h"
 #include "components/manta/features.h"
@@ -144,6 +145,8 @@ void MahiProvider::Summarize(const std::string& input,
                              const std::optional<std::string>& context,
                              const std::optional<std::string>& url,
                              MantaGenericCallback done_callback) {
+  chromeos::MagicBoostState::AssertPreconditionsOfHelpMeReadOrCrash();
+
   proto::Request request;
   request.set_feature_name(proto::FeatureName::CHROMEOS_READER_SUMMARY);
 
@@ -185,6 +188,8 @@ void MahiProvider::Elucidate(const std::string& input,
                              const std::string& title,
                              const std::optional<std::string>& url,
                              MantaGenericCallback done_callback) {
+  chromeos::MagicBoostState::AssertPreconditionsOfHelpMeReadOrCrash();
+
   proto::Request request;
   request.set_feature_name(proto::FeatureName::CHROMEOS_READER_ELUCIDATION);
 
@@ -234,6 +239,8 @@ void MahiProvider::QuestionAndAnswer(const std::string& original_content,
                                      const std::vector<MahiQAPair> QAHistory,
                                      const std::string& question,
                                      MantaGenericCallback done_callback) {
+  chromeos::MagicBoostState::AssertPreconditionsOfHelpMeReadOrCrash();
+
   proto::Request request;
   request.set_feature_name(proto::FeatureName::CHROMEOS_READER_Q_AND_A);
 
