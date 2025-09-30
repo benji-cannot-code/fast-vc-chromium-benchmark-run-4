cef72c96bc78469ddde319e68527889e977cafe6
Mike Wittman <wittman@chromium.org>
2025-09-30 14:55:14 -0700

    flags: remove enable-download-service-foreground-session
    This removes the flag and retains the default behavior.
    
    Fixed: 40273337
    Change-Id: I35647cbcb09bb6fbb7246e46c3bb168c02c8a1ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7001110
    Auto-Submit: Mike Wittman <wittman@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1523165}

--- components/download/public/background_service/features.cc
+++ components/download/public/background_service/features.cc
@@ -5,20 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/download/public/background_service/features.h"
 
-#include "build/build_config.h"
-
 namespace download {
 
 BASE_FEATURE(kDownloadServiceFeature,
              "DownloadService",
              base::FEATURE_DISABLED_BY_DEFAULT);
-
-BASE_FEATURE(kDownloadServiceForegroundSessionIOSFeature,
-#if BUILDFLAG(IS_IOS)
-             base::FEATURE_ENABLED_BY_DEFAULT
-#else
-             base::FEATURE_DISABLED_BY_DEFAULT
-#endif
-);
 
 }  // namespace download
