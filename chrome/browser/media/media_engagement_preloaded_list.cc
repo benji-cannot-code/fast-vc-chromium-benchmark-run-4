71b98941bae08bc37e0d53136404c735d512380d
Jan Keitel <jkeitel@google.com>
2025-09-30 01:18:31 -0700

    Resolve UNSAFE_TODO in media_engagement_preloaded_list.cc
    No functionality changes.
    
    Bug: 435317390
    Change-Id: Ibd3568814eef018d6ec4ec33bb79624680554087
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6994203
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1522721}

--- chrome/browser/media/media_engagement_preloaded_list.cc
+++ chrome/browser/media/media_engagement_preloaded_list.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstdint>
 
 #include "base/compiler_specific.h"
+#include "base/containers/span.h"
+#include "base/containers/to_vector.h"
 #include "base/files/file_util.h"
 #include "base/no_destructor.h"
 #include "base/path_service.h"
@@ -64,10 +66,7 @@ bool MediaEngagementPreloadedList::LoadFromFile(const base::FilePath& path) {
   }
 
   // Copy data from the protobuf message.
-  dafsa_ = std::vector<uint8_t>(
-      message.dafsa().c_str(),
-      UNSAFE_TODO(message.dafsa().c_str() + message.dafsa().length()));
-
+  dafsa_ = base::ToVector(base::as_byte_span(message.dafsa()));
   is_loaded_ = true;
   return true;
 }
