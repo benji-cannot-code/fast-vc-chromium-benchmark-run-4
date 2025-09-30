7b62fb9b0263099ef4f80cdbc12de65cba4ca973
Radu Nitescu <radunitescu@google.com>
2025-09-30 03:24:17 -0700

    [iOS] Extract Lens side panel and generalize the class
    This is a mechanical move, in preparation for the custom bottom sheet
    presentation, the side panel is extracted and refactored in a separate
    class to be reused in a subsequent CL.
    
    Change-Id: I13142c1c59aef121202a1d8885ffe35d53d5ffc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6998467
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1522760}

---
+++ ios/chrome/browser/lens_overlay/ui/lens_overlay_panel.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_LENS_OVERLAY_UI_LENS_OVERLAY_PANEL_H_
+#define IOS_CHROME_BROWSER_LENS_OVERLAY_UI_LENS_OVERLAY_PANEL_H_
+
+#import <UIKit/UIKit.h>
+
+// A general purpose presentation panel for Lens Overlay.
+// Can be used either as a side panel or a bottom sheet.
+@interface LensOverlayPanel : UIViewController
+
+// Creates a new instance wrapping the given content view controller.
+- (instancetype)initWithContent:(UIViewController*)contentViewController
+                   insetContent:(BOOL)insetContent;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_LENS_OVERLAY_UI_LENS_OVERLAY_PANEL_H_
