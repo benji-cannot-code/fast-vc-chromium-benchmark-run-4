d315c89ddd892e67cc90b4349c1e73117b870587
Dominic Austria <dominicaustria@google.com>
2025-09-30 12:56:15 -0700

    Revise tab grouping transition histogram
    We revise the Tab.GroupingTransition because of data mixing after relabeling
    buckets, caused by an earlier CL linked below.
    https://chromium-review.googlesource.com/c/chromium/src/+/6934776
    
    OBSOLETE_HISTOGRAM[Tab.GroupingTransition]=Replaced by Tab.GroupingTransition2
    
    Bug: 448139551
    Change-Id: I163088e1b38f65733a12bcecf2e762c62309b71f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6996248
    Reviewed-by: Alison Gale <agale@chromium.org>
    Commit-Queue: Dominic Austria <dominicaustria@google.com>
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1523097}

--- chrome/browser/ui/tabs/tab_creation_metrics_controller_browsertest.cc
+++ chrome/browser/ui/tabs/tab_creation_metrics_controller_browsertest.cc
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace tabs {
 
-constexpr char kHistogramName[] = "Tab.GroupingTransition";
+constexpr char kHistogramName[] = "Tab.GroupingTransition2";
 
 class TabCreationMetricsTest : public InProcessBrowserTest {
  public:
