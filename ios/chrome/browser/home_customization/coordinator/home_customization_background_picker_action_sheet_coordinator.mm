1b9e827af8e4afa36118b6dd24c8ec1845d7b782
Cheick Cisse <cheickcisse@google.com>
2025-09-30 08:19:25 -0700

    [iOS] Fix crash when dismissing customization action picker
    This CL fixes a crash in
    HomeCustomizationBackgroundPickerActionSheetCoordinator where we call
    NOTREACHED() when HomeCustomizationBackgroundStyle::kDefault on dismiss
    that was wrong because if the current background is default and user
    did not make a selection this will stay kDefault.
    
    Bug: 448353243
    Change-Id: I247ff2912052fb7231b63b8e943564ab86a71c41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6999490
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Cr-Commit-Position: refs/heads/main@{#1522875}

--- ios/chrome/browser/home_customization/coordinator/home_customization_background_picker_action_sheet_coordinator.mm
+++ ios/chrome/browser/home_customization/coordinator/home_customization_background_picker_action_sheet_coordinator.mm
@@ -364,7 +364,7 @@ CGFloat const kSheetCornerRadius = 30;
           _backgroundConfigurationMediator.backgroundSelectionOutcome);
       break;
     case HomeCustomizationBackgroundStyle::kDefault:
-      NOTREACHED();
+      return;
   }
 }
 
