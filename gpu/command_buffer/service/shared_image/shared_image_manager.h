403093a9d0bb8d5e7fa65872f1688b47d7812069
Colin Blundell <blundell@chromium.org>
2025-09-29 23:51:54 -0700

    [//gpu] Call IOSurfaceImageBackingFactory for GMBHandle creation on Mac
    Changes SharedImageFactory from calling into GMBFactory to create
    GMBHandles on Mac, instead having it call IOSurfaceImageBackingFactory.
    The former calls through to the latter [1].
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/6988611
    
    Bug: 404905709
    Change-Id: Ic564a31738e6fc1dea1b6d28b7540fc4791f5ee9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6994238
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1522685}

--- gpu/command_buffer/service/shared_image/shared_image_manager.h
+++ gpu/command_buffer/service/shared_image/shared_image_manager.h
@@ -158,9 +158,11 @@ class GPU_GLES2_EXPORT SharedImageManager
     return display_context_on_another_thread_;
   }
 
+#if !BUILDFLAG(IS_MAC)
   GpuMemoryBufferFactory* gpu_memory_buffer_factory() {
     return gpu_memory_buffer_factory_.get();
   }
+#endif
 
   bool SupportsScanoutImages();
 
@@ -202,7 +204,9 @@ class GPU_GLES2_EXPORT SharedImageManager
   bool supports_overlays_on_ozone_ = false;
 #endif
 
+#if !BUILDFLAG(IS_MAC)
   std::unique_ptr<GpuMemoryBufferFactory> gpu_memory_buffer_factory_;
+#endif
 
   THREAD_CHECKER(thread_checker_);
 };
