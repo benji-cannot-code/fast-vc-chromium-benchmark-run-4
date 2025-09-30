fdf1a565d8a691091ac9ae8be116331bc0a8217e
Abe Boujane <boujane@google.com>
2025-09-30 12:23:00 -0700

    move html code to dedicated file for actor overlay
    Bug: 447114657
    Change-Id: I1de32fe844d36d3fa47869aba9e4e478cee75244
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7000473
    Auto-Submit: Abe Boujane <boujane@google.com>
    Reviewed-by: Jacob Stanley <jacobstanley@google.com>
    Commit-Queue: Jacob Stanley <jacobstanley@google.com>
    Commit-Queue: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1523058}

---
+++ chrome/browser/resources/actor_overlay/app.html.ts
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {ActorOverlayAppElement} from './app.js';
+
+export function getHtml(this: ActorOverlayAppElement) {
+  // clang-format off
+  return html`<!--_html_template_start_-->
+<div id="magicCursor"></div>
+<!--_html_template_end_-->`;
+  // clang-format on
+}
