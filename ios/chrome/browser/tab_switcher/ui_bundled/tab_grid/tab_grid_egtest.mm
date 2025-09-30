4c185cf7db963b9120d0cb5e8b8dfd5d0e473f7f
Scott Yoder <scottyoder@google.com>
2025-09-30 11:22:27 -0700

    [iOS] Disable testOpenSearchedTabFromAnotherWindowWhenTabGridIsVisible
    Bug: 448400563
    Change-Id: If02622eae9eaacd4eaa0b88907b2cf2bc71fc90f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6999309
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Owners-Override: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1523025}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_grid_egtest.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_grid_egtest.mm
@@ -2414,7 +2414,8 @@ void EchoURLDefaultSearchEngineResponseProvider::GetResponseHeadersAndBody(
 
 // Ensures that when users tap on a tab from tab search result and this tab is
 // in another window currently displaying tab grid, the tab is opened.
-- (void)testOpenSearchedTabFromAnotherWindowWhenTabGridIsVisible {
+// TODO(crbug.com/448400563): Re-enable this test.
+- (void)DISABLED_testOpenSearchedTabFromAnotherWindowWhenTabGridIsVisible {
   if (![ChromeEarlGrey areMultipleWindowsSupported]) {
     EARL_GREY_TEST_DISABLED(@"Multiple windows can't be opened.");
   }
