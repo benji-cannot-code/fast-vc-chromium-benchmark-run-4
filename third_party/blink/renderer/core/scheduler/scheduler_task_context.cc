d2a0efcc22bf393b1d535e6864237b9d6aae9e4e
Scott Haseley <shaseley@chromium.org>
2025-09-30 08:43:26 -0700

    Scheduling APIs: Clean up SchedulerYieldDisallowCrossFrameInheritance
    The SchedulerYieldDisallowCrossFrameInheritance feature has been
    enabled by default since M138. This CL removes the flag since there
    weren't any issues with the change, and this is the behavior we
    settled on.
    
    Bug: 419066548
    Change-Id: I1739b24bf254f4b538743cb2a9ee450343584af1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6997527
    Reviewed-by: Michal Mocny <mmocny@chromium.org>
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1522888}

--- third_party/blink/renderer/core/scheduler/scheduler_task_context.cc
+++ third_party/blink/renderer/core/scheduler/scheduler_task_context.cc
@@ -5,12 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/scheduler/scheduler_task_context.h"
 
-#include "base/memory/scoped_refptr.h"
 #include "third_party/blink/renderer/core/dom/abort_signal.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/scheduler/dom_task_signal.h"
-#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
-#include "third_party/blink/renderer/platform/weborigin/security_origin.h"
 
 namespace blink {
 
@@ -19,8 +16,6 @@ SchedulerTaskContext::SchedulerTaskContext(ExecutionContext* context,
                                            DOMTaskSignal* priority_source)
     : abort_source_(abort_source),
       priority_source_(priority_source),
-      security_origin_(
-          base::WrapRefCounted(context->GetMutableSecurityOrigin())),
       scheduler_execution_context_(context) {}
 
 void SchedulerTaskContext::Trace(Visitor* visitor) const {
@@ -39,10 +34,7 @@ DOMTaskSignal* SchedulerTaskContext::PrioritySource() {
 
 bool SchedulerTaskContext::CanPropagateTo(
     const ExecutionContext& target) const {
-  return RuntimeEnabledFeatures::
-                 SchedulerYieldDisallowCrossFrameInheritanceEnabled()
-             ? &target == scheduler_execution_context_.Get()
-             : target.GetSecurityOrigin()->CanAccess(security_origin_.get());
+  return &target == scheduler_execution_context_.Get();
 }
 
 }  // namespace blink
