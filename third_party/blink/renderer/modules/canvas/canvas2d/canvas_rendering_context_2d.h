665b14455fa4cd7622bbe11fbda89a9cd266f7f7
Jean-Philippe Gravel <jpgravel@chromium.org>
2025-09-30 12:55:02 -0700

    Abort hibernation if the canvas is reset
    It used to be that the only way one could reset a canvas back to its
    original state was by changing its size. Page would often reset canvas
    by doing `canvas.width = canvas.width;`. Eventually, a more explicit
    `ctx.reset()` API was introduced. It remains that the two approaches are
    functionally identical: they both reset the canvas to its original
    state.
    
    If the canvas is reset while it's hibernated, hibernation has to be
    ended, or else, it would de-hibernate after the reset, restoring the
    image from before the reset. This was correctly handled for canvas
    resizes. It wasn't however done for calls to `reset()`. That's because
    the de-hibernation code was in CanvasRenderingContext2D::Reset(), which
    somehow is used for canvas resizes but not canvas resets. The fix is to
    move the logic to ResetInternal(), which is used for all cases.
    
    Fixing this can technically have a visible effect, which would manifest itself as a single glitched frame right after going back to a hidden page. This would only happen if hibernation is enabled, if the page calls reset only once while hibernating, and doesn't redraw over all the pixels from the previous frame. Hibernation is currently disabled, so this cannot happen right now. Even with hibernation enabled, this glitch
    would be very unlikely and definitely not something web-page would use
    as a "feature".
    
    Bug: 410843050
    Change-Id: Ibf59a47c6ca3ebabc17255486b73fcfdb5af5453
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6973988
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1523095}

--- third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.h
+++ third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.h
@@ -180,7 +180,6 @@ class MODULES_EXPORT CanvasRenderingContext2D final
   }
 
   // CanvasRenderingContext implementation
-  void Reset() override;
   bool IsComposited() const override;
   scoped_refptr<CanvasResource> PaintRenderingResultsToResource(
       SourceDrawingBuffer source_buffer,
@@ -306,6 +305,8 @@ class MODULES_EXPORT CanvasRenderingContext2D final
   friend class CanvasRenderingContext2DTestBase;
   FRIEND_TEST_ALL_PREFIXES(CanvasRenderingContext2DTestAccelerated,
                            PrepareMailboxWhenContextIsLostWithFailedRestore);
+
+  void ResetInternal() override;
 
   CanvasResourceProvider* GetResourceProvider() const override;
   void Dispose() override;
