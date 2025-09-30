221c463a3267677ac4284fe5d8d579cf5a89af1d
Nan Lin <linnan@chromium.org>
2025-09-30 14:43:51 -0700

    Actor: Make observation delay timeout a feature param
    Fixed: 447789076
    Change-Id: I0b6641d988b822ef31750bcf8ec1eff4dfabab06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7001362
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1523158}

--- chrome/common/chrome_features.h
+++ chrome/common/chrome_features.h
@@ -273,6 +273,8 @@ extern const base::FeatureParam<base::TimeDelta>(
 COMPONENT_EXPORT(CHROME_FEATURES)
 extern const base::FeatureParam<base::TimeDelta>(
     kGlicActorPageStabilityInvokeCallbackDelay);
+COMPONENT_EXPORT(CHROME_FEATURES)
+extern const base::FeatureParam<base::TimeDelta>(kActorObservationDelayTimeout);
 
 COMPONENT_EXPORT(CHROME_FEATURES)
 extern const base::FeatureParam<bool> kGlicActorScrollTargetIntoView;
