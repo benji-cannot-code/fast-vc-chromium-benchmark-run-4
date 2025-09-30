1e1f7410fe4800eabbb5713a6b98ed4ee4db4f9a
Tiffany Song <tiffanysong@google.com>
2025-09-30 07:21:27 -0700

    Stop filtering 404 navigations on iOS
    Previously, any 404 navigation would be filtered out and not be recorded
    to the history. Now, if the flag `history::kVisitedLinksOn404` is
    enabled, we will record 404 responses to history.
    
    Bug: 428922150
    Change-Id: I128a478dab6aaf67bbf8ff518380250b00d78417
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6986224
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Tiffany Song <tiffanysong@google.com>
    Reviewed-by: Svend Larsen <svend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1522842}

--- ios/chrome/browser/history/model/history_tab_helper.mm
+++ ios/chrome/browser/history/model/history_tab_helper.mm
@@ -5,8 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/history/model/history_tab_helper.h"
 
+#import "base/feature_list.h"
 #import "base/memory/ptr_util.h"
 #import "base/metrics/histogram_macros.h"
+#import "components/history/core/browser/features.h"
 #import "components/history/core/browser/history_constants.h"
 #import "components/history/core/browser/history_service.h"
 #import "components/history/core/browser/history_types.h"
@@ -234,7 +236,7 @@ void HistoryTabHelper::DidFinishNavigation(
     return;
   }
 
-  // Do not record failed navigation nor 404 to the history (to prevent them
+  // Do not record failed navigation to the history (to prevent them
   // from showing up as Most Visited tiles on NTP).
   UMA_HISTOGRAM_BOOLEAN("History.Is4XXOr5XXStatusCode",
                         navigation_context->GetError());
@@ -244,8 +246,13 @@ void HistoryTabHelper::DidFinishNavigation(
     return;
   }
 
+  // If `history::kVisitedLinksOn404` is enabled, record 404s in History.
+  const bool should_record_404 =
+      base::FeatureList::IsEnabled(history::kVisitedLinksOn404);
+
   if (navigation_context->GetResponseHeaders() &&
-      navigation_context->GetResponseHeaders()->response_code() == 404) {
+      navigation_context->GetResponseHeaders()->response_code() == 404 &&
+      !should_record_404) {
     return;
   }
 
