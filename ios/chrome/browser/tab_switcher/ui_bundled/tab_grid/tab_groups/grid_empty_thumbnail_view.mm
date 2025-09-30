57f4b5e942423e56c61bd396fa932eb365909ca8
Chris Lu <thegreenfrog@google.com>
2025-09-29 18:00:28 -0700

    [ios] Use smaller spacing in empty thumbnail when in GroupTabView
    For the EmptyThumbnailLayoutTypeCenteredPortrait layout when showing
    in the GroupTabView, the spacing should be smaller since the view is
    smaller.
    
    Screenshot: https://screenshot.googleplex.com/5BaYkB4XHXFuaLJ
    
    Bug: 443976867
    Change-Id: Ibbf2af49e20d9a48cb404589f3d630a203df7558
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6986183
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1522612}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/grid_empty_thumbnail_view.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/grid_empty_thumbnail_view.mm
@@ -80,7 +80,9 @@ const CGFloat kGridCellBarHeight = 22;
     case EmptyThumbnailLayoutTypeCenteredPortrait:
       [NSLayoutConstraint activateConstraints:_portraitCenteredConstraints];
       _stackView.alignment = UIStackViewAlignmentLeading;
-      _stackView.spacing = kLargeSpacingGroup;
+      _stackView.spacing = _type == EmptyThumbnailTypeGridCell
+                               ? kLargeSpacingGroup
+                               : kSmallVerticalSpacing;
       break;
     case EmptyThumbnailLayoutTypeLandscape:
       [NSLayoutConstraint activateConstraints:_landscapeConstraints];
