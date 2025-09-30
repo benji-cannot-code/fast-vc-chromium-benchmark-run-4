acee1dbe7d1a44beca2bdd7ae9c00d721a6eee68
Orko Garai <orko@igalia.com>
2025-09-30 02:56:35 -0700

    gamepad: Implement connected and disconnected WindowEventHandlers
    Implement Window.ongamepadconnected and Window.ongamepaddisconnected as
    per https://github.com/w3c/gamepad/pull/144.
    
    This fixes the gamepad idlharness.window.html interop wpt failures seen
    in chromium vs. other browsers.
    
    R=mattreynolds
    
    Bug: 40175074
    Change-Id: Ida8bf56dd8bdebf151704528db7d01ded04db706
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6984681
    Auto-Submit: Orko Garai <orko@igalia.com>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Commit-Queue: Javier Fernandez <jfernandez@igalia.com>
    Reviewed-by: Javier Fernandez <jfernandez@igalia.com>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1522755}

---
+++ third_party/blink/renderer/modules/gamepad/dom_window_gamepad.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_GAMEPAD_DOM_WINDOW_GAMEPAD_H_
+#define THIRD_PARTY_BLINK_RENDERER_MODULES_GAMEPAD_DOM_WINDOW_GAMEPAD_H_
+
+#include "third_party/blink/renderer/core/dom/events/event_target.h"
+#include "third_party/blink/renderer/core/event_type_names.h"
+
+namespace blink {
+
+class DOMWindowGamepad {
+ public:
+  DEFINE_STATIC_ATTRIBUTE_EVENT_LISTENER(gamepadconnected, kGamepadconnected)
+  DEFINE_STATIC_ATTRIBUTE_EVENT_LISTENER(gamepaddisconnected,
+                                         kGamepaddisconnected)
+};
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_MODULES_GAMEPAD_DOM_WINDOW_GAMEPAD_H_
