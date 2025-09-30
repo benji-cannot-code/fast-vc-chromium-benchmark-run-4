9d61cefc9995ed4542cdcfbcf2153159ad97a62b
Anton Bershanskyi <bershanskyi@gmail.com>
2025-09-29 21:50:18 -0700

    [Extensions] Consisent keys in {Get,Set}ManifestData()
    Use manifest_keys::* consistently in {Get,Set}ManifestData(). This has
    no behavior changes, just code refactor.
    
    Bug: 324534603
    Change-Id: I209e1bd0c4d873875b22725bb2afdfffef39b946
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6879023
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Anton Bershanskyi <bershanskyi@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1522658}

--- chrome/browser/background/background_contents_service_unittest.cc
+++ chrome/browser/background/background_contents_service_unittest.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/scoped_user_pref_update.h"
 #include "content/public/test/browser_task_environment.h"
 #include "extensions/common/extension.h"
+#include "extensions/common/manifest_handlers/icons_handler.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/platform_test.h"
 #include "ui/message_center/public/cpp/notification.h"
@@ -151,6 +152,10 @@ class BackgroundContentsServiceNotificationTest
 
   std::unique_ptr<NotificationDisplayServiceTester> display_service_;
   std::unique_ptr<BackgroundContentsService> background_service_;
+
+  bool HasIcons(scoped_refptr<extensions::Extension> extension) {
+    return !extensions::IconsInfo::GetIcons(extension.get()).empty();
+  }
 };
 
 TEST_F(BackgroundContentsServiceTest, Create) {
@@ -264,7 +269,7 @@ TEST_F(BackgroundContentsServiceNotificationTest, TestShowBalloon) {
   scoped_refptr<extensions::Extension> extension =
       extension_test_util::LoadManifest("image_loading_tracker", "app.json");
   ASSERT_TRUE(extension.get());
-  ASSERT_TRUE(extension->GetManifestData("icons"));
+  ASSERT_TRUE(HasIcons(extension));
 
   const message_center::Notification notification =
       CreateCrashNotification(extension);
@@ -275,7 +280,7 @@ TEST_F(BackgroundContentsServiceNotificationTest, TestShowBalloonShutdown) {
   scoped_refptr<extensions::Extension> extension =
       extension_test_util::LoadManifest("image_loading_tracker", "app.json");
   ASSERT_TRUE(extension.get());
-  ASSERT_TRUE(extension->GetManifestData("icons"));
+  ASSERT_TRUE(HasIcons(extension));
 
   std::string notification_id = BackgroundContentsService::
       GetNotificationDelegateIdForExtensionForTesting(extension->id());
@@ -296,7 +301,7 @@ TEST_F(BackgroundContentsServiceNotificationTest, TestShowBalloonNoIcon) {
   scoped_refptr<extensions::Extension> extension =
       extension_test_util::LoadManifest("app", "manifest.json");
   ASSERT_TRUE(extension.get());
-  ASSERT_FALSE(extension->GetManifestData("icons"));
+  ASSERT_FALSE(HasIcons(extension));
 
   const message_center::Notification notification =
       CreateCrashNotification(extension);
