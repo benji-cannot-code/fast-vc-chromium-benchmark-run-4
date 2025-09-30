e643166da4e7080efbc584724684385ba0b2eed7
Le Hoang Quyen <lehoangquyen@chromium.org>
2025-09-30 10:54:45 -0700

    gpu: Add UMA metrics for PersistentCache
    Add 2 new metrics for:
    - Detecting whether GpuPersistentCache is ready by the time Load/Store is called.
    - Calculating elapsed time in GpuPersistentCache::LoadData
    
    Bug: 399642827
    Change-Id: Ic84b2056e009818abc95ad5de8e5b3844fe66c75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6999728
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Auto-Submit: Quyen Le <lehoangquyen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1522996}

--- gpu/command_buffer/service/gpu_persistent_cache.h
+++ gpu/command_buffer/service/gpu_persistent_cache.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <dawn/platform/DawnPlatform.h>
 
 #include <memory>
+#include <string_view>
 
 #include "base/files/file.h"
 #include "base/memory/ref_counted.h"
@@ -27,7 +28,7 @@ namespace gpu {
 class GPU_GLES2_EXPORT GpuPersistentCache
     : public dawn::platform::CachingInterface {
  public:
-  GpuPersistentCache();
+  explicit GpuPersistentCache(std::string_view cache_prefix);
   ~GpuPersistentCache() override;
 
   GpuPersistentCache(const GpuPersistentCache&) = delete;
@@ -48,6 +49,11 @@ class GPU_GLES2_EXPORT GpuPersistentCache
                  size_t value_size) override;
 
  private:
+  std::string GetHistogramName(std::string_view metric) const;
+
+  // Prefix to prepend to UMA histogram's name. e.g GraphiteDawn, WebGPU
+  const std::string cache_prefix_;
+
   base::Lock lock_;
   std::unique_ptr<persistent_cache::PersistentCache> persistent_cache_
       GUARDED_BY(lock_);
