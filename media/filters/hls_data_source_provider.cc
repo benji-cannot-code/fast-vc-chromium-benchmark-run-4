a8026b1e00d63eae7a6bb676b886b01bebea8f04
weidongliu <weidongliu@chromium.org>
2025-09-29 19:10:42 -0700

    spanification: spanify //media/filters.
    Span some simple content in //media/filters that is not associated with
    too many files.
    
    Bug: 40285824
    Change-Id: Ie4d2f95e8be792dbf8cb4cae8a2aea56963e6cc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6982692
    Commit-Queue: Weidong Liu <weidongliu@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1522632}

--- media/filters/hls_data_source_provider.cc
+++ media/filters/hls_data_source_provider.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "media/filters/hls_data_source_provider.h"
 
 #include "base/trace_event/trace_event.h"
@@ -101,7 +96,7 @@ uint8_t* HlsDataSourceStream::LockStreamForWriting(int ensure_minimum_space) {
   if (ensure_minimum_space > remaining_bytes) {
     buffer_.resize(write_index_ + ensure_minimum_space);
   }
-  return buffer_.data() + write_index_;
+  return &buffer_[write_index_];
 }
 
 void HlsDataSourceStream::UnlockStreamPostWrite(int read_size,
