35dfde22bb44ef419cd2689d53e7677ac6b83962
Brian Begnoche <bcb@chromium.org>
2025-09-30 06:32:03 -0700

    Add filtering to only send a credit card form CSD ping on navs with low site engagement.
    Bug: 443098659
    Change-Id: I3358d827e6cb007b84e9cc69330b3d3d46375032
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6986201
    Commit-Queue: Brian Begnoche <bcb@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1522813}

--- components/safe_browsing/content/browser/client_side_detection_host.h
+++ components/safe_browsing/content/browser/client_side_detection_host.h
@@ -287,6 +287,9 @@ class ClientSideDetectionHost
   FRIEND_TEST_ALL_PREFIXES(
       ClientSideDetectionHostCreditCardFormTest,
       EventDoesNotTriggerPreclassificationChecksWhenESBDisabled);
+  FRIEND_TEST_ALL_PREFIXES(
+      ClientSideDetectionHostCreditCardFormTest,
+      CreditCardFormDoesNotStartPreclassificationOnHighSiteEngagement);
   FRIEND_TEST_ALL_PREFIXES(ClientSideDetectionHostCreditCardFormTest,
                            CreditCardFormTriggersPreclassificationCheck);
   FRIEND_TEST_ALL_PREFIXES(ClientSideDetectionHostCreditCardFormTest,
