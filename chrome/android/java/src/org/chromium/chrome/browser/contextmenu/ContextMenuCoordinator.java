230ebb8d47ff8c29dfa0fb3922e7d1c0f1b73293
Jenna Himawan <jhimawan@google.com>
2025-09-29 17:50:56 -0700

    Fix keyboard navigation for BasicListMenu
    Demo: https://screencast.googleplex.com/cast/NjQyMjcxMzk4Mjc3OTM5MnwzNjdhYTQxMy1iNw
    
    Demo 2: https://screencast.googleplex.com/cast/NjY3Njk3NjgwMTY3NzMxMnwyYjhjOWExZC00Mw
    
    Bug: 447191530
    Change-Id: I99804a310c6b7e18802110875ad19c66e6ac9ac2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6981273
    Reviewed-by: Neil Coronado <nemco@google.com>
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1522605}

--- chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ContextMenuCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ContextMenuCoordinator.java
@@ -360,7 +360,12 @@ public class ContextMenuCoordinator implements ContextMenuUi, FlyoutHandler<Cont
         mListViews.add(listView);
 
         listItems.addObserver(
-                new AccessibilityListObserver(listView, /* headerModelList= */ null, listItems));
+                new AccessibilityListObserver(
+                        listView,
+                        /* headerView= */ null,
+                        listView,
+                        /* headerModelList= */ null,
+                        listItems));
         mWebContentsObserver =
                 new WebContentsObserver(mWebContents) {
                     @Override
