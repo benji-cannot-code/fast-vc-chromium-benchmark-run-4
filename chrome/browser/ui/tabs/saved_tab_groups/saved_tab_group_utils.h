c7966486f8b8a35dc104e75f6da0e5affbea8b37
Yuheng Huang <yuhengh@chromium.org>
2025-09-30 16:48:18 -0700

    Add tab groups menu to Mac main menu.
    This CL adds a tab groups menu for Mac main menu. The menu is similar to the tab group menu in 3 dots menu -> tab groups. Also move some of the code out of SavedTabGroupTabMenuModel to shared between the new Mac main menu and the tab group menu.
    
    PRD:https://docs.google.com/document/d/1tpftvd1MQG4YpXWxHMzqstXVkambrFlQ2aPRxpz5e0o/edit?usp=sharing&resourcekey=0-XarwI_Hd8_UiUKnv9H11Lw
    Screenshot:https://screenshot.googleplex.com/dd8XzabNwaEHjJt
    
    Bug: 433807682
    Change-Id: I3a0294d6b4ef1de5611238b3b3a537ba0b053355
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6875963
    Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1523218}

--- chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_utils.h
+++ chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_utils.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/span.h"
 #include "base/uuid.h"
+#include "chrome/browser/ui/tabs/saved_tab_groups/tab_group_menu_action.h"
 #include "chrome/browser/ui/tabs/tab_group_deletion_dialog_controller.h"
 #include "components/data_sharing/public/group_data.h"
 #include "components/saved_tab_groups/public/saved_tab_group.h"
@@ -37,6 +38,7 @@ struct ActivityLogItem;
 namespace tab_groups {
 
 class SavedTabGroupTab;
+class TabGroupSyncService;
 
 enum class GroupDeletionReason {
   ClosedLastTab,
@@ -176,6 +178,10 @@ class SavedTabGroupUtils {
   // must exist.
   static tabs::TabInterface* GetGroupedTab(LocalTabGroupID group_id,
                                            LocalTabID tab_id);
+
+  static void PerformTabGroupMenuAction(const TabGroupMenuAction& action,
+                                        Browser* browser,
+                                        TabGroupSyncService* tab_group_service);
 };
 
 }  // namespace tab_groups
