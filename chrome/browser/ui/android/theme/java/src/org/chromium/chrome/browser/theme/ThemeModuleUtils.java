9017874de103084b2ec3bcb561edcc2bcdb8215f
Sinan Sahin <sinansahin@google.com>
2025-09-30 15:45:23 -0700

    Clean up upstream theme module code
    Bug: 419879223
    Change-Id: I6978cf46955eff76a5b644abc4ed3f9dd3da7fe0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7001600
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1523192}

--- chrome/browser/ui/android/theme/java/src/org/chromium/chrome/browser/theme/ThemeModuleUtils.java
+++ chrome/browser/ui/android/theme/java/src/org/chromium/chrome/browser/theme/ThemeModuleUtils.java
@@ -5,18 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.theme;
 
-import org.chromium.base.ResettersForTesting;
-import org.chromium.base.ServiceLoaderUtil;
 import org.chromium.build.annotations.NullMarked;
-import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 
 /** Utility class for interactions with ThemeOverlayProvider. */
 @NullMarked
 public final class ThemeModuleUtils {
-
-    private static @Nullable ThemeOverlayProvider sInstance;
-
     private ThemeModuleUtils() {}
 
     /** Returns whether theme module is enabled. */
@@ -27,33 +21,5 @@ public final class ThemeModuleUtils {
     /** Returns whether enable all the dependency features. */
     public static boolean isForceEnableDependencies() {
         return isEnabled() && ChromeFeatureList.sAndroidThemeModuleForceDependencies.getValue();
-    }
-
-    /**
-     * Get the theme module to provide the overlay. This will be a theme resource used for {@link
-     * android.content.res.Resources.Theme#applyStyle(int, boolean)}.
-     */
-    public static ThemeOverlayProvider getProviderInstance() {
-        if (sInstance == null) {
-            sInstance = ServiceLoaderUtil.maybeCreate(ThemeOverlayProvider.class);
-            if (sInstance == null) {
-                sInstance = new EmptyThemeOverlayProvider();
-            }
-        }
-        return sInstance;
-    }
-
-    /** Override the provider instance with a test object. */
-    public static void setProviderInstanceForTesting(@Nullable ThemeOverlayProvider provider) {
-        ThemeOverlayProvider instance = sInstance;
-        sInstance = provider;
-        ResettersForTesting.register(() -> sInstance = instance);
-    }
-
-    private static class EmptyThemeOverlayProvider implements ThemeOverlayProvider {
-        @Override
-        public int getThemeOverlay() {
-            return 0;
-        }
     }
 }
