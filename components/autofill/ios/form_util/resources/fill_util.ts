1d210f9b2ca45c79077481414741cd33b56352ed
Alexandra Pereira <alexandrasp@google.com>
2025-09-30 14:57:42 -0700

    [iOS][autofill] Make getCanonicalActionForForm a utils function
    This refactoring improves code clarity and simplifies the adoption of
    the `gCrWeb` class by isolating internal utility functions and removing
    them from the __gCrWeb API.
    
    Bug: 399666983
    Change-Id: Ib69db14435c55a0a20d9267476171071eda6026d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6997387
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Commit-Queue: Alexandra Pereira <alexandrasp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1523169}

--- components/autofill/ios/form_util/resources/fill_util.ts
+++ components/autofill/ios/form_util/resources/fill_util.ts
@@ -491,12 +491,12 @@ function absoluteURL(doc: Document, relativeURL: string): string {
  * function GetCanonicalActionForForm.
  * @return Canonical action.
  */
-gCrWebLegacy.fill.getCanonicalActionForForm = function(
+function getCanonicalActionForForm(
     formElement: HTMLFormElement): string {
   const rawAction = formElement.getAttribute('action') || '';
   const absoluteUrl = absoluteURL(formElement.ownerDocument, rawAction);
   return removeQueryAndReferenceFromURL(absoluteUrl);
-};
+}
 
 declare interface OptionFieldStrings {
     option_values: string[] & {toJSON?: string|null};
@@ -778,4 +778,5 @@ export {
   shouldAutocomplete,
   getAriaLabel,
   getAriaDescription,
+  getCanonicalActionForForm,
 };
--- components/autofill/ios/form_util/resources/fill_util_test.ts
+++ components/autofill/ios/form_util/resources/fill_util_test.ts
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {CrWebApi, gCrWeb} from '//ios/web/public/js_messaging/resources/gcrweb.js';
-import {getAriaDescription, getAriaLabel, getUniqueID, shouldAutocomplete} from '//components/autofill/ios/form_util/resources/fill_util.js';
+import {getAriaDescription, getAriaLabel, getCanonicalActionForForm, getUniqueID, shouldAutocomplete} from '//components/autofill/ios/form_util/resources/fill_util.js';
 
 /**
 * @fileoverview Registers a testing-only `CrWebApi` to expose fill utils
@@ -17,5 +17,6 @@ fillApi.addFunction('getUniqueID', getUniqueID);
 fillApi.addFunction('shouldAutocomplete', shouldAutocomplete);
 fillApi.addFunction('getAriaLabel', getAriaLabel);
 fillApi.addFunction('getAriaDescription', getAriaDescription);
+fillApi.addFunction('getCanonicalActionForForm', getCanonicalActionForForm);
 
 gCrWeb.registerApi('fill_test_api', fillApi);
