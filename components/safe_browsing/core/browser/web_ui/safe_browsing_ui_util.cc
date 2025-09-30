38080da093169bfc5157655944e2bc3c0c5ac06c
Shanthanu Bhardwaj <xanth@google.com>
2025-09-30 08:23:12 -0700

    Simplify the Reporting Events tab UI on chrome://safe-browsing
    - Replace the freeform JSON with a compact expandable table with 1 row per
    event
    - The events can be expanded to display the full event JSON
    - Add some CSS for the reporting events table
    - New UI: https://screencast.googleplex.com/api/gif/NjE1ODUyOTY4ODc2NDQxNnxjYTEzZjc4MS0wNQ/image
    - Previous UI: https://screenshot.googleplex.com/bPVWzEsKAwNrpzu
    
    Change-Id: I1470829222cce75c7dfffa1061a1b31f976cf491
    Bug: 394403142
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6922751
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Shanthanu Bhardwaj <xanth@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1522877}

--- components/safe_browsing/core/browser/web_ui/safe_browsing_ui_util.cc
+++ components/safe_browsing/core/browser/web_ui/safe_browsing_ui_util.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/time_formatting.h"
 #include "base/json/json_writer.h"
 #include "base/strings/utf_string_conversions.h"
+#include "components/enterprise/connectors/core/reporting_constants.h"
 #include "components/safe_browsing/core/browser/referring_app_info.h"
 #include "components/safe_browsing/core/common/proto/csd.to_value.h"
 #include "components/safe_browsing/core/common/proto/realtimeapi.to_value.h"
@@ -513,8 +514,18 @@ base::Value::Dict SerializeUploadEventsRequest(
                              upload_events_request));
 #endif
   message.Set("response", result.Clone());
+
   base::Value::Dict wrapper;
   wrapper.Set("message", SerializeJson(message));
+  auto& event = upload_events_request.events()[0];
+  wrapper.Set("timeMillis",
+              event.time().seconds() * 1000 + event.time().nanos() / 1000000.0);
+  wrapper.Set("event_type",
+              enterprise_connectors::GetEventName(event.event_case()));
+  wrapper.Set("profile", upload_events_request.has_profile());
+  wrapper.Set("device", upload_events_request.has_device());
+  wrapper.Set("success",
+              result.FindBool("uploaded_successfully").value_or(false));
   return wrapper;
 }
 
