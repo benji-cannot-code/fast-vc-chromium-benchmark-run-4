a9c9edad60601ce4e7e946e52a246a0d8f08daad
Aldo Culquicondor <acondor@chromium.org>
2025-10-01 15:28:06 -0700

    Enable tab strip UI optimizations by default
    This change enables the `MvcUpdateViewWhenModelChanged` and
    `ReloadTabUiResourcesIfChanged` features by default on Android. The
    corresponding flag descriptions are removed.
    
    Fixed: 442564652,442547361
    Bug: 416752339
    Change-Id: Id2fd73a17dd928e8b631b1964bb4d2d770693f2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7003669
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Auto-Submit: Aldo Culquicondor <acondor@chromium.org>
    Commit-Queue: Aldo Culquicondor <acondor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1523879}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -917,7 +917,7 @@ BASE_FEATURE(kMostVisitedTilesReselect, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kMultiInstanceApplicationStatusCleanup,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-BASE_FEATURE(kMvcUpdateViewWhenModelChanged, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kMvcUpdateViewWhenModelChanged, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kNativePageTransitionHardwareCapture,
              base::FEATURE_ENABLED_BY_DEFAULT);
@@ -1002,7 +1002,7 @@ BASE_FEATURE(kRelatedSearchesAllLanguage, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kRelatedSearchesSwitch, base::FEATURE_ENABLED_BY_DEFAULT);
 
-BASE_FEATURE(kReloadTabUiResourcesIfChanged, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kReloadTabUiResourcesIfChanged, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kRemoveTabFocusOnShowingAndSelect,
              base::FEATURE_ENABLED_BY_DEFAULT);
