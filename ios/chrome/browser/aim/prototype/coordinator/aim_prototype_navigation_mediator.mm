745c1bb468baf6e280b10bd5fe7dca09e0388547
Christian Xu <christianxu@google.com>
2025-10-01 02:34:48 -0700

    [iOS][AIM] Fix AIM loading navigation in browser
    Fix AIM not dismissing when loading a navigation in the browser.
    
    Bug: None
    Change-Id: I01c55a781bb598c11ae668033fe1c9c1ec9b8bf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6999515
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1523432}

--- ios/chrome/browser/aim/prototype/coordinator/aim_prototype_navigation_mediator.mm
+++ ios/chrome/browser/aim/prototype/coordinator/aim_prototype_navigation_mediator.mm
@@ -158,7 +158,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // performed, for example. This hack postpones the dismiss action.
   __weak __typeof(self) weakSelf = self;
   base::OnceClosure completion = base::BindOnce(^{
-    [weakSelf dismissAIMPrototype];
+    [weakSelf.delegate dismissAIMPrototype];
   });
   constexpr base::TimeDelta kDelay = base::Seconds(0.5);
   base::SequencedTaskRunner::GetCurrentDefault()->PostDelayedTask(
