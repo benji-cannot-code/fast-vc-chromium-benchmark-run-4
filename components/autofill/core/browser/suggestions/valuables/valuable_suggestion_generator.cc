714767e93d83d763c4134d8802de32e14383a953
Norge Vizcay <vizcay@google.com>
2025-10-01 09:39:40 -0700

    Use the general Wallet valuables IPH for loyalty cards.
    A new and more generic IPH is being introduced for Wallet passes shown
    via autofill ai suggestions. With this CL, the new IPH will replace the
    one that was put in place for loyalty cards.
    
    Bug: 441736482
    Change-Id: I33c4b0afc426f4b2fdedb2a58648c3f6b469e867
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7003120
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Luchen Peng <luchenpeng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1523638}

--- components/autofill/core/browser/suggestions/valuables/valuable_suggestion_generator.cc
+++ components/autofill/core/browser/suggestions/valuables/valuable_suggestion_generator.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/suggestions/suggestion.h"
 #include "components/autofill/core/browser/suggestions/suggestion_generator.h"
 #include "components/autofill/core/browser/suggestions/suggestion_type.h"
+#include "components/autofill/core/common/autofill_features.h"
 #include "components/feature_engagement/public/feature_constants.h"
 #include "components/strings/grit/components_strings.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -25,6 +26,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 namespace {
+
+// Returns true if any of the features that use wallet public passes are
+// enabled.
+bool WalletPublicPassesEnabled() {
+  return base::FeatureList::IsEnabled(
+             features::kAutofillAiWalletVehicleRegistration) ||
+         base::FeatureList::IsEnabled(
+             features::kAutofillAiWalletFlightReservation);
+}
 
 // Creates a fallback icon used when there is no logo for loyalty card program.
 // The icon consists of the first letter of the merchant name.
@@ -103,8 +113,13 @@ Suggestion CreateLoyaltyCardSuggestion(
   suggestion.payload = Suggestion::Guid(loyalty_card.id().value());
   SetLoyaltyCardIconURL(suggestion, loyalty_card.program_logo(),
                         valuables_manager, loyalty_card.merchant_name());
-  suggestion.iph_metadata = Suggestion::IPHMetadata(
-      &feature_engagement::kIPHAutofillEnableLoyaltyCardsFeature);
+  if (WalletPublicPassesEnabled()) {
+    suggestion.iph_metadata = Suggestion::IPHMetadata(
+        &feature_engagement::kIPHAutofillAiValuablesFeature);
+  } else {
+    suggestion.iph_metadata = Suggestion::IPHMetadata(
+        &feature_engagement::kIPHAutofillEnableLoyaltyCardsFeature);
+  }
   return suggestion;
 }
 
