ea692ca15b2a9dd1e54d4c2f74f919bf47662388
Colin Blundell <blundell@chromium.org>
2025-10-01 00:01:23 -0700

    [//gpu] Remove ClientDiscardableTextureManager
    Unused post-OOP-C.
    
    Bug: 391648152
    Change-Id: I5cb0b64739ac045b40b6bd81fe6ff55ee174aaa5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6999667
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1523349}

--- gpu/command_buffer/client/client_discardable_manager.h
+++ gpu/command_buffer/client/client_discardable_manager.h
@@ -16,9 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gpu {
 
-// ClientDiscardableManager is a helper class used by the
-// ClientDiscardableTextureManager. It allows for the creation and management
-// of ClientDiscardableHandles.
+// ClientDiscardableManager is a helper class used by the client side of the
+// transfer cache. It allows for the creation and management of
+// ClientDiscardableHandles.
 class GPU_COMMAND_BUFFER_CLIENT_EXPORT ClientDiscardableManager {
  public:
   ClientDiscardableManager();
