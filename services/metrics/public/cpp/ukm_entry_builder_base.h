b3cabff6016d1356b32d5a8349d1d4eddf9f6684
Yuki Shiino <yukishiino@chromium.org>
2025-10-01 00:52:23 -0700

    ELUD: Add ADVANCED_MEMORY_SAFETY_CHECKS to UkmEntryBuilderBase
    Adds ADVANCED_MEMORY_SAFETY_CHECKS to UkmEntryBuilderBase as a
    mitigation of crbug.com/433395880
    
    Bug: 433395880
    Change-Id: Ic732048183d8bf9b75c4c7cf4edf77e04507597f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6998730
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Mikihito Matsuura <mikt@google.com>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1523377}

--- services/metrics/public/cpp/ukm_entry_builder_base.h
+++ services/metrics/public/cpp/ukm_entry_builder_base.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SERVICES_METRICS_PUBLIC_CPP_UKM_ENTRY_BUILDER_BASE_H_
 #define SERVICES_METRICS_PUBLIC_CPP_UKM_ENTRY_BUILDER_BASE_H_
 
+#include "base/memory/safety_checks.h"
 #include "services/metrics/public/cpp/metrics_export.h"
 #include "services/metrics/public/cpp/ukm_recorder.h"
 #include "services/metrics/public/cpp/ukm_source_id.h"
@@ -16,6 +17,9 @@ namespace ukm::internal {
 // A base class for generated UkmEntry builder objects.
 // This class should not be used directly.
 class METRICS_EXPORT UkmEntryBuilderBase {
+  // TODO(crbug.com/433395880): Remove this macro once it gets fixed.
+  ADVANCED_MEMORY_SAFETY_CHECKS();
+
  public:
   UkmEntryBuilderBase(const UkmEntryBuilderBase&) = delete;
   UkmEntryBuilderBase(UkmEntryBuilderBase&&);
