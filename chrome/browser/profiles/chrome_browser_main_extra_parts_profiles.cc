91fd0f4e8a8d08ad38ba6482a920158c5b8c295f
Hira Mahmood <hiramahmood@google.com>
2025-10-01 09:33:57 -0700

    Introduce IOSPromoTriggerService for decoupled promo requests
    This CL introduces IOSPromoTriggerService, a new KeyedService that acts
    as a central hub for triggering "desktop-to-ios" promos.
    
    This provides a single, decoupled pathway for any UI component (e.g.,
    WebUI like Safety Hub, or native UI like the Lens Overlay) to request
    that a promo be shown, without needing knowledge of the presentation
    logic.
    
    Subsequent CLs will add the UI controller that listens for these
    requests and the callers that make them.
    
    go/xplat-growth-promos-design for more context
    
    Bug: 444266197
    Low-Coverage-Reason: OTHER factory is boilerplate/trivial
    Change-Id: I09c6b5c04294e7e234618ffcce6d86745f19206f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6926240
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1523633}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -363,6 +363,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/lens/lens_keyed_service_factory.h"
 #include "chrome/browser/ui/media_router/media_router_ui_service_factory.h"
 #include "chrome/browser/ui/performance_controls/performance_controls_hats_service_factory.h"
+#include "chrome/browser/ui/promos/ios_promo_trigger_service_factory.h"
 #include "chrome/browser/ui/safety_hub/password_status_check_service_factory.h"
 #include "chrome/browser/ui/safety_hub/safety_hub_hats_service_factory.h"
 #include "chrome/browser/ui/tabs/organization/tab_organization_service_factory.h"
@@ -1004,6 +1005,9 @@ void ChromeBrowserMainExtraPartsProfiles::
   visited_url_ranking::VisitedURLRankingServiceFactory::GetInstance();
 #if !BUILDFLAG(IS_ANDROID)
   InstantServiceFactory::GetInstance();
+#endif
+#if !BUILDFLAG(IS_ANDROID)
+  IOSPromoTriggerServiceFactory::GetInstance();
 #endif
   IpProtectionCoreHostFactory::GetInstance();
 #if BUILDFLAG(IS_WIN)
