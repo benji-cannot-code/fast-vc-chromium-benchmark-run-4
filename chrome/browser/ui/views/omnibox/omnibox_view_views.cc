74553661525f0b05267b1f5bb79296b0922cdcad
Justin Donnelly <jdonnelly@chromium.org>
2025-10-01 15:41:19 -0700

    [omnibox] Restrict AIM entrypoint enabling to en/US.
    In https://crrev.com/c/6997238, I mistakenly default enabled
    `kAiModeOmniboxEntryPoint`, which would launch the feature for all
    users, despite the fact that it has only been tested in en/US so far.
    This change switches that Feature back to default disabled, introduces a
    new default-enabled Feature, `kAiModeOmniboxEntryPointEnUs`, and uses
    that to enable the entrypoint for en/US users by passing it as the
    optional param to
    `AimEligibilityService::GenericKillSwitchFeatureCheck`.
    
    Additionally, this fixes an issue where
    `OmniboxViewViews:ShouldInstallPlaceholderText` was not consulting the
    eligibility service.
    
    Change-Id: I6d3523672dfab35497db5ff55966a233d876bec8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7000754
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1523887}

--- chrome/browser/ui/views/omnibox/omnibox_view_views.cc
+++ chrome/browser/ui/views/omnibox/omnibox_view_views.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "build/build_config.h"
 #include "chrome/app/chrome_command_ids.h"
+#include "chrome/browser/autocomplete/aim_eligibility_service_factory.h"
 #include "chrome/browser/command_updater.h"
 #include "chrome/browser/external_protocol/external_protocol_handler.h"
 #include "chrome/browser/history_clusters/history_clusters_tab_helper.h"
@@ -1671,7 +1672,7 @@ void OmniboxViewViews::OnBlur() {
   render_text->SetWhitespaceElision(false);
   render_text->SetDisplayOffset(0);
 
-  // |location_bar_view_| can be null in tests.
+  // `location_bar_view_` can be null in tests.
   if (location_bar_view_) {
     location_bar_view_->OnOmniboxBlurred();
 
@@ -2359,8 +2360,18 @@ void OmniboxViewViews::UpdatePlaceholderTextColor() {
 }
 
 bool OmniboxViewViews::ShouldInstallAimPlaceholderText() const {
-  return omnibox_feature_configs::AiModeOmniboxEntryPoint::Get().enabled &&
-         model()->is_caret_visible();
+  // `location_bar_view_` can be null in tests.
+  if (!location_bar_view_) {
+    return false;
+  }
+
+  const auto* aim_eligibility_service =
+      AimEligibilityServiceFactory::GetForProfile(
+          location_bar_view_->profile());
+  const bool is_aim_entrypoint_enabled =
+      OmniboxFieldTrial::IsAimOmniboxEntrypointEnabled(aim_eligibility_service);
+
+  return is_aim_entrypoint_enabled && model()->is_caret_visible();
 }
 
 bool OmniboxViewViews::ShouldShowAimPlaceholderText() const {
@@ -2371,6 +2382,7 @@ bool OmniboxViewViews::ShouldShowAimPlaceholderText() const {
       !AimButtonVisible()) {
     return false;
   }
+
   // The placeholder text should only be shown when the omnibox is visibly
   // focused and the popup selection state is normal (i.e. no popup buttons are
   // focused and we are not in keyword mode). The hint text will be shown on NTP
