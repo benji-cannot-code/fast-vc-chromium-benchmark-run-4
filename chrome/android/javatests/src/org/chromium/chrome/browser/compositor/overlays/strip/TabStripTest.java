336c844c71e1087d8c8d30f263f621724806edfc
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2025-10-01 09:54:23 -0700

    Revert "Change desktop-like incognito window features to CachedFlag"
    This reverts commit fffdffe48d62987b00479817c035c7c7db7207fc.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5950870987472896
    
    Sample build with failed test: https://ci.chromium.org/b/8702267901799414225
    Affected test(s):
    [ninja://chrome/android:chrome_public_test_apk/org.chromium.chrome.browser.TabsTest#testAlertDialogDoesNotChangeActiveModel](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Fandroid:chrome_public_test_apk%2Forg.chromium.chrome.browser.TabsTest%23testAlertDialogDoesNotChangeActiveModel?q=VHash%3A75530eb78968e457)
    [ninja://chrome/android:chrome_public_test_apk/org.chromium.chrome.browser.tabmodel.IncognitoTabModelTest#testRemoveLastTab](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Fandroid:chrome_public_test_apk%2Forg.chromium.chrome.browser.tabmodel.IncognitoTabModelTest%23testRemoveLastTab?q=VHash%3A75530eb78968e457)
    [ninja://chrome/android:chrome_public_test_apk/org.chromium.chrome.browser.toolbar.LocationBarModelTest#testOnIncognitoStateChange_newTab__from_true_to_true](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Fandroid:chrome_public_test_apk%2Forg.chromium.chrome.browser.toolbar.LocationBarModelTest%23testOnIncognitoStateChange_newTab__from_true_to_true?q=VHash%3A75530eb78968e457)
    [ninja://chrome/android:chrome_public_test_apk/org.chromium.components.embedder_support.virtual_structure.PageContentProtoViewStructureBuilderTest#builderDoesNothingOnIncognitoTabs](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Fandroid:chrome_public_test_apk%2Forg.chromium.components.embedder_support.virtual_structure.PageContentProtoViewStructureBuilderTest%23builderDoesNothingOnIncognitoTabs?q=VHash%3A75530eb78968e457)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5950870987472896&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F6987361&type=BUG
    
    Original change's description:
    > Change desktop-like incognito window features to CachedFlag
    >
    > They are needed before native.
    >
    > Bug: 447456298
    > Change-Id: I6d2c3b5817338564042a4b8a272f23ecb48bed6b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6987361
    > Commit-Queue: Shu Yang <shuyng@google.com>
    > Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1523052}
    >
    
    Bug: 447456298
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I38d40c1b790cc1e9706103f662c45ae08c35913b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7003664
    Commit-Queue: Andrew Williams <awillia@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Andrew Williams <awillia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1523650}

--- chrome/android/javatests/src/org/chromium/chrome/browser/compositor/overlays/strip/TabStripTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/compositor/overlays/strip/TabStripTest.java
@@ -31,7 +31,6 @@ import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features;
-import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.compositor.layouts.components.CompositorButton;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
@@ -576,8 +575,6 @@ public class TabStripTest {
     @LargeTest
     @Feature({"TabStrip"})
     @Restriction(DeviceFormFactor.TABLET_OR_DESKTOP)
-    // TODO(crbug.com/439491767): Fix broken tests caused by desktop-like incognito window.
-    @DisableFeatures(ChromeFeatureList.ANDROID_OPEN_INCOGNITO_AS_WINDOW)
     public void testCloseLastIncognitoTab() {
         InstrumentationRegistry.getInstrumentation().waitForIdleSync();
         Assert.assertFalse(
@@ -616,8 +613,6 @@ public class TabStripTest {
     @LargeTest
     @Feature({"TabStrip"})
     @Restriction(DeviceFormFactor.TABLET_OR_DESKTOP)
-    // TODO(crbug.com/439491767): Fix broken tests caused by desktop-like incognito window.
-    @DisableFeatures(ChromeFeatureList.ANDROID_OPEN_INCOGNITO_AS_WINDOW)
     public void testCloseAllIncognitoTabsFromTabMenu() {
         // 1. Create two incognito tabs
         InstrumentationRegistry.getInstrumentation().waitForIdleSync();
@@ -682,8 +677,6 @@ public class TabStripTest {
     @LargeTest
     @Restriction(DeviceFormFactor.TABLET_OR_DESKTOP)
     @Feature({"TabStrip"})
-    // TODO(crbug.com/439491767): Fix broken tests caused by desktop-like incognito window.
-    @DisableFeatures(ChromeFeatureList.ANDROID_OPEN_INCOGNITO_AS_WINDOW)
     public void testTabSelectionViewDoesNotBreakModelSwitch() {
         InstrumentationRegistry.getInstrumentation().waitForIdleSync();
         Assert.assertFalse(
@@ -1191,8 +1184,6 @@ public class TabStripTest {
     @LargeTest
     @Feature({"TabStrip"})
     @Restriction(DeviceFormFactor.TABLET_OR_DESKTOP)
-    // TODO(crbug.com/439491767): Fix broken tests caused by desktop-like incognito window.
-    @DisableFeatures(ChromeFeatureList.ANDROID_OPEN_INCOGNITO_AS_WINDOW)
     public void testHoverOnTabStrip_switchTabModel() throws Exception {
         // Open regular tabs.
         ChromeTabUtils.newTabsFromMenu(
