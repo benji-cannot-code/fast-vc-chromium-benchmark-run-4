01c0e6961f010452c8dab18f8fde7aa06577e9ef
Tiffany Shen <tishen@google.com>
2025-10-01 11:25:03 -0700

    [Save PDF to Drive] Add accessibility according to a11y greenlines
    This change makes it possible to navigate through the account chooser
    with voiceover and select an account using keyboard controls.
    Accessibility behavior follows a11y greenline spec:
    https://www.figma.com/design/belCkGktoKYRTwmGuMofas/Save-to-Drive---Chrome?node-id=5378-106935&m=dev
    
    Single account demo:
    http://screencast/cast/NTU2NjkwODE0NDI4Nzc0NHw1Y2QwYWIyYi03ZA
    Multi-account demo:
    http://screencast/cast/NDUwODAzNDc0MjA5MTc3Nnw5MzUzYzQ2NC0yZQ
    
    Bug: 434686397
    Change-Id: Ie876aa7dfa4de413c8acd19a859910d0200f8ac8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7003623
    Reviewed-by: Darryl James <dljames@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Tiffany Shen <tishen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1523740}

--- chrome/browser/ui/views/save_to_drive/account_chooser_radio_group_view.cc
+++ chrome/browser/ui/views/save_to_drive/account_chooser_radio_group_view.cc
@@ -34,6 +34,10 @@ AccountChooserRadioButtonRow::AccountChooserRadioButtonRow(
       /*vertical=*/ChromeLayoutProvider::Get()->GetDistanceMetric(
           DISTANCE_EXTENSIONS_MENU_BUTTON_MARGIN),
       /*horizontal=*/0));
+  SetFocusBehavior(FocusBehavior::ALWAYS);
+  GetViewAccessibility().SetRole(ax::mojom::Role::kMenuItemRadio);
+  GetViewAccessibility().SetName(
+      base::StrCat({account.full_name, " ", account.email}));
 
   auto account_row_container = std::make_unique<views::FlexLayoutView>();
   auto account_row = CreateAccountRow(account);
@@ -88,6 +92,7 @@ AccountChooserRadioGroupView::AccountChooserRadioGroupView(
   CHECK(accounts.size() > 1) << "Account chooser radio group view should only "
                                 "be used for multi-account cases.";
   SetCascadingRadioGroupView(this, views::kCascadingRadioGroupView);
+  SetFocusBehavior(FocusBehavior::ACCESSIBLE_ONLY);
   GetViewAccessibility().SetRole(ax::mojom::Role::kRadioGroup);
   GetViewAccessibility().SetName(l10n_util::GetStringUTF16(
       IDS_ACCOUNT_CHOOSER_RADIO_GROUP_ACCESSIBILITY_LABEL));
