480bcf3ca6f8399b755fa53fe53151bb0407fa51
Andreu Botella <abotella@igalia.com>
2025-10-01 11:07:08 -0700

    [line-clamp] Don't insert the line-clamp ellipsis inside nowrap text
    When we initially implemented the `CSSLineClampLineBreakingEllipsis`
    runtime feature, which takes the line-clamp ellipsis into account when
    line-breaking (as opposed to using `LineTruncator`), we allowed the
    ellipsis to be inserted inside `text-wrap: nowrap` text (e.g.
    `white-space: pre`).
    
    The reason for this was that the spec text for `block-ellipsis` says
    that the ellipsis is placed after the last soft wrap opportunity that
    would still allow the ellipsis to not overflow the line box; and
    `text-wrap: nowrap` does not remove soft wrap opportunities, it just
    prevents wrapping at those.
    
    As it turns out, though, this was an oversight by the spec editor, and
    recently in https://github.com/w3c/csswg-drafts/issues/12811 it was
    resolved that `nowrap` blocks the insertion of ellipsis, except at
    forced line breaks. Similarly, in
    https://github.com/w3c/csswg-drafts/issues/12008 it was resolved that
    the ellipsis should be inserted as if appended after whitespace
    processing part II.
    
    Together, these two resolutions mean a line with the ellipsis is
    line-broken as usual, except that the available width is reduced by
    the width of the ellipsis; and then the ellipsis is inserted at the
    end, after whitespace collapsing (which in Chromium happens in the
    `LogicalLineBuilder`, not in the `LineBreaker`). This CL implements
    this behavior.
    
    One catch is, what happens with lines that end in hanging whitespace?
    Glyphs can only hang at the start or end of the line, and the ellipsis
    should block the hanging. The CSSWG didn't discuss this case yet, and
    there's an issue at https://github.com/w3c/csswg-drafts/issues/12857.
    This CL currently leaves Chromium's behavior there slightly broken,
    which we will fix in a follow-up.
    
    This CL also fixes the `webkit-line-clamp-024.html` and
    `line-clamp-with-abspos-017.html` WPT tests, since they don't work
    with the specified behavior.
    
    Bug: 40336192, 445681957, 445680405
    Change-Id: Ibf8f61ddefd9cf5fdbc88fc5bb667363dfcdd266
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6994661
    Commit-Queue: Andreu Botella <abotella@igalia.com>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1523726}

--- third_party/blink/renderer/core/layout/inline/line_breaker.h
+++ third_party/blink/renderer/core/layout/inline/line_breaker.h
@@ -88,9 +88,6 @@ class CORE_EXPORT LineBreaker {
     DCHECK(RuntimeEnabledFeatures::CSSLineClampLineBreakingEllipsisEnabled());
     line_clamp_ellipsis_width_ = width;
     UpdateAvailableWidth();
-    if (current_style_ && !disallow_auto_wrap_ && auto_wrap_ != !!width) {
-      SetCurrentStyleForce(*current_style_);
-    }
   }
 
   // Computing |LineBreakerMode::kMinContent| with |MaxSizeCache| caches
@@ -214,7 +211,6 @@ class CORE_EXPORT LineBreaker {
 
   void HandleTrailingSpaces(const InlineItem&, LineInfo*);
   void HandleTrailingSpaces(const InlineItem&, const ShapeResult*, LineInfo*);
-  void RemoveLineClampTrailingSpace(LineInfo*);
   void RemoveTrailingCollapsibleSpace(LineInfo*);
   void SplitTrailingBidiPreservedSpace(LineInfo*);
   LayoutUnit TrailingCollapsibleSpaceWidth(LineInfo*);
@@ -271,14 +267,6 @@ class CORE_EXPORT LineBreaker {
   bool MayBeAtomicInline(wtf_size_t offset) const;
   const InlineItem* TryGetAtomicInlineItemAfter(const InlineItem& item) const;
   unsigned IgnorableBidiControlLength(const InlineItem& item) const;
-
-  bool ShouldWrapLine(const ComputedStyle& style) const {
-    return line_clamp_ellipsis_width_ || style.ShouldWrapLine();
-  }
-  bool ShouldBreakOnlyAfterWhiteSpace(const ComputedStyle& style) const {
-    return (style.ShouldPreserveWhiteSpaces() && ShouldWrapLine(style)) ||
-           style.GetLineBreak() == LineBreak::kAfterWhiteSpace;
-  }
 
   bool ShouldPushFloatAfterLine(UnpositionedFloat*, LineInfo*);
   void HandleFloat(const InlineItem&,
