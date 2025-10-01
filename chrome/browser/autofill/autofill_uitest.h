1fa38b638c2695908be1cf2ceb2796ec1d22c2e4
Dominic Battre <battre@chromium.org>
2025-10-01 05:03:44 -0700

    Fix captured_sites_interactive_tests
    https://chromium-review.googlesource.com/c/chromium/src/+/6956986
    modified the theme provider to have a 500ms caret blink speed. Our tests
    waited for a page to cool down by checking that there are no new
    compositor frames drawn within 500ms. This was a problem if a field had
    focus. This CL sets a very slow caret blink speed so that a blinking
    cursor is not the reason why a test does not proceed.
    
    Bug: 446080325
    Change-Id: If4087c5b841612cba980ab25e9d9cc796c506a54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6999717
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Auto-Submit: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1523474}

--- chrome/browser/autofill/autofill_uitest.h
+++ chrome/browser/autofill/autofill_uitest.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/keycodes/dom/dom_key.h"
 #include "ui/events/keycodes/keyboard_code_conversion.h"
 #include "ui/events/keycodes/keyboard_codes.h"
+#include "ui/native_theme/mock_os_settings_provider.h"
 
 namespace autofill {
 
@@ -183,6 +184,7 @@ class AutofillUiTest : public InProcessBrowserTest,
       base::BindRepeating(&AutofillUiTest::HandleKeyPressEvent,
                           base::Unretained(this))};
 
+  ui::MockOsSettingsProvider os_settings_provider_;
   std::unique_ptr<ui::ScopedAnimationDurationScaleMode> disable_animation_;
 };
 
