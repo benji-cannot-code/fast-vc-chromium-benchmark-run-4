fecd09b4f55bf1bc999b7b606104b5104f2b3203
Christian Xu <christianxu@google.com>
2025-10-01 02:31:59 -0700

    [iOS] Cleanup max zps matches expired flag
    Fixed: 361119186
    Change-Id: I19205c510bff58ea9e33c1d95a2896a9ebaf50d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6999067
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1523430}

--- ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
+++ ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
@@ -675,9 +675,6 @@ extern const char kOmniboxLocalHistoryZeroSuggestBeyondNTPDescription[];
 extern const char kOmniboxMaxURLMatchesName[];
 extern const char kOmniboxMaxURLMatchesDescription[];
 
-extern const char kOmniboxMaxZPSMatchesName[];
-extern const char kOmniboxMaxZPSMatchesDescription[];
-
 extern const char kOmniboxMiaZps[];
 extern const char kOmniboxMiaZpsDescription[];
 
