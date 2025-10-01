f059deb326728ac17e34f1e040d56d047096f217
David Sanders <dsanders11@ucsbalum.com>
2025-10-01 00:04:09 -0700

    Remove unused include in gpu_memory_buffer_handle.h
    Bug: 40318405
    Change-Id: I8844fba44d4815056d032ea6c0f7fcfb8af1487b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6998275
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1523354}

--- services/video_capture/ash/video_frame_handler_ash.cc
+++ services/video_capture/ash/video_frame_handler_ash.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/video_transformation.h"
 #include "mojo/public/cpp/bindings/self_owned_receiver.h"
 #include "mojo/public/cpp/system/buffer.h"
+#include "ui/gfx/generic_shared_memory_id.h"
 #include "ui/gfx/gpu_memory_buffer_handle.h"
 
 namespace crosapi {
