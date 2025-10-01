e2aa3803c9b12d0d14934d364716ed6d32eff094
Tomasz Moniuszko <tmoniuszko@opera.com>
2025-10-01 10:31:56 -0700

    Add missing profile.h include
    IWYU fix. Needed for Profile::FromBrowserContext() call.
    Fixes compilation error in some build configurations.
    
    Bug: None
    Change-Id: I7c9265067fb9f5759d8414a02a7fe294a227dc23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7002900
    Auto-Submit: Tomasz Moniuszko <tmoniuszko@opera.com>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1523696}

--- chrome/browser/ui/webui/devtools/devtools_ui.cc
+++ chrome/browser/ui/webui/devtools/devtools_ui.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "chrome/browser/devtools/devtools_availability_checker.h"
 #include "chrome/browser/devtools/url_constants.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/devtools/devtools_ui_data_source.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/url_constants.h"
