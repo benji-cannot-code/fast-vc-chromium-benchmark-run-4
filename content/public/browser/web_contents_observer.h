afcb81e0e0213cf0a3aaf31bdc7f0aab1de77068
David Sanders <dsanders11@ucsbalum.com>
2025-09-30 23:27:31 -0700

    Remove unused include in web_contents_observer.h
    Bug: 40318405
    Change-Id: I4a10e99bc35d3ad2ac92bc35e64a91f1fcce2c33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6997937
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1523342}

--- content/public/browser/web_contents_observer.h
+++ content/public/browser/web_contents_observer.h
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/frame/viewport_intersection_state.mojom-forward.h"
 #include "third_party/blink/public/mojom/loader/resource_load_info.mojom-forward.h"
 #include "third_party/blink/public/mojom/media/capture_handle_config.mojom-forward.h"
-#include "third_party/skia/include/core/SkColor.h"
 #include "ui/base/page_transition_types.h"
 #include "ui/base/window_open_disposition.h"
 
