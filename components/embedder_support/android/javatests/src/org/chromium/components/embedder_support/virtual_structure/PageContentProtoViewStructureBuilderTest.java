e2a0de14550c905ee8d48e988ec17659197ecf81
Shu Yang <shuyng@google.com>
2025-10-01 13:31:55 -0700

    Reland "Change desktop-like incognito window features to CachedFlag"
    This is a reland of commit fffdffe48d62987b00479817c035c7c7db7207fc
    
    Original change's description:
    > Change desktop-like incognito window features to CachedFlag
    >
    > They are needed before native.
    >
    > Bug: 447456298
    > Change-Id: I6d2c3b5817338564042a4b8a272f23ecb48bed6b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6987361
    > Commit-Queue: Shu Yang <shuyng@google.com>
    > Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1523052}
    
    Include-Ci-Only-Tests: chromium.android.desktop:android-desktop-x64-rel-15-tests|*
    Bug: 447456298
    Change-Id: Id0fc5785eb951788274e3e627dfc797e748eba0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7004603
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Owners-Override: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Shu Yang <shuyng@google.com>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1523818}

--- components/embedder_support/android/javatests/src/org/chromium/components/embedder_support/virtual_structure/PageContentProtoViewStructureBuilderTest.java
+++ components/embedder_support/android/javatests/src/org/chromium/components/embedder_support/virtual_structure/PageContentProtoViewStructureBuilderTest.java
@@ -20,6 +20,7 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
@@ -37,6 +38,8 @@ import org.chromium.components.autofill.TestViewStructure;
 })
 @Batch(Batch.PER_CLASS)
 @EnableFeatures({"AnnotatedPageContentsVirtualStructure"})
+// TODO(crbug.com/439491767): Fix broken tests caused by desktop-like incognito window.
+@DisableFeatures("AndroidOpenIncognitoAsWindow")
 public class PageContentProtoViewStructureBuilderTest {
 
     private static final String TEST_PATH =
