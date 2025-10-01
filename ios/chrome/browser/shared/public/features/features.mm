fda9c1969bb6d5965bfd42e5a6962050d31d878a
Aliona DANGLA <alionadangla@google.com>
2025-10-01 08:31:44 -0700

    [iOS] Remove fullscreen-improvement flag
    Fixed: 361121640
    Change-Id: Iac77c815b9eb5482cbcf09d461347db3db0e3af3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7003683
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1523590}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -432,8 +432,6 @@ BASE_FEATURE(kContentNotificationDeliveredNAU,
 
 const char kDeliveredNAUMaxPerSession[] = "DeliveredNAUMaxPerSession";
 
-BASE_FEATURE(kFullscreenImprovement, base::FEATURE_DISABLED_BY_DEFAULT);
-
 BASE_FEATURE(kNewSyncOptInIllustration, base::FEATURE_DISABLED_BY_DEFAULT);
 
 bool IsNewSyncOptInIllustration() {
