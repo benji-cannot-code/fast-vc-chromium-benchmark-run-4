2f3ca5b0a9a289384ba15f05e3e5d4d977feb9da
Asami Doi <asamidoi@chromium.org>
2025-10-01 05:27:24 -0700

    [ios] Add libc++ path and -explicit-module-build
    Add the path to libc++ to swiftflags. This will be used when enabling
    the C++/Swift interoperability.
    
    Also add -explicit-module-build flag to swiftc to make sure that the
    swift compiler doesn't implicitly generate and use a modulemap for the
    system standard library.
    
    Bug: 40911785
    Change-Id: Ic5f6938db3c1e2bbad5d8e92143c24bcd716999b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6973409
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1523483}

--- build/toolchain/apple/swiftc.py
+++ build/toolchain/apple/swiftc.py
@@ -386,6 +386,7 @@ def invoke_swift_compiler(args, extras_args, build_cache_dir, output_file_map):
       '-save-temps',
       '-no-color-diagnostics',
       '-serialize-diagnostics',
+      '-explicit-module-build',
       '-emit-dependencies',
       '-emit-module',
       '-emit-module-path',
