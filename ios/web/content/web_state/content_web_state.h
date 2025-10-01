2c3a077a5b10357ba7d012dcf9e7e3c0b7477192
Sylvain Defresne <sdefresne@chromium.org>
2025-10-01 09:14:55 -0700

    [ios][blink] Prepare for removal of CRWSessionStorage*
    The support for legacy session storage format is going to be dropped
    soon. This means that CRWSessionStorage* will eventually be removed.
    
    To prepare for that, start refactoring ContentWebState so to instead
    use the protobuf based serialization primitives. As a first step add
    a new helper type ContentWebState::SerializedState that abstract how
    the serialized data is stored.
    
    Bug: 40945317
    Cq-Include-Trybots: luci.chromium.try:ios-blink-rel-fyi
    Change-Id: I8418e1e6c9af6b18760311a6f74509634d1108bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6994881
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1523617}

--- ios/web/content/web_state/content_web_state.h
+++ ios/web/content/web_state/content_web_state.h
@@ -230,6 +230,14 @@ class ContentWebState : public WebState,
       content::WebContents* source) override;
 
  private:
+  // Store serialized state.
+  class SerializedState;
+
+  // Private constructor.
+  ContentWebState(const CreateParams& params,
+                  WebStateID unique_identifier,
+                  std::unique_ptr<SerializedState> serialized_state);
+
   // Helper method to register notification observers.
   void RegisterNotificationObservers();
   void OnKeyboardShow(NSNotification* notification);
@@ -237,7 +245,7 @@ class ContentWebState : public WebState,
 
   raw_ptr<WebStateDelegate> delegate_ = nullptr;
   CRCWebViewportContainerView* web_view_;
-  CRWSessionStorage* session_storage_;
+  std::unique_ptr<SerializedState> serialized_state_;
   std::unique_ptr<content::WebContents> web_contents_;
   std::unique_ptr<content::WebContents> child_web_contents_;
   std::unique_ptr<web::SessionCertificatePolicyCache> certificate_policy_cache_;
