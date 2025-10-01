b192e7b2cbe197ae4dcdef0caea670bc20944bdc
Calder Kitagawa <ckitagawa@chromium.org>
2025-10-01 13:56:53 -0700

    [Pinned Tabs] Fix content description
    1. Don't allow the "pin" to be focusable.
    2. Announce pinned tabs as "Title, pinned tab".
    
    Fixed: 448557337
    Change-Id: Id9f69f7148387d2ebd4fc04fe29abf772039cae2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7004103
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1523831}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridView.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridView.java
@@ -258,6 +258,7 @@ public class TabGridView extends SelectableItemViewBase<TabListEditorItemSelecti
         }
         mActionButton.setBackgroundResource(R.drawable.small_icon_background);
         mActionButton.setImageBitmap(sCloseButtonBitmapWeakRef.get());
+        mActionButton.setFocusable(true);
     }
 
     private void setTabActionButtonPinDrawable() {
@@ -266,12 +267,14 @@ public class TabGridView extends SelectableItemViewBase<TabListEditorItemSelecti
         mActionButton.setImageDrawable(
                 ContextCompat.getDrawable(getContext(), R.drawable.ic_keep_24dp));
         mActionButton.setBackground(null);
+        mActionButton.setFocusable(false);
     }
 
     private void setTabActionButtonOverflowDrawable() {
         mActionButton.setImageDrawable(
                 ResourcesCompat.getDrawable(
                         getResources(), R.drawable.ic_more_vert_24dp, getContext().getTheme()));
+        mActionButton.setFocusable(true);
     }
 
     private void applyActionButtonTint() {
@@ -300,6 +303,7 @@ public class TabGridView extends SelectableItemViewBase<TabListEditorItemSelecti
         mActionButton.setImageDrawable(
                 AnimatedVectorDrawableCompat.create(
                         getContext(), R.drawable.ic_check_googblue_20dp_animated));
+        mActionButton.setFocusable(false);
     }
 
     private void setTabActionButtonDrawable() {
