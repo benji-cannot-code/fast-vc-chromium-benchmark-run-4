626ea770b9580718f61170ce5902de6b54c1d554
Andrew Rayskiy <greengrape@google.com>
2025-10-01 03:52:42 -0700

    Protocol Handler UX (5/x): Attach the picker coordinator to TabFeatures
    UXC: go/cros-protocol-handler-ux
    
    Bug: 422422887
    Change-Id: I74285c61b1fac6b3090aaa4fd67702bed307621f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6994884
    Reviewed-by: Simon Hangl <simonha@google.com>
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1523452}

--- chrome/browser/ui/tabs/tab_features.cc
+++ chrome/browser/ui/tabs/tab_features.cc
@@ -113,6 +113,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/side_panel/glic/glic_side_panel_coordinator.h"
 
 #endif
+
+#if BUILDFLAG(IS_CHROMEOS)
+#include "chrome/browser/apps/app_service/app_service_proxy_factory.h"  // nogncheck
+#include "chrome/browser/ui/views/web_apps/protocol_handler_picker_coordinator.h"
+#endif
+
 namespace tabs {
 
 TabFeatures::TabFeatures() = default;
@@ -432,6 +438,15 @@ void TabFeatures::Init(TabInterface& tab, Profile* profile) {
   tab_contextualization_controller_ =
       GetUserDataFactory().CreateInstance<lens::TabContextualizationController>(
           tab, &tab);
+
+#if BUILDFLAG(IS_CHROMEOS)
+  if (apps::AppServiceProxyFactory::IsAppServiceAvailableForProfile(profile)) {
+    protocol_handler_picker_coordinator_ =
+        GetUserDataFactory()
+            .CreateInstance<web_app::ProtocolHandlerPickerCoordinator>(
+                tab, tab, apps::AppServiceProxyFactory::GetForProfile(profile));
+  }
+#endif
 }
 
 TabUIHelper* TabFeatures::SetTabUIHelperForTesting(
