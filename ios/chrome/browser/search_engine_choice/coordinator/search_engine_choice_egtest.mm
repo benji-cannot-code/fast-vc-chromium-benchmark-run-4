919d174a7d03115f5c65d6e4e92855d3df214caf
Jérôme Lebel <jlebel@chromium.org>
2025-10-01 10:29:54 -0700

    [iOS] Enable testUserActionWhenExpandingSnippetChevron
    SearchEngineTestCase.testUserActionWhenExpandingSnippetChevron doesn't
    seem to be flaky anymore.
    
    Fixed: 333519732
    Change-Id: Iefbb51766485da275a2c396b33adcb1247018cc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6999831
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1523694}

--- ios/chrome/browser/search_engine_choice/coordinator/search_engine_choice_egtest.mm
+++ ios/chrome/browser/search_engine_choice/coordinator/search_engine_choice_egtest.mm
@@ -152,8 +152,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 // Test that the snippet can be expanded or collapsed.
-// TODO(crbug.com/333519732): Fix this flaky test.
-- (void)FLAKY_testUserActionWhenExpandingSnippetChevron {
+- (void)testUserActionWhenExpandingSnippetChevron {
   // Checks that the choice screen is shown
   [SearchEngineChoiceEarlGreyUI verifySearchEngineChoiceScreenIsDisplayed];
   if ([ChromeEarlGrey isIPadIdiom]) {
