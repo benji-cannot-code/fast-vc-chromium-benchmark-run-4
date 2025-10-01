f41af70daa5a64601d9b50751382e610e5d3f486
Dibyajyoti Pal <dibyapal@google.com>
2025-09-30 17:33:49 -0700

    [Predictable App Updating] UX metrics added
    This CL adds some metrics (and corresponding tests) about:
    1. Recording the number of times the user clicks on the 3 dot menu
       entry for triggering the update.
    2. Recording the number of times the dialog shows up as an user
       action.
    3. Records the outcome of the dialog closing.
    
    Bug: 447149311
    Change-Id: I15bfd3ee6deed36eb82461a0a4a4642c202c36bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6985246
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1523246}

--- chrome/browser/ui/web_applications/web_app_menu_model.cc
+++ chrome/browser/ui/web_applications/web_app_menu_model.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window/public/browser_window_features.h"
 #include "chrome/browser/ui/extensions/extensions_container.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "chrome/browser/ui/toolbar/app_menu_model.h"
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/web_applications/app_browser_controller.h"
 #include "chrome/browser/ui/web_applications/web_app_browser_controller.h"
@@ -115,8 +116,7 @@ void WebAppMenuModel::ExecuteCommand(int command_id, int event_flags) {
     case IDC_WEB_APP_UPGRADE_DIALOG:
       CHECK(base::FeatureList::IsEnabled(
           features::kWebAppPredictableAppUpdating));
-      // TODO(crbug.com/432252208): Log menu action for predictable app
-      // updating.
+      LogMenuAction(MENU_ACTION_TRIGGER_APP_UPDATE_DIALOG);
       browser()->app_controller()->CreateMetadataAndTriggerAppUpdateDialog(
           base::TimeTicks::Now());
       break;
