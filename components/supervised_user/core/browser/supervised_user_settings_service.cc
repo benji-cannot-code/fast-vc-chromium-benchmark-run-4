7e6d4edbb967554a8ce485be43ffcebaa967a9bb
Tomek Jurkiewicz <tju@google.com>
2025-10-01 04:52:13 -0700

    Stop removing `kGeolocationDisabled` in SupervisedUserSettingsService.
    It is never set there in the first place.
    
    Bug: 448384587
    Change-Id: I5e93f89f9a63cc74a105893dba43ca481b094c91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6999672
    Commit-Queue: Tomek Jurkiewicz <tju@google.com>
    Reviewed-by: Nohemi Fernandez <fernandex@google.com>
    Cr-Commit-Position: refs/heads/main@{#1523465}

--- components/supervised_user/core/browser/supervised_user_settings_service.cc
+++ components/supervised_user/core/browser/supervised_user_settings_service.cc
@@ -173,7 +173,6 @@ void SupervisedUserSettingsService::SetActive(bool active) {
   } else {
     RemoveLocalSetting(supervised_user::kSigninAllowed);
     RemoveLocalSetting(supervised_user::kCookiesAlwaysAllowed);
-    RemoveLocalSetting(supervised_user::kGeolocationDisabled);
   }
 
   InformSubscribers();
