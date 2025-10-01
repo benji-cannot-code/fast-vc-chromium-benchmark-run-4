b56f0a00516c69fb36503e4c321125e2bb485b2a
Sasha Kunitskaya <saskekun@google.com>
2025-10-01 10:08:38 -0700

    Refactor `IsSyncingUserSelectableTypesAllowedByPolicy` to take `SyncService`.
    This change modifies
    `signin_util::IsSyncingUserSelectableTypesAllowedByPolicy` to accept a
    `const syncer::SyncService*` instead of a `Profile&`. Call sites are
    updated to pass the `SyncService` obtained from the `Profile`. This
    makes the utility function more flexible by directly using the
    `SyncService` instance.
    
    Bug: 444657437
    Change-Id: I1753540996dfc9edc53fff5876e6e400291366af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7003325
    Reviewed-by: Ernest Nguyen Hung <ernn@google.com>
    Reviewed-by: Amelie Schneider <amelies@google.com>
    Commit-Queue: Sasha Kunitskaya <saskekun@google.com>
    Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1523667}

--- chrome/browser/ui/tabs/recent_tabs_sub_menu_model.cc
+++ chrome/browser/ui/tabs/recent_tabs_sub_menu_model.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sessions/tab_restore_service_factory.h"
 #include "chrome/browser/signin/signin_util.h"
 #include "chrome/browser/sync/session_sync_service_factory.h"
+#include "chrome/browser/sync/sync_service_factory.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_live_tab_context.h"
@@ -434,7 +435,8 @@ void RecentTabsSubMenuModel::BuildTabsFromOtherDevices() {
   if (base::FeatureList::IsEnabled(
           syncer::kReplaceSyncPromosWithSignInPromos) &&
       !signin_util::IsSyncingUserSelectableTypesAllowedByPolicy(
-          *browser_->profile(), {syncer::UserSelectableType::kTabs})) {
+          SyncServiceFactory::GetForProfile(browser_->profile()),
+          {syncer::UserSelectableType::kTabs})) {
     return;
   }
 
