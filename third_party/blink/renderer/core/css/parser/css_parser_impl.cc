497ad990b49b39cca912dca8275c37012b633784
moonira <moonira@google.com>
2025-10-01 08:57:00 -0700

    Fix @custom-media parser implementation
    The parser didn't correctly account for semicolons at the end of the
    declaration. Fix it and add unit tests.
    
    Bug: 40781325
    Change-Id: Id09ae2e3559068d08aee1c09b3897d3a1bb098b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6983368
    Commit-Queue: Munira Tursunova <moonira@google.com>
    Reviewed-by: Steinar H Gunderson <sesse@chromium.org>
    Auto-Submit: Munira Tursunova <moonira@google.com>
    Cr-Commit-Position: refs/heads/main@{#1523600}

--- third_party/blink/renderer/core/css/parser/css_parser_impl.cc
+++ third_party/blink/renderer/core/css/parser/css_parser_impl.cc
@@ -2724,6 +2724,7 @@ std::optional<bool> GetBooleanValue(const CSSParserToken& token) {
 
 StyleRuleCustomMedia* CSSParserImpl::ConsumeCustomMediaRule(
     CSSParserTokenStream& stream) {
+  CSSParserTokenStream::Boundary boundary(stream, kSemicolonToken);
   const CSSParserToken& name_token = stream.Peek();
   if (!IsValidExtensionName(name_token)) {
     ConsumeErroneousAtRule(stream, CSSAtRuleID::kCSSAtRuleCustomMedia);
@@ -2736,6 +2737,7 @@ StyleRuleCustomMedia* CSSParserImpl::ConsumeCustomMediaRule(
   if (bool_val.has_value()) {
     stream.ConsumeIncludingWhitespace();
     if (!stream.AtEnd()) {
+      ConsumeErroneousAtRule(stream, CSSAtRuleID::kCSSAtRuleCustomMedia);
       return nullptr;
     }
     return MakeGarbageCollected<StyleRuleCustomMedia>(
