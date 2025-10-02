af75dbf896e14a4deb78db8c35be2d6275ac62e6
Colin Blundell <blundell@chromium.org>
2025-10-02 00:05:42 -0700

    [//ui] Port DisplayManagerAndroid's construction of DisplayColorSpaces
    Moves it away from the deprecated constructor that takes in BufferFormat
    to the variant that takes in SharedImageFormat [1].
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/6983155
    
    Bug: 445879532
    Change-Id: Ie8dee08c835e7a8f6b3db8af9977a8b82a41ca31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7002401
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1524034}

--- ui/android/display_android_manager.cc
+++ ui/android/display_android_manager.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notimplemented.h"
 #include "base/trace_event/trace_event.h"
 #include "components/viz/common/features.h"
+#include "components/viz/common/resources/shared_image_format.h"
 #include "components/viz/common/viz_utils.h"
 #include "skia/ext/skcolorspace_trfn.h"
 #include "ui/android/screen_android.h"
@@ -155,8 +156,8 @@ void DisplayAndroidManager::DoUpdateDisplay(display::Display* display,
       hdr_max_luminance_ratio = 1.f;
     }
     // Propagate this into the DisplayColorSpaces.
-    gfx::DisplayColorSpaces display_color_spaces(gfx::ColorSpace::CreateSRGB(),
-                                                 gfx::BufferFormat::RGBA_8888);
+    gfx::DisplayColorSpaces display_color_spaces(
+        gfx::ColorSpace::CreateSRGB(), viz::SinglePlaneFormat::kRGBA_8888);
     display_color_spaces.SetHDRMaxLuminanceRelative(hdr_max_luminance_ratio);
     for (auto needs_alpha : {true, false}) {
       // TODO: Low-end devices should specify RGB_565 as the buffer format for
