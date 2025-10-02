d1b96e425ffe635bb69ee7f4ff1c1d9dfcaa2bc0
Samar <samarchehade@google.com>
2025-10-02 06:11:33 -0700

    [Clank][Gardener] Disable failing test
    Bug: 422882351
    Change-Id: I177c4479284ed992e3a3264f8f6dd04048274eb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7006088
    Reviewed-by: Samar Chehade <samarchehade@google.com>
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Owners-Override: Samar Chehade <samarchehade@google.com>
    Auto-Submit: Samar Chehade <samarchehade@google.com>
    Commit-Queue: Samar Chehade <samarchehade@google.com>
    Reviewed-by: Duong Dac <ddac@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1524152}

--- chrome/android/javatests/src/org/chromium/chrome/browser/firstrun/FirstRunIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/firstrun/FirstRunIntegrationTest.java
@@ -805,6 +805,7 @@ public class FirstRunIntegrationTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/422882351")
     public void testNativeInitBeforeFragment() throws Exception {
         FirstRunPagesTestCase testCase = new FirstRunPagesTestCase().withoutSignIn();
         initializePreferences(testCase);
