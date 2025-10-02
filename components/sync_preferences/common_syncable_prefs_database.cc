5c4dd23bd29a563b2ead796b7d12acc771b9b9da
Scott Yoder <scottyoder@google.com>
2025-10-02 04:24:07 -0700

    [ios] Add cross-device pref for iOS 16th active day pref
    This commit introduces a new cross-device pref to track the 16th
    active day for cross-platform promos on iOS. This preference will
    be used to determine when to show specific promotions to Desktop
    users, about features that are also available on iOS.
    
    Fixed: 443978815
    Change-Id: I8eea36eac5b292784cbb78a794e5219ed24dc5d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7001459
    Reviewed-by: Ankush Singh <ankushkush@google.com>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Benjamin Williams <bwwilliams@google.com>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1524114}

--- components/sync_preferences/common_syncable_prefs_database.cc
+++ components/sync_preferences/common_syncable_prefs_database.cc
@@ -146,6 +146,7 @@ enum {
   kAutofillAiLastVersionDeduped = 96,
   kCrossDeviceOmniboxIsInBottomPosition = 97,
   kAutofillWasNameAndEmailProfileUsed = 98,
+  kCrossDeviceCrossPlatformPromosIOS16thActiveDay = 99,
   // See components/sync_preferences/README.md about adding new entries here.
   // vvvvv IMPORTANT! vvvvv
   // Note to the reviewer: IT IS YOUR RESPONSIBILITY to ensure that new syncable
@@ -264,6 +265,10 @@ constexpr auto kCommonSyncablePrefsAllowlist =
           PrefSensitivity::kNone, MergeBehavior::kNone}},
         {prefs::kCrossDeviceOmniboxIsInBottomPosition,
          {syncable_prefs_ids::kCrossDeviceOmniboxIsInBottomPosition,
+          syncer::PREFERENCES, PrefSensitivity::kNone,
+          MergeBehavior::kMergeableDict}},
+        {prefs::kCrossDeviceCrossPlatformPromosIOS16thActiveDay,
+         {syncable_prefs_ids::kCrossDeviceCrossPlatformPromosIOS16thActiveDay,
           syncer::PREFERENCES, PrefSensitivity::kNone,
           MergeBehavior::kMergeableDict}},
         {prefs::kSafeBrowsingEnabled,
