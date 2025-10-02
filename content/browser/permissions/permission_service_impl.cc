2deb5d5c3bbe78cc5f96ca2eb3407962c16be5a7
Florian Jacky <fjacky@google.com>
2025-10-02 06:35:54 -0700

    Map coarse location grants to granted permission
    The query API uses a permission descriptor to query the permission
    state. Since the coarse location MVP currently doesn't use a special
    permission descriptor, the query API can only query whether precise
    location is granted, which results in UNSATISFIED_OPTIONS for a coarse
    location granted. This CL maps this permission state to GRANTED for the
    purposes of the query API.
    
    Bug: 393053278
    Change-Id: If200e7bdcedba7176e7a37c428823c11ccb04c01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7003266
    Commit-Queue: Florian Jacky <fjacky@chromium.org>
    Reviewed-by: Nicola Tommasi <tommasin@chromium.org>
    Reviewed-by: Thomas Nguyen <tungnh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1524162}

--- content/browser/permissions/permission_service_impl.cc
+++ content/browser/permissions/permission_service_impl.cc
@@ -13,8 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/command_line.h"
+#include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/memory/ptr_util.h"
+#include "components/content_settings/core/common/features.h"
 #include "content/browser/bad_message.h"
 #include "content/browser/permissions/embedded_permission_control_checker.h"
 #include "content/browser/permissions/permission_controller_impl.h"
@@ -29,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/features_generated.h"
 #include "third_party/blink/public/common/permissions/permission_utils.h"
 #include "third_party/blink/public/mojom/permissions/permission.mojom-shared.h"
+#include "third_party/blink/public/mojom/permissions/permission_status.mojom-data-view.h"
 #include "url/origin.h"
 
 using blink::mojom::EmbeddedPermissionControlClient;
@@ -335,7 +338,24 @@ void PermissionServiceImpl::OnRequestPermissionsResponse(
 
 void PermissionServiceImpl::HasPermission(PermissionDescriptorPtr permission,
                                           PermissionStatusCallback callback) {
-  std::move(callback).Run(GetPermissionResult(permission).status);
+  auto permission_status = GetPermissionResult(permission).status;
+  if (base::FeatureList::IsEnabled(
+          content_settings::features::kApproximateGeolocationPermission) &&
+      blink::PermissionDescriptorToPermissionType(permission) ==
+          blink::PermissionType::GEOLOCATION &&
+      permission_status ==
+          blink::mojom::PermissionStatus::UNSATISFIED_OPTIONS) {
+    // TODO(crbug.com/430586927): This is a short-term solution. Once the
+    // geolocation permission descriptor supports isPrecise, the correct
+    // preciseness should be queried. The query API uses a permission descriptor
+    // to query the permission state. Since the coarse location MVP currently
+    // doesn't use a special permission descriptor, the query API can only query
+    // whether precise location is granted, which results in UNSATISFIED_OPTIONS
+    // for a coarse location granted. We can map this to a GRANTED for the
+    // purposes of the query API.
+    permission_status = PermissionStatus::GRANTED;
+  }
+  std::move(callback).Run(permission_status);
 }
 
 void PermissionServiceImpl::RevokePermission(
