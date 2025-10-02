72bb6a2193a49dd49c2c064cad9cd0bcf06af1ca
David Benjamin <davidben@chromium.org>
2025-10-02 08:14:22 -0700

    Fix a C++23 incompatibility in //gin
    T must be now complete when ~unique_ptr<T> is instantiated. The
    unique_ptr<CppHeap> = nullptr parameter requires CppHeap be available.
    
    Bug: 388068055
    Change-Id: Ib3d900c53b453a026adc249db126fb6d11ceae10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7002494
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Auto-Submit: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1524219}

--- gin/public/isolate_holder.h
+++ gin/public/isolate_holder.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gin/public/v8_idle_task_runner.h"
 #include "v8/include/v8-array-buffer.h"
 #include "v8/include/v8-callbacks.h"
+#include "v8/include/v8-cppgc.h"
 #include "v8/include/v8-forward.h"
 #include "v8/include/v8-isolate.h"
 
