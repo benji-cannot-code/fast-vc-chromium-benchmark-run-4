4a76cf3b4446cb68ab84539554f3077ce0f6f141
Roman Arora <romanarora@chromium.org>
2025-10-02 08:39:36 -0700

    [realbox][next] Add base feature and wire feature params
    Adds a base feature for the upcoming Realbox 'Next' UI experience.
    Though this experience is meant to launch as an additive experience to
    the Composebox feature, it relies on a dedicated base feature flag to
    allow a greater amount of launch control and granularity.
    
    Design doc: go/next-desktop-ntp-realbox
    
    Bug: 443061135, 444064886
    Change-Id: Id27664f4557b256db38c98e4cd23f03fcc9b61ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6991509
    Auto-Submit: Roman Arora <romanarora@chromium.org>
    Reviewed-by: Ananya Seelam <ananyaseelam@google.com>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1524231}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1665,13 +1665,11 @@ const FeatureEntry::FeatureParam kComposeboxShowContextMenuAndZps[] = {
     {"ShowContextMenu", "true"},
 };
 const FeatureEntry::FeatureParam kShowNextRealboxTall[] = {
-    {"ShowContextMenu", "true"},
-    {"RealboxLayoutMode", ntp_composebox::kRealboxLayoutModeTall},
+    {"RealboxLayoutMode", ntp_realbox::kRealboxLayoutModeTall},
     {"CyclingPlaceholders", "true"},
 };
 const FeatureEntry::FeatureParam kShowNextRealboxCompact[] = {
-    {"ShowContextMenu", "true"},
-    {"RealboxLayoutMode", ntp_composebox::kRealboxLayoutModeCompact},
+    {"RealboxLayoutMode", ntp_realbox::kRealboxLayoutModeCompact},
     {"CyclingPlaceholders", "true"},
 };
 const FeatureEntry::FeatureParam kComposeboxShowContextMenuAndZpsMultiFile[] = {
@@ -1709,10 +1707,6 @@ const FeatureEntry::FeatureVariation kNtpComposeboxVariations[] = {
      std::size(kComposeboxShowContextMenu), nullptr},
     {"- Show Contextual Input Menu and ZPS", kComposeboxShowContextMenuAndZps,
      std::size(kComposeboxShowContextMenuAndZps), nullptr},
-    {"- Show Next Realbox (Tall)", kShowNextRealboxTall,
-     std::size(kShowNextRealboxTall), nullptr},
-    {"- Show Next Realbox (Compact)", kShowNextRealboxCompact,
-     std::size(kShowNextRealboxCompact), nullptr},
     {"- Show Contextual Input Menu and ZPS 5 File Limit",
      kComposeboxShowContextMenuAndZpsMultiFile,
      std::size(kComposeboxShowContextMenuAndZpsMultiFile), nullptr},
@@ -1724,6 +1718,13 @@ const FeatureEntry::FeatureVariation kNtpComposeboxVariations[] = {
      std::size(kShowToolsAndModels), nullptr},
     {"- Show Create Image in Context Menu", kShowCreateImageTool,
      std::size(kShowCreateImageTool), nullptr},
+};
+
+const FeatureEntry::FeatureVariation kNtpRealboxNextVariations[] = {
+    {"- Show Next Realbox (Tall)", kShowNextRealboxTall,
+     std::size(kShowNextRealboxTall), nullptr},
+    {"- Show Next Realbox (Compact)", kShowNextRealboxCompact,
+     std::size(kShowNextRealboxCompact), nullptr},
 };
 
 #endif  // BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_MAC) ||
@@ -7552,6 +7553,12 @@ const FeatureEntry kFeatureEntries[] = {
      FEATURE_WITH_PARAMS_VALUE_TYPE(ntp_composebox::kNtpComposebox,
                                     kNtpComposeboxVariations,
                                     "NtpComposebox")},
+
+    {"ntp-realbox-next", flag_descriptions::kNtpRealboxNextName,
+     flag_descriptions::kNtpRealboxNextDescription, kOsDesktop,
+     FEATURE_WITH_PARAMS_VALUE_TYPE(ntp_realbox::kNtpRealboxNext,
+                                    kNtpRealboxNextVariations,
+                                    "NtpRealboxNext")},
 
     {"composebox-uses-chrome-compose-client",
      flag_descriptions::kNtpComposeboxUsesChromeComposeClientName,
