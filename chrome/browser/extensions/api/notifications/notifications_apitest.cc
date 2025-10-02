4d648b29b4a16334348e1da4ea282ca51e1964b1
James Cook <jamescook@chromium.org>
2025-10-02 16:48:36 -0700

    extensions: Enable more notification API tests on desktop Android
    Several of the tests now pass, due to improvements in the test
    infrastructure.
    
    Bug: 441566438
    Change-Id: I482f698b6599d2a1dae00790e74734b5522b9055
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7006557
    Reviewed-by: Zhengzheng Liu <zhzhliu@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1524517}

--- chrome/browser/extensions/api/notifications/notifications_apitest.cc
+++ chrome/browser/extensions/api/notifications/notifications_apitest.cc
@@ -47,13 +47,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/mac_util.h"
 #endif
 
-#if !BUILDFLAG(IS_ANDROID)
-#include "chrome/browser/ui/browser.h"
-#include "chrome/test/base/interactive_test_utils.h"
-#endif  // BUILDFLAG(IS_ANDROID)
-
 #if BUILDFLAG(ENABLE_PLATFORM_APPS)
 #include "chrome/browser/apps/platform_apps/app_browsertest_util.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/test/base/interactive_test_utils.h"
 #endif  // BUILDFLAG(ENABLE_PLATFORM_APPS)
 
 static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
@@ -237,9 +234,7 @@ IN_PROC_BROWSER_TEST_F(NotificationsApiTestWithServiceWorker,
 }
 
 // Native notifications don't support (or use) observers.
-// TODO(crbug.com/371431032): Port to desktop Android. LoadExtensionAndWait()
-// times out. Also, the test extensions may need to move to manifest V3.
-#if !BUILDFLAG(IS_MAC) && !BUILDFLAG(IS_ANDROID)
+#if !BUILDFLAG(IS_MAC)
 IN_PROC_BROWSER_TEST_F(NotificationsApiTest, TestByUser) {
   const extensions::Extension* extension =
       LoadExtensionAndWait("notifications/api/by_user");
@@ -325,9 +320,6 @@ IN_PROC_BROWSER_TEST_F(NotificationsApiTest, TestGetPermissionLevel) {
   }
 }
 
-#if !BUILDFLAG(IS_ANDROID)
-// TODO(crbug.com/371431032): Port to desktop Android. LoadExtensionAndWait()
-// times out. Also, the test extensions may need to move to manifest V3.
 IN_PROC_BROWSER_TEST_F(NotificationsApiTest, TestOnPermissionLevelChanged) {
   const extensions::Extension* extension =
       LoadExtensionAndWait("notifications/api/permission");
@@ -414,6 +406,8 @@ IN_PROC_BROWSER_TEST_F(NotificationsApiTest, TestRequireInteraction) {
   EXPECT_TRUE(notification->never_timeout());
 }
 
+#if BUILDFLAG(ENABLE_PLATFORM_APPS)
+// The following tests exercise platform app behavior.
 IN_PROC_BROWSER_TEST_F(NotificationsApiTest, TestShouldDisplayNormal) {
   ExtensionTestMessageListener notification_created_listener("created");
   const Extension* extension = LoadAppWithWindowState(
@@ -541,5 +535,4 @@ IN_PROC_BROWSER_TEST_F(NotificationsApiTest, TestSmallImage) {
   EXPECT_FALSE(notification->small_image().IsEmpty());
   EXPECT_TRUE(notification->small_image_needs_additional_masking());
 }
-
-#endif  // !BUILDFLAG(IS_ANDROID)
+#endif  // BUILDFLAG(ENABLE_PLATFORM_APPS)
