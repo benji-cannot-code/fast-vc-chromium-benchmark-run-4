8e95c7edb775c1299666210920eb0dfb4baa6679
Svend Larsen <svend@chromium.org>
2025-10-02 07:51:56 -0700

    [VL] Let callers specify 404 policy in GetMostRecentVisitsForGurl()
    404 visits have historically not been eligible for History, so callers
    of History querying methods have not needed to consider whether 404
    visits are relevant for their feature or not. In the future, when
    history::kVisitedLinksOn404 is enabled, 404s will become eligible for
    History, and therefore be returnable by GetMostRecentVisitsForGurl().
    Not all callers want to receive 404 visits, so this CL adds a
    policy_for_404_visits param to the method to allow callers to specify
    whether they want 404 visits.
    
    This CL is part of a larger project to make links `:visited` after a 404
    visit. Project links:
    - Google-internal project doc: go/stop-filtering-history-by-status-code
    - Issue tree / work status: https://crbug.com/427225439/dependencies
    
    Fixed: 441271806
    Change-Id: Ia881d28b74caa00366c2a69c532c3efaab8a5d01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7002348
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Svend Larsen <svend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1524201}

--- components/history/core/browser/history_service.h
+++ components/history/core/browser/history_service.h
@@ -497,6 +497,7 @@ class HistoryService : public KeyedService,
   base::CancelableTaskTracker::TaskId GetMostRecentVisitsForGurl(
       GURL url,
       int max_visits,
+      VisitQuery404sPolicy policy_for_404_visits,
       QueryURLAndVisitsCallback callback,
       base::CancelableTaskTracker* tracker);
 
