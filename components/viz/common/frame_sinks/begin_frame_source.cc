0936e8e19a0f4e9fe2da4a9b84cc4a60bc8f7295
Jonathan Ross <jonross@chromium.org>
2025-10-02 14:51:10 -0700

    Introduce BeginFrameSource::SchedulerClient
    Currently BeginFrameObservers are notified in the order in which they
    begin observation. This means there is no guarantee that
    viz::DisplayScheduler will be notified after all clients. Since the
    OnBeginFrame signal is used to make Deadline decisions, we can end up
    choosing the incorrect deadline based on partial information.
    
    This change adds an alternative way to observer the BeginFrames. The
    BeginFrameSource::SchedulerClient will always be notified after all
    other observers. We have viz::DisplayScheduler implement this.
    
    This is controlled behind a flag, so we aren't removing the old observer
    path for now
    
    Bug: 40900977
    
    Change-Id: Ic62c522e6920b05a35388fdeea09f8717730801b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6998406
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1524466}

--- components/viz/common/frame_sinks/begin_frame_source.cc
+++ components/viz/common/frame_sinks/begin_frame_source.cc
@@ -172,6 +172,10 @@ void BeginFrameSource::SetIsGpuBusy(bool busy) {
   }
 }
 
+void BeginFrameSource::SetSchedulerClient(SchedulerClient* scheduler_client) {
+  scheduler_client_ = scheduler_client;
+}
+
 bool BeginFrameSource::RequestCallbackOnGpuAvailable() {
   if (!is_gpu_busy_) {
     DCHECK_EQ(gpu_busy_response_state_, GpuBusyThrottlingState::kIdle);
@@ -220,6 +224,13 @@ void BeginFrameSource::RecordBeginFrameSourceAccuracy(base::TimeDelta delta) {
   total_delta_ = base::TimeDelta();
 }
 #endif
+
+void BeginFrameSource::IssueBeginFrameToSchedulerClient(
+    const BeginFrameArgs& args) {
+  if (scheduler_client_) {
+    scheduler_client_->OnBeginFrameForScheduling(args);
+  }
+}
 
 // StubBeginFrameSource ---------------------------------------------------
 StubBeginFrameSource::StubBeginFrameSource()
@@ -312,6 +323,7 @@ void BackToBackBeginFrameSource::OnTimerTick() {
   DCHECK(!pending_observers.empty());
   for (BeginFrameObserver* obs : pending_observers)
     FilterAndIssueBeginFrame(obs, args);
+  IssueBeginFrameToSchedulerClient(args);
 }
 
 // DelayBasedBeginFrameSource ---------------------------------------------
@@ -436,6 +448,7 @@ void DelayBasedBeginFrameSource::OnTimerTick() {
   for (BeginFrameObserver* obs : observers) {
     IssueBeginFrameToObserver(obs, last_begin_frame_args_);
   }
+  IssueBeginFrameToSchedulerClient(last_begin_frame_args_);
   last_vsync_interval_ = time_source_->Interval();
 }
 
@@ -585,6 +598,7 @@ void ExternalBeginFrameSource::OnBeginFrame(const BeginFrameArgs& args) {
       continue;
     FilterAndIssueBeginFrame(obs, args);
   }
+  IssueBeginFrameToSchedulerClient(args);
 }
 
 BeginFrameArgs ExternalBeginFrameSource::GetMissedBeginFrameArgs(
