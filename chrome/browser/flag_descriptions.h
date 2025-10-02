b152d1bbb8943b642acf95a3bbeba2cb3992e830
Haihan Chen <haihan@google.com>
2025-10-02 14:56:46 -0700

    [File Download Access Prevention] Cleanup exposed flag on chrome://flags
    Cleanup exposed flag as m140 has passed. No customer has reached out lately for issues to justify extending this.
    
    Bug: 448412395, 427495303
    Change-Id: I334835a212f0670dadb4997d22d4ae2b58eb2f68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6998051
    Commit-Queue: Haihan Chen <haihan@google.com>
    Reviewed-by: Nasser Al-shawwa <alshawwa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1524468}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/buildflag.h"
 #include "chrome/common/buildflags.h"
 #include "components/compose/buildflags.h"
-#include "components/enterprise/buildflags/buildflags.h"
 #include "components/paint_preview/buildflags/buildflags.h"
 #include "components/signin/public/base/signin_buildflags.h"
 #include "components/webui/flags/feature_entry.h"
@@ -4922,11 +4921,6 @@ extern const char kSupervisedUserLocalWebApprovalsDescription[];
 extern const char kHistoryOptInEducationalTipName[];
 extern const char kHistoryOptInEducationalTipDescription[];
 #endif  // BUILDFLAG(IS_ANDROID)
-
-#if BUILDFLAG(ENTERPRISE_CONTENT_ANALYSIS)
-extern const char kEnterpriseFileObfuscationName[];
-extern const char kEnterpriseFileObfuscationDescription[];
-#endif  // BUILDFLAG(ENTERPRISE_CONTENT_ANALYSIS)
 
 // ============================================================================
 // Don't just add flags to the end, put them in the right section in
