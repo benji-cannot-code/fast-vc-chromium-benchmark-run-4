6626c94d807cb50c58a3fb250fae402bdf67eecf
Linyu He <linyuh@google.com>
2025-10-02 11:16:01 -0700

    Fix AndroidBaseWindow::SetBounds when its param crosses JNI
    The bug:
    
    Android Java Rect's ctor takes (left, top, right, bottom), but the C++
    AndroidBaseWindow::SetBounds passed (left, top, width, height).
    
    Change-Id: I895d2d4d233a7c701e40f4f65cd858fa709f9a0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7004573
    Commit-Queue: Linyu He <linyuh@google.com>
    Reviewed-by: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1524338}

--- chrome/browser/ui/browser_window/test/android/java/src/org/chromium/chrome/browser/ui/browser_window/AndroidBaseWindowNativeUnitTestSupport.java
+++ chrome/browser/ui/browser_window/test/android/java/src/org/chromium/chrome/browser/ui/browser_window/AndroidBaseWindowNativeUnitTestSupport.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.ui.browser_window;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockingDetails;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.withSettings;
 
@@ -105,6 +106,12 @@ final class AndroidBaseWindowNativeUnitTestSupport {
     @CalledByNative
     private void invokeDestroy() {
         mAndroidBaseWindow.destroy();
+    }
+
+    @CalledByNative
+    private void verifyBoundsToSet(int left, int top, int right, int bottom) {
+        assert mockingDetails(mChromeAndroidTask).isMock();
+        verify(mChromeAndroidTask).setBoundsInDp(new Rect(left, top, right, bottom));
     }
 
     @CalledByNative
