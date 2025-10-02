6de5a34542db2d686de28840c9cebd14bbf707e2
Henrique Nakashima <hnakashima@chromium.org>
2025-10-02 15:14:45 -0700

    [Android] Disable JsSandboxServiceTest#testUnicodeConsoleMessage
    Bug: 448997217
    Change-Id: Ida68a936ed02fdf266fe5d4f059cbdb78638ce82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7005191
    Auto-Submit: Henrique Nakashima <hnakashima@chromium.org>
    Owners-Override: David Maunder <davidjm@chromium.org>
    Owners-Override: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Haiyang Pan <hypan@google.com>
    Reviewed-by: David Maunder <davidjm@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Haiyang Pan <hypan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1524478}

--- android_webview/javatests/src/org/chromium/android_webview/test/services/JsSandboxServiceTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/services/JsSandboxServiceTest.java
@@ -1567,6 +1567,7 @@ public class JsSandboxServiceTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/448997217")
     public void testUnicodeConsoleMessage() throws Throwable {
         final Context context = ContextUtils.getApplicationContext();
         final ListenableFuture<JavaScriptSandbox> jsSandboxFuture =
