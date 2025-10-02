8518b013626fc0ee9c8dd6880e951db3d26326a7
Sky Malice <skym@chromium.org>
2025-10-02 08:12:08 -0700

    [Persist] Pull id and type back to service.
    Bug: 448151025
    Change-Id: Ic80b62df01f58def431135aaf55c83445bc42740
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7004114
    Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1524216}

--- chrome/browser/tab/tab_state_storage_backend.h
+++ chrome/browser/tab/tab_state_storage_backend.h
@@ -30,7 +30,7 @@ class TabStateStorageBackend {
 
   void Initialize();
 
-  void Save(std::unique_ptr<TabStoragePackage> package);
+  void Save(int id, int type, std::unique_ptr<TabStoragePackage> package);
 
   void LoadAllNodes(base::OnceCallback<void(std::vector<NodeState>)> callback);
 
