412a818055ffde33c127f5930d5b701b6207aa41
Nasser Al-shawwa <alshawwa@chromium.org>
2025-10-02 14:05:31 -0700

    Enable watermark customization by default
    Bug: 425991579
    Change-Id: I2183ec4a9f2d0d52532786dfe9aa68ba11245faa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7004096
    Commit-Queue: Nasser Al-shawwa <alshawwa@chromium.org>
    Reviewed-by: Haihan Chen <haihan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1524441}

--- chrome/browser/enterprise/watermark/watermark_features.cc
+++ chrome/browser/enterprise/watermark/watermark_features.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace enterprise_watermark {
 
-BASE_FEATURE(kEnableWatermarkCustomization, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kEnableWatermarkCustomization, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kEnableWatermarkTestPage, base::FEATURE_DISABLED_BY_DEFAULT);
 
