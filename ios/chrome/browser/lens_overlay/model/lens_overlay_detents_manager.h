915ac45ce82e16559d9cc3726c46584d875815db
Radu Nitescu <radunitescu@google.com>
2025-10-02 03:12:28 -0700

    [iOS] Integrate unified detents system in the Lens detents manager
    The detents manager will now manage proxied detents, which allows
    changing the underlying implementation.
    
    Change-Id: I0377ab7b2e7c973eae2a17359bba1ab054033676
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7000111
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1524095}

--- ios/chrome/browser/lens_overlay/model/lens_overlay_detents_manager.h
+++ ios/chrome/browser/lens_overlay/model/lens_overlay_detents_manager.h
@@ -8,9 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
+#import "ios/chrome/browser/lens_overlay/model/lens_overlay_bottom_sheet.h"
 #import "ios/chrome/browser/lens_overlay/model/lens_overlay_sheet_detent_state.h"
 
 @protocol LensOverlayDetentsManagerDelegate;
+@protocol LensOverlayBottomSheetPresenter;
 
 // Manages the detents for a given bottom sheet, adapting to different detent
 // sizes.
@@ -61,7 +63,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                    (SheetDetentPresentationStategy)presentationStrategy
     NS_DESIGNATED_INITIALIZER;
 
+- (instancetype)initWithLensOverlayBottomSheet:
+                    (id<LensOverlayBottomSheet>)lensOverlayBottomSheet
+                                        window:(UIWindow*)window
+                          presentationStrategy:(SheetDetentPresentationStategy)
+                                                   presentationStrategy
+    NS_DESIGNATED_INITIALIZER;
+
 - (instancetype)init NS_UNAVAILABLE;
+
+// Adjusts the detents of the given sheet based on the sheet state.
+- (void)adjustDetentsForState:(SheetDetentState)state animated:(BOOL)animated;
 
 // Adjusts the detents of the given sheet based on the sheet state.
 - (void)adjustDetentsForState:(SheetDetentState)state;
