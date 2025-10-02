0b131c4e6a0ba62502f8517866199fac286123f7
Andreas Haas <ahaas@chromium.org>
2025-10-01 22:25:57 -0700

    [services/proxy_resolver] Add tags to uses of v8::External
    So far, all uses of v8::External shared a generic type tag. This allows
    type confusion if the memory in the V8 Heap sandbox gets corrupted. With
    this CL, type-specific tags get introduced and used by call sites of
    v8::External.
    
    Bug: 436799229
    
    This CL was uploaded by git cl split.
    
    Change-Id: Icad25bf954633cc9c76db6871207f10b7d6f2aff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7000116
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Andreas Haas <ahaas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1524009}

--- services/proxy_resolver/proxy_resolver_v8.cc
+++ services/proxy_resolver/proxy_resolver_v8.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/single_thread_task_runner.h"
 #include "gin/array_buffer.h"
 #include "gin/converter.h"
+#include "gin/public/gin_embedders.h"
 #include "gin/public/isolate_holder.h"
 #include "gin/v8_initializer.h"
 #include "net/base/ip_address.h"
@@ -517,7 +518,9 @@ class ProxyResolverV8::Context {
     v8::Isolate::Scope isolate_scope(isolate_);
     v8::HandleScope scope(isolate_);
 
-    v8_this_.Reset(isolate_, v8::External::New(isolate_, this));
+    v8_this_.Reset(
+        isolate_,
+        v8::External::New(isolate_, this, gin::kProxyResolverV8ContextTag));
     v8::Local<v8::External> v8_this =
         v8::Local<v8::External>::New(isolate_, v8_this_);
 
@@ -686,7 +689,8 @@ class ProxyResolverV8::Context {
   // V8 callback for when "alert()" is invoked by the PAC script.
   static void AlertCallback(const v8::FunctionCallbackInfo<v8::Value>& args) {
     Context* context =
-        static_cast<Context*>(v8::External::Cast(*args.Data())->Value());
+        static_cast<Context*>(v8::External::Cast(*args.Data())
+                                  ->Value(gin::kProxyResolverV8ContextTag));
 
     // Like firefox we assume "undefined" if no argument was specified, and
     // disregard any arguments beyond the first.
@@ -734,7 +738,8 @@ class ProxyResolverV8::Context {
       const v8::FunctionCallbackInfo<v8::Value>& args,
       net::ProxyResolveDnsOperation op) {
     Context* context =
-        static_cast<Context*>(v8::External::Cast(*args.Data())->Value());
+        static_cast<Context*>(v8::External::Cast(*args.Data())
+                                  ->Value(gin::kProxyResolverV8ContextTag));
 
     std::string hostname;
 
