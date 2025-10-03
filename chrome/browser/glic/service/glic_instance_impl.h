d45546d1c8447948e99a7047c9d83f36fdd2ff2f
Bryant Chandler <bryantchandler@google.com>
2025-10-03 12:56:51 -0700

    Refactor GlicInstanceImpl to remove Attach/Detach
    Remove the unused `AttachInstance()` and `DetachInstance()` methods from
    `GlicInstanceImpl` and the `InstanceCoordinatorDelegate`.
    
    Change-Id: I6a6a6964f4f30f78512515225d3372d599fdbfb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7008253
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Auto-Submit: Bryant Chandler <bryantchandler@chromium.org>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1524967}

--- chrome/browser/glic/service/glic_instance_impl.h
+++ chrome/browser/glic/service/glic_instance_impl.h
@@ -54,8 +54,6 @@ class GlicInstanceImpl : public GlicInstance,
   class InstanceCoordinatorDelegate {
    public:
     virtual ~InstanceCoordinatorDelegate() = default;
-    virtual void AttachInstance(GlicInstance* instance) = 0;
-    virtual void DetachInstance(GlicInstance* instance) = 0;
     virtual void OnInstanceOrphaned(GlicInstance* instance) = 0;
     // Called by an instance when its visibility state changes.
     virtual void OnInstanceVisibilityChanged(GlicInstance* instance,
@@ -81,8 +79,6 @@ class GlicInstanceImpl : public GlicInstance,
   // GlicSharingManagerProvider implementation.
   GlicSharingManager& sharing_manager() override;
 
-  void AttachInstance();
-  void DetachInstance();
   void CloseInstanceAndShutdown();
   bool IsShowing() const override;
 
