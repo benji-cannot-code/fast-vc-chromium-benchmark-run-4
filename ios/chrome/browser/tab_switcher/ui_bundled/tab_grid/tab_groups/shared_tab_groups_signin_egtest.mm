6b1b6bfbf9ea5a38822dbe66bf026f45e203f6c0
ginnnnnnny <ginnyhuang@chromium.org>
2025-10-03 15:58:49 -0700

    [iOS][gardening] Disable testJoinGroupSignedInDisabled on iOS 26
    Bug: 449215049
    Change-Id: I59e9315475d9ea1faf88d33cb9d58f4ea54653c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7008911
    Owners-Override: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1525068}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/shared_tab_groups_signin_egtest.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/shared_tab_groups_signin_egtest.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Foundation/Foundation.h>
 
 #import "base/feature_list.h"
+#import "base/ios/ios_util.h"
 #import "base/strings/sys_string_conversions.h"
 #import "base/test/ios/wait_util.h"
 #import "components/collaboration/public/features.h"
@@ -337,6 +338,10 @@ AppLaunchConfiguration SharedTabGroupAppLaunchConfiguration(
 
 // Tests joining a group when sign in is disabled.
 - (void)testJoinGroupSignedInDisabled {
+  // TODO(crbug.com/449215049): Re-enable the test on iOS26.
+  if (base::ios::IsRunningOnIOS26OrLater()) {
+    EARL_GREY_TEST_DISABLED(@"Test flaky on iOS 26.");
+  }
   [ChromeEarlGrey setBoolValue:NO forUserPref:prefs::kSigninAllowed];
 
   [TabGroupAppInterface mockSharedEntitiesPreview];
