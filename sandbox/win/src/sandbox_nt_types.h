04224abec46ea428571d7e4edaf1d9eed2139dcf
James Forshaw <forshaw@chromium.org>
2025-10-03 11:06:20 -0700

    Reland "[Windows] Add a sandbox string class for low-level heap allocations."
    This is a reland of commit 2f89ea6c9429d9544f4bc9a0aba2940b0b235e9c
    
    The original patch removed a call to strlen, which was reintroduced
    by the compiler. As the code was being called early in process
    startup it wasn't valid to call an imported function and this resulted
    in a crash in certain builds. This updated CL reintroduces the
    NTDLL strlen call.
    
    Original change's description:
    > [Windows] Add a sandbox string class for low-level heap allocations.
    >
    > This CL adds a class to represent a low-level heap allocated string.
    > In addition the class is then used in various NT utility functions
    > which return strings with corresponding changes to the call sites for
    > those utilities.
    >
    > Bug: 447171244
    > Change-Id: Iaaa714707b4476582b34aa12a4e99522ec1a9a41
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7000096
    > Reviewed-by: Alex Gough <ajgo@chromium.org>
    > Commit-Queue: James Forshaw <forshaw@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1524571}
    
    Bug: 447171244
    Change-Id: Icd2b9b71e9f383507b5ee998f92273f1483998b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7002993
    Commit-Queue: James Forshaw <forshaw@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1524880}

--- sandbox/win/src/sandbox_nt_types.h
+++ sandbox/win/src/sandbox_nt_types.h
@@ -36,6 +36,7 @@ struct NtExports {
   NtWaitForSingleObjectFunction          WaitForSingleObject;
   RtlAllocateHeapFunction                RtlAllocateHeap;
   RtlAnsiStringToUnicodeStringFunction   RtlAnsiStringToUnicodeString;
+  RtlInitAnsiStringFunction              RtlInitAnsiString;
   RtlCompareUnicodeStringFunction        RtlCompareUnicodeString;
   RtlCreateHeapFunction                  RtlCreateHeap;
   RtlDestroyHeapFunction                 RtlDestroyHeap;
@@ -43,7 +44,6 @@ struct NtExports {
   RtlNtStatusToDosErrorFunction          RtlNtStatusToDosError;
   UNSAFE_BUFFER_USAGE _strnicmpFunction  _strnicmp;
   UNSAFE_BUFFER_USAGE strlenFunction     strlen;
-  UNSAFE_BUFFER_USAGE wcslenFunction     wcslen;
   UNSAFE_BUFFER_USAGE memcpyFunction     memcpy;
 };
 // clang-format on
