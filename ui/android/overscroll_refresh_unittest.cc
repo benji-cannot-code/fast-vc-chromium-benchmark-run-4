8b32a28a112372e93570079deed97ed4e3de12a7
Grace Cham <hscham@google.com>
2025-10-03 07:31:00 -0700

    Reland "Add unittest swipe-to-navigate not triggered when not start from edge"
    This reverts commit 965c80984d87d88d425a42f69876ee1019756b14.
    
    Reason for revert: fixed test on "android-arm64-tests" by initializing
    viewport size properly
    
    Original change's description:
    > Revert "Add unittest swipe-to-navigate not triggered when not start from edge"
    >
    > This reverts commit ba410e11c03437d3bb848322da5704734711c8db.
    >
    > Reason for revert: The test is failing on builder "android-arm64-tests"
    >
    > Original change's description:
    > > Add unittest swipe-to-navigate not triggered when not start from edge
    > >
    > > On horizontal overscroll, if initial position is away from horizontal
    > > edges, the refresh effect should call Reset() to change consumption
    > > state back to disabled, since we already know that this is a horizontal
    > > overscroll that would not activate a swipe-to-navigate effect, and not
    > > waiting for future scroll update.
    > >
    > > The new test case will be modified when we change the behavior to also
    > > be dependent on source device.
    > >
    > > Bug: 364370338
    > > Test: ui/android/overscroll_refresh_unittest.cc
    > > Change-Id: I4b08c446f23c045ced0e535cf78b928bfaea4df8
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6998887
    > > Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    > > Commit-Queue: Grace Cham <hscham@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1523268}
    >
    > Bug: 448651861
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: Ia0ff000897e03e17e1205d86b47b99d7240a0f6d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7004225
    > Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    > Auto-Submit: Ritika Gupta <ritikagup@google.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    > Owners-Override: Ritika Gupta <ritikagup@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1523664}
    
          "ui_android_unittests on Android device Pixel 4" compile-and-test\
          -- \
          --gtest_filter=OverscrollRefreshTest.NavigateNotTriggeredIfStartAwayFromEdge \
          https://ci.chromium.org/ui/inv/u-hscham-2025-10-03-01-28-00-b0767d049cb1a5a7
    
    Bug: 448651861
    Test: vpython3 tools/utr -p chrome -B ci -b android-arm64-tests -t \
    Change-Id: I2f2d59949ed77127c01071d24f90f100325cb7f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7005464
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Auto-Submit: Grace Cham <hscham@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1524767}

--- ui/android/overscroll_refresh_unittest.cc
+++ ui/android/overscroll_refresh_unittest.cc
@@ -4,11 +4,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "ui/android/overscroll_refresh.h"
+
 #include "base/android/scoped_java_ref.h"
 #include "cc/input/overscroll_behavior.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/android/overscroll_refresh_handler.h"
 #include "ui/gfx/geometry/point_f.h"
+#include "ui/gfx/geometry/vector2d_f.h"
 
 namespace ui {
 
@@ -480,6 +482,11 @@ TEST_F(OverscrollRefreshTest, OverscrollBehaviorYNonePreventsTriggerStart) {
 TEST_F(OverscrollRefreshTest, TriggerSwipeToNavigate) {
   OverscrollRefresh effect(this, kDefaultEdgeWidth);
 
+  // Make sure viewport width is larger than edge width * 2.
+  gfx::SizeF viewport(100, 100);
+  gfx::SizeF content_size(100, 10000);
+  effect.OnFrameUpdated(viewport, gfx::PointF(), content_size, true);
+
   EXPECT_FALSE(effect.IsActive());
   EXPECT_FALSE(effect.IsAwaitingScrollUpdateAck());
 
@@ -518,6 +525,31 @@ TEST_F(OverscrollRefreshTest, TriggerSwipeToNavigate) {
   EXPECT_FALSE(effect.IsActive());
   EXPECT_TRUE(GetAndResetPullReleased());
   EXPECT_TRUE(GetAndResetRefreshAllowed());
+}
+
+TEST_F(OverscrollRefreshTest, NavigateNotTriggeredIfStartAwayFromEdge) {
+  OverscrollRefresh effect(this, kDefaultEdgeWidth);
+
+  // Make sure viewport width is larger than edge width * 2.
+  gfx::SizeF viewport(100, 100);
+  gfx::SizeF content_size(100, 10000);
+  effect.OnFrameUpdated(viewport, gfx::PointF(), content_size, true);
+
+  // Start from position with x-coordinate towards center by edge width.
+  auto startPos = kStartPos + gfx::Vector2dF(kDefaultEdgeWidth, 0);
+  effect.OnScrollBegin(startPos);
+  EXPECT_FALSE(effect.IsActive());
+  EXPECT_TRUE(effect.IsAwaitingScrollUpdateAck());
+
+  gfx::Vector2dF scroll_right = gfx::Vector2dF(10, 0);
+  effect.OnOverscrolled(cc::OverscrollBehavior(), scroll_right,
+                        blink::WebGestureDevice::kTouchscreen);
+  EXPECT_FALSE(effect.IsActive());
+  EXPECT_FALSE(effect.IsAwaitingScrollUpdateAck());
+  EXPECT_FALSE(effect.WillHandleScrollUpdate(gfx::Vector2dF(500, 0)));
+  effect.OnScrollEnd(gfx::Vector2dF());
+  EXPECT_FALSE(GetAndResetPullStarted());
+  EXPECT_FALSE(GetAndResetPullReleased());
 }
 
 TEST_F(OverscrollRefreshTest, OverscrollBehaviorXAutoTriggersStart) {
