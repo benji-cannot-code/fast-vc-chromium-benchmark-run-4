082918f4c2f2a999bb966d1984089af7dff141c7
Sylvain Defresne <sdefresne@chromium.org>
2025-10-03 02:09:28 -0700

    [ios] Fix a crash when +[NSLocale preferredLanguages] is empty
    The documentation for +[NSLocale preferredLanguages] does not mention
    that the value returned will always be empty, and some users indeed
    crash on startup when accessing the object at index 0.
    
    Replace usage of `-objectAtIndex:` with `-firstObject` which returns
    nil if the array is empty and thus is safe to call. Adapt the client
    code to work if the value is nil (as storing nil in NSUserDefaults is
    forbidden).
    
    Fixed: 448293042
    Change-Id: I87ca95efa62ae9ce84df04dbef7d0190233ddfa5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7003045
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1524673}

--- components/previous_session_info/previous_session_info.mm
+++ components/previous_session_info/previous_session_info.mm
@@ -258,7 +258,7 @@ static PreviousSessionInfo* gSharedInstance = nil;
         lastSystemStartTime;
 
     NSString* lastRanLanguage = [defaults stringForKey:kLastRanLanguage];
-    NSString* currentLanguage = [[NSLocale preferredLanguages] objectAtIndex:0];
+    NSString* currentLanguage = [[NSLocale preferredLanguages] firstObject];
     gSharedInstance.isFirstSessionAfterLanguageChange =
         ![lastRanLanguage isEqualToString:currentLanguage];
 
@@ -324,8 +324,8 @@ static PreviousSessionInfo* gSharedInstance = nil;
   [defaults setObject:currentOSVersion forKey:kPreviousSessionInfoOSVersion];
 
   // Set the current language.
-  NSString* currentLanguage = [[NSLocale preferredLanguages] objectAtIndex:0];
-  [defaults setObject:currentLanguage forKey:kLastRanLanguage];
+  NSString* currentLanguage = [[NSLocale preferredLanguages] firstObject];
+  [defaults setObject:(currentLanguage ?: @"") forKey:kLastRanLanguage];
 
   // Clear the memory warning flag.
   [defaults
