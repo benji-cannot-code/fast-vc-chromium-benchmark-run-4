eceaf392846e97419b8c8bef3b31bf9772638ec3
Daniel White <danieltwhite@google.com>
2025-10-03 08:45:35 -0700

    [iOS] iOS 16 deprecation code cleanup
    Each year the Chrome for iOS team increases the application's minimum
    supported iOS version. During the process, run/compile-time flags are
    added throughout the codebase to transitions areas of the code using
    deprecated APIs to the newer variants. Once the minimum supported
    version is bumped, these guards are no longer needed.
    
    This CL is part of a large-scale effort to remove the remaining
    obsolete guards from the application's codebase.
    
    This CL was uploaded by git cl split.
    
    R=fedegermi@google.com
    
    Bug: 403260493
    Change-Id: I96b460593d3aa2926b5dc8ee6748d599019d79b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7003640
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Auto-Submit: Daniel White <danieltwhite@google.com>
    Cr-Commit-Position: refs/heads/main@{#1524805}

--- ios/chrome/browser/browser_view/ui_bundled/browser_view_controller.mm
+++ ios/chrome/browser/browser_view/ui_bundled/browser_view_controller.mm
@@ -923,15 +923,13 @@ const CGFloat kTopDynamicIslandInset = 24;
     self.view.overrideUserInterfaceStyle = UIUserInterfaceStyleDark;
   }
 
-  if (@available(iOS 17, *)) {
-    NSArray<UITrait>* traits = TraitCollectionSetForTraits(nil);
-    __weak __typeof(self) weakSelf = self;
-    UITraitChangeHandler handler = ^(id<UITraitEnvironment> traitEnvironment,
-                                     UITraitCollection* previousCollection) {
-      [weakSelf updateUIOnTraitChange:previousCollection];
-    };
-    [self registerForTraitChanges:traits withHandler:handler];
-  }
+  NSArray<UITrait>* traits = TraitCollectionSetForTraits(nil);
+  __weak __typeof(self) weakSelf = self;
+  UITraitChangeHandler handler = ^(id<UITraitEnvironment> traitEnvironment,
+                                   UITraitCollection* previousCollection) {
+    [weakSelf updateUIOnTraitChange:previousCollection];
+  };
+  [self registerForTraitChanges:traits withHandler:handler];
 }
 
 - (void)viewSafeAreaInsetsDidChange {
@@ -1731,10 +1729,8 @@ const CGFloat kTopDynamicIslandInset = 24;
 
 // Notifies or modifies BVC owned UI elements when a UITrait has been changed.
 - (void)updateUIOnTraitChange:(UITraitCollection*)previousTraitCollection {
-  if (@available(iOS 17.0, *)) {
-    if (base::FeatureList::IsEnabled(kEnableTraitCollectionWorkAround)) {
-      [self updateTraitsIfNeeded];
-    }
+  if (base::FeatureList::IsEnabled(kEnableTraitCollectionWorkAround)) {
+    [self updateTraitsIfNeeded];
   }
 
   // After `-shutdown` is called, profile is invalid and will cause a
