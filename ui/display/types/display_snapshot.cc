88f2b04aba1fc039633afac8220068b722beb741
Colin Blundell <blundell@chromium.org>
2025-10-03 00:19:01 -0700

    [//ui] Add DisplaySnapshot::PrimarySharedImageFormat()
    Will be used to transition callers of DisplaySnapshot::PrimaryFormat().
    Once all callers are converted, will rename this method PrimaryFormat().
    
    Bug: 445879532
    Change-Id: Idbacb39d814212b59a2bcd85a1f6a4b9bd699e3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7006509
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1524631}

--- ui/display/types/display_snapshot.cc
+++ ui/display/types/display_snapshot.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/strings/stringprintf.h"
+#include "components/viz/common/resources/shared_image_format_utils.h"
 
 namespace display {
 
@@ -157,7 +158,13 @@ std::string DisplaySnapshot::ToString() const {
 
 // static
 gfx::BufferFormat DisplaySnapshot::PrimaryFormat() {
-  return gfx::BufferFormat::BGRA_8888;
+  return viz::SinglePlaneSharedImageFormatToBufferFormat(
+      PrimarySharedImageFormat());
+}
+
+// static
+viz::SharedImageFormat DisplaySnapshot::PrimarySharedImageFormat() {
+  return viz::SinglePlaneFormat::kBGRA_8888;
 }
 
 void DisplaySnapshot::AddIndexToDisplayId() {
