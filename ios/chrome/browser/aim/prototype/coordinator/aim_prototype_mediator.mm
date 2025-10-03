8b592dcd573e7ec13d1f0871353fac2d3cb1b8e2
Ameur Hosni <ameurhosni@google.com>
2025-10-03 04:22:51 -0700

    [IOS][AIM] Refrain from auto-triggering AIM
    Bug: 448635997
    Change-Id: I4c263b45204274101680bdb93e4b15c2b0797038
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7002911
    Auto-Submit: Ameur Hosni <ameurhosni@google.com>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1524708}

--- ios/chrome/browser/aim/prototype/coordinator/aim_prototype_mediator.mm
+++ ios/chrome/browser/aim/prototype/coordinator/aim_prototype_mediator.mm
@@ -46,11 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-/// Minimum number of whitespace to auto trigger AIM.
-constexpr size_t kMinWhitespaceTriggerAIM = 1;
-/// Minimum number of characters to auto trigger AIM.
-constexpr size_t kMinCharTriggerAIM = 10;
-
 // Reads data from a file URL. Runs on a background thread.
 NSData* ReadDataFromURL(GURL url) {
   NSURL* ns_url = net::NSURLWithGURL(url);
@@ -115,14 +110,6 @@ CreateInputDataFromAnnotatedPageContent(
   input_data->page_url = web_state->GetVisibleURL();
   input_data->page_title = base::UTF16ToUTF8(web_state->GetTitle());
   return input_data;
-}
-
-/// Returns the number of whitespace in `string`.
-size_t WhitespaceCount(const std::u16string& string) {
-  return std::count_if(
-      string.begin(), string.end(),
-      [](unsigned char c) { return std::isspace(c); }  // The condition
-  );
 }
 
 }  // namespace
@@ -592,14 +579,6 @@ size_t WhitespaceCount(const std::u16string& string) {
          userInputInProgress:(BOOL)userInputInProgress {
   // Update mic button visibility.
   [self.consumer hideMicButton:text.length()];
-
-  // Auto trigger AIM if conditions are met.
-  if (!_AIModeEnabled && userInputInProgress && isSearchQuery) {
-    if (text.length() >= kMinCharTriggerAIM &&
-        WhitespaceCount(text) >= kMinWhitespaceTriggerAIM) {
-      [self.consumer setAIModeEnabled:YES];
-    }
-  }
 }
 
 #pragma mark - Private helpers
