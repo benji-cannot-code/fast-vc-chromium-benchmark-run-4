6da2b36ddd8ebf875443b05d000f132bd8add6f7
Calder Kitagawa <ckitagawa@chromium.org>
2025-10-03 15:33:48 -0700

    [Tab Menu] Stop swallowing touch inputs in TouchTrackingListView
    The implementation was swallowing touch inputs by not calling the super
    method and preventing scrolling.
    
    Also change the layout_height to match_parent (the default behavior
    inside PopupWindow) so the ListView is not larger than the PopupWindow.
    
    Fixed: 448646859
    Change-Id: I29a122eab13c54031da05764bcc65541677f26a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7003302
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Reviewed-by: Jenna Himawan <jhimawan@google.com>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1525054}

--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/list_view/TouchTrackingListView.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/list_view/TouchTrackingListView.java
@@ -82,7 +82,7 @@ public final class TouchTrackingListView extends ListView implements ListViewTou
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
         mGestureDetector.onTouchEvent(ev);
-        return false;
+        return super.onInterceptTouchEvent(ev);
     }
 
     @Override
