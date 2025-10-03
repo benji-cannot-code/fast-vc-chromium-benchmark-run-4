0da8230a715371f05289f6a0cbe729a593fd8e0c
Shu Yang <shuyng@google.com>
2025-10-03 15:52:22 -0700

    Revert "Remove flag NativePageTransitionHardwareCapture"
    This reverts commit 8967a3aed90544dffd39041b24c977322546c286.
    
    Reason for revert:
    ScreenshotCaptureTest#testNavigatingAwayFromNtpToNormalPage
    is consistently failing.
    
    Original change's description:
    > Remove flag NativePageTransitionHardwareCapture
    >
    > Bug: 434928245
    > Change-Id: I3a1201e68022f9743de8327eac72c2dbea8c6c5c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7003568
    > Auto-Submit: Aldo Culquicondor <acondor@chromium.org>
    > Commit-Queue: Aldo Culquicondor <acondor@chromium.org>
    > Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    > Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1524892}
    
    Bug: 434928245
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I80ca2fde7f7b708a667760b769af7049f21f70ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7007522
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Shu Yang <shuyng@google.com>
    Owners-Override: Shu Yang <shuyng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1525065}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -360,6 +360,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kMostVisitedTilesReselect,
     &kMultiInstanceApplicationStatusCleanup,
     &kMvcUpdateViewWhenModelChanged,
+    &kNativePageTransitionHardwareCapture,
     &kNavBarColorAnimation,
     &kNotificationPermissionVariant,
     &kNotificationPermissionBottomSheet,
@@ -926,6 +927,9 @@ BASE_FEATURE(kMultiInstanceApplicationStatusCleanup,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kMvcUpdateViewWhenModelChanged, base::FEATURE_ENABLED_BY_DEFAULT);
+
+BASE_FEATURE(kNativePageTransitionHardwareCapture,
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kNavBarColorAnimation, base::FEATURE_ENABLED_BY_DEFAULT);
 
