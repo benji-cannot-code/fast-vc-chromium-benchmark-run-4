73ef92c793619d954525b81605b2b19b7118dae4
Jun Ishiguro <junis@google.com>
2025-10-02 22:27:37 -0700

    Do not take Welcome Recap screenshot if user session is not active
    This CL updates the condition to take a screenshot for Welcome Recap so
    that it may not be taken if user session is not active (e.g., when the
    user adding screen is visible after password is entered.)
    
    Bug: b:445921830
    Change-Id: Iae9727072e3d18b8bbb5155fdefb30d607e99cf4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6998501
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1524603}

--- ash/wm/window_restore/window_restore_metrics.h
+++ ash/wm/window_restore/window_restore_metrics.h
@@ -40,7 +40,8 @@ enum class ScreenshotOnShutdownStatus {
   kFailedInGuestOrPublicUserSession,
   kFailedOtherUserIsActive,
   kFailedWithVisibleWindowFromOtherUser,
-  kMaxValue = kFailedWithVisibleWindowFromOtherUser,
+  kFailedSessionIsNotActive,
+  kMaxValue = kFailedSessionIsNotActive,
 };
 
 // Enumeration of the ways the informed restore dialog could be closed. Used for
