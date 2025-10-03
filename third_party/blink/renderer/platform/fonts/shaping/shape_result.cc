0ac9156835c24b607cfa54e16f8278979f28c405
Ian Kilpatrick <ikilpatrick@chromium.org>
2025-10-02 20:29:01 -0700

    [cleanup] Remove DeprecatedInkBounds.
    This patch removes DeprecatedInkBounds on ShapeResult making this
    object (with GC header) 64 bytes.
    
    This *might* have a marginal performance improvement due to this.
    
    👎 Charts-chartjs                              [ +0.3%,  +1.2%]
       Score                                       [ -0.0%,  +0.2%]
    👍 TodoMVC-Vue                                 [ -0.7%,  -0.2%]
    👍 TodoMVC-WebComponents                       [ -0.5%,  -0.1%]
    
    Bug: 448683997
    Change-Id: I64ecb44f3a6ecbeeec91d7297e9dc9dbfb4a4637
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6700589
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1524581}

--- third_party/blink/renderer/platform/fonts/shaping/shape_result.cc
+++ third_party/blink/renderer/platform/fonts/shaping/shape_result.cc
@@ -87,7 +87,6 @@ ASSERT_SIZE(ShapeResultCharacterData, SameSizeAsShapeResultCharacterData);
 struct SameSizeAsShapeResult {
   Vector<int> character_position_;
   Vector<UntracedMember<void*>, 1> runs_;
-  UntracedMember<void*> deprecated_ink_bounds_;
   float width;
   unsigned start_index_;
   unsigned bitfields;
@@ -430,7 +429,6 @@ ShapeResult::ShapeResult(const ShapeResult& other)
 ShapeResult::~ShapeResult() = default;
 
 void ShapeResult::Trace(Visitor* visitor) const {
-  visitor->Trace(deprecated_ink_bounds_);
   visitor->Trace(runs_);
   visitor->Trace(character_position_);
 }
