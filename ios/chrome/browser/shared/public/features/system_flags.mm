08225b6afb7aeb6ed305a0a284eb26e3bdc5f0cb
Jérôme Lebel <jlebel@chromium.org>
2025-10-03 09:48:58 -0700

    [iOS] Add fullscreen sign-in promo in the experimental flags
    This patch removes UpgradePromoForceEnabled and adds
    "Fullscreen sign-in" value in "Force Promo" experimental flags.
    
    Fixed: 448876793
    Change-Id: I9efd62ad2afaed7ccfbeb6c8a5c0b128971ef7c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7003798
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1524841}

--- ios/chrome/browser/shared/public/features/system_flags.mm
+++ ios/chrome/browser/shared/public/features/system_flags.mm
@@ -31,7 +31,6 @@ NSString* const kAlternateDiscoverFeedServerURL =
 NSString* const kEnableStartupCrash = @"EnableStartupCrash";
 NSString* const kFirstRunForceEnabled = @"FirstRunForceEnabled";
 NSString* const kFirstRunForceDisabled = @"FirstRunForceDisabled";
-NSString* const kUpgradePromoForceEnabled = @"UpgradePromoForceEnabled";
 NSString* const kOriginServerHost = @"AlternateOriginServerHost";
 NSString* const kWhatsNewPromoStatus = @"WhatsNewPromoStatus";
 NSString* const kClearApplicationGroup = @"ClearApplicationGroup";
@@ -80,11 +79,6 @@ bool AlwaysDisplayFirstRun() {
 bool NeverDisplayFirstRun() {
   return
       [[NSUserDefaults standardUserDefaults] boolForKey:kFirstRunForceDisabled];
-}
-
-bool AlwaysDisplayUpgradePromo() {
-  return [[NSUserDefaults standardUserDefaults]
-      boolForKey:kUpgradePromoForceEnabled];
 }
 
 NSString* GetOriginServerHost() {
