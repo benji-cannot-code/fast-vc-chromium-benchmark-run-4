62d397d1966af02c6e4e09bb695ad3b92a1af448
Ziad Youssef <ziadyoussef@chromium.org>
2025-10-03 12:09:27 -0700

    Update logic used for APK type synthetic trial
    We invalidated the original logic by adding library dep to standalone. I
    updated the logic to use the library name in the manifest
    
    Bug: 438943049
    Test: manual
    Change-Id: I4ffa37d07281171aaed3e3dff29a2326d87cadb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7008330
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Commit-Queue: Ziad Youssef <ziadyoussef@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1524907}

--- android_webview/browser/aw_apk_type.h
+++ android_webview/browser/aw_apk_type.h
@@ -9,7 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace android_webview {
 
 // GENERATED_JAVA_ENUM_PACKAGE: org.chromium.android_webview
-enum class ApkType { STANDALONE = 0, MONOCHROME = 1, TRICHROME = 2 };
+enum class ApkType {
+  STANDALONE = 0,
+  MONOCHROME = 1,
+  TRICHROME = 2,
+  UNKNOWN = 3
+};
 
 }  // namespace android_webview
 
