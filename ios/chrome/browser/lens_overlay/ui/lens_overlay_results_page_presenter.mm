90c952089671f967b22d89a7a624dad35ecc9593
Radu Nitescu <radunitescu@google.com>
2025-10-03 02:06:38 -0700

    [iOS] Disable gesture workaround in Lens Overlay for custom bottom sheet
    This is not needed for the new presentation.
    
    Change-Id: Id93aaa6840f3ccdd3a9659ce5e33611aeee0a8f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7003010
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1524672}

--- ios/chrome/browser/lens_overlay/ui/lens_overlay_results_page_presenter.mm
+++ ios/chrome/browser/lens_overlay/ui/lens_overlay_results_page_presenter.mm
@@ -243,7 +243,8 @@ const CGFloat kSidePanelHorizontalOcclusionInset = 24.0f;
   // To prevent such a behavior, extract the recognizers added as a consequence
   // of presenting and allow touches to be delivered to views.
   __block NSSet<UIGestureRecognizer*>* panRecognizersBeforePresenting =
-      [self panGestureRecognizersOnWindow];
+      UseCustomLensOverlayBottomSheet() ? [[NSSet alloc] init]
+                                        : [self panGestureRecognizersOnWindow];
 
   [self setUpVisibleAreaLayoutGuideIfNeeded];
   [self monitorResultsBottomSheetPosition];
@@ -258,7 +259,10 @@ const CGFloat kSidePanelHorizontalOcclusionInset = 24.0f;
 
   ProceduralBlock afterPresentation = ^{
     [weakSelf resultsPagePresentationDidAppear];
-    [weakSelf handlePanRecognizersAddedAfter:panRecognizersBeforePresenting];
+    if (!UseCustomLensOverlayBottomSheet()) {
+      [weakSelf handlePanRecognizersAddedAfter:panRecognizersBeforePresenting];
+    }
+
     if (completion) {
       completion();
     }
