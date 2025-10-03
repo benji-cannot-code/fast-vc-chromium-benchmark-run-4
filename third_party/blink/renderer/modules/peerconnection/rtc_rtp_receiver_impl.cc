945ef47910ae0726ea86ce5d2f7485b1b9781ed4
Tony Herre <toprice@chromium.org>
2025-10-03 02:23:48 -0700

    Cleanup launched RTCAlignReceivedEncodedVideoTransforms
    Bug: 40942405
    Change-Id: I98a681fd6055ca7d0350037a7e9422b53d3c69cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7003035
    Reviewed-by: Palak Agarwal <agpalak@chromium.org>
    Auto-Submit: Tony Herre <toprice@chromium.org>
    Commit-Queue: Palak Agarwal <agpalak@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1524681}

--- third_party/blink/renderer/modules/peerconnection/rtc_rtp_receiver_impl.cc
+++ third_party/blink/renderer/modules/peerconnection/rtc_rtp_receiver_impl.cc
@@ -21,9 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-BASE_FEATURE(kRTCAlignReceivedEncodedVideoTransforms,
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 RtpReceiverState::RtpReceiverState(
     scoped_refptr<base::SingleThreadTaskRunner> main_task_runner,
     scoped_refptr<base::SingleThreadTaskRunner> signaling_task_runner,
@@ -163,10 +160,7 @@ class RTCRtpReceiverImpl::RTCRtpReceiverInternal
       CHECK(webrtc_receiver_->media_type() == webrtc::MediaType::VIDEO);
       encoded_video_transformer_ =
           std::make_unique<RTCEncodedVideoStreamTransformer>(
-              main_task_runner_, base::FeatureList::IsEnabled(
-                                     kRTCAlignReceivedEncodedVideoTransforms)
-                                     ? std::move(decode_metronome)
-                                     : nullptr);
+              main_task_runner_, std::move(decode_metronome));
       webrtc_receiver_->SetFrameTransformer(
           encoded_video_transformer_->Delegate());
     }
