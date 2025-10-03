da368e9d471d68af575ab91697257384d2beb822
Charles Hager <clhager@google.com>
2025-10-03 10:21:28 -0700

    Default enable clipboard data controls feature
    Bug: 435698331
    Change-Id: I2f4577364e279f1db3de1ee426b9c1236a4e6649
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7006328
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Charles Hager <clhager@google.com>
    Cr-Commit-Position: refs/heads/main@{#1524856}

--- components/enterprise/data_controls/core/browser/features.cc
+++ components/enterprise/data_controls/core/browser/features.cc
@@ -9,7 +9,7 @@ namespace data_controls {
 
 #if BUILDFLAG(IS_ANDROID)
 BASE_FEATURE(kEnableClipboardDataControlsAndroid,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 #endif
 
 BASE_FEATURE(kEnableDownloadDataControls, base::FEATURE_DISABLED_BY_DEFAULT);
