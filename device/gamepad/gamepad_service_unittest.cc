4e2fe39478a0372657f6006eb9262e5318aa1227
Ming-Ying Chung <mych@chromium.org>
2025-10-02 21:58:54 -0700

    [gardener] Disable flaky `RemoveGamepadTwice` test
    This target has been flaky on linux TSan [since it was added][1]
    
    [1]: https://ci.chromium.org/ui/test/chromium/ninja%3A%2F%2Fdevice%3Adevice_unittests%2FGamepadServiceSimulationTest.RemoveGamepadTwice?q=VHASH%3Af90d8116e685d199
    
    
    Bug: 448993918
    Change-Id: Ibbb914f1b6279eca39d441b80fbe4e96dce8768a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7007671
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Owners-Override: Ming-Ying Chung <mych@chromium.org>
    Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1524597}

--- device/gamepad/gamepad_service_unittest.cc
+++ device/gamepad/gamepad_service_unittest.cc
@@ -1001,7 +1001,8 @@ TEST_F(GamepadServiceSimulationTest, TokenNotFound) {
   service()->SimulateInputFrame(random_token);
 }
 
-TEST_F(GamepadServiceSimulationTest, RemoveGamepadTwice) {
+// TODO(crbug.com/448993918): Re-enable this test
+TEST_F(GamepadServiceSimulationTest, DISABLED_RemoveGamepadTwice) {
   // Mark `consumer` active.
   auto* consumer = CreateConsumer();
   EXPECT_TRUE(service()->ConsumerBecameActive(consumer));
