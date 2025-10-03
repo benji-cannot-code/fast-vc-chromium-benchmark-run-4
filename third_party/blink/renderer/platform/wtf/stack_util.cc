a4b44696ee5a799f79abcdd6c60b915b0db406a8
Kent Tamura <tkent@chromium.org>
2025-10-03 01:37:41 -0700

    Convert `#pragma allow_unsafe_buffers` in stack_util.cc to UNSAFE_TODO
    This CL has no behavior changes.
    
    Bug: 351564777
    Change-Id: I38628597d3ad213acd33c2fa09fc7b45c4c7da80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7007750
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1524656}

--- third_party/blink/renderer/platform/wtf/stack_util.cc
+++ third_party/blink/renderer/platform/wtf/stack_util.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "third_party/blink/renderer/platform/wtf/stack_util.h"
 
 #include "build/build_config.h"
@@ -128,7 +123,7 @@ void* GetStackStartImpl() {
     error = pthread_attr_getstack(&attr, &base, &size);
     CHECK(!error);
     pthread_attr_destroy(&attr);
-    return reinterpret_cast<uint8_t*>(base) + size;
+    return UNSAFE_TODO(reinterpret_cast<uint8_t*>(base) + size);
   }
 #if BUILDFLAG(IS_FREEBSD)
   pthread_attr_destroy(&attr);
@@ -229,7 +224,7 @@ size_t ThreadStackSize() {
   // tracks the end of the committed range. We're after the end of the reserved
   // stack area (most of which will be uncommitted, most times.)
   MEMORY_BASIC_INFORMATION stack_info;
-  memset(&stack_info, 0, sizeof(MEMORY_BASIC_INFORMATION));
+  UNSAFE_TODO(memset(&stack_info, 0, sizeof(MEMORY_BASIC_INFORMATION)));
   size_t result_size =
       VirtualQuery(&stack_info, &stack_info, sizeof(MEMORY_BASIC_INFORMATION));
   DCHECK_GE(result_size, sizeof(MEMORY_BASIC_INFORMATION));
