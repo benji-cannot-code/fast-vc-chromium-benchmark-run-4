5b46be71545f5d63b708cb339ce5ee3375ba0509
Ameur Hosni <ameurhosni@google.com>
2025-10-03 05:49:14 -0700

    [IOS][Omnibox] Fix autocomplete input clear crash
    The crash is due a bad access to AutocompleteInput method.
    
    Bug: 448290547
    Change-Id: I1bef6c0862becab346b164f80234ce8c1339b322
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7002934
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1524729}

--- ios/chrome/browser/omnibox/model/omnibox_text_controller.mm
+++ ios/chrome/browser/omnibox/model/omnibox_text_controller.mm
@@ -221,6 +221,10 @@ const char kOmniboxFocusResultedInNavigation[] =
 }
 
 - (void)revertState {
+  if (!_omniboxTextModel) {
+    return;
+  }
+
   [self setInputInProgress:NO];
   _omniboxTextModel->input.Clear();
   _omniboxTextModel->paste_state = OmniboxPasteState::kNone;
