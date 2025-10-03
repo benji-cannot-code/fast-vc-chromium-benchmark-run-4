850ec0afea45e00c7b52d3604a88d3a55393fac7
Steven Luong <stluong@chromium.org>
2025-10-03 14:16:15 -0700

    Move side panel header code out of SidePanelCoordinator
    The side panel header code should be moved out of the
    SidePanelCoordinator and into the side_panel_header.h/.cc file instead
    so that the coordinator can focus on coodinating which side panel
    entry to show instead of also needing to layout the header as well.
    
    Fixed: 448129053
    Change-Id: Ia7bc90632321fe0c9dd722748cc38818644de865
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7005045
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Steven Luong <stluong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1525021}

--- chrome/browser/ui/views/side_panel/side_panel_coordinator.h
+++ chrome/browser/ui/views/side_panel/side_panel_coordinator.h
@@ -30,15 +30,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/view_observer.h"
 
 class BrowserView;
+class SidePanelHeader;
 
 namespace actions {
 class ActionItem;
 }  // namespace actions
 
 namespace views {
-class ImageButton;
-class MenuRunner;
-class ToggleImageButton;
 class View;
 }  // namespace views
 
@@ -94,15 +92,9 @@ class SidePanelCoordinator final : public SidePanelUIBase,
 
   SidePanelEntry* GetCurrentSidePanelEntryForTesting();
 
-  views::ToggleImageButton* GetHeaderPinButtonForTesting() {
-    return header_pin_button_;
-  }
-
-  views::ImageButton* GetHeaderMoreInfoButtonForTesting() {
-    return header_more_info_button_;
-  }
-
   SidePanelEntry* GetLoadingEntryForTesting() const;
+
+  SidePanelHeader* GetSidePanelHeaderForTesting() { return side_panel_header_; }
 
  private:
   friend class SidePanelCoordinatorTest;
@@ -151,8 +143,6 @@ class SidePanelCoordinator final : public SidePanelUIBase,
   // The key is the unique key of the action item that has changed.
   void OnActionItemChanged(UniqueKey key);
 
-  std::unique_ptr<views::View> CreateHeader();
-
   void NotifyPinnedContainerOfActiveStateChange(SidePanelEntryKey key,
                                                 bool show_active_in_toolbar);
 
@@ -190,23 +180,8 @@ class SidePanelCoordinator final : public SidePanelUIBase,
   // item associated with the side panel entry changes.
   base::CallbackListSubscription action_item_controller_subscription_;
 
-  // Used to update icon in the side panel header.
-  raw_ptr<views::ImageView, AcrossTasksDanglingUntriaged> panel_icon_ = nullptr;
-
-  // Used to update the displayed title in the side panel header.
-  raw_ptr<views::Label, AcrossTasksDanglingUntriaged> panel_title_ = nullptr;
-
-  // Used to update the visibility of the 'Open in New Tab' header button.
-  raw_ptr<views::ImageButton, AcrossTasksDanglingUntriaged>
-      header_open_in_new_tab_button_ = nullptr;
-
-  // Used to update the visibility of the pin header button.
-  raw_ptr<views::ToggleImageButton, AcrossTasksDanglingUntriaged>
-      header_pin_button_ = nullptr;
-
-  // Used to update the visibility of the more info button.
-  raw_ptr<views::ImageButton, AcrossTasksDanglingUntriaged>
-      header_more_info_button_ = nullptr;
+  raw_ptr<SidePanelHeader, AcrossTasksDanglingUntriaged> side_panel_header_ =
+      nullptr;
 
   // Model for the more info menu.
   std::unique_ptr<ui::MenuModel> more_info_menu_model_;
--- chrome/browser/ui/views/side_panel/side_panel_coordinator_browsertest.cc
+++ chrome/browser/ui/views/side_panel/side_panel_coordinator_browsertest.cc
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/side_panel/side_panel_entry.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_entry_id.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_entry_observer.h"
+#include "chrome/browser/ui/views/side_panel/side_panel_header.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_registry.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_util.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_view.h"
@@ -182,7 +183,10 @@ class SidePanelCoordinatorTest : public InProcessBrowserTest {
   }
 
   std::u16string_view GetTitleText() {
-    return coordinator()->panel_title_->GetText();
+    return coordinator()
+        ->GetSidePanelHeaderForTesting()
+        ->panel_title()
+        ->GetText();
   }
 
   void AddTabToBrowser(const GURL& tab_url) {
@@ -2283,7 +2287,10 @@ IN_PROC_BROWSER_TEST_F(SidePanelCoordinatorTest,
   coordinator()->SetNoDelaysForTesting(true);
   coordinator()->DisableAnimationsForTesting();
   coordinator()->Show(SidePanelEntry::Id::kBookmarks);
-  EXPECT_TRUE(coordinator()->GetHeaderPinButtonForTesting()->GetVisible());
+  EXPECT_TRUE(coordinator()
+                  ->GetSidePanelHeaderForTesting()
+                  ->header_pin_button()
+                  ->GetVisible());
 
   // Make a guest window. This process can be either synchronous or
   // asynchronous, so use RunUntil.
@@ -2300,7 +2307,9 @@ IN_PROC_BROWSER_TEST_F(SidePanelCoordinatorTest,
   coordinator->SetNoDelaysForTesting(true);
   coordinator->DisableAnimationsForTesting();
   coordinator->Show(SidePanelEntry::Id::kBookmarks);
-  EXPECT_FALSE(coordinator->GetHeaderPinButtonForTesting()->GetVisible());
+  EXPECT_FALSE(coordinator->GetSidePanelHeaderForTesting()
+                   ->header_pin_button()
+                   ->GetVisible());
 }
 #endif  // !BUILDFLAG(IS_CHROMEOS)
 
@@ -2320,7 +2329,7 @@ IN_PROC_BROWSER_TEST_F(SidePanelCoordinatorTest,
       GetKeyForExtension(extension->id())));
 
   views::ToggleImageButton* pin_button =
-      coordinator()->GetHeaderPinButtonForTesting();
+      coordinator()->GetSidePanelHeaderForTesting()->header_pin_button();
   EXPECT_TRUE(pin_button->GetVisible());
   EXPECT_FALSE(pin_button->GetToggled());
 
@@ -2356,7 +2365,7 @@ IN_PROC_BROWSER_TEST_F(SidePanelCoordinatorLensOverlayTest,
                                    SidePanelEntry::PanelType::kContent),
                                SidePanelEntry::Id::kLensOverlayResults);
   views::ImageButton* more_info_button =
-      coordinator()->GetHeaderMoreInfoButtonForTesting();
+      coordinator()->GetSidePanelHeaderForTesting()->header_more_info_button();
   EXPECT_TRUE(more_info_button->GetVisible());
 }
 
@@ -2369,7 +2378,7 @@ IN_PROC_BROWSER_TEST_F(SidePanelCoordinatorLensOverlayTest,
                                    SidePanelEntry::PanelType::kContent),
                                SidePanelEntry::Id::kLens);
   views::ImageButton* more_info_button =
-      coordinator()->GetHeaderMoreInfoButtonForTesting();
+      coordinator()->GetSidePanelHeaderForTesting()->header_more_info_button();
   EXPECT_FALSE(more_info_button->GetVisible());
 }
 
