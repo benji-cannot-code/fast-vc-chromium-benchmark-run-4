deb24c478c04206a0c3760c055ef95d51b88e1e7
Christian Xu <christianxu@google.com>
2025-10-03 07:21:24 -0700

    [iOS] Remove the kLensOverlayDisablePriceInsights flag
    This flag is no longer needed because the feature has been fully
    abandoned. This CL removes the flag definition and all
    associated conditional code checks.
    
    Fixed: 394145578
    Change-Id: Ifdc4248f2404155217d7802e11d56f79686e687a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7005456
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Reviewed-by: Cheick Cisse <cheickcisse@google.com>
    Cr-Commit-Position: refs/heads/main@{#1524763}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -167,9 +167,6 @@ const base::NotFatalUntil kLensOverlayNotFatalUntil = base::NotFatalUntil::M200;
 
 BASE_FEATURE(kLensLoadAIMInLensResultPage, base::FEATURE_DISABLED_BY_DEFAULT);
 
-BASE_FEATURE(kLensOverlayDisablePriceInsights,
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
 BASE_FEATURE(kLensOverlayPriceInsightsCounterfactual,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
