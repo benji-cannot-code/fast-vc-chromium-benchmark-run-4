f59d95cbbfa882e8dc583f749a1cadfb89a9cf27
Andres Sevilla <andresesevilla@google.com>
2025-10-03 16:21:56 -0700

    Disable IP Protection when --disable-http2 is used
    The --disable-http2 command line flag breaks IP Protection when using
    HTTPS proxy chains. Chrome attempts to use HTTP/1.1 with the proxies,
    but this is blocked for security reasons.
    
    As a straightforward solution, this change disables the IP Protection
    feature entirely when the --disable-http2 switch is present. Use of this
    command line flag is expected to be uncommon.
    
    Fixed: 435494271
    Change-Id: I1131925f476b810e118377624b235dd8d4a0eb95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7008708
    Reviewed-by: Zainab Rizvi <rizvis@google.com>
    Commit-Queue: Zainab Rizvi <rizvis@google.com>
    Reviewed-by: Andrew Williams <awillia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1525075}

--- chrome/browser/ip_protection/ip_protection_core_host.cc
+++ chrome/browser/ip_protection/ip_protection_core_host.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/privacy_sandbox/tracking_protection_prefs.h"
 #include "components/privacy_sandbox/tracking_protection_settings.h"
 #include "components/variations/service/variations_service.h"
+#include "components/network_session_configurator/common/network_switches.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/storage_partition.h"
 #include "google_apis/common/api_key_request_util.h"
@@ -456,10 +457,12 @@ bool IpProtectionCoreHost::CanRequestOAuthToken() {
 
 // static
 bool IpProtectionCoreHost::CanIpProtectionBeEnabled() {
+  const base::CommandLine& command_line =
+      *base::CommandLine::ForCurrentProcess();
   return base::FeatureList::IsEnabled(
              net::features::kEnableIpProtectionProxy) &&
-         !base::CommandLine::ForCurrentProcess()->HasSwitch(
-             switches::kDisableIpProtectionProxy);
+         !command_line.HasSwitch(switches::kDisableIpProtectionProxy) &&
+         !command_line.HasSwitch(switches::kDisableHttp2);
 }
 
 bool IpProtectionCoreHost::ShouldDisableIpProtectionForEnterpriseForTesting() {
--- chrome/browser/ip_protection/ip_protection_core_host_factory_unittest.cc
+++ chrome/browser/ip_protection/ip_protection_core_host_factory_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/testing_profile.h"
 #include "content/public/test/browser_task_environment.h"
 #include "net/base/features.h"
+#include "components/network_session_configurator/common/network_switches.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
@@ -124,6 +125,23 @@ class IpProtectionCoreHostFactoryOptOutEnabled
 
 TEST_F(IpProtectionCoreHostFactoryOptOutEnabled,
        ServiceCreationFailsWhenUserOptedOut) {
+  IpProtectionCoreHost* service =
+      IpProtectionCoreHostFactory::GetForProfile(profile());
+  ASSERT_FALSE(service);
+}
+
+class IpProtectionCoreHostFactoryHttp2DisabledTest
+    : public IpProtectionCoreHostFactoryTest {
+ public:
+  IpProtectionCoreHostFactoryHttp2DisabledTest()
+      : IpProtectionCoreHostFactoryTest(
+            /*feature_enabled=*/true,
+            /*command_line_switch=*/switches::kDisableHttp2) {}
+};
+
+TEST_F(IpProtectionCoreHostFactoryHttp2DisabledTest,
+       ServiceCreationFailsWhenHttp2Disabled) {
+  // IP Protection requires HTTP/2.
   IpProtectionCoreHost* service =
       IpProtectionCoreHostFactory::GetForProfile(profile());
   ASSERT_FALSE(service);
