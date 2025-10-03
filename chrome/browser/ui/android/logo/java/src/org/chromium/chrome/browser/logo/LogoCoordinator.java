c745e7a921e728043a32e84a5b799dff31dd5b04
Xi Han <hanxi@google.com>
2025-10-03 15:12:57 -0700

    [Theme] Tint google logo for color themes.
    In this CL, we set tinted color for Google logo when NTP's background
    is customized:
    - When a customized color is selected, its primary color is used to tint
      the logo;
    - When a customized image is selected, white color is used;
    - For default theme, remains the same as before:
      - light mode: color logo;
      - dark mode: white logo.
    Screenshots: http://shortn/_lZVmMDEzm2.
    
    Bug: 423579377
    Change-Id: Ide34f6095747d5c71750d1d0ffa4111ef706eb4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7003407
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1525048}

--- chrome/browser/ui/android/logo/java/src/org/chromium/chrome/browser/logo/LogoCoordinator.java
+++ chrome/browser/ui/android/logo/java/src/org/chromium/chrome/browser/logo/LogoCoordinator.java
@@ -16,11 +16,11 @@ import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.logo.LogoBridge.Logo;
+import org.chromium.chrome.browser.ntp_customization.NtpCustomizationUtils;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.content_public.browser.LoadUrlParams;
 import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.ui.modelutil.PropertyModelChangeProcessor;
-import org.chromium.ui.util.ColorUtils;
 
 /** Coordinator used to fetch and load logo image for Start surface and NTP. */
 @NullMarked
@@ -63,12 +63,7 @@ public class LogoCoordinator {
         if (ChromeFeatureList.sAndroidLogoViewRefactor.isEnabled()) {
             defaultGoogleLogoDrawable =
                     ContextCompat.getDrawable(context, R.drawable.ic_google_logo);
-            boolean isNightMode = ColorUtils.inNightMode(context);
-            if (isNightMode) {
-                // Check the mode before applying a tinted color. A transparent tint in light mode
-                // will cause the logo's color to disappear.
-                defaultGoogleLogoDrawable.setTint(context.getColor(R.color.google_logo_tint_color));
-            }
+            NtpCustomizationUtils.setTintForDefaultGoogleLogo(context, defaultGoogleLogoDrawable);
         }
 
         mMediator =
