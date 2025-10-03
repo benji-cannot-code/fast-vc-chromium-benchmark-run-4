2894f13ebb3982f254dac798b43cc7f56aedca93
Wenyu Fu <wenyufu@chromium.org>
2025-10-03 16:25:32 -0700

    [BrowserControls] Check TopControlsRefactor for LTCV2
    Using sLockTopControlsOnLargeTabletsV2 (LTCV2) without
    TopControlsRefactor is an invalid flag setup - it will disable part of
    the logic in LockTopControlsOnLargeTablets (LTC) in
    BrowserControlsManager.
    
    This change makes LTCV2 depends on TopControlsRefactor by always
    checking both flags together.
    
    Bug: 446779702
    Change-Id: I2aa657374a3b11584033b0dd1b9379d4e7ad7975
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7007630
    Auto-Submit: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1525076}

--- chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
@@ -609,8 +609,7 @@ public class RootUiCoordinator
             mExclusiveAccessManager = null;
         }
 
-        if (ChromeFeatureList.sLockTopControlsOnLargeTabletsV2.isEnabled()) {
-
+        if (BrowserControlsUtils.doSyncMinHeightWithTotalHeightV2()) {
             if (DeviceInfo.isDesktop()
                     || BrowserControlsUtils.doSyncMinHeightWithTotalHeight(mActivity)) {
                 mTopControlsStacker.setScrollingDisabled(true);
