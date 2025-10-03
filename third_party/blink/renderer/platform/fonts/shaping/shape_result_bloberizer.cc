1fbf7c377c3b2301a80f00f05d64835f883831eb
Kent Tamura <tkent@chromium.org>
2025-10-03 00:18:24 -0700

    CanvasTextNG: Remove a FillGlyphs constructor for TextRunPaintInfo
    It's not used any longer in production code. We use another constructor
    with PlainTextNode.
    
    The test "ShapeResultBloberizerTest.
    CommonAccentRightToLeftFillGlyphBuffer" is removed because FillGlyphs
    for PlainTextNode doesn't support RTL.
    
    This CL has no behavior changes.
    
    Bug: 448683997
    Change-Id: I9de4c7fccfe57aed115ae6bcbe900fbd90ef5acd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7005649
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1524630}

--- third_party/blink/renderer/platform/fonts/shaping/shape_result_bloberizer.cc
+++ third_party/blink/renderer/platform/fonts/shaping/shape_result_bloberizer.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/fonts/shaping/caching_word_shaper.h"
 #include "third_party/blink/renderer/platform/fonts/shaping/shape_result.h"
 #include "third_party/blink/renderer/platform/fonts/shaping/shape_result_view.h"
-#include "third_party/blink/renderer/platform/fonts/text_run_paint_info.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/text/text_break_iterator.h"
 #include "third_party/blink/renderer/platform/text/text_run.h"
@@ -25,12 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 namespace {
-
-// A helper for FillGlyphsSlow().
-inline const ShapeResult* GetShapeResult(
-    const Member<const ShapeResult>& item) {
-  return item.Get();
-}
 
 // A helper for FillGlyphsSlow().
 inline const ShapeResult* GetShapeResult(const PlainTextItem& item) {
@@ -453,30 +446,6 @@ class ClusterStarts {
 
 ShapeResultBloberizer::FillGlyphs::FillGlyphs(
     const FontDescription& font_description,
-    const TextRunPaintInfo& run_info,
-    const ShapeResultBuffer& result_buffer,
-    const Type type)
-    : ShapeResultBloberizer(font_description, type) {
-  if (CanUseFastPath(run_info.from, run_info.to, run_info.run.length(),
-                     result_buffer.HasVerticalOffsets())) {
-    DVLOG(4) << "FillGlyphs fast path";
-    DCHECK(!result_buffer.HasVerticalOffsets());
-    DCHECK_NE(type_, ShapeResultBloberizer::Type::kTextIntercepts);
-    DCHECK_NE(type_, ShapeResultBloberizer::Type::kEmitText);
-    advance_ =
-        FillFastHorizontalGlyphs(result_buffer, run_info.run.Direction());
-    return;
-  }
-
-  DVLOG(4) << "FillGlyphs slow path";
-
-  auto results = result_buffer.results_;
-  FillGlyphsSlow(run_info.run.ToStringView(), run_info.run.Direction(), results,
-                 run_info.from, run_info.to);
-}
-
-ShapeResultBloberizer::FillGlyphs::FillGlyphs(
-    const FontDescription& font_description,
     const PlainTextNode& node,
     const Type type)
     : ShapeResultBloberizer(font_description, type) {
@@ -629,24 +598,6 @@ bool ShapeResultBloberizer::CanUseFastPath(
          !shape_result->HasVerticalOffsets() &&
          type_ != ShapeResultBloberizer::Type::kTextIntercepts &&
          type_ != ShapeResultBloberizer::Type::kEmitText;
-}
-
-float ShapeResultBloberizer::FillFastHorizontalGlyphs(
-    const ShapeResultBuffer& result_buffer,
-    TextDirection text_direction) {
-  DCHECK(!result_buffer.HasVerticalOffsets());
-  DCHECK_NE(type_, ShapeResultBloberizer::Type::kTextIntercepts);
-
-  float advance = 0;
-  auto results = result_buffer.results_;
-
-  for (unsigned i = 0; i < results.size(); ++i) {
-    const auto& word_result =
-        IsLtr(text_direction) ? results[i] : results[results.size() - 1 - i];
-    advance = FillFastHorizontalGlyphs(word_result.Get(), advance);
-  }
-
-  return advance;
 }
 
 float ShapeResultBloberizer::FillFastHorizontalGlyphs(const ShapeResult* result,
--- third_party/blink/renderer/platform/fonts/shaping/shape_result_bloberizer.h
+++ third_party/blink/renderer/platform/fonts/shaping/shape_result_bloberizer.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/paint/node_id.h"
 #include "third_party/blink/renderer/platform/fonts/canvas_rotation_in_vertical.h"
 #include "third_party/blink/renderer/platform/fonts/glyph.h"
-#include "third_party/blink/renderer/platform/fonts/shaping/shape_result_buffer.h"
 #include "third_party/blink/renderer/platform/fonts/simple_font_data.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
@@ -27,7 +26,7 @@ namespace blink {
 
 class FontDescription;
 class PlainTextNode;
-struct TextRunPaintInfo;
+class ShapeResultView;
 
 class PLATFORM_EXPORT ShapeResultBloberizer {
   STACK_ALLOCATED();
@@ -124,7 +123,6 @@ class PLATFORM_EXPORT ShapeResultBloberizer {
                       unsigned length,
                       bool has_vertical_offsets);
   bool CanUseFastPath(unsigned from, unsigned to, const ShapeResultView*);
-  float FillFastHorizontalGlyphs(const ShapeResultBuffer&, TextDirection);
   float FillFastHorizontalGlyphs(const ShapeResult*, float advance = 0);
   static void AddFastHorizontalGlyphToBloberizer(void* context,
                                                  unsigned,
@@ -228,10 +226,7 @@ struct PLATFORM_EXPORT ShapeResultBloberizer::FillTextEmphasisGlyphsNG
 
 struct PLATFORM_EXPORT ShapeResultBloberizer::FillGlyphs
     : public ShapeResultBloberizer {
-  FillGlyphs(const FontDescription&,
-             const TextRunPaintInfo&,
-             const ShapeResultBuffer&,
-             Type);
+  // This doesn't work for RTL. See PlainTextPainter::DrawWithBidiReorder().
   FillGlyphs(const FontDescription& font_description,
              const PlainTextNode& node,
              Type type);
