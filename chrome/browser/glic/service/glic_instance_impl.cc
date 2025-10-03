49ad5cbe8f73bafdf84610f12d45f28c150e26f6
Bryant Chandler <bryantchandler@chromium.org>
2025-10-03 16:29:44 -0700

    Revert "[Glic multi-instance] Properly call NotifyPanelWillOpen/WasClosed"
    This reverts commit ab76750618a268b256db4b727ab2c8dad148bb17.
    
    Reason for revert: This breaks in-place conversation switching
    
    Original change's description:
    > [Glic multi-instance] Properly call NotifyPanelWillOpen/WasClosed
    >
    > In multi instance NotifyPanelWillOpen should be called when instance goes from not having any auto-open embedders to having one.
    > NotifyPanelWasClosed should be called when there are no auto open embedders (an entrypoint has to be triggered to show)
    >
    > Bug: 447610723
    > Change-Id: I9a42736b43587a8fffd1fbc46bf0d63908f685a0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6996728
    > Commit-Queue: Basia Zimirska <basiaz@google.com>
    > Reviewed-by: Dan Harrington <harringtond@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1524435}
    
    Bug: 447610723
    Change-Id: I5140187a9b32447ca42ff0887546e3520a9855db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7009042
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Bryant Chandler <bryantchandler@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1525077}

--- chrome/browser/glic/service/glic_instance_impl.cc
+++ chrome/browser/glic/service/glic_instance_impl.cc
@@ -149,8 +149,7 @@ void GlicInstanceImpl::Show(EmbedderType type, tabs::TabInterface* tab) {
     SetActiveEmbedderAndNotifyStateChange(new_key);
   }
 
-  MaybeShowHostUi(embedder_to_show, auto_open_embedders_.size() == 0);
-  auto_open_embedders_.emplace(new_key);
+  MaybeShowHostUi(embedder_to_show);
   embedder_to_show->Show();
 }
 
@@ -158,7 +157,6 @@ void GlicInstanceImpl::Close(EmbedderType type, tabs::TabInterface* tab) {
   EmbedderKey key = GetEmbedderKey(type, tab);
   auto* embedder = GetEmbedderForKey(key);
   if (embedder) {
-    auto_open_embedders_.erase(key);
     embedder->Close();
   }
   MaybeDeactivateEmbedderAndCloseHostUi(key);
@@ -295,12 +293,7 @@ void GlicInstanceImpl::UnbindTab(tabs::TabInterface* tab) {
       active_embedder_key_.value() == EmbedderKey(tab)) {
     DeactivateCurrentEmbedder();
   }
-  auto key = EmbedderKey(tab);
-  embedders_.erase(key);
-  auto_open_embedders_.erase(key);
-  if (auto_open_embedders_.size() == 0) {
-    host_.PanelWasClosed();
-  }
+  embedders_.erase(EmbedderKey(tab));
 }
 
 bool GlicInstanceImpl::IsOrphaned() const {
@@ -480,8 +473,7 @@ void GlicInstanceImpl::ClearActiveEmbedderAndNotifyStateChange() {
   return;
 }
 
-void GlicInstanceImpl::MaybeShowHostUi(GlicUiEmbedder* embedder,
-                                       bool callPanelWillOpen) {
+void GlicInstanceImpl::MaybeShowHostUi(GlicUiEmbedder* embedder) {
   Host::EmbedderDelegate* delegate = embedder->GetHostEmbedderDelegate();
   if (!delegate) {
     return;
@@ -496,13 +488,11 @@ void GlicInstanceImpl::MaybeShowHostUi(GlicUiEmbedder* embedder,
   host_.NotifyWindowIntentToShow();
 
   // TODO: NotifyPanelStateChanged() here
-  // TODO(crbug.com/448879415): pass in the correct invocation source
-  if (callPanelWillOpen) {
-    Host::PanelWillOpenOptions options;
-    options.conversation_id = conversation_id();
-    host_.PanelWillOpen(mojom::InvocationSource::kTopChromeButton,
-                        std::move(options));
-  }
+  // TODO: pass in the correct invocation source
+  Host::PanelWillOpenOptions options;
+  options.conversation_id = conversation_id();
+  host_.PanelWillOpen(mojom::InvocationSource::kTopChromeButton,
+                      std::move(options));
 }
 
 void GlicInstanceImpl::Detach() {
@@ -539,10 +529,9 @@ void GlicInstanceImpl::SwitchConversation(
 
 void GlicInstanceImpl::MaybeDeactivateEmbedderAndCloseHostUi(EmbedderKey key) {
   if (active_embedder_key_.has_value() && active_embedder_key_.value() == key) {
+    // TODO: Figure out what else should go into host_.PanelWasClosed() and
+    // maybe call it here.
     DeactivateCurrentEmbedder();
-    if (auto_open_embedders_.size() == 0) {
-      host_.PanelWasClosed();
-    }
   }
 }
 
