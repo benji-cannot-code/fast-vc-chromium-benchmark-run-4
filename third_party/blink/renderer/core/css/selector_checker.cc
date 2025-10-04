4c25d36e4646606b9919cc6ecbd191b2a0d95513
Rune Lillesveen <futhark@chromium.org>
2025-10-04 03:44:47 -0700

    Remove CSSNegatedFeatureless runtime flag
    In stable since M139
    
    Bug: 378966977
    Change-Id: I62125ead5ba52d1b658cdcb33ec15999ad291780
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7008367
    Reviewed-by: Steinar H Gunderson <sesse@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1525195}

--- third_party/blink/renderer/core/css/selector_checker.cc
+++ third_party/blink/renderer/core/css/selector_checker.cc
@@ -1365,9 +1365,7 @@ ALWAYS_INLINE bool SelectorChecker::CheckOne(
         selector.GetPseudoType() != CSSSelector::kPseudoScope) {
       return false;
     }
-    if (RuntimeEnabledFeatures::CSSNegatedFeaturelessEnabled()) {
-      return MatchShadowHost(context, result) == kFeaturelessMatches;
-    }
+    return MatchShadowHost(context, result) == kFeaturelessMatches;
   }
   if (RuntimeEnabledFeatures::CSSLogicalCombinationPseudoEnabled()) {
     if (context.pseudo_id != kPseudoIdNone) {
@@ -2878,11 +2876,8 @@ bool SelectorChecker::CheckPseudoClass(const SelectorCheckingContext& context,
               : WebFeature::kCSSSelectorHostContextInLiveProfile);
       [[fallthrough]];
     case CSSSelector::kPseudoHost:
-      if (RuntimeEnabledFeatures::CSSNegatedFeaturelessEnabled()) {
-        DCHECK(!IsAtShadowHost(context));
-        return false;
-      }
-      return CheckPseudoHost(context, result);
+      DCHECK(!IsAtShadowHost(context));
+      return false;
     case CSSSelector::kPseudoSpatialNavigationFocus:
       DCHECK(is_ua_rule_);
       return MatchesSpatialNavigationFocusPseudoClass(element);
