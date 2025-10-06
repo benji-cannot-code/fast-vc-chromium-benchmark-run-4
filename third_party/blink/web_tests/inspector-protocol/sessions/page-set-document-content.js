828d4eb8c86b3a73c371f2c237ea0945c8d144b3
Natallia Harshunova <nharshunova@chromium.org>
2025-10-06 02:38:39 -0700

    Add await for the enable() method of the protocol page to fix test
    Bug: 444048840
    Change-Id: I56adf187aa62b7bdef8f841d216287ccf908b59c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7003016
    Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>
    Reviewed-by: Nikolay Vitkov <nvitkov@chromium.org>
    Reviewed-by: Ergün Erdoğmuş <ergunsh@chromium.org>
    Commit-Queue: Natallia Harshunova <nharshunova@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1525494}

--- third_party/blink/web_tests/inspector-protocol/sessions/page-set-document-content.js
+++ third_party/blink/web_tests/inspector-protocol/sessions/page-set-document-content.js
@@ -4,9 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   var page = await testRunner.createPage();
 
   var session1 = await page.createSession();
-  session1.protocol.Page.enable();
+  await session1.protocol.Page.enable();
   var session2 = await page.createSession();
-  session2.protocol.Page.enable();
+  await session2.protocol.Page.enable();
 
   var promise1 = session1.protocol.Page.onceFrameNavigated();
   var promise2 = session2.protocol.Page.onceFrameNavigated();
