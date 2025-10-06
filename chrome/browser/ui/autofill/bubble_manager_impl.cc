5213f929d121920ad94a76cae4ed49cc811e61f4
Vidhan Jain <vidhanj@google.com>
2025-10-06 05:35:17 -0700

    Ensure the bubbles are shown only when the tab is activated
    If the tab is not activated, the bubbles are added to the queue and
    shown once the tab becomes active again.
    
    Bug: 432429605
    Change-Id: I5a26bf18fea689ebfbd80d94fb59aaa9f9be40ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6999880
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1525543}

--- chrome/browser/ui/autofill/bubble_manager_impl.cc
+++ chrome/browser/ui/autofill/bubble_manager_impl.cc
@@ -123,7 +123,8 @@ bool BubbleManagerImpl::PendingRequest::operator<(
   return time_added < other.time_added;
 }
 
-BubbleManagerImpl::BubbleManagerImpl(tabs::TabInterface* tab) {
+BubbleManagerImpl::BubbleManagerImpl(tabs::TabInterface* tab)
+    : tab_interface_(tab) {
   tab_subscriptions_.push_back(tab->RegisterWillDeactivate(base::BindRepeating(
       &BubbleManagerImpl::TabWillEnterBackground, base::Unretained(this))));
   tab_subscriptions_.push_back(tab->RegisterDidActivate(base::BindRepeating(
@@ -135,6 +136,14 @@ BubbleManagerImpl::~BubbleManagerImpl() = default;
 void BubbleManagerImpl::RequestShowController(
     BubbleControllerBase& controller_to_show,
     bool force_show) {
+  base::WeakPtr<BubbleControllerBase> controller_weak_ptr =
+      controller_to_show.GetBubbleControllerBaseWeakPtr();
+
+  if (!tab_interface_->IsActivated()) {
+    AddToPendingQueue(controller_weak_ptr);
+    return;
+  }
+
   if (force_show) {
     base::UmaHistogramEnumeration("Autofill.Bubble.RequestShow.ForceShow",
                                   controller_to_show.GetBubbleType());
@@ -142,8 +151,6 @@ void BubbleManagerImpl::RequestShowController(
 
   base::UmaHistogramEnumeration("Autofill.Bubble.RequestShow",
                                 controller_to_show.GetBubbleType());
-  base::WeakPtr<BubbleControllerBase> controller_weak_ptr =
-      controller_to_show.GetBubbleControllerBaseWeakPtr();
 
   base::AutoReset<bool> show_request_guard(&handling_show_request_, true);
 
