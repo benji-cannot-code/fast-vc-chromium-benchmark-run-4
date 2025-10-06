d827908b08bd8c509d48bf67b726082b26a664c6
Dhruv Kathpalia <dhruvkathpalia@google.com>
2025-10-06 15:27:30 -0700

    [realbox] Add "Compact" layout mode to the searchbox.
    This change introduces a new `realbox-layout-mode='Compact'` for the
    searchbox, which includes a new "Add Context" button, hides the default
    search icon, and adjusts the positioning of other icons like Lens and
    Voice search buttons. The placeholder text and compose button styling
    are also updated to match the new compact layout.
    Screencast:
    http://screencast/cast/NTQ5Njk1MjI1Mzg0MTQwOHw3MGJiNmVhNi1hMA
    
    Bug: b:443299699
    
    Change-Id: Id74debef84f4ae42295634bafe379eed383e3642
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6998274
    Reviewed-by: Ananya Seelam <ananyaseelam@google.com>
    Commit-Queue: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1525899}

--- ui/webui/resources/cr_components/composebox/contextual_entrypoint_and_carousel.ts
+++ ui/webui/resources/cr_components/composebox/contextual_entrypoint_and_carousel.ts
@@ -70,6 +70,7 @@ export class ContextualEntrypointAndCarouselElement extends I18nMixinLit
   static override get properties() {
     return {
       showDropdown: {type: Boolean},
+      compactMode: {type: Boolean},
       attachmentFileTypes_: {type: String},
       contextMenuEnabled_: {type: Boolean},
       files_: {type: Object},
@@ -99,6 +100,7 @@ export class ContextualEntrypointAndCarouselElement extends I18nMixinLit
   }
 
   accessor showDropdown: boolean = false;
+  accessor compactMode: boolean = false;
   protected accessor attachmentFileTypes_: string =
       loadTimeData.getString('composeboxAttachmentFileTypes');
   protected accessor contextMenuEnabled_: boolean =
