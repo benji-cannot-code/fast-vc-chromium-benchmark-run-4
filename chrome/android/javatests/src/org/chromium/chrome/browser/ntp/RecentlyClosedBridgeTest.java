3b53f62cd65c1e83420822c21bbf0c31d3af8ca2
Calder Kitagawa <ckitagawa@chromium.org>
2025-10-06 13:54:53 -0700

    [Recent Tabs] Change single tab restoration to create a new tab
    Rather than swapping web contents create a new tab then close the old
    one. This is the same approach taken on desktop [1]. This allows us to
    deprecate SwapWebContents which moves us towards the desktop model of a
    fixed 1:1 relationship between tabs and WebContents. This will help
    significantly reduce code complexity.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ui/browser_tabrestore.cc;drc=2de29efe573e2d6cf3831c516cb47772c57e135a;l=285
    
    Bug: 449703414
    Change-Id: I439dae4325e9fb2089ac7b4518961089698fbd56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7011011
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1525841}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ntp/RecentlyClosedBridgeTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ntp/RecentlyClosedBridgeTest.java
@@ -182,7 +182,7 @@ public class RecentlyClosedBridgeTest {
     @DisableIf.Build(
             sdk_is_greater_than = Build.VERSION_CODES.TIRAMISU,
             message = "crbug.com/355058571")
-    public void testOpenRecentlyClosedTab_InCurrentTab() {
+    public void testOpenRecentlyClosedTab_CurrentTabDisposition() {
         final String[] urls = new String[] {getUrl(TEST_PAGE_A), getUrl(TEST_PAGE_B)};
         final Tab tabA = mActivityTestRule.loadUrlInNewTab(urls[0], /* incognito= */ false);
         final Tab tabB = mActivityTestRule.loadUrlInNewTab(urls[1], /* incognito= */ false);
@@ -222,23 +222,24 @@ public class RecentlyClosedBridgeTest {
         // 2. tabC - now TEST_PAGE_B.
         final List<Tab> tabs = getAllTabs();
         Assert.assertEquals(2, tabs.size());
-        // Restored onto tab B.
-        Assert.assertEquals(tabC, tabs.get(1));
-        Assert.assertEquals(titles[1], ChromeTabUtils.getTitleOnUiThread(tabC));
-        Assert.assertEquals(urls[1], ChromeTabUtils.getUrlOnUiThread(tabC).getSpec());
+        // Restored in same position as tabC.
+        Tab newTabC = tabs.get(1);
+        Assert.assertNotEquals(tabC, newTabC);
+        Assert.assertEquals(titles[1], ChromeTabUtils.getTitleOnUiThread(newTabC));
+        Assert.assertEquals(urls[1], ChromeTabUtils.getUrlOnUiThread(newTabC).getSpec());
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
-                    Assert.assertNotNull(tabC.getWebContents());
+                    Assert.assertNotNull(newTabC.getWebContents());
                     // Should only have one navigation entry as it replaced TEST_PAGE_C.
                     Assert.assertEquals(
                             1,
-                            tabC.getWebContents()
+                            newTabC.getWebContents()
                                     .getNavigationController()
                                     .getNavigationHistory()
                                     .getEntryCount());
 
                     // Has renderer for foreground tab.
-                    Assert.assertNotNull(tabC.getWebContents().getRenderWidgetHostView());
+                    Assert.assertNotNull(newTabC.getWebContents().getRenderWidgetHostView());
                 });
     }
 
@@ -411,7 +412,7 @@ public class RecentlyClosedBridgeTest {
     @DisableIf.Build(
             sdk_is_greater_than = Build.VERSION_CODES.TIRAMISU,
             message = "crbug.com/355058571")
-    public void testOpenRecentlyClosedTab_FromGroupClosure_InCurrentTab() {
+    public void testOpenRecentlyClosedTab_FromGroupClosure_CurrentTabDisposition() {
         if (mTabGroupModelFilter == null) return;
 
         // Tab order is inverted in when closing.
@@ -446,7 +447,7 @@ public class RecentlyClosedBridgeTest {
                             mTabModel, group.getTabs().get(1), WindowOpenDisposition.CURRENT_TAB);
                 });
 
-        // 1. tabA restored over blank tab.
+        // 1. tabA state replaced blank tab.
         final List<Tab> tabs = getAllTabs();
         Assert.assertEquals(1, tabs.size());
         Assert.assertEquals(titles[1], ChromeTabUtils.getTitleOnUiThread(tabs.get(0)));
@@ -464,7 +465,7 @@ public class RecentlyClosedBridgeTest {
                             WindowOpenDisposition.NEW_BACKGROUND_TAB);
                 });
 
-        // 1. tabA restored over blank tab.
+        // 1. tabA state replaced blank tab.
         // 2. tabB restored.
         tabs.clear();
         tabs.addAll(getAllTabs());
