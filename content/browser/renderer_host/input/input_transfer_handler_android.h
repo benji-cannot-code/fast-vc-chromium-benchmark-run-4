22694c7d29e1d5ba480d38131ec63a003b03f2f8
Kartar Singh <kartarsingh@google.com>
2025-10-06 06:50:26 -0700

    Revert "[InputVizard] Skip pointer transfer logic when Android fix is available."
    This reverts commit 5f9599932d5b3c10afe3e41fa95366d90eab1e98.
    
    Reason for revert: The Android side fix doesn't seem to be working
    fully. Second pointer events do go to Viz directly, but the third
    pointer events can still make it to Browser process while Viz has an
    active touch sequence. Android side Google internal bug for this:
    http://b/449665569
    
    Original change's description:
    > [InputVizard] Skip pointer transfer logic when Android fix is available.
    >
    > In https://crbug.com/395596230, we found out new pointers on Chrome
    > window could go to Browser window, even when there's an already active
    > touch sequence on Viz. This has been since fixed as part of this
    > Google-internal Android bug: http://b/397979572.
    >
    > In this change we are skipping this mitigation logic we added on Google
    > Android 16+ devices, since they shuould already receive pointer downs
    > directly on Viz. The fix is not available on all Android 16+ releases
    > since the fix was done late in Android 16 release cycle, and some of the
    > OEMs might not have picked up the fix.
    >
    > Bug: 398208297
    > Change-Id: I7e7d1c75badbe88360e3238e8c1f50f4445c6e07
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6994549
    > Commit-Queue: Kartar Singh <kartarsingh@google.com>
    > Reviewed-by: Jonathan Ross <jonross@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1522713}
    
    Bug: 398208297
    Change-Id: Id5043b7903ef2d797aad48873beea7d125d03f82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7011788
    Commit-Queue: Kartar Singh <kartarsingh@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1525571}

--- content/browser/renderer_host/input/input_transfer_handler_android.h
+++ content/browser/renderer_host/input/input_transfer_handler_android.h
@@ -83,8 +83,6 @@ class CONTENT_EXPORT InputTransferHandlerAndroid {
 
   void OnTouchEnd(base::TimeTicks event_time);
 
-  static bool NewPointersGoDirectlyToViz();
-
   // Virtual for testing.
   virtual bool IsTouchSequencePotentiallyActiveOnViz() const;
 
