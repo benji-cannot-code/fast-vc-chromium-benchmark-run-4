f1ed4c6831139fecc14ea420713de05ca888da22
Marc Treib <treib@chromium.org>
2025-10-06 07:05:38 -0700

    Remove about:flags entry separate-profiles-for-managed-accounts
    Note: The underlying base::Feature is not removed yet - it's still used
    by some integration tests that cover migration flows.
    
    Fixed: 448390061
    Change-Id: Icc356e478ccb0ada9b63db91bbb43de832a3989b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7006503
    Auto-Submit: Marc Treib <treib@chromium.org>
    Commit-Queue: Jood Hajeer <jood@google.com>
    Reviewed-by: Jood Hajeer <jood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1525576}

--- ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
+++ ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
@@ -868,9 +868,6 @@ extern const char kSendTabToSelfIOSPushNotificationsDescription[];
 extern const char kSendUmaOverAnyNetwork[];
 extern const char kSendUmaOverAnyNetworkDescription[];
 
-extern const char kSeparateProfilesForManagedAccountsName[];
-extern const char kSeparateProfilesForManagedAccountsDescription[];
-
 extern const char kShareExtensionForMultiprofileName[];
 extern const char kShareExtensionForMultiprofileDescription[];
 
