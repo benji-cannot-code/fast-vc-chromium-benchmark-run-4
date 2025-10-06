80883b968aeb312f145b575ec9048494727a83f1
Marc Treib <treib@chromium.org>
2025-10-06 06:19:49 -0700

    Implement SyncServiceObserver::OnSyncShutdown for WebUI handlers
    The implementations are simply NOTREACHED() since these UIs are
    destroyed before the Profile and its KeyedServices.
    
    Bug: 40882734
    Change-Id: Icec61873471cb67248ff1a125fb3f85b243bf5de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6988330
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1525561}

--- chrome/browser/ui/webui/settings/people_handler.h
+++ chrome/browser/ui/webui/settings/people_handler.h
@@ -180,6 +180,7 @@ class PeopleHandler : public SettingsPageUIHandler,
 
   // syncer::SyncServiceObserver implementation.
   void OnStateChanged(syncer::SyncService* sync_service) override;
+  void OnSyncShutdown(syncer::SyncService* sync_service) override;
 
   // content::WebContentsObserver implementation
   void BeforeUnloadDialogCancelled() override;
