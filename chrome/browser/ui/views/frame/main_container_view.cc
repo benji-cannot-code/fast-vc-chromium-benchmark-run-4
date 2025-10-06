c097d9ecd80a93b48dba0fae069cdb4f01bc884e
Vince Lugli <lugli@google.com>
2025-10-06 16:46:10 -0700

    [TBHSP] Move InfoBar and TopContainer into MainContainer
    Bug: 448165794,448164932
    Change-Id: If32624efb60234f4157087e34137f111e1e7462d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7004144
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Vince Lugli <lugli@google.com>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1525928}

---
+++ chrome/browser/ui/views/frame/main_container_view.cc
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/frame/main_container_view.h"
+
+#include "ui/base/metadata/metadata_impl_macros.h"
+
+MainContainerView::MainContainerView() = default;
+MainContainerView::~MainContainerView() = default;
+
+BEGIN_METADATA(MainContainerView)
+END_METADATA
