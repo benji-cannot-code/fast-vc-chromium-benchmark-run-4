2bb465abe864de2863214b877f44e7f9e59e8cf5
Trevor Perrier <perrier@chromium.org>
2025-10-06 12:24:31 -0700

    Remove unused `draggable_area_` from GWC
    The `draggable_area_` variable was never used by GlicWindowControllerImpl so can safely be removed.
    
    Change-Id: If3110e34b563e34f00536506f869bafecf93206f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7009798
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Trevor Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1525775}

--- chrome/browser/glic/widget/glic_window_controller_impl.cc
+++ chrome/browser/glic/widget/glic_window_controller_impl.cc
@@ -942,11 +942,9 @@ void GlicWindowControllerImpl::SetDraggingAreasAndWatchForMouseEvents() {
   window_event_observer_ =
       std::make_unique<WindowEventObserver>(this, GetGlicView());
 
-  if (!draggable_area_) {
-    // Set the draggable area to the top bar of the window.
-    GetGlicView()->SetDraggableAreas(
-        {{0, 0, GetGlicView()->width(), kDraggableAreaHeight}});
-  }
+  // Set the draggable area to the top bar of the window.
+  GetGlicView()->SetDraggableAreas(
+      {{0, 0, GetGlicView()->width(), kDraggableAreaHeight}});
 }
 
 GlicView* GlicWindowControllerImpl::GetGlicView() const {
@@ -1245,7 +1243,6 @@ void GlicWindowControllerImpl::ResetAndHidePanel() {
   // detached, attached or currently closed.
 
   // Floating Panel State
-  draggable_area_ = std::nullopt;
   window_event_observer_.reset();
   glic_window_animator_.reset();
   glic_widget_observation_.Reset();
