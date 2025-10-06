3f4b3d0700f54ef19641082a71806e34c21c5a4a
Dan Harrington <harringtond@chromium.org>
2025-10-06 10:09:45 -0700

    Initialize testing mode in `NonInteractiveGlicTest`.
    The default constructor for `NonInteractiveGlicTest` now sets `GlicFocusedBrowserManager` to testing mode, as intended.
    
    Bug: b:447705905
    Change-Id: Ifdf7229c5728a07e972d765e7b7f8745d8465d5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7011635
    Auto-Submit: Dan Harrington <harringtond@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Commit-Queue: Dan Harrington <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1525678}

--- chrome/browser/glic/test_support/non_interactive_glic_test.cc
+++ chrome/browser/glic/test_support/non_interactive_glic_test.cc
@@ -9,7 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace glic {
 
-NonInteractiveGlicTest::NonInteractiveGlicTest() = default;
+NonInteractiveGlicTest::NonInteractiveGlicTest() {
+  GlicFocusedBrowserManager::SetTestingModeForTesting(true);
+}
 
 NonInteractiveGlicTest::NonInteractiveGlicTest(
     const base::FieldTrialParams& glic_params,
