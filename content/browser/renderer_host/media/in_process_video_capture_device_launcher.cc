c328042f4ff21fe6625ff46850ca0568a98fa6c0
Palak Agarwal <agpalak@chromium.org>
2025-10-06 02:35:16 -0700

    Add new enum type for nativeMacOSPicker device type
    Bug: 448881311
    Change-Id: I1050850a75afe9feef103d8f6ce6bcd8248f80e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7002914
    Reviewed-by: Johannes Kron <kron@chromium.org>
    Commit-Queue: Palak Agarwal <agpalak@chromium.org>
    Reviewed-by: Tove Petersson <tovep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1525492}

--- content/browser/renderer_host/media/in_process_video_capture_device_launcher.cc
+++ content/browser/renderer_host/media/in_process_video_capture_device_launcher.cc
@@ -123,7 +123,8 @@ enum DesktopCaptureImplementation {
   kScreenCaptureKitDeviceMac = 2,
   kDesktopCaptureDeviceMac = 3,
   kLegacyDesktopCaptureDevice = 4,
-  kImplementationCount = 5,
+  kNativeMacOSPickerCaptureDevice = 5,
+  kImplementationCount = 6,
 };
 
 // These values are persisted to logs. Entries should not be renumbered and
@@ -149,7 +150,11 @@ enum DesktopCaptureImplementationAndType {
   kLegacyDesktopCaptureDeviceTypeScreen = 17,
   kLegacyDesktopCaptureDeviceTypeWindow = 18,
   kLegacyDesktopCaptureDeviceTypeWebContents = 19,
-  kMaxValue = kLegacyDesktopCaptureDeviceTypeWebContents,
+  kNativeMacOSPickerCaptureDeviceTypeNone = 20,
+  kNativeMacOSPickerCaptureDeviceTypeScreen = 21,
+  kNativeMacOSPickerCaptureDeviceTypeWindow = 22,
+  kNativeMacOSPickerCaptureDeviceTypeWebContents = 23,
+  kMaxValue = kNativeMacOSPickerCaptureDeviceTypeWebContents,
 };
 
 void ReportDesktopCaptureImplementationAndType(
@@ -178,7 +183,7 @@ DesktopCaptureImplementation CreatePlatformDependentVideoCaptureDevice(
   if (picker) {
     device_out = picker->CreateDevice(desktop_id);
     if (device_out) {
-      return kScreenCaptureKitDeviceMac;
+      return kNativeMacOSPickerCaptureDevice;
     }
     return kNoImplementation;
   }
