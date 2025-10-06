a4df085ef605fe48c7dd98d759061f10ec7a0a0b
Antonio Rivera <antoniori@google.com>
2025-10-06 13:34:14 -0700

    Add bitrate metrics to the starboard renderer pipeline.
    In particular, this adds:
    Cast.Platform.AudioBitrate
    Cast.Platform.VideoBitrate
    
    The logic should match this CMA logic:
    https://source.chromium.org/chromium/chromium/src/+/main:chromecast/media/cma/pipeline/media_pipeline_impl.cc;l=494;drc=3dd1b27a7cb34cc30ee4d8ddc2146972b5254201
    
    Test: Added unit tests
    
    Bug: b:443137644
    Change-Id: I55c28a2db85744b6d6fcba14a20ce5e13899aaa4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7005094
    Reviewed-by: Shawn Quereshi <shawnq@google.com>
    Commit-Queue: Antonio Rivera <antoniori@google.com>
    Cr-Commit-Position: refs/heads/main@{#1525827}

--- chromecast/starboard/media/renderer/client_stats_tracker.h
+++ chromecast/starboard/media/renderer/client_stats_tracker.h
@@ -10,19 +10,35 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "base/sequence_checker.h"
+#include "base/timer/elapsed_timer.h"
 #include "chromecast/starboard/media/media/starboard_api_wrapper.h"
+#include "media/base/pipeline_status.h"
 #include "media/base/renderer_client.h"
 
 namespace chromecast {
+
+namespace metrics {
+class CastMetricsHelper;
+}  // namespace metrics
+
 namespace media {
 
 // Tracks media stats and reports them to a RendererClient.
 //
+// In order to prevent flooding logs, this class is rate limited to once every
+// 5s at most. In other words, if UpdateStats is called, subsequent calls within
+// the next 5s will be ignored (though stats will be tracked internally).
+//
+// 5s was chosen to match the logic of CMA:
+// https://source.chromium.org/chromium/chromium/src/+/main:chromecast/media/cma/pipeline/media_pipeline_impl.cc;l=494;drc=3dd1b27a7cb34cc30ee4d8ddc2146972b5254201
+//
 // This class is not threadsafe, and must only be used on a single sequence.
 class ClientStatsTracker {
  public:
-  // `client` must not be null.
-  explicit ClientStatsTracker(::media::RendererClient* client);
+  // `client` and `cast_metrics_helper` must not be null.
+  explicit ClientStatsTracker(
+      ::media::RendererClient* client,
+      chromecast::metrics::CastMetricsHelper* cast_metrics_helper);
   ~ClientStatsTracker();
 
   // Updates stats based on a buffer pushed to starboard.
@@ -37,11 +53,25 @@ class ClientStatsTracker {
   void UpdateVideoStats(const StarboardPlayerInfo& player_info,
                         const StarboardSampleInfo& sample_info);
 
+  // If enough time has passed, notifies clients of new stats and resets the
+  // timer. Otherwise, this is a no-op.
+  void MaybeSendStatsUpdate();
+
   SEQUENCE_CHECKER(sequence_checker_);
 
   raw_ptr<::media::RendererClient> client_ = nullptr;
-  uint32_t total_video_frames_decoded_ = 0;
-  uint32_t total_video_frames_dropped_ = 0;
+  raw_ptr<chromecast::metrics::CastMetricsHelper> cast_metrics_helper_ =
+      nullptr;
+
+  // These stats track values that we have already reported.
+  ::media::PipelineStatistics total_reported_stats_;
+  // These stats track values that we have not yet reported, due to rate
+  // limiting.
+  ::media::PipelineStatistics pending_stats_;
+
+  // Tracks the amount of time that has elapsed since our last update to client_
+  // and cast_metrics_helper_. Used to rate-limit updates.
+  base::ElapsedTimer last_update_timer_;
 };
 
 }  // namespace media
