d8c347083ee4d2c50f3d7642fe014192fa383c3f
Zonghan Xu <xzonghan@google.com>
2025-10-06 09:16:55 -0700

    [Android Signals Report] Add new `HasPotentiallyHarmful` signals to reporting
    Add the signal value of `HasPotentiallyHarmful` to reporting now that internal method has been implemented.
    
    Bug: 446681100
    Change-Id: I15bf8385fb4ddc94bdc07f7170813010089c1289
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7004270
    Reviewed-by: Sébastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Auto-Submit: Zonghan Xu <xzonghan@chromium.org>
    Commit-Queue: Zonghan Xu <xzonghan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1525648}

--- components/device_signals/core/browser/android/android_os_signals_collector.h
+++ components/device_signals/core/browser/android/android_os_signals_collector.h
@@ -16,9 +16,7 @@ class CloudPolicyManager;
 namespace safe_browsing {
 
 enum class VerifyAppsEnabledResult;
-
-using VerifyAppsResponseCallback =
-    base::OnceCallback<void(VerifyAppsEnabledResult)>;
+enum class HasHarmfulAppsResultStatus;
 
 }  // namespace safe_browsing
 
@@ -44,12 +42,17 @@ class AndroidOsSignalsCollector : public BaseSignalsCollector {
                     base::OnceClosure done_closure);
 
   void OnIsVerifyAppsEnabled(
-      UserPermission permission,
-      const SignalsAggregationRequest& request,
       SignalsAggregationResponse& response,
       std::unique_ptr<OsSignalsResponse> os_signals_response,
       base::OnceClosure done_closure,
       safe_browsing::VerifyAppsEnabledResult result);
+
+  void OnHasPotentiallyHarmfulApps(
+      SignalsAggregationResponse& response,
+      std::unique_ptr<OsSignalsResponse> os_signals_response,
+      base::OnceClosure done_closure,
+      safe_browsing::HasHarmfulAppsResultStatus result,
+      int num_of_apps);
 
   const raw_ptr<policy::CloudPolicyManager> device_cloud_policy_manager_;
   base::WeakPtrFactory<AndroidOsSignalsCollector> weak_factory_{this};
