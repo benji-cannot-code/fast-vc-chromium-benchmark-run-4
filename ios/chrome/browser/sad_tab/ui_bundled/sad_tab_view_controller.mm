8f8d247f084f99b8f41e8584c0f9bffba82dea0a
Sylvain Defresne <sdefresne@chromium.org>
2025-10-06 09:27:00 -0700

    Revert "[iOS] iOS 16 deprecation code cleanup"
    This reverts commit 076e8a1f12bd3a9b4d3ad1d18581ad26ff074d0b.
    
    Reason for revert: breaks the compilation of iOS WebView which
    needs to support iOS 16.0.
    
    Failures:
    https://ci.chromium.org/ui/p/chrome/builders/ci/ios-web-view-simulator/146869/overview
    
    Original change's description:
    > [iOS] iOS 16 deprecation code cleanup
    >
    > Each year the Chrome for iOS team increases the application's minimum
    > supported iOS version. During the process, run/compile-time flags are
    > added throughout the codebase to transitions areas of the code using
    > deprecated APIs to the newer variants. Once the minimum supported
    > version is bumped, these guards are no longer needed.
    >
    > This CL is part of a large-scale effort to remove the remaining
    > obsolete guards from the application's codebase.
    >
    > This CL was uploaded by git cl split.
    >
    > R=gambard@chromium.org
    >
    > Bug: 403260493
    > Change-Id: I6b0fdd131d316a19c5ed2505d7ce1ef607d25c8d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7004274
    > Commit-Queue: Daniel White <danieltwhite@google.com>
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Auto-Submit: Daniel White <danieltwhite@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1524911}
    
    Bug: 403260493
    Change-Id: I939c9e9990d98cb32888823be07e943002387acb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7011789
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Reviewed-by: Daniel White <danieltwhite@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1525652}

--- ios/chrome/browser/sad_tab/ui_bundled/sad_tab_view_controller.mm
+++ ios/chrome/browser/sad_tab/ui_bundled/sad_tab_view_controller.mm
@@ -72,10 +72,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self.overscrollActionsController setStyle:style];
   [self updateOverscrollActionsState];
 
-  NSArray<UITrait>* traits = TraitCollectionSetForTraits(
-      @[ UITraitHorizontalSizeClass.class, UITraitVerticalSizeClass.class ]);
-  [self registerForTraitChanges:traits
-                     withAction:@selector(updateOverscrollActionsState)];
+  if (@available(iOS 17, *)) {
+    NSArray<UITrait>* traits = TraitCollectionSetForTraits(
+        @[ UITraitHorizontalSizeClass.class, UITraitVerticalSizeClass.class ]);
+    [self registerForTraitChanges:traits
+                       withAction:@selector(updateOverscrollActionsState)];
+  }
 }
 
 - (void)viewDidLayoutSubviews {
@@ -88,6 +90,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   self.sadTabView.frame = newFrame;
   [self.scrollView setContentSize:newFrame.size];
 }
+
+#if !defined(__IPHONE_17_0) || __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_17_0
+- (void)traitCollectionDidChange:(UITraitCollection*)previousTraitCollection {
+  [super traitCollectionDidChange:previousTraitCollection];
+  if (@available(iOS 17, *)) {
+    return;
+  }
+
+  [self updateOverscrollActionsState];
+}
+#endif
 
 #pragma mark - SadTabViewDelegate
 
