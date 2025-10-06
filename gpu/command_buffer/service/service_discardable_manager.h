2baa6781b5754ca599ee38e4e38de920359c4a89
Colin Blundell <blundell@chromium.org>
2025-10-06 03:22:20 -0700

    [//gpu] Remove ServiceDiscardableManager memory dump reporting
    This always reports 0 post-OOP-C.
    
    Bug: 391648152
    Change-Id: I4841a88c182f43eb250b19e0b39eeafeeda45c4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7006386
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1525511}

--- gpu/command_buffer/service/service_discardable_manager.h
+++ gpu/command_buffer/service/service_discardable_manager.h
@@ -9,9 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/containers/lru_cache.h"
-#include "base/memory/memory_pressure_listener.h"
 #include "base/memory/raw_ptr.h"
-#include "base/trace_event/memory_dump_provider.h"
 #include "gpu/command_buffer/common/discardable_handle.h"
 #include "gpu/command_buffer/service/context_group.h"
 #include "gpu/gpu_gles2_export.h"
@@ -23,8 +21,7 @@ class TextureManager;
 class TextureRef;
 }
 
-class GPU_GLES2_EXPORT ServiceDiscardableManager
-    : public base::trace_event::MemoryDumpProvider {
+class GPU_GLES2_EXPORT ServiceDiscardableManager {
  public:
   explicit ServiceDiscardableManager(const GpuPreferences& preferences);
 
@@ -32,11 +29,7 @@ class GPU_GLES2_EXPORT ServiceDiscardableManager
   ServiceDiscardableManager& operator=(const ServiceDiscardableManager&) =
       delete;
 
-  ~ServiceDiscardableManager() override;
-
-  // base::trace_event::MemoryDumpProvider implementation.
-  bool OnMemoryDump(const base::trace_event::MemoryDumpArgs& args,
-                    base::trace_event::ProcessMemoryDump* pmd) override;
+  ~ServiceDiscardableManager();
 
  private:
   struct GpuDiscardableEntry {
