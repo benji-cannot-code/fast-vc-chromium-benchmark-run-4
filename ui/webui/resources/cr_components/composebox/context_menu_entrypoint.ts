7e459b77447dc6b1593b85da7dbd7fc4e3d5f1f0
Aviv Kiss <avivkiss@google.com>
2025-10-06 14:23:30 -0700

    [Cleanup] Apply consistent naming to protected members of context_menu_entrypoint.
    This was an ask from dpapad@ in https://crrev.com/c/6965307.
    
    Bug: b:443939809
    
    Change-Id: Ib5ab4b9689dc637c575316598567ada5ea95cade
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7007842
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Aviv Kiss <avivkiss@google.com>
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Auto-Submit: Aviv Kiss <avivkiss@google.com>
    Cr-Commit-Position: refs/heads/main@{#1525865}

--- ui/webui/resources/cr_components/composebox/context_menu_entrypoint.ts
+++ ui/webui/resources/cr_components/composebox/context_menu_entrypoint.ts
@@ -52,7 +52,7 @@ export class ContextMenuEntrypointElement extends
     return {
       inputsDisabled: {type: Boolean},
       showContextMenuDescription: {type: Boolean},
-      inCreateImageMode: {
+      inCreateImageMode_: {
         reflect: true,
         type: Boolean,
       },
@@ -72,7 +72,7 @@ export class ContextMenuEntrypointElement extends
 
   accessor inputsDisabled: boolean = false;
   accessor showContextMenuDescription: boolean = false;
-  protected accessor inCreateImageMode: boolean = false;
+  protected accessor inCreateImageMode_: boolean = false;
   protected accessor tabSuggestions_: TabInfo[] = [];
   protected accessor tabPreviewUrl_: string = '';
   protected accessor tabPreviewsEnabled_: boolean =
@@ -104,7 +104,7 @@ export class ContextMenuEntrypointElement extends
       }});
   }
 
-  protected addTabContext(e: Event) {
+  protected addTabContext_(e: Event) {
     e.stopPropagation();
 
     const tabElement = e.currentTarget! as HTMLButtonElement;
@@ -137,16 +137,16 @@ export class ContextMenuEntrypointElement extends
     this.tabPreviewUrl_ = previewDataUrl || '';
   }
 
-  protected shouldShowTabPreview(): boolean {
+  protected shouldShowTabPreview_(): boolean {
     return this.tabPreviewsEnabled_ && this.tabPreviewUrl_ !== '';
   }
 
-  protected openImageUpload() {
+  protected openImageUpload_() {
     this.fire('open-image-upload');
     this.$.menu.close();
   }
 
-  protected openFileUpload() {
+  protected openFileUpload_() {
     this.fire('open-file-upload');
     this.$.menu.close();
   }
@@ -158,8 +158,8 @@ export class ContextMenuEntrypointElement extends
   }
 
   protected onCreateImageClick_() {
-    this.fire('create-image-click',
-        {inCreateImageMode: this.inCreateImageMode});
+    this.fire(
+        'create-image-click', {inCreateImageMode: this.inCreateImageMode_});
     this.$.menu.close();
   }
 }
