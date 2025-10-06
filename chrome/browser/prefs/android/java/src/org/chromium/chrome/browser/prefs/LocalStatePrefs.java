3f704dee9dc8ae99eeea3121c00b7d7babbde5c3
Jenna Himawan <jhimawan@google.com>
2025-10-06 15:09:23 -0700

    Add isNativeInitialized method to LocalStatePrefs
    Bug: 444690101
    Change-Id: I571561b2e78f8f1431ee26759eb6653f7d0fa179
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7007965
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1525890}

--- chrome/browser/prefs/android/java/src/org/chromium/chrome/browser/prefs/LocalStatePrefs.java
+++ chrome/browser/prefs/android/java/src/org/chromium/chrome/browser/prefs/LocalStatePrefs.java
@@ -5,19 +5,34 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.prefs;
 
+import org.jni_zero.CalledByNative;
 import org.jni_zero.JNINamespace;
 import org.jni_zero.NativeMethods;
 
 import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
 import org.chromium.components.prefs.PrefService;
 
 /** Helper for retrieving the Local State {@link PrefService}. */
 @JNINamespace("chrome_browser_prefs")
 @NullMarked
 public class LocalStatePrefs {
+    private static boolean sIsNativeReady;
+
     /** Returns the {@link PrefService} associated with local state. */
-    public static PrefService get() {
+    public static @Nullable PrefService get() {
+        if (!areNativePrefsLoaded()) return null;
         return LocalStatePrefsJni.get().getPrefService();
+    }
+
+    /** Returns whether the native side is initialized. */
+    public static boolean areNativePrefsLoaded() {
+        return sIsNativeReady;
+    }
+
+    @CalledByNative
+    private static void setNativePrefsLoaded() {
+        sIsNativeReady = true;
     }
 
     @NativeMethods
--- chrome/browser/prefs/android/local_state_prefs_android.cc
+++ chrome/browser/prefs/android/local_state_prefs_android.cc
@@ -12,6 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chrome_browser_prefs {
 
+void OnLocalStatePrefsLoaded() {
+  JNIEnv* env = base::android::AttachCurrentThread();
+  Java_LocalStatePrefs_setNativePrefsLoaded(env);
+}
+
 static base::android::ScopedJavaLocalRef<jobject>
 JNI_LocalStatePrefs_GetPrefService(JNIEnv* env) {
   return g_browser_process->local_state()->GetJavaObject();
