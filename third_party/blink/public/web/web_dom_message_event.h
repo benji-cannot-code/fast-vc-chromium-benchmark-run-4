70fab8838ae122e14c3ac0c1dcf8792169b6537a
Mike West <mkwst@chromium.org>
2025-10-06 11:02:44 -0700

    Drop unused bits of `WebDOMMessageEvent`.
    `WebDOMMessageEvent::Origin()` is unused, as are many of the arguments
    of the object's constructor. Dropping them to make an upcoming
    refactoring of `MessageEvent` simpler.
    
    Bug: 449581913
    Change-Id: Ibf44eb554dea0b0eb509881eb7c206da5b323a8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7011527
    Commit-Queue: Mike West <mkwst@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1525716}

--- third_party/blink/public/web/web_dom_message_event.h
+++ third_party/blink/public/web/web_dom_message_event.h
@@ -33,12 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <optional>
 
-#include "base/unguessable_token.h"
-#include "third_party/blink/public/common/messaging/message_port_channel.h"
-#include "third_party/blink/public/common/messaging/transferable_message.h"
 #include "third_party/blink/public/platform/web_common.h"
-#include "third_party/blink/public/platform/web_string.h"
-#include "third_party/blink/public/web/web_document.h"
 #include "third_party/blink/public/web/web_dom_event.h"
 #include "third_party/blink/public/web/web_serialized_script_value.h"
 
@@ -48,22 +43,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class WebFrame;
-
 // An interface for posting message events to the target frame. The message
 // events are used for communication between documents and described here:
 // http://www.w3.org/TR/2012/WD-webmessaging-20120313/#terminology
 class BLINK_EXPORT WebDOMMessageEvent : public WebDOMEvent {
  public:
-  WebDOMMessageEvent(const WebSerializedScriptValue& message_data,
-                     const WebString& origin = WebString(),
-                     const WebFrame* source_frame = nullptr,
-                     const WebDocument& target_document = WebDocument(),
-                     std::vector<MessagePortChannel> ports =
-                         std::vector<MessagePortChannel>());
+  explicit WebDOMMessageEvent(const WebSerializedScriptValue& message_data);
   WebDOMMessageEvent() = default;
-
-  WebString Origin();
 
 #if INSIDE_BLINK
   explicit WebDOMMessageEvent(MessageEvent* e) : WebDOMEvent(e) {}
