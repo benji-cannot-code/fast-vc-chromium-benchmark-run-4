692b019295b02ccce1e4fa44aad560b7d435ef25
Marc Treib <treib@chromium.org>
2025-10-07 02:39:34 -0700

    Implement SyncServiceObserver::OnSyncShutdown in c/b/ash/
    Specifically:
    
    SyncAppsyncOptinClient: The implementation can simply be NOTREACHED(),
    since this class must be destroyed before SyncService shutdown (per ctor
    precondition comment, and I've also verified manually that this is
    indeed the case in practice).
    
    SyncConsentScreen: OnSyncShutdown() is *probably* not reached in
    practice, but it's hard to be sure, and it's safe & easy to reset the
    SyncService observation.
    
    The eventual goal is to make SyncServiceObserver::OnSyncShutdown() pure
    virtual, so that all implementations need to consider whether they need
    to do something there (i.e. unregister themselves, drop and SyncService
    pointers).
    
    Bug: 40882734
    Change-Id: Ia3b712bcdb3eb77fc2670c2c3b1c0231fff5aa11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7011294
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1526142}

--- chrome/browser/ash/login/screens/sync_consent_screen.h
+++ chrome/browser/ash/login/screens/sync_consent_screen.h
@@ -106,6 +106,7 @@ class SyncConsentScreen : public BaseScreen,
 
   // syncer::SyncServiceObserver:
   void OnStateChanged(syncer::SyncService* sync) override;
+  void OnSyncShutdown(syncer::SyncService* sync) override;
 
   // Reacts to user action on sync.
   void OnContinue(const bool opted_in,
