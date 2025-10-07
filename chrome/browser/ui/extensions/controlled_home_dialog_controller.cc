11f4760e2a9b3c9c05e9e8d5667f35b0b097c9a7
EmiliaPaz <emiliapaz@chromium.org>
2025-10-06 17:46:16 -0700

    [Extensions] Enable controlled home on Desktop Android
    Bug: 441590893
    Change-Id: I8f694a4e3abb015a955a7790bb05e59c989a60ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7013384
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1525950}

--- chrome/browser/ui/extensions/controlled_home_dialog_controller.cc
+++ chrome/browser/ui/extensions/controlled_home_dialog_controller.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/auto_reset.h"
 #include "base/no_destructor.h"
-#include "base/strings/utf_string_conversions.h"
 #include "base/task/single_thread_task_runner.h"
 #include "chrome/browser/extensions/extension_util.h"
 #include "chrome/browser/extensions/settings_api_helpers.h"
@@ -18,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/branded_strings.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/strings/grit/components_strings.h"
-#include "components/vector_icons/vector_icons.h"
 #include "content/public/browser/page_navigator.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/referrer.h"
