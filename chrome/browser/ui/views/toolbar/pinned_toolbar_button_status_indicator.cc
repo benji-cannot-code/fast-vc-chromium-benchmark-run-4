854216cce478c4e2e287520e34a14e530d72b699
Atharv Maan <atharvmaan@chromium.org>
2025-10-07 16:13:48 -0700

    Refactor PinnedToolbarButtonStatusIndicator paint as active logic
    Refactor the paint as active logic to use
    PlatformStyle::kInactiveWidgetControlsAppearDisabled instead of directly
    using IS_MAC buildflag.
    
    Fixed: 423005399
    Change-Id: I47e47bf7f3bd60c5bf93c53973ee14633393e09d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7018796
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Atharv Maan <atharvmaan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1526581}

--- chrome/browser/ui/views/toolbar/pinned_toolbar_button_status_indicator.cc
+++ chrome/browser/ui/views/toolbar/pinned_toolbar_button_status_indicator.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/geometry/insets.h"
 #include "ui/views/cascading_property.h"
+#include "ui/views/style/platform_style.h"
 #include "ui/views/view_utils.h"
 #include "ui/views/widget/widget.h"
 
@@ -72,13 +73,12 @@ void PinnedToolbarButtonStatusIndicator::OnPaint(gfx::Canvas* canvas) {
 
   // Non-mac desktop devices show active icon colors even when the browser
   // window is inactive.
+  const bool paint_as_active =
+      !views::PlatformStyle::kInactiveWidgetControlsAppearDisabled ||
+      GetWidget()->ShouldPaintAsActive();
+
   std::optional<ui::ColorId> color_id =
-#if BUILDFLAG(IS_MAC)
-      GetWidget()->ShouldPaintAsActive() ? active_color_id_
-                                         : inactive_color_id_;
-#else
-      active_color_id_;
-#endif
+      paint_as_active ? active_color_id_ : inactive_color_id_;
 
   flags.setColor(color_id.has_value()
                      ? GetColorProvider()->GetColor(color_id.value())
