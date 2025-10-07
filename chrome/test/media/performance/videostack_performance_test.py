49282b8d01cfc91dedbada2f2397f32de714637e
Bailey Myers-Morgan <bmyersmorgan@google.com>
2025-10-07 11:35:56 -0700

    [VideoStack] Create directory and scaffolding for VideoStack AV performance tests
    Bug: 446000305
    Change-Id: I935e9eca0755083a224df1a1e3172025f35d0de8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7014628
    Reviewed-by: Zijie He <zijiehe@google.com>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Bailey Myers-Morgan <bmyersmorgan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1526438}

---
+++ chrome/test/media/performance/videostack_performance_test.py
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env vpython3
+# Copyright 2025 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+""" Execute VideoStack audio/video performance tests. """
+
+import sys
+
+
+#TODO(crbug.com/446000305): Implement tests.
+def main() -> int:
+    print('Running performance test.')
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
