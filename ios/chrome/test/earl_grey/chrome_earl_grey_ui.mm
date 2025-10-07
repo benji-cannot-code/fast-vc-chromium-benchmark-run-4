ba03f57e7e8b0a5a09f6c192c879be89a454e79f
Filipa Senra <fsenra@google.com>
2025-10-07 07:07:39 -0700

    [QD] Merge constants into one file
    This CL merges the constants used by Quick Delete into one file. It also
    renames it for better consistency.
    
    Bypass-Check-License: Renaming file
    Bug: 367734944
    Change-Id: If59c064e796b6730c22a29842c0e1da1be5b7e22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7008378
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Filipa Senra <fsenra@google.com>
    Cr-Commit-Position: refs/heads/main@{#1526259}

--- ios/chrome/test/earl_grey/chrome_earl_grey_ui.mm
+++ ios/chrome/test/earl_grey/chrome_earl_grey_ui.mm
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/strings/grit/components_strings.h"
 #import "ios/chrome/browser/browser_container/ui_bundled/edit_menu_app_interface.h"
 #import "ios/chrome/browser/popup_menu/ui_bundled/popup_menu_constants.h"
-#import "ios/chrome/browser/settings/ui_bundled/cells/clear_browsing_data_constants.h"
+#import "ios/chrome/browser/settings/ui_bundled/clear_browsing_data/quick_delete_constants.h"
 #import "ios/chrome/browser/shared/ui/table_view/table_view_constants.h"
 #import "ios/chrome/browser/toolbar/ui_bundled/public/toolbar_constants.h"
 #import "ios/chrome/grit/ios_strings.h"
