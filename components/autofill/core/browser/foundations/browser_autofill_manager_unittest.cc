7e71753f9dc3d8643b68218137a7513522260051
Christoph Schwering <schwering@google.com>
2025-10-06 23:19:57 -0700

    [Autofill] Clean up AutofillUnionTypesForAutofillAi
    Bug: 432645177
    Change-Id: I6500cb8337e2cc69bf71c95c646fb39d06a21b42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7015088
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Commit-Queue: Dominic Battre <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1526037}

--- components/autofill/core/browser/foundations/browser_autofill_manager_unittest.cc
+++ components/autofill/core/browser/foundations/browser_autofill_manager_unittest.cc
@@ -7585,12 +7585,6 @@ TEST_F(BrowserAutofillManagerTest, ComposeSuggestionsAreQueriedForTextareas) {
 class BrowserAutofillManagerTest_AutofillAi
     : public BrowserAutofillManagerTest {
  public:
-  BrowserAutofillManagerTest_AutofillAi() {
-    feature_list_.InitWithFeatures({features::kAutofillAiWithDataSchema,
-                                    features::kAutofillUnionTypesForAutofillAi},
-                                   {});
-  }
-
   void SetUp() override {
     BrowserAutofillManagerTest::SetUp();
     autofill_client().set_entity_data_manager(
@@ -7683,7 +7677,8 @@ class BrowserAutofillManagerTest_AutofillAi
   }
 
  private:
-  base::test::ScopedFeatureList feature_list_;
+  base::test::ScopedFeatureList feature_list_{
+      features::kAutofillAiWithDataSchema};
   AutofillWebDataServiceTestHelper webdata_helper_{
       std::make_unique<EntityTable>()};
   FormData passport_form_;
