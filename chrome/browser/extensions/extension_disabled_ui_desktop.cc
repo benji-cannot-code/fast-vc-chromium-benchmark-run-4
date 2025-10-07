3da3d957e1579512a62bc3c011222602ccc88978
vptyshchenko@yandex-team.ru <vptyshchenko@yandex-team.ru>
2025-10-07 03:21:36 -0700

    Fix ExtensionDisabledGlobalError extensions comparison
    Extension comparison must be done by comparing IDs instead of comparing
    extension pointer. This is important in case of extension updating:
    during this process Extension object is recreated, which may lead to
    crash if we have triggered the same error twice (this is achievable by
    updating the same extension twice and then removing it, after this,
    some of the global errors will remain visible and could access removed
    extension).
    
    Change-Id: I56e1f71c6cb07d09f9f0906633975887234a4930
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7000087
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Alexander Yashkin <a-v-y@yandex-team.ru>
    Cr-Commit-Position: refs/heads/main@{#1526160}

--- chrome/browser/extensions/extension_disabled_ui_desktop.cc
+++ chrome/browser/extensions/extension_disabled_ui_desktop.cc
@@ -261,8 +261,9 @@ void ExtensionDisabledGlobalError::OnExtensionUninstallDialogClosed(
 void ExtensionDisabledGlobalError::OnExtensionLoaded(
     content::BrowserContext* browser_context,
     const Extension* extension) {
-  if (extension != extension_)
+  if (extension->id() != extension_->id()) {
     return;
+  }
   RemoveGlobalError();
 }
 
@@ -270,8 +271,9 @@ void ExtensionDisabledGlobalError::OnExtensionUninstalled(
     content::BrowserContext* browser_context,
     const Extension* extension,
     UninstallReason reason) {
-  if (extension != extension_)
+  if (extension->id() != extension_->id()) {
     return;
+  }
   RemoveGlobalError();
 }
 
