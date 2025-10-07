d1b71705d2b98a26e7266d0431fc1dc0c481964b
Eriko Kurimoto <elkurin@chromium.org>
2025-10-06 18:40:55 -0700

    Revert "Reland "[Windows] Add a sandbox string class for low-level heap allocations.""
    This reverts commit bec2adbc7df6acd3ec9771c1d2a3861f2dc8e193.
    
    Reason for revert: Primary culprit of win-asan bot breakage by
    GPU process crash
    
    Original change's description:
    > Reland "[Windows] Add a sandbox string class for low-level heap allocations."
    >
    > This is a reland of commit 2f89ea6c9429d9544f4bc9a0aba2940b0b235e9c
    >
    > The original patch removed a call to strlen, which was reintroduced
    > by the compiler. As the code was being called early in process
    > startup it wasn't valid to call an imported function and this resulted
    > in a crash in certain builds. This updated CL reintroduces the
    > NTDLL strlen call.
    >
    > Original change's description:
    > > [Windows] Add a sandbox string class for low-level heap allocations.
    > >
    > > This CL adds a class to represent a low-level heap allocated string.
    > > In addition the class is then used in various NT utility functions
    > > which return strings with corresponding changes to the call sites for
    > > those utilities.
    > >
    > > Bug: 447171244
    > > Change-Id: Iaaa714707b4476582b34aa12a4e99522ec1a9a41
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7000096
    > > Reviewed-by: Alex Gough <ajgo@chromium.org>
    > > Commit-Queue: James Forshaw <forshaw@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1524571}
    >
    > Bug: 447171244
    > Change-Id: Icd2b9b71e9f383507b5ee998f92273f1483998b4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7002993
    > Commit-Queue: James Forshaw <forshaw@chromium.org>
    > Reviewed-by: Alex Gough <ajgo@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1524880}
    
    Bug: 447171244
    Change-Id: Ie156f368176ece3298660822803f791b4f00d0b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7005550
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Eriko Kurimoto <elkurin@chromium.org>
    Owners-Override: Eriko Kurimoto <elkurin@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1525971}

--- sandbox/win/fuzzer/sandbox_nt_utils_fuzzer.cc
+++ sandbox/win/fuzzer/sandbox_nt_utils_fuzzer.cc
@@ -6,18 +6,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
-#include <string_view>
-
 #include "sandbox/win/src/nt_internals.h"
 #include "sandbox/win/src/sandbox_nt_util.h"
+
+using ScopedUnicodeString =
+    std::unique_ptr<UNICODE_STRING, sandbox::NtAllocDeleter>;
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   if (size % 2 == 1)
     return 0;
 
-  std::wstring_view module_path{
-      reinterpret_cast<wchar_t*>(const_cast<uint8_t*>(data)),
-      size / sizeof(wchar_t)};
-  sandbox::ExtractModuleName(module_path);
+  UNICODE_STRING module_path;
+  module_path.Buffer = reinterpret_cast<wchar_t*>(const_cast<uint8_t*>(data));
+  module_path.Length = size;
+  module_path.MaximumLength = size;
+
+  ScopedUnicodeString result(sandbox::ExtractModuleName(&module_path));
   return 0;
 }
