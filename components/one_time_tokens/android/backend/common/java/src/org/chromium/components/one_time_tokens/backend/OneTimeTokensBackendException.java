7cab8c681a48519357a4910ffe61bce26bf56244
trang mai <trangm@google.com>
2025-10-07 01:04:34 -0700

    [OTP] Add OneTimeTokensBackendException
    Bug: 445928642
    Change-Id: Iedcd4c673acf21845e83ff179b84e0b486b82a0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7002964
    Reviewed-by: Matthias Körber <koerber@google.com>
    Commit-Queue: Trang Mai <trangm@google.com>
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1526096}

---
+++ components/one_time_tokens/android/backend/common/java/src/org/chromium/components/one_time_tokens/backend/OneTimeTokensBackendException.java
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.one_time_tokens.backend;
+
+import androidx.annotation.Nullable;
+
+import org.chromium.build.annotations.NullMarked;
+
+/** Exception thrown when there is an error in the one-time token backend. */
+@NullMarked
+public class OneTimeTokensBackendException extends Exception {
+    private final @OneTimeTokensBackendErrorCode int mErrorCode;
+
+    public OneTimeTokensBackendException(
+            @Nullable String message, @OneTimeTokensBackendErrorCode int errorCode) {
+        super(message);
+        mErrorCode = errorCode;
+    }
+
+    public @OneTimeTokensBackendErrorCode int getErrorCode() {
+        return mErrorCode;
+    }
+}
