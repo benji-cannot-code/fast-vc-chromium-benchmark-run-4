bff23e0c28724e42571b7aeb1fb6ab7462871a91
HuanPo Lin <robertlin@chromium.org>
2025-10-06 19:44:38 -0700

    prefetch: Reflect Devtools emulated user agent override in headers
    Currently, if a navigation used prefetched result and the User-Agent
    override information are unexpected values. This CL makes the prefetch
    uses the same userAgent header values from Devtools emulation mode as
    the referrer page.
    
    Related document:
    https://docs.google.com/document/d/1ONfF4dfeVP5T0HHNfma6B3yL4muYukC7aLmFUuHvmVY/edit?usp=sharing
    
    Bug: 422193319, 444065296
    Change-Id: Id5ec30c56428d9c39dd961f030b1bab9c12dc65c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6719107
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Ken Okada <kenoss@chromium.org>
    Reviewed-by: Takashi Nakayama <tnak@chromium.org>
    Reviewed-by: Victor Tan <victortan@chromium.org>
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1525993}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/prefetch/prefetch-ua-override.js
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(/** @type {import('test_runner').TestRunner} */ testRunner) {
+    const { page, dp, session } = await testRunner.startBlank(
+      `Tests that prefetch gets the UA override.`);
+
+  await dp.Preload.enable();
+  await session.protocol.Emulation.setUserAgentOverride({
+    userAgent: 'Lynx v0.1',
+    userAgentMetadata: {
+      platform: 'Lynx',
+      platformVersion: '0.1',
+      architecture: '',
+      model: 'foobar',
+      mobile: true
+    }
+  });
+
+  page.navigate("https://127.0.0.1:8443/inspector-protocol/prefetch/resources/prefetch-echo-header.html");
+  await dp.Preload.oncePrefetchStatusUpdated(e => e.params.status === 'Ready');
+
+  // Go to the prefetched page.
+  await session.evaluate(`document.getElementById('link').click()`);
+  let textContent = await session.evaluate(`document.body.textContent`);
+
+  // Wait until header is printed.
+  while (textContent === undefined) {
+    textContent = await session.evaluate(`document.body.textContent`);
+  }
+
+  testRunner.log(textContent);
+  testRunner.completeTest();
+});
---
+++ third_party/blink/web_tests/virtual/enable-prefetch-devtools-ua-override/README.md
@@ -0,0 +1,2 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+This test suite is for tests of speculation rules prefetch Devtools UA override. See https://crbug.com/422193319
