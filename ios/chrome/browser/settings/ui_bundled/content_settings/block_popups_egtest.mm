6aacb37593ad4ade443ccb4f42007031ff618e35
ginnnnnnny <ginnyhuang@chromium.org>
2025-10-07 09:08:34 -0700

    [iOS] Deflake test case testSettingsPageWithExceptions
    Bug: 441738071
    Change-Id: I38f3390d85efb52ba4d895af13272ab5360e5ba5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7017068
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1526326}

--- ios/chrome/browser/settings/ui_bundled/content_settings/block_popups_egtest.mm
+++ ios/chrome/browser/settings/ui_bundled/content_settings/block_popups_egtest.mm
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/scoped_block_popups_pref.h"
+#import "ios/chrome/test/earl_grey/scoped_disable_timer_tracking.h"
 #import "ios/chrome/test/earl_grey/web_http_server_chrome_test_case.h"
 #import "ios/chrome/test/scoped_eg_synchronization_disabler.h"
 #import "ios/testing/earl_grey/earl_grey_test.h"
@@ -174,21 +175,16 @@ class ScopedBlockPopupsException {
 
 // Tests that the "exceptions" section on the settings page is hidden and
 // revealed properly when the preference switch is toggled.
-// TODO(crbug.com/441738071): Flaky on device.
-#if TARGET_OS_IPHONE && !TARGET_OS_SIMULATOR
-#define MAYBE_testSettingsPageWithExceptions \
-  FLAKY_testSettingsPageWithExceptions
-#else
-#define MAYBE_testSettingsPageWithExceptions \
-  testSettingsPageWithExceptions
-#endif
-- (void)MAYBE_testSettingsPageWithExceptions {
+- (void)testSettingsPageWithExceptions {
   std::string allowedPattern = "[*.]example.com";
   ScopedBlockPopupsPref prefSetter(CONTENT_SETTING_BLOCK);
   ScopedBlockPopupsException exceptionSetter(allowedPattern);
 
   [ChromeEarlGreyUI openSettingsMenu];
-  [ChromeEarlGreyUI tapSettingsMenuButton:ContentSettingsButton()];
+  {
+    ScopedDisableTimerTracking disabler;
+    [ChromeEarlGreyUI tapSettingsMenuButton:ContentSettingsButton()];
+  }
   [[EarlGrey selectElementWithMatcher:BlockPopupsSettingsButton()]
       performAction:grey_tap()];
 
