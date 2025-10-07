54fe343a1a92ad8fa7ee42b8f371b8d5be171d3a
Christoph Schwering <schwering@google.com>
2025-10-07 03:20:27 -0700

    [Autofill] Remove / fix wrong LINT IfThisThenThats
    The CL changes the following IfThisThenThat:
    
    - It removes IfThisThenThat AutofillAiEntityType --> GetNameForI18n().
      The two are rather unrelated.
    
    - It removes IfthisThenThat GetNameForI18n() --> AutofillAiEntityType
      (the reverse direction of the previous one).
      Its point was that if a new entity type is added to the schema,
      the C++ compiler asks for an update to GetNameForI18n() because of
      its `switch`, and then the IfThisThenThat asks for a change to the
      enum. That's quite indirect and it doesn't scale to >1 enums because
      IfThisThenThat cannot be nested on Gerrit.
      crrev.com/c/7011295 extends update_autofill_enums.py as a replacement.
    
    - It updates IfThisThenThat Autofill.Ai.EntityType --> GetNameForI18n()
      and  EntityTypeToMetricsString() --> AutofillAiEntityType to
      to Autofill.Ai.EntityType <-> EntityTypeToMetricsString().
      That old relationships were wrong: the histogram's variants are
      determined by EntityTypeToMetricsString(), not GetNameForI18n().
    
    NO_IFTTT=No enum change.
    
    Bug: 40100455
    Change-Id: Ide9755398b93c4e9aa088371f0f0ab82eaf3a28c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7009813
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1526158}

--- components/autofill/core/browser/data_model/autofill_ai/entity_type.cc
+++ components/autofill/core/browser/data_model/autofill_ai/entity_type.cc
@@ -151,7 +151,6 @@ bool EntityType::ImportOrder(const EntityType& lhs, const EntityType& rhs) {
   return rank(lhs) < rank(rhs);
 }
 
-// LINT.IfChange(EntityType)
 std::u16string EntityType::GetNameForI18n() const {
   switch (name()) {
     case EntityTypeName::kDriversLicense:
@@ -176,7 +175,6 @@ std::u16string EntityType::GetNameForI18n() const {
   }
   NOTREACHED();
 }
-// LINT.ThenChange(/tools/metrics/histograms/metadata/autofill/enums.xml:AutofillAiEntityType)
 
 std::optional<EntityTypeName> ToSafeEntityTypeName(
     std::underlying_type_t<EntityTypeName> raw_value) {
