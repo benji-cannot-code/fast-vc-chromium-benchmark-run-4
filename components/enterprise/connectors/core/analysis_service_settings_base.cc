7f0f919a61a5cfda82c6849df91f8bb21722cd27
Ana Ercegovac <anaercegovac@google.com>
2025-10-07 01:45:40 -0700

    Move constructor logic to AnalysisServiceSettingsBase.
    Bug: 444237640
    Change-Id: I526006d8302e5af0a3da0386893f08914993d678
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7003799
    Reviewed-by: Haihan Chen <haihan@google.com>
    Commit-Queue: Ana Ercegovac <anaercegovac@google.com>
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1526117}

--- components/enterprise/connectors/core/analysis_service_settings_base.cc
+++ components/enterprise/connectors/core/analysis_service_settings_base.cc
@@ -12,6 +12,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace enterprise_connectors {
 
+AnalysisServiceSettingsBase::AnalysisServiceSettingsBase(
+    const base::Value& settings_value,
+    const ServiceProviderConfig& service_provider_config) {
+  if (!settings_value.is_dict()) {
+    return;
+  }
+
+  const auto& settings_dict = settings_value.GetDict();
+
+  if (!TryParseServiceProviderData(settings_dict, service_provider_config)) {
+    return;
+  }
+
+  ParseBlockSettings(settings_dict);
+  ParseMinimumDataSize(settings_dict);
+  ParseCustomMessages(settings_dict);
+  ParseJustificationTags(settings_dict);
+}
+
 bool AnalysisServiceSettingsBase::TryParseServiceProviderData(
     const base::Value::Dict& settings_dict,
     const ServiceProviderConfig& service_provider_config) {
@@ -195,7 +214,6 @@ void AnalysisServiceSettingsBase::AddUrlPatternSettings(
   }
 }
 
-AnalysisServiceSettingsBase::AnalysisServiceSettingsBase() = default;
 AnalysisServiceSettingsBase::AnalysisServiceSettingsBase(
     AnalysisServiceSettingsBase&&) = default;
 AnalysisServiceSettingsBase& AnalysisServiceSettingsBase::operator=(
