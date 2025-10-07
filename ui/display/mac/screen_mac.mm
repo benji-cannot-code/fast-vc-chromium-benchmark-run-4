c5d611abc6eddaa5a1a1da107bbee0944e6f44c5
Colin Blundell <blundell@chromium.org>
2025-10-06 23:53:28 -0700

    [//ui] Port screen_mac.mm's calls to DisplayColorSpaces
    Ports it from using the deprecated SetOutputColorSpaceAndBufferFormat()
    to the replacement SetOutputColorSpaceAndFormat() as part of eliminating
    BufferFormat in favor of SharedImageFormat. The change is a no-op
    functionally [1].
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/7006387
    
    Bug: 445879532
    Change-Id: I53a99f5715afd23f2037f6a841ed93c91f00c75d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7010850
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1526051}

--- ui/display/mac/screen_mac.mm
+++ ui/display/mac/screen_mac.mm
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
 #include "components/device_event_log/device_event_log.h"
+#include "components/viz/common/resources/shared_image_format.h"
 #include "ui/display/display.h"
 #include "ui/display/display_change_notifier.h"
 #include "ui/display/mac/screen_mac_headless.h"
@@ -165,9 +166,10 @@ DisplayMac BuildDisplayForScreen(NSScreen* screen) {
     if (enable_hdr) {
       bool needs_alpha_values[] = {true, false};
       for (const auto& needs_alpha : needs_alpha_values) {
-        display_color_spaces.SetOutputColorSpaceAndBufferFormat(
+        display_color_spaces.SetOutputColorSpaceAndFormat(
             gfx::ContentColorUsage::kHDR, needs_alpha,
-            gfx::ColorSpace::CreateExtendedSRGB(), gfx::BufferFormat::RGBA_F16);
+            gfx::ColorSpace::CreateExtendedSRGB(),
+            viz::SinglePlaneFormat::kRGBA_F16);
       }
       display_color_spaces.SetHDRMaxLuminanceRelative(hdr_max_lum_relative);
     }
