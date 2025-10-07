7362d82c0372f0bf78461c8dcce8c96d858d3592
Tom Sepez <tsepez@chromium.org>
2025-10-06 19:49:10 -0700

    Remove another UNSAFE_TODO in video_codec_string_parsers.cc
    Use aggregate initialization of a std::array rather than memset.
    
    Change-Id: Ibcbee0dc47ffa9e2cb47b7fea06622b5d773faf2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7013393
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1525995}

--- media/base/video_codec_string_parsers.cc
+++ media/base/video_codec_string_parsers.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <array>
 #include <string_view>
 
-#include "base/compiler_specific.h"
 #include "base/logging.h"
 #include "base/notreached.h"
 #include "base/strings/string_number_conversions.h"
@@ -675,10 +674,7 @@ std::optional<VideoType> ParseHEVCCodecId(std::string_view codec_id) {
     return std::nullopt;
   }
 
-  std::array<uint8_t, 6> constraint_flags;
-  UNSAFE_TODO(memset(constraint_flags.data(), 0,
-                     (constraint_flags.size() *
-                      sizeof(decltype(constraint_flags)::value_type))));
+  std::array<uint8_t, 6> constraint_flags = {};
 
   if (elem.size() > 10) {
     DVLOG(4) << __func__ << ": unexpected number of trailing bytes in HEVC "
