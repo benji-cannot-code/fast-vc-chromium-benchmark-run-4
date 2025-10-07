048edf09308b8024b176c5fa3cd81b01e9c4f4b8
Victor Hugo Vianna Silva <victorvianna@google.com>
2025-10-07 10:00:42 -0700

    Fix C++23 build error in components/origin_matcher/
    OriginMatcherRule must be a complete type by the time it's manipulated
    with unique_ptr in [1]. I was surprised to see this refers to the C++
    class in [2] and not the Mojo class in [3].
    Fix by ensuring that the C++ header is included before the Mojo one.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:out/mac-Debug/gen/components/origin_matcher/origin_matcher.mojom.h;l=477;drc=9a3a7956c6e16af479ac11fe63e1ceac53091ad0
    [2] https://source.chromium.org/chromium/chromium/src/+/main:components/origin_matcher/origin_matcher_internal.h;l=20;drc=2de29efe573e2d6cf3831c516cb47772c57e135a
    [3] https://source.chromium.org/chromium/chromium/src/+/main:out/mac-Debug/gen/components/origin_matcher/origin_matcher.mojom.h;l=211;drc=9a3a7956c6e16af479ac11fe63e1ceac53091ad0
    
    Bug: 388068055
    Change-Id: I28df3a402d008e01e1d3b6f9bf13c13301131b85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7003254
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1526370}

--- components/origin_matcher/origin_matcher_mojom_traits.h
+++ components/origin_matcher/origin_matcher_mojom_traits.h
@@ -9,9 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "components/origin_matcher/origin_matcher.h"
-#include "components/origin_matcher/origin_matcher.mojom.h"
 #include "components/origin_matcher/origin_matcher_internal.h"
 #include "mojo/public/cpp/bindings/struct_traits.h"
+
+// List below origin_matcher_internal.h, so OriginMatcherRule's a complete type.
+#include "components/origin_matcher/origin_matcher.mojom.h"
 
 namespace mojo {
 
--- components/origin_matcher/origin_matcher_unittest.cc
+++ components/origin_matcher/origin_matcher_unittest.cc
@@ -7,13 +7,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/test/scoped_feature_list.h"
 #include "components/origin_matcher/features.h"
-#include "components/origin_matcher/origin_matcher.mojom.h"
 #include "components/origin_matcher/origin_matcher_internal.h"
 #include "mojo/public/cpp/test_support/test_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
 #include "url/origin.h"
 #include "url/url_util.h"
+
+// List below origin_matcher_internal.h, so OriginMatcherRule's a complete type.
+#include "components/origin_matcher/origin_matcher.mojom.h"
 
 namespace origin_matcher {
 
