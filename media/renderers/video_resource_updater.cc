f02658636cd4e0feaaf6f315e0794b7803c55d06
kylechar <kylechar@chromium.org>
2025-10-07 10:04:59 -0700

    Switch VideoResourceUpdater row size function
    Change viz::ResourceSizes::CheckedWidthInBytes() to instead be
    SharedMemoryRowSizeForSharedImageFormat(). This is the last usage of
    deprecated resource size functions.
    
    The new function aligns row widths to 4 bytes but this shouldn't make
    any difference since format is either RGBA_8888, BGRA_8888 or RGBA_F16
    which are always four byte aligned.
    
    Bug: 446886466
    Change-Id: Ib224e043349bc3629e76c6015f64722e53d3aee7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7012168
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1526375}

--- media/renderers/video_resource_updater.cc
+++ media/renderers/video_resource_updater.cc
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/quads/compositor_render_pass.h"
 #include "components/viz/common/quads/texture_draw_quad.h"
 #include "components/viz/common/quads/video_hole_draw_quad.h"
-#include "components/viz/common/resources/resource_sizes.h"
 #include "components/viz/common/resources/shared_image_format_utils.h"
 #include "gpu/GLES2/gl2extchromium.h"
 #include "gpu/command_buffer/client/client_shared_image.h"
@@ -941,8 +940,10 @@ bool VideoResourceUpdater::WriteRGBPixelsToTexture(
     FrameResource* hardware_resource) {
   CHECK(!hardware_resource->is_software());
   viz::SharedImageFormat resource_format = hardware_resource->format();
-  size_t bytes_per_row = viz::ResourceSizes::CheckedWidthInBytes<size_t>(
-      video_frame->coded_size().width(), resource_format);
+  size_t bytes_per_row =
+      viz::SharedMemoryRowSizeForSharedImageFormat(
+          resource_format, 0, video_frame->coded_size().width())
+          .value();
   const int stride = video_frame->stride(VideoFrame::Plane::kARGB);
   const bool has_compatible_stride =
       stride > 0 && static_cast<size_t>(stride) == bytes_per_row;
