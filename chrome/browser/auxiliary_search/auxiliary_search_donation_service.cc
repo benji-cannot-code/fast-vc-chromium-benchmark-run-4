4c999327883b1825b94ebb0843b6f424749c2c73
Darren Shen <shend@chromium.org>
2025-10-06 18:18:01 -0700

    Add skeleton AuxiliarySearchDonationService service + factory.
    Add a new keyed service that observes
    PageContentAnnotationsService. The implementation of the observer
    method is left for a future CL to keep this CL small.
    
    Bug: 432359106
    Change-Id: Ibf236a5612a60cf12d3f35067484d03c14e3e657
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6998874
    Reviewed-by: Hailey Wang <haileywang@google.com>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1525963}

---
+++ chrome/browser/auxiliary_search/auxiliary_search_donation_service.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/auxiliary_search/auxiliary_search_donation_service.h"
+
+#include "components/page_content_annotations/core/page_content_annotation_type.h"
+#include "components/page_content_annotations/core/page_content_annotations_common.h"
+#include "components/page_content_annotations/core/page_content_annotations_service.h"
+#include "url/gurl.h"
+
+AuxiliarySearchDonationService::AuxiliarySearchDonationService(
+    page_content_annotations::PageContentAnnotationsService*
+        page_content_annotations_service)
+    : page_content_annotations_service_(page_content_annotations_service) {
+  CHECK(page_content_annotations_service_);
+  page_content_annotations_service_->AddObserver(
+      page_content_annotations::AnnotationType::kContentVisibility, this);
+}
+
+AuxiliarySearchDonationService::~AuxiliarySearchDonationService() {
+  page_content_annotations_service_->RemoveObserver(
+      page_content_annotations::AnnotationType::kContentVisibility, this);
+}
+
+void AuxiliarySearchDonationService::OnPageContentAnnotated(
+    const GURL& url,
+    const page_content_annotations::PageContentAnnotationsResult& result) {
+  // TODO: b/432359106 - Implement this using FetchAndRankHelper and
+  // AuxiliarySearchDonor
+}
