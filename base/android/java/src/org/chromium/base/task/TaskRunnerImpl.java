30bef716d441a36f81864425c6fa736f1c1baf43
Anand Ravi <anandrv@google.com>
2025-10-07 06:22:34 -0700

    [base/android] Introduce Location class in Java
    When analyzing traces, it is often difficult to determine the origin of
    a task posted to the native Android task runner from Java, making it
    hard to attribute work to the correct feature or component. While C++
    has base::Location and FROM_HERE to solve this, there has been no direct
    equivalent for the Java task API.
    
    This initial change lays the groundwork by introducing
    org.chromium.base.task.Location, a Java class mirroring the C++
    base::Location concept. It then plumbs the source location data from
    TaskRunnerImpl to the native task runner, which adds the information to
    trace events. A follow-up change will introduce the build-time bytecode
    rewriting that replaces methods that queue tasks to the native task
    runner with overloads that take in a Location object, and pass a
    Location object populated with the correct source code location
    information.
    
    Change-Id: Iebe4daced56dda0c7bcdc1cacff9b63e92dbe0c5
    Bug: 448086090
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6995600
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Anand Ravi <anandrv@google.com>
    Cr-Commit-Position: refs/heads/main@{#1526242}

--- base/android/java/src/org/chromium/base/task/TaskRunnerImpl.java
+++ base/android/java/src/org/chromium/base/task/TaskRunnerImpl.java
@@ -174,13 +174,13 @@ public class TaskRunnerImpl implements TaskRunner {
         }
         // Lock-free path when native is initialized.
         if (mNativeTaskRunnerAndroid != 0) {
-            queueDelayedTaskToNative(mNativeTaskRunnerAndroid, task, delay);
+            queueDelayedTaskToNative(mNativeTaskRunnerAndroid, task, delay, null);
             return;
         }
         synchronized (mPreNativeTaskLock) {
             oneTimeInitialization();
             if (mNativeTaskRunnerAndroid != 0) {
-                queueDelayedTaskToNative(mNativeTaskRunnerAndroid, task, delay);
+                queueDelayedTaskToNative(mNativeTaskRunnerAndroid, task, delay, null);
                 return;
             }
             // We don't expect a whole lot of these, if that changes consider pooling them.
@@ -265,13 +265,14 @@ public class TaskRunnerImpl implements TaskRunner {
         synchronized (mPreNativeTaskLock) {
             if (mPreNativeTasks != null) {
                 for (Runnable task : mPreNativeTasks) {
-                    queueDelayedTaskToNative(nativeTaskRunnerAndroid, task, 0);
+                    queueDelayedTaskToNative(nativeTaskRunnerAndroid, task, 0, null);
                 }
                 mPreNativeTasks = null;
             }
             if (mPreNativeDelayedTasks != null) {
                 for (Pair<Runnable, Long> task : mPreNativeDelayedTasks) {
-                    queueDelayedTaskToNative(nativeTaskRunnerAndroid, task.first, task.second);
+                    queueDelayedTaskToNative(
+                            nativeTaskRunnerAndroid, task.first, task.second, null);
                 }
                 mPreNativeDelayedTasks = null;
             }
@@ -292,10 +293,21 @@ public class TaskRunnerImpl implements TaskRunner {
     }
 
     private static void queueDelayedTaskToNative(
-            long nativeTaskRunnerAndroid, Runnable task, long delay) {
+            long nativeTaskRunnerAndroid, Runnable task, long delay, @Nullable Location location) {
         // If there's no delay, then try to store it in the table. Otherwise use the map.
         int taskIndex = queueTask(task, /* useTable= */ delay == 0);
-        TaskRunnerImplJni.get().postDelayedTask(nativeTaskRunnerAndroid, delay, taskIndex);
+        if (location != null) {
+            TaskRunnerImplJni.get()
+                    .postDelayedTaskWithLocation(
+                            nativeTaskRunnerAndroid,
+                            delay,
+                            taskIndex,
+                            location.fileName,
+                            location.functionName,
+                            location.lineNumber);
+        } else {
+            TaskRunnerImplJni.get().postDelayedTask(nativeTaskRunnerAndroid, delay, taskIndex);
+        }
     }
 
     @CalledByNative
@@ -344,5 +356,13 @@ public class TaskRunnerImpl implements TaskRunner {
         void destroy(long nativeTaskRunnerAndroid);
 
         void postDelayedTask(long nativeTaskRunnerAndroid, long delay, int taskIndex);
+
+        void postDelayedTaskWithLocation(
+            long nativeTaskRunnerAndroid,
+            long delay,
+            int taskIndex,
+            String fileName,
+            String functionName,
+            int lineNumber);
     }
 }
