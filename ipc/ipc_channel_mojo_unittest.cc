698c5a304d38aa9c505c4f43c10ac085fa905a16
Tom Sepez <tsepez@chromium.org>
2025-10-07 13:33:22 -0700

    Remove IPC::SyncMessage
    There haven't been sync messages for some time, but now that
    dependencies have been cleaned up, the class can be removed.
    
    In turn, remove portions of IPC::SyncChannel, though still more
    can be removed in the future.
    
    Change-Id: I136de819d5d2628b64cd35f2dceafef782a89466
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6967048
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1526497}

--- ipc/ipc_channel_mojo_unittest.cc
+++ ipc/ipc_channel_mojo_unittest.cc
@@ -60,7 +60,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ipc/ipc_mojo_handle_attachment.h"
 #include "ipc/ipc_mojo_param_traits.h"
 #include "ipc/ipc_sync_channel.h"
-#include "ipc/ipc_sync_message.h"
 #include "ipc/ipc_test.test-mojom.h"
 #include "ipc/urgent_message_observer.h"
 #include "mojo/core/test/mojo_test_base.h"
