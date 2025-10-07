2dba855aa00668b411a805d91ca2c47bfe638090
moonira <moonira@google.com>
2025-10-07 02:24:34 -0700

    Add check for custom-medias in MediaQueryEvaluator::EvalFeature
    Since parsing of custom medias is now supported in @media query [0], it
    causes MediaQueryEvaluator::EvalStyleFeature() to be called for
    custom-media names inside media query, which causes crashes [1]. Add
    check to return `KleeneValue::kUnknown` for custom media expressions
    for now until later supported.
    
    This CL also introduces new `MediaQueryExp::Type` for custom media
    queries to distinguish them from `MediaQueryExp` with custom
    properties.
    
    [0] https://chromium-review.googlesource.com/c/chromium/src/+/6978670
    [1] https://issues.chromium.org/issues/448757864
    
    Fixed: 448757864
    Change-Id: Id7f6b57d125ca1cf8d8cc773580383f3af9f31de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7003011
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Munira Tursunova <moonira@google.com>
    Cr-Commit-Position: refs/heads/main@{#1526136}

--- third_party/blink/renderer/core/css/media_query_evaluator.cc
+++ third_party/blink/renderer/core/css/media_query_evaluator.cc
@@ -1762,6 +1762,13 @@ KleeneValue MediaQueryEvaluator::EvalFeature(
     return KleeneValue::kUnknown;
   }
 
+  if (RuntimeEnabledFeatures::CSSCustomMediaEnabled() &&
+      feature.IsCustomMedia() &&
+      CSSVariableParser::IsValidVariableName(feature.Name())) {
+    // TODO(crbug.com/40781325): Support evaluation of custom-media queries.
+    return KleeneValue::kUnknown;
+  }
+
   if (feature.HasStyleRange() ||
       CSSVariableParser::IsValidVariableName(feature.Name())) {
     return EvalStyleFeature(feature, result_flags);
--- third_party/blink/renderer/core/css/media_query_exp.h
+++ third_party/blink/renderer/core/css/media_query_exp.h
@@ -283,6 +283,7 @@ class CORE_EXPORT MediaQueryExp {
                               bool supports_element_dependent);
   static MediaQueryExp Create(const AtomicString& media_feature,
                               const MediaQueryExpBounds&);
+  static MediaQueryExp Create(const AtomicString& custom_media);
   static MediaQueryExp Create(const MediaQueryExpValue& reference_value,
                               const MediaQueryExpBounds&);
   static MediaQueryExp Invalid() { return MediaQueryExp(); }
@@ -294,9 +295,10 @@ class CORE_EXPORT MediaQueryExp {
   bool IsValid() const { return type_ != Type::kInvalid; }
   bool HasMediaFeature() const { return type_ == Type::kMediaFeature; }
   bool HasStyleRange() const { return type_ == Type::kStyleRange; }
+  bool IsCustomMedia() const { return type_ == Type::kCustomMedia; }
 
   const AtomicString& MediaFeature() const {
-    DCHECK(HasMediaFeature());
+    DCHECK(HasMediaFeature() || IsCustomMedia());
     return media_feature_;
   }
 
@@ -324,11 +326,13 @@ class CORE_EXPORT MediaQueryExp {
   unsigned GetUnitFlags() const;
 
  private:
-  enum class Type { kMediaFeature, kStyleRange, kInvalid };
+  enum class Type { kMediaFeature, kCustomMedia, kStyleRange, kInvalid };
 
   MediaQueryExp() = default;
   MediaQueryExp(const String& media_feature, const MediaQueryExpValue&);
-  MediaQueryExp(const String& media_feature, const MediaQueryExpBounds&);
+  MediaQueryExp(const String& media_feature,
+                const MediaQueryExpBounds&,
+                Type type);
   MediaQueryExp(const CSSUnparsedDeclarationValue& reference_value,
                 const MediaQueryExpBounds&);
 
@@ -394,7 +398,7 @@ class CORE_EXPORT MediaQueryFeatureExpNode : public MediaQueryExpNode {
   void Trace(Visitor*) const override;
 
   const String& Name() const {
-    DCHECK(HasMediaFeature());
+    DCHECK(HasMediaFeature() || IsCustomMedia());
     return exp_.MediaFeature();
   }
 
@@ -405,6 +409,7 @@ class CORE_EXPORT MediaQueryFeatureExpNode : public MediaQueryExpNode {
 
   bool HasMediaFeature() const { return exp_.HasMediaFeature(); }
   bool HasStyleRange() const { return exp_.HasStyleRange(); }
+  bool IsCustomMedia() const { return exp_.IsCustomMedia(); }
 
   const MediaQueryExpBounds& Bounds() const { return exp_.Bounds(); }
 
