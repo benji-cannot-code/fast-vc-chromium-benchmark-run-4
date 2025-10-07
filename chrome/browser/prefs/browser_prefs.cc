77936320995c4acaf3972ce42cc70cc3b09fb6ed
Benjamin Williams <bwwilliams@google.com>
2025-10-07 16:06:24 -0700

    Register Safety Check prefs in chrome/browser_prefs
    This change registers the Safety Check preferences in
    chrome/browser_prefs.cc.
    
    This is a fast-follow CL to crrev.com/c/7005785, which introduced the
    cross-device pref for the Safety Check Home Module but forgot to
    register the new prefs.
    
    Fixed: 448987228
    Change-Id: Id649988d80e84a30dc747286a4067658a0742592
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7016651
    Auto-Submit: Benjamin Williams <bwwilliams@google.com>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1526576}

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -167,6 +167,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/regional_capabilities/regional_capabilities_prefs.h"
 #include "components/safe_browsing/buildflags.h"
 #include "components/safe_browsing/core/common/safe_browsing_prefs.h"
+#include "components/safety_check/safety_check_prefs.h"
 #include "components/saved_tab_groups/public/pref_names.h"
 #include "components/search_engines/search_engine_choice/search_engine_choice_service.h"
 #include "components/search_engines/template_url_prepopulate_data.h"
@@ -2121,6 +2122,7 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry,
   safe_browsing::file_type::RegisterProfilePrefs(registry);
 #endif
   safe_browsing::RegisterProfilePrefs(registry);
+  safety_check::prefs::RegisterProfilePrefs(registry);
   SearchPrefetchService::RegisterProfilePrefs(registry);
   blocked_content::SafeBrowsingTriggeredPopupBlocker::RegisterProfilePrefs(
       registry);
