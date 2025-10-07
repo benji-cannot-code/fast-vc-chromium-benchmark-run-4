f22f5a2260685812d2689bd13fe7d11da959d71f
Dominik Röttsches <drott@chromium.org>
2025-10-07 09:24:18 -0700

    Add xml Rust crate to Chromium Rust crates
    Add the xml Rust cratex [1],[2] to Chromium as a future replacement for
    libxml2 SAX parsing. Based on a working proof-of-concept [3] this crate
    is the suitable replacement of choice. Written in safe Rust, and with a
    streaming parsing API that is suitable for integration into
    XMLDocumentParser.
    
    A discussion of this crate vs. quick-xml can be found in the (internal) doc [5].
    
    ATLs LGTM in: [4]
    
    [1] https://crates.io/crates/xml
    [2] https://github.com/kornelski/xml-rs
    [3] https://chromium-review.googlesource.com/c/chromium/src/+/6828846
    [4] https://groups.google.com/a/google.com/g/chrome-atls-discuss/c/ISFumR4PPeA/m/bNrxP3RQAgAJ
    [5] http://go/chrome_x_mitigation
    
    Bug: 441911594
    Change-Id: I1c7a25756be5b016b314d3dba790c75c41338428
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6959529
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1526334}

---
+++ third_party/rust/chromium_crates_io/vendor/xml-v1/src/reader/parser/inside_cdata.rs
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+use crate::common::is_whitespace_char;
+use crate::reader::error::SyntaxError;
+use crate::reader::events::XmlEvent;
+use crate::reader::lexer::Token;
+
+use super::{PullParser, Result, State};
+
+impl PullParser {
+    pub fn inside_cdata(&mut self, t: Token) -> Option<Result> {
+        match t {
+            Token::CDataEnd => {
+                let event = if self.config.cdata_to_characters {
+                    // start called push_pos, but there will be no event to pop it
+                    if self.buf.is_empty() {
+                        self.next_pos();
+                    }
+                    None
+                } else {
+                    let data = self.take_buf();
+                    Some(Ok(XmlEvent::CData(data)))
+                };
+                self.into_state(State::OutsideTag, event)
+            },
+
+            Token::Character(c) if !self.is_valid_xml_char(c) => {
+                Some(self.error(SyntaxError::InvalidCharacterEntity(c as u32)))
+            },
+            Token::Character(c) => {
+                if !is_whitespace_char(c) {
+                    self.inside_whitespace = false;
+                }
+                self.buf.push(c);
+                None
+            },
+            _ => {
+                debug_assert!(false, "unreachable");
+                None
+            },
+        }
+    }
+}
