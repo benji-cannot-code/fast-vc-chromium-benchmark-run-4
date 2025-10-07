824e8faa94b269b4a826635f0c0e8989391bc21c
Ibrahim Kanouche <kanouche@google.com>
2025-10-07 07:32:10 -0700

    [iOS] Implement handleFeatureToggle functionality for Permission Features.
    Adds updateFeatureRowsAvailability and revokePermission. Feature rows
    now appear when permissions are granted and allow users to revoke
    permissions via toggle switches. UI automatically refreshes when
    permissions change.
    
    Bug: 447649727
    Change-Id: I61047d9eb800300c49e5b157710508513a58a3ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7002510
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Auto-Submit: Ibrahim Kanouche <kanouche@google.com>
    Commit-Queue: Ibrahim Kanouche <kanouche@google.com>
    Cr-Commit-Position: refs/heads/main@{#1526267}

--- ios/chrome/browser/intelligence/page_action_menu/ui/page_action_menu_mutator.h
+++ ios/chrome/browser/intelligence/page_action_menu/ui/page_action_menu_mutator.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
+@class PageActionMenuFeature;
+
 // Page Menu Action Feature types.
 typedef NS_ENUM(NSInteger, PageActionMenuFeatureType);
 
@@ -37,6 +39,12 @@ typedef NS_ENUM(NSInteger, PageActionMenuFeatureType);
 
 // Returns the current site domain for permission context.
 - (NSString*)currentSiteDomain;
+
+// Revokes the specified permission for the current site.
+- (void)revokePermission:(PageActionMenuFeatureType)featureType;
+
+// Returns array of currently active features to display.
+- (NSArray<PageActionMenuFeature*>*)activeFeatures;
 
 @end
 
