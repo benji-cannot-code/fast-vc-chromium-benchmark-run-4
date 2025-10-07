3e16df9a38f153d31e8337c500f11563c401a372
Sam Davis Omekara Jr <samomekarajr@microsoft.com>
2025-10-07 12:10:14 -0700

    [Gap Decorations]: Remove GapIntersection code from GapGeometry
    This CL removes the `GapIntersection` code from the `GapGeometry` class.
    The move to MainxCross model has been completed hence it's safe to
    remove the code related to previous Gap Intersection model.
    
    Bug: 357648037, 440123087, 436140061
    Change-Id: I7bf291d81219cfa6e7022397ce067f04db97fce6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6973224
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Commit-Queue: Sam Davis Omekara <samomekarajr@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1526454}

--- third_party/blink/renderer/core/layout/physical_box_fragment.cc
+++ third_party/blink/renderer/core/layout/physical_box_fragment.cc
@@ -1095,7 +1095,7 @@ PhysicalRect PhysicalBoxFragment::ComputeSelfInkOverflow() const {
         LayoutUnit(MaxGapDecorationsWidth(style.ColumnRuleWidth()));
     LayoutUnit block_thickness =
         LayoutUnit(MaxGapDecorationsWidth(style.RowRuleWidth()));
-    PhysicalRect rect = gap_geometry->ComputeInkOverflowForGapsOptimized(
+    PhysicalRect rect = gap_geometry->ComputeInkOverflowForGaps(
         Style().GetWritingDirection(), Size(), inline_thickness,
         block_thickness);
     ink_overflow.Unite(rect);
