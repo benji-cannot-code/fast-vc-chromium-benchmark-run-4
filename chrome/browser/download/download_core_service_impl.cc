914a9f18167613365176e4de1a19a32b501e8d85
James Cook <jamescook@chromium.org>
2025-10-07 10:13:43 -0700

    extensions: Compile downloads API code on desktop Android
    This is the first step towards making the downloads API work on desktop
    Android. The core downloads_api.h/cc is added to the build. A few
    instances of ENABLE_EXTENSIONS are replaced with ENABLE_EXTENSIONS_CORE
    in the chrome downloads source. A couple of dialogs that only have
    toolkit views implementations are ifdef'd out, as they will need
    Android-specific implementations.
    
    Bug: 405219117
    Change-Id: I843f400a43b07f54e30f9e168ab7da1683fe6477
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7014040
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: Zhengzheng Liu <zhzhliu@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1526382}

--- chrome/browser/download/download_core_service_impl.cc
+++ chrome/browser/download/download_core_service_impl.cc
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/download_manager.h"
 #include "extensions/buildflags/buildflags.h"
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
+#if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 #include "chrome/browser/extensions/api/downloads/downloads_api.h"
 #endif
 
@@ -63,7 +63,7 @@ DownloadCoreServiceImpl::GetDownloadManagerDelegate() {
 
   manager_delegate_->SetDownloadManager(manager);
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
+#if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
   extension_event_router_ =
       std::make_unique<extensions::ExtensionDownloadsEventRouter>(profile_,
                                                                   manager);
@@ -103,7 +103,7 @@ DownloadHistory* DownloadCoreServiceImpl::GetDownloadHistory() {
   return download_history_.get();
 }
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
+#if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 extensions::ExtensionDownloadsEventRouter*
 DownloadCoreServiceImpl::GetExtensionEventRouter() {
   return extension_event_router_.get();
@@ -173,7 +173,7 @@ void DownloadCoreServiceImpl::Shutdown() {
     // manually earlier. See http://crbug.com/131692
     profile_->GetDownloadManager()->Shutdown();
   }
-#if BUILDFLAG(ENABLE_EXTENSIONS)
+#if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
   extension_event_router_.reset();
 #endif
   manager_delegate_.reset();
