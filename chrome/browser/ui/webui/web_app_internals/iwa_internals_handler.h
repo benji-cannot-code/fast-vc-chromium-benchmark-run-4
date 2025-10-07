26e618c427f1a7f7eb004d007c966bb7992631bb
Bhaskar Sharma <bhaskarsharma@google.com>
2025-10-07 09:07:18 -0700

    Add 'Delete IWA' button on chrome://web-app-internals
    'Delete IWA' button has been added beside 'Perform update now' button.
    It opens a confirmation dialog which when approved deletes the IWA using
    existing logic. A browser test has been added to
    web_app_internals_browsertest.c to ensure the DeleteIsolatedWebApp
    handler method successfully uninstalls the IWA.
    
    Feature: https://b.corp.google.com/issues/433462563
    
    Low-Coverage-Reason: HARD_TO_TEST
    
    Change-Id: I91cb9254ca380bd5460b80272f4304c7243bbd7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6958952
    Commit-Queue: Bhaskar Sharma <bhaskarsharma@google.com>
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Robert Ferens <rferens@google.com>
    Cr-Commit-Position: refs/heads/main@{#1526325}

--- chrome/browser/ui/webui/web_app_internals/iwa_internals_handler.h
+++ chrome/browser/ui/webui/web_app_internals/iwa_internals_handler.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/types/expected.h"
 #include "base/types/optional_ref.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/webui/web_app_internals/web_app_internals.mojom.h"
 #include "components/webapps/common/web_app_id.h"
 #include "components/webapps/isolated_web_apps/types/iwa_version.h"
@@ -90,6 +91,9 @@ class IwaInternalsHandler {
   void SetAllowDowngradesForIsolatedWebApp(bool allow_downgrades,
                                            const webapps::AppId& app_id);
 
+  void DeleteIsolatedWebApp(const webapps::AppId& app_id,
+                            Handler::DeleteIsolatedWebAppCallback callback);
+
  private:
   class IsolatedWebAppDevBundleSelectListener;
   class IwaManifestInstallUpdateHandler;
@@ -124,6 +128,8 @@ class IwaInternalsHandler {
       const webapps::AppId& app_id,
       base::optional_ref<const web_app::IwaSourceDevModeWithFileOp> location,
       base::OnceCallback<void(const std::string&)> callback);
+
+  gfx::NativeWindow GetHostingNativeWindow();
 
   const raw_ref<content::WebUI> web_ui_;
   const raw_ref<Profile> profile_;
