31e82808eb406f9f3b8ec029b355dfb5885729eb
Saifuddin Hitawala <hitawala@chromium.org>
2025-10-07 11:19:48 -0700

    [content] Add UMA for if RFHI::Unload happens with ViewTransitions
    For cross-document view transitions, we plan to defer Unload until
    view transition and thus copy output requests are completed.
    
    This change adds an UMA to track how common it is for a page that
    needs cross-document transition to need Unload.
    
    Bug: 420648512
    Change-Id: Ibf9118e18d12435774770e452ece4ed489d70eae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7001259
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1526422}

--- content/browser/renderer_host/navigation_request.h
+++ content/browser/renderer_host/navigation_request.h
@@ -1701,6 +1701,12 @@ class CONTENT_EXPORT NavigationRequest
   // value will be returned.
   PrerenderHostId GetPrerenderHostId() const;
 
+  // Checks whether the navigation request contains active view transition
+  // resources.
+  bool HasViewTransitionResources() const {
+    return !!view_transition_resources_;
+  }
+
  private:
   friend class NavigationRequestTest;
   FRIEND_TEST_ALL_PREFIXES(NavigationRequestTest, SanitizeRedirectsForCommit);
