495c87dbb60cb884637fac6bf3d20d5515dcafcf
Ivan Šandrk <isandrk@chromium.org>
2025-10-07 03:52:10 -0700

    Revert "[Browser Layout] Switch to new frame layout API by default"
    This reverts commit d031210359bc83e34633a9ec8ebf8fbd44687b69.
    
    Reason for revert: Broke some tests, example failures https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/54040/overview
    
    Original change's description:
    > [Browser Layout] Switch to new frame layout API by default
    >
    > This CL:
    >  - Makes the "use new layout" feature into an always-on killswitch
    >  - Renames BrowserViewLayoutDelegateImplNew to
    >    BrowserViewLayoutDelegateImpl
    >  - Removes references to the feature flag elsewhere
    >
    > The effect will be that in addition to the flag being on for all CQ
    > testbots and ToT builds, it will also be on for Canary, Dev, and CI
    > bots.
    >
    > This should allow us to collect error reports from more users and
    > determine if there are any issues with the new logic.
    >
    > Bug: 443123625
    > Change-Id: Id2d7f6818ed1aba1ed8842b24275e0e7dfe0488e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7011507
    > Commit-Queue: Dana Fried <dfried@chromium.org>
    > Reviewed-by: Darryl James <dljames@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1525685}
    
    Bug: 443123625
    Change-Id: Ic93ddf8fb9b49299ed8504ef4b75163db23e0d99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7016627
    Auto-Submit: Ivan Šandrk <isandrk@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Ivan Šandrk <isandrk@chromium.org>
    Commit-Queue: Ivan Šandrk <isandrk@chromium.org>
    Owners-Override: Ivan Šandrk <isandrk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1526174}

--- chrome/browser/ui/views/frame/browser_view_layout_delegate_impl.cc
+++ chrome/browser/ui/views/frame/browser_view_layout_delegate_impl.cc
@@ -21,19 +21,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/rect.h"
 #include "ui/views/view.h"
 
-namespace {
-
-// Feature that manages the transition between old and current browser layout
-// delegate. This feature is on by default and provided only as a killswitch.
-BASE_FEATURE(kDesktopNewTopAreaLayoutFeature, base::FEATURE_ENABLED_BY_DEFAULT);
-
-}  // namespace
-
 // static
 std::unique_ptr<BrowserViewLayoutDelegate>
 BrowserViewLayoutDelegateImplBase::CreateDelegate(BrowserView& browser_view) {
-  if (base::FeatureList::IsEnabled(kDesktopNewTopAreaLayoutFeature)) {
-    return std::make_unique<BrowserViewLayoutDelegateImpl>(browser_view);
+  if (base::FeatureList::IsEnabled(features::kDesktopNewTopAreaLayoutFeature)) {
+    return std::make_unique<BrowserViewLayoutDelegateImplNew>(browser_view);
   }
   return std::make_unique<BrowserViewLayoutDelegateImplOld>(browser_view);
 }
@@ -234,13 +226,14 @@ BrowserViewLayoutDelegateImplOld::GetBoundsForWebAppFrameToolbarInBrowserView()
   return gfx::ToEnclosingRect(bounds_f);
 }
 
-BrowserViewLayoutDelegateImpl::BrowserViewLayoutDelegateImpl(
+BrowserViewLayoutDelegateImplNew::BrowserViewLayoutDelegateImplNew(
     BrowserView& browser_view)
     : BrowserViewLayoutDelegateImplBase(browser_view) {}
-BrowserViewLayoutDelegateImpl::~BrowserViewLayoutDelegateImpl() = default;
+BrowserViewLayoutDelegateImplNew::~BrowserViewLayoutDelegateImplNew() = default;
 
 gfx::Rect
-BrowserViewLayoutDelegateImpl::GetBoundsForTabStripRegionInBrowserView() const {
+BrowserViewLayoutDelegateImplNew::GetBoundsForTabStripRegionInBrowserView()
+    const {
   const gfx::Size tabstrip_minimum_size =
       browser_view().tab_strip_view()->GetMinimumSize();
   const auto layout = GetFrameView()->GetBrowserLayoutParams();
@@ -268,7 +261,7 @@ BrowserViewLayoutDelegateImpl::GetBoundsForTabStripRegionInBrowserView() const {
 }
 
 gfx::Rect
-BrowserViewLayoutDelegateImpl::GetBoundsForToolbarInVerticalTabBrowserView()
+BrowserViewLayoutDelegateImplNew::GetBoundsForToolbarInVerticalTabBrowserView()
     const {
   const gfx::Size toolbar_preferred_size =
       browser_view().toolbar()->GetPreferredSize();
@@ -294,7 +287,7 @@ BrowserViewLayoutDelegateImpl::GetBoundsForToolbarInVerticalTabBrowserView()
 }
 
 gfx::Rect
-BrowserViewLayoutDelegateImpl::GetBoundsForWebAppFrameToolbarInBrowserView()
+BrowserViewLayoutDelegateImplNew::GetBoundsForWebAppFrameToolbarInBrowserView()
     const {
   if (!GetFrameView()->ShouldShowWebAppFrameToolbar()) {
     return gfx::Rect();
--- chrome/browser/ui/views/frame/browser_view_layout_delegate_impl_browsertest.cc
+++ chrome/browser/ui/views/frame/browser_view_layout_delegate_impl_browsertest.cc
@@ -158,7 +158,7 @@ class BrowserViewLayoutDelegateImplBrowsertest
     auto* const layout = browser_view->GetBrowserViewLayout();
     if (use_new_delegate) {
       layout->SetDelegateForTesting(
-          std::make_unique<BrowserViewLayoutDelegateImpl>(*browser_view));
+          std::make_unique<BrowserViewLayoutDelegateImplNew>(*browser_view));
     } else {
       layout->SetDelegateForTesting(
           std::make_unique<BrowserViewLayoutDelegateImplOld>(*browser_view));
