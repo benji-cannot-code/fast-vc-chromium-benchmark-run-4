8e24075f1aa1644fb77c69599f2c2086abbccbf4
Nicola Tommasi <tommasin@chromium.org>
2025-10-07 00:08:11 -0700

    [AI-Fixit][Settings] Merge site_settings_page/ into site_settings/
    In order to improve WebUI privacy_page subfolder organization we're
    merging site_settings_page/ into site_settings/
    
    Bypass-Check-License: Files moved
    Bug: 441410654
    Change-Id: If595581c0c72dce8f9a43b1b936e7b8ba9cb9a93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7002672
    Commit-Queue: Nicola Tommasi <tommasin@chromium.org>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1526074}

--- chrome/browser/resources/settings/site_settings/constants.ts
+++ chrome/browser/resources/settings/site_settings/constants.ts
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * individual permissions under Site Details should appear here.
  * This should be kept in sync with the |kContentSettingsTypeGroupNames| array
  * in chrome/browser/ui/webui/settings/site_settings_helper.cc. See
- * chrome/browser/resources/settings/site_settings_page/site_settings_page_util
+ * chrome/browser/resources/settings/site_settings/site_settings_page_util
  * for translations.
  */
 
@@ -209,7 +209,7 @@ export const SITE_EXCEPTION_WILDCARD: string = '*';
 
 /**
  * Corresponds to the animation-duration CSS parameter defined in
- * chrome/browser/resources/settings/site_settings_page/site_review_shared.css.
+ * chrome/browser/resources/settings/site_settings/site_review_shared.css.
  * Set to be slightly higher, as we want to ensure that the animation is
  * finished before updating the model for the right visual effect.
  */
