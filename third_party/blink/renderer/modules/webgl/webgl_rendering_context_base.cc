c48dcda758396ed059e31095a0126282428fc861
Colin Blundell <blundell@chromium.org>
2025-10-07 00:04:29 -0700

    [Blink] Move up size check in WebGLRCBase
    Put the short-circuit out for this next to the place where the size is
    obtained. No functional change, just cleanup.
    
    Bug: 352263194
    Change-Id: Iea353a5abb634d1c56fe36ef92b324355a1c3bab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7006506
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1526068}

--- third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
+++ third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
@@ -1855,6 +1855,13 @@ bool WebGLRenderingContextBase::
   }
 
   auto size = Host()->Size();
+  const auto& capabilities =
+      context_provider_wrapper->ContextProvider().GetCapabilities();
+  if (size.width() > capabilities.max_texture_size ||
+      size.height() > capabilities.max_texture_size) {
+    return false;
+  }
+
   auto format = GetSharedImageFormat();
 
   bool using_webgl_image_chromium =
@@ -1864,13 +1871,6 @@ bool WebGLRenderingContextBase::
   bool using_swap_chain =
       GetDrawingBuffer() && GetDrawingBuffer()->UsingSwapChain();
   if (!using_swap_chain && !using_webgl_image_chromium) {
-    return false;
-  }
-
-  const auto& capabilities =
-      context_provider_wrapper->ContextProvider().GetCapabilities();
-  if (size.width() > capabilities.max_texture_size ||
-      size.height() > capabilities.max_texture_size) {
     return false;
   }
 
