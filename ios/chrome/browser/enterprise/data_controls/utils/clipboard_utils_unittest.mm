0a9d9b23aba37fe4933cba21102eb9bee1c1e60c
Xu Li <mxlg@google.com>
2025-10-07 09:33:46 -0700

    [iOS] Implement IsPasteAllowedByPolicy for data_controls.
    Bug: 438202190
    Change-Id: I8eba299aaeb65c40c9e78c9b491b37b9cbbcf4b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7013727
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Xu Li <mxlg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1526344}

--- ios/chrome/browser/enterprise/data_controls/utils/clipboard_utils_unittest.mm
+++ ios/chrome/browser/enterprise/data_controls/utils/clipboard_utils_unittest.mm
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "testing/gmock/include/gmock/gmock.h"
 #import "testing/gtest/include/gtest/gtest.h"
 #import "testing/platform_test.h"
+#import "ui/base/clipboard/clipboard_metadata.h"
 #import "url/gurl.h"
 
 using ::testing::_;
@@ -61,6 +62,13 @@ class ClipboardUtilsTest : public PlatformTest {
     profile_ = std::move(builder).Build();
     rules_service_ = static_cast<MockIOSRulesService*>(
         IOSRulesServiceFactory::GetForProfile(profile_.get()));
+
+    TestProfileIOS::Builder builder2;
+    builder2.AddTestingFactory(IOSRulesServiceFactory::GetInstance(),
+                               base::BindOnce(&BuildMockIOSRulesService));
+    profile2_ = std::move(builder2).Build();
+    rules_service2_ = static_cast<MockIOSRulesService*>(
+        IOSRulesServiceFactory::GetForProfile(profile2_.get()));
   }
 
  protected:
@@ -68,6 +76,8 @@ class ClipboardUtilsTest : public PlatformTest {
   IOSChromeScopedTestingLocalState scoped_testing_local_state_;
   std::unique_ptr<TestProfileIOS> profile_;
   raw_ptr<MockIOSRulesService> rules_service_;
+  std::unique_ptr<TestProfileIOS> profile2_;
+  raw_ptr<MockIOSRulesService> rules_service2_;
 };
 
 TEST_F(ClipboardUtilsTest, IsPasteAllowedByPolicy_Allow) {
@@ -77,9 +87,11 @@ TEST_F(ClipboardUtilsTest, IsPasteAllowedByPolicy_Allow) {
                                                nullptr, profile_.get()))
       .WillOnce(::testing::Return(Verdict::Allow()));
 
-  PastePolicyVerdict verdict = IsPasteAllowedByPolicy(
-      source_url, destination_url, {}, nullptr, profile_.get());
+  PastePolicyVerdict verdict =
+      IsPasteAllowedByPolicy(source_url, destination_url,
+                             ui::ClipboardMetadata(), nullptr, profile_.get());
   EXPECT_EQ(verdict.verdict.level(), Rule::Level::kAllow);
+  EXPECT_FALSE(verdict.dialog_triggered_by_source);
 }
 
 TEST_F(ClipboardUtilsTest, IsPasteAllowedByPolicy_Warn) {
@@ -90,8 +102,10 @@ TEST_F(ClipboardUtilsTest, IsPasteAllowedByPolicy_Warn) {
       .WillOnce(::testing::Return(Verdict::Warn({})));
 
   PastePolicyVerdict verdict = IsPasteAllowedByPolicy(
-      source_url, destination_url, {}, profile_.get(), profile_.get());
+      source_url, destination_url, ui::ClipboardMetadata(), profile_.get(),
+      profile_.get());
   EXPECT_EQ(verdict.verdict.level(), Rule::Level::kWarn);
+  EXPECT_FALSE(verdict.dialog_triggered_by_source);
 }
 
 TEST_F(ClipboardUtilsTest, IsPasteAllowedByPolicy_Block) {
@@ -102,8 +116,82 @@ TEST_F(ClipboardUtilsTest, IsPasteAllowedByPolicy_Block) {
       .WillOnce(::testing::Return(Verdict::Block({})));
 
   PastePolicyVerdict verdict = IsPasteAllowedByPolicy(
-      source_url, destination_url, {}, profile_.get(), profile_.get());
+      source_url, destination_url, ui::ClipboardMetadata(), profile_.get(),
+      profile_.get());
   EXPECT_EQ(verdict.verdict.level(), Rule::Level::kBlock);
+  EXPECT_FALSE(verdict.dialog_triggered_by_source);
+}
+
+TEST_F(ClipboardUtilsTest,
+       IsPasteAllowedByPolicy_DifferentProfiles_SourceWarns) {
+  GURL source_url(kSourceUrl);
+  GURL destination_url(kDestinationUrl);
+  EXPECT_CALL(*rules_service_,
+              GetPasteVerdict(source_url, destination_url, _, _))
+      .WillOnce(::testing::Return(Verdict::Warn({})));
+  EXPECT_CALL(*rules_service2_,
+              GetPasteVerdict(source_url, destination_url, _, _))
+      .WillOnce(::testing::Return(Verdict::NotSet()));
+
+  PastePolicyVerdict verdict = IsPasteAllowedByPolicy(
+      source_url, destination_url, ui::ClipboardMetadata(), profile_.get(),
+      profile2_.get());
+  EXPECT_EQ(verdict.verdict.level(), Rule::Level::kWarn);
+  EXPECT_TRUE(verdict.dialog_triggered_by_source);
+}
+
+TEST_F(ClipboardUtilsTest,
+       IsPasteAllowedByPolicy_DifferentProfiles_DestinationWarns) {
+  GURL source_url(kSourceUrl);
+  GURL destination_url(kDestinationUrl);
+  EXPECT_CALL(*rules_service_,
+              GetPasteVerdict(source_url, destination_url, _, _))
+      .WillOnce(::testing::Return(Verdict::NotSet()));
+  EXPECT_CALL(*rules_service2_,
+              GetPasteVerdict(source_url, destination_url, _, _))
+      .WillOnce(::testing::Return(Verdict::Warn({})));
+
+  PastePolicyVerdict verdict = IsPasteAllowedByPolicy(
+      source_url, destination_url, ui::ClipboardMetadata(), profile_.get(),
+      profile2_.get());
+  EXPECT_EQ(verdict.verdict.level(), Rule::Level::kWarn);
+  EXPECT_FALSE(verdict.dialog_triggered_by_source);
+}
+
+TEST_F(ClipboardUtilsTest,
+       IsPasteAllowedByPolicy_DifferentProfiles_SourceBlocks) {
+  GURL source_url(kSourceUrl);
+  GURL destination_url(kDestinationUrl);
+  EXPECT_CALL(*rules_service_,
+              GetPasteVerdict(source_url, destination_url, _, _))
+      .WillOnce(::testing::Return(Verdict::Block({})));
+  EXPECT_CALL(*rules_service2_,
+              GetPasteVerdict(source_url, destination_url, _, _))
+      .WillOnce(::testing::Return(Verdict::NotSet()));
+
+  PastePolicyVerdict verdict = IsPasteAllowedByPolicy(
+      source_url, destination_url, ui::ClipboardMetadata(), profile_.get(),
+      profile2_.get());
+  EXPECT_EQ(verdict.verdict.level(), Rule::Level::kBlock);
+  EXPECT_TRUE(verdict.dialog_triggered_by_source);
+}
+
+TEST_F(ClipboardUtilsTest,
+       IsPasteAllowedByPolicy_DifferentProfiles_DestinationBlocks) {
+  GURL source_url(kSourceUrl);
+  GURL destination_url(kDestinationUrl);
+  EXPECT_CALL(*rules_service_,
+              GetPasteVerdict(source_url, destination_url, _, _))
+      .WillOnce(::testing::Return(Verdict::NotSet()));
+  EXPECT_CALL(*rules_service2_,
+              GetPasteVerdict(source_url, destination_url, _, _))
+      .WillOnce(::testing::Return(Verdict::Block({})));
+
+  PastePolicyVerdict verdict = IsPasteAllowedByPolicy(
+      source_url, destination_url, ui::ClipboardMetadata(), profile_.get(),
+      profile2_.get());
+  EXPECT_EQ(verdict.verdict.level(), Rule::Level::kBlock);
+  EXPECT_FALSE(verdict.dialog_triggered_by_source);
 }
 
 TEST_F(ClipboardUtilsTest, IsCopyAllowedByPolicy_Allow) {
@@ -113,8 +201,8 @@ TEST_F(ClipboardUtilsTest, IsCopyAllowedByPolicy_Allow) {
   EXPECT_CALL(*rules_service_, GetCopyToOSClipboardVerdict(source_url))
       .WillOnce(::testing::Return(Verdict::Allow()));
 
-  CopyPolicyVerdicts verdicts =
-      IsCopyAllowedByPolicy(source_url, {}, profile_.get());
+  CopyPolicyVerdicts verdicts = IsCopyAllowedByPolicy(
+      source_url, ui::ClipboardMetadata(), profile_.get());
   EXPECT_EQ(verdicts.copy_action_verdict.level(), Rule::Level::kAllow);
   EXPECT_TRUE(verdicts.copy_to_os_clipbord);
 }
@@ -124,8 +212,8 @@ TEST_F(ClipboardUtilsTest, IsCopyAllowedByPolicy_SourceBlocked) {
   EXPECT_CALL(*rules_service_, GetCopyRestrictedBySourceVerdict(source_url))
       .WillOnce(::testing::Return(Verdict::Block({})));
 
-  CopyPolicyVerdicts verdicts =
-      IsCopyAllowedByPolicy(source_url, {}, profile_.get());
+  CopyPolicyVerdicts verdicts = IsCopyAllowedByPolicy(
+      source_url, ui::ClipboardMetadata(), profile_.get());
   EXPECT_EQ(verdicts.copy_action_verdict.level(), Rule::Level::kBlock);
   EXPECT_FALSE(verdicts.copy_to_os_clipbord);
 }
@@ -137,8 +225,8 @@ TEST_F(ClipboardUtilsTest, IsCopyAllowedByPolicy_OSBlocked) {
   EXPECT_CALL(*rules_service_, GetCopyToOSClipboardVerdict(source_url))
       .WillOnce(::testing::Return(Verdict::Block({})));
 
-  CopyPolicyVerdicts verdicts =
-      IsCopyAllowedByPolicy(source_url, {}, profile_.get());
+  CopyPolicyVerdicts verdicts = IsCopyAllowedByPolicy(
+      source_url, ui::ClipboardMetadata(), profile_.get());
   EXPECT_EQ(verdicts.copy_action_verdict.level(), Rule::Level::kAllow);
   EXPECT_FALSE(verdicts.copy_to_os_clipbord);
 }
@@ -150,8 +238,8 @@ TEST_F(ClipboardUtilsTest, IsCopyAllowedByPolicy_Warn) {
   EXPECT_CALL(*rules_service_, GetCopyToOSClipboardVerdict(source_url))
       .WillOnce(::testing::Return(Verdict::Allow()));
 
-  CopyPolicyVerdicts verdicts =
-      IsCopyAllowedByPolicy(source_url, {}, profile_.get());
+  CopyPolicyVerdicts verdicts = IsCopyAllowedByPolicy(
+      source_url, ui::ClipboardMetadata(), profile_.get());
   EXPECT_EQ(verdicts.copy_action_verdict.level(), Rule::Level::kWarn);
   EXPECT_TRUE(verdicts.copy_to_os_clipbord);
 }
@@ -163,8 +251,8 @@ TEST_F(ClipboardUtilsTest, IsCopyAllowedByPolicy_WarnAndOSWarn) {
   EXPECT_CALL(*rules_service_, GetCopyToOSClipboardVerdict(source_url))
       .WillOnce(::testing::Return(Verdict::Warn({})));
 
-  CopyPolicyVerdicts verdicts =
-      IsCopyAllowedByPolicy(source_url, {}, profile_.get());
+  CopyPolicyVerdicts verdicts = IsCopyAllowedByPolicy(
+      source_url, ui::ClipboardMetadata(), profile_.get());
   EXPECT_EQ(verdicts.copy_action_verdict.level(), Rule::Level::kWarn);
   EXPECT_TRUE(verdicts.copy_to_os_clipbord);
 }
