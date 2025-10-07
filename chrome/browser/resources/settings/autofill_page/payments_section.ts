020cec6741ea572307c8dd35ecdc55355e311cd6
Stanislav Mikheyev <mikhe@google.com>
2025-10-07 05:54:51 -0700

    [Settings/Autofill] Add isYourSavedInfoSubpage prop to payments page
    During 'Your saved info' page UI deck reuse of the existing payments
    page was approved with several small changes. This CL implements them:
    
    * Remove "shadow" around the page
    * Change title to reflect the new category name
    * Add containers around the content of the page
    
    Bug: 438667361
    Change-Id: I9b1bafaf86f766a659572a58e12dc951f3548d19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7000230
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Stanislav Mikheyev <mikhe@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1526224}

--- chrome/browser/resources/settings/autofill_page/payments_section.ts
+++ chrome/browser/resources/settings/autofill_page/payments_section.ts
@@ -22,6 +22,7 @@ import '../simple_confirmation_dialog.js';
 import './passwords_shared.css.js';
 import './payments_list.js';
 import './virtual_card_unenroll_dialog.js';
+import './your_saved_info_shared.css.js';
 
 import type {CrActionMenuElement} from 'chrome://resources/cr_elements/cr_action_menu/cr_action_menu.js';
 import {AnchorAlignment} from 'chrome://resources/cr_elements/cr_action_menu/cr_action_menu.js';
@@ -61,6 +62,10 @@ declare global {
     'remote-card-menu-click': RemoteCardMenuClickEvent;
   }
 }
+
+// TODO(crbug.com/447113309): This file along with all of its dependencies
+// should be moved to .../settings/your_saved_info_page directory after
+// full release of the `Your Saved Info` page.
 
 export interface SettingsPaymentsSectionElement {
   $: {
@@ -211,6 +216,16 @@ export class SettingsPaymentsSectionElement extends
           return loadTimeData.getString('autofillPayOverTimeSettingsSublabel');
         },
       },
+
+      /**
+       * Indicates if this element is used as a Your saved info subpage. Causes
+       * slight adjustments like different title, no page shadow, cards being
+       * visible.
+       */
+      isYourSavedInfoSubpage_: {
+        type: Boolean,
+        value: () => loadTimeData.getBoolean('enableYourSavedInfoSettingsPage'),
+      },
     };
   }
 
@@ -239,6 +254,7 @@ export class SettingsPaymentsSectionElement extends
   declare private cardBenefitsSublabel_: string;
   declare private shouldShowPayOverTimeSettings_: boolean;
   declare private payOverTimeSublabel_: string;
+  declare private isYourSavedInfoSubpage_: boolean;
 
   override connectedCallback() {
     super.connectedCallback();
@@ -304,6 +320,15 @@ export class SettingsPaymentsSectionElement extends
     this.paymentsManager_.removePersonalDataManagerListener(
         this.setPersonalDataListener_);
     this.setPersonalDataListener_ = null;
+  }
+
+  private getMultiCardClass_(): string {
+    return this.isYourSavedInfoSubpage_ ? 'multi-card' : '';
+  }
+
+  private getPageTitleLabel_(): string {
+    return this.i18n(
+      this.isYourSavedInfoSubpage_ ? 'paymentsTitle' : 'creditCards');
   }
 
   private setCreditCards_(cardList: chrome.autofillPrivate.CreditCardEntry[]) {
