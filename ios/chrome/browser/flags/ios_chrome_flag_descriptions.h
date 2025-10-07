46ab33141b6e1c1a5c7040c930703077ac16c5ad
Louis Romero <lpromero@google.com>
2025-10-07 05:09:18 -0700

    Remove enable-disco-feed-endpoint
    This is launched and enabled.
    This should not land before crrev.com/i/8634700
    
    Fixed: 427494095
    Change-Id: I7ea2f3b91da641e6f1fd0e3ae437c7edd1f7ba35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7006494
    Auto-Submit: Louis Romero <lpromero@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1526207}

--- ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
+++ ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
@@ -321,9 +321,6 @@ extern const char kEnableAutofillAddressSavePromptDescription[];
 extern const char kEnableASWebAuthenticationSessionName[];
 extern const char kEnableASWebAuthenticationSessionDescription[];
 
-extern const char kEnableDiscoverFeedDiscoFeedEndpointName[];
-extern const char kEnableDiscoverFeedDiscoFeedEndpointDescription[];
-
 extern const char kEnableFeedAblationName[];
 extern const char kEnableFeedAblationDescription[];
 
--- ios/chrome/browser/ntp/ui_bundled/new_tab_page_feature.mm
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_feature.mm
@@ -30,10 +30,6 @@ const char kDeprecateFeedHeaderParameterHeaderBottomPadding[] =
 
 #pragma mark - Feature declarations
 
-BASE_FEATURE(kEnableDiscoverFeedDiscoFeedEndpoint,
-             "EnableDiscoFeedEndpoint",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 BASE_FEATURE(kEnableNTPViewHierarchyRepair,
              "NTPViewHierarchyRepair",
              base::FEATURE_ENABLED_BY_DEFAULT);
