dfead73f950072daa0ce5d98f06222660ec0dcae
Xiaohan Wang <xhwang@chromium.org>
2025-10-06 18:47:03 -0700

    misc: Migrate BASE_FEATURE macros to 2-argument version
    This CL migrates three-argument BASE_FEATURE macros in misc folders to
    the new two-argument version where the C++ identifier and the string
    feature name match.
    
    This automated refactoring helps ensure that the C++ identifier and the
    string feature name always match, reducing potential errors.
    
    Enabled-by-default-reason: refactor
    NO_IFTTT=refactor
    
    Bug: 436274260
    Change-Id: I6e6ad3be36980273b1dd8b53bc6a40012a4d5b9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7009146
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1525973}

--- extensions/common/extension_features.cc
+++ extensions/common/extension_features.cc
@@ -156,7 +156,6 @@ BASE_FEATURE(kDebuggerAPIRestrictedToDevMode,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kExtensionBrowserNamespaceAlternative,
-             "ExtensionBrowserNamespaceAlternative",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kOptimizeServiceWorkerStartRequests,
@@ -169,7 +168,6 @@ BASE_FEATURE(kContentVerifyJobUseJobVersionForHashing,
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kRuntimeOnMessageWebExtensionPolyfillSupport,
-             "RuntimeOnMessageWebExtensionPolyfillSupport",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 }  // namespace extensions_features
