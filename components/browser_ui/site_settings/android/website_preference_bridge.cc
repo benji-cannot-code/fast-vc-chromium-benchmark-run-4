84a8326acb20ba33d61fe15cc7d7c0c31d702ca3
Yifan Luo <lyf@chromium.org>
2025-10-07 04:01:56 -0700

    [SiteSettingsRadioButton] Fix histogram Permissions.SiteSettingsChanged
    Bug: 449918080
    Change-Id: I1b2ad617ef52f3ae723b8e76a5aa9dc1669b349e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6999772
    Reviewed-by: Elias Klim <elklm@chromium.org>
    Commit-Queue: Yifan Luo <lyf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1526175}

--- components/browser_ui/site_settings/android/website_preference_bridge.cc
+++ components/browser_ui/site_settings/android/website_preference_bridge.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/cdm/browser/media_drm_storage_impl.h"
 #include "components/content_settings/browser/ui/cookie_controls_util.h"
 #include "components/content_settings/core/browser/content_settings_registry.h"
+#include "components/content_settings/core/browser/content_settings_uma_util.h"
 #include "components/content_settings/core/browser/cookie_settings.h"
 #include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "components/content_settings/core/browser/permission_settings_registry.h"
@@ -965,7 +966,8 @@ static void JNI_WebsitePreferenceBridge_SetContentSettingEnabled(
     jboolean allow) {
   ContentSettingsType type =
       static_cast<ContentSettingsType>(content_settings_type);
-  base::UmaHistogramEnumeration("Permissions.SiteSettingsChanged", type);
+  content_settings_uma_util::RecordContentSettingsHistogram(
+      "Permissions.SiteSettingsChanged", type);
 
   if (type == ContentSettingsType::SOUND) {
     if (allow) {
