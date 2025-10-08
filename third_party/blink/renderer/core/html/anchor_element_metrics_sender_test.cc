22e7112b7e2435eef82256dc766826d7380c5922
Takashi Nakayama <tnak@chromium.org>
2025-10-08 00:49:00 -0700

    Launch PreloadingEagerHeuristics on Desktop
    This CL enables the feature `kPreloadingEagerHeuristics` by default on
    Desktop with setting the parameter to the final value settled by our
    experiment. This CL also updates some unittests to avoid flakiness on
    some platforms by enabling the feature.
    
    DesignDoc:
    https://docs.google.com/document/d/1YPbtUPfZIDElzBZNx8IQMzRFvy8oauLG_i1XIr6jgTs
    
    Approval on Stable 1% (internal only):
    https://docs.google.com/document/d/1BVAvb9mg0CNLHgCzPdaR2qHZ8bcZq_ksNlCruTF3gsE
    
    Bug: 406371881
    Change-Id: Ia2ca7a21c5676e0a6572de85e14ff45000d64ce7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7005636
    Auto-Submit: Takashi Nakayama <tnak@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1526731}

--- third_party/blink/renderer/core/html/anchor_element_metrics_sender_test.cc
+++ third_party/blink/renderer/core/html/anchor_element_metrics_sender_test.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html/anchor_element_metrics_sender.h"
 
 #include "base/containers/contains.h"
+#include "base/strings/strcat.h"
+#include "base/strings/string_number_conversions.h"
 #include "base/test/bind.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/simple_test_tick_clock.h"
@@ -161,12 +163,23 @@ class AnchorElementMetricsSenderTest : public SimTest {
     // Allows WidgetInputHandlerManager::InitOnInputHandlingThread() to run.
     platform_->RunForPeriod(base::Milliseconds(1));
     // Report all anchors to avoid non-deterministic behavior.
-    std::map<std::string, std::string> params;
-    params["random_anchor_sampling_period"] = "1";
-    params["intersection_observation_after_fcp_only"] = "false";
+    std::map<std::string, std::string> nav_predictor_params;
+    nav_predictor_params["random_anchor_sampling_period"] = "1";
+    nav_predictor_params["intersection_observation_after_fcp_only"] = "false";
+    // Set "eager" hover time to the same as "moderate" hover time to avoid test
+    // flakiness.
+    const std::string eager_hover_time = base::StrCat(
+        {base::NumberToString(
+             AnchorElementInteractionTracker::kModerateHoverDwellTime
+                 .InMilliseconds()),
+         "ms"});
+    std::map<std::string, std::string> eager_heuristics_params = {
+        {{"hover_dwell_time", eager_hover_time}}};
 
-    feature_list_.InitAndEnableFeatureWithParameters(
-        features::kNavigationPredictor, params);
+    feature_list_.InitWithFeaturesAndParameters(
+        {{features::kNavigationPredictor, nav_predictor_params},
+         {features::kPreloadingEagerHeuristics, eager_heuristics_params}},
+        {});
 
     IntersectionObserver::SetThrottleDelayEnabledForTesting(false);
 
@@ -1045,12 +1058,12 @@ TEST_F(AnchorElementMetricsSenderTest,
   base::RunLoop().RunUntilIdle();
   EXPECT_EQ(1u, hosts_.size());
   const auto& mock_host = hosts_[0];
-  EXPECT_EQ(1u, mock_host->pointer_data_on_hover_.size());
-  EXPECT_TRUE(
-      mock_host->pointer_data_on_hover_[0]->pointer_data->is_mouse_pointer);
-  EXPECT_NEAR(
-      20.0 * std::sqrt(2.0),
-      mock_host->pointer_data_on_hover_[0]->pointer_data->mouse_velocity, 0.5);
+  // Must have at least one hover data.
+  EXPECT_LE(1u, mock_host->pointer_data_on_hover_.size());
+  for (const auto& data : mock_host->pointer_data_on_hover_) {
+    EXPECT_TRUE(data->pointer_data->is_mouse_pointer);
+    EXPECT_NEAR(20.0 * std::sqrt(2.0), data->pointer_data->mouse_velocity, 0.5);
+  }
 }
 
 TEST_F(AnchorElementMetricsSenderTest, MaxIntersectionObservations) {
