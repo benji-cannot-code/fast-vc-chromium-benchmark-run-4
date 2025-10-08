34db3573ce7e20c5b50660889321b015e9a6b7bf
Antonio Gomes <tonikitoo@igalia.com>
2025-10-08 08:33:24 -0700

    Pass `local_state` to ash::InputMethodDelegateImpl ctor
    This CL is a step forward eliminating the uses of `g_browser_process`
    from c/b/ash/input_method/. There still references to it in this
    directory that will be handled in a follow up CL.
    
    No behavior change expected.
    
    R=hidehiko@chromium.org, junis@google.com
    
    Bug: 446058312
    Change-Id: Ibe36dc8ab95b457c394cd16d607ec96bbafe6537
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7017915
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Jun Ishiguro <junis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1526937}

--- chrome/browser/ash/input_method/input_method_delegate_impl.h
+++ chrome/browser/ash/input_method/input_method_delegate_impl.h
@@ -8,7 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/memory/raw_ref.h"
 #include "ui/base/ime/ash/input_method_delegate.h"
+
+class PrefService;
 
 namespace ash {
 namespace input_method {
@@ -17,7 +20,8 @@ namespace input_method {
 // BrowserProcess.
 class InputMethodDelegateImpl : public InputMethodDelegate {
  public:
-  InputMethodDelegateImpl();
+  // `local_state` must be non-null, and must outlive `this`.
+  explicit InputMethodDelegateImpl(PrefService* local_state);
 
   InputMethodDelegateImpl(const InputMethodDelegateImpl&) = delete;
   InputMethodDelegateImpl& operator=(const InputMethodDelegateImpl&) = delete;
@@ -28,6 +32,9 @@ class InputMethodDelegateImpl : public InputMethodDelegate {
   std::string GetHardwareKeyboardLayouts() const override;
   std::u16string GetLocalizedString(int resource_id) const override;
   void SetHardwareKeyboardLayoutForTesting(const std::string& layout) override;
+
+ private:
+  const raw_ref<PrefService> local_state_;
 };
 
 }  // namespace input_method
