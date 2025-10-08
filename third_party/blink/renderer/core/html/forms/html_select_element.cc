031695fdfa6947b617a186037fa0266c60a23d61
Joey Arhar <jarhar@chromium.org>
2025-10-08 15:38:18 -0700

    Correct size attribute parsing for SelectMobileDesktopParity
    The code which determined whether to use ListBox or MenuList rendering
    for SelectMobileDesktopParity assumed that invalid or missing size
    attributes set the size_ variable to 1, but it actually gets set to 0.
    This patch fixes it by handling the size_ variable the same as the
    non-SelectMobileDesktopParity branch.
    
    This patch also adds a call to invalidate :-internal-list-box since it
    certainly seems needed, but it doesn't actually seem to change the
    behavior.
    
    Fixed: 450045066
    Bug: 439964654
    Change-Id: Ibeafa40bfeb19efe647a61283de6077dd3338535
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7022339
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1527201}

--- third_party/blink/renderer/core/html/forms/html_select_element.cc
+++ third_party/blink/renderer/core/html/forms/html_select_element.cc
@@ -235,7 +235,7 @@ void HTMLSelectElement::UpdateUsesMenuList() {
       uses_menu_list_ =
           FastHasAttribute(html_names::kSizeAttr) ? size_ == 1 : false;
     } else {
-      uses_menu_list_ = size_ == 1;
+      uses_menu_list_ = size_ <= 1;
     }
     return;
   }
@@ -1605,6 +1605,7 @@ void HTMLSelectElement::ChangeRendering() {
   if (UsesMenuList() != old_uses_menu_list) {
     select_type_->WillBeDestroyed();
     select_type_ = SelectType::Create(*this);
+    PseudoStateChanged(CSSSelector::kPseudoListBox);
   }
   if (!InActiveDocument())
     return;
