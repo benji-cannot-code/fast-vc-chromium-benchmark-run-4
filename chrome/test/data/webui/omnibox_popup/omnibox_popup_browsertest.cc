36d6aed1a94b7a46f2eab8029bcc1cf04fb48364
dpapad <dpapad@chromium.org>
2025-10-08 15:11:57 -0700

    WebUI Omnibox: Disable OmniboxPopupTest.App on linux-js-coverage-rel.
    DevToolsListener::VerifyAllScriptsAreParsedRepeatedly() seems confused
    when multiple instances of the same WebUI exist and throws the following
    error
    
    [1380392:1380392:1006/174456.007070:FATAL:chrome/test/base/devtools_listener.cc:156]
    Check failed: all_scripts_parsed_. All scripts in coverage results were
    not retrieved after 10s of waiting
    
    This likely happens because chrome://omnibox-popup.top-chrome is loaded
    twice, once in a tab during the test, and once as part of
    OmniboxPopupPresenter regardless of the test.
    
    Bug: 40284073, b:445424843
    Change-Id: Ifde636a3bb4bb1b8ee7998cea8189ae6dab293c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7018871
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: David Yeung <dayeung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1527187}

--- chrome/test/data/webui/omnibox_popup/omnibox_popup_browsertest.cc
+++ chrome/test/data/webui/omnibox_popup/omnibox_popup_browsertest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/test/scoped_feature_list.h"
+#include "build/config/coverage/buildflags.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/test/base/web_ui_mocha_browser_test.h"
 #include "components/omnibox/common/omnibox_features.h"
@@ -20,7 +21,13 @@ class OmniboxPopupBrowserTest : public WebUIMochaBrowserTest {
       omnibox::kWebUIOmniboxPopup};
 };
 
+#if BUILDFLAG(USE_JAVASCRIPT_COVERAGE)
+// TODO(crbug.com/40284073): Test fails with JS coverage turned on.
+#define MAYBE_App DISABLED_App
+#else
+#define MAYBE_App App
+#endif
 typedef OmniboxPopupBrowserTest OmniboxPopupTest;
-IN_PROC_BROWSER_TEST_F(OmniboxPopupTest, App) {
+IN_PROC_BROWSER_TEST_F(OmniboxPopupTest, MAYBE_App) {
   RunTest("omnibox_popup/app_test.js", "mocha.run();");
 }
