37dd3ba1b5ecda12573d75b667fdb37cb54a8dd1
Dhruv Kathpalia <dhruvkathpalia@google.com>
2025-10-08 09:49:10 -0700

    [ntp-realbox] Add feature flags for the 2 Tall realbox variants.
    This change refactors the `RealboxLayoutMode` enum to distinguish
    between two tall layouts for the Realbox. The existing "Tall" mode is
    now represented by "TallBottomContext" (context button displayed at the
    bottom of dropdown) and "TallTopContext" (context button displayed above
    the dropdown items). CSS and HTML templates are updated to apply the
    same styling to both new modes.
    
    Bug: b:448168842
    
    Change-Id: I601cd80f511a64d75652c52ec7ff93e13edcf7a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7014528
    Reviewed-by: Ananya Seelam <ananyaseelam@google.com>
    Commit-Queue: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1526975}

--- chrome/browser/ui/webui/new_tab_page/composebox/variations/composebox_fieldtrial.h
+++ chrome/browser/ui/webui/new_tab_page/composebox/variations/composebox_fieldtrial.h
@@ -90,7 +90,8 @@ extern const base::FeatureParam<bool> kCyclingPlaceholders;
 
 // Enum for `kRealboxLayoutMode`.
 enum class RealboxLayoutMode {
-  kTall,
+  kTallBottomContext,
+  kTallTopContext,
   kCompact,
 };
 
@@ -100,7 +101,9 @@ extern const base::FeatureParam<RealboxLayoutMode> kRealboxLayoutMode;
 bool IsNtpRealboxNextEnabled(Profile* profile);
 
 // String constants for RealboxLayoutMode.
-inline constexpr char kRealboxLayoutModeTall[] = "Tall";
+inline constexpr char kRealboxLayoutModeTallBottomContext[] =
+    "TallBottomContext";
+inline constexpr char kRealboxLayoutModeTallTopContext[] = "TallTopContext";
 inline constexpr char kRealboxLayoutModeCompact[] = "Compact";
 
 // Returns the string representation of `RealboxLayoutMode`.
