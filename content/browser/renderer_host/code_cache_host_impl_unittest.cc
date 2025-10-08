fb6abab77a219adad22edc2522f2dae641b85369
Olivier Li <olivierli@chromium.org>
2025-10-08 09:53:49 -0700

    Be more precise under UsePersistentCacheForCodeCache.
    This CL: 1) Addresses the fact that ClearCodeCacheEntry is a no-op under
    the experiment. 2) Avoids storing WASM entries within the JS code cache.
    3) Enhances testing to verify that the right code paths are exercised.
    
    Bug: 374930286,377475540
    Change-Id: I8c16a2edc4d56860c6baaf379fcf58e40c787203
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7013337
    Commit-Queue: Olivier Li <olivierli@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1526978}

--- content/browser/renderer_host/code_cache_host_impl_unittest.cc
+++ content/browser/renderer_host/code_cache_host_impl_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/test/bind.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/time/time.h"
 #include "content/browser/child_process_security_policy_impl.h"
@@ -64,6 +65,13 @@ class CodeCacheHostImplTest : public testing::Test,
     }
   }
 
+  void TearDown() override {
+    // The GeneratedCodeCache should not be exercised at all for the queries
+    // done in this test since they are expected to be served from
+    // PersistentCache.
+    histogram_tester.ExpectTotalCount("SiteIsolatedCodeCache.JS.Behaviour", 0);
+  }
+
   bool IsSitePerProcessOrStricter() { return GetParam(); }
 
   void SetupRendererWithLock(int process_id, const GURL& url) {
@@ -82,6 +90,7 @@ class CodeCacheHostImplTest : public testing::Test,
 
  protected:
   BrowserTaskEnvironment task_environment_;
+  base::HistogramTester histogram_tester;
   std::vector<int> added_renderers_;
   TestBrowserContext browser_context_;
   base::test::ScopedFeatureList feature_list_;
