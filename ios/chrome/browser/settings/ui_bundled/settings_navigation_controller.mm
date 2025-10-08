88b508aaa98a4bf246efcd5424d5d1b05ba51fa7
Chirag Arora <heychirag@google.com>
2025-10-08 07:46:06 -0700

    [site_info] Add support for opening Tracking Protection Settings page
    This commit adds support for opening the tracking protection settings
    page from the 'Settings' link in the footer section. The site-info page
    is first closed and then the 'Incognito tracking protections' settings
    page is opened.
    
    Bug: 442799468
    Change-Id: Ieb3c570428fda5c4a92b9ff6fd24d34229079056
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7011528
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Fiona Macintosh <fmacintosh@google.com>
    Commit-Queue: Chirag Arora <heychirag@google.com>
    Cr-Commit-Position: refs/heads/main@{#1526904}

--- ios/chrome/browser/settings/ui_bundled/settings_navigation_controller.mm
+++ ios/chrome/browser/settings/ui_bundled/settings_navigation_controller.mm
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/settings/ui_bundled/password/passwords_coordinator.h"
 #import "ios/chrome/browser/settings/ui_bundled/privacy/privacy_coordinator.h"
 #import "ios/chrome/browser/settings/ui_bundled/privacy/privacy_safe_browsing_coordinator.h"
+#import "ios/chrome/browser/settings/ui_bundled/privacy/tracking_protections/tracking_protections_coordinator.h"
 #import "ios/chrome/browser/settings/ui_bundled/safety_check/safety_check_coordinator.h"
 #import "ios/chrome/browser/settings/ui_bundled/safety_check/safety_check_table_view_controller.h"
 #import "ios/chrome/browser/settings/ui_bundled/settings_navigation_controller_constants.h"
@@ -97,6 +98,7 @@ NSString* const kSettingsDoneButtonId = @"kSettingsDoneButtonId";
     PrivacySafeBrowsingCoordinatorDelegate,
     SafetyCheckCoordinatorDelegate,
     SyncEncryptionPassphraseTableViewControllerPresentationDelegate,
+    TrackingProtectionsCoordinatorDelegate,
     UIAdaptivePresentationControllerDelegate,
     UINavigationControllerDelegate>
 
@@ -110,6 +112,9 @@ NSString* const kSettingsDoneButtonId = @"kSettingsDoneButtonId";
 
 // Privacy settings coordinator.
 @property(nonatomic, strong) PrivacyCoordinator* privacySettingsCoordinator;
+
+@property(nonatomic, strong)
+    TrackingProtectionsCoordinator* trackingProtectionsCoordinator;
 
 // Sync settings coordinator.
 @property(nonatomic, strong)
@@ -436,6 +441,20 @@ NSString* const kSettingsDoneButtonId = @"kSettingsDoneButtonId";
 }
 
 + (instancetype)
+    trackingProtectionControllerForBrowser:(Browser*)browser
+                                  delegate:
+                                      (id<SettingsNavigationControllerDelegate>)
+                                          delegate {
+  SettingsNavigationController* navigationController =
+      [[SettingsNavigationController alloc]
+          initWithRootViewController:nil
+                             browser:browser
+                            delegate:delegate];
+  [navigationController showTrackingProtectionSettings];
+  return navigationController;
+}
+
++ (instancetype)
     addressDetailsControllerForBrowser:(Browser*)browser
                               delegate:
                                   (id<SettingsNavigationControllerDelegate>)
@@ -701,6 +720,7 @@ NSString* const kSettingsDoneButtonId = @"kSettingsDoneButtonId";
   [self stopSafetyCheckCoordinator];
   [self stopPrivacySafeBrowsingCoordinator];
   [self stopPrivacySettingsCoordinator];
+  [self stopTrackingProtectionsCoordinator];
   [self stopInactiveTabSettingsCoordinator];
   [self stopPasswordDetailsCoordinator];
   [self stopAutofillProfileEditCoordinator];
@@ -804,6 +824,15 @@ NSString* const kSettingsDoneButtonId = @"kSettingsDoneButtonId";
   [self.privacySettingsCoordinator start];
 }
 
+// Starts PrivacyCoordinator and opens TrackingProtection Settings.
+- (void)showTrackingProtectionSettings {
+  self.trackingProtectionsCoordinator = [[TrackingProtectionsCoordinator alloc]
+      initWithBaseNavigationController:self
+                               browser:self.browser];
+  self.trackingProtectionsCoordinator.delegate = self;
+  [self.trackingProtectionsCoordinator start];
+}
+
 - (void)showSyncServices {
   if ([self.topViewController
           isKindOfClass:[ManageSyncSettingsCoordinator class]]) {
@@ -871,6 +900,12 @@ NSString* const kSettingsDoneButtonId = @"kSettingsDoneButtonId";
 - (void)stopPrivacySettingsCoordinator {
   [self.privacySettingsCoordinator stop];
   self.privacySettingsCoordinator = nil;
+}
+
+// Stops the tracking protections coordinator if it exists.
+- (void)stopTrackingProtectionsCoordinator {
+  [self.trackingProtectionsCoordinator stop];
+  self.trackingProtectionsCoordinator = nil;
 }
 
 // Stops the underlying Sync settings coordinator if it exists.
@@ -985,6 +1020,14 @@ NSString* const kSettingsDoneButtonId = @"kSettingsDoneButtonId";
     (PrivacyCoordinator*)coordinator {
   DCHECK_EQ(self.privacySettingsCoordinator, coordinator);
   [self stopPrivacySettingsCoordinator];
+}
+
+#pragma mark - TrackingProtectionsCoordinatorDelegate
+
+- (void)trackingProtectionsCoordinatorDidRemove:
+    (TrackingProtectionsCoordinator*)coordinator {
+  DCHECK_EQ(self.trackingProtectionsCoordinator, coordinator);
+  [self stopTrackingProtectionsCoordinator];
 }
 
 #pragma mark - ManageSyncSettingsCoordinatorDelegate
