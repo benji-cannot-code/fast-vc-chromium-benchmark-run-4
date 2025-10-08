e1fbbd6c5a65a85e814511d2e1681fb6f34679b8
Rainhard Findling <rainhard@chromium.org>
2025-10-08 10:20:54 -0700

    Settings code health: re-enable SoundPage test
    After the refactoring (crrev.com/c/7012688) it seems this test is
    passing as is. Tentatiely re-enabling it - in case it fails we at least
    get new info what the error is that it runs into.
    
    Fixed: 40669164
    Change-Id: I29028891f2af3d943b22268f5e4f594608011064
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7021789
    Reviewed-by: Yifan Luo <lyf@chromium.org>
    Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1527002}

--- chrome/test/data/webui/settings/settings_browsertest.cc
+++ chrome/test/data/webui/settings/settings_browsertest.cc
@@ -1704,8 +1704,7 @@ IN_PROC_BROWSER_TEST_F(SettingsSiteSettingsPageTest,
           "runMochaSuite('UnusedSitePermissionsReview')");
 }
 
-// TODO(crbug.com/40669164): flaky crash on Linux Tests (dbg).
-IN_PROC_BROWSER_TEST_F(SettingsSiteSettingsPageTest, DISABLED_SoundPage) {
+IN_PROC_BROWSER_TEST_F(SettingsSiteSettingsPageTest, SoundPage) {
   RunTest("settings/sound_page_test.js", "runMochaSuite('SoundPage')");
 }
 
