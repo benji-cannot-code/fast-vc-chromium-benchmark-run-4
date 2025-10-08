884755a9c781977f87f706aa034d727ae58ea248
Dibyajyoti Pal <dibyapal@google.com>
2025-10-08 13:47:22 -0700

    [Predictable App Update] Clearing pending updates dynamically update menu
    This CL updates the ManifestUpdateSilentCommand so that whenever pending
    updates are being set (or cleared), the observers fire based on the
    state of the pending update info stored on the app, instead of waiting
    for the whole command to complete. This serves 2 purposes:
    1. Keeps the behavior of the observer closer to where the field it is
    observing is, and makes it happen based on a single source of truth,
    AKA whatever is on the web app.
    2. Fixes a bug where clearing the pending update info wouldn't
    dynamically update the app browser's menu button's state.
    
    Adds a test that verifies that 2 is working as intended.
    
    Fixed: 449222137
    Change-Id: I0c491fe2318f749017fe2e9a00e7b11a6987de15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7008806
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1527141}

--- chrome/browser/ui/views/web_apps/frame_toolbar/web_app_menu_button.cc
+++ chrome/browser/ui/views/web_apps/frame_toolbar/web_app_menu_button.cc
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/web_apps/frame_toolbar/web_app_menu_button.h"
 
+#include "base/callback_list.h"
 #include "base/functional/bind.h"
+#include "base/functional/callback_forward.h"
 #include "base/metrics/user_metrics.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
@@ -110,6 +112,11 @@ void WebAppMenuButton::UpdateStateForTesting() {
   UpdateTextAndHighlightColor(HasPendingUpdate());
 }
 
+base::CallbackListSubscription WebAppMenuButton::AwaitLabelTextUpdated(
+    base::RepeatingClosure callback) {
+  return label()->AddTextChangedCallback(callback);
+}
+
 void WebAppMenuButton::ShowMenu(int run_types) {
   Browser* browser = browser_view_->browser();
   RunMenu(std::make_unique<WebAppMenuModel>(browser_view_, browser), browser,
@@ -182,7 +189,6 @@ void WebAppMenuButton::UpdateTextAndHighlightColor(bool is_pending_update) {
   }
 
   const bool label_present_and_visible = !text.empty();
-
   SetHorizontalAlignment(label_present_and_visible ? gfx::ALIGN_RIGHT
                                                    : gfx::ALIGN_CENTER);
 
