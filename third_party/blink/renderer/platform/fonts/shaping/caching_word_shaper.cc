36308b320faac9750a711dc7c8110a2f2025c367
Kent Tamura <tkent@chromium.org>
2025-10-08 00:25:52 -0700

    CanvasTextNG: Stop using ShapeResultBuffer instances
    ShapeResultBuffer is used only in
    CachingWordShaper::EmphasisMarkGlyphData(), and it stores only one
    ShapeResult. So, we can operate the ShapeResult directly, and don't need
    to use ShapeResultBuffer.
    
    This CL removes unused ShapeResultBuffer methods, and ShapeResultBuffer
    class now provides only a static method.
    
    This CL has no behavior changes.
    
    Bug: 448683997
    Change-Id: Ibcc33c1b8c8e989a940891674cfbf40bfba6536b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7017311
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1526719}

--- third_party/blink/renderer/platform/fonts/shaping/caching_word_shaper.cc
+++ third_party/blink/renderer/platform/fonts/shaping/caching_word_shaper.cc
@@ -27,16 +27,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/fonts/shaping/caching_word_shaper.h"
 
 #include "third_party/blink/renderer/platform/fonts/shaping/caching_word_shape_iterator.h"
-#include "third_party/blink/renderer/platform/fonts/shaping/shape_result_buffer.h"
 
 namespace blink {
 
 GlyphData CachingWordShaper::EmphasisMarkGlyphData(
     const TextRun& emphasis_mark_run) const {
-  ShapeResultBuffer buffer;
-  buffer.AppendResult(CachingWordShapeIterator::ShapeWordWithoutSpacing(
-      emphasis_mark_run, &font_));
-  return buffer.EmphasisMarkGlyphData(font_.GetFontDescription());
+  return CachingWordShapeIterator::ShapeWordWithoutSpacing(emphasis_mark_run,
+                                                           &font_)
+      ->EmphasisMarkGlyphData(font_.GetFontDescription());
 }
 
 }  // namespace blink
