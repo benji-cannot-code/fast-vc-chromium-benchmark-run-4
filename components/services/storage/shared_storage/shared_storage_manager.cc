ca1c3bd1732eb01ded8ff1e7c4a42faf6b2bb0df
Patrick Monette <pmonette@chromium.org>
2025-10-08 06:09:42 -0700

    [SharedStorageManager] Split OnMemoryPressure
    In a future CL, MemoryPressureListener will change from a callback-based
    API to an interface-based API. In this world, the OnMemoryPressure()
    function can't have additional parameters.
    
    This CL splits the current OnMemoryPressure() function so that the
    callback passed to `memory_pressure_listener_` does not have that extra
    parameter.
    
    Bug: 436324601
    Change-Id: I65dda1d1ff50c56644ad83ac5904373bb8e6c663
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7017460
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1526857}

--- components/services/storage/shared_storage/shared_storage_manager.cc
+++ components/services/storage/shared_storage/shared_storage_manager.cc
@@ -47,8 +47,7 @@ SharedStorageManager::SharedStorageManager(
           FROM_HERE,
           base::MemoryPressureListenerTag::kSharedStorageManager,
           base::BindRepeating(&SharedStorageManager::OnMemoryPressure,
-                              base::Unretained(this),
-                              base::DoNothing()))) {
+                              base::Unretained(this)))) {
   timer_.Start(FROM_HERE, options_->stale_purge_initial_interval,
                base::BindOnce(&SharedStorageManager::PurgeStale,
                               weak_ptr_factory_.GetWeakPtr()));
@@ -58,7 +57,7 @@ SharedStorageManager::~SharedStorageManager() {
   RecordShutdownMetrics();
 }
 
-void SharedStorageManager::OnMemoryPressure(
+void SharedStorageManager::HandleMemoryPressure(
     base::OnceCallback<void()> callback,
     base::MemoryPressureLevel memory_pressure_level) {
   DCHECK(callback);
@@ -71,6 +70,11 @@ void SharedStorageManager::OnMemoryPressure(
   }
 
   database_->TrimMemory(std::move(callback));
+}
+
+void SharedStorageManager::OnMemoryPressure(
+    base::MemoryPressureLevel memory_pressure_level) {
+  HandleMemoryPressure(base::DoNothing(), memory_pressure_level);
 }
 
 void SharedStorageManager::OnOperationResult(OperationResult result) {
