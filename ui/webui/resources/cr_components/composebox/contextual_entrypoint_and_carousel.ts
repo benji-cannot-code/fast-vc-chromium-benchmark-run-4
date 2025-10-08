8e39fac08a8187dc533ce3d379ff5140f482c309
Dhruv Kathpalia <dhruvkathpalia@google.com>
2025-10-08 14:32:16 -0700

    [ntp-realbox] Tall Realbox Arm 2 UI changes.
    The context menu's placement and the carousel divider's visibility are
    now dynamically adjusted based on whether the mode is 'Compact',
    'TallTopContext', or 'TallBottomContext'.
    Screenshot:
    http://screencast/cast/NDgwMjQ0MzgyODI2NDk2MHxhZWIxOWY2YS04ZQ
    
    Bug: b:448168268
    
    Change-Id: Ie943ef3dc861278ee6de55954e2a410e1764511f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7018148
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Reviewed-by: Hannah Collins <hannahrogers@google.com>
    Commit-Queue: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Cr-Commit-Position: refs/heads/main@{#1527164}

--- ui/webui/resources/cr_components/composebox/contextual_entrypoint_and_carousel.ts
+++ ui/webui/resources/cr_components/composebox/contextual_entrypoint_and_carousel.ts
@@ -70,7 +70,7 @@ export class ContextualEntrypointAndCarouselElement extends I18nMixinLit
   static override get properties() {
     return {
       showDropdown: {type: Boolean},
-      compactMode: {type: Boolean},
+      realboxLayoutMode: {type: String},
       attachmentFileTypes_: {type: String},
       contextMenuEnabled_: {type: Boolean},
       files_: {type: Object},
@@ -84,11 +84,11 @@ export class ContextualEntrypointAndCarouselElement extends I18nMixinLit
         type: Boolean,
       },
       showContextMenuDescription_: {type: Boolean},
-      showFileCarousel_ : {
+      showFileCarousel_: {
         reflect: true,
         type: Boolean,
       },
-      inDeepSearchMode_ : {
+      inDeepSearchMode_: {
         reflect: true,
         type: Boolean,
       },
@@ -100,7 +100,7 @@ export class ContextualEntrypointAndCarouselElement extends I18nMixinLit
   }
 
   accessor showDropdown: boolean = false;
-  accessor compactMode: boolean = false;
+  accessor realboxLayoutMode: string = '';
   protected accessor attachmentFileTypes_: string =
       loadTimeData.getString('composeboxAttachmentFileTypes');
   protected accessor contextMenuEnabled_: boolean =
