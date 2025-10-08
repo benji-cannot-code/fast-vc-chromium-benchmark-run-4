f9267862e412505aec931f123960da66b0d0a9b8
Vinny Persky <vinnypersky@google.com>
2025-10-08 15:35:39 -0700

    Reorder issuer names in the BNPL suggestion second line string experiment
    This CL reorders the issuer names in the BNPL suggestion second line
    string based on product alignment.
    
    Screenshot: https://screenshot.googleplex.com/A26LnzsSR2dFBdu
    
    Bug: 450285482
    Change-Id: I2cc167d188e2cbae8b6d092fd91fc163d7bdc124
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7022348
    Commit-Queue: Vinny Persky <vinnypersky@google.com>
    Reviewed-by: Wilson Low <wilsonlow@google.com>
    Cr-Commit-Position: refs/heads/main@{#1527199}

--- components/autofill/core/browser/suggestions/payments/payments_suggestion_generator.cc
+++ components/autofill/core/browser/suggestions/payments/payments_suggestion_generator.cc
@@ -1267,9 +1267,9 @@ Suggestion CreateBnplSuggestion(
       switch (id) {
         case BnplIssuer::IssuerId::kBnplAffirm:
           return 4;
-        case BnplIssuer::IssuerId::kBnplZip:
-          return 3;
         case BnplIssuer::IssuerId::kBnplKlarna:
+          return 3;
+        case BnplIssuer::IssuerId::kBnplZip:
           return 2;
         case BnplIssuer::IssuerId::kBnplAfterpay:
           NOTREACHED();
