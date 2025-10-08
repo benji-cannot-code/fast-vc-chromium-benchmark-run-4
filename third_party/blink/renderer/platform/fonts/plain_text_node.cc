efd0f522e3b9b9ae13a3321ecf47c230eefceba4
Kent Tamura <tkent@chromium.org>
2025-10-08 00:17:14 -0700

    CanvasTextNG: Remove unused CachingWordShapeIterator methods
    Now CachingWordShapeIterator provides only static methods.
    This CL has no behavior changes.
    
    Bug: 448683997
    Change-Id: Ib78ebd262f36e19db7c7df1cb4d827f14edb7e56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7014949
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1526716}

--- third_party/blink/renderer/platform/fonts/plain_text_node.cc
+++ third_party/blink/renderer/platform/fonts/plain_text_node.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/fonts/shaping/harfbuzz_shaper.h"
 #include "third_party/blink/renderer/platform/fonts/shaping/shape_result.h"
 #include "third_party/blink/renderer/platform/fonts/shaping/shape_result_buffer.h"
+#include "third_party/blink/renderer/platform/fonts/shaping/shape_result_spacing.h"
 #include "third_party/blink/renderer/platform/fonts/shaping/shape_result_view.h"
 #include "third_party/blink/renderer/platform/text/bidi_paragraph.h"
 #include "third_party/blink/renderer/platform/text/text_run.h"
--- third_party/blink/renderer/platform/fonts/shaping/caching_word_shape_iterator.cc
+++ third_party/blink/renderer/platform/fonts/shaping/caching_word_shape_iterator.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/fonts/shaping/caching_word_shape_iterator.h"
 
 #include "third_party/blink/renderer/platform/fonts/shaping/harfbuzz_shaper.h"
+#include "third_party/blink/renderer/platform/fonts/shaping/shape_cache.h"
 #include "third_party/blink/renderer/platform/fonts/shaping/shape_result.h"
 
 namespace blink {
@@ -13,7 +14,7 @@ namespace blink {
 const ShapeResult* CachingWordShapeIterator::ShapeWordWithoutSpacing(
     const TextRun& word_run,
     const Font* font) {
-  ShapeCacheEntry* cache_entry = shape_cache_->Add(word_run);
+  ShapeCacheEntry* cache_entry = font->GetShapeCache()->Add(word_run);
   if (cache_entry && *cache_entry)
     return *cache_entry;
 
@@ -26,16 +27,6 @@ const ShapeResult* CachingWordShapeIterator::ShapeWordWithoutSpacing(
     *cache_entry = shape_result;
 
   return shape_result;
-}
-
-const ShapeResult* CachingWordShapeIterator::ShapeWord(const TextRun& word_run,
-                                                       const Font* font) {
-  const ShapeResult* result = ShapeWordWithoutSpacing(word_run, font);
-  if (!spacing_.HasSpacing()) [[likely]] {
-    return result;
-  }
-
-  return result->ApplySpacingToCopy(spacing_, word_run);
 }
 
 }  // namespace blink
