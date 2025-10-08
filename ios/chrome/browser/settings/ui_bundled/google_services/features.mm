9edd79bf8048917f740fdd5b8df097993d6006ca
Rainhard Findling <rainhard@chromium.org>
2025-10-08 04:54:48 -0700

    [Cake] Enable LinkedServicesSettingIos by default
    Fixed: 449972759
    Bug: 324091979
    Change-Id: Ic2477f8d39eebb3c1a209b8f043de9973671b0b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7013594
    Auto-Submit: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1526833}

--- ios/chrome/browser/settings/ui_bundled/google_services/features.mm
+++ ios/chrome/browser/settings/ui_bundled/google_services/features.mm
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/settings/ui_bundled/google_services/features.h"
 
-BASE_FEATURE(kLinkedServicesSettingIos, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kLinkedServicesSettingIos, base::FEATURE_ENABLED_BY_DEFAULT);
 
 bool IsLinkedServicesSettingIosEnabled() {
   return base::FeatureList::IsEnabled(kLinkedServicesSettingIos);
