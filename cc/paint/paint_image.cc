ef169a961bee6e0b73fd11fc06d16bee6300cffd
Colin Blundell <blundell@chromium.org>
2025-10-08 00:18:12 -0700

    [//cc] Remove TextureBacking::GetAcceleratedSkImage()
    This now always returns null.
    
    Bug: 404905709
    Change-Id: I9d34e2a2c2cf5468b8e505df1921bace5e8724ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7016230
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1526717}

--- cc/paint/paint_image.cc
+++ cc/paint/paint_image.cc
@@ -227,8 +227,6 @@ void PaintImage::CreateSkImage() {
         std::make_unique<SkiaPaintImageGenerator>(paint_image_generator_,
                                                   kDefaultFrameIndex,
                                                   kDefaultGeneratorClientId));
-  } else if (texture_backing_) {
-    cached_sk_image_ = texture_backing_->GetAcceleratedSkImage();
   }
 }
 
