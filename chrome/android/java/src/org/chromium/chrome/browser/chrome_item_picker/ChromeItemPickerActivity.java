4c3ca6a7dda6cdfb715c5bbdb084c46b235f0712
Bhuvana Betini <bbetini@google.com>
2025-10-08 14:08:38 -0700

    [ChromeItemPicker] Implement Headless TabModel Access
    This CL establishes the architecture for the Chrome Item Picker UI. It
    creates a class TabItemPickerProvider that handles tab-specific logic
    that returns a list of Tabs to the ChromeItemPickerActivity once the
    TabModel is initialized.
    
    Bug: 445715015
    Change-Id: Ie9e7cec237dd0b6b06c298a0b7616c298ddf2933
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6991055
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Commit-Queue: Bhuvana Betini <bbetini@google.com>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1527156}

--- chrome/android/java/src/org/chromium/chrome/browser/chrome_item_picker/ChromeItemPickerActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/chrome_item_picker/ChromeItemPickerActivity.java
@@ -5,26 +5,71 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.chrome_item_picker;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import org.chromium.base.CallbackController;
+import org.chromium.base.IntentUtils;
+import org.chromium.base.Log;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.IntentHandler;
 import org.chromium.chrome.browser.SnackbarActivity;
+import org.chromium.chrome.browser.tabmodel.TabModelSelector;
+import org.chromium.chrome.browser.tabwindow.TabWindowManager;
 
 /** An activity that serves as an entry point for selecting Chrome items, like tabs. */
 @NullMarked
 public class ChromeItemPickerActivity extends SnackbarActivity {
-    private static final String TAG = "ChromeItemPickerActivity";
+    private static final String TAG = "ChromeItemPicker";
+
+    private final CallbackController mCallbackController = new CallbackController();
+    private int mWindowId;
+    @Nullable private TabItemPickerCoordinator mItemPickerCoordinator;
 
     @Override
     protected void onCreateInternal(@Nullable Bundle savedInstanceState) {
         super.onCreateInternal(savedInstanceState);
         setContentView(R.layout.chrome_item_picker_activity);
+
+        // TODO: Set mWindowId to be the most recently used window.
+        mWindowId =
+                IntentUtils.safeGetIntExtra(
+                        getIntent(),
+                        IntentHandler.EXTRA_WINDOW_ID,
+                        TabWindowManager.INVALID_WINDOW_ID);
+        if (mWindowId == TabWindowManager.INVALID_WINDOW_ID) {
+            mWindowId =
+                    savedInstanceState == null ? 0 : savedInstanceState.getInt("window_index", 0);
+        }
+
+        mItemPickerCoordinator =
+                new TabItemPickerCoordinator(mCallbackController, getProfileSupplier(), mWindowId);
+
+        mItemPickerCoordinator.requestTabModel(this::handleModelFailure);
     }
 
     @Override
     protected void onDestroy() {
+        mCallbackController.destroy();
+
         super.onDestroy();
+    }
+
+    private void onError(String error) {
+        Log.e(TAG, error);
+        final Intent resultIntent = new Intent();
+        resultIntent.putExtra(IntentHandler.EXTRA_ITEM_PICKER_ERROR, error);
+        setResult(Activity.RESULT_CANCELED, resultIntent);
+        finish();
+    }
+
+    private void handleModelFailure(@Nullable TabModelSelector tabModelSelector) {
+        if (tabModelSelector == null) {
+            onError("Failed to launch activity.");
+            return;
+        }
     }
 }
