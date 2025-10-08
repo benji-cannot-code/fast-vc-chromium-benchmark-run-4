568476947e77577dbc977a332e352f5f65cc71a0
Philip Pfaffe <pfaffe@chromium.org>
2025-10-08 02:24:08 -0700

    Network.setBlockedURLs: Configure whether to block or not block a pattern
    This allows for finegrained control over blocking patterns and also for
    composition with Network.emulateNetworkConditionsByRule. Specifically,
    this can be used to prevent broad blocking patterns from blocking
    requests that should be throttled instead.
    
    URL blocking is handled on the renderer-side, whereas throttling happens
    in the network service. If a request matches both a blocking pattern and
    a throttling pattern, it will be blocked. To allow users to, e.g., block
    all requests to *://*example.com/*, but throttle requets to
    *://foo.example.com/*, they can now configure the latter as "not
    blocked" explicitly to make this scenario work.
    
    Bug: 443904207
    Change-Id: I22725ad80226275c9410b362fdddeee265cb7673
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7006155
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Auto-Submit: Philip Pfaffe <pfaffe@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1526781}

--- third_party/blink/web_tests/http/tests/inspector-protocol/network/url-blocking.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/network/url-blocking.js
@@ -41,18 +41,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await testRequest('resources/b.html', false);
 
   testRunner.log('\nTesting `urlPatterns` parameter:');
-  await dp.Network.setBlockedURLs({urlPatterns: ['http://*:*/*/b.html']});
+  await dp.Network.setBlockedURLs(
+      {urlPatterns: [{urlPattern: 'http://*:*/*/b.html', block: true}]});
   await testRequest('resources/a.html', false);
   await testRequest('resources/b.html', true);
 
   testRunner.log('\nTesting `urls` and `urlPatterns` combined:');
   await dp.Network.setBlockedURLs({
     urls: ['*a.html'],
-    urlPatterns: ['http://*:*/*/b.html']
+    urlPatterns: [{urlPattern: 'http://*:*/*/b.html', block: true}]
   });
   await testRequest('resources/a.html', true);
   await testRequest('resources/b.html', true);
   await testRequest('resources/c.html', false);
+
+  testRunner.log('\nTesting allowlisting');
+  await dp.Network.setBlockedURLs({
+    urlPatterns: [
+      {urlPattern: 'http://*:*/*/b.html', block: false},
+      {urlPattern: 'http://*:*/*', block: true}
+    ]
+  });
+  await testRequest('resources/a.html', true);
+  await testRequest('resources/b.html', false);
+  await testRequest('resources/c.html', true);
 
   testRunner.log('\nTesting clearing blocked URLs:');
   await dp.Network.setBlockedURLs({urls: [], urlPatterns: []});
@@ -60,8 +72,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await testRequest('resources/b.html', false);
 
   testRunner.log('\nTest setting patterns that fail to parse:');
-  testRunner.log(await dp.Network.setBlockedURLs({urlPatterns: ['ht tp://']}));
-  testRunner.log(await dp.Network.setBlockedURLs({urlPatterns: ['*.css']}));
+  testRunner.log(await dp.Network.setBlockedURLs({urlPatterns: [{urlPattern: 'ht tp://', block: true}]}));
+  testRunner.log(await dp.Network.setBlockedURLs({urlPatterns: [{urlPattern: '*.css', block: true}]}));
 
   testRunner.completeTest();
 })
