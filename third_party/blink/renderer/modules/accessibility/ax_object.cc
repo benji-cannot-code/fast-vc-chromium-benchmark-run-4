41e3ba57c9cfd807a0e63df9a051ce7d722da89f
moonira <moonira@google.com>
2025-10-08 06:02:58 -0700

    Remove redundant TODO
    Already fixed in
    https://chromium-review.googlesource.com/c/chromium/src/+/7003230
    Just forgot to remove TODO.
    
    
    Bug: 414556050
    Change-Id: Icd931465af29fc2477de9bd3284649eeee729925
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7021389
    Commit-Queue: Munira Tursunova <moonira@google.com>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Auto-Submit: Munira Tursunova <moonira@google.com>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1526855}

--- third_party/blink/renderer/modules/accessibility/ax_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object.cc
@@ -7102,7 +7102,6 @@ void AXObject::SetScrollOffset(const gfx::Point& offset,
     return;
 
   // TODO(bokan): This should potentially be a UserScroll.
-  // TODO(crbug.com/414556050): Pass the correct `ScrollSourceType`.
   area->SetScrollOffset(ScrollOffset(offset.OffsetFromOrigin()),
                         mojom::blink::ScrollType::kProgrammatic, source_type);
 }
