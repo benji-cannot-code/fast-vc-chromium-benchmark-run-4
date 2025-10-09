f1c03cae6fe9b9e1459b61f40f06cab537fd3638
Kaan Alsan <alsan@chromium.org>
2025-10-09 11:30:48 -0700

    Reland "Integrate CookieControlsPageActionController with the browser"
    This is a reland of commit 13a9813ebf42e70fb00b107e7daa7e8d727923d2
    
    See diff from patchset 1 for changes. The original CL was reverted
    because the bubble controller was trying to remove its observation of
    the CookieControlsController during shutdown, but
    CookieControlsController was already destroyed. This CL fixes this by
    explicitly closing the bubble when the tab is deactivated.
    
    Original change's description:
    > Integrate CookieControlsPageActionController with the browser
    >
    > CookieControlsPageActionController, which manages the migrated cookie
    > controls page action path, is hooked up to the rest of the browser,
    > allowing it to take effect. This change consists of:
    > - Defining the browser action to trigger the page action
    > - Initializing the controller in TabFeatures
    > - Updating tests to use the new browser action to simulate invoking the page action
    > - Enabling the migration flag in fieldtrial_testing_config.json and adding it to the about://flags option.
    >
    > Bug: 376283777
    > Change-Id: I4250c1c2d2193cbd02050a1591725a45d660ee6e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6956907
    > Commit-Queue: Kaan Alsan <alsan@chromium.org>
    > Reviewed-by: Dana Fried <dfried@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1526323}
    
    Bug: 376283777
    Change-Id: Ia534aa62902e850ee4c1384f22af7261522b55ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7021969
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1527679}

--- chrome/browser/ui/tabs/tab_features.cc
+++ chrome/browser/ui/tabs/tab_features.cc
@@ -70,6 +70,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/commerce/price_insights_page_action_view_controller.h"
 #include "chrome/browser/ui/views/file_system_access/file_system_access_page_action_controller.h"
 #include "chrome/browser/ui/views/intent_picker/intent_picker_view_page_action_controller.h"
+#include "chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_page_action_controller.h"
 #include "chrome/browser/ui/views/page_action/action_ids.h"
 #include "chrome/browser/ui/views/page_action/page_action_controller.h"
 #include "chrome/browser/ui/views/page_action/page_action_properties_provider.h"
@@ -215,6 +216,14 @@ void TabFeatures::Init(TabInterface& tab, Profile* profile) {
       manage_passwords_page_action_controller_ =
           std::make_unique<ManagePasswordsPageActionController>(
               *page_action_controller_);
+    }
+
+    if (IsPageActionMigrated(PageActionIconType::kCookieControls)) {
+      cookie_controls_page_action_controller_ =
+          GetUserDataFactory()
+              .CreateInstance<CookieControlsPageActionController>(
+                  tab, tab, *profile, *page_action_controller_);
+      cookie_controls_page_action_controller_->Init();
     }
   }
 
