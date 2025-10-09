d278243a63ae62cee3c6c779392dd8295349680f
Foromo Daniel Soromou <koretadaniel@chromium.org>
2025-10-09 15:40:00 -0700

    Remove WidgetObserver::OnWidgetClosing from DataSharingBubbleController
    Bug: 40194222
    Change-Id: Iba36a5c8ec1b088d24feb03b9d83046bac4c9109
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7028943
    Commit-Queue: Daniel Soromou <koretadaniel@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1527810}

--- chrome/browser/ui/views/data_sharing/data_sharing_bubble_controller.h
+++ chrome/browser/ui/views/data_sharing/data_sharing_bubble_controller.h
@@ -16,15 +16,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/base/unowned_user_data/scoped_unowned_user_data.h"
-#include "ui/views/widget/widget_observer.h"
+#include "ui/views/widget/widget.h"
 
 class BrowserWindowInterface;
 class Profile;
 class TabStripModel;
 
 // Controller responsible for hosting the data sharing bubble per browser.
-class DataSharingBubbleController : public views::WidgetObserver,
-                                    public DataSharingUI::Delegate {
+class DataSharingBubbleController : public DataSharingUI::Delegate {
  public:
   DECLARE_USER_DATA(DataSharingBubbleController);
 
@@ -38,7 +37,7 @@ class DataSharingBubbleController : public views::WidgetObserver,
   DataSharingBubbleController(const DataSharingBubbleController&) = delete;
   DataSharingBubbleController& operator=(const DataSharingBubbleController&) =
       delete;
-  ~DataSharingBubbleController() override;
+  virtual ~DataSharingBubbleController();
 
   static DataSharingBubbleController* From(
       BrowserWindowInterface* browser_window_interface);
@@ -63,9 +62,6 @@ class DataSharingBubbleController : public views::WidgetObserver,
       collaboration::CollaborationControllerDelegate::ResultCallback callback);
 
   void OnUrlReadyToShare(GURL url);
-
-  // views::WidgetObserver
-  void OnWidgetClosing(views::Widget* widget) override;
 
   // DataSharingUI::Delegate
   void ApiInitComplete() override;
@@ -93,6 +89,8 @@ class DataSharingBubbleController : public views::WidgetObserver,
   }
 
  private:
+  void OnWidgetClosing(views::Widget::ClosedReason closed_reason);
+
   void MaybeRunJoinCallback(bool on_close);
 
   Profile* GetProfile();
@@ -100,9 +98,6 @@ class DataSharingBubbleController : public views::WidgetObserver,
   const raw_ref<BrowserWindowInterface> browser_;
   const raw_ref<Profile> profile_;
   const raw_ref<TabStripModel> tab_strip_model_;
-
-  base::ScopedObservation<views::Widget, views::WidgetObserver>
-      bubble_widget_observation_{this};
 
   // Callback to invoke when the widget closes.
   OnCloseCallback on_close_callback_;
