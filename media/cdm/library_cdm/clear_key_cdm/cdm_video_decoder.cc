c74059de4272d26d7cb17a80405a0ff2c1ef1bf4
Miguel Casas-Sanchez <mcasas@chromium.org>
2025-10-08 17:06:43 -0700

    media: Minor cleanups allowing use_libvpx=false GN
    While building Cobalt (a downstream embedder) //media targets with
    use_libvpx=false I noticed that supported_types_unittest.cc makes
    the assumption that VP9 is supported everywhere but in fact the
    code uses ENABLE_LIBVPX guard ([1]).
    
    cdm_video_decoder.cc also needs two explicit //media/base includes.
    
    No new functionality intended.
    
    Bug: 437151722
    Change-Id: I6cad64319d95f717a3fc6100567e4af11d24390c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7018429
    Commit-Queue: Miguel Casas-Sanchez <mcasas@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1527233}

--- media/cdm/library_cdm/clear_key_cdm/cdm_video_decoder.cc
+++ media/cdm/library_cdm/clear_key_cdm/cdm_video_decoder.cc
@@ -32,6 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/decoder_status.h"
 #include "media/base/media_switches.h"
 #include "media/base/media_util.h"
+#include "media/base/video_decoder.h"
+#include "media/base/video_frame.h"
 #include "media/cdm/cdm_type_conversion.h"
 #include "media/cdm/library_cdm/cdm_host_proxy.h"
 #include "media/media_buildflags.h"
