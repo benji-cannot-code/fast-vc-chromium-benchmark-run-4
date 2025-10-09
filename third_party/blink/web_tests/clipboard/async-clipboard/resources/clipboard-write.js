874c86bae1e40132fdc38f394c0fd0e659503777
Shweta Bindal <shwetabindal@microsoft.com>
2025-10-09 11:04:52 -0700

    [Clipboard] Divided selective format test to reduce test time
    Since there were too many test in 1 test file, its execution time was
    around 7-8 seconds which made it flaky.
    Divided tests in 4 file to reduce execution time.
    
    Bug: 448666461, 448312276
    Change-Id: I9b7a99fbef226a49fe925edca51538af760bb7ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7005887
    Reviewed-by: Rohan Raja <roraja@microsoft.com>
    Commit-Queue: Shweta Bindal <shwetabindal@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1527652}

---
+++ third_party/blink/web_tests/clipboard/async-clipboard/resources/clipboard-write.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+'use strict';
+async function WritePlainTextAndHTMLToClipboard() {
+  await test_driver.set_permission({name: 'clipboard-read'}, 'granted');
+  await test_driver.set_permission({name: 'clipboard-write'}, 'granted');
+  const formatPlain = 'text/plain';
+  const formatHtml = 'text/html';
+  const blobPlain = new Blob(['plain text content'], {type: formatPlain});
+  const blobHtml = new Blob(['<p>html content</p>'], {type: formatHtml});
+  const clipboardItem = new ClipboardItem({
+    [formatPlain]: blobPlain,
+    [formatHtml]: blobHtml
+  });
+  await waitForUserActivation();
+  await navigator.clipboard.write([clipboardItem]);
+}
+
+async function WriteCustomFormatToClipboard() {
+  await test_driver.set_permission({name: 'clipboard-read'}, 'granted');
+  await test_driver.set_permission({name: 'clipboard-write'}, 'granted');
+  const formatPlain = 'text/plain';
+  const formatCustom = 'web application/x-custom';
+  const blobPlain = new Blob(['plain text content'], {type: formatPlain});
+  const blobCustom = new Blob(['custom format data'], {type: 'application/x-custom'});
+  const clipboardItem = new ClipboardItem({
+    [formatPlain]: blobPlain,
+    [formatCustom]: blobCustom
+  });
+  await waitForUserActivation();
+  await navigator.clipboard.write([clipboardItem]);
+}
