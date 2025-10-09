3b1a44628a3480ca74e8286f52d596446b1bd16b
Thomas Guilbert <tguilbert@chromium.org>
2025-10-09 14:38:55 -0700

    [CodeHealth] Add span-based AudioBus::WrapMemory
    This CL adds versions of WrapMemory which take in spans instead of raw
    pointers.
    
    These functions will be used to gradually replace uses of WrapMemory()
    which use raw pointers.
    
    Bug: 373960632
    Change-Id: Id4b5990f2a5c9ff9faf176bb2b40c4235a6e053e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7023718
    Auto-Submit: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1527785}

--- media/base/audio_bus.h
+++ media/base/audio_bus.h
@@ -60,6 +60,10 @@ class MEDIA_EXPORT AudioBus {
                                               base::span<float> data);
   static std::unique_ptr<AudioBus> WrapMemory(const AudioParameters& params,
                                               void* data);
+  static std::unique_ptr<AudioBus> WrapMemory(const AudioParameters& params,
+                                              base::span<uint8_t> data);
+  static std::unique_ptr<AudioBus> WrapMemory(const AudioParameters& params,
+                                              base::span<float> data);
   static std::unique_ptr<const AudioBus> WrapReadOnlyMemory(
       const AudioParameters& params,
       const void* data);
@@ -231,7 +235,6 @@ class MEDIA_EXPORT AudioBus {
 
  protected:
   AudioBus(int channels, int frames);
-  AudioBus(int channels, int frames, float* data);
   AudioBus(int channels, int frames, base::span<float> data);
   explicit AudioBus(int channels);
 
