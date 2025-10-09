1283699ff62e1f2b557992a442fa43fccd1e8deb
John Kim <johnykim@google.com>
2025-10-09 13:48:08 -0700

    [FPF-CI] Canvas Noise Worker Integration (4/4)
    See previous CLs starting at
    (https://chromium-review.googlesource.com/c/chromium/src/+/6892077) for
    more context. This CL implements (2), the mojo pipe to update canvas
    noise tokens for shared workers. This mojo pipe is passed the same way
    as the initial canvas noise token. Where the remote end is owned by the
    SharedWorkerHost, and the receiver is owned by the shared worker's
    ExecutionContext.
    
    This CL also cleans up some tests made in prior CLs (1-3).
    
    This CL's design doc is a merge between these two docs:
    go/web-afp-interventions-design and go/canvas-noise-token-refactor
    
    Fixed: 405141821
    Change-Id: I49db64d7bfd7d06cddc56869176d17cf9841e97c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7006915
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Auto-Submit: John Kim <johnykim@google.com>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1527763}

--- third_party/blink/renderer/core/exported/web_shared_worker_impl.cc
+++ third_party/blink/renderer/core/exported/web_shared_worker_impl.cc
@@ -233,7 +233,9 @@ void WebSharedWorkerImpl::StartWorkerContext(
         coep_reporting_observer,
     CrossVariantMojoReceiver<mojom::blink::ReportingObserverInterfaceBase>
         dip_reporting_observer,
-    std::optional<blink::NoiseToken> canvas_noise_token) {
+    std::optional<blink::NoiseToken> canvas_noise_token,
+    CrossVariantMojoReceiver<mojom::CanvasNoiseTokenUpdaterInterfaceBase>
+        canvas_noise_token_observer) {
   DCHECK(IsMainThread());
   DCHECK(web_worker_fetch_context);
   CHECK(constructor_origin.Get()->CanAccessSharedWorkers());
@@ -297,7 +299,7 @@ void WebSharedWorkerImpl::StartWorkerContext(
       blink::SecurityOrigin::CreateFromUrlOrigin(
           url::Origin(origin_from_browser)),
       std::move(coep_reporting_observer), std::move(dip_reporting_observer),
-      std::move(canvas_noise_token));
+      std::move(canvas_noise_token), std::move(canvas_noise_token_observer));
 
   auto thread_startup_data = WorkerBackingThreadStartupData::CreateDefault();
   thread_startup_data.atomics_wait_mode =
@@ -384,7 +386,9 @@ std::unique_ptr<WebSharedWorker> WebSharedWorker::CreateAndStart(
         coep_reporting_observer,
     CrossVariantMojoReceiver<mojom::blink::ReportingObserverInterfaceBase>
         dip_reporting_observer,
-    std::optional<blink::NoiseToken> canvas_noise_token) {
+    std::optional<blink::NoiseToken> canvas_noise_token,
+    CrossVariantMojoReceiver<mojom::CanvasNoiseTokenUpdaterInterfaceBase>
+        canvas_noise_token_observer) {
   auto worker =
       base::WrapUnique(new WebSharedWorkerImpl(token, std::move(host), client));
   worker->StartWorkerContext(
@@ -397,7 +401,7 @@ std::unique_ptr<WebSharedWorker> WebSharedWorker::CreateAndStart(
       std::move(policy_container), std::move(web_worker_fetch_context),
       ukm_source_id, require_cross_site_request_for_cookies,
       std::move(coep_reporting_observer), std::move(dip_reporting_observer),
-      std::move(canvas_noise_token));
+      std::move(canvas_noise_token), std::move(canvas_noise_token_observer));
   return worker;
 }
 
