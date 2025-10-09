1be62fa3cb17726672622947bad0281a45f97e90
Patrick Noland <pnoland@google.com>
2025-10-09 09:56:34 -0700

    Add aim toggle only variant of multimodal omnibox feature
    Bug: 450029115
    Change-Id: I3ccdf36904100b32a11879532a4f488ff21aefd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7018197
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1527596}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -2017,6 +2017,13 @@ const FeatureEntry::FeatureVariation
          std::size(kOmniboxRemoveSearchReadyOmniboxNoMatchParam), nullptr}};
 #endif
 
+const FeatureEntry::FeatureParam kOmniboxAimToggleOnlyParam[] = {
+    {"aim_toggle_only", "true"}};
+
+const FeatureEntry::FeatureVariation kOmniboxMultimodalInputVariants[] = {
+    {"AIM Toggle Only", kOmniboxAimToggleOnlyParam,
+     std::size(kOmniboxAimToggleOnlyParam), nullptr}};
+
 const FeatureEntry::FeatureParam
     kOmniboxImprovementForLFFVariationsSwitchToTabChip[] = {
         {OmniboxFieldTrial::kOmniboxImprovementForLFFSwitchToTabChip.name,
@@ -7103,7 +7110,9 @@ const FeatureEntry kFeatureEntries[] = {
 
     {"omnibox-multimodal-input", flag_descriptions::kOmniboxMultimodalInputName,
      flag_descriptions::kOmniboxMultimodalInputDescription, kOsAndroid,
-     FEATURE_VALUE_TYPE(omnibox::kOmniboxMultimodalInput)},
+     FEATURE_WITH_PARAMS_VALUE_TYPE(omnibox::kOmniboxMultimodalInput,
+                                    kOmniboxMultimodalInputVariants,
+                                    "OmniboxMultiModalInput")},
 
     {"omnibox-remove-search-ready-omnibox",
      flag_descriptions::kOmniboxRemoveSearchReadyOmniboxName,
