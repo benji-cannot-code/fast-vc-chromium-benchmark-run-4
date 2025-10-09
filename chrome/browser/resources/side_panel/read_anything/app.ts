9bba9cd91062f2e8261b9059fbacac43b01d6ca7
Lauren Winston <lwinston@google.com>
2025-10-08 18:57:21 -0700

    [Read aloud] Fix keyboard shortcut bug from bracket placement
    An incorrect bracket placement was incorrectly introduced in crrev.com/c/6973133
    
    Fixed: b/450103924
    Change-Id: I6a88beb9211818fec149cf4cf7f8a8332d68d26f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7017828
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1527284}

--- chrome/browser/resources/side_panel/read_anything/app.ts
+++ chrome/browser/resources/side_panel/read_anything/app.ts
@@ -509,8 +509,9 @@ export class AppElement extends AppElementBase implements SpeechListener,
   protected onKeyDown_(e: KeyboardEvent) {
     if (e.key === 'k') {
       e.stopPropagation();
+      e.preventDefault();
+      this.speechController_.onPlayPauseKeyPress(this.$.container);
     }
-    this.speechController_.onPlayPauseKeyPress(this.$.container);
   }
 }
 
