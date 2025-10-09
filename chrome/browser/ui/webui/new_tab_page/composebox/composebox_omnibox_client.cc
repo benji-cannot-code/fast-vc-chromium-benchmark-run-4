52a172fdba2c290f7b6a8ef9d754ec0bb8914aca
Marlon Facey <mfacey@chromium.org>
2025-10-09 08:22:56 -0700

    [NTP Composebox] Use query of destination url to navigate
    Navigation did not account for typed suggest as of yet. This updates
    the client to accommodate typed suggest by using the "q" param in the
    destination url to navigate instead of entered text/match's
    fillIntoEdit.
    
    Bug: 450301316
    Change-Id: I5bde2c35fc59100f59047671d7962887ac5b7a67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7023549
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Auto-Submit: Marlon Facey <mfacey@chromium.org>
    Commit-Queue: Nihar Majmudar <niharm@google.com>
    Reviewed-by: Nihar Majmudar <niharm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1527541}

--- chrome/browser/ui/webui/new_tab_page/composebox/composebox_omnibox_client.cc
+++ chrome/browser/ui/webui/new_tab_page/composebox/composebox_omnibox_client.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/autocomplete_match.h"
 #include "components/omnibox/composebox/composebox_query_controller.h"
 #include "components/omnibox/composebox/contextual_session_service.h"
+#include "net/base/url_util.h"
 #include "ui/webui/resources/cr_components/composebox/composebox.mojom.h"
 
 namespace composebox {
@@ -48,11 +49,10 @@ void ComposeboxOmniboxClient::OnAutocompleteAccept(
     const AutocompleteMatch& alternative_nav_match) {
   const std::map<std::string, std::string>& additional_params =
       lens::GetParametersMapWithoutQuery(destination_url);
-  // Use text for regular query (verbatim match) or use
-  // matches input text for dropdown matches.
-  composebox_handler_->SubmitQuery(
-      base::UTF16ToUTF8(text.empty() ? match.fill_into_edit : text),
-      disposition, additional_params);
+
+  std::string query_text;
+  net::GetValueForKeyInQuery(destination_url, "q", &query_text);
+  composebox_handler_->SubmitQuery(query_text, disposition, additional_params);
 }
 
 std::optional<lens::proto::LensOverlaySuggestInputs>
