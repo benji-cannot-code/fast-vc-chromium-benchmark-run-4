d14c629dd4acfa4076105daacd6b9232d8e874a9
Nicola Tommasi <tommasin@chromium.org>
2025-10-09 01:38:18 -0700

    [AI-Fixit][Settings]: Import loadTimeData from settings.js
    Standardize loadTimeData import in WebUI Settings tests to prevent
    silent data population failures.
    
    Bug: 382645171
    Change-Id: I99b0917b833cc62e77f7498406a32e6c7d19c542
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7021578
    Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    Auto-Submit: Nicola Tommasi <tommasin@chromium.org>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1527385}

--- chrome/test/data/webui/settings/sound_page_test.ts
+++ chrome/test/data/webui/settings/sound_page_test.ts
@@ -5,11 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // clang-format off
 import {webUIListenerCallback} from 'chrome://resources/js/cr.js';
-import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import type {SoundPageElement} from 'chrome://settings/lazy_load.js';
 import {SiteSettingsPrefsBrowserProxyImpl} from 'chrome://settings/lazy_load.js';
 import type {SettingsToggleButtonElement} from 'chrome://settings/settings.js';
-import {resetRouterForTesting} from 'chrome://settings/settings.js';
+import {loadTimeData, resetRouterForTesting} from 'chrome://settings/settings.js';
 import {assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
 
