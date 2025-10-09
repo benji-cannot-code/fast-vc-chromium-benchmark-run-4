3b331f629e1d2a14000229edbe2d41f091771315
Fredrik Söderquist <fs@opera.com>
2025-10-09 09:58:55 -0700

    Remove unused CanvasimageSource predicates
    The IsCSSImageValue() and IsSVGSource() predicate functions are never
    called.
    
    Change-Id: I2c64ed93b784710d6e9439c08cc5564465640874
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7026091
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1527600}

--- third_party/blink/renderer/core/html/canvas/image_element_base.cc
+++ third_party/blink/renderer/core/html/canvas/image_element_base.cc
@@ -48,10 +48,6 @@ mojom::blink::PreferredColorScheme ImageElementBase::PreferredColorScheme()
       style);
 }
 
-bool ImageElementBase::IsSVGSource() const {
-  return CachedImage() && IsA<SVGImage>(CachedImage()->GetImage());
-}
-
 bool ImageElementBase::IsImageElement() const {
   return CachedImage() && !IsA<SVGImage>(CachedImage()->GetImage());
 }
