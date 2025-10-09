a2940acbbfbf70026b8d68a7450b2a171542092c
Mingyu Lei <leimy@chromium.org>
2025-10-08 22:26:38 -0700

    WaaP: implement mock up of the reload button
    Background of the project: we are planning to productionalize the Webium
    and eventually replace the entire top chrome by a web view (instead of
    c++ view). This chain of CLs aim to implement a web view version of a
    small component (reload button) and collect traces and metrics for
    further analysis and improvement. More details can be found from this
    document:
    https://docs.google.com/document/d/1sV7aiVoAZBa28FRtxYNrajOGnhRubJVJVtc2Chfka2o/edit?usp=sharing&resourcekey=0-v1C-IR3njYRCfduHeD72bg
    (googlers only)
    
    This CL adds the reload button as a placeholder, it only displays the
    button's content with out the actual reload functionality implemented.
    
    Bug: 444358999
    Change-Id: I06444ce00cccfc5ea72bad1e46055f2bd6661637
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6938788
    Reviewed-by: Teresa Mao <temao@chromium.org>
    Commit-Queue: Mingyu Lei <leimy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1527332}

--- chrome/browser/resources/reload_button/app.html.ts
+++ chrome/browser/resources/reload_button/app.html.ts
@@ -3,9 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// TODO(crbug.com/444358999): implement the reload button
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
 import type {ReloadButtonAppElement} from './app.js';
 
 export function getHtml(this: ReloadButtonAppElement) {
-  return ``;
+  return html`<!--_html_template_start_-->
+<cr-icon-button class="${this.reloadOrStopIcon_}"
+    @click="${this.onReloadOrStopClick_}">
+</cr-icon-button>
+<!--_html_template_end_-->`;
 }
--- chrome/browser/resources/reload_button/app.ts
+++ chrome/browser/resources/reload_button/app.ts
@@ -3,10 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// TODO(crbug.com/444358999): implement the reload button
+import '//resources/cr_elements/cr_button/cr_button.js';
+import '//resources/cr_elements/cr_icon_button/cr_icon_button.js';
+import '//resources/cr_elements/icons.html.js';
 
 import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
 
+import {getCss} from './app.css.js';
 import {getHtml} from './app.html.js';
 
 export class ReloadButtonAppElement extends CrLitElement {
@@ -14,8 +17,31 @@ export class ReloadButtonAppElement extends CrLitElement {
     return 'reload-button-app';
   }
 
+  static override get styles() {
+    return getCss();
+  }
+
   override render() {
     return getHtml.bind(this)();
+  }
+
+  static override get properties() {
+    return {
+      reloadOrStopIcon_: {state: true, type: String},
+    };
+  }
+
+  protected accessor reloadOrStopIcon_: string = 'icon-refresh';
+
+  protected setReloadStopState(isLoading: boolean) {
+    this.reloadOrStopIcon_ = isLoading ? 'icon-clear' : 'icon-refresh';
+  }
+
+  // TODO(crbug.com/444358999): implement the reload logic
+  protected onReloadOrStopClick_(_: Event) {
+    this.reloadOrStopIcon_ = this.reloadOrStopIcon_ === 'icon-refresh' ?
+        'icon-clear' :
+        'icon-refresh';
   }
 }
 
