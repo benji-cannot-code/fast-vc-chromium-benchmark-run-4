3cd663943f40685a52a24a215b5b5f7b583a0d58
Tom Anderson <thomasanderson@chromium.org>
2025-10-09 07:27:19 -0700

    Trim os_crypt/sync visibility list
    Dependencies on sync os_crypt have been removed as part of work on bug
    447372315. This CL minimizes the visibility list after those changes.
    
    R=thestig
    
    Change-Id: I20efd5bc6216077da9a39c06269b1abbb7300d90
    Bug: 447372315
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7023196
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1527510}

--- content/browser/service_host/utility_process_host.cc
+++ content/browser/service_host/utility_process_host.cc
@@ -57,10 +57,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/mojom/network_service.mojom.h"
 #endif
 
-#if BUILDFLAG(IS_MAC)
-#include "components/os_crypt/sync/os_crypt_switches.h"
-#endif
-
 #if BUILDFLAG(IS_OZONE)
 #include "ui/ozone/public/ozone_switches.h"
 #endif
