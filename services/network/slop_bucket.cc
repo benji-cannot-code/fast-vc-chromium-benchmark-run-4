1261d49c8033bf3962d37f279d948b354812545e
Yoichi Osato <yoichio@chromium.org>
2025-10-08 21:51:32 -0700

    Add new UMA for SlopBucket
    This CL adds new UMA and feature for SlopBucket considering priority and
    cache.
    This CL also adds parameters to fieldtrial_testing_config.json for
    new finch arms.
    
    Bug: 406416750
    Change-Id: I4a9dfe08a0c69cf0ff4cb2ec5c35da355c622aa7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7017872
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1527321}

--- services/network/slop_bucket.cc
+++ services/network/slop_bucket.cc
@@ -117,6 +117,10 @@ constexpr base::FeatureParam<base::MemoryPressureLevel>
                                      base::MEMORY_PRESSURE_LEVEL_MODERATE,
                                      &kMemoryPressureLevelOptions);
 
+// "enabled_for_cache_response" parameter.
+constexpr base::FeatureParam<bool>
+    kEnabledForCacheresponse(&kSlopBucket, "enabled_for_cache_response", true);
+
 }  // namespace
 
 // This class encapsulates the runtime configuration of SlopBucket. When
@@ -135,6 +139,9 @@ class SlopBucket::Configuration {
   size_t max_chunks_total() const { return max_chunks_total_; }
   base::MemoryPressureLevel memory_pressure_disable_level() const {
     return memory_pressure_disable_level_;
+  }
+  bool enabled_for_cache_response() const {
+    return enabled_for_cache_response_;
   }
 
  private:
@@ -188,6 +195,8 @@ class SlopBucket::Configuration {
           "max_chunks_total is less than max_chunks_per_request");
     }
     memory_pressure_disable_level_ = kMemoryPressureDisableLevelParam.Get();
+
+    enabled_for_cache_response_ = kEnabledForCacheresponse.Get();
   }
 
   void DisableWithWarning(const char* warning) {
@@ -210,6 +219,8 @@ class SlopBucket::Configuration {
 
   base::MemoryPressureLevel memory_pressure_disable_level_ =
       kDefaultMemoryPressureDisableLevel;
+
+  bool enabled_for_cache_response_ = true;
 };
 
 // SlopBucketManager tracks global state for SlopBuckets. It is thread-hostile.
@@ -268,6 +279,10 @@ class SlopBucket::Manager {
 
   size_t chunk_size() const { return configuration_.chunk_size(); }
   size_t min_buffer_size() const { return configuration_.min_buffer_size(); }
+
+  bool enabled_for_cache_response() const {
+    return configuration_.enabled_for_cache_response();
+  }
 
   scoped_refptr<ChunkIOBuffer> RequestChunk(size_t existing_chunks) {
     DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
@@ -512,6 +527,16 @@ std::unique_ptr<SlopBucket> SlopBucket::RequestSlopBucket(
     DVLOG(1) << "Refused to create SlopBucket for request to '"
              << for_request->url() << "' with priority "
              << net::RequestPriorityToString(priority);
+    return nullptr;
+  }
+
+  using CacheEntryStatus = net::HttpResponseInfo::CacheEntryStatus;
+  const CacheEntryStatus& cache_entry_status =
+      for_request->response_info().cache_entry_status;
+  const bool is_response_from_cache =
+      cache_entry_status == CacheEntryStatus::ENTRY_USED ||
+      cache_entry_status == CacheEntryStatus::ENTRY_VALIDATED;
+  if (is_response_from_cache && !manager.enabled_for_cache_response()) {
     return nullptr;
   }
 
