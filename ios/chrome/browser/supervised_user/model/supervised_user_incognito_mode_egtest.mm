9473fd5f5004339b0cc27dfe833faea2cf91da6a
Asami Doi <asamidoi@chromium.org>
2025-10-09 02:52:40 -0700

    [ios] Enable kEnableNTPViewHierarchyRepair in SupervisedUserIncognitoModeTestCase
    Enable kEnableNTPViewHierarchyRepair in
    SupervisedUserIncognitoModeTestCase, otherwise
    testTabGridIncognitoDisabled test fails with the
    UIViewControllerHierarchyInconsistency exception.
    
    The test failure can be reproducible with the fix to a snapshot
    (crrev.com/7003272). Using a newer API for a snapshot
    (drawViewHierarchyInRect) triggers the view hierarchy check.
    
    Bug: 40799579
    Change-Id: I0a18dbf2d88dcc4810348d03d4b99230e3c11c82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7015092
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1527409}

--- ios/chrome/browser/supervised_user/model/supervised_user_incognito_mode_egtest.mm
+++ ios/chrome/browser/supervised_user/model/supervised_user_incognito_mode_egtest.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/authentication/test/signin_earl_grey.h"
 #import "ios/chrome/browser/authentication/test/signin_earl_grey_ui_test_util.h"
 #import "ios/chrome/browser/metrics/model/metrics_app_interface.h"
+#import "ios/chrome/browser/ntp/ui_bundled/new_tab_page_feature.h"
 #import "ios/chrome/browser/policy/model/policy_earl_grey_matchers.h"
 #import "ios/chrome/browser/popup_menu/ui_bundled/popup_menu_constants.h"
 #import "ios/chrome/browser/signin/model/fake_system_identity.h"
@@ -52,6 +53,12 @@ id<GREYMatcher> SupervisedIncognitoMessage() {
 @end
 
 @implementation SupervisedUserIncognitoModeTestCase
+
+- (AppLaunchConfiguration)appConfigurationForTestCase {
+  AppLaunchConfiguration config = [super appConfigurationForTestCase];
+  config.features_enabled.push_back(kEnableNTPViewHierarchyRepair);
+  return config;
+}
 
 - (void)setupAndRegisterHistogramTester {
   chrome_test_util::GREYAssertErrorNil(
