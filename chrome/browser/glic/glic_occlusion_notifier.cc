600de5c2a2d86ad10cc58322539cef0d5fe041e7
Anthony Cui <cuianthony@google.com>
2025-10-09 10:22:28 -0700

    Update occlusion tracker when GlicFloatingUi is created/destroyed
    Bug: 450304610
    Change-Id: Ib0f4b8589d65bb366ed28725c950dff5fd9a56fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7023970
    Commit-Queue: Anthony Cui <cuianthony@chromium.org>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1527618}

--- chrome/browser/glic/glic_occlusion_notifier.cc
+++ chrome/browser/glic/glic_occlusion_notifier.cc
@@ -5,9 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/glic/glic_occlusion_notifier.h"
 
+#include "base/feature_list.h"
 #include "chrome/browser/glic/widget/glic_widget.h"
 #include "chrome/browser/picture_in_picture/picture_in_picture_occlusion_tracker.h"
 #include "chrome/browser/picture_in_picture/picture_in_picture_window_manager.h"
+#include "chrome/common/chrome_features.h"
 #include "ui/views/widget/widget.h"
 
 namespace glic {
@@ -25,6 +27,12 @@ GlicOcclusionNotifier::~GlicOcclusionNotifier() {
 void GlicOcclusionNotifier::PanelStateChanged(
     const mojom::PanelState& panel_state,
     const GlicWindowController::PanelStateContext& context) {
+  // Under GlicMultiInstance, occlusion tracking is managed through
+  // GlicFloatingUi.
+  if (base::FeatureList::IsEnabled(features::kGlicMultiInstance)) {
+    return;
+  }
+
   PictureInPictureOcclusionTracker* tracker =
       PictureInPictureWindowManager::GetInstance()->GetOcclusionTracker();
   if (!window_controller_->IsDetached() || !tracker) {
