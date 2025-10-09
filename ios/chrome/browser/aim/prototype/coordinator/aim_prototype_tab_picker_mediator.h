aae4e0a80667fddd0b680a601077a9c3bcce0718
Ameur Hosni <ameurhosni@google.com>
2025-10-09 08:11:51 -0700

    [IOS][AIM] Prepare tab picker files for AIM
    This CL prepares strandard coordinator/mediator/VC files for aim tab
    picker.
    
    Bug: 449132198
    Change-Id: I808ec44675dd307807626dfaf4558d769904f826
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7012687
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1527533}

---
+++ ios/chrome/browser/aim/prototype/coordinator/aim_prototype_tab_picker_mediator.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_AIM_PROTOTYPE_COORDINATOR_AIM_PROTOTYPE_TAB_PICKER_MEDIATOR_H_
+#define IOS_CHROME_BROWSER_AIM_PROTOTYPE_COORDINATOR_AIM_PROTOTYPE_TAB_PICKER_MEDIATOR_H_
+
+#import "ios/chrome/browser/tab_switcher/tab_grid/base_grid/coordinator/base_grid_mediator.h"
+
+// The tab picker mediator for AIM.
+@interface AimPrototypeTabPickerMediator : BaseGridMediator
+
+- (instancetype)initWithGridConsumer:(id<TabCollectionConsumer>)gridConsumer;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_AIM_PROTOTYPE_COORDINATOR_AIM_PROTOTYPE_TAB_PICKER_MEDIATOR_H_
---
+++ ios/chrome/browser/aim/prototype/ui/aim_prototype_tab_picker_view_controller.mm
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/aim/prototype/ui/aim_prototype_tab_picker_view_controller.h"
+
+#import "ios/chrome/browser/tab_switcher/tab_grid/base_grid/ui/base_grid_view_controller.h"
+
+@implementation AimPrototypeTabPickerViewController
+
+- (instancetype)init {
+  self = [super init];
+
+  if (self) {
+    _gridViewController = [[BaseGridViewController alloc] init];
+    _gridViewController.theme = GridThemeLight;
+    [_gridViewController setTabGridMode:TabGridMode::kSelection];
+  }
+
+  return self;
+}
+
+@end
