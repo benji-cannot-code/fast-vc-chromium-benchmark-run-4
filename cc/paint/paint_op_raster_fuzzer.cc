612989f9936fbd5a65cde94c4c68c1ef24d94ec4
Colin Blundell <blundell@chromium.org>
2025-10-09 00:00:33 -0700

    [//cc] Remove unneeded BindToCurrentSequence() call
    The callsites of this method make the call [1].
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:cc/paint/paint_op_raster_fuzzer.cc;l=160-171?q=paint_op_raster_fuzz&ss=chromium
    
    Bug: 391648152
    Change-Id: Ib16a39d58eb909133aff565a7cafa88a9821e4dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7021791
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1527356}

--- cc/paint/paint_op_raster_fuzzer.cc
+++ cc/paint/paint_op_raster_fuzzer.cc
@@ -86,7 +86,6 @@ void Raster(scoped_refptr<viz::TestContextProvider> context_provider,
 
   SkImageInfo image_info = SkImageInfo::MakeN32(
       kRasterDimension, kRasterDimension, kOpaque_SkAlphaType);
-  context_provider->BindToCurrentSequence();
   sk_sp<SkSurface> surface = SkSurfaces::RenderTarget(
       context_provider->GrContext(), skgpu::Budgeted::kYes, image_info);
   SkCanvas* canvas = surface->getCanvas();
