67a6fe628d1b615aa77c1d22cd864dfc082e586d
Nick Birnie <birnie@google.com>
2025-10-09 15:38:06 -0700

    [glic] Add detailed close reasons for Glic FRE
    The Glic.Fre.WidgetClosedReason histogram had a high volume of entries in the "Unspecified" bucket, which made it difficult to diagnose why the First Run Experience was being closed.
    
    This was caused by GlicFreController::OnTabShowingModalWillDetach logging two distinct actions (closing the host tab and moving the host tab) with views::Widget::ClosedReason::kUnspecified.
    
    This change introduces a new, more detailed histogram, Glic.Fre.WidgetClosedReason2, with a corresponding GlicFreWidgetClosedReason enum. This new enum adds kHostTabClosed and kHostTabMoved to provide more granular insight into why the FRE widget is closed.
    
    Bug: b:437970065
    Change-Id: Ib69cc9cf944443ca3295de4c4a69e9030061063e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7023811
    Reviewed-by: Andrea Gomez <andreaxg@google.com>
    Commit-Queue: Nick Birnie <birnie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1527809}

--- chrome/browser/glic/fre/glic_fre_controller_browsertest.cc
+++ chrome/browser/glic/fre/glic_fre_controller_browsertest.cc
@@ -267,8 +267,8 @@ IN_PROC_BROWSER_TEST_F(GlicFreControllerBrowserTest,
 
   WaitForFreClose();
   histogram_tester_.ExpectUniqueSample(
-      "Glic.Fre.WidgetClosedReason",
-      /*sample=*/views::Widget::ClosedReason::kUnspecified,
+      "Glic.Fre.WidgetClosedReason2",
+      /*sample=*/glic::GlicFreWidgetClosedReason::kHostTabClosed,
       /*expected_bucket_count=*/1);
 }
 
@@ -283,6 +283,10 @@ IN_PROC_BROWSER_TEST_F(GlicFreControllerBrowserTest, FreAcceptance) {
   EXPECT_EQ(user_action_tester_.GetActionCount("Glic.Fre.Accept"), 1);
   WaitForFreClose();
   WaitForGlicPanelShow();
+  histogram_tester_.ExpectUniqueSample(
+      "Glic.Fre.WidgetClosedReason2",
+      /*sample=*/glic::GlicFreWidgetClosedReason::kAcceptButtonClicked,
+      /*expected_bucket_count=*/1);
 }
 
 IN_PROC_BROWSER_TEST_F(GlicFreControllerBrowserTest, DoNotCrashOnBrowserClose) {
@@ -293,8 +297,8 @@ IN_PROC_BROWSER_TEST_F(GlicFreControllerBrowserTest, DoNotCrashOnBrowserClose) {
 
   chrome::CloseAllBrowsers();
   histogram_tester_.ExpectUniqueSample(
-      "Glic.Fre.WidgetClosedReason",
-      /*sample=*/views::Widget::ClosedReason::kUnspecified,
+      "Glic.Fre.WidgetClosedReason2",
+      /*sample=*/glic::GlicFreWidgetClosedReason::kHostTabClosed,
       /*expected_bucket_count=*/1);
 }
 
@@ -329,6 +333,28 @@ IN_PROC_BROWSER_TEST_F(GlicFreControllerBrowserTest,
 
   EXPECT_EQ(user_action_tester_.GetActionCount("Glic.Fre.LoadingPanelClosed"),
             1);
+  histogram_tester_.ExpectUniqueSample(
+      "Glic.Fre.WidgetClosedReason2",
+      /*sample=*/glic::GlicFreWidgetClosedReason::kHostTabClosed,
+      /*expected_bucket_count=*/1);
+}
+
+IN_PROC_BROWSER_TEST_F(GlicFreControllerBrowserTest, FreRejection) {
+  // Open the FRE dialog in a tab.
+  glic_fre_controller().ShowFreDialog(
+      browser(), mojom::InvocationSource::kTopChromeButton);
+  WaitForFreShow();
+
+  // Reject the FRE and confirm it closed.
+  glic_fre_controller().RejectFre();
+  EXPECT_EQ(user_action_tester_.GetActionCount("Glic.Fre.NoThanks"), 1);
+  WaitForFreClose();
+
+  // Verify the close reason was logged correctly.
+  histogram_tester_.ExpectUniqueSample(
+      "Glic.Fre.WidgetClosedReason2",
+      /*sample=*/glic::GlicFreWidgetClosedReason::kCancelButtonClicked,
+      /*expected_bucket_count=*/1);
 }
 
 }  // namespace
