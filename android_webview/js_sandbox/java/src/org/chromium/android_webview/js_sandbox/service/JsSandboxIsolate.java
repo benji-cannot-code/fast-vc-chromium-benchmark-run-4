46f1c3cd5105dba6135e1fceb4923aaa8cbc8c5f
Brandon Fong <bjfong@google.com>
2025-10-09 11:10:52 -0700

    Revert "Roll src/third_party/androidx_javascriptengine/src/ e3b6fc680..7539442db (6 commits)"
    This reverts commit 2d1c40943c7f01a87e3ce493a77d846b970e7b16.
    
    Reason for revert: Builder failure, first failure: https://ci.chromium.org/ui/p/chrome/builders/ci/android-desktop-x64-compile-all-rel/3920/overview
    
    Original change's description:
    > Roll src/third_party/androidx_javascriptengine/src/ e3b6fc680..7539442db (6 commits)
    >
    > A new aidl interface introduced with this roll is stubbed out to build the code successfully.
    >
    > https://chromium.googlesource.com/aosp/platform/frameworks/support/javascriptengine/javascriptengine/src.git/+log/e3b6fc680728..7539442db1dc
    >
    > $ git log e3b6fc680..7539442db --date=short --no-merges --format='%ad %ae %s'
    > 2025-10-07 alexmirzea Merge "Add MessagePort(postMessage) API for JavaScriptEngine." into androidx-main
    > 2025-10-03 alexmirzea Merge "Add SequencedExecutor for ordered task execution." into androidx-main
    > 2025-10-01 abhijithnair Merge "Disable 100% failing WebViewJavaScriptSandboxTest" into androidx-main
    > 2025-04-29 pbirk Merge "Disable test failing on SDK 26" into androidx-main
    > 2025-04-10 android-test-infra-autosubmit Merge "Reduce the heap size used for testing OOM" into androidx-main
    > 2025-03-17 android-test-infra-autosubmit Merge "Temporarily disabling JavaScriptEngine OOM tests" into androidx-main
    >
    > Created with:
    >   roll-dep src/third_party/androidx_javascriptengine/src
    >
    > Bug: 435619571
    > Change-Id: I2683cadb1d7474985ad7755b5edfcb7bc3cdb43f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7021677
    > Commit-Queue: Alex Mîrzea <alexmirzea@google.com>
    > Reviewed-by: Ashley Newson <ashleynewson@chromium.org>
    > Reviewed-by: Peter Beverloo <peter@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1527559}
    
    Bug: 435619571
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ic4d17c7d70ff0a0ea129743db6f9b01c4c8a64e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7027055
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1527657}

--- android_webview/js_sandbox/java/src/org/chromium/android_webview/js_sandbox/service/JsSandboxIsolate.java
+++ android_webview/js_sandbox/java/src/org/chromium/android_webview/js_sandbox/service/JsSandboxIsolate.java
@@ -20,7 +20,6 @@ import org.chromium.android_webview.js_sandbox.common.IJsSandboxIsolate;
 import org.chromium.android_webview.js_sandbox.common.IJsSandboxIsolateCallback;
 import org.chromium.android_webview.js_sandbox.common.IJsSandboxIsolateClient;
 import org.chromium.android_webview.js_sandbox.common.IJsSandboxIsolateSyncCallback;
-import org.chromium.android_webview.js_sandbox.common.IMessagePort;
 import org.chromium.base.Log;
 import org.chromium.build.annotations.Contract;
 import org.chromium.build.annotations.NullMarked;
@@ -256,11 +255,6 @@ public class JsSandboxIsolate extends IJsSandboxIsolate.Stub {
 
     public static void initializeEnvironment() {
         JsSandboxIsolateJni.get().initializeEnvironment();
-    }
-
-    @Override
-    public IMessagePort provideMessagePort(String name, IMessagePort port) {
-        throw new UnsupportedOperationException();
     }
 
     @NativeMethods
