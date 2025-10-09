1b51ea059ccab1a33fa449bd5270beda0ea43905
Moe Ahmadi <mahmadi@chromium.org>
2025-10-09 08:41:04 -0700

    [omnibox][zps] Remove crash keys and dump without crashing
    Bug: 447939867
    Change-Id: I9304286df442a8d6d5fe4424fd1acceeb6c44338
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7025392
    Auto-Submit: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1527548}

--- components/omnibox/browser/autocomplete_grouper_sections.cc
+++ components/omnibox/browser/autocomplete_grouper_sections.cc
@@ -11,8 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/contains.h"
 #include "base/dcheck_is_on.h"
-#include "base/debug/crash_logging.h"
-#include "base/debug/dump_without_crashing.h"
 #include "base/notreached.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
@@ -142,13 +140,6 @@ void ZpsSection::InitFromMatches(ACMatches& matches) {
             return transformed;
           }(),
           ", ");
-      SCOPED_CRASH_KEY_STRING32("ZpsSection", "match-type", match_type);
-      SCOPED_CRASH_KEY_STRING32("ZpsSection", "match-group-id", match_group_id);
-      SCOPED_CRASH_KEY_STRING32("ZpsSection", "match-relevance",
-                                match_relevance);
-      SCOPED_CRASH_KEY_STRING32("ZpsSection", "group-description",
-                                group_description);
-      base::debug::DumpWithoutCrashing();
 #if DCHECK_IS_ON()
       NOTREACHED() << "Match with type " << match_type << " and group id "
                    << match_group_id << " and relevance " << match_relevance
