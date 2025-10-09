0f6d18cbf01a09bd3818448f268fc7efb27e0b68
Andrew Grieve <agrieve@chromium.org>
2025-10-09 06:51:57 -0700

    Android: Remove Shadow for ContextMenuHeaderCoordinator
    Bug: 341267427
    Change-Id: I947a820375600324cb3122f3ac44f37c0fb531b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7024673
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1527488}

--- chrome/android/junit/src/org/chromium/chrome/browser/contextmenu/ContextMenuCoordinatorTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/contextmenu/ContextMenuCoordinatorTest.java
@@ -128,19 +128,6 @@ public class ContextMenuCoordinatorTest {
         }
     }
 
-    /** No-op constructor for test cases that does not care of creation of real object. */
-    @Implements(ContextMenuHeaderCoordinator.class)
-    public static class ShadowContextMenuHeaderCoordinator {
-        public ShadowContextMenuHeaderCoordinator() {}
-
-        @Implementation
-        public void __constructor__(
-                Activity activity,
-                ContextMenuParams params,
-                Profile profile,
-                ContextMenuNativeDelegate nativeDelegate) {}
-    }
-
     /** Helper shadow to set the results for {@link Profile#fromWebContents}. */
     @Implements(Profile.class)
     public static class ShadowProfile {
@@ -178,6 +165,7 @@ public class ContextMenuCoordinatorTest {
         mActivityScenarioRule.getScenario().onActivity((activity) -> mActivity = activity);
         mCoordinator = new ContextMenuCoordinator(TOP_CONTENT_OFFSET_PX, mNativeDelegate);
         ShadowProfile.sProfileFromWebContents = mProfile;
+        ContextMenuHeaderCoordinator.setDisableForTesting(true);
     }
 
     @Test
@@ -294,11 +282,7 @@ public class ContextMenuCoordinatorTest {
     @DisabledTest(message = "crbug.com/1444964")
     @DisableFeatures(ContentFeatures.TOUCH_DRAG_AND_CONTEXT_MENU)
     @Config(
-            shadows = {
-                ShadowContextMenuDialog.class,
-                ShadowContextMenuHeaderCoordinator.class,
-                ShadowProfile.class
-            },
+            shadows = {ShadowContextMenuDialog.class, ShadowProfile.class},
             qualifiers = "mdpi")
     public void testDisplayMenu() {
         final int triggeringTouchXDp = 100;
@@ -331,11 +315,7 @@ public class ContextMenuCoordinatorTest {
     @DisabledTest(message = "crbug.com/1444964")
     @EnableFeatures({ContentFeatures.TOUCH_DRAG_AND_CONTEXT_MENU})
     @Config(
-            shadows = {
-                ShadowContextMenuDialog.class,
-                ShadowContextMenuHeaderCoordinator.class,
-                ShadowProfile.class
-            },
+            shadows = {ShadowContextMenuDialog.class, ShadowProfile.class},
             qualifiers = "mdpi")
     @CommandLineFlags.Add(ContextMenuSwitches.FORCE_CONTEXT_MENU_POPUP)
     public void testDisplayMenu_DragEnabled() {
