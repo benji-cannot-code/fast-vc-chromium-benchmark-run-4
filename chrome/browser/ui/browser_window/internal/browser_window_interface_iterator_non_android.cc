4c800e292bfd7d293e36027b7ebb5f833133230a
Glenn Hartmann <hartmanng@chromium.org>
2025-10-09 06:42:57 -0700

    Prevent GetLastActiveBrowserWindowInterfaceWithAnyProfile() from returning a browser that is pending deletion.
    This is to prevent potential dangling pointer crashes - for example,
    crbug.com/448384940 (see
    https://chromium-review.googlesource.com/c/chromium/src/+/7014089).
    
    Bug: 448384940
    Change-Id: I6a6a696476e90b34e1c6d1db4ba4105b2bbcdc97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7022215
    Commit-Queue: Glenn Hartmann <hartmanng@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1527479}

--- chrome/browser/ui/browser_window/internal/browser_window_interface_iterator_non_android.cc
+++ chrome/browser/ui/browser_window/internal/browser_window_interface_iterator_non_android.cc
@@ -17,6 +17,9 @@ std::vector<BrowserWindowInterface*> GetAllBrowserWindowInterfaces() {
   return results;
 }
 
+// TODO(crbug.com/431671320): This is implemented in terms of BrowserList to
+// ensure it stays in sync with other BrowserList APIs during migration. It
+// can be implemented directly once clients are migrated off of BrowserList.
 void ForEachCurrentBrowserWindowInterfaceOrderedByActivation(
     base::FunctionRef<bool(BrowserWindowInterface*)> on_browser) {
   // Make a copy of the BrowserList to simplify the case where we need to
@@ -42,6 +45,9 @@ void ForEachCurrentBrowserWindowInterfaceOrderedByActivation(
   }
 }
 
+// TODO(crbug.com/431671320): This is implemented in terms of BrowserList to
+// ensure it stays in sync with other BrowserList APIs during migration. It
+// can be implemented directly once clients are migrated off of BrowserList.
 void ForEachCurrentAndNewBrowserWindowInterfaceOrderedByActivation(
     base::FunctionRef<bool(BrowserWindowInterface*)> on_browser) {
   // Make a copy of the BrowserList to simplify the case where we need to
@@ -68,8 +74,11 @@ void ForEachCurrentAndNewBrowserWindowInterfaceOrderedByActivation(
 }
 
 BrowserWindowInterface* GetLastActiveBrowserWindowInterfaceWithAnyProfile() {
-  // TODO(crbug.com/431671448): This is implemented in terms of BrowserList to
-  // ensure it stays in sync with other BrowserList APIs during migration. It
-  // can be implemented directly once clients are migrated off of BrowserList.
-  return BrowserList::GetInstance()->GetLastActive();
+  BrowserWindowInterface* last_active = nullptr;
+  ForEachCurrentBrowserWindowInterfaceOrderedByActivation(
+      [&](BrowserWindowInterface* browser) {
+        last_active = browser;
+        return false;  // stop iterating
+      });
+  return last_active;
 }
