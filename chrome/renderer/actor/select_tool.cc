922b8c18656818d969e4ff15e4faa66112b36397
David Bokan <bokan@chromium.org>
2025-10-09 09:26:04 -0700

    Actor: Scale coordinates for device scale factor
    The incoming coordinates are in DIPs but the injected input events in
    Blink must use physical coordinates. Scale the coordinates in ToolBase
    and rename the mojom coordinate to make it clear these are DIPs.
    
    Fixed: b:449826348
    Change-Id: I5e1c4e639306a129f5d77e1f17af28d967c6bc5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7024671
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1527580}

--- chrome/renderer/actor/select_tool.cc
+++ chrome/renderer/actor/select_tool.cc
@@ -82,7 +82,7 @@ SelectTool::ValidatedResult SelectTool::Validate() const {
   CHECK(frame_->GetWebFrame());
   CHECK(frame_->GetWebFrame()->FrameWidget());
 
-  if (target_->is_coordinate()) {
+  if (target_->is_coordinate_dip()) {
     NOTIMPLEMENTED() << "Coordinate-based target is not yet supported.";
     return base::unexpected(MakeErrorResult());
   }
