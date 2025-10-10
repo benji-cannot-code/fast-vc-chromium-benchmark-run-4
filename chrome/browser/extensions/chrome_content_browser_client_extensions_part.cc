063082f85012142802e7d007b98d2444d96f00bd
Akihiro Ota <akihiroota@chromium.org>
2025-10-10 09:23:51 -0700

    Accessibility: Update kiosk extension tests to run mv3 ChromeVox
    In this change we parametrize KioskAccessibilityExtensionTest against
    ChromeVox mv2/mv3. We also add the necessary logic to get the tests
    passing in mv3. This change is required to unblock
    https://crrev.com/c/6962374, which adds a finch field trial for
    ChromeVox in manifest v3.
    
    Bug: 388867840
    Change-Id: I553370a5f38213e1a0cd932b8b04568b6874e9c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6986684
    Reviewed-by: Andrea Orru <andreaorru@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1528182}

--- chrome/browser/extensions/chrome_content_browser_client_extensions_part.cc
+++ chrome/browser/extensions/chrome_content_browser_client_extensions_part.cc
@@ -93,6 +93,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IS_CHROMEOS)
 #include "chrome/browser/chromeos/extensions/vpn_provider/vpn_service_factory.h"
 #include "chromeos/constants/chromeos_features.h"
+#include "components/user_manager/user_manager_impl.h"
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
 #if BUILDFLAG(ENABLE_PDF)
@@ -654,7 +655,13 @@ bool ChromeContentBrowserClientExtensionsPart::
   // of e.g. throwing errors in response to installation events (where the
   // worker is registered, but then immediately unregistered).
   if (!registered_version.IsValid()) {
+#if BUILDFLAG(IS_CHROMEOS)
+    // Make an exception for kiosk mode, since kiosk sessions use temporary
+    // profiles, which are discarded when a session ends.
+    return !user_manager::UserManager::Get()->IsLoggedInAsAnyKioskApp();
+#else
     return true;
+#endif
   }
 
   // Don't allow the unregistration of a valid, enabled service worker-based
