d8014d00d88992bfa25e71cfcc76fd137226153d
Andrew Grieve <agrieve@chromium.org>
2025-10-10 10:21:29 -0700

    Android: Fix some new lint warnings for [TextConcatSpace]
    Bug: 450675216
    Change-Id: Ib7e8b6f768e71e02334bf8e6e9d69ba5ef50b110
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7030461
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1528212}

--- android_webview/support_library/java/src/org/chromium/support_lib_glue/SupportLibWebSettingsAdapter.java
+++ android_webview/support_library/java/src/org/chromium/support_lib_glue/SupportLibWebSettingsAdapter.java
@@ -197,7 +197,7 @@ class SupportLibWebSettingsAdapter implements WebSettingsBoundaryInterface {
                 Log.w(
                         TAG,
                         "setAlgorithmicDarkeningAllowed() is a no-op in an app with"
-                                + "targetSdkVersion<T");
+                                + " targetSdkVersion<T");
                 return;
             }
             mAwSettings.setAlgorithmicDarkeningAllowed(allow);
