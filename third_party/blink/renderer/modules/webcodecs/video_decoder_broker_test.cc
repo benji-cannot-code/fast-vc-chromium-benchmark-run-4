eccbcee5ed021077fe520064e7a35c240a68661b
Dale Curtis <dalecurtis@chromium.org>
2025-10-09 18:11:13 -0700

    Restore kResolutionBasedDecoderPriority feature for testing
    Turns out this feature is used by ChromeOS test, so we can't
    remove it. This restores the functionality while keeping it
    enabled for WebCodecs "no-preference"
    
    R=tguilbert
    
    Bug: 368085608
    Change-Id: Iadf1f505c21128fe9237b404590684076e357694
    Fixed: 450026182
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7027375
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1527870}

--- third_party/blink/renderer/modules/webcodecs/video_decoder_broker_test.cc
+++ third_party/blink/renderer/modules/webcodecs/video_decoder_broker_test.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/run_loop.h"
 #include "base/task/single_thread_task_runner.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/threading/thread.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
@@ -415,6 +416,8 @@ TEST_F(VideoDecoderBrokerTest, Init_DenyAcceleration) {
 }
 
 TEST_F(VideoDecoderBrokerTest, Decode_MultipleAccelerationPreferences) {
+  base::test::ScopedFeatureList enabled_features_{
+      media::kResolutionBasedDecoderPriority};
   V8TestingScope v8_scope;
   ExecutionContext* execution_context = v8_scope.GetExecutionContext();
 
