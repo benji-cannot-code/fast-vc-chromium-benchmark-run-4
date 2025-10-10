92185e4b14bbfd7b9c8784565670a301a48f7cff
Kaan Alsan <alsan@chromium.org>
2025-10-10 08:06:08 -0700

    Avoid installing PEH for enterprise or managed devices
    Change-Id: I9a5b8891d0f338bbec3fe0c580c6986405cea0fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6979924
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1528138}

--- chrome/browser/platform_experience/installer/installer_win.cc
+++ chrome/browser/platform_experience/installer/installer_win.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/platform_experience/installer/installer_win.h"
 
 #include "base/command_line.h"
+#include "base/enterprise_util.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/logging.h"
@@ -51,6 +52,9 @@ bool PlatformExperienceHelperMightBeInstalled() {
 // Returns whether we should attempt installing the platform experience helper.
 bool ShouldInstallPlatformExperienceHelper() {
   if (base::win::GetVersion() < base::win::Version::WIN10) {
+    return false;
+  }
+  if (base::IsManagedOrEnterpriseDevice()) {
     return false;
   }
   return !PlatformExperienceHelperMightBeInstalled();
