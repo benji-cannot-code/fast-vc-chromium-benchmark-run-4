b3d42df3565248265e45c0b914b5407460742220
Nic Champagne Williamson <champnic@microsoft.com>
2025-10-10 12:51:18 -0700

    MFClear Deprecation: Remove MediaFoundation rendering FrameServer mode
    This is the first major part of removing MediaFoundation for
    Clear content support. This removes the bulk of the FrameServer
    MF rendering mode, which is only used for clear content. DComp mode
    is used for both MF Clear and MF PlayReady (DRM protected content)
    support.
    
    High-level changes:
    * Removes the feature flags and about flags for choosing rendering mode.
    * Removes the media_foundation_texture_pool and FramePool functions, which were used to provide shared textures for FrameServer mode.
    * Removes media_foundation_rendering_mode and related mojo and functions.
    * Simplifies media_foundation_renderer(_client) to assume DComp mode.
    * Removes OverlayState observation code used for dynamically switching
    between FrameServer and DComp modes.
    * Removes code for reading frames from MF Renderer.
    * Removes rendering code from MF RendererClient.
    
    
    Bug: 448103582
    Change-Id: I45802facb5796affd9ed81959c2c66e2563c30eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7008120
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Sangbaek Park <sangbaekpark@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Commit-Queue: Nic Williamson <champnic@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1528310}

--- media/renderers/win/media_foundation_renderer_extension.h
+++ media/renderers/win/media_foundation_renderer_extension.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/win/scoped_handle.h"
 #include "media/base/media_export.h"
-#include "media/renderers/win/media_foundation_rendering_mode.h"
 #include "ui/gfx/geometry/rect.h"
 
 namespace media {
@@ -37,17 +36,6 @@ class MEDIA_EXPORT MediaFoundationRendererExtension {
   using SetOutputRectCB = base::OnceCallback<void(bool)>;
   virtual void SetOutputRect(const ::gfx::Rect& rect,
                              SetOutputRectCB callback) = 0;
-
-  // Notify that the frame has been displayed and can be reused.
-  virtual void NotifyFrameReleased(
-      const base::UnguessableToken& frame_token) = 0;
-
-  // Request a new frame to be provided to the client.
-  virtual void RequestNextFrame() = 0;
-
-  // Change which mode we are using for video frame rendering.
-  virtual void SetMediaFoundationRenderingMode(
-      MediaFoundationRenderingMode mode) = 0;
 };
 
 }  // namespace media
