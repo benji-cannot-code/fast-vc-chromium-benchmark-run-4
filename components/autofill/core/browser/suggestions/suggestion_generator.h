28e3c54ca53393edb32c2236f5dc34b5f83bdaa9
Mikita Kuchyn <kuchyn@google.com>
2025-10-10 08:23:35 -0700

    [PASG] Create PlusAddressSuggestionGenerator basic interface
    Create basic interface for PlusAddressSuggestionGenerator as a
    basis for the future logic transfer from BrowserAutofillManager.
    
    Low-Coverage-Reason: TESTS_IN_SEPARATE_CL
    Bug: 409962888
    Change-Id: Ibae65e6221482daeeedbce2f158cfa00365c8eec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7003023
    Commit-Queue: Mikita Kuchyn <kuchyn@chromium.org>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1528147}

--- components/autofill/core/browser/suggestions/suggestion_generator.h
+++ components/autofill/core/browser/suggestions/suggestion_generator.h
@@ -22,6 +22,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/suggestions/one_time_passwords/one_time_password_suggestion_data.h"
 #include "components/autofill/core/browser/suggestions/passkeys/hybrid_passkey_availability.h"
 #include "components/autofill/core/browser/suggestions/payments/save_and_fill_suggestion.h"
+#include "components/autofill/core/browser/suggestions/plus_addresses/create_plus_address_suggestion.h"
+#include "components/autofill/core/browser/suggestions/plus_addresses/plus_address.h"
+#include "components/autofill/core/browser/suggestions/plus_addresses/plus_address_for_address_suggestion.h"
 #include "components/autofill/core/browser/suggestions/suggestion.h"
 #include "components/autofill/core/browser/webdata/autocomplete/autocomplete_entry.h"
 
@@ -65,6 +68,9 @@ class SuggestionGenerator {
     kLoyaltyCard,
     kIdentityCredential,
     kPasskey,
+    kPlusAddress,
+    kCreatePlusAddress,
+    kPlusAddressForAddress,
     kCompose,
     kOneTimePassword,
     kMaxValue = kOneTimePassword
@@ -87,7 +93,10 @@ class SuggestionGenerator {
                                       IdentityCredential,
                                       HybridPasskeyAvailability,
                                       SaveAndFillSuggestion,
-                                      OneTimePasswordSuggestionData>;
+                                      OneTimePasswordSuggestionData,
+                                      PlusAddress,
+                                      CreatePlusAddressSuggestion,
+                                      PlusAddressForAddressSuggestion>;
 
   // Obtains data that will be used to generate suggestions on a given
   // `trigger_field` that belongs to `form`.
