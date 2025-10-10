99cbe86cdde4cdb88bc289c054729b911b36e476
Christopher Cameron <ccameron@chromium.org>
2025-10-10 04:30:32 -0700

    Fix skcms::TransferFunction_eval/invert accuracy
    skcms uses various "optimizations" for the pow function, which result in
    significant error. Replace these in the math used by the CSS color tests
    with a copy-pasted version that uses std functions.
    
    Update expectations for several WPT tests to no longer fail.
    
    There are several SVG tests that use srgb-linear and will require
    careful rebaselining. Leave the old implementation in place for those
    for now.
    
    These new functions will get removed when skcms is updated.
    
    Bug: 450045076
    Change-Id: I629d5896e86dc32a4e962b3c0e9ce351390fab77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7021694
    Reviewed-by: Kaylee Lubick <kjlubick@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1528051}

--- third_party/blink/renderer/platform/graphics/color_test.cc
+++ third_party/blink/renderer/platform/graphics/color_test.cc
@@ -841,7 +841,7 @@ TEST(BlinkColor, CarryForwardAnalogousMissingComponents) {
           Color::ColorSpace::kXYZD50,
           Color::FromColorSpace(Color::ColorSpace::kSRGB, std::nullopt, 1, 2),
           Color::FromColorSpace(Color::ColorSpace::kXYZD50, std::nullopt,
-                                1.017f, 3.634f),
+                                1.0171882f, 3.6346194f),
       },
       {
           // rgb(1 none 2) -> xyz(... none ...)
@@ -854,8 +854,8 @@ TEST(BlinkColor, CarryForwardAnalogousMissingComponents) {
           // rgb(1 2 none) -> xyz(... none)
           Color::ColorSpace::kXYZD50,
           Color::FromColorSpace(Color::ColorSpace::kSRGB, 1, 2, std::nullopt),
-          Color::FromColorSpace(Color::ColorSpace::kXYZD50, 2.344f, 3.773f,
-                                std::nullopt),
+          Color::FromColorSpace(Color::ColorSpace::kXYZD50, 2.3440469f,
+                                3.7738409f, std::nullopt),
       },
       {
           // rgb(none none none) -> lch(0 0 none)
