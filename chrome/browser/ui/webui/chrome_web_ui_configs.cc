7c78bb01272d9f61f4c8ef58ec9d8d7146e61e4c
Mingyu Lei <leimy@chromium.org>
2025-10-10 15:15:58 -0700

    WaaP: fix reload button includes for Android
    We don't need the reload button UI for Android but it's still included,
    this will cause trouble when the file is referenced later from browser
    ui.
    
    Bug: 444358999
    Change-Id: I0941ab488aa0869b023f84a23a0f7be226228628
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7025691
    Reviewed-by: Teresa Mao <temao@chromium.org>
    Commit-Queue: Teresa Mao <temao@chromium.org>
    Auto-Submit: Mingyu Lei <leimy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1528398}

--- chrome/browser/ui/webui/chrome_web_ui_configs.cc
+++ chrome/browser/ui/webui/chrome_web_ui_configs.cc
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/policy/policy_ui.h"
 #include "chrome/browser/ui/webui/predictors/predictors_ui.h"
 #include "chrome/browser/ui/webui/privacy_sandbox/privacy_sandbox_internals_ui.h"
-#include "chrome/browser/ui/webui/reload_button/reload_button_ui.h"
 #include "chrome/browser/ui/webui/saved_tab_groups_unsupported/saved_tab_groups_unsupported_ui.h"
 #include "chrome/browser/ui/webui/segmentation_internals/segmentation_internals_ui.h"
 #include "chrome/browser/ui/webui/signin_internals_ui.h"
@@ -72,6 +71,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/actor/ui/actor_overlay_ui.h"
 #include "chrome/browser/contextual_tasks/contextual_tasks_ui.h"
 #include "chrome/browser/ui/tabs/tab_group_home/tab_group_home_ui.h"
+#include "chrome/browser/ui/webui/reload_button/reload_button_ui.h"
 #include "chrome/browser/ui/webui_browser/webui_browser_ui.h"
 #if !BUILDFLAG(IS_CHROMEOS)
 #include "chrome/browser/ui/webui/app_home/app_home_ui.h"
