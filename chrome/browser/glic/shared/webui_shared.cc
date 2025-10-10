d08b5331a6ac391f63e8f907a577b36cf1927271
Michelle Abreo <michelleabreo@google.com>
2025-10-10 10:33:47 -0700

    [UnifiedFRE] Move glic_request_headers to new shared/ resource package.
    This CL introduces a shared/ directory for code common to both the glic/
    (chrome://glic) and fre/ (chrome://glic-fre) WebUIs. The first file
    moved is glic_request_headers.ts.
    
    This refactoring is necessary to enable future work on the unified FRE,
    which will involve the main glic/ app depending on the fre/ package.
    Previously, fre/ depended on glic/ (see
    https://chromium-review.googlesource.com/c/chromium/src/+/6841972), so a
    direct glic/ -> fre/ dependency would cause a circular reference. Moving
    the shared `glic_request_header.js` to a common `shared/` location
    resolves this issue by allowing both `glic/` and `fre/` to depend on
    `shared/` independently.
    
    go/bb-fre-sidepanel-dd
    
    Bug: b:440652864
    Change-Id: I0afb48ad5d0a47ffe571992e6ea5682711166950
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7026333
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Michelle Abreo <michelleabreo@google.com>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1528219}

--- chrome/browser/glic/shared/webui_shared.cc
+++ chrome/browser/glic/shared/webui_shared.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/channel_info.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/chrome_switches.h"
+#include "chrome/grit/glic_shared_resources.h"
+#include "chrome/grit/glic_shared_resources_map.h"
 #include "content/public/browser/web_ui_data_source.h"
 
 namespace glic {
@@ -38,6 +40,8 @@ void ConfigureSharedWebUISource(content::WebUIDataSource& source) {
                    base::FeatureList::IsEnabled(features::kGlicHeader)
                        ? features::kGlicHeaderRequestTypes.Get()
                        : "");
+
+  source.AddResourcePaths(kGlicSharedResources);
 }
 
 }  // namespace glic
