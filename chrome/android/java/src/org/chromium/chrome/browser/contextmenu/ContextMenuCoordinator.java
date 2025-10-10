51fc1b0772341a867fb0ca28de5985a1a0db9897
Masa Fujita <massan@google.com>
2025-10-09 22:32:46 -0700

    flyout: a11y: Only the frontmost flyout popup should have focus
    Unless specified otherwise, all of the `ListView`s in all of the flyout
    popups get focus when the devices exits touch mode due to keyboard
    input. This is problematic when the user tries to navigate the flyout
    menus using both mouse and keyboard.
    
    This CL makes sure that only the frontmost popup has focus and the other
    popups lose focus when necessary, to make sure that navigation does not
    break down in these circumstances.
    
    Bug: 447103380
    Change-Id: I8dc22ee728bbe247b11254c47a03bf18ef0bc3a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7015568
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Commit-Queue: Masa Fujita <massan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1527929}

--- chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ContextMenuCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ContextMenuCoordinator.java
@@ -408,6 +408,10 @@ public class ContextMenuCoordinator implements ContextMenuUi, FlyoutHandler<Cont
 
         mDialogs.subList(clearFromIndex, mDialogs.size()).clear();
         mListViews.subList(clearFromIndex, mListViews.size()).clear();
+
+        if (mDialogs.size() > 0) {
+            mDialogs.get(mDialogs.size() - 1).popupWindow.setWindowFocus(true);
+        }
     }
 
     @Override
@@ -445,7 +449,12 @@ public class ContextMenuCoordinator implements ContextMenuUi, FlyoutHandler<Cont
                             }
                         });
 
+        assert mDialogs.size() > 0;
+        mDialogs.get(mDialogs.size() - 1).popupWindow.setWindowFocus(false);
+
+        dialog.setWindowFocus(true);
         dialog.show();
+
         mDialogs.add(new FlyoutPopupEntry(item, dialog));
     }
 
--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/ContextMenuDialog.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/ContextMenuDialog.java
@@ -16,6 +16,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnDragListener;
 import android.view.View.OnLayoutChangeListener;
+import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
 import android.view.WindowManager;
@@ -32,6 +33,7 @@ import org.chromium.ui.animation.EmptyAnimationListener;
 import org.chromium.ui.dragdrop.DragEventDispatchHelper;
 import org.chromium.ui.dragdrop.DragEventDispatchHelper.DragEventDispatchDestination;
 import org.chromium.ui.interpolators.Interpolators;
+import org.chromium.ui.listmenu.ListMenuUtils;
 import org.chromium.ui.util.ColorUtils;
 import org.chromium.ui.widget.AnchoredPopupWindow;
 import org.chromium.ui.widget.FlyoutPopupSpecCalculator;
@@ -402,5 +404,14 @@ public class ContextMenuDialog extends AlwaysDismissedDialog {
     @Nullable
     OnDragListener getOnDragListenerForTesting() {
         return mDragEventDispatchHelper;
+    }
+
+    /**
+     * Set the focus state for this dialog's content view.
+     *
+     * @param hasFocus Whether this dialog's content should have focus.
+     */
+    public void setWindowFocus(boolean hasFocus) {
+        ListMenuUtils.setWindowFocus((ViewGroup) mContentView, hasFocus);
     }
 }
