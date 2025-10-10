d438f43edaad849a3ac22090fcf94477d1f62314
Hitarth Kothari <hitarthkothari@google.com>
2025-10-10 10:46:15 -0700

    [Zoom Indicator] Add metrics for new zoom entry points
    Adds 3 simple user actions to capture feature usage.
     * Record every time the zoom in/ out button on the menu is pressed
     * Record every time the zoom indicator on the omnibox is pressed
     * Record every time the immersive mode button on the menu is pressed.
    
    Bug: 450863595
    Change-Id: I2422119b767ad0a587cad158840c3aa84d4fb0b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7032639
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Auto-Submit: Hitarth Kothari <hitarthkothari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1528231}

--- components/browser_ui/accessibility/android/java/src/org/chromium/components/browser_ui/accessibility/PageZoomIndicatorCoordinator.java
+++ components/browser_ui/accessibility/android/java/src/org/chromium/components/browser_ui/accessibility/PageZoomIndicatorCoordinator.java
@@ -102,6 +102,7 @@ public class PageZoomIndicatorCoordinator {
         assumeNonNull(mView);
 
         mMediator.showPopupWindow(mZoomIndicatorViewSupplier.get(), mPopupWindow);
+        PageZoomUma.logZoomIndicatorClicked();
     }
 
     /** Hide the zoom feature UI from the user. */
