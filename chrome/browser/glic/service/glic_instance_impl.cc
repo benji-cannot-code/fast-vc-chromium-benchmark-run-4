be1ea978941bf4c87272d27905e2bfde0cbd6807
Bryant Chandler <bryantchandler@google.com>
2025-10-10 11:59:39 -0700

    Glic: Close side panel on detach
    When detaching from the side panel, the side panel UI should be closed to ensure a clean state. This is done after showing the floating embedder to ensure all detaching work is complete. Closing within the embedder can cause problems as the embedder is deleted and replaced with an inactive one on close.
    
    Bug: b:450327156
    Change-Id: I6a6a69648673748bf76bbdb82c3b82fcb7b91b38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7029903
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1528282}

--- chrome/browser/glic/service/glic_instance_impl.cc
+++ chrome/browser/glic/service/glic_instance_impl.cc
@@ -168,6 +168,7 @@ void GlicInstanceImpl::Show(EmbedderType type, tabs::TabInterface* tab) {
 
 void GlicInstanceImpl::Detach(tabs::TabInterface* tab) {
   Show(EmbedderType::kFloating, tab);
+  Close(EmbedderType::kSidePanel, tab);
 }
 
 void GlicInstanceImpl::Close(EmbedderType type, tabs::TabInterface* tab) {
