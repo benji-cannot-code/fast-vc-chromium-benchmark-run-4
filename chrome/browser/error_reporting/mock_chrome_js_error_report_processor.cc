4cbea01acb3f1a983a7f968892891f502669ac0a
Jonathan Lee <jonathanjlee@google.com>
2025-10-10 09:06:57 -0700

    Reland "[crash] Desensitize error reporting tests to query string formatting"
    This is a reland of commit cf327731d750e208a04267380f43472bb59f15be
    
    Changes since original (PS1):
      Relax the assertions so that they will pass for both the real and mock
      ChromeOS Media Apps (in line with the original logic).
      `is_chrome_branded=true` will enable the real app [0].
    
    [0]: https://source.chromium.org/chromium/chromium/src/+/main:ash/webui/media_app_ui/media_app_ui.gni;l=8-10;drc=613e7634d019af47e6d612a708541f291b708dc2;bpv=0;bpt=0
    
    Original change's description:
    > [crash] Desensitize error reporting tests to query string formatting
    >
    > Some error reporting tests check the `application/x-www-form-urlencoded`
    > query string as it appears on the HTTP wire. This has disadvantages:
    > * Assertion failures don't pinpoint which parameter(s) had unexpected
    >   values [0].
    > * %-encodings are not unique. For example, a recent change in
    >   `cpp-httplib` [1] causes `mock_chromeos_crash_reporter` [2] to send
    >   requests with `+` instead of `%20` for spaces, breaking these tests.
    > * %-encoded data in tests are difficult to read, especially with
    >   multiple %-encoding rounds.
    >
    > This CL:
    > a. Augments `MockCrashEndpoint::Request` to parse and decode the query
    >    (similar to JavaScript's `new URLSearchParams(...)` [3]).
    > b. Rewrites each assertion in an equivalent (or stronger) decoded form.
    >
    > This CL should have no coverage changes, but should unblock rolling
    > crashpad.
    >
    > [0]: https://ci.chromium.org/ui/p/chromium/builders/try/linux-chromeos-rel/2553626/overview
    > [1]: https://github.com/yhirose/cpp-httplib/commit/c0c36f021defe69033ffbebbf04f54c68c4d0b25
    > [2]: https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/error_reporting/mock_chromeos_crash_reporter.cc;l=118;drc=2018660bccb396128a946dadc3d330801bf2ed3d;bpv=0;bpt=0
    > [3]: https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams/URLSearchParams
    >
    > Fixed: 450582917
    > Change-Id: I181b136093d44a85dbe202052f1c10fbc029e787
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7007503
    > Reviewed-by: Darren Shen <shend@chromium.org>
    > Commit-Queue: Mark Mentovai <mark@chromium.org>
    > Reviewed-by: Mark Mentovai <mark@chromium.org>
    > Reviewed-by: John Palmer <jopalmer@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1527709}
    
    Change-Id: I649c3c86d6f3c5e693dbbb07aaed154f950f436a
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7029844
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1528170}

--- chrome/browser/error_reporting/mock_chrome_js_error_report_processor.cc
+++ chrome/browser/error_reporting/mock_chrome_js_error_report_processor.cc
@@ -17,14 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/crash/content/browser/error_reporting/mock_crash_endpoint.h"
 #include "components/variations/variations_crash_keys.h"
 
-const char MockChromeJsErrorReportProcessor::
-    kDefaultExperimentListStringPreEscaping[] =
-        "6598898b-ac59b6dc,";  // variations::GetExperimentListInfo() always
-                               // leaves a trailing comma.
-
 const char MockChromeJsErrorReportProcessor::kDefaultExperimentListString[] =
-    "6598898b-ac59b6dc%2C";  // The URL escaping turns the comma into %2C in the
-                             // query string.
+    "6598898b-ac59b6dc,";  // variations::GetExperimentListInfo() always
+                           // leaves a trailing comma.
 
 // Tricium gets confused by the #if's and thinks we should change this to
 // "= default".
@@ -76,7 +71,7 @@ MockChromeJsErrorReportProcessor::GetExperimentListInfo() const {
   }
   variations::ExperimentListInfo result;
   result.num_experiments = 1;
-  result.experiment_list = kDefaultExperimentListStringPreEscaping;
+  result.experiment_list = kDefaultExperimentListString;
   return result;
 }
 
