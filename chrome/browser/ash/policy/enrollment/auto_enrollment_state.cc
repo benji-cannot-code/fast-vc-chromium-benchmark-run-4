e36b66c1809ab657c325f38c874355a57bc5c4fa
Roland Bock <rbock@google.com>
2025-10-10 09:27:40 -0700

    [enrollment] Introduce new state for already owned devices
    Before this change:
    If already owned (e.g. enrolled) devices go through OOBE again, e.g. due
    to a crash before OOBE is completed, they go through state determination
    again. This will detect the established ownership and declare
    "No Enrollment". This results in an attempt to clear block-devmode from
    VPD and FWMP. The latter fails as the TPM is already locked, but this
    seems pretty close to an enrollment escape (by allowing devmode).
    
    This change introduces a new state `kDeviceAlreadyOwned` to indicate
    that ownership has already been taken. In this case, no enrollment will
    be attempted and block-devmode will be left untouched.
    
    Bug: b:445121415
    Test: New and adjusted unit tests
    Change-Id: I618d2a0ee16e10313446e3f1b19d539980446414
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7030680
    Reviewed-by: Artem Sumaneev <asumaneev@google.com>
    Commit-Queue: Roland Bock <rbock@google.com>
    Cr-Commit-Position: refs/heads/main@{#1528185}

--- chrome/browser/ash/policy/enrollment/auto_enrollment_state.cc
+++ chrome/browser/ash/policy/enrollment/auto_enrollment_state.cc
@@ -26,6 +26,8 @@ std::string_view AutoEnrollmentResultToString(AutoEnrollmentResult result) {
       return "Device disabled";
     case AutoEnrollmentResult::kSuggestedEnrollment:
       return "Suggested enrollment";
+    case AutoEnrollmentResult::kDeviceAlreadyOwned:
+      return "Device already owned";
   }
 }
 
