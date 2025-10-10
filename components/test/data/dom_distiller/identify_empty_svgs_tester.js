1b1a969ec1576f3e121eadc569c5322b323c5cfb
Brandon Wylie <wylieb@google.com>
2025-10-10 12:53:36 -0700

    [reader mode] Hide empty svg elements
    Elements that refer to "#local-res" are rendered as empty. These
    elements aren't trivially captured by a CSS query. Capturing these
    elements in javascript, and adding a class to them so they can be styled
    in CSS.
    
    Bug: 445759621
    Change-Id: I00804b15d7a44b9a67c91aef658ef4056c74278b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7022785
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1528311}

---
+++ components/test/data/dom_distiller/identify_empty_svgs_tester.js
@@ -0,0 +1,75 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+suite('IdentifyEmptySVGsTest', function() {
+  test('Correctly identifies SVG with href', async function() {
+    const {assert} = await import('./chai.js');
+
+    const container = document.createElement('div');
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    const use = document.createElementNS('http://www.w3.org/2000/svg', 'use');
+    use.setAttribute('href', '#local-resource');
+    svg.appendChild(use);
+    container.appendChild(svg);
+    document.body.appendChild(container);
+
+    identifyEmptySVGs(container);
+
+    assert.isTrue(svg.classList.contains('distilled-svg-with-local-ref'));
+
+    document.body.removeChild(container);
+  });
+
+  test('Correctly identifies SVG with xlink:href', async function() {
+    const {assert} = await import('./chai.js');
+
+    const container = document.createElement('div');
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    const use = document.createElementNS('http://www.w3.org/2000/svg', 'use');
+    use.setAttributeNS('http://www.w3.org/1999/xlink', 'href', '#local-resource');
+    svg.appendChild(use);
+    container.appendChild(svg);
+    document.body.appendChild(container);
+
+    identifyEmptySVGs(container);
+
+    assert.isTrue(svg.classList.contains('distilled-svg-with-local-ref'));
+
+    document.body.removeChild(container);
+  });
+
+  test('Ignores SVG without local resource pointer', async function() {
+    const {assert} = await import('./chai.js');
+
+    const container = document.createElement('div');
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    const use = document.createElementNS('http://www.w3.org/2000/svg', 'use');
+    use.setAttribute('href', 'http://example.com#resource');
+    svg.appendChild(use);
+    container.appendChild(svg);
+    document.body.appendChild(container);
+
+    identifyEmptySVGs(container);
+
+    assert.isFalse(svg.classList.contains('distilled-svg-with-local-ref'));
+
+    document.body.removeChild(container);
+  });
+
+  test('Ignores SVG without use element', async function() {
+    const {assert} = await import('./chai.js');
+
+    const container = document.createElement('div');
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    container.appendChild(svg);
+    document.body.appendChild(container);
+
+    identifyEmptySVGs(container);
+
+    assert.isFalse(svg.classList.contains('distilled-svg-with-local-ref'));
+
+    document.body.removeChild(container);
+  });
+});
