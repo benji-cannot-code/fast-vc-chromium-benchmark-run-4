5ab16409dd15f1c13b03561b97cc75a6257ab1a6
Marc Treib <treib@chromium.org>
2025-10-10 01:09:18 -0700

    Implement SyncServiceObserver::OnSyncShutdown in ios/
    This implements (overrides) the method OnSyncShutdown in the two
    SyncServiceObserver implementations in ios/:
    * SyncErrorInfoBarDelegate: This is a UI attached to the profile, and
      uses a ProfileIOS*. That means it must be destroyed before profile
      shutdown, and thus also before the SyncService is Shutdown(). So the
      implementation of OnSyncShutdown() can simply be NOTREACHED().
    * TargetDeviceListWaiter (part of the send-tab-to-self code): This one
      is *probably* also destroyed before profile shutdown, but it's harder
      to be sure. So this one actually implements OnSyncShutdown() (in a
      straightforward way, just stop observing the SyncService).
    
    As a semi-related cleanup, this also migrates both these classes from
    manual Add/RemoveObserver() calls to using ScopedObservation instead.
    
    The eventual goal is to make SyncServiceObserver::OnSyncShutdown() pure
    virtual, so that all implementations need to consider whether they need
    to do something there (i.e. unregister themselves, drop and SyncService
    pointers).
    
    Bug: 40882734
    Change-Id: I8f4edc894b7c38e2fdfa1f2a975d0ce6ec82570f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7027132
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1527978}

--- ios/chrome/browser/settings/model/sync/utils/sync_error_infobar_delegate.h
+++ ios/chrome/browser/settings/model/sync/utils/sync_error_infobar_delegate.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <string>
 
 #import "base/memory/raw_ptr.h"
+#import "base/scoped_observation.h"
 #import "components/infobars/core/confirm_infobar_delegate.h"
 #import "components/sync/service/sync_service.h"
 #import "components/sync/service/sync_service_observer.h"
@@ -58,6 +59,7 @@ class SyncErrorInfoBarDelegate : public ConfirmInfoBarDelegate,
 
   // syncer::SyncServiceObserver implementation.
   void OnStateChanged(syncer::SyncService* sync) override;
+  void OnSyncShutdown(syncer::SyncService* sync) override;
 
   // Called when the infobar is dismissed through timing out.
   void InfoBarDismissedByTimeout() const;
@@ -68,6 +70,8 @@ class SyncErrorInfoBarDelegate : public ConfirmInfoBarDelegate,
 
  private:
   const raw_ptr<ProfileIOS> profile_;
+  base::ScopedObservation<syncer::SyncService, SyncErrorInfoBarDelegate>
+      sync_observation_{this};
   const id<SyncPresenter> presenter_;
   const SyncErrorInfoBarTrigger trigger_;
   syncer::SyncService::UserActionableError error_state_;
