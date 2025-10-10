3639d72453c471db61e715d4669793fa3a01b86e
Michelle Abreo <michelleabreo@google.com>
2025-10-10 11:34:06 -0700

    [UnifiedFRE] Add FrePageHandlerFactory to GlicUI WebUI Controller.
    This does some initial C++ side plumbing for moving the fre into the
    sidepanel. This allows us to use the two page handlers as individual
    components in the unified FRE.
    
    In my next CL I will add implement the typescript changes-- adding the
    AppRouter logic to the TS controllers.
    
    go/bb-fre-sidepanel-dd
    
    Bug: b:440652864
    Change-Id: Iffaa586131dc8ed48695c815cc21556962dbff30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7022071
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Commit-Queue: Michelle Abreo <michelleabreo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1528263}

--- chrome/browser/glic/host/glic_ui.h
+++ chrome/browser/glic/host/glic_ui.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_GLIC_HOST_GLIC_UI_H_
 #define CHROME_BROWSER_GLIC_HOST_GLIC_UI_H_
 
+#include "chrome/browser/glic/fre/glic_fre.mojom.h"
 #include "chrome/browser/glic/host/glic.mojom.h"
 #include "content/public/browser/web_ui_controller.h"
 #include "content/public/browser/webui_config.h"
@@ -15,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace glic {
 class GlicPageHandler;
+class GlicFrePageHandler;
 class GlicUI;
 
 class GlicUIConfig : public content::DefaultWebUIConfig<GlicUI> {
@@ -25,13 +27,17 @@ class GlicUIConfig : public content::DefaultWebUIConfig<GlicUI> {
 
 // The WebUI for chrome://glic
 class GlicUI : public ui::MojoWebUIController,
-               public glic::mojom::PageHandlerFactory {
+               public glic::mojom::PageHandlerFactory,
+               public glic::mojom::FrePageHandlerFactory {
  public:
   explicit GlicUI(content::WebUI* web_ui);
   ~GlicUI() override;
 
   void BindInterface(
       mojo::PendingReceiver<glic::mojom::PageHandlerFactory> receiver);
+
+  void BindInterface(
+      mojo::PendingReceiver<glic::mojom::FrePageHandlerFactory> receiver);
 
   // When called, the UI will believe it is offline when it is launched from the
   // current test.
@@ -44,9 +50,15 @@ class GlicUI : public ui::MojoWebUIController,
       mojo::PendingReceiver<glic::mojom::PageHandler> receiver,
       mojo::PendingRemote<glic::mojom::Page> page) override;
 
+  void CreatePageHandler(
+      mojo::PendingReceiver<glic::mojom::FrePageHandler> fre_receiver) override;
+
   std::unique_ptr<GlicPageHandler> page_handler_;
+  std::unique_ptr<GlicFrePageHandler> fre_page_handler_;
 
   mojo::Receiver<glic::mojom::PageHandlerFactory> page_factory_receiver_{this};
+  mojo::Receiver<glic::mojom::FrePageHandlerFactory> fre_page_factory_receiver_{
+      this};
 
   static bool simulate_no_connection_;
 
