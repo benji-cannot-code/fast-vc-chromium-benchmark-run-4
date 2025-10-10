740fad225ea1d2312ad611bb7165e98373e0970c
Lei Zhang <thestig@chromium.org>
2025-10-10 11:25:19 -0700

    [PDF Ink Signatures] Make PdfInkUndoRedoModelTest.Stress less flaky
    Lower the cycle count for sanitizer and CFI bots. This is the same
    strategy as https://crrev.com/1358218, but just for a different set of
    bots.
    
    Bug: 450894558
    Change-Id: I8074c957b26fede78a733e266a14ce72195fdf16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7032486
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1528257}

--- pdf/pdf_ink_undo_redo_model_unittest.cc
+++ pdf/pdf_ink_undo_redo_model_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 #include <set>
 
+#include "base/cfi_buildflags.h"
 #include "pdf/pdf_ink_ids.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -428,11 +429,14 @@ TEST(PdfInkUndoRedoModelTest, DrawDrawEraseStrokesAndShapesUndoRedo) {
 }
 
 TEST(PdfInkUndoRedoModelTest, Stress) {
-#if defined(NDEBUG)
-  constexpr size_t kCycles = 10000;
-#else
-  // The larger non-debug value is too slow for "dbg" bots.
+#if !defined(NDEBUG) || defined(ADDRESS_SANITIZER) ||         \
+    defined(MEMORY_SANITIZER) || defined(THREAD_SANITIZER) || \
+    BUILDFLAG(CFI_ICALL_CHECK)
+  // The larger non-debug value is too slow for "dbg" bots and bots with
+  // sanitizers enabled.
   constexpr size_t kCycles = 1000;
+#else
+  constexpr size_t kCycles = 10000;
 #endif
 
   PdfInkUndoRedoModel undo_redo;
