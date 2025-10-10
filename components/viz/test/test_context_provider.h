22659ec75994509abe911ec2a0b349f24cb9ad92
Colin Blundell <blundell@chromium.org>
2025-10-10 02:10:13 -0700

    [Viz] Remove TestContextProvider::GrContext()
    Nothing calls this anymore.
    
    Bug: 391648152
    Change-Id: I702aeaa0b001ba86a163bc1bb7ca6f1d9b26749f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7021634
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1528005}

--- components/viz/test/test_context_provider.h
+++ components/viz/test/test_context_provider.h
@@ -27,10 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "third_party/skia/include/core/SkRefCnt.h"
 
-namespace skia_bindings {
-class GrContextForGLES2Interface;
-}
-
 namespace viz {
 class TestGLES2Interface;
 class TestRasterInterface;
@@ -88,8 +84,6 @@ class TestContextProvider
   void RemoveObserver(ContextLostObserver* obs) override;
   unsigned int GetGrGLTextureFormat(SharedImageFormat format) const override;
 
-  class GrDirectContext* GrContext();
-
   TestGLES2Interface* TestContextGL();
   TestRasterInterface* GetTestRasterInterface();
 
@@ -124,7 +118,6 @@ class TestContextProvider
   // Used for GLES2 contexts.
   std::unique_ptr<TestGLES2Interface> context_gl_;
   std::unique_ptr<gpu::raster::RasterInterface> raster_interface_gles_;
-  std::unique_ptr<skia_bindings::GrContextForGLES2Interface> gr_context_;
 
   // Used for raster contexts.
   std::unique_ptr<TestRasterInterface> raster_context_;
