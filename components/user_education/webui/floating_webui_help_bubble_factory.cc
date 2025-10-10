b84988d06f031c8bba2f0064a590a84d36191bc1
Lei Zhang <thestig@chromium.org>
2025-10-10 12:06:58 -0700

    [User Education] Simplify HelpBubbleHandlerBase interface
    Start by updating the HelpBubbleHandlerBase::GetWebContents() comment.
    Better explain when GetWebContents() would return nullptr, which is
    only in tests that are not set up correctly. Offer developers reading
    the code better advice on what to do in that case. Then follow the
    updated comment and remove nullptr checks in all the GetWebContents()
    callers.
    
    As a result, VisibilityProvider::CheckIsVisible() can be simplified to
    return a bool instead of a tri-state. Then delete test code that are no
    longer applicable.
    
    Change-Id: Idbd072bbe09a5190672d6c69ae5721804f663eef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7029560
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1528287}

--- components/user_education/webui/floating_webui_help_bubble_factory.cc
+++ components/user_education/webui/floating_webui_help_bubble_factory.cc
@@ -49,9 +49,6 @@ views::WebView* GetWebViewForElement(const ui::TrackedElement* element) {
   const auto* const element_webui =
       element->AsA<TrackedElementHelpBubbleWebUIAnchor>();
   auto* const contents = element_webui->handler()->GetWebContents();
-  if (!contents) {
-    return nullptr;
-  }
   auto* const widget = views::Widget::GetWidgetForNativeWindow(
       contents->GetTopLevelNativeWindow());
   if (!widget) {
