6c3667ea16cc126a89b79868da368e0e23091d24
Morten Stenshorne <mstensho@chromium.org>
2025-10-10 02:21:59 -0700

    Remove unused ExpressionHeapVector
    Change-Id: I12a6fbd268ba5013e0124b0859f7d0a4678d6dfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7027191
    Reviewed-by: Steinar H Gunderson <sesse@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1528013}

--- third_party/blink/renderer/core/css/media_query.h
+++ third_party/blink/renderer/core/css/media_query.h
@@ -41,10 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 
 namespace blink {
-class MediaQueryExp;
 class MediaQueryExpNode;
-
-using ExpressionHeapVector = Vector<MediaQueryExp>;
 
 class CORE_EXPORT MediaQuery : public GarbageCollected<MediaQuery> {
  public:
