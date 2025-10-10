afbd3a61808cac88130b98d008f22fd8fad72d73
Colin Blundell <blundell@chromium.org>
2025-10-10 02:10:45 -0700

    [//gpu] Remove GrContextForGLES2Interface
    This is no longer used post-OOP-C.
    
    Bug: 391648152
    Change-Id: Icf6791617590d87830c931488ddf4e48f639d2ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7019510
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1528006}

--- gpu/skia_bindings/gles2_implementation_with_grcontext_support.cc
+++ gpu/skia_bindings/gles2_implementation_with_grcontext_support.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
-#include "gpu/skia_bindings/grcontext_for_gles2_interface.h"
 #include "third_party/khronos/GLES2/gl2ext.h"
 #include "third_party/skia/include/gpu/ganesh/GrDirectContext.h"
 
