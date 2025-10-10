a9a1597c3380cef6fcb904e4109587b263702ede
Koji Ishii <kojii@chromium.org>
2025-10-10 06:21:31 -0700

    Remove the runtime flag `LayoutNGShapeCache`
    This flag was under a Finch study, which was fully enabled on
    stable for WebView on Jul 3. Other platforms were enabled
    earlier, on Apr 24.
    
    The field trial config for `LayoutNGCache` was removed in
    crrev.com/c/6472457 but the WebView part was revived at
    crrev.com/c/6486747 as the WebView needed more Finch.
    
    Bug: 377832797
    Change-Id: Ic31bf15b3ec2cc052f3c1586a8aa89cc3e9afc9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7031780
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1528091}

--- third_party/blink/renderer/core/layout/inline/inline_node_test.cc
+++ third_party/blink/renderer/core/layout/inline/inline_node_test.cc
@@ -1723,27 +1723,7 @@ TEST_F(InlineNodeTest, FontFeaturesInitial) {
   EXPECT_FALSE(is_initial("no-kern"));
 }
 
-TEST_F(InlineNodeTest, ShapeCacheDisabled) {
-  ScopedLayoutNGShapeCacheForTest scoped_feature(false);
-
-  SetupHtml("t",
-            "<style>div { font-family: serif; }</style>"
-            "<div id=t>abc</div>");
-  InlineNodeForTest node = CreateInlineNode();
-  node.CollectInlines();
-  EXPECT_EQ("abc", node.Text());
-
-  const String& text_content(node.Text().c_str());
-  InlineItems& items = node.Items();
-  ShapeResultSpacing spacing(text_content, node.IsSvgText());
-
-  EXPECT_FALSE(
-      node.IsNGShapeCacheAllowed(text_content, nullptr, items, spacing));
-}
-
 TEST_F(InlineNodeTest, ShapeCacheLongString) {
-  ScopedLayoutNGShapeCacheForTest scoped_feature(true);
-
   for (const unsigned text_length :
        {NGShapeCache::kMaxTextLengthOfEntries - 1,
         NGShapeCache::kMaxTextLengthOfEntries,
@@ -1769,8 +1749,6 @@ TEST_F(InlineNodeTest, ShapeCacheLongString) {
 }
 
 TEST_F(InlineNodeTest, ShapeCacheMultiItems) {
-  ScopedLayoutNGShapeCacheForTest scoped_feature(true);
-
   SetupHtml("t", "<div id=t>abc<span>def</span>ghi</div>");
   InlineNodeForTest node = CreateInlineNode();
   node.CollectInlines();
@@ -1785,8 +1763,6 @@ TEST_F(InlineNodeTest, ShapeCacheMultiItems) {
 }
 
 TEST_F(InlineNodeTest, ShapeCacheSpacingRequired) {
-  ScopedLayoutNGShapeCacheForTest scoped_feature(true);
-
   SetupHtml("t",
             "<style>div { letter-spacing: 5px; }</style>"
             "<div id=t>abc</div>");
--- third_party/blink/renderer/platform/fonts/shaping/ng_shape_cache.h
+++ third_party/blink/renderer/platform/fonts/shaping/ng_shape_cache.h
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/fonts/shaping/shape_result.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_hash_map.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
-#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/text/text_direction.h"
 #include "third_party/blink/renderer/platform/wtf/forward.h"
 #include "third_party/blink/renderer/platform/wtf/hash_functions.h"
@@ -49,9 +48,7 @@ class NGShapeCache : public GarbageCollected<NGShapeCache> {
   static constexpr unsigned kMaxSize = 2048;
 
   explicit NGShapeCache(const SimpleFontData* primary_font)
-      : primary_font_(primary_font) {
-    DCHECK(RuntimeEnabledFeatures::LayoutNGShapeCacheEnabled());
-  }
+      : primary_font_(primary_font) {}
   NGShapeCache(const NGShapeCache&) = delete;
   NGShapeCache& operator=(const NGShapeCache&) = delete;
 
