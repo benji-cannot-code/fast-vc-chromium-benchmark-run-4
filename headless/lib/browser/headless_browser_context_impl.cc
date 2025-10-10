7c99a417dea3a3ddb78c25800221ce62f99e1be9
Andrey Kosyakov <caseq@chromium.org>
2025-10-10 09:25:38 -0700

    Do not defer HeadlessRequestContextManager destruction to the IO thread
    The class is actually created and operates on the UI thread, so destructing it on the IO thread is totally pointless and confusing. Synchronous destruction resolves potential UaF wrt  HeadlessBrowserImpl::os_crypt_async_.
    
    Change-Id: I69372d83f22cfb837c8cfebc720f1772e9e66d4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7029519
    Reviewed-by: Peter Kvitek <kvitekp@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1528184}

--- headless/lib/browser/headless_browser_context_impl.cc
+++ headless/lib/browser/headless_browser_context_impl.cc
@@ -106,11 +106,6 @@ HeadlessBrowserContextImpl::~HeadlessBrowserContextImpl() {
   if (content::RenderProcessHost::run_renderer_in_process())
     content::RenderProcessHost::ShutDownInProcessRenderer();
 
-  if (request_context_manager_) {
-    content::GetIOThreadTaskRunner({})->DeleteSoon(
-        FROM_HERE, request_context_manager_.release());
-  }
-
   ShutdownStoragePartitions();
 
   BrowserContextDependencyManager::GetInstance()->DestroyBrowserContextServices(
