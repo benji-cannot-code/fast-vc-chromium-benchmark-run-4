6f851c549f1ac6b689442350ae0866ed7d291037
James Cook <jamescook@chromium.org>
2025-10-10 20:10:51 -0700

    extensions: Enable WebRequestPersistentListenersTest on desktop Android
    PRE_ steps now work on Android, so we can enable this test.
    
    Bug: 40200835
    Change-Id: Iaead52bf1ba708f272ba7a6eb17bdf22405a9db7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7033418
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1528492}

--- chrome/browser/extensions/api/web_request/web_request_apitest.cc
+++ chrome/browser/extensions/api/web_request/web_request_apitest.cc
@@ -6344,8 +6344,6 @@ class WebRequestPersistentListenersTest
   std::unique_ptr<ExtensionTestMessageListener> test_listener_;
 };
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
-// Doesn't run on desktop android, probably bug in PRE implementation.
 // Tests that webRequest listeners are persistent across browser restarts.
 IN_PROC_BROWSER_TEST_P(WebRequestPersistentListenersTest,
                        PRE_TestListenersArePersistent) {
@@ -6397,6 +6395,8 @@ IN_PROC_BROWSER_TEST_P(WebRequestPersistentListenersTest,
   EXPECT_EQ(2, request_count.GetInt());
 }
 
+#if !BUILDFLAG(IS_ANDROID)
+// Android only supports manifest V3 / service worker.
 INSTANTIATE_TEST_SUITE_P(
     PersistentBackground,
     WebRequestPersistentListenersTest,
@@ -6408,6 +6408,8 @@ INSTANTIATE_TEST_SUITE_P(
             ContextType::kPersistentBackground,
             BackgroundResourceFetchTestCase::kBackgroundResourceFetchDisabled)),
     ExtensionWebRequestApiTestWithContextType::PrintToStringParamName());
+#endif  // !BUILDFLAG(IS_ANDROID)
+
 INSTANTIATE_TEST_SUITE_P(
     ServiceWorker,
     WebRequestPersistentListenersTest,
@@ -6419,7 +6421,6 @@ INSTANTIATE_TEST_SUITE_P(
             ContextType::kServiceWorker,
             BackgroundResourceFetchTestCase::kBackgroundResourceFetchDisabled)),
     ExtensionWebRequestApiTestWithContextType::PrintToStringParamName());
-#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
 class ManifestV3WebRequestApiTest : public ExtensionWebRequestApiTest {
  public:
