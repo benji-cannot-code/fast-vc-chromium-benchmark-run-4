cd930cee35149eb2ab1dcf51b563812629958435
Ernest Nguyen Hung <ernn@google.com>
2025-10-10 20:12:00 -0700

    [DBSC] Use 'unexportable_keys' level retry mechanism for standard
    It removes the custom retrying logic for DBSC standard to rely on the
    common `unexportable_keys` level retry mechanism instead. In the next CL
    `UnexportableKeyService` will be simplified by removing the entry logic
    configuration.
    
    Bug: 405408610
    Change-Id: I0c295d39d66e9d4853aa523f51761d39a298f149
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7017620
    Commit-Queue: Ernest Nguyen Hung <ernn@google.com>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1528494}

--- net/device_bound_sessions/registration_fetcher.cc
+++ net/device_bound_sessions/registration_fetcher.cc
@@ -122,7 +122,7 @@ void SignChallengeWithKey(
 
   unexportable_key_service.SignSlowlyAsync(
       key_id, base::as_byte_span(*header_and_payload), kTaskPriority,
-      /*max_retries=*/0,
+      /*max_retries=*/3,
       base::BindOnce(&OnDataSigned, expected_algorithm.value(),
                      std::move(expected_public_key).value(),
                      std::ref(unexportable_key_service), *header_and_payload,
@@ -454,7 +454,6 @@ class RegistrationFetcherImpl : public RegistrationFetcher {
     return SessionError::kSuccess;
   }
 
-  static constexpr size_t kMaxSigningFailures = 2;
   static constexpr size_t kMaxChallenges = 5;
 
   void AttemptChallengeSigning() {
@@ -474,17 +473,10 @@ class RegistrationFetcherImpl : public RegistrationFetcher {
       std::optional<RegistrationFetcher::RegistrationToken>
           registration_token) {
     if (!registration_token) {
-      number_of_signing_failures_++;
-      if (number_of_signing_failures_ < kMaxSigningFailures) {
-        AttemptChallengeSigning();
-        // `this` may be deleted.
-        return;
-      } else {
-        RunCallback(
-            RegistrationResult(SessionError{SessionError::kSigningError}));
-        // `this` may be deleted.
-        return;
-      }
+      RunCallback(
+          RegistrationResult(SessionError{SessionError::kSigningError}));
+      // `this` may be deleted.
+      return;
     }
 
     url_fetcher_ = std::make_unique<URLFetcher>(context_, fetcher_endpoint_,
@@ -784,7 +776,6 @@ class RegistrationFetcherImpl : public RegistrationFetcher {
   GURL provider_url_;
   std::optional<std::string> current_challenge_;
   std::optional<std::string> current_authorization_;
-  size_t number_of_signing_failures_ = 0;
   size_t number_of_challenges_ = 0;
 
   base::WeakPtrFactory<RegistrationFetcherImpl> weak_ptr_factory_{this};
