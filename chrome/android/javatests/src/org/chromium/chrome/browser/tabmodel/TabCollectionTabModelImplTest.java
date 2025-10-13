e85a666e725ff923ff8528ea0d30f74fef46c227
Neil Coronado <nemco@google.com>
2025-10-13 15:31:39 -0700

    [TabCollections] Pass allowUndo to #willCloseMultipleTabs
    Pass allowUndo to #willCloseMultipleTabs.
    -We now have access to this information, and can notify accordingly.
    -Needed by UndoRefocusHelper to re-select on undo-closure.
    
    Fixed: 451645233
    Change-Id: I949d197afc17331ad0104b2d596da1118a9e0452
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7038076
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Neil Coronado <nemco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1529168}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/TabCollectionTabModelImplTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/TabCollectionTabModelImplTest.java
@@ -3188,7 +3188,7 @@ public class TabCollectionTabModelImplTest {
         assertEquals(4, getCount());
         assertTabsInOrderAre(List.of(tab0, tab1, tab2, tab3));
 
-        assertEquals(tab0, getCurrentTab());
+        assertEquals(tab2, getCurrentTab());
         ThreadUtils.runOnUiThreadBlocking(() -> mCollectionModel.removeObserver(observer));
     }
 
