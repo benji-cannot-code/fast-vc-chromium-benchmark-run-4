dfe78c2d1f7e26ad42e6f31a6dd5c2fceb9c868a
Kalvin Lee <kdlee@chromium.org>
2025-10-13 09:48:55 -0700

    Migrate `generate_top_domains_trie`
    This CL is intended to be trivial. It introduces a new template that
    `top_domain_generator` can use to generate constants, and makes the
    constants `inline constexpr std::array`s.
    
    As a proof-of-concept, this first CL migrates
    `generate_top_domains_trie`.
    
    Bug: 449800517
    Change-Id: I84872f41c8c635fcaa5348ebab7dd72ad04db0f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7016111
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1528959}

--- components/url_formatter/spoof_checks/idn_spoof_checker.cc
+++ components/url_formatter/spoof_checks/idn_spoof_checker.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_local_storage.h"
 #include "build/build_config.h"
 #include "components/url_formatter/spoof_checks/skeleton_generator.h"
+#include "components/url_formatter/spoof_checks/top_domains/domains-trie.h"
 #include "net/base/lookup_string_in_fixed_set.h"
 #include "net/base/registry_controlled_domains/registry_controlled_domain.h"
 #include "net/extras/preload_data/decoder.h"
@@ -151,14 +152,12 @@ bool IsSubdomainOf(std::u16string_view hostname,
                         base::CompareCase::INSENSITIVE_ASCII);
 }
 
-#include "components/url_formatter/spoof_checks/top_domains/domains-trie-inc.cc"
-
 // All the domains in the above file have 4 or fewer labels.
 const size_t kNumberOfLabelsToCheck = 4;
 
 IDNSpoofChecker::HuffmanTrieParams g_trie_params{
-    kTopDomainsHuffmanTree, sizeof(kTopDomainsHuffmanTree), kTopDomainsTrie,
-    kTopDomainsTrieBits, kTopDomainsRootPosition};
+    kTopDomainsHuffmanTree.data(), kTopDomainsHuffmanTree.size(),
+    kTopDomainsTrie.data(), kTopDomainsTrieBits, kTopDomainsRootPosition};
 
 // Allow these common words that are whole script confusables. They aren't
 // confusable with any words in Latin scripts.
@@ -835,8 +834,8 @@ void IDNSpoofChecker::SetTrieParamsForTesting(
 // static
 void IDNSpoofChecker::RestoreTrieParamsForTesting() {
   g_trie_params = HuffmanTrieParams{
-      kTopDomainsHuffmanTree, sizeof(kTopDomainsHuffmanTree), kTopDomainsTrie,
-      kTopDomainsTrieBits, kTopDomainsRootPosition};
+      kTopDomainsHuffmanTree.data(), kTopDomainsHuffmanTree.size(),
+      kTopDomainsTrie.data(), kTopDomainsTrieBits, kTopDomainsRootPosition};
 }
 
 }  // namespace url_formatter
