6c8892d1856f2f273a4997894f45656abf49a3df
David M <davidjm@google.com>
2025-10-13 10:06:18 -0700

    Only show ShopCard arm 4 for price tracking users
    This arm underperformed on our experiment. One theory
    is that the card is shown out of context to users
    not familiar with price tracking. This CL updates
    the arm to only show the card for users with 1 or more
    price tracked items. Otherwise regular tab resumption
    is shown.
    
    Bug: 451581864
    Change-Id: Ie8f2d0e2731bb8963c876c1fa3d7d34d2ad1efaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7037731
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1528978}

--- ios/chrome/browser/content_suggestions/ui_bundled/tab_resumption/tab_resumption_mediator.mm
+++ ios/chrome/browser/content_suggestions/ui_bundled/tab_resumption/tab_resumption_mediator.mm
@@ -853,9 +853,13 @@ class TabResumptionMediatorProxy {
           if (!strongSelf || !strongSelf.delegate) {
             return;
           }
-          [strongSelf onPriceTrackedBookmarksReceived:subscriptions
-                                                  url:url
-                                                 item:item];
+          if (subscriptions.empty()) {
+            [strongSelf fetchImageForItem:item];
+          } else {
+            [strongSelf onPriceTrackedBookmarksReceived:subscriptions
+                                                    url:url
+                                                   item:item];
+          }
         }));
   } else {
     // Fetch the favicon.
