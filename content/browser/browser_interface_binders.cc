c26b9640e184ceef2ccf66881277c89ee1ddc227
Chris Staite <christopher.staite@menlosecurity.com>
2025-10-13 01:04:21 -0700

    Allow embedder to override Battery Monitor
    The ability to override Binders registered with PopulateBinderMapWithContext should be extended to all Binders.
    
    This CL moves device::mojom::BatteryMonitor.  This final override cleans up the header significantly.
    
    Bug: 405869197
    Change-Id: I57bdbbed0fc076c59369db81c9ae68e69bf91461
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7030884
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Mike West <mkwst@chromium.org>
    Reviewed-by: Leon Han <shulianghan@microsoft.com>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1528775}

--- content/browser/browser_interface_binders.cc
+++ content/browser/browser_interface_binders.cc
@@ -620,15 +620,9 @@ void EmptyBinderForFrame(RenderFrameHost* host,
            << " for the frame/document scope";
 }
 
-BatteryMonitorBinder& GetBatteryMonitorBinderOverride() {
-  static base::NoDestructor<BatteryMonitorBinder> binder;
-  return *binder;
-}
-
 void BindBatteryMonitor(
-    RenderFrameHostImpl* host,
+    RenderFrameHost* host,
     mojo::PendingReceiver<device::mojom::BatteryMonitor> receiver) {
-  const auto& binder = GetBatteryMonitorBinderOverride();
   // TODO(crbug.com/1007264, crbug.com/1290231): remove fenced frame specific
   // code when permission policy implements the battery status API support.
   if (host->IsNestedWithinFencedFrame()) {
@@ -637,10 +631,7 @@ void BindBatteryMonitor(
                                 BIBI_BIND_BATTERY_MONITOR_FOR_FENCED_FRAME);
     return;
   }
-  if (binder)
-    binder.Run(std::move(receiver));
-  else
-    GetDeviceService().BindBatteryMonitor(std::move(receiver));
+  GetDeviceService().BindBatteryMonitor(std::move(receiver));
 }
 
 #if BUILDFLAG(ENABLE_COMPUTE_PRESSURE)
@@ -721,9 +712,6 @@ void BindRenderFrameHostImpl(RenderFrameHost* host,
 void PopulateFrameBinders(RenderFrameHostImpl* host, mojo::BinderMap* map) {
   map->Add<blink::mojom::AudioContextManager>(base::BindRepeating(
       &RenderFrameHostImpl::GetAudioContextManager, base::Unretained(host)));
-
-  map->Add<device::mojom::BatteryMonitor>(
-      base::BindRepeating(&BindBatteryMonitor, base::Unretained(host)));
 
   map->Add<blink::mojom::CacheStorage>(base::BindRepeating(
       &RenderFrameHostImpl::BindCacheStorage, base::Unretained(host)));
@@ -1159,6 +1147,7 @@ void PopulateBinderMapWithContext(
 
   map->Add<blink::mojom::BackgroundFetchService>(
       &BackgroundFetchServiceImpl::CreateForFrame);
+  map->Add<device::mojom::BatteryMonitor>(&BindBatteryMonitor);
   map->Add<blink::mojom::ColorChooserFactory>(&BindColorChooserFactoryForFrame);
   map->Add<blink::mojom::EyeDropperChooser>(&EyeDropperChooserImpl::Create);
   map->Add<blink::mojom::CookieStore>(
@@ -1805,9 +1794,4 @@ void PopulateBinderMap(ServiceWorkerHost* host, mojo::BinderMap* map) {
 }
 
 }  // namespace internal
-
-void OverrideBatteryMonitorBinderForTesting(BatteryMonitorBinder binder) {
-  internal::GetBatteryMonitorBinderOverride() = std::move(binder);
-}
-
 }  // namespace content
