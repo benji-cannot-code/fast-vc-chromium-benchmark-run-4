84db8022527b1166b918a622f5d8dcf6d18f35e8
Omar Flores <omarflores@google.com>
2025-10-13 16:14:19 -0700

    [tree view] Setting up correct uma metric for tree view elements
    Updating the uma metric to reflect any nodes that are expanded in the
    tree view under the bookmarks side panel.
    
    Bug: 378534484
    Change-Id: I66e2766b1312ca9d843df14965d3e813b254150a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6987222
    Reviewed-by: Alison Gale <agale@chromium.org>
    Commit-Queue: Omar Flores <omarflores@google.com>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1529191}

--- chrome/browser/resources/side_panel/bookmarks/power_bookmarks_list.ts
+++ chrome/browser/resources/side_panel/bookmarks/power_bookmarks_list.ts
@@ -840,7 +840,8 @@ export class PowerBookmarksListElement extends PolymerElement implements
   }
 
   private recordBookmarkCountMetrics_() {
-    const count =
+    const count = this.bookmarksTreeViewEnabled_ ?
+        this.keyArrowNavigationService_.getElementCount() :
         this.displayLists_.reduce((prev, curr) => prev + curr.length, 0);
     const metricName = `PowerBookmarks.SidePanel${
         this.hasSomeActiveFilter_ ? '.SearchOrFilter' : ''}.BookmarksShown`;
@@ -871,6 +872,7 @@ export class PowerBookmarksListElement extends PolymerElement implements
     event: MouseEvent,
   }>) {
     this.notifyBookmarksListResize_();
+    afterNextRender(this, () => this.recordBookmarkCountMetrics_());
   }
   /**
    * Invoked when the user clicks a power bookmarks row. This will either
