84f1a205c217e13ce5b9f02edb71586ce35343c4
Suyeon Ji <zeesuyeon@gmail.com>
2025-10-13 07:59:52 -0700

    Refactor trusted types to use WindowOrWorkerGlobalScope partial mixin
    This patch aligns trustedTypes with the WindowOrWorkerGlobalScope mixin
    to follow the spec. It introduces GlobalTrustedTypes to handle both
    Window and WorkerGlobalScope cases.
    
    This CL has no behavior changes.
    
    Bug: 449023252
    Change-Id: I2d12ba54486e5c8bd0f50a80062fc7f7332e1f1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7027351
    Reviewed-by: Jinho Bang <zino@chromium.org>
    Commit-Queue: Jinho Bang <zino@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1528909}

---
+++ third_party/blink/renderer/core/trustedtypes/global_trusted_types.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/core/trustedtypes/global_trusted_types.h"
+
+#include "third_party/blink/renderer/core/frame/local_dom_window.h"
+#include "third_party/blink/renderer/core/workers/worker_global_scope.h"
+
+namespace blink {
+
+// static
+TrustedTypePolicyFactory* GlobalTrustedTypes::trustedTypes(
+    ScriptState* script_state,
+    LocalDOMWindow& window) {
+  return window.GetTrustedTypesForWorld(script_state->World());
+}
+
+// static
+TrustedTypePolicyFactory* GlobalTrustedTypes::trustedTypes(
+    ScriptState* script_state,
+    WorkerGlobalScope& worker) {
+  return worker.GetTrustedTypes();
+}
+
+}  // namespace blink
