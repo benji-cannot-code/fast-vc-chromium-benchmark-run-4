c2e90a571a35661640a521483206cb1ec91bb609
Maggie Jennings <mjenn@google.com>
2025-10-13 10:19:17 -0700

    [Task Icon] Update text to "Check your task"
    Also removes highlighting logic based on the floaty view shown as the
    actuation view has been removed. Without the actuation view to show, the
    highlighting logic results in the Gemini button being highlighted
    whenever the floaty is showing.
    
    Before: https://screenshot.googleplex.com/A4byrDmKaJTknHM
    After: https://screenshot.googleplex.com/8h2xTKwK4mZnDR3
    
    Bug: 450924642, 440650782
    Change-Id: I56f791de9c2bd6218c223023b66e81182d7dc82e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7037071
    Commit-Queue: Maggie Jennings <mjenn@google.com>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1528986}

--- chrome/browser/ui/views/tabs/tab_strip_action_container_browsertest.cc
+++ chrome/browser/ui/views/tabs/tab_strip_action_container_browsertest.cc
@@ -609,7 +609,7 @@ IN_PROC_BROWSER_TEST_F(TabStripActionContainerBrowserTest,
   EXPECT_TRUE(GlicActorTaskIcon()->GetVisible());
   EXPECT_TRUE(GlicActorTaskIcon()->GetIsShowingNudge());
   // TODO(crbug.com/431015299): Replace with finalized strings when ready.
-  EXPECT_EQ(GlicActorTaskIcon()->GetText(), u"Your task needs attention");
+  EXPECT_EQ(GlicActorTaskIcon()->GetText(), u"Check your task");
 
   ResetAnimation(1);
 
@@ -645,7 +645,7 @@ IN_PROC_BROWSER_TEST_F(TabStripActionContainerBrowserTest,
   EXPECT_TRUE(GlicActorTaskIcon()->GetVisible());
   EXPECT_TRUE(GlicActorTaskIcon()->GetIsShowingNudge());
   // TODO(crbug.com/431015299): Replace with finalized strings when ready.
-  EXPECT_EQ(GlicActorTaskIcon()->GetText(), u"Your task needs attention");
+  EXPECT_EQ(GlicActorTaskIcon()->GetText(), u"Check your task");
 
   ResetAnimation(1);
 
