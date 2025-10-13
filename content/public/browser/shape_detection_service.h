9dd7a7717ff4d55cb841387b7a6d0bc4a9fd7705
dongguoz <dongguoz@google.com>
2025-10-13 10:28:33 -0700

    Refactor: Enable ShapeDetectionService to content/public/browser
    This CL allows the ShapeDetectionService to be used by other
    components. This is a refactoring that does not change any
    functionality
    
    Design doc: go/shape-detection-service-content-public-api
    
    Bug: 432158448
    Change-Id: I6bcc5fe62b66e973a18d186774ffc7fc0f39a8c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6992001
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Commit-Queue: Dongguo Zhang <dongguo@google.com>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1528993}

---
+++ content/public/browser/shape_detection_service.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_PUBLIC_BROWSER_SHAPE_DETECTION_SERVICE_H_
+#define CONTENT_PUBLIC_BROWSER_SHAPE_DETECTION_SERVICE_H_
+
+#include "content/common/content_export.h"
+#include "services/shape_detection/public/mojom/shape_detection_service.mojom.h"
+
+namespace content {
+
+// Returns the browser's remote interface to the global ShapeDetectionService
+// instance, which is started lazily.
+CONTENT_EXPORT shape_detection::mojom::ShapeDetectionService*
+GetShapeDetectionService();
+
+}  // namespace content
+
+#endif  // CONTENT_PUBLIC_BROWSER_SHAPE_DETECTION_SERVICE_H_
