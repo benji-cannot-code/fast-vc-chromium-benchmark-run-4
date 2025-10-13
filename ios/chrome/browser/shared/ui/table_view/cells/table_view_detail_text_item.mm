980329686d0e15ffb3fdb27e77c7828d027bbebb
Vidhan Jain <vidhanj@google.com>
2025-10-13 07:48:22 -0700

    [iOS] Convey new tab for TableViewDetailTextCellAccessorySymbolExternalLink
    This change only affects the accessibility label to announce the change
    of tabs for the cell in the voice over mode.
    
    All the current applications of
    TableViewDetailTextCellAccessorySymbolExternalLink open a link in new
    tab.
    
    Bug: 450629638
    Change-Id: I3be181551b0e4404711bcb4953678e2b76169f6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7032004
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1528903}

--- ios/chrome/browser/shared/ui/table_view/cells/table_view_detail_text_item.mm
+++ ios/chrome/browser/shared/ui/table_view/cells/table_view_detail_text_item.mm
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/table_view/table_view_cells_constants.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
+#import "ios/chrome/grit/ios_strings.h"
+#import "ui/base/l10n/l10n_util_mac.h"
 
 #pragma mark - TableViewDetailTextItem
 
@@ -41,7 +43,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   configuration.subtitleNumberOfLines = self.allowMultilineDetailText ? 0 : 1;
 
   cell.contentConfiguration = configuration;
-  cell.accessibilityLabel = configuration.accessibilityLabel;
+
+  NSString* accessibilityLabel = configuration.accessibilityLabel;
+  // If the cell indicates an external link, append an accessibility hint for
+  // screen readers.
+  if (self.accessorySymbol ==
+      TableViewDetailTextCellAccessorySymbolExternalLink) {
+    NSString* hint = l10n_util::GetNSString(IDS_IOS_OPENS_IN_NEW_TAB);
+    accessibilityLabel =
+        [NSString stringWithFormat:@"%@, %@", accessibilityLabel, hint];
+  }
+
+  cell.accessibilityLabel = accessibilityLabel;
 
   // Accessory symbol.
   switch (self.accessorySymbol) {
