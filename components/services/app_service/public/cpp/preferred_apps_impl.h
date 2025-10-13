228ab3184bcb101dbc1bc927268a730695ae9dfe
Andrew Rayskiy <greengrape@google.com>
2025-10-13 06:39:10 -0700

    Adjust protocol link preferences when app handlers change
    This CL listens to incoming app updates and informs the preferred apps
    list if there's a negative delta in handled protocols (i.e. if some
    matching intent filters have been removed).
    
    Bug: 422422887
    Change-Id: I249c40f755b3a1424193d16188db7f6ca71055b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7013695
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Cr-Commit-Position: refs/heads/main@{#1528875}

--- components/services/app_service/public/cpp/preferred_apps_impl.h
+++ components/services/app_service/public/cpp/preferred_apps_impl.h
@@ -62,6 +62,8 @@ class PreferredAppsImpl {
 #if BUILDFLAG(IS_CHROMEOS)
   void SetProtocolLinkPreference(const std::string& app_id,
                                  IntentFilterPtr protocol_link_filter);
+  void RemoveProtocolLinkFilters(const std::string& app_id,
+                                 IntentFilters protocol_link_filters);
 #endif
   void RemoveSupportedLinksPreference(const std::string& app_id);
 
@@ -96,6 +98,8 @@ class PreferredAppsImpl {
 #if BUILDFLAG(IS_CHROMEOS)
   void SetProtocolLinkPreferenceImpl(const std::string& app_id,
                                      IntentFilterPtr protocol_link_filter);
+  void RemoveProtocolLinkFiltersImpl(const std::string& app_id,
+                                     IntentFilters protocol_link_filters);
 #endif
   void RemoveSupportedLinksPreferenceImpl(const std::string& app_id);
 
