44169e35f54e96cbf334f3957876ef597ff1f498
Lauren Winston <lwinston@google.com>
2025-10-13 16:28:21 -0700

    [Read aloud] Handle replaceDomNode differently when Readability is used.
    Note that the flag is still a no-op, but this was a pretty major
    bug noticed when testing Readability.js locally, so it is better
    to go ahead and fix this now to reduce debug time in the future
    when Readability or alternative distillation methods are experimented
    with.
    
    This fix will allow highlighting to work with alternative
    distillation methods instead of crashing.
    
    Bug: 440400392
    Change-Id: I2bbe639e87f60e38a7bad77f8fb46973ec17aa53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7038923
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1529196}

--- chrome/browser/resources/side_panel/read_anything/read_anything.d.ts
+++ chrome/browser/resources/side_panel/read_anything/read_anything.d.ts
@@ -81,6 +81,9 @@ declare namespace chrome {
     // Whether the TS text segmentation feature flag is enabled.
     let isTsTextSegmentationEnabled: boolean;
 
+    // Whether Readability.js is used as the primary distillation method.
+    let isReadabilityEnabled: boolean;
+
     // Whether the phrase highlighting feature flag is enabled.
     let isPhraseHighlightingEnabled: boolean;
 
