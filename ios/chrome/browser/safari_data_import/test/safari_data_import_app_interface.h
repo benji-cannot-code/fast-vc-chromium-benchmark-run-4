b9018a0638141685fea17d54e6bbcd98b7670034
ginnnnnnny <ginnyhuang@chromium.org>
2025-10-14 11:27:59 -0700

    [iOS] Add more test cases for Safari data import
    Bug: 441124865
    Change-Id: Ibc47d1194266d6a7680e02931e6c05006153f2e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7036736
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1529671}

--- ios/chrome/browser/safari_data_import/test/safari_data_import_app_interface.h
+++ ios/chrome/browser/safari_data_import/test/safari_data_import_app_interface.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <UIKit/UIKit.h>
 
 /// Available files that could be used for Safari data import test cases.
-enum class SafariDataImportTestFile { kValid, kInvalid };
+enum class SafariDataImportTestFile { kValid, kPartiallyValid, kInvalid };
 
 @interface SafariDataImportAppInterface : NSObject
 
