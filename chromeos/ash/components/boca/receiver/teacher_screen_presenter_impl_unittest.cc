5bc3564e29905979b6ad7df1e7cb822cd15ae6aa
Ahmed Nasr <anasr@google.com>
2025-10-14 07:46:58 -0700

    Add IsPresenting method to TeacherScreenPresenter.
    Bug: b:450507101
    Change-Id: Ic9431f5b75dff28c9c675b222d76e3a2be4323e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7027774
    Reviewed-by: Benjamin Zielinski <bzielinski@google.com>
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1529526}

--- chromeos/ash/components/boca/receiver/teacher_screen_presenter_impl_unittest.cc
+++ chromeos/ash/components/boca/receiver/teacher_screen_presenter_impl_unittest.cc
@@ -119,6 +119,7 @@ TEST_F(TeacherScreenPresenterImplTest, StartFailureOnGetConnectionCode) {
                   base::DoNothing());
 
   EXPECT_FALSE(start_future.Get());
+  EXPECT_FALSE(presenter.IsPresenting());
 }
 
 TEST_F(TeacherScreenPresenterImplTest, Stop) {
@@ -150,6 +151,7 @@ TEST_F(TeacherScreenPresenterImplTest, Stop) {
   presenter.Stop(stop_future.GetCallback());
   EXPECT_TRUE(stop_future.Get());
   EXPECT_FALSE(disconnected_called);
+  EXPECT_FALSE(presenter.IsPresenting());
 }
 
 TEST_F(TeacherScreenPresenterImplTest, StopFailsWhenStartInProgress) {
@@ -211,6 +213,7 @@ TEST_P(TeacherScreenPresenterImplTest, Start) {
                     disconnected_called = true;
                   }));
 
+  EXPECT_TRUE(presenter.IsPresenting());
   EXPECT_EQ(start_future.Get(), GetParam().start_success);
 
   std::move(session_finished_callback).Run();
