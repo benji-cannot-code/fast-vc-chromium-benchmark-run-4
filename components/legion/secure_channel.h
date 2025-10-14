8a9a48ac468c48c22b4db13743cf5a0a9e82d751
Nikhil Jakhar <nikhiljakhar@google.com>
2025-10-14 01:28:47 -0700

    legion: Implement the SecureChannel class
    This CL provides the concrete implementation of the `SecureChannel`
    layer. It orchestrates the secure communication flow, including
    attestation, Oak handshake, and encryption/decryption of requests and
    responses. The `SecureChannel` uses a state machine to manage the
    different stages of the secure channel establishment. The `Transport`
    interface is updated to allow setting a message received callback.
    
    Bug: b:444610593
    Change-Id: I648e079c84872eeefb45c879ddb34bd3398cb29c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7016847
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Mohamad Arab <arabm@google.com>
    Commit-Queue: Nikhil Jakhar <nikhiljakhar@google.com>
    Cr-Commit-Position: refs/heads/main@{#1529374}

--- components/legion/secure_channel.h
+++ components/legion/secure_channel.h
@@ -6,10 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_LEGION_SECURE_CHANNEL_H_
 #define COMPONENTS_LEGION_SECURE_CHANNEL_H_
 
-#include <cstdint>
-#include <memory>
 #include <optional>
-#include <vector>
 
 #include "base/functional/callback.h"
 #include "components/legion/legion_common.h"
@@ -26,6 +23,14 @@ enum class ResultCode {
   kAuthenticationFailed,
   // A transient network error occurred. The client may retry the request.
   kNetworkError,
+  // Attestation failed. The client should not retry the request.
+  kAttestationFailed,
+  // Handshake or attestation failed. The client should not retry the request.
+  kHandshakeFailed,
+  // Encryption failed. The client should not retry the request.
+  kEncryptionFailed,
+  // Decryption failed. The client should not retry the request.
+  kDecryptionFailed,
 };
 
 // Interface for the Secure Channel Layer.
@@ -34,13 +39,13 @@ enum class ResultCode {
 // and using the WebSocketClient for transport.
 class SecureChannel {
  public:
-  using OnWriteCompletedCallback =
+  using OnResponseReceivedCallback =
       base::OnceCallback<void(ResultCode, std::optional<Response>)>;
 
   virtual ~SecureChannel() = default;
 
   // Asynchronously performs the operation over the secure channel.
-  virtual void Write(Request request, OnWriteCompletedCallback callback) = 0;
+  virtual void Write(Request request, OnResponseReceivedCallback callback) = 0;
 };
 
 }  // namespace legion
