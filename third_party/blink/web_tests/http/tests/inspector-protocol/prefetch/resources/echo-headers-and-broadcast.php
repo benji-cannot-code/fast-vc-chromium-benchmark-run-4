150cfb3452067e2155dcca952a33310215939466
HuanPo Lin <robertlin@chromium.org>
2025-10-14 04:46:41 -0700

    Reland "prefetch: Reflect Devtools emulated user agent override in headers"
    This is a reland of commit e62f0f7c7367a140329448d355757fe8845d8692
    
    The reason why the original CL was failing:
    https://chromium-review.googlesource.com/c/chromium/src/+/7015190
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20MSan%20Tests/59207/overview
    The CL was detected of use-of-uninitialized-value by MSan.
    
    Diff from the original CLs:
    `DevtoolsOverriddenOutputParams` in `content/browser/devtools/devtools_instrumentation.h` has been given
    default value.
    The problem can be reproduce on `linux_chromium_msan_rel_ng ` trybot
    without the update, and the trybot is passing with the update.
    
    Original change's description:
    > prefetch: Reflect Devtools emulated user agent override in headers
    >
    > Currently, if a navigation used prefetched result and the User-Agent
    > override information are unexpected values. This CL makes the prefetch
    > uses the same userAgent header values from Devtools emulation mode as
    > the referrer page.
    >
    > Related document:
    > https://docs.google.com/document/d/1ONfF4dfeVP5T0HHNfma6B3yL4muYukC7aLmFUuHvmVY/edit?usp=sharing
    >
    > Bug: 422193319, 444065296
    > Change-Id: Id5ec30c56428d9c39dd961f030b1bab9c12dc65c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6719107
    > Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    > Reviewed-by: Ken Okada <kenoss@chromium.org>
    > Reviewed-by: Takashi Nakayama <tnak@chromium.org>
    > Reviewed-by: Victor Tan <victortan@chromium.org>
    > Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1525993}
    
    Bug: 422193319, 444065296
    Change-Id: I14955192936e95e01f1adb9863eddee561e2a7d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7015170
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Takashi Nakayama <tnak@chromium.org>
    Reviewed-by: Ken Okada <kenoss@chromium.org>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1529431}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/prefetch/resources/echo-headers-and-broadcast.php
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<!doctype html>
+<html>
+<body>
+<?php
+foreach (getallheaders() as $name => $value) {
+    echo "$name: $value\n";
+}
+?>
+</body>
+</html>
