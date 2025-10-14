6e9f8dc57839474fa7eda3e375efca31d47f5f18
Tzarial <zork@chromium.org>
2025-10-14 12:40:29 -0700

    Reland "[a11y] Implement Android chrome text cursor blink interval for caret"
    This reverts commit 63c6cb72e5ff51f6d31b5ba028bb446cbec81a45.
    
    Reason for revert: Relanding change with fixes for build failure.
    
    Original change's description:
    > Revert "[a11y] Implement Android chrome text cursor blink interval for caret"
    >
    > This reverts commit 82b75d34183c4a3a152b8e0232589b2242153703.
    >
    > Reason for revert:
    > https://g-issues.chromium.org/issues/449229043
    >
    > Original change's description:
    > > [a11y] Implement Android chrome text cursor blink interval for caret
    > >
    > > Currently, Android Chrome uses a hardcoded blink interval for the text
    > > input caret and does not respect the user's preference set in the
    > > Android system settings. This CL improves platform integration and
    > > accessibility by aligning Chrome's behavior with the rest of the OS.
    > > This is implemented by calling getTextCursorBlinkIntervalMillis() API.
    > > The public documentation is here:
    > > https://developer.android.com/reference/android/view/ViewConfiguration#getTextCursorBlinkIntervalMillis()
    > >
    > > Chrome from the system settings.
    > >
    > > AX-Relnotes: The user can now adjust the cursor blinking rate in Android
    > > Fixed: 430011534
    > > Change-Id: Ibb3b41d985e4e577e7ab6f5811445e02d74a7f91
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6862412
    > > Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    > > Reviewed-by: Wez <wez@chromium.org>
    > > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > > Commit-Queue: Tzarial <zork@chromium.org>
    > > Reviewed-by: Amanda Lin Dietz <aldietz@google.com>
    > > Reviewed-by: Avi Drissman <avi@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1524180}
    >
    > Change-Id: I95c7b9d3c5f161c3870424cc7526ec689a3ae5ff
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7009151
    > Commit-Queue: Shu Yang <shuyng@google.com>
    > Owners-Override: Shu Yang <shuyng@google.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1525151}
    
    Change-Id: I7ec73cec7bdad2e6c667e8c0b2b71f759115cc6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7022594
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Tzarial <zork@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1529715}

--- ui/accessibility/android/java/src/org/chromium/ui/accessibility/AccessibilityState.java
+++ ui/accessibility/android/java/src/org/chromium/ui/accessibility/AccessibilityState.java
@@ -27,6 +27,7 @@ import android.os.Build;
 import android.os.Handler;
 import android.os.SystemClock;
 import android.provider.Settings;
+import android.view.ViewConfiguration;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityManager;
 import android.view.autofill.AutofillManager;
@@ -37,11 +38,13 @@ import org.jni_zero.CalledByNative;
 import org.jni_zero.JNINamespace;
 import org.jni_zero.NativeMethods;
 
+import org.chromium.base.AconfigFlaggedApiDelegate;
 import org.chromium.base.ActivityState;
 import org.chromium.base.ApplicationState;
 import org.chromium.base.ApplicationStatus;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
+import org.chromium.base.ServiceLoaderUtil;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.build.annotations.NullMarked;
@@ -86,6 +89,7 @@ public class AccessibilityState {
             "Accessibility.Android.UpdateAccessibilityServices.Runtime";
     private static final int MAX_RUNTIME_BUCKET = 16 * 1000; // 16,000 microseconds = 16ms.
     private static int sPollCount;
+    private static @Nullable AconfigFlaggedApiDelegate sAconfigFlaggedApiDelegate;
 
     /** Interface for the observers of the system's accessibility state. */
     public interface Listener {
@@ -254,6 +258,7 @@ public class AccessibilityState {
     private static boolean sDisplayInversionEnabled;
     private static boolean sHighContrastEnabled;
     private static int sFontWeightAdjustment;
+    private static int sTextCursorBlinkInterval;
     private static float sAnimatorDurationScale;
 
     // Observers for various System, Activity, and Settings states relevant to accessibility.
@@ -490,6 +495,13 @@ public class AccessibilityState {
         return sAnimatorDurationScale;
     }
 
+    /** Returns the current TEXT_CURSOR_BLINK_INTERVAL from the users OS accessibility settings. */
+    @CalledByNative
+    public static int getTextCursorBlinkInterval() {
+        if (!sExtraStateInitialized) updateExtraState();
+        return sTextCursorBlinkInterval;
+    }
+
     /** Returns whether the user settings specify preferred reduced motion. */
     @CalledByNative
     public static boolean prefersReducedMotion() {
@@ -524,6 +536,19 @@ public class AccessibilityState {
                         ContextUtils.getApplicationContext().getContentResolver(),
                         Settings.Global.ANIMATOR_DURATION_SCALE,
                         1f);
+
+        if (sAconfigFlaggedApiDelegate == null) {
+            sAconfigFlaggedApiDelegate =
+                    ServiceLoaderUtil.maybeCreate(AconfigFlaggedApiDelegate.class);
+        }
+
+        if (sAconfigFlaggedApiDelegate != null && context instanceof Activity) {
+            ViewConfiguration viewConfiguration = ViewConfiguration.get(context);
+            sTextCursorBlinkInterval =
+                    sAconfigFlaggedApiDelegate.getTextCursorBlinkInterval(viewConfiguration);
+        } else {
+            sTextCursorBlinkInterval = 500;
+        }
 
         int highTextContrastEnabled =
                 Settings.Secure.getInt(
@@ -1039,7 +1064,7 @@ public class AccessibilityState {
         contentResolver.unregisterContentObserver(sTextContrastObserver);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
             UiModeManager uiModeManager =
-                (UiModeManager) context.getSystemService(Context.UI_MODE_SERVICE);
+                    (UiModeManager) context.getSystemService(Context.UI_MODE_SERVICE);
             if (uiModeManager != null && sContrastChangeListener != null) {
                 uiModeManager.removeContrastChangeListener(sContrastChangeListener);
             }
@@ -1054,6 +1079,7 @@ public class AccessibilityState {
         sHighContrastEnabled = false;
         sAnimatorDurationScale = 1f;
         sAccessibilityManager = null;
+        sTextCursorBlinkInterval = 500;
     }
 
     private static void processServicesChange() {
@@ -1066,6 +1092,7 @@ public class AccessibilityState {
         AccessibilityStateJni.get().onAnimatorDurationScaleChanged();
         AccessibilityStateJni.get().onDisplayInversionEnabledChanged(isDisplayInversionEnabled());
         AccessibilityStateJni.get().onContrastLevelChanged(isHighContrastEnabled());
+        AccessibilityStateJni.get().onTextCursorBlinkIntervalChanged(getTextCursorBlinkInterval());
     }
 
     private static class ServicesObserver extends ContentObserver {
@@ -1094,6 +1121,8 @@ public class AccessibilityState {
         void onDisplayInversionEnabledChanged(boolean enabled);
 
         void onContrastLevelChanged(boolean highContrastEnabled);
+
+        void onTextCursorBlinkIntervalChanged(int textCursorBlinkInterval);
 
         void recordAccessibilityServiceInfoHistograms();
     }
