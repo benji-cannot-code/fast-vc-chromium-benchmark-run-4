93e12a5708d92c3ccdaa21026a1ba3481136aed1
Ivana Žužić <izuzic@google.com>
2025-10-14 10:30:18 -0700

    [PasskeyUnlockErrorUI] Introduce PasskeyUnlockManager and its factory.
    This change adds the `PasskeyUnlockManager` as a `KeyedService` to track
    the state of passkey unlockability. It observes `EnclaveManager`,
    `PasskeyModel`, and `SyncService` to determine if passkeys are locked
    and can be unlocked. The manager will notify observers when the unlock
    state changes. The factory is added to
    `ChromeBrowserMainExtraPartsProfiles` and depends on
    `PasskeyModelFactory`, `EnclaveManagerFactory` and `SyncServiceFactory`.
    
    passkey unlock manager skeleton. The logic will be introduced and tested
    in a later CL.
    
    Change-Id: I868780296ebd3e7c1095a5c206c3f4ac2eb83a3f
    Bug: 449948647
    Low-Coverage-Reason: TRIVIAL_CHANGE This change introduces only the
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7021632
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Adem Derinel <derinel@google.com>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1529609}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -442,6 +442,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if !BUILDFLAG(IS_ANDROID)
 #include "chrome/browser/password_manager/startup_passwords_import_service_factory.h"  // nogncheck (Desktop only)
+#include "chrome/browser/webauthn/passkey_unlock_manager_factory.h"
+#include "device/fido/features.h"
 #endif
 #if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_WIN)
 #include "chrome/browser/policy/messaging_layer/util/manual_test_heartbeat_event_factory.h"
@@ -1141,6 +1143,11 @@ void ChromeBrowserMainExtraPartsProfiles::
   PageColorsControllerFactory::GetInstance();
 #endif
   passage_embeddings::PassageEmbedderModelObserverFactory::GetInstance();
+#if !BUILDFLAG(IS_ANDROID)
+  if (base::FeatureList::IsEnabled(device::kPasskeyUnlockErrorUi)) {
+    webauthn::PasskeyUnlockManagerFactory::GetInstance();
+  }
+#endif
   password_manager::PasswordManagerLogRouterFactory::GetInstance();
   password_manager::PasswordRequirementsServiceFactory::GetInstance();
 #if BUILDFLAG(BUILD_WITH_TFLITE_LIB)
