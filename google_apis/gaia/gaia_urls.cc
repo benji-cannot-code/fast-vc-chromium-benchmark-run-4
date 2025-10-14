60fa1f92b35a048d2ea46122b6ee6662a254028c
Amelie Schneider <amelies@google.com>
2025-10-14 07:35:40 -0700

    Append `laf=b64bin` to `ListAccountsURLWithSource()`
    Also includes some additional clean up of the legacy format.
    
    Bug: 443901858
    Change-Id: I372b5048b973d22b1aaa016d0b2194c37be28aa7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7030881
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Amelie Schneider <amelies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1529520}

--- google_apis/gaia/gaia_urls.cc
+++ google_apis/gaia/gaia_urls.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "google_apis/gaia/gaia_features.h"
 #include "google_apis/gaia/gaia_switches.h"
 #include "google_apis/google_api_keys.h"
+#include "net/base/url_util.h"
 #include "url/url_canon.h"
 #include "url/url_constants.h"
 
@@ -72,7 +73,7 @@ const char kSigninChromeSyncKeysRecoverabilityUrlSuffix[] =
 const char kServiceLogoutUrlSuffix[] = "Logout";
 const char kBlankPageSuffix[] = "chrome/blank.html";
 const char kOAuthMultiloginSuffix[] = "oauth/multilogin";
-const char kListAccountsSuffix[] = "ListAccounts?json=standard";
+const char kListAccountsSuffix[] = "ListAccounts?json=standard&laf=b64bin";
 const char kEmbeddedSigninSuffix[] = "embedded/setup/chrome/usermenu";
 const char kAddAccountSuffix[] = "AddSession";
 const char kReauthSuffix[] = "embedded/xreauth/chrome";
@@ -331,12 +332,11 @@ const GURL& GaiaUrls::google_apis_origin_url() const {
 
 GURL GaiaUrls::ListAccountsURLWithSource(const std::string& source) {
   if (source.empty()) {
-    // TODO(crbug.com/443901858): Append laf=b64bin parameter universally.
     return list_accounts_url_;
   } else {
     std::string query = list_accounts_url_.GetQuery();
     return list_accounts_url_.Resolve(base::StringPrintf(
-        "?gpsia=1&source=%s&laf=b64bin&%s", source.c_str(), query.c_str()));
+        "?gpsia=1&source=%s&%s", source.c_str(), query.c_str()));
   }
 }
 
