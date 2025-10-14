6dd1db356ff230a21b3aa5b67664dfa1e2bf13f7
harringtond <harringtond@google.com>
2025-10-14 14:33:14 -0700

    Try to fix flakes in testCreateTabByClickingOnLink
    If clicking opens a tab at index 0, it would break our
    test. Update the test to track the glic instance
    correctly, regardless of tab index.
    
    Also clean up glic instance tracking functions
    
    Bug: b:449366743
    Change-Id: I6a6a696463ae69f1eb4b85194ec25752c5d42e64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7027376
    Reviewed-by: Nick Birnie <birnie@google.com>
    Commit-Queue: Dan Harrington <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1529788}

--- chrome/browser/glic/host/glic_api_browsertest.cc
+++ chrome/browser/glic/host/glic_api_browsertest.cc
@@ -791,6 +791,8 @@ IN_PROC_BROWSER_TEST_P(GlicApiTest, testCreateTabByClickingOnLink) {
   RunTestSequence(OpenGlicWindow(GlicWindowMode::kDetached,
                                  GlicInstrumentMode::kHostAndContents),
                   CheckTabCount(1));
+  // Have the test track this tab's glic instance.
+  TrackGlicInstanceWithId(GetGlicInstance()->id());
   content::RenderFrameHost* guest_frame = FindGlicGuestMainFrame();
   ExecuteJsTest();
   ASSERT_TRUE(base::test::RunUntil([&]() {
@@ -893,7 +895,7 @@ IN_PROC_BROWSER_TEST_P(GlicApiTest, testMultiplePanelsDetachedAndFloating) {
 
   // Select the second tab, open glic, and execute the test on the second
   // instance.
-  SetGlicInstanceTabIndex(1);
+  TrackGlicInstanceWithTabIndex(1);
   browser()->tab_strip_model()->ActivateTabAt(1);
   RunTestSequence(InstrumentTab(kSecondTab),
                   OpenGlicWindow(GlicWindowMode::kDetached,
@@ -901,7 +903,7 @@ IN_PROC_BROWSER_TEST_P(GlicApiTest, testMultiplePanelsDetachedAndFloating) {
   ExecuteJsTest({.params = base::Value("second")});
 
   // Continue on the first tab.
-  SetGlicInstanceTabIndex(0);
+  TrackGlicInstanceWithTabIndex(0);
   ContinueJsTest();
 }
 
@@ -986,7 +988,7 @@ IN_PROC_BROWSER_TEST_P(GlicApiTest, testIsBrowserOpen) {
   browser_activator().SetMode(BrowserActivator::Mode::kFirst);
   RunTestSequence(OpenGlicWindow(GlicWindowMode::kDetached,
                                  GlicInstrumentMode::kHostAndContents));
-  TrackGlicInstanceById(GetGlicInstance()->id());
+  TrackGlicInstanceWithId(GetGlicInstance()->id());
   ExecuteJsTest();
 
   // Open a new incognito tab so that Chrome doesn't exit, and close the first
@@ -1239,7 +1241,7 @@ IN_PROC_BROWSER_TEST_P(GlicApiTestWithOneTab,
 IN_PROC_BROWSER_TEST_P(GlicApiTestWithOneTab,
                        testGetFocusedTabStateV2WithNavigationWhenInactive) {
   SKIP_TEST_FOR_MULTI_INSTANCE();
-  TrackGlicInstanceById(GetGlicInstance()->id());
+  TrackGlicInstanceWithId(GetGlicInstance()->id());
   // Confirm that the observer is notified through getFocusedTabState of the
   // initial state, i.e. the first page navigation. It should then hide.
   ExecuteJsTest();
@@ -2313,7 +2315,7 @@ IN_PROC_BROWSER_TEST_P(GlicApiTestWithOneTab,
   // resulting in deleting the second glic instance.
   ASSERT_TRUE(AddTabAtIndex(1, GURL("about:blank"), ui::PAGE_TRANSITION_TYPED));
   browser()->tab_strip_model()->ActivateTabAt(1);
-  SetGlicInstanceTabIndex(1);
+  TrackGlicInstanceWithTabIndex(1);
   RunTestSequence(InstrumentTab(kSecondTab),
                   OpenGlicWindow(GlicWindowMode::kDetached,
                                  GlicInstrumentMode::kHostAndContents));
