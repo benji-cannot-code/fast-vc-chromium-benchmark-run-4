ee0401bea0b079c2e1d3b38499bffc6a83a46e06
Boris Sazonov <bsazonov@chromium.org>
2025-10-14 02:34:30 -0700

    [Signin][iOS] Add AccountRetrievalWaitsForRestoration feature flag
    Not used for now - will be used in subsequent internal CLs.
    
    Bug: 448822558
    Change-Id: Ic01097e848fe7bf942b7321c9e9246f103c29ad0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7036696
    Reviewed-by: Menghan Yang <myuu@google.com>
    Reviewed-by: Liza Bipin <mlbipin@google.com>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1529394}

--- components/signin/public/base/signin_switches.cc
+++ components/signin/public/base/signin_switches.cc
@@ -36,6 +36,8 @@ const char kForceFreDefaultBrowserStep[] = "force-fre-default-browser-step";
 // clang-format on
 
 #if BUILDFLAG(IS_IOS)
+BASE_FEATURE(kAccountRetrievalWaitsForRestoration,
+             base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kAllowlistScopesForMdmErrors, base::FEATURE_DISABLED_BY_DEFAULT);
 #endif
 
