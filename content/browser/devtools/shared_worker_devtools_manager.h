f13e48a9a03506e0c1ad2c36b561ab96a818a42f
Eden Wang <nedenwang@gmail.com>
2025-10-14 03:07:43 -0700

    DevTools: Implement waitForDebuggerOnStart for SharedWorker auto-attach
    Fix `waitForDebuggerOnStart` for SharedWorker auto-attach by aligning
    its implementation with `ServiceWorkerDevToolsManager`.
    
    An `Observer` pattern, modeled on the one in
    `ServiceWorkerDevToolsManager`, is added to
    `SharedWorkerDevToolsManager`. This allows the `BrowserAutoAttacher` to
    intercept worker creation and enforce the pause, preventing a race
    condition with injected scripts.
    
    Bug: 428100629
    Change-Id: Iab60d14d30dd0a0e8634ca7637737ee5c21ecf8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7037151
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Auto-Submit: Wang Neden <nedenwang@gmail.com>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1529406}

--- content/browser/devtools/shared_worker_devtools_manager.h
+++ content/browser/devtools/shared_worker_devtools_manager.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/singleton.h"
+#include "base/observer_list.h"
 #include "base/unguessable_token.h"
 #include "content/public/browser/devtools_agent_host.h"
 #include "third_party/blink/public/mojom/devtools/devtools_agent.mojom.h"
@@ -25,6 +26,13 @@ class SharedWorkerHost;
 // This class lives on UI thread.
 class SharedWorkerDevToolsManager {
  public:
+  class Observer : public base::CheckedObserver {
+   public:
+    virtual void SharedWorkerCreated(SharedWorkerDevToolsAgentHost* host,
+                                     bool* should_pause_on_start) = 0;
+    virtual void SharedWorkerDestroyed(SharedWorkerDevToolsAgentHost* host) = 0;
+  };
+
   // Returns the SharedWorkerDevToolsManager singleton.
   static SharedWorkerDevToolsManager* GetInstance();
 
@@ -48,11 +56,16 @@ class SharedWorkerDevToolsManager {
 
   SharedWorkerDevToolsAgentHost* GetDevToolsHost(SharedWorkerHost* host);
 
+  void AddObserver(Observer* observer);
+  void RemoveObserver(Observer* observer);
+
  private:
   friend struct base::DefaultSingletonTraits<SharedWorkerDevToolsManager>;
 
   SharedWorkerDevToolsManager();
   ~SharedWorkerDevToolsManager();
+
+  base::ObserverList<Observer> observer_list_;
 
   // We retatin agent hosts as long as the shared worker is alive.
   std::map<SharedWorkerHost*, scoped_refptr<SharedWorkerDevToolsAgentHost>>
