025dd6d860b9607ef4bf67ab7a0c424fff32acf3
Colin Blundell <blundell@chromium.org>
2025-10-14 04:51:47 -0700

    [//ui] Implement NativePixmap::GetSharedImageFormat() in subclasses
    Takes step toward removing NativePixmap::GetBufferFormat() by removing
    its usage to implement GetSharedImageFormat(). Future CLs will convert
    all callers of the former and remove it entirely.
    
    This CL is a reland. See the fix by diffing the last PS against PS1.
    
    Bug: 445879532
    Change-Id: I20dc953479c386948a1f249f69bfdcde9dd459d4
    No-Presubmit: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7036918
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1529433}

--- ui/ozone/platform/flatland/flatland_sysmem_native_pixmap.cc
+++ ui/ozone/platform/flatland/flatland_sysmem_native_pixmap.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/fuchsia/fuchsia_logging.h"
 #include "base/logging.h"
+#include "components/viz/common/resources/shared_image_format_utils.h"
 #include "ui/gfx/geometry/rect_f.h"
 
 namespace ui {
@@ -55,6 +56,11 @@ uint64_t FlatlandSysmemNativePixmap::GetBufferFormatModifier() const {
 
 gfx::BufferFormat FlatlandSysmemNativePixmap::GetBufferFormat() const {
   return collection_->format();
+}
+
+viz::SharedImageFormat FlatlandSysmemNativePixmap::GetSharedImageFormat()
+    const {
+  return viz::GetSharedImageFormat(collection_->format());
 }
 
 gfx::Size FlatlandSysmemNativePixmap::GetBufferSize() const {
