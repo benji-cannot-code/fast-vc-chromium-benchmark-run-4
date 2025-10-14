f2c499a594f4b8dd8f1c7a332ee388dcd749060e
Trevor Perrier <perrier@chromium.org>
2025-10-14 15:52:57 -0700

    [GlicMI] Add should_focus to GlicInstance::Show
    Show is called very frequently (and sometimes for panels that are
    already showing). Instead of always focusing when show is called this
    Cl updates adds a bool argument to show `should_foucs`.
    
    Focus is only applied on toggle and daisy chaining.
    
    Bug: b:448896996
    Change-Id: I52623b19216a3b46224b4110bb2d3f3d275f6f87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7035451
    Commit-Queue: Trevor Perrier <perrier@chromium.org>
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1529835}

--- chrome/browser/glic/service/glic_ui_types.cc
+++ chrome/browser/glic/service/glic_ui_types.cc
@@ -10,11 +10,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace glic {
 
+ShowOptions::ShowOptions(EmbedderOptions embedder_options_in)
+    : embedder_options(embedder_options_in) {}
+ShowOptions::~ShowOptions() = default;
+
 // static
-FloatingShowOptions FloatingShowOptions::From(
-    BrowserWindowInterface* anchor_browser) {
-  return {GlicWidget::GetInitialBounds(anchor_browser,
-                                       GlicFloatingUi::GetDefaultSize())};
+ShowOptions ShowOptions::ForFloating(BrowserWindowInterface* anchor_browser) {
+  return ForFloating(GlicWidget::GetInitialBounds(
+      anchor_browser, GlicFloatingUi::GetDefaultSize()));
 }
+
+ShowOptions ShowOptions::ForFloating(gfx::Rect initial_bounds) {
+  return ShowOptions{FloatingShowOptions{initial_bounds}};
+}
+
+ShowOptions ShowOptions::ForSidePanel(tabs::TabInterface& bound_tab) {
+  return ShowOptions{SidePanelShowOptions{bound_tab}};
+}
+
+// end static
 
 }  // namespace glic
