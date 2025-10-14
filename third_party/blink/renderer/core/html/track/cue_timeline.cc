1826830dc502f60c942cc60e9084982ca0188c4c
Syed AbuTalib <lowkey@google.com>
2025-10-14 16:00:43 -0700

    Handle newly introduced cues in TimeMarchesOn
    This CL adds a new `newly_introduced_cues_` member to `CueTimeline` to
    track cues that are added via `AddCue` or `AddCues`.
    
    As seen in step 5 of the Time Marches On algorithm (
    https://html.spec.whatwg.org/multipage/media.html#list-of-newly-introduced-cues
    ), we must keep track of newly introduced cues and remove the missed
    cues that were newly introduced.
    
    This CL prevents `onenter` and `onexit` events from being fired due to
    missed cues completely before the current timestamp. The events will
    continue to fire if the user seeks to before the cue's startTime.
    
    Bug: 324909835
    Change-Id: I28c6be52466e0a90accbe86de9205bce5f429eb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6997932
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Commit-Queue: Syed AbuTalib <lowkey@google.com>
    Cr-Commit-Position: refs/heads/main@{#1529839}

--- third_party/blink/renderer/core/html/track/cue_timeline.cc
+++ third_party/blink/renderer/core/html/track/cue_timeline.cc
@@ -79,6 +79,7 @@ void CueTimeline::AddCue(TextTrack* track, TextTrackCue* cue) {
 }
 
 void CueTimeline::AddCueInternal(TextTrackCue* cue) {
+  newly_introduced_cues_.insert(cue);
   CueInterval interval = CreateCueInterval(cue);
   if (!cue_tree_.Contains(interval))
     cue_tree_.Add(interval);
@@ -100,6 +101,7 @@ void CueTimeline::RemoveCue(TextTrack*, TextTrackCue* cue) {
 }
 
 void CueTimeline::RemoveCueInternal(TextTrackCue* cue) {
+  newly_introduced_cues_.erase(cue);
   CueInterval interval = CreateCueInterval(cue);
   cue_tree_.Remove(interval);
 
@@ -222,12 +224,18 @@ void CueTimeline::TimeMarchesOn() {
     missed_cues.ReserveInitialCapacity(potentially_skipped_cues.size());
 
     for (CueInterval cue : potentially_skipped_cues) {
-      // Consider cues that may have been missed since the last seek time.
+      // Consider cues that may have been missed since the last seek time. Do
+      // not add cues into `missed_cues` that are also in
+      // `newly_introduced_cues_`, as stated in
+      // https://html.spec.whatwg.org/multipage/media.html#time-marches-on
       if (cue.Low() > std::max(last_seek_time, last_time) &&
-          cue.High() < movie_time)
+          cue.High() < movie_time &&
+          !newly_introduced_cues_.Contains(cue.Data())) {
         missed_cues.push_back(cue);
+      }
     }
   }
+  newly_introduced_cues_.clear();
 
   last_update_time_ = movie_time;
 
@@ -552,6 +560,7 @@ void CueTimeline::DidMoveToNewDocument(Document& /*old_document*/) {
 
 void CueTimeline::Trace(Visitor* visitor) const {
   visitor->Trace(media_element_);
+  visitor->Trace(newly_introduced_cues_);
   visitor->Trace(cue_event_timer_);
   visitor->Trace(cue_timestamp_event_timer_);
 }
