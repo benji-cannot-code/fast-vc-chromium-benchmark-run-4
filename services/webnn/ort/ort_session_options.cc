01720464230c81feeb83a1ca354d34185b271f5a
mingmingtasd <mingming1.xu@intel.com>
2025-10-13 19:22:44 -0700

    WebNN: Log EP devices that will be selected for ORT backend
    This CL allows logging of EP devices that will be selected by ORT,
    according to the given WebNN device type. The log is printed when the
    ORT logging level is set to VERBOSE or INFO.
    
    Bug: 444049496
    Change-Id: I09415550d4e6b8a04c3100703c23b0498130d4a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6978393
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    Commit-Queue: Mingming1 Xu <mingming1.xu@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1529275}

--- services/webnn/ort/ort_session_options.cc
+++ services/webnn/ort/ort_session_options.cc
@@ -5,9 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/webnn/ort/ort_session_options.h"
 
+#include <string_view>
+
 #include "base/command_line.h"
+#include "base/strings/strcat.h"
 #include "base/strings/stringprintf.h"
 #include "services/webnn/ort/environment.h"
+#include "services/webnn/ort/logging.h"
 #include "services/webnn/ort/ort_status.h"
 #include "services/webnn/ort/platform_functions_ort.h"
 #include "services/webnn/public/cpp/webnn_trace.h"
@@ -19,6 +23,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace webnn::ort {
 
 namespace {
+
+// Helper to convert `mojom::Device` to string for logging.
+std::string_view WebNNDeviceTypeToString(mojom::Device device_type) {
+  switch (device_type) {
+    case mojom::Device::kCpu:
+      return "CPU";
+    case mojom::Device::kGpu:
+      return "GPU";
+    case mojom::Device::kNpu:
+      return "NPU";
+  }
+}
 
 // Execution Provider selection delegate function that selects EPs based on
 // WebNN device type.
@@ -56,6 +72,16 @@ EpSelectionPolicyDelegate(const OrtEpDevice** ep_devices,
   CHECK_LE(selected_devices.size(), max_selected)
       << "Selected device count (" << selected_devices.size()
       << ") exceeds maximum allowed (" << max_selected << ")";
+
+  OrtLoggingLevel ort_logging_level = GetOrtLoggingLevel();
+  if (ort_logging_level == ORT_LOGGING_LEVEL_VERBOSE ||
+      ort_logging_level == ORT_LOGGING_LEVEL_INFO) {
+    // Logs selected EP devices for the given WebNN device type.
+    const OrtApi* ort_api = PlatformFunctions::GetInstance()->ort_api();
+    LogEpDevices(ort_api, selected_devices,
+                 base::StrCat({"Selected OrtEpDevice for WebNN ",
+                               WebNNDeviceTypeToString(device_type)}));
+  }
 
   for (size_t i = 0; i < selected_devices.size(); ++i) {
     // SAFETY: ORT guarantees that `selected` is valid and contains
