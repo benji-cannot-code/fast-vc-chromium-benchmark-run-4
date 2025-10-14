937b9a1979c274e7f55bc79b940725d5823d1dee
Mike West <mkwst@chromium.org>
2025-10-14 10:30:40 -0700

    [OriginAPI] Pass origin objects through `postMessage()` mojo calls.
    This CL adjusts the `LocalFrame::PostMessageEvent` and
    `RemoteFrame::RouteMessageEvent` mojo methods to accept
    `url.mojom.Origin` objects rather than strings to represent the
    message's sender and acceptable targets.
    
    This is one in a series of patches aiming to shift `MessageEvent` to
    hold the correct `SecurityOrigin` rather than a string:
    
    1. https://crrev.com/c/7003817 Remove `MessageEvent::origin()`
    2. https://crrev.com/c/7003199 Shift to `SecurityOrigin`
    3. https://crrev.com/c/7013689 [You are here.]
    
    Bug: 449581913
    Change-Id: I90cf29f380b5f04ee1d1b2f1d6305c01cf95e391
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7013689
    Reviewed-by: Antonio Rivera <antoniori@google.com>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Commit-Queue: Mike West <mkwst@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1529610}

---
+++ third_party/blink/web_tests/external/wpt/html/browsers/origin/tentative/api/origin-from-messageevent-opaque.window.js
@@ -0,0 +1,69 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: title=`Origin.from(MessageEvent)` from opaque origins.
+// META: script=/common/get-host-info.sub.js
+
+async_test(t => {
+  const el = document.createElement('iframe');
+  el.sandbox = "allow-scripts";
+  el.srcdoc = `<script>window.top.postMessage("Hi.", "*");<\/script>`;
+  window.addEventListener("message", t.step_func(e => {
+    if (e.source === el.contentWindow) {
+      const origin = Origin.from(e);
+      assert_true(!!origin);
+      assert_true(origin.opaque);
+      t.done();
+    }
+  }));
+  document.body.appendChild(el);
+}, `Origin.from(MessageEvent) returns an opaque origin for a sandboxed frame.`);
+
+async_test(t => {
+  const el = document.createElement('iframe');
+  el.sandbox = "allow-scripts";
+  el.srcdoc = `
+    <script>
+      window.top.postMessage("Hi.", "*");
+      window.top.postMessage("Bye.", "*");
+    <\/script>`;
+  let eventOrigin = null;
+  window.addEventListener("message", t.step_func(e => {
+    if (e.source === el.contentWindow) {
+      const origin = Origin.from(e);
+      assert_true(!!origin);
+      assert_true(origin.opaque);
+      if (eventOrigin) {
+        assert_true(eventOrigin.isSameOrigin(origin));
+        t.done();
+      } else {
+        eventOrigin = origin;
+      }
+    }
+  }));
+  document.body.appendChild(el);
+}, `Origin.from(MessageEvent) returns the same opaque origin for each message from a sandboxed frame.`);
+
+async_test(t => {
+  const el = document.createElement('iframe');
+  el.sandbox = "allow-scripts";
+  el.srcdoc = `
+    <script>
+      window.top.postMessage("Hi.", "*");
+      window.addEventListener("message", e => { navigation.reload(); });
+    <\/script>`;
+  let eventOrigin = null;
+  window.addEventListener("message", t.step_func(e => {
+    if (e.source === el.contentWindow) {
+      const origin = Origin.from(e);
+      assert_true(!!origin);
+      assert_true(origin.opaque);
+      if (eventOrigin) {
+        assert_false(eventOrigin.isSameOrigin(origin));
+        t.done();
+      } else {
+        eventOrigin = origin;
+        e.source.postMessage("Reload thyself.", "*");
+      }
+    }
+  }));
+  document.body.appendChild(el);
+}, `Origin.from(MessageEvent) returns distinct opaque origins for each message from a reloaded sandboxed frame.`);
