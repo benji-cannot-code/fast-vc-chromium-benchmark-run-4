103f723da4211f3d8cb6e4f853df9c5331528cc1
Oleksii Kalinin <kalinino@google.com>
2025-10-14 05:22:30 -0700

    Add Omnibox Autofocus on Incognito NTP feature flags
    This change introduces new feature flags for experimenting with various
    behaviors for autofocusing the Omnibox on the Incognito NTP.
    
    Change-Id: I8977d3f1d641deab58f60d84a358d15eb5b30598
    Bug: 427904341
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6923827
    Reviewed-by: Kamil Jarosz <kjarosz@google.com>
    Commit-Queue: Oleksii Kalinin <kalinino@google.com>
    Reviewed-by: Matt Dembski <dembski@google.com>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1529445}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -368,6 +368,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kNotificationPermissionVariant,
     &kNotificationTrampoline,
     &kOmahaMinSdkVersionAndroid,
+    &kOmniboxAutofocusOnIncognitoNtp,
     &kPageAnnotationsService,
     &kPageContentProvider,
     &kPartnerCustomizationsUma,
@@ -963,6 +964,9 @@ BASE_FEATURE(kPwaRestoreUi, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kPwaRestoreUiAtStartup, base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kOmahaMinSdkVersionAndroid, base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kOmniboxAutofocusOnIncognitoNtp,
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kShortCircuitUnfocusAnimation, base::FEATURE_DISABLED_BY_DEFAULT);
 
