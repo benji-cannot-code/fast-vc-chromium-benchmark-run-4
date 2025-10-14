511f1113e0c8874a4441c75676a7975ec41663cc
Arthur Milchior <arthurmilchior@google.com>
2025-10-14 02:26:23 -0700

    [iOS]Uses GaiaId type when possible in ['signout']
    GaiaId in iOS is currently often encoded as a NSString or more rarely
    a std::string. It should ideally be replaced by GaiaId, a c++ class
    that contains simply the gaia id (or an empty string to represents the
    id missing).
    
    In order for the migration not to break ios_internal, some virtual
    method has to be temporarily duplicated, taking both kind of
    arguments. In this case, both functions call the other function, and
    the classes inheriting this abstract class must implement one of the
    two functions.
    
    This is part of the chain
    + crrev.com/c/7003548 (that is splitted)
    - crrev.com/i/8634707
    - crrev.com/c/7021136
    - crrev.com/i/8650938
    - crrev.com/c/7021672
    
    This CL was uploaded by git cl split.
    
    Bug: 449195064
    Change-Id: I37680710e1504f313e566a45325fa63a5a1973de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7021175
    Commit-Queue: Menghan Yang <myuu@google.com>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Menghan Yang <myuu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1529389}

--- ios/chrome/browser/authentication/ui_bundled/signout_action_sheet/signout_action_sheet_coordinator_unittest.mm
+++ ios/chrome/browser/authentication/ui_bundled/signout_action_sheet/signout_action_sheet_coordinator_unittest.mm
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/signin/public/base/signin_metrics.h"
 #import "components/signin/public/base/signin_pref_names.h"
 #import "components/sync/test/mock_sync_service.h"
+#import "google_apis/gaia/gaia_id.h"
 #import "ios/chrome/app/application_delegate/app_state.h"
 #import "ios/chrome/browser/policy/model/policy_util.h"
 #import "ios/chrome/browser/shared/coordinator/scene/scene_state.h"
@@ -264,12 +265,10 @@ TEST_F(SignoutActionSheetCoordinatorTest,
   SignInManagedIdentity();
 
   // Mark the user as "migrated from previously syncing".
-  GetPrefs()->SetString(
-      prefs::kGoogleServicesSyncingGaiaIdMigratedToSignedIn,
-      base::SysNSStringToUTF8(
-          authentication_service()
-              ->GetPrimaryIdentity(signin::ConsentLevel::kSignin)
-              .gaiaID));
+  GetPrefs()->SetString(prefs::kGoogleServicesSyncingGaiaIdMigratedToSignedIn,
+                        authentication_service()
+                            ->GetPrimaryIdentity(signin::ConsentLevel::kSignin)
+                            .gaiaId.ToString());
   GetPrefs()->SetString(
       prefs::kGoogleServicesSyncingUsernameMigratedToSignedIn,
       base::SysNSStringToUTF8(
