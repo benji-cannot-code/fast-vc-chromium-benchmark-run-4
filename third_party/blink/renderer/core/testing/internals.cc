928cd00e849c55209dcadb8f16c892dbe4ece685
Colin Blundell <blundell@chromium.org>
2025-10-14 00:14:39 -0700

    [Blink] Port internals.cc to disable canvas acceleration via context
    We're in the process of removing
    HTMLCanvasElement::DisableAccelerationForCanvas2D(), as this is a
    Canvas2D-specific concept whose implementation belongs on the context.
    This CL ports testing/internals.cc's call to be on the context [1]. This
    necessitates adding the method to canvas_rendering_context.h as it is
    not possible to reference CanvasRenderingContext2D from core/. This is
    still better than the current structure as it will enable removing the
    method entirely from HTMLCanvasElement in followup.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.cc;l=1170-1172;drc=62a9a00176f862e688fa47919ed6f19b59f77b5f;bpv=1;bpt=1
    
    Bug: 352263194
    Change-Id: Ie040b504e16e7b37d39cc29d457ee8430c863a5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7035591
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1529346}

--- third_party/blink/renderer/core/testing/internals.cc
+++ third_party/blink/renderer/core/testing/internals.cc
@@ -3557,7 +3557,7 @@ void Internals::forceLoseCanvasContext(CanvasRenderingContext* context) {
 
 void Internals::disableCanvasAccelerationForCanvas2D(
     HTMLCanvasElement* canvas) {
-  canvas->DisableAccelerationForCanvas2D();
+  canvas->RenderingContext()->DisableAccelerationForCanvas2D();
 }
 
 bool Internals::isCanvasImageSourceAccelerated(
