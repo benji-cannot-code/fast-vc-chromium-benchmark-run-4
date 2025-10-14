366647df07b6c1d3574c2147d5f22edab4c4f57e
Jimmy <jimmyxgong@chromium.org>
2025-10-14 13:14:10 -0700

    Reset f11/f12 prefs on reset keyboard pref settings
    Fixes a bug in which the f11/f12 for internal keyboards settings
    did not reset correctly when requested. This should now reset back
    to the disabled state.
    
    Fixed: 447553843
    Test: ash_unittests
    Change-Id: I2aec2c654d4316a9967199d25aecc189b43f996a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7038203
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1529742}

--- ash/system/input_device_settings/input_device_settings_controller_impl.cc
+++ ash/system/input_device_settings/input_device_settings_controller_impl.cc
@@ -70,6 +70,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/ui_base_features.h"
 #include "ui/events/ash/keyboard_capability.h"
+#include "ui/events/ash/mojom/extended_fkeys_modifier.mojom.h"
 #include "ui/events/ash/top_row_action_keys.h"
 #include "ui/events/devices/input_device.h"
 #include "ui/events/devices/keyboard_device.h"
@@ -2168,6 +2169,8 @@ void InputDeviceSettingsControllerImpl::RestoreDefaultKeyboardRemappings(
   mojom::KeyboardSettingsPtr new_settings = keyboard.settings->Clone();
   new_settings->modifier_remappings = {};
   new_settings->six_pack_key_remappings = mojom::SixPackKeyInfo::New();
+  new_settings->f11 = ui::mojom::ExtendedFkeysModifier::kDisabled;
+  new_settings->f12 = ui::mojom::ExtendedFkeysModifier::kDisabled;
   if (keyboard.meta_key == ui::mojom::MetaKey::kCommand) {
     new_settings->modifier_remappings[ui::mojom::ModifierKey::kControl] =
         ui::mojom::ModifierKey::kMeta;
