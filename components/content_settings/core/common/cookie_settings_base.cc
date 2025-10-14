5b5d43d6f353b8d4a4d772f7e2e3270a08912de6
Fiona Macintosh <fmacintosh@google.com>
2025-10-14 07:19:02 -0700

    Clean up unused content setting feature from 3pcd
    This feature was added as part of the 3pcd implementation and never
    launched. It does not have plans for future use and is just tech debt
    
    Bug: b:382043551
    Change-Id: I6cfb100b59392a5e8b9227cba272ceb9125f5afe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7030627
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Fiona Macintosh <fmacintosh@google.com>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1529509}

--- components/content_settings/core/common/cookie_settings_base.cc
+++ components/content_settings/core/common/cookie_settings_base.cc
@@ -54,7 +54,6 @@ constexpr StorageAccessResult GetStorageAccessResult(
     case AllowMechanism::kNone:
       return StorageAccessResult::ACCESS_BLOCKED;
     case AllowMechanism::kAllowByExplicitSetting:
-    case AllowMechanism::kAllowByTrackingProtectionException:
     case AllowMechanism::kAllowByGlobalSetting:
     case AllowMechanism::kAllowByEnterprisePolicyCookieAllowedForUrls:
       return StorageAccessResult::ACCESS_ALLOWED;
@@ -104,7 +103,6 @@ constexpr std::optional<SettingSource> GetSettingSource(
     // Other mechanisms do not map to a `SettingSource`.
     case AllowMechanism::kNone:
     case AllowMechanism::kAllowByExplicitSetting:
-    case AllowMechanism::kAllowByTrackingProtectionException:
     case AllowMechanism::kAllowByGlobalSetting:
     case AllowMechanism::kAllowByEnterprisePolicyCookieAllowedForUrls:
     case AllowMechanism::kAllowByStorageAccess:
@@ -216,7 +214,6 @@ bool CookieSettingsBase::IsAnyTpcdMetadataAllowMechanism(
   switch (mechanism) {
     case AllowMechanism::kNone:
     case AllowMechanism::kAllowByExplicitSetting:
-    case AllowMechanism::kAllowByTrackingProtectionException:
     case AllowMechanism::kAllowByGlobalSetting:
     case AllowMechanism::kAllowBy3PCD:
     case AllowMechanism::kAllowBy3PCDHeuristics:
@@ -249,7 +246,6 @@ bool CookieSettingsBase::Is1PDtRelatedAllowMechanism(
       return true;
     case AllowMechanism::kNone:
     case AllowMechanism::kAllowByExplicitSetting:
-    case AllowMechanism::kAllowByTrackingProtectionException:
     case AllowMechanism::kAllowByGlobalSetting:
     case AllowMechanism::kAllowBy3PCD:
     case AllowMechanism::kAllowBy3PCDHeuristics:
@@ -295,7 +291,6 @@ CookieSettingsBase::AllowMechanismToMetadataSourceType(
       return MetadataSourceType::Heuristics;
     case AllowMechanism::kNone:
     case AllowMechanism::kAllowByExplicitSetting:
-    case AllowMechanism::kAllowByTrackingProtectionException:
     case AllowMechanism::kAllowByGlobalSetting:
     case AllowMechanism::kAllowByStorageAccess:
     case AllowMechanism::kAllowByTopLevelStorageAccess:
@@ -566,20 +561,6 @@ CookieSettingsBase::IsAllowedBy3pcdMetadataGrantsSettings(
   return {allowed, std::move(info)};
 }
 
-CookieSettingsBase::IsAllowedWithMetadata
-CookieSettingsBase::IsAllowedByTrackingProtectionSetting(
-    const GURL& url,
-    const GURL& first_party_url) const {
-  SettingInfo info;
-  bool allowed =
-      base::FeatureList::IsEnabled(
-          privacy_sandbox::kTrackingProtectionContentSettingFor3pcb) &&
-      GetContentSetting(url, first_party_url,
-                        ContentSettingsType::TRACKING_PROTECTION,
-                        &info) == CONTENT_SETTING_ALLOW;
-  return {allowed, std::move(info)};
-}
-
 bool CookieSettingsBase::IsAllowedBy3pcdHeuristicsGrantsSettings(
     const GURL& url,
     const GURL& first_party_url,
@@ -708,15 +689,6 @@ CookieSettingsBase::DecideAccess(const GURL& url,
     return AllowAllCookies{ThirdPartyCookieAllowMechanism::kAllowBy3PCD};
   }
 
-  // Check for a TRACKING_PROTECTION exception, which should also disable 3PCB.
-  if (IsAllowedWithMetadata tp_info =
-          IsAllowedByTrackingProtectionSetting(url, first_party_url);
-      tp_info.allowed) {
-    setting_info = std::move(tp_info.info);
-    return AllowAllCookies{
-        ThirdPartyCookieAllowMechanism::kAllowByTrackingProtectionException};
-  }
-
   return AllowPartitionedCookies{};
 }
 
@@ -787,11 +759,6 @@ CookieSettingsBase::GetCookieSettingInternal(
     FireStorageAccessHistogram(
         GetStorageAccessResult(allow_cookies->mechanism));
 
-    if (allow_cookies->mechanism ==
-        ThirdPartyCookieAllowMechanism::kAllowByTrackingProtectionException) {
-      is_explicit_setting = true;
-      cookie_setting = CONTENT_SETTING_ALLOW;
-    }
     if (info) {
       if (std::optional<SettingSource> source =
               GetSettingSource(allow_cookies->mechanism);
