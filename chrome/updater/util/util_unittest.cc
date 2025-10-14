fa9c1414b089cc74ea60323cb91dbfb37105c471
S. Ganesh <ganesh@chromium.org>
2025-10-14 06:39:40 -0700

    updater: cleanup any left over update_client temp directories for posix
    Bug: 445972566,447234785
    Change-Id: Ia134ff560dd7f21e7e367dc7b2ec6d6dae040550
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7033553
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1529485}

--- chrome/updater/util/util_unittest.cc
+++ chrome/updater/util/util_unittest.cc
@@ -298,10 +298,6 @@ TEST(Util, EnumerateUpdateClientTempDirectories) {
         ADD_FAILURE() << "Unexpected directory: " << dir;
       }));
 
-  // TODO(crbug.com/447234785): enable the following test for `posix` after
-  // `CreateNewTempDirectory` in `file_util_posix.cc` starts respecting
-  // `prefix`.
-#if BUILDFLAG(IS_WIN)
   base::FilePath dir;
   for (const auto& matcher :
        {FILE_PATH_LITERAL("chrome_url_fetcher_"),
@@ -319,7 +315,6 @@ TEST(Util, EnumerateUpdateClientTempDirectories) {
 
   EXPECT_EQ(count, 3);
   EXPECT_TRUE(base::DeletePathRecursively(dir));
-#endif  // BUILDFLAG(IS_WIN)
 }
 
 }  // namespace updater
