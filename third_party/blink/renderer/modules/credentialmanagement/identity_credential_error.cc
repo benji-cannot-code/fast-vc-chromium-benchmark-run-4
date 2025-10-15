a16e8aec18f8e6881fd03396feee4122466a6696
Suresh Potti <sureshpotti@microsoft.com>
2025-10-15 08:39:13 -0700

    [FedCM] Add console warning message to FedCM breaking changes.
    Bug: 431249539, 427474985, 438736799
    Change-Id: I35c5fe3e66be7319a1fd334766660744df328405
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7031542
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: suresh potti <sureshpotti@microsoft.com>
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1530237}

--- third_party/blink/renderer/modules/credentialmanagement/identity_credential_error.cc
+++ third_party/blink/renderer/modules/credentialmanagement/identity_credential_error.cc
@@ -6,7 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/credentialmanagement/identity_credential_error.h"
 
 #include "third_party/blink/renderer/bindings/modules/v8/v8_identity_credential_error_init.h"
+#include "third_party/blink/renderer/core/execution_context/execution_context.h"
+#include "third_party/blink/renderer/core/inspector/console_message.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
+#include "third_party/blink/renderer/platform/bindings/script_state.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
@@ -44,6 +47,19 @@ String IdentityCredentialError::code(ExceptionState& exception_state) const {
         "FedCmErrorAttribute feature is enabled. Use 'error' attribute "
         "instead.");
     return String();
+  }
+
+  // Add console warning when code attribute is accessed
+  if (ScriptState* script_state =
+          ScriptState::ForCurrentRealm(exception_state.GetIsolate())) {
+    if (ExecutionContext* execution_context =
+            ExecutionContext::From(script_state)) {
+      execution_context->AddConsoleMessage(MakeGarbageCollected<ConsoleMessage>(
+          mojom::blink::ConsoleMessageSource::kJavaScript,
+          mojom::blink::ConsoleMessageLevel::kWarning,
+          "The 'code' attribute of IdentityCredentialError is deprecated and "
+          "will be removed in Chrome 145. Use the 'error' attribute instead."));
+    }
   }
 
   return error_code_;
