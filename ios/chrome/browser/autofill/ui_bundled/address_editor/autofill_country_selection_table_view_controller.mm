c2c728683acdc824d60790b81e9385eed613b2ea
Vidhan Jain <vidhanj@google.com>
2025-10-15 04:13:11 -0700

    [iOS] Remove stale TODOs in AutofillCountrySelectionTableViewController
    There is no plan for these metrics.
    
    Bug: 40253248
    Change-Id: I56285c41228f87b567f18703f00625e77cf361c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7041962
    Reviewed-by: Julia Sobiech <jsobiech@google.com>
    Commit-Queue: Julia Sobiech <jsobiech@google.com>
    Auto-Submit: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1530094}

--- ios/chrome/browser/autofill/ui_bundled/address_editor/autofill_country_selection_table_view_controller.mm
+++ ios/chrome/browser/autofill/ui_bundled/address_editor/autofill_country_selection_table_view_controller.mm
@@ -305,11 +305,9 @@ const CGFloat kBackButtonSize = 24;
 #pragma mark - SettingsControllerProtocol
 
 - (void)reportDismissalUserAction {
-  // TODO(crbug.com/40253248): Record for this VC.
 }
 
 - (void)reportBackUserAction {
-  // TODO(crbug.com/40253248): Record for this VC.
 }
 
 @end
