7e8eb74dca4b78c2057c982a2eb06b4c2f0e66eb
Jan Wilken Dörrie <jdoerrie@chromium.org>
2025-10-15 09:49:57 -0700

    [dbsc] Use correct keychain access group on macOS
    The keychain access group for the unexportable key service was
    hardcoded to the value for Chromium. This is incorrect for branded
    builds, such as Google Chrome, which have a different team and bundle
    identifier.
    
    This change uses the `GOOGLE_CHROME_BRANDING` build flag to select the
    correct keychain access group at build time. This ensures that the
    unexportable key service can correctly store and access keys in the
    keychain on all macOS builds.
    
    Bug: 384055845, 443931937
    
    Change-Id: I896af5e4080a8a8ab67fc5ddd85d7c4290efc1b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7037318
    Reviewed-by: thefrog <thefrog@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1530284}

--- net/device_bound_sessions/unexportable_key_service_factory.cc
+++ net/device_bound_sessions/unexportable_key_service_factory.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/device_bound_sessions/unexportable_key_service_factory.h"
 
 #include "base/logging.h"
+#include "build/branding_buildflags.h"
 #include "components/unexportable_keys/unexportable_key_service.h"
 #include "components/unexportable_keys/unexportable_key_service_impl.h"
 #include "components/unexportable_keys/unexportable_key_task_manager.h"
@@ -13,10 +14,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+// TODO: crbug.com/443932320 - Replace all usages of this class with //chrome's
+// `UnexportableKeyServiceFactory`. This already uses the version constants.
 #if BUILDFLAG(IS_MAC)
-// TODO(crbug.com/384055845): Use the correct value for Secure Enclave
 constexpr char kKeychainAccessGroup[] =
-    ".org.chromium.Chromium.unexportable-keys";
+// Ideally we'd just use `MAC_TEAM_IDENTIFIER_STRING` and
+// `MAC_BUNDLE_IDENTIFIER_STRING`, but we can't depend on //chrome here.
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+    "EQHXZ8M8AV.com.google.Chrome"
+#else
+    ".org.chromium.Chromium"
+#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
+    ".unexportable-keys";
 #endif  // BUILDFLAG(IS_MAC)
 
 // Returns a newly created task manager instance, or nullptr if unexportable
