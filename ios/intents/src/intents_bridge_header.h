5a40e8ddc9ce558c71cdc72941226148e2d4a0f4
Sylvain Defresne <sdefresne@chromium.org>
2025-10-15 05:25:39 -0700

    [ios] Remove //ios/intents/* targets and code
    They were sample on how to use `generate_intents = true`. This
    has now been used by //ios/chrome/widget_kit_extension and the
    sample is no longer needed.
    
    Bug: none
    Change-Id: I1fb4ca08cd13439707e0665ad09a407ec630f4de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7041742
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1530122}

--- ios/intents/src/intents_bridge_header.h
+++
@@ -1,9 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2024 The Chromium Authors
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#ifndef IOS_INTENTS_SRC_INTENTS_BRIDGE_HEADER_H_
-#define IOS_INTENTS_SRC_INTENTS_BRIDGE_HEADER_H_
-
-#endif  // IOS_INTENTS_SRC_INTENTS_BRIDGE_HEADER_H_
