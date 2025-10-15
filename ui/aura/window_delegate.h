d1af149330a1596f92314ec37611272c3b2bcf82
Yuki Shiino <yukishiino@chromium.org>
2025-10-15 01:48:14 -0700

    ELUD: Add ADVANCED_MEMORY_SAFETY_CHECKS to aura::WindowDelegate
    Adds ADVANCED_MEMORY_SAFETY_CHECKS as a mitigation of
    crbug.com/429093189
    
    Bug: 429093189
    Change-Id: I24de705466a21531d176ced27f4fb856842ace7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7030801
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1530034}

--- ui/aura/window_delegate.h
+++ ui/aura/window_delegate.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_AURA_WINDOW_DELEGATE_H_
 #define UI_AURA_WINDOW_DELEGATE_H_
 
+#include "base/memory/safety_checks.h"
 #include "ui/aura/aura_export.h"
 #include "ui/aura/window.h"
 #include "ui/events/event_constants.h"
@@ -28,6 +29,9 @@ namespace aura {
 
 // Delegate interface for aura::Window.
 class AURA_EXPORT WindowDelegate : public ui::EventHandler {
+  // TODO(crbug.com/429093189): Remove this macro once it gets fixed.
+  ADVANCED_MEMORY_SAFETY_CHECKS();
+
  public:
   // Returns the window's minimum size, or size 0,0 if there is no limit.
   virtual gfx::Size GetMinimumSize() const = 0;
