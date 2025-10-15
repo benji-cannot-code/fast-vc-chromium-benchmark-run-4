20e43c7077bf4756c898244c86a72abba9cdba62
Norge Vizcay <vizcay@google.com>
2025-10-15 11:07:00 -0700

    Launch AutofillEnableLoyaltyCardsFilling.
    Change-Id: I6a4ce24afb96c9272246b83f84893dedf28803ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7037593
    Auto-Submit: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1530342}

--- ios/chrome/browser/sync/model/sync_service_factory_unittest.mm
+++ ios/chrome/browser/sync/model/sync_service_factory_unittest.mm
@@ -99,9 +99,6 @@ class SyncServiceFactoryTest : public PlatformTest {
       }
     }
     datatypes.Put(syncer::WEBAUTHN_CREDENTIAL);
-    if (base::FeatureList::IsEnabled(syncer::kSyncAutofillLoyaltyCard)) {
-      datatypes.Put(syncer::AUTOFILL_VALUABLE);
-    }
     if (base::FeatureList::IsEnabled(syncer::kSyncSharedComment)) {
       datatypes.Put(syncer::SHARED_COMMENT);
     }
