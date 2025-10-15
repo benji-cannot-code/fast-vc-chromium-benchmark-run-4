220ab17c0e1fa7e902594d7bee92e1e92857e769
Jacobo Tapia <jatapiaro@google.com>
2025-10-15 12:42:36 -0700

    [Reading Mode]: Rename feature flag from ImmersiveReadingMode to ImmersiveReadAnything
    This is a follow-up to crrev.com/c/7042642. Fixes a typo in the enabled
    method name that shows IsImmersiveReadingAnythingEnabled instead of
    IsImmersiveReadAnythingEnabled.
    
    Bug: 452079704
    Change-Id: I025629624ed7274b011be3ceca1189d88082aa25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7046017
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Commit-Queue: Jacobo Misael Tapia <jatapiaro@google.com>
    Cr-Commit-Position: refs/heads/main@{#1530402}

--- ui/accessibility/accessibility_features.cc
+++ ui/accessibility/accessibility_features.cc
@@ -408,7 +408,7 @@ bool IsDataCollectionModeForScreen2xEnabled() {
 }
 
 BASE_FEATURE(kImmersiveReadAnything, base::FEATURE_DISABLED_BY_DEFAULT);
-bool IsImmersiveReadingAnythingEnabled() {
+bool IsImmersiveReadAnythingEnabled() {
   return base::FeatureList::IsEnabled(::features::kImmersiveReadAnything);
 }
 
