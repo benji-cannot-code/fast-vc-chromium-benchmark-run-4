28fe767b46a7d530a49311d9dbf14627b7208f29
Eric Leese <leese@chromium.org>
2025-10-14 23:17:55 -0700

    Refactor Android device connection states and add lock detection.
    This change introduces a `ConnectedState` enum to better represent the
    status of an Android device (e.g., connected, unauthorized, offline,
    locked). The `adb devices` output is now parsed to distinguish between
    these states. Additional ADB commands are run to detect if a connected
    device is currently locked, and this information is propagated to the
    DevTools UI. The UI messages are updated to provide more specific
    feedback based on the device's state.
    
    We no longer poll DevTools services on a device that is locked,
    as this has caused issues with hanging the adb service.
    
    Bug: 372055979
    Change-Id: I54993462e261c3bfc3db5629af3ffb781b340128
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7037037
    Auto-Submit: Eric Leese <leese@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1529963}

--- chrome/browser/devtools/device/devtools_device_discovery.cc
+++ chrome/browser/devtools/device/devtools_device_discovery.cc
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-
 #include "chrome/browser/devtools/device/devtools_device_discovery.h"
 
 #include <map>
@@ -442,8 +441,22 @@ void DevToolsDeviceDiscovery::DiscoveryRequest::ReceivedDevices(
     const AndroidDeviceManager::Devices& devices) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
   for (const auto& device : devices) {
-    device->QueryDeviceInfo(
-        base::BindOnce(&DiscoveryRequest::ReceivedDeviceInfo, this, device));
+    switch (device->connected_state()) {
+      case AndroidDeviceManager::DeviceInfo::kLocked:
+        // We don't expect this state to be reported by AdbDeviceProvider.
+        DCHECK(false);
+        ABSL_FALLTHROUGH_INTENDED;
+      case AndroidDeviceManager::DeviceInfo::kUnauthorized:
+      case AndroidDeviceManager::DeviceInfo::kOffline:
+        DiscoveryRequest::ReceivedDeviceInfo(
+            device, AndroidDeviceManager::DeviceInfo());
+        break;
+      case AndroidDeviceManager::DeviceInfo::kConnected:
+      case AndroidDeviceManager::DeviceInfo::kUnknown:
+        device->QueryDeviceInfo(base::BindOnce(
+            &DiscoveryRequest::ReceivedDeviceInfo, this, device));
+        break;
+    }
   }
 }
 
@@ -451,14 +464,25 @@ void DevToolsDeviceDiscovery::DiscoveryRequest::ReceivedDeviceInfo(
     scoped_refptr<AndroidDeviceManager::Device> device,
     const AndroidDeviceManager::DeviceInfo& device_info) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
+  AndroidDeviceManager::DeviceInfo device_info_copy = device_info;
+  if (device_info.connected_state ==
+      AndroidDeviceManager::DeviceInfo::kUnknown) {
+    // If the initial query said it was connected but querying the device for
+    // more information failed, it is offline. Otherwise, replace the connection
+    // state with info from the devices query.
+    device_info_copy.connected_state =
+        device->connected_state() ==
+                AndroidDeviceManager::DeviceInfo::kConnected
+            ? AndroidDeviceManager::DeviceInfo::kOffline
+            : device->connected_state();
+  }
   scoped_refptr<RemoteDevice> remote_device =
-      new RemoteDevice(device->serial(), device_info);
-  complete_devices_.push_back(std::make_pair(device, remote_device));
-  for (auto it = remote_device->browsers().begin();
-       it != remote_device->browsers().end(); ++it) {
+      new RemoteDevice(device->serial(), device_info_copy);
+  complete_devices_.emplace_back(device, remote_device);
+  for (auto & it : remote_device->browsers()) {
     device->SendJsonRequest(
-        (*it)->socket(), kVersionRequest,
-        base::BindOnce(&DiscoveryRequest::ReceivedVersion, this, device, *it));
+        it->socket(), kVersionRequest,
+        base::BindOnce(&DiscoveryRequest::ReceivedVersion, this, device, it));
   }
 }
 
@@ -612,7 +636,14 @@ DevToolsDeviceDiscovery::RemoteDevice::RemoteDevice(
     const AndroidDeviceManager::DeviceInfo& device_info)
     : serial_(serial),
       model_(device_info.model),
-      connected_(device_info.connected),
+      connected_(device_info.connected_state ==
+                 AndroidDeviceManager::DeviceInfo::kConnected),
+      unauthorized_(device_info.connected_state ==
+                        AndroidDeviceManager::DeviceInfo::kUnauthorized ||
+                    device_info.connected_state ==
+                        AndroidDeviceManager::DeviceInfo::kUnknown),
+      locked_(device_info.connected_state ==
+            AndroidDeviceManager::DeviceInfo::kLocked),
       screen_size_(device_info.screen_size) {
   for (auto it = device_info.browser_info.begin();
        it != device_info.browser_info.end(); ++it) {
