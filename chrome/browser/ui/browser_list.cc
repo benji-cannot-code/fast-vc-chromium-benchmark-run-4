ca7ff093eb4b94dec001d0045588ab3f22780c82
Alex Carutasu <alcaruta@microsoft.com>
2025-10-14 23:15:51 -0700

    [bedrock] Remove BrowserList::OnBrowserClosing()
    Now that all BrowserList::OnBrowserClosing() overrides are migrated to
    use alternatived like BrowserWindowInterface::RegisterBrowserDidClose()
    both upstream and downstream, we can remove its decl entirely.
    
    Note:
    There was actually one remaining reference as a mocked method in a
    helper class for BrowserShutdownBrowserTest. I migrated that to use
    RegisterBrowserDidClose() to enable the decl removal.
    
    Bug: 431671320
    Change-Id: I0641fb49f8a2c879fd4570af4e4d4bb3fe4d5e4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7043403
    Commit-Queue: Alex Carutasu <alcaruta@microsoft.com>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1529961}

--- chrome/browser/ui/browser_list.cc
+++ chrome/browser/ui/browser_list.cc
@@ -348,10 +348,6 @@ void BrowserList::NotifyBrowserNoLongerActive(Browser* browser) {
 // static
 void BrowserList::NotifyBrowserCloseStarted(Browser* browser) {
   GetInstance()->currently_closing_browsers_.insert(browser);
-
-  for (BrowserListObserver& observer : observers_.Get()) {
-    observer.OnBrowserClosing(browser);
-  }
 }
 
 // static
