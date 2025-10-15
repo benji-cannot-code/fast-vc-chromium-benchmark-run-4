fe868b353ac2d991c3f6951b0c49239fd1b5c080
Kent Tamura <tkent@chromium.org>
2025-10-14 22:45:50 -0700

    CanvasTextNG: Remove CachingWordShapeIterator
    The remaining code is moved to font.cc.
    This CL has no behavior changes.
    
    Bug: 448683997
    Change-Id: I4bdba32de2f0f57f01cd6d52f27eab30d85e4908
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7043363
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1529956}

--- third_party/blink/renderer/core/css/threaded/text_renderer_threaded_test.cc
+++ third_party/blink/renderer/core/css/threaded/text_renderer_threaded_test.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/fonts/font_description.h"
 #include "third_party/blink/renderer/platform/fonts/font_selector.h"
 #include "third_party/blink/renderer/platform/fonts/plain_text_painter.h"
-#include "third_party/blink/renderer/platform/fonts/shaping/caching_word_shape_iterator.h"
 #include "third_party/blink/renderer/platform/fonts/shaping/harfbuzz_shaper.h"
 #include "third_party/blink/renderer/platform/graphics/test/mock_paint_canvas.h"
 #include "third_party/blink/renderer/platform/language.h"
--- third_party/blink/renderer/platform/fonts/font.cc
+++ third_party/blink/renderer/platform/fonts/font.cc
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/fonts/font_cache.h"
 #include "third_party/blink/renderer/platform/fonts/font_fallback_list.h"
 #include "third_party/blink/renderer/platform/fonts/font_fallback_map.h"
-#include "third_party/blink/renderer/platform/fonts/shaping/caching_word_shape_iterator.h"
+#include "third_party/blink/renderer/platform/fonts/shaping/harfbuzz_shaper.h"
 #include "third_party/blink/renderer/platform/fonts/shaping/shape_result_bloberizer.h"
 #include "third_party/blink/renderer/platform/fonts/shaping/shape_result_spacing.h"
 #include "third_party/blink/renderer/platform/fonts/shaping/shape_result_view.h"
@@ -56,6 +56,21 @@ FontFallbackList* GetOrCreateFontFallbackList(
                                       ? font_selector->GetFontFallbackMap()
                                       : FontCache::Get().GetFontFallbackMap();
   return fallback_map.Get(font_description);
+}
+
+const ShapeResult* ShapeWordWithoutSpacing(const TextRun& word_run,
+                                           const Font& font) {
+  ShapeCacheEntry* cache_entry = font.GetShapeCache()->Add(word_run);
+  if (cache_entry && *cache_entry) {
+    return *cache_entry;
+  }
+
+  HarfBuzzShaper shaper(word_run.NormalizedUTF16());
+  ShapeResult* shape_result = shaper.Shape(&font, word_run.Direction());
+  if (cache_entry) {
+    *cache_entry = shape_result;
+  }
+  return shape_result;
 }
 
 }  // namespace
@@ -266,8 +281,7 @@ GlyphData Font::GetEmphasisMarkGlyphData(const AtomicString& mark) const {
   if (mark.empty())
     return GlyphData();
   if (!RuntimeEnabledFeatures::EmphasisMarkShapeCacheEnabled()) {
-    return CachingWordShapeIterator::ShapeWordWithoutSpacing(TextRun(mark),
-                                                             this)
+    return ShapeWordWithoutSpacing(TextRun(mark), *this)
         ->EmphasisMarkGlyphData(font_description_);
   }
   return EnsureFontFallbackList()
