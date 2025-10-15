83d316f26ea6f32722f8a7d0dfaf7bc821b42ec9
Nate Chapin <japhet@chromium.org>
2025-10-15 11:39:51 -0700

    Don't store NavigationState on DocumentState for synchronous commits
    When a browser-initiated same-document navigation occurs, we store
    NavigationState that we need to correctly notify thhe browser of
    completion of the same-document navigation. If JS runs before
    that the same-document navigation and triggers a new synchronous
    renderer-initiated same-document navigation, that new
    renderer-initiated navigation will clobber the browser-initiated
    same-document navigation's NavigationState, and we will crash
    when the browser-initiated same-document navigation finishes later.
    
    Therefore, ensure that synchronous renderer-initiated same-document
    navigations can plumb their NavigationState through to everywhere
    it is needed, rather than storing it on DocumentState and
    clobbering.
    
    Bug: 446142779
    Change-Id: I002615aa118fd8c79bd113c1b6d3c3e4acca8f11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7003682
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1530354}

--- content/renderer/render_frame_impl.h
+++ content/renderer/render_frame_impl.h
@@ -172,6 +172,7 @@ class DocumentState;
 class MediaPermissionDispatcher;
 class MHTMLPartsGenerationDelegateImpl;
 class NavigationClient;
+class NavigationState;
 class RenderAccessibilityManager;
 class RenderFrameObserver;
 
@@ -1059,6 +1060,7 @@ class CONTENT_EXPORT RenderFrameImpl
   mojom::DidCommitProvisionalLoadParamsPtr MakeDidCommitProvisionalLoadParams(
       blink::WebHistoryCommitType commit_type,
       ui::PageTransition transition,
+      NavigationState* navigation_state,
       const network::ParsedPermissionsPolicy& permissions_policy_header,
       const blink::DocumentPolicyFeatureState& document_policy_header,
       const std::optional<base::UnguessableToken>& embedding_token,
@@ -1068,7 +1070,8 @@ class CONTENT_EXPORT RenderFrameImpl
   // This could result either in the creation of a new entry or a modification
   // of the current entry or nothing. If a new entry was created,
   // returns true, false otherwise.
-  void UpdateNavigationHistory(blink::WebHistoryCommitType commit_type);
+  void UpdateNavigationHistory(blink::WebHistoryCommitType commit_type,
+                               NavigationState* navigation_state);
 
   // Notify render_view_ observers that a commit happened.
   void NotifyObserversOfNavigationCommit(ui::PageTransition transition);
@@ -1076,7 +1079,8 @@ class CONTENT_EXPORT RenderFrameImpl
   // Updates the internal state following a navigation commit. This should be
   // called before notifying the FrameHost of the commit.
   void UpdateStateForCommit(blink::WebHistoryCommitType commit_type,
-                            ui::PageTransition transition);
+                            ui::PageTransition transition,
+                            NavigationState* navigation_state);
 
   // Internal function used by same document navigation as well as cross
   // document navigation that updates the state of the RenderFrameImpl and sends
@@ -1084,6 +1088,7 @@ class CONTENT_EXPORT RenderFrameImpl
   void DidCommitNavigationInternal(
       blink::WebHistoryCommitType commit_type,
       ui::PageTransition transition,
+      NavigationState* navigation_state,
       const network::ParsedPermissionsPolicy& permissions_policy_header,
       const blink::DocumentPolicyFeatureState& document_policy_header,
       mojom::DidCommitProvisionalLoadInterfaceParamsPtr interface_params,
