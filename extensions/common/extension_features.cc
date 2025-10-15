6c6e1585e491c2b965e258d4e3f35c4a89c6024c
Austin Zhang <austinzzr@google.com>
2025-10-15 16:34:21 -0700

    Add feature for Enables the shouldShowPromotion API.
    Add the feature that enables the shouldShowPromotion API to determine
    which promotion to show for Chrome Enterprise on CWS.
    
    Extension API request doc: http://shortn/_HBB03W98hK
    
    Bug: b:436245479
    
    Change-Id: I7d6c5de51d0185ec9972ae938c1325b57017a206
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7042054
    Reviewed-by: Owen Min <zmin@chromium.org>
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: Austin Zhang <austinzzr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1530525}

--- extensions/common/extension_features.cc
+++ extensions/common/extension_features.cc
@@ -170,4 +170,6 @@ BASE_FEATURE(kContentVerifyJobUseJobVersionForHashing,
 BASE_FEATURE(kRuntimeOnMessageWebExtensionPolyfillSupport,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kEnableShouldShowPromotion, base::FEATURE_DISABLED_BY_DEFAULT);
+
 }  // namespace extensions_features
