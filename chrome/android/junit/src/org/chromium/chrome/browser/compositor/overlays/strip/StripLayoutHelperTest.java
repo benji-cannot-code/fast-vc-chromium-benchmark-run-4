40e1c720b65a9f1b1d924e1ce6d99644a5756ced
Neil Coronado <nemco@google.com>
2025-10-15 10:42:28 -0700

    [TabStrip] Fix mHandler formatting
    Fix mHandler formatting.
    -Wasn't a class variable, so the "m" prefix was unnecessary.
    -Just pass in the new object inline instead.
    
    Change-Id: Ib9ba408eefb2fcdca0ea6f0b8fe9b3cbbef2f036
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7042915
    Reviewed-by: Zhe Li <zheliooo@google.com>
    Commit-Queue: Zhe Li <zheliooo@google.com>
    Auto-Submit: Neil Coronado <nemco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1530324}

--- chrome/android/junit/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelperTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelperTest.java
@@ -289,9 +289,8 @@ public class StripLayoutHelperTest {
         mActivity.setTheme(R.style.Theme_BrowserUI_DayNight);
         when(mWindowAndroid.getActivity()).thenReturn(new WeakReference<>(mActivity));
         CompositorAnimationHandler.setTestingMode(true);
-        CompositorAnimationHandler mHandler =
-                new CompositorAnimationHandler(CallbackUtils.emptyRunnable());
-        when(mUpdateHost.getAnimationHandler()).thenReturn(mHandler);
+        when(mUpdateHost.getAnimationHandler())
+                .thenReturn(new CompositorAnimationHandler(CallbackUtils.emptyRunnable()));
         when(mModel.getProfile()).thenReturn(mProfile);
         DataSharingServiceFactory.setForTesting(mDataSharingService);
         TabGroupSyncServiceFactory.setForTesting(mTabGroupSyncService);
@@ -4439,10 +4438,6 @@ public class StripLayoutHelperTest {
     }
 
     private void setupForAnimations() {
-        CompositorAnimationHandler mHandler =
-                new CompositorAnimationHandler(CallbackUtils.emptyRunnable());
-        when(mUpdateHost.getAnimationHandler()).thenReturn(mHandler);
-
         // Update layout when updateHost.requestUpdate is called.
         doAnswer(
                         invocation -> {
