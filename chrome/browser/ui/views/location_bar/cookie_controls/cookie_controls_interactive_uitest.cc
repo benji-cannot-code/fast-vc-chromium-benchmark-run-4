c49c3ee7ad918f6b1d6a0636a2813802828d32fa
Dibyajyoti Pal <dibyapal@google.com>
2025-10-15 10:29:48 -0700

    [Gardening] Disable CookieControlsWithIphUiTest on win-asan bots
    Sample failed bot: https://ci.chromium.org/ui/p/chromium/builders/ci/win-asan/37378/overview
    
    Bug: 409272227
    Change-Id: Icb0d21982b541f78c83a342e96cea75167e5e4fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7046301
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Owners-Override: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Auto-Submit: Dibyajyoti Pal <dibyapal@chromium.org>
    Owners-Override: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1530318}

--- chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_interactive_uitest.cc
+++ chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_interactive_uitest.cc
@@ -407,8 +407,9 @@ class CookieControlsWithIphUiTest : public CookieControlsInteractiveTestBase {
   ~CookieControlsWithIphUiTest() override = default;
 };
 
-// TODO(crbug.com/409272227): IPH tests are flaky on Linux.
-#if BUILDFLAG(IS_LINUX) && defined(NDEBUG)
+// TODO(crbug.com/409272227): IPH tests are flaky on Linux and Win asan bots
+#if (BUILDFLAG(IS_LINUX) && defined(NDEBUG)) || \
+    (BUILDFLAG(IS_WIN) && defined(ADDRESS_SANITIZER))
 #define MAYBE_ShowAndDismissIphOnHighSiteEngagement \
   DISABLED_ShowAndDismissIphOnHighSiteEngagement
 #else
@@ -462,8 +463,9 @@ IN_PROC_BROWSER_TEST_F(CookieControlsWithIphUiTest,
           user_education::HelpBubbleView::kHelpBubbleElementIdForTesting));
 }
 
-// TODO(crbug.com/409272227): Flaky on linux-rel.
-#if BUILDFLAG(IS_LINUX) && defined(NDEBUG)
+// TODO(crbug.com/409272227): Flaky on linux-rel and win-asan.
+#if (BUILDFLAG(IS_LINUX) && defined(NDEBUG)) || \
+    (BUILDFLAG(IS_WIN) && defined(ADDRESS_SANITIZER))
 #define MAYBE_OpenUserBypassViaIconWhenIphVisible \
   DISABLED_OpenUserBypassViaIconWhenIphVisible
 #else
