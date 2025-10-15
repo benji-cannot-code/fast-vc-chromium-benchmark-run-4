48cfd8f85dc39c8b292ae44a298fb721566627b5
Min Xiong <minxiong@microsoft.com>
2025-10-15 06:34:10 -0700

    [iOS] Add FilterView for download list.
    This CL adds a new filter view component to the iOS Chrome download
    list, enabling users to filter downloads by file type categories.
    
    Key Changes:
    - Added DownloadListFilterView with horizontal scrollable filter chips
    - Supports 7 filter categories: All, Documents, Images, Videos, Audios,
     PDFs, Others
    - Each filter displays an SF Symbol icon and localized text label
    - Implements delegate pattern for filter selection notifications
    - Added filter visibility control to DownloadListConsumer protocol
    - Integrated filter view into download list table view controller
    - Added comprehensive localization strings for all filter types
    
    Strings added:
    - IDS_IOS_DOWNLOAD_FILTER_ALL
    https://drive.google.com/file/d/1b6D3OUKakVVde0bYFxUGmGtOlTpV9zT4/view?usp=drive_link
    - IDS_IOS_DOWNLOAD_FILTER_AUDIOS
    https://drive.google.com/file/d/1BdRMB2yT9gVDWeUEpng6llN4r6HQjUfQ/view?usp=drive_link
    - IDS_IOS_DOWNLOAD_FILTER_DOCUMENTS
    https://drive.google.com/file/d/1Jc8IN4xjBxczbgDKGZscTQANxSXtJD7z/view?usp=drive_link
    - IDS_IOS_DOWNLOAD_FILTER_IMAGES
    https://drive.google.com/file/d/1ozYRsTbHdD6JTh3O0kLoKddZKfywNla0/view?usp=drive_link
    - IDS_IOS_DOWNLOAD_FILTER_OTHERS
    https://drive.google.com/file/d/1pg-SztzSjw5JFXlbfG6tpfnkBVs7xmk9/view?usp=drive_link
    - IDS_IOS_DOWNLOAD_FILTER_PDFS
    https://drive.google.com/file/d/1QeAeKI4lbtRqM4FDDbqTiVrKCwne8ICq/view?usp=drive_link
    - IDS_IOS_DOWNLOAD_FILTER_VIDEOS
    https://drive.google.com/file/d/1BMmYoXsWeJl0QUkwM4u3Uy2OFIqJfces/view?usp=drive_link
    
    Change-Id: I92db663d3471ef430b7172aa890f4e4a099f430d
    Bug: none
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6966280
    Commit-Queue: Min Xiong <minxiong@microsoft.com>
    Reviewed-by: Quentin Pubert <qpubert@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1530163}

---
+++ ios/chrome/browser/download/ui/download_list/download_filter_cell.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_DOWNLOAD_UI_DOWNLOAD_LIST_DOWNLOAD_FILTER_CELL_H_
+#define IOS_CHROME_BROWSER_DOWNLOAD_UI_DOWNLOAD_LIST_DOWNLOAD_FILTER_CELL_H_
+
+#import <UIKit/UIKit.h>
+
+#import "ios/chrome/browser/download/model/download_filter_util.h"
+
+// Collection view cell representing an individual download filter option.
+// Displays an icon and text label for each filter type (e.g., All, Documents,
+// Images).
+@interface DownloadFilterCell : UICollectionViewCell
+
+// Configures the cell with the specified filter type, updating the display text
+// and icon.
+- (void)configureWithFilterType:(DownloadFilterType)filterType;
+
+// Returns the calculated width for the download filter cell with the given
+// filter type.
++ (CGFloat)cellSizeForFilterType:(DownloadFilterType)filterType;
+
+// Returns the standard height for download filter cells.
++ (CGFloat)cellHeight;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_DOWNLOAD_UI_DOWNLOAD_LIST_DOWNLOAD_FILTER_CELL_H_
