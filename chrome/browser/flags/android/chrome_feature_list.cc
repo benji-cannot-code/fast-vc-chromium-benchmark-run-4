8737f9396dc07b877778be69d45fbebb71b63544
Calder Kitagawa <ckitagawa@chromium.org>
2025-10-15 09:49:02 -0700

    [Tab] Create a flag to load all tabs at startup
    Introduce a flag that will be used to gate creating a WebContents for
    every tab at startup on Android. We will not create renderers for
    background tabs eagerly, but when enabled this flag will allow us to
    reuse more desktop code that expects tabs to always have WebContents.
    
    We speculate that there is a significant performance cost to this
    change as in local testing creating a single WebContents even without
    a renderer can take 10 ms and use O(100 KiB).
    
    Presently the intent is to 1) develop this feature for evaluation,
    2) measure the "real-world" performance cost, 3) profile why the cost
    is so high and optimize. Depending on the results we may consider
    shipping this change to some configurations.
    
    Bug: 448420873
    Change-Id: Ic1a7224a4aa38591fe606e38044230aa2c0552e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7042724
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1530283}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -349,6 +349,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kKeyboardEscBackNavigation,
     &kLegacyTabStateDeprecation,
     &kLensOnQuickActionSearchWidget,
+    &kLoadAllTabsAtStartup,
     &kLoadNativeEarly,
     &kLockBackPressHandlerAtStart,
     &kLockTopControlsOnLargeTablets,
@@ -690,6 +691,7 @@ BASE_FEATURE(kInstanceSwitcherV2, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kKeyboardEscBackNavigation, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kLegacyTabStateDeprecation, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kLensOnQuickActionSearchWidget, base::FEATURE_ENABLED_BY_DEFAULT);
+BASE_FEATURE(kLoadAllTabsAtStartup, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kLoadNativeEarly, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kLockBackPressHandlerAtStart, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kLockTopControlsOnLargeTablets, base::FEATURE_DISABLED_BY_DEFAULT);
