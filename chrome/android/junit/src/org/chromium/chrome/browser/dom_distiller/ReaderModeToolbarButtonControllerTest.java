5cccc406ff4a6fa8940eb0147a8dafd257e8d584
Evan Luo <evanluo@google.com>
2025-10-15 11:44:01 -0700

    [reader mode] Fix Reading Mode capitalization to Reading mode
    These changes only affect Android.
    
    Bottomsheet: https://screenshot.googleplex.com/4t7EnLeKQohvfw4.png
    CPA: https://screenshot.googleplex.com/7wEw5bYLQVNdE3F.png
    Overflow show: https://screenshot.googleplex.com/8d5xHNaGDaMBLJJ.png
    Overflow hide: https://screenshot.googleplex.com/5a6wyAeLjaoiYQW.png
    Message: https://screenshot.googleplex.com/5yvDA4ZjKDRK55q.png
    
    Bug: 446915810
    Change-Id: Id1a702466b2307da731e10ba9bfc495fb3f2dad6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7041425
    Commit-Queue: Evan Luo <evanluo@google.com>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1530360}

--- chrome/android/junit/src/org/chromium/chrome/browser/dom_distiller/ReaderModeToolbarButtonControllerTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/dom_distiller/ReaderModeToolbarButtonControllerTest.java
@@ -168,8 +168,7 @@ public class ReaderModeToolbarButtonControllerTest {
         when(mMockTab.getUrl()).thenReturn(new GURL("chrome-distiller://test"));
         when(mDomDistillerUrlUtilsJni.isDistilledPage(any())).thenReturn(true);
         controller.getTabSupplierObserverForTesting().onUrlUpdated(mMockTab);
-        assertEquals(
-                "Hide Reading Mode",
+        assertEquals("Hide Reading mode",
                 controller.getButtonDataForTesting().getButtonSpec().getContentDescription());
         assertEquals(R.string.hide_reading_mode_text, controller.getButtonDataForTesting().getButtonSpec().getHoverTooltipTextId());
         assertTrue(controller.getButtonDataForTesting().getButtonSpec().isChecked());
@@ -186,8 +185,7 @@ public class ReaderModeToolbarButtonControllerTest {
         when(mMockTab.getUrl()).thenReturn(new GURL("chrome-distiller://test"));
         when(mDomDistillerUrlUtilsJni.isDistilledPage(any())).thenReturn(true);
         controller.getTabSupplierObserverForTesting().onUrlUpdated(mMockTab);
-        assertEquals(
-                "Hide Reading Mode",
+        assertEquals("Hide Reading mode",
                 controller.getButtonDataForTesting().getButtonSpec().getContentDescription());
 
         // Now do the same thing with a null tab.
