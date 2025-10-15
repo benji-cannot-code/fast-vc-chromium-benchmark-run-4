e05077479a08a0589eb9767642313b99ba9d7b89
Antonio Alphonse <alphonsea@google.com>
2025-10-15 08:14:01 -0700

    crypto: add obsolete/sha1
    This CL preps to move SHA1 hashing functionality from crypto/hash to
    obsolete/sha1 by first creating the relevant files.
    
    Bug: 430344933
    Change-Id: I7996cf8dee8de1fbaa42b00cedb478981ac5edc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7042383
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1530215}

---
+++ crypto/obsolete/sha1_unittest.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "crypto/obsolete/sha1.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+using Sha1Test = ::testing::Test;
+
+namespace crypto::obsolete {
+
+TEST_F(Sha1Test, KnownAnswer) {
+  const std::string input = "The quick brown fox jumps over the lazy dog";
+  // clang-format off
+ constexpr auto expected = std::to_array<uint8_t>({
+   0x2f, 0xd4, 0xe1, 0xc6, 0x7a, 0x2d, 0x28, 0xfc,
+   0xed, 0x84, 0x9e, 0xe1, 0xbb, 0x76, 0xe7, 0x39,
+   0x1b, 0x93, 0xeb, 0x12,
+ });
+ // clang-format on
+
+  EXPECT_EQ(expected, crypto::obsolete::Sha1::Hash(base::as_byte_span(input)));
+}
+
+}  // namespace crypto::obsolete
