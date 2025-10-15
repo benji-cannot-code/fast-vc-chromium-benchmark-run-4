8d721e32762731a69ed7ece9c749e2a1d51d73c5
izuzic@google.com <izuzic@google.com>
2025-10-15 04:08:12 -0700

    [PasskeyUnlockErrorUI] PasskeyUnlockManager observes PasskeyModel
    This change adds `PasskeyModel::Observer` as a base class to
    `PasskeyUnlockManager` and includes stub implementations for the
    observer methods. A `raw_ptr` to `PasskeyModel` is also added as a
    member.
    
    contain the logic. A lot of methods are still just placeholders. More
    tests will be added in follow-up CLs.
    
    Change-Id: I39e8f19586decde53ec5e63d7f3c1804aa91d303
    Bug: 450271136, 449948456
    Low-Coverage-Reason: TRIVIAL_CHANGE Passkey unlock manager still doesn't
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7030861
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Reviewed-by: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1530092}

--- chrome/browser/webauthn/passkey_unlock_manager.cc
+++ chrome/browser/webauthn/passkey_unlock_manager.cc
@@ -9,9 +9,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notimplemented.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/webauthn/passkey_model_factory.h"
 
 namespace webauthn {
-PasskeyUnlockManager::PasskeyUnlockManager(Profile* profile) {}
+PasskeyUnlockManager::PasskeyUnlockManager(Profile* profile) {
+  passkey_model_observation_.Observe(
+      PasskeyModelFactory::GetForProfile(profile));
+  UpdateHasPasskeys();
+  NotifyObservers();
+}
 
 PasskeyUnlockManager::~PasskeyUnlockManager() = default;
 
@@ -36,9 +42,18 @@ void PasskeyUnlockManager::OpenTabForPasskeyUnlockHandler(Browser* browser) {
   NOTIMPLEMENTED();
 }
 
+PasskeyModel* PasskeyUnlockManager::passkey_model() {
+  return passkey_model_observation_.GetSource();
+}
+
+void PasskeyUnlockManager::UpdateHasPasskeys() {
+  has_passkeys_ = !passkey_model()->GetAllPasskeys().empty();
+}
+
 void PasskeyUnlockManager::NotifyObservers() {
-  // TODO(crbug.com/450271136): Implement and call when the state changes.
-  NOTIMPLEMENTED();
+  for (Observer& observer : observer_list_) {
+    observer.OnPasskeyUnlockManagerStateChanged();
+  }
 }
 
 void PasskeyUnlockManager::AsynchronouslyCheckGpmPinAvailability() {
@@ -54,6 +69,22 @@ void PasskeyUnlockManager::AsynchronouslyCheckSystemUVAvailability() {
 void PasskeyUnlockManager::AsynchronouslyLoadEnclaveManager() {
   // TODO(crbug.com/449949272): Implement and call in the constructor.
   NOTIMPLEMENTED();
+}
+
+// webauthn::PasskeyModel::Observer
+void PasskeyUnlockManager::OnPasskeysChanged(
+    const std::vector<webauthn::PasskeyModelChange>& changes) {
+  UpdateHasPasskeys();
+  NotifyObservers();
+}
+
+// webauthn::PasskeyModel::Observer
+void PasskeyUnlockManager::OnPasskeyModelShuttingDown() {}
+
+// webauthn::PasskeyModel::Observer
+void PasskeyUnlockManager::OnPasskeyModelIsReady(bool is_ready) {
+  UpdateHasPasskeys();
+  NotifyObservers();
 }
 
 }  // namespace webauthn
