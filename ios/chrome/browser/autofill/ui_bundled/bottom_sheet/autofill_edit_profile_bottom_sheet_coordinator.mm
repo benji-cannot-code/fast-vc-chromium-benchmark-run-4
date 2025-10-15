2c8aa62797fba22cb625c3379d251685af1416e9
Vidhan Jain <vidhanj@google.com>
2025-10-15 03:45:03 -0700

    Remove obsolete todo
    Bug: 40281788
    Change-Id: I2103f2a85b262b2bb76cbb3f574b06735f9e1416
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7041125
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Auto-Submit: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1530081}

--- ios/chrome/browser/autofill/ui_bundled/bottom_sheet/autofill_edit_profile_bottom_sheet_coordinator.mm
+++ ios/chrome/browser/autofill/ui_bundled/bottom_sheet/autofill_edit_profile_bottom_sheet_coordinator.mm
@@ -212,7 +212,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)autofillEditProfileMediatorDidFinish:
     (AutofillProfileEditMediator*)mediator {
-  // TODO(crbug.com/40281788): Implement.
 }
 
 - (void)willSelectCountryWithCurrentlySelectedCountry:(NSString*)country
