33c292055c438c6b5e626c3df864c84e59aa50e9
Dominic Battre <battre@chromium.org>
2025-10-15 05:55:51 -0700

    Add ExpiringSubscriptionManager for time-limited callbacks
    This CL introduces ExpiringSubscriptionManager, a generic class for
    managing event subscriptions that have a defined expiration time.
    
    Subscribers receive notifications for events until their subscription
    expires. When a subscription is about to expire, a preliminary callback
    offers a chance to extend its lifetime. If not extended, a final
    expiration callback is triggered.
    
    Bug: 415273270
    Change-Id: I3e9301b9f4fd68964d3b4ad8429323e008d48a21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7021074
    Reviewed-by: Matthias Körber <koerber@google.com>
    Auto-Submit: Dominic Battre <battre@chromium.org>
    Commit-Queue: Dominic Battre <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1530141}

---
+++ components/one_time_tokens/core/browser/util/expiring_subscription.cc
@@ -0,0 +1,55 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/one_time_tokens/core/browser/util/expiring_subscription.h"
+
+#include "components/one_time_tokens/core/browser/util/expiring_subscription_manager.h"
+
+namespace one_time_tokens {
+
+ExpiringSubscription::ExpiringSubscription() = default;
+
+ExpiringSubscription::ExpiringSubscription(
+    ExpiringSubscriptionHandle handle,
+    base::WeakPtr<ExpiringSubscriptionManagerBase> manager)
+    : handle_(std::move(handle)), manager_(std::move(manager)) {}
+
+ExpiringSubscription::ExpiringSubscription(ExpiringSubscription&& other) {
+  *this = std::move(other);
+}
+
+ExpiringSubscription::~ExpiringSubscription() {
+  if (manager_) {
+    manager_->Cancel(handle_);
+  }
+}
+
+bool ExpiringSubscription::IsAlive() const {
+  return manager_ && manager_->Exists(handle_);
+}
+
+void ExpiringSubscription::Cancel() {
+  if (manager_) {
+    manager_->Cancel(handle_);
+  }
+}
+
+void ExpiringSubscription::SetExpirationTime(base::Time new_expiration) {
+  if (manager_) {
+    manager_->SetExpirationTime(handle_, new_expiration);
+  }
+}
+
+ExpiringSubscription& ExpiringSubscription::operator=(
+    ExpiringSubscription&& other) {
+  if (manager_) {
+    manager_->Cancel(handle_);
+  }
+  handle_ = std::move(other.handle_);
+  manager_ = other.manager_;
+  return *this;
+}
+
+}  // namespace one_time_tokens
