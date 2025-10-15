e66dbdf77c14b4d0557bb8e51bd5d69311b772bc
Masa Fujita <massan@google.com>
2025-10-15 00:01:01 -0700

    flyout: Add feature flag for using submenus in the app menu
    Bug: 438629323
    Change-Id: Idc1d09e013510351ae8a34beb52c9e24a7487f1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7041062
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Masa Fujita <massan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1529987}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2256,6 +2256,9 @@ extern const char kStrictOriginIsolationName[];
 extern const char kStrictOriginIsolationDescription[];
 
 #if BUILDFLAG(IS_ANDROID)
+extern const char kSubmenusInAppMenuName[];
+extern const char kSubmenusInAppMenuDescription[];
+
 extern const char kSubmenusTabContextMenuLffTabStripName[];
 extern const char kSubmenusTabContextMenuLffTabStripDescription[];
 #endif  // BUILDFLAG(IS_ANDROID)
