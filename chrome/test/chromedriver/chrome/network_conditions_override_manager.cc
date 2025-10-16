1e1c13c097dc71c525555c8e0ff51851134593ab
Alex N. Jose <alexnj@chromium.org>
2025-10-16 09:10:37 -0700

    Fix a UAF crash within OverrideNetworkConditions
    The raw_ptr saved for subsequent application would get freed between
    back-to-back OverrideNetworkConditions calls, causing a UAF crash.
    
    Bug: 42323833
    Change-Id: I6399f06ad4a99c9b54566463988ce67980661faa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7047422
    Reviewed-by: Maksim Sadym <sadym@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Alex N. Jose <alexnj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1530891}

--- chrome/test/chromedriver/chrome/network_conditions_override_manager.cc
+++ chrome/test/chromedriver/chrome/network_conditions_override_manager.cc
@@ -20,8 +20,10 @@ NetworkConditionsOverrideManager::~NetworkConditionsOverrideManager() = default;
 Status NetworkConditionsOverrideManager::OverrideNetworkConditions(
     const NetworkConditions& network_conditions) {
   Status status = ApplyOverride(&network_conditions);
-  if (status.IsOk())
-    overridden_network_conditions_ = &network_conditions;
+  if (status.IsOk()) {
+    overridden_network_conditions_ =
+        std::make_unique<NetworkConditions>(network_conditions);
+  }
   return status;
 }
 
@@ -42,7 +44,7 @@ Status NetworkConditionsOverrideManager::OnEvent(
 
 Status NetworkConditionsOverrideManager::ApplyOverrideIfNeeded() {
   if (overridden_network_conditions_)
-    return ApplyOverride(overridden_network_conditions_);
+    return ApplyOverride(overridden_network_conditions_.get());
   return Status(kOk);
 }
 
