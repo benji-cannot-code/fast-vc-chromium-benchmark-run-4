7d24eb9af832bf71d3a9cccdc850ac2bf2f18c39
hiroki oshima <hiroki.oshima@gmail.com>
2025-10-15 18:04:49 -0700

    fixed dangling raw_ptr in ToastManagerImplTest
    Bug: 40212619
    
    Change-Id: I3a631f275496bfb69933d79777f4c327d6834d41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7032853
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Reviewed-by: Di Wu <diwux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1530565}

--- ash/system/toast/toast_manager_unittest.cc
+++ ash/system/toast/toast_manager_unittest.cc
@@ -109,6 +109,11 @@ class ToastManagerImplTest : public AshTestBase,
     SetShouldLockScreenAutomatically(false);
   }
 
+  void TearDown() override {
+    manager_ = nullptr;
+    AshTestBase::TearDown();
+  }
+
  protected:
   ToastManagerImpl* manager() { return manager_; }
 
@@ -196,7 +201,7 @@ class ToastManagerImplTest : public AshTestBase,
   }
 
  private:
-  raw_ptr<ToastManagerImpl, DanglingUntriaged> manager_ = nullptr;
+  raw_ptr<ToastManagerImpl> manager_ = nullptr;
   unsigned int serial_ = 0;
 };
 
