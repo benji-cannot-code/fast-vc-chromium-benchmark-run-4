5af54a194c8d4ac413114576524560a2714bed47
David Sanders <dsanders11@ucsbalum.com>
2025-10-15 18:27:50 -0700

    Restore forward decl in //ui/compositor/layer_owner.h
    This forward decl was removed as part of
    https://chromium-review.googlesource.com/c/chromium/src/+/7003915 due to
    the `std::unique_ptr<T>` destructor requiring a complete type in C++23.
    This can be avoided by creating a second constructor rather than using a
    default argument.
    
    Should save ~1.12 GiB of compiler input size, which was added in the
    aforementioned CL.
    
    Bug: 429365675
    Change-Id: I60c3c08a9fd4f9a7d158134ed2f1f37f48f009b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7043153
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1530579}

--- ui/compositor/layer_owner.cc
+++ ui/compositor/layer_owner.cc
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 
+LayerOwner::LayerOwner() = default;
+
 LayerOwner::LayerOwner(std::unique_ptr<Layer> layer) {
   if (layer)
     SetLayer(std::move(layer));
--- ui/compositor/layer_owner.h
+++ ui/compositor/layer_owner.h
@@ -11,9 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/observer_list.h"
 #include "ui/compositor/compositor_export.h"
-#include "ui/compositor/layer.h"
 
 namespace ui {
+
+class Layer;
 
 class COMPOSITOR_EXPORT LayerOwner {
  public:
@@ -29,7 +30,8 @@ class COMPOSITOR_EXPORT LayerOwner {
     virtual ~Observer() = default;
   };
 
-  explicit LayerOwner(std::unique_ptr<Layer> layer = nullptr);
+  explicit LayerOwner();
+  explicit LayerOwner(std::unique_ptr<Layer> layer);
 
   LayerOwner(const LayerOwner&) = delete;
   LayerOwner& operator=(const LayerOwner&) = delete;
