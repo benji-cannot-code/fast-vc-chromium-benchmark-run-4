470a7dee72c00b9fe11e3a9acedab008cf278750
Michael Checo <michaelcheco@google.com>
2025-10-16 10:03:48 -0700

    Add onAudioPacket to ClientApi interface
    Change-Id: Ia7b9873268bbb206376b475e31bbb2671fdd50ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7046777
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Benjamin Zielinski <bzielinski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1530922}

--- ash/webui/boca_receiver_app_ui/resources/untrusted/app/boca_receiver_app.ts
+++ ash/webui/boca_receiver_app_ui/resources/untrusted/app/boca_receiver_app.ts
@@ -9,6 +9,23 @@ import {ConnectionClosedReason, ReceiverInfo, UserInfo} from '../mojom/boca_rece
 
 import {BrowserProxyImpl} from './browser_proxy.js';
 
+/**
+ * TODO(b/449757180): Remove this definition once the
+ * actual mojom::DecodedAudioPacket struct has been added and can be used
+ * directly.
+ *
+ * This is a temporary client-side representation of the
+ * mojom.DecodedAudioPacket.
+ */
+export interface DecodedAudioPacket {
+  /** Sample rate (e.g., 48000). */
+  sampleRate: number;
+  /** Number of channels (e.g., 2 for stereo). */
+  channels: number;
+  /** The raw, interleaved 16-bit PCM audio samples. */
+  data: Int16Array;
+}
+
 export declare interface ClientApi {
   /**
    * Initializes the client with the receiver info.
@@ -26,6 +43,12 @@ export declare interface ClientApi {
    * after onConnecting is called.
    */
   onFrameReceived(image: BitmapN32): void;
+
+  /**
+   * Notifies the client that an audio packet has been received.
+   * @param packet The audio packet to be processed.
+   */
+  onAudioPacket(packet: DecodedAudioPacket): void;
 
   /**
    * Notifies the client that the receiver is connecting to a new host.
