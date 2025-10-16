cb2b9c77f9fd4648b67d636e67f16360715eefae
Colin Blundell <blundell@chromium.org>
2025-10-15 23:49:10 -0700

    [//ui] Have NativePixmapDmaBuf hold SIFormat internally
    Followup CLs will add a constructor that passes SIFormat and
    incrementally convert clients to use this constructor.
    
    Bug: 445879532
    Change-Id: I0ad9cbe9f20e025fddb3f02d0bd79b54f117ecd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7044886
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1530656}

--- ui/gfx/linux/native_pixmap_dmabuf.h
+++ ui/gfx/linux/native_pixmap_dmabuf.h
@@ -50,7 +50,7 @@ class COMPONENT_EXPORT(GFX) NativePixmapDmaBuf : public gfx::NativePixmap {
 
  private:
   gfx::Size size_;
-  gfx::BufferFormat format_;
+  viz::SharedImageFormat format_;
   gfx::NativePixmapHandle handle_;
 };
 
