9ac281aeb443df35ea9cb6af3b1cfc65891a6e12
Ramin Halavati <rhalavati@chromium.org>
2025-10-16 14:25:25 -0700

    [a11y] Return empty role description for Grid, Table, and TreeGrid.
    Role description is not needed for `kGrid`, `kTable`, and `kTreeGrid`
    Chrome roles on Android since TalkBack recognizes the role correctly.
    
    Update`BrowserAccessibilityAndroid::GetRoleDescription` and remove
    role description for them in all Android test expectations.
    
    AX-Relnotes: n/a
    Bug: 447360631
    Change-Id: I45bd7fd8985a7317acaf0a3877d8e48b8b392fe0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7044706
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Auto-Submit: Ramin Halavati <rhalavati@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1531077}

--- content/browser/accessibility/browser_accessibility_android.cc
+++ content/browser/accessibility/browser_accessibility_android.cc
@@ -1540,6 +1540,7 @@ std::u16string BrowserAccessibilityAndroid::GetRoleDescription() const {
     case ax::mojom::Role::kDetails:
     case ax::mojom::Role::kEmphasis:
     case ax::mojom::Role::kForm:
+    case ax::mojom::Role::kGrid:
     case ax::mojom::Role::kImage:
     case ax::mojom::Role::kProgressIndicator:
     case ax::mojom::Role::kRowGroup:
@@ -1549,7 +1550,9 @@ std::u16string BrowserAccessibilityAndroid::GetRoleDescription() const {
     case ax::mojom::Role::kStrong:
     case ax::mojom::Role::kSubscript:
     case ax::mojom::Role::kSuperscript:
+    case ax::mojom::Role::kTable:
     case ax::mojom::Role::kTextField:
+    case ax::mojom::Role::kTreeGrid:
     case ax::mojom::Role::kTime:
       // No role description on Android.
       break;
