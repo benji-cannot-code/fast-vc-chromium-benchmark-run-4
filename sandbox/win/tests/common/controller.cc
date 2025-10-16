138166f483ad918d19929a0d19f99a3698793bd4
Egor Pasko <pasko@chromium.org>
2025-10-16 11:03:08 -0700

    win: Use TestFuture for testing SpawnTargetAsync
    Replace the use of RunLoop with base::test::TestFuture to avoid creating
    a callback manually.
    
    Bug: 448511116
    Change-Id: Id5a9b39ce316481508de32dfb5f9c2561b6f2a1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7044431
    Reviewed-by: James Forshaw <forshaw@chromium.org>
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1530958}

--- sandbox/win/tests/common/controller.cc
+++ sandbox/win/tests/common/controller.cc
@@ -21,13 +21,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notreached.h"
 #include "base/process/process.h"
 #include "base/process/process_handle.h"
-#include "base/run_loop.h"
 #include "base/sequence_checker.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/strings/sys_string_conversions.h"
-#include "base/test/bind.h"
 #include "base/test/task_environment.h"
+#include "base/test/test_future.h"
 #include "base/test/test_timeouts.h"
 #include "base/time/time.h"
 #include "base/unguessable_token.h"
@@ -333,18 +332,14 @@ int TestRunner::InternalRunTest(const wchar_t* command) {
     }
   } else {
     base::test::TaskEnvironment task_environment;
-    base::RunLoop run_loop;
-    broker_->SpawnTargetAsync(
-        prog_name, arguments.c_str(), std::move(policy_),
-        base::BindLambdaForTesting(
-            [&](base::win::ScopedProcessInformation proc_info, DWORD error,
-                ResultCode res) {
-              target = proc_info.Take();
-              last_error = error;
-              result = res;
-              run_loop.Quit();
-            }));
-    run_loop.Run();
+    base::test::TestFuture<base::win::ScopedProcessInformation, DWORD,
+                           ResultCode>
+        test_future;
+    broker_->SpawnTargetAsync(prog_name, arguments.c_str(), std::move(policy_),
+                              test_future.GetCallback());
+    base::win::ScopedProcessInformation proc_info;
+    std::tie(proc_info, last_error, result) = test_future.Take();
+    target = proc_info.Take();
   }
 
   if (SBOX_ALL_OK != result)
