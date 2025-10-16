6b95f88588a81f49f04e111307d4d87acbd9d7cb
Alex Chen <alexwchen@chromium.org>
2025-10-16 07:47:01 -0700

    [omnibox-next] Fix icons for featured enterprise search matches
    This CL updates relevant code so that we use vector icons instead of
    favicons for featured enterprise search aggregator matches in the WebUI
    Omnibox popup. This change ensures that the proper favicon_url is used
    for featured enterprise site search matches. Lastly, it adds a colored
    background container similar to starter pack matches to match
    the Views Omnibox popup.
    
    This change also makes the favicon of a site search engine visible
    before navigation by setting the match icon_url based on the
    keyword's TemplateURL favicon_url.
    
    This change is similar to https://crrev.com/c/6998247 which applied
    similar formatting for starter pack.
    
    BEFORE: https://screenshot.googleplex.com/9UH8mNffKazTU6V
    AFTER:
     - WebUI with icon_url unset: https://screenshot.googleplex.com/AWLtiayp6QNMLEp
     - WebUI with icon_url set: https://screenshot.googleplex.com/ADUsKTAD4NSgxcW
     - Views popup with icon_url unset: https://screenshot.googleplex.com/7gr3ncfvwzXYk7g
     - Views popup with icon_url set: https://screenshot.googleplex.com/7wS4ygYo7tm2N82
    
    Bug: 452322593
    Change-Id: I17876196dfa09044624b2d6bdc200d188c46c960
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7042259
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Alex Chen <alexwchen@chromium.org>
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1530843}

--- ui/webui/resources/cr_components/searchbox/searchbox_icon.ts
+++ ui/webui/resources/cr_components/searchbox/searchbox_icon.ts
@@ -14,6 +14,7 @@ import {getHtml} from './searchbox_icon.html.js';
 
 const CALCULATOR: string = 'search-calculator-answer';
 const DOCUMENT_MATCH_TYPE: string = 'document';
+const FEATURED_ENTERPRISE_SEARCH: string = 'featured-enterprise-search';
 const HISTORY_CLUSTER_MATCH_TYPE: string = 'history-cluster';
 const PEDAL: string = 'pedal';
 const STARTER_PACK: string = 'starter-pack';
@@ -89,6 +90,14 @@ export class SearchboxIconElement extends CrLitElement {
        * Whether icon belongs to a starter pack match.
        */
       isStarterPack: {
+        type: Boolean,
+        reflect: true,
+      },
+
+      /**
+       * Whether icon belongs to a featured enterprise search match.
+       */
+      isFeaturedEnterpriseSearch: {
         type: Boolean,
         reflect: true,
       },
@@ -179,6 +188,7 @@ export class SearchboxIconElement extends CrLitElement {
   accessor inSearchbox: boolean = false;
   accessor isAnswer: boolean = false;
   accessor isStarterPack = false;
+  accessor isFeaturedEnterpriseSearch = false;
   accessor isWeatherAnswer: boolean = false;
   accessor isEnterpriseSearchAggregatorPeopleType: boolean = false;
   accessor maskImage: string = '';
@@ -206,6 +216,8 @@ export class SearchboxIconElement extends CrLitElement {
       this.isEnterpriseSearchAggregatorPeopleType =
           this.computeIsEnterpriseSearchAggregatorPeopleType_();
       this.isStarterPack = this.computeIsStarterPack_();
+      this.isFeaturedEnterpriseSearch =
+          this.computeIsFeaturedEnterpriseSearch();
       this.isWeatherAnswer = this.computeIsWeatherAnswer_();
       this.hasImage = this.computeHasImage_();
       this.maskImage = this.computeMaskImage_();
@@ -261,7 +273,9 @@ export class SearchboxIconElement extends CrLitElement {
         return `url(${this.match.iconPath})`;
       }
 
-      if (this.match.type !== HISTORY_CLUSTER_MATCH_TYPE) {
+      // Featured enterprise search suggestions have the icon set match.iconUrl.
+      if (this.match.type !== HISTORY_CLUSTER_MATCH_TYPE &&
+          this.match.type !== FEATURED_ENTERPRISE_SEARCH) {
         return getFaviconForPageURL(
             this.match.destinationUrl.url, /* isSyncedUrlForHistoryUi= */ false,
             /* remoteIconUrlForUma= */ '', /* size= */ 16,
@@ -308,10 +322,11 @@ export class SearchboxIconElement extends CrLitElement {
     if (this.isLensSearchbox_ && this.inSearchbox) {
       return `url(${this.defaultIcon})`;
     }
-    // Enterprise search aggregator people and starter pack suggestions should
-    // show icon even in searchbox.
+    // Enterprise search aggregator people and starter pack/featured enterprise
+    // search suggestions should show icon even in searchbox.
     if (this.match &&
         (!this.match.isRichSuggestion || this.match.type === STARTER_PACK ||
+         this.match.type === FEATURED_ENTERPRISE_SEARCH ||
          this.match.isEnterpriseSearchAggregatorPeopleType ||
          !this.inSearchbox)) {
       return `url(${this.match.iconPath})`;
@@ -433,8 +448,9 @@ export class SearchboxIconElement extends CrLitElement {
     this.imageError_ = true;
   }
 
-  // All pedals, starter pack suggestions, and AiS except weather should have
-  // a colored background container that matches the current theme.
+  // All pedals, starter pack/featured enterprise search suggestions, and AiS
+  // except weather should have a colored background container that matches the
+  // current theme.
   // TODO(niharm): Refactor logic in C++ and send via mojom in
   // "chrome/browser/ui/webui/searchbox/searchbox_handler.cc".
   private computeHasIconContainerBackground_(): boolean {
@@ -442,6 +458,7 @@ export class SearchboxIconElement extends CrLitElement {
       return this.match.type === PEDAL ||
           this.match.type === HISTORY_CLUSTER_MATCH_TYPE ||
           this.match.type === CALCULATOR || this.match.type === STARTER_PACK ||
+          this.match.type === FEATURED_ENTERPRISE_SEARCH ||
           (!!this.match.answer && !this.isWeatherAnswer);
     }
     return false;
@@ -449,6 +466,10 @@ export class SearchboxIconElement extends CrLitElement {
 
   private computeIsStarterPack_(): boolean {
     return this.match?.type === STARTER_PACK;
+  }
+
+  private computeIsFeaturedEnterpriseSearch(): boolean {
+    return this.match?.type === FEATURED_ENTERPRISE_SEARCH;
   }
 }
 
