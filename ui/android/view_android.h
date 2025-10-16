e62c750047d77d676fbfc3681130a8c7abbd93d1
Keita Suzuki <suzukikeita@chromium.org>
2025-10-16 16:49:07 -0700

    Add a hit test readiness check for prerendering
    This change introduces a callback mechanism to `ViewAndroid` to allow
    `RenderWidgetHostViewAndroid` to determine if a view is ready for hit
    testing.
    
    A crash was observed in `RenderWidgetHostViewBase::ProcessTouchEvent`
    when touch events were processed for a page that was in the process of
    prerendering.
    
    To fix this, a `HitTestCallback` is added to `ViewAndroid`.
    `RenderWidgetHostViewAndroid` sets this callback to a function that
    checks if the frame tree is currently prerendering. `ViewAndroid` then
    uses this callback to determine if it should proceed with hit testing.
    This prevents the crash by deferring touch event processing until the
    page has finished prerendering.
    
    Bug: 442832509
    Change-Id: Ibf793dfa53ae38e198fd16e6ce24b1643d72a844
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7040604
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Keita Suzuki <suzukikeita@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1531139}

--- ui/android/view_android.h
+++ ui/android/view_android.h
@@ -68,6 +68,8 @@ class UI_ANDROID_EXPORT ViewAndroid {
   using CopyViewCallback =
       base::RepeatingCallback<void(std::unique_ptr<viz::CopyOutputRequest>)>;
 
+  using HitTestCallback = base::RepeatingCallback<bool()>;
+
   // Stores an anchored view to delete itself at the end of its lifetime
   // automatically. This helps manage the lifecyle without the dependency
   // on |ViewAndroid|.
@@ -226,6 +228,8 @@ class UI_ANDROID_EXPORT ViewAndroid {
   std::unique_ptr<viz::CopyOutputRequest> MaybeRequestCopyOfView(
       std::unique_ptr<viz::CopyOutputRequest> request);
 
+  void SetHitTestCallback(HitTestCallback callback);
+
   void set_event_handler(EventHandlerAndroid* handler) {
     event_handler_ = handler;
   }
@@ -356,6 +360,10 @@ class UI_ANDROID_EXPORT ViewAndroid {
 
   // Copy output of View rather than window.
   CopyViewCallback copy_view_callback_;
+
+  // Conducts additional HitTest check to determine if a HitTest can actually
+  // be sent to the view.
+  HitTestCallback hit_test_callback_;
 
   bool controls_resize_view_ = false;
 
