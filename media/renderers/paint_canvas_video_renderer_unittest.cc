a3d964bcc15f89826e39f6eb54ece2d95eb92528
Colin Blundell <blundell@chromium.org>
2025-10-15 23:51:28 -0700

    [//media] Port PCVR tests to call PaintOOPR()
    We are incrementally hardening PCVR::Paint() to be able to verify that
    its passed-in raster context provider is either (a) null or (b) supports
    `gpu_rasterization`. This is trivially true in these software paths.
    
    Bug: 391648152
    Change-Id: I6faf242448b887bf77b8b5f3fe1bd5e3b7277e5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7044427
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1530660}

--- media/renderers/paint_canvas_video_renderer_unittest.cc
+++ media/renderers/paint_canvas_video_renderer_unittest.cc
@@ -331,7 +331,7 @@ void PaintCanvasVideoRendererTest::PaintWithoutFrame(cc::PaintCanvas* canvas) {
   flags.setFilterQuality(cc::PaintFlags::FilterQuality::kLow);
   PaintCanvasVideoRenderer::PaintParams params;
   params.dest_rect = kNaturalRect;
-  renderer_.Paint(nullptr, canvas, flags, params, nullptr);
+  renderer_.PaintOOPR(nullptr, canvas, flags, params, nullptr);
 }
 
 void PaintCanvasVideoRendererTest::Paint(scoped_refptr<VideoFrame> video_frame,
@@ -355,7 +355,7 @@ void PaintCanvasVideoRendererTest::PaintRotated(
   PaintCanvasVideoRenderer::PaintParams params;
   params.dest_rect = dest_rect;
   params.transformation = video_transformation;
-  renderer_.Paint(std::move(video_frame), canvas, flags, params, nullptr);
+  renderer_.PaintOOPR(std::move(video_frame), canvas, flags, params, nullptr);
 }
 
 void PaintCanvasVideoRendererTest::Copy(scoped_refptr<VideoFrame> video_frame,
@@ -423,11 +423,11 @@ TEST_F(PaintCanvasVideoRendererTest, ReinterpretAsSRGB) {
 
   PaintCanvasVideoRenderer::PaintParams params;
   params.dest_rect = kNaturalRect;
-  renderer_.Paint(natural_frame(), target_canvas(), flags, params, nullptr);
+  renderer_.PaintOOPR(natural_frame(), target_canvas(), flags, params, nullptr);
   EXPECT_NE(SK_ColorRED, bitmap()->getColor(0, 0));
 
   params.reinterpret_as_srgb = true;
-  renderer_.Paint(natural_frame(), target_canvas(), flags, params, nullptr);
+  renderer_.PaintOOPR(natural_frame(), target_canvas(), flags, params, nullptr);
   EXPECT_EQ(SK_ColorRED, bitmap()->getColor(0, 0));
 }
 
@@ -443,7 +443,7 @@ TEST_F(PaintCanvasVideoRendererTest, RGBAF16) {
 
   PaintCanvasVideoRenderer::PaintParams params;
   params.dest_rect = kNaturalRect;
-  renderer_.Paint(frame, &canvas, flags, params, nullptr);
+  renderer_.PaintOOPR(frame, &canvas, flags, params, nullptr);
   EXPECT_EQ(SK_ColorRED, bitmap.getColor(0, 0));
 }
 
@@ -459,7 +459,7 @@ TEST_F(PaintCanvasVideoRendererTest, RGBAF16_N32_Output) {
 
   PaintCanvasVideoRenderer::PaintParams params;
   params.dest_rect = kNaturalRect;
-  renderer_.Paint(frame, &canvas, flags, params, nullptr);
+  renderer_.PaintOOPR(frame, &canvas, flags, params, nullptr);
   EXPECT_EQ(SK_ColorRED, bitmap.getColor(0, 0));
 }
 
@@ -781,8 +781,8 @@ TEST_F(PaintCanvasVideoRendererTest, Y16) {
   flags.setFilterQuality(cc::PaintFlags::FilterQuality::kNone);
   PaintCanvasVideoRenderer::PaintParams paint_params;
   paint_params.dest_rect = gfx::RectF(bitmap.width(), bitmap.height());
-  renderer_.Paint(std::move(video_frame), &canvas, flags, paint_params,
-                  nullptr);
+  renderer_.PaintOOPR(std::move(video_frame), &canvas, flags, paint_params,
+                      nullptr);
   for (int j = 0; j < bitmap.height(); j++) {
     for (int i = 0; i < bitmap.width(); i++) {
       const int value = i + j * bitmap.width();
@@ -1022,7 +1022,7 @@ TEST_F(PaintCanvasVideoRendererTest, CorrectFrameSizeToVisibleRect) {
   cc::PaintFlags flags;
   PaintCanvasVideoRenderer::PaintParams params;
   params.dest_rect = gfx::RectF(visible_size.width(), visible_size.height());
-  renderer_.Paint(std::move(video_frame), &canvas, flags, params, nullptr);
+  renderer_.PaintOOPR(std::move(video_frame), &canvas, flags, params, nullptr);
 
   EXPECT_EQ(fWidth / 2, renderer_.LastImageDimensionsForTesting().width());
   EXPECT_EQ(fWidth / 2, renderer_.LastImageDimensionsForTesting().height());
