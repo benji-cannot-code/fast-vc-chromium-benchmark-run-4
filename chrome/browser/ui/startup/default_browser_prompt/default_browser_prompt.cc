74bc5df7c5de9cd2f6f196bb98bf87bed762d497
Kaan Alsan <alsan@chromium.org>
2025-10-16 12:48:10 -0700

    Remove unused functions from default_browser_prompt
    Change-Id: I0fc24bec8a7f3c691fd04a2bb966921e5b0c3fea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7049024
    Reviewed-by: Alison Gale <agale@chromium.org>
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1531020}

--- chrome/browser/ui/startup/default_browser_prompt/default_browser_prompt.cc
+++ chrome/browser/ui/startup/default_browser_prompt/default_browser_prompt.cc
@@ -36,36 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-void ShowPrompt() {
-  // Show the default browser request prompt in the most recently active,
-  // visible, tabbed browser. Do not show the prompt if no such browser exists.
-  ForEachCurrentBrowserWindowInterfaceOrderedByActivation(
-      [](BrowserWindowInterface* browser) {
-        // |browser| may be null in UI tests. Also, don't show the prompt in an
-        // app window, which is not meant to be treated as a Chrome window. Only
-        // show in a normal, tabbed browser.
-        if (browser &&
-            browser->GetType() != BrowserWindowInterface::TYPE_NORMAL) {
-          return true;  // continue iterating
-        }
-
-        // In ChromeBot tests, there might be a race. This line appears to get
-        // called during shutdown and the active web contents can be nullptr.
-        content::WebContents* web_contents =
-            browser->GetTabStripModel()->GetActiveWebContents();
-        if (!web_contents ||
-            web_contents->GetVisibility() != content::Visibility::VISIBLE) {
-          return true;  // continue iterating
-        }
-
-        DefaultBrowserInfoBarDelegate::Create(
-            infobars::ContentInfoBarManager::FromWebContents(web_contents),
-            browser->GetProfile(),
-            /*can_pin_to_taskbar=*/false);
-        return false;  // stop iterating
-      });
-}
-
 // Do not show the prompt if "suppress_default_browser_prompt_for_version" in
 // the initial preferences is set to the current version.
 bool ShouldShowDefaultBrowserPromptForCurrentVersion() {
@@ -166,6 +136,3 @@ void ShowDefaultBrowserPrompt(Profile* profile,
                                            profile, std::move(done_callback)));
 }
 
-void ShowPromptForTesting() {
-  ShowPrompt();
-}
