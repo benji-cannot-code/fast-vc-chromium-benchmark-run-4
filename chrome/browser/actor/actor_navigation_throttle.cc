8488c7853ac5a0c5d74aa3a7f588bdd352029cfd
Joshua Thomas <masnoble@chromium.org>
2025-10-15 21:53:10 -0700

    block navigation to dangerous content types
    Bug: 441817524
    Change-Id: I1ee7eb3cd66f6e0d2193fea291938ae7889820d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7021500
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Joshua Thomas <masnoble@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1530635}

--- chrome/browser/actor/actor_navigation_throttle.cc
+++ chrome/browser/actor/actor_navigation_throttle.cc
@@ -7,7 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
+#include "base/containers/fixed_flat_set.h"
 #include "base/memory/ptr_util.h"
+#include "base/strings/stringprintf.h"
+#include "chrome/browser/actor/actor_features.h"
 #include "chrome/browser/actor/actor_keyed_service.h"
 #include "chrome/browser/actor/actor_task.h"
 #include "chrome/browser/actor/execution_engine.h"
@@ -19,8 +22,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/navigation_throttle_registry.h"
 #include "content/public/browser/web_contents.h"
+#include "net/http/http_response_headers.h"
 
 namespace actor {
+namespace {
+constexpr auto kBlockedMimeTypes = base::MakeFixedFlatSet<std::string_view>({
+    "application/javascript",
+    "application/json",
+    "application/xml",
+    "text/javascript",
+    "text/csv",
+    "text/json",
+    "text/xml",
+});
+}  // namespace
 
 // static
 void ActorNavigationThrottle::MaybeCreateAndAdd(
@@ -82,6 +97,32 @@ ActorNavigationThrottle::WillRedirectRequest() {
 
 content::NavigationThrottle::ThrottleCheckResult
 ActorNavigationThrottle::WillProcessResponse() {
+  if (base::FeatureList::IsEnabled(
+          kGlicBlockNavigationToDangerousContentTypes)) {
+    const net::HttpResponseHeaders* headers =
+        navigation_handle()->GetResponseHeaders();
+    std::string mime_type;
+    if (headers->GetMimeType(&mime_type) &&
+        kBlockedMimeTypes.contains(mime_type)) {
+      GetJournal().Log(
+          navigation_handle()->GetURL(), task_id_, mojom::JournalTrack::kActor,
+          "NavThrottle",
+          JournalDetailsBuilder()
+              .AddError(base::StringPrintf(
+                  "Navigate to disallowed content-type: %s", mime_type.c_str()))
+              .Build());
+
+      // If the navigation we're about to cancel is attributable to the actor's
+      // tool usage, consider the action a failure.
+      if (navigation_handle()->IsInPrimaryMainFrame() && execution_engine_) {
+        execution_engine_->FailCurrentTool(
+            mojom::ActionResultCode::kTriggeredNavigationBlocked);
+      }
+
+      return content::NavigationThrottle::CANCEL_AND_IGNORE;
+    }
+  }
+
   if (!execution_engine_ ||
       !execution_engine_->ShouldGateNavigation(
           *navigation_handle(),
