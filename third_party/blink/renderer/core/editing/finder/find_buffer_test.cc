9c9143fc698bfcf7c35d89c3e84cdbe0dbc071aa
Kent Tamura <tkent@chromium.org>
2025-10-16 10:27:05 -0700

    FindInPage: Allow multi-paragraph matching in textarea
    We used to support matching a query to multiple paragraphs only in
    <textarea>, but it stopped working since a change of the
    <textarea>-internal LayoutObject structure.
    
    This CL re-adds the behavior, by add support of searching multiple IFCs
    only in <textarea>.
    
    * GetFirstBlockLevelAncestorInclusive() and
      IsInSameUninterruptedBlock()
      They ignore anonymous LayoutBlockFlows in <textarea>.
    
    * AddTextToBuffer()
      It updates `offset_mapping_` every time.
    
    Fixed: 438220615
    Change-Id: Id24e1f3d2afb98e101a32a8a437406b837982599
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7048359
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1530933}

--- third_party/blink/renderer/core/editing/finder/find_buffer_test.cc
+++ third_party/blink/renderer/core/editing/finder/find_buffer_test.cc
@@ -1153,4 +1153,10 @@ TEST_F(FindBufferTest, OrphanRubyTextCrash) {
   EXPECT_EQ(1u, CaseInsensitiveMatchCount(buffer, u"textd"));
 }
 
+TEST_F(FindBufferTest, TextareaMultilines) {
+  SetBodyContent("<textarea>line1\nline2\n</textarea>");
+  FindBuffer buffer(WholeDocumentRange(), RubySupport::kEnabledIfNecessary);
+  EXPECT_EQ(1u, CaseInsensitiveMatchCount(buffer, u"ne1 li"));
+}
+
 }  // namespace blink
