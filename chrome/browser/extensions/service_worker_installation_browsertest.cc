560660266f6e707f65f17b3a4a75ea309ae26763
James Cook <jamescook@chromium.org>
2025-10-16 15:59:53 -0700

    extensions: Add ServiceWorkerInstallationBrowserTest to desktop Android
    Now that Android supports PRE_ steps in tests we can add this file of
    tests.
    
    Bug: 40200835
    Change-Id: If09d818ba8998972a2844c26dbcfb231f0b2dd02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7050331
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1531120}

--- chrome/browser/extensions/service_worker_installation_browsertest.cc
+++ chrome/browser/extensions/service_worker_installation_browsertest.cc
@@ -11,9 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_registry.h"
 #include "extensions/browser/process_manager.h"
 #include "extensions/browser/test_extension_registry_observer.h"
+#include "extensions/buildflags/buildflags.h"
 #include "extensions/common/extension.h"
 #include "extensions/test/extension_test_message_listener.h"
 #include "extensions/test/test_extension_dir.h"
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace extensions {
 
