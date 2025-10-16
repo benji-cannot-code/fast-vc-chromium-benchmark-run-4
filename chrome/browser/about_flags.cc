ac70f3413f2705dba218dbf7ee79a54b47fb5883
Thomas Nguyen <tungnh@chromium.org>
2025-10-16 03:25:11 -0700

    [PEPC] Enable geolocation element android flag
    Bug: 435366610
    Change-Id: I532010c6b46b62d23333a660f40dec64fa8a4d4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7046879
    Commit-Queue: Thomas Nguyen <tungnh@chromium.org>
    Reviewed-by: Elias Klim <elklm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1530732}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -9058,7 +9058,7 @@ const FeatureEntry kFeatureEntries[] = {
     {"geolocation-element",
      flag_descriptions::kGeolocationPermissionControlName,
      flag_descriptions::kGeolocationPermissionControlDescription,
-     kOsMac | kOsWin | kOsLinux,
+     kOsMac | kOsWin | kOsLinux | kOsAndroid,
      FEATURE_VALUE_TYPE(blink::features::kGeolocationElement)},
 
     {"permission-promise-lifetime-modulation",
