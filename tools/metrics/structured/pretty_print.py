6089980e32b15b94bef175800babd983e4dc4040
Alicja Opalinska <aopalinska@google.com>
2025-10-16 07:49:06 -0700

    Moved args parsing for pretty print to the common file.
    Simplified the pretty_print scripts for xml files and move the system
    argument parsing logic to the common script and the DoPresubmitMain()
    function.
    
    Follow up to this CL:
    chromium-review.googlesource.com/c/chromium/src/+/6764670
    
    Bug: 402130955
    Change-Id: I33214ece57c098afaa1446214b5932eb844596f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7044756
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Commit-Queue: Alicja Opalinska <aopalinska@google.com>
    Reviewed-by: Ramon Cano Aparicio <rcanoaparicio@google.com>
    Cr-Commit-Position: refs/heads/main@{#1530846}

--- tools/metrics/structured/pretty_print.py
+++ tools/metrics/structured/pretty_print.py
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import argparse
 import os
 import sys
 
@@ -15,35 +14,16 @@ import presubmit_util
 
 
 def main():
-  """Pretty-prints the structured metrics in structured.xml file.
-
-  Args:
-    --non-interactive: (Optional) Does not print log info messages and does not
-        prompt user to accept the diff.
-    --presubmit: (Optional) Simply prints a message if the input is not
-        formatted correctly instead of modifying the file.
-    --diff: (Optional) Prints diff to stdout rather than modifying the file.
-    --cleanup: (Optional) Removes any backup file created during the execution.
-
-  Example usage:
-    pretty_print.py --diff --cleanup
-  """
-  parser = argparse.ArgumentParser()
-  # The following optional flags are used by common/presubmit_util.py
-  parser.add_argument('--non-interactive', action="store_true")
-  parser.add_argument('--presubmit', action="store_true")
-  parser.add_argument('--diff', action="store_true")
-  parser.add_argument('--cleanup',
-                      action="store_true",
-                      help="Remove the backup file after a successful run.")
-
+  """Pretty-prints the structured metrics in structured.xml file."""
   dirname = os.path.dirname(os.path.realpath(__file__))
   xml = dirname + '/sync/structured.xml'
   old_xml = dirname + '/sync/structured.old.xml'
 
-  presubmit_util.DoPresubmitMain(sys.argv, xml, old_xml,
-                                 lambda x: repr(model.Model(x, 'chrome')))
+  presubmit_util.DoPresubmitMain(xml,
+                                 old_xml,
+                                 lambda x: repr(model.Model(x, 'chrome')),
+                                 description=main.__doc__)
 
 
 if __name__ == '__main__':
-  sys.exit(main())
+  main()
