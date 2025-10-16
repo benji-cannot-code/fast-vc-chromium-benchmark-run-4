9df9977803fabfe9fd7843cd0707ef159fd1c368
James Cook <jamescook@chromium.org>
2025-10-16 15:47:41 -0700

    extensions: Add PermissionsManagerBrowserTest to desktop Android
    Now that PRE_ steps are fixed on Android, these tests can be added to
    the build.
    
    Bug: 40200835
    Change-Id: Id4d63014bfe44acd2f3d097afd9a0f9d046f8ec4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7049907
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1531114}

--- chrome/browser/extensions/permissions/permissions_manager_browsertest.cc
+++ chrome/browser/extensions/permissions/permissions_manager_browsertest.cc
@@ -3,12 +3,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "extensions/browser/permissions_manager.h"
+
 #include "chrome/browser/extensions/extension_browsertest.h"
 #include "chrome/browser/profiles/profile.h"
 #include "content/public/test/browser_test.h"
-#include "extensions/browser/permissions_manager.h"
+#include "extensions/buildflags/buildflags.h"
 #include "extensions/common/extension_features.h"
 #include "testing/gtest/include/gtest/gtest.h"
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace extensions {
 
