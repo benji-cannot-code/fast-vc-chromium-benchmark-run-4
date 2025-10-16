855aca0e93c6278eb67fa2567a931b87a12b52ba
Justin <justincohen@google.com>
2025-10-16 11:22:57 -0700

    ios: Remove workaround for EarlGrey hang in link_to_text tests
    Removes the ScopedSynchronizationDisabler and related cleanup taps from
    link_to_text_egtest.
    
    This was added to work around an EarlGrey synchronization issue where
    long-pressing web content could cause the test to hang on tearDown. This
    underlying issue is now resolved, making the workaround unnecessary.
    
    Bug: 40191349
    Change-Id: Idb072f3af1a8d60f4298c27990a2697cae016441
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7046759
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1530973}

--- ios/chrome/browser/link_to_text/ui_bundled/link_to_text_egtest.mm
+++ ios/chrome/browser/link_to_text/ui_bundled/link_to_text_egtest.mm
@@ -202,10 +202,6 @@ std::unique_ptr<net::test_server::HttpResponse> LoadHtml(
 
   GREYAssertEqual(linkToTextMatcher, nil, @"Link to text button visible");
 
-  // TODO(crbug.com/40191349): Tap to dismiss the system selection callout
-  // buttons so tearDown doesn't hang when `disabler` goes out of scope.
-  [[EarlGrey selectElementWithMatcher:chrome_test_util::WebViewMatcher()]
-      performAction:grey_tap()];
 }
 
 // TODO(crbug.com/386205292): Test is flaky.
@@ -230,11 +226,6 @@ std::unique_ptr<net::test_server::HttpResponse> LoadHtml(
                             selectorWithElementID:kSimpleTextElementId],
                         true)];
 
-  // TODO(crbug.com/40191349): Xcode 13 gesture recognizers seem to get stuck
-  // when the user longs presses on plain text.  For this test, disable EG
-  // synchronization.
-  ScopedSynchronizationDisabler disabler;
-
   // Ensure the menu is visible by finding the Paste button.
   // TODO(crbug.com/328271981): either remove call to selectElementWithMatcher
   // or do something with its return value
@@ -246,11 +237,6 @@ std::unique_ptr<net::test_server::HttpResponse> LoadHtml(
       l10n_util::GetNSString(IDS_IOS_SHARE_LINK_TO_TEXT));
 
   GREYAssertEqual(linkToTextMatcher, nil, @"Link to text button visible");
-
-  // TODO(crbug.com/40191349): Tap to dismiss the system selection callout
-  // buttons so tearDown doesn't hang when `disabler` goes out of scope.
-  [[EarlGrey selectElementWithMatcher:chrome_test_util::WebViewMatcher()]
-      performAction:grey_tap()];
 }
 
 @end
