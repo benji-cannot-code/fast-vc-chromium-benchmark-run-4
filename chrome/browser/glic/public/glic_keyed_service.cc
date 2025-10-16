d75526940194a86f8d642fd37037612623519f23
Basia Zimirska <basiaz@google.com>
2025-10-16 07:56:41 -0700

    [GlicMI] Extract calculating active browser code.
    Changing 'if (!new_attached_browser && !is_detached)'  to
    'if (!new_attached_browser)' is a no-op. We only use new_attached_browser when we want to Show glic, so even if it's calculated for FLoaty, it won't be used (if there's a Floaty we'd call Close() instead)
    
    Bug: 450657918
    Change-Id: I66dd059991f4e0521a340f21343c28d5d0901c6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7044066
    Commit-Queue: Dan Harrington <harringtond@chromium.org>
    Auto-Submit: Basia Zimirska <basiaz@google.com>
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1530852}

--- chrome/browser/glic/public/glic_keyed_service.cc
+++ chrome/browser/glic/public/glic_keyed_service.cc
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/glic/public/glic_enabling.h"
 #include "chrome/browser/glic/public/glic_keyed_service_factory.h"
 #include "chrome/browser/glic/service/glic_instance_coordinator_impl.h"
+#include "chrome/browser/glic/widget/browser_conditions.h"
 #include "chrome/browser/glic/widget/glic_widget.h"
 #include "chrome/browser/glic/widget/glic_window_controller_impl.h"
 #include "chrome/browser/global_features.h"
@@ -258,7 +259,8 @@ void GlicKeyedService::ToggleUI(BrowserWindowInterface* bwi,
     return;
   }
 
-  window_controller().Toggle(bwi, prevent_close, source);
+  window_controller().Toggle(bwi ? bwi : GetActiveGlicEligibleBrowser(profile_),
+                             prevent_close, source);
 }
 
 void GlicKeyedService::OpenFreDialogInNewTab(BrowserWindowInterface* bwi,
