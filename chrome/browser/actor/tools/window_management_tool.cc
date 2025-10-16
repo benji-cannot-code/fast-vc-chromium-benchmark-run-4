4685f8344e34b3bf75f00d7f0feb8f54791701d5
Ekaterina Kulikova <kate.kulikova@lge.com>
2025-10-16 07:13:55 -0700

    IWYU: Add missing include of observation_delay_controller.h in window_management_tool.cc
    In the CL https://crrev.com/c/6950902 added
    WindowManagementTool::GetObservationDelayer() but its return type of
    std::unique_ptr<ObservationDelayController> requires the include in GCC
    compiler.
    Added required include and deps to fix the issue.
    
    Bug: 41455655
    Change-Id: I8cb4bdd9176854ac39e579bbe0771fafce4c2243
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7046319
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Kevin Graney <kmg@google.com>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1530825}

--- chrome/browser/actor/tools/window_management_tool.cc
+++ chrome/browser/actor/tools/window_management_tool.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/bind.h"
 #include "chrome/browser/actor/actor_task.h"
+#include "chrome/browser/actor/tools/observation_delay_controller.h"
 #include "chrome/browser/actor/tools/tool_callbacks.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_list.h"
