165d38d37864178c8c25ae8f6ec9adea50cc630f
Eric Ekey <ericekey@google.com>
2025-10-16 07:39:33 -0700

    [iOS] Stub Coordinator for Synced Set Up
    This change stubs the coordinator for the new Synced Set Up experience
    (DD: go/synced-set-up-dd).
    
    Fixed: 452400687
    Change-Id: Ie9bb59dc36a216cc47e30c73e5f9b6bc1a5993c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7042258
    Reviewed-by: Benjamin Williams <bwwilliams@google.com>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Auto-Submit: Eric Ekey <ericekey@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1530838}

---
+++ ios/chrome/browser/synced_set_up/coordinator/synced_set_up_coordinator.mm
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/synced_set_up/coordinator/synced_set_up_coordinator.h"
+
+@implementation SyncedSetUpCoordinator
+
+@end
