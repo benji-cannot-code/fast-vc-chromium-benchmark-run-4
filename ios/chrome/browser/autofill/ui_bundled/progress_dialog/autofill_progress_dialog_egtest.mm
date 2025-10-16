b8e67257b364747470f004b7159bac2c9c670df3
yiwenqian <yiwenqian@google.com>
2025-10-16 14:42:39 -0700

    [Hagrid iOS] Clean up flag kAutofillEnableFpanRiskBasedAuthentication for Bling only.
    Hagrid is fully launched on Bling. The overall flag will be cleaned up
    after Hagrid on ChromeOs luanched.
    
    Launch cal: https://launch.corp.google.com/launch/4398739
    
    Bug: 452443697
    
    Change-Id: I4ca860313ccd3903483804d064df6dc2825a9ea8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7046255
    Commit-Queue: Yiwen Qian <yiwenqian@google.com>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1531087}

--- ios/chrome/browser/autofill/ui_bundled/progress_dialog/autofill_progress_dialog_egtest.mm
+++ ios/chrome/browser/autofill/ui_bundled/progress_dialog/autofill_progress_dialog_egtest.mm
@@ -32,15 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation AutofillProgressDialogDismissEGTest
 
-- (AppLaunchConfiguration)appConfigurationForTestCase {
-  AppLaunchConfiguration config;
-
-  config.features_enabled.push_back(
-      autofill::features::kAutofillEnableFpanRiskBasedAuthentication);
-
-  return config;
-}
-
 - (void)setUp {
   [super setUp];
   [AutofillAppInterface setUpFakeCreditCardServer];
