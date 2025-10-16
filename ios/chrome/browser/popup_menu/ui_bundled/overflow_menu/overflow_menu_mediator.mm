919fb62028f75f733813a06feb7933ced2bc977b
Gayane Petrosyan <gayane@google.com>
2025-10-16 11:57:04 -0700

    [iOS] Remove blue dot flag
    Bug: 394151940
    Change-Id: I39074919407fca69abe0a7d5c7e0e55983c81927
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7042342
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Commit-Queue: Gayane Petrosyan <gayane@google.com>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1530993}

--- ios/chrome/browser/popup_menu/ui_bundled/overflow_menu/overflow_menu_mediator.mm
+++ ios/chrome/browser/popup_menu/ui_bundled/overflow_menu/overflow_menu_mediator.mm
@@ -2676,12 +2676,6 @@ OverflowMenuFooter* CreateOverflowMenuManagedFooter(
 // Dismisses the menu and opens settings.
 - (void)openSettings {
   if (self.engagementTracker) {
-    if (!IsBlueDotOnToolsMenuButtoneEnabled() &&
-        self.settingsDestination.badge == BadgeTypePromo) {
-      self.engagementTracker->NotifyEvent(
-          feature_engagement::events::kBlueDotPromoOverflowMenuDismissed);
-      [self.popupMenuHandler updateToolsMenuBlueDotVisibility];
-    }
     self.engagementTracker->NotifyEvent(
         feature_engagement::events::kSettingsOnOverflowMenuUsed);
   }
