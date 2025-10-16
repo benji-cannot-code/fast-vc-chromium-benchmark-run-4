7f916c06e59c40ff74359476fde669f5ed951daa
Sunny Sachanandani <sunnyps@chromium.org>
2025-10-16 11:29:57 -0700

    [graphite] Add plumbing for IOSurface backpressure fences
    This adds a gfx::MTLSharedEventFence type that wraps a MTLSharedEvent
    and its associated value for waiting. This will be plumbed from the
    OverlayRepresentation to the ImageTransportSurfaceOverlayMac instead of
    via the GLContext as is done today. This CL only introduces the plumbing
    and doesn't have any functional change.
    
    Bug: 391990607
    Change-Id: I23c52bb001cb2198da9098fa31a387d26a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7045912
    Auto-Submit: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1530979}

--- gpu/command_buffer/service/shared_image/iosurface_image_backing.mm
+++ gpu/command_buffer/service/shared_image/iosurface_image_backing.mm
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/gpu/graphite/Recorder.h"
 #include "third_party/skia/include/gpu/graphite/Surface.h"
 #include "third_party/skia/include/private/chromium/GrPromiseImageTexture.h"
+#include "ui/gfx/mac/mtl_shared_event_fence.h"
 #include "ui/gl/egl_surface_io_surface.h"
 #include "ui/gl/gl_context.h"
 #include "ui/gl/gl_display.h"
@@ -866,6 +867,8 @@ class IOSurfaceImageBacking::OverlayRepresentation final
   bool BeginReadAccess(gfx::GpuFenceHandle& acquire_fence) override;
   void EndReadAccess(gfx::GpuFenceHandle release_fence) override;
   gfx::ScopedIOSurface GetIOSurface() const override;
+  std::vector<gfx::MTLSharedEventFence> GetBackpressureFences()
+      const override;
   bool IsInUseByWindowServer() const override;
 
   gfx::ScopedIOSurface io_surface_;
@@ -912,6 +915,19 @@ void IOSurfaceImageBacking::OverlayRepresentation::EndReadAccess(
 gfx::ScopedIOSurface
 IOSurfaceImageBacking::OverlayRepresentation::GetIOSurface() const {
   return io_surface_;
+}
+
+std::vector<gfx::MTLSharedEventFence>
+IOSurfaceImageBacking::OverlayRepresentation::GetBackpressureFences() const {
+  auto* iosurface_backing = static_cast<IOSurfaceImageBacking*>(backing());
+  AutoLock auto_lock(iosurface_backing);
+
+  std::vector<gfx::MTLSharedEventFence> backpressure_fences;
+  for (const auto& [shared_event, signaled_value] :
+       iosurface_backing->exclusive_shared_events_) {
+    backpressure_fences.emplace_back(shared_event.get(), signaled_value);
+  }
+  return backpressure_fences;
 }
 
 bool IOSurfaceImageBacking::OverlayRepresentation::IsInUseByWindowServer()
