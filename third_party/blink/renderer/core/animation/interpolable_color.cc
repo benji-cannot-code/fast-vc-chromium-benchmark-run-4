218eb09b7be2a2970956e577b0755e219c92d830
Kevin Ellis <kevers@google.com>
2025-10-17 10:40:22 -0700

    [waapi] Fix handling of colorspace mismatch
    Previously the "from", "to" and "result" interpolated values all
    had to agree on the colorspace. Though the "from" and "to" must be
    in alignment, the resultant colorspace can simply be set based on
    the "from" keyframe's colorspace. This resultant colorspace can later
    be converted to another colorspace as needed for further animations.
    
    Bug: 433202354
    Change-Id: Id21d1ac3faa1bd6a1201cec16746c79ba573b0e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7055382
    Reviewed-by: David Awogbemila <awogbemila@chromium.org>
    Commit-Queue: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1531541}

--- third_party/blink/renderer/core/animation/interpolable_color.cc
+++ third_party/blink/renderer/core/animation/interpolable_color.cc
@@ -318,7 +318,6 @@ void InterpolableColor::Interpolate(const InterpolableValue& to,
   InterpolableColor& result_color = To<InterpolableColor>(result);
 
   DCHECK_EQ(to_color.color_space_, color_space_);
-  DCHECK_EQ(result_color.color_space_, color_space_);
 
   result_color.param0_ =
       InterpolableNumber::Interpolate(param0_, to_color.param0_, progress);
@@ -337,6 +336,7 @@ void InterpolableColor::Interpolate(const InterpolableValue& to,
       webkit_link_, to_color.webkit_link_, progress);
   result_color.quirk_inherit_ = InterpolableNumber::Interpolate(
       quirk_inherit_, to_color.quirk_inherit_, progress);
+  result_color.color_space_ = color_space_;
 }
 
 void InterpolableColor::Composite(const BaseInterpolableColor& value,
