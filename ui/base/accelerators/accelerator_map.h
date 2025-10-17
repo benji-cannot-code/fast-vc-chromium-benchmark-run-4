998ed1e11be12449d43fc07874afaea99668b87e
Ekaterina Kulikova <kate.kulikova@lge.com>
2025-10-17 05:19:24 -0700

    GCC: Remove extra std:: namespace qualifier in accelerator_map.h
    In the CL https://crrev.com/c/6981628 added a template explicit
    specialization for struct std::hash<ui::Accelerator> within
    std namespace.
    To avoid GCC build error std:: namespace qualifier was removed.
    
    Bug: 40565911
    Change-Id: I272a1fe9e8a288104bde11155f0febba3e43e9f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7053048
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1531358}

--- ui/base/accelerators/accelerator_map.h
+++ ui/base/accelerators/accelerator_map.h
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace std {
 template <>
-struct std::hash<ui::Accelerator> {
+struct hash<ui::Accelerator> {
   size_t operator()(const ui::Accelerator& a) const { return a.hash(); }
 };
 
