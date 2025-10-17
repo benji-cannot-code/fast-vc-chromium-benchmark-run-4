7d75f84ad07eaa2156bf04b95dbfb4c62b9a0cd9
manukh <manukh@chromium.org>
2025-10-16 22:32:12 -0700

    [omnibox][webui] Don't cache webUI popup match favicons
    URL Autocomplete matches like facebook.com show the site favicon in the
    left. This is implemented via a:
    
    `<div style="background-image: url(chrome://favicon2/...)"></div>`
    
    Browsers automatically cache some network requests responses. Including
    the `url()`s in CSS style.
    
    This is problematic for the webUI popup, whose lifetime is connected
    with the browser window. (Unlike e.g. chrome://history whose life time
    is reset when the tab is refreshed or re-opened). So when users type a
    website they've never visited before, e.g. 'google.com' , they see the
    generic globe icon (expected). But after navigating, they expect future
    'google.com' matches to show the correct favicon. But they don't,
    because the `chrome://favicon2...` URL is unchanged and the old cached
    response is returned.
    
    This is especially noticeable since the tab-strip and views omnibox
    input row will show the new favicon.
    https://screenshot.googleplex.com/6MrvZEDp4hZyQwe
    
    This occurs very frequently; basically every time the user visits a new
    site through the omnibox, then sees the site suggested again in the
    omnibox.
    
    This CL bypasses the CSS caching by adding a `Date.now()` timestamp to
    the favicon URL as a search param. This shouldn't affect the favicon
    returned, since chrome://favicon2... URLs are parsed and handled
    internally; the google.com actual favicon URL is parsed as a separate
    search param unpolluted by the timestamp.
    
    There are potentially other discrepencies between the webUI popup
    favicons and views popup favicons. But the webUI probably does a better
    job overall except in this particular case. See b:449797165 for details.
    
    Bug: b:449797165
    Change-Id: Icb5a6caf47e5fec46b54952a78e3efae3e87e4ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7048317
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1531227}

--- ui/webui/resources/cr_components/searchbox/searchbox_dropdown.ts
+++ ui/webui/resources/cr_components/searchbox/searchbox_dropdown.ts
@@ -289,7 +289,7 @@ export class SearchboxDropdownElement extends CrLitElement {
    */
   protected groupIdsForSideType_(side: SideType): number[] {
     return [...new Set<number>(
-        this.result?.matches?.map(match => match.suggestionGroupId)
+        this.result?.matches.map(match => match.suggestionGroupId)
             .filter(groupId => this.sideTypeForGroup_(groupId) === side))];
   }
 
@@ -314,7 +314,7 @@ export class SearchboxDropdownElement extends CrLitElement {
    *     so it knows its position in the list of matches.
    */
   protected matchIndex_(match: AutocompleteMatch): number {
-    return this.result?.matches?.indexOf(match) ?? -1;
+    return this.result?.matches.indexOf(match) ?? -1;
   }
 
   /**
