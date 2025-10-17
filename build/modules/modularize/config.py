010ff4edb304541ec4d3fca434ccea957818d4cf
Matt Stark <msta@google.com>
2025-10-16 18:12:01 -0700

    Fix minor bugs for modularize that prevented it from working on some platforms.
    Bug: 435303825
    Change-Id: I6a6a6964868d5ac11dcfffc38485b576d319d25f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7048060
    Auto-Submit: Matt Stark <msta@google.com>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1531164}

--- build/modules/modularize/config.py
+++ build/modules/modularize/config.py
@@ -56,7 +56,7 @@ def fix_graph(graph: dict[str, Header],
 
   def force_textual(key: str):
     if key in graph:
-      graph[key].textual = value
+      graph[key].textual = True
 
   def add_dep(frm, to, check=True):
     if check:
