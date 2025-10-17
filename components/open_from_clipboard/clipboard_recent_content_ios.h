6dba07236c7a855b096d81b68e9951aafb776974
Sylvain Defresne <sdefresne@chromium.org>
2025-10-17 02:26:16 -0700

    [ios] Fix unsafe buffer access in ClipboardRecentContentIOS
    Change ClipboardRecentContentIOS implementation to remove an unsafe
    buffer access (by converting a for-loop from index to range).
    
    Also change the constructor to accept the `application_scheme` as an
    std::string_view (which will allow to initialise it with a constant
    declared as `inline constexpr std::string_view kSomeScheme = "...";`).
    
    Fix the name of the getAuthorizedSchemeList(...) function to follow
    the naming convention.
    
    Bug: 40285824
    Change-Id: Ib42098653af8a08f5a08b05dd410cccffc46394a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7046582
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1531308}

--- components/open_from_clipboard/clipboard_recent_content_ios.h
+++ components/open_from_clipboard/clipboard_recent_content_ios.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_OPEN_FROM_CLIPBOARD_CLIPBOARD_RECENT_CONTENT_IOS_H_
 
 #include <string>
+#include <string_view>
 
 #include "base/time/time.h"
 #include "components/open_from_clipboard/clipboard_recent_content.h"
@@ -30,7 +31,7 @@ class ClipboardRecentContentIOS : public ClipboardRecentContent {
   // |group_user_defaults| is the NSUserDefaults used to store information on
   // pasteboard entry expiration. This information will be shared with other
   // application in the application group.
-  ClipboardRecentContentIOS(const std::string& application_scheme,
+  ClipboardRecentContentIOS(std::string_view application_scheme,
                             NSUserDefaults* group_user_defaults,
                             bool only_use_clipboard_async);
 
