6e0dd87aa88e4a48b1884d0ad120fd0225afd49c
Dhruv Kathpalia <dhruvkathpalia@google.com>
2025-10-17 13:25:40 -0700

    [composebox] Adjust styling for file thumbnails in File Carousel.
    Updates icon sizes, adds hover effects to file chips and remove buttons
    and modifies the background and fill color of the remove button.
    Screenshot:
    https://screenshot.googleplex.com/7kexnRNRq75AchN.png
    https://screenshot.googleplex.com/K8bYwVo8ouwRsdJ.png
    
    Bug: b:452481330
    Change-Id: If1816676b17646083532c250af40fd14d81cfc17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7050653
    Commit-Queue: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1531622}

--- ui/webui/resources/cr_components/composebox/file_thumbnail.html.ts
+++ ui/webui/resources/cr_components/composebox/file_thumbnail.html.ts
@@ -29,6 +29,7 @@ export function getHtml(this: ComposeboxFileThumbnailElement) {
         </div>
       </div>
       <p class="title">${this.file.name}</p>
+      <div class="chip-overlay"></div>
     </div>
   ` : this.file.objectUrl ? html`
     <div id="imgChip">
@@ -72,6 +73,7 @@ export function getHtml(this: ComposeboxFileThumbnailElement) {
         </div>
       </div>
       <p class="title" id="pdfTitle">${this.file.name}</p>
+      <div class="chip-overlay"></div>
     </div>
   `}
 </div>
