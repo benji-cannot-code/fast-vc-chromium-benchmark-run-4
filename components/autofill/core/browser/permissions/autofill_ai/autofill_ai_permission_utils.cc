ea0ff58d739ce456edd5db839d263ba75bb24916
Tobiasz Ciepliński <tobson@google.com>
2025-10-17 03:04:26 -0700

    Rename add entity action to include storage type.
    Bug: 452437465
    Change-Id: Id6173604da1000fe9288162b2ef0fe7a806898f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7046466
    Commit-Queue: Tobiasz Ciepliński <tobson@google.com>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1531324}

--- components/autofill/core/browser/permissions/autofill_ai/autofill_ai_permission_utils.cc
+++ components/autofill/core/browser/permissions/autofill_ai/autofill_ai_permission_utils.cc
@@ -102,7 +102,7 @@ void MaybeOutputReason(std::string* out, std::string_view message) {
 // the AutofillAI is disabled.
 [[nodiscard]] bool IsRelevantForDataTransparency(AutofillAiAction action) {
   switch (action) {
-    case AutofillAiAction::kAddEntityInstanceInSettings:
+    case AutofillAiAction::kAddLocalEntityInstanceInSettings:
     case AutofillAiAction::kCrowdsourcingVote:
     case AutofillAiAction::kFilling:
     case AutofillAiAction::kImport:
@@ -142,7 +142,7 @@ void MaybeOutputReason(std::string* out, std::string_view message) {
              features::kAutofillAiServerModelUseCacheResults.Get();
     case AutofillAiAction::kImportToWallet:
       return is_enabled(features::kAutofillAiWalletVehicleRegistration);
-    case AutofillAiAction::kAddEntityInstanceInSettings:
+    case AutofillAiAction::kAddLocalEntityInstanceInSettings:
     case AutofillAiAction::kCrowdsourcingVote:
     case AutofillAiAction::kEditAndDeleteEntityInstanceInSettings:
     case AutofillAiAction::kFilling:
@@ -169,7 +169,7 @@ void MaybeOutputReason(std::string* out, std::string_view message) {
     case AutofillAiAction::kIphForOptIn:
     case AutofillAiAction::kServerClassificationModel:
     case AutofillAiAction::kUseCachedServerClassificationModelResults:
-    case AutofillAiAction::kAddEntityInstanceInSettings:
+    case AutofillAiAction::kAddLocalEntityInstanceInSettings:
     case AutofillAiAction::kCrowdsourcingVote:
     case AutofillAiAction::kEditAndDeleteEntityInstanceInSettings:
     case AutofillAiAction::kFilling:
@@ -235,7 +235,7 @@ void MaybeOutputReason(std::string* out, std::string_view message) {
                  features::kAutofillAiIdentityAndTravelPrefs);
     case AutofillAiAction::kServerClassificationModel:
     case AutofillAiAction::kUseCachedServerClassificationModelResults:
-    case AutofillAiAction::kAddEntityInstanceInSettings:
+    case AutofillAiAction::kAddLocalEntityInstanceInSettings:
     case AutofillAiAction::kCrowdsourcingVote:
     case AutofillAiAction::kEditAndDeleteEntityInstanceInSettings:
     case AutofillAiAction::kListEntityInstancesInSettings:
@@ -288,7 +288,7 @@ void MaybeOutputReason(std::string* out, std::string_view message) {
   const bool user_opted_in = GetAutofillAiOptInStatus(client);
   // Note that the policy can become disabled even after a user has opted in.
   switch (action) {
-    case AutofillAiAction::kAddEntityInstanceInSettings:
+    case AutofillAiAction::kAddLocalEntityInstanceInSettings:
     case AutofillAiAction::kCrowdsourcingVote:
     case AutofillAiAction::kEditAndDeleteEntityInstanceInSettings:
     case AutofillAiAction::kFilling:
@@ -352,7 +352,7 @@ void MaybeOutputReason(std::string* out, std::string_view message) {
       return true;
     }
     switch (action) {
-      case AutofillAiAction::kAddEntityInstanceInSettings:
+      case AutofillAiAction::kAddLocalEntityInstanceInSettings:
       case AutofillAiAction::kCrowdsourcingVote:
       case AutofillAiAction::kEditAndDeleteEntityInstanceInSettings:
       case AutofillAiAction::kFilling:
@@ -393,7 +393,7 @@ void MaybeOutputReason(std::string* out, std::string_view message) {
     std::string* debug_message) {
   // Off-the-record.
   switch (action) {
-    case AutofillAiAction::kAddEntityInstanceInSettings:
+    case AutofillAiAction::kAddLocalEntityInstanceInSettings:
     case AutofillAiAction::kCrowdsourcingVote:
     case AutofillAiAction::kEditAndDeleteEntityInstanceInSettings:
     case AutofillAiAction::kImport:
