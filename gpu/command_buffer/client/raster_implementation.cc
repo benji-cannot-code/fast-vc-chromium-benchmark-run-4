752225053b2b1c8c5fff9b40f28b62808caa34d0
Colin Blundell <blundell@chromium.org>
2025-10-17 01:12:26 -0700

    [//gpu] Remove ImageDecodeAcceleratorProxy
    This class is now an empty shell.
    
    Bug: 450466845
    Change-Id: I04de8f631f960effb3c60b46c1f2f67001d3c940
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7047047
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1531276}

--- gpu/command_buffer/client/raster_implementation.cc
+++ gpu/command_buffer/client/raster_implementation.cc
@@ -48,7 +48,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/paint/transfer_cache_serialize_helper.h"
 #include "components/miracle_parameter/common/public/miracle_parameter.h"
 #include "gpu/command_buffer/client/gpu_control.h"
-#include "gpu/command_buffer/client/image_decode_accelerator_interface.h"
 #include "gpu/command_buffer/client/query_tracker.h"
 #include "gpu/command_buffer/client/raster_cmd_helper.h"
 #include "gpu/command_buffer/client/shared_memory_limits.h"
@@ -560,8 +559,7 @@ RasterImplementation::RasterImplementation(
     RasterCmdHelper* helper,
     TransferBufferInterface* transfer_buffer,
     bool lose_context_when_out_of_memory,
-    GpuControl* gpu_control,
-    ImageDecodeAcceleratorInterface* image_decode_accelerator)
+    GpuControl* gpu_control)
     : ImplementationBase(helper, transfer_buffer, gpu_control),
       helper_(helper),
       error_bits_(0),
@@ -572,8 +570,7 @@ RasterImplementation::RasterImplementation(
       font_manager_(this, helper->command_buffer()),
       lost_(false),
       max_inlined_entry_size_(kMaxTransferCacheEntrySizeForTransferBuffer),
-      transfer_cache_(this),
-      image_decode_accelerator_(image_decode_accelerator) {
+      transfer_cache_(this) {
   DCHECK(helper);
   DCHECK(transfer_buffer);
   DCHECK(gpu_control);
