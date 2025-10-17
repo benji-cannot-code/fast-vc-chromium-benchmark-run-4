230cc601a453b5c68ddae09ba8aad38f1170557f
Stefano Duo <stefanoduo@google.com>
2025-10-17 10:04:39 -0700

    Rework the logic that decides when to run tests against HttpEngine
    We need a way to do the following:
    1. For tests that rely on Cronet internals, never run a them against
       HttpEngine, where those internals are not accessible.
    2. For tests that only rely on public APIs, run them against HttpEngine
       only if contains the code.
    
    (2) can be true in two separate scenarios:
    a. The test is running against a device with an SDK extensions high
       enough. High enough means higher than the one that contains an
       HttpEngine version that contains the required code.
    b. The test is running within Android. In this case, test code and code
       are imported alongside eachother, meaning that (2) is always true.
    
    The way these containts are implemented is:
    * Integer.MAX_VALUE is not a sentinel value for
      IgnoreFor.requiredSdkExtensionForPlatform: if set to that value, the
      test will never run against HttpEngine. This covers scenario (1).
    * If IgnoreFor.requiredSdkExtensionForPlatform is not set to this new
      sentinel value:
      * Within Android (i.e., when BuildConfig.CRONET_FOR_AOSP == true), the
        test will always run.
      * Outside of Android (i.e., when BuildConfig.Cronet_FOR_AOSP ==
        false), the test will run only if the device has an SDK extention
        higher than IgnoreFor.requireSdkExtensionForPlatform.
    
    Bug: b:416711180
    Change-Id: Ib23af6ebc48ad65760100c57d4b71fc8e575a442
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7041042
    Reviewed-by: Mohannad Farrag <aymanm@google.com>
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1531519}

--- components/cronet/android/test/javatests/src/org/chromium/net/CronetTestRuleTest.java
+++ components/cronet/android/test/javatests/src/org/chromium/net/CronetTestRuleTest.java
@@ -126,7 +126,10 @@ public class CronetTestRuleTest {
     @Test
     @SmallTest
     @IgnoreFor(
-            implementations = {CronetImplementation.FALLBACK, CronetImplementation.AOSP_PLATFORM},
+            implementations = {
+                CronetImplementation.FALLBACK,
+                CronetImplementation.AOSP_PLATFORM,
+            },
             reason = "Testing the rule")
     public void testRunOnlyNativeMustRun() {
         assertThat(mTestRule.testingJavaImpl()).isFalse();
@@ -143,7 +146,7 @@ public class CronetTestRuleTest {
     @IgnoreFor(
             implementations = {
                 CronetImplementation.STATICALLY_LINKED,
-                CronetImplementation.AOSP_PLATFORM
+                CronetImplementation.AOSP_PLATFORM,
             },
             reason = "Testing the rule")
     public void testRunOnlyJavaMustRun() {
@@ -159,7 +162,7 @@ public class CronetTestRuleTest {
     @IgnoreFor(
             implementations = {
                 CronetImplementation.STATICALLY_LINKED,
-                CronetImplementation.FALLBACK
+                CronetImplementation.FALLBACK,
             },
             reason = "Testing the rule")
     @RequiresMinAndroidApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
