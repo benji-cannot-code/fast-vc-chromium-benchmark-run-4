b830816d955cefddaa937816d3e08cc490608722
Juston Li <justonli@google.com>
2025-10-17 13:45:44 -0700

    Revert "Disable enable_validating_command_decoder for desktop android"
    This reverts commit a0ad9eccb8ea7def9c786fbcc08038d3877e1793.
    
    Reason for revert: remove desktop android build flags
    
    Bug: 426456696
    
    Original change's description:
    > Disable enable_validating_command_decoder for desktop android
    >
    > Desktop android devices exclusively use angle so should only use
    > passthrough command decoder
    >
    > Bug: 376280554
    > Bug: 388074349
    > Change-Id: I790bc5c78f767988b2d1c3f8427c6f0b43acbdf9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6150382
    > Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    > Commit-Queue: Juston Li <justonli@google.com>
    > Reviewed-by: Colin Blundell <blundell@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1406399}
    
    Change-Id: Ifc3f869601382dff306e2aaa3eab91d6e363d190
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6777890
    Commit-Queue: Juston Li <justonli@google.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1531634}

--- ui/gl/gl_features.cc
+++ ui/gl/gl_features.cc
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace features {
 namespace {
 
-#if BUILDFLAG(IS_ANDROID) && BUILDFLAG(ENABLE_VALIDATING_COMMAND_DECODER)
+#if BUILDFLAG(IS_ANDROID)
 const base::FeatureParam<std::string>
     kPassthroughCommandDecoderBlockListByBrand{
         &kDefaultPassthroughCommandDecoder, "BlockListByBrand", ""};
