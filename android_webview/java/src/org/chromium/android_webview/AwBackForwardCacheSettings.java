fd205eb91d6bb7789f841d2c02be5d1521a5f95c
Peter Pakkenberg <pbirk@chromium.org>
2025-10-17 08:46:59 -0700

    Revert "Change the API shape for BackForwardCacheSettings"
    This reverts commit 239e4cbbe1639a8832049050b05c0b338c4be11d.
    
    Reason for revert: Breaks AndroidX expectations http://b/432395269#comment8
    
    Original change's description:
    > Change the API shape for BackForwardCacheSettings
    >
    > This change alters the structure of BackForwardCacheSettings by grouping
    > the settings directly on the Settings object. This removes the need for
    > a builder and the corresponding setter method in the WebSettingsCompat
    > class.
    >
    > Bug: b:432395269
    > Change-Id: If461708b6538e70999cb614f0273b4c963514171
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7010856
    > Commit-Queue: Ashley Newson <ashleynewson@chromium.org>
    > Auto-Submit: Sayed Elabady <elabadysayed@chromium.org>
    > Commit-Queue: Sayed Elabady <elabadysayed@chromium.org>
    > Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    > Reviewed-by: Ashley Newson <ashleynewson@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1529531}
    
    NO_IFTTT=Rollback, but keeping constants we want to reuse on reland
    
    Bug: b:432395269
    Change-Id: If53687eb67129ef5cd6fe58f88c5853815a875b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7054684
    Auto-Submit: Peter Birk Pakkenberg <pbirk@chromium.org>
    Commit-Queue: Peter Birk Pakkenberg <pbirk@chromium.org>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1531452}

---
+++ android_webview/java/src/org/chromium/android_webview/AwBackForwardCacheSettings.java
@@ -0,0 +1,46 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.android_webview;
+
+import org.chromium.build.annotations.NullMarked;
+import org.jni_zero.CalledByNative;
+import org.jni_zero.JNINamespace;
+
+/** Settings for toggling back/forward cache behavior. */
+@JNINamespace("android_webview")
+@NullMarked
+public class AwBackForwardCacheSettings extends AwSupportLibIsomorphic {
+    private final int mTimeoutInSeconds;
+    private final int mMaxPagesInCache;
+
+    public AwBackForwardCacheSettings(int timeoutInSeconds, int maxPagesInCache) {
+        mTimeoutInSeconds = timeoutInSeconds;
+        mMaxPagesInCache = maxPagesInCache;
+    }
+
+    @CalledByNative
+    public int getTimeoutInSeconds() {
+        return mTimeoutInSeconds;
+    }
+
+    @CalledByNative
+    public int getMaxPagesInCache() {
+        return mMaxPagesInCache;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof AwBackForwardCacheSettings)) {
+            return false;
+        }
+        AwBackForwardCacheSettings that = (AwBackForwardCacheSettings) o;
+        return mTimeoutInSeconds == that.mTimeoutInSeconds
+                && mMaxPagesInCache == that.mMaxPagesInCache;
+    }
+}
--- android_webview/java/src/org/chromium/android_webview/AwSettings.java
+++ android_webview/java/src/org/chromium/android_webview/AwSettings.java
@@ -53,6 +53,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * Stores Android WebView specific settings that does not need to be synced to WebKit.
@@ -215,8 +216,7 @@ public class AwSettings {
     private boolean mBackForwardCacheEnabled;
     private boolean mHasCalledSetBackForwardCacheEnabledBefore;
 
-    private int mBackForwardCacheTimeoutInSeconds;
-    private int mBackForwardCacheMaxPagesInCache;
+    private @Nullable AwBackForwardCacheSettings mAwBackForwardCacheSettings;
 
     private boolean mCssHexAlphaColorEnabled;
     private boolean mScrollTopLeftInteropEnabled;
@@ -363,14 +363,9 @@ public class AwSettings {
                     AwSettings.this::updateBackForwardCacheEnabledOnUiThreadLocked);
         }
 
-        void updateBackForwardCacheSettingsTimeout() {
+        void updateBackForwardCacheSettings() {
             runOnUiThreadBlockingAndLocked(
-                    AwSettings.this::updateBackForwardCacheSettingsTimeoutOnUiThreadLocked);
-        }
-
-        void updateBackForwardCacheSettingsMaxPagesInCache() {
-            runOnUiThreadBlockingAndLocked(
-                    AwSettings.this::updateBackForwardCacheSettingsMaxPagesInCacheOnUiThreadLocked);
+                    AwSettings.this::updateBackForwardCacheSettingsOnUiThreadLocked);
         }
 
         void updateGeolocationEnabled() {
@@ -1852,45 +1847,26 @@ public class AwSettings {
         }
     }
 
-    public void setBackForwardCacheTimeoutInSeconds(int timeoutInSeconds) {
-        if (TRACE) Log.i(TAG, "setBackForwardCacheTimeoutInSeconds=" + timeoutInSeconds);
+    public void setBackForwardCacheSettings(AwBackForwardCacheSettings backForwardCacheSettings) {
+        if (TRACE) Log.i(TAG, "setBackForwardCacheSettings=" + backForwardCacheSettings);
+        assert backForwardCacheSettings != null;
         // Setting BackForwardCacheSettings implicitly enables BFCache as well.
         setBackForwardCacheEnabled(true);
         synchronized (mAwSettingsLock) {
-            if (mBackForwardCacheTimeoutInSeconds == timeoutInSeconds) {
+            if (Objects.equals(mAwBackForwardCacheSettings, backForwardCacheSettings)) {
                 return;
             }
-            mBackForwardCacheTimeoutInSeconds = timeoutInSeconds;
-            mEventHandler.updateBackForwardCacheSettingsTimeout();
-        }
-    }
-
-    public void setBackForwardCacheMaxPagesInCache(int maxPagesInCache) {
-        if (TRACE) Log.i(TAG, "setBackForwardCacheMaxPagesInCache=" + maxPagesInCache);
-        // Setting BackForwardCacheSettings implicitly enables BFCache as well.
-        setBackForwardCacheEnabled(true);
-        synchronized (mAwSettingsLock) {
-            if (mBackForwardCacheMaxPagesInCache == maxPagesInCache) {
-                return;
-            }
-            mBackForwardCacheMaxPagesInCache = maxPagesInCache;
-            mEventHandler.updateBackForwardCacheSettingsMaxPagesInCache();
+            mAwBackForwardCacheSettings = backForwardCacheSettings;
+            mEventHandler.updateBackForwardCacheSettings();
         }
     }
 
     @CalledByNative
-    public int getBackForwardCacheSettingsTimeout() {
+    @Nullable
+    public AwBackForwardCacheSettings getBackForwardCacheSettings() {
         synchronized (mAwSettingsLock) {
             assert Thread.holdsLock(mAwSettingsLock);
-            return mBackForwardCacheTimeoutInSeconds;
-        }
-    }
-
-    @CalledByNative
-    public int getBackForwardCacheSettingsMaxPagesInCache() {
-        synchronized (mAwSettingsLock) {
-            assert Thread.holdsLock(mAwSettingsLock);
-            return mBackForwardCacheMaxPagesInCache;
+            return mAwBackForwardCacheSettings;
         }
     }
 
@@ -2198,25 +2174,19 @@ public class AwSettings {
     private void updateBackForwardCacheEnabledOnUiThreadLocked() {
         assert mEventHandler.mHandler != null;
         ThreadUtils.assertOnUiThread();
-        if (mNativeAwSettings == 0) return;
-        AwSettingsJni.get().updateBackForwardCacheEnabledLocked(mNativeAwSettings, AwSettings.this);
+        if (mNativeAwSettings != 0) {
+            AwSettingsJni.get()
+                    .updateBackForwardCacheEnabledLocked(mNativeAwSettings, AwSettings.this);
+        }
     }
 
-    private void updateBackForwardCacheSettingsTimeoutOnUiThreadLocked() {
+    private void updateBackForwardCacheSettingsOnUiThreadLocked() {
         assert mEventHandler.mHandler != null;
         ThreadUtils.assertOnUiThread();
-        if (mNativeAwSettings == 0) return;
-        AwSettingsJni.get()
-                .updateBackForwardCacheSettingsTimeoutLocked(mNativeAwSettings, AwSettings.this);
-    }
-
-    private void updateBackForwardCacheSettingsMaxPagesInCacheOnUiThreadLocked() {
-        assert mEventHandler.mHandler != null;
-        ThreadUtils.assertOnUiThread();
-        if (mNativeAwSettings == 0) return;
-        AwSettingsJni.get()
-                .updateBackForwardCacheSettingsMaxPagesInCacheLocked(
-                        mNativeAwSettings, AwSettings.this);
+        if (mNativeAwSettings != 0) {
+            AwSettingsJni.get()
+                    .updateBackForwardCacheSettingsLocked(mNativeAwSettings, AwSettings.this);
+        }
     }
 
     private void updateGeolocationEnabledOnUiThreadLocked() {
@@ -2377,10 +2347,7 @@ public class AwSettings {
 
         void updateBackForwardCacheEnabledLocked(long nativeAwSettings, AwSettings caller);
 
-        void updateBackForwardCacheSettingsTimeoutLocked(long nativeAwSettings, AwSettings caller);
-
-        void updateBackForwardCacheSettingsMaxPagesInCacheLocked(
-                long nativeAwSettings, AwSettings caller);
+        void updateBackForwardCacheSettingsLocked(long nativeAwSettings, AwSettings caller);
 
         boolean isForceDarkApplied(long nativeAwSettings, AwSettings caller);
 
