1cadf5e9b82b441952d673537886deae78bcd915
Mohamed Amir Yosef <mamir@chromium.org>
2025-10-17 09:39:14 -0700

    [CredMan] Change order of abort check to match spec in create()
    This CL changes order in which
    AuthenticationCredentialsContainer::create() checks if the request has
    been aborted to match the spec
    https://www.w3.org/TR/credential-management-1/#algorithm-create
    
    Specifically options.signal should be checked prior to processing the
    relevant credential interface objects.
    
    Change-Id: Ia15ce4f266036605cad7a38182a7ac4c2104ba8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7055202
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1531477}

--- third_party/blink/renderer/modules/credentialmanagement/authentication_credentials_container.cc
+++ third_party/blink/renderer/modules/credentialmanagement/authentication_credentials_container.cc
@@ -1599,6 +1599,11 @@ AuthenticationCredentialsContainer::create(
           script_state);
   auto promise = resolver->Promise();
 
+  if (options->hasSignal() && options->signal()->aborted()) {
+    resolver->Reject(options->signal()->reason(script_state));
+    return promise;
+  }
+
   if (RuntimeEnabledFeatures::WebIdentityDigitalCredentialsCreationEnabled(
           resolver->GetExecutionContext()) &&
       IsDigitalIdentityCredentialType(*options)) {
@@ -1767,10 +1772,6 @@ AuthenticationCredentialsContainer::create(
 
   std::unique_ptr<ScopedAbortState> scoped_abort_state = nullptr;
   if (auto* signal = options->getSignalOr(nullptr)) {
-    if (signal->aborted()) {
-      resolver->Reject(signal->reason(script_state));
-      return promise;
-    }
     auto* handle = signal->AddAlgorithm(
         MakeGarbageCollected<PublicKeyRequestAbortAlgorithm>(script_state));
     scoped_abort_state = std::make_unique<ScopedAbortState>(signal, handle);
