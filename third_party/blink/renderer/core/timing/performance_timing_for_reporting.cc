eb726c7253d36b9b034e959599c7df7cd5ea6471
Scott Haseley <shaseley@chromium.org>
2025-10-17 10:43:44 -0700

    Remove obsolete LargestContentfulPaintDetailsForReporting::paint_time
    Drive-by removal of this field, found while code reading. The text and
    image times are merged later in the pipeline, and this isn't used.
    
    Bug: None
    Change-Id: I8263f512b1f9ac822c78c8ff7d927c80b0b96057
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7050707
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Auto-Submit: Scott Haseley <shaseley@chromium.org>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Johannes Henkel <johannes@chromium.org>
    Reviewed-by: Michal Mocny <mmocny@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1531543}

--- third_party/blink/renderer/core/timing/performance_timing_for_reporting.cc
+++ third_party/blink/renderer/core/timing/performance_timing_for_reporting.cc
@@ -88,7 +88,6 @@ LargestContentfulPaintDetailsForReporting PerformanceTimingForReporting::
           timing.largest_contentful_paint_image_bpp,
           largest_text_paint_time,
           timing.largest_text_paint_size,
-          timing.largest_contentful_paint_time,
 
           timing.largest_contentful_paint_image_request_priority,
           merged_unclamped_paint_time};
