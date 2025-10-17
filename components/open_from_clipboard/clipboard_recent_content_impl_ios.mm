48722faed35563bcc7543e94f83769ae80a57faf
Sylvain Defresne <sdefresne@chromium.org>
2025-10-17 01:46:46 -0700

    [ios] Use #import instead of #include in Objective-C++ files
    This is recommended by the style guide.
    
    Change-Id: I0493962b9e2ad890c2541c95ff0b5abfbb9f9443
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7046407
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1531289}

--- components/open_from_clipboard/clipboard_recent_content_impl_ios.mm
+++ components/open_from_clipboard/clipboard_recent_content_impl_ios.mm
@@ -10,10 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/apple/foundation_util.h"
 #import "base/functional/bind.h"
-#include "base/notreached.h"
-#include "base/strings/sys_string_conversions.h"
-#include "base/system/sys_info.h"
-#include "components/open_from_clipboard/clipboard_async_wrapper_ios.h"
+#import "base/notreached.h"
+#import "base/strings/sys_string_conversions.h"
+#import "base/system/sys_info.h"
+#import "components/open_from_clipboard/clipboard_async_wrapper_ios.h"
 
 ContentType const ContentTypeURL = @"ContentTypeURL";
 ContentType const ContentTypeText = @"ContentTypeString";
