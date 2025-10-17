b931274fea6e4d7fce77f573560eca87ec0178bd
Tom Sepez <tsepez@chromium.org>
2025-10-17 13:19:56 -0700

    Rename ipc_handle_unittest.cc to handle_win_unittest.cc
    Small tidy. Makes unittest file match the filename it is testing.
    
    Change-Id: I22bf6d4fb934e0902f9baa612573fdc94ecadc31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7055984
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Auto-Submit: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1531616}

--- ipc/handle_win_unittest.cc
+++ ipc/handle_win_unittest.cc
@@ -3,10 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "ipc/handle_win.h"
+
 #include <windows.h>
 
 #include "base/files/file.h"
-#include "ipc/handle_win.h"
 #include "ipc/platform_file_for_transit.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
