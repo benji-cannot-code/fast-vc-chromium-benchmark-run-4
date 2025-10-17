b64f18549b8e4ecf7942b2f467e49eb4f75dac07
Elias Homsi <eliashomsi@google.com>
2025-10-17 07:49:23 -0700

    Deprecate `GetURLFActiveContentAreaUser` function.
    Active Gaia on URLF should apply to all google sites, which is the same
    as content scan, we should deprecate GetURLFActiveContentAreaUser and
    replace it with GetActiveContentAreaUser. For context
    crbug.com/450930195
    
    Change-Id: I4f49c30a3a06b6e48b6f62066d72f6a0bcc9e395
    Bug: 425370101
    Bug: 450930195
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7042515
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Auto-Submit: Elias Homsi <eliashomsi@google.com>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Commit-Queue: Elias Homsi <eliashomsi@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1531413}

--- ios/chrome/browser/safe_browsing/model/chrome_enterprise_url_lookup_service_factory.mm
+++ ios/chrome/browser/safe_browsing/model/chrome_enterprise_url_lookup_service_factory.mm
@@ -115,7 +115,7 @@ ChromeEnterpriseRealTimeUrlLookupServiceFactory::BuildServiceInstanceFor(
       /*is_guest_session=*/false,
       base::BindRepeating(&enterprise_connectors::GetProfileEmail,
                           identity_manager),
-      base::BindRepeating(&enterprise_connectors::GetURLFActiveContentAreaUser,
+      base::BindRepeating(&enterprise_connectors::GetActiveContentAreaUser,
                           IdentityManagerFactory::GetForProfile(profile)),
       base::BindRepeating(&IsProfileAffiliated, profile),
       IsCommandLineSwitchEnabled());
