61b7b9727892c90f2ccce5f5b4cb0686dd01045e
Daniel Vogelheim <vogelheim@chromium.org>
2025-10-17 09:47:41 -0700

    [Trusted Types] Check TT for both versions of SVG script href.
    SVG's script element allows both href and xlink:href attributes. Make
    sure either will trigger the check.
    
    Fixed: 448854242
    Change-Id: I711f18aa437ff21e1e68e8d165cbd88a77da75d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7046236
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Daniel Vogelheim <vogelheim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1531491}

--- third_party/blink/renderer/core/svg/svg_animated_string.cc
+++ third_party/blink/renderer/core/svg/svg_animated_string.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/svg/svg_element.h"
 #include "third_party/blink/renderer/core/trustedtypes/trusted_script_url.h"
 #include "third_party/blink/renderer/core/trustedtypes/trusted_types_util.h"
+#include "third_party/blink/renderer/core/xlink_names.h"
 
 namespace blink {
 
@@ -33,7 +34,8 @@ void SVGAnimatedString::setBaseVal(const V8UnionStringOrTrustedScriptURL* value,
         // removed, we need to support both ways:
         if (RuntimeEnabledFeatures::TrustedTypesHTMLEnabled()) {
           // https://github.com/w3c/svgwg/pull/934
-          if (AttributeName() == svg_names::kHrefAttr) {
+          if (AttributeName() == svg_names::kHrefAttr ||
+              AttributeName() == xlink_names::kHrefAttr) {
             string = TrustedTypesCheckForScriptURL(
                 string, ContextElement()->GetExecutionContext(),
                 "SVGScriptElement", "href", exception_state);
