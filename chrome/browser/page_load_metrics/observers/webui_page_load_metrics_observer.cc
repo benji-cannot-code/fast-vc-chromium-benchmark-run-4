fc070973c4a716e39f588431b2279df653d0c9ec
Todd Reifsteck <toddreif@microsoft.com>
2025-10-17 12:22:34 -0700

    Record FCP and LCP for each WebUI variant
    Fixed: 40485276
    Change-Id: I9758de5a2354ca713d8d93926cc12defe9f63490
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7023870
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Auto-Submit: Todd Reifsteck <toddreif@microsoft.com>
    Commit-Queue: Todd Reifsteck <toddreif@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1531595}

--- chrome/browser/page_load_metrics/observers/webui_page_load_metrics_observer.cc
+++ chrome/browser/page_load_metrics/observers/webui_page_load_metrics_observer.cc
@@ -28,6 +28,9 @@ void WebUIPageLoadMetricsObserver::OnFirstContentfulPaintInPage(
   CHECK(timing.paint_timing->first_contentful_paint.has_value());
   PAGE_LOAD_HISTOGRAM(kWebUINavigationToFCPHistogramName,
                       timing.paint_timing->first_contentful_paint.value());
+  PAGE_LOAD_HISTOGRAM(base::StrCat({kWebUINavigationToFCPHistogramName, ".",
+                                    GetDelegate().GetUrl().host()}),
+                      timing.paint_timing->first_contentful_paint.value());
 }
 
 void WebUIPageLoadMetricsObserver::OnComplete(
@@ -84,5 +87,9 @@ bool WebUIPageLoadMetricsObserver::RecordSessionEndHistograms() {
 
   PAGE_LOAD_HISTOGRAM(kWebUINavigationToLCPHistogramName,
                       main_frame_largest_contentful_paint.Time().value());
+  PAGE_LOAD_HISTOGRAM(base::StrCat({kWebUINavigationToLCPHistogramName, ".",
+                                    GetDelegate().GetUrl().host()}),
+                      main_frame_largest_contentful_paint.Time().value());
+
   return true;
 }
