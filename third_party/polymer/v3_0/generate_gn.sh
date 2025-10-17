33d31eb2f3fcb0f30323a3846b3ce1f77e0eb27f
Peter KH <pkotwicz@google.com>
2025-10-17 11:08:21 -0700

    Fix "git cl format" call in polymer reproduce.sh
    The "git cl format" call was failing when reproduce.sh was being called
    from //third_party/polymer directory due to the paths not being relative
    to the git repository root.
    
    Bug: 450087665
    Change-Id: Iee75ea134e156e43296bc0535c5ef7f9a90ed3ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7047977
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1531555}

--- third_party/polymer/v3_0/generate_gn.sh
+++ third_party/polymer/v3_0/generate_gn.sh
@@ -17,7 +17,7 @@ for dir in $dirs; do
     if [ "$content" ]; then
       echo "Writing $gn_file"
       echo "$content" > "$gn_file"
-      gn_files="$gn_files $gn_file"
+      gn_files="$gn_files $(realpath $gn_file)"
     elif [ -f "$gn_file" ]; then
       echo "Removing $gn_file"
       rm "$gn_file"
