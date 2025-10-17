7a4a085c914ef93931a8a04b9af8de58b6d2ef6b
Andrew Moylan <amoylan@google.com>
2025-10-16 17:10:51 -0700

    Remove media::kCrosExpandSodaLanguages
    The feature has been enabled by default since M130.
    
    This change also removes the capability to use Finch-based feature
    params (attached to kCrosExpandSodaLanguages) to expand the set of
    supported SODA languages. These are managed directly in the C++ instead.
    
    Bug: chromium:427473920
    Change-Id: I41df882232cdfd75d3a628766334106c0d0c6465
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7048277
    Reviewed-by: Evan Liu <evliu@google.com>
    Commit-Queue: Andrew Moylan <amoylan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1531144}

--- components/soda/soda_features.cc
+++ components/soda/soda_features.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace speech {
 #if BUILDFLAG(IS_CHROMEOS)
-BASE_FEATURE(kCrosExpandSodaLanguages, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kFeatureManagementCrosSodaConchLanguages,
              base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kCrosSodaConchLanguages, base::FEATURE_ENABLED_BY_DEFAULT);
