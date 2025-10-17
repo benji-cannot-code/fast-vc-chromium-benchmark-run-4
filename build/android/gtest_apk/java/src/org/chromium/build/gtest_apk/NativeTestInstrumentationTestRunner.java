afda8f034857bd28ee2265198e5faff71fe7156f
Michael Thiessen <mthiesse@chromium.org>
2025-10-17 11:24:43 -0700

    Keep the test process alive after Activity finishing for browser tests
    Some Android 16+ devices seem to kill the process immediately upon
    finishing the Activity, not leaving us enough time to report test
    results back.
    
    Use a service to keep the process alive after Activity finishing.
    
    Also fixed a race in test shutdown leading to a hang if the
    SharedMemoryManager is deleted after the task executor is shut down.
    
    Bug: 40200835
    Change-Id: If6daee85fef6a1f5f141eb5623a182f4fb773b38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7042740
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1531563}

--- build/android/gtest_apk/java/src/org/chromium/build/gtest_apk/NativeTestInstrumentationTestRunner.java
+++ build/android/gtest_apk/java/src/org/chromium/build/gtest_apk/NativeTestInstrumentationTestRunner.java
@@ -12,9 +12,11 @@ import android.app.Instrumentation;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
+import android.os.IBinder;
 import android.os.Process;
 import android.text.TextUtils;
 import android.util.Log;
@@ -65,6 +67,16 @@ public class NativeTestInstrumentationTestRunner extends Instrumentation {
     private File mStdoutFile;
     private Bundle mTransparentArguments;
     private boolean mKeepUserDataDir;
+    private final ServiceConnection mConnection =
+            new ServiceConnection() {
+                @Override
+                public void onServiceConnected(ComponentName name, IBinder service) {}
+
+                @Override
+                public void onServiceDisconnected(ComponentName name) {
+                    getContext().unbindService(this);
+                }
+            };
 
     @Override
     public void onCreate(Bundle arguments) {
@@ -294,11 +306,28 @@ public class NativeTestInstrumentationTestRunner extends Instrumentation {
         return i;
     }
 
+    private Intent getProcessKeepaliveServiceIntent() {
+        Intent i = new Intent();
+        i.setComponent(
+                new ComponentName(
+                        getContext().getPackageName(),
+                        "org.chromium.build.gtest_apk.ProcessKeepaliveService"));
+        return i;
+    }
+
     /** Starts the NativeTest Activity. */
     private class ShardStarter implements Runnable {
         @Override
         public void run() {
             getContext().startActivity(createShardMainIntent());
+            // Start a service to keep the test process alive after the Activity finishes. Some
+            // Android 16+ devices seem to otherwise kill the process immediately after the Activity
+            // is destroyed.
+            getContext()
+                    .bindService(
+                            getProcessKeepaliveServiceIntent(),
+                            mConnection,
+                            Context.BIND_AUTO_CREATE);
         }
     }
 
@@ -313,6 +342,8 @@ public class NativeTestInstrumentationTestRunner extends Instrumentation {
 
         @Override
         public void run() {
+            getContext().unbindService(mConnection);
+
             if (mPid != Process.myPid()) {
                 Process.killProcess(mPid);
                 try {
---
+++ build/android/gtest_apk/java/src/org/chromium/build/gtest_apk/ProcessKeepaliveService.java
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.build.gtest_apk;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * Noop service that keeps the process alive long enough to submit test results after the Activities
+ * finish.
+ */
+public class ProcessKeepaliveService extends Service {
+    private final IProcessKeepaliveService.Stub mBinder = new IProcessKeepaliveService.Stub() {};
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+}
