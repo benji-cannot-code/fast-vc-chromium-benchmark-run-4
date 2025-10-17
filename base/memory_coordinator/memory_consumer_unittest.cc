adb2eab8d22464f10b8f168e05210b6f22a601be
Patrick Monette <pmonette@chromium.org>
2025-10-17 07:49:10 -0700

    Rename ScopedMemoryConsumerRegistration to MemoryConsumerRegistration
    No functional changes.
    
    Bug: 406578344
    Change-Id: Ice4c0a71af51088c099f2267d0ad809bebcf09f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7049440
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Owners-Override: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1531412}

--- base/memory_coordinator/memory_consumer_unittest.cc
+++ base/memory_coordinator/memory_consumer_unittest.cc
@@ -11,12 +11,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
-TEST(MemoryConsumerTest, ScopedMemoryConsumerRegistration) {
+TEST(MemoryConsumerTest, MemoryConsumerRegistration) {
   TestMemoryConsumerRegistry test_registry;
 
   MockMemoryConsumer consumer;
-  ScopedMemoryConsumerRegistration scoped_registration("consumer", {},
-                                                       &consumer);
+  MemoryConsumerRegistration registration("consumer", {}, &consumer);
 
   EXPECT_CALL(consumer, OnReleaseMemory());
   test_registry.NotifyReleaseMemory();
@@ -26,8 +25,7 @@ TEST(MemoryConsumerTest, UpdateMemoryLimit) {
   TestMemoryConsumerRegistry test_registry;
 
   MockMemoryConsumer consumer;
-  ScopedMemoryConsumerRegistration scoped_registration("consumer", {},
-                                                       &consumer);
+  MemoryConsumerRegistration registration("consumer", {}, &consumer);
 
   // Initial limit value of 100.
   EXPECT_EQ(consumer.memory_limit(), 100);
