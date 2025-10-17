78df62e9c496683827af82295bbd1b5bf8accecf
Tobiasz Ciepliński <tobson@google.com>
2025-10-17 11:05:31 -0700

    Add action type for adding a server stored entity.
    Bug: 452437465
    Change-Id: Ied1d4bb26cc4a9f89d9f5a53546bf159aed56acf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7048801
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Tobiasz Ciepliński <tobson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1531551}

--- components/autofill/core/browser/permissions/autofill_ai/autofill_ai_permission_utils.cc
+++ components/autofill/core/browser/permissions/autofill_ai/autofill_ai_permission_utils.cc
@@ -103,6 +103,7 @@ void MaybeOutputReason(std::string* out, std::string_view message) {
 [[nodiscard]] bool IsRelevantForDataTransparency(AutofillAiAction action) {
   switch (action) {
     case AutofillAiAction::kAddLocalEntityInstanceInSettings:
+    case AutofillAiAction::kAddServerEntityInstanceInSettings:
     case AutofillAiAction::kCrowdsourcingVote:
     case AutofillAiAction::kFilling:
     case AutofillAiAction::kImport:
@@ -133,6 +134,9 @@ void MaybeOutputReason(std::string* out, std::string_view message) {
   }
 
   switch (action) {
+    case AutofillAiAction::kAddServerEntityInstanceInSettings:
+      return is_enabled(features::kAutofillAiWalletVehicleRegistration) ||
+             is_enabled(features::kAutofillAiWalletFlightReservation);
     case AutofillAiAction::kIphForOptIn:
       return is_enabled(feature_engagement::kIPHAutofillAiOptInFeature);
     case AutofillAiAction::kServerClassificationModel:
@@ -161,11 +165,14 @@ void MaybeOutputReason(std::string* out, std::string_view message) {
     const syncer::SyncService* sync_service,
     std::string* debug_message) {
   switch (action) {
+    case AutofillAiAction::kAddServerEntityInstanceInSettings:
     case AutofillAiAction::kImportToWallet:
       return sync_service &&
              sync_service->GetUserSettings()->GetSelectedTypes().Has(
                  syncer::UserSelectableType::kPayments) &&
-             sync_service->GetActiveDataTypes().Has(syncer::AUTOFILL_VALUABLE);
+             sync_service->GetActiveDataTypes().Has(
+                 syncer::AUTOFILL_VALUABLE) &&
+             sync_service->IsSyncFeatureEnabled();
     case AutofillAiAction::kIphForOptIn:
     case AutofillAiAction::kServerClassificationModel:
     case AutofillAiAction::kUseCachedServerClassificationModelResults:
@@ -236,6 +243,7 @@ void MaybeOutputReason(std::string* out, std::string_view message) {
     case AutofillAiAction::kServerClassificationModel:
     case AutofillAiAction::kUseCachedServerClassificationModelResults:
     case AutofillAiAction::kAddLocalEntityInstanceInSettings:
+    case AutofillAiAction::kAddServerEntityInstanceInSettings:
     case AutofillAiAction::kCrowdsourcingVote:
     case AutofillAiAction::kEditAndDeleteEntityInstanceInSettings:
     case AutofillAiAction::kListEntityInstancesInSettings:
@@ -297,6 +305,7 @@ void MaybeOutputReason(std::string* out, std::string_view message) {
     case AutofillAiAction::kServerClassificationModel:
     case AutofillAiAction::kUseCachedServerClassificationModelResults:
       return policy_pref_enabled && user_opted_in;
+    case AutofillAiAction::kAddServerEntityInstanceInSettings:
     case AutofillAiAction::kImportToWallet:
       return policy_pref_enabled && user_opted_in &&
              client.IsImportingToWalletEnabled();
@@ -353,6 +362,7 @@ void MaybeOutputReason(std::string* out, std::string_view message) {
     }
     switch (action) {
       case AutofillAiAction::kAddLocalEntityInstanceInSettings:
+      case AutofillAiAction::kAddServerEntityInstanceInSettings:
       case AutofillAiAction::kCrowdsourcingVote:
       case AutofillAiAction::kEditAndDeleteEntityInstanceInSettings:
       case AutofillAiAction::kFilling:
@@ -394,6 +404,7 @@ void MaybeOutputReason(std::string* out, std::string_view message) {
   // Off-the-record.
   switch (action) {
     case AutofillAiAction::kAddLocalEntityInstanceInSettings:
+    case AutofillAiAction::kAddServerEntityInstanceInSettings:
     case AutofillAiAction::kCrowdsourcingVote:
     case AutofillAiAction::kEditAndDeleteEntityInstanceInSettings:
     case AutofillAiAction::kImport:
