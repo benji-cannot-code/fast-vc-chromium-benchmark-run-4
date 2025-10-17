4df34696d0206a2721dcb9301f72ee7873469e59
Sana Akbani <sanaakbani@google.com>
2025-10-16 18:49:36 -0700

    [GlicMI] Revise opener approach for daisy chaining through links inside glic.
    This CL allows daisy chaining from the source instance directly. When
    CreateTab is called from the glic page handler, the glic instance calls
    to the glic keyed service to create the new tab. CreateTab is now
    refactored to return a tab, and once this tab is created, we show the
    instance directly on this tab.
    
    Bug: 445984911
    Change-Id: I745e7a0fc4193c9dff2ad96dc2b2e0a3fd8a4a6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7050762
    Commit-Queue: Sana Akbani <sanaakbani@google.com>
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1531175}

--- chrome/browser/glic/host/host.h
+++ chrome/browser/glic/host/host.h
@@ -80,8 +80,7 @@ class Host : public GlicSharingManagerProvider {
   class InstanceDelegate {
    public:
     virtual ~InstanceDelegate() = default;
-    virtual void CreateTab(
-        content::RenderFrameHost* source,
+    virtual tabs::TabInterface* CreateTab(
         const ::GURL& url,
         bool open_in_background,
         const std::optional<int32_t>& window_id,
