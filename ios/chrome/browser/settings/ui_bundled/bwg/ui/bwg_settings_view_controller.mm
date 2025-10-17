f08ca5f7a78936c9b80e87a56ee68c6fa11c2201
Joemer Ramos <joemerramos@google.com>
2025-10-17 00:42:23 -0700

    [iOS] Add Managed Connected Apps in Gemini Settings
    Adds a setting row in Gemini settings for Extensions. Also added QoL
    metric changes
    
    UI Video: crbug.com/444586297#comment3
    
    Fixed: 444586297
    Change-Id: Iff4a7710d8cec61ba2db0d77107df4ff113924d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7042353
    Auto-Submit: Joemer Ramos <joemerramos@google.com>
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1531267}

--- ios/chrome/browser/settings/ui_bundled/bwg/ui/bwg_settings_view_controller.mm
+++ ios/chrome/browser/settings/ui_bundled/bwg/ui/bwg_settings_view_controller.mm
@@ -3,11 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/settings/ui_bundled/bwg//ui/bwg_settings_view_controller.h"
+#import "ios/chrome/browser/settings/ui_bundled/bwg/ui/bwg_settings_view_controller.h"
 
 #import "base/apple/foundation_util.h"
-#import "base/metrics/user_metrics.h"
-#import "base/metrics/user_metrics_action.h"
+#import "ios/chrome/browser/intelligence/bwg/metrics/bwg_metrics.h"
 #import "ios/chrome/browser/intelligence/features/features.h"
 #import "ios/chrome/browser/net/model/crurl.h"
 #import "ios/chrome/browser/settings/ui_bundled/bwg/coordinator/bwg_settings_mutator.h"
@@ -32,12 +31,14 @@ typedef NS_ENUM(NSInteger, SectionIdentifier) {
   SectionIdentifierLocation = kSectionIdentifierEnumZero,
   SectionIdentifierPageContent,
   SectionIdentifierActivity,
+  SectionIdentifierExtensions,
 };
 
 typedef NS_ENUM(NSInteger, ItemType) {
   ItemTypeLocation = kItemTypeEnumZero,
   ItemTypePageContentSharing,
   ItemTypeAppActivity,
+  ItemTypeExtensions,
   ItemTypeLocationFooter,
   ItemTypePageContentSharingFooter,
   ItemTypeAppActivityFooter,
@@ -65,8 +66,6 @@ NSString* const kPageContentSharingAction = @"PageContentSharingAction";
   TableViewMultiDetailTextItem* _preciseLocationItem;
   // Switch item for toggling page content sharing.
   TableViewSwitchItem* _pageContentSharingItem;
-  // BWG Apps activity item. Uses `accessoryView` to create a tappable icon.
-  TableViewDetailTextItem* _BWGAppsActivityItem;
   // Location view controller shown when precise location row is tapped.
   BWGLocationViewController* _locationViewController;
   // Precise location preference value.
@@ -81,6 +80,7 @@ NSString* const kPageContentSharingAction = @"PageContentSharingAction";
   [super viewDidLoad];
   self.tableView.accessibilityIdentifier = kBWGSettingsViewTableIdentifier;
   self.title = l10n_util::GetNSString(IDS_IOS_BWG_SETTINGS_TITLE);
+  RecordBWGSettingsOpened();
   [self loadModel];
 }
 
@@ -142,16 +142,20 @@ NSString* const kPageContentSharingAction = @"PageContentSharingAction";
       toSectionWithIdentifier:SectionIdentifierActivity];
   [model setFooter:BWGAppActivityFooterItem
       forSectionWithIdentifier:SectionIdentifierActivity];
+
+  [model addSectionWithIdentifier:SectionIdentifierExtensions];
+  [model addItem:[self BWGExtensionsItem]
+      toSectionWithIdentifier:SectionIdentifierExtensions];
 }
 
 #pragma mark - SettingsControllerProtocol
 
 - (void)reportDismissalUserAction {
-  base::RecordAction(base::UserMetricsAction("MobileGeminiSettingsClose"));
+  RecordBWGSettingsClose();
 }
 
 - (void)reportBackUserAction {
-  base::RecordAction(base::UserMetricsAction("MobileGeminiSettingsBack"));
+  RecordBWGSettingsBack();
 }
 
 #pragma mark - Private
@@ -214,6 +218,18 @@ NSString* const kPageContentSharingAction = @"PageContentSharingAction";
   return BWGAppActivityItem;
 }
 
+// Creates the BWG extensions item.
+- (TableViewDetailTextItem*)BWGExtensionsItem {
+  TableViewDetailTextItem* BWGExtensionsItem =
+      [[TableViewDetailTextItem alloc] initWithType:ItemTypeExtensions];
+  BWGExtensionsItem.text =
+      l10n_util::GetNSString(IDS_IOS_BWG_SETTINGS_EXTENSIONS_TITLE);
+  BWGExtensionsItem.accessorySymbol =
+      TableViewDetailTextCellAccessorySymbolExternalLink;
+  BWGExtensionsItem.accessibilityTraits = UIAccessibilityTraitLink;
+  return BWGExtensionsItem;
+}
+
 // Called from the PageContentSharing setting's UIControlEventTouchUpInside.
 // Updates underlying page content sharing pref.
 - (void)pageContentSharingSwitchTapped:(UISwitch*)switchView {
@@ -248,9 +264,14 @@ NSString* const kPageContentSharingAction = @"PageContentSharingAction";
 
   if ([self.tableViewModel itemTypeForIndexPath:indexPath] ==
       ItemTypeAppActivity) {
-    base::RecordAction(
-        base::UserMetricsAction("Settings.BWGSettings.BWGAppActivity"));
+    RecordBWGSettingsAppActivity();
     [self.mutator openNewTabWithURL:GURL(kBWGAppActivityURL)];
+  }
+
+  if ([self.tableViewModel itemTypeForIndexPath:indexPath] ==
+      ItemTypeExtensions) {
+    RecordBWGSettingsExtensions();
+    [self.mutator openNewTabWithURL:GURL(kBWGExtensionsURL)];
   }
 
   [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
--- ios/chrome/browser/shared/model/url/chrome_url_constants.cc
+++ ios/chrome/browser/shared/model/url/chrome_url_constants.cc
@@ -183,3 +183,5 @@ const char kBWGPreciseLocationURL[] =
 
 const char kBWGPageContentSharingURL[] =
     "https://support.google.com/gemini?p=chrome_PH#topic=15280100";
+
+const char kBWGExtensionsURL[] = "https://gemini.google.com/apps";
