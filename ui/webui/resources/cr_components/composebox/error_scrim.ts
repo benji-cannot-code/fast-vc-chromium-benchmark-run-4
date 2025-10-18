5566142a5cb825f0dfab2ef1d7ebc70c3b3afbac
Hannah Collins <hannahrogers@google.com>
2025-10-17 17:05:20 -0700

    [ntp-realbox] Fix error scrim based on latest mocks
    https://screencast.googleplex.com/cast/NjQ3MTk4NzEzOTQ0NDczNnw2MTFjNzM4Mi1hNQ
    
    Fixed: 452057439
    Change-Id: Ic86405b5a8c05b1254cf52daeb6ee4d5a028154b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7047381
    Commit-Queue: Hannah Collins <hannahrogers@google.com>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1531710}

--- ui/webui/resources/cr_components/composebox/error_scrim.ts
+++ ui/webui/resources/cr_components/composebox/error_scrim.ts
@@ -28,6 +28,9 @@ export class ErrorScrimElement extends I18nMixinLit
 
   static override get properties() {
     return {
+      compactMode: {
+        type: Boolean,
+      },
       showErrorScrim_: {
         reflect: true,
         type: Boolean,
@@ -38,6 +41,7 @@ export class ErrorScrimElement extends I18nMixinLit
     };
   }
 
+  accessor compactMode: boolean = false;
   protected accessor showErrorScrim_: boolean = false;
   protected accessor errorMessage_: string = '';
 
@@ -45,15 +49,19 @@ export class ErrorScrimElement extends I18nMixinLit
     super.updated(changedProperties);
     const changedPrivateProperties =
         changedProperties as Map<PropertyKey, unknown>;
-    if (changedPrivateProperties.has('showErrorScrim_') &&
-        this.showErrorScrim_) {
-      const announcer = getAnnouncerInstance();
-      announcer.announce(this.errorMessage_);
-      const dismissErrorButton =
-          this.shadowRoot.querySelector<HTMLElement>('#dismissErrorButton');
-      if (dismissErrorButton) {
-        dismissErrorButton.focus();
+    if (changedPrivateProperties.has('showErrorScrim_')) {
+      if (this.showErrorScrim_) {
+        const announcer = getAnnouncerInstance();
+        announcer.announce(this.errorMessage_);
+        const dismissErrorButton =
+            this.shadowRoot.querySelector<HTMLElement>('#dismissErrorButton');
+        if (dismissErrorButton) {
+          dismissErrorButton.focus();
+        }
       }
+      this.fire('error-scrim-visibility-changed', {
+        showErrorScrim: this.showErrorScrim_,
+      });
     }
   }
 
