124738f384b46a0ce2d8b57e90e8270d22c08e2f
Wenyu Fu <wenyufu@chromium.org>
2025-10-17 23:03:25 -0700

    [SurfaceColor] Update Tab group list containment colors
    Update the containment bg color and the tab group favicon color.
    
    * Light: https://screenshot.googleplex.com/Bwe2bGL9UhaCDWz
    * Dark: https://screenshot.googleplex.com/4RS4bmVhoqxCFti
    
    Bug: 452969174
    Change-Id: I880dc3e6ba890e017c579aadb0be7d54aaea1e92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7055443
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Joel Tan-Aristy <jtanaristy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1531766}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupFaviconCluster.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupFaviconCluster.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.tasks.tab_management;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.os.Handler;
@@ -196,6 +197,8 @@ public class TabGroupFaviconCluster extends ConstraintLayout {
         trackingFaviconResolver.runOnCompletion(onFaviconCompletion);
     }
 
+    private boolean mContainmentEnabled;
+
     /** Constructor for inflation. */
     public TabGroupFaviconCluster(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -207,6 +210,7 @@ public class TabGroupFaviconCluster extends ConstraintLayout {
         for (int corner = Corner.TOP_LEFT; corner <= Corner.BOTTOM_LEFT; corner++) {
             TabGroupFaviconQuarter quarter = getTabGroupFaviconQuarter(corner);
             quarter.adjustPositionForCorner(corner, getId());
+            quarter.setContainmentEnabled(mContainmentEnabled);
         }
     }
 
@@ -228,5 +232,18 @@ public class TabGroupFaviconCluster extends ConstraintLayout {
 
     private TabGroupFaviconQuarter getTabGroupFaviconQuarter(@Corner int corner) {
         return (TabGroupFaviconQuarter) getChildAt(corner);
+    }
+
+    void setContainmentEnabled(boolean isEnabled) {
+        mContainmentEnabled = isEnabled;
+
+        setBackgroundTintList(
+                ColorStateList.valueOf(
+                        TabUiThemeProvider.getTabGroupClusterBackgroundTint(
+                                getContext(), isEnabled)));
+        for (int corner = Corner.TOP_LEFT; corner <= Corner.BOTTOM_LEFT; corner++) {
+            TabGroupFaviconQuarter quarter = getTabGroupFaviconQuarter(corner);
+            quarter.setContainmentEnabled(mContainmentEnabled);
+        }
     }
 }
