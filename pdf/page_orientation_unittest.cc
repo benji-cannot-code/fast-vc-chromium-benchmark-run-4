34404e9284f212931d785243d9fdfbe9e7e4ff61
Andy Phan <andyphan@chromium.org>
2025-10-20 14:31:01 -0700

    [PDF Ink Signatures] Move and rename ToPDFiumRotation()
    Rename `ToPDFiumRotation()` to `GetClockwiseRotationSteps()`, and then
    move it from //pdf/pdfium/pdfium_page.h to //pdf/page_orientation.h.
    
    https://crrev.com/c/7039182 will add a new caller that is unrelated to
    PDFium rotations.
    
    This is a mechanical refactoring and should cause no change in behavior.
    
    Bug: 427236732
    Change-Id: Ia9cda460301d02eac2ef699a2de66cc55d339ca1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7039180
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1532541}

--- pdf/page_orientation_unittest.cc
+++ pdf/page_orientation_unittest.cc
@@ -10,6 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chrome_pdf {
 namespace {
 
+TEST(PageOrientationTest, GetClockwiseRotationSteps) {
+  EXPECT_EQ(GetClockwiseRotationSteps(PageOrientation::kOriginal), 0);
+  EXPECT_EQ(GetClockwiseRotationSteps(PageOrientation::kClockwise90), 1);
+  EXPECT_EQ(GetClockwiseRotationSteps(PageOrientation::kClockwise180), 2);
+  EXPECT_EQ(GetClockwiseRotationSteps(PageOrientation::kClockwise270), 3);
+}
+
 TEST(PageOrientationTest, RotateClockwise) {
   EXPECT_EQ(RotateClockwise(PageOrientation::kOriginal),
             PageOrientation::kClockwise90);
