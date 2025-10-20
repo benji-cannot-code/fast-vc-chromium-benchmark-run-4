b853c3db09d877995e61bab83a0a63e5bf7a3c40
Norge Vizcay <vizcay@google.com>
2025-10-20 09:13:09 -0700

    Implement ValuableMetadataSyncBridge skeleton.
    Bug: 436551488
    Change-Id: I1b08cc97683ec3610ced311c15172df907fc5ef3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7062515
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1532329}

--- components/sync/model/model_error.h
+++ components/sync/model/model_error.h
@@ -196,7 +196,8 @@ class ModelError {
     kContactInfoFailedToLoadAutofillWebDatabase = 185,
     kContactInfoFailedToLoadProfilesFromTable = 186,
     kGenericTestError = 187,
-    kMaxValue = kGenericTestError,
+    kAutofillValuableMetadataFailedToLoadDatabase = 188,
+    kMaxValue = kAutofillValuableMetadataFailedToLoadDatabase,
   };
   // LINT.ThenChange(//tools/metrics/histograms/metadata/sync/enums.xml:SyncModelError)
 
