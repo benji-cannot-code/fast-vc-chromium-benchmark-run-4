c653fe251728a7c3afa09ae12ccf6fbd6ead1ca7
Keren Zhu <kerenzhu@chromium.org>
2025-10-20 10:51:51 -0700

    Revert "mac: Fix macOS 26 traffic light contrast for dark frames in light mode"
    This reverts commit f3c6fcb1eeeeb15ee412c7081dcf3dca43c5d133.
    
    Reason for revert: causes problem in window background blending on
    Win10 + ANGLE D3D11 for translucent windows. crbug.com/445485657
    
    Original change's description:
    > mac: Fix macOS 26 traffic light contrast for dark frames in light mode
    >
    > In macOS 26, the window traffic light buttons can lose contrast and
    > become invisible on inactive windows. This occurs when the operating
    > system is in light mode while the window frame uses a dark background
    > color.
    >
    > It appears that this is because macOS is using a window's NSAppearance
    > to determine the traffic lights' color. Bad colors are chosen by the
    > system when a light appearance (NSAppearanceNameAqua) uses dark colors.
    >
    > To fix this, the window's NSAppearance is now set based on the luminance
    > of its frame color rather than the browser-wide theme. If the frame
    > color is dark, the window's appearance is now set to dark.
    >
    > Fixed: 442612600
    > Change-Id: I65ac33324d2db2bec12a9a856fd0126fe265207e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6914855
    > Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    > Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1512214}
    
    Bug: 445485657
    Change-Id: Iede19df3ebf832c71b8c6ca5a547361ff1308e00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7062815
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1532391}

--- ui/views/widget/desktop_aura/desktop_window_tree_host.h
+++ ui/views/widget/desktop_aura/desktop_window_tree_host.h
@@ -69,6 +69,10 @@ class VIEWS_EXPORT DesktopWindowTreeHost {
   // Called from DesktopNativeWidgetAura::OnWidgetInitDone().
   virtual void OnWidgetInitDone() = 0;
 
+  virtual void OnWidgetThemeChanged(
+      ui::ColorProviderKey::ColorMode color_mode,
+      std::optional<SkColor> background_color) = 0;
+
   // Called from DesktopNativeWidgetAura::OnWindowActivated().
   // `active`: if `DesktopNativeWidgetAura::content_window()` contains the
   // `aura::Window` that gains active.
@@ -190,8 +194,6 @@ class VIEWS_EXPORT DesktopWindowTreeHost {
   virtual bool IsFullscreen() const = 0;
 
   virtual void SetOpacity(float opacity) = 0;
-
-  virtual void SetBackgroundColor(SkColor background_color) = 0;
 
   // See NativeWidgetPrivate::SetAspectRatio for more information about what
   // `excluded_margin` does.
