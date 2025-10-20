0885aca7bdd1b3dbd0b4dbeb404d93f2ec542800
Ewann Pellé <ewannpv@chromium.org>
2025-10-20 07:14:02 -0700

    [iOS] Add ButtonStackViewController
    This CL introduces a new reusable UI component,
    `ButtonStackViewController`, for managing a standardized stack of action
    buttons in iOS Chrome.
    
    The `ButtonStackViewController` provides a consistent way to display
    primary, secondary, and tertiary actions at the bottom of a view, with
    support for different button styles, loading/confirmation states, and
    custom content views.
    
    This component will be used to unify the design and behavior of action
    button patterns across various features in the application.
    
    Bug: 444648926
    Change-Id: I020b479abe06ff2feea22f4d3218f68b7c242729
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6929642
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1532250}

---
+++ ios/chrome/common/ui/button_stack/button_stack_action_delegate.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_COMMON_UI_BUTTON_STACK_BUTTON_STACK_ACTION_DELEGATE_H_
+#define IOS_CHROME_COMMON_UI_BUTTON_STACK_BUTTON_STACK_ACTION_DELEGATE_H_
+
+// Delegate for handling actions from a ButtonStackViewController.
+@protocol ButtonStackActionDelegate
+
+// Called when the primary action button is tapped.
+- (void)didTapPrimaryActionButton;
+
+// Called when the secondary action button is tapped.
+- (void)didTapSecondaryActionButton;
+
+// Called when the tertiary action button is tapped.
+- (void)didTapTertiaryActionButton;
+
+@end
+
+#endif  // IOS_CHROME_COMMON_UI_BUTTON_STACK_BUTTON_STACK_ACTION_DELEGATE_H_
---
+++ ios/chrome/common/ui/button_stack/button_stack_configuration.mm
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/common/ui/button_stack/button_stack_configuration.h"
+
+#import "ios/chrome/common/ui/button_stack/button_stack_constants.h"
+
+@implementation ButtonStackConfiguration
+
+- (instancetype)init {
+  self = [super init];
+  if (self) {
+    _primaryButtonStyle = ButtonStackButtonStylePrimary;
+    _secondaryButtonStyle = ButtonStackButtonStyleSecondary;
+    _tertiaryButtonStyle = ButtonStackButtonStyleSecondary;
+  }
+  return self;
+}
+
+@end
