1893a7ade75b5acc478a68eb14cedc3ca3c5f576
Varun Khaneja <vakh@chromium.org>
2025-10-20 01:08:56 -0700

    http => https scheme for the link to Noise Protocol Framework.
    Change-Id: Iadca402bad2e38619706df94679ed893dd4842f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7056708
    Auto-Submit: Varun Khaneja <vakh@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Mohamad Arab <arabm@google.com>
    Commit-Queue: Mohamad Arab <arabm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1532109}

--- components/legion/noise.h
+++ components/legion/noise.h
@@ -20,7 +20,7 @@ namespace legion {
 
 // Noise implements a small subset of the Noise Protocol Framework [1].
 //
-// http://www.noiseprotocol.org/noise.html#the-handshakestate-object
+// https://www.noiseprotocol.org/noise.html#the-handshakestate-object
 class Noise {
  public:
   // HandshakeType enumerates the supported handshake patterns.
