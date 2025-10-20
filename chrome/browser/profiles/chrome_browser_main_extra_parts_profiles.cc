6bec03783171b00789af858f8233d9f97b5dd5e2
Kamil Jarosz <kjarosz@google.com>
2025-10-20 09:39:55 -0700

    Remove ACT survey and Incognito survey service
    The study has concluded, so we can remove the survey.
    
    Additionally, this CL removes the Incognito survey service, as it looks
    like we won't be needing that anymore.
    
    OBSOLETE_HISTOGRAM[PrivacySandbox.ActSurvey.Status]=Study has concluded, survey has been removed.
    
    Fixed: 450979582
    Change-Id: I81860d812d1dc40ce0dc15c806182529c1a78d67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7062036
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Reviewed-by: Matt Dembski <dembski@google.com>
    Commit-Queue: Matt Dembski <dembski@google.com>
    Reviewed-by: Andrew Liu <liu@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1532345}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -169,7 +169,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_service_factory.h"
 #include "chrome/browser/preloading/search_preload/search_preload_service_factory.h"
 #include "chrome/browser/privacy/privacy_metrics_service_factory.h"
-#include "chrome/browser/privacy_sandbox/incognito/privacy_sandbox_incognito_survey_service_factory.h"
 #include "chrome/browser/privacy_sandbox/notice/notice_service_factory.h"
 #include "chrome/browser/privacy_sandbox/privacy_sandbox_service_factory.h"
 #include "chrome/browser/privacy_sandbox/privacy_sandbox_settings_factory.h"
@@ -1221,7 +1220,6 @@ void ChromeBrowserMainExtraPartsProfiles::
   prerender::NoStatePrefetchLinkManagerFactory::GetInstance();
   prerender::NoStatePrefetchManagerFactory::GetInstance();
   PrivacyMetricsServiceFactory::GetInstance();
-  PrivacySandboxIncognitoSurveyServiceFactory::GetInstance();
   PrivacySandboxNoticeServiceFactory::GetInstance();
   PrivacySandboxServiceFactory::GetInstance();
   PrivacySandboxSettingsFactory::GetInstance();
