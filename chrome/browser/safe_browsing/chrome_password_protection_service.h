0163c9e639eba18df0ffc2207dfa5f882fa7919c
Nwokedi Idika <nwokedi@chromium.org>
2025-10-20 14:22:29 -0700

    [PhishGuard] Don't send pw reuse pings in alert mode
    When an admin has configured their policy to be in alert mode, any
    password reuse on a non-allowlisted page is a security event. In these
    cases, no ping needs to be sent to Safe Browsing. This change adds a
    check to stop sending pings to Safe Browsing when alert mode is
    configured.
    
    Bug: 453699961
    Change-Id: I83bd084dc59c3af62c9bc4104dbf42cbf831cb14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7064213
    Commit-Queue: Nwokedi Idika <nwokedi@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1532535}

--- chrome/browser/safe_browsing/chrome_password_protection_service.h
+++ chrome/browser/safe_browsing/chrome_password_protection_service.h
@@ -468,6 +468,8 @@ class ChromePasswordProtectionService
                            OnEnterpriseTriggerOffGSuite);
   FRIEND_TEST_ALL_PREFIXES(ChromePasswordProtectionServiceBrowserTest,
                            VerifyIsInExcludedCountry);
+  FRIEND_TEST_ALL_PREFIXES(ChromePasswordProtectionServiceTest,
+                           VerifyAlertModeForEnterprisePasswordEntryPing);
 
  private:
   friend class MockChromePasswordProtectionService;
