9b77424a538e613a7f13624972e5f3cfaf1e54ab
Ireneusz Szulc <ireneuszszulc@google.com>
2025-10-20 00:58:48 -0700

    YourSavedInfo: Update Autofill label in 3-dot menu
    This commit changes the label in the 3-dot menu (wrench menu) of the
    Autofill menu item to display "Your saved info" when
    `kYourSavedInfoBrandingInSettings` feature flag is enabled.
    Otherwise, it defaults to the original "Passwords and Autofill" label.
    It also changes the icon next to it.
    
    Screenshots:
    - when feature flag is off: https://screenshot.googleplex.com/3qcfxwnHYc5oDim
    - when feature flag `YourSavedInfoBrandingInSettings` is on: https://screenshot.googleplex.com/4ZHT8wX33vwergd
    
    This change is part of the "Your Saved Info" rebranding effort.
    
    Bug: 452603303
    Change-Id: I3643001e3f144702e19da8c1437fa532c08c754a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7053331
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Julia Sobiech <jsobiech@google.com>
    Commit-Queue: Ireneusz Szulc <ireneuszszulc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1532107}

--- chrome/browser/ui/toolbar/app_menu_model.cc
+++ chrome/browser/ui/toolbar/app_menu_model.cc
@@ -96,6 +96,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/branded_strings.h"
 #include "chrome/grit/generated_resources.h"
 #include "chrome/grit/theme_resources.h"
+#include "components/autofill/core/common/autofill_features.h"
 #include "components/bookmarks/common/bookmark_pref_names.h"
 #include "components/contextual_tasks/public/features.h"
 #include "components/dom_distiller/content/browser/distillable_page_utils.h"
@@ -1907,9 +1908,18 @@ void AppMenuModel::Build() {
   if (!browser_->profile()->IsGuestSession()) {
     sub_menus_.push_back(
         std::make_unique<PasswordsAndAutofillSubMenuModel>(this));
-    AddSubMenuWithStringIdAndVectorIcon(
-        this, IDC_PASSWORDS_AND_AUTOFILL_MENU, IDS_PASSWORDS_AND_AUTOFILL_MENU,
-        sub_menus_.back().get(), vector_icons::kPasswordManagerIcon);
+    int string_id = base::FeatureList::IsEnabled(
+                        autofill::features::kYourSavedInfoBrandingInSettings)
+                        ? IDS_SETTINGS_YOUR_SAVED_INFO
+                        : IDS_PASSWORDS_AND_AUTOFILL_MENU;
+    const gfx::VectorIcon& vector_icon =
+        base::FeatureList::IsEnabled(
+            autofill::features::kYourSavedInfoBrandingInSettings)
+            ? vector_icons::kPersonTextIcon
+            : vector_icons::kPasswordManagerIcon;
+    AddSubMenuWithStringIdAndVectorIcon(this, IDC_PASSWORDS_AND_AUTOFILL_MENU,
+                                        string_id, sub_menus_.back().get(),
+                                        vector_icon);
     SetElementIdentifierAt(
         GetIndexOfCommandId(IDC_PASSWORDS_AND_AUTOFILL_MENU).value(),
         kPasswordAndAutofillMenuItem);
