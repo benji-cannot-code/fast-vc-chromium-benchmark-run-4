81e81ae655c17802ed851e4702136d883e9e3c7f
Kalvin Lee <kdlee@chromium.org>
2025-10-20 00:08:16 -0700

    FrameFirstPaint: Use `uintptr_t`
    The `frame` member is not used as a pointer, but only as a
    non-dereferenced identifier.
    
    Per `//base/memory/raw_ptr.md`, don't propagate along the `void *`,
    which would (theoretically) set off the `raw_ptr_plugin`, and convert it
    instead to a `uintptr_t`.
    
    Bug: 330759291
    Change-Id: I49f2b6e253925ad0c298eda1e19e7062f2df3f42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7055882
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1532087}

--- third_party/blink/renderer/platform/graphics/paint/paint_controller.cc
+++ third_party/blink/renderer/platform/graphics/paint/paint_controller.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/graphics/paint/paint_controller.h"
 
+#include <cstdint>
 #include <memory>
 
 #include "base/logging.h"
@@ -887,7 +888,7 @@ void PaintController::BeginFrame(const void* frame) {
 
 FrameFirstPaint PaintController::EndFrame(const void* frame) {
   FrameFirstPaint result = frame_first_paints_.back();
-  DCHECK(result.frame == frame);
+  DCHECK(result.frame == reinterpret_cast<uintptr_t>(frame));
   frame_first_paints_.pop_back();
   return result;
 }
--- third_party/blink/renderer/platform/graphics/paint/paint_controller.h
+++ third_party/blink/renderer/platform/graphics/paint/paint_controller.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_PAINT_PAINT_CONTROLLER_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_PAINT_PAINT_CONTROLLER_H_
 
+#include <cstdint>
 #include <memory>
 #include <optional>
 #include <utility>
@@ -54,12 +55,12 @@ enum class PaintBenchmarkMode {
 struct FrameFirstPaint {
   DISALLOW_NEW();
   explicit FrameFirstPaint(const void* frame)
-      : frame(frame),
+      : frame(reinterpret_cast<uintptr_t>(frame)),
         first_painted(false),
         text_painted(false),
         image_painted(false) {}
 
-  const void* frame = nullptr;
+  uintptr_t frame = 0u;
   bool first_painted : 1;
   bool text_painted : 1;
   bool image_painted : 1;
