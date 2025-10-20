ddf253d36025908e213b74a7bc547f2162577aee
Antonio Sartori <antoniosartori@chromium.org>
2025-10-20 03:07:59 -0700

    [identifiability] Turn off meta experiment
    This won't be needed anymore. Let's turn off the flag first, then
    we'll start removing the code.
    
    Bug: 451514156
    Change-Id: I13719cbc2e0cd752749566864d3f8feb8dae3592
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7062171
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1532164}

--- chrome/common/privacy_budget/privacy_budget_features.cc
+++ chrome/common/privacy_budget/privacy_budget_features.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace features {
 
 BASE_FEATURE(kIdentifiabilityStudyMetaExperiment,
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 const base::FeatureParam<double>
     kIdentifiabilityStudyMetaExperimentActivationProbability = {
