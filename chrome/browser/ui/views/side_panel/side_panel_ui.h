b0f57ee8d1b9f8140f297dc3a8e5cee080448cd7
Claudio DeSouza <cdesouza@chromium.org>
2025-10-20 12:03:10 -0700

    Fix IWYU in `side_panel_ui.h`
    This is a simple correction for the recently introduced `ShowFrom`
    function relying on transient inclusions for `gfx::Rect`.
    
    Bug: 445453126
    Change-Id: Iba14b5ab900270551299b825fa4726b5758bf74c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7062059
    Commit-Queue: Claudio DeSouza <cdesouza@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1532443}

--- chrome/browser/ui/views/side_panel/side_panel_ui.h
+++ chrome/browser/ui/views/side_panel/side_panel_ui.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/side_panel/side_panel_entry_id.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_entry_key.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_enums.h"
+#include "ui/gfx/geometry/rect.h"
 
 namespace content {
 class WebContents;
