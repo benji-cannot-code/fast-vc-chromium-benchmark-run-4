faac51849d49f30daf50175bdca2b8b69f607b3f
Antonio Gomes <tonikitoo@igalia.com>
2025-10-20 13:37:15 -0700

    Pass `local_state` to ash::input_method::InputMethodPersistence ctor
    This CL passes to the constructor of InputMethodPersistence, an
    `local_state` object, so that it stops referring to `g_browser_process`.
    
    R=hidehiko@chromium.org, junis@google.com
    
    Bug: 446058312
    Change-Id: Ia545aee4530c8cd072d18e7d784eea61ab4b6196
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7041349
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Jun Ishiguro <junis@google.com>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1532507}

--- chrome/browser/ash/input_method/input_method_persistence.h
+++ chrome/browser/ash/input_method/input_method_persistence.h
@@ -9,9 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/memory/raw_ptr.h"
+#include "base/memory/raw_ref.h"
 #include "ui/base/ime/ash/input_method_manager.h"
 
 class AccountId;
+class PrefService;
 
 namespace ash {
 namespace input_method {
@@ -25,7 +27,10 @@ class InputMethodPersistence : public InputMethodManager::Observer {
   // provided InputMethodManager. The client is responsible for calling
   // OnSessionStateChange whenever the InputMethodManager::UISessionState
   // changes.
-  explicit InputMethodPersistence(InputMethodManager* input_method_manager);
+  //
+  // `local_state` must be non-null, and must outlive `this`.
+  InputMethodPersistence(PrefService* local_state,
+                         InputMethodManager* input_method_manager);
 
   InputMethodPersistence(const InputMethodPersistence&) = delete;
   InputMethodPersistence& operator=(const InputMethodPersistence&) = delete;
@@ -47,12 +52,15 @@ class InputMethodPersistence : public InputMethodManager::Observer {
 
   // Update user last input method ID into known_user data.
   static void SetUserLastInputMethodPreferenceForTesting(
+      PrefService& local_state,
       const AccountId& account_id,
       const std::string& input_method);
 
  private:
   void PersistUserInputMethod(const std::string& input_method_id,
                               Profile* profile);
+
+  const raw_ref<PrefService> local_state_;
 
   raw_ptr<InputMethodManager> input_method_manager_;
 };
