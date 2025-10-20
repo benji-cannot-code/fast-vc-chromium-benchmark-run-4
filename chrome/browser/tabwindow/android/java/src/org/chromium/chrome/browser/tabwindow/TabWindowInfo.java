1cde77c12aa44f46e7891205071b44523774864b
Sky Malice <skym@chromium.org>
2025-10-20 10:08:52 -0700

    Create TabWindowManager#getTabAndWindowIdByTabId.
    Bug: None
    Change-Id: Id9ab6eb3e8b732d4c4fae8941646681d31773c5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7049688
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1532364}

---
+++ chrome/browser/tabwindow/android/java/src/org/chromium/chrome/browser/tabwindow/TabWindowInfo.java
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.tabwindow;
+
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.chrome.browser.tab.Tab;
+import org.chromium.chrome.browser.tabmodel.TabModel;
+import org.chromium.chrome.browser.tabmodel.TabModelSelector;
+
+/** Holds objects related to a tab and where it lives. */
+@NullMarked
+public class TabWindowInfo {
+    public final @WindowId int windowId;
+    public final TabModelSelector tabModelSelector;
+    public final TabModel tabModel;
+    public final Tab tab;
+
+    public TabWindowInfo(
+            @WindowId int windowId, TabModelSelector tabModelSelector, TabModel tabModel, Tab tab) {
+        this.tab = tab;
+        this.tabModel = tabModel;
+        this.tabModelSelector = tabModelSelector;
+        this.windowId = windowId;
+    }
+}
