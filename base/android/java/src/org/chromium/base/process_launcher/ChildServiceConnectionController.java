ad31e573ba72a3cff7ee31ec085094bd9a5ce0a0
Shin Kawamura <kawasin@google.com>
2025-10-20 01:12:31 -0700

    Add UMA metrics for the count of service bindings
    Existing `Android.ChildProcessBinding.{type}Connections` UMA metrics
    counts the number of processes (i.e. ChildProcessConnection), not actual
    the number of service bindings (i.e. ConnectionRecord in AMS).
    
    EffectiveBindingState feature is to reduce the number of service
    binding. The new UMA metrics
    `Android.ChildProcessBinding.{type}BindingCount` helps understand the
    impact of the feature.
    
    Bug: 445572578
    Change-Id: I61d42854ddbe0599069d831b4e45830907fea5ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7052163
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Shin Kawamura <kawasin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1532112}

--- base/android/java/src/org/chromium/base/process_launcher/ChildServiceConnectionController.java
+++ base/android/java/src/org/chromium/base/process_launcher/ChildServiceConnectionController.java
@@ -133,7 +133,7 @@ import org.chromium.build.annotations.NullMarked;
     boolean isUnbound();
 
     /**
-     * @return a string representation of the current binding state for debugging purposes.
+     * @return the detailed binding state for debugging purposes.
      */
-    String buildDebugStateString();
+    ChildProcessConnectionState getConnectionStateForDebugging();
 }
