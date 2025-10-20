7ef45acf0f41ec729755d7905e002f18bb3e6aff
Henrique Nakashima <hnakashima@chromium.org>
2025-10-20 14:29:16 -0700

    [Android] Prepare to make ObservableSupplier.get() return @Nullable (2)
    This is a set of fixes to change ObservableSupplier<E> from extend
    Supplier<E> to Supplier<@Nullable E>.
    
    Bug: 453007852,448691376
    Change-Id: Ibfa18b7c01685e332e0c68587721ebd6fd6eab40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7063353
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1532539}

--- chrome/browser/ui/android/hats/test/java/src/org/chromium/chrome/browser/ui/hats/TestSurveyUtils.java
+++ chrome/browser/ui/android/hats/test/java/src/org/chromium/chrome/browser/ui/hats/TestSurveyUtils.java
@@ -18,6 +18,7 @@ import org.junit.runners.model.Statement;
 
 import org.chromium.base.CommandLine;
 import org.chromium.base.ThreadUtils;
+import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.base.test.util.InMemorySharedPreferences;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.lifecycle.ActivityLifecycleDispatcher;
@@ -25,7 +26,6 @@ import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 
 import java.util.Map;
-import java.util.function.Supplier;
 
 /** Util class for survey related testing. */
 public class TestSurveyUtils {
@@ -157,7 +157,7 @@ public class TestSurveyUtils {
         }
 
         @Override
-        public Supplier<Boolean> getCrashUploadPermissionSupplier() {
+        public ObservableSupplier<Boolean> getCrashUploadPermissionSupplier() {
             return mCrashUploadPermissionSupplier;
         }
     }
