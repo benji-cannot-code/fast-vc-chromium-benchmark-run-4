88e3056134b2202f2aa551bf7f63d76300083c3e
Devon Loehr <dloehr@google.com>
2025-10-20 07:47:41 -0700

    [rustmojo] Initial deparser implementation
    This adds a minimal serializer for MojomValues. Unlike parsing, it
    takes two inputs: the value itself, and the format it will take on the
    wire. Annoyingly, this means we have to do lots of extra validation to
    ensure the two formats match.
    
    Tested manually, apart from version numbers this outputs the same thing
    as the C++ bindings do for my test case.
    
    Bug: 444509367
    Change-Id: Ibb0c38498657111068d44be34c06cc78169d2f9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7046838
    Reviewed-by: Julia Hansbrough <flowerhack@google.com>
    Commit-Queue: Devon Loehr <dloehr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1532271}

--- mojo/public/rust/mojom_parser/lib.rs
+++ mojo/public/rust/mojom_parser/lib.rs
@@ -9,12 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // public, etc. For now, just export everything blindly.
 
 mod ast;
+mod deparse_values;
 mod pack;
 mod parse_messages;
 mod parse_primitives;
 mod parse_values;
 
 pub use crate::ast::*;
+pub use crate::deparse_values::*;
 pub use crate::pack::*;
 pub use crate::parse_messages::*;
 pub use crate::parse_values::*;
