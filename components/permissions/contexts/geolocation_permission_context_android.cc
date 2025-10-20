a3b14af8bf6547a554e177f027ca81f99dddb763
Nate Fischer <ntfschr@chromium.org>
2025-10-20 10:15:15 -0700

    Revert "Reland "[permissions] Add browsertest for approximate geolocation flow""
    This reverts commit 1de85075524390e77d117067c3bb76bf2c7cd557.
    
    Reason for revert: test failures in crbug.com/452645984
    
    Original change's description:
    > Reland "[permissions] Add browsertest for approximate geolocation flow"
    >
    > This is a reland of commit 9d3515e8595490db2c399a1b3a06e26c5af7b517
    >
    > Original change's description:
    > > [permissions] Add browsertest for approximate geolocation flow
    > >
    > > This CL adds a browsertest that verifies the flow for requesting
    > > geolocation permission with the approximate/precise option when only
    > > approximate location is granted.
    > >
    > > The test needs to be on Android because the feature is not implemented
    > > on desktop yet.
    > >
    > > Bug: 393053278
    > > Change-Id: Ic6fbc6ecf1d0e77e92392a73d5e83cf940009edd
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7035572
    > > Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    > > Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    > > Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1530773}
    >
    > Bug: 393053278
    > Change-Id: I3a1cc994a993669309fe5125e6b2f5dcacfd514e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7052722
    > Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    > Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1532195}
    
    Bug: 393053278
    Bug: 452645984
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I90ce98cad75acb541d685e1c876768d8fd01e825
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7062057
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1532369}

--- components/permissions/contexts/geolocation_permission_context_android.cc
+++ components/permissions/contexts/geolocation_permission_context_android.cc
@@ -144,7 +144,9 @@ void GeolocationPermissionContextAndroid::RequestPermission(
                                 .status;
   if (!request_data->IsEmbeddedPermissionElementInitiated() &&
       (status == PermissionStatus::GRANTED) &&
-      ShouldRepromptUser(web_contents)) {
+      ShouldRepromptUserForPermissions(web_contents,
+                                       {content_settings_type()}) ==
+          PermissionRepromptState::kShow) {
     if (auto* manager =
             PermissionRequestManager::FromWebContents(web_contents)) {
       if (manager->IsCurrentRequestEmbeddedPermissionElementInitiated() &&
@@ -489,14 +491,6 @@ void GeolocationPermissionContextAndroid::FinishNotifyPermissionSet(
 void GeolocationPermissionContextAndroid::SetLocationSettingsForTesting(
     std::unique_ptr<LocationSettings> settings) {
   location_settings_ = std::move(settings);
-}
-
-bool GeolocationPermissionContextAndroid::ShouldRepromptUser(
-    content::WebContents* web_contents) {
-  return should_reprompt_user_for_testing_.value_or(
-      ShouldRepromptUserForPermissions(web_contents,
-                                       {content_settings_type()}) ==
-      PermissionRepromptState::kShow);
 }
 
 }  // namespace permissions
