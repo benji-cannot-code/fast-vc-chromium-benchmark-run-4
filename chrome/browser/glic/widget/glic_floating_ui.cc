2f33b78f262f368fdc0779bdc6dbbc234092e623
Bryant Chandler <bryantchandler@chromium.org>
2025-10-20 11:47:21 -0700

    [GlicMI] Add comments for calls that cause this destruction
    Change-Id: I64c5360a4ed31a2752b29d8f06c9a96f1d6adb4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7063840
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1532428}

--- chrome/browser/glic/widget/glic_floating_ui.cc
+++ chrome/browser/glic/widget/glic_floating_ui.cc
@@ -254,6 +254,7 @@ void GlicFloatingUi::Close() {
   glic_widget_observation_.Reset();
   glic_widget_.reset();
   user_resizable_ = false;
+  // NOTE: `this` will be destroyed after this call.
   delegate_->WillCloseFor(FloatingEmbedderKey{});
 }
 
@@ -360,6 +361,7 @@ base::WeakPtr<views::View> GlicFloatingUi::GetView() {
 void GlicFloatingUi::SwitchConversation(
     glic::mojom::ConversationInfoPtr info,
     mojom::WebClientHandler::SwitchConversationCallback callback) {
+  // NOTE: `this` may be destroyed after this call.
   delegate_->SwitchConversation(
       ShowOptions::ForFloating(GetGlicWidget()->GetWindowBoundsInScreen()),
       std::move(info), std::move(callback));
--- chrome/browser/glic/widget/glic_side_panel_ui.cc
+++ chrome/browser/glic/widget/glic_side_panel_ui.cc
@@ -115,6 +115,7 @@ void GlicSidePanelUi::Detach() {
   if (!tab_) {
     return;
   }
+  // NOTE: `this` will be destroyed after this call.
   delegate_->Detach(tab_.get());
 }
 
@@ -141,6 +142,7 @@ void GlicSidePanelUi::SidePanelStateChanged(
   // Showing only happens through glic entrypoint, hiding can also be triggered
   // by side panel coordinator when replacing glic with another entry.
   if (state != GlicSidePanelCoordinator::State::kShown && tab_) {
+    // NOTE: `this` will be destroyed after this call.
     delegate_->WillCloseFor(tab_.get());
   }
 }
@@ -148,6 +150,7 @@ void GlicSidePanelUi::SidePanelStateChanged(
 void GlicSidePanelUi::SwitchConversation(
     glic::mojom::ConversationInfoPtr info,
     mojom::WebClientHandler::SwitchConversationCallback callback) {
+  // NOTE: `this` may be destroyed after this call.
   delegate_->SwitchConversation(ShowOptions::ForSidePanel(*tab_),
                                 std::move(info), std::move(callback));
 }
@@ -188,6 +191,7 @@ void GlicSidePanelUi::Close() {
   }
   panel_state_.kind = mojom::PanelState_Kind::kHidden;
   delegate_->NotifyPanelStateChanged();
+  // NOTE: `this` will be destroyed after this call.
   glic_side_panel_coordinator->Close();
 }
 
