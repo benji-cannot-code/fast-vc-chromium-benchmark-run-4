01e937ac3dfb46bdbf5b474c420b2af323885602
Jian Li <jianli@chromium.org>
2025-10-20 15:01:13 -0700

    Support showing omnibox action button only when on focus
    To support this behavior, a new property showOnlyOnFocus is added
    to Action. BaseSuggestionView is updated to react to this behavior.
    
    Bug: 451731639
    Change-Id: I3a6a5af8786d3fcc6bc9b0b4192c7d3d99289972
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7049593
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Jian Li <jianli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1532569}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/BaseSuggestionView.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/BaseSuggestionView.java
@@ -17,7 +17,6 @@ import android.widget.ImageView;
 import androidx.annotation.LayoutRes;
 import androidx.annotation.VisibleForTesting;
 import androidx.appcompat.content.res.AppCompatResources;
-import androidx.appcompat.widget.AppCompatImageView;
 
 import org.chromium.build.annotations.CheckDiscard;
 import org.chromium.build.annotations.NullMarked;
@@ -42,7 +41,7 @@ public class BaseSuggestionView<T extends View> extends SuggestionLayout {
     public final T contentView;
     public final ActionChipsView actionChipsView;
     public final RoundedCornerOutlineProvider decorationIconOutline;
-    private final List<ImageView> mActionButtons;
+    private final List<ActionButtonView> mActionButtons;
     private final SimpleSelectionController mActionButtonsHighlighter;
     private @Nullable Runnable mOnFocusViaSelectionListener;
 
@@ -114,7 +113,7 @@ public class BaseSuggestionView<T extends View> extends SuggestionLayout {
     /**
      * @return List of Action views.
      */
-    public List<ImageView> getActionButtons() {
+    public List<ActionButtonView> getActionButtons() {
         return mActionButtons;
     }
 
@@ -141,7 +140,7 @@ public class BaseSuggestionView<T extends View> extends SuggestionLayout {
      */
     private void increaseActionButtonsCount(int desiredViewCount) {
         for (int index = mActionButtons.size(); index < desiredViewCount; index++) {
-            ImageView actionView = new AppCompatImageView(getContext());
+            ActionButtonView actionView = new ActionButtonView(getContext());
             actionView.setClickable(true);
             actionView.setFocusable(true);
             actionView.setScaleType(ImageView.ScaleType.CENTER);
@@ -201,9 +200,21 @@ public class BaseSuggestionView<T extends View> extends SuggestionLayout {
     }
 
     @Override
+    public void onHoverChanged(boolean hovered) {
+        super.onHoverChanged(hovered);
+        for (ActionButtonView v : mActionButtons) {
+            v.onParentViewHoverChanged(hovered);
+        }
+    }
+
+    @Override
     public void setSelected(boolean selected) {
         super.setSelected(selected);
         if (mActionButtonsHighlighter != null) mActionButtonsHighlighter.reset();
+        for (ActionButtonView v : mActionButtons) {
+            v.onParentViewSelected(selected);
+        }
+
         if (selected && mOnFocusViaSelectionListener != null) {
             mOnFocusViaSelectionListener.run();
         }
--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/BaseSuggestionViewBinder.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/BaseSuggestionViewBinder.java
@@ -164,12 +164,13 @@ public final class BaseSuggestionViewBinder<T extends View>
         view.setActionButtonsCount(actionCount);
 
         // Drawable retrieved once here (expensive) and will be copied multiple times (cheap).
-        final List<ImageView> actionViews = view.getActionButtons();
+        final List<ActionButtonView> actionViews = view.getActionButtons();
         for (int index = 0; index < actionCount; index++) {
-            final ImageView actionView = actionViews.get(index);
+            final ActionButtonView actionView = actionViews.get(index);
             final Action action = actions.get(index);
             actionView.setOnClickListener(v -> action.callback.run());
             actionView.setContentDescription(action.accessibilityDescription);
+            actionView.enableShowOnlyOnFocus(action.showOnlyOnFocus);
             applySelectableBackground(model, actionView);
             updateIcon(
                     model,
@@ -214,7 +215,7 @@ public final class BaseSuggestionViewBinder<T extends View>
         // scheme will be applied then.
         if (actions == null) return;
 
-        final List<ImageView> actionViews = view.getActionButtons();
+        final List<ActionButtonView> actionViews = view.getActionButtons();
         for (int index = 0; index < actionViews.size(); index++) {
             ImageView actionView = actionViews.get(index);
 
