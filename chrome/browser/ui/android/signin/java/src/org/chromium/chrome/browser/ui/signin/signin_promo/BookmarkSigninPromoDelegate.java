b34be62de9ffa1e23ce4e3f060fa0189591d0eb8
quacksort <quacksort@google.com>
2025-10-20 04:59:21 -0700

    [Signin][Android] Add strings to seamless sign-in NTP promo UIs
    This CL adds the correct strings to the new Seamless Sign-in promo UIs
    in the NTP page.
    
    Bug: 448362200
    Change-Id: I31891a1d285a08a14763ad29eade1df388c3f23e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7037971
    Reviewed-by: Menghan Yang <myuu@google.com>
    Commit-Queue: Lucia Giorgi <quacksort@google.com>
    Cr-Commit-Position: refs/heads/main@{#1532200}

--- chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/signin_promo/BookmarkSigninPromoDelegate.java
+++ chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/signin_promo/BookmarkSigninPromoDelegate.java
@@ -92,7 +92,7 @@ public class BookmarkSigninPromoDelegate extends SigninPromoDelegate {
     }
 
     @Override
-    String getDescription() {
+    String getDescription(@Nullable String accountEmail) {
         switch (mPromoState) {
             case PromoState.SIGNIN:
                 return mContext.getString(R.string.signin_promo_description_bookmarks);
