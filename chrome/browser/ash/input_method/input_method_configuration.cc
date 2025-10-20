9fc83358119db650d51a6d5cffedad2242c4966c
Antonio Gomes <tonikitoo@igalia.com>
2025-10-19 21:12:02 -0700

    Change ash::input_method::InputMethodPersistence ownership
    Previously, InputMethodPersistence instance was stored in a free
    pointer, declared in the anonymous namespace in
    input_method_persistence.cc.
    This free pointer was manually instantiated in
    ash::input_method::Initialize(), and deleted in
    ash::input_method::Shutdown().
    
    This CL changes this ownership model: now, InputMethodPersistence is
    owned by InputMethodManagerImpl. In practical terms,
    InputMethodManagerImpl is also instantiated in
    ash::input_method::Initialize() and destructed in
    ash::input_method::Shutdown(), and so continues so
    InputMethodPersistence, not changing the current life cycles.
    
    This CL is a predecessor CL to remove the direct references to
    `g_browser_process->local_state()` from input_method_persistence.cc
    
    R=hidehiko@chromium.org, junis@google.com
    
    Bug: 446058312
    Change-Id: I83ee6be82337ab17bc17bb80fe51736c392dbee6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7046780
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Jun Ishiguro <junis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1532055}

--- chrome/browser/ash/input_method/input_method_configuration.cc
+++ chrome/browser/ash/input_method/input_method_configuration.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/input_method/component_extension_ime_manager_delegate_impl.h"
 #include "chrome/browser/ash/input_method/input_method_delegate_impl.h"
 #include "chrome/browser/ash/input_method/input_method_manager_impl.h"
-#include "chrome/browser/ash/input_method/input_method_persistence.h"
 #include "components/application_locale_storage/application_locale_storage.h"
 #include "components/prefs/pref_service.h"
 #include "ui/base/ime/ash/fake_ime_keyboard.h"
@@ -27,7 +26,6 @@ namespace {
 
 bool g_disable_extension_loading = false;
 Accessibility* g_accessibility = nullptr;
-InputMethodPersistence* g_input_method_persistence = nullptr;
 
 }  // namespace
 
@@ -51,9 +49,6 @@ void Initialize(PrefService* local_state,
 
   delete g_accessibility;
   g_accessibility = new Accessibility(impl);
-
-  delete g_input_method_persistence;
-  g_input_method_persistence = new InputMethodPersistence(impl);
 }
 
 void InitializeForTesting(InputMethodManager* mock_manager) {
@@ -67,9 +62,6 @@ void DisableExtensionLoading() {
 void Shutdown() {
   delete g_accessibility;
   g_accessibility = nullptr;
-
-  delete g_input_method_persistence;
-  g_input_method_persistence = nullptr;
 
   InputMethodManager::Shutdown();
 }
--- chrome/browser/ash/input_method/input_method_manager_impl.cc
+++ chrome/browser/ash/input_method/input_method_manager_impl.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/trace_event.h"
 #include "chrome/browser/ash/input_method/assistive_window_controller.h"
 #include "chrome/browser/ash/input_method/candidate_window_controller.h"
+#include "chrome/browser/ash/input_method/input_method_persistence.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/browser_process_platform_part_ash.h"
 #include "chrome/browser/lifetime/termination_notification.h"
@@ -1095,6 +1096,11 @@ InputMethodManagerImpl::InputMethodManagerImpl(
       component_extension_ime_manager_->GetAllIMEAsInputMethodDescriptor();
   util_.ResetInputMethods(descriptors);
 
+  // InputMethodPersistence ctor calls back a virtual method of
+  // InputMethodManager. Hence, it can not be instantiated from the ctor
+  // initialization list.
+  persistence_ = std::make_unique<InputMethodPersistence>(this);
+
   // We should not use ALL_BROWSERS_CLOSING here since logout might be cancelled
   // by JavaScript after ALL_BROWSERS_CLOSING is sent (crosbug.com/11055).
   on_app_terminating_subscription_ =
@@ -1103,6 +1109,10 @@ InputMethodManagerImpl::InputMethodManagerImpl(
 }
 
 InputMethodManagerImpl::~InputMethodManagerImpl() {
+  // Ensure that `persistence_` gets reset prior to `observers_` since it is
+  // itself an observer.
+  persistence_.reset();
+
   if (candidate_window_controller_.get()) {
     candidate_window_controller_->RemoveObserver(this);
   }
