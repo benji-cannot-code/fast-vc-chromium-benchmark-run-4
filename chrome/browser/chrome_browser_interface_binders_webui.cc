71b932433d15c7cbe6590e48451e8065ff8b4e7e
dpapad <dpapad@chromium.org>
2025-10-20 14:54:27 -0700

    WebUI: Simplify Mojo bindings backend registration, part 1.
    In this part updating only NewTabFooterUI, NewTabPageThirdPartyUI and
    SettingsUI as examples. Remaining
    RegisterWebUIControllerInterfaceBinder() calls should be migrated in
    follow-up CLs.
    
    Instead of using RegisterWebUIControllerInterfaceBinder() use
    WebUIBrowserInterfaceBrokerRegistry::ForWebUI().Add() calls which are
    much easier to work with. For example note how registration for
    NewTabFooterUI can now be guarded by
    base::FeatureList::IsEnabled(ntp_features::kNtpFooter) whereas
    previously this was not possible (not practical).
    
    Bug: 452983498
    Change-Id: Ic84bbc423b0bed5140bb8856df0940e6108967a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7056843
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1532562}

--- chrome/browser/chrome_browser_interface_binders_webui.cc
+++ chrome/browser/chrome_browser_interface_binders_webui.cc
@@ -161,6 +161,7 @@ void PopulateChromeWebUIFrameInterfaceBrokers(
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
 #if !BUILDFLAG(IS_ANDROID)
+  PopulateChromeWebUIFrameInterfaceBrokersTrustedPartsDesktop(registry);
   PopulateChromeWebUIFrameInterfaceBrokersUntrustedPartsDesktop(registry);
 #endif  // !BUILDFLAG(IS_ANDROID)
 
