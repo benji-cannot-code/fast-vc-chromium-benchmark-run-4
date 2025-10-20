51430d09151c4b7dfbaf640fe477142a691037c2
EmiliaPaz <emiliapaz@chromium.org>
2025-10-20 14:33:27 -0700

    Reland: [Extensions] Add UpdateSiteAccess to extensions menu model
    This is a reland of crrev.com/c/7024121. Original CL was reverted due
    to a Desktop Android compilation error in the browser test fixed by:
      - Use BrowserWindowInterface instead of Browser
      - Include web contents header
    
    This CL also changes the test target for the browser test. Originally,
    it was added under 'browser_tests`. However, the test should be cross-
    platform. Thus, now the CL is added to 'platform_browser_tests.
    
    Original change's description:
    > [Extensions] Add UpdateSiteAccess to extensions menu model
    >
    > Move the extensions logic handling of OnSiteAccessSelected on the
    > extensions menu from the platform delegate views to the platform-agnostic menu model.
    >
    > Bug: 449814184
    > Change-Id: Ib6136a7cc2d1e888b3df48de61c1c09b4944781c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7024121
    > Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    > Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1531010}
    
    Change-Id: I99b977b71cf5f2b21d48d9b1436b70749b6fb581
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7058814
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1532545}

--- chrome/browser/ui/extensions/extensions_menu_view_model.h
+++ chrome/browser/ui/extensions/extensions_menu_view_model.h
@@ -8,9 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "extensions/browser/permissions_manager.h"
+#include "extensions/common/extension_id.h"
 
-class ExtensionsMenuViewPlatformDelegate;
+namespace content {
+class WebContents;
+}  // namespace content
+
 class BrowserWindowInterface;
+class ExtensionsMenuViewPlatformDelegate;
 
 // The platform agnostic controller for the extensions menu.
 // TODO(crbug.com/449814184): Move the observers from
@@ -18,7 +23,7 @@ class BrowserWindowInterface;
 class ExtensionsMenuViewModel
     : public extensions::PermissionsManager::Observer {
  public:
-  explicit ExtensionsMenuViewModel(
+  ExtensionsMenuViewModel(
       BrowserWindowInterface* browser,
       std::unique_ptr<ExtensionsMenuViewPlatformDelegate> platform_delegate);
   ExtensionsMenuViewModel(const ExtensionsMenuViewModel&) = delete;
@@ -29,6 +34,11 @@ class ExtensionsMenuViewModel
   // PermissionsManager::Observer:
   void OnHostAccessRequestAdded(const extensions::ExtensionId& extension_id,
                                 int tab_id) override;
+
+  // Updates the extension's site access for the current site.
+  void UpdateSiteAccess(
+      const extensions::ExtensionId& extension_id,
+      extensions::PermissionsManager::UserSiteAccess site_access);
 
  private:
   content::WebContents* GetActiveWebContents();
