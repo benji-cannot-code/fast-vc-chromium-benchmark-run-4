d53481568416f8d966e1c868e021290c4f7b6da3
Andrew Grieve <agrieve@chromium.org>
2025-10-20 08:42:11 -0700

    licenses.py: Code health refactor
    Behaviour Changes:
    * Adds "list" command
    * Sort by case-insensitive names
    * Removes ability to output to stdout
    * Add additional_readme_paths.json files to depfile
    * Fix SPDX command failing when license file is not valid utf-8
    
    Refactor-type changes:
    * Use argparse subparsers
    * Share discovery code with all sub-commands
    * Rewrite tests as integration tests /w goldens
    
    Tested:
    * Ensured about:credits didn't change
      * Except for some reason a single license did change its line endings...
    
    Bug: None
    Change-Id: Ic4f5edcb4a208350592899866e391636f3d0f8a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7047098
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Rachael Newitt <renewitt@google.com>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1532301}

---
+++ tools/licenses/tests/mock_gn.py
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env python3
+# Copyright 2025 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import argparse
+import sys
+
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('cmd', choices=['desc'])
+  parser.add_argument('--root', required=True)
+  parser.add_argument('outdir')
+  parser.add_argument('gn_target', choices=['//foo'])
+  parser.add_argument('what', choices=['deps'])
+  parser.add_argument('--as', choices=['buildfile'], required=True)
+  parser.add_argument('--all', required=True, action='store_true')
+  args = parser.parse_args()
+
+  print(f'{args.root}/third_party/sample3/BUILD.gn')
+  print(f'{args.root}/third_party/sample2/BUILD.gn')
+  print(f'{args.root}/third_party/pruned/BUILD.gn')
+  print(f'{args.root}/some/other/dir/BUILD.gn')
+  return 0
+
+
+if __name__ == '__main__':
+  sys.exit(main())
