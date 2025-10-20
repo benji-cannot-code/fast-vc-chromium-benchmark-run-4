cd986faf0d836f0adf57daaf1857e5cb42661d82
Sarah Krakowiak <skrakowi@chromium.org>
2025-10-20 07:44:35 -0700

    Enable notifications global cache list by default
    Bug: 427931569
    Change-Id: Ia5719710a7d2a4e5e7722b5568e9bc9c96f19c1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7062993
    Reviewed-by: Nwokedi Idika <nwokedi@chromium.org>
    Commit-Queue: Nwokedi Idika <nwokedi@chromium.org>
    Auto-Submit: Sarah Krakowiak <skrakowi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1532267}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -215,7 +215,7 @@ BASE_FEATURE(kExternalAppRedirectTelemetry,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kGlobalCacheListForGatingNotificationProtections,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kGooglePlayProtectInApkTelemetry,
              "SafeBrowsingGooglePlayProtectInApkTelemetry",
