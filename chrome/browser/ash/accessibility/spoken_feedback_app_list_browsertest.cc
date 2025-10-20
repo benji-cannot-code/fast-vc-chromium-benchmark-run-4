5eeb9b027c3898fcd9ab685f8a7a64496c55ec36
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2025-10-20 03:32:06 -0700

    Revert "Reland: Add finch field trial for chromevox in mv3"
    This reverts commit f8823534986814cc0cbeecf0c225c214c3f5eaf7.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5893008139485184
    
    Sample build with failed test: https://ci.chromium.org/b/8700727151298446433
    Affected test(s):
    [ninja://chrome/test:browser_tests/QuickInsertAccessibilityBrowserTest.SetDescendantAnnouncesDescendantAfterKeyEvent](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Ftest:browser_tests%2FQuickInsertAccessibilityBrowserTest.SetDescendantAnnouncesDescendantAfterKeyEvent?q=VHash%3Addee393e1fa4b060)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5893008139485184&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F7047197&type=BUG
    
    Original change's description:
    > Reland: Add finch field trial for chromevox in mv3
    >
    > This is a reland of https://crrev.com/c/6962374. The original change
    > was reverted due to failing msan tests, which we are disabling for now
    > to unblock the finch rollout. Small adjustments have been made to two
    > test suites to ensure they pass if ChromeVox in mv3 is enabled (which
    > happens for a small number of builders when the finch field trial
    > is added).
    >
    > Bug: 433771715
    > Change-Id: Ic893558d472b5aa50039be437afdcebf2accd0d6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7047197
    > Reviewed-by: Katie Dektar <katie@chromium.org>
    > Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1531639}
    >
    
    Bug: 433771715
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I2abf70b166203f8735d91ed7248a2f4fd19b2e89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7058030
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Reviewed-by: Mahmoud Rashad <mmrashad@google.com>
    Owners-Override: Mahmoud Rashad <mmrashad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1532173}

--- chrome/browser/ash/accessibility/spoken_feedback_app_list_browsertest.cc
+++ chrome/browser/ash/accessibility/spoken_feedback_app_list_browsertest.cc
@@ -200,6 +200,12 @@ class SpokenFeedbackAppListBaseTest : public LoggedInSpokenFeedbackTest {
     AppListTestApi().DisableAppListNudge(true);
     AppListControllerImpl::SetSunfishNudgeDisabledForTest(true);
 
+    scoped_feature_list_.InitWithFeatures(
+        {features::kLauncherSearchControl,
+         features::kFeatureManagementLocalImageSearch},
+        {features::kScannerDogfood, features::kSunfishFeature,
+         ash::assistant::features::kEnableNewEntryPoint});
+
     LoggedInSpokenFeedbackTest::SetUp();
   }
 
@@ -209,8 +215,6 @@ class SpokenFeedbackAppListBaseTest : public LoggedInSpokenFeedbackTest {
   }
 
   void SetUpCommandLine(base::CommandLine* command_line) override {
-    LoggedInSpokenFeedbackTest::SetUpCommandLine(command_line);
-
     if (variant_ == kTestAsGuestUser) {
       command_line->AppendSwitch(switches::kGuestSession);
       command_line->AppendSwitch(::switches::kIncognito);
@@ -218,12 +222,6 @@ class SpokenFeedbackAppListBaseTest : public LoggedInSpokenFeedbackTest {
       command_line->AppendSwitchASCII(
           switches::kLoginUser, user_manager::GuestAccountId().GetUserEmail());
     }
-
-    scoped_feature_list_.InitWithFeatures(
-        {features::kLauncherSearchControl,
-         features::kFeatureManagementLocalImageSearch},
-        {features::kScannerDogfood, features::kSunfishFeature,
-         ash::assistant::features::kEnableNewEntryPoint});
   }
 
   void SetUpOnMainThread() override {
@@ -1081,12 +1079,6 @@ IN_PROC_BROWSER_TEST_P(SpokenFeedbackAppListSearchTest, VocalizeResultCount) {
 }
 
 IN_PROC_BROWSER_TEST_P(SpokenFeedbackAppListSearchTest, SearchCategoryFilter) {
-  if (GetParam().manifest_version() == ManifestVersion::kThree) {
-    // TODO(https://crbug.com/433771715): This test is failing on MSAN builds
-    // when ChromeVox mv3 is enabled.
-    return;
-  }
-
   chromevox_test_utils()->EnableChromeVox();
   ShowAppList();
 
