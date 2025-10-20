c0f97cd80952a7f93bc611117a2c720c98d3289f
thefrog <thefrog@chromium.org>
2025-10-20 07:39:48 -0700

    Deprecate SafeBrowsing.BrowserThrottle.CheckerOnIOLifetime
    Fixed: 445167942
    Change-Id: I0fc9cfbd5812c64f0cbad07c5ccc6751682753a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7055742
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1532263}

--- components/safe_browsing/content/browser/url_checker_holder.cc
+++ components/safe_browsing/content/browser/url_checker_holder.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/scoped_refptr.h"
-#include "base/metrics/histogram_functions.h"
 #include "components/safe_browsing/content/browser/web_ui/safe_browsing_ui.h"
 #include "components/safe_browsing/core/browser/hashprefix_realtime/hash_realtime_service.h"
 #include "components/safe_browsing/core/browser/realtime/url_lookup_service_base.h"
@@ -81,7 +80,6 @@ UrlCheckerHolder::UrlCheckerHolder(
       url_lookup_service_(url_lookup_service),
       hash_realtime_service_(hash_realtime_service),
       hash_realtime_selection_(hash_realtime_selection),
-      creation_time_(base::TimeTicks::Now()),
       is_async_check_(is_async_check),
       check_allowlist_before_hash_database_(
           check_allowlist_before_hash_database),
@@ -90,9 +88,6 @@ UrlCheckerHolder::UrlCheckerHolder(
 
 UrlCheckerHolder::~UrlCheckerHolder() {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  base::UmaHistogramMediumTimes(
-      "SafeBrowsing.BrowserThrottle.CheckerOnIOLifetime",
-      base::TimeTicks::Now() - creation_time_);
 }
 
 void UrlCheckerHolder::Start(const StartParams& params) {
