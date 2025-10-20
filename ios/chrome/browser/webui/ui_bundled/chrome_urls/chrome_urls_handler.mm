ead95b310e7eb6f2ad35f208561625c295b447fc
Sylvain Defresne <sdefresne@chromium.org>
2025-10-20 09:52:47 -0700

    [ios] Use std::string_view in ios/chrome/browser/webui/ui_bundled
    In preparation for converting some globals from `const char[]` to
    `inline constexpr std::string_view` use `std::string_view` instead
    of `const std::string&`.
    
    This also removes some unnecessary copies (as there is a implicit
    constructor for `std::string` from `const char*`).
    
    Bug: none
    Change-Id: Ib459d10e5df2e1e8dce03833584974e5931ece10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7054343
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1532348}

--- ios/chrome/browser/webui/ui_bundled/chrome_urls/chrome_urls_handler.mm
+++ ios/chrome/browser/webui/ui_bundled/chrome_urls/chrome_urls_handler.mm
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chrome_urls {
 
 namespace {
-bool IsWebUIInternal(const std::string& host) {
+bool IsWebUIInternal(std::string_view host) {
   return host == commerce::kChromeUICommerceInternalsHost ||
          host == optimization_guide_internals::
                      kChromeUIOptimizationGuideInternalsHost ||
