dcf110b0a6347a50dd441f35f5c8bbb83ab89388
Dale Curtis <dalecurtis@chromium.org>
2025-10-20 12:20:12 -0700

    Restore VideoDecodeBatching feature that's used by ChromeOS
    This reverts commit 7ee4c9f65cfb4bd28a7cffb1cdee267a636600f5. It
    turns out this is used by ChromeOS to disable the feature on boards
    where performance is very negatively impacted.
    
    The things changed from a pure revert are:
    * Note added to media_switches.cc about why it must be kept.
    * Kept the deletion of the field trial config since it's enabled.
    
    R=tmathmeyer
    
    Bug: 40942038
    Fixed: 453477703
    Change-Id: I4d041da50f7fc6633ee96b95675dacbe3483825d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7063847
    Commit-Queue: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1532459}

--- media/filters/video_decoder_stream_unittest.cc
+++ media/filters/video_decoder_stream_unittest.cc
@@ -623,7 +623,8 @@ TEST_P(VideoDecoderStreamTest, Read_AfterReset) {
 
 // Tests config changes with increasing sizes work correctly.
 TEST_P(VideoDecoderStreamTest, ConfigChangeIncreasingSize) {
-  if (GetParam().parallel_decoding != 1) {
+  if (base::FeatureList::IsEnabled(kVideoDecodeBatching) &&
+      GetParam().parallel_decoding != 1) {
     // Fake demuxer allows reading over different configs when batch decoding is
     // enabled, so we need to skip this test.
     return;
@@ -665,7 +666,8 @@ TEST_P(VideoDecoderStreamTest, ConfigChangeIncreasingSize) {
 
 // Tests config changes with decreasing sizes work correctly.
 TEST_P(VideoDecoderStreamTest, ConfigChangeDecreasingSize) {
-  if (GetParam().parallel_decoding != 1) {
+  if (base::FeatureList::IsEnabled(kVideoDecodeBatching) &&
+      GetParam().parallel_decoding != 1) {
     // Fake demuxer allows reading over different configs when batch decoding is
     // enabled, so we need to skip this test.
     return;
@@ -688,7 +690,8 @@ TEST_P(VideoDecoderStreamTest, ConfigChangeDecreasingSize) {
 // Tests that the decoder stream will remain on the same decoder when elided
 // EOS processing is enabled.
 TEST_P(VideoDecoderStreamTest, ConfigChangeElidedEOS) {
-  if (GetParam().parallel_decoding != 1) {
+  if (base::FeatureList::IsEnabled(kVideoDecodeBatching) &&
+      GetParam().parallel_decoding != 1) {
     // Fake demuxer allows reading over different configs when batch decoding is
     // enabled, so we need to skip this test.
     return;
@@ -835,6 +838,8 @@ TEST_P(VideoDecoderStreamTest, Read_BlockedDemuxerAndDecoder) {
 }
 
 TEST_P(VideoDecoderStreamTest, BatchDecodingWithPlatformDecoder) {
+  base::test::ScopedFeatureList scoped_feature_list;
+  scoped_feature_list.InitAndEnableFeature(kVideoDecodeBatching);
   int parallel_decodings = GetParam().parallel_decoding;
 
   Initialize();
@@ -855,6 +860,9 @@ TEST_P(VideoDecoderStreamTest, BatchDecodingWithPlatformDecoder) {
 }
 
 TEST_P(VideoDecoderStreamTest, NoBatchDecodingWithNonPlatformDecoder) {
+  base::test::ScopedFeatureList scoped_feature_list;
+  scoped_feature_list.InitAndEnableFeature(kVideoDecodeBatching);
+
   Initialize();
   // Set the decoder as not platform decoder, so that it prevents single
   // demuxer read to return multiple DecoderBuffers.
