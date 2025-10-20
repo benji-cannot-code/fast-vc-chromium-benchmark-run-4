aec09a9c8237ce37cedbcde975e0b6fc89f9da58
Colin Blundell <blundell@chromium.org>
2025-10-20 01:23:44 -0700

    [//gpu] Remove CanDecodeWithHWAcceleration() from ContextSupport
    This method now always returns false.
    
    Bug: 450466845
    Change-Id: I6278d4d93803b3bac2ed051bd2f91a639cdd8501
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7046230
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1532118}

--- gpu/command_buffer/client/context_support.h
+++ gpu/command_buffer/client/context_support.h
@@ -19,10 +19,6 @@ namespace gfx {
 class GpuFence;
 }
 
-namespace cc {
-struct ImageHeaderMetadata;
-}
-
 namespace gpu {
 
 struct SyncToken;
@@ -86,11 +82,6 @@ class ContextSupport {
   virtual void DeleteTransferCacheEntry(uint32_t type, uint32_t id) = 0;
 
   virtual unsigned int GetTransferBufferFreeSize() const = 0;
-
-  // Determines if |image_metadata| corresponds to an image that can be decoded
-  // using hardware decode acceleration.
-  virtual bool CanDecodeWithHardwareAcceleration(
-      const cc::ImageHeaderMetadata* image_metadata) const = 0;
 
  protected:
   ContextSupport() = default;
