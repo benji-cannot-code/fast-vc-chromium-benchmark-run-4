586996fc7ea45cce602254dd41035d3828d8728d
Shlomi Ben-Shimon <shlomib@google.com>
2025-10-21 10:51:44 -0700

    Record playback manual close related statistics.
    1. The playback location when a manual close was triggered (in overview/classic).
    2. For playbacks that were closed during initialization, record how long the user waited before closing.
    
    Analysis doc: https://docs.google.com/document/d/1QNObJ3txG55ijcBRnqSjew72WjwGvgcz3J6MLtzEpe0/edit?pli=1&resourcekey=0-zEdAdo6PEB_7UwwxSkUpuA&tab=t.0#heading=h.qih76ghsusyx
    
    Bug: 448315814
    
    Change-Id: I8aa5d48705049a1fd7810a2438698954a9ff0bf0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7058013
    Auto-Submit: Shlomi Ben Shimon <shlomib@google.com>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Reviewed-by: Andrea Gomez <andreaxg@google.com>
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1533073}

--- chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/player/PlayerMediator.java
+++ chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/player/PlayerMediator.java
@@ -45,6 +45,8 @@ import java.util.concurrent.TimeUnit;
 class PlayerMediator implements InteractionHandler {
     private static final long SEEK_BACK_NANOS = -10 * 1_000_000_000L;
     private static final long SEEK_FORWARD_NANOS = 10 * 1_000_000_000L;
+    private static final int NANOS_IN_MILLS = 1_000_000;
+
     private final PlayerCoordinator mCoordinator;
     private final Delegate mDelegate;
     private final PropertyModel mModel;
@@ -68,6 +70,8 @@ class PlayerMediator implements InteractionHandler {
     private boolean mScreenLocked;
     private long mLastStartTimeMillisLockedScreen;
     private long mTotalTimeMillisLockedScreen;
+
+    private long mPlaybackCreationStartTimeMillis;
 
     private final PlaybackListener mPlaybackListener =
             new PlaybackListener() {
@@ -255,6 +259,9 @@ class PlayerMediator implements InteractionHandler {
     }
 
     void setPlaybackState(@PlaybackListener.State int currentPlaybackState) {
+        if (currentPlaybackState == PLAYBACK_CREATION) {
+            mPlaybackCreationStartTimeMillis = mClock.currentTimeMillis();
+        }
         mModel.set(PlayerProperties.PLAYBACK_STATE, currentPlaybackState);
     }
 
@@ -309,6 +316,8 @@ class PlayerMediator implements InteractionHandler {
 
     @Override
     public void onCloseClick() {
+        reportCloseClickInfo();
+
         mCoordinator.closeClicked();
     }
 
@@ -572,5 +581,24 @@ class PlayerMediator implements InteractionHandler {
 
     void setClockForTesting(Clock clock) {
         mClock = clock;
+    }
+
+    private void reportCloseClickInfo() {
+        if (mModel.get(PlayerProperties.PLAYBACK_STATE) == ERROR) {
+            return;
+        }
+
+        PlaybackMode playbackMode =
+                PlaybackMode.fromValue(mModel.get(PlayerProperties.REQUESTED_PLAYBACK_MODE));
+
+        if (mModel.get(PlayerProperties.PLAYBACK_STATE) == PLAYBACK_CREATION) {
+            ReadAloudMetrics.recordPlaybackPositionInManualClose(0, playbackMode);
+            ReadAloudMetrics.recordTimeToMidLoadingManualClose(
+                    mClock.currentTimeMillis() - mPlaybackCreationStartTimeMillis, playbackMode);
+            return;
+        }
+
+        ReadAloudMetrics.recordPlaybackPositionInManualClose(
+                mModel.get(PlayerProperties.ELAPSED_NANOS) / NANOS_IN_MILLS, playbackMode);
     }
 }
