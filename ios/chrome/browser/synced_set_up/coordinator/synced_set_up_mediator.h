6a2e38f8b69e8a5f836debfa6ac525e3b72ccb4f
Eric Ekey <ericekey@google.com>
2025-10-21 07:11:22 -0700

    [iOS][SyncedSetUp] Stub Mediator for Synced Set Up
    This change stubs the mediator for the new Synced Set Up experience
    (DD: go/synced-set-up-dd).
    
    R=bwwilliams@google.com, gambard@chromium.org
    
    Fixed: 453701499
    Change-Id: I63426699815322c3dda99354748874cd091b4165
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7064331
    Reviewed-by: Benjamin Williams <bwwilliams@google.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Eric Ekey <ericekey@google.com>
    Auto-Submit: Eric Ekey <ericekey@google.com>
    Cr-Commit-Position: refs/heads/main@{#1532920}

---
+++ ios/chrome/browser/synced_set_up/coordinator/synced_set_up_mediator.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SYNCED_SET_UP_COORDINATOR_SYNCED_SET_UP_MEDIATOR_H_
+#define IOS_CHROME_BROWSER_SYNCED_SET_UP_COORDINATOR_SYNCED_SET_UP_MEDIATOR_H_
+
+#import <Foundation/Foundation.h>
+
+namespace sync_preferences {
+class CrossDevicePrefTracker;
+}  // namespace sync_preferences
+
+// Mediator responsible for querying and applying tracked prefs on a synced
+// device.
+@interface SyncedSetUpMediator : NSObject
+
+- (instancetype)initWithPrefTracker:
+    (sync_preferences::CrossDevicePrefTracker*)tracker;
+
+- (instancetype)init NS_UNAVAILABLE;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_SYNCED_SET_UP_COORDINATOR_SYNCED_SET_UP_MEDIATOR_H_
---
+++ ios/chrome/browser/synced_set_up/coordinator/synced_set_up_mediator.mm
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/synced_set_up/coordinator/synced_set_up_mediator.h"
+
+#import "base/memory/raw_ptr.h"
+#import "components/sync_preferences/cross_device_pref_tracker/cross_device_pref_tracker.h"
+
+@implementation SyncedSetUpMediator {
+  // Tracker for retrieving cross device preferences.
+  raw_ptr<sync_preferences::CrossDevicePrefTracker> _prefTracker;
+}
+
+- (instancetype)initWithPrefTracker:
+    (sync_preferences::CrossDevicePrefTracker*)tracker {
+  if ((self = [super init])) {
+    CHECK(tracker);
+    _prefTracker = tracker;
+  }
+  return self;
+}
+
+@end
