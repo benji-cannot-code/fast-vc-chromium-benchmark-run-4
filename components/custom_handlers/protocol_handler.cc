df0b7c3e25ff2df8d6557bf8fc9ad0e4407f4e63
Solomon Kinard <solomonkinard@chromium.org>
2025-10-21 07:40:24 -0700

    CustomHandlers: Spell check and change function argument comment style
    https://google.github.io/styleguide/cppguide.html#Function_Argument_Comments
    
    Bug: 346952289
    Change-Id: I2e8f2d58cadf7ed3be66f10c06d409de9f733c6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7062920
    Reviewed-by: Elias Klim <elklm@chromium.org>
    Commit-Queue: Elias Klim <elklm@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1532943}

--- components/custom_handlers/protocol_handler.cc
+++ components/custom_handlers/protocol_handler.cc
@@ -74,7 +74,7 @@ ProtocolHandler ProtocolHandler::CreateWebAppProtocolHandler(
     const GURL& url,
     const std::string& app_id) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
-  return ProtocolHandler(protocol, url, app_id, /*extendion_id*/ std::nullopt,
+  return ProtocolHandler(protocol, url, app_id, /*extension_id=*/std::nullopt,
                          base::Time::Now(),
                          blink::ProtocolHandlerSecurityLevel::kStrict);
 }
@@ -86,7 +86,7 @@ ProtocolHandler ProtocolHandler::CreateExtensionProtocolHandler(
     const std::string& extension_id) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
   return ProtocolHandler(
-      protocol, url, /*app_id*/ std::nullopt, extension_id, base::Time::Now(),
+      protocol, url, /*app_id=*/std::nullopt, extension_id, base::Time::Now(),
       blink::ProtocolHandlerSecurityLevel::kExtensionFeatures);
 }
 
@@ -152,7 +152,7 @@ ProtocolHandler ProtocolHandler::CreateProtocolHandler(
     if (app_id_val->is_string())
       app_id = app_id_val->GetString();
     return ProtocolHandler(protocol, GURL(url), app_id,
-                           /*extension_id*/ std::nullopt, time, security_level);
+                           /*extension_id=*/std::nullopt, time, security_level);
   }
 
   if (const base::Value* extension_id_val = value.Find("extension_id")) {
@@ -160,7 +160,7 @@ ProtocolHandler ProtocolHandler::CreateProtocolHandler(
     if (extension_id_val->is_string()) {
       extension_id = extension_id_val->GetString();
     }
-    return ProtocolHandler(protocol, GURL(url), /*app_id*/ std::nullopt,
+    return ProtocolHandler(protocol, GURL(url), /*app_id=*/std::nullopt,
                            extension_id, time, security_level);
   }
 
