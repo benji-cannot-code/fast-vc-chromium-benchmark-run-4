0722226eab5a7bfd6a692aaec71fcd2ff5e9bbdd
Jack Thiesen <jthiesen@chromium.org>
2025-10-21 15:42:48 -0700

    Implement BrowserWindowCreatorBridge.createBrowserWindowAsync()
    This change creates and tests a new function that creates a browser
    window on Android asynchronously in BrowserWindowCreatorBridge.
    
    Bug: 452059980
    Change-Id: Iddee1f6d7c2f4577ec85b47b0bf5ad1d3ecb08e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7047421
    Commit-Queue: Jack Thiesen <jthiesen@chromium.org>
    Reviewed-by: Linyu He <linyuh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1533286}

--- chrome/browser/ui/browser_window/internal/android/java/src/org/chromium/chrome/browser/ui/browser_window/ChromeAndroidTaskTrackerImpl.java
+++ chrome/browser/ui/browser_window/internal/android/java/src/org/chromium/chrome/browser/ui/browser_window/ChromeAndroidTaskTrackerImpl.java
@@ -252,6 +252,12 @@ final class ChromeAndroidTaskTrackerImpl implements ChromeAndroidTaskTracker {
         }
     }
 
+    Map<Integer, ChromeAndroidTask> getPendingTasksForTesting() {
+        synchronized (mTasksLock) {
+            return mPendingTasks;
+        }
+    }
+
     @GuardedBy("mTasksLock")
     private void removeInternalLocked(int taskId) {
         var taskRemoved = mTasks.remove(taskId);
