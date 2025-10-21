1b948cbd89088fedd9359ec9b7a09e175db42069
Jonathan Lee <jonathanjlee@google.com>
2025-10-21 16:21:21 -0700

    [code-health] Clean up stale `kFeedNoViewCache` feature
    The feature isn't consumed anywhere. The logic was probably already
    cleaned up.
    
    Fixed: 356236410
    Change-Id: I9300ff7351816f745d1f204eacaceb2294dc1c6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7068631
    Auto-Submit: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1533297}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -191,7 +191,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &feed::kFeedHeaderRemoval,
     &feed::kFeedImageMemoryCacheSizePercentage,
     &feed::kFeedLoadingPlaceholder,
-    &feed::kFeedNoViewCache,
     &feed::kFeedPerformanceStudy,
     &feed::kFeedRecyclerBinderUnmountOnDetach,
     &feed::kFeedShowSignInCommand,
