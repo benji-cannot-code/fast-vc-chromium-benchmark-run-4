3fd8ca4df15d1bad734e554fbe33da32258c021f
Ioana Pandele <ioanap@chromium.org>
2025-10-21 03:36:58 -0700

    Enable filling heuristics and permissions flags
    Bug: 453936824
    Change-Id: I1740bcac33b6dc73f1d0f66cf3ca57fc8ef16cf4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7063590
    Reviewed-by: Oleksandr Tara <otara@google.com>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1532847}

--- components/password_manager/core/browser/features/password_features.cc
+++ components/password_manager/core/browser/features/password_features.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace password_manager::features {
 #if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)  // Desktop
 BASE_FEATURE(kActorLogin, base::FEATURE_ENABLED_BY_DEFAULT);
-BASE_FEATURE(kActorLoginFillingHeuristics, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kActorLoginFillingHeuristics, base::FEATURE_ENABLED_BY_DEFAULT);
 #endif  // !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
 
 #if BUILDFLAG(IS_ANDROID)
@@ -75,7 +75,7 @@ BASE_FEATURE(kDownloadModelForPasswordChange,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 #if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)  // Desktop
-BASE_FEATURE(kEnableActorLoginPermissions, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kEnableActorLoginPermissions, base::FEATURE_ENABLED_BY_DEFAULT);
 #endif  // !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
 
 BASE_FEATURE(kFetchChangePasswordUrlForPasswordChange,
