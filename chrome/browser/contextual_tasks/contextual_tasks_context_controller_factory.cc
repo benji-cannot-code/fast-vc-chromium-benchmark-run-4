d5e15e25151ab8fd185b780ee47eea0982534a57
Sophie Chang <sophiechang@chromium.org>
2025-10-20 18:21:34 -0700

    Add initial service to determine tab context for a given piece of text
    Bug: 452056256
    Change-Id: I31a39c7ff0f667c9809bf92fe8d670d6092207a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7064262
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1532658}

--- chrome/browser/contextual_tasks/contextual_tasks_context_controller_factory.cc
+++ chrome/browser/contextual_tasks/contextual_tasks_context_controller_factory.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "components/contextual_tasks/public/contextual_tasks_service.h"
 #include "components/contextual_tasks/public/features.h"
-#include "components/omnibox/browser/aim_eligibility_service.h"
 
 namespace contextual_tasks {
 
