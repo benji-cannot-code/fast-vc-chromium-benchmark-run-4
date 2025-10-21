5d7cd4c13d11fbcc57227c6da45c2379f845213b
Gauthier Ambard <gambard@chromium.org>
2025-10-21 08:38:59 -0700

    [iOS] Add a content configuration for an info button
    This CL adds a new content configuration, used when adding an Info
    button to a cell.
    
    Bug: 443034511
    Change-Id: I10189470f018b6b6e70dcd377fcbb1f9fd525ed2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7062941
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1532984}

---
+++ ios/chrome/browser/shared/ui/table_view/content_configuration/info_button_content_view.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SHARED_UI_TABLE_VIEW_CONTENT_CONFIGURATION_INFO_BUTTON_CONTENT_VIEW_H_
+#define IOS_CHROME_BROWSER_SHARED_UI_TABLE_VIEW_CONTENT_CONFIGURATION_INFO_BUTTON_CONTENT_VIEW_H_
+
+#import <UIKit/UIKit.h>
+
+#import "ios/chrome/browser/shared/ui/table_view/content_configuration/chrome_content_view.h"
+
+@class InfoButtonContentConfiguration;
+
+// A view that displays a single info button, leading-aligned.
+@interface InfoButtonContentView : UIView <ChromeContentView>
+
+// Returns the view, configured with `configuration`.
+- (instancetype)initWithConfiguration:
+    (InfoButtonContentConfiguration*)configuration;
+
+// Returns the info button, to be used in testing only.
+- (UIButton*)infoButtonForTesting;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_SHARED_UI_TABLE_VIEW_CONTENT_CONFIGURATION_INFO_BUTTON_CONTENT_VIEW_H_
