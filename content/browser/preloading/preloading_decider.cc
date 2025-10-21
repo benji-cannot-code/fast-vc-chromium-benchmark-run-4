bf5cc2b4a29d8e691956a73ef8fb6561a9470d56
Takashi Nakayama <tnak@chromium.org>
2025-10-20 20:14:45 -0700

    Preloading: Hardcode EagernessSet values for predictors
    This CL removes dynamic configuration for predictor eagerness levels via
    feature parameters, as these values are now static.
    
    A feature param `kPreloadingModerateViewportHeuristics` is also removed
    by this change as it is not used by any experiments, the associated
    feature is now clarified as specific for the "moderate" eagerness.
    
    Change-Id: I6acfb48f9e53d9c13173e273d7b0d9ecc293ae93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7063970
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Takashi Nakayama <tnak@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1532707}

--- content/browser/preloading/preloading_decider.cc
+++ content/browser/preloading/preloading_decider.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 #include <cmath>
-#include <string_view>
 #include <vector>
 
 #include "base/check_is_test.h"
@@ -15,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/enum_set.h"
 #include "base/feature_list.h"
 #include "base/numerics/safe_conversions.h"
-#include "base/strings/string_split.h"
 #include "content/browser/devtools/devtools_instrumentation.h"
 #include "content/browser/devtools/devtools_preload_storage.h"
 #include "content/browser/preloading/prefetch/no_vary_search_helper.h"
@@ -41,20 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 namespace {
-
-content::PreloadingDecider::EagernessSet EagernessSetFromFeatureParam(
-    std::string_view value) {
-  content::PreloadingDecider::EagernessSet set;
-  for (std::string_view piece : base::SplitStringPiece(
-           value, ",", base::TRIM_WHITESPACE, base::SPLIT_WANT_NONEMPTY)) {
-    if (piece == "conservative") {
-      set.Put(blink::mojom::SpeculationEagerness::kConservative);
-    } else if (piece == "moderate") {
-      set.Put(blink::mojom::SpeculationEagerness::kModerate);
-    }
-  }
-  return set;
-}
 
 void OnPrefetchDestroyed(WeakDocumentPtr document, const GURL& url) {
   PreloadingDecider* preloading_decider =
@@ -88,10 +72,7 @@ bool PredictionOccursInOtherWebContents(
 class PreloadingDecider::BehaviorConfig {
  public:
   BehaviorConfig()
-      : eager_viewport_heuristic_eagerness_{blink::mojom::SpeculationEagerness::
-                                                kEager},
-        ml_model_eagerness_{blink::mojom::SpeculationEagerness::kModerate},
-        ml_model_enacts_candidates_(
+      : ml_model_enacts_candidates_(
             blink::features::kPreloadingModelEnactCandidates.Get()),
         ml_model_prefetch_moderate_threshold_{std::clamp(
             blink::features::kPreloadingModelPrefetchModerateThreshold.Get(),
@@ -115,12 +96,6 @@ class PreloadingDecider::BehaviorConfig {
     }
 
     CHECK(pointer_down_eagerness_.HasAll(pointer_hover_eagerness_));
-
-    static const base::FeatureParam<std::string> kViewportHeuristicEagerness{
-        &blink::features::kPreloadingModerateViewportHeuristics,
-        "viewport_heuristic_eagerness", "moderate"};
-    moderate_viewport_heuristic_eagerness_ =
-        EagernessSetFromFeatureParam(kViewportHeuristicEagerness.Get());
   }
 
   EagernessSet EagernessSetForPredictor(
@@ -130,12 +105,12 @@ class PreloadingDecider::BehaviorConfig {
     } else if (predictor == preloading_predictor::kUrlPointerHoverOnAnchor) {
       return pointer_hover_eagerness_;
     } else if (predictor == preloading_predictor::kModerateViewportHeuristic) {
-      return moderate_viewport_heuristic_eagerness_;
+      return EagernessSet{blink::mojom::SpeculationEagerness::kModerate};
     } else if (predictor == preloading_predictor::kEagerViewportHeuristic) {
-      return eager_viewport_heuristic_eagerness_;
+      return EagernessSet{blink::mojom::SpeculationEagerness::kEager};
     } else if (predictor ==
                preloading_predictor::kPreloadingHeuristicsMLModel) {
-      return ml_model_eagerness_;
+      return EagernessSet{blink::mojom::SpeculationEagerness::kModerate};
     } else {
       NOTREACHED() << "unexpected predictor " << predictor.name() << "/"
                    << predictor.ukm_value();
@@ -183,9 +158,6 @@ class PreloadingDecider::BehaviorConfig {
 
   EagernessSet pointer_down_eagerness_;
   EagernessSet pointer_hover_eagerness_;
-  EagernessSet moderate_viewport_heuristic_eagerness_;
-  const EagernessSet eager_viewport_heuristic_eagerness_;
-  const EagernessSet ml_model_eagerness_;
   const bool ml_model_enacts_candidates_ = false;
   const PreloadingConfidence ml_model_prefetch_moderate_threshold_{
       kNoThreshold};
