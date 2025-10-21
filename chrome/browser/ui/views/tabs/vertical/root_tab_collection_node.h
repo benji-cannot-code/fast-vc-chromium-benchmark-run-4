44384a303f50f30035f27aa447860f398016bac7
Charles Meng <charlesmeng@chromium.org>
2025-10-21 14:33:55 -0700

    [Vertical Tabs] Reland "Refactor add_child_to_node_ and Initialize"
    Store the add_child_to_node_ callback on the parent only. Changes
    Initialize flow so that it only appends the child's view to the parent
    after the child is initialized (i.e. all of the child's children will
    have been added to the child).
    
    Fixed: 450304539
    Change-Id: I591d76d0bcd37ec7afcfbd74d8c9c556a326afe6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7050942
    Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    Commit-Queue: Charles Meng <charlesmeng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1533246}

--- chrome/browser/ui/views/tabs/vertical/root_tab_collection_node.h
+++ chrome/browser/ui/views/tabs/vertical/root_tab_collection_node.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_observation.h"
 #include "base/types/expected.h"
 #include "chrome/browser/ui/tabs/tab_strip_api/observation/tab_strip_api_observer.h"
+#include "chrome/browser/ui/tabs/tab_strip_api/tab_strip_api_data_model.mojom-forward.h"
 #include "chrome/browser/ui/views/tabs/vertical/tab_collection_node.h"
 #include "mojo/public/cpp/bindings/associated_receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
@@ -24,9 +25,9 @@ class RootTabCollectionNode
     : public TabCollectionNode,
       public tabs_api::observation::TabStripApiObserver {
  public:
-  explicit RootTabCollectionNode(tabs_api::TabStripService* service_register,
-                                 views::View* parent_view,
-                                 CustomAddChildView add_node_to_parent_);
+  explicit RootTabCollectionNode(
+      tabs_api::TabStripService* service_register,
+      CustomAddChildViewCallback add_node_view_to_parent);
   ~RootTabCollectionNode() override;
 
   // tabs_api::observation::TabStripApiObserver
@@ -42,6 +43,14 @@ class RootTabCollectionNode
                                collection_created_event) override;
 
  private:
+  // TabCollectionNode needs to be initialized with data, however we need the
+  // container's children later in the constructor of RootTabCollectionNode.
+  // Use this helper so that we only have to call GetTabs once.
+  explicit RootTabCollectionNode(
+      tabs_api::TabStripService* tab_strip_service,
+      tabs_api::mojom::ContainerPtr container,
+      CustomAddChildViewCallback add_node_view_to_parent);
+
   base::ScopedObservation<tabs_api::TabStripService,
                           tabs_api::observation::TabStripApiObserver>
       service_observer_{this};
