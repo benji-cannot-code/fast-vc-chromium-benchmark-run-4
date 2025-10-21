d48b02c77a14af0961f948d810c006dcbb9ac18d
Carlos Knippschild <carlosk@chromium.org>
2025-10-21 09:42:50 -0700

    Add histogram to track opens of chrome://glic in a regular tab
    Which is supposed to be used only for development purposes; this
    histogram should help in understanding if its being overused.
    
    Change-Id: I6a6a6964992911ea7057f008b1e4eca267189b22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7065433
    Auto-Submit: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Nick Birnie <birnie@google.com>
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1533021}

--- chrome/browser/glic/host/glic_page_handler.cc
+++ chrome/browser/glic/host/glic_page_handler.cc
@@ -798,6 +798,7 @@ class GlicWebClientHandler : public glic::mojom::WebClientHandler,
       panel_opening_data->panel_state = host().GetPanelState(this).Clone();
       panel_opening_data->invocation_source =
           mojom::InvocationSource::kUnsupported;
+      base::UmaHistogramBoolean("Glic.Host.OpenedInRegularTab", true);
       web_client_->NotifyPanelWillOpen(std::move(panel_opening_data),
                                        base::DoNothing());
     }
@@ -1350,6 +1351,7 @@ class GlicWebClientHandler : public glic::mojom::WebClientHandler,
 
   void PanelWillOpen(glic::mojom::PanelOpeningDataPtr panel_opening_data,
                      PanelWillOpenCallback done) override {
+    base::UmaHistogramBoolean("Glic.Host.OpenedInRegularTab", false);
     web_client_->NotifyPanelWillOpen(
         std::move(panel_opening_data),
         base::BindOnce(
