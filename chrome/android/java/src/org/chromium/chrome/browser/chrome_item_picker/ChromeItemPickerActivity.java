c264641d7ced95d880523eed1e481a2d0c17454d
Bhuvana Betini <bbetini@google.com>
2025-10-21 14:38:25 -0700

    [ChromeItemPicker] Sets Window ID to be last used Window if Invalid
    This CL sets the window ID to be the last used window if the intent
    passes in an invalid window ID.
    
    Bug: 453748939
    Change-Id: I3986f7c23d280b58a6ed396ff166a716813d9139
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7063540
    Commit-Queue: Bhuvana Betini <bbetini@google.com>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1533247}

--- chrome/android/java/src/org/chromium/chrome/browser/chrome_item_picker/ChromeItemPickerActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/chrome_item_picker/ChromeItemPickerActivity.java
@@ -17,6 +17,7 @@ import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.IntentHandler;
 import org.chromium.chrome.browser.SnackbarActivity;
+import org.chromium.chrome.browser.multiwindow.MultiWindowUtils;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.chrome.browser.tabwindow.TabWindowManager;
 
@@ -36,15 +37,17 @@ public class ChromeItemPickerActivity extends SnackbarActivity {
         ViewGroup containerView = findViewById(R.id.chrome_item_picker_container);
         ViewGroup rootView = containerView;
 
-        // TODO: Set mWindowId to be the most recently used window.
         mWindowId =
                 IntentUtils.safeGetIntExtra(
                         getIntent(),
                         IntentHandler.EXTRA_WINDOW_ID,
                         TabWindowManager.INVALID_WINDOW_ID);
         if (mWindowId == TabWindowManager.INVALID_WINDOW_ID) {
-            mWindowId =
-                    savedInstanceState == null ? 0 : savedInstanceState.getInt("window_index", 0);
+            mWindowId = MultiWindowUtils.getLastAccessedWindowId();
+        }
+        if (mWindowId == TabWindowManager.INVALID_WINDOW_ID) {
+            handleFailureToShowPicker("Could not determine a valid target browser window ID.");
+            return;
         }
 
         mItemPickerCoordinator =
@@ -70,13 +73,16 @@ public class ChromeItemPickerActivity extends SnackbarActivity {
 
     private void handleModelFailure(@Nullable TabModelSelector tabModelSelector) {
         if (tabModelSelector == null) {
-            final String error = "Failed to launch activity.";
-            Log.e(TAG, error);
-            final Intent resultIntent = new Intent();
-            resultIntent.putExtra(IntentHandler.EXTRA_ITEM_PICKER_ERROR, error);
-            setResult(Activity.RESULT_CANCELED, resultIntent);
-            finish();
+            handleFailureToShowPicker("Failed to launch activity.");
             return;
         }
+    }
+
+    private void handleFailureToShowPicker(String error) {
+        Log.e(TAG, error);
+        final Intent resultIntent = new Intent();
+        resultIntent.putExtra(IntentHandler.EXTRA_ITEM_PICKER_ERROR, error);
+        setResult(Activity.RESULT_CANCELED, resultIntent);
+        finish();
     }
 }
