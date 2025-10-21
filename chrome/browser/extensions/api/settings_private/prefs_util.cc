2aa0c17db788bdfaa4f9c7bcad941ef9d6f00472
Michelle Abreo <michelleabreo@google.com>
2025-10-21 13:25:19 -0700

    Add toggle in settings for Web Actuation consent.
    This change introduces a new toggle to the Gemini settings page
    (chrome://settings/ai/gemini) for controlling the web actuation setting.
    This new "Let Gemini browse for you" setting allows users to opt-in to
    Gemini browser control.
    
    The implementation includes a new settings-toggle-button, an expandable
    section with more details. The strings are not finalized and are marked
    as translateable=false. Will update with finalized strings after they
    are approved on 10/20. The new UI is conditionally rendered based on the
    `webActuationFeatureEnabled_` flag. UMA metrics have been added to track
    the on/off toggle action as well as "Learn More" clicks for the new
    setting.
    
    https://screenshot.googleplex.com/7gkCqst223HUghB
    
    Figma:
    https://www.figma.com/design/PdXCsaWPS59ONmtOy58xPj/BlueDog--Code-Yellow--File-1-?node-id=3941-464527&t=kF1n3Xzn9PvDVrZO-1
    
    Bug: b:445429376
    Change-Id: Ia250e3949e32ec4b5cbece6d3436723d1716aa7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7015148
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Michelle Abreo <michelleabreo@google.com>
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1533203}

--- chrome/browser/extensions/api/settings_private/prefs_util.cc
+++ chrome/browser/extensions/api/settings_private/prefs_util.cc
@@ -1310,6 +1310,8 @@ const PrefsUtil::TypedPrefMap& PrefsUtil::GetAllowlistedKeys() {
         settings_api::PrefType::kDictionary;
     (*s_allowlist)[prefs::kGeminiSettings] =
         settings_api::PrefType::kNumber;
+    (*s_allowlist)[glic::prefs::kGlicUserEnabledActuationOnWeb] =
+        settings_api::PrefType::kBoolean;
   }
 #endif
 
--- chrome/browser/glic/host/glic_api_browsertest.cc
+++ chrome/browser/glic/host/glic_api_browsertest.cc
@@ -143,6 +143,8 @@ std::vector<std::string> GetTestSuiteNames() {
       "GlicApiTestWithMqlsIdGetterEnabled",
       "GlicApiTestWithMqlsIdGetterDisabled",
       "GlicApiTestRuntimeFeatureOff",
+      "GlicApiTestWithWebActuationSettingDisabled",
+      "GlicApiTestWithWebActuationSettingEnabled",
   };
 }
 
@@ -295,6 +297,27 @@ class GlicApiTestWithDefaultTabContextDisabled : public GlicApiTestWithOneTab {
   GlicApiTestWithDefaultTabContextDisabled() {
     feature_list_.InitWithFeatures({},
                                    {features::kGlicDefaultTabContextSetting});
+  }
+
+ private:
+  base::test::ScopedFeatureList feature_list_;
+};
+
+class GlicApiTestWithWebActuationSettingEnabled : public GlicApiTestWithOneTab {
+ public:
+  GlicApiTestWithWebActuationSettingEnabled() {
+    feature_list_.InitWithFeatures({features::kGlicWebActuationSetting}, {});
+  }
+
+ private:
+  base::test::ScopedFeatureList feature_list_;
+};
+
+class GlicApiTestWithWebActuationSettingDisabled
+    : public GlicApiTestWithOneTab {
+ public:
+  GlicApiTestWithWebActuationSettingDisabled() {
+    feature_list_.InitWithFeatures({}, {features::kGlicWebActuationSetting});
   }
 
  private:
@@ -553,6 +576,22 @@ IN_PROC_BROWSER_TEST_P(GlicApiTestWithDefaultTabContextEnabled,
   ExecuteJsTest();
   browser()->profile()->GetPrefs()->SetBoolean(
       prefs::kGlicDefaultTabContextEnabled, false);
+  ContinueJsTest();
+}
+
+IN_PROC_BROWSER_TEST_P(GlicApiTestWithWebActuationSettingDisabled,
+                       testWebActuationSettingIsUndefinedWhenFeatureDisabled) {
+  ExecuteJsTest();
+}
+
+IN_PROC_BROWSER_TEST_P(GlicApiTestWithWebActuationSettingEnabled,
+                       testGetWebActuationSetting) {
+  browser()->profile()->GetPrefs()->SetBoolean(
+      prefs::kGlicUserEnabledActuationOnWeb, false);
+  ExecuteJsTest();
+
+  browser()->profile()->GetPrefs()->SetBoolean(
+      prefs::kGlicUserEnabledActuationOnWeb, true);
   ContinueJsTest();
 }
 
@@ -2693,6 +2732,14 @@ INSTANTIATE_TEST_SUITE_P(,
                          &WithTestParams::PrintTestVariant);
 INSTANTIATE_TEST_SUITE_P(,
                          MAYBE_GlicApiTestWithOneTabMoreDebounceDelay,
+                         DefaultTestParamSet(),
+                         &WithTestParams::PrintTestVariant);
+INSTANTIATE_TEST_SUITE_P(,
+                         GlicApiTestWithWebActuationSettingDisabled,
+                         DefaultTestParamSet(),
+                         &WithTestParams::PrintTestVariant);
+INSTANTIATE_TEST_SUITE_P(,
+                         GlicApiTestWithWebActuationSettingEnabled,
                          DefaultTestParamSet(),
                          &WithTestParams::PrintTestVariant);
 
