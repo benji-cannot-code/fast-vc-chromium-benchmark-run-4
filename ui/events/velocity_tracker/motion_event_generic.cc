77c940128694c5c22e0f23095b82968d48d16ffa
Mitsuru Oshima <oshima@chromium.org>
2025-10-21 02:11:42 -0700

    Expand the touch slop when the touch panel reports zero radius.
    A touch panel may report different location on touch press and touch
    press events, even though a user just tap the screen. This can be
    large enough on certain touch panel that tap is rejected. Such
    panel typically reports zero radius, so this CL uses that as an signal
    to expand the slop value 2x.
    
    Bug: 309522790
    Test: GestureRecognizerTest.GestureEventTap is updated to cover
          this case.
    
    Change-Id: I518a278740004d3758eeb01f85abc45ed1d87a93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7007987
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1532817}

--- ui/events/velocity_tracker/motion_event_generic.cc
+++ ui/events/velocity_tracker/motion_event_generic.cc
@@ -17,11 +17,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 
-PointerProperties::PointerProperties()
-    : PointerProperties(0, 0, 0) {
-}
+PointerProperties::PointerProperties() : PointerProperties(0, 0, 0) {}
 
-PointerProperties::PointerProperties(float x, float y, float touch_major)
+PointerProperties::PointerProperties(float x,
+                                     float y,
+                                     float touch_major,
+                                     bool has_native_touch_major)
     : id(0),
       tool_type(MotionEvent::ToolType::UNKNOWN),
       x(x),
@@ -36,7 +37,8 @@ PointerProperties::PointerProperties(float x, float y, float touch_major)
       tilt_y(0),
       twist(0),
       tangential_pressure(0),
-      source_device_id(0) {}
+      source_device_id(0),
+      has_native_touch_major(has_native_touch_major) {}
 
 PointerProperties::PointerProperties(const MotionEvent& event,
                                      size_t pointer_index)
@@ -54,7 +56,8 @@ PointerProperties::PointerProperties(const MotionEvent& event,
       tilt_y(event.GetTiltY(pointer_index)),
       twist(event.GetTwist(pointer_index)),
       tangential_pressure(event.GetTangentialPressure(pointer_index)),
-      source_device_id(0) {}
+      source_device_id(0),
+      has_native_touch_major(event.HasNativeTouchMajor(pointer_index)) {}
 
 PointerProperties::PointerProperties(const PointerProperties& other) = default;
 
@@ -175,6 +178,11 @@ float MotionEventGeneric::GetTouchMinor(size_t pointer_index) const {
   return pointers_[pointer_index].touch_minor;
 }
 
+bool MotionEventGeneric::HasNativeTouchMajor(size_t pointer_index) const {
+  DCHECK_LT(pointer_index, pointers_.size());
+  return pointers_[pointer_index].has_native_touch_major;
+}
+
 float MotionEventGeneric::GetOrientation(size_t pointer_index) const {
   DCHECK_LT(pointer_index, pointers_.size());
   return pointers_[pointer_index].orientation;
@@ -238,6 +246,14 @@ float MotionEventGeneric::GetHistoricalTouchMajor(
     size_t historical_index) const {
   DCHECK_LT(historical_index, historical_events_.size());
   return historical_events_[historical_index]->GetTouchMajor(pointer_index);
+}
+
+bool MotionEventGeneric::GetHistoricalHasNativeTouchMajor(
+    size_t pointer_index,
+    size_t historical_index) const {
+  DCHECK_LT(historical_index, historical_events_.size());
+  return historical_events_[historical_index]->HasNativeTouchMajor(
+      pointer_index);
 }
 
 float MotionEventGeneric::GetHistoricalX(size_t pointer_index,
@@ -332,10 +348,10 @@ MotionEventGeneric::MotionEventGeneric(const MotionEvent& event,
     historical_event->set_action(Action::MOVE);
     historical_event->set_event_time(event.GetHistoricalEventTime(h));
     for (size_t i = 0; i < pointer_count; ++i) {
-      historical_event->PushPointer(
-          PointerProperties(event.GetHistoricalX(i, h),
-                            event.GetHistoricalY(i, h),
-                            event.GetHistoricalTouchMajor(i, h)));
+      historical_event->PushPointer(PointerProperties(
+          event.GetHistoricalX(i, h), event.GetHistoricalY(i, h),
+          event.GetHistoricalTouchMajor(i, h),
+          event.GetHistoricalHasNativeTouchMajor(i, h)));
     }
     PushHistoricalEvent(std::move(historical_event));
   }
