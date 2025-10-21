e3f00ef7524452725a57b1dbd3d712e5c3ea9984
Michael Tang <tangm@microsoft.com>
2025-10-21 15:05:29 -0700

    Avoid thrashing swap chain allocation in auto HDR fallback case
    This CL moves clearing enable_vp_auto_hdr_ on failing ToggleVpAutoHDR to
    after RevertSwapChainToSDR. RevertSwapChainToSDR calls
    ReallocateSwapChain, which set enable_vp_auto_hdr_, so prior to this
    change, clearing enable_vp_auto_hdr_ would have no effect.
    
    SwapChainPresenter attempts to use auto HDR on all NVidia devices, but
    the feature (i.e. RTX True HDR) is only supported on the RTX 20 series
    and later. On the unsupported devices, we would repeat the follow
    pattern every frame:
    
    - Allocate an HDR swap chain due to enable_vp_auto_hdr_
    - Attempt ToggleVpAutoHDR and fail
    - Clear enable_vp_auto_hdr_
    - RevertSwapChainToSDR, which allocates an SDR swap chain and sets
      enable_vp_auto_hdr_
    
    Bug: 452830050
    Change-Id: I398f6f6c9634a668b7fc48c7ae3c44821c4eead0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7054750
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Commit-Queue: Michael Tang <tangm@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1533259}

--- ui/gl/swap_chain_presenter.cc
+++ ui/gl/swap_chain_presenter.cc
@@ -2279,8 +2279,6 @@ bool SwapChainPresenter::VideoProcessorBlt(
                            video_context.Get(), video_processor.Get(),
                            use_vp_auto_hdr);
       if (FAILED(hr)) {
-        enable_vp_auto_hdr_ = false;
-
         if (use_vp_auto_hdr) {
           if (!RevertSwapChainToSDR(video_device, video_processor,
                                     video_processor_enumerator, swap_chain3,
@@ -2290,6 +2288,7 @@ bool SwapChainPresenter::VideoProcessorBlt(
 
           use_vp_auto_hdr = false;
         }
+        enable_vp_auto_hdr_ = false;
       }
     }
 
