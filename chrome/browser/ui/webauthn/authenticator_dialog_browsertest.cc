f1822fbe385eadce5a075bdb6037210ffe15c14e
Yurii Lahodiuk <ylahodiuk@google.com>
2025-10-21 12:04:25 -0700

    Revert "webauthn: support opportunistically provided security domain keys."
    This reverts commit ea7b11b681b93ccd9ec643b530f007438fa4d61e.
    
    Reason for revert: The CL caused a failure of the test `TrustedVaultEncryptionKeysTabHelperBrowserTest.SetPasskeysKeyInEnclaveManager` (https://ci.chromium.org/ui/p/chromium/builders/ci/Mac13%20Tests/34919/overview).
    
    Original change's description:
    > webauthn: support opportunistically provided security domain keys.
    >
    > If a user signs in with a passkey that supports PRF, accounts.google.com
    > may be able to provide security domain secrets opportunistically in the
    > future. In order to support that the EnclaveManager needs to be able to
    > accept a `StoreKeys` call outside the context of a WebAuthn call.
    >
    > Bug: b:442804402
    >
    > Change-Id: If70cfa9247ddd11deb26fc2f22d0f182edfefc7e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5758602
    > Reviewed-by: Marc Treib <treib@chromium.org>
    > Commit-Queue: Yurii Lahodiuk <ylahodiuk@google.com>
    > Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1532933}
    
    Bug: b:442804402
    Change-Id: I5558b1fd01387e8508bf8cafb8870123439295cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7067537
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Auto-Submit: Yurii Lahodiuk <ylahodiuk@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Yann Dago <ydago@chromium.org>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1533127}

--- chrome/browser/ui/webauthn/authenticator_dialog_browsertest.cc
+++ chrome/browser/ui/webauthn/authenticator_dialog_browsertest.cc
@@ -1004,11 +1004,8 @@ document.addEventListener('DOMContentLoaded', function() {
 class QuitBrowserWhenKeysStored : public EnclaveManager::Observer {
  public:
   explicit QuitBrowserWhenKeysStored(Browser* browser) : browser_(browser) {
-    EnclaveManager* const enclave_manager =
-        EnclaveManagerFactory::GetAsEnclaveManagerForProfile(
-            browser_->profile());
-    enclave_manager->AddObserver(this);
-    store_keys_lock_ = enclave_manager->GetStoreKeysLock();
+    EnclaveManagerFactory::GetAsEnclaveManagerForProfile(browser_->profile())
+        ->AddObserver(this);
   }
 
   // EnclaveManager::Observer
@@ -1027,7 +1024,6 @@ class QuitBrowserWhenKeysStored : public EnclaveManager::Observer {
 
  private:
   raw_ptr<Browser> browser_;
-  std::unique_ptr<EnclaveManager::StoreKeysLock> store_keys_lock_;
 };
 
 IN_PROC_BROWSER_TEST_F(AuthenticatorWindowTest, RecoverSecurityDomain) {
--- chrome/browser/webauthn/gpm_enclave_controller.cc
+++ chrome/browser/webauthn/gpm_enclave_controller.cc
@@ -548,13 +548,6 @@ Profile* GPMEnclaveController::GetProfile() const {
       ->GetOriginalProfile();
 }
 
-void GPMEnclaveController::ShowSecurityDomainRecoveryUI() {
-  // The acquired lock indicates that the explicit key retrieval flow is being
-  // used.
-  store_keys_lock_ = enclave_manager_->GetStoreKeysLock();
-  model_->SetStep(Step::kRecoverSecurityDomain);
-}
-
 GPMEnclaveController::AccountState
 GPMEnclaveController::account_state_for_testing() const {
   return account_state_;
@@ -734,7 +727,6 @@ void GPMEnclaveController::OnKeysStored() {
 
   CHECK(enclave_manager_->has_pending_keys());
   CHECK(!enclave_manager_->is_ready());
-  store_keys_lock_.reset();
 
   if ((pin_metadata_.has_value() && pin_metadata_->usable_pin_metadata) ||
       *can_make_uv_keys_) {
@@ -778,7 +770,7 @@ void GPMEnclaveController::RecoverSecurityDomain() {
       trusted_vault::SecurityDomainId::kPasskeys,
       kICloudKeychainRecoveryKeyAccessGroup);
 #else
-  ShowSecurityDomainRecoveryUI();
+  model_->SetStep(Step::kRecoverSecurityDomain);
 #endif  // BUILDFLAG(IS_MAC)
 }
 
@@ -856,7 +848,7 @@ void GPMEnclaveController::OnICloudKeysRetrievedForRecovery(
       });
   if (local_icloud_key_it == local_icloud_keys.end()) {
     FIDO_LOG(DEBUG) << "Could not find matching iCloud recovery key";
-    ShowSecurityDomainRecoveryUI();
+    model_->SetStep(Step::kRecoverSecurityDomain);
     return;
   }
   const auto member_key_it = std::ranges::max_element(
@@ -869,12 +861,11 @@ void GPMEnclaveController::OnICloudKeysRetrievedForRecovery(
   if (!security_domain_secret) {
     FIDO_LOG(ERROR)
         << "Could not decrypt security domain secret with iCloud key";
-    ShowSecurityDomainRecoveryUI();
+    model_->SetStep(Step::kRecoverSecurityDomain);
     return;
   }
   FIDO_LOG(EVENT) << "Successful recovery from iCloud recovery key";
   recovered_with_icloud_keychain_ = true;
-  store_keys_lock_ = enclave_manager_->GetStoreKeysLock();
   enclave_manager_->StoreKeys(user_gaia_id_,
                               {std::move(*security_domain_secret)},
                               member_key_it->version);
