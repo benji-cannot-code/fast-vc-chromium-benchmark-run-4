f9143b909f3028e420869a1c30541eea68511bc5
Sophie Chang <sophiechang@chromium.org>
2025-10-22 14:35:47 -0700

    Allow for observing changes to the subscription tier
    Bug: 454192176
    Change-Id: I8deafc057ed7609517cfc1329f777b0464296526
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7075717
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1533910}

--- chrome/browser/subscription_eligibility/subscription_eligibility_service_browsertest.cc
+++ chrome/browser/subscription_eligibility/subscription_eligibility_service_browsertest.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/subscription_eligibility/subscription_eligibility_service.h"
 
+#include <optional>
+
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/subscription_eligibility/subscription_eligibility_prefs.h"
 #include "chrome/browser/subscription_eligibility/subscription_eligibility_service_factory.h"
@@ -13,6 +15,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test.h"
 
 namespace subscription_eligibility {
+
+namespace {
+
+class SubscriptionEligibilityServiceObserver
+    : public SubscriptionEligibilityService::Observer {
+ public:
+  std::optional<int32_t> new_subscription_tier() const {
+    return new_subscription_tier_;
+  }
+
+ private:
+  // SubscriptionEligibilityService::Observer:
+  void OnAiSubscriptionTierUpdated(int32_t new_subscription_tier) override {
+    new_subscription_tier_ = new_subscription_tier;
+  }
+
+  std::optional<int32_t> new_subscription_tier_;
+};
 
 class SubscriptionEligibilityServiceTest : public InProcessBrowserTest {
  public:
@@ -31,8 +51,16 @@ IN_PROC_BROWSER_TEST_F(SubscriptionEligibilityServiceTest,
                        GetAiSubscriptionTier) {
   EXPECT_EQ(service()->GetAiSubscriptionTier(), 0);
 
+  SubscriptionEligibilityServiceObserver observer;
+  service()->AddObserver(&observer);
+
   SetAiSubscriptionTierForProfile(1);
   EXPECT_EQ(service()->GetAiSubscriptionTier(), 1);
+
+  ASSERT_TRUE(observer.new_subscription_tier());
+  EXPECT_EQ(*observer.new_subscription_tier(), 1);
 }
+
+}  // namespace
 
 }  // namespace subscription_eligibility
