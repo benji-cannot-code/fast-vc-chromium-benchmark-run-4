afb483570c2abfda857c3807d78c0cb4930be29c
EmiliaPaz <emiliapaz@chromium.org>
2025-10-21 18:03:48 -0700

    [Extensions] Add ToolbarActionsModel::Observer to menu controller
    ExtensionsMenuController should hold all the extensions logic observers,
    and the notify each platform through the platform delegate.
    
    Therefore, this CL adds ToolbarActionsModel::Observer to the menu
    controller and implement OnToolbarActionAdded(). Other methods will be
    implemented in follow up CLs.
    
    Bug: 449814184
    Change-Id: I1896919b52f5a3ef469136fd4552d8f25e675d2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7057441
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1533339}

--- chrome/browser/ui/extensions/extensions_menu_view_model.h
+++ chrome/browser/ui/extensions/extensions_menu_view_model.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_EXTENSIONS_EXTENSIONS_MENU_VIEW_MODEL_H_
 
 #include "base/memory/raw_ptr.h"
+#include "chrome/browser/ui/toolbar/toolbar_actions_model.h"
 #include "extensions/browser/permissions_manager.h"
 #include "extensions/common/extension_id.h"
 
@@ -20,8 +21,8 @@ class ExtensionsMenuViewPlatformDelegate;
 // The platform agnostic controller for the extensions menu.
 // TODO(crbug.com/449814184): Move the observers from
 // ExtensionsMenuViewController here.
-class ExtensionsMenuViewModel
-    : public extensions::PermissionsManager::Observer {
+class ExtensionsMenuViewModel : public extensions::PermissionsManager::Observer,
+                                public ToolbarActionsModel::Observer {
  public:
   ExtensionsMenuViewModel(
       BrowserWindowInterface* browser,
@@ -29,7 +30,7 @@ class ExtensionsMenuViewModel
   ExtensionsMenuViewModel(const ExtensionsMenuViewModel&) = delete;
   const ExtensionsMenuViewModel& operator=(const ExtensionsMenuViewModel&) =
       delete;
-  virtual ~ExtensionsMenuViewModel();
+  ~ExtensionsMenuViewModel() override;
 
   // Updates the extension's site access for the current site.
   void UpdateSiteAccess(
@@ -50,6 +51,16 @@ class ExtensionsMenuViewModel
   void OnHostAccessRequestAdded(const extensions::ExtensionId& extension_id,
                                 int tab_id) override;
 
+  // ToolbarActionsModel::Observer:
+  void OnToolbarActionAdded(
+      const ToolbarActionsModel::ActionId& action_id) override;
+  void OnToolbarActionRemoved(
+      const ToolbarActionsModel::ActionId& action_id) override;
+  void OnToolbarActionUpdated(
+      const ToolbarActionsModel::ActionId& action_id) override;
+  void OnToolbarModelInitialized() override;
+  void OnToolbarPinnedActionsChanged() override;
+
  private:
   content::WebContents* GetActiveWebContents();
 
@@ -62,6 +73,10 @@ class ExtensionsMenuViewModel
   base::ScopedObservation<extensions::PermissionsManager,
                           extensions::PermissionsManager::Observer>
       permissions_manager_observation_{this};
+
+  const raw_ptr<ToolbarActionsModel> toolbar_model_;
+  base::ScopedObservation<ToolbarActionsModel, ToolbarActionsModel::Observer>
+      toolbar_model_observation_{this};
 };
 
 #endif  // CHROME_BROWSER_UI_EXTENSIONS_EXTENSIONS_MENU_VIEW_MODEL_H_
