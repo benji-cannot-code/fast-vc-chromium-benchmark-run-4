0fbb890ed423d23be9a7febe77949f569f7b7ea3
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2025-10-22 08:04:29 -0700

    Don't update extensions for soon-to-be-destroyed profiles
    Change ExtensionUpdater::CheckNow() so if the profile is scheduled for
    destruction soon (usually in 1-2s max), it doesn't start a new
    update check.
    
    This should save resources and fix a DCHECK that can trigger in the
    wild: ScopedProfileKeepAlive doesn't like if we try to acquire a
    keepalive during this 1-2s period.
    
    Bug: 368360956
    Change-Id: I4d3ac1987d60ab348b7394b3b1b513dde4415ca5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6983895
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1533627}

--- chrome/browser/extensions/updater/extension_updater.cc
+++ chrome/browser/extensions/updater/extension_updater.cc
@@ -441,6 +441,14 @@ void ExtensionUpdater::CheckNow(CheckParams params) {
     will_check_soon_ = false;
   }
 
+  std::unique_ptr<ScopedProfileKeepAlive> keep_alive =
+      ScopedProfileKeepAlive::TryAcquire(
+          profile_, ProfileKeepAliveOrigin::kExtensionUpdater);
+  if (!keep_alive) {
+    // Profile will be destroyed soon, don't start an update check.
+    return;
+  }
+
   int request_id = next_request_id_++;
 
   VLOG(2) << "Starting update check " << request_id;
@@ -454,8 +462,7 @@ void ExtensionUpdater::CheckNow(CheckParams params) {
   request.update_found_callback = params.update_found_callback;
   request.callback = std::move(params.callback);
   request.install_immediately = params.install_immediately;
-  request.profile_keep_alive = std::make_unique<ScopedProfileKeepAlive>(
-      profile_, ProfileKeepAliveOrigin::kExtensionUpdater);
+  request.profile_keep_alive = std::move(keep_alive);
 
   EnsureDownloaderCreated();
 
