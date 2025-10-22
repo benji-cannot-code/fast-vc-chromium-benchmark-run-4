6804056be204a8a6945c7cf884d3e41623df046c
Vladimir Levin <vmpstr@chromium.org>
2025-10-22 07:26:36 -0700

    VT: Remove ViewTransition inherit stable flags
    These flags have been stable for a while and this is a clean up patch
    
    R=flackr@chromium.org
    
    Change-Id: I30d0d94cf84dd651365c11536dd47877e2b2f2ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7067793
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1533599}

--- third_party/blink/renderer/core/view_transition/view_transition_style_tracker.cc
+++ third_party/blink/renderer/core/view_transition/view_transition_style_tracker.cc
@@ -2211,7 +2211,6 @@ CSSStyleSheet& ViewTransitionStyleTracker::UAStyleSheet() {
     builder.AddUAStyle(RuntimeEnabledFeatures::ScopedViewTransitionsEnabled()
                            ? AnimationUAStylesScoped()
                            : AnimationUAStyles());
-    builder.AddFlagGuardedDefaultAnimationStyles();
   }
 
   // If we started the animation then we always create the full dynamic style
