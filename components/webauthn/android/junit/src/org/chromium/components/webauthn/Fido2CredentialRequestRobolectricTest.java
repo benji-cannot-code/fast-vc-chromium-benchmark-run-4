f8d40f335f4857657ab961cf8a2ae549964615d6
Ken Buchanan <kenrb@chromium.org>
2025-10-22 13:59:44 -0700

    [WebAuthn] Enable timeouts for Immediate CredMan requests
    The Immediate Get request timer, which by default aborts the request
    if credential enumeration takes more than 500ms, does not currently
    work for requests that are handled entirely by CredMan. This is
    because the browser cannot know if UI has been shown or not when a
    timeout occurs.
    
    This change adds a path through `prepareGetCredential` for immediate
    requests. It will now enumerate the credentials in advance of sending
    the `getCredential` request which will show UI, allowing an abort
    in cases where enumeration is slow.
    
    This CL also does a minor cleanup in `CredManHelper` by removing
    redundant cleanup calls during cancellation.
    
    Bug: 433543129
    Change-Id: If27bcf04d90df55d877f2fb536d925805a25b0aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7076033
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1533897}

--- components/webauthn/android/junit/src/org/chromium/components/webauthn/Fido2CredentialRequestRobolectricTest.java
+++ components/webauthn/android/junit/src/org/chromium/components/webauthn/Fido2CredentialRequestRobolectricTest.java
@@ -325,6 +325,7 @@ public class Fido2CredentialRequestRobolectricTest {
                         /* getCallback= */ any(),
                         /* errorCallback= */ any(),
                         /* barrier= */ any(),
+                        /* stopImmediateTimer= */ any(),
                         /* ignoreGpm= */ eq(true));
         verify(mBrowserBridgeMock)
                 .onCredentialsDetailsListReceived(
@@ -515,6 +516,7 @@ public class Fido2CredentialRequestRobolectricTest {
                         /* getCallback= */ any(),
                         /* errorCallback= */ any(),
                         /* barrier= */ any(),
+                        /* stopImmediateTimer= */ any(),
                         /* ignoreGpm= */ eq(false));
         verify(mBrowserBridgeMock, never()).onCredManUiClosed(any(), anyBoolean());
     }
@@ -545,6 +547,7 @@ public class Fido2CredentialRequestRobolectricTest {
                         /* getCallback= */ any(),
                         /* errorCallback= */ any(),
                         /* barrier= */ any(),
+                        /* stopImmediateTimer= */ any(),
                         /* ignoreGpm= */ eq(true));
         verify(mBrowserBridgeMock, times(1))
                 .onCredentialsDetailsListReceived(
@@ -578,7 +581,7 @@ public class Fido2CredentialRequestRobolectricTest {
 
         // CredManHelper class is responsible to return the status.
         assertThat(mCallback.getStatus()).isEqualTo(null);
-        verify(mCredManHelperMock).cancelGetAssertion();
+        verify(mCredManHelperMock).cancelGetAssertion(AuthenticatorStatus.ABORT_ERROR);
         verify(mBrowserBridgeMock, never()).cleanupRequest(any());
         verify(mBrowserBridgeMock, never()).onCredManUiClosed(any(), anyBoolean());
     }
@@ -603,7 +606,7 @@ public class Fido2CredentialRequestRobolectricTest {
         mRequest.cancelGetAssertion();
 
         verify(mBarrierMock).onFido2ApiCancelled();
-        verify(mCredManHelperMock).cancelGetAssertion();
+        verify(mCredManHelperMock).cancelGetAssertion(AuthenticatorStatus.ABORT_ERROR);
         verify(mBrowserBridgeMock).cleanupRequest(any());
         verify(mBrowserBridgeMock, never()).onCredManUiClosed(any(), anyBoolean());
     }
@@ -659,7 +662,7 @@ public class Fido2CredentialRequestRobolectricTest {
         assertThat(mCallback.getStatus()).isNull();
         verify(mCredManHelperMock, times(1))
                 .startPrefetchRequest(
-                        any(), any(), any(), any(), any(), any(), any(), anyBoolean());
+                        any(), any(), any(), any(), any(), any(), any(), any(), anyBoolean());
     }
 
     @Test
@@ -688,13 +691,15 @@ public class Fido2CredentialRequestRobolectricTest {
 
         String originString = Fido2CredentialRequest.convertOriginToString(mOrigin);
         verify(mCredManHelperMock)
-                .startGetRequest(
+                .startPrefetchRequest(
                         eq(mRequestOptions),
                         eq(originString),
                         eq(TEST_CLIENT_DATA_JSON.getBytes()),
-                        /* clientDataHash= */ notNull(),
+                        /* clientDataHash= */ any(),
                         /* getCallback= */ any(),
                         /* errorCallback= */ any(),
+                        /* barrier= */ any(),
+                        /* stopImmediateTimer= */ notNull(),
                         /* ignoreGpm= */ eq(false));
         verify(mBrowserBridgeMock, never()).onCredManUiClosed(any(), anyBoolean());
     }
