46c368da754a536c3a61dc66edc3efa8e5b7c969
Dominik Bylica <bylica@google.com>
2025-10-22 05:24:16 -0700

    [WebPrinting] Be explicit about printing being available in window global scope.
    Bug: 453926041
    Change-Id: Iaf6c73f253e4ebc0f33ed7acc5fc0ddb5bf97945
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7072652
    Reviewed-by: Vlad Krot <vkrot@google.com>
    Auto-Submit: Dominik Bylica <bylica@google.com>
    Commit-Queue: Dominik Bylica <bylica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1533544}

--- third_party/blink/renderer/modules/printing/web_printing_manager.h
+++ third_party/blink/renderer/modules/printing/web_printing_manager.h
@@ -27,7 +27,7 @@ class MODULES_EXPORT WebPrintingManager : public ScriptWrappable,
  public:
   static const char kSupplementName[];
 
-  // Getter for printing
+  // Getter for printing (available in the window global scope)
   static WebPrintingManager* GetWebPrintingManager(ExecutionContext&);
 
   explicit WebPrintingManager(ExecutionContext*);
