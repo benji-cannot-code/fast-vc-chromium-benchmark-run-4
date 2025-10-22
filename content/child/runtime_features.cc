e5e25c4b5d1af21c204a8eabe3896d8638c12053
Alvin Ji <alvinji@chromium.org>
2025-10-21 22:06:42 -0700

    geolocation: Override enableHighAccuracy hint for Android location request
    This change adjusts geolocation accuracy hints on Android to account for
    an update in the backend location provider. Before the
    ApproximateGeolocationPermissionEnabled feature, the enableHighAccuracy
    setting only influenced update priority, not the actual precision (which
    was always precise). Now, with the feature enabled, this setting
    controls the provider to return precise or approximate location. To
    ensure websites experience consistent behavior, this CL conditionally
    overrides the accuracy hint to maintain the expected precise location
    results.
    
    Change-Id: I55cb3d834076c38f64a67de38c3d7245067efb25
    Bug: 444334148
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7058857
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Alvin Ji <alvinji@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1533404}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "cc/base/features.h"
 #include "components/attribution_reporting/features.h"
+#include "components/content_settings/core/common/features.h"
 #include "content/common/content_navigation_policy.h"
 #include "content/common/content_switches_internal.h"
 #include "content/common/features.h"
@@ -348,6 +349,9 @@ void SetRuntimeFeaturesFromChromiumFeatures() {
            kSetOnlyIfOverridden},
           {"AttributionReporting",
            raw_ref(features::kPrivacySandboxAdsAPIsM1Override)},
+          {"ApproximateGeolocationPermission",
+           raw_ref(
+               content_settings::features::kApproximateGeolocationPermission)},
           {"AndroidDownloadableFontsMatching",
            raw_ref(features::kAndroidDownloadableFontsMatching)},
 #if BUILDFLAG(IS_ANDROID)
