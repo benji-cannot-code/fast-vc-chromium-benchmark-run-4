eca97cb43ed7bec4a75e35251496d70de9769822
Matt Menke <mmenke@chromium.org>
2025-10-21 21:41:08 -0700

    [HEv3] Align AttemptManager and HttpStreamPool SPDY session checks.
    In particular, make AttemptManager::HasAvailableSpdySession() check if
    a request is using TLS or not, to match
    HttpStreamPool::FindAvailableSpdySession's HTTPS check. It's unclear
    if this is needed, but seems safest to keep them in sync. The
    SpdySessionKey does not include protocol, though does include port.
    
    Also attempt to optimize AttemptManager::HasAvailableSpdySession() a
    little more, by having it check for a SPDY session before having it
    check if the host RequiresHTTP11(). Most of the time there won't be
    a session (otherwise we would have used it already), and HTTP/1.1
    won't be required, so this should generally short circuit the
    fairly heavy weight RequiresHTTP11() call that will just return false
    most of the time.
    
    BUG: 346835898
    Change-Id: I2ca4e6d8509d2a4ac2d0089ef1fb44f86037d3d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7064216
    Commit-Queue: mmenke <mmenke@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1533395}

--- net/http/http_stream_pool_attempt_manager.cc
+++ net/http/http_stream_pool_attempt_manager.cc
@@ -1795,6 +1795,21 @@ void HttpStreamPool::AttemptManager::OnJobDone(Job* job) {
 }
 
 bool HttpStreamPool::AttemptManager::HasAvailableSpdySession() const {
+  if (!is_using_tls_) {
+    return false;
+  }
+
+  // Check for an available session before checking if it can be used. It's
+  // rare for HTTP/1.1 to be required in general, and checking it is can be
+  // slow. Also, it's fairly rare for there to be a session available here
+  // (outside of the session aliasing case), since the Job already checked for
+  // one.
+  if (!spdy_session_pool()->HasAvailableSession(spdy_session_key(),
+                                                IsIpBasedPoolingEnabledForH2(),
+                                                /*is_websocket=*/false)) {
+    return false;
+  }
+
   // If the destination is marked as requiring HTTP/1.1, act as if there's no
   // available SPDY session. This matches the behavior of
   // HttpStreamPool::FindAvailableSpdySession().
@@ -1802,10 +1817,7 @@ bool HttpStreamPool::AttemptManager::HasAvailableSpdySession() const {
                              stream_key().network_anonymization_key())) {
     return false;
   }
-
-  return spdy_session_pool()->HasAvailableSession(
-      spdy_session_key(), IsIpBasedPoolingEnabledForH2(),
-      /*is_websocket=*/false);
+  return true;
 }
 
 void HttpStreamPool::AttemptManager::MaybeStartDraining() {
