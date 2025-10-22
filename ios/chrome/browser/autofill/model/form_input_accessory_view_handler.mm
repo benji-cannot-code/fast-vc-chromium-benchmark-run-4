f3502e276b5dc456770f815be7f9e9354b35e5d7
Leo Zhao <leozhao@google.com>
2025-10-22 07:48:00 -0700

    [iOS] Add an user action to track the usage of the close button from the Keyboard Accessory
    Bug: 452111358
    Change-Id: Ib3524d475dfaffe6cf6d1991334f4e58d07db694
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7067617
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Commit-Queue: Leo Zhao <leozhao@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Noémie St-Onge <noemies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1533612}

--- ios/chrome/browser/autofill/model/form_input_accessory_view_handler.mm
+++ ios/chrome/browser/autofill/model/form_input_accessory_view_handler.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/apple/foundation_util.h"
 #import "base/metrics/histogram_macros.h"
+#import "base/metrics/user_metrics.h"
 #import "base/notreached.h"
 #import "base/strings/sys_string_conversions.h"
 #import "components/autofill/ios/browser/suggestion_controller_java_script_feature.h"
@@ -199,6 +200,8 @@ NSArray* FindDescendantToolbarItemsForActionName(
 #pragma mark - FormInputNavigator
 
 - (void)closeKeyboardWithButtonPress {
+  base::RecordAction(
+      base::UserMetricsAction("KeyboardAccessory_CloseButtonPressed"));
   [self closeKeyboardLoggingButtonPressed];
 }
 
