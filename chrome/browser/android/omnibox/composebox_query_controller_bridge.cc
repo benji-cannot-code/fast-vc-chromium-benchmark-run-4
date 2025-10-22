71b2e502eab739e3cffb66883c7f20b1d9e4c461
Tomasz Wiszkowski <ender@google.com>
2025-10-22 11:11:25 -0700

    Disable Fusebox on non AIM-eligible profiles.
    Suppressing ComposeBoxQueryController blocks the entire functionality.
    
    Change-Id: Ia72b5920c505ef9aecdba88e27c8c5e4e2fe59c5
    Fixed: 449040616
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7070088
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1533779}

--- chrome/browser/android/omnibox/composebox_query_controller_bridge.cc
+++ chrome/browser/android/omnibox/composebox_query_controller_bridge.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/span.h"
 #include "base/time/time.h"
 #include "base/unguessable_token.h"
+#include "chrome/browser/autocomplete/aim_eligibility_service_factory.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
@@ -18,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/channel_info.h"
 #include "components/lens/contextual_input.h"
 #include "components/lens/lens_bitmap_processing.h"
+#include "components/omnibox/browser/aim_eligibility_service.h"
 #include "url/android/gurl_android.h"
 #include "url/gurl.h"
 
@@ -26,6 +28,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 static jlong JNI_ComposeBoxQueryControllerBridge_Init(JNIEnv* env,
                                                       Profile* profile) {
+  auto* aim_service = AimEligibilityServiceFactory::GetForProfile(profile);
+  if (!aim_service || !aim_service->IsAimEligible()) {
+    return 0L;
+  }
+
   ComposeboxQueryControllerBridge* instance =
       new ComposeboxQueryControllerBridge(profile);
   return reinterpret_cast<intptr_t>(instance);
