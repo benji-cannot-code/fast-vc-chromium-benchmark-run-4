40113f20684632f3a918c0e21b26e11ca4c103f1
Josh Karlin <jkarlin@chromium.org>
2025-10-22 11:47:08 -0700

    Reland [Performance] Speed up ComputeHashForWideString
    The slow path for String hashing has a character-at-a-time latin1 check.
    This CL replaces it with a word-at-a-time check.
    
    This reduces CPU usage by .3% on a couple of news sites, see the bug for
    more detail.
    
    Bug: 445914395
    Change-Id: I3ee75938da187b254347b07e3752ef693aa2ebcb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6965611
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Josh Karlin <jkarlin@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1533596}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7075571
    Cr-Commit-Position: refs/heads/main@{#1533800}

--- third_party/blink/renderer/platform/wtf/text/string_statics.cc
+++ third_party/blink/renderer/platform/wtf/text/string_statics.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/text/convert_to_8bit_hash_reader.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_impl.h"
+#include "third_party/blink/renderer/platform/wtf/text/utf16.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_uchar.h"
 #include "third_party/blink/renderer/platform/wtf/wtf.h"
 
@@ -97,14 +98,7 @@ bool NewlineThenWhitespaceStringsTable::IsNewlineThenWhitespaces(
 }
 
 WTF_EXPORT unsigned ComputeHashForWideString(base::span<const UChar> str) {
-  bool is_all_latin1 = true;
-  for (UChar ch : str) {
-    if (ch & 0xff00) {
-      is_all_latin1 = false;
-      break;
-    }
-  }
-  if (is_all_latin1) {
+  if (ContainsOnlyLatin1(str)) {
     return StringHasher::ComputeHashAndMaskTop8Bits<ConvertTo8BitHashReader>(
         reinterpret_cast<const char*>(str.data()), str.size());
   } else {
