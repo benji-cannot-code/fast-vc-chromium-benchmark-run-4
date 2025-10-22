1368d4348017211c910b8b9d8caeed0545c487d1
Peter Conn <peconn@chromium.org>
2025-10-22 00:49:26 -0700

    Add plumbing for addQuicHint WebView API
    This CL exposes to the WebView layer the ability to set a QUIC hint for a domain.
    
    Bug: 445339041
    Change-Id: Ie3d6000a239bfd4d6e6b0ef9dc1dac0cd20c08fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7056182
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1533456}

--- android_webview/browser/aw_browser_context.cc
+++ android_webview/browser/aw_browser_context.cc
@@ -96,6 +96,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/mojom/network_context.mojom.h"
 #include "services/network/public/mojom/url_loader.mojom.h"
 #include "third_party/blink/public/common/origin_trials/trial_token_validator.h"
+#include "url/android/gurl_android.h"
 #include "url/gurl.h"
 
 // Must come after all headers that specialize FromJniType() / ToJniType().
@@ -786,6 +787,17 @@ const std::vector<scoped_refptr<AwOriginMatchedHeader>>&
 AwBrowserContext::GetOriginMatchedHeaders() {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
   return origin_matched_headers_;
+}
+
+void AwBrowserContext::AddQuicHints(JNIEnv* env,
+                                    const std::vector<GURL>& origins) {
+  std::vector<url::SchemeHostPort> scheme_host_ports(origins.size());
+  for (const GURL& origin : origins) {
+    scheme_host_ports.emplace_back(origin);
+  }
+
+  GetDefaultStoragePartition()->GetNetworkContext()->AddQuicHints(
+      scheme_host_ports, net::NetworkAnonymizationKey());
 }
 
 void AwBrowserContext::SetServiceWorkerIoThreadClient(
--- services/network/test/test_network_context.h
+++ services/network/test/test_network_context.h
@@ -401,6 +401,9 @@ class TestNetworkContext : public mojom::NetworkContext {
       const base::UnguessableToken& throttling_profile_id,
       mojo::PendingReceiver<network::mojom::DurableMessageCollector> receiver)
       override {}
+  void AddQuicHints(
+      const std::vector<url::SchemeHostPort>& origins,
+      const net::NetworkAnonymizationKey& network_anonymization_key) override {}
 };
 
 }  // namespace network
