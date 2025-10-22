8114b3437f881e1628939d6b4d3681e85991c640
Jeffrey Young <cowmoo@google.com>
2025-10-22 15:11:36 -0700

    p13n: Url type guard strengthening
    Split the isImageDataUrl type guard into isUrl type guard and
    isImageDataUrl boolean function. This provides an actually sound type
    guard, but still validates that the data is a data url before
    proceeding.
    
    Bug: b:453736076
    Test: ash_unittests --gtest_filter=*SeaPenUtils*
    Test: ash_unittests --gtest_filter=*PersonalizationApp*
    Change-Id: I74863590fc0b3a22d29b4e70409a0890d80f98e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7076717
    Reviewed-by: Fred Shih <ffred@chromium.org>
    Commit-Queue: Jeffrey Young <cowmoo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1533933}

--- ash/webui/common/resources/sea_pen/sea_pen_recent_wallpapers_element.ts
+++ ash/webui/common/resources/sea_pen/sea_pen_recent_wallpapers_element.ts
@@ -30,7 +30,7 @@ import {logRecentImageActionMenuItemClick, RecentImageActionMenuItem} from './se
 import {getTemplate} from './sea_pen_recent_wallpapers_element.html.js';
 import {SeaPenRouterElement} from './sea_pen_router_element.js';
 import {WithSeaPenStore} from './sea_pen_store.js';
-import {getUserVisibleQuery, isActiveSeaPenQuery, isImageDataUrl, isNonEmptyArray, isPersonalizationApp, isSeaPenImageId} from './sea_pen_utils.js';
+import {getUserVisibleQuery, isActiveSeaPenQuery, isImageDataUrl, isNonEmptyArray, isPersonalizationApp, isSeaPenImageId, isUrl} from './sea_pen_utils.js';
 
 export class SeaPenRecentImageDeleteEvent extends CustomEvent<null> {
   static readonly EVENT_NAME = 'sea-pen-recent-image-delete';
@@ -131,7 +131,8 @@ export class SeaPenRecentWallpapersElement extends WithSeaPenStore {
   private onRecentImagesChanged_(recentImages: SeaPenImageId[]|null) {
     this.recentImagesToDisplay_ = (recentImages || []).filter(id => {
       if (this.recentImageDataLoading_[id] === false) {
-        return isImageDataUrl(this.recentImageData_[id]?.url);
+        const dataUrl = this.recentImageData_[id]?.url;
+        return isUrl(dataUrl) && isImageDataUrl(dataUrl);
       }
       return true;
     });
@@ -153,7 +154,7 @@ export class SeaPenRecentWallpapersElement extends WithSeaPenStore {
     for (let i = this.recentImagesToDisplay_.length - 1; i >= 0; i--) {
       const id = this.recentImagesToDisplay_[i];
       const data = recentImageData[id];
-      const validData = isImageDataUrl(data?.url);
+      const validData = isUrl(data?.url) && isImageDataUrl(data.url);
       const failed = recentImageDataLoading[id] === false && !validData;
       if (failed) {
         this.splice('recentImagesToDisplay_', i, 1);
@@ -182,7 +183,7 @@ export class SeaPenRecentWallpapersElement extends WithSeaPenStore {
       return null;
     }
     const data = recentImageData[recentImage];
-    if (!isImageDataUrl(data?.url)) {
+    if (!isUrl(data?.url) || !isImageDataUrl(data.url)) {
       return {url: ''};
     }
     return data.url;
--- chrome/test/data/webui/chromeos/ash_common/sea_pen/sea_pen_utils_test.ts
+++ chrome/test/data/webui/chromeos/ash_common/sea_pen/sea_pen_utils_test.ts
@@ -3,13 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {isImageDataUrl, isNonEmptyFilePath} from 'chrome://resources/ash/common/sea_pen/sea_pen_utils.js';
+import {isImageDataUrl, isNonEmptyFilePath, isUrl} from 'chrome://resources/ash/common/sea_pen/sea_pen_utils.js';
 import {assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
 
 suite('isImageDataUrlTest', function() {
   test('invalid types', function() {
     for (const value of [null, undefined, false, 'not_url', 0, 1, {}]) {
-      assertFalse(isImageDataUrl(value), `${value} is not Url`);
+      assertFalse(isUrl(value), `${value} is not Url`);
     }
   });
 
