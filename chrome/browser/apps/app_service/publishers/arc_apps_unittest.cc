9e71e2813ca71fb016cf5130602fb82246f09a04
Jun Ishiguro <junis@google.com>
2025-10-21 22:17:21 -0700

    Remove redundant ReinitializeForTesting call
    Bug: None
    Change-Id: I3e08f078183367f87e482b8ae982f38306dff3ca
    Cq-Include-Trybots: luci.chromium.try:linux_chromium_chromeos_msan_rel_ng
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7070866
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1533407}

--- chrome/browser/apps/app_service/publishers/arc_apps_unittest.cc
+++ chrome/browser/apps/app_service/publishers/arc_apps_unittest.cc
@@ -910,7 +910,6 @@ class ArcAppsPublisherPromiseAppTest : public ArcAppsPublisherTest {
  public:
   void SetUp() override {
     ArcAppsPublisherTest::SetUp();
-    app_service_proxy()->ReinitializeForTesting(profile());
     service()->SetSkipAlmanacForTesting(true);
   }
 
