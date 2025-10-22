b4737bf1b58b25f72c14bf2da7867c6acb88615a
Justin Cohen <justincohen@google.com>
2025-10-22 14:07:42 -0700

    omnibox: Speculative fix for AddRowWordsToIndex crash.
    Something appears to be corrupting the `word_map_` map in
    URLIndexPrivateData, leading to a crash when the tree re-balances itself. It appears this only happens when an empty term is inserted.
    Try a speculative fix by skipping empty terms, and add some crash
    keys to confirm if this happens for non-empty terms.
    
    Bug: 348617573
    Change-Id: I5d98bd48410e0149f89d5ea04260480c60264cfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7067972
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1533898}

--- components/omnibox/browser/url_index_private_data.cc
+++ components/omnibox/browser/url_index_private_data.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/flat_set.h"
 #include "base/containers/stack.h"
+#include "base/debug/crash_logging.h"
 #include "base/feature_list.h"
 #include "base/i18n/case_conversion.h"
 #include "base/memory/raw_ptr.h"
@@ -788,8 +789,16 @@ void URLIndexPrivateData::AddRowWordsToIndex(const history::URLRow& row,
   String16Set title_words = String16SetFromString16(
       title, word_starts ? &word_starts->title_word_starts_ : nullptr);
   for (const auto& word :
-       base::STLSetUnion<String16Set>(url_words, title_words))
+       base::STLSetUnion<String16Set>(url_words, title_words)) {
+    // Speculative fix for crbug.com/348617573.
+    if (word.empty()) {
+      continue;
+    }
+    // Some crash keys if the fix doesn't work.
+    SCOPED_CRASH_KEY_STRING256("Bug348617573", "url", gurl.spec());
+    SCOPED_CRASH_KEY_STRING32("Bug348617573", "word", base::UTF16ToUTF8(word));
     AddWordToIndex(word, history_id);
+  }
 
   search_term_cache_.clear();  // Invalidate the term cache.
 }
