68aff2ff71fe2625014aa6d84152f3242eed03aa
Fiaz Muhammad <mfiaz@google.com>
2025-10-22 08:42:36 -0700

    [Persist] Add the ability to register GURLs available pre-native
    - Add the ability to register GURLs available pre-native.
    - During configuration, we deserialize GURLs to make them available
    	pre-native. The Holder nested class lets us provide one for
    	when overrides are enabled, and another for when they are
    	disabled.
    
    Bug: 454104029
    Change-Id: I0902590211134a40788cefd1965c8e6749604ef9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7070043
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Fiaz Muhammad <mfiaz@google.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1533654}

--- chrome/browser/url_constants/android/java/src/org/chromium/chrome/browser/url_constants/UrlConstantResolverFactory.java
+++ chrome/browser/url_constants/android/java/src/org/chromium/chrome/browser/url_constants/UrlConstantResolverFactory.java
@@ -12,7 +12,9 @@ import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.chrome.browser.url_constants.UrlConstantResolver.PreNativeGurlHolder;
 import org.chromium.components.embedder_support.util.UrlConstants;
+import org.chromium.url.GURL;
 
 /**
  * This factory creates and keeps a single ExtensionsUrlOverrideRegistryManager for incognito
@@ -20,6 +22,12 @@ import org.chromium.components.embedder_support.util.UrlConstants;
  */
 @NullMarked
 public class UrlConstantResolverFactory {
+    private static final String SERIALIZED_NATIVE_NTP_URL =
+            "82,1,true,0,13,0,-1,0,-1,16,6,0,-1,22,1,0,-1,0,-1,false,false,chrome-native://newtab/";
+    private static final String SERIALIZED_NTP_URL =
+            "73,1,true,0,6,0,-1,0,-1,9,6,0,-1,15,1,0,-1,0,-1,false,false,chrome://newtab/";
+
+    private static @Nullable PreNativeGurlHolder sPreNativeNtpGurl;
     private static @Nullable UrlConstantResolver sOriginalResolver;
     private static @Nullable UrlConstantResolver sIncognitoResolver;
     private static @Nullable UrlConstantResolver sResolverForTesting;
@@ -55,6 +63,7 @@ public class UrlConstantResolverFactory {
 
     private static UrlConstantResolver buildOriginalResolver() {
         UrlConstantResolver resolver = new UrlConstantResolver();
+        resolver.registerPreNativeGurl(UrlConstants.NTP_URL, getPreNativeNtpGurlHolder());
         if (!ChromeFeatureList.sChromeNativeUrlOverriding.isEnabled()) return resolver;
 
         resolver.registerOverride(
@@ -80,6 +89,7 @@ public class UrlConstantResolverFactory {
 
     private static UrlConstantResolver buildIncognitoResolver() {
         UrlConstantResolver resolver = new UrlConstantResolver();
+        resolver.registerPreNativeGurl(UrlConstants.NTP_URL, getPreNativeNtpGurlHolder());
         if (!ChromeFeatureList.sChromeNativeUrlOverriding.isEnabled()) return resolver;
 
         resolver.registerOverride(
@@ -106,5 +116,26 @@ public class UrlConstantResolverFactory {
     public static void resetResolvers() {
         sOriginalResolver = null;
         sIncognitoResolver = null;
+    }
+
+    private static PreNativeGurlHolder getPreNativeNtpGurlHolder() {
+        if (sPreNativeNtpGurl == null) {
+            sPreNativeNtpGurl = buildPreNativeNtpGurlHolder();
+        }
+        return sPreNativeNtpGurl;
+    }
+
+    private static PreNativeGurlHolder buildPreNativeNtpGurlHolder() {
+        GURL gurlOverride = null;
+        if (ChromeFeatureList.sChromeNativeUrlOverriding.isEnabled()) {
+            gurlOverride = deserializeGurlString(SERIALIZED_NTP_URL);
+        }
+
+        return new PreNativeGurlHolder(
+                deserializeGurlString(SERIALIZED_NATIVE_NTP_URL), gurlOverride);
+    }
+
+    private static GURL deserializeGurlString(String serializedGurl) {
+        return GURL.deserializeLatestVersionOnly(serializedGurl.replace(',', '\0'));
     }
 }
