a5a089578f66b25f8ffd7162be340d88c34d2fa3
Jihad Hanna <jihadghanna@google.com>
2025-10-22 03:27:10 -0700

    Record whether a user has WP1 setting enabled or disabled
    Bug: 445679087
    Change-Id: I4e7edd66000e5fdba78dd71560ceef7ffb1e9d45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7067655
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1533507}

--- components/autofill/core/browser/webdata/account_settings/account_setting_service.h
+++ components/autofill/core/browser/webdata/account_settings/account_setting_service.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string_view>
 
+#include "base/scoped_observation.h"
+#include "components/autofill/core/browser/webdata/account_settings/account_setting_sync_bridge.h"
 #include "components/keyed_service/core/keyed_service.h"
 
 namespace syncer {
@@ -17,12 +19,11 @@ class DataTypeControllerDelegate;
 
 namespace autofill {
 
-class AccountSettingSyncBridge;
-
 // Manages settings stored in your Google account. These settings differ from
 // regular prefs, since they originate from the user's account and are available
 // beyond Chrome.
-class AccountSettingService : public KeyedService {
+class AccountSettingService : public KeyedService,
+                              public AccountSettingSyncBridge::Observer {
  public:
   explicit AccountSettingService(
       std::unique_ptr<AccountSettingSyncBridge> bridge);
@@ -37,7 +38,14 @@ class AccountSettingService : public KeyedService {
   GetSyncControllerDelegate();
 
  private:
+  // AccountSettingSyncBridge::Observer:
+  void OnDataLoadedFromDisk() override;
+
   std::unique_ptr<AccountSettingSyncBridge> sync_bridge_;
+
+  base::ScopedObservation<AccountSettingSyncBridge,
+                          AccountSettingSyncBridge::Observer>
+      scoped_observation_{this};
 };
 
 }  // namespace autofill
