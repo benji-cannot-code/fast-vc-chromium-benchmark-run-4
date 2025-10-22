767409af8c541ea94a42e68e261badabdc18dc16
Evan Stade <evanstade@microsoft.com>
2025-10-22 07:27:48 -0700

    IDB: SQLite store - handle error case when failing to write blob to DB
    This fixes DlpScopedFileAccessDelegateBrowserTest.UploadFrameIDBProtectedDeny
    where writing fails due to a DLP policy.
    
    Bug: 419208485
    Change-Id: Ibbe70e5bb7d9ae250efce22dfeb4c247a41635c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7056184
    Reviewed-by: Abhishek Shanthkumar <abhishek.shanthkumar@microsoft.com>
    Commit-Queue: Evan Stade <evanstade@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1533601}

--- storage/browser/test/fake_blob.h
+++ storage/browser/test/fake_blob.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef STORAGE_BROWSER_TEST_FAKE_BLOB_H_
 #define STORAGE_BROWSER_TEST_FAKE_BLOB_H_
 
+#include <optional>
 #include <string>
 
 #include "mojo/public/cpp/bindings/pending_remote.h"
@@ -18,7 +19,10 @@ namespace storage {
 class FakeBlob : public blink::mojom::Blob {
  public:
   explicit FakeBlob(const std::string& uuid);
+  ~FakeBlob() override;
 
+  // If no body is set, then reading will return an error through the
+  // BlobReaderClient interface. It's valid to set an empty body.
   void set_body(std::string_view body) { body_ = std::string(body); }
 
   mojo::PendingRemote<blink::mojom::Blob> Clone();
@@ -41,7 +45,7 @@ class FakeBlob : public blink::mojom::Blob {
 
  private:
   const std::string uuid_;
-  std::string body_;
+  std::optional<std::string> body_;
 };
 
 }  // namespace storage
