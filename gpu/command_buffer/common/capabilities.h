b9932361ca2a596c1a5a26031c8b6af8542e6380
Colin Blundell <blundell@chromium.org>
2025-10-21 23:53:33 -0700

    [//gpu] Remove gpu_memory_buffer_support.h (one of them at least)
    This CL reduces the number of gpu_memory_buffer_support.h files in the
    codebase from 2 to 1 by moving the type defined in
    //gpu/command_buffer/common/gpu_memory_buffer_support.h to
    //ui/gfx/buffer_types.h. We'll tackle the other one in a separate
    effort.
    
    Bug: 404905709
    Change-Id: I962549c563a5974e3645c2adc501a217635232be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7062602
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1533427}

--- gpu/command_buffer/common/capabilities.h
+++ gpu/command_buffer/common/capabilities.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/flat_map.h"
 #include "gpu/command_buffer/common/gpu_command_buffer_common_export.h"
-#include "gpu/command_buffer/common/gpu_memory_buffer_support.h"
 #include "ui/gfx/buffer_types.h"
 #include "ui/gfx/surface_origin.h"
 
@@ -71,7 +70,7 @@ struct GPU_COMMAND_BUFFER_COMMON_EXPORT Capabilities {
 
   bool using_vulkan_context = false;
 
-  GpuMemoryBufferFormatSet gpu_memory_buffer_formats = {
+  gfx::GpuMemoryBufferFormatSet gpu_memory_buffer_formats = {
       gfx::BufferFormat::BGR_565,   gfx::BufferFormat::RGBA_4444,
       gfx::BufferFormat::RGBA_8888, gfx::BufferFormat::RGBX_8888,
       gfx::BufferFormat::YVU_420,
