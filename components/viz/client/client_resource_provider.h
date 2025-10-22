f0e93bc520f1edc008891ff34ed226541d1669dc
Zhenyao Mo <zmo@chromium.org>
2025-10-22 14:58:31 -0700

    Change imported_resources_ from flat_map to unordered_map
    flat_map visit is O(n), with a lot of tiles, this is slow.
    
    TEST=manual,bots
    
    Bug: 452168713
    Change-Id: Ife64b7fa134b76d83e079cbf64f26fda7b3b09ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7068679
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Reviewed-by: vikas soni <vikassoni@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1533923}

--- components/viz/client/client_resource_provider.h
+++ components/viz/client/client_resource_provider.h
@@ -7,9 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_VIZ_CLIENT_CLIENT_RESOURCE_PROVIDER_H_
 
 #include <memory>
+#include <unordered_map>
 #include <vector>
 
-#include "base/containers/flat_map.h"
 #include "base/functional/callback.h"
 #include "base/memory/weak_ptr.h"
 #include "base/task/sequenced_task_runner.h"
@@ -172,7 +172,7 @@ class VIZ_CLIENT_EXPORT ClientResourceProvider {
 
   THREAD_CHECKER(thread_checker_);
 
-  base::flat_map<ResourceId, ImportedResource> imported_resources_;
+  std::unordered_map<ResourceId, ImportedResource> imported_resources_;
   // The ResourceIds in ClientResourceProvider start from 1 to avoid
   // conflicts with id from DisplayResourceProvider.
   ResourceIdGenerator id_generator_;
