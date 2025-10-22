31bfed549207eb9a854c5e7681556915d8380e1e
Tomasz Wiszkowski <ender@google.com>
2025-10-22 11:23:07 -0700

    Respect AIM upload-ability restrictions.
    Change-Id: I11eca9986aadd796f1e0b4a504e02bdf5676a9f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7070230
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1533786}

--- chrome/browser/android/omnibox/composebox_query_controller_bridge.cc
+++ chrome/browser/android/omnibox/composebox_query_controller_bridge.cc
@@ -39,7 +39,8 @@ static jlong JNI_ComposeBoxQueryControllerBridge_Init(JNIEnv* env,
 }
 
 ComposeboxQueryControllerBridge::ComposeboxQueryControllerBridge(
-    Profile* profile) {
+    Profile* profile)
+    : profile_{profile} {
   auto query_controller_config_params = std::make_unique<
       ComposeboxQueryController::QueryControllerConfigParams>();
   query_controller_config_params->send_lns_surface = false;
@@ -80,8 +81,14 @@ ComposeboxQueryControllerBridge::AddFile(
 
   std::optional<lens::ImageEncodingOptions> image_options = std::nullopt;
   lens::MimeType mime_type;
+  AimEligibilityService* aim_service =
+      AimEligibilityServiceFactory::GetForProfile(profile_);
 
   if (file_type.find("pdf") != std::string::npos) {
+    if (!aim_service->IsPdfUploadEligible()) {
+      return {};
+    }
+
     mime_type = lens::MimeType::kPdf;
   } else if (file_type.find("image") != std::string::npos) {
     mime_type = lens::MimeType::kImage;
@@ -91,7 +98,8 @@ ComposeboxQueryControllerBridge::AddFile(
                                                .max_width = 1600,
                                                .compression_quality = 40};
   } else {
-    NOTREACHED();
+    // Unsupported mime type.
+    return {};
   }
 
   std::unique_ptr<lens::ContextualInputData> input_data =
--- chrome/browser/android/omnibox/composebox_query_controller_bridge.h
+++ chrome/browser/android/omnibox/composebox_query_controller_bridge.h
@@ -42,6 +42,7 @@ class ComposeboxQueryControllerBridge
       const std::optional<FileUploadErrorType>& error_type) override;
 
  private:
+  raw_ptr<Profile> profile_;
   std::unique_ptr<ComposeboxQueryController> query_controller_;
 };
 
