3717b36aea96bde71f62ad89e39e64b579ee72c6
Colin Blundell <blundell@chromium.org>
2025-10-22 00:47:27 -0700

    [//gpu] Remove GL_COMMANDS_ISSUED_TIMESTAMP_CHROMIUM
    This is now unused. There is more cleanup to do, which I left for
    followup to minimize the size of this already large CL.
    
    Bug: 450466845
    Change-Id: I7dea734752ec214aa23fae5b95f3ba1498c88710
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7067892
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1533455}

--- gpu/command_buffer/build_raster_cmd_buffer.py
+++ gpu/command_buffer/build_raster_cmd_buffer.py
@@ -50,7 +50,6 @@ _NAMED_TYPE_INFO = {
     'is_complete': True,
     'valid': [
       'GL_COMMANDS_ISSUED_CHROMIUM',
-      'GL_COMMANDS_ISSUED_TIMESTAMP_CHROMIUM',
       'GL_COMMANDS_COMPLETED_CHROMIUM',
     ],
   },
