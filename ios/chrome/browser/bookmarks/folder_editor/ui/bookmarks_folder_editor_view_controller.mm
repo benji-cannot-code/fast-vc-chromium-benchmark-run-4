35888aadee23487b062a20fde91859ef90d00f38
Arthur Milchior <arthurmilchior@google.com>
2025-10-22 12:20:17 -0700

    [iOS][Bookmarks] Move folder_editor to its own folder
    This CL moves the bookmarks folder editor to its own folder to follow
    the new bookmarks structure.
    
    CL done by Gemini
    
    Bypass-Check-License: Moved files
    Bug: None
    Change-Id: I634faa5769072cf15c67133255f0456dbdf1269c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7072811
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Huiting Yu <huitingyu@google.com>
    Commit-Queue: Huiting Yu <huitingyu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1533834}

--- ios/chrome/browser/bookmarks/folder_editor/ui/bookmarks_folder_editor_view_controller.mm
+++ ios/chrome/browser/bookmarks/folder_editor/ui/bookmarks_folder_editor_view_controller.mm
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2014 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-#import "ios/chrome/browser/bookmarks/ui_bundled/folder_editor/bookmarks_folder_editor_view_controller.h"
+#import "ios/chrome/browser/bookmarks/folder_editor/ui/bookmarks_folder_editor_view_controller.h"
 
 #import <memory>
 #import <set>
