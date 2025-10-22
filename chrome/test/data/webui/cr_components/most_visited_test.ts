c9f9bfa3f1ab78c68e0b615919311e2df7a9647e
Sho Mizoe <shomizoe@google.com>
2025-10-22 13:53:11 -0700

    [ntp][next] Fix the Most-visited component to load NTP 3P UI
    This CL updates the logic in most-visited.{ts,html} to change its
    behavior based on values passed from outside the component.
    
    The following are manually confirmed:
    
    - When NtpNextFeatures is enabled: https://screenshot.googleplex.com/4fWQKBj5z8WJGbG
    - The said feature is disabled: https://screenshot.googleplex.com/4yZyS5LcRturXGw
    - NTP 3P: https://screenshot.googleplex.com/6VH97maQJD4gxok
    
    Fixed: b:452896823
    Change-Id: I76013b14281f72ce225c2e633a9da69a6792d62f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7064138
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Commit-Queue: Sho Mizoe <shomizoe@google.com>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1533892}

--- chrome/test/data/webui/cr_components/most_visited_test.ts
+++ chrome/test/data/webui/cr_components/most_visited_test.ts
@@ -158,15 +158,30 @@ function leaveUrlInput() {
   return microtasksFinished();
 }
 
-function setUpTest(singleRow: boolean, reflowOnOverflow: boolean) {
+interface SetUpTestOptions {
+  singleRow: boolean;
+  reflowOnOverflow: boolean;
+  enableShowMoreButton: boolean;
+}
+
+function setUpTest(providedOptions: Partial<SetUpTestOptions> = {}) {
+  const defaultOptions = {
+    singleRow: false,
+    reflowOnOverflow: false,
+    enableShowMoreButton: false,
+  };
+  const options = {...defaultOptions, ...providedOptions};
   document.body.innerHTML = window.trustedTypes!.emptyHTML;
 
   createBrowserProxy();
   createWindowProxy();
 
   mostVisited = new MostVisitedElement();
-  mostVisited.singleRow = singleRow;
-  mostVisited.reflowOnOverflow = reflowOnOverflow;
+  mostVisited.singleRow = options.singleRow;
+  mostVisited.reflowOnOverflow = options.reflowOnOverflow;
+  if (options.enableShowMoreButton) {
+    mostVisited.setAttribute('enable-show-more-button', '');
+  }
   document.body.appendChild(mostVisited);
   assertEquals(1, handler.getCallCount('updateMostVisitedInfo'));
   return wide();
@@ -174,7 +189,7 @@ function setUpTest(singleRow: boolean, reflowOnOverflow: boolean) {
 
 suite('General', () => {
   setup(async () => {
-    await setUpTest(/*singleRow=*/ false, /*reflowOnOverflow=*/ false);
+    await setUpTest();
   });
 
   test('empty shows add shortcut only', async () => {
@@ -211,13 +226,12 @@ suite('General', () => {
 suite('ShowMoreButton', () => {
   suiteSetup(() => {
     loadTimeData.overrideValues({
-      ntpNextFeaturesEnabled: true,
       maxTilesBeforeShowMore: MAX_TILES_BEFORE_SHOW_MORE,
     });
   });
 
   test('Show more button is shown with 6 or more tiles', async () => {
-    await setUpTest(false, true);
+    await setUpTest({reflowOnOverflow: true, enableShowMoreButton: true});
     await addTiles(MAX_TILES_BEFORE_SHOW_MORE + 1);
     assertTrue(isVisible(getShowMoreButton()));
     assertAddShortcutHidden();
@@ -225,7 +239,7 @@ suite('ShowMoreButton', () => {
   });
 
   test('Show more button is hidden with 5 or fewer tiles', async () => {
-    await setUpTest(false, true);
+    await setUpTest({reflowOnOverflow: true, enableShowMoreButton: true});
     await addTiles(MAX_TILES_BEFORE_SHOW_MORE);
     assertFalse(isVisible(getShowMoreButton()));
     assertAddShortcutShown();
@@ -234,7 +248,7 @@ suite('ShowMoreButton', () => {
   test(
       'Clicking the show more button shows all tiles and hides the button',
       async () => {
-        await setUpTest(false, true);
+        await setUpTest({reflowOnOverflow: true, enableShowMoreButton: true});
         await addTiles(MAX_TILES_BEFORE_SHOW_MORE + 2);  // 7 tiles.
         const showMoreButton = getShowMoreButton();
         assertTrue(isVisible(showMoreButton));
@@ -252,7 +266,7 @@ suite('ShowMoreButton', () => {
 
 function createLayoutsSuite(singleRow: boolean, reflowOnOverflow: boolean) {
   setup(async () => {
-    await setUpTest(singleRow, reflowOnOverflow);
+    await setUpTest({singleRow, reflowOnOverflow});
   });
 
   test('four tiles fit on one line with addShortcut', async () => {
@@ -384,7 +398,7 @@ function createLayoutsSuite(singleRow: boolean, reflowOnOverflow: boolean) {
 function createLayoutsWidthsSuite(singleRow: boolean) {
   suite('test various widths', () => {
     setup(async () => {
-      await setUpTest(singleRow, false);
+      await setUpTest({singleRow});
     });
 
     test('six / three is max for narrow', async () => {
@@ -493,7 +507,7 @@ function columnCount(): number {
 function createLayoutsWidthsReflowSuite(singleRow: boolean) {
   suite('test reflow on various widths', () => {
     setup(async () => {
-      await setUpTest(singleRow, /*reflowOnOverflow=*/ true);
+      await setUpTest({singleRow, reflowOnOverflow: true});
     });
 
     test('No hidden tiles', async () => {
@@ -563,7 +577,7 @@ suite('Reflow Layouts', () => {
 
 suite('LoggingAndUpdates', () => {
   setup(async () => {
-    await setUpTest(/*singleRow=*/ false, /*reflowOnOverflow=*/ false);
+    await setUpTest();
   });
 
   test('rendering tiles logs event', async () => {
@@ -654,7 +668,7 @@ suite('Modification', () => {
   });
 
   setup(async () => {
-    await setUpTest(/*singleRow=*/ false, /*reflowOnOverflow=*/ false);
+    await setUpTest();
   });
 
   suite('add dialog', () => {
@@ -1159,7 +1173,7 @@ suite('Modification', () => {
 
 function createDragAndDropSuite(singleRow: boolean, reflowOnOverflow: boolean) {
   setup(async () => {
-    await setUpTest(singleRow, reflowOnOverflow);
+    await setUpTest({singleRow, reflowOnOverflow});
   });
 
   test('drag first tile to second position', async () => {
@@ -1272,7 +1286,7 @@ suite('DragAndDrop', () => {
 
 suite('Theming', () => {
   setup(async () => {
-    await setUpTest(/*singleRow=*/ false, /*reflowOnOverflow=*/ false);
+    await setUpTest();
   });
 
   test('RIGHT_TO_LEFT tile title text direction', async () => {
@@ -1339,7 +1353,7 @@ suite('Preloading', () => {
   suiteSetup(() => {});
 
   setup(async () => {
-    await setUpTest(/*singleRow=*/ false, /*reflowOnOverflow=*/ false);
+    await setUpTest();
   });
 
   test('onMouseHover Trigger', async () => {
@@ -1386,7 +1400,7 @@ suite('EnterpriseShortcuts', () => {
   suiteSetup(() => {});
 
   setup(async () => {
-    await setUpTest(/*singleRow=*/ false, /*reflowOnOverflow=*/ false);
+    await setUpTest();
   });
 
   function createEnterpriseShortcut(
