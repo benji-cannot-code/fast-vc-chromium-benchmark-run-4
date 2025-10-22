2f8099bdf635e630196ff3ac02acf6026ad698b2
Dominik Bylica <bylica@google.com>
2025-10-22 03:35:06 -0700

    [WebPrinting] Do not mention navigator.
    `printing` now originates from a global scope, so this needed to be
    cleaned up.
    
    Bug: 453926041
    Change-Id: I4cb425e92cf1725dd97b55a13e192afc65bd2034
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7066775
    Commit-Queue: Dominik Bylica <bylica@google.com>
    Reviewed-by: Vlad Krot <vkrot@google.com>
    Auto-Submit: Dominik Bylica <bylica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1533510}

--- third_party/blink/renderer/modules/printing/web_printing_manager.cc
+++ third_party/blink/renderer/modules/printing/web_printing_manager.cc
@@ -10,10 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/browser_interface_broker_proxy.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
-#include "third_party/blink/renderer/core/execution_context/navigator_base.h"
+#include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/modules/printing/web_printer.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_hash_set.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
+#include "third_party/blink/renderer/platform/supplementable.h"
 #include "third_party/blink/renderer/platform/wtf/functional.h"
 
 namespace blink {
