4a7242b3cc2387e948bb741fcd00e1af53a07600
Egor Pasko <pasko@chromium.org>
2025-10-22 07:07:01 -0700

    Reformat c/b/about_flags.cc
    Presubmit was complaining that formatting is wrong in lines far away
    from the modified ones (in another change of mine).
    
    So I started with:
      git cl format --full chrome/browser/about_flags.cc
    
    But then droger@ noticed that the automatic formatting looks wrong for
    one comment line, where the line wants to get attached to a comment for
     #endif. So we added a newline and reformatted again in full.
    
    Bug: None
    Change-Id: I021e8e464aed9af565dab50437eb26e7b7ea28b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7073071
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1533584}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1648,10 +1648,8 @@ const FeatureEntry::FeatureParam kShowToolsAndModels[] = {
     {"ForceToolsAndModels", "true"},
 };
 const FeatureEntry::FeatureParam kShowCreateImageTool[] = {
-    {"ShowContextMenu", "true"},
-    {"ShowToolsAndModels", "true"},
-    {"ShowCreateImageTool", "true"},
-    {"ShowComposeboxZps", "true"},
+    {"ShowContextMenu", "true"},     {"ShowToolsAndModels", "true"},
+    {"ShowCreateImageTool", "true"}, {"ShowComposeboxZps", "true"},
     {"ForceToolsAndModels", "true"},
 };
 const FeatureEntry::FeatureParam kComposeboxNext[] = {
@@ -13406,8 +13404,8 @@ const FeatureEntry kFeatureEntries[] = {
                                     kProfilePickerTextVariations,
                                     "ProfilePickerTextVariations")},
 #endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
-    // Add new entries above this line.
 
+    // Add new entries above this line.
     // NOTE: Adding a new flag requires adding a corresponding entry to enum
     // "LoginCustomFlags" in tools/metrics/histograms/enums.xml. See "Flag
     // Histograms" in tools/metrics/histograms/README.md (run the
