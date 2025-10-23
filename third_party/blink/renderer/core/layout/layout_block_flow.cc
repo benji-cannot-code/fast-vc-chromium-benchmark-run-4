3b807ce599a46c7ca388c88ecc9bd969437da525
Steve Kobes <skobes@chromium.org>
2025-10-23 10:30:50 -0700

    Fix crash when <html> element becomes position: fixed.
    We shouldn't move the document element's LayoutObject into the anonymous
    LayoutViewTransitionRoot. Instead, let them remain siblings.
    
    Bug: 445765272
    Change-Id: I28030d6ea7a44c6676779b50eae478f053538db9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7077378
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1534436}

--- third_party/blink/renderer/core/layout/layout_block_flow.cc
+++ third_party/blink/renderer/core/layout/layout_block_flow.cc
@@ -399,7 +399,7 @@ void LayoutBlockFlow::ChildBecameFloatingOrOutOfFlow(LayoutBox* child) {
     return;
   }
   auto* next = DynamicTo<LayoutBlockFlow>(child->NextSibling());
-  if (next && next->IsAnonymousBlockFlow()) {
+  if (next && next->IsAnonymousBlockFlow() && !next->IsViewTransitionRoot()) {
     MoveChildTo(next, child, next->FirstChild(), false);
   }
 }
