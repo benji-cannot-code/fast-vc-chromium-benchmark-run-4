d4b3e0320c1b6f44416174fe4a632b60bd92a148
Adam Rice <ricea@chromium.org>
2025-10-23 12:42:18 -0700

    absl::Hash support for base::StrongAlias
    Implement AbslHashValue() for base::StrongAlias so that it can be used
    as a key in Abseil hash tables.
    
    Adjust BUILD.gn and DEPS files to permit AbslHashValue() implementations
    to be tested with the absl::VerifyTypeImplementsAbslHashCorrectly()
    function. It is helpful to find incorrect hashing implementations.
    
    This CL has the side-effect of moving some types that were using
    std::hash specializations to use Abseil's native hash implementation
    instead. This is expected to be strictly beneficial.
    
    BUG=452082294
    
    Change-Id: I7c22d7d19146e967d91323b17a878983ff5b7d34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7066374
    Reviewed-by: Danil Chapovalov <danilchap@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1534537}

--- base/types/strong_alias_unittest.cc
+++ base/types/strong_alias_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/types/supports_ostream_operator.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/abseil-cpp/absl/hash/hash_testing.h"
 #include "third_party/perfetto/include/perfetto/test/traced_value_test_support.h"
 
 namespace base {
@@ -385,6 +386,15 @@ void StreamOperatorExists() {
 TEST(StrongAliasTest, TracedValueSupport) {
   using IntAlias = StrongAlias<class FooTag, int>;
   EXPECT_EQ(perfetto::TracedValueToString(IntAlias(42)), "42");
+}
+
+TYPED_TEST(StrongAliasTest, AbslHashValue) {
+  using FooAlias = StrongAlias<class FooTag, TypeParam>;
+
+  EXPECT_TRUE(absl::VerifyTypeImplementsAbslHashCorrectly(
+      {FooAlias(GetExampleValue<TypeParam>(0)),
+       FooAlias(GetExampleValue<TypeParam>(0)),
+       FooAlias(GetExampleValue<TypeParam>(1))}));
 }
 
 }  // namespace base
