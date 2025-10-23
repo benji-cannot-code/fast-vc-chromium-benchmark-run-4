c53319e90ff5f1f3e772dc1fbd376bfc99fa5286
Arthur Sonzogni <arthursonzogni@chromium.org>
2025-10-23 10:24:39 -0700

    Fix unsafe buffer usage in remoting/protocol/fake_datagram_socket.cc
    Replaced raw pointer parameters with base::span and net::IOBuffer::first()
    to fix unsafe buffer operations.
    
    Bug: 435317390
    Bug: 454533341
    Fixed: 454533341
    Change-Id: I6cee41ed89182dc0e4b161448dc0e138e38c326f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7075193
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Commit-Queue: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1534429}

--- remoting/protocol/fake_datagram_socket.cc
+++ remoting/protocol/fake_datagram_socket.cc
@@ -9,8 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/containers/contains.h"
+#include "base/containers/span.h"
 #include "base/functional/bind.h"
 #include "base/location.h"
+#include "base/numerics/safe_conversions.h"
+#include "base/strings/string_view_util.h"
 #include "base/task/single_thread_task_runner.h"
 #include "net/base/address_list.h"
 #include "net/base/io_buffer.h"
@@ -104,16 +107,14 @@ int FakeDatagramSocket::DoSend(const scoped_refptr<net::IOBuffer>& buf,
     return r;
   }
 
-  written_packets_.push_back(std::string());
-  written_packets_.back().assign(buf->data(),
-                                 UNSAFE_TODO(buf->data() + buf_len));
+  auto packet = buf->first(base::checked_cast<size_t>(buf_len));
+  written_packets_.emplace_back(base::as_string_view(packet));
 
   if (peer_socket_.get()) {
     task_runner_->PostTask(
         FROM_HERE,
-        base::BindOnce(
-            &FakeDatagramSocket::AppendInputPacket, peer_socket_,
-            std::string(buf->data(), UNSAFE_TODO(buf->data() + buf_len))));
+        base::BindOnce(&FakeDatagramSocket::AppendInputPacket, peer_socket_,
+                       std::string(base::as_string_view(packet))));
   }
 
   return buf_len;
@@ -121,12 +122,12 @@ int FakeDatagramSocket::DoSend(const scoped_refptr<net::IOBuffer>& buf,
 
 int FakeDatagramSocket::CopyReadData(const scoped_refptr<net::IOBuffer>& buf,
                                      int buf_len) {
-  int size =
-      std::min(buf_len, static_cast<int>(input_packets_[input_pos_].size()));
-  UNSAFE_TODO(
-      memcpy(buf->data(), &(*input_packets_[input_pos_].begin()), size));
+  const size_t read_size = std::min(base::checked_cast<size_t>(buf_len),
+                                    input_packets_[input_pos_].size());
+  buf->span().copy_prefix_from(
+      base::as_byte_span(input_packets_[input_pos_]).first(read_size));
   ++input_pos_;
-  return size;
+  return read_size;
 }
 
 FakeDatagramChannelFactory::FakeDatagramChannelFactory()
