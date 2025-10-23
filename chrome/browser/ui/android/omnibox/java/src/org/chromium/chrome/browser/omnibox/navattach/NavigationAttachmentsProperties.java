9470e176894c06ce64e4468c657d9f2b527239ac
Tomasz Wiszkowski <ender@google.com>
2025-10-23 16:10:05 -0700

    Add AI Mode button to Attachments popup.
    Bug: 450966731
    Change-Id: I02f6e42f23e7660a6c497f4f1ce2370ee3617d9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7081092
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1534661}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/navattach/NavigationAttachmentsProperties.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/navattach/NavigationAttachmentsProperties.java
@@ -51,6 +51,10 @@ class NavigationAttachmentsProperties {
     public static final WritableObjectPropertyKey<Callback<Boolean>> ON_USE_AI_MODE_CHANGED =
             new WritableObjectPropertyKey<>();
 
+    /** Action to perform when the user clicks the AI Mode button in the popup. */
+    public static final WritableObjectPropertyKey<Runnable> POPUP_AI_MODE_CLICKED =
+            new WritableObjectPropertyKey<>();
+
     /** Action to perform when the user clicks the Camera button in the popup. */
     public static final WritableObjectPropertyKey<Runnable> POPUP_CAMERA_CLICKED =
             new WritableObjectPropertyKey<>();
@@ -85,6 +89,7 @@ class NavigationAttachmentsProperties {
         BUTTON_ADD_CLICKED,
         NAVIGATION_TYPE_VISIBLE,
         ON_USE_AI_MODE_CHANGED,
+        POPUP_AI_MODE_CLICKED,
         POPUP_CAMERA_CLICKED,
         POPUP_CLIPBOARD_BUTTON_VISIBLE,
         POPUP_CLIPBOARD_CLICKED,
