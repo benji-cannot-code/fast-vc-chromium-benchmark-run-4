a46b4d8af3aeccfd75a11ee0a78fdfb32ee5e5a0
Nihar Majmudar <niharm@google.com>
2025-10-23 15:55:18 -0700

    [composebox] Don't clear matches if image zps disabled.
    Dropdown hidden which is sufficient for hiding any matches from users
    when an image is uploaded. This still works for create image mode
    because that follows a different branch of logic.
    
    Change-Id: Ib2507e0a667de80787f0ea92ab0a1ca611059d08
    Bug: 454676093
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7080466
    Reviewed-by: Marlon Facey <mfacey@chromium.org>
    Commit-Queue: Nihar Majmudar <niharm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1534650}

--- ui/webui/resources/cr_components/composebox/composebox.ts
+++ ui/webui/resources/cr_components/composebox/composebox.ts
@@ -866,7 +866,6 @@ export class ComposeboxElement extends I18nMixinLit
           this.queryAutocomplete(/* clearMatches= */ true);
         } else {
           this.showDropdown_ = false;
-          this.clearAutocompleteMatches_();
         }
       }
 
