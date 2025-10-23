3fc1cb423dc00ba7cb445935b469aca2be5e79eb
Kim-Anh Tran <kimanh@chromium.org>
2025-10-23 04:28:07 -0700

    Add reload-page-with-log.js webtest
    reload-with-script.js is currently flaky and is pointing towards a bug
    that we have (script may not run at the right time, or potentially it is
    a console logs related issue). This adds a new test which checks if we
    in general retain logs on reloads.
    
    Bug: 443229243
    Change-Id: I741d3813f7f4384e4b0bbc0b84075b6e2a3d4185
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7073711
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Kim-Anh Tran <kimanh@chromium.org>
    Reviewed-by: Philip Pfaffe <pfaffe@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1534223}

---
+++ third_party/blink/web_tests/inspector-protocol/page/reload-page-with-log.js
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(/** @type {import('test_runner').TestRunner} */ testRunner) {
+  const {session, dp} = await testRunner.startBlank(
+      'Tests that reloading a page with its own logs retains the logs.');
+
+  dp.Runtime.enable();
+  const firstLogPromise = dp.Runtime.onceConsoleAPICalled();
+  dp.Page.enable();
+
+  testRunner.log('Initial load:');
+  await dp.Page.navigate({url: testRunner.url('../resources/console-log.html')});
+  let event = await firstLogPromise;
+  testRunner.log(`PAGE: ${event.params.args[0].value}`);
+
+  const secondLogPromise = dp.Runtime.onceConsoleAPICalled();
+  testRunner.log('Reloading page:');
+  dp.Page.reload();
+  event = await secondLogPromise;
+  testRunner.log(`PAGE: ${event.params.args[0].value}`);
+
+  testRunner.completeTest();
+})
