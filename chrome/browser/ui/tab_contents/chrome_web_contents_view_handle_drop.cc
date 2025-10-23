c4744b7aa6d967c32ad06edb601a11abc46848c8
Alison Gale <agale@chromium.org>
2025-10-23 09:25:35 -0700

    [SxS] Hide drop target when web contents handles drop
    Basically in any situation where we are about to show the drop target or
    nudge and the user drops a link in the web contents, we miss the drop
    end events that cause us to hide the nudge or drop target.
    
    Change-Id: I250a2c0ac8716b7a8c1ee89426cfae2482b495d9
    Bug: 450560773
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7045939
    Commit-Queue: Alison Gale <agale@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1534391}

--- chrome/browser/ui/tab_contents/chrome_web_contents_view_handle_drop.cc
+++ chrome/browser/ui/tab_contents/chrome_web_contents_view_handle_drop.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/enterprise/common/files_scan_data.h"
 #include "components/safe_browsing/buildflags.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/browser/web_contents_delegate.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_view_delegate.h"
 #include "content/public/common/drop_data.h"
@@ -151,6 +152,9 @@ void HandleOnPerformingDrop(
     content::WebContentsViewDelegate::DropCompletionCallback callback) {
   CHECK(callback);
   absl::Cleanup cleanup = [&] {
+    if (web_contents->GetDelegate()) {
+      web_contents->GetDelegate()->HandleDragEnded();
+    }
     std::move(callback).Run(std::move(drop_data));
   };
 
