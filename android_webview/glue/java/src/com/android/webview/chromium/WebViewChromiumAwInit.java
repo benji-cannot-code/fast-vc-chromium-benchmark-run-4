5a44d083dd8d0587339359ef435d552e17f4d136
Chidera Olibie <colibie@google.com>
2025-10-23 13:00:02 -0700

    [wv][experiment] Add flag to stop isMultiprocessEnabled from starting Chromium
    This cl extends the startuptasks androidx api with a flag to stop
    isMultiProcessEnabled from kickstarting startup when running startup
    async.
    
    Previously, the call starts up chromium in order to access the
    isMultiProcess from RenderProcessHost. RenderProcessHost gets its
    multiprocess value from a commandline check. We already have access to
    this value earlier on during WebViewChromiumFactoryProvider
    initialization.
    
    Bug: 397372092, 314273745
    Change-Id: Ifd51c05edcdf62ca2b0adba0896b0b79c337916c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7079050
    Commit-Queue: Chidera Olibie <colibie@google.com>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1534550}

--- android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumAwInit.java
+++ android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumAwInit.java
@@ -1097,6 +1097,14 @@ public class WebViewChromiumAwInit {
         return mFactory.getSharedStatics();
     }
 
+    boolean isMultiProcessEnabled() {
+        return mFactory.isMultiProcessEnabled();
+    }
+
+    boolean isAsyncStartupWithMultiProcessExperimentEnabled() {
+        return mFactory.isAsyncStartupWithMultiProcessExperimentEnabled();
+    }
+
     public AwTracingController getAwTracingController() {
         triggerAndWaitForChromiumStarted(CallSite.GET_AW_TRACING_CONTROLLER);
         return mChromiumStartedGlobals.mAwTracingController;
