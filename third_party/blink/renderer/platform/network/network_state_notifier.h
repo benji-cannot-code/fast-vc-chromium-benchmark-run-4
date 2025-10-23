f378932de3a83e824825b80d5e46e6dd5f46fd2f
Eden Wang <nedenwang@gmail.com>
2025-10-23 04:09:41 -0700

    DevTools: Fix crash and logic for network condition emulation
    Passing a throughput of -1 to Network.emulateNetworkConditions could
    cause a renderer crash due to a failed DCHECK in NetworkStateNotifier
    when reading navigator.connection properties.
    
    Additionally, a throughput of 0 was incorrectly treated as a command
    to clear emulation settings, rather than applying full throttling.
    
    Bug: 452681511
    Change-Id: I13194f3f333b1f79d018a752bca278afee264bbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7055463
    Auto-Submit: Wang Neden <nedenwang@gmail.com>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1534215}

--- third_party/blink/renderer/platform/network/network_state_notifier.h
+++ third_party/blink/renderer/platform/network/network_state_notifier.h
@@ -52,7 +52,7 @@ class PLATFORM_EXPORT NetworkStateNotifier {
 
  public:
   struct NetworkState {
-    static const int kInvalidMaxBandwidth = -1;
+    static constexpr int kInvalidMaxBandwidth = -1;
     bool on_line_initialized = false;
     bool on_line = true;
     bool connection_initialized = false;
@@ -250,7 +250,7 @@ class PLATFORM_EXPORT NetworkStateNotifier {
       WebConnectionType,
       std::optional<WebEffectiveConnectionType> effective_type,
       int64_t http_rtt_msec,
-      double max_bandwidth_mbps);
+      std::optional<double> max_bandwidth_mbps);
   void SetSaveDataEnabledOverride(bool enabled);
   void ClearOverride();
 
