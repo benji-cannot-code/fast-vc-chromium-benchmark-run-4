449430f2ddeb6601593dc2e95dde9fd004d272dd
Colin Blundell <blundell@chromium.org>
2025-10-23 00:29:01 -0700

    [//cc] Remove RasterInterface::GetQueryObjectui64V()
    Nothing calls this method any longer.
    
    Bug: 450466845
    Change-Id: I867b894eeabf2f91fb77021f71ff7e52e447755e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7073811
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1534127}

--- gpu/command_buffer/build_raster_cmd_buffer.py
+++ gpu/command_buffer/build_raster_cmd_buffer.py
@@ -261,10 +261,6 @@ _FUNCTION_INFO = {
     'type': 'NoCommand',
     'gl_test_func': 'glGetQueryObjectuiv',
   },
-  'GetQueryObjectui64vEXT': {
-    'type': 'NoCommand',
-    'gl_test_func': 'glGetQueryObjectui64v',
-  },
   'OrderingBarrierCHROMIUM': {
     'type': 'NoCommand',
   },
