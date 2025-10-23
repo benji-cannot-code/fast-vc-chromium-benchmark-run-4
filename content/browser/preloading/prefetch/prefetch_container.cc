f5f06c49fc1943ac92d4861de8b761180c0eed26
Hiroshige Hayashizaki <hiroshige@chromium.org>
2025-10-23 12:31:44 -0700

    Do not call PrefetchContainer::Observer::OnDeterminedHead() inside dtor
    Before this CL, `PrefetchContainer::OnDeterminedHead()` and thus
    `PrefetchContainer::Observer::OnDeterminedHead()` have been called after
    `PrefetchContainer::Observer::OnWillBeDestroyed()` when
    `PrefetchContainer` is destroyed during redirect eligibility check.
    
    This CL makes such `PrefetchContainer::OnDeterminedHead()` no-op when
    called during dtor, in order to:
    
    - Prevent observer callbacks after `OnWillBeDestroyed()`.
    - Prepare for further changes for crbug.com/400761083 without worrying
      about behavior differences around
      `PrefetchContainer::OnDeterminedHead()` calls inside dtor (which
      will be anyway no-op after this CL).
    
    The behavior changes should be OK (and guarded by the kill switch
    `kPrefetchGracefulNotification` just in case):
    
    - Changes to `LoadState` and No-Vary-Search data aren't used because
      `PrefetchContainer` is anyway about to be destroyed.
    - `time_header_determined_successfully_` anyway hasn't been set since
      before this CL in this case (head is not received).
    - Skipping `PrefetchContainer::Observer::OnDeterminedHead()`:
      - `PrefetchMatchResolver::OnDeterminedHead()` already has
        `CHECK(!prefetch_container.is_in_dtor());`. So skipping the call
        here also fixes the CHECK failure (which has been extremely rarely
        observed).
      - `PrefetchContainerObserver::OnDeterminedHead()` (used by
        `PrefetchHandleImpl`) has been no-op in
        this case because head is not received.
      - Other `OnDeterminedHead()` overrides are empty.
    
    Bug: 400761083
    Change-Id: Ibac728ef064b84def6d9796e941943f22b40caa1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6861598
    Reviewed-by: Ken Okada <kenoss@chromium.org>
    Reviewed-by: Taiyo Mizuhashi <taiyo@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1534532}

--- content/browser/preloading/prefetch/prefetch_container.cc
+++ content/browser/preloading/prefetch/prefetch_container.cc
@@ -893,6 +893,14 @@ void PrefetchContainer::CancelStreamingURLLoaderIfNotServing() {
 }
 
 void PrefetchContainer::OnDeterminedHead() {
+  if (base::FeatureList::IsEnabled(features::kPrefetchGracefulNotification) &&
+      is_in_dtor_) {
+    // This can be called due to the loader cancellation during the
+    // `PrefetchContainer` destruction. No state changes should be made and
+    // observers shouldn't be notified during destruction.
+    return;
+  }
+
   SetLoadState(LoadState::kDeterminedHead);
 
   if (GetNonRedirectHead()) {
