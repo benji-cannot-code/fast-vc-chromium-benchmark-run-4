32e38bb0ba6bfe84f39db5bacf2178bd7dbfcb03
Sylvain Defresne <sdefresne@chromium.org>
2025-10-23 01:26:26 -0700

    Disable ManageSyncSettingsTestCase/testSwitchAccountFromAccountMenu
    The test is flaky, causing the bots to fail (or retry it and risk
    getting over the allocated time for running all the tests).
    
    Bug: 454271530
    Change-Id: I6c57bcbc6fa5f0cad8caf524cc60c7cc3a571e02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7071836
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1534152}

--- ios/chrome/browser/settings/ui_bundled/google_services/manage_sync_settings_egtest.mm
+++ ios/chrome/browser/settings/ui_bundled/google_services/manage_sync_settings_egtest.mm
@@ -178,7 +178,7 @@ void ExpectBatchUploadConfirmationSnackbar(int count, NSString* email) {
     config.features_enabled.push_back(kLinkedServicesSettingIos);
   }
 
-  if ([self isRunningTest:@selector(testSwitchAccountFromAccountMenu)] ||
+  if ([self isRunningTest:@selector(DISABLED_testSwitchAccountFromAccountMenu)] ||
       [self isRunningTest:@selector(testSignOutFromAccountFromAccountMenu)]) {
     config.features_enabled.push_back(kSeparateProfilesForManagedAccounts);
   }
@@ -1656,7 +1656,7 @@ void ExpectBatchUploadConfirmationSnackbar(int count, NSString* email) {
 }
 
 // Test switching account from the account menu.
-- (void)testSwitchAccountFromAccountMenu {
+- (void)DISABLED_testSwitchAccountFromAccountMenu {
   FakeSystemIdentity* fakeIdentity = [FakeSystemIdentity fakeIdentity1];
   FakeSystemIdentity* fakeIdentity2 = [FakeSystemIdentity fakeIdentity2];
   [SigninEarlGrey addFakeIdentity:fakeIdentity];
