92e74f7767606d2f2f46e58a9a888971f1d46810
Matt Reynolds <mattreynolds@google.com>
2025-10-22 19:01:03 -0700

    bluetooth: Add timeout=long in web tests
    Adds META: timeout=long or <meta name="timeout" content="long">
    to (non-generated) web tests in
    
    //third_party/blink/web_test/wpt_internal/bluetooth
    
    Bug: 454418870
    Change-Id: I16315227d62da05f071d44b30ab3aa0234bcb847
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7075876
    Reviewed-by: Alvin Ji <alvinji@chromium.org>
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Auto-Submit: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1534031}

--- third_party/blink/web_tests/wpt_internal/bluetooth/device/watchAdvertisements/manufacturer-uuid-zero-or-ffff.https.window.js
+++ third_party/blink/web_tests/wpt_internal/bluetooth/device/watchAdvertisements/manufacturer-uuid-zero-or-ffff.https.window.js
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: script=/resources/testdriver-vendor.js
 // META: script=/bluetooth/resources/bluetooth-test.js
 // META: script=/bluetooth/resources/bluetooth-fake-devices.js
+// META: timeout=long
 'use strict';
 const test_desc =
     `Ensure Manufacturer UUIDs 0x0000 and 0xffff are valid and do not crash` +
