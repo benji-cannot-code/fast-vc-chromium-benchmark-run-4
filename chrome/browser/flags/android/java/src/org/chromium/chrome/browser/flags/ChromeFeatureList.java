f944c45e88610728f3155f2044ef945d091b1072
Martin Kong <martinkong@google.com>
2025-10-23 08:16:04 -0700

    Remove the concurrent load feature param
    We have determined that for the loadNativeEarly experiment,
    concurrent load results in better startup performance than
    sequential load. Hence, this CL removes the concurrent load
    feature param and always executes the concurrent load code.
    
    Bug: 454351984
    Change-Id: Ib65981a554952348945662c4582af989dbad001f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7076031
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Martin Kong <martinkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1534345}

--- chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
@@ -1638,9 +1638,6 @@ public abstract class ChromeFeatureList {
     public static final IntCachedFeatureParam sEdgeToEdgeTabletMinWidthThreshold =
             newIntCachedFeatureParam(EDGE_TO_EDGE_TABLET, "e2e_tablet_width_threshold", -1);
 
-    public static final BooleanCachedFeatureParam sLoadNativeEarlyConcurrentLoad =
-            newBooleanCachedFeatureParam(LOAD_NATIVE_EARLY, "concurrent_load", true);
-
     public static final BooleanCachedFeatureParam sTabGroupListContainment =
             newBooleanCachedFeatureParam(
                     GRID_TAB_SWITCHER_SURFACE_COLOR_UPDATE, "tab_group_list_containment", true);
@@ -1805,7 +1802,6 @@ public abstract class ChromeFeatureList {
                     sEdgeToEdgeUseBackupNavbarInsetsOemList,
                     sEdgeToEdgeUseBackupNavbarInsetsOemMinVersions,
                     sEdgeToEdgeUseBackupNavbarInsetsUseGestures,
-                    sLoadNativeEarlyConcurrentLoad,
                     sLowMemoryDeviceThresholdMb,
                     sMagicStackAndroidShowAllModules,
                     sMaliciousApkDownloadCheckTelemetryOnly,
