0d236ca64bd78a74678b75cf859cc7d7127b22e5
Bryant Chandler <bryantchandler@google.com>
2025-10-23 16:31:00 -0700

    [GlicMI] Always close existing floaty when a new one is created
    This CL ensures that if a floaty is shown by any mechanism,
    we close any other floaties first. This is done by moving
    the logic from `OnDetachRequested` to a new method
    `OnFloatyWillShow` which is called from
    `CreateActiveEmbedderForFloaty`. This bug was introduced
    when we moved UnbindEmbedder and made it only be called
    by tab binding. This new change is effectively the same
    as what we did when we moved UnbindEmbedder. It ensures
    that if we ever show a floaty for any reason, we make
    sure all other floaties are closed.
    
    Bug: b:453942053
    Change-Id: Ic441794253ebfa647300d03200f0b4276a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7078991
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1534669}

--- chrome/browser/glic/service/glic_instance_impl.cc
+++ chrome/browser/glic/service/glic_instance_impl.cc
@@ -219,9 +219,6 @@ void GlicInstanceImpl::Show(const ShowOptions& options) {
 
 void GlicInstanceImpl::Detach(tabs::TabInterface* tab) {
   instance_metrics_.OnDetach();
-  if (coordinator_delegate_) {
-    coordinator_delegate_->OnDetachRequested(this, tab);
-  }
   auto show_options =
       ShowOptions::ForFloating(tab->GetBrowserWindowInterface());
   show_options.focus_on_show = true;
@@ -548,6 +545,9 @@ GlicUiEmbedder* GlicInstanceImpl::CreateActiveEmbedderForSidePanel(
 
 GlicUiEmbedder* GlicInstanceImpl::CreateActiveEmbedderForFloaty(
     const gfx::Rect& initial_bounds) {
+  if (coordinator_delegate_) {
+    coordinator_delegate_->OnWillCreateFloaty();
+  }
   EmbedderKey key = FloatingEmbedderKey();
   auto [entry_iter, _] = embedders_.try_emplace(key);
   entry_iter->second.embedder = std::make_unique<GlicFloatingUi>(
