3717b3298d8e39d7b4bbca96fc4967b76dcdbf4b
James Cook <jamescook@chromium.org>
2025-10-23 10:58:11 -0700

    extensions: Modernize code in downloads_api.h/cc, part 2
    Using base::TimeDelta and utilities like base::Seconds() is now
    preferred to storing integer seconds. Switch over some test support
    code that uses integer seconds.
    
    base::TimeDeltas are passed by value because they are small (and their
    header says to).
    
    Bug: 405219117
    Change-Id: I7aeab0a0eacc542e44f5e76280349faea890679f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7075650
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1534459}

--- chrome/browser/extensions/api/downloads/downloads_api.cc
+++ chrome/browser/extensions/api/downloads/downloads_api.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/cancelable_task_tracker.h"
 #include "base/task/current_thread.h"
 #include "base/task/single_thread_task_runner.h"
+#include "base/time/time.h"
 #include "base/values.h"
 #include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
@@ -760,8 +761,8 @@ class ExtensionDownloadsEventRouterData : public base::SupportsUserData::Data {
   void OnItemUpdated() { ++updated_; }
   void OnChangedFired() { ++changed_fired_; }
 
-  static void SetDetermineFilenameTimeoutSecondsForTesting(int s) {
-    determine_filename_timeout_s_ = s;
+  static void SetDetermineFilenameTimeoutForTesting(base::TimeDelta timeout) {
+    determine_filename_timeout_ = timeout;
   }
 
   void BeginFilenameDetermination(
@@ -782,7 +783,7 @@ class ExtensionDownloadsEventRouterData : public base::SupportsUserData::Data {
         base::BindOnce(
             &ExtensionDownloadsEventRouterData::DetermineFilenameTimeout,
             weak_ptr_factory_->GetWeakPtr()),
-        base::Seconds(determine_filename_timeout_s_));
+        determine_filename_timeout_);
   }
 
   void DetermineFilenameTimeout() { CallFilenameCallback(); }
@@ -906,7 +907,7 @@ class ExtensionDownloadsEventRouterData : public base::SupportsUserData::Data {
   }
 
  private:
-  static int determine_filename_timeout_s_;
+  static base::TimeDelta determine_filename_timeout_;
 
   struct DeterminerInfo {
     DeterminerInfo();
@@ -970,7 +971,8 @@ class ExtensionDownloadsEventRouterData : public base::SupportsUserData::Data {
       weak_ptr_factory_;
 };
 
-int ExtensionDownloadsEventRouterData::determine_filename_timeout_s_ = 15;
+base::TimeDelta ExtensionDownloadsEventRouterData::determine_filename_timeout_ =
+    base::Seconds(15);
 
 ExtensionDownloadsEventRouterData::DeterminerInfo::DeterminerInfo(
     const ExtensionId& e_id,
@@ -1756,10 +1758,10 @@ ExtensionDownloadsEventRouter::~ExtensionDownloadsEventRouter() {
     router->UnregisterObserver(this);
 }
 
-void ExtensionDownloadsEventRouter::
-    SetDetermineFilenameTimeoutSecondsForTesting(int s) {
-  ExtensionDownloadsEventRouterData::
-      SetDetermineFilenameTimeoutSecondsForTesting(s);
+void ExtensionDownloadsEventRouter::SetDetermineFilenameTimeoutForTesting(
+    base::TimeDelta timeout) {
+  ExtensionDownloadsEventRouterData::SetDetermineFilenameTimeoutForTesting(
+      timeout);
 }
 
 void ExtensionDownloadsEventRouter::SetUiEnabled(const Extension* extension,
