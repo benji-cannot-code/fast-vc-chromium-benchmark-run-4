cf4bd23b9ce4ae6787cc5a3caa385f16ff41aa4b
Georg Neis <neis@chromium.org>
2025-10-22 21:10:11 -0700

    ash: Remove some unused code from crosapi VpnService implementation
    Bug: b:365741912, b:365902693
    Change-Id: I53e9de15b778004b9d138fe8f2dcf1b1d7acedb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7071754
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1534061}

--- chrome/browser/ash/crosapi/vpn_service_ash.h
+++ chrome/browser/ash/crosapi/vpn_service_ash.h
@@ -80,11 +80,6 @@ class VpnServiceForExtensionAsh : public crosapi::mojom::VpnServiceForExtension,
                            CreateConfigurationCallback) override;
   void DestroyConfiguration(const std::string& configuration_name,
                             DestroyConfigurationCallback) override;
-  void BindPepperVpnProxyObserver(
-      const std::string& configuration_name,
-      mojo::PendingRemote<crosapi::mojom::PepperVpnProxyObserver>
-          pepper_vpn_proxy_observer,
-      BindPepperVpnProxyObserverCallback) override;
 
   // ash::NetworkConfigurationObserver:
   void OnConfigurationRemoved(const std::string& service_path,
@@ -94,7 +89,6 @@ class VpnServiceForExtensionAsh : public crosapi::mojom::VpnServiceForExtension,
   // Otherwise, returns std::nullopt.
   std::optional<std::string> GetActiveConfigurationObjectPath() const;
 
-  bool OwnsActiveConfiguration() const;
   bool HasConfigurationForServicePath(const std::string& service_path) const;
 
   void DestroyAllConfigurations();
@@ -250,9 +244,6 @@ class VpnServiceForExtensionAsh::VpnConfiguration
 
   virtual const std::optional<std::string>& service_path() const = 0;
   virtual void set_service_path(std::string) = 0;
-
-  virtual void BindPepperVpnProxyObserver(
-      mojo::PendingRemote<crosapi::mojom::PepperVpnProxyObserver>) = 0;
 };
 
 }  // namespace crosapi
