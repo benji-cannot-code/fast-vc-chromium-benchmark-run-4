e30db50236e8c97ccff5d34dc99630aa7fdc43c4
Anton Goncharenko <agonch@google.com>
2025-10-23 09:24:50 -0700

    [Android][Signin] Refactor SigninAndHistorySyncCoordinator.Result
    SigninAndHistorySyncCoordinator.Result is refactored from an @IntDef
    to a class that holds two booleans: mSignedIn and mHistorySyncOptIn.
    This change is made to more clearly pass the results of the sign-in
    and history sync flow to the SigninSnackbarController. The controller
    needs to know exactly which actions the user performed during the
    flow.
    
    Note the particular edge case when ShouldSignOutOnDecline=True.
    Upon declining history opt-in dialog HistorySyncMediator triggers
    an async operation to signout the user. But we immediately call
    BottomSheet's dismissHistorySync and then onFlowComplete and require
    to know "did the user sign in during this flow". It is incorrect to
    re-query IdentityManager immediately after a signout. Thus, I added
    `didSignOut` in previous CL and use it in BottomSheetSignin and
    FullscreenSignin.
    
    Bug: 437039311
    Change-Id: I4ff9dbb84db35f96bb22379443ad63cf28f7e0bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7062593
    Reviewed-by: Menghan Yang <myuu@google.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Anton Goncharenko <agonch@google.com>
    Cr-Commit-Position: refs/heads/main@{#1534390}

--- chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/FullscreenSigninAndHistorySyncCoordinator.java
+++ chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/FullscreenSigninAndHistorySyncCoordinator.java
@@ -77,7 +77,7 @@ public final class FullscreenSigninAndHistorySyncCoordinator
          */
         Promise<@Nullable Void> getNativeInitializationPromise();
 
-        void onFlowComplete(@SigninAndHistorySyncCoordinator.Result int result);
+        void onFlowComplete(SigninAndHistorySyncCoordinator.Result result);
     }
 
     /**
@@ -216,11 +216,11 @@ public final class FullscreenSigninAndHistorySyncCoordinator
                     assumeNonNull(signinManager);
                     signinManager.signOut(SignoutReason.ABORT_SIGNIN);
                 }
-                mDelegate.onFlowComplete(SigninAndHistorySyncCoordinator.Result.INTERRUPTED);
+                mDelegate.onFlowComplete(SigninAndHistorySyncCoordinator.Result.aborted());
                 break;
             case ChildView.HISTORY_SYNC:
                 if (!mDidShowSignin) {
-                    mDelegate.onFlowComplete(SigninAndHistorySyncCoordinator.Result.INTERRUPTED);
+                    mDelegate.onFlowComplete(SigninAndHistorySyncCoordinator.Result.aborted());
                     return BackPressResult.SUCCESS;
                 }
                 showChildView(ChildView.SIGNIN);
@@ -240,7 +240,7 @@ public final class FullscreenSigninAndHistorySyncCoordinator
     @Override
     public void advanceToNextPage() {
         if (!isSignedIn() || mCurrentView == ChildView.HISTORY_SYNC) {
-            mDelegate.onFlowComplete(SigninAndHistorySyncCoordinator.Result.INTERRUPTED);
+            mDelegate.onFlowComplete(SigninAndHistorySyncCoordinator.Result.aborted());
             return;
         }
         Profile profile = assumeNonNull(mProfileSupplier.get()).getOriginalProfile();
@@ -249,7 +249,8 @@ public final class FullscreenSigninAndHistorySyncCoordinator
             HistorySyncHelper historySyncHelper = HistorySyncHelper.getForProfile(profile);
             historySyncHelper.recordHistorySyncNotShown(mSigninAccessPoint);
             // TODO(crbug.com/376469696): Differentiate the failure & completion case here.
-            mDelegate.onFlowComplete(SigninAndHistorySyncCoordinator.Result.COMPLETED);
+            mDelegate.onFlowComplete(
+                    new SigninAndHistorySyncCoordinator.Result(mDidShowSignin, false));
             return;
         }
         showChildView(ChildView.HISTORY_SYNC);
@@ -339,11 +340,9 @@ public final class FullscreenSigninAndHistorySyncCoordinator
             mHistorySyncCoordinator.destroy();
             mHistorySyncCoordinator = null;
         }
-        @SigninAndHistorySyncCoordinator.Result
-        int flowResult =
-                isHistorySyncAccepted
-                        ? SigninAndHistorySyncCoordinator.Result.COMPLETED
-                        : SigninAndHistorySyncCoordinator.Result.INTERRUPTED;
+        SigninAndHistorySyncCoordinator.Result flowResult =
+                new SigninAndHistorySyncCoordinator.Result(
+                        mDidShowSignin && !didSignOut, isHistorySyncAccepted);
         mDelegate.onFlowComplete(flowResult);
     }
 
