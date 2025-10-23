d6e3f8be9d4532ccea866a1cd51ad128a4972b06
Henrique Nakashima <hnakashima@chromium.org>
2025-10-23 14:50:42 -0700

    [Android] Remove ViewElement for status icon
    The status icon seems to be technically displayed but doesn't
    actually appear in incognito in phones. Just remove it from the
    ViewElements for now.
    
    Bug: 415805917
    Change-Id: I6b510c320478bb0983f97ae8374bf948c2eb3f4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7079535
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1534625}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/transit/omnibox/OmniboxFacility.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/transit/omnibox/OmniboxFacility.java
@@ -34,7 +34,6 @@ public class OmniboxFacility extends Facility<CtaPageStation> {
             ACTION_CONTAINER.descendant(withId(R.id.delete_button));
     private final boolean mIncognito;
     private final FakeOmniboxSuggestions mFakeSuggestions;
-    public ViewElement<View> statusIconElement;
     public ViewElement<UrlBar> urlBarElement;
     public ViewElement<View> actionContainerElement;
     public ViewElement<View> micButtonElement;
@@ -50,10 +49,13 @@ public class OmniboxFacility extends Facility<CtaPageStation> {
         declareView(instanceOf(ScrimView.class));
 
         // Unscoped elements exist in PageStations too.
+        //
         // Action buttons are 71% displayed in tablets (though the actual image is fully displayed).
+        //
+        // TODO(crbug.com/416324280): Add status icon back with id R.id.location_bar_status_icon as
+        // optional ViewElement.
         if (!mIncognito) {
             // Regular tab
-            statusIconElement = declareView(STATUS_ICON, ViewElement.unscopedOption());
             urlBarElement = declareView(URL_FIELD, ViewElement.unscopedOption());
             actionContainerElement =
                     declareView(
@@ -65,21 +67,11 @@ public class OmniboxFacility extends Facility<CtaPageStation> {
                             ViewElement.newOptions().unscoped().displayingAtLeast(50).build());
             declareNoView(DELETE_BUTTON);
         } else {
-            if (mHostStation.getActivity().isTablet()) {
-                // Incognito tab in tablet
-                statusIconElement = declareView(STATUS_ICON, ViewElement.unscopedOption());
-                urlBarElement = declareView(URL_FIELD, ViewElement.unscopedOption());
-                declareNoView(ACTION_CONTAINER);
-                declareNoView(MIC_BUTTON);
-                declareNoView(DELETE_BUTTON);
-            } else {
-                // Incognito tab in phone
-                declareNoView(STATUS_ICON);
-                urlBarElement = declareView(URL_FIELD, ViewElement.unscopedOption());
-                declareNoView(ACTION_CONTAINER);
-                declareNoView(MIC_BUTTON);
-                declareNoView(DELETE_BUTTON);
-            }
+            // Incognito tab
+            urlBarElement = declareView(URL_FIELD, ViewElement.unscopedOption());
+            declareNoView(ACTION_CONTAINER);
+            declareNoView(MIC_BUTTON);
+            declareNoView(DELETE_BUTTON);
         }
     }
 
