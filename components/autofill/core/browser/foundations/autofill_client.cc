a2b8623436edcd5e073d3c3c7076d9af69e0b09f
Ricardo Fernandez de Arellano <ricardofda@google.com>
2025-10-23 08:00:14 -0700

    Remove AutofillClient::GetSmsOtpBackend.
    This change removes AutofillClient::GetSmsOtpBackend() from the AutofillClient and derived classes as it has been replaced by AutofillClient::GetOneTimeTokenService.
    
    Bug: 415273270
    Fixed: b:451665530
    Change-Id: I318e02d00c1df7655904f749871d24540c7636ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7063393
    Commit-Queue: Ricardo Fernandez de Arellano <ricardofda@google.com>
    Reviewed-by: Luchen Peng <luchenpeng@google.com>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1534331}

--- components/autofill/core/browser/foundations/autofill_client.cc
+++ components/autofill/core/browser/foundations/autofill_client.cc
@@ -344,10 +344,6 @@ OtpFieldDetector* AutofillClient::GetOtpFieldDetector() {
   return nullptr;
 }
 
-one_time_tokens::SmsOtpBackend* AutofillClient::GetSmsOtpBackend() const {
-  return nullptr;
-}
-
 one_time_tokens::OneTimeTokenService* AutofillClient::GetOneTimeTokenService()
     const {
   return nullptr;
