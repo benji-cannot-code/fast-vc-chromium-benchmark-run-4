c7abc64e5be5983fb4e3d86a893d4e85d9e2c665
Bruno Braga <brunobraga@google.com>
2025-10-23 08:34:47 -0700

    [AutofillOnTyping] Acceptance rate based on classification type metrics
    There are reasonable changes that the acceptance level varies between
    classified and unclassified fields. Since unclassified fields likely
    have a higher acceptance rate, this metric will help us to make
    a launch decision in the future, which could be only unclassified
    fields.
    
    Note that unclassified fields in canary accounted for 65% of all
    fillings.
    
    Bug: 381994105
    Change-Id: Icff60b7dde6749a309e3b7ab716d7eda669fe62c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7079782
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1534358}

--- components/autofill/core/browser/metrics/form_events/address_form_event_logger.h
+++ components/autofill/core/browser/metrics/form_events/address_form_event_logger.h
@@ -72,9 +72,10 @@ class AddressFormEventLogger : public FormEventLoggerBase {
   // For the profiles used to build the shown suggestions,
   // `profile_last_used_time_per_guid` specifies the last time each of the
   // profiles was used.
-  void OnDidShownAutofillOnTyping(
+  void OnDidShowAddressOnTyping(
       FieldGlobalId field_global_id,
       FieldTypeSet field_types_used,
+      FieldTypeSet triggering_field_types,
       std::map<std::string, base::TimeDelta> profile_last_used_time_per_guid);
 
   // `field_global_id` is the id of the field where a
@@ -84,11 +85,10 @@ class AddressFormEventLogger : public FormEventLoggerBase {
   // which `value` was derived from.
   // `profile_used_guid` specifies the profile used to build
   // the accepted suggestion.
-  void OnDidAcceptAutofillOnTyping(
-      FieldGlobalId field_global_id,
-      const std::u16string& value,
-      FieldType field_type_used_to_build_suggestion,
-      const std::string profile_used_guid);
+  void OnDidAcceptAddressOnTyping(FieldGlobalId field_global_id,
+                                  const std::u16string& value,
+                                  FieldType field_type_used_to_build_suggestion,
+                                  const std::string profile_used_guid);
   void LogAutofillAddressOnTypingCorrectnessMetrics(const FormStructure& form);
 
   void OnDestroyed() override;
@@ -120,9 +120,10 @@ class AddressFormEventLogger : public FormEventLoggerBase {
   // suggestion.
   DenseSet<AutofillProfileRecordTypeCategory> profile_categories_filled_;
   // For fields where `SuggestionType::kAddressEntryOnTyping`
-  // suggestions were shown, store the `FieldTypeSet` used to build the
-  // suggestions keyed by the field global identifier.
-  std::map<FieldGlobalId, FieldTypeSet>
+  // suggestions were shown, stores whether the field is classified and the
+  // `FieldTypeSet` used to build the suggestions, keyed by the field global
+  // identifier.
+  std::map<FieldGlobalId, std::pair<bool, FieldTypeSet>>
       fields_where_autofill_on_typing_was_shown_;
   // For profiles that were used to build
   // `SuggestionType::kAddressEntryOnTyping` suggestions, store their last usage
