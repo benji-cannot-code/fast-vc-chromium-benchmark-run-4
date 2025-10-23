3d54169b9e76b7a8cdd18dd1046cef8fdd53c245
Christopher Grant <cjgrant@chromium.org>
2025-10-23 11:49:09 -0700

    NTP Promos: Fix translation on Setup List hide Undo button
    A conversion from .html to .html.ts broke the translation on one button,
    because of missing template tags.
    
    Bug: 454391114
    Change-Id: Ibbeb7911f64b5dd24c52291a47d55340deab6cd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7076797
    Auto-Submit: Christopher Grant <cjgrant@chromium.org>
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1534497}

--- chrome/browser/resources/new_tab_page/ntp_promo/setup_list.html.ts
+++ chrome/browser/resources/new_tab_page/ntp_promo/setup_list.html.ts
@@ -8,7 +8,7 @@ import {html} from '//resources/lit/v3_0/lit.rollup.js';
 import type {SetupListElement} from './setup_list.js';
 
 export function getHtml(this: SetupListElement) {
-  return html`
+  return html`<!--_html_template_start_-->
 <ntp-module-header-v2
     id="moduleHeaderElementV2"
     header-text="${this.i18n('modulesSetupListTitle')}"
@@ -58,5 +58,5 @@ ${this.showInfoDialog_ ? html`
         @close="${this.onInfoDialogClose_}">>
     </ntp-info-dialog>
 ` : ''}
-`;
+<!--_html_template_end_-->`;
 }
