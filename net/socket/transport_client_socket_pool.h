03ba058a8451c9ec0c31b6bc5433146c5581292e
Yuki Shiino <yukishiino@chromium.org>
2025-10-23 02:07:46 -0700

    ELUD: Add ADVANCED_MEMORY_SAFETY_CHECKS to TransportClientSocketPool::Request
    Adds ADVANCED_MEMORY_SAFETY_CHECKS as a mitigation of
    crbug.com/422046500
    
    Debugging notes:
    - NetLog::observer_capture_modes_ is at offset 0x002c
    - NetLogWithSource has a raw_ptr to NetLog
    - NetLogWithSource never be allocated by operator new
    - NetLogWithSource is part of object of
      TransportClientSocketPool::Request
    
    Bug: 422046500
    Change-Id: I226590287c9e8a27cff837dd1768effaa2da1f7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7071751
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1534175}

--- net/socket/transport_client_socket_pool.h
+++ net/socket/transport_client_socket_pool.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/memory/raw_ptr.h"
+#include "base/memory/safety_checks.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
@@ -88,6 +89,9 @@ class NET_EXPORT_PRIVATE TransportClientSocketPool
   };
 
   class NET_EXPORT_PRIVATE Request {
+    // TODO(crbug.com/422046500): Remove this macro once the bug gets fixed.
+    ADVANCED_MEMORY_SAFETY_CHECKS();
+
    public:
     // If |proxy_auth_callback| is null, proxy auth challenges will
     // result in an error.
