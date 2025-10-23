cd171677de9b23c58c40424e457b252665efcb28
Kaleigh Rosenblat <kaleigh@google.com>
2025-10-23 09:52:58 -0700

    Allow future policies on Desktop Android when feature is enabled.
    This change introduces a new feature flag, `kFuturePoliciesOnDesktopAndroid`, which, when enabled, prevents policies marked as "future" from being filtered out by the `ConfigurationPolicyHandlerList`. This allows for testing of future policies on stable and beta Desktop Android builds without needing to use the kEnableExperimentalPolicies flag.
    
    Bug: crbug.com/452666657
    Change-Id: I9b8ab652213cf1fddea1360f79b1a9d7f067f571
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7047320
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Kaleigh Rosenblat <kaleigh@google.com>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1534409}

--- components/policy/core/browser/configuration_policy_handler_list.cc
+++ components/policy/core/browser/configuration_policy_handler_list.cc
@@ -6,10 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/core/browser/configuration_policy_handler_list.h"
 
 #include "base/check_is_test.h"
+#include "base/feature_list.h"
 #include "base/functional/bind.h"
+#include "build/android_buildflags.h"
 #include "components/policy/core/browser/configuration_policy_handler.h"
 #include "components/policy/core/browser/configuration_policy_handler_parameters.h"
 #include "components/policy/core/browser/policy_error_map.h"
+#include "components/policy/core/common/features.h"
 #include "components/policy/core/common/policy_logger.h"
 #include "components/policy/core/common/values_util.h"
 #include "components/policy/policy_constants.h"
@@ -142,6 +145,13 @@ bool ConfigurationPolicyHandlerList::IsBlockedFuturePolicy(
     const base::flat_set<std::string>& future_policies_allowed,
     const PolicyDetails& policy_details,
     PolicyMap::const_reference entry) const {
+#if BUILDFLAG(IS_DESKTOP_ANDROID)
+  if (base::FeatureList::IsEnabled(features::kFuturePoliciesOnDesktopAndroid) &&
+      policy_details.is_future) {
+    return false;
+  }
+#endif  // BUILDFLAG(IS_DESKTOP_ANDROID)
+
   return !are_future_policies_allowed_by_default_ && policy_details.is_future &&
          !future_policies_allowed.contains(entry.first);
 }
--- components/policy/core/common/features.cc
+++ components/policy/core/common/features.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/metrics/field_trial_params.h"
 #include "base/time/time.h"
+#include "build/android_buildflags.h"
 
 namespace policy::features {
 
@@ -37,5 +38,13 @@ const base::FeatureParam<base::TimeDelta> kPolicyRegistrationDelay{
 
 // Used to add a captive portal check in SafeSitesNavigationThrottle.
 BASE_FEATURE(kSafeSitesCaptivePortalCheck, base::FEATURE_ENABLED_BY_DEFAULT);
+
+// TODO(https://crbug.com/452666657): Remove this feature flag after launching
+// policies to supported on Android Desktop.
+#if BUILDFLAG(IS_DESKTOP_ANDROID)
+BASE_FEATURE(kFuturePoliciesOnDesktopAndroid,
+             "FuturePoliciesOnDesktopAndroid",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+#endif  // BUILDFLAG(IS_DESKTOP_ANDROID)
 
 }  // namespace policy::features
