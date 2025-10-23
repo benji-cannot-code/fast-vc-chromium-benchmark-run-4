2853f0ff70337ccd81ac377fae379916a0dff5a0
Dan Harrington <harringtond@chromium.org>
2025-10-23 12:31:21 -0700

    Disable kGlicMultiInstance in some tests
    These tests don't work with kGlicMultiInstance, and will need
    fixed.
    
    Bug: b:453696965
    Change-Id: I4dcd0611f18e1cf77f7e2482245c51326a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7080437
    Commit-Queue: Trevor Perrier <perrier@chromium.org>
    Auto-Submit: Dan Harrington <harringtond@chromium.org>
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1534531}

--- chrome/browser/glic/glic_profile_manager_interactive_uitest.cc
+++ chrome/browser/glic/glic_profile_manager_interactive_uitest.cc
@@ -62,7 +62,10 @@ class GlicProfileManagerUiTest
                                 {features::kGlicWarming,
                                  {{features::kGlicWarmingDelayMs.name, "0"},
                                   {features::kGlicWarmingJitterMs.name, "0"}}}},
-          /*disabled_features=*/{});
+          /*disabled_features=*/{
+              // TODO(b/453696965): These tests need fixed to work with
+              // kGlicMultiInstance.
+              features::kGlicMultiInstance});
     } else {
       feature_list_.InitWithFeaturesAndParameters(
           /*enabled_features=*/{{features::kGlicFreWarming, {}},
@@ -71,7 +74,10 @@ class GlicProfileManagerUiTest
                                      {features::kGlicWarmingDelayMs.name, "0"},
                                      {features::kGlicWarmingJitterMs.name, "0"},
                                  }}},
-          /*disabled_features=*/{features::kGlicWarmMultiple});
+          /*disabled_features=*/{features::kGlicWarmMultiple,
+                                 // TODO(b/453696965): These tests need fixed to
+                                 // work with kGlicMultiInstance.
+                                 features::kGlicMultiInstance});
     }
   }
   ~GlicProfileManagerUiTest() override = default;
