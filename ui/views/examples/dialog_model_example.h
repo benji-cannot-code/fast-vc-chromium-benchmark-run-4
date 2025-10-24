1d8f15cdd458c85ef251a3e51c5946c8252da667
Keren Zhu <kerenzhu@chromium.org>
2025-10-24 10:08:27 -0700

    views: Add a DialogModel example to views examples
    This new example demonstrates how to use ui::DialogModel and
    BubbleDialogModelHost to create a dialog with various elements,
    including textfields, password fields, checkboxes, comboboxes, and
    custom views. It also shows how to handle button callbacks.
    
    screenshot:
    https://drive.google.com/file/d/18vkkBJnoYaOLtLtNivB4Dc0Tee55cTNk/view?usp=sharing
    
    Bug: none
    Change-Id: Ib3dce1d8e5ef66f867a6ba70c721202b4925cb17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7079538
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1535117}

---
+++ ui/views/examples/dialog_model_example.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_VIEWS_EXAMPLES_DIALOG_MODEL_EXAMPLE_H_
+#define UI_VIEWS_EXAMPLES_DIALOG_MODEL_EXAMPLE_H_
+
+#include "ui/views/examples/example_base.h"
+
+namespace views::examples {
+
+class VIEWS_EXAMPLES_EXPORT DialogModelExample : public ExampleBase {
+ public:
+  DialogModelExample();
+  DialogModelExample(const DialogModelExample&) = delete;
+  DialogModelExample& operator=(const DialogModelExample&) = delete;
+  ~DialogModelExample() override;
+
+  // ExampleBase:
+  void CreateExampleView(View* container) override;
+
+ private:
+  void ShowDialog();
+
+  raw_ptr<View> show_dialog_button_ = nullptr;
+};
+
+}  // namespace views::examples
+
+#endif  // UI_VIEWS_EXAMPLES_DIALOG_MODEL_EXAMPLE_H_
