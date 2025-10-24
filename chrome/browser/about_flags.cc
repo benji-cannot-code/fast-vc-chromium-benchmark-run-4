52d49745b9bcb7debca172e7b88f4f0b2c1e5f26
Mike West <mkwst@chromium.org>
2025-10-24 01:26:46 -0700

    [Connection-Allowlists] Add a flag to about:flags.
    This CL adds a flag to play around with the prototype. Fun for all ages!
    
    This is one in a series of CLs to prototype the mechanism:
    
    1. https://crrev.com/c/6985001 - Data structure and parsing.
    2. https://crrev.com/c/7071008 - Add allowlists to ParsedHeaders.
    3. https://crrev.com/c/7072371 - Store allowlists on PolicyContainer.
    4. https://crrev.com/c/7072352 - Enforce allowlists in Blink.
    5. https://crrev.com/c/7073851 - [You are here.]
    
    Bug: 447954811
    Change-Id: I1936c2161be081f6490f32632fc995553ddfd399
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7073851
    Commit-Queue: Mike West <mkwst@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1534829}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -13438,6 +13438,10 @@ const FeatureEntry kFeatureEntries[] = {
      FEATURE_VALUE_TYPE(features::kWebAppMigratePreinstalledChat)},
 #endif
 
+    {"connection-allowlists", flag_descriptions::kConnectionAllowlistsName,
+     flag_descriptions::kConnectionAllowlistsDescription, kOsAll,
+     FEATURE_VALUE_TYPE(network::features::kConnectionAllowlists)},
+
     // Add new entries above this line.
     // NOTE: Adding a new flag requires adding a corresponding entry to enum
     // "LoginCustomFlags" in tools/metrics/histograms/enums.xml. See "Flag
