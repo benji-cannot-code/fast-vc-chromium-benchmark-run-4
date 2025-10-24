2dccaaf2044b212bc6055e661f1bb77977547e39
Andrea Orru <andreaorru@chromium.org>
2025-10-24 12:48:23 -0700

    Revert "Creates a 'Done' Button that appears in the Item Picker"
    This reverts commit d8f10594e49ebcd575091605c5bf86898f7274ec.
    
    Reason for revert: suspected for tree closure error https://ci.chromium.org/ui/p/chromium/builders/ci/android-cast-arm-dbg/17721/blamelist
    
    Original change's description:
    > Creates a 'Done' Button that appears in the Item Picker
    >
    > This CL implements a new Creation Mode for the Tab List Editor
    > Coordinator that conditionally allows a 'Done' Button to
    > appear, and hides the Menu Button. The 'Done' button is right-
    > aligned like the Menu Button.
    >
    > Change-Id: I7e58f749a1dc075d8aea09e0d66e684581508fdf
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7056622
    > Reviewed-by: Sky Malice <skym@chromium.org>
    > Commit-Queue: Bhuvana Betini <bbetini@google.com>
    > Reviewed-by: Siddhartha S <ssid@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1535209}
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: If7a994487734c96b4d4a7657becfa0d59ed64884
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7083393
    Owners-Override: Andrea Orru <andreaorru@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Andrea Orru <andreaorru@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1535238}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListEditorProperties.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListEditorProperties.java
@@ -39,9 +39,6 @@ public class TabListEditorProperties {
     public static final PropertyModel.WritableIntPropertyKey TOP_MARGIN =
             new PropertyModel.WritableIntPropertyKey();
 
-    public static final PropertyModel.WritableIntPropertyKey CREATION_MODE =
-            new PropertyModel.WritableIntPropertyKey();
-
     public static final PropertyKey[] ALL_KEYS =
             new PropertyKey[] {
                 IS_VISIBLE,
@@ -51,7 +48,6 @@ public class TabListEditorProperties {
                 TOOLBAR_TEXT_TINT,
                 TOOLBAR_BUTTON_TINT,
                 TOOLBAR_TITLE,
-                TOP_MARGIN,
-                CREATION_MODE
+                TOP_MARGIN
             };
 }
--- chrome/android/java/src/org/chromium/chrome/browser/chrome_item_picker/TabItemPickerCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/chrome_item_picker/TabItemPickerCoordinator.java
@@ -174,7 +174,7 @@ public class TabItemPickerCoordinator {
                 modalDialogManager,
                 /* desktopWindowStateManager= */ null,
                 /* edgeToEdgeSupplier= */ null,
-                CreationMode.ITEM_PICKER,
+                CreationMode.FULL_SCREEN,
                 /* undoBarExplicitTrigger= */ null,
                 /* componentName= */ "TabItemPickerCoordinator");
     }
