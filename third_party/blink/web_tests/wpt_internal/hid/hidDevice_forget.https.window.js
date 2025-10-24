477837d5a2e4edd7811d53aa713050f2e7fab478
Matt Reynolds <mattreynolds@google.com>
2025-10-24 16:37:29 -0700

    hid: Long timeout for hidDevice_forget.https.window.js
    Bug: 449337533
    Change-Id: Ie09675c757079a29725a3bdc88894c4dff5b40d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7076184
    Auto-Submit: Matt Reynolds <mattreynolds@chromium.org>
    Commit-Queue: Alvin Ji <alvinji@chromium.org>
    Reviewed-by: Alvin Ji <alvinji@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1535403}

--- third_party/blink/web_tests/wpt_internal/hid/hidDevice_forget.https.window.js
+++ third_party/blink/web_tests/wpt_internal/hid/hidDevice_forget.https.window.js
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: script=/resources/testdriver-vendor.js
 // META: script=/webhid/resources/common.js
 // META: script=resources/automation.js
+// META: timeout=long
 'use strict';
 
 const kTestVendorId = 0x1234;
