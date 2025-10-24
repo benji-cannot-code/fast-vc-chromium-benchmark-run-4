df0e0f000630d7e9c2174b570af0703e7b1e0cc1
Hailey Wang <haileywang@google.com>
2025-10-24 11:52:14 -0700

    Revert "[clank][geo] Add OS-location warning to location settings subpage"
    This reverts commit 600af808da3098b9053e0728b15943d0ef6b1e92.
    
    Reason for revert: Causing test failure on Clank. crbug.com/454942769.
    
    Original change's description:
    > [clank][geo] Add OS-location warning to location settings subpage
    >
    > This CL touches the location settings subpage that allows, when the
    > kApproximateGeolocationPermission feature flag is enabled, to switch
    > between approximate and precise location grant for a site. In
    > particular it adds a warning in case the granted accuracy (precise) is
    > higher than the accuracy granted at the OS level (approximate). The
    > warning appears and disappears depending on the user selection.
    >
    > Bug: 437026554
    > Change-Id: Iacd9db1df130ea1b956d89dc92bb60348fe0f01f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7072231
    > Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    > Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    > Reviewed-by: Martin Šrámek <msramek@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1535014}
    
    Bug: 437026554
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ibb229e22ac0aa592a5583aafe12ba5523d195598
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7083389
    Auto-Submit: Hailey Wang <haileywang@google.com>
    Owners-Override: Hailey Wang <haileywang@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1535208}

--- components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/LocationPermissionOptionsPreference.java
+++ components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/LocationPermissionOptionsPreference.java
@@ -36,7 +36,6 @@ public class LocationPermissionOptionsPreference extends Preference
     private @MonotonicNonNull GeolocationSetting mSetting;
     private @MonotonicNonNull RadioButtonWithDescription mPrecise;
     private @MonotonicNonNull RadioButtonWithDescription mApproximate;
-    private @MonotonicNonNull LocationPermissionSubpageSettings mSubpage;
     private boolean mIsPreciseSelected;
 
     public LocationPermissionOptionsPreference(Context context, AttributeSet attrs) {
@@ -66,12 +65,9 @@ public class LocationPermissionOptionsPreference extends Preference
     }
 
     public void initialize(
-            @NonNull BrowserContextHandle browserContextHandle,
-            @NonNull Website site,
-            @NonNull LocationPermissionSubpageSettings subpage) {
+            @NonNull BrowserContextHandle browserContextHandle, @NonNull Website site) {
         mBrowserContextHandle = browserContextHandle;
         mSite = site;
-        mSubpage = subpage;
 
         PermissionInfo info = mSite.getPermissionInfo(ContentSettingsType.GEOLOCATION_WITH_OPTIONS);
         assumeNonNull(info);
@@ -91,7 +87,6 @@ public class LocationPermissionOptionsPreference extends Preference
         assumeNonNull(mSetting);
         assumeNonNull(mSite);
         assumeNonNull(mBrowserContextHandle);
-        assumeNonNull(mSubpage);
 
         mIsPreciseSelected = mPrecise.isChecked();
         boolean isPermissionAllowed = mSetting.mApproximate == ContentSetting.ALLOW;
@@ -100,7 +95,6 @@ public class LocationPermissionOptionsPreference extends Preference
         assumeNonNull(mSite.getPermissionInfo(ContentSettingsType.GEOLOCATION_WITH_OPTIONS))
                 .setGeolocationSetting(
                         mBrowserContextHandle, new GeolocationSetting(approximate, precise));
-        mSubpage.setUpOsWarningPreferences();
     }
 
     public @Nullable RadioButtonWithDescription getApproximateButtonForTesting() {
