0e16ffe063a6d948605b2ea884504790a574951d
Dana Fried <dfried@chromium.org>
2025-10-24 06:35:40 -0700

    [Browser Layout] Move params to a new file with utility methods
    This moves BrowserLayoutParams to its own file and adds a method to
    get a params block relative to a particular rectangle. This can allow
    us to translate to local coordinates when laying out the browser view
    or the top container.
    
    It also adds a method to BrowserViewLayoutDelegate to return a set of
    layout params in the BrowserView's local coordinates, to make
    calculating the new layout easier.
    
    Tests for the new functionality have been added.
    
    Bug: 453717426
    Change-Id: Ida8458989bd396b7b9d8a3b383f37961fc4a80c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7081751
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1534963}

--- chrome/browser/ui/views/frame/layout/browser_view_layout_delegate.h
+++ chrome/browser/ui/views/frame/layout/browser_view_layout_delegate.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_VIEWS_FRAME_LAYOUT_BROWSER_VIEW_LAYOUT_DELEGATE_H_
 
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/views/frame/layout/browser_view_layout_params.h"
 #include "ui/gfx/native_ui_types.h"
 
 class ImmersiveModeController;
@@ -31,6 +32,7 @@ class BrowserViewLayoutDelegate {
   virtual gfx::Rect GetBoundsForTabStripRegionInBrowserView() const = 0;
   virtual gfx::Rect GetBoundsForToolbarInVerticalTabBrowserView() const = 0;
   virtual gfx::Rect GetBoundsForWebAppFrameToolbarInBrowserView() const = 0;
+  virtual BrowserLayoutParams GetBrowserLayoutParams() const = 0;
   virtual void LayoutWebAppWindowTitle(
       const gfx::Rect& available_space,
       views::Label& window_title_label) const = 0;
