6ee4d1476b238b6b4825444d075b08844673ad47
Bhuvana Betini <bbetini@google.com>
2025-10-24 11:54:02 -0700

    Creates a 'Done' Button that appears in the Item Picker
    This CL implements a new Creation Mode for the Tab List Editor
    Coordinator that conditionally allows a 'Done' Button to
    appear, and hides the Menu Button. The 'Done' button is right-
    aligned like the Menu Button.
    
    Change-Id: I7e58f749a1dc075d8aea09e0d66e684581508fdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7056622
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Bhuvana Betini <bbetini@google.com>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1535209}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListEditorActionViewLayout.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListEditorActionViewLayout.java
@@ -18,10 +18,12 @@ import androidx.collection.ArraySet;
 import org.chromium.base.MathUtils;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
+import org.chromium.chrome.browser.tasks.tab_management.TabListEditorCoordinator.CreationMode;
 import org.chromium.chrome.tab_ui.R;
 import org.chromium.components.browser_ui.widget.NumberRollView;
 import org.chromium.ui.listmenu.ListMenuButton;
 import org.chromium.ui.listmenu.ListMenuDelegate;
+import org.chromium.ui.widget.ButtonCompat;
 
 import java.util.ArrayList;
 import java.util.Set;
@@ -32,19 +34,29 @@ import java.util.Set;
  */
 @NullMarked
 public class TabListEditorActionViewLayout extends LinearLayout {
-    /** All {@link TabListEditoreMenuItem} action views with menu items. */
+    /** All {@link TabListEditorMenuItem} action views with menu items. */
     private final ArrayList<TabListEditorMenuItem> mMenuItemsWithActionView = new ArrayList<>();
 
     /** The {@link TabListEditoreMenuItem}s with visible action views. */
     private final Set<TabListEditorMenuItem> mVisibleActions = new ArraySet<>();
 
-    /** {@link ListMenuButton} for showing the {@link TabListEditorMenu}. */
+    /**
+     * {@link ListMenuButton} for showing the {@link TabListEditorMenu}. Only one of mMenuButton and
+     * mDoneButton will be visible.
+     */
     private ListMenuButton mMenuButton;
+
+    /**
+     * {@link View} for showing the done button. Only one of mMenuButton and mDoneButton will be
+     * visible.
+     */
+    private ButtonCompat mDoneButton;
 
     private final LinearLayout.LayoutParams mActionViewParams;
 
     private @Nullable ActionViewLayoutDelegate mDelegate;
     private boolean mHasMenuOnlyItems;
+    private @CreationMode int mCreationMode;
 
     /** Delegate updates in response to which action views are visible. */
     public interface ActionViewLayoutDelegate {
@@ -70,6 +82,7 @@ public class TabListEditorActionViewLayout extends LinearLayout {
         super.onFinishInflate();
         mMenuButton = findViewById(R.id.list_menu_button);
         mMenuButton.tryToFitLargestItem(true);
+        mDoneButton = findViewById(R.id.done_button);
     }
 
     ListMenuButton getListMenuButtonForTesting() {
@@ -81,6 +94,11 @@ public class TabListEditorActionViewLayout extends LinearLayout {
      */
     public void setListMenuDelegate(ListMenuDelegate delegate) {
         mMenuButton.setDelegate(delegate);
+    }
+
+    public void setCreationMode(@CreationMode int creationMode) {
+        mCreationMode = creationMode;
+        update();
     }
 
     /**
@@ -134,9 +152,7 @@ public class TabListEditorActionViewLayout extends LinearLayout {
 
     @Override
     public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        // Get empty size without action views.
         removeAllActionViews();
-        mMenuButton.setVisibility(View.VISIBLE);
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         final int width = getMeasuredWidth();
 
@@ -151,32 +167,42 @@ public class TabListEditorActionViewLayout extends LinearLayout {
         boolean hasForcedAnyActionViewToMenu = false;
         final int childMeasureSpec =
                 View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
-        for (TabListEditorMenuItem menuItem : mMenuItemsWithActionView) {
-            final View actionView = assumeNonNull(menuItem.getActionView());
-            actionView.measure(childMeasureSpec, childMeasureSpec);
-            final int actionViewWidth = actionView.getMeasuredWidth();
-            if (usedWidth + actionViewWidth > width || hasForcedAnyActionViewToMenu) {
-                // The ActionView doesn't fit. Ensure it still has a LayoutParams.
-                actionView.setLayoutParams(mActionViewParams);
-                hasForcedAnyActionViewToMenu = true;
-                continue;
-            }
+        if (mCreationMode != CreationMode.ITEM_PICKER) {
+            for (TabListEditorMenuItem menuItem : mMenuItemsWithActionView) {
+                final View actionView = assumeNonNull(menuItem.getActionView());
+                actionView.measure(childMeasureSpec, childMeasureSpec);
+                final int actionViewWidth = actionView.getMeasuredWidth();
+                if (usedWidth + actionViewWidth > width || hasForcedAnyActionViewToMenu) {
+                    // The ActionView doesn't fit. Ensure it still has a LayoutParams.
+                    actionView.setLayoutParams(mActionViewParams);
+                    hasForcedAnyActionViewToMenu = true;
+                    continue;
+                }
 
-            // Add views in front of the menu button.
-            addView(actionView, getChildCount() - 1, mActionViewParams);
-            mVisibleActions.add(menuItem);
-            usedWidth += actionViewWidth;
-            requiredWidth += actionViewWidth;
+                // Add views in front of the menu button.
+                addView(actionView, getChildCount() - 1, mActionViewParams);
+                mVisibleActions.add(menuItem);
+                usedWidth += actionViewWidth;
+                requiredWidth += actionViewWidth;
+            }
         }
+
         if (mDelegate != null) {
             // Any items in mVisibleActions will appear in the Toolbar. The remaining items will be
             // forced into the overflow menu.
             mDelegate.setVisibleActionViews(mVisibleActions);
         }
-        if (mHasMenuOnlyItems || hasForcedAnyActionViewToMenu) {
+        if (mCreationMode == CreationMode.ITEM_PICKER) {
+            mDoneButton.setVisibility(View.VISIBLE);
+            mMenuButton.setVisibility(View.GONE);
+            mDoneButton.measure(childMeasureSpec, childMeasureSpec);
+            requiredWidth += mDoneButton.getMeasuredWidth();
+        } else if (mHasMenuOnlyItems || hasForcedAnyActionViewToMenu) {
+            mDoneButton.setVisibility(View.GONE);
             mMenuButton.setVisibility(View.VISIBLE);
             requiredWidth += mMenuButton.getMeasuredWidth();
         } else {
+            mDoneButton.setVisibility(View.GONE);
             mMenuButton.setVisibility(View.GONE);
         }
 
--- chrome/android/java/src/org/chromium/chrome/browser/chrome_item_picker/TabItemPickerCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/chrome_item_picker/TabItemPickerCoordinator.java
@@ -174,7 +174,7 @@ public class TabItemPickerCoordinator {
                 modalDialogManager,
                 /* desktopWindowStateManager= */ null,
                 /* edgeToEdgeSupplier= */ null,
-                CreationMode.FULL_SCREEN,
+                CreationMode.ITEM_PICKER,
                 /* undoBarExplicitTrigger= */ null,
                 /* componentName= */ "TabItemPickerCoordinator");
     }
