42b8450dfc9886f0755c092c93c749a8353bdd19
Aliaksei Bahdzevich <thelex@google.com>
2025-10-24 02:57:07 -0700

    Implement view-source navigation throttle
    Change adds new navigation throttle that sends every view-source:
    navigation to safe browsing backend in case of active enterprise
    policies
    
    Bug: 437913384
    Change-Id: I617fb1105ea8d35de6037ef216798b21faa31824
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7048803
    Reviewed-by: thefrog <thefrog@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Aliaksei Bahdzevich <thelex@google.com>
    Cr-Commit-Position: refs/heads/main@{#1534886}

--- chrome/browser/chrome_content_browser_client_navigation_throttles.cc
+++ chrome/browser/chrome_content_browser_client_navigation_throttles.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/buildflag.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/data_sharing/data_sharing_navigation_throttle.h"
+#include "chrome/browser/enterprise/data_protection/view_source_navigation_throttle.h"
 #include "chrome/browser/first_party_sets/first_party_sets_navigation_throttle.h"
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/interstitials/enterprise_util.h"
@@ -473,6 +474,8 @@ void CreateAndAddChromeThrottlesForNavigation(
         std::make_unique<safe_browsing::DelayedWarningNavigationThrottle>(
             registry));
   }
+  enterprise_data_protection::ViewSourceNavigationThrottle::MaybeCreateAndAdd(
+      registry, ui_manager);
 #endif  // BUILDFLAG(SAFE_BROWSING_AVAILABLE)
 
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
