e69aadedf036a34052696e215221ebb8f795ae08
Tiffany Shen <tishen@google.com>
2025-10-24 10:35:38 -0700

    [Save to Drive] Add a feature flag for save to drive survey
    This change also adds feature params to supply consumer and enterprise trigger IDs.
    
    Bug: 454388176
    Change-Id: Ie840b273d85722a0bb7c685abf41ae25e04313bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7082868
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Tiffany Shen <tishen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1535146}

--- pdf/pdf_features.h
+++ pdf/pdf_features.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef PDF_PDF_FEATURES_H_
 #define PDF_PDF_FEATURES_H_
 
+#include <string>
+
 #include "base/feature_list.h"
 #include "base/metrics/field_trial_params.h"
 #include "pdf/buildflags.h"
@@ -37,6 +39,12 @@ extern const base::FeatureParam<bool> kPdfInk2TextHighlighting;
 
 #if BUILDFLAG(ENABLE_PDF_SAVE_TO_DRIVE)
 BASE_DECLARE_FEATURE(kPdfSaveToDrive);
+
+BASE_DECLARE_FEATURE(kPdfSaveToDriveSurvey);
+extern const base::FeatureParam<std::string>
+    kPdfSaveToDriveSurveyConsumerTriggerId;
+extern const base::FeatureParam<std::string>
+    kPdfSaveToDriveSurveyEnterpriseTriggerId;
 #endif  // BUILDFLAG(ENABLE_PDF_SAVE_TO_DRIVE)
 
 // Sets whether the OOPIF PDF policy enables the OOPIF PDF viewer. Otherwise,
