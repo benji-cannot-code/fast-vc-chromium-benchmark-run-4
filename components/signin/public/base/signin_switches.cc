c01cbc5935139e372bbc561f44b01a1b95e7cb0d
Sasha Kunitskaya <saskekun@google.com>
2025-10-24 08:02:02 -0700

    [Growth] Adding profile picker strings variations
    Displaying different variations of the header and subtitle in the
    profile picker page, depending on the ProfilePickerTextVariations
    feature flag variation (five versions implemented).
    
    This change is only applied if the user has only one profile.
    
    Bug: 451467899
    Change-Id: I3895cfe2dc84bc96d3e77e40bdaa98366ea098cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7079610
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Commit-Queue: Sasha Kunitskaya <saskekun@google.com>
    Cr-Commit-Position: refs/heads/main@{#1535019}

--- components/signin/public/base/signin_switches.cc
+++ components/signin/public/base/signin_switches.cc
@@ -343,7 +343,6 @@ BASE_FEATURE(kProfileCreationDeclineSigninCTAExperiment,
 BASE_FEATURE(kProfilePickerTextVariations, base::FEATURE_DISABLED_BY_DEFAULT);
 constexpr base::FeatureParam<ProfilePickerVariation>::Option
     kProfilePickerVariations[] = {
-        {ProfilePickerVariation::kDefault, "default"},
         {ProfilePickerVariation::kKeepWorkAndLifeSeparate,
          "keep-work-and-life-separate"},
         {ProfilePickerVariation::kGotAnotherGoogleAccount,
@@ -356,7 +355,8 @@ constexpr base::FeatureParam<ProfilePickerVariation>::Option
 constexpr base::FeatureParam<ProfilePickerVariation>
     kProfilePickerTextVariation{
         &kProfilePickerTextVariations, "profile-picker-variation",
-        ProfilePickerVariation::kDefault, &kProfilePickerVariations};
+        ProfilePickerVariation::kKeepWorkAndLifeSeparate,
+        &kProfilePickerVariations};
 #endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
 
 BASE_FEATURE(kProfilesReordering, base::FEATURE_DISABLED_BY_DEFAULT);
