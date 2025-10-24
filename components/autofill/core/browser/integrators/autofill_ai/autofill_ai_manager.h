771297cc5a8644e6d28cdc5ff42154a24a3d2cb2
Jihad Hanna <jihadghanna@google.com>
2025-10-24 06:18:27 -0700

    Reduce number of arguments in AiManager::HandleFooPromptResult()
    Bug: 40100455
    Change-Id: I98bb7d5ae353ec9e1060c76258882a82cac1b1d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7079291
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1534951}

--- components/autofill/core/browser/integrators/autofill_ai/autofill_ai_manager.h
+++ components/autofill/core/browser/integrators/autofill_ai/autofill_ai_manager.h
@@ -183,9 +183,7 @@ class AutofillAiManager {
   // Updates the `EntityDataManager` and the save strike database depending on
   // the prompt `result`.
   void HandleSavePromptResult(
-      const GURL& form_url,
-      uint64_t form_session_id,
-      const std::string& domain,
+      const FormData& form,
       ukm::SourceId ukm_source_id,
       EntityInstance entity,
       AutofillClient::AutofillAiBubbleClosedReason close_reason);
@@ -193,8 +191,7 @@ class AutofillAiManager {
   // Updates the `EntityDataManager` and the update strike database depending on
   // the prompt `result`.
   void HandleUpdatePromptResult(
-      uint64_t form_session_id,
-      const std::string& domain,
+      const FormData& form,
       ukm::SourceId ukm_source_id,
       EntityInstance updated_entity,
       const EntityInstance::EntityId& existing_entity_id,
@@ -204,9 +201,7 @@ class AutofillAiManager {
   // the Google Wallet server. Updates the strike database depending on the
   // prompt `result`.
   void HandleUpstreamEntityPrompt(
-      const GURL& form_url,
-      uint64_t form_session_id,
-      const std::string& domain,
+      const FormData& form,
       ukm::SourceId ukm_source_id,
       EntityInstance entity,
       EntityInstance::EntityId local_entity,
