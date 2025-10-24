47b7c06e404f99d80f8abbdf818a185d84358ea0
Jiacheng Guo <gjc@google.com>
2025-10-23 23:13:36 -0700

    Experimental feature to allow the navigation to resume after speculative process kills
    The CL adds a feature to allow the navigation before committing to
    resume event if the process for the speculative RFH is killed. A new RFH
    will be automatically created in the next call to
    GetFrameHostForNavigation. The CL also adds a UMA for collecting the
    frequency of such events and whether it is possible to recover.
    
    Bug: 453878130
    Change-Id: I6192de2f1ee49a801fe2bcbe283b245f92668e19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7062071
    Commit-Queue: Jiacheng Guo <gjc@google.com>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1534784}

--- content/browser/security/dip/document_isolation_policy_browsertest.cc
+++ content/browser/security/dip/document_isolation_policy_browsertest.cc
@@ -595,7 +595,9 @@ IN_PROC_BROWSER_TEST_P(DocumentIsolationPolicyBrowserTest,
     // the navigation doesn't use the same process as the crashed process, we
     // can crash the process after the final RenderFrameHost has been picked
     // instead, and the navigation will commit normally.
-    if (ShouldCreateNewHostForAllFrames() || IsBackForwardCacheEnabled()) {
+    if (!base::FeatureList::IsEnabled(
+            features::kResumeNavigationWithSpeculativeRFHProcessGone) &&
+        (ShouldCreateNewHostForAllFrames() || IsBackForwardCacheEnabled())) {
       EXPECT_FALSE(dip_navigation.was_committed());
       return;
     }
@@ -678,7 +680,9 @@ IN_PROC_BROWSER_TEST_P(DocumentIsolationPolicyBrowserTest,
     // the navigation doesn't use the same process as the crashed process, we
     // can crash the process after the final RenderFrameHost has been picked
     // instead, and the navigation will commit normally.
-    if (ShouldCreateNewHostForAllFrames() || IsBackForwardCacheEnabled()) {
+    if (!base::FeatureList::IsEnabled(
+            features::kResumeNavigationWithSpeculativeRFHProcessGone) &&
+        (ShouldCreateNewHostForAllFrames() || IsBackForwardCacheEnabled())) {
       EXPECT_FALSE(non_dip_navigation.was_committed());
       EXPECT_EQ(current_frame_host()
                     ->policy_container_host()
@@ -721,25 +725,9 @@ IN_PROC_BROWSER_TEST_P(DocumentIsolationPolicyBrowserTest,
     // Finish the navigation to the non DIP page.
     ASSERT_TRUE(non_dip_navigation.WaitForNavigationFinished());
 
-    // The navigation will fail if we create speculative RFH when the navigation
-    // started (instead of only when the response started), because the renderer
-    // process will crash and trigger deletion of the speculative RFH and the
-    // navigation using that speculative RFH. BFCache forces a BrowsingInstance
-    // swap (even in this same-site case), hence it also necessitates a
-    // speculative RFH.
-    // TODO(crbug.com/40261276): If the final RenderFrameHost picked for the
-    // navigation doesn't use the same process the navigation should not stop
-    // but go on and commit normally
-    if (ShouldCreateNewHostForAllFrames() || IsBackForwardCacheEnabled()) {
-      EXPECT_FALSE(non_dip_navigation.was_committed());
-      EXPECT_EQ(current_frame_host()
-                    ->policy_container_host()
-                    ->policies()
-                    .document_isolation_policy,
-                GetDocumentIsolationPolicy());
-      return;
-    }
-
+    // The navigation will not fail after killing the process of the dip page.
+    // A new render process has been created during the navigation to the
+    // non-dip page.
     EXPECT_TRUE(non_dip_navigation.was_successful());
     EXPECT_EQ(current_frame_host()
                   ->policy_container_host()
