9e46e6284ceb701b2a13707bb9a26e3fd66d1bbb
Mike West <mkwst@chromium.org>
2025-10-24 01:21:56 -0700

    [Connection-Allowlist] Define the data structures, and parse headers.
    This CL introduces a parser and mojo struct/typemap for the Connection
    Allowlist proposal sketched in http://github.com/mikewest/anti-exfil.
    
    This is one in a series of CLs to prototype the mechanism:
    
    1. https://crrev.com/c/6985001 - [You are here.]
    2. https://crrev.com/c/7071008 - Add allowlists to ParsedHeaders.
    3. https://crrev.com/c/7072371 - Store allowlists on PolicyContainer.
    4. https://crrev.com/c/7072352 - Enforce allowlists in Blink.
    5. https://crrev.com/c/7073851 - about:flags.
    
    Bug: 447954811
    Change-Id: I2d0429e79413a8207afc8f8698560f873036af53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6985001
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1534821}

---
+++ services/network/public/cpp/connection_allowlist_mojom_traits.cc
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/network/public/cpp/connection_allowlist_mojom_traits.h"
+
+namespace mojo {
+
+// static
+bool StructTraits<network::mojom::ConnectionAllowlistDataView,
+                  network::ConnectionAllowlist>::
+    Read(network::mojom::ConnectionAllowlistDataView data,
+         network::ConnectionAllowlist* out) {
+  if (!data.ReadAllowlist(&out->allowlist) ||
+      !data.ReadReportingEndpoint(&out->reporting_endpoint) ||
+      !data.ReadIssues(&out->issues)) {
+    return false;
+  }
+  return true;
+}
+
+// static
+bool StructTraits<network::mojom::ConnectionAllowlistsDataView,
+                  network::ConnectionAllowlists>::
+    Read(network::mojom::ConnectionAllowlistsDataView data,
+         network::ConnectionAllowlists* out) {
+  if (!data.ReadEnforced(&out->enforced) ||
+      !data.ReadReportOnly(&out->report_only)) {
+    return false;
+  }
+  return true;
+}
+
+}  // namespace mojo
