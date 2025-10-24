bfb2ccb91a5e7e448c02287affe434de908ab8f5
Norge Vizcay <vizcay@google.com>
2025-10-24 08:28:54 -0700

    Rename AddOrUpdateValuableMetadata into AddOrUpdateEntityMetadata,
    Bug: 436551488
    Change-Id: I58670849a6c1468bfb6e42a7005f2ca806898b8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7079396
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1535042}

--- components/autofill/core/browser/webdata/autofill_ai/entity_table.cc
+++ components/autofill/core/browser/webdata/autofill_ai/entity_table.cc
@@ -375,7 +375,7 @@ bool EntityTable::RemoveEntityMetadata(const EntityInstance::EntityId& guid) {
                              entities_metadata::kEntityGuid, *guid);
 }
 
-bool EntityTable::AddOrUpdateValuableMetadata(
+bool EntityTable::AddOrUpdateEntityMetadata(
     const EntityInstance::EntityMetadata& metadata) {
   sql::Transaction transaction(db());
   return transaction.Begin() && RemoveEntityMetadata(metadata.guid) &&
--- components/autofill/core/browser/webdata/valuables/valuable_metadata_sync_bridge.cc
+++ components/autofill/core/browser/webdata/valuables/valuable_metadata_sync_bridge.cc
@@ -182,7 +182,7 @@ ValuableMetadataSyncBridge::MergeRemoteChanges(
             change->data().specifics.autofill_valuable_metadata();
         EntityInstance::EntityMetadata remote =
             CreateValuableMetadataFromSpecifics(specifics);
-        if (!table->AddOrUpdateValuableMetadata(remote)) {
+        if (!table->AddOrUpdateEntityMetadata(remote)) {
           return syncer::ModelError(
               FROM_HERE, syncer::ModelError::Type::
                              kAutofillValuableMetadataFailedToLoadDatabase);
