1cb27c62e12173f1fb75b2f268ad88e0ad43a0f0
Calder Kitagawa <ckitagawa@chromium.org>
2025-10-24 14:18:20 -0700

    AuxiliarySearchProvider better use JniType
    TabImpl#getAllNativePtrs does not really need to exist and it causes
    extra JNI roundtrips wherever it is used. This CL migrates one client
    off of it.
    
    Bug: 454938134
    Change-Id: I39677a56f7a6798a6b191f3393d76f950febb931
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7081652
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1535313}

--- chrome/browser/auxiliary_search/java/src/org/chromium/chrome/browser/auxiliary_search/AuxiliarySearchBridge.java
+++ chrome/browser/auxiliary_search/java/src/org/chromium/chrome/browser/auxiliary_search/AuxiliarySearchBridge.java
@@ -56,7 +56,7 @@ public class AuxiliarySearchBridge {
         AuxiliarySearchBridgeJni.get()
                 .getNonSensitiveTabs(
                         mNativeBridge,
-                        tabs.toArray(new Tab[0]),
+                        tabs,
                         new Callback<Object[]>() {
                             @Override
                             public void onResult(Object[] tabs) {
@@ -125,7 +125,9 @@ public class AuxiliarySearchBridge {
         long getForProfile(@JniType("Profile*") Profile profile);
 
         void getNonSensitiveTabs(
-                long nativeAuxiliarySearchProvider, Tab[] tabs, Callback<Object[]> callback);
+                long nativeAuxiliarySearchProvider,
+                @JniType("std::vector<TabAndroid*>") List<Tab> tabs,
+                Callback<Object[]> callback);
 
         void getNonSensitiveHistoryData(
                 long nativeAuxiliarySearchProvider,
