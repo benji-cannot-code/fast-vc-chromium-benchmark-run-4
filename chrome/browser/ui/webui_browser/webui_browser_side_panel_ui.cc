64c7706f3eafce3fa3453676132f32a64940e1dd
Caroline Rising <corising@chromium.org>
2025-10-24 12:50:34 -0700

    Update side panel current_key to take panel type as a param
    Bug: 445442616
    Change-Id: Iba21cb5ca21a12da15f60d28bb86b623fa8de488
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7080145
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1535239}

--- chrome/browser/ui/webui_browser/webui_browser_side_panel_ui.cc
+++ chrome/browser/ui/webui_browser/webui_browser_side_panel_ui.cc
@@ -40,11 +40,12 @@ WebUIBrowserSidePanelUI::WebUIBrowserSidePanelUI(Browser* browser)
 WebUIBrowserSidePanelUI::~WebUIBrowserSidePanelUI() = default;
 
 void WebUIBrowserSidePanelUI::Close() {
-  if (!current_key().has_value()) {
+  if (!current_key(SidePanelEntry::PanelType::kContent).has_value()) {
     return;
   }
 
-  if (SidePanelEntry* entry = GetEntryForUniqueKey(*current_key())) {
+  if (SidePanelEntry* entry = GetEntryForUniqueKey(
+          *current_key(SidePanelEntry::PanelType::kContent))) {
     entry->OnEntryWillHide(SidePanelEntryHideReason::kSidePanelClosed);
   }
   // Asynchronously close the side panel in webshell.
@@ -94,7 +95,7 @@ void WebUIBrowserSidePanelUI::Show(
   }
 
   SidePanelEntry* entry = GetEntryForUniqueKey(input);
-  if (current_key() && *current_key() == input) {
+  if (current_key(entry->type()) && *current_key(entry->type()) == input) {
     waiter(entry->type())->ResetLoadingEntryIfNecessary();
 
     // TODO(webium): Implement the following:
@@ -121,8 +122,9 @@ void WebUIBrowserSidePanelUI::PopulateSidePanel(
     std::optional<SidePanelUtil::SidePanelOpenTrigger> open_trigger,
     SidePanelEntry* entry,
     std::optional<std::unique_ptr<views::View>> content_view) {
-  if (current_key()) {
-    SidePanelEntry* previous_entry = GetEntryForUniqueKey(*current_key());
+  if (current_key(entry->type())) {
+    SidePanelEntry* previous_entry =
+        GetEntryForUniqueKey(*current_key(entry->type()));
     if (previous_entry) {
       previous_entry->OnEntryWillHide(SidePanelEntryHideReason::kReplaced);
 
@@ -189,11 +191,12 @@ void WebUIBrowserSidePanelUI::MaybeShowEntryOnTabStripModelChanged(
       old_contextual_registry
           ->GetActiveEntryFor(SidePanelEntry::PanelType::kContent)
           .has_value() &&
-      current_key().has_value() &&
+      current_key(SidePanelEntry::PanelType::kContent).has_value() &&
       (*old_contextual_registry->GetActiveEntryFor(
            SidePanelEntry::PanelType::kContent))
-              ->key() == current_key()->key &&
-      current_key()->tab_handle) {
+              ->key() ==
+          current_key(SidePanelEntry::PanelType::kContent)->key &&
+      current_key(SidePanelEntry::PanelType::kContent)->tab_handle) {
     auto* active_entry =
         old_contextual_registry
             ->GetActiveEntryFor(SidePanelEntry::PanelType::kContent)
@@ -207,11 +210,11 @@ void WebUIBrowserSidePanelUI::MaybeShowEntryOnTabStripModelChanged(
 
 void WebUIBrowserSidePanelUI::OnSidePanelClosed(
     SidePanelEntry::PanelType type) {
-  if (!current_key()) {
+  if (!current_key(type)) {
     return;
   }
 
-  SidePanelEntry* previous_entry = GetEntryForUniqueKey(*current_key());
+  SidePanelEntry* previous_entry = GetEntryForUniqueKey(*current_key(type));
   SetCurrentKey(type, std::nullopt);
   if (previous_entry) {
     previous_entry->OnEntryHidden();
