5466750345952f77a6b914bfa0187ef5338d5784
Chrome Crash (Prod) <crash@system.gserviceaccount.com>
2025-10-24 11:59:43 -0700

    Revert "Add some CHECKs to catch unexpected re-entrancy in the SocketPools."
    This reverts commit 2257459333be7cbabaebbcd72cbeb3571b99e77b.
    
    Reason for revert: 
    The Suspect CL https://chromium-review.googlesource.com/q/Iad06d9f8c890e4dc035fcd9c1a850fb1444aca48 is highly likely the cause of the crash. Please see the crash report http://crash/1d3b26e656ca1a38.
    Rebase the Revert CL on top of main branch with conflicts allowed option to prevent false positive merge conflicts.
    
    If you agree with the revert CL, please approve it and mark as Commit-Queue +2 to submit it.
    
    More details can be found in the go/crash-revert-cls-creation.
    
    Original change's description:
    > Add some CHECKs to catch unexpected re-entrancy in the SocketPools.
    > 
    > We're seeing some crashes that may or may not be caused by re-entrancy
    > in TransportClientSocketPool, which should not occur. The SocketPools
    > are complicated enough that it may be worth keeping these checks. They
    > should be lightweight, with minimal perf impact.
    > 
    > Bug: 422046500
    > Change-Id: Iad06d9f8c890e4dc035fcd9c1a850fb1444aca48
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7075120
    > Commit-Queue: mmenke <mmenke@chromium.org>
    > Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1534332}
    > 
    Crash Bug: 454924370
    
    Change-Id: I66518e09d76a2009d823c1557cdd8a4a0f70deb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7079159
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: mmenke <mmenke@chromium.org>
    Reviewed-by: mmenke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1535211}

--- net/socket/transport_client_socket_pool.cc
+++ net/socket/transport_client_socket_pool.cc
@@ -121,18 +121,6 @@ ConnectJob* TransportClientSocketPool::Request::ReleaseJob() {
   return job;
 }
 
-TransportClientSocketPool::ScopedReentrancyProtector::ScopedReentrancyProtector(
-    TransportClientSocketPool* parent)
-    : parent_(parent) {
-  CHECK(!parent_->reentrancy_protector_);
-  parent_->reentrancy_protector_ = true;
-}
-
-TransportClientSocketPool::ScopedReentrancyProtector::
-    ~ScopedReentrancyProtector() {
-  parent_->reentrancy_protector_ = false;
-}
-
 struct TransportClientSocketPool::IdleSocket {
   // An idle socket can't be used if it is disconnected or has been used
   // before and has received data unexpectedly (hence no longer idle).  The
@@ -272,7 +260,6 @@ int TransportClientSocketPool::RequestSocket(
     const ProxyAuthCallback& proxy_auth_callback,
     bool fail_if_alias_requires_proxy_override,
     const NetLogWithSource& net_log) {
-  ScopedReentrancyProtector protector(this);
   CHECK(callback);
   CHECK(handle);
 
@@ -326,8 +313,6 @@ int TransportClientSocketPool::RequestSockets(
     bool fail_if_alias_requires_proxy_override,
     CompletionOnceCallback callback,
     const NetLogWithSource& net_log) {
-  ScopedReentrancyProtector protector(this);
-
   // TODO(eroman): Split out the host and port parameters.
   net_log.AddEvent(NetLogEventType::TCP_CLIENT_SOCKET_POOL_REQUESTED_SOCKETS,
                    [&] { return NetLogGroupIdParams(group_id); });
@@ -593,8 +578,6 @@ void TransportClientSocketPool::LogBoundConnectJobToRequest(
 void TransportClientSocketPool::SetPriority(const GroupId& group_id,
                                             ClientSocketHandle* handle,
                                             RequestPriority priority) {
-  ScopedReentrancyProtector protector(this);
-
   auto group_it = group_map_.find(group_id);
   if (group_it == group_map_.end()) {
     DCHECK(base::Contains(pending_callback_map_, handle));
@@ -609,8 +592,6 @@ void TransportClientSocketPool::SetPriority(const GroupId& group_id,
 void TransportClientSocketPool::CancelRequest(const GroupId& group_id,
                                               ClientSocketHandle* handle,
                                               bool cancel_connect_job) {
-  ScopedReentrancyProtector protector(this);
-
   auto callback_it = pending_callback_map_.find(handle);
   if (callback_it != pending_callback_map_.end()) {
     int result = callback_it->second.result;
@@ -629,8 +610,8 @@ void TransportClientSocketPool::CancelRequest(const GroupId& group_id,
         if (group->unbound_request_count() == 0)
           socket->Disconnect();
       }
-      ReleaseSocketInternal(handle->group_id(), std::move(socket),
-                            handle->group_generation());
+      ReleaseSocket(handle->group_id(), std::move(socket),
+                    handle->group_generation());
     }
     return;
   }
@@ -671,7 +652,6 @@ void TransportClientSocketPool::CancelRequest(const GroupId& group_id,
 
 void TransportClientSocketPool::CloseIdleSockets(
     const char* net_log_reason_utf8) {
-  ScopedReentrancyProtector protector(this);
   CleanupIdleSockets(true, net_log_reason_utf8);
   DCHECK_EQ(0, idle_socket_count_);
 }
@@ -1051,14 +1031,6 @@ void TransportClientSocketPool::ReleaseSocket(
     const GroupId& group_id,
     std::unique_ptr<StreamSocket> socket,
     int64_t group_generation) {
-  ScopedReentrancyProtector protector(this);
-  ReleaseSocketInternal(group_id, std::move(socket), group_generation);
-}
-
-void TransportClientSocketPool::ReleaseSocketInternal(
-    const GroupId& group_id,
-    std::unique_ptr<StreamSocket> socket,
-    int64_t group_generation) {
   auto i = group_map_.find(group_id);
   CHECK(i != group_map_.end());
 
@@ -1071,7 +1043,7 @@ void TransportClientSocketPool::ReleaseSocketInternal(
   CHECK_GT(group->active_socket_count(), 0);
   group->DecrementActiveSocketCount();
 
-  bool can_reuse_socket = false;
+  bool can_resuse_socket = false;
   std::string_view not_reusable_reason;
   if (!socket->IsConnectedAndIdle()) {
     if (!socket->IsConnected()) {
@@ -1082,10 +1054,10 @@ void TransportClientSocketPool::ReleaseSocketInternal(
   } else if (group_generation != group->generation()) {
     not_reusable_reason = kSocketGenerationOutOfDate;
   } else {
-    can_reuse_socket = true;
+    can_resuse_socket = true;
   }
 
-  if (can_reuse_socket) {
+  if (can_resuse_socket) {
     DCHECK(not_reusable_reason.empty());
 
     // Add it to the idle list.
@@ -1182,8 +1154,6 @@ void TransportClientSocketPool::OnIPAddressChanged(
 void TransportClientSocketPool::FlushWithError(
     int error,
     const char* net_log_reason_utf8) {
-  // This method doesn't use ScopedReentrancyProtector, because the methods it
-  // invokes all use their own.
   CancelAllConnectJobs();
   CloseIdleSockets(net_log_reason_utf8);
   CancelAllRequestsWithError(error);
@@ -1282,7 +1252,6 @@ void TransportClientSocketPool::AddIdleSocket(
 }
 
 void TransportClientSocketPool::CancelAllConnectJobs() {
-  ScopedReentrancyProtector protector(this);
   for (auto i = group_map_.begin(); i != group_map_.end();) {
     Group* group = i->second;
     CHECK(group);
@@ -1299,7 +1268,6 @@ void TransportClientSocketPool::CancelAllConnectJobs() {
 }
 
 void TransportClientSocketPool::CancelAllRequestsWithError(int error) {
-  ScopedReentrancyProtector protector(this);
   for (auto i = group_map_.begin(); i != group_map_.end();) {
     Group* group = i->second;
     CHECK(group);
@@ -1366,7 +1334,6 @@ bool TransportClientSocketPool::CloseOneIdleSocketExceptInGroup(
 void TransportClientSocketPool::OnConnectJobComplete(Group* group,
                                                      int result,
                                                      ConnectJob* job) {
-  ScopedReentrancyProtector protector(this);
   DCHECK_NE(ERR_IO_PENDING, result);
   DCHECK(group_map_.find(group->group_id()) != group_map_.end());
   DCHECK_EQ(group, group_map_[group->group_id()]);
