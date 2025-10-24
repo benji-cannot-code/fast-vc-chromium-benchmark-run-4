56db8a97e9bf32764bd66af0bf5839c2309f69fc
James Sun <jamessun@google.com>
2025-10-24 13:56:57 -0700

    [CleanUp] Limit the mock class scope to its own test file
    This CL limits MockCreditCardFormEventLogger and
    MockBrowserAutofillManager to the test file by moving to anonymous
    namespace to avoid unintended linkage in autofill namespace
    
    Bug: 444684247
    Change-Id: I4a58b09e53ca7bf681e7d78dad4f521837afabc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7081345
    Commit-Queue: James Sun <jamessun@google.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Cr-Commit-Position: refs/heads/main@{#1535296}

--- components/autofill/core/browser/payments/bnpl_manager_unittest.cc
+++ components/autofill/core/browser/payments/bnpl_manager_unittest.cc
@@ -45,7 +45,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/origin.h"
 
-namespace autofill {
+namespace autofill::payments {
+
+using IssuerId = autofill::BnplIssuer::IssuerId;
+using ::testing::_;
+using ::testing::AnyOf;
+using ::testing::Eq;
+using ::testing::Field;
+using ::testing::FieldsAre;
+using ::testing::InSequence;
+using ::testing::Matcher;
+using ::testing::NiceMock;
+using ::testing::Property;
+using ::testing::Return;
+using ::testing::ReturnRef;
+using ::testing::SaveArg;
+using ::testing::Test;
+
+namespace {
 class MockCreditCardFormEventLogger
     : public autofill_metrics::CreditCardFormEventLogger {
  public:
@@ -67,26 +84,7 @@ class MockBrowserAutofillManager : public TestBrowserAutofillManager {
               (),
               (override));
 };
-}  // namespace autofill
 
-namespace autofill::payments {
-
-using IssuerId = autofill::BnplIssuer::IssuerId;
-using ::testing::_;
-using ::testing::AnyOf;
-using ::testing::Eq;
-using ::testing::Field;
-using ::testing::FieldsAre;
-using ::testing::InSequence;
-using ::testing::Matcher;
-using ::testing::NiceMock;
-using ::testing::Property;
-using ::testing::Return;
-using ::testing::ReturnRef;
-using ::testing::SaveArg;
-using ::testing::Test;
-
-namespace {
 class PaymentsNetworkInterfaceMock : public PaymentsNetworkInterface {
  public:
   PaymentsNetworkInterfaceMock()
@@ -253,7 +251,7 @@ class BnplManagerTest : public Test,
     CreateAutofillDriver();
 
     credit_card_form_event_logger_ =
-        std::make_unique<NiceMock<autofill::MockCreditCardFormEventLogger>>(
+        std::make_unique<NiceMock<MockCreditCardFormEventLogger>>(
             &autofill_manager());
 
     ON_CALL(autofill_manager(), GetCreditCardFormEventLogger())
@@ -336,8 +334,7 @@ class BnplManagerTest : public Test,
 
  protected:
   base::test::TaskEnvironment task_environment_;
-  std::unique_ptr<autofill::MockCreditCardFormEventLogger>
-      credit_card_form_event_logger_;
+  std::unique_ptr<MockCreditCardFormEventLogger> credit_card_form_event_logger_;
   std::unique_ptr<BnplManager> bnpl_manager_;
   raw_ptr<PaymentsNetworkInterfaceMock> payments_network_interface_;
   base::test::ScopedFeatureList scoped_feature_list_;
