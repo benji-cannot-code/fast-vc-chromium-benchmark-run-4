fc79bc8cfce69e79e90cc0d65ff67f2b66dab141
Robbie Gibson <rkgibson@google.com>
2025-10-24 09:16:22 -0700

    [iOS] Save customization choice immediately for main menu
    Before, any in-progress customization changes from the main
    customization menu weren't committed until the menu was closed. This
    meant that if the user made a change in the main menu, then made a
    change in a submenu, and then cancelled in the submenu, the state went
    back to the initial background, not the background after the first
    change. This is confusing to many users.
    
    To fix this, changes on the main menu are now immediately committed, so
    later cancels will reset to the newly committed state.
    
    As part of this, the customization service's stored recently used list
    can now diverge more from the recently used UI, as the UI is only
    updated when the menu first opens. To prevent any issues with
    user-provided images being deleted too early, these images are now no
    longer cleaned up when they leave the service's recently used list, as
    there may still be UI options for these images.
    
    Instead, these no-longer-accessible images will be cleaned up on the
    next app start.
    
    Bug: b:454342960
    Change-Id: I4c103099c8a65a79741d34ece43e9e5d92815b28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7080289
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1535074}

--- ios/chrome/browser/home_customization/coordinator/home_customization_background_configuration_mediator.mm
+++ ios/chrome/browser/home_customization/coordinator/home_customization_background_configuration_mediator.mm
@@ -387,6 +387,10 @@ const net::NetworkTrafficAnnotationTag kTrafficAnnotation =
       [self generateRecentBackgroundForConfiguration:configurationItem]);
 }
 
+- (void)saveBackground {
+  [self saveCurrentTheme];
+}
+
 #pragma mark - HomeBackgroundCustomizationServiceObserving
 
 - (void)onBackgroundChanged {
--- ios/chrome/browser/home_customization/ui/home_customization_main_view_controller.mm
+++ ios/chrome/browser/home_customization/ui/home_customization_main_view_controller.mm
@@ -387,6 +387,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   [self.customizationMutator
       applyBackgroundForConfiguration:backgroundConfiguration];
+  // Main menu does not have Cancel/Done buttons, so save the background
+  // immediately.
+  [self.customizationMutator saveBackground];
 
   _recentBackgroundClickCount += 1;
 
