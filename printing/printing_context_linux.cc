01e883f7f745f86f7bace94cdb23b29bcb684dbf
Lei Zhang <thestig@chromium.org>
2025-10-23 21:17:37 -0700

    Avoid a reachable NOTREACHED() in PrintingContextLinux
    In https://crrev.com/249831, PrintingContextLinux::AskUserForSettings()
    handled a nullptr crash by checking for it and marking the nullptr path
    with NOTREACHED(). Since then, NOTREACHED() has become fatal when
    reached. So fix this issue again by simply handling the nullptr and
    returning early.
    
    Bug: 454314510
    Change-Id: Ia0eacd8a6e3a1886b7d161410e75b5fe3822a64a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7081117
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1534751}

--- printing/printing_context_linux.cc
+++ printing/printing_context_linux.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/check.h"
-#include "base/notreached.h"
 #include "base/values.h"
 #include "build/buildflag.h"
 #include "printing/buildflags/buildflags.h"
@@ -56,9 +55,9 @@ void PrintingContextLinux::AskUserForSettings(int max_pages,
                                               bool is_scripted,
                                               PrintSettingsCallback callback) {
   if (!print_dialog_) {
-    // Can only get here if the renderer is sending bad messages.
-    // http://crbug.com/341777
-    NOTREACHED();
+    // Can only get here if the renderer is sending bad messages. Ignore.
+    // https://crbug.com/41088489
+    return;
   }
 
   print_dialog_->ShowDialog(delegate_->GetParentView(), has_selection,
