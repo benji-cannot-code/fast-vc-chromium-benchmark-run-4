a5492c4ce99b8d14e9401088396dcdfbf07d6216
Yuki Shiino <yukishiino@chromium.org>
2025-10-24 00:26:06 -0700

    ELUD: Add ADVANCED_MEMORY_SAFETY_CHECKS to download::DownloadItemImpl
    Adds ADVANCED_MEMORY_SAFETY_CHECKS as a mitigation of
    crbug.com/422045023
    
    Bug: 422045023
    Change-Id: Ia80539baf88d9edf292ff83922393f173b27b761
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7076738
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1534801}

--- components/download/public/common/download_item_impl.h
+++ components/download/public/common/download_item_impl.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/functional/callback_forward.h"
 #include "base/memory/raw_ptr.h"
+#include "base/memory/safety_checks.h"
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
 #include "base/threading/thread_checker.h"
@@ -45,6 +46,9 @@ class DownloadItemImplDelegate;
 class COMPONENTS_DOWNLOAD_EXPORT DownloadItemImpl
     : public DownloadItem,
       public DownloadDestinationObserver {
+  // TODO(crbug.com/422045023): Remove this macro once the bug gets fixed.
+  ADVANCED_MEMORY_SAFETY_CHECKS();
+
  public:
   // Information about the initial request that triggers the download. Most of
   // the fields are immutable after the DownloadItem is successfully
