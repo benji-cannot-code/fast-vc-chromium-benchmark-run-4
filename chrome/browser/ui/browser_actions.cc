febf2b8e8ade1da5854466b2100d25e999260c46
Kunal Daftari <kunaldaftari@google.com>
2025-10-24 10:17:44 -0700

    [Vertical Tabs] Tab Search Button
    This CL builds off of the top button container. It
    introduces the tab search button. This requires modifying
    the non client hit test to accommodate this change as well as
    creating an action item relationship.
    
    Bug: 439961435
    Change-Id: Ib96180cad5ac39c5ec30b296adebdadc6c496911
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6973126
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Kunal Daftari <kunaldaftari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1535129}

--- chrome/browser/ui/browser_actions.cc
+++ chrome/browser/ui/browser_actions.cc
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/search/omnibox_utils.h"
 #include "chrome/browser/ui/send_tab_to_self/send_tab_to_self_bubble.h"
 #include "chrome/browser/ui/send_tab_to_self/send_tab_to_self_toolbar_icon_controller.h"
+#include "chrome/browser/ui/tabs/features.h"
 #include "chrome/browser/ui/tabs/public/tab_features.h"
 #include "chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_utils.h"
 #include "chrome/browser/ui/toolbar/cast/cast_toolbar_button_util.h"
@@ -476,7 +477,10 @@ void BrowserActions::InitializeBrowserActions() {
           .SetEnabled(IncognitoModePrefs::IsIncognitoAllowed(profile))
           .Build());
 
-  if (features::HasTabSearchToolbarButton()) {
+  // Both TabSearch in the toolbar and in Vertical Tabs implementations use
+  // ActionItems to represent the 'TabSearch' action.
+  if (features::HasTabSearchToolbarButton() ||
+      tabs::IsVerticalTabsFeatureEnabled()) {
     root_action_item_->AddChild(
         ChromeMenuAction(
             base::BindRepeating(
