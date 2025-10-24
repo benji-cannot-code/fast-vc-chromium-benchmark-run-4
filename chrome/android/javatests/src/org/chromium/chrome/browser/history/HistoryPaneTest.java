eb02183a9d0988455ac6d03a6f054d2cc2ffdb8c
Henrique Nakashima <hnakashima@chromium.org>
2025-10-24 10:47:31 -0700

    [Android] Re-enable 2 cases in HistoryPaneTest
    Both the causes of failures in android-14-arm64-rel and
    android-16-x64-rel are fixed.
    
    Cq-Include-Trybots: luci.chromium.try:android-16-x64-rel,android-14-arm64-rel
    Bug: 442651404,442838631
    Change-Id: I2e039b9645eca6c44266d25964a8320bf2e2ba5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7080388
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1535152}

--- chrome/android/javatests/src/org/chromium/chrome/browser/history/HistoryPaneTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/history/HistoryPaneTest.java
@@ -17,7 +17,6 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.browsing_data.BrowsingDataBridge;
@@ -80,7 +79,6 @@ public class HistoryPaneTest {
 
     @Test
     @MediumTest
-    @DisabledTest(message = "https://crbug.com/442838631")
     public void testOpenedHistoryItem_HistoryItemsAreDisplayed() {
         String urlOne =
                 mCtaTestRule.getTestServer().getURL("/chrome/test/data/android/navigate/one.html");
@@ -99,7 +97,6 @@ public class HistoryPaneTest {
 
     @Test
     @MediumTest
-    @DisabledTest(message = "https://crbug.com/442651404")
     public void testOpenedHistoryItem_SearchMatch() {
         String urlOne =
                 mCtaTestRule.getTestServer().getURL("/chrome/test/data/android/navigate/one.html");
