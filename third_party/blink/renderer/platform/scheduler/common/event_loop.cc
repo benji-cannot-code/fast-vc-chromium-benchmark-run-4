fedff6bc1b24838db3592ba9845668c2dcce671e
Scott Haseley <shaseley@chromium.org>
2025-10-24 11:59:32 -0700

    TaskAttribution: Add BlinkTaskState trace events for microtasks
    This CL primarily adds trace events with task state information for
    microtasks, so we can get the task_state_id. We already output this for
    async task propagation when creating a TaskScope, e.g. postMessage,
    setTimeout propagation, but don't for microtasks since this is handled
    by v8. This will be valuable to diagnose soft nav detection issues
    related to task attribution.
    
    To add these trace events, we use v8's PromiseHook API to get callbacks
    before and after running Promise-related microtask, as well as add hooks
    into EventLoop for internal microtasks. Unconditionally registering a
    PromiseHook would likely be too expensive, so we mitigate this by only
    registering the hook when tracing is enabled for the relevant category,
    and we only check this at construction time and when tracing is turned
    on (via AsyncEnabledStateObserver).
    
    This also unifies the existing trace event, renaming BlinkTaskScope to
    BlinkTaskState, and moving this to a new "task_attribution" trace
    category (given the additional PromiseHook overhead and main use case, I
    wanted to separate this from "scheduler", which has other use cases).
    
    Bug: 447555069
    Enabled-by-default-reason: killswitch
    Change-Id: I1579ca66c60202ed4f597d7167ec37df3b5aff7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6986393
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Reviewed-by: Michal Mocny <mmocny@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1535210}

--- third_party/blink/renderer/platform/scheduler/common/event_loop.cc
+++ third_party/blink/renderer/platform/scheduler/common/event_loop.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/bindings/script_forbidden_scope.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/scheduler/public/frame_or_worker_scheduler.h"
+#include "third_party/blink/renderer/platform/scheduler/public/task_attribution_tracker.h"
 #include "v8/include/v8.h"
 
 namespace blink {
@@ -102,6 +103,7 @@ void EventLoop::RunPendingMicrotask(void* data) {
   auto* self = static_cast<EventLoop*>(data);
   base::OnceClosure task = std::move(self->pending_microtasks_.front());
   self->pending_microtasks_.pop_front();
+  TaskAttributionTracker::MicrotaskTraceScope scope(self->isolate_);
   std::move(task).Run();
 }
 
