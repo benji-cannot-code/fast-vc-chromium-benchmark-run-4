05a629c2fff1cd0c22c6abd477bdf6405cb752e1
Dave Tapuska <dtapuska@chromium.org>
2025-10-24 09:46:08 -0700

    [actor] Support knowing the interrupted state
    The interrupted state is different from the paused state. It needs
    to be not accounted for in the time spent metrics. Ensure that the
    front end can let the ActorTask know about the state.
    
    Bug: b:454633178, b:440107430
    Change-Id: I86489cc8087cedf02d5bff08e3eba112fdbf9537
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7081023
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Reviewed-by: Nick Birnie <birnie@google.com>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1535094}

--- chrome/browser/actor/actor_task.cc
+++ chrome/browser/actor/actor_task.cc
@@ -31,10 +31,15 @@ namespace actor {
 
 namespace {
 
-bool IsStateActive(ActorTask::State state) {
+bool IsStateActiveAndNotWaiting(ActorTask::State state) {
   return (state == ActorTask::State::kCreated ||
           state == ActorTask::State::kActing ||
           state == ActorTask::State::kReflecting);
+}
+
+bool IsStateActive(ActorTask::State state) {
+  return (IsStateActiveAndNotWaiting(state) ||
+          state == ActorTask::State::kWaitingOnUser);
 }
 
 void SetFocusState(content::WebContents* contents,
@@ -117,9 +122,13 @@ void ActorTask::SetState(State new_state) {
             {kReflecting, kPausedByActor, kPausedByUser, kCancelled,
              kFinished}},
            {kReflecting,
-            {kActing, kPausedByActor, kPausedByUser, kCancelled, kFinished}},
+            {kActing, kPausedByActor, kPausedByUser, kCancelled, kFinished,
+             kWaitingOnUser}},
            {kPausedByActor, {kReflecting, kCancelled, kFinished}},
            {kPausedByUser, {kReflecting, kCancelled, kFinished}},
+           {kWaitingOnUser,
+            {kActing, kReflecting, kPausedByActor, kPausedByUser, kCancelled,
+             kFinished}},
            {kCancelled, {}},
            {kFinished, {}}}));
   if (new_state != state_) {
@@ -134,7 +143,7 @@ void ActorTask::SetState(State new_state) {
 
   // If the old state was active, add its duration to the total active time for
   // the task.
-  if (IsActive()) {
+  if (IsStateActiveAndNotWaiting(old_state)) {
     total_active_time_ += old_state_duration;
   }
 
@@ -262,6 +271,20 @@ void ActorTask::Resume() {
 bool ActorTask::IsPaused() const {
   return (GetState() == State::kPausedByActor) ||
          (GetState() == State::kPausedByUser);
+}
+
+void ActorTask::Interrupt() {
+  if (GetState() != State::kReflecting) {
+    return;
+  }
+  SetState(State::kWaitingOnUser);
+}
+
+void ActorTask::Uninterrupt() {
+  if (GetState() != State::kWaitingOnUser) {
+    return;
+  }
+  SetState(State::kReflecting);
 }
 
 bool ActorTask::IsStopped() const {
@@ -508,6 +531,8 @@ std::string ToString(const ActorTask::State& state) {
       return "Cancelled";
     case kFinished:
       return "Finished";
+    case kWaitingOnUser:
+      return "WaitingOnUser";
   }
 }
 
