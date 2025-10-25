9d90be96cc4961f9d7aceae44f4579d69e2957a9
Stefan Zager <szager@chromium.org>
2025-10-24 18:11:03 -0700

    [IntersectionObserver] Clean up tracking of per-frame state
    Currently, we calculate has_active_observations and
    needs_occlusion_tracking inline while computing observations. There
    are a few problems with that:
    
    - The code is complicated and hard to follow.
    - The post-layout and post-lifecycle IntersectionObserver tree walks
    have slightly different behaviors, so it's not always clear when or
    how a state variable is recomputed.
    - The post-layout tree walk may run multiple times, interleaved with
    tree mutations caused by script, so it's not always clear when the
    state variables get their final value.
    - The most authoritative bookkeeping is done during the post-lifecycle
    walk, but that is too late to bundle state information with a commit.
    
    This CL factors out the computation of per-frame state into a separate
    tree walk, UpdateIntersectionObserverStatus. It runs after the main
    style/layout/pre-paint loop when the values are stable, but still
    early enough to bundle the information along with the pending commit.
    
    Bug: chromium:40914013
    Change-Id: I9d4764c92a800f39f0cb8825b89d86a8a7636377
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7080431
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1535429}

--- third_party/blink/renderer/core/frame/local_frame_view.h
+++ third_party/blink/renderer/core/frame/local_frame_view.h
@@ -1033,7 +1033,12 @@ class CORE_EXPORT LocalFrameView final
 
   void ForAllRemoteFrameViews(base::FunctionRef<void(RemoteFrameView&)>);
 
-  bool UpdateViewportIntersectionsForSubtree(
+  // Recomputes the values returned by HasActiveIntersectionObservations() and
+  // NeedsOcclusionTracking().
+  void UpdateIntersectionObserverStatus() override;
+  bool HasActiveIntersectionObservations() const override;
+  bool NeedsOcclusionTracking() const override;
+  void UpdateViewportIntersectionsForSubtree(
       unsigned parent_flags,
       ComputeIntersectionsContext&) override;
   void DeliverSynchronousIntersectionObservations();
@@ -1189,6 +1194,12 @@ class CORE_EXPORT LocalFrameView final
 #endif
 
   IntersectionObservationState intersection_observation_state_;
+  // True if this FrameView or any descendant FrameView has active
+  // IntersectionObservers.
+  bool has_active_intersection_observations_ = false;
+  // True if this FrameView or any descendant FrameView has active
+  // IntersectionObservers for which observer->trackVisibility() is true.
+  bool needs_occlusion_tracking_ = false;
   gfx::Vector2dF accumulated_scroll_delta_since_last_intersection_update_;
   // Used only if the frame is the local root.
   HeapTaskRunnerTimer<LocalFrameView> delayed_intersection_timer_;
