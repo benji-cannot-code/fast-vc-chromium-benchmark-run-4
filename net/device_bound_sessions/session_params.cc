b89fd69173f82a5401929a54c0be2d05a6b4074e
Daniel Rubery <drubery@chromium.org>
2025-10-27 11:25:21 -0700

    Basic mojo typemapping for SessionParams
    This CL creates a mojo typemap for SessionParams. Since this is largely
    routine code, it's split out from the dependent CL, which uses it.
    
    Compile-Size: Size increase is unavoidable.
    Bug: 439821296
    Change-Id: I2b14e3770ba1b162443dacca6246e5198386e0ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7040427
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Etienne Dechamps <edechamps@google.com>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1536113}

--- net/device_bound_sessions/session_params.cc
+++ net/device_bound_sessions/session_params.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace net::device_bound_sessions {
 
+SessionParams::SessionParams() = default;
+
 SessionParams::SessionParams(
     std::string id,
     GURL fetcher_url,
--- net/device_bound_sessions/session_params.h
+++ net/device_bound_sessions/session_params.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace net::device_bound_sessions {
 
+// LINT.IfChange(SessionParams)
 // Struct to contain the parameters from the session instruction JSON.
 // https://github.com/WICG/dbsc/blob/main/README.md#session-registration-instructions-json
 // This is sent on session creation and session refresh
@@ -49,6 +50,7 @@ struct NET_EXPORT SessionParams final {
     std::string attributes;
   };
 
+  SessionParams();
   SessionParams(std::string id,
                 GURL fetcher_url,
                 std::string refresh_url,
@@ -71,6 +73,7 @@ struct NET_EXPORT SessionParams final {
   unexportable_keys::UnexportableKeyId key_id;
   std::vector<std::string> allowed_refresh_initiators;
 };
+// LINT.ThenChange(//services/network/public/mojom/device_bound_sessions.mojom:DeviceBoundSessionParams)
 
 // Struct to contain the parameters from the .well-known JSON.
 struct NET_EXPORT WellKnownParams {
