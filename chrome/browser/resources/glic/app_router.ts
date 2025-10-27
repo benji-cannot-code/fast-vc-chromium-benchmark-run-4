fa2920f6d7e2fd131e331ad632e488414e966f62
Michelle Abreo <michelleabreo@google.com>
2025-10-27 09:33:11 -0700

    Set PanelStateKind when transitioning from FRE to GLIC.
    This ensures the correct loading screen (side panel vs. floating) is
    consistently displayed when transitioning from FRE to GLIC. The cached
    state is now applied to the GlicAppController immediately upon creation,
    resolving the issue where the default loader was sometimes shown.
    
    screencast
    before:https://screencast.googleplex.com/cast/NTM5ODQ1MTkyNjg1OTc3Nnw1M2E2N2Y0ZC04NA
    screencast
    after:https://screencast.googleplex.com/cast/NTkzNTQxMjExMDQyNjExMnxjOWZkZDkzZS1jOA
    
    Change-Id: I6cb06db0303e18620d823709509c37fc047fc53b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7084742
    Commit-Queue: Nick Birnie <birnie@google.com>
    Auto-Submit: Michelle Abreo <michelleabreo@google.com>
    Reviewed-by: Nick Birnie <birnie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1536027}

--- chrome/browser/resources/glic/app_router.ts
+++ chrome/browser/resources/glic/app_router.ts
@@ -28,6 +28,7 @@ export class AppRouter implements PageInterface {
   private freContainer: HTMLElement;
   private browserProxy: BrowserProxyImpl;
   private currentView: AppView|undefined;
+  private currentPanelStateKind: PanelStateKind|undefined;
 
   constructor() {
     this.glicContainer = getRequiredElement('glic-app-container');
@@ -54,6 +55,9 @@ export class AppRouter implements PageInterface {
       case AppView.GLIC:
         if (!this.glicController) {
           this.glicController = new GlicAppController(this.browserProxy);
+          if (this.currentPanelStateKind !== undefined) {
+            this.glicController.updatePageState(this.currentPanelStateKind);
+          }
           this.freAppController?.destroyWebview();
           this.freAppController = undefined;
         }
@@ -89,6 +93,7 @@ export class AppRouter implements PageInterface {
   }
 
   updatePageState(panelStateKind: PanelStateKind) {
+    this.currentPanelStateKind = panelStateKind;
     this.glicController?.updatePageState(panelStateKind);
   }
 
