4fa86c289886279c9d8b40e5fd9d35e312e4bdd4
Joshua Hood <jdh@chromium.org>
2025-10-27 07:32:49 -0700

    Revert "[iOS] Implement GTS New Tab Group button"
    This reverts commit d44d6c1b520e90a04870e428d90b7b8119ed0512.
    
    Reason for revert: Multiple "ios_chrome_ui_eg2tests_module iPad Pro ... (retry shards) on Mac-15" steps  failing on builder "chromium/ci/ios-simulator-noncq"
    
    List of failed builders:
    
    Builder: ios-simulator-noncq
    https://ci.chromium.org/p/chromium/builders/ci/ios-simulator-noncq
    First failing build:
    https://ci.chromium.org/p/chromium/builders/ci/ios-simulator-noncq/b8700085729149191969
    
    Original change's description:
    > [iOS] Implement GTS New Tab Group button
    >
    > This change adds a "New Tab Group" button to the Tab Group recall
    > surface. Pressing the button will create a new tab group that is
    > populated with a NTP.
    >
    > Tab Groups Page with new button:
    > https://screenshot.googleplex.com/AZCCZNB98dRZMat.png
    >
    > New Tab Group creation view:
    > https://screenshot.googleplex.com/5gP9g3r89s6ENVx.png
    >
    > Fixed: 448100252
    > Change-Id: I74d05aa958afa5900e28bffdb81c29817c5ff656
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7063635
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Commit-Queue: Briana McClure <bmcclure@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1535382}
    
    Bug: 454745272
    Change-Id: Ib88ee6741ec408054bb92e29ffe6e1f4aaa3833b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7084450
    Owners-Override: Joshua Hood <jdh@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Joshua Hood <jdh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1535931}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/create_tab_group_coordinator.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/create_tab_group_coordinator.mm
@@ -42,8 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   raw_ptr<const TabGroup> _tabGroup;
   // Transition delegate for the animation to show/hide.
   CreateTabGroupTransitionDelegate* _transitionDelegate;
-  // Whether a new tab should be inserted into the new group.
-  BOOL _createNewTabForGroup;
 }
 
 #pragma mark - Public
@@ -59,23 +57,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (self) {
     _identifiers = identifiers;
     _animatedDismissal = YES;
-  }
-  return self;
-}
-
-- (instancetype)
-    initTabGroupCreationWithBaseViewController:(UIViewController*)viewController
-                                       browser:(Browser*)browser
-                                  selectedTabs:
-                                      (const std::set<web::WebStateID>&)
-                                          identifiers
-                          createNewTabForGroup:(BOOL)createNewTabForGroup {
-  CHECK(identifiers.empty())
-      << "A new tab will be insterted into the tab group.";
-  self = [super initWithBaseViewController:viewController browser:browser];
-  if (self) {
-    _animatedDismissal = YES;
-    _createNewTabForGroup = createNewTabForGroup;
   }
   return self;
 }
@@ -118,10 +99,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   BOOL tabSynced =
       syncService && syncService->GetUserSettings()->GetSelectedTypes().Has(
                          syncer::UserSelectableType::kTabs);
-  _viewController = [[CreateTabGroupViewController alloc]
-          initWithEditMode:editMode
-                 tabSynced:tabSynced
-      createNewTabForGroup:_createNewTabForGroup];
+  _viewController =
+      [[CreateTabGroupViewController alloc] initWithEditMode:editMode
+                                                   tabSynced:tabSynced];
 
   FaviconLoader* faviconLoader = nil;
   collaboration::CollaborationService* collaborationService =
@@ -140,13 +120,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                                 browser:browser
                           faviconLoader:faviconLoader];
     _mediator.delegate = self;
-  } else if (_createNewTabForGroup) {
-    _mediator = [[CreateTabGroupMediator alloc]
-        initTabGroupCreationWithConsumer:_viewController
-                            selectedTabs:_identifiers
-                                 browser:browser
-                           faviconLoader:faviconLoader
-                    createNewTabForGroup:_createNewTabForGroup];
   } else {
     _mediator = [[CreateTabGroupMediator alloc]
         initTabGroupCreationWithConsumer:_viewController
@@ -154,7 +127,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                                  browser:browser
                            faviconLoader:faviconLoader];
   }
-
   _viewController.mutator = _mediator;
   _viewController.delegate = self;
 
