54662a23daefcfa7d4ff81a51af48525f771bf39
Colin Blundell <blundell@chromium.org>
2025-10-27 04:02:58 -0700

    [//gpu] Remove caching from SIFactory::IsNativeBufferSupported()
    This caching is an artifact of when this information was sent over to
    the browser process. Removing it will allow for making this method
    static and removing the other duplicate implementations by having those
    callsites call this.
    
    Bug: 404905709
    Change-Id: Icae10fe73b9e161be0fd7a76b6680382b50b87c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7079131
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1535850}

--- gpu/ipc/common/gpu_memory_buffer_support.h
+++ gpu/ipc/common/gpu_memory_buffer_support.h
@@ -62,7 +62,6 @@ class GPU_IPC_COMMON_EXPORT GpuMemoryBufferSupport {
     return IsNativeGpuMemoryBufferConfigurationSupported(format, usage);
   }
 
- private:
   // Returns whether the provided buffer format is supported.
   static bool IsNativeGpuMemoryBufferConfigurationSupported(
       viz::SharedImageFormat format,
