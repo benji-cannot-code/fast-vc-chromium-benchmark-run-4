a32f3a285eb98aa2ab6a6c77b04f12445244af04
Andrew Grieve <agrieve@chromium.org>
2025-10-27 14:17:45 -0700

    Android: Remove Robolectric shadow for ShareSheetPropertyModelBuilder
    Bug: 341267427
    Change-Id: Icbf3edee1f204181b1c5a8b989b35b71a4471497
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7088328
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Owners-Override: Henrique Nakashima <hnakashima@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1536232}

--- chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/share_sheet/ShareSheetPropertyModelBuilder.java
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/share_sheet/ShareSheetPropertyModelBuilder.java
@@ -13,6 +13,7 @@ import android.graphics.drawable.Drawable;
 import android.view.View.OnClickListener;
 
 import org.chromium.base.ContextUtils;
+import org.chromium.base.ResettersForTesting;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
@@ -69,6 +70,8 @@ class ShareSheetPropertyModelBuilder {
     private final PackageManager mPackageManager;
     private final Profile mProfile;
 
+    private static @Nullable List<PropertyModel> sSelectThirdPartyAppsOverrideForTesting;
+
     ShareSheetPropertyModelBuilder(
             BottomSheetController bottomSheetController,
             PackageManager packageManager,
@@ -107,6 +110,11 @@ class ShareSheetPropertyModelBuilder {
                 /* showNewBadge= */ false);
     }
 
+    public static void setSelectThirdPartyAppsOverrideForTesting(List<PropertyModel> value) {
+        sSelectThirdPartyAppsOverrideForTesting = value;
+        ResettersForTesting.register(() -> sSelectThirdPartyAppsOverrideForTesting = null);
+    }
+
     protected List<PropertyModel> selectThirdPartyApps(
             ShareSheetBottomSheetContent bottomSheet,
             Set<Integer> contentTypes,
@@ -115,6 +123,10 @@ class ShareSheetPropertyModelBuilder {
             long shareStartTime,
             @LinkGeneration int linkGenerationStatusForMetrics,
             LinkToggleMetricsDetails linkToggleMetricsDetails) {
+        if (sSelectThirdPartyAppsOverrideForTesting != null) {
+            return sSelectThirdPartyAppsOverrideForTesting;
+        }
+
         List<String> thirdPartyActivityNames = FALLBACK_ACTIVITIES;
         List<ResolveInfo> resolveInfoList =
                 getCompatibleApps(contentTypes, params.getFileContentType());
