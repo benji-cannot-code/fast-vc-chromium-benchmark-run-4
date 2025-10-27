fadc93ccf264796f8595ac8350e92bbcb75b0aad
Charlotte Cloutier <cloutierc@google.com>
2025-10-27 12:13:01 -0700

    [iOS] Add tests for persist tab context files
    Bug: 447646545
    Change-Id: I4fdaef15dc199e6e779979d96c8f4d8f8aded5e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7018210
    Commit-Queue: Charlotte Cloutier <cloutierc@google.com>
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1536154}

--- ios/chrome/browser/intelligence/persist_tab_context/model/persist_tab_context_browser_agent.mm
+++ ios/chrome/browser/intelligence/persist_tab_context/model/persist_tab_context_browser_agent.mm
@@ -28,8 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
 #import "ios/web/public/web_state.h"
 
-// TODO(crbug.com/447646545): Add test coverage for persist tab context
-
 namespace {
 
 // Prefix used for filenames of persisted page context files.
