2a2cc7fa7a05ea18c09b910a30de2e7cd088a4dd
Matt Jones <mdjones@chromium.org>
2025-10-27 16:42:36 -0700

    [Contextual Tasks] Create UI service for incognito and guest
    Since the ContextualTasksContextController now supports guest and
    incognito profiles (https://crrev.com/c/7081083), we can now support
    them from the UI service as well. This patch updates the factory to
    create the ContextualTasksUiService for guest and incognito profiles.
    
    This also addresses a crash when the feature flag is enabled and the
    user attempts to open an incognito tab. The change to IsAiUrl is to
    address what is most likely a red herring crash stack caused by the
    same flow.
    
    Bug: 452127302, 455108064
    Change-Id: Ifdb877aeada0d75d239e1dbfb04097d7836d3e55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7087460
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Mickey Burks <mickeyburks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1536323}

--- chrome/browser/contextual_tasks/contextual_tasks_ui_service_factory.cc
+++ chrome/browser/contextual_tasks/contextual_tasks_ui_service_factory.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/contextual_tasks/contextual_tasks_context_controller_factory.h"
 #include "chrome/browser/contextual_tasks/contextual_tasks_ui_service.h"
 #include "chrome/browser/profiles/profile.h"
+#include "components/contextual_tasks/public/features.h"
 
 namespace contextual_tasks {
 
@@ -33,12 +34,12 @@ ContextualTasksUiServiceFactory::GetForBrowserContextIfExists(
       GetInstance()->GetServiceForBrowserContext(context, false));
 }
 
-// TODO(452127302): Support guest profiles when our dependencies do.
 ContextualTasksUiServiceFactory::ContextualTasksUiServiceFactory()
     : ProfileKeyedServiceFactory(
           "ContextualTasksUiService",
           ProfileSelections::Builder()
-              .WithRegular(ProfileSelection::kOriginalOnly)
+              .WithRegular(ProfileSelection::kOwnInstance)
+              .WithGuest(ProfileSelection::kOwnInstance)
               .Build()) {
   DependsOn(ContextualTasksContextControllerFactory::GetInstance());
 }
@@ -46,6 +47,10 @@ ContextualTasksUiServiceFactory::ContextualTasksUiServiceFactory()
 std::unique_ptr<KeyedService>
 ContextualTasksUiServiceFactory::BuildServiceInstanceForBrowserContext(
     content::BrowserContext* context) const {
+  if (!base::FeatureList::IsEnabled(kContextualTasks)) {
+    return nullptr;
+  }
+
   Profile* profile = Profile::FromBrowserContext(context);
   return std::make_unique<ContextualTasksUiService>(
       ContextualTasksContextControllerFactory::GetForProfile(profile));
