77b9d150537a8f874f3dd72930c7f876fb8a664c
Michelle Abreo <michelleabreo@google.com>
2025-10-27 10:58:27 -0700

    [HandoffButton] Fix crash on close by separating callbacks
    Prevents a crash in HandoffButtonController where the widget could be
    destroyed multiple times. This happened because the
    `MakeCloseSynchronous` callback (`CloseButton`) was also responsible for
    initiating the widget destruction (`widget_->CloseNow()` and
    `widget_.reset()`).
    
    The fix uses a new `OnWidgetDestroying` method as the synchronous
    callback, safely handling pointer cleanup without re-triggering
    destruction. `CloseButton` is now only used to start the close process.
    
    Bug: b:451646616
    Change-Id: I5c1ed91e41497982daa8e80b3b520c060e0a2554
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7079227
    Reviewed-by: Abe Boujane <boujane@google.com>
    Commit-Queue: Michelle Abreo <michelleabreo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1536091}

--- chrome/browser/actor/ui/handoff_button_controller.h
+++ chrome/browser/actor/ui/handoff_button_controller.h
@@ -86,6 +86,7 @@ class HandoffButtonController {
   virtual ActorUiTabControllerInterface* GetTabController();
   virtual void UpdateBounds();
   virtual void UpdateVisibility();
+  void OnWidgetDestroying(views::Widget::ClosedReason reason);
 
   tabs::TabDialogManager* GetTabDialogManager();
 
