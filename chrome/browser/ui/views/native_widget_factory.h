5b92a22f7fc99ca9ed4dd31992ba9b6ba4cacb5d
jongmok.kim <jongmok.kim@navercorp.com>
2025-10-27 06:26:22 -0700

    Use more standard enumerator names in native widget
    Bug: 40281257
    Change-Id: Ia37931e041c990f42a77d24397d9036395f8cf3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7081813
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Jongmok Kim <jongmok.kim@navercorp.com>
    Cr-Commit-Position: refs/heads/main@{#1535901}

--- chrome/browser/ui/views/native_widget_factory.h
+++ chrome/browser/ui/views/native_widget_factory.h
@@ -9,8 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/widget/widget.h"
 
 enum class NativeWidgetType {
-  NATIVE_WIDGET_AURA,
-  DESKTOP_NATIVE_WIDGET_AURA,
+  kNativeWidgetAura,
+  kDesktopNativeWidgetAura,
 };
 
 // Responsible for creating and configuring a NativeWidget type.
