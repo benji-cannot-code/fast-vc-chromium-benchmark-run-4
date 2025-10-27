7d7d41aad0764f0e2068fd20fa2f7189c4ff4a98
Kent Tamura <tkent@chromium.org>
2025-10-26 23:59:08 -0700

    CanvasTextNG: Call DidSwitchFrame() in FinalizeFrame()
    DidSwitchFrame() of PlainTextPainter and UniqueFontSelector should be
    called in OffscreenCanvasRenderingContext2D::FinalizeFrame() because
    it's the common path on exposing an image of an OffscreenCanvas.
    
    * Add CanvasRenderingContextHost::NotifyCachesOfSwitchingFrame() to
      simplify the code calling DidSwitchFrame().
    
    Bug: 450531794
    Change-Id: I57d997bf33652be3581a3107ee10f00d4ca8f891
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7060791
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1535759}

--- third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.cc
+++ third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.cc
@@ -229,11 +229,8 @@ ImageBitmap* OffscreenCanvas::transferToImageBitmap(
                                       "ImageBitmap construction failed");
   }
 
-  if (plain_text_painter_ != nullptr) {
-    plain_text_painter_->DidSwitchFrame();
-  }
-  if (unique_font_selector_) {
-    unique_font_selector_->DidSwitchFrame();
+  if (!RuntimeEnabledFeatures::CanvasTextSwitchFrameOnFinalizeEnabled()) {
+    NotifyCachesOfSwitchingFrame();
   }
   return image;
 }
@@ -283,13 +280,9 @@ scoped_refptr<Image> OffscreenCanvas::GetSourceImageForCanvas(
   }
   *status = image ? kNormalSourceImageStatus : kInvalidSourceImageStatus;
 
-  if (RuntimeEnabledFeatures::CanvasTextTexImage2DFixEnabled()) {
-    if (plain_text_painter_) {
-      plain_text_painter_->DidSwitchFrame();
-    }
-    if (unique_font_selector_) {
-      unique_font_selector_->DidSwitchFrame();
-    }
+  if (RuntimeEnabledFeatures::CanvasTextTexImage2DFixEnabled() &&
+      !RuntimeEnabledFeatures::CanvasTextSwitchFrameOnFinalizeEnabled()) {
+    NotifyCachesOfSwitchingFrame();
   }
   return image;
 }
@@ -563,11 +556,8 @@ bool OffscreenCanvas::PushFrame(scoped_refptr<CanvasResource>&& canvas_resource,
       std::move(canvas_resource), current_frame_damage_rect_, IsOpaque());
   current_frame_damage_rect_ = SkIRect::MakeEmpty();
 
-  if (plain_text_painter_ != nullptr) {
-    plain_text_painter_->DidSwitchFrame();
-  }
-  if (unique_font_selector_) {
-    unique_font_selector_->DidSwitchFrame();
+  if (!RuntimeEnabledFeatures::CanvasTextSwitchFrameOnFinalizeEnabled()) {
+    NotifyCachesOfSwitchingFrame();
   }
   return true;
 }
--- third_party/blink/renderer/modules/canvas/offscreencanvas2d/offscreen_canvas_rendering_context_2d.cc
+++ third_party/blink/renderer/modules/canvas/offscreencanvas2d/offscreen_canvas_rendering_context_2d.cc
@@ -138,6 +138,9 @@ void OffscreenCanvasRenderingContext2D::FinalizeFrame(FlushReason reason) {
     return;
   }
   resource_provider_->FlushCanvas(reason);
+  if (RuntimeEnabledFeatures::CanvasTextSwitchFrameOnFinalizeEnabled()) {
+    Host()->NotifyCachesOfSwitchingFrame();
+  }
 }
 
 // BaseRenderingContext2D implementation
