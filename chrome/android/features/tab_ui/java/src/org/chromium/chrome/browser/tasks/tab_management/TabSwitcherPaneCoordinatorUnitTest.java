f61a8e90ef9796473016e4a021295567bacb79f3
Hitarth Kothari <hitarthkothari@google.com>
2025-10-27 11:23:00 -0700

    [Pinned Tabs] fix blincking effect caused by pinned tabs strip
    Demo: http://screencast/cast/NjM4Mzk4NTEzODU5Nzg4OHxhOGQ2NDBlYS05Nw
    
    Bug: 449670068
    Change-Id: Ibe10f95f918733bc5d54396be7cedb7d12e9472e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7068374
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Hitarth Kothari <hitarthkothari@google.com>
    Reviewed-by: Madhav Pruthi <madhavpruthi@google.com>
    Commit-Queue: Hitarth Kothari <hitarthkothari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1536109}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherPaneCoordinatorUnitTest.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherPaneCoordinatorUnitTest.java
@@ -33,7 +33,6 @@ import android.graphics.Bitmap;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
-import android.widget.LinearLayout;
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
@@ -564,7 +563,7 @@ public class TabSwitcherPaneCoordinatorUnitTest {
 
         // Verify that the container is a LinearLayout.
         ViewGroup container = (ViewGroup) mContainerView.getChildAt(0);
-        assertTrue(container instanceof LinearLayout);
+        assertTrue(container instanceof FrameLayout);
         // Verify the children of the LinearLayout.
         assertEquals(2, container.getChildCount());
         FrameLayout pinnedTabsContainer = container.findViewById(R.id.pinned_tabs_container);
@@ -582,7 +581,7 @@ public class TabSwitcherPaneCoordinatorUnitTest {
 
         // Verify that the container is a LinearLayout with the original TabListRecyclerView.
         ViewGroup container = (ViewGroup) mContainerView.getChildAt(0);
-        assertTrue(container instanceof LinearLayout);
+        assertTrue(container instanceof FrameLayout);
         FrameLayout pinnedTabsContainer = container.findViewById(R.id.pinned_tabs_container);
         FrameLayout tabListContainer = container.findViewById(R.id.tab_list_container);
         assertEquals(0, pinnedTabsContainer.getChildCount());
