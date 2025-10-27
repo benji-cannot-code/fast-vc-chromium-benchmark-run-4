5d71708f5e1630fafa61664602ec0495d87d1ba9
Tiffany Shen <tishen@google.com>
2025-10-27 15:08:11 -0700

    [Save to Drive] Gate survey behind PdfSaveToDriveSurvey flag
    To limit the survey's launch to specific countries and categorize by
    consumer and enterprise, the survey needs to be gated behind a new flag.
    Note: in the demo with surveys enabled, a different trigger ID is used
    to show that the surveys are separate.
    
    Demo with surveys enabled:
    http://screencast/cast/NDgzNTg3Mjc2NjY4OTI4MHwyYjJhOWMyMS0xOQ, ran using
    `out/Default/chrome
    --enable-features="PdfSaveToDrive,HappinessTrackingSurveysForDesktopDemo:auto_prompt/false,PdfSaveToDriveSurvey:en_site_id/etKhHztBR0ugnJ3q1cK0TKzkyTyw/probability/1.0/consumer-trigger-id/QA4ECrqpN0ugnJ3q1cK0U1jrufAq/enterprise-trigger-id/etKhHztBR0ugnJ3q1cK0TKzkyTyw"`
    Demo with surveys disabled:
    http://screencast/cast/NjYwMjA4OTA4MDgxNTYxNnw2MzFiZDE3Ni03OQ, ran using
    `out/Default/chrome --enable-features=PdfSaveToDrive
    --disable-features=PdfSaveToDriveSurvey`
    
    Bug: 454388176
    Change-Id: Id551552d424c0906254ebb40b911049901323287
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7085492
    Commit-Queue: Tiffany Shen <tishen@google.com>
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Auto-Submit: Tiffany Shen <tishen@google.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1536267}

--- chrome/browser/save_to_drive/save_to_drive_flow.cc
+++ chrome/browser/save_to_drive/save_to_drive_flow.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
 #include "extensions/browser/guest_view/mime_handler_view/mime_handler_view_guest.h"
+#include "pdf/pdf_features.h"
 
 namespace save_to_drive {
 namespace {
@@ -193,14 +194,24 @@ void SaveToDriveFlow::ShowHatsSurveyWithDelay() {
       {"Resumable upload",
        drive_uploader_ && drive_uploader_->get_drive_uploader_type() ==
                               DriveUploaderType::kResumable}};
-  hats_service_->LaunchDelayedSurvey(kHatsSurveyTriggerPdfSaveToDrive,
+  hats_service_->LaunchDelayedSurvey(GetHatsSurveyTriggerId(),
                                      kHatsSurveyTimeout.InMilliseconds(),
                                      std::move(product_specific_bits_data));
 }
 
+std::string SaveToDriveFlow::GetHatsSurveyTriggerId() {
+  if (save_to_drive_account_info_ && save_to_drive_account_info_->is_managed) {
+    return kHatsSurveyEnterpriseTriggerPdfSaveToDrive;
+  }
+  return kHatsSurveyConsumerTriggerPdfSaveToDrive;
+}
+
 void SaveToDriveFlow::Stop() {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  ShowHatsSurveyWithDelay();
+  if (base::FeatureList::IsEnabled(
+          chrome_pdf::features::kPdfSaveToDriveSurvey)) {
+    ShowHatsSurveyWithDelay();
+  }
   DeleteForCurrentDocument(&render_frame_host());
   // Don't do anything else here. The flow will be destroyed after this line.
 }
