20b874e707a6839cce5091ae733e5d38f29c0611
Hannah Collins <hannahrogers@google.com>
2025-10-27 13:44:07 -0700

    [ntp-realbox] Add the recent tab chip to realbox next variants
    Also, adds ShowComposeboxTypedSuggest to the Next Single Context
    variant, as it was missed initially due to a merge conflict.
    
    Change-Id: I7b67352aecc91f524a3641c52eddad3e9125c985
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7087307
    Commit-Queue: Hannah Collins <hannahrogers@google.com>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1536215}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1643,15 +1643,18 @@ const FeatureEntry::FeatureParam kComposeboxNext[] = {
     {"ShowToolsAndModels", "true"},
     {"ShowCreateImageTool", "true"},
     {"ForceToolsAndModels", "true"},
+    {"ShowRecentTabChip", "true"},
 };
 const FeatureEntry::FeatureParam kComposeboxNextSingleContext[] = {
     {"MaxNumFiles", "1"},
     {"ShowContextMenu", "true"},
     {"ShowContextMenuTabPreviews", "true"},
     {"ShowComposeboxZps", "true"},
+    {"ShowComposeboxTypedSuggest", "true"},
     {"ShowToolsAndModels", "true"},
     {"ShowCreateImageTool", "true"},
     {"ForceToolsAndModels", "true"},
+    {"ShowRecentTabChip", "true"},
 };
 
 const FeatureEntry::FeatureVariation kNtpComposeboxVariations[] = {
