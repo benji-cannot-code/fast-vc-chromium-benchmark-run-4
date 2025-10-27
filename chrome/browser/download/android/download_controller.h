15751e6dffdbe79c0a09f66dce7ed59e9283d0c9
Min Qin <qinmin@chromium.org>
2025-10-27 12:16:20 -0700

    show warning dialog if the download is marked as sensitive
    Change-Id: Ic97970fccbe50dc7e65456aef032ce1673ef6f52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7076999
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1536156}

--- chrome/browser/download/android/download_controller.h
+++ chrome/browser/download/android/download_controller.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/download/android/dangerous_download_dialog_bridge.h"
 #include "chrome/browser/download/android/download_callback_validator.h"
 #include "chrome/browser/download/android/download_controller_base.h"
+#include "chrome/browser/download/android/policy_warning_download_dialog_bridge.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_key.h"
 #include "components/safe_browsing/android/safe_browsing_api_handler_bridge.h"
@@ -93,6 +94,10 @@ class DownloadController : public DownloadControllerBase {
   // Shows the DangerousDownloadDialog (generic dangerous filetype warning).
   void OnDangerousDownload(download::DownloadItem* item);
 
+  // The download item contains sensitive content.
+  // Shows the PolicyWarningDownloadDialog.
+  void OnSensitiveDownload(download::DownloadItem* item);
+
   // Shows the UI warnings from Safe Browsing malicious APK download check.
   void ShowDangerousDownloadWarning(DownloadUIModel& model);
 
@@ -118,6 +123,9 @@ class DownloadController : public DownloadControllerBase {
   DownloadCallbackValidator validator_;
 
   std::unique_ptr<DangerousDownloadDialogBridge> dangerous_download_bridge_;
+
+  std::unique_ptr<PolicyWarningDownloadDialogBridge>
+      policy_warning_download_bridge_;
 
   // The item currently or previously doing an app verification
   // prompt. Because we show at most one at a time, this does not need
