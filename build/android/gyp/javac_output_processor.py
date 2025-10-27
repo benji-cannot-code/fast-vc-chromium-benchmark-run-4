b48a5785685fd1a275f03d89ade49c6e8ce88501
Andrew Grieve <agrieve@chromium.org>
2025-10-27 10:17:29 -0700

    javac_output_processor.py: Fix crash when turbine .srcjar has a warning
    Turbine supports .srcjar files directly, and the path it emits for
    warnings is the path within .srcjar.
    
    Change-Id: Idbfc9bd9953b289754a229897364ae76839f8e51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7087008
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1536066}

--- build/android/gyp/javac_output_processor.py
+++ build/android/gyp/javac_output_processor.py
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import functools
 import os
 import pathlib
+import posixpath
 import re
 import shlex
 import sys
@@ -124,9 +125,15 @@ class JavacOutputProcessor:
         if short_class_name in self._missing_classes_short:
           continue
         path = m.group('path')
-        package_name = _extract_package_name(path)
-        if not package_name:
-          continue
+        if os.path.exists(path):
+          package_name = _extract_package_name(path)
+          if not package_name:
+            continue
+        else:
+          # This can happen for .srcjars in turbine.py (which directly supports
+          # .srcjar files).
+          # E.g.: "org/chromium/network/mojom/ParsedHeaders.java"
+          package_name = posixpath.dirname(path).replace('/', '.')
         full_class_name = f'{package_name}.{short_class_name}'
 
       if full_class_name not in self._missing_classes_full:
