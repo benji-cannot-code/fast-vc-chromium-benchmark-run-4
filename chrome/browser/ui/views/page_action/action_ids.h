beeb24874c8db2122f68e0c37bb9d111422df99e
Khalid Peer <khalidpeer@chromium.org>
2025-10-27 10:29:59 -0700

    [omnibox] Add DCHECKs to validate proper AIM page action placement.
    As a follow-up to crrev.com/c/7083849, this CL adds some DCHECKs in
    order to ensure that the AIM page action is always positioned as the
    left-most page action in the location bar (relative to other page
    actions).
    
    Bug: b:448720377
    Change-Id: I581802350d0476ef8ed7a194a8d9da6839c4a2a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7082429
    Auto-Submit: Khalid Peer <khalidpeer@chromium.org>
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1536075}

--- chrome/browser/ui/views/page_action/action_ids.h
+++ chrome/browser/ui/views/page_action/action_ids.h
@@ -17,6 +17,7 @@ namespace page_actions {
 // For now, the order of the page actions will be based on their position in
 // the array.
 inline constexpr std::array<actions::ActionId, 17> kActionIds = {
+    kActionAiMode,
     kActionSidePanelShowLensOverlayResults,
     kActionShowTranslate,
     kActionShowMemorySaverChip,
@@ -31,10 +32,13 @@ inline constexpr std::array<actions::ActionId, 17> kActionIds = {
     kActionShowCollaborationRecentActivity,
     kActionAutofillMandatoryReauth,
     kActionFind,
-    kActionAiMode,
     kActionShowCookieControls,
     kActionShowAddressesBubbleOrPage,
 };
+
+static_assert(kActionIds[0] == kActionAiMode,
+              "kActionAiMode must be the first entry in kActionIds to ensure "
+              "it's the left-most page action");
 
 }  // namespace page_actions
 
