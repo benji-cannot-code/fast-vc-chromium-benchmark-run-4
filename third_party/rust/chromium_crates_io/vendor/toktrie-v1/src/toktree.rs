62871eb20adf2024da05d106ec0cc83713840da7
Manish Goregaokar <manishearth@google.com>
2025-10-27 09:04:04 -0700

    Roll toktrie: 1.2.0 => 1.3.0 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * toktrie: 1.2.0 => 1.3.0; https://docs.rs/crate/toktrie/1.3.0
    
    Bug: None
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Change-Id: I7a4aa0bfbe9292ccc5c4267e3cc5aea01a276d51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7086727
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Auto-Submit: Manish Goregaokar <manishearth@google.com>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1536000}

--- third_party/rust/chromium_crates_io/vendor/toktrie-v1/src/toktree.rs
+++ third_party/rust/chromium_crates_io/vendor/toktrie-v1/src/toktree.rs
@@ -730,7 +730,7 @@ impl TokTrie {
         r
     }
 
-    pub fn node_children(&self, n: &TrieNode) -> NodeChildren {
+    pub fn node_children(&self, n: &TrieNode) -> NodeChildren<'_> {
         let off = self.node_offset(n);
         NodeChildren {
             trie: self,
