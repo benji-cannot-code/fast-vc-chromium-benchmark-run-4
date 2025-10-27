2a570982325d533ba5a76e236da5768eb968fe74
Torne (Richard Coles) <torne@google.com>
2025-10-27 10:07:06 -0700

    webview: disable origin trials component fetching.
    Disable fetching the origin trials component in the embedded WebView, by
    putting it behind a new disabled-by-default feature. This gives us the
    option to re-enable it via Finch if necessary, for now.
    
    This does not disable origin trials themselves, only fetching the
    component used to update the origin trial configuration without a binary
    update; origin trials will behave according to their binary defaults.
    
    This CL does not affect the registration of the origin trials component
    in the nonembedded service, as that cannot be guarded by Finch. However,
    since this was the only remaining enabled component configured in
    WebView, the embedded WebView will no longer connect to
    ComponentsProviderService during startup at all, so we won't schedule
    any more instances of the nonembedded component updater service job.
    
    We're attempting to turn down the component fetching logic in WebView,
    so this also adds a check to ensure that no other components are
    registered (unless the origin trials component has been re-enabled), to
    prevent any accidental re-enabling before it's removed entirely.
    
    Bug: 438310407
    Change-Id: I4167cbfb0a9f60a3ba6bd5f457c81c9e54f8f707
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7008232
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Richard (Torne) Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1536053}

--- android_webview/browser/component_updater/origin_trials_component_loader.cc
+++ android_webview/browser/component_updater/origin_trials_component_loader.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "android_webview/browser/component_updater/loader_policies/origin_trials_component_loader_policy.h"
+#include "android_webview/common/aw_features.h"
+#include "base/feature_list.h"
 #include "components/component_updater/android/component_loader_policy.h"
 #include "components/component_updater/android/component_loader_policy_forward.h"
 
@@ -15,6 +17,11 @@ namespace android_webview {
 
 void LoadOriginTrialsComponent(
     component_updater::ComponentLoaderPolicyVector& policies) {
+  if (!base::FeatureList::IsEnabled(
+          features::kWebViewFetchOriginTrialsComponent)) {
+    return;
+  }
+
   policies.push_back(std::make_unique<OriginTrialsComponentLoaderPolicy>());
 }
 
