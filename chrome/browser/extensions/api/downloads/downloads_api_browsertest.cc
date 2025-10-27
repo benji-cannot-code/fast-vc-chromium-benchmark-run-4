e22105b8a6f3e9294a339a8f318237a06bab6606
James Cook <jamescook@chromium.org>
2025-10-27 13:24:20 -0700

    extensions: Port more extensions API tests to desktop Android, part 6
    There have been some fixes to the underlying test framework that
    appear to have fixed some tests that were flaky. Enable them.
    
    Bug: 405219117
    Change-Id: Ia0a2cb964107f07413125a3258da1dc7b8053642
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7081138
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1536196}

--- chrome/browser/extensions/api/downloads/downloads_api_browsertest.cc
+++ chrome/browser/extensions/api/downloads/downloads_api_browsertest.cc
@@ -1421,9 +1421,7 @@ IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
   ASSERT_EQ(all_downloads[0]->GetId(), static_cast<uint32_t>(*item_id));
 }
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
 // Test the |id| parameter for search().
-// Flaky on desktop Android.
 IN_PROC_BROWSER_TEST_F(DownloadExtensionTest, DownloadExtensionTest_SearchId) {
   DownloadManager::DownloadVector items;
   CreateTwoDownloads(&items);
@@ -1443,7 +1441,6 @@ IN_PROC_BROWSER_TEST_F(DownloadExtensionTest, DownloadExtensionTest_SearchId) {
 }
 
 // Test specifying both the |id| and |filename| parameters for search().
-// Flaky on desktop Android.
 IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
                        DownloadExtensionTest_SearchIdAndFilename) {
   DownloadManager::DownloadVector items;
@@ -1457,7 +1454,6 @@ IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
   ASSERT_TRUE(result->is_list());
   ASSERT_EQ(0UL, result->GetList().size());
 }
-#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
 // Test a single |orderBy| parameter for search().
 IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
@@ -1541,9 +1537,7 @@ IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
   ASSERT_EQ(1UL, result->GetList().size());
 }
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
 // Test the |state| option for search().
-// Flaky on desktop Android.
 IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
                        DownloadExtensionTest_SearchState) {
   DownloadManager::DownloadVector items;
@@ -1561,7 +1555,6 @@ IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
 }
 
 // Test the |limit| option for search().
-// Flaky on desktop Android.
 IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
                        DownloadExtensionTest_SearchLimit) {
   DownloadManager::DownloadVector items;
@@ -1574,7 +1567,6 @@ IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
   ASSERT_TRUE(result->is_list());
   ASSERT_EQ(1UL, result->GetList().size());
 }
-#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
 // Test invalid search parameters.
 IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
