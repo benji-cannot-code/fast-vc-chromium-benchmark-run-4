e3b437c000c2de3531fb7130dfd0038d43c49ee1
Mason Freed <masonf@chromium.org>
2025-10-27 11:08:01 -0700

    Remove HTMLCommandActionToggleInterest feature
    This was a proposal related to interest invokers that wasn't
    adopted in the end. Remove supporting code and the flag.
    
    Bug: 326681249
    Change-Id: I96075a845ab8e9124ec6b2bbc142fc003821fff0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7064536
    Commit-Queue: David Baron <dbaron@chromium.org>
    Reviewed-by: David Baron <dbaron@chromium.org>
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1536099}

--- third_party/blink/renderer/core/dom/element.h
+++ third_party/blink/renderer/core/dom/element.h
@@ -236,8 +236,6 @@ enum class CommandEventType {
   // kClose
   // Input / Select
   kShowPicker,
-  // Interest invokers (`interestfor`)
-  kToggleInterest,
   // Number Input
   kStepUp,
   kStepDown,
