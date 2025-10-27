0888605335df1c91b82d3b09a4c037de8f2bd8f1
Kevin Ellis <kevers@google.com>
2025-10-27 11:31:31 -0700

    Scoped-VT anchor-positioning
    The layout object for a ::view-transition pseudo is now a sibling
    of the layout object for its originating element, and positioned
    using position-anchor.
    
    Bug: 446688001
    Change-Id: Ia8f496c262e7f790a4132ed4278ec634fcafc2a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7017769
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Commit-Queue: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1536120}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -4632,8 +4632,10 @@ bool Element::SkipStyleRecalcForContainer(
   // originating element's box and cannot be skipped if the originating element
   // is a size container because the pseudo-element and its box need to be
   // created before layout.
+  // The same applies to scoped view-transitions.
   if (!style.ScrollMarkerGroupNone() ||
       CanGeneratePseudoElement(kPseudoIdScrollButton) ||
+      CanGeneratePseudoElement(kPseudoIdViewTransition) ||
       HasSiblingBoxPseudoElements()) {
     return false;
   }
@@ -5606,16 +5608,25 @@ void Element::RebuildTransitionLayoutTree(
 }
 
 void Element::AttachTransitionPseudoElements(AttachContext& context) {
-  // For a document transition, the LayoutObject for the ::view-transition
-  // pseudo-element is wrapped by the anonymous LayoutViewTransitionRoot,
-  // which represents the snapshot containing block.
-  //
-  // The LayoutViewTransitionRoot is a child of the LayoutView, and will be
-  // injected by LayoutView::AddChild.
+  ViewTransition* transition = ViewTransitionUtils::GetTransition(*this);
+  if (!transition) {
+    return;
+  }
+
   // See LayoutTreeBuilderTraversal::ParentLayoutObject.
   AttachContext children_context(context);
   if (context.parent && context.parent->IsDocumentElement()) {
+    // For a document transition, the LayoutObject for the ::view-transition
+    // pseudo-element is wrapped by the anonymous LayoutViewTransitionRoot,
+    // which represents the snapshot containing block.
+    //
+    // The LayoutViewTransitionRoot is a child of the LayoutView, and will be
+    // injected by LayoutView::AddChild.
     children_context.parent = GetDocument().GetLayoutView();
+  } else if (GetLayoutObject() && transition->Scope() == this) {
+    // The layout object for the ::view-transition() pseudo is a sibling
+    // of the scoped elements layout object.
+    children_context.parent = GetLayoutObject()->Parent();
   }
 
   auto attach_pseudo = [&](PseudoElement* pseudo_element) {
@@ -9747,13 +9758,8 @@ PseudoElement* Element::CreatePseudoElementIfNeeded(
 
   PseudoElement* pseudo_element =
       PseudoElement::Create(this, pseudo_id, pseudo_argument);
-  if (RuntimeEnabledFeatures::ScopedViewTransitionsEnabled()) {
-    if (!pseudo_element) {
-      // TODO(crbug.com/405117185): Replace with DCHECK(pseudo_element) once we
-      // properly track per-scope view transition names.
-      return nullptr;
-    }
-  }
+  CHECK(pseudo_element);
+
   EnsureElementRareData().SetPseudoElement(pseudo_id, pseudo_element,
                                            pseudo_argument);
   pseudo_element->InsertedInto(*this);
@@ -10238,7 +10244,8 @@ bool Element::HasSiblingBoxPseudoElements() const {
   for (PseudoId pseudo_id :
        {kPseudoIdScrollButtonBlockStart, kPseudoIdScrollButtonInlineStart,
         kPseudoIdScrollButtonInlineEnd, kPseudoIdScrollButtonBlockEnd,
-        kPseudoIdScrollMarkerGroupAfter, kPseudoIdScrollMarkerGroupBefore}) {
+        kPseudoIdScrollMarkerGroupAfter, kPseudoIdScrollMarkerGroupBefore,
+        kPseudoIdViewTransition}) {
     if (rare_data->GetPseudoElement(pseudo_id)) {
       return true;
     }
@@ -11902,6 +11909,7 @@ void Element::UpdateTransitionPseudoElements(
     }
 
     bool had_transition_pseudo = !!GetPseudoElement(kPseudoIdViewTransition);
+
     PseudoElement* transition_pseudo =
         UpdatePseudoElement(kPseudoIdViewTransition, style_recalc_change,
                             style_recalc_context, g_null_atom);
@@ -12688,6 +12696,15 @@ Element* Element::ImplicitAnchorElement() const {
         }
         return pseudo_element->UltimateOriginatingElement()
             .ImplicitAnchorElement();
+
+      case kPseudoIdViewTransition: {
+        Element* parent = parentElement();
+        if (!parent->IsDocumentElement()) {
+          return parent;
+        }
+        break;
+      }
+
       default:
         return nullptr;
     }
--- third_party/blink/renderer/core/dom/pseudo_element.cc
+++ third_party/blink/renderer/core/dom/pseudo_element.cc
@@ -364,7 +364,8 @@ bool PseudoElement::IsLayoutSiblingOfOriginatingElement(
          pseudo_id == kPseudoIdScrollButton ||
          pseudo_id == kPseudoIdScrollMarkerGroup ||
          pseudo_id == kPseudoIdScrollMarkerGroupAfter ||
-         pseudo_id == kPseudoIdScrollMarkerGroupBefore;
+         pseudo_id == kPseudoIdScrollMarkerGroupBefore ||
+         pseudo_id == kPseudoIdViewTransition;
 }
 
 bool PseudoElement::IsLayoutSiblingOfOriginatingElement() const {
