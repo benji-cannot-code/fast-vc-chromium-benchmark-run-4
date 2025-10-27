31e60da0cf4df9f044818f0efef31ff9b2e51963
John Kim <johnykim@google.com>
2025-10-27 08:43:37 -0700

    [FPF-CI] Remove Canvas Noise Integration in Workers (1/4).
    As per https://privacysandbox.google.com/overview/status, Canvas
    Interventions will be part of the deprecation. This CL removes the
    Shared Worker user bypass integration from
    https://chromium-review.googlesource.com/c/chromium/src/+/7006915.
    
    Bug: 454083467
    Change-Id: Ib4efe8e2a2bc8a7714c4dafbd2d66026f37906c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7067041
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: John Kim <johnykim@google.com>
    Cr-Commit-Position: refs/heads/main@{#1535983}

--- components/fingerprinting_protection_filter/interventions/browser/canvas_interventions_web_contents_helper.cc
+++ components/fingerprinting_protection_filter/interventions/browser/canvas_interventions_web_contents_helper.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/service_worker_context.h"
-#include "content/public/browser/shared_worker_service.h"
 #include "content/public/browser/storage_partition.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_user_data.h"
@@ -65,11 +64,6 @@ void CanvasInterventionsWebContentsHelper::
       .GetBrowserContext()
       ->GetStoragePartition(GetWebContents().GetSiteInstance())
       ->GetServiceWorkerContext()
-      ->UpdateAllCanvasNoiseTokensFromTopLevelSite(first_party_url);
-  GetWebContents()
-      .GetBrowserContext()
-      ->GetStoragePartition(GetWebContents().GetSiteInstance())
-      ->GetSharedWorkerService()
       ->UpdateAllCanvasNoiseTokensFromTopLevelSite(first_party_url);
 }
 
--- content/renderer/worker/embedded_shared_worker_stub.cc
+++ content/renderer/worker/embedded_shared_worker_stub.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/messaging/message_port_descriptor.h"
 #include "third_party/blink/public/mojom/browser_interface_broker.mojom.h"
 #include "third_party/blink/public/mojom/devtools/devtools_agent.mojom.h"
-#include "third_party/blink/public/mojom/fingerprinting_protection/canvas_interventions.mojom.h"
 #include "third_party/blink/public/mojom/renderer_preference_watcher.mojom.h"
 #include "third_party/blink/public/mojom/service_worker/service_worker_object.mojom.h"
 #include "third_party/blink/public/mojom/worker/worker_content_settings_proxy.mojom.h"
@@ -69,8 +68,6 @@ EmbeddedSharedWorkerStub::EmbeddedSharedWorkerStub(
     mojo::PendingReceiver<blink::mojom::ReportingObserver>
         dip_reporting_observer,
     std::optional<blink::NoiseToken> canvas_noise_token,
-    mojo::PendingReceiver<blink::mojom::CanvasNoiseTokenUpdater>
-        canvas_noise_token_observer,
     const std::vector<std::string>& cors_exempt_header_list)
     : receiver_(this, std::move(receiver)) {
   DCHECK(main_script_load_params);
@@ -145,7 +142,7 @@ EmbeddedSharedWorkerStub::EmbeddedSharedWorkerStub(
       std::move(web_worker_fetch_context), std::move(host), this, ukm_source_id,
       require_cross_site_request_for_cookies,
       std::move(coep_reporting_observer), std::move(dip_reporting_observer),
-      std::move(canvas_noise_token), std::move(canvas_noise_token_observer));
+      std::move(canvas_noise_token));
 
   // If the host drops its connection, then self-destruct.
   receiver_.set_disconnect_handler(base::BindOnce(
