f5503b024715f8d87ccb91e4eee2aa7f7d25ab35
Steinar H. Gunderson <sesse@chromium.org>
2025-10-27 05:38:25 -0700

    Fix an #include in the wrong place.
    Change-Id: Iaddd998b5246a9f1391eb785f49e4776a88d29c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7080020
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1535880}

--- third_party/blink/renderer/core/css/rule_set.h
+++ third_party/blink/renderer/core/css/rule_set.h
@@ -21,13 +21,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *
  */
 
-#include "base/memory/stack_allocated.h"
-
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_CSS_RULE_SET_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_CSS_RULE_SET_H_
 
 #include "base/compiler_specific.h"
 #include "base/gtest_prod_util.h"
+#include "base/memory/stack_allocated.h"
 #include "base/substring_set_matcher/substring_set_matcher.h"
 #include "base/types/pass_key.h"
 #include "third_party/blink/renderer/core/core_export.h"
