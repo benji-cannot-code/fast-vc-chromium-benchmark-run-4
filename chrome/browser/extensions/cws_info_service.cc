6ad08cafdbed47ea8bccbd46b08977eb35558d9c
Olivier Li Shing Tat-Dupuis <olivierli@chromium.org>
2025-10-27 08:31:41 -0700

    Experiment with checking for CWS information at BEST_EFFORT priority
    `extensions::CWSInfoService::CheckAndMaybeFetchInfo()` is identified in
    Chrome profiles with low responsiveness. This CL experiments with
    running the timer that checks for information at best effort priority in
    in hopes that it runs at more appropriate moments especially if used in
    conjunction with EnableBestEffortTaskInhibitingPolicy.
    
    Bug: 424432184, 441949788
    Change-Id: I231277d1ee399d702e89ecff9c1a0b579145cbc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7081231
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Olivier Li <olivierli@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1535973}

--- chrome/browser/extensions/cws_info_service.cc
+++ chrome/browser/extensions/cws_info_service.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/contains.h"
 #include "base/containers/fixed_flat_map.h"
 #include "base/containers/queue.h"
+#include "base/features.h"
 #include "base/i18n/time_formatting.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
@@ -361,6 +362,11 @@ void CWSInfoService::CheckAndMaybeFetchInfo() {
 }
 
 void CWSInfoService::ScheduleCheck(int seconds) {
+  if (base::features::IsReducePPMsEnabled()) {
+    info_check_timer_.SetTaskRunner(
+        content::GetUIThreadTaskRunner({base::TaskPriority::BEST_EFFORT}));
+  }
+
   info_check_timer_.Start(FROM_HERE, base::Seconds(seconds), this,
                           &CWSInfoService::CheckAndMaybeFetchInfo);
 }
