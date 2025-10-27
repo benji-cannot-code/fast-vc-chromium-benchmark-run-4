5fa807058659e197fd79522268ba53ab84ca75a7
Sky Malice <skym@chromium.org>
2025-10-27 11:54:10 -0700

    Remove NonNull from EditTextWithLeading.
    Bug: 453667875
    Change-Id: I493dcb719fcba8d600760efb1882f407f81ff218
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7086980
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Auto-Submit: Sky Malice <skym@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1536144}

--- ui/android/java/src/org/chromium/ui/widget/EditTextWithLeading.java
+++ ui/android/java/src/org/chromium/ui/widget/EditTextWithLeading.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.ui.widget;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
 import android.util.AttributeSet;
 
 import androidx.appcompat.widget.AppCompatEditText;
@@ -27,7 +26,7 @@ public class EditTextWithLeading extends AppCompatEditText {
      * TextView - that is no leading will be applied. This method is also available for use from
      * subclasses besides inflation from XML.
      */
-    public EditTextWithLeading(@NonNull Context context, @Nullable AttributeSet attrs) {
+    public EditTextWithLeading(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         TextViewLeadingUtils.applySpacingAttributes(this, attrs, context);
     }
