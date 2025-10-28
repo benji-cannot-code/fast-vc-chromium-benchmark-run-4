2932f75eb6f5ccf315ff1c62f518ec79666c52f3
Zgroza Klimek <zgroza@chromium.org>
2025-10-28 08:30:49 -0700

    Fix ephemeral permission expiry mechanism on some builds
    There is a weird issue on some builds where using `++` instead of `+=`
    after inserting to a map caused the result to not be incremented at all.
    I have no idea why this might happen, but it breaks permission expiry
    there as a whole.
    
    The only build on which I was able to reproduce this is
    on a physical ChromeOS device with config:
    ```
    import("//build/args/chromeos/brya.gni")
    is_chrome_branded = true
    is_debug = false
    
    is_official_build = false
    symbol_level = 1
    enable_profiling = true
    
    optimize_webui = true
    target_os = "chromeos"
    use_remoteexec = true
    target_cpu = "x64"
    ```
    
    Added some unit tests to ensure the behavior is correct.
    
    Bug: 449931160
    Change-Id: I3489f5008a1ecb3d6fb0ac3eb58e2450aef1aecd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7016020
    Reviewed-by: Simon Hangl <simonha@google.com>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Olga Korokhina <korokhina@google.com>
    Commit-Queue: Zgroza (Luke) Klimek <zgroza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1536688}

--- chrome/browser/permissions/one_time_permissions_tracker.h
+++ chrome/browser/permissions/one_time_permissions_tracker.h
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // This observable class keeps track of one-time permission related browsing
 // states.
+class OneTimePermissionsTrackerTest;
+
 class OneTimePermissionsTracker : public KeyedService {
   using NotifyFunction =
       void (OneTimePermissionsTracker::*)(const url::Origin&);
@@ -72,6 +74,13 @@ class OneTimePermissionsTracker : public KeyedService {
 
   // Fires all running timers for testing purposes.
   void FireRunningTimersForTesting();
+
+  FRIEND_TEST_ALL_PREFIXES(OneTimePermissionsTrackerTest,
+                           ShouldIgnoreOrigin_IsolatedWebApp);
+  FRIEND_TEST_ALL_PREFIXES(OneTimePermissionsTrackerTest,
+                           ShouldIgnoreOrigin_OpaqueOrigin);
+  FRIEND_TEST_ALL_PREFIXES(OneTimePermissionsTrackerTest,
+                           ShouldIgnoreOrigin_ChromePages);
 
  protected:
   void NotifyLastPageFromOriginClosed(const url::Origin& origin);
