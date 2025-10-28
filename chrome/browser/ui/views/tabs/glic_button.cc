870db07a7de467ad4d337c16abebd3946a64ccb9
Ian Wells <iwells@chromium.org>
2025-10-28 12:37:24 -0700

    Glic Entrypoint Variations: only use kColorSysOnSurface when using the alt background color
    We were using ui::kColorSysOnSurface for the foreground color even when
    the flag was off. Some custom themes from extensions can make the
    surface colors dark while Chrome thinks it's a light theme, so
    kColorSysOnSurface is dark and can't be seen against the background.
    
    This CL changes back to using a foreground color ID that works in this
    situation and is drawn as white.
    
    However, we'll still need kColorSysOnSurface when the alt icon is
    enabled because we set a background color that matches Chrome's
    light/dark theme rather than the custom theme.
    
    Bug: 455014439
    Change-Id: I005154b1d958a4ce607d9bfb60cd0496905689a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7087595
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1536836}

--- chrome/browser/ui/views/tabs/glic_button.cc
+++ chrome/browser/ui/views/tabs/glic_button.cc
@@ -57,7 +57,9 @@ constexpr ui::ColorId kHighlightColorId = ui::kColorSysPrimary;
 constexpr ui::ColorId kTextOnHighlight = ui::kColorSysOnPrimary;
 constexpr ui::ColorId kTextDisabledOnHighlight = kTextOnHighlight;
 constexpr ui::ColorId kTextDisabled = ui::kColorLabelForegroundDisabled;
-constexpr ui::ColorId kDefaultTextColorV2 = ui::kColorSysOnSurface;
+
+constexpr ui::ColorId kForeground = kColorNewTabButtonForegroundFrameActive;
+constexpr ui::ColorId kForegroundOnAltBackground = ui::kColorSysOnSurface;
 
 constexpr int kIconSize = 16;
 
@@ -97,8 +99,9 @@ ui::ImageModel GetNormalIcon() {
         *ui::ResourceBundle::GetSharedInstance().GetImageSkiaNamed(
             IDR_GLIC_BUTTON_ALT_ICON));
   }
-  return ui::ImageModel::FromVectorIcon(GlicVectorIcon(),
-                                        ui::kColorSysOnSurface, kIconSize);
+  return ui::ImageModel::FromVectorIcon(
+      GlicVectorIcon(),
+      ShouldUseAltIcon() ? kForegroundOnAltBackground : kForeground, kIconSize);
 }
 
 ui::ImageModel GetIconForHighlight() {
@@ -453,9 +456,7 @@ void GlicButton::AnnounceNudgeShown() {
 }
 
 void GlicButton::SetDefaultColors() {
-  SetForegroundFrameActiveColorId(
-      EntrypointVariationsEnabled() ? kDefaultTextColorV2
-                                    : kColorNewTabButtonForegroundFrameActive);
+  SetForegroundFrameActiveColorId(kColorNewTabButtonForegroundFrameActive);
   SetForegroundFrameInactiveColorId(kColorNewTabButtonForegroundFrameInactive);
   SetBackgroundFrameActiveColorId(kColorNewTabButtonCRBackgroundFrameActive);
   SetBackgroundFrameInactiveColorId(
@@ -477,12 +478,12 @@ void GlicButton::UpdateTextAndBackgroundColors() {
       SetForegroundFrameActiveColorId(kTextOnHighlight);
       SetTextColor(STATE_DISABLED, kTextDisabledOnHighlight);
     } else {
-      SetForegroundFrameActiveColorId(kDefaultTextColorV2);
+      SetForegroundFrameActiveColorId(kForegroundOnAltBackground);
       SetTextColor(STATE_DISABLED, kTextDisabled);
     }
   } else {
     SetBackgroundFrameActiveColorId(kColorNewTabButtonCRBackgroundFrameActive);
-    SetForegroundFrameActiveColorId(kDefaultTextColorV2);
+    SetForegroundFrameActiveColorId(kForeground);
     SetTextColor(STATE_DISABLED, kTextDisabled);
   }
 
