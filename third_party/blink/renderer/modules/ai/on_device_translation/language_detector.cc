494a01c03ed27528bd6ab099d9fd224e4cd0f879
Isaac Ahouma <iahouma@google.com>
2025-10-28 13:52:30 -0700

    [Built-In AI] Relax activation requirements for Language Detector API
    This follows  https://chromium-review.googlesource.com/7068601.
    
    Bug: 454435239
    Test: Automated, activation requirements change only when flag enabled.
    Change-Id: Ia94d54ed43ed7cf8ff037d676be53b22901b70e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7091063
    Reviewed-by: Nathan Memmott <memmott@chromium.org>
    Commit-Queue: Isaac Ahouma <iahouma@google.com>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1536866}

--- third_party/blink/renderer/modules/ai/on_device_translation/language_detector.cc
+++ third_party/blink/renderer/modules/ai/on_device_translation/language_detector.cc
@@ -117,7 +117,7 @@ class LanguageDetectorCreateTask
 
     if (!context->IsServiceWorkerGlobalScope() &&
         RequiresUserActivation(result) &&
-        !LocalFrame::ConsumeTransientUserActivation(window->GetFrame())) {
+        !MeetsUserActivationRequirements(window)) {
       GetResolver()->RejectWithDOMException(
           DOMExceptionCode::kNotAllowedError,
           kExceptionMessageUserActivationRequired);
