f036be80e9af75996b1e73fda8212142f4329ed5
dpapad <dpapad@chromium.org>
2025-10-27 17:34:33 -0700

    WebUI: Disallow @ts-expect-error, @ts-no-check via ESLint checks.
    This is to more accurately enforce the styleguide at [1].
    
    As part of this CL fixing a few remaining violations in the codebase, by
    modifying the code. In a couple of cases in the CrOS Settings codebase,
    suppressing the ESLint checks since the violations are not trivial to
    address.
    
    [1] https://google.github.io/styleguide/tsguide.html#ts-ignore
    
    Fixed: 454067870
    Bug: b:266455078
    Change-Id: I8e98ed9166102f82774f668b6ed234fba15c415a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7081017
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1536356}

--- tools/typescript/definitions/pending.d.ts
+++ tools/typescript/definitions/pending.d.ts
@@ -106,3 +106,8 @@ interface Uint8ArrayConstructor {
 interface ImageCapture {
   grabFrame(): Promise<ImageBitmap>;
 }
+
+// https://developer.mozilla.org/en-US/docs/Web/API/PerformanceObserver/observe
+interface PerformanceObserverInit {
+  durationThreshold?: number;
+}
