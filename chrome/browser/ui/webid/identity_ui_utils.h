88c8a4440d43762532c355c98082dc33f13c3c12
Ekaterina Kulikova <kate.kulikova@lge.com>
2025-10-28 00:42:31 -0700

    IWYU: Add missing include of identity_request_account.h in identity_ui_utils.h
    In the CL https://crrev.com/c/6344590 used forward declared class
    IdentityRequestAccount but its requires the include in GCC compiler.
    Added required include and removed forward declaration to fix the issue.
    
    Bug: 41455655
    Change-Id: I600eafa956c2ecb6cdbb7c90a5309d722c81a10c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7074894
    Commit-Queue: Kate Kulikova <kate.kulikova@lge.com>
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1536485}

--- chrome/browser/ui/webid/identity_ui_utils.h
+++ chrome/browser/ui/webid/identity_ui_utils.h
@@ -10,12 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "build/build_config.h"
 #include "build/buildflag.h"
+#include "content/public/browser/webid/identity_request_account.h"
 #include "ui/gfx/image/canvas_image_source.h"
 #include "ui/gfx/image/image_skia.h"
-
-namespace content {
-class IdentityRequestAccount;
-}  // namespace content
 
 using IdentityRequestAccountPtr =
     scoped_refptr<content::IdentityRequestAccount>;
