bcf6f1e673a910f8ced7f2db8895f36b4efd19d6
Hiroki Nakagawa <nhiroki@chromium.org>
2025-10-27 23:57:00 -0700

    Prefetch: Add request integrity headers to prefetch requests
    This CL adds request integrity headers to prefetch requests. These
    headers are normally added by RequestHeaderIntegrityUrlLoaderThrottle,
    but prefetch requests do not currently support URLLoaderThrottles.
    Therefore, this CL adds the headers via an alternative path.
    
    The prefetch logic is implemented in content/, while the headers are
    implemented in chrome/. To cross this module boundary, this CL
    introduces a new ContentBrowserClient function.
    
    Bug: b:445439417
    Change-Id: Ie7ba13d41bdd211c9b9bda2ef8708c9df5d042db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7061792
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Taiyo Mizuhashi <taiyo@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1536476}

--- content/public/browser/content_browser_client.cc
+++ content/public/browser/content_browser_client.cc
@@ -2030,4 +2030,10 @@ bool ContentBrowserClient::ShouldAnimateBackForwardTransitions() {
 #endif
 }
 
+void ContentBrowserClient::ModifyRequestHeadersForPrefetch(
+    const GURL& url,
+    bool is_redirect,
+    net::HttpRequestHeaders& headers,
+    net::HttpRequestHeaders& cors_exempt_headers) {}
+
 }  // namespace content
