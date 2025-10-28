62e0463b6c6b7d8154e1e9fedcb3846d6002cfb9
Andrew Grieve <agrieve@chromium.org>
2025-10-27 17:40:40 -0700

    Android: Remove Robolectric shadow for AndroidTaskUtils
    Bug: 341267427
    Change-Id: Ib1a39ed7db0cc2462b00411b8998a7e180b41ba8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7088407
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1536361}

--- chrome/browser/ui/android/multiwindow/java/src/org/chromium/chrome/browser/multiwindow/MultiInstanceStateUnitTest.java
+++ chrome/browser/ui/android/multiwindow/java/src/org/chromium/chrome/browser/multiwindow/MultiInstanceStateUnitTest.java
@@ -21,8 +21,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.annotation.Config;
-import org.robolectric.annotation.Implementation;
-import org.robolectric.annotation.Implements;
 
 import org.chromium.base.ActivityState;
 import org.chromium.base.ApplicationStatus;
@@ -37,18 +35,8 @@ import java.util.Map;
 
 /** Unit tests for {@link MultiInstanceState}. */
 @RunWith(BaseRobolectricTestRunner.class)
-@Config(
-        manifest = Config.NONE,
-        shadows = {MultiInstanceStateUnitTest.ShadowAndroidTaskUtils.class})
+@Config(manifest = Config.NONE)
 public class MultiInstanceStateUnitTest {
-    @Implements(AndroidTaskUtils.class)
-    static class ShadowAndroidTaskUtils {
-        @Implementation
-        public static RecentTaskInfo getTaskInfoFromTask(AppTask task) {
-            return sTasks.get(task);
-        }
-    }
-
     private static final Map<AppTask, RecentTaskInfo> sTasks = new HashMap<>();
 
     private MultiInstanceState mMultiInstanceState;
@@ -114,6 +102,7 @@ public class MultiInstanceStateUnitTest {
 
     @Before
     public void setUp() {
+        AndroidTaskUtils.setTaskInfosForTesting(sTasks);
         MultiInstanceState.maybeCreate(this::getChromeTasks, this::matchesBaseActivity);
         mMultiInstanceState = MultiInstanceState.getInstanceForTesting();
     }
@@ -123,7 +112,6 @@ public class MultiInstanceStateUnitTest {
         ApplicationStatus.destroyForJUnitTests();
         sTasks.clear();
         mMultiInstanceState.clear();
-        mMultiInstanceState = null;
     }
 
     private BaseActivity createTaskAndLaunchActivity(int taskId, BaseActivity activity) {
--- chrome/browser/util/android/java/src/org/chromium/chrome/browser/util/AndroidTaskUtils.java
+++ chrome/browser/util/android/java/src/org/chromium/chrome/browser/util/AndroidTaskUtils.java
@@ -26,6 +26,7 @@ import org.chromium.build.annotations.Nullable;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 /** Deals with Document-related API calls. */
@@ -40,6 +41,7 @@ public class AndroidTaskUtils {
     private static final int MAX_NUM_TASKS = 100;
 
     @Nullable private static AppTask sAppTaskForTesting;
+    @Nullable private static Map<AppTask, RecentTaskInfo> sTaskInfosForTesting;
 
     /**
      * Finishes tasks other than the one with the given ID that were started with the given data in
@@ -88,10 +90,15 @@ public class AndroidTaskUtils {
 
     /**
      * Returns the RecentTaskInfo for the task, if the ActivityManager succeeds in finding the task.
+     *
      * @param task AppTask containing information about a task.
      * @return The RecentTaskInfo associated with the task, or null if it couldn't be found.
      */
     public static @Nullable RecentTaskInfo getTaskInfoFromTask(AppTask task) {
+        if (sTaskInfosForTesting != null) {
+            return sTaskInfosForTesting.get(task);
+        }
+
         RecentTaskInfo info = null;
         try {
             info = task.getTaskInfo();
@@ -101,8 +108,14 @@ public class AndroidTaskUtils {
         return info;
     }
 
+    public static void setTaskInfosForTesting(Map<AppTask, RecentTaskInfo> map) {
+        sTaskInfosForTesting = map;
+        ResettersForTesting.register(() -> sTaskInfosForTesting = null);
+    }
+
     /**
      * Returns the baseIntent of the RecentTaskInfo associated with the given task.
+     *
      * @param task Task to get the baseIntent for.
      * @return The baseIntent, or null if it couldn't be retrieved.
      */
