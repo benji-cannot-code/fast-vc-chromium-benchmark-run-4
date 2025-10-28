899c8a52893f83986f5802389a715d66647c0001
Nikhil Jakhar <nikhiljakhar@google.com>
2025-10-28 01:43:07 -0700

    legion: Add placeholder AttestationHandlerImpl
    This change introduces `AttestationHandlerImpl` as a placeholder, always
    returning an empty attestation request using the session proto and
    always validating any response.
    
    Bug: b:444610593
    Change-Id: I22d04320130fbe2d5ac2919456d719b591727d5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7072074
    Reviewed-by: Mohamad Arab <arabm@google.com>
    Commit-Queue: Nikhil Jakhar <nikhiljakhar@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1536516}

---
+++ components/legion/attestation_handler_impl.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/legion/attestation_handler_impl.h"
+
+#include "third_party/oak/chromium/proto/session/session.pb.h"
+
+namespace legion {
+
+AttestationHandlerImpl::AttestationHandlerImpl() = default;
+AttestationHandlerImpl::~AttestationHandlerImpl() = default;
+
+std::optional<oak::session::v1::AttestRequest>
+AttestationHandlerImpl::GetAttestationRequest() {
+  // For now, this is a placeholder that sends an empty attestation request.
+  return oak::session::v1::AttestRequest();
+}
+
+bool AttestationHandlerImpl::VerifyAttestationResponse(
+    const oak::session::v1::AttestResponse& evidence) {
+  // For now, this is a placeholder that assumes any response is valid.
+  return true;
+}
+
+}  // namespace legion
