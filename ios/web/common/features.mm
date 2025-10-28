d79582cba5600e4bc6a08c482cd4d1b72186d92d
Vincent Boisselle <vincb@google.com>
2025-10-27 18:59:20 -0700

    [ios] Keep SSL status up to date reliably
    Sometimes webkit navigations aren't handled by the browser proactively
    which results in having the SSLStatus of the GetLastCommittedItem() out
    of sync (where -updateSSLStatusForCurrentNavigationItem isn't called for
    the new nav item).
    
    NavigationItems can be created lazily but the SSLStatus is currently
    only updated proactively so the SSLStatus can get out of sync if the
    browser doesn't proactively handle the WK navigation.
    
    Bug: 442623892, 454691494
    Change-Id: Ief14ea36e8e7ddfa0351a61c99890a9d56a9396f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6996707
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1536394}

--- ios/web/common/features.mm
+++ ios/web/common/features.mm
@@ -94,4 +94,7 @@ bool CreateTabHelperOnlyForRealizedWebStates() {
   return base::FeatureList::IsEnabled(kCreateTabHelperOnlyForRealizedWebStates);
 }
 
+BASE_FEATURE(kUpdateSSLStatusOnNavigationItemLazyCreation,
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
 }  // namespace web::features
