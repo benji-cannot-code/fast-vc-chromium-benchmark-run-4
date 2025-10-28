e60a93160e16134a2da40f7469ce6b956cde2064
Zgroza Klimek <zgroza@chromium.org>
2025-10-28 06:37:02 -0700

    SmartCard: Disable Smart Card settings UI in non-regular profiles
    Web Smart Card API is available only to Isolated Web Apps, which are not
    available in certain contexts—for example guest/incognito sessions. It
    doesn't make sense then to display permission WebUI in those (it would
    be useless anyway).
    
    Bug: 454152416
    Change-Id: Iff2c4621cf8885165fa9c9682f4b87cc0698467a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7075034
    Commit-Queue: Zgroza (Luke) Klimek <zgroza@chromium.org>
    Reviewed-by: Olga Korokhina <korokhina@google.com>
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Reviewed-by: Simon Hangl <simonha@google.com>
    Cr-Commit-Position: refs/heads/main@{#1536615}

--- chrome/browser/ui/webui/settings/settings_ui.cc
+++ chrome/browser/ui/webui/settings/settings_ui.cc
@@ -122,6 +122,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/base/signin_pref_names.h"
 #include "components/signin/public/base/signin_switches.h"
 #include "components/sync/base/features.h"
+#include "content/public/browser/isolated_web_apps_policy.h"
 #include "content/public/browser/url_data_source.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
@@ -533,7 +534,8 @@ SettingsUI::SettingsUI(content::WebUI* web_ui)
 #if BUILDFLAG(IS_CHROMEOS)
   html_source->AddBoolean(
       "enableSmartCardReadersContentSetting",
-      base::FeatureList::IsEnabled(blink::features::kSmartCard));
+      base::FeatureList::IsEnabled(blink::features::kSmartCard) &&
+          content::AreIsolatedWebAppsEnabled(profile));
 #endif
 
 #if BUILDFLAG(IS_WIN) && BUILDFLAG(GOOGLE_CHROME_BRANDING)
