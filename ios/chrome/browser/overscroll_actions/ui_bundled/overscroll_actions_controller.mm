01a16632d08d9461ab36c0cb8f7d7a9fc246e473
Sylvain Defresne <sdefresne@chromium.org>
2025-10-28 00:43:08 -0700

    [code-health] Fix unsafe buffer in OverscrollActionsController
    Add a default constructor to SpringInsetState that initializes all
    the fields to 0.0 instead of using memset(...) to clear an instance.
    
    Bug: 40285824
    Change-Id: Ief0158b42c7b51ce0d912e9f3a052beb7f20ecb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7086148
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1536486}

--- ios/chrome/browser/overscroll_actions/ui_bundled/overscroll_actions_controller.mm
+++ ios/chrome/browser/overscroll_actions/ui_bundled/overscroll_actions_controller.mm
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #import "ios/chrome/browser/overscroll_actions/ui_bundled/overscroll_actions_controller.h"
 
 #import <QuartzCore/QuartzCore.h>
@@ -85,13 +80,13 @@ const CGFloat kSpringDampiness = 0.35;
 static int gInstanceCount = 0;
 
 // This holds the current state of the bounce back animation.
-typedef struct {
-  CGFloat yInset;
-  CGFloat headerInset;
-  CGFloat velocityInset;
-  CGFloat initialTopMargin;
-  CFAbsoluteTime time;
-} SpringInsetState;
+struct SpringInsetState {
+  CGFloat yInset = 0.0;
+  CGFloat headerInset = 0.0;
+  CGFloat velocityInset = 0.0;
+  CGFloat initialTopMargin = 0.0;
+  CFAbsoluteTime time = 0.0;
+};
 
 // Used to set the height of a view frame.
 // Implicit animations are disabled when setting the new frame.
@@ -970,7 +965,7 @@ UIEdgeInsets TopContentInset(UIScrollView* scrollView, CGFloat topInset) {
       [CADisplayLink displayLinkWithTarget:self
                                   selector:@selector(updateBounce)];
   _dpLink = dpLink;
-  memset(&_bounceState, 0, sizeof(_bounceState));
+  _bounceState = SpringInsetState();
   if (self.overscrollState == OverscrollState::ACTION_READY) {
     CGFloat distanceScrolled =
         [self scrollView].contentOffset.y - self.initialContentOffset;
