ee91c8958498496a155d4d8e4d59c5c732ba7990
François Doray <fdoray@chromium.org>
2025-10-27 22:05:32 -0700

    [leveldb] Add feature to control cache size.
    The SuppressMemoryListeners experiment shows that not purging
    LevelDB caches on memory pressure causes a statistically significant
    memory regression (which makes sense) with no obvious speed regression.
    Building on this, this CL adds a features to always keep the caches
    smaller, so we can measure the memory/speed impact.
    
    Bug: 443264587
    Change-Id: Ic001a0c0d3bce232bf479e093298eaa7f1d2e035
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7087671
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Auto-Submit: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1536451}

--- third_party/leveldatabase/leveldb_chrome.cc
+++ third_party/leveldatabase/leveldb_chrome.cc
@@ -5,12 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/leveldatabase/leveldb_chrome.h"
 
+#include <cstddef>
 #include <memory>
 #include <set>
 #include <vector>
 
 #include "base/containers/contains.h"
 #include "base/containers/flat_set.h"
+#include "base/byte_size.h"
+#include "base/feature_list.h"
 #include "base/files/file.h"
 #include "base/files/file_util.h"
 #include "base/format_macros.h"
@@ -39,7 +42,25 @@ namespace leveldb_chrome {
 
 namespace {
 
+// Feature to override the size of LevelDB block caches.
+//
+// The SuppressMemoryListeners experiment shows that not purging LevelDB caches
+// on memory pressure causes a statistically significant memory regression
+// (which makes sense) with no obvious speed regression. Building on this, this
+// feature will allow measuring the speed/memory impact of always keeping the
+// caches smaller.
+BASE_FEATURE(kLevelDBCacheSize, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE_PARAM(size_t,
+                   kLevelDBCacheSize_SizeBytes,
+                   &kLevelDBCacheSize,
+                   "leveldb_cache_size_bytes",
+                   base::KiBU(256).InBytes());
+
 size_t DefaultBlockCacheSize() {
+  if (base::FeatureList::IsEnabled(kLevelDBCacheSize)) {
+    return kLevelDBCacheSize_SizeBytes.Get();
+  }
+
   if (base::SysInfo::IsLowEndDeviceOrPartialLowEndModeEnabled()) {
     return 1 << 20;  // 1MB
   } else {
