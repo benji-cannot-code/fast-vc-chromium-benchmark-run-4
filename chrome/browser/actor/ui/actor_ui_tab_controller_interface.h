88026bcb5fea487897d58426a5684ac42b973843
Jacob Stanley <jacobstanley@google.com>
2025-10-28 07:28:39 -0700

    [Actor Overlay] Change on hover debouncing from 150ms to 25ms
    This allows for the scrim background update to be a bit smoother and
    more responsive.
    
    Screencast: https://screencast.googleplex.com/cast/NTY3NDMxNDg1NzcwOTU2OHxjYjNlZDM2MS03Mw
    
    Bug: b:454641152
    Change-Id: I17f48a1b760d25c19af1e72486922dfd44176954
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7088009
    Commit-Queue: Jacob Stanley <jacobstanley@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1536643}

--- chrome/browser/actor/ui/actor_ui_tab_controller_interface.h
+++ chrome/browser/actor/ui/actor_ui_tab_controller_interface.h
@@ -51,7 +51,7 @@ inline std::ostream& operator<<(std::ostream& os, UiTabState state) {
 }
 
 static constexpr base::TimeDelta kUpdateScrimBackgroundDebounceDelay =
-    base::Milliseconds(150);
+    base::Milliseconds(25);
 
 class ActorUiTabControllerFactoryInterface {
  public:
