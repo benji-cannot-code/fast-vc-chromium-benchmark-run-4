a709620f374b5ca08adbe314931ca7f0004e7461
Andrey Kosyakov <caseq@chromium.org>
2025-10-27 20:37:41 -0700

    Streams: do not expect JS to never throw (part 1)
    A perfectly valid JS code can still throw under certain circumstances,
    such as a stack overflow or `SetAbortScriptExecution()` callback.
    
    Under certain circumstances, we can end up with an ExceptionState
    indicating an exception while v8's pending exception has been cleared,
    which would cause bindings constructor callback to bail out without
    calling `return_value->AssociateWithWrapper()`, which would yield a
    platform object that has a null wrapped object.
    
    Bug: 427166012
    Change-Id: I4eca1888e232cfa176e94b24642e7ae3905b9b67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7087446
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1536430}

--- third_party/blink/renderer/core/streams/transform_stream.cc
+++ third_party/blink/renderer/core/streams/transform_stream.cc
@@ -192,8 +192,10 @@ TransformStream* TransformStream::Create(
   auto* stream = Create(script_state, CreateTrivialStartAlgorithm(),
                         transform_algorithm, flush_algorithm, 1, size_algorithm,
                         0, size_algorithm, exception_state);
-  DCHECK(stream);
-  DCHECK(!exception_state.HadException());
+  if (exception_state.HadException()) {
+    return nullptr;
+  }
+  CHECK(stream);
   TransformStreamDefaultController* controller =
       stream->transform_stream_controller_;
   transform_algorithm->SetController(controller);
@@ -243,6 +245,9 @@ TransformStream* TransformStream::Create(
              writable_size_algorithm, readable_high_water_mark,
              readable_size_algorithm, exception_state);
 
+  if (exception_state.HadException()) {
+    return nullptr;
+  }
   // 10. Let controller be ObjectCreate(the original value of
   //     TransformStreamDefaultController's prototype property).
   auto* controller = MakeGarbageCollected<TransformStreamDefaultController>();
@@ -260,7 +265,6 @@ TransformStream* TransformStream::Create(
     CHECK(rethrow_scope.HasCaught());
     return nullptr;
   }
-
   // 13. Resolve startPromise with startResult.
   start_promise->Resolve(start_result);
 
@@ -824,7 +828,11 @@ void TransformStream::Initialize(
       script_state, start_algorithm, write_algorithm, close_algorithm,
       abort_algorithm, writable_high_water_mark, writable_size_algorithm,
       exception_state);
-  DCHECK(!exception_state.HadException());
+  if (exception_state.HadException()) {
+    return;
+  }
+
+  CHECK(stream->writable_);
 
   // 6. Let pullAlgorithm be the following steps:
   //    a. Return ! TransformStreamDefaultSourcePullAlgorithm(stream).
@@ -844,7 +852,10 @@ void TransformStream::Initialize(
   stream->readable_ = ReadableStream::Create(
       script_state, start_algorithm, pull_algorithm, cancel_algorithm,
       readable_high_water_mark, readable_size_algorithm, exception_state);
-  DCHECK(!exception_state.HadException());
+  if (exception_state.HadException()) {
+    return;
+  }
+  CHECK(stream->readable_);
 
   //  9. Set stream.[[backpressure]] and stream.[[backpressureChangePromise]] to
   //     undefined.
--- third_party/blink/renderer/core/streams/writable_stream.cc
+++ third_party/blink/renderer/core/streams/writable_stream.cc
@@ -199,7 +199,7 @@ WritableStream* WritableStream::Create(ScriptState* script_state,
       exception_state);
 
   //  8. Return stream.
-  return stream;
+  return exception_state.HadException() ? nullptr : stream;
 }
 
 // static
