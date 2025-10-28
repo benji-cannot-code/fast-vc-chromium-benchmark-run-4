f01a398d36de1f90b83e556c7d05cfb47560d2f0
Dylan Cutler <dylancutler@google.com>
2025-10-28 14:55:29 -0700

    Apply optimization guide checks when origin gating enabled in MayActOnTab
    This CL fixes a bug in navigation gating where the optimization
    guide blocklist may not be applied if the browser starts the task
    on a sensitive site. This CL applies changes navigation gating to
    keep using the optimization guide blocklist during safety checks
    before every action.
    
    The CL ensures that URLs whose origin has been added to the per-task
    allow list do not have the optimization guide check applied. This is
    in case that a user does confirm in the dialog to interact with the
    sensitive site.
    
    Bug: 439672418
    Change-Id: Ib8b711899177fc0b353a2fabe7d134603191580c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7088088
    Commit-Queue: Dylan Cutler <dylancutler@google.com>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1536911}

--- chrome/browser/actor/actor_policy_checker.h
+++ chrome/browser/actor/actor_policy_checker.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "chrome/common/actor/task_id.h"
 #include "components/prefs/pref_change_registrar.h"
+#include "third_party/abseil-cpp/absl/container/flat_hash_set.h"
+#include "url/origin.h"
 
 class GURL;
 class Profile;
@@ -39,6 +41,7 @@ class ActorPolicyChecker {
   void MayActOnTab(const tabs::TabInterface& tab,
                    AggregatedJournal& journal,
                    TaskId task_id,
+                   const absl::flat_hash_set<url::Origin>& allowed_origins,
                    DecisionCallback callback);
   void MayActOnUrl(const GURL& url,
                    bool allow_insecure_http,
