84a934a5c6acd949e66002b213f51017d6dd7f96
chrstne <chrstne@google.com>
2025-10-28 09:33:39 -0700

    Rename actor task icon/nudge paused task variable
    Bug: 440107430
    Change-Id: I4bc84d8cd38ec0398a503e6f0a7e33ad8ccd9490
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7090812
    Reviewed-by: Maggie Jennings <mjenn@google.com>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Commit-Queue: Christine Ying <chrstne@google.com>
    Cr-Commit-Position: refs/heads/main@{#1536725}

--- chrome/browser/ui/tabs/glic_actor_task_icon_manager.cc
+++ chrome/browser/ui/tabs/glic_actor_task_icon_manager.cc
@@ -93,7 +93,7 @@ void GlicActorTaskIconManager::UpdateTaskIcon(bool is_showing,
   // TODO(crbug.com/431015299): Cache some of these values.
   auto completed_tasks =
       actor_service_->FindTaskIdsInInactive(&IsRecentlyCompletedTask);
-  auto paused_by_actor_tasks =
+  auto paused_or_yielded_actor_tasks =
       actor_service_->FindTaskIdsInActive([](const ActorTask& task) {
         return (task.GetState() == ActorTask::State::kPausedByActor ||
                 task.GetState() == ActorTask::State::kWaitingOnUser);
@@ -115,7 +115,7 @@ void GlicActorTaskIconManager::UpdateTaskIcon(bool is_showing,
   current_actor_task_icon_state_.is_visible = true;
 
   // Apply text state change.
-  if (!paused_by_actor_tasks.empty()) {
+  if (!paused_or_yielded_actor_tasks.empty()) {
     current_actor_task_icon_state_.text =
         ActorTaskIconState::Text::kNeedsAttention;
   } else if (!completed_tasks.empty()) {
@@ -135,13 +135,13 @@ void GlicActorTaskIconManager::UpdateTaskNudge() {
   // TODO(crbug.com/431015299): Cache some of these values.
   auto completed_tasks =
       actor_service_->FindTaskIdsInInactive(&IsRecentlyCompletedTask);
-  auto paused_by_actor_tasks =
+  auto paused_or_yielded_actor_tasks =
       actor_service_->FindTaskIdsInActive([](const ActorTask& task) {
         return (task.GetState() == ActorTask::State::kPausedByActor ||
                 task.GetState() == ActorTask::State::kWaitingOnUser);
       });
 
-  if (!paused_by_actor_tasks.empty()) {
+  if (!paused_or_yielded_actor_tasks.empty()) {
     current_actor_task_nudge_state_.text =
         ActorTaskNudgeState::Text::kNeedsAttention;
   } else if (!completed_tasks.empty()) {
