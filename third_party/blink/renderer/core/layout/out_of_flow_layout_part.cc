86a4a1e27f246a30c1c20fc3423053344a4ec60b
Yanling Wang <yanlingwang@microsoft.com>
2025-10-28 13:52:20 -0700

    [Masonry] Fix column-intrinsic-maximums.html
    This test was failing because padding was excluded when the containing
    block for out-of-flow items was a masonry container. This CL restores
    the padding area in the containing block calculation, aligning the
    behavior with grid. This CL also updates the expectation files for
    several other OOF static position tests in masonry to match the new
    logic.
    
    Bug: 343257585
    Change-Id: I3282acf4905a363b3368b9ebfc635600e0567c1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7076955
    Reviewed-by: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Commit-Queue: Yanling Wang <yanlingwang@microsoft.com>
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1536865}

--- third_party/blink/renderer/core/layout/out_of_flow_layout_part.cc
+++ third_party/blink/renderer/core/layout/out_of_flow_layout_part.cc
@@ -911,8 +911,7 @@ OutOfFlowLayoutPart::GetContainingBlockInfo(
     } else {
       rect = MasonryLayoutAlgorithm::ComputeOutOfFlowItemContainingRect(
           To<LayoutMasonry>(containing_box).CachedPlacementData(), layout_data,
-          style, borders, size, container_builder_->BorderScrollbarPadding(),
-          item);
+          style, borders, size, item);
     }
 
     return {.writing_direction = style.GetWritingDirection(),
