86bd0c65b93d4c3a2c6c89144c1a21de0c51dc18
Josiah Kiehl <kiehl@chromium.org>
2025-10-28 10:31:55 -0700

    [Contextual Tasks] Basic fixup and temporary styling for cr-composebox
    This is a basic fixup of the `<cr-composebox>` element within the
    Contextual Tasks UI to get it to render without errors. The styling is
    temporary, however submitting queries does work.
    
    Bug: 451688545
    Change-Id: Ida27aa9b170f60c427010e20fd794d18f06ad3a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7083991
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Auto-Submit: Josiah Kiehl <kiehl@google.com>
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1536773}

--- chrome/browser/contextual_tasks/contextual_tasks_ui.cc
+++ chrome/browser/contextual_tasks/contextual_tasks_ui.cc
@@ -56,8 +56,8 @@ ContextualTasksUI::ContextualTasksUI(content::WebUI* web_ui)
   source->AddString("composeboxAttachmentFileTypes", "");
   source->AddInteger("composeboxFileMaxSize", 0);
   source->AddInteger("composeboxFileMaxCount", 0);
-  // Disable typed suggest.
-  source->AddBoolean("composeboxShowTypedSuggest", false);
+  // Enable typed suggest.
+  source->AddBoolean("composeboxShowTypedSuggest", true);
   // Disable ZPS.
   source->AddBoolean("composeboxShowZps", false);
   // Disable image context suggestions.
@@ -81,11 +81,13 @@ ContextualTasksUI::ContextualTasksUI(content::WebUI* web_ui)
   source->AddBoolean(
       "enableThumbnailSizingTweaks",
       lens::features::GetVisualSelectionUpdatesEnableThumbnailSizingTweaks());
-  source->AddString("searchboxComposePlaceholder", "Placeholder Text");
+  source->AddString("searchboxComposePlaceholder", "[i18n] Ask Google...");
   source->AddBoolean("composeboxShowPdfUpload", false);
   source->AddBoolean("composeboxSmartComposeEnabled", false);
   source->AddBoolean("composeboxShowDeepSearchButton", false);
   source->AddBoolean("composeboxShowCreateImageButton", false);
+  source->AddBoolean("composeboxShowRecentTabChip", false);
+  source->AddBoolean("composeboxShowSubmit", true);
 }
 
 ContextualTasksUI::~ContextualTasksUI() = default;
@@ -144,10 +146,11 @@ void ContextualTasksUI::CreatePageHandler(
     mojo::PendingReceiver<searchbox::mojom::PageHandler>
         pending_searchbox_handler) {
   DCHECK(pending_page.is_valid());
-  auto handler = std::make_unique<ContextualTasksComposeboxHandler>(
+  composebox_handler_ = std::make_unique<ContextualTasksComposeboxHandler>(
       Profile::FromWebUI(web_ui()), web_ui()->GetWebContents(),
       std::move(pending_page_handler), std::move(pending_page),
       std::move(pending_searchbox_handler));
+  composebox_handler_->SetPage(std::move(pending_searchbox_page));
 }
 
 ContextualTasksUI::FrameNavObserver::FrameNavObserver(
--- chrome/browser/resources/contextual_tasks/app.ts
+++ chrome/browser/resources/contextual_tasks/app.ts
@@ -3,13 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import '//resources/cr_components/composebox/composebox.js';
+
 import {CrLitElement} from 'chrome://resources/lit/v3_0/lit.rollup.js';
 
 import {getCss} from './app.css.js';
 import {getHtml} from './app.html.js';
 import type {BrowserProxy} from './contextual_tasks_browser_proxy.js';
 import {BrowserProxyImpl} from './contextual_tasks_browser_proxy.js';
-import '//resources/cr_components/composebox/composebox.js';
 
 export class ContextualTasksAppElement extends CrLitElement {
   static get is() {
