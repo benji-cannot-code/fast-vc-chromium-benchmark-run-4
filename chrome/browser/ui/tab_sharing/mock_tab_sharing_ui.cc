dba8e7324203f2c00d41ed4d7a6f7a1d5e5bec7d
Tove Petersson <tovep@chromium.org>
2025-10-28 11:53:58 -0700

    Eliminate MockTabSharingUIViews duplication
    Coalesced the duplicated MockTabSharingUIViews classes into
    a new reusable MockTabSharingUI class in
    chrome/browser/ui/tab_sharing/mock_tab_sharing_ui.*
    
    Bug: 441128451
    Change-Id: I96f6a2add8e943cac59fa20d089ff16748ed7716
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7085513
    Reviewed-by: Palak Agarwal <agpalak@chromium.org>
    Commit-Queue: Tove Petersson <tovep@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1536821}

---
+++ chrome/browser/ui/tab_sharing/mock_tab_sharing_ui.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/tab_sharing/mock_tab_sharing_ui.h"
+
+MockTabSharingUI::MockTabSharingUI()
+    : uma_logger_(content::DesktopMediaID::Type::TYPE_WEB_CONTENTS) {}
+
+MockTabSharingUI::~MockTabSharingUI() = default;
+
+gfx::NativeViewId MockTabSharingUI::OnStarted(
+    base::OnceClosure stop_callback,
+    content::MediaStreamUI::SourceCallback source_callback,
+    const std::vector<content::DesktopMediaID>& media_ids) {
+  return 0;
+}
+
+ScreensharingControlsHistogramLogger& MockTabSharingUI::GetUmaLogger() {
+  return uma_logger_;
+}
+
+void MockTabSharingUI::OnRegionCaptureRectChanged(
+    const std::optional<gfx::Rect>& region_capture_rect) {}
