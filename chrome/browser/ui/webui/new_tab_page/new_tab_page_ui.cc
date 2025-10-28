e3a079ea7d5ec9ad860116efa76ee1fb2404a2d0
Alex Chen <alexwchen@chromium.org>
2025-10-27 19:10:13 -0700

    [NTPShortcuts] Select enterprise shortcuts by default
    Currently, when enterprise shortcuts are set by policy, they appear
    as an additional selection option but are not selected by default. This
    change updates the flow so that when enterprise shortcut mixing is
    enabled, if the user has not previously disabled
    or enabled enterprise shortcuts, they will be visible and enabled by
    default.
    
    This CL updates NewTabPageUI to listen for policy updates and
    set the enterprise shortcuts visibility accordingly.
    
    This CL also updates the CustomizeChromePageHandler to only set the
    enterprise shortcuts visibility pref if the feature is enabled and
    policy is set.
    
    Bug: b:441766227
    Change-Id: Ie941850a9e19ffa9b579ce11156f00d676278ffd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7082030
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Commit-Queue: Alex Chen <alexwchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1536400}

--- chrome/browser/ui/webui/new_tab_page/new_tab_page_ui.cc
+++ chrome/browser/ui/webui/new_tab_page/new_tab_page_ui.cc
@@ -93,6 +93,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/history_clusters/core/features.h"
 #include "components/ntp_tiles/features.h"
 #include "components/ntp_tiles/most_visited_sites.h"
+#include "components/ntp_tiles/pref_names.h"
 #include "components/ntp_tiles/tile_type.h"
 #include "components/omnibox/browser/aim_eligibility_service.h"
 #include "components/omnibox/browser/omnibox_prefs.h"
@@ -768,6 +769,10 @@ NewTabPageUI::NewTabPageUI(content::WebUI* web_ui)
       ntp_prefs::kNtpShortcutsVisible,
       base::BindRepeating(&NewTabPageUI::OnTilesVisibilityPrefChanged,
                           weak_ptr_factory_.GetWeakPtr()));
+  pref_change_registrar_.Add(
+      ntp_tiles::prefs::kEnterpriseShortcutsPolicyList,
+      base::BindRepeating(&NewTabPageUI::OnEnterpriseShortcutsPolicyChanged,
+                          weak_ptr_factory_.GetWeakPtr()));
 
   // Store basic theme info in load time data to make the background color and
   // background image available as soon as the page loads to prevent a potential
@@ -1247,7 +1252,27 @@ void NewTabPageUI::OnTilesVisibilityPrefChanged() {
   }
 }
 
+void NewTabPageUI::OnEnterpriseShortcutsPolicyChanged() {
+  // If enterprise shortcuts feature or mixing is disabled, do nothing.
+  if (!base::FeatureList::IsEnabled(ntp_tiles::kNtpEnterpriseShortcuts) ||
+      !ntp_tiles::kNtpEnterpriseShortcutsAllowMixingParam.Get()) {
+    return;
+  }
+  // If enterprise shortcuts are available by policy and the user
+  // has not previously set the visibility preference, then enable enterprise
+  // shortcuts by default.
+  if (!profile_->GetPrefs()
+           ->GetList(ntp_tiles::prefs::kEnterpriseShortcutsPolicyList)
+           .empty() &&
+      !profile_->GetPrefs()->HasPrefPath(
+          ntp_prefs::kNtpEnterpriseShortcutsVisible)) {
+    profile_->GetPrefs()->SetBoolean(ntp_prefs::kNtpEnterpriseShortcutsVisible,
+                                     true);
+  }
+}
+
 void NewTabPageUI::OnLoad() {
+  OnEnterpriseShortcutsPolicyChanged();
   base::Value::Dict update;
   update.Set("navigationStartTime",
              navigation_start_time_.InMillisecondsFSinceUnixEpoch());
