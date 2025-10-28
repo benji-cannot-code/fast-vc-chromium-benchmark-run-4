193b47b5da4f9c973843f8cc192abb77d324d22c
Tsuyoshi Horo <horo@chromium.org>
2025-10-27 19:45:14 -0700

    Disable InteractionTestUtilBrowserTest.CompareScreenshot_WebPage*
    According to the flakiness dashboard both CompareScreenshot_WebPage and
    CompareScreenshot_WebPageElement are flaky on all platforms.
    
    Bug: 455686746, 433607240
    Change-Id: I5ec251dfa410b8a80739aafbee433fbd31b6e94e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7089831
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Auto-Submit: Tsuyoshi Horo <horo@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Owners-Override: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1536413}

--- chrome/test/interaction/interaction_test_util_browser_browsertest.cc
+++ chrome/test/interaction/interaction_test_util_browser_browsertest.cc
@@ -141,8 +141,9 @@ IN_PROC_BROWSER_TEST_F(InteractionTestUtilBrowserTest,
   }
 }
 
+// TODO(crbug.com/455686746): This test is flaky on all platforms.
 IN_PROC_BROWSER_TEST_F(InteractionTestUtilBrowserTest,
-                       CompareScreenshot_WebPage) {
+                       DISABLED_CompareScreenshot_WebPage) {
   // Set the browser view to a consistent size.
   BrowserView* const browser_view =
       BrowserView::GetBrowserViewForBrowser(browser());
@@ -161,8 +162,9 @@ IN_PROC_BROWSER_TEST_F(InteractionTestUtilBrowserTest,
                  /*baseline_cl=*/"3924454"));
 }
 
+// TODO(crbug.com/455686746): This test is flaky on all platforms.
 IN_PROC_BROWSER_TEST_F(InteractionTestUtilBrowserTest,
-                       CompareScreenshot_WebPageElement) {
+                       DISABLED_CompareScreenshot_WebPageElement) {
   // Set the browser view to a consistent size.
   BrowserView* const browser_view =
       BrowserView::GetBrowserViewForBrowser(browser());
