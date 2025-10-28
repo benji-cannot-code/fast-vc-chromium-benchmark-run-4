b4091167e2eeef62fd739b934b9e67d3369cd59a
Yuki Shiino <yukishiino@chromium.org>
2025-10-27 19:51:01 -0700

    ELUD: Add ADVANCED_MEMORY_SAFETY_CHECKS to aura::Window
    Adds ADVANCED_MEMORY_SAFETY_CHECKS as a mitigation of
    crbug.com/376575664
    
    Bug: 376575664
    Change-Id: Ic9305c0cb58c0587b242d5c409850a1d78d27c32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7082094
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1536414}

--- ui/aura/window.h
+++ ui/aura/window.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/containers/flat_set.h"
 #include "base/memory/raw_ptr.h"
+#include "base/memory/safety_checks.h"
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
 #include "base/time/time.h"
@@ -111,6 +112,9 @@ class AURA_EXPORT Window : public ui::LayerDelegate,
                            public ui::PropertyHandler,
                            public ui::metadata::MetaDataProvider,
                            public viz::HostFrameSinkClient {
+  // TODO(crbug.com/376575664): Remove this macro once the bug gets fixed.
+  ADVANCED_MEMORY_SAFETY_CHECKS();
+
  public:
   METADATA_HEADER_BASE(Window);
 
