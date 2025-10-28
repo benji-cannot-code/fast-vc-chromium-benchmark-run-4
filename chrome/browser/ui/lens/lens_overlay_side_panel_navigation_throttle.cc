261d8ac167cb9a35b3337351bddc82201ecaa120
Duncan Mercer <mercerd@google.com>
2025-10-28 15:45:58 -0700

    [AIM] Fix AIM opening in side panel when AIM eligibility disabled
    AIM should only open in the side panel if AIM M3 is enabled via the AIM
    eligibility service. This is planned to be cherry picked to M143 and M142.
    
    Bug: b:450085243
    Change-Id: I6b10e9f8c0a492a39f2c2131bd4bb5c71676c10b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7091097
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1536948}

--- chrome/browser/ui/lens/lens_overlay_side_panel_navigation_throttle.cc
+++ chrome/browser/ui/lens/lens_overlay_side_panel_navigation_throttle.cc
@@ -6,10 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/lens/lens_overlay_side_panel_navigation_throttle.h"
 
 #include "base/memory/ptr_util.h"
+#include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/lens/lens_overlay_controller.h"
 #include "chrome/browser/ui/lens/lens_overlay_side_panel_coordinator.h"
 #include "chrome/browser/ui/lens/lens_overlay_theme_utils.h"
 #include "chrome/browser/ui/lens/lens_overlay_url_builder.h"
+#include "chrome/browser/ui/lens/lens_search_feature_flag_utils.h"
 #include "components/lens/lens_features.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/navigation_throttle_registry.h"
@@ -92,7 +94,10 @@ LensOverlaySidePanelNavigationThrottle::HandleSidePanelRequest() {
   // not supported in the side panel it should also be handled by the side panel
   // coordinator so it can open in a new tab without changing the
   // loading/offline state.
-  if (!lens::IsValidSearchResultsUrl(url) || ShouldOpenSearchURLInNewTab(url)) {
+  auto* const profile =
+      controller->GetTabInterface()->GetBrowserWindowInterface()->GetProfile();
+  if (!lens::IsValidSearchResultsUrl(url) ||
+      ShouldOpenSearchURLInNewTab(url, lens::IsAimM3Enabled(profile))) {
     return content::NavigationThrottle::CANCEL;
   }
 
