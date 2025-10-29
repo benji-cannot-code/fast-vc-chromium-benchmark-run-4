389eb37d93e60b52511ab5431852115d1c844d1a
Sky Debreuil <skydebreuil@google.com>
2025-10-29 16:12:08 -0700

    Removed SharedImageFormatRestrictedSinglePlaneUtils class
    The SharedImageFormatRestrictedSinglePlaneUtils class is only used in
    one spot - so we just inline it there.
    
    Also moved TextureStorageFormat method to anonymous namespace.
    
    Change-Id: Iecfd4b9e604b199a95308d8c4216dbc29bf0a1dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7097766
    Auto-Submit: Sky Debreuil <skydebreuil@google.com>
    Commit-Queue: Peter McNeeley <petermcneeley@google.com>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Peter McNeeley <petermcneeley@google.com>
    Cr-Commit-Position: refs/heads/main@{#1537634}

--- components/viz/common/resources/shared_image_format_utils.h
+++ components/viz/common/resources/shared_image_format_utils.h
@@ -9,14 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/component_export.h"
 #include "components/viz/common/resources/shared_image_format.h"
 
-namespace gpu {
-class SharedImageFormatRestrictedUtilsAccessor;
-}  // namespace gpu
-
-namespace cc {
-class PerfContextProvider;
-}
-
 namespace gfx {
 enum class BufferFormat : uint8_t;
 }
@@ -24,10 +16,6 @@ enum class BufferFormat : uint8_t;
 enum SkColorType : int;
 
 namespace viz {
-
-class ContextProviderCommandBuffer;
-class TestContextProvider;
-class TestInProcessContextProvider;
 
 // Returns the closest SkColorType for a given single planar `format`.
 //
@@ -115,24 +103,6 @@ bool IsOddSizeMultiPlanarBuffersAllowed();
 // Returns a span containing all mappable SharedImageFormats.
 COMPONENT_EXPORT(VIZ_SHARED_IMAGE_FORMAT)
 base::span<const SharedImageFormat> GetMappableSharedImageFormatForTesting();
-
-// Utilities that conceptually belong only on the service side, but are
-// currently used by some clients. Usage is restricted to friended clients.
-class COMPONENT_EXPORT(VIZ_SHARED_IMAGE_FORMAT)
-    SharedImageFormatRestrictedSinglePlaneUtils {
- private:
-  friend class ContextProviderCommandBuffer;
-  friend class TestContextProvider;
-  friend class TestInProcessContextProvider;
-  friend class cc::PerfContextProvider;
-  friend class gpu::SharedImageFormatRestrictedUtilsAccessor;
-
-  // |use_angle_rgbx_format| should be true when the
-  // GL_ANGLE_rgbx_internal_format extension is available.
-  static unsigned int ToGLTextureStorageFormat(SharedImageFormat format,
-                                               bool use_angle_rgbx_format);
-};
-
 }  // namespace viz
 
 #endif  // COMPONENTS_VIZ_COMMON_RESOURCES_SHARED_IMAGE_FORMAT_UTILS_H_
