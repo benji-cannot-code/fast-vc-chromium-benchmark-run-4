a1de7f56c1bc3517ac4acd99b710b7940e6f8c5a
Prameet Shah <phshah@google.com>
2025-10-28 17:04:18 -0700

    gd: Remove the expired flag 'game-dashboard-game-pwas'
    Remove references to 'game-dashboard-game-pwas'.
    In GameDashboardController, merge the list of PWA game
    app IDs to a single list of game App IDs.
    
    Bug: 394308611
    Test: Ran ash unit tests.
    Change-Id: I2544c83997ea48f1daf47882100073d663a30b28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7093297
    Reviewed-by: Gina Domergue <gdomergue@google.com>
    Commit-Queue: Prameet Shah <phshah@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1537003}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -893,9 +893,6 @@ BASE_FEATURE(kGaiaRecordAccountCreation, base::FEATURE_ENABLED_BY_DEFAULT);
 // Enables Gamepad Support.
 BASE_FEATURE(kGameDashboardGamepadSupport, base::FEATURE_DISABLED_BY_DEFAULT);
 
-// Enables the Game Dashboard for additional PWA games.
-BASE_FEATURE(kGameDashboardGamePWAs, base::FEATURE_ENABLED_BY_DEFAULT);
-
 // Enables the Game Dashboard Main Menu utility views.
 BASE_FEATURE(kGameDashboardUtilities, base::FEATURE_DISABLED_BY_DEFAULT);
 
@@ -3107,10 +3104,6 @@ bool IsLockScreenHideSensitiveNotificationsSupported() {
 
 bool IsGameDashboardGamepadSupportEnabled() {
   return base::FeatureList::IsEnabled(kGameDashboardGamepadSupport);
-}
-
-bool IsGameDashboardGamePWAsEnabled() {
-  return base::FeatureList::IsEnabled(kGameDashboardGamePWAs);
 }
 
 bool AreGameDashboardUtilitiesEnabled() {
