88cce86de12cd12976c3a99c0ab1282a4249e6ee
Ernesto Izquierdo Clua <eic@google.com>
2025-10-29 11:28:32 -0700

    [ios] Create DataControlsEditMenuBuilder
    Creates an EditMenuBuilder that removes the share option from the
    context menu of web sites when disallowed by Enterprise policies.
    
    This is a multipart CL. The next patch uses the builder to modify the
    context menu in Chrome iOS.
    
    Bug: 439545286
    Change-Id: I243502f9503156ae7266d3ff4a983100e8b7480b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7081664
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1537471}

---
+++ ios/chrome/browser/enterprise/data_controls/model/data_controls_edit_menu_builder.h
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_ENTERPRISE_DATA_CONTROLS_MODEL_DATA_CONTROLS_EDIT_MENU_BUILDER_H_
+#define IOS_CHROME_BROWSER_ENTERPRISE_DATA_CONTROLS_MODEL_DATA_CONTROLS_EDIT_MENU_BUILDER_H_
+
+#import <UIKit/UIKit.h>
+
+#import "ios/chrome/browser/browser_container/model/edit_menu_builder.h"
+
+// A builder that modifies the edit menu based on Data Controls policies.
+@interface DataControlsEditMenuBuilder : NSObject <EditMenuBuilder>
+@end
+
+#endif  // IOS_CHROME_BROWSER_ENTERPRISE_DATA_CONTROLS_MODEL_DATA_CONTROLS_EDIT_MENU_BUILDER_H_
---
+++ ios/chrome/browser/enterprise/data_controls/model/data_controls_edit_menu_builder.mm
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/enterprise/data_controls/model/data_controls_edit_menu_builder.h"
+
+#import "ios/chrome/browser/enterprise/data_controls/model/data_controls_tab_helper.h"
+#import "ios/web/public/web_state.h"
+
+@implementation DataControlsEditMenuBuilder
+
+- (void)buildEditMenuWithBuilder:(id<UIMenuBuilder>)builder
+                      inWebState:(web::WebState*)webState {
+  if (!webState) {
+    return;
+  }
+
+  data_controls::DataControlsTabHelper* tab_helper =
+      data_controls::DataControlsTabHelper::GetOrCreateForWebState(webState);
+  if (tab_helper && !tab_helper->ShouldAllowShare()) {
+    [builder removeMenuForIdentifier:UIMenuShare];
+  }
+}
+
+@end
