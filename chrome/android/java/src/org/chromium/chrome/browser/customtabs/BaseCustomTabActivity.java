f0ee67e4f5cd692dd265eaf7cc49ca840b508349
Sinan Sahin <sinansahin@google.com>
2025-10-29 15:32:50 -0700

    Remove power saver state from night mode calculation
    Night mode (aka dark mode) currently gets enabled when the battery
    saver is on. This was useful for Android P- where night mode wasn't
    widely available. However, now that our min API is 29 (Android Q), using
    the power saver state to enable night mode is no longer useful.
    
    Simplifying the night mode logic is also going to make migrating to
    UiModeManager for night mode easier.
    
    Bug: 455964293
    Change-Id: I75a458cfde5d54e044cf843a90d560fce3e489dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7093458
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1537594}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/BaseCustomTabActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/BaseCustomTabActivity.java
@@ -104,7 +104,6 @@ import org.chromium.chrome.browser.init.ActivityProfileProvider;
 import org.chromium.chrome.browser.lifecycle.ActivityLifecycleDispatcher;
 import org.chromium.chrome.browser.metrics.UmaSessionStats;
 import org.chromium.chrome.browser.night_mode.NightModeStateProvider;
-import org.chromium.chrome.browser.night_mode.PowerSavingModeMonitor;
 import org.chromium.chrome.browser.profiles.OtrProfileId;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.profiles.ProfileProvider;
@@ -269,8 +268,7 @@ public abstract class BaseCustomTabActivity extends ChromeActivity {
     public CustomTabNightModeStateController getCustomTabNightModeStateController() {
         if (mNightModeStateController == null) {
             mNightModeStateController =
-                    new CustomTabNightModeStateController(
-                            getLifecycleDispatcher(), PowerSavingModeMonitor.getInstance());
+                    new CustomTabNightModeStateController(getLifecycleDispatcher());
         }
         return mNightModeStateController;
     }
--- chrome/android/junit/src/org/chromium/chrome/browser/customtabs/CustomTabNightModeStateControllerTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/customtabs/CustomTabNightModeStateControllerTest.java
@@ -37,7 +37,6 @@ import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.Batch;
 import org.chromium.chrome.browser.lifecycle.ActivityLifecycleDispatcher;
 import org.chromium.chrome.browser.night_mode.NightModeStateProvider;
-import org.chromium.chrome.browser.night_mode.PowerSavingModeMonitor;
 import org.chromium.chrome.browser.night_mode.SystemNightModeMonitor;
 
 /** Tests for {@link CustomTabNightModeStateController}. */
@@ -46,12 +45,10 @@ import org.chromium.chrome.browser.night_mode.SystemNightModeMonitor;
 @Config(manifest = Config.NONE)
 public class CustomTabNightModeStateControllerTest {
     @Rule public final MockitoRule mMockitoRule = MockitoJUnit.rule();
-    @Mock private PowerSavingModeMonitor mPowerSavingModeMonitor;
     @Mock private SystemNightModeMonitor mSystemNightModeMonitor;
     @Mock private ActivityLifecycleDispatcher mActivityLifecycleDispatcher;
     @Mock private AppCompatDelegate mAppCompatDelegate;
     @Captor private ArgumentCaptor<SystemNightModeMonitor.Observer> mSystemNightModeObserverCaptor;
-    @Captor private ArgumentCaptor<Runnable> mPowerSavingObserverCaptor;
 
     private CustomTabNightModeStateController mNightModeController;
 
@@ -60,11 +57,8 @@ public class CustomTabNightModeStateControllerTest {
         doNothing()
                 .when(mSystemNightModeMonitor)
                 .addObserver(mSystemNightModeObserverCaptor.capture());
-        doNothing().when(mPowerSavingModeMonitor).addObserver(mPowerSavingObserverCaptor.capture());
         SystemNightModeMonitor.setInstanceForTesting(mSystemNightModeMonitor);
-        mNightModeController =
-                new CustomTabNightModeStateController(
-                        mActivityLifecycleDispatcher, mPowerSavingModeMonitor);
+        mNightModeController = new CustomTabNightModeStateController(mActivityLifecycleDispatcher);
     }
 
     @Test
@@ -101,29 +95,12 @@ public class CustomTabNightModeStateControllerTest {
     }
 
     @Test
-    public void ignoresPowerSaving_WhenSchemeForced() {
-        initializeWithColorScheme(COLOR_SCHEME_LIGHT);
-        setPowerSavingMode(true);
-        assertFalse(mNightModeController.isInNightMode());
-    }
-
-    @Test
     public void followsSystemSetting_WhenSchemeIsSystem() {
         setSystemNightMode(true);
         initializeWithColorScheme(COLOR_SCHEME_SYSTEM);
         assertTrue(mNightModeController.isInNightMode());
 
         setSystemNightMode(false);
-        assertFalse(mNightModeController.isInNightMode());
-    }
-
-    @Test
-    public void followsPowerSavingMode_WhenSchemeIsSystem() {
-        setPowerSavingMode(true);
-        initializeWithColorScheme(COLOR_SCHEME_SYSTEM);
-        assertTrue(mNightModeController.isInNightMode());
-
-        setPowerSavingMode(false);
         assertFalse(mNightModeController.isInNightMode());
     }
 
@@ -143,20 +120,12 @@ public class CustomTabNightModeStateControllerTest {
         NightModeStateProvider.Observer observer = mock(NightModeStateProvider.Observer.class);
         initializeWithColorScheme(COLOR_SCHEME_SYSTEM);
         mNightModeController.addObserver(observer);
-        setPowerSavingMode(true);
         verify(observer, never()).onNightModeStateChanged();
     }
 
     private void initializeWithColorScheme(int colorScheme) {
         Intent intent = new CustomTabsIntent.Builder().setColorScheme(colorScheme).build().intent;
         mNightModeController.initialize(mAppCompatDelegate, intent);
-    }
-
-    private void setPowerSavingMode(boolean isPowerSaving) {
-        when(mPowerSavingModeMonitor.powerSavingIsOn()).thenReturn(isPowerSaving);
-        for (Runnable observer : mPowerSavingObserverCaptor.getAllValues()) {
-            observer.run();
-        }
     }
 
     private void setSystemNightMode(boolean isInNightMode) {
