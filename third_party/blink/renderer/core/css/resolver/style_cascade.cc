55581fec9b1c8dce645534c1469e9ec30f23a529
Anders Hartvoll Ruud <andruud@chromium.org>
2025-10-29 11:53:09 -0700

    Guard against invalid adjustment of writing-mode/direction for tables
    StyleAdjuster overwrites the writing-mode and direction for elements
    with table display types, which is either wrong because we don't
    actually want to modify the computed value (Issue 40527196),
    or it's wrong because it's *too late* to make adjustments to cascade-
    affecting properties at that point.
    
    This CL does not try to fix this (but see previous attempt by Ian,
    CL:3938617), but instead guards against it in StyleCascade by ensuring
    that the "writing-direction" we use during cascade expansion is
    the same we use during the apply step, and (crucially) remains
    the same during *subsequent* apply steps on the same StyleCascade
    object.
    
    This fixes a NOTREACHED that occurs due to the Apply step for the base
    style and the Apply step for the animation style (with an adjustment
    in between) having unexpectedly different writing-directions.
    
    Fixed: 444818221
    Bug: 40527196
    Change-Id: I139f723bfd4c0bd6407b3aa1c44f6e9b66fed352
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7096301
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1537479}

--- third_party/blink/renderer/core/css/resolver/style_cascade.cc
+++ third_party/blink/renderer/core/css/resolver/style_cascade.cc
@@ -472,6 +472,7 @@ void StyleCascade::AnalyzeIfNeeded() {
 
 void StyleCascade::AnalyzeMatchResult() {
   AddExplicitDefaults();
+  writing_direction_ = state_.StyleBuilder().GetWritingDirection();
 
   int index = 0;
   for (const MatchedProperties& properties :
@@ -1507,7 +1508,7 @@ const CSSValue* StyleCascade::ResolveFlipRevert(const CSSProperty& property,
   // Note: the value is transformed *from* the property we're reverting *to*.
   const CSSValue* flipped = TryValueFlips::FlipValue(
       /* from_property */ to_property.PropertyID(), unflipped,
-      value.Transform(), state_.StyleBuilder().GetWritingDirection());
+      value.Transform(), writing_direction_);
   return Resolve(property, *flipped, tree_scope,
                  /*mixin_parameter_bindings=*/nullptr, priority, origin,
                  resolver);
@@ -2919,8 +2920,7 @@ const CSSProperty& StyleCascade::ResolveSurrogate(const CSSProperty& property) {
   // currently a flag to distinguish such surrogates from e.g. css-logical
   // properties.
   depends_on_cascade_affecting_property_ = true;
-  const CSSProperty* original =
-      property.SurrogateFor(state_.StyleBuilder().GetWritingDirection());
+  const CSSProperty* original = property.SurrogateFor(writing_direction_);
   DCHECK(original);
   return *original;
 }
