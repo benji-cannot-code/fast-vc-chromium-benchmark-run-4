92cb65148e22c9d9c4db3c0f12a7546bc61a7640
kylechar <kylechar@chromium.org>
2025-10-29 08:54:09 -0700

    Remove lock from GpuPersistentCache
    The underlying PersistentCache implementation is already thread safe so
    lock isn't really necessary. To guard against races setting
    `persistent_cache_` add an atomic flag that is set after pointer is
    successfully initialized. That ensures other threads don't dereference
    the pointer until it's ready.
    
    Bug: 399642827, 453687625
    Change-Id: Ia7dddd8f652c9010cae852acb0e856bb7da2f45d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7074521
    Reviewed-by: Quyen Le <lehoangquyen@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1537365}

--- gpu/command_buffer/service/gpu_persistent_cache.h
+++ gpu/command_buffer/service/gpu_persistent_cache.h
@@ -8,12 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <dawn/platform/DawnPlatform.h>
 
+#include <atomic>
 #include <memory>
 #include <string_view>
 
 #include "base/memory/ref_counted.h"
-#include "base/synchronization/lock.h"
-#include "base/thread_annotations.h"
+#include "base/synchronization/atomic_flag.h"
 #include "components/persistent_cache/backend_params.h"
 #include "gpu/gpu_gles2_export.h"
 
@@ -34,6 +34,7 @@ class GPU_GLES2_EXPORT GpuPersistentCache
   GpuPersistentCache(const GpuPersistentCache&) = delete;
   GpuPersistentCache& operator=(const GpuPersistentCache&) = delete;
 
+  // This can only be called once but is thread safe w.r.t loads and stores.
   void InitializeCache(persistent_cache::BackendParams backend_params);
 
   // dawn::platform::CachingInterface implementation.
@@ -54,12 +55,12 @@ class GPU_GLES2_EXPORT GpuPersistentCache
   // Prefix to prepend to UMA histogram's name. e.g GraphiteDawn, WebGPU
   const std::string cache_prefix_;
 
-  size_t load_count_ = 0;
-  size_t store_count_ = 0;
+  std::atomic<size_t> load_count_ = 0;
+  std::atomic<size_t> store_count_ = 0;
 
-  base::Lock lock_;
-  std::unique_ptr<persistent_cache::PersistentCache> persistent_cache_
-      GUARDED_BY(lock_);
+  // `persistent_cache_` should only be accessed after `initialized_` is set.
+  base::AtomicFlag initialized_;
+  std::unique_ptr<persistent_cache::PersistentCache> persistent_cache_;
 };
 
 }  // namespace gpu
