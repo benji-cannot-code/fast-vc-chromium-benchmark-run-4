7b41aad1726842dd1c3be58cf3d44e85d5bbe7e3
Scott Haseley <shaseley@chromium.org>
2025-10-29 15:10:56 -0700

    PaintTiming: Flag-guard recent frame index initialization fix
    This adds a flag around a recent fix (crrev.com/c/7091357) to the frame
    index initialization for text paint timing records so we can run a
    holdback. In order to flag guard the fix, this moves setting the frame
    index out of QueueToMeasurePaintTime since we were doing this correctly
    for largest-ignore-image.
    
    Bug: 455791378
    Change-Id: Id518e348331576df8b486894cd706d4f0b5f694f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7097680
    Reviewed-by: Annie Sullivan <sullivan@chromium.org>
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Reviewed-by: Johannes Henkel <johannes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1537568}

--- third_party/blink/renderer/core/paint/timing/text_paint_timing_detector.cc
+++ third_party/blink/renderer/core/paint/timing/text_paint_timing_detector.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/feature_list.h"
 #include "third_party/blink/renderer/core/css/properties/longhands.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/local_frame_view.h"
@@ -20,6 +21,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
 namespace blink {
+
+namespace {
+BASE_FEATURE(kTextPaintTimingFrameIndexInitializationFix,
+             base::FEATURE_ENABLED_BY_DEFAULT);
+}  // namespace
 
 TextPaintTimingDetector::TextPaintTimingDetector(
     LocalFrameView* frame_view,
@@ -210,6 +216,9 @@ void TextPaintTimingDetector::ReportLargestIgnoredText() {
   DCHECK(document);
   PaintTiming::From(*document).MarkFirstContentfulPaint();
 
+  // TODO(crbug.com/455791378): Move this to `QueueToMeasurePaintTime` once
+  // `kTextPaintTimingFrameIndexInitializationFix` is removed.
+  record->SetFrameIndex(frame_index_);
   QueueToMeasurePaintTime(*record->GetNode()->GetLayoutObject(), record);
 }
 
@@ -324,6 +333,13 @@ TextRecord* TextPaintTimingDetector::MaybeRecordTextRecord(
             object, frame_visual_rect, property_tree_state, frame_view_),
         frame_visual_rect, root_visual_rect, is_needed_for_element_timing,
         context);
+  }
+
+  if (base::FeatureList::IsEnabled(
+          kTextPaintTimingFrameIndexInitializationFix)) {
+    // TODO(crbug.com/455791378): Move this to `QueueToMeasurePaintTime` once
+    // `kTextPaintTimingFrameIndexInitializationFix` is removed.
+    record->SetFrameIndex(frame_index_);
   }
   QueueToMeasurePaintTime(object, record);
   return record;
