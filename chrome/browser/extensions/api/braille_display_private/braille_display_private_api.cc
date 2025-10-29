2d066f69a9a1e11846ecc81a5ddcd490b1739f81
Antonio Gomes <tonikitoo@igalia.com>
2025-10-29 05:49:59 -0700

    Stop depending on //chrome/browser/ash/login/lock
    ... from //chrome/browser/extensions/api/braille_display_private.
    
    R=dljames@chromium.org
    
    Bug: 353332589
    Change-Id: I4a99581c97977d411e1533158067f9a18599692e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7091352
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1537265}

--- chrome/browser/extensions/api/braille_display_private/braille_display_private_api.cc
+++ chrome/browser/extensions/api/braille_display_private/braille_display_private_api.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_CHROMEOS)
 #include "chrome/browser/ash/accessibility/accessibility_manager.h"
-#include "chrome/browser/ash/login/lock/screen_locker.h"
 #include "extensions/browser/extensions_browser_client.h"
 #endif
 
