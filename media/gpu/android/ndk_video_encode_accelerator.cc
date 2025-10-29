cb3f90109b1e9ea1e46514515afb410a4f3ec8b1
Eugene Zemtsov <eugene@chromium.org>
2025-10-29 15:46:11 -0700

    Reland "media: Add UMAs for NDK VEA"
    This reverts commit c788952ea3b3d75477cdb9d69eb972ed30498fc1.
    
    Reason for revert: reverted by mistake as a suspected culprit 
    
    Original change's description:
    > Revert "media: Add UMAs for NDK VEA"
    >
    > This reverts commit 0d9d14704c1801d15f78b671761e4b9116974b1c.
    >
    > Reason for revert: Potential culprit for `external/wpt/mediacapture-record/passthrough/MediaRecorder-passthrough.https.html` failing 
    >
    > Original change's description:
    > > media: Add UMAs for NDK VEA
    > >
    > > Media.VideoEncoder.NDKVEA.EncodeStatus
    > > Media.VideoEncoder.NDKVEA.InitStatus
    > > Media.VideoEncoder.NDKVEA.EncodingLatency
    > >
    > > Bug: 455614300
    > > Change-Id: I76f9be481fbcccd83fd096798692b2534867fa70
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7089194
    > > Reviewed-by: Evan Liu <evliu@google.com>
    > > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > > Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1536957}
    >
    > Bug: 455614300
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I61414bf2f962724335a8944c2e98f866a67b6833
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7094917
    > Owners-Override: Julia Sobiech <jsobiech@google.com>
    > Reviewed-by: Piotr Kotynia <piotrkotynia@google.com>
    > Commit-Queue: Julia Sobiech <jsobiech@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1537168}
    
    Bug: 455614300
    Change-Id: Ibd933e337cbd4c3dc593c3f77bc5d3fa932f1ecd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7094978
    Reviewed-by: Evan Liu <evliu@google.com>
    Reviewed-by: Julia Sobiech <jsobiech@google.com>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1537615}

--- media/gpu/android/ndk_video_encode_accelerator.cc
+++ media/gpu/android/ndk_video_encode_accelerator.cc
@@ -8,9 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 
 #include "base/bits.h"
+#include "base/functional/callback_helpers.h"
 #include "base/logging.h"
 #include "base/memory/shared_memory_mapping.h"
 #include "base/memory/unsafe_shared_memory_region.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/notimplemented.h"
 #include "base/strings/stringprintf.h"
 #include "base/task/bind_post_task.h"
@@ -23,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/video_frame.h"
 #include "media/gpu/android/video_accelerator_util.h"
 #include "media/gpu/command_buffer_helper.h"
+#include "media/gpu/gpu_video_encode_accelerator_helpers.h"
 #include "media/parsers/h264_level_limits.h"
 #include "media/parsers/h264_parser.h"
 #include "media/parsers/temporal_scalability_id_extractor.h"
@@ -490,6 +493,24 @@ void WaitForSyncTokenOnGpuThread(
     base::OnceClosure done_cb) {
   command_buffer_helper->WaitForSyncToken(sync_token, std::move(done_cb));
 }
+
+constexpr std::string_view kEncoderStatusHistogramPrefix =
+    "Media.VideoEncoder.NDKVEA.EncodeStatus.";
+
+std::string GetEncoderStatusHistogramName(VideoCodecProfile profile) {
+  return base::StrCat(
+      {kEncoderStatusHistogramPrefix,
+       GetCodecNameForUMA(VideoCodecProfileToVideoCodec(profile))});
+}
+
+constexpr std::string_view kInitStatusHistogramPrefix =
+    "Media.VideoEncoder.NDKVEA.InitStatus.";
+
+std::string GetInitStatusHistogramName(VideoCodecProfile profile) {
+  return base::StrCat(
+      {kInitStatusHistogramPrefix,
+       GetCodecNameForUMA(VideoCodecProfileToVideoCodec(profile))});
+}
 }  // namespace
 
 NdkVideoEncodeAccelerator::PendingEncode::PendingEncode(
@@ -515,6 +536,12 @@ NdkVideoEncodeAccelerator::~NdkVideoEncodeAccelerator() {
   // It's supposed to be cleared by Destroy(), it basically checks
   // that we destroy `this` correctly.
   DCHECK(!media_codec_);
+
+  if (!error_occurred_ && have_encoded_frames_) {
+    base::UmaHistogramEnumeration(
+        GetEncoderStatusHistogramName(config_.output_profile),
+        EncoderStatus::Codes::kOk);
+  }
 }
 
 VideoEncodeAccelerator::SupportedProfiles
@@ -567,6 +594,10 @@ EncoderStatus NdkVideoEncodeAccelerator::Initialize(
   }
 
   const EncoderStatus status = ResetMediaCodec();
+
+  base::UmaHistogramEnumeration(
+      GetInitStatusHistogramName(config_.output_profile), status.code());
+
   if (!status.is_ok()) {
     return status;
   }
@@ -710,6 +741,7 @@ void NdkVideoEncodeAccelerator::Destroy() {
     media_codec_.reset();
   }
   gl_renderer_.reset();
+  metrics_helper_.reset();
   delete this;
 }
 
@@ -814,24 +846,26 @@ bool NdkVideoEncodeAccelerator::SetInputBufferLayout(
   return true;
 }
 
-base::TimeDelta NdkVideoEncodeAccelerator::AssignMonotonicTimestamp(
+base::TimeDelta NdkVideoEncodeAccelerator::RecordFrameTimestamps(
     base::TimeDelta real_timestamp) {
   base::TimeDelta step = base::Seconds(1) / effective_framerate_;
   auto result = next_timestamp_;
-  generated_to_real_timestamp_map_[result] = real_timestamp;
+  generated_to_real_timestamp_map_[result] = {real_timestamp,
+                                              base::TimeTicks::Now()};
   next_timestamp_ += step;
   return result;
 }
 
-base::TimeDelta NdkVideoEncodeAccelerator::RetrieveRealTimestamp(
+std::optional<NdkVideoEncodeAccelerator::FrameTimestampInfo>
+NdkVideoEncodeAccelerator::RetrieveFrameTimestamps(
     base::TimeDelta monotonic_timestamp) {
-  base::TimeDelta result;
   auto it = generated_to_real_timestamp_map_.find(monotonic_timestamp);
   if (it != generated_to_real_timestamp_map_.end()) {
-    result = it->second;
+    FrameTimestampInfo result = it->second;
     generated_to_real_timestamp_map_.erase(it);
+    return result;
   }
-  return result;
+  return std::nullopt;
 }
 
 void NdkVideoEncodeAccelerator::FeedInput() {
@@ -930,7 +964,7 @@ void NdkVideoEncodeAccelerator::FeedInput() {
   // monotonically increase according to the configured frame rate.
   // We do the opposite for each output buffer, to restore accurate frame
   // timestamps.
-  auto timestamp = AssignMonotonicTimestamp(frame->timestamp());
+  auto timestamp = RecordFrameTimestamps(frame->timestamp());
 
   if (use_surface_as_input_) {
     FeedGLSurface(std::move(frame), timestamp);
@@ -1087,6 +1121,9 @@ void NdkVideoEncodeAccelerator::NotifyErrorStatus(EncoderStatus status) {
   MEDIA_LOG(ERROR, log_) << EncoderStatusCodeToString(status.code()) << " "
                          << status.message();
   if (!error_occurred_) {
+    base::UmaHistogramEnumeration(
+        GetEncoderStatusHistogramName(config_.output_profile), status.code());
+
     task_runner_->PostTask(
         FROM_HERE,
         base::BindOnce(&VideoEncodeAccelerator::Client::NotifyErrorStatus,
@@ -1209,6 +1246,12 @@ void NdkVideoEncodeAccelerator::DrainOutput() {
     return;
   }
 
+  base::ScopedClosureRunner release_buffer(base::BindOnce(
+      [](NdkMediaCodecWrapper* media_codec, int index) {
+        AMediaCodec_releaseOutputBuffer(media_codec->codec(), index, false);
+      },
+      base::Unretained(media_codec_.get()), output_buffer.buffer_index));
+
   base::UnsafeSharedMemoryRegion region = bitstream_buffer.TakeRegion();
   auto mapping =
       region.MapAt(bitstream_buffer.offset(), bitstream_buffer.size());
@@ -1225,10 +1268,18 @@ void NdkVideoEncodeAccelerator::DrainOutput() {
   }
 
   output_dst.copy_prefix_from(out_buffer_data);
-  auto timestamp = RetrieveRealTimestamp(
+  auto timestamp_info = RetrieveFrameTimestamps(
       base::Microseconds(mc_buffer_info.presentationTimeUs));
-  auto metadata = BitstreamBufferMetadata(mc_buffer_size + config_size,
-                                          key_frame, timestamp);
+  if (!timestamp_info.has_value()) {
+    MEDIA_LOG(ERROR, log_) << "Failed to find timestamp for encoded frame. ts:"
+                           << mc_buffer_info.presentationTimeUs;
+    NOTREACHED(base::NotFatalUntil::M150)
+        << "Failed to find timestamp for encoded frame. ts:"
+        << mc_buffer_info.presentationTimeUs;
+    timestamp_info = FrameTimestampInfo();
+  }
+  auto metadata = BitstreamBufferMetadata(
+      mc_buffer_size + config_size, key_frame, timestamp_info->real_timestamp);
   if (aligned_size_) {
     metadata.encoded_size = aligned_size_;
   }
@@ -1261,13 +1312,15 @@ void NdkVideoEncodeAccelerator::DrainOutput() {
     ++input_since_keyframe_count_;
   }
 
+  auto encoding_latency =
+      base::TimeTicks::Now() - timestamp_info->encode_start_time;
+  metrics_helper_->EncodeOneFrame(key_frame, encoding_latency);
+
   task_runner_->PostTask(
       FROM_HERE,
       base::BindOnce(&VideoEncodeAccelerator::Client::BitstreamBufferReady,
                      client_ptr_factory_->GetWeakPtr(), bitstream_buffer.id(),
                      metadata));
-  AMediaCodec_releaseOutputBuffer(media_codec_->codec(),
-                                  output_buffer.buffer_index, false);
 }
 
 EncoderStatus NdkVideoEncodeAccelerator::ResetMediaCodec() {
@@ -1387,6 +1440,10 @@ EncoderStatus NdkVideoEncodeAccelerator::ResetMediaCodec() {
     MEDIA_LOG(ERROR, log_) << "Can't start media codec. Error " << status;
     return {EncoderStatus::Codes::kEncoderInitializationError};
   }
+
+  metrics_helper_ = std::make_unique<VEAEncodingLatencyMetricsHelper>(
+      "Media.VideoEncoder.NDKVEA.EncodingLatency.",
+      VideoCodecProfileToVideoCodec(config_.output_profile));
 
   MEDIA_LOG(INFO, log_) << "Created MediaCodec (" << name.value()
                         << ") for config: " << config_.AsHumanReadableString();
