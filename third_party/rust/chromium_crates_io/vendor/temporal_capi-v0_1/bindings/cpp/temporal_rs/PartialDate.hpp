49ec10e40ccf8a97310c96059fb88bee889d86a0
Julia Sobiech <jsobiech@google.com>
2025-10-29 04:46:10 -0700

    Revert "Update temporal_capi to 0.1.1"
    This reverts commit c7077a4cf80347d69b63052ceb1b4dbefa75d73f.
    
    Reason for revert: Potential culprit for Builder "chromium/ci/win32-official" failing at https://cr-buildbucket.appspot.com/build/8699679244960729281
    
    Original change's description:
    > Update temporal_capi to 0.1.1
    >
    > Depends-on: I469ee9c7f023d85b80041138b9120fba86315a29
    > Change-Id: I994d44fdb072f32be12c73f1645c09f7ff875f2e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7092641
    > Reviewed-by: Michael Achenbach <machenbach@chromium.org>
    > Auto-Submit: Manish Goregaokar <manishearth@google.com>
    > Commit-Queue: Michael Achenbach <machenbach@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1537153}
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I40d2aeab87c9c3933d7685f5946cafb1452b0b04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7092983
    Owners-Override: Julia Sobiech <jsobiech@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Julia Sobiech <jsobiech@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Julia Sobiech <jsobiech@google.com>
    Cr-Commit-Position: refs/heads/main@{#1537228}

--- third_party/rust/chromium_crates_io/vendor/temporal_capi-v0_1/bindings/cpp/temporal_rs/PartialDate.hpp
+++ third_party/rust/chromium_crates_io/vendor/temporal_capi-v0_1/bindings/cpp/temporal_rs/PartialDate.hpp
@@ -18,7 +18,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace temporal_rs {
 namespace capi {
+    extern "C" {
 
+    } // extern "C"
 } // namespace capi
 } // namespace
 
