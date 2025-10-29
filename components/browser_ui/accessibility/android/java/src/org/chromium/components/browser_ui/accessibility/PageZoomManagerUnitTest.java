bd5c00b090b8317e1c64b95fbc32e231743b39fc
Mark Schillaci <mschillaci@google.com>
2025-10-28 18:10:01 -0700

    [Desktop Zoom] Add a scale factor that can be tuned per platform/hardware
    This CL adds the basic functionality to have an additional scaling
    factor that can be included in the zoom calculation for a page. On
    newer devices with high resolution (e.g. Desktop devices or external
    monitors), the default size of the web is slightly smaller than ideal,
    so we need an ability to scale this up (the implementation also allows
    for down scaling). This capability already exists on the other platforms
    such as Windows, and it is included here for Android.
    
    Future CLs will continue the project to add a tunable parameter for
    the scaling factor to do user testing across a variety of scal factors
    and devices / form factors.
    
    Bug: 450281745
    Change-Id: Iccfe1209737119e6dd7f8d5235b1a8976dc8243b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7081043
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Hitarth Kothari <hitarthkothari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1537040}

--- components/browser_ui/accessibility/android/java/src/org/chromium/components/browser_ui/accessibility/PageZoomManagerUnitTest.java
+++ components/browser_ui/accessibility/android/java/src/org/chromium/components/browser_ui/accessibility/PageZoomManagerUnitTest.java
@@ -19,9 +19,13 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import org.chromium.base.test.BaseRobolectricTestRunner;
+import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.content.browser.HostZoomMapImpl;
 import org.chromium.content.browser.HostZoomMapImplJni;
 import org.chromium.content_public.browser.BrowserContextHandle;
+import org.chromium.content_public.browser.ContentFeatureList;
+import org.chromium.content_public.browser.ContentFeatureMap;
+import org.chromium.content_public.browser.ContentFeatureMapJni;
 import org.chromium.content_public.browser.HostZoomMap;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.ui.modelutil.PropertyModel;
@@ -29,6 +33,7 @@ import org.chromium.ui.modelutil.PropertyModel;
 /** Unit tests for {@link PageZoomManager}. */
 @SmallTest
 @RunWith(BaseRobolectricTestRunner.class)
+@DisableFeatures({ContentFeatureList.ANDROID_DESKTOP_ZOOM_SCALING})
 public class PageZoomManagerUnitTest {
     // Error messages
     private static final String CURRENT_ZOOM_FAILURE =
@@ -39,6 +44,7 @@ public class PageZoomManagerUnitTest {
             "Failure in increase zoom method. Expected 1 JNI call but none occurred.";
 
     @Mock private HostZoomMapImpl.Natives mHostZoomMapMock;
+    @Mock private ContentFeatureMap.Natives mContentFeatureListMapMock;
     @Mock private PageZoomManagerDelegate mPageZoomManagerDelegateMock;
     @Mock private WebContents mWebContentsMock;
     @Mock private BrowserContextHandle mBrowserContextHandleMock;
@@ -50,6 +56,7 @@ public class PageZoomManagerUnitTest {
     public void setUp() {
         MockitoAnnotations.initMocks(this);
         HostZoomMapImplJni.setInstanceForTesting(mHostZoomMapMock);
+        ContentFeatureMapJni.setInstanceForTesting(mContentFeatureListMapMock);
 
         when(mPageZoomManagerDelegateMock.getWebContents()).thenReturn(mWebContentsMock);
         when(mPageZoomManagerDelegateMock.getBrowserContextHandle())
