03ecc6a2203d06f1876f0131188f9d80c0636379
Slobodan Pejic <slobodan@chromium.org>
2025-10-29 03:34:56 -0700

    Add Glic API for form filling.
    This change adds a method to request an autofill selection dialog, and
    the response method that is invoked once the user has made a selection.
    One or more forms could be filled at once.
    
    For each form (FormFillingRequest) the following are provided:
    * A requested type of data, and
    * selections (a list of AutofillSuggestion) where each has
        * a primary title,
        * a secondary details text, and
        * an identifier that is not displayed to the user.
    
    For example, a list of addresses for the shipping form and a list
    of credit cards for the payment form should be shown as two selectors in
    the UI.
    
    The ChatUI responds with
    * a list of FormFillingResponse where each contains just
        * the suggestion identifier.
    
    We are anticipating future need for the ChatUI to return extra data per
    form. So, the SelectAutofillSuggestionsDialogResponse contains a list of
    FormFillingResponse instead of a list of suggestion ids.
    
    Bug: b:452063571
    
    Change-Id: I7d1d3bed3fae8c92f352f23c8db3831c08180681
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7081650
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Commit-Queue: Slobodan Pejic <slobodan@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1537193}

--- chrome/browser/glic/host/glic_page_handler.cc
+++ chrome/browser/glic/host/glic_page_handler.cc
@@ -79,6 +79,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/actor/task_id.h"
 #include "chrome/common/actor_webui.mojom.h"
 #include "chrome/common/chrome_features.h"
+#include "components/autofill/core/browser/integrators/glic/actor_form_filling_types.h"
 #include "components/content_settings/core/common/content_settings_types.h"
 #include "components/feedback/content/content_tracing_manager.h"
 #include "components/feedback/feedback_data.h"
@@ -699,6 +700,13 @@ class GlicWebClientHandler : public glic::mojom::WebClientHandler,
                     base::BindRepeating(
                         &GlicWebClientHandler::
                             RequestToShowCredentialSelectionDialog,
+                        base::Unretained(this)));
+        request_to_show_autofill_suggestions_dialog_subscription_ =
+            actor_service
+                ->AddRequestToShowAutofillSuggestionsDialogSubscriberCallback(
+                    base::BindRepeating(
+                        &GlicWebClientHandler::
+                            RequestToShowAutofillSuggestionsDialog,
                         base::Unretained(this)));
         request_to_show_user_confirmation_dialog_subscription_ =
             actor_service
@@ -1639,6 +1647,7 @@ class GlicWebClientHandler : public glic::mojom::WebClientHandler,
     pinned_tabs_changed_subscription_ = {};
     pinned_tab_data_changed_subscription_ = {};
     request_to_show_credential_selection_dialog_subscription_ = {};
+    request_to_show_autofill_suggestions_dialog_subscription_ = {};
     request_to_show_user_confirmation_dialog_subscription_ = {};
     request_to_confirm_navigation_subscription_ = {};
     browser_attach_observation_.reset();
@@ -1731,6 +1740,37 @@ class GlicWebClientHandler : public glic::mojom::WebClientHandler,
     web_client_->NotifyActorTaskStateChanged(task.id().value(), state);
   }
 
+  void RequestToShowAutofillSuggestionsDialog(
+      actor::TaskId task_id,
+      const std::vector<autofill::ActorFormFillingRequest>& requests,
+      actor::ActorKeyedService::AutofillSuggestionsSelectedCallback
+          on_autofill_suggestions_selected) {
+    std::vector<actor::webui::mojom::FormFillingRequestPtr> mojo_requests;
+    for (const auto& request : requests) {
+      auto mojo_request = actor::webui::mojom::FormFillingRequest::New();
+      mojo_request->requested_data =
+          static_cast<int64_t>(request.requested_data);
+      for (const auto& suggestion : request.suggestions) {
+        auto mojo_suggestion = actor::webui::mojom::AutofillSuggestion::New();
+        mojo_suggestion->id = suggestion.id;
+        mojo_suggestion->title = suggestion.title;
+        mojo_suggestion->details = suggestion.details;
+        if (suggestion.icon) {
+          mojo_suggestion->icon = suggestion.icon->AsBitmap();
+        }
+        mojo_request->suggestions.push_back(std::move(mojo_suggestion));
+      }
+      mojo_requests.push_back(std::move(mojo_request));
+    }
+
+    auto dialog_request =
+        actor::webui::mojom::SelectAutofillSuggestionsDialogRequest::New(
+            task_id.value(), std::move(mojo_requests));
+
+    web_client_->RequestToShowAutofillSuggestionsDialog(
+        std::move(dialog_request), std::move(on_autofill_suggestions_selected));
+  }
+
   void RequestToShowCredentialSelectionDialog(
       actor::TaskId task_id,
       const base::flat_map<std::string, gfx::Image>& icons,
@@ -1806,6 +1846,8 @@ class GlicWebClientHandler : public glic::mojom::WebClientHandler,
   base::CallbackListSubscription actor_task_state_changed_subscription_;
   base::CallbackListSubscription
       request_to_show_credential_selection_dialog_subscription_;
+  base::CallbackListSubscription
+      request_to_show_autofill_suggestions_dialog_subscription_;
   base::CallbackListSubscription
       request_to_show_user_confirmation_dialog_subscription_;
   base::CallbackListSubscription request_to_confirm_navigation_subscription_;
---
+++ components/autofill/core/browser/integrators/glic/actor_form_filling_types.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/autofill/core/browser/integrators/glic/actor_form_filling_types.h"
+
+namespace autofill {
+
+ActorSuggestion::ActorSuggestion() = default;
+ActorSuggestion::ActorSuggestion(const ActorSuggestion&) = default;
+ActorSuggestion& ActorSuggestion::operator=(const ActorSuggestion&) = default;
+ActorSuggestion::~ActorSuggestion() = default;
+
+ActorFormFillingRequest::ActorFormFillingRequest() = default;
+ActorFormFillingRequest::ActorFormFillingRequest(
+    const ActorFormFillingRequest&) = default;
+ActorFormFillingRequest& ActorFormFillingRequest::operator=(
+    const ActorFormFillingRequest&) = default;
+ActorFormFillingRequest::~ActorFormFillingRequest() = default;
+
+}  // namespace autofill
