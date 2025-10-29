54e3d3ef6d5ea6c7c1212b9b506e00d5d3a09882
Alexis Menard <alexis.menard@intel.com>
2025-10-29 14:50:15 -0700

    Reland webnn: Make WebNNContextImpl not ref counted.
    Fix: Some backends do not support multi-threading. Let's make sure we
    keep it that way by passing the right task runner.
    
    > webnn: Make WebNNContextImpl not ref counted.
    >
    > WebNNContextImpl is owned by WebNNContextProviderImpl and it's the only
    > object with a strong reference to it. Therefore, it doesn't need to be
    > ref counted. This change will make WebNNContextProviderImpl use
    > std::unique_ptr with a custom deleter to delete WebNNContextImpl on the
    > correct sequence.
    >
    > Bug: 449152887
    > Change-Id: Ic133a9a242352af2baf2b97713a8c799ccbebc1d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7083730
    > Reviewed-by: Hu, Ningxin <ningxin.hu@intel.com>
    > Reviewed-by: Reilly Grant <reillyg@chromium.org>
    > Commit-Queue: Menard, Alexis <alexis.menard@intel.com>
    > Cr-Commit-Position: refs/heads/main@{#1537458}
    
    Bug: 449152887
    Change-Id: I0d7e3ddbe3c31a604235db6108d6a297c2befdb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7097765
    Commit-Queue: Menard, Alexis <alexis.menard@intel.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1537547}

--- services/webnn/webnn_context_impl.cc
+++ services/webnn/webnn_context_impl.cc
@@ -38,6 +38,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace webnn {
 
+WebNNContextImpl::TaskRunnerDeleter::TaskRunnerDeleter(
+    scoped_refptr<base::SequencedTaskRunner> task_runner)
+    : task_runner_(std::move(task_runner)) {}
+
+WebNNContextImpl::TaskRunnerDeleter::~TaskRunnerDeleter() = default;
+
+WebNNContextImpl::TaskRunnerDeleter::TaskRunnerDeleter(
+    WebNNContextImpl::TaskRunnerDeleter&&) = default;
+
+WebNNContextImpl::TaskRunnerDeleter&
+WebNNContextImpl::TaskRunnerDeleter::operator=(
+    WebNNContextImpl::TaskRunnerDeleter&&) = default;
+
 WebNNContextImpl::WebNNContextImpl(
     mojo::PendingReceiver<mojom::WebNNContext> receiver,
     base::WeakPtr<WebNNContextProviderImpl> context_provider,
@@ -51,12 +64,11 @@ WebNNContextImpl::WebNNContextImpl(
     scoped_refptr<base::SingleThreadTaskRunner> owning_task_runner,
     gpu::SharedImageManager* shared_image_manager,
     scoped_refptr<base::SingleThreadTaskRunner> main_task_runner)
-    : WebNNObjectImpl<mojom::WebNNContext,
+    : WebNNObjectBase<mojom::WebNNContext,
                       blink::WebNNContextToken,
                       mojo::Receiver<mojom::WebNNContext>>(
           std::move(receiver),
-          sequence->scheduler_task_runner(),
-          std::move(owning_task_runner)),
+          sequence->scheduler_task_runner()),
       context_provider_(std::move(context_provider)),
       properties_(IntersectWithBaseProperties(std::move(properties))),
       options_(std::move(options)),
@@ -66,7 +78,8 @@ WebNNContextImpl::WebNNContextImpl(
       read_tensor_producer_(std::move(read_tensor_producer)),
       memory_type_tracker_(std::move(memory_tracker)),
       shared_image_manager_(shared_image_manager),
-      main_task_runner_(std::move(main_task_runner)) {
+      main_task_runner_(std::move(main_task_runner)),
+      owning_task_runner_(std::move(owning_task_runner)) {
 #if BUILDFLAG(BUILD_TFLITE_WITH_XNNPACK)
   // Initialize XNNPACK
   const xnn_status status = xnn_initialize(/*allocator=*/nullptr);
