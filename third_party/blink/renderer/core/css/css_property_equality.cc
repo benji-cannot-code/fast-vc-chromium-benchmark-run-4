c93349818d2216df7bd4c85a7f1412b8ec6ff121
Kent Tamura <tkent@chromium.org>
2025-10-29 15:09:31 -0700

    text-justify: Do not generate an enum type
    We should stop generating an enum, and use a hand-made enum because:
    
    * A part of justification code is in renderer/platform/.
    
    * We need to accept `distribute` alias
      https://drafts.csswg.org/css-text-3/#valdef-text-justify-distribute
      > UAs must also support the alternate keyword distribute which must
      > compute to `inter-character`
    
      This CL adds support of `distribute` parsing.
    
    Bug: 40321528
    Change-Id: I6855ff31ceb7de5fc78013dc1d5473927bd512c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7089851
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1537567}

--- third_party/blink/renderer/core/css/css_property_equality.cc
+++ third_party/blink/renderer/core/css/css_property_equality.cc
@@ -758,7 +758,7 @@ bool CSSPropertyEquality::PropertiesEqual(const PropertyHandle& property,
     case CSSPropertyID::kTextIndent:
       return a.TextIndent() == b.TextIndent();
     case CSSPropertyID::kTextJustify:
-      return a.TextJustify() == b.TextJustify();
+      return a.GetTextJustify() == b.GetTextJustify();
     case CSSPropertyID::kTextOverflow:
       return a.TextOverflow() == b.TextOverflow();
     case CSSPropertyID::kTextRendering:
