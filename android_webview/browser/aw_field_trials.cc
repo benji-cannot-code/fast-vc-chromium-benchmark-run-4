c444ee3652bf8ecbaf786d82ab4d9560c71da287
Oksana Zhuravlova <oksamyt@chromium.org>
2025-10-29 08:47:22 -0700

    Enable SendEmptyGestureScrollUpdate by default on Android
    This change enables the SendEmptyGestureScrollUpdate feature by default
    on Android except WebView. The experiment on WebView will be rolled
    further and the feature will be enabled by default on WebView after the
    experiment has concluded on Webview.
    
    The feature is being launched on Android based on 1% stable results
    (http://b/418042038#comment9). The enabled group shows improvements in
    scroll jank metrics and no guardrail metrics regressions.
    
    Bug: 402144902
    Change-Id: I5f60be0a8412666eb2e82a8d8dd79784f2b8f032
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7091094
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1537363}

--- android_webview/browser/aw_field_trials.cc
+++ android_webview/browser/aw_field_trials.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/common/features_generated.h"
 #include "ui/android/ui_android_features.h"
+#include "ui/base/ui_base_features.h"
 #include "ui/gl/gl_features.h"
 #include "ui/gl/gl_switches.h"
 
@@ -113,6 +114,11 @@ void AwFieldTrials::RegisterFeatureOverrides(base::FeatureList* feature_list) {
 
   // Disable scrollbar-width on WebView.
   aw_feature_overrides.DisableFeature(blink::features::kScrollbarWidth);
+
+  // TODO(crbug.com/402144902): Remove this once webview experiment has
+  // concluded.
+  aw_feature_overrides.DisableFeature(
+      ::features::kSendEmptyGestureScrollUpdate);
 
   // Disable Populating the VisitedLinkDatabase on WebView.
   aw_feature_overrides.DisableFeature(history::kPopulateVisitedLinkDatabase);
