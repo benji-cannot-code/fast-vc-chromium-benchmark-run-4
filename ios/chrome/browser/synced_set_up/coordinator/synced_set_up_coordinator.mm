625db58f95af6f5f2946b95a41c8d2a120ce26ce
Benjamin Williams <bwwilliams@google.com>
2025-10-29 03:32:32 -0700

    [iOS][SyncedSetUp] Add custom animations for Synced Set Up interstitial
    This change introduces custom animations for the presentation and
    dismissal of the Synced Set Up welcome interstitial.
    
    This is achieved by creating a custom animator that, internally, uses
    TabsClosureAnimation.
    
    In a follow-up CL, TabsClosureAnimation will be refactored and renamed
    to a more generic name and moved to a more appropriate location in the
    codebase.
    
    Demo video:
    https://drive.google.com/file/d/16_WqK8e3JKbcxuQHHrmnIDXj35gDVI5A/view?usp=sharing&resourcekey=0-_QjIj9eKEbp0Q6E1q8f1Sg
    
    Change-Id: I6fd41a0f4b7a5eee0f4fa06a0b08a9f32f06cf8a
    Fixed: 455813524
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7091371
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1537189}

--- ios/chrome/browser/synced_set_up/coordinator/synced_set_up_coordinator.mm
+++ ios/chrome/browser/synced_set_up/coordinator/synced_set_up_coordinator.mm
@@ -5,7 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/synced_set_up/coordinator/synced_set_up_coordinator.h"
 
+#import "base/functional/bind.h"
 #import "base/not_fatal_until.h"
+#import "base/task/sequenced_task_runner.h"
+#import "base/time/time.h"
 #import "components/prefs/pref_service.h"
 #import "components/sync_device_info/device_info_sync_service.h"
 #import "ios/chrome/app/app_startup_parameters.h"
@@ -19,8 +22,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/synced_set_up/coordinator/synced_set_up_coordinator_delegate.h"
 #import "ios/chrome/browser/synced_set_up/coordinator/synced_set_up_mediator.h"
 #import "ios/chrome/browser/synced_set_up/coordinator/synced_set_up_mediator_delegate.h"
+#import "ios/chrome/browser/synced_set_up/ui/synced_set_up_animator.h"
+#import "ios/chrome/browser/synced_set_up/ui/synced_set_up_view_controller.h"
 
-@interface SyncedSetUpCoordinator () <SyncedSetUpMediatorDelegate>
+namespace {
+
+// Time delay before automatically dismissing the Synced Set Up interstitial.
+constexpr base::TimeDelta kDismissalDelay = base::Seconds(5);
+
+}  // namespace
+
+@interface SyncedSetUpCoordinator () <SyncedSetUpMediatorDelegate,
+                                      UIViewControllerTransitioningDelegate>
 @end
 
 @implementation SyncedSetUpCoordinator {
@@ -29,7 +42,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   AppStartupParameters* _startupParameters;
   // Mediator for Synced Set Up. Used to determine and apply a set of synced
   // prefs to the local device.
-  SyncedSetUpMediator* _syncedSetUpMediator;
+  SyncedSetUpMediator* _mediator;
+  // View controller responsible for displaying the Synced Set Up interstitial.
+  SyncedSetUpViewController* _viewController;
 }
 
 - (instancetype)initWithBaseViewController:(UIViewController*)viewController
@@ -47,8 +62,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark - ChromeCoordinator
 
 - (void)start {
-  // Create the `SyncedSetUpMediator` to query and apply synced prefs.
   ProfileIOS* profile = self.profile;
+
   sync_preferences::CrossDevicePrefTracker* tracker =
       CrossDevicePrefTrackerFactory::GetForProfile(profile);
   AuthenticationService* authService =
@@ -60,7 +75,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   signin::IdentityManager* identityManager =
       IdentityManagerFactory::GetForProfile(profile);
 
-  _syncedSetUpMediator =
+  _mediator =
       [[SyncedSetUpMediator alloc] initWithPrefTracker:tracker
                                  authenticationService:authService
                                  accountManagerService:accountManagerService
@@ -68,22 +83,87 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                                     profilePrefService:profile->GetPrefs()
                                      startupParameters:_startupParameters
                                        identityManager:identityManager];
-  _syncedSetUpMediator.delegate = self;
+  _mediator.delegate = self;
+
+  _viewController = [[SyncedSetUpViewController alloc] init];
+  _viewController.modalPresentationStyle = UIModalPresentationCustom;
+  _viewController.transitioningDelegate = self;
+
+  _mediator.consumer = _viewController;
 }
 
 - (void)stop {
-  _syncedSetUpMediator.delegate = nil;
-  _syncedSetUpMediator.consumer = nil;
-  [_syncedSetUpMediator disconnect];
-  _syncedSetUpMediator = nil;
+  if (!_viewController.isBeingDismissed) {
+    [_viewController.presentingViewController
+        dismissViewControllerAnimated:YES
+                           completion:nil];
+  }
+
+  _mediator.delegate = nil;
+  _mediator.consumer = nil;
+  [_mediator disconnect];
+  _mediator = nil;
+  _viewController = nil;
   _startupParameters = nil;
 }
 
 #pragma mark - SyncedSetUpMediatorDelegate
 
 - (void)syncedSetUpMediatorDidComplete:(SyncedSetUpMediator*)mediator {
-  CHECK_EQ(_syncedSetUpMediator, mediator);
-  [self.delegate syncedSetUpCoordinatorWantsToBeDismissed:self];
+  CHECK_EQ(_mediator, mediator);
+  [self dismissSyncedSetUp];
+}
+
+#pragma mark - UIViewControllerTransitioningDelegate
+
+// Called when the view controller is being presented.
+- (id<UIViewControllerAnimatedTransitioning>)
+    animationControllerForPresentedController:(UIViewController*)presented
+                         presentingController:(UIViewController*)presenting
+                             sourceController:(UIViewController*)source {
+  return [[SyncedSetUpAnimator alloc] initForPresenting:YES];
+}
+
+// Called when the view controller is being dismissed.
+- (id<UIViewControllerAnimatedTransitioning>)
+    animationControllerForDismissedController:(UIViewController*)dismissed {
+  return [[SyncedSetUpAnimator alloc] initForPresenting:NO];
+}
+
+#pragma mark - Private Methods
+
+// Shows the full-screen welcome interstitial for the Synced Set Up flow.
+- (void)showSyncedSetUpInterstitial {
+  if (!_viewController || _viewController.presentingViewController) {
+    return;
+  }
+
+  [self.baseViewController presentViewController:_viewController
+                                        animated:YES
+                                      completion:nil];
+
+  __weak __typeof(self) weakSelf = self;
+  base::SequencedTaskRunner::GetCurrentDefault()->PostDelayedTask(
+      FROM_HERE, base::BindOnce(^{
+        [weakSelf dismissSyncedSetUp];
+      }),
+      kDismissalDelay);
+}
+
+// Dismisses the full-screen interstitial.
+- (void)dismissSyncedSetUp {
+  if (_viewController.isBeingDismissed) {
+    return;
+  }
+
+  __weak __typeof(self) weakSelf = self;
+  [_viewController.presentingViewController
+      dismissViewControllerAnimated:YES
+                         completion:^{
+                           [weakSelf.delegate
+                               syncedSetUpCoordinatorWantsToBeDismissed:
+                                   weakSelf];
+                         }];
 }
 
 @end
