96f76f87f3db87a497205b767266e21f22d7d7a6
Danil Chapovalov <danilchap@chromium.org>
2025-10-29 13:08:18 -0700

    Roll abseil_revision 215d8a0e75..aed1a5e5f7
    Change Log:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+log/215d8a0e75..aed1a5e5f7
    Full diff:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+/215d8a0e75..aed1a5e5f7
    
    Bug: None
    Change-Id: Icff820fb1fcd51981bcbb459d93926f284cf5b2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7094923
    Auto-Submit: Danil Chapovalov <danilchap@chromium.org>
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1537500}

--- third_party/abseil-cpp/absl/strings/internal/escaping.h
+++ third_party/abseil-cpp/absl/strings/internal/escaping.h
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <cassert>
 
-#include "absl/strings/internal/resize_uninitialized.h"
+#include "absl/strings/resize_and_overwrite.h"
 
 namespace absl {
 ABSL_NAMESPACE_BEGIN
@@ -43,12 +43,14 @@ void Base64EscapeInternal(const unsigned char* src, size_t szsrc, String* dest,
                           bool do_padding, const char* base64_chars) {
   const size_t calc_escaped_size =
       CalculateBase64EscapedLenInternal(szsrc, do_padding);
-  STLStringResizeUninitialized(dest, calc_escaped_size);
-
-  const size_t escaped_len = Base64EscapeInternal(
-      src, szsrc, &(*dest)[0], dest->size(), base64_chars, do_padding);
-  assert(calc_escaped_size == escaped_len);
-  dest->erase(escaped_len);
+  StringResizeAndOverwrite(
+      *dest, calc_escaped_size,
+      [src, szsrc, base64_chars, do_padding](char* buf, size_t buf_size) {
+        const size_t escaped_len = Base64EscapeInternal(
+            src, szsrc, buf, buf_size, base64_chars, do_padding);
+        assert(escaped_len == buf_size);
+        return escaped_len;
+      });
 }
 
 }  // namespace strings_internal
