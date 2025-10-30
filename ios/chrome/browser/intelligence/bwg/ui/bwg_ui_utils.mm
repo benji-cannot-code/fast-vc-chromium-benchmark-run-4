a011687fa2f20a15c06312621716d741b9b37e0a
Joemer Ramos <joemerramos@google.com>
2025-10-30 09:10:34 -0700

    [iOS] Add Contextual Cueing Chip Unit Test For BWGTabHelper
    Adds test to test sending badge update commands to location bar badge.
    This effectively allows BWGTabHelper to send any type of contextual
    chip.
    
    Fixed: 455578470
    Change-Id: Ia4fad4c33205a0f5f0dbe58f08598d298fad2fff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7085655
    Commit-Queue: Joemer Ramos <joemerramos@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1537979}

--- ios/chrome/browser/intelligence/bwg/ui/bwg_ui_utils.mm
+++ ios/chrome/browser/intelligence/bwg/ui/bwg_ui_utils.mm
@@ -48,7 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IOS_USE_BRANDED_SYMBOLS)
   return CustomSymbolWithPointSize(kGeminiBrandedLogoImage, pointSize);
 #else
-  return CustomSymbolWithPointSize(kGeminiNonBrandedLogoImage, pointSize);
+  return DefaultSymbolWithPointSize(kGeminiNonBrandedLogoImage, pointSize);
 #endif
 }
 
