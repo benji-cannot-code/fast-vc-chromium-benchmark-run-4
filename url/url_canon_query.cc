d7d1ba0b6ec9d284a6eeb8c541a91a88c650616b
Sergio Solano <sergiosolano@google.com>
2025-10-29 20:57:14 -0700

    Refactor: Modernize AppendRaw8BitQueryString with base::span instead of raw pointers management.
    UNSAFE_TODO macros have been removed.
    
    
    spanification: automatically spanify url/url_canon_query.cc etc.
    
    This is the result of running the automatic spanification on linux and
    updating code to use and pass spans where size is known.
    
    The original patch was fully automated using script:
    //tools/clang/spanify/rewrite-multiple-platforms.sh -platforms=linux
    Then refined with gemini-cli
    
    gemini-run/batch-run-1758857589/group_170
    
    BUG=439964610
    
    Change-Id: I0c69ebbf2cbb0aa04c6fbf0be721b9fdd25db0af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7056349
    Auto-Submit: Sergio Solano <sergiosolano@google.com>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1537725}

--- url/url_canon_query.cc
+++ url/url_canon_query.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 
 #include "base/compiler_specific.h"
+#include "base/containers/span.h"
 #include "url/url_canon.h"
 #include "url/url_canon_internal.h"
 
@@ -47,15 +48,15 @@ namespace {
 // match the given |type| in SharedCharTypes. This version will accept 8 or 16
 // bit characters, but assumes that they have only 7-bit values. It also assumes
 // that all UTF-8 values are correct, so doesn't bother checking
-template<typename CHAR>
-void AppendRaw8BitQueryString(const CHAR* source, int length,
+template <typename CHAR>
+void AppendRaw8BitQueryString(std::basic_string_view<CHAR> source,
                               CanonOutput* output) {
-  for (int i = 0; i < length; i++) {
-    if (!IsQueryChar(static_cast<unsigned char>(UNSAFE_TODO(source[i])))) {
-      AppendEscapedChar(static_cast<unsigned char>(UNSAFE_TODO(source[i])),
-                        output);
+  for (const CHAR& c : source) {
+    unsigned char uc = static_cast<unsigned char>(c);
+    if (!IsQueryChar(uc)) {
+      AppendEscapedChar(uc, output);
     } else {  // Doesn't need escaping.
-      output->push_back(static_cast<char>(UNSAFE_TODO(source[i])));
+      output->push_back(static_cast<char>(uc));
     }
   }
 }
@@ -90,7 +91,7 @@ void DoConvertToQueryEncoding(std::basic_string_view<CHAR> input,
     // necessary values.
     RawCanonOutput<1024> eight_bit;
     RunConverter(input, converter, &eight_bit);
-    AppendRaw8BitQueryString(eight_bit.data(), eight_bit.length(), output);
+    AppendRaw8BitQueryString(eight_bit.view(), output);
 
   } else {
     // No converter, do our own UTF-8 conversion.
