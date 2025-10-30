e161febad00cab291ae1f35e1c307eff2289502f
Alison Gale <agale@chromium.org>
2025-10-30 13:51:48 -0700

    Improve bookmark drag/drop handling in tree view
    Together these changes fix a bug where when dragging a folder inside a
    folder and immediately dropping it, it would go into the fallback rather
    than having the drag cancelled.
    
    - Fix hasActiveDrag change detection. Noticed it wasn't updating
    - Don't cancel drags when bookmarkRowElement is undefined. Instead use the fallback because this means it is being dropped into the top level folder.
    - Cancel drops into a parent or self
    
    Bug: 454946612
    Change-Id: Id4d1177b4d4806c9f20e5c7c718f99546b3c00b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7089168
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1538164}

--- chrome/browser/resources/side_panel/bookmarks/power_bookmarks_drag_manager.ts
+++ chrome/browser/resources/side_panel/bookmarks/power_bookmarks_drag_manager.ts
@@ -58,6 +58,7 @@ interface PowerBookmarksDragDelegate extends HTMLElement {
   getFallbackBookmark(): BookmarksTreeNode;
   getFallbackDropTargetElement(): HTMLElement;
   onFinishDrop(dropTarget: BookmarksTreeNode): void;
+  setHasActiveDrag(hasActiveDrag: boolean): void;
 }
 
 class DragSession {
@@ -87,7 +88,10 @@ class DragSession {
       return target instanceof PowerBookmarkRowElement && !target.bookmark.url;
     }) as PowerBookmarkRowElement;
 
-    if (!bookmarkRowElement) {
+    const isTreeViewEnabled =
+        loadTimeData.getBoolean('bookmarksTreeViewEnabled');
+
+    if (!isTreeViewEnabled && !bookmarkRowElement) {
       // Invalid drag over element. Cancel session.
       this.cancel();
       return;
@@ -98,24 +102,32 @@ class DragSession {
 
     this.resetState_();
 
-    let dropTargetBookmark = bookmarkRowElement.bookmark;
+    let dropTargetBookmark: BookmarksTreeNode = bookmarkRowElement?.bookmark;
     let dragOverElement: HTMLElement = bookmarkRowElement;
 
-    const invalidDropTarget = dropTargetBookmark.unmodifiable ||
-        dropTargetBookmark.url ||
-        (this.dragData_.elements &&
-         this.dragData_.elements.some(
-             element => element.id === dropTargetBookmark.id));
-    if (invalidDropTarget) {
-      dropTargetBookmark = this.delegate_.getFallbackBookmark();
-      dragOverElement = this.delegate_.getFallbackDropTargetElement();
+    if (isTreeViewEnabled) {
+      if (!bookmarkRowElement || bookmarkRowElement.bookmark.unmodifiable) {
+        dropTargetBookmark = this.delegate_.getFallbackBookmark();
+        dragOverElement = this.delegate_.getFallbackDropTargetElement();
+      }
+    } else {
+      const invalidDropTarget = dropTargetBookmark.unmodifiable ||
+          dropTargetBookmark.url ||
+          (this.dragData_.elements &&
+           this.dragData_.elements.some(
+               element => element.id === dropTargetBookmark.id));
+      if (invalidDropTarget) {
+        dropTargetBookmark = this.delegate_.getFallbackBookmark();
+        dragOverElement = this.delegate_.getFallbackDropTargetElement();
+      }
     }
 
     const draggedBookmarks = this.dragData_.elements!;
-    const dropTargetIsParent = !draggedBookmarks.some(
+    const dropTargetIsParentOrSelf = !draggedBookmarks.some(
         (bookmark: chrome.bookmarks.BookmarkTreeNode) =>
-            bookmark.parentId !== dropTargetBookmark.id);
-    if (draggedBookmarks.length === 0 || dropTargetIsParent) {
+            bookmark.parentId !== dropTargetBookmark.id &&
+            bookmark.id !== dropTargetBookmark.id);
+    if (draggedBookmarks.length === 0 || dropTargetIsParentOrSelf) {
       this.cancel();
       return;
     }
@@ -149,8 +161,6 @@ class DragSession {
     if (this.lastDragOverElement_) {
       this.lastDragOverElement_.removeAttribute(DROP_POSITION_ATTR);
     }
-    this.delegate_.getFallbackDropTargetElement().removeAttribute(
-        DROP_POSITION_ATTR);
   }
 
   static createFromBookmark(
@@ -215,6 +225,7 @@ export class PowerBookmarksDragManager {
     }
     this.dragSession_.cancel();
     this.dragSession_ = null;
+    this.delegate_.setHasActiveDrag(false);
   }
 
   private onChromeDragEnter_(dragData: chrome.bookmarkManagerPrivate.DragData) {
@@ -245,6 +256,7 @@ export class PowerBookmarksDragManager {
     this.dragSession_ =
         DragSession.createFromBookmark(this.delegate_, bookmark);
     this.dragSession_.start(e);
+    this.delegate_.setHasActiveDrag(true);
   }
 
   private onDragOver_(e: DragEvent) {
@@ -271,5 +283,6 @@ export class PowerBookmarksDragManager {
     e.preventDefault();
     this.dragSession_.finish();
     this.dragSession_ = null;
+    this.delegate_.setHasActiveDrag(false);
   }
 }
