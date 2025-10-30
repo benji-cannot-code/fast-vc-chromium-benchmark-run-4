c5a3b91dcea0f896e341193bc756389ef226cd18
Ewann Pelle <ewannpv@chromium.org>
2025-10-30 03:27:01 -0700

    [iOS] Configure ButtonStack in Settings UI
    This change refactors the Settings UI to use the new ButtonStackViewController.
    
    Bug: 444648926
    Change-Id: Ifa2c6ac8cc0da776daf1ce0e22ba5c5dbac4a1a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7079443
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1537825}

--- ios/chrome/browser/settings/ui_bundled/clear_browsing_data/quick_delete_view_controller.mm
+++ ios/chrome/browser/settings/ui_bundled/clear_browsing_data/quick_delete_view_controller.mm
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/ui/table_view/content_configuration/table_view_cell_content_configuration.h"
 #import "ios/chrome/browser/shared/ui/table_view/table_view_utils.h"
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
+#import "ios/chrome/common/ui/button_stack/button_stack_configuration.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/confirmation_alert/confirmation_alert_action_handler.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
@@ -131,10 +132,11 @@ typedef NS_ENUM(NSInteger, ItemIdentifier) {
   self.titleTextStyle = UIFontTextStyleTitle2;
   self.titleString = l10n_util::GetNSString(IDS_IOS_CLEAR_BROWSING_DATA_TITLE);
   self.customSpacing = kTitleVerticalPadding;
-  self.primaryActionString =
+  self.configuration.primaryActionString =
       l10n_util::GetNSString(IDS_IOS_DELETE_BROWSING_DATA_BUTTON);
-  self.secondaryActionString =
+  self.configuration.secondaryActionString =
       l10n_util::GetNSString(IDS_IOS_DELETE_BROWSING_DATA_CANCEL);
+  self.configuration.primaryButtonStyle = ChromeButtonStylePrimaryDestructive;
 
   self.underTitleView = _tableView;
 
@@ -143,7 +145,6 @@ typedef NS_ENUM(NSInteger, ItemIdentifier) {
   self.topAlignedLayout = YES;
   self.customContentBottomInset = 0;
   self.actionHandler = self;
-  self.destructiveAction = YES;
 
   [super viewDidLoad];
 
@@ -375,8 +376,7 @@ typedef NS_ENUM(NSInteger, ItemIdentifier) {
 - (void)deletionInProgress {
   self.primaryActionButton.accessibilityLabel =
       l10n_util::GetNSString(IDS_IOS_DELETE_BROWSING_DATA_IN_PROGRESS_NOTICE);
-  self.isLoading = YES;
-  self.isConfirmed = NO;
+  [self setLoading:YES];
 
   self.view.window.userInteractionEnabled = NO;
   // Disable accessibility elements on entire window to avoid Voiceover focusing
@@ -393,8 +393,8 @@ typedef NS_ENUM(NSInteger, ItemIdentifier) {
   self.view.window.userInteractionEnabled = YES;
   self.view.window.accessibilityElementsHidden = NO;
 
-  self.isLoading = NO;
-  self.isConfirmed = YES;
+  [self setLoading:NO];
+  [self setConfirmed:YES];
   TriggerHapticFeedbackForNotification(UINotificationFeedbackTypeSuccess);
 
   // If Voiceover is enabled, inform users that their browsing data has been
--- ios/chrome/browser/settings/ui_bundled/password/widget_promo_instructions/widget_promo_instructions_view_controller.mm
+++ ios/chrome/browser/settings/ui_bundled/password/widget_promo_instructions/widget_promo_instructions_view_controller.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/strings/grit/components_strings.h"
 #import "ios/chrome/browser/settings/ui_bundled/password/widget_promo_instructions/widget_promo_instructions_constants.h"
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
+#import "ios/chrome/common/ui/button_stack/button_stack_configuration.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/confirmation_alert/confirmation_alert_view_controller.h"
 #import "ios/chrome/common/ui/instruction_view/instruction_view.h"
@@ -92,7 +93,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   instructionsViewController.subtitleString =
       l10n_util::GetNSString(IDS_IOS_WIDGET_PROMO_INSTRUCTIONS_SUBTITLE);
   instructionsViewController.subtitleTextStyle = UIFontTextStyleBody;
-  instructionsViewController.secondaryActionString =
+  instructionsViewController.configuration.secondaryActionString =
       l10n_util::GetNSString(IDS_CLOSE);
   instructionsViewController.showDismissBarButton = NO;
   instructionsViewController.topAlignedLayout = YES;
