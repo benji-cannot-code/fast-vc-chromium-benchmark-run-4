9b6603b538b021521013a937daccbec5bdf484fc
Quentin Pubert <qpubert@google.com>
2025-10-30 06:43:45 -0700

    [iOS] Clean-up DriveFileUpload::FetchSaveToDriveClientFolder
    This CL makes the method pure virtual since all subclasses now correctly
    override it.
    
    Bug: 452543744
    Change-Id: I00ad39024dd1c4c8011136ddcecc4e23ead197ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7098179
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Auto-Submit: Quentin Pubert <qpubert@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1537883}

--- ios/chrome/browser/drive/model/drive_file_uploader.mm
+++ ios/chrome/browser/drive/model/drive_file_uploader.mm
@@ -10,9 +10,3 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 DriveFileUploader::DriveFileUploader() = default;
 
 DriveFileUploader::~DriveFileUploader() = default;
-
-void DriveFileUploader::FetchSaveToDriveClientFolder(
-    NSString* folder_name,
-    DriveFolderCompletionCallback completion_callback) {
-  NOTIMPLEMENTED();
-}
