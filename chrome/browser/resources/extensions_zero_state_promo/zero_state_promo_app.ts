66b9d0c9463eccc15471b5e789720500581bdf4a
Howard Chang <howardcha@google.com>
2025-10-30 13:35:04 -0700

    Add cws zero state iph variation 3
    This cl is similar to https://chromium-review.googlesource.com/c/chromium/src/+/7084950
    
    Demo: https://screenshot.googleplex.com/Bw3Mfz9CxjRaJnz
    
    Bug: 409573170
    Change-Id: I8545e2674ec70d26ac99e241ed38ad9a972ff320
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7092648
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: Howard Chang <howardcha@google.com>
    Cr-Commit-Position: refs/heads/main@{#1538136}

--- chrome/browser/resources/extensions_zero_state_promo/zero_state_promo_app.ts
+++ chrome/browser/resources/extensions_zero_state_promo/zero_state_promo_app.ts
@@ -40,6 +40,7 @@ export class ZeroStatePromoAppElement extends CrLitElement {
       showPlainLinksUi_: {type: Boolean},
       showChipsV1_: {type: Boolean},
       showChipsV2_: {type: Boolean},
+      showChipsV3_: {type: Boolean},
     };
   }
 
@@ -56,6 +57,9 @@ export class ZeroStatePromoAppElement extends CrLitElement {
 
   protected accessor showChipsV2_: boolean =
       loadTimeData.getBoolean('showChipsUiV2');
+
+  protected accessor showChipsV3_: boolean =
+      loadTimeData.getBoolean('showChipsUiV3');
 
   protected onChromeWebStoreButtonClick_() {
     this.apiProxy_.launchWebStoreLink(WebStoreLinkClicked.kDiscoverExtension);
