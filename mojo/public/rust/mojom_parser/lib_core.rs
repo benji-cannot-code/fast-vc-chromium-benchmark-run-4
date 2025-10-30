acc6f6ce94b78c5976d222282844bf1e05daf294
Devon Loehr <dloehr@google.com>
2025-10-30 13:17:25 -0700

    [rustmojo] Start working on generating code for mojom parsing
    This CL lays the groundwork for hooking actual rust types up to the
    parser. It primarily does two things:
    1. Defines a trait for a rust type that can be (de)serialized to a
    mojom message, and implements it for u8 as an example.
    2. Sets up the build system so we can start writing a proc macro to
    derive the type automatically.
    
    In followup CLs, we'll implement the trait for the rest of the
    primitive types, write the proc macro, and then provide a
    parsing/deparsing API that accepts and returns objects with that
    trait, instead of only Mojom ASTs.
    
    Bug: 444509367
    Change-Id: I67908f891c9ee8f196ac7cec120c6fa0f501f75d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7068293
    Commit-Queue: Devon Loehr <dloehr@google.com>
    Reviewed-by: Julia Hansbrough <flowerhack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1538106}

--- mojo/public/rust/mojom_parser/lib_core.rs
+++ mojo/public/rust/mojom_parser/lib_core.rs
@@ -14,6 +14,7 @@ mod pack;
 mod parse_messages;
 mod parse_primitives;
 mod parse_values;
+mod parsing_trait;
 
 pub use crate::ast::*;
 pub use crate::deparse_values::*;
@@ -21,3 +22,4 @@ pub use crate::pack::*;
 pub use crate::parse_messages::*;
 pub use crate::parse_primitives::ParserData;
 pub use crate::parse_values::*;
+pub use crate::parsing_trait::*;
---
+++ mojo/public/rust/mojom_parser/parsing_attribute.rs
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#[proc_macro_derive(MojomParse)]
+pub fn derive(_input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    // FOR_RELEASE: Do this
+    proc_macro::TokenStream::new()
+}
