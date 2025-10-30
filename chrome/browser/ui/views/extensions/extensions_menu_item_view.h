8009819c15ba3b5d5cc3629fa5441181f6e2f4ea
EmiliaPaz <emiliapaz@chromium.org>
2025-10-29 23:19:17 -0700

    [Extensions] Add MenuItemInfo to extensions menu model
    Add a MenuItemInfo struct that holds the information needed by each
    platform delegate to display the extensions menu item. This required
    moving some enums from ExtensionsMenuItemView to MenuItemInfo.
    
    Bug: 449814184
    Change-Id: I7542cfc046f98b79fa52df19176d607c27cbb27f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7059148
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Auto-Submit: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1537755}

--- chrome/browser/ui/views/extensions/extensions_menu_item_view.h
+++ chrome/browser/ui/views/extensions/extensions_menu_item_view.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_forward.h"
 #include "base/functional/callback_helpers.h"
 #include "base/memory/raw_ptr.h"
+#include "chrome/browser/ui/extensions/extensions_menu_view_model.h"
 #include "chrome/browser/ui/views/extensions/extension_context_menu_controller.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/controls/button/button.h"
@@ -38,36 +39,6 @@ class ExtensionMenuItemView : public views::FlexLayoutView,
   METADATA_HEADER(ExtensionMenuItemView, views::FlexLayoutView)
 
  public:
-  enum class SiteAccessToggleState {
-    // Button is not visible.
-    kHidden,
-    // Button is visible and off.
-    kOff,
-    // Button is visible and on.
-    kOn,
-  };
-
-  enum class SitePermissionsButtonState {
-    // Button is not visible.
-    kHidden,
-    // Button is visible, but disabled.
-    kDisabled,
-    // Button is visible and enabled.
-    kEnabled,
-  };
-
-  // Extension site access displayed in the site permissions button.
-  enum class SitePermissionsButtonAccess {
-    // Extension has no site access.
-    kNone,
-    // Extension has site access when clicked.
-    kOnClick,
-    // Extension has site access to this site.
-    kOnSite,
-    // Extension has site access to all sites.
-    kOnAllSites
-  };
-
   ExtensionMenuItemView(Browser* browser,
                         std::unique_ptr<ToolbarActionViewController> controller,
                         bool allow_pinning);
@@ -84,10 +55,7 @@ class ExtensionMenuItemView : public views::FlexLayoutView,
   ~ExtensionMenuItemView() override;
 
   // Updates the controller and child views to be on sync with the parent views.
-  void Update(SiteAccessToggleState site_access_toggle_state,
-              SitePermissionsButtonState site_permissions_button_state,
-              SitePermissionsButtonAccess site_permissions_button_access,
-              bool is_enterprise);
+  void Update(ExtensionsMenuViewModel::MenuItemInfo menu_item);
 
   // Updates the pin button.
   void UpdatePinButton(bool is_force_pinned, bool is_pinned);
