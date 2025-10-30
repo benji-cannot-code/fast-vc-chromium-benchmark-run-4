8fa1995bb024e99dd4b993e0f4f9b43796f8ab85
Brian Sheedy <bsheedy@chromium.org>
2025-10-30 13:47:04 -0700

    Add GPU Linux/RTX 4070 Super tester
    Adds a CI tester for the GPU Linux/RTX 4070 Super config and its trybot
    mirror. Tests will be enabled in a follow-up CL.
    
    Also drive-by adds the Win/Snapdragon X Elite config to the list of
    configs monitored by the bad machine finder since it was missed when
    testing as added.
    
    Bug: 456164620
    Change-Id: I0c7c0d3f4e2409884793df5bc030231625231ca8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7097940
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1538156}

--- content/test/gpu/find_bad_machines.py
+++ content/test/gpu/find_bad_machines.py
@@ -35,6 +35,7 @@ MIXIN_GROUPS = {
         'linux_intel_uhd_770_stable',
         'linux_nvidia_gtx_1660_experimental',
         'linux_nvidia_gtx_1660_stable',
+        'linux_nvidia_rtx_4070_super_stable',
         'mac_arm64_apple_m1_gpu_experimental',
         'mac_arm64_apple_m1_gpu_stable',
         'mac_arm64_apple_m2_retina_gpu_experimental',
@@ -55,6 +56,7 @@ MIXIN_GROUPS = {
         'win11_nvidia_rtx_4070_super_experimental',
         'win11_nvidia_rtx_4070_super_stable',
         'win11_qualcomm_adreno_690_stable',
+        'win11_qualcomm_snapdragon_x_elite_stable',
     ],
 }
 
