5f51f888615a7a9577dd03fb3e10ba3effe257a4
Joemer Ramos <joemerramos@google.com>
2025-10-30 13:19:43 -0700

    [iOS]: Add LocationBarBadgeMigration feature flag
    Bug: N/A
    Change-Id: I2c594c8601eda01fa3cfbdfde8247649c411ec11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7100963
    Reviewed-by: Ibrahim Kanouche <kanouche@google.com>
    Auto-Submit: Joemer Ramos <joemerramos@google.com>
    Commit-Queue: Joemer Ramos <joemerramos@google.com>
    Cr-Commit-Position: refs/heads/main@{#1538110}

--- ios/chrome/browser/shared/public/features/features.h
+++ ios/chrome/browser/shared/public/features/features.h
@@ -1162,4 +1162,10 @@ bool ShouldShowKeyboardAccessorySymbols();
 // Returns true if lens and voice search can be shown in the keyboard accessory.
 bool ShouldShowKeyboardAccessoryFeatures();
 
+// Enables the LocationBarBadgeMigration feature.
+BASE_DECLARE_FEATURE(kLocationBarBadgeMigration);
+
+// Returns true if the LocationBarBadgeMigration feature is enabled.
+bool IsLocationBarBadgeMigrationEnabled();
+
 #endif  // IOS_CHROME_BROWSER_SHARED_PUBLIC_FEATURES_FEATURES_H_
--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -1259,3 +1259,9 @@ bool ShouldShowKeyboardAccessoryFeatures() {
       kDisableKeyboardAccessory, kDisableKeyboardAccessoryParam);
   return feature_param == kDisableKeyboardAccessoryOnlyFeatures;
 }
+
+BASE_FEATURE(kLocationBarBadgeMigration, base::FEATURE_DISABLED_BY_DEFAULT);
+
+bool IsLocationBarBadgeMigrationEnabled() {
+  return base::FeatureList::IsEnabled(kLocationBarBadgeMigration);
+}
