ea707322071d4b5dbab1311106c21267c0728c3d
Evan Stade <evanstade@microsoft.com>
2025-10-30 10:38:06 -0700

    IDB: SQLite store - add histogram for file path length limitation
    Since the longest path length used by the backing store is increasing by
    23 for SQLite, and Windows Chromium limits file path lengths to 260
    chars (Windows may or may not have this limitation built in), add some
    metrics to gauge the scale of this problem. There are histograms for
    both LevelDB and SQLite. They are only logged when on-disk LevelDB is in
    use, since SQLite is never used so far, meaning logging in both cases
    would add code complexity with zero functional benefit.
    
    Note that there is already logic which checks this for LevelDB, and
    emits to a bucket of WebCore.IndexedDB.BackingStore.OpenStatus if
    the path is too long. However this existing logic/histogram is flawed
    because it fails to take into account the longest name used by LevelDB
    for a utility file.
    
    Bug: 419203257
    Change-Id: I72a3461aabe70dd58e16695ec884415b06b8593a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7089267
    Reviewed-by: Abhishek Shanthkumar <abhishek.shanthkumar@microsoft.com>
    Commit-Queue: Evan Stade <evanstade@microsoft.com>
    Reviewed-by: Mingyu Lei <leimy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1538043}

--- content/browser/indexed_db/instance/bucket_context.cc
+++ content/browser/indexed_db/instance/bucket_context.cc
@@ -79,6 +79,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "storage/browser/quota/quota_manager_proxy.h"
 #include "url/origin.h"
 
+#if BUILDFLAG(IS_WIN)
+#include "base/win/windows_types.h"
+#endif
+
 namespace content::indexed_db {
 namespace {
 
@@ -982,6 +986,32 @@ BucketContext::InitBackingStore(bool create_if_missing) {
     } else {
       database_path = data_path_.Append(GetLevelDBFileName(bucket_locator()));
       blob_path = data_path_.Append(GetBlobStoreFileName(bucket_locator()));
+
+#if BUILDFLAG(IS_WIN)
+      int max_ldb_file_path_length =
+          // The longest file path LevelDB uses.
+          database_path.AppendASCII("MANIFEST-000001").value().size();
+
+      // Underflow (i.e. file path length <= MAX_PATH) intentionally emits to
+      // the 0 bucket.
+      base::UmaHistogramCounts100("IndexedDB.FilePathLengthOverflow.LevelDB",
+                                  max_ldb_file_path_length - MAX_PATH);
+
+      int max_sqlite_file_path_length =
+          data_path_
+              .Append(GetSqliteDbDirectory(bucket_locator()))
+              // All database names hash to the same length file name.
+              .Append(DatabaseNameToFileName(u"any_string"))
+              // The WAL file will use the path with "-wal" appended. This appends ".wal".
+              .AddExtensionASCII("wal")
+              .value()
+              .size();
+
+      // Underflow (i.e. file path length <= MAX_PATH) intentionally emits to
+      // the 0 bucket.
+      base::UmaHistogramCounts100("IndexedDB.FilePathLengthOverflow.SQLite",
+                                  max_sqlite_file_path_length - MAX_PATH);
+#endif
     }
 
     if (IsPathTooLong(database_path)) {
