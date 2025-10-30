36d22354e051f5f6bea07cfac737e73d39d13ea0
Nohemi Fernandez <fernandex@google.com>
2025-10-30 06:10:37 -0700

    [iOS] Fix action latency in testTranslateInReaderMode on iOS26.1.
    The newest version of XCode Beta for iOS26 introduces latency between
    EG test actions which results in the infobar being hidden before the
    test identifies it. Add ScopedSynchronizationDisabler to avoid
    introducing this additional latency.
    
    Bug: 456123877
    Change-Id: I51a19b1efb17cae4e9c538182f9ce7c120b32269
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7101078
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@google.com>
    Cr-Commit-Position: refs/heads/main@{#1537872}

--- ios/chrome/browser/infobars/ui_bundled/translate_infobar_egtest.mm
+++ ios/chrome/browser/infobars/ui_bundled/translate_infobar_egtest.mm
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/scoped_disable_timer_tracking.h"
 #import "ios/chrome/test/earl_grey/web_http_server_chrome_test_case.h"
+#import "ios/chrome/test/scoped_eg_synchronization_disabler.h"
 #import "ios/components/webui/web_ui_url_constants.h"
 #import "ios/testing/earl_grey/app_launch_manager.h"
 #import "ios/testing/earl_grey/earl_grey_test.h"
@@ -1435,11 +1436,6 @@ void TestResponseProvider::GetLanguageResponse(
 // Tests that translation settings in Reader Mode is displayed and that
 // translation is applied when selected.
 - (void)testTranslateInReaderMode {
-  // TODO(crbug.com/456123877): Re-enable the test.
-  if (@available(iOS 26.1, *)) {
-    EARL_GREY_TEST_DISABLED(@"Test disabled on iOS 26.1.");
-  }
-
 #if !TARGET_OS_SIMULATOR
   if ([ChromeEarlGrey isIPadIdiom]) {
     EARL_GREY_TEST_DISABLED(@"Disabled on iPad devices");
@@ -1465,6 +1461,11 @@ void TestResponseProvider::GetLanguageResponse(
       waitForSufficientlyVisibleElementWithMatcher:
           grey_accessibilityID(kReaderModeViewAccessibilityIdentifier)];
 
+  // iOS26 introduces latency in the UI detection logic, which results in the
+  // infobar disappearing before the EG test attempts to detect it.
+  // Temporarily disabling synchronization allows the infobar to be detected
+  // within the expected latency.
+  ScopedSynchronizationDisabler disabler;
   // Select translation in the tools menu.
   [ChromeEarlGreyUI openToolsMenu];
 
