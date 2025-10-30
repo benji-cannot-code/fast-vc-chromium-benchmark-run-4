b4f56e6512b70c82054fae5899b212737a32d3b0
Kent Tamura <tkent@chromium.org>
2025-10-30 07:30:14 -0700

    Remove the "MacImeLiveConversionFix" feature flag
    It has been enabled since M133, and we have seen no issues caused
    by the feature.
    This CL has no behavior changes.
    
    Bug: 455818549
    Change-Id: Ia0b9b0d618fc271832c64858b835a7bdc9966518
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7099519
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1537910}

--- content/browser/renderer_host/render_widget_host_view_mac_unittest.mm
+++ content/browser/renderer_host/render_widget_host_view_mac_unittest.mm
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/render_widget_host_delegate.h"
 #include "content/browser/renderer_host/text_input_manager.h"
 #include "content/browser/site_instance_group.h"
-#include "content/common/features.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/render_widget_host_view_mac_delegate.h"
@@ -1714,9 +1713,6 @@ TEST_F(InputMethodMacTest, SetMarkedText) {
 // This test makes sure that selectedRange and markedRange are updated correctly
 // in various scenarios.
 TEST_F(InputMethodMacTest, MarkedRangeSelectedRange) {
-  if (!base::FeatureList::IsEnabled(features::kMacImeLiveConversionFix)) {
-    return;
-  }
   // If the replacement range is valid, the range should be replaced with the
   // new text.
   {
