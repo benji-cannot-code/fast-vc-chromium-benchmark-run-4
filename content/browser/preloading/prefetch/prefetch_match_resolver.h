d4f50498fb9304c19bcdd0f5b2761c3353370229
kenoss <kenoss@chromium.org>
2025-10-29 18:22:48 -0700

    Prerender2Fallback: Add debug metrics for prefetch ahead of prerender at matching
    This CL adds debug metrics to understand the state of prefetch when a
    prerender navigation fails and falls back to a regular navigation.
    The metrics are recorded when `PrerenderURLLoaderThrottle` aborts a
    prerender navigation.
    
    The new metrics include:
    
    - Whether a prefetch ahead of prerender existed.
    - `PrefetchStatus`
    - The servable state and matcher action
    - The size of the prefetch queue and the index of the candidate in the
      queue.
    
    These metrics will help diagnose issues with the prefetch/prerender
    integration and improve its reliability.
    
    Bug: 342089123, 360094997, 455448933
    Change-Id: I279cc3693d6d44414976a04a605a305b88e3734d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7049619
    Reviewed-by: Max Curran <curranmax@chromium.org>
    Commit-Queue: Ken Okada <kenoss@chromium.org>
    Reviewed-by: Taiyo Mizuhashi <taiyo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1537688}

--- content/browser/preloading/prefetch/prefetch_match_resolver.h
+++ content/browser/preloading/prefetch/prefetch_match_resolver.h
@@ -99,7 +99,10 @@ enum class PrefetchPotentialCandidateServingResult {
   // don't expect to enter this path.
   kNotServedOnDeterminedHeadWithNotServableUnknown = 13,
 
-  kMaxValue = kNotServedOnDeterminedHeadWithNotServableUnknown,
+  // A special value for `PrefetchMatchResolver::UnblockForNoCandidates()`.
+  kNotServedNoCandidates = 13,
+
+  kMaxValue = kNotServedNoCandidates,
 };
 // LINT.ThenChange(//tools/metrics/histograms/metadata/prefetch/enums.xml)
 
@@ -163,6 +166,8 @@ class CONTENT_EXPORT PrefetchMatchResolver final
       base::WeakPtr<PrefetchServingPageMetricsContainer>
           serving_page_metrics_container,
       Callback callback);
+
+  void AttachPrefetchMatchPrerenderDebugMetrics();
 
  private:
   struct CandidateData final {
