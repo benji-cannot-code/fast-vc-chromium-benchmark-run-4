da7132211ae5b5a86855a2eb0a653d23d4db890c
Pier-Alexandre Bouchard <pabouchard@google.com>
2025-10-29 18:35:38 -0700

    [iOS] Apply custom background color on NTP
    This CL enables applying a custom background color selected from the
    color slider on the NTP. It also loads the previously stored custom
    color when available. Additionally, it ensures that color selection
    behaves as expected, updates the default “no color” state to use the
    NTP’s default background color, and initializes the slider to pure red
    (the leftmost color on the hue slider).
    
    Specs:
    https://www.figma.com/design/OhxFjWMYDbWVV9G7msccJM/Bling-NTP-Customization?node-id=5160-89721&m=dev
    
    Screenshot:
    - https://screenshot.googleplex.com/5KpRbnezhj4LPkn
    - https://screenshot.googleplex.com/8ufhZm2czK49rQt
    - https://screenshot.googleplex.com/7woStc8vEo2vfTK
    
    Bug: 437134368
    Change-Id: If390cd7ca9a6655ac9842469e7086600acddb437
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7093404
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Pier-Alexandre Bouchard <pabouchard@google.com>
    Cr-Commit-Position: refs/heads/main@{#1537691}

--- ios/chrome/browser/home_customization/coordinator/background_customization_configuration_item.mm
+++ ios/chrome/browser/home_customization/coordinator/background_customization_configuration_item.mm
@@ -33,6 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @synthesize accessibilityName = _accessibilityName;
 @synthesize accessibilityValue = _accessibilityValue;
+@synthesize backgroundColor = _backgroundColor;
+@synthesize isCustomColor = _isCustomColor;
 
 - (instancetype)initWithUserUploadedImagePath:(NSString*)imagePath
                            framingCoordinates:
