3c2743fac0c8bd89e1f143074c3be72ba89ec911
Evan Stade <evanstade@microsoft.com>
2025-10-29 21:48:36 -0700

    IDB: add test to verify behavior of rollback during blob commit.
    Bug: 419208485
    Change-Id: I3998750f568ebf739a295c8d6c3a625f0b016835
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7091875
    Reviewed-by: Abhishek Shanthkumar <abhishek.shanthkumar@microsoft.com>
    Commit-Queue: Evan Stade <evanstade@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1537738}

--- content/browser/indexed_db/instance/sqlite/database_connection.cc
+++ content/browser/indexed_db/instance/sqlite/database_connection.cc
@@ -1226,7 +1226,6 @@ void DatabaseConnection::RollBackTransaction(
   }
 
   // Abort ongoing blob writes, if any.
-  // TODO(crbug.com/419208485): Be sure to test this case.
   blob_write_callback_.Reset();
   CancelBlobWriting();
 
