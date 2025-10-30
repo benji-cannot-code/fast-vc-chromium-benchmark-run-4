e6829fe7611da3ccbfbbaf19d9a8da59f15edffc
Yoichi Osato <yoichio@chromium.org>
2025-10-29 23:32:19 -0700

    NQE: Add defer_until_next_step param to finch
    This CL also move NetworkQualityEstimator::NotifyStartTransaction
    to the appropriate body.
    
    Bug: 453732745
    Change-Id: I42fd1ed2d5f98a6a2a3d6a30880c4fff3bdffae9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7091415
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1537757}

--- net/nqe/network_quality_estimator.cc
+++ net/nqe/network_quality_estimator.cc
@@ -289,9 +289,6 @@ void NetworkQualityEstimator::NotifyStartTransactionInternalAsync(
 
 void NetworkQualityEstimator::WaitNotifyStartTransactionDone(
     const URLRequest& request) {
-  TRACE_EVENT(NetTracingCategory(),
-              "NetworkQualityEstimator::NotifyStartTransaction");
-  SCOPED_UMA_HISTOGRAM_TIMER("NQE.Duration.NotifyStartTransaction");
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   CHECK(base::FeatureList::IsEnabled(
       kNetworkQualityEstimatorAsyncNotifyStartTransaction));
@@ -309,7 +306,9 @@ void NetworkQualityEstimator::WaitNotifyStartTransactionDone(
 void NetworkQualityEstimator::NotifyStartTransactionInternal(
     const URLRequest& request,
     const base::TimeTicks& time) {
-  TRACE_EVENT("loading", "NetworkQualityEstimator::NotifyStartTransaction");
+  TRACE_EVENT(NetTracingCategory(),
+              "NetworkQualityEstimator::NotifyStartTransaction");
+  SCOPED_UMA_HISTOGRAM_TIMER("NQE.Duration.NotifyStartTransaction");
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   if (!RequestSchemeIsHTTPOrHTTPS(request))
