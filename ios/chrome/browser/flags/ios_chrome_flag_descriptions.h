8e1377b04d7093af01cad699472a02771519eb60
Nicolas MacBeth <nicolasmacbeth@google.com>
2025-10-30 07:35:35 -0700

    [iOS] Feature and flag to enable web page reported image surfacing
    Bug: 455903668
    Change-Id: Ia9c4768475f487f6466a0ba68d6c9a9d6831897e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7098138
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1537913}

--- ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
+++ ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
@@ -1007,6 +1007,9 @@ extern const char kWebPageAlternativeTextZoomDescription[];
 extern const char kWebPageDefaultZoomFromDynamicTypeName[];
 extern const char kWebPageDefaultZoomFromDynamicTypeDescription[];
 
+extern const char kWebPageReportedImagesSheetName[];
+extern const char kWebPageReportedImagesSheetDescription[];
+
 extern const char kWebPageTextZoomIPadName[];
 extern const char kWebPageTextZoomIPadDescription[];
 
