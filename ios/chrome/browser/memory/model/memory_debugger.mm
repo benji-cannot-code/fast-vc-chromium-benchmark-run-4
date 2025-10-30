b6148981fcd35aac16d4e7e9be48e19af24628bd
Sylvain Defresne <sdefresne@chromium.org>
2025-10-30 05:58:05 -0700

    [ios] Use base::HeapArray<uint8_t> in MemoryDebugger
    This remove the need for an UNSAFE_BUFFERS(...) annotation in the
    implementation. Internally base::HeapArray<uint8_t> uses the same
    std::unique_ptr<uint8_t[]>.
    
    Bug: 40285824
    Change-Id: I34c39ac765e1e5ed2fd54da864700d648e834778
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7099039
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1537865}

--- ios/chrome/browser/memory/model/memory_debugger.mm
+++ ios/chrome/browser/memory/model/memory_debugger.mm
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <algorithm>
 #import <memory>
 
-#import "base/containers/span.h"
+#import "base/containers/heap_array.h"
 #import "ios/chrome/browser/memory/model/memory_metrics.h"
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
 #import "ios/chrome/common/ui/util/device_util.h"
@@ -44,7 +44,7 @@ const CGFloat kPadding = 10;
   UITextField* _continuousMemoryWarningField;
 
   // A place to store the artificial memory bloat.
-  std::unique_ptr<uint8_t[]> _bloat;
+  base::HeapArray<uint8_t> _bloat;
 
   // Distance the view was pushed up to accommodate the keyboard.
   CGFloat _keyboardOffset;
@@ -433,11 +433,9 @@ const CGFloat kPadding = 10;
   }
   const CGFloat kBloatSizeBytes = ceil(bloatSizeMB * kNumBytesInMB);
   const uint64_t kNumberOfBytes = static_cast<uint64_t>(kBloatSizeBytes);
-  _bloat.reset(kNumberOfBytes ? new uint8_t[kNumberOfBytes] : nullptr);
-  if (_bloat) {
-    // SAFETY: _bloat is a buffer of exactly kNumberOfBytes uint8_t.
-    std::ranges::fill(UNSAFE_BUFFERS(base::span(_bloat.get(), kNumberOfBytes),
-                                     uint8_t{0xff}));
+  _bloat = base::HeapArray<uint8_t>::Uninit(kNumberOfBytes);
+  if (!_bloat.empty()) {
+    std::ranges::fill(_bloat, uint8_t{0xff});
   } else {
     if (kNumberOfBytes) {
       [self alert:@"Could not allocate memory."];
