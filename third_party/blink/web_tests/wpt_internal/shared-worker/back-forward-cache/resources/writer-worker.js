f067a8a48a40b2d76642bc92d9764da07720c4da
Anna Sato <annasato@chromium.org>
2025-10-30 02:02:53 -0700

    WPT for Extended Lifetime SharedWorker + BFCache
    The test confirms two behaviors:
    - A page connected to an extended-lifetime Shared Worker can be successfully stored in and restored from BFCache.
    - The Shared Worker remains alive and can perform background tasks (writing to IndexedDB here) even after its client has been navigated away.
    
    Currently this test is expected to failed since a page is blocked from
    entering BFCache if it is the last client of a worker, even if the
    worker has an extended lifetime.
    
    Bug: 406420935
    Change-Id: I26596af40e34040ca401a64b45fee06e13d7ec1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7047957
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Anna Sato <annasato@chromium.org>
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Auto-Submit: Anna Sato <annasato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1537801}

---
+++ third_party/blink/web_tests/wpt_internal/shared-worker/back-forward-cache/resources/writer-worker.js
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+self.onconnect = event => {
+  const port = event.ports[0];
+  port.onmessage = async (msg) => {
+    if (msg.data.command === 'try_to_write') {
+      const request = self.indexedDB.open(msg.data.dbName, 1);
+      const storeName = msg.data.storeName
+      request.onupgradeneeded = e =>
+          e.target.result.createObjectStore(storeName);
+      request.onsuccess = e => {
+        const tx = e.target.result.transaction(storeName, 'readwrite');
+        tx.objectStore(storeName).put('value', 'key');
+      };
+    }
+  };
+};
