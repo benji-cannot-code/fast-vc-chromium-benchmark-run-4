b600c7c491430cf97acdf0714fc89a90661260eb
christianxu <christianxu@chromium.org>
2025-10-31 10:07:28 -0700

    [iOS][Multiline] Fix wrong color on edit
    Reapply the typing attributes when UITextView resets it.
    
    Fixed: 455553393
    Change-Id: Iadcd33ddf49ddde6e8d49277a6d2c7dff0e2204d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7106518
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1538682}

--- ios/chrome/browser/omnibox/ui/omnibox_text_view_ios.mm
+++ ios/chrome/browser/omnibox/ui/omnibox_text_view_ios.mm
@@ -65,6 +65,9 @@ using enum OmniboxKeyboardAction;
   OmniboxPresentationContext _presentationContext;
   /// The default foreground color for text.
   UIColor* _defaultTextColor;
+
+  /// The omnibox typing attributes.
+  NSDictionary<NSAttributedStringKey, id>* _omniboxTypingAttributes;
 }
 
 @synthesize omniboxTextInputDelegate = _omniboxTextInputDelegate;
@@ -102,6 +105,7 @@ using enum OmniboxKeyboardAction;
     if (@available(iOS 18, *)) {
       self.writingToolsBehavior = UIWritingToolsBehaviorNone;
     }
+    [self updateOmniboxTypingAttributes];
 
     // Disable drag on iPhone because there's nowhere to drag to
     if (ui::GetDeviceFormFactor() != ui::DEVICE_FORM_FACTOR_TABLET) {
@@ -409,7 +413,6 @@ using enum OmniboxKeyboardAction;
   [attributes setValue:self.currentFont forKey:NSFontAttributeName];
   [attributes setValue:self.selectedTextBackgroundColor
                 forKey:NSBackgroundColorAttributeName];
-  self.typingAttributes = attributes;
 
   // Also apply the attributes to the whole text.
   NSMutableAttributedString* attributedText = [self.attributedText mutableCopy];
@@ -926,6 +929,17 @@ using enum OmniboxKeyboardAction;
   return IsCompactWidth(self) ? self.normalFont : self.largerFont;
 }
 
+/// Updates the omnibox typing attributes with the current font.
+- (void)updateOmniboxTypingAttributes {
+  NSMutableDictionary<NSAttributedStringKey, id>* attributes =
+      self.typingAttributes.mutableCopy;
+  [attributes setValue:self.currentFont forKey:NSFontAttributeName];
+  [attributes setValue:UIColor.clearColor
+                forKey:NSBackgroundColorAttributeName];
+  [attributes setValue:_defaultTextColor forKey:NSForegroundColorAttributeName];
+  _omniboxTypingAttributes = attributes;
+}
+
 #pragma mark Helpers
 
 /// Length of added text in the omnibox (autocomplete and additional text).
@@ -1140,6 +1154,13 @@ using enum OmniboxKeyboardAction;
   [self.omniboxTextInputDelegate textInputDidChange:self];
   [self updatePlaceholderVisibility];
   [self.heightDelegate textViewContentChanged:self];
+  self.typingAttributes = _omniboxTypingAttributes;
+}
+
+- (void)textViewDidChangeSelection:(UITextView*)textView {
+  // UITextView resets typing attributes on text selection change, reapply them
+  // here.
+  self.typingAttributes = _omniboxTypingAttributes;
 }
 
 - (BOOL)textView:(UITextView*)textView
@@ -1157,6 +1178,7 @@ using enum OmniboxKeyboardAction;
 - (void)textViewDidBeginEditing:(UITextView*)textView {
   _editing = YES;
   [self.omniboxTextInputDelegate textInputDidBeginEditing:self];
+  [self updateOmniboxTypingAttributes];
 }
 
 - (void)textViewDidEndEditing:(UITextView*)textView {
