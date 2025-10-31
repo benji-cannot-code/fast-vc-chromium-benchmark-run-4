be9591f587546897784beaea35342437c0035c9b
Marlon Facey <mfacey@chromium.org>
2025-10-31 13:06:51 -0700

    [Omnibox Next] Add icons to context menu
    Add vector icons for add file, add image and deep search icons. Add
    png for create images icon.
    
    Light: http://screenshot/53L8NN2GykZBs5V
    Dark: http://screenshot/8ms2Ssqr2z2kPhB
    
    Bug: 452331116
    Change-Id: Ieee95fa1df3271359a8f9353b94dc696804f3fe9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7098312
    Reviewed-by: David Yeung <dayeung@chromium.org>
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1538803}

--- chrome/browser/ui/omnibox/omnibox_context_menu_controller.h
+++ chrome/browser/ui/omnibox/omnibox_context_menu_controller.h
@@ -12,6 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class BrowserWindowInterface;
 class GURL;
 
+namespace ui {
+class ImageModel;
+}  // namespace ui
+
 // OmniboxContextMenuController creates and manages state for the context menu
 // shown for the omnibox.
 class OmniboxContextMenuController : public ui::SimpleMenuModel::Delegate {
@@ -31,15 +35,18 @@ class OmniboxContextMenuController : public ui::SimpleMenuModel::Delegate {
 
  private:
   void BuildMenu();
-
   // Adds a IDC_* style command to the menu with a string16.
   void AddItem(int id, const std::u16string str);
-  // Adds a IDC_* style command to the menu with a localized string.
-  void AddItem(int id, int localization_id);
+  // Adds a IDC_* style command to the menu with a localized string and icon.
+  void AddItemWithStringIdAndIcon(int id,
+                                  int localization_id,
+                                  const ui::ImageModel& icon);
   // Adds a separator to the menu.
   void AddSeparator();
   // Adds recent tabs as items to the menu.
   void AddRecentTabItems();
+  // Adds the static items with icons.
+  void AddStaticItems();
   // Adds a title with a localized string to the menu.
   void AddTitleWithStringId(int localization_id);
 
