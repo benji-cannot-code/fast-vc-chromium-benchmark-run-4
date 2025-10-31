6121470a8d7287664d7a557986b22dd7978d43a5
Minseong Kim <jja08111@gmail.com>
2025-10-31 03:31:39 -0700

    [css-lists] Use default init in HTMLOListElement
    This CL has no behavior changes.
    
    Bug: 40682542
    Change-Id: I6fdc96eb2a9b432d074c5759ea405b4c70142d6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7100163
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Minseong Kim <jja08111@gmail.com>
    Reviewed-by: Daniil Sakhapov <sakhapov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1538488}

--- third_party/blink/renderer/core/html/html_olist_element.cc
+++ third_party/blink/renderer/core/html/html_olist_element.cc
@@ -34,12 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 HTMLOListElement::HTMLOListElement(Document& document)
-    : HTMLElement(html_names::kOlTag, document),
-      start_(0xBADBEEF),
-      item_count_(0),
-      has_explicit_start_(false),
-      is_reversed_(false),
-      should_recalculate_item_count_(false) {}
+    : HTMLElement(html_names::kOlTag, document) {}
 
 bool HTMLOListElement::IsPresentationAttribute(
     const QualifiedName& name) const {
