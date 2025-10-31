07b055655529fe7ee5f973f05415ccbd6cbde36d
Kristi Saney <kristislee@google.com>
2025-10-30 17:37:42 -0700

    [Read aloud] Require page action migration be enabled for omnibox chip
    Bug: 440174061
    Change-Id: I8e715bb941a1511e6dc29da520393c8857201e83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7103490
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Auto-Submit: Kristi Saney <kristislee@google.com>
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1538313}

--- chrome/browser/ui/read_anything/read_anything_side_panel_controller.cc
+++ chrome/browser/ui/read_anything/read_anything_side_panel_controller.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/read_anything/read_anything_service.h"
 #include "chrome/browser/ui/read_anything/read_anything_side_panel_web_view.h"
 #include "chrome/browser/ui/tabs/public/tab_features.h"
+#include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/interaction/browser_elements_views.h"
 #include "chrome/browser/ui/views/page_action/page_action_controller.h"
@@ -295,7 +296,8 @@ void ReadAnythingSidePanelController::DidStopLoading() {
 
 void ReadAnythingSidePanelController::UpdatePageActionVisibility(
     bool should_show) {
-  if (!features::IsReadAnythingOmniboxChipEnabled()) {
+  if (!base::FeatureList::IsEnabled(features::kPageActionsMigration) ||
+      !features::IsReadAnythingOmniboxChipEnabled()) {
     return;
   }
 
