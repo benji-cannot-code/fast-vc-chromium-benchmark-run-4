7bf77178201a3abf45ce5c77c99bb674bfef29a9
Taiyo Mizuhashi <taiyo@chromium.org>
2025-10-30 20:33:48 -0700

    Revert "[Read aloud] Require page action migration be enabled for omnibox chip"
    This reverts commit c650da76e3ef734716f05d0373244d48d7ab8048.
    
    Reason for revert: Resolving merge conflict for crrev.com/c/7104401
    
    Original change's description:
    > [Read aloud] Require page action migration be enabled for omnibox chip
    >
    > Bug: 440174061
    > Change-Id: I8e715bb941a1511e6dc29da520393c8857201e83
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7103490
    > Commit-Queue: Lauren Winston <lwinston@google.com>
    > Commit-Queue: Kristi Saney <kristislee@google.com>
    > Auto-Submit: Kristi Saney <kristislee@google.com>
    > Reviewed-by: Lauren Winston <lwinston@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1538313}
    
    Bug: 440174061
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Id145384de85cd4c014da4655c8def00c3f617e04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7104760
    Owners-Override: Taiyo Mizuhashi <taiyo@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Taiyo Mizuhashi <taiyo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1538364}

--- chrome/browser/ui/read_anything/read_anything_side_panel_controller.cc
+++ chrome/browser/ui/read_anything/read_anything_side_panel_controller.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/read_anything/read_anything_service.h"
 #include "chrome/browser/ui/read_anything/read_anything_side_panel_web_view.h"
 #include "chrome/browser/ui/tabs/public/tab_features.h"
-#include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/interaction/browser_elements_views.h"
 #include "chrome/browser/ui/views/page_action/page_action_controller.h"
@@ -296,8 +295,7 @@ void ReadAnythingSidePanelController::DidStopLoading() {
 
 void ReadAnythingSidePanelController::UpdatePageActionVisibility(
     bool should_show) {
-  if (!base::FeatureList::IsEnabled(features::kPageActionsMigration) ||
-      !features::IsReadAnythingOmniboxChipEnabled()) {
+  if (!features::IsReadAnythingOmniboxChipEnabled()) {
     return;
   }
 
