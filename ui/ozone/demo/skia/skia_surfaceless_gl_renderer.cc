696b713d51dc16c0006bd4f88215cfa2b51e8c63
Colin Blundell <blundell@chromium.org>
2025-10-31 00:52:10 -0700

    [//ui] Port SkiaSurfacelessGLRenderer's call to CreateNativePixmap()
    Move it from passing BufferFormat to passing SharedImageFormat.
    
    Bug: 450325664
    Change-Id: I9742b57fb33437ec1c1712feb9e743960ae8ebf1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7100758
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1538429}

--- ui/ozone/demo/skia/skia_surfaceless_gl_renderer.cc
+++ ui/ozone/demo/skia/skia_surfaceless_gl_renderer.cc
@@ -118,8 +118,9 @@ bool SurfacelessSkiaGlRenderer::BufferWrapper::Initialize(
     const gfx::Size& size) {
   glGenTextures(1, &gl_tex_);
 
-  gfx::BufferFormat format = viz::SinglePlaneSharedImageFormatToBufferFormat(
-      display::DisplaySnapshot::PrimaryFormat());
+  auto format = display::DisplaySnapshot::PrimaryFormat();
+  gfx::BufferFormat buffer_format =
+      viz::SinglePlaneSharedImageFormatToBufferFormat(format);
 
   pixmap_ = OzonePlatform::GetInstance()
                 ->GetSurfaceFactoryOzone()
@@ -130,7 +131,8 @@ bool SurfacelessSkiaGlRenderer::BufferWrapper::Initialize(
       OzonePlatform::GetInstance()
           ->GetSurfaceFactoryOzone()
           ->GetCurrentGLOzone()
-          ->ImportNativePixmap(pixmap_, format, gfx::BufferPlane::DEFAULT, size,
+          ->ImportNativePixmap(pixmap_, buffer_format,
+                               gfx::BufferPlane::DEFAULT, size,
                                gfx::ColorSpace(), GL_TEXTURE_2D, gl_tex_);
 
   if (!pixmap_gl_binding_) {
