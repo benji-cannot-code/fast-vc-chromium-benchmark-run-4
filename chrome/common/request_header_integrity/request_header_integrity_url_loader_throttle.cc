97e418f7936455260de150c3d4f0f7d879a66c81
Deepak Ravichandran <deepakr@google.com>
2025-10-31 12:40:23 -0700

    Doing some minor code clean up for better compilation and optimization.
    The commit mentions in the "DEPS" file are already submitted in a preceding CL.
    (See Bug for more details)
    
    Bug: 352255225
    Change-Id: I61eaf4d0e900d2b2cf74eb3f48468202e9dfece5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7104006
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: David Bienvenu <davidbienvenu@chromium.org>
    Commit-Queue: Deepak Ravichandran <deepakr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1538792}

--- chrome/common/request_header_integrity/request_header_integrity_url_loader_throttle.cc
+++ chrome/common/request_header_integrity/request_header_integrity_url_loader_throttle.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "build/branding_buildflags.h"
 #include "chrome/common/channel_info.h"
-#include "chrome/common/request_header_integrity/build_derived_values.h"
 #include "components/embedder_support/user_agent_utils.h"
 #include "components/google/core/common/google_util.h"
 #include "google_apis/google_api_keys.h"
@@ -24,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/mojom/network_context.mojom.h"
 
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+#include "chrome/common/request_header_integrity/internal/build_derived_values.h"
 #include "chrome/common/request_header_integrity/internal/google_header_names.h"
 #endif
 
@@ -41,6 +41,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if !defined(COPYRIGHT_HEADER_NAME)
 #define COPYRIGHT_HEADER_NAME "X-Placeholder-4"
+#endif
+
+#if !defined(CHROME_COPYRIGHT)
+#define CHROME_COPYRIGHT "X-COPYRIGHT"
+#endif
+
+#if !defined(LASTCHANGE_YEAR)
+#define LASTCHANGE_YEAR "1969"
 #endif
 
 namespace request_header_integrity {
