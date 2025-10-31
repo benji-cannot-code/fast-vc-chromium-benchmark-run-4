f9c6e5f83d552988c3ddba42d5eddde613943f68
Mohamed Adel <adelm@google.com>
2025-10-31 16:07:39 -0700

    [Settings Search] Send Query to SettingsIndexData to perform Search
    This change adds a registry of the available prefs (for now) and uses
    them to index the xml data. Currently, the logic is too naive and is
    meant to be built on in next steps so it is able to determine what prefs
    are visible to the user and can be retrieved when querying. This is fine
    for now.
    
    Bug: 444470792
    Change-Id: I5587803b959e3d3fbe4ad7a250347d2b47fffeef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7103722
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Moe Adel <adelm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1538895}

--- chrome/android/java/src/org/chromium/chrome/browser/settings/search/SettingsSearchCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/search/SettingsSearchCoordinator.java
@@ -74,6 +74,7 @@ public class SettingsSearchCoordinator {
 
     // Set to {@code true} after queries are entered. Used to conditionally clear up the screen.
     private boolean mQueryEntered;
+    private SettingsIndexData mIndexData;
 
     // Interface to communite with search backend and receive results asynchronously.
     public interface SearchCallback {
@@ -145,7 +146,20 @@ public class SettingsSearchCoordinator {
         }
     }
 
+    @Initializer
+    @EnsuresNonNull("mIndexData")
+    private void initIndex() {
+        if (mIndexData == null) {
+            mIndexData = new SettingsIndexData();
+        } else {
+            // (crbug.com/456817438): Index only when stale.
+            mIndexData.initIndex(mActivity);
+        }
+    }
+
     private void enterSearchState() {
+        initIndex();
+
         if (mMultiColumnSettings != null && !mMultiColumnSettingsBackActionHandlerSet) {
             mActivity
                     .getOnBackPressedDispatcher()
@@ -296,7 +310,8 @@ public class SettingsSearchCoordinator {
                         String query = s.toString().trim();
                         if (query.length() > 0) {
                             mQueryEntered = true;
-                            performSearch(SettingsSearchCoordinator.this::displayResultsFragment);
+                            performSearch(
+                                    query, SettingsSearchCoordinator.this::displayResultsFragment);
                         } else {
                             if (mQueryEntered) clearFragment(/* addToBackStack= */ false);
                         }
@@ -307,10 +322,11 @@ public class SettingsSearchCoordinator {
     /**
      * Performs search by sending the query to search backend.
      *
+     * @param query The search query the user entered.
      * @param callback The callback function to be executed when results are available.
      */
     @EnsuresNonNull("mSearchRunnable")
-    private void performSearch(SearchCallback callback) {
+    private void performSearch(String query, SearchCallback callback) {
         if (mSearchRunnable != null) {
             // Debouncing to avoid initiating search for each keystroke entered fast.
             // We sets some delay before initiating search (see postDelayed() below) so that
@@ -318,9 +334,7 @@ public class SettingsSearchCoordinator {
             mHandler.removeCallbacks(mSearchRunnable);
         }
 
-        // TODO(jinsukkim): Connect to real search backend. For now, get a canned result
-        //    to let the UI journey proceed.
-        mSearchRunnable = () -> callback.onSearchResults(new SearchResults());
+        mSearchRunnable = () -> callback.onSearchResults(mIndexData.search(query));
         mHandler.postDelayed(mSearchRunnable, 200);
     }
 
