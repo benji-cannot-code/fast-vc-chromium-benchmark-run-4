ac93017ecc98f2883639a190e4896dc6d56a1300
Jan Wilken Dörrie <jdoerrie@chromium.org>
2025-10-31 06:43:37 -0700

    [dbsc] Add API to list all signing keys
    This change introduces a new method to the `UnexportableKeyService`
    interface, `GetAllSigningKeysForGarbageCollectionSlowlyAsync()`, which
    allows clients to retrieve the identifiers of all available unexportable
    signing keys.
    
    This API is a foundational change for upcoming features that require the
    ability to enumerate all existing keys for garbage collection purposes.
    
    A placeholder implementation is provided that returns an empty list of
    keys. The full implementation that retrieves keys from the underlying
    operating system key store will be added in a subsequent change.
    
    The corresponding fake and mock services are updated to include the new
    method.
    
    Bug: 455537760
    Change-Id: I57a4c83959241514fdcfaaab85e34a2c8ee7288b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7105798
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Ernest Nguyen Hung <ernn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1538550}

--- components/unexportable_keys/unexportable_key_service_impl_unittest.cc
+++ components/unexportable_keys/unexportable_key_service_impl_unittest.cc
@@ -31,9 +31,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace unexportable_keys {
 
 using ::base::test::ErrorIs;
+using ::base::test::ValueIs;
 using ::testing::AtLeast;
 using ::testing::ElementsAreArray;
 using ::testing::Invoke;
+using ::testing::IsEmpty;
 using ::testing::NiceMock;
 using ::testing::Return;
 
@@ -289,6 +291,18 @@ TEST_F(UnexportableKeyServiceImplTest,
   EXPECT_TRUE(from_wrapped_future.IsReady());
   // Key IDs should be the same.
   EXPECT_EQ(key_id, from_wrapped_future.Get());
+}
+
+TEST_F(UnexportableKeyServiceImplTest,
+       GetAllSigningKeysForGarbageCollectionSlowlyAsync) {
+  base::test::TestFuture<ServiceErrorOr<std::vector<UnexportableKeyId>>>
+      get_all_keys_future;
+  service().GetAllSigningKeysForGarbageCollectionSlowlyAsync(
+      kTaskPriority, get_all_keys_future.GetCallback());
+  RunBackgroundTasks();
+  // TODO: crbug.com/455538141 - Update the test when the retrieval is
+  // implemented.
+  EXPECT_THAT(get_all_keys_future.Get(), ValueIs(IsEmpty()));
 }
 
 TEST_F(UnexportableKeyServiceImplTest, Sign) {
