a556af0400e0e65ccbb5fedd54a0e917f873c9f8
Gary Klassen <gklassen@chromium.org>
2025-10-31 12:49:58 -0700

    Fix TabId for CaptureRegion
    Bug: b:452032491
    
    Change-Id: I63b6257ada79ec615df1d01141a09ee98a426c4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7106681
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Commit-Queue: Gary Klassen <gklassen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1538799}

--- chrome/browser/glic/host/glic_region_capture_controller.cc
+++ chrome/browser/glic/host/glic_region_capture_controller.cc
@@ -5,9 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/glic/host/glic_region_capture_controller.h"
 
+#include "chrome/browser/glic/host/context/glic_tab_data.h"
 #include "chrome/browser/glic/host/glic.mojom.h"
 #include "chrome/browser/lens/region_search/lens_region_search_controller.h"
-#include "components/sessions/content/session_tab_helper.h"
 #include "content/public/browser/web_contents.h"
 #include "ui/views/widget/widget.h"
 
@@ -93,7 +93,7 @@ void GlicRegionCaptureController::OnRegionSelected(const gfx::Rect& rect) {
     return;
   }
   auto result = mojom::CaptureRegionResult::New();
-  result->tab_id = sessions::SessionTabHelper::IdForTab(web_contents_).id();
+  result->tab_id = GetTabId(web_contents_);
   result->region = mojom::CapturedRegion::NewRect(rect);
   capture_region_observer_->OnUpdate(std::move(result), std::nullopt);
 }
