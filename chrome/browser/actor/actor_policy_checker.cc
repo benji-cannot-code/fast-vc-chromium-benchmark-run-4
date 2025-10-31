1d0fed0f3328daca824738ede98784836b49b132
William Liu <liuwilliam@chromium.org>
2025-10-31 11:20:55 -0700

    Exempt Dogfooders on Chrome side
    A temporary measure before the Finch seed is picked up by all
    Dogfooder clients.
    
    Bug: 454914273, 454961534
    Change-Id: I666504a094a843d6df748917380c1d50d72855a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7102978
    Auto-Submit: William Liu <liuwilliam@chromium.org>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Kevin Graney <kmg@google.com>
    Commit-Queue: William Liu <liuwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1538741}

--- chrome/browser/actor/actor_policy_checker.cc
+++ chrome/browser/actor/actor_policy_checker.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/actor/actor_keyed_service.h"
 #include "chrome/browser/actor/aggregated_journal.h"
 #include "chrome/browser/actor/site_policy.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/enterprise/browser_management/browser_management_service.h"
 #include "chrome/browser/enterprise/browser_management/management_service_factory.h"
 #include "chrome/browser/glic/glic_pref_names.h"
@@ -18,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/buildflags.h"
 #include "chrome/common/chrome_features.h"
 #include "components/prefs/pref_service.h"
+#include "components/variations/service/variations_service.h"
 #include "content/public/browser/web_contents.h"
 
 namespace actor {
@@ -48,6 +50,12 @@ std::string GlicActuationOnWebPrefToString(int value) {
   }
   NOTREACHED();
 }
+
+bool IsLikelyDogfoodClient() {
+  variations::VariationsService* variations_service =
+      g_browser_process->variations_service();
+  return variations_service && variations_service->IsLikelyDogfoodClient();
+}
 #endif  // BUILDFLAG(ENABLE_GLIC)
 
 bool HasActuationCapability(Profile* profile) {
@@ -69,6 +77,7 @@ bool HasActuationCapability(Profile* profile) {
       features::kGlicActorEnterprisePrefDefault.Get();
   const int capability_pref =
       profile->GetPrefs()->GetInteger(glic::prefs::kGlicActuationOnWeb);
+  bool is_likely_dogfood_client = IsLikelyDogfoodClient();
 
   VLOG(1) << "Is browser managed: " << is_managed;
   VLOG(1) << "kGlicActorEnterprisePrefDefault value: "
@@ -77,8 +86,9 @@ bool HasActuationCapability(Profile* profile) {
           << profile->GetPrefs()->IsManagedPreference(
                  glic::prefs::kGlicActuationOnWeb)
           << " value: " << GlicActuationOnWebPrefToString(capability_pref);
+  VLOG(1) << "is_likely_dogfood_client: " << is_likely_dogfood_client;
 
-  if (!is_managed) {
+  if (!is_managed || is_likely_dogfood_client) {
     return true;
   }
 
