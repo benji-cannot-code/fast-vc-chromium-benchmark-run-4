2125f009cd48385db7b710e1e94cc4d65448cc90
Simeon Anfinrud <sanfin@chromium.org>
2025-10-31 00:42:21 -0700

    [chromecast] Tear down CWCA when backgrounding under certain conditions.
    When playing video, backgrounding is enabled if PiP is enabled.
    
    * This will be changed once Android TV launchers fix the "Watch Next"
      cards that are used to return to the app when backgrounded.
    * If picture-in-picture is enabled, then video playback can continue
      uninterrupted and teardown is not needed.
    
    When playing audio, backgrounding is enabled if touch input is enabled.
    
    * This will be changed once the "Now Playing" cards created by Android
      TV launchers are fixed.
    * Until then, devices with a touch screen are assumed to have a window
      management UI that can return to the Activity, so background audio
      is allowed to continue.
    
    Bug: 361186605
    Bug: 429081455
    Bug: 420403805
    Bug: 445494995
    Test: cast_shell_junit_tests
    Change-Id: Ia24ded30a0713aaf12a7b312f17279c1662b14e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7038670
    Commit-Queue: Simeon Anfinrud <sanfin@chromium.org>
    Reviewed-by: Sandeep Vijayasekar <sandv@google.com>
    Cr-Commit-Position: refs/heads/main@{#1538419}

--- chromecast/browser/android/junit/src/org/chromium/chromecast/shell/CastWebContentsActivityTest.java
+++ chromecast/browser/android/junit/src/org/chromium/chromecast/shell/CastWebContentsActivityTest.java
@@ -25,11 +25,13 @@ import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.Application;
 import android.app.PictureInPictureParams;
+import android.app.UiModeManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.content.res.Configuration;
 import android.media.AudioManager;
 import android.net.Uri;
 import android.os.PatternMatcher;
@@ -65,6 +67,7 @@ import org.robolectric.shadow.api.Shadow;
 import org.robolectric.shadows.ShadowActivity;
 import org.robolectric.shadows.ShadowActivityManager;
 import org.robolectric.shadows.ShadowPackageManager;
+import org.robolectric.shadows.ShadowUIModeManager;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.base.test.BaseRobolectricTestRunner;
@@ -149,6 +152,7 @@ public class CastWebContentsActivityTest {
     private int mNextMediaId;
     private Application mApplication;
     private ShadowActivityManager mShadowActivityManager;
+    private ShadowUIModeManager mShadowUIModeManager;
     private ShadowPackageManager mShadowPackageManager;
     private ActivityController<CastWebContentsActivity> mActivityLifecycle;
     private CastWebContentsActivity mActivity;
@@ -173,6 +177,11 @@ public class CastWebContentsActivityTest {
                         (ActivityManager)
                                 RuntimeEnvironment.application.getSystemService(
                                         Context.ACTIVITY_SERVICE));
+        mShadowUIModeManager =
+                Shadows.shadowOf(
+                        (UiModeManager)
+                                RuntimeEnvironment.application.getSystemService(
+                                        Context.UI_MODE_SERVICE));
         mShadowPackageManager =
                 Shadows.shadowOf(RuntimeEnvironment.application.getPackageManager());
         mActivityLifecycle =
@@ -443,6 +452,7 @@ public class CastWebContentsActivityTest {
         Window window = mock(Window.class);
         mActivityLifecycle.create().start().resume();
         shadowActivity.setWindow(window);
+        updateMediaState(true, true);
         MotionEvent event = mock(MotionEvent.class);
         when(event.getAction()).thenReturn(MotionEvent.ACTION_DOWN);
         when(window.superDispatchTouchEvent(event)).thenReturn(true);
@@ -475,9 +485,11 @@ public class CastWebContentsActivityTest {
 
     @Test
     @Config(shadows = {ExtendedShadowActivity.class})
-    public void testStopWhileInPipModeDoesNotClosesActivity() {
+    public void testStopWhileInPipModeDoesNotCloseActivity() {
+        mShadowPackageManager.setSystemFeature(PackageManager.FEATURE_PICTURE_IN_PICTURE, true);
         mShadowActivityManager.setLockTaskModeState(ActivityManager.LOCK_TASK_MODE_NONE);
         mActivityLifecycle.create().start().resume();
+        updateMediaState(true, true);
         mActivity.onUserLeaveHint();
         mActivity.onPictureInPictureModeChanged(true, null);
         verifyBroadcastedIntent(
@@ -487,6 +499,50 @@ public class CastWebContentsActivityTest {
                     assertFalse(mActivity.isFinishing());
                 },
                 false);
+    }
+
+    @Test
+    public void testStopWhileNoMediaPlayingClosesActivity() {
+        mShadowActivityManager.setLockTaskModeState(ActivityManager.LOCK_TASK_MODE_NONE);
+        mActivityLifecycle.create().start().resume();
+        updateMediaState(false, false);
+        verifyBroadcastedIntent(
+                filterFor(CastWebContentsIntentUtils.ACTION_ACTIVITY_STOPPED),
+                () -> {
+                    mActivity.onUserLeaveHint();
+                    assertTrue(mActivity.isFinishing());
+                },
+                true);
+    }
+
+    @Test
+    public void testStopWhileAudioIsPlayingOnNonTvDoesNotCloseActivity() {
+        mShadowUIModeManager.setCurrentModeType(Configuration.UI_MODE_TYPE_NORMAL);
+        mShadowActivityManager.setLockTaskModeState(ActivityManager.LOCK_TASK_MODE_NONE);
+        mActivityLifecycle.create().start().resume();
+        updateMediaState(true, false);
+        verifyBroadcastedIntent(
+                filterFor(CastWebContentsIntentUtils.ACTION_ACTIVITY_STOPPED),
+                () -> {
+                    mActivity.onUserLeaveHint();
+                    assertFalse(mActivity.isFinishing());
+                },
+                false);
+    }
+
+    @Test
+    public void testStopWhileAudioIsPlayingOnTvClosesActivity() {
+        mShadowUIModeManager.setCurrentModeType(Configuration.UI_MODE_TYPE_TELEVISION);
+        mShadowActivityManager.setLockTaskModeState(ActivityManager.LOCK_TASK_MODE_NONE);
+        mActivityLifecycle.create().start().resume();
+        updateMediaState(true, false);
+        verifyBroadcastedIntent(
+                filterFor(CastWebContentsIntentUtils.ACTION_ACTIVITY_STOPPED),
+                () -> {
+                    mActivity.onUserLeaveHint();
+                    assertTrue(mActivity.isFinishing());
+                },
+                true);
     }
 
     @Test
