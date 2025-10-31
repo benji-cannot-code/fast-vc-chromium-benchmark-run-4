09e1b6625cc446440d1751fcea974e68bc0afa18
Minoru Chikamune <chikamune@chromium.org>
2025-10-30 23:50:31 -0700

    Make PageLifecycleMetricsOnNewPageCommit UKM more accurate
    As rakina commented below, the previous
    PageLifecycleMetricsOnNewPageCommit UKM was not accurate. This CL makes
    more accurate.
    
    https://chromium-review.googlesource.com/c/chromium/src/+/7082104/comment/4cd029bd_728c4465/
    
    The previous implementation for measuring the processing time of the old
    page's lifecycle events was found to be inaccurate in some
    scenarios. The measurement could include time spent on tasks other than
    the actual page lifecycle event handling. Also, it did not correctly
    handle navigation paths where a RenderDocument is not used (e.g., when
    the feature is disabled), in which case the events are run from a
    different location.
    
    To address this, this patch moves the time measurement logic into
    FrameLoader::DispatchUnloadEventAndFillOldDocumentInfoIfNeeded().
    
    Bug: 455419434
    Change-Id: Ia03c07938fa027481721743739a439bc104748cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7091479
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1538402}

--- content/browser/navigation_browsertest.cc
+++ content/browser/navigation_browsertest.cc
@@ -8971,8 +8971,45 @@ IN_PROC_BROWSER_TEST_F(NavigationBrowserTest,
   ASSERT_EQ(false, EvalJs(child_ftn, "document.hasFocus();"));
 }
 
-IN_PROC_BROWSER_TEST_F(NavigationBrowserTest,
-                       RecordPageLifecycleMetricsOnNewPageCommit) {
+class PageLifecycleMetricsOnNewPageCommitBrowserTest
+    : public NavigationBrowserTest,
+      public testing::WithParamInterface<std::pair<std::string, std::string>> {
+ public:
+  PageLifecycleMetricsOnNewPageCommitBrowserTest() {
+    feature_list_.InitFromCommandLine(GetParam().first, GetParam().second);
+  }
+
+  void AddSlowPagehideEventHandlerToCurrentWebContents(
+      const base::Location location = FROM_HERE) {
+    // Wait for 1 second in the pagehide event handler as we want to measure the
+    // slow pagehide's callback case of 300 ms.
+    EXPECT_TRUE(ExecJs(web_contents(), R"(
+      window.addEventListener('pagehide', function() {
+        const start = Date.now();
+        while (Date.now() - start <= 300) {
+        }
+      });
+    )")) << location.ToString();
+  }
+
+ private:
+  base::test::ScopedFeatureList feature_list_;
+};
+
+INSTANTIATE_TEST_SUITE_P(
+    All,
+    PageLifecycleMetricsOnNewPageCommitBrowserTest,
+    testing::Values(
+        std::make_pair(/*enable_features=*/"BackForwardCache,RenderDocument",
+                       /*disable_features=*/""),
+        std::make_pair(/*enable_features=*/"RenderDocument",
+                       /*disable_features=*/"BackForwardCache"),
+        std::make_pair(
+            /*enable_features=*/"",
+            /*disable_features=*/"BackForwardCache,RenderDocument")));
+
+IN_PROC_BROWSER_TEST_P(PageLifecycleMetricsOnNewPageCommitBrowserTest,
+                       RecordUkm) {
   const std::string_view kTargetUkmEntryName =
       ukm::builders::PageLifecycleMetricsOnNewPageCommit::kEntryName;
   const std::string_view kTargetUkmMetricName =
@@ -8993,18 +9030,11 @@ IN_PROC_BROWSER_TEST_F(NavigationBrowserTest,
     const ukm::mojom::UkmEntry* entry = entries[0];
     ukm_recorder.ExpectEntrySourceHasUrl(entry, url_1);
     // The previous page doesn't have pagehide.
-    EXPECT_LE(*ukm_recorder.GetEntryMetric(entry, kTargetUkmMetricName),
-              ukm::GetExponentialBucketMinForFineUserTiming(100u));
+    EXPECT_NEAR(*ukm_recorder.GetEntryMetric(entry, kTargetUkmMetricName), 0u,
+                100u);
   }
 
-  EXPECT_TRUE(ExecJs(web_contents(), R"(
-    window.addEventListener('pagehide', function() {
-      const start = Date.now();
-      // Wait for 1 second.
-      while (Date.now() - start <= 1000) {
-      }
-    });
-  )"));
+  AddSlowPagehideEventHandlerToCurrentWebContents();
 
   {
     GURL url_2(embedded_test_server()->GetURL("a.test", "/title2.html"));
@@ -9018,9 +9048,9 @@ IN_PROC_BROWSER_TEST_F(NavigationBrowserTest,
     EXPECT_EQ(2u, entries.size());
     const ukm::mojom::UkmEntry* entry = entries[1];
     ukm_recorder.ExpectEntrySourceHasUrl(entry, url_2);
-    // The previous page's pagehide will take over 1000 ms.
-    EXPECT_GE(*ukm_recorder.GetEntryMetric(entry, kTargetUkmMetricName),
-              ukm::GetExponentialBucketMinForFineUserTiming(1000u));
+    // The previous page's pagehide will take over 300 ms.
+    EXPECT_NEAR(*ukm_recorder.GetEntryMetric(entry, kTargetUkmMetricName),
+                ukm::GetExponentialBucketMinForFineUserTiming(300u), 100u);
   }
 
   {
@@ -9036,18 +9066,11 @@ IN_PROC_BROWSER_TEST_F(NavigationBrowserTest,
     const ukm::mojom::UkmEntry* entry = entries[2];
     ukm_recorder.ExpectEntrySourceHasUrl(entry, url_3);
     // The previous page doesn't have pagehide.
-    EXPECT_LE(*ukm_recorder.GetEntryMetric(entry, kTargetUkmMetricName),
-              ukm::GetExponentialBucketMinForFineUserTiming(100u));
+    EXPECT_NEAR(*ukm_recorder.GetEntryMetric(entry, kTargetUkmMetricName), 0u,
+                100u);
   }
 
-  EXPECT_TRUE(ExecJs(web_contents(), R"(
-    window.addEventListener('pagehide', function() {
-      const start = Date.now();
-      // Wait for 1 second.
-      while (Date.now() - start <= 1000) {
-      }
-    });
-  )"));
+  AddSlowPagehideEventHandlerToCurrentWebContents();
 
   {
     GURL url_4(embedded_test_server()->GetURL("b.test", "/title1.html"));
@@ -9063,8 +9086,8 @@ IN_PROC_BROWSER_TEST_F(NavigationBrowserTest,
     ukm_recorder.ExpectEntrySourceHasUrl(entry, url_4);
     // The previous page's pagehide event runs in a different renderer process,
     // so this navigation is not blocked.
-    EXPECT_LE(*ukm_recorder.GetEntryMetric(entry, kTargetUkmMetricName),
-              ukm::GetExponentialBucketMinForFineUserTiming(100u));
+    EXPECT_NEAR(*ukm_recorder.GetEntryMetric(entry, kTargetUkmMetricName), 0u,
+                100u);
   }
 }
 
--- third_party/blink/renderer/core/loader/frame_loader.cc
+++ third_party/blink/renderer/core/loader/frame_loader.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/auto_reset.h"
 #include "base/notreached.h"
+#include "base/time/time.h"
 #include "base/trace_event/typed_macros.h"
 #include "base/unguessable_token.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
@@ -410,8 +411,13 @@ void FrameLoader::DispatchUnloadEventAndFillOldDocumentInfoIfNeeded(
       (frame_->GetPage()->GetFocusController().FocusedFrame() == frame_);
   old_document_info->overlay_color = frame_->GetFrameOverlayColor();
 
+  base::ElapsedTimer elapsed_timer;
   frame_->GetDocument()->DispatchUnloadEvents(
       &old_document_info->unload_timing_info);
+  old_document_info->total_lifecycle_events_processing_time_on_commit =
+      std::max(
+          old_document_info->total_lifecycle_events_processing_time_on_commit,
+          elapsed_timer.Elapsed());
 }
 
 void FrameLoader::DidExplicitOpen() {
@@ -1154,8 +1160,11 @@ void FrameLoader::CommitNavigation(
   auto url_origin = SecurityOrigin::Create(navigation_params->url);
   ScopedOldDocumentInfoForCommitCapturer scoped_old_document_info(
       MakeGarbageCollected<OldDocumentInfoForCommit>(url_origin));
+  scoped_old_document_info.CurrentInfo()
+      ->total_lifecycle_events_processing_time_on_commit =
+      navigation_params->navigation_timings
+          .total_lifecycle_events_processing_time_on_commit;
 
-  base::TimeDelta total_lifecycle_events_processing_time_on_commit;
   FrameSwapScope frame_swap_scope(frame_owner);
   {
     base::AutoReset<bool> scoped_committing(&committing_navigation_, true);
@@ -1187,7 +1196,6 @@ void FrameLoader::CommitNavigation(
       return;
     }
 
-    base::ElapsedTimer elapsed_timer;
     // If the frame is provisional, swap it in now. However, if `SwapIn()`
     // returns false, JS caused `frame_` to be removed, so just return. In case
     // this triggers a local RenderFrame swap, it might trigger the unloading
@@ -1198,7 +1206,6 @@ void FrameLoader::CommitNavigation(
     // the provisional frame's document isn't the one that gets used.
     if (is_provisional && !frame_->SwapIn())
       return;
-    total_lifecycle_events_processing_time_on_commit = elapsed_timer.Elapsed();
   }
 
   tls_version_warning_origins_.clear();
@@ -1241,12 +1248,6 @@ void FrameLoader::CommitNavigation(
         std::move(navigation_params->policy_container));
   }
 
-  // Read the value before `navigation_params` is moved below. This value is
-  // recorded in UKM after CommitDocumentLoader() creates a new document.
-  total_lifecycle_events_processing_time_on_commit +=
-      navigation_params->navigation_timings
-          .total_lifecycle_events_processing_time_on_commit;
-
   // TODO(dgozman): get rid of provisional document loader and most of the code
   // below. We should probably call DocumentLoader::CommitNavigation directly.
   DocumentLoader* new_document_loader = MakeGarbageCollected<DocumentLoader>(
@@ -1265,7 +1266,8 @@ void FrameLoader::CommitNavigation(
         frame_->GetDocument()->UkmSourceID())
         .SetPageLifecycleEventsTotalProcessingTime(
             ukm::GetExponentialBucketMinForFineUserTiming(
-                total_lifecycle_events_processing_time_on_commit
+                scoped_old_document_info.CurrentInfo()
+                    ->total_lifecycle_events_processing_time_on_commit
                     .InMilliseconds()))
         .Record(frame_->GetDocument()->UkmRecorder());
   }
