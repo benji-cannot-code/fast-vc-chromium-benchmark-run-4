954350d07b3516b1cb0352f887451b2eef47ff92
Dan Murphy <dmurph@chromium.org>
2025-10-31 11:58:51 -0700

    [PWA] Simple browser test for PreinstalledWebAppManager
    This change adds a simple browser test for PreinstalledWebAppManager,
    where it expects the normal startup mechanism of preinstalling to work,
    with minimal testing overrides.
    
    The tests check that the preinstall works, and then the delayed update
    code works.
    
    Bug: 456554331
    Change-Id: I55ed30dab4e6b759458998281184c7d60578c5ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7096759
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Auto-Submit: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1538764}

--- chrome/browser/web_applications/web_app_provider.h
+++ chrome/browser/web_applications/web_app_provider.h
@@ -16,7 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/types/pass_key.h"
 #include "build/build_config.h"
 #include "components/keyed_service/core/keyed_service.h"
+#include "components/webapps/common/manifest_id_constants.h"
 #include "components/webapps/common/web_app_id.h"
+#include "url/gurl.h"
 
 class Profile;
 
