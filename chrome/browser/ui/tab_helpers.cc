64821de1faa54e7d8ba53926e12889c0a8172990
Tom Van Goethem <tov@chromium.org>
2025-10-30 18:08:37 -0700

    [FPF-CI] Remove block canvas readback experiment
    As per https://privacysandbox.google.com/overview/status, Canvas Interventions will be deprecated. This CL removes the experiment where canvas readback is blocked, which was introduced in crrev.com/c/6926120.
    
    Bug: 454083467
    Change-Id: I9ea54e5ada5bc738387ca62f1c6a767aae25cf0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7095463
    Auto-Submit: Tom Van Goethem <tov@chromium.org>
    Reviewed-by: Jean-Philippe Gravel <jpgravel@chromium.org>
    Reviewed-by: John Kim <johnykim@google.com>
    Reviewed-by: Mike Taylor <miketaylr@chromium.org>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Tom Van Goethem <tov@chromium.org>
    Reviewed-by: Zainab Rizvi <rizvis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1538332}

--- chrome/browser/ui/tab_helpers.cc
+++ chrome/browser/ui/tab_helpers.cc
@@ -179,8 +179,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/javascript_dialogs/javascript_tab_modal_dialog_manager_delegate_android.h"
 #include "components/facilitated_payments/core/features/features.h"
 #include "components/fingerprinting_protection_filter/common/fingerprinting_protection_filter_features.h"
-#include "components/fingerprinting_protection_filter/interventions/browser/canvas_interventions_web_contents_helper.h"
-#include "components/fingerprinting_protection_filter/interventions/common/interventions_features.h"
 #include "components/ip_protection/common/ip_protection_status.h"
 #include "components/page_load_metrics/browser/features.h"
 #include "components/sensitive_content/android/android_sensitive_content_client.h"
@@ -379,18 +377,6 @@ void TabHelpers::AttachTabHelpers(WebContents* web_contents) {
         HostContentSettingsMapFactory::GetForProfile(profile),
         TrackingProtectionSettingsFactory::GetForProfile(profile),
         profile->IsIncognitoProfile());
-  }
-
-  if (fingerprinting_protection_interventions::features::
-          ShouldBlockCanvasReadbackForIncognitoState(
-              profile->IsIncognitoProfile()) ||
-      fingerprinting_protection_interventions::features::
-          IsCanvasInterventionsEnabledForIncognitoState(
-              profile->IsIncognitoProfile())) {
-    fingerprinting_protection_interventions::
-        CanvasInterventionsWebContentsHelper::CreateForWebContents(
-            web_contents,
-            profile->IsIncognitoProfile());
   }
 
   // Only create the IpProtectionStatus if the User Bypass feature is enabled.
