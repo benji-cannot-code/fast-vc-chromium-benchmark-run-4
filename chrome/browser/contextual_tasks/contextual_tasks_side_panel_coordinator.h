07093f694983063a3932b679598f52a9b10beb8d
Yuheng Huang <yuhengh@chromium.org>
2025-10-31 14:39:58 -0700

    [Contextual tasks] fix dangling ptr of WebView
    Fix a bug of dangling pointer of a WebView. Also change the contextual
    task menu item to able to close the side panel if it's open for testing.
    
    Bug: None
    Change-Id: I877f110f10311d3db731c5146d87b117a9538ff0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7107412
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1538864}

--- chrome/browser/contextual_tasks/contextual_tasks_side_panel_coordinator.h
+++ chrome/browser/contextual_tasks/contextual_tasks_side_panel_coordinator.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_CONTEXTUAL_TASKS_CONTEXTUAL_TASKS_SIDE_PANEL_COORDINATOR_H_
 
 #include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "chrome/browser/contextual_tasks/contextual_tasks_ui.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "ui/base/unowned_user_data/scoped_unowned_user_data.h"
@@ -27,6 +28,7 @@ class WebView;
 namespace contextual_tasks {
 
 class ContextualTasksContextController;
+class ContextualTasksWebView;
 
 class ContextualTasksSidePanelCoordinator {
  public:
@@ -86,9 +88,10 @@ class ContextualTasksSidePanelCoordinator {
   // Context controller to query task information.
   const raw_ptr<ContextualTasksContextController> context_controller_;
 
-  // WebView for the current side panel. The WebContents in the WebView is owned
-  // by the cache and can change based on active task change.
-  raw_ptr<views::WebView> web_view_ = nullptr;
+  // WebView of the current side panel. It's owned by side panel framework so
+  // weak pointer is needed in case it's destroyed. The WebContents in the
+  // WebView is owned by the cache and can change based on active task change.
+  base::WeakPtr<ContextualTasksWebView> web_view_ = nullptr;
 
   // WebContents cache for each task.
   // It's okay to assume there is only 1 WebContents per task per window.
