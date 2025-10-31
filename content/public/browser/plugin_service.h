b07532a2c78d85ce1d2dae2ba47a319ee4557020
Lei Zhang <thestig@chromium.org>
2025-10-30 17:52:24 -0700

    Remove unused PluginService(Impl) code
    After PPAPI removal, PluginService::GetRegisteredPluginInfo() and
    PluginServiceImpl::SetMaxPpapiProcessesPerProfileForTesting() have no
    callers. Remove these methods and related code.
    
    Change-Id: I85e198548ede5e70fd632135f33cb96ee68f265e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7103074
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1538322}

--- content/public/browser/plugin_service.h
+++ content/public/browser/plugin_service.h
@@ -28,7 +28,6 @@ namespace content {
 
 class BrowserContext;
 class PluginServiceFilter;
-struct ContentPluginInfo;
 struct WebPluginInfo;
 
 // This class lives on the UI thread.
@@ -92,12 +91,6 @@ class CONTENT_EXPORT PluginService {
   // Synchronously loads plugins if necessary and returns the list of plugin
   // infos.
   virtual std::vector<WebPluginInfo> GetPluginsSynchronous() = 0;
-
-  // Returns information about a plugin if it exists, otherwise `nullptr`. The
-  // caller does not own the pointer, and it's not guaranteed to live past the
-  // call stack.
-  virtual const ContentPluginInfo* GetRegisteredPluginInfo(
-      const base::FilePath& plugin_path) = 0;
 
   virtual void SetFilter(PluginServiceFilter* filter) = 0;
   virtual PluginServiceFilter* GetFilter() = 0;
