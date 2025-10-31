083493d3b7d8710c8590d41ba8d0d5c4085ed71e
Aishwarya Rajesh <aishwaryarj@google.com>
2025-10-31 16:09:44 -0700

    [RWM] Add metric Android.MultiWindowMode.CloseWindow.AppSource
    The CL adds an enumerated histogram to track app-triggered hard
    closures:
    1) From the window manager
    2) From instance retention expiration
    3) From last tab closure
    
    Bug: 454636721
    Change-Id: Icf06c2699004a7e802ff8f91e9c78194a5777c42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7101067
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1538896}

--- chrome/android/javatests/src/org/chromium/chrome/browser/multiwindow/MultiInstanceManagerApi31Test.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/multiwindow/MultiInstanceManagerApi31Test.java
@@ -31,6 +31,7 @@ import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
+import org.chromium.chrome.browser.multiwindow.MultiInstanceManager.CloseWindowAppSource;
 import org.chromium.chrome.browser.multiwindow.MultiInstanceManager.PersistedInstanceType;
 import org.chromium.chrome.browser.preferences.ChromePreferenceKeys;
 import org.chromium.chrome.browser.preferences.ChromeSharedPreferences;
@@ -114,13 +115,11 @@ public class MultiInstanceManagerApi31Test {
                                             .getActivity()
                                             .getMultiInstanceMangerForTesting();
                     multiInstanceManager.closeInstance(
-                            otherActivities[0].getWindowIdForTesting(),
-                            otherActivities[0].getTaskId());
+                            otherActivities[0].getWindowIdForTesting(), CloseWindowAppSource.OTHER);
                     multiInstanceManager.closeInstance(
-                            otherActivities[1].getWindowIdForTesting(),
-                            otherActivities[1].getTaskId());
+                            otherActivities[1].getWindowIdForTesting(), CloseWindowAppSource.OTHER);
                     multiInstanceManager.closeInstance(
-                            newActivity.getWindowIdForTesting(), newActivity.getTaskId());
+                            newActivity.getWindowIdForTesting(), CloseWindowAppSource.OTHER);
                 });
     }
 
@@ -168,10 +167,9 @@ public class MultiInstanceManagerApi31Test {
                                             .getActivity()
                                             .getMultiInstanceMangerForTesting();
                     multiInstanceManager.closeInstance(
-                            otherActivities[0].getWindowIdForTesting(),
-                            otherActivities[0].getTaskId());
+                            otherActivities[0].getWindowIdForTesting(), CloseWindowAppSource.OTHER);
                     multiInstanceManager.closeInstance(
-                            newActivity.getWindowIdForTesting(), newActivity.getTaskId());
+                            newActivity.getWindowIdForTesting(), CloseWindowAppSource.OTHER);
                 });
     }
 
