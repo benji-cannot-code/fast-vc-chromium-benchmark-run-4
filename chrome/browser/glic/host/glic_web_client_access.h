3cead416022efad43a340ecd59bade3d7fdcb766
Chris Fredrickson <cfredric@chromium.org>
2025-10-31 13:45:59 -0700

    Glic multi instance support for confirmation IPCs to web client
    Bug: 439672418, 454961534
    Change-Id: Ie4843f1036b6ba35f522125ef5572d3802c2cf44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7087650
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1538828}

--- chrome/browser/glic/host/glic_web_client_access.h
+++ chrome/browser/glic/host/glic_web_client_access.h
@@ -8,7 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Interface to the glic web client, provided by the glic WebUI.
 #include "base/functional/callback_forward.h"
+#include "chrome/browser/actor/actor_task_delegate.h"
 #include "chrome/browser/glic/host/glic.mojom.h"
+#include "url/origin.h"
 
 namespace glic {
 
@@ -43,6 +45,20 @@ class GlicWebClientAccess {
 
   // Informs the web client that additional context is available.
   virtual void NotifyAdditionalContext(mojom::AdditionalContextPtr context) = 0;
+
+  virtual void RequestToShowCredentialSelectionDialog(
+      actor::TaskId task_id,
+      const base::flat_map<std::string, gfx::Image>& icons,
+      const std::vector<actor_login::Credential>& credentials,
+      actor::ActorTaskDelegate::CredentialSelectedCallback callback) = 0;
+  virtual void RequestToShowUserConfirmationDialog(
+      actor::TaskId task_id,
+      const url::Origin& navigation_origin,
+      actor::ActorTaskDelegate::UserConfirmationDialogCallback callback) = 0;
+  virtual void RequestToConfirmNavigation(
+      actor::TaskId task_id,
+      const url::Origin& navigation_origin,
+      actor::ActorTaskDelegate::NavigationConfirmationCallback callback) = 0;
 };
 
 }  // namespace glic
