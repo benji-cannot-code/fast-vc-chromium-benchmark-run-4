1e4497302383fa66461b59cce2ff177126492077
Kent Tamura <tkent@chromium.org>
2025-10-30 18:58:12 -0700

    url: Use string_views for ParseAuthority() arguments
    * ParseAuthority(const char16_t*, ...) without ParseMode is removed
      because it's deprecated and no one uses it.
    
    * DoParseAfter*SpecialScheme() are also changed to use string_views
      because they call DoParseAuthority().
    
    This CL has no behavior changes.
    
    Bug: 350788890
    Change-Id: I78e9db9a1b116beeacd1a036d2e8a3eceaea401a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7100138
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Owners-Override: Hayato Ito <hayato@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1538339}

--- url/third_party/mozilla/url_parse.h
+++ url/third_party/mozilla/url_parse.h
@@ -390,20 +390,13 @@ void ParseAuthority(const char* spec,
                     Component* password,
                     Component* hostname,
                     Component* port_num);
-COMPONENT_EXPORT(URL)
-void ParseAuthority(const char16_t* spec,
-                    const Component& auth,
-                    Component* username,
-                    Component* password,
-                    Component* hostname,
-                    Component* port_num);
 
 // Does a best effort parse of input `spec`, in range `auth`. If a particular
 // component is not found, it will be set to invalid. `ParserMode` is used to
 // determine the appropriate authority terminator. See `IsAuthorityTerminator`
 // for details.
 COMPONENT_EXPORT(URL)
-void ParseAuthority(const char* spec,
+void ParseAuthority(std::string_view spec,
                     const Component& auth,
                     ParserMode parser_mode,
                     Component* username,
@@ -411,7 +404,7 @@ void ParseAuthority(const char* spec,
                     Component* hostname,
                     Component* port_num);
 COMPONENT_EXPORT(URL)
-void ParseAuthority(const char16_t* spec,
+void ParseAuthority(std::u16string_view spec,
                     const Component& auth,
                     ParserMode parser_mode,
                     Component* username,
