007d820013125e934babef92200da58fd75a50c0
Andy Phan <andyphan@chromium.org>
2025-10-30 17:01:37 -0700

    [PDF Ink Signatures] Remove unused param in StartTextHighlight()
    The timestamp parameter in PdfInkModule::StartTextHighlight() is unused.
    Remove it.
    
    Change-Id: Ied4af496a4989544ebc77140b175020cd3230789
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7103573
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1538292}

--- pdf/pdf_ink_module.cc
+++ pdf/pdf_ink_module.cc
@@ -374,7 +374,7 @@ bool PdfInkModule::OnMouseDown(const blink::WebMouseEvent& event) {
     MaybeFinishStrokeForMissingMouseUpEvent();
 
     if (IsHighlightingTextAtPosition(position)) {
-      return StartTextHighlight(position, event.ClickCount(), event.TimeStamp(),
+      return StartTextHighlight(position, event.ClickCount(),
                                 ink::StrokeInput::ToolType::kMouse);
     }
 
@@ -488,8 +488,7 @@ bool PdfInkModule::OnTouchStart(const blink::WebTouchEvent& event) {
 
     if (IsHighlightingTextAtPosition(position)) {
       // Multi-click text selection for touch is not supported.
-      return StartTextHighlight(position, /*click_count=*/1, event.TimeStamp(),
-                                tool_type);
+      return StartTextHighlight(position, /*click_count=*/1, tool_type);
     }
     return StartStroke(position, event.TimeStamp(), tool_type);
   }
@@ -925,7 +924,6 @@ void PdfInkModule::EraseHelper(const gfx::PointF& position, int page_index) {
 
 bool PdfInkModule::StartTextHighlight(const gfx::PointF& position,
                                       int click_count,
-                                      base::TimeTicks timestamp,
                                       ink::StrokeInput::ToolType tool_type) {
   client_->StrokeStarted();
 
