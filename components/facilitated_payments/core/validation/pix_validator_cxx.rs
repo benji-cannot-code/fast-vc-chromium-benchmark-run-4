8f2c2e04c5d555bc7599edee111ff649cb88da06
Daniel Cheng <dcheng@chromium.org>
2025-10-31 14:21:43 -0700

    Implement Rust version of Pix code validator
    This CL does not implement a Rust version of `ContainsPixIdentifier()`,
    as it isn't needed: Rust doesn't need a fast reject, as it can simply
    validate in-process.
    
    Bug: 454668204
    Change-Id: I4e44cb4288f0d60bbfbec37a459a2f3cf6b00b9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7068618
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1538849}

---
+++ components/facilitated_payments/core/validation/pix_validator_cxx.rs
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chromium::import! {
+    "//components/facilitated_payments/core/validation:pix_validator_rs";
+}
+
+#[cxx::bridge(namespace=payments::facilitated)]
+pub mod ffi {
+    enum PixQrCodeType {
+        Invalid,
+        Dynamic,
+        Static,
+    }
+
+    extern "Rust" {
+        fn get_pix_qr_code_type(code: &[u8]) -> PixQrCodeType;
+    }
+}
+
+impl std::convert::From<Option<pix_validator_rs::PixQrCodeType>> for ffi::PixQrCodeType {
+    fn from(value: Option<pix_validator_rs::PixQrCodeType>) -> Self {
+        match value {
+            None => Self::Invalid,
+            Some(pix_validator_rs::PixQrCodeType::Dynamic) => Self::Dynamic,
+            Some(pix_validator_rs::PixQrCodeType::Static) => Self::Static,
+        }
+    }
+}
+
+fn get_pix_qr_code_type(code: &[u8]) -> ffi::PixQrCodeType {
+    pix_validator_rs::get_pix_qr_code_type(code).into()
+}
