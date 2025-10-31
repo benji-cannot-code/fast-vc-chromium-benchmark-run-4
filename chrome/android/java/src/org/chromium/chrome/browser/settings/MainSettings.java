b6ffe63cc6814f343185a92a11167ef2e612b9e1
Hidehiko Abe <hidehiko@chromium.org>
2025-10-31 05:20:28 -0700

    Update address bar sub title in main menu on settings update.
    This adds callback invoked on updates of address bar position settings,
    so the subtitle in main settings fragment will follow the update.
    
    BUG=454425323
    TEST=Tryjob. Ran on DUT making sure the subtitle in the main menu
         is updated.
    
    Change-Id: I4233f1d60825f2d94deb6ea209513f40ae7ee621
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7082323
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1538520}

--- chrome/android/java/src/org/chromium/chrome/browser/settings/MainSettings.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/MainSettings.java
@@ -9,6 +9,7 @@ import static org.chromium.build.NullUtil.assumeNonNull;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.os.Bundle;
@@ -64,8 +65,8 @@ import org.chromium.chrome.browser.sync.SyncServiceFactory;
 import org.chromium.chrome.browser.sync.settings.ManageSyncSettings;
 import org.chromium.chrome.browser.sync.settings.SignInPreference;
 import org.chromium.chrome.browser.sync.settings.SyncSettingsUtils;
-import org.chromium.chrome.browser.toolbar.ToolbarPositionController;
 import org.chromium.chrome.browser.toolbar.adaptive.AdaptiveToolbarStatePredictor;
+import org.chromium.chrome.browser.toolbar.settings.AddressBarPreference;
 import org.chromium.chrome.browser.toolbar.settings.AddressBarSettingsFragment;
 import org.chromium.chrome.browser.tracing.settings.DeveloperSettings;
 import org.chromium.chrome.browser.ui.messages.snackbar.SnackbarManager;
@@ -98,6 +99,7 @@ import java.util.Map;
 public class MainSettings extends ChromeBaseSettingsFragment
         implements TemplateUrlService.LoadListener,
                 TemplateUrlService.TemplateUrlServiceObserver,
+                SharedPreferences.OnSharedPreferenceChangeListener,
                 SyncService.SyncStateChangedListener,
                 SigninManager.SignInStateObserver,
                 SettingsCustomTabLauncher.SettingsCustomTabLauncherClient {
@@ -193,6 +195,11 @@ public class MainSettings extends ChromeBaseSettingsFragment
             templateUrlService.addObserver(this);
         }
 
+        SharedPreferences sharedPreferences = ContextUtils.getAppSharedPreferences();
+        if (sharedPreferences != null) {
+            sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+        }
+
         SyncService syncService = SyncServiceFactory.getForProfile(getProfile());
         if (syncService != null) {
             syncService.addSyncStateChangedListener(this);
@@ -211,6 +218,12 @@ public class MainSettings extends ChromeBaseSettingsFragment
         if (syncService != null) {
             syncService.removeSyncStateChangedListener(this);
         }
+
+        SharedPreferences sharedPreferences = ContextUtils.getAppSharedPreferences();
+        if (sharedPreferences != null) {
+            sharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
+        }
+
         TemplateUrlService templateUrlService =
                 TemplateUrlServiceFactory.getForProfile(getProfile());
         if (templateUrlService != null) {
@@ -568,7 +581,10 @@ public class MainSettings extends ChromeBaseSettingsFragment
 
         if (showSetting) {
             Preference addressBarPreference = addPreferenceIfAbsent(PREF_ADDRESS_BAR);
-            addressBarPreference.setSummary(ToolbarPositionController.getToolbarPositionResId());
+            addressBarPreference.setSummary(
+                    AddressBarPreference.isToolbarConfiguredToShowOnTop()
+                            ? R.string.address_bar_settings_top
+                            : R.string.address_bar_settings_bottom);
             updateNewPreferenceAndIncrementViewCount(
                     addressBarPreference,
                     AddressBarSettingsFragment.getTitle(getContext()),
@@ -689,6 +705,14 @@ public class MainSettings extends ChromeBaseSettingsFragment
     @Override
     public void onTemplateURLServiceChanged() {
         updateSearchEnginePreference();
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(
+            SharedPreferences sharedPreferences, @Nullable String key) {
+        if (ChromePreferenceKeys.TOOLBAR_TOP_ANCHORED.equals(key)) {
+            updateAddressBarPreference();
+        }
     }
 
     @Override
