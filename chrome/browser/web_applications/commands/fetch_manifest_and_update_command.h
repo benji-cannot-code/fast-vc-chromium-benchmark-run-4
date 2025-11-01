7ac7c6fda06e2046fb7e3e342b4a783a664d5bd4
Dan Murphy <dmurph@chromium.org>
2025-10-31 18:11:47 -0700

    [PWA] Remove initial app locking for FetchManifestAndUpdateCommand
    To prevent holding an app lock while waiting for a url to load or the
    manifest to be fetched, wait until the manifest is fetched and deemed
    different to lock the app for updating. This still means that the icons
    are downloaded with the app lock, but at least we then know that there
    is a manifest change, and the icons also have a timeout anyways.
    
    This change also uses the use_manifest_icons_as_trusted option which was
    recently created, to avoid copying to trusted icons unnecessarily.
    
    R=mek@chromium.org
    
    Fixed: 456773468
    Change-Id: Ia8cda02825dd0c5cc46f64710f8b1c7a492d506a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7106377
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1538925}

--- chrome/browser/web_applications/commands/fetch_manifest_and_update_command.h
+++ chrome/browser/web_applications/commands/fetch_manifest_and_update_command.h
@@ -6,10 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_WEB_APPLICATIONS_COMMANDS_FETCH_MANIFEST_AND_UPDATE_COMMAND_H_
 #define CHROME_BROWSER_WEB_APPLICATIONS_COMMANDS_FETCH_MANIFEST_AND_UPDATE_COMMAND_H_
 
+#include <memory>
+
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/web_applications/commands/fetch_manifest_and_update_result.h"
 #include "chrome/browser/web_applications/commands/web_app_command.h"
 #include "chrome/browser/web_applications/jobs/manifest_to_web_app_install_info_job.h"
+#include "chrome/browser/web_applications/locks/shared_web_contents_lock.h"
 #include "chrome/browser/web_applications/locks/shared_web_contents_with_app_lock.h"
 #include "chrome/browser/web_applications/web_app_install_utils.h"
 #include "components/webapps/browser/install_result_code.h"
@@ -39,7 +42,7 @@ struct WebAppInstallInfo;
 //   and call the appropriate observer.
 // - This command will call the OnWebAppManifestUpdated observer method.
 class FetchManifestAndUpdateCommand
-    : public WebAppCommand<SharedWebContentsWithAppLock,
+    : public WebAppCommand<SharedWebContentsLock,
                            FetchManifestAndUpdateResult> {
  public:
   FetchManifestAndUpdateCommand(const GURL& install_url,
@@ -47,14 +50,14 @@ class FetchManifestAndUpdateCommand
                                 FetchManifestAndUpdateCallback callback);
   ~FetchManifestAndUpdateCommand() override;
 
-  void StartWithLock(
-      std::unique_ptr<SharedWebContentsWithAppLock> lock) override;
+  void StartWithLock(std::unique_ptr<SharedWebContentsLock> lock) override;
 
  private:
   void OnUrlLoaded(webapps::WebAppUrlLoaderResult result);
   void OnManifestRetrieved(
       const base::expected<blink::mojom::ManifestPtr,
                            blink::mojom::RequestManifestErrorPtr>& result);
+  void OnAppLockAcquired();
   void OnWebAppInfoCreatedFromManifest(
       std::unique_ptr<WebAppInstallInfo> install_info);
   void OnIconsFetched();
@@ -64,7 +67,8 @@ class FetchManifestAndUpdateCommand
   const GURL install_url_;
   const webapps::ManifestId expected_manifest_id_;
 
-  std::unique_ptr<SharedWebContentsWithAppLock> lock_;
+  std::unique_ptr<SharedWebContentsLock> web_contents_lock_;
+  std::unique_ptr<SharedWebContentsWithAppLock> app_lock_;
   std::unique_ptr<webapps::WebAppUrlLoader> url_loader_;
   std::unique_ptr<WebAppDataRetriever> data_retriever_;
   std::unique_ptr<WebAppInstallInfo> install_info_;
