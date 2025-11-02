a5d148beb312c6b4ecbe3e934d9d4d870aa3c39c
Venkatesh Srinivas <venkateshs@chromium.org>
2025-11-01 17:32:20 -0700

    FieldTrial: Convert FieldTrialEntry::activated to std::atomic<uint32_t>
    FieldTrialEntry used a legacy Atomic32 as a concurrent flag. Convert to
    an std::atomic<uint32_t>, as std::atomic is standard and broadly supported. FieldTrialEntry::kPersistentTypeId is not changed since the representation of the type has not changed.
    
    No behavior change expected.
    
    Bug: 40175832
    Change-Id: Iede6c368738fcf07edacd15173f32e148d92cb13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4338018
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Commit-Queue: Venkatesh Srinivas <venkateshs@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1539064}

--- base/metrics/field_trial_entry.h
+++ base/metrics/field_trial_entry.h
@@ -9,12 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
+#include <atomic>
 #include <map>
 #include <string>
 #include <string_view>
 #include <vector>
 
-#include "base/atomicops.h"
 #include "base/base_export.h"
 #include "base/compiler_specific.h"
 #include "base/metrics/persistent_memory_allocator.h"
@@ -48,11 +48,8 @@ struct BASE_EXPORT FieldTrialEntry {
   }
 
   // Whether or not this field trial is activated. This is really just a
-  // boolean but using a 32 bit value for portability reasons. It should be
-  // accessed via NoBarrier_Load()/NoBarrier_Store() to prevent the compiler
-  // from doing unexpected optimizations because it thinks that only one
-  // thread is accessing the memory location.
-  subtle::Atomic32 activated;
+  // boolean but using a 32 bit value for portability reasons.
+  std::atomic<uint32_t> activated;
 
   // On e.g. x86, alignof(uint64_t) is 4.  Ensure consistent size and
   // alignment of `pickle_size` across platforms. This can be considered
