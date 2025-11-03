b2218d40204b6ca3a54261da869cfc2f92224213
Marc Treib <treib@chromium.org>
2025-11-03 10:06:03 -0800

    Sync: Introduce ConfigureDataTypeManagerReason histogram
    This adds a new histogram Sync.ConfigureDataTypeManagerReason,
    which records the "reason" a configuration was triggered.
    
    Bug: 40897778
    Change-Id: I0968bf83fc142efc3c75035c9ccde4516a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7112780
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1539436}

--- components/sync/service/sync_service_impl.cc
+++ components/sync/service/sync_service_impl.cc
@@ -1740,6 +1740,9 @@ void SyncServiceImpl::ConfigureDataTypeManager(
                                     ? ConfigureDataTypeManagerOption::kTransport
                                     : ConfigureDataTypeManagerOption::kFeature);
 
+  base::UmaHistogramEnumeration("Sync.ConfigureDataTypeManagerReason",
+                                configure_context.reason);
+
   base::UmaHistogramBoolean("Sync.ConfigureDataTypeManager.Start",
                             is_first_time_sync_configure_);
 
