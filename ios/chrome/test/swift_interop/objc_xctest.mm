dbb4e2ef34a2647c113cd61a56329b4500757b6c
Justin Novosad <junov@google.com>
2025-11-03 15:48:23 -0800

    Add support for Objective-C calling Swift with C++ interop enabled
    Key changes include:
    - Modifying `swift_source_set.gni` to expose necessary SDK header include
      paths for C++ interop.
    - Introducing a new GN config `swift_cxx_interop_includes` to define
      these include paths.
    - Adding a temporary workaround in `objc_interop_tests` to suppress
      clang errors on iOS SDK headers when C++ interop is used.
    - Adding a new Objective-C++ unit test (`testCallSwiftFromObjC`) to
      verify successful calls from Objective-C to Swift code using C++ interop.
    
    Bug: 456798764
    Change-Id: I9ebd0123af5c764db7c5a7c2047dcbb05e501187
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7103507
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1539677}

--- ios/chrome/test/swift_interop/objc_xctest.mm
+++ ios/chrome/test/swift_interop/objc_xctest.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <XCTest/XCTest.h>
 
 #import "base/logging.h"
+#import "ios/chrome/test/swift_interop/swift_interop_tests.h"
 
 @interface ObjCInteropTestCase : XCTestCase
 @end
@@ -15,6 +16,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)testEmpty {
   LOG(INFO) << "This is a dependency on //base";
+}
+
+- (void)testCallSwiftFromObjC {
+  // Verify that Objective-C can call swift code that was compiled with
+  // C++ interop enabled.
+  EnumTest* test = [[EnumTest alloc] init];
+  NSError* error = nil;
+  BOOL success = [test testEnumsAndReturnError:&error];
+  XCTAssertTrue(success);
+  XCTAssertNil(error, @"Error: %@", error);
 }
 
 @end
