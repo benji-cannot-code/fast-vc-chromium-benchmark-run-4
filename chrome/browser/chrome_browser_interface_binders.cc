19d3239d49a340cf9eb50210736c4d7bf83c2622
Lei Zhang <thestig@chromium.org>
2025-11-03 14:27:44 -0800

    [PDF] Implement PdfHelpBubbleHandlerFactory
    The existing user_education::HelpBubbleHandler class lives in a webui
    sub-directory, and as such, it expects to be able to access a WebUI.
    Meanwhile, the PDF Viewer does not have a WebUI, even though it uses web
    technologies. To bridge this gap, add a new PdfHelpBubbleHandlerFactory
    class to handle calls to the interface with the same name. Internally,
    PdfHelpBubbleHandlerFactory creates a custom WebContentsGetter
    implementation for use with HelpBubbleHandler that is designed to work
    with the PDF Viewer.
    
    Then register PdfHelpBubbleHandlerFactory with the browser's binder map.
    After this CL, https://crrev.com/c/7056207 will call it from the TS
    side.
    
    Bug: 417240168
    Change-Id: Ia1d126406648f6cc4e3c80efb3f131a7815ebdc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7056325
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1539626}

--- chrome/browser/chrome_browser_interface_binders.cc
+++ chrome/browser/chrome_browser_interface_binders.cc
@@ -62,6 +62,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/binder_map.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/self_owned_receiver.h"
+#include "pdf/buildflags.h"
 #include "services/image_annotation/public/mojom/image_annotation.mojom.h"
 #include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/common/features_generated.h"
@@ -140,6 +141,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/speech/speech_recognition_service_factory.h"
 #include "media/mojo/mojom/speech_recognition_service.mojom.h"
 #endif  // BUILDFLAG(ENABLE_BROWSER_SPEECH_SERVICE)
+
+#if BUILDFLAG(ENABLE_PDF)
+#include "chrome/browser/pdf/pdf_help_bubble_handler_factory.h"
+#include "ui/webui/resources/cr_components/help_bubble/help_bubble.mojom.h"
+#endif
 
 #if BUILDFLAG(ENABLE_PRINT_PREVIEW)
 #include "chrome/browser/printing/web_api/web_printing_service_binder.h"
@@ -566,6 +572,11 @@ void PopulateChromeFrameBinders(
 #if BUILDFLAG(IS_WIN)
   map->Add<media::mojom::MediaFoundationPreferences>(
       &BindMediaFoundationPreferences);
+#endif
+
+#if BUILDFLAG(ENABLE_PDF)
+  map->Add<help_bubble::mojom::PdfHelpBubbleHandlerFactory>(
+      &pdf::PdfHelpBubbleHandlerFactory::Create);
 #endif
 
 #if BUILDFLAG(ENABLE_PRINT_PREVIEW)
