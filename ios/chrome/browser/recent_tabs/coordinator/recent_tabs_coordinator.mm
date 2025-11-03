b32af014d71e6ab4cb83c4751162fff07966fdd6
Arthur Milchior <arthurmilchior@google.com>
2025-11-03 10:31:58 -0800

    [iOS]Stops propagate to the signin coordinator
    When the Recent Tabs Coordinator is stopped, it stops the signin
    coordinator, ensuring that its dealloc does not CHECK.
    
    Fixed: 456151604
    Change-Id: I7ea159d0e4534f695a6885f5611c9c1a84de02e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7106120
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1539459}

--- ios/chrome/browser/recent_tabs/coordinator/recent_tabs_coordinator.mm
+++ ios/chrome/browser/recent_tabs/coordinator/recent_tabs_coordinator.mm
@@ -172,6 +172,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self.recentTabsNavigationController
       dismissViewControllerAnimated:YES
                          completion:self.completion];
+  [self stopSigninCoordinator];
   self.recentTabsNavigationController = nil;
   self.recentTabsContextMenuHelper = nil;
   [self.sharingCoordinator stop];
