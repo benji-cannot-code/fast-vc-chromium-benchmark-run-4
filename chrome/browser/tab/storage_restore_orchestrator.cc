f0a765dbbd9b95bfd63705c09084aaabb53117a4
Fiaz Muhammad <mfiaz@google.com>
2025-11-03 14:13:55 -0800

    [Persist] Init StorageRestoreOrchestrator
    - Init StorageRestoreOrchestrator and android class.
    - StorageRestoreOrchestrator is owned by Java. It must be used in the
    	duration StorageLoadedData is available (the restoration
    	process).
    
    Bug: 456508880
    Change-Id: I5a4fbac03ac10862059adc2363506c0998de69a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7113138
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Auto-Submit: Fiaz Muhammad <mfiaz@google.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1539618}

---
+++ chrome/browser/tab/storage_restore_orchestrator.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/tab/storage_restore_orchestrator.h"
+
+namespace tabs {
+
+StorageRestoreOrchestrator::StorageRestoreOrchestrator(
+    TabStripCollection* collection,
+    TabStateStorageService* service,
+    StorageLoadedData* loaded_data)
+    : collection_(collection), service_(service), loaded_data_(loaded_data) {}
+
+StorageRestoreOrchestrator::~StorageRestoreOrchestrator() = default;
+
+void StorageRestoreOrchestrator::OnChildrenAdded(
+    const TabCollection::Position& position,
+    const TabCollectionNodes& handles) {}
+
+void StorageRestoreOrchestrator::OnChildrenRemoved(
+    const TabCollectionNodes& handles) {}
+
+void StorageRestoreOrchestrator::OnChildMoved(
+    const TabCollection::Position& to_position,
+    const NodeData& node_data) {}
+
+void StorageRestoreOrchestrator::Save() {}
+
+}  // namespace tabs
