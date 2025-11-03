2b2423013f1e896eeaddf20bbd84eeb3ea29b424
Nicolas MacBeth <nicolasmacbeth@google.com>
2025-11-03 10:39:31 -0800

    [iOS] Web page reported og:image snackbar/sheet on pageload
    When the flag is enabled, surface the web-page reported og:image in a
    sheet (notified through a snackbar) which also shows the image's
    resolution. Also reports if no og:image is present. Screenshots:
    https://screenshot.googleplex.com/4N7CQ5Ppx9pK9fo and
    https://screenshot.googleplex.com/83SY5gEGhERksti
    
    Fixed: 455903668
    Change-Id: Ic0cd9788e395bad49e157428174da3a72493559d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7098041
    Auto-Submit: Nicolas MacBeth <nicolasmacbeth@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1539469}

--- ios/chrome/browser/browser_view/ui_bundled/tab_lifecycle_mediator.mm
+++ ios/chrome/browser/browser_view/ui_bundled/tab_lifecycle_mediator.mm
@@ -285,7 +285,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     // TODO(crbug.com/448157489): Remove this or refactor to
     // `HandlerForProtocol`.
-    if (IsAskGeminiSnackbarEnabled()) {
+    if (IsAskGeminiSnackbarEnabled() || IsWebPageReportedImagesSheetEnabled()) {
       BWGTabHelper->SetSnackbarCommandsHandler(
           static_cast<id<SnackbarCommands>>(_commandDispatcher));
     }
