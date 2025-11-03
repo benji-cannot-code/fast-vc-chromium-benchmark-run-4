5024c3bf9a252aa98aeac8da436b84ad29ffa4ac
Lei Zhang <thestig@chromium.org>
2025-11-03 11:39:02 -0800

    Update libtess2 to 8dbd6483e920311a58c9af10a10beb278efebc36
    https://github.com/memononen/libtess2/compare/9a450cc..8dbd648
    
    2025-10-16 me@txgk.in Fix some compilation warnings
    2025-10-07 memononen@gmail.com Merge pull request #81 from sfreilich/loosen-assert
    2025-10-07 sfreilich@google.com Very slightly loosen some asserts by adding FLT_MIN tolerance
    
    Change-Id: I854b039951312af3edae070cf1aa174ef9af56d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7108464
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1539517}

--- third_party/libtess2/src/Source/sweep.c
+++ third_party/libtess2/src/Source/sweep.c
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 */
 
 #include "libtess2/tess_assert.h"
+#include <float.h>
 #include <stddef.h>
 #include <setjmp.h>		/* longjmp */
 
@@ -606,11 +607,14 @@ static int CheckForIntersect( TESStesselator *tess, ActiveRegion *regUp )
 	DebugEvent( tess );
 
 	tesedgeIntersect( dstUp, orgUp, dstLo, orgLo, &isect );
-	/* The following properties are guaranteed: */
-	tess_assert( MIN( orgUp->t, dstUp->t ) <= isect.t );
-	tess_assert( isect.t <= MAX( orgLo->t, dstLo->t ));
-	tess_assert( MIN( dstLo->s, dstUp->s ) <= isect.s );
-	tess_assert( isect.s <= MAX( orgLo->s, orgUp->s ));
+	/*
+	 * The following properties are guaranteed (with a little wiggle-room to
+	 * account for loss of precision if the values are subnormal.)
+	 */
+	tess_assert( MIN( orgUp->t, dstUp->t ) <= isect.t + FLT_MIN);
+	tess_assert( isect.t <= MAX( orgLo->t, dstLo->t ) + FLT_MIN);
+	tess_assert( MIN( dstLo->s, dstUp->s ) <= isect.s + FLT_MIN);
+	tess_assert( isect.s <= MAX( orgLo->s, orgUp->s ) + FLT_MIN);
 
 	if( VertLeq( &isect, tess->event )) {
 		/* The intersection point lies slightly to the left of the sweep line,
@@ -1144,7 +1148,9 @@ static void InitEdgeDict( TESStesselator *tess )
 static void DoneEdgeDict( TESStesselator *tess )
 {
 	ActiveRegion *reg;
+#ifndef NDEBUG
 	int fixedEdges = 0;
+#endif
 
 	while( (reg = (ActiveRegion *)dictKey( dictMin( tess->dict ))) != NULL ) {
 		/*
@@ -1154,7 +1160,9 @@ static void DoneEdgeDict( TESStesselator *tess )
 		*/
 		if( ! reg->sentinel ) {
 			tess_assert( reg->fixUpperEdge );
+#ifndef NDEBUG
 			tess_assert( ++fixedEdges == 1 );
+#endif
 		}
 		tess_assert( reg->windingNumber == 0 );
 		DeleteRegion( tess, reg );
