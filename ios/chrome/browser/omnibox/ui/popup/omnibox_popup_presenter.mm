f7bcdd584781cf947dd5818ddd3e9544206bd58f
Radu Nitescu <radunitescu@google.com>
2025-11-03 08:16:51 -0800

    [iOS] Exclude Lens & AIM from bottom omnibox evolution special treatment
    Change-Id: I8709bb4233dd3cee1b6b1594d7c2ab80e3549e25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7112619
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1539365}

--- ios/chrome/browser/omnibox/ui/popup/omnibox_popup_presenter.mm
+++ ios/chrome/browser/omnibox/ui/popup/omnibox_popup_presenter.mm
@@ -394,6 +394,11 @@ const CGFloat kFadeAnimationVerticalOffset = 12;
 }
 
 - (BOOL)useBottomOmniboxInPopup {
+  if (_presentationContext == OmniboxPresentationContext::kLensOverlay ||
+      _presentationContext == OmniboxPresentationContext::kAIMPrototype) {
+    return NO;
+  }
+
   BOOL inPortrait = IsPortrait(self.viewController.view.window);
   if (omnibox::ForceBottomOmniboxInEditState()) {
     return inPortrait;
