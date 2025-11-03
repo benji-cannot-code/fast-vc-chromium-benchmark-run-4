11482c36691f5a1d1116e0982802c258f9974c59
Mingjing Zhang <mjzhang@chromium.org>
2025-11-03 13:14:09 -0800

    Use TransferableResource::GetSize() in media/
    As part of the effort of refactoring TransferableResource to use
    the size from ClientSharedImage (instead of a standalone field), this
    CL replaces the existing usage of TransferableResource::size in media/
    with GetSize().
    
    Bug: 40286368
    Change-Id: I491e9f1de63e141ed3824a4109209a529db58df8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7111261
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Mingjing Zhang <mjzhang@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1539580}

--- media/renderers/video_resource_updater_unittest.cc
+++ media/renderers/video_resource_updater_unittest.cc
@@ -349,7 +349,7 @@ TEST_F(VideoResourceUpdaterTest, SoftwareFrameRGB) {
 #if BUILDFLAG(IS_MAC)
     EXPECT_EQ(resource.resource.format, viz::SinglePlaneFormat::kBGRA_8888);
 #else
-    EXPECT_EQ(resource.resource.size, video_frame->coded_size());
+    EXPECT_EQ(resource.resource.GetSize(), video_frame->coded_size());
 
     if (fmt == PIXEL_FORMAT_XBGR) {
       EXPECT_EQ(resource.resource.format, viz::SinglePlaneFormat::kRGBA_8888);
@@ -375,7 +375,7 @@ TEST_F(VideoResourceUpdaterTest, SoftwareFrameRGBNonOrigin) {
     VideoFrameExternalResource resource =
         updater->CreateExternalResourceFromVideoFrame(video_frame);
     EXPECT_EQ(VideoFrameResourceType::RGB, resource.type);
-    EXPECT_EQ(resource.resource.size, video_frame->coded_size());
+    EXPECT_EQ(resource.resource.GetSize(), video_frame->coded_size());
 
     auto rect = video_frame->visible_rect();
 
@@ -404,7 +404,7 @@ TEST_F(VideoResourceUpdaterTest, SoftwareFrameY16NonOrigin) {
   VideoFrameExternalResource resource =
       updater->CreateExternalResourceFromVideoFrame(video_frame);
   EXPECT_EQ(VideoFrameResourceType::RGB, resource.type);
-  EXPECT_EQ(resource.resource.size, video_frame->coded_size());
+  EXPECT_EQ(resource.resource.GetSize(), video_frame->coded_size());
 
   auto rect = video_frame->visible_rect();
 
@@ -440,7 +440,7 @@ TEST_F(VideoResourceUpdaterTest, SoftwareFrameRGBAF16) {
   VideoFrameExternalResource resource =
       updater->CreateExternalResourceFromVideoFrame(video_frame);
   EXPECT_EQ(VideoFrameResourceType::RGB, resource.type);
-  EXPECT_EQ(resource.resource.size, video_frame->coded_size());
+  EXPECT_EQ(resource.resource.GetSize(), video_frame->coded_size());
 }
 
 TEST_F(VideoResourceUpdaterTest, HighBitFrameNoF16) {
