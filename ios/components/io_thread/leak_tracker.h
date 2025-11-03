d0dfd8a83088cb107cc7e348bc9e99ecfec317ff
Sylvain Defresne <sdefresne@chromium.org>
2025-11-03 08:50:05 -0800

    [ios] Fix unsafe buffer in LeakTracker
    Use an std::array<T, N> instead of a C array as the former enforce
    bound check on indexing, and use range based for loop in the "hack"
    to keep the variables alive.
    
    Bug: 40285824
    Change-Id: Ia1186adc0495b07bc3801d1047aa256d2c061db6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7100485
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1539383}

--- ios/components/io_thread/leak_tracker.h
+++ ios/components/io_thread/leak_tracker.h
@@ -3,15 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #ifndef IOS_COMPONENTS_IO_THREAD_LEAK_TRACKER_H_
 #define IOS_COMPONENTS_IO_THREAD_LEAK_TRACKER_H_
 
 #include <stddef.h>
+
+#include <array>
 
 #include "build/build_config.h"
 
@@ -85,15 +82,14 @@ class LeakTracker : public base::LinkNode<LeakTracker<T>> {
     // Copy the first 3 leak allocation callstacks onto the stack.
     // This way if we hit the CHECK() in a release build, the leak
     // information will be available in mini-dump.
-    const size_t kMaxStackTracesToCopyOntoStack = 3;
-    base::debug::StackTrace stacktraces[kMaxStackTracesToCopyOntoStack];
+    std::array<base::debug::StackTrace, 3> stacktraces;
 
     for (base::LinkNode<LeakTracker<T>>* node = instances()->head();
          node != instances()->end(); node = node->next()) {
       base::debug::StackTrace& allocation_stack =
           node->value()->allocation_stack_;
 
-      if (count < kMaxStackTracesToCopyOntoStack) {
+      if (count < stacktraces.size()) {
         stacktraces[count] = allocation_stack;
       }
 
@@ -109,8 +105,8 @@ class LeakTracker : public base::LinkNode<LeakTracker<T>> {
     // Hack to keep `stacktraces` and `count` alive (so compiler
     // doesn't optimize it out, and it will appear in mini-dumps).
     if (count == 0x1234) {
-      for (size_t i = 0; i < kMaxStackTracesToCopyOntoStack; ++i) {
-        stacktraces[i].Print();
+      for (const base::debug::StackTrace& stacktrace : stacktraces) {
+        stacktrace.Print();
       }
     }
   }
