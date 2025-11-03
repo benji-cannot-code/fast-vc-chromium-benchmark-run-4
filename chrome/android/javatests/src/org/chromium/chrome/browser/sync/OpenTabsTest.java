c9daeac48f1a7c91ce97ab3838fcf1ff80e2efe7
Michael Tatarski <mtatarski@google.com>
2025-11-03 02:05:20 -0800

    Un-batch test suites
    The edited files are depending on the primary account, which is a
    mutable global state and therefore not thread safe. Thus, those test
    suites shouldn't be batched.
    
    Bug: 340914561, 456774681
    Change-Id: Ibdf290cfc9d6abb424661eb94c6845de7e33a82d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7101483
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Auto-Submit: Michael Tatarski <mtatarski@google.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1539242}

--- chrome/android/javatests/src/org/chromium/chrome/browser/sync/OpenTabsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/sync/OpenTabsTest.java
@@ -20,10 +20,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.ThreadUtils;
-import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaNotSatisfiedException;
+import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.tab.Tab;
@@ -49,7 +49,7 @@ import java.util.Map;
 /** Test suite for the open tabs (sessions) sync data type. */
 @RunWith(ChromeJUnit4ClassRunner.class)
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
-@Batch(Batch.PER_CLASS)
+@DoNotBatch(reason = "Manages sign-in state, which is global.")
 public class OpenTabsTest {
     @Rule public SyncTestRule mSyncTestRule = new SyncTestRule();
 
