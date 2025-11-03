b31c35f1c7ca3970bf51bdbdf1da6e465cfe82cc
Tom Sepez <tsepez@chromium.org>
2025-11-03 14:35:21 -0800

    Remove stray ipc_message.h include in ipc_message_unittest.cc
    It is already included as the first header per style guide. Likely
    the result of an overzealous merge.
    
    Change-Id: I7146a8e865ae7a049d8d6a5248d4871755c17f9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7113419
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1539634}

--- ipc/ipc_message_unittest.cc
+++ ipc/ipc_message_unittest.cc
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/platform_thread.h"
 #include "base/values.h"
 #include "build/build_config.h"
-#include "ipc/ipc_message.h"
 #include "ipc/param_traits_macros.h"
 #include "ipc/param_traits_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
