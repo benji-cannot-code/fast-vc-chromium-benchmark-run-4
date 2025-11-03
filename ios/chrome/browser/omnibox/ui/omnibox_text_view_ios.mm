b9d60a3cf4a787d707bc391fe8dec2d25b1d5e0f
Ameur Hosni <ameurhosni@google.com>
2025-11-03 08:03:28 -0800

    [IOS][Multiline] Use the default line fragment padding
    This CL defaults to using the default line fragment padding as the
    current padding still leads the scroll bar to interfere with the
    text.
    
    Bug: 454955370
    Change-Id: I716565cce8d2b5d90d9ebb1d42c0b43bca2d1ce6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7112781
    Auto-Submit: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1539347}

--- ios/chrome/browser/omnibox/ui/omnibox_text_view_ios.mm
+++ ios/chrome/browser/omnibox/ui/omnibox_text_view_ios.mm
@@ -99,7 +99,6 @@ using enum OmniboxKeyboardAction;
     self.textAlignment = NSTextAlignmentNatural;
     self.keyboardType = UIKeyboardTypeWebSearch;
     self.smartQuotesType = UITextSmartQuotesTypeNo;
-    self.textContainer.lineFragmentPadding = 3;
     self.dataDetectorTypes = UIDataDetectorTypeNone;
     self.allowsEditingTextAttributes = NO;
     if (@available(iOS 18, *)) {
