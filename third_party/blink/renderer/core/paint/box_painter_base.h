40108b964112633e00c19610b48ff758e04f942a
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2025-11-03 08:53:26 -0800

    Revert "Fix border-shape painting"
    This reverts commit b574b7f4b2336830564c5579cf45208a883dff56.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5407268251107328
    
    Sample build with failed test: https://ci.chromium.org/b/8699219734605948417
    Affected test(s):
    [ninja://third_party/blink/renderer/controller:blink_unittests/BlockLayoutAlgorithmTest.HandleTextControlPlaceholderCrash](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fthird_party%2Fblink%2Frenderer%2Fcontroller:blink_unittests%2FBlockLayoutAlgorithmTest.HandleTextControlPlaceholderCrash?q=VHash%3A9870520eab331a24)
    [ninja://third_party/blink/renderer/controller:blink_unittests/ElementInnerTest.FirstLineWithTextTransform](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fthird_party%2Fblink%2Frenderer%2Fcontroller:blink_unittests%2FElementInnerTest.FirstLineWithTextTransform?q=VHash%3A9870520eab331a24)
    [ninja://third_party/blink/renderer/controller:blink_unittests/ElementRuleCollectorTest.FirstLineUseCounted](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fthird_party%2Fblink%2Frenderer%2Fcontroller:blink_unittests%2FElementRuleCollectorTest.FirstLineUseCounted?q=VHash%3A9870520eab331a24)
    [ninja://third_party/blink/renderer/controller:blink_unittests/InlineLayoutAlgorithmTest.TypesForFirstLine](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fthird_party%2Fblink%2Frenderer%2Fcontroller:blink_unittests%2FInlineLayoutAlgorithmTest.TypesForFirstLine?q=VHash%3A9870520eab331a24)
    [ninja://third_party/blink/renderer/controller:blink_unittests/LayoutObjectSimTest.FirstLineBackgroundImageDirtyStyleCrash](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fthird_party%2Fblink%2Frenderer%2Fcontroller:blink_unittests%2FLayoutObjectSimTest.FirstLineBackgroundImageDirtyStyleCrash?q=VHash%3A9870520eab331a24)
    and 7 more ...
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5407268251107328&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F7101318&type=BUG
    
    Original change's description:
    > Fix border-shape painting
    >
    > For painting we should use outer path without a stroke data applied.
    >
    > Also, this CL fixes which geometry box is used for shadow painting.
    >
    > Bug: 448653232
    > Change-Id: Ic82dbd1a6f3406d19934c30369a8038a11844002
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7101318
    > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1539255}
    >
    
    Bug: 448653232
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I7703d1acd65dfd4d15c2cc6a3de510186382adc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7112220
    Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Owners-Override: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1539384}

--- third_party/blink/renderer/core/paint/box_painter_base.h
+++ third_party/blink/renderer/core/paint/box_painter_base.h
@@ -35,7 +35,6 @@ class GraphicsContext;
 class ImageResourceObserver;
 class LayoutBox;
 class Node;
-struct BorderShapeReferenceRects;
 struct PaintInfo;
 struct PhysicalRect;
 
@@ -77,7 +76,6 @@ class BoxPainterBase {
       const PaintInfo&,
       const PhysicalRect&,
       const ComputedStyle&,
-      std::optional<BorderShapeReferenceRects> border_shape_rects,
       PhysicalBoxSides sides_to_include = PhysicalBoxSides(),
       bool background_is_skipped = true);
 
