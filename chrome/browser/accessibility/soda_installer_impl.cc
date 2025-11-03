fbbc2a11476a40c8dde1f6a9dee5da65667b8e0b
Evan Liu <evliu@google.com>
2025-11-03 12:51:47 -0800

    Remove DCHECK_LE in SodaInstallerImpl::UpdateAndNotifyOnSodaProgress
    This CL removes the DCHECK_LE that asserts that the downloaded bytes is less than the total bytes. It is now possible for the downloaded bytes to be greater than the total bytes.
    
    Fixed: 449732766
    Change-Id: I57601843ff2b7ce3b6d785c22c864204cec867e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7102302
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1539568}

--- chrome/browser/accessibility/soda_installer_impl.cc
+++ chrome/browser/accessibility/soda_installer_impl.cc
@@ -259,7 +259,6 @@ void SodaInstallerImpl::UpdateAndNotifyOnSodaProgress(
   if (total_bytes == 0)
     return;
 
-  DCHECK_LE(downloaded_bytes, total_bytes);
   int progress =
       100 * std::clamp(static_cast<double>(downloaded_bytes) / total_bytes,
                         0.0, 1.0);
