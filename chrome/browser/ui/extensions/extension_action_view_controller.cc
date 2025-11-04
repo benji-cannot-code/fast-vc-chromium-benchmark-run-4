7f60a5882b6f00d881570a8553bc23118727f580
Shuhei Takahashi <nya@chromium.org>
2025-11-03 17:01:21 -0800

    extensions: Guarantee EAVC methods never crash
    This is a part of the patch series to move stateful UI logic from
    ExtensionActionViewController to ExtensionActionPlatformDelegate.
    
    ExtensionActionViewController does not own the extension or extension
    action in question, so it is possible that it can be accessed after the
    extension is uninstalled. Former behavior of the class varied per
    method: some methods work, but others caused crashes. This is
    undesirable as we factor out UI-dependent logic from the class and its
    lifetime is not directly associated with corresponding views.
    
    This patch adds a guarantee that its methods do not cause crashes even
    after the extension is uninstalled. Also, GetId() is guaranteed to
    return a consistent value.
    
    We will also add a browser test, but in a separate patch.
    
    Bug: 448199168
    Change-Id: I18cba46d5f0c0f55ae52347773d442257608d4a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7100019
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1539714}

--- chrome/browser/ui/extensions/extension_action_view_controller.cc
+++ chrome/browser/ui/extensions/extension_action_view_controller.cc
@@ -180,7 +180,8 @@ ExtensionActionViewController::ExtensionActionViewController(
     extensions::ExtensionRegistry* extension_registry,
     ExtensionsContainerViews* extensions_container,
     std::unique_ptr<ExtensionActionPlatformDelegate> platform_delegate)
-    : extension_(std::move(extension)),
+    : extension_id_(extension->id()),
+      extension_(std::move(extension)),
       browser_(browser),
       profile_(browser->GetProfile()),
       extension_action_(extension_action),
@@ -204,7 +205,7 @@ ExtensionActionViewController::~ExtensionActionViewController() {
 }
 
 std::string ExtensionActionViewController::GetId() const {
-  return extension_->id();
+  return extension_id_;
 }
 
 void ExtensionActionViewController::SetDelegate(
@@ -587,8 +588,14 @@ bool ExtensionActionViewController::GetExtensionCommand(
 ToolbarActionViewController::HoverCardState
 ExtensionActionViewController::GetHoverCardState(
     content::WebContents* web_contents) const {
-  DCHECK(ExtensionIsValid());
   DCHECK(web_contents);
+
+  if (!ExtensionIsValid()) {
+    HoverCardState state;
+    state.site_access = HoverCardState::SiteAccess::kExtensionDoesNotWantAccess;
+    state.policy = HoverCardState::AdminPolicy::kNone;
+    return state;
+  }
 
   url::Origin origin =
       web_contents->GetPrimaryMainFrame()->GetLastCommittedOrigin();
@@ -639,7 +646,9 @@ ExtensionActionViewController::GetIconImageSourceForTesting(
 void ExtensionActionViewController::TriggerPopup(PopupShowAction show_action,
                                                  bool by_user,
                                                  ShowPopupCallback callback) {
-  DCHECK(ExtensionIsValid());
+  if (!ExtensionIsValid()) {
+    return;
+  }
 
   content::WebContents* const web_contents = GetCurrentWebContents();
   const int tab_id = sessions::SessionTabHelper::IdForTab(web_contents).id();
