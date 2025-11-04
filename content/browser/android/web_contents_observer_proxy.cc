58b424359c07fda70bbf81e99be5f81601878e42
Dan Polanco <polardz@google.com>
2025-11-04 13:52:11 -0800

    Remove DidUpdateAudioMutingState from Java
    The `DidUpdateAudioMutingState` method is no longer required on the Java
    side as the responsibility for managing the media muting state has been
    moved to C++ in MediaStateObserver.
    
    First exposed here:
    https://chromium-review.googlesource.com/c/chromium/src/+/6909732
    
    Bug: 430072416
    Change-Id: I6b2df7c02d1f9a6d0036d8e6d0b0b642dbedf927
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7118685
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Yaron Friedman <yfriedman@chromium.org>
    Auto-Submit: Dan Polanco <polardz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1540315}

--- content/browser/android/web_contents_observer_proxy.cc
+++ content/browser/android/web_contents_observer_proxy.cc
@@ -389,10 +389,4 @@ void WebContentsObserverProxy::WasDiscarded() {
   Java_WebContentsObserverProxy_wasDiscarded(env, java_observer_);
 }
 
-void WebContentsObserverProxy::DidUpdateAudioMutingState(bool muted) {
-  JNIEnv* env = AttachCurrentThread();
-  Java_WebContentsObserverProxy_didUpdateAudioMutingState(env, java_observer_,
-                                                          muted);
-}
-
 }  // namespace content
