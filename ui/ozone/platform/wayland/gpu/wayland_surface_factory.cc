14546a659048966fd20e43b0976eaa3e3a49778c
Colin Blundell <blundell@chromium.org>
2025-11-04 00:02:18 -0800

    //ui: Eliminate WaylandSurfaceFactory::CreateNativePixmap(BufferFormat)
    Instead, have it directly implement
    CreateNativePixmap(SharedImageFormat), as the only usage of the former
    is now in SurfaceFactoryOzone::CreateNativePixmap(SIF).
    
    Bug: 450325664
    Change-Id: I1d37a6b79d667691b4a76241d331d349d58b7db8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7105551
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1539811}

--- ui/ozone/platform/wayland/gpu/wayland_surface_factory.cc
+++ ui/ozone/platform/wayland/gpu/wayland_surface_factory.cc
@@ -260,7 +260,7 @@ scoped_refptr<gfx::NativePixmap> WaylandSurfaceFactory::CreateNativePixmap(
     gfx::AcceleratedWidget widget,
     gpu::VulkanDeviceQueue* device_queue,
     gfx::Size size,
-    gfx::BufferFormat format,
+    viz::SharedImageFormat format,
     gfx::BufferUsage usage,
     std::optional<gfx::Size> framebuffer_size) {
   if (framebuffer_size &&
@@ -270,13 +270,14 @@ scoped_refptr<gfx::NativePixmap> WaylandSurfaceFactory::CreateNativePixmap(
 #if defined(WAYLAND_GBM)
   auto* gbm_device = buffer_manager_->GetGbmDevice();
   if (gbm_device && gbm_device->CanCreateBufferForFormat(
-                        GetFourCCFormatFromBufferFormat(format))) {
+                        GetFourCCFormatFromSharedImageFormat(format))) {
     scoped_refptr<GbmPixmapWayland> pixmap =
         base::MakeRefCounted<GbmPixmapWayland>(buffer_manager_);
 
     auto native_usage = BufferUsageToNativePixmapUsage(usage);
-    if (!pixmap->InitializeBuffer(widget, size, format, native_usage,
-                                  framebuffer_size)) {
+    if (!pixmap->InitializeBuffer(widget, size,
+                                  viz::SharedImageFormatToBufferFormat(format),
+                                  native_usage, framebuffer_size)) {
       return nullptr;
     }
     return pixmap;
