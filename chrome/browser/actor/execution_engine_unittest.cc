12f29c2fb604c3844bb7fe49bfaa0d4577245f59
Dave Tapuska <dtapuska@chromium.org>
2025-11-03 18:16:23 -0800

    [actor] Add total task duration
    Move histograms into code where all histograms are recorded.
    Record the entire task duration including paused time.
    
    Bug: b:456129572, b:454961534
    Change-Id: Ieeb5881e4b53e249aca7fbb632ba11b8f7543d12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7108198
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1539743}

--- chrome/browser/actor/execution_engine_unittest.cc
+++ chrome/browser/actor/execution_engine_unittest.cc
@@ -73,6 +73,10 @@ constexpr char kActorFakeToolDurationHistogram[] =
     "Actor.Tools.ExecutionDuration.FakeTool";
 constexpr char kActorTaskInterruptionCompletedHistogram[] =
     "Actor.Task.Interruptions.Completed";
+constexpr char kActorTaskDurationWallClockCompletedHistogram[] =
+    "Actor.Task.Duration.WallClock.Completed";
+constexpr char kActorTaskDurationWallClockCancelledHistogram[] =
+    "Actor.Task.Duration.WallClock.Cancelled";
 
 constexpr char kActorTaskDurationVisibleCompletedHistogram[] =
     "Actor.Task.Duration.Visible.Completed";
@@ -443,6 +447,8 @@ TEST_F(ExecutionEngineTest, ActorTaskCompletedHistogram) {
   histograms_.ExpectTimeBucketCount(kActorTaskDurationCompletedHistogram,
                                     task_duration, 1);
   histograms_.ExpectBucketCount(kActorTaskCountCompletedHistogram, 4, 1);
+  histograms_.ExpectTimeBucketCount(
+      kActorTaskDurationWallClockCompletedHistogram, task_duration, 1);
 }
 
 TEST_F(ExecutionEngineTest, ActorTaskCompletedWithPauseHistogram) {
@@ -477,6 +483,9 @@ TEST_F(ExecutionEngineTest, ActorTaskCompletedWithPauseHistogram) {
   histograms_.ExpectTimeBucketCount(kActorTaskDurationCompletedHistogram,
                                     active_duration1 + active_duration2, 1);
   histograms_.ExpectBucketCount(kActorTaskCountCompletedHistogram, 1, 1);
+  histograms_.ExpectTimeBucketCount(
+      kActorTaskDurationWallClockCompletedHistogram, base::Milliseconds(650),
+      1);
 }
 
 TEST_F(ExecutionEngineTest, ActorTaskCancelledHistogram) {
@@ -501,6 +510,8 @@ TEST_F(ExecutionEngineTest, ActorTaskCancelledHistogram) {
   histograms_.ExpectTimeBucketCount(kActorTaskDurationCancelledHistogram,
                                     task_duration, 1);
   histograms_.ExpectBucketCount(kActorTaskCountCancelledHistogram, 2, 1);
+  histograms_.ExpectTimeBucketCount(
+      kActorTaskDurationWallClockCancelledHistogram, task_duration, 1);
 }
 
 TEST_F(ExecutionEngineTest, ActorTaskCountAndDurationHistograms) {
@@ -640,6 +651,9 @@ TEST_F(ExecutionEngineTest, CompletedWithInterruptHistogram) {
                                     active_duration1 + active_duration2, 1);
   histograms_.ExpectBucketCount(kActorTaskCountCompletedHistogram, 1, 1);
   histograms_.ExpectBucketCount(kActorTaskInterruptionCompletedHistogram, 1, 1);
+  histograms_.ExpectTimeBucketCount(
+      kActorTaskDurationWallClockCompletedHistogram, base::Milliseconds(650),
+      1);
 }
 
 TEST_F(ExecutionEngineTest, VisibleNotVisibleActuationCompletedHistogram) {
