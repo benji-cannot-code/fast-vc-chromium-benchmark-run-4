b3d0305fe85656bf4d129f3215f31bb4eaf8a5c8
Timofey Chudakov <tchudakov@google.com>
2025-11-04 07:47:56 -0800

    [KA] Do not set icon tint in the ChipView::setIcon.
    This CL splits the API to set the ChipView icon and optionally
    set its tint to be equal to primary text tint. Right now,
    ChipView::setTint(icon, false) will reset the existing icon's
    tint, which breaks all Android icons with android:tint attribute
    defined for them.
    
    Bug: 361327939
    Change-Id: I4c04d3123a6bfeefccd1bf9658720792d2a931bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7079792
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1540047}

--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/chips/ChipViewRenderTest.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/chips/ChipViewRenderTest.java
@@ -106,7 +106,7 @@ public class ChipViewRenderTest {
                 new ChipView(mActivityTestRule.getActivity(), null, 0, R.style.AssistiveChip);
         chip.getPrimaryTextView().setText("Primary text");
         chip.getSecondaryTextView().setText("Secondary text");
-        chip.setIcon(R.drawable.ic_settings_gear_24dp, /* tintWithTextColor= */ true);
+        chip.setIconWithTint(R.drawable.ic_settings_gear_24dp, /* tintWithTextColor= */ true);
         chip.addRemoveIcon();
 
         renderChip(chip, "single_line_chip");
@@ -124,10 +124,29 @@ public class ChipViewRenderTest {
                                 .inflate(R.layout.two_line_chip_view_test_item, null);
         chip.getPrimaryTextView().setText("Primary text");
         chip.getSecondaryTextView().setText("Secondary text");
-        chip.setIcon(R.drawable.ic_settings_gear_24dp, /* tintWithTextColor= */ true);
+        chip.setIconWithTint(R.drawable.ic_settings_gear_24dp, /* tintWithTextColor= */ true);
         chip.addRemoveIcon();
 
         renderChip(chip, "two_line_chip");
+    }
+
+    @Test
+    @MediumTest
+    @Feature({"RenderTest"})
+    public void renderTwoLineChipNoTintedIcon() throws Exception {
+        ChipView chip =
+                (ChipView)
+                        mActivityTestRule
+                                .getActivity()
+                                .getLayoutInflater()
+                                .inflate(R.layout.two_line_chip_view_test_item, null);
+        chip.getPrimaryTextView().setText("Primary text");
+        chip.getSecondaryTextView().setText("Secondary text");
+        // Do not reset the icon's tint and make sure the test renders a white image.
+        chip.setIconWithTint(R.drawable.ic_settings_gear_24dp, /* tintWithTextColor= */ false);
+        chip.addRemoveIcon();
+
+        renderChip(chip, "two_line_chip_no_tinted_icon");
     }
 
     @Test
@@ -138,7 +157,7 @@ public class ChipViewRenderTest {
                 new ChipView(mActivityTestRule.getActivity(), null, 0, R.style.AssistiveChip);
         chip.getPrimaryTextView().setText("Primary looooooooong text");
         chip.getSecondaryTextView().setText("S. t.");
-        chip.setIcon(R.drawable.ic_settings_gear_24dp, /* tintWithTextColor= */ true);
+        chip.setIconWithTint(R.drawable.ic_settings_gear_24dp, /* tintWithTextColor= */ true);
         chip.addRemoveIcon();
         reduceChipWidth(chip);
 
@@ -157,7 +176,7 @@ public class ChipViewRenderTest {
                                 .inflate(R.layout.two_line_chip_view_test_item, null);
         chip.getPrimaryTextView().setText("Primary loooooooong text");
         chip.getSecondaryTextView().setText("S. t. ");
-        chip.setIcon(R.drawable.ic_settings_gear_24dp, /* tintWithTextColor= */ true);
+        chip.setIconWithTint(R.drawable.ic_settings_gear_24dp, /* tintWithTextColor= */ true);
         chip.addRemoveIcon();
         reduceChipWidth(chip);
 
