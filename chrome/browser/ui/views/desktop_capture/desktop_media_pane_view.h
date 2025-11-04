65fbafd5a894d2d2cf78ab1ec783d2b974ef54b5
Johannes Kron <kron@chromium.org>
2025-11-04 00:47:45 -0800

    Reland "Add AudioPermissionWarningView for macOS"
    This reverts commit 4b585ff3fda994a3f3fd90e34f1513612fbaea71.
    
    Reason for revert: Only run tests on supported macOS versions.
    
    Original change's description:
    > Revert "Add AudioPermissionWarningView for macOS"
    >
    > This reverts commit 4c38d24eb5962685a7e87983e6042a28520f7e73.
    >
    > Reason for revert: Consistent failures in DesktopMediaPickerAudioPermissionTests since this landed
    >
    > Original change's description:
    > > Add AudioPermissionWarningView for macOS
    > >
    > > On macOS, capturing system audio requires explicit user permission. The
    > > first time a user attempts this, a system prompt asks for approval. If
    > > this permission has been denied, either via this prompt or previously in
    > > System Settings, subsequent attempts to share audio fail silently.
    > >
    > > To provide better feedback, this change introduces
    > > `AudioPermissionWarningView`. This new view is displayed within the
    > > media picker UI when the user enables audio sharing but the required
    > > system permission is in a denied state.
    > >
    > > The view informs the user about the missing permission and includes a
    > > button to open the relevant System Settings pane, offering a clear path
    > > to resolve the issue.
    > >
    > > This functionality is controlled by the
    > > `DesktopMediaPickerCheckAudioPermissions` feature flag, which is
    > > disabled by default.
    > >
    > > Bug: 447521447
    > > Change-Id: I0f98b2709c8560583002575cc2aa63373401c8e3
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7090135
    > > Reviewed-by: Tove Petersson <tovep@chromium.org>
    > > Commit-Queue: Johannes Kron <kron@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1539229}
    >
    > Bug: 447521447,457438275
    > Bug: 447521447
    > Change-Id: I5bebcedda921fefa7eebe1b85cc6f85059a35f58
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7112923
    > Auto-Submit: Katie Dektar <katie@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Katie Dektar <katie@chromium.org>
    > Owners-Override: Katie Dektar <katie@chromium.org>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1539458}
    
    Bug: 447521447,457438275
    Bug: 447521447
    Change-Id: Icc59690dbb62d4ff313b0564f0e57bbbdab68ab3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7112423
    Commit-Queue: Johannes Kron <kron@chromium.org>
    Reviewed-by: Tove Petersson <tovep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1539829}

--- chrome/browser/ui/views/desktop_capture/desktop_media_pane_view.h
+++ chrome/browser/ui/views/desktop_capture/desktop_media_pane_view.h
@@ -46,6 +46,8 @@ class DesktopMediaPaneView : public views::View {
 #if BUILDFLAG(IS_MAC)
   void OnScreenCapturePermissionUpdate(bool has_permission);
   bool WasPermissionButtonClicked() const;
+  void SetAudioWarningVisible(bool visible);
+  bool IsAudioWarningVisible() const;
 #endif
 
  private:
