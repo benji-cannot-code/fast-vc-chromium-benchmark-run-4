d8229aa20a957fe4c7b8702291668595bb3e756b
Dominique Fauteux-Chapleau <domfc@chromium.org>
2025-11-04 07:41:04 -0800

    Rename ProxyOverrideRules ProxyChain to ProxyList
    This is done to be consistent with the distinction between a "proxy
    chain" and "proxy list", see the comment at
    https://chromium-review.googlesource.com/c/chromium/src/+/7068077/comment/7cd358ed_0229eccd/
    
    BYPASS_POLICY_COMPATIBILITY_CHECK=Field rename for unreleased policy
    
    Bug: 419548922
    Change-Id: I91d51211bc814e5358b44027e2da9c3a64d6b975
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7118878
    Reviewed-by: Igor <igorcov@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Andreea Costinas <acostinas@google.com>
    Cr-Commit-Position: refs/heads/main@{#1540041}

--- components/proxy_config/pref_proxy_config_tracker_impl_unittest.cc
+++ components/proxy_config/pref_proxy_config_tracker_impl_unittest.cc
@@ -508,7 +508,7 @@ TEST_F(PrefProxyConfigOverrideRulesTest, DynamicPolicy) {
                                                  "https://some.app.com",
                                                  "https://other.app.com",
                                              ],
-                                             "ProxyChain": [
+                                             "ProxyList": [
                                                  "HTTPS proxy.app:443",
                                                  "DIRECT",
                                              ],
@@ -560,7 +560,7 @@ TEST_F(PrefProxyConfigOverrideRulesTest, DynamicPolicy) {
                                              "DestinationMatchers": [
                                                  "https://some.other.app.com",
                                              ],
-                                             "ProxyChain": [
+                                             "ProxyList": [
                                                  "DIRECT",
                                                  "PROXY some.host:123",
                                                  "HTTPS proxy.app:443",
@@ -585,7 +585,7 @@ TEST_F(PrefProxyConfigOverrideRulesTest, DynamicPolicy) {
                                              "DestinationMatchers": [
                                                  "https://some.special.app.com",
                                              ],
-                                             "ProxyChain": [
+                                             "ProxyList": [
                                                  "DIRECT",
                                              ],
                                          }
