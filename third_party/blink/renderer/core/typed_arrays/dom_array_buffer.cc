27d8a3cebbd7e29bd3365611156508cfbe44a3e4
Andreas Haas <ahaas@chromium.org>
2025-11-04 12:34:29 -0800

    Reland "Replace generic tag kDomWrappersTag"
    The original CL introduced type-specific tags, but these tags did not
    respect sub-typing relationships. The fix re-introduces the sub-typing
    relationship between DOMArrayBufferView and the DOMTypedArrays, and
    between the ObservableArrays.
    
    Original description:
    
    This is another step in our journey to remove uses of generic type tags.
    
    Different than for ScriptWrappables of other WebIDL files, type tags are
    generated explicitly even for generated types. The reason is that there
    are only two types so far, so it does not seem worth the effort to
    generate two tags automatically.
    
    Bug: 457372605
    Change-Id: Ia91d920034ef6e151af063ec4abb68b1b396e425
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7116232
    Commit-Queue: Andreas Haas <ahaas@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1540256}

--- third_party/blink/renderer/core/typed_arrays/dom_array_buffer.cc
+++ third_party/blink/renderer/core/typed_arrays/dom_array_buffer.cc
@@ -33,8 +33,10 @@ const WrapperTypeInfo DOMArrayBuffer::wrapper_type_info_body_{
     nullptr,
     "ArrayBuffer",
     nullptr,
-    kDOMWrappersTag,
-    kDOMWrappersTag,
+    static_cast<v8::CppHeapPointerTag>(
+        ScriptWrappableArrayTag::kDOMArrayBufferTag),
+    static_cast<v8::CppHeapPointerTag>(
+        ScriptWrappableArrayTag::kDOMArrayBufferTag),
     WrapperTypeInfo::kWrapperTypeObjectPrototype,
     WrapperTypeInfo::kObjectClassId,
     WrapperTypeInfo::kIdlOtherType,
