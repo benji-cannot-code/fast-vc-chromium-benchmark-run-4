997c9a2a82263e56ef16c0c17fe96728c97f8e21
Rafał Godlewski <rgod@google.com>
2025-11-04 07:34:06 -0800

    [iOS][WebAuthn] Enable passkey shim by default
    Bug: 369629469
    Change-Id: I9290ed4fb52e4543908596af95c86eb2259ba224
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7102497
    Reviewed-by: Sebastien S-G <sebsg@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1540038}

--- components/webauthn/ios/features.mm
+++ components/webauthn/ios/features.mm
@@ -5,5 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "components/webauthn/ios/features.h"
 
-BASE_FEATURE(kIOSPasskeyShim, base::FEATURE_DISABLED_BY_DEFAULT);
+// Enabled by default in M144. It should be kept around as a kill-switch
+// indefinitely.
+BASE_FEATURE(kIOSPasskeyShim, base::FEATURE_ENABLED_BY_DEFAULT);
+
 BASE_FEATURE(kIOSPasskeyModalLoginWithShim, base::FEATURE_DISABLED_BY_DEFAULT);
