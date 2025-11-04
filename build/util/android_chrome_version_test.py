2041ef3a81a6a6c453189f7bf49560f5c62fed10
Fuhsin Liao <fuhsinliao@google.com>
2025-11-03 21:29:33 -0800

    Revert "Use 64_32 version code for non-high-end android Chrome"
    This reverts commit 79588c3217fc141e5a7cc2a17e00a46eb3861fbd.
    
    Reason for revert: This cl is causing build failure on multiple builders, such as 'android-arm64-high', 'android-x86_64'
    Example failure link: https://ci.chromium.org/ui/p/chrome/builders/ci/android-arm64-high/34031/overview
    
    Original change's description:
    > Use 64_32 version code for non-high-end android Chrome
    >
    > Bug: b:454606759
    > Change-Id: If497ca93b233ce86ab827e8767dfc9664350c1d0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7112818
    > Commit-Queue: Ben Mason <benmason@chromium.org>
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1539373}
    
    Bug: b:454606759
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ifbe06ccd322283cb7fe21eedb1c158ad4415da76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7115801
    Commit-Queue: Fuhsin Liao <fuhsinliao@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Fuhsin Liao <fuhsinliao@google.com>
    Auto-Submit: Fuhsin Liao <fuhsinliao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1539780}

--- build/util/android_chrome_version_test.py
+++ build/util/android_chrome_version_test.py
@@ -127,7 +127,7 @@ class _VersionTest(unittest.TestCase):
                                   arch='arm64')
     arch_chrome_version_code = output['CHROME_VERSION_CODE']
 
-    self.assertEqual(arch_chrome_version_code, '484400004')
+    self.assertEqual(arch_chrome_version_code, '484400005')
 
   def testGenerateVersionCodesAndroidArchArm64Variants(self):
     """Assert it handles 64-bit-specific additional version codes correctly.
@@ -192,7 +192,7 @@ class _VersionTest(unittest.TestCase):
                                   arch='x64')
     arch_chrome_version_code = output['CHROME_VERSION_CODE']
 
-    self.assertEqual(arch_chrome_version_code, '484400007')
+    self.assertEqual(arch_chrome_version_code, '484400008')
 
   def testGenerateVersionCodesAndroidArchX64Variants(self):
     """Assert it handles 64-bit-specific additional version codes correctly.
@@ -462,7 +462,7 @@ class _VersionGroupedTest(unittest.TestCase):
                                   arch='arm64')
     arch_chrome_version_code = output['CHROME_VERSION_CODE']
 
-    self.assertEqual(arch_chrome_version_code, '575000002')
+    self.assertEqual(arch_chrome_version_code, '575000004')
 
   def testGenerateVersionCodesAndroidArchArm64Variants(self):
     """Assert it handles 64-bit-specific additional version codes correctly.
@@ -527,7 +527,7 @@ class _VersionGroupedTest(unittest.TestCase):
                                   arch='x64')
     arch_chrome_version_code = output['CHROME_VERSION_CODE']
 
-    self.assertEqual(arch_chrome_version_code, '575000008')
+    self.assertEqual(arch_chrome_version_code, '575000009')
 
   def testGenerateVersionCodesAndroidArchX64Variants(self):
     """Assert it handles 64-bit-specific additional version codes correctly.
