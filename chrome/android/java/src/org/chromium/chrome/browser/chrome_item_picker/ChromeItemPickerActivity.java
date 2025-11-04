b762caad9f809abc21368b340c57c1324bcae2c8
Hailey Wang <haileywang@google.com>
2025-11-04 15:07:48 -0800

    [AIM] Add JNI layer for page content extraction service
    Bug: 456275776
    Change-Id: I91a42002dba16bb13380285f03c5adecfd5c8294
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7098835
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1540354}

--- chrome/android/java/src/org/chromium/chrome/browser/chrome_item_picker/ChromeItemPickerActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/chrome_item_picker/ChromeItemPickerActivity.java
@@ -18,6 +18,9 @@ import org.chromium.chrome.R;
 import org.chromium.chrome.browser.IntentHandler;
 import org.chromium.chrome.browser.SnackbarActivity;
 import org.chromium.chrome.browser.multiwindow.MultiWindowUtils;
+import org.chromium.chrome.browser.page_content_annotations.PageContentExtractionService;
+import org.chromium.chrome.browser.page_content_annotations.PageContentExtractionServiceFactory;
+import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.chrome.browser.tabwindow.TabWindowManager;
 
@@ -28,6 +31,7 @@ public class ChromeItemPickerActivity extends SnackbarActivity {
 
     private int mWindowId;
     private @Nullable TabItemPickerCoordinator mItemPickerCoordinator;
+    private @Nullable PageContentExtractionService mPageContentExtractionService;
 
     @Override
     protected void onCreateInternal(@Nullable Bundle savedInstanceState) {
@@ -60,6 +64,16 @@ public class ChromeItemPickerActivity extends SnackbarActivity {
                         containerView);
 
         mItemPickerCoordinator.showTabItemPicker(this::handleModelFailure);
+    }
+
+    @Override
+    protected void onProfileAvailable(Profile profile) {
+        super.onProfileAvailable(profile);
+        mPageContentExtractionService = PageContentExtractionServiceFactory.getForProfile(profile);
+        mPageContentExtractionService.getAllCachedTabIds(
+                (tabIds) -> {
+                    // TODO(haileywang): Display the list of tabs.
+                });
     }
 
     @Override
--- chrome/browser/page_content_annotations/page_content_extraction_service.h
+++ chrome/browser/page_content_annotations/page_content_extraction_service.h
@@ -15,6 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/proto/features/common_quality_data.pb.h"
 #include "content/public/browser/visibility.h"
 
+#if BUILDFLAG(IS_ANDROID)
+#include "base/android/jni_android.h"
+#endif  // BUILDFLAG(IS_ANDROID)
+
 namespace base {
 class FilePath;
 }  // namespace base
@@ -45,6 +49,12 @@ class PageContentExtractionService : public KeyedService,
         content::Page& page,
         const optimization_guide::proto::AnnotatedPageContent& page_content) {}
   };
+
+#if BUILDFLAG(IS_ANDROID)
+  // Returns a Java object for the given service.
+  static base::android::ScopedJavaLocalRef<jobject> GetJavaObject(
+      PageContentExtractionService* service);
+#endif  // BUILDFLAG(IS_ANDROID)
 
   PageContentExtractionService(os_crypt_async::OSCryptAsync* os_crypt_async,
                                const base::FilePath& profile_path);
