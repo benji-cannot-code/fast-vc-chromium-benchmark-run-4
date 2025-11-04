bd7275522f02e98d184ef678ee8baf85cd2dcd7f
Arthur Sonzogni <arthursonzogni@chromium.org>
2025-11-04 08:30:29 -0800

    Fix unsafe buffer usage in process_policy_test.cc
    Replaced swprintf_s with base::StringPrintf and base::SysUTF8ToWide
    to fix unsafe buffer error(s).
    
    Initial patchset generated by headless gemini-cli using:
    //agents/prompts/projects/spanification/run.py
    
    Fixed: 457423605
    Change-Id: Idb9dd610116e7fca25a847e90029a5d394913229
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7110746
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1540079}

--- sandbox/win/src/process_policy_test.cc
+++ sandbox/win/src/process_policy_test.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/memory/free_deleter.h"
+#include "base/strings/stringprintf.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/win/scoped_handle.h"
 #include "base/win/scoped_process_information.h"
@@ -105,13 +106,7 @@ SBOX_TESTS_COMMAND int Process_Crash(int argc, wchar_t** argv) {
 }
 // Generate a event name, used to test thread creation.
 std::wstring GenerateEventName(DWORD pid) {
-  wchar_t buff[30] = {};
-  int res = UNSAFE_TODO(swprintf_s(buff, sizeof(buff) / sizeof(buff[0]),
-                                   L"ProcessPolicyTest_%08x", pid));
-  if (-1 != res) {
-    return std::wstring(buff);
-  }
-  return std::wstring();
+  return base::SysUTF8ToWide(base::StringPrintf("ProcessPolicyTest_%08x", pid));
 }
 
 // This is the function that is called when testing thread creation.
