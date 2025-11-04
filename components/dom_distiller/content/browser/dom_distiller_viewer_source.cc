bc8a3ecd908bcccf704bf4d4cde1482ed3782c64
Evan Luo <evanluo@google.com>
2025-11-04 13:39:09 -0800

    [reader mode] Use SetTemporaryZoomLevel to override default zoom setting
    Previous implementation of SetLevelForHostAndScheme was causing a crash
    that we circumvented as a stopgap using SetLevelForHost which was not
    what we ultimately wanted. This change uses SetTemporaryZoomLevel on the
    RM page web contents to help accomplish the same thing that
    SetLevelForHostAndScheme did.
    
    Bug: 454349127
    Change-Id: Ia0eb1e4affe8a7ecb3facb07623a84779dc9accc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7092877
    Commit-Queue: Evan Luo <evanluo@google.com>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1540305}

--- components/dom_distiller/content/browser/dom_distiller_viewer_source.cc
+++ components/dom_distiller/content/browser/dom_distiller_viewer_source.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/dom_distiller/core/viewer.h"
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/back_forward_cache.h"
+#include "content/public/browser/host_zoom_map.h"
 #include "content/public/browser/navigation_details.h"
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/navigation_handle.h"
@@ -175,6 +176,17 @@ void DomDistillerViewerSource::RequestViewerHandle::DOMContentLoaded(
   if (render_frame_host->GetParentOrOuterDocument()) {
     return;
   }
+
+#if BUILDFLAG(IS_ANDROID)
+  // Reading mode should not be affected by the default zoom level. There is a
+  // JS font scaling applied based on distilled_page_prefs, so we want to set
+  // temporary zoom level of 0 so that the zoom settings do not stack on top of
+  // one another.
+  content::HostZoomMap* host_zoom_map =
+      content::HostZoomMap::GetForWebContents(web_contents());
+  host_zoom_map->SetTemporaryZoomLevel(
+      web_contents()->GetPrimaryMainFrame()->GetGlobalId(), 0.0);
+#endif  // BUILDFLAG(IS_ANDROID)
 
   // Execute the scripts in buffer_ one-by-one, starting from the front of the
   // list.
