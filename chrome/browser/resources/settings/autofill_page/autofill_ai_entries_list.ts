1b751e582cf30d1ca8811e0bb9377362ee9201b5
Stanislav Mikheyev <mikhe@google.com>
2025-11-04 09:06:25 -0800

    YourSavedInfo: make entries list title configurable
    Bug: 451349831
    Change-Id: Id5c9e22c23b0015afd6217df0c3c4ba5922e90ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7112523
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Stanislav Mikheyev <mikhe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1540099}

--- chrome/browser/resources/settings/autofill_page/autofill_ai_entries_list.ts
+++ chrome/browser/resources/settings/autofill_page/autofill_ai_entries_list.ts
@@ -93,6 +93,10 @@ export class SettingsAutofillAiEntriesListElement extends
         value: null,
       },
 
+      title: {
+        type: String,
+      },
+
       allowEditing_: {
         type: Object,
         value: false,
@@ -153,6 +157,7 @@ export class SettingsAutofillAiEntriesListElement extends
 
   declare ineligibleUser: boolean;
   declare allowedEntityTypes: Set<EntityTypeName>|null;
+  declare title: string;
   declare private allowEditing_: boolean;
   declare private activeEntityInstance_: EntityInstance|null;
   declare private completeEntityTypesList_: EntityType[];
