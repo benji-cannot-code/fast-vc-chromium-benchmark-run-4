d9c3f12c4577b90b37dd5228407f0aded719eca1
Wen Zhao <wenz@google.com>
2025-11-04 14:44:32 -0800

    Remove `ENABLE_ANDROID_SITE_ISOLATION` build flag.
    This change removes the `ENABLE_ANDROID_SITE_ISOLATION` build flag in
    favor of an existing field trial overrides approach as in
    https://chromium-review.googlesource.com/c/chromium/src/+/7063544
    
    Change-Id: I2e86c99da1166bee6632a0b7dc13ca3f594d1bbe
    Bug: 433313577, 453856709
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7077000
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Wen Zhao <wenz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1540339}

--- chrome/browser/policy/site_isolation_policy_browsertest.cc
+++ chrome/browser/policy/site_isolation_policy_browsertest.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/test/scoped_amount_of_physical_memory_override.h"
 #include "base/test/scoped_feature_list.h"
-#include "build/android_buildflags.h"
 #include "build/build_config.h"
 #include "chrome/browser/chrome_content_browser_client.h"
 #include "chrome/browser/profiles/profile.h"
@@ -319,7 +318,7 @@ IN_PROC_BROWSER_TEST_F(SiteIsolationPolicyBrowserTest,
   // without an explicit enterprise policy).
   EXPECT_FALSE(base::CommandLine::ForCurrentProcess()->HasSwitch(
       switches::kDisableSiteIsolation));
-#if BUILDFLAG(IS_ANDROID) && !BUILDFLAG(ENABLE_ANDROID_SITE_ISOLATION)
+#if BUILDFLAG(IS_ANDROID)
   EXPECT_FALSE(base::CommandLine::ForCurrentProcess()->HasSwitch(
       switches::kDisableSiteIsolationForPolicy));
   EXPECT_EQ(CheckUseDedicatedProcessesForAllSitesWithAndroidState(
@@ -331,7 +330,7 @@ IN_PROC_BROWSER_TEST_F(SiteIsolationPolicyBrowserTest,
             base::FeatureList::IsEnabled(features::kSitePerProcess));
 #else
   EXPECT_TRUE(content::SiteIsolationPolicy::UseDedicatedProcessesForAllSites());
-#endif  // BUILDFLAG(IS_ANDROID) && !BUILDFLAG(ENABLE_ANDROID_SITE_ISOLATION)
+#endif  // BUILDFLAG(IS_ANDROID)
 }
 
 #if BUILDFLAG(IS_ANDROID)
