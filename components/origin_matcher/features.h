633efd4c7bae4b7b73d6cf5b179324f1499f35ed
Andrew Williams <awillia@chromium.org>
2025-11-04 08:41:27 -0800

    Export origin_matcher::kOriginMatcherNewCopyAssignment
    Adds `COMPONENT_EXPORT(ORIGIN_MATCHER)` to the feature declaration to
    address failures on the WebView dbg bots
    
    Example failure:
    https://ci.chromium.org/ui/p/chromium/builders/try/android-webview-12-x64-dbg/180/test-results
    
    Bug: None
    Change-Id: I57dbe28b8d551d55711349e7f735dee270c9ecd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7111263
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Andrew Williams <awillia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1540091}

--- components/origin_matcher/features.h
+++ components/origin_matcher/features.h
@@ -6,12 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_ORIGIN_MATCHER_FEATURES_H_
 #define COMPONENTS_ORIGIN_MATCHER_FEATURES_H_
 
+#include "base/component_export.h"
 #include "base/features.h"
 
 namespace origin_matcher {
 
 // When enabled, OriginMatcher::operator= will use the new, hopefully faster
 // implementation that does not serialize and deserialize all the rules.
+COMPONENT_EXPORT(ORIGIN_MATCHER)
 BASE_DECLARE_FEATURE(kOriginMatcherNewCopyAssignment);
 
 }  // namespace origin_matcher
