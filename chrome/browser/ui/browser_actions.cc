7447035ab417136f4853ef2cc7d75078e2ace96f
Gustavo Martin <gusmartin@google.com>
2025-11-04 08:38:11 -0800

    Update ToolbarButton RM entrypoint to go through ReadAnythingController.
    UI Browser Action now calls ReadAnythingController which currently
    delegates logic to SidePanelUI.
    
    This CL is part of efforts for the new ReadAnythingController to handle
    all Reading Mode entrypoints (see go/irm-hld) and remove all entrypoints
    that directly refer to the ReadAnythingSidePanelController.
    
    Bug: 449169270
    Change-Id: Iedc3ced89ae826658513846876b006a88bf810d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7101425
    Reviewed-by: Eitan Goldberger <eitang@google.com>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Gustavo Martin <gusmartin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1540088}

--- chrome/browser/ui/browser_actions.cc
+++ chrome/browser/ui/browser_actions.cc
@@ -275,7 +275,8 @@ void BrowserActions::InitializeBrowserActions() {
             .Build());
   }
 
-  if (features::IsReadAnythingOmniboxChipEnabled()) {
+  if (features::IsReadAnythingOmniboxChipEnabled() ||
+      features::IsImmersiveReadAnythingEnabled()) {
     actions::ActionItem::InvokeActionCallback read_anything_callback =
         base::BindRepeating(
             [](BrowserWindowInterface* bwi, actions::ActionItem* item,
