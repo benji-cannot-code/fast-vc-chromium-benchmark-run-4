ddb443951ac88844d9e0e576c482d78259a3648f
Jason Hu <hujasonx@google.com>
2025-11-04 12:40:59 -0800

    [context-input] Split ContextualSessionService into multiple files
    There will be more changes that affect the SessionHandle coming up, so
    having it in its own file will help organize the code.
    
    Bug: b:457425602
    Change-Id: Ie1e51603323287ac577a43a61f825b495ea03b44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7112930
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Cr-Commit-Position: refs/heads/main@{#1540264}

--- chrome/browser/contextual_search/contextual_search_web_contents_helper.h
+++ chrome/browser/contextual_search/contextual_search_web_contents_helper.h
@@ -8,14 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "components/contextual_search/contextual_search_service.h"
+#include "components/contextual_search/contextual_search_session_handle.h"
 #include "content/public/browser/web_contents_user_data.h"
 
 namespace content {
 class WebContents;
 }
 
-// Helper class that scopes the `ContextualSearchService::SessionHandle`'s
+// Helper class that scopes the `ContextualSearchSessionHandle`'s
 // lifetime to a `content::WebContents`.
 class ContextualSearchWebContentsHelper
     : public content::WebContentsUserData<ContextualSearchWebContentsHelper> {
@@ -28,13 +28,12 @@ class ContextualSearchWebContentsHelper
 
   // Takes ownership of a contextual session handle.
   void set_session_handle(
-      std::unique_ptr<contextual_search::ContextualSearchService::SessionHandle>
+      std::unique_ptr<contextual_search::ContextualSearchSessionHandle>
           handle) {
     session_handle_ = std::move(handle);
   }
   // Returns the owned contextual session handle. May return nullptr.
-  contextual_search::ContextualSearchService::SessionHandle* session_handle()
-      const {
+  contextual_search::ContextualSearchSessionHandle* session_handle() const {
     return session_handle_.get();
   }
 
@@ -43,7 +42,7 @@ class ContextualSearchWebContentsHelper
       content::WebContents* web_contents);
   friend class content::WebContentsUserData<ContextualSearchWebContentsHelper>;
 
-  std::unique_ptr<contextual_search::ContextualSearchService::SessionHandle>
+  std::unique_ptr<contextual_search::ContextualSearchSessionHandle>
       session_handle_;
 
   WEB_CONTENTS_USER_DATA_KEY_DECL();
--- chrome/browser/ui/webui/searchbox/contextual_searchbox_handler_unittest.cc
+++ chrome/browser/ui/webui/searchbox/contextual_searchbox_handler_unittest.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/searchbox/searchbox_test_utils.h"
 #include "chrome/browser/ui/webui/webui_embedding_context.h"
 #include "components/contextual_search/contextual_search_metrics_recorder.h"
+#include "components/contextual_search/contextual_search_service.h"
 #include "components/contextual_search/internal/test_composebox_query_controller.h"
 #include "components/lens/tab_contextualization_controller.h"
 #include "components/omnibox/browser/searchbox.mojom.h"
