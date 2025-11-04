62d0dc94d8d0532d99c2e8b686c6984f2dda9a22
Fuhsin Liao <fuhsinliao@google.com>
2025-11-03 21:43:25 -0800

    Revert "Add field trial testing config entry for Exclusive Access Manager"
    This reverts commit 683085b073a6c6140dfbe6af6c7475272473e42f.
    
    Reason for revert: This cl is causing test failures on 'android-10-x86-nofieldtrial-rel' trybot. Example failure: https://ci.chromium.org/ui/p/chromium/builders/ci/android-10-x86-nofieldtrial-rel/865/overview
    
    Original change's description:
    > Add field trial testing config entry for Exclusive Access Manager
    >
    > This change is in preparation of adding a finch config CL/812771914
    >
    > Changes:
    > - Move ExclusiveAccessManager native initialization inside of initialize
    >  method instead of the constructor, as we might not be ready to
    >  initialize native components at that point.
    > - Update ExclusiveAccessManager to fail gracefully if native is not
    >  initialized.
    > - Update FullscreenTestUtils.togglePersistentFullscreen to accept the
    >  tab instead of the delegate as we need to provide native frame
    >  pointer for exclusive access manager to function correctly.
    >
    > Bug: b:424090399
    > Change-Id: I62da2f2e06bc7b11859da32d4eb356e714743792
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6994742
    > Commit-Queue: Abdelrahman Eed <abdoeed@google.com>
    > Reviewed-by: Sky Malice <skym@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1539315}
    
    Bug: b:424090399
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I7f1efb23727a1488b100c42de0a8d891ca50b86d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7116878
    Auto-Submit: Fuhsin Liao <fuhsinliao@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Fuhsin Liao <fuhsinliao@google.com>
    Owners-Override: Fuhsin Liao <fuhsinliao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1539784}

--- chrome/android/java/src/org/chromium/chrome/browser/ui/ExclusiveAccessManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ui/ExclusiveAccessManager.java
@@ -46,9 +46,6 @@ public class ExclusiveAccessManager
     private final TabModelObserver mTabModelObserver;
     private final ObservableSupplierImpl<Boolean> mExclusiveAccessState =
             new ObservableSupplierImpl<>(false);
-    // TODO(https://crbug.com/456395886): use Context instead of Activity.
-    private final Activity mActivity;
-    private final ActivityTabProvider mActivityTabProvider;
 
     public ExclusiveAccessManager(
             Activity activity,
@@ -56,8 +53,9 @@ public class ExclusiveAccessManager
             ActivityTabProvider activityTabProvider,
             @Nullable DesktopWindowStateManager desktopWindowStateManager) {
         mFullscreenManager = fullscreenManager;
-        mActivity = activity;
-        mActivityTabProvider = activityTabProvider;
+        mExclusiveAccessManagerAndroidNativePointer =
+                ExclusiveAccessManagerJni.get()
+                        .init(this, activity, fullscreenManager, activityTabProvider);
         if (desktopWindowStateManager != null) {
             mDesktopWindowStateManager = desktopWindowStateManager;
             mDesktopWindowStateManager.addObserver(this);
@@ -66,8 +64,6 @@ public class ExclusiveAccessManager
                 new FullscreenManager.FullscreenManagerDelegate() {
                     @Override
                     public void onExitFullscreen(@Nullable Tab tab) {
-                        if (mExclusiveAccessManagerAndroidNativePointer == 0) return;
-
                         if (tab == null) {
                             ExclusiveAccessManagerJni.get()
                                     .exitExclusiveAccess(
@@ -81,8 +77,6 @@ public class ExclusiveAccessManager
                 new TabModelObserver() {
                     @Override
                     public void willCloseTab(Tab tab, boolean didCloseAlone) {
-                        if (mExclusiveAccessManagerAndroidNativePointer == 0) return;
-
                         ExclusiveAccessManagerJni.get()
                                 .onTabClosing(
                                         mExclusiveAccessManagerAndroidNativePointer,
@@ -110,10 +104,6 @@ public class ExclusiveAccessManager
         for (TabModel model : modelSelector.getModels()) {
             model.addObserver(mTabModelObserver);
         }
-
-        mExclusiveAccessManagerAndroidNativePointer =
-                ExclusiveAccessManagerJni.get()
-                        .init(this, mActivity, mFullscreenManager, mActivityTabProvider);
     }
 
     public ObservableSupplier<Boolean> getExclusiveAccessStateSupplier() {
@@ -121,8 +111,6 @@ public class ExclusiveAccessManager
     }
 
     private void deactivateTab(Tab tab) {
-        if (mExclusiveAccessManagerAndroidNativePointer == 0) return;
-
         ExclusiveAccessManagerJni.get()
                 .onTabDeactivated(
                         mExclusiveAccessManagerAndroidNativePointer, tab.getWebContents());
@@ -139,8 +127,6 @@ public class ExclusiveAccessManager
      */
     public void enterFullscreenModeForTab(
             RenderFrameHost renderFrameHost, FullscreenOptions options) {
-        if (mExclusiveAccessManagerAndroidNativePointer == 0) return;
-
         ExclusiveAccessManagerJni.get()
                 .enterFullscreenModeForTab(
                         mExclusiveAccessManagerAndroidNativePointer,
@@ -156,15 +142,12 @@ public class ExclusiveAccessManager
      * @param webContents exit requester
      */
     public void exitFullscreenModeForTab(@Nullable WebContents webContents) {
-        if (mExclusiveAccessManagerAndroidNativePointer == 0) return;
-
         ExclusiveAccessManagerJni.get()
                 .exitFullscreenModeForTab(mExclusiveAccessManagerAndroidNativePointer, webContents);
     }
 
     /** Force exit of all exclusive access controllers */
     public void exitExclusiveAccess() {
-        if (mExclusiveAccessManagerAndroidNativePointer == 0) return;
         ExclusiveAccessManagerJni.get()
                 .exitExclusiveAccess(mExclusiveAccessManagerAndroidNativePointer);
         // Marking state in case of enabled locks without fullscreen
@@ -173,8 +156,6 @@ public class ExclusiveAccessManager
 
     /** Checks if any of the fullscreen, keyboard lock or pointer lock is on */
     public boolean hasExclusiveAccess() {
-        if (mExclusiveAccessManagerAndroidNativePointer == 0) return false;
-
         return ExclusiveAccessManagerJni.get()
                 .hasExclusiveAccess(mExclusiveAccessManagerAndroidNativePointer);
     }
@@ -185,8 +166,8 @@ public class ExclusiveAccessManager
      * @param webContents the requester of check
      * @return is currently in fullscreen
      */
-    public boolean isFullscreenForTabOrPending(@Nullable WebContents webContents) {
-        if (webContents == null || mExclusiveAccessManagerAndroidNativePointer == 0) {
+    public boolean isFullscreenForTabOrPending(WebContents webContents) {
+        if (webContents == null) {
             return false;
         }
         return ExclusiveAccessManagerJni.get()
@@ -202,8 +183,6 @@ public class ExclusiveAccessManager
      * @return true if the key was handled
      */
     public boolean preHandleKeyboardEvent(long nativeKeyEvent) {
-        if (mExclusiveAccessManagerAndroidNativePointer == 0) return false;
-
         boolean handled =
                 ExclusiveAccessManagerJni.get()
                         .preHandleKeyboardEvent(
@@ -222,8 +201,6 @@ public class ExclusiveAccessManager
      * @param escKeyLocked whether the escape key is to be locked.
      */
     public void requestKeyboardLock(WebContents webContents, boolean escKeyLocked) {
-        if (mExclusiveAccessManagerAndroidNativePointer == 0) return;
-
         ExclusiveAccessManagerJni.get()
                 .requestKeyboardLock(
                         mExclusiveAccessManagerAndroidNativePointer, webContents, escKeyLocked);
@@ -237,8 +214,6 @@ public class ExclusiveAccessManager
      * @param webContents the WebContents cancelling keyboard lock
      */
     public void cancelKeyboardLockRequest(WebContents webContents) {
-        if (mExclusiveAccessManagerAndroidNativePointer == 0) return;
-
         ExclusiveAccessManagerJni.get()
                 .cancelKeyboardLockRequest(
                         mExclusiveAccessManagerAndroidNativePointer, webContents);
@@ -248,8 +223,6 @@ public class ExclusiveAccessManager
 
     public void requestPointerLock(
             WebContents webContents, boolean userGesture, boolean lastUnlockedByTarget) {
-        if (mExclusiveAccessManagerAndroidNativePointer == 0) return;
-
         ExclusiveAccessManagerJni.get()
                 .requestPointerLock(
                         mExclusiveAccessManagerAndroidNativePointer,
@@ -261,8 +234,6 @@ public class ExclusiveAccessManager
     }
 
     public void lostPointerLock() {
-        if (mExclusiveAccessManagerAndroidNativePointer == 0) return;
-
         ExclusiveAccessManagerJni.get()
                 .lostPointerLock(mExclusiveAccessManagerAndroidNativePointer);
         // Checking internal state of the controllers in case other locks are on
@@ -271,8 +242,6 @@ public class ExclusiveAccessManager
 
     @Override
     public void onDesktopWindowingModeChanged(boolean isInDesktopWindow) {
-        if (mExclusiveAccessManagerAndroidNativePointer == 0) return;
-
         if (isInDesktopWindow && mFullscreenManager.getPersistentFullscreenMode()) {
             // Exiting when fullscreen should be lost
             ExclusiveAccessManagerJni.get()
--- chrome/android/javatests/src/org/chromium/chrome/browser/fullscreen/FullscreenManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/fullscreen/FullscreenManagerTest.java
@@ -241,10 +241,7 @@ public class FullscreenManagerTest {
     @Feature({"Fullscreen"})
     @DisableFeatures({
         ChromeFeatureList.FULLSCREEN_INSETS_API_MIGRATION,
-        ChromeFeatureList.FULLSCREEN_INSETS_API_MIGRATION_ON_AUTOMOTIVE,
-        // TODO(https://crbug.com/456056229): investigate supporting delayed fullscreen entry when
-        // using exclusive access manager
-        ChromeFeatureList.ENABLE_EXCLUSIVE_ACCESS_MANAGER
+        ChromeFeatureList.FULLSCREEN_INSETS_API_MIGRATION_ON_AUTOMOTIVE
     })
     public void testDelayedPersistentFullscreenLegacy() {
         WebPageStation page = mActivityTestRule.startOnUrl(LONG_HTML_TEST_PAGE);
@@ -259,7 +256,7 @@ public class FullscreenManagerTest {
         ChromeTabUtils.newTabFromMenu(InstrumentationRegistry.getInstrumentation(), activity);
 
         // Having the background tab enter fullscreen should be delayed until it comes foreground.
-        FullscreenTestUtils.togglePersistentFullscreen(tab, true);
+        FullscreenTestUtils.togglePersistentFullscreen(delegate, true);
         Assert.assertFalse(getPersistentFullscreenMode());
 
         // Put the tab foreground and assert the fullscreen was entered.
@@ -275,9 +272,6 @@ public class FullscreenManagerTest {
         ChromeFeatureList.FULLSCREEN_INSETS_API_MIGRATION,
         ChromeFeatureList.FULLSCREEN_INSETS_API_MIGRATION_ON_AUTOMOTIVE
     })
-    // TODO(https://crbug.com/456056229): investigate supporting delayed fullscreen entry when
-    // using exclusive access manager
-    @DisableFeatures({ChromeFeatureList.ENABLE_EXCLUSIVE_ACCESS_MANAGER})
     public void testDelayedPersistentFullscreen() {
         WebPageStation page = mActivityTestRule.startOnUrl(LONG_HTML_TEST_PAGE);
 
@@ -291,7 +285,7 @@ public class FullscreenManagerTest {
         ChromeTabUtils.newTabFromMenu(InstrumentationRegistry.getInstrumentation(), activity);
 
         // Having the background tab enter fullscreen should be delayed until it comes foreground.
-        FullscreenTestUtils.togglePersistentFullscreen(tab, true);
+        FullscreenTestUtils.togglePersistentFullscreen(delegate, true);
         Assert.assertFalse(getPersistentFullscreenMode());
 
         // Put the tab foreground and assert the fullscreen was entered.
