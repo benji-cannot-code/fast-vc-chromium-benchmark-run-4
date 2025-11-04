bd16c7feadacdc650cf5551b57a33137a0a6dac5
Jan Roguwski <jroguwski@google.com>
2025-11-04 05:30:01 -0800

    Close windows from splash screen
    This change forces all system web dialog instances to close when closing
    the kiosk app launch splash screen.
    
    It fixes the root cause of exploits where windows created during app
    launch persists to the kiosk session.
    
    fixed: 432822788
    Change-Id: I47e4ea95b44fb34ae87b0c5ed31130979c6ac4c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7066918
    Reviewed-by: Ashutosh Singhal <macinashutosh@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Jan Roguwski <jroguwski@google.com>
    Reviewed-by: Polina Bondarenko <pbond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1539960}

--- chrome/browser/ash/login/app_mode/kiosk_launch_controller.cc
+++ chrome/browser/ash/login/app_mode/kiosk_launch_controller.cc
@@ -57,6 +57,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ash/login/login_display_host.h"
 #include "chrome/browser/ui/webui/ash/login/app_launch_splash_screen_handler.h"
 #include "chrome/browser/ui/webui/ash/login/network_state_informer.h"
+#include "chrome/browser/ui/webui/ash/system_web_dialog/system_web_dialog_delegate.h"
 #include "chromeos/ash/components/install_attributes/install_attributes.h"
 #include "chromeos/ash/components/network/network_handler.h"
 #include "chromeos/ash/components/network/network_state.h"
@@ -516,6 +517,13 @@ void KioskLaunchController::CleanUp() {
   cleaned_up_ = true;
 
   splash_wait_timer_.Stop();
+
+  // Close all windows opened on the splash screen before entering the kiosk
+  // session.
+  auto web_dialog_instances = SystemWebDialogDelegate::GetAllInstances();
+  for (auto instance : web_dialog_instances) {
+    instance->Close();
+  }
 
   splash_screen_ = nullptr;
 
