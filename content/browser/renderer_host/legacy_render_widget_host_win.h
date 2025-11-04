924bcc33abac30cc4b9e835d5cf87d8d9d96a0cb
John An <johna@microsoft.com>
2025-11-04 10:24:42 -0800

    Ensure correct pointer handling on LegacyRenderWidgetHostHWND hide
    On Windows today, if the RenderWidgetHostViewAura is occluded (hidden)
    during an ongoing touch gesture, and the touch gesture ends, the
    LegacyRenderWidgetHostHWND will not deliver the WM_POINTERUP/LEAVE to
    the WindowEventTarget that handled the WM_POINTERDOWN/ENTER. This is
    because as part of RWHVA::Hide, the LRWHH is reparented and
    WindowEventTargets are indexed by LRWHH parent hwnd.
    
    This change resolves this issue by tracking the down pointer ids and
    parent hwnd before hide and then forwarding all WM_POINTER* for the
    down pointer ids before hide to the parent hwnd before hide.
    
    Bug: 415092031
    Change-Id: I6d94613864252de25697bcbcf0c306f966559f2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7009113
    Reviewed-by: Olga Gerchikov <gerchiko@microsoft.com>
    Commit-Queue: John An <johna@microsoft.com>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1540166}

--- content/browser/renderer_host/legacy_render_widget_host_win.h
+++ content/browser/renderer_host/legacy_render_widget_host_win.h
@@ -16,7 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wrl/client.h>
 
 #include <memory>
+#include <set>
 
+#include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
 #include "content/common/content_export.h"
 #include "ui/accessibility/platform/ax_fragment_root_delegate_win.h"
@@ -37,6 +39,8 @@ namespace content {
 class DirectManipulationBrowserTestBase;
 class DirectManipulationHelper;
 class RenderWidgetHostViewAura;
+FORWARD_DECLARE_TEST(RenderWidgetHostViewAuraTest,
+                     LegacyRenderWidgetHostHWNDPointerEventsWhileHidden);
 
 // Reasons for the existence of this class outlined below:
 // 1. Some screen readers expect every tab / every unique web content container
@@ -139,6 +143,8 @@ class CONTENT_EXPORT LegacyRenderWidgetHostHWND
  private:
   friend class AccessibilityObjectLifetimeWinBrowserTest;
   friend class DirectManipulationBrowserTestBase;
+  FRIEND_TEST_ALL_PREFIXES(RenderWidgetHostViewAuraTest,
+                           LegacyRenderWidgetHostHWNDPointerEventsWhileHidden);
 
   explicit LegacyRenderWidgetHostHWND(RenderWidgetHostViewAura* host);
   ~LegacyRenderWidgetHostHWND() override;
@@ -204,6 +210,13 @@ class CONTENT_EXPORT LegacyRenderWidgetHostHWND
 
   // Instruct aura::WindowTreeHost to use the HWND's parent for lookup.
   std::unique_ptr<ui::ViewProp> window_tree_host_prop_;
+
+  // Track pointers that were down before hide and parent window before hide.
+  // This is to ensure any touch gestures initiated before hide and ended
+  // during hide are routed to the correct WindowEventTarget.
+  // See comment in OnPointer.
+  std::set<uint32_t> down_pointers_before_hide_;
+  HWND parent_before_hide_ = nullptr;
 
   CR_MSG_MAP_CLASS_DECLARATIONS(LegacyRenderWidgetHostHWND)
 };
