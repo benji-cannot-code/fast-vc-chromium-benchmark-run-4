10542e6409e771d8c91a47078fd23a2c40f24e8f
Kelsen Liu <kelsenliu@chromium.org>
2025-11-04 05:53:32 -0800

    Spanify ScopedTransferBufferPtr to avoid unsafe buffer usage
    Adds `as_byte_span()` methods to `ScopedTransferBufferPtr` class.
    
    Bug: 40284755
    Change-Id: I481e51eeba5f87f6aef840c1a96cc1556c5989ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7110238
    Commit-Queue: kelsen liu <kelsenliu@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1539973}

--- gpu/command_buffer/client/transfer_buffer.h
+++ gpu/command_buffer/client/transfer_buffer.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/containers/circular_deque.h"
+#include "base/containers/span.h"
 #include "base/memory/raw_ptr.h"
 #include "base/unguessable_token.h"
 #include "gpu/command_buffer/client/gpu_command_buffer_client_export.h"
@@ -226,6 +227,16 @@ class GPU_COMMAND_BUFFER_CLIENT_EXPORT ScopedTransferBufferPtr {
 
   void* address() const {
     return buffer_;
+  }
+
+  base::span<uint8_t> as_byte_span() {
+    // TODO(kelsen): Change `buffer_` to raw_span;
+    return UNSAFE_TODO(base::span(static_cast<uint8_t*>(buffer_), size_));
+  }
+
+  base::span<const uint8_t> as_byte_span() const {
+    // TODO(kelsen): Change `buffer_` to raw_span;
+    return UNSAFE_TODO(base::span(static_cast<const uint8_t*>(buffer_), size_));
   }
 
   // Returns true if |memory| lies inside this buffer.
