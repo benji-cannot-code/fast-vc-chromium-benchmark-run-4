a69b98232dc6986aa8e6beff2b2bc03ceff03003
Fiona Macintosh <fmacintosh@google.com>
2025-11-05 13:56:36 -0800

    [ACT] Clean up Clank IsIpProtectionDisabledForEnterprise
    Bug: b:452905381
    Change-Id: I207f14038b2aee84ffcc878cf43412f3bc14f0cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7119099
    Commit-Queue: Kevin Graney <kmg@google.com>
    Auto-Submit: Fiona Macintosh <fmacintosh@google.com>
    Reviewed-by: Kevin Graney <kmg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1540861}

--- chrome/browser/privacy_sandbox/android/tracking_protection_settings_bridge.cc
+++ chrome/browser/privacy_sandbox/android/tracking_protection_settings_bridge.cc
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/android/scoped_java_ref.h"
-#include "chrome/browser/privacy_sandbox/tracking_protection_settings_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sync/sync_service_factory.h"
 #include "components/prefs/pref_service.h"
@@ -17,11 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using base::android::JavaParamRef;
 
 namespace {
-privacy_sandbox::TrackingProtectionSettings* GetTrackingProtectionSettings(
-    const base::android::JavaRef<jobject>& j_profile) {
-  return TrackingProtectionSettingsFactory::GetForProfile(
-      Profile::FromJavaObject(j_profile));
-}
 
 PrefService* GetPrefService(const base::android::JavaRef<jobject>& j_profile) {
   return Profile::FromJavaObject(j_profile)->GetPrefs();
@@ -32,14 +26,6 @@ syncer::SyncService* GetSyncService(
   return SyncServiceFactory::GetForProfile(Profile::FromJavaObject(j_profile));
 }
 }  // namespace
-
-jboolean
-JNI_TrackingProtectionSettingsBridge_IsIpProtectionDisabledForEnterprise(
-    JNIEnv* env,
-    const JavaParamRef<jobject>& j_profile) {
-  return GetTrackingProtectionSettings(j_profile)
-      ->IsIpProtectionDisabledForEnterprise();
-}
 
 void JNI_TrackingProtectionSettingsBridge_MaybeSetRollbackPrefsModeB(
     JNIEnv* env,
