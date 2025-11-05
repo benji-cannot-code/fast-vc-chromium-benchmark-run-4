42fde03c8ba50e64b459a425697cb8d94fcc4ae6
Eric Ekey <ericekey@google.com>
2025-11-05 14:49:45 -0800

    [iOS] Add Impressions Histogram for App Bundle Promo
    This change adds a histogram to collect impression metrics for the App
    Bundle Promo in the Magic Stack.
    
    Fixed: 458103554
    Change-Id: I564cb8a50b27f20278fd13ac4daa3b8f77d76816
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7122459
    Commit-Queue: Eric Ekey <ericekey@google.com>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Auto-Submit: Eric Ekey <ericekey@google.com>
    Reviewed-by: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1540893}

--- ios/chrome/browser/content_suggestions/ui_bundled/content_suggestions_coordinator.mm
+++ ios/chrome/browser/content_suggestions/ui_bundled/content_suggestions_coordinator.mm
@@ -56,6 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/content_suggestions/ui_bundled/content_suggestions_delegate.h"
 #import "ios/chrome/browser/content_suggestions/ui_bundled/content_suggestions_image_data_source.h"
 #import "ios/chrome/browser/content_suggestions/ui_bundled/content_suggestions_mediator.h"
+#import "ios/chrome/browser/content_suggestions/ui_bundled/content_suggestions_metrics_constants.h"
 #import "ios/chrome/browser/content_suggestions/ui_bundled/content_suggestions_metrics_recorder.h"
 #import "ios/chrome/browser/content_suggestions/ui_bundled/content_suggestions_view_controller.h"
 #import "ios/chrome/browser/content_suggestions/ui_bundled/content_suggestions_view_controller_audience.h"
@@ -855,6 +856,7 @@ using segmentation_platform::TipIdentifier;
     case ContentSuggestionsModuleType::kAppBundlePromo: {
       registry->NotifyCardShown(
           segmentation_platform::kAppBundlePromoEphemeralModule);
+      UMA_HISTOGRAM_BOOLEAN(kAppBundlePromoImpression, true);
       break;
     }
     case ContentSuggestionsModuleType::kDefaultBrowser: {
