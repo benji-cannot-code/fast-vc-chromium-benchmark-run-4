c0fb4685ac5d5a318ff5b9cab67fe5c85c379d44
Grace Cham <hscham@google.com>
2025-11-04 20:37:03 -0800

    Guard touchpad swipe-to-navigate feature with feature flag
    As a kill switch by finch config as fallback.
    This will not be visible to user (e.g. on chrome://flags). They can
    enable/disable the feature on accessibility settings.
    
          accessibility settings when run chrome with \
          command-line flag \
          --disable-features=kAndroidTouchpadOverscrollHistoryNavigation \
          and will follow the previous settings when flag is re-enabled
    
    Test: swipe-to-navigate stops working and option not shown on \
    Bug: 364370338
    Change-Id: I4e358facc3a28a7da5d0a3b0f8465307c0028bc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7061290
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Grace Cham <hscham@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1540458}

--- ui/android/ui_android_features.cc
+++ ui/android/ui_android_features.cc
@@ -44,4 +44,7 @@ BASE_FEATURE(kUseNewEtc1Encoder, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kCheckHitEligibility, base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kAndroidTouchpadOverscrollHistoryNavigation,
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
 }  // namespace ui
