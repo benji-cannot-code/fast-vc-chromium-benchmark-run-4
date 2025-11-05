d03b2c8a80fb29cfb4cfa9caff99ca009c4b4099
Ananya <ananyaseelam@google.com>
2025-11-05 09:38:29 -0800

    [omnibox-next] Limit number of tabs shown in context menu
    We only want to show the 5 most recently active tabs. This CL adds this
    limit.
    
    Bug: 456513170
    Change-Id: I828be9899405969edfdf6c4577260b17d205d331
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7122318
    Reviewed-by: Marlon Facey <mfacey@chromium.org>
    Commit-Queue: Ananya Seelam <ananyaseelam@google.com>
    Cr-Commit-Position: refs/heads/main@{#1540703}

--- chrome/browser/ui/omnibox/omnibox_context_menu_controller.h
+++ chrome/browser/ui/omnibox/omnibox_context_menu_controller.h
@@ -6,14 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_OMNIBOX_OMNIBOX_CONTEXT_MENU_CONTROLLER_H_
 #define CHROME_BROWSER_UI_OMNIBOX_OMNIBOX_CONTEXT_MENU_CONTROLLER_H_
 
+#include <string>
+
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/task/cancelable_task_tracker.h"
+#include "base/time/time.h"
 #include "ui/menus/simple_menu_model.h"
+#include "url/gurl.h"
 
 class BrowserWindowInterface;
-class GURL;
 class FaviconService;
 
 namespace favicon_base {
