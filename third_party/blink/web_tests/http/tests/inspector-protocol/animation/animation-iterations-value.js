cf6569e5fae744c8f05e15482f1274f370b9318e
Ergün Erdoğmuş <ergunsh@chromium.org>
2025-11-05 05:03:12 -0800

    [InspectorAnimations] Mark `iterations` as `null` when they are infinite
    This is already the behavior for the case where the JSON-RPC transport is
    used. This change codifies that behavior on the implementation level &
    makes it consistent between transports.
    
    Change-Id: I5a824c19efe702009ff1de3d40ab38b51531d15b
    Fixed: 444666860
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7124102
    Auto-Submit: Ergün Erdoğmuş <ergunsh@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Ergün Erdoğmuş <ergunsh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1540576}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/animation/animation-iterations-value.js
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(/** @type {import('test_runner').TestRunner} */ testRunner) {
+  var {page, session, dp} = await testRunner.startHTML(`
+    <div id='node' style='background-color: red; height: 100px'></div>
+    <div id='node2' style='background-color: blue; height: 100px'></div>
+  `, 'Tests that animation iterations are reported correctly.');
+
+  dp.Animation.enable();
+
+  // Test for infinite iterations
+  let animationStartedPromise = dp.Animation.onceAnimationStarted();
+  session.evaluate(`
+    window.animation = node.animate([{ width: '100px' }, { width: '200px' }], { duration: 1000, iterations: Infinity });
+  `);
+  let response = await animationStartedPromise;
+  let animation = response.params.animation;
+  testRunner.log('Animation with infinite iterations started');
+  testRunner.log(`iterations: ${'iterations' in animation.source ? animation.source.iterations : 'omitted'}`);
+
+  // Test for finite iterations
+  animationStartedPromise = dp.Animation.onceAnimationStarted();
+  session.evaluate(`
+    window.animation2 = node2.animate([{ width: '100px' }, { width: '200px' }], { duration: 1000, iterations: 5 });
+  `);
+  response = await animationStartedPromise;
+  animation = response.params.animation;
+  testRunner.log('Animation with finite iterations started');
+  testRunner.log(`iterations: ${animation.source.iterations}`);
+
+
+  testRunner.completeTest();
+})
