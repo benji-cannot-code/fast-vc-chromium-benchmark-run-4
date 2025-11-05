7020871ce56ca15f741eff5c13c798acba83e53e
Mingjing Zhang <mjzhang@chromium.org>
2025-11-05 11:55:17 -0800

    Use getters from TransferableResource in blink
    This CL replaces direct usage of fields of TransferableResource in
    third_party/blink with their corresponding getters.
    
    Bug: 40286368
    Change-Id: Ib85ed17d2ee340a38ed47aade65017a27c01182b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7125180
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Mingjing Zhang <mjzhang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1540804}

--- third_party/blink/renderer/platform/graphics/gpu/drawing_buffer_test.cc
+++ third_party/blink/renderer/platform/graphics/gpu/drawing_buffer_test.cc
@@ -403,7 +403,7 @@ TEST_F(DrawingBufferImageChromiumTest, VerifyResizingReallocatesImages) {
   EXPECT_TRUE(drawing_buffer_->PrepareTransferableResource(&resource,
                                                            &release_callback));
   EXPECT_EQ(initial_size, sii->MostRecentSize());
-  EXPECT_TRUE(resource.is_overlay_candidate);
+  EXPECT_TRUE(resource.GetIsOverlayCandidate());
   EXPECT_EQ(initial_size, resource.GetSize());
   testing::Mock::VerifyAndClearExpectations(gl_);
   VerifyStateWasRestored();
@@ -439,7 +439,7 @@ TEST_F(DrawingBufferImageChromiumTest, VerifyResizingReallocatesImages) {
   EXPECT_TRUE(drawing_buffer_->PrepareTransferableResource(&resource,
                                                            &release_callback));
   EXPECT_EQ(alternate_size, sii->MostRecentSize());
-  EXPECT_TRUE(resource.is_overlay_candidate);
+  EXPECT_TRUE(resource.GetIsOverlayCandidate());
   EXPECT_EQ(alternate_size, resource.GetSize());
   gpu::Mailbox mailbox4 = gl_->last_imported_shared_image();
   EXPECT_EQ(2u, sii->shared_image_count());
@@ -475,7 +475,7 @@ TEST_F(DrawingBufferImageChromiumTest, VerifyResizingReallocatesImages) {
   EXPECT_TRUE(drawing_buffer_->PrepareTransferableResource(&resource,
                                                            &release_callback));
   EXPECT_EQ(initial_size, sii->MostRecentSize());
-  EXPECT_TRUE(resource.is_overlay_candidate);
+  EXPECT_TRUE(resource.GetIsOverlayCandidate());
   EXPECT_EQ(initial_size, resource.GetSize());
   testing::Mock::VerifyAndClearExpectations(gl_);
   gpu::Mailbox mailbox6 = gl_->last_imported_shared_image();
@@ -491,7 +491,7 @@ TEST_F(DrawingBufferImageChromiumTest, VerifyResizingReallocatesImages) {
   EXPECT_TRUE(drawing_buffer_->PrepareTransferableResource(&resource,
                                                            &release_callback));
   EXPECT_EQ(initial_size, sii->MostRecentSize());
-  EXPECT_TRUE(resource.is_overlay_candidate);
+  EXPECT_TRUE(resource.GetIsOverlayCandidate());
   EXPECT_EQ(initial_size, resource.GetSize());
   std::move(release_callback).Run(gpu::SyncToken(), false /* lostResource */);
   EXPECT_EQ(2u, sii->shared_image_count());
