49e125cb7aaef976a96f14603d9344ed85cf50ea
David Benjamin <davidben@chromium.org>
2025-11-05 13:06:37 -0800

    Migrate //content to base::HexEncodeLower
    Bug: 456472080
    Change-Id: I2d82ff936522415dd5efb8b6eb358f231b566866
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7109758
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1540832}

--- content/web_test/common/web_test_hash_util.cc
+++ content/web_test/common/web_test_hash_util.cc
@@ -10,8 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 std::string Md5AsHexForWebTestPixels(base::span<const uint8_t> pixels) {
-  return base::ToLowerASCII(
-      base::HexEncode(crypto::obsolete::Md5::Hash(pixels)));
+  return base::HexEncodeLower(crypto::obsolete::Md5::Hash(pixels));
 }
 
 }  // namespace content
