585ec2dc8828e9dff1a8d2aa4d7e3d08534263d3
Sylvain Defresne <sdefresne@chromium.org>
2025-11-05 07:41:40 -0800

    [ios] Convert SyncErrorBrowserAgent to TabsDependencyInstaller
    Instead of re-implementing the observation of unrealized WebStates
    when they are inserted in the WebStateList in SyncErrorBrowserAgent,
    use TabsDependencyInstaller which is providing exactly that.
    
    Bug: none
    Change-Id: I504af2aadf901a09efb5d2463725ef0a4859d43b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7124358
    Commit-Queue: Jood Hajeer <jood@google.com>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Jood Hajeer <jood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1540634}

--- ios/chrome/browser/sync/model/sync_error_browser_agent_unittest.mm
+++ ios/chrome/browser/sync/model/sync_error_browser_agent_unittest.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/passwords/model/password_tab_helper.h"
 #import "ios/chrome/browser/shared/model/browser/test/test_browser.h"
 #import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
+#import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
 #import "ios/chrome/browser/sync/model/sync_service_factory.h"
 #import "ios/web/public/test/fakes/fake_web_state.h"
 #import "ios/web/public/test/web_task_environment.h"
