0976f2af9ffa0e54153ab746a26e8dedcd632139
Tom Anderson <thomasanderson@chromium.org>
2025-11-04 16:56:55 -0800

    Use dbus_utils::CallMethod and ConnectToSignal in chrome/browser
    R=thestig
    
    Change-Id: If00097c324ed4d26e4d5bd62fb83d94026714799
    Bug: None
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7122102
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1540398}

--- chrome/browser/dbus_memory_pressure_evaluator_linux.cc
+++ chrome/browser/dbus_memory_pressure_evaluator_linux.cc
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_features.h"
 #include "components/dbus/thread_linux/dbus_thread_linux.h"
 #include "components/dbus/utils/check_for_service_and_start.h"
-#include "dbus/message.h"
+#include "components/dbus/utils/connect_to_signal.h"
 #include "dbus/object_path.h"
 #include "dbus/object_proxy.h"
 
@@ -98,8 +98,8 @@ void DbusMemoryPressureEvaluatorLinux::CheckIfLmmIsAvailableResponse(
 
     object_proxy_ =
         system_bus_->GetObjectProxy(kLmmService, dbus::ObjectPath(kLmmObject));
-    object_proxy_->ConnectToSignal(
-        kLmmInterface, kLowMemoryWarningSignal,
+    dbus_utils::ConnectToSignal(
+        object_proxy_, kLmmInterface, kLowMemoryWarningSignal,
         base::BindRepeating(
             &DbusMemoryPressureEvaluatorLinux::OnLowMemoryWarning,
             weak_ptr_factory_.GetWeakPtr()),
@@ -137,8 +137,9 @@ void DbusMemoryPressureEvaluatorLinux::CheckIfPortalIsAvailableResponse(
 
     object_proxy_ = session_bus_->GetObjectProxy(
         kXdgPortalService, dbus::ObjectPath(kXdgPortalObject));
-    object_proxy_->ConnectToSignal(
-        kXdgPortalMemoryMonitorInterface, kLowMemoryWarningSignal,
+    dbus_utils::ConnectToSignal(
+        object_proxy_, kXdgPortalMemoryMonitorInterface,
+        kLowMemoryWarningSignal,
         base::BindRepeating(
             &DbusMemoryPressureEvaluatorLinux::OnLowMemoryWarning,
             weak_ptr_factory_.GetWeakPtr()),
@@ -166,15 +167,14 @@ void DbusMemoryPressureEvaluatorLinux::OnSignalConnected(
 }
 
 void DbusMemoryPressureEvaluatorLinux::OnLowMemoryWarning(
-    dbus::Signal* signal) {
+    dbus_utils::ConnectToSignalResultSig<"y"> result) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
-  dbus::MessageReader reader(signal);
-  uint8_t lmm_level;
-  if (!reader.PopByte(&lmm_level)) {
+  if (!result.has_value()) {
     LOG(WARNING) << "Failed to parse low memory level";
     return;
   }
+  auto [lmm_level] = *result;
 
   // static_cast is needed as lmm_level is a uint8_t, which is often an alias to
   // char, meaning that sending it to the output stream would just print the
--- chrome/browser/dbus_memory_pressure_evaluator_linux.h
+++ chrome/browser/dbus_memory_pressure_evaluator_linux.h
@@ -18,12 +18,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequence_checker.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
+#include "components/dbus/utils/connect_to_signal.h"
 #include "components/memory_pressure/system_memory_pressure_evaluator.h"
 #include "dbus/bus.h"
-
-namespace dbus {
-class Signal;
-}  // namespace dbus
 
 namespace memory_pressure {
 class MemoryPressureVoter;
@@ -91,7 +88,7 @@ class DbusMemoryPressureEvaluatorLinux
                          const std::string& signal,
                          bool connected);
 
-  void OnLowMemoryWarning(dbus::Signal* signal);
+  void OnLowMemoryWarning(dbus_utils::ConnectToSignalResultSig<"y"> result);
 
   // Converts a pressure level from LMM to base's memory pressure constants.
   base::MemoryPressureLevel LmmToBasePressureLevel(uint8_t lmm_level);
