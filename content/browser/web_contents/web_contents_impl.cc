fc7da8d11ae805554699a0a7541b56273466d929
Alison Gale <agale@chromium.org>
2025-11-04 16:29:47 -0800

    [SxS] Don't enumerate directories when contents are inactive
    Made a similar change in RunFileChooser but missed this method
    
    Change-Id: Ic57dafe223bd49e317f1f66cc84c6601b049c68c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7121862
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1540388}

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -8214,6 +8214,10 @@ void WebContentsImpl::EnumerateDirectory(
     // Do not allow background tab to open file chooser.
     return;
   }
+  if (!delegate_->IsContentsActive(this)) {
+    // Do not allow inactive tabs to open file chooser.
+    return;
+  }
   if (active_file_chooser_) {
     // Only allow one active file chooser at one time.
     return;
