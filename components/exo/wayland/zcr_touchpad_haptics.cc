d24ecb68515f9228de9cce5e4612822a76834f53
Sergii Bykov <sbykov@google.com>
2025-11-06 07:30:20 -0800

    CodeHealth: Clean up stale base::Feature ExoHapticFeedbackSupport
    Bug: 356233813
    Change-Id: I6197b411119ee0ee223119afd46f157e7ded858e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7124383
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Sergii Bykov <sbykov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1541217}

--- components/exo/wayland/zcr_touchpad_haptics.cc
+++ components/exo/wayland/zcr_touchpad_haptics.cc
@@ -31,9 +31,6 @@ class WaylandTouchpadHapticsDelegate {
   ~WaylandTouchpadHapticsDelegate() = default;
 
   void UpdateTouchpadHapticsState() {
-    if (!base::FeatureList::IsEnabled(ash::features::kExoHapticFeedbackSupport))
-      return;
-
     ui::InputController* controller =
         ui::OzonePlatform::GetInstance()->GetInputController();
     if (!controller) {
@@ -77,8 +74,6 @@ void touchpad_haptics_play(wl_client* client,
                            wl_resource* resource,
                            uint32_t effect,
                            int32_t strength) {
-  if (!base::FeatureList::IsEnabled(ash::features::kExoHapticFeedbackSupport))
-    return;
   GetUserDataAs<WaylandTouchpadHapticsDelegate>(resource)->Play(effect,
                                                                 strength);
 }
