0ef38b4139d3bd28e435580017716aa9785c9fe6
Sylvain Defresne <sdefresne@chromium.org>
2025-11-06 09:30:59 -0800

    [ios] Remove session_state legacy target
    This code is no longer used and can be removed.
    
    This is part of a multi-CLs change:
      - https://crrev.com/c/6979449
      - https://crrev.com/c/6979586
      - https://crrev.com/c/6979667
      - https://crrev.com/c/6979626
      - https://crrev.com/c/6983123
      - https://crrev.com/c/6983202
      - https://crrev.com/c/7074893
      - https://crrev.com/c/6983024
      - https://crrev.com/c/6983144
      - https://crrev.com/c/6982943
     => https://crrev.com/c/6983445
      - https://crrev.com/c/6983504
      - https://crrev.com/c/6983010
      - https://crrev.com/c/6983050
      - https://crrev.com/c/6983423
      - https://crrev.com/c/6983287
      - https://crrev.com/c/7003327
      - https://crrev.com/c/7002783
      - https://crrev.com/c/7002962
      - https://crrev.com/c/7003743
      - https://crrev.com/c/7003123
      - https://crrev.com/c/7003225
      - https://crrev.com/c/7003227
      - https://crrev.com/c/7003087
      - https://crrev.com/c/7027213
    
    Bug: 40945317
    Change-Id: I9e87943d682e822aa8d2adb0a53fabe63b33b1c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6983445
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541279}

--- ios/chrome/browser/profile/model/keyed_service_factories.mm
+++ ios/chrome/browser/profile/model/keyed_service_factories.mm
@@ -144,7 +144,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/segmentation_platform/model/segmentation_platform_service_factory.h"
 #import "ios/chrome/browser/sessions/model/ios_chrome_tab_restore_service_factory.h"
 #import "ios/chrome/browser/sessions/model/session_restoration_service_factory.h"
-#import "ios/chrome/browser/sessions/model/web_session_state_cache_factory.h"
 #import "ios/chrome/browser/share_extension/model/share_extension_service_factory.h"
 #import "ios/chrome/browser/share_kit/model/share_kit_service_factory.h"
 #import "ios/chrome/browser/shared/model/browser/browser_list_factory.h"
@@ -393,7 +392,6 @@ void EnsureProfileKeyedServiceFactoriesBuilt() {
   VariationsClientServiceFactory::GetInstance();
   VerdictCacheManagerFactory::GetInstance();
   VisitedURLRankingServiceFactory::GetInstance();
-  WebSessionStateCacheFactory::GetInstance();
   // Keep the above list alphabetized! Don't just add new entries at the end.
 
 #if BUILDFLAG(BUILD_WITH_TFLITE_LIB)
