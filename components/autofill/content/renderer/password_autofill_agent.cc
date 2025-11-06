5398d541ad385662525c28b9da681e8ba12680d1
Oleksandr Tara <otara@google.com>
2025-11-06 01:43:02 -0800

    Inform PasswordManager about actor login filling fields
    This should trigger `ProvisionallySave` for the form being filled.
    Otherwise we do not see the submitted form and therefore don't update
    actor login permissions.
    
    Fixed: 456375487
    Bug: 454961534
    Change-Id: I873bc04f442012f724748d0a51daebff363adc8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7117079
    Commit-Queue: Oleksandr Tara <otara@google.com>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541105}

--- components/autofill/content/renderer/password_autofill_agent.cc
+++ components/autofill/content/renderer/password_autofill_agent.cc
@@ -993,6 +993,11 @@ void PasswordAutofillAgent::FillField(
     return;
   }
   DoFillField(input_element, value, field_properties);
+  if (base::FeatureList::IsEnabled(
+          password_manager::features::kActorLoginTreatFillingAsUserInput)) {
+    InformBrowserAboutUserInput(input_element.GetOwningFormForAutofill(),
+                                input_element, /*form_cache=*/{});
+  }
   std::move(success_callback).Run(true);
 }
 
