8c3e61b60dddc98295bf2447daac30234e7d7c89
Kent Tamura <tkent@chromium.org>
2025-11-05 18:51:50 -0800

    url: Introduce MakeRange(size_t, size_t)
    This is helpful to reduce type casts.
    This CL fixes ambiguous function call errors due to two MakeRange().
    
    Bug: 350788890
    Change-Id: I22895bb3a30604551ab0ab403545df1b11294849
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7123400
    Owners-Override: Hayato Ito <hayato@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1540992}

--- url/third_party/mozilla/url_parse.cc
+++ url/third_party/mozilla/url_parse.cc
@@ -58,6 +58,12 @@ std::ostream& operator<<(std::ostream& os, const Parsed& parsed) {
             << ", has_opaque_path: " << parsed.has_opaque_path << " }";
 }
 
+Component MakeRange(size_t begin, size_t end) {
+  CHECK_LE(begin, end);
+  return Component(base::checked_cast<int>(begin),
+                   base::checked_cast<int>(end - begin));
+}
+
 namespace {
 
 // Returns true if the given character is a valid digit to use in a port.
@@ -308,7 +314,7 @@ bool DoExtractScheme(std::basic_string_view<CharT> url, Component* scheme) {
   // Find the first colon character.
   for (size_t i = begin; i < url.size(); i++) {
     if (url[i] == ':') {
-      *scheme = MakeRange(begin, base::checked_cast<int>(i));
+      *scheme = MakeRange(begin, i);
       return true;
     }
   }
