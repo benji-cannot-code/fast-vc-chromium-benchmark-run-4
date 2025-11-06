a4f1dfdd696ad074379a72010527abb35a187c0e
Arthur Milchior <arthurmilchior@google.com>
2025-11-06 01:08:36 -0800

    [iOS]Send tab to self listen to primary identity
    This listen to change of primary account. If the account is removed,
    i.e. the user is signed-out, the view is closed. Maybe an error message
    would be better, but given how rare this is expected to be it may not be
    worth it. If the account changed to a different account (only possible
    on ipad), this request to refresh the view.
    
    Fixed: 456763077
    Change-Id: I87a76b09e5d13c393a0951b2b986ee38a1b6943f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7108160
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541096}

--- ios/chrome/browser/send_tab_to_self/coordinator/send_tab_to_self_coordinator.mm
+++ ios/chrome/browser/send_tab_to_self/coordinator/send_tab_to_self_coordinator.mm
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/authentication/ui_bundled/signin_presenter.h"
 #import "ios/chrome/browser/infobars/ui_bundled/presentation/infobar_modal_positioner.h"
 #import "ios/chrome/browser/send_tab_to_self/coordinator/send_tab_to_self_coordinator_delegate.h"
+#import "ios/chrome/browser/send_tab_to_self/coordinator/send_tab_to_self_mediator.h"
+#import "ios/chrome/browser/send_tab_to_self/coordinator/send_tab_to_self_mediator_delegate.h"
 #import "ios/chrome/browser/send_tab_to_self/model/send_tab_to_self_browser_agent.h"
 #import "ios/chrome/browser/send_tab_to_self/ui/send_tab_to_self_modal_delegate.h"
 #import "ios/chrome/browser/send_tab_to_self/ui/send_tab_to_self_modal_presentation_controller.h"
@@ -52,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/signin/model/avatar_provider.h"
 #import "ios/chrome/browser/signin/model/chrome_account_manager_service.h"
 #import "ios/chrome/browser/signin/model/chrome_account_manager_service_factory.h"
+#import "ios/chrome/browser/signin/model/identity_manager_factory.h"
 #import "ios/chrome/browser/signin/model/system_identity.h"
 #import "ios/chrome/browser/sync/model/send_tab_to_self_sync_service_factory.h"
 #import "ios/chrome/browser/sync/model/sync_service_factory.h"
@@ -128,6 +131,7 @@ void OpenManageDevicesTab(CommandDispatcher* dispatcher) {
 }  // namespace
 
 @interface SendTabToSelfCoordinator () <InfobarModalPositioner,
+                                        SendTabToSelfMediatorDelegate,
                                         SendTabToSelfModalDelegate,
                                         UIViewControllerTransitioningDelegate> {
   std::unique_ptr<TargetDeviceListWaiter> _targetDeviceListWaiter;
@@ -154,6 +158,8 @@ void OpenManageDevicesTab(CommandDispatcher* dispatcher) {
   SigninCoordinator* _signinCoordinator;
   // The navigation controller displaying the send tab to self.
   UINavigationController* _navigationController;
+  // The mediator of this coordinator.
+  SendTabToSelfMediator* _mediator;
 }
 
 #pragma mark - Public
@@ -187,6 +193,12 @@ void OpenManageDevicesTab(CommandDispatcher* dispatcher) {
     [self.delegate sendTabToSelfCoordinatorWantsToBeStopped:self];
     return;
   }
+  _mediator = [[SendTabToSelfMediator alloc]
+      initWithAuthenticationService:AuthenticationServiceFactory::GetForProfile(
+                                        self.profile)
+                    identityManager:IdentityManagerFactory::GetForProfile(
+                                        self.profile)];
+  _mediator.delegate = self;
   [self show];
 }
 
@@ -198,6 +210,9 @@ void OpenManageDevicesTab(CommandDispatcher* dispatcher) {
   // Abort the waiting if it's still ongoing.
   _targetDeviceListWaiter.reset();
   [self stopSigninCoordinator];
+  [_mediator disconnect];
+  _mediator.delegate = nil;
+  _mediator = nil;
   [_navigationController.presentingViewController
       dismissViewControllerAnimated:YES
                          completion:self.dismissedCompletion];
@@ -222,6 +237,23 @@ void OpenManageDevicesTab(CommandDispatcher* dispatcher) {
                  presentingViewController:presenting];
   presentationController.modalPositioner = self;
   return presentationController;
+}
+
+#pragma mark - SendTabToSelfMediatorDelegate
+
+- (void)mediatorWantsToBeStopped:(SendTabToSelfMediator*)mediator {
+  CHECK_EQ(mediator, _mediator, base::NotFatalUntil::M150);
+  [self.delegate sendTabToSelfCoordinatorWantsToBeStopped:self];
+}
+
+- (void)mediatorWantsToRefreshView:(SendTabToSelfMediator*)mediator {
+  CHECK_EQ(mediator, _mediator, base::NotFatalUntil::M150);
+  if (_signinCoordinator) {
+    // Nothing to refresh in case of sign-in. The signin coordinator will deal
+    // with the update itself.
+    return;
+  }
+  [self show];
 }
 
 #pragma mark - InfobarModalPositioner
---
+++ ios/chrome/browser/send_tab_to_self/coordinator/send_tab_to_self_mediator_delegate.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SEND_TAB_TO_SELF_COORDINATOR_SEND_TAB_TO_SELF_MEDIATOR_DELEGATE_H_
+#define IOS_CHROME_BROWSER_SEND_TAB_TO_SELF_COORDINATOR_SEND_TAB_TO_SELF_MEDIATOR_DELEGATE_H_
+
+@class SendTabToSelfMediator;
+
+@protocol SendTabToSelfMediatorDelegate
+
+// Requests to stop the send-tab-to-self
+- (void)mediatorWantsToBeStopped:(SendTabToSelfMediator*)mediator;
+
+// Requests to refresh the view’s content.
+- (void)mediatorWantsToRefreshView:(SendTabToSelfMediator*)mediator;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_SEND_TAB_TO_SELF_COORDINATOR_SEND_TAB_TO_SELF_MEDIATOR_DELEGATE_H_
