1b69b661c7a4b8ea1a6acd46d4e962d22c98f69f
Aaron Leventhal <aleventhal@google.com>
2025-11-06 08:06:50 -0800

    [APC] Map form action url through new APC field
    Bug: 441842653
    Change-Id: If51340cc428bb33d4d2a57346317b486f0f44610
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7125178
    Auto-Submit: Aaron Leventhal <aleventhal@google.com>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Reviewed-by: Aaron Leventhal <aleventhal@google.com>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1541236}

--- third_party/blink/renderer/modules/content_extraction/ai_page_content_agent.cc
+++ third_party/blink/renderer/modules/content_extraction/ai_page_content_agent.cc
@@ -656,6 +656,8 @@ void ProcessFormNode(const HTMLFormElement& form_element,
   if (const auto& name = form_element.GetName()) {
     form_data->form_name = name;
   }
+  form_data->action_url = KURL(form_element.action());
+
   attributes.form_data = std::move(form_data);
 }
 
