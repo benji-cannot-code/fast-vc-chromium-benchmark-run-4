d073dfe700c3c51de4b5e5aa774181723c216e79
Steve Becker <stevebe@microsoft.com>
2025-11-05 21:47:50 -0800

    [DomStorage] Update LocalStorageImpl to use database ReadAllMetadata()
    Updates `LocalStorageImpl` to use
    `AsyncDomStorageDatabase::ReadAllMetadata()` in two locations:
    
    1. The implementation of `mojom::LocalStorageControl::GetUsage()`, which
    returns the storage key, last modified timestamp and total bytes for
    each local storage map.
    
    2. `DeleteStaleStorageAreas()` uses `ReadAllMetadata()` to determine
    which stale local storage maps to delete. A map becomes stale after 400
    days without use.
    
    Bug: 377242771
    Change-Id: I0ebbf5f956d46ee7b48e32e127acec13370c49c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7093333
    Reviewed-by: Evan Stade <evanstade@microsoft.com>
    Commit-Queue: Steve Becker <stevebe@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1541040}

--- components/services/storage/dom_storage/local_storage_impl.h
+++ components/services/storage/dom_storage/local_storage_impl.h
@@ -143,7 +143,7 @@ class LocalStorageImpl : public base::trace_event::MemoryDumpProvider,
   // from that function, or through |on_database_open_callbacks_|.
   void RetrieveStorageUsage(GetUsageCallback callback);
   void OnGotWriteMetaData(GetUsageCallback callback,
-                          std::vector<DomStorageDatabase::KeyValuePair> data);
+                          StatusOr<DomStorageDatabase::Metadata> all_metadata);
 
   void OnGotStorageUsageForShutdown(
       std::vector<mojom::StorageUsageInfoPtr> usage);
@@ -157,7 +157,7 @@ class LocalStorageImpl : public base::trace_event::MemoryDumpProvider,
   // the database. See crbug.com/40281870 for more info.
   void DeleteStaleStorageAreas();
   void OnGotMetaDataToDeleteStaleStorageAreas(
-      std::vector<DomStorageDatabase::KeyValuePair> data);
+      StatusOr<DomStorageDatabase::Metadata> all_metadata);
   void OnReceiverDisconnected();
 
   // Passed in by the StorageServiceImpl that owns this object. Used to signal
