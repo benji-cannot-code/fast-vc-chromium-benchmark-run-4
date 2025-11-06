80e3fa1c0f5077b873fd9940f48d8c7b2b89577d
Nic Champagne Williamson <champnic@microsoft.com>
2025-11-06 14:07:40 -0800

    Fix Dolby EC-3 regression due to MediaFoundationAudioDecoder::ConfigureOutput
    A recent commit changed the signature of the ConfigureOutput function
    to return an HRESULT instead of a bool. However, one usage of this
    function wasn't updated, so it was still checking for a bool value. Thus
    when the function succeeds with S_OK, which is 0, the check would
    evaluate as false which was interpreted as a failure.
    
    Bug: 452663013
    Change-Id: I436929ed79cf51ada32f8aa8948aeed5f995d04d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7124910
    Reviewed-by: Piet Schouten <Piet.Schouten@microsoft.com>
    Commit-Queue: Nic Williamson <champnic@microsoft.com>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541437}

--- media/filters/win/media_foundation_audio_decoder.cc
+++ media/filters/win/media_foundation_audio_decoder.cc
@@ -510,7 +510,7 @@ MediaFoundationAudioDecoder::PumpOutput(PumpState pump_state) {
 
   if (hr == MF_E_TRANSFORM_STREAM_CHANGE &&
       pump_state != PumpState::kStreamChange) {
-    if (!ConfigureOutput()) {
+    if (FAILED(ConfigureOutput())) {
       return OutputStatus::kFailed;
     }
 
