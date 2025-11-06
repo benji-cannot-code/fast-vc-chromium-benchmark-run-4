79c8b5480534c6860a5346779036cb1fac747ade
Sylvain Defresne <sdefresne@chromium.org>
2025-11-06 12:20:11 -0800

    [ios] Stop observing Browser in SendTabToSelfBrowserAgent
    As BrowserAgents are owned by the Browser to which they are attached,
    it is not necessary to observe the Browser for destruction. Instead
    the code in BrowserDestroyed() can be moved to the destructor of the
    BrowserAgent.
    
    Bug: none
    Change-Id: I31b517c6521d1f1107540e4600c5e9009b492e58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7124108
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541379}

--- ios/chrome/browser/send_tab_to_self/model/send_tab_to_self_browser_agent.h
+++ ios/chrome/browser/send_tab_to_self/model/send_tab_to_self_browser_agent.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/memory/raw_ptr.h"
 #import "base/scoped_observation.h"
 #import "components/send_tab_to_self/send_tab_to_self_model_observer.h"
-#import "ios/chrome/browser/shared/model/browser/browser_observer.h"
 #import "ios/chrome/browser/shared/model/browser/browser_user_data.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list_observer.h"
 #import "ios/web/public/web_state_observer.h"
@@ -34,8 +33,7 @@ class SendTabToSelfBrowserAgent
     : public BrowserUserData<SendTabToSelfBrowserAgent>,
       public send_tab_to_self::SendTabToSelfModelObserver,
       public WebStateListObserver,
-      public web::WebStateObserver,
-      public BrowserObserver {
+      public web::WebStateObserver {
  public:
   ~SendTabToSelfBrowserAgent() override;
 
@@ -66,9 +64,6 @@ class SendTabToSelfBrowserAgent
 
   explicit SendTabToSelfBrowserAgent(Browser* browser);
 
-  // BrowserObserver::
-  void BrowserDestroyed(Browser* browser) override;
-
   // Display an infobar for `entry` on the specified `web_state`.
   void DisplayInfoBar(web::WebState* web_state,
                       const send_tab_to_self::SendTabToSelfEntry* entry);
@@ -85,8 +80,6 @@ class SendTabToSelfBrowserAgent
 
   // The WebState that is being observed for activation, if any.
   raw_ptr<web::WebState> pending_web_state_ = nullptr;
-
-  base::ScopedObservation<Browser, BrowserObserver> browser_observation_{this};
 
   base::ScopedObservation<WebStateList, WebStateListObserver>
       web_state_list_observation_{this};
