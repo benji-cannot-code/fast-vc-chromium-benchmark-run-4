97a0d52fd78aca3efcc62f8785f8f9a00b4a78fd
Masa Fujita <massan@google.com>
2025-11-06 14:46:21 -0800

    flyout: Do not expose the list of windows
    Now that `FlyoutController` manages the list of the popups, we can hide
    `FlyoutPopupEntry` so that the users have only access to the window.
    
    Bug: 433410990
    Change-Id: Iaa2207eb770350027bffdc7d955e5055d2860647
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7116518
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Commit-Queue: Masa Fujita <massan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1541456}

--- ui/android/junit/src/org/chromium/ui/hierarchicalmenu/FlyoutControllerUnitTest.java
+++ ui/android/junit/src/org/chromium/ui/hierarchicalmenu/FlyoutControllerUnitTest.java
@@ -160,7 +160,7 @@ public class FlyoutControllerUnitTest {
         triggerHoverEnter(mSubmenuLevel0, 0, List.of(mSubmenuLevel0));
 
         // Verify that before the delay, no new window is added.
-        Assert.assertEquals("There should be 1 popup.", 1, mFlyoutController.getPopups().size());
+        Assert.assertEquals("There should be 1 popup.", 1, mFlyoutController.getNumberOfPopups());
         verify(mFlyoutHandler, never()).createAndShowFlyoutPopup(any(), any(), any());
 
         // Wait for the UI delay.
@@ -168,7 +168,7 @@ public class FlyoutControllerUnitTest {
 
         // Verify that the call to create a new popup (level 1) is called.
         verify(mFlyoutHandler).createAndShowFlyoutPopup(eq(mSubmenuLevel0), eq(mListView), any());
-        Assert.assertEquals("There should be 2 popups.", 2, mFlyoutController.getPopups().size());
+        Assert.assertEquals("There should be 2 popups.", 2, mFlyoutController.getNumberOfPopups());
 
         // Hover on an item inside the level 1 popup for long enough.
         triggerHoverEnter(mSubmenuLevel1, 1, List.of(mSubmenuLevel0, mSubmenuLevel1));
@@ -176,7 +176,7 @@ public class FlyoutControllerUnitTest {
 
         // Verify that the call to create another popup (level 2) is called.
         verify(mFlyoutHandler).createAndShowFlyoutPopup(eq(mSubmenuLevel1), eq(mListView), any());
-        Assert.assertEquals("There should be 3 popups.", 3, mFlyoutController.getPopups().size());
+        Assert.assertEquals("There should be 3 popups.", 3, mFlyoutController.getNumberOfPopups());
     }
 
     @Test
@@ -193,7 +193,7 @@ public class FlyoutControllerUnitTest {
         waitForUiDelay();
 
         // Popups of level 1 and 2 should be removed.
-        Assert.assertEquals("There should be 1 popup.", 1, mFlyoutController.getPopups().size());
+        Assert.assertEquals("There should be 1 popup.", 1, mFlyoutController.getNumberOfPopups());
 
         // Create level 1 and 2 popup windows.
         triggerHoverEnter(mSubmenuLevel0, 0, List.of(mSubmenuLevel0));
@@ -206,7 +206,7 @@ public class FlyoutControllerUnitTest {
         waitForUiDelay();
 
         // Level 2 popup should be removed, but level 1 popup should remain.
-        Assert.assertEquals("There should be 2 popups.", 2, mFlyoutController.getPopups().size());
+        Assert.assertEquals("There should be 2 popups.", 2, mFlyoutController.getNumberOfPopups());
     }
 
     @Test
@@ -222,7 +222,7 @@ public class FlyoutControllerUnitTest {
         waitForUiDelay();
 
         // Level 2 popup should be removed, but level 1 popup should remain.
-        Assert.assertEquals("There should be 2 popups.", 2, mFlyoutController.getPopups().size());
+        Assert.assertEquals("There should be 2 popups.", 2, mFlyoutController.getNumberOfPopups());
     }
 
     private void triggerHoverEnter(ListItem item, int level, List<ListItem> path) {
