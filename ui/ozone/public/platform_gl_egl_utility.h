9648a88f1634b6239c675c6978cd7c412f725782
Colin Blundell <blundell@chromium.org>
2025-11-05 23:52:31 -0800

    [//ui] Remove unneeded PlatformGLEGLUtility method
    Both implementations of this method are now empty.
    
    Bug: 457646315
    Change-Id: If23f508e3ebd8a34e80dd27a68586731527db858
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7124018
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541067}

--- ui/ozone/public/platform_gl_egl_utility.h
+++ ui/ozone/public/platform_gl_egl_utility.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/component_export.h"
 #include "base/scoped_environment_variable_override.h"
 #include "third_party/khronos/EGL/egl.h"
-#include "ui/gfx/gpu_extra_info.h"
 
 namespace ui {
 
@@ -30,11 +29,6 @@ class COMPONENT_EXPORT(OZONE_BASE) PlatformGLEGLUtility {
   // Chooses alpha and buffer size values.
   virtual void ChooseEGLAlphaAndBufferSize(EGLint* alpha_size,
                                            EGLint* buffer_size) = 0;
-
-  // Fills in the platform specific bits of the GPU extra info holder.
-  // |enable_native_gpu_memory_buffers| should be taken from GpuPreferences.
-  virtual void CollectGpuExtraInfo(bool enable_native_gpu_memory_buffers,
-                                   gfx::GpuExtraInfo& gpu_extra_info) const = 0;
 
   // X11 specific; returns whether the platform supports visuals.
   virtual bool HasVisualManager();
