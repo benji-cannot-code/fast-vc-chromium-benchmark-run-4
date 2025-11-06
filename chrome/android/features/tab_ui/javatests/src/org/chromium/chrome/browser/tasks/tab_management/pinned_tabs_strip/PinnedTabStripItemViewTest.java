5e216a7e26ebe78107ae4fbdeb00361694973593
Sky Malice <skym@chromium.org>
2025-11-06 12:54:38 -0800

    Disable flaky PinnedTabStripItemViewTest render tests.
    Bug: 456490811, 456503160, 456499455
    Change-Id: Ib08b719bea9d34fa9769262971fb73824a23a780
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7128513
    Commit-Queue: Sky Malice <skym@chromium.org>
    Auto-Submit: Sky Malice <skym@chromium.org>
    Commit-Queue: Madhav Pruthi <madhavpruthi@google.com>
    Reviewed-by: Madhav Pruthi <madhavpruthi@google.com>
    Owners-Override: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541397}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/pinned_tabs_strip/PinnedTabStripItemViewTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/pinned_tabs_strip/PinnedTabStripItemViewTest.java
@@ -40,6 +40,7 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.BaseActivityTestRule;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.tab_ui.TabListFaviconProvider;
 import org.chromium.chrome.browser.tasks.tab_management.TabActionListener;
@@ -248,6 +249,7 @@ public class PinnedTabStripItemViewTest {
     @Test
     @SmallTest
     @Feature({"RenderTest"})
+    @DisabledTest(message = "https://crbug.com/456490811")
     public void testRenderView() throws Exception {
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
@@ -262,6 +264,7 @@ public class PinnedTabStripItemViewTest {
     @Test
     @SmallTest
     @Feature({"RenderTest"})
+    @DisabledTest(message = "https://crbug.com/456503160")
     public void testRenderView_NotSelected() throws Exception {
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
@@ -276,6 +279,7 @@ public class PinnedTabStripItemViewTest {
     @Test
     @SmallTest
     @Feature({"RenderTest"})
+    @DisabledTest(message = "https://crbug.com/456499455")
     public void testRenderView_Incognito() throws Exception {
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
