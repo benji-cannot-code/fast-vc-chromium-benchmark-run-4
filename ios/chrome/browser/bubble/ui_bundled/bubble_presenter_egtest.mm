32f2e23d999086347b7888d529a2f9af6ea909b2
Scott Yoder <scottyoder@google.com>
2025-11-05 18:18:44 -0800

    [ios] Make BubblePresenterTestCase test more resilient
    This change makes several improvements to the
    testLensOverlayEntrypointTipDismissedWhenOmniboxPositionChanged test:
     * sets the iph_feature_enabled in the appConfiguration: method rather
       than forcing a second restart in the test
     * uses waitForUIElementTo[Dis]AppearWithMatcher methods which use a
       standard timeout to wait for a view to appear/disappear - 1 second
       is not enough when EG test bots run slow
    
    Change-Id: Idbfebad742f216e7785951737abf32bf947b53b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7125383
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1540983}

--- ios/chrome/browser/bubble/ui_bundled/bubble_presenter_egtest.mm
+++ ios/chrome/browser/bubble/ui_bundled/bubble_presenter_egtest.mm
@@ -103,6 +103,7 @@ void ReloadFromOmnibox() {
           (testLensOverlayEntrypointTipDismissedWhenOmniboxPositionChanged)]) {
     config.features_enabled.push_back(kEnableLensOverlay);
     config.features_disabled.push_back(kPageActionMenu);
+    config.iph_feature_enabled = "IPH_iOSLensOverlayEntrypointTip";
   }
 
   return config;
@@ -628,9 +629,6 @@ void ReloadFromOmnibox() {
   if ([ChromeEarlGrey isIPadIdiom]) {
     EARL_GREY_TEST_SKIPPED(@"Skipped for iPad (IPH is iPhone only)");
   }
-  RelaunchConfigurationWithIPHFeature([self appConfigurationForTestCase],
-                                      @"IPH_iOSLensOverlayEntrypointTip",
-                                      /*safari_switcher=*/NO);
 
   // Load a random page.
   GREYAssertTrue(self.testServer->Start(), @"Server did not start.");
@@ -638,12 +636,9 @@ void ReloadFromOmnibox() {
   [ChromeEarlGrey loadURL:destinationUrl1];
 
   // Verify Lens overlay entrypoint tip is shown.
-  GREYAssertTrue(
-      [ChromeEarlGrey
-          testUIElementAppearanceWithMatcher:grey_accessibilityID(
-                                                 kBubbleViewArrowViewIdentifier)
-                                     timeout:base::Seconds(1)],
-      @"Lens overlay entrypoint tip is not shown");
+  [ChromeEarlGrey
+      waitForUIElementToAppearWithMatcher:grey_accessibilityID(
+                                              kBubbleViewArrowViewIdentifier)];
 
   // Move Omnibox to the bottom.
   [ChromeEarlGrey setBoolValue:YES
@@ -652,10 +647,8 @@ void ReloadFromOmnibox() {
 
   // Verify Lens overlay entrypoint tip is hidden after Omnibox position
   // changed.
-  GREYAssertFalse(
-      [ChromeEarlGrey testUIElementAppearanceWithMatcher:
-                          grey_accessibilityID(kBubbleViewArrowViewIdentifier)],
-      @"Lens overlay entrypoint tip is still shown");
+  [ChromeEarlGrey waitForUIElementToDisappearWithMatcher:
+                      grey_accessibilityID(kBubbleViewArrowViewIdentifier)];
 }
 
 @end
