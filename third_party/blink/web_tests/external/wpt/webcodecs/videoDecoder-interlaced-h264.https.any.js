c3c622befd810f9204c5feb368b1c7693704f189
Dale Curtis <dalecurtis@chromium.org>
2025-11-06 10:39:16 -0800

    [WebCodecs] Route interlaced H.264 content to the software decoder
    None of our hardware decoders support interlaced decoding, so we
    should route this content to the software decoder.
    
    The wrinkle is that there's no information in the codec string
    which would tell us that the content is interlaced. For AVC content
    we can peek at the description, but for annex-b content we won't
    know until the first decode() call.
    
    Given how rare this content is, it's not worth holding all annex-b
    configure() calls until the first decode().
    
    As such, this fix only works for AVC formatted content, annex-b
    content will continue to throw a decoding error; though I've
    promoted the DVLOG to a DLOG to help identify this case more
    quickly in the future.
    
    Bug: 456919096
    
    Change-Id: I8a5578e3abdf6a018316931f883796d84f080d8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7115500
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541313}

---
+++ third_party/blink/web_tests/external/wpt/webcodecs/videoDecoder-interlaced-h264.https.any.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=window,dedicatedworker
+// META: script=videoDecoder-codec-specific-setup.js
+// META: variant=?h264_interlaced_avc
+
+promise_test(async t => {
+  await checkImplements();
+
+  const callbacks = {};
+  const decoder = createVideoDecoder(t, callbacks);
+  decoder.configure(CONFIG);
+  decoder.decode(CHUNKS[0]);
+
+  let outputs = 0;
+  callbacks.output = frame => {
+    outputs++;
+    assert_equals(frame.timestamp, 0, 'timestamp');
+    frame.close();
+  };
+
+  await decoder.flush();
+  assert_equals(outputs, 1, 'outputs');
+}, 'Test decoding h.264 interlaced content');
