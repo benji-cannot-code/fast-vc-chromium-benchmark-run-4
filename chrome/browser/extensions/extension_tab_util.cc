580a1ae96e58946cb4a3b414706a9312383be739
EmiliaPaz <emiliapaz@chromium.org>
2025-11-06 10:58:57 -0800

    [Extensions] Enable CreateMutedInfo in Desktop Android
    Some tab muted functionality was enabled on Desktop Android, thus
    CreateMutedInfo() can be migrated.
    
    Bug: 430344931
    Change-Id: I495bda5afe00e5160ccbda3547e626fd1d03b532
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7124353
    Auto-Submit: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Andrea Orru <andreaorru@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541330}

--- chrome/browser/extensions/extension_tab_util.cc
+++ chrome/browser/extensions/extension_tab_util.cc
@@ -649,7 +649,6 @@ base::Value::Dict ExtensionTabUtil::CreateWindowValueForExtension(
       extension, populate_tab_behavior, context);
 }
 
-#if !BUILDFLAG(IS_ANDROID)
 // static
 api::tabs::MutedInfo ExtensionTabUtil::CreateMutedInfo(
     content::WebContents* contents) {
@@ -673,7 +672,6 @@ api::tabs::MutedInfo ExtensionTabUtil::CreateMutedInfo(
   }
   return info;
 }
-#endif
 
 // static
 ExtensionTabUtil::ScrubTabBehavior ExtensionTabUtil::GetScrubTabBehavior(
