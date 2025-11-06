f053083495d93f25bf3b7c65b4ad01217691fb50
Sky Malice <skym@chromium.org>
2025-11-06 14:34:28 -0800

    Disable flaky #testOverflow().
    Bug: 458450979
    Change-Id: Ifd9e0c335c36c1386c057f49d28905101b1b2779
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7128640
    Commit-Queue: Sky Malice <skym@chromium.org>
    Owners-Override: Sky Malice <skym@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Auto-Submit: Sky Malice <skym@chromium.org>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541447}

--- chrome/browser/optimization_guide/android/java/src/org/chromium/chrome/browser/optimization_guide/OptimizationGuidePushNotificationManagerUnitTest.java
+++ chrome/browser/optimization_guide/android/java/src/org/chromium/chrome/browser/optimization_guide/OptimizationGuidePushNotificationManagerUnitTest.java
@@ -29,6 +29,7 @@ import org.mockito.junit.MockitoRule;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.util.Batch;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.browser.preferences.ChromeSharedPreferences;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.profiles.ProfileManager;
@@ -173,6 +174,7 @@ public class OptimizationGuidePushNotificationManagerUnitTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/458450979")
     public void testOverflow() {
         OptimizationGuidePushNotificationManager.setNativeIsInitializedForTesting(false);
 
