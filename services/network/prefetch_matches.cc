16a97034ca9d85c81ba091d9a29d88f78fdd31df
Adam Rice <ricea@chromium.org>
2025-11-06 04:08:48 -0800

    Subresource prefetch: Histograms for header mismatches
    Log new histograms for headers missing from the prefetch, headers with
    different values in the prefetch, and headers that were in the prefetch
    but not in the real request.
    
    The new histograms are:
    
    Network.PrefetchMatches.FirstHeaderMissingFromPrefetch
    Network.PrefetchMatches.FirstHeaderValueMismatch
    Network.PrefetchMatches.HeaderOnlyInPrefetch
    
    Use absl::flat_hash_map instead of std::unordered_map in the header
    comparison routine to improve efficiency.
    
    Add LowerCaseRequestHeaderToEnum() function and RequestHeader enum to
    aid logging request headers to UMA. Add LogLowerCaseRequestHeaderToUma()
    function to log a request header to the supplied histogram.
    
    The new functions are in separate files to avoid making
    prefetch_matches.cc harder to read than it is already.
    
    Bug: 342445996
    Change-Id: I7d5c3dfc64e873b66517680ee4777463b5156420
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7106918
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541144}

--- services/network/prefetch_matches.cc
+++ services/network/prefetch_matches.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <string_view>
 #include <type_traits>
-#include <unordered_map>
 #include <utility>
 
 #include "base/check.h"
@@ -38,7 +37,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/data_element.h"
 #include "services/network/public/cpp/resource_request.h"
 #include "services/network/public/cpp/resource_request_body.h"
+#include "services/network/request_header_to_enum.h"
 #include "services/network/stringify_enum.h"
+#include "third_party/abseil-cpp/absl/container/flat_hash_map.h"
 
 namespace network {
 
@@ -353,7 +354,7 @@ using IgnoredHeadersType = decltype(kIgnoredHeaders);
 bool MatchHeadersWithExceptions(const net::HttpRequestHeaders& prefetch_headers,
                                 const net::HttpRequestHeaders& real_headers,
                                 const IgnoredHeadersType& ignored_headers) {
-  std::unordered_map<std::string, std::string_view> lowered_prefetch_headers;
+  absl::flat_hash_map<std::string, std::string_view> lowered_prefetch_headers;
   const net::HttpRequestHeaders::HeaderVector& prefetch_headers_vector =
       prefetch_headers.GetHeaderVector();
   lowered_prefetch_headers.reserve(prefetch_headers_vector.size());
@@ -370,22 +371,37 @@ bool MatchHeadersWithExceptions(const net::HttpRequestHeaders& prefetch_headers,
   }
   const net::HttpRequestHeaders::HeaderVector& real_headers_vector =
       real_headers.GetHeaderVector();
-  size_t real_header_count = 0;
   for (const auto& keyvalue : real_headers_vector) {
     std::string lowered_key = base::ToLowerASCII(keyvalue.key);
     if (ignored_headers.contains(lowered_key)) {
       continue;
     }
-    ++real_header_count;
     auto it = lowered_prefetch_headers.find(lowered_key);
     if (it == lowered_prefetch_headers.end()) {
+      LogLowerCaseRequestHeaderToUma(
+          "Network.PrefetchMatches.FirstHeaderMissingFromPrefetch",
+          lowered_key);
       return false;
     }
     if (it->second != keyvalue.value) {
+      LogLowerCaseRequestHeaderToUma(
+          "Network.PrefetchMatches.FirstHeaderValueMismatch", lowered_key);
       return false;
     }
+    lowered_prefetch_headers.erase(it);
   }
-  return real_header_count == lowered_prefetch_headers.size();
+  if (lowered_prefetch_headers.empty()) {
+    return true;
+  }
+
+  // Any value in `lowered_prefetch_headers` that hasn't been erased was missing
+  // from the real headers. Prefetches never have a lot of headers, so just
+  // record all of them.
+  for (const auto& [name, _] : lowered_prefetch_headers) {
+    LogLowerCaseRequestHeaderToUma(
+        "Network.PrefetchMatches.HeaderOnlyInPrefetch", name);
+  }
+  return false;
 }
 
 // MatchByType() can be overloaded to provide special behavior for specific
