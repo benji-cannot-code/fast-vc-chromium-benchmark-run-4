722367b928c24293b05e81df736919fef69e0571
Antonio Alphonse <alphonsea@google.com>
2025-11-05 16:25:42 -0800

    ash/wallpaper: base/hash/sha1.h -> crypto/obsolete/sha1.h
    SHA-1 is currently being used inside of HashWallpaperFilesIdStr to
    define a hashed id for wallpaper files. This function is meant to be a
    way to identify users' wallpaper files in the filesystem. Since changing
    the hashing algorithm from SHA-1 to another may cause users' wallpapers
    to become lost, we should hold off on switching to SHA-256 (or another
    hashing method) for wallpaper file IDs.
    
    This CL instead migrates to using crypto/obsolete/sha1.h instead of
    base/hash/sha1.h. Both function the exact same, they're just in
    different locations.
    
    Bug: 430344933
    Change-Id: I4b85a014a35becc0daff1355369729fbdfeef843
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7119618
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Auto-Submit: Antonio Alphonse <alphonsea@google.com>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1540939}

--- chrome/browser/ui/ash/wallpaper/wallpaper_controller_client_impl.cc
+++ chrome/browser/ui/ash/wallpaper/wallpaper_controller_client_impl.cc
@@ -24,8 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/containers/extend.h"
 #include "base/functional/bind.h"
-#include "base/hash/hash.h"
-#include "base/hash/sha1.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/path_service.h"
 #include "base/rand_util.h"
@@ -66,6 +64,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/user_manager/known_user.h"
 #include "components/user_manager/user_manager.h"
 #include "content/public/browser/render_widget_host_view.h"
+#include "crypto/obsolete/sha1.h"
 #include "ui/display/screen.h"
 #include "url/gurl.h"
 
@@ -74,6 +73,12 @@ using file_manager::VolumeManager;
 using session_manager::SessionManager;
 using wallpaper_handlers::BackdropSurpriseMeImageFetcher;
 
+namespace wallpaper {
+std::string GetHexForWallpaperFilesId(
+    const base::span<const uint8_t> files_id_unhashed) {
+  return base::HexEncodeLower(crypto::obsolete::Sha1::Hash(files_id_unhashed));
+}
+}  // namespace wallpaper
 namespace {
 
 // Known user keys.
@@ -125,7 +130,7 @@ std::string HashWallpaperFilesIdStr(std::string_view files_id_unhashed) {
   // presumably meant to lowercase the input string before hashing, but it did
   // not.
   base::Extend(data, base::as_byte_span(files_id_unhashed));
-  return base::HexEncodeLower(base::SHA1Hash(data));
+  return wallpaper::GetHexForWallpaperFilesId(data);
 }
 
 // Returns true if wallpaper files id can be returned successfully.
