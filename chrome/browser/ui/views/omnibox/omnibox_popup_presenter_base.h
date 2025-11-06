7df5d8f69be9ad1d086f18730253eeaf242c5a5b
Darryl James <dljames@chromium.org>
2025-11-06 12:00:21 -0800

    Revert "[omnibox] 5/5 Add interactive testing for Gemini verbatim/null matches."
    This reverts commit 23751922a3707a7f426a21dd138d59ad3dc91fdc.
    
    Reason for revert: Tree closure on Linux Chromium OS MSAN builder see crbug.com/458415911
    
    Original change's description:
    > [omnibox] 5/5 Add interactive testing for Gemini verbatim/null matches.
    >
    > Ensures for webui omnibox popup that:
    > * popup resurfaces if it is hidden
    > * no matches show when there's a query in Gemini mode.
    > * null matches are hidden and default suggestions still
    > navigate correctly.
    >
    > Change-Id: Ib0aa88be5e014ce53a555378a3cb373e00f066a1
    > Bug: b:452668962, b:454995208
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7103260
    > Reviewed-by: Dana Fried <dfried@chromium.org>
    > Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1541350}
    
    Bug: b:452668962, b:454995208
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I417dc7804582d4f8c2391044eec292c25862013b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7128591
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541355}

--- chrome/browser/ui/views/omnibox/omnibox_popup_presenter_base.h
+++ chrome/browser/ui/views/omnibox/omnibox_popup_presenter_base.h
@@ -29,7 +29,6 @@ class RoundedOmniboxResultsFrame;
 // work with OmniboxPopupViewWebUI directly.
 class OmniboxPopupPresenterBase {
  public:
-  DECLARE_CLASS_ELEMENT_IDENTIFIER_VALUE(kRoundedResultsFrame);
   explicit OmniboxPopupPresenterBase(LocationBarView* location_bar_view);
   OmniboxPopupPresenterBase(const OmniboxPopupPresenterBase&) = delete;
   OmniboxPopupPresenterBase& operator=(const OmniboxPopupPresenterBase&) =
@@ -75,7 +74,6 @@ class OmniboxPopupPresenterBase {
 
  private:
   friend class OmniboxPopupViewWebUITest;
-  friend class OmniboxWebUiInteractiveTest;
 
   void OnWidgetClosed(views::Widget::ClosedReason closed_reason);
 
