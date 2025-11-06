493523f39fd0c276fbe33a6c23bd098708b56f23
Osama Fathy <osamafathy@google.com>
2025-11-05 19:21:00 -0800

    Reland "OOBE: Enable kOobeAutoEnrollmentCheckForced by default and add tests"
    This is a reland of commit f6ee81dd8f7b16101ade4f11e5cb8b5b5ece6263
    
    Original change's description:
    > OOBE: Enable kOobeAutoEnrollmentCheckForced by default and add tests
    >
    > Enable the `kOobeAutoEnrollmentCheckForced` feature by default, ensuring
    > that the Auto Enrollment check must be completed before OOBE is marked
    > as complete. This prevents potential bypasses of the Auto Enrollment
    > check.
    >
    > Key changes include:
    > *   Flipping `kOobeAutoEnrollmentCheckForced` to enabled by default.
    > *   Introducing a test-only flag `skip_auto_enrollment_check_for_tests`
    >     in `WizardContext` to allow bypassing the check in specific tests.
    > *   Updating existing browser tests to explicitly mark OOBE as complete
    >     before proceeding to screens like GAIA, which now require it.
    > *   Adding new browser tests to verify:
    >     *   `SignInFatalErrorScreen` is shown if Guest or User login is
    >         attempted before OOBE completion when the check is forced.
    >     *   OOBE completion status is set correctly after the Update screen.
    > *   Moving the clearing of the `kAutoEnrollmentCheckExited` preference
    >     to `PerformOOBECompletedActions`.
    >
    > Bug: 439252187
    > Change-Id: Idb32c0fa56f3b9b8ec5632ae573220c0e2ecb532
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7020409
    > Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    > Commit-Queue: Osama Fathy <osamafathy@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1540225}
    
    Bug: 439252187
    Change-Id: I393616d9f29b6607290afcb41db06664bfd635d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7123981
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Commit-Queue: Osama Fathy <osamafathy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1540999}

--- chrome/browser/ash/login/oobe_metrics_browsertest.cc
+++ chrome/browser/ash/login/oobe_metrics_browsertest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/metrics/histogram_tester.h"
 #include "chrome/browser/ash/login/login_pref_names.h"
 #include "chrome/browser/ash/login/screens/consolidated_consent_screen.h"
+#include "chrome/browser/ash/login/startup_utils.h"
 #include "chrome/browser/ash/login/test/cryptohome_mixin.h"
 #include "chrome/browser/ash/login/test/js_checker.h"
 #include "chrome/browser/ash/login/test/login_manager_mixin.h"
@@ -272,13 +273,20 @@ class FirstUserOobeMetricsTest : public OobeMetricsTest {
 
 IN_PROC_BROWSER_TEST_F(FirstUserOobeMetricsTest,
                        OobeBoundaryMilestonesMetrics) {
-  login_manager_mixin_.LoginAsNewRegularUser();
-  OobeScreenExitWaiter(GetFirstSigninScreen()).Wait();
+  LoginDisplayHost::default_host()->StartWizard(GetFirstSigninScreen());
+  OobeScreenWaiter(GetFirstSigninScreen()).Wait();
 
   cros_events::OOBE_OobeStarted oobe_started_event;
   oobe_started_event.SetIsFlexFlow(false).SetChromeMilestone(
       version_info::GetMajorVersionNumberAsInt());
   ValidateEventRecorded(oobe_started_event);
+
+  if (ash::features::IsOobeAutoEnrollmentCheckForcedEnabled()) {
+    // Showing the GAIA screen requires OOBE to be marked complete.
+    StartupUtils::MarkOobeCompleted();
+  }
+  login_manager_mixin_.LoginAsNewRegularUser();
+  OobeScreenExitWaiter(GetFirstSigninScreen()).Wait();
 
   cros_events::OOBE_PreLoginOobeCompleted prelogin_oobe_completed_event;
   prelogin_oobe_completed_event
@@ -334,6 +342,13 @@ IN_PROC_BROWSER_TEST_F(FirstUserOobeMetricsTest,
 // So, the following tests should only run on branded builds.
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
 IN_PROC_BROWSER_TEST_F(FirstUserOobeMetricsTest, ClientIdNotReset) {
+  LoginDisplayHost::default_host()->StartWizard(GetFirstSigninScreen());
+  OobeScreenWaiter(GetFirstSigninScreen()).Wait();
+  if (ash::features::IsOobeAutoEnrollmentCheckForcedEnabled()) {
+    // Showing the GAIA screen requires OOBE to be marked complete.
+    StartupUtils::MarkOobeCompleted();
+  }
+
   login_manager_mixin_.LoginAsNewRegularUser();
   OobeScreenExitWaiter(GetFirstSigninScreen()).Wait();
 
@@ -356,6 +371,12 @@ IN_PROC_BROWSER_TEST_F(FirstUserOobeMetricsTest, ClientIdNotReset) {
 }
 
 IN_PROC_BROWSER_TEST_F(FirstUserOobeMetricsTest, ClientIdReset) {
+  LoginDisplayHost::default_host()->StartWizard(GetFirstSigninScreen());
+  OobeScreenWaiter(GetFirstSigninScreen()).Wait();
+  if (ash::features::IsOobeAutoEnrollmentCheckForcedEnabled()) {
+    StartupUtils::MarkOobeCompleted();
+  }
+
   login_manager_mixin_.LoginAsNewRegularUser();
   OobeScreenExitWaiter(GetFirstSigninScreen()).Wait();
 
