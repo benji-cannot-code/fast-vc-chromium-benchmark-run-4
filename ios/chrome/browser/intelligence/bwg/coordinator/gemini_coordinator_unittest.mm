050461fc299fa9cdb164596209b5f330132ba1b6
Tarek Makkouk <tarekmakkouk@google.com>
2025-11-06 08:15:31 -0800

    [iOS] Update Helios promo conditions
    Leverage the Ask Gemini eligibility signal to conditionally show the
    fullscreen promo while suppressing the chip and other conflicting
    promos.
    
    Bug: 453978851, 453978794
    Change-Id: Id555bcc7bb24d3e94af2abf80b82d4a6a5e3eb39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7083660
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Commit-Queue: Tarek Makkouk <tarekmakkouk@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1541242}

--- ios/chrome/browser/intelligence/bwg/coordinator/gemini_coordinator_unittest.mm
+++ ios/chrome/browser/intelligence/bwg/coordinator/gemini_coordinator_unittest.mm
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/fullscreen/ui_bundled/test/test_fullscreen_controller.h"
 #import "ios/chrome/browser/intelligence/bwg/coordinator/bwg_coordinator.h"
 #import "ios/chrome/browser/intelligence/bwg/utils/bwg_constants.h"
+#import "ios/chrome/browser/intelligence/features/features.h"
 #import "ios/chrome/browser/shared/model/browser/browser_list.h"
 #import "ios/chrome/browser/shared/model/browser/browser_list_factory.h"
 #import "ios/chrome/browser/shared/model/browser/test/test_browser.h"
@@ -127,6 +128,7 @@ TEST_F(GeminiCoordinatorTest, FullscreenNotExitedOnAIHubEntryPoint) {
 
 // Tests fullscreen mode exiting when promo shows from the promo entry point.
 TEST_F(GeminiCoordinatorTest, FullscreenExitedOnPromoEntryPoint) {
+  feature_list_.InitWithFeatures({kGeminiNavigationPromo, kAskGeminiChip}, {});
   auto* tracker = static_cast<feature_engagement::test::MockTracker*>(
       feature_engagement::TrackerFactory::GetForProfile(
           profile_manager_.GetProfileWithName(kFirstProfileName)));
@@ -142,6 +144,8 @@ TEST_F(GeminiCoordinatorTest, FullscreenExitedOnPromoEntryPoint) {
   EXPECT_CALL(
       *tracker,
       NotifyEvent(feature_engagement::events::kIOSGeminiPromoFirstCompletion));
+
+  EXPECT_CALL(*tracker, NotifyEvent("fullscreen_promos_group_trigger"));
 
   StartCoordinatorWithEntryPoint(bwg::EntryPoint::Promo);
 
