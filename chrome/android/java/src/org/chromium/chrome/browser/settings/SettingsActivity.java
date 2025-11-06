223d9c623d30a3c760ae2ee03c1672c63b345712
Gazal <agazal@google.com>
2025-11-06 13:20:38 -0800

    Reapply "[Settings containment] Hide divider in multi-column contained view"
    This reverts commit 384da37b1b17dfe32c442ae6f460afa60600c834.
    
    In the original CL, WideDisplayPadding wasn't being applied to
    PrivacyGuideFragment because it does not inherit
    PreferenceFragmentCompat. To fix the test failures
    (https://ci.chromium.org/ui/p/chromium/builders/ci/android-14-tablet-landscape-arm64-rel/6863/test-results),
    we've now applying WideDisplayPadding in addition when
    `MAIN_FRAGMENT_TAG.equals(fragment.getTag())`
    
    Bug: 433576895, 454180240
    Change-Id: Ie0a21467cf8ad2ce37579f766c75e39cdf3a89be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7126933
    Auto-Submit: Gazal Agarwal <agazal@google.com>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541410}

--- chrome/android/java/src/org/chromium/chrome/browser/settings/SettingsActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/SettingsActivity.java
@@ -195,9 +195,9 @@ public class SettingsActivity extends ChromeBaseAppCompatActivity
                         getModalDialogManagerSupplier()),
                 /* recursive= */ true);
         fragmentManager.registerFragmentLifecycleCallbacks(
-                new WideDisplayPaddingApplier(), /* recursive= */ false);
+                new WideDisplayPaddingApplier(), /* recursive= */ true);
         fragmentManager.registerFragmentLifecycleCallbacks(
-                new SettingsMetricsReporter(), /* recursive= */ false);
+                new SettingsMetricsReporter(), /* recursive= */ true);
 
         if (isContainmentEnabled()) {
             // In multi-column mode, the main settings fragment is a child of the
@@ -436,8 +436,11 @@ public class SettingsActivity extends ChromeBaseAppCompatActivity
      * MainSettings when in multi-column, two-pane mode.
      */
     private boolean shouldSkipContainmentForMainSettings(PreferenceFragmentCompat fragment) {
-        return fragment instanceof MainSettings
-                && getUseMultiColumn()
+        return fragment instanceof MainSettings && isMultiColumnSettingsVisible();
+    }
+
+    public boolean isMultiColumnSettingsVisible() {
+        return getUseMultiColumn()
                 && mMultiColumnSettings != null
                 && mMultiColumnSettings.isTwoPane();
     }
@@ -980,10 +983,8 @@ public class SettingsActivity extends ChromeBaseAppCompatActivity
                 Fragment fragment,
                 View view,
                 @Nullable Bundle savedInstanceState) {
-            if (MAIN_FRAGMENT_TAG.equals(fragment.getTag())) {
-                // Apply the wide display style after the main fragment is committed since its views
-                // (particularly a recycler view) are not accessible before the transaction
-                // completes.
+            if (fragment instanceof PreferenceFragmentCompat
+                    || MAIN_FRAGMENT_TAG.equals(fragment.getTag())) {
                 WideDisplayPadding.apply(fragment, SettingsActivity.this);
             }
         }
