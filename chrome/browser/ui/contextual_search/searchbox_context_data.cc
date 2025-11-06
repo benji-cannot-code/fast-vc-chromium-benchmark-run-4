c586ac979fbea706873a108bdcfb287c6acf21a9
Aviv Kiss <avivkiss@google.com>
2025-11-06 14:41:01 -0800

    [aim][omnibox] Introduce SearchboxContextData class for aim context.
    This will enable sharing aim context between webui/views.
    
    NO_IFTTT=IFTTT added in this change.
    
    Bug: b:457860153
    Change-Id: I62f1aeedbaf755226313264fa68620b4213b59fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7122043
    Auto-Submit: Aviv Kiss <avivkiss@google.com>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Aviv Kiss <avivkiss@google.com>
    Cr-Commit-Position: refs/heads/main@{#1541448}

---
+++ chrome/browser/ui/contextual_search/searchbox_context_data.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/contextual_search/searchbox_context_data.h"
+
+SearchboxContextData::Context::Context() = default;
+SearchboxContextData::Context::~Context() = default;
+
+SearchboxContextData::SearchboxContextData() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+}
+SearchboxContextData::~SearchboxContextData() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+}
+
+void SearchboxContextData::SetPendingContext(std::unique_ptr<Context> context) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  pending_context_ = std::move(context);
+}
+
+std::unique_ptr<SearchboxContextData::Context>
+SearchboxContextData::TakePendingContext() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  return std::move(pending_context_);
+}
