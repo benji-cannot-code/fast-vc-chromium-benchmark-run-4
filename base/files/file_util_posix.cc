57b25c8c2443f3119e40bbc76a4f8a26b61d5a37
Joshua Pawlicki <waffles@chromium.org>
2025-11-06 10:51:56 -0800

    base: Remove leading '.' from temp dirs on POSIX.
    Perhaps there's no need to try to hide temporary directories.
    
    Fixed: 452882316
    Change-Id: Ie3d01d9641d62434c44e643ee332bba332b7f5ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7069157
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541321}

--- base/files/file_util_posix.cc
+++ base/files/file_util_posix.cc
@@ -128,9 +128,9 @@ bool VerifySpecificPathControlledByUser(const FilePath& path,
 }
 #endif
 
-base::FilePath GetTempTemplate(FilePath::StringViewType prefix = "") {
+base::FilePath GetTempTemplate(FilePath::StringViewType prefix, bool hidden) {
   return FormatTemporaryFileName(
-      StrCat({prefix, prefix.empty() ? "" : ".", "XXXXXX"}));
+      StrCat({prefix, prefix.empty() ? "" : ".", "XXXXXX"}), hidden);
 }
 
 bool AdvanceEnumeratorWithStat(FileEnumerator* traversal,
@@ -684,7 +684,7 @@ ScopedFD CreateAndOpenFdForTemporaryFileInDir(const FilePath& directory,
   ScopedBlockingCall scoped_blocking_call(
       FROM_HERE,
       BlockingType::MAY_BLOCK);  // For call to mkstemp().
-  *path = directory.Append(GetTempTemplate());
+  *path = directory.Append(GetTempTemplate("", true));
   const std::string& tmpdir_string = path->value();
   // this should be OK since mkstemp just replaces characters in place
   char* buffer = const_cast<char*>(tmpdir_string.c_str());
@@ -873,7 +873,8 @@ bool CreateTemporaryFileInDir(const FilePath& dir, FilePath* temp_file) {
   return fd.is_valid();
 }
 
-FilePath FormatTemporaryFileName(FilePath::StringViewType identifier) {
+FilePath FormatTemporaryFileName(FilePath::StringViewType identifier,
+                                 bool hidden) {
 #if BUILDFLAG(IS_APPLE)
   std::string_view prefix = base::apple::BaseBundleID();
 #elif BUILDFLAG(GOOGLE_CHROME_BRANDING)
@@ -881,7 +882,7 @@ FilePath FormatTemporaryFileName(FilePath::StringViewType identifier) {
 #else
   std::string_view prefix = "org.chromium.Chromium";
 #endif
-  return FilePath(StrCat({".", prefix, ".", identifier}));
+  return FilePath(StrCat({hidden ? "." : "", prefix, ".", identifier}));
 }
 
 ScopedFILE CreateAndOpenTemporaryStreamInDir(const FilePath& dir,
@@ -937,7 +938,7 @@ bool CreateNewTempDirectory(FilePath::StringViewType prefix,
     return false;
   }
 
-  return CreateTemporaryDirInDirImpl(tmpdir, GetTempTemplate(prefix),
+  return CreateTemporaryDirInDirImpl(tmpdir, GetTempTemplate(prefix, false),
                                      new_temp_path);
 }
 
