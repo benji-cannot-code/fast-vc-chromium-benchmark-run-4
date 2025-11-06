3e5e3aeec0173be188dfa02d533c2563d24ca196
Lei Zhang <thestig@chromium.org>
2025-11-05 16:07:16 -0800

    Modernize PluginService::GetPluginInfoByPath()
    Change GetPluginInfoByPath() to return std::optional<WebPluginInfo>
    instead of using an out-parameter. Also rename it to
    GetPluginInfoByPathForTesting() since there are no more production
    callers.
    
    Change-Id: I61247221e30046a9484f3b369035d91a719ff249
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7115406
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1540935}

--- content/public/browser/plugin_service.h
+++ content/public/browser/plugin_service.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_PUBLIC_BROWSER_PLUGIN_SERVICE_H_
 #define CONTENT_PUBLIC_BROWSER_PLUGIN_SERVICE_H_
 
+#include <optional>
 #include <string>
 #include <vector>
 
@@ -72,11 +73,10 @@ class CONTENT_EXPORT PluginService {
                              WebPluginInfo* info,
                              std::string* actual_mime_type) = 0;
 
-  // Get plugin info by plugin path (including disabled plugins). Returns true
-  // if the plugin is found and WebPluginInfo has been filled in |info|. This
-  // will use cached data in the plugin list.
-  virtual bool GetPluginInfoByPath(const base::FilePath& plugin_path,
-                                   WebPluginInfo* info) = 0;
+  // Gets plugin info by plugin path (including disabled plugins). This will use
+  // cached data in the plugin list.
+  virtual std::optional<WebPluginInfo> GetPluginInfoByPathForTesting(
+      const base::FilePath& plugin_path) = 0;
 
   // Asynchronously loads plugins if necessary and then calls back to the
   // provided function on the calling sequence on completion.
