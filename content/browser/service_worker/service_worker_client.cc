b3e90b1d5cefaee1f02e1a24ede7c71ad9f7f4d6
Yoshisato Yanagisawa <yyanagisawa@chromium.org>
2025-11-05 19:16:44 -0800

    Fix crash in ServiceWorker BFCache eviction logic
    This patch fixes a crash that occurs when a ServiceWorker client in the
    BackForwardCache is evicted.
    
    The crash happens due to an information inconsistency. A
    ServiceWorkerClient could enter the BackForwardCache without a
    controller, but later an attempt to evict it (e.g., due to a
    postMessage) would cause a crash.
    
    The eviction logic in `EvictBackForwardCachedControllee` would call
    `RemoveControlleeFromBackForwardCacheMap`, which CHECK-fails if the
    client is not in the `bfcached_controllee_map_`. A client that entered
    BFCache without a controller would not be in this map.
    
    This patch fixes this by centralizing the safety check. It introduces a
    new flag in `ServiceWorkerClient`:
    `was_controlled_when_entered_back_forward_cache_`. This flag explicitly
    tracks whether the client had a controller when it entered BFCache.
    
    The check is now performed inside `EvictBackForwardCachedControllee`
    before calling `RemoveControlleeFromBackForwardCacheMap`. This ensures
    that any code path that triggers an eviction is safe. This also allows
    removing a previous workaround for this issue.
    
    Bug: 341322515
    Change-Id: I4d03d09cea8b4db86d3d4bbebcb61be69caf22c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7123459
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1540998}

--- content/browser/service_worker/service_worker_client.cc
+++ content/browser/service_worker/service_worker_client.cc
@@ -912,6 +912,7 @@ void ServiceWorkerClient::OnEnterBackForwardCache() {
     SCOPED_CRASH_KEY_BOOL("SWC_OnEBFC", "is_inherited", is_inherited());
     CHECK(!controller_->BFCacheContainsControllee(client_uuid()));
     controller_->MoveControlleeToBackForwardCacheMap(client_uuid());
+    was_controlled_when_entered_back_forward_cache_ = true;
   }
   is_in_back_forward_cache_ = true;
 }
@@ -929,6 +930,7 @@ void ServiceWorkerClient::OnRestoreFromBackForwardCache() {
     controller_->RestoreControlleeFromBackForwardCacheMap(client_uuid());
   }
   is_in_back_forward_cache_ = false;
+  was_controlled_when_entered_back_forward_cache_ = false;
 }
 
 void ServiceWorkerClient::SyncMatchingRegistrations() {
@@ -1059,6 +1061,7 @@ void ServiceWorkerClient::UpdateController(bool notify_controllerchange) {
 
   scoped_refptr<ServiceWorkerVersion> previous_version = controller_;
   controller_ = version;
+  was_controlled_when_entered_back_forward_cache_ = false;
   if (version) {
     // TODO(crbug.com/330928087): remove check when this issue resolved.
     SCOPED_CRASH_KEY_NUMBER("SWV_RCFBCM", "client_type",
@@ -1074,6 +1077,7 @@ void ServiceWorkerClient::UpdateController(bool notify_controllerchange) {
       // |this| was not |version|'s controllee when |OnEnterBackForwardCache|
       // was called.
       version->MoveControlleeToBackForwardCacheMap(client_uuid());
+      was_controlled_when_entered_back_forward_cache_ = true;
     }
     if (running_status_observer_) {
       version->AddObserver(running_status_observer_.get());
