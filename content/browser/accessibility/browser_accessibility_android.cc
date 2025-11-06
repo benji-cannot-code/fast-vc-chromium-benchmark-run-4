c6bca9253ab1ebf8f0b559d3f481991e514d90d6
Ramin Halavati <rhalavati@chromium.org>
2025-11-05 22:54:05 -0800

    [a11y] Mark kColumn, kRowGroup, and kTableHeaderContainer as not reached
    `kColumn`, `kRowGroup, and `kTableHeaderContainer` are not used in
    Android. Mark them as not reached in
    `BrowserAccessibilityAndroid::GetRoleDescription()`.
    
    AX-Relnotes: n/a
    Bug: 447360631
    Change-Id: Iff79371b0ecead8d4b00c0cc5d6c70f3baa982be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7124361
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Auto-Submit: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541054}

--- content/browser/accessibility/browser_accessibility_android.cc
+++ content/browser/accessibility/browser_accessibility_android.cc
@@ -1514,7 +1514,6 @@ std::u16string BrowserAccessibilityAndroid::GetRoleDescription() const {
     case ax::mojom::Role::kProgressIndicator:
     case ax::mojom::Role::kRadioButton:
     case ax::mojom::Role::kRowHeader:
-    case ax::mojom::Role::kRowGroup:
     case ax::mojom::Role::kSectionFooter:
     case ax::mojom::Role::kSectionHeader:
     case ax::mojom::Role::kSectionWithoutName:
@@ -1533,10 +1532,13 @@ std::u16string BrowserAccessibilityAndroid::GetRoleDescription() const {
       break;
 
     // Roles not used on Android.
+    case ax::mojom::Role::kColumn:
     case ax::mojom::Role::kListGrid:
     case ax::mojom::Role::kMenuItemSeparator:
     case ax::mojom::Role::kPdfActionableHighlight:
     case ax::mojom::Role::kPdfRoot:
+    case ax::mojom::Role::kRowGroup:
+    case ax::mojom::Role::kTableHeaderContainer:
     case ax::mojom::Role::kWebView:
       NOTREACHED();
 
