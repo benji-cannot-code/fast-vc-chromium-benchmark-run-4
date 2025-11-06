aa88527371eeb1c0e454a1dcc06739fcd4c1de79
Omar Flores <omarflores@google.com>
2025-11-06 13:42:18 -0800

    [tree-view] Updating accessibility rules
    Bug: 448177504
    Change-Id: Ifa4e2e63c026c4b82cd74cd0830b6206844de33f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7124660
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Omar Flores <omarflores@google.com>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541421}

--- chrome/browser/resources/side_panel/bookmarks/power_bookmark_row.html.ts
+++ chrome/browser/resources/side_panel/bookmarks/power_bookmark_row.html.ts
@@ -11,7 +11,8 @@ export function getHtml(this: PowerBookmarkRowElement) {
   // clang-format off
   const urlListItem = html`
 <cr-url-list-item id="crUrlListItem"
-    role="listitem"
+    role="treeitem"
+    aria-level="${this.depth + 1}"
     .size="${this.listItemSize}"
     .url="${this.getUrl_()}"
     ?selected="${this.isSelected}"
@@ -77,6 +78,7 @@ export function getHtml(this: PowerBookmarkRowElement) {
     return html`<!--_html_template_start_-->
 <cr-expand-button no-hover id="expandButton"
     .expanded="${this.toggleExpand}"
+    aria-expanded="${this.toggleExpand}"
     tab-index="-1"
     collapse-icon="cr:expand-more"
     ?selected="${this.isSelected}"
