86ddc48792eed47c294743ebde9179ea4b3279c5
Fiona Macintosh <fmacintosh@google.com>
2025-11-07 07:06:34 -0800

    [ACT] Clean up Clank page info ACT logic
    Bug: b:452905381
    Change-Id: Iec029f88ddc445f58daf9fd6ebb6c66d936c1235
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7118425
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Fiona Macintosh <fmacintosh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1541783}

--- components/content_settings/android/java/src/org/chromium/components/content_settings/CookieControlsBridge.java
+++ components/content_settings/android/java/src/org/chromium/components/content_settings/CookieControlsBridge.java
@@ -71,13 +71,6 @@ public class CookieControlsBridge {
         }
     }
 
-    public void onTrackingProtectionsChangedForSite() {
-        if (mNativeCookieControlsBridge != 0) {
-            CookieControlsBridgeJni.get()
-                    .onTrackingProtectionsChangedForSite(mNativeCookieControlsBridge);
-        }
-    }
-
     public void onUiClosing() {
         if (mNativeCookieControlsBridge != 0) {
             CookieControlsBridgeJni.get().onUiClosing(mNativeCookieControlsBridge);
@@ -139,8 +132,6 @@ public class CookieControlsBridge {
 
         void setThirdPartyCookieBlockingEnabledForSite(
                 long nativeCookieControlsBridge, boolean blockCookies);
-
-        void onTrackingProtectionsChangedForSite(long nativeCookieControlsBridge);
 
         void onUiClosing(long nativeCookieControlsBridge);
 
