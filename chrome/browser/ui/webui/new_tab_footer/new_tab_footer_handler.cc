bcebff44d73bdae8ca2a1db2dd184c3e936dea16
Paul Adedeji <pauladedeji@google.com>
2025-11-07 12:10:39 -0800

    [ntp footer] Hide customize chrome button when it is not supported.
    Makes it so the footer hides the button whenever the tab it is on
    updates, if the tab it is in doesn't support customize chrome.
    
    This is the same way the app menu model hides customize chrome whenever
    the tab is incognito or guest[1].
    
    http://screencast/cast/NTQ5NDI4MTk5MjE0Mjg0OHwyMzQ1NTIwZS0yMg
    
    [1]https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ui/toolbar/app_menu_model.cc;drc=21901e419ea81226d1bfd4563f30f7f2fd63767e;l=983
    
    Change-Id: Id47df8d5ed8e8a853b3104c67ebf3cfccf59ab90
    Bug: b:455164566
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7127361
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Auto-Submit: Paul Adedeji <pauladedeji@google.com>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541957}

--- chrome/browser/ui/webui/new_tab_footer/new_tab_footer_handler.cc
+++ chrome/browser/ui/webui/new_tab_footer/new_tab_footer_handler.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/new_tab_footer/new_tab_footer.mojom.h"
 #include "chrome/browser/ui/webui/new_tab_footer/new_tab_footer_helper.h"
 #include "chrome/browser/ui/webui/new_tab_page/new_tab_page_ui.h"
+#include "chrome/browser/ui/webui/side_panel/customize_chrome/customize_chrome_page_handler.h"
 #include "chrome/browser/ui/webui/webui_embedding_context.h"
 #include "chrome/browser/ui/webui/webui_util_desktop.h"
 #include "chrome/common/pref_names.h"
@@ -236,7 +237,11 @@ void NewTabFooterHandler::AttachedTabStateUpdated(const GURL& url) {
   } else if (ntp_footer::IsExtensionNtp(url, profile_)) {
     ntp_type = new_tab_footer::mojom::NewTabPageType::kExtension;
   }
-  document_->AttachedTabStateUpdated(ntp_type);
+
+  bool can_customize_chrome = CustomizeChromePageHandler::IsSupported(
+      NtpCustomBackgroundServiceFactory::GetForProfile(profile_), profile_);
+
+  document_->AttachedTabStateUpdated(ntp_type, can_customize_chrome);
 }
 
 void NewTabFooterHandler::OnCustomBackgroundImageUpdated() {
