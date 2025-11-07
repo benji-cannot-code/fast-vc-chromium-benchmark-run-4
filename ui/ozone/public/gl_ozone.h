de3074e0c6502dc20c105c14389a815e134700b2
Colin Blundell <blundell@chromium.org>
2025-11-06 23:52:02 -0800

    [//ui] Make GLOzone::ImportNativePixmap(BufferFormat) private
    All external callers have now been converted to
    ImportNativePixmap(SIFormat); the only remaining caller is in the
    wrapper GLOzoneEGL::ImportNativePixmap(SIFormat) itself.
    
    Make this method private in the interface and all implementations of it
    to allow us to incrementally convert implementations to instead implement
    ImportNativePixmap(SIFormat) natively while being sure that eliminating
    their native implementations of ImportNativePixmap(BufferFormat) will
    not result in any problems.
    
    Bug: 450325664
    Change-Id: Ie9c754e9a211f12d111ceb4cb8ffc09fa46283d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7123704
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541630}

--- ui/ozone/public/gl_ozone.h
+++ ui/ozone/public/gl_ozone.h
@@ -78,14 +78,6 @@ class COMPONENT_EXPORT(OZONE_BASE) GLOzone {
   // live until glDeleteTextures fn is called on all platforms.
   virtual std::unique_ptr<NativePixmapGLBinding> ImportNativePixmap(
       scoped_refptr<gfx::NativePixmap> pixmap,
-      gfx::BufferFormat plane_format,
-      gfx::BufferPlane plane,
-      gfx::Size plane_size,
-      const gfx::ColorSpace& color_space,
-      GLenum target,
-      GLuint texture_id) = 0;
-  virtual std::unique_ptr<NativePixmapGLBinding> ImportNativePixmap(
-      scoped_refptr<gfx::NativePixmap> pixmap,
       viz::SharedImageFormat plane_format,
       gfx::BufferPlane plane,
       gfx::Size plane_size,
@@ -125,6 +117,16 @@ class COMPONENT_EXPORT(OZONE_BASE) GLOzone {
   virtual scoped_refptr<gl::GLSurface> CreateOffscreenGLSurface(
       gl::GLDisplay* display,
       const gfx::Size& size) = 0;
+
+ private:
+  virtual std::unique_ptr<NativePixmapGLBinding> ImportNativePixmap(
+      scoped_refptr<gfx::NativePixmap> pixmap,
+      gfx::BufferFormat plane_format,
+      gfx::BufferPlane plane,
+      gfx::Size plane_size,
+      const gfx::ColorSpace& color_space,
+      GLenum target,
+      GLuint texture_id) = 0;
 };
 
 }  // namespace ui
