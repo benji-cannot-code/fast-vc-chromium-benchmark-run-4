b6a50baa479537f52ed93bdd812fb05073f92e9b
Kristi Saney <kristislee@google.com>
2025-11-07 12:18:59 -0800

    [Reading mode] Only show the omnibox chip for the active tab
    Bug: 455640523
    Change-Id: I5f223e5aaacb9ea8942386250dc93741edacc8f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7129145
    Reviewed-by: Eitan Goldberger <eitang@google.com>
    Auto-Submit: Kristi Saney <kristislee@google.com>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1541962}

--- chrome/browser/ui/read_anything/read_anything_side_panel_controller.cc
+++ chrome/browser/ui/read_anything/read_anything_side_panel_controller.cc
@@ -244,6 +244,7 @@ bool ReadAnythingSidePanelController::IsActivePageDistillable() const {
 
 void ReadAnythingSidePanelController::TabForegrounded(tabs::TabInterface* tab) {
   UpdateIphVisibility();
+  CheckIfGoodCandidateForReadingMode();
 }
 
 void ReadAnythingSidePanelController::TabWillDetach(
@@ -282,8 +283,11 @@ void ReadAnythingSidePanelController::DidStopLoading() {
   // The page finished loading.
   loading_ = false;
   UpdateIphVisibility();
+  CheckIfGoodCandidateForReadingMode();
+}
 
-  if (!features::IsReadAnythingOmniboxChipEnabled()) {
+void ReadAnythingSidePanelController::CheckIfGoodCandidateForReadingMode() {
+  if (!features::IsReadAnythingOmniboxChipEnabled() || !tab_->IsActivated()) {
     return;
   }
 
