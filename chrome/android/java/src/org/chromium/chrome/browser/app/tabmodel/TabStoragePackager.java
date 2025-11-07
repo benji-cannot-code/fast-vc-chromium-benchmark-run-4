c51bc83f233679e628c61b7c6833cdee9b212432
Calder Kitagawa <ckitagawa@chromium.org>
2025-11-07 12:02:38 -0800

    [Persist] Clear cache on tab model destruction
    The packager does not currently listen for tab model destruction so the map remains populated indefinitely. In practice each TabStripCollection pointer is probably unique so a collision would be rare. However, it is better to observe the lifecycle and respond accordingly.
    
    Bug: 458679374
    Change-Id: Icf4e24e5d89fb1873ea60cb4ebab7f81a0f557ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7131958
    Reviewed-by: Sky Malice <skym@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541947}

--- chrome/android/java/src/org/chromium/chrome/browser/app/tabmodel/TabStoragePackager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/tabmodel/TabStoragePackager.java
@@ -21,6 +21,7 @@ import org.chromium.chrome.browser.tab.TabAssociatedApp;
 import org.chromium.chrome.browser.tab.TabStateExtractor;
 import org.chromium.chrome.browser.tab.WebContentsState;
 import org.chromium.chrome.browser.tabmodel.TabModel;
+import org.chromium.chrome.browser.tabmodel.TabModelObserver;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.chrome.browser.tabwindow.TabWindowManager;
 import org.chromium.chrome.browser.tabwindow.WindowId;
@@ -137,6 +138,8 @@ public class TabStoragePackager {
         }
         assert tabModel != null && selector != null;
 
+        configureRemoveFromCacheOnDestroy(tabModel, collection);
+
         @WindowId
         int windowId = TabWindowManagerSingleton.getInstance().getWindowIdForSelector(selector);
         assert windowId != TabWindowManager.INVALID_WINDOW_ID;
@@ -157,6 +160,8 @@ public class TabStoragePackager {
         TabStripCollection archivedCollection = tabModel.getTabStripCollection();
         if (!Objects.equals(archivedCollection, collection)) return null;
 
+        configureRemoveFromCacheOnDestroy(tabModel, collection);
+
         return TabModelInfo.createForArchivedModel();
     }
 
@@ -171,6 +176,18 @@ public class TabStoragePackager {
                         info.windowId,
                         info.tabModelType,
                         info.activeTabSupplier.get());
+    }
+
+    private void configureRemoveFromCacheOnDestroy(
+            TabModel tabModel, TabStripCollection collection) {
+        tabModel.addObserver(
+                new TabModelObserver() {
+                    @Override
+                    public void onDestroy() {
+                        mTabModelInfoMap.remove(collection);
+                        tabModel.removeObserver(this);
+                    }
+                });
     }
 
     @NativeMethods
