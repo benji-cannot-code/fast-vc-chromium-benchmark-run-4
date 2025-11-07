9bd5045d155873ecc88c4a6e20d3730a3e43a52b
Takashi Toyoshima <toyoshim@chromium.org>
2025-11-06 20:33:46 -0800

    DSEPrewarm: Disable in BrowserViewDataProtectionTest.DC_Screenshot
    This change adds `ScopedPrewarmFeatureList` to
    `BrowserViewDataProtectionTest` to prevent prewarming from interfering
    with tests, specifically addressing an issue where
    `browser()->GetWidget()` might return an unexpected widget related to
    prewarm pages.
    
    Bug: 458274323, 445883308
    Change-Id: I880c51a63b0f72463fd2cc02be35ab66e5908649
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7127518
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541595}

--- chrome/browser/ui/views/frame/browser_view_browsertest.cc
+++ chrome/browser/ui/views/frame/browser_view_browsertest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "chrome/browser/devtools/devtools_window_testing.h"
 #include "chrome/browser/policy/dm_token_utils.h"
+#include "chrome/browser/preloading/scoped_prewarm_feature_list.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/safe_browsing/chrome_enterprise_url_lookup_service_factory.h"
 #include "chrome/browser/ui/browser.h"
@@ -1046,7 +1047,9 @@ class FakeRealTimeUrlLookupService
 
 class BrowserViewDataProtectionTest : public InProcessBrowserTest {
  public:
-  BrowserViewDataProtectionTest() {
+  BrowserViewDataProtectionTest()
+      : scoped_prewarm_feature_list_(test::ScopedPrewarmFeatureList::
+                                         PrewarmState::kEnabledWithNoTrigger) {
     scoped_feature_list_.InitAndEnableFeature(features::kSideBySide);
   }
   BrowserViewDataProtectionTest(const BrowserViewDataProtectionTest&) = delete;
@@ -1096,6 +1099,12 @@ class BrowserViewDataProtectionTest : public InProcessBrowserTest {
 
  private:
   base::CallbackListSubscription create_services_subscription_;
+  // TODO(https://crbug.com/458274323): browser()->GetWidget() seems returning
+  // a wrong Widget, one for the prewarm page, unexpectedly, might be due to
+  // missing MPArch support?
+  // Investigate details, and fix it to remove this workaround so that
+  // DC_Screenshot test can pass stably.
+  test::ScopedPrewarmFeatureList scoped_prewarm_feature_list_;
   base::test::ScopedFeatureList scoped_feature_list_;
 };
 
