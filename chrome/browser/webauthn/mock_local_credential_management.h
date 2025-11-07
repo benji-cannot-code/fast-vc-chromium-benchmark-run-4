18e55f4728abe2da17452115528e8a1e19dae920
Darwin Yang <darwinyang@google.com>
2025-11-07 07:27:18 -0800

    [refactor] Refactored the webauthn classes into their own source sets
    Refactored some classes in `//chrome/browser/webauthn` to be in their
    own source sets instead of in `//chrome/browser/`. This is needed for a
    follow-up CL (https://crrev.com/c/7126930) where the payments code that
    needs to be included in `//chrome/browser/` needs to depend on webauthn
    classes that are also in `//chrome/browser/` causing a circular
    dependency.
    
    Splitting up huge build files is the recommended approach rather than
    adding more into `//chrome/browser`
    https://chat.google.com/room/AAAAqkI-BEQ/G4i_F69VFuY.
    
    The classes that were moved are:
    - `local_credential_management`
    - `chrome_web_authentication_delegate`
    - `unexportable_key_utils`
    - `webauthn_pref_names`
    
    I also created a `MockLocalCredentialManagement` class for tests.
    
    Bug: 441553248
    Change-Id: I1252df4ae05ac520602d7efaa76f2787426c887f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7097942
    Reviewed-by: Adem Derinel <derinel@google.com>
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Auto-Submit: Darwin Yang <darwinyang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541793}

---
+++ chrome/browser/webauthn/mock_local_credential_management.h
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_WEBAUTHN_MOCK_LOCAL_CREDENTIAL_MANAGEMENT_H_
+#define CHROME_BROWSER_WEBAUTHN_MOCK_LOCAL_CREDENTIAL_MANAGEMENT_H_
+
+#include "chrome/browser/webauthn/local_credential_management.h"
+#include "device/fido/discoverable_credential_metadata.h"
+#include "testing/gmock/include/gmock/gmock.h"
+
+class MockLocalCredentialManagement : public LocalCredentialManagement {
+ public:
+  MockLocalCredentialManagement();
+
+  ~MockLocalCredentialManagement() override;
+
+  MOCK_METHOD(void,
+              HasCredentials,
+              (base::OnceCallback<void(bool)>),
+              (override));
+
+  MOCK_METHOD(
+      void,
+      Enumerate,
+      (base::OnceCallback<void(std::optional<std::vector<
+                                   device::DiscoverableCredentialMetadata>>)>),
+      (override));
+
+  MOCK_METHOD(void,
+              Delete,
+              (base::span<const uint8_t>, base::OnceCallback<void(bool)>),
+              (override));
+
+  MOCK_METHOD(void,
+              Edit,
+              (base::span<uint8_t>,
+               std::string,
+               base::OnceCallback<void(bool)>),
+              (override));
+};
+
+#endif  // CHROME_BROWSER_WEBAUTHN_MOCK_LOCAL_CREDENTIAL_MANAGEMENT_H_
