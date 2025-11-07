a5730b70cc1726ed3054238bfd326f6b12a29c9a
Jan Keitel <jkeitel@google.com>
2025-11-07 13:59:27 -0800

    Avoid copies in bytes <-> base64 helpers in FIDO authenticator
    Both helper functions had unnecessary copies prior to this CL, since
    both received arguments by value, but only passed on views/spans to
    their callees.
    
    Bug: 40100455
    Change-Id: I31c55cb1efe1a1f1d19b18c5343dbabcafe3b32a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7130920
    Commit-Queue: Olivia Saul <osaul@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Reviewed-by: Olivia Saul <osaul@google.com>
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1542015}

--- components/autofill/core/browser/payments/credit_card_fido_authenticator.cc
+++ components/autofill/core/browser/payments/credit_card_fido_authenticator.cc
@@ -7,12 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <string>
+#include <string_view>
 #include <utility>
 #include <vector>
 
 #include "base/android/android_info.h"
 #include "base/base64.h"
 #include "base/containers/flat_set.h"
+#include "base/containers/span.h"
 #include "base/strings/string_util.h"
 #include "build/build_config.h"
 #include "components/autofill/core/browser/autofill_progress_dialog_type.h"
@@ -44,11 +46,11 @@ constexpr int kWebAuthnTimeoutMs = 3 * 60 * 1000;  // 3 minutes
 constexpr char kGooglePaymentsRpid[] = "google.com";
 constexpr char kGooglePaymentsRpName[] = "Google Payments";
 
-std::vector<uint8_t> Base64ToBytes(std::string base64) {
+std::vector<uint8_t> Base64ToBytes(std::string_view base64) {
   return base::Base64Decode(base64).value_or(std::vector<uint8_t>());
 }
 
-base::Value BytesToBase64(const std::vector<uint8_t> bytes) {
+base::Value BytesToBase64(base::span<const uint8_t> bytes) {
   return base::Value(base::Base64Encode(bytes));
 }
 }  // namespace
