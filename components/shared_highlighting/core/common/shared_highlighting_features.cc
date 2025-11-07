c50e6579406c34805a1ce549f33d710200bb39df
Sergii Bykov <sbykov@google.com>
2025-11-07 02:10:29 -0800

    CodeHealth: Clean up stale base::Feature SharedHighlightingManager
    Bug: 356623476
    Change-Id: I8560904359f9e2b1ddf4dd81e65f6f2fa2d1febe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7124845
    Commit-Queue: Sergii Bykov <sbykov@google.com>
    Reviewed-by: Cheick Cisse <cheickcisse@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541678}

--- components/shared_highlighting/core/common/shared_highlighting_features.cc
+++ components/shared_highlighting/core/common/shared_highlighting_features.cc
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace shared_highlighting {
 
-BASE_FEATURE(kSharedHighlightingManager, base::FEATURE_DISABLED_BY_DEFAULT);
-
 int GetPreemptiveLinkGenTimeoutLengthMs() {
 #if BUILDFLAG(IS_ANDROID)
   return 100;
