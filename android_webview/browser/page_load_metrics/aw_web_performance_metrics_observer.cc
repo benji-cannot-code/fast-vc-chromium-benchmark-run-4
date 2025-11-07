dd16c153597bef5afbf4ef0464a91c37379583c2
Joanne de Abreu <jdeabreu@google.com>
2025-11-07 01:37:36 -0800

    WebView Web Performance Metrics API: Add AwWebPerformanceMetricsObserver
    This adds a new WebView specific PageLoadMetricsObserver that will
    ultimately be used to expose key web performance metrics data via new
    WebView apis.
    
    Bug: b:432696062
    Change-Id: Iba56d97af3c049d0da38a43d312362638e3c382f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7122295
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Joanne de Abreu <jdeabreu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1541666}

---
+++ android_webview/browser/page_load_metrics/aw_web_performance_metrics_observer.cc
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "android_webview/browser/page_load_metrics/aw_web_performance_metrics_observer.h"
+
+namespace android_webview {
+
+const char* AwWebPerformanceMetricsObserver::GetObserverName() const {
+  static const char kName[] = "AwWebPerformanceMetricsObserver";
+  return kName;
+}
+
+page_load_metrics::PageLoadMetricsObserver::ObservePolicy
+AwWebPerformanceMetricsObserver::OnPrerenderStart(
+    content::NavigationHandle* navigation_handle,
+    const GURL& currently_committed_url) {
+  return CONTINUE_OBSERVING;
+}
+
+page_load_metrics::PageLoadMetricsObserver::ObservePolicy
+AwWebPerformanceMetricsObserver::OnFencedFramesStart(
+    content::NavigationHandle* navigation_handle,
+    const GURL& currently_committed_url) {
+  // Fenced frames are not supported in WebView
+  return STOP_OBSERVING;
+}
+
+}  // namespace android_webview
