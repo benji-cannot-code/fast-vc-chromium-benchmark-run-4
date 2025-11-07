a639ade05af3d31c43077d0ba5c49647b66e094b
Calder Kitagawa <ckitagawa@chromium.org>
2025-11-07 10:36:31 -0800

    [Persist] Change active tab to a supplier
    In the packager we should be using a supplier for the active tab since
    otherwise the cache data could be stale.
    
    Bug: 458679374, 458129124
    Change-Id: I7410880cfbf62307b1af62c05f59b9250a7d052b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7131938
    Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541904}

--- chrome/android/java/src/org/chromium/chrome/browser/app/tabmodel/TabStoragePackager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/tabmodel/TabStoragePackager.java
@@ -31,6 +31,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.function.Supplier;
 
 /** Saves Java-accessible data for use in C++. */
 @JNINamespace("tabs")
@@ -43,31 +44,35 @@ public class TabStoragePackager {
     private static class TabModelInfo {
         public final @WindowId int windowId;
         public final @TabModelType int tabModelType;
-        public final @Nullable Tab activeTab;
+        public final Supplier<@Nullable Tab> activeTabSupplier;
 
         /**
          * @param windowId The {@link WindowId} the {@link TabModel} is associated with.
          * @param tabModelType The type of tab model being saved.
-         * @param activeTab The active tab in the tab model.
+         * @param activeTabSupplier The supplier of the active tab in the tab model.
          */
         TabModelInfo(
-                @WindowId int windowId, @TabModelType int tabModelType, @Nullable Tab activeTab) {
+                @WindowId int windowId,
+                @TabModelType int tabModelType,
+                Supplier<@Nullable Tab> activeTabSupplier) {
             this.windowId = windowId;
             this.tabModelType = tabModelType;
-            this.activeTab = activeTab;
+            this.activeTabSupplier = activeTabSupplier;
         }
 
         /**
          * @param windowId The {@link WindowId} the {@link TabModel} is associated with.
          * @param isOffTheRecord Whether the tab model is off the record.
-         * @param activeTab The active tab in the tab model.
+         * @param activeTabSupplier The supplier of the active tab in the tab model.
          */
         public static TabModelInfo createForWindowScopedModel(
-                @WindowId int windowId, boolean isOffTheRecord, @Nullable Tab activeTab) {
+                @WindowId int windowId,
+                boolean isOffTheRecord,
+                Supplier<@Nullable Tab> activeTabSupplier) {
             return new TabModelInfo(
                     windowId,
                     isOffTheRecord ? TabModelType.INCOGNITO : TabModelType.REGULAR,
-                    activeTab);
+                    activeTabSupplier);
         }
 
         /**
@@ -78,7 +83,7 @@ public class TabStoragePackager {
             return new TabModelInfo(
                     TabWindowManager.INVALID_WINDOW_ID,
                     TabModelType.ARCHIVED,
-                    /* activeTab= */ null);
+                    /* activeTabSupplier= */ () -> null);
         }
     }
 
@@ -137,7 +142,7 @@ public class TabStoragePackager {
         assert windowId != TabWindowManager.INVALID_WINDOW_ID;
 
         return TabModelInfo.createForWindowScopedModel(
-                windowId, tabModel.isOffTheRecord(), tabModel.getCurrentTabSupplier().get());
+                windowId, tabModel.isOffTheRecord(), tabModel.getCurrentTabSupplier());
     }
 
     @Nullable
@@ -165,7 +170,7 @@ public class TabStoragePackager {
                         mNativeTabStoragePackager,
                         info.windowId,
                         info.tabModelType,
-                        info.activeTab);
+                        info.activeTabSupplier.get());
     }
 
     @NativeMethods
