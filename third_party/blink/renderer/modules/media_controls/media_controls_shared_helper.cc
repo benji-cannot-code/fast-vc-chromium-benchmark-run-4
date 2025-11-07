9ea3416b312e6558a5e1ad650a6e4518adb78ef5
Tommy Steimel <steimel@chromium.org>
2025-11-07 10:01:25 -0800

    [Media Controls] Use saturated_cast to safely format media time
    For the Blink media controls, we currently cast the length of the media
    from a double to an int. This is undefined behavior whenever the double
    falls outside of the range of an int. This CL changes the static_cast
    to a saturated_cast to avoid the undefined behavior.
    
    Bug: 41492424
    Change-Id: I279292c6db3600b71942fa0f13466b218ff26b2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7129698
    Auto-Submit: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541888}

--- third_party/blink/renderer/modules/media_controls/media_controls_shared_helper.cc
+++ third_party/blink/renderer/modules/media_controls/media_controls_shared_helper.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <cmath>
 
+#include "base/numerics/safe_conversions.h"
 #include "third_party/blink/public/mojom/use_counter/metrics/web_feature.mojom-blink.h"
 #include "third_party/blink/renderer/core/dom/events/event.h"
 #include "third_party/blink/renderer/core/fullscreen/fullscreen.h"
@@ -111,7 +112,7 @@ String MediaControlsSharedHelpers::FormatTime(double time) {
   if (!std::isfinite(time))
     time = 0;
 
-  int seconds = static_cast<int>(fabs(time));
+  int seconds = base::saturated_cast<int>(fabs(time));
   int minutes = seconds / 60;
   int hours = minutes / 60;
 
