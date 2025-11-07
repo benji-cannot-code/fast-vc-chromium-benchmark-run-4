e9c65c9749b1a1b7c0dae0c87f20b6fe4aa24015
Brandon Fong <bjfong@google.com>
2025-11-07 10:32:34 -0800

    [tips] Add a new tab launch type for tips notifications animations
    Add a new tab launch type for tips notifications when launching an NTP
    to remove the new NTP animation.
    
    Bug: 447428229
    Change-Id: I9967f2905031edb42a0e116f57474d53f96d64b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7127245
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1541903}

--- chrome/browser/tabpersistence/android/java/src/org/chromium/chrome/browser/tabpersistence/FlatBufferTabStateSerializer.java
+++ chrome/browser/tabpersistence/android/java/src/org/chromium/chrome/browser/tabpersistence/FlatBufferTabStateSerializer.java
@@ -238,6 +238,8 @@ public class FlatBufferTabStateSerializer implements TabStateSerializer {
                 return TabLaunchType.FROM_TAB_LIST_INTERFACE;
             case TabLaunchTypeAtCreation.FROM_LINK_CREATING_NEW_WINDOW:
                 return TabLaunchType.FROM_LINK_CREATING_NEW_WINDOW;
+            case TabLaunchTypeAtCreation.FROM_TIPS_NOTIFICATIONS:
+                return TabLaunchType.FROM_TIPS_NOTIFICATIONS;
             case TabLaunchTypeAtCreation.SIZE:
                 return TabLaunchType.SIZE;
             case TabLaunchTypeAtCreation.UNKNOWN:
@@ -320,6 +322,8 @@ public class FlatBufferTabStateSerializer implements TabStateSerializer {
                 return TabLaunchTypeAtCreation.FROM_TAB_LIST_INTERFACE;
             case TabLaunchType.FROM_LINK_CREATING_NEW_WINDOW:
                 return TabLaunchTypeAtCreation.FROM_LINK_CREATING_NEW_WINDOW;
+            case TabLaunchType.FROM_TIPS_NOTIFICATIONS:
+                return TabLaunchTypeAtCreation.FROM_TIPS_NOTIFICATIONS;
             case TabLaunchType.SIZE:
                 return TabLaunchTypeAtCreation.SIZE;
             default:
