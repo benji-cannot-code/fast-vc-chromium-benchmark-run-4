aab43959068247c81dd24080c36ade808bf855d6
Kene Okoye <kenok@google.com>
2025-11-06 18:14:34 -0800

    [actor] Add tests to verify ActorTaskState histogram variant
    Change-Id: I5dcabf7f3599a375195220c2afaaff1c1e9974a6
    Bug: b:457798269
    Fixed: b:457798269
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7124231
    Commit-Queue: Kevin Graney <kmg@google.com>
    Reviewed-by: Kevin Graney <kmg@google.com>
    Auto-Submit: Kene Okoye <kenok@google.com>
    Cr-Commit-Position: refs/heads/main@{#1541558}

--- chrome/browser/actor/histograms_unittest.cc
+++ chrome/browser/actor/histograms_unittest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/test/metrics/histogram_variants_reader.h"
+#include "chrome/browser/actor/actor_task.h"
 #include "chrome/browser/actor/tool_request_variant.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -40,6 +41,23 @@ TEST(ActorHistogramsTest, CheckToolRequestVariantNames) {
   CheckToolRequestVariantNamesImpl(
       *tool_requests, std::make_index_sequence<
                           std::variant_size_v<actor::ToolRequestVariant>>{});
+}
+
+TEST(ActorHistogramsTest, CheckActorTaskStateVariantNames) {
+  std::optional<base::HistogramVariantsEntryMap> task_states =
+      base::ReadVariantsFromHistogramsXml("ActorTaskState", "actor");
+  ASSERT_TRUE(task_states.has_value());
+  ASSERT_EQ(task_states->size(),
+            static_cast<size_t>(ActorTask::State::kMaxValue) + 1);
+
+  for (int i = 0; i <= static_cast<int>(ActorTask::State::kMaxValue); i++) {
+    std::string state_string = ToString(static_cast<ActorTask::State>(i));
+    EXPECT_TRUE(task_states->contains(state_string))
+        << "ActorTask::State " << state_string
+        << " not found in "
+           "//tools/metrics/histograms/metadata/actor/"
+           "histograms.xml:ActorTaskState.";
+  }
 }
 
 }  // namespace
