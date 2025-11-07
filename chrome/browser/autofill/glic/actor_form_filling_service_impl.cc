df411f499097fa108bdc8e6b1b3df5ea192d2479
Jan Keitel <jkeitel@google.com>
2025-11-06 22:50:56 -0800

    Always call callback asynchronously in actor form filling
    See //docs/chrome_browser_design_principles.md - callbacks should
    either always called synchronously or asynchronously.
    
    Bug: 455788947
    Change-Id: I9fbfb95bcb8decdc727110d32de8f7621931e52d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7124215
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1541614}

--- chrome/browser/autofill/glic/actor_form_filling_service_impl.cc
+++ chrome/browser/autofill/glic/actor_form_filling_service_impl.cc
@@ -452,12 +452,19 @@ void ActorFormFillingServiceImpl::FillSuggestions(
     base::span<const ActorFormFillingSelection> chosen_suggestions,
     base::OnceCallback<void(base::expected<void, ActorFormFillingError>)>
         callback) {
+  // Helper to make the early returns less verbose.
+  auto post_error = [&callback](const base::Location& location,
+                                ActorFormFillingError error) {
+    base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
+        location, base::BindOnce(std::move(callback), base::unexpected(error)));
+  };
+
   using enum ActorFormFillingError;
   base::expected<std::reference_wrapper<BrowserAutofillManager>,
                  ActorFormFillingError>
       maybe_client = GetAutofillManager(tab);
   if (!maybe_client.has_value()) {
-    std::move(callback).Run(base::unexpected(maybe_client.error()));
+    post_error(FROM_HERE, maybe_client.error());
     return;
   }
   BrowserAutofillManager& autofill_manager = maybe_client.value();
@@ -467,7 +474,7 @@ void ActorFormFillingServiceImpl::FillSuggestions(
           chosen_suggestions,
           [&](ActorSuggestionId id) { return fill_data_.contains(id); },
           &ActorFormFillingSelection::selected_suggestion_id)) {
-    std::move(callback).Run(base::unexpected(kOther));
+    post_error(FROM_HERE, kOther);
     return;
   }
 
@@ -488,7 +495,7 @@ void ActorFormFillingServiceImpl::FillSuggestions(
       } else {
         // TODO(crbug.com/455788947): Consider being more lenient and complying
         // with partial form fills.
-        std::move(callback).Run(base::unexpected(kNoForm));
+        post_error(FROM_HERE, kNoForm);
         return;
       }
     }
