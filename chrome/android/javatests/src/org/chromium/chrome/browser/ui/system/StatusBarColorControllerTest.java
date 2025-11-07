83cf624eb13af732b8fc85d5aa856c2d9809e20e
Joel Hockey <joelhockey@chromium.org>
2025-11-06 21:56:07 -0800

    [Gardener] Disable flaky StatusBarColorControllerTest
    disable flaky
    testBrandColorIgnoredWhenOmniboxIsFocused_FeatureMatchToolbarColorEnabled
    
    Bug: 458551111
    Change-Id: I3340af5bc6fc4c86ab02befe6cf5856b8e181047
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7129741
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Owners-Override: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541607}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ui/system/StatusBarColorControllerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ui/system/StatusBarColorControllerTest.java
@@ -316,6 +316,7 @@ public class StatusBarColorControllerTest {
     @LargeTest
     @Feature({"StatusBar"})
     @Restriction({DeviceFormFactor.PHONE}) // Status bar is always black on tablets
+    @DisabledTest(message = "crbug.com/458551111")
     public void testBrandColorIgnoredWhenOmniboxIsFocused_FeatureMatchToolbarColorEnabled()
             throws Exception {
         ChromeTabbedActivity activity = mActivityTestRule.getActivity();
