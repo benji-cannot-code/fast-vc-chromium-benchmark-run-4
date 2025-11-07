6030fba110229a4f1123cbfba6d85c38a03eb019
Gauthier Ambard <gambard@chromium.org>
2025-11-06 23:46:16 -0800

    [iOS] Use real NavigationBar in share extension
    For better positioning in iOS 26, this CL updates how the navigation
    bar is shown in the extension.
    
    Before:
    https://screenshot.googleplex.com/93GibLLXAMaPwZY
    
    After:
    https://screenshot.googleplex.com/7twmhZkmuKJVyap
    https://screenshot.googleplex.com/8dKQcLd4jewPSJo
    
    Bug: 444648926
    Change-Id: I1b3aae191201a094ec66018c6e3c9b9f3fb092b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7124722
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Elmehdi Rahmaoui <erahmaoui@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541626}

--- ios/chrome/share_extension/share_extension_sheet.mm
+++ ios/chrome/share_extension/share_extension_sheet.mm
@@ -101,10 +101,13 @@ CGFloat const kUpdatedMainViewCornerRadius = 32.0;
 
   self.customContentBottomInset = 0;
   self.customGradientViewHeight = 0;
+  self.showDismissBarButton = NO;
 
-  self.titleView = [self configureSheetTitleView];
-
-  self.dismissBarButtonSystemItem = UIBarButtonSystemItemClose;
+  self.navigationItem.titleView = [self configureSheetTitleView];
+  self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]
+      initWithBarButtonSystemItem:UIBarButtonSystemItemClose
+                           target:self
+                           action:@selector(dismissSheet)];
 
   if (app_group::MultiProfileShareExtensionEnabled()) {
     self.mainBackgroundColor = [UIColor colorNamed:kSecondaryBackgroundColor];
@@ -251,10 +254,6 @@ CGFloat const kUpdatedMainViewCornerRadius = 32.0;
 }
 
 #pragma mark - ConfirmationAlertActionHandler
-
-- (void)confirmationAlertDismissAction {
-  [self.delegate didTapCloseShareExtensionSheet:self];
-}
 
 - (void)confirmationAlertPrimaryAction {
   NSString* gaiaID = self.selectedAccountInfo.gaiaIDString;
@@ -660,6 +659,11 @@ CGFloat const kUpdatedMainViewCornerRadius = 32.0;
         constraintEqualToConstant:kDefaultSnapshotViewSize],
   ]];
   return snapshotView;
+}
+
+// Called when the sheet wants to be dismissed.
+- (void)dismissSheet {
+  [self.delegate didTapCloseShareExtensionSheet:self];
 }
 
 @end
