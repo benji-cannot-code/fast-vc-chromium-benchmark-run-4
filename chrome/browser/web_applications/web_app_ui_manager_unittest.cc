f25e9868bc4e530c48392729dea7b9a251294602
Andrew Rayskiy <greengrape@google.com>
2025-11-07 08:32:21 -0800

    Remove unused url_handler_launch_url from AppLaunchParams
    Bug: 40127045
    Change-Id: I7e08f84353e100b4409b102c8f6b9e4f05d9ea85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7123069
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Auto-Submit: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541835}

--- chrome/browser/web_applications/web_app_ui_manager_unittest.cc
+++ chrome/browser/web_applications/web_app_ui_manager_unittest.cc
@@ -56,7 +56,6 @@ class WebAppUiManagerTest : public testing::Test {
   apps::AppLaunchParams CreateLaunchParams(
       const base::CommandLine& command_line,
       const std::vector<base::FilePath>& launch_files,
-      const std::optional<GURL>& url_handler_launch_url,
       const std::optional<GURL>& protocol_handler_launch_url) {
     apps::AppLaunchParams params(
         kTestAppId, apps::LaunchContainer::kLaunchContainerNone,
@@ -65,7 +64,6 @@ class WebAppUiManagerTest : public testing::Test {
     params.current_directory = base::FilePath(kCurrentDirectory);
     params.command_line = command_line;
     params.launch_files = launch_files;
-    params.url_handler_launch_url = url_handler_launch_url;
     params.protocol_handler_launch_url = protocol_handler_launch_url;
 
     return params;
@@ -94,10 +92,6 @@ class WebAppUiManagerTest : public testing::Test {
               expected_results.current_directory);
     EXPECT_EQ(actual_results.launch_source, expected_results.launch_source);
     EXPECT_EQ(actual_results.launch_files, expected_results.launch_files);
-    EXPECT_EQ(actual_results.url_handler_launch_url,
-              expected_results.url_handler_launch_url);
-    ValidateOptionalGURL(actual_results.url_handler_launch_url,
-                         expected_results.url_handler_launch_url);
     ValidateOptionalGURL(actual_results.protocol_handler_launch_url,
                          expected_results.protocol_handler_launch_url);
 
@@ -115,13 +109,11 @@ TEST_F(WebAppUiManagerTest, DefaultParamsTab) {
 
   apps::AppLaunchParams expected_results =
       CreateLaunchParams(command_line, std::vector<base::FilePath>(),
-                         /*url_handler_launch_url=*/std::nullopt,
                          /*protocol_handler_launch_url=*/std::nullopt);
 
   ValidateLaunchParams(
       WebAppUiManager::CreateAppLaunchParamsWithoutWindowConfig(
           kTestAppId, command_line, base::FilePath(kCurrentDirectory),
-          /*url_handler_launch_url=*/std::nullopt,
           /*protocol_handler_launch_url=*/std::nullopt,
           /*file_launch_url=*/std::nullopt, /*launch_files=*/{}),
       expected_results);
@@ -133,13 +125,11 @@ TEST_F(WebAppUiManagerTest, DefaultParamsStandalone) {
 
   apps::AppLaunchParams expected_results =
       CreateLaunchParams(command_line, std::vector<base::FilePath>(),
-                         /*url_handler_launch_url=*/std::nullopt,
                          /*protocol_handler_launch_url=*/std::nullopt);
 
   ValidateLaunchParams(
       WebAppUiManager::CreateAppLaunchParamsWithoutWindowConfig(
           kTestAppId, command_line, base::FilePath(kCurrentDirectory),
-          /*url_handler_launch_url=*/std::nullopt,
           /*protocol_handler_launch_url=*/std::nullopt,
           /*file_launch_url=*/std::nullopt, /*launch_files=*/{}),
       expected_results);
@@ -152,15 +142,14 @@ TEST_F(WebAppUiManagerTest, ProtocolHandlerUrl) {
   base::CommandLine command_line = CreateCommandLine();
   command_line.AppendArg(protocol_handler_launch_url.value().spec());
 
-  apps::AppLaunchParams expected_results =
-      CreateLaunchParams(command_line, std::vector<base::FilePath>(),
-                         std::nullopt, protocol_handler_launch_url);
+  apps::AppLaunchParams expected_results = CreateLaunchParams(
+      command_line, std::vector<base::FilePath>(), protocol_handler_launch_url);
   expected_results.launch_source = apps::LaunchSource::kFromProtocolHandler;
 
   ValidateLaunchParams(
       WebAppUiManager::CreateAppLaunchParamsWithoutWindowConfig(
           kTestAppId, command_line, base::FilePath(kCurrentDirectory),
-          /*url_handler_launch_url=*/std::nullopt, protocol_handler_launch_url,
+          protocol_handler_launch_url,
           /*file_launch_url=*/std::nullopt, /*launch_files=*/{}),
       expected_results);
 }
@@ -173,15 +162,14 @@ TEST_F(WebAppUiManagerTest, LaunchApplication_ProtocolMailTo) {
 
   command_line.AppendArg(protocol_handler_launch_url.value().spec());
 
-  apps::AppLaunchParams expected_results =
-      CreateLaunchParams(command_line, std::vector<base::FilePath>(),
-                         std::nullopt, protocol_handler_launch_url);
+  apps::AppLaunchParams expected_results = CreateLaunchParams(
+      command_line, std::vector<base::FilePath>(), protocol_handler_launch_url);
   expected_results.launch_source = apps::LaunchSource::kFromProtocolHandler;
 
   ValidateLaunchParams(
       WebAppUiManager::CreateAppLaunchParamsWithoutWindowConfig(
           kTestAppId, command_line, base::FilePath(kCurrentDirectory),
-          std::nullopt, protocol_handler_launch_url, std::nullopt, {}),
+          protocol_handler_launch_url, std::nullopt, {}),
       expected_results);
 }
 
@@ -193,12 +181,12 @@ TEST_F(WebAppUiManagerTest, LaunchApplication_ProtocolDisallowed) {
   command_line.AppendArg("https://www.test.com/");
 
   apps::AppLaunchParams expected_results =
-      CreateLaunchParams(command_line, {}, std::nullopt, std::nullopt);
+      CreateLaunchParams(command_line, {}, std::nullopt);
 
   ValidateLaunchParams(
       WebAppUiManager::CreateAppLaunchParamsWithoutWindowConfig(
           kTestAppId, command_line, base::FilePath(kCurrentDirectory),
-          std::nullopt, std::nullopt, std::nullopt, {}),
+          std::nullopt, std::nullopt, {}),
       expected_results);
 }
 
