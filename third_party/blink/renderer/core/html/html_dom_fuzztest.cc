d7c39d9d82dfa57b1a4bdbbb1c145764b928b180
Joanmarie Diggs <jdiggs@igalia.com>
2025-11-07 01:58:59 -0800

    Add HTML+CSS DOM fuzzing infrastructure based on FuzzTest
    Provides:
    * Reusable HTML tag and attribute FuzzTest domains.
    * Reusable CSS style property FuzzTest domains.
    * "DomScenario" infrastructure to create and run tests in which the
      initial DOM and modifications to that DOM are fuzzed.
    * The ability to override the functionality in the test runner. For
      instance, an accessibility fuzzer could dump the accessibility tree
      after the initial creation and subsequent modification.
    * A new FuzzTest test for HTML using the above domains and
      infrastructure.
    
    Bug: 446479786
    
    Change-Id: I7957cf53dca86ce0dc4d68c6fc66ce3d32795f8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6962254
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Joanmarie Diggs <jdiggs@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1541675}

---
+++ third_party/blink/renderer/core/html/html_dom_fuzztest.cc
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/core/html_names.h"
+#include "third_party/blink/renderer/core/testing/fuzztest_utils/css_domains.h"
+#include "third_party/blink/renderer/core/testing/fuzztest_utils/dom_scenario_runner.h"
+#include "third_party/blink/renderer/core/testing/fuzztest_utils/html_domains.h"
+
+namespace blink {
+
+class HTMLSpec : public DomScenarioDomainSpecification {
+ public:
+  fuzztest::Domain<QualifiedName> AnyTag() override { return AnyHtmlTag(); }
+  fuzztest::Domain<std::pair<QualifiedName, std::string>>
+  AnyAttributeNameValuePair() override {
+    return AnyHtmlAttributeNameValuePair();
+  }
+  fuzztest::Domain<std::string> AnyStyles() override {
+    return AnyCssDeclaration();
+  }
+  fuzztest::Domain<std::string> AnyText() override {
+    return fuzztest::PrintableAsciiString();
+  }
+  int GetMaxDomNodes() override { return 8; }
+  int GetMaxAttributesPerNode() override { return 3; }
+  fuzztest::Domain<QualifiedName> GetRootElementTag() override {
+    return fuzztest::Just<QualifiedName>(
+        html_names::TagToQualifiedName(html_names::HTMLTag::kBody));
+  }
+};
+
+class HTMLDomScenarioRunner : public DomScenarioRunner {
+ public:
+  HTMLDomScenarioRunner() = default;
+
+  void HTML(const DomScenario& input) { RunTest(input); }
+};
+
+FUZZ_TEST_F(HTMLDomScenarioRunner, HTML)
+    .WithDomains(BuildDomScenarios<HTMLSpec>());
+
+}  // namespace blink
