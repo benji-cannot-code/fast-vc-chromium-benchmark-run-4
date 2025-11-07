e795d486b3dcfe8401399ba52c6eab4d6972ef0b
Samuel Huang <huangs@chromium.org>
2025-11-07 13:31:29 -0800

    [Reader Mode] Make and use local modded copies of Readability{-readerable}.js.
    To improve velocity, this CL duplicates the core Readability library
    files:
    * Readability.js
    * Readability-readerable.js
    into new direrectory modded_src/ under //third_party/readability, copied
    from //third_party/readability/src, and update instructions.
    
    We're using direct copy instead of patching for simplicity: We only need
    to copy the two files verbatim from the library. Effort will still be
    made to upstream our changes and roll from the library, but now we can
    decouple merging from code changes, and enable code review iteration for
    these files in Chromium.
    
    For testing (manually run locally), we'd *temporarily* copy the modded
    file back to src/ and run `npm test`, then restore the files. Any
    divergence should be noted in README.chromium.
    
    Bug: 458753174
    Change-Id: I4eb30e39c6126dea67fb23066cf94687c7d84b11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7133518
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1541993}

--- components/dom_distiller/core/javascript/readability_triggering.js
+++ components/dom_distiller/core/javascript/readability_triggering.js
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 function initialize() {
   // This include will be processed at build time by grit.
   // clang-format off
-        // <include src="../../../../third_party/readability/src/Readability-readerable.js">
+        // <include src="../../../../third_party/readability/modded_src/Readability-readerable.js">
   // clang-format on
   window.isProbablyReaderable = isProbablyReaderable;
 }
