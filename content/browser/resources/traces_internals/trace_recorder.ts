d2a5f75c7871bcd100943ccfd9f78b33ca2bd1d3
Jesse McKenna <jessemckenna@google.com>
2025-11-07 15:08:24 -0800

    tracing: add file I/O ETW provider
    This change enables Chrome tracing to capture and decode file I/O ETW
    events, to be passed to Perfetto. These are Windows native events that
    describe file I/O operations like file creation, read, write, etc.
    
    The corresponding Perfetto protos can be seen here:
    https://source.chromium.org/chromium/chromium/src/+/main:third_party/perfetto/protos/perfetto/trace/etw/etw.proto;l=211;drc=a01a11ae2a7c0476c9ddfb0e0f77891b86110663
    
    This is part of a broader effort to add ETW kernel events to Chrome
    traces for Perfetto (Google-internal design doc:
    go/windows-system-tracing). A subsequent change to Perfetto will add
    visualization for these events.
    
    Note: this CL will be submitted after cl/814443722 (Google-internal).
    
    Bug: 400769108
    Change-Id: I3b94dfcc55f4ee47118afa153e4d3d0b69a5975d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6863216
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1542057}

--- content/browser/resources/traces_internals/trace_recorder.ts
+++ content/browser/resources/traces_internals/trace_recorder.ts
@@ -43,7 +43,7 @@ export interface TraceRecorderElement {
 }
 
 // <if expr="is_win">
-type EtwProviderType = 'scheduler'|'memory';
+type EtwProviderType = 'scheduler'|'memory'|'file';
 // </if>
 
 export class TraceRecorderElement extends CrLitElement {
@@ -132,6 +132,7 @@ export class TraceRecorderElement extends CrLitElement {
       {[provider in EtwProviderType]: Set<string>} = {
         'memory': new Set(),
         'scheduler': new Set(),
+        'file': new Set(),
       };
   protected accessor etwExpanded_: boolean = false;
   // </if>
@@ -388,6 +389,7 @@ export class TraceRecorderElement extends CrLitElement {
           [...this.enabledEtwEvents['scheduler']];
       this.etwConfig.memoryProviderEvents =
           [...this.enabledEtwEvents['memory']];
+      this.etwConfig.fileProviderEvents = [...this.enabledEtwEvents['file']];
     }
 
     this.updateUrlFromConfig_();
@@ -477,6 +479,12 @@ export class TraceRecorderElement extends CrLitElement {
         keyword: 'MEMINFO',
         provider: 'memory',
         description: 'Enables memory counters (free list, zero list, etc.)',
+      },
+      {
+        name: 'File I/O',
+        keyword: 'FILE_IO',
+        provider: 'file',
+        description: 'Enables file I/O events',
       },
     ];
     // </if>
@@ -691,6 +699,8 @@ export class TraceRecorderElement extends CrLitElement {
           new Set(this.etwConfig.schedulerProviderEvents);
       this.enabledEtwEvents['memory'] =
           new Set(this.etwConfig.memoryProviderEvents);
+      this.enabledEtwEvents['file'] =
+          new Set(this.etwConfig.fileProviderEvents);
     }
     // </if>
   }
