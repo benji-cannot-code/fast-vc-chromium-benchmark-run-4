0c10f8f0bf9b9030902e612e6743d02c08bc52b4
Ashley Newson <ashleynewson@chromium.org>
2025-11-07 06:46:29 -0800

    [webview] Add WebViewBuilder setProfile support
    Adds support for setProfile to the WebViewBuilder configuration
    boundary interface and adapter logic.
    
    Bug: b:409740828
    Change-Id: I46ba98e2ee5a316fe98ff17abe3011b0fa976690
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7129198
    Reviewed-by: Peter Birk Pakkenberg <pbirk@chromium.org>
    Auto-Submit: Ashley Newson <ashleynewson@chromium.org>
    Commit-Queue: Ashley Newson <ashleynewson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541772}

--- android_webview/support_library/java/src/org/chromium/support_lib_glue/SupportLibWebViewBuilderAdapter.java
+++ android_webview/support_library/java/src/org/chromium/support_lib_glue/SupportLibWebViewBuilderAdapter.java
@@ -13,6 +13,7 @@ import androidx.annotation.Nullable;
 import com.android.webview.chromium.SharedWebViewChromium;
 import com.android.webview.chromium.WebkitToSharedGlueConverter;
 
+import org.chromium.android_webview.AwBrowserContextStore;
 import org.chromium.android_webview.AwContents;
 import org.chromium.support_lib_boundary.WebViewBuilderBoundaryInterface;
 import org.chromium.support_lib_boundary.WebViewBuilderBoundaryInterface.ConfigField;
@@ -41,10 +42,10 @@ class SupportLibWebViewBuilderAdapter implements WebViewBuilderBoundaryInterface
 
     static class Builder implements BiConsumer<@ConfigField Integer, Object> {
         private boolean mRestrictJavascriptInterface;
-
         private @Nullable List<Object> mJavascriptInterfaceObjects;
         private @Nullable List<String> mJavascriptInterfaceNames;
         private @Nullable List<List<String>> mJavascriptInterfaceOriginPatterns;
+        private @Nullable String mProfileName;
 
         @Override
         public void accept(@ConfigField Integer key, Object value) {
@@ -69,6 +70,11 @@ class SupportLibWebViewBuilderAdapter implements WebViewBuilderBoundaryInterface
                                     (List<List<String>>) interfaceParams[2];
                             break;
                         }
+                    case ConfigField.PROFILE_NAME:
+                        {
+                            mProfileName = (String) value;
+                            break;
+                        }
                 }
             } catch (ClassCastException e) {
                 throw new InternalApiMismatchException(key + " was not configured correctly", e);
@@ -83,6 +89,11 @@ class SupportLibWebViewBuilderAdapter implements WebViewBuilderBoundaryInterface
             SharedWebViewChromium sharedWebViewChromium =
                     WebkitToSharedGlueConverter.getSharedWebViewChromium(webview);
             AwContents awContents = sharedWebViewChromium.getAwContents();
+
+            if (mProfileName != null) {
+                awContents.setBrowserContextForPublicApi(
+                        AwBrowserContextStore.getNamedContext(mProfileName, true));
+            }
 
             boolean allowlistJavascriptInterfaces =
                     mJavascriptInterfaceObjects != null
