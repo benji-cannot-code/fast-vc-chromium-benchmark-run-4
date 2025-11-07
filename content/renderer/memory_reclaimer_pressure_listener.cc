0224ce6a494e9cd9d0b757343b2c2ece6dab802e
Patrick Monette <pmonette@chromium.org>
2025-11-07 12:52:49 -0800

    Make a MemoryPressureListener for MemoryReclaimer
    The goal of this change is to separate all the memory pressure
    interventions in RenderThreadImpl::OnMemoryPressure so that they can
    be invoked separately.
    
    This is a necessary change for migrating all MemoryPressureListeners
    to the base::MemoryConsumer interface, where each intervention is
    tagged with different properties.
    
    Bug: 436324601
    Change-Id: Ie6c3fe4ae2f6ce36612a913ef181d5d478fef1d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7127962
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541975}

---
+++ content/renderer/memory_reclaimer_pressure_listener.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/renderer/memory_reclaimer_pressure_listener.h"
+
+#include "partition_alloc/memory_reclaimer.h"
+
+namespace content {
+
+MemoryReclaimerPressureListener::MemoryReclaimerPressureListener()
+    : memory_pressure_listener_registration_(
+          FROM_HERE,
+          base::MemoryPressureListenerTag::kMemoryReclaimerPressureListener,
+          this) {}
+
+MemoryReclaimerPressureListener::~MemoryReclaimerPressureListener() = default;
+
+void MemoryReclaimerPressureListener::OnMemoryPressure(
+    base::MemoryPressureLevel level) {
+  ::partition_alloc::MemoryReclaimer::Instance()->ReclaimAll();
+}
+
+}  // namespace content
--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -132,7 +132,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/port_util.h"
 #include "net/base/registry_controlled_domains/registry_controlled_domain.h"
 #include "net/base/url_util.h"
-#include "partition_alloc/memory_reclaimer.h"
 #include "services/network/public/cpp/network_switches.h"
 #include "services/service_manager/public/cpp/interface_provider.h"
 #include "services/viz/public/cpp/gpu/context_provider_command_buffer.h"
@@ -1719,7 +1718,6 @@ void RenderThreadImpl::OnMemoryPressure(
       SkGraphics::PurgeAllCaches();
     }
   }
-  ::partition_alloc::MemoryReclaimer::Instance()->ReclaimAll();
 }
 
 void RenderThreadImpl::OnRendererInterfaceReceiver(
