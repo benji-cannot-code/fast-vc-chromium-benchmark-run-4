b440f00ed50d08bb1328f3015717ace12349bdb1
Fiaz Muhammad <mfiaz@google.com>
2025-11-07 08:13:20 -0800

    [Persist] RestoreIdAssociator for Restoring Node Storage Ids
    - The RestoreIdAssociator performs the mapping of handles to nodes.
    - We have an android version and an interface.
    - It traverses upwards through the tree and loads each collection
    	to the storage map along the way. It does not load nodes more
            than once.
    - There is a builder class that performs the registration of the
            various nodes in storage.
    
    Bug: 456508880
    Change-Id: Id0c6140e1e264db360169da65a18834c005e7f0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7128558
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541823}

---
+++ chrome/browser/android/restore_id_associator_builder_android.h
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ANDROID_RESTORE_ID_ASSOCIATOR_BUILDER_ANDROID_H_
+#define CHROME_BROWSER_ANDROID_RESTORE_ID_ASSOCIATOR_BUILDER_ANDROID_H_
+
+#include "chrome/browser/android/restore_id_associator_android.h"
+#include "chrome/browser/tab/protocol/children.pb.h"
+#include "chrome/browser/tab/protocol/tab_state.pb.h"
+#include "chrome/browser/tab/restore_id_associator.h"
+#include "chrome/browser/tab/restore_id_associator_builder.h"
+
+namespace tabs {
+
+// Maps storage IDs to TabCollection and TabInterface nodes.
+class RestoreIdAssociatorBuilderAndroid : public RestoreIdAssociatorBuilder {
+ public:
+  using RestoreIdAssociatorState =
+      RestoreIdAssociatorAndroid::RestoreIdAssociatorState;
+
+  RestoreIdAssociatorBuilderAndroid(OnTabAssociation, OnCollectionAssociation);
+  ~RestoreIdAssociatorBuilderAndroid() override;
+
+  void RegisterCollection(int storage_id,
+                          const tabs_pb::Children& children) override;
+  void RegisterTab(int storage_id, const tabs_pb::TabState& tab_state) override;
+
+  std::unique_ptr<RestoreIdAssociator> BuildAssociator() override;
+
+ private:
+  std::unique_ptr<RestoreIdAssociatorState> state_;
+};
+
+}  // namespace tabs
+
+#endif  // CHROME_BROWSER_ANDROID_RESTORE_ID_ASSOCIATOR_BUILDER_ANDROID_H_
