d06e1aee4d48bdba328b962d7a168db88f5378be
Struan Shrimpton <sshrimp@google.com>
2025-11-06 16:53:46 -0800

    Tag rdb results with their metrics
    We mostly care about the score metric but adding all of them is easy
    enough and will give us flexibility in our analysis.
    
    Bug: None
    Change-Id: Ie2938db42b521e8a960cebcfafc5f47c345aa63f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7128137
    Commit-Queue: Struan Shrimpton <sshrimp@google.com>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541529}

--- agents/testing/resultdb.py
+++ agents/testing/resultdb.py
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import sys
 
 import constants
+import metrics
 import results
 
 sys.path.insert(0, str(constants.CHROMIUM_SRC / 'build' / 'util'))
@@ -35,6 +36,10 @@ class ResultDBReporter:
             constants.CHROMIUM_SRC)
         posix_path = relative_path.as_posix()
         for iteration_result in test_result.iteration_results:
+            tags = [(name.replace('.', '_').lower(), str(value))
+                    for name, value in metrics.iterate_over_nested_metrics(
+                        iteration_result.metrics)]
+
             self._result_sink_client.Post(
                 test_id=str(posix_path),
                 status=(result_types.PASS
@@ -46,4 +51,5 @@ class ResultDBReporter:
                     'fineName': '',  # Leave blank for scheme 'flat'.
                     'caseNameComponents': [str(posix_path)],
                 },
-                test_file=f'//{str(posix_path)}')
+                test_file=f'//{str(posix_path)}',
+                tags=tags)
