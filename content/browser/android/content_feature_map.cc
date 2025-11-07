d6e2916f67b5530feb93ac55d6dae7adb1e3f996
David Tseng <dtseng@google.com>
2025-11-07 11:02:50 -0800

    android-a11y: support action set selection on any ANI with text
    Chrome within web contents can generally support a DOM based selection
    on a node with text.
    
    From the accessibility tree, if the accessible name comes from rendered
    text e.g. contents, we map this name to Android's ANI.setText().
    
    When we receive a set selection request, we handle it by creating a text
    AXPosition selection. This takes care of adjusting the node downward for
    a text position.
    
    In this change, we therefore consider any node that populates text
    (via ANI.setText) as selectable and adds the ACTION_SET_SELECTION.
    
    Change-Id: I20e49d004fae80e9bb9868dffbbb6db47c8f3bff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7103558
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541928}

--- content/browser/android/content_feature_map.cc
+++ content/browser/android/content_feature_map.cc
@@ -36,6 +36,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &features::kAccessibilityDeprecateTypeAnnounce,
     &features::kAccessibilityPageZoomV2,
     &features::kAccessibilityPopulateSupplementalDescriptionApi,
+    &features::kAccessibilitySetSelectableOnAllNodesWithText,
     &features::kAccessibilityUnifiedSnapshots,
     &features::kAccessibilityManageBroadcastReceiverOnBackground,
     &features::kAndroidDesktopZoomScaling,
