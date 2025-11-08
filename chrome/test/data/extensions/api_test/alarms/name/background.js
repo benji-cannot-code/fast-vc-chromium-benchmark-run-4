609f3ba681d2ac5e9ed7de2009e1eef049a5bb6d
Anton Bershanskyi <bershanskyi@gmail.com>
2025-11-07 19:32:19 -0800

    [Extensions] Histogram for extension alarm name length
    On every extension alarm change, AlarmManager serializes all alarms for
    the said extension. Overly long serialized data negatively affects
    browser performance (the severity of the problem depends on the system).
    Bug 1400582 (CL 4583941) limited extensions to 500 alarms each.
    Extensions still can create large serializations by specifying alarm
    names of unbounded lengths. This CL creates a histogram capturing
    prevalence of extensions utilizing long alarm names. This data will be
    used later to determine an optimal limit for lengths of extension alarm
    name.
    
    Change-Id: I53ec3ce708e424f38050e364394594e8f1a39731
    Bug: 445720439
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6961075
    Commit-Queue: Anton Bershanskyi <bershanskyi@gmail.com>
    Reviewed-by: Justin Lulejian <jlulejian@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1542102}

---
+++ chrome/test/data/extensions/api_test/alarms/name/background.js
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.test.runTests([
+  async function hasNoAlarms() {
+    // No alarms should be initially registered, since this extension only
+    // runs once.
+    const alarms = await chrome.alarms.getAll();
+    chrome.test.assertEq(0, alarms.length);
+    chrome.test.succeed();
+  },
+
+  async function setAlarms() {
+    // Create alarms that won't have time to run (so they remain in storage
+    // to be counted in the histogram).
+    const createParams = {delayInMinutes: 120};
+    for (const length of [10, 200, 50]) {
+      await chrome.alarms.create('a'.repeat(length), createParams);
+    }
+    const alarms = await chrome.alarms.getAll();
+    chrome.test.assertEq(3, alarms.length);
+    chrome.test.succeed();
+  },
+]);
