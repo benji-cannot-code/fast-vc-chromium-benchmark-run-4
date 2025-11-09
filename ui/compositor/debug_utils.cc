cf6e779600110d4ee444b5086d1308e4708cad7d
Zoraiz Naeem <zoraiznaeem@chromium.org>
2025-11-09 14:28:20 -0800

    Print layer background-color with dumping layer tree for debugging
    Change-Id: I24e136c2daadec371eb33ec60f9aa33d718c25d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7133093
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1542341}

--- ui/compositor/debug_utils.cc
+++ ui/compositor/debug_utils.cc
@@ -16,7 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/numerics/angle_conversions.h"
 #include "cc/trees/layer_tree_host.h"
+#include "ui/color/color_provider_utils.h"
 #include "ui/compositor/layer.h"
+#include "ui/compositor/layer_type.h"
 #include "ui/gfx/geometry/point.h"
 #include "ui/gfx/geometry/point_conversions.h"
 #include "ui/gfx/geometry/transform.h"
@@ -107,6 +109,11 @@ void PrintLayerHierarchyImp(const Layer* layer,
     *out << "\n" << property_indent_str;
     *out << "rounded-corners-radii: "
          << layer->rounded_corner_radii().ToString();
+  }
+
+  if (layer->type() == LAYER_SOLID_COLOR) {
+    *out << "\n" << property_indent_str;
+    *out << "background-color=" << ui::SkColorName(layer->background_color());
   }
 
   const ui::Layer* mask = const_cast<ui::Layer*>(layer)->layer_mask_layer();
