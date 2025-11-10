91ae37d3d4f7c882f54fbeef6cb7b73271e55365
Glenn Hartmann <hartmanng@chromium.org>
2025-11-10 12:37:01 -0800

    [bedrock] Migrate read_anything_service.cc away from BrowserListObserver.
    Note that this changes the existing observer from listening to all
    browsers globally to just listening to browsers within the relevant
    Profile.
    
    This migration is part of project bedrock to reduce the dependencies on
    Browser and BrowserList. See https://crbug.com/431671320 for more info.
    
    Bug: 459505408
    Change-Id: Ifecb8278b83b8f9ae914a6f240b78d256a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7088446
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Glenn Hartmann <hartmanng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1542723}

--- chrome/browser/ui/read_anything/read_anything_service.cc
+++ chrome/browser/ui/read_anything/read_anything_service.cc
@@ -13,8 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_features.h"
+#include "chrome/browser/ui/browser_window/public/profile_browser_collection.h"
 #include "chrome/browser/ui/read_anything/read_anything_service_factory.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_entry_id.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_entry_key.h"
@@ -57,8 +57,10 @@ ReadAnythingService::ReadAnythingService(Profile* profile) : profile_(profile) {
             &ReadAnythingService::OnLocalSidePanelSwitchDelayTimeout,
             weak_ptr_factory_.GetWeakPtr()));
   }
-  if (features::IsDataCollectionModeForScreen2xEnabled()) {
-    browser_list_observer_.Observe(BrowserList::GetInstance());
+  if (features::IsDataCollectionModeForScreen2xEnabled() &&
+      profile_->AllowsBrowserWindows()) {
+    browser_collection_observer_.Observe(
+        ProfileBrowserCollection::GetForProfile(profile_));
   }
 }
 
@@ -167,9 +169,8 @@ void ReadAnythingService::OnLocalSidePanelSwitchDelayTimeout() {
   RemoveGDocsHelperExtension();
 }
 
-void ReadAnythingService::OnBrowserSetLastActive(Browser* browser) {
-  if (!features::IsDataCollectionModeForScreen2xEnabled() ||
-      browser->profile() != profile_) {
+void ReadAnythingService::OnBrowserActivated(BrowserWindowInterface* browser) {
+  if (!features::IsDataCollectionModeForScreen2xEnabled()) {
     return;
   }
 
