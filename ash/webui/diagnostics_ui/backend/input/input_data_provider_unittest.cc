b5fa6761344ae368bb4d7459b8bf2b70fba20f86
Arthur Sonzogni <arthursonzogni@chromium.org>
2025-11-10 05:48:45 -0800

    Convert to UNSAFE_TODO in ash
    This is an automated #cleanup patch using the [Script] below.
    
    We are migrating from coarse-grained file-level suppression (#pragma
    allow_unsafe_buffers) to granular, expression-level markers
    (UNSAFE_TODO()). The pragma disables safety checks for an entire file,
    whereas UNSAFE_TODO() isolates specific potentially unsafe operations,
    allowing the rest of the file to be enforced as safe.
    
    This CL was uploaded by an experimental version of git cl split
    (https://crbug.com/389069356).
    
    Script: https://docs.google.com/document/d/1ORQGBNn2R-CEvNbDTjRd-GrOBOFlCxIHdcvSUA_EhR4/edit?usp=sharing
    AX-Relnotes: N/A
    Cleanup: This is an automated #cleanup.
    Bug: 409340989
    Change-Id: I9105d78f2022eb450cf80b48adf900519a16f9a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7132318
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1542507}

--- ash/webui/diagnostics_ui/backend/input/input_data_provider_unittest.cc
+++ ash/webui/diagnostics_ui/backend/input/input_data_provider_unittest.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "ash/webui/diagnostics_ui/backend/input/input_data_provider.h"
 
 #include <cstdint>
@@ -30,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/webui/diagnostics_ui/mojom/input_data_provider.mojom.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "base/command_line.h"
+#include "base/compiler_specific.h"
 #include "base/containers/flat_map.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
@@ -738,7 +734,8 @@ class InputDataProviderTest : public AshTestBase {
     size_t i;
 
     i = 0;
-    for (auto* iter = list.begin(); iter != list.end(); iter++, i++) {
+    for (auto* iter = list.begin(); iter != list.end();
+         UNSAFE_TODO(iter++), i++) {
       (*provider_->watchers_)[id]->PostKeyEvent(iter->down, iter->key.key_code,
                                                 iter->key.at_scan_code);
     }
@@ -747,7 +744,8 @@ class InputDataProviderTest : public AshTestBase {
     ASSERT_EQ(std::size(list), fake_observer->events_.size());
 
     i = 0;
-    for (auto* iter = list.begin(); iter != list.end(); iter++, i++) {
+    for (auto* iter = list.begin(); iter != list.end();
+         UNSAFE_TODO(iter++), i++) {
       EXPECT_EQ(
           *fake_observer->events_[i].second,
           mojom::KeyEvent(/*id=*/id,
