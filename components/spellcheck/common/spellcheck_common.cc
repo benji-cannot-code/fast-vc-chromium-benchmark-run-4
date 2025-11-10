30c2a1c68d310dd2cb78f713bc5765748f9d894b
Darren Shen <shend@chromium.org>
2025-11-09 17:54:23 -0800

    Enable Galician spellcheck
    Add Galician (gl) to the list of spellcheck languages.
    
    The bdic file should already be available at:
    https://redirector.gvt1.com/edgedl/chrome/dict/gl-1-0.bdic
    
    Tested manually with some Galician words.
    
    Bug: 41185763
    Change-Id: Iaeecd4fb19560e460720705f49c85e515aeec186
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7126480
    Reviewed-by: Henrique Ferreiro <hferreiro@igalia.com>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1542374}

--- components/spellcheck/common/spellcheck_common.cc
+++ components/spellcheck/common/spellcheck_common.cc
@@ -59,6 +59,7 @@ static constexpr LanguageRegion kSupportedSpellCheckerLanguages[] = {
     {"fo", "fo-FO"},
     {"fr", "fr-FR"},
     {"fr-FR", "fr-FR"},
+    {"gl", "gl"},
     {"he", "he-IL"},
     {"hi", "hi-IN"},
     {"hr", "hr-HR"},
@@ -151,6 +152,9 @@ base::FilePath GetVersionedFileName(std::string_view input_language,
 
       // March 2022: Update uk-UA dictionary from upstream.
       {"uk-UA", "-5-0"},
+
+      // Nov 2025: Initial check-in of Galician
+      {"gl", "-1-0"},
   };
 
   // Generate the bdict file name using default version string or special
