b1cbc54b388bbe1f0b3951f50401fdd18bfda7c2
Ananya <ananyaseelam@google.com>
2025-11-10 14:34:24 -0800

    [omnibox-next] Add tab contextualization
    This CL implements getting the tab page content from a selected tab and
    uploading it via the query_controller.
    
    Demo: https://screencast.googleplex.com/cast/NDY1NzMxNzIwMjEwMDIyNHw1MDNiOWYzOC0zOQ
    Bug: b:457793755
    Change-Id: I7173f1d7898bc4fdb721a77e8ac1f728cdae87ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7128960
    Commit-Queue: Ananya Seelam <ananyaseelam@google.com>
    Reviewed-by: Marlon Facey <mfacey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1542788}

--- chrome/browser/ui/omnibox/omnibox_context_menu_controller.h
+++ chrome/browser/ui/omnibox/omnibox_context_menu_controller.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/task/cancelable_task_tracker.h"
 #include "base/time/time.h"
+#include "base/unguessable_token.h"
 #include "ui/menus/simple_menu_model.h"
 #include "url/gurl.h"
 
@@ -36,6 +37,10 @@ namespace contextual_search {
 class ContextualSearchContextController;
 }
 
+namespace lens {
+struct ContextualInputData;
+}  // namespace lens
+
 // OmniboxContextMenuController creates and manages state for the context menu
 // shown for the omnibox.
 class OmniboxContextMenuController : public ui::SimpleMenuModel::Delegate {
@@ -44,6 +49,12 @@ class OmniboxContextMenuController : public ui::SimpleMenuModel::Delegate {
                                         OmniboxPopupFileSelector* file_selector,
                                         content::WebContents* aim_web_contents,
                                         OmniboxEditModel* edit_model);
+  struct TabInfo {
+    int tab_id;
+    std::u16string title;
+    GURL url;
+    base::TimeTicks last_active;
+  };
 
   OmniboxContextMenuController(const OmniboxContextMenuController&) = delete;
   OmniboxContextMenuController& operator=(const OmniboxContextMenuController&) =
@@ -54,6 +65,7 @@ class OmniboxContextMenuController : public ui::SimpleMenuModel::Delegate {
   ui::SimpleMenuModel* menu_model() { return menu_model_.get(); }
 
   void ExecuteCommand(int command_id, int event_flags) override;
+  void AddTabContext(const TabInfo& tab_info);
 
  private:
   void BuildMenu();
@@ -75,6 +87,8 @@ class OmniboxContextMenuController : public ui::SimpleMenuModel::Delegate {
   void AddStaticItems();
   // Adds a title with a localized string to the menu.
   void AddTitleWithStringId(int localization_id);
+  // Gets the most recent tabs.
+  std::vector<OmniboxContextMenuController::TabInfo> GetRecentTabs();
   // Adds the tabs favicon to the menu.
   void AddTabFavicon(int command_id,
                      const GURL& url,
@@ -83,8 +97,10 @@ class OmniboxContextMenuController : public ui::SimpleMenuModel::Delegate {
   void OnFaviconDataAvailable(
       int command_id,
       const favicon_base::FaviconImageResult& image_result);
-  // Returns whether the tab is valid to be shown in the context menu.
-  bool IsValidTab(GURL url);
+
+  void OnGetTabPageContext(
+      const base::UnguessableToken& context_token,
+      std::unique_ptr<lens::ContextualInputData> page_content_data);
 
   std::unique_ptr<ui::SimpleMenuModel> menu_model_;
   base::WeakPtr<content::WebContents> web_contents_;
