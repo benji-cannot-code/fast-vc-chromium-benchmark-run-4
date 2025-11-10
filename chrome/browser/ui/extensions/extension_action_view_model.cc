145b09b65756b9c5d9682e836debbca3408ff807
Shuhei Takahashi <nya@chromium.org>
2025-11-10 15:55:25 -0800

    extensions: Guard side panel related code
    This is a part of the patch series to compile ExtensionActionViewModel
    on Desktop Android.
    
    We don't have an immediate plan to support side panels on Desktop
    Android, thus guard side panel related code for now.
    
    Bug: 448199168
    Change-Id: Ib8c406ba109a7a16c9e4f9c232a08d217db3bece
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7082091
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1542830}

--- chrome/browser/ui/extensions/extension_action_view_model.cc
+++ chrome/browser/ui/extensions/extension_action_view_model.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/strcat.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
-#include "chrome/browser/extensions/api/side_panel/side_panel_service.h"
 #include "chrome/browser/extensions/commands/command_service.h"
 #include "chrome/browser/extensions/extension_action_runner.h"
 #include "chrome/browser/extensions/extension_context_menu_model.h"
@@ -30,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/extensions/extension_action_platform_delegate.h"
 #include "chrome/browser/ui/extensions/extension_popup_types.h"
-#include "chrome/browser/ui/extensions/extension_side_panel_utils.h"
 #include "chrome/browser/ui/extensions/icon_with_badge_image_source.h"
 #include "chrome/browser/ui/tabs/tab_list_interface.h"
 #include "chrome/browser/ui/toolbar/toolbar_actions_model.h"
@@ -50,6 +48,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/image/image_skia.h"
 #include "ui/gfx/native_ui_types.h"
 #include "ui/native_theme/native_theme.h"
+
+#if BUILDFLAG(ENABLE_EXTENSIONS)
+#include "chrome/browser/extensions/api/side_panel/side_panel_service.h"
+#include "chrome/browser/ui/extensions/extension_side_panel_utils.h"
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
 using extensions::ActionInfo;
 using extensions::CommandService;
@@ -347,10 +350,15 @@ bool ExtensionActionViewModel::IsEnabled(
     return true;
   }
 
+#if BUILDFLAG(ENABLE_EXTENSIONS)
   extensions::SidePanelService* side_panel_service =
       extensions::SidePanelService::Get(profile_);
-  return side_panel_service &&
-         side_panel_service->HasSidePanelActionForTab(*extension(), tab_id);
+  if (side_panel_service &&
+      side_panel_service->HasSidePanelActionForTab(*extension(), tab_id)) {
+    return true;
+  }
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS)}
+  return false;
 }
 
 bool ExtensionActionViewModel::IsShowingPopup() const {
@@ -414,8 +422,10 @@ void ExtensionActionViewModel::ExecuteUserAction(InvocationSource source) {
     TriggerPopup(PopupShowAction::kShow, kByUser, ShowPopupCallback());
   } else if (action ==
              extensions::ExtensionAction::ShowAction::kToggleSidePanel) {
+#if BUILDFLAG(ENABLE_EXTENSIONS)
     extensions::side_panel_util::ToggleExtensionSidePanel(browser_,
                                                           extension()->id());
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
   }
 }
 
@@ -681,11 +691,15 @@ ExtensionActionViewModel::GetIconImageSource(content::WebContents* web_contents,
   // is disabled.
   bool action_is_visible = extension_action_->GetIsVisible(tab_id);
 
+#if BUILDFLAG(ENABLE_EXTENSIONS)
   extensions::SidePanelService* side_panel_service =
       extensions::SidePanelService::Get(profile_);
   bool has_side_panel_action =
       side_panel_service &&
       side_panel_service->HasSidePanelActionForTab(*extension(), tab_id);
+#else   // BUILDFLAG(ENABLE_EXTENSIONS)
+  bool has_side_panel_action = false;
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
   bool is_grayscale =
       GetSiteInteraction(web_contents) ==
           extensions::SitePermissionsHelper::SiteInteraction::kNone &&
