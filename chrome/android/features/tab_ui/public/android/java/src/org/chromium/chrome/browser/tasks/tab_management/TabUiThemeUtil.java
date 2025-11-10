c5221fe6507daf40026c0fa4120cc4f180d76ed6
Wenyu Fu <wenyufu@chromium.org>
2025-11-10 12:55:18 -0800

    [SurfaceColor] Update tab strip background color
    Change tab strip bg: scHigh -> scHighest to create more contrast with
    the toolbar. Unfocused color remains unchanged.
    
    Preview: https://screenshot.googleplex.com/3A6uadHGfaRGKNB
    Bug: 456666847
    Change-Id: I90cb2749d76e4bd8cec683ceae4c6b84c034062a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7128274
    Reviewed-by: Joel Tan-Aristy <jtanaristy@google.com>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1542734}

--- chrome/android/features/tab_ui/public/android/java/src/org/chromium/chrome/browser/tasks/tab_management/TabUiThemeUtil.java
+++ chrome/android/features/tab_ui/public/android/java/src/org/chromium/chrome/browser/tasks/tab_management/TabUiThemeUtil.java
@@ -68,7 +68,7 @@ public class TabUiThemeUtil {
         if (isIncognito) {
             return ContextCompat.getColor(context, R.color.tab_strip_tablet_bg_incognito);
         }
-        return SemanticColorUtils.getColorSurfaceContainerHigh(context);
+        return SemanticColorUtils.getColorSurfaceContainerHighest(context);
     }
 
     private static @ColorInt int getTabStripBackgroundColorUnfocused(
