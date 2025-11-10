ce9345bee748a9ede65b5d79ea3d11c58ddc32ff
Tomasz Wiszkowski <ender@google.com>
2025-11-10 15:40:12 -0800

    Cleanup: Rename RV NavigationAttachments to FuseboxAttachment classes
    Improves naming consistency in the fusebox attachment feature by using
    the singular 'FuseboxAttachment' naming convention throughout.
    
    - Rename NavigationAttachmentsRecyclerView and Adapter as Fuxebox*
    - Update all references in Java files, XML layouts, and BUILD.gn
    
    Change-Id: I6a7579e52d2a4c586b10ac14dc870ee9dcf5d4e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7138128
    Reviewed-by: Jian Li <jianli@chromium.org>
    Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1542826}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxAttachmentDetailsFetcher.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxAttachmentDetailsFetcher.java
@@ -20,7 +20,7 @@ import org.chromium.base.FileUtils;
 import org.chromium.base.task.AsyncTask;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
-import org.chromium.chrome.browser.omnibox.fusebox.NavigationAttachmentsRecyclerViewAdapter.FuseboxAttachmentType;
+import org.chromium.chrome.browser.omnibox.fusebox.FuseboxAttachmentRecyclerViewAdapter.FuseboxAttachmentType;
 
 import java.io.IOException;
 import java.io.InputStream;
--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxAttachmentRecyclerViewAdapter.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxAttachmentRecyclerViewAdapter.java
@@ -15,9 +15,9 @@ import org.chromium.ui.modelutil.SimpleRecyclerViewAdapter;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-/** ModelListAdapter for NavigationAttachments component. */
+/** ModelListAdapter for FuseboxAttachment component. */
 @NullMarked
-class NavigationAttachmentsRecyclerViewAdapter extends SimpleRecyclerViewAdapter {
+class FuseboxAttachmentRecyclerViewAdapter extends SimpleRecyclerViewAdapter {
     @IntDef({
         FuseboxAttachmentType.ATTACHMENT_FILE,
         FuseboxAttachmentType.ATTACHMENT_IMAGE,
@@ -30,7 +30,7 @@ class NavigationAttachmentsRecyclerViewAdapter extends SimpleRecyclerViewAdapter
         int ATTACHMENT_TAB = 2;
     }
 
-    NavigationAttachmentsRecyclerViewAdapter(ModelList data) {
+    FuseboxAttachmentRecyclerViewAdapter(ModelList data) {
         super(data);
         registerType(
                 FuseboxAttachmentType.ATTACHMENT_FILE,
