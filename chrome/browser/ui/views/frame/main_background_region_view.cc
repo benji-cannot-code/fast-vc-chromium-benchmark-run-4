a2241aadf457221d8a4f6cba60ed17387b083d58
Eshwar Stalin <estalin@chromium.org>
2025-11-10 10:58:55 -0800

    Setting default visibility of MainBackgroundRegionView to hide
    The old layout implementation which is default currently doesn't set the
    visibility of MainBackgroundRegionView. This could cause issues on
    platforms like ChromeOS with a themed image as background and isn't
    great from performance perspective as well. So explicitly setting this
    to hide and letting new layout manage the visibility.
    
    Change-Id: I4089efd837ffd3e73984816320f2ccb6cffadd05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7135560
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1542659}

--- chrome/browser/ui/views/frame/main_background_region_view.cc
+++ chrome/browser/ui/views/frame/main_background_region_view.cc
@@ -10,7 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/canvas.h"
 
 MainBackgroundRegionView::MainBackgroundRegionView(BrowserView& browser_view)
-    : browser_view_(browser_view) {}
+    : browser_view_(browser_view) {
+  SetVisible(false);
+}
 MainBackgroundRegionView::~MainBackgroundRegionView() = default;
 
 void MainBackgroundRegionView::OnPaint(gfx::Canvas* canvas) {
