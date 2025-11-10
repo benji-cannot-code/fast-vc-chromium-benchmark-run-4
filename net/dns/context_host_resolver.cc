a7089d7647d4a4b7ba594bcd391900993eb2e1cd
Yoichi Osato <yoichio@chromium.org>
2025-11-09 23:09:49 -0800

    Add test for ERR_DNS_CACHE_INVALIDATION_IN_PROGRESS case.
    This patch is a follow-up of
    crrev.com/c/7126301/comment/c1154f72_99e49e71/.
    
    This patch adds a ContextHostResolver test for propagating the
    `ERR_DNS_CACHE_INVALIDATION_IN_PROGRESS` error when
    HostResolverManager is `InvalidationInProgress`.
    
    This patch also refactors two ContextHostResolver::CreateRequest()
    by unifying the logic.
    
    Bug: 453732745
    Change-Id: If508a4ddb41c142c44a0c62cfec1e235c980a8bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7135968
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1542423}

--- net/dns/context_host_resolver.cc
+++ net/dns/context_host_resolver.cc
@@ -67,8 +67,8 @@ void ContextHostResolver::OnShutdown() {
 }
 
 std::unique_ptr<HostResolver::ResolveHostRequest>
-ContextHostResolver::CreateRequest(
-    url::SchemeHostPort host,
+ContextHostResolver::CreateRequestInternal(
+    HostResolver::Host host,
     NetworkAnonymizationKey network_anonymization_key,
     NetLogWithSource source_net_log,
     std::optional<ResolveHostParameters> optional_parameters) {
@@ -86,9 +86,21 @@ ContextHostResolver::CreateRequest(
   CHECK(resolve_context_);
 
   return manager_->CreateRequest(
-      Host(std::move(host)), std::move(network_anonymization_key),
+      std::move(host), std::move(network_anonymization_key),
       std::move(source_net_log), std::move(optional_parameters),
       resolve_context_.get());
+}
+
+std::unique_ptr<HostResolver::ResolveHostRequest>
+ContextHostResolver::CreateRequest(
+    url::SchemeHostPort host,
+    NetworkAnonymizationKey network_anonymization_key,
+    NetLogWithSource source_net_log,
+    std::optional<ResolveHostParameters> optional_parameters) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  return CreateRequestInternal(
+      Host(std::move(host)), std::move(network_anonymization_key),
+      std::move(source_net_log), std::move(optional_parameters));
 }
 
 std::unique_ptr<HostResolver::ResolveHostRequest>
@@ -98,16 +110,9 @@ ContextHostResolver::CreateRequest(
     const NetLogWithSource& source_net_log,
     const std::optional<ResolveHostParameters>& optional_parameters) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-
-  if (shutting_down_) {
-    return HostResolver::CreateFailingRequest(ERR_CONTEXT_SHUT_DOWN);
-  }
-
-  CHECK(resolve_context_);
-
-  return manager_->CreateRequest(host, network_anonymization_key,
-                                 source_net_log, optional_parameters,
-                                 resolve_context_.get());
+  return CreateRequestInternal(HostResolver::Host(std::move(host)),
+                               network_anonymization_key, source_net_log,
+                               optional_parameters);
 }
 
 std::unique_ptr<HostResolver::ServiceEndpointRequest>
