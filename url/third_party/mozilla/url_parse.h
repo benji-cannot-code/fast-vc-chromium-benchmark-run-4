80aa435d98d2feeb7e84ea85cc0eaa5a42e65daa
Kent Tamura <tkent@chromium.org>
2025-11-09 20:24:07 -0800

    url: Add AsViewOn() and MaybeAsViewOn() to url::Component
    They're string_view variants of as_string_view_on() and
    maybe_as_string_view_on(), which are unsafe and should be removed.
    
    We have two AsViewOn()s and two MaybeAsViewOn()s. They are for
    std::basic_string_view and std::basic_string. An implicit conversion
    from a std::basic_string to a std::basic_string_view isn't applied in
    this case.
    
    This CL has no behavior changes.
    
    Bug: 350788890
    Change-Id: I006a804f3b51f6be23e569d3aaa052df05760417
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7136431
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Owners-Override: Hayato Ito <hayato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1542399}

--- url/third_party/mozilla/url_parse.h
+++ url/third_party/mozilla/url_parse.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <iosfwd>
 #include <optional>
+#include <string>
 #include <string_view>
 
 #include "base/check.h"
@@ -78,6 +79,21 @@ struct Component {
     return std::basic_string_view(&source[begin], static_cast<size_t>(len));
   }
 
+  // Returns a string_view using `source` as a backend.
+  template <typename CharT>
+  std::basic_string_view<CharT> AsViewOn(
+      std::basic_string_view<CharT> source) const {
+    DCHECK(is_valid());
+    return source.substr(static_cast<size_t>(begin), static_cast<size_t>(len));
+  }
+
+  // Returns a string_view using `source` as a backend.
+  template <typename CharT>
+  std::basic_string_view<CharT> AsViewOn(
+      const std::basic_string<CharT>& source) const {
+    return AsViewOn(std::basic_string_view<CharT>(source));
+  }
+
   // Returns a std::optional<string_view> using `source` as a backend.
   // Returns std::nullopt if the component is invalid.
   template <typename CharT>
@@ -87,6 +103,25 @@ struct Component {
       return std::nullopt;
     }
     return std::basic_string_view(&source[begin], len);
+  }
+
+  // Returns a std::optional<string_view> using `source` as a backend.
+  // Returns std::nullopt if the component is invalid.
+  template <typename CharT>
+  std::optional<std::basic_string_view<CharT>> MaybeAsViewOn(
+      std::basic_string_view<CharT> source) const {
+    if (!is_valid()) {
+      return std::nullopt;
+    }
+    return source.substr(static_cast<size_t>(begin), static_cast<size_t>(len));
+  }
+
+  // Returns a std::optional<string_view> using `source` as a backend.
+  // Returns std::nullopt if the component is invalid.
+  template <typename CharT>
+  std::optional<std::basic_string_view<CharT>> MaybeAsViewOn(
+      const std::basic_string<CharT>& source) const {
+    return MaybeAsViewOn(std::basic_string_view<CharT>(source));
   }
 
   int begin;  // Byte offset in the string of this component.
