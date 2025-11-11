eb38d29736af3b56f62d324976078b206604632f
Bryant Chandler <bryantchandler@google.com>
2025-11-11 15:30:28 -0800

    [GlicMI] Add SidePanelEntryHideReason::kBackgrounded
    This allows subscribers to SidePanelEntryWillHide to differentiate between
    panels being closed because of a tab switch and the other close reasons.
    
    Bug: b:458406475
    Change-Id: Ie4cfb8f69d364729ad30debe9b98816a6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7138263
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Bryan Nguyen <nguyenbryan@google.com>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1543389}

--- chrome/browser/ui/webui_browser/webui_browser_side_panel_ui.cc
+++ chrome/browser/ui/webui_browser/webui_browser_side_panel_ui.cc
@@ -42,12 +42,19 @@ WebUIBrowserSidePanelUI::WebUIBrowserSidePanelUI(Browser* browser)
 WebUIBrowserSidePanelUI::~WebUIBrowserSidePanelUI() = default;
 
 void WebUIBrowserSidePanelUI::Close(SidePanelEntry::PanelType panel_type) {
+  Close(/*suppress_animations=*/true, panel_type,
+        SidePanelEntryHideReason::kSidePanelClosed);
+}
+
+void WebUIBrowserSidePanelUI::Close(bool suppress_animations,
+                                    SidePanelEntry::PanelType panel_type,
+                                    SidePanelEntryHideReason reason) {
   if (!IsSidePanelShowing(panel_type)) {
     return;
   }
 
   if (SidePanelEntry* entry = GetEntryForUniqueKey(*current_key(panel_type))) {
-    entry->OnEntryWillHide(SidePanelEntryHideReason::kSidePanelClosed);
+    entry->OnEntryWillHide(reason);
   }
   // Asynchronously close the side panel in webshell.
   // WebUI then notifies the browser when the side panel is actually closed
@@ -74,11 +81,6 @@ void WebUIBrowserSidePanelUI::DisableAnimationsForTesting() {}
 
 void WebUIBrowserSidePanelUI::SetNoDelaysForTesting(
     bool no_delays_for_testing) {}
-
-void WebUIBrowserSidePanelUI::Close(bool suppress_animations,
-                                    SidePanelEntry::PanelType panel_type) {
-  Close(panel_type);
-}
 
 content::WebContents* WebUIBrowserSidePanelUI::GetWebContentsForId(
     SidePanelEntryId entry_id) const {
@@ -199,7 +201,8 @@ void WebUIBrowserSidePanelUI::MaybeShowEntryOnTabStripModelChanged(
     current_side_panel_view_.reset();
   }
 
-  Close(/*suppress_animations=*/true, panel_type);
+  Close(/*suppress_animations=*/true, panel_type,
+        SidePanelEntryHideReason::kSidePanelClosed);
 }
 
 void WebUIBrowserSidePanelUI::OnSidePanelClosed(
