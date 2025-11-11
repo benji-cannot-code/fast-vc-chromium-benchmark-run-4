d6cfa461594a2e43cde35ca092d4ff71e20f0d2d
Slobodan Pejic <slobodan@chromium.org>
2025-11-11 11:26:28 -0800

    Move FindLastObservedNodeForActionTarget to command location.
    Make FindLastObservedNodeForActionTarget available to more than
    AttemptFormFillingTool.
    
    Following up from review comment:
    https://chromium-review.googlesource.com/c/chromium/src/+/7098938/comment/f45ab9aa_ee1894a9/
    
    Bug: b:452065032
    Change-Id: I3e14ef04cf071c4ebf21a352e281236988b4a791
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7131501
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Slobodan Pejic <slobodan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1543254}

--- chrome/browser/actor/tools/attempt_form_filling_tool.cc
+++ chrome/browser/actor/tools/attempt_form_filling_tool.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/content/browser/page_content_proto_util.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
-#include "third_party/abseil-cpp/absl/functional/overload.h"
 
 namespace actor {
 
@@ -37,22 +36,6 @@ using optimization_guide::proto::AnnotatedPageContent;
 using optimization_guide::proto::FormFillingRequest;
 
 namespace {
-
-std::optional<optimization_guide::TargetNodeInfo>
-FindLastObservedNodeForActionTarget(
-    const optimization_guide::proto::AnnotatedPageContent* apc,
-    const PageTarget& target) {
-  return std::visit(
-      absl::Overload{
-          [&](const DomNode& node) {
-            return FindLastObservedNodeForActionTargetId(apc, node);
-          },
-          [&](const gfx::Point& point) {
-            return FindLastObservedNodeForActionTargetPoint(apc, point);
-          },
-      },
-      target);
-}
 
 FieldGlobalId GetFieldIdFromPageTarget(
     const optimization_guide::proto::AnnotatedPageContent* last_observation,
