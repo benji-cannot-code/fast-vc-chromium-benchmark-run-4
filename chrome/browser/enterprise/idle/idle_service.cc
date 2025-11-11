6b568d6f893b396882717f3c268e5d2884204718
Glenn Hartmann <hartmanng@chromium.org>
2025-11-10 16:42:47 -0800

    [bedrock] Migrate idle_service.cc away from BrowserListObserver.
    Note that this changes the existing observer from listening to all
    browsers globally to just listening to browsers within the relevant
    Profile.
    
    This migration is part of project bedrock to reduce the dependencies on
    Browser and BrowserList. See https://crbug.com/431671320 for more info.
    
    Bug: 459505408
    Change-Id: Ic8d66413d9528ac2fc64bc9f2a6e27916a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7088445
    Commit-Queue: Glenn Hartmann <hartmanng@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1542862}

--- chrome/browser/enterprise/idle/idle_service.cc
+++ chrome/browser/enterprise/idle/idle_service.cc
@@ -24,18 +24,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_list.h"
-#include "chrome/browser/ui/browser_list_observer.h"
+#include "chrome/browser/ui/browser_window/public/browser_collection_observer.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface_iterator.h"
+#include "chrome/browser/ui/browser_window/public/profile_browser_collection.h"
 #include "chrome/browser/ui/idle_bubble.h"
 #endif  // !BUILDFLAG(IS_ANDROID)
 
 namespace enterprise_idle {
 
 #if !BUILDFLAG(IS_ANDROID)
-// Observes OnBrowserSetLastActive(). If the kIdleTimeoutShowBubbleOnStartup
+// Observes OnBrowserActivated(). If the kIdleTimeoutShowBubbleOnStartup
 // pref is true, it shows a bubble when a browser comes into focus. See
 // ShowBubbleAction.
-class IdleService::BrowserObserver : public BrowserListObserver {
+class IdleService::BrowserObserver : public BrowserCollectionObserver {
  public:
   explicit BrowserObserver(Profile* profile) : profile_(profile) {
     CHECK_EQ(profile_->GetOriginalProfile(), profile_);
@@ -43,23 +44,25 @@ class IdleService::BrowserObserver : public BrowserListObserver {
 
   void StartObserving() {
     if (!observation_.IsObserving()) {
-      observation_.Observe(BrowserList::GetInstance());
+      if (profile_->AllowsBrowserWindows()) {
+        observation_.Observe(ProfileBrowserCollection::GetForProfile(profile_));
+      }
       if (BrowserWindowInterface* const bwi =
               GetLastActiveBrowserWindowInterfaceWithAnyProfile()) {
-        OnBrowserSetLastActiveInternal(bwi);
+        OnBrowserActivatedInternal(bwi);
       }
     }
   }
 
   void StopObserving() { observation_.Reset(); }
 
-  // BrowserListObserver:
-  void OnBrowserSetLastActive(Browser* browser) override {
-    OnBrowserSetLastActiveInternal(browser);
+  // BrowserCollectionObserver:
+  void OnBrowserActivated(BrowserWindowInterface* browser) override {
+    OnBrowserActivatedInternal(browser);
   }
 
  private:
-  void OnBrowserSetLastActiveInternal(BrowserWindowInterface* bwi) {
+  void OnBrowserActivatedInternal(BrowserWindowInterface* bwi) {
     CHECK(bwi);
     Profile* const profile = bwi->GetProfile();
     auto* prefs = profile->GetPrefs();
@@ -93,7 +96,8 @@ class IdleService::BrowserObserver : public BrowserListObserver {
   }
 
   const raw_ptr<Profile> profile_;
-  base::ScopedObservation<BrowserList, BrowserListObserver> observation_{this};
+  base::ScopedObservation<ProfileBrowserCollection, BrowserCollectionObserver>
+      observation_{this};
 };
 #else
 // BrowserObserver for Android, to minimize #ifdef hell.
