27aaabaf2d3920484285b620a650e967f9fd25ef
Eain Chen <eainchen@microsoft.com>
2025-11-11 14:05:33 -0800

    Change PreserveDropEffect runtime config to test
    We have received customer feedback regarding Outlook's email writing:
    when a user drags a recipient between the To, Cc, or Bcc fields, the
    recipient is copied to the target field while remaining in the source
    field, creating duplicates. However, it's expected the recipient should
    be removed from the source field. This is related to the
    'PreserveDropEffect' feature. Based on our current investigation, we can
    prevent this behavior by disabling the flag during runtime.
    
    In this CL, we will update this setting’s runtime configuration status
    from stable to test, which could unblock the unexpected duplicate
    recipient behavior for the customer.
    
    Bug: 458617018,434151262
    Change-Id: I41ae1d25cdbe8e0dd069d52ecfa20acf93ca53c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7133850
    Reviewed-by: Mustaq Ahmed <mustaq@chromium.org>
    Reviewed-by: Olga Gerchikov <gerchiko@microsoft.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Eain Chen <eainchen@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1543335}

--- chrome/browser/ui/views/drag_and_drop_interactive_uitest.cc
+++ chrome/browser/ui/views/drag_and_drop_interactive_uitest.cc
@@ -783,6 +783,8 @@ class DragAndDropBrowserTest : public InProcessBrowserTest,
     // targets have not been determined yet, and may interfere with the tests
     // below by shifting the contents around.
     // These overrides should be removed once the parameters are finalized.
+    //
+    // Ensure PreserveDropEffect is enabled for DragAndDropBrowserTest.
     feature_list_.InitWithFeaturesAndParameters(
         {{features::kSideBySide,
           {{features::kSideBySideDropTargetMinWidth.name, "0"},
@@ -791,7 +793,8 @@ class DragAndDropBrowserTest : public InProcessBrowserTest,
           {{features::kSideBySideDropTargetNudgeMinWidth.name, "0"},
            {features::kSideBySideDropTargetNudgeMaxWidth.name, "0"},
            {features::kSideBySideDropTargetNudgeToFullMinWidth.name, "0"},
-           {features::kSideBySideDropTargetNudgeToFullMaxWidth.name, "0"}}}},
+           {features::kSideBySideDropTargetNudgeToFullMaxWidth.name, "0"}}},
+         {blink::features::kPreserveDropEffect, {}}},
         {blink::features::kSupportOpeningDraggedLinksInSameTab});
     InProcessBrowserTest::SetUp();
   }
@@ -1210,6 +1213,9 @@ class DragAndDropDragLinksInSameTabBrowserTest : public DragAndDropBrowserTest {
     // targets have not been determined yet, and may interfere with the tests
     // below by shifting the contents around.
     // These overrides should be removed once the parameters are finalized.
+    //
+    // Ensure PreserveDropEffect is enabled based on the setting of parent class
+    // DragAndDropBrowserTest.
     feature_list_.InitWithFeaturesAndParameters(
         {{features::kSideBySide,
           {{features::kSideBySideDropTargetMinWidth.name, "0"},
@@ -1219,6 +1225,7 @@ class DragAndDropDragLinksInSameTabBrowserTest : public DragAndDropBrowserTest {
            {features::kSideBySideDropTargetNudgeMaxWidth.name, "0"},
            {features::kSideBySideDropTargetNudgeToFullMinWidth.name, "0"},
            {features::kSideBySideDropTargetNudgeToFullMaxWidth.name, "0"}}},
+         {blink::features::kPreserveDropEffect, {}},
          {blink::features::kSupportOpeningDraggedLinksInSameTab, {}}},
         {});
     InProcessBrowserTest::SetUp();
