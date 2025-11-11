6766ea398e5cd048ca165feb6ca0c76531bf635f
Ioana Pandele <ioanap@chromium.org>
2025-11-11 09:12:16 -0800

    Enable field visibility check flag by default
    Bug: 457971819
    Change-Id: I3b957c105a137c11da026b5a33bf8170d2c459f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7141760
    Reviewed-by: Fiorella Barrientos Villalta <fiorellab@google.com>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1543170}

--- components/password_manager/core/browser/fake_form_fetcher.h
+++ components/password_manager/core/browser/fake_form_fetcher.h
@@ -97,6 +97,8 @@ class FakeFormFetcher : public FormFetcher {
 
   void SetBlocklisted(bool is_blocklisted);
 
+  bool HasConsumers();
+
   void NotifyFetchCompleted();
 
   void SetProfileStoreBackendError(
