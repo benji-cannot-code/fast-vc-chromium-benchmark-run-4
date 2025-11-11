ac39b78df85fe3726ec364b15df51f2c886e7dcf
David Bokan <bokan@chromium.org>
2025-11-11 10:57:44 -0800

    actor: Fix openInBackground param for createActorTab
    This is amending a mistake I made in https://crrev.com/c/7129238.  The
    C++ code used foreground but the TS uses opensInBackground and the
    interface between the two mixed up the comparison and accidentally
    inverted it.
    
    This CL updates the C++ side to also use opensInBackground to prevent
    confusion like this. Also adds a test.
    
    (Also fix journal entry name which was a typo copied and pasted)
    
    Bug: b:454325735, 454961534
    Change-Id: I93ebd9979035896b7e7e11dabbabd37f93759f46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7139339
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1543235}

--- chrome/browser/glic/host/glic_page_handler.cc
+++ chrome/browser/glic/host/glic_page_handler.cc
@@ -1119,7 +1119,7 @@ class GlicWebClientHandler : public glic::mojom::WebClientHandler,
   }
 
   void CreateActorTab(int32_t task_id,
-                      bool foreground,
+                      bool open_in_background,
                       std::optional<int32_t> initiator_tab_id,
                       std::optional<int32_t> initiator_window_id,
                       CreateActorTabCallback callback) override {
@@ -1129,7 +1129,7 @@ class GlicWebClientHandler : public glic::mojom::WebClientHandler,
       return;
     }
     host().instance_delegate().CreateActorTab(
-        actor::TaskId(task_id), foreground, initiator_tab_id,
+        actor::TaskId(task_id), open_in_background, initiator_tab_id,
         initiator_window_id, std::move(callback));
   }
 
