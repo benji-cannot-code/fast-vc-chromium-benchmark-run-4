dabbda1a0fe627749d0a22928691ae3d379c5f99
Daniel Rubery <drubery@chromium.org>
2025-11-10 18:52:35 -0800

    Roll quote: 1.0.41 => 1.0.42 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * quote: 1.0.41 => 1.0.42; https://docs.rs/crate/quote/1.0.42
    
    Bug: None
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Change-Id: I8323d4fdacb8165cf72a3043cc796ccb65fcf3cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7136420
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1542920}

--- third_party/rust/chromium_crates_io/vendor/quote-v1/src/lib.rs
+++ third_party/rust/chromium_crates_io/vendor/quote-v1/src/lib.rs
@@ -90,10 +90,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //! [prettyplease]: https://github.com/dtolnay/prettyplease
 
 // Quote types in rustdoc of other crates get linked to here.
-#![doc(html_root_url = "https://docs.rs/quote/1.0.41")]
+#![doc(html_root_url = "https://docs.rs/quote/1.0.42")]
 #![allow(
     clippy::doc_markdown,
     clippy::elidable_lifetime_names,
+    clippy::items_after_statements,
     clippy::missing_errors_doc,
     clippy::missing_panics_doc,
     clippy::module_name_repetitions,
