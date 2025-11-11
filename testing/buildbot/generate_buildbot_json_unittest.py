30207bfc641fa4a487ab3b9889de75f2056c5d3f
Ben Joyce <bjoyce@google.com>
2025-11-11 10:59:40 -0800

    Add support for module_scheme in generate_buildbot_json
    This is required for structured test ids with resultdb to be on par with the module_scheme support in starlark. A Module_scheme arg can be added to the gn_isolate_map.pyl or the test_suites.pyl file to get it added to the builder spec.
    
    This change will allow internal changes to the spec file such as:
    
    https://chrome-internal-review.googlesource.com/c/clank/internal/apps/+/8754883
    
    https://chrome-internal-review.googlesource.com/c/chrome/src-internal/+/8749116
    
    NO_IFTTT=Non starlark change, but similar support is in https://chromium-review.googlesource.com/c/infra/chromium/+/7004570
    
    Bug: 428963926
    Change-Id: I5fbaa91dfeafe8f6c0e2e875fccf9db9af681a8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6697156
    Commit-Queue: Benjamin Joyce (Ben) <bjoyce@google.com>
    Reviewed-by: Garrett Beaty <gbeaty@google.com>
    Cr-Commit-Position: refs/heads/main@{#1543236}

--- testing/buildbot/generate_buildbot_json_unittest.py
+++ testing/buildbot/generate_buildbot_json_unittest.py
@@ -1703,6 +1703,17 @@ GN_ISOLATE_MAP="""\
   'foo_test': {
     'label': '//chrome/test:foo_test',
     'type': 'windowed_test_launcher',
+    'module_name': '//chrome/test:foo_test',
+  }
+}
+"""
+
+GN_ISOLATE_MAP_MODULE_SCHEME = """\
+{
+  'foo_test': {
+    'label': '//chrome/test:foo_test',
+    'type': 'windowed_test_launcher',
+    'module_scheme': 'gtest',
   }
 }
 """
@@ -1712,6 +1723,7 @@ GPU_TELEMETRY_GN_ISOLATE_MAP="""\
   'telemetry_gpu_integration_test': {
     'label': '//chrome/test:telemetry_gpu_integration_test',
     'type': 'script',
+    'module_name': '//chrome/test:telemetry_gpu_integration_test',
       }
 }
 """
@@ -1721,6 +1733,7 @@ GPU_TELEMETRY_GN_ISOLATE_MAP_ANDROID = """\
   'telemetry_gpu_integration_test_android_chrome': {
     'label': '//chrome/test:telemetry_gpu_integration_test_android_chrome',
     'type': 'script',
+    'module_name': '//chrome/test:telemetry_gpu_integration_test_android_chrome',
       }
 }
 """
@@ -1730,6 +1743,7 @@ GPU_TELEMETRY_GN_ISOLATE_MAP_ANDROID_WEBVIEW = """\
   'telemetry_gpu_integration_test_android_webview': {
     'label': '//chrome/test:telemetry_gpu_integration_test_android_webview',
     'type': 'script',
+    'module_name': '//chrome/test:telemetry_gpu_integration_test_android_webview',
       }
 }
 """
@@ -1739,6 +1753,7 @@ GPU_TELEMETRY_GN_ISOLATE_MAP_FUCHSIA = """\
   'telemetry_gpu_integration_test_fuchsia': {
     'label': '//chrome/test:telemetry_gpu_integration_test_fuchsia',
     'type': 'script',
+    'module_name': '//chrome/test:telemetry_gpu_integration_test_fuchsia',
       }
 }
 """
@@ -1748,6 +1763,18 @@ GPU_TELEMETRY_GN_ISOLATE_MAP_CAST_STREAMING = """\
   'telemetry_gpu_integration_test_fuchsia': {
     'label': '//chrome/test:telemetry_gpu_integration_test_fuchsia',
     'type': 'script',
+    'module_name': '//chrome/test:telemetry_gpu_integration_test_fuchsia',
+      }
+}
+"""
+
+GPU_TELEMETRY_GN_ISOLATE_MAP_MODULE_SCHEME = """\
+{
+  'telemetry_gpu_integration_test': {
+    'label': '//chrome/test:telemetry_gpu_integration_test',
+    'type': 'script',
+    'module_name': '//chrome/test:telemetry_gpu_integration_test',
+    'module_scheme': 'single',
       }
 }
 """
@@ -1757,6 +1784,7 @@ GN_ISOLATE_MAP_KEY_LABEL_MISMATCH="""\
   'foo_test': {
     'label': '//chrome/test:foo_test_tmp',
     'type': 'windowed_test_launcher',
+    'module_scheme': '//chrome/test:foo_test_tmp',
   }
 }
 """
@@ -1766,6 +1794,7 @@ GN_ISOLATE_MAP_USING_IMPLICIT_NAME="""\
   'foo_test': {
     'label': '//chrome/foo_test',
     'type': 'windowed_test_launcher',
+    'module_scheme': '//chrome/foo_test',
   }
 }
 """
@@ -1932,6 +1961,15 @@ class UnitTest(TestCase):
         'Malformed.*//chrome/foo_test.*for key.*'
         'foo_test.*'):
       fbb.check_input_file_consistency(verbose=True)
+    self.assertFalse(fbb.printed_lines)
+
+  def test_gn_isolate_map_with_module_scheme(self):
+    fbb = FakeBBGen(self.args,
+                    FOO_GTESTS_WATERFALL,
+                    FOO_TEST_SUITE,
+                    LUCI_MILO_CFG,
+                    gn_isolate_map=GN_ISOLATE_MAP_MODULE_SCHEME)
+    fbb.check_output_file_consistency(verbose=True)
     self.assertFalse(fbb.printed_lines)
 
   def test_noop_exception_does_nothing(self):
@@ -2140,6 +2178,16 @@ class UnitTest(TestCase):
                     LUCI_MILO_CFG,
                     exceptions=NO_BAR_TEST_EXCEPTIONS,
                     gn_isolate_map=GPU_TELEMETRY_GN_ISOLATE_MAP)
+    fbb.check_output_file_consistency(verbose=True)
+    self.assertFalse(fbb.printed_lines)
+
+  def test_gpu_telemetry_tests_module_scheme(self):
+    fbb = FakeBBGen(self.args,
+                    FOO_GPU_TELEMETRY_TEST_WATERFALL,
+                    COMPOSITION_SUITE_WITH_TELEMETRY_TEST,
+                    LUCI_MILO_CFG,
+                    exceptions=NO_BAR_TEST_EXCEPTIONS,
+                    gn_isolate_map=GPU_TELEMETRY_GN_ISOLATE_MAP_MODULE_SCHEME)
     fbb.check_output_file_consistency(verbose=True)
     self.assertFalse(fbb.printed_lines)
 
