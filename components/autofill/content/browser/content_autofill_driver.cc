a6ce51025898a90159b0e68dc9193c80f6886589
Christoph Schwering <schwering@google.com>
2025-11-10 17:40:27 -0800

    [Autofill] Add AutofillDriver::IsEmbedded()
    Bug: 459210100
    Change-Id: I489359facddbd94a8b968402ff986c48bcbabcc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7131805
    Reviewed-by: Olivia Saul <osaul@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1542890}

--- components/autofill/content/browser/content_autofill_driver.cc
+++ components/autofill/content/browser/content_autofill_driver.cc
@@ -396,6 +396,15 @@ ContentAutofillDriver* ContentAutofillDriver::GetParent() {
   return GetForRenderFrameHost(parent_rfh);
 }
 
+bool ContentAutofillDriver::IsActive() const {
+  return render_frame_host_->IsActive();
+}
+
+bool ContentAutofillDriver::IsEmbedded() const {
+  return render_frame_host_->GetMainFrame() !=
+         render_frame_host_->GetOutermostMainFrameOrEmbedder();
+}
+
 ContentAutofillClient& ContentAutofillDriver::GetAutofillClient() {
   return owner_->client();
 }
@@ -426,10 +435,6 @@ ukm::SourceId ContentAutofillDriver::GetPageUkmSourceId() const {
   CHECK(!render_frame_host_->IsInLifecycleState(
           content::RenderFrameHost::LifecycleState::kPrerendering));
   return render_frame_host_->GetPageUkmSourceId();
-}
-
-bool ContentAutofillDriver::IsActive() const {
-  return render_frame_host_->IsActive();
 }
 
 bool ContentAutofillDriver::HasSharedAutofillPermission() const {
--- components/autofill/ios/browser/autofill_driver_ios.mm
+++ components/autofill/ios/browser/autofill_driver_ios.mm
@@ -199,6 +199,14 @@ AutofillDriverIOS* AutofillDriverIOS::GetParent() {
   return parent_.get();
 }
 
+bool AutofillDriverIOS::IsActive() const {
+  return true;  // iOS has no MPArch.
+}
+
+bool AutofillDriverIOS::IsEmbedded() const {
+  return false;  // iOS has no MPArch.
+}
+
 AutofillClient& AutofillDriverIOS::GetAutofillClient() {
   return *client_;
 }
@@ -209,11 +217,6 @@ BrowserAutofillManager& AutofillDriverIOS::GetAutofillManager() {
 
 ukm::SourceId AutofillDriverIOS::GetPageUkmSourceId() const {
   return ukm::GetSourceIdForWebStateDocument(web_state_);
-}
-
-// Return true as iOS has no MPArch.
-bool AutofillDriverIOS::IsActive() const {
-  return true;
 }
 
 bool AutofillDriverIOS::HasSharedAutofillPermission() const {
