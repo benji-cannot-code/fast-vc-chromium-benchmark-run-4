a83750321869253d148e461c78fab6ec21240e27
Sky Malice <skym@chromium.org>
2025-11-11 15:41:51 -0800

    [AIM] Restrict file picker to PDFs.
    Bug: 457478003
    Change-Id: I42a4c7c3e48e89fa3937be99d530039bae08e48a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7139118
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Jian Li <jianli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1543393}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/NavigationAttachmentsMediatorUnitTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/NavigationAttachmentsMediatorUnitTest.java
@@ -20,6 +20,7 @@ import static org.mockito.Mockito.verify;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.view.LayoutInflater;
@@ -31,6 +32,8 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
@@ -48,6 +51,7 @@ import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.components.omnibox.AutocompleteRequestType;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.ui.base.Clipboard;
+import org.chromium.ui.base.MimeTypeUtils;
 import org.chromium.ui.base.TestActivity;
 import org.chromium.ui.base.WindowAndroid;
 import org.chromium.ui.modelutil.MVCListAdapter;
@@ -71,6 +75,8 @@ public class NavigationAttachmentsMediatorUnitTest {
     @Mock private Tab mTab1;
     @Mock private Tab mTab2;
     @Mock private WebContents mWebContents;
+
+    @Captor private ArgumentCaptor<Intent> mIntentCaptor;
 
     private ActivityController<TestActivity> mActivityController;
     private Context mContext;
@@ -379,6 +385,13 @@ public class NavigationAttachmentsMediatorUnitTest {
         doReturn(false).when(mComposeBoxQueryControllerBridge).isCreateImagesEligible();
         recreateMediator();
         assertFalse(mModel.get(NavigationAttachmentsProperties.POPUP_CREATE_IMAGE_BUTTON_VISIBLE));
+    }
+
+    @Test
+    public void onFilePickerClicked_setsMimeType() {
+        mModel.get(NavigationAttachmentsProperties.POPUP_FILE_CLICKED).run();
+        verify(mWindowAndroid).showCancelableIntent(mIntentCaptor.capture(), any(), any());
+        assertEquals(MimeTypeUtils.PDF_MIME_TYPE, mIntentCaptor.getValue().getType());
     }
 
     @Test
