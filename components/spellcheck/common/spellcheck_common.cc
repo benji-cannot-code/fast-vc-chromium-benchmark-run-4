6133d52db39c2d1717270cd24d624e8206d166d1
Henrique Ferreiro <hferreiro@igalia.com>
2025-11-11 02:55:49 -0800

    Remove unused spellcheck::IsValidRegion
    This function was added at https://crrev.com/851f97db but never actually
    used. Also, use an anonymous namespace for private code.
    
    Change-Id: I65c1dc1f14d784e2383cc45ab537bc5dc672841c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7137660
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1543039}

--- components/spellcheck/common/spellcheck_common.cc
+++ components/spellcheck/common/spellcheck_common.cc
@@ -19,6 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace spellcheck {
 
+namespace {
+
 struct LanguageRegion {
   const char* language;         // The language.
   const char* language_region;  // language & region, used by dictionaries.
@@ -93,14 +95,6 @@ static constexpr LanguageRegion kSupportedSpellCheckerLanguages[] = {
     // clang-format on
 };
 
-bool IsValidRegion(const std::string& region) {
-  for (const auto& lang_region : kSupportedSpellCheckerLanguages) {
-    if (lang_region.language_region == region)
-      return true;
-  }
-  return false;
-}
-
 // This function returns the language-region version of language name.
 // e.g. returns hi-IN for hi.
 std::string GetSpellCheckLanguageRegion(std::string_view input_language) {
@@ -111,6 +105,8 @@ std::string GetSpellCheckLanguageRegion(std::string_view input_language) {
 
   return std::string(input_language);
 }
+
+}  // namespace
 
 base::FilePath GetVersionedFileName(std::string_view input_language,
                                     const base::FilePath& dict_dir) {
