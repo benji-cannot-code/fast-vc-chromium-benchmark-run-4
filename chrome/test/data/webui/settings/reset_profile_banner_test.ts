30bc031f0627c9c2adc9f4b940cb98f150892996
Nicola Tommasi <tommasin@chromium.org>
2025-11-11 00:26:24 -0800

    [WebUI][CodeHealth]: Import loadTimeData from settings.js
    Standardize loadTimeData import in WebUI Settings tests to prevent
    silent data population failures.
    
    Bug: 382645171
    Change-Id: I6f61a388ae1c2104bdbfe9212e529aeb5c06dd41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7139420
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Nicola Tommasi <tommasin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1543000}

--- chrome/test/data/webui/settings/reset_profile_banner_test.ts
+++ chrome/test/data/webui/settings/reset_profile_banner_test.ts
@@ -4,10 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 // clang-format off
-import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import type {SettingsResetProfileBannerElement} from 'chrome://settings/settings.js';
-import {ResetBrowserProxyImpl, Router, routes} from 'chrome://settings/settings.js';
+import {loadTimeData, ResetBrowserProxyImpl, Router, routes} from 'chrome://settings/settings.js';
 import {assertEquals, assertFalse, assertNotEquals, assertTrue} from 'chrome://webui-test/chai_assert.js';
 
 import {TestResetBrowserProxy} from './test_reset_browser_proxy.js';
