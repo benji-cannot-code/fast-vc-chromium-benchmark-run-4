5d024b16fed6e60cf965a700eb6073d253703dfa
Timofey Chudakov <tchudakov@google.com>
2025-11-10 23:20:38 -0800

    Use ChipView::setIconWithTint in ContextMenuChipController.
    ChipView::setIcon got deprecated because:
    a) ChipView::setIcon(icon, false) always resets the icon's tint instead
       of preserving it.
    b) ChipView::setIcon sets the tint on the ImageView rather than the
       underlying drawable. Once set, the tint on the ImageView cannot be
       completely removed: ImageView::setImageTintList(null) will remove the
       tint from the icon icon instead of using the original icon's tint.
    
    iconResourceId is never set in production code, so this change is a
    no-op.
    
    Bug: 454608496
    Change-Id: If1bbc42d5f4146b15530ada78bdb06dc7c634ede
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7137882
    Reviewed-by: Jenna Himawan <jhimawan@google.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1542987}

--- chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ContextMenuChipController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ContextMenuChipController.java
@@ -159,7 +159,8 @@ class ContextMenuChipController implements View.OnClickListener {
                 .setMaxWidth(getChipTextMaxWidthPx(chipRenderParams.isRemoveIconHidden));
 
         if (chipRenderParams.iconResourceId != 0) {
-            mChipView.setIcon(chipRenderParams.iconResourceId, false);
+            mChipView.setIconWithTint(
+                    chipRenderParams.iconResourceId, /* tintWithTextColor= */ false);
         }
 
         if (!chipRenderParams.isRemoveIconHidden) {
