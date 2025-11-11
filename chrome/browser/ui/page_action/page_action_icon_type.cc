c727ef70e79f52275a4168028b54c50798733fbb
Steven Luong <stluong@chromium.org>
2025-11-11 14:03:08 -0800

    Adds page action chip to toggle the toolbar height side panel
    Adds a page action chip that toggles the toolbar toolbar height side
    panel to open and close.
    
    Bug: 457820269
    Change-Id: I3c63bb17a7d73de33e32618c649410eb1db204d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7121859
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Steven Luong <stluong@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1543332}

--- chrome/browser/ui/page_action/page_action_icon_type.cc
+++ chrome/browser/ui/page_action/page_action_icon_type.cc
@@ -62,6 +62,8 @@ const base::FeatureParam<bool>* GetPageActionsMigrationParam(
     case PageActionIconType::kSaveIban:
     case PageActionIconType::kSaveCard:
       return &features::kPageActionsMigrationSavePayments;
+    case PageActionIconType::kContextualSidePanel:
+      return &features::kPageActionsContextualSidePanel;
     default:
       return nullptr;
   }
--- chrome/browser/ui/views/page_action/page_action_icon_controller.cc
+++ chrome/browser/ui/views/page_action/page_action_icon_controller.cc
@@ -291,6 +291,9 @@ void PageActionIconController::Init(const PageActionIconParams& params,
       case PageActionIconType::kReadingMode:
         // Do nothing as this action was added after the migration.
         break;
+      case PageActionIconType::kContextualSidePanel:
+        // Do nothing as this action was added after the migration.
+        break;
     }
   }
 
