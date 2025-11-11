bfc22abf6f6c8e1bbccc6b8cbed77e3352dabbcd
Eliot Courtney <edcourtney@google.com>
2025-11-10 18:30:38 -0800

    Plumb additional media sharing parameters.
    Bug: 352187279
    Test: CQ + manual test
    Change-Id: I961337dfc47d73ae272a9499cbafbecc6b5b3376
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7130558
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Eliot Courtney <edcourtney@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Jordan Bayles <jophba@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1542909}

--- chrome/browser/media/android/media_capture_picker_manager_bridge.h
+++ chrome/browser/media/android/media_capture_picker_manager_bridge.h
@@ -14,10 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/media/webrtc/desktop_media_picker.h"
 #include "content/public/browser/desktop_media_id.h"
 
-namespace content {
-class WebContents;
-}  // namespace content
-
 class MediaCapturePickerManagerBridge {
  public:
   MediaCapturePickerManagerBridge();
@@ -32,9 +28,7 @@ class MediaCapturePickerManagerBridge {
 
   // Shows a dialog to select a media source to capture. The initiator (not
   // necessarily target) of the capture request is `web_contents`.
-  void Show(content::WebContents* web_contents,
-            const std::u16string& app_name,
-            bool request_audio,
+  void Show(const DesktopMediaPicker::Params& params,
             DesktopMediaPicker::DoneCallback callback);
 
   // Called from Java via JNI when the dialog resolves.
