2ee51a250273f84ba095ffd9bbc14a92ed1b8097
Andrew Williams <awillia@chromium.org>
2025-11-11 06:07:07 -0800

    Make IdentityManager creation CHECK fatal
    The CHECK has been in the code for a while with no crash reports, so it
    seems safe to make it fatal
    
    Fixed: 436208345
    Change-Id: If29c62f74f29927eb568ac88f07ce63dc3d98b21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7131969
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Andrew Williams <awillia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1543083}

--- chrome/browser/profiles/profile_impl.cc
+++ chrome/browser/profiles/profile_impl.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/compiler_specific.h"
 #include "base/containers/contains.h"
-#include "base/debug/dump_without_crashing.h"
 #include "base/environment.h"
 #include "base/files/file.h"
 #include "base/files/file_path.h"
@@ -1121,8 +1120,7 @@ void ProfileImpl::OnLocaleReady(CreateMode create_mode) {
   // Check that the IdentityManager was not created before the browser context
   // services were created. This ensures that browser tests can override the
   // IdentityManager with a fake.
-  CHECK(!IdentityManagerFactory::GetForProfileIfExists(this),
-        base::NotFatalUntil::M160);
+  CHECK(!IdentityManagerFactory::GetForProfileIfExists(this));
 
   BrowserContextDependencyManager::GetInstance()->CreateBrowserContextServices(
       this);
