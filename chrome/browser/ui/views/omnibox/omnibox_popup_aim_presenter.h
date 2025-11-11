a722cb6298ed050f6d2e788e7b7bd848826f8542
David Yeung <dayeung@chromium.org>
2025-11-11 13:06:43 -0800

    [Omnibox AIM] Compose widget will hide on widget focus loss
    There is a listener now for widget de-activation. This enables scenarios
    like closing the compose dialog when losing focus by clicking the
    tabstrip or content area.
    
    Bug: b/454970531
    Change-Id: I17ba04e85527d34c38492aae2c4e68ada7d28022
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7139803
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1543299}

--- chrome/browser/ui/views/omnibox/omnibox_popup_aim_presenter.h
+++ chrome/browser/ui/views/omnibox/omnibox_popup_aim_presenter.h
@@ -6,17 +6,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_OMNIBOX_OMNIBOX_POPUP_AIM_PRESENTER_H_
 #define CHROME_BROWSER_UI_VIEWS_OMNIBOX_OMNIBOX_POPUP_AIM_PRESENTER_H_
 
-#include <cstddef>
-#include <optional>
-
+#include "base/scoped_observation.h"
 #include "chrome/browser/ui/views/omnibox/omnibox_popup_presenter_base.h"
+#include "ui/views/widget/widget.h"
+#include "ui/views/widget/widget_observer.h"
 
 class LocationBarView;
 class OmniboxController;
 
 // Implements subclass of OmniboxPopupPresenterBase to present the AI-Mode
 // compose-plate into an Omnibox popup.
-class OmniboxPopupAimPresenter : public OmniboxPopupPresenterBase {
+class OmniboxPopupAimPresenter : public OmniboxPopupPresenterBase,
+                                 public views::WidgetObserver {
  public:
   OmniboxPopupAimPresenter(LocationBarView* location_bar_view,
                            OmniboxController* controller);
@@ -24,9 +25,21 @@ class OmniboxPopupAimPresenter : public OmniboxPopupPresenterBase {
   OmniboxPopupAimPresenter& operator=(const OmniboxPopupAimPresenter&) = delete;
   ~OmniboxPopupAimPresenter() override;
 
+  // OmniboxPopupPresenterBase:
+  void Show() override;
+  void Hide() override;
+
  protected:
   // OmniboxPopupPresenterBase overrides:
   void WidgetDestroyed() override;
+
+ private:
+  // views::WidgetObserver:
+  void OnWidgetActivationChanged(views::Widget* widget, bool active) override;
+
+  const raw_ptr<OmniboxController> controller_;
+  base::ScopedObservation<views::Widget, views::WidgetObserver>
+      widget_observation_{this};
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_OMNIBOX_OMNIBOX_POPUP_AIM_PRESENTER_H_
