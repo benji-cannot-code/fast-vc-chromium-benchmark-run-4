b924458c65b75d1bfc2bd8180b514b409208d06d
Jenna Himawan <jhimawan@google.com>
2025-11-11 12:08:38 -0800

    Add feature flag for cross-platform synced setup
    Fixed: 459799872
    Change-Id: I1107a5556adc8b89e8f0fc013a7a6ee3211a8c7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7142658
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1543276}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -461,6 +461,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kUseLibunwindstackNativeUnwinderAndroid,
     &kWebApkMinShellVersion,
     &kWebOtpCrossDeviceSimpleString,
+    &kXplatSyncedSetup,
     &language::kCctAutoTranslate,
     &language::kDetailedLanguageSettings,
     &media::kAutoPictureInPictureAndroid,
@@ -829,6 +830,7 @@ BASE_FEATURE(kUseLibunwindstackNativeUnwinderAndroid, base::FEATURE_ENABLED_BY_D
 // Shows only the remote device name on the Android notification instead of
 // a descriptive text.
 BASE_FEATURE(kWebOtpCrossDeviceSimpleString, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kXplatSyncedSetup, base::FEATURE_DISABLED_BY_DEFAULT);
 // go/keep-sorted end
 // BASE_FEATURE_END
 
--- chrome/browser/flags/android/chrome_feature_list.h
+++ chrome/browser/flags/android/chrome_feature_list.h
@@ -300,6 +300,7 @@ BASE_DECLARE_FEATURE(kUseActivityManagerForTabActivation);
 BASE_DECLARE_FEATURE(kUseInitialNetworkStateAtStartup);
 BASE_DECLARE_FEATURE(kUseLibunwindstackNativeUnwinderAndroid);
 BASE_DECLARE_FEATURE(kWebOtpCrossDeviceSimpleString);
+BASE_DECLARE_FEATURE(kXplatSyncedSetup);
 // go/keep-sorted end
 // BASE_DECLARE_FEATURE_END
 
