703895c5e33156587fe3678be47d1051b1096db1
Michael Wojcicka <mwoj@google.com>
2025-11-11 15:44:36 -0800

    [glic] Refactor AppPlatformMetricsServiceTestBase
    Bug: b:458771561
    Change-Id: If609a4110b7aa93250f2994bfa2848c9896211df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7132860
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Auto-Submit: Michael Wojcicka <mwoj@google.com>
    Commit-Queue: Michael Wojcicka <mwoj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1543396}

--- chrome/browser/apps/app_service/metrics/app_platform_metrics_service_unittest.cc
+++ chrome/browser/apps/app_service/metrics/app_platform_metrics_service_unittest.cc
@@ -2769,11 +2769,6 @@ TEST_F(AppDiscoveryMetricsTest, AppActivityMetricsRecordedForTwoInstances) {
 class AppPlatformMetricsServiceObserverTest
     : public AppPlatformMetricsServiceTestBase {
  protected:
-  void SetUp() override {
-    // Set up test user.
-    AddRegularUser("test@test.com");
-  }
-
   MockObserver* observer() { return &observer_; }
 
  private:
