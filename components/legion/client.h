81d39bc802691a152b3a3f053a95410e640fb81e
Nikhil Jakhar <nikhiljakhar@google.com>
2025-11-11 06:19:38 -0800

    legion: Add request_id for request-response mapping in Client
    This change introduces a mechanism to reliably map server responses to
    their corresponding requests in the Legion client and added unit test
    for the same. Refactored unit tests to reuse code.
    
    Bug: b:444610593
    Change-Id: Ia77f7df89d1bcc54d19ac1dfb08a5dbfd6b1d172
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7138140
    Commit-Queue: Nikhil Jakhar <nikhiljakhar@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Auto-Submit: Nikhil Jakhar <nikhiljakhar@google.com>
    Reviewed-by: Oleh Lamzin <lamzin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1543086}

--- components/legion/client.h
+++ components/legion/client.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_LEGION_CLIENT_H_
 #define COMPONENTS_LEGION_CLIENT_H_
 
+#include <cstdint>
 #include <memory>
 #include <string>
 
@@ -58,8 +59,6 @@ class Client {
 
   Client(const Client&) = delete;
   Client& operator=(const Client&) = delete;
-  Client(Client&&);
-  Client& operator=(Client&&);
 
   // Sends a request with a single text content.
   void SendTextRequest(const std::string& text,
@@ -84,6 +83,7 @@ class Client {
 
   std::unique_ptr<SecureChannel> secure_channel_;
   proto::FeatureName feature_name_;
+  int32_t next_request_id_{1};
 };
 
 }  // namespace legion
