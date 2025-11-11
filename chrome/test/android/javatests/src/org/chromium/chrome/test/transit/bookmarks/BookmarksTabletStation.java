ee16369cb81a66f7efa74ef9c91bdd8880bce100
Henrique Nakashima <hnakashima@chromium.org>
2025-11-11 14:45:20 -0800

    [Android] Create Bookmarks Stations
    Different Stations are needed for Phones and Tablets because the
    Activity is different. We can use a facade later to abstract this away.
    
    Add test to TabbedAppMenuPTTest to enter bookmarks.
    
    Bug: 459875984
    Change-Id: Id3462ed6dcd6af8161a02cb3db85d44d56017677
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7142030
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Owners-Override: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1543354}

---
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/transit/bookmarks/BookmarksTabletStation.java
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.test.transit.bookmarks;
+
+import static org.chromium.base.test.transit.Condition.whetherEquals;
+import static org.chromium.base.test.transit.SimpleConditions.uiThreadCondition;
+
+import org.chromium.base.test.transit.Element;
+import org.chromium.chrome.browser.bookmarks.BookmarkPage;
+import org.chromium.chrome.test.transit.page.CtaPageStation;
+import org.chromium.chrome.test.transit.page.NativePageCondition;
+
+/** The Bookmarks page in tablets and desktop. */
+public class BookmarksTabletStation extends CtaPageStation {
+    public Element<BookmarkPage> nativePageElement;
+
+    public static Builder<BookmarksTabletStation> newBuilder() {
+        return new Builder<>(BookmarksTabletStation::new);
+    }
+
+    public BookmarksTabletStation(Config config) {
+        super(config);
+
+        nativePageElement =
+                declareEnterConditionAsElement(
+                        new NativePageCondition<>(BookmarkPage.class, loadedTabElement));
+        declareEnterCondition(
+                uiThreadCondition(
+                        "NativePage has title \"Bookmarks\"",
+                        nativePageElement,
+                        nativePage -> whetherEquals("Bookmarks", nativePage.getTitle())));
+    }
+}
