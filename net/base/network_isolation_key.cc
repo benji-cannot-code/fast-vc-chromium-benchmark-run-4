98d40ce0ddb7ad49a3c099298087da77b0e4f75e
Tsuyoshi Horo <horo@chromium.org>
2025-11-10 16:39:31 -0800

    Make net::IsolationInfo/NIK/NAK cheap to copy
    IsolationInfo, NetworkIsolationKey and NetworkAnonymizationKey were
    unnecessarily expensive to copy due to their data members (e.g.,
    url::Origin). These objects are frequently passed around and copied in
    various parts of the codebase, leading to redundant operations and
    potential for optimization. Notably, the `isolation_info` within
    `URLLoaderFactoryParams` is copied every time for subresource requests,
    which is particularly wasteful.
    
    This change is to introduce ref-counted `Data` members for these
    classes. As a result, copying these objects becomes a cheap ref-count
    operation instead of a deep copy of all members. This eliminates
    unnecessary overhead.
    
    Each class now holds its data in a non-null, ref-counted `Data` member
    making them cheap to copy:
      - IsolationInfo::Data contains:
        - RequestType request_type
        - std::optional<url::Origin> top_frame_origin
        - std::optional<url::Origin> frame_origin
        - std::optional<FrameAncestorRelation> frame_ancestor_relation
        - SiteForCookies site_for_cookies
        - NetworkIsolationKey network_isolation_key
        - NetworkAnonymizationKey network_anonymization_key
      - NetworkIsolationKey::Data contains:
        - std::optional<SchemefulSite> top_frame_site
        - std::optional<SchemefulSite> frame_site
        - std::optional<base::UnguessableToken> nonce
        - NetworkIsolationPartition network_isolation_partition
      - NetworkAnonymizationKey::Data contains:
        - std::optional<SchemefulSite> top_frame_site
        - bool is_cross_site
        - std::optional<base::UnguessableToken> nonce
        - NetworkIsolationPartition network_isolation_partition
    
    Bug: 458213459
    Change-Id: I55142d6740090829cc4a06d11c4d282409a9deb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7122882
    Reviewed-by: mmenke <mmenke@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1542860}

--- net/base/network_isolation_key.cc
+++ net/base/network_isolation_key.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 #include <string>
 
+#include "base/check.h"
+#include "base/no_destructor.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/unguessable_token.h"
@@ -23,10 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace net {
 
 namespace {
-
-std::string GetSiteDebugString(const std::optional<SchemefulSite>& site) {
-  return site ? site->GetDebugString() : "null";
-}
 
 std::string GetNetworkIsolationPartitionStringForCacheKey(
     NetworkIsolationPartition network_isolation_partition) {
@@ -51,20 +49,26 @@ NetworkIsolationKey::NetworkIsolationKey(
     SchemefulSite&& frame_site,
     std::optional<base::UnguessableToken>&& nonce,
     NetworkIsolationPartition network_isolation_partition)
-    : top_frame_site_(std::move(top_frame_site)),
-      frame_site_(std::make_optional(std::move(frame_site))),
-      nonce_(std::move(nonce)),
-      network_isolation_partition_(network_isolation_partition) {
-  DCHECK(!nonce_ || !nonce_->is_empty());
-}
+    : NetworkIsolationKey(
+          base::MakeRefCounted<Data>(std::move(top_frame_site),
+                                     std::move(frame_site),
+                                     std::move(nonce),
+                                     network_isolation_partition)) {}
 
-NetworkIsolationKey::NetworkIsolationKey() = default;
+NetworkIsolationKey::NetworkIsolationKey()
+    : NetworkIsolationKey(Data::GetEmptyData()) {}
 
 NetworkIsolationKey::NetworkIsolationKey(
     const NetworkIsolationKey& network_isolation_key) = default;
 
 NetworkIsolationKey::NetworkIsolationKey(
     NetworkIsolationKey&& network_isolation_key) = default;
+
+NetworkIsolationKey::NetworkIsolationKey(const scoped_refptr<const Data>& data)
+    : data_(data) {
+  CHECK(data_);
+  CHECK(!data_->nonce() || !data_->nonce()->is_empty());
+}
 
 NetworkIsolationKey::~NetworkIsolationKey() = default;
 
@@ -81,10 +85,12 @@ NetworkIsolationKey NetworkIsolationKey::CreateTransientForTesting() {
 
 NetworkIsolationKey NetworkIsolationKey::CreateWithNewFrameSite(
     const SchemefulSite& new_frame_site) const {
-  if (!top_frame_site_)
+  if (data_->is_empty()) {
     return NetworkIsolationKey();
-  return NetworkIsolationKey(top_frame_site_.value(), new_frame_site, nonce_,
-                             network_isolation_partition_);
+  }
+  return NetworkIsolationKey(data_->top_frame_site().value(), new_frame_site,
+                             data_->nonce(),
+                             data_->network_isolation_partition());
 }
 
 std::optional<std::string> NetworkIsolationKey::ToCacheKeyString() const {
@@ -92,28 +98,31 @@ std::optional<std::string> NetworkIsolationKey::ToCacheKeyString() const {
     return std::nullopt;
 
   std::string network_isolation_partition_string =
-      network_isolation_partition_ == NetworkIsolationPartition::kGeneral
+      GetNetworkIsolationPartition() == NetworkIsolationPartition::kGeneral
           ? ""
           : " " + GetNetworkIsolationPartitionStringForCacheKey(
-                      network_isolation_partition_);
-  return top_frame_site_->Serialize() + " " + frame_site_->Serialize() +
+                      GetNetworkIsolationPartition());
+  return GetTopFrameSite()->Serialize() + " " + GetFrameSite()->Serialize() +
          network_isolation_partition_string;
 }
 
 std::string NetworkIsolationKey::ToDebugString() const {
+  if (data_->is_empty()) {
+    return "null null";
+  }
   // The space-separated serialization of |top_frame_site_| and
   // |frame_site_|.
-  std::string return_string = GetSiteDebugString(top_frame_site_);
-  return_string += " " + GetSiteDebugString(frame_site_);
+  std::string return_string = GetTopFrameSite()->GetDebugString();
+  return_string += " " + GetFrameSite()->GetDebugString();
 
-  if (nonce_.has_value()) {
-    return_string += " (with nonce " + nonce_->ToString() + ")";
+  if (GetNonce().has_value()) {
+    return_string += " (with nonce " + GetNonce()->ToString() + ")";
   }
 
-  if (network_isolation_partition_ != NetworkIsolationPartition::kGeneral) {
+  if (GetNetworkIsolationPartition() != NetworkIsolationPartition::kGeneral) {
     return_string +=
         " (" +
-        NetworkIsolationPartitionToDebugString(network_isolation_partition_) +
+        NetworkIsolationPartitionToDebugString(GetNetworkIsolationPartition()) +
         ")";
   }
 
@@ -121,13 +130,7 @@ std::string NetworkIsolationKey::ToDebugString() const {
 }
 
 bool NetworkIsolationKey::IsFullyPopulated() const {
-  if (!top_frame_site_.has_value()) {
-    return false;
-  }
-  if (!frame_site_.has_value()) {
-    return false;
-  }
-  return true;
+  return !IsEmpty();
 }
 
 bool NetworkIsolationKey::IsTransient() const {
@@ -137,17 +140,17 @@ bool NetworkIsolationKey::IsTransient() const {
 }
 
 bool NetworkIsolationKey::IsEmpty() const {
-  return !top_frame_site_.has_value() && !frame_site_.has_value();
+  return data_->is_empty();
 }
 
 bool NetworkIsolationKey::IsOpaque() const {
-  if (top_frame_site_->opaque()) {
+  if (GetTopFrameSite()->opaque()) {
     return true;
   }
-  if (frame_site_->opaque()) {
+  if (GetFrameSite()->opaque()) {
     return true;
   }
-  if (nonce_.has_value()) {
+  if (GetNonce().has_value()) {
     return true;
   }
   return false;
@@ -158,5 +161,34 @@ NET_EXPORT std::ostream& operator<<(std::ostream& os,
   os << nik.ToDebugString();
   return os;
 }
+
+// static
+scoped_refptr<NetworkIsolationKey::Data>
+NetworkIsolationKey::Data::GetEmptyData() {
+  static base::NoDestructor<scoped_refptr<NetworkIsolationKey::Data>>
+      empty_data(base::MakeRefCounted<Data>(base::PassKey<Data>()));
+  return *empty_data;
+}
+
+NetworkIsolationKey::Data::Data(base::PassKey<Data>)
+    : network_isolation_partition_(NetworkIsolationPartition::kGeneral) {
+  CHECK(is_empty());
+}
+
+NetworkIsolationKey::Data::Data(
+    SchemefulSite&& top_frame_site,
+    SchemefulSite&& frame_site,
+    std::optional<base::UnguessableToken>&& nonce,
+    NetworkIsolationPartition network_isolation_partition)
+    : top_frame_site_(std::move(top_frame_site)),
+      frame_site_(std::move(frame_site)),
+      nonce_(std::move(nonce)),
+      network_isolation_partition_(network_isolation_partition) {
+  CHECK(top_frame_site_.has_value());
+  CHECK(frame_site_.has_value());
+  CHECK(!is_empty());
+}
+
+NetworkIsolationKey::Data::~Data() = default;
 
 }  // namespace net
