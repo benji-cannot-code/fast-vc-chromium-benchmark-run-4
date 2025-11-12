0f3a20d9126eb552a980dce09de52e78265564ff
Sylvain Defresne <sdefresne@chromium.org>
2025-11-12 06:20:29 -0800

    [ios] Use OverlayRequestId in OverlayPresentationContextImpl
    As the lifetime of OverlayRequest is unrelated to the lifetime of
    OverlayPresentationContextImpl, it is unsafe to use raw_ptr<...>
    to them (including using them as key in std::map<...>).
    
    Additionally, OverlayPresentationContextImpl never dereference the
    pointer to OverlayRequest it stored, using them only as identifier,
    so it is possible to replace all uses by OverlayRequestId.
    
    This requires explicitly passing the OverlayRequest pointer to the
    ShowUIForPresentedRequest method.
    
    This is part of a multi-CL change:
      - https://crrev.com/c/7124406
     => https://crrev.com/c/7123830
      - https://crrev.com/c/7123705
    
    Bug: none
    Change-Id: Iaa1f56dd62644513b6e773c3edaf2cbe4e2e7fe9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7123830
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1543694}

--- ios/chrome/browser/overlays/ui_bundled/overlay_presentation_context_impl.h
+++ ios/chrome/browser/overlays/ui_bundled/overlay_presentation_context_impl.h
@@ -9,11 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <UIKit/UIKit.h>
 
 #import "base/memory/raw_ptr.h"
-#include "base/memory/weak_ptr.h"
-#include "base/observer_list.h"
-#include "base/scoped_observation.h"
-#include "ios/chrome/browser/overlays/model/public/overlay_modality.h"
+#import "base/memory/weak_ptr.h"
+#import "base/observer_list.h"
+#import "base/scoped_observation.h"
+#import "ios/chrome/browser/overlays/model/public/overlay_modality.h"
 #import "ios/chrome/browser/overlays/model/public/overlay_presentation_context.h"
+#import "ios/chrome/browser/overlays/model/public/overlay_request.h"
 #import "ios/chrome/browser/overlays/model/public/overlay_user_data.h"
 #import "ios/chrome/browser/overlays/ui_bundled/overlay_presentation_context_fullscreen_disabler.h"
 #import "ios/chrome/browser/overlays/ui_bundled/overlay_request_coordinator.h"
@@ -122,7 +123,7 @@ class OverlayPresentationContextImpl : public OverlayPresentationContext {
   UIViewController* GetBaseViewController(OverlayRequest* request) const;
 
   // Returns the UI state for `request`.
-  OverlayRequestUIState* GetRequestUIState(OverlayRequest* request) const;
+  OverlayRequestUIState* GetRequestUIState(OverlayRequestId request_id) const;
 
   // Returns the presentation capabilities required to show `request`.
   UIPresentationCapabilities GetRequiredPresentationCapabilities(
@@ -135,8 +136,8 @@ class OverlayPresentationContextImpl : public OverlayPresentationContext {
   // Creates the current UIPresentationCapabilities based on the current state.
   UIPresentationCapabilities ConstructPresentationCapabilities();
 
-  // Shows the UI for the presented request using the container coordinator.
-  void ShowUIForPresentedRequest();
+  // Shows the UI for the request using the container coordinator.
+  void ShowUIForPresentedRequest(OverlayRequest* request);
 
   // Called when the UI for `request_` has finished being presented.
   void OverlayUIWasPresented();
@@ -214,12 +215,14 @@ class OverlayPresentationContextImpl : public OverlayPresentationContext {
   // The presentation capabilities of `coordinator_`'s view controller.
   UIPresentationCapabilities presentation_capabilities_ =
       UIPresentationCapabilities::kNone;
-  // The request that is currently presented by `presenter_`.  When a new
-  // request is presented, the UI state for the request will be added to
-  // `states_`.
-  raw_ptr<OverlayRequest, DanglingUntriaged> request_ = nullptr;
-  // Map storing the UI state for each OverlayRequest.
-  std::map<OverlayRequest*, std::unique_ptr<OverlayRequestUIState>> states_;
+  // Identifier of the request that is currently presented by `presenter_`.
+  // When a new request is presented, the UI state for the request will be
+  // added to `states_`.
+  OverlayRequestId request_id_;
+  // Map storing the UI state for each OverlayRequest (identified by their
+  // OverlayRequestId).
+  std::map<OverlayRequestId, std::unique_ptr<OverlayRequestUIState>> states_;
+  // List of registered OverlayPresentationContextObservers.
   base::ObserverList<OverlayPresentationContextObserver,
                      /* check_empty= */ true>
       observers_;
