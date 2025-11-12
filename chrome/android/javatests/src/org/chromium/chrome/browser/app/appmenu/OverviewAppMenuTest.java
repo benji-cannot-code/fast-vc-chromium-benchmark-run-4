ffd65f7f7f88873e343664cf4d1dde56c830ef0c
Sirisha Kavuluru <skavuluru@google.com>
2025-11-12 09:07:43 -0800

    [Test] Re-enable testIncognitoAllMenuItems on desktop builders
    Change-Id: I646d565059f29c02f136f70d1dd44b3bfd3cfabb
    Include-Ci-Only-Tests: chromium.android.desktop:android-desktop-x64-rel-15-tests|chrome_public_test_apk
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7143549
    Commit-Queue: Sirisha Kavuluru <skavuluru@google.com>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1543784}

--- chrome/android/javatests/src/org/chromium/chrome/browser/app/appmenu/OverviewAppMenuTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/app/appmenu/OverviewAppMenuTest.java
@@ -20,7 +20,6 @@ import org.mockito.junit.MockitoRule;
 
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
@@ -41,7 +40,6 @@ import org.chromium.chrome.test.transit.hub.RegularTabSwitcherStation;
 import org.chromium.chrome.test.transit.hub.TabSwitcherAppMenuFacility;
 import org.chromium.chrome.test.transit.quick_delete.QuickDeleteDialogFacility;
 import org.chromium.components.feature_engagement.Tracker;
-import org.chromium.ui.base.DeviceFormFactor;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -115,7 +113,6 @@ public class OverviewAppMenuTest {
     @Test
     @LargeTest
     @Feature({"Browser", "Main"})
-    @DisableIf.Device(DeviceFormFactor.DESKTOP) // TODO(crbug.com/439491767): Close incognito window
     public void testIncognitoAllMenuItems() {
         IncognitoTabSwitcherStation incognitoTabSwitcher =
                 mTabSwitcher.openAppMenu().openNewIncognitoTabOrWindow().openIncognitoTabSwitcher();
@@ -132,6 +129,8 @@ public class OverviewAppMenuTest {
             menu.closeProgrammatically();
             if (!IncognitoUtils.shouldOpenIncognitoAsWindow()) {
                 incognitoTabSwitcher.selectRegularTabsPane();
+            } else {
+                incognitoTabSwitcher.finishActivity();
             }
         }
     }
