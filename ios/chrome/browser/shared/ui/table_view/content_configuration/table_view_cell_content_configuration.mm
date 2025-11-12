a594a5216143e5d8412e3c7629a04ce014fc6a9b
Gauthier Ambard <gambard@chromium.org>
2025-11-12 03:55:17 -0800

    [iOS] Return super as User Input Label when title is nil
    When there is no title, provide the default behaviour.
    
    Fixed: 458955255
    Change-Id: Icaffc44d6c36c21b65d031d80cf4c09ec00fc5a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7145480
    Reviewed-by: Louis Romero <lpromero@google.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1543649}

--- ios/chrome/browser/shared/ui/table_view/content_configuration/table_view_cell_content_configuration.mm
+++ ios/chrome/browser/shared/ui/table_view/content_configuration/table_view_cell_content_configuration.mm
@@ -125,6 +125,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (NSArray<NSString*>*)accessibilityUserInputLabels {
+  if (!self.title) {
+    return [super accessibilityUserInputLabels];
+  }
   return @[ self.title ];
 }
 
