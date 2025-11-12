2b3770b88639aebcd05ab7dcbe8698b2a3d0e8cd
Yuki Shiino <yukishiino@chromium.org>
2025-11-11 23:37:35 -0800

    ELUD: Add ADVANCED_MEMORY_SAFETY_CHECKS to views::Button
    Adds ADVANCED_MEMORY_SAFETY_CHECKS as a mitigation of
    crbug.com/451373711
    
    Bug: 451373711
    Change-Id: I70cde307c4da3ea38482aa4f0191a52e4d3e5650
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7086169
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1543558}

--- ui/views/controls/button/button.h
+++ ui/views/controls/button/button.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
+#include "base/memory/safety_checks.h"
 #include "base/memory/weak_ptr.h"
 #include "build/build_config.h"
 #include "ui/accessibility/ax_enums.mojom.h"
@@ -50,6 +51,9 @@ class ButtonController;
 // be part of the focus chain.
 class VIEWS_EXPORT Button : public View, public AnimationDelegateViews {
   METADATA_HEADER(Button, View)
+
+  // TODO(crbug.com/451373711): Remove this macro once the bug gets fixed.
+  ADVANCED_MEMORY_SAFETY_CHECKS();
 
  public:
   // Button states for various button sub-types.
