cb624afc4c9b9a5d03e926b65877a866f3b027d4
Tom Anderson <thomasanderson@chromium.org>
2025-11-11 17:03:12 -0800

    Use dbus_xdg::ConnectToSignal in GlobalAcceleratorListenerLinux
    R=thestig
    
    Bug: None
    Change-Id: I5fdc10c9deef7bd52417aa1c8b32aa8cbc2b2bf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7143562
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1543440}

--- ui/base/accelerators/global_accelerator_listener/global_accelerator_listener_linux.h
+++ ui/base/accelerators/global_accelerator_listener/global_accelerator_listener_linux.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "components/dbus/utils/connect_to_signal.h"
 #include "components/dbus/xdg/request.h"
 #include "dbus/bus.h"
 #include "dbus/object_proxy.h"
@@ -96,7 +97,7 @@ class GlobalAcceleratorListenerLinux : public GlobalAcceleratorListener {
       base::expected<dbus_xdg::Dictionary, dbus_xdg::ResponseError> results);
 
   // Callbacks for DBus signals.
-  void OnActivatedSignal(dbus::Signal* signal);
+  void OnActivatedSignal(dbus_utils::ConnectToSignalResultSig<"ost"> result);
 
   void OnSignalConnected(const std::string& interface_name,
                          const std::string& signal_name,
