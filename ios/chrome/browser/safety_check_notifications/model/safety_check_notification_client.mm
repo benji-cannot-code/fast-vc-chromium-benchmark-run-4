1553092c14f79a7163ae8239cee36b5a6a6d4434
Chrome Crash (Prod) <crash@system.gserviceaccount.com>
2025-11-12 13:17:55 -0800

    Revert "[iOS] Fix crash when browser is nullptr in SafetyCheckNotificationClient"
    This reverts commit c6585fb393754dee9a860e5515dd75ae465d569c.
    
    Reason for revert: 
    The Suspect CL https://chromium-review.googlesource.com/q/I521126651f9e3df4f6985a7ac15cd8ebc6e6abce is highly likely the cause of the crash. Please see the crash report http://crash/3801169d895f7220.
    Rebase the Revert CL on top of main branch with conflicts allowed option to prevent false positive merge conflicts.
    
    If you agree with the revert CL, please approve it and mark as Commit-Queue +2 to submit it.
    
    More details can be found in the go/crash-revert-cls-creation.
    
    Original change's description:
    > [iOS] Fix crash when browser is nullptr in SafetyCheckNotificationClient
    > 
    > Fixes a crash introduced in https://chromium-review.googlesource.com/c/chromium/src/+/7118628 where profile is
    > accessed from a browser which is potentially nullptr.
    > 
    > Fixed: 458802981, 458802404
    > Change-Id: I521126651f9e3df4f6985a7ac15cd8ebc6e6abce
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7136561
    > Commit-Queue: Gayane Petrosyan <gayane@google.com>
    > Reviewed-by: Benjamin Williams <bwwilliams@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1542566}
    > 
    Crash Bug: 459931369
    
    Change-Id: Ibd6ec4c6f871e2005841d72d0996e556aeaf19d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7142381
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Harry Souders <harrysouders@google.com>
    Owners-Override: Harry Souders <harrysouders@google.com>
    Cr-Commit-Position: refs/heads/main@{#1543926}

--- ios/chrome/browser/safety_check_notifications/model/safety_check_notification_client.mm
+++ ios/chrome/browser/safety_check_notifications/model/safety_check_notification_client.mm
@@ -318,7 +318,8 @@ bool SafetyCheckNotificationClient::IsPermitted() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   return IsSafetyCheckNotificationPermitted(
-      GetApplicationContext()->GetLocalState(), GetActiveForegroundProfile());
+      GetApplicationContext()->GetLocalState(),
+      GetActiveForegroundBrowser()->GetProfile());
 }
 
 bool SafetyCheckNotificationClient::IsSceneLevelForegroundActive() {
@@ -740,12 +741,4 @@ bool SafetyCheckNotificationClient::CheckAndResetIfSchedulingIsAllowed() {
       prefs::kIosSafetyCheckNotificationFirstPresentTimestamp);
 
   return true;
-}
-
-ProfileIOS* SafetyCheckNotificationClient::GetActiveForegroundProfile() const {
-  Browser* browser = GetActiveForegroundBrowser();
-  if (!browser) {
-    return nullptr;
-  }
-  return browser->GetProfile();
 }
