8f6b050f9c6ec1329f8f47ef4f00ac0dfc9f14ba
Colin Blundell <blundell@chromium.org>
2025-11-12 00:50:19 -0800

    [//ui] Have NativePixmapEGLX11Binding::Create() take in SIFormat
    Eliminates the need for callers to do this conversion.
    
    Bug: 450325664
    Change-Id: I4771425844da3cf4388ae11981fb5f1cb9b25f6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7130921
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1543575}

--- ui/ozone/platform/x11/x11_surface_factory.cc
+++ ui/ozone/platform/x11/x11_surface_factory.cc
@@ -146,7 +146,6 @@ class GLOzoneEGLX11 : public GLOzoneEGL {
       const gfx::ColorSpace& color_space,
       GLenum target,
       GLuint texture_id) override {
-    auto buffer_format = viz::SharedImageFormatToBufferFormat(plane_format);
     switch (GetNativePixmapSupportType()) {
       case NativePixmapSupportType::kDMABuf: {
         return NativePixmapEGLBinding::Create(pixmap, plane_format, plane,
@@ -155,7 +154,7 @@ class GLOzoneEGLX11 : public GLOzoneEGL {
       }
       case NativePixmapSupportType::kX11Pixmap: {
         return NativePixmapEGLX11Binding::Create(
-            pixmap, buffer_format, plane_size, target, texture_id);
+            pixmap, plane_format, plane_size, target, texture_id);
       }
       default:
         return nullptr;
