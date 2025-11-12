71e68288ae67e33fc9df7da202a8a52f32232055
Eric Leese <leese@chromium.org>
2025-11-12 03:52:20 -0800

    Collect font at-rules for pseudo-elements
    We show styles for pseudo-element children like ::first-letter in the DevTools
    styles panel, and they may specify different fonts in some cases, so we should
    collect any font-related at-rules related to pseudo-element children.
    
    Bug: 408969009
    Change-Id: Iad234413a557f2ca136fd080f34af55fbf596ef4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7134129
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Eric Leese <leese@chromium.org>
    Reviewed-by: Philip Pfaffe <pfaffe@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1543647}

---
+++ third_party/blink/web_tests/inspector-protocol/css/css-font-at-rules-pseudoelements.js
@@ -0,0 +1,69 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(/** @type {import('test_runner').TestRunner} */ testRunner) {
+  const {dp} = await testRunner.startHTML(
+      `
+  <style>
+  #test {
+    font-family: Papyrus;
+  }
+
+  #test::before {
+    content: "before";
+    font-family: Bixa;
+    font-palette: --palette;
+    font-variant-alternates: swash(fancy);
+  }
+
+  #test::after {
+    content: "after";
+    font-family: Bixa;
+    font-palette: --palette;
+    font-variant-alternates: swash(boring);
+  }
+
+  #test::first-letter {
+    font-family: Bixa;
+    font-variant-alternates: styleset(cool);
+  }
+
+  @font-palette-values --palette {
+    font-family: Bixa;
+    override-colors: 0 red;
+  }
+
+  @font-feature-values Bixa {
+    @swash {
+      fancy: 1;
+      boring: 2;
+    }
+    @styleset {
+      cool: 3;
+    }
+    @stylistic {
+      something: 4;
+    }
+  }
+
+  @font-face {
+    font-family: Bixa;
+    src: local(Bixa);
+  }
+  </style>
+
+  <div id=test>div</div>
+  `,
+      'Test that font related at-rules are reported when used in pseudo-elements');
+
+  await dp.DOM.enable();
+  await dp.CSS.enable();
+
+  const {result: {root}} = await dp.DOM.getDocument();
+  const {result: {nodeId}} =
+      await dp.DOM.querySelector({nodeId: root.nodeId, selector: '#test'});
+
+  const {result: {cssAtRules}} = await dp.CSS.getMatchedStylesForNode({nodeId});
+
+  testRunner.log(cssAtRules);
+
+  testRunner.completeTest();
+});
