a069dbe6f123a70b6be6892fef83c7d62ff3f43c
Christoph Schwering <schwering@google.com>
2025-11-12 08:14:40 -0800

    [Autofill] Remove inconsistent blank lines
    Bug: 40100455
    Change-Id: Ib504243705df594f3aa7272cff9ae47ae07969ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7146762
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1543747}

--- components/autofill/core/common/autofill_features.h
+++ components/autofill/core/common/autofill_features.h
@@ -187,12 +187,10 @@ BASE_DECLARE_FEATURE(kAutofillEnableImportWhenMultiplePhoneNumbers);
 COMPONENT_EXPORT(AUTOFILL)
 BASE_DECLARE_FEATURE(kAutofillEnableKeyboardAccessoryChipRedesign);
 #endif  // BUILDFLAG(IS_ANDROID)
-
 #if BUILDFLAG(IS_ANDROID)
 COMPONENT_EXPORT(AUTOFILL)
 BASE_DECLARE_FEATURE(kAutofillEnableKeyboardAccessoryChipWidthAdjustment);
 #endif  // BUILDFLAG(IS_ANDROID)
-
 COMPONENT_EXPORT(AUTOFILL)
 BASE_DECLARE_FEATURE(kAutofillEnableLabelPrecedenceForTurkishAddresses);
 COMPONENT_EXPORT(AUTOFILL)
@@ -201,7 +199,6 @@ BASE_DECLARE_FEATURE(kAutofillEnableLoyaltyCardsFilling);
 COMPONENT_EXPORT(AUTOFILL)
 BASE_DECLARE_FEATURE(kAutofillEnableSecurityTouchEventFilteringAndroid);
 #endif  // BUILDFLAG(IS_ANDROID)
-
 COMPONENT_EXPORT(AUTOFILL)
 BASE_DECLARE_FEATURE(kAutofillEnableSupportForHomeAndWork);
 COMPONENT_EXPORT(AUTOFILL)
