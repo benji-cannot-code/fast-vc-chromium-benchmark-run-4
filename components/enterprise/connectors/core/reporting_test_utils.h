b16a3bacd895cf24259daafff13537abb9363c4c
Xu Li <mxlg@google.com>
2025-11-12 11:07:10 -0800

    Move all data_controls related validation to //component.
    The previous CL
    https://chromium-review.googlesource.com/c/chromium/src/+/7138601 was
    reverted because of a fuchsia build failure. Adding build flags in the
    CL to fix the issue and reland the change.
    
    Bug: 458769338
    Change-Id: I1c5986b24ef1da6b716ff27b8d1077616a9949d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7147173
    Commit-Queue: Xu Li <mxlg@google.com>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1543850}

--- components/enterprise/connectors/core/reporting_test_utils.h
+++ components/enterprise/connectors/core/reporting_test_utils.h
@@ -15,9 +15,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
 #include "base/values.h"
+#include "components/enterprise/buildflags/buildflags.h"
 #include "components/enterprise/common/proto/synced/browser_events.pb.h"
-#include "components/enterprise/data_controls/core/browser/verdict.h"
 #include "components/policy/test_support/embedded_policy_test_server.h"
+
+#if BUILDFLAG(ENTERPRISE_DATA_CONTROLS)
+#include "components/enterprise/data_controls/core/browser/verdict.h"
+#endif  // BUILDFLAG(ENTERPRISE_DATA_CONTROLS)
 
 class PrefService;
 
@@ -115,6 +119,7 @@ class EventReportValidatorBase {
       int expected_net_error_code,
       const ::chrome::cros::reporting::proto::UrlInfo& expected_referrers);
 
+#if BUILDFLAG(ENTERPRISE_DATA_CONTROLS)
   void ExpectDataControlsSensitiveDataEvent(
       const std::string& expected_url,
       const std::string& expected_tab_url,
@@ -127,6 +132,7 @@ class EventReportValidatorBase {
       const std::string& expected_profile_username,
       const std::string& expected_profile_identifier,
       int64_t expected_content_size);
+#endif  // BUILDFLAG(ENTERPRISE_DATA_CONTROLS)
 
   // Closure to run once all expected events are validated.
   void SetDoneClosure(base::RepeatingClosure closure);
@@ -165,6 +171,12 @@ class EventReportValidatorBase {
           expected_identities);
   void ValidateMimeType(const base::Value::Dict* value,
                         const std::set<std::string>* expected_mimetypes);
+
+#if BUILDFLAG(ENTERPRISE_DATA_CONTROLS)
+  void ValidateDataControlsTriggerdRules(
+      const base::Value::Dict* value,
+      const data_controls::Verdict::TriggeredRules& expected_triggered_rules);
+#endif  // BUILDFLAG(ENTERPRISE_DATA_CONTROLS)
 
   raw_ptr<policy::MockCloudPolicyClient> client_;
   base::RepeatingClosure done_closure_;
