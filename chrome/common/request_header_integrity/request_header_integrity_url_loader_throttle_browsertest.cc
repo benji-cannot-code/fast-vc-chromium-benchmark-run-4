6b4443c461908359fcb812a8cd46b6951430590b
Lily Chen <chlily@chromium.org>
2025-11-11 22:23:28 -0800

    Remove kIgnoreCertificateErrors in ['components/guest_contents', 'chrome/common/request_header_integrity']
    The switch --ignore-certificate-errors is frequently misused throughout
    the codebase by tests that use an HTTPS EmbeddedTestServer to serve
    URLs with custom hostname(s). Ignoring certificate errors is usually
    incorrect and unnecessary; the better way is to configure the HTTPS
    EmbeddedTestServer with the proper certificates.
    
    This is done by configuring valid test certs using variously:
     * net::EmbeddedTestServer::CERT_TEST_NAMES,
     * generated test certificate configured by:
          * net::EmbeddedTestServer::SetCertHostnames(),
          * manually setting `ServerCertificateConfig::dns_names`,
          * using the default ServerCertificateConfig configuration provided
            by content::BrowserTestBase with embedded_https_test_server().
    
    Some tests were also using the switch entirely unnecessarily as they
    were not using an HTTPS test server, or did not use custom hostnames.
    
    This CL covers test files in:
    ['components/guest_contents', 'chrome/common/request_header_integrity']
    
    This CL was uploaded by git cl split.
    
    R=robliao@chromium.org
    
    Bug: 40147519
    Change-Id: I53360d57415be03310bc42f80cbc9cd3b61098ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7143022
    Auto-Submit: Lily Chen <chlily@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1543539}

--- chrome/common/request_header_integrity/request_header_integrity_url_loader_throttle_browsertest.cc
+++ chrome/common/request_header_integrity/request_header_integrity_url_loader_throttle_browsertest.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/branding_buildflags.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
-#include "components/network_session_configurator/common/network_switches.h"
 #include "content/public/test/browser_test.h"
 #include "net/dns/mock_host_resolver.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
@@ -68,12 +67,8 @@ class RequestHeaderIntegrityURLLoaderThrottleBrowserTest
         &RequestHeaderIntegrityURLLoaderThrottleBrowserTest::RequestHandler,
         base::Unretained(this)));
 
+    server().SetCertHostnames({"www.google.com", "www.chromium.org"});
     server().StartAcceptingConnections();
-  }
-
-  void SetUpCommandLine(base::CommandLine* command_line) override {
-    // Required since the test server has no real certs to present.
-    command_line->AppendSwitch(switches::kIgnoreCertificateErrors);
   }
 
  protected:
