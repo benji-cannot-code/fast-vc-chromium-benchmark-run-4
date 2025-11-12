a40f0be64867ad230825c171d4c6594a1ad3af6c
Sourav <sinhasourav@google.com>
2025-11-12 01:51:49 -0800

    [iOSFidoImportExport] Add Continue button to Export UI
    This CL introduces 'Continue' navigation button to the
    `CredentialExportViewController` to control the credential export flow.
    
    Bug: 456365102
    Change-Id: Ie90cd1c0b996d9e45abfc03c2767697d50842208
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7094049
    Reviewed-by: Noémie St-Onge <noemies@google.com>
    Reviewed-by: Rafał Godlewski <rgod@google.com>
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1543610}

--- ios/chrome/browser/credential_exchange/ui/credential_export_view_controller.h
+++ ios/chrome/browser/credential_exchange/ui/credential_export_view_controller.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @protocol CredentialExportViewControllerPresentationDelegate;
 
+API_AVAILABLE(ios(26.0))
 @interface CredentialExportViewController
     : UITableViewController <CredentialExportConsumer>
 
