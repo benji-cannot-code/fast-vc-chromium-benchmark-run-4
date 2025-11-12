dbf05f5da0cf2461806ddafc4954a6de940a8336
S Ganesh <ganesh@chromium.org>
2025-11-12 15:22:53 -0800

    Revert "[Task List Bubble] Show the bubble on nudge or nudge click"
    This reverts commit 6da994fe6e1ac58ff5a9ffec0236971eaed97d21.
    
    Reason for revert: interactive_ui_tests failed
    GlicWindowControllerUiTest.GlicButtonAndTaskIconButtonTogglesConversationAndActuationView
    GlicWindowControllerUiTest.TaskIconTogglesGlicWindow
    
    https://ci.chromium.org/ui/p/chrome/builders/ci/mac64/261585/overview
    
    Original change's description:
    > [Task List Bubble] Show the bubble on nudge or nudge click
    >
    > Adds a subscription pattern for the nudge controller to receive bubble
    > updates and show the bubble from the nudge.
    > Adds calls to show the ActorTaskListBubble on nudge click as well as
    > suppressing the nudge on a row click.
    > Also updates the nudge label to "Check your tasks" per recent UX
    > feedback.
    >
    > Screenshare demo: https://screencast.googleplex.com/cast/NTgyNTMzODc5MjczODgxNnw5MzRmZmQyOC0wYQ
    > (Note: Handoff button is set to pause tasks only for demo purposes)
    >
    > Bug: 440770955, 440773268
    > Change-Id: I227fa84ea744add87fe237e243ea4879487b59ee
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7128325
    > Reviewed-by: Abe Boujane <boujane@google.com>
    > Reviewed-by: Alison Gale <agale@chromium.org>
    > Reviewed-by: Christine Ying <chrstne@google.com>
    > Commit-Queue: Maggie Jennings <mjenn@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1543906}
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 440770955, 440773268
    Change-Id: I2a8c0137e3b911cc7dbb22d9fdb6ff255a6039c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7147209
    Reviewed-by: Alison Gale <agale@chromium.org>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Owners-Override: S Ganesh <ganesh@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1543999}

--- chrome/browser/actor/ui/task_list_bubble/actor_task_list_bubble_controller.h
+++ chrome/browser/actor/ui/task_list_bubble/actor_task_list_bubble_controller.h
@@ -46,7 +46,6 @@ class ActorTaskListBubbleController : public views::WidgetObserver {
 
 #if BUILDFLAG(ENABLE_GLIC)
   void ShowBubble(views::View* anchor_view);
-  void OnStateUpdate(const actor::TaskId& task_id);
 #endif
 
   void OnWidgetDestroyed(views::Widget* widget) override;
@@ -62,9 +61,6 @@ class ActorTaskListBubbleController : public views::WidgetObserver {
 #if BUILDFLAG(ENABLE_GLIC)
   ActorTaskListBubbleRowButtonParams CreateRowButtonParamsForTaskState(
       tabs::ActorTaskListBubbleRowState task_state);
-
-  std::vector<base::CallbackListSubscription>
-      bubble_state_change_callback_subscription_;
 #endif
 
   base::ScopedObservation<views::Widget, views::WidgetObserver>
--- chrome/browser/ui/views/tabs/tab_strip_action_container.cc
+++ chrome/browser/ui/views/tabs/tab_strip_action_container.cc
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/view_class_properties.h"
 
 #if BUILDFLAG(ENABLE_GLIC)
-#include "chrome/browser/actor/ui/task_list_bubble/actor_task_list_bubble_controller.h"
 #include "chrome/browser/glic/glic_profile_manager.h"
 #include "chrome/browser/glic/host/glic.mojom.h"
 #include "chrome/browser/glic/host/host.h"
@@ -632,27 +631,23 @@ void TabStripActionContainer::OnGlicButtonAnimationEnded() {
 
 void TabStripActionContainer::OnGlicActorTaskIconClicked() {
   Profile* profile = tab_strip_controller_->GetProfile();
+  glic::GlicKeyedServiceFactory::GetGlicKeyedService(profile)->ToggleUI(
+      tab_strip_controller_->GetBrowserWindowInterface(),
+      /*prevent_close=*/false, glic::mojom::InvocationSource::kActorTaskIcon);
 
-  if (base::FeatureList::IsEnabled(features::kGlicActorUiNudgeRedesign)) {
-    ActorTaskListBubbleController* controller =
-        ActorTaskListBubbleController::From(
-            tab_strip_controller_->GetBrowserWindowInterface());
-    controller->ShowBubble(glic_actor_task_icon_);
-  } else {
-    if (glic_actor_task_icon_->GetIsShowingNudge()) {
-      auto* icon_manager =
-          tabs::GlicActorTaskIconManagerFactory::GetForProfile(profile);
-      icon_manager->ClearStoppedTasks();
-      // If a nudge is showing, activate the last actuated tab on click of the
-      // Task Icon.
-      if (tabs::TabInterface* last_updated_tab =
-              icon_manager->GetLastUpdatedTab()) {
-        TabStripModel* tab_strip_model =
-            tab_strip_controller_->GetBrowserWindowInterface()
-                ->GetTabStripModel();
-        int tab_index = tab_strip_model->GetIndexOfTab(last_updated_tab);
-        tab_strip_model->ActivateTabAt(tab_index);
-      }
+  if (glic_actor_task_icon_->GetIsShowingNudge()) {
+    auto* icon_manager =
+        tabs::GlicActorTaskIconManagerFactory::GetForProfile(profile);
+    icon_manager->ClearStoppedTasks();
+    // If a nudge is showing, activate the last actuated tab on click of the
+    // Task Icon.
+    if (tabs::TabInterface* last_updated_tab =
+            icon_manager->GetLastUpdatedTab()) {
+      TabStripModel* tab_strip_model =
+          tab_strip_controller_->GetBrowserWindowInterface()
+              ->GetTabStripModel();
+      int tab_index = tab_strip_model->GetIndexOfTab(last_updated_tab);
+      tab_strip_model->ActivateTabAt(tab_index);
     }
   }
 
