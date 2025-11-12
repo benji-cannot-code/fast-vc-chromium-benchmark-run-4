ceac92bbf10d7dfc82250eeea9db8d214803248b
Chirag Arora <heychirag@google.com>
2025-11-12 12:47:09 -0800

    Revert "metrics: Add a histogram for max CLS in Incognito"
    OBSOLETE_HISTOGRAMS=ACT efforts are being wound down.
    
    Bug: b:455504667
    Change-Id: I16a3ef4fc1bb68acaace7ba7a6fdba15697df476
    Reverts: Ifdeb95410e2a01f59c700df8db2e68a3f40862b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7146902
    Reviewed-by: Kamil Jarosz <kjarosz@google.com>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Chirag Arora <heychirag@google.com>
    Cr-Commit-Position: refs/heads/main@{#1543908}

--- chrome/browser/page_load_metrics/observers/core/ukm_page_load_metrics_observer.h
+++ chrome/browser/page_load_metrics/observers/core/ukm_page_load_metrics_observer.h
@@ -24,17 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/perfetto/include/perfetto/tracing/event_context.h"
 #include "ui/base/page_transition_types.h"
 
-namespace internal {
-
-extern const char
-    kHistogramLayoutInstabilityMaxCumulativeShiftScoreSessionWindowGap1000msMax5000ms2
-        [];
-extern const char
-    kHistogramLayoutInstabilityMaxCumulativeShiftScoreSessionWindowGap1000msMax5000ms2Incognito
-        [];
-
-}  // namespace internal
-
 namespace content {
 class BrowserContext;
 }
@@ -56,11 +45,10 @@ class UkmPageLoadMetricsObserver
  public:
   // Returns a UkmPageLoadMetricsObserver, or nullptr if it is not needed.
   static std::unique_ptr<page_load_metrics::PageLoadMetricsObserver>
-  CreateIfNeeded(bool is_incognito);
+  CreateIfNeeded();
 
   explicit UkmPageLoadMetricsObserver(
-      network::NetworkQualityTracker* network_quality_tracker,
-      bool is_incognito);
+      network::NetworkQualityTracker* network_quality_tracker);
 
   UkmPageLoadMetricsObserver(const UkmPageLoadMetricsObserver&) = delete;
   UkmPageLoadMetricsObserver& operator=(const UkmPageLoadMetricsObserver&) =
@@ -389,9 +377,6 @@ class UkmPageLoadMetricsObserver
   page_load_metrics::NavigationHandleUserData::InitiatorLocation
       navigation_trigger_type_ = page_load_metrics::NavigationHandleUserData::
           InitiatorLocation::kOther;
-
-  // Whether the WebContents being observed is for an Incognito profile.
-  bool is_incognito_;
 
   base::WeakPtrFactory<UkmPageLoadMetricsObserver> weak_factory_{this};
 };
