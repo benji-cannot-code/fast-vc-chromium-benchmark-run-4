d837d8783b98c8d8c0d67ebc7e2723fa9508f7f4
Aviv Kiss <avivkiss@google.com>
2025-11-12 13:48:59 -0800

    Reland "[aim][popup] Add page/page handler Mojo interface and visibility events."
    This is a reland of commit 6ff1965ba6ea341098216e3b113d5d7153adb1e8
    
    I added a missing dependency in `chrome/browser/ui/webui/searchbox/BUILD.gn` that is needed due to the imports in OmniboxPopupUI -- added the mirrored try builder from https://ci.chromium.org/ui/p/chromium/builders/ci/Win%20Builder/258801/infra to confirm.
    
    Original change's description:
    > [aim][popup] Add page/page handler Mojo interface and visibility events.
    >
    > The AIM popup hosts the <cr-composebox> which is hosted in a views
    > Widget. The webcontents of the widget are always present and the
    > containing Widget is shown and hidden in order to display or hide the
    > composebox. Due to current limitations in WebUI <cr-composebox> is not
    > notified of visibility events. This CL adds a Mojom interface to inform
    > the WebUI of visibility events to allow it to react properly to being
    > shown or hidden on-screen.
    >
    > This CL also adds a `Close()` method to the PageHandler to allow for the
    > WebUI code to close the popup in response to user events.
    >
    > https://crrev.com/c/7138389 is a WIP CL that shows how the
    > omnibox_popup_aim.mojom may evolve to accommodate passing context into
    > the composebox.
    >
    > Bug: b:457860153
    > Change-Id: If0861eef300682017e775b252a1a79da50a274a7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7113727
    > Auto-Submit: Aviv Kiss <avivkiss@google.com>
    > Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Commit-Queue: Aviv Kiss <avivkiss@google.com>
    > Reviewed-by: John Lee <johntlee@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1543804}
    
    Bug: b:457860153
    
    Reland "[aim][popup] Add page/page handler Mojo interface and visibility events."
    
    This is a reland of commit 6ff1965ba6ea341098216e3b113d5d7153adb1e8
    
    Original change's description:
    > [aim][popup] Add page/page handler Mojo interface and visibility events.
    >
    > The AIM popup hosts the <cr-composebox> which is hosted in a views
    > Widget. The webcontents of the widget are always present and the
    > containing Widget is shown and hidden in order to display or hide the
    > composebox. Due to current limitations in WebUI <cr-composebox> is not
    > notified of visibility events. This CL adds a Mojom interface to inform
    > the WebUI of visibility events to allow it to react properly to being
    > shown or hidden on-screen.
    >
    > This CL also adds a `Close()` method to the PageHandler to allow for the
    > WebUI code to close the popup in response to user events.
    >
    > https://crrev.com/c/7138389 is a WIP CL that shows how the
    > omnibox_popup_aim.mojom may evolve to accommodate passing context into
    > the composebox.
    >
    > Bug: b:457860153
    > Change-Id: If0861eef300682017e775b252a1a79da50a274a7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7113727
    > Auto-Submit: Aviv Kiss <avivkiss@google.com>
    > Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Commit-Queue: Aviv Kiss <avivkiss@google.com>
    > Reviewed-by: John Lee <johntlee@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1543804}
    
    Bug: b:457860153
    Change-Id: I16a483b8f3aa3d4720626e295d47e3ba744cb27b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7147584
    Reviewed-by: David Yeung <dayeung@chromium.org>
    Auto-Submit: Aviv Kiss <avivkiss@google.com>
    Commit-Queue: Aviv Kiss <avivkiss@google.com>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1543947}

--- chrome/browser/ui/views/omnibox/omnibox_aim_popup_webui_content.cc
+++ chrome/browser/ui/views/omnibox/omnibox_aim_popup_webui_content.cc
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/omnibox/omnibox_context_menu.h"
 #include "chrome/browser/ui/views/omnibox/omnibox_popup_presenter.h"
 #include "chrome/browser/ui/views/omnibox/rounded_omnibox_results_frame.h"
+#include "chrome/browser/ui/webui/omnibox_popup/omnibox_popup_aim_handler.h"
+#include "chrome/browser/ui/webui/omnibox_popup/omnibox_popup_ui.h"
 #include "chrome/common/webui_url_constants.h"
 #include "components/input/native_web_keyboard_event.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
@@ -32,10 +34,28 @@ OmniboxAimPopupWebUIContent::OmniboxAimPopupWebUIContent(
 
 OmniboxAimPopupWebUIContent::~OmniboxAimPopupWebUIContent() = default;
 
+void OmniboxAimPopupWebUIContent::ShowUI() {
+  auto* webui_controller = contents_wrapper()->GetWebUIController();
+  if (webui_controller) {
+    auto* omnibox_popup_ui = webui_controller->GetAs<OmniboxPopupUI>();
+    if (omnibox_popup_ui && omnibox_popup_ui->popup_aim_handler()) {
+      omnibox_popup_ui->popup_aim_handler()->OnShow();
+    }
+  }
+}
+
 void OmniboxAimPopupWebUIContent::CloseUI() {
   // Update the popup state manager that the aim popup is closing.
   // LocationBarView is subscribed to state changes and will close the widget.
   controller()->popup_state_manager()->SetPopupState(OmniboxPopupState::kNone);
+
+  auto* webui_controller = contents_wrapper()->GetWebUIController();
+  if (webui_controller) {
+    auto* omnibox_popup_ui = webui_controller->GetAs<OmniboxPopupUI>();
+    if (omnibox_popup_ui && omnibox_popup_ui->popup_aim_handler()) {
+      omnibox_popup_ui->popup_aim_handler()->OnClose();
+    }
+  }
 }
 
 BEGIN_METADATA(OmniboxAimPopupWebUIContent)
--- chrome/browser/ui/views/omnibox/omnibox_aim_popup_webui_content.h
+++ chrome/browser/ui/views/omnibox/omnibox_aim_popup_webui_content.h
@@ -30,6 +30,7 @@ class OmniboxAimPopupWebUIContent : public OmniboxPopupWebUIBaseContent {
 
   // WebUIContentsWrapper::Host:
   void CloseUI() override;
+  void ShowUI() override;
 };
 
 BEGIN_VIEW_BUILDER(/* no export */,
