3e9fcca43e22f2cf4b0de29653e5bebc72ec60c4
Dhruv Kathpalia <dhruvkathpalia@google.com>
2025-11-11 16:38:38 -0800

    [realbox] Disable copy/paste, drag/drop when realbox next is disabled.
    Disable the capy and paste functionality and drag and drop functionality
    for the context when realbox next is disabled. This prevents the crash
    as there is no contextual-entrypoint-and-carousel element when realbox
    next is disabled which is queried for these 2 functionalities.
    Bug: b:459040503
    
    Change-Id: Iacb102d0d0d77b90ded104cc27dd4faf2572be7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7143291
    Commit-Queue: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Reviewed-by: Hannah Collins <hannahrogers@google.com>
    Cr-Commit-Position: refs/heads/main@{#1543425}

--- ui/webui/resources/cr_components/searchbox/searchbox.ts
+++ ui/webui/resources/cr_components/searchbox/searchbox.ts
@@ -455,7 +455,7 @@ export class SearchboxElement extends SearchboxElementBase implements
 
   private pageHandler_: PageHandlerInterface;
   private callbackRouter_: PageCallbackRouter;
-  protected dragAndDropHandler: DragAndDropHandler;
+  protected dragAndDropHandler: DragAndDropHandler|null = null;
   private dragAndDropEnabled_: boolean =
       loadTimeData.getBoolean('dragAndDropEnabled');
   private autocompleteResultChangedListenerId_: number|null = null;
@@ -470,8 +470,6 @@ export class SearchboxElement extends SearchboxElementBase implements
 
     this.pageHandler_ = SearchboxBrowserProxy.getInstance().handler;
     this.callbackRouter_ = SearchboxBrowserProxy.getInstance().callbackRouter;
-    this.dragAndDropHandler =
-        new DragAndDropHandler(this, this.dragAndDropEnabled_);
   }
 
   override async connectedCallback() {
@@ -502,6 +500,8 @@ export class SearchboxElement extends SearchboxElementBase implements
     }
 
     if (this.ntpRealboxNextEnabled) {
+      this.dragAndDropHandler =
+          new DragAndDropHandler(this, this.dragAndDropEnabled_);
       this.refreshTabSuggestions_();
     }
   }
@@ -794,7 +794,7 @@ export class SearchboxElement extends SearchboxElementBase implements
   }
 
   protected onInputPaste_(e: ClipboardEvent) {
-    if (e.clipboardData?.files && e.clipboardData.files.length > 0) {
+    if (this.ntpRealboxNextEnabled && e.clipboardData?.files && e.clipboardData.files.length > 0) {
       const files = Array.from(e.clipboardData.files);
       if (files.length > 0) {
         e.preventDefault();
