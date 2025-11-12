44045cb24e013aa4cf45b7e900c747acb6f8cd7e
David Grogan <dgrogan@chromium.org>
2025-11-11 16:00:39 -0800

    [css-values] Reduce viewport units when scrollbars are unconditional
    When the root element has unconditional scrollbars (i.e.
    overflow:scroll), we reduce the viewport units accordingly. All viewport
    units are affected.
    
    The modified units need to affect properties specified on the root
    element, so we resolve the properties that can affect scrollbars early.
    
    Note this change is not yet complete. The dynamic test fails, scrollbar
    gutters aren't respected yet, and a few other small things.
    
    Bug: 354751900
    Change-Id: I51653d0491a32f86539e219a3e99a6eead3c7dc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7098882
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: David Grogan <dgrogan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1543410}

--- third_party/blink/renderer/core/layout/layout_view.h
+++ third_party/blink/renderer/core/layout/layout_view.h
@@ -179,8 +179,11 @@ class CORE_EXPORT LayoutView : public LayoutBlockFlow {
     return autosize_v_scrollbar_mode_;
   }
 
-  void CalculateScrollbarModes(mojom::blink::ScrollbarMode& h_mode,
-                               mojom::blink::ScrollbarMode& v_mode) const;
+  void CalculateScrollbarModes(
+      mojom::blink::ScrollbarMode& h_mode,
+      mojom::blink::ScrollbarMode& v_mode,
+      std::optional<EOverflow> overflow_x = std::nullopt,
+      std::optional<EOverflow> overflow_y = std::nullopt) const;
 
   bool CanHaveAdditionalCompositingReasons() const override {
     NOT_DESTROYED();
@@ -269,6 +272,8 @@ class CORE_EXPORT LayoutView : public LayoutBlockFlow {
   gfx::SizeF LargeViewportSizeForViewportUnits() const;
   // https://drafts.csswg.org/css-values-4/#dynamic-viewport-size
   gfx::SizeF DynamicViewportSizeForViewportUnits() const;
+  gfx::SizeF SubtractUnconditionalScrollbarsFromViewportUnits(
+      const gfx::SizeF& viewport_size) const;
 
   // Get the size to evaluate width and height media queries against when
   // paginating / printing.
@@ -339,6 +344,7 @@ class CORE_EXPORT LayoutView : public LayoutBlockFlow {
   LayoutViewTransitionRoot* GetViewTransitionRoot() const;
 
   void CacheScrollDimensions();
+  bool SetScrollbarSizesForViewportUnits(const gfx::Size& size);
 
  private:
   void StyleDidChange(StyleDifference,
@@ -420,6 +426,9 @@ class CORE_EXPORT LayoutView : public LayoutBlockFlow {
   std::optional<CachedScrollDimensions> cached_scroll_dimensions_;
 
   mutable PhysicalRect previous_background_rect_;
+
+  int vertical_scrollbar_width_for_viewport_units_ = 0;
+  int horizontal_scrollbar_height_for_viewport_units_ = 0;
 };
 
 template <>
