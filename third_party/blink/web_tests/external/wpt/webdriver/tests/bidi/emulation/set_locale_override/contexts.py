b8c249b5804861dc61a38731a74551bec20a09f7
Jonathan Lee <jonathanjlee@google.com>
2025-11-12 15:27:08 -0800

    Import wpt@20997066f7c047c162f2e7599cc95af1bddc2e23
    https://github.com/web-platform-tests/wpt/compare/f62764048...20997066f
    
    Using wpt-import in Chromium 4e875d6fc1296d09200e44379da4ade2a71f1ce7.
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    foolip@chromium.org:
      external/wpt/fullscreen
    jfernandez@igalia.com:
      external/wpt/css/css-align
    kojii@chromium.org, mstensho@chromium.org:
      external/wpt/css/CSS2
    mustaq@chromium.org:
      external/wpt/html/interaction/focus
    pastithas@chromium.org:
      external/wpt/resources
    smcgruer@chromium.org:
      external/wpt/css/css-position
    
    NOAUTOREVERT=true
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac-skia-alt-arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac12.0.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac13.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac14-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac14.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac15-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac15.arm64-blink-rel
    Change-Id: I5b696f586a2e873fd5da657d9a9d6bfdd2f088e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7147483
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1544002}

--- third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/emulation/set_locale_override/contexts.py
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/emulation/set_locale_override/contexts.py
@@ -7,9 +7,9 @@ pytestmark = pytest.mark.asyncio
 async def test_contexts(
     bidi_session,
     new_tab,
-    some_locale,
     assert_locale_against_default,
     assert_locale_against_value,
+    some_locale,
 ):
     new_context = await bidi_session.browsing_context.create(type_hint="tab")
 
@@ -35,8 +35,8 @@ async def test_contexts(
 async def test_multiple_contexts(
     bidi_session,
     new_tab,
-    assert_locale_against_value,
     assert_locale_against_default,
+    assert_locale_against_value,
     some_locale,
 ):
     new_context = await bidi_session.browsing_context.create(type_hint="tab")
@@ -64,11 +64,11 @@ async def test_multiple_contexts(
 async def test_iframe(
     bidi_session,
     new_tab,
-    some_locale,
-    domain,
     inline,
     another_locale,
     assert_locale_against_value,
+    some_locale,
+    domain,
 ):
     # Set locale override.
     await bidi_session.emulation.set_locale_override(
@@ -124,8 +124,8 @@ async def test_locale_override_applies_to_existing_sandbox(
     bidi_session,
     new_tab,
     another_locale,
-    assert_locale_against_value,
     assert_locale_against_default,
+    assert_locale_against_value,
 ):
     sandbox_name = "test"
 
