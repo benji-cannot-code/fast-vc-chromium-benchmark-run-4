91ba0134d69da0c098e3ef9f807b89998bcfdce1
Nan Lin <linnan@chromium.org>
2025-11-12 13:37:51 -0800

    Actor: Clean up local timeout and invoke delay in PageStabilityMonitor
    Change-Id: I56b2ed9fe13564d8c61d67993ccc3079920153af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7144098
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1543938}

--- chrome/browser/actor/tools/observation_delay_controller_browsertest.cc
+++ chrome/browser/actor/tools/observation_delay_controller_browsertest.cc
@@ -302,11 +302,8 @@ class ObservationDelayControllerTest
     scoped_feature_list_.InitWithFeaturesAndParameters(
         /*enabled_features=*/
         {{features::kGlicActor,
-          {// Effectively disable the timeouts to prevent flakes.
-           {features::kGlicActorPageStabilityLocalTimeout.name, "30000ms"},
+          {// Effectively disable the timeout to prevent flakes.
            {features::kGlicActorPageStabilityTimeout.name, "30000ms"},
-           // Do not use an invoke delay
-           {features::kGlicActorPageStabilityInvokeCallbackDelay.name, "0ms"},
            // Use small LCP delay.
            {features::kActorObservationDelayLcp.name, "100ms"}}},
          {features::kGlic, {}},
@@ -489,11 +486,8 @@ class ObservationDelayControllerLcpTest
     scoped_feature_list_.InitWithFeaturesAndParameters(
         /*enabled_features=*/
         {{features::kGlicActor,
-          {// Effectively disable the timeouts to prevent flakes.
-           {features::kGlicActorPageStabilityLocalTimeout.name, "30000ms"},
+          {// Effectively disable the timeout to prevent flakes.
            {features::kGlicActorPageStabilityTimeout.name, "30000ms"},
-           // Do not use an invoke delay
-           {features::kGlicActorPageStabilityInvokeCallbackDelay.name, "0ms"},
            // Do not use min wait
            {features::kGlicActorPageStabilityMinWait.name, "0ms"},
            {features::kActorObservationDelayLcp.name, lcp_delay}}},
