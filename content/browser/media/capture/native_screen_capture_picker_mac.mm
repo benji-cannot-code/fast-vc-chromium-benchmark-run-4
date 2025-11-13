b49a602bd978c9cc2333b13d23755ebb9679ef42
Palak Agarwal <agpalak@chromium.org>
2025-11-13 12:46:37 -0800

    Mark kAllowChangingSelectedContent flag to enabled
    This is because the code is already checked by another experimental flag
    kUseSCContentSharingPicker which is not in an active state right now.
    When kUseSCContentSharingPicker will be rolled out in finch, this
    feature will also get used according to the config.
    
    Bug: 409475502
    Change-Id: Icafec14b8511cbd1e32f9c9695dba31b5ef5900e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7151119
    Reviewed-by: Tove Petersson <tovep@chromium.org>
    Commit-Queue: Palak Agarwal <agpalak@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1544469}

--- content/browser/media/capture/native_screen_capture_picker_mac.mm
+++ content/browser/media/capture/native_screen_capture_picker_mac.mm
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // follow an app into its fullscreen presentation mode.
 // TODO(crbug.com/409475502): Remove this feature once it has been rolled out to
 // stable for a few milestones.
-BASE_FEATURE(kAllowChangingSelectedContent, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kAllowChangingSelectedContent, base::FEATURE_ENABLED_BY_DEFAULT);
 
 using Source = webrtc::DesktopCapturer::Source;
 
