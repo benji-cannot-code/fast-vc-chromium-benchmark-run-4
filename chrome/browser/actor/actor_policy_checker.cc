1d3d774f65e1e996e438f83baef974d211c3317f
William Liu <liuwilliam@chromium.org>
2025-11-13 09:13:21 -0800

    Add a feature param switch to bypass the actor policy checker
    So non-offical build can access Bluedog easily. Also plan to
    Finch this param to all the DF population in case
    IsLikelyDogfoodClient() does not work reliably.
    
    Bug: 454961534, 454005651
    Change-Id: Ia2f05e2acf625a9da51f47cd3f375fbe18f260bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7151480
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: William Liu <liuwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1544304}

--- chrome/browser/actor/actor_policy_checker.cc
+++ chrome/browser/actor/actor_policy_checker.cc
@@ -263,6 +263,7 @@ bool ActorPolicyChecker::ComputeActOnWebCapability() {
 #if !BUILDFLAG(ENABLE_GLIC)
   return true;
 #else
+  bool policy_exemption = features::kGlicActorPolicyControlExemption.Get();
   bool is_likely_dogfood_client = IsLikelyDogfoodClient();
   auto* profile = service_->GetProfile();
   CHECK(profile);
@@ -277,6 +278,7 @@ bool ActorPolicyChecker::ComputeActOnWebCapability() {
   journal_->Log(
       GURL(), TaskId(), "ActorPolicyChecker::ComputeActOnWebCapability",
       JournalDetailsBuilder()
+          .Add("policy_exemption", base::ToString(policy_exemption))
           .Add("is_likely_dogfood_client",
                base::ToString(is_likely_dogfood_client))
           .Add("is_browser_managed", base::ToString(is_browser_managed))
@@ -287,7 +289,7 @@ bool ActorPolicyChecker::ComputeActOnWebCapability() {
                base::ToString(actuation_enabled_for_managed_user))
           .Build());
 
-  if (is_likely_dogfood_client) {
+  if (is_likely_dogfood_client || policy_exemption) {
     return true;
   }
   if (account_eligible_for_actuation_for_testing_) [[unlikely]] {
