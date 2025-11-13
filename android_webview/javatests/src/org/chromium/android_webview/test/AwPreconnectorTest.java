d9f114e99c736925d86e04773b83a4e4b294ab45
Peter Conn <peconn@chromium.org>
2025-11-13 12:18:10 -0800

    Make Profile#preconnect throw on invalid URL.
    Previously we failed silently. Throwing is better.
    
    Bug: 432235374
    Change-Id: I398b27d7cfc994d7381a13f9b37723e9242f0e46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7150638
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Reviewed-by: Alex Mitra <alexmitra@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1544420}

--- android_webview/javatests/src/org/chromium/android_webview/test/AwPreconnectorTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwPreconnectorTest.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.android_webview.test;
 
+import static org.junit.Assert.assertThrows;
+
 import androidx.test.filters.SmallTest;
 
 import org.junit.After;
@@ -89,6 +91,26 @@ public class AwPreconnectorTest extends AwParameterizedTest {
         } finally {
             serverThread2.shutdown();
         }
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"AndroidWebView"})
+    public void connects_invalidUrl() throws Exception {
+        AwBrowserContext profile = mTestRule.getProfileSync("Default", /* createIfNeeded= */ true);
+
+        ThreadUtils.runOnUiThreadBlocking(
+                () -> {
+                    assertThrows(
+                            IllegalArgumentException.class,
+                            () -> profile.getPreconnector().preconnect(new GURL("invalid")));
+                    // Note: We require the scheme (so https://www.example.com).
+                    assertThrows(
+                            IllegalArgumentException.class,
+                            () ->
+                                    profile.getPreconnector()
+                                            .preconnect(new GURL("www.example.com")));
+                });
     }
 
     // A Server that waits for a single connection to be opened, then closes.
