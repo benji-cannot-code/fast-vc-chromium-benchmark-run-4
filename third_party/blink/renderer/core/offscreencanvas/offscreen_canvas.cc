d67c722d19132ba09cfd0db960e6bedd960638d2
Colin Blundell <blundell@chromium.org>
2025-11-12 23:57:11 -0800

    [Blink] Remove FlushReason::kToBlob
    Neither FlushReason::kToBlob nor FlushReason::kOther are ever
    referenced other than being passed (i.e., they have equivalent
    semantics, which is "not one of the FlushReason values that is directly
    equality-checked somewhere in the code"). Hence, we can replace usage
    of the former with usage of the latter.
    
    Bug: 352263194
    Change-Id: I2c5291fbfdb1455760004cdd98914a7fae0ad761
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7146443
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1544191}

--- third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.cc
+++ third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.cc
@@ -339,7 +339,7 @@ ScriptPromise<Blob> OffscreenCanvas::convertToBlob(
   // Finalize frame will be called in GetImage, but if there's no
   // resourceProvider yet then the IsPaintable check will fail
   if (context_) {
-    context_->FinalizeFrame(FlushReason::kToBlob);
+    context_->FinalizeFrame(FlushReason::kOther);
   }
 
   if (!IsPaintable() || Size().IsEmpty()) {
@@ -358,7 +358,7 @@ ScriptPromise<Blob> OffscreenCanvas::convertToBlob(
 
   base::TimeTicks start_time = base::TimeTicks::Now();
   scoped_refptr<StaticBitmapImage> image_bitmap =
-      context_->GetImage(FlushReason::kToBlob);
+      context_->GetImage(FlushReason::kOther);
   if (image_bitmap) {
     auto* resolver = MakeGarbageCollected<ScriptPromiseResolver<Blob>>(
         script_state, exception_state.GetContext());
