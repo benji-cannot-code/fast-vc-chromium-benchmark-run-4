5f15e2d5878e928e6451a87bcbe9d1e5a2fec17c
Jinsuk Kim <jinsukkim@chromium.org>
2025-11-13 13:01:13 -0800

    [Android][Settings] Apply containment style to search results
    Extend PulseDrawable to support different radius at the top and bottom
    corners.
    
    Screenshot: https://drive.google.com/file/d/1tVJ5NIfEpwIkohIVr2qP9EOpMIY4L7lL/view?usp=drive_link
    Bug: 444471770
    Change-Id: I067ff0fd23ba94520c49c0fbc3c682018314e5ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7146640
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Moe Adel <adelm@google.com>
    Reviewed-by: Gazal Agarwal <agazal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1544494}

--- chrome/android/java/src/org/chromium/chrome/browser/settings/SettingsActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/SettingsActivity.java
@@ -284,7 +284,7 @@ public class SettingsActivity extends ChromeBaseAppCompatActivity
         if (ChromeFeatureList.sSearchInSettings.isEnabled()) {
             mSearchCoordinator =
                     new SettingsSearchCoordinator(
-                            this, this::getUseMultiColumn, mMultiColumnSettings);
+                            this, this::getUseMultiColumn, mMultiColumnSettings, mItemDecorations);
             mSearchCoordinator.initializeSearchUi();
         }
 
