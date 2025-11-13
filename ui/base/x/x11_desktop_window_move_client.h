e29a6c6192192190f7d9f6cb12829e7e9e5051aa
Lei Zhang <thestig@chromium.org>
2025-11-12 16:29:43 -0800

    Remove superfluous callback.h includes from //ui headers
    Remove unused includes mostly found by heuristics in headers that do not
    contain keywords like "Callback" or "Closure".
    
    Bug: 40318405
    Change-Id: I07e93692f9a78f9db9c3ed51de7d2b9d5c8bc2ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7139545
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Owners-Override: Nico Weber <thakis@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1544041}

--- ui/base/x/x11_desktop_window_move_client.h
+++ ui/base/x/x11_desktop_window_move_client.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define UI_BASE_X_X11_DESKTOP_WINDOW_MOVE_CLIENT_H_
 
 #include "base/component_export.h"
-#include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
 #include "ui/base/x/x11_move_loop_delegate.h"
 #include "ui/base/x/x11_whole_screen_move_loop.h"
--- ui/ozone/platform/drm/common/drm_wrapper.h
+++ ui/ozone/platform/drm/common/drm_wrapper.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/files/scoped_file.h"
-#include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
 #include "third_party/perfetto/include/perfetto/tracing/traced_value_forward.h"
 #include "ui/display/types/display_constants.h"
