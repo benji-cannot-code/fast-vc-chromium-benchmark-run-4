13c95db60aa5c1bd959965ef94636416ac0097d7
Jun Ishiguro <junis@google.com>
2025-11-13 09:14:15 -0800

    [glic] Add InstallAttributes to KioskArcvmAppServiceTest
    This CL creates ScopedStubInstallAttributes in KioskArcvmAppServiceTest.
    
    Bug: b:459884527
    Change-Id: I9d1b36cad4d0c604c919388aab6ba2e97d8b2b93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7149779
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1544305}

--- chrome/browser/ash/app_mode/arcvm_app/kiosk_arcvm_app_service_unittest.cc
+++ chrome/browser/ash/app_mode/arcvm_app/kiosk_arcvm_app_service_unittest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
 #include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile.h"
+#include "chromeos/ash/components/install_attributes/stub_install_attributes.h"
 #include "chromeos/ash/components/settings/cros_settings.h"
 #include "chromeos/ash/experiences/arc/session/arc_service_manager.h"
 #include "chromeos/ash/experiences/arc/session/arc_stop_reason.h"
@@ -191,6 +192,7 @@ class KioskArcvmAppServiceTest : public testing::Test {
   content::BrowserTaskEnvironment task_environment;
   ArcAppTest arc_app_test_;
 
+  ScopedStubInstallAttributes scoped_stub_install_attributes_;
   ScopedTestingCrosSettings scoped_testing_cros_settings_;
 
   data_decoder::test::InProcessDataDecoder in_process_data_decoder_;
