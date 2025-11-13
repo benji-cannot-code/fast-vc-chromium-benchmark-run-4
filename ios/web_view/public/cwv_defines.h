443abc323661bcf1485541372f11d3ce97132bba
Carlos Acosta <cyacosta@google.com>
2025-11-13 12:43:06 -0800

    [iOS] Add support for Virtual Card Numbers (VCN)
    Define the `IOS_WEB_VIEW_SUPPORTS_VCN` flag in `cwv_defines.h` to enable
    the use of Virtual Card Numbers within the iOS Web View environment.
    This allows dependent components to check for VCN support and utilize
    related functionality.
    
    Bug: 437893439
    Bug: 437893439
    Change-Id: I3007feb3fcf7a42c60de6b177dd57f6301db1ae7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7138801
    Reviewed-by: John Wu <jzw@chromium.org>
    Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    Commit-Queue: Carlos Acosta <cyacosta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1544463}

--- ios/web_view/public/cwv_defines.h
+++ ios/web_view/public/cwv_defines.h
@@ -139,4 +139,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Supports -[CWVDownloadTask redirectedURL]
 #define IOS_WEB_VIEW_SUPPORTS_DOWNLOAD_TASK_REDIRECTED_URL 1
 
+// Supports VCN.
+#define IOS_WEB_VIEW_SUPPORTS_VCN 1
+
 #endif  // IOS_WEB_VIEW_PUBLIC_CWV_DEFINES_H_
