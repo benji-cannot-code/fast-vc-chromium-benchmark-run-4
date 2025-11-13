115e2ced82b2bd22c03fb031d88bfe2596afa7bf
Peter Kvitek <kvitekp@chromium.org>
2025-11-13 12:58:37 -0800

    [headless] Fix scaled full screen window size handling on Windows
    This CL fixes a double scaling issue with zoomed windows on Windows in
    headless mode and adds relevant tests for all platforms. It also fixes a
    problem with render widget host not converting headless window bounds to
    DIP before passing them over to the renderer. This second problem was
    previously masked by the first one.
    
    Bug: 460458101
    Change-Id: Iae9af541ab2238b60ad4397227bff74f223d6828
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7129323
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1544489}

--- components/headless/test/data/protocol/shared/fullscreen-window-size-scaled.js
+++ components/headless/test/data/protocol/shared/fullscreen-window-size-scaled.js
@@ -1,14 +1,14 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2023 The Chromium Authors
+// Copyright 2025 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 //
-// META: --screen-info={1600x1200}
+// META: --screen-info={3200x2400 devicePixelRatio=2.0}
 // META: --window-size=800,600
 
 (async function(testRunner) {
-  const {dp} =
-      await testRunner.startBlank(`Tests fullscreen browser window size.`);
+  const {dp} = await testRunner.startBlank(
+      `Tests scaled fullscreen browser window size.`);
 
   await dp.Page.enable();
 
