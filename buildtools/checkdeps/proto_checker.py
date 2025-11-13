a2cc24434b3f2dbdd68a6f237d1e8fba4cd1c42f
jj <jj@imput.net>
2025-11-13 11:44:22 -0800

    [buildtools] Check entire file instead of bailing early
    Instead of scanning the entire source file, cpp_checker and
    proto_checker bail out early if the file appears to not have
    any more relevant preprocessor macros.
    
    Due to this, it ended up missing a bunch of things. For
    example, rev: 7130941, 7130961, 7131658, 7131539
    
    Make it so that they always scan the entire file.
    Together with crrev.com/c/7130901, this change is
    performance-neutral in local testing.
    
    Change-Id: Id81822f87bb14d6264abfacf741fc57e8fc7bc78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7132359
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1544362}

--- buildtools/checkdeps/proto_checker.py
+++ buildtools/checkdeps/proto_checker.py
@@ -21,9 +21,6 @@ class ProtoChecker(object):
       '.proto',
   ]
 
-  # The maximum number of non-import lines we can see before giving up.
-  _MAX_UNINTERESTING_LINES = 50
-
   # The maximum line length, this is to be efficient in the case of very long
   # lines (which can't be import).
   _MAX_LINE_LENGTH = 128
@@ -99,9 +96,6 @@ class ProtoChecker(object):
     last_import = 0
     with codecs.open(filepath, encoding='utf-8') as f:
       for line_num, line in enumerate(f):
-        if line_num - last_import > self._MAX_UNINTERESTING_LINES:
-          break
-
         line = line.strip()
 
         is_import, violation = self.CheckLine(rules, line, filepath)
