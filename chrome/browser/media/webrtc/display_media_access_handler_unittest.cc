2eb73bfad161264ac848856e9b54be21c0e22f31
Palak Agarwal <agpalak@chromium.org>
2025-11-13 09:34:18 -0800

    Add FAILED_DUE_TO_SHUTDOWN error when rfh is not valid
    This change also converts REQUEST_CANCELLED errors to
    FAILED_DUE_TO_SHUTDOWN errors as the reason for the request being
    cancelled is the context getting destroyed.
    
    Bug: 453600255, 416456028
    Change-Id: I5d4aded0eea35552377adda1729330dd62bd666d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7090133
    Reviewed-by: Tove Petersson <tovep@chromium.org>
    Commit-Queue: Palak Agarwal <agpalak@chromium.org>
    Reviewed-by: Simon Hangl <simonha@google.com>
    Cr-Commit-Position: refs/heads/main@{#1544340}

--- chrome/browser/media/webrtc/display_media_access_handler_unittest.cc
+++ chrome/browser/media/webrtc/display_media_access_handler_unittest.cc
@@ -438,10 +438,11 @@ TEST_P(DisplayMediaAccessHandlerActiveRfhTest, ProcessRequest) {
                  /*expect_result=*/true, /*expect_picker=*/active_rfh_,
                  request);
 
-  EXPECT_EQ(result,
-            active_rfh_
-                ? blink::mojom::MediaStreamRequestResult::OK
-                : blink::mojom::MediaStreamRequestResult::INVALID_STATE);
+  EXPECT_EQ(
+      result,
+      active_rfh_
+          ? blink::mojom::MediaStreamRequestResult::OK
+          : blink::mojom::MediaStreamRequestResult::FAILED_DUE_TO_SHUTDOWN);
 }
 
 #if BUILDFLAG(IS_CHROMEOS)
