f610bf68e94d3b4ceed2021df2e75336f807b381
Jan Wilken Dörrie <jdoerrie@chromium.org>
2025-11-13 08:54:43 -0800

    Use `UnexportableKeyServiceFactory` to get the Keychain access group.
    Replaces a hardcoded constant with a call to
    `UnexportableKeyServiceFactory::GetKeychainAccessGroup()` to ensure a
    consistent definition of the Keychain access group used for unexportable
    keys on macOS.
    
    Change-Id: I2f7b6c523149daecc781c572de868575397d321f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7151058
    Auto-Submit: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1544272}

--- chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.cc
+++ chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.cc
@@ -121,7 +121,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_MAC)
 #include "base/mac/process_requirement.h"
-#include "chrome/common/chrome_version.h"
+#include "chrome/browser/signin/bound_session_credentials/unexportable_key_service_factory.h"
 #endif  // BUILDFLAG(IS_MAC)
 
 namespace {
@@ -129,12 +129,6 @@ namespace {
 // The number of restarts to wait until removing the enable-benchmarking flag.
 constexpr int kEnableBenchmarkingCountdownDefault = 3;
 constexpr char kEnableBenchmarkingPrefId[] = "enable_benchmarking_countdown";
-
-#if BUILDFLAG(IS_MAC)
-constexpr char kUnexportableKeysKeychainAccessGroup[] =
-    MAC_TEAM_IDENTIFIER_STRING "." MAC_BUNDLE_IDENTIFIER_STRING
-                               ".unexportable-keys";
-#endif  // BUILDFLAG(IS_MAC)
 
 void RecordMemoryMetrics();
 
@@ -882,7 +876,8 @@ void RecordStartupMetrics() {
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
   crypto::UnexportableKeyProvider::Config config;
 #if BUILDFLAG(IS_MAC)
-  config.keychain_access_group = kUnexportableKeysKeychainAccessGroup;
+  config.keychain_access_group =
+      UnexportableKeyServiceFactory::GetKeychainAccessGroup();
 #endif  // BUILDFLAG(IS_MAC)
   crypto::MaybeMeasureTpmOperations(std::move(config));
 #endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
