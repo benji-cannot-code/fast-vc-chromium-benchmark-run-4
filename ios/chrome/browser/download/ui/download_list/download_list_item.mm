a0acb6c2b9784972448640bc4266dbc329d63d50
Min Xiong <minxiong@microsoft.com>
2025-11-12 19:47:09 -0800

    [iOS] Fix the canceled task can't be removed and the file exist check.
    - download_list_mediator.mm: Remove download records unconditionally
    after delete attempt
    - download_list_item.mm: Add empty path check in filePath getter
    - download_list_table_view_controller.mm: Add state and path validation
    before opening files
    
    Change-Id: I640ef9117aed21a912799ff47595b4b5485f2295
    Bug: 459848851
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7144218
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Min Xiong <minxiong@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1544132}

--- ios/chrome/browser/download/ui/download_list/download_list_item.mm
+++ ios/chrome/browser/download/ui/download_list/download_list_item.mm
@@ -123,6 +123,9 @@ NSString* const kStatusTextEmptyString = @"";
 }
 
 - (base::FilePath)filePath {
+  if (_downloadRecord.file_path.empty()) {
+    return base::FilePath();
+  }
   return ConvertToAbsoluteDownloadPath(_downloadRecord.file_path);
 }
 
