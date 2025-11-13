bed086f918e0d75633ae028f62bc0ad035260db6
Maggie Jennings <mjenn@google.com>
2025-11-13 12:02:48 -0800

    Reland "[Task List Bubble] Show the bubble on nudge or nudge click"
    This is a reland of commit 6da994fe6e1ac58ff5a9ffec0236971eaed97d21
    
    Removes the GlicWindowControllerUi Tests relating to the Task Icon as
    the Actuation view is deprecated and these tests are no longer relevant.
    Also adds policy check to some tests, as required by crrev.com/c/7142029
    
    Original change's description:
    > [Task List Bubble] Show the bubble on nudge or nudge click
    >
    > Adds a subscription pattern for the nudge controller to receive bubble
    > updates and show the bubble from the nudge.
    > Adds calls to show the ActorTaskListBubble on nudge click as well as
    > suppressing the nudge on a row click.
    > Also updates the nudge label to "Check your tasks" per recent UX
    > feedback.
    >
    > Screenshare demo: https://screencast.googleplex.com/cast/NTgyNTMzODc5MjczODgxNnw5MzRmZmQyOC0wYQ
    > (Note: Handoff button is set to pause tasks only for demo purposes)
    >
    > Bug: 440770955, 440773268
    > Change-Id: I227fa84ea744add87fe237e243ea4879487b59ee
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7128325
    > Reviewed-by: Abe Boujane <boujane@google.com>
    > Reviewed-by: Alison Gale <agale@chromium.org>
    > Reviewed-by: Christine Ying <chrstne@google.com>
    > Commit-Queue: Maggie Jennings <mjenn@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1543906}
    
    Bug: 440770955, 440773268
    Change-Id: Ib4feb804ffbcacc7fc91119671047e9a7ec99774
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7147467
    Reviewed-by: Alison Gale <agale@chromium.org>
    Commit-Queue: Maggie Jennings <mjenn@google.com>
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Commit-Queue: Dan Harrington <harringtond@chromium.org>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Auto-Submit: Maggie Jennings <mjenn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1544393}

--- chrome/browser/actor/ui/task_list_bubble/actor_task_list_bubble_controller.cc
+++ chrome/browser/actor/ui/task_list_bubble/actor_task_list_bubble_controller.cc
@@ -11,6 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/actor/actor_task.h"
 #include "chrome/browser/actor/resources/grit/actor_browser_resources.h"
 #include "chrome/browser/actor/ui/task_list_bubble/actor_task_list_bubble.h"
+#include "chrome/browser/ui/browser_element_identifiers.h"
+#include "chrome/browser/ui/views/interaction/browser_elements_views.h"
+#include "chrome/browser/ui/views/tabs/tab_strip_action_container.h"
 #include "ui/base/l10n/l10n_util.h"
 #if BUILDFLAG(ENABLE_GLIC)
 #include "chrome/browser/glic/public/glic_keyed_service.h"
@@ -24,7 +27,17 @@ ActorTaskListBubbleController::ActorTaskListBubbleController(
     BrowserWindowInterface* browser_window)
     : browser_(browser_window),
       scoped_unowned_user_data_(browser_window->GetUnownedUserDataHost(),
-                                *this) {}
+                                *this) {
+#if BUILDFLAG(ENABLE_GLIC)
+  if (auto* manager = tabs::GlicActorTaskIconManagerFactory::GetForProfile(
+          browser_->GetProfile())) {
+    bubble_state_change_callback_subscription_.push_back(
+        manager->RegisterTaskListBubbleStateChange(
+            base::BindRepeating(&ActorTaskListBubbleController::OnStateUpdate,
+                                base::Unretained(this))));
+  }
+#endif
+}
 
 ActorTaskListBubbleController::~ActorTaskListBubbleController() = default;
 
@@ -39,6 +52,9 @@ void ActorTaskListBubbleController::ShowBubble(views::View* anchor_view) {
   }
   bubble_widget_ =
       ActorTaskListBubble::ShowBubble(anchor_view, std::move(param_list));
+  if (widget_observation_.IsObserving()) {
+    widget_observation_.Reset();
+  }
   widget_observation_.Observe(bubble_widget_);
 }
 
@@ -54,6 +70,19 @@ ActorTaskListBubbleController::CreateRowButtonParamsForTaskState(
           base::Unretained(this), task_state.task_id),
   };
 }
+
+void ActorTaskListBubbleController::OnStateUpdate(
+    const actor::TaskId& task_id) {
+  if (auto* browser_view = BrowserElementsViews::From(browser_)) {
+    TabStripActionContainer* tab_strip_action_container =
+        browser_view->GetViewAs<TabStripActionContainer>(
+            kTabStripActionContainerElementId);
+    if (tab_strip_action_container &&
+        tab_strip_action_container->GetIsShowingGlicActorTaskIconNudge()) {
+      ShowBubble(tab_strip_action_container->glic_actor_task_icon());
+    }
+  }
+}
 #endif
 
 void ActorTaskListBubbleController::OnWidgetDestroyed(views::Widget* widget) {
@@ -67,19 +96,19 @@ void ActorTaskListBubbleController::GetOnTaskRowClickCallback(
   Profile* profile = browser_->GetProfile();
   auto* icon_manager =
       tabs::GlicActorTaskIconManagerFactory::GetForProfile(profile);
-  tabs::TabInterface* last_tab =
-      icon_manager->GetLastUpdatedTabForTaskId(task_id);
-  if (!last_tab) {
-    return;
+  if (tabs::TabInterface* last_tab =
+          icon_manager->GetLastUpdatedTabForTaskId(task_id)) {
+    int tab_index = browser_->GetTabStripModel()->GetIndexOfTab(last_tab);
+    browser_->GetTabStripModel()->ActivateTabAt(tab_index);
+    if (auto* glic_service =
+            glic::GlicKeyedServiceFactory::GetGlicKeyedService(profile)) {
+      glic_service->ToggleUI(browser_, /*prevent_close=*/true,
+                             glic::mojom::InvocationSource::kActorTaskIcon);
+    }
   }
-  int tab_index = browser_->GetTabStripModel()->GetIndexOfTab(last_tab);
-  browser_->GetTabStripModel()->ActivateTabAt(tab_index);
-  auto* glic_service =
-      glic::GlicKeyedServiceFactory::GetGlicKeyedService(profile);
-  if (glic_service) {
-    glic_service->ToggleUI(browser_, /*prevent_close=*/true,
-                           glic::mojom::InvocationSource::kActorTaskIcon);
-  }
+  // Regardless of tab navigation, remove the row and close the bubble when
+  // done.
+  icon_manager->RemoveRowFromTaskListBubble(task_id);
   bubble_widget_->Close();
 #endif
 }
