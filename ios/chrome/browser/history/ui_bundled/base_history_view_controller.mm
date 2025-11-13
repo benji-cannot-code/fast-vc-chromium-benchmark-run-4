971bbf76a17208bed07bc115bf13102913ca7328
Gauthier Ambard <gambard@chromium.org>
2025-11-13 00:25:56 -0800

    [iOS] Remove dead code
    This code is doing nothing and is not used.
    
    Bug: 443034511
    Change-Id: I847f67642654eb9a314337db06f57f71b3a7f265
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7146082
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1544207}

--- ios/chrome/browser/history/ui_bundled/base_history_view_controller.mm
+++ ios/chrome/browser/history/ui_bundled/base_history_view_controller.mm
@@ -22,8 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/drag_and_drop/model/drag_item_util.h"
 #import "ios/chrome/browser/drag_and_drop/model/table_view_url_drag_drop_handler.h"
 #import "ios/chrome/browser/history/ui_bundled/base_history_view_controller+subclassing.h"
-#import "ios/chrome/browser/history/ui_bundled/history_entries_status_item.h"
-#import "ios/chrome/browser/history/ui_bundled/history_entries_status_item_delegate.h"
 #import "ios/chrome/browser/history/ui_bundled/history_entry_inserter.h"
 #import "ios/chrome/browser/history/ui_bundled/history_entry_item.h"
 #import "ios/chrome/browser/history/ui_bundled/history_menu_provider.h"
@@ -104,7 +102,6 @@ static const base::TimeDelta kDelayUntilReadyToRemoveLoadingIndicatorsMs =
 }  // namespace
 
 @interface BaseHistoryViewController () <
-    HistoryEntriesStatusItemDelegate,
     HistoryEntryInserterDelegate,
     TableViewLinkHeaderFooterItemDelegate> {
   // Closure to request next page of history.
@@ -372,14 +369,6 @@ static const base::TimeDelta kDelayUntilReadyToRemoveLoadingIndicatorsMs =
   // history via delete browsing data.
   self.filterQueryResult = YES;
   [self showHistoryMatchingQuery:_currentQuery];
-}
-
-#pragma mark - HistoryEntriesStatusItemDelegate
-
-- (void)historyEntriesStatusItem:(HistoryEntriesStatusItem*)item
-               didRequestOpenURL:(const GURL&)URL {
-  // TODO(crbug.com/41366648): Migrate. This will navigate to the status message
-  // "Show Full History" URL.
 }
 
 #pragma mark - HistoryEntryInserterDelegate
--- ios/chrome/browser/history/ui_bundled/history_table_view_controller.mm
+++ ios/chrome/browser/history/ui_bundled/history_table_view_controller.mm
@@ -22,8 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/drag_and_drop/model/drag_item_util.h"
 #import "ios/chrome/browser/drag_and_drop/model/table_view_url_drag_drop_handler.h"
 #import "ios/chrome/browser/history/ui_bundled/base_history_view_controller+subclassing.h"
-#import "ios/chrome/browser/history/ui_bundled/history_entries_status_item.h"
-#import "ios/chrome/browser/history/ui_bundled/history_entries_status_item_delegate.h"
 #import "ios/chrome/browser/history/ui_bundled/history_entry_inserter.h"
 #import "ios/chrome/browser/history/ui_bundled/history_entry_item.h"
 #import "ios/chrome/browser/history/ui_bundled/history_menu_provider.h"
