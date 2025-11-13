455690e537bba78a6593e687ac8c027afed5c2f7
Anton Bershanskyi <bershanskyi@gmail.com>
2025-11-13 13:22:08 -0800

    [Extensions] Refactor enum LaunchType to later convert to enum class
    Add new members to non-class enum LaunchType to enable migration to enum
    class. For example, for LAUNCH_TYPE_REGULAR add kRegular to let code use
    LaunchType::kRegular. Refactior GetLaunchTypePrefValue() to validate
    read value.
    
    Bug: 420858216
    Change-Id: Ifcfaad1545fc525b919a7949ae6ced6af814cc29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7129661
    Commit-Queue: Anton Bershanskyi <bershanskyi@gmail.com>
    Reviewed-by: Solomon Kinard <solomonkinard@chromium.org>
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1544525}

--- chrome/browser/extensions/sync/extension_sync_data.cc
+++ chrome/browser/extensions/sync/extension_sync_data.cc
@@ -73,7 +73,7 @@ ExtensionSyncData::ExtensionSyncData()
       supports_disable_reasons_(false),
       incognito_enabled_(false),
       remote_install_(false),
-      launch_type_(LAUNCH_TYPE_INVALID) {}
+      launch_type_(LaunchType::kInvalid) {}
 
 ExtensionSyncData::ExtensionSyncData(const Extension& extension,
                                      bool enabled,
@@ -89,7 +89,7 @@ ExtensionSyncData::ExtensionSyncData(const Extension& extension,
                         update_url,
                         StringOrdinal(),
                         StringOrdinal(),
-                        LAUNCH_TYPE_INVALID) {}
+                        LaunchType::kInvalid) {}
 
 ExtensionSyncData::ExtensionSyncData(const Extension& extension,
                                      bool enabled,
@@ -198,7 +198,8 @@ void ExtensionSyncData::ToAppSpecifics(sync_pb::AppSpecifics* specifics) const {
 
   // The corresponding validation of this value during processing of an
   // ExtensionSyncData is in ExtensionSyncService::ApplySyncData.
-  if (launch_type_ >= LAUNCH_TYPE_FIRST && launch_type_ < NUM_LAUNCH_TYPES &&
+  if (launch_type_ >= LaunchType::kFirst &&
+      launch_type_ < LaunchType::kNumLaunchTypes &&
       sync_pb::AppSpecifics_LaunchType_IsValid(sync_launch_type)) {
     specifics->set_launch_type(sync_launch_type);
   }
@@ -280,7 +281,7 @@ bool ExtensionSyncData::PopulateFromAppSpecifics(
 
   launch_type_ = specifics.has_launch_type()
                      ? static_cast<LaunchType>(specifics.launch_type())
-                     : LAUNCH_TYPE_INVALID;
+                     : LaunchType::kInvalid;
 
   // Bookmark apps and chrome apps both have "app" specifics, but only bookmark
   // apps filled out the `bookmark_app*` fields.
