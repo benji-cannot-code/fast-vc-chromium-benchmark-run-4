7711e1f897697a8d221ed95bf90b8e437f1a6c46
Ambareesh Balaji <ambareeshbalaji@gmail.com>
2025-11-12 23:03:19 -0800

    RTCVideoDecoderAdapter: Ensure HW decoder can handle resolution
    This adds a check that the current resolution is supported by the
    decoder before decoding. This avoids the decoder stall after fallback
    until the next key frame.
    
    Bug: 458768128
    Change-Id: I0d48649373f5e0c68928ace831ad8e066cf2e021
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7134138
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1544173}

--- third_party/blink/renderer/platform/peerconnection/rtc_video_decoder_adapter.cc
+++ third_party/blink/renderer/platform/peerconnection/rtc_video_decoder_adapter.cc
@@ -235,6 +235,8 @@ class RTCVideoDecoderAdapter::Impl {
              CrossThreadOnceClosure flush_fail_cb);
   void RegisterDecodeCompleteCallback(webrtc::DecodedImageCallback* callback);
 
+  bool IsDecoderConfigSupported(const media::VideoDecoderConfig& config) const;
+
  private:
   std::optional<RTCVideoDecoderFallbackReason> NeedSoftwareFallback(
       media::VideoCodec codec,
@@ -448,6 +450,15 @@ void RTCVideoDecoderAdapter::Impl::RegisterDecodeCompleteCallback(
   // decode_complete_callback_ should be called once with a valid pointer.
   DCHECK_EQ(decode_complete_callback_, nullptr);
   decode_complete_callback_ = callback;
+}
+
+bool RTCVideoDecoderAdapter::Impl::IsDecoderConfigSupported(
+    const media::VideoDecoderConfig& config) const {
+  // This function is invoked by any thread. |gpu_factories_|'s lifetime is
+  // guaranteed by RTCVideoDecoder's client and the thread safety of
+  // IsDecoderConfigSupported() is guaranteed by the GpuVideoAcceleratorFactories.
+  return gpu_factories_->IsDecoderConfigSupported(config) !=
+         media::GpuVideoAcceleratorFactories::Supported::kFalse;
 }
 
 void RTCVideoDecoderAdapter::Impl::OnDecodeDone(media::DecoderStatus status) {
@@ -770,6 +781,17 @@ bool RTCVideoDecoderAdapter::CheckResolutionAndNumInstances(
         config_.codec(),
         RTCVideoDecoderFallbackReason::kParseErrorOnResolutionCheck);
     return false;
+  }
+
+  if (config_.coded_size() != *resolution) {
+    config_.set_coded_size(*resolution);
+    if (!impl_->IsDecoderConfigSupported(config_)) {
+      DVLOG(1) << "Unsupported resolution";
+      RecordRTCVideoDecoderFallbackReason(
+          config_.codec(),
+          RTCVideoDecoderFallbackReason::kUnsupportedResolution);
+      return false;
+    }
   }
 
   if (resolution->GetArea() >= kMinResolution.GetArea()) {
