c8ef97ea63590d52208c9322e2d9492a650b9cb1
Andrew Grieve <agrieve@chromium.org>
2025-11-13 12:21:51 -0800

    JNI Zero: Fix some types missing prefix when --prefix-package is used
    Bug: 460411818
    Change-Id: I68225d384c0696ede82e7ce15874e1a299ed9e61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7151718
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1544426}

--- third_party/jni_zero/test/java/src/org/jni_zero/TinySample.java
+++ third_party/jni_zero/test/java/src/org/jni_zero/TinySample.java
@@ -5,7 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.jni_zero;
 
-class TinySample {
+
+public class TinySample {
     @NativeMethods()
     interface Natives {
         void foo(Object a, int b);
--- third_party/jni_zero/test/java/src/org/jni_zero/extrapackage/ImportsTinySample.java
+++ third_party/jni_zero/test/java/src/org/jni_zero/extrapackage/ImportsTinySample.java
@@ -1,9 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.jni_zero.extrapackage;
 
+import org.jni_zero.NativeMethods;
 import org.jni_zero.TinySample;
 
-class ImportsTinySample {
+public class ImportsTinySample {
     TinySample a;
 
     @NativeMethods()
