f837f013036878302d3a6ca7b5db59a7ab8fa8e8
Xu Li <mxlg@google.com>
2025-11-13 11:41:51 -0800

    [iOS] Delete ShouldAllowShare API from the tab_helper.
    Bug: 458696435
    Change-Id: I705c2f737e818b495d1a0fa063ef312ff53a25ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7147419
    Commit-Queue: Xu Li <mxlg@google.com>
    Reviewed-by: Ernesto Izquierdo Clua <eic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1544357}

--- ios/chrome/browser/enterprise/data_controls/model/data_controls_tab_helper.h
+++ ios/chrome/browser/enterprise/data_controls/model/data_controls_tab_helper.h
@@ -45,9 +45,6 @@ class DataControlsTabHelper
   void ShouldAllowCut(base::OnceCallback<void(bool)> callback);
 
   // Determines if sharing should be allowed.
-  void ShouldAllowShare(base::OnceCallback<void(bool)> callback);
-
-  // Determines if sharing should be allowed.
   bool ShouldAllowShare();
 
   // Sets the command handler for Data Controls.
