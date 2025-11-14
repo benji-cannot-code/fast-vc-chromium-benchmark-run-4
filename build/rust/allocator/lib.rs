607e09ca52e2f5398bcdf591ebf3006cd5b16491
Milad Fa <mfarazma@ibm.com>
2025-11-14 10:18:00 -0800

    Simplify rustc_nightly_capability usage
    We can use the `RUSTC_BOOTSTRAP=1` env variable to make
    use of unstable features and flags even with a stable
    version of the rustc.
    
    Change-Id: I0da31024b50c340edb2b9663825e292436eaae1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7153321
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1545081}

--- build/rust/allocator/lib.rs
+++ build/rust/allocator/lib.rs
@@ -19,13 +19,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // TODO(https://crbug.com/410596442): Stop using unstable features here.
 // https://github.com/rust-lang/rust/issues/29603 tracks stabilization of the `linkage` feature.
-#![cfg_attr(not(rust_allocator_no_nightly_capability), feature(linkage))]
+#![feature(linkage)]
 // Required to apply `#[rustc_std_internal_symbol]` to our alloc error handler
 // so the name is correctly mangled as rustc expects.
 //
 // TODO(https://crbug.com/410596442): Stop using internal features here.
-#![cfg_attr(not(rust_allocator_no_nightly_capability), allow(internal_features))]
-#![cfg_attr(not(rust_allocator_no_nightly_capability), feature(rustc_attrs))]
+#![allow(internal_features)]
+#![feature(rustc_attrs)]
 
 /// Module that provides `#[global_allocator]` / `GlobalAlloc` interface for
 /// using an allocator from C++.
@@ -81,7 +81,6 @@ mod rust_allocator {
 /// `rustc`.
 ///
 /// TODO(https://crbug.com/410596442): Stop using internal features here.
-#[cfg(not(rust_allocator_no_nightly_capability))]
 mod both_allocators {
     use alloc_error_handler_impl_ffi::rust_allocator_internal as ffi;
 
@@ -92,9 +91,21 @@ mod both_allocators {
     #[linkage = "weak"]
     fn __rust_no_alloc_shim_is_unstable_v2() {}
 
+    #[cfg(rust_allocator_no_nightly_capability)]
+    #[rustc_std_internal_symbol]
+    #[linkage = "weak"]
+    fn __rust_no_alloc_shim_is_unstable() {}
+
     #[rustc_std_internal_symbol]
     #[linkage = "weak"]
     fn __rust_alloc_error_handler_should_panic_v2() -> u8 {
+        0
+    }
+
+    #[cfg(rust_allocator_no_nightly_capability)]
+    #[rustc_std_internal_symbol]
+    #[linkage = "weak"]
+    fn __rust_alloc_error_handler_should_panic() -> u8 {
         0
     }
 
@@ -105,37 +116,6 @@ mod both_allocators {
     fn __rust_alloc_error_handler(_size: usize, _align: usize) {
         // TODO(lukasza): Investigate if we can just call `std::process::abort()` here.
         // (Not really _needed_, but it could simplify code a little bit.)
-        unsafe { ffi::alloc_error_handler_impl() }
-    }
-}
-
-#[cfg(rust_allocator_no_nightly_capability)]
-mod both_allocators {
-    use alloc_error_handler_impl_ffi::rust_allocator_internal as ffi;
-
-    fn oom_should_panic_impl() -> u8 {
-        0
-    }
-
-    #[no_mangle]
-    fn __rust_no_alloc_shim_is_unstable() {}
-
-    #[no_mangle]
-    fn __rust_no_alloc_shim_is_unstable_v2() {}
-
-    #[no_mangle]
-    fn __rust_alloc_error_handler_should_panic() -> u8 {
-        oom_should_panic_impl()
-    }
-
-    #[no_mangle]
-    fn __rust_alloc_error_handler_should_panic_v2() -> u8 {
-        oom_should_panic_impl()
-    }
-
-    #[allow(non_upper_case_globals)]
-    #[no_mangle]
-    fn __rust_alloc_error_handler(_size: usize, _align: usize) {
         unsafe { ffi::alloc_error_handler_impl() }
     }
 }
