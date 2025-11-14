83c73b3e57a87bad7a06cbd013be30c569134ffc
Robbie Gibson <rkgibson@google.com>
2025-11-14 12:40:02 -0800

    [iOS] Add fade animations to NTP background image transitions
    This CL adds fade animations when transition to, from, and between NTP
    background images.
    
    Video: https://drive.google.com/file/d/1gna6uY6f-dohPcAFiV7A7LnLjFJIUs_6/view?usp=sharing&resourcekey=0-GAbtF2U8ypVX2gK0L_le2g
    
    Bug: b:456468183
    Change-Id: I3f4552c60b984bf4b25f9eb134982a91da7c14d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7153938
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1545202}

--- ios/chrome/browser/home_customization/ui/home_customization_image_view.mm
+++ ios/chrome/browser/home_customization/ui/home_customization_image_view.mm
@@ -111,6 +111,10 @@ CGRect UpdateDesiredFrame(CGRect desired_frame,
   [self updateImagePosition];
 }
 
+- (UIImage*)image {
+  return _imageView.image;
+}
+
 #pragma mark - View Initialization
 
 // Configures the image view and adds it to the scroll view.
