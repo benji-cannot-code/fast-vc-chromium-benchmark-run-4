0982fdaa5a8c7c7753825e807ec976c06e0b2496
Oleksandr Tara <otara@google.com>
2025-11-14 11:34:52 -0800

    Record user selected permission duration in attempt login tool
    Drive-by change: add attempt login test case in
    ActorLoginQualityLoggerTest
    
    Bug: 459397449, 454961534
    Change-Id: I60e701812cfb0806f472fc315c6fe109deb6ad1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7157373
    Commit-Queue: Oleksandr Tara <otara@google.com>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Auto-Submit: Oleksandr Tara <otara@google.com>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1545150}

--- chrome/browser/password_manager/actor_login/actor_login_quality_logger.h
+++ chrome/browser/password_manager/actor_login/actor_login_quality_logger.h
@@ -36,6 +36,9 @@ class ActorLoginQualityLogger
   void AddAttemptLoginDetails(
       optimization_guide::proto::ActorLoginQuality_AttemptLoginDetails
           attempt_login_details) override;
+  void SetPermissionPicked(
+      optimization_guide::proto::ActorLoginQuality_PermissionOption
+          permission_option) override;
   void UploadFinalLog(optimization_guide::ModelQualityLogsUploaderService*
                           mqls_uploader) const override;
 
