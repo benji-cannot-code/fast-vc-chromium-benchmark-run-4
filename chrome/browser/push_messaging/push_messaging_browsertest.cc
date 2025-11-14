ed699a3f56e09eb975ebf398510f3a2d67219c8b
Lily Chen <chlily@chromium.org>
2025-11-14 07:40:01 -0800

    Remove kIgnoreCertificateErrors in chrome/browser/push_messaging
    The switch --ignore-certificate-errors is frequently misused throughout
    the codebase by tests that use an HTTPS EmbeddedTestServer to serve
    URLs with custom hostname(s). Ignoring certificate errors is usually
    incorrect and unnecessary; the better way is to configure the HTTPS
    EmbeddedTestServer with the proper certificates.
    
    This is done by configuring valid test certs using variously:
     * net::EmbeddedTestServer::CERT_TEST_NAMES,
     * generated test certificate configured by:
          * net::EmbeddedTestServer::SetCertHostnames(),
          * manually setting `ServerCertificateConfig::dns_names`,
          * using the default ServerCertificateConfig configuration provided
            by content::BrowserTestBase with embedded_https_test_server().
    
    Some tests were also using the switch entirely unnecessarily as they
    were not using an HTTPS test server, or did not use custom hostnames.
    
    This CL covers test files in:
    chrome/browser/push_messaging
    
    This CL was uploaded by git cl split.
    
    R=peter@chromium.org
    
    Bug: 40147519
    Change-Id: Ic542602390ec468578ffdf4ecf55b047451022c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7142425
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Auto-Submit: Lily Chen <chlily@chromium.org>
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1544959}

--- chrome/browser/push_messaging/push_messaging_browsertest.cc
+++ chrome/browser/push_messaging/push_messaging_browsertest.cc
@@ -59,7 +59,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/gcm_driver/instance_id/instance_id_profile_service.h"
 #include "components/keep_alive_registry/keep_alive_registry.h"
 #include "components/keep_alive_registry/keep_alive_types.h"
-#include "components/network_session_configurator/common/network_switches.h"
 #include "components/permissions/permission_request_manager.h"
 #include "components/push_messaging/app_identifier.h"
 #include "components/push_messaging/app_identifier_test_support.h"
@@ -188,6 +187,8 @@ class PushMessagingBrowserTestBase
     https_server_ = std::make_unique<net::EmbeddedTestServer>(
         net::EmbeddedTestServer::TYPE_HTTPS);
     https_server_->ServeFilesFromSourceDirectory(GetChromeTestDataDir());
+    // Used for PushMessagingPartitionedBrowserTest.CrossOriginFrame.
+    https_server_->SetCertHostnames({"embedder.com", "requester.com"});
     content::SetupCrossSiteRedirector(https_server_.get());
 
     site_engagement::SiteEngagementScore::SetParamValuesForTesting();
@@ -197,10 +198,6 @@ class PushMessagingBrowserTestBase
     // Enable experimental features for subscription restrictions.
     command_line->AppendSwitch(
         switches::kEnableExperimentalWebPlatformFeatures);
-
-    // HTTPS server only serves a valid cert for localhost, so this is needed to
-    // load webby domains like "embedded.com" without an interstitial.
-    command_line->AppendSwitch(switches::kIgnoreCertificateErrors);
   }
 
   // InProcessBrowserTest:
