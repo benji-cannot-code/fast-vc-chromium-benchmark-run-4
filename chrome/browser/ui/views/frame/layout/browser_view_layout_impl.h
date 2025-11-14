a789851d2522ace6ae64cf8ebc84bf7a1c29fc1f
Dana Fried <dfried@chromium.org>
2025-11-14 14:09:37 -0800

    [Chrome Next] Add loading bar to new layout.
    This adds the WebUI loading bar to the new tabbed browser layout.
    
    Bug: 453717426
    Change-Id: I8124c66905698619332c83db736a5059cc9fea80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7153658
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1545252}

--- chrome/browser/ui/views/frame/layout/browser_view_layout_impl.h
+++ chrome/browser/ui/views/frame/layout/browser_view_layout_impl.h
@@ -55,9 +55,6 @@ class BrowserViewLayoutImpl : public BrowserViewLayout {
   // rendering bugs.
   void DoPostLayoutVisualAdjustments();
 
-  // Returns whether the top contents separator should go in the top container.
-  bool ContentsSeparatorInTopContainer() const;
-
   // Returns the minimum size of all toolbar-height content except the toolbar-
   // height side panel.
   gfx::Size GetMinimumMainAreaSize() const;
@@ -68,6 +65,15 @@ class BrowserViewLayoutImpl : public BrowserViewLayout {
   // Returns the type of tabstrip present.
   enum class TabStripType { kNone, kWebUi, kVertical, kHorizontal };
   TabStripType GetTabStripType() const;
+
+  // Returns the type of top separator.
+  enum class TopSeparatorType {
+    kNone,
+    kLoadingBar,
+    kTopContainer,
+    kMultiContents
+  };
+  TopSeparatorType GetTopSeparatorType() const;
 
   // BrowserViewLayout overrides:
   gfx::Point GetDialogPosition(const gfx::Size& dialog_size) const override;
