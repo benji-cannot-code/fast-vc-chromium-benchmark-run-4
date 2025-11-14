e9640e66d86d97e6cb91798b2ffdf0071aae919c
Wenyu Fu <wenyufu@chromium.org>
2025-11-13 22:03:06 -0800

    [BrowserControls] Always perform offset adjustment for animation
    BCIV currently cannot handle complex animation movements like
    SHOW_TOP_ANCHOR. We remove the check to `requestNewFrame` when
    performing the animation adjustment so browser can take over and apply
    offsets to layers.
    
    Bug: 417238089
    Change-Id: I9cb25fe686a74793d906baac80bea7127b902486
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7153120
    Reviewed-by: Peilin Wang <peilinwang@google.com>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1544751}

--- chrome/browser/browser_controls/android/java/src/org/chromium/chrome/browser/browser_controls/TopControlsStacker.java
+++ chrome/browser/browser_controls/android/java/src/org/chromium/chrome/browser/browser_controls/TopControlsStacker.java
@@ -229,7 +229,6 @@ public class TopControlsStacker implements BrowserControlsStateProvider.Observer
             repositionLayers(
                     mBrowserControlsSizer.getTopControlOffset(),
                     mBrowserControlsSizer.getTopControlsMinHeightOffset(),
-                    animate,
                     isBrowserControlsVisibilityForced());
         }
 
@@ -317,7 +316,6 @@ public class TopControlsStacker implements BrowserControlsStateProvider.Observer
     private void repositionLayers(
             int initialTopOffset,
             int initialTopControlsMinHeightOffset,
-            boolean requestNewFrame,
             boolean offsetsAppliedByBrowser) {
         if (!BrowserControlsUtils.isTopControlsRefactorOffsetEnabled()) return;
 
@@ -328,8 +326,6 @@ public class TopControlsStacker implements BrowserControlsStateProvider.Observer
                             + initialTopOffset
                             + " minHeightOffset="
                             + initialTopControlsMinHeightOffset
-                            + " requestNewFrame="
-                            + requestNewFrame
                             + " offsetsAppliedByBrowser="
                             + offsetsAppliedByBrowser);
         }
@@ -367,7 +363,7 @@ public class TopControlsStacker implements BrowserControlsStateProvider.Observer
         // updates to layer that's changed from visible -> hidden.
 
         // The algorithm adjust layer's offset based on whether the control is showing or hiding.
-        if (requestNewFrame && initialTopOffset != 0 && hasAnimatingLayer) {
+        if (hasAnimatingLayer && initialTopOffset != 0) {
             // When animated size change, the browser controls will try to ensure it snaps to its
             // resting position. We'll use the minHeight as comparison first, then compare the
             // topOffset.
@@ -588,8 +584,8 @@ public class TopControlsStacker implements BrowserControlsStateProvider.Observer
      * most layer until the specified layer **(exclusive)**.
      *
      * <p><b>Warning:</b> The height returned might not be accurate during {@link
-     * #repositionLayers(int, int, boolean, boolean)} ()}, so it should not be used to determine a
-     * layer's attribute.
+     * #repositionLayers(int, int, boolean)} ()}, so it should not be used to determine a layer's
+     * attribute.
      *
      * @param stopLayer the layer in the stack order to stop at.
      * @return the total height of the visible UI from the specified layer to the top, or {@link
@@ -652,10 +648,7 @@ public class TopControlsStacker implements BrowserControlsStateProvider.Observer
         if (mControls.isEmpty()) return;
 
         repositionLayers(
-                topOffset,
-                topControlsMinHeightOffset,
-                requestNewFrame,
-                requestNewFrame || isVisibilityForced);
+                topOffset, topControlsMinHeightOffset, requestNewFrame || isVisibilityForced);
     }
 
     /** Tear down |this| and clear all existing controls from the Map. */
