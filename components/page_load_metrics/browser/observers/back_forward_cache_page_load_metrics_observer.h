c11bfa9f529bcf0310e0cc5fc3ee25aba4ef2ad4
Chirag Arora <heychirag@google.com>
2025-11-14 04:28:22 -0800

    Revert "metrics: Add BFCachePolyfillSecond histogram in Incognito"
    OBSOLETE_HISTOGRAMS=ACT efforts are being wound down.
    
    Bug: b:455504667
    Change-Id: Ic6276dbfccafa0451e410e5aed972a22beafcf88
    Reverts: I1eb541953e87b3419c609299842abc8739519ab8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7150840
    Reviewed-by: Kamil Jarosz <kjarosz@google.com>
    Commit-Queue: Chirag Arora <heychirag@google.com>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1544867}

--- components/page_load_metrics/browser/observers/back_forward_cache_page_load_metrics_observer.h
+++ components/page_load_metrics/browser/observers/back_forward_cache_page_load_metrics_observer.h
@@ -41,9 +41,6 @@ extern const char
 extern const char
     kHistogramSecondRequestAnimationFrameAfterBackForwardCacheRestore[];
 extern const char
-    kHistogramSecondRequestAnimationFrameAfterBackForwardCacheRestoreIncognito
-        [];
-extern const char
     kHistogramThirdRequestAnimationFrameAfterBackForwardCacheRestore[];
 extern const char kHistogramFirstInputDelayAfterBackForwardCacheRestore[];
 extern const char kHistogramCumulativeShiftScoreAfterBackForwardCacheRestore[];
@@ -56,7 +53,7 @@ extern const char kHistogramCumulativeShiftScoreAfterBackForwardCacheRestore[];
 class BackForwardCachePageLoadMetricsObserver
     : public page_load_metrics::PageLoadMetricsObserver {
  public:
-  explicit BackForwardCachePageLoadMetricsObserver(bool is_incognito);
+  BackForwardCachePageLoadMetricsObserver();
 
   BackForwardCachePageLoadMetricsObserver(
       const BackForwardCachePageLoadMetricsObserver&) = delete;
@@ -175,9 +172,6 @@ class BackForwardCachePageLoadMetricsObserver
   // IDs for the navigations when the page is restored from the back-forward
   // cache.
   std::vector<ukm::SourceId> back_forward_cache_navigation_ids_;
-
-  // Whether the WebContents being observed is for an Incognito profile.
-  bool is_incognito_;
 };
 
 #endif  // COMPONENTS_PAGE_LOAD_METRICS_BROWSER_OBSERVERS_BACK_FORWARD_CACHE_PAGE_LOAD_METRICS_OBSERVER_H_
