9c8d0610363aa93c1bb36c1a118767e5d1968a59
Chirag Arora <heychirag@google.com>
2025-11-14 04:29:46 -0800

    Revert "metrics: Add PageLoad.Clients.Ads.AdPaintTiming.NavigationToFirstContentfulPaint3.Incognito histogram"
    OBSOLETE_HISTOGRAMS=ACT efforts are being wound down.
    
    Bug: b:455504667
    Change-Id: I7596e4f47adc53baacb4d76b81b3af20fbb8450a
    Reverts: I87e96f3c462cdaa773b8edbfacf4d3f89da4a7d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7151199
    Reviewed-by: Kamil Jarosz <kjarosz@google.com>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Chirag Arora <heychirag@google.com>
    Cr-Commit-Position: refs/heads/main@{#1544870}

--- android_webview/browser/page_load_metrics/page_load_metrics_initialize.cc
+++ android_webview/browser/page_load_metrics/page_load_metrics_initialize.cc
@@ -47,7 +47,6 @@ class PageLoadMetricsEmbedder
   page_load_metrics::PageLoadMetricsMemoryTracker*
   GetMemoryTrackerForBrowserContext(
       content::BrowserContext* browser_context) override;
-  bool IsIncognito(content::WebContents* web_contents) override;
 
  protected:
   // page_load_metrics::PageLoadMetricsEmbedderBase:
@@ -101,11 +100,6 @@ PageLoadMetricsEmbedder::GetMemoryTrackerForBrowserContext(
 
   return AwPageLoadMetricsMemoryTrackerFactory::GetForBrowserContext(
       browser_context);
-}
-
-bool PageLoadMetricsEmbedder::IsIncognito(content::WebContents* web_contents) {
-  // Android web view doesn't have Incognito mode.
-  return false;
 }
 
 }  // namespace
--- components/page_load_metrics/browser/test_metrics_web_contents_observer_embedder.cc
+++ components/page_load_metrics/browser/test_metrics_web_contents_observer_embedder.cc
@@ -248,11 +248,6 @@ bool TestMetricsWebContentsObserverEmbedder::ShouldObserveScheme(
   return false;
 }
 
-bool TestMetricsWebContentsObserverEmbedder::IsIncognito(
-    content::WebContents* web_contents) {
-  return false;
-}
-
 PageLoadMetricsMemoryTracker*
 TestMetricsWebContentsObserverEmbedder::GetMemoryTrackerForBrowserContext(
     content::BrowserContext* browser_context) {
