1b846eb832a44b25df973cd6bd87bffbe1b1484f
Thomas Thrainer <thomasth@google.com>
2025-11-14 05:17:29 -0800

    Desktop plumbing for the `Sync.TrustedVaultAddKeysSuccessfully` metric
    This CL makes sure to pass the correct
    `TrustedVaultUserActionTriggerForUMA` value to the `TrustedVaultClient`
    during user initiated key retrievals.
    This ensures that the `Sync.TrustedVaultAddKeysSuccessfully` metric can
    be emitted with the correct trigger value (see
    https://crrev.com/c/7130940).
    
    Bug: 458664467
    Change-Id: I6d4486468c45c3a7332f95fe61ae63e76a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7140379
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Commit-Queue: Thomas Thrainer <thomasth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1544892}

--- chrome/browser/trusted_vault/trusted_vault_encryption_keys_tab_helper.h
+++ chrome/browser/trusted_vault/trusted_vault_encryption_keys_tab_helper.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "chrome/common/trusted_vault_encryption_keys_extension.mojom.h"
+#include "components/trusted_vault/trusted_vault_client.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
 
@@ -44,6 +45,11 @@ class TrustedVaultEncryptionKeysTabHelper
 
   ~TrustedVaultEncryptionKeysTabHelper() override;
 
+  // Set the user action that triggered opening the WebContents this tab helper
+  // is associated with.
+  void SetUserActionTrigger(
+      trusted_vault::TrustedVaultUserActionTriggerForUMA trigger);
+
   // content::WebContentsObserver:
   void DidFinishNavigation(
       content::NavigationHandle* navigation_handle) override;
@@ -69,6 +75,9 @@ class TrustedVaultEncryptionKeysTabHelper
   // metrics).
   const raw_ptr<trusted_vault::TrustedVaultService> trusted_vault_service_;
   const raw_ptr<EnclaveManager> enclave_manager_;
+
+  std::optional<trusted_vault::TrustedVaultUserActionTriggerForUMA>
+      user_action_trigger_;
 
   WEB_CONTENTS_USER_DATA_KEY_DECL();
 };
