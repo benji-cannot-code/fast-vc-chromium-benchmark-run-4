8ad450c4f8c7d49cb418e7a5fd62ade8fd7fb3e1
Egor Pasko <pasko@chromium.org>
2025-11-14 08:48:32 -0800

    android: Update license in ashmem.h
    Update license of [1] to match the one in ashmem-dev.c. We chose the
    Apache 2.0 licence when moving the files, but forgot to update the
    license notice in one of the file headers.
    
    Also, to remove presubmit warnings replace some tab characters by spaces
    and clang-format the file after that.
    
    [1] base/android/linker/ashmem.h
    
    Bypass-Check-License: Moved files
    Bug: 433478402
    Change-Id: I024137c722d59bfdcab39bb5880fe02440962880
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7157214
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Egor Pasko <pasko@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1545009}

--- base/android/linker/ashmem-dev.c
+++ base/android/linker/ashmem-dev.c
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * limitations under the License.
  */
 
+// This file is partially taken from AOSP and keeps its license.
+
 #include "ashmem.h"
 
 #include <dlfcn.h>
