c45d20b24a75a762e39ec8c2cbeb533ca06ba5da
Arthur Sonzogni <arthursonzogni@chromium.org>
2025-11-14 05:19:42 -0800

    Convert to UNSAFE_TODO in gpu/ipc/common
    This is an automated #cleanup patch using the [Script] below.
    
    We are migrating from coarse-grained file-level suppression (#pragma
    allow_unsafe_buffers) to granular, expression-level markers
    (UNSAFE_TODO()). The pragma disables safety checks for an entire file,
    whereas UNSAFE_TODO() isolates specific potentially unsafe operations, allowing
    the rest of the file to be enforced as safe.
    
    This CL was uploaded by an experimental version of git cl split
    (https://crbug.com/389069356).
    
    Script: https://docs.google.com/document/d/1ORQGBNn2R-CEvNbDTjRd-GrOBOFlCxIHdcvSUA_EhR4/edit?usp=sharing
    AX-Relnotes: N/A
    Cleanup: This is an automated #cleanup.
    Bug: 409340989
    Change-Id: I116b80a324076f3776bb989499c6c1648408fa38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7132300
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Ali Hijazi <ahijazi@chromium.org>
    Commit-Queue: Ali Hijazi <ahijazi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1544894}

--- gpu/ipc/common/dxgi_helpers.cc
+++ gpu/ipc/common/dxgi_helpers.cc
@@ -3,14 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "gpu/ipc/common/dxgi_helpers.h"
 
 #include "base/check.h"
+#include "base/compiler_specific.h"
 #include "base/debug/crash_logging.h"
 #include "base/debug/dump_without_crashing.h"
 #include "base/logging.h"
@@ -198,8 +194,8 @@ bool CopyD3D11TexToMem(
 
   return libyuv::NV12Copy(
              source_buffer, source_stride,
-             source_buffer + texture_desc.Height * source_stride, source_stride,
-             dst_buffer.data(), dest_stride,
+             UNSAFE_TODO(source_buffer + texture_desc.Height * source_stride),
+             source_stride, dst_buffer.data(), dest_stride,
              dst_buffer.subspan(texture_desc.Height * dest_stride).data(),
              dest_stride, texture_desc.Width, texture_desc.Height) == 0;
 }
