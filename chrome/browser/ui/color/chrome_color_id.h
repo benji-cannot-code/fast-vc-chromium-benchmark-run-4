84f30eeaaec8e4371b0d3e94137f20a4ea9043a4
Omar Flores <omarflores@google.com>
2025-11-13 16:31:17 -0800

    [ntp-next] Updating theming for dark mode and correct colors
    Screencast: https://screencast.googleplex.com/cast/NDk4OTMyNTc5NTEzMTM5MnwyMmNmYTI4Ny0yMg
    
    Bug: 459985025
    Change-Id: I0669075ba1f131e88f0db671514744bcaaaa5f23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7152083
    Reviewed-by: Jennifer Serrano <jennserrano@google.com>
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Commit-Queue: Omar Flores <omarflores@google.com>
    Cr-Commit-Position: refs/heads/main@{#1544640}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -245,6 +245,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorNewTabPageButtonForeground) \
   E_CPONLY(kColorNewTabPageCartModuleDiscountChipBackground) \
   E_CPONLY(kColorNewTabPageCartModuleDiscountChipForeground) \
+  E_CPONLY(kColorNewTabPageActionChipTextBody) \
+  E_CPONLY(kColorNewTabPageActionChipTextTitle) \
+  E_CPONLY(kColorNewTabPageActionChipDeepSearchIcon) \
   E_CPONLY(kColorNewTabPageChipBackground) \
   E_CPONLY(kColorNewTabPageChipForeground) \
   E_CPONLY(kColorComposeboxBackground) \
