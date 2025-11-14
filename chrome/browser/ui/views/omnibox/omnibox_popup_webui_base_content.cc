096d332bd58d280e3b4b26e1b470baebabe569f0
David Yeung <dayeung@chromium.org>
2025-11-13 16:58:02 -0800

    [Omnibox WebUI popup] Set the preferred size on auto resize
    On resize events, make sure the WebView has set the preferred size. This
    will cause a content height of 0 in some cases because the preferred
    height is incorrect.
    
    This is likely what is attributing the dropdown to show as a 0 content
    height.
    
    Bug: 460332162, 460446437
    Change-Id: Ic0f163bbe53c85b5cb5e8e582b85e4dacd751c1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7152663
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Marlon Facey <mfacey@chromium.org>
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1544658}

--- chrome/browser/ui/views/omnibox/omnibox_popup_webui_base_content.cc
+++ chrome/browser/ui/views/omnibox/omnibox_popup_webui_base_content.cc
@@ -112,6 +112,7 @@ void OmniboxPopupWebUIBaseContent::ShowCustomContextMenu(
 void OmniboxPopupWebUIBaseContent::ResizeDueToAutoResize(
     content::WebContents* source,
     const gfx::Size& new_size) {
+  WebView::ResizeDueToAutoResize(source, new_size);
   if (GetVisible()) {
     popup_presenter_->SetWidgetContentHeight(new_size.height());
   }
