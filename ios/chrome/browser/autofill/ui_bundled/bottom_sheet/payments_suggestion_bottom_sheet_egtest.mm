8d796f20b3dc3ea00a8844c64c7fa252386cff24
Benjamin Williams <bwwilliams@google.com>
2025-11-14 08:46:12 -0800

    [iOS][2024 PE] Remove Cancel button from 'Payment methods' sheet
    This CL removes the "Cancel" button from the 'Payment methods' Autofill
    sheet, as the "Done" button (in iOS 26 rendered as a checkmark) provides
    the same functionality.
    
    Screenshots:
    Before: https://screenshot.googleplex.com/9BCEJ9g8RQ77DCQ
    After: https://screenshot.googleplex.com/34kevn9kAgoEaVZ
    
    Bug: 41493004
    Change-Id: I5d1caf6bff7d7c7dfa426418cdd0022aea08bd1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7151838
    Auto-Submit: Benjamin Williams <bwwilliams@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1545005}

--- ios/chrome/browser/autofill/ui_bundled/bottom_sheet/payments_suggestion_bottom_sheet_egtest.mm
+++ ios/chrome/browser/autofill/ui_bundled/bottom_sheet/payments_suggestion_bottom_sheet_egtest.mm
@@ -596,7 +596,7 @@ void CheckAutofillSuggestionAcceptedIndexMetricsCount(
 
   // Close the context menu.
   [[EarlGrey
-      selectElementWithMatcher:chrome_test_util::NavigationBarCancelButton()]
+      selectElementWithMatcher:chrome_test_util::NavigationBarDoneButton()]
       performAction:grey_tap()];
 
   [ChromeEarlGreyUI waitForAppToIdle];
@@ -787,7 +787,7 @@ void CheckAutofillSuggestionAcceptedIndexMetricsCount(
 
   // Close the context menu.
   [[EarlGrey
-      selectElementWithMatcher:chrome_test_util::NavigationBarCancelButton()]
+      selectElementWithMatcher:chrome_test_util::NavigationBarDoneButton()]
       performAction:grey_tap()];
 
   // Try to reopen the bottom sheet.
