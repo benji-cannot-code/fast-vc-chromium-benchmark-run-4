b03a46ecb7c103d0472d6e9847217c9d31983aca
Sourav <sinhasourav@google.com>
2025-11-14 10:35:34 -0800

    [iOSFidoImportExport] Add Select/Deselect All button
    This CL introduces a "Select all" / "Deselect all" button in the toolbar
    of the CredentialExportViewController. This allows users to easily
    toggle the selection state of all listed passwords and passkeys for
    export.
    
    Bug: 460194802
    Change-Id: Ifbe046d470de9d39e01e082bbab4de246b63a39c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7147902
    Reviewed-by: Noémie St-Onge <noemies@google.com>
    Reviewed-by: Rafał Godlewski <rgod@google.com>
    Commit-Queue: Sourav Uttam Sinha <sinhasourav@google.com>
    Cr-Commit-Position: refs/heads/main@{#1545094}

--- ios/chrome/browser/credential_exchange/ui/credential_export_constants.h
+++ ios/chrome/browser/credential_exchange/ui/credential_export_constants.h
@@ -11,4 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Accessibility identifier for the continue button.
 extern NSString* const kCredentialExportContinueButtonAccessibilityIdentifier;
 
+// Identifier for the Select/Deselect All button in the toolbar.
+extern NSString* const kCredentialExportSelectAllButtonAccessibilityIdentifier;
+
 #endif  // IOS_CHROME_BROWSER_CREDENTIAL_EXCHANGE_UI_CREDENTIAL_EXPORT_CONSTANTS_H_
