0d2e6297491e7ac5ddae26e73984ff05c48ef2f7
Marc Treib <treib@chromium.org>
2025-11-14 00:32:32 -0800

    BookmarkDataTypeProcessor: Handle GC directives
    This adds handling of GC directives to the bookmark processor, in the
    same way as it was implemented in the ClientTagBasedDataTypeProcessor in
    https://crrev.com/c/7003557: By basically treating it as an incremental
    update, but synthesizing deletions for all entities that aren't
    contained in the update.
    
    One small difference is in detecting the "still existing" entities:
    In the ClientTagBasedDataTypeProcessor this is simply based on client
    tag hashes, but for historical reasons bookmarks don't always have a
    CTH. Instead, here it is based on DetermineLocalTrackedEntityToUpdate().
    
    Bug: 459528897
    Change-Id: Ie06b01248cf8ae58e250cec93e486de16a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7146900
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1544782}

--- components/sync_bookmarks/synced_bookmark_tracker_entity.cc
+++ components/sync_bookmarks/synced_bookmark_tracker_entity.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/trace_event/memory_usage_estimator.h"
 #include "components/bookmarks/browser/bookmark_node.h"
+#include "components/sync/engine/commit_and_get_updates_types.h"
 #include "components/sync/protocol/entity_data.h"
 #include "components/sync/protocol/entity_metadata.pb.h"
 #include "components/sync/protocol/entity_specifics.pb.h"
@@ -49,6 +50,13 @@ SyncedBookmarkTrackerEntity::~SyncedBookmarkTrackerEntity() = default;
 
 bool SyncedBookmarkTrackerEntity::IsUnsynced() const {
   return metadata_.sequence_number() > metadata_.acked_sequence_number();
+}
+
+bool SyncedBookmarkTrackerEntity::IsUnsyncedLocalCreation() const {
+  // `kUncommittedVersion` implies that the entity is unsynced but add this
+  // condition for clarity.
+  return metadata_.server_version() == syncer::kUncommittedVersion &&
+         IsUnsynced();
 }
 
 bool SyncedBookmarkTrackerEntity::MatchesData(
