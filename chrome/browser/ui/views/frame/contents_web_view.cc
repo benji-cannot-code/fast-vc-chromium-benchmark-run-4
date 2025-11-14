990924d2c9d0542dd85dd0bdd1a1d5a755eff4bb
Thomas Lukaszewicz <tluk@chromium.org>
2025-11-13 21:48:45 -0800

    Ensure ContentsWebView always sets an opaque or transparent BG color
    This CL ensures that the kColorWebContentsBackground mixer definition
    always results in a completely opaque color.
    
    This CL addresses an issue on linux where native gtk mixer
    definitions resulted in a semi-transparent kColorWebContentsBackground
    which caused CHECK crashes in
    RenderWidgetHostViewBase::SetBackgroundColor().
    
    DumpWithoutCrashing() is added to ContentsWebView to de-risk merging.
    This will be updated to a CHECK in follow-up CLs.
    
    Fixed: 456309057
    Change-Id: Ia9b1938a772cd8b7a9ba24f1f80ddd82e8aff512
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7153999
    Auto-Submit: Tom Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1544746}

--- chrome/browser/ui/views/frame/contents_web_view.cc
+++ chrome/browser/ui/views/frame/contents_web_view.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/frame/contents_web_view.h"
 
+#include "base/debug/dump_without_crashing.h"
 #include "chrome/browser/ui/color/chrome_color_id.h"
 #include "chrome/browser/ui/views/frame/web_contents_close_handler.h"
 #include "chrome/browser/ui/views/status_bubble_views.h"
@@ -122,6 +123,15 @@ void ContentsWebView::UpdateBackgroundColor() {
   const SkColor color = GetColorProvider()->GetColor(
       is_letterboxing() ? kColorWebContentsBackgroundLetterboxing
                         : kColorWebContentsBackground);
+  // `color` must be opaque, see RenderWidgetHostView::SetBackgroundColor() for
+  // details.
+  // TODO(crbug.com/456309057): Update this to be a CHECK.
+  if (SkColorGetA(color) != SK_AlphaOPAQUE) {
+    base::debug::DumpWithoutCrashing();
+    if (background_visible_) {
+      return;
+    }
+  }
 
   ui::Layer* background_layer = layer();
   background_layer->SetColor(background_visible_ ? color : SK_ColorTRANSPARENT);
