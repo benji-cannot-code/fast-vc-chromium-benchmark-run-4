3dbfdfc63a676e97ce24a644fa97ca36b88396e3
dpapad <dpapad@chromium.org>
2025-11-14 10:34:35 -0800

    WebUI: Implement @webui-eslint/web-component-missing-deps check.
    This check finds all child custom elements used in an .html.ts template
    file and compares them with the imports appearing in the corresponding
    class definition file to find missing dependencies.
    
    Once enabled it will programmatically address a very frequent source of
    developer confusion where direct dependencies are not always explicitly
    imported, often leading to hard to debug failures in tests.
    
    It will be enabled gradually in follow-up CLs by passing
    enable_web_component_missing_deps in build_webui() as violations are
    fixed. After that it will be enabled by default.
    
    Bug: 457866803
    Change-Id: Ib5cef7d62e910aaccf6ac0814ff5bf3ca35577e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7123208
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1545093}

--- ui/webui/resources/tools/tests/eslint_ts/eslint_expected.config.mjs
+++ ui/webui/resources/tools/tests/eslint_ts/eslint_expected.config.mjs
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import path from 'path';
 
-import defaultConfig from '../../../eslint_ts.config_base.mjs';
+import {defaultConfig} from '../../../eslint_ts.config_base.mjs';
 
 export default [
   ...defaultConfig,
---
+++ ui/webui/resources/tools/tests/eslint_ts/with_webui_plugin_web_component_missing_deps_violations.ts
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Test file for @webui-eslint/web-component-missing-deps
+
+import 'chrome://resources/cr_elements/cr_expand_button/cr_expand_button.js';
+import './other_button.js';
+
+import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
+
+import {getHtml} from './with_webui_plugin_web_component_missing_deps_violations.html.js';
+
+export class DummyButtonElement extends CrLitElement {
+  static get is() {
+    return 'dummy-button';
+  }
+
+  override render() {
+    return getHtml.bind(this)();
+  }
+}
+
+customElements.define(DummyButtonElement.is, DummyButtonElement);
