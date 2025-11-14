027db27d6c677b63bb701fa00494f8141e7330f7
Zoraiz Naeem <zoraiznaeem@chromium.org>
2025-11-13 16:14:00 -0800

    [glic] Use NOT_DRAWN_LAYER for GlicWidget on ChromeOS
    Currently GlicWidget's TEXTURE_LAYER is used only to paint the
    background of GlicView. However, after crrev.com/c/7148097, the
    background will be handled by separate SOLID_COLOR_LAYER. Therefore
    nothing is painted in the TEXTURE_LAYER of the widget.
    
    Therefore, we can use the NOT_DRAWN_LAYER to save on memory, reduce
    paint invalidation and reduce overdraw.
    
    Bug: b:452406346, b:459890634
    Change-Id: Ibb1b2223b413fc42aa5df5509eb99eb4ceb56b2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7148357
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1544632}

--- chrome/browser/glic/widget/glic_widget.cc
+++ chrome/browser/glic/widget/glic_widget.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/base_window.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/color/color_provider_key.h"
+#include "ui/compositor/layer_type.h"
 #include "ui/display/display.h"
 #include "ui/display/display_finder.h"
 #include "ui/display/screen.h"
@@ -312,6 +313,8 @@ std::unique_ptr<GlicWidget> GlicWidget::Create(views::WidgetDelegate* delegate,
   params.wayland_app_id = params.wm_class_class + "-glic";
 #endif  // BUILDFLAG(IS_LINUX)
 #if BUILDFLAG(IS_CHROMEOS)
+  // TODO(b:452406346): Use a `LAYER_NOT_DRAWN` for all the platforms.
+  params.layer_type = ui::LAYER_NOT_DRAWN;
   params.shadow_type = views::Widget::InitParams::ShadowType::kDrop;
   params.init_properties_container.SetProperty(
       chromeos::kShouldHaveHighlightBorderOverlay, true);
