aacee576a26bcc7ac6c3a2c2d5999cb32ab5764b
Benjamin Keen <bkeen@google.com>
2025-11-14 08:54:29 -0800

    [CodeHealth] Remove stale base::Feature ImageAnnotationServiceExperimental
    Removing stale feature since it was introduced in M74 and is currently
    disabled.
    
    Bug: 356624985
    Change-Id: I92159d28ffcb5731d75c2a8cf65bba8518a546f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7154099
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Benjamin Keen <bkeen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1545013}

--- services/image_annotation/image_annotation_service.h
+++ services/image_annotation/image_annotation_service.h
@@ -9,9 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
-#include "base/feature_list.h"
 #include "base/memory/scoped_refptr.h"
-#include "base/metrics/field_trial_params.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "services/image_annotation/annotator.h"
@@ -19,9 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 
 namespace image_annotation {
-
-// Whether or not to override service parameters for experimentation.
-BASE_DECLARE_FEATURE(kImageAnnotationServiceExperimental);
 
 class ImageAnnotationService : public mojom::ImageAnnotationService {
  public:
@@ -38,27 +33,6 @@ class ImageAnnotationService : public mojom::ImageAnnotationService {
   ~ImageAnnotationService() override;
 
  private:
-  // Service params:
-
-  // The url of the service that fetches descriptions given image pixels.
-  static constexpr base::FeatureParam<std::string> kPixelsServerUrl{
-      &kImageAnnotationServiceExperimental, "server_url",
-      "https://ckintersect-pa.googleapis.com/v1/intersect/pixels"};
-  // The url of the service that returns the supported languages.
-  static constexpr base::FeatureParam<std::string> kLangsServerUrl{
-      &kImageAnnotationServiceExperimental, "langs_server_url",
-      "https://ckintersect-pa.googleapis.com/v1/intersect/langs"};
-  // An override Google API key. If empty, the API key with which the browser
-  // was built (if any) will be used instead.
-  static constexpr base::FeatureParam<std::string> kApiKey{
-      &kImageAnnotationServiceExperimental, "api_key", ""};
-  static constexpr base::FeatureParam<int> kThrottleMs{
-      &kImageAnnotationServiceExperimental, "throttle_ms", 300};
-  static constexpr base::FeatureParam<int> kBatchSize{
-      &kImageAnnotationServiceExperimental, "batch_size", 10};
-  static constexpr base::FeatureParam<double> kMinOcrConfidence{
-      &kImageAnnotationServiceExperimental, "min_ocr_confidence", 0.7};
-
   // mojom::ImageAnnotationService implementation:
   void BindAnnotator(mojo::PendingReceiver<mojom::Annotator> receiver) override;
 
