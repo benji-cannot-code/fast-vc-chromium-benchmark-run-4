1bd37da68ce32b1fbef7e6da88bedc777b283d2b
Alex Ilin <alexilin@chromium.org>
2025-11-14 08:36:18 -0800

    [DBSC] ScopedMockUnexportableKeyProvider to use MockUnexportableKeyProvider
    Refactor ScopedMockUnexportableKeyProvider to encapsulate a
    MockUnexportableKeyProvider member instead of inheriting from
    crypto::UnexportableKeyProvider and manually defining MOCK_METHODs.
    
    This reduces code duplication as MockUnexportableKeyProvider already
    defines the necessary mock methods.
    
    Change-Id: Iadddf38e9327719ec1b9240c7263f9da78a5b893
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7156562
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Auto-Submit: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1545001}

--- components/unexportable_keys/unexportable_key_service_impl_unittest.cc
+++ components/unexportable_keys/unexportable_key_service_impl_unittest.cc
@@ -440,7 +440,7 @@ TEST_F(UnexportableKeyServiceImplTest, DeleteKeyCallsProvider) {
 
   // Delete the key.
   EXPECT_CALL(
-      SwitchToMockKeyProvider(),
+      SwitchToMockKeyProvider().mock(),
       DeleteSigningKeySlowly(Eq(service().GetWrappedKey(key_id).value())))
       .WillOnce(Return(true));
 
