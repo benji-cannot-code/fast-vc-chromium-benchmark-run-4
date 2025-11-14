a4eb1cef6678438884127744b6ba1652d8512368
Jan Wilken Dörrie <jdoerrie@chromium.org>
2025-11-14 06:43:00 -0800

    [dbsc] Implement asynchronous deletion of unexportable keys.
    This change introduces a new `DeleteKeyTask` within the
    `UnexportableKeyTaskManager` to handle the asynchronous deletion of
    signing keys. The `UnexportableKeyServiceImpl::DeleteKeySlowlyAsync`
    method now dispatches the deletion request to the task manager, which in
    turn uses the underlying
    `UnexportableKeyProvider::DeleteSigningKeySlowly` API. New tests are
    added to cover the successful and failed scenarios of key deletion.
    
    Bug: 455538141
    Change-Id: Id49fb3ba51b8b77ab35c93d675d4c010ba3b688b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7139664
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Auto-Submit: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1544937}

--- components/unexportable_keys/background_task_type.cc
+++ components/unexportable_keys/background_task_type.cc
@@ -20,6 +20,8 @@ std::string_view GetBackgroundTaskTypeSuffixForHistograms(
       return ".FromWrappedKey";
     case BackgroundTaskType::kSign:
       return ".Sign";
+    case BackgroundTaskType::kDeleteKey:
+      return ".DeleteKey";
   }
   NOTREACHED();
 }
