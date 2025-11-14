0f0d4f0c608a342fa0f3e50b14ac201af46a5cce
Jonathan Ross <jonross@chromium.org>
2025-11-14 09:31:09 -0800

    Invalidate AsyncLayerTreeFrameSink WeakPtrs on Compositor Thread
    The `WeakPtrFactory` is bound on `AsyncLayerTreeFrameSink::BindToClient`
    on the Compositor Thread.
    
    Teardown of `AsyncLayerTreeFrameSink` is on the Renderer Main thread.
    
    So the Linux Chromium Asan bot detected incorrect thread invalidation.
    
    Here we explicitly invalidate the `WeakPtrs` on the Compositor thread
    during `DetachFromClient`.
    
    TEST: SitePerProcessInteractiveBrowserTest.ShowAndHideDatePopupInOOPIFMultipleTimes
    Bug: 460466081
    Change-Id: Ie2f8772f8c7c357fb7e6682b89518fc0c7250384
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7153623
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Stacy Gaikovaia <gaiko@google.com>
    Cr-Commit-Position: refs/heads/main@{#1545043}

--- cc/mojo_embedder/async_layer_tree_frame_sink.cc
+++ cc/mojo_embedder/async_layer_tree_frame_sink.cc
@@ -182,6 +182,7 @@ void AsyncLayerTreeFrameSink::DetachFromClient() {
   compositor_frame_sink_.reset();
   compositor_frame_sink_associated_.reset();
   LayerTreeFrameSink::DetachFromClient();
+  weak_factory_.InvalidateWeakPtrs();
 }
 
 void AsyncLayerTreeFrameSink::SetLocalSurfaceId(
