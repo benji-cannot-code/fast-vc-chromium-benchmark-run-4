a690e4960c3ef2976d65a5d14dd257deb0515771
Jan Wilken Dörrie <jdoerrie@chromium.org>
2025-11-14 04:44:07 -0800

    [dbsc] Make `ScopedMockUnexportableKeyProvider` a gMock object
    This change refactors `ScopedMockUnexportableKeyProvider` to inherit
    from `crypto::UnexportableKeyProvider` and use `MOCK_METHOD` for its
    interface. A `ForwardingUnexportableKeyProvider` is used to direct calls
    from the global mock provider to the `ScopedMockUnexportableKeyProvider`
    instance. Default behaviors are set up in the constructor using
    `ON_CALL`. This allows tests to set specific expectations on the mock
    provider's methods.
    
    Change-Id: Iabd87deda66b877be24e367b7c357854c2bf29fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7156357
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1544878}

--- components/unexportable_keys/scoped_mock_unexportable_key_provider.h
+++ components/unexportable_keys/scoped_mock_unexportable_key_provider.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/queue.h"
 #include "crypto/unexportable_key.h"
+#include "testing/gmock/include/gmock/gmock.h"
 
 namespace unexportable_keys {
 
@@ -15,18 +16,37 @@ namespace unexportable_keys {
 // `MockUnexportableKey`s while it is in scope.
 // The mock provider will return mock keys previously added via
 // `AddNextGeneratedKey()` in the queue order.
-class ScopedMockUnexportableKeyProvider {
+class ScopedMockUnexportableKeyProvider
+    : public crypto::UnexportableKeyProvider {
  public:
   ScopedMockUnexportableKeyProvider();
   ScopedMockUnexportableKeyProvider(const ScopedMockUnexportableKeyProvider&) =
       delete;
   ScopedMockUnexportableKeyProvider& operator=(
       const ScopedMockUnexportableKeyProvider&) = delete;
-  ~ScopedMockUnexportableKeyProvider();
+  ~ScopedMockUnexportableKeyProvider() override;
+
+  // crypto::UnexportableKeyProvider:
+  MOCK_METHOD(std::optional<crypto::SignatureVerifier::SignatureAlgorithm>,
+              SelectAlgorithm,
+              (base::span<const crypto::SignatureVerifier::SignatureAlgorithm>
+                   acceptable_algorithms),
+              (override));
+  MOCK_METHOD(std::unique_ptr<crypto::UnexportableSigningKey>,
+              GenerateSigningKeySlowly,
+              (base::span<const crypto::SignatureVerifier::SignatureAlgorithm>
+                   acceptable_algorithms),
+              (override));
+  MOCK_METHOD(std::unique_ptr<crypto::UnexportableSigningKey>,
+              FromWrappedSigningKeySlowly,
+              (base::span<const uint8_t> wrapped_key),
+              (override));
+  MOCK_METHOD(bool,
+              DeleteSigningKeySlowly,
+              (base::span<const uint8_t> wrapped_key),
+              (override));
 
   void AddNextGeneratedKey(std::unique_ptr<crypto::UnexportableSigningKey> key);
-
-  std::unique_ptr<crypto::UnexportableSigningKey> GetNextGeneratedKey();
 
  private:
   base::queue<std::unique_ptr<crypto::UnexportableSigningKey>>
