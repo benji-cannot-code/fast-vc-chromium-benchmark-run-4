ce95fe4693ae14c61c87a38b5700900cebe3db78
Etienne Bergeron <etienneb@google.com>
2025-11-14 13:44:50 -0800

    Add experiment to not use pending transaction
    This CL is adding a finch experiment to make field study and confirm
    that the use of the pending transaction is not required.
    
    The UkmDatabase is using WAL mode which is a journaling mode that keeps
    the pending changes into a separate (db-wal). The main benefit of that
    journaling mode is to reduce the heavy wait of sync operations of writes
    into the main database.
    
    The pending transaction mechanism was intended to mimic this behavior.
    Changes were kept and not committed to the main db until enough changes
    were available.
    
    Change-Id: Ice16980a9b4eec1e35608bc07ca2c3849ceab30e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7133103
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1545239}

--- components/segmentation_platform/internal/database/ukm_database_backend.cc
+++ components/segmentation_platform/internal/database/ukm_database_backend.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/check_is_test.h"
+#include "base/feature_list.h"
 #include "base/files/file_util.h"
 #include "base/functional/bind.h"
 #include "base/logging.h"
@@ -24,6 +25,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sql/transaction.h"
 
 namespace segmentation_platform {
+
+BASE_FEATURE(kInhibitTransactionFromSegmentationDB,
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 namespace {
 
@@ -117,6 +121,8 @@ UkmDatabaseBackend::UkmDatabaseBackend(
       url_table_(&db_),
       uma_metrics_table_(&db_) {
   DETACH_FROM_SEQUENCE(sequence_checker_);
+  inhibit_transaction_ =
+      base::FeatureList::IsEnabled(kInhibitTransactionFromSegmentationDB);
   db_.set_error_callback(base::BindRepeating(&ErrorCallback));
 }
 
@@ -391,6 +397,9 @@ void UkmDatabaseBackend::DeleteAllUrls() {
 
 void UkmDatabaseBackend::TrackChangesInTransaction(int change_count) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  if (inhibit_transaction_) {
+    return;
+  }
 
   // No transaction has begun, begin one.
   if (!current_transaction_) {
@@ -409,6 +418,10 @@ void UkmDatabaseBackend::TrackChangesInTransaction(int change_count) {
 
 void UkmDatabaseBackend::RestartTransaction() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  if (inhibit_transaction_) {
+    return;
+  }
 
   if (current_transaction_) {
     current_transaction_->Commit();
