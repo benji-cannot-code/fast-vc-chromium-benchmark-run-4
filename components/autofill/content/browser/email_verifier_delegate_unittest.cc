78d42b3bb516ec86c5ffc9d0673c85b7f04744dd
Jan Keitel <jkeitel@google.com>
2025-11-14 02:49:48 -0800

    Reland "Use RunOnceCallback in //components/autofill"
    This reverts commit d5fcaf567235849cedafd0630c190bc5096a02e4.
    
    Reason for revert: Used RunOnceCallbackRepeatedly instead of
    RunOnceCallback whenever they are used in a default action.
    
    The change compared to the original CL is contain in PS1 -> PS2.
    
    Original change's description:
    > Revert "Use RunOnceCallback in //components/autofill"
    >
    > This reverts commit 5d94ec68b314a0035e6788552e645e995d71568e.
    >
    > Reason for revert:
    > CreditCardFormEventLoggerTest.CreditCardSelectedFormEventsFillTwice fails
    >
    > Original change's description:
    > > Use RunOnceCallback in //components/autofill
    > >
    > > Bug: 40100455
    > > Change-Id: Id27e499f9eac439630961c249d76df04bf16534f
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7146408
    > > Auto-Submit: Jan Keitel <jkeitel@google.com>
    > > Reviewed-by: Christoph Schwering <schwering@google.com>
    > > Commit-Queue: Christoph Schwering <schwering@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1544336}
    >
    > Bug: 40100455, 460496352
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 40100455
    > Change-Id: I1d9cb713d9d4853ae48bbabaf989d544a8e9373a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7153340
    > Owners-Override: Shu Yang <shuyng@google.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Shu Yang <shuyng@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1544516}
    
    Bug: 40100455, 460496352
    Change-Id: I5b4633d55b57b05ebb09ebd8efbd6348d5b6738b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7156694
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1544836}

--- components/autofill/content/browser/email_verifier_delegate_unittest.cc
+++ components/autofill/content/browser/email_verifier_delegate_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/autofill/content/browser/email_verifier_delegate.h"
 
+#include "base/test/gmock_callback_support.h"
 #include "base/test/mock_callback.h"
 #include "base/test/scoped_feature_list.h"
 #include "components/autofill/core/browser/autofill_field.h"
@@ -25,10 +26,10 @@ namespace autofill {
 
 namespace {
 
+using ::base::test::RunOnceCallback;
 using ::testing::_;
 using ::testing::NiceMock;
 using ::testing::Return;
-using ::testing::WithArgs;
 
 class MockEmailVerifier : public content::webid::EmailVerifier {
  public:
@@ -116,10 +117,7 @@ TEST_F(EmailVerifierDelegateTest, VerificationTriggered) {
   form->field(0)->set_autofilled_type(EMAIL_ADDRESS);
 
   EXPECT_CALL(*email_verifier_, Verify("test@example.com", "test_nonce", _))
-      .WillOnce(WithArgs<2>(
-          [](content::webid::EmailVerifier::OnEmailVerifiedCallback callback) {
-            std::move(callback).Run("test_token");
-          }));
+      .WillOnce(RunOnceCallback<2>("test_token"));
 
   EXPECT_CALL(*driver_, DispatchEmailVerifiedEvent(form->field(0)->global_id(),
                                                    "test_token"));
@@ -253,10 +251,7 @@ TEST_F(EmailVerifierDelegateTest, VerificationFails) {
   form->field(0)->set_autofilled_type(EMAIL_ADDRESS);
 
   EXPECT_CALL(*email_verifier_, Verify)
-      .WillOnce(WithArgs<2>(
-          [](content::webid::EmailVerifier::OnEmailVerifiedCallback callback) {
-            std::move(callback).Run(std::nullopt);
-          }));
+      .WillOnce(RunOnceCallback<2>(std::nullopt));
 
   // When the verification fails, the event is not dispatched.
   EXPECT_CALL(*driver_, DispatchEmailVerifiedEvent).Times(0);
