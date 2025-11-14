2aea7a34467e326967d6dfc6a111b1d2fd7075dc
Steinar H. Gunderson <sesse@chromium.org>
2025-11-14 06:13:02 -0800

    Make LocalFrame not Supplementable.
    Part of Supplementable cleanup. Done with a script followed by
    human cleanup/review and git cl format.
    
    This required extending ForwardDeclaredMember with an extra
    (optional) parameter to support supplements that inherit twice
    from GarbageCollectedMixin.
    
    Bug: 460165754
    Change-Id: Ic8ac4b3dd20810b77943a5b36955cbc597a7e82f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7151118
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1544917}

--- third_party/blink/renderer/controller/dev_tools_frontend_impl.h
+++ third_party/blink/renderer/controller/dev_tools_frontend_impl.h
@@ -52,7 +52,6 @@ class LocalFrame;
 // it's host (mojom.DevToolsFrontendHost) is destroyed.
 class DevToolsFrontendImpl final
     : public GarbageCollected<DevToolsFrontendImpl>,
-      public Supplement<LocalFrame>,
       public mojom::blink::DevToolsFrontend,
       public InspectorFrontendClient,
       public WidgetCreationObserver {
@@ -91,6 +90,7 @@ class DevToolsFrontendImpl final
   // InspectorFrontendClient implementation.
   void SendMessageToEmbedder(base::Value::Dict) override;
 
+  Member<LocalFrame> local_frame_;
   Member<DevToolsHost> devtools_host_;
   String api_script_;
   // The host_ must outlive the ExecutionContext of LocalFrame, so it should not
