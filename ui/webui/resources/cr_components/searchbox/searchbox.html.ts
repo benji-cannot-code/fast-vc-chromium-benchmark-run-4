a09f95708bdba6bb97bc8d725b5dadf78971d45c
Dhruv Kathpalia <dhruvkathpalia@google.com>
2025-11-14 13:16:51 -0800

    [ntp-realbox] Fix Realbox Next drag and drop overlay.
    This change moves the animated glow inside the dropdown container for
    Realbox Next. It also adds styles to make the searchbox input and icons
    transparent when a file is being dragged over the Realbox Next, and
    lowers the z-index of the compose button during file drag. The
    border-radius override for Realbox Next with a visible dropdown is
    removed.
    Before:
    http://screencast/cast/NjM0MzQwMzIwMDU3NzUzNnxiMTg0NWFkZi0wZg
    After:
    http://screencast/cast/NjE0NTU0OTIyMzg1NDA4MHw2MmI5ZmVkYS0yNA
    http://screencast/cast/NDc1MzI0NDg0MzY3MTU1MnxiN2JlMGYyNS0xNw
    
    Bug: b:460480937
    Change-Id: I3a10ed43c9b7de7491ef85ff6871cba0ba745871
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7158550
    Commit-Queue: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Reviewed-by: Hannah Collins <hannahrogers@google.com>
    Cr-Commit-Position: refs/heads/main@{#1545224}

--- ui/webui/resources/cr_components/searchbox/searchbox.html.ts
+++ ui/webui/resources/cr_components/searchbox/searchbox.html.ts
@@ -10,8 +10,6 @@ import type {SearchboxElement} from './searchbox.js';
 export function getHtml(this: SearchboxElement) {
   // clang-format off
   return html`<!--_html_template_start_-->
-<search-animated-glow animation-state="${this.animationState}">
-</search-animated-glow>
 ${this.ntpRealboxNextEnabled ? html`
 <ntp-error-scrim id="errorScrim"
     ?compact-mode="${this.searchboxLayoutMode === 'Compact'}">
@@ -58,6 +56,8 @@ ${this.ntpRealboxNextEnabled ? html`
 
   ${this.ntpRealboxNextEnabled ? html`
     <div class="dropdownContainer">
+      <search-animated-glow animation-state="${this.animationState}">
+      </search-animated-glow>
       <contextual-entrypoint-and-carousel id="context"
           part="contextual-entrypoint-and-carousel"
           exportparts="composebox-entrypoint, context-menu-entrypoint-icon, voice-icon"
