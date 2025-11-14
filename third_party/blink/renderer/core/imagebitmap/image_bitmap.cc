559134a74de4504ca0701f1fba8ca5e271abb8cb
Colin Blundell <blundell@chromium.org>
2025-11-14 01:08:28 -0800

    [Blink] Remove FlushReason::kCreateImageBitmap
    Neither FlushReason::kCreateImageBitmap nor FlushReason::kOther are ever
    referenced other than being passed (i.e., they have equivalent
    semantics, which is "not one of the FlushReason values that is directly
    equality-checked somewhere in the code"). Hence, we can replace usage
    of the former with usage of the latter.
    
    Note: The FlushReason params passed to StaticBitmapImageTransform
    bottom out in being passed to CanvasResourceProvider::Snapshot() [1].
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/static_bitmap_image_transform.cc;l=288;drc=373d7fdb08be061501a27cf72cb0ff846fbd0899;bpv=1;bpt=1
    
    Bug: 352263194
    Change-Id: I3d55ea40aa3894140fe566bc549a73c9dd89929c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7147346
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1544799}

--- third_party/blink/renderer/core/imagebitmap/image_bitmap.cc
+++ third_party/blink/renderer/core/imagebitmap/image_bitmap.cc
@@ -241,8 +241,7 @@ scoped_refptr<StaticBitmapImage> ApplyTransformsFromOptions(
   params.sampling = options.sampling;
   params.source_rect = options.source_rect;
   params.dest_size = options.dest_size;
-  return StaticBitmapImageTransform::Apply(FlushReason::kCreateImageBitmap,
-                                           source, params);
+  return StaticBitmapImageTransform::Apply(FlushReason::kOther, source, params);
 }
 
 scoped_refptr<StaticBitmapImage> MakeBlankImage(
@@ -375,7 +374,7 @@ ImageBitmap::ImageBitmap(HTMLCanvasElement* canvas,
                          const ImageBitmapOptions* options) {
   SourceImageStatus status;
   scoped_refptr<Image> image_input = canvas->GetSourceImageForCanvas(
-      FlushReason::kCreateImageBitmap, &status, gfx::SizeF());
+      FlushReason::kOther, &status, gfx::SizeF());
   if (status != kNormalSourceImageStatus)
     return;
   DCHECK(IsA<StaticBitmapImage>(image_input.get()));
@@ -399,8 +398,7 @@ ImageBitmap::ImageBitmap(OffscreenCanvas* offscreen_canvas,
                          const ImageBitmapOptions* options) {
   SourceImageStatus status;
   scoped_refptr<Image> raw_input = offscreen_canvas->GetSourceImageForCanvas(
-      FlushReason::kCreateImageBitmap, &status,
-      gfx::SizeF(offscreen_canvas->Size()));
+      FlushReason::kOther, &status, gfx::SizeF(offscreen_canvas->Size()));
   DCHECK(IsA<StaticBitmapImage>(raw_input.get()));
   scoped_refptr<StaticBitmapImage> input =
       static_cast<StaticBitmapImage*>(raw_input.get());
@@ -514,8 +512,8 @@ scoped_refptr<StaticBitmapImage> ImageBitmap::Transfer() {
     // This approach is slow and wateful but it is only to handle extremely
     // rare edge cases.
     if (!image_->HasOneRef()) {
-      auto copy = StaticBitmapImageTransform::Clone(
-          FlushReason::kCreateImageBitmap, image_);
+      auto copy =
+          StaticBitmapImageTransform::Clone(FlushReason::kOther, image_);
       if (!copy) {
         return nullptr;
       }
--- third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.cc
+++ third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.cc
@@ -298,7 +298,7 @@ ScriptPromise<ImageBitmap> OffscreenCanvas::CreateImageBitmap(
     return EmptyPromise();
   }
   if (context_) {
-    context_->FinalizeFrame(FlushReason::kCreateImageBitmap);
+    context_->FinalizeFrame(FlushReason::kOther);
   }
   return ImageBitmapSource::FulfillImageBitmap(
       script_state,
