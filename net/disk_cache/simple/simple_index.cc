cdb0439e06beaac2ab1a9fa68b13652596226f6c
Yoichi Osato <yoichio@chromium.org>
2025-11-13 20:07:23 -0800

    Add disk_cache TRACEs to simple cache.
    This adds TRACEs to see the disk cache performance and timing.
    
    Bug: 460304696
    Change-Id: I2ee0df87292d83e4d3d0c96f1df1ac2ae8efea09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7149421
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1544707}

--- net/disk_cache/simple/simple_index.cc
+++ net/disk_cache/simple/simple_index.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/task_runner.h"
 #include "base/time/time.h"
 #include "base/trace_event/memory_usage_estimator.h"
+#include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
 #include "net/base/features.h"
 #include "net/base/net_errors.h"
@@ -644,6 +645,8 @@ bool SimpleIndex::UpdateEntryIteratorSize(
 
 void SimpleIndex::MergeInitializingSet(
     std::unique_ptr<SimpleIndexLoadResult> load_result) {
+  TRACE_EVENT("disk_cache", "SimpleIndex::MergeInitializingSet", "cache_type_",
+              cache_type_);
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   EntrySet* index_file_entries = &load_result->entries;
