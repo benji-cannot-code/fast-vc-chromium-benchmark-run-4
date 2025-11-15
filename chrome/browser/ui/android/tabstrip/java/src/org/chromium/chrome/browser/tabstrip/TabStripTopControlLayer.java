802f16fddb101968a053e1f6e8abf1296bd30753
Wenyu Fu <wenyufu@chromium.org>
2025-11-14 17:51:27 -0800

    [BrowserControls] Modularize TabStripTopControlLayer
    Move TabStripTopControlLayer to its own component before more change are
    added.
    
    Bug: 454115281
    Change-Id: I2e10108d2b304dd53b8c65c7ac6bb2439c5a43f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7159217
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1545365}

--- chrome/browser/ui/android/tabstrip/java/src/org/chromium/chrome/browser/tabstrip/TabStripTopControlLayer.java
+++ chrome/browser/ui/android/tabstrip/java/src/org/chromium/chrome/browser/tabstrip/TabStripTopControlLayer.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.toolbar;
+package org.chromium.chrome.browser.tabstrip;
 
 import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.build.annotations.NullMarked;
@@ -18,7 +18,7 @@ import org.chromium.chrome.browser.toolbar.top.tab_strip.TabStripTransitionCoord
  * during tab strip height transition.
  */
 @NullMarked
-class TabStripTopControlLayer extends ObservableSupplierImpl<Integer>
+public class TabStripTopControlLayer extends ObservableSupplierImpl<Integer>
         implements TopControlLayer, TabStripTransitionHandler {
 
     public TabStripTopControlLayer(int tabStripHeight) {
