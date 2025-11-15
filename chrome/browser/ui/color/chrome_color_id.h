88cd34cfcc59b123dc291f290bbf06bf109dcd14
Teresa Mao <temao@chromium.org>
2025-11-14 21:02:35 -0800

    [omnibox-next] Add color for the arrow icon in the submit button.
    Bug: 459815241
    Change-Id: I161b0447d723c13bc8203f84d1f57e3916efb5db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7159252
    Commit-Queue: Darryl James <dljames@chromium.org>
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Auto-Submit: Teresa Mao <temao@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1545407}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -388,6 +388,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorOmniboxComposeboxForegroundDisabled) \
   E_CPONLY(kColorOmniboxComposeboxPrimaryAction) \
   E_CPONLY(kColorOmniboxComposeboxSubmitButtonBackground) \
+  E_CPONLY(kColorOmniboxComposeboxSubmitButtonIcon) \
   E_CPONLY(kColorOmniboxContextEntrypointHoverBackground) \
   E_CPONLY(kColorOmniboxContextEntrypointText) \
   E_CPONLY(kColorOmniboxIconBackground) \
