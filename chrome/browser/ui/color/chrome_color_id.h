8076bcf0b6881a8e8aaf726ede8f0deb6dc6c83f
Jilin Yang <jilinyang@google.com>
2025-11-14 18:12:53 -0800

    [NTP-Real-Box]: Correct the Loading spinner color
    https://screencast.googleplex.com/cast/NTcxNzcyMzU1NTI5OTMyOHxjMTkyMTIwNy1kMA
    
    Fixed: 452369590
    
    Change-Id: I6f661c43553e420c39cba676187cf8dbd8b337b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7128087
    Commit-Queue: Jilin Yang <jilinyang@google.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1545374}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -251,6 +251,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorNewTabPageChipBackground) \
   E_CPONLY(kColorNewTabPageChipForeground) \
   E_CPONLY(kColorComposeboxBackground) \
+  E_CPONLY(kColorComposeboxFileChipSpinner) \
   E_CPONLY(kColorComposeboxFont) \
   E_CPONLY(kColorComposeboxFontLight) \
   E_CPONLY(kColorComposeboxCancelButton) \
