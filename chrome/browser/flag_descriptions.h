7f24eeca943870018264c768c0d635798158f2cb
Bhaskar Sharma <bhaskarsharma@google.com>
2025-11-17 12:25:59 -0800

    Added feature flag to enable SecurityInfo in WebRequest API for controlled frames.
    Feature flag has been added to enable WebRequest.SecurityInfo API for
    ControlledFrames. It will provide certificate information for a web
    request.
    
    Fixed: 458651706
    Change-Id: I913923298a8a9d109cc849857e52eeef4dfd5f69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7151043
    Reviewed-by: Vlad Krot <vkrot@google.com>
    Commit-Queue: Bhaskar Sharma <bhaskarsharma@google.com>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1546088}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1769,7 +1769,7 @@ inline constexpr char kWebRequestSecurityInfoName[] =
     "Enable SecurityInfo in WebRequest API";
 inline constexpr char kWebRequestSecurityInfoDescription[] =
     "Enables SecurityInfo in WebRequest API for extensions, allowing "
-    "them to retrieve certificate details.";
+    "listeners to retrieve certificate details of web requests.";
 
 inline constexpr char kEnableWindowsGamingInputDataFetcherName[] =
     "Enable Windows.Gaming.Input";
@@ -3147,6 +3147,12 @@ inline constexpr char
     kContextualSearchBoxUsesContextualSearchProviderDescription[] =
         "Enables the contextual search box to use the ContextualSearchProvider "
         "instead of the ZeroSuggestProvider as the source for suggestions.";
+
+inline constexpr char kControlledFrameWebRequestSecurityInfoName[] =
+    "Enable SecurityInfo in WebRequest API for ControlledFrame";
+inline constexpr char kControlledFrameWebRequestSecurityInfoDescription[] =
+    "Enables SecurityInfo in WebRequest API for ControlledFrames, allowing "
+    "listeners to retrieve certificate details of web requests.";
 
 inline constexpr char kContextualSearchOpenLensActionUsesThumbnailName[] =
     "Contextual search open Lens action uses thumbnail";
