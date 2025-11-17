8e2b5bce1a68e5d8f7b2cb16625f7d3f9fa0495e
Muhammad Salmaan <musalmaan@chromium.org>
2025-11-17 14:00:35 -0800

    Enable field trial testing for infobar refresh
    Enabled field trial testing for infobar refresh.
    
    Bug: 452613393
    Change-Id: I88d20e630d7cabbee94bb60a1e4b9e7ca4a2853e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7143178
    Reviewed-by: Daniel Soromou <koretadaniel@chromium.org>
    Commit-Queue: Muhammad Salmaan <musalmaan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1546138}

--- chrome/browser/ui/views/infobars/confirm_infobar.cc
+++ chrome/browser/ui/views/infobars/confirm_infobar.cc
@@ -156,6 +156,7 @@ ConfirmInfoBar::ConfirmInfoBar(std::unique_ptr<ConfirmInfoBarDelegate> delegate)
   } else {
     AddContentChildView(std::move(link_unique_ptr));
   }
+  RecalculateLayoutBalancing();
 }
 ConfirmInfoBar::~ConfirmInfoBar() = default;
 
