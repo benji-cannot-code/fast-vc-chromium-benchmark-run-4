26d883a8ad3b09763417be3ad3259b63467a7d9d
Brandon Fong <bjfong@google.com>
2025-11-17 09:11:17 -0800

    [tips] Add metrics for promo events for opt in and feature tips
    Add metrics for both opt in promo and feature tip promo events.
    
    Bug: 449986209
    Change-Id: I307e17ef78e4dd249758be04b0fe5667b27a951f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7159228
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1545967}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -2631,8 +2631,9 @@ public class ChromeTabbedActivity extends ChromeActivity {
                                     mRootUiCoordinator.getBottomSheetController(),
                                     getQuickDeleteController(),
                                     getWindowAndroid(),
-                                    getCurrentTabModel().isIncognito());
-                    mTipsPromoCoordinator.showBottomSheet(fromTipsNotifications);
+                                    getCurrentTabModel().isIncognito(),
+                                    fromTipsNotifications);
+                    mTipsPromoCoordinator.showBottomSheet();
                 }
                 break;
             case TabOpenType.OPEN_NEW_INCOGNITO_TAB:
