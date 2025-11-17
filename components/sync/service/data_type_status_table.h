92050563d60c748ef1fc74661989fc4554d200b6
Michael Tatarski <mtatarski@google.com>
2025-11-17 03:15:41 -0800

    Replace `std::map` with `absl::flat_hash_map` in `DataTypeStatusTable`
    This change uses a more efficient hash-based container for
    `TypeErrorMap`, which can improve performance for lookups and
    insertions.
    
    Bug: 441179972
    Change-Id: If8a5da305e34d7ec8ba2e5e75d12b92dc1466c1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7152079
    Auto-Submit: Michael Tatarski <mtatarski@google.com>
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1545801}

--- components/sync/service/data_type_status_table.h
+++ components/sync/service/data_type_status_table.h
@@ -6,17 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_SYNC_SERVICE_DATA_TYPE_STATUS_TABLE_H_
 #define COMPONENTS_SYNC_SERVICE_DATA_TYPE_STATUS_TABLE_H_
 
-#include <map>
-
 #include "components/sync/base/data_type.h"
 #include "components/sync/service/sync_error.h"
+#include "third_party/abseil-cpp/absl/container/flat_hash_map.h"
 
 namespace syncer {
 
 // Class to keep track of data types that have encountered an error during sync.
 class DataTypeStatusTable {
  public:
-  using TypeErrorMap = std::map<DataType, SyncError>;
+  using TypeErrorMap = absl::flat_hash_map<DataType, SyncError>;
 
   DataTypeStatusTable();
   DataTypeStatusTable(const DataTypeStatusTable& other);
