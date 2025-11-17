2d488306e357c850dd52c78815c8d3a95c54d437
Steinar H. Gunderson <sesse@chromium.org>
2025-11-17 06:50:23 -0800

    Remove unused WorkerClients ex-Supplementable members.
    Bug: 460165754
    Change-Id: I6bd8a352f07bf3a18254488a74ee2f9d79572ae6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7157608
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1545893}

--- third_party/blink/renderer/modules/csspaint/paint_worklet_proxy_client.cc
+++ third_party/blink/renderer/modules/csspaint/paint_worklet_proxy_client.cc
@@ -63,8 +63,7 @@ PaintWorkletProxyClient::PaintWorkletProxyClient(
     scoped_refptr<base::SingleThreadTaskRunner> main_thread_runner,
     base::WeakPtr<PaintWorkletPaintDispatcher> paint_dispatcher,
     scoped_refptr<base::SingleThreadTaskRunner> compositor_host_queue)
-    : worker_clients_(nullptr),
-      paint_dispatcher_(std::move(paint_dispatcher)),
+    : paint_dispatcher_(std::move(paint_dispatcher)),
       compositor_host_queue_(std::move(compositor_host_queue)),
       worklet_id_(worklet_id),
       state_(RunState::kUninitialized),
@@ -172,7 +171,6 @@ void PaintWorkletProxyClient::Dispose() {
 }
 
 void PaintWorkletProxyClient::Trace(Visitor* visitor) const {
-  visitor->Trace(worker_clients_);
   PaintWorkletPainter::Trace(visitor);
 }
 
