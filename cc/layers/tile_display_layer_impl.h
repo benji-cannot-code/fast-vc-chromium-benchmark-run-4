c1d1fbde041af681b8bee299aa83fd16e95c8db9
Colin Blundell <blundell@chromium.org>
2025-11-17 01:40:32 -0800

    [//cc] Refactor TileDisplayLayerImpl's debug border quad appending logic
    Align it with the structure of PictureLayerImpl's logic [1] to get
    closer to deduping this logic into TileBasedLayerImpl (with the parts
    that are *actually* different abstracted).
    
    There are no functional changes.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:cc/layers/picture_layer_impl.cc;l=298-319?q=AppendQuadsSpe&ss=chromium
    
    Bug: 450651370
    Change-Id: If792d9c57d056c2256fedaf07ebd4210f83c445a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7157237
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1545762}

--- cc/layers/tile_display_layer_impl.h
+++ cc/layers/tile_display_layer_impl.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/cc_export.h"
 #include "cc/layers/tile_based_layer_impl.h"
 #include "cc/mojom/missing_tile_reason.mojom.h"
+#include "cc/tiles/tile_draw_info.h"
 #include "cc/tiles/tile_index.h"
 #include "cc/tiles/tile_priority.h"
 #include "cc/tiles/tiling_coverage_iterator.h"
@@ -53,6 +54,18 @@ class CC_EXPORT TileDisplayLayerImpl : public TileBasedLayerImpl {
     explicit Tile(TileDisplayLayerImpl& layer, const TileContents& contents);
     ~Tile();
     Tile(Tile&&);
+
+    TileDrawInfo::Mode draw_mode() {
+      CHECK(IsReadyToDraw());
+      if (solid_color()) {
+        return TileDrawInfo::SOLID_COLOR_MODE;
+      } else if (is_oom()) {
+        return TileDrawInfo::OOM_MODE;
+      } else {
+        CHECK(resource());
+        return TileDrawInfo::RESOURCE_MODE;
+      }
+    }
 
     const TileContents& contents() const { return contents_; }
 
