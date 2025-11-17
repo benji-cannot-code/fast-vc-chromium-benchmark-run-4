2356e7a21d4fef3bb3521c33aeb336f67852bb7d
Ramin Halavati <rhalavati@chromium.org>
2025-11-17 15:17:17 -0800

    Add alias to catch NOTREACHED crashes in GetRoleDescription on Android.
    To be able to detect roles that are used unexpectedly on Android, an
    `alias` is added for the role value in
    `BrowserAccessibilityAndroid::GetRoleDescription()` so that it would be
    available in crash logs.
    
    AX-Relnotes: n/a
    Bug: 460866659
    Change-Id: Ibd939a0202349caf7f36935a9050816861edb94e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7156520
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Auto-Submit: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1546185}

--- content/browser/accessibility/browser_accessibility_android.cc
+++ content/browser/accessibility/browser_accessibility_android.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check_deref.h"
 #include "base/containers/contains.h"
+#include "base/debug/alias.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/i18n/break_iterator.h"
@@ -1540,8 +1541,11 @@ std::u16string BrowserAccessibilityAndroid::GetRoleDescription() const {
     case ax::mojom::Role::kPdfRoot:
     case ax::mojom::Role::kRowGroup:
     case ax::mojom::Role::kTableHeaderContainer:
-    case ax::mojom::Role::kWebView:
-      NOTREACHED();
+    case ax::mojom::Role::kWebView: {
+      ax::mojom::Role role = GetRole();
+      base::debug::Alias(&role);
+      NOTREACHED() << "Role: " << static_cast<int>(role);
+    }
 
     case ax::mojom::Role::kFigure:
       // Default is IDS_AX_ROLE_FIGURE.
