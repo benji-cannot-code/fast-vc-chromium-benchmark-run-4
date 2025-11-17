88d50f2ad4d18e10fab630130987b24e0bc7ac42
Lei Zhang <thestig@chromium.org>
2025-11-17 14:29:08 -0800

    Forward declare base::OnceCallback in callback_function_base.h
    In Blink's renderer/platform/bindings/callback_function_base.h, switch
    from base/functional/callback.h to callback_forward.h. This helps avoid
    transitively including callback.h in 2000+ translation units. Most of
    the TUs are generated Blink files for classes in the
    CallbackFunctionBase hierarchy that do not involve base::OnceCallback.
    
    Change-Id: I997de0bc7ad676697f54789cd2ef624242590933
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7161084
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1546159}

--- third_party/blink/renderer/platform/bindings/callback_function_base.cc
+++ third_party/blink/renderer/platform/bindings/callback_function_base.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/bindings/callback_function_base.h"
 
+#include "base/functional/callback.h"
 #include "third_party/blink/renderer/platform/bindings/binding_security_for_platform.h"
 #include "third_party/blink/renderer/platform/bindings/exception_messages.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
