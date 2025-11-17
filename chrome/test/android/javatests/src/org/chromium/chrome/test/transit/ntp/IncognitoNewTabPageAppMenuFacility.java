0ef0a339e0cb6621f9eceb14b00812fd0385e28a
Henrique Nakashima <hnakashima@chromium.org>
2025-11-17 13:48:34 -0800

    [Android] Update AppMenu Facilities expectations in incognito pages
    When AndroidOpenIncognitoAsWindow is enabled, the incognito app menus
    are expected to _not_ have History.
    
    This fixes TabbedAppMenuPTTest when AndroidOpenIncognitoAsWindow
    is enabled:
    - testWebPageIncognitoAppMenuItems
    - testNewTabPageIncognitoAppMenuItems
    
    Also make ScrollableFacility#verifyPresentItems clearer about which
    item was not found.
    
    Bug: 439491767
    Change-Id: I2212f746f675e2dc886762e5013c692d6c40ca64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7162876
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Linyu He <linyuh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1546130}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/transit/ntp/IncognitoNewTabPageAppMenuFacility.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/transit/ntp/IncognitoNewTabPageAppMenuFacility.java
@@ -35,7 +35,11 @@ public class IncognitoNewTabPageAppMenuFacility
             mNewIncognitoWindow = declareMenuItem(items, NEW_INCOGNITO_WINDOW_ID);
         }
 
-        declareMenuItem(items, HISTORY_ID);
+        if (IncognitoUtils.shouldOpenIncognitoAsWindow()) {
+            declareAbsentMenuItem(items, HISTORY_ID);
+        } else {
+            declareMenuItem(items, HISTORY_ID);
+        }
         declareAbsentMenuItem(items, DELETE_BROWSING_DATA_ID);
 
         declareMenuItem(items, DOWNLOADS_ID);
