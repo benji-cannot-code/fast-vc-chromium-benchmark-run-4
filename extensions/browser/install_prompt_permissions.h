790fc7e955a36c7201aae19c97493779feca4cf4
Jaewon Jung <jw.jung@navercorp.com>
2025-11-17 01:43:12 -0800

    extensions: Move InstallPromptPermissions to extensions/browser
    InstallPromptPermissions no longer has any //chrome dependencies. Move
    it to //extensions.
    
    Signed-off-by: Jaewon Jung <jw.jung@navercorp.com>
    Bug: 358567092
    Change-Id: Ia36e690fdce4810c4a4687711b51c9021e88be64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7137498
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1545765}

--- extensions/browser/install_prompt_permissions.h
+++ extensions/browser/install_prompt_permissions.h
@@ -1,10 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2019 The Chromium Authors
+// Copyright 2025 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_EXTENSIONS_INSTALL_PROMPT_PERMISSIONS_H_
-#define CHROME_BROWSER_EXTENSIONS_INSTALL_PROMPT_PERMISSIONS_H_
+#ifndef EXTENSIONS_BROWSER_INSTALL_PROMPT_PERMISSIONS_H_
+#define EXTENSIONS_BROWSER_INSTALL_PROMPT_PERMISSIONS_H_
 
 #include <string>
 #include <vector>
@@ -38,4 +38,4 @@ struct InstallPromptPermissions {
 
 }  // namespace extensions
 
-#endif  // CHROME_BROWSER_EXTENSIONS_INSTALL_PROMPT_PERMISSIONS_H_
+#endif  // EXTENSIONS_BROWSER_INSTALL_PROMPT_PERMISSIONS_H_
