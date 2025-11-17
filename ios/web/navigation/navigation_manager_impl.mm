2514703de49c3002c07086e10a397d929f3c2319
Sylvain Defresne <sdefresne@chromium.org>
2025-11-17 11:30:57 -0800

    [ios] Fix unsafe buffer in NavigationManagerImpl
    Use base::span::subspan(...) instead of creating the base::span(...)
    from an iterator and a count.
    
    Bug: 40285824
    Change-Id: Ia8b8b354e69808b3c33948b64f0454b9c4638a17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7156570
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1546057}

--- ios/web/navigation/navigation_manager_impl.mm
+++ ios/web/navigation/navigation_manager_impl.mm
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #import "ios/web/navigation/navigation_manager_impl.h"
 
 #import <Foundation/Foundation.h>
@@ -216,7 +211,7 @@ void NavigationManagerImpl::SerializeToProto(
   DCHECK_LE(length + offset, items.size());
 
   storage.set_last_committed_item_index(last_committed_item_index);
-  for (const auto* item : base::span(items.begin() + offset, length)) {
+  for (const auto* item : base::span(items).subspan(offset, length)) {
     item->SerializeToProto(*storage.add_items());
   }
 }
