201c289ce642de6cc205fc6db1923f98cd96092a
Hailey Wang <haileywang@google.com>
2025-11-17 15:48:37 -0800

    [AIM] Observe when all tab model initialized
    Record metrics only when all tab models are initialized.
    
    Delete stale cache entries.
    
    Change-Id: I0adba3fd3ac81871c3f9bf969c4ce9da7b4147e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7137775
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1546193}

--- chrome/browser/page_content_annotations/page_content_extraction_service.cc
+++ chrome/browser/page_content_annotations/page_content_extraction_service.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/content/browser/page_content_proto_provider.h"
 #include "components/optimization_guide/proto/features/common_quality_data.pb.h"
 #include "components/page_content_annotations/core/page_content_annotations_features.h"
+#include "components/page_content_annotations/core/page_content_cache.h"
 #include "components/page_content_annotations/core/page_content_cache_handler.h"
 #include "components/page_content_annotations/core/web_state_wrapper.h"
 #include "content/public/browser/browser_context.h"
@@ -144,6 +145,14 @@ void PageContentExtractionService::OnNewNavigation(
   if (is_page_content_cache_enabled_) {
     page_content_cache_handler_->OnNewNavigation(
         tab_id, ToWebStateWrapper(web_contents));
+  }
+}
+
+void PageContentExtractionService::RunCleanUpTasksWithActiveTabs(
+    const std::set<int64_t>& all_tab_ids) {
+  if (is_page_content_cache_enabled_) {
+    page_content_cache_handler_->page_content_cache()
+        ->RunCleanUpTasksWithActiveTabs(all_tab_ids);
   }
 }
 
