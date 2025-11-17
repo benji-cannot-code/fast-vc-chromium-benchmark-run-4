7374ddd2ed99660fdd92e768160d9f00eb751c02
Eric Ekey <ericekey@google.com>
2025-11-17 12:57:18 -0800

    [iOS] Remove SafetyCheckItemLayoutType
    This change removes an unused enum SafetyCheckItemLayoutType and renames
    the containing header file to reflect the remaining contents.
    
    Bypass-Check-License: Renamed file
    Fixed: 461502095
    Change-Id: I6165d84697de5c53342d37f7ccac5d4e98670820
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7161834
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Eric Ekey <ericekey@google.com>
    Cr-Commit-Position: refs/heads/main@{#1546100}

--- ios/chrome/browser/content_suggestions/ui_bundled/safety_check/safety_check_item_type.h
+++ ios/chrome/browser/content_suggestions/ui_bundled/safety_check/safety_check_item_type.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_BROWSER_CONTENT_SUGGESTIONS_UI_BUNDLED_SAFETY_CHECK_TYPES_H_
-#define IOS_CHROME_BROWSER_CONTENT_SUGGESTIONS_UI_BUNDLED_SAFETY_CHECK_TYPES_H_
+#ifndef IOS_CHROME_BROWSER_CONTENT_SUGGESTIONS_UI_BUNDLED_SAFETY_CHECK_SAFETY_CHECK_ITEM_TYPE_H_
+#define IOS_CHROME_BROWSER_CONTENT_SUGGESTIONS_UI_BUNDLED_SAFETY_CHECK_SAFETY_CHECK_ITEM_TYPE_H_
 
 // The possible types of Safety Check (Magic Stack) module items.
 enum class SafetyCheckItemType {
@@ -17,11 +17,4 @@ enum class SafetyCheckItemType {
   kMaxValue = kDefault
 };
 
-// The possible layout types for a given Safety Check (Magic Stack) module item.
-enum class SafetyCheckItemLayoutType {
-  kHero = 1,
-  kCompact = 2,
-  kMaxValue = kCompact
-};
-
-#endif  // IOS_CHROME_BROWSER_CONTENT_SUGGESTIONS_UI_BUNDLED_SAFETY_CHECK_TYPES_H_
+#endif  // IOS_CHROME_BROWSER_CONTENT_SUGGESTIONS_UI_BUNDLED_SAFETY_CHECK_SAFETY_CHECK_ITEM_TYPE_H_
