6c6af0567b918724a1e8ba7ddd6d1c9f58f2e16f
Dan Polanco <polardz@google.com>
2025-11-17 09:06:48 -0800

    [Media Indicator] Add SHARING UI signal logic
    The MediaStateObserver now listens for changes in the `IsBeingMirrored`
    state of the WebContents. A follow-up CL is needed to trigger this
    signal.
    
    Bug: 453698583
    Change-Id: I060b8e522eb3cb48050db3c55ab58b83d4e0354c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7162075
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Dan Polanco <polardz@google.com>
    Commit-Queue: Dan Polanco <polardz@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1545964}

--- chrome/browser/android/media_state_observer.h
+++ chrome/browser/android/media_state_observer.h
@@ -45,6 +45,9 @@ class MediaStateObserver
   // Called when the audio capture state of the WebContents has changed.
   void OnIsCapturingAudioChanged(content::WebContents* web_contents,
                                  bool is_capturing_audio) override;
+  // Called when the mirroring state of the WebContents has changed.
+  void OnIsBeingMirroredChanged(content::WebContents* web_contents,
+                                bool is_being_mirrored) override;
 
  private:
   friend class content::WebContentsUserData<MediaStateObserver>;
@@ -62,6 +65,7 @@ class MediaStateObserver
   void UpdateMediaState();
 
   // Keep track of the media state.
+  bool is_being_mirrored_ = false;
   bool is_capturing_video_ = false;
   bool is_capturing_audio_ = false;
   bool is_audio_muted_ = false;
