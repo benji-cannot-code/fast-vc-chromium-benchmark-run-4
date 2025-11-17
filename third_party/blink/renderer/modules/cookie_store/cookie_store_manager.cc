eb2bd7e9b6125f87bb2ee02e2d9033c9d191a669
Steinar H. Gunderson <sesse@chromium.org>
2025-11-17 04:35:58 -0800

    Remove unused ServiceWorkerRegistration ex-Supplementable members.
    Bug: 460165754
    Change-Id: I89223123abf47f94b789d285ed9849c9ad9b0481
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7157453
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Auto-Submit: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1545833}

--- third_party/blink/renderer/modules/cookie_store/cookie_store_manager.cc
+++ third_party/blink/renderer/modules/cookie_store/cookie_store_manager.cc
@@ -97,8 +97,7 @@ CookieStoreManager* CookieStoreManager::cookies(
 }
 
 CookieStoreManager::CookieStoreManager(ServiceWorkerRegistration& registration)
-    : service_worker_registration_(registration),
-      registration_(&registration),
+    : registration_(&registration),
       backend_(registration.GetExecutionContext()),
       default_cookie_url_(DefaultCookieURL(&registration)) {
   auto* execution_context = registration.GetExecutionContext();
@@ -181,7 +180,6 @@ CookieStoreManager::getSubscriptions(ScriptState* script_state,
 void CookieStoreManager::Trace(Visitor* visitor) const {
   visitor->Trace(registration_);
   visitor->Trace(backend_);
-  visitor->Trace(service_worker_registration_);
   ScriptWrappable::Trace(visitor);
 }
 
