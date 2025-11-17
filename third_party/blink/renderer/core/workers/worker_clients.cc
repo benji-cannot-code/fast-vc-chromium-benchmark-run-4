7f0fddc66bb94c588f8a6e6211c102c1a33a5069
Steinar H. Gunderson <sesse@chromium.org>
2025-11-17 06:50:10 -0800

    Make WorkerClients not Supplementable.
    Part of Supplementable cleanup. Done with a script followed by
    human cleanup/review and git cl format.
    
    Bug: 460165754
    Change-Id: I7206e4db36fa58a5c05d3257a6b99b4f18a46d59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7157607
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1545892}

--- third_party/blink/renderer/core/workers/worker_clients.cc
+++ third_party/blink/renderer/core/workers/worker_clients.cc
@@ -7,6 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-template class CORE_TEMPLATE_EXPORT Supplement<WorkerClients>;
+void WorkerClients::Trace(Visitor* visitor) const {
+  visitor->Trace(animation_worklet_proxy_client_);
+  visitor->Trace(paint_worklet_proxy_client_);
+}
 
 }  // namespace blink
