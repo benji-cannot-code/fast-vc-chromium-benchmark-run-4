9414bcebf2ceefeae48e21e48c1f058ad1954f8c
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2025-11-17 07:20:12 -0800

    Revert "Enable `SyncEnablePasswordsSyncErrorMessageAlternative` by default"
    This reverts commit 56bffb69d661769a2875d5ea166d6e28a92e968f.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/6243231986614272
    
    Sample build with failed test: https://ci.chromium.org/b/8697958075706489409
    Affected test(s):
    [://chrome/android\:chrome_public_test_apk!junit:org.chromium.chrome.browser.sync.ui:SyncErrorMessageTest#testSyncErrorMessageForTrustedVaultKeyRequiredContent](https://ci.chromium.org/ui/test/chromium/:%2F%2Fchrome%2Fandroid%5C:chrome_public_test_apk%21junit:org.chromium.chrome.browser.sync.ui:SyncErrorMessageTest%23testSyncErrorMessageForTrustedVaultKeyRequiredContent?q=VHash%3A766a353923e50791)
    [://chrome/android\:chrome_public_test_apk!junit:org.chromium.chrome.browser.sync.ui:SyncErrorMessageTest#testSyncErrorMessageShownForTrustedVaultKeyRequiredForSignedInUsers](https://ci.chromium.org/ui/test/chromium/:%2F%2Fchrome%2Fandroid%5C:chrome_public_test_apk%21junit:org.chromium.chrome.browser.sync.ui:SyncErrorMessageTest%23testSyncErrorMessageShownForTrustedVaultKeyRequiredForSignedInUsers?q=VHash%3A766a353923e50791)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F6243231986614272&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F7152858&type=BUG
    
    Original change's description:
    > Enable `SyncEnablePasswordsSyncErrorMessageAlternative` by default
    >
    > The feature was approved for full launch.
    >
    > Bug: b:379880828
    > Change-Id: I879815df07c87561809dc63cc4ac6e9ebc4b6806
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7152858
    > Reviewed-by: Ankush Singh <ankushkush@google.com>
    > Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    > Commit-Queue: Yurii Lahodiuk <ylahodiuk@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1545792}
    >
    
    Bug: b:379880828
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ia2da95b3fc46f5864088a76dcbb100d7704d6d68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7161223
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Owners-Override: Antonio Sartori <antoniosartori@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1545906}

--- components/sync/base/features.h
+++ components/sync/base/features.h
@@ -198,7 +198,7 @@ BASE_DECLARE_FEATURE(kWebApkBackupAndRestoreBackend);
 BASE_DECLARE_FEATURE(kSyncEnablePasswordsSyncErrorMessageAlternative);
 inline constexpr base::FeatureParam<int>
     kSyncEnablePasswordsSyncErrorMessageAlternativeVersion{
-        &kSyncEnablePasswordsSyncErrorMessageAlternative, "version", 3};
+        &kSyncEnablePasswordsSyncErrorMessageAlternative, "version", 1};
 #endif  // BUILDFLAG(IS_ANDROID)
 
 #if BUILDFLAG(IS_IOS)
