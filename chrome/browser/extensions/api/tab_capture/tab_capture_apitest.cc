68bd7b46e4ba2d64a75b640646f058bb4593e558
James Cook <jamescook@chromium.org>
2025-11-18 14:58:34 -0800

    extensions: Clean up tab_capture_apitest.cc for desktop Android
    This CL doesn't port the tests to desktop Android (they fail deep in
    the media code and I'm still investigating). Instead this CL cleans up
    Browser and TabStripModel references in preparation for an eventual
    port to desktop Android.
    
    Bug: 427298135
    Test: browser_tests *TabCaptureApi*
    Change-Id: I44b0bfe537a952d3d14357509d83aab300ed407a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7169738
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1546828}

--- chrome/browser/extensions/api/tab_capture/tab_capture_apitest.cc
+++ chrome/browser/extensions/api/tab_capture/tab_capture_apitest.cc
@@ -77,9 +77,8 @@ class TabCaptureApiTest : public ExtensionApiTest {
   }
 
   void SimulateMouseClickInCurrentTab() {
-    content::SimulateMouseClick(
-        browser()->tab_strip_model()->GetActiveWebContents(), 0,
-        blink::WebMouseEvent::Button::kLeft);
+    content::SimulateMouseClick(GetActiveWebContents(), 0,
+                                blink::WebMouseEvent::Button::kLeft);
   }
 };
 
@@ -119,14 +118,8 @@ IN_PROC_BROWSER_TEST_F(TabCaptureApiTest, GetUserMediaTest) {
 
   EXPECT_TRUE(listener.WaitUntilSatisfied());
 
-  content::OpenURLParams params(GURL(url::kAboutBlankURL), content::Referrer(),
-                                WindowOpenDisposition::NEW_FOREGROUND_TAB,
-                                ui::PAGE_TRANSITION_LINK, false);
-  content::WebContents* web_contents =
-      browser()->OpenURL(params, /*navigation_handle_callback=*/{});
-
   content::RenderFrameHost* const main_frame =
-      web_contents->GetPrimaryMainFrame();
+      NavigateToURLInNewTab(GURL(url::kAboutBlankURL));
   ASSERT_TRUE(main_frame);
   listener.Reply(base::StringPrintf("web-contents-media-stream://%i:%i",
                                     main_frame->GetProcess()->GetDeprecatedID(),
@@ -156,11 +149,11 @@ IN_PROC_BROWSER_TEST_F(TabCaptureApiTest, ActiveTabPermission) {
 
   // Open a new tab and make sure capture is denied.
   EXPECT_TRUE(before_open_tab.WaitUntilSatisfied());
-  content::OpenURLParams params(GURL(url::kAboutBlankURL), content::Referrer(),
-                                WindowOpenDisposition::NEW_FOREGROUND_TAB,
-                                ui::PAGE_TRANSITION_LINK, false);
-  content::WebContents* web_contents =
-      browser()->OpenURL(params, /*navigation_handle_callback=*/{});
+  content::RenderFrameHost* const main_frame =
+      NavigateToURLInNewTab(GURL(url::kAboutBlankURL));
+  ASSERT_TRUE(main_frame);
+  content::WebContents* const web_contents =
+      content::WebContents::FromRenderFrameHost(main_frame);
   ASSERT_TRUE(web_contents) << "Failed to open new tab";
   before_open_tab.Reply("");
 
@@ -175,7 +168,7 @@ IN_PROC_BROWSER_TEST_F(TabCaptureApiTest, ActiveTabPermission) {
 
   // Open a new tab and make sure capture is denied.
   EXPECT_TRUE(before_open_new_tab.WaitUntilSatisfied());
-  browser()->OpenURL(params, /*navigation_handle_callback=*/{});
+  NavigateToURLInNewTab(GURL(url::kAboutBlankURL));
   before_open_new_tab.Reply("");
 
   // Add extension to allowlist and make sure capture succeeds.
@@ -259,9 +252,10 @@ IN_PROC_BROWSER_TEST_F(TabCaptureApiTest, Constraints) {
 }
 
 // Tests that the tab indicator (in the tab strip) is shown during tab capture.
+// TODO(crbug.com/427298135): Port this to BrowserWindowInterface after
+// TabListInterfaceObserver supports TabChangedAt().
 IN_PROC_BROWSER_TEST_F(TabCaptureApiTest, TabIndicator) {
-  content::WebContents* const contents =
-      browser()->tab_strip_model()->GetActiveWebContents();
+  content::WebContents* const contents = GetActiveWebContents();
   ASSERT_THAT(GetTabAlertStatesForContents(contents), ::testing::IsEmpty());
 
   // A TabStripModelObserver that quits the MessageLoop whenever the
@@ -335,12 +329,11 @@ IN_PROC_BROWSER_TEST_F(TabCaptureApiTest, MultipleExtensions) {
   ASSERT_TRUE(extension_b_ready.WaitUntilSatisfied());
 
   // Open a page and grant permissions.
-  content::OpenURLParams params(embedded_test_server()->GetURL("/simple.html"),
-                                content::Referrer(),
-                                WindowOpenDisposition::NEW_FOREGROUND_TAB,
-                                ui::PAGE_TRANSITION_LINK, false);
-  content::WebContents* web_contents =
-      browser()->OpenURL(params, /*navigation_handle_callback=*/{});
+  content::RenderFrameHost* const main_frame =
+      NavigateToURLInNewTab(embedded_test_server()->GetURL("/simple.html"));
+  ASSERT_TRUE(main_frame);
+  content::WebContents* const web_contents =
+      content::WebContents::FromRenderFrameHost(main_frame);
   ASSERT_TRUE(web_contents) << "Failed to open new tab";
   auto* perm_granter =
       ActiveTabPermissionGranter::FromWebContents(web_contents);
