1e02164d81929609f3aee477ddbdd67984d87732
Joe Mason <joenotcharles@google.com>
2025-11-18 10:26:02 -0800

    Add byte_size.mojom to replace byte_count.mojom
    base::ByteSize and base::ByteSizeDelta replace base::ByteCount, which
    was marked deprecated in https://crrev.com/c/7042842.
    
    R=oksamyt
    
    Bug: 448661443
    Change-Id: If56deefd96f7a968370f6524c6ddfef11dfeb142
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7163775
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1546660}

---
+++ mojo/public/cpp/base/byte_size_mojom_traits.cc
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "mojo/public/cpp/base/byte_size_mojom_traits.h"
+
+#include "base/byte_size.h"
+
+namespace mojo {
+
+// static
+bool StructTraits<mojo_base::mojom::ByteSizeDataView, base::ByteSize>::Read(
+    mojo_base::mojom::ByteSizeDataView data,
+    base::ByteSize* byte_size) {
+  // ByteSize holds a uint64_t, but any value over max(int64_t) is invalid, so
+  // that every valid ByteSize can convert to ByteSizeDelta. The ByteSize
+  // constructor will CHECK if given an out-of-range value.
+  if (data.size() > base::ByteSize::Max().InBytes()) {
+    return false;
+  }
+  *byte_size = base::ByteSize(data.size());
+  return true;
+}
+
+// static
+bool StructTraits<
+    mojo_base::mojom::ByteSizeDeltaDataView,
+    base::ByteSizeDelta>::Read(mojo_base::mojom::ByteSizeDeltaDataView data,
+                               base::ByteSizeDelta* byte_size_delta) {
+  *byte_size_delta = base::ByteSizeDelta(data.delta());
+  return true;
+}
+
+}  // namespace mojo
