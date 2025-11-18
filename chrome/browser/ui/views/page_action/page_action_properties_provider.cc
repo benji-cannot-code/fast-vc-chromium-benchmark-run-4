ce90d7f82e9dc2650fe5971e80ddefab811e1847
Steven Luong <stluong@chromium.org>
2025-11-18 10:51:28 -0800

    Reland show page action to trigger toolbar height side panel
    Adds a page action chip that toggles the toolbar toolbar height side
    panel to open and close.
    
    This CL is a reland of https://chromium-review.googlesource.com/c/chromium/src/+/7121859.
    The CL was previously reverted because the
    ContextualTasksPageActionControllerInteractiveTest didn't properly
    enable the page actions migration flag so the test ended up failing.
    
    Bug: 456546489
    Change-Id: I23b8e0d6ca1ce1c1bc16fad1cbf35e76cf290782
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7143568
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Steven Luong <stluong@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1546683}

--- chrome/browser/ui/views/page_action/page_action_properties_provider.cc
+++ chrome/browser/ui/views/page_action/page_action_properties_provider.cc
@@ -201,6 +201,14 @@ constexpr auto kPageActionProperties =
                 .type = PageActionIconType::kSaveCard,
             },
         },
+        {
+            kActionContextualPanelPageActionChip,
+            {
+                .histogram_name = "ContextualSidePanel",
+                .type = PageActionIconType::kContextualSidePanel,
+                .element_identifier = kContextualTasksPageActionElementId,
+            },
+        },
     });
 
 constexpr bool CheckIgnoreFlagUsage() {
