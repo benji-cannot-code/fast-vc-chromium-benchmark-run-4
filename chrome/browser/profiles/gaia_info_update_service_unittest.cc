b5223130eebcb9af6e962929e06bcb1368f810ff
Atharv Maan <atharvmaan@chromium.org>
2025-11-18 10:27:25 -0800

    [Start Chrome with Windows] Move StartupLaunchManager to 'c/b/startup`
    This CL moves StartupLaunchManager out of `c/b/background` into its own
    subdirectory `c/b/startup`. This makes it easier to extend the
    StartupLaunchManager to support foreground launches as part of `Start
    Chrome with Windows` project.
    
    No logical changes are expected from this CL.
    
    Design doc: go/start-chrome-with-windows-dd
    
    Change-Id: I441575053e3f5373e24ae3dacb2aedd0a7a874a4
    Fixed: 458757363
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7062053
    Reviewed-by: Darryl James <dljames@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Commit-Queue: Atharv Maan <atharvmaan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1546662}

--- chrome/browser/profiles/gaia_info_update_service_unittest.cc
+++ chrome/browser/profiles/gaia_info_update_service_unittest.cc
@@ -54,10 +54,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/image/image_unittest_util.h"
 
 #if BUILDFLAG(ENABLE_GLIC)
-#include "chrome/browser/background/startup_launch_manager.h"
 #include "chrome/browser/glic/glic_pref_names.h"
 #include "chrome/browser/glic/public/glic_enabling.h"
 #include "chrome/browser/glic/test_support/glic_test_environment.h"
+#include "chrome/browser/startup/startup_launch_manager.h"
 #endif
 
 using signin::constants::kNoHostedDomainFound;
--- chrome/browser/startup/startup_launch_manager.h
+++ chrome/browser/startup/startup_launch_manager.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_BACKGROUND_STARTUP_LAUNCH_MANAGER_H_
-#define CHROME_BROWSER_BACKGROUND_STARTUP_LAUNCH_MANAGER_H_
+#ifndef CHROME_BROWSER_STARTUP_STARTUP_LAUNCH_MANAGER_H_
+#define CHROME_BROWSER_STARTUP_STARTUP_LAUNCH_MANAGER_H_
 
 #include <set>
 
@@ -43,4 +43,4 @@ class StartupLaunchManager {
   std::set<StartupLaunchReason> registered_launch_reasons_;
 };
 
-#endif  // CHROME_BROWSER_BACKGROUND_STARTUP_LAUNCH_MANAGER_H_
+#endif  // CHROME_BROWSER_STARTUP_STARTUP_LAUNCH_MANAGER_H_
