b33339229298f30ebc3e4d894090b2d8e59af791
Muyao Xu <muyaoxu@google.com>
2025-11-17 18:09:30 -0800

    [Code Health] Clean up stale base::Feature "InternalServerSideSpeechRecognitionControl"
    Bug: 356234677
    Change-Id: I34bffe9d111349d76962c3eaf18bbc0d5927433c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7158597
    Reviewed-by: Benjamin Zielinski <bzielinski@google.com>
    Commit-Queue: Muyao Xu <muyaoxu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1546257}

--- ash/constants/ash_features.h
+++ ash/constants/ash_features.h
@@ -538,8 +538,6 @@ COMPONENT_EXPORT(ASH_CONSTANTS) BASE_DECLARE_FEATURE(kInstantTethering);
 COMPONENT_EXPORT(ASH_CONSTANTS)
 BASE_DECLARE_FEATURE(kInternalServerSideSpeechRecognition);
 COMPONENT_EXPORT(ASH_CONSTANTS)
-BASE_DECLARE_FEATURE(kInternalServerSideSpeechRecognitionControl);
-COMPONENT_EXPORT(ASH_CONSTANTS)
 BASE_DECLARE_FEATURE(kInternalServerSideSpeechRecognitionByFinch);
 COMPONENT_EXPORT(ASH_CONSTANTS)
 BASE_DECLARE_FEATURE(kInternalServerSideSpeechRecognitionUSMModelFinch);
@@ -1198,8 +1196,6 @@ COMPONENT_EXPORT(ASH_CONSTANTS)
 bool IsInternalServerSideSpeechRecognitionEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS)
 bool IsInternalServerSideSpeechRecognitionEnabledByFinch();
-COMPONENT_EXPORT(ASH_CONSTANTS)
-bool IsInternalServerSideSpeechRecognitionControlEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsJupiterScreensaverEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS)
 bool IsLauncherContinueSectionWithRecentsEnabled();
