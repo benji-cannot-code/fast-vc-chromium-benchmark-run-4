e1eecc4333815ff801322f04ff6aff10cace1c17
Antonio Sartori <antoniosartori@chromium.org>
2025-11-18 07:25:14 -0800

    [identifiability] Remove unused header in keyboard_layout.cc
    Bug: 451513878
    Change-Id: Ib6aa8f66c62bafce248233b209a1e5afdbd527fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7166713
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Auto-Submit: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1546531}

--- third_party/blink/renderer/modules/keyboard/keyboard_layout.cc
+++ third_party/blink/renderer/modules/keyboard/keyboard_layout.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
-#include "third_party/blink/renderer/platform/privacy_budget/identifiability_digest_helpers.h"
 #include "third_party/blink/renderer/platform/wtf/functional.h"
 
 namespace blink {
