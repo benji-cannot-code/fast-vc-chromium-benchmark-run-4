1c1e0e5fb54d0968712ea91e5796deaf80260ae4
Glenn Hartmann <hartmanng@chromium.org>
2025-11-18 07:36:58 -0800

    Make sure GlobalFeatures gets deconstructed last in TestingBrowserProcess.
    Several features rely on (or soon will rely on) GlobalBrowserCollection,
    which is a member of GlobalFeatures. In order to prevent use-after-free
    issues, we have to make sure that |features_| is the first data member
    of TestingBrowserProcess.
    
    Bug: 459777668
    Change-Id: I2ed9196c1b93345722e90573f5541a9c6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7163096
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Auto-Submit: Glenn Hartmann <hartmanng@chromium.org>
    Commit-Queue: Glenn Hartmann <hartmanng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1546536}

--- chrome/test/base/testing_browser_process.h
+++ chrome/test/base/testing_browser_process.h
@@ -55,7 +55,7 @@ class MetricsService;
 namespace network {
 class TestNetworkConnectionTracker;
 class TestNetworkQualityTracker;
-}
+}  // namespace network
 
 namespace os_crypt_async {
 class OSCryptAsync;
@@ -224,6 +224,12 @@ class TestingBrowserProcess
 
   void ShutdownBrowserPolicyConnector();
 
+  // This member needs to stay at or near the top of the list so it gets
+  // destroyed late in the shutdown process. Several other members rely on
+  // |features_|, so having it lower in this file could cause use-after-free
+  // issues.
+  std::unique_ptr<GlobalFeatures> features_;
+
   // The value returned by `IsShuttingDown()`.
   bool is_shutting_down_ = false;
 
@@ -300,7 +306,6 @@ class TestingBrowserProcess
 
   std::unique_ptr<StatusTray> status_tray_;
   std::unique_ptr<os_crypt_async::OSCryptAsync> os_crypt_async_;
-  std::unique_ptr<GlobalFeatures> features_;
 };
 
 // RAII (resource acquisition is initialization) for TestingBrowserProcess.
