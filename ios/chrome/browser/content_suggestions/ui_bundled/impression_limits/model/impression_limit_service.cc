19d0b3e58555a253c5fef76f4b4049771d3c4f7a
Eric Ekey <ericekey@google.com>
2025-11-18 14:37:49 -0800

    [iOS] Enable -Wexit-time-destructors for Impression Limit Service
    Fix existing code that triggers this warning.
    
    Bug: 40031409
    Change-Id: Ief66a82fc436e0e4356f2d7aa23453ba0103fb1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7158019
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Eric Ekey <ericekey@google.com>
    Cr-Commit-Position: refs/heads/main@{#1546819}

--- ios/chrome/browser/content_suggestions/ui_bundled/impression_limits/model/impression_limit_service.cc
+++ ios/chrome/browser/content_suggestions/ui_bundled/impression_limits/model/impression_limit_service.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/values_util.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/no_destructor.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/time/time.h"
 #include "base/values.h"
@@ -358,10 +359,10 @@ void ImpressionLimitService::OnUntracked(const GURL& url) {
 }
 
 const std::set<std::string_view> ImpressionLimitService::GetAllowListedPrefs() {
-  static std::set<std::string_view> prefs = {
-      tab_resumption_prefs::kTabResumptionRegularUrlImpressions,
-      tab_resumption_prefs::kTabResumptionWithPriceDropUrlImpressions,
-      tab_resumption_prefs::kTabResumptionWithPriceTrackableUrlImpressions,
-      shop_card_prefs::kShopCardPriceDropUrlImpressions};
-  return prefs;
+  static base::NoDestructor<std::set<std::string_view>> prefs(
+      {tab_resumption_prefs::kTabResumptionRegularUrlImpressions,
+       tab_resumption_prefs::kTabResumptionWithPriceDropUrlImpressions,
+       tab_resumption_prefs::kTabResumptionWithPriceTrackableUrlImpressions,
+       shop_card_prefs::kShopCardPriceDropUrlImpressions});
+  return *prefs;
 }
