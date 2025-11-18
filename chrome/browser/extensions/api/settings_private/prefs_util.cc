18d1ecfd7628fc05bad42472865c88e34392a103
Dominique Fauteux-Chapleau <domfc@chromium.org>
2025-11-18 14:21:58 -0800

    Change proxy settings UI to handle proxy override rules
    See the design doc at go/cbe-dt-proxy-override-design
    for screenshots of the new UI
    
    Bug: 419548922
    Change-Id: Ibc6ff26b2961cd1b9fd8426687f61674ecc61e09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7127863
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1546808}

--- chrome/browser/extensions/api/settings_private/prefs_util.cc
+++ chrome/browser/extensions/api/settings_private/prefs_util.cc
@@ -1217,6 +1217,9 @@ const PrefsUtil::TypedPrefMap& PrefsUtil::GetAllowlistedKeys() {
   // Proxy settings.
   (*s_allowlist)[proxy_config::prefs::kProxy] =
       settings_api::PrefType::kDictionary;
+  // Proxy override rules.
+  (*s_allowlist)[proxy_config::prefs::kProxyOverrideRules] =
+      settings_api::PrefType::kList;
 
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
   (*s_allowlist)[::prefs::kUserFeedbackAllowed] =
