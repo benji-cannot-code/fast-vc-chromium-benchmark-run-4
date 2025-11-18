31595171ab8aa9b16d8c69f39470ec0197947140
L. David Baron <dbaron@chromium.org>
2025-11-17 22:52:13 -0800

    [gardener] Add timeout=long to shared-worker-mixed-clients-eviction.window.js
    Bug: 456429762
    Change-Id: I567b017119085e2ae0ceb1c7052b78af7503fc65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7157371
    Auto-Submit: David Baron <dbaron@chromium.org>
    Commit-Queue: Anna Sato <annasato@chromium.org>
    Reviewed-by: Anna Sato <annasato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1546310}

--- third_party/blink/web_tests/wpt_internal/shared-worker/back-forward-cache/shared-worker-mixed-clients-eviction.window.js
+++ third_party/blink/web_tests/wpt_internal/shared-worker/back-forward-cache/shared-worker-mixed-clients-eviction.window.js
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: script=/html/browsers/browsing-the-web/back-forward-cache/resources/rc-helper.js
 // META: script=/html/browsers/browsing-the-web/remote-context-helper/resources/remote-context-helper.js
 // META: script=/html/browsers/browsing-the-web/remote-context-helper-tests/resources/test-helper.js
+// META: timeout=long
 
 'use strict';
 
