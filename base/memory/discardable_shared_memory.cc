fea2f17835be8bb7c4f79929857e030a32ea7af0
Egor Pasko <pasko@chromium.org>
2025-11-18 12:41:17 -0800

    android: cleanup: Remove macro defs from linker/ashmem.h
    We are defining macros like ASHMEM_NOT_PURGED originally from
    <linux/ashmem.h>. I am not sure why. Perhaps, some time ago the file was
    not in sysroot/NDK?
    
    We have the header file in sysroot(s) like:
      //third_party/android_prebuilts/build_tools/sysroots/
        i686-unknown-linux-musl/include/linux/ashmem.h
    
    This linux/ashmem.h from sysroot defines strictly more than
    linker/ashmem.h. We use the former in seccomp-bpf-helpers already.
    
    Instead of defining these macros again, just include the contents of
    linux/ashmem.h where needed.
    
    Bypass-Check-License: AOSP copyright for ashmem.h
    Bug: None
    Change-Id: I3641415dd0800a4069b226f665753eebe3941cf2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7170063
    Reviewed-by: Fred Shih <ffred@chromium.org>
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1546741}

--- base/memory/discardable_shared_memory.cc
+++ base/memory/discardable_shared_memory.cc
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if BUILDFLAG(IS_ANDROID)
+#include <linux/ashmem.h>
+
 #include "base/android/linker/ashmem.h"
 #endif
 
