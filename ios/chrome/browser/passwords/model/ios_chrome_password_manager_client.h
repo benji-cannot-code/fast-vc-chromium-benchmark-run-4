47bdee695feddbd85b669090372e10e1a5955ad0
Noémie St-Onge <noemies@google.com>
2025-11-18 13:37:04 -0800

    [iOSPasskeysM3] Add IOSWebAuthnCredentialsDelegate
    This CL adds the iOS implementation of the WebAuthnCredentialsDelegate
    and makes the IOSChromePasswordManagerClient provide an instance of it
    via the GetWebAuthnCredentialsDelegateForDriver method.
    
    Bug: 459451476
    Change-Id: I76b9cfe3962f45eac242cbbb1788030d66bb8d61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7147980
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1546786}

--- ios/chrome/browser/passwords/model/ios_chrome_password_manager_client.h
+++ ios/chrome/browser/passwords/model/ios_chrome_password_manager_client.h
@@ -156,6 +156,9 @@ class IOSChromePasswordManagerClient
   void UpdateFormManagers() override;
   bool IsIsolationForPasswordSitesEnabled() const override;
   bool IsNewTabPage() const override;
+  password_manager::WebAuthnCredentialsDelegate*
+  GetWebAuthnCredentialsDelegateForDriver(
+      password_manager::PasswordManagerDriver* driver) override;
   safe_browsing::PasswordProtectionService* GetPasswordProtectionService()
       const override;
   autofill::AutofillCrowdsourcingManager* GetAutofillCrowdsourcingManager()
--- ios/chrome/browser/passwords/model/ios_chrome_password_manager_client.mm
+++ ios/chrome/browser/passwords/model/ios_chrome_password_manager_client.mm
@@ -30,10 +30,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/password_manager/core/browser/password_requirements_service.h"
 #import "components/password_manager/core/browser/password_sync_util.h"
 #import "components/password_manager/core/common/password_manager_pref_names.h"
+#import "components/password_manager/ios/ios_password_manager_driver.h"
 #import "components/password_manager/ios/password_manager_ios_util.h"
 #import "components/sync/service/sync_service.h"
 #import "components/translate/core/browser/translate_manager.h"
 #import "components/ukm/ios/ukm_url_recorder.h"
+#import "components/webauthn/ios/features.h"
+#import "components/webauthn/ios/ios_webauthn_credentials_delegate_factory.h"
 #import "ios/chrome/browser/enterprise/connectors/reporting/ios_reporting_event_router_factory.h"
 #import "ios/chrome/browser/passwords/model/features.h"
 #import "ios/chrome/browser/passwords/model/ios_chrome_account_password_store_factory.h"
@@ -388,6 +391,18 @@ bool IOSChromePasswordManagerClient::IsIsolationForPasswordSitesEnabled()
 
 bool IOSChromePasswordManagerClient::IsNewTabPage() const {
   return false;
+}
+
+password_manager::WebAuthnCredentialsDelegate*
+IOSChromePasswordManagerClient::GetWebAuthnCredentialsDelegateForDriver(
+    password_manager::PasswordManagerDriver* driver) {
+  if (!base::FeatureList::IsEnabled(kIOSPasskeyModalLoginWithShim)) {
+    return nullptr;
+  }
+
+  return IOSWebAuthnCredentialsDelegateFactory::GetFactory(bridge_.webState)
+      ->GetDelegateForFrame(
+          static_cast<IOSPasswordManagerDriver*>(driver)->web_frame_id());
 }
 
 safe_browsing::PasswordProtectionService*
