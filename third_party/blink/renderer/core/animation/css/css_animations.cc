e45dffae83d28a26a2e5d36fb0021532443c9082
David Awogbemila <awogbemila@chromium.org>
2025-11-18 05:58:55 -0800

    [animation-trigger] Restrict attachment updates to relevant animations
    It was wasteful to iterate over all animations in all timelines when
    performing trigger attachment updates. This patch changes this so that
    we only iterate over animations with attachments.
    
    There is also some suspicion[1] that this wastefulness might be
    contributing to the renderer hang in the linked bugs.
    
    This patch doesn't introduce a functional change.
    
    [1] https://issues.chromium.org/issues/446159591#comment2
    
    Bug: 447174988, 446159591
    Change-Id: I666118b5d8e33920de34045aa6124c8553fe7c09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7140163
    Reviewed-by: David Awogbemila <awogbemila@chromium.org>
    Commit-Queue: David Awogbemila <awogbemila@chromium.org>
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1546480}

--- third_party/blink/renderer/core/animation/css/css_animations.cc
+++ third_party/blink/renderer/core/animation/css/css_animations.cc
@@ -2315,6 +2315,17 @@ void CSSAnimations::MaybeApplyPendingUpdate(Element* element) {
     }
     css_animation.SetRange(entry.range_start, entry.range_end);
     css_animation.SetTriggerAttachments(entry.trigger_attachments);
+    if (RuntimeEnabledFeatures::LimitTriggerAttachmentUpdatesEnabled()) {
+      if (entry.trigger_attachments) {
+        element->GetDocument()
+            .GetDocumentAnimations()
+            .AddPendingTriggerAttachmentUpdate(&css_animation);
+      } else {
+        element->GetDocument()
+            .GetDocumentAnimations()
+            .RemovePendingTriggerAttachmentUpdate(&css_animation);
+      }
+    }
     css_animation.SetTriggerActionPlayState(
         entry.play_state_list[entry.index % entry.play_state_list.size()]);
     running_animations_[entry.index]->Update(entry);
@@ -2352,6 +2363,10 @@ void CSSAnimations::MaybeApplyPendingUpdate(Element* element) {
 
     if (!entry.trigger_attachments) {
       animation->play();
+    } else if (RuntimeEnabledFeatures::LimitTriggerAttachmentUpdatesEnabled()) {
+      element->GetDocument()
+          .GetDocumentAnimations()
+          .AddPendingTriggerAttachmentUpdate(animation);
     }
     if (inert_animation->Paused()) {
       animation->pause();
