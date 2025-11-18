8f3afc9c6672e278b643ecfd88fac3c6868b0d05
Steve Becker <stevebe@microsoft.com>
2025-11-17 16:15:32 -0800

    [DomStorage] Implement and use SessionStorageLevelDB::ReadAllMetadata
    Creates a vector of `MapMetadata` to return from all 'namespace-'
    prefixed entries in the LevelDB. Each entry provides the map's session
    ID, storage key and map ID through this format:
    
    key: 'namespace-<session id>-<storage key>'
    
    value: <map id>
    
    Also reads the 'next-map-id' key. `SessionStorageLevelDB` stores map IDs
    as integer text strings, For example, the characters "154".
    `ReadAllMetadata()` converts these text values into `int64_t` map IDs.
    
    Bug: 377242771
    Change-Id: If18313c37cdad6b74d0113afcede32f19dd3d987
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7159708
    Commit-Queue: Steve Becker <stevebe@microsoft.com>
    Reviewed-by: Evan Stade <evanstade@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1546210}

---
+++ components/services/storage/dom_storage/leveldb/test_support/test_leveldb_utils.cc
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/services/storage/dom_storage/leveldb/test_support/test_leveldb_utils.h"
+
+namespace storage {
+
+std::vector<uint8_t> ToBytes(base::span<const uint8_t> source) {
+  return std::vector<uint8_t>(source.begin(), source.end());
+}
+
+}  // namespace storage
