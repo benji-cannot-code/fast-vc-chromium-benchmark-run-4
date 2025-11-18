2813022d68517e19cac0bb54c6109368ec16874e
Timofey Chudakov <tchudakov@google.com>
2025-11-18 09:28:38 -0800

    [KA] Remove TabSwitchingDelegate::addTab.
    The method has no production usage.
    
    Bug: n/a
    Change-Id: If68d71b3fb6ef51d84d0c74739d32f32f69697e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7167075
    Reviewed-by: Luchen Peng <luchenpeng@google.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Auto-Submit: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1546615}

--- chrome/browser/keyboard_accessory/android/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/button_group_component/KeyboardAccessoryButtonGroupMediator.java
+++ chrome/browser/keyboard_accessory/android/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/button_group_component/KeyboardAccessoryButtonGroupMediator.java
@@ -89,11 +89,6 @@ class KeyboardAccessoryButtonGroupMediator
     }
 
     @Override
-    public void addTab(KeyboardAccessoryData.Tab tab) {
-        mModel.get(TABS).add(tab);
-    }
-
-    @Override
     public void removeTab(KeyboardAccessoryData.Tab tab) {
         mModel.get(TABS).remove(tab);
     }
