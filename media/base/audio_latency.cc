1868a72bf67e44ae3cc4c647d8f97c3975b86a43
Richard Townsend <ritownsend@google.com>
2025-11-18 12:42:21 -0800

    cleanup: use C++20 to calculate buffer size
    Use std::bit_ceil to be a little more idiomatic.
    
    The old code truncated the 20/80ms calculation implicitly via
    a hidden static cast. The new code makes this explicit, and
    replaces the hidden truncation with a more explicit round().
    This gives identical results for the standard sampling
    rates.
    
    Change-Id: I093cb8feee54aa6a9ac18c8820c1bebc821ebbdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7156005
    Commit-Queue: Richard Townsend <ritownsend@google.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1546742}

--- media/base/audio_latency.cc
+++ media/base/audio_latency.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include <algorithm>
+#include <bit>
 #include <cmath>
 #include <numeric>
 
@@ -27,25 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace media {
-
-namespace {
-
-#if !BUILDFLAG(IS_WIN) && !BUILDFLAG(IS_FUCHSIA)
-// Taken from "Bit Twiddling Hacks"
-// http://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
-uint32_t RoundUpToPowerOfTwo(uint32_t v) {
-  v--;
-  v |= v >> 1;
-  v |= v >> 2;
-  v |= v >> 4;
-  v |= v >> 8;
-  v |= v >> 16;
-  v++;
-  return v;
-}
-#endif
-
-}  // namespace
 
 // static
 bool AudioLatency::IsResamplingPassthroughSupported(Type type) {
@@ -72,7 +54,8 @@ int AudioLatency::GetHighLatencyBufferSize(int sample_rate,
 #if BUILDFLAG(USE_CRAS)
   // Use 80ms rounded to a power of 2.
   const double eighty_ms_size = 8.0 * sample_rate / 100;
-  const int high_latency_buffer_size = RoundUpToPowerOfTwo(eighty_ms_size);
+  const int high_latency_buffer_size =
+      std::bit_ceil(static_cast<uint32_t>(std::round(eighty_ms_size)));
 #elif BUILDFLAG(IS_FUCHSIA)
   // Use 80ms buffers. Doesn't need to be aligned to power of 2, but it should
   // be a multiple of the scheduling period used for audio threads.
@@ -104,7 +87,8 @@ int AudioLatency::GetHighLatencyBufferSize(int sample_rate,
   // On Linux, the minimum hardware buffer size is 512, so the lower calculated
   // values are unused.  OSX may have a value as low as 128.
   const double twenty_ms_size = 2.0 * sample_rate / 100;
-  const int high_latency_buffer_size = RoundUpToPowerOfTwo(twenty_ms_size);
+  const int high_latency_buffer_size =
+      std::bit_ceil(static_cast<uint32_t>(std::round(twenty_ms_size)));
 #endif
 
   return std::max(preferred_buffer_size, high_latency_buffer_size);
