ce6cd0303515741b681e35f5ba2adb9a97a3d65b
Shin Kawamura <kawasin@google.com>
2025-11-17 17:18:55 -0800

    Ensure ScopedServiceBindingBatch is closed in a local stack
    @MustBeClosed enforces that ScopedServiceBindingBatch.scoped() is always
    used in try-with-resources syntax.
    
    To prevent partially overlapped scope window, ScopedServiceBindingBatch
    should be closed before the caller function returns.
    
    Bug: 458576170
    Change-Id: I100d42865abfa57165a589b725b3899151adfb26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7161180
    Commit-Queue: Shin Kawamura <kawasin@google.com>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1546239}

--- base/android/java/src/org/chromium/base/process_launcher/ScopedServiceBindingBatch.java
+++ base/android/java/src/org/chromium/base/process_launcher/ScopedServiceBindingBatch.java
@@ -7,6 +7,8 @@ package org.chromium.base.process_launcher;
 
 import android.os.Handler;
 
+import com.google.errorprone.annotations.MustBeClosed;
+
 import org.jni_zero.CalledByNative;
 
 import org.chromium.base.BindingRequestQueue;
@@ -36,6 +38,7 @@ public interface ScopedServiceBindingBatch extends AutoCloseable {
      * the UI thread.
      */
     @CalledByNative
+    @MustBeClosed
     static @Nullable ScopedServiceBindingBatch scoped() {
         ScopedServiceBindingBatchImpl realBatch = ScopedServiceBindingBatchImpl.scoped();
         if (realBatch != null) {
