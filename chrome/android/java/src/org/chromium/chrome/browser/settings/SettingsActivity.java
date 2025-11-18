38961faf404fcdc7a5d78f644ca7a7916d66f86e
Hidehiko Abe <hidehiko@chromium.org>
2025-11-18 09:56:44 -0800

    Fix UMA report of Settings.FragmentAttached
    This CL slightly relaxes the condition to take the metrics.
    In Multi-Column enabled, we do not use MAIN_FRAGMENT_TAG, as most
    fragments are handled in the library side.
    
    BUG=404074032
    TEST=Tryjob. Run on DUT.
    
    Change-Id: I4a28cc7db9cb2eaca643713563d68823d8008af1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7167150
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Gazal Agarwal <agazal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1546639}

--- chrome/android/java/src/org/chromium/chrome/browser/settings/SettingsActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/SettingsActivity.java
@@ -1042,7 +1042,8 @@ public class SettingsActivity extends ChromeBaseAppCompatActivity
         @Override
         public void onFragmentAttached(
                 FragmentManager fragmentManager, Fragment fragment, Context context) {
-            if (!MAIN_FRAGMENT_TAG.equals(fragment.getTag())) {
+            if (!(fragment instanceof SettingsFragment)
+                    && !MAIN_FRAGMENT_TAG.equals(fragment.getTag())) {
                 return;
             }
 
