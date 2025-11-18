f4fefc7f4bd184632f4b5e96f2068429168dcb1e
Steinar H. Gunderson <sesse@chromium.org>
2025-11-18 06:32:30 -0800

    Make Page not Supplementable.
    Part of Supplementable cleanup. Done with a script followed by
    human cleanup/review and git cl format.
    
    Bug: 460165754
    Change-Id: I861e0068e3598152775574a8464d9b043983d6d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7156580
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1546497}

--- third_party/blink/renderer/modules/modules_initializer.cc
+++ third_party/blink/renderer/modules/modules_initializer.cc
@@ -120,19 +120,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace blink {
-namespace {
 
 #if BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_DESKTOP_ANDROID)
 
 class SuspendCaptureObserver : public GarbageCollected<SuspendCaptureObserver>,
-                               public Supplement<Page>,
                                public PageVisibilityObserver {
  public:
-  static constexpr auto kSupplementIndex =
-      Page::Supplements::kSuspendCaptureObserver;
-
-  explicit SuspendCaptureObserver(Page& page)
-      : Supplement<Page>(page), PageVisibilityObserver(&page) {}
+  explicit SuspendCaptureObserver(Page& page) : PageVisibilityObserver(&page) {}
 
   // PageVisibilityObserver overrides:
   void PageVisibilityChanged() override {
@@ -158,14 +152,11 @@ class SuspendCaptureObserver : public GarbageCollected<SuspendCaptureObserver>,
   }
 
   void Trace(Visitor* visitor) const override {
-    Supplement<Page>::Trace(visitor);
     PageVisibilityObserver::Trace(visitor);
   }
 };
 
 #endif  // BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_DESKTOP_ANDROID)
-
-}  // namespace
 
 void ModulesInitializer::Initialize() {
   // Strings must be initialized before calling CoreInitializer::init().
@@ -371,7 +362,8 @@ void ModulesInitializer::ProvideModulesToPage(
   StorageNamespace::ProvideSessionStorageNamespaceTo(page, namespace_id);
   AudioGraphTracer::ProvideAudioGraphTracerTo(page);
 #if BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_DESKTOP_ANDROID)
-  page.ProvideSupplement(MakeGarbageCollected<SuspendCaptureObserver>(page));
+  page.SetSuspendCaptureObserver(
+      MakeGarbageCollected<SuspendCaptureObserver>(page));
 #endif  // BUILDFLAG(IS_ANDROID)  && !BUILDFLAG(IS_DESKTOP_ANDROID)
 }
 
