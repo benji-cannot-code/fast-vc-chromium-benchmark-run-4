04a21cd66aaa49638f716dbf6ca36ce9852eb7b6
Andrew Paseltiner <apaseltiner@chromium.org>
2025-11-18 11:39:00 -0800

    Add JS-surface test coverage for scheduler.postTask
    https://developer.mozilla.org/en-US/docs/Web/API/Scheduler/postTask
    
    Change-Id: Ib39d99b6055bfb49ee86c27c0801709b1aed3eb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7169406
    Auto-Submit: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Josh Karlin <jkarlin@chromium.org>
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1546707}

--- chrome/browser/page_load_metrics/observers/ad_metrics/ads_page_load_metrics_observer_browsertest.cc
+++ chrome/browser/page_load_metrics/observers/ad_metrics/ads_page_load_metrics_observer_browsertest.cc
@@ -3281,6 +3281,7 @@ constexpr SurfaceTestCase kSurfaceTestCases[] = {
     {"promise_resolve", "Promise.resolve().then($1);"},
     {"promise_reject", "Promise.reject().catch($1);"},
     {"queueMicrotask", "queueMicrotask($1);"},
+    {"postTask", "scheduler.postTask($1);"},
     {"dynamic_script_tag", R"SCRIPT(
       const s = document.createElement("script");
       s.innerText = '$1();';
