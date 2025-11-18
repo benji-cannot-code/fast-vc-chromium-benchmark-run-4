5e18edaa6ec30eba2e9179458d6dd1622e1c656e
Jérôme Lebel <jlebel@chromium.org>
2025-11-18 05:46:04 -0800

    [iOS] Move PillView into its one file
    PillView is also renamed into SearchEngineCurrentDefaultPillView.
    There is no behavior or UI changes with this patch.
    
    Bug: 458252292
    Change-Id: I844a9b0944ea91acacd3d1924cec64474e86b89f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7166329
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1546476}

---
+++ ios/chrome/browser/search_engine_choice/ui/search_engine_current_default_pill_view.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SEARCH_ENGINE_CHOICE_UI_SEARCH_ENGINE_CURRENT_DEFAULT_PILL_VIEW_H_
+#define IOS_CHROME_BROWSER_SEARCH_ENGINE_CHOICE_UI_SEARCH_ENGINE_CURRENT_DEFAULT_PILL_VIEW_H_
+
+#import <UIKit/UIKit.h>
+
+// View to display the 'Current default' pill.
+@interface SearchEngineCurrentDefaultPillView : UIView
+
+- (instancetype)init NS_DESIGNATED_INITIALIZER;
+- (instancetype)initWithFrame:(CGRect)frame NS_UNAVAILABLE;
+- (instancetype)initWithCoder:(NSCoder*)coder NS_UNAVAILABLE;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_SEARCH_ENGINE_CHOICE_UI_SEARCH_ENGINE_CURRENT_DEFAULT_PILL_VIEW_H_
