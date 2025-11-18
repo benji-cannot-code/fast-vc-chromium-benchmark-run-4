cdf9917bc5b3aaef07d73b29cab0b7b8fba3f61a
Andrew Paseltiner <apaseltiner@chromium.org>
2025-11-18 08:40:07 -0800

    Remove DB version metrics for source/report modification
    OBSOLETE_HISTOGRAMS=ARA is deprecated and there will be no further
    removal of DB migrations until the entire API is removed.
    
    Change-Id: I08d08f0a8a52647484313b02e409348af84f64ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7167787
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Auto-Submit: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1546583}

--- content/browser/attribution_reporting/attribution_resolver_unittest.cc
+++ content/browser/attribution_reporting/attribution_resolver_unittest.cc
@@ -233,10 +233,7 @@ class AttributionResolverTest : public testing::Test {
 };
 
 TEST_F(AttributionResolverTest, ImpressionStoredAndRetrieved_ValuesIdentical) {
-  base::HistogramTester histograms;
   storage()->StoreSource(SourceBuilder().Build());
-  histograms.ExpectBucketCount("Conversions.DbVersionOnSourceStored",
-                               AttributionStorageSql::kCurrentVersionNumber, 1);
   EXPECT_THAT(storage()->GetActiveSources(),
               ElementsAre(SourceBuilder().BuildStored()));
 }
@@ -478,8 +475,6 @@ TEST_F(AttributionResolverTest,
 }
 
 TEST_F(AttributionResolverTest, ConversionReportDeleted_RemovedFromStorage) {
-  base::HistogramTester histograms;
-
   storage()->StoreSource(SourceBuilder().Build());
   EXPECT_EQ(AttributionTrigger::EventLevelResult::kSuccess,
             MaybeCreateAndStoreEventLevelReport(DefaultTrigger()));
@@ -490,7 +485,6 @@ TEST_F(AttributionResolverTest, ConversionReportDeleted_RemovedFromStorage) {
   DeleteReports(reports);
 
   EXPECT_THAT(storage()->GetAttributionReports(base::Time::Max()), IsEmpty());
-  histograms.ExpectTotalCount("Conversions.DbVersionOnReportSentAndDeleted", 1);
 }
 
 TEST_F(AttributionResolverTest,
--- content/browser/attribution_reporting/attribution_storage_sql.cc
+++ content/browser/attribution_reporting/attribution_storage_sql.cc
@@ -1517,14 +1517,7 @@ bool AttributionStorageSql::DeleteReport(AttributionReport::Id report_id) {
     return true;
   }
 
-  bool success = DeleteReportInternal(report_id);
-  if (success) {
-    base::UmaHistogramCustomCounts(
-        "Conversions.DbVersionOnReportSentAndDeleted", kCurrentVersionNumber,
-        /*min=*/58,
-        /*exclusive_max=*/88, /*buckets=*/30);
-  }
-  return success;
+  return DeleteReportInternal(report_id);
 }
 
 bool AttributionStorageSql::DeleteReportInternal(
