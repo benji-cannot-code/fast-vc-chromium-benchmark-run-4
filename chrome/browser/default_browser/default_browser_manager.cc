e13aced464378681ff6dc4c4c61482da07a7e42b
Foromo Daniel Soromou <koretadaniel@chromium.org>
2025-11-18 09:03:42 -0800

    Default browser: Add a monitor for system default browser changes
    Introduce a new framework to monitor for changes to the system's default
    browser for the http and https protocols. This provides a centralized,
    observable service for other parts to use when they need to react to
    such a change.
    
    The initial implementation provides a functional monitor for Windows,
    which asynchronously watches the relevant `UserChoice` registry key from
    a background sequence. A no-op implementation is provided for all other
    platforms.
    
    Low-Coverage-Reason: The main implementation of the manger will be in
    different CL.
    
    Follow-up: 1- Change handler from the manager side. 2- Metric recording.
    3- More browsers tests on the manager side.
    
    Bug: 454597108
    Change-Id: I0f34b842a913c1cde818df452bb0ab751b71ff7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7141265
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Commit-Queue: Daniel Soromou <koretadaniel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1546600}

--- chrome/browser/default_browser/default_browser_manager.cc
+++ chrome/browser/default_browser/default_browser_manager.cc
@@ -5,11 +5,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/default_browser/default_browser_manager.h"
 
+#include <utility>
+
+#include "base/functional/callback.h"
 #include "base/memory/scoped_refptr.h"
+#include "chrome/browser/default_browser/default_browser_monitor.h"
 #include "chrome/browser/default_browser/setters/shell_integration_default_browser_setter.h"
 #include "chrome/browser/shell_integration.h"
 
 namespace default_browser {
+
+DefaultBrowserManager::DefaultBrowserManager() = default;
+
+DefaultBrowserManager::~DefaultBrowserManager() = default;
 
 // Static
 void DefaultBrowserManager::GetDefaultBrowserState(
@@ -24,6 +32,17 @@ DefaultBrowserManager::CreateControllerFor(
     DefaultBrowserEntrypointType entrypoint) {
   return std::make_unique<DefaultBrowserController>(
       std::make_unique<ShellIntegrationDefaultBrowserSetter>(), entrypoint);
+}
+
+base::CallbackListSubscription
+DefaultBrowserManager::RegisterDefaultBrowserChanged(
+    base::RepeatingClosure callback) {
+  if (!monitor_) {
+    monitor_ = std::make_unique<DefaultBrowserMonitor>();
+    monitor_->StartMonitor();
+  }
+
+  return monitor_->RegisterDefaultBrowserChanged(std::move(callback));
 }
 
 }  // namespace default_browser
