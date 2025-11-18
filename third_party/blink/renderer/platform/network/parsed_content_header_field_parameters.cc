5d523eebe9677ff58fa51c4dce8d9c8449caa71c
Fredrik Söderquist <fs@opera.com>
2025-11-18 15:43:04 -0800

    Use StringView for ParsedContentHeaderFieldParameters
    Pass a StringView to the ParameterValueForName() function. It's very
    common to call this function with a string literal.
    
    Propagate up to the two users ParsedContentDisposition and
    ParsedContentType.
    
    Change-Id: If22763665d43003340c3303be8fe68246551a314
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7167923
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1546853}

--- third_party/blink/renderer/platform/network/parsed_content_header_field_parameters.cc
+++ third_party/blink/renderer/platform/network/parsed_content_header_field_parameters.cc
@@ -68,7 +68,7 @@ ParsedContentHeaderFieldParameters::Parse(HeaderFieldTokenizer tokenizer,
 }
 
 String ParsedContentHeaderFieldParameters::ParameterValueForName(
-    const String& name) const {
+    StringView name) const {
   if (!name.ContainsOnlyASCIIOrEmpty())
     return String();
 
