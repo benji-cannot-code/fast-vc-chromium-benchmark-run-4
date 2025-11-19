1d4079ed6228002dbbefbafb47ed242b4a341d51
Yuwei Huang <yuweih@chromium.org>
2025-11-19 10:53:11 -0800

    [crd host][wayland] Rename PipewireCaptureStreamManager
    PipewireCaptureStreamManager is a misnormer, since it only works in
    GNOME. I'm about to introduce a simple PortalCaptureStreamManager for
    investigation, so I'd rename PipewireCaptureStreamManager to
    GnomeCaptureStreamManager to prevent confusions.
    
    Bug: 445973705
    Change-Id: Ida6d623aef5a4d536e582febccf24683729dd381
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7171159
    Auto-Submit: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1547341}

--- remoting/host/linux/gnome_remote_desktop_session.h
+++ remoting/host/linux/gnome_remote_desktop_session.h
@@ -17,12 +17,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/types/expected.h"
 #include "remoting/host/linux/ei_sender_session.h"
 #include "remoting/host/linux/gdbus_connection_ref.h"
+#include "remoting/host/linux/gnome_capture_stream_manager.h"
 #include "remoting/host/linux/gnome_desktop_resizer.h"
 #include "remoting/host/linux/gnome_display_config_dbus_client.h"
 #include "remoting/host/linux/gnome_display_config_monitor.h"
 #include "remoting/host/linux/gnome_headless_detector.h"
 #include "remoting/host/linux/gvariant_ref.h"
-#include "remoting/host/linux/pipewire_capture_stream_manager.h"
 #include "remoting/host/linux/pipewire_mouse_cursor_capturer.h"
 #include "remoting/host/persistent_display_layout_manager.h"
 
@@ -61,7 +61,7 @@ class GnomeRemoteDesktopSession {
     return initialization_state_ == InitializationState::kInitialized;
   }
 
-  base::WeakPtr<PipewireCaptureStreamManager> capture_stream_manager() {
+  base::WeakPtr<GnomeCaptureStreamManager> capture_stream_manager() {
     DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
     DCHECK(is_initialized());
     return capture_stream_manager_.GetWeakPtr();
@@ -153,7 +153,7 @@ class GnomeRemoteDesktopSession {
       GUARDED_BY_CONTEXT(sequence_checker_);
   GnomeDisplayConfigMonitor display_config_monitor_ GUARDED_BY_CONTEXT(
       sequence_checker_){display_config_client_.GetWeakPtr()};
-  PipewireCaptureStreamManager capture_stream_manager_
+  GnomeCaptureStreamManager capture_stream_manager_
       GUARDED_BY_CONTEXT(sequence_checker_);
   PipewireMouseCursorCapturer mouse_cursor_capturer_ GUARDED_BY_CONTEXT(
       sequence_checker_){display_config_monitor_.GetWeakPtr(),
