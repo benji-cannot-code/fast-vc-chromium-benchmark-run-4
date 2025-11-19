f04d9e5b3f25ceb2a6e8a73350997b3880adb308
kylechar <kylechar@chromium.org>
2025-11-19 09:17:24 -0800

    Remove OverlayProcessorUsingStrategy.NumOverlays* histograms
    The histograms have all expired and are no longer very useful.
    
    Fixed: 449728318
    Change-Id: Ie101890160008842cb165c8054bc148817409b41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7163395
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1547259}

--- components/viz/service/display/overlay_processor_using_strategy.cc
+++ components/viz/service/display/overlay_processor_using_strategy.cc
@@ -75,12 +75,6 @@ enum class PromotingMaskCandidates {
 constexpr char kShouldAttemptMultipleOverlaysHistogramName[] =
     "Compositing.Display.OverlayProcessorUsingStrategy."
     "ShouldAttemptMultipleOverlays";
-constexpr char kNumOverlaysPromotedHistogramName[] =
-    "Compositing.Display.OverlayProcessorUsingStrategy.NumOverlaysPromoted";
-constexpr char kNumOverlaysAttemptedHistogramName[] =
-    "Compositing.Display.OverlayProcessorUsingStrategy.NumOverlaysAttempted";
-constexpr char kNumOverlaysFailedHistogramName[] =
-    "Compositing.Display.OverlayProcessorUsingStrategy.NumOverlaysFailed";
 constexpr char kWorkingScaleFactorHistogramName[] =
     "Compositing.Display.OverlayProcessorUsingStrategy."
     "WorkingScaleFactorForRequiredOverlays";
@@ -413,9 +407,6 @@ void OverlayProcessorUsingStrategy::ProcessForOverlays(
   }
 
   DCHECK(candidates->empty() || success);
-  UMA_HISTOGRAM_COUNTS_100(kNumOverlaysPromotedHistogramName,
-                           candidates->size());
-
   UpdateOverlayStatusMap(*candidates);
   UpdateDamageRect(surface_damage_rect_list, *damage_rect);
 
@@ -959,8 +950,6 @@ bool OverlayProcessorUsingStrategy::AttemptMultipleOverlays(
     AggregatedRenderPass* render_pass,
     OverlayCandidateList& candidates) {
   if (sorted_candidates.empty()) {
-    UMA_HISTOGRAM_COUNTS_100(kNumOverlaysAttemptedHistogramName, 0);
-    UMA_HISTOGRAM_COUNTS_100(kNumOverlaysFailedHistogramName, 0);
     return false;
   }
 
@@ -987,8 +976,6 @@ bool OverlayProcessorUsingStrategy::AttemptMultipleOverlays(
           PromotingMaskCandidates::kNoNotRequired);
     }
 
-    UMA_HISTOGRAM_COUNTS_100(kNumOverlaysAttemptedHistogramName, 0);
-    UMA_HISTOGRAM_COUNTS_100(kNumOverlaysFailedHistogramName, 0);
     return false;
   }
 
@@ -1058,7 +1045,6 @@ bool OverlayProcessorUsingStrategy::AttemptMultipleOverlays(
     // Check for support.
     CheckOverlaySupport(new_plane_candidate, &candidates);
   }
-  const int num_overlays_attempted = candidates.size();
 
   // Update the test candidates so we can process the result, use EraseIf below
   // and tell the OverlayCombinationCache which ones succeeded/failed.
@@ -1086,12 +1072,6 @@ bool OverlayProcessorUsingStrategy::AttemptMultipleOverlays(
   std::erase_if(test_candidates, [](auto& proposed) -> bool {
     return !proposed.candidate.overlay_handled;
   });
-  const int num_overlays_promoted = candidates.size();
-
-  UMA_HISTOGRAM_COUNTS_100(kNumOverlaysAttemptedHistogramName,
-                           num_overlays_attempted);
-  UMA_HISTOGRAM_COUNTS_100(kNumOverlaysFailedHistogramName,
-                           num_overlays_attempted - num_overlays_promoted);
 
   if (candidates.empty()) {
     LogStrategyEnumUMA(OverlayStrategy::kNoStrategyAllFail);
