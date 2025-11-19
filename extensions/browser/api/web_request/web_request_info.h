4b29132be172e1f37c86bc78b8bf35471e76e70d
Vlad Krot <vkrot@google.com>
2025-11-19 06:54:01 -0800

    [Extensions] Web Request parse SecurityInfo
    The change parses SecurityInfo object according to specification:
    https://github.com/w3c/webextensions/pull/899
    
    @devlin has given approval to start working on this feature before the
    spec is finalized.
    
    Fixed: 460072357
    Change-Id: I160c4b61c0d2da46eab10fae4e0794c7f692d26f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7167074
    Commit-Queue: Vlad Krot <vkrot@google.com>
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1547169}

--- extensions/browser/api/web_request/web_request_info.h
+++ extensions/browser/api/web_request/web_request_info.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ipc/constants.mojom.h"
 #include "net/http/http_request_headers.h"
 #include "net/http/http_response_headers.h"
+#include "net/ssl/ssl_info.h"
 #include "services/network/public/cpp/resource_request.h"
 #include "services/network/public/mojom/url_response_head.mojom-forward.h"
 #include "url/gurl.h"
@@ -99,6 +100,8 @@ struct WebRequestInfo {
   // Fill in response data for this request.
   void AddResponseInfoFromResourceResponse(
       const network::mojom::URLResponseHead& response);
+
+  void AddSslInfo(const std::optional<net::SSLInfo>& ssl_info);
 
   // Erases all actions in `dnr_actions` that are associated with the given
   // `extension_id`.
@@ -210,6 +213,9 @@ struct WebRequestInfo {
   // TODO(karandeepb, mcnee): For subresources, having "parent" in the name is
   // misleading. This should be renamed to indicate that this is the initiator.
   const content::GlobalRenderFrameHostId parent_routing_id;
+
+  // For SecurityInfo object.
+  std::optional<net::SSLInfo> ssl_info;
 };
 
 }  // namespace extensions
