b9843fcf319864efaf1253f29bce2b4ac0c749cd
Geoff Lang <geofflang@chromium.org>
2025-11-19 14:23:25 -0800

    Add metrics of how often old GPU persistent caches are deleted.
    This will tell us how often we are in a cold start state where the
    previous cache was deleted due to a version change in Chrome, Skia or
    Dawn.
    
    Bug: 399642827
    Change-Id: I934e5b683a4abd935067191c9133fb5f3081979c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7170148
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Geoff Lang <geofflang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1547481}

--- components/viz/host/persistent_cache_sandboxed_file_factory.cc
+++ components/viz/host/persistent_cache_sandboxed_file_factory.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/hash/sha1.h"
 #include "base/logging.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/strings/string_util.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
@@ -59,19 +60,22 @@ void DeleteStaleFiles(const base::FilePath& cache_root_dir,
 
   const std::string version_suffix = GetVersionSuffix(product);
 
+  bool deleted_stale_cache = false;
   base::FilePath cache_dir = cache_root_dir.Append(cache_id);
-  if (!base::PathExists(cache_dir)) {
-    return;
-  }
-
-  base::FileEnumerator enumerator(cache_dir, false,
-                                  base::FileEnumerator::DIRECTORIES);
-  for (base::FilePath name = enumerator.Next(); !name.empty();
-       name = enumerator.Next()) {
-    if (name.BaseName().MaybeAsASCII() != version_suffix) {
-      base::DeletePathRecursively(name);
+  if (base::PathExists(cache_dir)) {
+    base::FileEnumerator enumerator(cache_dir, false,
+                                    base::FileEnumerator::DIRECTORIES);
+    for (base::FilePath name = enumerator.Next(); !name.empty();
+         name = enumerator.Next()) {
+      if (name.BaseName().MaybeAsASCII() != version_suffix) {
+        base::DeletePathRecursively(name);
+        deleted_stale_cache = true;
+      }
     }
   }
+
+  base::UmaHistogramBoolean("GPU.PersistentCache.StaleCacheDeleted",
+                            deleted_stale_cache);
 }
 
 bool CreateCacheDirectory(const base::FilePath& cache_dir) {
