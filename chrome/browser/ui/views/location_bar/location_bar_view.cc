ee530bfcc1be44ae8d5e51b71977468550bebf10
jj <jj@imput.net>
2025-11-19 12:55:48 -0800

    [Linux UI] Respect middle click paste preference
    Use the "middle click paste" function to determine whether
    to accept selection clipboard pasting.
    
    Bug: 399412221
    Change-Id: I2d70eb10e41825808b2a020f4d203c257577a85c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7157243
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Ananya Seelam <ananyaseelam@google.com>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1547421}

--- chrome/browser/ui/views/location_bar/location_bar_view.cc
+++ chrome/browser/ui/views/location_bar/location_bar_view.cc
@@ -2001,6 +2001,7 @@ void LocationBarView::OnLocationIconPressed(const ui::MouseEvent& event) {
   }
 
   if (event.IsOnlyMiddleMouseButton() &&
+      ui::Clipboard::IsMiddleClickPasteEnabled() &&
       ui::Clipboard::IsSupportedClipboardBuffer(
           ui::ClipboardBuffer::kSelection)) {
     std::u16string text;
