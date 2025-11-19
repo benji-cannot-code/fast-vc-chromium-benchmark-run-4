fd65b60b601685708fc27ad625503b7ed83a14f5
Caroline Rising <corising@chromium.org>
2025-11-19 13:31:24 -0800

    Add tab group support to vertical tabs.
    Still to do: collapse and integrate the bubble view
    
    Bug: 439955962, 461864400, 439961205, 439960615
    Change-Id: Ide07c11b136cab3210d77b1cb3828f1fa214416c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7169517
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1547443}

--- chrome/browser/ui/views/tabs/vertical/tab_collection_node.cc
+++ chrome/browser/ui/views/tabs/vertical/tab_collection_node.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/tab_strip_api/utilities/tab_strip_api_utilities.h"
 #include "chrome/browser/ui/views/tabs/vertical/vertical_pinned_tab_container_view.h"
 #include "chrome/browser/ui/views/tabs/vertical/vertical_split_tab_view.h"
+#include "chrome/browser/ui/views/tabs/vertical/vertical_tab_group_view.h"
 #include "chrome/browser/ui/views/tabs/vertical/vertical_tab_strip_view.h"
 #include "chrome/browser/ui/views/tabs/vertical/vertical_tab_view.h"
 #include "chrome/browser/ui/views/tabs/vertical/vertical_unpinned_tab_container_view.h"
@@ -69,8 +70,7 @@ std::unique_ptr<views::View> TabCollectionNode::CreateViewForNode(
     case Type::kSplitTab:
       return std::make_unique<VerticalSplitTabView>(node_for_view);
     case Type::kTabGroup:
-      // TODO(crbug.com/442567916): support tab groups.
-      break;
+      return std::make_unique<VerticalTabGroupView>(node_for_view);
     case Type::kTab:
       return std::make_unique<VerticalTabView>(node_for_view);
   }
