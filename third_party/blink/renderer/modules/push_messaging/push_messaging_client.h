95bd37f66f79cf9e182252a451a07e940a6044fa
Steinar H. Gunderson <sesse@chromium.org>
2025-11-19 01:03:46 -0800

    Remove leftover includes of supplementable.h.
    Some fixup includes were needed, since headers relied on
    supplementable.h to bring in e.g. Member.
    
    Bug: 460165754
    Change-Id: Iab3a205d6c0a355dc9f910cd69a9428862591551
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7168906
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1547025}

--- third_party/blink/renderer/modules/push_messaging/push_messaging_client.h
+++ third_party/blink/renderer/modules/push_messaging/push_messaging_client.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_wrapper_mode.h"
-#include "third_party/blink/renderer/platform/supplementable.h"
 
 namespace blink {
 
--- third_party/blink/renderer/modules/screen_orientation/screen_orientation_controller.h
+++ third_party/blink/renderer/modules/screen_orientation/screen_orientation_controller.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/modules/screen_orientation/web_lock_orientation_callback.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_associated_remote.h"
-#include "third_party/blink/renderer/platform/supplementable.h"
 #include "ui/display/mojom/screen_orientation.mojom-blink.h"
 #include "ui/gfx/geometry/rect.h"
 
