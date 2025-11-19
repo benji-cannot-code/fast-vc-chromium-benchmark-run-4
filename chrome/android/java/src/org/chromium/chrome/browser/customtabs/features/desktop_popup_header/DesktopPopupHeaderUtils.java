437fac89b828dc65e51cec6e5edeff6e74700988
Bartosz Chominski <chominskib@google.com>
2025-11-19 11:56:43 -0800

    popup-ui: Implement DesktopPopupHeader component
    This component is built using the Model-View-ViewModel pattern.
    
    A video of how the UI introduced by this CL looks like: https://screencast.googleplex.com/cast/NjE4NTk3ODYwNjY0OTM0NHxjYmMwOThiZi02NQ
    
    go/popups-on-clank
    go/al-clank-popup
    
    Bug: 413578988
    Change-Id: If9e0b60145365229ae3246e57e606d2f7f0b112e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7146429
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Commit-Queue: Bartosz Chomiński <chominskib@google.com>
    Cr-Commit-Position: refs/heads/main@{#1547382}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/desktop_popup_header/DesktopPopupHeaderUtils.java
@@ -0,0 +1,55 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.customtabs.features.desktop_popup_header;
+
+import android.os.Build;
+
+import androidx.annotation.ChecksSdkIntAtLeast;
+import androidx.annotation.IdRes;
+import androidx.annotation.LayoutRes;
+
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.chrome.R;
+import org.chromium.chrome.browser.browserservices.intents.BrowserServicesIntentDataProvider;
+import org.chromium.chrome.browser.browserservices.intents.BrowserServicesIntentDataProvider.CustomTabsUiType;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
+
+@NullMarked
+public class DesktopPopupHeaderUtils {
+    /**
+     * Checks whether desktop-like contextual popup UI for Custom Tabs is enabled.
+     *
+     * @param intentDataProvider contains intent data related to the current browser service.
+     * @return true when the intent will result in opening a new popup window, otherwise false.
+     */
+    @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.VANILLA_ICE_CREAM)
+    public static boolean isDesktopPopupHeaderEnabled(
+            final BrowserServicesIntentDataProvider intentDataProvider) {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM
+                && ChromeFeatureList.sAndroidWindowPopupCustomTabUi.isEnabled()
+                && intentDataProvider.getUiType() == CustomTabsUiType.POPUP;
+    }
+
+    @LayoutRes
+    public static int getMainLayoutId() {
+        return R.layout.custom_tab_desktop_popup_main_layout;
+    }
+
+    @LayoutRes
+    public static int getHeaderLayoutId() {
+        return R.layout.custom_tab_desktop_popup_header_layout;
+    }
+
+    @IdRes
+    public static int getContentViewId() {
+        return R.id.desktop_popup_content;
+    }
+
+    @IdRes
+    public static int getHeaderViewStubViewId() {
+        return R.id.desktop_popup_header;
+    }
+}
