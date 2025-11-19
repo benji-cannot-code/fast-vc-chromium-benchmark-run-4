e4b9d5c56d0898133a70b8882de5e2353915cd24
Sasha Kunitskaya <saskekun@google.com>
2025-11-19 08:29:41 -0800

    [UNO FF] Add prefs for showing history promo 5 times
    The history sync promo on History page should be shown five times only.
    This CL introduces signin prefs to implement this and adds method to
    define whether the promo should be shown or not
    
    Bug: 418144407
    Change-Id: I203082b5ee38ed55a7e909eb05314d28ee86fb48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7118424
    Reviewed-by: Amelie Schneider <amelies@google.com>
    Commit-Queue: Sasha Kunitskaya <saskekun@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1547228}

--- chrome/browser/signin/signin_promo.cc
+++ chrome/browser/signin/signin_promo.cc
@@ -189,6 +189,8 @@ void RegisterProfilePrefs(
                                 0);
   registry->RegisterIntegerPref(
       prefs::kAddressSignInPromoShownCountPerProfileForLimitsExperiment, 0);
+  registry->RegisterIntegerPref(
+      prefs::kHistoryPageHistorySyncPromoShownCountPerProfile, 0);
 }
 
 }  // namespace signin
