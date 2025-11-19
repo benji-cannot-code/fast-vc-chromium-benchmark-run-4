318f88dbdfb08936fe1bc65b1406d32178135354
Michael Wu <mklw@google.com>
2025-11-19 13:46:16 -0800

    Add metrics for Desktop-like Incognito windows on Clank
    - Number of running (active) Incognito instances, for Desktop and non-Desktop
    - Number of total (inactive + active) Incognito instances, for Desktop and non-Desktop
    - Clicks on new Incognito indicator button and the associated
    "Close all windows" button.
    
    Bug: 449614593
    Change-Id: I7b5c9ba67c110b1fe2bfd97de92eaebeeda60b09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7100890
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Commit-Queue: Michael Wu <mklw@google.com>
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1547454}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/incognito/IncognitoIndicatorCoordinator.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/incognito/IncognitoIndicatorCoordinator.java
@@ -16,6 +16,7 @@ import android.view.ViewStub;
 
 import androidx.annotation.VisibleForTesting;
 
+import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
@@ -186,6 +187,7 @@ public class IncognitoIndicatorCoordinator extends ToolbarChild
                 (model, view) -> {
                     int menuId = model.get(ListMenuItemProperties.MENU_ITEM_ID);
                     if (menuId == R.id.close_all_incognito_windows_menu_id) {
+                        RecordUserAction.record("MobileIncognitoIndicatorCloseAllWindows");
                         IncognitoTabHostUtils.closeAllIncognitoTabs();
                     }
                     if (mMenuWindow != null) {
@@ -250,6 +252,7 @@ public class IncognitoIndicatorCoordinator extends ToolbarChild
             mMenuWindow.dismiss();
             return;
         }
+        RecordUserAction.record("MobileIncognitoIndicatorClicked");
         Context context = mIncognitoIndicator.getContext();
         createAndShowMenu(context, buildMenuItems(context));
     }
