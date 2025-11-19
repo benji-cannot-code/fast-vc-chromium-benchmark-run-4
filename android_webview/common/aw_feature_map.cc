dd37e22c22f52556cb3d79b059513e0a8fc21da6
Peter Beverloo <peter@chromium.org>
2025-11-19 09:27:27 -0800

    Remove code for WebView's QUIC idle connection timeout experiment
    There's still another experiment running to see whether it makes sense
    to adjust the default value in Chromium. Until that resolves we can
    hardcode the optimal value for WebView in our code.
    
    The WebView-sided experiment is no longer active as of 142.0.7418.3.
    
    Bug: 427195615
    
    Change-Id: I6d44dcc80e030e91e61e5cb74edd966955d52ce2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7170964
    Reviewed-by: Ashley Newson <ashleynewson@chromium.org>
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1547270}

--- android_webview/common/aw_feature_map.cc
+++ android_webview/common/aw_feature_map.cc
@@ -57,7 +57,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &features::kWebViewMoveWorkToProviderInit,
     &features::kWebViewOptInToGmsBindServiceOptimization,
     &features::kWebViewPrefetchNativeLibrary,
-    &features::kWebViewQuicConnectionTimeout,
     &features::kWebViewRecordAppCacheHistograms,
     &features::kWebViewReduceUAAndroidVersionDeviceModel,
     &features::kWebViewReducedSeedExpiration,
