76b9ef273ff4e1b808bf5801bc80d5e0e4afe14b
Yuwei Huang <yuweih@chromium.org>
2025-11-19 15:56:33 -0800

    [crd host][wayland] Add Request and Session D-BUS interfaces
    These are needed for interaction with XDG portal APIs. See:
    
    * https://flatpak.github.io/xdg-desktop-portal/docs/doc-org.freedesktop.portal.Request.html
    * https://flatpak.github.io/xdg-desktop-portal/docs/doc-org.freedesktop.portal.Session.html
    
    Bug: 445973705
    Change-Id: I02951bdc63d9b5ab58224e4a176ed898323244c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7174003
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Auto-Submit: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1547521}

---
+++ remoting/host/linux/dbus_interfaces/org_freedesktop_portal_Session.h
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This file was generated from org.freedesktop.portal.Session.xml
+
+#ifndef REMOTING_HOST_LINUX_DBUS_INTERFACES_ORG_FREEDESKTOP_PORTAL_SESSION_H_
+#define REMOTING_HOST_LINUX_DBUS_INTERFACES_ORG_FREEDESKTOP_PORTAL_SESSION_H_
+
+#include "remoting/host/linux/gvariant_type.h"
+
+namespace remoting::org_freedesktop_portal_Session {
+
+// method
+struct Close {
+  static constexpr char kInterfaceName[] = "org.freedesktop.portal.Session";
+  static constexpr char kMethodName[] = "Close";
+  static constexpr gvariant::Type kInType{"()"};
+  static constexpr gvariant::Type kOutType{"()"};
+};
+
+// property
+struct version {
+  static constexpr char kInterfaceName[] = "org.freedesktop.portal.Session";
+  static constexpr char kPropertyName[] = "version";
+  static constexpr gvariant::Type kType{"u"};
+  static constexpr bool kReadable = true;
+  static constexpr bool kWritable = false;
+};
+
+// signal
+struct Closed {
+  static constexpr char kInterfaceName[] = "org.freedesktop.portal.Session";
+  static constexpr char kSignalName[] = "Closed";
+  static constexpr gvariant::Type kType{
+      "("
+      "a{sv}"  // details
+      ")"};
+};
+
+}  // namespace remoting::org_freedesktop_portal_Session
+
+#endif  // REMOTING_HOST_LINUX_DBUS_INTERFACES_ORG_FREEDESKTOP_PORTAL_SESSION_H_
