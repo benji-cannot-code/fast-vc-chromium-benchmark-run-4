f16acd1f080771530649f38b7886709c70fc0be3
Alex Rudenko <alexrudenko@chromium.org>
2025-11-19 06:11:39 -0800

    Add a feature flag for approval-mode DevTools debugging server
    Bug: 460665929
    Change-Id: I0a224259c4c572ca3a0132e98abe8166ec241437
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7155552
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1547151}

--- chrome/browser/devtools/features.h
+++ chrome/browser/devtools/features.h
@@ -124,6 +124,8 @@ BASE_DECLARE_FEATURE(kDevToolsStartingStyleDebugging);
 
 BASE_DECLARE_FEATURE(kDevToolsEnableDurableMessages);
 
+BASE_DECLARE_FEATURE(kDevToolsAcceptDebuggingConnections);
+
 BASE_DECLARE_FEATURE(kDevToolsShowPolicyDialog);
 
 BASE_DECLARE_FEATURE(kDevToolsAiAssistanceContextSelectionAgent);
