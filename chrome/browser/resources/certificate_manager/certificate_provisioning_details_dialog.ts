68003ab184703f434d9071a849f457fdf2a73a13
dpapad <dpapad@chromium.org>
2025-11-19 11:39:03 -0800

    WebUI: Enable @webui-eslint/web-component-missing-deps by default.
    Specifically
     - Enable the new ESLint check by default for non-Ash build_webui()
       targets.
     - Disable it explicitly for c/b/r/settings/ and c/b/r/new_tab_page/
       temporarily until the check is updated to correctly detect missing deps
       in WebUIs that use lazy loading (actively worked and expected to
       land in a quick follow-up CL).
    
    All violations have already been fixed in prior CLs, except a few that
    slipped and are fixed here.
    
    Bug: 457866803
    Change-Id: I8691ce7e9358500fe800d24b4ae5b005fc9685b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7171069
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Teresa Mao <temao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1547375}

--- chrome/browser/resources/certificate_manager/certificate_provisioning_details_dialog.ts
+++ chrome/browser/resources/certificate_manager/certificate_provisioning_details_dialog.ts
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview 'certificate-provisioning-details-dialog' allows the user to
  * view the details of an in-progress certiifcate provisioning process.
  */
+import '//resources/cr_elements/cr_button/cr_button.js';
 import '//resources/cr_elements/cr_expand_button/cr_expand_button.js';
 import '//resources/cr_elements/cr_dialog/cr_dialog.js';
 import '//resources/cr_elements/cr_collapse/cr_collapse.js';
