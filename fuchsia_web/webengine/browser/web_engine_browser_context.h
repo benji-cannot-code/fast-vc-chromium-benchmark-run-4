78fd6877751645819fdbd62823e1e36710c48ba7
zijiehe-google-com <zijiehe@google.com>
2025-11-19 11:04:04 -0800

    [fuchsia] Finish push messaging api implementation
    This CL largely finishes the push messaging api, there are several
    TODOs, but none of them impact the functionality.
    
    The platform notification is not a must-have in fuchsia_web / webengine,
    but it can be meaningful for debugging purpose. To try the e2e push
    messaging api behavior locally, we can inspect the web-engine stdout to
    retrieve the pushed messages from the PlatformNotificationServiceImpl.
    E.g. I use https://simple-push-demo.vercel.app to try it, and the output
    looks like,
    
    ```
    [352115.929056][26760547][26760549][web_instances:web_instances_63273b01-b398-43b3-9aa1-d2d523eb3ef0][web_engine_exe] WARN: [fuchsia_web/webengine/browser/platform_notification_service_impl.cc(25)] DisplayNotification p#https://simple-push-demo.vercel.app/#00 from https://simple-push-demo.vercel.app/ in https://simple-push-demo.vercel.app/
    [352115.929106][26760547][26760549][web_instances:web_instances_63273b01-b398-43b3-9aa1-d2d523eb3ef0][web_engine_exe] WARN: [fuchsia_web/webengine/browser/platform_notification_service_impl.cc(27)] Received Payload: Push data: 'blablabla'
    ```
    
    I'd expect this change to increase the binary size, so this CL doesn't
    enable the WEB_ENGINE_ENABLE_PUSH_MESSAGING_API. I will do it
    separately.
    
    Bug: 424479300
    Change-Id: If7206ad7d9fe435722a288d3eac6df5f2bdab25f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7047169
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Zijie He <zijiehe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1547357}

--- fuchsia_web/webengine/browser/web_engine_browser_context.h
+++ fuchsia_web/webengine/browser/web_engine_browser_context.h
@@ -13,12 +13,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/keyed_service/core/simple_factory_key.h"
 #include "components/reduce_accept_language/browser/in_memory_reduce_accept_language_service.h"
 #include "content/public/browser/browser_context.h"
+#include "fuchsia_web/webengine/browser/platform_notification_service_impl.h"
 #include "fuchsia_web/webengine/browser/push_messaging_service_impl.h"
 #include "fuchsia_web/webengine/browser/web_engine_permission_delegate.h"
 
 class WebEngineNetLogObserver;
 
 namespace network {
+class NetworkConnectionTracker;
 class NetworkQualityTracker;
 }
 
@@ -33,12 +35,14 @@ class WebEngineBrowserContext final : public content::BrowserContext {
   static std::unique_ptr<WebEngineBrowserContext> CreatePersistent(
       base::FilePath data_directory,
       network::NetworkQualityTracker* network_quality_tracker,
-      os_crypt_async::OSCryptAsync* os_crypt_async);
+      os_crypt_async::OSCryptAsync* os_crypt_async,
+      network::NetworkConnectionTracker* network_connection_tracker);
 
   // Creates a browser context with no support for persistent data.
   static std::unique_ptr<WebEngineBrowserContext> CreateIncognito(
       network::NetworkQualityTracker* network_quality_tracker,
-      os_crypt_async::OSCryptAsync* os_crypt_async);
+      os_crypt_async::OSCryptAsync* os_crypt_async,
+      network::NetworkConnectionTracker* network_connection_tracker);
 
   ~WebEngineBrowserContext() override;
 
@@ -75,7 +79,8 @@ class WebEngineBrowserContext final : public content::BrowserContext {
   explicit WebEngineBrowserContext(
       base::FilePath data_dir_path,
       network::NetworkQualityTracker* network_quality_tracker,
-      os_crypt_async::OSCryptAsync* os_crypt_async);
+      os_crypt_async::OSCryptAsync* os_crypt_async,
+      network::NetworkConnectionTracker* network_connection_tracker);
 
   const base::FilePath data_dir_path_;
 
@@ -87,6 +92,7 @@ class WebEngineBrowserContext final : public content::BrowserContext {
       reduce_accept_language_delegate_;
 #ifdef WEB_ENGINE_ENABLE_PUSH_MESSAGING_API
   PushMessagingServiceImpl push_messaging_service_;
+  PlatformNotificationServiceImpl platform_notification_service_;
 #endif
 };
 
--- fuchsia_web/webengine/browser/web_engine_browser_main_parts.h
+++ fuchsia_web/webengine/browser/web_engine_browser_main_parts.h
@@ -44,6 +44,10 @@ namespace media {
 class FuchsiaCdmManager;
 }
 
+namespace network {
+class NetworkConnectionTracker;
+}
+
 namespace inspect {
 class ComponentInspector;
 }
@@ -58,12 +62,14 @@ class FrameHostImpl final : public fuchsia::web::FrameHost {
       inspect::Node inspect_node,
       WebEngineDevToolsController* devtools_controller,
       network::NetworkQualityTracker* network_quality_tracker,
-      os_crypt_async::OSCryptAsync* os_crypt_async)
-      : context_(
-            WebEngineBrowserContext::CreateIncognito(network_quality_tracker,
-                                                     os_crypt_async),
-            std::move(inspect_node),
-            devtools_controller) {}
+      os_crypt_async::OSCryptAsync* os_crypt_async,
+      network::NetworkConnectionTracker* network_connection_tracker)
+      : context_(WebEngineBrowserContext::CreateIncognito(
+                     network_quality_tracker,
+                     os_crypt_async,
+                     network_connection_tracker),
+                 std::move(inspect_node),
+                 devtools_controller) {}
   ~FrameHostImpl() override = default;
 
   FrameHostImpl(const FrameHostImpl&) = delete;
@@ -161,6 +167,8 @@ class WEB_ENGINE_EXPORT WebEngineBrowserMainParts
       network_quality_observer_;
 
   std::unique_ptr<os_crypt_async::OSCryptAsync> os_crypt_async_;
+
+  raw_ptr<network::NetworkConnectionTracker> network_connection_tracker_;
 
   // Allows the instance to respond gracefully to explicit teardown via the
   // component framework.
