60218e417b69ae5717ce74891c857daf6593bd83
Karol Sygiet <sygiet@google.com>
2025-11-19 07:15:47 -0800

    [RSG] Apply fetched plus address as the AutofillProfile's email in ASG
    Since after the new suggestion generation logic is finished, the ASG
    won't be able to rely on the plus_address_email_override_ being set from
    the constructor, it is needed that the PlusAddress fetched by the PASG
    is applied whenever possible.
    
    This CL mimics and existing behaviour from BrowserAutofillManager in
    AddressSuggestionGenerator.
    
    Bug: 409962888
    Change-Id: I81e9d32e73febc6a7afbf24ad7fc5991113b3f10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7157685
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Cr-Commit-Position: refs/heads/main@{#1547179}

--- components/autofill/core/browser/foundations/browser_autofill_manager.cc
+++ components/autofill/core/browser/foundations/browser_autofill_manager.cc
@@ -3536,6 +3536,11 @@ void BrowserAutofillManager::InitializeSuggestionGenerators(
               delegate, IsPlusAddressesManuallyTriggered(trigger_source)));
     }
   }
+  if (relevant_filling_products.contains(FillingProduct::kAddress)) {
+    suggestion_generators_.push_back(
+        std::make_unique<AddressSuggestionGenerator>(std::nullopt,
+                                                     log_manager()));
+  }
 }
 
 }  // namespace autofill
--- components/autofill/core/browser/suggestions/addresses/address_suggestion_generator.cc
+++ components/autofill/core/browser/suggestions/addresses/address_suggestion_generator.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_deref.h"
 #include "base/containers/contains.h"
 #include "base/containers/extend.h"
+#include "base/containers/map_util.h"
 #include "base/containers/to_vector.h"
 #include "base/feature_list.h"
 #include "base/i18n/case_conversion.h"
@@ -819,6 +820,34 @@ std::vector<Suggestion> GenerateAddressOnTypingSuggestions(
   return suggestions;
 }
 
+// If `plus_address_email_override` exits it is returned. Otherwise tries to
+// find plus addresses in the `all_suggestion_data` and returns the first of
+// them. If `all_suggestion_data` doesn't contain any plus addresses, an empty
+// string is returned.
+std::string GetPlusAddressEmailOverride(
+    const std::optional<std::string>& plus_address_email_override,
+    const base::flat_map<SuggestionGenerator::SuggestionDataSource,
+                         std::vector<SuggestionGenerator::SuggestionData>>&
+        all_suggestion_data) {
+  // TODO(crbug.com/409962888): Remove this early return once the new suggestion
+  // generation logic is launched.
+  if (plus_address_email_override) {
+    return *plus_address_email_override;
+  }
+
+  const std::vector<SuggestionGenerator::SuggestionData>* plus_address_data =
+      base::FindOrNull(all_suggestion_data,
+                       SuggestionGenerator::SuggestionDataSource::kPlusAddress);
+  if (!plus_address_data || plus_address_data->empty()) {
+    plus_address_data = base::FindOrNull(
+        all_suggestion_data,
+        SuggestionGenerator::SuggestionDataSource::kPlusAddressForAddress);
+  }
+  return (plus_address_data && !plus_address_data->empty())
+             ? std::get<PlusAddress>(plus_address_data->front()).value()
+             : std::string();
+}
+
 }  // namespace
 
 std::vector<Suggestion> GetSuggestionsOnTypingForProfile(
@@ -1005,10 +1034,13 @@ void AddressSuggestionGenerator::GenerateSuggestions(
         [](SuggestionData& suggestion_data) {
           return std::get<AutofillProfile>(std::move(suggestion_data));
         });
+
     callback({FillingProduct::kAddress,
-              GenerateAddressSuggestions(form, trigger_field, form_structure,
-                                         trigger_autofill_field, client,
-                                         profiles_to_suggest)});
+              GenerateAddressSuggestions(
+                  form, trigger_field, form_structure, trigger_autofill_field,
+                  client, profiles_to_suggest,
+                  GetPlusAddressEmailOverride(plus_address_email_override_,
+                                              all_suggestion_data))});
     return;
   }
 
@@ -1112,7 +1144,8 @@ std::vector<Suggestion> AddressSuggestionGenerator::GenerateAddressSuggestions(
     const FormStructure* form_structure,
     const AutofillField* trigger_autofill_field,
     const AutofillClient& client,
-    std::vector<AutofillProfile>& profiles_to_suggest) {
+    std::vector<AutofillProfile>& profiles_to_suggest,
+    const std::string& plus_address_email_override) {
   if (!form_structure || !trigger_autofill_field ||
       !client.GetIdentityManager()) {
     return {};
@@ -1130,7 +1163,7 @@ std::vector<Suggestion> AddressSuggestionGenerator::GenerateAddressSuggestions(
           .email,
       field_types_, GetSuggestionType(trigger_field),
       trigger_autofill_field->Type().GetAddressType(), trigger_field,
-      plus_address_email_override_, client.GetAppLocale());
+      plus_address_email_override, client.GetAppLocale());
 
   // Add devtools test addresses suggestion if it exists.
   if (std::optional<Suggestion> test_addresses_suggestion =
