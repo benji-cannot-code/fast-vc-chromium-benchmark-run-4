3bd441078b83e0c88f02824274ad3809e711930a
Morten Stenshorne <mstensho@chromium.org>
2025-11-19 12:30:26 -0800

    AddOutOfFlowChildCandidate() to take LogicalStaticPosition.
    Instead of passing ALL members of LogicalStaticPosition as parameters to
    AddOutOfFlowChildCandidate(), let it take a LogicalStaticPosition
    parameter.
    
    Clean up AlignmentOffsetForOutOfFlow() parameter list too.
    
    No behavior changes intended.
    
    Change-Id: I5a2f93108668ba32fd93b4649c38a05f1e6b1b2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7172280
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1547406}

--- third_party/blink/renderer/core/layout/grid/grid_layout_utils.h
+++ third_party/blink/renderer/core/layout/grid/grid_layout_utils.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_GRID_GRID_LAYOUT_UTILS_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_GRID_GRID_LAYOUT_UTILS_H_
 
-#include "third_party/blink/renderer/core/layout/geometry/static_position.h"
 #include "third_party/blink/renderer/platform/geometry/layout_unit.h"
 #include "third_party/blink/renderer/platform/wtf/wtf_size_t.h"
 
@@ -21,8 +20,8 @@ class GridTrackList;
 enum class AxisEdge;
 struct BoxStrut;
 struct GridItemData;
-struct LogicalOffset;
 struct LogicalSize;
+struct LogicalStaticPosition;
 struct MinMaxSizesResult;
 
 // Update the provided `available_size`, `min_available_size`, and
@@ -68,9 +67,7 @@ void ComputeOutOfFlowOffsetAndSize(
 void AlignmentOffsetForOutOfFlow(AxisEdge inline_axis_edge,
                                  AxisEdge block_axis_edge,
                                  LogicalSize container_size,
-                                 LogicalStaticPosition::InlineEdge* inline_edge,
-                                 LogicalStaticPosition::BlockEdge* block_edge,
-                                 LogicalOffset* offset);
+                                 LogicalStaticPosition*);
 
 // Per the Grid spec [1] there is special logic for the contribution size to use
 // for intrinsic minimums. This method returns the contribution size of
--- third_party/blink/renderer/core/layout/mathml/math_scripts_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/mathml/math_scripts_layout_algorithm.cc
@@ -108,7 +108,8 @@ void MathScriptsLayoutAlgorithm::GatherChildren(
     if (child.IsOutOfFlowPositioned()) {
       if (container_builder) {
         container_builder->AddOutOfFlowChildCandidate(
-            block_child, BorderScrollbarPadding().StartOffset());
+            block_child,
+            LogicalStaticPosition(BorderScrollbarPadding().StartOffset()));
       }
       continue;
     }
