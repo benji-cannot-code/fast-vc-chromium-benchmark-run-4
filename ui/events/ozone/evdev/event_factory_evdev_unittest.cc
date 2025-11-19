83ca73d11c7634fac547fd475a91366b61a12ca4
Benjamin Keen <bkeen@google.com>
2025-11-18 22:47:41 -0800

    [CodeHealth] Remove stale base::Feature EnableOrdinalMotion
    Removing feature since it was introduced in M88 and has not been enabled
    by default. Also, the TODO bug (linked below) to always enable on CrOS
    has been closed as "Won't fix (Obsolete)".
    
    Bug: 356624881, b:171249701
    Change-Id: Ic88249f4ee61db54ab821ab7588d091d935d23d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7147404
    Commit-Queue: Benjamin Keen <bkeen@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1546980}

--- ui/events/ozone/evdev/event_factory_evdev_unittest.cc
+++ ui/events/ozone/evdev/event_factory_evdev_unittest.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 
 #include "base/task/single_thread_task_runner.h"
-#include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -43,7 +42,6 @@ class MockPlatformEventObserver : public PlatformEventObserver {
 class EventFactoryEvdevTest : public testing::Test {
  protected:
   EventFactoryEvdevTest() : event_factory_(nullptr, &device_manager_, nullptr) {
-    scoped_feature_list_.InitAndEnableFeature(kEnableOrdinalMotion);
     event_factory_.SetUserInputTaskRunner(
         base::SingleThreadTaskRunner::GetCurrentDefault());
     event_factory_.Init();
@@ -51,29 +49,10 @@ class EventFactoryEvdevTest : public testing::Test {
   }
 
   base::test::SingleThreadTaskEnvironment task_environment_;
-  base::test::ScopedFeatureList scoped_feature_list_;
   testing::StrictMock<MockDeviceManager> device_manager_;
   testing::NiceMock<MockPlatformEventObserver> event_observer_;
   EventFactoryEvdev event_factory_;
 };
-
-TEST_F(EventFactoryEvdevTest, OrdinalImpliesFlag) {
-  EXPECT_CALL(event_observer_, WillProcessEvent)
-      .WillOnce([](const PlatformEvent& platform_event) {
-        MouseEvent mouse_event(platform_event);
-        EXPECT_TRUE(mouse_event.flags() & EF_UNADJUSTED_MOUSE);
-        EXPECT_EQ(mouse_event.movement(), gfx::Vector2dF(2.67, 3.14));
-      });
-
-  gfx::Vector2dF ordinal_movement(2.67, 3.14);
-  event_factory_.DispatchMouseMoveEvent(
-      MouseMoveEventParams{0,
-                           0,
-                           {},
-                           &ordinal_movement,
-                           PointerDetails(EventPointerType::kMouse),
-                           EventTimeForNow()});
-}
 
 TEST_F(EventFactoryEvdevTest, NoOrdinalImpliesNoFlag) {
   EXPECT_CALL(event_observer_, WillProcessEvent)
