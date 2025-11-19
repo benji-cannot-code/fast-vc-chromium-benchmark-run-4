cb3fcd19a56273005aefda1cde4f9903ac3e7dff
Carlos Marcelo Barrera Nolasco <carlosbarrera@google.com>
2025-11-19 08:07:36 -0800

    [a11y] Update TextInputState.getSurroundingTextInternal to include the offset.
    The offset returned by `getSurroundingText` was previously hardcoded as
    -1. This update includes a more accurate calculation of this value,
    subtracting the length of the surrounding text to the left of the cursor
    from the selection's starting index (the cursor's current index).
    
    The corresponding test has also been updated to validate the offset.
    
    Bug: 419335930
    Change-Id: I21d2e0349f7fd536313d183851ca51766e614c40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7147322
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Carlos Marcelo Barrera Nolasco <carlosbarrera@google.com>
    Cr-Commit-Position: refs/heads/main@{#1547205}

--- content/public/android/java/src/org/chromium/content/browser/input/TextInputState.java
+++ content/public/android/java/src/org/chromium/content/browser/input/TextInputState.java
@@ -139,8 +139,11 @@ public class TextInputState {
                             mSelection.start() - beforeLength,
                             mSelection.end() + afterLength);
         }
+
         return new SurroundingTextInternal(
-                text, beforeLength, mSelection.end() - (mSelection.start() - beforeLength), -1);
+                text, /* selectionStart= */ beforeLength,
+                /* selectionEnd= */ beforeLength + mSelection.end() - mSelection.start(),
+                /* offset= */ mSelection.start() - beforeLength);
     }
 
     @Override
