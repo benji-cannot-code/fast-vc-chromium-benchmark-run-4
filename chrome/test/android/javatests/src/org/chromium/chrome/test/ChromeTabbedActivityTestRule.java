2594dff89bf1e78990425c2297af0f0858204167
Michael Wu <mklw@google.com>
2025-11-19 13:50:10 -0800

    Create startOnIncognitoBlankPage() so tests can start in Incognito
    This would enable tests which do not require regular windows to start by
    directly opening an Incognito window, instead of creating a Regular
    window and then an Incognito window, as is the current behavior with
    Desktop-like Incognito windows. This would reduce the overhead of
    opening extra windows and therefore speed up the test suite. Transferred
    one test to use this new method as proof of concept - more tests to
    follow later.
    
    Bug: 462156364
    Change-Id: I51b412bdd3ae5d5892131da8fea2d53613f49fe8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7158646
    Owners-Override: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Michael Wu <mklw@google.com>
    Cr-Commit-Position: refs/heads/main@{#1547455}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/ChromeTabbedActivityTestRule.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/ChromeTabbedActivityTestRule.java
@@ -11,6 +11,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.runner.lifecycle.Stage;
 
@@ -18,6 +19,7 @@ import org.junit.Assert;
 
 import org.chromium.base.ActivityState;
 import org.chromium.base.ApplicationStatus;
+import org.chromium.base.IntentUtils;
 import org.chromium.base.Log;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.ApplicationTestUtils;
@@ -25,6 +27,7 @@ import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
+import org.chromium.chrome.browser.IntentHandler;
 import org.chromium.chrome.browser.incognito.IncognitoUtils;
 import org.chromium.chrome.browser.omnibox.UrlBar;
 import org.chromium.chrome.browser.password_manager.PasswordManagerTestHelper;
@@ -97,6 +100,19 @@ public class ChromeTabbedActivityTestRule extends ChromeActivityTestRule<ChromeT
      */
     public void startMainActivityOnBlankPage() {
         startMainActivityWithURL("about:blank");
+    }
+
+    /** Starts the Main activity on a blank page in incognito mode. */
+    public void startMainActivityOnIncognitoBlankPage() {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+        intent.putExtra(IntentHandler.EXTRA_OPEN_NEW_INCOGNITO_TAB, true);
+        prepareUrlIntent(intent, "about:blank");
+        intent.putExtra(
+                android.provider.Browser.EXTRA_APPLICATION_ID,
+                ApplicationProvider.getApplicationContext().getPackageName());
+        IntentUtils.addTrustedIntentExtras(intent);
+        startMainActivityFromIntent(intent, "about:blank");
     }
 
     /**
