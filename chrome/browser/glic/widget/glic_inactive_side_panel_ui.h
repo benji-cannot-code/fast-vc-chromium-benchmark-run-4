3425a1d4eea609216012f34d3398829b9211c208
Bryant Chandler <bryantchandler@google.com>
2025-11-19 14:11:49 -0800

    [GlicMI] Make sure that the webui_contents has a compositor for screenshot
    There is a mechanism in place to ensure that screeenshots come from the
    instant that it's requested instead of at some point in the future. But
    that only works if the webcontents has a compositor. We were removing
    GlicView from the view hierarchy before requesting the screenshot, so
    it wasn't getting that mechanism. This change adjusts to request the
    screenshot before the GlicView is removed from the hierarchy.
    
    Bug: b:461827011
    Change-Id: If42f3f5aba792bc1eb1d58bc58a0c13b6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7172987
    Reviewed-by: Basia Zimirska <basiaz@google.com>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1547470}

--- chrome/browser/glic/widget/glic_inactive_side_panel_ui.h
+++ chrome/browser/glic/widget/glic_inactive_side_panel_ui.h
@@ -14,10 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/image/image_skia.h"
 #include "ui/views/view_observer.h"
 
-namespace content {
-class WebContents;
-}  // namespace content
-
 namespace views {
 class View;
 }  // namespace views
@@ -31,11 +27,9 @@ class GlicInactiveSidePanelUi : public GlicUiEmbedder,
  public:
   static std::unique_ptr<GlicInactiveSidePanelUi> CreateForVisibleTab(
       base::WeakPtr<tabs::TabInterface> tab,
-      content::WebContents* glic_webui_contents,
       GlicUiEmbedder::Delegate& delegate);
   static std::unique_ptr<GlicInactiveSidePanelUi> CreateForBackgroundTab(
       base::WeakPtr<tabs::TabInterface> tab,
-      content::WebContents* glic_webui_contents,
       GlicUiEmbedder::Delegate& delegate);
 
   ~GlicInactiveSidePanelUi() override;
