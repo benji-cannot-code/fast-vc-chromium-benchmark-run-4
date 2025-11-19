6df2d798f2de9d320aaf8ebb433ce1b56a3d208e
Joemer Ramos <joemerramos@google.com>
2025-11-19 09:13:58 -0800

    [iOS] Create a Feature Param for Ask Gemini Chip Prepopulate Floaty
    Creates a feature param within the feature flag AskGeminiChip for code
    that involves prepopulating the Gemini floaty with a prompt.
    
    Fixed: 462074071
    Change-Id: Idd6072bb2859e7743a216da526a2c525887da863
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7172021
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Auto-Submit: Joemer Ramos <joemerramos@google.com>
    Cr-Commit-Position: refs/heads/main@{#1547255}

--- ios/chrome/browser/intelligence/features/features.mm
+++ ios/chrome/browser/intelligence/features/features.mm
@@ -41,6 +41,8 @@ const char kAskGeminiChipUseSnackbar[] = "AskGeminiChipUseSnackbar";
 
 const char kAskGeminiChipIgnoreCriteria[] = "AskGeminiChipIgnoreCriteria";
 
+const char kAskGeminiChipPrepopulateFloaty[] = "AskGeminiChipPrepopulateFloaty";
+
 bool IsAskGeminiChipEnabled() {
   return base::FeatureList::IsEnabled(kAskGeminiChip);
 }
@@ -53,6 +55,11 @@ bool IsAskGeminiChipIgnoreCriteria() {
 bool IsAskGeminiSnackbarEnabled() {
   return base::GetFieldTrialParamByFeatureAsBool(
       kAskGeminiChip, kAskGeminiChipUseSnackbar, false);
+}
+
+bool IsAskGeminiChipPrepopulateFloatyEnabled() {
+  return base::GetFieldTrialParamByFeatureAsBool(
+      kAskGeminiChip, kAskGeminiChipPrepopulateFloaty, false);
 }
 
 BASE_FEATURE(kGeminiCrossTab, base::FEATURE_DISABLED_BY_DEFAULT);
