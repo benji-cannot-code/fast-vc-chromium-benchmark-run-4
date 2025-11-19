fddca785fd347efd94547a740d4b2cf946f5f5d4
EmiliaPaz <emiliapaz@chromium.org>
2025-11-19 09:30:18 -0800

    [Extensions] Remove TabStripModelObserver from menu delegate views
    Remove TabStripModelObserver from the menu platform delegate
    for views::Views by adding the following observers to the platform-
    agnostic menu model:
      (1) TabStripModelObserver::TabChangedAt ->
          WebContentsObserver::DidFinishNavigation
      (2) TabStripModelObserver::OnTabStripModelChanged ->
          TabListInterfaceObserver::OnActiveTabChanged
    
    Note: currently ExtensionActionRunner also observes
    WebContentsObserver::DidFinishNavigation, which then notifies the menu
    to update its content for each toolbar action via
    ToolbarActionsModel::Observer::OnToolbarActionUpdated. That means the
    menu is updated n(# actions) + 1 times, resulting in updating the
    page that many times. To maintain current implementation, lets have
    them on both place for now and we can reconsider later. Similarly for
    ExtensionActionViewModel updated in crrev.com/c/7082090.
    
    Bug: 449814184
    Change-Id: I0feeb78fc65815a192394575f33d0ef3940e541e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7037738
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1547272}

--- chrome/browser/ui/extensions/extensions_menu_view_platform_delegate.h
+++ chrome/browser/ui/extensions/extensions_menu_view_platform_delegate.h
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_EXTENSIONS_EXTENSIONS_MENU_VIEW_PLATFORM_DELEGATE_H_
 
 #include "chrome/browser/ui/extensions/extensions_menu_view_model.h"
+#include "chrome/browser/ui/toolbar/toolbar_actions_model.h"
+#include "extensions/common/extension_id.h"
 
 namespace content {
 class WebContents;
@@ -25,6 +27,11 @@ class ExtensionsMenuViewPlatformDelegate {
   // Detaches the delegate from a platform-agnostic menu view model. It is
   // called by the model on its destructor.
   virtual void DetachFromModel() = 0;
+
+  // Notifies the delegate that the active web contents changed to
+  // `web_contents`.
+  virtual void OnActiveWebContentsChanged(
+      content::WebContents* web_contents) = 0;
 
   // Notifies the delegate that a new host access request was added or updated
   // for `extension_id` on `web_contents`.
