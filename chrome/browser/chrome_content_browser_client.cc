44295ba2f16e8ecad34b0a0b14441496b227e855
Nikolaos Papaspyrou <nikolaos@chromium.org>
2025-11-19 07:48:00 -0800

    cpu_performance: Override the default implementation for Chrome
    This CL overrides the default calculation of CPU performance tier for
    the Chrome embedder. The branded version of Chrome uses a different
    implementation, contained in an internal repository.
    
    Also, the CL contains some minor cleanup related to
    //content/browser/cpu_performance.
    
    Bug: 449760252
    Change-Id: I35d841bbec90d4bb581c6fa5edd1ec24ccbd770b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7101020
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Nikolaos Papaspyrou <nikolaos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1547188}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -720,7 +720,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(ENABLE_REQUEST_HEADER_INTEGRITY)
 #include "chrome/common/request_header_integrity/request_header_integrity_url_loader_throttle.h"  // nogncheck crbug.com/1125897
-#endif
+#endif  // BUILDFLAG(ENABLE_REQUEST_HEADER_INTEGRITY)
+
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+#include "chrome/browser/cpu_performance_internal/cpu_performance.h"
+#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
 
 #include "base/win/windows_h_disallowed.h"
 
@@ -8941,4 +8945,14 @@ void ChromeContentBrowserClient::RecordAssistedLogin(
       break;
   }
   password_manager::metrics_util::RecordBrowserAssistedLogin(pwm_login_type);
+}
+
+blink::mojom::PerformanceTier
+ChromeContentBrowserClient::GetCpuPerformanceTier() {
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+  if (auto tier = cpu_performance::GetTier(); tier.has_value()) {
+    return *tier;
+  }
+#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
+  return ContentBrowserClient::GetCpuPerformanceTier();
 }
