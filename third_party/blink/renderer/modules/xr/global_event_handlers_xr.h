1cbdc81d1796e45d058049b9a6568b6e98095395
Suyeon Ji <zeesuyeon@gmail.com>
2025-11-18 20:49:22 -0800

    Introduce GlobalEventHandlers partial mixin for onbeforexrselect
    This patch migrates partial interfaces to an interface mixin to align
    with the specification.
    
    Originally, these partial interfaces were introduced as an workaround
    for a layering violation between core/ and modules/, since Blink could
    not include mixins across those layers.
    
    Now that the IDL compiler supports cross-component mixins[1], the
    workaround is no longer needed, and this patch replaces the partial
    interfaces with an interface mixin to follow the spec[2].
    
    This CL has no behavior changes.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/6968299
    [2] https://immersive-web.github.io/dom-overlays/#onbeforexrselect
    
    Bug: 453570181, 1109272
    Change-Id: Ie4772a96a60f6eae913d4f71e65d0532e95ebcf2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7126504
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Commit-Queue: Suyeon Ji <zeesuyeon@gmail.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1546958}

--- third_party/blink/renderer/modules/xr/global_event_handlers_xr.h
+++ third_party/blink/renderer/modules/xr/global_event_handlers_xr.h
@@ -10,10 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-// TODO(https://crbug.com/1109272): This should be changed to a partial
-// interface mixin once that's supported by the IDL generator. Currently,
-// GlobalEventHandlers is an interface mixin with five separate implementing
-// interfaces.
 class GlobalEventHandlersXR {
   STATIC_ONLY(GlobalEventHandlersXR);
 
