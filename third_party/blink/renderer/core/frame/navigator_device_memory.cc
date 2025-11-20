01e8471915b54fc840f3d0aa381115e3630d9423
Mike Taylor <miketaylr@chromium.org>
2025-11-20 05:50:52 -0800

    [IdentifiabilityStudy] Remove code for measuring device memory
    Including some leftover headers
    
    Bug: 451513878
    Change-Id: Iaa437aec7e72f913b9d21052d7bd141f06619c75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7170044
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Mike Taylor <miketaylr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1547762}

--- third_party/blink/renderer/core/frame/navigator_device_memory.cc
+++ third_party/blink/renderer/core/frame/navigator_device_memory.cc
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/navigator_device_memory.h"
 
 #include "third_party/blink/public/common/device_memory/approximated_device_memory.h"
-#include "third_party/blink/public/common/privacy_budget/identifiability_metric_builder.h"
-#include "third_party/blink/public/common/privacy_budget/identifiability_metrics.h"
 #include "third_party/blink/public/mojom/use_counter/metrics/web_feature.mojom-shared.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
