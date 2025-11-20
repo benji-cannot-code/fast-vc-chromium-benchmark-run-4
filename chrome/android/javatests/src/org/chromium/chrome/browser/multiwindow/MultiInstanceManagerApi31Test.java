39bd7dba55b8e3384ee4c3de889776454439ae0b
Henrique Nakashima <hnakashima@chromium.org>
2025-11-20 14:26:42 -0800

    [Android] Disable 3 MultiInstanceManagerApi31Test in arm64
    Bug: 454379518,431699625
    Change-Id: I0d8b24574eb80665f68232b09658646e288bd3d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7181320
    Reviewed-by: Richard Townsend <ritownsend@google.com>
    Auto-Submit: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1548073}

--- chrome/android/javatests/src/org/chromium/chrome/browser/multiwindow/MultiInstanceManagerApi31Test.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/multiwindow/MultiInstanceManagerApi31Test.java
@@ -32,6 +32,7 @@ import org.chromium.base.test.util.ApplicationTestUtils;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
@@ -192,6 +193,7 @@ public class MultiInstanceManagerApi31Test {
 
     @Test
     @SmallTest
+    @DisableIf.Build(supported_abis_includes = "arm64-v8a", message = "https://crbug.com/454379518")
     public void moveTabsToOtherWindow_singleWindowOpen() {
         ChromeTabUtils.newTabFromMenu(
                 InstrumentationRegistry.getInstrumentation(), mActivityTestRule.getActivity());
@@ -216,6 +218,7 @@ public class MultiInstanceManagerApi31Test {
 
     @Test
     @SmallTest
+    @DisableIf.Build(supported_abis_includes = "arm64-v8a", message = "https://crbug.com/454379518")
     public void moveTabGroupToOtherWindow_singleWindowOpen() {
         verifyInstanceState(/* expectedActiveInstances= */ 1, /* expectedTotalInstances= */ 1);
         TabGroupMetadata tabGroupMetadata = getTabGroupMetaData();
@@ -265,6 +268,7 @@ public class MultiInstanceManagerApi31Test {
 
     @Test
     @SmallTest
+    @DisableIf.Build(supported_abis_includes = "arm64-v8a", message = "https://crbug.com/454379518")
     public void openUrlInSelectedWindow_singleWindowOpen() {
         LoadUrlParams urlParams = new LoadUrlParams(new GURL("about:blank"));
         verifyInstanceState(/* expectedActiveInstances= */ 1, /* expectedTotalInstances= */ 1);
