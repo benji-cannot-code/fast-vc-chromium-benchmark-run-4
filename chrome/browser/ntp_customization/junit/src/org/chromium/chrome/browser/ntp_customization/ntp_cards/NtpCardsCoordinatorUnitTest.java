33aad8551882655ae81f9aa941bd2db28deb5ba3
Jenna Himawan <jhimawan@google.com>
2025-11-19 21:59:32 -0800

    Add new items to NTP cards bottom sheet
    Similar to structure of https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ntp_customization/java/res/layout/ntp_customization_feed_bottom_sheet.xml
    
    Demo of prototype: https://screencast.googleplex.com/cast/NTU2MDM1NjQ5MDQ0NDgwMHw0MTE2MGRhZC1iYw
    
    Bug: 458409311
    Change-Id: I63ed6d4d7e537454caefbb69fd9b8c7a857b3049
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7169741
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1547638}

--- chrome/browser/ntp_customization/junit/src/org/chromium/chrome/browser/ntp_customization/ntp_cards/NtpCardsCoordinatorUnitTest.java
+++ chrome/browser/ntp_customization/junit/src/org/chromium/chrome/browser/ntp_customization/ntp_cards/NtpCardsCoordinatorUnitTest.java
@@ -10,8 +10,8 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 import android.content.Context;
+import android.view.ContextThemeWrapper;
 
-import androidx.test.core.app.ApplicationProvider;
 import androidx.test.filters.SmallTest;
 
 import org.junit.Before;
@@ -22,11 +22,13 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
+import org.chromium.base.ContextUtils;
 import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.ntp_customization.BottomSheetDelegate;
+import org.chromium.chrome.browser.ntp_customization.R;
 import org.chromium.chrome.browser.profiles.Profile;
 
 /** Unit tests for {@link NtpCardsCoordinator} */
@@ -39,13 +41,16 @@ public class NtpCardsCoordinatorUnitTest {
     @Mock private Profile mProfile;
 
     private NtpCardsCoordinator mCoordinator;
+    private Context mContext;
 
     @Before
     public void setUp() {
-        Context context = ApplicationProvider.getApplicationContext();
+        mContext =
+                new ContextThemeWrapper(
+                        ContextUtils.getApplicationContext(), R.style.Theme_BrowserUI_DayNight);
         mCoordinator =
                 new NtpCardsCoordinator(
-                        context, mBottomSheetDelegate, new ObservableSupplierImpl<>(mProfile));
+                        mContext, mBottomSheetDelegate, new ObservableSupplierImpl<>(mProfile));
     }
 
     @Test
