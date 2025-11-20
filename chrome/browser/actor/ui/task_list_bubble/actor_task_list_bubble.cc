8857b47d1f8a29f0a0efc726fc893b604e428de9
Maggie Jennings <mjenn@google.com>
2025-11-19 18:42:35 -0800

    [Task Nudge] Do not activate window when TaskListBubble shows
    widget->Show() will always activate the window where the widget is
    shown, which is not the desired behavior (see description
    ofb/453719104). ShowInactive() will show the bubble, without activating
    the window.
    
    Before: https://screencast.googleplex.com/cast/NjEyNjE5NTgzMjc4Mjg0OHwwMGUwYzhiYy00Yw
    
    After: https://screencast.googleplex.com/cast/NjQwMjMwNDExMzYzOTQyNHxjZDkxYjFkMy1mMg
    
    Bug: 462140399
    Change-Id: Id54ac3aebb99b644a13041a88ffa2964d0d7071b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7172950
    Commit-Queue: Maggie Jennings <mjenn@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1547592}

--- chrome/browser/actor/ui/task_list_bubble/actor_task_list_bubble.cc
+++ chrome/browser/actor/ui/task_list_bubble/actor_task_list_bubble.cc
@@ -48,7 +48,8 @@ views::Widget* ActorTaskListBubble::ShowBubble(
   bubble->set_margins(gfx::Insets::VH(kVerticalMargin, 0));
 
   auto* widget = views::BubbleDialogDelegate::CreateBubble(std::move(bubble));
-  widget->Show();
+  // The bubble should never activate the window it's shown in.
+  widget->ShowInactive();
 
   return widget;
 }
