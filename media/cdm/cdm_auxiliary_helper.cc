6eb1fc03ea72952c010a15c89fb4c3966ca652a8
Feras Aldahlawi <frs@chromium.org>
2025-11-20 12:42:14 -0800

    Fix build failure by adding out-of-line constructor
    The build was failing with a chromium-style error: `Complex
    class/struct needs an explicit out-of-line constructor`. This was
    because the `CdmMetricsData` struct contained `std::optional`
    members, which makes it a complex struct.
    
    This commit fixes the build by defining the constructor and
    destructor for `CdmMetricsData` out-of-line in the `.cc` file.
    
    Change-Id: I07fdeaf45cae6d2f26579146e005955a6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7173070
    Reviewed-by: Vikram Pasupathy <vpasupathy@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Feras Aldahlawi <frs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1548015}

--- media/cdm/cdm_auxiliary_helper.cc
+++ media/cdm/cdm_auxiliary_helper.cc
@@ -17,6 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
+CdmMetricsData::CdmMetricsData() = default;
+CdmMetricsData::~CdmMetricsData() = default;
+
 CdmAuxiliaryHelper::CdmAuxiliaryHelper() = default;
 CdmAuxiliaryHelper::~CdmAuxiliaryHelper() = default;
 
