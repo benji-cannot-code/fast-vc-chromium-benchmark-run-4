3827329dac938a4c0b551b0933d64bf4902d4aaf
Cheick Cisse <cheickcisse@google.com>
2025-11-20 10:25:55 -0800

    [iOS] Adjust What's New screenshot top offset for iOS 26
    This CL updates the top anchor constraint for the screenshot view wrapper
    in WhatsNewScreenshotViewController.
    
    Previously, the top offset was hardcoded to 29pt. On iOS 26, layout
    changes require a larger offset of 45pt. This change extracts these
    values into constants and introduces a helper method,
    `screenshotViewTopOffset`, to apply the correct spacing.
    
    Screenshot:
    https://screenshot.googleplex.com/9WZGGHD9rcby6Zb.png
    https://screenshot.googleplex.com/5UpYMfm3HeqdvDy.png
    
    Bug: 427953436
    Change-Id: If03fdd042a465d0ea7bb505350fd52d8c83ae872
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7172028
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1547933}

--- ios/chrome/browser/whats_new/ui/whats_new_screenshot_view_controller.mm
+++ ios/chrome/browser/whats_new/ui/whats_new_screenshot_view_controller.mm
@@ -23,6 +23,8 @@ namespace {
 constexpr CGFloat kSpacingBeforeImageIfNoNavigationBar = 24;
 constexpr CGFloat kLabelBottomMargin = -40;
 constexpr CGFloat kLabelFontSize = 15;
+constexpr CGFloat kScreenshotViewTopOffset = 29;
+constexpr CGFloat kScreenshotViewTopOffsetiOS26 = 45;
 NSString* const kDarkModeAnimationSuffix = @"_darkmode";
 }  // namespace
 
@@ -224,7 +226,7 @@ NSString* const kDarkModeAnimationSuffix = @"_darkmode";
         constraintEqualToAnchor:self.view.trailingAnchor],
     [self.screenshotViewWrapper.animationView.topAnchor
         constraintEqualToAnchor:self.view.topAnchor
-                       constant:29],
+                       constant:[self screenshotViewTopOffset]],
     [self.screenshotViewWrapper.animationView.bottomAnchor
         constraintEqualToAnchor:self.view.centerYAnchor],
   ]];
@@ -274,6 +276,14 @@ NSString* const kDarkModeAnimationSuffix = @"_darkmode";
   } else {
     [self toggleDarkModeOnTraitChange];
   }
+}
+
+// Returns the top offset for the screenshot view.
+- (CGFloat)screenshotViewTopOffset {
+  if (@available(iOS 26, *)) {
+    return kScreenshotViewTopOffsetiOS26;
+  }
+  return kScreenshotViewTopOffset;
 }
 
 @end
