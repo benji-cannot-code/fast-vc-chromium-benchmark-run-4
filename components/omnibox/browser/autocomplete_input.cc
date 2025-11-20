d5d291ef48c60709b044d722a6e05ae8ff03136d
manukh <manukh@chromium.org>
2025-11-20 13:50:35 -0800

    [omnibox][keyword] Rm unused `AdjustInputForStarterPackEngines()`
    The method `AutocompleteInput::AdjustInputForStarterPackEngines()` was
    unused.
    
    The other keyword related methods in Autocomplete/Omnibox code are:
    
    OmniboxEditModel
      MaybeStripKeyword
      MaybePrependKeyword
      AllowKeywordSpaceTriggering
      OnAfterPossibleChange
      MaybeAcceptKeywordBySpace
      CreatedKeywordSearchByInsertingSpaceInMiddle
      IsSpaceCharForAcceptingKeyword
    
    KeywordProvider
      GetKeywordForText
    
    AutocompleteInput
      AdjustInputForStarterPackEngines (unused)
      GetSubstitutingTemplateURLForInput
      ExtractKeywordFromInput
      SplitReplacementStringFromInput
      CleanUserInputKeyword
      SplitKeywordFromInput
    
    AutocompleteController
      UpdateKeywordDescriptions
      UpdateAssociatedKeywords
      MaybeCleanSuggestionsForKeywordMode
    
    Bug: b:462477113
    Change-Id: Idad50147e5a165592973459ce974b9943c741f3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7180332
    Auto-Submit: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Ananya Seelam <ananyaseelam@google.com>
    Cr-Commit-Position: refs/heads/main@{#1548055}

--- components/omnibox/browser/autocomplete_input.cc
+++ components/omnibox/browser/autocomplete_input.cc
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-
 #include "components/omnibox/browser/autocomplete_input.h"
 
 #include <string_view>
@@ -746,30 +745,6 @@ AutocompleteInput::GetFeaturedKeywordMode(std::u16string_view text) {
   if (text.starts_with(u'@'))
     return FeaturedKeywordMode::kPrefix;
   return FeaturedKeywordMode::kFalse;
-}
-
-// static
-const TemplateURL* AutocompleteInput::AdjustInputForStarterPackEngines(
-    TemplateURLService* model,
-    AutocompleteInput* input) {
-  DCHECK(model);
-
-  // If not in keyword mode, then `input` is definitely not in a starter pack
-  // scope, so early exit.
-  if (!input->prefer_keyword()) {
-    return nullptr;
-  }
-
-  // If in a starter pack scope, should run the provider with only
-  // the user text AFTER the keyword.  E.g. if the input is "@history text",
-  // set the autocomplete input to just "text".
-  const TemplateURL* template_url =
-      AutocompleteInput::GetSubstitutingTemplateURLForInput(model, input);
-  if (template_url && template_url->starter_pack_id() > 0) {
-    return template_url;
-  }
-
-  return nullptr;
 }
 
 // static
