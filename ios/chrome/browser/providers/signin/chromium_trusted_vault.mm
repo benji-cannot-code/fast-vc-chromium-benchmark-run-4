d98f31b8a94a3f8d876b7e7344645976fe698e4f
Thomas Thrainer <thomasth@google.com>
2025-11-20 08:09:18 -0800

    iOS plumbing for the `Sync.TrustedVaultAddKeysSuccessfully` metric (3)
    This is the third CL ensuring that the correct
    `TrustedVaultUserActionTriggerForUMA` value is passed to the
    `TrustedVaultClient` observers during user initiated key retrievals.
    
    It cleans up external interfaces once the internal code was adapted to
    the changes introduced in https://crrev.com/c/7146402.
    
    This will ensure that the `Sync.TrustedVaultAddKeysSuccessfully` metric
    can be emitted with the correct trigger value (see
    https://crrev.com/c/7130940).
    
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-noncq
    Bug: 458664467
    Change-Id: Id6ef91ff94b5c62c78ce6df44b640def6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7146188
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Thomas Thrainer <thomasth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1547842}

--- ios/chrome/browser/providers/signin/chromium_trusted_vault.mm
+++ ios/chrome/browser/providers/signin/chromium_trusted_vault.mm
@@ -31,6 +31,7 @@ class ChromiumTrustedVaultClientBackend final
   CancelDialogCallback Reauthentication(
       id<SystemIdentity> identity,
       trusted_vault::SecurityDomainId security_domain_id,
+      trusted_vault::TrustedVaultUserActionTriggerForUMA trigger,
       UIViewController* presenting_view_controller,
       CompletionBlock completion) final;
   CancelDialogCallback FixDegradedRecoverability(
@@ -76,6 +77,7 @@ TrustedVaultClientBackend::CancelDialogCallback
 ChromiumTrustedVaultClientBackend::Reauthentication(
     id<SystemIdentity> identity,
     trusted_vault::SecurityDomainId security_domain_id,
+    trusted_vault::TrustedVaultUserActionTriggerForUMA trigger,
     UIViewController* presenting_view_controller,
     CompletionBlock completion) {
   NOTREACHED();
--- ios/chrome/browser/settings/ui_bundled/password/password_settings/password_settings_mediator_unittest.mm
+++ ios/chrome/browser/settings/ui_bundled/password/password_settings/password_settings_mediator_unittest.mm
@@ -78,6 +78,7 @@ class MockTrustedVaultClientBackend : public TrustedVaultClientBackend {
               Reauthentication,
               (id<SystemIdentity> identity,
                trusted_vault::SecurityDomainId security_domain_id,
+               trusted_vault::TrustedVaultUserActionTriggerForUMA trigger,
                UIViewController* presenting_view_controller,
                CompletionBlock completion),
               (override));
