1dfd96b89bb110ff26812e337f7992a255dbb65b
Colin Blundell <blundell@chromium.org>
2025-11-20 00:06:37 -0800

    [Blink] Call DoExternalDrawAndSnapshot() in video_frame_image_util.cc
    Precursor to doing further refactoring of the CRPBitmap implementation
    of this method.
    
    Bug: 352263194
    Change-Id: If7ae513549f15fb545bfef779ffb8f580498becc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7172398
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1547661}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
@@ -209,6 +209,9 @@ class PLATFORM_EXPORT CanvasResourceProvider
   virtual scoped_refptr<CanvasResource> ProduceCanvasResource(FlushReason) = 0;
   virtual scoped_refptr<StaticBitmapImage> Snapshot(
       ImageOrientation = ImageOrientationEnum::kDefault) = 0;
+  virtual scoped_refptr<StaticBitmapImage> DoExternalDrawAndSnapshot(
+      base::FunctionRef<void(MemoryManagedPaintCanvas&)> draw_callback,
+      ImageOrientation orientation = ImageOrientationEnum::kDefault) = 0;
 
   void SetDelegate(Delegate* delegate) { delegate_ = delegate; }
 
@@ -426,7 +429,7 @@ class PLATFORM_EXPORT CanvasResourceProviderBitmap
       ImageOrientation = ImageOrientationEnum::kDefault) override;
   scoped_refptr<StaticBitmapImage> DoExternalDrawAndSnapshot(
       base::FunctionRef<void(MemoryManagedPaintCanvas&)> draw_callback,
-      ImageOrientation orientation = ImageOrientationEnum::kDefault);
+      ImageOrientation orientation = ImageOrientationEnum::kDefault) override;
 
   void RasterRecord(cc::PaintRecord last_recording) override;
   bool WritePixels(const SkImageInfo& orig_info,
@@ -534,6 +537,9 @@ class PLATFORM_EXPORT CanvasResourceProviderSharedImage
   bool IsSoftwareSharedImageGpuChannelLost() const final;
   void ExternalCanvasDrawHelper(
       base::FunctionRef<void(MemoryManagedPaintCanvas&)> draw_callback) final;
+  scoped_refptr<StaticBitmapImage> DoExternalDrawAndSnapshot(
+      base::FunctionRef<void(MemoryManagedPaintCanvas&)> draw_callback,
+      ImageOrientation orientation = ImageOrientationEnum::kDefault) final;
   void RasterRecord(cc::PaintRecord last_recording) override;
   sk_sp<SkSurface> CreateSkSurface() const override;
   void OnFlushForImage(cc::PaintImage::ContentId content_id) final;
