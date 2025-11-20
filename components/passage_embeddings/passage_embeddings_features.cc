35b5f2374a519291653e686b4dab3cbc18b11a95
Sophie Chang <sophiechang@chromium.org>
2025-11-19 22:12:00 -0800

    Enable passage embeddings by default on desktop
    Change-Id: I33eea3e0eb6c38cda6011d99200f57784357eea1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7173072
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1547639}

--- components/passage_embeddings/passage_embeddings_features.cc
+++ components/passage_embeddings/passage_embeddings_features.cc
@@ -11,7 +11,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace passage_embeddings {
 
-BASE_FEATURE(kPassageEmbedder, base::FEATURE_DISABLED_BY_DEFAULT);
+namespace {
+
+constexpr auto enabled_by_default_desktop_only =
+#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_IOS)
+    base::FEATURE_DISABLED_BY_DEFAULT;
+#else
+    base::FEATURE_ENABLED_BY_DEFAULT;
+#endif
+
+}  // namespace
+
+BASE_FEATURE(kPassageEmbedder, enabled_by_default_desktop_only);
 
 const base::FeatureParam<int> kUserInitiatedPriorityNumThreads(
     &kPassageEmbedder,
