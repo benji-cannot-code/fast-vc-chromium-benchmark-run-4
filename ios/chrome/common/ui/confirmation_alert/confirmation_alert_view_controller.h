721ddd71b0bada52a19ec899ba6aade0ec41ce54
Ewann Pellé <ewannpv@chromium.org>
2025-11-20 08:23:00 -0800

    [iOS] Move height detent logic to ButtonStackViewController
    Moves the dynamic height detent calculation from
    ConfirmationAlertViewController to its superclass,
    ButtonStackViewController, to allow reuse in other subclasses like
    PromoStyleViewController.
    
    Screenshots:
    https://drive.google.com/drive/folders/1vsF66wEDu5BykqF-cIutUSrgqpVMWQlX?resourcekey=0-vOg4Gg7rtwH_cwsD5r5A0w&usp=sharing
    
    Bug: 444648926
    Change-Id: I313ccff4e4fd751e276688865cc06417d9a9137d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7172494
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1547852}

--- ios/chrome/common/ui/confirmation_alert/confirmation_alert_view_controller.h
+++ ios/chrome/common/ui/confirmation_alert/confirmation_alert_view_controller.h
@@ -159,15 +159,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // nothing.
 - (void)customizeSubtitle:(UITextView*)subtitle;
 
-// Detent that attempts to fit the preferred height of the content. Detent may
-// be inactive in some size classes, so it should be used together with at
-// least one other detent.
-- (UISheetPresentationControllerDetent*)
-    preferredHeightDetent API_AVAILABLE(ios(16));
-
-// Calculates the preferred height of the content.
-- (CGFloat)preferredHeightForContent;
-
 @end
 
 #endif  // IOS_CHROME_COMMON_UI_CONFIRMATION_ALERT_CONFIRMATION_ALERT_VIEW_CONTROLLER_H_
