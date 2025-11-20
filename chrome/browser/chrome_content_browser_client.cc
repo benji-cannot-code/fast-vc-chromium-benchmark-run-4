e0afef66616c917beff4fb6128f75ff60ddda1f6
krishna dheeraj Pannala <kpannala@microsoft.com>
2025-11-20 07:27:24 -0800

    Rename kWindowsSystemProxyResolver to kProxyResolver
    This CL renames the sandbox enum kWindowsSystemProxyResolver to
    kProxyResolver to prepare for potential multi-platform support
    while keeping the Windows-only [EnableIf=is_win] guards in place.
    
    This is a pure refactoring change with no functional modifications.
    The sandbox remains Windows-only at this time.
    
    Bug: 442313607
    Change-Id: I14a9d29e679588e9a002d3631065741b423e7cfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7118079
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: suresh potti <sureshpotti@microsoft.com>
    Commit-Queue: krishna dheeraj Pannala <kpannala@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1547820}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -5131,7 +5131,7 @@ std::wstring ChromeContentBrowserClient::GetAppContainerSidForSandboxType(
     case sandbox::mojom::Sandbox::kServiceWithJit:
     case sandbox::mojom::Sandbox::kIconReader:
     case sandbox::mojom::Sandbox::kMediaFoundationCdm:
-    case sandbox::mojom::Sandbox::kWindowsSystemProxyResolver:
+    case sandbox::mojom::Sandbox::kProxyResolver:
       // Should never reach here.
       NOTREACHED();
   }
@@ -5228,7 +5228,7 @@ bool ChromeContentBrowserClient::PreSpawnChild(
     case sandbox::mojom::Sandbox::kService:
     case sandbox::mojom::Sandbox::kIconReader:
     case sandbox::mojom::Sandbox::kMediaFoundationCdm:
-    case sandbox::mojom::Sandbox::kWindowsSystemProxyResolver:
+    case sandbox::mojom::Sandbox::kProxyResolver:
       break;
   }
 
