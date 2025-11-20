32ad8c29094be306a9e58100fa6b30469a529365
Abdelrahman Eed <abdoeed@google.com>
2025-11-20 07:56:35 -0800

    Add unit test for Pointer Lock on Android.
    This change introduces a unit test for RenderWidgetHostViewAndroid to verify the functionality of LockPointer, ChangePointerLock, and UnlockPointer when the kPointerLockOnAndroid feature is enabled. The TestViewAndroidDelegate is updated to create a focusable FrameLayout, which is necessary for pointer lock operations to succeed.
    
    Bug: b:462351889
    Change-Id: Ia7cfb4ee4a04c3dfce24c1612e9a725332d00b3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7169011
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Abdelrahman Eed <abdoeed@google.com>
    Cr-Commit-Position: refs/heads/main@{#1547832}

--- ui/android/javatests/src/org/chromium/ui/test/util/TestViewAndroidDelegate.java
+++ ui/android/javatests/src/org/chromium/ui/test/util/TestViewAndroidDelegate.java
@@ -6,10 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.ui.test.util;
 
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import org.jni_zero.CalledByNative;
 import org.jni_zero.JNINamespace;
 
+import org.chromium.base.ContextUtils;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.ui.base.ViewAndroidDelegate;
 
@@ -36,7 +38,10 @@ public class TestViewAndroidDelegate extends ViewAndroidDelegate {
      */
     @CalledByNative
     private static TestViewAndroidDelegate create() {
-        return new TestViewAndroidDelegate(/* containerView= */ null);
+        FrameLayout layout = new FrameLayout(ContextUtils.getApplicationContext());
+        layout.setFocusable(true);
+        layout.setFocusableInTouchMode(true);
+        return new TestViewAndroidDelegate(layout);
     }
 
     /**
