d6a5435d39db8a006ea644f44dbe039ebb818ecb
Marijn Kruisselbrink <mek@chromium.org>
2025-11-20 10:42:58 -0800

    [PWAs] Fix Navigation Capturing for projector SWA
    The NavigationCapturingProcess code does not support capturing
    into a SWA (and will DCHECK in various places if it tries to do so),
    so make sure that (the Chrome OS implementation of)
    NavigationCapturingSettings filters out any SWAs. This has the
    main effect of no longer (trying to) capture screencast.apps.chrome
    URLs into the projector SWA.
    Instead the existing ChromeOsReimplNavigationCapturingThrottle logic
    (which generally correctly handles these redirects) is fixed to also
    handle capturing in this target=_blank case.
    
    Finally this CL also adds the missing target=_blank case to the
    ProjectorNavigationCapturingParameterizedTest test, to verify that
    all this works.
    
    Bug: 461802078
    Change-Id: Iff059cda509c376a65239719450101965ce24e0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7169449
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Benjamin Zielinski <bzielinski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1547944}

--- chrome/browser/apps/link_capturing/navigation_capturing_settings_cros_impl.cc
+++ chrome/browser/apps/link_capturing/navigation_capturing_settings_cros_impl.cc
@@ -15,6 +15,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace web_app {
 
+namespace {
+bool IsSystemWebApp(Profile* profile, const std::string& app_id) {
+  bool is_system_web_app = false;
+  apps::AppServiceProxyFactory::GetForProfile(profile)
+      ->AppRegistryCache()
+      .ForOneApp(app_id, [&is_system_web_app](const apps::AppUpdate& update) {
+        if (update.InstallReason() == apps::InstallReason::kSystem) {
+          is_system_web_app = true;
+        }
+      });
+  return is_system_web_app;
+}
+}  // namespace
+
 std::unique_ptr<NavigationCapturingSettings>
 NavigationCapturingSettings::Create(Profile& profile) {
   return std::make_unique<NavigationCapturingSettingsCrosImpl>(profile);
@@ -47,7 +61,8 @@ NavigationCapturingSettingsCrosImpl::GetCapturingWebAppForUrl(const GURL& url) {
   auto* proxy = apps::AppServiceProxyFactory::GetForProfile(&profile_.get());
   auto app_id = apps::FindAppIdsToLaunchForUrl(proxy, url).preferred;
   if (!app_id.has_value() ||
-      proxy->AppRegistryCache().GetAppType(*app_id) != apps::AppType::kWeb) {
+      proxy->AppRegistryCache().GetAppType(*app_id) != apps::AppType::kWeb ||
+      IsSystemWebApp(&profile_.get(), *app_id)) {
     return std::nullopt;
   }
   return app_id;
