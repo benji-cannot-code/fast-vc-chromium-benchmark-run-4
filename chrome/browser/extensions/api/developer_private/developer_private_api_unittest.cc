d5516dbddab4c932a74edcba2eb452190025fa75
Kay Lin <kaiyilin@google.com>
2025-11-20 00:03:47 -0800

    Extension: Enable tests on Android Desktop
    Enable android desktop tests below
    - ReloadBadExtensionToLoadUnpackedRetry
    - LoadUnpackedFailsWithoutDevMode
    - LoadUnpackedFailsWithBlocklistingPolicy
    
    Test DeveloperPrivateNotifyDragInstallInProgress remains disabled as
    file information cannot be retrieved from drag action on Android.
    
    Bug: 439448250
    Change-Id: Ic17bf1bdff0715ab236f1517a7ec841879c64194
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7168223
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Commit-Queue: Kay Lin <kaiyilin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1547660}

--- chrome/browser/extensions/api/developer_private/developer_private_api_unittest.cc
+++ chrome/browser/extensions/api/developer_private/developer_private_api_unittest.cc
@@ -1302,8 +1302,6 @@ TEST_F(DeveloperPrivateApiUnitTest, LoadUnpackedRetryId) {
   }
 }
 
-// TODO(crbug.com/439448250): Enable on desktop android.
-#if BUILDFLAG(ENABLE_EXTENSIONS)
 // Tests calling "reload" on an unpacked extension with a manifest error,
 // resulting in the reload failing. The reload call should then respond with
 // the load error, which includes a retry GUID to be passed to loadUnpacked().
@@ -1430,6 +1428,11 @@ TEST_F(DeveloperPrivateApiUnitTest, ReloadBadExtensionToLoadUnpackedRetry) {
   }
 }
 
+// On Android, file information cannot be directly accessed from the **drag
+// event**. We must instead use the **drop event** to retrieve the file data.
+// See {@link DeveloperPrivateNotifyDragInstallInProgressFunction} for detailed
+// information.
+#if !BUILDFLAG(IS_ANDROID)
 TEST_F(DeveloperPrivateApiUnitTest,
        DeveloperPrivateNotifyDragInstallInProgress) {
   std::unique_ptr<content::WebContents> web_contents(
@@ -1518,7 +1521,7 @@ TEST_F(DeveloperPrivateApiUnitTest,
   api::DeveloperPrivateNotifyDragInstallInProgressFunction::
       SetDropFileForTesting(nullptr);
 }
-#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
+#endif
 
 // Test developerPrivate.requestFileSource.
 TEST_F(DeveloperPrivateApiUnitTest, DeveloperPrivateRequestFileSource) {
@@ -1747,8 +1750,6 @@ TEST_F(DeveloperPrivateApiUnitTest, DeveloperPrivateDevMode) {
   }
 }
 
-// TODO(crbug.com/439448250): Enable on desktop android.
-#if BUILDFLAG(ENABLE_EXTENSIONS)
 TEST_F(DeveloperPrivateApiUnitTest, LoadUnpackedFailsWithoutDevMode) {
   std::unique_ptr<content::WebContents> web_contents(
       content::WebContentsTester::CreateTestWebContents(profile(), nullptr));
@@ -1792,7 +1793,6 @@ TEST_F(DeveloperPrivateApiUnitTest, LoadUnpackedFailsWithBlocklistingPolicy) {
       function.get(), "[]", profile());
   EXPECT_THAT(error, testing::HasSubstr("policy"));
 }
-#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
 TEST_F(DeveloperPrivateApiUnitTest,
        LoadUnpackedWorksWithBlocklistingPolicyAlongAllowlistingPolicy) {
