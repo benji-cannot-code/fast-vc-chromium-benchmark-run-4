b5942cdaf486759e07941a26c55ce79dc15aa4ad
Shakti Sahu <shaktisahu@chromium.org>
2025-11-20 13:07:23 -0800

    [contextual_tasks] Separate BUILD target for glow logic
    This CL makes separate BUILD targets for border view and tab
    underline views without any dependency on glic targets. Only
    exception is the dependency on glic resources which is acceptable
    for now. The reason for independent build targets is to avoid
    accidental glic logic inside generic view classes which will be
    incorrect for contextual_tasks.
    
    Only the ControllerImpl classes can have dependency on glic and
    contextual_tasks both.
    
    Bug: 456589738
    Change-Id: I854ccde25b51147333ab7b94270e5502930d87a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7174305
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1548028}

--- chrome/browser/glic/browser_ui/context_sharing_border_view.cc
+++ chrome/browser/glic/browser_ui/context_sharing_border_view.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <math.h>
 
 #include "base/debug/crash_logging.h"
-#include "chrome/browser/actor/ui/actor_border_view_controller.h"
 #include "chrome/browser/glic/browser_ui/context_sharing_border_view_controller.h"
 #include "chrome/browser/themes/theme_service.h"
 #include "chrome/browser/ui/browser.h"
