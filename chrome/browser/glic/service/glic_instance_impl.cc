2c0b90ec14a3465a540fdea8bed7cee1f09f667c
Bryant Chandler <bryantchandler@google.com>
2025-11-20 15:26:32 -0800

    [GlicMI] Delete GlicInstanceImpl when last embedder unbinds
    Centralize the logic to remove the GlicInstanceImpl when the last
    embedder is unbound inside UnbindEmbedder. This simplifies call sites
    like OnBoundTabDestroyed and ensures the instance is always cleaned up
    correctly.
    
    Bug: b:462222213
    Change-Id: I61320f88fcc934e87f077280bddb1cbe6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7179280
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1548109}

--- chrome/browser/glic/service/glic_instance_impl.cc
+++ chrome/browser/glic/service/glic_instance_impl.cc
@@ -555,6 +555,12 @@ void GlicInstanceImpl::UnbindEmbedder(EmbedderKey key) {
   // Avoid dangling raw_ptr.
   host_.SetDelegate(&empty_embedder_delegate_);
   embedders_.erase(key);
+
+  // Remove the instance if all embedders are gone.
+  if (embedders_.empty() && coordinator_delegate_) {
+    // This call will delete `this`.
+    coordinator_delegate_->RemoveInstance(this);
+  }
 }
 
 Host& GlicInstanceImpl::host() {
@@ -776,11 +782,8 @@ void GlicInstanceImpl::MaybeShowHostUi(GlicUiEmbedder* embedder) {
 void GlicInstanceImpl::OnBoundTabDestroyed(tabs::TabInterface* tab,
                                            const InstanceId& instance_id) {
   instance_metrics_.OnBoundTabDestroyed();
+  // This call may delete `this`.
   UnbindEmbedder(tab);
-  if (embedders_.empty() && coordinator_delegate_) {
-    // This call will delete `this`.
-    coordinator_delegate_->RemoveInstance(this);
-  }
 }
 
 void GlicInstanceImpl::OnBoundTabActivated(tabs::TabInterface* tab) {
@@ -989,11 +992,8 @@ void GlicInstanceImpl::MaybeRemoveBlankInstanceOnClose() {
     return;
   }
 
+  // This call will delete `this`.
   UnbindEmbedder(*tab);
-  if (coordinator_delegate_) {
-    // This call will delete `this`.
-    coordinator_delegate_->RemoveInstance(this);
-  }
 }
 
 void GlicInstanceImpl::NotifyInstanceActivationChanged(bool is_active) {
