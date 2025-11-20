bf927e9b9ddb5447a8bd77b697e4419c86e196c5
Bruno Braga <brunobraga@google.com>
2025-11-20 03:37:00 -0800

    [AutofillAi - M4] Create EntityDataManager.java to forward calls to CC
    This method is similar to PersonalDataManager.java and will be used by
    settings to communicate with EDM in the c++ side.
    
    For now I added a simple method and all the boilerplate, methods to
    retrieve, update and add entities will be added in a follow up.
    
    Bug: 411324196
    Change-Id: Ieeefcd776ae8612703d05836418f006465d7a9fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7172417
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1547727}

---
+++ chrome/browser/autofill/android/java/src/org/chromium/chrome/browser/autofill/autofill_ai/EntityDataManagerFactory.java
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.autofill.autofill_ai;
+
+import org.chromium.base.ResettersForTesting;
+import org.chromium.base.ThreadUtils;
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
+import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.chrome.browser.profiles.ProfileKeyedMap;
+
+/** Provides access to {@link EntityDataManager}s for a given {@link Profile}. */
+@NullMarked
+public class EntityDataManagerFactory {
+    private static final ProfileKeyedMap<EntityDataManager> sProfileMap =
+            ProfileKeyedMap.createMapOfDestroyables(
+                    ProfileKeyedMap.ProfileSelection.REDIRECTED_TO_ORIGINAL);
+    private static @Nullable EntityDataManager sManagerForTesting;
+
+    /** Return the {@link EntityDataManager} associated with the passed in {@link Profile}. */
+    public static EntityDataManager getForProfile(Profile profile) {
+        if (sManagerForTesting != null) return sManagerForTesting;
+        ThreadUtils.assertOnUiThread();
+        return sProfileMap.getForProfile(profile, EntityDataManager::new);
+    }
+
+    public static void setInstanceForTesting(EntityDataManager manager) {
+        var oldValue = sManagerForTesting;
+        sManagerForTesting = manager;
+        ResettersForTesting.register(() -> sManagerForTesting = oldValue);
+    }
+}
