06f6fa9924861c18432fccd40128d8c2f17c1c1d
Lei Zhang <thestig@chromium.org>
2025-11-19 23:01:55 -0800

    Remove PluginService::GetPluginsAsync() use in DownloadTargetDeterminer
    Since PluginService::GetPlugins() does not block and easier to use,
    switch from GetPluginsAsync() to GetPlugins() in
    DownloadTargetDeterminer. Then DetermineIfHandledSafelyHelper() can be
    removed as well and all of its callers can switch to
    DetermineIfHandledSafelyHelperSynchronous(). With that change, the
    `STATE_DETERMINE_IF_HANDLED_SAFELY_BY_BROWSER` state and associated code
    can be removed as well.
    
    With the async DetermineIfHandledSafelyHelper() gone, let
    DetermineIfHandledSafelyHelperSynchronous() take that name.
    
    Bug: 459520485
    Change-Id: Ie1e187f1074a3c9cb44b72d28efe167075bf2929
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7159087
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1547648}

--- chrome/browser/download/download_target_determiner_unittest.cc
+++ chrome/browser/download/download_target_determiner_unittest.cc
@@ -1126,8 +1126,7 @@ TEST_F(DownloadTargetDeterminerTest, DefaultVirtual) {
 }
 
 #if BUILDFLAG(ENABLE_PLUGINS)
-TEST_F(DownloadTargetDeterminerTest,
-       DetermineIfHandledSafelyHelperSynchronous) {
+TEST_F(DownloadTargetDeterminerTest, DetermineIfHandledSafelyHelper) {
   const char16_t kPluginName[] = u"PDF";
   const char kPdfMimeType[] = "application/pdf";
   const char kPdfFileType[] = "pdf";
@@ -1152,9 +1151,8 @@ TEST_F(DownloadTargetDeterminerTest,
   std::unique_ptr<download::MockDownloadItem> item =
       CreateActiveDownloadItem(1, download_test_case);
 
-  EXPECT_TRUE(
-      DownloadTargetDeterminer::DetermineIfHandledSafelyHelperSynchronous(
-          item.get(), base::FilePath(), kPdfMimeType));
+  EXPECT_TRUE(DownloadTargetDeterminer::DetermineIfHandledSafelyHelper(
+      item.get(), base::FilePath(), kPdfMimeType));
 }
 #endif
 
