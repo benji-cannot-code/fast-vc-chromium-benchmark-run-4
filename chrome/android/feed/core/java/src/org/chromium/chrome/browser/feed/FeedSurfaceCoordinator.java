73dfa8253afcc6da9b82c7c1ddfce5ce244756dd
Xi Han <hanxi@google.com>
2025-11-19 17:09:55 -0800

    [Theme] Add gradient overlay for toolbar on NTP.
    In this CL:
    1. Add a NtpBackgroundImageLayout. It contains the existing
       NtpBackgroundImageView and a gradient overlay view at top.
    2. The gradient overlay view starts from color black with alpha 30 at
       the top, and black with alpha 10 in the middle, and transparent at
       the bottom. The total height is 120dp.
    3. Introduce NtpBackgroundImageCoordinator to create and interact with
       NtpBackgroundImageLayout.
    Screenshots: http://shortn/_dngL0vBLDS.
    
    Bug: 423579377
    Change-Id: Ia5a71efa163bcd94f519ae352dad38704e5454a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7158451
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1547548}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceCoordinator.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceCoordinator.java
@@ -64,7 +64,7 @@ import org.chromium.chrome.browser.ntp_customization.NtpCustomizationCoordinator
 import org.chromium.chrome.browser.ntp_customization.NtpCustomizationCoordinatorFactory;
 import org.chromium.chrome.browser.ntp_customization.NtpCustomizationMetricsUtils;
 import org.chromium.chrome.browser.ntp_customization.NtpCustomizationUtils.NtpBackgroundImageType;
-import org.chromium.chrome.browser.ntp_customization.theme.NtpBackgroundImageView;
+import org.chromium.chrome.browser.ntp_customization.theme.NtpBackgroundImageCoordinator;
 import org.chromium.chrome.browser.ntp_customization.theme.upload_image.BackgroundImageInfo;
 import org.chromium.chrome.browser.privacy.settings.PrivacyPreferencesManagerImpl;
 import org.chromium.chrome.browser.profiles.Profile;
@@ -136,7 +136,6 @@ public class FeedSurfaceCoordinator
 
     private FeedSurfaceMediator mMediator;
     private final boolean mIsNtpCustomizationV2Enabled;
-    private @Nullable NtpBackgroundImageView mBackgroundImageView;
     private final UiConfig mUiConfig;
     private final FrameLayout mRootView;
     private boolean mIsActive;
@@ -189,6 +188,7 @@ public class FeedSurfaceCoordinator
     private final boolean mIsNewTabPageCustomizationEnabled;
     private @Nullable ImageButton mNtpCustomizationButton;
     private @Nullable NtpCustomizationConfigManager mNtpCustomizationConfigManager;
+    private @Nullable NtpBackgroundImageCoordinator mNtpBackgroundImageCoordinator;
     private NtpCustomizationConfigManager.@Nullable HomepageStateListener mHomepageStateListener;
 
     /** Provides the additional capabilities needed for the container view. */
@@ -485,7 +485,9 @@ public class FeedSurfaceCoordinator
 
         mIsNtpCustomizationV2Enabled = ChromeFeatureList.sNewTabPageCustomizationV2.isEnabled();
         if (mIsNewTabPageCustomizationEnabled) {
-            addBackgroundImageView();
+            mNtpBackgroundImageCoordinator =
+                    new NtpBackgroundImageCoordinator(
+                            mActivity, mRootView, mUiConfig, mDefaultBackgroundColor);
         }
 
         // Pull-to-refresh set up.
@@ -710,19 +712,6 @@ public class FeedSurfaceCoordinator
         }
     }
 
-    private void addBackgroundImageView() {
-        mBackgroundImageView = new NtpBackgroundImageView(mActivity, mUiConfig);
-        FrameLayout.LayoutParams backgroundLayoutParams =
-                new FrameLayout.LayoutParams(
-                        FrameLayout.LayoutParams.MATCH_PARENT,
-                        FrameLayout.LayoutParams.MATCH_PARENT);
-        mBackgroundImageView.setLayoutParams(backgroundLayoutParams);
-
-        // Applies an opaque background color to prevent any views underneath from being visible.
-        mBackgroundImageView.setBackgroundColor(mDefaultBackgroundColor);
-        mRootView.addView(mBackgroundImageView);
-    }
-
     // Sets the background image for the embedder NTP.
     private void setBackground(
             Bitmap originalBitmap,
@@ -733,7 +722,7 @@ public class FeedSurfaceCoordinator
         }
 
         mRecyclerView.setBackgroundColor(Color.TRANSPARENT);
-        assumeNonNull(mBackgroundImageView)
+        assumeNonNull(mNtpBackgroundImageCoordinator)
                 .setBackground(originalBitmap, backgroundImageInfo, backgroundType);
     }
 
@@ -744,8 +733,8 @@ public class FeedSurfaceCoordinator
      */
     private void setBackgroundColor(@ColorInt int backgroundColor) {
         mRecyclerView.setBackgroundColor(backgroundColor);
-        if (mBackgroundImageView != null) {
-            mBackgroundImageView.setImageBitmap(null);
+        if (mNtpBackgroundImageCoordinator != null) {
+            mNtpBackgroundImageCoordinator.clearBackground();
         }
 
         if (mNtpHeader != null) {
@@ -877,8 +866,8 @@ public class FeedSurfaceCoordinator
             mNtpCustomizationConfigManager.removeListener(mHomepageStateListener);
             mHomepageStateListener = null;
         }
-        if (mBackgroundImageView != null) {
-            mBackgroundImageView.destroy();
+        if (mNtpBackgroundImageCoordinator != null) {
+            mNtpBackgroundImageCoordinator.destroy();
         }
     }
 
@@ -1523,7 +1512,8 @@ public class FeedSurfaceCoordinator
         return mRootView;
     }
 
-    public void setBackgroundImageViewForTesting(NtpBackgroundImageView backgroundImageView) {
-        mBackgroundImageView = backgroundImageView;
+    public void setBackgroundImageCoordinatorForTesting(
+            NtpBackgroundImageCoordinator backgroundImageCoordinator) {
+        mNtpBackgroundImageCoordinator = backgroundImageCoordinator;
     }
 }
