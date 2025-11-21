2b7267b70490e1fbdb2e35fcc7be53a18254e801
David Bienvenu <davidbienvenu@chromium.org>
2025-11-21 09:44:19 -0800

    Reenable renderer_context browser tests on Windows
    Bug: 410751413
    Change-Id: I57add2cb53f3c8ca1da9b29e32c590cb60195cca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7180185
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1548558}

--- chrome/browser/renderer_context_menu/spelling_options_submenu_observer_browsertest.cc
+++ chrome/browser/renderer_context_menu/spelling_options_submenu_observer_browsertest.cc
@@ -113,17 +113,8 @@ IN_PROC_BROWSER_TEST_F(SpellingOptionsSubMenuObserverTest, ToggleSpelling) {
   EXPECT_TRUE(menu()->IsCommandIdChecked(IDC_CHECK_SPELLING_WHILE_TYPING));
 }
 
-// TODO(https://crbug.com/410751413): Deleting temporary directories using
-// test_file_util is flaky on Windows.
-#if BUILDFLAG(IS_WIN)
-#define MAYBE_ToggleSpellingWithEnhancedSpellCheck \
-  DISABLED_ToggleSpellingWithEnhancedSpellCheck
-#else
-#define MAYBE_ToggleSpellingWithEnhancedSpellCheck \
-  ToggleSpellingWithEnhancedSpellCheck
-#endif
 IN_PROC_BROWSER_TEST_F(SpellingOptionsSubMenuObserverTest,
-                       MAYBE_ToggleSpellingWithEnhancedSpellCheck) {
+                       ToggleSpellingWithEnhancedSpellCheck) {
   InitMenu(true, true, "en-US,es", std::vector<std::string>(1, "en-US"));
 
   // Verify that because 'Use Enhanced spell check' is checked, the
@@ -143,15 +134,7 @@ IN_PROC_BROWSER_TEST_F(SpellingOptionsSubMenuObserverTest,
 // Single accept language is selected based on the dictionaries preference.
 // Consequently selecting multilingual spellcheck should copy all accept
 // languages into spellcheck dictionaries preference.
-// TODO(https://crbug.com/410751413): Deleting temporary directories using
-// test_file_util is flaky on Windows.
-#if BUILDFLAG(IS_WIN)
-#define MAYBE_SelectMultilingual DISABLED_SelectMultilingual
-#else
-#define MAYBE_SelectMultilingual SelectMultilingual
-#endif
-IN_PROC_BROWSER_TEST_F(SpellingOptionsSubMenuObserverTest,
-                       MAYBE_SelectMultilingual) {
+IN_PROC_BROWSER_TEST_F(SpellingOptionsSubMenuObserverTest, SelectMultilingual) {
   InitMenu(true, false, "en-US,es", std::vector<std::string>(1, "en-US"));
   EXPECT_FALSE(menu()->IsCommandIdChecked(IDC_SPELLCHECK_MULTI_LINGUAL));
   EXPECT_TRUE(menu()->IsCommandIdChecked(IDC_SPELLCHECK_LANGUAGES_FIRST));
@@ -167,15 +150,8 @@ IN_PROC_BROWSER_TEST_F(SpellingOptionsSubMenuObserverTest,
 // Multilingual spellcheck is selected when all dictionaries are used for
 // spellcheck. Consequently selecting "English (United States)" should set
 // spellcheck dictionaries preferences to ["en-US"].
-// TODO(https://crbug.com/410751413): Deleting temporary directories using
-// test_file_util is flaky on Windows.
-#if BUILDFLAG(IS_WIN)
-#define MAYBE_SelectFirstLanguage DISABLED_SelectFirstLanguage
-#else
-#define MAYBE_SelectFirstLanguage SelectFirstLanguage
-#endif
 IN_PROC_BROWSER_TEST_F(SpellingOptionsSubMenuObserverTest,
-                       MAYBE_SelectFirstLanguage) {
+                       SelectFirstLanguage) {
   std::vector<std::string> dictionaries;
   dictionaries.push_back("en-US");
   dictionaries.push_back("es");
@@ -189,15 +165,8 @@ IN_PROC_BROWSER_TEST_F(SpellingOptionsSubMenuObserverTest,
 }
 
 // Single dictionary should be selected based on preferences.
-// TODO(https://crbug.com/410751413): Deleting temporary directories using
-// test_file_util is flaky on Windows.
-#if BUILDFLAG(IS_WIN)
-#define MAYBE_SingleLanguageSelected DISABLED_SingleLanguageSelected
-#else
-#define MAYBE_SingleLanguageSelected SingleLanguageSelected
-#endif
 IN_PROC_BROWSER_TEST_F(SpellingOptionsSubMenuObserverTest,
-                       MAYBE_SingleLanguageSelected) {
+                       SingleLanguageSelected) {
   InitMenu(true, false, "en-US", std::vector<std::string>(1, "en-US"));
   EXPECT_TRUE(menu()->IsCommandIdChecked(IDC_SPELLCHECK_LANGUAGES_FIRST));
 }
