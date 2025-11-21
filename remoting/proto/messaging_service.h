f32a5eeaceb5122ff9844f8ffacdcbf969e765ad
Joe Downing <joedow@google.com>
2025-11-21 13:12:32 -0800

    Roll /remoting/internal w/ minimal code changes needed to build remoting targets
    https://chrome-internal.googlesource.com/chrome/remoting/internal.git/+log/541223d62370..7532f5929c12
    
    Created with:
      roll-dep src/remoting/internal
    
    Bug: 359620500
    Change-Id: I3b11db827e769d0c011e18ea9536d0e995c6bcb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7186884
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Auto-Submit: Joe Downing <joedow@chromium.org>
    Commit-Queue: Jamie Walch <jamiewalch@chromium.org>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1548693}

--- remoting/proto/messaging_service.h
+++ remoting/proto/messaging_service.h
@@ -57,6 +57,11 @@ struct BurstStruct {
   std::string payload;
 };
 
+struct EncryptedStruct {
+  std::string payload;
+  std::string unencrypted_payload;
+};
+
 struct SimpleStruct {
   std::string payload;
 };
@@ -71,8 +76,9 @@ struct SystemTestStruct {
   SystemTestStruct& operator=(const SystemTestStruct&);
   ~SystemTestStruct();
 
-  std::variant<PingPongStruct,
-               BurstStruct,
+  std::variant<BurstStruct,
+               EncryptedStruct,
+               PingPongStruct,
                SimpleStruct,
                ShareSessionTokenStruct>
       test_message;
