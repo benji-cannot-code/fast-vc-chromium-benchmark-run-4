2c4858a6ecd2503921dbc63f3434e76dcdd1f8ee
Dongheun Kang <dongheun.kang@lge.com>
2025-11-20 18:16:39 -0800

    Improve EDID codec bitmask parsing
    Adds return-value checking for StringToUint() and logs a debug warning
    when parsing fails to avoid using invalid EDID codec values.
    
    Bug: 1270685
    Change-Id: I7b43bb43c1db001b2737910f997d29c171143cba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7154567
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1548217}

--- media/base/media_switches.cc
+++ media/base/media_switches.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/feature_list.h"
+#include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
 #include "build/build_config.h"
 #include "build/chromecast_buildflags.h"
@@ -1728,9 +1729,13 @@ uint32_t GetPassthroughAudioFormats() {
     auto* command_line = base::CommandLine::ForCurrentProcess();
     uint32_t value = 0;
     if (command_line->HasSwitch(switches::kAudioCodecsFromEDID)) {
-      base::StringToUint(
-          command_line->GetSwitchValueASCII(switches::kAudioCodecsFromEDID),
-          &value);
+      const std::string switch_value =
+          command_line->GetSwitchValueASCII(switches::kAudioCodecsFromEDID);
+      if (!base::StringToUint(switch_value, &value)) {
+        LOG(WARNING) << "Invalid value for --audio-codecs-from-edid: "
+                     << switch_value << ". Falling back to 0.";
+        return 0u;
+      }
     }
     return value;
   }();
