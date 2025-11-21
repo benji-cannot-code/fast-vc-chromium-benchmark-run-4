3fb5b5e3ab6335674b4d11779d69e086ac389ca7
Kevin McNee <mcnee@chromium.org>
2025-11-21 11:26:10 -0800

    Revert "[iOS][mvt-customization] Add collection view to display MVT"
    This reverts commit 177e3b9e57c7c23ebf5c2d3210375b59129eff71.
    
    Reason for revert: Suspect for check_static_initializers failure in https://ci.chromium.org/ui/b/8697573247101749761
    There appears to be a static initializer in most_visited_tiles_collection_view.mm
    
    Original change's description:
    > [iOS][mvt-customization] Add collection view to display MVT
    >
    > This CL creates a collection view that would replace the stack view to
    > hold the most visited tiles. In case there are <= 3 tiles, the view
    > should look exactly the same as the stack view version, with the
    > addition of the "+" button. When there are more tiles than that (to be
    > supported later,) the width of each cell would be the same as on
    > narrow-width shortcut cell, and the 5th cell would be half visible,
    > hinting the user to scroll left (or right in RTL.)
    >
    > Screenshot: https://screenshot.googleplex.com/9aM5UJpWU6fPbUr
    > (RTL) https://screenshot.googleplex.com/6BqmNfeB86BFVPK
    >
    > Design doc: go/bling-mvt-tech-design
    >
    > Fixed: 459882500
    > Change-Id: Ie0ab1bc49f626071850b573297fdec0f39253ebb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7180422
    > Reviewed-by: Scott Yoder <scottyoder@google.com>
    > Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1548526}
    
    Bug: 459882500
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Idb64783db2d58ac277ee3876ec2e8b7976190c70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7186189
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Auto-Submit: Kevin McNee <mcnee@chromium.org>
    Owners-Override: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1548636}

--- ios/chrome/browser/content_suggestions/ui_bundled/cells/content_suggestions_tile_constants.mm
+++ ios/chrome/browser/content_suggestions/ui_bundled/cells/content_suggestions_tile_constants.mm
@@ -19,6 +19,10 @@ const CGFloat kSymbolContentSuggestionsPointSize = 22;
 
 }  // namespace
 
+const int kContentSuggestionsTilesHorizontalSpacingRegular = 19;
+
+const int kContentSuggestionsTilesHorizontalSpacingCompact = 5;
+
 NSString* TitleForCollectionShortcutType(NTPCollectionShortcutType type) {
   switch (type) {
     case NTPCollectionShortcutTypeBookmark:
