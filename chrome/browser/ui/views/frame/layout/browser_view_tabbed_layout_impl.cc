778cc1332fd760521a0fc97e0e6a843dc2ff9f7a
Dana Fried <dfried@chromium.org>
2025-11-21 12:03:07 -0800

    [Chrome Next] Rename layout classes (Part 1)
    This renames BrowserViewLayoutImpl to BrowserViewTabbedLayoutImpl for
    parity with BrowserViewAppLayoutImpl.
    
    Bug: 453717426
    Change-Id: I011d7f7797c32f93b30fd7881ea24675837e7dc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7180963
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1548656}

--- chrome/browser/ui/views/frame/layout/browser_view_tabbed_layout_impl.cc
+++ chrome/browser/ui/views/frame/layout/browser_view_tabbed_layout_impl.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/views/frame/layout/browser_view_layout_impl.h"
+#include "chrome/browser/ui/views/frame/layout/browser_view_tabbed_layout_impl.h"
 
 #include <algorithm>
 #include <memory>
@@ -36,7 +36,7 @@ static constexpr int kLoadingBarOffset =
 
 }  // namespace
 
-BrowserViewLayoutImpl::BrowserViewLayoutImpl(
+BrowserViewTabbedLayoutImpl::BrowserViewTabbedLayoutImpl(
     std::unique_ptr<BrowserViewLayoutDelegate> delegate,
     Browser* browser,
     BrowserViewLayoutViews views)
@@ -44,10 +44,10 @@ BrowserViewLayoutImpl::BrowserViewLayoutImpl(
                                   browser,
                                   std::move(views)) {}
 
-BrowserViewLayoutImpl::~BrowserViewLayoutImpl() = default;
+BrowserViewTabbedLayoutImpl::~BrowserViewTabbedLayoutImpl() = default;
 
-BrowserViewLayoutImpl::TopSeparatorType
-BrowserViewLayoutImpl::GetTopSeparatorType() const {
+BrowserViewTabbedLayoutImpl::TopSeparatorType
+BrowserViewTabbedLayoutImpl::GetTopSeparatorType() const {
   if (!delegate().IsToolbarVisible() && !delegate().IsBookmarkBarVisible()) {
     return TopSeparatorType::kNone;
   }
@@ -90,8 +90,8 @@ BrowserViewLayoutImpl::GetTopSeparatorType() const {
   return TopSeparatorType::kMultiContents;
 }
 
-std::pair<gfx::Size, gfx::Size> BrowserViewLayoutImpl::GetMinimumTabStripSize()
-    const {
+std::pair<gfx::Size, gfx::Size>
+BrowserViewTabbedLayoutImpl::GetMinimumTabStripSize() const {
   switch (GetTabStripType()) {
     case TabStripType::kHorizontal:
       return std::make_pair(gfx::Size(),
@@ -109,7 +109,7 @@ std::pair<gfx::Size, gfx::Size> BrowserViewLayoutImpl::GetMinimumTabStripSize()
   }
 }
 
-gfx::Size BrowserViewLayoutImpl::GetMinimumMainAreaSize() const {
+gfx::Size BrowserViewTabbedLayoutImpl::GetMinimumMainAreaSize() const {
   const gfx::Size toolbar_size = views().toolbar->GetMinimumSize();
   const gfx::Size bookmark_bar_size =
       (views().bookmark_bar && views().bookmark_bar->GetVisible())
@@ -135,8 +135,8 @@ gfx::Size BrowserViewLayoutImpl::GetMinimumMainAreaSize() const {
   return gfx::Size(width, height);
 }
 
-BrowserViewLayoutImpl::TabStripType BrowserViewLayoutImpl::GetTabStripType()
-    const {
+BrowserViewTabbedLayoutImpl::TabStripType
+BrowserViewTabbedLayoutImpl::GetTabStripType() const {
   if (views().webui_tab_strip && views().webui_tab_strip->GetVisible()) {
     return TabStripType::kWebUi;
   }
@@ -147,7 +147,8 @@ BrowserViewLayoutImpl::TabStripType BrowserViewLayoutImpl::GetTabStripType()
                                          : TabStripType::kNone;
 }
 
-gfx::Size BrowserViewLayoutImpl::GetMinimumSize(const views::View* host) const {
+gfx::Size BrowserViewTabbedLayoutImpl::GetMinimumSize(
+    const views::View* host) const {
   // This is a simplified version of the same method in
   // `BrowserViewLayoutImplOld` that assumes a standard browser.
   const auto [vertical_tabstrip_size, horizontal_tabstrip_size] =
@@ -184,8 +185,8 @@ gfx::Size BrowserViewLayoutImpl::GetMinimumSize(const views::View* host) const {
   return gfx::Size(min_width, min_height);
 }
 
-BrowserViewLayoutImpl::ProposedLayout
-BrowserViewLayoutImpl::CalculateProposedLayout(
+BrowserViewTabbedLayoutImpl::ProposedLayout
+BrowserViewTabbedLayoutImpl::CalculateProposedLayout(
     const BrowserLayoutParams& browser_params) const {
   // TODO(https://crbug.com/453717426): Consider caching layouts of the same
   // size if no `InvalidateLayout()` has happened.
@@ -535,7 +536,7 @@ BrowserViewLayoutImpl::CalculateProposedLayout(
   return layout;
 }
 
-gfx::Rect BrowserViewLayoutImpl::CalculateTopContainerLayout(
+gfx::Rect BrowserViewTabbedLayoutImpl::CalculateTopContainerLayout(
     ProposedLayout& layout,
     BrowserLayoutParams params,
     bool needs_exclusion) const {
--- chrome/browser/ui/views/frame/layout/browser_view_tabbed_layout_impl.h
+++ chrome/browser/ui/views/frame/layout/browser_view_tabbed_layout_impl.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_VIEWS_FRAME_LAYOUT_BROWSER_VIEW_LAYOUT_IMPL_H_
-#define CHROME_BROWSER_UI_VIEWS_FRAME_LAYOUT_BROWSER_VIEW_LAYOUT_IMPL_H_
+#ifndef CHROME_BROWSER_UI_VIEWS_FRAME_LAYOUT_BROWSER_VIEW_TABBED_LAYOUT_IMPL_H_
+#define CHROME_BROWSER_UI_VIEWS_FRAME_LAYOUT_BROWSER_VIEW_TABBED_LAYOUT_IMPL_H_
 
 #include <optional>
 #include <utility>
@@ -23,12 +23,13 @@ class View;
 
 // Provides a specialized layout implementation for normal tabbed browsers.
 // Should not be used for other types of browsers.
-class BrowserViewLayoutImpl : public BrowserViewLayoutImplCommon {
+class BrowserViewTabbedLayoutImpl : public BrowserViewLayoutImplCommon {
  public:
-  BrowserViewLayoutImpl(std::unique_ptr<BrowserViewLayoutDelegate> delegate,
-                        Browser* browser,
-                        BrowserViewLayoutViews views);
-  ~BrowserViewLayoutImpl() override;
+  BrowserViewTabbedLayoutImpl(
+      std::unique_ptr<BrowserViewLayoutDelegate> delegate,
+      Browser* browser,
+      BrowserViewLayoutViews views);
+  ~BrowserViewTabbedLayoutImpl() override;
 
  protected:
   // BrowserViewLayoutImplCommon:
@@ -61,4 +62,4 @@ class BrowserViewLayoutImpl : public BrowserViewLayoutImplCommon {
   TopSeparatorType GetTopSeparatorType() const;
 };
 
-#endif  // CHROME_BROWSER_UI_VIEWS_FRAME_LAYOUT_BROWSER_VIEW_LAYOUT_IMPL_H_
+#endif  // CHROME_BROWSER_UI_VIEWS_FRAME_LAYOUT_BROWSER_VIEW_TABBED_LAYOUT_IMPL_H_
