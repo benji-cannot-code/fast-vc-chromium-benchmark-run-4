e6153f8fc9fc737564d2186ceea9a5c03e6a7c8c
Georg Neis <neis@chromium.org>
2025-11-20 18:11:13 -0800

    ash: Remove a dependency on browser_finder.h
    To do so, introduce ash::BrowserController::GetBrowserForTab and
    implement it via TabInterface (no need to iterate over all browsers).
    
    Bug: b:369688254
    Change-Id: I18742a527776ed2b49ff91eec6ddb6c8e7e111fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7173665
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1548216}

--- chrome/browser/ash/browser_delegate/browser_controller_impl.cc
+++ chrome/browser/ash/browser_delegate/browser_controller_impl.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_helpers.h"
 #include "chromeos/ash/components/browser_context_helper/browser_context_helper.h"
 #include "components/account_id/account_id.h"
+#include "components/tabs/public/tab_interface.h"
 #include "ui/aura/window.h"
 
 namespace {
@@ -135,6 +136,14 @@ BrowserDelegate* BrowserControllerImpl::GetBrowserForWindow(
   BrowserView* browser_view =
       BrowserView::GetBrowserViewForNativeWindow(window);
   return GetDelegate(browser_view ? browser_view->browser() : nullptr);
+}
+
+BrowserDelegate* BrowserControllerImpl::GetBrowserForTab(
+    content::WebContents* contents) {
+  auto* tab = tabs::TabInterface::GetFromContents(contents);
+  return GetDelegate(
+      tab ? tab->GetBrowserWindowInterface()->GetBrowserForMigrationOnly()
+          : nullptr);
 }
 
 BrowserDelegate* BrowserControllerImpl::FindWebApp(const AccountId& account_id,
