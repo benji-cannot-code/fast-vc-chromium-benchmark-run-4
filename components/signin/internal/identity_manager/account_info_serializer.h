074a48fd19e8f71cb9454bfe5e58f01649552ab5
Alex Ilin <alexilin@chromium.org>
2025-11-21 03:00:37 -0800

    [signin] Introduce AccountInfo::Builder
    A new builder interface allows constructing AccountInfo instances
    without directly manipulating struct members.
    
    This CL also converts a couple of clients to use the builder facility.
    
    Bug: 458409080
    Change-Id: Ibe488795c45f4f127c83e13c5734e8534aa72642
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7131699
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1548345}

--- components/signin/internal/identity_manager/account_info_serializer.h
+++ components/signin/internal/identity_manager/account_info_serializer.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 struct AccountInfo;
 class AccountCapabilities;
+struct CoreAccountId;
 
 namespace signin {
 
@@ -34,12 +35,13 @@ class AccountInfoSerializer {
   static std::optional<AccountInfo> FromValue(const base::Value::Dict& dict);
 
  private:
-  // Deserializes `account_info` from `dict`. Does not read `account_id`.
-  static void DeserializeAccountInfoFromDict(const base::Value::Dict& dict,
-                                             AccountInfo& account_info);
+  // Deserializes `account_info` from `dict`. Does not read account ID from
+  // `dict` but sets `account_id` instead.
+  static std::optional<AccountInfo> DeserializeAccountInfoFromDict(
+      const base::Value::Dict& dict,
+      const CoreAccountId& account_id);
 
-  // Serializes `account_info` to `dict`. Does not write `account_id` or
-  // `last_downloaded_image_url_with_size`.
+  // Serializes `account_info` to `dict`. Does not write `account_id`.
   static void SerializeAccountInfoToDict(base::Value::Dict& dict,
                                          const AccountInfo& account_info);
 
