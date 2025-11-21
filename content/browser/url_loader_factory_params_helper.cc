96b203afd45356787b1d6c28a224edfdbe1c4373
Shivani Sharma <shivanisha@chromium.org>
2025-11-20 17:13:57 -0800

    [Connection-Allowlist] Adding network restrictions id to URLLoaderFactory
    This CL is the first one in the series that moves the functionality
    of Connection Allowlist prototype to check the enforcements in the content/browser and network service layers. The idea is to reuse the
    infrastructure for network revocation that is already implemented in
    these layers for fenced frames disableUntrustedNetwork() API (see
    NetworkContext:: IsNetworkForNonceAndUrlAllowed()).
    For reference, connection allowlists's prototype started in the chain
    of CLs beginning at https://chromium-review.googlesource.com/c/chromium/src/+/6985001.
    
    This one does the following:
    1. Creates an identifier in a given frame's URLLoaderFactory so any
    requests that go through this factory can be checked to apply network
    restrictions, if applicable. The check itself will be in the follow up.
    2. It reuses the value of DocumentAssociatedData::token() for this id
    since it is already uniquely created for each document.
    
    Tests: This CL only assigns the id in the URLLoaderFactory, which will
    be a path exercised by many existing tests. The next CL will actually
    test the network restrictions being checked.
    
    TODOs: This Cl only assigns the network_restriction_id for a frame's
    subresource URLLoaderFactory. The other cases will be done in follow
    ups. Those cases will also use the DocumentAssociatedData::token() of
    the associated frame, wherever possible.
    
    Bug: 447954811
    
    Change-Id: I483309f6632150dbc2ad73cfe2ac7dcb4d147dfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7082870
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Shivani Sharma <shivanisha@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1548189}

--- content/browser/url_loader_factory_params_helper.cc
+++ content/browser/url_loader_factory_params_helper.cc
@@ -78,7 +78,8 @@ network::mojom::URLLoaderFactoryParamsPtr CreateParams(
     net::CookieSettingOverrides cookie_setting_overrides,
     std::string_view debug_tag,
     bool require_cross_site_request_for_cookies,
-    bool is_for_service_worker) {
+    bool is_for_service_worker,
+    const std::optional<base::UnguessableToken>& network_restrictions_id) {
   DCHECK(process);
 
   network::mojom::URLLoaderFactoryParamsPtr params =
@@ -90,6 +91,8 @@ network::mojom::URLLoaderFactoryParamsPtr CreateParams(
   params->is_trusted = is_trusted;
   if (top_frame_token)
     params->top_frame_id = top_frame_token.value().value();
+
+  params->network_restrictions_id = network_restrictions_id;
   params->isolation_info = isolation_info;
 
   params->disable_web_security =
@@ -188,7 +191,12 @@ URLLoaderFactoryParamsHelper::CreateForFrame(
       frame->CreateDeviceBoundSessionObserver(), trust_token_issuance_policy,
       trust_token_redemption_policy, cookie_setting_overrides, debug_tag,
       /*require_cross_site_request_for_cookies=*/false,
-      /*is_for_service_worker=*/false);
+      /*is_for_service_worker=*/false,
+      /*network_restrictions_id=*/
+      frame->document_associated_data().token()->is_empty()
+          ? std::nullopt
+          : std::make_optional(
+                frame->document_associated_data().token().value()));
 }
 
 // static
@@ -225,7 +233,8 @@ URLLoaderFactoryParamsHelper::CreateForIsolatedWorld(
       trust_token_redemption_policy, cookie_setting_overrides,
       "ParamHelper::CreateForIsolatedWorld",
       /*require_cross_site_request_for_cookies=*/false,
-      /*is_for_service_worker=*/false);
+      /*is_for_service_worker=*/false,
+      /*TODO(crbug.com/447954811): network_restrictions_id*/ std::nullopt);
 }
 
 network::mojom::URLLoaderFactoryParamsPtr
@@ -260,7 +269,8 @@ URLLoaderFactoryParamsHelper::CreateForPrefetch(
       network::mojom::TrustTokenOperationPolicyVerdict::kForbid,
       cookie_setting_overrides, "ParamHelper::CreateForPrefetch",
       /*require_cross_site_request_for_cookies=*/false,
-      /*is_for_service_worker=*/false);
+      /*is_for_service_worker=*/false,
+      /*TODO(crbug.com/447954811): network_restrictions_id*/ std::nullopt);
 }
 
 // static
@@ -310,7 +320,8 @@ URLLoaderFactoryParamsHelper::CreateForWorker(
       network::mojom::TrustTokenOperationPolicyVerdict::kPotentiallyPermit,
       network::mojom::TrustTokenOperationPolicyVerdict::kPotentiallyPermit,
       net::CookieSettingOverrides(), debug_tag,
-      require_cross_site_request_for_cookies, is_for_service_worker);
+      require_cross_site_request_for_cookies, is_for_service_worker,
+      /*TODO(crbug.com/447954811): network_restrictions_id*/ std::nullopt);
 }
 
 // static
@@ -374,7 +385,8 @@ URLLoaderFactoryParamsHelper::CreateForEarlyHintsPreload(
       network::mojom::TrustTokenOperationPolicyVerdict::kForbid,
       net::CookieSettingOverrides(), "ParamHelper::CreateForEarlyHintsPreload",
       /*require_cross_site_request_for_cookies=*/false,
-      /*is_for_service_worker=*/false);
+      /*is_for_service_worker=*/false,
+      /*TODO(crbug.com/447954811): network_restrictions_id*/ std::nullopt);
 }
 
 // static
