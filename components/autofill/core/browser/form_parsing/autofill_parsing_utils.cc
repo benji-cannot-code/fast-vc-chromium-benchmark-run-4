557254c35ba1b2a54e2db72dd8ca19817440a286
Maryia Mankevich <mmaryia@google.com>
2025-11-21 04:36:31 -0800

    Clean up AutofillImproveCityFieldClassification
    Bug: 330508437
    Change-Id: I91eb212cda984c17cf333c70c77f32d9987fe701
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7062580
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Maryia Mankevich <mmaryia@google.com>
    Cr-Commit-Position: refs/heads/main@{#1548377}

--- components/autofill/core/browser/form_parsing/autofill_parsing_utils.cc
+++ components/autofill/core/browser/form_parsing/autofill_parsing_utils.cc
@@ -27,8 +27,6 @@ const base::Feature* GetFeatureOfRegexFeature(RegexFeature feature) {
       return &features::kAutofillSupportLastNamePrefix;
     case RegexFeature::kAutofillEnableLoyaltyCardsFilling:
       return &features::kAutofillEnableLoyaltyCardsFilling;
-    case RegexFeature::kAutofillImproveCityFieldClassification:
-      return &features::kAutofillImproveCityFieldClassification;
     case RegexFeature::kAutofillSupportSplitZipCode:
       return &features::kAutofillSupportSplitZipCode;
     case RegexFeature::kAutofillDisallowMoreHyphenLikeLabels:
