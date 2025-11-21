29627675283217d49bf82a1954fc7708b487649f
Stanisław Solarewicz <ssolarewicz@chromium.org>
2025-11-21 04:40:09 -0800

    [Okta SSO] Add ProxyingURLLoaderFactory boilerplate.
    Add a scaffolding for a URLLoaderFactory that in the future will
    intercept requests which should be handled with an sso extension.
    
    The factory is conditionally injected by checking if the platform auth
    provider managed is enabled. In the future it will also have to check
    if Okta is among allowed IDPs for the enterprise extensisble sso.
    
    At the moment even when injected, the factory will simply pass the
    request down the chain, essentially not introducing any new behaviour.
    
    Bug: b:433226247
    Change-Id: I46eb986040818d7067bddee7abfd5cbcfae47c1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7173259
    Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    Commit-Queue: Stanisław Solarewicz <ssolarewicz@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1548383}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -431,6 +431,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sandbox/win/src/sandbox_policy.h"
 #elif BUILDFLAG(IS_MAC)
 #include "chrome/browser/browser_process_platform_part_mac.h"
+#include "chrome/browser/enterprise/platform_auth/platform_auth_features.h"
+#include "chrome/browser/enterprise/platform_auth/platform_auth_proxying_url_loader_factory.h"
 #include "chrome/common/chrome_version.h"
 #include "components/soda/constants.h"
 #include "sandbox/mac/sandbox_serializer.h"
@@ -6394,6 +6396,13 @@ void ChromeContentBrowserClient::WillCreateURLLoaderFactory(
         captive_portal::CaptivePortalTabHelper::FromWebContents(web_contents) &&
         captive_portal::CaptivePortalTabHelper::FromWebContents(web_contents)
             ->is_captive_portal_window();
+  }
+#endif
+
+#if BUILDFLAG(IS_MAC)
+  if (base::FeatureList::IsEnabled(enterprise_auth::kOktaSSO)) {
+    enterprise_auth::ProxyingURLLoaderFactory::MaybeProxyRequest(
+        request_initiator, factory_builder);
   }
 #endif
 
