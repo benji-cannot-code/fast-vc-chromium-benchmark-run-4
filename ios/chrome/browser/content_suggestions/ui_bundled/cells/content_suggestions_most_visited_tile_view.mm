78c8f824c86ade0843fdbafd7962e52f495bd5fd
ginnnnnnny <ginnyhuang@chromium.org>
2025-11-21 12:55:57 -0800

    Reland "[iOS][mvt-customization] Add collection view to display MVT"
    This is a reland of commit 177e3b9e57c7c23ebf5c2d3210375b59129eff71
    
    Original change's description:
    > [iOS][mvt-customization] Add collection view to display MVT
    >
    > This CL creates a collection view that would replace the stack view to
    > hold the most visited tiles. In case there are <= 3 tiles, the view
    > should look exactly the same as the stack view version, with the
    > addition of the "+" button. When there are more tiles than that (to be
    > supported later,) the width of each cell would be the same as on
    > narrow-width shortcut cell, and the 5th cell would be half visible,
    > hinting the user to scroll left (or right in RTL.)
    >
    > Screenshot: https://screenshot.googleplex.com/9aM5UJpWU6fPbUr
    > (RTL) https://screenshot.googleplex.com/6BqmNfeB86BFVPK
    >
    > Design doc: go/bling-mvt-tech-design
    >
    > Fixed: 459882500
    > Change-Id: Ie0ab1bc49f626071850b573297fdec0f39253ebb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7180422
    > Reviewed-by: Scott Yoder <scottyoder@google.com>
    > Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1548526}
    
    Change-Id: I019d7e1f480e811b082c842f3e5ff95c12345265
    Fixed: 459882500
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7185221
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1548684}

--- ios/chrome/browser/content_suggestions/ui_bundled/cells/content_suggestions_most_visited_tile_view.mm
+++ ios/chrome/browser/content_suggestions/ui_bundled/cells/content_suggestions_most_visited_tile_view.mm
@@ -96,6 +96,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return self;
 }
 
+#pragma mark - UIContentView
+
 - (void)setConfiguration:(id<UIContentConfiguration>)config {
   if (![config isKindOfClass:ContentSuggestionsMostVisitedItem.class]) {
     return;
