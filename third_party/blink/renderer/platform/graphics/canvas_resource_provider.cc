146583e73b11d12b00f6314cc415437f569f5daa
Colin Blundell <blundell@chromium.org>
2025-11-20 23:55:27 -0800

    [Blink] Inline CreateSkSurface() in CRPBitmap::DoExternalDrawAndSnapshot
    The method body is here [1]. This is the next step in our gradually
    decoupling this method from CRPBitmap.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc;l=234?q=CanvasResourceProviderBitmap::CreateSkSu&ss=chromium
    
    Bug: 352263194
    Change-Id: If750017d7a2062467d218d87f362cae2dcacfb3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7178620
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1548295}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
@@ -182,7 +182,9 @@ CanvasResourceProviderBitmap::DoExternalDrawAndSnapshot(
     clear_frame_ = false;
 
     if (!surface_) {
-      surface_ = CreateSkSurface();
+      const auto info = GetSkImageInfo().makeAlphaType(kPremul_SkAlphaType);
+      const auto props = GetSkSurfaceProps();
+      surface_ = SkSurfaces::Raster(info, &props);
     }
 
     if (!skia_canvas_) {
