3dcd707a598285739215a360944d8a5c8b511ac5
Ari Chivukula <arichiv@chromium.org>
2025-11-20 18:53:42 -0800

    [deprecations] Cleanup old entries
    All of these are past the expiration date and no longer have counters in
    use.
    
    Change-Id: Iff3d6324d11b91992013eb411988fe9377fd1c3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7181981
    Commit-Queue: Sun Yueru <yrsun@chromium.org>
    Reviewed-by: Sun Yueru <yrsun@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1548231}

--- components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
+++ components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
@@ -141,7 +141,6 @@ UseCounterMetricsRecorder::GetAllowedUkmFeatures() {
           WebFeature::kThirdPartyFileSystem,
           WebFeature::kThirdPartyIndexedDb,
           WebFeature::kThirdPartyCacheStorage,
-          WebFeature::kDeclarativeShadowRoot,
           WebFeature::kOverlayPopup,
           WebFeature::kOverlayPopupAd,
           WebFeature::kTrustTokenXhr,
