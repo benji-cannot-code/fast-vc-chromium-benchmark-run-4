e39844fe2967baa1171e046d07ad94bb5f01a5cc
Duong Dac <ddac@chromium.org>
2025-11-21 07:23:03 -0800

    Replace kSync with kSignin in c/b/glic
    Bug: 417950948
    Change-Id: Ib8214d6563b0409cbe81ab870d0a04211641501c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7179500
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1548467}

--- chrome/browser/glic/test_support/glic_test_util.cc
+++ chrome/browser/glic/test_support/glic_test_util.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/identity_manager/account_capabilities_test_mutator.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/signin/public/identity_manager/identity_test_utils.h"
+#include "components/sync/base/features.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace glic {
@@ -269,8 +270,11 @@ void InvalidateAccount(Profile* profile) {
       identity_manager->HasAccountWithRefreshTokenInPersistentErrorState(
           identity_manager->GetPrimaryAccountId(
               signin::ConsentLevel::kSignin)));
-  ASSERT_FALSE(
-      identity_manager->HasPrimaryAccount(signin::ConsentLevel::kSync));
+  if (!base::FeatureList::IsEnabled(
+          syncer::kReplaceSyncPromosWithSignInPromos)) {
+    ASSERT_FALSE(
+        identity_manager->HasPrimaryAccount(signin::ConsentLevel::kSync));
+  }
   ASSERT_TRUE(
       identity_manager->HasPrimaryAccount(signin::ConsentLevel::kSignin));
 }
