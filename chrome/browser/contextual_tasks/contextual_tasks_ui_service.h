7962946b8a3750d668b2b88cf540a8dc90cdea4a
Shakti Sahu <shaktisahu@chromium.org>
2025-11-20 17:52:24 -0800

    [contextual_tasks] Added attached tabs menu dropdown
    This CL adds the UI for attached tabs. A placeholder icon was
    added which when clicked will reveal the list of tabs attached to
    the task. The tab item when clicked from the dropdown will result
    in a tab switch. If the tab isn't a open tab, the browser will
    create a new tab with the given URL.
    
    Bug: 460614856
    Change-Id: Id6e17e8b265018c3dbfaf5f376621beeb82b0350
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7154021
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Mickey Burks <mickeyburks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1548207}

--- chrome/browser/contextual_tasks/contextual_tasks_ui_service.h
+++ chrome/browser/contextual_tasks/contextual_tasks_ui_service.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 
 #include "base/memory/weak_ptr.h"
+#include "chrome/browser/contextual_tasks/contextual_tasks.mojom.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "content/public/browser/frame_tree_node_id.h"
 #include "url/gurl.h"
@@ -104,6 +105,12 @@ class ContextualTasksUiService : public KeyedService {
   // Move the WebContents for the given task to a new tab.
   virtual void MoveTaskUiToToNewTab(const base::Uuid& task_id,
                                     BrowserWindowInterface* browser);
+
+  // Called when a tab in the sources menu is clicked. Switches to the tab or
+  // reopens the tab depending on whether the tab is already open on tab strip.
+  void OnTabClickedFromSourcesMenu(int32_t tab_id,
+                                   const GURL& url,
+                                   BrowserWindowInterface* browser);
 
  private:
   const raw_ptr<Profile> profile_;
