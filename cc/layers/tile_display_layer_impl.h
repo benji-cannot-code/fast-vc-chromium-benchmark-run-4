5ba62a600a712c14fce1c8970ffd5c2a24c17211
Colin Blundell <blundell@chromium.org>
2025-11-21 01:09:03 -0800

    [//cc] Add helpers to produce TilingSetCoverageIterator
    Augments PictureLayerImpl and TileDisplayLayerImpl with helpers to
    produce the TilingSetCoverageIterator that they use in
    AppendQuadsSpecialization(). This will allow for turning this into a
    templated virtual method on TileBasedLayerImpl in followup.
    
    Bug: 450651370
    Change-Id: I1d63bdc11e2a475fbd52360be0457eb8d6fcdb14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7171206
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1548318}

--- cc/layers/tile_display_layer_impl.h
+++ cc/layers/tile_display_layer_impl.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/tiles/tile_index.h"
 #include "cc/tiles/tile_priority.h"
 #include "cc/tiles/tiling_coverage_iterator.h"
+#include "cc/tiles/tiling_set_coverage_iterator.h"
 #include "components/viz/common/resources/shared_image_format.h"
 #include "components/viz/common/resources/transferable_resource.h"
 #include "ui/gfx/geometry/axis_transform2d.h"
@@ -218,6 +219,9 @@ class CC_EXPORT TileDisplayLayerImpl : public TileBasedLayerImpl {
       AppendQuadsData* append_quads_data,
       viz::SharedQuadState* shared_quad_state,
       const Occlusion& scaled_occlusion) override;
+  TilingSetCoverageIterator<Tiling> Cover(const gfx::Rect& coverage_rect,
+                                          float coverage_scale,
+                                          float ideal_contents_scale);
 
   bool is_directly_composited_image_ = false;
   bool nearest_neighbor_ = false;
