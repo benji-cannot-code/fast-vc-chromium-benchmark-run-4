9e7315a135bc12d7f386c430c08d7ceeef3ff9b5
Eric Ekey <ericekey@google.com>
2025-11-21 06:36:19 -0800

    [iOS] Refactor IconDetailView Initializer
    This change reduces the number of initializer arguments for the
    IconDetailView by introducing an IconDetailViewConfiguration that
    packages data to pass to the view.
    
    Fixed: 371968237
    Change-Id: Ifdcbe1e270a61b0824d5cb682d82f92c90d81463
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7173238
    Reviewed-by: Benjamin Williams <bwwilliams@google.com>
    Commit-Queue: Eric Ekey <ericekey@google.com>
    Cr-Commit-Position: refs/heads/main@{#1548434}

--- ios/chrome/browser/content_suggestions/ui_bundled/safety_check/ui/safety_check_view.mm
+++ ios/chrome/browser/content_suggestions/ui_bundled/safety_check/ui/safety_check_view.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/strings/string_number_conversions.h"
 #import "components/version_info/version_info.h"
 #import "ios/chrome/browser/content_suggestions/ui_bundled/cells/icon_detail_view.h"
+#import "ios/chrome/browser/content_suggestions/ui_bundled/cells/icon_detail_view_configuration.h"
 #import "ios/chrome/browser/content_suggestions/ui_bundled/cells/icon_view.h"
 #import "ios/chrome/browser/content_suggestions/ui_bundled/cells/multi_row_container_view.h"
 #import "ios/chrome/browser/content_suggestions/ui_bundled/magic_stack/magic_stack_module_content_view_delegate.h"
@@ -240,11 +241,6 @@ bool IsDefault(SafetyCheckState* state) {
 // and `layoutType`.
 - (IconDetailView*)iconDetailView:(SafetyCheckItemType)itemType
                        layoutType:(IconDetailViewLayoutType)layoutType {
-  NSString* symbolName = [self symbolNameForItemType:itemType];
-
-  // `kInfoCircleSymbol` is the only default symbol used within the Safety Check
-  // view(s).
-  BOOL usesDefaultSymbol = [symbolName isEqualToString:kInfoCircleSymbol];
 
   if (!IsNTPBackgroundCustomizationEnabled()) {
     // Determine the symbol color palette and symbol background color based on
@@ -260,27 +256,27 @@ bool IsDefault(SafetyCheckState* state) {
     }
   }
 
-  IconDetailView* view = [[IconDetailView alloc]
-                       initWithTitle:[self titleText:itemType]
-                         description:
-                             (layoutType == IconDetailViewLayoutType::kHero
-                                  ? [self descriptionText:itemType]
-                                  : [self compactDescriptionText:itemType])
-                          layoutType:layoutType
-                     backgroundImage:nil
-                          symbolName:symbolName
-                  symbolColorPalette:_symbolColorPalette
-               symbolBackgroundColor:_symbolBackgroundColor
-      symbolContainerBackgroundColor:_symbolContainerBackgroundColor
-                   usesDefaultSymbol:usesDefaultSymbol
-                       showCheckmark:(itemType == SafetyCheckItemType::kAllSafe)
-             accessibilityIdentifier:
-                 [self accessibilityIdentifierForItemType:itemType]];
+  NSString* symbolName = [self symbolNameForItemType:itemType];
 
+  IconDetailViewConfiguration* viewConfig = [IconDetailViewConfiguration
+      configurationWithTitleText:[self titleText:itemType]
+                 descriptionText:(layoutType == IconDetailViewLayoutType::kHero
+                                      ? [self descriptionText:itemType]
+                                      : [self
+                                            compactDescriptionText:itemType])];
+  viewConfig.symbolName = symbolName;
+  viewConfig.symbolColorPalette = _symbolColorPalette;
+  viewConfig.symbolBackgroundColor = _symbolBackgroundColor;
+  viewConfig.usesDefaultSymbol = [symbolName isEqualToString:kInfoCircleSymbol];
+  viewConfig.layoutType = layoutType;
+  viewConfig.showCheckmark = itemType == SafetyCheckItemType::kAllSafe;
+  viewConfig.accessibilityIdentifier =
+      [self accessibilityIdentifierForItemType:itemType];
+
+  IconDetailView* view =
+      [[IconDetailView alloc] initWithConfiguration:viewConfig];
   view.identifier = NameForSafetyCheckItemType(itemType);
-
   view.tapDelegate = self;
-
   return view;
 }
 
