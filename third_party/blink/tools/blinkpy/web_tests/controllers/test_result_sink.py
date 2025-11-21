40f52a163b3823dc8744829b2746777846efabf8
Jonathan Lee <jonathanjlee@google.com>
2025-11-21 14:28:25 -0800

    [web-tests] Actually report device failures (3/n)
    This CL resumes a series of patches aimed at removing legacy web
    test-specific logic from `TestResultSink` so that `run_wpt_tests.py` can
    reuse the logic for adding common web test tags.
    
    As it turns out, `TestResultSink` never even reported device failures
    because of an early out before `TestRunResults.add()`. The integration
    test masked this bug because a different code path filled in a default
    SKIP result. The bug also likely went unnoticed in the wild because only
    Fuchsia can raise device failures---an exceedingly rare combination.
    
    Coerce `device_failed` to `TIMEOUT` as soon as the `TestResult` is
    constructed, then fix the `WebTestRunner` logic to always report device
    failures as unexpected timeouts. By moving the `device_failed` handling
    earlier in the reporting pipeline:
    * `TestResultSink` doesn't need to do anything special.
    * Legacy JSON results are consistent with the ResultDB results.
    
    Bug: 406299273
    Change-Id: Icc81b32de2e3d8321cea24277756b2c55958b160
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7186803
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Auto-Submit: Jonathan Lee <jonathanjlee@google.com>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1548731}

--- third_party/blink/tools/blinkpy/web_tests/controllers/test_result_sink.py
+++ third_party/blink/tools/blinkpy/web_tests/controllers/test_result_sink.py
@@ -29,18 +29,6 @@ logging.getLogger("urllib3").setLevel(logging.WARNING)
 _log = logging.getLogger(__name__)
 
 
-# A map from the enum values of typ.ResultType to luci.resultdb.v1.WebTest.Status.
-# See valid values at
-# https://source.chromium.org/chromium/infra/infra_superproject/+/main:infra/go/src/go.chromium.org/luci/resultdb/proto/v1/test_result.proto?q=WebTest
-_result_type_to_web_test_status = {
-    ResultType.Pass: 'PASS',
-    ResultType.Failure: 'FAIL',
-    ResultType.Timeout: 'TIMEOUT',
-    ResultType.Crash: 'CRASH',
-    ResultType.Skip: 'SKIP',
-}
-
-
 class TestResultSinkClosed(Exception):
     """Raises if sink() is called over a closed TestResultSink instance."""
 
@@ -93,20 +81,6 @@ class TestResultSink:
 
     def _send(self, data):
         self._session.post(self._url, data=json.dumps(data)).raise_for_status()
-
-    def _web_test_status(self, result):
-        """Returns the luci.resultdb.v1.WebTest.Status enum value corresponding to the result type.
-
-        Args:
-            result: The TestResult object to find the web test status of.
-        Returns:
-            The luci.resultdb.v1.WebTest.Status enum value.
-        """
-        status = _result_type_to_web_test_status.get(
-            ResultType.Timeout if result.device_failed else result.type)
-
-        assert status is not None, 'unsupported result.type %r' % result.type
-        return status
 
     def _tags(self, result):
         """Returns a list of tags that should be added into a given test result.
@@ -263,10 +237,8 @@ class TestResultSink:
             },
             'frameworkExtensions': {
                 'webTest': {
-                    # device failures are never expected.
-                    'isExpected': not result.device_failed
-                    and result.is_expected,
-                    'status': self._web_test_status(result),
+                    'isExpected': result.is_expected,
+                    'status': result.type,
                 },
             },
         }
@@ -284,12 +256,7 @@ class TestResultSink:
             }
             r['testIdStructured'] = struct_test_dict
 
-        if result.device_failed:
-            r['statusV2'] = 'FAILED'
-            r['failureReason'] = {
-                'kind': 'TIMEOUT',
-            }
-        elif result.is_expected:
+        if result.is_expected:
             if result.type == ResultType.Skip:
                 r['statusV2'] = 'SKIPPED'
                 # ResultDB requires a skipped reason message to be uploaded for all skipped tests.
