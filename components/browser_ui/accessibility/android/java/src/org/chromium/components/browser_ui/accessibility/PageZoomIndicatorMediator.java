1cb67aa3d98813f88d82626af9e757d5eaa24182
Hitarth Kothari <hitarthkothari@google.com>
2025-11-21 08:02:46 -0800

    [Zoom Indicator] Make the Page Zoom popup window focusable and add an accessibility pane title.
    This improves accessibility by allowing screen readers to focus on the
    popup window and providing a descriptive title for the pane.
    
    This CL also makes the text fields in the popup and the menu polite live
    regions, ensuring TB feedback to changes in zoom level.
    
    Bug: 462203347, 462211158, 462199718
    Change-Id: I489956c947e04e3d78d99c4ca3a01c014e57e6c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7178680
    Auto-Submit: Hitarth Kothari <hitarthkothari@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1548492}

--- components/browser_ui/accessibility/android/java/src/org/chromium/components/browser_ui/accessibility/PageZoomIndicatorMediator.java
+++ components/browser_ui/accessibility/android/java/src/org/chromium/components/browser_ui/accessibility/PageZoomIndicatorMediator.java
@@ -96,7 +96,7 @@ class PageZoomIndicatorMediator {
                         view,
                         ViewGroup.LayoutParams.WRAP_CONTENT,
                         ViewGroup.LayoutParams.WRAP_CONTENT);
-        popupWindow.setFocusable(false);
+        popupWindow.setFocusable(true);
         popupWindow.setOutsideTouchable(true);
         popupWindow.setOnDismissListener(onDismissListener);
 
