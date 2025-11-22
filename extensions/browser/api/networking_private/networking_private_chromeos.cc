ccab9869652f3c8e771239b7d35ee7a529b583e4
Pavol Marko <pmarko@google.com>
2025-11-22 08:08:03 -0800

    cleanup: Remove network_portal_detector refs from extensions/browser/api/networking_private
    network_portal_detector is not used anymore (see CL:5458690).
    Remove obsolete references to it.
    
    Bug: 447558638
    Change-Id: I96284270139640db883e233f697a80a1a159bcc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7173085
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1548885}

--- extensions/browser/api/networking_private/networking_private_chromeos.cc
+++ extensions/browser/api/networking_private/networking_private_chromeos.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/network/network_util.h"
 #include "chromeos/ash/components/network/onc/network_onc_utils.h"
 #include "chromeos/ash/components/network/onc/onc_translator.h"
-#include "chromeos/ash/components/network/portal_detector/network_portal_detector.h"
 #include "chromeos/ash/components/network/technology_state_controller.h"
 #include "components/onc/onc_constants.h"
 #include "content/public/browser/browser_context.h"
