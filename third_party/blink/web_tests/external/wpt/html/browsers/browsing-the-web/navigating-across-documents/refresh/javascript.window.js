d781065e8e734feab2a2f2e56c01b8ddadb19338
Jonathan Lee <jonathanjlee@google.com>
2025-11-21 18:19:12 -0800

    Import wpt@c3e2c704fc37bb6e69c6d2b646b775f4e97c4991
    https://github.com/web-platform-tests/wpt/compare/8d6f5b94d...c3e2c704f
    
    Using wpt-import in Chromium 52ca070390db06846c0c6d793a636770d5a40607.
    With Chromium commits locally applied on WPT:
      49da3dacf3 "In wpt test, use performance.getEntriesByType for [...]
    
    This is a manual roll with commit 7d1e648a5f and two dependent commits
    (9809f197b6, 2c077c4387) reapplied. The first in the chain couldn't be
    exported because it renames files for two `subgrid/masonry-subgrid-*`
    tests, but https://github.com/web-platform-tests/wpt/pull/56110, a PR
    to split up those two tests, landed first.
    
    This import resolves the merge conflict as if 7d1e648a5f had never
    changed `subgrid/masonry-subgrid-*`. It should be straightforward to
    rename `masonry` to `grid-lanes` for the split tests.
    
    Mechanically generated from `web_tests/` with:
      ../tools/wpt_import.py \
        --ignore-exportable-commits=7d1e648a5f,9809f197b6,2c077c4387 \
        --credentials-json ~/wpt-credentials.json
      git checkout 7d1e648a5f~1 \
        external/wpt/css/css-grid/masonry/tentative/subgrid/masonry-subgrid-00'[12]*.html'
      git diff 7d1e648a5f^! external/wpt | git apply --index
      git rm --force \
        external/wpt/css/css-grid/grid-lanes/tentative/subgrid/grid-lanes-subgrid-00'[12]*.html'
      git commit -m "Resolve conflict in favor of upstream"
      git format-patch --stdout 9809f197b6^! -- external/wpt | git am
      git format-patch --stdout 2c077c4387^! -- external/wpt | git am
    
    Bug: 462806688
    Change-Id: I03fb091e5302271bb4950fc15f94f2f1e1602ced
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac-skia-alt-arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac12.0-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac12.0.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac13-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac13.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac14-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac14.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac15.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:win10.20h2-blink-rel
    No-Export: true
    Validate-Test-Flakiness: skip
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7188818
    Auto-Submit: Jonathan Lee <jonathanjlee@google.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1548808}

---
+++ third_party/blink/web_tests/external/wpt/html/browsers/browsing-the-web/navigating-across-documents/refresh/javascript.window.js
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+async_test(t => {
+  const frame = document.createElement("iframe");
+  t.add_cleanup(() => frame.remove());
+  const path = "resources/javascript.asis";
+  frame.src = path;
+  frame.onload = t.step_func(() => {
+    assert_equals(frame.contentWindow.location.href, new URL(path, self.location).href);
+    assert_equals(frame.contentWindow.x, 1);
+    t.step_timeout(() => {
+      assert_equals(frame.contentWindow.x, 1);
+      t.done();
+    }, 100);
+  });
+  document.body.appendChild(frame);
+}, "Refresh to a javascript: URL should not work");
