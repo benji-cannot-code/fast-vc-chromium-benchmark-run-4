fd1888507d2b3f923d6e992a5526975d4819f1d5
Gary Klassen <gklassen@chromium.org>
2025-11-21 18:55:05 -0800

    Add Page.getAnnotatedPageContent DevTools command.
    This change introduces a new experimental DevTools command,
    Page.getAnnotatedPageContent, which allows fetching AI-annotated page
    content.
    
    To avoid a circular dependency between //content and
    //components/optimization_guide, a delegate pattern is used.
    
    Bug: b: 462206707
    Change-Id: I37a0ef0cd4cfb9aced56ba6e4e2ba05d654ac608
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7169447
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Gary Klassen <gklassen@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1548816}

--- content/browser/devtools/protocol/page_handler.h
+++ content/browser/devtools/protocol/page_handler.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <map>
 #include <memory>
+#include <optional>
 #include <string>
 #include <variant>
 #include <vector>
@@ -194,6 +195,9 @@ class PageHandler : public DevToolsDomainHandler,
                                const Binary& data) override;
 
   Response SetPrerenderingAllowed(bool is_allowed) override;
+  void GetAnnotatedPageContent(
+      std::optional<bool> include_actionable_information,
+      std::unique_ptr<GetAnnotatedPageContentCallback> callback) override;
 
   Response AssureTopLevelActiveFrame();
 
