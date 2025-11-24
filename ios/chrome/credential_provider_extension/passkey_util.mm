16bfa9fd8c958c6c8291d19db69d30e8fa9bd8e2
Alexis Hetu <sugoi@chromium.org>
2025-11-24 08:12:31 -0800

    [iOS] Remove dependency on Authentication Services for UV
    This CL makes use of the ConvertToUserVerificationRequirement
    function from device/fido in order to further unify user
    verification requirement code between the CPE and
    PasskeyJavaScriptFeature.
    
    Bug: 460484682
    Change-Id: I4d5365b4062d77986251b02260f4d0bf88aafc87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7180800
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Reviewed-by: Noémie St-Onge <noemies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1549214}

--- ios/chrome/credential_provider_extension/passkey_util.mm
+++ ios/chrome/credential_provider_extension/passkey_util.mm
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/credential_provider_extension/passkey_util.h"
 
-#import <AuthenticationServices/AuthenticationServices.h>
-
 #import "base/apple/foundation_util.h"
 #import "base/containers/span.h"
 #import "base/strings/string_number_conversions.h"
@@ -16,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/webauthn/core/browser/gpm_user_verification_policy.h"
 #import "components/webauthn/core/browser/passkey_model_utils.h"
 #import "device/fido/fido_types.h"
+#import "device/fido/fido_user_verification_requirement.h"
 #import "ios/chrome/common/app_group/app_group_constants.h"
 #import "ios/chrome/common/credential_provider/ASPasskeyCredentialIdentity+credential.h"
 #import "ios/chrome/common/credential_provider/archivable_credential+passkey.h"
@@ -140,35 +139,6 @@ void SaveToIdentityStore(id<Credential> credential,
   };
   [ASCredentialIdentityStore.sharedStore
       getCredentialIdentityStoreStateWithCompletion:stateCompletion];
-}
-
-// Returns the UserVerificationRequirement based on the provided
-// `user_verification_preference_string`. The passed string is expected to match
-// one of the user verification preference options made available by the
-// WebAuthn API.
-std::optional<device::UserVerificationRequirement>
-UserVerificationRequirementFromString(
-    ASAuthorizationPublicKeyCredentialUserVerificationPreference
-        user_verification_preference_string) {
-  if ([user_verification_preference_string
-          isEqualToString:
-              ASAuthorizationPublicKeyCredentialUserVerificationPreferenceRequired]) {
-    return device::UserVerificationRequirement::kRequired;
-  } else if (
-      [user_verification_preference_string
-          isEqualToString:
-              ASAuthorizationPublicKeyCredentialUserVerificationPreferencePreferred]) {
-    return device::UserVerificationRequirement::kPreferred;
-  } else if (
-      [user_verification_preference_string
-          isEqualToString:
-              ASAuthorizationPublicKeyCredentialUserVerificationPreferenceDiscouraged]) {
-    return device::UserVerificationRequirement::kDiscouraged;
-  } else {
-    // Either indicates that the WebAuthn API changed, or that the website
-    // provided an unexpected/empty string.
-    return std::nullopt;
-  }
 }
 
 }  // namespace
@@ -328,18 +298,14 @@ BOOL ShouldPerformUserVerificationForPreference(
     return NO;
   }
 
-  std::optional<device::UserVerificationRequirement>
-      user_verification_requirement = UserVerificationRequirementFromString(
-          user_verification_preference_string);
-
-  if (!user_verification_requirement.has_value()) {
-    // Fall back to the default preference as per the WebAuthn spec.
-    user_verification_requirement =
-        device::UserVerificationRequirement::kPreferred;
-  }
-
+  // Fall back to the `kPreferred` UV requirement as per the WebAuthn spec.
+  std::string user_verification_requirement_string =
+      SysNSStringToUTF8(user_verification_preference_string);
   return webauthn::GpmWillDoUserVerification(
-      *user_verification_requirement, is_biometric_authentication_enabled);
+      device::ConvertToUserVerificationRequirement(
+          user_verification_requirement_string)
+          .value_or(device::UserVerificationRequirement::kPreferred),
+      is_biometric_authentication_enabled);
 }
 
 void SavePasskeyCredential(id<Credential> credential) {
