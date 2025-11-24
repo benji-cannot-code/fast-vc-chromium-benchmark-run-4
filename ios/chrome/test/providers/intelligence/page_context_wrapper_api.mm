43d19d8afbbd7fc61ba3c3c7dd110357059437ad
Vincent Boisselle <vincb@google.com>
2025-11-24 08:24:08 -0800

    [ios] Move intelligence/ providers to a dedicated folder
    Bug: 460380319
    Change-Id: I06a303b570a866f1b18ee9eb59c384a3272a8455
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7152555
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1549218}

---
+++ ios/chrome/test/providers/intelligence/page_context_wrapper_api.mm
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/public/provider/chrome/browser/intelligence/page_context_wrapper_api.h"
+
+#import <string>
+
+namespace {
+// Script to check whether PageContext should be detached from the request.
+constexpr const char16_t* kShouldDetachPageContextScript = u"return false;";
+}  // namespace
+
+namespace ios::provider {
+
+const std::u16string GetPageContextShouldDetachScriptV2() {
+  return kShouldDetachPageContextScript;
+}
+
+}  // namespace ios::provider
