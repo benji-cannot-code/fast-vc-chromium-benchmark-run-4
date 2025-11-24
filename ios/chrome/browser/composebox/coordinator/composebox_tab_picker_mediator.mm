7f15a4d7e620ead0a10d36b09716c6acc02624d8
Aliona DANGLA <alionadangla@google.com>
2025-11-24 09:59:49 -0800

    [iOS] Add attachment limit to tab picker and display snackbar
    See video:
    https://drive.google.com/file/d/1KbILkTzmRodJtCt_zLUe0nw4DZr30p9J/view?usp=sharing
    
    Bug: 458059298
    Change-Id: I8f7867def2904e4bef6efc0fb1273ba5258900f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7185607
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1549272}

--- ios/chrome/browser/composebox/coordinator/composebox_tab_picker_mediator.mm
+++ ios/chrome/browser/composebox/coordinator/composebox_tab_picker_mediator.mm
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/composebox/coordinator/composebox_tab_picker_mediator.h"
 
 #import "base/strings/string_number_conversions.h"
+#import "ios/chrome/browser/composebox/coordinator/composebox_constants.h"
 #import "ios/chrome/browser/composebox/coordinator/web_state_deferred_executor.h"
+#import "ios/chrome/browser/composebox/ui/composebox_snackbar_presenter.h"
 #import "ios/chrome/browser/composebox/ui/composebox_tab_picker_consumer.h"
 #import "ios/chrome/browser/intelligence/persist_tab_context/model/persist_tab_context_browser_agent.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
@@ -109,8 +111,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)userTappedOnItemID:(GridItemIdentifier*)itemID {
-  CHECK(self.modeHolder.mode == TabGridMode::kSelection);
-  if (itemID.type == GridItemType::kTab) {
+  CHECK_EQ(self.modeHolder.mode, TabGridMode::kSelection);
+  CHECK_EQ(itemID.type, GridItemType::kTab);
+  if (![self.selectedEditingItems containItem:itemID] &&
+      self.selectedEditingItems.tabsCount >= kAttachmentLimit) {
+    ComposeboxSnackbarPresenter* snackbar =
+        [[ComposeboxSnackbarPresenter alloc] initWithBrowser:self.browser];
+    [snackbar showAttachmentLimitSnackbar];
+    return;
+  }
     web::WebState* webState = GetWebState(
         self.webStateList,
         WebStateSearchCriteria{
@@ -134,7 +143,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                               updateSnapshotForWebState:webState->GetWeakPtr()
                                                  itemID:itemID];
                         }];
-    }
   }
   [super userTappedOnItemID:itemID];
 }
