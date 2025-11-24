f8bc3aa7e8808f40349aedcca6d2042a42829a82
Xiaochen Zhou <xiaochenzh@chromium.org>
2025-11-24 07:29:14 -0800

    Observation delay controller excludes ad frames from waiting for load completion.
    Gated by a default disabled feature.
    
    When feature enabled, observation delay controller will exclude ad
    subframes loading when wait for load completion. It immediately moves to
    the next state (wait for visual state update) if the frame tree is not
    loading.
    
    FrameTree::IsLoadingIncludingInnerFrameTrees() now takes in a parameter
    to exclude ad subframes from query, default to false.
    
    Bug: b:437396526
    Change-Id: Ib08944edc6901b16b929186e7072895289438d31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7152541
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Xiaochen Zhou <xiaochenzh@chromium.org>
    Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1549192}

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -2518,7 +2518,13 @@ SiteInstanceImpl* WebContentsImpl::GetSiteInstance() {
 }
 
 bool WebContentsImpl::IsLoading() {
-  return primary_frame_tree_.IsLoadingIncludingInnerFrameTrees();
+  return primary_frame_tree_.IsLoadingIncludingInnerFrameTrees(
+      /*exclude_ad_subframes=*/false);
+}
+
+bool WebContentsImpl::IsLoadingExcludingAdSubframes() const {
+  return primary_frame_tree_.IsLoadingIncludingInnerFrameTrees(
+      /*exclude_ad_subframes=*/true);
 }
 
 double WebContentsImpl::GetLoadProgress() {
