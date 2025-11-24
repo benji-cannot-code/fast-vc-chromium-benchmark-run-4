10e7c61d1c3afd697725fc75976c1025fa007f4d
Tommy Nyquist <nyquist@chromium.org>
2025-11-24 14:49:48 -0800

    [contextual_tasks] Decorate context with contextual search data.
    This CL allows the `PendingContextDecorator` to access contextual search
    data to enrich the `ContextualTaskContext`. This is done by passing the
    `ContextualSearchSessionHandle` to the context decorators through the
    `ContextDecorationParams`. The decorator can then use the session handle
    to get information about uploaded context tokens and add them as
    `UrlAttachment`s to the context.
    
    Key changes include:
    - Added `AsWeakPtr()` to `ContextualSearchSessionHandle` to allow it to
      be passed to other services.
    - Updated `PendingContextDecorator` to use the session handle and its
      context controller to get information about uploaded files.
    - Updated `ContextualTaskContext` to include and expose data from
      contextual search context.
    - Added unit tests to verify that the `PendingContextDecorator`
      correctly decorates the context with data from the contextual search
      service and behaves correctly when data is missing.
    
    R=hujasonx@google.com, shaktisahu@chromium.org
    
    Bug: b:462641325
    Change-Id: Ida38b1f868c687e8d4793732626a8d6268686d66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7198120
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1549431}

--- components/contextual_search/contextual_search_session_handle.cc
+++ components/contextual_search/contextual_search_session_handle.cc
@@ -212,4 +212,9 @@ ContextualSearchSessionHandle::GetUploadedContextTokens() const {
   return uploaded_context_tokens_;
 }
 
+base::WeakPtr<ContextualSearchSessionHandle>
+ContextualSearchSessionHandle::AsWeakPtr() {
+  return weak_ptr_factory_.GetWeakPtr();
+}
+
 }  // namespace contextual_search
