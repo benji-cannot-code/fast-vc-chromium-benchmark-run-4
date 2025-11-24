824ef644487cbb95b78167af8e305fc02f1e1595
Antonio Alphonse <alphonsea@google.com>
2025-11-24 08:12:53 -0800

    policy: base/hash/sha1 -> crypto/obsolete/sha1
    Required for backward compatibility with the client_id derived from
    /etc/machine-id, which is used as a filename for storing the DM token.
    
    DM tokens are associated by hashed client IDs, and are stored on-disk.
    Changing the hashing algorithm for the client id may break already
    existing enrollments or create twin enrollments (that is, enrollments
    with different client information, but the same DM token). This could
    lead to issues with fetching policies and providing browser status
    reports. Because of this, this change migrates to the non-deprecated
    API.
    
    This should be a mechanical change, with no expected behavior change.
    
    Bug: 430344933
    Change-Id: If847524a2c8bf30f5e251d07b77fd79d429cf561
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7142339
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Antonio Alphonse <alphonsea@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Reviewed-by: Igor <igorcov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1549215}

--- chrome/browser/policy/browser_dm_token_storage_linux.cc
+++ chrome/browser/policy/browser_dm_token_storage_linux.cc
@@ -14,10 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/functional/callback_helpers.h"
-#include "base/hash/sha1.h"
 #include "base/logging.h"
 #include "base/path_service.h"
 #include "base/strings/string_util.h"
+#include "base/strings/string_view_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/syslog_logging.h"
 #include "base/task/task_traits.h"
@@ -25,8 +25,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/scoped_blocking_call.h"
 #include "chrome/common/chrome_paths.h"
 #include "components/policy/core/common/policy_logger.h"
+#include "crypto/obsolete/sha1.h"
 
 namespace policy {
+
+// Returns the SHA-1 hash of the input string. This is used to hash the
+// machine-id for use as a client ID. This is required for backwards
+// compatibility with existing enrolled machines.
+std::array<uint8_t, crypto::obsolete::kSha1Size> Sha1ForMachineId(
+    std::string_view input) {
+  return crypto::obsolete::Sha1::Hash(input);
+}
 
 namespace {
 
@@ -92,8 +101,9 @@ BrowserDMTokenStorageLinux::BrowserDMTokenStorageLinux()
 BrowserDMTokenStorageLinux::~BrowserDMTokenStorageLinux() = default;
 
 std::string BrowserDMTokenStorageLinux::InitClientId() {
-  if (!client_id_.empty())
+  if (!client_id_.empty()) {
     return client_id_;
+  }
 
   // The client ID is derived from /etc/machine-id
   // (https://www.freedesktop.org/software/systemd/man/machine-id.html). As per
@@ -113,7 +123,7 @@ std::string BrowserDMTokenStorageLinux::InitClientId() {
   }
 
   std::string machine_id_base64;
-  base::Base64UrlEncode(base::SHA1HashString(std::string(machine_id_trimmed)),
+  base::Base64UrlEncode(Sha1ForMachineId(machine_id_trimmed),
                         base::Base64UrlEncodePolicy::OMIT_PADDING,
                         &machine_id_base64);
 
@@ -136,8 +146,9 @@ std::string BrowserDMTokenStorageLinux::InitEnrollmentToken() {
   base::FilePath token_file_path =
       dir_policy_files_path.Append(kEnrollmentTokenFilename);
 
-  if (!base::ReadFileToString(token_file_path, &enrollment_token))
+  if (!base::ReadFileToString(token_file_path, &enrollment_token)) {
     return std::string();
+  }
 
   return std::string(
       base::TrimWhitespaceASCII(enrollment_token, base::TRIM_ALL));
@@ -146,12 +157,14 @@ std::string BrowserDMTokenStorageLinux::InitEnrollmentToken() {
 
 std::string BrowserDMTokenStorageLinux::InitDMToken() {
   base::FilePath token_file_path;
-  if (!GetDmTokenFilePath(&token_file_path, InitClientId(), false))
+  if (!GetDmTokenFilePath(&token_file_path, InitClientId(), false)) {
     return std::string();
+  }
 
   std::string token;
-  if (!base::ReadFileToString(token_file_path, &token))
+  if (!base::ReadFileToString(token_file_path, &token)) {
     return std::string();
+  }
 
   return std::string(base::TrimWhitespaceASCII(token, base::TRIM_ALL));
 }
@@ -171,8 +184,9 @@ bool BrowserDMTokenStorageLinux::InitEnrollmentErrorOption() {
   base::FilePath options_file_path =
       dir_policy_files_path.Append(kEnrollmentOptionsFilePath);
 
-  if (!base::ReadFileToString(options_file_path, &options))
+  if (!base::ReadFileToString(options_file_path, &options)) {
     return false;
+  }
 
   return base::TrimWhitespaceASCII(options, base::TRIM_ALL) ==
          kEnrollmentMandatoryOption;
@@ -201,8 +215,10 @@ BrowserDMTokenStorageLinux::SaveDMTokenTaskRunner() {
 
 std::string BrowserDMTokenStorageLinux::ReadMachineIdFile() {
   std::string machine_id;
-  if (!base::ReadFileToString(base::FilePath(kMachineIdFilename), &machine_id))
+  if (!base::ReadFileToString(base::FilePath(kMachineIdFilename),
+                              &machine_id)) {
     return std::string();
+  }
   return machine_id;
 }
 
