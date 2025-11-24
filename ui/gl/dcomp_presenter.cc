baf8feb47ab68f35d4d35a437c8583243552b982
Michael Tang <tangm@microsoft.com>
2025-11-24 10:46:31 -0800

    Do not DumpWithoutCrashing in DCompPresenter::Present for presentation loss
    PRESENTATION_ERROR_LOST usually happens when device removed is caught
    internally in DWM when using DComp textures. Now that this is understood
    we can treat this as an "expected unrecoverable" case like regular
    device removed.
    
    Bug: 385039726
    Change-Id: Ie93c33ef0e24756a6d05bc1729321a3df73184cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7187427
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Reviewed-by: Michael Tang <tangm@microsoft.com>
    Commit-Queue: Michael Tang <tangm@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1549305}

--- ui/gl/dcomp_presenter.cc
+++ ui/gl/dcomp_presenter.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/gl/dcomp_presenter.h"
 
+#include <winerror.h>
+
 #include <memory>
 #include <utility>
 
@@ -121,7 +123,8 @@ void DCompPresenter::Present(SwapCompletionCallback completion_callback,
   if (!result.has_value()) {
     const HRESULT device_removed_reason =
         gl::GetDirectCompositionD3D11Device()->GetDeviceRemovedReason();
-    if (SUCCEEDED(device_removed_reason)) {
+    const bool not_device_removed = SUCCEEDED(device_removed_reason);
+    if (not_device_removed && result.error().hr != PRESENTATION_ERROR_LOST) {
       SCOPED_CRASH_KEY_NUMBER("gpu", "DCompPresenter.SWAP_FAILED.reason",
                               static_cast<int>(result.error().reason));
       SCOPED_CRASH_KEY_NUMBER(
@@ -130,7 +133,8 @@ void DCompPresenter::Present(SwapCompletionCallback completion_callback,
       base::debug::DumpWithoutCrashing();
     } else {
       // Ignore device removed cases as they don't usually indicate a problem
-      // originating from viz.
+      // originating from viz. `PRESENTATION_ERROR_LOST` usually happens when
+      // device removed is caught internally in DWM when using DComp textures.
     }
 
     std::move(completion_callback)
