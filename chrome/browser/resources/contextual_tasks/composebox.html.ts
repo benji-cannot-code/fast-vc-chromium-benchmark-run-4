58eca42d58fd424772395e52bac61f7b430e8f47
Bryan Nguyen <nguyenbryan@google.com>
2025-11-24 14:02:12 -0800

    [AIM] Show Lens icon in composebox.
    Handler will be implemented when overlay is ready to integrate.
    
    Bug: 461911729
    Change-Id: I6da164e435959ef8667d19666eaa6c37047781e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7181864
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Bryan Nguyen <nguyenbryan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1549412}

--- chrome/browser/resources/contextual_tasks/composebox.html.ts
+++ chrome/browser/resources/contextual_tasks/composebox.html.ts
@@ -22,6 +22,7 @@ export function getHtml(this: ContextualTasksComposeboxElement) {
       ?autofocus="${false}"
       carousel-on-top_
       .isCollapsible="${true}"
+      lens-button-disabled_$="${false}"
     >
         <!-- Currently an empty div that has height to force the input textarea to
             not flow behind the Lens icon. Eventually, this should contain the
