6530669d1aef3b8b9fd7caddc76b48484f4d6561
Sophie Chang <sophiechang@chromium.org>
2025-11-24 11:07:12 -0800

    Migrate context entrypoint clicked histogram to ContextualSearch
    Bug: 445841607
    Change-Id: I727bd7ce4076db93c52f57d65a5fe82a5164907b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7199501
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1549320}

--- ui/webui/resources/cr_components/composebox/context_menu_entrypoint.ts
+++ ui/webui/resources/cr_components/composebox/context_menu_entrypoint.ts
@@ -123,6 +123,7 @@ export class ContextMenuEntrypointElement extends
       loadTimeData.getBoolean('composeboxShowCreateImageButton');
   protected maxFileCount_: number =
       loadTimeData.getInteger('composeboxFileMaxCount');
+  private metricsSource_: string = loadTimeData.getString('composeboxSource');
 
   constructor() {
     super();
@@ -173,6 +174,10 @@ export class ContextMenuEntrypointElement extends
   }
 
   protected onEntrypointClick_() {
+    const metricName =
+        'ContextualSearch.ContextMenuEntry.Clicked.' + this.metricsSource_;
+    chrome.metricsPrivate.recordBoolean(metricName, true);
+
     if (this.entrypointName === 'Omnibox') {
       const entrypoint =
           this.shadowRoot.querySelector<HTMLElement>('#entrypoint');
@@ -184,9 +189,6 @@ export class ContextMenuEntrypointElement extends
       return;
     }
 
-    const metricName =
-        'NewTabPage.' + this.entrypointName + '.ContextMenuEntry.Clicked';
-    chrome.metricsPrivate.recordBoolean(metricName, true);
     this.showMenuAtEntrypoint_();
   }
 
