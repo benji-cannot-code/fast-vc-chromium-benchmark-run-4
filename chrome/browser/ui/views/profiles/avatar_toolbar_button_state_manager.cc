97d4178080599ae584952286b2983f6022fe2abd
Yurii Lahodiuk <ylahodiuk@google.com>
2025-11-24 06:33:11 -0800

    [PasskeyUnlockErrorUI] Add parameterized flag for multi-arm study
    In the scope of this change we are introducing a parameterized flag that
    allows to configure an arm of the Passkey Unlock Error UI study. Each
    study arm uses its own error message text labels.
    
    Also, as a cosmetic change, in the scope of this CL we updated
    `profile_menu_view.cc` to use
    `PasskeyUnlockManager::IsPasskeyUnlockErrorUiEnabled()` instead of
    checking each flag separately.
    
    Bug: b:454658811
    Change-Id: Ifbc6adef9e546f3fe5ad7e7f136192ca183dc287
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7185382
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Commit-Queue: Yurii Lahodiuk <ylahodiuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1549163}

--- chrome/browser/ui/views/profiles/avatar_toolbar_button_state_manager.cc
+++ chrome/browser/ui/views/profiles/avatar_toolbar_button_state_manager.cc
@@ -1201,11 +1201,8 @@ class PasskeyStateProvider : public StateProvider,
   }
 
   std::u16string GetAvatarTooltipText() const final {
-    // TODO(crbug.com/454658811): Add support for other experiment arms and
-    // check if the tooltip text needs to be updated.
     return passkey_manager_observation_.GetSource()
-        ->GetPasskeyErrorProfilePillTitle(
-            webauthn::PasskeyUnlockManager::ExperimentArm::kVerify);
+        ->GetPasskeyErrorProfilePillTitle();
   }
 
   std::pair<ChromeColorIds, ChromeColorIds> GetInkdropColors() const override {
@@ -1213,10 +1210,8 @@ class PasskeyStateProvider : public StateProvider,
   }
 
   std::u16string GetText() const override {
-    // TODO(crbug.com/454658811): Add support for other experiment arms.
     return passkey_manager_observation_.GetSource()
-        ->GetPasskeyErrorProfilePillTitle(
-            webauthn::PasskeyUnlockManager::ExperimentArm::kVerify);
+        ->GetPasskeyErrorProfilePillTitle();
   }
 
  private:
--- chrome/browser/webauthn/passkey_unlock_manager_unittest.cc
+++ chrome/browser/webauthn/passkey_unlock_manager_unittest.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/webauthn/passkey_unlock_manager.h"
 
+#include <string>
+
 #include "base/rand_util.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
@@ -15,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/webauthn/mock_enclave_manager.h"
 #include "chrome/browser/webauthn/passkey_model_factory.h"
 #include "chrome/browser/webauthn/passkey_unlock_manager_factory.h"
+#include "chrome/grit/generated_resources.h"
 #include "chrome/test/base/testing_profile.h"
 #include "components/sync/base/pref_names.h"
 #include "components/sync/test/test_sync_service.h"
@@ -26,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/fido/features.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/base/l10n/l10n_util.h"
 
 namespace webauthn {
 
@@ -142,6 +146,13 @@ class PasskeyUnlockManagerTest : public testing::Test {
     task_environment_.FastForwardBy(delta);
   }
 
+  void EnableUiExperimentArm(std::string arm_name) {
+    feature_params_[device::kPasskeyUnlockErrorUiExperimentArm.name] = arm_name;
+    feature_list_.Reset();
+    feature_list_.InitAndEnableFeatureWithParameters(
+        device::kPasskeyUnlockErrorUi, feature_params_);
+  }
+
   void SetUpEnclaveManager(bool ready) {
     MockEnclaveManager* enclave_manager_mock = static_cast<MockEnclaveManager*>(
         EnclaveManagerFactory::GetForProfile(profile_.get()));
@@ -156,6 +167,7 @@ class PasskeyUnlockManagerTest : public testing::Test {
   content::BrowserTaskEnvironment task_environment_{
       base::test::TaskEnvironment::TimeSource::MOCK_TIME};
   base::test::ScopedFeatureList feature_list_{device::kPasskeyUnlockManager};
+  std::map<std::string, std::string> feature_params_;
   raw_ptr<PasskeyUnlockManager> passkey_unlock_manager_;
   raw_ptr<syncer::TestSyncService> test_sync_service_;
   std::unique_ptr<TestingProfile> profile_;
@@ -318,6 +330,40 @@ TEST_F(PasskeyUnlockManagerTest, LogsPasskeyCountHistogramWithPasskeys) {
 
   AdvanceClock(base::Seconds(31));
   histogram_tester.ExpectUniqueSample("WebAuthentication.PasskeyCount", 1, 1);
+}
+
+TEST_F(PasskeyUnlockManagerTest, TextLablesForDifferentUiExperimentArms) {
+  SetUpPasskeyUnlockManager();
+
+  EnableUiExperimentArm("text_with_verify_wording");
+  EXPECT_EQ(passkey_unlock_manager()->GetPasskeyErrorProfilePillTitle(),
+            l10n_util::GetStringUTF16(IDS_AVATAR_BUTTON_PASSKEYS_ERROR_VERIFY));
+  EXPECT_EQ(passkey_unlock_manager()->GetPasskeyErrorProfileMenuDetails(),
+            l10n_util::GetStringUTF16(
+                IDS_PROFILE_MENU_PASSKEYS_ERROR_DESCRIPTION_VERIFY));
+  EXPECT_EQ(
+      passkey_unlock_manager()->GetPasskeyErrorProfileMenuButtonLabel(),
+      l10n_util::GetStringUTF16(IDS_PROFILE_MENU_PASSKEYS_ERROR_BUTTON_VERIFY));
+
+  EnableUiExperimentArm("text_with_get_wording");
+  EXPECT_EQ(passkey_unlock_manager()->GetPasskeyErrorProfilePillTitle(),
+            l10n_util::GetStringUTF16(IDS_AVATAR_BUTTON_PASSKEYS_ERROR_GET));
+  EXPECT_EQ(passkey_unlock_manager()->GetPasskeyErrorProfileMenuDetails(),
+            l10n_util::GetStringUTF16(
+                IDS_PROFILE_MENU_PASSKEYS_ERROR_DESCRIPTION_GET));
+  EXPECT_EQ(
+      passkey_unlock_manager()->GetPasskeyErrorProfileMenuButtonLabel(),
+      l10n_util::GetStringUTF16(IDS_PROFILE_MENU_PASSKEYS_ERROR_BUTTON_GET));
+
+  EnableUiExperimentArm("text_with_unlock_wording");
+  EXPECT_EQ(passkey_unlock_manager()->GetPasskeyErrorProfilePillTitle(),
+            l10n_util::GetStringUTF16(IDS_AVATAR_BUTTON_PASSKEYS_ERROR_UNLOCK));
+  EXPECT_EQ(passkey_unlock_manager()->GetPasskeyErrorProfileMenuDetails(),
+            l10n_util::GetStringUTF16(
+                IDS_PROFILE_MENU_PASSKEYS_ERROR_DESCRIPTION_UNLOCK));
+  EXPECT_EQ(
+      passkey_unlock_manager()->GetPasskeyErrorProfileMenuButtonLabel(),
+      l10n_util::GetStringUTF16(IDS_PROFILE_MENU_PASSKEYS_ERROR_BUTTON_UNLOCK));
 }
 
 }  // namespace
