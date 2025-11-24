3c4bf268b795e2fc00b8627e9bf71a1445332ac4
David Sanders <dsanders11@ucsbalum.com>
2025-11-24 11:59:55 -0800

    Remove unused include in page_content_extraction_service.h
    Should save ~99 MiB of compiler input size.
    
    Bug: 40318405
    Change-Id: Ie5fdbe7eacf0cee1324c95cfa80647d550e3166e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7197080
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#1549351}

--- chrome/browser/page_content_annotations/page_content_extraction_service.h
+++ chrome/browser/page_content_annotations/page_content_extraction_service.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "base/supports_user_data.h"
 #include "base/time/time.h"
-#include "chrome/browser/page_content_annotations/multi_source_page_context_fetcher.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/optimization_guide/proto/features/common_quality_data.pb.h"
 #include "components/page_content_annotations/core/web_state_wrapper.h"
