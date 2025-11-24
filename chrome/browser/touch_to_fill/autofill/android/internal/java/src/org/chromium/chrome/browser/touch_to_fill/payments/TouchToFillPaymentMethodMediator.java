ce33efc61dd9e3aff8a1c78a3b2235efd4793a8b
Fernando Ramirez <ferny@google.com>
2025-11-24 10:40:52 -0800

    [BNPL][Clank] Log TTF user actions for progress screen
    User action logging demo:
      http://screencast/cast/NTM0MjQwOTk4NTYyMjAxNnw4YjYzMWU2My02MQ
    
    Validate-Test-Flakiness: skip
    Bug: b:438785863, 430575808
    Change-Id: I5a76e5433fa434e232acd835e29231a999cc0312
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7164335
    Commit-Queue: Fernando Ramirez <ferny@google.com>
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1549301}

--- chrome/browser/touch_to_fill/autofill/android/internal/java/src/org/chromium/chrome/browser/touch_to_fill/payments/TouchToFillPaymentMethodMediator.java
+++ chrome/browser/touch_to_fill/autofill/android/internal/java/src/org/chromium/chrome/browser/touch_to_fill/payments/TouchToFillPaymentMethodMediator.java
@@ -265,7 +265,7 @@ class TouchToFillPaymentMethodMediator {
 
     // LINT.IfChange
 
-    // TODO(crbug.com/438785863): Add ToS, progress, and error screen user actions.
+    // TODO(crbug.com/438785863): Add ToS and error screen user actions.
     @VisibleForTesting
     static final String TOUCH_TO_FILL_BNPL_USER_ACTION = "Autofill.TouchToFill.Bnpl.UserAction";
 
@@ -306,6 +306,10 @@ class TouchToFillPaymentMethodMediator {
     @VisibleForTesting
     static final String ISSUER_SELECTION_SCREEN_SETTINGS_LINK_SELECTED =
             ".IssuerSelectionScreen.SettingsLinkSelected";
+
+    @VisibleForTesting static final String PROGRESS_SCREEN_SHOWN = ".ProgressScreen.Shown";
+
+    @VisibleForTesting static final String PROGRESS_SCREEN_DISMISSED = ".ProgressScreen.Dismissed";
 
     // LINT.ThenChange(/tools/metrics/actions/actions.xml)
 
@@ -652,6 +656,8 @@ class TouchToFillPaymentMethodMediator {
         mModel.set(SHEET_CLOSED_DESCRIPTION_ID, R.string.autofill_bnpl_progress_sheet_closed);
         mModel.set(FOCUSED_VIEW_ID_FOR_ACCESSIBILITY, R.id.touch_to_fill_progress_screen);
         mModel.set(VISIBLE, true);
+
+        recordTouchToFillBnplUserAction(PROGRESS_SCREEN_SHOWN);
     }
 
     /**
@@ -823,6 +829,8 @@ class TouchToFillPaymentMethodMediator {
             if (mSuggestions != null) {
                 if (mModel.get(CURRENT_SCREEN) == BNPL_ISSUER_SELECTION_SCREEN) {
                     recordTouchToFillBnplUserAction(ISSUER_SELECTION_SCREEN_DISMISSED);
+                } else if (mModel.get(CURRENT_SCREEN) == PROGRESS_SCREEN) {
+                    recordTouchToFillBnplUserAction(PROGRESS_SCREEN_DISMISSED);
                 }
                 RecordHistogram.recordEnumeratedHistogram(
                         TOUCH_TO_FILL_CREDIT_CARD_OUTCOME_HISTOGRAM,
