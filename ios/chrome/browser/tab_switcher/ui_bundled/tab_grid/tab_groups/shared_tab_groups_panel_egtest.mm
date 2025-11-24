10b40ff382bd935665016c675fe9279e62565f81
Chris Lu <thegreenfrog@google.com>
2025-11-24 12:01:01 -0800

    Reland "[ios] Add kIOSAutoOpenRemoteTabGroupsSettings to field trial config"
    This reverts commit 94367c5e184583d641513220211491b1b4ccfa42.
    
    Reason for revert: enabled feature for EGTests that need to add shared tab.
    
    Original change's description:
    > Revert "[ios] Add kIOSAutoOpenRemoteTabGroupsSettings to field trial config"
    >
    > This reverts commit 135a6bf2b4932c3ca5716327cbb5fc708a68b991.
    >
    > Reason for revert: This is causing bot failures.
    >
    > Builder: ios-simulator-full-configs
    > https://ci.chromium.org/p/chromium/builders/ci/ios-simulator-full-configs
    > First failing build:
    > https://ci.chromium.org/p/chromium/builders/ci/ios-simulator-full-configs/b8697656881623811169
    >
    > Original change's description:
    > > [ios] Add kIOSAutoOpenRemoteTabGroupsSettings to field trial config
    > >
    > > Change-Id: I210028ae6e3cc3c148833147f36ba7edca062f18
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7179182
    > > Reviewed-by: Briana McClure <bmcclure@google.com>
    > > Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    > > Auto-Submit: Chris Lu <thegreenfrog@chromium.org>
    > > Commit-Queue: Briana McClure <bmcclure@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1547910}
    >
    > Bug: 462625995
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I12b2c6194e71046616507f0466719bd143c383ef
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7184578
    > Auto-Submit: Ankush Singh <ankushkush@google.com>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1548324}
    
    Bug: 462625995
    Change-Id: I61b8644eaf3a76f249b56280869163fefffa49f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7186973
    Reviewed-by: Louis Romero <lpromero@google.com>
    Reviewed-by: Briana McClure <bmcclure@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1549352}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/shared_tab_groups_panel_egtest.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/shared_tab_groups_panel_egtest.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/strings/grit/components_strings.h"
 #import "ios/chrome/browser/authentication/test/signin_earl_grey.h"
 #import "ios/chrome/browser/authentication/test/signin_earl_grey_ui_test_util.h"
+#import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/signin/model/fake_system_identity.h"
 #import "ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_group_app_interface.h"
 #import "ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_groups_constants.h"
@@ -74,6 +75,7 @@ void AddSharedGroup(BOOL owner,
   AppLaunchConfiguration config;
   config.features_enabled.push_back(
       data_sharing::features::kDataSharingFeature);
+  config.features_disabled.push_back(kIOSAutoOpenRemoteTabGroupsSettings);
   // Add the flag to use FakeTabGroupSyncService.
   config.additional_args.push_back(
       "--" + std::string(test_switches::kEnableFakeTabGroupSyncService));
--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_group_sync_egtest.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_group_sync_egtest.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "components/data_sharing/public/features.h"
 #import "ios/chrome/browser/authentication/test/signin_earl_grey_ui_test_util.h"
+#import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/signin/model/fake_system_identity.h"
 #import "ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_group_app_interface.h"
 #import "ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_groups_constants.h"
@@ -133,6 +134,7 @@ void CloseGroupAtIndex(int group_cell_index) {
       "--" + std::string(test_switches::kEnableFakeTabGroupSyncService));
   config.features_enabled.push_back(
       data_sharing::features::kDataSharingFeature);
+  config.features_disabled.push_back(kIOSAutoOpenRemoteTabGroupsSettings);
   return config;
 }
 
