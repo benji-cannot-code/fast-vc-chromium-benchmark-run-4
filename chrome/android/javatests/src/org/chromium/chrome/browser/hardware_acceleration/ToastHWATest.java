8638cbd8f7dd240b2aa7be2316146fe269fb6420
Henrique Nakashima <hnakashima@chromium.org>
2025-11-24 13:14:21 -0800

    [Android] Refactor DownloadTestRule into a Helper
    - Not all test cases in a class need to use the same entry point
    - CustomMainActivityStart doesn't need to be implemented
    - Doesn't require wrapping/chaining test rules
    
    Bug: 462156364
    Change-Id: Ieaa18f6d913d7c8b389a2d548422af8dea33c876
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7173823
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1549394}

--- chrome/android/javatests/src/org/chromium/chrome/browser/hardware_acceleration/ToastHWATest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/hardware_acceleration/ToastHWATest.java
@@ -80,7 +80,6 @@ public class ToastHWATest implements CustomMainActivityStart {
     @Override
     public void customMainActivityStart() throws InterruptedException {
         mDownloadTestRule.startMainActivityOnBlankPage();
-        mDownloadTestRule.deleteFilesInDownloadDirectory(TEST_FILES);
     }
 
     @Test
