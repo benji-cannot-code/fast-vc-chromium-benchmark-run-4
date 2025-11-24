79c4bff738b310cbed6efecd0211fb6fd65d534b
Chidera Olibie <colibie@google.com>
2025-11-24 14:31:43 -0800

    [wv] Remove unused HandlerThread.
    Sequel to https://crrev.com/i/8785207
    
    Change-Id: Ief456ba17dfef22cdb5fbd6653a8b7311fc93de9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7198754
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Chidera Olibie <colibie@google.com>
    Auto-Submit: Chidera Olibie <colibie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1549424}

--- android_webview/java/src/org/chromium/android_webview/common/PlatformServiceBridge.java
+++ android_webview/java/src/org/chromium/android_webview/common/PlatformServiceBridge.java
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.android_webview.common;
 
 import android.content.Context;
-import android.os.Handler;
-import android.os.HandlerThread;
 
 import org.chromium.base.Callback;
 import org.chromium.base.ServiceLoaderUtil;
@@ -25,10 +23,6 @@ import org.chromium.build.annotations.Nullable;
 public abstract class PlatformServiceBridge {
     private static @Nullable PlatformServiceBridge sInstance;
     private static final Object sInstanceLock = new Object();
-
-    private static @Nullable HandlerThread sHandlerThread;
-    private static @Nullable Handler sHandler;
-    private static final Object sHandlerLock = new Object();
 
     protected PlatformServiceBridge() {}
 
@@ -56,18 +50,6 @@ public abstract class PlatformServiceBridge {
         synchronized (sInstanceLock) {
             sInstance = testBridge;
         }
-    }
-
-    // Return a handler appropriate for executing blocking Platform Service tasks.
-    public static Handler getHandler() {
-        synchronized (sHandlerLock) {
-            if (sHandler == null) {
-                sHandlerThread = new HandlerThread("PlatformServiceBridgeHandlerThread");
-                sHandlerThread.start();
-                sHandler = new Handler(sHandlerThread.getLooper());
-            }
-        }
-        return sHandler;
     }
 
     // Can WebView use Google Play Services (a.k.a. GMS)?
