0a720d445cae2c53ed09cc1be8b9dbf73f10d3f0
Deepak Ravichandran <deepakr@google.com>
2025-11-24 08:10:20 -0800

    Register google-chrome:// URI scheme on Linux
    This change allows branded Chrome builds to register
    "google-chrome://" and unbranded Chromium builds to register
    "chromium://" as a URI scheme handler on Linux.
    
    1. `GetMimeTypeSchemeHandlers()` to `shell_integration_linux.cc`
    to dynamically provide scheme handlers based on branding.
    2. Updates `desktop.template` with a placeholder
    `@@X_SCHEME_HANDLERS@@` for these dynamic handlers.
    3. Modifies `GetDesktopFileContentsForCommand()` to incorporate the
    dynamic scheme handlers.
    4. Adds a unit test in `shell_integration_linux_unittest.cc` to
    verify the correct generation of the `MimeType` field with the new
    scheme handler.
    
    TESTED=`xdg-mime query default x-scheme-handler/google-chrome`
    returns `google-chrome.desktop` on local build.
    
    Bug: b:446672134
    Change-Id: Ice7ed7a1ba66c4bc56f8579d7fcf1795d72c18b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7087621
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Deepak Ravichandran <deepakr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1549212}

--- chrome/browser/shell_integration_linux.h
+++ chrome/browser/shell_integration_linux.h
@@ -94,6 +94,7 @@ std::string GetDesktopFileContentsForCommand(
     const std::string& categories,
     const std::string& mime_type,
     bool no_display,
+    std::string_view extra_mime_types,
     std::set<web_app::DesktopActionInfo> action_info);
 
 // Returns contents for a .desktop file that launches chrome at the given url
@@ -123,6 +124,9 @@ base::FilePath GetMimeTypesRegistrationFilename(
 // passed to `xdg-mime` to register one or more custom MIME types in Linux.
 std::string GetMimeTypesRegistrationFileContents(
     const apps::FileHandlers& file_handlers);
+
+// Returns the mime type handler for direct launch.
+std::string GetDirectLaunchMimeTypeHandler();
 
 // Windows that correspond to web apps need to have a deterministic (and
 // different) WMClass than normal chrome windows so the window manager groups
