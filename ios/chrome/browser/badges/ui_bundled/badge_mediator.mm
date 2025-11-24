a602fb6b9ebbe75beb12ccda9e521349c631e636
Ibrahim Kanouche <kanouche@google.com>
2025-11-23 20:47:46 -0800

    [PSF] Add incognito-mode check before showing multi-badge state.
    This change adds an additional condition to only displays the
    multi-badge state when the Proactive Suggestions Framework is enabled
    and the user is not in incognito mode.
    
    Fixed: 462241971
    Change-Id: I57a31b01e86a905bc5160945730d3e89c477d4bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7169029
    Commit-Queue: Ibrahim Kanouche <kanouche@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Auto-Submit: Ibrahim Kanouche <kanouche@google.com>
    Cr-Commit-Position: refs/heads/main@{#1549045}

--- ios/chrome/browser/badges/ui_bundled/badge_mediator.mm
+++ ios/chrome/browser/badges/ui_bundled/badge_mediator.mm
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/shared/ui/list_model/list_model.h"
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
+#import "ios/web/public/browser_state.h"
 #import "ios/web/public/permissions/permissions.h"
 #import "ios/web/public/web_state_observer_bridge.h"
 
@@ -209,7 +210,8 @@ LocationBarBadgeType LocationBarBadgeTypeFromBadgeType(BadgeType badgeType) {
     if (infobarTypeBadgeStatePair.first ==
         InfobarType::kInfobarTypePermissions) {
       // TODO(crbug.com/458307626): Migrate to LocationBarBadge.
-      if (IsProactiveSuggestionsFrameworkEnabled() && self.webState) {
+      if (IsProactiveSuggestionsFrameworkEnabled() && self.webState &&
+          !self.webState->GetBrowserState()->IsOffTheRecord()) {
         // Check camera permission.
         if (self.webState->GetStateForPermission(web::PermissionCamera) ==
             web::PermissionStateAllowed) {
@@ -417,7 +419,8 @@ LocationBarBadgeType LocationBarBadgeTypeFromBadgeType(BadgeType badgeType) {
 
   NSArray<id<BadgeItem>>* badges = self.badges;
 
-  if (IsProactiveSuggestionsFrameworkEnabled()) {
+  if (IsProactiveSuggestionsFrameworkEnabled() && self.webState &&
+      !self.webState->GetBrowserState()->IsOffTheRecord()) {
     [self handleMultiBadgeDisplay:badges];
   } else {
     [self handleSingleBadgeDisplay:badges];
