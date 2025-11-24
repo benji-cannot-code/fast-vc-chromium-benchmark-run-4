810539472fd80db89fc0191bed895b710d2ba8bf
Alexis Hetu <sugoi@chromium.org>
2025-11-24 08:13:55 -0800

    [iOS] Provide serialized authenticator data during passkey creation
    This CL adds data to the return value of
    MakeAttestationObjectForCreation. When answering passkey creation
    requests directly, the PublicKeyCredential object will need to know
    about the AuthenticatorData, so the serialization of that object is
    now provided as a MakeAttestationObjectForCreation output.
    
    Bug: 460485333
    Change-Id: Id7162e88c5bc0fedfafb6ec03580381405f05383
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7179760
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Noémie St-Onge <noemies@google.com>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1549216}

--- components/webauthn/core/browser/passkey_model_utils.cc
+++ components/webauthn/core/browser/passkey_model_utils.cc
@@ -162,6 +162,11 @@ std::vector<uint8_t> ExtensionInputData::EvaluateHMAC(
                           : base::as_byte_span(hmac_secret));
 }
 
+SerializedAttestationObject::SerializedAttestationObject() = default;
+SerializedAttestationObject::SerializedAttestationObject(
+    SerializedAttestationObject&& other) = default;
+SerializedAttestationObject::~SerializedAttestationObject() = default;
+
 std::vector<sync_pb::WebauthnCredentialSpecifics> FilterShadowedCredentials(
     base::span<const sync_pb::WebauthnCredentialSpecifics> passkeys) {
   // Collect all explicitly shadowed credentials.
@@ -340,7 +345,7 @@ std::vector<uint8_t> MakeAuthenticatorDataForAssertion(std::string_view rp_id,
       .SerializeToByteArray();
 }
 
-std::vector<uint8_t> MakeAttestationObjectForCreation(
+SerializedAttestationObject MakeAttestationObjectForCreation(
     std::string_view rp_id,
     bool did_complete_uv,
     base::span<const uint8_t> credential_id,
@@ -366,11 +371,17 @@ std::vector<uint8_t> MakeAttestationObjectForCreation(
   device::AuthenticatorData authenticator_data(
       crypto::hash::Sha256(rp_id), flags, kSignatureCounter,
       std::move(attested_credential_data), /*extensions=*/std::nullopt);
+  SerializedAttestationObject serialized_attestation_object;
+  serialized_attestation_object.authenticator_data =
+      authenticator_data.SerializeToByteArray();
+
   device::AttestationObject attestationObject(
       std::move(authenticator_data),
       std::make_unique<device::NoneAttestationStatement>());
+  serialized_attestation_object.attestation_object =
+      cbor::Writer::Write(device::AsCBOR(attestationObject)).value();
 
-  return cbor::Writer::Write(device::AsCBOR(attestationObject)).value();
+  return serialized_attestation_object;
 }
 
 std::optional<std::vector<uint8_t>> GenerateEcSignature(
