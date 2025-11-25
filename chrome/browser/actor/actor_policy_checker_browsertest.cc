04aba8fd2b3367771830ea3cf2f9a15fea27ce43
Hidehiko Abe <hidehiko@chromium.org>
2025-11-25 09:20:36 -0800

    Permanently disable a few actor policy tests in ChromeOS
    Some tests are exercising a feature that is not supported in ChromeOS,
    which is to sign-out or re-sign-in primary accounts.
    Excludes these tests from the build permanently.
    
    BUG=460824365
    TEST=Tryjob
    
    Change-Id: Ie4ce7a2aa88b5a3b73937a25b4b4740fd8ec7200
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7182524
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: William Liu <liuwilliam@chromium.org>
    Reviewed-by: Jun Ishiguro <junis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1549867}

--- chrome/browser/actor/actor_policy_checker_browsertest.cc
+++ chrome/browser/actor/actor_policy_checker_browsertest.cc
@@ -54,8 +54,10 @@ struct TestAccount {
 };
 
 constexpr TestAccount kNonEnterpriseAccount = {"foo@testbar.com", ""};
+#if !BUILDFLAG(IS_CHROMEOS)
 constexpr TestAccount kEnterpriseAccount = {"foo@testenterprise.com",
                                             "testenterprise.com"};
+#endif  // !BUILDFLAG(IS_CHROMEOS)
 }  // namespace
 
 class ActorPolicyCheckerBrowserTestBase : public ActorToolsTest {
@@ -446,14 +448,10 @@ class ActorPolicyCheckerBrowserTestWithManagedAccount
   base::test::ScopedFeatureList scoped_feature_list_;
 };
 
-// TODO(crbug.com/460824365): Enable on ChromeOS.
-#if BUILDFLAG(IS_CHROMEOS)
-#define MAYBE_CapabilityUpdatedForAccount DISABLED_CapabilityUpdatedForAccount
-#else
-#define MAYBE_CapabilityUpdatedForAccount CapabilityUpdatedForAccount
-#endif
+// Note: sign-out from enterprise account is not allowed in ChromeOS.
+#if !BUILDFLAG(IS_CHROMEOS)
 IN_PROC_BROWSER_TEST_F(ActorPolicyCheckerBrowserTestWithManagedAccount,
-                       MAYBE_CapabilityUpdatedForAccount) {
+                       CapabilityUpdatedForAccount) {
   // No account is signed in, thus no capability.
   EXPECT_FALSE(ActorKeyedService::Get(browser()->profile())
                    ->GetPolicyChecker()
@@ -477,6 +475,7 @@ IN_PROC_BROWSER_TEST_F(ActorPolicyCheckerBrowserTestWithManagedAccount,
                   ->GetPolicyChecker()
                   .can_act_on_web());
 }
+#endif  // !BUILDFLAG(IS_CHROMEOS)
 
 IN_PROC_BROWSER_TEST_F(ActorPolicyCheckerBrowserTestWithManagedAccount,
                        GlicUserStatusChanged) {
@@ -503,15 +502,11 @@ IN_PROC_BROWSER_TEST_F(ActorPolicyCheckerBrowserTestWithManagedAccount,
 using ActorPolicyCheckerBrowserTestWithManagedAccountWithPolicy =
     ActorPolicyCheckerBrowserTestManagedBrowser;
 
-// TODO(crbug.com/460824365): Enable on ChromeOS.
-#if BUILDFLAG(IS_CHROMEOS)
-#define MAYBE_CapabilityUpdatedForAccount DISABLED_CapabilityUpdatedForAccount
-#else
-#define MAYBE_CapabilityUpdatedForAccount CapabilityUpdatedForAccount
-#endif
+// Note: sign-out from enterprise account is not allowed in ChromeOS.
+#if !BUILDFLAG(IS_CHROMEOS)
 IN_PROC_BROWSER_TEST_F(
     ActorPolicyCheckerBrowserTestWithManagedAccountWithPolicy,
-    MAYBE_CapabilityUpdatedForAccount) {
+    CapabilityUpdatedForAccount) {
   UpdateGeminiActOnWebPolicy(
       glic::prefs::GlicActuationOnWebPolicyState::kDisabled);
   EXPECT_FALSE(ActorKeyedService::Get(browser()->profile())
@@ -531,5 +526,6 @@ IN_PROC_BROWSER_TEST_F(
                   ->GetPolicyChecker()
                   .can_act_on_web());
 }
+#endif  // !BUILDFLAG(IS_CHROMEOS)
 
 }  // namespace actor
