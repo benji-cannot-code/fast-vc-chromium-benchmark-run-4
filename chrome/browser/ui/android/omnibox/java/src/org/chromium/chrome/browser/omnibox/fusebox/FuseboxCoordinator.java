c584d9e8521d5f0d74f4848c86c9eff9478fe34f
Tomasz Wiszkowski <ender@google.com>
2025-11-25 15:50:36 -0800

    Ensure Fusebox monitors for profile changes
    Change-Id: Ifb93cb8309bc9a6d007dd06b06c1b59fb066ddc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7205307
    Reviewed-by: Jian Li <jianli@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1550130}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxCoordinator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxCoordinator.java
@@ -14,9 +14,9 @@ import androidx.annotation.VisibleForTesting;
 import androidx.appcompat.content.res.AppCompatResources;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import org.chromium.base.Callback;
 import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.base.supplier.ObservableSupplierImpl;
-import org.chromium.base.supplier.OneShotCallback;
 import org.chromium.base.supplier.OneshotSupplier;
 import org.chromium.build.annotations.Initializer;
 import org.chromium.build.annotations.NullMarked;
@@ -62,6 +62,8 @@ public class FuseboxCoordinator implements UrlFocusChangeListener, TemplateUrlSe
     private TemplateUrlService mTemplateUrlService;
     private final ObservableSupplierImpl<Boolean> mOnCompactModeChangedSupplier =
             new ObservableSupplierImpl<>(false);
+    private final ObservableSupplier<Profile> mProfileSupplier;
+    private final Callback<Profile> mProfileObserver = this::onProfileAvailable;
 
     public FuseboxCoordinator(
             Context context,
@@ -75,6 +77,7 @@ public class FuseboxCoordinator implements UrlFocusChangeListener, TemplateUrlSe
                     autocompleteRequestTypeSupplier) {
         mContext = context;
         mWindowAndroid = windowAndroid;
+        mProfileSupplier = profileObservableSupplier;
         mTabModelSelectorSupplier = tabModelSelectorSupplier;
         mAutocompleteRequestTypeSupplier = autocompleteRequestTypeSupplier;
 
@@ -121,13 +124,16 @@ public class FuseboxCoordinator implements UrlFocusChangeListener, TemplateUrlSe
                                 OmniboxFeatures.sShowDedicatedModeButton.getValue())
                         .build();
         PropertyModelChangeProcessor.create(mModel, mViewHolder, FuseboxViewBinder::bind);
-        new OneShotCallback<>(profileObservableSupplier, this::onProfileAvailable);
+        mProfileSupplier.addObserver(mProfileObserver);
     }
 
     @VisibleForTesting
     void onProfileAvailable(Profile profile) {
         // Reset previous Mediator instance in case we migrate to continuous Profile observing.
-        mMediator = null;
+        if (mMediator != null) {
+            mMediator.destroy();
+            mMediator = null;
+        }
 
         mComposeBoxQueryControllerBridge = ComposeBoxQueryControllerBridge.getForProfile(profile);
         AutocompleteController.getForProfile(profile)
@@ -151,6 +157,7 @@ public class FuseboxCoordinator implements UrlFocusChangeListener, TemplateUrlSe
     }
 
     public void destroy() {
+        mProfileSupplier.removeObserver(mProfileObserver);
         if (mMediator != null) {
             mMediator.destroy();
             mMediator = null;
