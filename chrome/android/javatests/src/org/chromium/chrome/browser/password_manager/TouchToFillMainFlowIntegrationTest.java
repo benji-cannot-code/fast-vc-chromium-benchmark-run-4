508fb5610ef3dfe4757af55a98938cd52842377c
Piotr Kotynia <piotrkotynia@google.com>
2025-11-25 08:34:17 -0800

    Reland "[Android] Show Keyboard Accessory suggestions after scroll handling"
    This is a reland of commit 8fd1a706b57e662948549e85d1c29ba57d245968
    
    In addition to the original change, flag was disabled for
    AutofillKeyboardIntegrationTest as waitForKeyboardAccessoryToBeShown
    doesn't work correctly because of the crbug.com/462636368 bug.
    
    Original change's description:
    > [Android] Show Keyboard Accessory suggestions after scroll handling
    >
    > Currently, the Keyboard Accessory is triggered within
    > DidReceiveLeftMouseDownOrGestureTapInNode(), which executes before the
    > page handles scrolling (e.g., scrolling an input into view). This often
    > causes the accessory's position to be calculated using stale
    > coordinates, leading to collisions with the virtual keyboard or the
    > omnibox.
    >
    > This CL defers the suggestion trigger to
    > DidCompleteFocusChangeInFrame(). This method runs after scroll handling
    > is complete, ensuring that the accessory's dynamic positioning is
    > calculated based on the final, scrolled state of the page.
    >
    > This change is guarded by the
    > AutofillAndroidKeyboardAccessoryDynamicPositioning feature flag.
    >
    > Note: This change does not address page scrolling initiated by
    > JavaScript.
    >
    > Old behavior: http://shortn/_9wcmBmvqKA
    > New behavior: http://shortn/_0yiKSD3Knm
    >
    > Bug: 458644290
    > Change-Id: I350be5a39fa61f4dc7095bfeb11fce83d05c01e8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7170452
    > Reviewed-by: Christoph Schwering <schwering@google.com>
    > Commit-Queue: Piotr Kotynia <piotrkotynia@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1549130}
    
    Bug: 458644290
    Change-Id: I35fd5dbbe1d3909c704cdbee6c2f43a215be4410
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7202988
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Piotr Kotynia <piotrkotynia@google.com>
    Cr-Commit-Position: refs/heads/main@{#1549831}

--- chrome/android/javatests/src/org/chromium/chrome/browser/password_manager/TouchToFillMainFlowIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/password_manager/TouchToFillMainFlowIntegrationTest.java
@@ -24,6 +24,8 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.DoNotBatch;
+import org.chromium.base.test.util.Features.DisableFeatures;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.R;
@@ -111,8 +113,10 @@ public class TouchToFillMainFlowIntegrationTest {
         mSigninTestRule.tearDownRule();
     }
 
+    // TODO(crbug.com/462636368): Turn on the flag after blink bug is fixed.
     @Test
     @MediumTest
+    @DisableFeatures(ChromeFeatureList.AUTOFILL_ANDROID_KEYBOARD_ACCESSORY_DYNAMIC_POSITIONING)
     public void testClickingSuggestionPopulatesForm()
             throws TimeoutException, InterruptedException {
         // Fill the password store.
