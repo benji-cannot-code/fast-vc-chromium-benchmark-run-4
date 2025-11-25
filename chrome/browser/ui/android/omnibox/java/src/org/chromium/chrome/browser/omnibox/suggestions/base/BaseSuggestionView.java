5ce81fb72156a1e3e9b811a6bb4c18c4a1d283ef
Jian Li <jianli@chromium.org>
2025-11-24 16:19:33 -0800

    Select the action button view when it is navigated to via keyboard
    Bug: 462195669
    Change-Id: I1b572bc9e3c15db06ec4c8abcb80f7dc12f83287
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7197714
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Jian Li <jianli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1549464}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/BaseSuggestionView.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/BaseSuggestionView.java
@@ -13,6 +13,7 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.accessibility.AccessibilityEvent;
 import android.widget.ImageView;
 
 import androidx.annotation.LayoutRes;
@@ -126,13 +127,15 @@ public class BaseSuggestionView<T extends View> extends SuggestionLayout {
      * @param isSelected whether to apply hairline
      */
     private void highlightActionButton(int buttonIndex, boolean isHighlighted) {
-        mActionButtons
-                .get(buttonIndex)
-                .setForeground(
-                        isHighlighted
-                                ? AppCompatResources.getDrawable(
-                                        getContext(), R.drawable.hairline_circle)
-                                : null);
+        ActionButtonView actionButtonView = mActionButtons.get(buttonIndex);
+        actionButtonView.setForeground(
+                isHighlighted
+                        ? AppCompatResources.getDrawable(getContext(), R.drawable.hairline_circle)
+                        : null);
+        actionButtonView.setSelected(isHighlighted);
+        if (isHighlighted) {
+            actionButtonView.sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_SELECTED);
+        }
     }
 
     /**
