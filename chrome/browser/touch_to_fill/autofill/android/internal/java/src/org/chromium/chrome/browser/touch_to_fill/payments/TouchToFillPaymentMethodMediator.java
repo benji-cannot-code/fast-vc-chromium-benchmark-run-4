c4c6560f074123810bef7a564096752286312146
Yishui Liu <yishuil@google.com>
2025-11-25 10:51:38 -0800

    [BNPL][Clank] Log user action for ToS shown
    Adding logging for BNPL ToS bottom sheet shown on issuer level.
    
    Screencast:
    http://screencast/cast/NjIzMjQxMTc2NjUyMTg1Nnw5NzdkMjcyYS04MA
    
    Design doc: go/autofill-clank-bnpl
    
    Bug: b:438785863
    Change-Id: I2dcf678f263a0b40bf1e00c664a920bba30268c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7113404
    Reviewed-by: Viplav Kadam <viplavkadam@google.com>
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Commit-Queue: Yishui Liu <yishuil@google.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Reviewed-by: Olivia Saul <osaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1549930}

--- chrome/browser/touch_to_fill/autofill/android/internal/java/src/org/chromium/chrome/browser/touch_to_fill/payments/TouchToFillPaymentMethodMediator.java
+++ chrome/browser/touch_to_fill/autofill/android/internal/java/src/org/chromium/chrome/browser/touch_to_fill/payments/TouchToFillPaymentMethodMediator.java
@@ -315,6 +315,14 @@ class TouchToFillPaymentMethodMediator {
 
     @VisibleForTesting static final String ERROR_SCREEN_DISMISSED = ".ErrorScreen.Dismissed";
 
+    @VisibleForTesting static final String AFFIRM_TOS_SCREEN = ".AffirmTosScreen";
+
+    @VisibleForTesting static final String KLARNA_TOS_SCREEN = ".KlarnaTosScreen";
+
+    @VisibleForTesting static final String ZIP_TOS_SCREEN = ".ZipTosScreen";
+
+    @VisibleForTesting static final String SCREEN_SHOWN = ".Shown";
+
     // LINT.ThenChange(/tools/metrics/actions/actions.xml)
 
     // LINT.IfChange
@@ -331,6 +339,7 @@ class TouchToFillPaymentMethodMediator {
     private List<LoyaltyCard> mAffiliatedLoyaltyCards;
     private List<LoyaltyCard> mAllLoyaltyCards;
     private List<BnplIssuerContext> mBnplIssuerContexts;
+    private String mBnplIssuerIdWithTosShown;
     private Function<LoyaltyCard, Drawable> mValuableImageFunction;
     private BottomSheetFocusHelper mBottomSheetFocusHelper;
     private boolean mShouldShowScanCreditCard;
@@ -790,6 +799,9 @@ class TouchToFillPaymentMethodMediator {
         mModel.set(CURRENT_SCREEN, BNPL_ISSUER_TOS_SCREEN);
         mModel.set(SHEET_ITEMS, sheetItems);
         mModel.set(VISIBLE, true);
+
+        mBnplIssuerIdWithTosShown = bnplIssuerTosDetail.getIssuerId();
+        recordTouchToFillBnplTosUserAction();
     }
 
     void hideSheet() {
@@ -1359,6 +1371,28 @@ class TouchToFillPaymentMethodMediator {
                 // Nothing is recorded for all other issuerId's.
                 break;
         }
+    }
+
+    private void recordTouchToFillBnplTosUserAction() {
+        String tosUserAction;
+        switch (mBnplIssuerIdWithTosShown) {
+            case "affirm":
+                tosUserAction = AFFIRM_TOS_SCREEN;
+                break;
+            case "klarna":
+                tosUserAction = KLARNA_TOS_SCREEN;
+                break;
+            case "zip":
+                tosUserAction = ZIP_TOS_SCREEN;
+                break;
+            default:
+                // Nothing is recorded for all other issuerId's.
+                return;
+        }
+
+        tosUserAction += SCREEN_SHOWN;
+
+        recordTouchToFillBnplUserAction(tosUserAction);
     }
 
     private static void recordTouchToFillBnplUserAction(String userAction) {
