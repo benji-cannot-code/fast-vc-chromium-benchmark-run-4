0bfec499f5bacf342fef446de77fcaab2289f817
Aliona DANGLA <alionadangla@google.com>
2025-11-25 11:46:59 -0800

    [iOS] Prevent to attach the current tab if the limit is already reached
    See video:
    https://drive.google.com/file/d/12ytLyvJYZD2SvDvlRu6glDAQQq7xasU0/view?usp=drive_link
    
    Bug: 458059298
    Change-Id: I8974c266917890062182baccfe01c616e8a4b229
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7198540
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1549972}

--- ios/chrome/browser/composebox/coordinator/composebox_input_plate_coordinator.mm
+++ ios/chrome/browser/composebox/coordinator/composebox_input_plate_coordinator.mm
@@ -393,6 +393,10 @@ const CGFloat kSnackbarBottomMargin = 10;
   [_omniboxCoordinator clearSuggestionsAndRestartAutocomplete];
 }
 
+- (void)showAttachmentLimitError {
+  [self showMaxAttachmentSnackbarError];
+}
+
 #pragma mark - LocationBarURLLoader
 
 - (void)loadGURLFromLocationBar:(const GURL&)url
