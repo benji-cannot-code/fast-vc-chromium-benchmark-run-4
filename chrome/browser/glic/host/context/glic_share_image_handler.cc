0458ff5ae724f8c7abc049c2f357d257c93a90fc
Ian Vollick <vollick@chromium.org>
2025-11-24 16:08:21 -0800

    Create a new instance for GlicShareImage
    This is for the 'Create image with Gemini' context menu item. In multi-
    instance mode, we will close if detached and then force a new instance.
    
    credit: bryantchandler@ for proposing this approach and walking me
    through the implementation.
    
    Bug: b:462241968
    Change-Id: Ie64f5b75663a78be474534408b8fe8fda081e5da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7187361
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1549462}

--- chrome/browser/glic/host/context/glic_share_image_handler.cc
+++ chrome/browser/glic/host/context/glic_share_image_handler.cc
@@ -9,10 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/glic/fre/glic_fre_controller.h"
 #include "chrome/browser/glic/host/context/glic_page_context_fetcher.h"
 #include "chrome/browser/glic/public/glic_keyed_service.h"
+#include "chrome/browser/glic/widget/glic_window_controller.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_features.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/toasts/api/toast_id.h"
 #include "chrome/browser/ui/toasts/toast_controller.h"
+#include "chrome/common/chrome_features.h"
 #include "content/public/browser/render_frame_host.h"
 
 namespace glic {
@@ -210,7 +212,19 @@ void GlicShareImageHandler::OnReceivedTabContext(
   }
 
   auto* instance = service_->GetInstanceForTab(tab);
-  if (!instance || !instance->IsShowing()) {
+  if (base::FeatureList::IsEnabled(features::kGlicMultiInstance)) {
+    if (instance &&
+        instance->GetPanelState().kind == mojom::PanelStateKind::kDetached) {
+      CHECK(instance->IsShowing()) << ", should be showing if detached";
+      service_->CloseFloatingPanel();
+    }
+    // We always want to call ToggleUI for multi-instance to force a new
+    // instance to be created.
+    glic_panel_open_time_ = base::TimeTicks::Now();
+    // Note: if the FRE was showing, this will just cause it to be reshown.
+    service_->ToggleUI(browser, /*prevent_close=*/true,
+                       mojom::InvocationSource::kSharedImage);
+  } else if (!instance || !instance->IsShowing()) {
     glic_panel_open_time_ = base::TimeTicks::Now();
     // Note: if the FRE was showing, this will just cause it to be reshown.
     service_->ToggleUI(browser, /*prevent_close=*/true,
