45e625deeb63641dad2d4b7824fbc929ecae2bfd
Wenyu Fu <wenyufu@chromium.org>
2025-11-25 12:31:44 -0800

    [BrowserControls] Remove offset tags on animation start
    Remove the offset tags from toolbar and tab strip since the browser will
    drive their offsets and movement.
    
    Bug: 417238089
    Change-Id: I45b4bb0f09f64fc1f607c1649728b99537896586
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7204722
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Peilin Wang <peilinwang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1549997}

--- chrome/browser/ui/android/tabstrip/java/src/org/chromium/chrome/browser/tabstrip/TabStripTopControlLayer.java
+++ chrome/browser/ui/android/tabstrip/java/src/org/chromium/chrome/browser/tabstrip/TabStripTopControlLayer.java
@@ -186,6 +186,15 @@ public class TabStripTopControlLayer extends ObservableSupplierImpl<Integer>
         }
     }
 
+    @Override
+    public void prepForHeightAdjustmentAnimation(int latestYOffset) {
+        if (mTabStrip == null) return;
+        // This is called after onTransitionRequested, which triggers the change in visibility.
+        // The offset tags needs to be set here, since the tab strip do not want to drive the
+        // animation using the offset tags.
+        mTabStrip.updateOffsetTagsInfo(null);
+    }
+
     // Implements TabStripTransitionHandler
 
     @Override
@@ -211,7 +220,6 @@ public class TabStripTopControlLayer extends ObservableSupplierImpl<Integer>
                         newHeight,
                         applyScrimOverlay,
                         onHeightTransitionStartCallback);
-        mTabStrip.updateOffsetTagsInfo(null);
     }
 
     @EnsuresNonNullIf("mTransitionState")
