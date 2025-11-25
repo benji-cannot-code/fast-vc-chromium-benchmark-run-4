93648035eccf6556a5ad3a6f5f331ce41c83e44b
fikremen <fikremengistu@chromium.org>
2025-11-25 07:37:05 -0800

    Reland "Add HaTS to Bundled Security Settings page"
    This is a reland of commit 4952ce04691b3ebad30bdae198fa17d7896f727e
    
    The original change was failing because of an error in the expected
    client channel on chromeos devices in the
    HatsHandlerTest.HandleSecurityPageHatsRequest_PassesArgumentsToHatsService
    unit test.
    
    Original change's description:
    > Add HaTS to Bundled Security Settings page
    >
    > This change adds logic to trigger HaTS surveys from the new Security
    > Settings V2 page and refactors the existing security HaTS browser
    > proxy to:
    > - Track time on page and user actions on the bundled settings
    >     page, such as bundle radio button clicks and row expansions.
    > - Introduce and track the
    >     `SecuritySettingsBundleSetting` to capture the state of the security
    >     settings bundle when the page is opened.
    > - Update the `securityPageHatsRequest` WebUI API to accept
    >     new interactions and the initial security bundle setting.
    > - Remove the now-redundant HaTS trigger logic on the original security page
    >
    > 'NO_IFTTT=adding SecuritySettingsBundleSetting to a new file. No need to
    > change enum itself'
    >
    > Requirements Doc: go/bundled-security-settings-hats
    > Bug: 416284320
    >
    > Change-Id: I930a13ec1caba1881ee53031d29f513a299e0e03
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7153318
    > Reviewed-by: Dana Fried <dfried@chromium.org>
    > Commit-Queue: Fikre Mengistu <fikremengistu@chromium.org>
    > Reviewed-by: Martin Šrámek <msramek@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1548460}
    
    Bug: 416284320
    Change-Id: I62bdb5b2a4f861be3b0e98a2e9c351852146ca8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7201164
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Commit-Queue: Fikre Mengistu <fikremengistu@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1549782}

--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -1084,10 +1084,6 @@ const base::FeatureParam<std::string>
     kHappinessTrackingSurveysForSecurityPageTriggerId{
         &kHappinessTrackingSurveysForSecurityPage, "security-page-trigger-id",
         ""};
-const base::FeatureParam<bool>
-    kHappinessTrackingSurveysForSecurityPageRequireInteraction{
-        &kHappinessTrackingSurveysForSecurityPage,
-        "security-page-require-interaction", false};
 #endif  // !BUILDFLAG(IS_ANDROID)
 
 #if BUILDFLAG(IS_CHROMEOS)
--- chrome/test/data/webui/settings/settings_browsertest.cc
+++ chrome/test/data/webui/settings/settings_browsertest.cc
@@ -1568,12 +1568,6 @@ IN_PROC_BROWSER_TEST_F(SettingsSecurityPageTest, FlagsDisabled) {
   RunTest("settings/security_page_test.js", "runMochaSuite('FlagsDisabled')");
 }
 
-IN_PROC_BROWSER_TEST_F(SettingsSecurityPageTest,
-                       SecurityPageHappinessTrackingSurveys) {
-  RunTest("settings/security_page_test.js",
-          "runMochaSuite('SecurityPageHappinessTrackingSurveys')");
-}
-
 IN_PROC_BROWSER_TEST_F(SettingsSecurityPageTest, JavascriptOptimizer) {
   RunTest("settings/security_page_test.js",
           "runMochaSuite('JavascriptOptimizer')");
@@ -1595,6 +1589,12 @@ using SettingsSecurityPageV2Test = SettingsBrowserTest;
 
 IN_PROC_BROWSER_TEST_F(SettingsSecurityPageV2Test, Main) {
   RunTest("settings/security_page_v2_test.js", "runMochaSuite('Main')");
+}
+
+IN_PROC_BROWSER_TEST_F(SettingsSecurityPageV2Test,
+                       SecurityPageHappinessTrackingSurveys) {
+  RunTest("settings/security_page_v2_test.js",
+          "runMochaSuite('SecurityPageV2HappinessTrackingSurveys')");
 }
 
 #if !BUILDFLAG(IS_CHROMEOS)
