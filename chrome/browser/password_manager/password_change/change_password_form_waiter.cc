45bdfe423c3459996e20340448511ef77b4ff7ef
Viktor Semeniuk <vsemeniuk@google.com>
2025-11-25 09:17:35 -0800

    Start countdown for timeout only after model has been downloaded
    This CL fixes a bug when timeout callback was invoked before the model
    is downloaded and available.
    
    Bug: 452883239
    Change-Id: Id555e50fb8417a650b94b9a5fb214d854a934cf0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7203188
    Reviewed-by: Anna Tsvirchkova <atsvirchkova@google.com>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1549863}

--- chrome/browser/password_manager/password_change/change_password_form_waiter.cc
+++ chrome/browser/password_manager/password_change/change_password_form_waiter.cc
@@ -254,7 +254,7 @@ void ChangePasswordFormWaiter::DidStartLoading() {
 }
 
 void ChangePasswordFormWaiter::DidStopLoading() {
-  if (web_contents()->IsLoading()) {
+  if (web_contents()->IsLoading() || model_loaded_subscription_) {
     return;
   }
   timeout_timer_.Start(FROM_HERE, timeout_, this,
