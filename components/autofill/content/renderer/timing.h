f874102c24efaec4d6fb76c1f80977523ce3de0c
Christoph Schwering <schwering@google.com>
2025-11-25 09:28:30 -0800

    [Autofill] Notify AutofillAgent when DevTools is connected
    Before this CL, opening DevTools on an already-loaded page does not
    automatically emit form issues -- form issues are only emitted when
    AutofillAgent extracts all forms the next time.
    
    This CL informs AutofillAgent when DevTools is connected, so
    AutofillAgent can emit form issues.
    
    Bug: 40249826, 40178859, 461746798
    Change-Id: If28349b7efb62e6f4d7daa70b6bf41e4d0327ccc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7165016
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1549875}

--- components/autofill/content/renderer/timing.h
+++ components/autofill/content/renderer/timing.h
@@ -38,6 +38,7 @@ struct CallTimerState {
     kEmitFormIssuesToDevtools,
     kExtractForms,
     kExtractFormsAndNotifyPasswordAutofillAgent,
+    kOnDevToolsSessionConnectionChanged,
   };
   CallSite call_site = internal::IsRequired();
   base::TimeTicks last_autofill_agent_reset = internal::IsRequired();
