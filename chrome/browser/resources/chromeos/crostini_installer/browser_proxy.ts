9d4efeffcad3c3b1c0c1c1bb336e033d62dbf133
dpapad <dpapad@chromium.org>
2025-11-24 16:23:09 -0800

    CrOS: Migrate c/b/r/chromeos/crostini_installer to TypeScript.
    As part of the migration also had to switch from the old Polymer({...})
    syntax to "extends PolymerElement" which is required for TypeScript
    Polymer code.
    
    Bug: None
    Change-Id: Ia77762b154d119f28118070c5cf053945a259771
    Bypass-Check-License: Renamed from '.js' to '.ts'
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7188079
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1549466}

--- chrome/browser/resources/chromeos/crostini_installer/browser_proxy.ts
+++ chrome/browser/resources/chromeos/crostini_installer/browser_proxy.ts
@@ -4,30 +4,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {PageCallbackRouter, PageHandlerFactory, PageHandlerRemote} from './crostini_installer.mojom-webui.js';
+import type {PageHandlerInterface} from './crostini_installer.mojom-webui.js';
 
 export class BrowserProxy {
+  callbackRouter: PageCallbackRouter;
+  handler: PageHandlerInterface;
+
   constructor() {
-    /** @type {PageCallbackRouter} */
     this.callbackRouter = new PageCallbackRouter();
-    /** @type {PageHandlerRemote} */
     this.handler = new PageHandlerRemote();
 
     const factory = PageHandlerFactory.getRemote();
     factory.createPageHandler(
         this.callbackRouter.$.bindNewPipeAndPassRemote(),
-        this.handler.$.bindNewPipeAndPassReceiver());
+        (this.handler as PageHandlerRemote).$.bindNewPipeAndPassReceiver());
   }
 
-  /** @return {!BrowserProxy} */
-  static getInstance() {
+  static getInstance(): BrowserProxy {
     return instance || (instance = new BrowserProxy());
   }
 
-  /** @param {!BrowserProxy} obj */
-  static setInstance(obj) {
+  static setInstance(obj: BrowserProxy) {
     instance = obj;
   }
 }
 
-/** @type {?BrowserProxy} */
-let instance = null;
+let instance: BrowserProxy|null = null;
