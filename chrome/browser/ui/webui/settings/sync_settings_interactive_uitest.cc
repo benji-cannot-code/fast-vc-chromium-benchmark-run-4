b17f36cbec843702e143002238d23861c8308ed3
David Roger <droger@chromium.org>
2025-11-25 08:07:36 -0800

    [signin][test] Verify histograms in SyncSettingsInteractiveTest
    Bug: 457428660
    Change-Id: Ieb997827809404324b8bf8fa66b98f0cec8df83a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7201785
    Reviewed-by: Anthi Orfanou <anthie@google.com>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1549806}

--- chrome/browser/ui/webui/settings/sync_settings_interactive_uitest.cc
+++ chrome/browser/ui/webui/settings/sync_settings_interactive_uitest.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/test_future.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/signin/identity_test_environment_profile_adaptor.h"
@@ -169,6 +170,7 @@ IN_PROC_BROWSER_TEST_F(SyncSettingsInteractiveTest,
 // signing-in from the settings menu.
 IN_PROC_BROWSER_TEST_F(SyncSettingsInteractiveTest,
                        ShowHistorySyncOptinDialogFromSettingsSignin) {
+  base::HistogramTester histogram_tester;
   // Handle the Gaia signin page.
   embedded_test_server()->StartAcceptingConnections();
 
@@ -216,7 +218,11 @@ IN_PROC_BROWSER_TEST_F(SyncSettingsInteractiveTest,
                          UiElementHasAppeared(kHistoryOptinAcceptButton)),
       WaitForStateChange(kHistorySyncOptinDialogContentsId,
                          UiElementHasAppeared(kHistoryOptinRejectButton)));
-  // TODO(crbug.com/457428660): Add metrics checks once they are implemented.
+
+  histogram_tester.ExpectUniqueSample("Signin.HistorySyncOptIn.Started",
+                                      signin_metrics::AccessPoint::kSettings,
+                                      1);
+  histogram_tester.ExpectTotalCount("Signin.HistorySyncOptIn.Completed", 0);
 }
 
 // Tests that a signed in user on the web can trigger and see the History
