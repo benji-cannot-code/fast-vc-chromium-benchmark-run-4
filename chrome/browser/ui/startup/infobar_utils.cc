761c4ad7e0fc00da5db19291a80efd5987b309e6
Fiona Macintosh <fmacintosh@google.com>
2025-11-25 11:07:19 -0800

    [3PC] Clean up obsolete 3PCD infobar
    This infobar was implemented specifically for testing third-party cookie
    deprecation; given the cancelation of 3PCD it is no longer needed
    
    Bug: b:442876161
    Change-Id: If75c0b229ad9134817510ce3ae7585a47afb9d9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7177841
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Fiona Macintosh <fmacintosh@google.com>
    Reviewed-by: Dylan Cutler <dylancutler@google.com>
    Reviewed-by: Daniel Soromou <koretadaniel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1549942}

--- chrome/browser/ui/startup/infobar_utils.cc
+++ chrome/browser/ui/startup/infobar_utils.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/startup/obsolete_system_infobar_delegate.h"
 #include "chrome/browser/ui/startup/startup_browser_creator.h"
 #include "chrome/browser/ui/startup/startup_types.h"
-#include "chrome/browser/ui/startup/test_third_party_cookie_phaseout_infobar_delegate.h"
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
@@ -30,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "content/public/common/content_switches.h"
 #include "google_apis/google_api_keys.h"
-#include "services/network/public/cpp/network_switches.h"
 
 #if !BUILDFLAG(IS_CHROMEOS) && !BUILDFLAG(IS_ANDROID)
 #include "chrome/browser/ui/startup/default_browser_prompt/default_browser_prompt.h"
@@ -98,9 +96,6 @@ bool IsGpuTest() {
 }
 #endif
 
-BASE_FEATURE(kShowTestThirdPartyCookiePhaseoutInfoBar,
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 }  // namespace
 
 void AddInfoBarsIfNecessary(BrowserWindowInterface* browser,
@@ -136,13 +131,6 @@ void AddInfoBarsIfNecessary(BrowserWindowInterface* browser,
     if (base::CommandLine::ForCurrentProcess()->HasSwitch(
             switches::kProtectedAudiencesConsentedDebugToken)) {
       BiddingAndAuctionConsentedDebuggingDelegate::Create(web_contents);
-    }
-
-    if (base::CommandLine::ForCurrentProcess()->HasSwitch(
-            network::switches::kTestThirdPartyCookiePhaseout) &&
-        base::FeatureList::IsEnabled(
-            kShowTestThirdPartyCookiePhaseoutInfoBar)) {
-      TestThirdPartyCookiePhaseoutInfoBarDelegate::Create(web_contents);
     }
   }
 
