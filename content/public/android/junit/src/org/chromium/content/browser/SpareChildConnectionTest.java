a3362d855b39f5f810fb2280336a93331d2ca5e4
Jiacheng Guo <gjc@google.com>
2025-11-25 21:17:30 -0800

    Allow the spare renderer to use the warm-up connection on Android
    This CL sets the requested initial binding state of the warm-up
    connection after the process is started. This allows the webview apps to
    provide a prewarm connection to the created spare renderer.
    
    Bug: 455620851
    Change-Id: I329566d3ee8f551706c66bf34fd30cf948ab25fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7172259
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Jiacheng Guo <gjc@google.com>
    Reviewed-by: Ziad Youssef <ziadyoussef@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1550242}

--- content/public/android/junit/src/org/chromium/content/browser/SpareChildConnectionTest.java
+++ content/public/android/junit/src/org/chromium/content/browser/SpareChildConnectionTest.java
@@ -295,6 +295,7 @@ public class SpareChildConnectionTest {
                     mSpareConnection.getConnection(
                             mConnectionAllocator, mServiceCallback, testBindingState);
             assertNotNull(connection);
+            mTestConnectionFactory.simulateConnectionBindingSuccessfully();
             assertEquals(testBindingState, connection.bindingStateCurrent());
             if (testBindingState == ChildBindingState.WAIVED) {
                 assertEquals(0, connection.getStrongBindingCount());
