d669f4119fc1b49e1cbef1fe634b1c09a62b0dfe
Anders Hartvoll Ruud <andruud@chromium.org>
2025-11-26 11:11:25 -0800

    Store layer association in the RuleSet (@property)
    We currently set the CascadeLayer association directly on the rule
    as a side effect of RuleSet building. This approach does not work,
    as it may lead to rules that are associated with non-existent layers
    (see Issue 463698792); we need to store the layer association next to
    the rule in the RuleSet instead.
    
    Fixes DCHECK involving unconnected RuleSets and rebuilding of
    the cascade layer map (CreateUnconnectedRuleSets_LayeredPropertyRules,
    Issue 428664521).
    
    This is expected to slightly increase memory usage: although a rule
    is typically only stored in a single RuleSet, it usually also exists
    in at least one other container on ScopedStyleResolver. The assumption
    is that we have so few of these rules that the memory impact
    is acceptable.
    
    Issue: 463698792, 428664521
    Change-Id: Ica0404644599c0b64b5312ca352721b2fa3ae50a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7205948
    Reviewed-by: Steinar H Gunderson <sesse@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1550649}

--- third_party/blink/renderer/core/css/rule_set.cc
+++ third_party/blink/renderer/core/css/rule_set.cc
@@ -918,9 +918,10 @@ void RuleSet::AddKeyframesRule(StyleRuleKeyframes* rule,
   keyframes_rules_.push_back(CascadeLayered<StyleRuleKeyframes>(rule, layer));
 }
 
-void RuleSet::AddPropertyRule(StyleRuleProperty* rule) {
+void RuleSet::AddPropertyRule(StyleRuleProperty* rule,
+                              const CascadeLayer* layer) {
   need_compaction_ = true;
-  property_rules_.push_back(rule);
+  property_rules_.push_back(CascadeLayered<StyleRuleProperty>(rule, layer));
 }
 
 void RuleSet::AddCounterStyleRule(StyleRuleCounterStyle* rule) {
@@ -997,8 +998,7 @@ void RuleSet::AddChildRules(StyleRule* parent_rule,
     } else if (auto* keyframes_rule = DynamicTo<StyleRuleKeyframes>(rule)) {
       AddKeyframesRule(keyframes_rule, cascade_layer);
     } else if (auto* property_rule = DynamicTo<StyleRuleProperty>(rule)) {
-      property_rule->SetCascadeLayer(cascade_layer);
-      AddPropertyRule(property_rule);
+      AddPropertyRule(property_rule, cascade_layer);
     } else if (auto* counter_style_rule =
                    DynamicTo<StyleRuleCounterStyle>(rule)) {
       counter_style_rule->SetCascadeLayer(cascade_layer);
