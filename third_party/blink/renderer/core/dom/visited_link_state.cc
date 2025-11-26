d2b38e6b4603e1def7fd0a4c8af86f1b1b6c9299
Steinar H. Gunderson <sesse@chromium.org>
2025-11-26 04:53:02 -0800

    Remove VisitedLinkState histograms.
    We spend ~0.2% of Speedometer3 time just MD5-hashing the names of these
    histograms over and over again, and from discussing with the team, they
    are not that important. (If one uses the macros instead, they are
    pre-hashed, so that would be an alternative rewrite.)
    
    In addition to the Speedometer3 gains, we use 0.5–1.0% of the style
    perftest, depending on which page; pages with many links naturally
    hit this path more often.
    
    Change-Id: I828ae8e1984c5ab664928f3d2bf5969a4fa3fb09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7198304
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1550422}

--- third_party/blink/renderer/core/dom/visited_link_state.cc
+++ third_party/blink/renderer/core/dom/visited_link_state.cc
@@ -112,8 +112,7 @@ static inline LinkHash LinkHashForElement(
     const AtomicString& attribute = AtomicString()) {
   DCHECK(attribute.IsNull() || LinkAttribute(element) == attribute);
   return base::FeatureList::IsEnabled(
-                     blink::features::
-                         kPartitionVisitedLinkDatabaseWithSelfLinks)
+             blink::features::kPartitionVisitedLinkDatabaseWithSelfLinks)
              ? PartitionedLinkHashForElement(element, attribute)
              : UnpartitionedLinkHashForElement(element, attribute);
 }
@@ -216,22 +215,16 @@ EInsideLink VisitedLinkState::DetermineLinkStateSlowCase(
   // links can be tested in platform independent manner, without
   // explicit support in the test harness.
   if (attribute.empty()) {
-    base::UmaHistogramBoolean(
-        "Blink.History.VisitedLinks.IsLinkStyledAsVisited", true);
     return EInsideLink::kInsideVisitedLink;
   }
 
   if (LinkHash hash = LinkHashForElement(element, attribute)) {
     links_checked_for_visited_state_.insert(hash);
     if (Platform::Current()->IsLinkVisited(hash)) {
-      base::UmaHistogramBoolean(
-          "Blink.History.VisitedLinks.IsLinkStyledAsVisited", true);
       return EInsideLink::kInsideVisitedLink;
     }
   }
 
-  base::UmaHistogramBoolean("Blink.History.VisitedLinks.IsLinkStyledAsVisited",
-                            false);
   return EInsideLink::kInsideUnvisitedLink;
 }
 
