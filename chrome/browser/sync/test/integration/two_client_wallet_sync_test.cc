d40a4a15ff20d8902661328cf9bef461377169fc
Mikel Astiz <mastiz@chromium.org>
2025-11-26 10:08:04 -0800

    [SyncTests] Modernize SingleClientWalletSyncTest
    ...by parameterizing them to also test the upcoming transport-only mode.
    
    Generally no logical changes are required for this type, with the
    exception of tests that exercise the sync-paused state (replaced by the
    sign-in pending state). However, the helper functions need to be updated
    significantly to allow toggling which underlying store to use, which
    requires updating some additional files too (trivial changes).
    
    In one test, a secondary test expectation failed to hold true in
    transport mode. Fixing this seems complicated, so in this patch the
    expectation is relaxed with a TODO to reintroduce it in follow-up
    patches.
    
    Bug: 353425612
    Change-Id: I4e1e63280ef5b9fd6c8dc767e3554a1c37fcb681
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7201860
    Reviewed-by: Mahmoud Rashad <mmrashad@google.com>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1550598}

--- chrome/browser/sync/test/integration/two_client_wallet_sync_test.cc
+++ chrome/browser/sync/test/integration/two_client_wallet_sync_test.cc
@@ -52,6 +52,12 @@ class TwoClientWalletSyncTest : public SyncTest {
 
   ~TwoClientWalletSyncTest() override = default;
 
+  wallet_helper::StoreType GetStoreType() const {
+    return GetSetupSyncMode() == SyncTest::SetupSyncMode::kSyncTransportOnly
+               ? wallet_helper::StoreType::kAccountStore
+               : wallet_helper::StoreType::kProfileStore;
+  }
+
   // Needed for AwaitQuiescence().
   bool TestUsesSelfNotifications() override { return true; }
 
@@ -87,7 +93,7 @@ IN_PROC_BROWSER_TEST_F(TwoClientWalletSyncTest, UpdateCreditCardMetadata) {
   ASSERT_EQ(1u, card.usage_history().use_count());
   card.usage_history().set_use_count(2);
   card.usage_history().set_use_date(kLaterTime);
-  UpdateServerCardMetadata(0, card);
+  UpdateServerCardMetadata(0, card, GetStoreType());
 
   // Wait for the change to propagate.
   EXPECT_TRUE(AutofillWalletChecker(0, 1).Wait());
@@ -123,7 +129,7 @@ IN_PROC_BROWSER_TEST_F(TwoClientWalletSyncTest,
   ASSERT_EQ(1u, card.usage_history().use_count());
   card.usage_history().set_use_count(2);
   card.usage_history().set_use_date(kLaterTime);
-  UpdateServerCardMetadata(0, card);
+  UpdateServerCardMetadata(0, card, GetStoreType());
 
   // Simulate going online again.
   fake_server::FakeServerHttpPostProvider::EnableNetwork();
@@ -163,7 +169,7 @@ IN_PROC_BROWSER_TEST_F(TwoClientWalletSyncTest,
   ASSERT_EQ(1u, card.usage_history().use_count());
   card.usage_history().set_use_count(3);
   card.usage_history().set_use_date(kLaterTime);
-  UpdateServerCardMetadata(0, card);
+  UpdateServerCardMetadata(0, card, GetStoreType());
 
   credit_cards = GetServerCreditCards(1);
   ASSERT_EQ(1u, credit_cards.size());
@@ -171,7 +177,7 @@ IN_PROC_BROWSER_TEST_F(TwoClientWalletSyncTest,
   ASSERT_EQ(1u, card.usage_history().use_count());
   card.usage_history().set_use_count(2);
   card.usage_history().set_use_date(kEvenLaterTime);
-  UpdateServerCardMetadata(1, card);
+  UpdateServerCardMetadata(1, card, GetStoreType());
 
   // Simulate going online again.
   fake_server::FakeServerHttpPostProvider::EnableNetwork();
@@ -208,7 +214,7 @@ IN_PROC_BROWSER_TEST_F(TwoClientWalletSyncTest,
 
   // Update the billing address.
   card.set_billing_address_id(kDefaultBillingAddressID);
-  UpdateServerCardMetadata(0, card);
+  UpdateServerCardMetadata(0, card, GetStoreType());
   EXPECT_TRUE(AutofillWalletChecker(0, 1).Wait());
 
   // Make sure both clients have the updated billing_address_id.
@@ -237,7 +243,7 @@ IN_PROC_BROWSER_TEST_F(TwoClientWalletSyncTest,
   // Update the billing address.
   ASSERT_EQ(kDefaultBillingAddressID, card.billing_address_id());
   card.set_billing_address_id(kDifferentBillingAddressId);
-  UpdateServerCardMetadata(0, card);
+  UpdateServerCardMetadata(0, card, GetStoreType());
   EXPECT_TRUE(AutofillWalletChecker(0, 1).Wait());
 
   // Make sure both clients have the updated billing_address_id.
@@ -267,7 +273,7 @@ IN_PROC_BROWSER_TEST_F(
 
   // Update the billing address (replace a remote profile by a local profile).
   card.set_billing_address_id(kLocalBillingAddressId);
-  UpdateServerCardMetadata(0, card);
+  UpdateServerCardMetadata(0, card, GetStoreType());
   EXPECT_TRUE(AutofillWalletChecker(0, 1).Wait());
 
   // Make sure both clients have the updated billing_address_id (local profile
@@ -303,7 +309,7 @@ IN_PROC_BROWSER_TEST_F(
   // We treat the corner-case of merging data after initial sync (with
   // use_count==1) differently, set use-count to a higher value.
   card.usage_history().set_use_count(2);
-  UpdateServerCardMetadata(0, card);
+  UpdateServerCardMetadata(0, card, GetStoreType());
 
   credit_cards = GetServerCreditCards(1);
   ASSERT_EQ(1u, credit_cards.size());
@@ -314,7 +320,7 @@ IN_PROC_BROWSER_TEST_F(
   // We treat the corner-case of merging data after initial sync (with
   // use_count==1) differently, set use-count to a higher value.
   card.usage_history().set_use_count(2);
-  UpdateServerCardMetadata(1, card);
+  UpdateServerCardMetadata(1, card, GetStoreType());
 
   // Simulate going online again.
   fake_server::FakeServerHttpPostProvider::EnableNetwork();
--- chrome/browser/sync/test/integration/wallet_helper.h
+++ chrome/browser/sync/test/integration/wallet_helper.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "base/time/time.h"
 #include "chrome/browser/sync/test/integration/multi_client_status_change_checker.h"
-#include "chrome/browser/sync/test/integration/sync_test.h"
 #include "components/autofill/core/browser/data_manager/payments/payments_data_manager.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -44,6 +43,14 @@ inline constexpr char kDefaultBillingAddressID[] = "billing address entity ID";
 inline constexpr char kDefaultCreditCardCloudTokenDataID[] =
     "cloud token data ID";
 
+// Represents the two underlying instances of AutofillWebDataService, one for
+// the profile store (used when sync-the-feature is enabled) and the other one
+// for the account store (used otherwise).
+enum class StoreType {
+  kProfileStore,
+  kAccountStore,
+};
+
 // Used to access the `PaymentsDataManager` for a particular sync profile.
 [[nodiscard]] autofill::PaymentsDataManager* GetPaymentsDataManager(int index);
 
@@ -61,40 +68,49 @@ GetProfileWebDataService(int index);
 [[nodiscard]] scoped_refptr<autofill::AutofillWebDataService>
 GetAccountWebDataService(int index);
 
-void SetServerCreditCards(
+// Same as above but returns the instance corresponding to `store_type`.
+[[nodiscard]] scoped_refptr<autofill::AutofillWebDataService> GetWebDataService(
     int profile,
-    const std::vector<autofill::CreditCard>& credit_cards);
+    StoreType store_type);
+
+void SetServerCreditCards(int profile,
+                          const std::vector<autofill::CreditCard>& credit_cards,
+                          StoreType store_type);
 
 void SetPaymentsCustomerData(
     int profile,
-    const autofill::PaymentsCustomerData& customer_data);
+    const autofill::PaymentsCustomerData& customer_data,
+    StoreType store_type);
 
 void SetCreditCardCloudTokenData(
     int profile,
-    const std::vector<autofill::CreditCardCloudTokenData>& cloud_token_data);
+    const std::vector<autofill::CreditCardCloudTokenData>& cloud_token_data,
+    StoreType store_type);
 
 void SetServerCardCredentialData(int profile,
-                                 const autofill::CreditCard& credit_card);
+                                 const autofill::CreditCard& credit_card,
+                                 StoreType store_type);
 
 void RemoveServerCardCredentialData(int profile,
-                                    const autofill::CreditCard& credit_card);
+                                    const autofill::CreditCard& credit_card,
+                                    StoreType store_type);
 
 void UpdateServerCardCredentialData(int profile,
-                                    const autofill::CreditCard& credit_card);
+                                    const autofill::CreditCard& credit_card,
+                                    StoreType store_type);
 
 void UpdateServerCardMetadata(int profile,
-                              const autofill::CreditCard& credit_card);
+                              const autofill::CreditCard& credit_card,
+                              StoreType store_type);
 
-std::vector<autofill::PaymentsMetadata> GetServerCardsMetadata(int profile);
+std::vector<autofill::PaymentsMetadata> GetServerCardsMetadata(
+    int profile,
+    StoreType store_type);
 
 // Function supports AUTOFILL_WALLET_DATA and AUTOFILL_WALLET_OFFER.
-// TODO(crbug.com/353425612): The default value for setup_sync_mode should be
-// removed once all callers are updated.
-sync_pb::DataTypeState GetWalletDataTypeState(
-    syncer::DataType type,
-    int profile,
-    SyncTest::SetupSyncMode setup_sync_mode =
-        SyncTest::SetupSyncMode::kSyncTheFeature);
+sync_pb::DataTypeState GetWalletDataTypeState(syncer::DataType type,
+                                              int profile,
+                                              StoreType store_type);
 
 sync_pb::SyncEntity CreateDefaultSyncWalletCard();
 
@@ -153,28 +169,6 @@ class AutofillWalletChecker : public StatusChangeChecker,
 
   const int profile_a_;
   const int profile_b_;
-};
-
-// Checker to block until autofill wallet metadata sizes match on both profiles.
-class AutofillWalletMetadataSizeChecker
-    : public StatusChangeChecker,
-      public autofill::PaymentsDataManager::Observer {
- public:
-  AutofillWalletMetadataSizeChecker(int profile_a, int profile_b);
-  ~AutofillWalletMetadataSizeChecker() override;
-
-  // StatusChangeChecker implementation.
-  bool IsExitConditionSatisfied(std::ostream* os) override;
-
-  // autofill::PaymentsDataManager::Observer implementation.
-  void OnPaymentsDataChanged() override;
-
- private:
-  bool IsExitConditionSatisfiedImpl();
-
-  const int profile_a_;
-  const int profile_b_;
-  bool checking_exit_condition_in_flight_ = false;
 };
 
 // Checker to block until a new progress marker with correct timestamp is
