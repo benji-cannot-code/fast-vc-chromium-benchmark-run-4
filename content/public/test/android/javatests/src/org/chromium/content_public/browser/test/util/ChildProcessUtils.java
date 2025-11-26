cfb2f42a57a2ecf676cb7084838a6edb9fe980f2
Ziad Youssef <ziadyoussef@chromium.org>
2025-11-26 08:32:26 -0800

    Add test to check spare renderer reuse existing child connection
    Test: autoninja -C out/Default webview_instrumentation_test_apk &&  out/Default/bin/run_webview_instrumentation_test_apk -f SpareRendererTest#testChildConnectionUsedBySpareRenderer
    
    Bug: 455620851
    Change-Id: I85d7763c1a71a8d2f6feb5bbcdbf67926e81a748
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7172290
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Ziad Youssef <ziadyoussef@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1550533}

---
+++ content/public/test/android/javatests/src/org/chromium/content_public/browser/test/util/ChildProcessUtils.java
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.content_public.browser.test.util;
+
+import org.chromium.content.browser.ChildProcessLauncherHelperImpl;
+import org.chromium.content.browser.LauncherThread;
+
+import java.util.concurrent.FutureTask;
+
+// Utility class for testing child process related behavior.
+public class ChildProcessUtils {
+    private ChildProcessUtils() {}
+
+    // Returns the number of sandboxed connection currently connected.
+    public static int getConnectedSandboxedServicesCount() {
+        try {
+            FutureTask<Integer> task =
+                    new FutureTask<>(
+                            () ->
+                                    ChildProcessLauncherHelperImpl
+                                            .getConnectedSandboxedServicesCountForTesting());
+            LauncherThread.post(task);
+            return task.get();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
