4990d560b457550c9a7bae2b2aedeaa64be0e637
Adam Abeshouse <aabeshouse@google.com>
2025-11-26 12:24:50 -0800

    Show JS optimizations bubble from omnibar icon
    Clicking on the omnibar optimizations icon now opens
    a bubble that allows the user to enable v8 optimizations
    (except when v8 optimizations are disabled due to an
    enterprise policy, in which case the bubble doesn't
    have a button).
    
    Browser tests and screenshot tests will be completed in later CLs (crbug.com/462425975).
    
    Note: a new browsing instance has to be started (e.g.
    a new tab) in order for the updated content setting to
    take effect. In future work, we may seek to have this
    take effect on refresh. (crbug.com/457420369).
    
    Bug: 458711797
    Change-Id: I5f19c2024dfeeb64cf1861b1304f552948d1c86c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7152304
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Adam Abeshouse <aabeshouse@google.com>
    Cr-Commit-Position: refs/heads/main@{#1550685}

--- chrome/browser/ui/browser_actions.cc
+++ chrome/browser/ui/browser_actions.cc
@@ -67,6 +67,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/file_system_access/file_system_access_bubble_controller.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/toolbar_button_provider.h"
+#include "chrome/browser/ui/views/js_optimization/js_optimizations_page_action_controller.h"
 #include "chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_page_action_controller.h"
 #include "chrome/browser/ui/views/location_bar/lens_overlay_homework_page_action_controller.h"
 #include "chrome/browser/ui/views/media_router/cast_browser_controller.h"
@@ -405,7 +406,16 @@ void BrowserActions::InitializeBrowserActions() {
             base::BindRepeating(
                 [](BrowserWindowInterface* bwi, actions::ActionItem* item,
                    actions::ActionInvocationContext context) {
-                  // TODO(crbug.com/458711797): Show bubble on click.
+                  views::View* anchor_view =
+                      bwi->GetBrowserForMigrationOnly()
+                          ->GetBrowserView()
+                          .toolbar_button_provider()
+                          ->GetAnchorView(kActionShowJsOptimizationsIcon);
+
+                  bwi->GetActiveTabInterface()
+                      ->GetTabFeatures()
+                      ->js_optimizations_page_action_controller()
+                      ->ShowBubble(anchor_view, item);
                 },
                 bwi))
             .SetActionId(kActionShowJsOptimizationsIcon)
--- chrome/browser/ui/views/js_optimization/js_optimizations_page_action_controller.h
+++ chrome/browser/ui/views/js_optimization/js_optimizations_page_action_controller.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_JS_OPTIMIZATION_JS_OPTIMIZATIONS_PAGE_ACTION_CONTROLLER_H_
 #define CHROME_BROWSER_UI_VIEWS_JS_OPTIMIZATION_JS_OPTIMIZATIONS_PAGE_ACTION_CONTROLLER_H_
 
+#include "base/memory/raw_ptr.h"
 #include "chrome/browser/ui/tabs/contents_observing_tab_feature.h"
 
 namespace page_actions {
@@ -16,6 +17,16 @@ namespace tabs {
 class TabInterface;
 }  // namespace tabs
 
+namespace views {
+class BubbleDialogModelHost;
+class View;
+}  // namespace views
+
+namespace actions {
+class ActionItem;
+}
+
+// Controls the visibility of the JS optimizations omnibar icon and bubble.
 class JsOptimizationsPageActionController
     : public tabs::ContentsObservingTabFeature {
  public:
@@ -28,13 +39,23 @@ class JsOptimizationsPageActionController
   JsOptimizationsPageActionController& operator=(
       const JsOptimizationsPageActionController&) = delete;
 
+  DECLARE_CLASS_ELEMENT_IDENTIFIER_VALUE(kBubbleBodyElementId);
+  DECLARE_CLASS_ELEMENT_IDENTIFIER_VALUE(kBubbleButtonElementId);
+
   // tabs::ContentsObservingTabFeature
   void PrimaryPageChanged(content::Page& page) override;
 
+  void ShowBubble(views::View* anchor_view, actions::ActionItem* item);
+
  private:
   void UpdateIconVisibility();
+  void OnBubbleHidden(actions::ActionItem* action_item);
+  views::BubbleDialogModelHost* CreateBubble(views::View* anchor_view,
+                                             actions::ActionItem* action_item);
+  void EnableV8Optimizations();
 
   const raw_ref<page_actions::PageActionController> page_action_controller_;
+  raw_ptr<views::BubbleDialogModelHost> bubble_ = nullptr;
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_JS_OPTIMIZATION_JS_OPTIMIZATIONS_PAGE_ACTION_CONTROLLER_H_
