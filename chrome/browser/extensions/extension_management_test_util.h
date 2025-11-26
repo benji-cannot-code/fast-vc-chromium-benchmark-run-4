e8858b8b6b5a20afa2a8f6c7a4193d397ef7f29f
Dzmitry Radchuk <radchuk@google.com>
2025-11-26 01:23:12 -0800

    Don't sync policy-enforced extension uninstalls.
    This change modifies ExtensionSyncService to ignore uninstalls triggered
    by the UNINSTALL_REASON_INTERNAL_MANAGEMENT reason. Test utilities are updated to allow simulating policy-based removal, and new unit and browser tests are added to verify that policy-driven uninstalls are not synced.
    
    
    Bug: 421167524
    Change-Id: Ica72ee2ba63aae2e8a5d3c0bec1680ff951296de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7168575
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Dzmitry Radchuk <radchuk@google.com>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1550331}

--- chrome/browser/extensions/extension_management_test_util.h
+++ chrome/browser/extensions/extension_management_test_util.h
@@ -47,6 +47,7 @@ class ExtensionManagementPrefUpdaterBase {
   void SetIndividualExtensionAutoInstalled(const ExtensionId& id,
                                            const std::string& update_url,
                                            bool forced);
+  void SetIndividualExtensionRemoved(const ExtensionId& id);
 
   // Helper functions for 'install_sources' manipulation.
   void UnsetInstallSources();
--- chrome/browser/extensions/extension_service_test_with_install.h
+++ chrome/browser/extensions/extension_service_test_with_install.h
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/scoped_observation.h"
 #include "chrome/browser/extensions/extension_service_user_test_base.h"
+#include "components/sync/model/sync_data.h"
+#include "components/sync/test/fake_sync_change_processor.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/browser/extension_registry_observer.h"
 #include "extensions/buildflags/buildflags.h"
@@ -139,7 +141,9 @@ class ExtensionServiceTestWithInstall : public ExtensionServiceUserTestBase,
   // files according to `delete_type`.
   void UninstallExtension(
       const std::string& id,
-      UninstallExtensionFileDeleteType delete_type = kDeleteAllVersions);
+      UninstallExtensionFileDeleteType delete_type = kDeleteAllVersions,
+      extensions::UninstallReason reason =
+          extensions::UNINSTALL_REASON_FOR_TESTING);
 
   void TerminateExtension(const std::string& id);
 
@@ -181,6 +185,42 @@ class ExtensionServiceTestWithInstall : public ExtensionServiceUserTestBase,
 
   base::ScopedObservation<ExtensionRegistry, ExtensionRegistryObserver>
       registry_observation_{this};
+};
+
+// This is a FakeSyncChangeProcessor specialization that maintains a store of
+// SyncData items, treating it like a map keyed by the extension id from the
+// SyncData. Each instance of this class should only be used for one data type
+// (which should be either extensions or apps) to match how the real sync
+// system handles things.
+class StatefulChangeProcessor : public syncer::FakeSyncChangeProcessor {
+ public:
+  explicit StatefulChangeProcessor(syncer::DataType expected_type);
+
+  StatefulChangeProcessor(const StatefulChangeProcessor&) = delete;
+  StatefulChangeProcessor& operator=(const StatefulChangeProcessor&) = delete;
+
+  ~StatefulChangeProcessor() override;
+
+  // We let our parent class, FakeSyncChangeProcessor, handle saving the
+  // changes for us, but in addition we "apply" these changes by treating
+  // the FakeSyncChangeProcessor's SyncDataList as a map keyed by extension
+  // id.
+  std::optional<syncer::ModelError> ProcessSyncChanges(
+      const base::Location& from_here,
+      const syncer::SyncChangeList& change_list) override;
+
+  // This is a helper which offers a wrapped version of this object suitable for
+  // passing in to MergeDataAndStartSyncing, which takes a
+  // std::unique_ptr<SyncChangeProcessor>, since in tests we typically don't
+  // want to give up ownership of a local change processor.
+  std::unique_ptr<syncer::SyncChangeProcessor> GetWrapped();
+
+  const syncer::SyncDataList& data() const { return data_; }
+
+ private:
+  // The expected DataType of changes that this processor will see.
+  const syncer::DataType expected_type_;
+  syncer::SyncDataList data_;
 };
 
 }  // namespace extensions
