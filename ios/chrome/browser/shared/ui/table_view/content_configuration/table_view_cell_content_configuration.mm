19b5125b38d746b4d6b2ffde53d6198cf4ec11e7
Gauthier Ambard <gambard@chromium.org>
2025-11-26 02:49:00 -0800

    [iOS] Remove table view from infobarmodal
    The infobar modals are using a TableView to display their content, but
    there is no reason for it.
    
    This CL replaces it with a stack view and ContentConfigurations.
    
    Before:
    https://screenshot.googleplex.com/AEME3ZxkBikVs2S
    https://screenshot.googleplex.com/DNA9TVpovWwxUEv
    https://screenshot.googleplex.com/BqAvuAZEke5tttV
    
    After:
    https://screenshot.googleplex.com/A53TS9G48dMZgGC
    https://screenshot.googleplex.com/93rR3SFEdfuMU89
    https://screenshot.googleplex.com/8LZTQCBnXGm2HMu
    
    BYPASS_RECITATION_REASON=This is a rewrite of existing tests. Yes it was done with the help of Gemini but it is really how I would have done it manually. I don't know how to modify it to avoid the recitation checker.
    
    Bypass-Check-License: file moved
    Bug: 443034511
    Change-Id: Iaa849ee33e09c3060013cf698e9a771f3ba36fb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7184360
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1550386}

--- ios/chrome/browser/shared/ui/table_view/content_configuration/table_view_cell_content_configuration.mm
+++ ios/chrome/browser/shared/ui/table_view/content_configuration/table_view_cell_content_configuration.mm
@@ -59,6 +59,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return cell;
 }
 
+- (UIView*)makeAccessibilityConfiguredContentView {
+  UIView* contentView = [self makeContentView];
+  contentView.isAccessibilityElement = YES;
+  contentView.accessibilityLabel = [self accessibilityLabel];
+  contentView.accessibilityValue = [self accessibilityValue];
+  contentView.accessibilityHint = [self accessibilityHint];
+  contentView.accessibilityUserInputLabels =
+      [self accessibilityUserInputLabels];
+  return contentView;
+}
+
 #pragma mark - UIContentConfiguration
 
 - (UIView<UIContentView>*)makeContentView {
