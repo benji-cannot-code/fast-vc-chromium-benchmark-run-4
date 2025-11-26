4f1283e10e8c4561f78a3f52f556b6b5540c352b
Abe Boujane <boujane@google.com>
2025-11-26 07:06:04 -0800

    [Actor] Move HandoffButtonController ownership to ActorUiContentsContainerController
    Refactors the ownership of HandoffButtonController from
    ActorUiTabController to ActorUiContentsContainerController. This change
    is necessary for other immersive mode blocked work. It decouples the
    button's lifetime from the tab controller and simplifies its
    initialization. Lifetime handling is now managed via
    base::ScopedClosureRunner for cleaner registration and unregisteration.
    
    Key changes:
    - ActorUiContentsContainerController now owns HandoffButtonController.
    - HandoffButtonController registers itself with ActorUiTabController.
    - Removed dependency on TabDialogManager; widget parenting is now direct.
    - Removed ActorUiTabControllerFactory::CreateHandoffButtonController.
    - Updated unit tests to reflect ownership and registration changes.
    
    Bug: 447624564
    Change-Id: I1b218e6df19f58937304d0ddeabcdfd6ac82de86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7204424
    Commit-Queue: Abe Boujane <boujane@google.com>
    Auto-Submit: Abe Boujane <boujane@google.com>
    Reviewed-by: Fiona Macintosh <fmacintosh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1550480}

--- chrome/browser/actor/ui/handoff_button_controller.h
+++ chrome/browser/actor/ui/handoff_button_controller.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string_view>
 
+#include "base/functional/callback_helpers.h"
 #include "base/memory/raw_ref.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/actor/ui/states/handoff_button_state.h"
@@ -24,7 +25,6 @@ class WidgetDelegate;
 
 namespace tabs {
 class TabInterface;
-class TabDialogManager;
 }  // namespace tabs
 
 namespace ui {
@@ -56,7 +56,7 @@ class HandoffButtonWidget : public views::Widget {
 
 class HandoffButtonController : public views::ViewObserver {
  public:
-  explicit HandoffButtonController(tabs::TabInterface& tab_interface);
+  explicit HandoffButtonController(views::View* anchor_view);
   ~HandoffButtonController() override;
 
   DECLARE_CLASS_ELEMENT_IDENTIFIER_VALUE(kHandoffButtonElementId);
@@ -64,7 +64,7 @@ class HandoffButtonController : public views::ViewObserver {
   HandoffButtonController(const HandoffButtonController&) = delete;
   HandoffButtonController& operator=(const HandoffButtonController&) = delete;
 
-  virtual void UpdateState(const HandoffButtonState& state,
+  virtual void UpdateState(HandoffButtonState state,
                            bool is_visible,
                            base::OnceClosure callback);
   // Returns true if the mouse is currently hovering over the handoff button.
@@ -72,10 +72,16 @@ class HandoffButtonController : public views::ViewObserver {
   // Returns true if the Handoff Button View is focused.
   virtual bool IsFocused();
 
+  base::WeakPtr<HandoffButtonController> GetWeakPtr();
+
+  // Registers the current tab interface.
+  [[nodiscard]] base::ScopedClosureRunner RegisterTabInterface(
+      tabs::TabInterface* tab_interface);
+
  protected:
+  void UnregisterTabInterface();
   void OnButtonPressed();
-  void ShouldShowButton(bool& show);
-  gfx::Rect GetHandoffButtonBounds(views::Widget* widget);
+  gfx::Rect GetHandoffButtonBounds();
   void UpdateButtonHoverStatus(bool is_hovered);
   // views::ViewObserver:
   void OnViewFocused(views::View* observed_view) override;
@@ -92,10 +98,7 @@ class HandoffButtonController : public views::ViewObserver {
   virtual void CloseButton(views::Widget::ClosedReason reason);
   virtual ActorUiTabControllerInterface* GetTabController();
   virtual void UpdateBounds();
-  virtual void UpdateVisibility();
   void OnWidgetDestroying(views::Widget::ClosedReason reason);
-
-  tabs::TabDialogManager* GetTabDialogManager();
 
   bool is_visible_ = false;
   bool is_hovering_ = false;
@@ -104,7 +107,9 @@ class HandoffButtonController : public views::ViewObserver {
       this};
   HandoffButtonState::ControlOwnership ownership_ =
       HandoffButtonState::ControlOwnership::kActor;
-  const raw_ref<tabs::TabInterface> tab_interface_;
+
+  raw_ptr<views::View> anchor_view_ = nullptr;
+  raw_ptr<tabs::TabInterface> tab_interface_ = nullptr;
 
   base::WeakPtrFactory<HandoffButtonController> weak_ptr_factory_{this};
 };
--- chrome/browser/actor/ui/mocks/mock_handoff_button_controller.cc
+++ chrome/browser/actor/ui/mocks/mock_handoff_button_controller.cc
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace actor::ui {
 
 MockHandoffButtonController::MockHandoffButtonController(
-    tabs::TabInterface& tab_interface)
-    : HandoffButtonController(tab_interface) {}
+    views::View* anchor_view)
+    : HandoffButtonController(anchor_view) {}
 MockHandoffButtonController::~MockHandoffButtonController() = default;
 
 }  // namespace actor::ui
