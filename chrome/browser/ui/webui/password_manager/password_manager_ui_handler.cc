fbefd1ab34d719c5441468aa33b52a7e28c06515
Andrii Natiahlyi <natiahlyi@google.com>
2025-11-26 06:31:25 -0800

    Migrate passwordsPrivate.setAccountStorageEnabled to a Mojo interface
    Bug: 432409279
    Change-Id: Ie07ad6134e7b86e6657ceaf39d4409d74c545355
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7201252
    Reviewed-by: Adem Derinel <derinel@google.com>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Andrii Natiahlyi <natiahlyi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1550466}

--- chrome/browser/ui/webui/password_manager/password_manager_ui_handler.cc
+++ chrome/browser/ui/webui/password_manager/password_manager_ui_handler.cc
@@ -91,3 +91,7 @@ void PasswordManagerUIHandler::IsAccountStorageEnabled(
   bool result = passwords_private_delegate_->IsAccountStorageEnabled();
   std::move(callback).Run(result);
 }
+
+void PasswordManagerUIHandler::SetAccountStorageEnabled(bool enabled) {
+  passwords_private_delegate_->SetAccountStorageEnabled(enabled, web_contents_);
+}
