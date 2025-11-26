d600e6541197415b36efcef7663281f5c3f69df9
Alexandra Pereira <alexandrasp@google.com>
2025-11-26 12:40:58 -0800

    [iOS][autofill] getIframeElements as util function
    Remove getIframeElements from gCrWebLegacy. This function has been
    converted to be a utility function and is now exported from
    form_utils.ts for shared use.
    
    Bug: 448990422
    Change-Id: Ic1440631737055bf7982c6edd4884bf09cf71213
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7205031
    Commit-Queue: Alexandra Pereira <alexandrasp@google.com>
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1550693}

---
+++ components/autofill/ios/form_util/resources/form_util_tests.ts
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {CrWebApi, gCrWeb} from '//ios/web/public/js_messaging/resources/gcrweb.js';
+import {getIframeElements} from '//components/autofill/ios/form_util/resources/form_utils.js';
+
+/**
+* @fileoverview Registers a testing-only `CrWebApi` to expose form utils
+* functions to native-side tests.
+*/
+
+const formApi = new CrWebApi();
+
+// go/keep-sorted start block=yes
+formApi.addFunction('getIframeElements', getIframeElements);
+// go/keep-sorted end
+
+gCrWeb.registerApi('form_test_api', formApi);
