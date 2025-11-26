e1077070802a96d5c2d3e066b593f79bc91fd621
Vincent Boisselle <vincb@google.com>
2025-11-26 13:03:49 -0800

    [ios] Fix proactive pwd generation sheet background hang
    Do not refocus on the field if the sheet is dismissed when going in
    the background.
    
    Add actions so we can get more precise breadcrumbs of when the
    hang happens.
    
    Also fix the dropdown metric so the usage of the proactive bottom
    sheet is also reported.
    
    Bug: 40746470, 441363369
    Change-Id: I2ef3a70fa0c7e24b21df4c599daf28626a8a9393
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7200364
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1550710}

--- ios/chrome/browser/passwords/ui_bundled/password_suggestion_coordinator.mm
+++ ios/chrome/browser/passwords/ui_bundled/password_suggestion_coordinator.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/check.h"
 #import "base/memory/weak_ptr.h"
 #import "base/metrics/histogram_functions.h"
+#import "base/metrics/user_metrics.h"
 #import "base/notreached.h"
 #import "base/strings/sys_string_conversions.h"
 #import "components/autofill/core/common/password_generation_util.h"
@@ -34,6 +35,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 constexpr CGFloat preferredCornerRadius = 20;
+constexpr char kUmaActionPrefix[] =
+    "IOS.PasswordManager.PasswordGenerationSheet";
 }  // namespace
 
 @interface PasswordSuggestionCoordinator () <
@@ -106,6 +109,7 @@ constexpr CGFloat preferredCornerRadius = 20;
   // the keyboard. This issue does not happen on mobile devices.
   // For more information, please see: https://www.crbug.com/1307759.
   if (ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_TABLET) {
+    [self recordAction:"CloseKeyboard"];
     [self closeKeyboard];
   }
 
@@ -131,6 +135,8 @@ constexpr CGFloat preferredCornerRadius = 20;
   if (!self.viewController.presentingViewController) {
     [self.delegate closePasswordSuggestion];
   }
+
+  [self recordAction:"Present"];
 }
 
 - (void)stop {
@@ -143,13 +149,12 @@ constexpr CGFloat preferredCornerRadius = 20;
 #pragma mark - ConfirmationAlertActionHandler
 
 - (void)confirmationAlertSecondaryAction {
-  [self handleDecision:NO];
-  [self incrementDismissCount];
-  [self refocusIfNeeded];
-  [self.delegate closePasswordSuggestion];
+  [self recordAction:"DismissWithButton"];
+  [self dismissWithRefocus:YES];
 }
 
 - (void)confirmationAlertPrimaryAction {
+  [self recordAction:"Accept"];
   [self handleDecision:YES];
   [self.delegate closePasswordSuggestion];
 }
@@ -158,19 +163,37 @@ constexpr CGFloat preferredCornerRadius = 20;
 
 - (void)presentationControllerDidDismiss:
     (UIPresentationController*)presentationController {
-  [self handleDecision:NO];
-  [self incrementDismissCount];
-  [self refocusIfNeeded];
-  [self.delegate closePasswordSuggestion];
+  [self recordAction:"DismissWithSwipe"];
+  [self dismissWithRefocus:YES];
 }
 
 #pragma mark - Notification callback
 
 - (void)applicationDidEnterBackground:(NSNotification*)notification {
-  [self confirmationAlertSecondaryAction];
+  [self recordAction:"DismissWithBackground"];
+  [self dismissWithRefocus:NO];
 }
 
 #pragma mark - Private methods
+
+- (void)dismissWithRefocus:(BOOL)refocus {
+  [self handleDecision:NO];
+  [self incrementDismissCount];
+  if (refocus) {
+    [self refocusIfNeeded];
+  }
+  [self.delegate closePasswordSuggestion];
+}
+
+- (void)recordAction:(std::string_view)name {
+  std::string_view sheetTypeFragment = _proactive ? ".Proactive." : ".";
+  base::RecordAction(base::UserMetricsAction(base::StrCat({
+                                                              kUmaActionPrefix,
+                                                              sheetTypeFragment,
+                                                              name,
+                                                          })
+                                                 .c_str()));
+}
 
 // Returns the user email.
 - (NSString*)userEmail {
@@ -340,7 +363,7 @@ constexpr CGFloat preferredCornerRadius = 20;
 }
 
 // Refocuses the field that was blurred to show the payments suggestion
-// bottom sheet, if deemded needed.
+// bottom sheet, if deemed needed.
 - (void)refocusIfNeeded {
   if (!base::FeatureList::IsEnabled(
           password_manager::features::
@@ -356,6 +379,7 @@ constexpr CGFloat preferredCornerRadius = 20;
   if (AutofillBottomSheetTabHelper* tabHelper =
           AutofillBottomSheetTabHelper::FromWebState(webState);
       tabHelper && _frame) {
+    [self recordAction:"Refocus"];
     tabHelper->RefocusElementIfNeeded(_frame->GetFrameId());
   }
 }
