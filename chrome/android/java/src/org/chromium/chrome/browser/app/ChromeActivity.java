a3840e76311be73d766547efd1932a5693aa827b
michalhoffmann <michalhoffmann@google.com>
2025-11-26 05:17:09 -0800

    Remove deprecated histograms
    This CL removes the histograms associated with bugs 40882352 and
    40882351 from the codebase and histograms.xml.
    
    OBSOLETE_HISTOGRAMS=Histograms expired and are no longer needed.
    
    Fixed: 40882352, 40882351
    Change-Id: I0beb4784411f159fa28119959b9045c35406f875
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7206227
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Michał Hoffmann <michalhoffmann@google.com>
    Commit-Queue: Michał Hoffmann <michalhoffmann@google.com>
    Reviewed-by: Kamil Jarosz <kjarosz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1550433}

--- chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
@@ -2640,10 +2640,6 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
             RecordUserAction.record("MobileMenuHistory");
             HistoryManagerUtils.showHistoryManager(
                     this, currentTab, getTabModelSelector().getCurrentModel().getProfile());
-            RecordHistogram.recordEnumeratedHistogram(
-                    "Android.OpenHistoryFromMenu.PerProfileType",
-                    type,
-                    BrowserProfileType.MAX_VALUE);
             return true;
         }
 
