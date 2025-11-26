557c2eeeab9e5dc6cdf934a73dc130b9065b6d28
Andrey Kosyakov <caseq@chromium.org>
2025-11-25 16:41:57 -0800

    Rewrite DevToolsTest.NewWindowFromBrowserContext as a headless test
    Bug: 40689291, 454825274
    
    Change-Id: I4fa176fd12e7d0fb9f2f89b1c463ecb8c580cc90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7204291
    Reviewed-by: Peter Kvitek <kvitekp@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1550153}

---
+++ components/headless/test/data/protocol/shared/window-with-new-context.js
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function(testRunner) {
+  const {dp} = await testRunner.startBlank(
+      'Tests Target.createTarget with new browser context');
+
+  const {browserContextId} = await dp.Target.createBrowserContext();
+  const {targetId} = await dp.Target.createTarget({
+    url: 'data:text/html,<!DOCTYPE html>',
+    browserContextId,
+    newWindow: true,
+  });
+  const {result} = await dp.Target.attachToTarget({targetId, flatten: true});
+  await dp.Target.disposeBrowserContext({browserContextId});
+  testRunner.log('Test ran to completion');
+  testRunner.completeTest();
+});
