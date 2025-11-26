b4c0561b8d357babb1d8942109100d512d36b3a6
Hayato Ito <hayato@chromium.org>
2025-11-25 19:32:14 -0800

    Add UMA histograms for HttpStreamRequest-related delays in Prefetch
    Add the following UMAs for Prefetch:
    
    - Prefetch.PrefetchContainer.CreateStreamDelay
    - Prefetch.PrefetchContainer.ConnectedCallbackDelay
    - Prefetch.PrefetchContainer.InitializeStreamDelay
    
    Change-Id: I3f87bf48f15f2eb80df83f9fc1ac3638d136d360
    Bug: 462314089
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7171754
    Reviewed-by: Taiyo Mizuhashi <taiyo@chromium.org>
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1550220}

--- content/browser/preloading/preload_serving_metrics.cc
+++ content/browser/preloading/preload_serving_metrics.cc
@@ -208,6 +208,11 @@ PrefetchContainerMetrics::PrefetchContainerMetrics() = default;
 
 PrefetchContainerMetrics::~PrefetchContainerMetrics() = default;
 
+PrefetchContainerMetrics::PrefetchContainerMetrics(
+    const PrefetchContainerMetrics&) = default;
+PrefetchContainerMetrics& PrefetchContainerMetrics::operator=(
+    const PrefetchContainerMetrics&) = default;
+
 PrefetchMatchPrefetchAheadOfPrerenderDebugMetrics::
     PrefetchMatchPrefetchAheadOfPrerenderDebugMetrics()
     :  // It will be filled just after ctor, but `PrefetchMatchResolverAction`
