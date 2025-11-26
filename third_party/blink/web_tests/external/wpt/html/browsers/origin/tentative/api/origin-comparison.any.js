ff47536fe276780b5a0527a4c2f5bae240386537
Mike West <mkwst@chromium.org>
2025-11-26 06:39:13 -0800

    [Origin API] Drop string parsers beyond `.from(...)`.
    Earlier versions of this API parsed strings into `Origin` objects via
    the constructor, `fromURL()` and `parse()`. We've since coalesced all of
    these into `Origin.from(...)` after discussion in [1].
    
    This CL removes these variants from our implementation, and adjusts
    tests accordingly.
    
    [1]: https://github.com/whatwg/html/pull/11846
    
    Bug: 434131026
    Change-Id: I8be1e1a0923b2cd48dcc7a0f80358237ecc9b88c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7204253
    Commit-Queue: Mike West <mkwst@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1550470}

--- third_party/blink/web_tests/external/wpt/html/browsers/origin/tentative/api/origin-comparison.any.js
+++ third_party/blink/web_tests/external/wpt/html/browsers/origin/tentative/api/origin-comparison.any.js
@@ -2,8 +2,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: title=`Origin` comparison
 
 test(t => {
-  const opaqueA = new Origin("null");
-  const opaqueB = new Origin("null");
+  const opaqueA = new Origin();
+  const opaqueB = new Origin();
+  assert_true(opaqueA.opaque);
+  assert_true(opaqueB.opaque);
 
   assert_true(opaqueA.isSameOrigin(opaqueA), "Opaque origin should be same-origin with itself.");
   assert_true(opaqueA.isSameSite(opaqueA), "Opaque origin should be same-site with itself.");
@@ -12,11 +14,11 @@ test(t => {
 }, "Comparison of opaque origins.");
 
 test(t => {
-  const a = new Origin("https://a.example");
-  const a_a = new Origin("https://a.a.example");
-  const b_a = new Origin("https://b.a.example");
-  const b = new Origin("https://b.example");
-  const b_b = new Origin("https://b.b.example");
+  const a = Origin.from("https://a.example");
+  const a_a = Origin.from("https://a.a.example");
+  const b_a = Origin.from("https://b.a.example");
+  const b = Origin.from("https://b.example");
+  const b_b = Origin.from("https://b.b.example");
 
   assert_true(a.isSameOrigin(a), "Origin should be same-origin with itself.");
   assert_false(a.isSameOrigin(a_a), "Origins with different subdomains should not be same-origin.");
@@ -38,8 +40,8 @@ test(t => {
 }, "Comparison of tuple origins.");
 
 test(t => {
-  const http = new Origin("http://a.example");
-  const https = new Origin("https://a.example");
+  const http = Origin.from("http://a.example");
+  const https = Origin.from("https://a.example");
 
   assert_false(http.isSameOrigin(https), "http is not same-site with https");
   assert_false(https.isSameOrigin(http), "https is not same-site with http");
