c2974a2bc783cb0caaeaf7a44dfe51f65d23db5c
Kristi Saney <kristislee@google.com>
2025-11-26 12:57:18 -0800

    [Reading mode] Log the distilled word count when opened with the omnibox
    Bug: 462249711
    Change-Id: Ib927f2853aa3e04b0b05d207819eba4322406287
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7206331
    Auto-Submit: Kristi Saney <kristislee@google.com>
    Reviewed-by: Evan Liu <evliu@google.com>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Shari Trewin <trewin@google.com>
    Commit-Queue: Shari Trewin <trewin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1550705}

--- chrome/browser/ui/webui/side_panel/read_anything/read_anything_untrusted_page_handler_browsertest.cc
+++ chrome/browser/ui/webui/side_panel/read_anything/read_anything_untrusted_page_handler_browsertest.cc
@@ -1507,13 +1507,16 @@ IN_PROC_BROWSER_TEST_F(
   base::HistogramTester histogram_tester;
   handler_ = CreateHandler();
   auto status = read_anything::mojom::DistillationStatus::kSuccess;
+  int word_count = 3001;
   SidePanelOpenTrigger trigger = SidePanelOpenTrigger::kReadAnythingOmniboxChip;
   Activate(true, &trigger);
 
-  handler_->OnDistillationStatus(status);
+  handler_->OnDistillationStatus(status, word_count);
 
   histogram_tester.ExpectUniqueSample(
       "Accessibility.ReadAnything.DistillationStatusAfterOmnibox", status, 1);
+  histogram_tester.ExpectUniqueSample(
+      "Accessibility.ReadAnything.WordsDistilledAfterOmnibox", word_count, 1);
 }
 
 IN_PROC_BROWSER_TEST_F(
@@ -1522,13 +1525,16 @@ IN_PROC_BROWSER_TEST_F(
   base::HistogramTester histogram_tester;
   handler_ = CreateHandler();
   auto status = read_anything::mojom::DistillationStatus::kSuccess;
+  int word_count = 3002;
   SidePanelOpenTrigger trigger = SidePanelOpenTrigger::kReadAnythingContextMenu;
   Activate(true, &trigger);
 
-  handler_->OnDistillationStatus(status);
+  handler_->OnDistillationStatus(status, word_count);
 
   histogram_tester.ExpectTotalCount(
       "Accessibility.ReadAnything.DistillationStatusAfterOmnibox", 0);
+  histogram_tester.ExpectTotalCount(
+      "Accessibility.ReadAnything.WordsDistilledAfterOmnibox", 0);
 }
 
 IN_PROC_BROWSER_TEST_F(
@@ -1538,14 +1544,18 @@ IN_PROC_BROWSER_TEST_F(
   handler_ = CreateHandler();
   auto status1 = read_anything::mojom::DistillationStatus::kSuccess;
   auto status2 = read_anything::mojom::DistillationStatus::kFailure;
+  int word_count1 = 3003;
+  int word_count2 = 3004;
   SidePanelOpenTrigger trigger = SidePanelOpenTrigger::kReadAnythingOmniboxChip;
   Activate(true, &trigger);
 
-  handler_->OnDistillationStatus(status1);
-  handler_->OnDistillationStatus(status2);
+  handler_->OnDistillationStatus(status1, word_count1);
+  handler_->OnDistillationStatus(status2, word_count2);
 
   histogram_tester.ExpectUniqueSample(
       "Accessibility.ReadAnything.DistillationStatusAfterOmnibox", status1, 1);
+  histogram_tester.ExpectUniqueSample(
+      "Accessibility.ReadAnything.WordsDistilledAfterOmnibox", word_count1, 1);
 }
 
 IN_PROC_BROWSER_TEST_F(ReadAnythingUntrustedPageHandlerTest,
@@ -1553,14 +1563,17 @@ IN_PROC_BROWSER_TEST_F(ReadAnythingUntrustedPageHandlerTest,
   base::HistogramTester histogram_tester;
   handler_ = CreateHandler();
   auto status = read_anything::mojom::DistillationStatus::kSuccess;
+  int word_count = 3005;
   SidePanelOpenTrigger trigger = SidePanelOpenTrigger::kReadAnythingOmniboxChip;
   Activate(true, &trigger);
 
   Activate(false);
-  handler_->OnDistillationStatus(status);
+  handler_->OnDistillationStatus(status, word_count);
 
   histogram_tester.ExpectUniqueSample(
       "Accessibility.ReadAnything.DistillationStatusAfterOmnibox", status, 1);
+  histogram_tester.ExpectUniqueSample(
+      "Accessibility.ReadAnything.WordsDistilledAfterOmnibox", word_count, 1);
 }
 
 IN_PROC_BROWSER_TEST_F(
@@ -1569,16 +1582,19 @@ IN_PROC_BROWSER_TEST_F(
   base::HistogramTester histogram_tester;
   handler_ = CreateHandler();
   auto status = read_anything::mojom::DistillationStatus::kSuccess;
+  int word_count = 3006;
   SidePanelOpenTrigger trigger = SidePanelOpenTrigger::kReadAnythingOmniboxChip;
   Activate(true, &trigger);
 
-  handler_->OnDistillationStatus(status);
+  handler_->OnDistillationStatus(status, word_count);
 
   Activate(false);
-  handler_->OnDistillationStatus(status);
+  handler_->OnDistillationStatus(status, word_count);
 
   histogram_tester.ExpectUniqueSample(
       "Accessibility.ReadAnything.DistillationStatusAfterOmnibox", status, 1);
+  histogram_tester.ExpectUniqueSample(
+      "Accessibility.ReadAnything.WordsDistilledAfterOmnibox", word_count, 1);
 }
 
 IN_PROC_BROWSER_TEST_F(ReadAnythingUntrustedPageHandlerTest,
--- chrome/renderer/accessibility/read_anything/read_anything_app_controller.cc
+++ chrome/renderer/accessibility/read_anything/read_anything_app_controller.cc
@@ -727,7 +727,8 @@ void ReadAnythingAppController::RecordDistillationSuccess() {
     distillationStatus = read_anything::mojom::DistillationStatus::kFailure;
   }
 
-  page_handler_->OnDistillationStatus(distillationStatus);
+  page_handler_->OnDistillationStatus(distillationStatus,
+                                      model_.words_distilled());
   ukm::builders::Accessibility_ReadAnything_Distillation(
       model_.GetUkmSourceId())
       .SetDistillationStatus(static_cast<int>(distillationStatus))
@@ -1211,6 +1212,7 @@ gin::ObjectTemplateBuilder ReadAnythingAppController::GetObjectTemplateBuilder(
                  &ReadAnythingAppController::OnSelectionChange)
       .SetMethod("onCollapseSelection",
                  &ReadAnythingAppController::OnCollapseSelection)
+      .SetMethod("onDistilled", &ReadAnythingAppController::OnDistilled)
       .SetProperty("supportedFonts",
                    &ReadAnythingAppController::GetSupportedFonts)
       .SetProperty("allFonts", &ReadAnythingAppController::GetAllFonts)
@@ -1821,6 +1823,10 @@ void ReadAnythingAppController::OnCopy() const {
 
 void ReadAnythingAppController::OnNoTextContent() {
   Distill();
+}
+
+void ReadAnythingAppController::OnDistilled(int word_count) {
+  model_.set_words_distilled(word_count);
 }
 
 void ReadAnythingAppController::UpdateWordsSeen(int words_seen) {
