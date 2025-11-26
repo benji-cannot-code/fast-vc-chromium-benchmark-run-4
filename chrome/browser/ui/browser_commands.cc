2de51b13d131f0c13f569904b28a6408c4ce1054
Alison Gale <agale@chromium.org>
2025-11-26 11:29:33 -0800

    [SxS] Use incognito NTP when creating a split in incognito mode
    Bug: 463905578
    Change-Id: Iad7879f39da6485531a43791bcaddf89d5734b7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7207754
    Commit-Queue: Alison Gale <agale@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1550657}

--- chrome/browser/ui/browser_commands.cc
+++ chrome/browser/ui/browser_commands.cc
@@ -126,6 +126,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/content_restriction.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
+#include "chrome/common/webui_url_constants.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/bookmarks/browser/bookmark_model.h"
 #include "components/bookmarks/browser/bookmark_node.h"
@@ -1438,8 +1439,13 @@ void NewSplitTab(BrowserWindowInterface* browser,
                  split_tabs::SplitTabCreatedSource source) {
   TabStripModel* const tab_strip_model = browser->GetTabStripModel();
   const int active_index = tab_strip_model->active_index();
+  // In Incognito mode, we can't show the regular Split View NTP so default to
+  // the regular NTP which renders special content when in Incognito.
+  const char* new_tab_url = browser->GetProfile()->IsIncognitoProfile()
+                                ? chrome::kChromeUINewTabURL
+                                : chrome::kChromeUISplitViewNewTabPageURL;
   tab_strip_model->delegate()->AddTabAt(
-      GURL(chrome::kChromeUISplitViewNewTabPageURL), active_index + 1, true,
+      GURL(new_tab_url), active_index + 1, true,
       tab_strip_model->GetTabGroupForTab(active_index),
       tab_strip_model->IsTabPinned(active_index));
   tab_strip_model->AddToNewSplit({active_index},
