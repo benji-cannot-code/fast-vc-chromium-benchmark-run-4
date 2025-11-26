f319f2cf8c2acdbb298d4d76f2aa12b1a9e1f4e8
Lingqi Chi <lingqi@chromium.org>
2025-11-25 17:22:15 -0800

    Revert "[TBHSP] Update SidePanelAnimationCoordinator's Observer API"
    This reverts commit 0eda25f638d92e3fe8c5598516539bee3cdb086c.
    
    Reason for revert: failing on win-asan
    https://ci.chromium.org/ui/p/chromium/builders/ci/win-asan/37877/overview
    
    Original change's description:
    > [TBHSP] Update SidePanelAnimationCoordinator's Observer API
    >
    > This CL updates the SidePanelAnimationCoordinator's Observer API to be
    > more granular in the types of updates it can notify its observers.
    >
    > API Changes:
    > - Added OnAnimationTypeStarted and OnAnimationTypeEnded
    >
    > The OnAnimationType* functions were added to separate observers who care
    > about specific animation sequences, from those that care about the type
    > of animation being performed. These additions will allow observers the
    > ability to perform prerequisite or cleanup tasks before and after the
    > main animation has completed. For example, at the end of an animation
    > you may want to reparent a specific view, or set some state before the
    > animation starts.
    >
    > Additionally, OnAnimationType functions can only be subscribed to by
    > using the animation type AddObserver/RemoveObserver functions. This
    > allows further separation by allowing observers to listen to either
    > animation sequence progress updates, or animation type updates, or both.
    >
    > OnAnimationSequenceEnded was not implemented as the naming suggests. It
    > was effectively telling observers that the main animation timeline had
    > ended and not the individual sequences. This function has been updated
    > to notify observers when a specific sequence tied to an animation id has
    > finished. This can happen at any point between the start and end of the
    > main animation timeline.
    >
    > Tests has been added to verify the new behavior.
    >
    > Change-Id: I931e5e5dc6d9085e7b63e61eeacc945e4078664b
    > Bug: 445452247
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7180522
    > Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    > Commit-Queue: Darryl James <dljames@chromium.org>
    > Reviewed-by: Caroline Rising <corising@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1549973}
    
    Bug: 445452247
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ifa6b1b46b851a85f53c1ca77fc60c75d5c9e86ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7206406
    Auto-Submit: Lingqi Chi <lingqi@chromium.org>
    Owners-Override: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1550170}

--- chrome/browser/ui/views/frame/shadow_overlay_view.h
+++ chrome/browser/ui/views/frame/shadow_overlay_view.h
@@ -19,11 +19,10 @@ class SidePanel;
 
 // This view is responsible for framing the primary elements of the UI when
 // toolbar height side panel is showing, providing a nice drop shadow.
-class ShadowOverlayView
-    : public views::View,
-      public views::LayoutDelegate,
-      public SidePanelAnimationCoordinator::AnimationIdObserver,
-      public views::ViewObserver {
+class ShadowOverlayView : public views::View,
+                          public views::LayoutDelegate,
+                          public SidePanelAnimationCoordinator::Observer,
+                          public views::ViewObserver {
   METADATA_HEADER(ShadowOverlayView, views::View)
 
  public:
@@ -43,7 +42,7 @@ class ShadowOverlayView
   views::ProposedLayout CalculateProposedLayout(
       const views::SizeBounds& size_bounds) const override;
 
-  // SidePanelAnimationCoordinator::AnimationIdObserver
+  // SidePanelAnimationCoordinator::Observer
   void OnAnimationSequenceProgressed(
       const SidePanelAnimationCoordinator::SidePanelAnimationId& animation_id,
       double animation_value) override;
