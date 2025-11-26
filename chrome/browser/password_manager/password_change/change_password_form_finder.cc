d68ed2ab8548736687c9c1acda6e52a5f5c33dd6
Anna Tsvirchkova <atsvirchkova@google.com>
2025-11-26 09:05:55 -0800

    [APC] Introduce feature flag for using improved open form step
    This CL introduces the kUseActionablesOpenFormStepForImprovedPasswordChange feature flag.
    Depending on whether it's on or off, it captures actionable or default annotated page content for the open form step.
    
    Bug: 439790313
    Change-Id: I8d9908fd276ba3b6c4eaacc51de24db85f501b39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7207409
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1550559}

--- chrome/browser/password_manager/password_change/change_password_form_finder.cc
+++ chrome/browser/password_manager/password_change/change_password_form_finder.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/barrier_closure.h"
 #include "base/check.h"
+#include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/strings/utf_string_conversions.h"
@@ -20,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/core/model_quality/model_execution_logging_wrappers.h"
 #include "components/optimization_guide/proto/features/password_change_submission.pb.h"
 #include "components/password_manager/core/browser/browser_save_password_progress_logger.h"
+#include "components/password_manager/core/browser/features/password_features.h"
 #include "components/password_manager/core/browser/password_form_manager.h"
 #include "components/password_manager/core/browser/password_manager_client.h"
 #include "content/public/browser/web_contents.h"
@@ -37,9 +39,14 @@ blink::mojom::AIPageContentOptionsPtr GetAIPageContentOptions() {
   // WebContents where password change is happening is hidden, and renderer
   // won't capture a snapshot unless it becomes visible again or
   // on_critical_path is set to true.
-  auto options = optimization_guide::DefaultAIPageContentOptions(
+  if (base::FeatureList::IsEnabled(
+          password_manager::features::
+              kUseActionablesForImprovedPasswordChange)) {
+    return optimization_guide::ActionableAIPageContentOptions(
+        /*on_critical_path =*/true);
+  }
+  return optimization_guide::DefaultAIPageContentOptions(
       /*on_critical_path =*/true);
-  return options;
 }
 
 std::unique_ptr<Logger> GetLoggerIfAvailable(
