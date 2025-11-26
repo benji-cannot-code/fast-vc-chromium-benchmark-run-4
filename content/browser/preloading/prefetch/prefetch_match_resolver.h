182f9a85a52fbaacf34cd9b787b007b94450db89
kenoss <kenoss@chromium.org>
2025-11-26 08:36:31 -0800

    Prefetch: Fix enum value of kNotServedNoCandidates
    Before this patch, the enum values of
    `kNotServedOnDeterminedHeadWithNotServableUnknown` and
    `kNotServedNoCandidates` of `PrefetchPotentialCandidateServingResult`
    are conflicted. This patch remedies it.
    
    NO_IFTTT=//tools/metrics/histograms/metadata/prefetch/enums.xml is correct.
    
    Bug: 342089123, 360094997, 455448933
    Change-Id: I25f0fd65d8cf7e18116c40a13f9e42898a0353a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7206550
    Auto-Submit: Ken Okada <kenoss@chromium.org>
    Reviewed-by: Taiyo Mizuhashi <taiyo@chromium.org>
    Commit-Queue: Taiyo Mizuhashi <taiyo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1550538}

--- content/browser/preloading/prefetch/prefetch_match_resolver.h
+++ content/browser/preloading/prefetch/prefetch_match_resolver.h
@@ -100,7 +100,7 @@ enum class PrefetchPotentialCandidateServingResult {
   kNotServedOnDeterminedHeadWithNotServableUnknown = 13,
 
   // A special value for `PrefetchMatchResolver::UnblockForNoCandidates()`.
-  kNotServedNoCandidates = 13,
+  kNotServedNoCandidates = 14,
 
   kMaxValue = kNotServedNoCandidates,
 };
