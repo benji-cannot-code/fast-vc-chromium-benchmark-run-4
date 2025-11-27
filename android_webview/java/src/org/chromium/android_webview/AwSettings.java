e2fd963e10dd767d8d9f3c39b0ea928108080611
Md Shahadat Hossain Shahin <shahinmd@google.com>
2025-11-27 09:26:57 -0800

    Integrate Android password echo split settings with web preferences
    Password echoing is the process of making the last character typed in a
    password field momentarily visible before it is replaced by a mask. The
    core android setting that controls this feature ("Show passwords") is
    being split, introducing two separate settings for physical keyboard
    input and virtual (touch) keyboard input.
    
    This CL integrates the new web preferences (introduced in
    crrev.com/c/7186349) with Android's password echo split settings. This
    integration is controlled by an Aconfig flag. When the flag is disabled,
    the new web preferences will reflect the legacy setting value. When the
    flag is enabled, the new web preferences will instead mirror the
    corresponding state of the new, split password echo settings.
    
    Bug: 463580423
    Change-Id: Iac3dee7f8c02df9ee59e9e7a2f915fe8f8e76360
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7123959
    Reviewed-by: Ashley Newson <ashleynewson@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Md Shahadat Hossain Shahin <shahinmd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1551165}

--- android_webview/java/src/org/chromium/android_webview/AwSettings.java
+++ android_webview/java/src/org/chromium/android_webview/AwSettings.java
@@ -16,7 +16,6 @@ import android.os.Build;
 import android.os.Handler;
 import android.os.Message;
 import android.os.Process;
-import android.provider.Settings;
 import android.webkit.WebSettings;
 
 import androidx.annotation.IntDef;
@@ -45,6 +44,7 @@ import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.metrics.RecordHistogram;
+import org.chromium.components.embedder_support.util.PasswordEchoSettingState;
 import org.chromium.components.webauthn.WebauthnMode;
 import org.chromium.components.webauthn.WebauthnModeProvider;
 import org.chromium.content_public.browser.WebContents;
@@ -236,7 +236,8 @@ public class AwSettings {
     private final boolean mAllowGeolocationOnInsecureOrigins;
     private final boolean mDoNotUpdateSelectionOnMutatingSelectionRange;
 
-    private boolean mPasswordEchoEnabled;
+    private boolean mPasswordEchoEnabledPhysical;
+    private boolean mPasswordEchoEnabledTouch;
 
     // Not accessed by the native side.
     private boolean mBlockSpecialFileUrls;
@@ -411,12 +412,9 @@ public class AwSettings {
                             .hasSystemFeature(PackageManager.FEATURE_TOUCHSCREEN);
 
             // Respect the system setting for password echoing.
-            mPasswordEchoEnabled =
-                    Settings.System.getInt(
-                                    context.getContentResolver(),
-                                    Settings.System.TEXT_SHOW_PASSWORD,
-                                    1)
-                            == 1;
+            final PasswordEchoSettingState state = PasswordEchoSettingState.getInstance();
+            mPasswordEchoEnabledPhysical = state.getPasswordEchoEnabledPhysical();
+            mPasswordEchoEnabledTouch = state.getPasswordEchoEnabledTouch();
 
             // By default, scale the text size by the system font scale factor. Embedders
             // may override this by invoking setTextZoom().
@@ -1552,15 +1550,24 @@ public class AwSettings {
     }
 
     @CalledByNative
-    private boolean getPasswordEchoEnabledLocked() {
+    private boolean getPasswordEchoEnabledPhysicalLocked() {
         assert Thread.holdsLock(mAwSettingsLock);
-        return mPasswordEchoEnabled;
+        return mPasswordEchoEnabledPhysical;
     }
 
-    public void setPasswordEchoEnabled(boolean enabled) {
+    @CalledByNative
+    private boolean getPasswordEchoEnabledTouchLocked() {
+        assert Thread.holdsLock(mAwSettingsLock);
+        return mPasswordEchoEnabledTouch;
+    }
+
+    public void setPasswordEchoEnabled(
+            boolean physicalSettingEnabled, boolean touchSettingEnabled) {
         synchronized (mAwSettingsLock) {
-            if (mPasswordEchoEnabled != enabled) {
-                mPasswordEchoEnabled = enabled;
+            if (mPasswordEchoEnabledPhysical != physicalSettingEnabled
+                    || mPasswordEchoEnabledTouch != touchSettingEnabled) {
+                mPasswordEchoEnabledPhysical = physicalSettingEnabled;
+                mPasswordEchoEnabledTouch = touchSettingEnabled;
                 mEventHandler.updateWebkitPreferencesLocked();
             }
         }
---
+++ components/embedder_support/android/java/src/org/chromium/components/embedder_support/util/PasswordEchoSettingObserver.java
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.embedder_support.util;
+
+import org.chromium.build.annotations.NullMarked;
+
+/** Interface for the classes that need to be notified of password echo setting state changes. */
+@NullMarked
+public interface PasswordEchoSettingObserver {
+    void onSettingChange();
+}
