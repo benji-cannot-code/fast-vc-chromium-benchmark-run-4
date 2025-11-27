b6e47c88bed227375943755f8c1e974d6834c4ce
Jan Keitel <jkeitel@google.com>
2025-11-27 00:16:37 -0800

    Add separate feature flags for actor address and payments filling
    These features serve as kill switches for specific data types, since
    the entire actor flow is already behind a separate flag.
    
    Bug: 455788947
    Change-Id: I384663d696d59e1f9999362a5f3202d3bd676ca0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7205126
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1550897}

--- chrome/browser/autofill/glic/actor_form_filling_service_impl.cc
+++ chrome/browser/autofill/glic/actor_form_filling_service_impl.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/autofill/autofill_client_provider.h"
 #include "chrome/browser/ui/autofill/autofill_client_provider_factory.h"
+#include "chrome/common/chrome_features.h"
 #include "components/autofill/content/browser/content_autofill_client.h"
 #include "components/autofill/core/browser/data_manager/payments/payments_data_manager.h"
 #include "components/autofill/core/browser/filling/form_filler.h"
@@ -388,13 +389,27 @@ void ActorFormFillingServiceImpl::GetSuggestions(
       case FormFillingRequest_RequestedData_SHIPPING_ADDRESS:
       case FormFillingRequest_RequestedData_BILLING_ADDRESS:
       case FormFillingRequest_RequestedData_HOME_ADDRESS:
-      case FormFillingRequest_RequestedData_WORK_ADDRESS:
+      case FormFillingRequest_RequestedData_WORK_ADDRESS: {
+        if (!base::FeatureList::IsEnabled(
+                ::features::kActorFormFillingServiceEnableAddress)) {
+          std::move(callback_with_metrics)
+              .Run(base::unexpected(kAutofillNotAvailable));
+          return;
+        }
         data = GetAddressSuggestions(representative_fields, autofill_manager);
         break;
-      case FormFillingRequest_RequestedData_CREDIT_CARD:
+      }
+      case FormFillingRequest_RequestedData_CREDIT_CARD: {
+        if (!base::FeatureList::IsEnabled(
+                ::features::kActorFormFillingServiceEnableCreditCard)) {
+          std::move(callback_with_metrics)
+              .Run(base::unexpected(kAutofillNotAvailable));
+          return;
+        }
         data =
             GetCreditCardSuggestions(representative_fields, autofill_manager);
         break;
+      }
       default:
         // Invalid request type.
         std::move(callback_with_metrics).Run(base::unexpected(kOther));
