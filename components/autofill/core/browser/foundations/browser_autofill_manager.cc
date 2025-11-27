520af6e22942bc372e76e52f2a24c33fc7c2f925
Christoph Schwering <schwering@google.com>
2025-11-27 07:10:04 -0800

    [Autofill] Move iOS-specific test-only member from BAM to CACIOS
    BrowserAutofillManager::consider_form_as_secure_for_testing_
    is only set in iOS tests. Its purpose is to make
    BrowserAutofillManager::IsFormNonSecure() true because iOS tests
    have invalid HTTPS certificates (crbug.com/355200775).
    
    This CL moves it to ChromeAutofillClientIOS.
    
    There are behavior differences though:
    - ChromeAutofillClientIOS::consider_as_secure_for_testing_ affects
      all frames of the client. That's arguably a win.
    - ChromeAutofillClientIOS::consider_as_secure_for_testing_ only
      affects AutofillClient::IsContextSecure(), not its caller
      BrowserAutofillManager::IsFormNonSecure(). As a result, to make
      BrowserAutofillManager::IsFormNonSecure() true, the form's target
      URL must be either invalid or HTTPS.
    
    Bug: 355148149
    Change-Id: Ie0428d552bedcc08e6a162a5a23ff67cbd2e40b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7205653
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1551095}

--- components/autofill/core/browser/foundations/browser_autofill_manager.cc
+++ components/autofill/core/browser/foundations/browser_autofill_manager.cc
@@ -1110,11 +1110,6 @@ void BrowserAutofillManager::OnTextFieldValueChangedImpl(
 }
 
 bool BrowserAutofillManager::IsFormNonSecure(const FormStructure& form) const {
-  // Check if testing override applies.
-  if (consider_form_as_secure_for_testing_.has_value() &&
-      consider_form_as_secure_for_testing_.value()) {
-    return false;
-  }
   return IsFormOrClientNonSecure(client(), form);
 }
 
@@ -2581,7 +2576,6 @@ const gfx::Image& BrowserAutofillManager::GetCardImage(
 // - No need to reset or recreate:
 //   - external_delegate_
 //   - fast_checkout_delegate_
-//   - consider_form_as_secure_for_testing_
 void BrowserAutofillManager::Reset() {
   // Process log events and record into UKM when the FormStructure is destroyed.
   for (const auto& [form_id, form_structure] : form_structures()) {
