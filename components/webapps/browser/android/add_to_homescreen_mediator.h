031cb412dfd467c616cc9bf8e1194f100781492f
Momoko Hattori <momohatt@google.com>
2025-11-27 09:04:25 -0800

    Move AddToHomescreenDataFetcher from A2HSMediator to A2HSCoordinator
    This CL moves AddToHomescreenDataFetcher from AddToHomescreenMediator to
    AddToHomescreenCoordinator class. This is a preparation for enabling
    auto-minted TWA in Chrome, as this change makes it easier to skip the
    creation of AddToHomescreenDialog based on the AppType inferred by
    AddToHomescreenDataFetcher.
    
    Bug: 462620051
    Change-Id: I266472674858336378b1b8285195d493f06925d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7141358
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Glenn Hartmann <hartmanng@chromium.org>
    Commit-Queue: Momoko Hattori <momohatt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1551152}

--- components/webapps/browser/android/add_to_homescreen_mediator.h
+++ components/webapps/browser/android/add_to_homescreen_mediator.h
@@ -11,11 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/android/jni_android.h"
 #include "base/android/scoped_java_ref.h"
+#include "base/functional/callback.h"
 #include "base/memory/weak_ptr.h"
-#include "components/webapps/browser/android/add_to_homescreen_data_fetcher.h"
 #include "components/webapps/browser/android/add_to_homescreen_installer.h"
 #include "components/webapps/browser/android/add_to_homescreen_params.h"
-#include "components/webapps/browser/banners/app_banner_settings_helper.h"
 #include "components/webapps/browser/installable/installable_logging.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "url/gurl.h"
@@ -26,7 +25,6 @@ class WebContents;
 
 namespace webapps {
 
-struct ShortcutInfo;
 class AddToHomescreenInstaller;
 
 using AppType = AddToHomescreenParams::AppType;
@@ -35,12 +33,12 @@ using AppType = AddToHomescreenParams::AppType;
 // org.chromium.components.webapps.addtohomescreen.AddToHomescreenMediator
 // in Java. It uses AddToHomescreenInstaller for installing the current app.
 // This class is owned, constructed, and destroyed by its Java counter-part.
-class AddToHomescreenMediator : public AddToHomescreenDataFetcher::Observer {
+class AddToHomescreenMediator {
  public:
   // Initializes the mediator for a given reference to the Java side object and
   // for a given WebContent.
-  // After initialization, either StartForAppBanner or StartForAppMenu should be
-  // called to update the UI accordingly.
+  // After initialization, SetWebAppInfo and SetIcon should be called to update
+  // the UI accordingly.
   AddToHomescreenMediator(
       const base::android::JavaParamRef<jobject>& java_ref,
       const base::android::JavaParamRef<jobject>& java_web_contents);
@@ -51,7 +49,12 @@ class AddToHomescreenMediator : public AddToHomescreenDataFetcher::Observer {
                                    const AddToHomescreenParams&)>
           event_callback);
 
-  void StartForAppMenu(JNIEnv* env, int app_menu_type);
+  // These 2 methods are called from the coordinator when the current flow
+  // started with startForAppMenu.
+  void OnAppMetadataAvailable(const std::u16string& user_title,
+                              const GURL& url,
+                              AddToHomescreenParams::AppType app_type);
+  void OnFullAppDataAvailable(std::unique_ptr<AddToHomescreenParams> params);
 
   // Called from the Java side when the user accepts app installation from the
   // dialog.
@@ -69,7 +72,7 @@ class AddToHomescreenMediator : public AddToHomescreenDataFetcher::Observer {
   void Destroy(JNIEnv* env);
 
  private:
-  ~AddToHomescreenMediator() override;
+  ~AddToHomescreenMediator();
 
   // Called immediatedly after |params_| is available. Displays |display_icon|
   // in the installation UI.
@@ -80,16 +83,6 @@ class AddToHomescreenMediator : public AddToHomescreenDataFetcher::Observer {
                      const GURL& url,
                      AddToHomescreenParams::AppType app_type);
 
-  // AddToHomescreenDataFetcher::Observer:
-  void OnUserTitleAvailable(const std::u16string& user_title,
-                            const GURL& url,
-                            AddToHomescreenParams::AppType app_type) override;
-
-  void OnDataAvailable(const ShortcutInfo& info,
-                       const SkBitmap& display_icon,
-                       AddToHomescreenParams::AppType app_type,
-                       InstallableStatusCode status_code) override;
-
   void RecordEventForAppMenu(AddToHomescreenInstaller::Event event,
                              const AddToHomescreenParams& a2hs_params);
 
@@ -98,16 +91,11 @@ class AddToHomescreenMediator : public AddToHomescreenDataFetcher::Observer {
 
   base::WeakPtr<content::WebContents> web_contents_;
 
-  // Fetches data required to add a shortcut.
-  std::unique_ptr<AddToHomescreenDataFetcher> data_fetcher_;
-
   std::unique_ptr<AddToHomescreenParams> params_;
 
   base::RepeatingCallback<void(AddToHomescreenInstaller::Event,
                                const AddToHomescreenParams&)>
       event_callback_;
-
-  int app_menu_type_ = AppBannerSettingsHelper::APP_MENU_OPTION_UNKNOWN;
 
   AddToHomescreenMediator(const AddToHomescreenMediator&) = delete;
   AddToHomescreenMediator& operator=(const AddToHomescreenMediator&) = delete;
