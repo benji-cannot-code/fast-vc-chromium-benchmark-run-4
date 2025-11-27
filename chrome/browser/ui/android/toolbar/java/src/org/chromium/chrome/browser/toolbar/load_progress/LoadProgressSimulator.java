3b41e022872b84e34c2c2d6884ac852512527223
Peilin Wang <peilinwang@google.com>
2025-11-27 08:09:35 -0800

    [Animated PB] Allow animation to finish before hiding
    Bug: 448677518
    Change-Id: Ife3bc41fdda6efa5a9e3a527294230e21d483ebc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7206007
    Commit-Queue: Peilin Wang <peilinwang@google.com>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1551122}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/load_progress/LoadProgressSimulator.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/load_progress/LoadProgressSimulator.java
@@ -13,6 +13,7 @@ import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.MathUtils;
 import org.chromium.build.annotations.NullMarked;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.ui.modelutil.PropertyModel;
 
 /**
@@ -30,6 +31,7 @@ class LoadProgressSimulator {
     private final Handler mHandler;
 
     private float mProgress;
+    private boolean mIsStarted;
 
     public LoadProgressSimulator(PropertyModel model) {
         mModel = model;
@@ -45,6 +47,7 @@ class LoadProgressSimulator {
                             mModel.set(
                                     LoadProgressProperties.COMPLETION_STATE,
                                     LoadProgressProperties.CompletionState.FINISHED_DO_ANIMATE);
+                            mIsStarted = false;
                             return;
                         }
                         sendEmptyMessageDelayed(
@@ -55,6 +58,7 @@ class LoadProgressSimulator {
 
     /** Start simulating load progress from a baseline of 0. */
     public void start() {
+        mIsStarted = true;
         mProgress = 0.0f;
         mModel.set(
                 LoadProgressProperties.COMPLETION_STATE,
@@ -65,9 +69,12 @@ class LoadProgressSimulator {
 
     /** Cancels simulating load progress. */
     public void cancel() {
-        mModel.set(
-                LoadProgressProperties.COMPLETION_STATE,
-                LoadProgressProperties.CompletionState.FINISHED_DONT_ANIMATE);
+        if (!ChromeFeatureList.sAndroidAnimatedProgressBarInBrowser.isEnabled()
+                || (mIsStarted && !MathUtils.areFloatsEqual(mProgress, 1.0f))) {
+            mModel.set(
+                    LoadProgressProperties.COMPLETION_STATE,
+                    LoadProgressProperties.CompletionState.FINISHED_DONT_ANIMATE);
+        }
         mHandler.removeMessages(MSG_ID_UPDATE_PROGRESS);
     }
 }
