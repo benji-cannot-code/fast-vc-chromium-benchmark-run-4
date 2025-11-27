4675d1cf926bd6586ddb28129d4cb6d8af74054f
Yuwei Huang <yuweih@chromium.org>
2025-11-26 16:38:06 -0800

    [crd host][wayland] Portal utility classes
    Introduce a few utility classes that will be used in following CLs:
    
    * ScopedPortalRequest which encapsulates the portal Request
    * ScopedPortalSession which encapsulates the portal Session
    * GVariantDictBuilder for building a vardict of type a{sv}
    * Common constants in portal_utils.h
    
    Bug: 445973705
    Change-Id: Icbd8db4861d22156f7360920d73d2066dc49893e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7205596
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1550791}

---
+++ remoting/host/linux/gvariant_dict_builder.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "remoting/host/linux/gvariant_dict_builder.h"
+
+#include "remoting/host/linux/gvariant_ref.h"
+
+namespace remoting {
+
+GVariantDictBuilder::GVariantDictBuilder() = default;
+GVariantDictBuilder::~GVariantDictBuilder() = default;
+
+GVariantDictBuilder& GVariantDictBuilder::AddVariant(
+    std::string_view key,
+    gvariant::GVariantRef<"v"> variant) {
+  entries_.emplace_back(key, variant);
+  return *this;
+}
+
+gvariant::GVariantRef<"a{sv}"> GVariantDictBuilder::Build() {
+  return gvariant::GVariantFrom(entries_);
+}
+
+}  // namespace remoting
