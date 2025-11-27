61c06913e8e2bcf4661976bf28661c79ad3d74d5
Eliot Courtney <edcourtney@google.com>
2025-11-27 06:18:59 -0800

    Re-filter tabs on state change.
    This CL updates the list of tabs used for media capture picking when
    relevant state about the tab changes (title, content, URL, etc).
    
    Bug: 352187279, 405234431, 405234529
    Test: CQ + unit test
    Change-Id: I2ce58c0699586a71b926db066075b7fe27c5c192
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7206372
    Commit-Queue: Eliot Courtney <edcourtney@chromium.org>
    Reviewed-by: Grace Cham <hscham@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1551056}

--- chrome/android/java/src/org/chromium/chrome/browser/media/MediaCapturePickerDialog.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/MediaCapturePickerDialog.java
@@ -43,7 +43,7 @@ import java.util.Map;
 
 /** Dialog for selecting a media source for media capture. */
 @NullMarked
-public class MediaCapturePickerDialog implements AllTabObserver.Observer {
+public class MediaCapturePickerDialog implements MediaCapturePickerTabObserver.Delegate {
     // This web contents is the one that is receiving the shared content.
     private final ModalDialogManager mModalDialogManager;
     private final MediaCapturePickerManager.Params mParams;
@@ -83,6 +83,10 @@ public class MediaCapturePickerDialog implements AllTabObserver.Observer {
             mModel.set(MediaCapturePickerItemProperties.SELECTED, true);
             mPositiveButton.setEnabled(true);
             mLastSelectedTabItemState = this;
+        }
+
+        void update() {
+            mModel.set(MediaCapturePickerItemProperties.TAB_NAME, mTab.getTitle());
         }
 
         void destroy() {
@@ -163,6 +167,13 @@ public class MediaCapturePickerDialog implements AllTabObserver.Observer {
         removed.destroy();
     }
 
+    @Override
+    public void onTabUpdated(Tab tab) {
+        final var state = mTabItemStateMap.get(tab);
+        assert state != null;
+        state.update();
+    }
+
     private void startAndroidCapturePrompt() {
         var fragment = MediaCapturePickerHeadlessFragment.getInstanceForCurrentActivity();
         assumeNonNull(fragment);
@@ -194,9 +205,9 @@ public class MediaCapturePickerDialog implements AllTabObserver.Observer {
     }
 
     void show() {
-        var allTabObserver =
-                new AllTabObserver(
-                        new MediaCapturePickerTabObserver(this, mParams, assumeNonNull(mDelegate)));
+        final var observer =
+                new MediaCapturePickerTabObserver(this, mParams, assumeNonNull(mDelegate));
+        final var allTabObserver = new AllTabObserver(observer);
 
         var controller =
                 new ModalDialogProperties.Controller() {
@@ -229,6 +240,7 @@ public class MediaCapturePickerDialog implements AllTabObserver.Observer {
                             mDelegate = null;
                         }
                         allTabObserver.destroy();
+                        observer.destroy();
                     }
                 };
 
--- chrome/android/javatests/src/org/chromium/chrome/browser/media/ui/MediaCapturePickerTabObserverTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/media/ui/MediaCapturePickerTabObserverTest.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.media.ui;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
@@ -15,17 +16,18 @@ import androidx.test.filters.SmallTest;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.chrome.browser.app.tabmodel.AllTabObserver;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.media.MediaCapturePickerManager;
 import org.chromium.chrome.browser.media.MediaCapturePickerTabObserver;
 import org.chromium.chrome.browser.tab.Tab;
+import org.chromium.chrome.browser.tab.TabObserver;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.content_public.browser.WebContents;
 
@@ -39,7 +41,7 @@ import org.chromium.content_public.browser.WebContents;
 public class MediaCapturePickerTabObserverTest {
     @Rule public final MockitoRule mMockitoRule = MockitoJUnit.rule();
 
-    @Mock private AllTabObserver.Observer mObserverDelegate;
+    @Mock private MediaCapturePickerTabObserver.Delegate mObserverDelegate;
     @Mock private MediaCapturePickerManager.Delegate mFilterDelegate;
 
     private static class ParamsBuilder {
@@ -88,9 +90,11 @@ public class MediaCapturePickerTabObserverTest {
         final Tab tab = createMockTab(params.webContents, /* isNative= */ false);
         observer.onTabAdded(tab);
         verify(mObserverDelegate).onTabAdded(tab);
+        verify(tab).addObserver(any());
 
         observer.onTabRemoved(tab);
         verify(mObserverDelegate).onTabRemoved(tab);
+        verify(tab).removeObserver(any());
     }
 
     @Test
@@ -103,8 +107,11 @@ public class MediaCapturePickerTabObserverTest {
         final Tab tab = createMockTab(/* webContents= */ null, /* isNative= */ true);
         observer.onTabAdded(tab);
         verify(mObserverDelegate, never()).onTabAdded(tab);
+        verify(tab).addObserver(any());
+
         observer.onTabRemoved(tab);
         verify(mObserverDelegate, never()).onTabRemoved(tab);
+        verify(tab).removeObserver(any());
     }
 
     @Test
@@ -121,15 +128,19 @@ public class MediaCapturePickerTabObserverTest {
 
         observer.onTabAdded(thisTab);
         verify(mObserverDelegate).onTabAdded(thisTab);
+        verify(thisTab).addObserver(any());
 
         observer.onTabAdded(anotherTab);
         verify(mObserverDelegate, never()).onTabAdded(anotherTab);
+        verify(anotherTab).addObserver(any());
 
         observer.onTabRemoved(thisTab);
         verify(mObserverDelegate).onTabRemoved(thisTab);
+        verify(thisTab).removeObserver(any());
 
         observer.onTabRemoved(anotherTab);
         verify(mObserverDelegate, never()).onTabRemoved(anotherTab);
+        verify(anotherTab).removeObserver(any());
     }
 
     @Test
@@ -147,6 +158,7 @@ public class MediaCapturePickerTabObserverTest {
 
         observer.onTabAdded(filteredTab);
         verify(mObserverDelegate, never()).onTabAdded(filteredTab);
+        verify(filteredTab).addObserver(any());
 
         // Tab that should not be filtered.
         final WebContents allowedWebContents = mock(WebContents.class);
@@ -155,5 +167,26 @@ public class MediaCapturePickerTabObserverTest {
 
         observer.onTabAdded(allowedTab);
         verify(mObserverDelegate).onTabAdded(allowedTab);
+        verify(allowedTab).addObserver(any());
+    }
+
+    @Test
+    @SmallTest
+    public void testTabUpdate() {
+        final var params = new ParamsBuilder().build();
+        final var observer =
+                new MediaCapturePickerTabObserver(mObserverDelegate, params, mFilterDelegate);
+
+        final Tab tab = createMockTab(params.webContents, /* isNative= */ false);
+        final ArgumentCaptor<TabObserver> tabObserverCaptor =
+                ArgumentCaptor.forClass(TabObserver.class);
+
+        observer.onTabAdded(tab);
+        verify(mObserverDelegate).onTabAdded(tab);
+        verify(tab).addObserver(tabObserverCaptor.capture());
+
+        final TabObserver tabObserver = tabObserverCaptor.getValue();
+        tabObserver.onTitleUpdated(tab);
+        verify(mObserverDelegate).onTabUpdated(tab);
     }
 }
