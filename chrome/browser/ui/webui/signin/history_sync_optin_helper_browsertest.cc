a1e400b70bfec15d904143aff4f50277437048e7
Alex Ilin <alexilin@chromium.org>
2025-11-26 23:46:45 -0800

    [signin] Stop using AccountInfo::hosted_domain
    AccountInfo::hosted_domain is deprecated and it's going to be moved to a
    private class section.
    
    Readers of the hosted_domain field get migrated to
    AccountInfo::GetHostedDomain(). GetHostedDomain() returns an
    std::optional with slightly changed semantics:
    - An empty hosted domain previously represented by the
      kNoHostedDomainFound sentinel is now represented as an empty string
    - An unknown hosted domain previously represented by an empty string is
      now represented by std::nullopt
    
    Writers of the hosted_domain field get migrated to
    AccountInfo::Builder::SetHostedDomain(). SetHostedDomain() doesn't allow
    resetting the field to an unknown state and it also treats an empty
    string the same way as kNoHostedDomainFound. Our goal is to deprecate
    kNoHostedDomainFound sentinel completely so this change systematically
    replaces `info.hosted_domain = kNoHostedDomainFound` with
    `builder.SetHostedDomain(std::string())`.
    
    This CL modifies files in ['chrome/browser/ui/webui/settings',
    'chrome/browser/ui/webui/signin',
    'chrome/browser/ui/webui/signin/history_sync_optin',
    'chrome/browser/ui/views/profiles', 'chrome/browser/ui/signin'].
    
    This CL was uploaded by git cl split.
    
    R=amelies@google.com
    
    Bug: 458409080
    Change-Id: I549cf9cdc485ebff60e29f1424e762e78953cec5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7201708
    Reviewed-by: Amelie Schneider <amelies@google.com>
    Auto-Submit: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1550875}

--- chrome/browser/ui/webui/signin/history_sync_optin_helper_browsertest.cc
+++ chrome/browser/ui/webui/signin/history_sync_optin_helper_browsertest.cc
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/base/signin_switches.h"
 #include "components/signin/public/identity_manager/account_state_fetcher.h"
 #include "components/signin/public/identity_manager/identity_test_environment.h"
-#include "components/signin/public/identity_manager/signin_constants.h"
 #include "components/signin/public/identity_manager/tribool.h"
 #include "components/sync/base/features.h"
 #include "components/sync/test/test_sync_service.h"
@@ -131,8 +130,10 @@ class HistorySyncOptinHelperBrowserTest
 
   // Updates the fields relating to the account management.
   void UpdateAccountManagementInfo(AccountInfo& account_info, bool is_managed) {
-    account_info.hosted_domain =
-        is_managed ? "example.com" : signin::constants::kNoHostedDomainFound;
+    account_info =
+        AccountInfo::Builder(account_info)
+            .SetHostedDomain(is_managed ? "example.com" : std::string())
+            .Build();
     CHECK(account_info.IsValid());
     identity_test_env()->UpdateAccountInfoForAccount(account_info);
   }
