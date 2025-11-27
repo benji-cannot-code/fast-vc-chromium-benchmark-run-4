5cab474289579b13b54b9091bdc4829684146031
Aliona DANGLA <alionadangla@google.com>
2025-11-27 03:24:52 -0800

    [iOS] Add a limit of 200MB for files
    See video:
    https://drive.google.com/file/d/1X6IhO-RE3g-TYfukm5ZXAqGcttAHd8BT/view?usp=drive_link
    
    Fixed: 462132200
    Change-Id: Ia6cfb2e509b04e055db6a8ad039786345d0212ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7207333
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1550981}

--- ios/chrome/browser/composebox/coordinator/composebox_constants.h
+++ ios/chrome/browser/composebox/coordinator/composebox_constants.h
@@ -10,5 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // The maximum number of attachments that can be added to a prompt.
 extern const NSUInteger kAttachmentLimit;
+// The maximum allowed size for PDF file uploads.
+extern const NSUInteger kMaxPDFFileSize;
 
 #endif  // IOS_CHROME_BROWSER_COMPOSEBOX_COORDINATOR_COMPOSEBOX_CONSTANTS_H_
--- ios/chrome/browser/composebox/coordinator/composebox_constants.mm
+++ ios/chrome/browser/composebox/coordinator/composebox_constants.mm
@@ -6,3 +6,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/composebox/coordinator/composebox_constants.h"
 
 const NSUInteger kAttachmentLimit = 10;
+const NSUInteger kMaxPDFFileSize = 200 * 1024 * 1024;
