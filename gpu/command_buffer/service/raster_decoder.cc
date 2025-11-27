ab0909092ac4c7b8b448e1f0be2125f51449eda7
Colin Blundell <blundell@chromium.org>
2025-11-26 23:58:22 -0800

    [//gpu] Guard RasterDecoder Skia cleanup by whether raster was performed
    Rather than doing this whenever GPU raster has been set as enabled by
    the client, do it only if a BeginRaster() operation was actually
    executed. This removes one server-side dependence on the concept of
    GPU raster being enabled.
    
    Bug: 443000825
    Change-Id: Ica25f76690838b492fffbb1d5b252dfcead145dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7205536
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1550879}

--- gpu/command_buffer/service/raster_decoder.cc
+++ gpu/command_buffer/service/raster_decoder.cc
@@ -855,6 +855,7 @@ class RasterDecoderImpl final : public RasterDecoder,
 
   bool use_gpu_raster_ = false;
   bool use_passthrough_ = false;
+  bool performed_raster_ = false;
 
   // The current decoder error communicates the decoder error through command
   // processing functions that do not return the error value. Should be set
@@ -1476,6 +1477,8 @@ error::Error RasterDecoderImpl::DoCommandsImpl(unsigned int num_commands,
   int process_pos = 0;
   CommandId command = static_cast<CommandId>(0);
 
+  performed_raster_ = false;
+
   while (process_pos < num_entries && result == error::kNoError &&
          commands_to_process_--) {
     const unsigned int size = cmd_data->value_header.size;
@@ -1572,8 +1575,9 @@ error::Error RasterDecoderImpl::DoCommandsImpl(unsigned int num_commands,
                << GetCommandName(command);
   }
 
-  if (use_gpu_raster_)
+  if (performed_raster_) {
     client()->ScheduleGrContextCleanup();
+  }
 
   return result;
 }
@@ -2779,6 +2783,9 @@ void RasterDecoderImpl::DoBeginRasterCHROMIUM(GLfloat r,
                        "No chromium raster support");
     return;
   }
+
+  performed_raster_ = true;
+
   if (sk_surface_) {
     LOCAL_SET_GL_ERROR(GL_INVALID_OPERATION, "glBeginRasterCHROMIUM",
                        "BeginRasterCHROMIUM without EndRasterCHROMIUM");
