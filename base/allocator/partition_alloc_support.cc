7092826d13f3bce8354a4f6073353e52024401c3
Ayumi Ono <ayumiohno@google.com>
2025-11-26 17:40:25 -0800

    PA: Add finch feature flag to enable/disable FreeWithSize
    Clang supports a sized version of operator delete. Providing the
    allocation size at deallocation time allows memory allocators to
    potentially skip a metadata lookup to determine the size of the object
    being freed. For PartitionAlloc, leveraging this size information could
    improve performance by avoiding unnecessary lookups and improving cache
    efficiency.
    
    This CL implements finch feature flag to enable or disable FreeWithSize.
    If the flag is disabled, FreeWithSizeInline is redirected to FreeInline.
    
    Bug: 410190984
    Change-Id: Ia9a7e3f27a5a5e5f5af10ec6c92566a568d8e60a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7201387
    Reviewed-by: Mikihito Matsuura <mikt@google.com>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Commit-Queue: Ayumi Ono <ayumiohno@google.com>
    Cr-Commit-Position: refs/heads/main@{#1550808}

--- base/allocator/partition_alloc_support.cc
+++ base/allocator/partition_alloc_support.cc
@@ -1130,6 +1130,9 @@ void PartitionAllocSupport::ReconfigureAfterFeatureListInit(
   partition_alloc::TagViolationReportingMode memory_tagging_reporting_mode =
       partition_alloc::TagViolationReportingMode::kUndefined;
 
+  const bool enable_free_with_size =
+      base::FeatureList::IsEnabled(base::features::kPartitionAllocFreeWithSize);
+
 #if PA_BUILDFLAG(HAS_MEMORY_TAGGING)
   // ShouldEnableMemoryTagging() checks kKillPartitionAllocMemoryTagging but
   // check here too to wrap the GetMemoryTaggingModeForCurrentThread() call.
@@ -1222,7 +1225,8 @@ void PartitionAllocSupport::ReconfigureAfterFeatureListInit(
       scheduler_loop_quarantine_global_config,
       scheduler_loop_quarantine_thread_local_config,
       scheduler_loop_quarantine_for_advanced_memory_safety_checks_config,
-      allocator_shim::EventuallyZeroFreedMemory(eventually_zero_freed_memory));
+      allocator_shim::EventuallyZeroFreedMemory(eventually_zero_freed_memory),
+      allocator_shim::EnableFreeWithSize(enable_free_with_size));
 
   const uint32_t extras_size = allocator_shim::GetMainPartitionRootExtrasSize();
   // As per description, extras are optional and are expected not to
