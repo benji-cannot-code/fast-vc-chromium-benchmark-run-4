2b71cf88644e4ce6cdc097089b9ac7b911e4ee11
Colin Blundell <blundell@chromium.org>
2025-11-27 00:06:48 -0800

    [//media] Remove unneeded util
    Nothing calls this anymore post-conversions to SharedImageFormat.
    
    Bug: 356649879
    Change-Id: I4768dcf02007634ca60825bcc60558689072943f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7207153
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1550893}

--- media/base/format_utils.h
+++ media/base/format_utils.h
@@ -11,12 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/resources/shared_image_format.h"
 #include "media/base/media_export.h"
 #include "media/base/video_types.h"
-#include "ui/gfx/buffer_types.h"
 
 namespace media {
-
-MEDIA_EXPORT std::optional<gfx::BufferFormat> VideoPixelFormatToGfxBufferFormat(
-    VideoPixelFormat pixel_format);
 
 MEDIA_EXPORT std::optional<VideoPixelFormat>
 SharedImageFormatToVideoPixelFormat(viz::SharedImageFormat format);
