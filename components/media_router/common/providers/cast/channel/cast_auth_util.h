9d06eddc119306329aff5e00a7259437af4b4624
Rune Lillesveen <futhark@chromium.org>
2025-11-27 07:04:37 -0800

    Remove CastNonceEnforced base feature
    This is an old feature (2017) which is off-by-default, and no trace of
    it in google3.
    
    This CL is created with the use of Gemini CLI
    
    Bug: 356236413
    Change-Id: Ic36bb7ed71b4fb297346846075e6cf76848b738f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7208977
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1551091}

--- components/media_router/common/providers/cast/channel/cast_auth_util.h
+++ components/media_router/common/providers/cast/channel/cast_auth_util.h
@@ -30,7 +30,6 @@ namespace cast_channel {
 using ::openscreen::cast::proto::AuthResponse;
 using ::openscreen::cast::proto::CastMessage;
 
-BASE_DECLARE_FEATURE(kEnforceNonceChecking);
 BASE_DECLARE_FEATURE(kEnforceSHA256Checking);
 
 struct AuthResult {
