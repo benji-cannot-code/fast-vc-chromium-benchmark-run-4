399dc1614e22f6ea902648e199b608c1adaeaa19
Marc Treib <treib@chromium.org>
2025-11-27 07:24:33 -0800

    [SyncTests] Modernize SyncErrorsTest
    ...to run in both Sync-the-feature mode and transport mode.
    
    Some bookmarks usages had to be updated to use the correct store (local
    or account), and usages of SyncDisabledChecker (which is specific to
    Sync-the-feature) were updated to SyncEngineStoppedChecker which was
    slightly extended to capture the error (if any) at the time the engine
    was stopped.
    
    Bug: 353425612
    Change-Id: Ie69648c1f8fe75daca8ef66d5cced08c6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7207240
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1551103}

--- chrome/browser/sync/test/integration/sync_engine_stopped_checker.cc
+++ chrome/browser/sync/test/integration/sync_engine_stopped_checker.cc
@@ -17,4 +17,8 @@ bool SyncEngineStoppedChecker::IsExitConditionSatisfied(std::ostream* os) {
   return !service()->IsEngineInitialized();
 }
 
+void SyncEngineStoppedChecker::WaitDone() {
+  service()->QueryDetailedSyncStatusForDebugging(&status_on_engine_stopped_);
+}
+
 }  // namespace syncer
