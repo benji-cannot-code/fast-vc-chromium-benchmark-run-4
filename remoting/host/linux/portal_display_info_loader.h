57248cd142d9a5f4dd8b89971600ae71fe29656d
Yuwei Huang <yuweih@chromium.org>
2025-11-26 21:56:02 -0800

    [crd host][wayland] PortalDesktopResizer & PortalDisplayInfoLoader
    This is pretty much what we did in GnomeDesktopResizer and
    GnomeDisplayInfoLoader prior to the integration with the GNOME
    DisplayConfig API. They just simply use the PipeWire stream for resizing
    and getting the resolution. Also, since there is no universal display
    changed signal in Wayland, we will be polling the display info for
    the time being.
    
    Bug: 445973705
    Change-Id: If020c94a5388eeffdbcb267fb08afeae6412647b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7208457
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1550855}

---
+++ remoting/host/linux/portal_display_info_loader.h
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef REMOTING_HOST_LINUX_PORTAL_DISPLAY_INFO_LOADER_H_
+#define REMOTING_HOST_LINUX_PORTAL_DISPLAY_INFO_LOADER_H_
+
+#include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
+#include "remoting/host/desktop_display_info_loader.h"
+#include "remoting/host/linux/capture_stream_manager.h"
+
+namespace remoting {
+
+class PortalDisplayInfoLoader : public DesktopDisplayInfoLoader {
+ public:
+  // `stream_manager` must outlive `this`.
+  explicit PortalDisplayInfoLoader(CaptureStreamManager& stream_manager);
+  PortalDisplayInfoLoader(const PortalDisplayInfoLoader&) = delete;
+  PortalDisplayInfoLoader& operator=(const PortalDisplayInfoLoader&) = delete;
+  ~PortalDisplayInfoLoader() override;
+
+  // DesktopDisplayInfoLoader interface.
+  DesktopDisplayInfo GetCurrentDisplayInfo() override;
+
+  base::WeakPtr<PortalDisplayInfoLoader> GetWeakPtr();
+
+ private:
+  raw_ptr<CaptureStreamManager> stream_manager_;
+  base::WeakPtrFactory<PortalDisplayInfoLoader> weak_ptr_factory_{this};
+};
+
+}  // namespace remoting
+
+#endif  // REMOTING_HOST_LINUX_PORTAL_DISPLAY_INFO_LOADER_H_
