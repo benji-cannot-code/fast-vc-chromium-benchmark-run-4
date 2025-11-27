429459924855996e41d5af7a24d0b41a962b8694
Ayumi Ono <ayumiohno@google.com>
2025-11-26 17:38:06 -0800

    PA: Optimize FreeWithSize in simple path.
    Clang supports a sized version of operator delete. Providing the
    allocation size at deallocation time allows memory allocators to
    potentially skip a metadata lookup to determine the size of the object
    being freed. For PartitionAlloc, leveraging this size information could
    improve performance by avoiding unnecessary lookups and improving cache
    efficiency.
    
    This CL implements FreeWithSize to directly utilize the provided size
    information, thereby avoiding metadata lookups in a simple path.
    Additionally, it introduces the necessary allocator shim overrides to
    intercept and correctly handle calls made to these sized delete
    functions.
    
    Bug: 410190984
    Change-Id: I36bb3e4c96ce5d34f64e3d58215e41d4ed60810b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7136445
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Ayumi Ono <ayumiohno@google.com>
    Cr-Commit-Position: refs/heads/main@{#1550805}

--- base/allocator/partition_allocator/src/partition_alloc/partition_alloc_perftest.cc
+++ base/allocator/partition_allocator/src/partition_alloc/partition_alloc_perftest.cc
@@ -90,6 +90,7 @@ class Allocator {
   virtual ~Allocator() = default;
   virtual void* Alloc(size_t size) = 0;
   virtual void Free(void* data) = 0;
+  virtual void FreeWithSize(void* data, size_t size) = 0;
 };
 
 class SystemAllocator : public Allocator {
@@ -98,6 +99,7 @@ class SystemAllocator : public Allocator {
   ~SystemAllocator() override = default;
   void* Alloc(size_t size) override { return malloc(size); }
   void Free(void* data) override { free(data); }
+  void FreeWithSize(void* data, size_t size) override { free(data); }
 };
 
 class PartitionAllocator : public Allocator {
@@ -114,6 +116,13 @@ class PartitionAllocator : public Allocator {
     // more common with PA-E.
     PartitionRoot::FreeInlineInUnknownRoot<
         partition_alloc::FreeFlags::kNoHooks>(data);
+  }
+  void FreeWithSize(void* data, size_t size) override {
+    // Even though it's easy to invoke the fast path with
+    // alloc_.Free<kNoHooks>(), we chose to use the slower path, because it's
+    // more common with PA-E.
+    PartitionRoot::FreeWithSizeInlineInUnknownRoot<
+        partition_alloc::FreeFlags::kNoHooks>(data, size);
   }
 
  private:
@@ -142,6 +151,13 @@ class PartitionAllocatorWithThreadCache : public Allocator {
     // more common with PA-E.
     PartitionRoot::FreeInlineInUnknownRoot<
         partition_alloc::FreeFlags::kNoHooks>(data);
+  }
+  void FreeWithSize(void* data, size_t size) override {
+    // Even though it's easy to invoke the fast path with
+    // alloc_.Free<kNoHooks>(), we chose to use the slower path, because it's
+    // more common with PA-E.
+    PartitionRoot::FreeWithSizeInlineInUnknownRoot<
+        partition_alloc::FreeFlags::kNoHooks>(data, size);
   }
 
  private:
@@ -181,6 +197,13 @@ class PartitionAllocatorWithAllocationStackTraceRecorder : public Allocator {
     // more common with PA-E.
     PartitionRoot::FreeInlineInUnknownRoot<
         partition_alloc::FreeFlags::kNoHooks>(data);
+  }
+  void FreeWithSize(void* data, size_t size) override {
+    // Even though it's easy to invoke the fast path with
+    // alloc_.Free<kNoHooks>(), we chose to use the slower path, because it's
+    // more common with PA-E.
+    PartitionRoot::FreeWithSizeInlineInUnknownRoot<
+        partition_alloc::FreeFlags::kNoHooks>(data, size);
   }
 
  private:
@@ -355,6 +378,37 @@ float MultiBucketWithFree(Allocator* allocator) {
   return timer.LapsPerSecond() * kMultiBucketRounds;
 }
 
+float MultiBucketWithFreeWithSize(Allocator* allocator) {
+  std::vector<void*> elems;
+  elems.reserve(kMultiBucketRounds);
+  // Do an initial round of allocation to make sure that the buckets stay in
+  // use (and aren't accidentally released back to the OS).
+  for (int i = 0; i < kMultiBucketRounds; i++) {
+    void* cur =
+        allocator->Alloc(kMultiBucketMinimumSize + (i * kMultiBucketIncrement));
+    PA_CHECK(cur != nullptr);
+    elems.push_back(cur);
+  }
+
+  ::base::LapTimer timer(kWarmupRuns, kTimeLimit, kTimeCheckInterval);
+  do {
+    for (int i = 0; i < kMultiBucketRounds; i++) {
+      void* cur = allocator->Alloc(kMultiBucketMinimumSize +
+                                   (i * kMultiBucketIncrement));
+      PA_CHECK(cur != nullptr);
+      allocator->FreeWithSize(
+          cur, kMultiBucketMinimumSize + (i * kMultiBucketIncrement));
+    }
+    timer.NextLap();
+  } while (!timer.HasTimeLimitExpired());
+
+  for (void* ptr : elems) {
+    allocator->Free(ptr);
+  }
+
+  return timer.LapsPerSecond() * kMultiBucketRounds;
+}
+
 float DirectMapped(Allocator* allocator) {
   constexpr size_t kSize = 2 * 1000 * 1000;
 
@@ -518,6 +572,12 @@ TEST_P(PartitionAllocMemoryAllocationPerfTest, MultiBucketWithFree) {
   auto params = GetParam();
   RunTest(std::get<int>(params), std::get<AllocatorType>(params),
           MultiBucketWithFree, nullptr, "MultiBucketWithFree");
+}
+
+TEST_P(PartitionAllocMemoryAllocationPerfTest, MultiBucketWithFreeWithSize) {
+  auto params = GetParam();
+  RunTest(std::get<int>(params), std::get<AllocatorType>(params),
+          MultiBucketWithFreeWithSize, nullptr, "MultiBucketWithFreeWithSize");
 }
 
 TEST_P(PartitionAllocMemoryAllocationPerfTest, DirectMapped) {
