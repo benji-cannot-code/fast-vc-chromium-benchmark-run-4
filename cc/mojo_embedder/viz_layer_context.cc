7841964e04a6b9f28e6c71ee99765a0d825c2402
Mingjing Zhang <mjzhang@chromium.org>
2025-11-28 15:05:23 -0800

    Use nullopt instead of empty TransferableResource
    This CL replaces the use of default constructed TransferableResource
    with nullopt in viz_layer_context.cc. This change is to avoid
    sending unnecessary empty TransferableResources, and avoid potential
    failures when in the future the Mailbox is replaced with
    ClientSharedImage in TransferableResource.
    
    Bug: 40286368
    Change-Id: I81da7e4855e573b7d83de648d26e2e382a0c7e3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7210851
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Mingjing Zhang <mjzhang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1551672}

--- cc/mojo_embedder/viz_layer_context.cc
+++ cc/mojo_embedder/viz_layer_context.cc
@@ -682,7 +682,7 @@ void SerializeHudLayerExtra(HeadsUpDisplayLayerImpl& layer,
     extra->uv_bottom_right =
         gfx::PointF(resource_uv_size.width(), resource_uv_size.height());
   } else {
-    extra->transferable_resource = viz::TransferableResource();
+    extra->transferable_resource = std::nullopt;
   }
 }
 
@@ -710,7 +710,7 @@ void SerializeTextureLayerExtra(
       CHECK_EQ(resources.size(), 1u);
       extra->transferable_resource = resources[0];
     } else {
-      extra->transferable_resource = viz::TransferableResource();
+      extra->transferable_resource = std::nullopt;
     }
 
     layer.ClearNeedsSetResourcePush();
