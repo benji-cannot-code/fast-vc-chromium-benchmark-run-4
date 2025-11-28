b0b6f7994db8931cd02f0c3b0e3f43fb0676059c
Antonio Sartori <antoniosartori@chromium.org>
2025-11-28 05:06:36 -0800

    [identifiability] Remove unused headers
    These headers are unused and are a leftover of a previous code deletion.
    
    Bug: 451512746
    Change-Id: I14d54b4391a835ba6391ee16a174f62e8e9cb895
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7209212
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Auto-Submit: Antonio Sartori <antoniosartori@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1551463}

--- third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.h
+++ third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.h
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "third_party/blink/public/common/privacy_budget/identifiable_surface.h"
-#include "third_party/blink/public/common/privacy_budget/identifiable_token.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/dom/events/event_target.h"
--- third_party/blink/renderer/modules/canvas/canvas2d/canvas_pattern.h
+++ third_party/blink/renderer/modules/canvas/canvas2d/canvas_pattern.h
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_CANVAS_CANVAS2D_CANVAS_PATTERN_H_
 
 #include "base/memory/scoped_refptr.h"
-#include "third_party/blink/public/common/privacy_budget/identifiable_token.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/graphics/canvas_high_entropy_op_type.h"
