cea19c2cc18ca5efd68e649ce9e3b5eda4b91f06
Antonio Sartori <antoniosartori@chromium.org>
2025-11-28 06:23:20 -0800

    [identifiability] Remove unused include
    Bug: 451512746
    Change-Id: I3dfad09af62c04ef38141da20375e44b9466a702
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7209335
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Auto-Submit: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1551498}

--- content/browser/service_worker/service_worker_context_wrapper.cc
+++ content/browser/service_worker/service_worker_context_wrapper.cc
@@ -72,7 +72,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "storage/browser/quota/quota_manager_proxy.h"
 #include "storage/browser/quota/special_storage_policy.h"
 #include "third_party/blink/public/common/features.h"
-#include "third_party/blink/public/common/privacy_budget/identifiability_study_settings.h"
 #include "third_party/blink/public/common/service_worker/embedded_worker_status.h"
 #include "third_party/blink/public/common/service_worker/service_worker_scope_match.h"
 #include "third_party/blink/public/common/service_worker/service_worker_status_code.h"
