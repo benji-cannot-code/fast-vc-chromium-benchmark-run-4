31bd24fe6edccaae6a8b321901a9e32389970c7c
Darren Shen <shend@chromium.org>
2025-11-27 20:33:37 -0800

    Temporary fix for help me write UI bug.
    https://crrev.com/c/6925280 caused a regression where if the
    context menu closes but the help me write card stays open, then
    none of the help me write UI will work the second time.
    
    This is due to prematurely destroying the help me write session.
    See the attached bug for a detailed explanation.
    
    Fix this temporarily by not destroying the help me write session
    when visibility of the menu is off.
    
    Bug: 463224686
    Change-Id: Ib41d0670a1739651efae845efa02e9e436a9fd05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7206316
    Commit-Queue: Michael Cui <mlcui@google.com>
    Auto-Submit: Darren Shen <shend@chromium.org>
    Reviewed-by: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1551307}

--- chrome/browser/ui/ash/editor_menu/editor_menu_controller_impl.cc
+++ chrome/browser/ui/ash/editor_menu/editor_menu_controller_impl.cc
@@ -210,7 +210,9 @@ void EditorMenuControllerImpl::OnPromoCardWidgetClosed(
   OnEditorCardHidden();
 }
 
-void EditorMenuControllerImpl::OnEditorMenuVisibilityChanged(bool visible) {
+void EditorMenuControllerImpl::OnEditorMenuVisibilityChanged(
+    bool visible,
+    bool destroy_session) {
   if (!card_session_ || card_session_->editor_manager() == nullptr) {
     return;
   }
@@ -218,7 +220,7 @@ void EditorMenuControllerImpl::OnEditorMenuVisibilityChanged(bool visible) {
   card_session_->editor_manager()->OnEditorMenuVisibilityChanged(visible);
 
   if (!visible) {
-    OnEditorCardHidden();
+    OnEditorCardHidden(destroy_session);
   }
 }
 
@@ -368,10 +370,10 @@ void EditorMenuControllerImpl::OnGetAnchorBoundsAndEditorContext(
   }
 }
 
-void EditorMenuControllerImpl::OnEditorCardHidden() {
+void EditorMenuControllerImpl::OnEditorCardHidden(bool destroy_session) {
   // The currently visible card is closing and being removed from the user's
   // view, the EditorCardSession has ended.
-  if (card_session_) {
+  if (destroy_session && card_session_) {
     card_session_.reset();
   }
 }
