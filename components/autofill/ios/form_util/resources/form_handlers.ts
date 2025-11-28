7dd34a7f90ea5c4f3dbf9ba2bf5a8c5b8ef1e168
Alexandra Pereira <alexandrasp@google.com>
2025-11-28 10:12:49 -0800

    [iOS][autofill] getFieldIdentifier as util function
    This is one of the multiple CLs that refactors Autofill features and
    resources, by removing functions from the gCrWeb object that are not
    used on the native side and only sharing them through export/import.
    
    It also improves code clarity and simplifies the adoption of the
    `gCrWeb` class.
    
    Bug: 448990422
    Change-Id: I02d7e949d5696d9e1f1c31a53b82022e289a9e97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7101460
    Reviewed-by: Sebastien S-G <sebsg@chromium.org>
    Commit-Queue: Alexandra Pereira <alexandrasp@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1551604}

--- components/autofill/ios/form_util/resources/form_handlers.ts
+++ components/autofill/ios/form_util/resources/form_handlers.ts
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {processChildFrameMessage} from '//components/autofill/ios/form_util/resources/child_frame_registration_lib.js';
 import {isAutofillableElement} from '//components/autofill/ios/form_util/resources/fill_element_inference_util.js';
 import * as fillUtil from '//components/autofill/ios/form_util/resources/fill_util.js';
-import {getFormIdentifier} from '//components/autofill/ios/form_util/resources/form_utils.js';
+import {getFieldIdentifier, getFormIdentifier} from '//components/autofill/ios/form_util/resources/form_utils.js';
 import {gCrWeb, gCrWebLegacy} from '//ios/web/public/js_messaging/resources/gcrweb.js';
 import {sendWebKitMessage} from '//ios/web/public/js_messaging/resources/utils.js';
 
@@ -182,7 +182,7 @@ function formActivity(evt: Event): void {
     'frameID': gCrWeb.getFrameId(),
     'formName': getFormIdentifier(form),
     'formRendererID': formRendererID,
-    'fieldIdentifier': gCrWebLegacy.form.getFieldIdentifier(field),
+    'fieldIdentifier': getFieldIdentifier(field),
     'fieldRendererID': fieldRendererID,
     'fieldType': fieldType,
     'type': evt.type,
