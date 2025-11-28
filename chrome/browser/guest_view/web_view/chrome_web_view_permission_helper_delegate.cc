de1dbc3b23b3d3084fb78888f2a53b5155e2127d
Palak Agarwal <agpalak@chromium.org>
2025-11-28 08:54:26 -0800

    Use only a single error reason when permission rejected by WebView
    This change converts the generic `PERMISSION_DENIED` errors to
    `PERMISSION_DENIED_BY_EMBEDDER_CONTEXT` when the permission is not
    allowed by WebView.
    
    This change also changes one of the `PERMISSION_DENIED` errors to
    `CAPTURE_NOT_ALLOWED_BY_POLICY` because it's only triggered when the
    policy is not enabled.
    
    Bug: 453600255
    Change-Id: Icc21ea46622fc2c32e7f8f9943becda75f3f347c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7205949
    Reviewed-by: Tove Petersson <tovep@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Palak Agarwal <agpalak@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1551582}

--- chrome/browser/guest_view/web_view/chrome_web_view_permission_helper_delegate.cc
+++ chrome/browser/guest_view/web_view/chrome_web_view_permission_helper_delegate.cc
@@ -208,7 +208,7 @@ void ChromeWebViewPermissionHelperDelegate::
     if (audio_denied || video_denied) {
       std::move(callback).Run(
           blink::mojom::StreamDevicesSet(),
-          blink::mojom::MediaStreamRequestResult::PERMISSION_DENIED,
+          blink::mojom::MediaStreamRequestResult::CAPTURE_NOT_ALLOWED_BY_POLICY,
           std::unique_ptr<content::MediaStreamUI>());
       return;
     }
@@ -233,10 +233,10 @@ void ChromeWebViewPermissionHelperDelegate::
         bool allow,
         const std::string& user_input) {
   if (!allow) {
-    std::move(callback).Run(
-        blink::mojom::StreamDevicesSet(),
-        blink::mojom::MediaStreamRequestResult::PERMISSION_DENIED,
-        std::unique_ptr<content::MediaStreamUI>());
+    std::move(callback).Run(blink::mojom::StreamDevicesSet(),
+                            blink::mojom::MediaStreamRequestResult::
+                                PERMISSION_DENIED_BY_EMBEDDER_CONTEXT,
+                            std::unique_ptr<content::MediaStreamUI>());
     return;
   }
   MediaCaptureDevicesDispatcher::GetInstance()->ProcessMediaAccessRequest(
