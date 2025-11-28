72807222e661d8b019a21b25de051f6e23d0ae67
Kamil Jarosz <kjarosz@google.com>
2025-11-28 13:52:34 -0800

    puma: Wire PumaService into IOSChromeMetricsServiceClient
    Wire PUMA service into the metrics service client on iOS. PUMA service
    is instantiated only behind a feature flag.
    
    Bug: b:452034784
    Change-Id: Ife5092e9ec9fb2cc4ce713f291cd9ee7a23aa5b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7205846
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Kamil Jarosz <kjarosz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1551657}

--- ios/chrome/browser/metrics/model/ios_chrome_metrics_service_client.mm
+++ ios/chrome/browser/metrics/model/ios_chrome_metrics_service_client.mm
@@ -56,6 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/metrics/net/net_metrics_log_uploader.h"
 #import "components/metrics/net/network_metrics_provider.h"
 #import "components/metrics/persistent_histograms.h"
+#import "components/metrics/private_metrics/puma_service.h"
 #import "components/metrics/stability_metrics_helper.h"
 #import "components/metrics/ui/form_factor_metrics_provider.h"
 #import "components/metrics/ui/screen_info_metrics_provider.h"
@@ -190,6 +191,7 @@ void IOSChromeMetricsServiceClient::RegisterPrefs(
   metrics::RegisterMetricsReportingStatePrefs(registry);
   ukm::UkmService::RegisterPrefs(registry);
   metrics::dwa::DwaService::RegisterPrefs(registry);
+  metrics::private_metrics::PumaService::RegisterPrefs(registry);
 }
 
 variations::SyntheticTrialRegistry*
@@ -207,6 +209,11 @@ ukm::UkmService* IOSChromeMetricsServiceClient::GetUkmService() {
 
 metrics::dwa::DwaService* IOSChromeMetricsServiceClient::GetDwaService() {
   return dwa_service_.get();
+}
+
+metrics::private_metrics::PumaService*
+IOSChromeMetricsServiceClient::GetPumaService() {
+  return puma_service_.get();
 }
 
 void IOSChromeMetricsServiceClient::SetMetricsClientId(
@@ -293,6 +300,11 @@ void IOSChromeMetricsServiceClient::Initialize() {
     dwa_service_ = std::make_unique<metrics::dwa::DwaService>(
         this, local_state,
         GetApplicationContext()->GetSharedURLLoaderFactory());
+  }
+
+  if (metrics::private_metrics::PumaService::IsPumaEnabled()) {
+    puma_service_ = std::make_unique<metrics::private_metrics::PumaService>(
+        this, local_state);
   }
 }
 
