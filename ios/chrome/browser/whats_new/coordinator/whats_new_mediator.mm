2d2a1550322a31e2f294c5d878d766bd319ddd51
Cheick Cisse <cheickcisse@google.com>
2025-11-28 15:50:15 -0800

    [ios][What's New] Add "Open New Tab" primary action
    This CL adds WhatsNewPrimaryAction::kNewTab to the supported primary
    actions in the What's New screen. When triggered, this action dismisses
    the What's New modal and opens the New Tab Page.
    
    Changes include:
    - Adding the kNewTab enum to WhatsNewPrimaryAction and updating
    whats_new_util.py to support it.
    - Implementing the action logic in WhatsNewMediator to dismiss the view
    and open kChromeUINewTabURL.
    - Refactoring WhatsNewMediator to use a helper method openNewTabWithUrl
    for code reuse.
    - Updating the "Enterprise Profiles" entry in whats_new_entries.plist
    to use this new action.
    - Updating the icon background color for docking promo from blue to green.
    
    Screen recording: https://drive.google.com/file/d/17SljZYyGP0KXwmoAsqyfhTLF_C9GCS2B/view?usp=sharing
    
    Bug: 464561913
    Change-Id: I4e25910d03288288c9328744261db127a6eb013b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7204240
    Reviewed-by: Pier-Alexandre Bouchard <pabouchard@google.com>
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Cr-Commit-Position: refs/heads/main@{#1551680}

--- ios/chrome/browser/whats_new/coordinator/whats_new_mediator.mm
+++ ios/chrome/browser/whats_new/coordinator/whats_new_mediator.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/default_browser/model/utils.h"
 #import "ios/chrome/browser/lens/ui_bundled/lens_entrypoint.h"
 #import "ios/chrome/browser/shared/coordinator/utils/credential_provider_settings_utils.h"
+#import "ios/chrome/browser/shared/model/url/chrome_url_constants.h"
 #import "ios/chrome/browser/shared/public/commands/application_commands.h"
 #import "ios/chrome/browser/shared/public/commands/lens_commands.h"
 #import "ios/chrome/browser/shared/public/commands/open_lens_input_selection_command.h"
@@ -71,6 +72,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       // Handles actions that open Chrome Password Manager.
       [self.settingsHandler showSavedPasswordsSettingsFromViewController:nil];
       break;
+    case WhatsNewPrimaryAction::kNewTab:
+      // Dismiss the What's New before opening a new tab.
+      [self.whatsNewHandler dismissWhatsNew];
+      // Handles actions that open a new tab.
+      [self openNewTabWithUrl:GURL(kChromeUINewTabURL)
+               transitionType:ui::PAGE_TRANSITION_TYPED];
+      break;
     case WhatsNewPrimaryAction::kNoAction:
     case WhatsNewPrimaryAction::kError:
       NOTREACHED();
@@ -79,9 +87,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)didTapLearnMoreButton:(const GURL&)learnMoreURL
                          type:(WhatsNewType)type {
-  UrlLoadParams params = UrlLoadParams::InNewTab(learnMoreURL);
-  params.web_params.transition_type = ui::PAGE_TRANSITION_AUTO_BOOKMARK;
-  self.urlLoadingAgent->Load(params);
+  [self openNewTabWithUrl:learnMoreURL
+           transitionType:ui::PAGE_TRANSITION_AUTO_BOOKMARK];
   base::UmaHistogramEnumeration("IOS.WhatsNew.LearnMoreTapped", type);
 }
 
@@ -140,6 +147,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)updateConsumer {
   [self.consumer setWhatsNewProperties:[self whatsNewChromeTipItems]
                           featureItems:[self whatsNewFeatureItems]];
+}
+
+// Opens a new tab with the given URL and transition type.
+- (void)openNewTabWithUrl:(const GURL&)url
+           transitionType:(ui::PageTransition)transitionType {
+  UrlLoadParams params = UrlLoadParams::InNewTab(url);
+  params.web_params.transition_type = transitionType;
+  self.urlLoadingAgent->Load(params);
 }
 
 @end
