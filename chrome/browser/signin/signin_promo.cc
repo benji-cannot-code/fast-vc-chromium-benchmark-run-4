d40cc95e5a87d2b00d698be1cf4fa421c0bcfc12
Kacper Bajkiewicz <kbajkiewicz@google.com>
2025-11-28 09:29:26 -0800

    [Growth] Record reprompt/signin count at which signin happened.
    Add new histograms and logging how many times promo used to signing into
    Chrome was shown before signin. Extracted logic for distinguishing
    states before signing in - signed out vs web signed in.
    
    Fixed: 458809362
    Change-Id: I436b8143604afa003e0c90a5e4ee9ba248d62cbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7205070
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Commit-Queue: Kacper Bajkiewicz <kbajkiewicz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1551591}

--- chrome/browser/signin/signin_promo.cc
+++ chrome/browser/signin/signin_promo.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/google/core/common/google_util.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/pref_service.h"
+#include "components/signin/public/base/signin_pref_names.h"
 #include "components/signin/public/base/signin_switches.h"
 #include "components/sync/base/features.h"
 #include "content/public/browser/browser_context.h"
