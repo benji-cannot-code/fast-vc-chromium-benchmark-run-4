d6f653579c18ca33837c915e666dc7b4ce65dd94
Allen Bauer <kylixrd@chromium.org>
2025-11-28 13:20:26 -0800

    [Omnibox][AIM] Observe WebUI popup visibility and reset if made invisible
    This observes the popup webui content and forces the visibility back to
    VISIBLE if it ever changes from that state. Also, instead of setting the
    initial size of the widget from within Show(), simply force the web
    content to be VISIBLE.
    
    Bug: b:462110941
    Change-Id: I8f988295e56c60ddd4c2c880148454f771911c7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7208359
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1551649}

--- chrome/browser/ui/views/omnibox/omnibox_popup_presenter_base.h
+++ chrome/browser/ui/views/omnibox/omnibox_popup_presenter_base.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "chrome/browser/ui/webui/searchbox/webui_omnibox_handler.h"
+#include "content/public/browser/web_contents_observer.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/views/view.h"
@@ -31,14 +32,14 @@ extern const void* kOmniboxWebUIPopupWidgetId;
 // this class is presentation only, i.e. Views and Widgets.  For omnibox logic
 // concerns and communication between native omnibox code and the WebUI code,
 // work with OmniboxPopupViewWebUI directly.
-class OmniboxPopupPresenterBase {
+class OmniboxPopupPresenterBase : public content::WebContentsObserver {
  public:
   DECLARE_CLASS_ELEMENT_IDENTIFIER_VALUE(kRoundedResultsFrame);
   explicit OmniboxPopupPresenterBase(LocationBarView* location_bar_view);
   OmniboxPopupPresenterBase(const OmniboxPopupPresenterBase&) = delete;
   OmniboxPopupPresenterBase& operator=(const OmniboxPopupPresenterBase&) =
       delete;
-  virtual ~OmniboxPopupPresenterBase();
+  ~OmniboxPopupPresenterBase() override;
 
   // Show or hide the popup widget with web view.
   virtual void Show();
@@ -93,6 +94,9 @@ class OmniboxPopupPresenterBase {
   // Returns the frame view of the widget if it exists. CHECKs if no widget
   // created
   RoundedOmniboxResultsFrame* GetResultsFrame() const;
+
+  // WebContentsObserver overrides:
+  void OnVisibilityChanged(content::Visibility visibility) override;
 
   // The location bar view that owns `this`.
   const raw_ptr<LocationBarView> location_bar_view_;
