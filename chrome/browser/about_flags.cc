68eee51eba507646a88efce5b05069d5a0fd2091
Rune Lillesveen <futhark@chromium.org>
2025-11-28 01:55:13 -0800

    Remove enable-aria-element-reflection feature flag
    The flag expired in M105. It is mapped directly to a blink runtime
    feature for which there is a base feature created that can be used
    instead from the command line if necessary.
    
    This CL is created with the use of Gemini CLI
    
    Bug: 356625602
    Change-Id: I75decfb55d2422f065d5d74d4d796be8ef904f7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7204933
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1551391}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -6582,10 +6582,7 @@ const FeatureEntry kFeatureEntries[] = {
      kOsAll,
      SINGLE_VALUE_TYPE(
          ::switches::kEnableExperimentalAccessibilityLanguageDetectionDynamic)},
-    {"enable-aria-element-reflection",
-     flag_descriptions::kAriaElementReflectionName,
-     flag_descriptions::kAriaElementReflectionDescription, kOsAll,
-     FEATURE_VALUE_TYPE(features::kEnableAriaElementReflection)},
+
 #if BUILDFLAG(IS_CHROMEOS)
     {"enable-cros-autocorrect-params-tuning",
      flag_descriptions::kAutocorrectParamsTuningName,
