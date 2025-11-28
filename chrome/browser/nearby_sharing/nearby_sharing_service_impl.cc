e913a2547fa7d86c8e531d786e10d42882f3d607
Georg Neis <neis@chromium.org>
2025-11-28 01:03:15 -0800

    Remove obsolete includes from c/b/nearby_sharing
    Bug: b:369688254
    Change-Id: I58792dc7f539983ad649c9f1a6d3a8421345b839
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7206977
    Auto-Submit: Georg Neis <neis@chromium.org>
    Reviewed-by: Di Wu <diwux@google.com>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1551371}

--- chrome/browser/nearby_sharing/nearby_sharing_service_impl.cc
+++ chrome/browser/nearby_sharing/nearby_sharing_service_impl.cc
@@ -51,8 +51,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/nearby_sharing/wifi_network_configuration/wifi_network_configuration_handler.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
-#include "chrome/browser/ui/browser_tabstrip.h"
-#include "chrome/browser/ui/scoped_tabbed_browser_displayer.h"
 #include "chrome/services/sharing/public/cpp/advertisement.h"
 #include "chrome/services/sharing/public/cpp/conversions.h"
 #include "chromeos/ash/components/nearby/common/connections_manager/nearby_connections_manager.h"
