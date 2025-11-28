58ced582693302b50fe7db8762f80e110613a88f
Kent Tamura <tkent@chromium.org>
2025-11-28 01:44:14 -0800

    Apply blink::StrCat() in renderer/core/t*/
    StrCat() is faster than operator+.
    This CL has no behavior changes.
    
    Bug: 419595599
    Change-Id: I509344fc8c821625ae49d0d71c5a35fc1dd17067
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7206176
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1551385}

--- third_party/blink/renderer/core/timing/background_tracing_helper.cc
+++ third_party/blink/renderer/core/timing/background_tracing_helper.cc
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/weborigin/security_origin.h"
 #include "third_party/blink/renderer/platform/wtf/text/ascii_ctype.h"
 #include "third_party/blink/renderer/platform/wtf/text/number_parsing_options.h"
-#include "third_party/blink/renderer/platform/wtf/text/string_operators.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_to_number.h"
 #include "url/url_constants.h"
 
