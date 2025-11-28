6d306041643c1f227302159103e6ebb8047390fd
Quentin Pubert <qpubert@google.com>
2025-11-28 07:51:34 -0800

    [iOS] Add file picker to file upload panel
    This CL adds a UIDocumentPickerViewController to the file upload panel
    to allow the selection of one or several files, or even the selection of
    a directory if that is allowed by the input element.
    
    Bug: 441659098
    Change-Id: I7f75825f99675d11ad815a3c8ca16a228a2fa03e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7145718
    Auto-Submit: Quentin Pubert <qpubert@google.com>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1551550}

--- ios/chrome/browser/download/model/download_app_interface.mm
+++ ios/chrome/browser/download/model/download_app_interface.mm
@@ -6,12 +6,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/download/model/download_app_interface.h"
 
 #import "base/apple/foundation_util.h"
+#import "base/files/file_enumerator.h"
 #import "base/files/file_path.h"
 #import "base/files/file_util.h"
+#import "base/functional/bind.h"
+#import "base/functional/callback.h"
+#import "base/strings/sys_string_conversions.h"
 #import "base/task/thread_pool.h"
+#import "base/threading/thread_restrictions.h"
 #import "ios/chrome/browser/download/model/download_directory_util.h"
 
 @implementation DownloadAppInterface
+
++ (void)deleteDownloadsDirectory {
+  base::FilePath downloadsDirectory;
+  GetDownloadsDirectory(&downloadsDirectory);
+  base::ThreadPool::PostTask(
+      FROM_HERE, {base::MayBlock(), base::TaskPriority::USER_BLOCKING},
+      base::BindOnce(base::IgnoreResult(&base::DeletePathRecursively),
+                     downloadsDirectory));
+}
 
 + (void)deleteDownloadsDirectoryFileWithName:(NSString*)fileName {
   NSURL* fileURL = [NSURL fileURLWithPath:fileName];
@@ -20,8 +34,40 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   base::FilePath filePath =
       downloadsDirectory.Append(base::apple::NSURLToFilePath(fileURL));
   base::ThreadPool::PostTask(
-      FROM_HERE, {base::MayBlock(), base::TaskPriority::BEST_EFFORT},
+      FROM_HERE, {base::MayBlock(), base::TaskPriority::USER_BLOCKING},
       base::BindOnce(base::IgnoreResult(&base::DeleteFile), filePath));
+}
+
++ (void)createDownloadsDirectoryFileWithName:(NSString*)fileName
+                                     content:(NSString*)content {
+  base::FilePath downloadsDirectory;
+  GetDownloadsDirectory(&downloadsDirectory);
+  base::ThreadPool::PostTask(
+      FROM_HERE, {base::MayBlock(), base::TaskPriority::USER_BLOCKING},
+      base::BindOnce(base::IgnoreResult(base::CreateDirectory),
+                     downloadsDirectory)
+          .Then(base::BindOnce(
+              [](const base::FilePath& filePath, std::string fileContent) {
+                base::WriteFile(filePath, fileContent);
+              },
+              downloadsDirectory.Append(base::SysNSStringToUTF8(fileName)),
+              base::SysNSStringToUTF8(content))));
+}
+
++ (int)fileCountInDownloadsDirectory {
+  base::ScopedAllowBlockingForTesting allow_blocking;
+  base::FilePath downloadsDirectory;
+  GetDownloadsDirectory(&downloadsDirectory);
+  int fileCount = 0;
+  if (base::DirectoryExists(downloadsDirectory)) {
+    base::FileEnumerator enumerator(downloadsDirectory, false,
+                                    base::FileEnumerator::FILES);
+    for (base::FilePath name = enumerator.Next(); !name.empty();
+         name = enumerator.Next()) {
+      fileCount++;
+    }
+  }
+  return fileCount;
 }
 
 @end
