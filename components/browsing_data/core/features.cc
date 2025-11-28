942557c8abc6bd5633ccefa9a2fe03440aee1a2c
Zaina Al-Mashni <zalmashni@google.com>
2025-11-28 10:15:42 -0800

    [History] Add feature enablement check for actor history integration M1
    Migrate the M1 feature usages to use
    IsBrowsingHistoryActorIntegrationM1Enabled() which checks for the
    enablement of any History Actor Integration related flag.
    
    Fixed: 464349668
    Change-Id: Ie2101cbde921248201bd12fc2722bb542f39fc04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7207193
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1551609}

--- components/browsing_data/core/features.cc
+++ components/browsing_data/core/features.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/browsing_data/core/features.h"
 
 #include "build/build_config.h"
+#include "components/history/core/browser/features.h"
 
 namespace browsing_data::features {
 #if BUILDFLAG(IS_ANDROID)
@@ -16,5 +17,11 @@ BASE_FEATURE(kBrowsingDataModel, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kDbdRevampDesktop, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kBrowsingHistoryActorIntegrationM1,
              base::FEATURE_ENABLED_BY_DEFAULT);
+
+bool IsBrowsingHistoryActorIntegrationM1Enabled() {
+  return base::FeatureList::IsEnabled(kBrowsingHistoryActorIntegrationM1) ||
+         base::FeatureList::IsEnabled(
+             history::kBrowsingHistoryActorIntegrationM2);
+}
 #endif  // !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
 }  // namespace browsing_data::features
