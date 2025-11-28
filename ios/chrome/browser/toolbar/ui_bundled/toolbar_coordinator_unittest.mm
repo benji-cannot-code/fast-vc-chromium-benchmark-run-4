60ba749e3337062c02f7fe95546ac9b7b697baae
christianxu <christianxu@chromium.org>
2025-11-28 02:46:11 -0800

    [iOS][Composebox] Fix toolbar test with composebox enabled
    Add missing command handler when composebox is enabled.
    
    Bug: None
    Change-Id: I2de3f4541cf5d5c789c7bbd65992c2f6dd758fea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7205960
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1551406}

--- ios/chrome/browser/toolbar/ui_bundled/toolbar_coordinator_unittest.mm
+++ ios/chrome/browser/toolbar/ui_bundled/toolbar_coordinator_unittest.mm
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_opener.h"
 #import "ios/chrome/browser/shared/public/commands/activity_service_commands.h"
 #import "ios/chrome/browser/shared/public/commands/application_commands.h"
+#import "ios/chrome/browser/shared/public/commands/browser_coordinator_commands.h"
 #import "ios/chrome/browser/shared/public/commands/bwg_commands.h"
 #import "ios/chrome/browser/shared/public/commands/command_dispatcher.h"
 #import "ios/chrome/browser/shared/public/commands/contextual_panel_entrypoint_iph_commands.h"
@@ -158,6 +159,12 @@ class ToolbarCoordinatorTest : public PlatformTest {
         startDispatchingToTarget:mock_contextual_panel_entrypoint_IPH_handler
                      forProtocol:@protocol(
                                      ContextualPanelEntrypointIPHCommands)];
+
+    id mock_browser_coordinator_commands =
+        OCMProtocolMock(@protocol(BrowserCoordinatorCommands));
+    [browser_->GetCommandDispatcher()
+        startDispatchingToTarget:mock_browser_coordinator_commands
+                     forProtocol:@protocol(BrowserCoordinatorCommands)];
 
     OmniboxPositionBrowserAgent::CreateForBrowser(browser_.get());
     // FullscreenController depends on ToolbarsSizeBrowserAgent, so the agent
