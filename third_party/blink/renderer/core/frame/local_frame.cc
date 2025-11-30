4f557a2b2c77a2bb1c76c6fbe79df797bd2ad44a
Fredrik Söderquist <fs@opera.com>
2025-11-30 15:07:05 -0800

    Don't use WebNode in LocalFrame::RequestVideoFrameAtWithBoundsHint
    No need to go through WebNode/WebElement when not passing out through
    the public API.
    
    Change-Id: Ia15ddbe4044083891421acc2768ba1f0e40be6ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7209455
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1551877}

--- third_party/blink/renderer/core/frame/local_frame.cc
+++ third_party/blink/renderer/core/frame/local_frame.cc
@@ -3739,9 +3739,7 @@ void LocalFrame::RequestVideoFrameAtWithBoundsHint(
     int max_area,
     base::OnceCallback<void(const SkBitmap&, const gfx::Rect&)> callback) {
   HitTestResult result = HitTestResultForVisualViewportPos(viewport_position);
-  Node* node = result.InnerNode();
-  auto* video = DynamicTo<HTMLVideoElement>(node);
-
+  auto* video = DynamicTo<HTMLVideoElement>(result.InnerNode());
   if (!video) {
     std::move(callback).Run(SkBitmap(), gfx::Rect());
     return;
@@ -3785,9 +3783,7 @@ void LocalFrame::RequestVideoFrameAtWithBoundsHint(
   }
 
   // Get the bounds of the video element.
-  WebNode web_node(node);
-  WebElement web_element = web_node.To<WebElement>();
-  auto bounds = web_element.BoundsInWidget();
+  gfx::Rect bounds = video->BoundsInWidget();
 
   std::move(callback).Run(converted_bitmap, bounds);
 }
