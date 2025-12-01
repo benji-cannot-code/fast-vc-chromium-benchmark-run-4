091b1862f471afa7936cc255f52756b6baefba2a
Sidhin Thomas <sidhin.thomas@gmail.com>
2025-12-01 12:08:45 -0800

    Remove unneeded mojom include from glic_button_controller
    Bug: None
    Change-Id: I879b033c65d629365dbf3de59d69fb836e90ead2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7213032
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1552345}

--- chrome/browser/glic/browser_ui/glic_button_controller.h
+++ chrome/browser/glic/browser_ui/glic_button_controller.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_list.h"
 #include "base/memory/raw_ptr.h"
-#include "chrome/browser/glic/host/glic.mojom.h"
 #include "chrome/browser/glic/widget/glic_window_controller.h"
 #include "components/prefs/pref_change_registrar.h"
 
