d1186cb3614c413fcda67b83b0bfa91804a84e2f
Nir Chachamovitz <nir.chachamovitz@island.io>
2025-12-01 13:38:59 -0800

    fix arm64 is_running_in_vm_ value
    base/cpu.h initializes is_running_in_vm_ to false,
    and it is only modified when running on ARCH_CPU_X86_FAMILY.
    Therefore, in Mac VMs, this boolean always remains false.
    Introduced due to recent bug in Mac 26.1,
    in which the serial number published for the virtual machine is 0.
    
    Bug: 463283603
    Change-Id: Ib84921481fba5fa4eceea6ac2078e49635849e49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7201806
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Michael Maltsev <michaelm@island.io>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1552404}

--- base/cpu.cc
+++ base/cpu.cc
@@ -44,6 +44,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 #endif
 
+#if BUILDFLAG(IS_MAC)
+#include "base/mac/mac_util.h"
+#endif
+
 namespace base {
 
 #if defined(ARCH_CPU_X86_FAMILY)
@@ -152,6 +156,10 @@ DEFINE_PROTECTED_DATA base::ProtectedMemory<CPU> g_cpu_instance;
 }  // namespace
 
 void CPU::Initialize() {
+#if BUILDFLAG(IS_MAC)
+  is_running_in_vm_ = mac::IsVirtualMachine();
+#endif
+
 #if defined(ARCH_CPU_X86_FAMILY)
   int cpu_info[4] = {-1, 0, 0, 0};
 
@@ -205,7 +213,9 @@ void CPU::Initialize() {
     // This is checking for any hypervisor. Hypervisors may choose not to
     // announce themselves. Hypervisors trap CPUID and sometimes return
     // different results to underlying hardware.
+#if !BUILDFLAG(IS_MAC)
     is_running_in_vm_ = (static_cast<uint32_t>(cpu_info[2]) & 0x80000000) != 0;
+#endif
 
     // AVX instructions will generate an illegal instruction exception unless
     //   a) they are supported by the CPU,
