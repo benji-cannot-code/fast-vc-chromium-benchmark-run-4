3ce218b2d57f4576984e8481433979865f309e52
Lei Zhang <thestig@chromium.org>
2025-12-01 14:57:52 -0800

    PartitionAlloc: Remove RawPtrTraits::kDisableBRP
    This trait does not appear to be used.
    
    Change-Id: Ib0ab1c33fe8502208ef3299572512726a91eae54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7211181
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Ali Hijazi <ahijazi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1552447}

--- base/allocator/partition_allocator/src/partition_alloc/pointers/raw_ptr_backup_ref_impl.cc
+++ base/allocator/partition_allocator/src/partition_alloc/pointers/raw_ptr_backup_ref_impl.cc
@@ -23,9 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base::internal {
 
-template <bool AllowDangling, bool DisableBRP>
-void RawPtrBackupRefImpl<AllowDangling, DisableBRP>::AcquireInternal(
-    uintptr_t address) {
+template <bool AllowDangling>
+void RawPtrBackupRefImpl<AllowDangling>::AcquireInternal(uintptr_t address) {
 #if PA_BUILDFLAG(DCHECKS_ARE_ON) || \
     PA_BUILDFLAG(ENABLE_BACKUP_REF_PTR_SLOW_CHECKS)
   PA_BASE_CHECK(UseBrp(address));
@@ -43,9 +42,8 @@ void RawPtrBackupRefImpl<AllowDangling, DisableBRP>::AcquireInternal(
   }
 }
 
-template <bool AllowDangling, bool DisableBRP>
-void RawPtrBackupRefImpl<AllowDangling, DisableBRP>::ReleaseInternal(
-    uintptr_t address) {
+template <bool AllowDangling>
+void RawPtrBackupRefImpl<AllowDangling>::ReleaseInternal(uintptr_t address) {
 #if PA_BUILDFLAG(DCHECKS_ARE_ON) || \
     PA_BUILDFLAG(ENABLE_BACKUP_REF_PTR_SLOW_CHECKS)
   PA_BASE_CHECK(UseBrp(address));
@@ -73,8 +71,8 @@ void RawPtrBackupRefImpl<AllowDangling, DisableBRP>::ReleaseInternal(
   }
 }
 
-template <bool AllowDangling, bool DisableBRP>
-void RawPtrBackupRefImpl<AllowDangling, DisableBRP>::ReportIfDanglingInternal(
+template <bool AllowDangling>
+void RawPtrBackupRefImpl<AllowDangling>::ReportIfDanglingInternal(
     uintptr_t address) {
   if (partition_alloc::internal::IsUnretainedDanglingRawPtrCheckEnabled()) {
     if (IsSupportedAndNotNull(address)) {
@@ -88,11 +86,11 @@ void RawPtrBackupRefImpl<AllowDangling, DisableBRP>::ReportIfDanglingInternal(
 }
 
 // static
-template <bool AllowDangling, bool DisableBRP>
-bool RawPtrBackupRefImpl<AllowDangling, DisableBRP>::
-    CheckPointerWithinSameAlloc(uintptr_t before_addr,
-                                uintptr_t after_addr,
-                                size_t type_size) {
+template <bool AllowDangling>
+bool RawPtrBackupRefImpl<AllowDangling>::CheckPointerWithinSameAlloc(
+    uintptr_t before_addr,
+    uintptr_t after_addr,
+    size_t type_size) {
   partition_alloc::internal::PtrPosWithinAlloc ptr_pos_within_alloc =
       partition_alloc::internal::IsPtrWithinSameAlloc(before_addr, after_addr,
                                                       type_size);
@@ -110,9 +108,8 @@ bool RawPtrBackupRefImpl<AllowDangling, DisableBRP>::
 #endif
 }
 
-template <bool AllowDangling, bool DisableBRP>
-bool RawPtrBackupRefImpl<AllowDangling, DisableBRP>::IsPointeeAlive(
-    uintptr_t address) {
+template <bool AllowDangling>
+bool RawPtrBackupRefImpl<AllowDangling>::IsPointeeAlive(uintptr_t address) {
 #if PA_BUILDFLAG(DCHECKS_ARE_ON) || \
     PA_BUILDFLAG(ENABLE_BACKUP_REF_PTR_SLOW_CHECKS)
   PA_BASE_CHECK(UseBrp(address));
@@ -128,14 +125,8 @@ bool RawPtrBackupRefImpl<AllowDangling, DisableBRP>::IsPointeeAlive(
 
 // Explicitly instantiates the two BackupRefPtr variants in the .cc. This
 // ensures the definitions not visible from the .h are available in the binary.
-template struct RawPtrBackupRefImpl</*AllowDangling=*/false,
-                                    /*DisableBRP=*/false>;
-template struct RawPtrBackupRefImpl</*AllowDangling=*/false,
-                                    /*DisableBRP=*/true>;
-template struct RawPtrBackupRefImpl</*AllowDangling=*/true,
-                                    /*DisableBRP=*/false>;
-template struct RawPtrBackupRefImpl</*AllowDangling=*/true,
-                                    /*DisableBRP=*/true>;
+template struct RawPtrBackupRefImpl</*AllowDangling=*/false>;
+template struct RawPtrBackupRefImpl</*AllowDangling=*/true>;
 
 #if PA_BUILDFLAG(DCHECKS_ARE_ON) || \
     PA_BUILDFLAG(ENABLE_BACKUP_REF_PTR_SLOW_CHECKS)
