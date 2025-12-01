63a4f286db7e77e028af6f609a8722068849ac60
Rakina Zata Amni <rakina@chromium.org>
2025-12-01 01:04:32 -0800

    Rename IsInitialWebUIScheme -> IsInitialWebUIURL
    We're not only checking the scheme, but also the path, so rename it
    to reflect what the actual check is.
    
    Bug: 457618572
    Change-Id: I93bdbc912f1e456c11da891c1b4cca26f7742966
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7201667
    Auto-Submit: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Ming-Ying Chung <mych@chromium.org>
    Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1551961}

--- content/public/browser/content_browser_client.cc
+++ content/public/browser/content_browser_client.cc
@@ -416,7 +416,7 @@ bool ContentBrowserClient::ShouldUrlUseApplicationIsolationLevel(
 }
 
 #if !BUILDFLAG(IS_ANDROID)
-bool ContentBrowserClient::IsInitialWebUIScheme(const GURL& url) {
+bool ContentBrowserClient::IsInitialWebUIURL(const GURL& url) {
   return false;
 }
 #endif  // !BUILDFLAG(IS_ANDROID)
