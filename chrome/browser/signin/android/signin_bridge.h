8da22c83c90e1e0a1d5ca0c2e952f26ddc0cdb07
Liza Bipin <mlbipin@google.com>
2025-12-01 02:32:35 -0800

    [Signin][Android] Redirect accounts once added via ADD_SESSION header
    * Once the account is added wait for cookies and refresh tokens to be
    ready before redirection. Video of updated flow in bug.
    
    * Tests and error handling will be added in a follow up CL.
    
    Bug: 456445865
    Change-Id: I571a67c93959f69dbf0beafb98639a99b50bd15e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7099021
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Liza Bipin <mlbipin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1551984}

--- chrome/browser/signin/android/signin_bridge.h
+++ chrome/browser/signin/android/signin_bridge.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "chrome/browser/android/tab_android.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/signin/core/browser/signin_header_helper.h"
 #include "components/signin/public/base/signin_metrics.h"
@@ -28,7 +29,7 @@ class SigninBridge : public KeyedService {
 
   // Opens a add account flow pre-filled with |prefilled_email| that opens
   // the specified |continue_url| upon completion.
-  virtual void StartAddAccountFlow(ui::WindowAndroid* window,
+  virtual void StartAddAccountFlow(TabAndroid* tab,
                                    const std::string& prefilled_email,
                                    const GURL& continue_url);
 
--- components/signin/public/browser/web_signin_tracker.h
+++ components/signin/public/browser/web_signin_tracker.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_SIGNIN_PUBLIC_BROWSER_WEB_SIGNIN_TRACKER_H_
 
 #include <optional>
+#include <string>
 
 #include "base/functional/callback_forward.h"
 #include "base/memory/raw_ptr.h"
@@ -41,7 +42,7 @@ class WebSigninTracker : public IdentityManager::Observer,
 
   WebSigninTracker(IdentityManager* identity_manager,
                    AccountReconcilor* account_reconcilor,
-                   CoreAccountId signin_account,
+                   std::variant<CoreAccountId, std::string> signin_account,
                    base::OnceCallback<void(Result)> callback,
                    std::optional<base::TimeDelta> timeout = std::nullopt);
 
@@ -62,9 +63,14 @@ class WebSigninTracker : public IdentityManager::Observer,
  private:
   void OnTimeoutReached();
   void FinishWithResult(WebSigninTracker::Result result);
+  bool MatchesRequestedAccount(const CoreAccountId& account_id,
+                               const std::string& account_email);
 
   const raw_ptr<IdentityManager> identity_manager_;
-  CoreAccountId signin_account_;
+  const raw_ptr<AccountReconcilor> account_reconcilor_;
+  // Holds either the CoreAccountId of the signed-in account, or the email
+  // address of the account has not been made available in IdentityManager yet.
+  std::variant<CoreAccountId, std::string> signin_account_;
   base::OnceCallback<void(Result)> callback_;
   base::ScopedObservation<IdentityManager, IdentityManager::Observer>
       identity_manager_observation_{this};
