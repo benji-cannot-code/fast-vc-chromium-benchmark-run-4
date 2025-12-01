f471a4c64682967f1db25defd8746f14f0dd001f
Aishwarya Rajesh <aishwaryarj@google.com>
2025-12-01 11:43:59 -0800

    [RWM] Support clearing custom name on name window dialog
    After the updates in this CL, a user can save an empty name on the name
    window dialog - this will clear a previously set custom name and / or
    use the active tab title as the window name subsequently.
    
    Before:
    https://screencast.googleplex.com/cast/NDc2Nzc0MjYzOTk5NjkyOHxjYjA4Mjk3ZC01YQ
    
    After:
    https://screencast.googleplex.com/cast/NDc2NTAxNTYwNDE5OTQyNHw5ZTVjZjc5Mi0xZQ
    
    Bug: 453688108
    Change-Id: I2643328c5c1b2d581d6c3c6a8b65e8b3e3605b96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7207551
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1552313}

--- chrome/browser/ui/android/multiwindow/java/src/org/chromium/chrome/browser/multiwindow/UiUtils.java
+++ chrome/browser/ui/android/multiwindow/java/src/org/chromium/chrome/browser/multiwindow/UiUtils.java
@@ -23,7 +23,6 @@ import androidx.annotation.IntDef;
 import androidx.annotation.VisibleForTesting;
 
 import com.google.android.material.textfield.TextInputEditText;
-import com.google.android.material.textfield.TextInputLayout;
 
 import org.chromium.base.Callback;
 import org.chromium.base.metrics.RecordUserAction;
@@ -147,7 +146,6 @@ public class UiUtils {
         ((TextView) dialog.findViewById(R.id.title))
                 .setText(res.getString(R.string.instance_switcher_name_window_confirm_header));
 
-        TextInputLayout textInputLayout = dialog.findViewById(R.id.new_window_title);
         TextInputEditText editText = dialog.findViewById(R.id.title_input_text);
         editText.setText(currentTitle);
         editText.requestFocus();
@@ -166,13 +164,10 @@ public class UiUtils {
                             recordChangeWindowNameUserAction(source);
                             nameChangedCallback.onResult(newTitle);
                         }
-                        dialog.dismiss();
                     } else {
-                        textInputLayout.setError(
-                                context.getString(
-                                        R.string.instance_switcher_name_window_missing_title));
-                        textInputLayout.requestFocus();
+                        nameChangedCallback.onResult(newTitle);
                     }
+                    dialog.dismiss();
                 });
 
         TextView negativeButton = dialog.findViewById(R.id.negative_button);
