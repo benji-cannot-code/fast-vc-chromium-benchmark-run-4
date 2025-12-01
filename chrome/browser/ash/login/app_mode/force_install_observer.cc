ccc363ffac2712850327f7d5f880b0416e3c9334
Irina <irfedorova@google.com>
2025-12-01 08:46:57 -0800

    kiosk: remove Kiosk.Extensions.InstallError.*
    Kiosk.Extensions.InstallError.OffStore and
    Kiosk.Extensions.InstallError.WebStore are not used in kiosk reliability
    dashboard, so they should be deleted.
    
    OBSOLETE_HISTOGRAMS=remove unused metric
    
    Fixed: 415782092
    Change-Id: If1963c37c4373b7466aceb20d50f4cdb506bf51a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7209379
    Commit-Queue: Irina Fedorova <irfedorova@google.com>
    Reviewed-by: Polina Bondarenko <pbond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1552185}

--- chrome/browser/ash/login/app_mode/force_install_observer.cc
+++ chrome/browser/ash/login/app_mode/force_install_observer.cc
@@ -59,18 +59,6 @@ bool IsExtensionInstallForcelistPolicyValid() {
   return errors.GetErrors(policy::key::kExtensionInstallForcelist).empty();
 }
 
-void RecordKioskExtensionInstallError(
-    extensions::InstallStageTracker::FailureReason reason,
-    bool is_from_store) {
-  if (is_from_store) {
-    base::UmaHistogramEnumeration("Kiosk.Extensions.InstallError.WebStore",
-                                  reason);
-  } else {
-    base::UmaHistogramEnumeration("Kiosk.Extensions.InstallError.OffStore",
-                                  reason);
-  }
-}
-
 void RecordKioskExtensionInstallTimedOut(bool timeout) {
   UMA_HISTOGRAM_BOOLEAN("Kiosk.Extensions.InstallTimedOut", timeout);
 }
@@ -120,15 +108,6 @@ void ForceInstallObserver::OnExtensionWaitTimeOut() {
 void ForceInstallObserver::OnForceInstalledExtensionsReady() {
   RecordKioskExtensionInstallTimedOut(false);
   ReportDone();
-}
-
-void ForceInstallObserver::OnForceInstalledExtensionFailed(
-    const extensions::ExtensionId& installation_id,
-    extensions::InstallStageTracker::FailureReason reason,
-    bool is_from_store) {
-  // We will still receive the OnForceInstalledExtensionsReady callback, so only
-  // log this failure.
-  RecordKioskExtensionInstallError(reason, is_from_store);
 }
 
 void ForceInstallObserver::ReportDone() {
