9dde69525479a45e34ea2701a1e18db25604ea37
Nicolas Dossou-Gbete <dgn@chromium.org>
2025-12-01 09:00:04 -0800

    Reland "rcaps: Add histograms to monitor the outcome of pending display data"
    This is a reland of commit bd4cc2e8b413c143cb2228723cbcd1addeff72ba
    
    Original change's description:
    > rcaps: Add histograms to monitor the outcome of pending display data
    >
    > "Search.ChoicePrefsCheck.PendingChoiceScreenDisplayStateStatus" will be
    > recorded when the pending display data is processed, reporting the
    > outcome of the operation.
    >
    > Bug: 462686031
    > Change-Id: I231f0c5a975586bc88bf210e634b570b1908a370
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7184942
    > Reviewed-by: James Lee <ljjlee@google.com>
    > Auto-Submit: Nicolas Dossou-Gbété <dgn@chromium.org>
    > Commit-Queue: James Lee <ljjlee@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1551487}
    
    Bug: 462686031
    Change-Id: I883737d2dab1184c9f54c2cf26e68af462a4de60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7210634
    Auto-Submit: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: James Lee <ljjlee@google.com>
    Commit-Queue: James Lee <ljjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1552194}

--- components/search_engines/search_engine_choice/search_engine_choice_service_unittest.cc
+++ components/search_engines/search_engine_choice/search_engine_choice_service_unittest.cc
@@ -475,6 +475,7 @@ class SearchEngineChoiceServiceDisplayStateRecordTest
   struct DisplayStateRecordExpectations {
     HistogramExpectation country_mismatch;
     HistogramExpectation selected_index;
+    HistogramExpectation display_state_status;
     std::vector<HistogramExpectation> impression_at_index;
   };
 
@@ -492,6 +493,11 @@ class SearchEngineChoiceServiceDisplayStateRecordTest
     CheckHistogramExpectation(
         histogram_tester, kPumaSearchChoiceScreenSelectedEngineIndexHistogram,
         expectations.selected_index, location);
+
+    CheckHistogramExpectation(
+        histogram_tester,
+        "Search.ChoicePrefsCheck.PendingChoiceScreenDisplayStateStatus",
+        expectations.display_state_status, location);
 
     ASSERT_LE(expectations.impression_at_index.size(), kMaxRegionalListSize);
     for (size_t i = 0; i < kMaxRegionalListSize; ++i) {
@@ -535,6 +541,7 @@ TEST_F(SearchEngineChoiceServiceDisplayStateRecordTest, Record) {
       histogram_tester,
       {.country_mismatch = ExpectHistogramBucket(false),
        .selected_index = ExpectHistogramBucket(2),
+       .display_state_status = ExpectHistogramNever(),
        .impression_at_index = {ExpectHistogramBucket(SEARCH_ENGINE_GOOGLE),
                                ExpectHistogramBucket(SEARCH_ENGINE_BING),
                                ExpectHistogramBucket(SEARCH_ENGINE_YAHOO)}});
@@ -575,6 +582,7 @@ TEST_F(SearchEngineChoiceServiceDisplayStateRecordTest, Record_Taiyaki) {
       histogram_tester,
       {.country_mismatch = ExpectHistogramBucket(false),
        .selected_index = ExpectHistogramBucket(2),
+       .display_state_status = ExpectHistogramNever(),
        .impression_at_index = {ExpectHistogramBucket(SEARCH_ENGINE_GOOGLE),
                                ExpectHistogramBucket(SEARCH_ENGINE_BING),
                                ExpectHistogramBucket(SEARCH_ENGINE_YAHOO)}});
@@ -610,6 +618,7 @@ TEST_F(SearchEngineChoiceServiceDisplayStateRecordTest,
       histogram_tester,
       {.country_mismatch = ExpectHistogramBucket(false),
        .selected_index = ExpectHistogramBucket(2),
+       .display_state_status = ExpectHistogramNever(),
        .impression_at_index = {ExpectHistogramBucket(SEARCH_ENGINE_GOOGLE),
                                ExpectHistogramBucket(SEARCH_ENGINE_BING),
                                ExpectHistogramBucket(SEARCH_ENGINE_YAHOO)}});
@@ -642,6 +651,7 @@ TEST_F(SearchEngineChoiceServiceDisplayStateRecordTest,
   CheckExpectations(histogram_tester,
                     {.country_mismatch = ExpectHistogramNever(),
                      .selected_index = ExpectHistogramNever(),
+                     .display_state_status = ExpectHistogramNever(),
                      .impression_at_index = {}});
 
   // The choice is coming from a non-eea country and won't be logged, don't
@@ -665,6 +675,7 @@ TEST_F(SearchEngineChoiceServiceDisplayStateRecordTest,
   CheckExpectations(histogram_tester,
                     {.country_mismatch = ExpectHistogramNever(),
                      .selected_index = ExpectHistogramNever(),
+                     .display_state_status = ExpectHistogramNever(),
                      .impression_at_index = {}});
 
   // The choice is coming from a non-eea country and won't be logged, don't
@@ -696,6 +707,7 @@ TEST_F(SearchEngineChoiceServiceDisplayStateRecordTest,
   CheckExpectations(histogram_tester,
                     {.country_mismatch = ExpectHistogramBucket(true),
                      .selected_index = ExpectHistogramBucket(0),
+                     .display_state_status = ExpectHistogramNever(),
                      .impression_at_index = {}});
 
   // The choice screen state should be cached for a next chance later.
@@ -733,6 +745,7 @@ TEST_F(SearchEngineChoiceServiceDisplayStateRecordTest, RecordFromCache) {
       histogram_tester,
       {.country_mismatch = ExpectHistogramNever(),
        .selected_index = ExpectHistogramNever(),
+       .display_state_status = ExpectHistogramBucket(2 /* kUploaded */),
        .impression_at_index = {ExpectHistogramBucket(SEARCH_ENGINE_GOOGLE),
                                ExpectHistogramBucket(SEARCH_ENGINE_BING),
                                ExpectHistogramBucket(SEARCH_ENGINE_YAHOO)}});
@@ -765,12 +778,44 @@ TEST_F(SearchEngineChoiceServiceDisplayStateRecordTest,
       histogram_tester,
       {.country_mismatch = ExpectHistogramNever(),
        .selected_index = ExpectHistogramNever(),
+       .display_state_status = ExpectHistogramBucket(2 /* kUploaded */),
        .impression_at_index = {ExpectHistogramBucket(SEARCH_ENGINE_GOOGLE),
                                ExpectHistogramBucket(SEARCH_ENGINE_BING),
                                ExpectHistogramBucket(SEARCH_ENGINE_YAHOO)}});
 
-  // The choice screen state should stay around.
+  // The choice screen state should now be cleared.
   EXPECT_FALSE(pref_service()->HasPrefPath(
+      prefs::kDefaultSearchProviderPendingChoiceScreenDisplayState));
+}
+
+TEST_F(SearchEngineChoiceServiceDisplayStateRecordTest,
+       RecordFromCacheSkipped_VariationsCountryMismatch) {
+  ChoiceScreenDisplayState display_state(
+      /*search_engines=*/{SEARCH_ENGINE_GOOGLE, SEARCH_ENGINE_BING,
+                          SEARCH_ENGINE_YAHOO},
+      /*country_id=*/kBelgiumCountryId,
+      /*is_current_default_search_presented=*/false,
+      /*includes_non_regional_set_engine=*/false,
+      /*selected_engine_index=*/0);
+  pref_service()->SetDict(
+      prefs::kDefaultSearchProviderPendingChoiceScreenDisplayState,
+      display_state.ToDict());
+  search_engines::MarkSearchEngineChoiceCompletedForTesting(*pref_service());
+
+  base::HistogramTester histogram_tester;
+  InitService({.variation_country_id = kUsaCountryId,
+               .client_country_id = kBelgiumCountryId,
+               .force_reset = true});
+
+  CheckExpectations(
+      histogram_tester,
+      {.country_mismatch = ExpectHistogramNever(),
+       .selected_index = ExpectHistogramNever(),
+       .display_state_status = ExpectHistogramBucket(3 /* kStayPending */),
+       .impression_at_index = {}});
+
+  // The choice screen state should stay around.
+  EXPECT_TRUE(pref_service()->HasPrefPath(
       prefs::kDefaultSearchProviderPendingChoiceScreenDisplayState));
 }
 
@@ -790,12 +835,79 @@ TEST_F(SearchEngineChoiceServiceDisplayStateRecordTest,
   base::HistogramTester histogram_tester;
   InitService({.force_reset = true});
 
-  CheckExpectations(histogram_tester,
-                    {.country_mismatch = ExpectHistogramNever(),
-                     .selected_index = ExpectHistogramNever(),
-                     .impression_at_index = {}});
+  CheckExpectations(
+      histogram_tester,
+      {.country_mismatch = ExpectHistogramNever(),
+       .selected_index = ExpectHistogramNever(),
+       .display_state_status = ExpectHistogramBucket(1 /* kTimedOut */),
+       .impression_at_index = {}});
 
-  // Choice not marked done, so the service also clear the pending state.
+  // Choice not marked done, so the service also clears the pending state.
+  EXPECT_FALSE(pref_service()->HasPrefPath(
+      prefs::kDefaultSearchProviderPendingChoiceScreenDisplayState));
+}
+
+TEST_F(SearchEngineChoiceServiceDisplayStateRecordTest,
+       RecordFromCacheCancelled_TimedOut) {
+  ChoiceScreenDisplayState display_state(
+      /*search_engines=*/{SEARCH_ENGINE_GOOGLE, SEARCH_ENGINE_BING,
+                          SEARCH_ENGINE_YAHOO},
+      /*country_id=*/kBelgiumCountryId,
+      /*is_current_default_search_presented=*/false,
+      /*includes_non_regional_set_engine=*/false,
+      /*selected_engine_index=*/0);
+  pref_service()->SetDict(
+      prefs::kDefaultSearchProviderPendingChoiceScreenDisplayState,
+      display_state.ToDict());
+
+  auto metadata = CreateChoiceCompletionMetadataWithProgram(
+      regional_capabilities::SerializeProgram(
+          regional_capabilities::Program::kWaffle));
+  metadata.timestamp = base::Time::Now() - base::Days(28);
+  search_engines::MarkSearchEngineChoiceCompletedForTesting(*pref_service(),
+                                                            metadata);
+
+  base::HistogramTester histogram_tester;
+  InitService({.force_reset = true});
+
+  CheckExpectations(
+      histogram_tester,
+      {.country_mismatch = ExpectHistogramNever(),
+       .selected_index = ExpectHistogramNever(),
+       .display_state_status = ExpectHistogramBucket(1 /* kTimedOut */),
+       .impression_at_index = {}});
+
+  // Pending state timed out, so the service also clears it.
+  EXPECT_FALSE(pref_service()->HasPrefPath(
+      prefs::kDefaultSearchProviderPendingChoiceScreenDisplayState));
+}
+
+TEST_F(SearchEngineChoiceServiceDisplayStateRecordTest,
+       RecordFromCacheCancelled_ParseError) {
+  ChoiceScreenDisplayState display_state(
+      /*search_engines=*/{SEARCH_ENGINE_GOOGLE, SEARCH_ENGINE_BING,
+                          SEARCH_ENGINE_YAHOO},
+      /*country_id=*/CountryId(),  // <= Causes the error
+      /*is_current_default_search_presented=*/false,
+      /*includes_non_regional_set_engine=*/false,
+      /*selected_engine_index=*/0);
+  pref_service()->SetDict(
+      prefs::kDefaultSearchProviderPendingChoiceScreenDisplayState,
+      display_state.ToDict());
+
+  search_engines::MarkSearchEngineChoiceCompletedForTesting(*pref_service());
+
+  base::HistogramTester histogram_tester;
+  InitService({.force_reset = true});
+
+  CheckExpectations(
+      histogram_tester,
+      {.country_mismatch = ExpectHistogramNever(),
+       .selected_index = ExpectHistogramNever(),
+       .display_state_status = ExpectHistogramBucket(0 /* kParseError */),
+       .impression_at_index = {}});
+
+  // Pending state is invalid, so the service also clears it.
   EXPECT_FALSE(pref_service()->HasPrefPath(
       prefs::kDefaultSearchProviderPendingChoiceScreenDisplayState));
 }
@@ -803,8 +915,7 @@ TEST_F(SearchEngineChoiceServiceDisplayStateRecordTest,
 TEST_F(SearchEngineChoiceServiceDisplayStateRecordTest,
        RecordFromCacheCancelled_UmaDisabled) {
   // Disable UMA reporting.
-  SearchEngineChoiceMetricsServiceAccessor::
-      SetForceIsMetricsReportingEnabledPrefLookup(false);
+  local_state().SetBoolean(metrics::prefs::kMetricsReportingEnabled, false);
 
   ChoiceScreenDisplayState display_state(
       /*search_engines=*/{SEARCH_ENGINE_GOOGLE, SEARCH_ENGINE_BING,
@@ -828,6 +939,7 @@ TEST_F(SearchEngineChoiceServiceDisplayStateRecordTest,
   CheckExpectations(histogram_tester_,
                     {.country_mismatch = ExpectHistogramNever(),
                      .selected_index = ExpectHistogramNever(),
+                     .display_state_status = ExpectHistogramNever(),
                      .impression_at_index = {}});
 }
 
