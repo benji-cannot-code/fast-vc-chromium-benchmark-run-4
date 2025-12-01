743072f725dbcfe819229d241d4cd7eb0369de6e
Colin Blundell <blundell@chromium.org>
2025-12-01 01:35:34 -0800

    [Blink] Eliminate CanvasResourceProvider::OnFlushForImage()
    This can now be folded into CanvasResourceProviderSI::OnFlushForImage().
    
    Bug: 352263194
    Change-Id: I237275415a0a0763cfd80a64568f6bec99116651
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7210450
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1551968}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
@@ -317,7 +317,6 @@ class PLATFORM_EXPORT CanvasResourceProvider
   mutable sk_sp<SkSurface> surface_;  // mutable for lazy init
   SkSurface::ContentChangeMode mode_ = SkSurface::kRetain_ContentChangeMode;
 
-  virtual void OnFlushForImage(cc::PaintImage::ContentId content_id);
   void OnMemoryDump(base::trace_event::ProcessMemoryDump*) override;
 
   HighEntropyCanvasOpType GetRecorderHighEntropyCanvasOpTypes() const;
@@ -587,7 +586,7 @@ class PLATFORM_EXPORT CanvasResourceProviderSharedImage
       ImageOrientation orientation) final;
   void RasterRecord(cc::PaintRecord last_recording) override;
   sk_sp<SkSurface> CreateSkSurface() const override;
-  void OnFlushForImage(cc::PaintImage::ContentId content_id) final;
+  void OnFlushForImage(cc::PaintImage::ContentId content_id);
   void OnMemoryDump(base::trace_event::ProcessMemoryDump* pmd) final;
   scoped_refptr<StaticBitmapImage> Snapshot(
       ImageOrientation = ImageOrientationEnum::kDefault) override;
