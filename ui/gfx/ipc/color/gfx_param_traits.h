12b92d9db2e4a95604a1bf1d9f5106c84cd02450
Colin Blundell <blundell@chromium.org>
2025-12-01 00:51:37 -0800

    [//ui] Remove DisplayColorSpaces legacy IPC param
    This is no longer used.
    
    Bug: 445879532
    Change-Id: I8a6c85344e8b6615728b8881a4a99948583e00bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7207195
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1551958}

--- ui/gfx/ipc/color/gfx_param_traits.h
+++ ui/gfx/ipc/color/gfx_param_traits.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gfx {
 class ColorSpace;
-class DisplayColorSpaces;
 }
 
 namespace IPC {
@@ -21,15 +20,6 @@ namespace IPC {
 template <>
 struct GFX_IPC_COLOR_EXPORT ParamTraits<gfx::ColorSpace> {
   typedef gfx::ColorSpace param_type;
-  static void Write(base::Pickle* m, const param_type& p);
-  static bool Read(const base::Pickle* m,
-                   base::PickleIterator* iter,
-                   param_type* r);
-};
-
-template <>
-struct GFX_IPC_COLOR_EXPORT ParamTraits<gfx::DisplayColorSpaces> {
-  typedef gfx::DisplayColorSpaces param_type;
   static void Write(base::Pickle* m, const param_type& p);
   static bool Read(const base::Pickle* m,
                    base::PickleIterator* iter,
