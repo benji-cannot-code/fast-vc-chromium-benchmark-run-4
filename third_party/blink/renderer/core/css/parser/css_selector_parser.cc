84725de2e538f995158d6bb231c03b12fb46a2b9
Steinar H. Gunderson <sesse@chromium.org>
2025-12-01 02:46:01 -0800

    Fix DCHECK when parsing invalid :is()/:where().
    I haven't looked into why we're missing lookahead here, but evidently,
    we do, and Restore() requires it.
    
    Fixed: 464739072
    Change-Id: If9cedb476d2a755558f2afc5f4baa958c3c7a9ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7204012
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1551989}

--- third_party/blink/renderer/core/css/parser/css_selector_parser.cc
+++ third_party/blink/renderer/core/css/parser/css_selector_parser.cc
@@ -406,6 +406,7 @@ CSSSelectorParser::ConsumeForgivingComplexSelectorList(
     if (selector.empty() || failed_parsing_ ||
         !AtEndOfComplexSelector(stream)) {
       output_.resize(subpos);  // Drop what we parsed so far.
+      stream.EnsureLookAhead();
       stream.Restore(state);
       AddPlaceholderSelectorIfNeeded(
           stream);  // Forwards until the end of the argument (i.e. to comma or
@@ -945,7 +946,6 @@ CSSSelector::PseudoType CSSSelectorParser::ParsePseudoType(
 
   return CSSSelector::PseudoType::kPseudoUnknown;
 }
-
 
 // static
 PseudoId CSSSelectorParser::ParsePseudoElement(const String& selector_string,
