f85c626d568329c9e7a1f5fab1ac38d739861b8d
Jihad Hanna <jihadghanna@google.com>
2025-12-01 04:19:08 -0800

    Move some submission states/methods from AutofillAgent to FormTracker
    This is part of a chain that is moving submission detection logic from
    AutofillAgent to FormTracker.
    
    Bug: 40281981
    Change-Id: Ib303983b789fadaaf4bed8a6679fe5607428567e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7208893
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1552032}

--- components/autofill/content/renderer/autofill_agent_test_api.h
+++ components/autofill/content/renderer/autofill_agent_test_api.h
@@ -24,10 +24,6 @@ class AutofillAgentTestApi {
     agent_->form_tracker_ = std::move(form_tracker);
   }
 
-  std::optional<FormData> provisionally_saved_form() {
-    return agent_->provisionally_saved_form();
-  }
-
   void FocusedElementChanged(blink::WebElement new_focused_element) {
     agent_->FocusedElementChanged(new_focused_element);
   }
@@ -46,8 +42,6 @@ class AutofillAgentTestApi {
       AutofillSuggestionTriggerSource trigger_source) {
     agent_->ShowSuggestionsForContentEditable(element, trigger_source);
   }
-
-  void OnFormNoLongerSubmittable() { agent_->OnFormNoLongerSubmittable(); }
 
   const FormCache& form_cache() { return agent_->form_cache_; }
 
