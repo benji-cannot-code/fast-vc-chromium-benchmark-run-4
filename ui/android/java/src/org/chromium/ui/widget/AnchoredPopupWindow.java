eb6e468868c58fc4ea0fe6d61726c958d9fdabc3
Sky Malice <skym@chromium.org>
2025-12-01 14:19:43 -0800

    [AIM] Style popup menu for incog.
    Bug: 460821438
    Change-Id: Ic8d38092060893794a73c1685809863efddb4bbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7213547
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1552428}

--- ui/android/java/src/org/chromium/ui/widget/AnchoredPopupWindow.java
+++ ui/android/java/src/org/chromium/ui/widget/AnchoredPopupWindow.java
@@ -1032,6 +1032,11 @@ public class AnchoredPopupWindow implements OnTouchListener, RectProvider.Observ
         mAllowNonTouchableSize = allowNonTouchableSize;
     }
 
+    /** Changes the background at runtime. */
+    public void setBackgroundDrawable(Drawable background) {
+        mPopupWindow.setBackgroundDrawable(background);
+    }
+
     // RectProvider.Observer implementation.
     @Override
     public void onRectChanged() {
