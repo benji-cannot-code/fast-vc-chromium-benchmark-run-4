a677f1e49413bb5eadc3812135177a216a5d6c40
Chris Lu <thegreenfrog@google.com>
2025-12-01 11:39:10 -0800

    [ios] Add kIOSAutoOpenRemoteTabGroupsSettings to field trial config
    This is a reland of https://chromium-review.googlesource.com/c/chromium/src/+/7201385
    
    TabStripSharedGroupTestCase was missing the feature disable since it
    was only covered in non-cq bot.
    
    Fixed: 463466042
    Change-Id: I56b59c346843ef5deaf9e8cc90f8cfdb7a9220b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7208043
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1552305}

--- ios/chrome/browser/tab_switcher/tab_strip/test/tab_strip_shared_group_egtest.mm
+++ ios/chrome/browser/tab_switcher/tab_strip/test/tab_strip_shared_group_egtest.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/authentication/test/signin_earl_grey.h"
 #import "ios/chrome/browser/authentication/test/signin_earl_grey_ui_test_util.h"
 #import "ios/chrome/browser/shared/model/url/chrome_url_constants.h"
+#import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/signin/model/fake_system_identity.h"
 #import "ios/chrome/browser/tab_switcher/tab_strip/ui/swift_constants_for_objective_c.h"
 #import "ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_group_app_interface.h"
@@ -78,6 +79,7 @@ void AddSharedGroup(BOOL owner,
   AppLaunchConfiguration config;
   config.features_enabled.push_back(
       data_sharing::features::kDataSharingFeature);
+  config.features_disabled.push_back(kIOSAutoOpenRemoteTabGroupsSettings);
   // Add the flag to use FakeTabGroupSyncService.
   config.additional_args.push_back(
       "--" + std::string(test_switches::kEnableFakeTabGroupSyncService));
--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_group_sync_signin_egtest.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_group_sync_signin_egtest.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/sync/base/command_line_switches.h"
 #import "components/sync/base/data_type.h"
 #import "ios/chrome/browser/authentication/test/signin_earl_grey.h"
+#import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/signin/model/fake_system_identity.h"
 #import "ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_groups_eg_utils.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
@@ -93,6 +94,7 @@ void WaitForEntitiesOnFakeServer(int entity_count) {
                                    syncer::kSyncShortNudgeDelayForTest);
   config.features_enabled.push_back(
       data_sharing::features::kDataSharingFeature);
+  config.features_disabled.push_back(kIOSAutoOpenRemoteTabGroupsSettings);
   return config;
 }
 
