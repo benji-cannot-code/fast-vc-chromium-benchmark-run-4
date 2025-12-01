658e459d6ad1aadc16cd96d0c1ceb26806e54fed
Alexis Hetu <sugoi@chromium.org>
2025-12-01 06:05:12 -0800

    [iOS] New RequestInfo structure for passkey related bottom sheets
    This CL changes the following:
    - Created RequestInfo to encapsulate parameters the bottom sheet
      will require from PasskeyTabHelper to fulfill the requests. For
      now, it adds frame_id to the information sent to bottom sheets,
      which originally only contained request_id.
    - Made StartPasskeyCreation and StartPasskeyAssertion public, as
      these will be used to fulfill passkey requests.
    - Added a passthrough implementation of ShowSuggestionBottomSheet
      and ShowCreationBottomSheet to be able to do easily do end to
      end testing until the UI is ready.
    
    Bug: 460485333
    Change-Id: I306cb5b81ed2df2a7421f6016bf5ec83a46a0590
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7205978
    Reviewed-by: Rafał Godlewski <rgod@google.com>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1552074}

--- components/webauthn/ios/ios_passkey_client.h
+++ components/webauthn/ios/ios_passkey_client.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_WEBAUTHN_IOS_IOS_PASSKEY_CLIENT_H_
 
 #include <cstdint>
+#include <string>
 
 #import "components/webauthn/ios/passkey_types.h"
 
@@ -22,6 +23,16 @@ namespace webauthn {
 // perform passkey related tasks.
 class IOSPasskeyClient {
  public:
+  // Provides information used by bottom sheets to fulfill passkey requests.
+  struct RequestInfo {
+    RequestInfo(std::string frame_id, std::string request_id);
+    RequestInfo(RequestInfo&& other);
+    ~RequestInfo();
+
+    std::string frame_id;
+    std::string request_id;
+  };
+
   virtual ~IOSPasskeyClient() = default;
 
   // Performs user verification and returns whether it was successful.
@@ -33,10 +44,10 @@ class IOSPasskeyClient {
                          KeysFetchedCallback callback) = 0;
 
   // Shows the bottom sheet with passkey suggestions.
-  virtual void ShowSuggestionBottomSheet(std::string request_id) = 0;
+  virtual void ShowSuggestionBottomSheet(RequestInfo request_info) = 0;
 
   // Shows the bottom sheet to confirm passkey creation.
-  virtual void ShowCreationBottomSheet(std::string request_id) = 0;
+  virtual void ShowCreationBottomSheet(RequestInfo request_info) = 0;
 
   // Sets whether showing the passkey creation infobar is allowed. Should be
   // enabled before passkey creation happens within the passkey model and
---
+++ components/webauthn/ios/ios_passkey_client.mm
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "components/webauthn/ios/ios_passkey_client.h"
+
+namespace webauthn {
+
+IOSPasskeyClient::RequestInfo::RequestInfo(std::string frame_id,
+                                           std::string request_id)
+    : frame_id(std::move(frame_id)), request_id(std::move(request_id)) {}
+
+IOSPasskeyClient::RequestInfo::RequestInfo(
+    IOSPasskeyClient::RequestInfo&& other) = default;
+IOSPasskeyClient::RequestInfo::~RequestInfo() = default;
+
+}  // namespace webauthn
