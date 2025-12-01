fabe3da9f63104f754b94d7cb070f8e998c4ee0e
Ron Sailer <rsailer@google.com>
2025-12-01 13:36:32 -0800

    Support kSafeBrowsingEnhanced as a synced pref on iOS.
    The change makes it so that a change in the pref on one client will sync
    to the iOS clients of the same user. The user will be notified of the
    change with an infobar banner.
    
    Screenshots:
    
    crbug.com/444232390#comment2
    
    Based on
    https://chromium-review.googlesource.com/c/chromium/src/+/6087986
    
    NO_IFTTT=Not actually changing the
    EnhancedSafeBrowsingInfobarInteraction enum, just changed it from 'enum'
    to 'enum class' to prevent naming collisions with kMaxValue.
    
    Bug: 444232390
    Change-Id: I91359daf15d6e143e8623c6687024e8ada1dfe80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7088627
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Joemer Ramos <joemerramos@google.com>
    Reviewed-by: Zack Han <zackhan@chromium.org>
    Commit-Queue: Ron Sailer <rsailer@chromium.org>
    Reviewed-by: Javier Castro <jacastro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1552400}

--- ios/chrome/browser/safe_browsing/ui_bundled/safe_browsing_coordinator.h
+++ ios/chrome/browser/safe_browsing/ui_bundled/safe_browsing_coordinator.h
@@ -7,10 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_CHROME_BROWSER_SAFE_BROWSING_UI_BUNDLED_SAFE_BROWSING_COORDINATOR_H_
 
 #import "ios/chrome/browser/shared/coordinator/chrome_coordinator/chrome_coordinator.h"
+#import "ios/components/security_interstitials/safe_browsing/safe_browsing_tab_helper_delegate.h"
 
 // Used as a delegate for SafeBrowsingTabHelper and makes initial call to open
 // safe browsing settings from interstitial page.
-@interface SafeBrowsingCoordinator : ChromeCoordinator
+@interface SafeBrowsingCoordinator
+    : ChromeCoordinator <SafeBrowsingTabHelperDelegate>
 
 @end
 
