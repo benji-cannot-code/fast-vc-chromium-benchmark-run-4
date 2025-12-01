0a0af2217c6270d4d6f2b4ae26a17a7cc55ea94c
Hailey Wang <haileywang@google.com>
2025-12-01 15:02:47 -0800

    [AIM] Polish fix for tab filter and profile in incognito
    Change TabPicker to check the incognito profile when present.
    
    Only check for tab frozen and uninitialized state when checking active
    tabs.
    
    Bug: 464524004
    Change-Id: Iec25894d0cd73cd9bb62cf64bd3e0069feddb143
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7208357
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1552454}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxMediator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxMediator.java
@@ -287,6 +287,7 @@ public class FuseboxMediator {
         Tab currentTab = assumeNonNull(tabModelSelector.getCurrentTab());
         boolean tabIsEligible =
                 FuseboxTabUtils.isTabEligibleForAttachment(currentTab)
+                        && FuseboxTabUtils.isTabActive(currentTab)
                         && !currentTab.isIncognitoBranded();
 
         if (tabIsEligible) {
