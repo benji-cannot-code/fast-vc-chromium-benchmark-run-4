e07297907260353c50bab6912dcc797628a1863d
shomizoe <shomizoe@google.com>
2025-12-01 10:12:08 -0800

    [ntp-next] Fix the crash happening when a tab is discarded
    This CL fixes the crash that occurs when an NTP is discarded.
    
    The existing logic in `ActionChipsHandler` and its dependency does not handle cases where an NTP is discarded. The CL fixes it by updating the logic so that the current NTP checks if the tab being discarded is itself and stops updating the page when it is the case.
    
    As part of the fix, this CL also simplified the code by removing `TabReadinessChecker`.
    
    Manual Testing
    ==============
    
    Repro
    -----
    https://screencast.googleplex.com/cast/NjYxMDQyMDQyODE3NzQwOHxmM2Q2ODk1ZC03Mg
    
    (This also repros in the canary by discarding an NTP in chrome://discards)
    
    Fix
    ---
    https://screencast.googleplex.com/cast/NTU3ODM2NDE2NDI0MzQ1NnxmYTNjYjYzMy03MA
    
    
    Fixed: b:463912238
    Change-Id: I1f1c6bd29f5c04e32f4086ec4efb343f6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7211636
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Auto-Submit: Sho Mizoe <shomizoe@google.com>
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1552249}

--- chrome/browser/ui/webui/new_tab_page/action_chips/action_chips_handler.h
+++ chrome/browser/ui/webui/new_tab_page/action_chips/action_chips_handler.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/new_tab_page/action_chips/action_chips.mojom.h"
 #include "chrome/browser/ui/webui/new_tab_page/action_chips/action_chips_generator.h"
 #include "chrome/browser/ui/webui/new_tab_page/action_chips/tab_id_generator.h"
-#include "chrome/browser/ui/webui/new_tab_page/action_chips/tab_readiness_checker.h"
 #include "chrome/browser/ui/webui/webui_embedding_context.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
@@ -34,7 +33,6 @@ class ActionChipsHandler : public action_chips::mojom::ActionChipsHandler,
       mojo::PendingRemote<action_chips::mojom::Page> page,
       Profile* profile,
       content::WebUI* web_ui,
-      const TabReadinessChecker* checker,
       std::unique_ptr<ActionChipsGenerator> action_chips_generator);
   ActionChipsHandler(const ActionChipsHandler&) = delete;
   ActionChipsHandler& operator=(const ActionChipsHandler&) = delete;
@@ -55,7 +53,6 @@ class ActionChipsHandler : public action_chips::mojom::ActionChipsHandler,
   mojo::Remote<action_chips::mojom::Page> page_;
   raw_ptr<Profile> profile_;
   raw_ptr<content::WebUI> web_ui_;
-  raw_ptr<const TabReadinessChecker> tab_readiness_checker_;
   std::unique_ptr<ActionChipsGenerator> action_chips_generator_;
   base::WeakPtrFactory<ActionChipsHandler> weak_factory_{this};
 };
