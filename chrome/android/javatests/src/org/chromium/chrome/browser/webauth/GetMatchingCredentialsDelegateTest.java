bbd7e863e395b4ce658b99afdaa5879c7aff70e7
Aishwarya Rajesh <aishwaryarj@google.com>
2025-12-02 11:16:28 -0800

    [Gardener] Disable consistently failing tests in GetMatchingCredentialsDelegateTest
    Bug: 465421209
    Change-Id: Idfc0e98c349c0a28bca62cfa54203015bab8bf01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7216255
    Owners-Override: Aishwarya Rajesh <aishwaryarj@google.com>
    Reviewed-by: Michael Wu <mklw@google.com>
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Reviewed-by: Ryan Thomas <rathomas@google.com>
    Cr-Commit-Position: refs/heads/main@{#1552979}

--- chrome/android/javatests/src/org/chromium/chrome/browser/webauth/GetMatchingCredentialsDelegateTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/webauth/GetMatchingCredentialsDelegateTest.java
@@ -23,6 +23,7 @@ import org.mockito.junit.MockitoRule;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
@@ -160,6 +161,7 @@ public class GetMatchingCredentialsDelegateTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "crbug.com/465421209")
     public void testGetMatchingCredentialIds_success() {
         String relyingPartyId = "subdomain.example.test";
         byte[][] allowCredentialIds =
@@ -183,6 +185,7 @@ public class GetMatchingCredentialsDelegateTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "crbug.com/465421209")
     public void testGetMatchingCredentialIds_requireThirdPartyBit() {
         String relyingPartyId = "subdomain.example.test";
         byte[][] allowCredentialIds =
