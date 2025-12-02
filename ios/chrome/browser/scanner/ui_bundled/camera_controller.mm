7cf5953a46eb6c0c92fe6af3f10aeb3b606e80f7
Daniel White <danieltwhite@google.com>
2025-12-02 01:17:28 -0800

    [iOS] iOS 16 deprecation code cleanup
    Each year the Chrome for iOS team increases the application's minimum
    supported iOS version. During the process, run/compile-time flags are
    added throughout the codebase to transitions areas of the code using
    deprecated APIs to the newer variants. Once the minimum supported
    version is bumped, these guards are no longer needed.
    
    This CL is part of a large-scale effort to remove the remaining
    obsolete guards from the application's codebase.
    
    This CL was uploaded by git cl split.
    
    R=gambard@chromium.org
    
    Bug: 403260493
    Change-Id: Ice948ffb24ff17dc97c873ca064ebae95b959753
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7205575
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Daniel White <danieltwhite@google.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1552653}

--- ios/chrome/browser/scanner/ui_bundled/camera_controller.mm
+++ ios/chrome/browser/scanner/ui_bundled/camera_controller.mm
@@ -45,11 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)stopReceivingNotifications;
 // Returns the camera attached to `_captureSession`.
 - (AVCaptureDevice*)camera;
-#if !defined(__IPHONE_17_0) || __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_17_0
-// Returns the AVCaptureVideoOrientation to compensate for the current
-// UIInterfaceOrientation. Defaults to AVCaptureVideoOrientationPortrait.
-- (AVCaptureVideoOrientation)videoOrientationForCurrentInterfaceOrientation;
-#endif
 
 @end
 
@@ -330,18 +325,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   DCHECK(captureSessionInput != nil);
   return [captureSessionInput device];
 }
-
-#if !defined(__IPHONE_17_0) || __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_17_0
-- (AVCaptureVideoOrientation)videoOrientationForCurrentInterfaceOrientation {
-  UIInterfaceOrientation orientation = GetInterfaceOrientation();
-  switch (orientation) {
-    case UIInterfaceOrientationUnknown:
-      return AVCaptureVideoOrientationPortrait;
-    default:
-      return static_cast<AVCaptureVideoOrientation>(orientation);
-  }
-}
-#endif
 
 #pragma mark - Notification Handlers
 
