4054b82915c2ac0f4622937af9b632bee40fe13a
Andrew Grieve <agrieve@chromium.org>
2025-12-02 10:54:02 -0800

    Android: Make CriteriaNotSatisfiedException not extend AssertionError
    AssertionErrors (like all Error types) are meant to be fatal.
    
    I found this by setting an exception breakpoint for AssertionErrors and
    it was rendered useless by repeated CriteriaNotSatisfiedExceptions.
    
    Change-Id: Ia3ed6d908d2207af1b3743eb9aff2a1599f50c8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7208151
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1552954}

--- base/test/android/javatests/src/org/chromium/base/test/util/CriteriaNotSatisfiedException.java
+++ base/test/android/javatests/src/org/chromium/base/test/util/CriteriaNotSatisfiedException.java
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.base.test.util;
 
 /** Exception indicating that a Criteria did not match expectations. */
-public class CriteriaNotSatisfiedException extends AssertionError {
+public class CriteriaNotSatisfiedException extends RuntimeException {
     /**
      * @param msg The reason the criteria was not met.
      */
