122c7d5e83c29df11636b4023a0169601277a9bf
Dominik Röttsches <drott@chromium.org>
2025-12-02 07:58:10 -0800

    Fix namespace reset in fragment parsing
    When an explicit xmlns='' is encountered on an element in the
    fragment. This child elements and elements below should no longer be
    assigned the default namespace of the parent element to which this
    fragment is appended.
    
    Same fix applied to Rust parser implementation.
    
    Add test to ensure namespace reset only applies to children.
    
    Run innerhtml-and-xml-namespaces.svg in virtual test suite for
    Rust parser.
    
    Bug: 463630186
    Change-Id: I545e914254fa5118cce4a681654c1ad087f3217c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7202654
    Reviewed-by: David Baron <dbaron@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1552847}

--- third_party/blink/renderer/core/xml/parser/xml_document_parser_rs.cc
+++ third_party/blink/renderer/core/xml/parser/xml_document_parser_rs.cc
@@ -90,11 +90,12 @@ AtomicString RustStrToAtomicString(rust::Str str) {
 bool HandleNamespaceAttributes(
     Vector<Attribute, kAttributePrealloc>& prefixed_attributes,
     xml_ffi::NamespacesIterator& namespaces,
+    bool& encountered_namespace_reset,
     ExceptionState& exception_state) {
   rust::String prefix;
   rust::String uri;
 
-  while (namespaces_next(namespaces, prefix, uri) && uri.length()) {
+  while (namespaces_next(namespaces, prefix, uri)) {
     AtomicString namespace_q_name = g_xmlns_atom;
     if (prefix.length()) {
       namespace_q_name = AtomicString(
@@ -105,6 +106,9 @@ bool HandleNamespaceAttributes(
     if (!parsed_name) {
       DCHECK(exception_state.HadException());
       return false;
+    }
+    if (parsed_name->LocalName() == g_xmlns_atom) {
+      encountered_namespace_reset = uri.empty();
     }
     prefixed_attributes.push_back(
         Attribute(std::move(*parsed_name), RustStrToAtomicString(uri)));
@@ -264,6 +268,7 @@ void XMLDocumentParserRs::ProcessEvents() {
 void XMLDocumentParserRs::ClearCurrentNodeStack() {
   current_node_ = nullptr;
   leaf_text_node_ = nullptr;
+  ancestor_resetting_namespace_ = nullptr;
 
   if (current_node_stack_.size()) {  // Aborted parsing.
     current_node_stack_.clear();
@@ -273,6 +278,7 @@ void XMLDocumentParserRs::ClearCurrentNodeStack() {
 void XMLDocumentParserRs::Trace(Visitor* visitor) const {
   visitor->Trace(current_node_);
   visitor->Trace(current_node_stack_);
+  visitor->Trace(ancestor_resetting_namespace_);
   visitor->Trace(leaf_text_node_);
   visitor->Trace(xml_errors_);
   visitor->Trace(document_);
@@ -358,6 +364,17 @@ void XMLDocumentParserRs::StartElementNs(
     return;
   }
 
+  bool is_first_element = !saw_first_element_;
+  saw_first_element_ = true;
+
+  Vector<Attribute, kAttributePrealloc> prefixed_attributes;
+  bool encountered_namespace_reset = false;
+  if (!HandleNamespaceAttributes(prefixed_attributes, namespaces,
+                                 encountered_namespace_reset,
+                                 IGNORE_EXCEPTION)) {
+    StopParsing();
+    return;
+  }
   // In fragment parsing, adjust namespace URI. If the parser library reports an
   // empty NS url, resolve it against the initially preserved namespace
   // hierarchy that is built when creating an XMLDocumentParser with the
@@ -372,18 +389,11 @@ void XMLDocumentParserRs::StartElementNs(
         adjusted_ns_uri = it->value;
       }
     } else {
-      adjusted_ns_uri = default_namespace_uri_;
+      adjusted_ns_uri =
+          encountered_namespace_reset || ancestor_resetting_namespace_
+              ? g_null_atom
+              : default_namespace_uri_;
     }
-  }
-
-  bool is_first_element = !saw_first_element_;
-  saw_first_element_ = true;
-  Vector<Attribute, kAttributePrealloc> prefixed_attributes;
-
-  if (!HandleNamespaceAttributes(prefixed_attributes, namespaces,
-                                 IGNORE_EXCEPTION)) {
-    StopParsing();
-    return;
   }
 
   v8::Isolate* isolate = document_->GetAgent().isolate();
@@ -450,6 +460,10 @@ void XMLDocumentParserRs::StartElementNs(
 
   SetAttributes(new_element, prefixed_attributes, GetParserContentPolicy());
 
+  if (parsing_fragment_ && encountered_namespace_reset) {
+    ancestor_resetting_namespace_ = new_element;
+  }
+
   new_element->BeginParsingChildren();
 
   if (new_element->IsScriptElement()) {
@@ -495,6 +509,10 @@ void XMLDocumentParserRs::EndElementNs(rust::Str local_name,
   if (!element) {
     PopCurrentNode();
     return;
+  }
+
+  if (ancestor_resetting_namespace_ == n) {
+    ancestor_resetting_namespace_ = nullptr;
   }
 
   element->FinishParsingChildren();
