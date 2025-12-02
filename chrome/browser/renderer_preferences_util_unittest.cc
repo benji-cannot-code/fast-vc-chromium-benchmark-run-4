2ae3311a826ab3ea91e10ce0b9d0d870cd0b8e34
Eric Lok <lokeric@google.com>
2025-12-02 00:47:16 -0800

    [Caret Browsing] Ensure feature is off when flag is disabled
    Ensures that caret browsing feature is OFF for Clank when the feature
    flag is disabled. This is important if we need to disable this feature
    under emergency circumstances.
    
    Bug: 369139090
    Test: New unit test
    Change-Id: I61e16d22f3fe0125b412277298bd7b0ca345e7e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7206457
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Auto-Submit: Eric Lok <lokeric@google.com>
    Cr-Commit-Position: refs/heads/main@{#1552638}

--- chrome/browser/renderer_preferences_util_unittest.cc
+++ chrome/browser/renderer_preferences_util_unittest.cc
@@ -8,11 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <array>
 
 #include "base/memory/raw_ptr.h"
+#include "base/test/scoped_feature_list.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/test/base/testing_profile.h"
 #include "components/content_settings/core/common/content_settings_pattern.h"
 #include "components/prefs/pref_service.h"
 #include "components/sync_preferences/testing_pref_service_syncable.h"
+#include "content/public/common/content_features.h"
 #include "content/public/test/browser_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/peerconnection/webrtc_ip_handling_policy.h"
@@ -133,3 +135,31 @@ TEST_F(RendererPreferencesUtilTest, WebRTCIPHandlingURLValidEntries) {
   EXPECT_EQ(renderer_preferences.webrtc_ip_handling_urls[1].handling,
             blink::mojom::WebRtcIpHandlingPolicy::kDefault);
 }
+
+#if BUILDFLAG(IS_ANDROID)
+TEST_F(RendererPreferencesUtilTest, CaretBrowsingAndroidKillSwitch) {
+  // Case 1: Feature Enabled, Pref Enabled -> Result: Enabled
+  {
+    base::test::ScopedFeatureList feature_list;
+    feature_list.InitAndEnableFeature(features::kAndroidCaretBrowsing);
+    pref_service_->SetBoolean(prefs::kCaretBrowsingEnabled, true);
+
+    blink::RendererPreferences renderer_preferences;
+    renderer_preferences_util::UpdateFromSystemSettings(&renderer_preferences,
+                                                        &profile_);
+    EXPECT_TRUE(renderer_preferences.caret_browsing_enabled);
+  }
+
+  // Case 2: Feature Disabled, Pref Enabled -> Result: Disabled
+  {
+    base::test::ScopedFeatureList feature_list;
+    feature_list.InitAndDisableFeature(features::kAndroidCaretBrowsing);
+    pref_service_->SetBoolean(prefs::kCaretBrowsingEnabled, true);
+
+    blink::RendererPreferences renderer_preferences;
+    renderer_preferences_util::UpdateFromSystemSettings(&renderer_preferences,
+                                                        &profile_);
+    EXPECT_FALSE(renderer_preferences.caret_browsing_enabled);
+  }
+}
+#endif
