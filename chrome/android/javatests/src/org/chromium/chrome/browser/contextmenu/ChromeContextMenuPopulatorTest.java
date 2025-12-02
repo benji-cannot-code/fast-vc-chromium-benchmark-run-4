29897948653aec055942bfdb2be2ef548dbb8a69
Charles Hager <clhager@google.com>
2025-12-01 18:23:12 -0800

    Revert "Make test APK the default browser app if it has APP_BROWSER category"
    This reverts commit b152d0d758c511db4d1811736bf69848759c2cf1.
    
    Reason for revert: Might be causing failures on the automotive bot.
    
    Original change's description:
    > Make test APK the default browser app if it has APP_BROWSER category
    >
    > Some Android APIs don't work if the APK isn't the default browser app.
    > To test code using these APIs, we grant "android.app.role.BROWSER" to
    > the test APK if it has "android.intent.category.APP_BROWSER".
    >
    > Changes:
    >
    > (1) Update local_device_instrumentation_test_run.py to grant test APK
    >     the browser role;
    >
    > (2) Fix two tests for the custom tab context menu as the default browser
    >     has different menu items:
    >     * ChromeContextMenuPopulatorTest
    >     * CustomTabContextMenuTest
    >
    >     Note on ChromeContextMenuPopulatorTest:
    >
    >     We need to annotate it with Batch.PER_CLASS instead of
    >     Batch.UNIT_TESTS. Otherwise, the test will be flaky as the test
    >     script in (1) will cause changes to a global state in
    >     SharedPreferences (ChromePreferenceKeys.CHROME_DEFAULT_BROWSER).
    >
    >     Using Bathc.PER_CLASS is also consistent with
    >     CustomTabContextMenuTest.
    >
    > Include-Ci-Only-Tests: true
    > Bug: 463735862
    > Change-Id: I166e0da00ce2696d25257b398f8a1247eee8504d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7204064
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Commit-Queue: Linyu He <linyuh@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1552336}
    
    Include-Ci-Only-Tests: true
    Bug: 463735862
    Change-Id: I6cf7adde06d1a98eb8a49fedad751b99ed9ee3f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7214063
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Linyu He <linyuh@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Charles Hager <clhager@google.com>
    Cr-Commit-Position: refs/heads/main@{#1552522}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ChromeContextMenuPopulatorTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ChromeContextMenuPopulatorTest.java
@@ -99,7 +99,7 @@ import java.util.List;
 
 /** Unit tests for the context menu logic of Chrome. */
 @RunWith(BaseJUnit4ClassRunner.class)
-@Batch(Batch.PER_CLASS)
+@Batch(Batch.UNIT_TESTS)
 public class ChromeContextMenuPopulatorTest {
     private static final String DATA_URL = "data:encodedstringblahblah";
     private static final String PAGE_URL = "http://www.blah.com/page_url";
@@ -397,8 +397,7 @@ public class ChromeContextMenuPopulatorTest {
 
         initializePopulator(ChromeContextMenuPopulator.ContextMenuMode.CUSTOM_TAB, params);
         int[] expected3 = {
-            R.id.contextmenu_open_in_new_chrome_tab,
-            R.id.contextmenu_open_in_chrome_incognito_tab,
+            R.id.contextmenu_open_in_browser_id,
             R.id.contextmenu_open_in_ephemeral_tab,
             R.id.contextmenu_copy_link_address,
             R.id.contextmenu_copy_link_text,
@@ -517,8 +516,7 @@ public class ChromeContextMenuPopulatorTest {
 
         initializePopulator(ChromeContextMenuPopulator.ContextMenuMode.CUSTOM_TAB, params);
         int[] expected3 = {
-            R.id.contextmenu_open_in_new_chrome_tab,
-            R.id.contextmenu_open_in_chrome_incognito_tab,
+            R.id.contextmenu_open_in_browser_id,
             R.id.contextmenu_open_in_ephemeral_tab,
             R.id.contextmenu_copy_link_address,
             R.id.contextmenu_copy_link_text,
@@ -595,8 +593,7 @@ public class ChromeContextMenuPopulatorTest {
 
         initializePopulator(ChromeContextMenuPopulator.ContextMenuMode.CUSTOM_TAB, params);
         int[] expected2 = {
-            R.id.contextmenu_open_in_new_chrome_tab,
-            R.id.contextmenu_open_in_chrome_incognito_tab,
+            R.id.contextmenu_open_in_browser_id,
             R.id.contextmenu_open_in_ephemeral_tab,
             R.id.contextmenu_copy_link_address,
             R.id.contextmenu_copy_link_text,
@@ -929,8 +926,7 @@ public class ChromeContextMenuPopulatorTest {
 
         initializePopulator(ChromeContextMenuPopulator.ContextMenuMode.CUSTOM_TAB, params);
         int[] expected3Tab1 = {
-            R.id.contextmenu_open_in_new_chrome_tab,
-            R.id.contextmenu_open_in_chrome_incognito_tab,
+            R.id.contextmenu_open_in_browser_id,
             R.id.contextmenu_open_in_ephemeral_tab,
             R.id.contextmenu_copy_link_address,
             R.id.contextmenu_copy_link_text,
@@ -1030,8 +1026,7 @@ public class ChromeContextMenuPopulatorTest {
 
         initializePopulator(ChromeContextMenuPopulator.ContextMenuMode.CUSTOM_TAB, params);
         int[] expected3Tab1 = {
-            R.id.contextmenu_open_in_new_chrome_tab,
-            R.id.contextmenu_open_in_chrome_incognito_tab,
+            R.id.contextmenu_open_in_browser_id,
             R.id.contextmenu_open_in_ephemeral_tab,
             R.id.contextmenu_copy_link_address,
             R.id.contextmenu_copy_link_text,
@@ -1186,8 +1181,7 @@ public class ChromeContextMenuPopulatorTest {
 
         initializePopulator(ChromeContextMenuPopulator.ContextMenuMode.CUSTOM_TAB, params);
         int[] expected3 = {
-            R.id.contextmenu_open_in_new_chrome_tab,
-            R.id.contextmenu_open_in_chrome_incognito_tab,
+            R.id.contextmenu_open_in_browser_id,
             R.id.contextmenu_open_image,
             R.id.contextmenu_open_image_in_ephemeral_tab,
             R.id.contextmenu_copy_image,
@@ -1294,8 +1288,7 @@ public class ChromeContextMenuPopulatorTest {
 
         initializePopulator(ChromeContextMenuPopulator.ContextMenuMode.CUSTOM_TAB, params);
         int[] expected3Tab1 = {
-            R.id.contextmenu_open_in_new_chrome_tab,
-            R.id.contextmenu_open_in_chrome_incognito_tab,
+            R.id.contextmenu_open_in_browser_id,
             R.id.contextmenu_open_in_ephemeral_tab,
             R.id.contextmenu_copy_link_address,
             R.id.contextmenu_save_link_as,
@@ -1454,8 +1447,7 @@ public class ChromeContextMenuPopulatorTest {
         // Custom tab should include read later.
         initializePopulator(ChromeContextMenuPopulator.ContextMenuMode.CUSTOM_TAB, params);
         int[] expected2 = {
-            R.id.contextmenu_open_in_new_chrome_tab,
-            R.id.contextmenu_open_in_chrome_incognito_tab,
+            R.id.contextmenu_open_in_browser_id,
             R.id.contextmenu_open_in_ephemeral_tab,
             R.id.contextmenu_copy_link_address,
             R.id.contextmenu_copy_link_text,
@@ -1777,8 +1769,7 @@ public class ChromeContextMenuPopulatorTest {
                         /* additionalNavigationParams= */ null);
         initializePopulator(ChromeContextMenuPopulator.ContextMenuMode.CUSTOM_TAB, linkParams);
         int[] linkExpected = {
-            R.id.contextmenu_open_in_new_chrome_tab,
-            R.id.contextmenu_open_in_chrome_incognito_tab,
+            R.id.contextmenu_open_in_browser_id,
             R.id.contextmenu_open_in_ephemeral_tab,
             R.id.contextmenu_copy_link_address,
             R.id.contextmenu_copy_link_text,
@@ -1820,8 +1811,7 @@ public class ChromeContextMenuPopulatorTest {
                         /* additionalNavigationParams= */ null);
         initializePopulator(ChromeContextMenuPopulator.ContextMenuMode.CUSTOM_TAB, imageParams);
         int[] imageExpected = {
-            R.id.contextmenu_open_in_new_chrome_tab,
-            R.id.contextmenu_open_in_chrome_incognito_tab,
+            R.id.contextmenu_open_in_browser_id,
             R.id.contextmenu_open_image,
             R.id.contextmenu_open_image_in_ephemeral_tab,
             R.id.contextmenu_copy_image,
