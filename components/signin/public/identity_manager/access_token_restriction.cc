bb088437331752a00c2b683af5a381a277eb2ca1
Faizur Rahman <faizur@chromium.org>
2025-12-02 06:33:32 -0800

    [Save to Drive] Conditionally include Drive scope for PDF save to Drive.
    Wrap the `GaiaConstants::kDriveOAuth2Scope` within the
    `ENABLE_PDF_SAVE_TO_DRIVE` build flag.
    
    Bug: 446152335
    Change-Id: Ib6aa17da7ac3ae97cdff86e88b02dacade61579a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7172949
    Commit-Queue: Faizur Rahman <faizur@chromium.org>
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Auto-Submit: Faizur Rahman <faizur@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1552788}

--- components/signin/public/identity_manager/access_token_restriction.cc
+++ components/signin/public/identity_manager/access_token_restriction.cc
@@ -12,6 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/plus_addresses/core/common/features.h"
 #include "google_apis/gaia/gaia_constants.h"
 
+#if !BUILDFLAG(IS_FUCHSIA)
+#include "pdf/buildflags.h"  // nogncheck
+#endif                       // !BUILDFLAG(IS_FUCHSIA)
+
 namespace signin {
 
 namespace {
@@ -34,10 +38,11 @@ bool IsUnrestrictedOAuth2Scopes(const std::string& scope) {
       // sign in.
       GaiaConstants::kSecureConnectOAuth2Scope,
 
-      // TODO(crbug.com/446152335): Consider wrapping this in
-      // `ENABLE_PDF_SAVE_TO_DRIVE` build flag.
-      // Required for save to Drive.
+#if !BUILDFLAG(IS_FUCHSIA)
+#if BUILDFLAG(ENABLE_PDF_SAVE_TO_DRIVE)
       GaiaConstants::kDriveOAuth2Scope,
+#endif  // BUILDFLAG(ENABLE_PDF_SAVE_TO_DRIVE)
+#endif  // !BUILDFLAG(IS_FUCHSIA)
 
       // TODO(b/321900823): Fix tests and move below scopes to require the
       // browser to be signed in.
