85caea60c21e6e9a80420dd32bc4e0451085e229
Magdalena Skarbinska <mskarbinska@google.com>
2025-12-02 05:02:02 -0800

    iwa: Add unmanaged updates discovery
    This change updates the IsolatedWebAppUpdateManager to detect updates
    for unmanaged IWAs during UpdateDiscoveryCheck. For all users installed
    IWAs it finds the update manifest URL stored within the app's
    IsolationData (this URL is sourced from app's web manifest during
    initial installation) and uses it to queue a discovery task, thus
    enabling automatic, periodic updates for these apps. This new discovery
    path is integrated with the existing update logic in which management
    policies maintain precedence over update manifest URL from
    IsolationData.
    
    Change-Id: I3e258826d80ed6fe0e2625440db8f82b0378d0f9
    Bug: 456736675
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7161815
    Commit-Queue: Magdalena Skarbińska <mskarbinska@google.com>
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Robert Ferens <rferens@google.com>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1552742}

--- chrome/browser/web_applications/isolated_web_apps/test/bundle_versions_storage.h
+++ chrome/browser/web_applications/isolated_web_apps/test/bundle_versions_storage.h
@@ -32,6 +32,10 @@ class BundleVersionsStorage {
       const GURL& base_url,
       const web_package::SignedWebBundleId& web_bundle_id);
 
+  static GURL GetBundleUrl(const GURL& base_url,
+                           const web_package::SignedWebBundleId& web_bundle_id,
+                           const IwaVersion& version);
+
   // Must be called once at startup.
   void SetBaseUrl(const GURL& base_url);
 
@@ -52,8 +56,12 @@ class BundleVersionsStorage {
   GURL GetUpdateManifestUrl(
       const web_package::SignedWebBundleId& web_bundle_id) const;
 
-  // Returns the update manifest for `web_bundle_id`. Will CHECK if there are no
-  // bundles served for this `web_bundle_id`.
+  // Returns the full URL to the bundle for `web_bundle_id` and `version`.
+  GURL GetBundleUrl(const web_package::SignedWebBundleId& web_bundle_id,
+                    const IwaVersion& version) const;
+
+  // Returns the update manifest for `web_bundle_id`. Will CHECK if there
+  // are no bundles served for this `web_bundle_id`.
   base::Value::Dict GetUpdateManifest(
       const web_package::SignedWebBundleId& web_bundle_id) const;
 
