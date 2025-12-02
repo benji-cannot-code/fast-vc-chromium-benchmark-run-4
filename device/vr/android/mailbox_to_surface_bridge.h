e818e93272cc4bc6690ea059d95d7db4ccbb025c
Colin Blundell <blundell@chromium.org>
2025-12-01 23:53:05 -0800

    [//device] Remove BufferFormat ref in mailbax_to_surface_bridge.h
    This is no longer needed.
    
    Bug: 356649879
    Change-Id: I3451ef7ec8affaa825e8a7069a32227744f88bd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7207573
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1552609}

--- device/vr/android/mailbox_to_surface_bridge.h
+++ device/vr/android/mailbox_to_surface_bridge.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/common/shared_image_usage.h"
 
 namespace gfx {
-enum class BufferFormat : uint8_t;
 class ColorSpace;
 class GpuFence;
 struct GpuMemoryBufferHandle;
