760dce69e7484a3891caef82937db5b17d1acb92
Dan Polanco <polardz@google.com>
2025-12-02 07:59:44 -0800

    Reland "[Media Indicator] Add testing for audible/muted"
    This is a reland of commit 21205c23cb76756fbfee25a5a6f1bf421410ab32
    
    Revert CL: https://crrev.com/c/7206183
    Revert fix: Use clickNodeWithJavaScript as it is more reliable.
    
    Original change's description:
    > [Media Indicator] Add testing for audible/muted
    >
    > Bug: 454045510
    > Change-Id: I440b7930671381a8b8ba2d21200b8d26302fc2cb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7213548
    > Commit-Queue: Dan Polanco <polardz@google.com>
    > Auto-Submit: Dan Polanco <polardz@google.com>
    > Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1552520}
    
    Bug: 454045510
    Change-Id: Idb34803a055c991d895443e2433b25264bccd281
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7212620
    Commit-Queue: Dan Polanco <polardz@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1552849}

--- chrome/browser/android/media_state_observer.cc
+++ chrome/browser/android/media_state_observer.cc
@@ -23,7 +23,6 @@ enum MediaState {
 };
 }  // namespace
 
-// TODO(crbug.com/454045510): Add tests.
 MediaStateObserver::MediaStateObserver(content::WebContents* web_contents)
     : content::WebContentsObserver(web_contents),
       content::WebContentsUserData<MediaStateObserver>(*web_contents),
