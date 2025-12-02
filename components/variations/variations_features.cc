c1c9483b688b5b7e69b1c546dd42f6fb480004c4
Ramon Cano Aparicio <rcanoaparicio@google.com>
2025-12-02 06:53:45 -0800

    Clean code of purge seed from memory experiment.
    The experiment doesn't cause any effect in local state base seeds.
    Remove the experiment so it affects to all the clients in Seed File
    experiment treatment group.
    
    Bug: 411433119
    Change-Id: I5fc0336db59ee8c4cc9b2a56830cc425642bf703
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7184838
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1552804}

--- components/variations/variations_features.cc
+++ components/variations/variations_features.cc
@@ -7,6 +7,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace variations::internal {
 
-BASE_FEATURE(kPurgeVariationsSeedFromMemory, base::FEATURE_DISABLED_BY_DEFAULT);
-
 }  // namespace variations::internal
--- components/variations/variations_features.h
+++ components/variations/variations_features.h
@@ -12,10 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace variations::internal {
 
-// A feature to allow removing the finch seed data from memory when it is no
-// longer needed.
-COMPONENT_EXPORT(VARIATIONS_FEATURES)
-BASE_DECLARE_FEATURE(kPurgeVariationsSeedFromMemory);
+// Add Variations Features here...
 
 }  // namespace variations::internal
 
