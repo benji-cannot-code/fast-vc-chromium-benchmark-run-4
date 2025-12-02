5c710595687eff6ea625a04195984a18b493fe77
Daniel Murphy <dmurph@chromium.org>
2025-12-02 14:12:22 -0800

    [PWA] Add persistable log to WebAppDatabase
    This change:
    - Adds a log to WebAppDatabase, and hooks it up to
      chrome://web-app-internals.
    - Reorders the web-app-internals page into hopefully better categories.
    - Adds a helper method to PersistableLog to output a list, and uses it
      in the command manager as well.
    
    Bug: 40224498
    Change-Id: Iae26a2c979e0349e260ac88fc6071736eb4abd89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7207934
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1553092}

--- chrome/browser/web_applications/web_app_provider.h
+++ chrome/browser/web_applications/web_app_provider.h
@@ -265,6 +265,8 @@ class WebAppProvider : public KeyedService {
   // is repeating so tests can test the throttle logic.
   base::RepeatingClosure DisableDelayedPostStartupWorkForTesting();
 
+  Profile* profile() const { return profile_.get(); }
+
  protected:
   virtual void StartImpl();
 
