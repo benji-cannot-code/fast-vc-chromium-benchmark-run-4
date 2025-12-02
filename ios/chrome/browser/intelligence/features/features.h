592bb50464b7e5662c1b60d90dab62b276a33199
Tarek Makkouk <tarekmakkouk@google.com>
2025-12-01 18:28:32 -0800

    [iOS] Add dynamic page context for copresence flag
    Bug: 465131961
    Change-Id: I4b748806c16f0a6d3b6c1e91897843f008bbe563
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7203890
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Reviewed-by: Joemer Ramos <joemerramos@google.com>
    Commit-Queue: Tarek Makkouk <tarekmakkouk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1552531}

--- ios/chrome/browser/intelligence/features/features.h
+++ ios/chrome/browser/intelligence/features/features.h
@@ -232,4 +232,8 @@ bool IsGeminiLiveEnabled();
 BASE_DECLARE_FEATURE(kGeminiPersonalization);
 bool IsGeminiPersonalizationEnabled();
 
+// Feature flag for Gemini Copresence.
+BASE_DECLARE_FEATURE(kGeminiCopresence);
+bool IsGeminiCopresenceEnabled();
+
 #endif  // IOS_CHROME_BROWSER_INTELLIGENCE_FEATURES_FEATURES_H_
