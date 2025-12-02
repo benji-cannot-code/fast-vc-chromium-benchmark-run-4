c7989eb858737003b7a25bdf5a46206147cfaab7
Bailey Myers-Morgan <bmyersmorgan@google.com>
2025-12-01 18:02:55 -0800

    [VideoStack] [OpenScreen] Launch chrome in fullscreen
    This removes scrollbars that are interfering with barcode extraction for
    the video analyzer.
    
    Bug: 455618553
    Change-Id: I73a709b808ab50676bcdb9e6d1f51e63a93012c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7214326
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Reviewed-by: Jordan Bayles <jophba@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1552489}

--- chrome/test/media/performance/openscreen_cast_performance_test.py
+++ chrome/test/media/performance/openscreen_cast_performance_test.py
@@ -50,7 +50,9 @@ CHROME_OPTIONS = [
     # Skips the first-run experience modal.
     "--no-first-run",
     # Prevents the "Set as default browser" prompt from appearing.
-    "--no-default-browser-check"
+    "--no-default-browser-check",
+    # Launches Chrome in fullscreen mode to prevent scrollbar clipping.
+    "--start-fullscreen",
 ]
 
 
