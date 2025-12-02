2611c8562fe945c38daecd04fda80e8d75556711
Justin Novosad <junov@chromium.org>
2025-12-02 11:01:02 -0800

    Add tests that define best practices for C++ observing swift
    Add tests that demonstrate:
    * How to register a method refcounted C++ class as an observer
    * How to register a base::RepeatingCallback as an observer
    * How to bind a base::RepeatingCallback to a Combine Publisher
    
    Bug: 460740228
    Change-Id: I4edd0dc1ba823f942e1ac5acec7ec2a511df6045
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7200768
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1552966}

---
+++ ios/chrome/test/swift_interop/closure/observer.cc
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/chrome/test/swift_interop/include/observer.h"
+
+SWIFT_DEFINE_INTEROP_WRAPPER(ValueDidChangeCallback,
+                             base::RepeatingCallback<void(int)>)
+SWIFT_DEFINE_REF_COUNTED_HELPERS(ValueObserver)
+
+ValueObserver::ValueObserver() = default;
+
+ValueObserver::~ValueObserver() = default;
+
+ValueDidChangeCallback ValueObserver::GetValueDidChangeCallback() {
+  return base::BindRepeating(&ValueObserver::ValueDidChange,
+                             weak_ptr_factory_.GetWeakPtr());
+}
+
+void ValueObserver::ValueDidChange(int value) {
+  value_ = value;
+}
+
+ValueObserver* ValueObserver::makeForSwift() {
+  // We inentionally leak the reference, which is adopted by swift.
+  return base::MakeRefCounted<ValueObserver>().release();
+}
