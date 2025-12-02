ae78162f250ba83eb56bb6906181272d22043263
Louis Romero <lpromero@google.com>
2025-12-02 09:31:46 -0800

    Ignore Composebox tests on iPad
    Composebox is not yet available on iPad.
    
    Fixed: 465431423
    Change-Id: Ie4cf394646edc3022e6e6cb5a1177008f63478ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7216253
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1552902}

--- ios/chrome/browser/composebox/ui/composebox_egtest.mm
+++ ios/chrome/browser/composebox/ui/composebox_egtest.mm
@@ -49,6 +49,11 @@ id<GREYMatcher> ComposeboxClearButtonMatcher() {
 
 // Tests that the Composebox is visible when tapping the omnibox.
 - (void)testComposeboxVisibility {
+  // Composebox is not available on iPad.
+  if ([ChromeEarlGrey isIPadIdiom]) {
+    EARL_GREY_TEST_SKIPPED(@"Skipped for iPad as composebox is not available.");
+  }
+
   [ChromeEarlGrey loadURL:self.testServer->GetURL("/")];
   [ChromeEarlGreyUI focusOmnibox];
 
@@ -76,6 +81,11 @@ id<GREYMatcher> ComposeboxClearButtonMatcher() {
 
 // Tests that typing in the Composebox shows the Send button.
 - (void)testComposeboxSendButtonVisibility {
+  // Composebox is not available on iPad.
+  if ([ChromeEarlGrey isIPadIdiom]) {
+    EARL_GREY_TEST_SKIPPED(@"Skipped for iPad as composebox is not available.");
+  }
+
   [ChromeEarlGrey loadURL:self.testServer->GetURL("/")];
   [ChromeEarlGreyUI focusOmnibox];
 
