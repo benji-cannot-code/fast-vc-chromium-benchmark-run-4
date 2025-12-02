f39364d6ac805416f3053f73c9579884f70c0b78
Tomasz Wiszkowski <ender@google.com>
2025-12-02 15:06:27 -0800

    Recognize attachments as user input and show Navigate button.
    Attachments are only available in the Fusebox using any of the AI
    tools. Presence of attachments is considered to be a sufficient user
    input. Mic and Lens buttons dont apply in the AI fusebox
    
    Fixed: 462450221
    Change-Id: I229b4741768cfdc9fc285b0b67fad9062298a7c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7219131
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1553128}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxCoordinator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxCoordinator.java
@@ -336,4 +336,9 @@ public class FuseboxCoordinator implements UrlFocusChangeListener, TemplateUrlSe
     public ObservableSupplier<Boolean> getOnCompactModeChangedSupplier() {
         return mOnCompactModeChangedSupplier;
     }
+
+    /** Returns whether the Fusebox Attachments list contains any user-added entries. */
+    public boolean hasUserAddedAttachments() {
+        return !mModelList.isEmpty();
+    }
 }
