9e2255bd37a8d61744168889f99942ca8265ab75
Zhentao Lu <zhentao.lu@microsoft.com>
2025-12-01 18:13:02 -0800

    [bedrock] migrate BrowserList::GetOffTheRecordBrowsersActiveForProfile to use ForEachCurrentBrowserWindowInterfaceOrderedByActivation
    The main content is to migrate BrowserList::GetOffTheRecordBrowsersActiveForProfile call sites
    to the BrowserWindowInterface api.
    
    
    Bug: 431671320
    Change-Id: I7d4506814c741540e88e8b74c12338e335960dd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7171345
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Qikai Zhong <qikaizhong@microsoft.com>
    Commit-Queue: Zhentao Lu <zhentao.lu@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1552506}

--- chrome/browser/ui/views/profiles/incognito_menu_view.cc
+++ chrome/browser/ui/views/profiles/incognito_menu_view.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/color/chrome_color_id.h"
 #include "chrome/browser/ui/ui_features.h"
@@ -41,8 +42,8 @@ IncognitoMenuView::IncognitoMenuView(ui::TrackedElement* anchor_element,
 IncognitoMenuView::~IncognitoMenuView() = default;
 
 void IncognitoMenuView::BuildMenu() {
-  int incognito_window_count =
-      BrowserList::GetOffTheRecordBrowsersActiveForProfile(&profile());
+  int incognito_window_count = static_cast<int>(
+      chrome::GetOffTheRecordBrowsersActiveForProfile(&profile()));
   std::u16string close_button_title = l10n_util::GetPluralStringFUTF16(
       IDS_INCOGNITO_PROFILE_MENU_CLOSE_X_WINDOWS_BUTTON,
       incognito_window_count);
@@ -66,7 +67,8 @@ void IncognitoMenuView::BuildMenu() {
 std::u16string IncognitoMenuView::GetAccessibleWindowTitle() const {
   return l10n_util::GetPluralStringFUTF16(
       IDS_INCOGNITO_BUBBLE_ACCESSIBLE_TITLE,
-      BrowserList::GetOffTheRecordBrowsersActiveForProfile(&profile()));
+      static_cast<int>(
+          chrome::GetOffTheRecordBrowsersActiveForProfile(&profile())));
 }
 
 void IncognitoMenuView::OnExitButtonClicked() {
