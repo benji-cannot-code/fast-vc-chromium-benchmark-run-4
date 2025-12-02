94f898a2c029ea6b1c4a5cfe9ce98451c7efd390
Ramon Cano Aparicio <rcanoaparicio@google.com>
2025-12-02 09:03:30 -0800

    Enable seed file experiment on Beta channel.
    The seed file experiment is now enabled for clients on the Beta channel,
    in addition to Canary and Dev.
    
    Bug: 445616300
    Change-Id: If10d6452c00a7d13258d8b1766fdc107cb23f39d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7185259
    Commit-Queue: Ramon Cano Aparicio <rcanoaparicio@google.com>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Cr-Commit-Position: refs/heads/main@{#1552882}

--- components/variations/seed_reader_writer_unittest.cc
+++ components/variations/seed_reader_writer_unittest.cc
@@ -159,9 +159,9 @@ class ExpectedFieldTrialGroupChannelsTest
 
 class ExpectedFieldTrialGroupAllChannelsTest
     : public ExpectedFieldTrialGroupChannelsTest {};
-class ExpectedFieldTrialGroupCanaryDevTest
+class ExpectedFieldTrialGroupPreStableTest
     : public ExpectedFieldTrialGroupChannelsTest {};
-class ExpectedFieldTrialGroupBetaStableUnknownTest
+class ExpectedFieldTrialGroupStableUnknownTest
     : public ExpectedFieldTrialGroupChannelsTest {};
 
 INSTANTIATE_TEST_SUITE_P(
@@ -198,15 +198,16 @@ TEST_P(ExpectedFieldTrialGroupAllChannelsTest, NoEntropyProvider) {
 
 INSTANTIATE_TEST_SUITE_P(
     All,
-    ExpectedFieldTrialGroupCanaryDevTest,
+    ExpectedFieldTrialGroupPreStableTest,
     ::testing::ConvertGenerator<ExpectedFieldTrialGroupTestParams::TupleT>(
         ::testing::Combine(::testing::Values(kRegularSeedFieldsPrefs,
                                              kSafeSeedFieldsPrefs),
                            ::testing::Values(version_info::Channel::CANARY,
-                                             version_info::Channel::DEV))));
+                                             version_info::Channel::DEV,
+                                             version_info::Channel::BETA))));
 
 // If channel is canary or dev, client is assigned a group.
-TEST_P(ExpectedFieldTrialGroupCanaryDevTest, AssignedGroup) {
+TEST_P(ExpectedFieldTrialGroupPreStableTest, AssignedGroup) {
   SeedReaderWriter seed_reader_writer(
       &local_state_, /*seed_file_dir=*/temp_dir_.GetPath(), kSeedFilename,
       kOldSeedFilename, GetParam().seed_fields_prefs, GetParam().channel,
@@ -218,16 +219,15 @@ TEST_P(ExpectedFieldTrialGroupCanaryDevTest, AssignedGroup) {
 
 INSTANTIATE_TEST_SUITE_P(
     All,
-    ExpectedFieldTrialGroupBetaStableUnknownTest,
+    ExpectedFieldTrialGroupStableUnknownTest,
     ::testing::ConvertGenerator<ExpectedFieldTrialGroupTestParams::TupleT>(
         ::testing::Combine(::testing::Values(kRegularSeedFieldsPrefs,
                                              kSafeSeedFieldsPrefs),
-                           ::testing::Values(version_info::Channel::BETA,
-                                             version_info::Channel::STABLE,
+                           ::testing::Values(version_info::Channel::STABLE,
                                              version_info::Channel::UNKNOWN))));
 
 // If channel is beta, stable, or unknown, client is not assigned a group.
-TEST_P(ExpectedFieldTrialGroupBetaStableUnknownTest, NotAssignedGroup) {
+TEST_P(ExpectedFieldTrialGroupStableUnknownTest, NotAssignedGroup) {
   SeedReaderWriter seed_reader_writer(
       &local_state_, /*seed_file_dir=*/temp_dir_.GetPath(), kSeedFilename,
       kOldSeedFilename, GetParam().seed_fields_prefs, GetParam().channel,
@@ -932,7 +932,8 @@ INSTANTIATE_TEST_SUITE_P(
                                              kSafeSeedFieldsPrefs),
                            ::testing::Values(kSeedFilesGroup),
                            ::testing::Values(version_info::Channel::CANARY,
-                                             version_info::Channel::DEV))));
+                                             version_info::Channel::DEV,
+                                             version_info::Channel::BETA))));
 
 // Verifies clients using local state to store seeds write seeds to Local State.
 TEST_P(SeedReaderWriterLocalStateGroupsTest, WriteSeed) {
@@ -1531,8 +1532,7 @@ INSTANTIATE_TEST_SUITE_P(
                                              kSafeSeedFieldsPrefs),
                            ::testing::Values(kNoGroup),
                            ::testing::Values(version_info::Channel::UNKNOWN,
-                                             version_info::Channel::STABLE,
-                                             version_info::Channel::BETA))));
+                                             version_info::Channel::STABLE))));
 
 INSTANTIATE_TEST_SUITE_P(
     ControlAndDefaultGroup,
