63e1cc83e8a3d89243d95d6d94080175c245d99f
Julia Hansbrough <flowerhack@google.com>
2025-12-01 18:00:29 -0800

    [rustmojo] Add simple data pipe producer + consumer test.
    A follow-on CL creates support for two-phase reads/writes, but it was
    rather large to do it all in one CL, so this CL gets the most basic form
    of functionality in place.
    
    Additionally drops a placeholder trap.rs file, as implementation of
    trap/wait are necessary for the two-phase reads/writes (also in a
    follow-on CL).
    
    Bug: 444509367
    Change-Id: I6e7d344dd8b5600a463bdeb0decb0e41bb67ae1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7172099
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Julia Hansbrough <flowerhack@google.com>
    Reviewed-by: Devon Loehr <dloehr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1552485}

--- mojo/public/rust/system/trap.rs
+++ mojo/public/rust/system/trap.rs
@@ -1,8 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2025 The Chromium Authors
+//Copyright 2025 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-pub mod data_pipe;
-pub mod mojo_types;
-pub mod trap;
+// FOR RELEASE: Implementation for traps will go here (necessary for
+// coordinating synchronous writes/reads).
