86f38b759dbc44afdd71684c45640019edb49a2d
Olivier Li <olivierli@chromium.org>
2025-12-02 09:29:25 -0800

    Correct use of PersistentCacheCollection in GeneratedCodeCacheContext
    The class is missing null checks on some of the functions that access
    the collection which can lead to crashes. The PersistentCacheCollection
    can be null during shutdown once GeneratedCodeCacheContext::Shutdown()
    has been called.
    
    This CL adds the necessary null checks and also cleans up the sequence
    checker use a bit.
    
    Bug: 465426158
    Change-Id: I7d68cf1a837481049fc8b69585c2957a4c87f234
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7216469
    Auto-Submit: Olivier Li <olivierli@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Olivier Li <olivierli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1552899}

--- content/browser/code_cache/generated_code_cache_context.cc
+++ content/browser/code_cache/generated_code_cache_context.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/code_cache/generated_code_cache_context.h"
 
 #include <memory>
+#include <optional>
 
 #include "base/feature_list.h"
 #include "base/files/file_path.h"
@@ -204,7 +205,11 @@ void GeneratedCodeCacheContext::ClearAndDeletePersistentCacheCollection() {
 std::optional<persistent_cache::PendingBackend>
 GeneratedCodeCacheContext::ShareReadOnlyConnection(
     const std::string& context_key) {
-  return persistent_cache_collection_->ShareReadOnlyConnection(context_key);
+  if (persistent_cache_collection_) {
+    return persistent_cache_collection_->ShareReadOnlyConnection(context_key);
+  }
+
+  return std::nullopt;
 }
 
 void GeneratedCodeCacheContext::InsertIntoPersistentCacheCollection(
@@ -212,6 +217,10 @@ void GeneratedCodeCacheContext::InsertIntoPersistentCacheCollection(
     std::string_view url,
     base::span<const uint8_t> content,
     persistent_cache::EntryMetadata metadata) {
+  if (!persistent_cache_collection_) {
+    return;
+  }
+
   // Since `content` is coming in through mojo it's important to make sure that
   // it's copied so it cannot be modified racily. This happens implicitly
   // because of the way the SQLite backend (the only backend available
@@ -232,6 +241,10 @@ std::optional<GeneratedCodeCacheContext::MetadataAndContent>
 GeneratedCodeCacheContext::FindInPersistentCacheCollection(
     const std::string& context_key,
     std::string_view url) {
+  if (!persistent_cache_collection_) {
+    return std::nullopt;
+  }
+
   mojo_base::BigBuffer content_buffer;
 
   // A BufferProvider for PersistentCache that puts a new mojo_base::BugBuffer
