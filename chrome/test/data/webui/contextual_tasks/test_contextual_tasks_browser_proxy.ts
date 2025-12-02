8af2e430c504720f552eaf1d5107c32700a1c369
Josiah Kiehl <kiehl@google.com>
2025-12-02 11:40:51 -0800

    Add tests for the Contextual Tasks composebox.
    Bootstrap composebox_test.ts for contextual_tasks. This tests the basic
    submit flow and sets up the composebox for further tests to be added.
    
    Bug: 451688705
    Change-Id: I484b71a908af3d24caebb50fa4542cd7074d9bef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7207875
    Commit-Queue: Josiah Kiehl <kiehl@google.com>
    Reviewed-by: Nihar Majmudar <niharm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1553002}

--- chrome/test/data/webui/contextual_tasks/test_contextual_tasks_browser_proxy.ts
+++ chrome/test/data/webui/contextual_tasks/test_contextual_tasks_browser_proxy.ts
@@ -7,9 +7,12 @@ import {PageCallbackRouter} from 'chrome://contextual-tasks/contextual_tasks.moj
 import type {PageHandlerInterface, PageInterface, PageRemote} from 'chrome://contextual-tasks/contextual_tasks.mojom-webui.js';
 import type {BrowserProxy} from 'chrome://contextual-tasks/contextual_tasks_browser_proxy.js';
 import type {PostMessageHandler} from 'chrome://contextual-tasks/post_message_handler.js';
+import type {PageHandler as ComposeboxPageHandler, PageHandlerFactory as ComposeboxPageHandlerFactory} from 'chrome://resources/cr_components/composebox/composebox.mojom-webui.js';
 import type {Uuid} from 'chrome://resources/mojo/mojo/public/mojom/base/uuid.mojom-webui.js';
 import type {Url} from 'chrome://resources/mojo/url/mojom/url.mojom-webui.js';
 import {TestBrowserProxy} from 'chrome://webui-test/test_browser_proxy.js';
+
+import {TestSearchboxPageHandler} from './test_searchbox_page_handler.js';
 
 const BASE64_HANDSHAKE_RESPONSE = 'CgIIAA==';
 
@@ -91,6 +94,7 @@ class TestContextualTasksPageHandler extends TestBrowserProxy implements
       'onWebviewMessage',
       'getHandshakeMessage',
       'submitQuery',
+      'getRecentTabs',
     ]);
 
     this.url_ = {url};
@@ -162,6 +166,11 @@ class TestContextualTasksPageHandler extends TestBrowserProxy implements
     return Promise.resolve({tabs: []});
   }
 
+  getRecentTabs() {
+    this.methodCalled('getRecentTabs');
+    return Promise.resolve({tabs: []});
+  }
+
   onTabClickedFromSourcesMenu(tabId: number, url: Url) {
     this.methodCalled('onTabClickedFromSourcesMenu', tabId, url);
   }
@@ -205,6 +214,11 @@ class TestContextualTasksPageHandler extends TestBrowserProxy implements
       metaKey: boolean, shiftKey: boolean) {
     this.methodCalled(
         'submitQuery', query, mouseButton, altKey, ctrlKey, metaKey, shiftKey);
+    return Promise.resolve();
+  }
+
+  postMessageToWebview(message: number[]) {
+    this.methodCalled('postMessageToWebview', message);
   }
 }
 
@@ -213,9 +227,11 @@ class TestContextualTasksPageHandler extends TestBrowserProxy implements
  * Tasks page to the browser on start up.
  */
 export class TestContextualTasksBrowserProxy extends TestBrowserProxy implements
-    BrowserProxy {
+    BrowserProxy, ComposeboxPageHandlerFactory {
   callbackRouter: PageCallbackRouter;
   handler: TestContextualTasksPageHandler;
+  composeboxHandler: TestBrowserProxy&ComposeboxPageHandler;
+  searchboxHandler: TestSearchboxPageHandler;
   page: MockPage;
   callbackRouterRemote: PageRemote;
 
@@ -223,11 +239,24 @@ export class TestContextualTasksBrowserProxy extends TestBrowserProxy implements
    * @param url The URL to load in the webview.
    */
   constructor(url: string) {
-    super([]);
+    super([
+      'createPageHandler',
+    ]);
     this.callbackRouter = new PageCallbackRouter();
     this.page = new MockPage();
     this.handler = new TestContextualTasksPageHandler(url, this.page);
+    this.composeboxHandler = new TestBrowserProxy();
+    this.searchboxHandler = new TestSearchboxPageHandler();
     this.callbackRouterRemote =
         this.callbackRouter.$.bindNewPipeAndPassRemote();
+  }
+
+  createPageHandler() {
+    this.methodCalled('createPageHandler');
+    return {
+      handler: this.handler,
+      composeboxHandler: this.composeboxHandler,
+      searchboxHandler: this.searchboxHandler,
+    };
   }
 }
