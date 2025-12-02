6dc8bc2491578cea39ef9c311d07e2f5f0c31eca
Fiaz Muhammad <mfiaz@google.com>
2025-12-02 14:07:30 -0800

    [Persist] Lazy Packaging for StorageUpdateUnits
    - Introduce PendingUpdates to allow for lazy packaging.
            PendingUpdates allow for deferring packaging until
            UpdateUnits must be created. This removes the performance
            overhead associated with the creation of units.
    - PendingUpdates handle the responsibility of the UI-thread-only
            packaging process and create db-thread-only UpdateUnits.
    - StorageUnits can return to being executed without exposing their
            underlying implementation details.
    
    Bug: 450575049
    Change-Id: If454c0ba8d268be3b34d0d863e2a5774790f3e3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7216528
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Auto-Submit: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1553086}

--- chrome/browser/tab/tab_storage_packager.h
+++ chrome/browser/tab/tab_storage_packager.h
@@ -65,11 +65,9 @@ class TabStoragePackager {
 
  private:
   std::unique_ptr<Payload> PackageTabGroupTabCollectionData(
-      const TabGroupTabCollection* collection,
-      StorageIdMapping& mapping);
+      const TabGroupTabCollection* collection);
   std::unique_ptr<Payload> PackageSplitTabCollectionData(
-      const SplitTabCollection* collection,
-      StorageIdMapping& mapping);
+      const SplitTabCollection* collection);
 };
 
 }  // namespace tabs
