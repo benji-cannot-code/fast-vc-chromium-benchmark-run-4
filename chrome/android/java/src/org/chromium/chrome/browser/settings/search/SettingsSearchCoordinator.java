180abdcb3741d8da637b480d8628b14fb4b520d3
Jinsuk Kim <jinsukkim@chromium.org>
2025-12-01 18:34:33 -0800

    [Android][Settings] Bottom margin for search bar in single column
    Gives a bottom margin to search bar in single column mode as per
    the mock.
    
    Before: https://screenshot.googleplex.com/4viagn8tGTCPDHS
    After: https://screenshot.googleplex.com/36tizNdMsjj959L
    
    Bug: 444468463
    Change-Id: I0c70d1c9159b29dca958fa09d5844ee8305485e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7213118
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Moe Adel <adelm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1552541}

--- chrome/android/java/src/org/chromium/chrome/browser/settings/search/SettingsSearchCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/search/SettingsSearchCoordinator.java
@@ -173,6 +173,7 @@ public class SettingsSearchCoordinator {
         LayoutInflater.from(mActivity).inflate(R.layout.settings_search_box, searchBoxParent, true);
         LayoutInflater.from(mActivity).inflate(R.layout.settings_search_query, actionBar, true);
         View searchBox = mActivity.findViewById(R.id.search_box);
+        setSearchBoxBottomMargin(searchBox, mUseMultiColumn);
         searchBox.setOnClickListener(v -> enterSearchState());
 
         if (mMultiColumnSettings != null) {
@@ -516,6 +517,7 @@ public class SettingsSearchCoordinator {
         View query = mActivity.findViewById(R.id.search_query_container);
         if (mUseMultiColumn) {
             ViewGroup actionBar = mActivity.findViewById(R.id.action_bar);
+            setSearchBoxBottomMargin(searchBox, true);
             assumeNonNull(actionBar).addView(searchBox);
             if (mFragmentState == FS_RESULTS) {
                 // Make the query edit UI visible which was hidden in single-column mode.
@@ -524,6 +526,7 @@ public class SettingsSearchCoordinator {
         } else {
             // Search bar goes beneath the toolbar (app_bar_layout) in single-column layout.
             ViewGroup appBarLayout = mActivity.findViewById(R.id.app_bar_layout);
+            setSearchBoxBottomMargin(searchBox, false);
             appBarLayout.addView(searchBox);
 
             // Query edit UI should be hidden while we're browsing results.
@@ -538,6 +541,12 @@ public class SettingsSearchCoordinator {
                 }
             }
         }
+    }
+
+    private void setSearchBoxBottomMargin(View searchBox, boolean multiColumn) {
+        var lp = (ViewGroup.MarginLayoutParams) searchBox.getLayoutParams();
+        lp.bottomMargin = multiColumn ? 0 : getPixelSize(R.dimen.settings_search_ui_bottom_margin);
+        searchBox.setLayoutParams(lp);
     }
 
     private void setUpQueryEdit(EditText queryEdit) {
