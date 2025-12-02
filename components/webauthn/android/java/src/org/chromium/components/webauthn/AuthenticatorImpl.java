a73d57d3fe63eeb114481ee2934e17a9bc7f59c4
Adem Derinel <derinel@google.com>
2025-12-02 01:40:09 -0800

    Android: Refactor GetMatchingCredentialIds into a dedicated delegate
    This change extracts the logic for handling `getMatchingCredentialIds` requests from `Fido2CredentialRequest` into a new class, `GetMatchingCredentialIdsDelegate`. This delegate is responsible for interacting with GMS Core to retrieve and filter credential IDs based on the provided allowlist and payment requirements. Tests for this functionality have been moved to a new test file, `GetMatchingCredentialsDelegateTest.java`. The callback interface for `getMatchingCredentialIds` is updated to allow a null response, signifying an error.
    
    Change-Id: I27d9bb48616acdf51b9d33c148a48583906b3d65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7150798
    Commit-Queue: Adem Derinel <derinel@google.com>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1552663}

--- components/webauthn/android/java/src/org/chromium/components/webauthn/AuthenticatorImpl.java
+++ components/webauthn/android/java/src/org/chromium/components/webauthn/AuthenticatorImpl.java
@@ -431,20 +431,12 @@ public final class AuthenticatorImpl implements Authenticator, AuthenticationCon
             String relyingPartyId,
             byte[][] credentialIds,
             boolean requireThirdPartyPayment,
-            GetMatchingCredentialIdsResponseCallback callback) {
+            GetMatchingCredentialIdsDelegate.ResponseCallback callback) {
         log(TAG, "getMatchingCredentialIds");
-        if (!GmsCoreUtils.isGetMatchingCredentialIdsSupported()) {
-            callback.onResponse(new ArrayList<byte[]>());
-            return;
-        }
 
-        getFido2CredentialRequest()
-                .handleGetMatchingCredentialIdsRequest(
-                        relyingPartyId,
-                        credentialIds,
-                        requireThirdPartyPayment,
-                        callback,
-                        this::onError);
+        GetMatchingCredentialIdsDelegate.getInstance()
+                .getMatchingCredentialIds(
+                        this, relyingPartyId, credentialIds, requireThirdPartyPayment, callback);
     }
 
     @Override
@@ -562,11 +554,6 @@ public final class AuthenticatorImpl implements Authenticator, AuthenticationCon
     private void completeRequest(@NonNull WebauthnRequestResponse requestResponse) {
         assumeNonNull(mRequestCallback).onComplete(requestResponse);
         if (mPendingFido2CredentialRequest != null) mPendingFido2CredentialRequest.destroyBridge();
-        cleanupRequest();
-    }
-
-    private void onError(Integer status) {
-        log(TAG, "Request completed with error: " + status);
         cleanupRequest();
     }
 
--- components/webauthn/android/java/src/org/chromium/components/webauthn/InternalAuthenticator.java
+++ components/webauthn/android/java/src/org/chromium/components/webauthn/InternalAuthenticator.java
@@ -30,6 +30,7 @@ import org.chromium.ui.base.WindowAndroid;
 import org.chromium.url.Origin;
 
 import java.nio.ByteBuffer;
+import java.util.List;
 
 /**
  * Acts as a bridge from InternalAuthenticator declared in
@@ -210,14 +211,18 @@ public class InternalAuthenticator {
                 relyingPartyId,
                 credentialIds,
                 requireThirdPartyPayment,
-                (matchingCredentialIds) -> {
-                    if (mNativeInternalAuthenticatorAndroid != 0) {
-                        InternalAuthenticatorJni.get()
-                                .invokeGetMatchingCredentialIdsResponse(
-                                        mNativeInternalAuthenticatorAndroid,
-                                        matchingCredentialIds.toArray(new byte[0][]));
-                    }
-                });
+                this::handleGetMatchingCredentialIdsResponse);
+    }
+
+    private void handleGetMatchingCredentialIdsResponse(
+            @Nullable List<byte[]> matchingCredentialIds) {
+        if (matchingCredentialIds == null || mNativeInternalAuthenticatorAndroid == 0) {
+            return;
+        }
+        InternalAuthenticatorJni.get()
+                .invokeGetMatchingCredentialIdsResponse(
+                        mNativeInternalAuthenticatorAndroid,
+                        matchingCredentialIds.toArray(new byte[0][]));
     }
 
     @CalledByNative
