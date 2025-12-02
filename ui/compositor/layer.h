6a40bfd7a839483778eaabc85b42ece3f05d604b
Mitsuru Oshima <oshima@chromium.org>
2025-12-02 00:14:31 -0800

    Revert "Report stack dump when the cc::Layer is deleted before ui::Layer."
    This reverts commit 6bf5918f9f5fc1a5b27a81faa423ef970fbfb787.
    
    Reason for revert: The bug was fixed in crrev.com/c/7182233
    
    Original change's description:
    > Report stack dump when the cc::Layer is deleted before ui::Layer.
    >
    > This also add null check in ui::Layer::SetMasksToBounds.
    >
    > This is to diagnose potential use-after-free or unexpected deletion
    > of cc::Layer.
    >
    > Bug: 443811562
    >
    > Change-Id: I07971c2e7438bd75b93a928926ad1441942fa3d7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7108304
    > Reviewed-by: James Cook <jamescook@chromium.org>
    > Reviewed-by: Colin Blundell <blundell@chromium.org>
    > Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1540957}
    
    Bug: 443811562
    Change-Id: If198132bdc83ef54e3dd0afaaf0cc4334685e379
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7206673
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1552626}

--- ui/compositor/layer.h
+++ ui/compositor/layer.h
@@ -621,10 +621,6 @@ class COMPOSITOR_EXPORT Layer : public LayerAnimationDelegate,
     compositor_ = compositor;
   }
 
-#if BUILDFLAG(IS_CHROMEOS)
-  void EnableLayerDestructionCheck();
-#endif
-
  private:
   friend class LayerOwner;
   class LayerMirror;
