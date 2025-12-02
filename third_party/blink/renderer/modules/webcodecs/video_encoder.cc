02ff57ff5f9308de7606ce15a30667e1880db5eb
Ma, Yingying <yingying.ma@intel.com>
2025-12-01 21:56:43 -0800

    Revert "[Win] Disable SharedImgage encode when Graphite is enabled."
    This reverts commit 9672041c84e43b9f301714e14d14a4cde8ee1dff.
    
    Reason for revert: No performance regression was observed on win10-perf with SharedImgage encode and Graphite enabled
    
    Original change's description:
    > [Win] Disable SharedImgage encode when Graphite is enabled.
    >
    > When SkiaGraphite is enabled, VideoFrame constructed from canvas is
    > with underlying D3D11 resource non-sharable, so that VEA has to
    > always copy per-frame and the synchronization overhead is high enough
    > that caused WebCodecs encoding performance regression.
    > This allows us experiment with WebRTC SharedImage encode without
    > impacting WebCodecs.
    >
    > Bug: 425118329
    > Change-Id: Ia78ecd8a5a6ff7dc6e4e5c87208ad77f88012da5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7077599
    > Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    > Commit-Queue: Jianlin Qiu <jianlin.qiu@intel.com>
    > Cr-Commit-Position: refs/heads/main@{#1535764}
    
    Bug: 425118329
    Change-Id: I3dbe5a793ff8d59c015f35dc243fad504f520a5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7204275
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Ma, Yingying <yingying.ma@intel.com>
    Reviewed-by: Qiu, Jianlin <jianlin.qiu@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1552584}

--- third_party/blink/renderer/modules/webcodecs/video_encoder.cc
+++ third_party/blink/renderer/modules/webcodecs/video_encoder.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/gpu/raster_context_provider.h"
 #include "gpu/GLES2/gl2extchromium.h"
 #include "gpu/command_buffer/client/raster_interface.h"
-#include "gpu/config/gpu_finch_features.h"
 #include "media/base/async_destroy_video_encoder.h"
 #include "media/base/limits.h"
 #include "media/base/media_log.h"
@@ -1099,9 +1098,6 @@ void VideoEncoder::ProcessEncode(Request* request) {
 
   bool mappable = frame->IsMappable() || frame->HasMappableGpuBuffer();
   bool can_handle_shared_image =
-#if BUILDFLAG(IS_WIN)
-      !base::FeatureList::IsEnabled(features::kSkiaGraphite) &&
-#endif  // BUILDFLAG(IS_WIN)
       encoder_info_.DoesSupportGpuSharedImages(frame->format()) &&
       frame->HasSharedImage();
 
