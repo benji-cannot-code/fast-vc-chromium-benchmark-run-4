7f91db47e9a217648f883b2c1eb56e0c60f0f9f3
Antonio Sartori <antoniosartori@chromium.org>
2025-12-02 04:11:35 -0800

    [identifiability] Remove dead code in //chrome/common/privacy_budget
    This is all dead code.
    
    Bug: 451513551
    Change-Id: Ic9b9087c1720dec1edc9b92583340f37cc53d1c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7207196
    Reviewed-by: Judith Hemp <hempjudith@google.com>
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1552733}

--- chrome/renderer/chrome_content_renderer_client_unittest.cc
+++ chrome/renderer/chrome_content_renderer_client_unittest.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/values.h"
 #include "build/build_config.h"
-#include "chrome/common/privacy_budget/scoped_privacy_budget_config.h"
 #include "content/public/common/content_switches.h"
 #include "extensions/buildflags/buildflags.h"
 #include "testing/gtest/include/gtest/gtest.h"
