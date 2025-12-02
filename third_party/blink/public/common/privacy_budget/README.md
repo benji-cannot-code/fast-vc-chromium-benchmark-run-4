6602532b8eced9a18a43add14d5bd3e4ef3066e2
Antonio Sartori <antoniosartori@chromium.org>
2025-12-02 07:18:58 -0800

    Reland "[identifiability] Remove privacy_budget code in blink"
    This is a reland of commit 7e6b7b1db3ae71a1c5cf7f9dcf401597ee4714aa. The header in //third_party/blink/renderer/modules/gamepad/navigator_gamepad.cc has been deleted so that this now compiles correctly.
    
    Original change's description:
    > [identifiability] Remove privacy_budget code in blink
    >
    > This CL removes the infrastructure code of the identifiability study
    > from //third_party/blink.
    >
    > Bug: 451512746
    > Change-Id: I8db0417bd2ca55179d2dbac5ac043c21af900e94
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7208895
    > Reviewed-by: Mike West <mkwst@chromium.org>
    > Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1552736}
    
    Bug: 451512746
    Change-Id: Ib2faa2ae6b0e42ed2251cfb243b662e0d2ba6cf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7213686
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1552819}

--- third_party/blink/public/common/privacy_budget/README.md
+++
@@ -1,7 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-# Privacy Budget: Core Metrics and Aggregation
-
-See [Privacy Budget: Code
-Locations](../../../../../docs/privacy_budget/privacy_budget_code_locations.md) for
-details.
-
--- third_party/blink/renderer/modules/gamepad/navigator_gamepad.cc
+++ third_party/blink/renderer/modules/gamepad/navigator_gamepad.cc
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/gamepad/gamepad_event.h"
 #include "third_party/blink/renderer/modules/gamepad/gamepad_raw_input_change_event.h"
 #include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
-#include "third_party/blink/renderer/platform/privacy_budget/identifiability_digest_helpers.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
 
