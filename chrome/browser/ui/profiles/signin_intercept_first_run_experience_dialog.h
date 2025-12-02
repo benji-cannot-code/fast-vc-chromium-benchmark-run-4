78c3ca2ebea90db9f5cfc73a851ee421b7461a3e
anthie@google.com <anthie@google.com>
2025-12-02 11:08:38 -0800

    Reland "Offer History Sync Optin Interception FRE dialog"
    This reverts commit 3887163abf241d450a493b99546e10d78b3decba.
    
    Reason for revert: Fixed test race conditions by reworking the
    test methods that interact with the history sync screen
    to wait for the flow to complete, by implementing a history
    sync service observer. The implementation was also re-worked
    to use the said service, that facilitates the testing.
    
    Original change's description:
    > Revert "Offer History Sync Optin Interception FRE dialog"
    >
    > This reverts commit 8dbabdb799926285176bea454e4b864827dff956.
    >
    > Reason for revert: culprit for failures on multiple bots
    > - https://ci.chromium.org/ui/p/chromium/builders/ci/Mac12%20Tests/37556/overview
    > - https://ci.chromium.org/ui/p/chromium/builders/ci/mac15-x64-rel-tests/10434/overview
    > - https://ci.chromium.org/ui/p/chromium/builders/ci/Mac13%20Tests/36444/overview
    >
    > Original change's description:
    > > Offer History Sync Optin Interception FRE dialog
    > >
    > > The history sync optin is now the first dialog
    > > offered via the FRE interception dialog.
    > >
    > > Since the dialog already handles accepting automatically
    > > the management and fetching the policies, the dialog
    > > can make use of the history_sync_optin_helper flow
    > > without the danger of displaying the management dialogs
    > > from the management handling steps of the helper.
    > >
    > > Bug: 418143300
    > > Change-Id: I8ebb9f06c173a30ba01941c4bbecd5e9c1d799da
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7132080
    > > Commit-Queue: Anthi Orfanou <anthie@google.com>
    > > Reviewed-by: Alex Ilin <alexilin@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1551472}
    >
    > Bug: 418143300
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: Ibf88a56132f0a521624833fe7f13001166cf2a4c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7210456
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Andrii Natiahlyi <natiahlyi@google.com>
    > Commit-Queue: Andrii Natiahlyi <natiahlyi@google.com>
    > Auto-Submit: Andrii Natiahlyi <natiahlyi@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1551569}
    
    Bug: 418143300
    Change-Id: I80e1a6a2067a73c64a3d4a10ffb691c93b1f27e2
    Include-Ci-Only-Tests: chromium.mac:mac15-x64-rel-tests,chromium.mac:mac-rel|browser_tests
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7209502
    Commit-Queue: Anthi Orfanou <anthie@google.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1552972}

--- chrome/browser/ui/profiles/signin_intercept_first_run_experience_dialog.h
+++ chrome/browser/ui/profiles/signin_intercept_first_run_experience_dialog.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/profiles/profile_customization_synced_theme_waiter.h"
 #include "chrome/browser/ui/signin/signin_modal_dialog.h"
 #include "chrome/browser/ui/signin/signin_view_controller_delegate.h"
+#include "chrome/browser/ui/webui/signin/history_sync_optin_helper.h"
 #include "chrome/browser/ui/webui/signin/profile_customization_handler.h"
 #include "google_apis/gaia/core_account_id.h"
 
@@ -55,7 +56,14 @@ class SigninInterceptFirstRunExperienceDialog
     // The user skipped profile customization.
     kProfileCustomizationClickSkip = 7,
 
-    kMaxValue = kProfileCustomizationClickSkip
+    // History sync optin screen was shown to the user.
+    kShowHistorySyncOptinScreen = 8,
+    // The user turned on history syncing.
+    kHistorySyncOptinAccept = 9,
+    // The user rejected history syncing.
+    kHistorySyncOptinReject = 10,
+
+    kMaxValue = kHistorySyncOptinReject
   };
 
   explicit SigninInterceptFirstRunExperienceDialog(
@@ -78,17 +86,21 @@ class SigninInterceptFirstRunExperienceDialog
   void OnModalDialogClosed() override;
 
  private:
-  // `InterceptTurnSyncOnHelperDelegate` needs access to private methods of
-  // `SigninInterceptFirstRunExperienceDialog`.
   class InterceptTurnSyncOnHelperDelegate;
+  class InterceptHistorySyncOptinHelperDelegate;
   friend class SigninInterceptFirstRunExperienceDialogBrowserTestBase;
 
   // Ordered list of first run steps. Some steps might be skipped but they
   // always appear in this order.
   enum class Step {
     kStart,
+    // Steps relevant for sync confirmation only.
     kTurnOnSync,
     kSyncConfirmation,
+    // Steps relevant for history sync optin screen only.
+    kStartHistorySyncOptin,
+    kShowHistorySyncScreen,
+    // Common steps.
     kWaitForSyncedTheme,
     kProfileCustomization,
     kProfileSwitchIPHAndCloseModal,
@@ -100,6 +112,10 @@ class SigninInterceptFirstRunExperienceDialog
   // Actions executed right after moving to a corresponding step.
   void DoTurnOnSync();
   void DoSyncConfirmation();
+
+  void DoStartHistorySync();
+  void DoShowHistorySyncOptin();
+
   void DoWaitForSyncedTheme();
   void DoProfileCustomization();
   void DoProfileSwitchIPHAndCloseModal();
@@ -122,10 +138,14 @@ class SigninInterceptFirstRunExperienceDialog
                           SigninViewControllerDelegate::Observer>
       dialog_delegate_observation_{this};
 
+  base::WeakPtr<InterceptHistorySyncOptinHelperDelegate>
+      history_sync_optin_delegate_;
+
   std::unique_ptr<content::WebContents>
       profile_customization_preloaded_contents_;
   std::unique_ptr<ProfileCustomizationSyncedThemeWaiter> synced_theme_waiter_;
   base::ScopedClosureRunner auto_accept_management_;
+
   base::WeakPtrFactory<SigninInterceptFirstRunExperienceDialog>
       weak_ptr_factory_{this};
 };
