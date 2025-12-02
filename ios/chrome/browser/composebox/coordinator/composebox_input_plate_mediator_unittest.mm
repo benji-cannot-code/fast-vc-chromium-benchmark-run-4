a6fda628b45a71774e98299d9691e32d6a282275
Aliona DANGLA <alionadangla@google.com>
2025-12-02 05:09:08 -0800

    Reland "[iOS] Update limit and clean attachment when mode change"
    This is a reland of commit c5d1074637c8577a297845cd4df98c77a3a5a0a6
    
    Original change's description:
    > [iOS] Update limit and clean attachment when mode change
    >
    > See video:
    > https://drive.google.com/file/d/1xuQlXoKabcrBsex4pnuGY-4hSETtlGHy/view?usp=drive_link
    >
    > Fixed: 454832175, 464254829
    > Change-Id: I473bd3870005a8b33dba1d80ecd7924e3f8bf46a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7210735
    > Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    > Reviewed-by: Christian Xu <christianxu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1552704}
    
    Change-Id: Ie001ca0b4b7bbb71e5b61969fdff7393a7685821
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7203953
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1552754}

--- ios/chrome/browser/composebox/coordinator/composebox_input_plate_mediator_unittest.mm
+++ ios/chrome/browser/composebox/coordinator/composebox_input_plate_mediator_unittest.mm
@@ -74,6 +74,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 - (void)disableCreateImageActions:(BOOL)disabled {
 }
+- (void)disableCameraActions:(BOOL)disabled {
+}
+- (void)disableGalleryActions:(BOOL)disabled {
+}
 
 @end
 
