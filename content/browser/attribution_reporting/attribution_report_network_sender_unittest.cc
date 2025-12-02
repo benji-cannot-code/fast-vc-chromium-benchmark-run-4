d4608c604211af3bbd7d904e227fda5193104848
Andrew Paseltiner <apaseltiner@chromium.org>
2025-12-02 07:48:46 -0800

    Bump/remove expiring Attribution Reporting metrics
    We extend the extra-report-delay and valid-report metrics, as they may
    still be useful during the deprecation period in order to identify any
    new bugs.
    
    OBSOLETE_HISTOGRAMS=Collected sufficient data and will not be making
    changes here.
    
    Bug: 465125965, 463369744
    Change-Id: Idfad4c38df3a686567b6f02cb1b1ed0a1293884f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7215911
    Auto-Submit: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1552838}

--- content/browser/attribution_reporting/attribution_report_network_sender_unittest.cc
+++ content/browser/attribution_reporting/attribution_report_network_sender_unittest.cc
@@ -515,8 +515,6 @@ TEST_F(AttributionReportNetworkSenderTest,
        EventLevelReportSent_MetricsRecorded) {
   static constexpr char kErrorCodeMetric[] =
       "Conversions.HttpResponseOrNetErrorCodeEventLevel";
-  static constexpr char kReportSizeMetric[] =
-      "Conversions.EventLevelReport.ReportBodySize";
 
   // All OK
   {
@@ -527,7 +525,6 @@ TEST_F(AttributionReportNetworkSenderTest,
     EXPECT_TRUE(test_url_loader_factory_.SimulateResponseForPendingRequest(
         kEventLevelReportUrl, ""));
     histograms.ExpectUniqueSample(kErrorCodeMetric, net::HTTP_OK, 1);
-    histograms.ExpectTotalCount(kReportSizeMetric, 1);
   }
 
   // Internal error
@@ -541,7 +538,6 @@ TEST_F(AttributionReportNetworkSenderTest,
         GURL(kEventLevelReportUrl), completion_status,
         network::mojom::URLResponseHead::New(), ""));
     histograms.ExpectUniqueSample(kErrorCodeMetric, net::ERR_FAILED, 1);
-    histograms.ExpectTotalCount(kReportSizeMetric, 1);
   }
   // External error
   {
@@ -552,7 +548,6 @@ TEST_F(AttributionReportNetworkSenderTest,
     EXPECT_TRUE(test_url_loader_factory_.SimulateResponseForPendingRequest(
         kEventLevelReportUrl, "", net::HTTP_UNAUTHORIZED));
     histograms.ExpectUniqueSample(kErrorCodeMetric, net::HTTP_UNAUTHORIZED, 1);
-    histograms.ExpectTotalCount(kReportSizeMetric, 1);
   }
   // Retried network change error
   {
@@ -568,8 +563,6 @@ TEST_F(AttributionReportNetworkSenderTest,
 
     ASSERT_TRUE(test_url_loader_factory_.SimulateResponseForPendingRequest(
         kEventLevelReportUrl, ""));
-
-    histograms.ExpectTotalCount(kReportSizeMetric, 1);
   }
 }
 
@@ -650,8 +643,6 @@ TEST_F(AttributionReportNetworkSenderTest,
 
   static constexpr char kErrorCodeMetric[] =
       "HttpResponseOrNetErrorCodeAggregatable2";
-  static constexpr char kReportSizeMetric[] =
-      "Conversions.AggregatableReport.ReportBodySize";
 
   for (const bool has_trigger_context_id : {false, true}) {
     SCOPED_TRACE(has_trigger_context_id);
@@ -671,7 +662,6 @@ TEST_F(AttributionReportNetworkSenderTest,
           kAggregatableReportUrl, ""));
       verify_histogram(histograms, kErrorCodeMetric, has_trigger_context_id,
                        net::HTTP_OK, 1);
-      histograms.ExpectTotalCount(kReportSizeMetric, 1);
     }
 
     // Internal error
@@ -686,7 +676,6 @@ TEST_F(AttributionReportNetworkSenderTest,
           network::mojom::URLResponseHead::New(), ""));
       verify_histogram(histograms, kErrorCodeMetric, has_trigger_context_id,
                        net::ERR_FAILED, 1);
-      histograms.ExpectTotalCount(kReportSizeMetric, 1);
     }
     // External error
     {
@@ -698,7 +687,6 @@ TEST_F(AttributionReportNetworkSenderTest,
           kAggregatableReportUrl, "", net::HTTP_UNAUTHORIZED));
       verify_histogram(histograms, kErrorCodeMetric, has_trigger_context_id,
                        net::HTTP_UNAUTHORIZED, 1);
-      histograms.ExpectTotalCount(kReportSizeMetric, 1);
     }
     // Retried network change error
     {
@@ -714,8 +702,6 @@ TEST_F(AttributionReportNetworkSenderTest,
 
       ASSERT_TRUE(test_url_loader_factory_.SimulateResponseForPendingRequest(
           kAggregatableReportUrl, ""));
-
-      histograms.ExpectTotalCount(kReportSizeMetric, 1);
     }
   }
 }
@@ -959,8 +945,6 @@ TEST_F(AttributionReportNetworkSenderTest,
         kAggregatableReportUrl, ""));
     verify_histogram(histograms, "HttpResponseOrNetErrorCodeAggregatable2",
                      has_trigger_context_id, net::HTTP_OK, 1);
-    histograms.ExpectTotalCount("Conversions.AggregatableReport.ReportBodySize",
-                                1);
   }
 }
 
