cdfd853f3b4f853e3530ea9ec67191099903d8d0
Sophie Chang <sophiechang@chromium.org>
2025-12-02 00:18:27 -0800

    Remove observer on PCAWebContentsObserver destruction
    Bug: 465192511
    Change-Id: I18c423cf5797d4738a12ea59abad92070c37d512
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7215033
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Auto-Submit: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Yoshiki Iguchi <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1552629}

--- chrome/browser/page_content_annotations/page_content_annotations_web_contents_observer.cc
+++ chrome/browser/page_content_annotations/page_content_annotations_web_contents_observer.cc
@@ -86,7 +86,10 @@ PageContentAnnotationsWebContentsObserver::GetAnnotatedPageContentRequest() {
 }
 
 PageContentAnnotationsWebContentsObserver::
-    ~PageContentAnnotationsWebContentsObserver() = default;
+    ~PageContentAnnotationsWebContentsObserver() {
+  page_content_annotations_service_->RemoveObserver(
+      AnnotationType::kContentVisibility, this);
+}
 
 void PageContentAnnotationsWebContentsObserver::
     DocumentOnLoadCompletedInPrimaryMainFrame() {
