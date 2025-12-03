191f5ff39386a0aaae329fa0792574192a9814bf
Steinar H. Gunderson <sesse@chromium.org>
2025-12-03 03:38:08 -0800

    Fix inadvertent O(n²) in RuleSet invalidation.
    When invalidating elements after a RuleSet has changed (e.g., because we
    added a new CSS rule), we'd go through the elements in the DOM
    recursively, but our StyleRecalcContext would be created anew for each
    element. This requires traversing all of the element's parents, which
    means that for an element of depth d, it requires going through O(d)
    elements. This means that in a deep DOM, we'd spend a lot of our time
    just creating these contexts.
    
    Instead, make the new context based on the parent's context, like we do
    in normal style recalculation. We factor out this code to avoid
    duplicating it.
    
    ChangeStyleNewRuleInExistingStylesheet gets ~21% faster,
    ChangeStyleUnrelatedAttributeSelector ~11%, others also go up.
    Speedometer3 is, sadly, unimpressed (RuleSet invalidation happens,
    but possibly outside the scoring periods).
    
    Change-Id: Ida245dfd5b8fcb469f932f97faf3370539751105
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7210824
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1553360}

--- third_party/blink/renderer/core/css/style_recalc_context.h
+++ third_party/blink/renderer/core/css/style_recalc_context.h
@@ -45,6 +45,10 @@ class CORE_EXPORT StyleRecalcContext {
       Element& originating_element,
       PseudoId);
 
+  static StyleRecalcContext FromParentContext(
+      const StyleRecalcContext& parent_context,
+      Element& element);
+
  private:
   // Build a StyleRecalcContext suitable for resolving the style for child
   // elements of the passed in element.
