67ed79c531d4906964cdf77e5e0e976e82883792
Thomas Lukaszewicz <tluk@chromium.org>
2025-12-03 12:08:19 -0800

    [bedrock] Add BrowserManagerService::AddBrowserForTesting()
    There are currently several deprecated unit tests under //chrome that
    manually instantiate self-owned browsers via the
    Browser::DeprecatedCreateOwnedForTesting() API.
    
    Recent CLs have introduced the new ProfileBrowserCollection and
    GlobalBrowserCollection abstractions to track application browser
    objects with appropriate scope.
    
    All code will be converted from using BrowserList to instead use
    these classes for Browser-related lookups / operations.
    
    This CL adds support for ProfileBrowserCollection to unit tests that
    instantiate Browsers via the deprecated test-only API. This is done
    by updating BrowserManagerService to track unowned Browser instances
    created by these unit tests. This change is intended to unblock
    BrowserList deprecation without requiring removal of all Browser
    unit tests from the codebase.
    
    Fixes a couple of unit tests incorrectly tearing down the Browser to
    fix the dangling refs.
    
    Bug: 431671320
    Skip-Clang-Tidy-Checks: modernize-use-emplace
    Change-Id: I1f20dfbaabe243cbafaa280cb1f6de54e234fbc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7220850
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1553617}

--- chrome/browser/profiles/profile_activity_metrics_recorder_unittest.cc
+++ chrome/browser/profiles/profile_activity_metrics_recorder_unittest.cc
@@ -16,8 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/metrics/desktop_session_duration/desktop_session_duration_tracker.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_list.h"
-#include "chrome/browser/ui/browser_manager_service.h"
-#include "chrome/browser/ui/browser_manager_service_factory.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/test/base/test_browser_window.h"
 #include "chrome/test/base/testing_browser_process.h"
@@ -64,27 +62,14 @@ class ProfileActivityMetricsRecorderTest : public testing::Test {
     ProfileActivityMetricsRecorder::CleanupForTesting();
     metrics::DesktopSessionDurationTracker::CleanupForTesting();
 
-    for (auto browser_and_profile : browsers_and_profiles_) {
-      // TODO(crbug.com/465101639): tracking and deleting these browsers should
-      // not be necessary, but currently not doing so results in a crash during
-      // profile destruction. This needs to be investigated.
-      BrowserManagerServiceFactory::GetForProfile(browser_and_profile.second)
-          ->DeleteBrowser(browser_and_profile.first);
-    }
   }
 
   void ActivateBrowser(Profile* profile) {
     Browser::CreateParams browser_params(profile, false);
-    std::unique_ptr<Browser> browser(
-        CreateBrowserWithTestWindowForParams(browser_params));
-    Browser* browser_ptr = browser.get();
-    BrowserManagerServiceFactory::GetForProfile(profile)->AddBrowser(
-        std::move(browser));
-
-    browsers_and_profiles_.emplace_back(browser_ptr, profile);
+    browsers_.push_back(CreateBrowserWithTestWindowForParams(browser_params));
 
     // This triggers the recorder to post a task, wait until that's done.
-    browser_ptr->DidBecomeActive();
+    browsers_.back().get()->DidBecomeActive();
     task_environment_.RunUntilIdle();
   }
 
@@ -123,7 +108,7 @@ class ProfileActivityMetricsRecorderTest : public testing::Test {
   TestingProfileManager profile_manager_;
   base::HistogramTester histogram_tester_;
 
-  std::vector<std::pair<Browser*, Profile*>> browsers_and_profiles_;
+  std::vector<std::unique_ptr<Browser>> browsers_;
 };
 
 TEST_F(ProfileActivityMetricsRecorderTest, GuestProfile) {
