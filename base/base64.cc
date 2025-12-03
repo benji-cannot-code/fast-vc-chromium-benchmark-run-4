d1072d360f7a58b196f87c1d1f3ab52da12644ae
Takuto Ikuta <tikuta@chromium.org>
2025-12-03 00:52:13 -0800

    base: Include utility for std::move
    Change-Id: Iae023ddc9ecbe6a1d767a00262bfd389dce2f98d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7202627
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1553288}

--- base/base64.cc
+++ base/base64.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 
 #include <string_view>
+#include <utility>
 
 #include "base/check.h"
 #include "base/numerics/checked_math.h"
