8428134f6ee289cdc30e5543a591fd009d78b447
Jan Keitel <jkeitel@google.com>
2025-12-03 12:46:47 -0800

    Use span-based file API in LocalMediaDataSource
    No functionality change.
    
    Bug: 435317390
    Change-Id: I841de215f84d010d11080f8d4607c4d2c8110a7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7209450
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1553639}

--- chrome/services/media_gallery_util/public/cpp/local_media_data_source_factory.cc
+++ chrome/services/media_gallery_util/public/cpp/local_media_data_source_factory.cc
@@ -5,9 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/services/media_gallery_util/public/cpp/local_media_data_source_factory.h"
 
+#include <optional>
 #include <vector>
 
-#include "base/compiler_specific.h"
+#include "base/containers/span.h"
 #include "base/files/file.h"
 #include "base/files/file_path.h"
 #include "base/functional/bind.h"
@@ -48,16 +49,14 @@ void ReadFile(const base::FilePath& file_path,
   }
 
   auto buffer = std::vector<char>(length);
-  int bytes_read = UNSAFE_TODO(file.Read(position, buffer.data(), length));
-  if (bytes_read == -1) {
+  std::optional<size_t> bytes_read =
+      file.Read(position, base::as_writable_byte_span(buffer));
+  if (!bytes_read) {
     OnReadComplete(main_task_runner, std::move(cb), false /*success*/,
                    std::vector<char>());
     return;
   }
-  DCHECK_GE(bytes_read, 0);
-  if (bytes_read < length)
-    buffer.resize(bytes_read);
-
+  buffer.resize(*bytes_read);
   OnReadComplete(main_task_runner, std::move(cb), true /*success*/,
                  std::move(buffer));
 }
