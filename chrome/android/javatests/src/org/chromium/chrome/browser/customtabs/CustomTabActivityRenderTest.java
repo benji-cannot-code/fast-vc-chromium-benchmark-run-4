c4f603b4bb14251088d645ed17c918f9ad57fc0e
Jinsuk Kim <jinsukkim@chromium.org>
2025-12-03 11:02:38 -0800

    [MtbCct] Disallow open-in-browser button in non-default CCT UI types
    Only CCT of default UI type should show open-in-browser action button
    in accordance with the visibility of the menu item. This CL
    sets the button state using the same condition as in menu property
    delegate (UI type/incognito/FirstRun status), keeping the button/menu
    state in sync.
    
    Bug: 465377262
    Change-Id: I959334197753586fe3782675fba342b5b0074246
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7218128
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1553575}

--- chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabActivityRenderTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabActivityRenderTest.java
@@ -59,6 +59,7 @@ import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.feature_engagement.TrackerFactory;
+import org.chromium.chrome.browser.firstrun.FirstRunStatus;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.test.ChromeJUnit4RunnerDelegate;
 import org.chromium.chrome.test.R;
@@ -139,6 +140,7 @@ public class CustomTabActivityRenderTest {
     public void setUp() {
         mEmbeddedTestServerRule.setServerUsesHttps(mRunWithHttps);
         mEmbeddedTestServerRule.setServerPort(PORT_NO);
+        FirstRunStatus.setFirstRunFlowComplete(true);
         prepareCctIntent();
         // Disable IPH to prevent the highlight showing in the renders.
         when(mTracker.shouldTriggerHelpUi(anyString())).thenReturn(false);
