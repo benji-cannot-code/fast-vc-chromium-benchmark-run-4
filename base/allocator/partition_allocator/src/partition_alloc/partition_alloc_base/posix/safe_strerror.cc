d52a6441fe8fba09459a7ec7a9e876d6c6b4621a
Arthur Sonzogni <arthursonzogni@chromium.org>
2025-12-03 06:10:48 -0800

    Convert to UNSAFE_TODO in partition_alloc in posix
    This is a reland of commit 28c7cb6b99dffd261c254343f151aa2021602866
    https://chromium-review.googlesource.com/c/chromium/src/+/7157555
    
    The diff was adding "Linux ChromiumOS MSan Builder" to the script.  Then
    I splat the patch to ease review and ensure most can ship without
    revert.  It has been split in 12 patches.
    
    Original change's description:
    -----------------------------
    Convert to UNSAFE_TODO in partition_alloc
    
    This is an automated #cleanup patch using the [Script] below.
    
    We are migrating from coarse-grained file-level suppression (#pragma
    allow_unsafe_buffers) to granular, expression-level markers
    (UNSAFE_TODO()). The pragma disables safety checks for an entire file,
    whereas UNSAFE_TODO() isolates specific potentially unsafe operations,
    allowing the rest of the file to be enforced as safe.
    
    This CL was uploaded by git cl split.
    
    Script: https://docs.google.com/document/d/1ORQGBNn2R-CEvNbDTjRd-GrOBOFlCxIHdcvSUA_EhR4/edit?usp=sharing
    AX-Relnotes: N/A
    Cleanup: This is an automated #cleanup.
    Bug: 409340989
    Cq-Include-Trybots: luci.chromium.try:linux_chromium_msan_rel_ng,linux_chromium_chromeos_msan_rel_ng,ios-asan
    Change-Id: Ib9ba9acdf4bfaab0e3919015f364b5777850459e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7198339
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1553404}

--- base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/posix/safe_strerror.cc
+++ base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/posix/safe_strerror.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40284755): Remove this and spanify to fix the errors.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "partition_alloc/partition_alloc_base/posix/safe_strerror.h"
 
 #include <cerrno>
@@ -15,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstring>
 
 #include "partition_alloc/build_config.h"
+#include "partition_alloc/partition_alloc_base/compiler_specific.h"
 
 namespace partition_alloc::internal::base {
 
@@ -44,7 +40,7 @@ namespace partition_alloc::internal::base {
     // glibc did not use buf and returned a static string instead. Copy it
     // into buf.
     buf[0] = '\0';
-    strncat(buf, rc, len - 1);
+    PA_UNSAFE_TODO(strncat(buf, rc, len - 1));
   }
   // The GNU version never fails. Unknown errors get an "unknown error" message.
   // The result is always null terminated.
@@ -73,7 +69,7 @@ namespace partition_alloc::internal::base {
     // it indirectly implies that typically ERANGE will be returned, instead
     // of truncating the string. We play it safe by always terminating the
     // string explicitly.
-    buf[len - 1] = '\0';
+    PA_UNSAFE_TODO(buf[len - 1]) = '\0';
   } else {
     // Error. POSIX is vague about whether the return value is itself a system
     // error code or something else. On Linux currently it is -1 and errno is
@@ -92,8 +88,8 @@ namespace partition_alloc::internal::base {
       strerror_error = result;
     }
     // snprintf truncates and always null-terminates.
-    snprintf(buf, len, "Error %d while retrieving error %d", strerror_error,
-             err);
+    PA_UNSAFE_TODO(snprintf(buf, len, "Error %d while retrieving error %d",
+                            strerror_error, err));
   }
   errno = old_errno;
 }
