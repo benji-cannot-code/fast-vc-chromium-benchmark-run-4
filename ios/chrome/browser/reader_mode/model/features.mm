e8ec48f0e0caac7522ce86db547079a0708293ff
Nohemi Fernandez <fernandex@google.com>
2025-12-03 04:32:21 -0800

    [iOS] Support badges in the omnibox for Reading Mode.
    Screen recording: crbug.com/463917509#comment5
    
    Bug: 463917509
    Change-Id: I7b9e1084441f05e77ae8415894ee127ae0f4601b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7209116
    Reviewed-by: Joemer Ramos <joemerramos@google.com>
    Commit-Queue: Nohemi Fernandez <fernandex@google.com>
    Reviewed-by: Quentin Pubert <qpubert@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1553373}

--- ios/chrome/browser/reader_mode/model/features.mm
+++ ios/chrome/browser/reader_mode/model/features.mm
@@ -45,6 +45,8 @@ BASE_FEATURE(kEnableReaderModePageEligibilityForToolsMenu,
 BASE_FEATURE(kEnableReaderModeOptimizationGuideEligibility,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kEnableReaderModeBadgeSupport, base::FEATURE_DISABLED_BY_DEFAULT);
+
 const char kReaderModeHeuristicPageLoadDelayDurationStringName[] =
     "reader-mode-heuristic-page-load-delay-duration-string";
 
@@ -89,4 +91,8 @@ bool IsReaderModeTranslationAvailable() {
 bool IsReaderModeOptimizationGuideEligibilityAvailable() {
   return base::FeatureList::IsEnabled(
       kEnableReaderModeOptimizationGuideEligibility);
+}
+
+bool IsReaderModeBadgeSupportEnabled() {
+  return base::FeatureList::IsEnabled(kEnableReaderModeBadgeSupport);
 }
