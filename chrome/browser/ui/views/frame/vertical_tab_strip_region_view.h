005f8d8ff7d8e41311b896482d8a21a962b6e279
Kunal Daftari <kunaldaftari@google.com>
2025-12-03 11:14:22 -0800

    [Vertical Tabs] Tab Context Menu Text
    This CL ensures that the text of the Tab Context Menu updates according
    to the current state of the window. If vertical tabs is enabled, then we
    should populate "Show Tabs Below" or "Close Tabs Below".
    
    Bug: 462464656
    Change-Id: I270f5ceeda3dc683db92b5c11f42d085f1e3eb48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7205507
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Kunal Daftari <kunaldaftari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1553581}

--- chrome/browser/ui/views/frame/vertical_tab_strip_region_view.h
+++ chrome/browser/ui/views/frame/vertical_tab_strip_region_view.h
@@ -73,6 +73,10 @@ class VerticalTabStripRegionView final : public views::AccessiblePaneView,
     return bottom_button_container_;
   }
 
+  VerticalTabStripController* GetVerticalTabStripController() {
+    return tab_strip_controller_.get();
+  }
+
   // views::View:
   void Layout(PassKey) override;
 
