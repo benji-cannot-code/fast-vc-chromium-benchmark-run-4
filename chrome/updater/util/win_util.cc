1e74c9ea168e44502db1d65c48d46e527bf6025c
S. Ganesh <ganesh@chromium.org>
2025-12-03 12:00:30 -0800

    updater: implement IProcessLauncher::LaunchCmdLine
    `IProcessLauncher::LaunchCmdLine` is being used by chrome, so it needs
    to be implemented.
    
    Bug: 465095657
    Change-Id: Ib4dc96f9c616ababde187155997e669ecca743a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7219999
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1553608}

--- chrome/updater/util/win_util.cc
+++ chrome/updater/util/win_util.cc
@@ -1441,16 +1441,6 @@ std::vector<DWORD> FindProcessesInSession(const std::wstring& process_name,
   return pids;
 }
 
-// Returns the first instance found of explorer.exe.
-std::optional<DWORD> GetExplorerPid() {
-  std::vector<DWORD> pids =
-      FindProcessesInSession(L"EXPLORER.EXE", GetActiveSessionId());
-  if (pids.empty()) {
-    return {};
-  }
-  return pids[0];
-}
-
 // Returns an impersonation token for the user running process_id.
 HResultOr<ScopedKernelHANDLE> GetImpersonationToken(
     std::optional<DWORD> process_id) {
@@ -1479,6 +1469,15 @@ HResultOr<ScopedKernelHANDLE> GetImpersonationToken(
 }
 
 }  // namespace
+
+std::optional<DWORD> GetExplorerPid() {
+  std::vector<DWORD> pids =
+      FindProcessesInSession(L"EXPLORER.EXE", GetActiveSessionId());
+  if (pids.empty()) {
+    return {};
+  }
+  return pids[0];
+}
 
 HResultOr<ScopedKernelHANDLE> GetLoggedOnUserToken() {
   return GetImpersonationToken(GetExplorerPid());
