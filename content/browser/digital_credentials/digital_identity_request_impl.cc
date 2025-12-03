bf38f45afb5c892ff87fe08b96ee1e8ecfbaa370
David Sanders <dsanders11@ucsbalum.com>
2025-12-03 13:53:47 -0800

    Remove unused includes in policy_container_host.h
    Should save ~75 MiB of compiler input size.
    
    Adds missing includes as needed to fix the build.
    
    Bug: 40318405
    Change-Id: Ie5b078eed19683cb6d80eaa1c18012c59ce9f091
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7220001
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#1553677}

--- content/browser/digital_credentials/digital_identity_request_impl.cc
+++ content/browser/digital_credentials/digital_identity_request_impl.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/barrier_callback.h"
 #include "base/base64url.h"
+#include "base/command_line.h"
 #include "base/containers/flat_set.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
