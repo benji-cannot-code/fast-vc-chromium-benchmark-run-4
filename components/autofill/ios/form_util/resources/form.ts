b6f11491e8646f8f2226d13309a26f953ff1c5cd
Alexandra Pereira <alexandrasp@google.com>
2025-12-03 06:22:13 -0800

    [iOS][autofill] Move getFieldName from gCrWebLegacy
    Converts the getFieldName function into a utility function and removes
    it from gCrWebLegacy. No functional change is intended.
    
    Bug: 464542835
    Change-Id: Ifd80d4a48bfc45b891af82eca820750d1e6e6584
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7216877
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Alexandra Pereira <alexandrasp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1553414}

--- components/autofill/ios/form_util/resources/form.ts
+++ components/autofill/ios/form_util/resources/form.ts
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {getRemoteFrameToken} from '//components/autofill/ios/form_util/resources/fill_util.js';
 import {getFormIdentifier} from '//components/autofill/ios/form_util/resources/form_utils.js';
 import {gCrWeb, gCrWebLegacy} from '//ios/web/public/js_messaging/resources/gcrweb.js';
-import {sendWebKitMessage, trim} from '//ios/web/public/js_messaging/resources/utils.js';
+import {sendWebKitMessage} from '//ios/web/public/js_messaging/resources/utils.js';
 
 /**
  * A WeakMap to track if the current value of a field was entered by user or
@@ -165,39 +165,6 @@ class FormSubmissionReportManager {
 const gFormSubmissionReportManager = new FormSubmissionReportManager();
 
 /**
- * Returns the field's `name` attribute if not space only; otherwise the
- * field's `id` attribute.
- *
- * The name will be used as a hint to infer the autofill type of the field.
- *
- * It aims to provide the logic in
- *     WebString nameForAutofill() const;
- * in chromium/src/third_party/WebKit/Source/WebKit/chromium/public/
- *  WebFormControlElement.h
- *
- * @param element An element of which the name for Autofill will be returned.
- * @return the name for Autofill.
- */
-function getFieldName(element: Element|null): string {
-  if (!element) {
-    return '';
-  }
-
-  if ('name' in element && element.name) {
-    const trimmedName = trim(element.name as string);
-    if (trimmedName.length > 0) {
-      return trimmedName;
-    }
-  }
-
-  if (element.id) {
-    return trim(element.id);
-  }
-
-  return '';
-}
-
-/**
  * Returns whether the last `input` or `change` event on `element` was
  * triggered by a user action (was "trusted"). Returns true by default if the
  * feature to fix the user edited bit isn't enabled which is the status quo.
@@ -334,11 +301,8 @@ function reportDetectedFormSubmission(
   }
 }
 
-
-
 gCrWebLegacy.form = {
   wasEditedByUser,
-  getFieldName,
   fieldWasEditedByUser,
   formSubmitted,
   reportFormSubmissionError,
