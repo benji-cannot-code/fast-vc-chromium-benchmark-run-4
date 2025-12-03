433ca6b0a73b19459caeade34ca714e80b2745b1
Mitsuru Oshima <oshima@chromium.org>
2025-12-02 18:41:27 -0800

    Do not clamp to the empty bounds when the empty window becomes target
    This is temporary fix to find why an empty window is selected as
    target.
    
    This also dumps the stack when this happens with the error log
    so that we can invesgitate when this happens.
    I'll add a test for the right condition when we figure it out.
    
    Bug: 462446075
    Change-Id: I2230cfa9c3b37027d0c0b575af124007b638a7a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7212404
    Reviewed-by: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1553204}

--- ash/root_window_controller.cc
+++ ash/root_window_controller.cc
@@ -80,6 +80,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/workspace/workspace_layout_manager.h"
 #include "ash/wm/workspace_controller.h"
 #include "base/command_line.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/functional/bind.h"
 #include "base/memory/raw_ptr.h"
 #include "base/metrics/histogram_macros.h"
@@ -370,8 +371,18 @@ class RootWindowTargeter : public aura::WindowTargeter {
       // adjust the location.
       bool bounded_click = ShouldConstrainMouseClick(event, has_capture_target);
       if (!has_capture_target || bounded_click) {
+        // TODO(crbug.com/462446075) : Remove this once the empty target window
+        // is identified.
+        if (window->bounds().IsEmpty()) {
+          LOG(ERROR) << " Window with empty bounds is target:"
+                     << window->GetName();
+          base::debug::DumpWithoutCrashing();
+        }
+        const gfx::Rect& window_bounds = window->bounds();
         gfx::Point new_location =
-            FitPointToBounds(event->location(), window->bounds());
+            window_bounds.IsEmpty()
+                ? event->location()
+                : FitPointToBounds(event->location(), window->bounds());
         // Do not change |location_f|. It's used to compute pixel position and
         // such client should know what they're doing.
         event->set_location(new_location);
