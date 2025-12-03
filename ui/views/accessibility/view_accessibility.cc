b04035a1a2918361dd6dc04187be6b0be8290147
Benjamin Beaudry <benjamin.beaudry@microsoft.com>
2025-12-03 13:38:27 -0800

    [views-ax] Implement ViewAccessibility::GetNativeObject
    This CL implements ViewAccessibility::GetNativeObject. Previously,
    this was only overridden in ViewAXPlatformNodeDelegate and AXVirtualView and would return the associated AXPlatformNode. Now, when ViewsAX is enabled, we’ll return the platform node created from the serialization of that ViewAccessibility object, queried from the WidgetAXManager.
    
    Bug: 40672241
    Change-Id: I86c16c4598965c6b08578a076fec14fa87c6efb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7224310
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Reviewed-by: Jacques Newman <janewman@microsoft.com>
    Auto-Submit: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Commit-Queue: Jacques Newman <janewman@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1553666}

--- ui/views/accessibility/view_accessibility.cc
+++ ui/views/accessibility/view_accessibility.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/accessibility/atomic_view_ax_tree_manager.h"
 #include "ui/views/accessibility/ax_update_notifier.h"
 #include "ui/views/accessibility/ax_virtual_view.h"
+#include "ui/views/accessibility/tree/widget_ax_manager.h"
 #include "ui/views/view.h"
 #include "ui/views/view_utils.h"
 #include "ui/views/widget/root_view.h"
@@ -1449,7 +1450,20 @@ void ViewAccessibility::SetChildTreeScaleFactor(float scale_factor) {
 }
 
 gfx::NativeViewAccessible ViewAccessibility::GetNativeObject() const {
-  return gfx::NativeViewAccessible();
+  if (!::features::IsAccessibilityTreeForViewsEnabled()) {
+    return gfx::NativeViewAccessible();
+  }
+
+  // TODO(crbug.com/40672241): Investigate whether this function should
+  // be removed altogether. Does it still make sense to let Views
+  // access the native accessible object directly with ViewsAX, or should the
+  // Views and Accessibility layer be completely separate?
+  Widget* widget = GetWidget();
+  if (!widget || !widget->ax_manager()) {
+    return gfx::NativeViewAccessible();
+  }
+
+  return widget->ax_manager()->GetNativeViewAccessibleForId(GetUniqueId());
 }
 
 void ViewAccessibility::AnnounceAlert(std::u16string_view text) {
