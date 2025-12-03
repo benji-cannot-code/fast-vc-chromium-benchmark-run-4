ed3bc05b8035b3d5e278c90f7023c520121b133c
Bryant Chandler <bryantchandler@google.com>
2025-12-03 14:54:19 -0800

    Reland "[GlicMI] Do warming on contents instead of instances"
    This is a reland of commit dd6120a16f971d2bacc6932519dc3975dc40f6e5
    
    Added unsubscription for WebContentsObserver
    
    Original change's description:
    > [GlicMI] Do warming on contents instead of instances
    >
    > This moves warming to the web contents. It has a few architectural benefits,
    > but one big thing is that it allows more aggressive hibernation.
    >
    > Bug: b:454120908
    > Change-Id: If5269c584ad52314e56b15b847fa6bc26a6a6964
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7129359
    > Reviewed-by: Fred Shih <ffred@chromium.org>
    > Reviewed-by: Dan Harrington <harringtond@chromium.org>
    > Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1553163}
    
    Bug: b:454120908
    Change-Id: Idbacfa41c0ff099c872b7961b11929908bf1b138
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7224591
    Reviewed-by: Fred Shih <ffred@chromium.org>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1553713}

--- chrome/browser/glic/public/glic_keyed_service.cc
+++ chrome/browser/glic/public/glic_keyed_service.cc
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/glic/host/glic.mojom.h"
 #include "chrome/browser/glic/host/glic_region_capture_controller.h"
 #include "chrome/browser/glic/host/glic_web_client_access.h"
+#include "chrome/browser/glic/host/glic_web_contents_warming_pool.h"
 #include "chrome/browser/glic/host/host.h"
 #include "chrome/browser/glic/host/webui_contents_container.h"
 #include "chrome/browser/glic/public/glic_enabling.h"
@@ -175,6 +176,8 @@ GlicKeyedService::GlicKeyedService(
       actor_task_manager_(
           std::make_unique<GlicActorTaskManager>(profile, actor_keyed_service)),
       tab_data_observer_(std::make_unique<GlicTabDataObserver>()),
+      web_contents_warming_pool_(
+          std::make_unique<GlicWebContentsWarmingPool>(profile)),
       contextual_cueing_service_(contextual_cueing_service) {
   CHECK(GlicEnabling::IsProfileEligible(Profile::FromBrowserContext(profile)));
   CHECK(actor_keyed_service);
@@ -241,6 +244,7 @@ void GlicKeyedService::Shutdown() {
   } else {
     CloseAndShutdown();
   }
+  web_contents_warming_pool_->Shutdown();
 
   GlicProfileManager* glic_profile_manager = GlicProfileManager::GetInstance();
   if (glic_profile_manager) {
@@ -680,7 +684,11 @@ void GlicKeyedService::FinishPreload(GlicPrewarmingChecksResult result) {
     return;
   }
 
-  window_controller().Preload();
+  if (base::FeatureList::IsEnabled(features::kGlicWebContentsWarming)) {
+    web_contents_warming_pool_->Preload();
+  } else {
+    window_controller().Preload();
+  }
 }
 
 void GlicKeyedService::FinishPreloadFre(GlicPrewarmingFreSource source,
