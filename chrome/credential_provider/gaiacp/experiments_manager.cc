e2f639491c4f57aced5ca315a133589fb4621c2c
Jan Keitel <jkeitel@google.com>
2025-12-03 00:05:34 -0800

    Resolve UNSAFE_TODO in experiments_manager.cc
    No functionality changes.
    
    Bug: 435317390
    Change-Id: If7399026cae410266f86524721fe63e28f7e8fbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7185647
    Reviewed-by: Yusuf Sengul <yusufsn@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Rakesh Soma <rakeshsoma@google.com>
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1553275}

--- chrome/credential_provider/gaiacp/experiments_manager.cc
+++ chrome/credential_provider/gaiacp/experiments_manager.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file.h"
 #include "base/json/json_reader.h"
 #include "base/strings/string_util.h"
+#include "base/strings/string_view_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/credential_provider/gaiacp/gcp_utils.h"
 #include "chrome/credential_provider/gaiacp/logging.h"
@@ -70,12 +71,12 @@ bool ExperimentsManager::ReloadExperiments(const std::wstring& sid) {
     return false;
   }
 
-  std::vector<char> buffer(experiments_file->GetLength());
-  UNSAFE_TODO(experiments_file->Read(0, buffer.data(), buffer.size()));
+  std::vector<uint8_t> buffer(experiments_file->GetLength());
+  experiments_file->Read(0, buffer);
   experiments_file.reset();
 
   std::optional<base::Value::Dict> experiments_data =
-      base::JSONReader::ReadDict(std::string_view(buffer.data(), buffer.size()),
+      base::JSONReader::ReadDict(base::as_string_view(buffer),
                                  base::JSON_ALLOW_TRAILING_COMMAS);
   if (!experiments_data) {
     LOGFN(ERROR) << "Failed to read experiments data from file!";
