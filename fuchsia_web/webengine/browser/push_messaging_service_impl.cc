ade7a20712c2bc602fcc09c33fcac47d32f3d59b
zijiehe-google-com <zijiehe@google.com>
2025-12-03 13:09:03 -0800

    [fuchsia] Replace PushMessagingServiceImpl/IsPermissionGranted with IsProtectedServiceWorker
    Bug: 424479300
    Change-Id: I9aeaa00ab3be2c95c6a05492a800dbe30d3fe11d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7224282
    Commit-Queue: Bailey Myers-Morgan <bmyersmorgan@google.com>
    Reviewed-by: Bailey Myers-Morgan <bmyersmorgan@google.com>
    Auto-Submit: Zijie He <zijiehe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1553654}

--- fuchsia_web/webengine/browser/push_messaging_service_impl.cc
+++ fuchsia_web/webengine/browser/push_messaging_service_impl.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/storage_partition.h"
+#include "fuchsia_web/webengine/browser/web_engine_config.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "services/network/public/mojom/network_context.mojom.h"
 #include "third_party/blink/public/common/permissions/permission_utils.h"
@@ -44,13 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 
 namespace {
-
-bool IsPermissionGranted(const GURL& origin) {
-  // Very likely this should be a command line flag based solution.
-  // TODO(crbug.com/424479300): Implement the permission control of using the
-  // push-messaging api.
-  return true;
-}
 
 void SubscriptionError(content::PushMessagingService::RegisterCallback callback,
                        blink::mojom::PushRegistrationStatus status) {
@@ -338,7 +332,7 @@ void PushMessagingServiceImpl::DoSubscribe(
   // system, web engine would use a different way of managing the permissions
   // directly from the |origin| and the subscriptions from document
   // and service worker are treated the same.
-  if (!IsPermissionGranted(origin)) {
+  if (!IsProtectedServiceWorker(origin)) {
     SubscriptionError(std::move(callback),
                       blink::mojom::PushRegistrationStatus::PERMISSION_DENIED);
     return;
