1111a1fa3453cf57fb0a161f285f1e4638909421
Martin Kong <martinkong@google.com>
2025-12-03 11:45:13 -0800

    Roll Error Prone from 2NlHzICEPBy3pqjkW... to QcxY40Xfv3er_kuOu...
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://skia-autoroll.corp.goog/r/error-prone-chromium
    Please CC agrieve@google.com,clank-build@google.com,clank-library-failures@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in Chromium: https://bugs.chromium.org/p/chromium/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Cq-Include-Trybots: luci.chrome.try:android-internal-dbg
    Tbr: clank-library-failures@google.com
    Change-Id: I5e0af2d184947cf2c486df64d737d558897fd1d6
    Bug: 465507149
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7207100
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Martin Kong <martinkong@google.com>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1553598}

--- content/public/android/junit/src/org/chromium/content/browser/selection/SelectionPopupControllerTest.java
+++ content/public/android/junit/src/org/chromium/content/browser/selection/SelectionPopupControllerTest.java
@@ -908,6 +908,7 @@ public class SelectionPopupControllerTest {
     }
 
     @Test
+    @SuppressWarnings("AssertSameIncompatible")
     public void testNewMenuIsProcessedForDifferentSelectionState() {
         Assert.assertNull(mController.getSelectionMenuCachedResultForTesting());
 
