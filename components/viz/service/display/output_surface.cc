7e6955bb231a036d0e9fcf9aa977887574943daa
Niklas Wenzel <dev@nikwen.de>
2025-12-02 17:53:11 -0800

    viz: Create IsBufferQueueSupportedAndEnabled()
    This is in preparation for crrev.com/c/7115438.
    
    In that CL, we need to access this logic from `SkiaOutputDeviceDComp`.
    
    Bug: 457463689
    Change-Id: I31bbaa6b5d79697c6bb5e1fc6738f6ea5a937b4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7210913
    Reviewed-by: Michael Tang <tangm@microsoft.com>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1553190}

--- components/viz/service/display/output_surface.cc
+++ components/viz/service/display/output_surface.cc
@@ -22,6 +22,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace viz {
 
+namespace {
+
+#if BUILDFLAG(IS_WIN)
+// Use BufferQueue for the primary plane instead of a DXGI swap chain or DComp
+// surface.
+BASE_FEATURE(kBufferQueue, base::FEATURE_DISABLED_BY_DEFAULT);
+#endif
+
+}  // namespace
+
 OutputSurface::Capabilities::Capabilities() = default;
 OutputSurface::Capabilities::~Capabilities() = default;
 OutputSurface::Capabilities::Capabilities(const Capabilities& capabilities) =
@@ -94,6 +104,12 @@ bool IsDelegatedCompositingSupportedAndEnabled(
 
   // Ensure we check the feature flag iff the feature is supported.
   return features::IsDelegatedCompositingEnabled();
+}
+
+bool IsBufferQueueSupportedAndEnabled(
+    OutputSurface::DCSupportLevel support_level) {
+  return support_level >= OutputSurface::DCSupportLevel::kDCompDynamicTexture &&
+         base::FeatureList::IsEnabled(kBufferQueue);
 }
 #endif
 
