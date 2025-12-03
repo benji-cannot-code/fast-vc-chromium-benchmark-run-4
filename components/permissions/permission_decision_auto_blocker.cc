b989552ff728b99ab37ed4658bf08b2a62672adc
Elias Klim <elklm@chromium.org>
2025-12-03 00:35:49 -0800

    [ClapperLoud] Fix the reset permissions button in PageInfo
    This CL resolves a permission request as Dismissed if the reset
    permission button is clicked while a permission request is displayed in
    PageInfo. Additionally it adjusts the ignore count for permission
    auto-blocking on Android Clapper Loud.
    
    When the PermissionsAndroidClapperLoud feature is enabled on Android,
    the number of ignored permission prompts before an origin is
    automatically blocked is reduced from 4 to 1.
    
    The Reset permissions button style is globally changed from FilledButton
    to TextButton.
    
    Bug: 465381373
    Change-Id: I0de8b188d5e1f053a13830936df5e5048a9c3cf9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7214370
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Judith Hemp <hempjudith@google.com>
    Commit-Queue: Elias Klim <elklm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1553282}

--- components/permissions/permission_decision_auto_blocker.cc
+++ components/permissions/permission_decision_auto_blocker.cc
@@ -17,6 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "components/permissions/features.h"
+#if BUILDFLAG(IS_ANDROID)
+#include "components/permissions/android/permissions_android_feature_map.h"
+#endif
 #include "components/permissions/permission_util.h"
 #include "url/gurl.h"
 
@@ -32,6 +35,12 @@ constexpr int kDefaultDismissalsBeforeBlock = 3;
 // The number of times that users may ignore a permission prompt from an origin
 // before it is automatically blocked.
 constexpr int kDefaultIgnoresBeforeBlock = 4;
+
+#if BUILDFLAG(IS_ANDROID)
+// The number of times that users may ignore a permission prompt from an origin.
+// before it is automatically blocked. This is used for the Clapper UI.
+constexpr int kClapperIgnoresBeforeBlock = 1;
+#endif
 
 // The number of times that users may dismiss a permission prompt that uses the
 // quiet UI from an origin before it is automatically blocked.
@@ -428,7 +437,14 @@ bool PermissionDecisionAutoBlocker::RecordIgnoreAndEmbargo(
                                           settings_map_)
           : -1;
 
-  if (current_ignore_count >= kDefaultIgnoresBeforeBlock) {
+  int ignores_before_block = kDefaultIgnoresBeforeBlock;
+#if BUILDFLAG(IS_ANDROID)
+  if (base::FeatureList::IsEnabled(permissions::kPermissionsAndroidClapperLoud)) {
+    ignores_before_block = kClapperIgnoresBeforeBlock;
+  }
+#endif
+
+  if (current_ignore_count >= ignores_before_block) {
     PlaceUnderEmbargo(url, permission, kPermissionIgnoreEmbargoKey);
     return true;
   }
