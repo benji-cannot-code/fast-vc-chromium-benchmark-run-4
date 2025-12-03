66852f7c63a155792f51eb8591041b71c3b2844e
Sylvain Defresne <sdefresne@chromium.org>
2025-12-03 05:24:24 -0800

    [ios] Remove indirections in BrowserViewVisibilityNotifierBrowserAgent
    Remove BrowserViewVisibilityAudience and BrowserViewVisibilityHandler
    which introduces two indirections (the protocol and the callback in the
    implementation) in how BrowserViewController informs the browser agent
    of the visibility change of the Browser.
    
    Instead inject a callback into BrowserViewController directly from the
    BrowserController if the BrowserViewVisibilityNotifierBrowserAgent is
    not null.
    
    Rename the method BrowserViewVisibilityStateDidChange(...) to
    NotifyBrowserViewVisibilityStateDidChange(...).
    
    Bug: none
    
    
    [ios] Remove two indirection in BrowserViewVisibilityNotifierBrowserAgent
    
    Remove BrowserViewVisibilityAudience and BrowserViewVisibilityHandler
    which introduces two indirections (the protocol and the callback in the
    implementation) in how BrowserViewController informs the browser agent
    of the visibility change of the Browser.
    
    Bug: none
    Change-Id: I27b4425e51708d97ec3a91789c44e5c299172b03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7207677
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1553388}

--- ios/chrome/browser/browser_view/model/browser_view_visibility_notifier_browser_agent.mm
+++ ios/chrome/browser/browser_view/model/browser_view_visibility_notifier_browser_agent.mm
@@ -6,36 +6,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/browser_view/model/browser_view_visibility_notifier_browser_agent.h"
 
 #import "base/functional/bind.h"
-#import "ios/chrome/browser/browser_view/model/browser_view_visibility_handler.h"
+#import "base/functional/callback.h"
 
 BrowserViewVisibilityNotifierBrowserAgent::
     BrowserViewVisibilityNotifierBrowserAgent(Browser* browser)
-    : BrowserUserData(browser) {
-  BrowserViewVisibilityStateChangeCallback callback =
-      base::BindRepeating(&BrowserViewVisibilityNotifierBrowserAgent::
-                              BrowserViewVisibilityStateDidChange,
-                          AsWeakPtr());
-  visibility_handler_ = [[BrowserViewVisibilityHandler alloc]
-      initWithVisibilityChangeCallback:callback];
-}
+    : BrowserUserData(browser) {}
 
 BrowserViewVisibilityNotifierBrowserAgent::
     ~BrowserViewVisibilityNotifierBrowserAgent() = default;
 
 base::CallbackListSubscription BrowserViewVisibilityNotifierBrowserAgent::
     RegisterBrowserVisibilityStateChangedCallback(
-        const VisibilityChangedCallback& callback) {
+        const BrowserViewVisibilityStateChangedCallback& callback) {
   return callbacks_.Add(callback);
 }
 
-id<BrowserViewVisibilityAudience>
-BrowserViewVisibilityNotifierBrowserAgent::GetBrowserViewVisibilityAudience() {
-  return visibility_handler_;
-}
-
-base::WeakPtr<BrowserViewVisibilityNotifierBrowserAgent>
-BrowserViewVisibilityNotifierBrowserAgent::AsWeakPtr() {
-  return weak_ptr_factory_.GetWeakPtr();
+BrowserViewVisibilityStateChangedCallback
+BrowserViewVisibilityNotifierBrowserAgent::GetNotificationCallback() {
+  using Self = BrowserViewVisibilityNotifierBrowserAgent;
+  return base::BindRepeating(&Self::BrowserViewVisibilityStateDidChange,
+                             weak_ptr_factory_.GetWeakPtr());
 }
 
 void BrowserViewVisibilityNotifierBrowserAgent::
--- ios/chrome/browser/browser_view/ui_bundled/browser_view_controller.h
+++ ios/chrome/browser/browser_view/ui_bundled/browser_view_controller.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/ios/block_types.h"
 #import "base/memory/raw_ptr.h"
 #import "base/memory/weak_ptr.h"
+#import "ios/chrome/browser/browser_view/public/browser_view_visibility_state_changed_callback.h"
 #import "ios/chrome/browser/browser_view/ui_bundled/tab_consumer.h"
 #import "ios/chrome/browser/contextual_panel/coordinator/contextual_sheet_presenter.h"
 #import "ios/chrome/browser/incognito_reauth/ui_bundled/incognito_reauth_consumer.h"
@@ -128,6 +129,10 @@ typedef struct {
 
 // Command handler for omnibox commands.
 @property(nonatomic, weak) id<OmniboxCommands> omniboxCommandsHandler;
+
+// Callback that will be invoked when the browser view visibility changed.
+@property(nonatomic, assign) const BrowserViewVisibilityStateChangedCallback&
+    browserViewVisibilityStateChangedCallback;
 
 // Opens a new tab as if originating from `originPoint` and `focusOmnibox`.
 - (void)openNewTabFromOriginPoint:(CGPoint)originPoint
