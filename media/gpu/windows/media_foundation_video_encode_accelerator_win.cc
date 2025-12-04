e379d8ed17c60726bf7a92215473d7c53c5c6a73
Colin Blundell <blundell@chromium.org>
2025-12-04 00:19:27 -0800

    [//media] Eliminate VideoFrame::IsMappableSharedImageEnabled()
    Have all clients directly call HasMappableGpuBuffer().
    
    Bug: 404905709
    Change-Id: I9a7802edd142886cf472bb62a671302c37b288dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7222630
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1553854}

--- media/gpu/windows/media_foundation_video_encode_accelerator_win.cc
+++ media/gpu/windows/media_foundation_video_encode_accelerator_win.cc
@@ -740,7 +740,7 @@ void MediaFoundationVideoEncodeAccelerator::QueueInput(
 
   result.generate_sample_on_wait_sync_token =
       command_buffer_helper_ && !frame->HasNativeGpuMemoryBuffer() &&
-      !frame->IsMappableSharedImageEnabled() && frame->HasSharedImage();
+      !frame->HasMappableGpuBuffer() && frame->HasSharedImage();
   if (result.generate_sample_on_wait_sync_token) {
     TRACE_EVENT0("media",
                  "MediaFoundationVideoEncodeAccelerator::"
