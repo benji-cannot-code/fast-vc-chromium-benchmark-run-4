b95fd9702eb4d2dbcd2633fe4b2d88aaa09b9a52
Joshua Hood <jdh@chromium.org>
2025-12-04 06:42:14 -0800

    [Bundled Security Settings] Add Security Keys and Certificates sections
    This CL adds "Manage security keys" and "Manage Certificates" rows
    to the Bundled Security Settings version of the
    chrome://settings/security page.
    
    Screenshot: crbug.com/460195217#comment9
    
    Bug: b:460195217
    Change-Id: Iedb354ba97fddf83d07d08c09631fdf74e6a349c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7205565
    Reviewed-by: Nicola Tommasi <tommasin@chromium.org>
    Commit-Queue: Joshua Hood <jdh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1553967}

--- chrome/test/data/webui/settings/settings_browsertest.cc
+++ chrome/test/data/webui/settings/settings_browsertest.cc
@@ -1592,6 +1592,12 @@ IN_PROC_BROWSER_TEST_F(SettingsSecurityPageV2Test, Main) {
 }
 
 IN_PROC_BROWSER_TEST_F(SettingsSecurityPageV2Test,
+                       SecurityKeysSubpageDisabled) {
+  RunTest("settings/security_page_v2_test.js",
+          "runMochaSuite('SecurityKeysSubpageDisabled')");
+}
+
+IN_PROC_BROWSER_TEST_F(SettingsSecurityPageV2Test,
                        SecurityPageHappinessTrackingSurveys) {
   RunTest("settings/security_page_v2_test.js",
           "runMochaSuite('SecurityPageV2HappinessTrackingSurveys')");
