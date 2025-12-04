b7826b0d6672f02cbb61929ce459a978dfe5d72e
yuyisi <hellofroser@gmail.com>
2025-12-04 10:11:29 -0800

    FIXUP: WinWindow doesn't handle WM_SETCURSOR
    Aura’s `WinWindow`, which is used in `aura_demo`, doesn’t handle Cursor
    events. This causes the cursor not to change even if
    WinWindow::SetCursor() is called. It should update the cursor after
    receiving the WM_SETCURSOR message. The change referenced
    `ui/views/win/hwnd_message_handler.cc` to add some handling for the
    `WM_SETCURSOR` message in WinWindow, ensuring it can set the cursor
    correctly.
    
    Fixed: 465179704
    Change-Id: I71a474a41a5244ada92bcd975dd696986acc650a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7210122
    Reviewed-by: Jesse McKenna <jessemckenna@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Auto-Submit: YISI YU <hellofroser@gmail.com>
    Reviewed-by: David Bienvenu <davidbienvenu@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1554093}

--- ui/platform_window/win/win_window.h
+++ ui/platform_window/win/win_window.h
@@ -81,6 +81,7 @@ class WIN_WINDOW_EXPORT WinWindow : public PlatformWindow,
     CR_MESSAGE_RANGE_HANDLER_EX(WM_MOUSEFIRST, WM_MOUSELAST, OnMouseRange)
     CR_MESSAGE_RANGE_HANDLER_EX(WM_NCMOUSEMOVE, WM_NCXBUTTONDBLCLK,
                                 OnMouseRange)
+    CR_MESSAGE_HANDLER_EX(WM_SETCURSOR, OnSetCursor);
     CR_MESSAGE_HANDLER_EX(WM_CAPTURECHANGED, OnCaptureChanged)
 
     CR_MESSAGE_HANDLER_EX(WM_KEYDOWN, OnKeyEvent)
@@ -108,6 +109,7 @@ class WIN_WINDOW_EXPORT WinWindow : public PlatformWindow,
   void OnDestroy();
   void OnPaint(HDC);
   void OnWindowPosChanged(WINDOWPOS* window_pos);
+  LRESULT OnSetCursor(UINT message, WPARAM w_param, LPARAM l_param);
 
   raw_ptr<PlatformWindowDelegate> delegate_;
 
