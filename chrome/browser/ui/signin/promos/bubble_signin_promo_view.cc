4d8e32b79dd7d7b071d941ce3be976d4ce76c911
Amelie Schneider <amelies@google.com>
2025-12-04 02:59:07 -0800

    [UNO] Show bookmarks sign in promo in separate bubble
    Until now, the bookmark sign in promo was shown in the footnote of the
    bookmark save bubble. With `UnoPhase2FollowUp`, the promo is now shown
    in a separate bubble that is shown if the user clicks on "Done". It
    follows the same logic as the autofill promos for being shown (max. 5
    times shown, max. 2 times dismissed).
    
    Before: https://screenshot.googleplex.com/5DGLVKfZAGz43TT
    After: https://screenshot.googleplex.com/8PHkZdqAB7HsPyh
    
    These changes are fully behind the flag `UnoPhase2FollowUp`, except for
    the metric
    `Signin.PromoLimitsExperiment.BookmarkSigninPromoShownCountAtSignin`,
    which without the flag enabled always records 0.
    
    Bug: 428140889
    Change-Id: Iadb24d8dc5a303f2266d08a27927b8885918f7e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7213561
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Amelie Schneider <amelies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1553897}

--- chrome/browser/ui/signin/promos/bubble_signin_promo_view.cc
+++ chrome/browser/ui/signin/promos/bubble_signin_promo_view.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/base/signin_metrics.h"
 #include "components/signin/public/base/signin_prefs.h"
 #include "components/strings/grit/components_strings.h"
+#include "components/sync/base/features.h"
 #include "content/public/browser/web_contents.h"
 #include "extensions/buildflags/buildflags.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -93,7 +94,9 @@ int GetSubtitleID(bool is_signin_promo,
         switch (signed_in_state) {
           case SignedInState::kSignedOut:
           case SignedInState::kWebOnlySignedIn:
-            return IDS_BOOKMARK_INSTALLED_PROMO_EXPLICIT_SIGNIN_MESSAGE;
+            return base::FeatureList::IsEnabled(syncer::kUnoPhase2FollowUp)
+                       ? IDS_BOOKMARK_INSTALLED_BUBBLE_PROMO_EXPLICIT_SIGNIN_MESSAGE
+                       : IDS_BOOKMARK_INSTALLED_PROMO_EXPLICIT_SIGNIN_MESSAGE;
           case SignedInState::kSignInPending:
             return IDS_BOOKMARK_VERIFY_PROMO_SUBTITLE;
           case SignedInState::kSignedIn:
@@ -338,7 +341,7 @@ void BubbleSignInPromoView::SignIn() {
 }
 
 void BubbleSignInPromoView::AddedToWidget() {
-  if (signin::IsAutofillSigninPromo(access_point_)) {
+  if (signin::IsBubbleSigninPromo(access_point_)) {
     scoped_widget_observation_.Observe(GetWidget());
   }
 }
@@ -346,7 +349,7 @@ void BubbleSignInPromoView::AddedToWidget() {
 void BubbleSignInPromoView::OnWidgetDestroying(views::Widget* widget) {
   // This should only be recorded for autofill bubble promos. Not for those
   // displayed in another bubble's footer.
-  if (!signin::IsAutofillSigninPromo(access_point_)) {
+  if (!signin::IsBubbleSigninPromo(access_point_)) {
     return;
   }
 
