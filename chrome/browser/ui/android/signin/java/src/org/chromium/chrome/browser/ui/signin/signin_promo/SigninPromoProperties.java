c3d5a2af43de729a1d5d41bc1c2eaedafe652b83
quacksort <quacksort@google.com>
2025-12-04 06:22:12 -0800

    [Signin][Android] Implement seamless sign-in loading state UI
    This CL introduces the SHOULD_SHOW_LOADING_STATE model property and a
    loading state UI for the seamless sign-in promo. The logic for the
    loading state will be added in a follow-up CL when the seamless sign-in
    API will be ready.
    
    When this property is set to true, the relevant UI elements are grayed
    out and disabled. This state will be used to indicate that the sign-in
    process is in progress.
    
    The appropriate string for the primary button during the loading state
    will be set in a follow-up CL.
    
    Screenshots: https://crbug.com/461474453#comment2
    
    Bug: 461474453
    Change-Id: I499bbc48947e191950519c51e5d4dce2459f208b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7205961
    Commit-Queue: Lucia Giorgi <quacksort@google.com>
    Reviewed-by: Samar Chehade <samarchehade@google.com>
    Cr-Commit-Position: refs/heads/main@{#1553955}

--- chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/signin_promo/SigninPromoProperties.java
+++ chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/signin_promo/SigninPromoProperties.java
@@ -60,6 +60,9 @@ final class SigninPromoProperties {
     static final PropertyModel.WritableBooleanPropertyKey SHOULD_SHOW_HEADER_WITH_AVATAR =
             new PropertyModel.WritableBooleanPropertyKey("should_show_header_with_avatar");
 
+    static final PropertyModel.WritableBooleanPropertyKey SHOULD_SHOW_LOADING_STATE =
+            new PropertyModel.WritableBooleanPropertyKey("should_show_loading_state");
+
     static final PropertyKey[] ALL_KEYS =
             new PropertyKey[] {
                 PROFILE_DATA,
@@ -73,7 +76,8 @@ final class SigninPromoProperties {
                 SHOULD_HIDE_SECONDARY_BUTTON,
                 SHOULD_HIDE_DISMISS_BUTTON,
                 SHOULD_SHOW_ACCOUNT_PICKER,
-                SHOULD_SHOW_HEADER_WITH_AVATAR
+                SHOULD_SHOW_HEADER_WITH_AVATAR,
+                SHOULD_SHOW_LOADING_STATE
             };
 
     private SigninPromoProperties() {}
@@ -90,7 +94,8 @@ final class SigninPromoProperties {
             boolean shouldSuppressSecondaryButton,
             boolean shouldHideDismissButton,
             boolean shouldShowAccountPicker,
-            boolean shouldShowHeaderWithAvatar) {
+            boolean shouldShowHeaderWithAvatar,
+            boolean shouldShowLoadingState) {
         return new PropertyModel.Builder(ALL_KEYS)
                 .with(PROFILE_DATA, profileData)
                 .with(ON_PRIMARY_BUTTON_CLICKED, (unusedView) -> onPrimaryButtonClicked.run())
@@ -104,6 +109,7 @@ final class SigninPromoProperties {
                 .with(SHOULD_HIDE_DISMISS_BUTTON, shouldHideDismissButton)
                 .with(SHOULD_SHOW_ACCOUNT_PICKER, shouldShowAccountPicker)
                 .with(SHOULD_SHOW_HEADER_WITH_AVATAR, shouldShowHeaderWithAvatar)
+                .with(SHOULD_SHOW_LOADING_STATE, shouldShowLoadingState)
                 .build();
     }
 }
