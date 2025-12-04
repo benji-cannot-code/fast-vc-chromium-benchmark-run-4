0bb6d0606dcd44367d51f228b158e34eda2bb935
Pier-Alexandre Bouchard <pabouchard@google.com>
2025-12-04 08:46:23 -0800

    [iOS] clean up code for GWS variation IDs to user feedback
    This change cleans up feature flagging logic associated with
    FeedbackIncludeGWSVariations feature flag as the feature is now rolled
    out.
    
    Depends-on: https://chrome-internal-review.googlesource.com/c/chrome/ios_internal/+/8816999
    Fixed: 424478603
    Change-Id: Ic591f4c71cba5d5e0c24f599d733bf328601cd53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7226581
    Commit-Queue: Pier-Alexandre Bouchard <pabouchard@google.com>
    Reviewed-by: Cheick Cisse <cheickcisse@google.com>
    Cr-Commit-Position: refs/heads/main@{#1554063}

--- ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
+++ ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
@@ -383,9 +383,6 @@ extern const char kFeedBackgroundRefreshDescription[];
 extern const char kFeedSwipeInProductHelpName[];
 extern const char kFeedSwipeInProductHelpDescription[];
 
-extern const char kFeedbackIncludeGWSVariationsName[];
-extern const char kFeedbackIncludeGWSVariationsDescription[];
-
 extern const char kFeedbackIncludeVariationsName[];
 extern const char kFeedbackIncludeVariationsDescription[];
 
