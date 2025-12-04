4c323a5cad7d69b189fce6fe0e87f1e19d57c8cf
Wayne Jackson Jr. <wbjacksonjr@google.com>
2025-12-04 06:18:01 -0800

    [metrics][compression_dictionary_transport] 6/n - Add EvictionReason Metric
    This CL is part of an effort to introduce a comprehensive suite of
    funnel metrics to measure the adoption and performance of the
    Compression Dictionary Transport feature.
    
    This CL introduces the Net.SharedDictionaryStorageOnDisk.MemoryCache.PreviousEvictionReason
    histogram. This metric records the specific reason (e.g., Memory
    Pressure, Cache Full) why a Shared Dictionary Storage instance was
    previously evicted from the in-memory cache.
    
    It is emitted when GetDictionarySync() is called for the first time on
    a re-created storage instance for a site isolation key that was
    previously evicted. This provides insight into the causes of dictionary
    unavailability when a storage needs to be reloaded, helping to
    understand the impact of system health (memory pressure) and cache size
    on feature performance.
    
    For the full metrics proposal see the document linked below.
    
    https://docs.google.com/document/d/1JXBKHRWcFs1WzW96lS1dsPPpqBJmwF3BKIKlSe7LblM/edit?usp=sharing&resourcekey=0-ntOuDsnKdnqOGaPfRRlvbg
    
    Bug: 456405285
    Change-Id: I55d4d1fd36e809e155f8d048285b0d8296e8cace
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7208873
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Patrick Meenan <pmeenan@chromium.org>
    Commit-Queue: Wayne Jackson Jr. <wbjacksonjr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1553952}

--- services/network/shared_dictionary/shared_dictionary_storage_on_disk.h
+++ services/network/shared_dictionary/shared_dictionary_storage_on_disk.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/extras/shared_dictionary/shared_dictionary_info.h"
 #include "net/extras/sqlite/sqlite_persistent_shared_dictionary_store.h"
 #include "net/shared_dictionary/shared_dictionary_isolation_key.h"
+#include "services/network/shared_dictionary/shared_dictionary_manager.h"
 #include "services/network/shared_dictionary/shared_dictionary_storage.h"
 #include "services/network/shared_dictionary/shared_dictionary_writer_on_disk.h"
 #include "url/gurl.h"
@@ -78,8 +79,7 @@ class SharedDictionaryStorageOnDisk : public SharedDictionaryStorage,
       const net::SharedDictionaryIsolationKey& isolation_key,
       base::ScopedClosureRunner on_deleted_closure_runner,
       scoped_refptr<SharedDictionaryCache> dictionary_cache,
-      bool was_previously_evicted,
-      bool was_previously_evicted_by_memory_pressure);
+      SharedDictionaryStorageEvictionReason previous_eviction_reason);
 
   SharedDictionaryStorageOnDisk(const SharedDictionaryStorageOnDisk&) = delete;
   SharedDictionaryStorageOnDisk& operator=(
@@ -172,8 +172,8 @@ class SharedDictionaryStorageOnDisk : public SharedDictionaryStorage,
 
   std::vector<base::OnceClosure> pending_get_dictionary_tasks_;
 
-  bool was_previously_evicted_ = false;
-  bool was_previously_evicted_by_memory_pressure_ = false;
+  SharedDictionaryStorageEvictionReason previous_eviction_reason_ =
+      SharedDictionaryStorageEvictionReason::kNotEvicted;
 
   base::WeakPtrFactory<SharedDictionaryStorageOnDisk> weak_factory_{this};
 };
