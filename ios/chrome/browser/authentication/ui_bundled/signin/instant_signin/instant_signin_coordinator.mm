594048a56e6b6bdaa9009ccce2d0da1321bd4b74
Noémie St-Onge <noemies@google.com>
2025-12-04 11:35:09 -0800

    [iOS] Clean up remaining TODOs for resolved bug
    Bug: 40067451
    Change-Id: Iab4c9ae015e221bddd69e9b5f85405a0eb5f0701
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7227877
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1554183}

--- ios/chrome/browser/authentication/ui_bundled/signin/instant_signin/instant_signin_coordinator.mm
+++ ios/chrome/browser/authentication/ui_bundled/signin/instant_signin/instant_signin_coordinator.mm
@@ -81,11 +81,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)dealloc {
-  // TODO(crbug.com/40067451): Switch back to DCHECK if the number of reports is
-  // low.
-  DUMP_WILL_BE_CHECK(!_mediator) << base::SysNSStringToUTF8([self description]);
-  DUMP_WILL_BE_CHECK(!_signinLogger)
-      << base::SysNSStringToUTF8([self description]);
+  // Not an invariant due to possible race conditions. DCHECKing for debugging
+  // purposes. See crbug.com/40067451.
+  DCHECK(!_mediator) << base::SysNSStringToUTF8([self description]);
+  DCHECK(!_signinLogger) << base::SysNSStringToUTF8([self description]);
 }
 
 #pragma mark - ChromeCoordinator
@@ -356,9 +355,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Adds an activity overlay to block the UI.
 - (void)showActivityOverlay {
-  // TODO(crbug.com/40067451): Switch back to DCHECK if the number of reports is
-  // low.
-  DUMP_WILL_BE_CHECK(!_activityOverlayCoordinator);
+  // Not an invariant due to possible race conditions. DCHECKing for debugging
+  // purposes. See crbug.com/40067451.
+  DCHECK(!_activityOverlayCoordinator);
   _activityOverlayCoordinator = [[ActivityOverlayCoordinator alloc]
       initWithBaseViewController:self.baseViewController
                          browser:self.browser];
