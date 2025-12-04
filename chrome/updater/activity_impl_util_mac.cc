2be3de1745d335cbf1a408d29f271829c7fd2793
Michael Herrmann <michael@herrmann.io>
2025-12-04 08:08:06 -0800

    Fix missing uses of macro KEYSTONE_NAME
    Before this commit, Brave's updater created a folder
    BraveSoftwareSoftwareUpdate (2x "Software") because its
    COMPANY_SHORTNAME_STRING is BraveSoftware and the previous
    implementation appended an additional "SoftwareUpdate".
    
    Change-Id: I1b15f6a1bce379b9cb01d72e35ff1b4ffd6962b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7216448
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1554035}

--- chrome/updater/activity_impl_util_mac.cc
+++ chrome/updater/activity_impl_util_mac.cc
@@ -43,7 +43,7 @@ base::FilePath GetActiveFile(const base::FilePath& home_dir,
                              const std::string& id) {
   return home_dir.Append("Library")
       .Append(COMPANY_SHORTNAME_STRING)
-      .Append(COMPANY_SHORTNAME_STRING "SoftwareUpdate")
+      .Append(KEYSTONE_NAME)
       .Append("Actives")
       .Append(id);
 }
