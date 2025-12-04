a09240d5003fd352380b807fea0ddff75d46b02c
Henrique Nakashima <hnakashima@chromium.org>
2025-12-04 13:13:51 -0800

    Reland "[Android] Add the Home button as OptionalViewElement in CtaPageStation"
    This is a reland of commit 54d0ff18b7bee27edf4a16cbf92493afc6730b52
    
    Fixed two bugs in Public Transit where:
    1. OptionalViewElement searched for the View in all Activities
    2. clickTo() did not attach the OptionalViewElement's owner as a context
    
    Original change's description:
    > [Android] Add the Home button as OptionalViewElement in CtaPageStation
    >
    > It had been removed because in tablets, depending on screen size, it
    > might not appear in the toolbar.
    >
    > Bug: 416324280
    > Cq-Include-Trybots: luci.chromium.try:android-12l-landscape-x64-dbg,android-desktop-15-x64-fyi-rel
    > Change-Id: I29adce16edbb52be9fc0239ceae6915bc0ce8680
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7204520
    > Reviewed-by: Xi Han <hanxi@chromium.org>
    > Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1553117}
    
    Bug: 416324280
    Cq-Include-Trybots: luci.chromium.try:android-12l-landscape-x64-dbg,android-desktop-15-x64-fyi-rel
    Change-Id: I2f6deec18ff57b69d9917faae05163c499993e60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7225513
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1554254}

--- chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/ToolbarTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/ToolbarTest.java
@@ -27,10 +27,10 @@ import android.content.res.Configuration;
 import android.view.View;
 import android.view.ViewGroup.MarginLayoutParams;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.test.annotation.UiThreadTest;
 import androidx.test.core.app.ApplicationProvider;
+import androidx.test.filters.LargeTest;
 import androidx.test.filters.MediumTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 
@@ -54,6 +54,7 @@ import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
+import org.chromium.base.test.util.ImportantFormFactors;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.base.ui.KeyboardUtils;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
@@ -82,6 +83,8 @@ import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.R;
 import org.chromium.chrome.test.transit.ChromeTransitTestRules;
 import org.chromium.chrome.test.transit.FreshCtaTransitTestRule;
+import org.chromium.chrome.test.transit.ntp.IncognitoNewTabPageStation;
+import org.chromium.chrome.test.transit.ntp.RegularNewTabPageStation;
 import org.chromium.chrome.test.transit.page.WebPageStation;
 import org.chromium.chrome.test.util.ChromeTabUtils;
 import org.chromium.chrome.test.util.MenuUtils;
@@ -640,6 +643,35 @@ public class ToolbarTest {
         }
     }
 
+    @Test
+    @LargeTest
+    // Disable opening windows side-by-side because home button might not show up on small windows.
+    @DisableFeatures(ChromeFeatureList.ROBUST_WINDOW_MANAGEMENT_EXPERIMENTAL)
+    @ImportantFormFactors(DeviceFormFactor.TABLET_OR_DESKTOP)
+    public void testHomeButton_loadsNtpOnSameTab() {
+        WebPageStation webPage = mPage;
+        webPage.homeButtonElement.checkPresent();
+
+        RegularNewTabPageStation ntp =
+                webPage.homeButtonElement
+                        .clickTo()
+                        .arriveAt(RegularNewTabPageStation.newBuilder().initFrom(webPage).build());
+        ntp.homeButtonElement.checkPresent();
+
+        WebPageStation incognitoWebPage = ntp.openNewIncognitoTabOrWindowFast().loadAboutBlank();
+        incognitoWebPage.homeButtonElement.checkPresent();
+
+        IncognitoNewTabPageStation incognitoNtp =
+                incognitoWebPage
+                        .homeButtonElement
+                        .clickTo()
+                        .arriveAt(
+                                IncognitoNewTabPageStation.newBuilder()
+                                        .initFrom(incognitoWebPage)
+                                        .build());
+        incognitoNtp.homeButtonElement.checkPresent();
+    }
+
     private void setAccessibilityEnabled(boolean enabled) {
         ThreadUtils.runOnUiThreadBlocking(
                 () -> ChromeAccessibilityUtil.get().setAccessibilityEnabledForTesting(enabled));
@@ -683,8 +715,7 @@ public class ToolbarTest {
                 });
     }
 
-    private void verifyTopControlsAccessibilityOrder(
-            @NonNull ToolbarPhone toolbar, @NonNull View ntpView) {
+    private void verifyTopControlsAccessibilityOrder(ToolbarPhone toolbar, View ntpView) {
         CriteriaHelper.pollUiThread(
                 () -> {
                     Criteria.checkThat(
@@ -698,8 +729,7 @@ public class ToolbarTest {
                 });
     }
 
-    private void verifyBottomControlsAccessibilityOrder(
-            @NonNull ToolbarPhone toolbar, @NonNull View ntpView) {
+    private void verifyBottomControlsAccessibilityOrder(ToolbarPhone toolbar, View ntpView) {
         CriteriaHelper.pollUiThread(
                 () -> {
                     Criteria.checkThat(
@@ -713,8 +743,7 @@ public class ToolbarTest {
                 });
     }
 
-    private void verifyAccessibilityOrderIsReset(
-            @NonNull ToolbarPhone toolbar, @Nullable View ntpView) {
+    private void verifyAccessibilityOrderIsReset(ToolbarPhone toolbar, View ntpView) {
         CriteriaHelper.pollUiThread(
                 () -> {
                     Criteria.checkThat(
