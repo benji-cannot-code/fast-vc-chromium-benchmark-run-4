7fea8c99c10d8e18dad235f8993449ea81ceb3df
Daniil Sakhapov <sakhapov@chromium.org>
2025-12-04 01:54:35 -0800

    Expose Event.pseudoTarget only on selected event types
    Also, return null for boundary events.
    
    Resolved in https://github.com/w3c/csswg-drafts/issues/12163
    
    Bug: 40639103
    Change-Id: I25706f3c56b5b0b9f829e484854d10a7b109d557
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7213558
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1553883}

--- third_party/blink/renderer/core/events/focus_event.h
+++ third_party/blink/renderer/core/events/focus_event.h
@@ -70,6 +70,8 @@ class FocusEvent final : public UIEvent {
     related_target_ = related_target;
   }
 
+  CSSPseudoElement* pseudoTarget() const { return Event::pseudoTarget(); }
+
   const AtomicString& InterfaceName() const override;
   bool IsFocusEvent() const override;
 
