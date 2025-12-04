1d9c29ec2d8ee07bcec55c8d7f678bbf69ab7e8d
Gary Klassen <gklassen@chromium.org>
2025-12-03 17:11:59 -0800

    Add Region to AdditionalContext types.
    Change-Id: I3893e02ba157e702d9f66f7f0ec179c3760a65d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7106149
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Commit-Queue: Gary Klassen <gklassen@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1553767}

--- chrome/browser/resources/glic/glic_api/glic_api.ts
+++ chrome/browser/resources/glic/glic_api/glic_api.ts
@@ -78,6 +78,7 @@ export declare interface AdditionalContextPart {
   annotatedPageData?: AnnotatedPageData;
   pdf?: PdfDocumentData;
   tabContext?: TabContextResult;
+  region?: CapturedRegion;
 }
 
 /**
