ba26184a4247bb1a84ad8d86465f4199655bbc25
Evan Luo <evanluo@google.com>
2025-12-04 13:10:39 -0800

    [sheriff] Disable ManageSyncSettingsTest.testLinkedServicesSettingEea
    Bug: 466094740
    Change-Id: I930993924cedd5672d7a2268b561a8621024e150
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7229882
    Auto-Submit: Evan Luo <evanluo@google.com>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Commit-Queue: Evan Luo <evanluo@google.com>
    Owners-Override: Evan Luo <evanluo@google.com>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1554249}

--- chrome/android/javatests/src/org/chromium/chrome/browser/sync/settings/ManageSyncSettingsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/sync/settings/ManageSyncSettingsTest.java
@@ -1148,6 +1148,7 @@ public class ManageSyncSettingsTest {
     @Test
     @LargeTest
     @Feature({"PersonalizedGoogleServices", "RenderTest"})
+    @DisabledTest(message = "Flaky - crbug.com/466094740")
     public void testLinkedServicesSettingEea() throws Exception {
         when(mRegionalCapabilities.isInEeaCountry()).thenReturn(true);
         mSyncTestRule.setUpAccountAndSignInForTesting();
