3bcc389849d42263e6a12f257da7fce9b1d8c9ea
Xiaoqian Dai <xdai@chromium.org>
2025-12-04 11:58:20 -0800

    capture mode: close screen capture session upon entering pinned mode.
    Bug: 463650936
    Change-Id: I5465ce51ceda524406d595c739d1306dbf85d156
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7214327
    Reviewed-by: Min Chen <minch@chromium.org>
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1554194}

--- ash/capture_mode/capture_mode_unittests.cc
+++ ash/capture_mode/capture_mode_unittests.cc
@@ -67,6 +67,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/desks/desks_test_util.h"
 #include "ash/wm/overview/overview_controller.h"
 #include "ash/wm/overview/overview_item.h"
+#include "ash/wm/screen_pinning_controller.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller_test_api.h"
 #include "ash/wm/window_state.h"
@@ -1996,6 +1997,18 @@ TEST_P(CaptureModeTest, WindowRecordingCaptureId) {
   controller->EndVideoRecording(EndRecordingReason::kStopRecordingButton);
   EXPECT_FALSE(controller->is_recording_in_progress());
   EXPECT_FALSE(window->subtree_capture_id().is_valid());
+}
+
+TEST_P(CaptureModeTest, StopOnPinnedStateChanged) {
+  auto* controller = CaptureModeController::Get();
+  controller->Start(CaptureModeEntryType::kQuickSettings);
+  EXPECT_TRUE(controller->IsActive());
+
+  // Pin a window.
+  std::unique_ptr<aura::Window> window = CreateTestWindow(gfx::Rect(200, 200));
+  wm::ActivateWindow(window.get());
+  window_util::PinWindow(window.get(), /*trusted=*/false);
+  EXPECT_FALSE(controller->IsActive());
 }
 
 TEST_P(CaptureModeTest, ClosingDimmedWidgetAboveRecordedWindow) {
