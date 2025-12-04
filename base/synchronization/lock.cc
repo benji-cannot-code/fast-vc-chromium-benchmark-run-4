87f5ec7bbc31b9fd6cb068d81fd6cc6a95217e0d
Nico Weber <thakis@chromium.org>
2025-12-04 13:08:34 -0800

    base: Remove Lock invariant checking support
    It's unused.
    
    Reverts most of https://chromium-review.googlesource.com/c/chromium/src/+/6058355
    
    Bug: 465165117
    Change-Id: I18cf6dd08986e322a14c0f576b6975d48dc5c0de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7216675
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1554246}

--- base/synchronization/lock.cc
+++ base/synchronization/lock.cc
@@ -15,9 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if DCHECK_IS_ON()
 #include <array>
-#include <memory>
 
-#include "base/functional/function_ref.h"
+#include "base/check_op.h"
 #include "base/synchronization/lock_subtle.h"
 #include "base/threading/platform_thread.h"
 
@@ -42,12 +41,6 @@ thread_local std::array<uintptr_t, kHeldLocksCapacity>
 thread_local size_t g_num_tracked_locks_held_by_thread = 0;
 
 }  // namespace
-
-Lock::Lock() = default;
-
-Lock::Lock(FunctionRef<void()> check_invariants)
-    : check_invariants_(
-          std::make_unique<FunctionRef<void()>>(check_invariants)) {}
 
 Lock::~Lock() {
   DCHECK(owning_thread_ref_.is_null());
@@ -90,18 +83,12 @@ void Lock::AssertNotHeld() const {
 
 void Lock::CheckHeldAndUnmark() {
   DCHECK_EQ(owning_thread_ref_, PlatformThread::CurrentRef());
-  if (check_invariants_) {
-    (*check_invariants_)();
-  }
   owning_thread_ref_ = PlatformThreadRef();
 }
 
 void Lock::CheckUnheldAndMark() {
   DCHECK(owning_thread_ref_.is_null());
   owning_thread_ref_ = PlatformThread::CurrentRef();
-  if (check_invariants_) {
-    (*check_invariants_)();
-  }
 }
 
 void Lock::AddToLocksHeldOnCurrentThread() {
--- chrome/updater/win/task_scheduler.h
+++ chrome/updater/win/task_scheduler.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/files/file_path.h"
+#include "base/functional/function_ref.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_refptr.h"
 
