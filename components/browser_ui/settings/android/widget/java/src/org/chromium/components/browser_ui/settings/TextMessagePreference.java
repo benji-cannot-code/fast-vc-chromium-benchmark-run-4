8d95e9cfc621b54291ef6faba6e4b25eff657ede
Evan Luo <evanluo@google.com>
2025-12-04 12:46:37 -0800

    Revert "[Settings containment] Align texts with header"
    This reverts commit 41ca915c465da461092453bfb4648254c7ae24d6.
    
    Reason for revert: Likely culprit for tests failing on android-10-x86-rel builder (crbug.com/466119731)
    
    Original change's description:
    > [Settings containment] Align texts with header
    >
    > As per the latest UI requirements, headers and texts must be aligned.
    >
    > Before: https://screenshot.googleplex.com/6q3QuYa44Co5AWK.png
    > After: https://screenshot.googleplex.com/4Nt4JR38NoSSacW.png
    >
    > Bug: 433576895, 465198553
    > Change-Id: I14c615ae9bddb7c9de306da028824c240c5d10b7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7219109
    > Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    > Auto-Submit: Gazal Agarwal <agazal@google.com>
    > Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    > Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1554052}
    
    Bug: 433576895, 465198553
    Change-Id: Ia92d08ec25805cb991d645dffcf9f4195f6815e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7229568
    Owners-Override: Evan Luo <evanluo@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Evan Luo <evanluo@google.com>
    Reviewed-by: Evan Luo <evanluo@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1554231}

--- components/browser_ui/settings/android/widget/java/src/org/chromium/components/browser_ui/settings/TextMessagePreference.java
+++ components/browser_ui/settings/android/widget/java/src/org/chromium/components/browser_ui/settings/TextMessagePreference.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.components.browser_ui.settings;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
@@ -34,12 +33,7 @@ public class TextMessagePreference extends ChromeBasePreference {
 
     @Override
     public @BackgroundStyle int getCustomBackgroundStyle() {
-        return BackgroundStyle.CARD;
-    }
-
-    @Override
-    public int getCustomBackgroundColor() {
-        return Color.TRANSPARENT;
+        return BackgroundStyle.NONE;
     }
 
     @Override
