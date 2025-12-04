c174f94140fb048381e23786e55eca84dd3ae639
Dale Curtis <dalecurtis@chromium.org>
2025-12-04 14:35:54 -0800

    [MediaCodec] Refuse to call setSurface() with invalid surface
    An internal bug b:30766796, suggests the following error message
    may be related to an invalid surface having been provided to the
    codec:
    * "dequeueBuffer: BufferQueue has no connected producer"
    
    This change adds a check for Surface.isValid() before calling
    setOutputSurface(). From code inspection, the underlying MediaCodec
    implementation doesn't seem to check this already.
    
    R=eugene
    
    Bug: 373986013
    Change-Id: I6f2ffd5d862272bc08748cd7f6b49f30dfe23fa4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7224540
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1554311}

--- media/base/android/java/src/org/chromium/media/MediaCodecBridge.java
+++ media/base/android/java/src/org/chromium/media/MediaCodecBridge.java
@@ -1086,6 +1086,10 @@ class MediaCodecBridge {
     @CalledByNative
     private boolean setSurface(Surface surface) {
         try {
+            if (surface == null || !surface.isValid()) {
+                Log.e(TAG, "Refusing to set an invalid surface.");
+                return false;
+            }
             mMediaCodec.setOutputSurface(surface);
         } catch (IllegalArgumentException | IllegalStateException e) {
             Log.e(TAG, "Cannot set output surface", e);
