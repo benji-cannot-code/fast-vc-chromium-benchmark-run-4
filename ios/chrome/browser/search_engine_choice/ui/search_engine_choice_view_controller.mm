1445f096ff535df4e2ea4723b620d091cdcd0acd
Jérôme Lebel <jlebel@chromium.org>
2025-12-04 08:25:25 -0800

    iOS,rcaps: Create SearchEngineExpandableSnippetView
    This patch cleans up SnippetSearchEngineButton by using
    SnippetSearchEngineTextView.
    The behavior is unchanged.
    
    The UI is unchanged except when the button is expanded with
    SearchEngineCurrentDefaultState::kOtherIsDefault.
    In this case the vertical bottom margin bug is fixed and the margin is
    the same than when the snippet is collapsed, as expected.
    
    Screenshots:
       Before:
    https://drive.google.com/file/d/1nQuAKq_9klMP0YTXlgM554k_q97wWlUd/view?resourcekey=0-nI2AbAx9mHtDNjg0QwVTTA
    
    https://drive.google.com/file/d/1GEr2vNZxT5Q1iMA7kUgYbZjRCHq86ggs/view?resourcekey=0-t4dPCUNZ5TM6Y-lQjqbAGg
    
    https://drive.google.com/file/d/1NVop_yqJPDPlZBW0CwErh3YdU1-MNj-s/view?resourcekey=0-O1svYvawUnZAPPRmQiN79Q
    
    https://drive.google.com/file/d/1XaXyc6wqGWuyHmx4xID4awZp_dKgjvvt/view?resourcekey=0-9hEWX9J5B4Ce7AkYWUBdSw
    
       After:
    https://drive.google.com/file/d/1bR-GRE6WD0OFLjGLI1y82qDZPwxmMpoM/view?resourcekey=0-e8kby2LywdcT6ISzGfrURQ
    
    https://drive.google.com/file/d/1Lxj7cw5uGB2qO-z8kggYEWugH1taupvn/view?resourcekey=0-RUOKKUrzKu7Bqv1JVAHZ3A
    
    https://drive.google.com/file/d/1ybCp7jbHjp6jp9kqbTxC62b850pS20Ja/view?resourcekey=0-0uLWraqhawHLzzyGCvN4mQ
    
    https://drive.google.com/file/d/1YYZSMDnrterI7b6RfPxJZwmldpVKsxLG/view?resourcekey=0-ig4B8LXqx2PRkVnVK8WN2g
    
    Fixed: 458277988
    Change-Id: I15f054427f78c2f6e4b38b950bc1f636b37d847c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7184698
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1554048}

--- ios/chrome/browser/search_engine_choice/ui/search_engine_choice_view_controller.mm
+++ ios/chrome/browser/search_engine_choice/ui/search_engine_choice_view_controller.mm
@@ -821,8 +821,7 @@ CGFloat GetSubtitleMarginDistance() {
   NSMutableSet<NSString*>* expandedSearchEngineKeyword = [NSMutableSet set];
   for (SnippetSearchEngineButton* oldSearchEngineButton in
            _searchEngineStackView.arrangedSubviews) {
-    if (oldSearchEngineButton.snippetButtonState ==
-        SnippetButtonState::kExpanded) {
+    if (oldSearchEngineButton.snippetExpanded) {
       [expandedSearchEngineKeyword
           addObject:oldSearchEngineButton.searchEngineKeyword];
     }
@@ -834,7 +833,7 @@ CGFloat GetSubtitleMarginDistance() {
     button = CreateSnippetSearchEngineButtonWithElement(element);
     button.animatedLayoutView = _scrollView;
     if ([expandedSearchEngineKeyword containsObject:element.keyword]) {
-      button.snippetButtonState = SnippetButtonState::kExpanded;
+      button.snippetExpanded = YES;
     }
     if ([selectedSearchEngineKeyword isEqualToString:element.keyword]) {
       button.checked = YES;
