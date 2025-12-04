e731eb60c3e629aa2f6f7f88eba952f1ad25b5e0
Russ Hamilton <behamilton@google.com>
2025-12-03 20:34:39 -0800

    Enable Actor Tools to execute on rendering-hidden elements
    Adds a call to DisplayLock::RevealAutoExpandableAncestors before we try
    to scroll to an out of view node. This allows actor tools to actuate on
    rendering-hidden elements such as <details>, nodes with the
    hidden="until-found" attribute, and nodes with the CSS property
    content-visibility:"auto".
    
    Bug: b:462063947
    Change-Id: Ia662d7d91291345045c13ba4863e7873a77b1510
    Fuchsia-Binary-Size: Size increase is unavoidable.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7189344
    Commit-Queue: Russ Hamilton <behamilton@google.com>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1553805}

--- third_party/blink/public/web/web_node.h
+++ third_party/blink/public/web/web_node.h
@@ -145,6 +145,8 @@ class BLINK_EXPORT WebNode {
 
   bool Focused() const;
 
+  void RevealAutoExpandableAncestors() const;
+
   WebPluginContainer* PluginContainer() const;
 
   bool IsInsideFocusableElementOrARIAWidget() const;
--- third_party/blink/renderer/core/exported/web_node.cc
+++ third_party/blink/renderer/core/exported/web_node.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/web/web_element_collection.h"
 #include "third_party/blink/renderer/bindings/core/v8/to_v8_traits.h"
 #include "third_party/blink/renderer/core/accessibility/ax_object_cache.h"
+#include "third_party/blink/renderer/core/display_lock/display_lock_utilities.h"
 #include "third_party/blink/renderer/core/dom/container_node.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/dom_node_ids.h"
@@ -274,6 +275,13 @@ std::vector<WebNode> WebNode::FindAllTextNodesMatchingRegex(
 
 bool WebNode::Focused() const {
   return private_->IsFocused();
+}
+
+void WebNode::RevealAutoExpandableAncestors() const {
+  auto result = DisplayLockUtilities::RevealAutoExpandableAncestors(*private_);
+  if (result.revealed_details || result.revealed_hidden_until_found) {
+    private_->GetDocument().UpdateStyleAndLayout(DocumentUpdateReason::kInput);
+  }
 }
 
 cc::ElementId WebNode::ScrollingElementIdForTesting() const {
