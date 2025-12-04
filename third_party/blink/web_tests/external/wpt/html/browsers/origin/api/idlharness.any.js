8eb211a88d51ad73e635b9e67ba13fbdd6c3405c
Mike West <mkwst@chromium.org>
2025-12-04 00:51:54 -0800

    [Origin API] Mark tests as non-tentative, and ship to stable.
    I2S: https://groups.google.com/a/chromium.org/g/blink-dev/c/CaCxnx6UaXo/m/WTelV2D8AgAJ
    
    Spec: https://html.spec.whatwg.org/multipage/browsers.html#the-origin-interface
    
    Bug: 434131026
    Change-Id: Ia7f99d9752507b16770d29d2d88eb546b013a387
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7205535
    Commit-Queue: Mike West <mkwst@chromium.org>
    Reviewed-by: Mike Taylor <miketaylr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1553869}

--- third_party/blink/web_tests/external/wpt/html/browsers/origin/api/idlharness.any.js
+++ third_party/blink/web_tests/external/wpt/html/browsers/origin/api/idlharness.any.js
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: script=/resources/idlharness.js
 
 idl_test(
-  ['origin.tentative'], [], (idl_array) => {
+  ['origin'], [], (idl_array) => {
     idl_array.add_objects({
       Origin: ["new Origin()"],
     });
