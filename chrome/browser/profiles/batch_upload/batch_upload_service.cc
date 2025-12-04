b04f9bceb424ae139da84ed27b76b00ddeaca3fb
Ryan Sultanem <rsult@google.com>
2025-12-04 14:59:40 -0800

    [Deps] Fixing deps in batch_upload_service.cc
    Fixes a `// nogncheck` added in a recent change:
    http://crrev.com/c/7213427
    
    The change is about moving "signin/signin_promo_util.h" into
    the `chrome/browser/signin` library so that it can directly be
    added in the batch_upload library without depending on `chrome/browser`.
    
    Also helps componentize signin library within the browser.
    
    Change-Id: Iebd94ce29650539bd67c0c24807bf6235964f25b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7217669
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1554319}

--- chrome/browser/profiles/batch_upload/batch_upload_service.cc
+++ chrome/browser/profiles/batch_upload/batch_upload_service.cc
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/profiles/batch_upload/batch_upload_delegate.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
-#include "chrome/browser/signin/signin_promo_util.h"  // nogncheck
+#include "chrome/browser/signin/signin_promo_util.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/toolbar_button_provider.h"
