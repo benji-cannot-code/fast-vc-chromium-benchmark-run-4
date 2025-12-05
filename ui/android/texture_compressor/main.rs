d8960d704e5297f89e4785146cf85e6cda1e5d7f
Lukasz Anforowicz <lukasza@chromium.org>
2025-12-05 13:34:18 -0800

    [rust] Use `chromium::import!` to avoid refering to mangled crate names.
    Bug: 463302946
    Change-Id: Ib95dc2b073b1431e6a41d6422c58affc185d4783
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7230181
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1554896}

--- ui/android/texture_compressor/main.rs
+++ ui/android/texture_compressor/main.rs
@@ -3,6 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+chromium::import! {
+    "//ui/android:texture_compressor";
+}
+
 use std::env;
 use std::fs;
 use std::fs::File;
@@ -10,7 +14,8 @@ use std::io::{BufReader, BufWriter};
 use std::path::Path;
 
 use bytemuck::cast_slice;
-use ui_sandroid_ctexture_ucompressor::{compress_etc1, decompress_etc1};
+
+use texture_compressor::{compress_etc1, decompress_etc1};
 
 const ETC1_BLOCK_SIZE: u32 = 8;
 
