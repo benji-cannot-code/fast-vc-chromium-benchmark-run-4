562d75223e92432beaf4e44c7a32c62739a3a35a
Alex Gough <ajgo@chromium.org>
2025-12-04 17:50:47 -0800

    Delayload secur32, winhttp, winspool & wintrust
    These DLLs are listed as imports of chrome.dll and so get loaded for
    every Chrome process. As renderer processes do not call functions in
    these DLLs they can be delayloaded, reducing renderer startup time.
    
    The EchoService and its supporting helper echo_preload_library.dll are
    used in tests to validate that the PreloadLibrary functionality works,
    including being able to resolve already loaded delayload functions in a
    utility. As secur32 is now delayloaded this test's DLL is updated to
    call timeGetTime in winmm.dll which remains a regular import.
    
    Local measurements on a warmed-up build:
    
      cl/7214648 Renderer.BrowserLaunchToRunLoopStart = 42.9 (this CL)
      cl/7173332 Renderer.BrowserLaunchToRunLoopStart = 47.0
      baseline   Renderer.BrowserLaunchToRunLoopStart = 48.5
    
    Bug: 40275328
    Change-Id: Ic4c0191926dc5f50b44c2c9cfaddd740afb73265
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7214648
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1554411}

--- content/browser/service_host/service_process_host_browsertest.cc
+++ content/browser/service_host/service_process_host_browsertest.cc
@@ -253,7 +253,7 @@ IN_PROC_BROWSER_TEST_F(ServiceProcessHostBrowserTest, PreloadLibraryNotSet) {
 
   base::RunLoop loop;
   echo_service->LoadNativeLibrary(
-      GetDllPath(kEchoPreloadLibrary), /*call_sec32_delayload=*/false,
+      GetDllPath(kEchoPreloadLibrary), /*call_winmm_delayload=*/false,
       base::BindLambdaForTesting([&](LoadStatus status, uint32_t result) {
         EXPECT_EQ(LoadStatus::kFailedLoadLibrary, status);
         EXPECT_EQ(DWORD{ERROR_ACCESS_DENIED}, result);
@@ -277,7 +277,7 @@ IN_PROC_BROWSER_TEST_F(ServiceProcessHostBrowserTest, PreloadLibraryPreloaded) {
   base::RunLoop loop;
   echo_service->LoadNativeLibrary(
       GetDllPath(kEchoPreloadLibrary),
-      /*call_sec32_delayload=*/true,
+      /*call_winmm_delayload=*/true,
       base::BindLambdaForTesting([&](LoadStatus status, uint32_t result) {
         EXPECT_EQ(LoadStatus::kSuccess, status);
         EXPECT_EQ(0u, result);
@@ -303,7 +303,7 @@ IN_PROC_BROWSER_TEST_F(ServiceProcessHostBrowserTest, PreloadLibraryMultiple) {
 
   base::RunLoop loop;
   echo_service->LoadNativeLibrary(
-      GetDllPath(kEchoPreloadLibrary), /*call_sec32_delayload=*/false,
+      GetDllPath(kEchoPreloadLibrary), /*call_winmm_delayload=*/false,
       base::BindLambdaForTesting([&](LoadStatus status, uint32_t result) {
         EXPECT_EQ(LoadStatus::kSuccess, status);
         EXPECT_EQ(0u, result);
@@ -327,7 +327,7 @@ IN_PROC_BROWSER_TEST_F(ServiceProcessHostBrowserTest, PreloadLibraryModName) {
   base::RunLoop loop;
   // Once preloaded can people simply provide the module name?
   echo_service->LoadNativeLibrary(
-      base::FilePath(kEchoPreloadLibrary), /*call_sec32_delayload=*/false,
+      base::FilePath(kEchoPreloadLibrary), /*call_winmm_delayload=*/false,
       base::BindLambdaForTesting([&](LoadStatus status, uint32_t result) {
         EXPECT_EQ(LoadStatus::kSuccess, status);
         EXPECT_EQ(0u, result);
