092f2390cef446160c8db11ce5755711d7174cb1
Kene Okoye <kenok@google.com>
2025-12-04 21:42:35 -0800

    [Actor] Rename ActorHistogramsTest to ActorMetricsTest
    Change-Id: I67d117414e815191d5b5dbc00e7a7eb6cead0acc
    Bug: b:462762031
    Fixed: b:462762031
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7229744
    Reviewed-by: Kevin Graney <kmg@google.com>
    Commit-Queue: Kene Okoye <kenok@google.com>
    Auto-Submit: Kene Okoye <kenok@google.com>
    Cr-Commit-Position: refs/heads/main@{#1554464}

--- chrome/browser/actor/actor_metrics_unittest.cc
+++ chrome/browser/actor/actor_metrics_unittest.cc
@@ -31,7 +31,7 @@ void CheckToolRequestVariantNamesImpl(
    ...);
 }
 
-TEST(ActorHistogramsTest, CheckToolRequestVariantNames) {
+TEST(ActorMetricsTest, CheckToolRequestVariantNames) {
   std::optional<base::HistogramVariantsEntryMap> tool_requests =
       base::ReadVariantsFromHistogramsXml("ToolRequest", "actor");
   ASSERT_TRUE(tool_requests.has_value());
@@ -43,7 +43,7 @@ TEST(ActorHistogramsTest, CheckToolRequestVariantNames) {
                           std::variant_size_v<actor::ToolRequestVariant>>{});
 }
 
-TEST(ActorHistogramsTest, CheckActorTaskStateVariantNames) {
+TEST(ActorMetricsTest, CheckActorTaskStateVariantNames) {
   std::optional<base::HistogramVariantsEntryMap> task_states =
       base::ReadVariantsFromHistogramsXml("ActorTaskState", "actor");
   ASSERT_TRUE(task_states.has_value());
