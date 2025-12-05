b43ae8eee27c32bcf1351035463bf75381182e41
Roberto Torres <jrtm@google.com>
2025-12-04 23:45:22 -0800

    spanification: automatically spanify .../vaapi/test/fake_libva_driver/fake_drv_video.cc etc.
    This is the result of running the automatic spanification on linux and
    updating code to use and pass spans where size is known.
    
    The original patch was fully automated using script:
    //tools/clang/spanify/rewrite-multiple-platforms.sh -platforms=linux
    Then refined with gemini-cli
    
    gemini-run/batch-run-1761798078/group_88
    
    BUG=439964610
    
    spanification: automatically spanify .../vaapi/test/fake_libva_driver/fake_drv_video.cc etc.
    
    This is the result of running the automatic spanification on linux and
    updating code to use and pass spans where size is known.
    
    The original patch was fully automated using script:
    //tools/clang/spanify/rewrite-multiple-platforms.sh -platforms=linux
    Then refined with gemini-cli
    
    gemini-run/batch-run-1761798078/group_88
    
    BUG=439964610
    
    Change-Id: I20e686f2440b57551076bd126fa46d367caf2776
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7205276
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Auto-Submit: Roberto Torres <jrtm@google.com>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1554524}

--- media/gpu/vaapi/test/fake_libva_driver/h264_decoder_delegate.h
+++ media/gpu/vaapi/test/fake_libva_driver/h264_decoder_delegate.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <va/va.h>
 
 #include "base/containers/lru_cache.h"
+#include "base/containers/span.h"
 #include "base/memory/raw_ptr.h"
 #include "media/gpu/vaapi/test/fake_libva_driver/context_delegate.h"
 #include "third_party/openh264/src/codec/api/wels/codec_api.h"
@@ -32,7 +33,7 @@ class H264DecoderDelegate : public ContextDelegate {
   void Run() override;
 
  private:
-  void OnFrameReady(unsigned char* pData[3], SBufferInfo* pDstInfo);
+  void OnFrameReady(base::span<uint8_t*, 3> pData, SBufferInfo* pDstInfo);
 
   raw_ptr<ISVCDecoder> svc_decoder_ = nullptr;
   const VAProfile profile_;
