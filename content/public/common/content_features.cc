b3408d41cf1c2132f57f27f6657ef26831840408
Ramin Halavati <rhalavati@chromium.org>
2025-12-04 22:23:25 -0800

    [a11y] Add AccessibilityExtendedSelection feature and flag.
    Adds `AccessibilityExtendedSelection` feature and flag to gate
    implementation of setting extended selection in Android.
    
    This flag will be used in the upcoming CL: http://crrev.com/c/7018751
    
    Bug: 443078007
    Change-Id: I6edd5f93836f13462eed03df551abe4476b7b39a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7207329
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1554485}

--- content/public/common/content_features.cc
+++ content/public/common/content_features.cc
@@ -1231,6 +1231,11 @@ const base::FeatureParam<bool> kAccessibilityDeprecateJavaNodeCacheDisableCache{
 BASE_FEATURE(kAccessibilityDeprecateTypeAnnounce,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+// When enabled, extended selections are sent to Android through setSelection
+// API.
+BASE_FEATURE(kAccessibilityExtendedSelection,
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 // When enabled, WINDOW_CONTENT_CHANGED events will be sent for each
 // LIVE_REGION_NODE_CHANGED rather than TYPE_ANNOUNCEMENT.
 // kAccessibilityDeprecateTypeAnnounce also encompasses ariaNotify, whereas this
