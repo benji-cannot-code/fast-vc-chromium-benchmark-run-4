e973e9403843ce37e76f7d5a7beac337bbb22eeb
Darwin Yang <darwinyang@google.com>
2025-12-05 11:43:59 -0800

    [spc] Add header logos and payment instrument details to the spc model
    Bug: 441559501
    Change-Id: I64c93704d9c49c203e633aee38776a844fb590a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7217689
    Commit-Queue: Darwin Yang <darwinyang@chromium.org>
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1554828}

--- components/payments/content/secure_payment_confirmation_model.h
+++ components/payments/content/secure_payment_confirmation_model.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "components/payments/content/payment_app.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 
 namespace payments {
@@ -27,6 +28,15 @@ class SecurePaymentConfirmationModel {
       delete;
   SecurePaymentConfirmationModel& operator=(
       const SecurePaymentConfirmationModel& other) = delete;
+
+  // Header logos.
+  const std::vector<PaymentApp::PaymentEntityLogo*> header_logos() const {
+    return header_logos_;
+  }
+  void set_header_logos(
+      const std::vector<PaymentApp::PaymentEntityLogo*> header_logos) {
+    header_logos_ = header_logos;
+  }
 
   // Title, e.g. "Use TouchID to verify and complete your purchase?"
   const std::u16string& title() const { return title_; }
@@ -61,10 +71,19 @@ class SecurePaymentConfirmationModel {
     instrument_label_ = instrument_label;
   }
 
-  // Label for the instrument row value, e.g. "****4444"
+  // Label for the instrument row value, e.g. "Chase Card"
   const std::u16string& instrument_value() const { return instrument_value_; }
   void set_instrument_value(const std::u16string& instrument_value) {
     instrument_value_ = instrument_value;
+  }
+
+  // Sub-label for the instrument row value, e.g. "****4444"
+  const std::u16string& instrument_details_value() const {
+    return instrument_details_value_;
+  }
+  void set_instrument_details_value(
+      const std::u16string& instrument_details_value) {
+    instrument_details_value_ = instrument_details_value;
   }
 
   // Instrument icon.
@@ -160,6 +179,8 @@ class SecurePaymentConfirmationModel {
   base::WeakPtr<SecurePaymentConfirmationModel> GetWeakPtr();
 
  private:
+  std::vector<PaymentApp::PaymentEntityLogo*> header_logos_;
+
   std::u16string title_;
   std::u16string description_;
 
@@ -169,6 +190,7 @@ class SecurePaymentConfirmationModel {
 
   std::u16string instrument_label_;
   std::u16string instrument_value_;
+  std::u16string instrument_details_value_;
   raw_ptr<const SkBitmap> instrument_icon_ = nullptr;
 
   std::u16string total_label_;
