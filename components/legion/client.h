ec9af1f6ac0689e0c5ad211f7124f4b19b43f68a
Nikhil Jakhar <nikhiljakhar@google.com>
2025-12-05 07:58:26 -0800

    legion: Refactor legion::Client into an interface and implementation
    This change splits the legion::Client class into an abstract base class
    (Client) and an implementation class (ClientImpl). The existing
    functionality from client.cc and client.h has been moved to
    client_impl.cc and client_impl.h, respectively. The unit tests have been
    updated to test the new ClientImpl class. This refactoring allows for
    easier mocking and dependency injection of the Legion client.
    
    BYPASS_LARGE_CHANGE_WARNING: The code is split into impl and interface
    
    Bug: b:465689650
    Change-Id: Icaf22c306638a1a32ad479aacdc17fa0c1528199
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7221694
    Commit-Queue: Nikhil Jakhar <nikhiljakhar@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Oleh Lamzin <lamzin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1554676}

--- components/legion/client.h
+++ components/legion/client.h
@@ -6,20 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_LEGION_CLIENT_H_
 #define COMPONENTS_LEGION_CLIENT_H_
 
-#include <cstdint>
 #include <memory>
 #include <string>
 
-#include "base/containers/flat_map.h"
-#include "base/containers/flat_set.h"
 #include "base/functional/callback.h"
-#include "base/memory/raw_ptr.h"
-#include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "base/types/expected.h"
+#include "components/legion/error_code.h"
 #include "components/legion/legion_common.h"
 #include "components/legion/proto/legion.pb.h"
-#include "components/legion/secure_channel.h"
 #include "url/gurl.h"
 
 namespace network::mojom {
@@ -28,7 +23,7 @@ class NetworkContext;
 
 namespace legion {
 
-// Client for starting the session and sending requests.
+// Interface for the legion client.
 class Client {
  public:
   using BinaryEncodedProtoRequest = Request;
@@ -54,9 +49,6 @@ class Client {
   using OnEstablishSessionCompletedCallback =
       base::OnceCallback<void(base::expected<void, ErrorCode>)>;
 
-  using SecureChannelFactory =
-      base::RepeatingCallback<std::unique_ptr<SecureChannel>()>;
-
   static std::unique_ptr<Client> Create(
       network::mojom::NetworkContext* network_context);
 
@@ -67,76 +59,34 @@ class Client {
   // Takes a URL without scheme and an api_key and returns a URL.
   static GURL FormatUrl(const std::string& url, const std::string& api_key);
 
-  ~Client();
-
-  Client(const Client&) = delete;
-  Client& operator=(const Client&) = delete;
+  virtual ~Client() = default;
 
   // Establishes a secure session without sending a request. The callback will
   // be invoked upon completion. Calling this function is optional as a session
   // will be established automatically when needed/first request is sent.
-  void EstablishSession(OnEstablishSessionCompletedCallback callback);
+  virtual void EstablishSession(
+      OnEstablishSessionCompletedCallback callback) = 0;
 
   // Sends a request with a single text content.
-  void SendTextRequest(proto::FeatureName feature_name,
-                       const std::string& text,
-                       OnTextRequestCompletedCallback callback,
-                       base::TimeDelta timeout = kDefaultTimeout);
+  virtual void SendTextRequest(proto::FeatureName feature_name,
+                               const std::string& text,
+                               OnTextRequestCompletedCallback callback) = 0;
+  virtual void SendTextRequest(proto::FeatureName feature_name,
+                               const std::string& text,
+                               OnTextRequestCompletedCallback callback,
+                               base::TimeDelta timeout) = 0;
 
   // Sends a `GenerateContentRequest`. The caller is responsible for populating
   // the `request` proto, including setting the content's role to "user".
-  void SendGenerateContentRequest(
+  virtual void SendGenerateContentRequest(
+      proto::FeatureName feature_name,
+      const proto::GenerateContentRequest& request,
+      OnGenerateContentRequestCompletedCallback callback) = 0;
+  virtual void SendGenerateContentRequest(
       proto::FeatureName feature_name,
       const proto::GenerateContentRequest& request,
       OnGenerateContentRequestCompletedCallback callback,
-      base::TimeDelta timeout = kDefaultTimeout);
-
- private:
-  friend class ClientTest;
-
-  explicit Client(SecureChannelFactory channel_factory);
-
-  // Recreates the secure channel and sets the response callback.
-  void RecreateSecureChannel();
-
-  // Sends a request over the secure channel.
-  void SendRequest(int32_t request_id,
-                   BinaryEncodedProtoRequest request,
-                   OnRequestCompletedCallback callback,
-                   base::TimeDelta timeout = kDefaultTimeout);
-
-  // Handles responses from the secure channel.
-  void OnResponseReceived(
-      base::expected<BinaryEncodedProtoResponse, ErrorCode> result);
-
-  // Wraps a request callback to record latency metrics upon completion.
-  void OnRequestCompleted(
-      OnRequestCompletedCallback callback,
-      base::TimeTicks start_time,
-      base::expected<BinaryEncodedProtoResponse, ErrorCode> result);
-
-  // Handles a request timeout.
-  void OnRequestTimeout(int32_t request_id);
-
-  // Fails all pending requests with the given error code.
-  void FailAllPendingRequests(ErrorCode error_code);
-
-  // Handles the result of a session establishment request.
-  void OnSessionEstablished(OnEstablishSessionCompletedCallback callback,
-                            base::expected<void, ErrorCode> result);
-
-  std::unique_ptr<SecureChannel> secure_channel_;
-  SecureChannelFactory secure_channel_factory_;
-  int32_t next_request_id_{1};
-
-  // Callbacks for requests that have been sent to the secure channel but have
-  // not yet received a response.
-  base::flat_map<int32_t, OnRequestCompletedCallback> pending_requests_;
-
-  // The request_ids of requests that have timed out.
-  base::flat_set<int32_t> timed_out_requests_;
-
-  base::WeakPtrFactory<Client> weak_factory_{this};
+      base::TimeDelta timeout) = 0;
 };
 
 }  // namespace legion
