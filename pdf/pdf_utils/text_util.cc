af88566f8e6cf8e5f27d62cfeec373a619ecec3e
Andy Phan <andyphan@chromium.org>
2025-12-05 15:51:10 -0800

    [PDF Ink Signatures] Add util function for word boundary
    Move some of the word boundary detection logic in
    FindMultipleClickBoundary() into a new util file. This will be used in
    https://crrev.com/c/7202150 to detect the ends of a text word.
    
    Bug: 427139501
    Change-Id: Iae2e2e07680f60cd12bc7513fc3596e105baab6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7202149
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1554977}

---
+++ pdf/pdf_utils/text_util.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "pdf/pdf_utils/text_util.h"
+
+#include <stdint.h>
+
+#include "base/strings/string_util.h"
+#include "pdf/pdfium/pdfium_range.h"
+
+namespace chrome_pdf {
+
+bool IsWordBoundary(uint32_t ch) {
+  // Deal with ASCII characters.
+  if (base::IsAsciiAlpha(ch) || base::IsAsciiDigit(ch) || ch == '_') {
+    return false;
+  }
+  return ch < 128 || ch == kZeroWidthSpace;
+}
+
+}  // namespace chrome_pdf
