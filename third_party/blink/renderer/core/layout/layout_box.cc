f0895242860881f46187860f223d716607c78e87
Ryo Hashimoto <hashimoto@google.com>
2025-12-04 17:27:41 -0800

    blink: Ignore transition when updating scrollbar nodes
    LayoutView::OverflowClipRect() doesn't exclude scrollbar from the rect when in transition, but this results in inconsistency between scrollbar dimensions held by blink and cc.
    
    Add a new parameter to OverflowClipRect() to disable this behavior so that blink and cc use the same scrollbar dimension values.
    
    BUG=459997344
    
    Change-Id: Ib6034eb079f1db25bd87b1af290d3acf445af118
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7206579
    Commit-Queue: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1554399}

--- third_party/blink/renderer/core/layout/layout_box.cc
+++ third_party/blink/renderer/core/layout/layout_box.cc
@@ -2491,6 +2491,11 @@ PhysicalRect LayoutBox::OverflowClipRect(
   return clip_rect;
 }
 
+PhysicalRect LayoutBox::OverflowClipRectForScrollNode(
+    const PhysicalOffset& location) const {
+  return OverflowClipRect(location);
+}
+
 bool LayoutBox::HasControlClip() const {
   NOT_DESTROYED();
   if (IsTextField() || IsMenuList() || IsInputButton()) [[unlikely]] {
--- third_party/blink/renderer/core/layout/layout_view.h
+++ third_party/blink/renderer/core/layout/layout_view.h
@@ -166,6 +166,8 @@ class CORE_EXPORT LayoutView : public LayoutBlockFlow {
   PhysicalRect OverflowClipRect(const PhysicalOffset& location,
                                 OverlayScrollbarClipBehavior =
                                     kIgnoreOverlayScrollbarSize) const override;
+  PhysicalRect OverflowClipRectForScrollNode(
+      const PhysicalOffset& location) const override;
 
   // If either direction has a non-auto mode, the other must as well.
   void SetAutosizeScrollbarModes(mojom::blink::ScrollbarMode h_mode,
@@ -374,6 +376,10 @@ class CORE_EXPORT LayoutView : public LayoutBlockFlow {
     NOT_DESTROYED();
     return false;
   }
+
+  PhysicalRect OverflowClipRectInternal(const PhysicalOffset& location,
+                                        OverlayScrollbarClipBehavior,
+                                        bool for_scroll_node) const;
 
   // The page area (content area) size of the first page, when printing. This
   // size should always be consulted when printing, also when not paginating
