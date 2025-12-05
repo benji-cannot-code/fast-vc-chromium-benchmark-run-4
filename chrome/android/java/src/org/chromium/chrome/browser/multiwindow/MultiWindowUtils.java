27716f074822f3ff13660b993d66de1e34342ab4
Joel Tan-Aristy <jtanaristy@google.com>
2025-12-05 11:47:17 -0800

    Adds unit tests MultiInstanceManagerApi31#moveTabsToOtherWindow
    Bug: 465141949
    Change-Id: If0f241b22aaf0327255d013410a0884cfb4b0fa8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7224769
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Commit-Queue: Joel Tan-Aristy <jtanaristy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1554831}

--- chrome/android/java/src/org/chromium/chrome/browser/multiwindow/MultiWindowUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/multiwindow/MultiWindowUtils.java
@@ -108,6 +108,7 @@ public class MultiWindowUtils implements ActivityStateListener {
     protected static @Nullable Supplier<Activity> sActivitySupplierForTesting;
 
     private static @Nullable Integer sMaxInstancesForTesting;
+    private static @Nullable Integer sIncognitoInstanceCountForTesting;
     private static @Nullable Integer sInstanceCountForTesting;
     private static @Nullable Boolean sMultiInstanceApi31EnabledForTesting;
     private final boolean mMultiInstanceApi31Enabled;
@@ -522,6 +523,9 @@ public class MultiWindowUtils implements ActivityStateListener {
      */
     // TODO (crbug.com/461553972): Remove this method after Robust Window Management is launched.
     public static int getIncognitoInstanceCount(boolean activeOnly) {
+        if (sIncognitoInstanceCountForTesting != null) {
+            return sIncognitoInstanceCountForTesting;
+        }
         int instanceType = PersistedInstanceType.OFF_THE_RECORD;
         if (activeOnly) {
             instanceType |= PersistedInstanceType.ACTIVE;
@@ -1249,6 +1253,11 @@ public class MultiWindowUtils implements ActivityStateListener {
         var oldValue = sInstance;
         sInstance = instance;
         ResettersForTesting.register(() -> sInstance = oldValue);
+    }
+
+    public static void setIncognitoInstanceCountForTesting(int instanceCount) {
+        sIncognitoInstanceCountForTesting = instanceCount;
+        ResettersForTesting.register(() -> sIncognitoInstanceCountForTesting = null);
     }
 
     public static void setInstanceCountForTesting(int instanceCount) {
