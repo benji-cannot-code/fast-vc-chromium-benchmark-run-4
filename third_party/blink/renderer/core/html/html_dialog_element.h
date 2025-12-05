9a8dc82d558616213c232440fa5566942ccb39f8
Marijn Kruisselbrink <mek@chromium.org>
2025-12-05 11:16:01 -0800

    Revert "Make light dismiss use click instead of pointerdown/up"
    This reverts commit 42184b6093f88d911f66e207ef9a40d23135f0c8.
    
    Reason for revert: One of the added tests is failing on https://ci.chromium.org/ui/p/chromium/builders/ci/mac15-arm64-rel-tests/16265/overview
    
    Original change's description:
    > Make light dismiss use click instead of pointerdown/up
    >
    > This patch makes light dismiss for popovers and dialogs use a single
    > click event instead of pointerdown and pointerup. This fixes several
    > bugs with light dismiss, especially on touch devices. More context here:
    > https://github.com/w3c/pointerevents/issues/542
    >
    > The MenuListCustomExpandCollapse a11y test expectations changed because
    > before this patch, the test wasn't actually opening and closing the
    > picker like it was intended to. With this patch it is opening and
    > closing the picker as intended.
    >
    > Bug: 425579196, 408010435
    > Change-Id: I739bf5ffe19062043e233c0bb0f7c96f7d8d31a4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6664038
    > Reviewed-by: Mason Freed <masonf@chromium.org>
    > Reviewed-by: Robert Flack <flackr@chromium.org>
    > Commit-Queue: Joey Arhar <jarhar@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1554711}
    
    Bug: 425579196, 408010435
    Change-Id: Icb23d296d134252b24f4bb8e4a1bc7345d55e8bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7233092
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Marijn Kruisselbrink <mek@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1554808}

--- third_party/blink/renderer/core/html/html_dialog_element.h
+++ third_party/blink/renderer/core/html/html_dialog_element.h
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_HTML_HTML_DIALOG_ELEMENT_H_
 
 #include "third_party/blink/renderer/core/core_export.h"
-#include "third_party/blink/renderer/core/events/pointer_event_factory.h"
 #include "third_party/blink/renderer/core/events/toggle_event.h"
 #include "third_party/blink/renderer/core/html/closewatcher/close_watcher.h"
 #include "third_party/blink/renderer/core/html/html_element.h"
@@ -86,19 +85,8 @@ class CORE_EXPORT HTMLDialogElement final : public HTMLElement {
   String closedBy() const;
   void setClosedBy(const String& return_value);
 
-  // HandleDialogLightDismiss is only called when the LightDismissFromClick flag
-  // is disabled, and HandleDialogLightDismissForClick is only called when
-  // LightDismissFromClick is enabled.
-  // HandleDialogLightDismiss is called twice for each click, once for
-  // pointerdown and once for pointerup.
-  // HandleDialogLightDismissForClick is only called once for each click and
-  // contains the relevant information from the corresponding pointerdown and
-  // pointerup events.
   static void HandleDialogLightDismiss(const PointerEvent& pointer_event,
                                        const Node& target_node);
-  static void HandleDialogLightDismissForClick(
-      const PointerEventFactory::PointerTarget& pointer_down_target,
-      const PointerEventFactory::PointerTarget& pointer_up_target);
 
   void CloseWatcherFiredCancel(Event*);
   void CloseWatcherFiredClose();
