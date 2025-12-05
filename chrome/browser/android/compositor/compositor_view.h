50f601b426ba989968e3581e0b2ff00f7b7fbeaa
Aldo Culquicondor <acondor@chromium.org>
2025-12-05 13:43:16 -0800

    Cleanup CompositorView destructor
    The comment seems stale, so an explicit pointer reset should no longer
    be necessary. Also, the WeakPtrFactory was unused.
    
    Change-Id: Id885d05d3121e3d581de77bea3ee0769e73b8aa1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7233734
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Aldo Culquicondor <acondor@chromium.org>
    Commit-Queue: Aldo Culquicondor <acondor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1554906}

--- chrome/browser/android/compositor/compositor_view.h
+++ chrome/browser/android/compositor/compositor_view.h
@@ -146,8 +146,6 @@ class CompositorView : public content::CompositorClient,
   bool overlay_video_mode_;
   bool overlay_immersive_ar_mode_;
   bool overlay_xr_full_screen_mode_;
-
-  base::WeakPtrFactory<CompositorView> weak_factory_{this};
 };
 
 }  // namespace android
