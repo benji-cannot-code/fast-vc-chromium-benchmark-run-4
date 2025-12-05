adbd7df1dc59476896f49462625a076d42070bf8
Dana Fried <dfried@chromium.org>
2025-12-05 14:01:27 -0800

    [Vertical Tabs] Paint top container in Vertical Tabs mode
    This fixes bugs where the region around the caption buttons aren't
    properly painted on Windows, Linux, and ChromeOS.
    
    This doesn't fix issues around the toolbar's own rounded corners, but
    that will be addressed in a follow-up.
    
    Bug: 465482604
    Change-Id: I37d1855e23ec21ee3b203a8b231ae39a807e49af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7233993
    Reviewed-by: Alison Gale <agale@chromium.org>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1554920}

--- chrome/browser/ui/views/frame/top_container_view.cc
+++ chrome/browser/ui/views/frame/top_container_view.cc
@@ -6,14 +6,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/top_container_view.h"
 
 #include "chrome/browser/ui/browser_element_identifiers.h"
+#include "chrome/browser/ui/color/chrome_color_id.h"
+#include "chrome/browser/ui/layout_constants.h"
 #include "chrome/browser/ui/views/frame/browser_frame_view.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/browser_widget.h"
 #include "chrome/browser/ui/views/frame/immersive_mode_controller.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
+#include "ui/color/color_variant.h"
 #include "ui/compositor/layer.h"
+#include "ui/gfx/canvas.h"
 #include "ui/views/paint_info.h"
 #include "ui/views/view_class_properties.h"
+
+#if BUILDFLAG(IS_CHROMEOS)
+#include "chromeos/constants/chromeos_features.h"
+#endif
 
 TopContainerView::TopContainerView(BrowserView* browser_view)
     : browser_view_(browser_view) {
@@ -62,6 +70,38 @@ void TopContainerView::PaintChildren(const views::PaintInfo& paint_info) {
   }
 #endif
   View::PaintChildren(paint_info);
+}
+
+void TopContainerView::OnPaintBackground(gfx::Canvas* canvas) {
+  // Top container draws an opaque background when in vertical tabstrip mode.
+  if (browser_view_->ShouldDrawVerticalTabStrip()) {
+    // Color matches toolbar; this provides a backdrop for e.g. caption buttons.
+    const SkColor color =
+        ui::ColorVariant(kColorToolbar).ResolveToSkColor(GetColorProvider());
+
+    // Rounded corners are drawn when not maximized or fullscreen.
+    bool use_rounded_corners =
+        !browser_view_->IsMaximized() && !browser_view_->IsFullscreen();
+#if BUILDFLAG(IS_CHROMEOS)
+    if (!chromeos::features::IsRoundedWindowsEnabled()) {
+      use_rounded_corners = false;
+    }
+#endif
+    if (use_rounded_corners) {
+      const float radius = GetLayoutConstant(TOOLBAR_CORNER_RADIUS);
+      const SkVector radii[4] = {
+          {radius, radius}, {radius, radius}, {0, 0}, {0, 0}};
+      const SkPath path = SkPath::RRect(
+          SkRRect::MakeRectRadii(gfx::RectToSkRect(GetLocalBounds()), radii));
+      cc::PaintFlags flags;
+      flags.setAntiAlias(true);
+      flags.setStyle(cc::PaintFlags::kFill_Style);
+      flags.setColor(color);
+      canvas->DrawPath(path, flags);
+    } else {
+      canvas->DrawColor(color);
+    }
+  }
 }
 
 void TopContainerView::ChildPreferredSizeChanged(views::View* child) {
