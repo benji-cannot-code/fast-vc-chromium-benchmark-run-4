a2aedae29adc2185050a979bc1b42dfcc5776bf6
Darren Shen <shend@chromium.org>
2025-12-04 22:25:35 -0800

    Change "Learn More" button to a link in Quick Insert feature tour.
    Note there's a temporary workaround to remove extra padding
    created automatically by SystemDialogDelegateView. This workaround
    will be removed in the next CL.
    
    Bug: 465882905
    Change-Id: I89578a3b3fcf64a4c94dc1e391002172fa195aea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7230225
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Michelle Chen <michellegc@google.com>
    Reviewed-by: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1554487}

--- ash/quick_insert/quick_insert_controller_unittest.cc
+++ ash/quick_insert/quick_insert_controller_unittest.cc
@@ -66,6 +66,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/size.h"
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/controls/button/button.h"
+#include "ui/views/controls/link.h"
 #include "ui/views/controls/textfield/textfield.h"
 #include "ui/views/focus/focus_manager.h"
 #include "ui/views/test/test_widget_builder.h"
@@ -469,9 +470,9 @@ TEST_F(QuickInsertControllerTest, ToggleWidgetOpensUrlAfterLearnMore) {
       OpenUrl(Property("host", &GURL::host, "support.google.com"), _, _))
       .Times(1);
 
-  const views::Button* button = feature_tour.learn_more_button_for_testing();
-  ASSERT_NE(button, nullptr);
-  LeftClickOn(button);
+  const views::Link* link = feature_tour.learn_more_link_for_testing();
+  ASSERT_NE(link, nullptr);
+  LeftClickOn(link);
   views::test::WidgetDestroyedWaiter(feature_tour.widget_for_testing()).Wait();
 
   EXPECT_FALSE(controller().widget_for_testing());
--- ash/quick_insert/views/quick_insert_feature_tour_unittest.cc
+++ ash/quick_insert/views/quick_insert_feature_tour_unittest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/event_constants.h"
 #include "ui/events/keycodes/keyboard_codes.h"
 #include "ui/views/controls/button/button.h"
+#include "ui/views/controls/link.h"
 #include "ui/views/test/widget_test.h"
 #include "ui/views/widget/widget.h"
 
@@ -64,7 +65,7 @@ TEST_F(QuickInsertFeatureTourTest,
 }
 
 TEST_F(QuickInsertFeatureTourTest,
-       ClickingLearnMoreButtonClosesWidgetAndTriggersCallback) {
+       ClickingLearnMoreLinkClosesWidgetAndTriggersCallback) {
   QuickInsertFeatureTour feature_tour;
   base::test::TestFuture<void> learn_more_future;
   feature_tour.MaybeShowForFirstUse(
@@ -72,9 +73,9 @@ TEST_F(QuickInsertFeatureTourTest,
       learn_more_future.GetRepeatingCallback(), base::DoNothing());
   views::test::WidgetVisibleWaiter(feature_tour.widget_for_testing()).Wait();
 
-  const views::Button* button = feature_tour.learn_more_button_for_testing();
-  ASSERT_NE(button, nullptr);
-  LeftClickOn(button);
+  const views::Link* link = feature_tour.learn_more_link_for_testing();
+  ASSERT_NE(link, nullptr);
+  LeftClickOn(link);
 
   views::test::WidgetDestroyedWaiter(feature_tour.widget_for_testing()).Wait();
   EXPECT_TRUE(learn_more_future.Wait());
