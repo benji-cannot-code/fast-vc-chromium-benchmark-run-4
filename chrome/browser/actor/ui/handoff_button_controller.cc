547a838bb26d0054922257d3384be87e0185c12b
Sidhin Thomas <sidhin.thomas@gmail.com>
2025-12-05 13:26:23 -0800

    Add missing header breaking the enable_glic=False config
    Bug: None
    Change-Id: I3848f8df7727a9b8a4d7a542e7fbfc618558e2da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7233570
    Reviewed-by: Kevin Graney <kmg@google.com>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Kevin Graney <kmg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1554887}

--- chrome/browser/actor/ui/handoff_button_controller.cc
+++ chrome/browser/actor/ui/handoff_button_controller.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/tabs/public/tab_features.h"
 #include "chrome/browser/ui/views/interaction/browser_elements_views.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/tabs/public/tab_interface.h"
 #include "components/vector_icons/vector_icons.h"
