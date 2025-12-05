d4c767765f103502fe5f4043349cb1c740c0d541
Ameur Hosni <ameurhosni@google.com>
2025-12-05 09:04:02 -0800

    [IOS][AIM] Record normal search navigation
    Bug: 466353929
    Change-Id: I091af35667d788ac4b7da9ad65168aa2c4f55dfa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7232071
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Auto-Submit: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1554710}

--- ios/chrome/browser/composebox/coordinator/composebox_input_plate_mediator.mm
+++ ios/chrome/browser/composebox/coordinator/composebox_input_plate_mediator.mm
@@ -1157,6 +1157,8 @@ CreateInputDataFromAnnotatedPageContent(
   switch (_modeHolder.mode) {
     case ComposeboxMode::kRegularSearch:
       [self.URLLoader loadURLParams:URLLoadParams];
+      [self.metricsRecorder recordAutocompleteRequestTypeAtNavigation:
+                                AutocompleteRequestType::kSearch];
       break;
     case ComposeboxMode::kAIM:
       [self.metricsRecorder recordAutocompleteRequestTypeAtNavigation:
