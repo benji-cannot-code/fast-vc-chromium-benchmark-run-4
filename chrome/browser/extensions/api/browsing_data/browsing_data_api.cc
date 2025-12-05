6b4c80fd8b13d907f31c770acca95188fbd6f136
Zaina Al-Mashni <zalmashni@google.com>
2025-12-05 05:42:12 -0800

    [DBD] Respond with error if removePasswords API is called
    Fixed: 429589322
    Change-Id: I700830731bdd97b1da5d99ddfdf6c38c6f3bf51b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7220690
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Auto-Submit: Zaina Al-Mashni <zalmashni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1554608}

--- chrome/browser/extensions/api/browsing_data/browsing_data_api.cc
+++ chrome/browser/extensions/api/browsing_data/browsing_data_api.cc
@@ -283,6 +283,11 @@ ExtensionFunction::ResponseAction BrowsingDataRemoverFunction::Run() {
 
   EXTENSION_FUNCTION_VALIDATE(GetRemovalMask(&removal_mask_));
 
+  if (IsRemovalDeprecated()) {
+    return RespondNow(
+        Error(extension_browsing_data_api_constants::kDeprecatedDataTypeError));
+  }
+
   const base::Value::List* origins =
       options.FindList(extension_browsing_data_api_constants::kOriginsKey);
   const base::Value::List* exclude_origins = options.FindList(
@@ -332,6 +337,10 @@ BrowsingDataRemoverFunction::~BrowsingDataRemoverFunction() = default;
 
 bool BrowsingDataRemoverFunction::IsPauseSyncAllowed() {
   return true;
+}
+
+bool BrowsingDataRemoverFunction::IsRemovalDeprecated() {
+  return false;
 }
 
 void BrowsingDataRemoverFunction::StartRemoving() {
@@ -566,6 +575,16 @@ bool BrowsingDataRemovePasswordsFunction::GetRemovalMask(
   LogUnsupportedDataTypeWarning(
       extension_browsing_data_api_constants::kPasswordsKeyDeprecated);
   return true;
+}
+
+bool BrowsingDataRemovePasswordsFunction::IsRemovalDeprecated() {
+#if !BUILDFLAG(IS_ANDROID)
+  if (base::FeatureList::IsEnabled(
+          browsing_data::features::kPasswordRemovalExtensionErrorKillSwitch)) {
+    return true;
+  }
+#endif  // !BUILDFLAG(IS_ANDROID)
+  return false;
 }
 
 bool BrowsingDataRemoveServiceWorkersFunction::GetRemovalMask(
