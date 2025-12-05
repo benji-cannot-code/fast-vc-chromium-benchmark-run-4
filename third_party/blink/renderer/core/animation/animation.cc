02a328ada1fabef114aeec82215706ae4d2dcf1f
David Awogbemila <awogbemila@chromium.org>
2025-12-05 09:44:33 -0800

    [animation-trigger] Ensure triggered CSS animations are exposed
    Normally, we pause a triggered CSS animation at the time of attaching
    the trigger. However, this means we will leave as idle a CSS animation
    whose trigger name we didn't find. This results in the animation being
    idle and not showing up in getAnimations.
    
    We should:
    
    - make them show up in getAnimations by pausing in anticipation of
      finding the trigger (which may show up later), and
    - spec this behavior.
    
    This CL also addresses the fact that pause() likely should not change
    the paused_for_trigger state of the animation.
    
    Bug: 390314945, 465915372
    Change-Id: I381ea0cc42772a1e4afb5e13d27631982c8be97d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7228342
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Reviewed-by: David Awogbemila <awogbemila@chromium.org>
    Commit-Queue: David Awogbemila <awogbemila@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1554739}

--- third_party/blink/renderer/core/animation/animation.cc
+++ third_party/blink/renderer/core/animation/animation.cc
@@ -1559,7 +1559,6 @@ void Animation::PauseInternal(ExceptionState& exception_state) {
   // 2. If the play state of animation is paused, abort these steps.
   if (pending_pause_ ||
       CalculateAnimationPlayState() == V8AnimationPlayState::Enum::kPaused) {
-    SetPausedForTrigger(false);
     return;
   }
 
@@ -1594,8 +1593,6 @@ void Animation::PauseInternal(ExceptionState& exception_state) {
       seek_time = EffectEnd();
     }
   }
-
-  SetPausedForTrigger(false);
 
   // 6. If seek time is resolved,
   //        If has finite timeline is true,
