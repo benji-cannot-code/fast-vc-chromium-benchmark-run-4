723c26b5a7461d36b1e8f3a3f0b639b0af876d97
Nate Chapin <japhet@chromium.org>
2025-12-05 15:14:06 -0800

    Disable lock top controls on desktop in Minimal UI/Window Controls Overlay TWAs
    Fixed: 460177198
    Change-Id: Ie0d64aa5bfea8c950877263e5a76210a4992d23b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7218608
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1554959}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/BaseCustomTabRootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/BaseCustomTabRootUiCoordinator.java
@@ -35,6 +35,7 @@ import org.chromium.base.supplier.OneshotSupplier;
 import org.chromium.base.supplier.OneshotSupplierImpl;
 import org.chromium.base.supplier.SettableObservableSupplier;
 import org.chromium.base.supplier.SupplierUtils;
+import org.chromium.blink.mojom.DisplayMode;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ActivityTabProvider;
 import org.chromium.chrome.browser.IntentHandler;
@@ -785,6 +786,11 @@ public class BaseCustomTabRootUiCoordinator extends RootUiCoordinator {
                             () -> mCompositorViewHolderSupplier.get().requestFocus(),
                             mClientPackageName);
             mBrowserControlsManager.addObserver(mWebAppHeaderLayoutCoordinator);
+            if (intentDataProvider.getResolvedDisplayMode() == DisplayMode.MINIMAL_UI
+                    || intentDataProvider.getResolvedDisplayMode()
+                            == DisplayMode.WINDOW_CONTROLS_OVERLAY) {
+                mBrowserControlsManager.disableSyncMinHeightWithTotalHeight();
+            }
         }
         if (DesktopPopupHeaderUtils.isDesktopPopupHeaderEnabled(intentDataProvider)) {
             final var desktopWindowStateManager = getDesktopWindowStateManager();
