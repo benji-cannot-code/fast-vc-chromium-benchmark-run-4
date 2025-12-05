42c69d4c57eec9d24848c71f734c477fdc6f7080
Rafał Godlewski <rgod@google.com>
2025-12-05 10:23:12 -0800

    [WebAuthn] Remove GetPasskeysForRelyingPartyID from passkey model
    GetPasskeysForRelyingPartyID() was returning all stored passkeys for a
    provided RP ID, excluding shadowed ones.
    
    This CL replaces it with a new universal GetPasskeys() function that
    allows specifying some criteria. In this case, this should be:
    GetPasskeys(rp_id, ShadowedCredentials::kExclude).
    
    Bug: 465377708
    Change-Id: I8ec75fd22554e2a4b3a109a2d1b4044e3ed0fd73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7221650
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1554765}

--- components/webauthn/ios/passkey_tab_helper.mm
+++ components/webauthn/ios/passkey_tab_helper.mm
@@ -271,7 +271,8 @@ bool PasskeyTabHelper::HasExcludedPasskey(
   }
 
   std::vector<sync_pb::WebauthnCredentialSpecifics> passkeys =
-      passkey_model_->GetPasskeysForRelyingPartyId(params.RpId());
+      passkey_model_->GetPasskeys(params.RpId(),
+                                  PasskeyModel::ShadowedCredentials::kExclude);
   for (const auto& passkey : passkeys) {
     if (exclude_credentials.contains(passkey.credential_id())) {
       return true;
@@ -284,7 +285,8 @@ std::vector<sync_pb::WebauthnCredentialSpecifics>
 PasskeyTabHelper::GetFilteredPasskeys(
     const AssertionRequestParams& params) const {
   std::vector<sync_pb::WebauthnCredentialSpecifics> passkeys =
-      passkey_model_->GetPasskeysForRelyingPartyId(params.RpId());
+      passkey_model_->GetPasskeys(params.RpId(),
+                                  PasskeyModel::ShadowedCredentials::kExclude);
   if (passkeys.empty()) {
     return passkeys;
   }
