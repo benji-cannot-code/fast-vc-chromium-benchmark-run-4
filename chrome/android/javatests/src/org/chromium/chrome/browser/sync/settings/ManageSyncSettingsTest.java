9703838bb81436dd1a1fa3d061c827ad260a489c
Gazal Agarwal <agazal@google.com>
2025-12-05 17:09:11 -0800

    Reland "[Settings containment] Update containment after verifying account"
    This reverts commit 8088351b921d43fd951c3f4a0e3a0085c6fec5b8.
    
    Reason for revert: Disabled ff for failing tests
    Original change's description:
    > Revert "[Settings containment] Update containment after verifying account"
    >
    > This reverts commit 8cf25dd8e8f4d5616e10bd61ea94556151ab1c8e.
    >
    > Reason for revert:
    > LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5709250379644928
    >
    > Sample build with failed test: https://ci.chromium.org/b/8696475261298137073
    > Affected test(s):
    > [://chrome/android\:chrome_public_test_apk!junit:org.chromium.chrome.browser.sync.settings:ManageSyncSettingsTest#testSignoutButton](https://ci.chromium.org/ui/test/chromium/:%2F%2Fchrome%2Fandroid%5C:chrome_public_test_apk%21junit:org.chromium.chrome.browser.sync.settings:ManageSyncSettingsTest%23testSignoutButton?q=VHash%3A82f25b9fdc8c2802)
    >
    > If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5709250379644928&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F7219230&type=BUG
    >
    > Original change's description:
    > > [Settings containment] Update containment after verifying account
    > >
    > > Before: https://screenshot.googleplex.com/5jRMGDn4bozg3p6.png, https://screencast.googleplex.com/cast/NjI0MzUyMTE4NTk3MjIyNHxjMThhZGI4Ny05OA
    > > After: https://screencast.googleplex.com/cast/NTkwOTE0MTI1NTA5NDI3MnxkNTQxOGExNC0wOQ
    > >
    > > Bug: 433576895, 464168241
    > > Change-Id: If641231b03474120e5f13a35a78db47c6cfd7d50
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7219230
    > > Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    > > Commit-Queue: Gazal Agarwal <agazal@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1553564}
    > >
    >
    > Bug: 433576895, 464168241
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: Id3e5f1693840526a24f522bc6b427b1858cd8d01
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7224398
    > Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    > Owners-Override: Ioana Pandele <ioanap@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1553875}
    
    Bug: 433576895, 464168241
    Change-Id: I76bf31a384355878bec71b7110c17d74a9d28da2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7233771
    Commit-Queue: Gazal Agarwal <agazal@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1555010}

--- chrome/android/javatests/src/org/chromium/chrome/browser/sync/settings/ManageSyncSettingsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/sync/settings/ManageSyncSettingsTest.java
@@ -368,6 +368,8 @@ public class ManageSyncSettingsTest {
     @Test
     @LargeTest
     @Feature({"Sync"})
+    // TODO(crbug.com/433576895): Re-enable containment feature once the test is fixed.
+    @DisableFeatures(ChromeFeatureList.ANDROID_SETTINGS_CONTAINMENT)
     public void testPressingSignOut() {
         mSyncTestRule.setUpAccountAndSignInForTesting();
 
@@ -836,6 +838,8 @@ public class ManageSyncSettingsTest {
     @Test
     @LargeTest
     @Feature({"Sync", "RenderTest"})
+    // TODO(crbug.com/433576895): Re-enable containment feature once the test is fixed.
+    @DisableFeatures(ChromeFeatureList.ANDROID_SETTINGS_CONTAINMENT)
     public void testSignoutButton() throws Exception {
         mSyncTestRule.setUpAccountAndSignInForTesting();
         final ManageSyncSettings fragment = startManageSyncPreferences();
@@ -1164,6 +1168,8 @@ public class ManageSyncSettingsTest {
     @Test
     @LargeTest
     @Feature({"PersonalizedGoogleServices"})
+    // TODO(crbug.com/433576895): Re-enable containment feature once the test is fixed.
+    @DisableFeatures(ChromeFeatureList.ANDROID_SETTINGS_CONTAINMENT)
     public void testClickPersonalizeGoogleServicesNonEEA() {
         mSyncTestRule.setUpAccountAndSignInForTesting();
         final ManageSyncSettings fragment = startManageSyncPreferences();
@@ -1180,6 +1186,8 @@ public class ManageSyncSettingsTest {
     @Test
     @LargeTest
     @Feature({"PersonalizedGoogleServices"})
+    // TODO(crbug.com/433576895): Re-enable containment feature once the test is fixed.
+    @DisableFeatures(ChromeFeatureList.ANDROID_SETTINGS_CONTAINMENT)
     public void testClickPersonalizeGoogleServicesEEA() {
         when(mRegionalCapabilities.isInEeaCountry()).thenReturn(true);
         mSyncTestRule.setUpAccountAndSignInForTesting();
