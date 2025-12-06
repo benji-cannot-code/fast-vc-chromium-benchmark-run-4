779795fdfc0f3dbac5c13317f53554836d631f63
Steve Becker <stevebe@microsoft.com>
2025-12-05 16:37:28 -0800

    [DomStorage] Implement DeleteStorageKeysFromSession for session storage
    `SessionStorageLevelDB::DeleteStorageKeysFromSession()` removes a
    metadata entry for each storage key using the LevelDB key:
    
    "namespace-<session_id>-<storage_key>"
    
    Optionally removes map key/value pairs using the LevelDB prefix:
    
    "map-<map_id>-"
    
    `DeleteStorageKeysFromSession()` callers specify which maps to delete.
    The function must not remove maps still referenced by clones in other
    sessions.
    
    Updates local storage to always delete its map key/values from the
    LevelDB because local storage does not support cloning.
    
    Bug: 377242771
    Change-Id: I93a5a9792902b5f62fa0d06ed6b7b67dbdcd5259
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7188078
    Commit-Queue: Steve Becker <stevebe@microsoft.com>
    Reviewed-by: Evan Stade <evanstade@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1555001}

--- components/services/storage/dom_storage/leveldb/local_storage_leveldb_unittest.cc
+++ components/services/storage/dom_storage/leveldb/local_storage_leveldb_unittest.cc
@@ -707,9 +707,12 @@ TEST_F(LocalStorageLevelDBTest,
           },
       }));
 
+  std::vector<DomStorageDatabase::MapLocator> maps_to_delete;
+  maps_to_delete.emplace_back(kLocalStorageSessionId, kFakeUrlStorageKey);
+
   DbStatus status = local_storage_leveldb->DeleteStorageKeysFromSession(
-      kLocalStorageSessionId, {kFakeUrlStorageKey},
-      /*excluded_cloned_map_ids=*/{});
+      kLocalStorageSessionId, /*metadata_to_delete=*/{kFakeUrlStorageKey},
+      std::move(maps_to_delete));
   EXPECT_TRUE(status.ok()) << status.ToString();
 
   // Verify the contents in the database, which should only include the
@@ -736,9 +739,12 @@ TEST_F(LocalStorageLevelDBTest, DeleteStorageKeysFromSessionWithWriteMetadata) {
           },
       }));
 
+  std::vector<DomStorageDatabase::MapLocator> maps_to_delete;
+  maps_to_delete.emplace_back(kLocalStorageSessionId, kFakeUrlStorageKey);
+
   DbStatus status = local_storage_leveldb->DeleteStorageKeysFromSession(
-      kLocalStorageSessionId, {kFakeUrlStorageKey},
-      /*excluded_cloned_map_ids=*/{});
+      kLocalStorageSessionId, /*metadata_to_delete=*/{kFakeUrlStorageKey},
+      std::move(maps_to_delete));
   EXPECT_TRUE(status.ok()) << status.ToString();
 
   // Verify the contents in the database, which should only include the
@@ -751,8 +757,7 @@ TEST_F(LocalStorageLevelDBTest, DeleteStorageKeysFromSessionWithWriteMetadata) {
   VerifyDatabaseVersionEntry(all_entries[0]);
 }
 
-TEST_F(LocalStorageLevelDBTest,
-       DeleteStorageKeysFromSessionWithWriteMapKeyValues) {
+TEST_F(LocalStorageLevelDBTest, DeleteStorageKeysFromSessionWithMapKeyValues) {
   std::unique_ptr<LocalStorageLevelDB> local_storage_leveldb;
   ASSERT_NO_FATAL_FAILURE(OpenInMemory(&local_storage_leveldb));
 
@@ -769,9 +774,12 @@ TEST_F(LocalStorageLevelDBTest,
                        },
                    }));
 
+  std::vector<DomStorageDatabase::MapLocator> maps_to_delete;
+  maps_to_delete.emplace_back(kLocalStorageSessionId, kFakeUrlStorageKey);
+
   DbStatus status = local_storage_leveldb->DeleteStorageKeysFromSession(
-      kLocalStorageSessionId, {kFakeUrlStorageKey},
-      /*excluded_cloned_map_ids=*/{});
+      kLocalStorageSessionId, /*metadata_to_delete=*/{kFakeUrlStorageKey},
+      std::move(maps_to_delete));
   EXPECT_TRUE(status.ok()) << status.ToString();
 
   // Verify the contents in the database, which should only include the
@@ -852,9 +860,14 @@ TEST_F(LocalStorageLevelDBTest,
       }));
 
   // Erase the first and third storage keys.
+  std::vector<DomStorageDatabase::MapLocator> maps_to_delete;
+  maps_to_delete.emplace_back(kLocalStorageSessionId, kFakeUrlStorageKey);
+  maps_to_delete.emplace_back(kLocalStorageSessionId, kThirdStorageKey);
+
   DbStatus status = local_storage_leveldb->DeleteStorageKeysFromSession(
-      kLocalStorageSessionId, {kFakeUrlStorageKey, kThirdStorageKey},
-      /*excluded_cloned_map_ids=*/{});
+      kLocalStorageSessionId,
+      /*metadata_to_delete=*/{kFakeUrlStorageKey, kThirdStorageKey},
+      std::move(maps_to_delete));
   EXPECT_TRUE(status.ok()) << status.ToString();
 
   // Verify the contents in the database, which should include the second
@@ -884,15 +897,22 @@ TEST_F(LocalStorageLevelDBTest,
   EXPECT_EQ(all_entries[3].value, ToBytes("value_3"));
 
   // Erase all the storage keys.
+  maps_to_delete.clear();
+  maps_to_delete.emplace_back(kLocalStorageSessionId, kFakeUrlStorageKey);
+  maps_to_delete.emplace_back(kLocalStorageSessionId, kSecondStorageKey);
+  maps_to_delete.emplace_back(kLocalStorageSessionId, kThirdStorageKey);
+  maps_to_delete.emplace_back(kLocalStorageSessionId, kFourthStorageKey);
+
   status = local_storage_leveldb->DeleteStorageKeysFromSession(
       kLocalStorageSessionId,
+      /*metadata_to_delete=*/
       {
           kFakeUrlStorageKey,
           kSecondStorageKey,
           kThirdStorageKey,
           kFourthStorageKey,
       },
-      /*excluded_cloned_map_ids=*/{});
+      std::move(maps_to_delete));
   EXPECT_TRUE(status.ok()) << status.ToString();
 
   // Verify the contents in the database, which should only include the
