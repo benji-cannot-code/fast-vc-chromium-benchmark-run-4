82615f2d0a4af8bf3389357a84be32c240579126
Colin Blundell <blundell@chromium.org>
2025-12-08 00:40:41 -0800

    [Blink] Have HTMLVideoElement hold CanvasSnapshotProvider
    Note: I did not do any broader renames here in this CL to limit scope;
    will do those in followup.
    
    Note2: Added includes here are ripple effects from removing
    canvas_resource_provider.h include from html_video_element.h.
    
    Bug: 352263194
    Change-Id: Ie0a3b2116268893936dbb37633d98a34e677d96d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7231666
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1555317}

--- third_party/blink/renderer/platform/graphics/canvas_snapshot_provider.h
+++ third_party/blink/renderer/platform/graphics/canvas_snapshot_provider.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/function_ref.h"
 #include "third_party/blink/renderer/platform/graphics/image_orientation.h"
+#include "third_party/blink/renderer/platform/graphics/static_bitmap_image.h"
 #include "ui/gfx/geometry/size.h"
 
 namespace blink {
@@ -27,6 +28,7 @@ class PLATFORM_EXPORT CanvasSnapshotProvider {
   virtual gfx::ColorSpace GetColorSpace() const = 0;
   virtual SkAlphaType GetAlphaType() const = 0;
   virtual bool IsValid() const = 0;
+  virtual bool IsGpuContextLost() const = 0;
   virtual bool IsExternalBitmapProvider() const { return false; }
 };
 
