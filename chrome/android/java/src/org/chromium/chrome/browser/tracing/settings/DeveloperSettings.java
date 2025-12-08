50d85003e8fecbe2e054b4389e7fd0856ad47383
Jinsuk Kim <jinsukkim@chromium.org>
2025-12-08 12:10:46 -0800

    [Settings Search] Index dynamic settings config for DeveloperSettings
    Hides the information text that should be hidden on beta/stable build
    from the search index.
    
    Bug: 444470792
    Change-Id: I68854eeb3fda5063efe909e62f1d8823b466ac38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7213097
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Moe Adel <adelm@google.com>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1555657}

--- chrome/android/java/src/org/chromium/chrome/browser/tracing/settings/DeveloperSettings.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tracing/settings/DeveloperSettings.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.tracing.settings;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import org.chromium.base.ResettersForTesting;
@@ -19,8 +20,10 @@ import org.chromium.chrome.R;
 import org.chromium.chrome.browser.preferences.ChromePreferenceKeys;
 import org.chromium.chrome.browser.preferences.ChromeSharedPreferences;
 import org.chromium.chrome.browser.settings.ChromeBaseSettingsFragment;
+import org.chromium.chrome.browser.settings.search.ChromeBaseSearchIndexProvider;
 import org.chromium.components.browser_ui.settings.EmbeddableSettingsPage;
 import org.chromium.components.browser_ui.settings.SettingsUtils;
+import org.chromium.components.browser_ui.settings.search.SettingsIndexData;
 
 /** Settings fragment containing preferences aimed at Chrome and web developers. */
 @NullMarked
@@ -59,9 +62,13 @@ public class DeveloperSettings extends ChromeBaseSettingsFragment
     public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String s) {
         SettingsUtils.addPreferencesFromResource(this, R.xml.developer_preferences);
 
-        if (VersionInfo.isBetaBuild() || VersionInfo.isStableBuild()) {
+        if (shouldRemoveBetaStableHint()) {
             getPreferenceScreen().removePreference(findPreference(UI_PREF_BETA_STABLE_HINT));
         }
+    }
+
+    private static boolean shouldRemoveBetaStableHint() {
+        return VersionInfo.isBetaBuild() || VersionInfo.isStableBuild();
     }
 
     @Override
@@ -78,4 +85,16 @@ public class DeveloperSettings extends ChromeBaseSettingsFragment
     public @Nullable String getMainMenuKey() {
         return "developer";
     }
+
+    public static final ChromeBaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new ChromeBaseSearchIndexProvider(
+                    DeveloperSettings.class.getName(), R.xml.developer_preferences) {
+
+                @Override
+                public void updateDynamicPreferences(Context context, SettingsIndexData indexData) {
+                    if (DeveloperSettings.shouldRemoveBetaStableHint()) {
+                        indexData.removeEntry(getUniqueId(UI_PREF_BETA_STABLE_HINT));
+                    }
+                }
+            };
 }
