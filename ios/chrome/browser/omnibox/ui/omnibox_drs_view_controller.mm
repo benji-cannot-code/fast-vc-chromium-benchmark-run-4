2e63e922567a62123780682e5ea8f86ba47ad0b5
Louis Romero <lpromero@google.com>
2025-12-08 02:46:12 -0800

    Fix an incorrect OmniboxPopupPresenterDelegate call
    This proxied call called the open method instead of the close method.
    
    Bug: none
    Change-Id: I3cef443004fcf2cef1b6be4db3a63c1c65906a72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7233672
    Auto-Submit: Louis Romero <lpromero@google.com>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1555368}

--- ios/chrome/browser/omnibox/ui/omnibox_drs_view_controller.mm
+++ ios/chrome/browser/omnibox/ui/omnibox_drs_view_controller.mm
@@ -238,7 +238,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)popupDidCloseForPresenter:(OmniboxPopupPresenter*)presenter {
-  [self.proxiedPresenterDelegate popupDidOpenForPresenter:presenter];
+  [self.proxiedPresenterDelegate popupDidCloseForPresenter:presenter];
   [self close];
 }
 
