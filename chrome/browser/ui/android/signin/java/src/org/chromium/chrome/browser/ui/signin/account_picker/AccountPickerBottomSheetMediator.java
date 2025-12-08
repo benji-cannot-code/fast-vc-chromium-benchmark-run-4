61b9af0dc454f74d2d36ef75cdecd226340c2499
Anton Goncharenko <agonch@google.com>
2025-12-08 11:04:32 -0800

    [Signin][Android] Log SIGNIN_ABORTED event if seamless signin abandoned
    Relaxes the assertion in SigninFlowTimestampsLogger.recordTimestamp to
    allow SIGNIN_ABORTED events to be logged even if the management notice
    was displayed but not explicitly accepted. This ensures accurate logging
    of sign-in flow abandonment, particularly when users cancel at the
    management notice step.
    
    Bug: 437038737
    Change-Id: I2429fb9790bae5e2b4ec8422a5afb2623d65beb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7209378
    Commit-Queue: Anton Goncharenko <agonch@google.com>
    Reviewed-by: Tanmoy Mollik <triploblastic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1555601}

--- chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/account_picker/AccountPickerBottomSheetMediator.java
+++ chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/account_picker/AccountPickerBottomSheetMediator.java
@@ -690,7 +690,9 @@ public class AccountPickerBottomSheetMediator
         mSelectedAccount = null;
         mDefaultAccount = null;
         mRequestDisplayBottomSheet = null;
-        // TODO(crbug.com/437038737): Log Event.SIGNIN_ABORTED timestamp.
+        if (mSigninTimestampsLogger != null) {
+            mSigninTimestampsLogger.recordTimestamp(Event.SIGNIN_ABORTED);
+        }
 
         mAccountPickerDelegate.onSeamlessSigninAbandoned();
     }
