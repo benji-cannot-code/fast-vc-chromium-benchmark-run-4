857d09471029b75d4684990277247df695523287
Kaan Alsan <alsan@chromium.org>
2025-12-08 15:52:06 -0800

    Refactor: Use TabSlotView* instead of Tab* in TabDragContext and TabSlotController interfaces.
    This change updates several methods in TabDragContext and TabSlotController to accept the base class TabSlotView* instead of the derived class Tab*. Implementations in TabStrip are updated to cast the TabSlotView* to Tab* where necessary. This promotes better abstraction and allows for more flexible usage of TabSlotView objects.
    
    Change-Id: Ia1011bce6f3518eddf5bdec690f403a6a9be75e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7231173
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1555792}

--- chrome/browser/ui/views/tabs/fake_tab_slot_controller.cc
+++ chrome/browser/ui/views/tabs/fake_tab_slot_controller.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/tabs/tab_container.h"
 #include "chrome/browser/ui/views/tabs/tab_strip_controller.h"
+#include "ui/views/view_utils.h"
 
 FakeTabSlotController::FakeTabSlotController(
     TabStripController* tab_strip_controller)
@@ -24,19 +25,19 @@ void FakeTabSlotController::ToggleTabGroupCollapsedState(
     const tab_groups::TabGroupId group,
     ToggleTabGroupCollapsedStateOrigin origin) {}
 
-bool FakeTabSlotController::IsActiveTab(const Tab* tab) const {
-  return active_tab_ == tab;
+bool FakeTabSlotController::IsActiveTab(const TabSlotView* tab) const {
+  return active_tab_ == views::AsViewClass<Tab>(tab);
 }
 
-bool FakeTabSlotController::IsTabSelected(const Tab* tab) const {
+bool FakeTabSlotController::IsTabSelected(const TabSlotView* tab) const {
   return false;
 }
 
-bool FakeTabSlotController::IsTabPinned(const Tab* tab) const {
+bool FakeTabSlotController::IsTabPinned(const TabSlotView* tab) const {
   return false;
 }
 
-bool FakeTabSlotController::IsTabFirst(const Tab* tab) const {
+bool FakeTabSlotController::IsTabFirst(const TabSlotView* tab) const {
   return false;
 }
 
