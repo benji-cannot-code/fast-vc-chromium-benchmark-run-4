8f2d4ac1408f6943d4c0a578beecf322b3667cef
Daniel Rubery <drubery@chromium.org>
2025-12-08 09:00:30 -0800

    Support "debug_redact" in ToValue
    This option already implies that field is sensitive and should not be
    displayed in debug formats. So respect that in ToValue and refuse to
    serialize it. Instead, we include the string "debug_redacted" as the value.
    
    Fixed: 460276925
    Change-Id: Ic3750276c4b4d8d0d96f21cc2b70ebf16a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7234532
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1555519}

--- components/proto_extras/proto_extras_lib.h
+++ components/proto_extras/proto_extras_lib.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base64.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/strings/stringprintf.h"
 #include "base/values.h"
 
 namespace absl {
@@ -57,6 +58,16 @@ std::string ToNumericTypeForValue(T value) {
 
 // Convert an absl::Cord of bytes into a string.
 std::string Base64EncodeCord(const absl::Cord& cord);
+
+// Serialization for fields marked `debug_redact`
+template <typename T>
+std::string ToValueForDebugRedact(const T& value) {
+  if constexpr (std::is_same_v<T, std::string>) {
+    return base::StringPrintf("<%zu-byte secret>", value.size());
+  } else {
+    return "<secret>";
+  }
+}
 
 }  // namespace proto_extras
 
