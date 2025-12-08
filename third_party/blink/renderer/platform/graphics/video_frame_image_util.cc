d5e58c51f2d5280dc5049c71a41fa29ed879953d
Colin Blundell <blundell@chromium.org>
2025-12-08 01:03:33 -0800

    Blink: Have CreateResourceProviderForVideoFrame return snapshot provider
    Changes this method to return the created object as a
    CanvasSnapshotProvider now that all clients have been converted. I
    leave renaming for followup to limit scope here.
    
    Bug: 352263194
    Change-Id: I17874e8383f1d7f2c780fc2e1f3b6f785da61a7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7231669
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1555329}

--- third_party/blink/renderer/platform/graphics/video_frame_image_util.cc
+++ third_party/blink/renderer/platform/graphics/video_frame_image_util.cc
@@ -208,7 +208,7 @@ scoped_refptr<viz::RasterContextProvider> GetRasterContextProvider() {
       wrapper->ContextProvider().RasterContextProvider());
 }
 
-std::unique_ptr<CanvasResourceProvider> CreateResourceProviderForVideoFrame(
+std::unique_ptr<CanvasSnapshotProvider> CreateResourceProviderForVideoFrame(
     gfx::Size size,
     viz::SharedImageFormat format,
     SkAlphaType alpha_type,
