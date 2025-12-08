77c603b09650b5a42b39510eaf8684a81e114e0e
Fiaz Muhammad <mfiaz@google.com>
2025-12-08 08:25:00 -0800

    [Persist] Fix Empty View margins for smaller screens
    - Add margins to move empty view beneath Archived Message Card.
    - This is also required to stop Empty View from stealing touch events
    	from the Archived Message Card.
    - Before: https://drive.google.com/file/d/1KpEOA_txoxW2xcN0uPy_KyzFcdogOBS9/view?usp=drive_link&resourcekey=0-Q1xhu43JIbmdyiFaq5tXTQ
    - After: https://drive.google.com/file/d/1ZCnIyvcf4JuA9QipZ_Bummiaw8nxpUnL/view?usp=drive_link&resourcekey=0-bH2KsnLb-PS9OkmMXXYSFQ
    
    Bug: 465759315
    Change-Id: I2db61e73f67d3f51f3a0d1c01e11e0c47cffa0b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7233992
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1555497}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherMessageManager.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherMessageManager.java
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.tasks.tab_management;
 
 import static org.chromium.build.NullUtil.assumeNonNull;
+import static org.chromium.chrome.browser.tasks.tab_management.MessageCardViewProperties.MESSAGE_TYPE;
+import static org.chromium.chrome.browser.tasks.tab_management.TabSwitcherMessageManager.MessageType.ARCHIVED_TABS_MESSAGE;
 import static org.chromium.chrome.browser.tasks.tab_management.UiTypeHelper.messageTypeToUiType;
 
 import android.app.Activity;
@@ -709,5 +711,11 @@ public class TabSwitcherMessageManager {
         TabGroupModelFilter tabGroupModelFilter = mCurrentTabGroupModelFilterSupplier.get();
         assumeNonNull(tabGroupModelFilter);
         return assumeNonNull(tabGroupModelFilter.getTabModel().getProfile());
+    }
+
+    /** Check to see if a {@link TabListModel} only contains the Archived Message card. */
+    public static boolean isOnlyArchivedMsg(TabListModel model) {
+        return model.size() == 1
+                && model.get(0).model.containsKeyEqualTo(MESSAGE_TYPE, ARCHIVED_TABS_MESSAGE);
     }
 }
