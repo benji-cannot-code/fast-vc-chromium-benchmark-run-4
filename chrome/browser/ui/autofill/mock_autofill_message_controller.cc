bb4a55a2eee34a294922587476a7ac6bc3a13b88
Timofey Chudakov <tchudakov@google.com>
2025-12-08 05:19:37 -0800

    [AutofillAi - M4] Move MockAutofillMessageController to a separate file.
    Bypass-Check-License: File content was moved.
    Bug: 460410690
    Change-Id: If132067fc48be1dad6721becaec128d39d19ef4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7227461
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1555414}

---
+++ chrome/browser/ui/autofill/mock_autofill_message_controller.cc
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/autofill/mock_autofill_message_controller.h"
+
+namespace autofill {
+
+MockAutofillMessageController::MockAutofillMessageController() = default;
+
+MockAutofillMessageController::~MockAutofillMessageController() = default;
+
+}  // namespace autofill
