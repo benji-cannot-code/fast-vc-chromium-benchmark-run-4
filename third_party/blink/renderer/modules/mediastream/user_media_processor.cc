58a79d29801d5f581bc2d441ffcb3d25053b1985
Palak Agarwal <agpalak@chromium.org>
2025-12-08 06:26:05 -0800

    Add new specific errors based on where they are triggered
    Bug: 453600255
    Change-Id: Ic5656e8e10ed1981aa6d0038c09a2e731e90ac33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7213152
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Palak Agarwal <agpalak@chromium.org>
    Reviewed-by: Tove Petersson <tovep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1555440}

--- third_party/blink/renderer/modules/mediastream/user_media_processor.cc
+++ third_party/blink/renderer/modules/mediastream/user_media_processor.cc
@@ -189,6 +189,12 @@ const char* MediaStreamRequestResultToString(MediaStreamRequestResult value) {
       return "INVALID_EXTENSION_TYPE_REQUEST";
     case MediaStreamRequestResult::CAPTURED_TAB_DESTROYED:
       return "CAPTURED_TAB_DESTROYED";
+    case MediaStreamRequestResult::SAFE_BROWSING_OBSERVER:
+      return "SAFE_BROWSING_OBSERVER";
+    case MediaStreamRequestResult::CAPTURE_NOT_ALLOWED_FOR_LONG_DOMAINS:
+      return "CAPTURE_NOT_ALLOWED_FOR_LONG_DOMAINS";
+    case MediaStreamRequestResult::CAPTURE_FROM_BACKGROUND_PAGE_ON_MAC:
+      return "CAPTURE_FROM_BACKGROUND_PAGE_ON_MAC";
   }
   NOTREACHED();
 }
@@ -347,6 +353,7 @@ String ErrorCodeToString(MediaStreamRequestResult result) {
     case MediaStreamRequestResult::CAPTURE_NOT_ALLOWED_BY_POLICY:
     case MediaStreamRequestResult::PERMISSION_DENIED_BY_EMBEDDER_CONTEXT:
     case MediaStreamRequestResult::DLP_PERMISSION_DENIED:
+    case MediaStreamRequestResult::SAFE_BROWSING_OBSERVER:
       return "Permission denied";
     case MediaStreamRequestResult::PERMISSION_DISMISSED:
       return "Permission dismissed";
@@ -356,6 +363,8 @@ String ErrorCodeToString(MediaStreamRequestResult result) {
     case MediaStreamRequestResult::INVALID_DEVICE_TYPE_REQUEST:
     case MediaStreamRequestResult::INVALID_EXTENSION_TYPE_REQUEST:
     case MediaStreamRequestResult::CAPTURE_NOT_ENABLED:
+    case MediaStreamRequestResult::CAPTURE_NOT_ALLOWED_FOR_LONG_DOMAINS:
+    case MediaStreamRequestResult::CAPTURE_FROM_BACKGROUND_PAGE_ON_MAC:
       return "Invalid state";
     case MediaStreamRequestResult::NO_HARDWARE:
       return "Requested device not found";
