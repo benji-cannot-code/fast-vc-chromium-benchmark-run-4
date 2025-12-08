60103273a52e59b5d0fd7b0c2f46b6c4ac1797f6
Yurii Lahodiuk <ylahodiuk@google.com>
2025-12-08 15:24:27 -0800

    webauthn: React to enclave state changes in GPM Enclave Controller
    This change fixes the following issues: "Chrome crashes after unlocking
    passkeys in a different browser tab" (crbug.com/465139934), "Chrome on
    Linux crashes after removing passkey access in GPM settings"
    (crbug.com/445161563).
    
    The idea of the fix: We introduce an indicator of staleness of the state
    of GPM Enclave Controller. GPM Enclave Controller observes state changes
    of Enclave Manager and activates the staleness indicator in the
    following cases:
    - Successful opportunistic key retrieval.
    - Successful key retrieval from the other tab.
    When a user returns to an old tab and tries to perform (or to resume) a
    passkey operation, GPM Enclave Controller will decide whether refreshing
    the state is needed. The state refresh is needed either if a staleness
    indicator is true, or if GPM Enclave Controller thinks that the account
    is ready but Enclave Manager is not ready.
    
    The change is protected by a flag
    `kWebAuthnEnableRefreshingStateOfGpmEnclaveController` (enabled by
    default).
    
    As a part of this change we also introduced the following regression
    tests:
    
    1) Starting passkey creation operation (when passkeys are locked), and
    performing unlock in a separate tab via explicit key retrieval, and
    resuming the passkey creation operation.
    
    2) Starting passkey creation operation (when passkeys are locked), and
    performing unlock in a separate tab via opportunistic key retrieval, and
    resuming the passkey creation operation.
    
    3) Starting passkey creation operation (when passkeys are unlocked), and
    performing Enclave Manager un-registration, and resuming the passkey
    creation operation.
    
    Also, in the scope of this CL we moved `EnclaveKeysWaiter` to
    `enclave_keys_waiter.h` because it is being used now in both:
    `enclave_authenticator_browsertest_base.cc` and
    `enclave_manager_unittest.cc`.
    
    Bug: 465139934, 445161563
    Change-Id: I5cf5039af4e226cd265e28e09dc5baf82b154675
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7213582
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Commit-Queue: Yurii Lahodiuk <ylahodiuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1555779}

---
+++ chrome/browser/webauthn/enclave_keys_waiter.cc
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/webauthn/enclave_keys_waiter.h"
+
+#include "base/functional/callback.h"
+#include "base/run_loop.h"
+
+EnclaveKeysWaiter::EnclaveKeysWaiter(EnclaveManager* enclave_manager)
+    : enclave_manager_(enclave_manager) {
+  enclave_manager->AddObserver(this);
+}
+
+EnclaveKeysWaiter::~EnclaveKeysWaiter() {
+  enclave_manager_->RemoveObserver(this);
+}
+
+EnclaveManager::OutOfContextRecoveryOutcome EnclaveKeysWaiter::Wait() {
+  run_loop_->Run();
+  return outcome_;
+}
+
+void EnclaveKeysWaiter::OnOutOfContextRecoveryCompletion(
+    EnclaveManager::OutOfContextRecoveryOutcome outcome) {
+  run_loop_->Quit();
+  outcome_ = outcome;
+}
