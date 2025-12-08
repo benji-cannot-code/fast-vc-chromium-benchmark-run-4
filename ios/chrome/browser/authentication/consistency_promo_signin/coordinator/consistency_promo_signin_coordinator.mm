3f99545ff83b230b7fcc0b8a192fd5ff64815e82
Arthur Milchior <arthurmilchior@google.com>
2025-12-08 06:21:35 -0800

    [iOS]Avoid having two add account flow from account menu
    Up to iOS 18, the add account flow can disappear silently without call
    back being called. We previously assumed we dealt with that by
    stopping the previous coordinator before starting a new one if we
    detected a second tap. This was not sufficient.
    
    Given that both Add Account and Reauth coordinators uses the same
    InternalSystemIdentityInteractionManager with the same lock, when
    starting one coordinator we should have ensured that both coordinators
    are stopped.
    
    For the sake of code simplicity, the consistency coordinator now
    ensure that all children coordinators are stopped before opening a new
    coordinator, this avoid the need to consider two special cases.
    
    Bug: 424516331
    Change-Id: I4b6fcac5ebdd3ee602fa8ea5953375eaa5d1e5b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7231996
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1555438}

--- ios/chrome/browser/authentication/consistency_promo_signin/coordinator/consistency_promo_signin_coordinator.mm
+++ ios/chrome/browser/authentication/consistency_promo_signin/coordinator/consistency_promo_signin_coordinator.mm
@@ -297,7 +297,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     // In case of double tap, let the first reauth proceed.
     return;
   }
-  [self.reauthCoordinator stop];
+  [self stopAddAccountFlows];
   self.reauthCoordinator = [[SigninReauthCoordinator alloc]
       initWithBaseViewController:self.navigationController
                          browser:self.browser
@@ -336,6 +336,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self.reauthCoordinator stop];
   self.reauthCoordinator = nil;
 }
+
+// Stop children coordinators that goes through the add account flow.
+- (void)stopAddAccountFlows {
+  [self stopReauthCoordinator];
+  [self stopAddAccountCoordinatorAnimated:NO];
+}
+
 // Does cleanup (metrics and remove coordinator) once the add-account flow is
 // finished. If `hasAccounts == NO` and `signinResult` is successful , the
 // function immediately signs in to Chrome with the identity acquired from the
@@ -381,7 +388,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // In case of double-tap, we must stop the already started coordinator. This
   // may occur because, up to iOS 18, the view may have disappeared without
   // calling the signin completion. See crbug.com/395959814
-  [self.addAccountCoordinator stop];
+  [self stopAddAccountFlows];
   if (hasAccounts) {
     RecordConsistencyPromoUserAction(
         signin_metrics::AccountConsistencyPromoAction::ADD_ACCOUNT_STARTED,
