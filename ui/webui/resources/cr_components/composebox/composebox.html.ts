b55b8f45620e290c6202ea9dc525e287578674ff
Duncan Mercer <mercerd@google.com>
2025-12-08 15:34:49 -0800

    [AIM] Make Composebox Dropdown support max # of suggestions
    This CL limits the number of visible suggestions in the Lens side panel
    composebox by calculating the maximum number of autocomplete suggestions
    that can be displayed in the Lens side panel's composebox dropdown based
    on the current window height and the heights of other elements. The
    `cr-composebox-dropdown` is updated to only render suggestions up to
    this calculated maximum. The calculation is updated on window resize and
    when the composebox height changes. The selecting a match logic was
    updated to in `cr-composebox-dropdown` to only allow selecting the
    visible matches.
    
    Bug: b:465801460
    Change-Id: I8d3ba185ca72f1038b7171624be72f2dda4c078b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7233790
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1555781}

--- ui/webui/resources/cr_components/composebox/composebox.html.ts
+++ ui/webui/resources/cr_components/composebox/composebox.html.ts
@@ -105,6 +105,7 @@ export function getHtml(this: ComposeboxElement) {
             role="listbox"
             .result="${this.result_}"
             .selectedMatchIndex="${this.selectedMatchIndex_}"
+            .maxSuggestions="${this.maxSuggestions}"
             @selected-match-index-changed="${this.onSelectedMatchIndexChanged_}"
             @match-focusin="${this.onMatchFocusin_}"
             @match-click="${this.onMatchClick_}"
--- ui/webui/resources/cr_components/composebox/composebox.ts
+++ ui/webui/resources/cr_components/composebox/composebox.ts
@@ -163,9 +163,11 @@ export class ComposeboxElement extends I18nMixinLit
       entrypointName: {type: String},
       transcript_: {type: String},
       receivedSpeech_: {type: Boolean},
+      maxSuggestions: {type: Number},
     };
   }
 
+  accessor maxSuggestions: number|null = null;
   accessor ntpRealboxNextEnabled: boolean = false;
   accessor searchboxLayoutMode: string = '';
   accessor carouselOnTop_: boolean = false;
@@ -335,7 +337,7 @@ export class ComposeboxElement extends I18nMixinLit
         // If the selected match is the default match (typing) the input will
         // already have been set by handleInput.
         if (!(this.selectedMatchIndex_ === 0 &&
-            this.selectedMatch_.allowedToBeDefaultMatch)) {
+              this.selectedMatch_.allowedToBeDefaultMatch)) {
           // Update the input.
           const text = this.selectedMatch_.fillIntoEdit;
           assert(text);
@@ -394,7 +396,7 @@ export class ComposeboxElement extends I18nMixinLit
 
     // If the composebox is not submittable or it is already expanded, do not
     // trigger the animation.
-    if(this.expanding_ && !this.submitEnabled_) {
+    if (this.expanding_ && !this.submitEnabled_) {
       requestAnimationFrame(() => {
         this.animationState = GlowAnimationState.EXPANDING;
       });
@@ -421,8 +423,9 @@ export class ComposeboxElement extends I18nMixinLit
     return this.$.matches;
   }
 
-  protected initializeState_(text: string = '', files: ContextualUpload[] = [],
-                             mode: ComposeboxMode = ComposeboxMode.DEFAULT) {
+  protected initializeState_(
+      text: string = '', files: ContextualUpload[] = [],
+      mode: ComposeboxMode = ComposeboxMode.DEFAULT) {
     if (text) {
       this.input_ = text;
       this.lastQueriedInput_ = text;
@@ -535,14 +538,14 @@ export class ComposeboxElement extends I18nMixinLit
   }
 
   protected async addFileContext_(e: CustomEvent<{
-      files: File[],
-      onContextAdded: (files: Map<UnguessableToken, ComposeboxFile>) => void,
+    files: File[],
+    onContextAdded: (files: Map<UnguessableToken, ComposeboxFile>) => void,
   }>) {
     const composeboxFiles: Map<UnguessableToken, ComposeboxFile> = new Map();
     for (const file of e.detail.files) {
       const fileBuffer = await file.arrayBuffer();
       const bigBuffer:
-            BigBuffer = {bytes: Array.from(new Uint8Array(fileBuffer))};
+          BigBuffer = {bytes: Array.from(new Uint8Array(fileBuffer))};
       const {token} = await this.searchboxHandler_.addFileContext(
           {
             fileName: file.name,
@@ -557,7 +560,8 @@ export class ComposeboxElement extends I18nMixinLit
         uuid: token,
         name: file.name,
         dataUrl: null,
-        objectUrl: file.type.includes('image') ? URL.createObjectURL(file) : null,
+        objectUrl: file.type.includes('image') ? URL.createObjectURL(file) :
+                                                 null,
         type: file.type,
         status: FileUploadStatus.kNotUploaded,
         url: null,
@@ -595,8 +599,11 @@ export class ComposeboxElement extends I18nMixinLit
   }
 
   protected async addTabContext_(e: CustomEvent<{
-      id: number, title: string, url: Url, delayUpload: boolean,
-      onContextAdded: (file: ComposeboxFile) => void,
+    id: number,
+    title: string,
+    url: Url,
+    delayUpload: boolean,
+    onContextAdded: (file: ComposeboxFile) => void,
   }>) {
     const {token} = await this.searchboxHandler_.addTabContext(
         e.detail.id, e.detail.delayUpload);
@@ -1107,8 +1114,7 @@ export class ComposeboxElement extends I18nMixinLit
       }
     } else if (file) {
       if (status === FileUploadStatus.kProcessingSuggestSignalsReady &&
-          this.showZps &&
-          !file.type.includes('image')) {
+          this.showZps && !file.type.includes('image')) {
         // Query autocomplete to get contextual suggestions for files.
         this.queryAutocomplete(/* clearMatches= */ true);
       }
