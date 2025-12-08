f58488fb61fbf26293cfebe53812a8796cee0b18
Simon Zünd <szuend@chromium.org>
2025-12-08 01:36:08 -0800

    [devtools] Re-enable call stack sidebar web tests
    R=alexrudenko@chromium.org
    
    Bug: 407751574
    Fixed: 40912755
    Change-Id: Idb02629eee79da243dd62b10158fcf1f2bed0dbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7232073
    Auto-Submit: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1555340}

--- third_party/blink/web_tests/http/tests/devtools/sources/debugger-ui/async-call-stack-async-function.js
+++ third_party/blink/web_tests/http/tests/devtools/sources/debugger-ui/async-call-stack-async-function.js
@@ -37,9 +37,7 @@ import * as SourcesModule from 'devtools/panels/sources/sources.js';
 
   SourcesTestRunner.startDebuggerTestPromise(/* quiet */ true)
       .then(() => SourcesTestRunner.runTestFunctionAndWaitUntilPausedPromise())
-      .then(
-          () => TestRunner.addSnifferPromise(
-              SourcesModule.CallStackSidebarPane.CallStackSidebarPane.prototype, 'updatedForTest'))
+      .then(() => SourcesModule.CallStackSidebarPane.CallStackSidebarPane.instance().updateComplete)
       .then(() => dumpCallStackSidebarPane())
       .then(() => SourcesTestRunner.completeDebuggerTest());
 
