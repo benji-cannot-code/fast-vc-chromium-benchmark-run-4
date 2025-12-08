2a6c72e2081e94e142fe4bc3d5bc04004be1e72c
Palak Agarwal <agpalak@chromium.org>
2025-12-08 06:22:02 -0800

    Add error for when capture is not enabled
    Bug: 453600255
    Change-Id: Ic7cc4a84ad583ecd1ca03559a2353a564d4ff5b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7213562
    Reviewed-by: Tove Petersson <tovep@chromium.org>
    Commit-Queue: Palak Agarwal <agpalak@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1555439}

--- content/browser/renderer_host/media/media_stream_manager.cc
+++ content/browser/renderer_host/media/media_stream_manager.cc
@@ -351,6 +351,8 @@ const char* RequestResultToString(
       return "INVALID_EXTENSION_TYPE_REQUEST";
     case blink::mojom::MediaStreamRequestResult::CAPTURED_TAB_DESTROYED:
       return "CAPTURED_TAB_DESTROYED";
+    case blink::mojom::MediaStreamRequestResult::CAPTURE_NOT_ENABLED:
+      return "CAPTURE_NOT_ENABLED";
   }
   NOTREACHED();
 }
