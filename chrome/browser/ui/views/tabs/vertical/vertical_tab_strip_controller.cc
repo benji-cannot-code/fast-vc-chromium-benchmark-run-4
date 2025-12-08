70c2a01793833b9a045577fbef37bd1612d68646
Eshwar Stalin <estalin@chromium.org>
2025-12-08 11:25:53 -0800

    Code cleanup for TabContextMenuController
    Introducing a delegate interface instead of using repeating callbacks
    and removing unused GetIndexFromMojomTab function.
    
    Change-Id: I08798c5bec0db474114e4c28df5b4793c0bc635e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7233448
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: Charles Meng <charlesmeng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1555620}

--- chrome/browser/ui/views/tabs/vertical/vertical_tab_strip_controller.cc
+++ chrome/browser/ui/views/tabs/vertical/vertical_tab_strip_controller.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/tabs/tab_context_menu_controller.h"
 #include "chrome/browser/ui/views/tabs/vertical/tab_collection_node.h"
-#include "components/browser_apis/tab_strip/tab_strip_api.mojom.h"
 #include "components/tabs/public/tab_collection_types.h"
 #include "components/tabs/public/tab_interface.h"
 
@@ -56,22 +55,8 @@ void VerticalTabStripController::ShowContextMenuForNode(
     return;
   }
 
-  context_menu_controller_ = std::make_unique<TabContextMenuController>(
-      base::BindRepeating(
-          &VerticalTabStripController::IsContextMenuCommandChecked,
-          base::Unretained(this)),
-      base::BindRepeating(
-          &VerticalTabStripController::IsContextMenuCommandEnabled,
-          base::Unretained(this), tab_index.value()),
-      base::BindRepeating(
-          &VerticalTabStripController::IsContextMenuCommandAlerted,
-          base::Unretained(this)),
-      base::BindRepeating(
-          &VerticalTabStripController::ExecuteContextMenuCommand,
-          base::Unretained(this), tab_index.value()),
-      base::BindRepeating(
-          &VerticalTabStripController::GetContextMenuAccelerator,
-          base::Unretained(this)));
+  context_menu_controller_ =
+      std::make_unique<TabContextMenuController>(tab_index.value(), this);
 
   auto model = menu_model_factory_->Create(
       context_menu_controller_.get(),
@@ -81,16 +66,6 @@ void VerticalTabStripController::ShowContextMenuForNode(
   context_menu_controller_->LoadModel(std::move(model));
 
   context_menu_controller_->RunMenuAt(point, source_type, source->GetWidget());
-}
-
-std::optional<int> VerticalTabStripController::GetIndexFromMojomTab(
-    const tabs_api::mojom::Tab& mojom_tab) {
-  const auto tab_handle = mojom_tab.id.ToTabHandle();
-
-  const int index = model_->GetIndexOfTab(tab_handle->Get());
-
-  return (index != TabStripModel::kNoTab) ? std::optional<int>(index)
-                                          : std::nullopt;
 }
 
 bool VerticalTabStripController::IsContextMenuCommandChecked(
