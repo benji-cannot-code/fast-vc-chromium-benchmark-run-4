c124b2ac73adb7f43ac6128fbc187715ce4d721f
Christian Dullweber <dullweber@chromium.org>
2025-12-08 09:34:48 -0800

    Legion: Add prototype implementation for ZSS requests
    Add a base::Feature for ZSS to use the legion backend and provide
    a prototype implementation to generate suggestions until
    pre/postprocessing is implemented.
    
    Bug: 460052805
    Change-Id: If88bc481f5488453455c917486ce049226ebb405
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7209479
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Oleh Lamzin <lamzin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1555537}

--- chrome/browser/contextual_cueing/contextual_cueing_features.h
+++ chrome/browser/contextual_cueing/contextual_cueing_features.h
@@ -13,6 +13,7 @@ namespace contextual_cueing {
 
 BASE_DECLARE_FEATURE(kContextualCueing);
 BASE_DECLARE_FEATURE(kGlicZeroStateSuggestions);
+BASE_DECLARE_FEATURE(kZeroStateSuggestionsUseLegion);
 
 // Whether contextual cueing is enabled.
 //
