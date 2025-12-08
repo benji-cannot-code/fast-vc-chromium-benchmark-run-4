322011af57e52ddc4ee77b8ea95a1f59acac5069
Gyuyoung Kim <gyuyoung@igalia.com>
2025-12-07 18:39:31 -0800

    [iOS Blink] Include video_capture_metrics_mac.h in video_capture_device_avfoundation.mm
    https://crrev.com/c/7212942 introduced the LogAVCaptureDeviceInfo()
    function, but video_capture_device_avfoundation.mm only included the
    corresponding header for the Mac port. To resolve the build failure in
    the iOS Blink port, this CL adds the header using an IS_APPLE guard.
    
    Bug: 461717105
    Change-Id: I311f319bef74d9fbb2e6094d622fcf22c14bb937
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7227215
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Reviewed-by: Miguel Casas-Sanchez <mcasas@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1555258}

--- media/capture/video/apple/video_capture_device_avfoundation.mm
+++ media/capture/video/apple/video_capture_device_avfoundation.mm
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/capture/video_capture_types.h"
 #include "ui/gfx/geometry/size.h"
 
-#if BUILDFLAG(IS_MAC)
+#if BUILDFLAG(IS_APPLE)
 #import "media/capture/video/mac/video_capture_metrics_mac.h"
 #endif
 
