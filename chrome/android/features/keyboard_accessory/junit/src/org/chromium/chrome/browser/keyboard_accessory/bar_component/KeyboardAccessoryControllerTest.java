5881a6fb1686339d4f7f63605fcf0d67f59a8221
Piotr Kotynia <piotrkotynia@google.com>
2025-12-08 02:48:54 -0800

    [Android] Delete "dismiss" for dynamically positioned keyboard accessory
    "Dismiss" item was only planned for the first version of revamp. If
    AUTOFILL_ANDROID_KEYBOARD_ACCESSORY_DYNAMIC_POSITIONING flag is on, it
    shouldn't be added.
    
    Bug: 458610269
    Change-Id: I9880af05c3dd5f6448cbd5a2facd21c7a1021bbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7232539
    Reviewed-by: Karol Sygiet <sygiet@google.com>
    Commit-Queue: Piotr Kotynia <piotrkotynia@google.com>
    Cr-Commit-Position: refs/heads/main@{#1555369}

--- chrome/android/features/keyboard_accessory/junit/src/org/chromium/chrome/browser/keyboard_accessory/bar_component/KeyboardAccessoryControllerTest.java
+++ chrome/android/features/keyboard_accessory/junit/src/org/chromium/chrome/browser/keyboard_accessory/bar_component/KeyboardAccessoryControllerTest.java
@@ -12,7 +12,9 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
@@ -51,6 +53,7 @@ import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.base.task.test.CustomShadowAsyncTask;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.Features;
+import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.chrome.browser.autofill.PersonalDataManager;
 import org.chromium.chrome.browser.autofill.PersonalDataManagerFactory;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
@@ -98,6 +101,7 @@ import java.util.function.Supplier;
         shadows = {CustomShadowAsyncTask.class})
 @Features.EnableFeatures({
     ChromeFeatureList.AUTOFILL_ANDROID_DESKTOP_KEYBOARD_ACCESSORY_REVAMP,
+    ChromeFeatureList.AUTOFILL_ANDROID_KEYBOARD_ACCESSORY_DYNAMIC_POSITIONING,
     ChromeFeatureList.AUTOFILL_ENABLE_KEYBOARD_ACCESSORY_CHIP_REDESIGN,
     ChromeFeatureList.AUTOFILL_ENABLE_KEYBOARD_ACCESSORY_CHIP_WIDTH_ADJUSTMENT,
 })
@@ -659,6 +663,7 @@ public class KeyboardAccessoryControllerTest {
     }
 
     @Test
+    @DisableFeatures(ChromeFeatureList.AUTOFILL_ANDROID_KEYBOARD_ACCESSORY_DYNAMIC_POSITIONING)
     public void testLargeFormFactorHasDismissButton() {
         when(mMockIsLargeFormFactorSupplier.get()).thenReturn(true);
 
@@ -671,6 +676,19 @@ public class KeyboardAccessoryControllerTest {
     }
 
     @Test
+    public void testLargeFormFactorDynamicPositioningHasNoDismissButton() {
+        when(mMockIsLargeFormFactorSupplier.get()).thenReturn(true);
+
+        mCoordinator.setSuggestions(List.of(mock(AutofillSuggestion.class)), mMockAutofillDelegate);
+
+        assertThat(mModel.get(BAR_ITEMS), contains(instanceOf(AutofillBarItem.class)));
+
+        assertThat(mModel.get(BAR_ITEMS_FIXED), not(hasItem(instanceOf(DismissBarItem.class))));
+        assertThat(mModel.get(BAR_ITEMS_FIXED), contains(instanceOf(SheetOpenerBarItem.class)));
+    }
+
+    @Test
+    @DisableFeatures(ChromeFeatureList.AUTOFILL_ANDROID_KEYBOARD_ACCESSORY_DYNAMIC_POSITIONING)
     public void testLargeFormFactorHasFixedItems() {
         when(mMockIsLargeFormFactorSupplier.get()).thenReturn(true);
         Provider<Action[]> generationProvider = new Provider<>(GENERATE_PASSWORD_AUTOMATIC);
