e659013c062b6d327be63262e1428693890b9004
Noam Rosenthal <nrosenthal@chromium.org>
2025-12-08 12:05:15 -0800

    Fix corner shape interpolation neutral value
    The neutral value should be 0 rather than round, allowing
    interpolation of the existing non-animated value with the keyframe
    value.
    
    Bug: 465806318
    Change-Id: I255cccebdb249e2dacd81af55cbea8f6f0b587a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7224151
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Noam Rosenthal <nrosenthal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1555652}

--- third_party/blink/renderer/core/animation/css_superellipse_interpolation_type.cc
+++ third_party/blink/renderer/core/animation/css_superellipse_interpolation_type.cc
@@ -161,7 +161,7 @@ InterpolationValue CSSSuperellipseInterpolationType::CreateNumberValue(
 InterpolationValue CSSSuperellipseInterpolationType::MaybeConvertNeutral(
     const InterpolationValue&,
     ConversionCheckers&) const {
-  return CreateNumberValue(Superellipse::Round());
+  return InterpolationValue(MakeGarbageCollected<InterpolableNumber>(0));
 }
 
 InterpolationValue CSSSuperellipseInterpolationType::MaybeConvertInitial(
