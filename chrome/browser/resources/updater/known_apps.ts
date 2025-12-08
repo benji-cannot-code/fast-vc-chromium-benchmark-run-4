e2db04badb98f96933fcb579692e9dee247bfdb9
Noah Rose Ledesma <noahrose@google.com>
2025-12-08 15:19:03 -0800

    Add a filter bar component
    Add an element to present a filter bar for the updater's event history
    log. Currently, this supports filtering by app, event type, update
    outcome, and date.
    
    Additionally, some style changes were made to the index to properly
    support light & dark modes.
    
    Bug: 456497443
    Change-Id: Id182c44edccd1e02a73eabc47dd8b8866a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7234472
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1555774}

---
+++ chrome/browser/resources/updater/known_apps.ts
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Utilities for accessing the set of apps known to this browser.
+ * Dynamic accessors over loadTimeData are used instead of constants to allow
+ * the set to be overridden in tests.
+ */
+
+import {loadTimeData} from '//resources/js/load_time_data.js';
+
+/**
+ * Produces a map of app names to their app IDs.
+ */
+export function getKnownApps(): Map<string, string[]> {
+  const numApps = loadTimeData.getInteger('numKnownApps');
+  const apps = new Map<string, string[]>();
+  for (let i = 0; i < numApps; i++) {
+    apps.set(
+        loadTimeData.getString(`knownAppName${i}`),
+        loadTimeData.getString(`knownAppIds${i}`).split(','));
+  }
+  return apps;
+}
+
+/**
+ * Produces a map of app IDs to app names.
+ */
+export function getKnownAppNamesById() {
+  return new Map<string, string>([...getKnownApps().entries()].flatMap(
+      ([appName, appIds]) => appIds.map((appId) => [appId, appName])));
+}
