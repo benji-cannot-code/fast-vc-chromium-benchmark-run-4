5058fe1d84d1cb90e99bbfb13cbb101c7b11cf82
Julia Przybytniowska <przybytniowska@google.com>
2025-12-08 09:38:51 -0800

    Organize imports in PromotionEligibility space
    BUG=b:436245479
    
    Change-Id: If9b838e6855a33d669f66f307880503649fd6c3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7228783
    Reviewed-by: Austin Zhang <austinzzr@google.com>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Julia Przybytniowska <przybytniowska@google.com>
    Cr-Commit-Position: refs/heads/main@{#1555542}

--- chrome/browser/ui/webui/policy/policy_ui_handler.cc
+++ chrome/browser/ui/webui/policy/policy_ui_handler.cc
@@ -56,7 +56,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/crx_file/id_util.h"
 #include "components/enterprise/browser/controller/browser_dm_token_storage.h"
 #include "components/enterprise/browser/controller/chrome_browser_cloud_management_controller.h"
-#include "components/enterprise/browser/promotion/promotion_eligibility_checker.h"
 #include "components/enterprise/browser/reporting/common_pref_names.h"
 #include "components/policy/core/browser/configuration_policy_handler_list.h"
 #include "components/policy/core/browser/policy_conversions.h"
