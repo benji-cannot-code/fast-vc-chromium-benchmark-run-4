648e3bfd183093f8e9b693b514bb9cf133459d7c
Vikram Pasupathy <vpasupathy@chromium.org>
2025-12-09 07:23:56 -0800

    cdm: Upgrade DCHECKS in testing code
    Guidance is to use CHECK instead of DCHECK, so in the cdm code, upgrade
    the tests that are using DCHECK to use CHECK.
    
    Bug: 406530495
    Change-Id: I5423a75069e7c45778a89f3523de3f5406b9cac2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6846365
    Commit-Queue: Vikram Pasupathy <vpasupathy@chromium.org>
    Reviewed-by: Feras Aldahlawi <frs@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1556118}

--- media/cdm/json_web_key_unittest.cc
+++ media/cdm/json_web_key_unittest.cc
@@ -23,7 +23,7 @@ class JSONWebKeyTest : public testing::Test {
   void ExtractJWKKeysAndExpect(const std::string& jwk,
                                bool expected_result,
                                size_t expected_number_of_keys) {
-    DCHECK(!jwk.empty());
+    CHECK(!jwk.empty());
     KeyIdAndKeyPairs keys;
     CdmSessionType session_type;
     EXPECT_EQ(expected_result,
@@ -34,7 +34,7 @@ class JSONWebKeyTest : public testing::Test {
   void ExtractSessionTypeAndExpect(const std::string& jwk,
                                    bool expected_result,
                                    CdmSessionType expected_type) {
-    DCHECK(!jwk.empty());
+    CHECK(!jwk.empty());
     KeyIdAndKeyPairs keys;
     CdmSessionType session_type;
     EXPECT_EQ(expected_result,
