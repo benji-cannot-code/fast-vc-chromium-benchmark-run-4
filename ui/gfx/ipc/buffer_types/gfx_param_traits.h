84f71a52d85b69465630b9d431c8dd9b3bfb87a8
Colin Blundell <blundell@chromium.org>
2025-12-09 00:10:36 -0800

    [//ui] Remove BufferUsageAndFormat IPC param traits
    This is now unused. I'll look at removing the header file in followup.
    
    Bug: 356649879
    Change-Id: I6aaba3733306d45caea4fe3ff9ba1d0fbfb5f384
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7236348
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1555944}

--- ui/gfx/ipc/buffer_types/gfx_param_traits.h
+++ ui/gfx/ipc/buffer_types/gfx_param_traits.h
@@ -11,17 +11,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/ipc/buffer_types/gfx_ipc_export.h"
 #include "ui/gfx/ipc/buffer_types/gfx_param_traits_macros.h"
 
-namespace IPC {
-
-template <>
-struct GFX_IPC_BUFFER_TYPES_EXPORT ParamTraits<gfx::BufferUsageAndFormat> {
-  typedef gfx::BufferUsageAndFormat param_type;
-  static void Write(base::Pickle* m, const param_type& p);
-  static bool Read(const base::Pickle* m,
-                   base::PickleIterator* iter,
-                   param_type* r);
-};
-
-}  // namespace IPC
-
 #endif  // UI_GFX_IPC_BUFFER_TYPES_GFX_PARAM_TRAITS_H_
