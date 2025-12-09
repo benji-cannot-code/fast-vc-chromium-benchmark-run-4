5d5dcb8f863a4f12424d9c0ce404b2e9a2cb4b7e
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2025-12-08 22:32:32 -0800

    Revert "DOMStorage: reuse DB sequence across profile restart."
    This reverts commit ae4cf43498d245331e7df6513570bd05245495e8.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/4834238479204352
    
    Sample build with failed test: https://ci.chromium.org/b/8696015364699867265
    Affected test(s):
    [://content/test\:content_unittests!gtest::StoragePartitionImplTest#RemoveLocalStorageForOneOrigin](https://ci.chromium.org/ui/test/chromium/:%2F%2Fcontent%2Ftest%5C:content_unittests%21gtest::StoragePartitionImplTest%23RemoveLocalStorageForOneOrigin?q=VHash%3A273ad6318a8d32c3)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F4834238479204352&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F7219148&type=BUG
    
    Original change's description:
    > DOMStorage: reuse DB sequence across profile restart.
    >
    > This change also refactors the creation of the database (aka "blocking")
    > task runner for session and local storage such that there's a single
    > shared stanza of logic and TaskTraits.
    >
    > Bug: 465497424
    > Change-Id: Ic10097ee6ab0a535f35404b8f325bba1e1af5cfd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7219148
    > Reviewed-by: Evan Stade <evanstade@microsoft.com>
    > Commit-Queue: Evan Stade <evanstade@microsoft.com>
    > Reviewed-by: Steve Becker <stevebe@microsoft.com>
    > Cr-Commit-Position: refs/heads/main@{#1555664}
    >
    
    Bug: 465497424
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I23d94283f9258bc7458d3ee118c76f7481d96f14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7239661
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Evan Stade <evanstade@microsoft.com>
    Reviewed-by: Evan Stade <evanstade@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1555913}

--- components/services/storage/dom_storage/session_storage_impl.h
+++ components/services/storage/dom_storage/session_storage_impl.h
@@ -33,6 +33,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "storage/common/database/db_status.h"
 #include "third_party/blink/public/mojom/dom_storage/session_storage_namespace.mojom.h"
 
+namespace base {
+class SequencedTaskRunner;
+}  // namespace base
+
 namespace blink {
 class StorageKey;
 }  // namespace blink
@@ -67,6 +71,8 @@ class SessionStorageImpl : public base::trace_event::MemoryDumpProvider,
       base::OnceCallback<void(SessionStorageImpl*)>;
   SessionStorageImpl(
       const base::FilePath& partition_directory,
+      scoped_refptr<base::SequencedTaskRunner> blocking_task_runner,
+      scoped_refptr<base::SequencedTaskRunner> memory_dump_task_runner,
       BackingMode backing_option,
       std::string database_name,
       DestructSessionStorageCallback destruct_callback,
@@ -216,15 +222,13 @@ class SessionStorageImpl : public base::trace_event::MemoryDumpProvider,
   } connection_state_ = NO_CONNECTION;
 
   const base::FilePath partition_directory_;
+  const scoped_refptr<base::SequencedTaskRunner> database_task_runner_;
 
   base::trace_event::MemoryAllocatorDumpGuid memory_dump_id_;
 
   mojo::Receiver<mojom::SessionStorageControl> receiver_;
 
   std::unique_ptr<AsyncDomStorageDatabase> database_;
-  // This can be true even if the profile is not in-memory, since we attempt
-  // to create an in-memory DB if on-disk fails. This variable has no meaning
-  // if `database_` is null.
   bool in_memory_ = false;
   bool tried_to_recreate_during_open_ = false;
 
