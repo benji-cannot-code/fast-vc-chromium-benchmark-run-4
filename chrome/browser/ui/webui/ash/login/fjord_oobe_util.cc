3837cb3bc7fb07064c45a77134e9cc085cd4287d
courtneywong <courtneywong@google.com>
2025-12-09 09:15:55 -0800

    fjord-oobe: Allowlist initial languages in the language selection menu
    Tested: Verified locally only allowlisted languages show in dropdown
    
    Bug: b:443859743
    Change-Id: I780613ba5b0ef0bfe660a24eb591a90951b90f12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7225570
    Reviewed-by: Osama Fathy <osamafathy@google.com>
    Commit-Queue: Courtney Wong <courtneywong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1556195}

--- chrome/browser/ui/webui/ash/login/fjord_oobe_util.cc
+++ chrome/browser/ui/webui/ash/login/fjord_oobe_util.cc
@@ -11,7 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash::fjord_util {
 
 namespace {
-const std::set<std::string>& kFjordOobeAllowedLanguages = {"en"};
+const std::set<std::string>& kFjordOobeAllowedLanguages = {
+    "en", "en-GB", "de", "fr", "ja", "fr-CA", "es"};
 }
 
 bool ShouldShowFjordOobe() {
