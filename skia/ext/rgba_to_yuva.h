c247bea99a30eed51b167ee7143adaba3c302c5e
Kramer Ge <fangzhoug@chromium.org>
2025-12-09 09:33:02 -0800

    Multiplanar: CopySharedImage clear to black in YUVA without src
    `CopySharedImage()` clears the region to black if `source_shared_image`
    is non-existent. If `dest_shared_image` is multiplanar it should clear
    YUVA planes similar to blit. Currently `dest_scoped_access->surface()`
    crashes because it is not single plane.
    
    Bug: 465527389
    Change-Id: I9b1e67badc04c2b045018e5ceb0d0f2bc160e9cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7219388
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Michael Ludwig <michaelludwig@google.com>
    Commit-Queue: Kramer Ge <fangzhoug@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1556211}

--- skia/ext/rgba_to_yuva.h
+++ skia/ext/rgba_to_yuva.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define SKIA_EXT_RGBA_TO_YUVA_H_
 
 #include "base/containers/span.h"
+#include "third_party/skia/include/core/SkColor.h"
 #include "third_party/skia/include/core/SkImage.h"
 #include "third_party/skia/include/core/SkRect.h"
 #include "third_party/skia/include/core/SkSurface.h"
@@ -21,13 +22,13 @@ namespace skia {
 // `dst_region`. If a default-constructed `dst_region` is passed in, the entire
 // destination image will be written to. If `clear_destination` is true, the
 // entire destination image will be cleared with black before the blit.
+// If `src_image` is null, draw black rect to the `dst_region` instead.
 SK_API void BlitRGBAToYUVA(SkImage* src_image,
                            base::span<SkSurface* const> dst_surfaces,
                            const SkYUVAInfo& dst_yuva_info,
                            const SkRect& dst_region = SkRect::MakeEmpty(),
                            bool clear_destination = false,
                            const SkRect& src_region = SkRect::MakeEmpty());
-
 }  // namespace skia
 
 #endif  // SKIA_EXT_RGBA_TO_YUVA_H_
