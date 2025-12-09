5b0f1c06518d520e3f6252a688ba3679c54fce50
Gauthier Ambard <gambard@chromium.org>
2025-12-09 12:41:19 -0800

    [iOS] Update spacing in the items
    According to designer specs.
    
    https://screenshot.googleplex.com/7ZnJBN6s6M6T3W9
    https://screenshot.googleplex.com/7dkpgri4gzECZmL
    
    Bug: 443034511
    Change-Id: Ib036a4888832066b4f0baa0ca690f65e2e4be7d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7242604
    Reviewed-by: Louis Romero <lpromero@google.com>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1556330}

--- ios/chrome/browser/shared/ui/table_view/content_configuration/table_view_cell_content_view.mm
+++ ios/chrome/browser/shared/ui/table_view/content_configuration/table_view_cell_content_view.mm
@@ -18,7 +18,11 @@ namespace {
 // trailing label (25).
 constexpr CGFloat kTitleSubtitleToTrailingWidthRatio = 3;
 
-constexpr CGFloat kLabelVerticalSpacing = 5;
+// Spacing between the different labels, vertically.
+constexpr CGFloat kLabelVerticalSpacing = 4;
+
+// Spacing between the different labels, horizontally.
+constexpr CGFloat kLabelHorizontalSpacing = 8;
 
 // The margin for the trailing edge of the content view, when there is an
 // accessory view in the cell.
@@ -436,6 +440,7 @@ constexpr CGFloat kTrailingMarginWithAccessory = 8;
 - (UIStackView*)createAllTextStack {
   UIStackView* stack = [[UIStackView alloc] init];
   stack.translatesAutoresizingMaskIntoConstraints = NO;
+  stack.spacing = kLabelHorizontalSpacing;
   return stack;
 }
 
