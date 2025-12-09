701586a365007960e8860d709a4c82a38540d15f
Ameur Hosni <ameurhosni@google.com>
2025-12-09 08:13:18 -0800

    [IOS][AIM] Show image gen canned suggestions
    This CL:
    - Adds a support for canned suggestions when image gen mode is enabled
    and an image has been attached.
    - Ensures we do not display suggestions for typed state when image gen
    is enabled.
    
    Bug: 466091091
    Change-Id: I193d8b1378728fd30aa39b3be86afde67f859c05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7232193
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1556150}

--- ios/chrome/browser/composebox/coordinator/composebox_omnibox_client_delegate.h
+++ ios/chrome/browser/composebox/coordinator/composebox_omnibox_client_delegate.h
@@ -7,12 +7,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_CHROME_BROWSER_COMPOSEBOX_COORDINATOR_COMPOSEBOX_OMNIBOX_CLIENT_DELEGATE_H_
 
 #import "ios/chrome/browser/composebox/coordinator/composebox_constants.h"
+#import "third_party/omnibox_proto/aim_tools_and_models.pb.h"
 
 struct UrlLoadParams;
 enum class WindowOpenDisposition;
 
 /// Delegate for ComposeboxOmniboxClient.
 @protocol ComposeboxOmniboxClientDelegate
+
+- (omnibox::ChromeAimToolsAndModels)composeboxToolMode;
 
 /// Returns the current attached suggest input in the composebox.
 - (std::optional<lens::proto::LensOverlaySuggestInputs>)suggestInputs;
