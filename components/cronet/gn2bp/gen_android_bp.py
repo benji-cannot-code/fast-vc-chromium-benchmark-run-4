ecd7ab18ab6879f2f1ae04ed30a6e31daa5540c0
Mohannad Farrag <aymanm@google.com>
2025-12-09 05:01:52 -0800

    Add no-rosegment into denylist
    Change-Id: Iab717c53ef8cd4a9eb4dc67fd869a1d7990fa7a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7224051
    Reviewed-by: Etienne Dechamps <edechamps@google.com>
    Commit-Queue: Mohannad Farrag <aymanm@google.com>
    Auto-Submit: Mohannad Farrag <aymanm@google.com>
    Commit-Queue: Etienne Dechamps <edechamps@google.com>
    Cr-Commit-Position: refs/heads/main@{#1556060}

--- components/cronet/gn2bp/gen_android_bp.py
+++ components/cronet/gn2bp/gen_android_bp.py
@@ -2699,7 +2699,11 @@ def _is_allowed_ldflag(flag):
       "-Wl,-z,max-page-size=",
       # Let Soong handle the stripping of debug library according to the
       # lunch configuration.
-      "-Wl,--strip-debug"
+      "-Wl,--strip-debug",
+      # Android is experimenting with XOM(crbug.com/379071663) which conflicts with
+      # rosegment flag. Disable this flag until XOM has landed, and we have
+      # an attribute which we can use to enable --no-rosegment.
+      "-Wl,--no-rosegment",
   ])
 
 
