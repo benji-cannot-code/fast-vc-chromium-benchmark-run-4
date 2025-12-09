8aaeacafd23f8bfdc6f5bc6c77729937a1efa494
Qikai <qikaizhong@microsoft.com>
2025-12-08 17:29:36 -0800

    [bedrock] Add EmbedderBrowserWindowFeatures::Init()
    Adds Init() method to EmbedderBrowserWindowFeatures for consistency with
    BrowserWindowFeatures, providing a unified initialization pattern for
    embedder-specific features.
    
    Bug: 449775911
    Change-Id: I59ac4ea7faa61b99f15c40a4e0601f36b79f4080
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7236591
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Auto-Submit: Qikai Zhong <qikaizhong@microsoft.com>
    Commit-Queue: Qikai Zhong <qikaizhong@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1555837}

--- chrome/browser/ui/browser_window/internal/embedder_browser_window_features.cc
+++ chrome/browser/ui/browser_window/internal/embedder_browser_window_features.cc
@@ -21,6 +21,10 @@ EmbedderBrowserWindowFeatures* EmbedderBrowserWindowFeatures::From(
   return Get(browser->GetUnownedUserDataHost());
 }
 
+void EmbedderBrowserWindowFeatures::Init(BrowserWindowInterface* browser) {
+  // Embedder implementation goes here.
+}
+
 void EmbedderBrowserWindowFeatures::InitPostWindowConstruction(
     BrowserWindowInterface* browser) {
   // Embedder implementation goes here.
