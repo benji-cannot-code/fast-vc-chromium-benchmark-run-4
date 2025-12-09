10160fffe3178dd82ac5e74350453c983b74d9b5
Mohamed Adel <adelm@google.com>
2025-12-09 08:17:18 -0800

    [Settings Search] Index dynamic settings config for AdMeasurementFrag
    Removes text based prefs.
    
    Bug: 444470792
    Change-Id: I6864ee0731d8ce94ac0808193bd89f8a9c210acd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7239917
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Moe Adel <adelm@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1556153}

--- chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/AdMeasurementFragment.java
+++ chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/AdMeasurementFragment.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.privacy_sandbox;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.text.style.ClickableSpan;
 import android.view.View;
@@ -25,6 +26,7 @@ import org.chromium.components.browser_ui.settings.ClickableSpansTextMessagePref
 import org.chromium.components.browser_ui.settings.SettingsFragment;
 import org.chromium.components.browser_ui.settings.SettingsUtils;
 import org.chromium.components.browser_ui.settings.TextMessagePreference;
+import org.chromium.components.browser_ui.settings.search.SettingsIndexData;
 import org.chromium.components.embedder_support.util.UrlConstants;
 import org.chromium.components.prefs.PrefService;
 import org.chromium.components.user_prefs.UserPrefs;
@@ -37,6 +39,18 @@ public class AdMeasurementFragment extends PrivacySandboxSettingsBaseFragment
     public static final String TOGGLE_PREFERENCE = "ad_measurement_toggle";
     public static final String DISCLAIMER_PREFERENCE = "ad_measurement_page_disclaimer";
     public static final String CONSIDER_BULLET_THREE = "ad_measurement_consider_bullet_three";
+    public static final String AD_MEASUREMENT_ENABLED_BULLET_ONE_PREF =
+            "ad_measurement_enabled_bullet_one";
+    public static final String AD_MEASUREMENT_ENABLED_BULLET_TWO_PREF =
+            "ad_measurement_enabled_bullet_two";
+    public static final String AD_MEASUREMENT_ENABLED_BULLET_THREE_PREF =
+            "ad_measurement_enabled_bullet_three";
+    public static final String AD_MEASUREMENT_CONSIDER_BULLET_ONE_PREF =
+            "ad_measurement_consider_bullet_one";
+    public static final String AD_MEASUREMENT_CONSIDER_BULLET_TWO_PREF =
+            "ad_measurement_consider_bullet_two";
+    public static final String AD_MEASUREMENT_CONSIDER_BULLET_THREE_PREF =
+            "ad_measurement_consider_bullet_three";
 
     private final ObservableSupplierImpl<String> mPageTitle = new ObservableSupplierImpl<>();
 
@@ -136,5 +150,16 @@ public class AdMeasurementFragment extends PrivacySandboxSettingsBaseFragment
 
     public static final ChromeBaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
             new ChromeBaseSearchIndexProvider(
-                    AdMeasurementFragment.class.getName(), R.xml.ad_measurement_preference);
+                    AdMeasurementFragment.class.getName(), R.xml.ad_measurement_preference) {
+                @Override
+                public void updateDynamicPreferences(Context context, SettingsIndexData indexData) {
+                    indexData.removeEntry(getUniqueId(AD_MEASUREMENT_ENABLED_BULLET_ONE_PREF));
+                    indexData.removeEntry(getUniqueId(AD_MEASUREMENT_ENABLED_BULLET_TWO_PREF));
+                    indexData.removeEntry(getUniqueId(AD_MEASUREMENT_ENABLED_BULLET_THREE_PREF));
+                    indexData.removeEntry(getUniqueId(AD_MEASUREMENT_CONSIDER_BULLET_ONE_PREF));
+                    indexData.removeEntry(getUniqueId(AD_MEASUREMENT_CONSIDER_BULLET_TWO_PREF));
+                    indexData.removeEntry(getUniqueId(AD_MEASUREMENT_CONSIDER_BULLET_THREE_PREF));
+                    indexData.removeEntry(getUniqueId(DISCLAIMER_PREFERENCE));
+                }
+            };
 }
