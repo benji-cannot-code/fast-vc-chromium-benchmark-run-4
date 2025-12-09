c29fd6702bd8194b269ec8c49909b4173a7d5d46
Marlon Facey <mfacey@chromium.org>
2025-12-09 11:34:10 -0800

    [Omnibox Next] Add loadTimeData and about flag to enable recent tab chip
    Disable the feature by default. Allow for the chip to show based on
    feature param. Add feature param to about flags entry.
    
    Bug: 465497143
    Change-Id: Ib8ca4553ef24e59d8e18e2b3f9cafc81b1463e4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7223711
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1556293}

--- chrome/browser/ui/omnibox/omnibox_next_features.cc
+++ chrome/browser/ui/omnibox/omnibox_next_features.cc
@@ -297,7 +297,7 @@ const base::FeatureParam<bool> kShowLensSearchChip(
 const base::FeatureParam<bool> kShowRecentTabChip(
     &internal::kWebUIOmniboxAimPopup,
     "ShowRecentTabChip",
-    true);
+    false);
 const base::FeatureParam<bool> kShowSmartCompose(
     &internal::kWebUIOmniboxAimPopup,
     "ShowSmartCompose",
