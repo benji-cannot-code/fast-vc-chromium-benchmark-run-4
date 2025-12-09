4d045071988daae8b4e217404499b666ae793cf6
Jenna Himawan <jhimawan@google.com>
2025-12-09 12:08:36 -0800

    Create PrefToValueMapBridge
    For mapping pref names to values. Follow-up to crrev.com/c/7204343.
    
    Bug: 442902926
    Change-Id: Ia9f19402ba29e0af593371d65bd366650a30fb3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7224533
    Reviewed-by: Benjamin Williams <bwwilliams@google.com>
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Reviewed-by: Charles Hager <clhager@google.com>
    Cr-Commit-Position: refs/heads/main@{#1556315}

---
+++ components/sync_preferences/synced_set_up/android/pref_to_value_map_bridge.cc
@@ -0,0 +1,51 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/sync_preferences/synced_set_up/android/pref_to_value_map_bridge.h"
+
+#include "base/android/jni_android.h"
+#include "base/android/jni_string.h"
+#include "components/sync_preferences/synced_set_up/android/jni_headers/PrefToValueMapBridge_jni.h"
+
+namespace sync_preferences::synced_set_up {
+
+static jlong JNI_PrefToValueMapBridge_Init(
+    JNIEnv* env,
+    const base::android::JavaRef<jobject>& j_caller) {
+  return reinterpret_cast<intptr_t>(new PrefToValueMapBridge(j_caller));
+}
+
+PrefToValueMapBridge::PrefToValueMapBridge(
+    const base::android::JavaRef<jobject>& j_obj)
+    : jobj_(j_obj) {}
+
+PrefToValueMapBridge::~PrefToValueMapBridge() = default;
+
+void PrefToValueMapBridge::PutStringValue(JNIEnv* env,
+                                          const std::string_view& key,
+                                          const std::string_view& value) {
+  Java_PrefToValueMapBridge_putStringValue(env, jobj_, key.data(),
+                                           value.data());
+}
+
+void PrefToValueMapBridge::PutIntValue(JNIEnv* env,
+                                       const std::string_view& key,
+                                       int value) {
+  Java_PrefToValueMapBridge_putIntValue(env, jobj_, key.data(), value);
+}
+
+void PrefToValueMapBridge::PutBooleanValue(JNIEnv* env,
+                                           const std::string_view& key,
+                                           bool value) {
+  Java_PrefToValueMapBridge_putBooleanValue(env, jobj_, key.data(), value);
+}
+
+void PrefToValueMapBridge::Destroy(JNIEnv* env) {
+  delete this;
+}
+
+}  // namespace sync_preferences::synced_set_up
+
+DEFINE_JNI(PrefToValueMapBridge)
