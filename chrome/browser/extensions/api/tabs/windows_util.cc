171fa2e7f2e72ed51e63493256ac554ae544c443
Devlin Cronin <rdevlin.cronin@chromium.org>
2025-12-09 13:06:51 -0800

    [Desktop Android] Enable windows_util::ShouldOpenIncognitoWindow()
    windows_util::ShouldOpenIncognitoWindow() wasn't enabled on the
    experimental desktop android platform becauase it required
    IsURLAllowedInIncognito(). This is now supported, so enable it.
    
    Bug: 371432155
    Change-Id: I603bd4bc356ce9d47902c288b92b2dc73045f6bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7242827
    Commit-Queue: James Cook <jamescook@chromium.org>
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1556351}

--- chrome/browser/extensions/api/tabs/windows_util.cc
+++ chrome/browser/extensions/api/tabs/windows_util.cc
@@ -100,9 +100,6 @@ bool CanOperateOnWindow(const ExtensionFunction* function,
              controller->profile();
 }
 
-#if !BUILDFLAG(IS_ANDROID)
-// TODO(crbug.com/371432155): Support on Android, specifically the call to
-// IsURLAllowedInIncognito() which is part of browser_navigator.h.
 IncognitoResult ShouldOpenIncognitoWindow(Profile* profile,
                                           std::optional<bool> incognito,
                                           std::vector<GURL>* urls,
@@ -152,6 +149,5 @@ IncognitoResult ShouldOpenIncognitoWindow(Profile* profile,
   return incognito_result ? IncognitoResult::kIncognito
                           : IncognitoResult::kRegular;
 }
-#endif  // !BUILDFLAG(IS_ANDROID)
 
 }  // namespace windows_util
