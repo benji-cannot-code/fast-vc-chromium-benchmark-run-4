f095fc732dfd90531494423685f60924d32626f7
Robbie McElrath <rmcelrath@chromium.org>
2025-12-08 18:09:46 -0800

    Remove library_name parameter from ChromeML::Get
    This was only used for testing and has been replaced with
    ChromeML::CreateForTesting. This also removes OnDeviceModelInternalImpl,
    which is no longer used.
    
    Bug: 449223318
    Change-Id: I472f0c0e69e0fa46bd49184fb094d13c572c4c9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7237040
    Reviewed-by: Nathan Memmott <memmott@chromium.org>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1555851}

--- services/on_device_model/fake/on_device_model_fake.h
+++ services/on_device_model/fake/on_device_model_fake.h
@@ -7,13 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define SERVICES_ON_DEVICE_MODEL_FAKE_ON_DEVICE_MODEL_FAKE_H_
 
 #include "base/component_export.h"
-#include "services/on_device_model/ml/on_device_model_internal.h"
+#include "services/on_device_model/ml/chrome_ml.h"
 
 namespace fake_ml {
 
 COMPONENT_EXPORT(ON_DEVICE_MODEL_FAKE)
 const ml::ChromeML* GetFakeChromeML();
 
-}  // namespace on_device_model
+}  // namespace fake_ml
 
 #endif  // SERVICES_ON_DEVICE_MODEL_FAKE_ON_DEVICE_MODEL_FAKE_H_
