c3db4083171636e47ab7d31f4d5db8a33c015adb
Hannah Collins <hannahrogers@google.com>
2025-12-09 07:58:26 -0800

    [ntp-realbox] Fix context menu positioning
    1) Ensure the context menu opens in the correct place after transitioning from realbox to composebox
    2) Ensure the context menu closes on any file upload errors.
    
    Fixed: 466391959, 461677254
    Change-Id: I2d158efd073c2f9f2b6ae7d9be07149f1d45102a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7238683
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1556140}

--- ui/webui/resources/cr_components/composebox/contextual_entrypoint_and_carousel.ts
+++ ui/webui/resources/cr_components/composebox/contextual_entrypoint_and_carousel.ts
@@ -312,6 +312,9 @@ export class ContextualEntrypointAndCarouselElement extends I18nMixinLit
           default:
             break;
         }
+        if (this.contextMenuEnabled_) {
+          this.$.contextEntrypoint.closeMenu();
+        }
       } else {
         file = {...file, status: status};
         this.files_.set(token, file);
@@ -537,6 +540,9 @@ export class ContextualEntrypointAndCarouselElement extends I18nMixinLit
     }
 
     this.recordFileValidationMetric_(metric);
+    if (this.contextMenuEnabled_) {
+      this.$.contextEntrypoint.closeMenu();
+    }
     this.fire('on-file-validation-error', {
       errorMessage: this.i18n(errorMessage),
     });
