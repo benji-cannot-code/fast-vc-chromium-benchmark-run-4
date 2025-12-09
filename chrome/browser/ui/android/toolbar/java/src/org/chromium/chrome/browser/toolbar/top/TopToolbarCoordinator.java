6454efa953d3fab6ebbd402eaecb42e11a325b71
Adriana Ixba <aixba@chromium.org>
2025-12-08 17:21:14 -0800

    Fix NullPointerException for TabStripTransitionCoordinator
    controlContainer could be null, this CL adds a check
    
    Bug: 463389171
    Change-Id: Ice8b23ce6bd7c0691c01d35b938fb0d9a6c91403
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7214650
    Commit-Queue: Adriana Ixba <aixba@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1555832}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/TopToolbarCoordinator.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/TopToolbarCoordinator.java
@@ -391,6 +391,7 @@ public class TopToolbarCoordinator implements Toolbar, TopControlLayer {
 
         mTabStripTransitionDelegateSupplier.runSyncOrOnAvailable(
                 (tabStripTransitionDelegate) -> {
+                    if (mControlContainer == null) return;
                     mTabStripTransitionCoordinator =
                             new TabStripTransitionCoordinator(
                                     browserControlsVisibilityManager,
