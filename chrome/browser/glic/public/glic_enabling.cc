21ad5be29bbbb744acd5330d80583b11dc8a34a3
James Lee <ljjlee@google.com>
2025-12-08 16:05:09 -0800

    Add synthetic trial logging for separate GLIC capabilities
    This adds users who are in the server-side experiment group and have a
    different eligibility in the old and new capability into a new
    "GlicEligibilitySeparateAccountCapabilityAffectedUsers" synthetic field
    trial.
    
    Bug: 462697239
    Change-Id: I5b990b855c1be4f3cd83ac374647c6cdee451ad3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7211506
    Auto-Submit: James Lee <ljjlee@google.com>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1555801}

--- chrome/browser/glic/public/glic_enabling.cc
+++ chrome/browser/glic/public/glic_enabling.cc
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/glic/host/auth_controller.h"
 #include "chrome/browser/glic/host/glic.mojom.h"
 #include "chrome/browser/glic/host/glic_features.mojom-features.h"
+#include "chrome/browser/glic/host/glic_synthetic_trial_manager.h"
+#include "chrome/browser/global_features.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_attributes_storage.h"
 #include "chrome/browser/profiles/profile_manager.h"
@@ -108,6 +110,25 @@ GlicEnabling::ProfileEnablement GlicEnabling::EnablementForProfile(
             : primary_account.capabilities.can_use_model_execution_features();
     result.primary_account_not_capable =
         (capability_value != signin::Tribool::kTrue);
+
+    // If the feature is overridden by a field trial, and the user's eligibility
+    // is known and different for the two capabilities, add them to a synthetic
+    // trial.
+    base::FieldTrial* field_trial = base::FeatureList::GetFieldTrial(
+        features::kGlicEligibilitySeparateAccountCapability);
+    if (field_trial &&
+        (primary_account.capabilities.can_use_gemini_in_chrome() !=
+         signin::Tribool::kUnknown) &&
+        (primary_account.capabilities.can_use_model_execution_features() !=
+         signin::Tribool::kUnknown) &&
+        (primary_account.capabilities.can_use_gemini_in_chrome() !=
+         primary_account.capabilities.can_use_model_execution_features())) {
+      g_browser_process->GetFeatures()
+          ->glic_synthetic_trial_manager()
+          ->SetSyntheticExperimentState(
+              kGlicEligibilitySeparateAccountCapabilitySyntheticTrialName,
+              field_trial->GetGroupNameWithoutActivation());
+    }
   }
 
   if (profile->GetPrefs()->GetInteger(::prefs::kGeminiSettings) !=
