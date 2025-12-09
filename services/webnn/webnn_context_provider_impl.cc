31cc75569d72bdb1d19e1ddc3dc6eeec4418e8e0
Bryan Bernhart <bryan.bernhart@intel.com>
2025-12-09 11:32:53 -0800

    WebNN: Refactor WebNNContextProviderImpl to enforce sequence check
    Renames the existing sequence checker to `main_sequence_checker_` and
    enforces its use on methods that require main-thread safety, preventing
    potential threading issues.
    
    Bug: 345352987
    Change-Id: Ia722a3fcc2c78ac377a733a2e3b247bef11049c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7233773
    Reviewed-by: Hu, Ningxin <ningxin.hu@intel.com>
    Commit-Queue: Bernhart, Bryan <bryan.bernhart@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1556291}

--- services/webnn/webnn_context_provider_impl.cc
+++ services/webnn/webnn_context_provider_impl.cc
@@ -127,7 +127,7 @@ WebNNContextProviderImpl::WebNNContextProviderImpl(
 }
 
 WebNNContextProviderImpl::~WebNNContextProviderImpl() {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  DCHECK_CALLED_ON_VALID_SEQUENCE(main_sequence_checker_);
 }
 
 std::unique_ptr<WebNNContextProviderImpl> WebNNContextProviderImpl::Create(
@@ -153,12 +153,13 @@ std::unique_ptr<WebNNContextProviderImpl> WebNNContextProviderImpl::Create(
 
 void WebNNContextProviderImpl::BindWebNNContextProvider(
     mojo::PendingReceiver<mojom::WebNNContextProvider> receiver) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(main_sequence_checker_);
   provider_receivers_.Add(this, std::move(receiver));
 }
 
 void WebNNContextProviderImpl::RemoveWebNNContextImpl(
     const blink::WebNNContextToken& handle) {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  DCHECK_CALLED_ON_VALID_SEQUENCE(main_sequence_checker_);
   auto it = context_impls_.find(handle);
   CHECK(it != context_impls_.end());
   context_impls_.erase(it);
@@ -167,7 +168,7 @@ void WebNNContextProviderImpl::RemoveWebNNContextImpl(
 #if BUILDFLAG(IS_WIN)
 void WebNNContextProviderImpl::DestroyAllContextsAndKillGpuProcess(
     const std::string& reason) {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  DCHECK_CALLED_ON_VALID_SEQUENCE(main_sequence_checker_);
   // Send the contexts lost reason to the renderer process.
   for (const auto& impl : context_impls_) {
     impl->OnLost(reason);
@@ -186,7 +187,7 @@ void WebNNContextProviderImpl::SetBackendForTesting(
 void WebNNContextProviderImpl::CreateWebNNContext(
     CreateContextOptionsPtr options,
     WebNNContextProvider::CreateWebNNContextCallback callback) {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  DCHECK_CALLED_ON_VALID_SEQUENCE(main_sequence_checker_);
   // Generates unique IDs for WebNNContextImpl.
   static base::AtomicSequenceNumber g_next_route_id;
 
@@ -322,7 +323,7 @@ void WebNNContextProviderImpl::OnCreateWebNNContextImpl(
     mojo::ScopedDataPipeProducerHandle write_tensor_producer,
     mojo::ScopedDataPipeConsumerHandle read_tensor_consumer,
     WebNNContextImplPtr context_impl) {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  DCHECK_CALLED_ON_VALID_SEQUENCE(main_sequence_checker_);
 
   if (!context_impl) {
     // TODO(crbug.com/40206287): Supporting WebNN on the platform.
@@ -348,7 +349,7 @@ void WebNNContextProviderImpl::OnCreateWebNNContextImpl(
 base::optional_ref<WebNNContextImpl>
 WebNNContextProviderImpl::GetWebNNContextImplForTesting(
     const blink::WebNNContextToken& handle) {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  DCHECK_CALLED_ON_VALID_SEQUENCE(main_sequence_checker_);
   const auto it = context_impls_.find(handle);
   if (it == context_impls_.end()) {
     mojo::ReportBadMessage(kBadMessageInvalidContext);
