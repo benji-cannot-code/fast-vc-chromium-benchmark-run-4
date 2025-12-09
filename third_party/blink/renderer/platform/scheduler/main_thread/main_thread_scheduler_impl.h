c0659e7e2769f8f81995be9e66bd665e61d59aea
Joe Mason <joenotcharles@google.com>
2025-12-09 15:07:24 -0800

    Remove InputScenarioPriorityBoost
    Bug: 406587000
    Change-Id: Ie4fb78b1e2aba4ff41498c2d3ea75b557251794e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7228284
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1556416}

--- third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl.h
+++ third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl.h
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/sequence_manager/task_time_observer.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/single_thread_task_runner.h"
-#include "base/threading/scoped_thread_priority.h"
 #include "base/time/time.h"
 #include "base/trace_event/trace_log.h"
 #include "build/build_config.h"
@@ -155,9 +154,6 @@ class PLATFORM_EXPORT MainThreadSchedulerImpl
     // std::nullopt.
     std::optional<features::TaskDeferralPolicy>
         discrete_input_task_deferral_policy;
-
-    bool input_scenario_priority_boost_enabled;
-    bool input_scenario_priority_boost_includes_loading;
   };
 
   static const char* RAILModeToString(RAILMode rail_mode);
@@ -782,10 +778,6 @@ class PLATFORM_EXPORT MainThreadSchedulerImpl
     // Task queues that have been detached from their scheduler and may have
     // pending tasks that need to run.
     HashSet<scoped_refptr<MainThreadTaskQueue>> detached_task_queues;
-
-    // Temporarily boosts the main thread priority. Only used if
-    // kInputScenarioPriorityBoost is enabled.
-    std::optional<base::ScopedBoostPriority> main_thread_priority_boost;
 
     // `WidgetScheduler`s that have not been shut down.
     HashSet<scoped_refptr<WidgetSchedulerImpl>> widget_schedulers;
