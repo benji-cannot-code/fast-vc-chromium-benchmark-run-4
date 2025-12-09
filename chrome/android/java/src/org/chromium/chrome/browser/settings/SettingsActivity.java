c8f4e49a4717c9209f4f756ea9c4e1f9afed744c
Jinsuk Kim <jinsukkim@chromium.org>
2025-12-08 16:29:19 -0800

    [Settings Search] Avoid calling search in standalone mode
    If a Fragment is opened as a standalone mode, it doesn't require
    search UI on it, and doesn't go through the same init path
    instantiating search coordinator either. Removes the check against
    search feature flag in menu setup methods and checks only the
    coordinator to invoke menu initialization.
    
    Bug: 466461783
    Change-Id: If127ceac92f81b9b2b89a5e206332121a98f39cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7239919
    Reviewed-by: Moe Adel <adelm@google.com>
    Commit-Queue: Moe Adel <adelm@google.com>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1555810}

--- chrome/android/java/src/org/chromium/chrome/browser/settings/SettingsActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/SettingsActivity.java
@@ -817,9 +817,7 @@ public class SettingsActivity extends ChromeBaseAppCompatActivity
         if (mTitleUpdater != null) {
             getSupportFragmentManager().unregisterFragmentLifecycleCallbacks(mTitleUpdater);
         }
-        if (ChromeFeatureList.sSearchInSettings.isEnabled()) {
-            assumeNonNull(mSearchCoordinator).destroy();
-        }
+        if (mSearchCoordinator != null) mSearchCoordinator.destroy();
         super.onDestroy();
     }
 
@@ -856,11 +854,11 @@ public class SettingsActivity extends ChromeBaseAppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        if (ChromeFeatureList.sSearchInSettings.isEnabled()) {
-            assert mSearchCoordinator != null;
-            assumeNonNull(mSearchCoordinator).hideHelpAndFeedbackIcon();
+        if (mSearchCoordinator != null) {
+            mSearchCoordinator.hideHelpAndFeedbackIcon();
             return false;
         }
+
         // By default, every screen in Settings shows a "Help & feedback" menu item.
         MenuItem help =
                 menu.add(
