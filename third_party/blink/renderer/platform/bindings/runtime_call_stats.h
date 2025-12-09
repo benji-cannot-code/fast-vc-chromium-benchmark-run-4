908ca2e366657913bd1cce1b75b0da5e114450ba
Bryan Enrique Gonzalez <bryanenriquegv@google.com>
2025-12-09 08:41:33 -0800

    spanification: automatically spanify .../platform/bindings/runtime_call_stats.cc etc.
    This is the result of running the automatic spanification on linux and
    updating code to use and pass spans where size is known.
    
    The original patch was fully automated using script:
    //tools/clang/spanify/rewrite-multiple-platforms.sh -platforms=linux
    Then refined with gemini-cli
    
    gemini-run/batch-run-1761116551/group_152
    
    BUG=439964610
    
    Change-Id: Idc317fec11d298491a0173f31112de9a6b64992c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7233300
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Bryan Enrique Gonzalez <bryanenriquegv@google.com>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1556177}

--- third_party/blink/renderer/platform/bindings/runtime_call_stats.h
+++ third_party/blink/renderer/platform/bindings/runtime_call_stats.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_BINDINGS_RUNTIME_CALL_STATS_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_BINDINGS_RUNTIME_CALL_STATS_H_
 
+#include <array>
 #include <optional>
 
 #include "base/check_op.h"
@@ -319,7 +320,7 @@ class PLATFORM_EXPORT RuntimeCallStats {
   void SetInUse(bool in_use) { in_use_ = in_use; }
 
   RuntimeCallCounter* GetCounter(CounterId id) {
-    return UNSAFE_TODO(&(counters_[static_cast<uint16_t>(id)]));
+    return &(counters_[static_cast<uint16_t>(id)]);
   }
 
   String ToString() const;
@@ -340,9 +341,8 @@ class PLATFORM_EXPORT RuntimeCallStats {
  private:
   raw_ptr<RuntimeCallTimer> current_timer_ = nullptr;
   bool in_use_ = false;
-  RuntimeCallCounter counters_[static_cast<int>(CounterId::kNumberOfCounters)];
-  static const int number_of_counters_ =
-      static_cast<int>(CounterId::kNumberOfCounters);
+  std::array<RuntimeCallCounter, static_cast<int>(CounterId::kNumberOfCounters)>
+      counters_;
   raw_ptr<const base::TickClock> clock_ = nullptr;
 
 #if BUILDFLAG(RCS_COUNT_EVERYTHING)
