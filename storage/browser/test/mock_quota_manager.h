8cc647dce6c4d72b6dc245d4b8580b314d6c7532
Lei Zhang <thestig@chromium.org>
2025-12-09 15:03:43 -0800

    Do IWYU in storage/browser/test/mock_quota_manager.h
    Add missing callback_helpers.h. The chain of transitive includes that
    provides callback_helpers.h will be broken in the near future.
    
    Also do IWYU for STL headers.
    
    Change-Id: I699c4f4f9d8fe19593a407d96631c14c37a8c692
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7241170
    Auto-Submit: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Evan Stade <evanstade@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1556413}

--- storage/browser/test/mock_quota_manager.h
+++ storage/browser/test/mock_quota_manager.h
@@ -8,13 +8,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include <limits>
 #include <list>
 #include <map>
 #include <memory>
 #include <set>
-#include <utility>
+#include <string>
 #include <vector>
 
+#include "base/functional/callback_helpers.h"
 #include "base/gtest_prod_util.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/time/time.h"
