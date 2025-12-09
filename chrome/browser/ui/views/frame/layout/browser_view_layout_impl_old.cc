f5e17c82af3c2609ef7481b11287936369a09e48
Dana Fried <dfried@chromium.org>
2025-12-08 16:09:03 -0800

    [Vertical Tabs] Report exclusion and toolbar height to vertical tabstrip
    NO_IFTTT=Switched from View to specific type, no other change.
    
    Bug: 466106703
    Change-Id: I5384267501aeb9e935869fe2b8cc9ac6697e0697
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7238392
    Reviewed-by: Alison Gale <agale@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1555803}

--- chrome/browser/ui/views/frame/layout/browser_view_layout_impl_old.cc
+++ chrome/browser/ui/views/frame/layout/browser_view_layout_impl_old.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/immersive_mode_controller.h"
 #include "chrome/browser/ui/views/frame/layout/browser_view_layout_delegate.h"
 #include "chrome/browser/ui/views/frame/multi_contents_view.h"
+#include "chrome/browser/ui/views/frame/vertical_tab_strip_region_view.h"
 #include "chrome/browser/ui/views/infobars/infobar_container_view.h"
 #include "chrome/browser/ui/views/side_panel/side_panel.h"
 #include "chrome/browser/ui/views/web_apps/frame_toolbar/web_app_frame_toolbar_view.h"
