d1a95e8034c452b31c65c2b12fb8b9fa220f9ff9
Julia Przybytniowska <przybytniowska@google.com>
2025-12-10 14:12:51 -0800

    Add webstorePrivate API to log when CEC promotion banner was clicked.
    This function allows the CWS to signal when the Chrome Enterprise Core
    (CEC) promotion banner is clicked by the user, enabling metric logging
    for these interactions.
    
    BUG=b:465700382
    
    Change-Id: Ib1eac2c5078dec1c79b0bb64afc0f3d7bcd95b54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7232050
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Julia Przybytniowska <przybytniowska@google.com>
    Reviewed-by: Austin Zhang <austinzzr@google.com>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1556948}

--- chrome/browser/extensions/api/webstore_private/webstore_private_api.h
+++ chrome/browser/extensions/api/webstore_private/webstore_private_api.h
@@ -463,6 +463,24 @@ class WebstorePrivateLogEnterprisePromoShownFunction
   ResponseAction Run() override;
 };
 
+class WebstorePrivateOnEnterprisePromoClickFunction : public ExtensionFunction {
+ public:
+  DECLARE_EXTENSION_FUNCTION("webstorePrivate.onEnterprisePromoClick",
+                             WEBSTOREPRIVATE_ONENTERPRISEPROMOCLICK)
+
+  WebstorePrivateOnEnterprisePromoClickFunction();
+
+  WebstorePrivateOnEnterprisePromoClickFunction(
+      const WebstorePrivateOnEnterprisePromoClickFunction&) = delete;
+  WebstorePrivateOnEnterprisePromoClickFunction& operator=(
+      const WebstorePrivateOnEnterprisePromoClickFunction&) = delete;
+
+ protected:
+  ~WebstorePrivateOnEnterprisePromoClickFunction() override = default;
+
+  ResponseAction Run() override;
+};
+
 #endif  // !BUILDFLAG(IS_ANDROID)
 
 }  // namespace extensions
