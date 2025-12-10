00f06641e35aadfce5b1f0a12725938de5a99c2f
Bofeng Chen <bofengchen@google.com>
2025-12-10 11:59:35 -0800

    [ntp-simplification] Add a pref to track user interaction with shortcuts
    This change introduces a new boolean user preference,
    kNtpShortcutsInteractedWith, which is set to true whenever a user
    performs an action related to the New Tab Page shortcuts (Most Visited
    tiles). This includes adding, deleting, updating, reordering, undoing
    actions on tiles, navigating from a tile, changing the visibility of
    shortcuts, or switching between Top Sites and Custom Links. Enterprise
    shortcuts are not impacted by this change.
    
    Bug: 465781055
    Change-Id: I97a8d9df1277243f733edd9258bad7783d684c5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7226093
    Reviewed-by: Hannah Collins <hannahrogers@google.com>
    Commit-Queue: Bofeng Chen <bofengchen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1556901}

--- chrome/browser/new_tab_page/new_tab_page_util.cc
+++ chrome/browser/new_tab_page/new_tab_page_util.cc
@@ -320,3 +320,8 @@ std::set<ntp_tiles::TileType> GetEnabledTileTypes(Profile* profile) {
   }
   return enabled_types;
 }
+
+void DisableShortcutsAutoRemoval(Profile* profile) {
+  profile->GetPrefs()->SetBoolean(ntp_prefs::kNtpShortcutsAutoRemovalDisabled,
+                                  true);
+}
--- chrome/browser/ui/webui/cr_components/most_visited/most_visited_handler.cc
+++ chrome/browser/ui/webui/cr_components/most_visited/most_visited_handler.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_observation.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/browser_features.h"
+#include "chrome/browser/new_tab_page/new_tab_page_util.h"
 #include "chrome/browser/ntp_tiles/chrome_most_visited_sites_factory.h"
 #include "chrome/browser/predictors/loading_predictor.h"
 #include "chrome/browser/predictors/loading_predictor_config.h"
@@ -101,6 +102,7 @@ void MostVisitedHandler::AddMostVisitedTile(
     const GURL& url,
     const std::string& title,
     AddMostVisitedTileCallback callback) {
+  DisableShortcutsAutoRemoval(profile_);
   if (most_visited_sites_->IsCustomLinksEnabled()) {
     bool success =
         most_visited_sites_->AddCustomLink(url, base::UTF8ToUTF16(title));
@@ -112,6 +114,7 @@ void MostVisitedHandler::AddMostVisitedTile(
 
 void MostVisitedHandler::DeleteMostVisitedTile(
     most_visited::mojom::MostVisitedTilePtr tile) {
+  DisableShortcutsAutoRemoval(profile_);
   if (IsFromEnterpriseShortcut(tile->source)) {
     CHECK(most_visited_sites_->IsEnterpriseShortcutsEnabled());
     most_visited_sites_->DeleteEnterpriseShortcut(tile->url);
@@ -152,6 +155,7 @@ void MostVisitedHandler::RestoreMostVisitedDefaults(
 void MostVisitedHandler::ReorderMostVisitedTile(
     most_visited::mojom::MostVisitedTilePtr tile,
     uint8_t new_pos) {
+  DisableShortcutsAutoRemoval(profile_);
   if (IsFromEnterpriseShortcut(tile->source)) {
     CHECK(most_visited_sites_->IsEnterpriseShortcutsEnabled());
     most_visited_sites_->ReorderEnterpriseShortcut(tile->url, new_pos);
@@ -189,6 +193,7 @@ void MostVisitedHandler::UpdateMostVisitedTile(
     const GURL& new_url,
     const std::string& new_title,
     UpdateMostVisitedTileCallback callback) {
+  DisableShortcutsAutoRemoval(profile_);
   if (IsFromEnterpriseShortcut(tile->source)) {
     CHECK(most_visited_sites_->IsEnterpriseShortcutsEnabled());
     bool success = most_visited_sites_->UpdateEnterpriseShortcut(
@@ -231,6 +236,7 @@ void MostVisitedHandler::OnMostVisitedTileNavigation(
     bool ctrl_key,
     bool meta_key,
     bool shift_key) {
+  DisableShortcutsAutoRemoval(profile_);
   logger_.LogMostVisitedNavigation(MakeNTPTileImpression(*tile, index));
 
   WindowOpenDisposition disposition = ui::DispositionFromClick(
@@ -264,6 +270,7 @@ void MostVisitedHandler::GetMostVisitedExpandedState(
 }
 
 void MostVisitedHandler::SetMostVisitedExpandedState(bool is_expanded) {
+  DisableShortcutsAutoRemoval(profile_);
   profile_->GetPrefs()->SetBoolean(ntp_prefs::kNtpShowAllMostVisitedTiles,
                                    is_expanded);
 }
