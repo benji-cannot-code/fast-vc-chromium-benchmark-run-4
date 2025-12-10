374cb977cfc5b12d120edb9a05ca6bc86c9fda7c
yuyisi <hellofroser@gmail.com>
2025-12-10 02:39:42 -0800

    FIXUP: Task manager doesn't close with Alt+F4
    Add an Alt+F4 accelerator on Windows for closing task manager.
    
    Fixed: 41441855
    Change-Id: I126b4e5adea2e42a5e932d8bff72253cc39fda4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7236609
    Reviewed-by: hewro <ihewro@chromium.org>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Auto-Submit: YISI YU <hellofroser@gmail.com>
    Commit-Queue: hewro <ihewro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1556629}

--- chrome/browser/ui/views/task_manager_view.cc
+++ chrome/browser/ui/views/task_manager_view.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
+#include "base/notreached.h"
 #include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
 #include "chrome/app/vector_icons/vector_icons.h"
@@ -235,11 +236,19 @@ gfx::Size TaskManagerView::CalculatePreferredSize(
 }
 
 bool TaskManagerView::AcceleratorPressed(const ui::Accelerator& accelerator) {
-  const bool is_valid_modifier =
-      accelerator.modifiers() == ui::EF_CONTROL_DOWN ||
-      accelerator.modifiers() == (ui::EF_CONTROL_DOWN | ui::EF_SHIFT_DOWN);
-  DCHECK(is_valid_modifier);
-  DCHECK_EQ(ui::VKEY_W, accelerator.key_code());
+  switch (accelerator.modifiers()) {
+    case ui::EF_CONTROL_DOWN:
+    case ui::EF_CONTROL_DOWN | ui::EF_SHIFT_DOWN:
+      CHECK_EQ(ui::VKEY_W, accelerator.key_code());
+      break;
+#if BUILDFLAG(IS_WIN)
+    case ui::EF_ALT_DOWN:
+      CHECK_EQ(ui::VKEY_F4, accelerator.key_code());
+      break;
+#endif
+    default:
+      NOTREACHED();
+  }
 
   GetWidget()->Close();
   return true;
@@ -708,6 +717,9 @@ void TaskManagerView::Init() {
   AddAccelerator(ui::Accelerator(ui::VKEY_W, ui::EF_CONTROL_DOWN));
   AddAccelerator(
       ui::Accelerator(ui::VKEY_W, ui::EF_CONTROL_DOWN | ui::EF_SHIFT_DOWN));
+#if BUILDFLAG(IS_WIN)
+  AddAccelerator(ui::Accelerator(ui::VKEY_F4, ui::EF_ALT_DOWN));
+#endif
 }
 
 void TaskManagerView::InitAlwaysOnTopState() {
