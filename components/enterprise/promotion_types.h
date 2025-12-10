11806d3bf3a4a0530bcea9c8aea34c0efeb2e301
Julia Przybytniowska <przybytniowska@google.com>
2025-12-10 01:12:16 -0800

    Define enum for CEC promotion banner histogram
    BUG=b:465700382
    
    Change-Id: I6f1e6f49b2092320e7bc6aebb3302bc9f26aab4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7228791
    Commit-Queue: Julia Przybytniowska <przybytniowska@google.com>
    Reviewed-by: Austin Zhang <austinzzr@google.com>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1556594}

--- components/enterprise/promotion_types.h
+++ components/enterprise/promotion_types.h
@@ -9,8 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace enterprise {
 
 // This enum is used to store which enterprise promotion should be shown. It
-// is stored as an integer in the preferences. This enum is used for
-// histogram recording and should not be renumbered.
+// is stored as an integer in the preferences.
 // LINT.IfChange(PromotionType)
 enum class PromotionType {
   kUnspecified = 0,
@@ -19,6 +18,13 @@ enum class PromotionType {
   kMaxValue = kChromeEnterprisePremium,
 };
 // LINT.ThenChange(//components/policy/proto/device_management_backend.proto:PromotionType)
+
+// This enum is used for histogram recording and should not be renumbered.
+enum class CwsPromotionBannerEvent {
+  kDisplayed = 0,
+  kClicked = 1,
+  kMaxValue = kClicked,
+};
 
 }  // namespace enterprise
 
