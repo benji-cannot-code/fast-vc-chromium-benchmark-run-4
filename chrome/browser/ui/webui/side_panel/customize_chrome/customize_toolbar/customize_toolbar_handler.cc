3156235e3d0d2e1ccd7112234d4cdbd36ab888b2
Foromo Daniel Soromou <koretadaniel@chromium.org>
2025-12-10 07:53:32 -0800

    [Refactor Branded Strings] IDS_NTP_CUSTOMIZE_TOOLBAR_CATEGORY_YOUR_*
    See: https://docs.google.com/document/d/1gK_4bAsyWtVHl8spYHumozi4k9vl4Yl3V9MratFK8f0
    
    Bug: 443700721
    Change-Id: I01d6ce2c424006c3e9415b0cea7cf345d841f08b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7243334
    Commit-Queue: Daniel Soromou <koretadaniel@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1556763}

--- chrome/browser/ui/webui/side_panel/customize_chrome/customize_toolbar/customize_toolbar_handler.cc
+++ chrome/browser/ui/webui/side_panel/customize_chrome/customize_toolbar/customize_toolbar_handler.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/webui_embedding_context.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/pref_names.h"
+#include "chrome/grit/branded_strings.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/contextual_tasks/public/features.h"
 #include "components/strings/grit/components_strings.h"
