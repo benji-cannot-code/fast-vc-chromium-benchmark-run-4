0840cf92a6f350e008aa9edeb43e61bf32b02e71
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2025-12-10 14:02:26 -0800

    Import wpt@4016865e7f163579d084597a57d2863ed04c480c
    https://github.com/web-platform-tests/wpt/compare/d85c75182...4016865e7
    
    Using wpt-import in Chromium e9f0ae9bb4b5a5d591203eae4361c2d5238f328c.
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    daniec@microsoft.com, siliu@microsoft.com, snianu@microsoft.com:
      external/wpt/input-events
    hongchan@chromium.org, mjwilson@chromium.org:
      external/wpt/webaudio
    
    NOAUTOREVERT=true
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac-skia-alt-arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac12.0-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac12.0.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac13.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac14.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac15.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:win10.20h2-blink-rel
    Change-Id: I55d10be890a80fff01c80197e6bf3bc881748bfa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7247929
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1556931}

--- third_party/blink/web_tests/external/wpt/sanitizer-api/support/util.js
+++ third_party/blink/web_tests/external/wpt/sanitizer-api/support/util.js
@@ -86,8 +86,6 @@ function assert_config(config, expected) {
 
   assert_config_is_valid(config);
 
-  // XXX dataAttributes
-  // XXX comments
   // XXX duplications
   // XXX other consistency checks
 
@@ -168,4 +166,12 @@ function assert_config(config, expected) {
   assert_elems("elements");
   assert_elems("removeElements");
   assert_elems("replaceWithChildrenElements");
+
+  if ("comments" in expected) {
+    assert_equals(config.comments, expected.comments, "comments should match");
+  }
+
+  if ("dataAttributes" in expected) {
+    assert_equals(config.dataAttributes, expected.dataAttributes, "dataAttributes should match");
+  }
 }
