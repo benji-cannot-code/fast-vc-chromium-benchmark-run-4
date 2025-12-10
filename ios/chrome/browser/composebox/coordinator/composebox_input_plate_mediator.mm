f03d5ae3d701e93809d116df5e0db48205a3b6df
Radu Nitescu <radunitescu@google.com>
2025-12-10 07:00:24 -0800

    [iOS][AIM] Refrain from showing the AIM nudge when DSE not Google
    Tests added for this
    
    Change-Id: Ie20adb54322f06fe4731f7a69f16c29d3b64b95f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7247148
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1556735}

--- ios/chrome/browser/composebox/coordinator/composebox_input_plate_mediator.mm
+++ ios/chrome/browser/composebox/coordinator/composebox_input_plate_mediator.mm
@@ -1218,7 +1218,8 @@ CreateInputDataFromAnnotatedPageContent(
   BOOL canSend = hasContent && !compactMode;
   BOOL showShortcuts = !hasContent && !canSend;
   BOOL showLeadingImage = !compactMode || !allowsMultimodalActions;
-  BOOL shouldPersistAIMButton = IsComposeboxAIMNudgeEnabled() && !compactMode;
+  BOOL shouldPersistAIMButton =
+      IsComposeboxAIMNudgeEnabled() && !compactMode && allowsMultimodalActions;
 
   ComposeboxInputPlateControls leadingAction =
       allowsMultimodalActions ? ComposeboxInputPlateControls::kPlus
