27dd30576ca86f5ce9dab7f5fe434468f4cbf5dc
Steve Kobes <skobes@chromium.org>
2025-12-10 10:02:18 -0800

    Remove fluent-scrollbars from about:flags.
    Bug: 398193016
    Change-Id: I2083b5eab42e6c341f31bb8a83e561d53fd30eae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7243930
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1556828}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -5760,13 +5760,6 @@ const FeatureEntry kFeatureEntries[] = {
         kOsWin | kOsLinux,
         FEATURE_VALUE_TYPE(features::kFluentOverlayScrollbar),
     },
-    {
-        "fluent-scrollbars",
-        flag_descriptions::kFluentScrollbarsName,
-        flag_descriptions::kFluentScrollbarsDescription,
-        kOsWin | kOsLinux,
-        FEATURE_VALUE_TYPE(features::kFluentScrollbar),
-    },
 #endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_LINUX)
 #if BUILDFLAG(IS_CHROMEOS)
     {
