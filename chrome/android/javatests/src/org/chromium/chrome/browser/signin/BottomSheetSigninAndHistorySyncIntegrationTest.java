9093c1d2c7093574d3e1ecab850046741986731e
Anton Goncharenko <agonch@google.com>
2025-12-10 01:27:24 -0800

    [Signin][Android] Confirm seamless signin flag enabled for undo snackbar
    Confirm that if shouldShowSigninSnackbar is set, the feature is enabled.
    
    Bug: 437039311
    Change-Id: Iec4a84a2a3636e210f94593deb252da3569efa6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7241747
    Commit-Queue: Anton Goncharenko <agonch@google.com>
    Reviewed-by: Menghan Yang <myuu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1556602}

--- chrome/android/javatests/src/org/chromium/chrome/browser/signin/BottomSheetSigninAndHistorySyncIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/signin/BottomSheetSigninAndHistorySyncIntegrationTest.java
@@ -750,6 +750,7 @@ public class BottomSheetSigninAndHistorySyncIntegrationTest {
 
     @Test
     @MediumTest
+    @EnableFeatures(SigninFeatures.ENABLE_SEAMLESS_SIGNIN)
     public void testSeamlessSignin() {
         HistogramWatcher signinHistogramWatcher =
                 HistogramWatcher.newBuilder()
