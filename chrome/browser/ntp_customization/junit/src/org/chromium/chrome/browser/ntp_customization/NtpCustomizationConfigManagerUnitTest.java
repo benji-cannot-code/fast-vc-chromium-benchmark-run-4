936815e646d289503083597e4743385878c7c663
Xinyi Ji <xinyiji@google.com>
2025-12-10 10:49:43 -0800

    [CustomizationV2] Implement Daily Update Logic (Part 4)
    This CL introduces the storage mechanism to support the "daily refresh" feature for NTP theme collections. It allows the next day's theme (image, crop settings, and primary color) to be fetched and stored in the background without affecting the user's currently active theme.
    
    Key changes include:
    1. Added new SharedPreferences keys to store theme collection information specifically for the daily refresh background.
    2. Introduced a separate file to save the daily refresh background image bitmap.
    3. Modified methods in NtpCustomizationUtils to accept an isDailyRefreshImage flag, enabling them to operate on either the active theme or the pending daily refresh theme.
    4. Implemented applyDailyRefreshTheme() to atomically switch the daily refresh theme to the active theme. This is achieved by copying SharedPreferences data and efficiently renaming the background image file.
    
    Bug: 423579377
    Change-Id: Id032495167de83ddada61e262ee044e6f9a96413
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7242286
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Xinyi Ji <xinyiji@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1556859}

--- chrome/browser/ntp_customization/junit/src/org/chromium/chrome/browser/ntp_customization/NtpCustomizationConfigManagerUnitTest.java
+++ chrome/browser/ntp_customization/junit/src/org/chromium/chrome/browser/ntp_customization/NtpCustomizationConfigManagerUnitTest.java
@@ -104,7 +104,8 @@ public class NtpCustomizationConfigManagerUnitTest {
 
         // Removes the newly generated file and cleans up SharedPreference.
         NtpCustomizationUtils.resetSharedPreferenceForTesting();
-        NtpCustomizationUtils.deleteBackgroundImageFileImpl();
+        NtpCustomizationUtils.deleteBackgroundImageFileImpl(
+                NtpCustomizationUtils.createBackgroundImageFile());
         NtpCustomizationConfigManager.setInstanceForTesting(null);
     }
 
@@ -117,7 +118,7 @@ public class NtpCustomizationConfigManagerUnitTest {
         BaseRobolectricTestRule.runAllBackgroundAndUi();
 
         // Verifies that the image file are saved to the disk and matrices are persisted to prefs.
-        assertTrue(NtpCustomizationUtils.getBackgroundImageFile().exists());
+        assertTrue(NtpCustomizationUtils.createBackgroundImageFile().exists());
         assertNotNull(
                 ChromeSharedPreferences.getInstance()
                         .readString(
