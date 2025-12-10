c6b2b85501fcd7aacc6dd631f06d4279b1ec21e0
Charles Meng <charlesmeng@chromium.org>
2025-12-10 14:28:42 -0800

    [Vertical Tabs] Fixes for tab painting
    Use IsHoverAnimationActive() instead of hovered_ state, as the fade out
    animation is still active even after the tab is no longer hovered. Also
    makes the tab not paint a background in the selected state if the theme
    property SHOULD_FILL_BACKGROUND_TAB_COLOR is 0 (e.g. in GTK/QT themes).
    
    Screencast (5x slowed):
    https://screencast.googleplex.com/cast/NTI4MDQyNjQ5NDAwMTE1MnxiMWQ2MWVlYS05Yg
    
    Fixed: 460535066
    Change-Id: I648c8cac658a26a4ba6746fac538889625b0bb94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7243426
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Charles Meng <charlesmeng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1556975}

--- chrome/browser/ui/views/tabs/vertical/vertical_tab_view.h
+++ chrome/browser/ui/views/tabs/vertical/vertical_tab_view.h
@@ -101,6 +101,7 @@ class VerticalTabView : public views::View,
   void UpdateColors();
   void UpdateContrastRatioValues();
 
+  bool IsHoverAnimationActive() const;
   double GetHoverAnimationValue() const;
   float GetHoverOpacity() const;
 
