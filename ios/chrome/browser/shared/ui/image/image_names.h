43502be2a951c236b0559690a0cf591fa609f6e7
Ewann Pelle <ewannpv@chromium.org>
2025-12-10 07:36:38 -0800

    [iOS] Remove unused image assets
    Bug: 467567246
    Change-Id: Ie8715d708ec48469508eff41126d9c55b8110678
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7245750
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1556756}

--- ios/chrome/browser/shared/ui/image/image_names.h
+++ ios/chrome/browser/shared/ui/image/image_names.h
@@ -13,8 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Branded image names.
 #if BUILDFLAG(IOS_USE_BRANDED_ASSETS)
-extern NSString* const kChromeAIHubHeaderImage;
-extern NSString* const kChromeDefaultBrowserIllustrationImage;
 extern NSString* const kChromeDefaultBrowserScreenBannerImage;
 extern NSString* const kChromeGuidedTourBannerImage;
 extern NSString* const kChromeNotificationsOptInBannerImage;
@@ -26,12 +24,7 @@ extern NSString* const kGoogleSearchEngineLogoImage;
 extern NSString* const kGooglePasswordManagerWidgetPromoImage;
 extern NSString* const kGooglePasswordManagerWidgetPromoDisabledImage;
 extern NSString* const kGoogleSettingsPasswordsInOtherAppsBannerImage;
-extern NSString* const kLensKeyboardAccessoryImage;
-extern NSString* const kFedexCarrierImage;
-extern NSString* const kUPSCarrierImage;
-extern NSString* const kUSPSCarrierImage;
 #else
-extern NSString* const kChromiumAIHubHeaderImage;
 extern NSString* const kChromiumDefaultBrowserIllustrationImage;
 extern NSString* const kChromiumDefaultBrowserScreenBannerImage;
 extern NSString* const kChromiumGuidedTourBannerImage;
