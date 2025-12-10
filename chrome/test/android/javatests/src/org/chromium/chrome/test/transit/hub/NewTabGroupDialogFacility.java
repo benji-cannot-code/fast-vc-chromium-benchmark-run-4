be211a0ac1fa42981a8c3ce51fb597f624c9a194
Fiaz Muhammad <mfiaz@google.com>
2025-12-10 14:41:06 -0800

    [Public Transit] Check if tab group exists in NewTabGroupDialogFacility
    Change-Id: I70cbea0257e6dea88b876587d877bd9ba19eb3ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7248089
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Auto-Submit: Fiaz Muhammad <mfiaz@google.com>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1556996}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/transit/hub/NewTabGroupDialogFacility.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/transit/hub/NewTabGroupDialogFacility.java
@@ -23,7 +23,6 @@ import android.content.Context;
 import android.view.View;
 import android.widget.EditText;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import org.hamcrest.Matcher;
@@ -43,6 +42,7 @@ import org.chromium.chrome.test.R;
 import org.chromium.chrome.test.transit.ChromeActivityTabModelBoundStation;
 import org.chromium.chrome.test.transit.SoftKeyboardFacility;
 import org.chromium.chrome.test.transit.tabmodel.TabGroupCreatedCondition;
+import org.chromium.chrome.test.transit.tabmodel.TabGroupExistsCondition;
 import org.chromium.chrome.test.transit.tabmodel.TabGroupUtil;
 import org.chromium.chrome.test.util.TabBinningUtil;
 import org.chromium.components.tab_groups.TabGroupColorId;
@@ -101,6 +101,9 @@ public class NewTabGroupDialogFacility<
             initTabGroupCreatedCondition();
         } else {
             titleInputElement = declareView(createTitleViewSpec(), inDialogOption());
+            declareEnterCondition(
+                    new TabGroupExistsCondition(
+                            getHostStation().tabGroupModelFilterElement, mTabIdsToGroup));
         }
 
         dialogElement =
@@ -134,7 +137,6 @@ public class NewTabGroupDialogFacility<
         doneButtonElement = declareView(withId(R.id.positive_button), inDialogOption());
     }
 
-    @NonNull
     private ViewSpec<View> createTitleViewSpec() {
         return viewSpec(
                 withId(R.id.title_input_text), isAssignableFrom(EditText.class), withText(mTitle));
