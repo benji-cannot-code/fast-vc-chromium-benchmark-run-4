d438ef8c745e46fd96eba735fcac78d54e5b5cae
Fiona Macintosh <fmacintosh@google.com>
2025-12-10 14:37:35 -0800

    [3PCD] Remove pref check from IsTrackingProtection3pcdEnabled
    Bug: b:462419925
    Change-Id: I1070242aca29836c0363bd3e6c060c4e4a323c5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7248066
    Commit-Queue: Fiona Macintosh <fmacintosh@google.com>
    Reviewed-by: Kevin Graney <kmg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1556990}

--- components/privacy_sandbox/tracking_protection_settings_unittest.cc
+++ components/privacy_sandbox/tracking_protection_settings_unittest.cc
@@ -27,10 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace privacy_sandbox {
 namespace {
 
-MATCHER_P(IsSameSite, site, "") {
-  return net::SchemefulSite::IsSameSite(site, arg);
-}
-
 class MockTrackingProtectionSettingsObserver
     : public TrackingProtectionSettingsObserver {
  public:
@@ -75,33 +71,6 @@ class TrackingProtectionSettingsTest : public testing::Test {
   std::unique_ptr<TrackingProtectionSettings> tracking_protection_settings_;
   base::test::SingleThreadTaskEnvironment task_environment_;
 };
-
-// Gets prefs
-
-TEST_F(TrackingProtectionSettingsTest, ReturnsTrackingProtection3pcdStatus) {
-  EXPECT_FALSE(
-      tracking_protection_settings()->IsTrackingProtection3pcdEnabled());
-  prefs()->SetBoolean(prefs::kTrackingProtection3pcdEnabled, true);
-  EXPECT_TRUE(
-      tracking_protection_settings()->IsTrackingProtection3pcdEnabled());
-}
-
-TEST_F(TrackingProtectionSettingsTest, AreAll3pcBlockedTrueInIncognito) {
-  prefs()->SetBoolean(prefs::kTrackingProtection3pcdEnabled, true);
-  EXPECT_TRUE(TrackingProtectionSettings(prefs(),
-                                         /*is_incognito=*/true)
-                  .AreAllThirdPartyCookiesBlocked());
-  EXPECT_FALSE(TrackingProtectionSettings(prefs(),
-                                          /*is_incognito=*/false)
-                   .AreAllThirdPartyCookiesBlocked());
-}
-
-TEST_F(TrackingProtectionSettingsTest, AreAll3pcBlockedFalseOutside3pcd) {
-  prefs()->SetBoolean(prefs::kTrackingProtection3pcdEnabled, false);
-  prefs()->SetBoolean(prefs::kBlockAll3pcToggleEnabled, true);
-  EXPECT_FALSE(
-      tracking_protection_settings()->AreAllThirdPartyCookiesBlocked());
-}
 
 // Calls observers
 
