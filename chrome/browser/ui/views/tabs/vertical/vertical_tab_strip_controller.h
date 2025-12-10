91eae41338e1eb40827e1fb7bf3f18f5d560dedb
Kunal Daftari <kunaldaftari@google.com>
2025-12-10 10:27:17 -0800

    [Vertical Tabs] Adding Basic Tab Interactions
    This CL works to add basic interactions to the vertical tab strip. This
    should include left-clicking, middle-clicking, and other tab selection
    methods.
    
    Bug: 465188840
    Change-Id: I24865f9a856df9c0593b8ae0487e6e9a90195a74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7212920
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Kunal Daftari <kunaldaftari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1556845}

--- chrome/browser/ui/views/tabs/vertical/vertical_tab_strip_controller.h
+++ chrome/browser/ui/views/tabs/vertical/vertical_tab_strip_controller.h
@@ -7,10 +7,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_VIEWS_TABS_VERTICAL_VERTICAL_TAB_STRIP_CONTROLLER_H_
 
 #include "chrome/browser/ui/tabs/tab_menu_model_factory.h"
+#include "chrome/browser/ui/tabs/tab_strip_user_gesture_details.h"
 #include "chrome/browser/ui/views/tabs/tab_context_menu_controller.h"
 
 class BrowserView;
 class TabCollectionNode;
+
+namespace tabs {
+class TabInterface;
+}
 
 namespace views {
 class View;
@@ -33,6 +38,13 @@ class VerticalTabStripController : public TabContextMenuController::Delegate {
                               views::View* source,
                               const gfx::Point& point,
                               ui::mojom::MenuSourceType source_type);
+
+  void SelectTab(const tabs::TabInterface* tab_interface,
+                 const TabStripUserGestureDetails& event);
+  void CloseTab(const tabs::TabInterface* tab_interface);
+  void ToggleSelected(const tabs::TabInterface* tab_interface);
+  void AddSelectionFromAnchorTo(const tabs::TabInterface* tab_interface);
+  void ExtendSelectionTo(const tabs::TabInterface* tab_interface);
 
   TabContextMenuController* GetTabContextMenuController() {
     return context_menu_controller_.get();
