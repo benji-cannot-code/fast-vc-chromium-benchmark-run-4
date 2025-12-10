a6753a99cb405c7968cbbc4b11b182d4a3f175c1
Nikhil Jakhar <nikhiljakhar@google.com>
2025-12-09 23:05:58 -0800

    legion: Refactor Legion Client request options into a struct
    This change introduces a `RequestOptions` struct to encapsulate
    parameters like timeout for `SendTextRequest` and
    `SendGenerateContentRequest`. This allows for easier extension with more
    options in the future without adding more overloaded methods. The
    existing overloads taking a `base::TimeDelta` are replaced by a single
    method taking the `RequestOptions` struct.
    
    Bug: b:466355215
    Change-Id: Iaaeae2df4b04661edba5c2d0ab9cbd29b5c4f39e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7236967
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Nikhil Jakhar <nikhiljakhar@google.com>
    Cr-Commit-Position: refs/heads/main@{#1556555}

--- chrome/browser/optimization_guide/legion_model_execution_fetcher_unittest.cc
+++ chrome/browser/optimization_guide/legion_model_execution_fetcher_unittest.cc
@@ -21,33 +21,21 @@ class MockLegionClient : public legion::Client {
  public:
   MOCK_METHOD(void,
               EstablishSession,
-              (legion::Client::OnEstablishSessionCompletedCallback),
+              (OnEstablishSessionCompletedCallback callback),
               (override));
   MOCK_METHOD(void,
               SendTextRequest,
-              (legion::proto::FeatureName,
-               const std::string&,
-               legion::Client::OnTextRequestCompletedCallback),
-              (override));
-  MOCK_METHOD(void,
-              SendTextRequest,
-              (legion::proto::FeatureName,
-               const std::string&,
-               legion::Client::OnTextRequestCompletedCallback,
-               base::TimeDelta),
+              (legion::proto::FeatureName feature_name,
+               const std::string& text,
+               OnTextRequestCompletedCallback callback,
+               const RequestOptions& options),
               (override));
   MOCK_METHOD(void,
               SendGenerateContentRequest,
-              (legion::proto::FeatureName,
-               const legion::proto::GenerateContentRequest&,
-               legion::Client::OnGenerateContentRequestCompletedCallback),
-              (override));
-  MOCK_METHOD(void,
-              SendGenerateContentRequest,
-              (legion::proto::FeatureName,
-               const legion::proto::GenerateContentRequest&,
-               legion::Client::OnGenerateContentRequestCompletedCallback,
-               base::TimeDelta),
+              (legion::proto::FeatureName feature_name,
+               const legion::proto::GenerateContentRequest& request,
+               OnGenerateContentRequestCompletedCallback callback,
+               const RequestOptions& options),
               (override));
 };
 
@@ -82,10 +70,13 @@ TEST_F(LegionModelExecutionFetcherTest, ConvertsZeroStateSuggestionsRequest) {
       mock_legion_client_,
       SendTextRequest(
           testing::Eq(legion::proto::FEATURE_NAME_CHROME_ZERO_STATE_SUGGESTION),
-          testing::Eq(expected_prompt), testing::_))
+          testing::Eq(expected_prompt), testing::_,
+          testing::Field(&legion::Client::RequestOptions::timeout,
+                         legion::Client::kDefaultTimeout)))
       .WillOnce([](legion::proto::FeatureName feature_name,
                    const std::string& request,
-                   legion::Client::OnTextRequestCompletedCallback callback) {});
+                   legion::Client::OnTextRequestCompletedCallback callback,
+                   const legion::Client::RequestOptions& options) {});
 
   fetcher_->ExecuteModel(ModelBasedCapabilityKey::kZeroStateSuggestions,
                          /*identity_manager=*/nullptr, request,
@@ -116,10 +107,13 @@ TEST_F(LegionModelExecutionFetcherTest,
       mock_legion_client_,
       SendTextRequest(
           testing::Eq(legion::proto::FEATURE_NAME_CHROME_ZERO_STATE_SUGGESTION),
-          testing::Eq(expected_prompt), testing::_))
+          testing::Eq(expected_prompt), testing::_,
+          testing::Field(&legion::Client::RequestOptions::timeout,
+                         legion::Client::kDefaultTimeout)))
       .WillOnce([](legion::proto::FeatureName feature_name,
                    const std::string& request,
-                   legion::Client::OnTextRequestCompletedCallback callback) {});
+                   legion::Client::OnTextRequestCompletedCallback callback,
+                   const legion::Client::RequestOptions& options) {});
 
   fetcher_->ExecuteModel(ModelBasedCapabilityKey::kZeroStateSuggestions,
                          /*identity_manager=*/nullptr, request,
@@ -133,10 +127,13 @@ TEST_F(LegionModelExecutionFetcherTest, ConvertsZeroStateSuggestionsResponse) {
       mock_legion_client_,
       SendTextRequest(
           testing::Eq(legion::proto::FEATURE_NAME_CHROME_ZERO_STATE_SUGGESTION),
-          testing::_, testing::_))
+          testing::_, testing::_,
+          testing::Field(&legion::Client::RequestOptions::timeout,
+                         legion::Client::kDefaultTimeout)))
       .WillOnce([&](legion::proto::FeatureName feature_name,
                     const std::string& request,
-                    legion::Client::OnTextRequestCompletedCallback callback) {
+                    legion::Client::OnTextRequestCompletedCallback callback,
+                    const legion::Client::RequestOptions& options) {
         std::move(callback).Run(base::ok(legion_response));
       });
 
--- components/legion/client_impl_unittest.cc
+++ components/legion/client_impl_unittest.cc
@@ -148,7 +148,7 @@ TEST_F(ClientImplTest, SendTextRequestSuccess) {
 
   base::test::TestFuture<base::expected<std::string, ErrorCode>> future;
   client_->SendTextRequest(proto::FeatureName::FEATURE_NAME_UNSPECIFIED,
-                           "some text", future.GetCallback());
+                           "some text", future.GetCallback(), /*options=*/{});
 
   const auto& result = future.Get();
   ASSERT_TRUE(result.has_value());
@@ -171,7 +171,7 @@ TEST_F(ClientImplTest, SendTextRequestWriteFails) {
 
   base::test::TestFuture<base::expected<std::string, ErrorCode>> future;
   client_->SendTextRequest(proto::FeatureName::FEATURE_NAME_UNSPECIFIED,
-                           "some text", future.GetCallback());
+                           "some text", future.GetCallback(), /*options=*/{});
 
   const auto& result = future.Get();
   ASSERT_FALSE(result.has_value());
@@ -213,7 +213,7 @@ TEST_F(ClientImplTest, IgnoresResponseWithUnknownRequestId) {
 
   base::test::TestFuture<base::expected<std::string, ErrorCode>> future;
   client_->SendTextRequest(proto::FeatureName::FEATURE_NAME_UNSPECIFIED,
-                           "some text", future.GetCallback());
+                           "some text", future.GetCallback(), /*options=*/{});
 
   // Run all pending tasks. The response callback in the client should have been
   // called, but it should have done nothing.
@@ -239,7 +239,7 @@ TEST_F(ClientImplTest, SecureChannelRecreation) {
   // Send a request that will fail.
   base::test::TestFuture<base::expected<std::string, ErrorCode>> future;
   client_->SendTextRequest(proto::FeatureName::FEATURE_NAME_UNSPECIFIED,
-                           "some text", future.GetCallback());
+                           "some text", future.GetCallback(), /*options=*/{});
 
   // Simulate a network error from the secure channel. This should trigger a
   // channel recreation.
@@ -280,7 +280,8 @@ TEST_F(ClientImplTest, SecureChannelRecreation) {
 
   base::test::TestFuture<base::expected<std::string, ErrorCode>> second_future;
   client_->SendTextRequest(proto::FeatureName::FEATURE_NAME_UNSPECIFIED,
-                           "some other text", second_future.GetCallback());
+                           "some other text", second_future.GetCallback(),
+                           /*options=*/{});
 
   const auto& second_result = second_future.Get();
   ASSERT_TRUE(second_result.has_value());
@@ -303,7 +304,7 @@ TEST_F(ClientImplTest, SendTextRequestTimeout) {
   base::test::TestFuture<base::expected<std::string, ErrorCode>> future;
   client_->SendTextRequest(proto::FeatureName::FEATURE_NAME_UNSPECIFIED,
                            "some text", future.GetCallback(),
-                           base::Seconds(10));
+                           /*options=*/{.timeout = base::Seconds(10)});
 
   // The request is sent but no response is received yet.
   ASSERT_FALSE(future.IsReady());
@@ -335,7 +336,7 @@ TEST_F(ClientImplTest, SendTextRequestResponseAfterTimeout) {
   base::test::TestFuture<base::expected<std::string, ErrorCode>> future;
   client_->SendTextRequest(proto::FeatureName::FEATURE_NAME_UNSPECIFIED,
                            "some text", future.GetCallback(),
-                           base::Seconds(10));
+                           /*options=*/{.timeout = base::Seconds(10)});
 
   // The request is sent but no response is received yet.
   ASSERT_FALSE(future.IsReady());
@@ -398,7 +399,7 @@ TEST_P(ClientImplSendTextRequestSecureChannelErrorTest, SendTextRequestError) {
 
   base::test::TestFuture<base::expected<std::string, ErrorCode>> future;
   client_->SendTextRequest(proto::FeatureName::FEATURE_NAME_UNSPECIFIED,
-                           "some text", future.GetCallback());
+                           "some text", future.GetCallback(), /*options=*/{});
 
   const auto& result = future.Get();
   ASSERT_FALSE(result.has_value());
@@ -434,7 +435,7 @@ TEST_P(ClientImplSendTextRequestResponseErrorTest, SendTextRequestError) {
 
   base::test::TestFuture<base::expected<std::string, ErrorCode>> future;
   client_->SendTextRequest(proto::FeatureName::FEATURE_NAME_UNSPECIFIED,
-                           "some text", future.GetCallback());
+                           "some text", future.GetCallback(), /*options=*/{});
 
   const auto& result = future.Get();
   ASSERT_FALSE(result.has_value());
@@ -508,7 +509,7 @@ TEST_P(ClientImplSendGenerateContentRequestErrorTest,
       future;
   client_->SendGenerateContentRequest(
       proto::FeatureName::FEATURE_NAME_UNSPECIFIED,
-      proto::GenerateContentRequest(), future.GetCallback());
+      proto::GenerateContentRequest(), future.GetCallback(), /*options=*/{});
 
   const auto& result = future.Get();
   ASSERT_FALSE(result.has_value());
@@ -582,7 +583,7 @@ TEST_F(ClientImplTest, EstablishSessionFailureFailsPendingRequests) {
   base::test::TestFuture<base::expected<std::string, ErrorCode>> text_future;
   client_->SendTextRequest(
       proto::FeatureName::FEATURE_NAME_DEMO_GEMINI_GENERATE_CONTENT,
-      "some text", text_future.GetCallback());
+      "some text", text_future.GetCallback(), /*options=*/{});
 
   // Attempt to establish the session, which will fail.
   base::test::TestFuture<base::expected<void, ErrorCode>> establish_future;
