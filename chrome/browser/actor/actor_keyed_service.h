bbe59b1ff18d3f1aac36ee7dd263dcee8836f81f
David Bokan <bokan@chromium.org>
2025-12-10 06:39:47 -0800

    Actor: Plumb context fetch errors out through proto
    Plumbs more details about what failed in a tab observation through the
    TabObservation proto. That way we can stop bucketing all observation
    errors as a "ScreenshotTimeout".
    
    Bug: b:462190108,b:465208148
    Change-Id: If1995aeb5f7199af05ca13f42297cfe739d09465
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7226893
    Commit-Queue: David Bokan <bokan@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1556714}

--- chrome/browser/actor/actor_keyed_service.h
+++ chrome/browser/actor/actor_keyed_service.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/actor/actor_task.h"
 #include "chrome/browser/actor/actor_task_delegate.h"
 #include "chrome/browser/actor/aggregated_journal.h"
+#include "chrome/browser/page_content_annotations/multi_source_page_context_fetcher.h"
 #include "chrome/browser/profiles/profile_observer.h"
 #include "chrome/common/actor/action_result.h"
 #include "chrome/common/actor/task_id.h"
@@ -33,10 +34,6 @@ class Profile;
 namespace content {
 class BrowserContext;
 }  // namespace content
-
-namespace page_content_annotations {
-struct FetchPageContextResult;
-}  // namespace page_content_annotations
 
 namespace actor {
 namespace ui {
@@ -123,15 +120,20 @@ class ActorKeyedService : public KeyedService,
 
   Profile* GetProfile();
 
-  using TabObservationResult = base::expected<
-      std::unique_ptr<page_content_annotations::FetchPageContextResult>,
-      std::string>;
-
+  using TabObservationResult =
+      page_content_annotations::FetchPageContextResultCallbackArg;
   // Request a TabObservation be generated from the given tab.
   void RequestTabObservation(
       tabs::TabInterface& tab,
       TaskId task_id,
       base::OnceCallback<void(TabObservationResult)> callback);
+
+  // A TabObservationResult may return the successful side of the base::expected
+  // but the partial errors in the FetchPageContextResult may be considered a
+  // failing result for actor. Returns a failing string in any case the result
+  // isn't usable for actor. Returns nullopt if the result is fully successful.
+  static std::optional<std::string> ExtractErrorMessageIfFailed(
+      const TabObservationResult& result);
 
   using TaskStateChangedCallback =
       base::RepeatingCallback<void(TaskId, ActorTask::State)>;
