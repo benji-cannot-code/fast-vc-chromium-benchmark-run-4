0825e3098b7a32bad350637193c27ae6d29e169f
Wenyu Fu <wenyufu@chromium.org>
2025-12-10 00:54:02 -0800

    [A11y] Remove announceForAccessibility for PageInfoContainer
    Replace with ViewCompat.setAccessibilityPaneTitle.
    
    Bug: 376756579
    Change-Id: I721356782347b9c671c33b616e6399638f579181
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7241917
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Auto-Submit: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1556591}

--- components/page_info/android/java/src/org/chromium/components/page_info/PageInfoContainer.java
+++ components/page_info/android/java/src/org/chromium/components/page_info/PageInfoContainer.java
@@ -129,7 +129,8 @@ public class PageInfoContainer extends FrameLayout {
         boolean showExpanded = mExpandedUrlTitle.getVisibility() != VISIBLE;
         mExpandedUrlTitle.setVisibility(showExpanded ? VISIBLE : GONE);
         mTruncatedUrlTitle.setVisibility(showExpanded ? GONE : VISIBLE);
-        announceForAccessibility(
+        ViewCompat.setAccessibilityPaneTitle(
+                this,
                 getResources()
                         .getString(
                                 showExpanded
