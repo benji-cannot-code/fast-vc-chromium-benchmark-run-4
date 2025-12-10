f87e29e2ee1d7fa2684dba87234cea0094dea455
Ewann Pelle <ewannpv@chromium.org>
2025-12-10 07:41:36 -0800

    [iOS] Add CloseOtherTabs feature flag
    Bug: 467585183
    Change-Id: I6d5f6dc6054d448220f4590a2de06869820d8926
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7246253
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1556759}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -1208,3 +1208,9 @@ BASE_FEATURE(kIOSWebContextMenuNewTitle, base::FEATURE_DISABLED_BY_DEFAULT);
 bool IsIOSWebContextMenuNewTitleEnabled() {
   return base::FeatureList::IsEnabled(kIOSWebContextMenuNewTitle);
 }
+
+BASE_FEATURE(kCloseOtherTabs, base::FEATURE_DISABLED_BY_DEFAULT);
+
+bool IsCloseOtherTabsEnabled() {
+  return base::FeatureList::IsEnabled(kCloseOtherTabs);
+}
