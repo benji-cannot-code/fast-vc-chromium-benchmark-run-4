18ca128e9d75b888a81d62518737f24eafbaf4db
Julia Przybytniowska <przybytniowska@google.com>
2025-12-10 12:36:53 -0800

    Add webstorePrivate API to log when CEC promotion banner was displayed.
    This function allows the CWS to signal when the Chrome Enterprise Core
    (CEC) promotion banner is displayed to the user, enabling metric logging
    for these impressions.
    
    BUG=b:465700382
    
    Change-Id: If3812c8e5344d5c65af79d89f84b34caf921c07b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7229147
    Reviewed-by: Tim <tjudkins@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Julia Przybytniowska <przybytniowska@google.com>
    Reviewed-by: Austin Zhang <austinzzr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1556918}

--- chrome/browser/extensions/api/webstore_private/webstore_private_api.cc
+++ chrome/browser/extensions/api/webstore_private/webstore_private_api.cc
@@ -1497,6 +1497,18 @@ void WebstorePrivateShouldShowEnterprisePromotionBannerFunction::
 
   Respond(WithArguments(api::webstore_private::ToString(api_promotion_type)));
 }
+
+WebstorePrivateLogEnterprisePromoShownFunction::
+    WebstorePrivateLogEnterprisePromoShownFunction() = default;
+
+ExtensionFunction::ResponseAction
+WebstorePrivateLogEnterprisePromoShownFunction::Run() {
+  base::UmaHistogramEnumeration(
+      "Enterprise.CwsPromotionBannerEvent",
+      enterprise::CwsPromotionBannerEvent::kDisplayed);
+
+  return RespondNow(NoArguments());
+}
 #endif  // !BUILDFLAG(IS_ANDROID)
 
 }  // namespace extensions
