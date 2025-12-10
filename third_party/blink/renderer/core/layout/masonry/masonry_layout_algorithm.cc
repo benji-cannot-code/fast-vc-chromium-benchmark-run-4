20719504ca05405b0e12a16f684b92f9896d7a89
Alison Maher <almaher@microsoft.com>
2025-12-10 14:04:36 -0800

    [Masonry] Update MasonryRunningPositions to use GridLanes naming
    Update the masonry_running_positions* files and objects to use the
    grid-lanes naming to match the new display type.
    
    This change should have no functional impact. More files to be updated
    in future CLs.
    
    Bug: 343257585
    Change-Id: I42430828166044b2d1b269ad9a6639fa75732b4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7245742
    Reviewed-by: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Commit-Queue: Alison Maher <almaher@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1556934}

--- third_party/blink/renderer/core/layout/masonry/masonry_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/masonry/masonry_layout_algorithm.cc
@@ -15,8 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/grid/grid_track_sizing_algorithm.h"
 #include "third_party/blink/renderer/core/layout/layout_utils.h"
 #include "third_party/blink/renderer/core/layout/logical_box_fragment.h"
+#include "third_party/blink/renderer/core/layout/masonry/grid_lanes_running_positions.h"
 #include "third_party/blink/renderer/core/layout/masonry/layout_grid_lanes.h"
-#include "third_party/blink/renderer/core/layout/masonry/masonry_running_positions.h"
 #include "third_party/blink/renderer/core/layout/masonry/stacking_baseline_accumulator.h"
 
 namespace blink {
@@ -86,7 +86,7 @@ MinMaxSizesResult MasonryLayoutAlgorithm::ComputeMinMaxSizes(
         return BorderScrollbarPadding().InlineSum();
       }
 
-      MasonryRunningPositions running_positions(
+      GridLanesRunningPositions running_positions(
           track_collection, style,
           ResolveItemToleranceForMasonry(style, masonry_available_size_),
           collapsed_track_indexes);
@@ -157,7 +157,7 @@ const LayoutResult* MasonryLayoutAlgorithm::Layout() {
   }
 
   if (!masonry_items.IsEmpty()) {
-    MasonryRunningPositions running_positions(
+    GridLanesRunningPositions running_positions(
         track_collection, Style(),
         ResolveItemToleranceForMasonry(Style(), masonry_available_size_),
         collapsed_track_indexes);
@@ -289,7 +289,7 @@ void MasonryLayoutAlgorithm::PlaceMasonryItems(
     GridSizingTrackCollection& track_collection,
     GridItems& masonry_items,
     wtf_size_t start_offset,
-    MasonryRunningPositions& running_positions,
+    GridLanesRunningPositions& running_positions,
     std::optional<SizingConstraint> sizing_constraint) {
   const auto& border_scrollbar_padding = BorderScrollbarPadding();
   const auto& container_space = GetConstraintSpace();
@@ -386,7 +386,7 @@ void MasonryLayoutAlgorithm::PlaceMasonryItems(
     bool item_moved_to_earlier_opening = false;
     if (is_dense_packing) {
       LayoutUnit updated_item_start_offset =
-          running_positions.GetEligibleTrackOpeningAndUpdateMasonryItemSpan(
+          running_positions.GetEligibleTrackOpeningAndUpdateGridLanesItemSpan(
               start_offset, masonry_item,
               /*item_stacking_axis_contribution=*/fragment_size +
                   stacking_axis_gap,
--- third_party/blink/renderer/core/layout/masonry/masonry_layout_algorithm.h
+++ third_party/blink/renderer/core/layout/masonry/masonry_layout_algorithm.h
@@ -18,7 +18,7 @@ class GridItems;
 class GridLayoutData;
 class GridLineResolver;
 class GridSizingTrackCollection;
-class MasonryRunningPositions;
+class GridLanesRunningPositions;
 enum class GridItemContributionType;
 enum class SizingConstraint;
 struct BoxStrut;
@@ -58,7 +58,7 @@ class CORE_EXPORT MasonryLayoutAlgorithm
       GridSizingTrackCollection& track_collection,
       GridItems& masonry_items,
       wtf_size_t start_offset,
-      MasonryRunningPositions& running_positions,
+      GridLanesRunningPositions& running_positions,
       std::optional<SizingConstraint> sizing_constraint = std::nullopt);
 
   // Places all out-of-flow (OOF) masonry items. For each item, this method
