567e282e5810d51c738541642d4b0803fc759d1e
Ibrahim Kanouche <kanouche@google.com>
2025-12-11 13:02:42 -0800

    [iOS] Fix fieldtrial config For Proactive Suggestions Framework.
    This CL enables the Proactive Suggestions Framework in the fieldtrial
    testing config.
    
    This CL also disables the Proactive Suggestions Framework for several
    EGTests in `ContextualPanelTestCase`, `TranslateInfobarTestCase` and
    `SaveProfileEGTest` that are currently failing when the framework is
    enabled.
    
    Bug: 446657345
    Change-Id: Iee931d455f05aeded2d63eb8ff1dc7366bf36d27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7223690
    Reviewed-by: Nohemi Fernandez <fernandex@google.com>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Ibrahim Kanouche <kanouche@google.com>
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1557640}

--- ios/chrome/browser/autofill/ui_bundled/save_profile_egtest.mm
+++ ios/chrome/browser/autofill/ui_bundled/save_profile_egtest.mm
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/infobars/ui_bundled/banners/infobar_banner_constants.h"
 #import "ios/chrome/browser/infobars/ui_bundled/infobar_earl_grey_ui_test_util.h"
 #import "ios/chrome/browser/infobars/ui_bundled/modals/infobar_address_profile_modal_constants.h"
+#import "ios/chrome/browser/intelligence/features/features.h"
 #import "ios/chrome/browser/metrics/model/metrics_app_interface.h"
 #import "ios/chrome/browser/signin/model/fake_system_identity.h"
 #import "ios/chrome/grit/ios_branded_strings.h"
@@ -217,6 +218,9 @@ void TypeTextInXframeField(NSString* fieldID, NSString* text) {
           isRunningTest:@selector(testUserData_LocalHideBottomSheetOnCancel)]) {
     // These test cases need a badge.
     config.features_disabled.push_back(kAutofillBadgeRemoval);
+    // TODO(crbug.com/467331873): Re-enable this when the test is updated for
+    // PSF.
+    config.features_disabled.push_back(kProactiveSuggestionsFramework);
   }
 
   config.features_disabled.push_back(
