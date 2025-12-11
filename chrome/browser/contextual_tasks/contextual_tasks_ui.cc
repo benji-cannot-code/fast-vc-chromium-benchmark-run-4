0789014ec1e32ce1058be0788d8aa8640b7eb57c
Diana OuYang <dianaou@google.com>
2025-12-11 10:19:33 -0800

    Allow Contextual Tasks to Receive Dark Mode Styling
    - Receive dark mode in contextual tasks ui
    - Pass a darkmode boolean to the app.ts file
    - Dark mode should be persisted as the setting active when contextual tasks was first called.
    
    Demo: https://screencast.googleplex.com/cast/NjE4NDgwOTkyMjU2MDAwMHw4ODFiOWJmZi1hNQ
    Bug: b:461911143
    Change-Id: I5d1fcc5ff1c6743883c1a68ef23b99a895411558
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7204822
    Commit-Queue: Diana OuYang <dianaou@google.com>
    Reviewed-by: Josiah Kiehl <kiehl@google.com>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1557520}

--- chrome/browser/contextual_tasks/contextual_tasks_ui.cc
+++ chrome/browser/contextual_tasks/contextual_tasks_ui.cc
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
+#include "chrome/browser/themes/theme_service.h"
+#include "chrome/browser/themes/theme_service_factory.h"
 #include "chrome/browser/ui/webui/cr_components/searchbox/searchbox_handler.h"
 #include "chrome/browser/ui/webui/new_tab_page/composebox/variations/composebox_fieldtrial.h"
 #include "chrome/browser/ui/webui/webui_embedding_context.h"
@@ -211,6 +213,9 @@ ContextualTasksUI::ContextualTasksUI(content::WebUI* web_ui)
       contextual_tasks::GetIsSteadyComposeboxVoiceSearchEnabled());
   source->AddBoolean("composeboxShowContextMenuTabPreviews", false);
   source->AddBoolean("composeboxContextMenuEnableMultiTabSelection", false);
+  source->AddBoolean("darkMode",
+                     ThemeServiceFactory::GetForProfile(Profile::FromWebUI(web_ui))
+                                ->BrowserUsesDarkColors());
   source->AddString(
       "composeboxSource",
       contextual_search::ContextualSearchMetricsRecorder::
--- chrome/browser/resources/contextual_tasks/app.ts
+++ chrome/browser/resources/contextual_tasks/app.ts
@@ -44,6 +44,10 @@ export class ContextualTasksAppElement extends CrLitElement {
       threadUrl_: {type: String},
       threadTitle_: {type: String},
       contextTabs_: {type: Array},
+      darkMode_: {
+        type: Boolean,
+        reflect: true,
+      },
       showComposebox_: {type: Boolean, reflect: true},
     };
   }
@@ -51,6 +55,7 @@ export class ContextualTasksAppElement extends CrLitElement {
   private browserProxy_: BrowserProxy = BrowserProxyImpl.getInstance();
   accessor isShownInTab_: boolean = true;  // Most start in a tab.
   protected accessor threadUrl_: string = '';
+  protected accessor darkMode_: boolean = loadTimeData.getBoolean('darkMode');
   private pendingUrl_: string = '';
   protected accessor threadTitle_: string = '';
   protected accessor contextTabs_: Tab[] = [];
@@ -189,7 +194,8 @@ export class ContextualTasksAppElement extends CrLitElement {
   private async updateCommonSearchParams() {
     // TODO(crbug.com/463729504): Add support for dark mode.
     const {params} = await this.browserProxy_.handler.getCommonSearchParams(
-        /*isDarkMode=*/ false, /*isSidePanel=*/ !this.isShownInTab_);
+        /*isDarkMode=*/ this.darkMode_,
+        /*isSidePanel=*/ !this.isShownInTab_);
     this.commonSearchParams_ = params;
     this.maybeLoadPendingUrl_();
   }
