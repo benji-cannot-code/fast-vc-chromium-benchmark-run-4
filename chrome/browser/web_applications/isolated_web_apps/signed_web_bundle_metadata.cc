1e9fc0fbd6babd58e91d0c02b12e958669c76913
Simon Hangl <simonha@google.com>
2025-12-11 09:18:50 -0800

    Show enterprise name in IWA user installer
    In a previous CL [1] a subset of the user install allowlist
    was introduced. This allowlist also contains the enterprise name
    and hence this information can now be shown in the IWA user
    installer.
    
    Bug: 467052022
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/7237913
    
    Change-Id: Id8b5290ab568128c40f4a17c352c9e3f0384e023
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7246393
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Commit-Queue: Simon Hangl <simonha@google.com>
    Cr-Commit-Position: refs/heads/main@{#1557471}

--- chrome/browser/web_applications/isolated_web_apps/signed_web_bundle_metadata.cc
+++ chrome/browser/web_applications/isolated_web_apps/signed_web_bundle_metadata.cc
@@ -11,12 +11,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback.h"
 #include "base/memory/weak_ptr.h"
+#include "base/strings/utf_string_conversions.h"
 #include "base/types/expected.h"
 #include "base/types/expected_macros.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/web_applications/callback_utils.h"
 #include "chrome/browser/web_applications/isolated_web_apps/commands/isolated_web_app_install_command_helper.h"
 #include "chrome/browser/web_applications/isolated_web_apps/jobs/prepare_install_info_job.h"
+#include "chrome/browser/web_applications/isolated_web_apps/runtime_data/chrome_iwa_runtime_data_provider.h"
 #include "chrome/browser/web_applications/web_app_install_info.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
 #include "chrome/browser/web_applications/web_contents/web_app_data_retriever.h"
@@ -129,7 +131,6 @@ void SignedWebBundleMetadata::Create(
   auto fetcher = std::make_unique<WebAppInstallInfoFetcher>(profile, provider,
                                                             url_info, source);
   WebAppInstallInfoFetcher& fetcher_ref = *fetcher.get();
-
   fetcher_ref.FetchAndReply(base::BindOnce(
       [](const IsolatedWebAppUrlInfo& url_info,
          const IwaSourceBundleWithMode& source,
@@ -138,10 +139,18 @@ void SignedWebBundleMetadata::Create(
         std::move(callback).Run(install_info.transform(
             [&url_info, &source](const WebAppInstallInfo& install_info)
                 -> SignedWebBundleMetadata {
+              const ChromeIwaRuntimeDataProvider::UserInstallAllowlistItemData*
+                  user_install_data =
+                      web_app::ChromeIwaRuntimeDataProvider::GetInstance()
+                          .GetUserInstallAllowlistData(
+                              url_info.web_bundle_id().id());
               return SignedWebBundleMetadata(
                   url_info, source, install_info.title.value(),
                   install_info.isolated_web_app_version(),
-                  install_info.GetIconBitmapsForSecureSurfaces());
+                  install_info.GetIconBitmapsForSecureSurfaces(),
+                  user_install_data
+                      ? std::optional(user_install_data->enterprise_name)
+                      : std::nullopt);
             }));
       },
       url_info, source,
@@ -155,9 +164,10 @@ SignedWebBundleMetadata SignedWebBundleMetadata::CreateForTesting(
     const IwaSourceBundleWithMode& source,
     const std::u16string& app_name,
     const IwaVersion& version,
-    DialogImageInfo image_info) {
+    DialogImageInfo image_info,
+    const std::optional<std::string>& enterprise_name) {
   return SignedWebBundleMetadata(url_info, source, app_name, version,
-                                 std::move(image_info));
+                                 std::move(image_info), enterprise_name);
 }
 
 SignedWebBundleMetadata::SignedWebBundleMetadata(
@@ -165,11 +175,13 @@ SignedWebBundleMetadata::SignedWebBundleMetadata(
     const IwaSourceBundleWithMode& source,
     const std::u16string& app_name,
     const IwaVersion& version,
-    DialogImageInfo image_info)
+    DialogImageInfo image_info,
+    const std::optional<std::string>& enterprise_name)
     : url_info_(url_info),
       app_name_(app_name),
       version_(version),
-      image_info_(std::move(image_info)) {}
+      image_info_(std::move(image_info)),
+      enterprise_name_(enterprise_name) {}
 
 SignedWebBundleMetadata::~SignedWebBundleMetadata() = default;
 
--- chrome/browser/web_applications/isolated_web_apps/signed_web_bundle_metadata.h
+++ chrome/browser/web_applications/isolated_web_apps/signed_web_bundle_metadata.h
@@ -43,7 +43,8 @@ class SignedWebBundleMetadata {
       const IwaSourceBundleWithMode& source,
       const std::u16string& app_name,
       const IwaVersion& version,
-      DialogImageInfo image_info);
+      DialogImageInfo image_info,
+      const std::optional<std::string>& enterprise_name);
 
   ~SignedWebBundleMetadata();
   SignedWebBundleMetadata(const SignedWebBundleMetadata&);
@@ -59,6 +60,10 @@ class SignedWebBundleMetadata {
 
   const DialogImageInfo& image_info() const { return image_info_; }
 
+  const std::optional<std::string>& enterprise_name() const {
+    return enterprise_name_;
+  }
+
   bool operator==(const SignedWebBundleMetadata& other) const;
 
  private:
@@ -66,12 +71,14 @@ class SignedWebBundleMetadata {
                           const IwaSourceBundleWithMode& source,
                           const std::u16string& app_name,
                           const IwaVersion& version,
-                          DialogImageInfo image_info);
+                          DialogImageInfo image_info,
+                          const std::optional<std::string>& enterprise_name);
 
   IsolatedWebAppUrlInfo url_info_;
   std::u16string app_name_;
   IwaVersion version_;
   DialogImageInfo image_info_;
+  std::optional<std::string> enterprise_name_;
 };
 
 }  // namespace web_app
