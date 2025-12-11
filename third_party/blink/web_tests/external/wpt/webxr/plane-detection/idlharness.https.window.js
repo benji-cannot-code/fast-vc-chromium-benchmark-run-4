fbbd27eb64c293400f09fbb56cb1e481f9bf9815
Alexander Cooper <alcooper@chromium.org>
2025-12-11 12:44:41 -0800

    Add missing blink-side features to plane-detection API
    Two features have been added to the plane-detection spec since we last
    did work on it. Namely these are the `semanticLabel` attribute and the
    `initiateRoomCapture` method. This adds the implementation for both of
    those, with `semanticLabel` returning an empty string as allowed by the
    spec for an "unknown" label, and with `initiateRoomCapture` doing the
    appropriate checks before resulting in a successful no-op, as none of
    our runtimes have the ability to request room capture explicitly, again
    as allowed by spec.
    
    Bug: 394636076
    Change-Id: I77f02a4d19b629786a3d01870af0b1a541c3b65c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7248998
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Auto-Submit: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1557631}

---
+++ third_party/blink/web_tests/external/wpt/webxr/plane-detection/idlharness.https.window.js
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+'use strict';
+
+idl_test(
+  ['webxr-plane-detection'],
+  ['webxr', 'dom'],
+  async idl_array => {
+    idl_array.add_objects({
+      // TODO: Add object instances
+    });
+  },
+  'WebXR Plane Detection Module IDL Test'
+);
