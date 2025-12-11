a410a5bf85bed1e653550d4825b9553410523ed5
Justin Lulejian <jlulejian@chromium.org>
2025-12-10 16:14:36 -0800

    [Extensions] Add crash keys to ExtensionFunction::SendResponseImpl
    This adds crash keys to capture the state of did_respond() and
    response_callback_.is_null() immediately before the callback is run, to
    aid in debugging crashes where
    DeveloperPrivateRemoveMultipleExtensionsFunction user dialog options
    might be getting called multiple times.
    
    Bug: 463124104
    Change-Id: I0741fb8e461765ffff6ee68954c8ccaff48b3b1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7243271
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1557030}

--- chrome/browser/extensions/api/developer_private/developer_private_functions_shared.cc
+++ chrome/browser/extensions/api/developer_private/developer_private_functions_shared.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/developer_private/developer_private_functions_shared.h"
 
 #include "base/barrier_closure.h"
+#include "base/debug/crash_logging.h"
 #include "base/files/file_util.h"
 #include "base/memory/ref_counted.h"
 #include "base/metrics/histogram_functions.h"
@@ -1643,6 +1644,10 @@ void DeveloperPrivateRemoveMultipleExtensionsFunction::OnDialogAccepted() {
     ExtensionRegistrar::Get(profile_)->UninstallExtension(
         extension_id, UNINSTALL_REASON_USER_INITIATED, nullptr);
   }
+  // TODO(crbug.com/463124104): Crash keys for `response_callback_` crashes.
+  SCOPED_CRASH_KEY_BOOL("ext", "EF_DP_dialog_did_respond", did_respond());
+  SCOPED_CRASH_KEY_BOOL("ext", "EF_DP_dialog_resp_cb_null",
+                        response_callback_is_null());
   Respond(NoArguments());
 }
 
