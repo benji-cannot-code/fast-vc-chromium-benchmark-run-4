dfa50acdf7ec272e955b9c474f18b85d6a6b1978
Joe Downing <joedow@google.com>
2025-12-11 15:16:00 -0800

    Create a 'CorpSignalingConnector' class to maintain a channel
    We use a wrapper class around the FTL signal strategy to watch
    for network changes and reconnect (w/ a backoff) when a
    machine's online status changes. This CL adds a similar class
    for Corp signaling. The Corp signaling case is a bit simpler
    as it does not need to deal with OAuth errors.
    
    Bug: 359620500
    Change-Id: Ic7371ce80868123e1fcc5619fbe82a304b3d8b17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7252921
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1557722}

--- remoting/host/remoting_me2me_host.cc
+++ remoting/host/remoting_me2me_host.cc
@@ -80,6 +80,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/host/config_file_watcher.h"
 #include "remoting/host/config_watcher.h"
 #include "remoting/host/corp_host_status_logger.h"
+#include "remoting/host/corp_signaling_connector.h"
 #include "remoting/host/crash_process.h"
 #include "remoting/host/create_desktop_interaction_strategy_factory.h"
 #include "remoting/host/desktop_environment.h"
@@ -503,18 +504,18 @@ class HostProcess : public ConfigWatcher::Delegate,
   // Must outlive |signal_strategy_| and |heartbeat_sender_|.
   std::unique_ptr<ZombieHostDetector> zombie_host_detector_;
 
-  // Signal strategies must outlive |ftl_signaling_connector_|.
+  // |signal_strategy_| must outlive |ftl_signaling_connector_|.
   std::unique_ptr<SignalStrategy> signal_strategy_;
-
   std::unique_ptr<FtlSignalingConnector> ftl_signaling_connector_;
+
+  // |corp_signal_strategy_| must outlive |corp_signaling_connector_|.
+  std::unique_ptr<SignalStrategy> corp_signal_strategy_;
+  std::unique_ptr<CorpSignalingConnector> corp_signaling_connector_;
 
   std::unique_ptr<HeartbeatSender> heartbeat_sender_;
   std::unique_ptr<FtlHostChangeNotificationListener>
       ftl_host_change_notification_listener_;
   std::unique_ptr<FtlEchoMessageListener> ftl_echo_message_listener_;
-
-  // TODO: joedow - Unify FTL and Corp classes with a single SignalStrategy.
-  std::unique_ptr<SignalStrategy> corp_signal_strategy_;
 
   std::unique_ptr<HostEventLogger> host_event_logger_;
 #if BUILDFLAG(IS_LINUX)
@@ -1768,7 +1769,9 @@ void HostProcess::InitializeSignaling() {
     corp_signal_strategy_ = std::make_unique<CorpSignalStrategy>(
         context_->url_loader_factory(),
         context_->create_client_cert_store_callback(), username, key_pair_);
-    corp_signal_strategy_->Connect();
+    corp_signaling_connector_ =
+        std::make_unique<CorpSignalingConnector>(corp_signal_strategy_.get());
+    corp_signaling_connector_->Start();
   }
 #endif
 
@@ -2110,6 +2113,7 @@ void HostProcess::OnHostOfflineReasonAck(bool success) {
   ftl_echo_message_listener_.reset();
   signal_strategy_.reset();
   corp_signal_strategy_.reset();
+  corp_signaling_connector_.reset();
   zombie_host_detector_.reset();
 
   if (state_ == HOST_GOING_OFFLINE_TO_RESTART) {
