55bcb56187f1579a5a87d8ea3be902e69802e647
Colin Blundell <blundell@chromium.org>
2025-12-11 00:53:36 -0800

    [//gpu] Rename ForOOPRasterContext
    It's more appropriately named ForGPURasterContext() now.
    
    Bug: 443000825
    Change-Id: I06a2dab4ba5ade6b9279530d7dc5a07c7af3253d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7247090
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1557194}

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -1597,7 +1597,7 @@ RenderThreadImpl::SharedCompositorWorkerContextProvider(
       gpu::kGpuFeatureStatusEnabled;
 
   auto shared_memory_limits =
-      support_gpu_rasterization ? gpu::SharedMemoryLimits::ForOOPRasterContext()
+      support_gpu_rasterization ? gpu::SharedMemoryLimits::ForGPURasterContext()
                                 : gpu::SharedMemoryLimits();
   shared_worker_context_provider_ =
       viz::ContextProviderCommandBuffer::CreateForRaster(
