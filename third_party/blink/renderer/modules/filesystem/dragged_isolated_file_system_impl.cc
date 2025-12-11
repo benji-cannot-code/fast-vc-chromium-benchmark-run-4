c77f1a8868a6e4452ee880df173a907e732e0e30
Steinar H. Gunderson <sesse@chromium.org>
2025-12-11 10:25:14 -0800

    Revert "Remove unused DataObject ex-Supplementable members."
    This reverts commit ec12718f993a9542680dee3c6b2e1b36896b9c83.
    (See blink-dev for background.)
    
    Bug: 460165754
    Change-Id: I9628cfa4f3ad29db1c11678cff5cdad16eb4bb8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7245788
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1557529}

--- third_party/blink/renderer/modules/filesystem/dragged_isolated_file_system_impl.cc
+++ third_party/blink/renderer/modules/filesystem/dragged_isolated_file_system_impl.cc
@@ -38,6 +38,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+DraggedIsolatedFileSystemImpl::DraggedIsolatedFileSystemImpl(
+    DataObject& data_object)
+    : data_object_(data_object) {}
+
 DOMFileSystem* DraggedIsolatedFileSystemImpl::GetDOMFileSystem(
     DataObject* host,
     ExecutionContext* execution_context,
@@ -65,13 +69,14 @@ DraggedIsolatedFileSystemImpl* DraggedIsolatedFileSystemImpl::From(
 
 void DraggedIsolatedFileSystemImpl::Trace(Visitor* visitor) const {
   visitor->Trace(filesystems_);
+  visitor->Trace(data_object_);
 }
 
 void DraggedIsolatedFileSystemImpl::PrepareForDataObject(
     DataObject* data_object) {
   DCHECK(IsMainThread());
   DraggedIsolatedFileSystemImpl* file_system =
-      MakeGarbageCollected<DraggedIsolatedFileSystemImpl>();
+      MakeGarbageCollected<DraggedIsolatedFileSystemImpl>(*data_object);
   data_object->SetDraggedIsolatedFileSystemImpl(file_system);
 }
 
