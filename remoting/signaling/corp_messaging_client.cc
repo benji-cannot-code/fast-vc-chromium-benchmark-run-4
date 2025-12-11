a07eec87f4eaf7f61cdebd31b4ec49ff42939265
Joe Downing <joedow@google.com>
2025-12-10 17:57:12 -0800

    Add a basic CorpSignalingStrategy impl which opens a channel
    This CL adds a simple implementation of the CorpSignalStrategy class
    which opens a channel and listens for messages. I am going to add
    a wrapper class in a follow-up CL which will allow me to handle
    signaling messages from either FTL or Corp and then provide that
    wrapper class to JingleSession. Landing a basic Corp impl makes
    that work a bit easier to reason about.
    
    Bug: 359620500
    Change-Id: Ieb8aaa624d2d553ea31af2cb3c68002941a6d1a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7248700
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1557073}

--- remoting/signaling/corp_messaging_client.cc
+++ remoting/signaling/corp_messaging_client.cc
@@ -5,7 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "remoting/signaling/corp_messaging_client.h"
 
+#include <memory>
+#include <string>
 #include <utility>
+#include <variant>
 
 #include "base/functional/callback.h"
 #include "base/functional/callback_helpers.h"
