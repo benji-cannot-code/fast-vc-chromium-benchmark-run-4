824ea69f08ab5d0c4e0a5937ab45b2bc3264392d
Oleh Lamzin <lamzin@google.com>
2025-12-11 03:24:32 -0800

    legion: Handle Mojo sandbox crash for GetHandshakeMessage
    Handle Mojo sandbox crash for GetHandshakeMessage() method.
    
    Bug: b:466305445
    Change-Id: I550d4102aa7372e03ff07e4829e49a944de8a07f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7247128
    Reviewed-by: Nikhil Jakhar <nikhiljakhar@google.com>
    Auto-Submit: Oleh Lamzin <lamzin@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Oleh Lamzin <lamzin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1557272}

--- chrome/browser/legion/secure_session_async_impl_browsertest.cc
+++ chrome/browser/legion/secure_session_async_impl_browsertest.cc
@@ -81,9 +81,14 @@ class SecureSessionAsyncImplBrowserTest : public InProcessBrowserTest {
  protected:
   void PerformValidHandshake(TestServerSecureSession& server_session) {
     auto client_handshake_request = [&]() {
-      base::test::TestFuture<oak::session::v1::HandshakeRequest> future;
+      base::test::TestFuture<std::optional<oak::session::v1::HandshakeRequest>>
+          future;
       client_session_->GetHandshakeMessage(future.GetCallback());
-      return future.Get();
+
+      auto result = future.Get();
+      CHECK(result.has_value());
+
+      return result.value();
     }();
 
     auto server_handshake_response = server_session.ProcessHandshake(
@@ -144,11 +149,15 @@ IN_PROC_BROWSER_TEST_F(SecureSessionAsyncImplBrowserTest,
 
 IN_PROC_BROWSER_TEST_F(SecureSessionAsyncImplBrowserTest,
                        GetHandshakeMessageSucceeds) {
-  base::test::TestFuture<oak::session::v1::HandshakeRequest> future;
+  base::test::TestFuture<std::optional<oak::session::v1::HandshakeRequest>>
+      future;
   client_session_->GetHandshakeMessage(future.GetCallback());
-  auto request = future.Get();
 
-  EXPECT_TRUE(request.has_noise_handshake_message());
+  auto result = future.Get();
+  ASSERT_TRUE(result.has_value());
+
+  auto request = result.value();
+  ASSERT_TRUE(request.has_noise_handshake_message());
 
   const auto& noise_msg = request.noise_handshake_message();
   EXPECT_EQ(noise_msg.ephemeral_public_key().size(), kP256X962Length);
@@ -160,7 +169,8 @@ IN_PROC_BROWSER_TEST_F(SecureSessionAsyncImplBrowserTest,
   // Though the result is not used, it's important to call GetHandshakeMessage()
   // before ProcessHandshakeResponse().
   {
-    base::test::TestFuture<oak::session::v1::HandshakeRequest> future;
+    base::test::TestFuture<std::optional<oak::session::v1::HandshakeRequest>>
+        future;
     client_session_->GetHandshakeMessage(future.GetCallback());
     ASSERT_TRUE(future.Wait());
   }
@@ -183,7 +193,8 @@ IN_PROC_BROWSER_TEST_F(SecureSessionAsyncImplBrowserTest,
   // Though the result is not used, it's important to call GetHandshakeMessage()
   // before ProcessHandshakeResponse().
   {
-    base::test::TestFuture<oak::session::v1::HandshakeRequest> future;
+    base::test::TestFuture<std::optional<oak::session::v1::HandshakeRequest>>
+        future;
     client_session_->GetHandshakeMessage(future.GetCallback());
     ASSERT_TRUE(future.Wait());
   }
@@ -222,9 +233,14 @@ IN_PROC_BROWSER_TEST_F(SecureSessionAsyncImplBrowserTest,
 IN_PROC_BROWSER_TEST_F(SecureSessionAsyncImplBrowserTest,
                        ProcessHandshakeResponseNonEmptyPlaintext) {
   auto client_handshake_request = [&]() {
-    base::test::TestFuture<oak::session::v1::HandshakeRequest> future;
+    base::test::TestFuture<std::optional<oak::session::v1::HandshakeRequest>>
+        future;
     client_session_->GetHandshakeMessage(future.GetCallback());
-    return future.Get();
+
+    auto result = future.Get();
+    CHECK(result.has_value());
+
+    return result.value();
   }();
 
   TestServerSecureSession server_session;
