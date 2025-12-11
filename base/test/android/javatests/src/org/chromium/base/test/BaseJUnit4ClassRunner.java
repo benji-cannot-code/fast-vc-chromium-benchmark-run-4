a62d2e68d067d222b1264ce829fc343f43772185
Sam Maier <smaier@chromium.org>
2025-12-11 09:40:10 -0800

    Add Leak Canary tests
    This cl adds the @EnableLeakChecks annotation, which can be used to
    check for leaks via Leak Canary in any instrumentation test.
    
    We also add PublicTransitLeakTest.java, which covers some of the most
    commonly used Activities.
    
    Change-Id: I2573b52fc7e914d555112425baaba01b67ff1557
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7162900
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1557482}

--- base/test/android/javatests/src/org/chromium/base/test/BaseJUnit4ClassRunner.java
+++ base/test/android/javatests/src/org/chromium/base/test/BaseJUnit4ClassRunner.java
@@ -43,6 +43,7 @@ import org.chromium.base.test.util.BaseRestrictions;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.DisableIfSkipCheck;
+import org.chromium.base.test.util.LeakCanaryChecker;
 import org.chromium.base.test.util.RequiresRestart;
 import org.chromium.base.test.util.RestrictionSkipCheck;
 import org.chromium.base.test.util.SkipCheck;
@@ -146,6 +147,7 @@ public class BaseJUnit4ClassRunner extends AndroidJUnit4ClassRunner {
     private long mTestStartTimeMs;
     private String mFailedBatchTestName;
     private JniTestInstancesSnapshot mJniZeroSnapshot;
+    private boolean mAnyTestFailed;
 
     /**
      * Create a BaseJUnit4ClassRunner to run {@code klass} and initialize values.
@@ -309,6 +311,7 @@ public class BaseJUnit4ClassRunner extends AndroidJUnit4ClassRunner {
 
                     @Override
                     public void testFailure(Failure failure) {
+                        mAnyTestFailed = true;
                         mPendingFailure = failure;
                     }
 
@@ -559,6 +562,9 @@ public class BaseJUnit4ClassRunner extends AndroidJUnit4ClassRunner {
         boolean finishSuccess = ActivityFinisher.finishAll();
         if (afterClassPassed && finishSuccess) {
             LifetimeAssert.assertAllInstancesDestroyedForTesting();
+            if (!mAnyTestFailed) {
+                LeakCanaryChecker.maybeCheckLeaks(getTestClass().getJavaClass());
+            }
         } else {
             LifetimeAssert.resetForTesting();
         }
