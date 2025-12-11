de63476cc2f211503f3d79abab682f583079b5ba
Marke Hallowell <wry@chromium.org>
2025-12-11 12:24:06 -0800

    [GlicMI] Add an options param to unpin tabs with client triggers.
    Bug: 461849870
    Change-Id: Ifda7d0d9d01520c60472af26ef7e84425199d130
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7249199
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Commit-Queue: Marke Hallowell <wry@chromium.org>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1557619}

--- chrome/browser/resources/glic/glic_api_impl/client/glic_api_client.ts
+++ chrome/browser/resources/glic/glic_api_impl/client/glic_api_client.ts
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import type {AdditionalContext, AnnotatedPageData, CaptureRegionErrorReason, CaptureRegionResult, ChromeVersion, ConversationInfo, CreateActorTabOptions, CreateTabOptions, DraggableArea, FocusedTabData, GetPinCandidatesOptions, GlicBrowserHost, GlicBrowserHostJournal, GlicBrowserHostMetrics, GlicHostRegistry, GlicWebClient, Journal, NavigationConfirmationRequest, Observable, ObservableValue, OnResponseStoppedDetails, OpenPanelInfo, OpenSettingsOptions, PageMetadata, PanelOpeningData, PanelState, PdfDocumentData, PinCandidate, PinTabsOptions, ResizeWindowOptions, ResumeActorTaskResult, Screenshot, ScrollToParams, SelectAutofillSuggestionsDialogRequest, SelectCredentialDialogRequest, TabContextOptions, TabContextResult, TabData, TaskOptions, UserConfirmationDialogRequest, UserProfileInfo, ViewChangedNotification, ViewChangeRequest, WebClientMode, ZeroStateSuggestions, ZeroStateSuggestionsOptions, ZeroStateSuggestionsV2} from '../../glic_api/glic_api.js';
+import type {AdditionalContext, AnnotatedPageData, CaptureRegionErrorReason, CaptureRegionResult, ChromeVersion, ConversationInfo, CreateActorTabOptions, CreateTabOptions, DraggableArea, FocusedTabData, GetPinCandidatesOptions, GlicBrowserHost, GlicBrowserHostJournal, GlicBrowserHostMetrics, GlicHostRegistry, GlicWebClient, Journal, NavigationConfirmationRequest, Observable, ObservableValue, OnResponseStoppedDetails, OpenPanelInfo, OpenSettingsOptions, PageMetadata, PanelOpeningData, PanelState, PdfDocumentData, PinCandidate, PinTabsOptions, ResizeWindowOptions, ResumeActorTaskResult, Screenshot, ScrollToParams, SelectAutofillSuggestionsDialogRequest, SelectCredentialDialogRequest, TabContextOptions, TabContextResult, TabData, TaskOptions, UnpinTabsOptions, UserConfirmationDialogRequest, UserProfileInfo, ViewChangedNotification, ViewChangeRequest, WebClientMode, ZeroStateSuggestions, ZeroStateSuggestionsOptions, ZeroStateSuggestionsV2} from '../../glic_api/glic_api.js';
 import {ActorTaskPauseReason, ActorTaskState, ActorTaskStopReason, HostCapability} from '../../glic_api/glic_api.js';
 import {ObservableValue as ObservableValueImpl, Subject} from '../../observable.js';
 
@@ -1085,14 +1085,15 @@ class GlicBrowserHostImpl implements GlicBrowserHost {
         .pinnedAll;
   }
 
-  async unpinTabs?(tabIds: string[]): Promise<boolean> {
+  async unpinTabs?
+      (tabIds: string[], options?: UnpinTabsOptions): Promise<boolean> {
     return (await this.sender.requestWithResponse(
-                'glicBrowserUnpinTabs', {tabIds}))
+                'glicBrowserUnpinTabs', {tabIds, options}))
         .unpinnedAll;
   }
 
-  unpinAllTabs?(): void {
-    this.sender.requestNoResponse('glicBrowserUnpinAllTabs', undefined);
+  unpinAllTabs?(options?: UnpinTabsOptions): void {
+    this.sender.requestNoResponse('glicBrowserUnpinAllTabs', {options});
   }
 
   getPinCandidates?
