ec63b6ec2be859cc7aa6de388abfbf157ea837c4
anthie@google.com <anthie@google.com>
2025-12-11 09:05:43 -0800

    Add typescript tests for the history sync optin screen
    Bug: 457370781
    Change-Id: I66ca0b1802e33317444903cc3c5476145ed8d8cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7247215
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Anthi Orfanou <anthie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1557458}

---
+++ chrome/test/data/webui/signin/test_history_sync_optin_browser_proxy.ts
@@ -0,0 +1,54 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import type {HistorySyncOptInBrowserProxy} from 'chrome://history-sync-optin/browser_proxy.js';
+import type {PageRemote} from 'chrome://history-sync-optin/history_sync_optin.mojom-webui.js';
+import {PageCallbackRouter, type PageHandlerInterface} from 'chrome://history-sync-optin/history_sync_optin.mojom-webui.js';
+import {TestBrowserProxy} from 'chrome://webui-test/test_browser_proxy.js';
+
+export class TestHistorySyncOptInHandler extends TestBrowserProxy implements
+    PageHandlerInterface {
+  callbackRouter: PageCallbackRouter;
+  constructor() {
+    super([
+      'accept',
+      'reject',
+      'requestAccountInfo',
+      'updateDialogHeight',
+    ]);
+    this.callbackRouter = new PageCallbackRouter();
+  }
+
+  accept() {
+    this.methodCalled('accept');
+  }
+
+  reject() {
+    this.methodCalled('reject');
+  }
+
+  requestAccountInfo() {
+    this.methodCalled('requestAccountInfo');
+  }
+
+  updateDialogHeight(height: number) {
+    this.methodCalled('updateDialogHeight', height);
+  }
+}
+
+export class TestHistorySyncOptInBrowserProxy extends TestBrowserProxy
+    implements HistorySyncOptInBrowserProxy {
+  callbackRouter: PageCallbackRouter;
+  page: PageRemote;
+  handler: TestHistorySyncOptInHandler;
+
+  constructor() {
+    super();
+    this.callbackRouter = new PageCallbackRouter();
+    this.page =
+        this.callbackRouter.$.bindNewPipeAndPassRemote();
+    this.handler = new TestHistorySyncOptInHandler();
+  }
+}
