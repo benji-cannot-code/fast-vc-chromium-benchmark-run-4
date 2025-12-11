9a9211e50645117a58b9174f8b1e4183febf613f
Maggie Jennings <mjenn@google.com>
2025-12-11 11:16:36 -0800

    [Cleanup] Clean up GlicActorUiNudgeRedesign in GlicActorTaskIconManager
    Removes GlicActorUiNudgeRedesign code paths from
    GlicActorTaskIconManager, including removing now unneeded dependencies.
    
    Bug: 462144644
    Change-Id: I57c35dcee362c47aa01173f5cfb500dd604703bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7248072
    Reviewed-by: Abe Boujane <boujane@google.com>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Maggie Jennings <mjenn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1557582}

--- chrome/browser/actor/ui/task_list_bubble/actor_task_list_bubble_controller_unittest.cc
+++ chrome/browser/actor/ui/task_list_bubble/actor_task_list_bubble_controller_unittest.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/test/widget_test.h"
 #include "ui/views/widget/unique_widget_ptr.h"
 #if BUILDFLAG(ENABLE_GLIC)
-#include "chrome/browser/glic/test_support/mock_glic_window_controller.h"
 #include "chrome/browser/ui/tabs/glic_actor_task_icon_manager.h"
 #include "chrome/browser/ui/tabs/glic_actor_task_icon_manager_factory.h"
 #endif
@@ -75,11 +74,10 @@ class ActorTaskListBubbleControllerTest : public ChromeViewsTestBase {
   std::unique_ptr<KeyedService> BuildGlicActorTaskIconManager(
       content::BrowserContext* context) {
     Profile* profile = Profile::FromBrowserContext(context);
-    window_controller_ = std::make_unique<glic::MockGlicWindowController>();
     auto* actor_service =
         actor::ActorKeyedServiceFactory::GetActorKeyedService(profile_.get());
     auto manager = std::make_unique<tabs::GlicActorTaskIconManager>(
-        profile, actor_service, *window_controller_.get());
+        profile, actor_service);
     return std::move(manager);
   }
 
@@ -98,7 +96,6 @@ class ActorTaskListBubbleControllerTest : public ChromeViewsTestBase {
     actor_task_list_bubble_controller_.reset();
     browser_window_interface_.reset();
     profile_.reset();
-    window_controller_.reset();
     anchor_widget_.reset();
 #endif
     ChromeViewsTestBase::TearDown();
@@ -128,7 +125,6 @@ class ActorTaskListBubbleControllerTest : public ChromeViewsTestBase {
   }
 
 #if BUILDFLAG(ENABLE_GLIC)
-  std::unique_ptr<glic::MockGlicWindowController> window_controller_;
   std::unique_ptr<TestingProfile> profile_;
   std::unique_ptr<ActorTaskListBubbleController>
       actor_task_list_bubble_controller_;
