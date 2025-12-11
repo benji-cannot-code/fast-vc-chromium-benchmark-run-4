ddbe669858b91efc088d7b50f3ffd0410313e9b8
Steinar H. Gunderson <sesse@chromium.org>
2025-12-11 03:21:22 -0800

    Revert "Refactor GlobalCacheStorage to use WindowOrWorkerGlobalScope"
    This reverts commit 5f3123c694b7a5a909fe827e745793fb60c0ea93.
    (See blink-dev for background.)
    
    This commit was not part of the original Supplementable changes,
    but builds on top of it and thus unfortunately must be reverted
    for the other reverts to go through cleanly.
    
    Bug: 460165754
    Change-Id: I5b31a86ccd72567920b70a6e896531592e0a4467
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7246281
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1557270}

--- third_party/blink/renderer/modules/cache_storage/global_cache_storage.h
+++ third_party/blink/renderer/modules/cache_storage/global_cache_storage.h
@@ -6,32 +6,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_CACHE_STORAGE_GLOBAL_CACHE_STORAGE_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_CACHE_STORAGE_GLOBAL_CACHE_STORAGE_H_
 
-#include "third_party/blink/renderer/platform/heap/garbage_collected.h"
-#include "third_party/blink/renderer/platform/heap/member.h"
+#include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 
 namespace blink {
 
 class CacheStorage;
 class ExceptionState;
 class ExecutionContext;
-class WindowOrWorkerGlobalScope;
+class LocalDOMWindow;
+class WorkerGlobalScope;
 
-class GlobalCacheStorage final : public GarbageCollected<GlobalCacheStorage>,
-                                 public GarbageCollectedMixin {
+class GlobalCacheStorage {
+  STATIC_ONLY(GlobalCacheStorage);
+
  public:
-  GlobalCacheStorage() = default;
-
   static bool CanCreateCacheStorage(ExecutionContext*, ExceptionState&);
 
-  static CacheStorage* caches(WindowOrWorkerGlobalScope&, ExceptionState&);
-
-  void Trace(Visitor* visitor) const override;
-
- private:
-  static GlobalCacheStorage& From(WindowOrWorkerGlobalScope&);
-  CacheStorage* Caches(WindowOrWorkerGlobalScope&, ExceptionState&);
-
-  Member<CacheStorage> caches_;
+  static CacheStorage* caches(LocalDOMWindow&, ExceptionState&);
+  static CacheStorage* caches(WorkerGlobalScope&, ExceptionState&);
 };
 
 }  // namespace blink
