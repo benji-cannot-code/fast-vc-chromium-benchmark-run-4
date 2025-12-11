478c3ae4ce06d761ba40f1e381c2fb285c99f166
David Yeung <dayeung@chromium.org>
2025-12-11 10:51:34 -0800

    [Mac] Disable zoom mac gesture events for WebUIContentsWrapper
    Moves the logic for blocking Mac zoom gestures (pinch-to-zoom and
    double-tap) from OmniboxPopupWebUIBaseContent to the shared
    WebUIContentsWrapper.
    
    This generalizes the behavior so all WebUI surfaces using this wrapper
    prevents the gesture events.
    
    Bug: 467942896
    Change-Id: I46fa07b84ace5dd213d734ddc7dd953ed47e9755
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7251885
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1557552}

--- chrome/browser/ui/views/omnibox/omnibox_popup_webui_base_content.cc
+++ chrome/browser/ui/views/omnibox/omnibox_popup_webui_base_content.cc
@@ -26,8 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/zoom/zoom_controller.h"
 #include "content/public/browser/render_widget_host_view.h"
 #include "content/public/browser/web_contents.h"
-#include "third_party/blink/public/common/input/web_gesture_event.h"
-#include "third_party/blink/public/common/input/web_input_event.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/base/mojom/menu_source_type.mojom.h"
 #include "ui/gfx/geometry/rounded_corners_f.h"
@@ -169,23 +167,6 @@ void OmniboxPopupWebUIBaseContent::LoadContent() {
   zoom_controller->SetZoomLevel(0);
 
   OnViewBoundsChanged(location_bar_view_);
-}
-
-bool OmniboxPopupWebUIBaseContent::PreHandleGestureEvent(
-    content::WebContents* source,
-    const blink::WebGestureEvent& event) {
-  // Block gestures that will zoom on Mac devices (i.e. pinch to zoom
-  // and double tap to zoom)
-#if BUILDFLAG(IS_MAC)
-  if (blink::WebInputEvent::IsPinchGestureEventType(event.GetType())) {
-    return true;
-  }
-
-  if (event.GetType() == blink::WebInputEvent::Type::kGestureDoubleTap) {
-    return true;
-  }
-#endif
-  return false;
 }
 
 void OmniboxPopupWebUIBaseContent::OnPopupHidden() {
