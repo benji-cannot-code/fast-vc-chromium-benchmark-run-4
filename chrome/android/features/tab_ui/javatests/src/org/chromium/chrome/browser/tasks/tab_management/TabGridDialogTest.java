c780ca4063226d8784da1a99e8b32db027a6941f
Ritika Gupta <ritikagup@google.com>
2025-12-11 15:13:04 -0800

    [Gardener] Disable failing test testStripDialog_TabListEditorCloseAll_NoCustomHomepage.
    Failing on builder "android-arm64-tests" and "test-tablet"
    
    Bug: 468062507
    Change-Id: Ib9fb538030df4f4d9e526fba4e4b27c1abfa6da1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7254484
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Ritika Gupta <ritikagup@google.com>
    Owners-Override: Ritika Gupta <ritikagup@google.com>
    Commit-Queue: Ritika Gupta <ritikagup@google.com>
    Cr-Commit-Position: refs/heads/main@{#1557717}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogTest.java
@@ -1672,6 +1672,7 @@ public class TabGridDialogTest {
     @Test
     @MediumTest
     @Restriction(DeviceFormFactor.PHONE)
+    @DisabledTest(message = "TODO(crbug.com/468062507)")
     public void testStripDialog_TabListEditorCloseAll_NoCustomHomepage() {
         ChromeTabbedActivity cta = mActivityTestRule.getActivity();
         // Create a tab group with 2 tabs.
