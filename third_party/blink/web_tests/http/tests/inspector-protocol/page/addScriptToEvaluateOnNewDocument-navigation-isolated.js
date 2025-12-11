927bf3c8955cc6832ba8703b4dccf66265cee5d0
Alex Rudenko <alexrudenko@chromium.org>
2025-12-11 01:26:41 -0800

    Add tests for addScriptToEvaluateOnNewDocument
    As far as I see these specific scenarios might not have had coverage.
    
    Change-Id: I42100d6f5cd2d839941e7d65e799ddf75ebdf9a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7248155
    Auto-Submit: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1557209}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/page/addScriptToEvaluateOnNewDocument-navigation-isolated.js
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(/** @type {import('test_runner').TestRunner} */ testRunner) {
+  const {dp} = await testRunner.startBlank(
+      `Tests that Page.addScriptToEvaluateOnNewDocument on works on navigations with isolated world.`);
+
+  await dp.Page.enable();
+  await dp.Runtime.enable();
+
+  dp.Runtime.onConsoleAPICalled(event => {
+    testRunner.log(event, 'console called: ');
+  });
+
+  await dp.Page.addScriptToEvaluateOnNewDocument({
+    source: 'console.log("evaluated")',
+    worldName: 'isolated',
+  });
+
+  const loaded = dp.Page.onceLoadEventFired();
+  await dp.Page.navigate({
+    url: testRunner.url('resources/empty.html')
+  });
+  await loaded;
+
+  testRunner.completeTest();
+});
