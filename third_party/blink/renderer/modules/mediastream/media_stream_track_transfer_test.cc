580a943af4e71f253e0420ee3087f364cfd49ca2
Steinar H. Gunderson <sesse@chromium.org>
2025-12-11 10:12:22 -0800

    Revert "Make GlobalCacheStorageImpl not a Supplement."
    This reverts commit eba21e5bbbc4347248c07d979354ffbdec12140d.
    (See blink-dev for background.)
    
    Bug: 460165754
    Change-Id: Ib03cac33987f54e959bd43a1246af64e2ac81443
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7245780
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1557508}

--- third_party/blink/renderer/modules/mediastream/media_stream_track_transfer_test.cc
+++ third_party/blink/renderer/modules/mediastream/media_stream_track_transfer_test.cc
@@ -117,7 +117,7 @@ class ScopedMockUserMediaClient {
                                                                original_.Get());
       } else {
         window->Supplementable<LocalDOMWindow,
-                               46>::RemoveSupplement<UserMediaClient>();
+                               47>::RemoveSupplement<UserMediaClient>();
       }
     }
   }
