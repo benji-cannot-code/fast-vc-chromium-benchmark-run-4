8a2388582d1b05beaa0573f9d19678cc439fb8df
Joel Tan-Aristy <jtanaristy@google.com>
2025-12-11 15:00:28 -0800

    [Icon] Update location on icon vector drawable
    Bug: 465844969
    Change-Id: I3a22b2f4a1e1699c72c766d613598b2ef64cfe26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7248990
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Joel Tan-Aristy <jtanaristy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1557710}

--- chrome/browser/ui/android/fast_checkout/internal/junit/src/org/chromium/chrome/browser/ui/fast_checkout/FastCheckoutDetailScreenViewTest.java
+++ chrome/browser/ui/android/fast_checkout/internal/junit/src/org/chromium/chrome/browser/ui/fast_checkout/FastCheckoutDetailScreenViewTest.java
@@ -452,7 +452,7 @@ public class FastCheckoutDetailScreenViewTest {
         assertTrue(addressImageView.isShown());
         assertThat(
                 shadowOf(addressImageView.getDrawable()).getCreatedFromResId(),
-                equalTo(R.drawable.location_on_logo));
+                equalTo(R.drawable.ic_location_on_fill_24dp));
 
         // Icon subtitle should be hidden.
         assertFalse(getTextViewFromListItemWithId(0, R.id.fast_checkout_record_type).isShown());
@@ -509,7 +509,7 @@ public class FastCheckoutDetailScreenViewTest {
         assertTrue(addressImageView.isShown());
         assertThat(
                 shadowOf(addressImageView.getDrawable()).getCreatedFromResId(),
-                equalTo(R.drawable.location_on_logo));
+                equalTo(R.drawable.ic_location_on_fill_24dp));
 
         // Icon subtitle should be hidden.
         assertFalse(getTextViewFromListItemWithId(0, R.id.fast_checkout_record_type).isShown());
