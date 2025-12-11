6aaa45d415c242df70aa2bc458342c99bd758b39
Steinar H. Gunderson <sesse@chromium.org>
2025-12-11 03:17:18 -0800

    Revert "Make InnerHtmlAgent not a Supplement."
    This reverts commit 7cf52b93ecfcc9800298f657b811f7e9b22ec173.
    (See blink-dev for background.)
    
    Bug: 460165754
    Change-Id: I95739ea4fd49bf87c7547ca112a585415bfe2b49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7247221
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1557267}

--- third_party/blink/renderer/modules/content_extraction/inner_html_agent.h
+++ third_party/blink/renderer/modules/content_extraction/inner_html_agent.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/content_extraction/inner_html.mojom-blink.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_receiver_set.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_wrapper_mode.h"
+#include "third_party/blink/renderer/platform/supplementable.h"
 
 namespace blink {
 
@@ -21,7 +22,7 @@ class LocalFrame;
 // to InnerHtmlBuilder to handle building of the text.
 class InnerHtmlAgent final : public GarbageCollected<InnerHtmlAgent>,
                              public mojom::blink::InnerHtmlAgent,
-                             public GarbageCollectedMixin {
+                             public Supplement<Document> {
  public:
   static const unsigned kSupplementIndex;
   static InnerHtmlAgent* From(Document&);
@@ -42,7 +43,6 @@ class InnerHtmlAgent final : public GarbageCollected<InnerHtmlAgent>,
  private:
   void Bind(mojo::PendingReceiver<mojom::blink::InnerHtmlAgent> receiver);
 
-  Member<Document> document_;
   HeapMojoReceiverSet<mojom::blink::InnerHtmlAgent, InnerHtmlAgent>
       receiver_set_;
 };
