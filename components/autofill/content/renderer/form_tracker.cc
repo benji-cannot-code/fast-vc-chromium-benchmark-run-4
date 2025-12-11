5a11e8c32459c0e4c36deadbf525c69c70c3880f
Victor Tan <victortan@chromium.org>
2025-12-11 13:50:37 -0800

    Revert "Pass heap snapshot source locations through blink::WebNode (and subclasses), autofill::FormRef, and autofill::FieldRef."
    This reverts commit 4156d6460349a7c1520136ff2be479065ad7c7cd.
    
    Reason for revert: tree close https://ci.chromium.org/ui/p/chromium/builders/ci/Android%20arm%20Builder%20(dbg)/84021/overview
    
    Original change's description:
    > Pass heap snapshot source locations through blink::WebNode (and subclasses), autofill::FormRef, and autofill::FieldRef.
    >
    > Bug: 342247579
    > Change-Id: Ieb2a490455048063184fb0c79a539093b5d26b2b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7224596
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Christoph Schwering <schwering@google.com>
    > Commit-Queue: David Baron <dbaron@chromium.org>
    > Reviewed-by: Dominic Farolino <dom@chromium.org>
    > Owners-Override: Daniel Cheng <dcheng@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1557625}
    
    Bug: 342247579
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I36ea76418694757cdc9e24582a801a70aeb47e7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7253393
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Victor Tan <victortan@chromium.org>
    Owners-Override: Victor Tan <victortan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1557668}

--- components/autofill/content/renderer/form_tracker.cc
+++ components/autofill/content/renderer/form_tracker.cc
@@ -778,7 +778,7 @@ void FormTracker::UpdateLastInteractedElement(
 }
 
 void FormTracker::ResetLastInteractedElements() {
-  last_interacted_ = {FormRef(), FieldRef()};
+  last_interacted_ = {};
   submission_triggering_events_ = {};
   if (form_element_observer_) {
     form_element_observer_->Disconnect();
--- third_party/blink/renderer/platform/heap/persistent.h
+++ third_party/blink/renderer/platform/heap/persistent.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_HEAP_PERSISTENT_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_HEAP_PERSISTENT_H_
 
-#include "third_party/blink/public/public_buildflags.h"
+#include "third_party/blink/renderer/platform/heap/heap_buildflags.h"
 #include "third_party/blink/renderer/platform/wtf/hash_functions.h"
 #include "third_party/blink/renderer/platform/wtf/hash_traits.h"
 #include "third_party/blink/renderer/platform/wtf/type_traits.h"
