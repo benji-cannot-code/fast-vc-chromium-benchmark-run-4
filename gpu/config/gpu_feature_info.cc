2289957eb9eb089524c3b9687880750ea0b2d32a
Victor Hugo Vianna Silva <victorvianna@google.com>
2025-12-11 14:34:39 -0800

    Fix some formatting issues in //gpu
    These files will be touched in an upcoming patch series, so
    this fixes existing `git cl format` issues in a dedicated CL.
    
    Change-Id: I58070a844a7e0fcf813f18ebc2c8f98ef012c84b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7247218
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1557695}

--- gpu/config/gpu_feature_info.cc
+++ gpu/config/gpu_feature_info.cc
@@ -13,8 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace gpu {
 
 GpuFeatureInfo::GpuFeatureInfo() {
-  for (auto& status : status_values)
+  for (auto& status : status_values) {
     status = kGpuFeatureStatusUndefined;
+  }
 }
 
 GpuFeatureInfo::GpuFeatureInfo(const GpuFeatureInfo&) = default;
