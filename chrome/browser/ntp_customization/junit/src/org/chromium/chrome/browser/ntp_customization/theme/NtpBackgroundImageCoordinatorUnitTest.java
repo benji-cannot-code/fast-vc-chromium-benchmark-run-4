024ec6532b068a3d007d9582a24712732cbca209
Xi Han <hanxi@google.com>
2025-12-11 08:52:59 -0800

    [Theme] Make gradient view gone as default.
    In this CL, the default visibility of gradient view is set to gone. It
    prevents the gradient view become visible when background image isn't
    initialized properly.
    
    Bug: 467968044
    Change-Id: Icc0dc2ae69092e6622ae443d4b4a52e0451b2c1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7252609
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Xinyi Ji <xinyiji@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1557445}

--- chrome/browser/ntp_customization/junit/src/org/chromium/chrome/browser/ntp_customization/theme/NtpBackgroundImageCoordinatorUnitTest.java
+++ chrome/browser/ntp_customization/junit/src/org/chromium/chrome/browser/ntp_customization/theme/NtpBackgroundImageCoordinatorUnitTest.java
@@ -24,6 +24,7 @@ import android.graphics.Matrix;
 import android.graphics.Point;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 import androidx.annotation.ColorInt;
@@ -42,6 +43,7 @@ import org.robolectric.annotation.Config;
 
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.chrome.browser.ntp_customization.NtpCustomizationUtils.NtpBackgroundImageType;
+import org.chromium.chrome.browser.ntp_customization.R;
 import org.chromium.chrome.browser.ntp_customization.theme.upload_image.BackgroundImageInfo;
 import org.chromium.chrome.browser.ntp_customization.theme.upload_image.CropImageUtils;
 import org.chromium.components.browser_ui.widget.displaystyle.DisplayStyleObserver;
@@ -60,6 +62,7 @@ public class NtpBackgroundImageCoordinatorUnitTest {
     @Mock private UiConfig mUiConfig;
     @Mock private Bitmap mBitmap;
     @Captor ArgumentCaptor<DisplayStyleObserver> mDisplayStyleObserverArgumentCaptor;
+    @Captor ArgumentCaptor<FrameLayout> mBackgroundImageLayoutCaptor;
 
     private Activity mActivity;
     private NtpBackgroundImageCoordinator mCoordinator;
@@ -100,7 +103,11 @@ public class NtpBackgroundImageCoordinatorUnitTest {
     public void testConstructor() {
         verify(mUiConfig, never()).addObserver(any(DisplayStyleObserver.class));
         assertEquals(COLOR, mPropertyModel.get(NtpBackgroundImageProperties.BACKGROUND_COLOR));
-        verify(mRootView).addView(any(View.class));
+        verify(mRootView).addView(mBackgroundImageLayoutCaptor.capture());
+
+        View backgroundImageLayout = mBackgroundImageLayoutCaptor.getValue();
+        View gradientView = backgroundImageLayout.findViewById(R.id.gradient_view);
+        assertEquals(View.GONE, gradientView.getVisibility());
     }
 
     @Test
