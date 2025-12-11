fe8b4c4a93c99151802645006d7c2050d0583d1c
Julia Hansbrough <flowerhack@google.com>
2025-12-10 16:12:46 -0800

    [rustmojo] Lay down structure & constructor for ergonomic Trap functions.
    Subsequent CLs implement add/remove trigger logic, but this CL lays out
    the general structure of the trap (and thus is a good time to chime in
    if this structure seems misguided).
    
    Change-Id: Ia2910d336dd2925e9ae19cbca86fe60e60b4dbf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7231137
    Commit-Queue: Julia Hansbrough <flowerhack@google.com>
    Reviewed-by: Devon Loehr <dloehr@google.com>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1557029}

--- mojo/public/rust/system/lib.rs
+++ mojo/public/rust/system/lib.rs
@@ -5,4 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 pub mod data_pipe;
 pub mod mojo_types;
-pub mod trap;
+pub mod raw_trap;
+// FOR_RELEASE: Rename this module to just `trap`.
+// (Git got grumpy when I tried to move trap.rs -> raw_trap.rs
+// and change trap.rs itself in the same CL.)
+pub mod safe_trap;
