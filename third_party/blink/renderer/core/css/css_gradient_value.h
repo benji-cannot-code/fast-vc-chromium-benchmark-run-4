1e7f0b944ac8f537ed6322178b92e2f67bb5b802
Rune Lillesveen <futhark@chromium.org>
2025-12-11 02:34:00 -0800

    Compute gradient stop colors at computed value time if necessary
    When working on this CL, I noticed we do not resolve computed values in
    gradients which are wrapped in other values like image-set() and
    cross-fade(). As a consequence of that, the added handling for
    kConstantGradientClass is currently not exercised as it is only used for
    solid colors inside cross-fade().
    
    Resolving computed values in image-set()/cross-fade() will be done in
    other CLs.
    
    Bug: 465006290
    Change-Id: I50bf0c14eee5612dd805a48741ac9a25c3c45105
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7245532
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1557244}

--- third_party/blink/renderer/core/css/css_gradient_value.h
+++ third_party/blink/renderer/core/css/css_gradient_value.h
@@ -42,6 +42,7 @@ namespace blink {
 class Color;
 class Gradient;
 class Document;
+class StyleResolverState;
 
 namespace cssvalue {
 
@@ -119,7 +120,7 @@ class CSSGradientValue : public CSSImageGeneratorValue {
 
   bool KnownToBeOpaque(const Document&, const ComputedStyle&) const;
   const CSSGradientValue* ResolveValuesIfNeeded(
-      const CSSToLengthConversionData& conversion_data) const;
+      const StyleResolverState&) const;
   CSSGradientValue* ComputedCSSValue(const ComputedStyle&,
                                      bool allow_visited_style,
                                      CSSValuePhase value_phase) const;
@@ -203,7 +204,7 @@ class CSSLinearGradientValue final : public CSSGradientValue {
   bool Equals(const CSSLinearGradientValue&) const;
 
   const CSSLinearGradientValue* ResolveValuesIfNeeded(
-      const CSSToLengthConversionData& conversion_data) const;
+      const StyleResolverState&) const;
   CSSLinearGradientValue* ComputedCSSValue(const ComputedStyle&,
                                            bool allow_visited_style,
                                            CSSValuePhase value_phase) const;
@@ -307,7 +308,7 @@ class CORE_EXPORT CSSRadialGradientValue final : public CSSGradientValue {
   bool Equals(const CSSRadialGradientValue&) const;
 
   const CSSRadialGradientValue* ResolveValuesIfNeeded(
-      const CSSToLengthConversionData& conversion_data) const;
+      const StyleResolverState&) const;
   CSSRadialGradientValue* ComputedCSSValue(const ComputedStyle&,
                                            bool allow_visited_style,
                                            CSSValuePhase value_phase) const;
@@ -359,7 +360,7 @@ class CSSConicGradientValue final : public CSSGradientValue {
   bool Equals(const CSSConicGradientValue&) const;
 
   const CSSConicGradientValue* ResolveValuesIfNeeded(
-      const CSSToLengthConversionData& conversion_data) const;
+      const StyleResolverState&) const;
   CSSConicGradientValue* ComputedCSSValue(const ComputedStyle&,
                                           bool allow_visited_style,
                                           CSSValuePhase value_phase) const;
@@ -402,6 +403,8 @@ class CSSConstantGradientValue final : public CSSGradientValue {
   CSSConstantGradientValue* ComputedCSSValue(const ComputedStyle&,
                                              bool allow_visited_style,
                                              CSSValuePhase value_phase) const;
+  const CSSConstantGradientValue* ResolveValuesIfNeeded(
+      const StyleResolverState&) const;
 
   void TraceAfterDispatch(blink::Visitor*) const;
 
