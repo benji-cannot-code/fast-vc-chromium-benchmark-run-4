ce64862d5ae9c2d12a58fa0662396220ae5cd4ca
Colin Blundell <blundell@chromium.org>
2025-12-11 00:06:33 -0800

    [Blink] Fork CanvasImageProvider for CRPExternalBitmap
    CanvasImageProvider is an inner class of CanvasResourceProvider that
    can run in either SW raster or HW raster mode. CRPExternalBitmap
    naturally runs it always in SW raster mode. As part of decoupling
    CRPExternalBitmap from CanvasResourceProvider, this CL forks
    "CanvasImageProvider in SW mode" into a new inner class of
    CRPExternalBitmap. CanvasImageProvider when run in SW mode is fairly
    trivial, as the short-circuit out here [1] means that `locked_images_`
    is never populated and the methods interacting with it are never called.
    Thus, the code to be forked is just this [2].
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc;l=1463-1465;drc=8bd3d24cc3bdbffe564eeeca112a6744e6766b17;bpv=1;bpt=1
    [2] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc;l=1400-1465;drc=8bd3d24cc3bdbffe564eeeca112a6744e6766b17;bpv=1;bpt=1
    
    Bug: 352263194
    Change-Id: I837f807da8a01abaa49349bd65d30f4933712e50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7241507
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1557174}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
@@ -458,7 +458,7 @@ class PLATFORM_EXPORT CanvasResourceProviderExternalBitmap
                                        SkAlphaType alpha_type,
                                        const gfx::ColorSpace& color_space);
 
-  ~CanvasResourceProviderExternalBitmap() override = default;
+  ~CanvasResourceProviderExternalBitmap() override;
 
   bool IsGpuContextLost() const override;
   bool IsValid() const override;
@@ -489,6 +489,11 @@ class PLATFORM_EXPORT CanvasResourceProviderExternalBitmap
   scoped_refptr<StaticBitmapImage> DoExternalDrawAndSnapshot(
       base::FunctionRef<void(MemoryManagedPaintCanvas&)> draw_callback,
       ImageOrientation orientation) override;
+
+ private:
+  class SoftwareImageProvider;
+
+  std::unique_ptr<SoftwareImageProvider> image_provider_;
 };
 
 // * Renders to a SharedImage, which manages memory internally.
