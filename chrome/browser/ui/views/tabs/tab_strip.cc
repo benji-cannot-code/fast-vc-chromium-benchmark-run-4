a18268630102855be8931e5190a1f367f4f3afa6
Steven Luong <stluong@chromium.org>
2025-12-11 11:31:02 -0800

    [Vertical Tabs] remove extra GetModelPinnedTabCount() function
    TabStrip implements a duplicate function to get the number of pinned
    tabs so it should be removed.
    
    Bug: 439956588
    Change-Id: I0e5fcda8c8ea004a16146c3aeeadde1083aa050a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7243685
    Auto-Submit: Steven Luong <stluong@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Steven Luong <stluong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1557592}

--- chrome/browser/ui/views/tabs/tab_strip.cc
+++ chrome/browser/ui/views/tabs/tab_strip.cc
@@ -409,7 +409,7 @@ class TabStrip::TabDragContextImpl : public TabDragContext,
   }
 
   int GetPinnedTabCount() const override {
-    return tab_strip_->GetModelPinnedTabCount();
+    return tab_strip_->NumPinnedTabsInModel();
   }
 
   TabGroupHeader* GetTabGroupHeader(
@@ -1559,17 +1559,6 @@ int TabStrip::GetModelCount() const {
   return controller_->GetCount();
 }
 
-int TabStrip::GetModelPinnedTabCount() const {
-  for (size_t i = 0; i < static_cast<size_t>(controller_->GetCount()); ++i) {
-    if (!controller_->IsTabPinned(static_cast<int>(i))) {
-      return static_cast<int>(i);
-    }
-  }
-
-  // All tabs are pinned.
-  return controller_->GetCount();
-}
-
 TabDragContext* TabStrip::GetDragContext() {
   return base::to_address(drag_context_);
 }
@@ -1600,9 +1589,9 @@ std::optional<int> TabStrip::GetActiveIndex() const {
 }
 
 int TabStrip::NumPinnedTabsInModel() const {
-  for (size_t i = 0; i < static_cast<size_t>(controller_->GetCount()); ++i) {
-    if (!controller_->IsTabPinned(static_cast<int>(i))) {
-      return static_cast<int>(i);
+  for (int i = 0; i < controller_->GetCount(); ++i) {
+    if (!controller_->IsTabPinned(i)) {
+      return i;
     }
   }
 
@@ -2516,7 +2505,6 @@ void TabStrip::AnnounceTabRemovedFromGroup(tab_groups::TabGroupId group_id) {
 BEGIN_METADATA(TabStrip)
 ADD_READONLY_PROPERTY_METADATA(int, TabCount)
 ADD_READONLY_PROPERTY_METADATA(int, ModelCount)
-ADD_READONLY_PROPERTY_METADATA(int, ModelPinnedTabCount)
 ADD_READONLY_PROPERTY_METADATA(int, StrokeThickness)
 ADD_READONLY_PROPERTY_METADATA(SkColor,
                                TabSeparatorColor,
