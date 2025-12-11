d0fb6e00ddb0d78cf2eec03f7b58988a0cb0d60c
Fiona Macintosh <fmacintosh@google.com>
2025-12-10 16:59:01 -0800

    [3PCD] Clean up AreAll3pcBlocked check in PS delegate
    Bug: b:462419925
    Change-Id: I23b05b9d256d49a0cfd1de7c332687f203813bf1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7248897
    Reviewed-by: Kevin Graney <kmg@google.com>
    Commit-Queue: Fiona Macintosh <fmacintosh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1557051}

--- chrome/browser/privacy_sandbox/privacy_sandbox_settings_delegate_unittest.cc
+++ chrome/browser/privacy_sandbox/privacy_sandbox_settings_delegate_unittest.cc
@@ -665,7 +665,6 @@ struct ThirdPartyCookiesBlockedByCookieDeprecationExperimentTestCase {
   bool is_profile_onboarded = false;
   content_settings::CookieControlsMode cookie_controls_mode_pref =
       content_settings::CookieControlsMode::kOff;
-  bool block_all_3pc_toggle_enabled = false;
   bool expected;
 };
 
@@ -690,12 +689,6 @@ const ThirdPartyCookiesBlockedByCookieDeprecationExperimentTestCase
             .is_profile_onboarded = true,
             .cookie_controls_mode_pref =
                 content_settings::CookieControlsMode::kBlockThirdParty,
-            .expected = false,
-        },
-        {
-            .is_client_eligible = true,
-            .is_profile_onboarded = true,
-            .block_all_3pc_toggle_enabled = true,
             .expected = false,
         },
 };
@@ -734,8 +727,6 @@ TEST_P(ThirdPartyCookiesBlockedByCookieDeprecationExperimentTest,
 
   prefs()->SetInteger(prefs::kCookieControlsMode,
                       static_cast<int>(test_case.cookie_controls_mode_pref));
-  prefs()->SetBoolean(prefs::kBlockAll3pcToggleEnabled,
-                      test_case.block_all_3pc_toggle_enabled);
 
   EXPECT_CALL(*experiment_manager(), IsClientEligible)
       .WillOnce(::testing::Return(test_case.is_client_eligible));
