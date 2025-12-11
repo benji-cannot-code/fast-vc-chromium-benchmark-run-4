fdae0959e1eb6a7db20fe9eebfbf2563f9212916
Sam Maier <smaier@chromium.org>
2025-12-11 08:29:40 -0800

    MEMORY LEAK: fix ViewportRectProvider never being unregistered
    We register ViewportRectProvider as a ComponentCallback, so the OS holds
    a reference onto it until we unregister, which we never did.
    
    Bug: 463687998
    Change-Id: Icf5235ef4882e7f227507a3bfcb35417aa966eee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7247906
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Auto-Submit: Sam Maier <smaier@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1557427}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxCoordinator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxCoordinator.java
@@ -87,6 +87,7 @@ public class FuseboxCoordinator implements UrlFocusChangeListener, TemplateUrlSe
     private final ObservableSupplier<Profile> mProfileSupplier;
     private final Callback<Profile> mProfileObserver = this::onProfileAvailable;
     private final SnackbarManager mSnackbarManager;
+    private final @Nullable ViewportRectProvider mViewportRectProvider;
 
     public FuseboxCoordinator(
             Context context,
@@ -112,6 +113,7 @@ public class FuseboxCoordinator implements UrlFocusChangeListener, TemplateUrlSe
             mViewHolder = null;
             mLocationBarDataProvider = null;
             mModel = new PropertyModel(FuseboxProperties.ALL_KEYS);
+            mViewportRectProvider = null;
             return;
         }
 
@@ -127,7 +129,7 @@ public class FuseboxCoordinator implements UrlFocusChangeListener, TemplateUrlSe
                 0,
                 0);
         var popupView = LayoutInflater.from(context).inflate(R.layout.fusebox_context_popup, null);
-        @Nullable RectProvider viewportRectProvider = new ViewportRectProvider(mContext);
+        mViewportRectProvider = new ViewportRectProvider(mContext);
 
         var popupWindowBuilder =
                 new AnchoredPopupWindow.Builder(
@@ -140,7 +142,7 @@ public class FuseboxCoordinator implements UrlFocusChangeListener, TemplateUrlSe
         popupWindowBuilder.setAnimateFromAnchor(true);
         popupWindowBuilder.setPreferredHorizontalOrientation(
                 HorizontalOrientation.LAYOUT_DIRECTION);
-        popupWindowBuilder.setViewportRectProvider(viewportRectProvider);
+        popupWindowBuilder.setViewportRectProvider(mViewportRectProvider);
 
         var popup = new FuseboxPopup(mContext, popupWindowBuilder.build(), popupView);
         mViewHolder = new FuseboxViewHolder(parent, popup);
@@ -223,6 +225,9 @@ public class FuseboxCoordinator implements UrlFocusChangeListener, TemplateUrlSe
         if (mComposeBoxQueryControllerBridge != null) {
             mComposeBoxQueryControllerBridge.destroy();
             mComposeBoxQueryControllerBridge = null;
+        }
+        if (mViewportRectProvider != null) {
+            mViewportRectProvider.destroy();
         }
     }
 
@@ -404,5 +409,9 @@ public class FuseboxCoordinator implements UrlFocusChangeListener, TemplateUrlSe
 
         @Override
         public void onLowMemory() {}
+
+        public void destroy() {
+            mContext.unregisterComponentCallbacks(this);
+        }
     }
 }
