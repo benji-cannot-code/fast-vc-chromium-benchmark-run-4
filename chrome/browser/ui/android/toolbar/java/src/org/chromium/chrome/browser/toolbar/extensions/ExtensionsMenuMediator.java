b47a2626ae55b741aa4425ca1a6d5b3d76c6360e
Shuhei Takahashi <nya@chromium.org>
2025-12-10 18:42:50 -0800

    reland: extensions: Check UI eligibility in ExtensionToolbarCoordinator
    This is a reland of commit 257a0801ca09c4ea5774567c6783994b0ba5f707
    
    Original change's description:
    > extensions: Check UI eligibility in ExtensionToolbarCoordinator
    >
    > This is a part of the patch series to use ExtensionActionViewModel on
    > Desktop Android.
    >
    > Somehow, it used to be ExtensionsMenuMediator's responsibility to
    > enable/disable the extensions UI according to the profile's eligibility.
    > This patch improves the enablement mechanism by making
    > ExtensionToolbarCoordinator responsible for the decision.
    >
    > This is useful for the ExtensionActionViewModel work as it reduces
    > dependencies to Profile.
    >
    > Bug: 448199168
    > Change-Id: I9a37d060f846ec42c5d64bfd293ec8e8b35b30be
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7236605
    > Reviewed-by: Patrick Noland <pnoland@chromium.org>
    > Reviewed-by: Masa Fujita <massan@google.com>
    > Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1555879}
    
    Bug: 448199168
    Change-Id: I1ad20598c5e2485c5b36effacfeaec8f4ce1eb15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7244210
    Commit-Queue: Masa Fujita <massan@google.com>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1557085}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/extensions/ExtensionsMenuMediator.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/extensions/ExtensionsMenuMediator.java
@@ -10,7 +10,6 @@ import android.graphics.Bitmap;
 import android.graphics.Rect;
 import android.view.View;
 
-import org.chromium.base.Callback;
 import org.chromium.base.lifetime.Destroyable;
 import org.chromium.base.supplier.NullableObservableSupplier;
 import org.chromium.base.supplier.ObservableSupplier;
@@ -39,11 +38,8 @@ class ExtensionsMenuMediator implements Destroyable {
     private final ActionsUpdateDelegate mActionsUpdateDelegate = new ActionsUpdateDelegate();
     private final Context mContext;
     private final ChromeAndroidTask mTask;
-    private final ObservableSupplier<@Nullable Profile> mProfileSupplier;
     private final Runnable mOnUpdateFinishedRunnable;
-    private final Callback<Boolean> mOnExtensionsAvailableCallback;
     private final ExtensionActionsUpdateHelper mExtensionActionsUpdateHelper;
-    private final Callback<@Nullable Profile> mProfileUpdatedCallback = this::onProfileUpdated;
     private final View mRootView;
 
     public ExtensionsMenuMediator(
@@ -53,14 +49,10 @@ class ExtensionsMenuMediator implements Destroyable {
             NullableObservableSupplier<Tab> currentTabSupplier,
             ModelList extensionModels,
             Runnable onUpdateFinishedRunnable,
-            Callback<Boolean> onExtensionsAvailableCallback,
             View rootView) {
         mTask = task;
-        mProfileSupplier = profileSupplier;
-        mProfileSupplier.addObserver(mProfileUpdatedCallback);
 
         mOnUpdateFinishedRunnable = onUpdateFinishedRunnable;
-        mOnExtensionsAvailableCallback = onExtensionsAvailableCallback;
         mContext = context;
         mRootView = rootView;
 
@@ -101,13 +93,6 @@ class ExtensionsMenuMediator implements Destroyable {
         }
     }
 
-    private void onProfileUpdated(@Nullable Profile profile) {
-        // TODO(crbug.com/422307625): Remove this check once extensions are ready for dogfooding.
-        boolean extensionsSupported =
-                profile != null ? ExtensionActionsBridge.extensionsEnabled(profile) : false;
-        mOnExtensionsAvailableCallback.onResult(extensionsSupported);
-    }
-
     private void onPrimaryClick(ListMenuButton buttonView, String actionId) {
         Tab currentTab = mExtensionActionsUpdateHelper.getCurrentTab();
         if (currentTab == null) {
@@ -134,7 +119,6 @@ class ExtensionsMenuMediator implements Destroyable {
     @Override
     public void destroy() {
         mExtensionActionsUpdateHelper.destroy();
-        mProfileSupplier.removeObserver(mProfileUpdatedCallback);
     }
 
     private class ActionsUpdateDelegate
