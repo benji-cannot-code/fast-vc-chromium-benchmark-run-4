e31ddbb5f918c2ec767716374b92fbbff45d93a1
Timofey Chudakov <tchudakov@google.com>
2025-12-11 05:15:20 -0800

    [AutofillAi - M4] Move dialog title strings to util file.
    This is needed to reuse the util function for the Android dialog.
    
    Bug: 460410690
    Change-Id: Ib14d3b355855d46efe1bd792515d341859743bd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7246103
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1557319}

---
+++ chrome/browser/ui/autofill/autofill_ai/autofill_ai_import_string_utils.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_AUTOFILL_AUTOFILL_AI_AUTOFILL_AI_IMPORT_STRING_UTILS_H_
+#define CHROME_BROWSER_UI_AUTOFILL_AUTOFILL_AI_AUTOFILL_AI_IMPORT_STRING_UTILS_H_
+
+#include <string>
+
+#include "components/autofill/core/browser/data_model/autofill_ai/entity_type.h"
+
+namespace autofill {
+
+std::u16string GetPromptTitle(EntityTypeName type_name, bool is_save_prompt);
+
+}  // namespace autofill
+
+#endif  // CHROME_BROWSER_UI_AUTOFILL_AUTOFILL_AI_AUTOFILL_AI_IMPORT_STRING_UTILS_H_
