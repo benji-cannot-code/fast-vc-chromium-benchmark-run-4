2eac635e9ee71b892c47bdf625415b89e6cdb643
Tarek Makkouk <tarekmakkouk@google.com>
2025-12-12 11:53:07 -0800

    [iOS] Add flag for enabling refactored FRE path through the BrowserAgent
    Bug: 468347209
    Change-Id: Ib1c490332a0ce2a8fb826d66074b53de5c90e15d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7254197
    Reviewed-by: Joemer Ramos <joemerramos@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Tarek Makkouk <tarekmakkouk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1558214}

--- ios/chrome/browser/flags/about_flags.mm
+++ ios/chrome/browser/flags/about_flags.mm
@@ -2874,6 +2874,9 @@ constexpr auto kFeatureEntries = std::to_array<flags_ui::FeatureEntry>({
     {"gemini-personalization", flag_descriptions::kGeminiPersonalizationName,
      flag_descriptions::kGeminiPersonalizationDescription, flags_ui::kOsIos,
      FEATURE_VALUE_TYPE(kGeminiPersonalization)},
+    {"gemini-refactored-fre", flag_descriptions::kGeminiRefactoredFREName,
+     flag_descriptions::kGeminiRefactoredFREDescription, flags_ui::kOsIos,
+     FEATURE_VALUE_TYPE(kGeminiRefactoredFRE)},
     {"composebox-fetch-contextual-suggestions-for-image",
      flag_descriptions::kComposeboxFetchContextualSuggestionsForImageName,
      flag_descriptions::
