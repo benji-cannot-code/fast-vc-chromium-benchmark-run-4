80cfb74b2c024f69f942148e98110cc71d85bda9
Kevin McNee <mcnee@chromium.org>
2025-12-12 07:30:20 -0800

    Move actor tools off of old MayActOnUrl implementation
    We now have a method on the tool delegate which goes through the policy
    checker.
    
    Bug: 458045204
    Change-Id: I2baedc0bc1e0108196e0f439576497e12f0c93bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7254596
    Auto-Submit: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: William Liu <liuwilliam@chromium.org>
    Reviewed-by: William Liu <liuwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1558020}

--- chrome/browser/actor/tools/tool_delegate.h
+++ chrome/browser/actor/tools/tool_delegate.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/flat_map.h"
 #include "base/functional/callback_forward.h"
+#include "chrome/browser/actor/site_policy.h"
 #include "chrome/common/actor_webui.mojom.h"
 #include "components/autofill/core/browser/integrators/glic/actor_form_filling_types.h"
 #include "components/password_manager/core/browser/actor_login/actor_login_types.h"
@@ -58,6 +59,12 @@ class ToolDelegate {
 
   // Returns the favicon service for the profile associated with the task.
   virtual favicon::FaviconService* GetFaviconService() = 0;
+
+  // Invokes the given callback according to whether the tool may navigate to
+  // the given URL.
+  virtual void IsAcceptableNavigationDestination(
+      const GURL& url,
+      DecisionCallbackWithReason callback) = 0;
 
   // Prompts the user to select a credential from the list of credentials, and
   // with optional icons for each site or app that is associated with the
