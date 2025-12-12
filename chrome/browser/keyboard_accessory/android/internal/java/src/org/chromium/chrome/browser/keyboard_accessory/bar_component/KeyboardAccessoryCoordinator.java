3fde8ae80362e01803391e8249752632d95b8871
Andrew Grieve <agrieve@chromium.org>
2025-12-12 12:49:45 -0800

    Android: Convert KeyboardInsetObservableSupplier -> SettableNonNullObservableSupplier
    Bug: 467389696
    Change-Id: I4e6c3437a895f8f14da9de0f72f7db77befce81c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7254198
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1558242}

--- chrome/browser/keyboard_accessory/android/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/bar_component/KeyboardAccessoryCoordinator.java
+++ chrome/browser/keyboard_accessory/android/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/bar_component/KeyboardAccessoryCoordinator.java
@@ -21,6 +21,7 @@ import androidx.viewpager.widget.ViewPager;
 import org.chromium.base.Callback;
 import org.chromium.base.TraceEvent;
 import org.chromium.base.lifetime.Destroyable;
+import org.chromium.base.supplier.NonNullObservableSupplier;
 import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.build.annotations.MonotonicNonNull;
 import org.chromium.build.annotations.NullMarked;
@@ -454,7 +455,7 @@ public class KeyboardAccessoryCoordinator implements KeyboardAccessoryVisualStat
         private final View mViewToPad;
         private final int mDefaultBottomPadding;
         private final ObservableSupplier<EdgeToEdgeController> mEdgeToEdgeControllerSupplier;
-        private final ObservableSupplier<Integer> mKeyboardInsetSupplier;
+        private final NonNullObservableSupplier<Integer> mKeyboardInsetSupplier;
         private @Nullable EdgeToEdgeController mEdgeToEdgeController;
         private final Callback<EdgeToEdgeController> mControllerChangedCallback =
                 this::updateEdgeToEdgeController;
@@ -466,7 +467,7 @@ public class KeyboardAccessoryCoordinator implements KeyboardAccessoryVisualStat
         EdgeToEdgePadObserver(
                 View view,
                 ObservableSupplier<EdgeToEdgeController> edgeToEdgeControllerSupplier,
-                ObservableSupplier<Integer> keyboardInsetSupplier) {
+                NonNullObservableSupplier<Integer> keyboardInsetSupplier) {
             mViewToPad = view;
             mDefaultBottomPadding = mViewToPad.getPaddingBottom();
             mEdgeToEdgeControllerSupplier = edgeToEdgeControllerSupplier;
