c7463325a30f192009edb7b722b72fafc37271ab
Steinar H. Gunderson <sesse@chromium.org>
2025-12-12 00:10:40 -0800

    Revert "Make Agent not Supplementable."
    This reverts commit 7f559a4dc3c40fa65c31f633428b25075a98a5d5.
    (See blink-dev for background.)
    
    Bug: 460165754
    Change-Id: I24f9695de696d7957195d19e340a07f81235417b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7246159
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1557898}

--- third_party/blink/renderer/core/execution_context/agent.cc
+++ third_party/blink/renderer/core/execution_context/agent.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/mutation_observer.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
-#include "third_party/blink/renderer/core/html/custom/custom_element_reaction_stack.h"
 
 namespace blink {
 
@@ -48,8 +47,7 @@ Agent::Agent(v8::Isolate* isolate,
 Agent::~Agent() = default;
 
 void Agent::Trace(Visitor* visitor) const {
-  visitor->Trace(custom_element_reaction_stack_);
-  visitor->Trace(mutation_observer_agent_data_);
+  Supplementable::Trace(visitor);
 }
 
 void Agent::AttachContext(ExecutionContext* context) {
--- third_party/blink/renderer/core/html/custom/custom_element_reaction_stack.h
+++ third_party/blink/renderer/core/html/custom/custom_element_reaction_stack.h
@@ -23,8 +23,11 @@ class Element;
 class CORE_EXPORT CustomElementReactionStack final
     : public GarbageCollected<CustomElementReactionStack>,
       public NameClient,
-      public GarbageCollectedMixin {
+      public Supplement<Agent> {
  public:
+  static constexpr auto kSupplementIndex =
+      Agent::Supplements::kCustomElementReactionStack;
+
   explicit CustomElementReactionStack(Agent& agent);
   CustomElementReactionStack(const CustomElementReactionStack&) = delete;
   CustomElementReactionStack& operator=(const CustomElementReactionStack&) =
@@ -46,7 +49,6 @@ class CORE_EXPORT CustomElementReactionStack final
   static CustomElementReactionStack& From(Agent& agent);
 
  private:
-  Member<Agent> agent_;
   friend class ResetCustomElementReactionStackForTest;
 
   using ElementReactionQueueMap =
