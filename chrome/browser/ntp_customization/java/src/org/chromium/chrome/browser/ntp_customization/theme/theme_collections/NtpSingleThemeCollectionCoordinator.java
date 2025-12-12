1ead9bf391b08be3097516ab54b751bb42b2bc3b
Yanting Yu <yyanting@google.com>
2025-12-12 11:26:13 -0800

    [NTP Theme] Add a metric for theme collection daily refresh.
    This CL adds metrics
    "NewTabPage.Customization.Theme.ThemeCollection.DailyRefresh.TurnOn" and
    "NewTabPage.Customization.Theme.ThemeCollection.DailyRefresh.TurnOff" to
    record the hashed theme collection ID when the user toggles on/off
    refresh daily switch button in single theme collection bottom sheet.
    This metric is logged when the bottom sheet is closed. If the user
    toggles on/off several times, and/or in several different theme
    collections, it will only record the last toggle behavior.
    
    The histogram is recorded as
    * turn-on https://drive.google.com/file/d/11c_IrE9nh0d4EKce4GeUJMv6e3tq0qnY/view?usp=sharing
    * turn-off https://drive.google.com/file/d/1VPKFWGvalOwZNK85GJF6-4haL292PVjb/view?usp=sharing
    
    Bug: 458757699
    Change-Id: Ib5cc21f5842eba2f4d02e10211e9a25eb95c48fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7253412
    Reviewed-by: Xinyi Ji <xinyiji@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Yanting Yu <yyanting@google.com>
    Cr-Commit-Position: refs/heads/main@{#1558194}

--- chrome/browser/ntp_customization/java/src/org/chromium/chrome/browser/ntp_customization/theme/theme_collections/NtpSingleThemeCollectionCoordinator.java
+++ chrome/browser/ntp_customization/java/src/org/chromium/chrome/browser/ntp_customization/theme/theme_collections/NtpSingleThemeCollectionCoordinator.java
@@ -22,6 +22,7 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.browser.ntp_customization.BottomSheetDelegate;
 import org.chromium.chrome.browser.ntp_customization.NtpCustomizationMetricsUtils;
 import org.chromium.chrome.browser.ntp_customization.R;
@@ -42,9 +43,6 @@ public class NtpSingleThemeCollectionCoordinator {
             "https://support.google.com/chrome/?p=new_tab";
     private static final int RECYCLE_VIEW_SPAN_COUNT = 3;
 
-    private String mThemeCollectionId;
-    private String mThemeCollectionTitle;
-    private int mThemeCollectionHash;
     private final List<CollectionImage> mThemeCollectionImageList = new ArrayList<>();
     private final Context mContext;
     private final View mNtpSingleThemeCollectionBottomSheetView;
@@ -61,9 +59,17 @@ public class NtpSingleThemeCollectionCoordinator {
     private final int mItemMaxWidth;
     private final int mSpacing;
     private final Runnable mOnDailyRefreshCancelledCallback;
+    private String mThemeCollectionId;
+    private String mThemeCollectionTitle;
+    private int mThemeCollectionHash;
     private boolean mHasDisplayedBefore;
     private int mScreenWidth;
+    // This variable is only used to record metrics.
     private boolean mIsThemeCollectionSelected;
+    // This variable is only used to record metrics.
+    private int mDailyRefreshThemeCollectionHash;
+    // This variable is only used to record metrics.
+    private @Nullable Boolean mIsDailyRefreshEnabled;
 
     /**
      * Constructor for the single theme collection coordinator.
@@ -185,6 +191,11 @@ public class NtpSingleThemeCollectionCoordinator {
         if (mNtpThemeCollectionsAdapter != null) {
             mNtpThemeCollectionsAdapter.clearOnClickListeners();
         }
+
+        if (mIsDailyRefreshEnabled != null) {
+            NtpCustomizationMetricsUtils.recordThemeCollectionDailyRefresh(
+                    mDailyRefreshThemeCollectionHash, mIsDailyRefreshEnabled);
+        }
     }
 
     /**
@@ -259,6 +270,8 @@ public class NtpSingleThemeCollectionCoordinator {
 
     /** Handles clicks on the daily refresh switch. */
     private void handleDailyRefreshClick(View view, Boolean isChecked) {
+        mIsDailyRefreshEnabled = isChecked;
+        mDailyRefreshThemeCollectionHash = mThemeCollectionHash;
         if (isChecked) {
             mNtpThemeCollectionManager.setThemeCollectionDailyRefreshed(mThemeCollectionId);
         } else {
