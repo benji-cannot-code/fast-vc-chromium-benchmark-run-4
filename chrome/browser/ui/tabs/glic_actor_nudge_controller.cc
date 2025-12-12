1e7c08106122d24fa0a39fc120a5c3fe58e6e0b8
Maggie Jennings <mjenn@google.com>
2025-12-11 16:14:56 -0800

    [Cleanup] Clean up GlicActorUiNudgeRedesign flag
    Bug: 462144644
    Change-Id: I8941a6113d9d69acb2253a36bd431627148e7439
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7248638
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Auto-Submit: Maggie Jennings <mjenn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1557757}

--- chrome/browser/ui/tabs/glic_actor_nudge_controller.cc
+++ chrome/browser/ui/tabs/glic_actor_nudge_controller.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/glic_actor_task_icon_manager_factory.h"
 #include "chrome/browser/ui/views/tabs/tab_strip_action_container.h"
 #include "chrome/common/buildflags.h"
-#include "chrome/common/chrome_features.h"
 #include "ui/base/l10n/l10n_util.h"
 
 namespace tabs {
@@ -35,8 +34,7 @@ GlicActorNudgeController::GlicActorNudgeController(
       browser_(browser),
       tab_strip_action_container_(tab_strip_action_container),
       scoped_data_holder_(browser->GetUnownedUserDataHost(), *this) {
-  if (base::FeatureList::IsEnabled(features::kGlicActorUi) &&
-      base::FeatureList::IsEnabled(features::kGlicActorUiNudgeRedesign)) {
+  if (base::FeatureList::IsEnabled(features::kGlicActorUi)) {
     RegisterActorNudgeStateCallback();
     UpdateCurrentActorNudgeState();
   }
@@ -80,15 +78,10 @@ void GlicActorNudgeController::OnStateUpdateImpl(
       // TODO(crbug.com/458391262) revisit or cleanup implementation here for
       // m144.
     case ActorTaskNudgeState::Text::kCompleteTasks:
-      if (!base::FeatureList::IsEnabled(features::kGlicActorUiNudgeRedesign)) {
-        tab_strip_action_container_->TriggerGlicActorNudge(
-            l10n_util::GetStringUTF16(IDR_ACTOR_TASK_COMPLETE_NUDGE_LABEL));
-      }
       break;
   }
 
-  if (base::FeatureList::IsEnabled(features::kGlicActorUiNudgeRedesign) &&
-      tab_strip_action_container_->GetIsShowingGlicActorTaskIconNudge()) {
+  if (tab_strip_action_container_->GetIsShowingGlicActorTaskIconNudge()) {
     actor::ui::RecordTaskNudgeShown(actor_task_nudge_state);
   }
 }
