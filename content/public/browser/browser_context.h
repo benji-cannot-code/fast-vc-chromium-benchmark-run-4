dd7f57f47d0f8c67ae2422e83555f31e0abc55ec
David Sanders <dsanders11@ucsbalum.com>
2025-12-11 18:51:36 -0800

    Switch include in browser_context.h to advanced_memory_safety_checks.h
    The new //base/memory/advanced_memory_safety_checks.h header is a much
    lighter variant for call sites that only need the
    ADVANCED_MEMORY_SAFETY_CHECKS macro.
    
    Should save ~309 MiB of compiler input size.
    
    Bug: 40318405
    Change-Id: I4604fda8bbd3302038ffb22b9d0047064568e681
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7217956
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1557812}

--- content/public/browser/browser_context.h
+++ content/public/browser/browser_context.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/functional/function_ref.h"
-#include "base/memory/safety_checks.h"
+#include "base/memory/advanced_memory_safety_checks.h"
 #include "base/memory/weak_ptr.h"
 #include "base/supports_user_data.h"
 #include "content/common/content_export.h"
