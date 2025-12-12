965d6406640d23b2b82b91bfd4b53c9354650582
Abdelrahman Eed <abdoeed@google.com>
2025-12-12 03:05:43 -0800

    Fix Crash in FullscreenController::EnterFullscreenModeInternal
    The crash is due to trying to enter Fullscreen before the active tab is
    set in ExclusiveAccessContext. The change makes sure to call
    `onObservingDifferentTab` as soon as we initialize the
    ExclusiveAccessContext Also added some logs, that would hopefully help
    debug the issue if it re-occurs.
    
    Bug: b:457694833
    Change-Id: I77c815f231a959c4fc44572a3ba09046a5645717
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7239252
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Commit-Queue: Abdelrahman Eed <abdoeed@google.com>
    Reviewed-by: Muyao Xu <muyaoxu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1557942}

--- chrome/android/java/src/org/chromium/chrome/browser/ui/ExclusiveAccessContext.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ui/ExclusiveAccessContext.java
@@ -9,6 +9,7 @@ import android.content.Context;
 
 import org.jni_zero.CalledByNative;
 
+import org.chromium.base.Log;
 import org.chromium.base.lifetime.Destroyable;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
@@ -26,6 +27,8 @@ import org.chromium.content_public.browser.WebContents;
  */
 @NullMarked
 public class ExclusiveAccessContext implements Destroyable {
+    private static final String TAG = "ExclusiveAccessCtx";
+
     private final Context mContext;
     private final FullscreenManager mFullscreenManager;
     private final ActivityTabProvider.ActivityTabTabObserver mActiveTabObserver;
@@ -46,9 +49,14 @@ public class ExclusiveAccessContext implements Destroyable {
         mContext = context;
         mFullscreenManager = fullscreenManager;
         mActiveTabObserver =
-                new ActivityTabProvider.ActivityTabTabObserver(activityTabProvider) {
+                new ActivityTabProvider.ActivityTabTabObserver(
+                        activityTabProvider, /* shouldTrigger= */ true) {
                     @Override
                     protected void onObservingDifferentTab(@Nullable Tab tab) {
+                        if (mActiveTab == null || tab == null) {
+                            Log.i(TAG, "onObservingDifferentTab is new tab null? " + (tab == null));
+                        }
+
                         mActiveTab = tab;
                     }
                 };
@@ -66,7 +74,12 @@ public class ExclusiveAccessContext implements Destroyable {
 
     @CalledByNative
     public @Nullable Profile getProfile() {
-        return mActiveTab != null ? mActiveTab.getProfile() : null;
+        if (mActiveTab == null) {
+            Log.e(TAG, "mActiveTab is null in getProfile");
+            return null;
+        }
+
+        return mActiveTab.getProfile();
     }
 
     @CalledByNative
