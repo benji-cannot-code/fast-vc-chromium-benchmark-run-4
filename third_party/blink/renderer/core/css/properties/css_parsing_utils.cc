18ef9ba01222ff86c8d0c5882e31cbe25a9d4916
Rune Lillesveen <futhark@chromium.org>
2025-12-12 02:20:28 -0800

    Add missing nullptr check for GetPage() in accentcolor handling
    A non-rendered constructed document does not have a Page.
    
    Bug: 467732064
    Change-Id: Ifaf12d7ce07afe8b91f7638ef5ea19de5da10007
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7252951
    Reviewed-by: Steinar H Gunderson <sesse@chromium.org>
    Auto-Submit: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1557932}

--- third_party/blink/renderer/core/css/properties/css_parsing_utils.cc
+++ third_party/blink/renderer/core/css/properties/css_parsing_utils.cc
@@ -2251,8 +2251,10 @@ bool SystemAccentColorAllowed(const CSSParserContext& context) {
   // contexts because it could be read back by the page and used for
   // fingerprinting.
   if (const auto* document = context.GetDocument()) {
-    if (document->GetPage()->GetChromeClient().IsIsolatedSVGChromeClient()) {
-      return false;
+    if (Page* page = document->GetPage()) {
+      if (page->GetChromeClient().IsIsolatedSVGChromeClient()) {
+        return false;
+      }
     }
   }
 
