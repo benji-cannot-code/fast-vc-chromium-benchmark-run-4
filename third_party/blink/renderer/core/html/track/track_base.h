abaef38dde34e282bbbf43f4a34c0c2f2e602ab7
Steinar H. Gunderson <sesse@chromium.org>
2025-12-12 00:08:07 -0800

    Revert "Make TrackBase not Supplementable."
    This reverts commit 9ae0d408a673b54881982f87ae32bcdd4b480a1e.
    (See blink-dev for background.)
    
    Bug: 460165754
    Change-Id: I12e64ee1b656bff2273c26dc2da409b4ae41ce2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7246154
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1557893}

--- third_party/blink/renderer/core/html/track/track_base.h
+++ third_party/blink/renderer/core/html/track/track_base.h
@@ -29,17 +29,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/public/platform/web_media_player.h"
 #include "third_party/blink/renderer/core/core_export.h"
-#include "third_party/blink/renderer/platform/forward_declared_member.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
+#include "third_party/blink/renderer/platform/supplementable.h"
 #include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
 
 namespace blink {
 
 class HTMLMediaElement;
-class SourceBufferTrackBaseSupplement;
 
-class CORE_EXPORT TrackBase : public GarbageCollectedMixin {
+class CORE_EXPORT TrackBase : public Supplementable<TrackBase, 1> {
  public:
+  enum class Supplements { kSourceBufferTrackBaseSupplement = 0 };
+
   // Subclasses of TrackBase including AudioTrack and VideoTrack allow changing
   // whether a track is enabled or selected. There are three sources where this
   // change can come from:
@@ -72,16 +73,6 @@ class CORE_EXPORT TrackBase : public GarbageCollectedMixin {
 
   void Trace(Visitor*) const override;
 
-  ForwardDeclaredMember<SourceBufferTrackBaseSupplement>
-  GetSourceBufferTrackBaseSupplement() const {
-    return source_buffer_track_base_supplement_;
-  }
-  void SetSourceBufferTrackBaseSupplement(
-      ForwardDeclaredMember<SourceBufferTrackBaseSupplement>
-          source_buffer_track_base_supplement) {
-    source_buffer_track_base_supplement_ = source_buffer_track_base_supplement;
-  }
-
  protected:
   TrackBase(WebMediaPlayer::TrackType,
             const AtomicString& label,
@@ -93,8 +84,6 @@ class CORE_EXPORT TrackBase : public GarbageCollectedMixin {
   AtomicString language_;
   String id_;
   Member<HTMLMediaElement> media_element_;
-  ForwardDeclaredMember<SourceBufferTrackBaseSupplement>
-      source_buffer_track_base_supplement_;
 };
 
 }  // namespace blink
--- third_party/blink/renderer/modules/mediasource/source_buffer_track_base_supplement.cc
+++ third_party/blink/renderer/modules/mediasource/source_buffer_track_base_supplement.cc
@@ -11,9 +11,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 // static
+const unsigned SourceBufferTrackBaseSupplement::kSupplementIndex =
+    static_cast<unsigned>(
+        TrackBase::Supplements::kSourceBufferTrackBaseSupplement);
+
+// static
 SourceBufferTrackBaseSupplement* SourceBufferTrackBaseSupplement::FromIfExists(
     TrackBase& track) {
-  return track.GetSourceBufferTrackBaseSupplement();
+  return Supplement<TrackBase>::From<SourceBufferTrackBaseSupplement>(track);
 }
 
 // static
@@ -22,7 +27,7 @@ SourceBufferTrackBaseSupplement& SourceBufferTrackBaseSupplement::From(
   SourceBufferTrackBaseSupplement* supplement = FromIfExists(track);
   if (!supplement) {
     supplement = MakeGarbageCollected<SourceBufferTrackBaseSupplement>(track);
-    track.SetSourceBufferTrackBaseSupplement(supplement);
+    Supplement<TrackBase>::ProvideTo(track, supplement);
   }
   return *supplement;
 }
@@ -37,7 +42,7 @@ SourceBuffer* SourceBufferTrackBaseSupplement::sourceBuffer(TrackBase& track) {
 
 SourceBufferTrackBaseSupplement::SourceBufferTrackBaseSupplement(
     TrackBase& track)
-    : track_base_(track) {}
+    : Supplement(track) {}
 
 void SourceBufferTrackBaseSupplement::SetSourceBuffer(
     TrackBase& track,
@@ -47,7 +52,7 @@ void SourceBufferTrackBaseSupplement::SetSourceBuffer(
 
 void SourceBufferTrackBaseSupplement::Trace(Visitor* visitor) const {
   visitor->Trace(source_buffer_);
-  visitor->Trace(track_base_);
+  Supplement<TrackBase>::Trace(visitor);
 }
 
 }  // namespace blink
