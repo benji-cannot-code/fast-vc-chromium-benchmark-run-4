e0a2aa85221b0defd32ea42d4a416cb7d1a13ad6
Patrick Monette <pmonette@chromium.org>
2025-12-12 10:31:29 -0800

    Using DeleteSoon for async registrations in memory pressure observers
    I initially didn't use DeleteSoon because I wanted to ensure that the
    unregistrations would always be executed, even in the case where the
    task queue is cleared without running.
    
    This approach isn't guaranteed to work because the task can also be
    deleted if it is rejected by the PostTask() call, which would result in
    the unregistration running in a non-thread-safe manner.
    
    An issue with DeleteSoon, is that the object is leaked if the task never
    ends up being run. While this is the behavior that we want in production
    code, this cause the leak sanitizer to complain in tests where the
    TaskEnvironment is not correctly flushed.
    
    Bug: 406578344
    Change-Id: I7c43e896e1b985aba045dcb5cf52b4d626d74e30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7252090
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1558155}

--- base/memory/memory_pressure_listener.cc
+++ base/memory/memory_pressure_listener.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 #include <utility>
 
+#include "base/debug/leak_annotations.h"
 #include "base/functional/bind.h"
 #include "base/memory/memory_pressure_listener_registry.h"
 #include "base/task/sequenced_task_runner.h"
@@ -144,12 +145,11 @@ AsyncMemoryPressureListenerRegistration::
     ~AsyncMemoryPressureListenerRegistration() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   if (main_thread_) {
-    // To ensure |main_thread_| is deleted on the correct thread, we transfer
-    // ownership to a no-op task. The object is deleted with the task, even
-    // if it's cancelled before it can run.
-    main_thread_task_runner_->PostTask(
-        FROM_HERE, BindOnce([](std::unique_ptr<MainThread> main_thread) {},
-                            std::move(main_thread_)));
+    // In tests, tasks on the main thread are not executed upon destruction of
+    // the TaskEnvironment. The main thread object thus gets tagged as leaking,
+    // which is fine in this case.
+    ANNOTATE_LEAKING_OBJECT_PTR(main_thread_.get());
+    main_thread_task_runner_->DeleteSoon(FROM_HERE, main_thread_.release());
   }
 }
 
