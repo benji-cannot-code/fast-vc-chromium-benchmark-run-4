66a70009df6f841b9f0e2b16df575cc5731400f8
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2025-12-12 08:46:42 -0800

    Revert "Remove insignificant trailing spaces from the output of testRunner.log()"
    This reverts commit 65c2187d64cb6fadc17087fa9cff4e2e945835c8.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/6421186574548992
    
    Sample build with failed test: https://ci.chromium.org/b/8695694665875663633
    Affected test(s):
    [://\:blink_web_tests!webtest::http/tests/inspector-protocol/fetch-later#activate-and-redirect-cross-site-when-document-alive.https.js](https://ci.chromium.org/ui/test/chromium/:%2F%2F%5C:blink_web_tests%21webtest::http%2Ftests%2Finspector-protocol%2Ffetch-later%23activate-and-redirect-cross-site-when-document-alive.https.js?q=VHash%3A9b30675a77baf900)
    [://\:blink_web_tests!webtest::http/tests/inspector-protocol/fetch-later#activate-and-redirect-when-document-alive.https.js](https://ci.chromium.org/ui/test/chromium/:%2F%2F%5C:blink_web_tests%21webtest::http%2Ftests%2Finspector-protocol%2Ffetch-later%23activate-and-redirect-when-document-alive.https.js?q=VHash%3A9b30675a77baf900)
    [://\:blink_web_tests!webtest::http/tests/inspector-protocol/fetch-later#activate-post-when-document-alive.https.js](https://ci.chromium.org/ui/test/chromium/:%2F%2F%5C:blink_web_tests%21webtest::http%2Ftests%2Finspector-protocol%2Ffetch-later%23activate-post-when-document-alive.https.js?q=VHash%3A9b30675a77baf900)
    [://\:blink_web_tests!webtest::http/tests/inspector-protocol/fetch-later#activate-when-document-alive.https.js](https://ci.chromium.org/ui/test/chromium/:%2F%2F%5C:blink_web_tests%21webtest::http%2Ftests%2Finspector-protocol%2Ffetch-later%23activate-when-document-alive.https.js?q=VHash%3A9b30675a77baf900)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F6421186574548992&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F7248977&type=BUG
    
    Original change's description:
    > Remove insignificant trailing spaces from the output of testRunner.log()
    >
    > ... to prevent linter from tripping on it.
    > NOPRESUBMIT=true
    > (because win-presubmit can't handle so many files)
    >
    > Change-Id: Ia072a0766dee97ed688e82b0703a5c78d677be87
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7248977
    > Reviewed-by: Alex N. Jose <alexnj@chromium.org>
    > Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1557684}
    >
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I6e2451cc61e43dd9a2ccfc12ffb8ff1194a8c2b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7256494
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Owners-Override: Slobodan Pejic <slobodan@google.com>
    Commit-Queue: Slobodan Pejic <slobodan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1558087}

--- third_party/blink/web_tests/http/tests/inspector-protocol/resources/inspector-protocol-test.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/resources/inspector-protocol-test.js
@@ -103,19 +103,13 @@ var TestRunner = class {
         else
           dumpProperties(value, prefix, prefixWithName);
       } else {
-        const valueStr = String(value).replace(/\n/g, ' ');
-        if (valueStr.length)
-          prefixWithName += ' ';
-        lines.push(prefixWithName + valueStr);
+        lines.push(prefixWithName + String(value).replace(/\n/g, ' '));
       }
     }
 
     function dumpProperties(object, prefix, firstLinePrefix) {
       prefix = prefix || '';
       firstLinePrefix = firstLinePrefix || prefix;
-      if (/\S$/.test(firstLinePrefix)) {
-        firstLinePrefix += ' ';
-      }
       lines.push(firstLinePrefix + '{');
 
       var propertyNames = Object.keys(object);
@@ -124,7 +118,7 @@ var TestRunner = class {
         var name = propertyNames[i];
         if (!object.hasOwnProperty(name))
           continue;
-        var prefixWithName = '    ' + prefix + name + ' :';
+        var prefixWithName = '    ' + prefix + name + ' : ';
         var value = object[name];
         if (stabilizeValues && stabilizeValues.includes(name)) {
           if (!stableValues.has(value)) {
@@ -142,12 +136,9 @@ var TestRunner = class {
     function dumpItems(object, prefix, firstLinePrefix) {
       prefix = prefix || '';
       firstLinePrefix = firstLinePrefix || prefix;
-      if (/\S$/.test(firstLinePrefix)) {
-        firstLinePrefix += ' ';
-      }
       lines.push(firstLinePrefix + '[');
       for (var i = 0; i < object.length; ++i)
-        dumpValue(object[i], '    ' + prefix, '    ' + prefix + '[' + i + '] :');
+        dumpValue(object[i], '    ' + prefix, '    ' + prefix + '[' + i + '] : ');
       lines.push(prefix + ']');
     }
 
