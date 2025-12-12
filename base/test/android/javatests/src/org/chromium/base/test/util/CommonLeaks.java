5208ed40257c5cb04d256c04a515e017a5dc2c1f
Sam Maier <smaier@chromium.org>
2025-12-12 08:34:38 -0800

    Reland "Add Leak Canary tests"
    This is a reland of commit 9f8d69c585d3694da4e694329ef86895f244f663
    
    Fixed cronet by using serviceloaders instead.
    
    Original change's description:
    > Add Leak Canary tests
    >
    > This cl adds the @EnableLeakChecks annotation, which can be used to
    > check for leaks via Leak Canary in any instrumentation test.
    >
    > We also add PublicTransitLeakTest.java, which covers some of the most
    > commonly used Activities.
    >
    > Change-Id: I2573b52fc7e914d555112425baaba01b67ff1557
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7162900
    > Commit-Queue: Sam Maier <smaier@chromium.org>
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1557482}
    
    Change-Id: Iec6417368a6305648fe15c47aff53ba863086364
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7252553
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1558078}

---
+++ base/test/android/javatests/src/org/chromium/base/test/util/CommonLeaks.java
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.base.test.util;
+
+import org.chromium.base.test.util.LeakCanaryChecker.LeakCanaryConfigProvider;
+import org.chromium.build.annotations.ServiceImpl;
+
+import java.util.Map;
+
+@SuppressWarnings("FieldCanBeFinal") // @IdentifierNameString requires non-final
+@ServiceImpl(LeakCanaryConfigProvider.class)
+public class CommonLeaks implements LeakCanaryConfigProvider {
+    // These are leaks we want to suppress globally for all tests. They could be legitimate leaks
+    // caused by //base (which we would hope to fix), or generic Android stuff that's unavoidable.
+
+    // Android's jank FrameTracker will post delayed messages to the looper. These messages hold
+    // references to our activities, but are scheduled for a few seconds delay. We could wait these
+    // messages out, but it's probably better to just directly ignore them (so we can avoid flakes).
+    private static final String sInteractionJankMonitorClass =
+            "com.android.internal.jank.InteractionJankMonitor$Configuration";
+    private static final String sInteractionJankMonitorField =
+            "com.android.internal.jank.InteractionJankMonitor$Configuration#mContext";
+
+    @Override
+    public Map<String, String> getInstanceFieldLeaks() {
+        return Map.of(sInteractionJankMonitorClass, sInteractionJankMonitorField);
+    }
+}
