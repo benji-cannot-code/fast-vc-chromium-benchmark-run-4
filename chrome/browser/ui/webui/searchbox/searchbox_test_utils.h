1e6298125b4da2fab39fe77f4f2b671f3892fa8d
Aviv Kiss <avivkiss@google.com>
2025-12-12 08:55:00 -0800

    [omnibox][next] Restore the popup on cancelling a file upload.
    Restoring the user to the popup closes the loop on the context
    preservation added in https://crrev.com/c/7230262 ensuring that the user
    is not surprised by preserved context at a later time. Additionally,
    this is better than some kind of clearing mechanism as it restores users
    to the place they last were and makes cancellation non-destructive. This
    is especially useful for multifile query construction where a user may
    open the file dialog multiple times and likely wants to preserve
    existing file selections even if they cancel their current one.
    
    Before: http://go/scrcast/NTE5NTczODE0NDMwOTI0OHw3ZWFiYzkwMC0wZQ
    After: http://go/scrcast/NjYxMzQxODMxNTM1MDAxNnw2MjRiYTBhMC1lNQ
    
    Bug: b:462763632
    Change-Id: I9d910601062c6dcf19873c18a594ceb38b15a1dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7245901
    Auto-Submit: Aviv Kiss <avivkiss@google.com>
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Reviewed-by: Marlon Facey <mfacey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1558095}

--- chrome/browser/ui/webui/searchbox/searchbox_test_utils.h
+++ chrome/browser/ui/webui/searchbox/searchbox_test_utils.h
@@ -106,6 +106,7 @@ class MockOmniboxEditModel : public OmniboxEditModel {
 
   // OmniboxEditModel:
   MOCK_METHOD(void, SetUserText, (const std::u16string&), (override));
+  MOCK_METHOD(void, OpenAiMode, (bool, bool), (override));
 };
 
 class MockLensSearchboxClient : public LensSearchboxClient {
