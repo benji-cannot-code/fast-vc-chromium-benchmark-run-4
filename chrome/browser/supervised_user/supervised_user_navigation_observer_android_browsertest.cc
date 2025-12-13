098964284d9ef35b4fac307c35042dfa0721cf23
Mark Schillaci <mschillaci@google.com>
2025-12-13 11:38:51 -0800

    [Desktop Zoom] Update default fieldtrial experiment
    This CL updated the fieltrial config for the desktop zoom feature to
    use a non-default value, which is the eventual launch candidate. The
    fact that we are scaling content 9% in some cases is not meaningful to
    include in render tests, and so we disable the feature in some cases.
    
    Bug: 450281745
    Change-Id: I057a6cef9364fc88ad99913a007ad7ec803772bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7238453
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1558444}

--- chrome/browser/supervised_user/supervised_user_navigation_observer_android_browsertest.cc
+++ chrome/browser/supervised_user/supervised_user_navigation_observer_android_browsertest.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_search_api/url_checker_client.h"
 #include "components/supervised_user/core/browser/supervised_user_interstitial.h"
 #include "components/supervised_user/core/common/features.h"
+#include "content/public/common/content_features.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/test_navigation_observer.h"
@@ -71,11 +72,22 @@ class SupervisedUserNavigationObserverAndroidBrowserTest
     // https), but the embedded test server runs on a random port and adds it to
     // the url spec.
     command_line->AppendSwitch(switches::kIgnoreGooglePortNumbers);
+
+#if BUILDFLAG(IS_ANDROID)
+    // On Android, we disable the feature that automatically scales web content
+    // because it is not meaningful and would change expected values.
+    scoped_feature_list_.InitWithFeatureStates(
+        {{kPropagateDeviceContentFiltersToSupervisedUser, true},
+         { features::kAndroidDesktopZoomScaling,
+           false }});
+#else
+    feature_list_.InitWithFeatureStates(
+        {{kPropagateDeviceContentFiltersToSupervisedUser, true}});
+#endif  // BUILDFLAG(IS_ANDROID)
   }
 
   base::HistogramTester histogram_tester_;
-  base::test::ScopedFeatureList scoped_feature_list_{
-      kPropagateDeviceContentFiltersToSupervisedUser};
+  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 // With disabled search content filters, the navigation is unchanged and safe
@@ -161,9 +173,22 @@ class SupervisedUserNavigationObserverNoApprovalsInterstitialAndroidBrowserTest
     navigation_observer.Wait();
   }
 
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+#if BUILDFLAG(IS_ANDROID)
+    // On Android, we disable the feature that automatically scales web content
+    // because it is not meaningful and would change expected values.
+    scoped_feature_list_.InitWithFeatureStates(
+        {{kSupervisedUserInterstitialWithoutApprovals, true},
+         { features::kAndroidDesktopZoomScaling,
+           false }});
+#else
+    feature_list_.InitWithFeatureStates(
+        {{kSupervisedUserInterstitialWithoutApprovals, true}});
+#endif  // BUILDFLAG(IS_ANDROID)
+  }
+
  private:
-  base::test::ScopedFeatureList scoped_feature_list_{
-      kSupervisedUserInterstitialWithoutApprovals};
+  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 // Shows the interstitial page when the search content filter is enabled.
