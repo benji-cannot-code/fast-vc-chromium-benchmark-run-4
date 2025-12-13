173782fff6cf40abc6e8c9d629f097b8bdf632ae
Sky Debreuil <skydebreuil@google.com>
2025-12-12 17:59:38 -0800

    Refactor UIResourceLayerImpl to use unnormalized coordinates
    Update UIResourceLayerImpl to generate TextureDrawQuads using
    unnormalized texture coordinates. This involves scaling the UV
    coordinates by the resource size in AppendQuads and setting the quad's
    is_tex_coords_normalized flag to false.
    
    To support this, update LayerTreeHostImpl to store the size of UI
    resources in UIResourceData upon creation or import. This is required as
    when running in TreesInViz mode the shared_image on UIResourceData is
    null.
    
    Add a GetUIResourceSize accessor to LayerTreeHostImpl and LayerTreeImpl.
    Update CreateUIResourceFromImportedResource to accept the resource size,
    and update LayerContextImpl to pass this information.
    
    Bug: 451876192
    Change-Id: Ia6dbee003f1c08884b09faabfb04a4581a4bf969
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7238552
    Commit-Queue: Sky Debreuil <skydebreuil@google.com>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    Auto-Submit: Sky Debreuil <skydebreuil@google.com>
    Cr-Commit-Position: refs/heads/main@{#1558383}

--- cc/trees/layer_tree_host_impl.cc
+++ cc/trees/layer_tree_host_impl.cc
@@ -6047,6 +6047,7 @@ void LayerTreeHostImpl::CreateUIResource(UIResourceId uid,
   data.opaque = bitmap.GetOpaque();
   data.shared_image = std::move(client_shared_image);
   data.resource_id_for_export = id;
+  data.size = upload_size;
   ui_resource_map_[uid] = std::move(data);
 
   MarkUIResourceNotEvicted(uid);
@@ -6062,6 +6063,7 @@ void LayerTreeHostImpl::CreateUIResource(UIResourceId uid,
 void LayerTreeHostImpl::CreateUIResourceFromImportedResource(
     UIResourceId uid,
     viz::ResourceId resource_id,
+    const gfx::Size& size,
     bool is_opaque) {
   TRACE_EVENT0(TRACE_DISABLED_BY_DEFAULT("cc.debug"),
                "LayerTreeHostImpl::CreateUIResourceFromResource");
@@ -6085,6 +6087,7 @@ void LayerTreeHostImpl::CreateUIResourceFromImportedResource(
   UIResourceData data;
   data.opaque = is_opaque;
   data.resource_id_for_export = resource_id;
+  data.size = size;
   ui_resource_map_[uid] = std::move(data);
 
   MarkUIResourceNotEvicted(uid);
@@ -6193,6 +6196,14 @@ viz::ResourceId LayerTreeHostImpl::ResourceIdForUIResource(
   if (iter != ui_resource_map_.end())
     return iter->second.resource_id_for_export;
   return viz::kInvalidResourceId;
+}
+
+gfx::Size LayerTreeHostImpl::GetUIResourceSize(UIResourceId uid) const {
+  if (auto it = ui_resource_map_.find(uid); it != ui_resource_map_.end()) {
+    return it->second.size;
+  }
+
+  return gfx::Size();
 }
 
 bool LayerTreeHostImpl::IsUIResourceOpaque(UIResourceId uid) const {
