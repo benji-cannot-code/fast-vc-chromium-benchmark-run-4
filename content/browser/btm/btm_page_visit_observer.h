15121b95a552ee75261fdff8bf1ee66bb3d1fc43
David Sanders <dsanders11@ucsbalum.com>
2025-12-12 16:36:52 -0800

    Add some missing includes of //base/functional/callback.h
    Bug: 40216326
    Change-Id: Ie2e2e9d67f50b20d88fd7613bbd79fa904b3d393
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7252984
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1558357}

--- content/browser/btm/btm_page_visit_observer.h
+++ content/browser/btm/btm_page_visit_observer.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/check_deref.h"
-#include "base/functional/callback_forward.h"
+#include "base/functional/callback.h"
 #include "base/memory/weak_ptr.h"
 #include "base/time/clock.h"
 #include "base/time/default_clock.h"
--- content/browser/web_contents_based_canceller.h
+++ content/browser/web_contents_based_canceller.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_BROWSER_WEB_CONTENTS_BASED_CANCELLER_H_
 #define CONTENT_BROWSER_WEB_CONTENTS_BASED_CANCELLER_H_
 
+#include "base/functional/callback.h"
 #include "content/public/browser/weak_document_ptr.h"
 #include "content/public/browser/web_contents_observer.h"
 
