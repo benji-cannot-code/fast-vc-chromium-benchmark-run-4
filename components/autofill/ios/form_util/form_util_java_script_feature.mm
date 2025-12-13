571d92fcbbd66ca6efd8c7c8edbd4524db26c2a2
Alexandra Pereira <alexandrasp@google.com>
2025-12-12 19:33:40 -0800

    [iOS][autofill] Move autofillSubmissionData out of gCrWeb
    This change is part of a series of CLs reorganizing autofill to improve
    code clarity. It simplifies the adoption of the gCrWeb class by
    isolating internal utility functions and removing them from the __gCrWeb
    API.
    
    Tests are updated accordingly, and save_profile_egtest tests that are no
    longer relevant have been removed.
    
    No functional change is intended.
    
    Bug: 448990422
    Change-Id: I6ccf463ff744927ebcbfaebe1e5af8d1b579f877
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7248639
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Commit-Queue: Alexandra Pereira <alexandrasp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1558399}

--- components/autofill/ios/form_util/form_util_java_script_feature.mm
+++ components/autofill/ios/form_util/form_util_java_script_feature.mm
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/public/js_messaging/java_script_feature_util.h"
 
 namespace {
-const char kFillScriptName[] = "fill";
 const char kFormScriptName[] = "form";
 }  // namespace
 
@@ -31,11 +30,6 @@ FormUtilJavaScriptFeature::FormUtilJavaScriptFeature()
           // worlds is required.
           web::ContentWorld::kAllContentWorlds,
           {FeatureScript::CreateWithFilename(
-               kFillScriptName,
-               FeatureScript::InjectionTime::kDocumentStart,
-               FeatureScript::TargetFrames::kAllFrames,
-               FeatureScript::ReinjectionBehavior::kInjectOncePerWindow),
-           FeatureScript::CreateWithFilename(
                kFormScriptName,
                FeatureScript::InjectionTime::kDocumentStart,
                FeatureScript::TargetFrames::kAllFrames,
