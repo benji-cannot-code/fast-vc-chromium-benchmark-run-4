d680c0bb7b8f1363a5f441a2837205d003034ffb
Harald Alvestrand <hta@chromium.org>
2025-12-15 03:40:46 -0800

    Use a few more tagged subscribe functions.
    This unblocks deprecating the tagless subscribes.
    
    Bug: webrtc:465197113
    Change-Id: I5ab1a6feab3d8d5edd6a0657f028009ecd3970d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7260757
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Harald Alvestrand <hta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1558646}

--- third_party/blink/renderer/modules/peerconnection/adapters/ice_transport_adapter_impl.cc
+++ third_party/blink/renderer/modules/peerconnection/adapters/ice_transport_adapter_impl.cc
@@ -93,16 +93,16 @@ void IceTransportAdapterImpl::SetupIceTransportChannel() {
         }
       });
   ice_transport_channel()->SubscribeCandidateGathered(
-      [that = weak_factory_.GetWeakPtr()](
-          webrtc::IceTransportInternal* transport,
-          const webrtc::Candidate& candidate) {
+      this, [that = weak_factory_.GetWeakPtr()](
+                webrtc::IceTransportInternal* transport,
+                const webrtc::Candidate& candidate) {
         if (that) {
           that->OnCandidateGathered(transport, candidate);
         }
       });
   ice_transport_channel()->SubscribeIceTransportStateChanged(
-      [that = weak_factory_.GetWeakPtr()](
-          webrtc::IceTransportInternal* transport) {
+      this, [that = weak_factory_.GetWeakPtr()](
+                webrtc::IceTransportInternal* transport) {
         if (that) {
           that->OnStateChanged(transport);
         }
@@ -115,8 +115,8 @@ void IceTransportAdapterImpl::SetupIceTransportChannel() {
         }
       });
   ice_transport_channel()->SubscribeRoleConflict(
-      [that = weak_factory_.GetWeakPtr()](
-          webrtc::IceTransportInternal* transport) {
+      this, [that = weak_factory_.GetWeakPtr()](
+                webrtc::IceTransportInternal* transport) {
         if (that) {
           that->OnRoleConflict(transport);
         }
