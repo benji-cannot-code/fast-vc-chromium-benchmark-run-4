d281634fb640eac61428c2f681742bfa5ae29d63
Georg Neis <neis@chromium.org>
2025-12-14 22:45:31 -0800

    ash: Eliminate a chrome/browser/ui/views/frame dependency
    Use the ImmersiveRevealedLock from chromeos/ui/frame/immersive/ instead.
    
    Bug: b:369688254
    Change-Id: I2e0ec26b8e2f8fef7d3387ccbe2b26a746eaa167
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7254435
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1558573}

--- chrome/browser/ash/boca/on_task/on_task_pod_controller_impl.cc
+++ chrome/browser/ash/boca/on_task/on_task_pod_controller_impl.cc
@@ -20,10 +20,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
-#include "chrome/browser/ui/views/frame/immersive_mode_controller.h"
 #include "chromeos/ash/components/boca/boca_metrics_util.h"
 #include "chromeos/strings/grit/chromeos_strings.h"
 #include "chromeos/ui/frame/frame_header.h"
+#include "chromeos/ui/frame/immersive/immersive_fullscreen_controller.h"
+#include "chromeos/ui/frame/immersive/immersive_revealed_lock.h"
 #include "content/public/browser/navigation_controller.h"
 #include "content/public/browser/web_contents.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -139,9 +140,15 @@ void OnTaskPodControllerImpl::ToggleTabStripVisibility(bool show,
   }
 
   // Acquire lock to reveal the tab strip.
-  tab_strip_reveal_lock_ =
-      ImmersiveModeController::From(&browser_->GetBrowser())
-          ->GetRevealedLock(ImmersiveModeController::ANIMATE_REVEAL_YES);
+  auto* widget =
+      views::Widget::GetWidgetForNativeWindow(browser_->GetNativeWindow());
+  chromeos::ImmersiveFullscreenController* controller =
+      widget ? chromeos::ImmersiveFullscreenController::Get(widget) : nullptr;
+  tab_strip_reveal_lock_.reset(
+      controller ? controller->GetRevealedLock(
+                       chromeos::ImmersiveRevealedLock::Delegate::
+                           AnimateReveal::ANIMATE_REVEAL_YES)
+                 : nullptr);
 }
 
 void OnTaskPodControllerImpl::SetSnapLocation(
@@ -275,7 +282,7 @@ views::Widget* OnTaskPodControllerImpl::GetPodWidgetForTesting() {
   return pod_widget_.get();
 }
 
-ImmersiveRevealedLock*
+chromeos::ImmersiveRevealedLock*
 OnTaskPodControllerImpl::GetTabStripRevealLockForTesting() {
   if (!tab_strip_reveal_lock_) {
     return nullptr;
--- chrome/browser/ash/boca/on_task/on_task_pod_controller_impl.h
+++ chrome/browser/ash/boca/on_task/on_task_pod_controller_impl.h
@@ -21,7 +21,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/rect.h"
 #include "ui/views/widget/widget.h"
 
+namespace chromeos {
 class ImmersiveRevealedLock;
+}  // namespace chromeos
 
 namespace ash {
 
@@ -67,7 +69,7 @@ class OnTaskPodControllerImpl : public OnTaskPodController,
 
   // Component accessors used for testing purposes.
   views::Widget* GetPodWidgetForTesting();
-  ImmersiveRevealedLock* GetTabStripRevealLockForTesting();
+  chromeos::ImmersiveRevealedLock* GetTabStripRevealLockForTesting();
   OnTaskPodSnapLocation GetSnapLocationForTesting() const;
 
  private:
@@ -82,7 +84,7 @@ class OnTaskPodControllerImpl : public OnTaskPodController,
   std::unique_ptr<views::Widget> pod_widget_;
 
   // Prevents the tab strip from hiding while in immersive fullscreen.
-  std::unique_ptr<ImmersiveRevealedLock> tab_strip_reveal_lock_;
+  std::unique_ptr<chromeos::ImmersiveRevealedLock> tab_strip_reveal_lock_;
 
   // Height of the window frame header. This is used to track the frame header
   // height when in unlocked mode for consistent positioning in locked mode.
