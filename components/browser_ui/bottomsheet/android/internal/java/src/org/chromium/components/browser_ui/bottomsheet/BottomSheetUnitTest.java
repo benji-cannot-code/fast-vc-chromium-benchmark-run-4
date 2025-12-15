20fd64c2d63ff111d94250813f423d036b8bd225
Wenyu Fu <wenyufu@chromium.org>
2025-12-15 13:43:41 -0800

    [BottomSheet] Make bottom_sheet_bg_color the default color
    As most of the bottom sheet are showing modal, using
    bottom_sheet_bg_color as the default color for the bottom sheet. As a
    result, we can make the previous sheet_bg_color macro a private color
    owned by the bottom sheet, renamed to getNonModalBottomSheetBgColor
    
    NO_IFTTT=Adding deprecate tag
    
    Bug: 468100488
    Change-Id: Ib70d4dcd6700589a03203ee19edc630af73d298d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7251724
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1558964}

--- components/browser_ui/bottomsheet/android/internal/java/src/org/chromium/components/browser_ui/bottomsheet/BottomSheetUnitTest.java
+++ components/browser_ui/bottomsheet/android/internal/java/src/org/chromium/components/browser_ui/bottomsheet/BottomSheetUnitTest.java
@@ -117,7 +117,7 @@ public class BottomSheetUnitTest {
 
     @Test
     public void testBackgroundColorAtFullHeightScrimmed_Light() {
-        doTestBackgroundColorFullHeightScrimmed(SemanticColorUtils.getSheetBgColor(mActivity));
+        doTestBackgroundColorFullHeightScrimmed(SemanticColorUtils.getColorSurface(mActivity));
     }
 
     private void doTestBackgroundColorFullHeightScrimmed(int expectedColor) {
@@ -151,7 +151,7 @@ public class BottomSheetUnitTest {
         mBottomSheet.showContent(mSheetContent);
         assertEquals(
                 "Unscrimmed sheet bg color is wrong.",
-                SemanticColorUtils.getSheetBgColor(mActivity),
+                SemanticColorUtils.getColorSurface(mActivity),
                 mBottomSheet.getSheetBackgroundColor());
     }
 
@@ -173,7 +173,7 @@ public class BottomSheetUnitTest {
         mBottomSheet.showContent(mSheetContent);
         assertEquals(
                 "PEEK state sheet bg color is wrong.",
-                SemanticColorUtils.getSheetBgColor(mActivity),
+                SemanticColorUtils.getColorSurface(mActivity),
                 mBottomSheet.getSheetBackgroundColor());
     }
 
@@ -183,7 +183,7 @@ public class BottomSheetUnitTest {
         int offset = 130;
         int expectedColor =
                 ColorUtils.overlayColor(
-                        SemanticColorUtils.getSheetBgColor(mActivity),
+                        SemanticColorUtils.getColorSurface(mActivity),
                         SemanticColorUtils.getColorSurfaceContainerHigh(mActivity),
                         0.5f // fraction = (130 - 60) / (200 - 60) = 0.5
                         );
@@ -194,7 +194,7 @@ public class BottomSheetUnitTest {
     public void testBackgroundColorNoTransitionInLight() {
         // Sheet color does not transition in light mode.
         showSheetThenScrollToHalfOffsetAndVerifyColor(
-                130, SemanticColorUtils.getSheetBgColor(mActivity));
+                130, SemanticColorUtils.getColorSurface(mActivity));
     }
 
     private void showSheetThenScrollToHalfOffsetAndVerifyColor(int offset, int expectedColor) {
@@ -232,7 +232,7 @@ public class BottomSheetUnitTest {
 
         assertEquals(
                 "Sheet bg color should be the override color.",
-                SemanticColorUtils.getSheetBgColor(mActivity),
+                SemanticColorUtils.getColorSurface(mActivity),
                 mBottomSheet.getSheetBackgroundColor());
     }
 }
