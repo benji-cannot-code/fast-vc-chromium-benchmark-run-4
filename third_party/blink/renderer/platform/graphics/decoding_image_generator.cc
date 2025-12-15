bdadc557297cc5c7fea15b9fa7bd3c920c95fece
Fredrik Söderquist <fs@opera.com>
2025-12-15 06:22:01 -0800

    Move ComputeInterpolationQuality() to graphics_context.cc
    This function does not have any relation Skia at this point in time.
    Move it closer to its single caller.
    
    Bug: 399058536
    Change-Id: I6e33b20a96ac84f122de3768254e12a422ac9759
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7260956
    Commit-Queue: Florin Malita <fmalita@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1558692}

--- third_party/blink/renderer/platform/graphics/decoding_image_generator.cc
+++ third_party/blink/renderer/platform/graphics/decoding_image_generator.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/image-decoders/segment_reader.h"
 #include "third_party/blink/renderer/platform/instrumentation/tracing/trace_event.h"
 #include "third_party/blink/renderer/platform/wtf/shared_buffer.h"
+#include "third_party/skia/include/core/SkCanvas.h"
 #include "third_party/skia/include/core/SkColorSpace.h"
 #include "third_party/skia/include/core/SkData.h"
 #include "third_party/skia/include/core/SkImageInfo.h"
