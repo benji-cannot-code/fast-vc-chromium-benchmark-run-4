d9cabf9da52e8d62a8f3654f63282a3d71b68799
Yuheng Huang <yuhengh@chromium.org>
2025-12-15 14:43:41 -0800

    [Contextual tasks] Close side panel if navigate to contextual tasks URL
    Close the side panel when the active tab is navigated to contextual
    tasks URL.
    
    Bug: 467750003
    Change-Id: I61c88064fa7d06c3aed9470e4008a190add419f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7257495
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1559009}

--- chrome/browser/contextual_tasks/contextual_tasks_ui_service.cc
+++ chrome/browser/contextual_tasks/contextual_tasks_ui_service.cc
@@ -63,11 +63,6 @@ constexpr char kUdmAiValue[] = "50";
 constexpr char kNemParam[] = "nem";
 constexpr char kNemAiValue[] = "143";
 
-bool IsContextualTasksHost(const GURL& url) {
-  return url.scheme() == content::kChromeUIScheme &&
-         url.host() == chrome::kChromeUIContextualTasksHost;
-}
-
 bool IsSignInDomain(const GURL& url) {
   if (!url.is_valid() || !url.SchemeIsHTTPOrHTTPS()) {
     return false;
@@ -295,7 +290,7 @@ bool ContextualTasksUiService::HandleNavigationImpl(
   }
 
   // Allow any navigation to the contextual tasks host.
-  if (IsContextualTasksHost(url_params.url)) {
+  if (IsContextualTasksUrl(url_params.url)) {
     return false;
   }
 
@@ -322,7 +317,7 @@ bool ContextualTasksUiService::HandleNavigationImpl(
   // Intercept any navigation where the wrapping WebContents is the WebUI host
   // unless it is the embedded page.
   if (is_from_embedded_page &&
-      IsContextualTasksHost(source_contents->GetLastCommittedURL())) {
+      IsContextualTasksUrl(source_contents->GetLastCommittedURL())) {
     // Ignore navigation triggered by UI.
     if (!url_params.is_renderer_initiated) {
       return false;
@@ -647,6 +642,11 @@ bool ContextualTasksUiService::IsAiUrl(const GURL& url) {
   }
 
   return false;
+}
+
+bool ContextualTasksUiService::IsContextualTasksUrl(const GURL& url) {
+  return url.scheme() == content::kChromeUIScheme &&
+         url.host() == chrome::kChromeUIContextualTasksHost;
 }
 
 bool ContextualTasksUiService::IsSearchResultsPage(const GURL& url) {
