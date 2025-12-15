91eb09fa0b0bd98353c6c882fa29f90ca70234c5
Abdelrahman Eed <abdoeed@google.com>
2025-12-15 01:34:00 -0800

    [Document PiP] Move setContentsBounds to WebContentsDelegateAndroid
    The setContentsBounds method is moved from TabWebContentsDelegateAndroid
    to the base WebContentsDelegateAndroid to allow its use by different
    activity types, such as DocumentPictureInPictureActivity. The core logic
    for moving an activity to specified bounds is extracted into a new
    utility method, MultiWindowUtils.moveActivityToBounds, and is now used
    by both ActivityTabWebContentsDelegateAndroid and
    DocumentPictureInPictureActivity. The native call path is updated to
    reflect this change.
    
    Bug: b:463335361
    Change-Id: Ia1832d02e79d1ecd4897c742ff1d359eab8f9364
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7222410
    Commit-Queue: Abdelrahman Eed <abdoeed@google.com>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1558607}

--- chrome/browser/android/tab_web_contents_delegate_android.cc
+++ chrome/browser/android/tab_web_contents_delegate_android.cc
@@ -79,6 +79,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/page/draggable_region.mojom.h"
 #include "third_party/blink/public/mojom/window_features/window_features.mojom.h"
 #include "third_party/skia/include/core/SkRegion.h"
+#include "ui/gfx/android/rect_jni_conversion.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/rect_f.h"
 #include "ui/gfx/geometry/skia_conversions.h"
@@ -103,23 +104,6 @@ using blink::mojom::FileChooserParams;
 using content::WebContents;
 
 namespace {
-
-static ScopedJavaLocalRef<jobject>
-JNI_TabWebContentsDelegateAndroidImpl_CreateJavaRectF(JNIEnv* env,
-                                                      const gfx::RectF& rect) {
-  return ScopedJavaLocalRef<jobject>(
-      Java_TabWebContentsDelegateAndroidImpl_createRectF(
-          env, rect.x(), rect.y(), rect.right(), rect.bottom()));
-}
-
-static ScopedJavaLocalRef<jobject>
-JNI_TabWebContentsDelegateAndroidImpl_CreateJavaRect(JNIEnv* env,
-                                                     const gfx::Rect& rect) {
-  return ScopedJavaLocalRef<jobject>(
-      Java_TabWebContentsDelegateAndroidImpl_createRect(
-          env, static_cast<int>(rect.x()), static_cast<int>(rect.y()),
-          static_cast<int>(rect.right()), static_cast<int>(rect.bottom())));
-}
 
 static ScopedJavaLocalRef<jobject>
 JNI_TabWebContentsDelegateAndroidImpl_CreateJavaWindowFeatures(
@@ -237,15 +221,12 @@ void TabWebContentsDelegateAndroid::FindMatchRectsReply(
   // Create the details object.
   ScopedJavaLocalRef<jobject> details_object =
       Java_TabWebContentsDelegateAndroidImpl_createFindMatchRectsDetails(
-          env, version, rects.size(),
-          JNI_TabWebContentsDelegateAndroidImpl_CreateJavaRectF(env,
-                                                                active_rect));
+          env, version, rects.size(), active_rect);
 
   // Add the rects
   for (size_t i = 0; i < rects.size(); ++i) {
     Java_TabWebContentsDelegateAndroidImpl_setMatchRectByIndex(
-        env, details_object, i,
-        JNI_TabWebContentsDelegateAndroidImpl_CreateJavaRectF(env, rects[i]));
+        env, details_object, i, rects[i]);
   }
 
   Java_TabWebContentsDelegateAndroidImpl_onFindMatchRectsAvailable(
@@ -420,24 +401,6 @@ WebContents* TabWebContentsDelegateAndroid::AddNewContents(
   return nullptr;
 }
 
-void TabWebContentsDelegateAndroid::SetContentsBounds(
-    content::WebContents* source,
-    const gfx::Rect& bounds) {
-  JNIEnv* env = AttachCurrentThread();
-  ScopedJavaLocalRef<jobject> obj = GetJavaDelegate(env);
-  if (!obj.is_null()) {
-    ScopedJavaLocalRef<jobject> jsource;
-    if (source) {
-      jsource = source->GetJavaWebContents();
-    }
-    ScopedJavaLocalRef<jobject> jbounds =
-        JNI_TabWebContentsDelegateAndroidImpl_CreateJavaRect(env, bounds);
-
-    Java_TabWebContentsDelegateAndroidImpl_setContentsBounds(env, obj, jsource,
-                                                             jbounds);
-  }
-}
-
 void TabWebContentsDelegateAndroid::OnDidBlockNavigation(
     content::WebContents* web_contents,
     const GURL& blocked_url,
@@ -525,14 +488,10 @@ void TabWebContentsDelegateAndroid::OnFindResultAvailable(
   const find_in_page::FindNotificationDetails& find_result =
       find_in_page::FindTabHelper::FromWebContents(web_contents)->find_result();
 
-  ScopedJavaLocalRef<jobject> selection_rect =
-      JNI_TabWebContentsDelegateAndroidImpl_CreateJavaRect(
-          env, find_result.selection_rect());
-
   // Create the details object.
   ScopedJavaLocalRef<jobject> details_object =
       Java_TabWebContentsDelegateAndroidImpl_createFindNotificationDetails(
-          env, find_result.number_of_matches(), selection_rect,
+          env, find_result.number_of_matches(), find_result.selection_rect(),
           find_result.active_match_ordinal(), find_result.final_update());
 
   Java_TabWebContentsDelegateAndroidImpl_onFindResultAvailable(env, obj,
