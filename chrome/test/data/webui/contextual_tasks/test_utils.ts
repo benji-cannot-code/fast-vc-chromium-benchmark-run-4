92a7fb3aa62c00172e3d1cb97e23ba032f79e451
David Pennington <dpenning@chromium.org>
2025-12-15 09:36:47 -0800

    [Contextual Tasks] Refactor the CSS for the Top Bar to match mocks
    Fixed Ellipses, fixed colors, fixed Icons.
    
    Fixed: 467361671
    Change-Id: Ia71e4eb3888885ebbb599e1e0ef0fc805bbc6429
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7248297
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Auto-Submit: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1558798}

--- chrome/test/data/webui/contextual_tasks/test_utils.ts
+++ chrome/test/data/webui/contextual_tasks/test_utils.ts
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {assertTrue} from 'chrome://webui-test/chai_assert.js';
+
 // Base64 encoding of a UI handshake request message [1, 2, 3].
 // Generated from btoa(String.fromCharCode(...[1, 2, 3]))
 export const HANDSHAKE_REQUEST_MESSAGE_BASE64 = 'AQID';
@@ -10,3 +12,9 @@ export const HANDSHAKE_REQUEST_MESSAGE_BASE64 = 'AQID';
 // Byte array of a typical handshake response from the webview.
 // Equivalent to base64 decoding 'CgIIAA=='
 export const HANDSHAKE_RESPONSE_BYTES = new Uint8Array([10, 2, 8, 0]);
+
+export function assertHTMLElement(element: Element|null|undefined):
+    asserts element is HTMLElement {
+  assertTrue(!!element);
+  assertTrue(element instanceof HTMLElement);
+}
