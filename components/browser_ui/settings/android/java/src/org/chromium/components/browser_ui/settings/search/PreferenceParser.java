6362a5c8905ec97c66db417f3e5a214b86f41583
Mohamed Adel <adelm@google.com>
2025-12-15 12:29:07 -0800

    [Settings Search] Add support for Bundle args that rely on the context
    Bug: 444470792
    Change-Id: Ie5b25185a9f497afcce1a0c7f8f01d7e99db3897
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7262826
    Commit-Queue: Moe Adel <adelm@google.com>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1558929}

--- components/browser_ui/settings/android/java/src/org/chromium/components/browser_ui/settings/search/PreferenceParser.java
+++ components/browser_ui/settings/android/java/src/org/chromium/components/browser_ui/settings/search/PreferenceParser.java
@@ -144,7 +144,7 @@ public class PreferenceParser {
             Bundle finalExtras = new Bundle();
             String childFragmentClass = bundle.getString(METADATA_FRAGMENT);
             if (childFragmentClass != null && providerMap.containsKey(childFragmentClass)) {
-                Bundle childDefaults = providerMap.get(childFragmentClass).getExtras();
+                Bundle childDefaults = providerMap.get(childFragmentClass).getExtras(context);
                 if (childDefaults != null) {
                     finalExtras.putAll(childDefaults);
                 }
