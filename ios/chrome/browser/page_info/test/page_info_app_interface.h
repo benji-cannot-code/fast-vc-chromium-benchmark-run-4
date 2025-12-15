1ca9feda4a77d47a6a1f02f26ce47bb064009501
Gauthier Ambard <gambard@chromium.org>
2025-12-15 02:59:16 -0800

    [iOS] Prepare to move page info
    To be landed before crrev.com/i/8840756.
    
    Bug: None
    Change-Id: I7a31b053428a8f6810405225132c9c591d6699b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7260701
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1558637}

---
+++ ios/chrome/browser/page_info/test/page_info_app_interface.h
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_PAGE_INFO_TEST_PAGE_INFO_APP_INTERFACE_H_
+#define IOS_CHROME_BROWSER_PAGE_INFO_TEST_PAGE_INFO_APP_INTERFACE_H_
+
+#import "ios/chrome/browser/page_info/ui_bundled/page_info_app_interface.h"
+
+#endif  // IOS_CHROME_BROWSER_PAGE_INFO_TEST_PAGE_INFO_APP_INTERFACE_H_
