729f15e159395f36a60aa75733b7ef676479387c
Renato Silva <rrsilva@google.com>
2025-12-15 11:27:13 -0800

    [ash][login] Handle stale token handle
    Handle the case when the status of the token handle is set to stale.
    This new state can be set in token_handle_store_impl.cc which is
    part of a refactoring that is currently behind a flag and running as
    an experiment. The 'old' code that handles the token handle currently
    does not handle this case and triggers an infinite crash loop.
    
    This patchset has been manually verified on a device ('eve') that had
    the token handle state artificially set to 'stale' and it did fix the
    crash loop.
    
    Bug: b:469041183
    Change-Id: I123eab021330bede940e57a30edf5c38ad189948
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7261702
    Commit-Queue: Istvan Nagy <iscsi@google.com>
    Auto-Submit: Renato Silva <rrsilva@google.com>
    Reviewed-by: Istvan Nagy <iscsi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1558872}

--- chrome/browser/ash/login/signin/token_handle_util.cc
+++ chrome/browser/ash/login/signin/token_handle_util.cc
@@ -29,6 +29,7 @@ const char kTokenHandleLastCheckedPref[] = "TokenHandleLastChecked";
 
 const char kHandleStatusValid[] = "valid";
 const char kHandleStatusInvalid[] = "invalid";
+const char kHandleStatusStale[] = "stale";
 
 constexpr int kMaxRetries = 3;
 
@@ -51,7 +52,8 @@ bool MaybeReturnCachedStatus(
     return true;
   }
 
-  if (*saved_status == kHandleStatusInvalid) {
+  if (*saved_status == kHandleStatusInvalid ||
+      *saved_status == kHandleStatusStale) {
     std::move(*callback).Run(account_id, token, /*reauth_required=*/true);
     return true;
   }
