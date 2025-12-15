bd388b043394584163016d64c6ef50592b60997f
Shakti Sahu <shaktisahu@chromium.org>
2025-12-15 14:16:15 -0800

    [contextual_tasks] Create default URLDeduplicationHelper
    This CL provides a default URL deduplication helper for contextual
    tasks.
    
    Bug: None
    Change-Id: I6c200ec38ead6aa55419f0ce8cd6c284d696a176
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7256612
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1558988}

---
+++ components/contextual_tasks/public/utils.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_CONTEXTUAL_TASKS_PUBLIC_UTILS_H_
+#define COMPONENTS_CONTEXTUAL_TASKS_PUBLIC_UTILS_H_
+
+#include <memory>
+
+namespace url_deduplication {
+class URLDeduplicationHelper;
+}  // namespace url_deduplication
+
+namespace contextual_tasks {
+
+// Creates a URLDeduplicationHelper for comparing URLs. Meant to be used for
+// showing the list of distinct URLs associated with a context.
+std::unique_ptr<url_deduplication::URLDeduplicationHelper>
+CreateURLDeduplicationHelperForContextualTask();
+
+}  // namespace contextual_tasks
+
+#endif  // COMPONENTS_CONTEXTUAL_TASKS_PUBLIC_UTILS_H_
