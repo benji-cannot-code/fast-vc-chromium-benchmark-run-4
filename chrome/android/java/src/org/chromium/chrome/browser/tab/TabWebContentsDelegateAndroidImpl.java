c11cd34dfb6f34ae8230796b441961e2b71dc8f5
Abdelrahman Eed <abdoeed@google.com>
2025-12-15 07:36:44 -0800

    [PointerLockOnAndroid] Disable Pointer lock for Android WebView
    Pointer lock is currently not supported in WebViews. However requesting
    the pointer lock was enabled by mistake, this CL moves requesting
    pointer lock to `tab_web_contents_delegate_android.cc`.
    
    Change-Id: Id8fa24a074ba52ea50b190731cf117fbd10b054c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7251415
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Abdelrahman Eed <abdoeed@google.com>
    Cr-Commit-Position: refs/heads/main@{#1558726}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/TabWebContentsDelegateAndroidImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/TabWebContentsDelegateAndroidImpl.java
@@ -280,8 +280,9 @@ final class TabWebContentsDelegateAndroidImpl extends TabWebContentsDelegateAndr
         return mDelegate.takeFocus(reverse);
     }
 
+    @CalledByNative
     @Override
-    public boolean preHandleKeyboardEvent(long nativeKeyEvent) {
+    protected boolean preHandleKeyboardEvent(long nativeKeyEvent) {
         return mDelegate.preHandleKeyboardEvent(nativeKeyEvent);
     }
 
