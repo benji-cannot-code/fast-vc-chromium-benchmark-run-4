2aff8f1d107fe39636e149c4d07df56385358b4c
Briana McClure <bmcclure@google.com>
2025-12-15 08:58:30 -0800

    [iOS] Update tab headers to correctly indicate group status
    This change updates the NTP header so that if a NTP is added to a tab
    group using one of the new Tab Group entry points, the header
    constraints will correctly update to display the Tab Group indicator.
    
    The tab group indicator is also updated so that kMove actions are
    recognized as a group update.
    
    Bug: 450580394
    Change-Id: I0f481d51c948f507e404763f302ccb7bbfdae761
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7254458
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Briana McClure <bmcclure@google.com>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1558771}

--- ios/chrome/browser/ntp/ui_bundled/new_tab_page_header_view.mm
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_header_view.mm
@@ -211,7 +211,7 @@ CGFloat MIAAnimationOpacityForScrollProgress(CGFloat percent) {
 
 @end
 
-@interface NewTabPageHeaderView ()
+@interface NewTabPageHeaderView () <TabGroupIndicatorViewDelegate>
 
 // The Lens button. May be null if Lens is not available.
 @property(nonatomic, strong, readwrite) ExtendedTouchTargetButton* lensButton;
@@ -957,6 +957,7 @@ CGFloat MIAAnimationOpacityForScrollProgress(CGFloat percent) {
   _tabGroupIndicatorView.hidden = YES;
   _tabGroupIndicatorView.translatesAutoresizingMaskIntoConstraints = NO;
   _tabGroupIndicatorView.showSeparator = YES;
+  _tabGroupIndicatorView.delegate = self;
   [self addSubview:_tabGroupIndicatorView];
 
   _toolbarTabGroupIndicartorConstraint = [_toolBarView.topAnchor
@@ -1436,6 +1437,19 @@ CGFloat MIAAnimationOpacityForScrollProgress(CGFloat percent) {
                 action:@selector(preloadVoiceSearch:)
       forControlEvents:UIControlEventTouchDown];
   [self.NTPShortcutsHandler preloadVoiceSearch];
+}
+
+#pragma mark - TabGroupIndicatorViewDelegate
+
+- (void)tabGroupIndicatorViewVisibilityUpdated:(BOOL)visible {
+  CHECK_EQ(_tabGroupIndicatorView.hidden, !visible);
+  if (visible) {
+    _toolbarTabGroupIndicartorConstraint.active = YES;
+    _toolbarNoTabGroupIndicartorConstraint.active = NO;
+  } else {
+    _toolbarTabGroupIndicartorConstraint.active = NO;
+    _toolbarNoTabGroupIndicartorConstraint.active = YES;
+  }
 }
 
 @end
--- ios/chrome/browser/toolbar/ui_bundled/tab_groups/coordinator/tab_group_indicator_mediator.mm
+++ ios/chrome/browser/toolbar/ui_bundled/tab_groups/coordinator/tab_group_indicator_mediator.mm
@@ -155,6 +155,7 @@ using tab_groups::SharingState;
   switch (change.type()) {
     case WebStateListChange::Type::kGroupVisualDataUpdate:
     case WebStateListChange::Type::kStatusOnly:
+    case WebStateListChange::Type::kMove:
       groupUpdate = YES;
       break;
     default:
