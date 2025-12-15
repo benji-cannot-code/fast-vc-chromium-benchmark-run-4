7ce41fe4f9dac1de454cc0c1434f897d205bf789
Vladimir Levin <vmpstr@chromium.org>
2025-12-15 13:29:25 -0800

    Overscroll: Ensure that the overscroll elements are DOM sorted.
    Ensures that we sort the elements by DOM order in the overscroll area
    tracker. This also adds ref counting to overscroll area targets, since
    if we have multiple commandfor references to the same element and one
    is removed, we need to know if there are still any references left. This
    was inadvertently tested by the new test.
    
    R=flackr@chromium.org
    
    Bug: 463970475
    Change-Id: I0edf5c8c97fa94d95717c0802ae695682050d049
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7256355
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1558959}

--- third_party/blink/renderer/core/dom/document.h
+++ third_party/blink/renderer/core/dom/document.h
@@ -98,6 +98,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/timer.h"
 #include "third_party/blink/renderer/platform/weborigin/kurl.h"
 #include "third_party/blink/renderer/platform/wtf/casting.h"
+#include "third_party/blink/renderer/platform/wtf/hash_counted_set.h"
 #include "third_party/blink/renderer/platform/wtf/hash_set.h"
 #include "third_party/perfetto/include/perfetto/tracing/traced_value_forward.h"
 
@@ -2293,7 +2294,7 @@ class CORE_EXPORT Document : public ContainerNode,
     }
   }
 
-  const HashSet<AtomicString>& OverscrollCommandTargets() const {
+  const HashCountedSet<AtomicString>& OverscrollCommandTargets() const {
     return overscroll_command_targets_;
   }
   void AddOverscrollCommandTarget(const AtomicString& target);
@@ -3221,12 +3222,12 @@ class CORE_EXPORT Document : public ContainerNode,
 
   bool responsive_embedded_sizing_ = false;
 
-  // A set of idrefs that have been identified by commandfor with an overscroll
+  // A map of idrefs that have been identified by commandfor with an overscroll
   // related command (e.g. toggle-overscroll). This determines a
   // :-internal-overscroll-target pseudo class. Whenever adding or removing
   // entries here, the element identified by the target needs to invalidate that
   // pseudo class.
-  HashSet<AtomicString> overscroll_command_targets_;
+  HashCountedSet<AtomicString> overscroll_command_targets_;
 
   // If you want to add new data members to blink::Document, please reconsider
   // if the members really should be in blink::Document.  document.h is a very
--- third_party/blink/renderer/core/overscroll/overscroll_area_tracker.cc
+++ third_party/blink/renderer/core/overscroll/overscroll_area_tracker.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/overscroll/overscroll_area_tracker.h"
 
 #include "third_party/blink/renderer/core/dom/element.h"
+#include "third_party/blink/renderer/core/dom/node.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 
 namespace blink {
@@ -22,7 +23,11 @@ void OverscrollAreaTracker::AddOverscroll(Element* element) {
 
 const VectorOf<Element>& OverscrollAreaTracker::DOMSortedElements() {
   if (needs_dom_sort_) {
-    // TODO(crbug.com/463970475): Implement.
+    std::sort(overscroll_members_.begin(), overscroll_members_.end(),
+              [](const Member<Element>& a, const Member<Element>& b) {
+                return a->compareDocumentPosition(b) &
+                       Node::kDocumentPositionFollowing;
+              });
     needs_dom_sort_ = false;
   }
   return overscroll_members_;
