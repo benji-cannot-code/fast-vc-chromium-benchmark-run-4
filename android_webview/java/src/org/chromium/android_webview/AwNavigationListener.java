0b67e140486f841e71eeb4b7bfc1f37eb6f1c098
Joanne de Abreu <jdeabreu@google.com>
2025-12-15 06:29:54 -0800

    WebView Web Performance Metrics API: Support lib binding -  LCP + performance mark
    Add support lib binding for the Largest Contentful Paint and performance
    mark methods in NavigationListener. Also change First Contentful Paint
    to milliseconds in line with the other methods.
    
    Bug: b:432696062
    Change-Id: I7c864c289efc0dffa887ec077d319713698c98d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7203950
    Reviewed-by: Sayed Elabady <elabadysayed@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Joanne de Abreu <jdeabreu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1558696}

--- android_webview/java/src/org/chromium/android_webview/AwNavigationListener.java
+++ android_webview/java/src/org/chromium/android_webview/AwNavigationListener.java
@@ -28,7 +28,7 @@ public interface AwNavigationListener {
 
     void onPageDOMContentLoadedEventFired(AwPage page);
 
-    void onFirstContentfulPaint(AwPage page, long loadTimeUs);
+    void onFirstContentfulPaint(AwPage page, long durationMs);
 
     void onLargestContentfulPaint(AwPage page, long durationMs);
 
