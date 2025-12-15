e4044f53915bc2769fe059decb86d171129eae9f
Michael Thiessen <mthiesse@chromium.org>
2025-12-15 15:17:01 -0800

    Fix leak when we don't get an LCP for CCT and the Tab is destroyed
    Bug: 465115033
    Change-Id: Ia91934215b010f955e9bdf8c23631a48dc343835
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7262656
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1559027}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabObserver.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabObserver.java
@@ -434,6 +434,10 @@ public class CustomTabObserver extends EmptyTabObserver {
     public void onDestroyed(Tab tab) {
         TabInteractionRecorder observer = TabInteractionRecorder.getFromTab(tab);
         if (observer != null) observer.onTabClosing();
+        if (mPageLoadMetricsObserver != null) {
+            PageLoadMetrics.removeObserver(mPageLoadMetricsObserver);
+            mPageLoadMetricsObserver = null;
+        }
     }
 
     @Override
