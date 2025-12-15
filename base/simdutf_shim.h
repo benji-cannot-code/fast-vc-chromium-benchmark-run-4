e6d8e500bf458ab8bfa7efd24f8fc64b3928bcbf
Lei Zhang <thestig@chromium.org>
2025-12-15 10:20:56 -0800

    Reland "Add the ability to use do base64 encoding in //base with simdutf"
    This is a reland of commit ec091ad0956324bab57c6271dec18f490f471259
    
    Fixed the simdutf shim to not have a circular dependency on //base by
    switching from base::span to std::span.
    
    Original change's description:
    > Add the ability to use do base64 encoding in //base with simdutf
    >
    > Add a second base64 encoding implementation that uses simdutf to
    > base::Base64EncodeAppend(), which also affects base::Base64Encode().
    > Casual performance testing shows simdutf is 30% faster than the existing
    > modp_b64 implementation. The new implementation is guarded by a feature
    > flag.
    >
    > To test the new implementation alongside the existing implementation,
    > parameterize Base64Test. Also update base64_encode_fuzzer to run the
    > fuzzer input through both implementations.
    >
    > Since some code does base64 encoding early during startup before feature
    > flag initialization, add temporary Base64EncodeEarlyStartup() and
    > Base64UrlEncodeEarlyStartup() variants that unconditionally calls the
    > modp_b64 implementation. These are intended to be temporary.
    >
    > Bug: 468035603
    > Change-Id: I5ddb8244181de77ded52b8dfbae32bd73891cece
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7254283
    > Reviewed-by: Nico Weber <thakis@chromium.org>
    > Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    > Reviewed-by: Owen Min <zmin@chromium.org>
    > Commit-Queue: Lei Zhang <thestig@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1558168}
    
    Bug: 468035603
    Change-Id: I21995a0059f34a5b615322ed9f9ec19d8dfed7b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7256303
    Reviewed-by: Owen Min <zmin@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1558827}

---
+++ base/simdutf_shim.h
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_SIMDUTF_SHIM_H_
+#define BASE_SIMDUTF_SHIM_H_
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include <span>
+
+// Shim so the //base GN target can avoid directly using simdutf.
+//
+// Do not use directly. Use APIs in base/base64.h instead.
+//
+// Deliberately uses std::span to avoid having a circular dependency on //base.
+
+namespace base::internal {
+
+[[nodiscard]] size_t simdutf_base64_length_from_binary(size_t length);
+
+[[nodiscard]] size_t simdutf_binary_to_base64(std::span<const uint8_t> input,
+                                              std::span<char> binary_output);
+
+}  // namespace base::internal
+
+#endif  // BASE_SIMDUTF_SHIM_H_
