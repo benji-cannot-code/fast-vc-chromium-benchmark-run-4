1e47078442991c2911ec48699f161758e21dbe7d
Ewann Pellé <ewannpv@chromium.org>
2025-12-15 11:36:28 -0800

    [iOS][Assistant] Add initial Assistant Sheet scaffolding
    This CL implements the initial scaffolding for the Assistant Sheet
    feature including:
    - Directory structure (coordinator, ui).
    - Basic Coordinator and ViewController and Mediator.
    - Integration with TabGridCoordinator behind kAssistantSheet flag.
    
    Bug: 469050167
    Change-Id: I3f28ce6e82e06e2c4dd5651d5bb3c05a24943806
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7262476
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1558883}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_grid_coordinator.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_grid_coordinator.mm
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/strings/grit/components_strings.h"
 #import "components/supervised_user/core/browser/supervised_user_utils.h"
 #import "ios/chrome/app/profile/first_run_profile_agent.h"
+#import "ios/chrome/browser/assistant/coordinator/assistant_sheet_coordinator.h"
 #import "ios/chrome/browser/bookmarks/model/bookmark_model_factory.h"
 #import "ios/chrome/browser/bookmarks/ui_bundled/home/bookmarks_coordinator.h"
 #import "ios/chrome/browser/bring_android_tabs/model/bring_android_tabs_to_ios_service.h"
@@ -285,6 +286,8 @@ bool FindNavigatorShouldBePresentedInBrowser(Browser* browser) {
   ProceduralBlock _guidedTourCompletionBlock;
   // App bar for the prototype.
   ChromeAppBarPrototype* _appBar;
+  // Coordinator for the Assistant Sheet.
+  AssistantSheetCoordinator* _assistantSheetCoordinator;
 }
 // Superclass property.
 @synthesize baseViewController = _baseViewController;
@@ -381,6 +384,8 @@ bool FindNavigatorShouldBePresentedInBrowser(Browser* browser) {
   [_incognitoGridCoordinator stopChildCoordinators];
   [_regularGridCoordinator stopChildCoordinators];
   [_tabGroupsPanelCoordinator stopChildCoordinators];
+  [_assistantSheetCoordinator stop];
+  _assistantSheetCoordinator = nil;
 
   [self cancelCollaborationFlows];
 
@@ -927,6 +932,15 @@ bool FindNavigatorShouldBePresentedInBrowser(Browser* browser) {
 
 - (void)prototypeGeminiCallback {
   CHECK(IsDiamondPrototypeEnabled());
+
+  if (IsAssistantSheetEnabled()) {
+    _assistantSheetCoordinator = [[AssistantSheetCoordinator alloc]
+        initWithBaseViewController:self.baseViewController
+                           browser:self.regularBrowser];
+    [_assistantSheetCoordinator start];
+    return;
+  }
+
   TabGridPage page = self.baseViewController.currentPage;
   if (page == TabGridPageTabGroups) {
     page = self.baseViewController.activePage;
