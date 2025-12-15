a0b150bef6c2677f7ccfc4a10089ae71d9b07525
William Liu <liuwilliam@chromium.org>
2025-12-15 08:14:51 -0800

    Enable some tests on ChromeOS
    Actuation feature is launching on ChromeOS in M144. For that we need to
    extend the test coverage on ChromeOs as much as we can. However signing
    out from the primary account is not supported on ChromeOS thus those
    portions of tests are still platform-disabled.
    
    Cq-Include-Trybots: luci.chromium.try:chromeos-amd64-generic-rel,linux-chromeos-rel
    Bug: 468339363, 465208148
    Change-Id: I3c5bf6379d3d72c1a13670122fae06f6efb02b18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7255898
    Commit-Queue: William Liu <liuwilliam@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1558744}

--- chrome/browser/actor/actor_policy_checker_browsertest.cc
+++ chrome/browser/actor/actor_policy_checker_browsertest.cc
@@ -55,10 +55,8 @@ struct TestAccount {
 };
 
 constexpr TestAccount kNonEnterpriseAccount = {"foo@testbar.com", ""};
-#if !BUILDFLAG(IS_CHROMEOS)
 constexpr TestAccount kEnterpriseAccount = {"foo@testenterprise.com",
                                             "testenterprise.com"};
-#endif  // !BUILDFLAG(IS_CHROMEOS)
 }  // namespace
 
 class ActorPolicyCheckerBrowserTestBase : public ActorToolsTest {
@@ -114,7 +112,7 @@ class ActorPolicyCheckerBrowserTestBase : public ActorToolsTest {
     identity_test_env_->SetAutomaticIssueOfAccessTokens(true);
 
     AccountInfo account_info = identity_test_env_->MakePrimaryAccountAvailable(
-        std::string(account->email), signin::ConsentLevel::kSync);
+        std::string(account->email), signin::ConsentLevel::kSignin);
 
     AccountCapabilitiesTestMutator mutator(&account_info.capabilities);
     mutator.set_can_use_model_execution_features(true);
@@ -447,8 +445,6 @@ class ActorPolicyCheckerBrowserTestWithManagedAccount
   base::test::ScopedFeatureList scoped_feature_list_;
 };
 
-// Note: sign-out from enterprise account is not allowed in ChromeOS.
-#if !BUILDFLAG(IS_CHROMEOS)
 IN_PROC_BROWSER_TEST_F(ActorPolicyCheckerBrowserTestWithManagedAccount,
                        CapabilityUpdatedForAccount) {
   // No account is signed in, thus no capability.
@@ -463,6 +459,8 @@ IN_PROC_BROWSER_TEST_F(ActorPolicyCheckerBrowserTestWithManagedAccount,
                    ->GetPolicyChecker()
                    .can_act_on_web());
 
+// Note: sign-out from enterprise account is not allowed in ChromeOS.
+#if !BUILDFLAG(IS_CHROMEOS)
   ClearPrimaryAccount();
   EXPECT_FALSE(ActorKeyedService::Get(browser()->profile())
                    ->GetPolicyChecker()
@@ -473,8 +471,8 @@ IN_PROC_BROWSER_TEST_F(ActorPolicyCheckerBrowserTestWithManagedAccount,
   EXPECT_TRUE(ActorKeyedService::Get(browser()->profile())
                   ->GetPolicyChecker()
                   .can_act_on_web());
-}
 #endif  // !BUILDFLAG(IS_CHROMEOS)
+}
 
 IN_PROC_BROWSER_TEST_F(ActorPolicyCheckerBrowserTestWithManagedAccount,
                        GlicUserStatusChanged) {
@@ -537,8 +535,6 @@ IN_PROC_BROWSER_TEST_F(ActorPolicyCheckerBrowserTestWithManagedAccount,
 using ActorPolicyCheckerBrowserTestWithManagedAccountWithPolicy =
     ActorPolicyCheckerBrowserTestManagedBrowser;
 
-// Note: sign-out from enterprise account is not allowed in ChromeOS.
-#if !BUILDFLAG(IS_CHROMEOS)
 IN_PROC_BROWSER_TEST_F(
     ActorPolicyCheckerBrowserTestWithManagedAccountWithPolicy,
     CapabilityUpdatedForAccount) {
@@ -548,6 +544,8 @@ IN_PROC_BROWSER_TEST_F(
                    ->GetPolicyChecker()
                    .can_act_on_web());
 
+// Note: sign-out from enterprise account is not allowed in ChromeOS.
+#if !BUILDFLAG(IS_CHROMEOS)
   ClearPrimaryAccount();
   // No capability because the policy is disabled.
   SimulatePrimaryAccountChangedSignIn(&kNonEnterpriseAccount);
@@ -560,7 +558,7 @@ IN_PROC_BROWSER_TEST_F(
   EXPECT_TRUE(ActorKeyedService::Get(browser()->profile())
                   ->GetPolicyChecker()
                   .can_act_on_web());
-}
 #endif  // !BUILDFLAG(IS_CHROMEOS)
+}
 
 }  // namespace actor
