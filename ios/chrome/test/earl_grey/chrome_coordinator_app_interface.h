68d5f1fcb05290b7e14d5ef484663f29e669f9c8
Łukasz Anforowicz <lukasza@chromium.org>
2025-12-15 13:46:55 -0800

    Revert "[iOS][fix-it] Migrate bookmarks_egtest.mm tests to isolated UI tests"
    This reverts commit 8170cdb45a080a9ae2c120551d1994df14e77971.
    
    Reason for revert: Suspected for causing gardening failures: 1) https://ci.chromium.org/ui/b/8695396770887576289/test-results?q=ID%3A%3A%2F%2Fios%2Fchrome%2Ftest%2Fearl_grey2%5C%3Aios_chrome_bookmarks_eg2tests_module!xctest%3A%3ABookmarksTestCase%23testCachePositionIsRecreated and https://ci.chromium.org/ui/b/8695389556205734129/test-results?q=ID%3A%3A%2F%2Fios%2Fchrome%2Ftest%2Fearl_grey2%5C%3Aios_chrome_bookmarks_eg2tests_module!xctest%3A%3ABookmarksTestCase%23testCachePositionIsRecreated
    
    Original change's description:
    > [iOS][fix-it] Migrate bookmarks_egtest.mm tests to isolated UI tests
    >
    > This CL does the following
    >
    > 1. Add bookmark coordinator starter in ChromeCoordinatorAppInterface
    > to be used in isolated UI tests
    > 2. Replace `[BookmarkEarlGreyUI openBookmarks]` by
    > `[ChromeCoordinatorAppInterface reset]` so the tests could run with
    > minimal UI
    > 3. For the tests that can't be easily isolated, move them to
    > BookmarksInteractionTestCase
    >
    > Fixed: 467297749
    > Change-Id: Ie72812c8483d779181107a7cdd643b95fd54aee4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7262214
    > Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    > Reviewed-by: Scott Yoder <scottyoder@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1558799}
    
    Bug: 467297749
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I57e43e0671b300e24a45a3e256ac873376c1453a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7263445
    Owners-Override: Łukasz Anforowicz <lukasza@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1558967}

--- ios/chrome/test/earl_grey/chrome_coordinator_app_interface.h
+++ ios/chrome/test/earl_grey/chrome_coordinator_app_interface.h
@@ -52,7 +52,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 + (void)startSearchWhatYouSeePromoCoordinator;
 + (void)startSnackbarCoordinator;
 + (void)startReadingListCoordinator;
-+ (void)startBookmarksCoordinator;
 
 // Stops the currently started coordinator.
 + (void)stopCoordinator;
--- ios/chrome/test/earl_grey/chrome_coordinator_app_interface.mm
+++ ios/chrome/test/earl_grey/chrome_coordinator_app_interface.mm
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/language/ios/browser/ios_language_detection_tab_helper.h"
 #import "components/prefs/pref_service.h"
 #import "ios/chrome/browser/autocomplete/model/autocomplete_browser_agent.h"
-#import "ios/chrome/browser/bookmarks/ui_bundled/home/bookmarks_coordinator.h"
 #import "ios/chrome/browser/browser_view/model/browser_view_visibility_notifier_browser_agent.h"
 #import "ios/chrome/browser/discover_feed/model/discover_feed_visibility_browser_agent.h"
 #import "ios/chrome/browser/history/ui_bundled/history_coordinator.h"
@@ -353,15 +352,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       initWithBaseViewController:[self rootViewController]
                          browser:self.helper.browser];
   [self.helper.coordinator start];
-}
-
-+ (void)startBookmarksCoordinator {
-  BookmarksCoordinator* coordinator =
-      [[BookmarksCoordinator alloc] initWithBrowser:self.helper.browser];
-  coordinator.baseViewController = [self rootViewController];
-  self.helper.coordinator = coordinator;
-  [self.helper.coordinator start];
-  [coordinator presentBookmarks];
 }
 
 #pragma mark - Private
