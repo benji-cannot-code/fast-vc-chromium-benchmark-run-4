c6c85299d03276235ca3c5f0d33f737efd953537
Colin Blundell <blundell@chromium.org>
2025-12-15 01:03:23 -0800

    [//media] Update check in GpuMemoryBufferVideoFrameMapper
    This is effectively the same check [1], but it is more easily readable
    as to the intent. Note in particular that the storage type will never
    be MAPPABLE_SHARED_IMAGE for NativePixmapFrameResource [2].
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:media/base/video_frame.cc;l=1174-1176?q=VideoFrame::HasM&ss=chromium
    [2] https://source.chromium.org/chromium/chromium/src/+/main:media/gpu/chromeos/native_pixmap_frame_resource.cc;l=320-324;drc=6fe04ac5cd13eaa7013d9da4b94144b4caa1fabf;bpv=1;bpt=1
    
    Bug: 404905709
    Change-Id: I05e25e5fab3de95df9f6c20ace23a61e6fb520bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7253293
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1558601}

--- media/gpu/chromeos/video_frame_resource.cc
+++ media/gpu/chromeos/video_frame_resource.cc
@@ -83,6 +83,10 @@ gfx::GpuMemoryBufferHandle VideoFrameResource::CreateGpuMemoryBufferHandle()
   return media::CreateGpuMemoryBufferHandle(frame_.get());
 }
 
+bool VideoFrameResource::HasMappableSharedImage() const {
+  return frame_->HasMappableSharedImage();
+}
+
 scoped_refptr<gpu::ClientSharedImage> VideoFrameResource::GetSharedImage()
     const {
   return frame_->shared_image();
