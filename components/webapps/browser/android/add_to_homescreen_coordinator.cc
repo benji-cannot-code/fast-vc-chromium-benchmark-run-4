ff927ab411d93eb8c87a3c526b78d3fa48cdfd6c
Momoko Hattori <momohatt@google.com>
2025-12-15 09:16:10 -0800

    Add TwaInstaller class to wrap the API call to WebApp service
    This CL adds a new TwaInstaller class to call the new API of the new
    WebApp service, and call its method from AddToHomescreenCoordinator.
    
    Bug: 449581904
    Change-Id: I0bcb01f801df5381d8f0ece3b1bcc0dcd058a7ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7206794
    Reviewed-by: Yi Xie <yixie@chromium.org>
    Commit-Queue: Momoko Hattori <momohatt@chromium.org>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Reviewed-by: Glenn Hartmann <hartmanng@chromium.org>
    Reviewed-by: Shengsong Tan <sstan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1558783}

--- components/webapps/browser/android/add_to_homescreen_coordinator.cc
+++ components/webapps/browser/android/add_to_homescreen_coordinator.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/webapps/browser/android/add_to_homescreen_mediator.h"
 #include "components/webapps/browser/android/add_to_homescreen_params.h"
 #include "components/webapps/browser/android/shortcut_info.h"
+#include "components/webapps/browser/android/twa_installer.h"
 #include "components/webapps/browser/banners/app_banner_manager.h"
 #include "components/webapps/browser/banners/app_banner_settings_helper.h"
 #include "content/public/browser/web_contents.h"
@@ -90,7 +91,10 @@ void AddToHomescreenCoordinator::OnDataAvailable(
   if (app_type == AddToHomescreenParams::AppType::TWA) {
     CHECK(!mediator_);
 
-    // TODO(crbug.com/449581904): Start TWA install flow from here.
+    TwaInstaller::Install(
+        std::move(params),
+        base::BindRepeating(&AddToHomescreenCoordinator::RecordEventForAppMenu,
+                            data_fetcher_->web_contents()));
 
     JNIEnv* env = base::android::AttachCurrentThread();
     Java_AddToHomescreenCoordinator_onFlowCompleted(env, java_coordinator_);
@@ -122,8 +126,7 @@ bool AddToHomescreenCoordinator::ShowForAppBanner(
   }
 
   if (params->app_type == AddToHomescreenParams::AppType::TWA) {
-    // TODO(crbug.com/449581904): Start TWA install flow from here.
-    return true;
+    return TwaInstaller::Install(std::move(params), std::move(event_callback));
   }
 
   JNIEnv* env = base::android::AttachCurrentThread();
--- components/webapps/browser/android/add_to_homescreen_installer.h
+++ components/webapps/browser/android/add_to_homescreen_installer.h
@@ -15,6 +15,7 @@ class WebContents;
 
 namespace webapps {
 
+// GENERATED_JAVA_ENUM_PACKAGE: org.chromium.components.webapps
 enum class AddToHomescreenEvent {
   INSTALL_STARTED,
   INSTALL_FAILED,
