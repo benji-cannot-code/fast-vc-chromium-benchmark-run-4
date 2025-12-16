3706af5f23e6ece4e1c90bac8df2a06aca4c7ca6
Sourav <sinhasourav@google.com>
2025-12-16 14:45:10 -0800

    [iOSFidoImportExport] Update cell UI in credential export view
    This CL adds a single line separator between table view cells and adds a
    light blue colour as the background for cells when they are selected.
    
    Screenshot: https://screenshot.googleplex.com/4s4NnQHirMbsT5m
    Bug: 469432375
    Change-Id: Iaa1784920b53915d5e003213387351c98c20d1b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7265296
    Commit-Queue: Sourav Uttam Sinha <sinhasourav@google.com>
    Reviewed-by: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1559599}

--- ios/chrome/browser/credential_exchange/ui/credential_export_view_controller.mm
+++ ios/chrome/browser/credential_exchange/ui/credential_export_view_controller.mm
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/ui/table_view/content_configuration/favicon_content_configuration.h"
 #import "ios/chrome/browser/shared/ui/table_view/content_configuration/table_view_cell_content_configuration.h"
 #import "ios/chrome/browser/shared/ui/table_view/table_view_utils.h"
+#import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/favicon/favicon_attributes.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ui/base/l10n/l10n_util.h"
@@ -55,6 +56,7 @@ NSString* const kCredentialSectionIdentifier = @"CredentialSection";
   [super viewDidLoad];
 
   self.title = l10n_util::GetNSString(IDS_IOS_EXPORT_PASSWORDS_AND_PASSKEYS);
+  self.tableView.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
 
   self.navigationItem.rightBarButtonItem = [self createContinueButton];
   _toggleAllButton = [self createToggleAllButton];
@@ -380,6 +382,10 @@ NSString* const kCredentialSectionIdentifier = @"CredentialSection";
   cell.contentConfiguration = contentConfig;
   cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
   cell.accessibilityTraits |= UIAccessibilityTraitButton;
+
+  UIView* selectedBackgroundView = [[UIView alloc] init];
+  selectedBackgroundView.backgroundColor = [UIColor colorNamed:kBlueHaloColor];
+  cell.selectedBackgroundView = selectedBackgroundView;
 }
 
 // Helper to load favicon and update configuration.
