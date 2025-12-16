1f769fbc3ecf29e3afbc49356434369f3762f6ca
Duncan Mercer <mercerd@google.com>
2025-12-15 16:25:50 -0800

    Move invocation source to components/lens
    This change will allow us to reuse this method for the Contextual Search
    query controller which currently does not add a `source=` param. This is
    a refactor and should be a noop.
    
    Bug: b:467397501
    Change-Id: Iaabdb3924c1a26d37af9622471923de9bb92d5b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7261786
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1559060}

--- chrome/browser/ui/lens/lens_overlay_gen204_controller.cc
+++ chrome/browser/ui/lens/lens_overlay_gen204_controller.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/lens/lens_overlay_url_builder.h"
 #include "components/base32/base32.h"
 #include "components/lens/lens_features.h"
+#include "components/lens/lens_url_utils.h"
 #include "components/metrics_services_manager/metrics_services_manager.h"
 #include "components/search_engines/template_url_service.h"
 #include "net/base/url_util.h"
--- components/lens/lens_url_utils.h
+++ components/lens/lens_url_utils.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/lens/lens_entrypoints.h"
 #include "components/lens/lens_metadata.mojom.h"
+#include "components/lens/lens_overlay_invocation_source.h"
 #include "components/lens/lens_overlay_mime_type.h"
 #include "third_party/lens_server_proto/lens_overlay_request_id.pb.h"
 
@@ -113,6 +114,11 @@ GURL AppendDarkModeParamToURL(const GURL& url_to_modify, bool use_dark_mode);
 // Remove parameters that cause the SRP to be rendered for the side panel. Used
 // when opening the SRP in a new tab.
 GURL RemoveSidePanelURLParameters(const GURL& url);
+
+// Appends the invocation source parameter to the URL.
+GURL AppendInvocationSourceParamToURL(
+    const GURL& url_to_modify,
+    lens::LensOverlayInvocationSource invocation_source);
 
 }  // namespace lens
 
