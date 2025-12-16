f948113ece8c627ef0c7554fc2f3625e518a5f9c
Christian Biesinger <cbiesinger@chromium.org>
2025-12-16 10:05:55 -0800

    [FedCM] Remove flags for shipped features
    This removes these three flags:
    kFedCmAlternativeIdentifiers
    kFedCmIframeOrigin
    kFedCmUseOtherAccountAndLabelsNewSyntax
    
    Fixed: 465392139, 465390917
    Change-Id: I2daf22358d6debd6c82f646a5a422c1dd1e59f74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7254603
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1559420}

--- content/browser/webid/flags.cc
+++ content/browser/webid/flags.cc
@@ -55,15 +55,6 @@ bool IsLightweightModeEnabled() {
   return base::FeatureList::IsEnabled(features::kFedCmLightweightMode);
 }
 
-bool IsAlternativeIdentifiersEnabled() {
-  return base::FeatureList::IsEnabled(features::kFedCmAlternativeIdentifiers);
-}
-
-bool IsUseOtherAccountAndLabelsNewSyntaxEnabled() {
-  return base::FeatureList::IsEnabled(
-      features::kFedCmUseOtherAccountAndLabelsNewSyntax);
-}
-
 bool IsFedCmEmbedderCheckEnabled() {
   return base::FeatureList::IsEnabled(features::kFedCmEmbedderCheck);
 }
@@ -74,10 +65,6 @@ bool IsAutofillEnabled() {
   // if the old one is enabled.
   return base::FeatureList::IsEnabled(features::kFedCmAutofill) ||
          IsDelegationEnabled();
-}
-
-bool IsIframeOriginEnabled() {
-  return base::FeatureList::IsEnabled(features::kFedCmIframeOrigin);
 }
 
 bool IsNonceInParamsEnabled() {
