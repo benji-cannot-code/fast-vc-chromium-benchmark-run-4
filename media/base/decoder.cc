ec68cf6cfb69a1a2c8bcb1621866f983714fc16a
Etienne Pierre-doray <etiennep@chromium.org>
2025-12-16 12:27:24 -0800

    [tracing] Migrate legacy async macros
    These are remaining edge case macros. Most macros were mechanically
    migrated in
    https://chromium-review.googlesource.com/c/chromium/src/+/6845697
    https://chromium-review.googlesource.com/c/chromium/src/+/6846023
    
    TRACE_EVENT_ASYNC_STEP_INTO/TRACE_EVENT_ASYNC_STEP_PAST don't have
    a mechanical counterpart, so I migrated case case by case.
    
    Bug: 432427382
    Change-Id: Iddf479fee8c7e099460111834175fa5d3404a624
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7256715
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1559510}

--- media/base/decoder.cc
+++ media/base/decoder.cc
@@ -21,7 +21,7 @@ bool Decoder::SupportsDecryption() const {
   return false;
 }
 
-std::string GetDecoderName(VideoDecoderType type) {
+const char* GetDecoderName(VideoDecoderType type) {
   switch (type) {
     case VideoDecoderType::kUnknown:
       return "Unknown Video Decoder";
@@ -60,7 +60,7 @@ std::string GetDecoderName(VideoDecoderType type) {
   }
 }
 
-std::string GetDecoderName(AudioDecoderType type) {
+const char* GetDecoderName(AudioDecoderType type) {
   switch (type) {
     case AudioDecoderType::kUnknown:
       return "Unknown Audio Decoder";
--- third_party/blink/renderer/platform/peerconnection/rtc_video_decoder_adapter.cc
+++ third_party/blink/renderer/platform/peerconnection/rtc_video_decoder_adapter.cc
@@ -641,7 +641,8 @@ bool RTCVideoDecoderAdapter::InitializeSync(
   }
 
   decoder_info_.implementation_name =
-      "ExternalDecoder (" + media::GetDecoderName(decoder_type_) + ")";
+      "ExternalDecoder (" + std::string(media::GetDecoderName(decoder_type_)) +
+      ")";
   return async_init_result_;
 }
 
