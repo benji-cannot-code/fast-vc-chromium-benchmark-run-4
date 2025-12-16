7d01defb209ef77237221d0e6d76f6127bf4e359
Keita Suzuki <suzukikeita@chromium.org>
2025-12-15 17:03:05 -0800

    Fix DSE prewarm metrics for RenderProcess reuse
    The current DSE prewarm metrics can be inaccurate in scenarios involving
    RenderProcessHost reuse. The prewarm status was tracked on the
    NavigationHandle, which failed to correctly attribute prewarming when a
    prewarmed process was reused for a subsequent, different navigation.
    This resulted in undercounting prewarmed navigations.
    
    This change moves the responsibility of storing the prewarm status to
    the RenderProcessHost itself. When a prewarmed prerender navigation
    commits, the status is attached directly to the process. The page load
    metrics observer can then reliably check the process to determine if the
    navigation occurred in a prewarmed renderer, regardless of process
    reuse.
    
    To facilitate this, PrerenderPrewarmNavigationData is moved from
    //components/page_load_metrics to the content layer in
    //content/public/browser.
    
    Details (internal doc):
    https://docs.google.com/document/d/1N9u6oE5EjJPyuDINnMKTWxNpNiSF6u71B5aTGmEgVAo/edit?tab=t.0
    
    NO_IFTTT=Moving the target file to a new directory
    
    Bug: 454263659
    Change-Id: Ifa65203507afb4f4a5de93f4f386e728346cf995
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7116488
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Keita Suzuki <suzukikeita@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1559078}

--- chrome/browser/preloading/prerender/prerender_manager.h
+++ chrome/browser/preloading/prerender/prerender_manager.h
@@ -61,8 +61,6 @@ class PrerenderManager : public content::WebContentsObserver,
   ~PrerenderManager() override;
 
   // content::WebContentsObserver
-  void DidStartNavigation(
-      content::NavigationHandle* navigation_handle) override;
   void DidFinishNavigation(
       content::NavigationHandle* navigation_handle) override;
 
@@ -144,7 +142,7 @@ class PrerenderManager : public content::WebContentsObserver,
   // Otherwise, returns kReady and sets `prewarm_url`.
   PrewarmDecision ShouldPrewarm(GURL& prewarm_url);
 
-  void OnPrerenderNavigationHandle(
+  void OnSearchPrewarmPrerenderNavigationHandle(
       content::NavigationHandle& navigation_handle);
 
   std::unique_ptr<content::PrerenderHandle> search_prewarm_handle_;
