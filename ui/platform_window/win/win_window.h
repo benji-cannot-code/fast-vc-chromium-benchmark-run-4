b040605551ae59581b37ada3b32615d3dbcb38e9
yuyisi <hellofroser@gmail.com>
2025-12-16 07:55:19 -0800

    Refine IME message handling in `WinWindow`
    An `InputMethod` pointer can be set for `ui::WinWindow` to handle IME
    window messages (including `WM_INPUTLANGCHANGE`). If no `InputMethod`
    pointer is set, the window will handle IME messages according to the
    default behavior by calling `DefWindowProc()`. The `InputMethod` pointer
    set in this way will automatically manage its own lifecycle, that is, it
    will set itself to nullptr after being released.
    
    not involve modifications to the core logic.
    
    Low-Coverage-Reason: TRIVIAL_CHANGE This change is very minor and does
    Bug: 466796611
    Change-Id: Ib094b978d407df982acfa81917b795423d21049d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7250549
    Reviewed-by: Jesse McKenna <jessemckenna@google.com>
    Reviewed-by: David Bienvenu <davidbienvenu@chromium.org>
    Auto-Submit: YISI YU <hellofroser@gmail.com>
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1559349}

--- ui/platform_window/win/win_window.h
+++ ui/platform_window/win/win_window.h
@@ -31,6 +31,10 @@ class WIN_WINDOW_EXPORT WinWindow : public PlatformWindow,
 
   ~WinWindow() override;
 
+  // Set an `input_method` object to handle IME-related window messages. If it
+  // is not set, the window will use the default handler function to process
+  // these messages.
+  // When `input_method` is destroyed, it will be automatically set to nullptr.
   void SetInputMethod(InputMethod* input_method);
 
  private:
@@ -109,6 +113,7 @@ class WIN_WINDOW_EXPORT WinWindow : public PlatformWindow,
     CR_MESSAGE_HANDLER_EX(WM_SYSCHAR, OnImeMessages)
     CR_MESSAGE_HANDLER_EX(WM_IME_CHAR, OnImeMessages)
     CR_MESSAGE_HANDLER_EX(WM_NCACTIVATE, OnNCActivate)
+    CR_MSG_WM_INPUTLANGCHANGE(OnInputLangChange)
 
     CR_MSG_WM_CLOSE(OnClose)
     CR_MSG_WM_CREATE(OnCreate)
@@ -120,6 +125,7 @@ class WIN_WINDOW_EXPORT WinWindow : public PlatformWindow,
   LRESULT OnMouseRange(UINT message, WPARAM w_param, LPARAM l_param);
   LRESULT OnCaptureChanged(UINT message, WPARAM w_param, LPARAM l_param);
   LRESULT OnKeyEvent(UINT message, WPARAM w_param, LPARAM l_param);
+  void OnInputLangChange(DWORD character_set, HKL input_language_id);
   LRESULT OnImeMessages(UINT message, WPARAM w_param, LPARAM l_param);
   LRESULT OnNCActivate(UINT message, WPARAM w_param, LPARAM l_param);
   void OnClose();
