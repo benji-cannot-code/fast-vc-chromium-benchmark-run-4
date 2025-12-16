04780bbb77d5c6dcd0ef311e8eb31991c89726e5
Tomasz Wiszkowski <ender@google.com>
2025-12-16 13:49:22 -0800

    Scroll Fusebox Attachments to reveal newly added items.
    Change-Id: I20736d775d8168830c7b59ec8a86ad0c65f0e591
    Fixed: 466141277
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7262112
    Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1559565}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxAttachmentRecyclerView.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxAttachmentRecyclerView.java
@@ -5,15 +5,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.omnibox.fusebox;
 
+import static org.chromium.build.NullUtil.assumeNonNull;
+
 import android.content.Context;
 import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 
+import androidx.annotation.VisibleForTesting;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.browser.omnibox.R;
 
 /** A RecyclerView for the FuseboxAttachment component. */
@@ -37,9 +41,41 @@ public class FuseboxAttachmentRecyclerView extends RecyclerView {
         }
     }
 
+    @VisibleForTesting
+    /* package */ static class ScrollToEndOnInsertionObserver extends AdapterDataObserver {
+        private final RecyclerView mView;
+
+        @VisibleForTesting
+        /* package */ ScrollToEndOnInsertionObserver(RecyclerView view) {
+            mView = view;
+        }
+
+        @Override
+        public void onItemRangeInserted(int startPosition, int itemCount) {
+            super.onItemRangeInserted(startPosition, itemCount);
+            int totalItemCount = assumeNonNull(mView.getAdapter()).getItemCount();
+            mView.scrollToPosition(totalItemCount - 1);
+        }
+    }
+
+    private final ScrollToEndOnInsertionObserver mScrollToEndOnInsertion;
+
     public FuseboxAttachmentRecyclerView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mScrollToEndOnInsertion = new ScrollToEndOnInsertionObserver(this);
         setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false));
         addItemDecoration(new SpacingItemDecoration(context));
+    }
+
+    @Override
+    public void setAdapter(@Nullable Adapter newAdapter) {
+        var oldAdapter = getAdapter();
+        if (oldAdapter != null) {
+            oldAdapter.unregisterAdapterDataObserver(mScrollToEndOnInsertion);
+        }
+        super.setAdapter(newAdapter);
+        if (newAdapter != null) {
+            newAdapter.registerAdapterDataObserver(mScrollToEndOnInsertion);
+        }
     }
 }
