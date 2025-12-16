33e3e82ecfe500adcac6bffdf911f43bf811599d
Andrey Kosyakov <caseq@chromium.org>
2025-12-16 14:19:23 -0800

    Deflake ...inspector-protocol/target/auto-attach-wait-for-debugger-2-sessions-worker.js
    We used to rely on a round-trip eval following
    Runtime.runIfWaitingForDebugger, which, unfortunately, is not currently
    proper sequenced with the worker script loading, so await the latter
    explicitly with recently introduced Inspector.workerScriptLoaded.
    
    Change-Id: If7214afff224bdfcd0549a7f214bad370c002985
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7262412
    Reviewed-by: Peter Kvitek <kvitekp@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Auto-Submit: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1559581}

--- third_party/blink/web_tests/http/tests/inspector-protocol/target/auto-attach-wait-for-debugger-2-sessions-worker.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/target/auto-attach-wait-for-debugger-2-sessions-worker.js
@@ -39,7 +39,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await wp1.Runtime.runIfWaitingForDebugger();
   testRunner.log(`Session 2 resumed!`);
 
-  await worker_session1.evaluate('');
+  await Promise.all([
+    wp1.Inspector.onceWorkerScriptLoaded(),
+    wp2.Inspector.onceWorkerScriptLoaded(),
+  ]);
 
   testRunner.completeTest();
 })
