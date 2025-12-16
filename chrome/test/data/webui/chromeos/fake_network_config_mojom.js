33f7ecdc176695bc37a06249bac2353d26d7e3f4
Chad Duffin <chadduffin@google.com>
2025-12-16 14:13:35 -0800

    Fix add/remove preferred network bug
    This change fixes a bug that prevented adding and removing a network to
    the preferred network list. This bug was introduced by
    https://chromium-review.googlesource.com/c/chromium/src/+/7180403 which
    changed an API that this logic used to expect a managed properties
    object instead of a network type.
    
    Bug: 466319348
    Test: Added unit test and confirmed fix on device.
    Change-Id: Ia0f586b67185b34d7aab09951787ed2f7221446e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7256445
    Reviewed-by: Jason Zhang <jiajunz@google.com>
    Commit-Queue: Chad Duffin <chadduffin@google.com>
    Reviewed-by: Nikhil Nayunigari <nikhilcn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1559578}

--- chrome/test/data/webui/chromeos/fake_network_config_mojom.js
+++ chrome/test/data/webui/chromeos/fake_network_config_mojom.js
@@ -108,6 +108,9 @@ export class FakeNetworkConfig {
     /** @private {!number} */
     this.apnIdCounter_ = 0;
 
+    /** @type {!Array<!{guid: string, properties: ConfigProperties}>} */
+    this.setPropertiesCalls = [];
+
     this.resetForTest();
   }
 
@@ -160,6 +163,7 @@ export class FakeNetworkConfig {
 
     this.serverCas_ = [];
     this.userCerts_ = [];
+    this.setPropertiesCalls = [];
 
     ['getNetworkState', 'getNetworkStateList', 'getDeviceStateList',
      'getManagedProperties', 'setNetworkTypeEnabledState', 'requestNetworkScan',
@@ -391,6 +395,7 @@ export class FakeNetworkConfig {
    */
   setProperties(guid, properties) {
     return new Promise(resolve => {
+      this.setPropertiesCalls.push({guid, properties});
       this.propertiesToSet_ =
           /** @type(!ConfigProperties)*/ (Object.assign({}, properties));
       this.methodCalled('setProperties');
