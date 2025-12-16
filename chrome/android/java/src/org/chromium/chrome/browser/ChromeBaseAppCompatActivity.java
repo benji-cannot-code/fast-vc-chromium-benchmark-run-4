bf31dd24e5718e4b8d2181a27e343ac42543386d
Menghan Yang <myuu@google.com>
2025-12-16 02:40:21 -0800

    [Android][Signin] Introduce ActivityResultTracker
    Previously, to start new activities, IntentRequestTracker was generally
    used, but the activity result callback is discarded if the base activity
    get killed when the new activity is in the foreground.
    
    It was an issue for the add account flow epecially, since 1% of sign-ins
    flows encounter the base activity restoration when the add account
    activity is shown.
    
    The previous solution was to separate the sign-in flow handling in a
    separate activity (SigninAndHistorySyncActivity) that handles the
    activity result via onActivityResult, to ensure that we don't break the
    sign-in flow for those 1% of users. But it's not ideal since an activity
    is heavy and communication with the embedder activity is difficult.
    
    Given the above: This CL is a prototype for a general solution to keep
    the activity result when the base activity is recreated in the
    background. Once the design is approved, it will be experimented with
    the Seamless Sign-in project before being used more widely.
    
    The sign-in API will be updated in following CLs
    
    See https://crbug.com/437039516#comment2 for the preview.
    
    Bug: 437039516
    Change-Id: I5839a56025e451b8784f00d207b3490727563b1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6999110
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Menghan Yang <myuu@google.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1559224}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeBaseAppCompatActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeBaseAppCompatActivity.java
@@ -70,6 +70,8 @@ import org.chromium.chrome.browser.ui.edge_to_edge.EdgeToEdgeUtils;
 import org.chromium.chrome.browser.ui.edge_to_edge.SimpleEdgeToEdgeController;
 import org.chromium.components.browser_ui.styles.SemanticColorUtils;
 import org.chromium.components.browser_ui.util.AutomotiveUtils;
+import org.chromium.ui.base.ActivityResultTracker;
+import org.chromium.ui.base.ActivityResultTrackerImpl;
 import org.chromium.ui.base.DeviceFormFactor;
 import org.chromium.ui.base.ImmutableWeakReference;
 import org.chromium.ui.base.UiAndroidFeatureList;
@@ -132,6 +134,10 @@ public class ChromeBaseAppCompatActivity extends AppCompatActivity
     // TODO(crbug.com/435269657): Update this and the ChromeActivity equivalent to OneShotSupplier
     protected final ObservableSupplierImpl<EdgeToEdgeController> mEdgeToEdgeControllerSupplier =
             new ObservableSupplierImpl<>();
+    // Manages activity results for this activity.
+    private final ActivityResultTrackerImpl mActivityResultTracker =
+            new ActivityResultTrackerImpl(
+                    new ActivityResultTrackerImpl.RegistryImpl(getActivityResultRegistry()));
 
     private NightModeStateProvider mNightModeStateProvider;
     private InsetObserver mInsetObserver;
@@ -216,6 +222,8 @@ public class ChromeBaseAppCompatActivity extends AppCompatActivity
 
         initializeNightModeStateProvider();
         mNightModeStateProvider.addObserver(this);
+
+        mActivityResultTracker.onRestoreInstanceState(savedInstanceState);
 
         // onCreate may initialize some views, need to apply themes before that can happen.
         applyThemeOverlays();
@@ -325,6 +333,7 @@ public class ChromeBaseAppCompatActivity extends AppCompatActivity
     @Override
     protected void onDestroy() {
         mNightModeStateProvider.removeObserver(this);
+        mActivityResultTracker.onDestroy();
         if (mModalDialogManagerSupplier.get() != null) {
             mModalDialogManagerSupplier.get().destroy();
         }
@@ -363,6 +372,7 @@ public class ChromeBaseAppCompatActivity extends AppCompatActivity
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         BundleUtils.saveLoadedSplits(outState);
+        mActivityResultTracker.onSaveInstanceState(outState);
     }
 
     // This method has different Nullness than Activity.onRestoreInstanceState().
@@ -757,6 +767,14 @@ public class ChromeBaseAppCompatActivity extends AppCompatActivity
     @VisibleForTesting
     public @Nullable EdgeToEdgeManager getEdgeToEdgeManager() {
         return mEdgeToEdgeManager;
+    }
+
+    /**
+     * Returns the {@link ActivityResultTracker} for launching new activities and watching for their
+     * result.
+     */
+    protected ActivityResultTracker getActivityResultTracker() {
+        return mActivityResultTracker;
     }
 
     /** Returns the {@link InsetObserver} for observing changes to the system insets. */
