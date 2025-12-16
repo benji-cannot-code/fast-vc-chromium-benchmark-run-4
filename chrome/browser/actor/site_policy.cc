d9948a8cef8398b36a8d0fdb590ee76725a19908
Dylan Cutler <dylancutler@google.com>
2025-12-16 11:07:33 -0800

    Use StrongAlias to differentiate origin gating allowlists
    When fixing crbug.com/465433353 we discovered that a function expecting
    the user-confirmed list of sensitive origins actually received
    the list of non-sensitive origins the actor is approved to work
    with during the task. To prevent this error from happening in the
    future, I am adding a StrongAlias for each list type to prevent this
    mistake at compile time.
    
    Bug: b:465433353, b:465208148
    Change-Id: I4bec38d6d80250492fea960e0838000229b4c9c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7262616
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Dylan Cutler <dylancutler@google.com>
    Cr-Commit-Position: refs/heads/main@{#1559464}

--- chrome/browser/actor/site_policy.cc
+++ chrome/browser/actor/site_policy.cc
@@ -143,7 +143,7 @@ void MayActOnUrlInternal(
     const GURL& url,
     bool allow_insecure_http,
     Profile* profile,
-    base::optional_ref<const absl::flat_hash_set<url::Origin>> allowed_origins,
+    base::optional_ref<const ConfirmedOriginSet> confirmed_origins,
     std::unique_ptr<DecisionWrapper> decision_wrapper) {
   if ((net::IsLocalhost(url) && url.SchemeIsHTTPOrHTTPS()) ||
       url.IsAboutBlank()) {
@@ -248,11 +248,11 @@ void MayActOnUrlInternal(
 
   // Blocklist is checked by `ShouldBlockNavigationUrlForOriginGating` when this
   // feature is enabled, and origins the user allowed the actor to interact with
-  // will be included in the `allowed_origins` set. If `url` has an origin not
+  // will be included in the `confirmed_origins` set. If `url` has an origin not
   // in the set, we apply the optimization guide check.
   if (IsNavigationGatingEnabled() &&
-      (!allowed_origins ||
-       base::Contains(*allowed_origins, url::Origin::Create(url)))) {
+      (!confirmed_origins ||
+       base::Contains(confirmed_origins->value(), url::Origin::Create(url)))) {
     decision_wrapper->Accept();
     return;
   }
@@ -298,7 +298,7 @@ void InitActionBlocklist(Profile* profile) {
 void MayActOnTab(const tabs::TabInterface& tab,
                  AggregatedJournal& journal,
                  TaskId task_id,
-                 const absl::flat_hash_set<url::Origin>& allowed_origins,
+                 const ConfirmedOriginSet& confirmed_origins,
                  DecisionCallbackWithReason callback) {
   content::WebContents& web_contents = *tab.GetContents();
 
@@ -330,7 +330,7 @@ void MayActOnTab(const tabs::TabInterface& tab,
   MayActOnUrlInternal(
       url, /*allow_insecure_http=*/false,
       Profile::FromBrowserContext(web_contents.GetBrowserContext()),
-      allowed_origins, std::move(decision_wrapper));
+      confirmed_origins, std::move(decision_wrapper));
 }
 
 void MayActOnUrl(const GURL& url,
