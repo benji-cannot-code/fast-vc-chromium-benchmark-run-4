f667cfbc356476b7073e013e833e25af48767f04
Gauthier Ambard <gambard@chromium.org>
2025-12-16 03:07:18 -0800

    Refactor PageInfo folders
    Split them in coordinator/ui/test
    
    Bypass-Check-License: Moving files
    Bug: None
    Change-Id: Ib20f2df8c00fdc08893b46e75da653f95a17347b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7252689
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Filipa Senra <fsenra@google.com>
    Cr-Commit-Position: refs/heads/main@{#1559234}

--- ios/chrome/browser/page_info/features/features.h
+++ ios/chrome/browser/page_info/features/features.h
@@ -1,11 +1,13 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2025 The Chromium Authors
+// Copyright 2023 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #ifndef IOS_CHROME_BROWSER_PAGE_INFO_FEATURES_FEATURES_H_
 #define IOS_CHROME_BROWSER_PAGE_INFO_FEATURES_FEATURES_H_
 
-#import "ios/chrome/browser/page_info/ui_bundled/features.h"
+// Whether the AboutThisSite feature is enabled. Only users with languages that
+// are supported should fetch the AboutThisSite information and see the UI.
+bool IsAboutThisSiteFeatureEnabled();
 
 #endif  // IOS_CHROME_BROWSER_PAGE_INFO_FEATURES_FEATURES_H_
