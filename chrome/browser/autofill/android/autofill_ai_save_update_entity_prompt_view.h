d0a304e9cc9c9203b56614997486de76a50eed1d
Timofey Chudakov <tchudakov@google.com>
2025-12-16 08:00:19 -0800

    [AutofillAi - M4] Create AutofillAiSaveUpdateEntityPrompt.
    This CL creates classes for the C++ and Java prompt view and controller.
    
    Bug: 460410690
    Change-Id: Ia6f9203a3be149b58c8c3503d546af22585353c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7242183
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1559355}

---
+++ chrome/browser/autofill/android/autofill_ai_save_update_entity_prompt_view.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_AUTOFILL_ANDROID_AUTOFILL_AI_SAVE_UPDATE_ENTITY_PROMPT_VIEW_H_
+#define CHROME_BROWSER_AUTOFILL_ANDROID_AUTOFILL_AI_SAVE_UPDATE_ENTITY_PROMPT_VIEW_H_
+
+namespace autofill {
+
+class AutofillAiSaveUpdateEntityPromptController;
+
+// The UI interface which prompts the user to confirm saving new/updating
+// existing address profile imported from a form submission.
+class AutofillAiSaveUpdateEntityPromptView {
+ public:
+  // Shows the Android message UI to the user. Return `false` if the message
+  // wasn't shown for any reason.
+  virtual bool Show(
+      const AutofillAiSaveUpdateEntityPromptController* controller) = 0;
+
+  virtual ~AutofillAiSaveUpdateEntityPromptView() = default;
+};
+
+}  // namespace autofill
+
+#endif  // CHROME_BROWSER_AUTOFILL_ANDROID_AUTOFILL_AI_SAVE_UPDATE_ENTITY_PROMPT_VIEW_H_
---
+++ chrome/browser/autofill/android/mock_autofill_ai_save_update_entity_prompt_view.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/autofill/android/mock_autofill_ai_save_update_entity_prompt_view.h"
+
+namespace autofill {
+
+MockAutofillAiSaveUpdateEntityPromptView::
+    MockAutofillAiSaveUpdateEntityPromptView() = default;
+
+MockAutofillAiSaveUpdateEntityPromptView::
+    ~MockAutofillAiSaveUpdateEntityPromptView() = default;
+
+}  // namespace autofill
