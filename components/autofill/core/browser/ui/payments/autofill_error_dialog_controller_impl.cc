aaa4c71afe986f097b837cc96d711dfe1b33fd3b
James Sun <jamessun@google.com>
2025-12-16 06:51:49 -0800

    [AIAE] Add BNPL unsupported currency error dialog
    This CL adds the error dialog for unsupported currency returned from the
    AI-based amount extraction.
    
    Error dialog: go/strpic/b66b90edd96536efb2e13be7ff241ed82f8f7e63
    
    bug: 467418149
    Change-Id: I696e55ff20bca5d63e6687571b2de799a10e5503
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7256300
    Commit-Queue: James Sun <jamessun@google.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1559303}

--- components/autofill/core/browser/ui/payments/autofill_error_dialog_controller_impl.cc
+++ components/autofill/core/browser/ui/payments/autofill_error_dialog_controller_impl.cc
@@ -105,6 +105,9 @@ const std::u16string AutofillErrorDialogControllerImpl::GetTitle() {
     case AutofillErrorDialogType::kBnplTemporaryError:
     case AutofillErrorDialogType::kBnplPermanentError:
       return l10n_util::GetStringUTF16(IDS_AUTOFILL_BNPL_ERROR_DIALOG_TITLE);
+    case AutofillErrorDialogType::kBnplUnsupportedCurrencyError:
+      return l10n_util::GetStringUTF16(
+          IDS_AUTOFILL_BNPL_UNSUPPORTED_CURRENCY_ERROR_DIALOG_TITLE);
 
     case AutofillErrorDialogType::kTypeUnknown:
       NOTREACHED();
@@ -169,6 +172,9 @@ const std::u16string AutofillErrorDialogControllerImpl::GetDescription() {
     case AutofillErrorDialogType::kBnplPermanentError:
       return l10n_util::GetStringUTF16(
           IDS_AUTOFILL_BNPL_PERMANENT_ERROR_DESCRIPTION);
+    case AutofillErrorDialogType::kBnplUnsupportedCurrencyError:
+      return l10n_util::GetStringUTF16(
+          IDS_AUTOFILL_BNPL_UNSUPPORTED_CURRENCY_ERROR_DESCRIPTION);
     case AutofillErrorDialogType::kTypeUnknown:
       NOTREACHED();
   }
