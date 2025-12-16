4617a5dac3c6efb7ae1a4d0118ddaed8fd9b538c
Daniil Sakhapov <sakhapov@chromium.org>
2025-12-16 15:19:41 -0800

    [a11y][carousel] Remove content from inactive column tabs from AX tree
    This CL:
    - Adds is_inside_inactive_column_tab_ bitfield to LayoutObject;
    - When the active column changes in a scroll-marker-group with tabs mode, marks all LayoutObjects inside inactive columns with this bit;
    - Accessibility code simply checks this bit to determine if a node is
    inside inactive column tab.
    
    Also, this CL adds an optimization to check if we are inside a scroller
    with scroll marker group in tabs mode before checking if we are inside
    an inactive tab.
    
    Bug: 462518542
    Change-Id: Ice00b8022f394eb0f0e2667b1bf81ae6207483ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7197847
    Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Lucas Radaelli <lucasradaelli@google.com>
    Cr-Commit-Position: refs/heads/main@{#1559602}

--- third_party/blink/renderer/modules/accessibility/ax_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object.cc
@@ -4158,7 +4158,7 @@ bool AXObject::ComputeIsIgnoredButIncludedInTree() {
   CHECK(!IsDetached());
 
   // Nothing inside an inactive scroll marker's tab is included in the tree.
-  if (InsideOriginatingElementForInactiveScrollMarkerInTabsMode()) {
+  if (InsideInactiveScrollMarkerTab()) {
     return false;
   }
 
