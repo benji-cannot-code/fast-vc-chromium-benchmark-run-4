411f685b50cfee84152fefd7760fbbbb62e28345
Sourav <sinhasourav@google.com>
2025-12-16 14:40:32 -0800

    [iOSFidoImportExport] Update export entry point UI
    This CL modifies the appearance of the "Export Data" entry point button
    within the password settings screen:
    
    It changes the text colour of the button to black when enabled and adds
    a disclosure indicator ('>') to the cell.
    
    Screenshot: https://screenshot.googleplex.com/At8rins9qfW4QEb
    Bug: 469408566
    Change-Id: Icb06ca74c5bc4c9152a5ad345b6b0d3b9532f20d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7266049
    Reviewed-by: Rafał Godlewski <rgod@google.com>
    Commit-Queue: Sourav Uttam Sinha <sinhasourav@google.com>
    Cr-Commit-Position: refs/heads/main@{#1559596}

--- ios/chrome/browser/settings/ui_bundled/password/password_settings/password_settings_view_controller.mm
+++ ios/chrome/browser/settings/ui_bundled/password/password_settings/password_settings_view_controller.mm
@@ -615,6 +615,8 @@ BOOL ShouldShowTurnOnPasswordsInOtherAppsItem(
     if (CredentialExchangeEnabled()) {
       exportPasswordsItem.text =
           l10n_util::GetNSString(IDS_IOS_EXPORT_PASSWORDS_AND_PASSKEYS);
+      exportPasswordsItem.accessoryType =
+          UITableViewCellAccessoryDisclosureIndicator;
     }
   }
   exportPasswordsItem.accessibilityTraits = UIAccessibilityTraitButton;
@@ -1256,7 +1258,7 @@ BOOL ShouldShowTurnOnPasswordsInOtherAppsItem(
   }
 
   if (_canExportCredentials) {
-    _exportPasswordsItem.textColor = [UIColor colorNamed:kBlueColor];
+    _exportPasswordsItem.textColor = [UIColor colorNamed:kTextPrimaryColor];
     _exportPasswordsItem.accessibilityTraits &= ~UIAccessibilityTraitNotEnabled;
   } else {
     // Disable, rather than remove, because the button will go back and forth
