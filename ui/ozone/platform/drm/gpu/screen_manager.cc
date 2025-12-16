a52ad3f83a5489528efe46081b5b7b4f5fd6f9a7
Colin Blundell <blundell@chromium.org>
2025-12-16 02:09:44 -0800

    [//ui] Have HardwareDisplayController talk SIFormat
    Eliminates needs for clients to do conversions.
    
    Bug: 356649879
    Change-Id: Ib789bb24ed228afbfe28f6c6c0162dc92992b325
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7262235
    Reviewed-by: Quyen Le <lehoangquyen@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1559211}

--- ui/ozone/platform/drm/gpu/screen_manager.cc
+++ ui/ozone/platform/drm/gpu/screen_manager.cc
@@ -541,9 +541,7 @@ void ScreenManager::SetPreferredModifiers(
         auto it = FindDisplayController(params.drm, params.crtc);
         DCHECK(*it);
         it->get()->UpdatePreferredModifierForFormat(
-            viz::SinglePlaneSharedImageFormatToBufferFormat(
-                display::DisplaySnapshot::PrimaryFormat()),
-            picked_modifier);
+            display::DisplaySnapshot::PrimaryFormat(), picked_modifier);
       }
     }
   }
