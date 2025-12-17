2f075673ad2b0b94dfeaa8d1c661cd611b4c962f
Ayumi Ono <ayumiohno@google.com>
2025-12-16 22:05:38 -0800

    Make ExtensionUrlOverrideStateTracker destructor virtual.
    `ExtensionUrlOverrideStateTrackerImpl` is a derived class that inherits
    from `ExtensionUrlOverrideStateTracker` (the base class).
    
    In `ExtensionUrlOverrideRegistryManager`, the member `state_tracker_` is
    declared as `std::unique_ptr<ExtensionUrlOverrideStateTracker>`.
    However, it is being initialized with
    `std::make_unique<ExtensionUrlOverrideStateTrackerImpl>()`.
    
    Without a `virtual` destructor in the base class, when `state_tracker_`
    is destroyed, only the base class destructor is invoked. This leads to:
    *   The wrong size parameter being passed to sized delete.
    *   Undefined Behavior because members unique to `ExtensionUrlOverrideStateTrackerImpl`, such as `std::unique_ptr<RegistrarSynchronizer>`, are not properly destructed.
    
    Making the base class destructor virtual ensures that the derived class
    destructor is also called, thus preventing these issues.
    
    Include-Ci-Only-Tests: chromium.android.desktop:android-desktop-x64-rel-15-tests|chrome_public_test_apk
    Change-Id: Ic3834ba2719e1efcccec4828acdec7dd74695561
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7267470
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Mikihito Matsuura <mikt@google.com>
    Commit-Queue: Ayumi Ono <ayumiohno@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1559746}

--- chrome/browser/android/extensions/extensions_url_override_state_tracker_impl.h
+++ chrome/browser/android/extensions/extensions_url_override_state_tracker_impl.h
@@ -33,7 +33,7 @@ class ExtensionUrlOverrideStateTrackerImpl
 
   ExtensionUrlOverrideStateTrackerImpl(Profile* profile,
                                        StateListener* listener);
-  ~ExtensionUrlOverrideStateTrackerImpl();
+  ~ExtensionUrlOverrideStateTrackerImpl() override;
 
   ExtensionUrlOverrideStateTrackerImpl(
       const ExtensionUrlOverrideStateTrackerImpl& client) = delete;
