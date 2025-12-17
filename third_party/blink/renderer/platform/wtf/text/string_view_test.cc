5b1c3d8e5dacf852bc44f829b73e3fb5016401db
Kent Tamura <tkent@chromium.org>
2025-12-17 01:08:01 -0800

    WTF: Add StringView::contains()
    It's compatible with std::basic_string_view::contains().
    This CL has no behavior changes.
    
    Change-Id: I61850ed4cb43d41b241dbf6b6c85003512bac628
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7261595
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1559799}

--- third_party/blink/renderer/platform/wtf/text/string_view_test.cc
+++ third_party/blink/renderer/platform/wtf/text/string_view_test.cc
@@ -6,8 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/text/string_view.h"
 
 #include "base/compiler_specific.h"
+#include "base/containers/span.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
+#include "third_party/blink/renderer/platform/wtf/text/character_names.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_impl.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
@@ -644,6 +646,20 @@ TEST(StringViewTest, NextCodePointOffset) {
   const UChar kTrail = 0xDC00;
   StringView broken3(base::span_from_ref(kTrail));
   EXPECT_EQ(1u, broken3.NextCodePointOffset(0));
+}
+
+TEST(StringViewTest, Contains) {
+  EXPECT_FALSE(StringView().contains(0));
+  EXPECT_FALSE(StringView("").contains(0));
+  EXPECT_FALSE(StringView(u"").contains(0));
+  EXPECT_FALSE(StringView("ascii").contains(0));
+  EXPECT_FALSE(StringView(u"ascii").contains(0));
+  EXPECT_TRUE(StringView(base::byte_span_from_cstring("as\0cii")).contains(0));
+  EXPECT_TRUE(StringView(base::span_from_cstring(u"asci\0i")).contains(0));
+
+  EXPECT_FALSE(StringView("ascii").contains(uchar::kBlackSquare));
+  EXPECT_FALSE(StringView(u"ascii").contains(uchar::kBlackSquare));
+  EXPECT_TRUE(StringView(u"ascii\u25A0").contains(uchar::kBlackSquare));
 }
 
 }  // namespace blink
--- third_party/blink/renderer/platform/wtf/text/text_encoding_registry.cc
+++ third_party/blink/renderer/platform/wtf/text/text_encoding_registry.cc
@@ -121,7 +121,7 @@ static void CheckExistingName(StringView alias,
 static bool IsUndesiredAlias(StringView alias) {
   // Reject aliases with version numbers that are supported by some back-ends
   // (such as "ISO_2022,locale=ja,version=0" in ICU).
-  if (alias.Find([](UChar ch) { return ch == ','; }) != kNotFound) {
+  if (alias.contains(',')) {
     return true;
   }
   // 8859_1 is known to (at least) ICU, but other browsers don't support this
