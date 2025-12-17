f63b033a0340f824aa2737f28732833fe57324e8
Olivier Robin <olivierrobin@google.com>
2025-12-17 10:09:04 -0800

    Fix dismissal of QR scanner view
    Setting the frame and relayouting the presented view before adding it to
    the view hierarchy can lead to infinite layout loop.
    
    Fix the issue by adding the view first, then layout, than apply the
    transformation.
    
    Note that the layouting must happen before the transform to have a
    smooth animation.
    
    Fixed: 469015351
    Change-Id: I7b09d6f86f3b307dadad9ad892744476d8568d3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7264788
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1560022}

--- ios/chrome/browser/scanner/ui_bundled/scanner_transitioning_delegate.mm
+++ ios/chrome/browser/scanner/ui_bundled/scanner_transitioning_delegate.mm
@@ -57,13 +57,11 @@ enum ScannerTransition { PRESENT, DISMISS };
 
   ProceduralBlock animations;
 
-  // Set the frame for the presented view.
-  presentedView.frame = finalFrame;
-  [presentedView layoutIfNeeded];
-
   switch (_transition) {
     case PRESENT: {
       [containerView insertSubview:presentedView belowSubview:presentingView];
+      presentedView.frame = finalFrame;
+      [presentedView layoutIfNeeded];
       animations = ^void {
         presentingView.transform =
             CGAffineTransformMakeTranslation(0, -finalFrame.size.height);
@@ -72,6 +70,8 @@ enum ScannerTransition { PRESENT, DISMISS };
     }
     case DISMISS: {
       [containerView insertSubview:presentedView aboveSubview:presentingView];
+      presentedView.frame = finalFrame;
+      [presentedView layoutIfNeeded];
       presentedView.transform =
           CGAffineTransformMakeTranslation(0, -finalFrame.size.height);
       animations = ^void {
