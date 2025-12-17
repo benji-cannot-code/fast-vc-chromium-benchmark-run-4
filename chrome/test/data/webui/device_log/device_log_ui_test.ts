f666598b587b1ef5edb7643e38709ba7c78ccb6c
Nina Satragno <nsatragno@chromium.org>
2025-12-17 15:27:29 -0800

    Componentize chrome://device-log
    Turn the chrome://device-log page into a lit component for ease of
    development and testing. This CL does the minimum necessary for that to
    happen.
    * Structural & BUILD.gn changes as needed.
    * Element IDs now follow camelCase and getRequiredElement was replaced
      by this.$.elementId.
    * Fixed repeated `logCheckboxContainer` id.
    * querySelectors are now relative to the shadow root.
    * Smoke test that verifies the component loads.
    
    A follow-up change will update the component to take advantage of
    declarative lit rendering instead.
    
    Bypass-Check-License: moved files from `device_log_ui` to `app`
    Bug: 469125041
    Change-Id: Ie09d548cfcee95756b0fddba96e6305663ba0338
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7266987
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1560241}

---
+++ chrome/test/data/webui/device_log/device_log_ui_test.ts
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'chrome://device-log/app.js';
+
+import type {DeviceLogAppElement} from 'chrome://device-log/app.js';
+import {assertTrue} from 'chrome://webui-test/chai_assert.js';
+
+suite('DeviceLog', function() {
+  let app: DeviceLogAppElement;
+
+  setup(function() {
+    document.body.innerHTML = window.trustedTypes!.emptyHTML;
+    app = document.createElement('device-log-app');
+    document.body.appendChild(app);
+  });
+
+  test('element exists', function() {
+    // TODO(crbug.com/469125041): add tests.
+    assertTrue(!!app);
+  });
+});
