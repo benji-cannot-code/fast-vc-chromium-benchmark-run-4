cb59c8e7700c1397f22fddc97d53913651118914
Scott Yoder <scottyoder@google.com>
2025-12-17 15:16:04 -0800

    [ios] Disable loadMinimalAppUI on NTPHomeTestCase tests on ipad-device
    For some reason loadMinimalAppUI causes these tests to fail on
    ipad-device.
    
    Bug: 469833796
    Change-Id: Ic7e6aebb2f5ad1842d88f6ddcbd3c862d3bc4a16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7270548
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1560234}

--- ios/chrome/browser/content_suggestions/ui_bundled/ntp_home_egtest.mm
+++ ios/chrome/browser/content_suggestions/ui_bundled/ntp_home_egtest.mm
@@ -65,6 +65,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "net/test/embedded_test_server/http_request.h"
 #import "net/test/embedded_test_server/http_response.h"
 #import "third_party/search_engines_data/resources/definitions/prepopulated_engines.h"
+#import "ui/base/device_form_factor.h"
 #import "ui/base/l10n/l10n_util.h"
 
 namespace {
@@ -169,6 +170,14 @@ bool AreNumbersEqual(CGFloat num1, CGFloat num2) {
 }
 
 - (BOOL)loadMinimalAppUI {
+  // TODO(crbug.com/469833796): Fix this issue on ipad-device bot.
+#if !TARGET_OS_SIMULATOR
+  // The app hasn't booted yet, so `isIpadIdiom` cannot be used here.
+  if (ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_TABLET) {
+    return NO;
+  }
+#endif
+
   std::vector<SEL> minimalAppUITests = {
       @selector(testAccessibility),
       @selector(testOmniboxWidthRotation),
