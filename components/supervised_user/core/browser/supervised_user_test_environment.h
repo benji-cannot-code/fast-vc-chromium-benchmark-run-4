a6303d31e20fa986f439626de30dcfdd466a9d71
Tomek Jurkiewicz <tju@google.com>
2025-12-17 08:23:21 -0800

    Change ContentFiltersObserverBridge to accept a nullable PrefService.
    Originally, PrefService was added as part of quickfix to
    crbug.com/463744991, but that's breaking dependency structure and
    violating the concept (ContentFilterObserverBridge is not bound to
    profile, but rather to the OS / Device).
    
    It is made optional to open door for instantiating
    ContentFiltersObserverBridge without the Profile context
    
    Bug: 469694485, 463744991
    Change-Id: I07650a476318ef5a7bca78340e6bfbe891150f37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7265331
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Tomek Jurkiewicz <tju@google.com>
    Cr-Commit-Position: refs/heads/main@{#1559980}

--- components/supervised_user/core/browser/supervised_user_test_environment.h
+++ components/supervised_user/core/browser/supervised_user_test_environment.h
@@ -124,7 +124,7 @@ class FakeContentFiltersObserverBridge final
  public:
   // Matching constructor of ContentFiltersObserverBridge.
   FakeContentFiltersObserverBridge(std::string_view setting_name,
-                                   const PrefService& pref_service);
+                                   const PrefService* pref_service);
   FakeContentFiltersObserverBridge(const FakeContentFiltersObserverBridge&) =
       delete;
   FakeContentFiltersObserverBridge& operator=(
