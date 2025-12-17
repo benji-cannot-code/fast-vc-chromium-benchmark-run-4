4b16f029d892e020d79090a9e2ad24f6f24c421e
Joe Peplowski <joepeplowski@google.com>
2025-12-17 12:22:15 -0800

    Add GeminiSettingsAction & GeminiSettingsActionType
    These will be used in future CLs to handle interactions with rows in
    Gemini dynamic settings.
    
    Bug: 462382850
    Change-Id: Idfbabea364c1b72e2fba086b2d43a081f696ceae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7264996
    Reviewed-by: Ibrahim Kanouche <kanouche@google.com>
    Reviewed-by: Joemer Ramos <joemerramos@google.com>
    Commit-Queue: Joe Peplowski <joepeplowski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1560118}

---
+++ ios/chrome/browser/settings/ui_bundled/bwg/model/gemini_settings_action.mm
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/settings/ui_bundled/bwg/model/gemini_settings_action.h"
+
+#import "base/check.h"
+#import "ios/chrome/browser/settings/ui_bundled/bwg/model/gemini_settings_action_type.h"
+
+@implementation GeminiSettingsAction
+
+- (instancetype)initWithType:(GeminiSettingsActionType)type
+                         URL:(NSURL*)URL
+              viewController:(UIViewController*)viewController {
+  self = [super init];
+  if (self) {
+    switch (type) {
+      case GeminiSettingsActionTypeViewController:
+        DCHECK(viewController && !URL);
+        break;
+      case GeminiSettingsActionTypeURL:
+        DCHECK(URL && !viewController);
+        break;
+      case GeminiSettingsActionTypeUnknown:
+        break;
+    }
+
+    _type = type;
+    _URL = URL;
+    _viewController = viewController;
+  }
+  return self;
+}
+
+@end
---
+++ ios/chrome/browser/settings/ui_bundled/bwg/model/gemini_settings_action_type.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SETTINGS_UI_BUNDLED_BWG_MODEL_GEMINI_SETTINGS_ACTION_TYPE_H_
+#define IOS_CHROME_BROWSER_SETTINGS_UI_BUNDLED_BWG_MODEL_GEMINI_SETTINGS_ACTION_TYPE_H_
+
+#import <Foundation/Foundation.h>
+
+// The type of the settings action.
+typedef NS_ENUM(NSInteger, GeminiSettingsActionType) {
+  GeminiSettingsActionTypeUnknown = 0,
+
+  // The settings action opens a view controller.
+  GeminiSettingsActionTypeViewController = 1,
+
+  // The settings action opens a URL.
+  GeminiSettingsActionTypeURL = 2,
+};
+
+#endif  // IOS_CHROME_BROWSER_SETTINGS_UI_BUNDLED_BWG_MODEL_GEMINI_SETTINGS_ACTION_TYPE_H_
