805b80c42a954d9746c130659f948b3123bd2525
François Doray <fdoray@chromium.org>
2025-12-17 10:25:33 -0800

    [bytesize] Migrate content::indexed_db::DatabaseConnection to ByteSize.
    Replaces deprecated ByteCount with stronger types to increase clarity
    and type safety:
    * ByteSize: A non-negative amount of bytes.
    * ByteSizeDelta: A delta of bytes, which can be positive or negative.
    
    This is a mechanical refactor with no behavioral change.
    
    Bug: 448661443
    Change-Id: I56cca1c3720f65bfc2f38cc43f3225d41694b201
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7266166
    Auto-Submit: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Evan Stade <evanstade@microsoft.com>
    Reviewed-by: Evan Stade <evanstade@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1560036}

--- content/browser/indexed_db/instance/sqlite/database_connection.h
+++ content/browser/indexed_db/instance/sqlite/database_connection.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <string_view>
 
-#include "base/byte_count.h"
+#include "base/byte_size.h"
 #include "base/files/file_path.h"
 #include "base/gtest_prod_util.h"
 #include "base/memory/weak_ptr.h"
@@ -255,7 +255,7 @@ class CONTENT_EXPORT DatabaseConnection {
       int compression_type);
 
   // Changes the size at which blobs are chunked.
-  static void OverrideMaxBlobSizeForTesting(base::ByteCount size);
+  static void OverrideMaxBlobSizeForTesting(base::ByteSize size);
 
  private:
   friend class BackingStoreSqliteTest;
