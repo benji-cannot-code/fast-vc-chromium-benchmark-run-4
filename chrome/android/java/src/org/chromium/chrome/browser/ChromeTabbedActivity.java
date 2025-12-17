8a2fbf9a6c2c85dedaa2c9f975a52052b63dcc79
Zhe Li <zheliooo@google.com>
2025-12-17 13:54:10 -0800

    Reland "[RWM][RecentlyClosed] Add InstanceStateObserver"
    This reverts commit 382a5dbbbdd5f089c44f62ed91bebf53ba5b9de3.
    
    Reason for revert: <Removed problematic line>
    
    Original change's description:
    > Revert "[RWM][RecentlyClosed] Add InstanceStateObserver"
    >
    > This reverts commit ac7259cf12f175344980ba8c47e0318ffd5c6fc0.
    >
    > Reason for revert: TabsTest#testIncognitoTabsNotRestoredAfterSwipe failed since crrev.com/c/7236031. Related deps revert. Check https://ci.chromium.org/ui/p/chromium/builders/ci/android-desktop-x64-rel-15-tests/15905/overview for more details.
    >
    > Original change's description:
    > > [RWM][RecentlyClosed] Add InstanceStateObserver
    > >
    > > Bug: 444679670
    > > Change-Id: I91b3d24da71f719080bcb35dbbdb9091e1bf6f43
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7236031
    > > Commit-Queue: Zhe Li <zheliooo@google.com>
    > > Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1558820}
    >
    > Bug: 444679670, 469128569
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 444679670
    > Change-Id: Ic340e9a9772e7f1824b3e3a8d33de27d0b853a8b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7261758
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Kay Lin <kaiyilin@google.com>
    > Commit-Queue: Kay Lin <kaiyilin@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1559168}
    
    Bug: 444679670, 469128569
    Bug: 444679670
    Change-Id: Iab9eaf4ec3cde8c26521006aa74a8fccfeb95ca0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7266306
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Zhe Li <zheliooo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1560182}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -4561,6 +4561,10 @@ public class ChromeTabbedActivity extends ChromeActivity implements PreAttachInt
         return mShowHistoryRunnable != null;
     }
 
+    public RecentlyClosedEntriesManager getRecentlyClosedEntriesManagerForTesting() {
+        return mRecentlyClosedEntriesManager;
+    }
+
     @Override
     public void onProvideKeyboardShortcuts(
             List<KeyboardShortcutGroup> data, Menu menu, int deviceId) {
