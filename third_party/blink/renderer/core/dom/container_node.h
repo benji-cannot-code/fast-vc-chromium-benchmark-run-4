56810991a15bdb7346207177d912afa6d2c37a75
Noam Rosenthal <nrosenthal@chromium.org>
2025-12-17 04:51:47 -0800

    Make sure the new streaming parser invalidates CSS correctly
    Some side effects of removing elements, such as the :has selectors, are
    handled differently with regards to cache invalidation based on whether
    the elements were inserted by the parser or by API.
    
    Some of the assumptions around the "parser" mode here are that the
    parser only works once on each element.
    
    With streamHTML* and <template contentmethod>, this might no longer be
    the case, as a new parser can be created for an already parsed element.
    This has result in racy scenarios of invalidation around CSS :has
    (potentially around other invalidations as well, but they are difficult
    to spot).
    
    The fix is to treat the "remove all children" invoked by patch templates
    as a regular RemoveChildren. Note that in all cases but scripts, this
    distinction is not web observable, and ParserRemoveChildren had no other
    callers.
    
    Bug: 431374376
    Change-Id: I45704d6a014b7c5c6103043cc9e6719c89527f0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7246719
    Commit-Queue: Noam Rosenthal <nrosenthal@google.com>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1559886}

--- third_party/blink/renderer/core/dom/container_node.h
+++ third_party/blink/renderer/core/dom/container_node.h
@@ -180,7 +180,6 @@ class CORE_EXPORT ContainerNode : public Node {
   void ParserFinishedBuildingDocumentFragment(ShouldNotifyInsertedNodes);
   void ParserRemoveChild(Node&);
   void ParserInsertBefore(Node* new_child, Node& ref_child);
-  void ParserRemoveAllChildren();
   void ParserReplaceChild(Node& new_child, Node& old_child);
   void ParserTakeAllChildrenFrom(ContainerNode&);
 
