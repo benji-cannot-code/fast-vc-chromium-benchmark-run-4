d2fb6846b01582cea8737372a500405eb1a50b7b
Angela Novakovic <novakovica@google.com>
2025-12-16 19:32:56 -0800

    [iOS][PRDBD] Complete Clear Browsing Data folder reorganization.
    This CL finishes restructuring the Clear Browsing Data folder per
    go/reorgdbd and go/bling-folder-structure.
    
    The remaining files within
    ios/chrome/browser/settings/ui_bundled/clear_browsing_data/ have now
    been moved into the new coordinator/, ui/, and test/ subfolders.
    
    This follows the previous CL which reorganized the Quick Delete Browsing
    Data subdirectory.
    
    BYPASS_LARGE_CHANGE_WARNING: Reorganization of the folder.
    
    Bypass-Check-License: Moved existing files with original copyright years.
    Fixed: 465737069
    Bug: 468329885
    Change-Id: I3f1768b1b799b6aa422c953eca9aab6775d9745c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7254139
    Reviewed-by: Zhaoyang Li <zhaoyangli@chromium.org>
    Commit-Queue: Angela Novakovic <novakovica@google.com>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Filipa Senra <fsenra@google.com>
    Reviewed-by: Noémie St-Onge <noemies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1559718}

--- ios/chrome/browser/flags/about_flags.mm
+++ ios/chrome/browser/flags/about_flags.mm
@@ -116,7 +116,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/promos_manager/model/features.h"
 #import "ios/chrome/browser/reader_mode/model/features.h"
 #import "ios/chrome/browser/screen_time/model/screen_time_buildflags.h"
-#import "ios/chrome/browser/settings/ui_bundled/clear_browsing_data/features.h"
+#import "ios/chrome/browser/settings/ui_bundled/clear_browsing_data/public/features.h"
 #import "ios/chrome/browser/settings/ui_bundled/password/password_manager_ui_features.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/shared/public/features/system_flags.h"
