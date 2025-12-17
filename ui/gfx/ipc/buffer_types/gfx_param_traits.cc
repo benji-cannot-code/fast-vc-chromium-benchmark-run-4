9b92ddc3957a09d6f59f86043171ed60846c94a8
Colin Blundell <blundell@chromium.org>
2025-12-16 23:52:30 -0800

    [//ui] Remove buffer_types param traits macros header
    This is no longer needed.
    
    Bug: 356649879
    Change-Id: Ic7188a7fc6260202aab4657462efdd1c2d7d5b20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7264944
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1559770}

--- ui/gfx/ipc/buffer_types/gfx_param_traits.cc
+++ ui/gfx/ipc/buffer_types/gfx_param_traits.cc
@@ -5,16 +5,3 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/gfx/ipc/buffer_types/gfx_param_traits.h"
 
-// Generate param traits write methods.
-#include "ipc/param_traits_write_macros.h"
-namespace IPC {
-#undef UI_GFX_IPC_BUFFER_TYPES_GFX_PARAM_TRAITS_MACROS_H_
-#include "ui/gfx/ipc/buffer_types/gfx_param_traits_macros.h"
-}  // namespace IPC
-
-// Generate param traits read methods.
-#include "ipc/param_traits_read_macros.h"
-namespace IPC {
-#undef UI_GFX_IPC_BUFFER_TYPES_GFX_PARAM_TRAITS_MACROS_H_
-#include "ui/gfx/ipc/buffer_types/gfx_param_traits_macros.h"
-}  // namespace IPC
--- ui/gfx/ipc/buffer_types/gfx_param_traits.h
+++ ui/gfx/ipc/buffer_types/gfx_param_traits.h
@@ -9,6 +9,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ipc/param_traits_macros.h"
 #include "ipc/param_traits_utils.h"
 #include "ui/gfx/ipc/buffer_types/gfx_ipc_export.h"
-#include "ui/gfx/ipc/buffer_types/gfx_param_traits_macros.h"
 
 #endif  // UI_GFX_IPC_BUFFER_TYPES_GFX_PARAM_TRAITS_H_
