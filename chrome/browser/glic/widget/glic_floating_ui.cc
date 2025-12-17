49495d9190cd6561709d6d8cdf3e0ddfaf2d661c
Bryant Chandler <bryantchandler@google.com>
2025-12-17 08:38:08 -0800

    [glic] Use GlicInstanceHelper for tab destruction observation
    Update GlicPinnedTabManager to subscribe to tab destruction via
    GlicInstanceHelper instead of observing WillDetach. This ensures that
    the destruction response happens after the tab restore service has
    collected its information.
    
    Also refactor GlicInstanceHelper::SubscribeToDestruction callback to
    remove the unused InstanceId parameter.
    
    Add browser test UnpinTabOnTabDestroyed.
    
    Bug: b:456482198
    Change-Id: I7aaa6eecfb7007653fb3fb1448d2d5d46a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7256609
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1559989}

--- chrome/browser/glic/widget/glic_floating_ui.cc
+++ chrome/browser/glic/widget/glic_floating_ui.cc
@@ -243,8 +243,7 @@ void GlicFloatingUi::MaybeSetWidgetCanResize() {
 #endif  // BUILDFLAG(IS_WIN)
 }
 
-void GlicFloatingUi::OnSourceTabDestroyed(tabs::TabInterface* tab,
-                                          const InstanceId& instance_id) {
+void GlicFloatingUi::OnSourceTabDestroyed(tabs::TabInterface* tab) {
   FloatingPanelCanAttachChanged(false);
 }
 
