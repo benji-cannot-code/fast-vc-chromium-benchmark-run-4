4cd0b38417f614ebbcc9ebc96b4bd4665b84eeef
Aman Verma <amanvr@google.com>
2025-12-17 12:20:10 -0800

    Enable UpdateScrollPredictorInputMapping default on Android
    This change enables the UpdateScrollPredictorInputMapping feature by
    default on Android except WebView. The experiment on WebView will be
    rolled further and the feature will be enabled by default on WebView
    after the experiment has concluded on Webview.
    
    The feature is being launched on Android based on 50%, 1% stable results
    https://docs.google.com/document/d/1Vtk3pBDm4Zh2nAGcX3ks_EjpkN9JRh_isyweuk1Raok/edit?tab=t.uael8musfbfl#heading=h.ewbmwf14xt8a.
    The enabled group shows improvements in scroll jank metrics.
    EventLatency.GestureScrollUpdate.Touchscreen.TotalLatency2 shows
    regression but it turns out to be due to how metric is calculated, this
    is being tracked in crbug.com/452340030, there are no other guardrail
    metrics regressions.
    
    Bug: b:444669046
    Change-Id: I5e95fcfaa17e21aa0b2e8ffcfc916f872c1a4d97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7146221
    Auto-Submit: Aman Verma <amanvr@google.com>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Aman Verma <amanvr@google.com>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1560116}

--- android_webview/browser/aw_field_trials.cc
+++ android_webview/browser/aw_field_trials.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/persistent_histogram_allocator.h"
 #include "base/path_service.h"
 #include "components/history/core/browser/features.h"
+#include "components/input/features.h"
 #include "components/metrics/persistent_histograms.h"
 #include "components/payments/content/android/payment_feature_map.h"
 #include "components/permissions/features.h"
@@ -64,6 +65,11 @@ void AwFieldTrials::RegisterFeatureOverrides(base::FeatureList* feature_list) {
   // fixed before enabling this feature flag for android.
   aw_feature_overrides.DisableFeature(
       blink::features::kAboutBlankPageRespectsDarkModeOnUserAction);
+
+  // TODO(crbug.com/444669046): Remove this once webview experiment has
+  // concluded.
+  aw_feature_overrides.DisableFeature(
+      input::features::kUpdateScrollPredictorInputMapping);
 
   // Disable enforcing `noopener` on Blob URL navigations on WebView.
   aw_feature_overrides.DisableFeature(
