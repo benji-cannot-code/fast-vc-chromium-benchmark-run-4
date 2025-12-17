b4875af257abb313ea7057b191f90a945ee95ec7
Timofey Chudakov <tchudakov@google.com>
2025-12-17 05:55:39 -0800

    [AutofillAi - M4] Handle message dismiss.
    Save/update message is shown as the first step in the Autofill AI
    save/update flow. The user can accept, dismiss or ignore the message.
    This CL handles the dismiss and ignore cases.
    
    Bug: 460410690
    Change-Id: I8f53fb8aa94ac4e27c0f4eac5743700cd6c8f324
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7261195
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1559907}

--- chrome/browser/ui/android/autofill/autofill_ai_save_update_entity_flow_manager_unittest.cc
+++ chrome/browser/ui/android/autofill/autofill_ai_save_update_entity_flow_manager_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/android/autofill/autofill_ai_save_update_entity_flow_manager.h"
 
+#include "base/test/mock_callback.h"
 #include "chrome/browser/autofill/android/save_update_address_profile_prompt_mode.h"
 #include "chrome/browser/ui/autofill/mock_autofill_message_controller.h"
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
@@ -15,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace autofill {
 
 using ::testing::_;
+using ::testing::SaveArgByMove;
 
 // TODO: crbug.com/460410690 - Cover different entity types.
 class AutofillAiSaveUpdateEntityFlowManagerTest
@@ -34,19 +36,53 @@ class AutofillAiSaveUpdateEntityFlowManagerTest
     return *flow_manager_;
   }
 
+  base::MockCallback<AutofillClient::EntityImportPromptResultCallback>&
+  prompt_closed_callback() {
+    return prompt_closed_callback_;
+  }
+
  private:
   MockAutofillMessageController autofill_message_controller_;
+  base::MockCallback<AutofillClient::EntityImportPromptResultCallback>
+      prompt_closed_callback_;
   std::unique_ptr<AutofillAiSaveUpdateEntityFlowManager> flow_manager_;
 };
 
-TEST_F(AutofillAiSaveUpdateEntityFlowManagerTest, ShowsMessage) {
+TEST_F(AutofillAiSaveUpdateEntityFlowManagerTest, ShowsMessage_MessageIngored) {
   test::PassportEntityOptions new_entity_options;
   new_entity_options.name = u"Jon doe";
   EntityInstance new_entity =
       test::GetPassportEntityInstance(new_entity_options);
-  EXPECT_CALL(message_controller(), Show(_));
+  std::unique_ptr<AutofillMessageModel> message_model;
 
-  flow_manager().OfferSave(new_entity);
+  // Show the message and save the message model.
+  EXPECT_CALL(message_controller(), Show(_))
+      .WillOnce(SaveArgByMove<0>(&message_model));
+  flow_manager().OfferSave(new_entity, prompt_closed_callback().Get());
+
+  // Simulate the user ignoring the message which dismissed it.
+  EXPECT_CALL(
+      prompt_closed_callback(),
+      Run(AutofillClient::AutofillAiBubbleClosedReason::kNotInteracted));
+  message_model->OnDismissed(messages::DismissReason::TIMER);
+}
+
+TEST_F(AutofillAiSaveUpdateEntityFlowManagerTest, ShowsMessage_MessageClosed) {
+  test::PassportEntityOptions new_entity_options;
+  new_entity_options.name = u"Jon doe";
+  EntityInstance new_entity =
+      test::GetPassportEntityInstance(new_entity_options);
+  std::unique_ptr<AutofillMessageModel> message_model;
+
+  // Show the message and save the message model.
+  EXPECT_CALL(message_controller(), Show(_))
+      .WillOnce(SaveArgByMove<0>(&message_model));
+  flow_manager().OfferSave(new_entity, prompt_closed_callback().Get());
+
+  // Simulate the swipe on the message that closes it.
+  EXPECT_CALL(prompt_closed_callback(),
+              Run(AutofillClient::AutofillAiBubbleClosedReason::kClosed));
+  message_model->OnDismissed(messages::DismissReason::GESTURE);
 }
 
 }  // namespace autofill
