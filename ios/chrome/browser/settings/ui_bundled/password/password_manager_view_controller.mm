33d60c77654d9877715bf7b65e135da40e8a9da1
Noémie St-Onge <noemies@google.com>
2025-12-17 14:04:51 -0800

    [iOS] Fix PasswordManagerViewControllerTest.TestOpenInSearchMode
    RunUntilIdle() wasn't always working well. Now waiting for a specific
    condition to be met, which is that the search controller becomes active.
    
    Fixed: 437314312
    Change-Id: I35479cc969f9a914cf59f801063971bcfb51b27b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7265926
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1560189}

--- ios/chrome/browser/settings/ui_bundled/password/password_manager_view_controller.mm
+++ ios/chrome/browser/settings/ui_bundled/password/password_manager_view_controller.mm
@@ -425,10 +425,6 @@ bool AreIssuesEqual(const std::vector<password_manager::AffiliatedGroup>& lhs,
   }
 }
 
-- (void)viewWillAppear:(BOOL)animated {
-  [super viewWillAppear:animated];
-}
-
 - (void)viewDidAppear:(BOOL)animated {
   [super viewDidAppear:animated];
   _hasViewAppeared = YES;
--- ios/chrome/browser/settings/ui_bundled/password/password_manager_view_controller_unittest.mm
+++ ios/chrome/browser/settings/ui_bundled/password/password_manager_view_controller_unittest.mm
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/test/bind.h"
 #import "base/test/ios/wait_util.h"
 #import "base/test/metrics/histogram_tester.h"
+#import "base/test/run_until.h"
 #import "components/affiliations/core/browser/fake_affiliation_service.h"
 #import "components/application_locale_storage/application_locale_storage.h"
 #import "components/google/core/common/google_util.h"
@@ -636,15 +637,7 @@ TEST_F(PasswordManagerViewControllerTest,
 
 // Tests that opening the PasswordManagerViewController in search mode shows the
 // expected content.
-// TODO(crbug.com/437314312): Deflake the test.
-TEST_F(PasswordManagerViewControllerTest, FLAKY_TestOpenInSearchMode) {
-  // TODO(crbug.com/437314312): Re-enable on device.
-#if !TARGET_OS_SIMULATOR
-  if (base::ios::IsRunningOnIOS26OrLater()) {
-    return;
-  }
-#endif
-
+TEST_F(PasswordManagerViewControllerTest, TestOpenInSearchMode) {
   // Call `settingsWillBeDismissed` on the initial view controller so that its
   // observers are reset.
   [GetPasswordManagerViewController() settingsWillBeDismissed];
@@ -675,8 +668,9 @@ TEST_F(PasswordManagerViewControllerTest, FLAKY_TestOpenInSearchMode) {
 
   // Verify that the search controller is active and that the content of table
   // view model is as expected in search mode.
-  RunUntilIdle();
-  EXPECT_TRUE(passwords_controller.navigationItem.searchController.active);
+  EXPECT_TRUE(base::test::RunUntil([&]() {
+    return passwords_controller.navigationItem.searchController.active;
+  }));
   EXPECT_EQ(1, [[passwords_controller tableViewModel] numberOfSections]);
   EXPECT_TRUE([passwords_controller.tableViewModel
       hasSectionForSectionIdentifier:SectionIdentifierSavedPasswords]);
