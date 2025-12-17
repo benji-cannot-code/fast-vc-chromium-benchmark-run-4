c455fb4530734f3efa626c10f7ba9e3c9c35c7c8
Mina Prapakamol <minap@google.com>
2025-12-16 21:40:37 -0800

    [iOS] Add store to disk option to automated PageContextExtraction
    Add `--save_page_context_locally` to "arguments passed on launch" to
    store extracted page context locally. This change also add logic to
    process test args, handle errors, and propagated page context extraction
    result and/or error from app interface back to the test.
    
    Bug: b:465016086
    Change-Id: I34120731bef6630e73f6d51e745da4a3a62cf556
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7261396
    Reviewed-by: Joemer Ramos <joemerramos@google.com>
    Reviewed-by: Amarjot Gill <amarjotgill@chromium.org>
    Commit-Queue: Mina Prapakamol <minap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1559741}

--- ios/chrome/browser/ai_prototyping/test/page_context_extraction_egtest.mm
+++ ios/chrome/browser/ai_prototyping/test/page_context_extraction_egtest.mm
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <XCTest/XCTest.h>
 
 #import "ios/chrome/browser/ai_prototyping/test/page_context_app_interface.h"
+#import "ios/chrome/browser/ai_prototyping/test/page_context_extraction_data.h"
+#import "ios/chrome/browser/ai_prototyping/test/test_args.h"
 #import "ios/chrome/test/earl_grey/chrome_actions.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
@@ -16,19 +18,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Test case for page context extraction.
 @interface PageContextExtraction : ChromeTestCase
+
+@property(strong, nonatomic) PageContextExtractionConfig* config;
 @end
 
 @implementation PageContextExtraction
 
-// Tests that the page context is extracted correctly.
-- (void)testExtractPageContext {
-  // TODO(crbug.com/465016086): Add implementation to load list of urls.
+- (void)setUp {
+  [super setUp];
+  self.config = [[PageContextExtractionConfig alloc]
+      initWithShouldStorePageContextLocally:
+          [TestArgs shouldStorePageContextLocallyFromTestArgs]];
+}
 
+// Extracts page context for the given `url`.
+- (PageContextExtractionResult*)extractPageContextForURL:(const GURL&)url {
   // Navigate to a site and wait for the site to complete loading before
   // capturing page context.
-  [ChromeEarlGrey loadURL:GURL("https://www.youtube.com/")
-        waitForCompletion:YES];
-  [PageContextAppInterface triggerPageContextCapture];
+  [ChromeEarlGrey loadURL:url waitForCompletion:YES];
+  [PageContextAppInterface triggerPageContextCaptureWithConfig:self.config];
 
   GREYCondition* condition = [GREYCondition
       conditionWithName:@"Page context extraction completed"
@@ -40,8 +48,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   BOOL success = [condition waitWithTimeout:30];
   XCTAssertTrue(success, @"Test timed out waiting for page context.");
 
-  NSString* pageContextString = [PageContextAppInterface pageContextResult];
-  NSLog(@"Captured Page Context: %@", pageContextString);
+  return [PageContextAppInterface pageContextResult];
+}
+
+// Tests that the page context is extracted correctly.
+- (void)testExtractPageContext {
+  // TODO(crbug.com/465016086): Add implementation to load list of urls.
+  PageContextExtractionResult* result =
+      [self extractPageContextForURL:GURL("https://www.youtube.com/")];
+
+  XCTAssertNotNil(result, @"Page context result should not be nil.");
+  XCTAssertNil(result.error, @"Page context capture failed with error: %@",
+               result.error);
+  NSLog(@"Captured Page Context: %@", result.pageContext);
+  if (self.config.shouldStorePageContextLocally) {
+    NSLog(@"Page Context should be stored on disk.");
+    XCTAssertNotNil(result.filePath,
+                    @"Page context local file path should not be nil.");
+    NSLog(@"Page Context saved to : %@", result.filePath);
+  }
 }
 
 @end
---
+++ ios/chrome/browser/ai_prototyping/test/test_args.h
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_AI_PROTOTYPING_TEST_TEST_ARGS_H_
+#define IOS_CHROME_BROWSER_AI_PROTOTYPING_TEST_TEST_ARGS_H_
+
+#import <Foundation/Foundation.h>
+
+static NSString* const kStorePageContextLocally =
+    @"--save_page_context_locally";
+static NSString* const kInputDir = @"--input_dir=";
+
+// A helper class for reading test arguments.
+@interface TestArgs : NSObject
+
+// Whether to store PageContext to disk.
++ (BOOL)shouldStorePageContextLocallyFromTestArgs;
+
+// Input directory for containing data required for PageContext extraction.
++ (NSString*)readInputDirFromTestArgs;
+
+// TODO(crbug.com/465016086): Add more test args such as upload to MQLS.
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_AI_PROTOTYPING_TEST_TEST_ARGS_H_
