56597f3f984573242931eb738606acb536a26177
Devlin Cronin <rdevlin.cronin@chromium.org>
2025-12-17 15:04:28 -0800

    [Desktop Android] Add GetLastActiveNormalBrowserWithProfile()
    Add browser_window_util::GetLastActiveNormalBrowserWithProfile() to
    retrieve the most-recently-active browser window with a given profile
    and BrowserWindowInterface::Type of TYPE_NORMAL. This is similar to the
    method in browser_finder.h, FindTabbedBrowser(), but is explicitly for
    extensions (which have different expectations and use cases than most
    callers in chrome; see the browser_window_util.h header comments).
    
    Also update callsites in ExtensionTabUtil that use FindTabbedBrowser()
    to use the new method instead.
    
    Bug: 430344931
    Change-Id: I0510be5c57c1ef24db07cee97c21cebcfcef3844
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7046340
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1560228}

--- chrome/browser/extensions/browser_window_util.cc
+++ chrome/browser/extensions/browser_window_util.cc
@@ -14,9 +14,48 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/tab_list_interface.h"
 #include "components/tabs/public/tab_interface.h"
 
+#if BUILDFLAG(IS_CHROMEOS)
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_window.h"
+#endif
+
 static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace extensions::browser_window_util {
+
+namespace {
+
+bool BrowserMatchesHelper(BrowserWindowInterface& browser,
+                          Profile& profile,
+                          bool include_incognito_or_parent,
+                          bool restrict_to_normal_browsers,
+                          bool restrict_to_current_workspace) {
+  if (browser.GetProfile() != &profile) {
+    if (!include_incognito_or_parent ||
+        !profile.IsSameOrParent(browser.GetProfile())) {
+      return false;
+    }
+  }
+
+  if (restrict_to_normal_browsers &&
+      browser.GetType() != BrowserWindowInterface::TYPE_NORMAL) {
+    return false;
+  }
+
+#if BUILDFLAG(IS_CHROMEOS)
+  if (restrict_to_current_workspace) {
+    Browser* browser_for_migration = browser.GetBrowserForMigrationOnly();
+    if (!browser_for_migration->window() ||
+        !browser_for_migration->window()->IsOnCurrentWorkspace()) {
+      return false;
+    }
+  }
+#endif
+
+  return true;
+}
+
+}  // namespace
 
 BrowserWindowInterface* GetBrowserForTabContents(
     content::WebContents& tab_contents) {
@@ -48,9 +87,31 @@ BrowserWindowInterface* GetLastActiveBrowserWithProfile(
   BrowserWindowInterface* last_active_browser = nullptr;
   ForEachCurrentBrowserWindowInterfaceOrderedByActivation(
       [&](BrowserWindowInterface* browser) {
-        if (browser->GetProfile() == &profile ||
-            (include_incognito_or_parent &&
-             profile.IsSameOrParent(browser->GetProfile()))) {
+        bool browser_matches =
+            BrowserMatchesHelper(*browser, profile, include_incognito_or_parent,
+                                 /*restrict_to_normal_browsers=*/false,
+                                 /*restrict_to_current_workspace=*/false);
+        if (browser_matches) {
+          last_active_browser = browser;
+          return false;  // Stop iterating.
+        }
+        return true;  // Continue iterating.
+      });
+
+  return last_active_browser;
+}
+
+BrowserWindowInterface* GetLastActiveNormalBrowserWithProfile(
+    Profile& profile,
+    bool include_incognito_or_parent) {
+  BrowserWindowInterface* last_active_browser = nullptr;
+  ForEachCurrentBrowserWindowInterfaceOrderedByActivation(
+      [&](BrowserWindowInterface* browser) {
+        bool browser_matches =
+            BrowserMatchesHelper(*browser, profile, include_incognito_or_parent,
+                                 /*restrict_to_normal_browsers=*/true,
+                                 /*restrict_to_current_workspace=*/true);
+        if (browser_matches) {
           last_active_browser = browser;
           return false;  // Stop iterating.
         }
