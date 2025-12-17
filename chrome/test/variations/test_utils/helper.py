51621f7084c9a9bdc62b1c11bb6418c6c35b7c8f
Arkadiusz Tomczak <artomczak@google.com>
2025-12-17 00:55:29 -0800

    Add retry logic for creating Desktop driver.
    We see that creating the driver has been flaky recently on some
    desktop platforms. This retry should help with reducing this
    flakiness.
    
    Bug: 466009188
    Change-Id: I523eddaa65bd594a5bcd9dd13ec395dd4cd2af80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7261982
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Commit-Queue: Arkadiusz Tomczak <artomczak@google.com>
    Reviewed-by: Ramon Cano Aparicio <rcanoaparicio@google.com>
    Cr-Commit-Position: refs/heads/main@{#1559792}

--- chrome/test/variations/test_utils/helper.py
+++ chrome/test/variations/test_utils/helper.py
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 import functools
+import logging
 import platform
 import subprocess
 import threading
@@ -92,5 +93,37 @@ def timeout(seconds: float):
             else:
                 # Function finished within timeout, returning the result
                 return result_container['result']
+        return wrapper
+    return decorator
+
+def retry(count: int):
+    """
+    A decorator that reties execution of the wrapped function.
+
+    Args:
+        count: How many times the function execution should be retried.
+
+    Returns:
+        The decorated function.
+    """
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+           trial = 1
+           while True:
+              try:
+                  return func(*args, **kwargs)
+              except Exception as e:
+                  if trial < count:
+                      logging.warning(
+                         f"#{trial} execution of {func.__name__} failed " +
+                         f"with error {e!r}, " +
+                         f"remaining {count - trial} / {count} reties.")
+                      trial += 1
+                  else:
+                      logging.warning(
+                         f"#{trial} execution of {func.__name__} failed."
+                      )
+                      raise
         return wrapper
     return decorator
