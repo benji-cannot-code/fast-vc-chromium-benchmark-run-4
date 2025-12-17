3cc9521e4ece9038ddce68a9fc1cec9b63ab4339
Victor Vianna <victorvianna@google.com>
2025-12-17 12:32:04 -0800

    Remove use of C++23 std::to_underlying
    C++23 is not allowed yet
    
    Change-Id: I14cd22eb1c6cac981c283d08dcf2b8057cacf7ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7269375
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1560124}

--- chrome/browser/glic/glic_zero_state_suggestions_manager.cc
+++ chrome/browser/glic/glic_zero_state_suggestions_manager.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
+#include "base/types/cxx23_to_underlying.h"
 #include "base/types/id_type.h"
 #include "chrome/browser/contextual_cueing/caching_zero_state_suggestions_manager.h"
 #include "chrome/browser/contextual_cueing/contextual_cueing_features.h"
@@ -57,7 +58,7 @@ bool HasUserNavigationBeyondInitial(content::WebContents& web_contents) {
   // user selected default page.
   while (index > 0) {
     content::NavigationEntry* entry = controller.GetEntryAtIndex(index);
-    auto transition = std::to_underlying(entry->GetTransitionType());
+    auto transition = base::to_underlying(entry->GetTransitionType());
     bool automatic = 0 != (transition & (ui::PAGE_TRANSITION_FORWARD_BACK |
                                          ui::PAGE_TRANSITION_HOME_PAGE |
                                          ui::PAGE_TRANSITION_FROM_API |
