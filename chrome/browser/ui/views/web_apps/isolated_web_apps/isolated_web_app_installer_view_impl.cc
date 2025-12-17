e93c34e7d739d088981801ae80c1651f817af215
Magdalena Skarbinska <mskarbinska@google.com>
2025-12-17 03:42:18 -0800

    IWA: Fix IWA installer icon shape inconsistency
    Ensures the app icon has the same round shape across the installer
    views. This is achieved by consistently applying the `MaskIconOnOs`
    function to maskable icons in each view, removing the one-time
    `icon_masked_` flag that prevented subsequent views from being masked.
    
    Change-Id: I4bf89e76aaef654f2e6f5f33452dca0e87ed35cd
    Bug: 469360308
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7265246
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Commit-Queue: Magdalena Skarbińska <mskarbinska@google.com>
    Cr-Commit-Position: refs/heads/main@{#1559862}

--- chrome/browser/ui/views/web_apps/isolated_web_apps/isolated_web_app_installer_view_impl.cc
+++ chrome/browser/ui/views/web_apps/isolated_web_apps/isolated_web_app_installer_view_impl.cc
@@ -539,7 +539,7 @@ void IsolatedWebAppInstallerViewImpl::ShowMetadataScreen(
   show_metadata_view_->SetTitle(bundle_metadata.app_name());
   show_metadata_view_->SetIcon(
       CreateImageModelFromBundleMetadata(bundle_metadata));
-  if (bundle_metadata.image_info().is_maskable && !icon_masked_) {
+  if (bundle_metadata.image_info().is_maskable) {
     web_app::MaskIconOnOs(
         GetIconBitmapFromBundleMetadataToUseInDialog(bundle_metadata),
         base::BindOnce(
@@ -553,7 +553,7 @@ void IsolatedWebAppInstallerViewImpl::ShowInstallScreen(
     const SignedWebBundleMetadata& bundle_metadata) {
   install_view_->SetTitle(bundle_metadata.app_name());
   install_view_->SetIcon(CreateImageModelFromBundleMetadata(bundle_metadata));
-  if (bundle_metadata.image_info().is_maskable && !icon_masked_) {
+  if (bundle_metadata.image_info().is_maskable) {
     web_app::MaskIconOnOs(
         GetIconBitmapFromBundleMetadataToUseInDialog(bundle_metadata),
         base::BindOnce(
@@ -574,7 +574,7 @@ void IsolatedWebAppInstallerViewImpl::ShowInstallSuccessScreen(
                                      bundle_metadata.app_name());
   install_success_view_->SetIcon(
       CreateImageModelFromBundleMetadata(bundle_metadata));
-  if (bundle_metadata.image_info().is_maskable && !icon_masked_) {
+  if (bundle_metadata.image_info().is_maskable) {
     web_app::MaskIconOnOs(
         GetIconBitmapFromBundleMetadataToUseInDialog(bundle_metadata),
         base::BindOnce(
@@ -719,12 +719,10 @@ views::Widget* IsolatedWebAppInstallerViewImpl::ShowChildDialog(
 void IsolatedWebAppInstallerViewImpl::OnIconMaskedUpdateAppIcon(
     InstallerDialogView* view,
     SkBitmap masked_bitmap) {
-  CHECK(!icon_masked_);
   CHECK(!masked_bitmap.drawsNothing());
   CHECK(view);
   view->SetIcon(ui::ImageModel::FromImageSkia(
       gfx::ImageSkia::CreateFrom1xBitmap(std::move(masked_bitmap))));
-  icon_masked_ = true;
 }
 
 void IsolatedWebAppInstallerViewImpl::ShowChildView(views::View* view) {
