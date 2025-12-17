d3bbc3398e4c510bb459efac2763916f177c36a3
Tim Judkins <tjudkins@chromium.org>
2025-12-16 16:52:02 -0800

    [Extensions WebIDL] Fix comment attribution for single line definitions
    This resolves a small gotcha with the previous comment extraction in
    our new WebIDL schema format, where if a node was on the same line as
    the parent node, a comment for the parent node could be incorrectly
    attached to the child node. This only really came up with single line
    enum definitions like:
    
    // Single line enum.
    enum SingleLineEnum {"value1", "value2"};
    
    This CL adds a basic catch to avoid these, along with a test using the
    above example to make sure the enum values don't get the parent comment
    added to them.
    
    Bug: 340297705
    Change-Id: I9a21177a7bb77dbbb77e57331e03fe87f938bac7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7264494
    Commit-Queue: Tim <tjudkins@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1559631}

--- tools/json_schema_compiler/web_idl_schema_test.py
+++ tools/json_schema_compiler/web_idl_schema_test.py
@@ -508,7 +508,7 @@ class WebIdlSchemaTest(unittest.TestCase):
             'description': 'Comment on sequence type.',
         }, custom_type['properties']['booleanSequence'])
 
-    enum_expected = {
+    expected_enum = {
         'enum': [{
             'name': 'name1',
             'description': 'Comment1.'
@@ -519,7 +519,20 @@ class WebIdlSchemaTest(unittest.TestCase):
         'type': 'string',
         'id': 'EnumType'
     }
-    self.assertEqual(enum_expected, getType(schema, 'EnumType'))
+    self.assertEqual(expected_enum, getType(schema, 'EnumType'))
+
+    expected_single_line_enum = {
+        'enum': [{
+            'name': 'value1'
+        }, {
+            'name': 'value2'
+        }],
+        'description': 'Single line enum.',
+        'type': 'string',
+        'id': 'SingleLineEnum',
+    }
+    self.assertEqual(expected_single_line_enum, getType(schema,
+                                                        'SingleLineEnum'))
 
     expected_type_with_function = {
         'name': 'callbackMember',
@@ -1027,7 +1040,6 @@ class WebIdlSchemaTest(unittest.TestCase):
 
     normal_function = getFunction(schema, 'normalFunction')
     self.assertFalse(hasattr(normal_function, 'platforms'))
-
 
   # Tests that the 'implemented_in' extended attribute on an interface
   # definition is copied into the resulting namespace after processing.
