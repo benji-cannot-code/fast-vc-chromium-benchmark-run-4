8c908c213c00ee02a197029083f2a76f55fd4a4c
Duncan Mercer <mercerd@google.com>
2025-12-17 10:24:37 -0800

    Fix CSB suggestions not showing on autofocus
    Since the callback was being added while the query flow router was
    "off", the callback was being ignored. I removed this IsOff check since
    this callback is meant to be ran once the query flow "turns on".
    
    Fixed: b:467397501
    Change-Id: I4e67d06b4618de54f8c24d75705430d8d771047c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7264496
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1560035}

--- chrome/browser/ui/lens/lens_query_flow_router.cc
+++ chrome/browser/ui/lens/lens_query_flow_router.cc
@@ -122,16 +122,10 @@ LensQueryFlowRouter::GetSuggestInputs() {
 
 void LensQueryFlowRouter::SetSuggestInputsReadyCallback(
     base::RepeatingClosure callback) {
-  // If Lens in contextual tasks is enabled, setup a callback to be called when
-  // the file upload status changes, which can then be used to know if the
-  // suggest inputs are ready.
-  if (IsOff()) {
-    return;
-  }
-
   // Return the callback immediately if the suggest inputs are already ready.
   if (AreLensSuggestInputsReady(GetSuggestInputs())) {
     callback.Run();
+    return;
   }
 
   if (contextual_tasks::GetEnableLensInContextualTasks()) {
@@ -248,7 +242,7 @@ void LensQueryFlowRouter::OnFileUploadStatusChanged(
   if (!suggest_inputs.has_value()) {
     return;
   }
-  if (AreLensSuggestInputsReady(GetSuggestInputs())) {
+  if (AreLensSuggestInputsReady(*suggest_inputs)) {
     if (suggest_inputs_ready_callback_) {
       suggest_inputs_ready_callback_.Run();
     }
