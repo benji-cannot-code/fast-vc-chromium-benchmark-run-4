125aaab67e278af25d93b957bc19243add0c4187
Timofey Chudakov <tchudakov@google.com>
2025-12-17 02:40:29 -0800

    [TechDebt] Move address save/update prompt to the autofill module.
    Bypass-Check-License: files were moved.
    Bug: 460410690
    Change-Id: I76462b8b97f0fab03260fe93046ecf1724e61940
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7254756
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1559845}

--- chrome/browser/autofill/android/save_update_address_profile_prompt_controller.cc
+++ chrome/browser/autofill/android/save_update_address_profile_prompt_controller.cc
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 
 // Must come after all headers that specialize FromJniType() / ToJniType().
-#include "chrome/android/chrome_jni_headers/SaveUpdateAddressProfilePromptController_jni.h"
+#include "chrome/browser/autofill/android/jni_headers/SaveUpdateAddressProfilePromptController_jni.h"
 
 namespace autofill {
 
--- chrome/browser/ui/android/autofill/save_update_address_profile_prompt_view_android.cc
+++ chrome/browser/ui/android/autofill/save_update_address_profile_prompt_view_android.cc
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/android/window_android.h"
 
 // Must come after all headers that specialize FromJniType() / ToJniType().
-#include "chrome/android/chrome_jni_headers/SaveUpdateAddressProfilePrompt_jni.h"
+#include "chrome/browser/autofill/android/jni_headers/SaveUpdateAddressProfilePrompt_jni.h"
 
 using base::android::JavaRef;
 using base::android::ScopedJavaLocalRef;
