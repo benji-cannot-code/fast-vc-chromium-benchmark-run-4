62dd7d0fad4ca280fc7c85e0018387aadd7f9abd
Monica Salama <msalama@chromium.org>
2025-12-17 06:51:26 -0800

    Rename ProcessDiceHeaderDelegate::EnableSync to CompleteProfileSignIn
    The ProcessDiceHeaderDelegate::EnableSync method has been renamed to
    CompleteProfileSignIn. This change reflects Sync being deprecated.
    
    Bug: 417950948
    Change-Id: Ied295b6bec1fa65824d377f0248ae1dc86e29212
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7251049
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Monica Salama <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1559929}

--- chrome/browser/ui/views/profiles/profile_picker_view_browsertest.cc
+++ chrome/browser/ui/views/profiles/profile_picker_view_browsertest.cc
@@ -614,7 +614,8 @@ class ProfilePickerCreationFlowBrowserTest
     // Simulate the Dice "ENABLE_SYNC" header parameter.
     auto process_dice_header_delegate_impl =
         ProcessDiceHeaderDelegateImpl::Create(contents);
-    process_dice_header_delegate_impl->EnableSync(account_info);
+    process_dice_header_delegate_impl->CompleteChromeSignInAfterGaiaSignin(
+        account_info);
   }
 
   AccountInfo FinishDiceSignIn(
