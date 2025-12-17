4c873e99d10859569a155a2f5fc2abd718bf418e
Asami Doi <asamidoi@chromium.org>
2025-12-17 06:34:18 -0800

    [ios] Add a feature flag to use new impl written in Swift
    The new implementation written in Swift will be enabled behind this
    flag.
    
    Bug: 451945291
    Change-Id: Ic00ecd96f889f3d355d5d8e968cbe518dac5d10e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7264847
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1559919}

---
+++ ios/chrome/common/swift/features.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_COMMON_SWIFT_FEATURES_H_
+#define IOS_CHROME_COMMON_SWIFT_FEATURES_H_
+
+#import "base/feature_list.h"
+
+// Feature flag to enable new implementation of coordinators/mediators written
+// in Swift.
+BASE_DECLARE_FEATURE(kSwiftCoordinator);
+
+// Returns true if new implementation written in Swift is enabled.
+bool IsSwiftCoordinatorEnabled();
+
+#endif  // IOS_CHROME_COMMON_SWIFT_FEATURES_H_
