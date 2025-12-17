f1b3766b888ebc9d541c1cdc933608ef70dd9a11
Matt Jones <mdjones@chromium.org>
2025-12-17 14:24:08 -0800

    [Contextual Tasks] Make new thread button more reliable
    This patch removes the member variable for the embedded URL on the app
    class in favor of simply setting the src property on the webview
    element. Previously, clicking the "new thread" button more than once
    would cause it to not function - this is because, once updated to the
    default page, it does not get updated as the thread is interacted with.
    This means that when we try to set it again (i.e. clicking the button),
    it's still holding the default thread URL and no change is detected.
    The way it is done in this patch is easier than trying to keep the URL
    in the webview and the member in sync.
    
    Bug: 466159624
    Change-Id: Idbb50bc91f98e98dfc9c39c691281d907e28e606
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7267285
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1560203}

--- chrome/browser/resources/contextual_tasks/app.ts
+++ chrome/browser/resources/contextual_tasks/app.ts
@@ -42,7 +42,6 @@ export class ContextualTasksAppElement extends CrLitElement {
   static override get properties() {
     return {
       isShownInTab_: {type: Boolean},
-      threadUrl_: {type: String},
       threadTitle_: {type: String},
       contextTabs_: {type: Array},
       darkMode_: {
@@ -56,7 +55,6 @@ export class ContextualTasksAppElement extends CrLitElement {
 
   private browserProxy_: BrowserProxy = BrowserProxyImpl.getInstance();
   accessor isShownInTab_: boolean = true;  // Most start in a tab.
-  protected accessor threadUrl_: string = '';
   protected accessor darkMode_: boolean = loadTimeData.getBoolean('darkMode');
   private pendingUrl_: string = '';
   protected accessor threadTitle_: string = '';
@@ -89,7 +87,7 @@ export class ContextualTasksAppElement extends CrLitElement {
     chrome.metricsPrivate.recordBoolean(
         'ContextualTasks.WebUI.UserAction.OpenNewThread', true);
     const {url} = await this.browserProxy_.handler.getThreadUrl();
-    this.threadUrl_ = url.url;
+    this.$.threadFrame.src = url.url;
   }
 
   override async connectedCallback() {
@@ -189,7 +187,7 @@ export class ContextualTasksAppElement extends CrLitElement {
     // null, and therefore not yet set, do not load the URL.
     if (this.pendingUrl_ && this.commonSearchParams_ &&
         this.oauthToken_ != null) {
-      this.threadUrl_ = this.pendingUrl_;
+      this.$.threadFrame.src = this.pendingUrl_;
       this.pendingUrl_ = '';
     }
   }
