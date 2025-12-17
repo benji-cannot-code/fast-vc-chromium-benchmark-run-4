ae27d36d61ca41793d3a7be663c8db3442fb1c23
Devlin Cronin <rdevlin.cronin@chromium.org>
2025-12-17 10:26:41 -0800

    [Extensions] Move opener validation out of OpenTabHelper
    Only the tabs API uses the opener parameter in OpenTab(). Move
    validation of the existence of the opener out of OpenTabHelper and into
    the TabsCreateFunction.
    
    The validation of the opener for the browser and the assignment of
    opener in the newly-opened tab remains in OpenTabHelper (for now).
    
    Bug: 430344931
    Change-Id: Iddb4997a8410197883109da49c707232565e990d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7267166
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1560038}

--- chrome/browser/extensions/open_tab_helper.h
+++ chrome/browser/extensions/open_tab_helper.h
@@ -9,10 +9,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 #include <string>
 
+#include "base/memory/raw_ptr.h"
 #include "base/types/expected.h"
 #include "base/values.h"
 
 class ExtensionFunction;
+
+namespace content {
+class WebContents;
+}
 
 namespace extensions {
 
@@ -24,13 +29,14 @@ class OpenTabHelper {
 
     bool create_browser_if_needed = false;
     std::optional<int> window_id;
-    std::optional<int> opener_tab_id;
     std::optional<std::string> url;
     std::optional<bool> active;
     std::optional<bool> split;
     std::optional<bool> pinned;
     std::optional<int> index;
     std::optional<int> bookmark_id;
+
+    raw_ptr<content::WebContents> opener_tab = nullptr;
   };
 
   // Opens a new tab given an extension function `function` and creation
