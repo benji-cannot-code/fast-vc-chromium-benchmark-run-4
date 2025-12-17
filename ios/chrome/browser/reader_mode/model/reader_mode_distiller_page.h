fc31ccc35b03b9afc5fafdad15911bcbf00006c2
Nan Lin <linnan@chromium.org>
2025-12-17 03:50:42 -0800

    [Code Health] Remove DanglingUntriaged Pointer in ios/chrome/browser/reader_mode/model/reader_mode_distiller_page.h
    Bug: 446106633
    Change-Id: Ia989682863941e098337128d43d62542ba3a55c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7261006
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1559865}

--- ios/chrome/browser/reader_mode/model/reader_mode_distiller_page.h
+++ ios/chrome/browser/reader_mode/model/reader_mode_distiller_page.h
@@ -9,13 +9,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <string>
 
 #import "base/memory/weak_ptr.h"
+#import "base/scoped_observation.h"
 #import "base/values.h"
 #import "components/dom_distiller/core/distiller_page.h"
 #import "ios/web/public/web_state.h"
+#import "ios/web/public/web_state_observer.h"
 #import "url/gurl.h"
 
 // Implementation of page distillation customized for the Reader Mode feature.
-class ReaderModeDistillerPage : public dom_distiller::DistillerPage {
+class ReaderModeDistillerPage : public dom_distiller::DistillerPage,
+                                public web::WebStateObserver {
  public:
   ReaderModeDistillerPage(web::WebState* web_state);
   ~ReaderModeDistillerPage() override;
@@ -26,9 +29,14 @@ class ReaderModeDistillerPage : public dom_distiller::DistillerPage {
   dom_distiller::DistillerType GetDistillerType() override;
 
  private:
+  // web::WebStateObserver:
+  void WebStateDestroyed(web::WebState* web_state) override;
+
   void HandleJavaScriptResult(const GURL& url, const base::Value* result);
 
-  raw_ptr<web::WebState, DanglingUntriaged> web_state_;
+  raw_ptr<web::WebState> web_state_;
+  base::ScopedObservation<web::WebState, web::WebStateObserver>
+      web_state_observation_{this};
   base::WeakPtrFactory<ReaderModeDistillerPage> weak_ptr_factory_{this};
 };
 
--- ios/chrome/browser/reader_mode/model/reader_mode_distiller_page.mm
+++ ios/chrome/browser/reader_mode/model/reader_mode_distiller_page.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/reader_mode/model/reader_mode_distiller_page.h"
 
 #import "base/base64.h"
+#import "base/check.h"
 #import "base/strings/utf_string_conversions.h"
 #import "components/dom_distiller/core/dom_distiller_features.h"
 #import "components/dom_distiller/ios/distiller_page_utils.h"
@@ -15,7 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/public/web_state.h"
 
 ReaderModeDistillerPage::ReaderModeDistillerPage(web::WebState* web_state)
-    : web_state_(web_state) {}
+    : web_state_(web_state) {
+  CHECK(web_state_);
+  web_state_observation_.Observe(web_state_);
+}
 ReaderModeDistillerPage::~ReaderModeDistillerPage() = default;
 
 void ReaderModeDistillerPage::DistillPageImpl(const GURL& url,
@@ -72,4 +76,10 @@ void ReaderModeDistillerPage::HandleJavaScriptResult(
       break;
     }
   }
+}
+
+void ReaderModeDistillerPage::WebStateDestroyed(web::WebState* web_state) {
+  CHECK_EQ(web_state_, web_state);
+  web_state_observation_.Reset();
+  web_state_ = nullptr;
 }
