1c1ac4500c0f875f149e19d38737e172ff597ebc
Aviv Kiss <avivkiss@google.com>
2025-12-17 10:22:04 -0800

    [omnibox][next] Update default flag values.
    Flip kShowCreateImageTool and kShowToolsAndModels defaults to true as
    work for these flags are now complete.
    
    Bug: 469399150
    Change-Id: Ied64859fe598d5b029ffd31f6f9bcc7bf7fa1975
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7266376
    Commit-Queue: Ananya Seelam <ananyaseelam@google.com>
    Reviewed-by: Ananya Seelam <ananyaseelam@google.com>
    Auto-Submit: Aviv Kiss <avivkiss@google.com>
    Cr-Commit-Position: refs/heads/main@{#1560031}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -870,44 +870,34 @@ const FeatureEntry::FeatureVariation
 #if !BUILDFLAG(IS_ANDROID)
 const FeatureEntry::FeatureParam kWebUIOmniboxAimPopupAddContextButtonNone[] = {
     {"AddContextButtonVariant", "none"},
-    {"ShowCreateImageTool", "true"},
-    {"ShowToolsAndModels", "true"},
     {"ShowVoiceSearchInSteadyComposebox", "true"},
     {"ShowVoiceSearchInExpandedComposebox", "true"},
 };
 const FeatureEntry::FeatureParam
     kWebUIOmniboxAimPopupAddContextButtonBelowResults[] = {
         {"AddContextButtonVariant", "below_results"},
-        {"ShowCreateImageTool", "true"},
         {"ShowLensSearchChip", "true"},
-        {"ShowToolsAndModels", "true"},
         {"ShowVoiceSearchInSteadyComposebox", "true"},
         {"ShowVoiceSearchInExpandedComposebox", "true"},
 };
 const FeatureEntry::FeatureParam
     kWebUIOmniboxAimPopupAddContextButtonAboveResults[] = {
         {"AddContextButtonVariant", "above_results"},
-        {"ShowCreateImageTool", "true"},
         {"ShowLensSearchChip", "true"},
-        {"ShowToolsAndModels", "true"},
         {"ShowVoiceSearchInSteadyComposebox", "true"},
         {"ShowVoiceSearchInExpandedComposebox", "true"},
 };
 const FeatureEntry::FeatureParam kWebUIOmniboxAimPopupAddContextButtonInline[] =
     {
         {"AddContextButtonVariant", "inline"},
-        {"ShowCreateImageTool", "true"},
         {"ShowLensSearchChip", "true"},
-        {"ShowToolsAndModels", "true"},
         {"ShowVoiceSearchInSteadyComposebox", "true"},
         {"ShowVoiceSearchInExpandedComposebox", "true"},
 };
 const FeatureEntry::FeatureParam
     kWebUIOmniboxAimPopupAddContextButtonMultiFile[] = {
         {"AddContextButtonVariant", "below_results"},
-        {"ShowCreateImageTool", "true"},
         {"ShowLensSearchChip", "true"},
-        {"ShowToolsAndModels", "true"},
         {"ShowVoiceSearchInSteadyComposebox", "true"},
         {"ShowVoiceSearchInExpandedComposebox", "true"},
         {"MaxNumFiles", "5"},
@@ -915,8 +905,6 @@ const FeatureEntry::FeatureParam
 const FeatureEntry::FeatureParam
     kWebUIOmniboxAimPopupAddContextButtonBelowResultsRecentTabChip[] = {
         {"AddContextButtonVariant", "below_results"},
-        {"ShowCreateImageTool", "true"},
-        {"ShowToolsAndModels", "true"},
         {"ShowVoiceSearchInSteadyComposebox", "true"},
         {"ShowVoiceSearchInExpandedComposebox", "true"},
         {"ShowRecentTabChip", "true"},
