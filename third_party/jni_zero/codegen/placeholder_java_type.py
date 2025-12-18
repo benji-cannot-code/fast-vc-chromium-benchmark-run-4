ea3243295e80fff7d0ce6702219fe8f16f58d136
Martin Kong <martinkong@google.com>
2025-12-18 10:27:26 -0800

    JNI Zero: Add @Generated annotation to every generated java file
    This CL adds the @Generated("JNI Zero") annotation to every java file
    generated by the JNI Zero framework, to opt code out of link checks.
    
    Bug: 470060355
    Change-Id: I64c85ef98a8c6261a08788070aa72b327de8530b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7276344
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Martin Kong <martinkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1560668}

--- third_party/jni_zero/codegen/placeholder_java_type.py
+++ third_party/jni_zero/codegen/placeholder_java_type.py
@@ -26,6 +26,9 @@ def Generate(java_class,
 
 package {java_class.class_without_prefix.package_with_dots};
 
+import javax.annotation.processing.Generated;
+
+@Generated("JNI Zero")
 public interface {java_class.name} {{
 """)
   if nested_classes:
