82bb0363db53dad02cbf1ba16437bb789b1e52c7
Min Qin <qinmin@chromium.org>
2025-12-18 14:40:23 -0800

    [ContextualTask] Check uploaded context before showing automated chip
    This CL allows Chrome to check whether the tab has been uploaded,
    before showing the chip again on composebox
    
    Change-Id: I1863a05208d75e073038888d952347624eacc9d5
    Bug: 467788267
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7254652
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1560839}

--- chrome/browser/contextual_tasks/contextual_tasks_ui.cc
+++ chrome/browser/contextual_tasks/contextual_tasks_ui.cc
@@ -41,8 +41,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/generated_resources.h"
 #include "chrome/grit/theme_resources.h"
 #include "components/contextual_search/contextual_search_metrics_recorder.h"
+#include "components/contextual_tasks/public/context_decoration_params.h"
 #include "components/contextual_tasks/public/contextual_task.h"
 #include "components/contextual_tasks/public/features.h"
+#include "components/contextual_tasks/public/utils.h"
 #include "components/lens/lens_features.h"
 #include "components/omnibox/browser/aim_eligibility_service.h"
 #include "components/omnibox/browser/searchbox.mojom-forward.h"
@@ -53,6 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/identity_manager/primary_account_access_token_fetcher.h"
 #include "components/strings/grit/components_strings.h"
 #include "components/tabs/public/tab_interface.h"
+#include "components/url_deduplication/url_deduplication_helper.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/page_navigator.h"
 #include "content/public/browser/web_contents.h"
@@ -473,6 +476,47 @@ void ContextualTasksUI::OnInnerWebContentsCreated(
   embedded_web_contents_ = inner_contents->GetWeakPtr();
 }
 
+void ContextualTasksUI::OnContextRetrievedForActiveTab(
+    int32_t tab_id,
+    const GURL& last_committed_url,
+    std::unique_ptr<contextual_tasks::ContextualTaskContext> context) {
+  // Do nothing is the webUI is no longer in side panel.
+  if (IsShownInTab()) {
+    return;
+  }
+
+  // If active tab or tab URL changed since the GetContextForTask() call, do
+  // nothing.
+  tabs::TabInterface* tab = GetBrowser()->GetActiveTabInterface();
+  if (!tab || tab->GetHandle().raw_value() != tab_id ||
+      tab->GetContents()->GetLastCommittedURL() != last_committed_url) {
+    return;
+  }
+
+  // If last_committed_url is already in the context, clear the suggested tab
+  // context.
+  std::unique_ptr<url_deduplication::URLDeduplicationHelper>
+      url_duplication_helper =
+          contextual_tasks::CreateURLDeduplicationHelperForContextualTask();
+  if (context->ContainsURL(last_committed_url, url_duplication_helper.get())) {
+    composebox_handler_->UpdateSuggestedTabContext(nullptr);
+    return;
+  }
+
+  UpdateSuggestedTabContext(tab);
+}
+
+void ContextualTasksUI::UpdateSuggestedTabContext(tabs::TabInterface* tab) {
+  content::WebContents* web_contents = tab->GetContents();
+  auto tab_data = searchbox::mojom::TabInfo::New();
+  tab_data->tab_id = tab->GetHandle().raw_value();
+  tab_data->title = base::UTF16ToUTF8(web_contents->GetTitle());
+  tab_data->url = web_contents->GetLastCommittedURL();
+  tab_data->last_active = std::max(web_contents->GetLastActiveTimeTicks(),
+                                   web_contents->GetLastInteractionTimeTicks());
+  composebox_handler_->UpdateSuggestedTabContext(std::move(tab_data));
+}
+
 void ContextualTasksUI::OnSidePanelStateChanged() {
   page_->OnSidePanelStateChanged();
 
@@ -493,9 +537,7 @@ void ContextualTasksUI::OnSidePanelStateChanged() {
       // The WebUI starts showing in the side panel, show the auto suggested
       // chip if possible.
       previous_web_ui_state_ = WebUIState::kShownInSidePanel;
-      // TODO(https://crbug.com/467696560): Get the correct upload status of the
-      // current tab.
-      OnActiveTabContextStatusChanged(TabContextStatus::kNotUploaded);
+      OnActiveTabContextStatusChanged();
     }
     display_mode_msg->mutable_payload()->set_display_mode(
         lens::CobrowsingDisplayModeParams::COBROWSING_SIDEPANEL);
@@ -508,8 +550,7 @@ void ContextualTasksUI::DisableActiveTabContextSuggestion() {
   ui_service_->set_auto_tab_context_suggestion_enabled(false);
 }
 
-void ContextualTasksUI::OnActiveTabContextStatusChanged(
-    TabContextStatus status) {
+void ContextualTasksUI::OnActiveTabContextStatusChanged() {
   if (!GetBrowser()) {
     return;
   }
@@ -519,11 +560,6 @@ void ContextualTasksUI::OnActiveTabContextStatusChanged(
   }
 
   if (!ui_service_->auto_tab_context_suggestion_enabled()) {
-    return;
-  }
-
-  if (status != TabContextStatus::kNotUploaded) {
-    composebox_handler_->UpdateSuggestedTabContext(nullptr);
     return;
   }
 
@@ -542,13 +578,21 @@ void ContextualTasksUI::OnActiveTabContextStatusChanged(
     return;
   }
 
-  auto tab_data = searchbox::mojom::TabInfo::New();
-  tab_data->tab_id = tab->GetHandle().raw_value();
-  tab_data->title = base::UTF16ToUTF8(web_contents->GetTitle());
-  tab_data->url = last_committed_url;
-  tab_data->last_active = std::max(web_contents->GetLastActiveTimeTicks(),
-                                   web_contents->GetLastInteractionTimeTicks());
-  composebox_handler_->UpdateSuggestedTabContext(std::move(tab_data));
+  if (!GetContextualSessionHandle()) {
+    return;
+  }
+
+  auto context_decoration_params =
+      std::make_unique<contextual_tasks::ContextDecorationParams>();
+  context_decoration_params->contextual_search_session_handle =
+      GetContextualSessionHandle()->AsWeakPtr();
+  context_controller_->GetContextForTask(
+      GetTaskId().value(),
+      {contextual_tasks::ContextualTaskContextSource::kPendingContextDecorator},
+      std::move(context_decoration_params),
+      base::BindOnce(&ContextualTasksUI::OnContextRetrievedForActiveTab,
+                     weak_ptr_factory_.GetWeakPtr(),
+                     tab->GetHandle().raw_value(), last_committed_url));
 }
 
 void ContextualTasksUI::TransferNavigationToEmbeddedPage(
--- chrome/browser/contextual_tasks/contextual_tasks_ui.h
+++ chrome/browser/contextual_tasks/contextual_tasks_ui.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/top_chrome/top_chrome_webui_config.h"
 #include "chrome/common/webui_url_constants.h"
 #include "components/contextual_search/contextual_search_session_handle.h"
+#include "components/contextual_tasks/public/contextual_task_context.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_ui_controller.h"
@@ -51,6 +52,10 @@ namespace contextual_tasks {
 class ContextualTasksContextController;
 class ContextualTasksUiService;
 }  // namespace contextual_tasks
+
+namespace tabs {
+class TabInterface;
+}  // namespace tabs
 
 class ContextualTasksComposeboxHandler;
 class ContextualTasksInternalsPageHandler;
@@ -84,14 +89,6 @@ class ContextualTasksUI : public TaskInfoDelegate,
     raw_ptr<contextual_tasks::ContextualTasksContextController>
         context_controller_;
     raw_ref<TaskInfoDelegate> task_info_delegate_;
-  };
-
-  // Enum representing the upload status of tab context.
-  enum class TabContextStatus {
-    kNotUploaded,
-    kPendingUpload,
-    kUploaded,
-    kIgnored,
   };
 
   explicit ContextualTasksUI(content::WebUI* web_ui);
@@ -176,7 +173,7 @@ class ContextualTasksUI : public TaskInfoDelegate,
   // Called when the active tab has been changed, either a new page is loaded or
   // a title change. This is only called when the of this class is rendered in
   // the side panel.
-  void OnActiveTabContextStatusChanged(TabContextStatus status);
+  void OnActiveTabContextStatusChanged();
 
   void SetComposeboxHandlerForTesting(
       std::unique_ptr<ContextualTasksComposeboxHandler> handler) {
@@ -221,6 +218,15 @@ class ContextualTasksUI : public TaskInfoDelegate,
   // WebContents. In practice, this is the creation of the WebContents hosting
   // the embedded remote page.
   void OnInnerWebContentsCreated(content::WebContents* inner_contents);
+
+  // Called when the contextual task context is returned by the service.
+  void OnContextRetrievedForActiveTab(
+      int32_t tab_id,
+      const GURL& last_committed_url,
+      std::unique_ptr<contextual_tasks::ContextualTaskContext> context);
+
+  // Called to update the suggested tab chip on composebox.
+  void UpdateSuggestedTabContext(tabs::TabInterface* tab);
 
   // The OAuth token fetcher is used to fetch the OAuth token for the signed in
   // user. This is used to authenticate the user when making requests in the
