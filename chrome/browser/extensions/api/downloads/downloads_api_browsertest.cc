f8998a0e753514b230cdb199c214b8e2243ee630
jj <jj@imput.net>
2025-12-18 15:28:06 -0800

    Fix building with safe_browsing_mode=0 (1/14)
    Fixes the following:
    ERROR Unresolved dependencies.
    //chrome/test:platform_browser_tests(//build/toolchain/linux:clang_x64)
      needs //components/safe_browsing/content/common:file_type_policies_test_support(//build/toolchain/linux:clang_x64)
    
    Bug: 390205854
    Change-Id: Ifc28ac8280aa6b131aee555045c6ec42394dea0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7278213
    Auto-Submit: jj <jj@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1560867}

--- chrome/browser/extensions/api/downloads/downloads_api_browsertest.cc
+++ chrome/browser/extensions/api/downloads/downloads_api_browsertest.cc
@@ -56,7 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/in_process_browser_test.h"
 #include "components/download/public/common/download_item.h"
 #include "components/prefs/pref_service.h"
-#include "components/safe_browsing/content/common/file_type_policies_test_util.h"
+#include "components/safe_browsing/buildflags.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
@@ -103,6 +103,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/ui_test_utils.h"
 #if !BUILDFLAG(IS_CHROMEOS)
 #include "chrome/browser/ui/download/download_display.h"
+#endif
+
+#if BUILDFLAG(SAFE_BROWSING_AVAILABLE)
+#include "components/safe_browsing/content/common/file_type_policies_test_util.h"
 #endif
 #endif
 
@@ -3372,7 +3376,7 @@ IN_PROC_BROWSER_TEST_F(
 #endif
 }
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
+#if BUILDFLAG(ENABLE_EXTENSIONS) && BUILDFLAG(SAFE_BROWSING_AVAILABLE)
 // Tests that overriding a dangerous file extension to a safe extension will
 // trigger the dangerous prompt and will not change the extension.
 // TODO(crbug.com/405219117): Port to desktop Android when the dangerous
@@ -3451,7 +3455,7 @@ IN_PROC_BROWSER_TEST_F(
   EXPECT_EQ(downloads_directory().AppendASCII("overridden.swf"),
             item->GetTargetFilePath());
 }
-#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS) && BUILDFLAG(SAFE_BROWSING_AVAILABLE)
 
 IN_PROC_BROWSER_TEST_F(
     DownloadExtensionTest,
@@ -4627,6 +4631,7 @@ void OnDangerPromptCreated(DownloadDangerPrompt* prompt) {
   prompt->InvokeActionForTesting(DownloadDangerPrompt::ACCEPT);
 }
 
+#if BUILDFLAG(SAFE_BROWSING_AVAILABLE)
 // TODO(crbug.com/450662444): Enable this test on desktop Android when the
 // DownloadDangerPrompt is implemented.
 IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
@@ -4671,6 +4676,7 @@ IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
 
   observer->WaitForFinished();
 }
+#endif  // BUILDFLAG(SAFE_BROWSING_AVAILABLE)
 
 // Test that file deletion event is correctly generated after download
 // completion.
