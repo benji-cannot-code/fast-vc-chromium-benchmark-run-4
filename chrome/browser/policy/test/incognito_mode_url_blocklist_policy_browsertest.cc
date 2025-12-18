52be522a4455a5e5d919eb7d4df96892898fa534
Mikołaj Wałachowski <mwalachowski@google.com>
2025-12-18 03:51:10 -0800

    Rename IncognitoModeBlocklist policy to IncognitoModeUrlBlocklist
    No behavior changes.
    
    This CL covers renaming of IncognitoModeBlocklist to
    IncognitoModeUrlBlocklist. The corresponding preference names, policy
    definitions, and pref callers have also been updated.
    
    Background: The IncognitoModeBlocklist and IncognitoModeAllowlist should
    be renamed to IncognitoModeUrlBlocklist and IncognitoModeUrlAllowlist,
    respectively. This clarifies that these policies are used for URL
    filtering within Incognito mode.
    
    To ensure CL diff clarity in the renamed files, this change was split
    into 3 CLs:
    * (current) Rename IncognitoModeBlocklist: https://crrev.com/c/7265745
    * Rename IncognitoModeAllowlist: https://crrev.com/c/7265944
    * Update policies .yaml descriptions: https://crrev.com/c/7265745
    
    Bug: 442891187
    Change-Id: Id996bddf99e3b0c2516b14a3af4ea7ecd6cb3f8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7247686
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Mikołaj Wałachowski <mwalachowski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1560456}

--- chrome/browser/policy/test/incognito_mode_url_blocklist_policy_browsertest.cc
+++ chrome/browser/policy/test/incognito_mode_url_blocklist_policy_browsertest.cc
@@ -63,7 +63,7 @@ IN_PROC_BROWSER_TEST_P(IncognitoUrlBlockingPolicyTest, IncognitoBlocklist) {
   base::Value::List blocklist;
   blocklist.Append("aaa.com");
   PolicyMap policies;
-  policies.Set(key::kIncognitoModeBlocklist, POLICY_LEVEL_MANDATORY,
+  policies.Set(key::kIncognitoModeUrlBlocklist, POLICY_LEVEL_MANDATORY,
                POLICY_SCOPE_USER, POLICY_SOURCE_CLOUD,
                base::Value(std::move(blocklist)), nullptr);
   UpdateProviderPolicy(policies);
@@ -87,7 +87,7 @@ IN_PROC_BROWSER_TEST_F(IncognitoUrlBlockingPolicyTest,
   base::Value::List blocklist;
   blocklist.Append("*");
   PolicyMap policies;
-  policies.Set(key::kIncognitoModeBlocklist, POLICY_LEVEL_MANDATORY,
+  policies.Set(key::kIncognitoModeUrlBlocklist, POLICY_LEVEL_MANDATORY,
                POLICY_SCOPE_USER, POLICY_SOURCE_CLOUD,
                base::Value(std::move(blocklist)), nullptr);
 
@@ -126,7 +126,7 @@ IN_PROC_BROWSER_TEST_F(IncognitoUrlBlockingPolicyTest,
   blocklist.Append("blocked2.com");
   blocklist.Append("allowed.com");
   PolicyMap policies;
-  policies.Set(key::kIncognitoModeBlocklist, POLICY_LEVEL_MANDATORY,
+  policies.Set(key::kIncognitoModeUrlBlocklist, POLICY_LEVEL_MANDATORY,
                POLICY_SCOPE_USER, POLICY_SOURCE_CLOUD,
                base::Value(std::move(blocklist)), nullptr);
 
@@ -168,7 +168,7 @@ IN_PROC_BROWSER_TEST_P(IncognitoUrlBlockingPolicyTest,
   base::Value::List incognito_blocklist;
   incognito_blocklist.Append("blockincognito.com");
   PolicyMap policies;
-  policies.Set(key::kIncognitoModeBlocklist, POLICY_LEVEL_MANDATORY,
+  policies.Set(key::kIncognitoModeUrlBlocklist, POLICY_LEVEL_MANDATORY,
                POLICY_SCOPE_USER, POLICY_SOURCE_CLOUD,
                base::Value(std::move(incognito_blocklist)), nullptr);
 
@@ -269,7 +269,7 @@ IN_PROC_BROWSER_TEST_P(IncognitoUrlBlockingPolicyTest,
   base::Value::List incognito_blocklist;
   incognito_blocklist.Append("blockedincognito.com");
   incognito_blocklist.Append("blockedboth.com");
-  policies.Set(key::kIncognitoModeBlocklist, POLICY_LEVEL_MANDATORY,
+  policies.Set(key::kIncognitoModeUrlBlocklist, POLICY_LEVEL_MANDATORY,
                POLICY_SCOPE_USER, POLICY_SOURCE_CLOUD,
                base::Value(std::move(incognito_blocklist)), nullptr);
 
