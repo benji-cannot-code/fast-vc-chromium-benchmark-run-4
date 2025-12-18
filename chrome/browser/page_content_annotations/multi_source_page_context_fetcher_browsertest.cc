6decaa76e7414535cfdffe215b2d39be9fa906d5
Khushal Sagar <khushalsagar@chromium.org>
2025-12-18 15:44:07 -0800

    Reapply "Redact passwords from screenshots using APC"
    This reverts commit 522c6ea763b328e6434b5866d76400093bb9b790. The
    failing test is disabled on LSAN/MSAN bots.
    
    Bug: 469749590
    Change-Id: Ideef8a259e3538655f50862025784ac0c5b30adb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7277304
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1560885}

--- chrome/browser/page_content_annotations/multi_source_page_context_fetcher_browsertest.cc
+++ chrome/browser/page_content_annotations/multi_source_page_context_fetcher_browsertest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_paths.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
+#include "components/optimization_guide/content/browser/page_content_proto_provider.h"
 #include "components/page_content_annotations/core/page_content_annotations_common.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test.h"
@@ -24,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/public/mojom/content_extraction/ai_page_content.mojom.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/gfx/codec/jpeg_codec.h"
@@ -69,6 +71,11 @@ class MultiSourcePageContextFetcherBrowserTest : public InProcessBrowserTest {
   MultiSourcePageContextFetcherBrowserTest() = default;
 
   ~MultiSourcePageContextFetcherBrowserTest() override = default;
+
+  void SetUp() override {
+    EnablePixelOutput();
+    InProcessBrowserTest::SetUp();
+  }
 
   void SetUpOnMainThread() override {
     InProcessBrowserTest::SetUpOnMainThread();
@@ -453,6 +460,64 @@ IN_PROC_BROWSER_TEST_F(WebpMultiSourcePageContextFetcherBrowserTest,
   EXPECT_FALSE(screenshot.dimensions.IsZero());
   ASSERT_GT(screenshot.screenshot_data.size(), 0);
   ASSERT_EQ(screenshot.mime_type, "image/webp");
+}
+
+class PasswordRedactionMultiSourcePageContextFetcherBrowserTest
+    : public MultiSourcePageContextFetcherBrowserTest {
+ public:
+  PasswordRedactionMultiSourcePageContextFetcherBrowserTest() {
+    std::vector<base::test::FeatureRefAndParams> enabled_features{
+        {kGlicScreenshotPasswordRedaction, {}},
+    };
+    features_.InitWithFeaturesAndParameters(enabled_features,
+                                            /*disabled_features=*/{});
+  }
+
+  ~PasswordRedactionMultiSourcePageContextFetcherBrowserTest() override =
+      default;
+
+ private:
+  base::test::ScopedFeatureList features_;
+};
+
+#if defined(ADDRESS_SANITIZER) || defined(MEMORY_SANITIZER)
+// TODO(crbug.com/469749590): Fix ASAN/MSAN failures on trybot.
+#define MAYBE_BasicRedaction DISABLED_BasicRedaction
+#else
+#define MAYBE_BasicRedaction BasicRedaction
+#endif
+IN_PROC_BROWSER_TEST_F(
+    PasswordRedactionMultiSourcePageContextFetcherBrowserTest,
+    MAYBE_BasicRedaction) {
+  ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(),
+                                           GetURL(kHostA, "/password.html")));
+
+  base::test::TestFuture<FetchPageContextResultCallbackArg> future;
+
+  FetchPageContextOptions options;
+  options.annotated_page_content_options =
+      optimization_guide::DefaultAIPageContentOptions(true);
+  options.screenshot_options =
+      ScreenshotOptions::ViewportOnly(/*paint_preview_options=*/std::nullopt);
+  FetchPageContext(*web_contents(), options, nullptr, future.GetCallback());
+
+  ASSERT_OK_AND_ASSIGN(std::unique_ptr<FetchPageContextResult> result,
+                       future.Take());
+
+  ASSERT_TRUE(result);
+  ASSERT_TRUE(result->screenshot_result.has_value());
+
+  ScreenshotResult& screenshot = result->screenshot_result.value();
+  EXPECT_FALSE(screenshot.dimensions.IsZero());
+  ASSERT_GT(screenshot.screenshot_data.size(), 0);
+  ASSERT_EQ(screenshot.mime_type, "image/jpeg");
+
+  SkBitmap bitmap = gfx::JPEGCodec::Decode(screenshot.screenshot_data);
+
+  EXPECT_FALSE(bitmap.isNull());
+  EXPECT_FALSE(bitmap.empty());
+  EXPECT_THAT(bitmap.getColor(10, 10),
+              IsColorWithinTolerance(SK_ColorBLACK, 0x20));
 }
 
 }  // namespace page_content_annotations
