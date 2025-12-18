819f942cf132177e945ef01673c145d1406c5e2a
Joshua Pawlicki <waffles@chromium.org>
2025-12-18 06:07:30 -0800

    Revert "Remove EligibilityService"
    This reverts commit bf8dc1aaa383a2edb9c11a2bbe8c416f5174ce73.
    
    Reason for revert: 
    
    All/ExperimentManagerImplSyntheticTrialTest.RegistersSyntheticTrial failures on Linux MSan: https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20MSan%20Tests/60588/overview
    
    Original change's description:
    > Remove EligibilityService
    >
    > Bug: 469047728
    > Change-Id: I2c2bfa6b536c192b9f2f6471bb468557fc008176
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7265664
    > Auto-Submit: Kevin Graney <kmg@google.com>
    > Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    > Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    > Reviewed-by: Fiona Macintosh <fmacintosh@google.com>
    > Reviewed-by: Joshua Hood <jdh@chromium.org>
    > Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1560436}
    
    Bug: 469047728
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ibd1e958f80a4b9f6b8608670f6cdaef311b2020b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7275704
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1560506}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -221,6 +221,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/user_event_service_factory.h"
 #include "chrome/browser/tab_group_sync/tab_group_sync_service_factory.h"
 #include "chrome/browser/themes/theme_service_factory.h"
+#include "chrome/browser/tpcd/experiment/eligibility_service_factory.h"
 #include "chrome/browser/translate/translate_ranker_factory.h"
 #include "chrome/browser/ui/autofill/autofill_client_provider_factory.h"
 #include "chrome/browser/ui/find_bar/find_bar_state_factory.h"
@@ -1401,6 +1402,7 @@ void ChromeBrowserMainExtraPartsProfiles::
   ToolbarActionsModelFactory::GetInstance();
 #endif
   TopSitesFactory::GetInstance();
+  tpcd::experiment::EligibilityServiceFactory::GetInstance();
   TrackingProtectionOnboardingFactory::GetInstance();
   TrackingProtectionSettingsFactory::GetInstance();
   translate::TranslateRankerFactory::GetInstance();
--- chrome/browser/tpcd/experiment/experiment_manager_impl_browsertest.cc
+++ chrome/browser/tpcd/experiment/experiment_manager_impl_browsertest.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "base/functional/callback_helpers.h"
 #include "base/location.h"
 #include "base/run_loop.h"
 #include "base/strings/to_string.h"
@@ -54,9 +53,7 @@ struct SyntheticTrialTestCase {
 };
 
 constexpr char kEligibleGroupName[] = "eligible";
-#if !BUILDFLAG(IS_CHROMEOS)
 constexpr char kOverrideGroupName[] = "override";
-#endif
 
 class ExperimentManagerImplBrowserTest : public InProcessBrowserTest {
  public:
@@ -103,7 +100,6 @@ class ExperimentManagerImplBrowserTest : public InProcessBrowserTest {
 
 // Android does not support PRE_ tests.
 #if !BUILDFLAG(IS_ANDROID)
-#if !BUILDFLAG(IS_CHROMEOS)
 class ExperimentManagerImplSyntheticTrialTest
     : public ExperimentManagerImplBrowserTest,
       public testing::WithParamInterface<SyntheticTrialTestCase> {
@@ -137,15 +133,6 @@ IN_PROC_BROWSER_TEST_P(ExperimentManagerImplSyntheticTrialTest,
 
 IN_PROC_BROWSER_TEST_P(ExperimentManagerImplSyntheticTrialTest,
                        RegistersSyntheticTrial) {
-  // TODO(crbug.com/469047728): Explore deletion of this test.
-  // Client eligibility is forced for this test because the EligibilityService
-  // was removed.
-  if (auto* manager =
-          ExperimentManagerImpl::GetForProfile(browser()->profile())) {
-    manager->SetClientEligibility(GetParam().new_state_eligible,
-                                  base::DoNothing());
-  }
-
   // Delay to make sure `CaptureEligibilityInLocalStatePref` has run.
   Wait();
 
@@ -304,7 +291,6 @@ IN_PROC_BROWSER_TEST_F(ExperimentManagerImplDisable3PCsSyntheticTrialTest,
   ASSERT_NE(group_name_hash, 0u);
   EXPECT_EQ(group_name_hash, HashName(kEligibleGroupName));
 }
-#endif  // !BUILDFLAG(IS_CHROMEOS)
 
 class ExperimentManagerImplSilentOnboardingSyntheticTrialTest
     : public ExperimentManagerImplBrowserTest {
@@ -316,6 +302,7 @@ class ExperimentManagerImplSilentOnboardingSyntheticTrialTest
             /*need_onboarding=*/true,
             /*enable_silent_onboarding=*/true) {}
 };
+
 IN_PROC_BROWSER_TEST_F(ExperimentManagerImplSilentOnboardingSyntheticTrialTest,
                        PRE_ExistingProfilesRegistersSyntheticTrial) {
   Wait();
