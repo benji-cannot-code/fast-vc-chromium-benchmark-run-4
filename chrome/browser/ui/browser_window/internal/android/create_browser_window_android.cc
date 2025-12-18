231e163969e2ac99f7f8a86571a146acf35b1ca5
Jack Thiesen <jthiesen@chromium.org>
2025-12-18 13:40:43 -0800

    Android's GetBrowserWindowCreationStatusForProfile() Checks for IncognitoModeAvailability
    Bug: 467705292
    Change-Id: I8d81beb58cb034045c92a5137e0051d42a6c6f3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7277169
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Jack Thiesen <jthiesen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1560803}

--- chrome/browser/ui/browser_window/internal/android/create_browser_window_android.cc
+++ chrome/browser/ui/browser_window/internal/android/create_browser_window_android.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/notimplemented.h"
+#include "chrome/browser/prefs/incognito_mode_prefs.h"
+#include "chrome/browser/profiles/nuke_profile_directory_utils.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser_window/internal/jni/AndroidBrowserWindowCreateParamsImpl_jni.h"
 #include "chrome/browser/ui/browser_window/internal/jni/BrowserWindowCreatorBridge_jni.h"
@@ -60,6 +62,12 @@ void CreateBrowserWindow(
 BrowserWindowInterface::CreationStatus GetBrowserWindowCreationStatusForProfile(
     Profile& profile) {
   if (profile.ShutdownStarted()) {
+    return BrowserWindowInterface::CreationStatus::kErrorShuttingDown;
+  }
+
+  if (!IncognitoModePrefs::CanOpenBrowser(&profile) ||
+      !profile.AllowsBrowserWindows() ||
+      IsProfileDirectoryMarkedForDeletion(profile.GetPath())) {
     return BrowserWindowInterface::CreationStatus::kErrorProfileUnsuitable;
   }
 
