d8af804e8cc53fe650ce2c0ac57463278574de9d
Dominik Röttsches <drott@chromium.org>
2025-12-18 02:04:54 -0800

    Increase WOFF2 hardcoded limit to 128MB
    This brings it inline with the preparations we did for that in
    WebFontDecoder. We missed that woff2 itself may block the decoding. I
    looked into piping this through from WebFontDecoder through OTS to the
    Woff2 Ouput buffer, but the maximum buffer value is not passed down as
    arguments to OTS, so changing the hard limit here is easier.
    
    Bug: 40233227, 453663666
    Change-Id: Idcc0772028609045cb7dcfccbaf288b1ab3a5a4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7246091
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Auto-Submit: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1560418}

--- third_party/woff2/include/woff2/output.h
+++ third_party/woff2/include/woff2/output.h
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace woff2 {
 
 // Suggested max size for output.
-const size_t kDefaultMaxSize = 30 * 1024 * 1024;
+const size_t kDefaultMaxSize = 128 * 1024 * 1024;
 
 /**
  * Output interface for the woff2 decoding.
