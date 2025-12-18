97ca328bc6555fa5de9902c393d805eb326bc88d
Joey Arhar <jarhar@chromium.org>
2025-12-17 16:09:35 -0800

    Rename CustomizableSelectInPage to CustomizableSelectListbox
    Listbox is a better term to describe this feature than "in page":
    1. The HTML spec defines this rendering mode as "list box":
       https://html.spec.whatwg.org/#list-box
    2. ARIA defines this widget as a "listbox":
       https://w3c.github.io/aria/#listbox
    3. Component libraries call this a "listbox":
       - https://react-spectrum.adobe.com/react-aria/ListBox.html
       - https://primereact.org/listbox/
       - https://headlessui.com/react/listbox
    
    Bug: 357649033
    Change-Id: I436f623ea91ba12b2be82c5522da0815e5472a25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7262921
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1560263}

--- third_party/blink/renderer/core/html/forms/html_option_element.cc
+++ third_party/blink/renderer/core/html/forms/html_option_element.cc
@@ -145,7 +145,7 @@ FocusableState HTMLOptionElement::SupportsFocus(
     bool base_with_picker =
         select->UsesMenuList() && popover && popover->popoverOpen();
     bool base_in_page =
-        RuntimeEnabledFeatures::CustomizableSelectInPageEnabled() &&
+        RuntimeEnabledFeatures::CustomizableSelectListboxEnabled() &&
         !select->UsesMenuList() && select->IsAppearanceBase();
     if (base_with_picker || base_in_page) {
       // If this option is being rendered as regular web content inside a
@@ -167,7 +167,7 @@ bool HTMLOptionElement::IsKeyboardFocusableSlow(
   if (!HTMLElement::IsKeyboardFocusableSlow(update_behavior)) {
     return false;
   }
-  if (!RuntimeEnabledFeatures::CustomizableSelectInPageEnabled() ||
+  if (!RuntimeEnabledFeatures::CustomizableSelectListboxEnabled() ||
       !OwnerSelectElement() || OwnerSelectElement()->UsesMenuList()) {
     return true;
   }
@@ -572,7 +572,7 @@ void HTMLOptionElement::DefaultEventHandlerInternal(Event& event) {
   }
 
   const bool appearance_base_in_page =
-      RuntimeEnabledFeatures::CustomizableSelectInPageEnabled() &&
+      RuntimeEnabledFeatures::CustomizableSelectListboxEnabled() &&
       !select->UsesMenuList() && select->IsAppearanceBase();
 
   if (!appearance_base_in_page && !select->PickerIsPopover()) {
