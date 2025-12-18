e19153274ba4f69cf7bbd5f8e3faaf95ca15154e
Piotr Kotynia <piotrkotynia@google.com>
2025-12-18 05:35:54 -0800

    [Android] Migrate annotations from androidx to chromium in autofill code
    Bug: 389129271
    Change-Id: I7c2552de0f551ac80d6ecfc420b7c39a451b9d3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7265251
    Auto-Submit: Piotr Kotynia <piotrkotynia@google.com>
    Commit-Queue: Piotr Kotynia <piotrkotynia@google.com>
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1560483}

--- chrome/android/junit/src/org/chromium/chrome/browser/autofill/AutofillImageFetcherTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/autofill/AutofillImageFetcherTest.java
@@ -22,7 +22,6 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Handler;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.content.res.AppCompatResources;
 import androidx.test.filters.SmallTest;
 
@@ -44,6 +43,7 @@ import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.HistogramWatcher;
+import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.browser.autofill.AutofillUiUtils.IconSpecs;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.components.autofill.ImageSize;
