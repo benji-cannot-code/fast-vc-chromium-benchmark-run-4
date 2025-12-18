f6c7ba175c4499e23014d95aeb9a507163f80f10
Dibyajyoti Pal <dibyapal@google.com>
2025-12-18 11:04:56 -0800

    [PWA] Suppress fatal asserts in BTMDatabase from SQLITE I/O errors
    The BTMDatabase is initialized for different profiles, and deleting
    them in the middle of testing fails with a
    `SQLITE_IOERR_DELETE_NOENT` error while running SQLITE queries as
    part of initialization, as a result of calling
    sql::Database::IsExpectedSqliteError().
    
    Since crrev.com/c/7264555 has gone in, the guidance on the exact
    logging mechanisms for these errors have changed, so this CL
    updates the BTMDatabase to use DLOG(ERROR)s instead of fatal
    assertions.
    
    As a consequence, this fixes flakiness in ALL profile deletion
    tests in Chrome which fail with I/O errors in the BTMDatabase
    initialization, with
    WebAppProfileDeletionTest_WebContentsGracefulShutdown being one of
    them.
    
    Fixed: 454830629
    Change-Id: I7f42d39915166eb68821035c3aeda1a9c8809eea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7263684
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Joshua Hood <jdh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1560691}

--- chrome/browser/ui/web_applications/web_app_profile_deletion_browsertest.cc
+++ chrome/browser/ui/web_applications/web_app_profile_deletion_browsertest.cc
@@ -122,8 +122,8 @@ class WebAppProfileDeletionBrowserTest : public WebAppBrowserTestBase {
 
     ProfileDestructionWaiter destruction_waiter(&profile_to_delete);
     profile_manager->GetDeleteProfileHelper().MaybeScheduleProfileForDeletion(
-        profile_to_delete.GetPath(), base::DoNothing(),
-        ProfileMetrics::DELETE_PROFILE_SETTINGS);
+        profile_path_to_delete, base::DoNothing(),
+        ProfileMetrics::DELETE_PROFILE_USER_MANAGER);
     destruction_waiter.Wait();
 
     return deleting_web_contents;
@@ -318,14 +318,8 @@ IN_PROC_BROWSER_TEST_F(WebAppProfileDeletionTest_WebContentsGracefulShutdown,
             webapps::WebAppUrlLoaderResult::kFailedWebContentsDestroyed);
 }
 
-// Flaky on Linux, see https://crbug.com/454830629.
-#if BUILDFLAG(IS_LINUX)
-#define MAYBE_IconDownloading DISABLED_IconDownloading
-#else
-#define MAYBE_IconDownloading IconDownloading
-#endif
 IN_PROC_BROWSER_TEST_F(WebAppProfileDeletionTest_WebContentsGracefulShutdown,
-                       MAYBE_IconDownloading) {
+                       IconDownloading) {
   WebAppIconDownloader icon_downloader;
 
   std::unique_ptr<content::WebContents> deleting_web_contents =
