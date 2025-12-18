91c7000b5b1641e9f8592922c722cac7945e80e1
Olivia Saul <osaul@google.com>
2025-12-18 15:31:18 -0800

    [AF] Clean up multiple request flag for VCN enrollment
    This CL is a reland of http://crrev.com/c/7206766 with fixes made as
    of patch set 2 to mitigate missed nullptrs.
    
    This CL cleans up the
    kAutofillEnableMultipleRequestInVirtualCardDownstreamEnrollment flag
    since Siyu is out until Q2 2026.
    
    Fixed: 432089598
    Change-Id: I0ac5934f7e19f21c9c5d94ff30a7a59cabc8fb4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7267176
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Qihui Zhao <qihuizhao@google.com>
    Commit-Queue: Olivia Saul <osaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1560870}

--- components/autofill/core/browser/payments/credit_card_save_manager_unittest.cc
+++ components/autofill/core/browser/payments/credit_card_save_manager_unittest.cc
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/payments/payments_autofill_client.h"
 #include "components/autofill/core/browser/payments/payments_customer_data.h"
 #include "components/autofill/core/browser/payments/payments_util.h"
+#include "components/autofill/core/browser/payments/test/mock_multiple_request_payments_network_interface.h"
 #include "components/autofill/core/browser/payments/test/mock_virtual_card_enrollment_manager.h"
 #include "components/autofill/core/browser/payments/test_credit_card_save_manager.h"
 #include "components/autofill/core/browser/payments/test_legal_message_line.h"
@@ -327,9 +328,15 @@ class CreditCardSaveManagerTest
         std::make_unique<payments::TestPaymentsNetworkInterface>(
             autofill_client().GetURLLoaderFactory(),
             autofill_client().GetIdentityManager(), &personal_data()));
+    payments_autofill_client().set_multiple_request_payments_network_interface(
+        std::make_unique<
+            autofill::payments::MockMultipleRequestPaymentsNetworkInterface>(
+            autofill_client().GetURLLoaderFactory(),
+            *autofill_client().GetIdentityManager()));
     payments_autofill_client().set_virtual_card_enrollment_manager(
         std::make_unique<MockVirtualCardEnrollmentManager>(
-            &payments_data_manager(), &payments_network_interface(),
+            &payments_data_manager(),
+            &multiple_request_payments_network_interface(),
             &autofill_client()));
     payments_autofill_client().SetLocalSaveCallbackOfferDecision(
         SaveCardOfferUserDecision::kAccepted);
@@ -540,6 +547,11 @@ class CreditCardSaveManagerTest
   payments::TestPaymentsNetworkInterface& payments_network_interface() {
     return static_cast<payments::TestPaymentsNetworkInterface&>(
         *payments_autofill_client().GetPaymentsNetworkInterface());
+  }
+  payments::MockMultipleRequestPaymentsNetworkInterface&
+  multiple_request_payments_network_interface() {
+    return *payments_autofill_client()
+                .GetMultipleRequestPaymentsNetworkInterface();
   }
   TestStrikeDatabase& strike_database() {
     return *autofill_client().GetStrikeDatabase();
