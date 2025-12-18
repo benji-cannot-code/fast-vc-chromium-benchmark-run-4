2ca28c89194c9acfd836c2a6406e46a8ace38afe
Khalid Peer <khalidpeer@chromium.org>
2025-12-18 11:35:10 -0800

    [omnibox-next] Use `IsContextSharingEnabled()` to gate Omnibox Next.
    In order to comply with the latest enterprise policies eng requirements,
    this CL adds logic to gate the overall Omnibox Next feature ("Add
    Context" button, AIM popup, etc.) using the
    `ContextualSearchService::IsContextSharingEnabled()` static method.
    Under the hood, this method reads the value of the
    `SearchContentSharingSettings` enterprise policy pref.
    
    BEFORE: http://screencast/cast/NTk2NjMwMTc2Njg3NzE4NHw5MzYyYjNiMy0xNw
    AFTER:  http://screencast/cast/NjM2NzAxNDAyODMxMjU3NnxlNTZiZTI4My00Ng
    
    Bug: b:469148777
    Change-Id: Ic9ddd5c313f9b50299b9d53a289cc6b83b966859
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7266844
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Khalid Peer <khalidpeer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1560713}

--- chrome/browser/ui/omnibox/omnibox_next_features.cc
+++ chrome/browser/ui/omnibox/omnibox_next_features.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/search_engines/template_url_service_factory.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/contextual_search/contextual_search_metrics_recorder.h"
+#include "components/contextual_search/contextual_search_service.h"
 #include "components/omnibox/browser/aim_eligibility_service.h"
 #include "ui/base/l10n/l10n_util.h"
 
@@ -174,7 +175,13 @@ bool IsAimPopupEnabled(Profile* profile) {
   }
 
   auto* aim_service = AimEligibilityServiceFactory::GetForProfile(profile);
-  return aim_service && aim_service->IsAimEligible();
+  // TODO(b/469148777): Implement more granular enterprise policy checks.
+  //   As noted in b/469148777#comment2, gating the entire AIM popup by the
+  //   "context sharing" enterprise policy pref is a stopgap measure, until
+  //   we're able to implement more granular checks in M145.
+  return aim_service && aim_service->IsAimEligible() &&
+         contextual_search::ContextualSearchService::IsContextSharingEnabled(
+             profile->GetPrefs());
 }
 
 bool IsCreateImagesEnabled(Profile* profile) {
