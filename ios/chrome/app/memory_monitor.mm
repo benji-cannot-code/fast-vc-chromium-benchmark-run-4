633a936dcf92f283f2d32201cd39b80283846e72
Federica Germinario <fedegermi@google.com>
2025-12-18 09:07:05 -0800

    [ios] Use umbrella headers for system frameworks
    Upodate code to use umbrella headers for system frameworks.
    A presubmit check will be added in a follow-up CL.
    
    Bug: 404539984
    Change-Id: I80b3da1a86f7e14a208089819610b4af67d8466b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7269684
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1560627}

--- ios/chrome/app/memory_monitor.mm
+++ ios/chrome/app/memory_monitor.mm
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/app/memory_monitor.h"
 
-#import <Foundation/NSPathUtilities.h>
+#import <Foundation/Foundation.h>
 #import <dispatch/dispatch.h>
 
 #import "base/apple/foundation_util.h"
