aff090d9f9eea8230fe55e77f0b932d7c66032e5
David Bokan <bokan@chromium.org>
2025-12-18 13:16:26 -0800

    Add journal logs helpful for unresponsive renderers
    To help debug cases timeout cases it'd be helpful to know whether the
    actor-related mojom messages were received/scheduled. Add some logs and
    add `SendLogBuffer` so we immediately IPC back any pending logs.
    
    Bug: b:467336183
    Change-Id: If2dab2de6984575b7f542376862b4316d8c732e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7277524
    Commit-Queue: David Bokan <bokan@chromium.org>
    Reviewed-by: William Liu <liuwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1560784}

--- chrome/renderer/actor/tool_executor.cc
+++ chrome/renderer/actor/tool_executor.cc
@@ -53,6 +53,12 @@ ToolExecutor::~ToolExecutor() {
 
 void ToolExecutor::InvokeTool(mojom::ToolInvocationPtr invocation,
                               ToolExecutorCallback callback) {
+  journal_->Log(invocation->task_id, "ToolExecutor::InvokeTool Received", {});
+
+  // Send the buffer now so the journal shows we received the message. This
+  // helps when debugging unresponsive renderers.
+  journal_->SendLogBuffer();
+
   if (tool_) {
     std::move(callback).Run(
         MakeResult(mojom::ActionResultCode::kExecutorBusy,
@@ -167,6 +173,12 @@ void ToolExecutor::CancelTool(const actor::TaskId& task_id) {
   journal_->Log(
       task_id, "ToolExecutor::CancelTool",
       JournalDetailsBuilder().Add("tool_already_finished", !tool_).Build());
+
+  // Send the buffer now so the journal shows we received the message. This
+  // helps when debugging unresponsive renderers.
+  // TODO(b/467336183): This instance should be removed once this bug is
+  // resolved.
+  journal_->SendLogBuffer();
 
   weak_ptr_factory_.InvalidateWeakPtrs();
   if (!tool_) {
