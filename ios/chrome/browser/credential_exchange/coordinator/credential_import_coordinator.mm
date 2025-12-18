c54de6498c8f6606b6a71bf3c5825987c92ad24c
Rafał Godlewski <rgod@google.com>
2025-12-18 08:31:34 -0800

    [iOS][CX] Add generic error alert
    Adds displaying a generic "trouble importing your data" alert. For now
    only displays is it when no trusted vault keys were fetched for passkey
    import. This should generally not happen, but rather than crash, for now
    the code will display alert and add NOTREACHED(NotFatalUntil::150).
    
    Later on, this could both be refactored into a CHECK.
    
    https://screenshot.googleplex.com/BHDbu5LGA5yRqDo
    
    Bug: 450982128
    Change-Id: I92a9aa34419923f31fa751454bec14cb16e12fb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7269724
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1560597}

--- ios/chrome/browser/credential_exchange/coordinator/credential_import_coordinator.mm
+++ ios/chrome/browser/credential_exchange/coordinator/credential_import_coordinator.mm
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/credential_exchange/coordinator/credential_import_coordinator.h"
 
+#import <UIKit/UIKit.h>
+
+#import "base/not_fatal_until.h"
 #import "base/notreached.h"
 #import "components/keyed_service/core/service_access_type.h"
 #import "components/metrics/metrics_pref_names.h"
@@ -151,20 +154,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       IDS_IOS_CREDENTIAL_EXCHANGE_NOTHING_IMPORTED_TITLE);
   NSString* message = l10n_util::GetNSString(
       IDS_IOS_CREDENTIAL_EXCHANGE_NOTHING_IMPORTED_MESSAGE);
-  _alertCoordinator = [[AlertCoordinator alloc]
-      initWithBaseViewController:self.baseViewController
-                         browser:self.browser
-                           title:title
-                         message:message];
-  __weak id<CredentialImportCoordinatorDelegate> weakDelegate = _delegate;
-  __weak __typeof(self) weakSelf = self;
-  [_alertCoordinator
-      addItemWithTitle:l10n_util::GetNSString(IDS_CLOSE)
-                action:^{
-                  [weakDelegate credentialImportCoordinatorDidFinish:weakSelf];
-                }
-                 style:UIAlertActionStyleCancel];
-  [_alertCoordinator start];
+  [self showAlertWithTitle:title
+                   message:message
+        baseViewController:self.baseViewController];
 }
 
 - (void)showConflictResolutionScreenWithPasswords:
@@ -329,7 +321,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)onTrustedVaultKeysFetched:(NSArray<NSData*>*)trustedVaultKeys {
   [_navigationController popToViewController:_viewController animated:YES];
   if (trustedVaultKeys.count == 0) {
-    // TODO(crbug.com/450982128): Handle error.
+    NSString* title =
+        l10n_util::GetNSString(IDS_IOS_CREDENTIAL_EXCHANGE_GENERIC_ERROR_TITLE);
+    [self showAlertWithTitle:title
+                     message:nil
+          baseViewController:_viewController];
+    NOTREACHED(base::NotFatalUntil::M150);
     return;
   }
 
@@ -366,6 +363,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                            authOnStart:NO];
   _reauthCoordinator.delegate = self;
   [_reauthCoordinator start];
+}
+
+// Starts the alert coordinator with `title` and `message` and initializes it
+// with `baseViewController`.
+- (void)showAlertWithTitle:(NSString*)title
+                   message:(NSString*)message
+        baseViewController:(UIViewController*)baseViewController {
+  _alertCoordinator =
+      [[AlertCoordinator alloc] initWithBaseViewController:baseViewController
+                                                   browser:self.browser
+                                                     title:title
+                                                   message:message];
+  __weak id<CredentialImportCoordinatorDelegate> weakDelegate = _delegate;
+  __weak __typeof(self) weakSelf = self;
+  [_alertCoordinator
+      addItemWithTitle:l10n_util::GetNSString(IDS_CLOSE)
+                action:^{
+                  [weakDelegate credentialImportCoordinatorDidFinish:weakSelf];
+                }
+                 style:UIAlertActionStyleCancel];
+  [_alertCoordinator start];
 }
 
 @end
