b8139f39b2e595acdf63f5da3bf3547a4acf7c0a
Shu Yang <shuyng@google.com>
2025-12-17 16:37:16 -0800

    Cache isTablet information after #attachBaseContext
    This is a short term fix until we propagate activity context for
    desktop-like incognito feature check.
    
    Bug: 467768341
    Change-Id: Ie3fc36551fe3dedd310a61c01499ea4b92bbeaa4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7261484
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Shu Yang <shuyng@google.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1560275}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -691,6 +691,7 @@ public class ChromeTabbedActivity extends ChromeActivity implements PreAttachInt
             setHasIncognitoExtra(getIntent());
         }
         super.attachBaseContext(newBase);
+        IncognitoUtils.initializeTabletStatus(this);
     }
 
     @Override
