b48c3bf3ded79d224e5ddfd6a2e9fbb4037057a8
Marc Treib <treib@chromium.org>
2025-12-18 07:34:52 -0800

    DeviceStatisticsRequest: Move Impl class into own files
    Previous CL https://crrev.com/c/7268767 split out interface vs impl.
    This CL moves the impl into its own separate files.
    
    Bug: 465716865
    Change-Id: Ib63039eb5e35eaac7521140148f57e416a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7274483
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Auto-Submit: Marc Treib <treib@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1560548}

--- components/sync/service/device_statistics_request_impl_unittest.cc
+++ components/sync/service/device_statistics_request_impl_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/sync/service/device_statistics_request.h"
+#include "components/sync/service/device_statistics_request_impl.h"
 
 #include "base/test/task_environment.h"
 #include "base/test/test_future.h"
@@ -22,9 +22,9 @@ namespace {
 const char kTestEmail[] = "test@gmail.com";
 const char kTestUrl[] = "https://test.com";
 
-class DeviceStatisticsRequestTest : public testing::Test {
+class DeviceStatisticsRequestImplTest : public testing::Test {
  public:
-  DeviceStatisticsRequestTest()
+  DeviceStatisticsRequestImplTest()
       : shared_url_loader_factory_(
             base::MakeRefCounted<network::WeakWrapperSharedURLLoaderFactory>(
                 &test_url_loader_factory_)) {
@@ -69,7 +69,7 @@ class DeviceStatisticsRequestTest : public testing::Test {
   CoreAccountInfo account_info_;
 };
 
-TEST_F(DeviceStatisticsRequestTest, ShouldSucceed) {
+TEST_F(DeviceStatisticsRequestImplTest, ShouldSucceed) {
   SetOkResponse("test_client");
 
   DeviceStatisticsRequestImpl request(identity_test_env_.identity_manager(),
@@ -89,7 +89,7 @@ TEST_F(DeviceStatisticsRequestTest, ShouldSucceed) {
   EXPECT_EQ("test_client", request.GetResults()[0].client_name());
 }
 
-TEST_F(DeviceStatisticsRequestTest, ShouldHandleAuthError) {
+TEST_F(DeviceStatisticsRequestImplTest, ShouldHandleAuthError) {
   DeviceStatisticsRequestImpl request(identity_test_env_.identity_manager(),
                                       shared_url_loader_factory_, "user_agent",
                                       account_info_, GURL(kTestUrl));
@@ -102,7 +102,7 @@ TEST_F(DeviceStatisticsRequestTest, ShouldHandleAuthError) {
   EXPECT_EQ(DeviceStatisticsRequest::State::kFailed, request.GetState());
 }
 
-TEST_F(DeviceStatisticsRequestTest, ShouldHandleNetworkError) {
+TEST_F(DeviceStatisticsRequestImplTest, ShouldHandleNetworkError) {
   auto head = network::mojom::URLResponseHead::New();
   head->headers = base::MakeRefCounted<net::HttpResponseHeaders>(
       "HTTP/1.1 500 Internal Server Error\nContent-Type: "
@@ -125,7 +125,7 @@ TEST_F(DeviceStatisticsRequestTest, ShouldHandleNetworkError) {
   EXPECT_EQ(DeviceStatisticsRequest::State::kFailed, request.GetState());
 }
 
-TEST_F(DeviceStatisticsRequestTest, ShouldRetryOnUnauthorized) {
+TEST_F(DeviceStatisticsRequestImplTest, ShouldRetryOnUnauthorized) {
   DeviceStatisticsRequestImpl request(identity_test_env_.identity_manager(),
                                       shared_url_loader_factory_, "user_agent",
                                       account_info_, GURL(kTestUrl));
@@ -157,7 +157,7 @@ TEST_F(DeviceStatisticsRequestTest, ShouldRetryOnUnauthorized) {
   EXPECT_EQ("test_client", request.GetResults()[0].client_name());
 }
 
-TEST_F(DeviceStatisticsRequestTest, ShouldRetryOnlyOnceOnUnauthorized) {
+TEST_F(DeviceStatisticsRequestImplTest, ShouldRetryOnlyOnceOnUnauthorized) {
   // The server always responds "unauthorized".
   SetUnauthorizedResponse();
 
