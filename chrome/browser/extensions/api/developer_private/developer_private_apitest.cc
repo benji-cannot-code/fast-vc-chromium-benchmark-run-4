7a4eb98145f3164572e1e40d1182c5d9044df850
Kay Lin <kaiyilin@google.com>
2025-12-17 18:36:02 -0800

    extension: UninstallMultipleExtensions test
    On Android, we do not support the dialog delegate thus we're not able to
    get pass the dialog triggered by the remove extension action. However,
    there is an `accept_bubble_for_testing` argument which should be a
    workaround for testing. This CL replace the window dialog logic with the
    `accecpt_bubble_for_testing` argument.
    
    Bug: 439447971
    Change-Id: Ic123d2e5b6293c346bb6d87dd93ae4e0c18789ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7267372
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Commit-Queue: Kay Lin <kaiyilin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1560338}

--- chrome/browser/extensions/api/developer_private/developer_private_apitest.cc
+++ chrome/browser/extensions/api/developer_private/developer_private_apitest.cc
@@ -48,12 +48,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/app_window/app_window_registry.h"
 #endif  // BUILDFLAG(ENABLE_PLATFORM_APPS)
 
-// TODO(crbug.com/439447971): Enable on desktop android.
-#if BUILDFLAG(ENABLE_EXTENSIONS)
+#if !BUILDFLAG(IS_ANDROID)
 #include "chrome/browser/ui/browser.h"
 #include "ui/views/widget/widget_delegate.h"
 #include "ui/views/window/dialog_delegate.h"
-#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
+#endif  // !BUILDFLAG(IS_ANDROID)
 
 static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
@@ -493,8 +492,6 @@ IN_PROC_BROWSER_TEST_F(DeveloperPrivateApiTest, InspectOffscreenDocument) {
   DevToolsWindowTesting::CloseDevToolsWindowSync(dev_tools_window);
 }
 
-// TODO(crbug.com/439447971): Enable on desktop android.
-#if BUILDFLAG(ENABLE_EXTENSIONS)
 IN_PROC_BROWSER_TEST_F(DeveloperPrivateApiTest, UninstallMultipleExtensions) {
   // Load first extension.
   static constexpr char kManifest_0[] =
@@ -524,23 +521,16 @@ IN_PROC_BROWSER_TEST_F(DeveloperPrivateApiTest, UninstallMultipleExtensions) {
 
   auto function = base::MakeRefCounted<
       api::DeveloperPrivateRemoveMultipleExtensionsFunction>();
-  std::unique_ptr<ExtensionFunctionDispatcher> dispatcher(
-      new ExtensionFunctionDispatcher(profile()));
+  std::unique_ptr<ExtensionFunctionDispatcher> dispatcher =
+      std::make_unique<ExtensionFunctionDispatcher>(profile());
   function->SetDispatcher(dispatcher->AsWeakPtr());
 
   std::string args =
       base::StrCat({"[[\"", extension_0_id, "\", \"", extension_1_id, "\"]]"});
-  function->SetArgs(base::test::ParseJsonList(args));
-
-  // Create a waiter to wait for the uninstall dialog to show up.
-  views::NamedWidgetShownWaiter waiter(views::test::AnyWidgetTestPasskey{},
-                                       "ExtensionMultipleUninstallDialog");
   api_test_utils::SendResponseHelper response_helper(function.get());
-
+  function->SetArgs(base::test::ParseJsonList(args));
+  function->accept_bubble_for_testing(true);
   function->RunWithValidation().Execute();
-
-  auto* widget = waiter.WaitIfNeededAndGet();
-  widget->widget_delegate()->AsDialogDelegate()->AcceptDialog();
   response_helper.WaitForResponse();
 
   // Verify the extensions are uninstalled.
@@ -549,7 +539,6 @@ IN_PROC_BROWSER_TEST_F(DeveloperPrivateApiTest, UninstallMultipleExtensions) {
   EXPECT_FALSE(extension_registry()->GetExtensionById(
       extension_1_id, ExtensionRegistry::EVERYTHING));
 }
-#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
 class DeveloperPrivateApiWithMV2DeprecationApiTest
     : public DeveloperPrivateApiTest,
