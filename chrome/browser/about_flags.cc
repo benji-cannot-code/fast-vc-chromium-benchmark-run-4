38751ca227945318c23cd41bb2a419a333300b7d
Marlon Facey <mfacey@chromium.org>
2025-12-17 18:07:52 -0800

    [Omnibox Next] Enable recent tab chips on all variants
    Bug: 450006744
    Change-Id: Iac9103dcb5cf7e43ecedd5b2dbac7026305b8491
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7272244
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Auto-Submit: Marlon Facey <mfacey@chromium.org>
    Reviewed-by: Nihar Majmudar <niharm@google.com>
    Commit-Queue: Nihar Majmudar <niharm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1560325}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -878,6 +878,7 @@ const FeatureEntry::FeatureParam
     kWebUIOmniboxAimPopupAddContextButtonBelowResults[] = {
         {"AddContextButtonVariant", "below_results"},
         {"ShowLensSearchChip", "true"},
+        {"ShowRecentTabChip", "true"},
         {"ShowVoiceSearchInSteadyComposebox", "true"},
         {"ShowVoiceSearchInExpandedComposebox", "true"},
 };
@@ -885,6 +886,7 @@ const FeatureEntry::FeatureParam
     kWebUIOmniboxAimPopupAddContextButtonAboveResults[] = {
         {"AddContextButtonVariant", "above_results"},
         {"ShowLensSearchChip", "true"},
+        {"ShowRecentTabChip", "true"},
         {"ShowVoiceSearchInSteadyComposebox", "true"},
         {"ShowVoiceSearchInExpandedComposebox", "true"},
 };
@@ -892,6 +894,7 @@ const FeatureEntry::FeatureParam kWebUIOmniboxAimPopupAddContextButtonInline[] =
     {
         {"AddContextButtonVariant", "inline"},
         {"ShowLensSearchChip", "true"},
+        {"ShowRecentTabChip", "true"},
         {"ShowVoiceSearchInSteadyComposebox", "true"},
         {"ShowVoiceSearchInExpandedComposebox", "true"},
 };
@@ -899,23 +902,17 @@ const FeatureEntry::FeatureParam
     kWebUIOmniboxAimPopupAddContextButtonMultiFile[] = {
         {"AddContextButtonVariant", "below_results"},
         {"ShowLensSearchChip", "true"},
+        {"ShowRecentTabChip", "true"},
         {"ShowVoiceSearchInSteadyComposebox", "true"},
         {"ShowVoiceSearchInExpandedComposebox", "true"},
         {"MaxNumFiles", "5"},
 };
 const FeatureEntry::FeatureParam
-    kWebUIOmniboxAimPopupAddContextButtonBelowResultsRecentTabChip[] = {
-        {"AddContextButtonVariant", "below_results"},
-        {"ShowVoiceSearchInSteadyComposebox", "true"},
-        {"ShowVoiceSearchInExpandedComposebox", "true"},
-        {"ShowRecentTabChip", "true"},
-};
-const FeatureEntry::FeatureParam
     kWebUIOmniboxAimPopupAddContextButtonDragAndDrop[] = {
         {"AddContextButtonVariant", "below_results"},
-        {"ShowCreateImageTool", "true"},
-        {"ShowToolsAndModels", "true"},
         {"EnableContextDragAndDrop", "true"},
+        {"ShowLensSearchChip", "true"},
+        {"ShowRecentTabChip", "true"},
         {"ShowVoiceSearchInSteadyComposebox", "true"},
         {"ShowVoiceSearchInExpandedComposebox", "true"},
 };
@@ -935,10 +932,6 @@ const FeatureEntry::FeatureVariation kWebUIOmniboxAimPopupVariations[] = {
     {"- \"Add Context\" Button below results, 5 File Limit",
      kWebUIOmniboxAimPopupAddContextButtonMultiFile,
      std::size(kWebUIOmniboxAimPopupAddContextButtonMultiFile), nullptr},
-    {"- \"Add Context\" Button below results, Recent Tab Chip",
-     kWebUIOmniboxAimPopupAddContextButtonBelowResultsRecentTabChip,
-     std::size(kWebUIOmniboxAimPopupAddContextButtonBelowResultsRecentTabChip),
-     nullptr},
     {"- \"Add Context\" Button enable drag and drop",
      kWebUIOmniboxAimPopupAddContextButtonDragAndDrop,
      std::size(kWebUIOmniboxAimPopupAddContextButtonDragAndDrop), nullptr}};
