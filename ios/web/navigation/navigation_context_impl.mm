d764821f31e5599e6b07995b1f83135dd83fbb95
Nan Lin <linnan@chromium.org>
2025-12-18 09:14:46 -0800

    [Code Health] Remove DanglingUntriaged Pointer in ios/web/navigation/navigation_context_impl.h
    Fixed: 446106584
    Change-Id: Ief6650ec57ccfd66cdbfb726802c8551a91f958b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7265336
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1560631}

--- ios/web/navigation/navigation_context_impl.mm
+++ ios/web/navigation/navigation_context_impl.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/memory/ptr_util.h"
 #import "ios/web/common/features.h"
 #import "ios/web/navigation/navigation_item_impl.h"
+#import "ios/web/public/web_state.h"
 #import "net/http/http_response_headers.h"
 
 namespace web {
@@ -52,7 +53,7 @@ NSString* NavigationContextImpl::GetDescription() const {
 #endif  // NDEBUG
 
 WebState* NavigationContextImpl::GetWebState() {
-  return web_state_;
+  return web_state_.get();
 }
 
 int64_t NavigationContextImpl::GetNavigationId() const {
@@ -209,7 +210,7 @@ NavigationContextImpl::NavigationContextImpl(WebState* web_state,
                                              bool has_user_gesture,
                                              ui::PageTransition page_transition,
                                              bool is_renderer_initiated)
-    : web_state_(web_state),
+    : web_state_(web_state ? web_state->GetWeakPtr() : nullptr),
       navigation_id_(CreateUniqueContextId()),
       url_(url),
       has_user_gesture_(has_user_gesture),
