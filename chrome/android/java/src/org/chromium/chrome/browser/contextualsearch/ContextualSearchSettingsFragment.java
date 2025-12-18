c41954445e3808c01c4bf576bc5f7b5868dbafbe
Gazal <agazal@google.com>
2025-12-18 14:57:07 -0800

    [Settings containment] Fix: Reapply styling on Contextual Search preference change
    When the "Touch to Search" toggle is enabled or disabled, the visibility of the "See better results" preference changes, which alters the layout of the settings page.
    
    Before: https://screencast.googleplex.com/cast/NDc1MTE5ODk2Mjg0MzY0OHw4NTEzYzkwOC1mYQ
    
    Bug: 433576895, 469022396
    Change-Id: I6cc6697964f668a3eb7d8a4f2b1e6d9a67dfd8fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7276487
    Auto-Submit: Gazal Agarwal <agazal@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1560847}

--- chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchSettingsFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchSettingsFragment.java
@@ -77,6 +77,7 @@ public class ContextualSearchSettingsFragment extends ChromeBaseSettingsFragment
                     ContextualSearchPolicy.setContextualSearchState(profile, (boolean) newValue);
                     ContextualSearchUma.logMainPreferenceChange((boolean) newValue);
                     seeBetterResultsSwitch.setVisible((boolean) newValue);
+                    notifyPreferencesUpdated();
                     updateSeeBetterResultsVisibility((boolean) newValue);
                     return true;
                 });
