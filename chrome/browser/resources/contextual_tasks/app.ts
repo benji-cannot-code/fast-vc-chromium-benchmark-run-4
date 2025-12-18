de6dd55614b28e5431c4ce9c3a543407a82bd1a1
Juan Mojica <juanmojica@google.com>
2025-12-18 15:19:35 -0800

    [Contextual Tasks] Add viewport params to common search parameters.
    This change updates the `addCommonSearchParams` method to append the
    `biw` (width) and `bih` (height) query parameters to the URL loaded in
    the contextual tasks webview. These values are derived dynamically from
    the `threadFrame` element's bounding client rect.
    
    Bug: 469853673
    Change-Id: Ic9bcce67853241424a6126ea08d0f5ec38e166b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7275041
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1560862}

--- chrome/browser/resources/contextual_tasks/app.ts
+++ chrome/browser/resources/contextual_tasks/app.ts
@@ -23,6 +23,10 @@ import {PostMessageHandler} from './post_message_handler.js';
 type ChromeEventFunctionType<T> =
     T extends ChromeEvent<infer ListenerType>? ListenerType : never;
 
+// The url query parameter keys for the viewport size.
+const VIEWPORT_HEIGHT_KEY = 'bih';
+const VIEWPORT_WIDTH_KEY = 'biw';
+
 export interface ContextualTasksAppElement {
   $: {
     threadFrame: chrome.webviewTag.WebView,
@@ -245,6 +249,20 @@ export class ContextualTasksAppElement extends CrLitElement {
         url.searchParams.set(key, value);
       }
     }
+
+    // The viewport width and height are also common search params but they
+    // are not retrieved from the browser as the viewport is determined by the
+    // contextual tasks webview.
+    const resultsBoundingRect = this.$.threadFrame.getBoundingClientRect();
+    if (resultsBoundingRect.width > 0) {
+      url.searchParams.set(
+          VIEWPORT_WIDTH_KEY, resultsBoundingRect.width.toString());
+    }
+    if (resultsBoundingRect.height > 0) {
+      url.searchParams.set(
+          VIEWPORT_HEIGHT_KEY, resultsBoundingRect.height.toString());
+    }
+
     return url;
   }
 
