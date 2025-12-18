1b81b9c6a8e1f18f8148ffb3d5950a60be402549
Dominik Röttsches <drott@chromium.org>
2025-12-18 06:00:53 -0800

    Ensure libxml2 is initialized for XmlDocPtrForString
    Preparation for potentially using XMLDocumentParserRs for non XSLT
    situations. With this forthcoming change, this is covered by the test
    virtual/xslt-enabled/fast/xsl/xslt-processor-template.html, in which
    XMlDocPtrForString is reached before previously calling into
    XmlDocumentParser or libxml2.
    
    Bug: 466303347
    Change-Id: I36d6cab9920aa4e4dde334e607f5297fa3d49b4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7275544
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1560497}

--- third_party/blink/renderer/core/xml/parser/xml_document_parser.cc
+++ third_party/blink/renderer/core/xml/parser/xml_document_parser.cc
@@ -743,7 +743,7 @@ static void ErrorFunc(void*, const char*, ...) {
   // FIXME: It would be nice to display error messages somewhere.
 }
 
-static void InitializeLibXMLIfNecessary() {
+static void EnsureLibXMLInitialized() {
   static bool did_init = false;
   if (did_init)
     return;
@@ -757,7 +757,7 @@ static void InitializeLibXMLIfNecessary() {
 scoped_refptr<XMLParserContext> XMLParserContext::CreateStringParser(
     xmlSAXHandlerPtr handlers,
     void* user_data) {
-  InitializeLibXMLIfNecessary();
+  EnsureLibXMLInitialized();
   xmlParserCtxtPtr parser =
       xmlCreatePushParserCtxt(handlers, nullptr, nullptr, 0, nullptr);
 
@@ -781,7 +781,7 @@ scoped_refptr<XMLParserContext> XMLParserContext::CreateMemoryParser(
     xmlSAXHandlerPtr handlers,
     void* user_data,
     const std::string& chunk) {
-  InitializeLibXMLIfNecessary();
+  EnsureLibXMLInitialized();
 
   // appendFragmentSource() checks that the length doesn't overflow an int.
   xmlParserCtxtPtr parser = xmlCreateMemoryParserCtxt(
@@ -1746,6 +1746,11 @@ xmlDocPtr XmlDocPtrForString(Document* document,
                              const String& url) {
   if (source.empty())
     return nullptr;
+
+  // In situations where the XMLDocumentParserRs is used as the primary parser,
+  // this might be the first call into libxml2.
+  EnsureLibXMLInitialized();
+
   // Parse in a single chunk into an xmlDocPtr
   // FIXME: Hook up error handlers so that a failure to parse the main
   // document results in good error messages.
