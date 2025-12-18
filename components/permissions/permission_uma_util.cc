2fca7a87db38afe48a59191d69f5820371d98ff3
Judith Hemp <hempjudith@google.com>
2025-12-18 04:06:12 -0800

    [ClapperQuiet] Adds new prompt disposition for Clapper Quiet UI
    This commit adds an new PermissionPromptDisposition and registers it in
    histograms.xml. This disposition will be used for the Android Clapper
    Quiet UI flow where the permission prompt is suppressed and an icon is
    shown in the omnibox.
    
    Bug: 463333225
    Change-Id: Ie92e6bf9e066abd6944496650fdbc1208c120e1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7256214
    Commit-Queue: Judith Hemp <hempjudith@google.com>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1560459}

--- components/permissions/permission_uma_util.cc
+++ components/permissions/permission_uma_util.cc
@@ -1948,6 +1948,8 @@ std::string PermissionUmaUtil::GetPromptDispositionString(
       return "MacOsPrompt";
     case PermissionPromptDisposition::MESSAGE_UI_LOUD:
       return "MessageUILoud";
+    case PermissionPromptDisposition::LOCATION_BAR_LEFT_CLAPPER_QUIET_ICON:
+      return "LocationBarLeftClapperQuietIcon";
   }
 
   NOTREACHED();
@@ -1988,6 +1990,7 @@ bool PermissionUmaUtil::IsPromptDispositionQuiet(
     case PermissionPromptDisposition::LOCATION_BAR_LEFT_QUIET_ABUSIVE_CHIP:
     case PermissionPromptDisposition::MINI_INFOBAR:
     case PermissionPromptDisposition::MESSAGE_UI:
+    case PermissionPromptDisposition::LOCATION_BAR_LEFT_CLAPPER_QUIET_ICON:
       return true;
     case PermissionPromptDisposition::ANCHORED_BUBBLE:
     case PermissionPromptDisposition::ELEMENT_ANCHORED_BUBBLE:
@@ -2022,6 +2025,7 @@ bool PermissionUmaUtil::IsPromptDispositionLoud(
     case PermissionPromptDisposition::MESSAGE_UI:
     case PermissionPromptDisposition::NONE_VISIBLE:
     case PermissionPromptDisposition::NOT_APPLICABLE:
+    case PermissionPromptDisposition::LOCATION_BAR_LEFT_CLAPPER_QUIET_ICON:
       return false;
   }
 }
