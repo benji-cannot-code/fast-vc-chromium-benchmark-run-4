36acd775779dcdd7272d3720e8026abc7c21190c
Atharv Maan <atharvmaan@chromium.org>
2025-12-18 15:03:47 -0800

    [Code Health] Remove stale base::Feature kPromoBrowserCommands
    This CL removes the stale base::Feature kPromoBrowserCommands and
    removes all code paths that relied on it.
    
    Bug: 356235454
    Change-Id: I5bb1bf5440b0c2ae50bdf5736018267160b21f79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7276506
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Atharv Maan <atharvmaan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1560849}

--- chrome/browser/new_tab_page/promos/promo_service.cc
+++ chrome/browser/new_tab_page/promos/promo_service.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/time/time.h"
 #include "base/values.h"
-#include "chrome/browser/browser_features.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/webui_url_constants.h"
@@ -185,9 +184,6 @@ void PromoService::Refresh() {
           ntp_features::kNtpMiddleSlotPromoDismissalParam) == "fake") {
     command_id = base::NumberToString(
         static_cast<int>(browser_command::mojom::Command::kNoOpCommand));
-  } else {
-    command_id = base::GetFieldTrialParamValueByFeature(
-        features::kPromoBrowserCommands, features::kBrowserCommandIdParam);
   }
 
   if (!command_id.empty()) {
