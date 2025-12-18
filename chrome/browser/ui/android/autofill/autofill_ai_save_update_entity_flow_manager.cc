68d385962ed80d4ec0c835b8cc25e70fa9a0b430
Timofey Chudakov <tchudakov@google.com>
2025-12-18 07:51:52 -0800

    [AutofillAi - M4] Run prompt closed callback in the prompt.
    Before this CL, the UI did not take any action when the save/update
    prompt was closed. This CL adds the logic to run the prompt closed
    callback after the prompt is closed.
    
    Bug: 460410690
    Change-Id: I5583b5858bcd0263783787825982f2f0add8ba72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7265148
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1560558}

--- chrome/browser/ui/android/autofill/autofill_ai_save_update_entity_flow_manager.cc
+++ chrome/browser/ui/android/autofill/autofill_ai_save_update_entity_flow_manager.cc
@@ -110,7 +110,8 @@ void AutofillAiSaveUpdateEntityFlowManager::OnMessagePrimaryAction(
           web_contents_);
   save_update_entity_prompt_controller_ =
       std::make_unique<AutofillAiSaveUpdateEntityPromptController>(
-          std::move(prompt_view_android), entity.type().name());
+          std::move(prompt_view_android), entity.type().name(),
+          std::move(prompt_closed_callback_));
   save_update_entity_prompt_controller_->DisplayPrompt();
 }
 
@@ -136,7 +137,9 @@ void AutofillAiSaveUpdateEntityFlowManager::OnMessageDismissed(
 
 void AutofillAiSaveUpdateEntityFlowManager::RunPromptClosedCallback(
     AutofillClient::AutofillAiBubbleClosedReason decision) {
-  std::move(prompt_closed_callback_).Run(decision);
+  if (prompt_closed_callback_) {
+    std::move(prompt_closed_callback_).Run(decision);
+  }
 }
 
 }  // namespace autofill
