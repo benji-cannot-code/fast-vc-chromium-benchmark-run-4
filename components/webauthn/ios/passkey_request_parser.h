e516378edc1ef7a91195e6a050697f5b37a93971
Alexis Hetu <sugoi@chromium.org>
2025-12-18 11:26:44 -0800

    [iOS] Move passkey related parsing code to its own file
    This CL performs a copy/paste of the parsing related code from
    PasskeyJavaScriptFeature into the new passkey_request_parser.*
    files. To make reviewing easier, no code is modified in this CL.
    Modifications will happen in subsequent CLs.
    
    Bug: 460485333
    Change-Id: I703c2880d8784e2a8a35aa9a54e576662ba36113
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7276565
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Reviewed-by: Noémie St-Onge <noemies@google.com>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1560704}

---
+++ components/webauthn/ios/passkey_request_parser.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_WEBAUTHN_IOS_PASSKEY_REQUEST_PARSER_H_
+#define COMPONENTS_WEBAUTHN_IOS_PASSKEY_REQUEST_PARSER_H_
+
+#import "base/values.h"
+#import "components/webauthn/ios/passkey_request_params.h"
+
+namespace webauthn {
+
+// Extracts all parameters required to build an ExtractAssertionRequestParams
+// object from the provided dictionary.
+AssertionRequestParams ExtractAssertionRequestParams(
+    const base::Value::Dict& dict);
+
+// Extracts all parameters required to build a RegistrationRequestParams object
+// from the provided dictionary.
+RegistrationRequestParams ExtractRegistrationRequestParams(
+    const base::Value::Dict& dict);
+
+}  // namespace webauthn
+
+#endif  // COMPONENTS_WEBAUTHN_IOS_PASSKEY_REQUEST_PARSER_H_
