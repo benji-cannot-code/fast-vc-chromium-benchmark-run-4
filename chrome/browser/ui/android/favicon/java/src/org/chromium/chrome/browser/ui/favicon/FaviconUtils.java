93131ad3ed610ae80cb13dfa43cf88d3cd717fa4
Hitarth Kothari <hitarthkothari@google.com>
2025-12-18 12:24:23 -0800

    Request larger favicons for history pane
    old: https://screenshot.googleplex.com/ZFaA5S7UMkQeP6t.png
    new: https://screenshot.googleplex.com/3KEjHecazAkV8oS.png
    diff: https://screenshot.googleplex.com/BXVHQ7JrHeyY6Qw.png
    
    Bug: 449171538
    Change-Id: Ia8d743fe97211e769f9247fdbd292e826a7e6228
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7277016
    Auto-Submit: Hitarth Kothari <hitarthkothari@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Hitarth Kothari <hitarthkothari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1560754}

--- chrome/browser/ui/android/favicon/java/src/org/chromium/chrome/browser/ui/favicon/FaviconUtils.java
+++ chrome/browser/ui/android/favicon/java/src/org/chromium/chrome/browser/ui/favicon/FaviconUtils.java
@@ -14,6 +14,7 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 
 import androidx.annotation.ColorInt;
+import androidx.annotation.Px;
 import androidx.appcompat.content.res.AppCompatResources;
 import androidx.core.graphics.drawable.RoundedBitmapDrawable;
 
@@ -21,6 +22,7 @@ import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.NullUnmarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.components.browser_ui.widget.RoundedIconGenerator;
+import org.chromium.ui.base.UiAndroidFeatureList;
 import org.chromium.ui.base.ViewUtils;
 import org.chromium.url.GURL;
 
@@ -28,10 +30,27 @@ import org.chromium.url.GURL;
 @NullMarked
 public class FaviconUtils {
     /**
+     * Returns the default pixel size for a favicon. Used when fetching a favicon from the
+     * LargeIconBridge.
+     *
+     * @param context The context to retrieve display metrics.
+     * @return The desired favicon size in pixels.
+     */
+    @Px
+    public static int getDefaultFaviconSize(Context context) {
+        if (!UiAndroidFeatureList.sAndroidHistoryPaneFavicons.isEnabled()) {
+            // This returns a pixel size. This is legacy behavior.
+            return context.getResources().getDimensionPixelSize(R.dimen.default_favicon_min_size);
+        }
+        return context.getResources().getDimensionPixelSize(R.dimen.default_favicon_size);
+    }
+
+    /**
      * Creates a {@link RoundedIconGenerator} to generate circular {@link Bitmap}s of favicons.
+     *
      * @param context The {@link Context} for accessing color and dimen resources.
-     * @return A {@link RoundedIconGenerator} that uses the default circle icon style. Intended
-     *         for monograms, e.g. a circle with character(s) in the center.
+     * @return A {@link RoundedIconGenerator} that uses the default circle icon style. Intended for
+     *     monograms, e.g. a circle with character(s) in the center.
      */
     public static RoundedIconGenerator createCircularIconGenerator(Context context) {
         Resources resources = context.getResources();
