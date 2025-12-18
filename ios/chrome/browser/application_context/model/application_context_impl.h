584067b9239f68faa8cabf2463ec505dd3fbc44d
Cheick Cisse <cheickcisse@google.com>
2025-12-18 05:50:42 -0800

    [iOS] Remove ApplicationContextImpl::SetApplicationLocale
    This method was marked for removal in crbug.com/414379493.
    It is no longer needed.
    
    Fixed: 414379493
    Change-Id: I1f9361492cc4e98319e68c4bfe66d5339508f6bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7270846
    Auto-Submit: Cheick Cisse <cheickcisse@google.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1560492}

--- ios/chrome/browser/application_context/model/application_context_impl.h
+++ ios/chrome/browser/application_context/model/application_context_impl.h
@@ -115,9 +115,6 @@ class ApplicationContextImpl : public ApplicationContext {
   // application states.
   void OnAppEnterState(AppState app_state);
 
-  // TODO(crbug.com/414379493): Remove this method.
-  void SetApplicationLocale(const std::string& locale);
-
   // Create the local state.
   void CreateLocalState();
 
