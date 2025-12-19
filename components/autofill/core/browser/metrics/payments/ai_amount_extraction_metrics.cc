0853d3cbe991622352ab056d84d2f1128c324703
Wilson Low <wilsonlow@google.com>
2025-12-18 17:26:48 -0800

    [AIAE][UMA] Log invalid response reason
    This CL logs UMA metrics for the invalid response reason whenever the AI
    amount extraction results in a `Invalid response` result.
    
    Bug: 444683986
    Change-Id: I1c4f587bbc2979c3b4e39dc524474535abd319b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7272024
    Reviewed-by: Olivia Saul <osaul@google.com>
    Reviewed-by: James Sun <jamessun@google.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Commit-Queue: Wilson Low <wilsonlow@google.com>
    Cr-Commit-Position: refs/heads/main@{#1560949}

--- components/autofill/core/browser/metrics/payments/ai_amount_extraction_metrics.cc
+++ components/autofill/core/browser/metrics/payments/ai_amount_extraction_metrics.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/strcat.h"
 #include "components/autofill/core/browser/data_model/payments/bnpl_issuer.h"
 #include "components/autofill/core/browser/metrics/payments/bnpl_metrics.h"
+#include "components/autofill/core/browser/payments/amount_extraction_manager.h"
 #include "services/metrics/public/cpp/ukm_builders.h"
 
 namespace autofill::autofill_metrics {
@@ -33,6 +34,33 @@ void LogAiAmountExtractedInIssuerRange(bool is_within_range,
 void LogAiAmountExtractionApcFetchResult(bool success) {
   base::UmaHistogramBoolean("Autofill.AiAmountExtraction.ApcFetchResult",
                             success);
+}
+
+void LogAiAmountExtractionInvalidResponseReason(
+    payments::AiAmountExtractionResult::Error error) {
+  AiAmountExtractionInvalidResponseReason reason;
+
+  switch (error) {
+    case payments::AiAmountExtractionResult::Error::kNegativeAmount:
+      reason = AiAmountExtractionInvalidResponseReason::kNegativeAmount;
+      break;
+    case payments::AiAmountExtractionResult::Error::kAmountMissing:
+      reason = AiAmountExtractionInvalidResponseReason::kAmountMissing;
+      break;
+    case payments::AiAmountExtractionResult::Error::kUnsupportedCurrency:
+      reason = AiAmountExtractionInvalidResponseReason::kUnsupportedCurrency;
+      break;
+    case payments::AiAmountExtractionResult::Error::kMissingCurrency:
+      reason = AiAmountExtractionInvalidResponseReason::kCurrencyCodeMissing;
+      break;
+    case payments::AiAmountExtractionResult::Error::kFailureToGenerateApc:
+    case payments::AiAmountExtractionResult::Error::kMissingServerResponse:
+    case payments::AiAmountExtractionResult::Error::kTimeout:
+      NOTREACHED();
+  }
+
+  base::UmaHistogramEnumeration(
+      "Autofill.AiAmountExtraction.InvalidResponseReason", reason);
 }
 
 }  // namespace autofill::autofill_metrics
