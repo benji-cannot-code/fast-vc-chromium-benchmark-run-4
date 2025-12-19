cfe4a30a75abf2b86d16749ffc11e5e3484edd52
Wilson Low <wilsonlow@google.com>
2025-12-19 13:43:30 -0800

    [AIAE][UKM] Log APC fetch result
    This CL logs UKM metrics for the annotated page content (APC) fetch
    result during the AI amount extraction process.
    
    This also renames UKM metric `Autofill.AiAmountExtractionComplete` to
    `Autofill.AiAmountExtraction.Result` for consistency.
    
    UKM Collection review approved: go/ai-ae-ukm
    
    Bug: 444685476
    Change-Id: Id5a71dbd54b2627beacdac4dd9cd6757d647db8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7269081
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Commit-Queue: Wilson Low <wilsonlow@google.com>
    Reviewed-by: James Sun <jamessun@google.com>
    Reviewed-by: Sun Yueru <yrsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1561350}

--- components/autofill/core/browser/metrics/payments/ai_amount_extraction_metrics.cc
+++ components/autofill/core/browser/metrics/payments/ai_amount_extraction_metrics.cc
@@ -18,7 +18,7 @@ void LogAiAmountExtractionResult(AiAmountExtractionResult result,
                                  ukm::SourceId ukm_source_id) {
   base::UmaHistogramEnumeration("Autofill.AiAmountExtraction.Result", result);
 
-  ukm::builders::Autofill_AiAmountExtractionComplete(ukm_source_id)
+  ukm::builders::Autofill_AiAmountExtraction_Result(ukm_source_id)
       .SetResult(static_cast<int64_t>(result))
       .Record(ukm::UkmRecorder::Get());
 }
@@ -31,9 +31,14 @@ void LogAiAmountExtractedInIssuerRange(bool is_within_range,
   base::UmaHistogramBoolean(histogram_name, is_within_range);
 }
 
-void LogAiAmountExtractionApcFetchResult(bool success) {
+void LogAiAmountExtractionApcFetchResult(bool success,
+                                         ukm::SourceId ukm_source_id) {
   base::UmaHistogramBoolean("Autofill.AiAmountExtraction.ApcFetchResult",
                             success);
+
+  ukm::builders::Autofill_AiAmountExtraction_ApcFetchResult(ukm_source_id)
+      .SetApcFetchResult(success)
+      .Record(ukm::UkmRecorder::Get());
 }
 
 void LogAiAmountExtractionInvalidResponseReason(
