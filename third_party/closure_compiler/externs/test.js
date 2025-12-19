cbeba261152db007ce0c6c136277030f37bcee07
Justin Lulejian <jlulejian@chromium.org>
2025-12-19 12:03:38 -0800

    [Extensions] Improve chrome.test.assert(Eq|Ne) equality checking
    Improves equality checking for:
    - Maps, Sets, and sparse arrays
    - Primitive Wrappers: Boolean, Number, String
    - Date, NaN, and functions
    
    It does this deep equality checking by updating the `checkDeepEq`
    implementation in `test_custom_bindings.js` to support recursive value
    checking for Map and Set objects. This logic handles keys and values
    in an order-independent manner.
    
    Additionally, this improves documentation by:
    - Modifying error messages to display:
      - value types on mismatches
      - raw values when JSON.stringify fails (example: undefined)
    - Adding API docs for checkDeepEq and assertNe
    - Explicitly documenting the lack of null+undefined comparisons
    
    Buddy: solomonkinard@ who wrote the initial version of the test code.
    Bug: 466303357
    Change-Id: I6cdf1a1c958a3431e1704ff2dbac73ce819064be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7233732
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Reviewed-by: Solomon Kinard <solomonkinard@chromium.org>
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1561288}

--- third_party/closure_compiler/externs/test.js
+++ third_party/closure_compiler/externs/test.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2023 The Chromium Authors
+// Copyright 2025 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -137,7 +137,8 @@ chrome.test.checkDeepEq = function(expected, actual) {};
 /**
  * @param {*=} expected
  * @param {*=} actual
- * @param {string=} message
+ * @param {string=} message A custom error message to print out with the test
+ *     failure, if any.
  * @see https://developer.chrome.com/extensions/test#method-assertEq
  */
 chrome.test.assertEq = function(expected, actual, message) {};
