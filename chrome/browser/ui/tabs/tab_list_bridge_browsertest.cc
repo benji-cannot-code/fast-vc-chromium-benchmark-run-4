1c44ab9730bf7714559e0a4571768e8212ec34b6
James Cook <jamescook@chromium.org>
2025-12-19 15:06:56 -0800

    Fix crash in ExtensionTabUtil::GetWindowIdOfGroup
    The original CL was missing a null check for the tab group model.
    The regression was introduced in:
    https://chromium-review.googlesource.com/c/chromium/src/+/7271571
    
    I couldn't just revert that because conflicting changes have
    landed in trunk.
    
    Bug: 470418661
    Change-Id: I550cf05fb0dfe90d2fbc9f43171157d72b0eea1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7281508
    Auto-Submit: James Cook <jamescook@chromium.org>
    Owners-Override: James Cook <jamescook@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1561401}

--- chrome/browser/ui/tabs/tab_list_bridge_browsertest.cc
+++ chrome/browser/ui/tabs/tab_list_bridge_browsertest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/strings/stringprintf.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/tabs/tab_group_model.h"
 #include "chrome/browser/ui/tabs/tab_strip_model_test_utils.h"
 #include "chrome/test/base/in_process_browser_test.h"
@@ -498,6 +499,26 @@ IN_PROC_BROWSER_TEST_F(TabListBridgeBrowserTest, HighlightTabs) {
   EXPECT_TRUE(tab_strip_model->IsTabSelected(1));
   EXPECT_FALSE(tab_strip_model->IsTabSelected(2));
   EXPECT_TRUE(tab_strip_model->IsTabSelected(3));
+}
+
+IN_PROC_BROWSER_TEST_F(TabListBridgeBrowserTest,
+                       ContainsTabGroupWhenTabGroupsNotSupported) {
+  // App windows don't allow tab groups.
+  Browser::CreateParams params = Browser::CreateParams::CreateForApp(
+      "some app", /*trusted_source=*/false, gfx::Rect(), browser()->profile(),
+      /*user_gesture=*/true);
+  // params.window = window2.release();
+  Browser* browser2 = Browser::Create(params);
+  BrowserList::SetLastActive(browser2);
+
+  ASSERT_FALSE(browser2->tab_strip_model()->SupportsTabGroups());
+
+  TabListInterface* tab_list_interface = TabListInterface::From(browser2);
+  ASSERT_TRUE(tab_list_interface);
+
+  // No crash when querying a tab strip that doesn't support groups.
+  EXPECT_FALSE(tab_list_interface->ContainsTabGroup(
+      tab_groups::TabGroupId::CreateEmpty()));
 }
 
 IN_PROC_BROWSER_TEST_F(TabListBridgeBrowserTest, AddTabsToGroup) {
