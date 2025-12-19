e858cfa6b24544b76dde8e77b975b56c611a2e80
Riley Tatum <rtatum@google.com>
2025-12-19 12:58:26 -0800

    [ntp-next] Add client-side sensitivity filter for recent tab chip
    Checks the visibility value in the HistoryDB, if in eligible locale,
    before displaying recent tab chip on the NTP.
    
    Bug: 453659240
    Change-Id: I1926324a5c60adae04a4427f5f47d90964fdcd3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7279260
    Reviewed-by: Jennifer Serrano <jennserrano@google.com>
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1561322}

--- chrome/browser/ui/webui/new_tab_page/action_chips/action_chips_handler.h
+++ chrome/browser/ui/webui/new_tab_page/action_chips/action_chips_handler.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback_forward.h"
 #include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
+#include "base/task/cancelable_task_tracker.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
 #include "chrome/browser/ui/webui/new_tab_page/action_chips/action_chips.mojom.h"
@@ -21,6 +23,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
+
+namespace base {
+class TimeTicks;
+}
+
+namespace history {
+class HistoryService;
+class QueryResults;
+}  // namespace history
+
+namespace tabs {
+class TabInterface;
+}
 
 class Profile;
 
@@ -46,13 +61,22 @@ class ActionChipsHandler : public action_chips::mojom::ActionChipsHandler,
 
  private:
   void SendActionChipsToUi(
+      base::TimeTicks start_time,
       std::vector<action_chips::mojom::ActionChipPtr> chips);
+  void OnGetHistoryData(const tabs::TabInterface* tab,
+                        base::TimeTicks start_time,
+                        history::QueryResults results);
 
   mojo::Receiver<action_chips::mojom::ActionChipsHandler> receiver_;
   mojo::Remote<action_chips::mojom::Page> page_;
   raw_ptr<Profile> profile_;
   raw_ptr<content::WebUI> web_ui_;
   std::unique_ptr<ActionChipsGenerator> action_chips_generator_;
+  raw_ptr<history::HistoryService> history_service_;
+
+  // Task tracker for history requests.
+  base::CancelableTaskTracker cancelable_task_tracker_;
+
   base::WeakPtrFactory<ActionChipsHandler> weak_factory_{this};
 };
 
