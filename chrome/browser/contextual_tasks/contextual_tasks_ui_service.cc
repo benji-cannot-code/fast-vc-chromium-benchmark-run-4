aa4651ce68fdf4505c3fe46a5edbbf6a5341866d
Keren Zhu <kerenzhu@chromium.org>
2025-12-18 21:05:23 -0800

    webium: fix null BrowserView in ContextualTasksUiService
    Fixed: 470059461
    Change-Id: Ib44cdc63eda36e4519660b46934a6bfbb0753e9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7276524
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1561000}

--- chrome/browser/contextual_tasks/contextual_tasks_ui_service.cc
+++ chrome/browser/contextual_tasks/contextual_tasks_ui_service.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/tab_enums.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
+#include "chrome/browser/ui/webui/webui_embedding_context.h"
 #include "chrome/common/webui_url_constants.h"
 #include "components/contextual_search/contextual_search_service.h"
 #include "components/contextual_tasks/public/account_utils.h"
@@ -397,14 +398,9 @@ bool ContextualTasksUiService::HandleNavigationImpl(
 
   bool is_nav_to_sign_in = IsSignInDomain(url_params.url);
 
-  BrowserWindowInterface* browser = nullptr;
-  if (source_contents) {
-    BrowserWindow* window =
-        BrowserWindow::FindBrowserWindowWithWebContents(source_contents);
-    if (window) {
-      browser = window->AsBrowserView()->browser();
-    }
-  }
+  BrowserWindowInterface* browser =
+      tab ? tab->GetBrowserWindowInterface()
+          : webui::GetBrowserWindowInterface(source_contents);
 
   // Intercept any navigation where the wrapping WebContents is the WebUI host
   // unless it is the embedded page.
