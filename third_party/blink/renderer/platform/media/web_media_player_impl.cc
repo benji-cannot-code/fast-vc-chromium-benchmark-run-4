48bcb7f4127f7db237fb33b8f591ba34d1508ef2
Ted Meyer <tmathmeyer@chromium.org>
2025-12-19 14:00:29 -0800

    [HLS] Replace track op functions with helper class
    As a result of the crazy media object lifetimes, the HLS player
    implementation always talks back to DemuxerManager via weakly bound
    repeating callbacks.
    
    ManifestDemuxer used
     - DemuxerManager::DemuxerRequestsSeek
    
    HlsManifestDemuxerEngine used
     - DemuxerManager::AddTrack
     - DemuxerManager::RemoveTrack
    
    It was slightly annoying to have to create a new bound callback for
    every new method that we needed to add (in this case, SetTrackState),
    and the class member list was getting polluted with a bunch of same-type
    repeating callbacks.
    
    This CL accomplishes two things -
     1. Clean up the method declarations for
        AddTrack/RemoveTrack/SetTrackState. Identical methods
        were originally declared in both DemuxerManager::Client
        and MediaPlayerClient. These can be shared in a super-interface
        for both, which is now called "TrackManager".
     2. Package up all the track management callbacks into a helper
        object which can be passed as a single parameter and class
        member to HlsManifestDemuxerEngine. This also makes it easier
        to unittest.
    
    Bug: 41393620
    Change-Id: I58914609071ce0a3fa77f835e1987cbb606dfc33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7228978
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1561362}

--- third_party/blink/renderer/platform/media/web_media_player_impl.cc
+++ third_party/blink/renderer/platform/media/web_media_player_impl.cc
@@ -1641,8 +1641,6 @@ void WebMediaPlayerImpl::OnEncryptedMediaInitData(
   encrypted_client_->Encrypted(init_data_type, init_data);
 }
 
-#if BUILDFLAG(ENABLE_FFMPEG) || BUILDFLAG(ENABLE_HLS_DEMUXER)
-
 void WebMediaPlayerImpl::AddTrack(const media::MediaTrack& track) {
   client_->AddTrack(track);
 }
@@ -1655,8 +1653,6 @@ void WebMediaPlayerImpl::SetTrackState(const media::MediaTrack& track,
                                        media::MediaTrack::State state) {
   client_->SetTrackState(track, state);
 }
-
-#endif  // BUILDFLAG(ENABLE_FFMPEG) || BUILDFLAG(ENABLE_HLS_DEMUXER)
 
 #if BUILDFLAG(ENABLE_HLS_DEMUXER)
 
