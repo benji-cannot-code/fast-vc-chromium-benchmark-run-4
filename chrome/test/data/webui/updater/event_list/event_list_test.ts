3251fdf4f792498e6489b69fa546648f97e0fbef
Noah Rose Ledesma <noahrose@google.com>
2025-12-19 11:09:25 -0800

    Update EventHistory library to handle base::Time
    base::TimeToValue serializes base::Time as a string-encoded integer
    number of microseconds since Windows Epoch. Update the EventHistory
    library to handle such values.
    
    Bug: 456498451
    Change-Id: Idc2d41dd3b2b9988342f12a7b3194a5e6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7277568
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1561257}

--- chrome/test/data/webui/updater/event_list/event_list_test.ts
+++ chrome/test/data/webui/updater/event_list/event_list_test.ts
@@ -49,7 +49,7 @@ suite('EventListElement', () => {
         'processToken': 'token1',
         'bound': 'START',
         'updaterVersion': '123.0.142.0',
-        'timestamp': '2025-11-24T12:00:00Z',
+        'timestamp': '13408459200000000',  // 2025-11-24T12:00:00Z
         'scope': 'USER',
       },
       {
@@ -132,7 +132,7 @@ suite('EventListElement', () => {
         'processToken': 'token1',
         'bound': 'START',
         'updaterVersion': '123.0.142.0',
-        'timestamp': '2025-11-24T12:00:00Z',
+        'timestamp': '13408459200000000',  // 2025-11-24T12:00:00Z
         'scope': 'USER',
       },
       {
@@ -214,7 +214,7 @@ suite('EventListElement', () => {
         'processToken': 'token1',
         'bound': 'START',
         'updaterVersion': '123.0.142.0',
-        'timestamp': '2025-11-24T12:00:00Z',
+        'timestamp': '13408459200000000',  // 2025-11-24T12:00:00Z
         'scope': 'USER',
       },
       {
@@ -288,7 +288,7 @@ suite('EventListElement', () => {
         'pid': '100',
         'processToken': 'token1',
         'bound': 'START',
-        'timestamp': '2025-12-17T12:00:00Z',
+        'timestamp': '13410446400000000',  // '2025-12-17T12:00:00Z'
       },
       {
         'eventType': 'UPDATER_PROCESS',
@@ -325,7 +325,7 @@ suite('EventListElement', () => {
         'pid': '200',
         'processToken': 'token2',
         'bound': 'START',
-        'timestamp': '2025-12-17T14:00:00Z',
+        'timestamp': '13410453600000000',  // 2025-12-17T14:00:00Z
       },
       {
         'eventType': 'UPDATER_PROCESS',
