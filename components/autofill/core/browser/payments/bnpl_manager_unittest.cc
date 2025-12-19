f2913ac32be42370d3e0faa40c3f1d336c45150c
Wilson Low <wilsonlow@google.com>
2025-12-19 15:14:52 -0800

    [AIAE][UKM] Log if AI amount extracted is in the selected issuer's range
    This CL logs UKM metrics about if the AI Amount Extracted is in the
    selected issuer's Range
    
    UKM Collection review approved: go/ai-ae-ukm
    
    Bug: 444685476
    Change-Id: I13fdba4eeb9a84e89091ba4ab83c973ae44eb976
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7266923
    Reviewed-by: Sun Yueru <yrsun@chromium.org>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Reviewed-by: James Sun <jamessun@google.com>
    Commit-Queue: Wilson Low <wilsonlow@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1561406}

--- components/autofill/core/browser/payments/bnpl_manager_unittest.cc
+++ components/autofill/core/browser/payments/bnpl_manager_unittest.cc
@@ -49,6 +49,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/ui/payments/bnpl_ui_delegate.h"
 #include "components/autofill/core/browser/ui/payments/select_bnpl_issuer_dialog_controller.h"
 #include "components/autofill/core/common/autofill_payments_features.h"
+#include "components/ukm/test_ukm_recorder.h"
+#include "services/metrics/public/cpp/ukm_builders.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/origin.h"
@@ -393,6 +395,7 @@ class BnplManagerTest : public Test,
   raw_ptr<PaymentsNetworkInterfaceMock> payments_network_interface_;
   base::test::ScopedFeatureList scoped_feature_list_;
   std::unique_ptr<base::HistogramTester> histogram_tester_;
+  ukm::TestAutoSetUkmRecorder test_ukm_recorder_;
 };
 
 // BNPL is currently only available for desktop platforms.
@@ -2771,6 +2774,21 @@ TEST_F(BnplManagerTest, LogAiAmountExtractedInIssuerRange_WithinRange) {
   histogram_tester_->ExpectUniqueSample(
       "Autofill.Bnpl.AiAmountExtraction.AmountInIssuerRange.Affirm",
       /*sample=*/1, /*expected_bucket_count=*/1);
+
+  auto entries = test_ukm_recorder_.GetEntriesByName(
+      ukm::builders::Autofill_Bnpl_AiAmountExtraction_AmountInIssuerRange::
+          kEntryName);
+  ASSERT_EQ(1u, entries.size());
+  test_ukm_recorder_.ExpectEntryMetric(
+      entries[0],
+      ukm::builders::Autofill_Bnpl_AiAmountExtraction_AmountInIssuerRange::
+          kIssuerName,
+      static_cast<int64_t>(IssuerId::kBnplAffirm));
+  test_ukm_recorder_.ExpectEntryMetric(
+      entries[0],
+      ukm::builders::Autofill_Bnpl_AiAmountExtraction_AmountInIssuerRange::
+          kIsWithinRangeName,
+      1);
 }
 
 // Test that the `AiAmountExtraction.AmountInIssuerRange` histogram is logged
@@ -2790,6 +2808,21 @@ TEST_F(BnplManagerTest, LogAiAmountExtractedInIssuerRange_OutsideRange) {
   histogram_tester_->ExpectUniqueSample(
       "Autofill.Bnpl.AiAmountExtraction.AmountInIssuerRange.Zip",
       /*sample=*/0, /*expected_bucket_count=*/1);
+
+  auto entries = test_ukm_recorder_.GetEntriesByName(
+      ukm::builders::Autofill_Bnpl_AiAmountExtraction_AmountInIssuerRange::
+          kEntryName);
+  ASSERT_EQ(1u, entries.size());
+  test_ukm_recorder_.ExpectEntryMetric(
+      entries[0],
+      ukm::builders::Autofill_Bnpl_AiAmountExtraction_AmountInIssuerRange::
+          kIssuerName,
+      static_cast<int64_t>(IssuerId::kBnplZip));
+  test_ukm_recorder_.ExpectEntryMetric(
+      entries[0],
+      ukm::builders::Autofill_Bnpl_AiAmountExtraction_AmountInIssuerRange::
+          kIsWithinRangeName,
+      0);  // False
 }
 
 // Test that the `AiAmountExtraction.AmountInIssuerRange` histogram is logged
@@ -2811,6 +2844,11 @@ TEST_F(BnplManagerTest, LogAiAmountExtractedInIssuerRange_LogsOnlyOnce) {
   histogram_tester_->ExpectUniqueSample(
       "Autofill.Bnpl.AiAmountExtraction.AmountInIssuerRange.Affirm",
       /*sample=*/1, /*expected_bucket_count=*/1);
+
+  auto entries = test_ukm_recorder_.GetEntriesByName(
+      ukm::builders::Autofill_Bnpl_AiAmountExtraction_AmountInIssuerRange::
+          kEntryName);
+  ASSERT_EQ(1u, entries.size());
 }
 #endif  // !BUILDFLAG(IS_IOS)
 
