70654e766ed82893c51c99abaca7b7217f7d784a
Joey Arhar <jarhar@chromium.org>
2025-12-19 10:03:52 -0800

    Avoid option MutationObserver in unnecessary cases
    This patch removes the MutationObserver from option elements in cases
    where they don't need it, including:
    - In a MenuList select with base appearance with a replaced button
    - In a MenuList select where they aren't the selected option
    - In a ListBox select with base appearance
    
    This should not have any observable behavior changes.
    
    Fixed: 392668961
    Change-Id: I61c08f2c5edd2fffaa4819a71c812feaaf37ee5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7045938
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1561223}

--- third_party/blink/renderer/core/html/forms/html_option_element.h
+++ third_party/blink/renderer/core/html/forms/html_option_element.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/dom/events/simulated_click_options.h"
 #include "third_party/blink/renderer/core/html/html_element.h"
+#include "third_party/blink/renderer/platform/scheduler/public/post_cancellable_task.h"
 
 namespace blink {
 
@@ -96,7 +97,10 @@ class CORE_EXPORT HTMLOptionElement final : public HTMLElement {
   String TextIndentedToRespectGroupLabel() const;
 
   // Update 'selectedness'.
-  void SetSelectedState(bool);
+  // Setting skip_mutation_observer_update to true prevents this method from
+  // calling UpdateMutationObserver.
+  void SetSelectedState(bool selected,
+                        bool skip_mutation_observer_update = false);
   // Update 'dirtiness'.
   void SetDirty(bool);
 
@@ -124,6 +128,9 @@ class CORE_EXPORT HTMLOptionElement final : public HTMLElement {
   // an HTMLOptionElement.
   static bool IsLabelContainerElement(const Element& element);
 
+  void UpdateMutationObserver(bool in_style_recalc);
+  bool HasMutationObserver() const { return text_observer_; }
+
  private:
   FocusableState SupportsFocus(UpdateBehavior update_behavior) const override;
   bool IsKeyboardFocusableSlow(UpdateBehavior update_behavior) const override;
@@ -150,6 +157,7 @@ class CORE_EXPORT HTMLOptionElement final : public HTMLElement {
   void ChooseOption(Event&);
 
   bool IsVisibleInViewport();
+  bool NeedsMutationObserver();
 
   Member<OptionTextObserver> text_observer_;
 
@@ -175,6 +183,15 @@ class CORE_EXPORT HTMLOptionElement final : public HTMLElement {
   // element with appearance:base-select.
   Member<HTMLElement> label_container_;
 
+  // DidChangeTextContent and UpdateLabel may update copies of the text content
+  // of this element into the UA ShadowRoot of this element and the nearest
+  // ancestor select element. This may be triggered as the result of the
+  // appearance of the nearest ancestor select element switching from base
+  // appearance to auto appearance, in which case we can't update the DOM yet
+  // because we are in style recalc. In this case, update_label_task_ holds a
+  // task to call DidChangeTextContent after style recalc is done.
+  TaskHandle update_label_task_;
+
   // Represents 'selectedness'.
   // https://html.spec.whatwg.org/C/#concept-option-selectedness
   bool is_selected_;
@@ -184,6 +201,9 @@ class CORE_EXPORT HTMLOptionElement final : public HTMLElement {
   // Represents the option being focused on in a multi-select non-contiguous
   // traversal via the keyboard.
   bool is_multi_select_focused_ = false;
+  // Gets set to true when a child element is inserted into this option
+  // element. Never gets set back to false once set to true.
+  bool was_element_inserted_ = false;
 
   friend class HTMLOptionElementTest;
 };
