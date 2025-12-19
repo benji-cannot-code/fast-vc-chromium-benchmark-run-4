9808e711359585f657001a6835dda2e6452ae418
Adithya Srinivasan <adithyas@chromium.org>
2025-12-19 14:44:06 -0800

    Fix window visibility check in GlicAnnotationManager
    We were using GlicKeyedService::IsWindowShowing, which is unimplemented and always returns false when GlicMultiInstance is enabled. This led to every scrollTo call failing with multi-instance enabled. This patch updates the class to use Host::GetPanelState, and also cleans up some other uses of GlicKeyedService tied to focused tab management.
    
    Bug: 467978092
    Change-Id: Icc61d5e638749388b4346026c6a4d51af1db20f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7267024
    Reviewed-by: Nick Birnie <birnie@google.com>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1561382}

--- chrome/browser/glic/host/glic_page_handler.cc
+++ chrome/browser/glic/host/glic_page_handler.cc
@@ -1549,7 +1549,7 @@ class GlicWebClientHandler : public glic::mojom::WebClientHandler,
       return;
     }
     annotation_manager_->ScrollTo(std::move(params), std::move(callback),
-                                  &host());
+                                  &host(), this);
   }
 
   void DropScrollToHighlight() override {
