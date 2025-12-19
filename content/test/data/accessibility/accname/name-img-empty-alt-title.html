chromium/chromium

commit 65c2187d64cb6fadc17087fa9cff4e2e945835c8

Remove insignificant trailing spaces from the output of testRunner.log()
... to prevent linter from tripping on it.
NOPRESUBMIT=true
(because win-presubmit can't handle so many files)

Change-Id: Ia072a0766dee97ed688e82b0703a5c78d677be87
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7248977
Reviewed-by: Alex N. Jose <alexnj@chromium.org>
Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
Cr-Commit-Position: refs/heads/main@{#1557684}

third_party/blink/web_tests/http/tests/inspector-protocol/accessibility/accessibility-nameSources-img-figure-expected.txt
@@ -23,7 +23,7 @@ Tests name sources in images and figures.
             }
         ]
         type : computedString
-        value : 
+        value :
     }
     nodeId : <string>
     parentId : <string>
@@ -289,7 +289,7 @@ Tests name sources in images and figures.
             }
         ]
         type : computedString
-        value : 
+        value :
     }
     nodeId : <string>
     parentId : <string>

benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 450206c829697ae8a1b24324501d16ed56e1e271

Reland "[a11y] Prevent tooltip from overriding empty alt on img/area"
This is a reland of commit b7f13d90d0135f3c32b9921cd60dddad12557b1a

Reason of reland:
The original CL was reverted due to test failures caused by changes
introduced in commit 65c2187d64cb6fadc17087fa9cff4e2e945835c8.

This reland fixes the issue by rebasing on top of that commit and
updating the test expectations.

Original change's description:
> [a11y] Prevent tooltip from overriding empty alt on img/area
>
> This patch prevents the tooltip (title attribute) from overriding the
> text alternative when the alt attribute is explicitly empty on <img> and
> <area> elements.
>
> This is because, according to the specifications [1][2], the alt
> attribute should be used as the text alternative even if it is an empty
> string. Currently, Blink incorrectly falls back to the tooltip when alt
> is empty.
>
> This change improves interoperability by aligning Blink's behavior with
> WebKit.
>
> [1] https://w3c.github.io/html-aam/#el-img-name [2]
> https://w3c.github.io/html-aam/#area-element-accessible-name-computation
>
> Bug: 401294444
> Change-Id: Ibf3a43f0c2d926c982345ea899d2afca0e048e95
> Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7234851
> Commit-Queue: Suyeon Ji <zeesuyeon@gmail.com>
> Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
> Reviewed-by: David Tseng <dtseng@chromium.org>
> Cr-Commit-Position: refs/heads/main@{#1557694}

Bug: 401294444
Change-Id: I12f3f699d1733e116b8682a8e543ea766962edcf
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7255875
Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
Reviewed-by: David Tseng <dtseng@chromium.org>
Cr-Commit-Position: refs/heads/main@{#1561187}

content/browser/accessibility/dump_accessibility_node_browsertest.cc
@@ -768,6 +768,10 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityAccNameTest, NameImageTitle) {
   RunAccNameTest(FILE_PATH_LITERAL("name-image-title.html"));
 }
 
+IN_PROC_BROWSER_TEST_P(DumpAccessibilityAccNameTest, NameImgEmptyAltTitle) {
+  RunAccNameTest(FILE_PATH_LITERAL("name-img-empty-alt-title.html"));
+}
+
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityAccNameTest, NameImgLabelAltTitle) {
   RunAccNameTest(FILE_PATH_LITERAL("name-img-label-alt-title.html"));
 }

third_party/blink/renderer/modules/accessibility/ax_node_object.cc
@@ -5323,7 +5323,9 @@ String AXNodeObject::TextAlternative(
   // visible element without causing an accessibility error or user problem.
   // Note: if this is part of another label or description, it needs to be
   // computed as a name, in order to contribute to that.
-  if (aria_label_or_description_root || !IsNameProhibited()) {
+  if ((aria_label_or_description_root || !IsNameProhibited()) &&
+      !(has_explicitly_empty_native_text_alternative &&
+        (IsA<HTMLImageElement>(node) || IsA<HTMLAreaElement>(node)))) {
     String resulting_text = TextAlternativeFromTooltip(
         name_from, name_sources, &found_text_alternative, &text_alternative,
         related_objects);
@@ -7306,7 +7308,7 @@ String AXNodeObject::NativeTextAlternative(
       name_sources->push_back(NameSource(*found_text_alternative, kAltAttr));
       name_sources->back().type = name_from;
     }
-    if (!alt.empty()) {
+    if (!alt.IsNull()) {
       text_alternative = alt;
       if (name_sources) {
         NameSource& source = name_sources->back();

content/public/android/javatests/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityTreeTest.java
@@ -232,6 +232,12 @@ public class WebContentsAccessibilityTreeTest {
         performAccnameTest("name-div-content-only.html");
     }
 
+    @Test
+    @SmallTest
+    public void test_nameImgEmptyAltTitle() {
+        performAccnameTest("name-img-empty-alt-title.html");
+    }
+
     // ------------------ ARIA TESTS ------------------ //
 
     @Test

content/test/content_test_bundle_data.filelist
@@ -224,6 +224,13 @@ data/accessibility/accname/name-image-spinbutton-valuenow-in-label-expected-blin
 data/accessibility/accname/name-image-spinbutton-valuenow-in-label.html
 data/accessibility/accname/name-image-title-expected-blink.txt
 data/accessibility/accname/name-image-title.html
+data/accessibility/accname/name-img-empty-alt-title-expected-android-external.txt
+data/accessibility/accname/name-img-empty-alt-title-expected-android.txt
+data/accessibility/accname/name-img-empty-alt-title-expected-auralinux.txt
+data/accessibility/accname/name-img-empty-alt-title-expected-blink.txt
+data/accessibility/accname/name-img-empty-alt-title-expected-mac.txt
+data/accessibility/accname/name-img-empty-alt-title-expected-win.txt
+data/accessibility/accname/name-img-empty-alt-title.html
 data/accessibility/accname/name-img-label-alt-title-expected-blink.txt
 data/accessibility/accname/name-img-label-alt-title.html
 data/accessibility/accname/name-img-label-expected-blink.txt

content/test/data/accessibility/accname/name-img-empty-alt-title-expected-android-external.txt
@@ -1,0 +1,4 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+WebView focusable focused actions:[CLEAR_FOCUS, AX_FOCUS] bundle:[chromeRole="rootWebArea", hasImage="true"]
+++View actions:[AX_FOCUS] bundle:[chromeRole="genericContainer", hasImage="true"]
+++++Image tooltipText:"title" viewIdResName:"test" actions:[AX_FOCUS] bundle:[chromeRole="image", hasImage="true", targetUrl="file:///storage/emulated/0/chromium_tests_root/content/test/data/accessibility/accname/foo.jpg"]

content/test/data/accessibility/accname/name-img-empty-alt-title-expected-android.txt
@@ -1,2 +1,2 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-image name='peanuts popcorn apple jacks' nameFrom=relatedElement
+android.widget.Image tooltip_text='title'

content/test/data/accessibility/accname/name-img-empty-alt-title-expected-auralinux.txt
@@ -1,0 +1,2 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+[image] name='To get missing image descriptions, open the context menu.' description='title' description-from:tooltip

content/test/data/accessibility/accname/name-img-empty-alt-title-expected-blink.txt
@@ -1,2 +1,2 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-image name='peanuts popcorn apple jacks' nameFrom=relatedElement
+image name='' nameFrom=attributeExplicitlyEmpty

content/test/data/accessibility/accname/name-img-empty-alt-title-expected-mac.txt
@@ -1,2 +1,1 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-image name='peanuts popcorn apple jacks' nameFrom=relatedElement

content/test/data/accessibility/accname/name-img-empty-alt-title-expected-win.txt
@@ -1,0 +1,2 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+ROLE_SYSTEM_GRAPHIC name='To get missing image descriptions, open the context menu.' READONLY description-from:tooltip description='title'

content/test/data/accessibility/accname/name-img-empty-alt-title.html
@@ -1,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<!--
+@BLINK-DENY:descri*
+-->
+<!doctype html>
+<html>
+<body>
+<img id="test" alt="" title="title" src="foo.jpg"/>
+</body>
+</html>

third_party/blink/web_tests/external/wpt/accname/name/comp_tooltip.tentative-expected.txt
@@ -1,6 +1,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-This is a testharness.js-based test.
-[FAIL] img with tooltip label with empty alt
-  assert_equals: <img title="title" data-expectedlabel="" alt="" data-testname="img with tooltip label with empty alt" class="ex" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="> expected "" but got "title"
-Harness: the test ran to completion.
-

third_party/blink/web_tests/http/tests/inspector-protocol/accessibility/accessibility-nameSources-img-figure-expected.txt
@@ -710,6 +710,10 @@ Tests name sources in images and figures.
         type : internalRole
         value : 99
     }
+    description : {
+        type : computedString
+        value : title
+    }
     domNode : img
     ignored : false
     name : {
@@ -724,23 +728,19 @@ Tests name sources in images and figures.
             }
             [2] : {
                 attribute : alt
-                type : attribute
-            }
-            [3] : {
-                attribute : title
                 attributeValue : {
                     type : string
-                    value : title
+                    value :
                 }
                 type : attribute
                 value : {
                     type : computedString
-                    value : title
+                    value :
                 }
             }
         ]
         type : computedString
-        value : title
+        value :
     }
     nodeId : <string>
     parentId : <string>