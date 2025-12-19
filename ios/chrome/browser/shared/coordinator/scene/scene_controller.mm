3a8935de9fca338308d12575734d94735b386b8f
Nan Lin <linnan@chromium.org>
2025-12-19 12:26:33 -0800

    [Code Health] Remove DanglingUntriaged Pointer in ApiAccessTokenFetcher and FetchProcess
    Fixed: 446101654, 446104234
    Change-Id: I3ccea978a8a431732562123982124cc00ec54ffa
    
    Cq-Include-Trybots: luci.chromium.try:ios-simulator
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs
    Change-Id: I3ccea978a8a431732562123982124cc00ec54ffa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7275825
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Anthi Orfanou <anthie@google.com>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1561298}

--- ios/chrome/browser/shared/coordinator/scene/scene_controller.mm
+++ ios/chrome/browser/shared/coordinator/scene/scene_controller.mm
@@ -2086,10 +2086,13 @@ using UserFeedbackDataCallback =
         self.mainInterface.profile->GetSharedURLLoaderFactory(),
         base::BindOnce(&OnListFamilyMembersResponse, primary_account.gaia, data)
             .Then(base::BindOnce(^{
-              [weakSelf presentUserFeedbackViewController:baseViewController
-                                     withUserFeedbackData:data
-                                 cancelFamilyMembersFetch:NO
-                                               completion:completion];
+              [weakSelf
+                  reportAnIssueFamilyMembersListFetchedForBaseViewController:
+                      baseViewController
+                                                            userFeedbackData:
+                                                                data
+                                                                  completion:
+                                                                      completion];
             })));
 
     // Timeout the request to list family members.
@@ -2108,6 +2111,23 @@ using UserFeedbackDataCallback =
                      withUserFeedbackData:data
                  cancelFamilyMembersFetch:NO
                                completion:completion];
+}
+
+// Callback for when the Family Members list is fetched.
+- (void)
+    reportAnIssueFamilyMembersListFetchedForBaseViewController:
+        (UIViewController*)baseViewController
+                                              userFeedbackData:
+                                                  (UserFeedbackData*)data
+                                                    completion:
+                                                        (UserFeedbackDataCallback)
+                                                            completion {
+  [self presentUserFeedbackViewController:baseViewController
+                     withUserFeedbackData:data
+                 cancelFamilyMembersFetch:NO
+                               completion:completion];
+  // Reset the fetcher now that it has done its job.
+  _familyMembersFetcher.reset();
 }
 
 - (void)presentUserFeedbackViewController:(UIViewController*)baseViewController
