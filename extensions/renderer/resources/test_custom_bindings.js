66b3b9b6a3e5898aa626e6246f95d8ade49a89ff
Justin Lulejian <jlulejian@chromium.org>
2025-12-19 12:04:14 -0800

    [Extensions] Add braces to if statements in test_custom_bindings.js
    extensions/renderer/resources/test_custom_bindings.js contained several
    if statements without curly braces. This coding style is inconsistent
    with the style guide
    (https://google.github.io/styleguide/tsguide.html#control-structures).
    
    Note: Although the style guide allows one-line if statements without
    braces, `git cl format --js` wraps the one-line to multiple so that was
    not done to avoid that conflict.
    
    Bug: N/A
    Change-Id: If8314caf2f9091584b2a6c3c228f1dcca3f3b72d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7239071
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Reviewed-by: Solomon Kinard <solomonkinard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1561289}

--- extensions/renderer/resources/test_custom_bindings.js
+++ extensions/renderer/resources/test_custom_bindings.js
@@ -205,11 +205,13 @@ apiBridge.registerCustomHook(function(api) {
 
   function safeFunctionApply(func, args) {
     try {
-      if (func)
+      if (func) {
         return $Function.apply(func, undefined, args);
+      }
     } catch (e) {
-      if (e === kFailureException)
+      if (e === kFailureException) {
         throw e;
+      }
       handleException(e.message, e);
     }
   }
@@ -231,8 +233,9 @@ apiBridge.registerCustomHook(function(api) {
     try {
       chromeTest.log(`( RUN      ) ${testName(currentTest)}`);
       bindingUtil.setExceptionHandler(function(message, e) {
-        if (e !== kFailureException)
+        if (e !== kFailureException) {
           chromeTest.fail(`uncaught exception: ${message}`);
+        }
       });
       const result = $Function.call(currentTest);
       if (result instanceof Promise) {
@@ -265,10 +268,11 @@ apiBridge.registerCustomHook(function(api) {
   function assertBool(test, expected, message) {
     if (test !== expected) {
       if (typeof test == 'string') {
-        if (message)
+        if (message) {
           message = `${test}\n${message}`;
-        else
+        } else {
           message = test;
+        }
       }
       chromeTest.fail(message);
     }
@@ -310,8 +314,9 @@ apiBridge.registerCustomHook(function(api) {
     // really fancy, there may be more sophisticated ways of doing this.
     Error.captureStackTrace(stack, failHandler);
 
-    if (!message)
+    if (!message) {
       message = 'FAIL (no message)';
+    }
 
     message += '\n' + stack.stack;
     console.log(`[FAIL] ${testName(currentTest)}: ${message}`);
