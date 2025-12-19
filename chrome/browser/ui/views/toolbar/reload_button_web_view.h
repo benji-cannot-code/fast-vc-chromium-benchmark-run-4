a10d49291ef92f14beafd0171b89e8cada3cb12b
Youssef Bourouphael <ybourouphael@google.com>
2025-12-19 11:08:12 -0800

    Remove redundant tooltip logic from ReloadButtonWebView
    ReloadButtonWebView was manually setting the tooltip text using
    views::View::SetTooltipText. However, since this is a WebUI-based
    control, the tooltip is already effectively managed by the HTML `title`
    attribute within the `reload-button-app` Web Component
    (chrome/browser/resources/reload_button/app.html.ts).
    
    The WebUI implementation dynamically updates the `title` based on the
    button state (loading, menu enabled, etc.) in `app.ts`. Therefore, the
    native Views-side tooltip handling is unnecessary and has been removed
    to avoid redundancy.
    
    Change-Id: Ie2472b59954977db06754bb3bb194039b31afb6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7268382
    Commit-Queue: Youseff Bourouphel <ybourouphael@google.com>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1561256}

--- chrome/browser/ui/views/toolbar/reload_button_web_view.h
+++ chrome/browser/ui/views/toolbar/reload_button_web_view.h
@@ -69,7 +69,6 @@ class ReloadButtonWebView : public views::View,
  private:
   void UpdateAccessibleHasPopup();
   void SetReloadButtonUIState();
-  void UpdateTooltipText();
 
   raw_ptr<ReloadButtonUI> reload_button_ui_ = nullptr;
   raw_ptr<views::WebView> web_view_ = nullptr;
