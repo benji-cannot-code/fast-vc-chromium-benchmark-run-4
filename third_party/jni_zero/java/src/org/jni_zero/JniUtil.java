4cf784f9a3d855fddfb99161f78bfd7d5178ded4
Andrew Grieve <agrieve@chromium.org>
2025-12-19 12:27:45 -0800

    JNI Zero: Switch from android_library -> java_library
    In order to support JVM
    
    Change-Id: I98dacaf17c7c8014002b21d2d9aa389b0c603975
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7281286
    Reviewed-by: Martin Kong <martinkong@google.com>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Martin Kong <martinkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1561300}

--- third_party/jni_zero/java/src/org/jni_zero/JniUtil.java
+++ third_party/jni_zero/java/src/org/jni_zero/JniUtil.java
@@ -5,8 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.jni_zero;
 
-import android.util.ArrayMap;
-
+import java.util.HashMap;
 import java.util.Map;
 
 /** Native helpers. */
@@ -25,7 +24,7 @@ public class JniUtil {
     @CalledByNative
     private static Map<Object, Object> arrayToMap(Object[] array) {
         int len = array.length;
-        Map<Object, Object> ret = new ArrayMap(len / 2);
+        Map<Object, Object> ret = new HashMap<>(len / 2);
         for (int i = 0; i < len; i += 2) {
             ret.put(array[i], array[i + 1]);
         }
