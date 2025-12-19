374875e029d8ed9002f87cd4ff14ba1ceab144af
Steve Becker <stevebe@microsoft.com>
2025-12-19 15:50:20 -0800

    [DomStorage] Add feature flag and DomStorageDatabase for SQLite
    Introduces the `DomStorageSqlite` class, which inherits the
    `DomStorageDatabase` interface. The current implementation returns
    `DbStatus::NotSupported()` for each override. Future changes will add
    implementations one by one, just like the migration to
    the `DomStorageDatabase` interface for LevelDB.
    
    Also, adds the `kDomStorageSqlite` feature flag to force enable SQLite
    by updating `DomStorageDatabaseFactory::Open()` to return
    `DomStorageSqlite`.
    
    Bug: 377242771
    Change-Id: I1cdd946e7e3e67010fc5c6f999b5b03f3ee69cd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7275044
    Commit-Queue: Steve Becker <stevebe@microsoft.com>
    Reviewed-by: Evan Stade <evanstade@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1561426}

---
+++ components/services/storage/dom_storage/features.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_SERVICES_STORAGE_DOM_STORAGE_FEATURES_H_
+#define COMPONENTS_SERVICES_STORAGE_DOM_STORAGE_FEATURES_H_
+
+#include "base/features.h"
+
+namespace storage {
+
+// Enable to use a SQLite database with local storage and session storage
+// instead of LevelDB. See "crbug.com/377242771: Migrate DOMStorage to use
+// SQLite" for more details.
+BASE_DECLARE_FEATURE(kDomStorageSqlite);
+
+}  // namespace storage
+
+#endif  // COMPONENTS_SERVICES_STORAGE_DOM_STORAGE_FEATURES_H_
