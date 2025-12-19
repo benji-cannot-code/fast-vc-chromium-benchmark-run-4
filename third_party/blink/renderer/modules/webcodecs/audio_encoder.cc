46c63e64d6d22a610263d6044e60a4c1b43bab7c
Dale Curtis <dalecurtis@chromium.org>
2025-12-19 14:50:47 -0800

    Impose a maximum concurrent encoding limit for AudioEncoders
    The way the WebCodecs API calculates queue sizes only works if there's
    also an internal limit on the number of pending encodes. Otherwise
    as soon as the message to encode a buffer is posted the queue size
    becomes zero (even if that message sits on another thread queue).
    
    AudioDecoder/VideoDecoder both use GetMaxDecodeRequests() which is
    always 1 for audio and varies for video. VideoEncoder computes limits
    based on reported codec capabilities, but defaults to 5. As such
    we've chosen the same limit for AudioEncoder for now.
    
    In local testing this seems to preserve the same encoding times we
    get with the current approach (which given there's no queue, are as
    optimal as possible). Switching to a strict 1 pending EncodeCB
    based approach slows encoding by ~56% (16s -> 25s on demo page).
    
    Fixed: 459832204
    Change-Id: Ic16ae0909c95cb6bb93a55a48eb244935658202a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7281011
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1561388}

--- third_party/blink/renderer/modules/webcodecs/audio_encoder.cc
+++ third_party/blink/renderer/modules/webcodecs/audio_encoder.cc
@@ -524,6 +524,14 @@ void AudioEncoder::ProcessConfigure(Request* request) {
           MakeUnwrappingCrossThreadHandle(request))));
 }
 
+bool AudioEncoder::ReadyToProcessNextRequest() {
+  if (active_encodes_ >= kMaxActiveEncodes) {
+    return false;
+  }
+
+  return Base::ReadyToProcessNextRequest();
+}
+
 void AudioEncoder::ProcessEncode(Request* request) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK_EQ(state_, V8CodecState::Enum::kConfigured);
@@ -552,6 +560,7 @@ void AudioEncoder::ProcessEncode(Request* request) {
           self->MakeEncodingError("Encoding error.", std::move(status)));
     }
 
+    --self->active_encodes_;
     req->EndTracing();
     self->ProcessRequests();
   };
@@ -579,6 +588,8 @@ void AudioEncoder::ProcessEncode(Request* request) {
 
   --requested_encodes_;
   ScheduleDequeueEvent();
+  ++active_encodes_;
+
   media_encoder_->Encode(
       std::move(audio_bus), timestamp,
       ConvertToBaseOnceCallback(CrossThreadBindOnce(
@@ -609,6 +620,11 @@ bool AudioEncoder::CanReconfigure(ParsedConfig& original_config,
 bool AudioEncoder::VerifyCodecSupport(ParsedConfig* config,
                                       String* js_error_message) {
   return VerifyCodecSupportStatic(config, js_error_message);
+}
+
+void AudioEncoder::ResetInternal(DOMException* ex) {
+  Base::ResetInternal(ex);
+  active_encodes_ = 0;
 }
 
 void AudioEncoder::OnNewEncode(InputType* input,
@@ -682,6 +698,10 @@ ScriptPromise<AudioEncoderSupport> AudioEncoder::isConfigSupported(
 
 const AtomicString& AudioEncoder::InterfaceName() const {
   return event_target_names::kAudioEncoder;
+}
+
+bool AudioEncoder::HasPendingActivity() const {
+  return (active_encodes_ > 0) || Base::HasPendingActivity();
 }
 
 DOMException* AudioEncoder::MakeOperationError(std::string error_msg,
