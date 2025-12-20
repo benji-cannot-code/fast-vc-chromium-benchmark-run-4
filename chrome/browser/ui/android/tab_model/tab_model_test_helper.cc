ddc8da98bfef9c1be7a1401f824fd78beb8f7e2f
James Cook <jamescook@chromium.org>
2025-12-19 16:17:45 -0800

    Start enabling ExtensionTabUtil::GetGroupById() on desktop Android
    Compile in the main body of the function and convert to using
    BrowserWindowInterface. A couple pieces still have to be ifdef'd out.
    
    This reveals we need TabListInterface::ListTabGroups(), so add that,
    with a Win/Mac/Linux implementation. The Android impl is a TODO since
    it requires JNI and a bit more thinking.
    
    Add a browser test for this function, since we apparently don't have
    any direct tests of this method (it's tested indirectly through
    tabGroups API tests, but those won't run on Android for a while).
    
    Bug: 405219902
    Change-Id: Ia3c2e29f6084b0cea85cab9bbb273a7a8b7e3968
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7282038
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Owners-Override: James Cook <jamescook@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1561441}

--- chrome/browser/ui/android/tab_model/tab_model_test_helper.cc
+++ chrome/browser/ui/android/tab_model/tab_model_test_helper.cc
@@ -199,6 +199,11 @@ bool TestTabModel::ContainsTabGroup(tab_groups::TabGroupId group_id) {
   return false;
 }
 
+std::vector<tab_groups::TabGroupId> TestTabModel::ListTabGroups() {
+  NOTIMPLEMENTED();
+  return {};
+}
+
 std::optional<tab_groups::TabGroupId> TestTabModel::AddTabsToGroup(
     std::optional<tab_groups::TabGroupId> group_id,
     const std::set<tabs::TabHandle>& tabs) {
@@ -449,6 +454,11 @@ void OwningTestTabModel::UnpinTab(tabs::TabHandle tab) {
 bool OwningTestTabModel::ContainsTabGroup(tab_groups::TabGroupId group_id) {
   NOTIMPLEMENTED();
   return false;
+}
+
+std::vector<tab_groups::TabGroupId> OwningTestTabModel::ListTabGroups() {
+  NOTIMPLEMENTED();
+  return {};
 }
 
 std::optional<tab_groups::TabGroupId> OwningTestTabModel::AddTabsToGroup(
