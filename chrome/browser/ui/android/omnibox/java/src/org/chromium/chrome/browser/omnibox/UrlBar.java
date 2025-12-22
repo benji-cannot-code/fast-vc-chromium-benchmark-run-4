a14cce2708e203fcfea61aaf53a4d925efcda992
Tomasz Wiszkowski <ender@google.com>
2025-12-22 14:47:31 -0800

    Disable multiline omnibox on Tablets
    We need to cherrypick this to M144..
    
    Change-Id: I6f8a33727b3f7b84420171078360540a3d98d8d1
    Fixed: 470093386
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7282944
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1561990}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/UrlBar.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/UrlBar.java
@@ -57,6 +57,7 @@ import org.chromium.components.browser_ui.share.ShareHelper;
 import org.chromium.components.browser_ui.util.FirstDrawDetector;
 import org.chromium.components.omnibox.OmniboxFeatures;
 import org.chromium.ui.KeyboardVisibilityDelegate;
+import org.chromium.ui.base.DeviceFormFactor;
 import org.chromium.ui.base.KeyNavigationUtil;
 import org.chromium.ui.display.DisplayAndroid;
 import org.chromium.ui.display.DisplayUtil;
@@ -354,7 +355,9 @@ public class UrlBar extends AutocompleteEditText {
         if (!mFocused) mFocusEventEmitted = false;
         super.onFocusChanged(focused, direction, previouslyFocusedRect);
 
-        if (!mIsInCct && OmniboxFeatures.sMultilineEditField.isEnabled()) {
+        if (!mIsInCct
+                && !DeviceFormFactor.isNonMultiDisplayContextOnTablet(getContext())
+                && OmniboxFeatures.sMultilineEditField.isEnabled()) {
             setInputIsMultilineEligible(false);
             if (focused) {
                 setSingleLine(false);
