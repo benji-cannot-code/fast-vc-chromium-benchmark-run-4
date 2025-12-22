5222a72743ced86a48efe0e43a1d3354ca05de81
Bao-Duy Tran <tranbaoduy@chromium.org>
2025-12-22 01:57:02 -0800

    Deprecate FetchJapaneseLegacyConfig mojo API of CrOS IME Service.
    ... and reduce all of its impl to no-op, effectively stopping
    all related requests to CrOS 1P IME shared lib and deprecating
    the latter's corresponding protobuf API as well.
    
    The APIs and all related impls are obsolete and no longer
    invoked. They were used by Mozc-to-CrOS-Prefs data migration
    for CrOS 1P Japanese IME settings which was terminated
    @ https://crrev.com/c/7206458.
    
    CQ_INCLUDE_TRYBOTS=luci.chrome.try:chromeos-betty-chrome
    
    Bug: b:465075400
    Change-Id: I66860094d71a635248d0e07f4cca531685d2ed5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7274356
    Reviewed-by: Georg Neis <neis@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Commit-Queue: Bao-Duy Tran <tranbaoduy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1561725}

--- chromeos/ash/services/ime/input_method_user_data_service_impl.h
+++ chromeos/ash/services/ime/input_method_user_data_service_impl.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/ash/services/ime/ime_shared_library_wrapper.h"
 #include "chromeos/ash/services/ime/public/cpp/shared_lib/interfaces.h"
-#include "chromeos/ash/services/ime/public/cpp/shared_lib/proto/fetch_japanese_legacy_config.pb.h"
 #include "chromeos/ash/services/ime/public/cpp/shared_lib/proto/user_data_service.pb.h"
 #include "chromeos/ash/services/ime/public/mojom/input_method_user_data.mojom.h"
 #include "chromeos/ash/services/ime/user_data_c_api_interface.h"
@@ -23,9 +22,6 @@ class InputMethodUserDataServiceImpl
   InputMethodUserDataServiceImpl(std::unique_ptr<UserDataCApiInterface> c_api);
 
   ~InputMethodUserDataServiceImpl() override;
-
-  void FetchJapaneseLegacyConfig(
-      FetchJapaneseLegacyConfigCallback callback) override;
 
   void FetchJapaneseDictionary(
       FetchJapaneseDictionaryCallback callback) override;
