9182b69c3e41a77902350e07a5c6efd90b694f85
Bao-Duy Tran <tranbaoduy@chromium.org>
2025-12-21 20:59:51 -0800

    Add arg names & comments for SetCurrentLayoutByName() callbacks.
    ... so their "success" meanings can be explicitly expressed,
    especially as this propagates through so many layers. This will
    soon help reveal some instances whereby non-sensical values are
    being fed as "success" arg to some of these callbacks.
    
    CQ_INCLUDE_TRYBOTS=luci.chrome.try:chromeos-betty-chrome
    
    Bug: b:469242705
    Change-Id: I8ab5ed15ec88fb7740fcfa383bb0cc1b63bc9a7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7267369
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Bao-Duy Tran <tranbaoduy@chromium.org>
    Reviewed-by: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1561689}

--- ui/events/ozone/evdev/keyboard_evdev.h
+++ ui/events/ozone/evdev/keyboard_evdev.h
@@ -80,7 +80,7 @@ class COMPONENT_EXPORT(EVDEV) KeyboardEvdev
 
   // Handle keyboard layout changes.
   void SetCurrentLayoutByName(const std::string& layout_name,
-                              base::OnceCallback<void(bool)> callback);
+                              base::OnceCallback<void(bool success)> callback);
 
  private:
   void UpdateModifier(int modifier_flag, bool down);
