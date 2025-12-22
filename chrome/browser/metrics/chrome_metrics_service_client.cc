01d21fd1ef28ee1fdd660845d294b5ff9f10d6a8
Sean Maher <spvm@chromium.org>
2025-12-22 11:44:11 -0800

    move ExhaustedIntervalsPerThirtySeconds recorder to a dedicated thread
    In the case that the main thread is blocked due to memory contention
    (the whole idea behind this system), the current Timer-based solution
    will not run.
    
    Moving the sampler to a separate thread should allow the system to
    record more often even during a stall due to lacking memory, as well as
    during memory-consuming tasks on the main thread.
    
    Also, turn the sampler into a MetricsProvider so that the measurements
    are only made when metrics recording is enabled.
    
    Bug: 465731177
    Change-Id: Ie0eb3295b152b3c2cef6131030ac995913742170
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7234451
    Owners-Override: Sean Maher <spvm@chromium.org>
    Commit-Queue: Sean Maher <spvm@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1561917}

--- chrome/browser/metrics/chrome_metrics_service_client.cc
+++ chrome/browser/metrics/chrome_metrics_service_client.cc
@@ -198,6 +198,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/metrics/antivirus_metrics_provider_win.h"
 #include "chrome/browser/metrics/google_update_metrics_provider_win.h"
+#include "chrome/browser/metrics/system_memory_list_metrics_provider_win.h"
 #include "chrome/browser/metrics/tpm_metrics_provider_win.h"
 #include "chrome/install_static/install_util.h"
 #include "chrome/installer/util/util_constants.h"
@@ -898,6 +899,8 @@ void ChromeMetricsServiceClient::RegisterMetricsServiceProviders() {
       std::make_unique<AntiVirusMetricsProvider>());
   metrics_service_->RegisterMetricsProvider(
       std::make_unique<TPMMetricsProvider>());
+  metrics_service_->RegisterMetricsProvider(
+      std::make_unique<SystemMemoryListMetricsProvider>());
 #endif  // BUILDFLAG(IS_WIN)
 
 #if BUILDFLAG(IS_MAC)
