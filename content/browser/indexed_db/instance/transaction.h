74da0ed73e4e27d7c3dc37c430d09a021b93346c
Abhishek Shanthkumar <abhishek.shanthkumar@microsoft.com>
2025-12-22 03:12:04 -0800

    IDB: Log backend durations of transaction start, commit and blob writes
    For commit, the measured duration does not include the time to write
    blobs, if any. It just measures the total duration of synchronous calls
    to the backing store for commit phase one and two.
    
    For blob writes, the asynchronous duration from start to finish is
    recorded.
    
    Bug: 436887363
    Change-Id: I3e35f8dc342a3724698794493c372db576b74454
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7252629
    Reviewed-by: Mingyu Lei <leimy@chromium.org>
    Reviewed-by: Evan Stade <evanstade@microsoft.com>
    Commit-Queue: Abhishek Shanthkumar <abhishek.shanthkumar@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1561744}

--- content/browser/indexed_db/instance/transaction.h
+++ content/browser/indexed_db/instance/transaction.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include <memory>
+#include <optional>
 #include <set>
 #include <tuple>
 
@@ -268,7 +269,7 @@ class CONTENT_EXPORT Transaction : public blink::mojom::IDBTransaction {
   bool IsTaskQueueEmpty() const;
   bool HasPendingTasks() const;
 
-  void BlobWriteComplete(Status result);
+  void BlobWriteComplete(base::TimeTicks start_time, Status result);
   void CloseOpenCursors();
   Status CommitPhaseTwo();
   void TimeoutFired();
@@ -295,6 +296,9 @@ class CONTENT_EXPORT Transaction : public blink::mojom::IDBTransaction {
   // backing store transaction.
   PartitionedLockHolder locks_receiver_;
   bool is_commit_pending_ = false;
+  // This accumulates the duration of synchronous work done by the backing store
+  // for transaction commit (phase one + phase two).
+  base::TimeDelta commit_synchronous_duration_;
 
   // We are owned by the connection object, but during force closes sometimes
   // there are issues if there is a pending OpenRequest. So use a WeakPtr.
