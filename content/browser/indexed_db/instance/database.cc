f07cc314dff2ba62ac7e4934d77ab4bbae98d3b6
Abhishek Shanthkumar <abhishek.shanthkumar@microsoft.com>
2025-12-21 23:59:45 -0800

    IDB: Kill renderer when cursor argument validations fail
    These lead to browser process crashes currently.
    
    Fixed: 468486047
    Change-Id: Ie9913a59ee468cda7e315aea2a1fefcdf481ceb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7281124
    Reviewed-by: Evan Stade <evanstade@microsoft.com>
    Commit-Queue: Abhishek Shanthkumar <abhishek.shanthkumar@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1561708}

--- content/browser/indexed_db/instance/database.cc
+++ content/browser/indexed_db/instance/database.cc
@@ -444,7 +444,7 @@ Status Database::VersionChangeOperation(int64_t version,
 Status Database::GetOperation(int64_t object_store_id,
                               int64_t index_id,
                               IndexedDBKeyRange key_range,
-                              CursorType cursor_type,
+                              bool key_only,
                               blink::mojom::IDBDatabase::GetCallback callback,
                               Transaction* transaction) {
   TRACE_EVENT1("IndexedDB", "Database::GetOperation", "txn.id",
@@ -460,7 +460,7 @@ Status Database::GetOperation(int64_t object_store_id,
     StatusOr<std::unique_ptr<BackingStore::Cursor>> backing_store_cursor;
     if (index_id == IndexedDBIndexMetadata::kInvalidId) {
       // ObjectStore Retrieval Operation
-      if (cursor_type == CursorType::kKeyOnly) {
+      if (key_only) {
         backing_store_cursor =
             transaction->BackingStoreTransaction()->OpenObjectStoreKeyCursor(
                 object_store_id, key_range,
@@ -471,7 +471,7 @@ Status Database::GetOperation(int64_t object_store_id,
                 object_store_id, key_range,
                 blink::mojom::IDBCursorDirection::Next);
       }
-    } else if (cursor_type == CursorType::kKeyOnly) {
+    } else if (key_only) {
       // Index Value Retrieval Operation
       backing_store_cursor =
           transaction->BackingStoreTransaction()->OpenIndexKeyCursor(
@@ -522,7 +522,7 @@ Status Database::GetOperation(int64_t object_store_id,
       return Status::OK();
     }
 
-    if (cursor_type == CursorType::kKeyOnly) {
+    if (key_only) {
       std::move(callback).Run(
           blink::mojom::IDBDatabaseGetResult::NewKey(std::move(key)));
       return Status::OK();
@@ -562,7 +562,7 @@ Status Database::GetOperation(int64_t object_store_id,
     std::move(callback).Run(blink::mojom::IDBDatabaseGetResult::NewEmpty(true));
     return Status::OK();
   }
-  if (cursor_type == CursorType::kKeyOnly) {
+  if (key_only) {
     // Index Value Retrieval Operation
     std::move(callback).Run(
         blink::mojom::IDBDatabaseGetResult::NewKey(std::move(primary_key)));
@@ -832,8 +832,7 @@ Status Database::OpenCursorOperation(
 
   StatusOr<std::unique_ptr<BackingStore::Cursor>> backing_store_cursor;
   if (params->index_id == IndexedDBIndexMetadata::kInvalidId) {
-    if (params->cursor_type == CursorType::kKeyOnly) {
-      CHECK_EQ(params->task_type, blink::mojom::IDBTaskType::Normal);
+    if (params->key_only) {
       backing_store_cursor =
           transaction->BackingStoreTransaction()->OpenObjectStoreKeyCursor(
               params->object_store_id, params->key_range, params->direction);
@@ -843,8 +842,7 @@ Status Database::OpenCursorOperation(
               params->object_store_id, params->key_range, params->direction);
     }
   } else {
-    CHECK_EQ(params->task_type, blink::mojom::IDBTaskType::Normal);
-    if (params->cursor_type == CursorType::kKeyOnly) {
+    if (params->key_only) {
       backing_store_cursor =
           transaction->BackingStoreTransaction()->OpenIndexKeyCursor(
               params->object_store_id, params->index_id, params->key_range,
@@ -872,8 +870,12 @@ Status Database::OpenCursorOperation(
 
   mojo::PendingAssociatedRemote<blink::mojom::IDBCursor> pending_remote;
   Cursor* cursor = Cursor::CreateAndBind(
-      std::move(*backing_store_cursor), params->cursor_type, params->task_type,
-      transaction->AsWeakPtr(), pending_remote);
+      std::move(*backing_store_cursor),
+      {params->index_id == IndexedDBIndexMetadata::kInvalidId
+           ? Cursor::Type::Source::kObjectStore
+           : Cursor::Type::Source::kIndex,
+       params->direction, params->key_only},
+      params->task_type, transaction->AsWeakPtr(), pending_remote);
   transaction->RegisterOpenCursor(cursor);
 
   blink::mojom::IDBValuePtr mojo_value;
