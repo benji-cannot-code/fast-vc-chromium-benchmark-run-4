05e3a32e3070c716a4e9253cbd145b992f388b7b
Alexandra Pereira <alexandrasp@google.com>
2025-12-22 07:37:29 -0800

    [iOS] Refactor Autofill form data objects to be classes
    This change converts AutofillFormData and AutofillFormFieldData
    interfaces into classes that inherit from a new JsonSafeObject base
    class. This base class ensures that any toJSON method on the object's
    prototype is null, preventing potential issues with custom
    implementations on websites.
    
    Bug: 448990422
    Change-Id: Ic01461a62697c8a40cf00bb66a873604256bb85e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7277380
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1561797}

--- components/password_manager/ios/resources/password_controller.ts
+++ components/password_manager/ios/resources/password_controller.ts
@@ -401,7 +401,7 @@ function getPasswordFormDataFromUnownedElements(): object|null {
   if (unownedControlElements.length === 0) {
     return null;
   }
-  const unownedForm = new gCrWebLegacy['common'].JSONSafeObject();
+  const unownedForm = new fillUtil.AutofillFormData();
   const hasUnownedForm = unownedFormElementsAndFieldSetsToFormData(
       window, fieldsets, unownedControlElements, /* iframeElements= */[], false,
       unownedForm);
