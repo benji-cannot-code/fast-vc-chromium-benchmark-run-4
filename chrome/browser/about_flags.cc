6321de72b383ffc77d85283cf40018de77088f95
Aviv Kiss <avivkiss@google.com>
2025-12-22 13:40:36 -0800

    [omnibox][next] Clean up about_flags.
    - Removes no context variant. This variant isn't adding value beyond what the existing classic popup
    webui flags provide -- removing to simplify flag-states/variants.
    - Clean up remaining voice search overrides (flipped in https://crrev.com/c/7269248).
    
    Bug: 469399150
    Change-Id: I0c50d9a676807b5a56972cc29a45d4676f868a24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7298388
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Auto-Submit: Aviv Kiss <avivkiss@google.com>
    Commit-Queue: Aviv Kiss <avivkiss@google.com>
    Cr-Commit-Position: refs/heads/main@{#1561968}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -871,16 +871,9 @@ const FeatureEntry::FeatureVariation
          std::size(kSearchPrefetchWithHoldback), nullptr}};
 
 #if !BUILDFLAG(IS_ANDROID)
-const FeatureEntry::FeatureParam kWebUIOmniboxAimPopupAddContextButtonNone[] = {
-    {"AddContextButtonVariant", "none"},
-    {"ShowVoiceSearchInSteadyComposebox", "true"},
-    {"ShowVoiceSearchInExpandedComposebox", "true"},
-};
 const FeatureEntry::FeatureParam
     kWebUIOmniboxAimPopupAddContextButtonBelowResults[] = {
         {"AddContextButtonVariant", "below_results"},
-        {"ShowVoiceSearchInSteadyComposebox", "true"},
-        {"ShowVoiceSearchInExpandedComposebox", "true"},
         {"ShowRecentTabChip", "true"},
 };
 const FeatureEntry::FeatureParam
@@ -901,8 +894,6 @@ const FeatureEntry::FeatureParam
 };
 
 const FeatureEntry::FeatureVariation kWebUIOmniboxAimPopupVariations[] = {
-    {"- No \"Add Context\" Button", kWebUIOmniboxAimPopupAddContextButtonNone,
-     std::size(kWebUIOmniboxAimPopupAddContextButtonNone), nullptr},
     {"- \"Add Context\" Button below results (Variant 1)",
      kWebUIOmniboxAimPopupAddContextButtonBelowResults,
      std::size(kWebUIOmniboxAimPopupAddContextButtonBelowResults), nullptr},
