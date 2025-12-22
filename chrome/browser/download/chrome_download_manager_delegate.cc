0b644fbd510303e479909cd4e4b27860391193fd
Ana Ercegovac <anaercegovac@google.com>
2025-12-22 09:46:51 -0800

    Move BinaryUploadService to components directory and enterprise_connectors namespace (4)
    This change updates remaining files in the chrome/browser/ directory to
    use enterprise_connectors::BinaryUploadService.
    
    Bug: 469317208
    Change-Id: I2feecc620b532c5303835c04d7f1e2dccc0af272
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7269491
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Ana Ercegovac <anaercegovac@google.com>
    Cr-Commit-Position: refs/heads/main@{#1561838}

--- chrome/browser/download/chrome_download_manager_delegate.cc
+++ chrome/browser/download/chrome_download_manager_delegate.cc
@@ -149,7 +149,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if BUILDFLAG(ENTERPRISE_CONTENT_ANALYSIS)
-#include "chrome/browser/safe_browsing/cloud_content_scanning/binary_upload_service.h"
+#include "components/enterprise/connectors/core/cloud_content_scanning/binary_upload_service.h"
 #include "components/enterprise/obfuscation/core/download_obfuscator.h"
 #endif
 
@@ -1054,7 +1054,7 @@ bool ChromeDownloadManagerDelegate::ShouldObfuscateDownload(
 
   // Skip obfuscation for large files if size is known.
   if (static_cast<size_t>(item->GetTotalBytes()) >
-      safe_browsing::BinaryUploadService::kMaxUploadSizeBytes) {
+      enterprise_connectors::BinaryUploadService::kMaxUploadSizeBytes) {
     return false;
   }
 
