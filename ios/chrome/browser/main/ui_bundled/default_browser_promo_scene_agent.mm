4fa97b3ecb9bd3f9c3063e88f85d964e52cd6c36
Cheick Cisse <cheickcisse@google.com>
2025-12-22 14:06:20 -0800

    [iOS] Refactor Files in default_browser/
    This CL refactors:
    - generic/
    - post_default_abandonment/
    - post_restore/
    - promo_handler/
    
    Bypass-Check-License: Refactor
    Bug: 467340284
    Change-Id: I288a32b5fde75b5338aa842916fc9c76129ef67e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7280002
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Owners-Override: Cheick Cisse <cheickcisse@google.com>
    Cr-Commit-Position: refs/heads/main@{#1561976}

--- ios/chrome/browser/main/ui_bundled/default_browser_promo_scene_agent.mm
+++ ios/chrome/browser/main/ui_bundled/default_browser_promo_scene_agent.mm
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/app/profile/profile_state_observer.h"
 #import "ios/chrome/browser/default_browser/model/features.h"
 #import "ios/chrome/browser/default_browser/model/utils.h"
-#import "ios/chrome/browser/default_browser/promo/post_default_abandonment/features.h"
+#import "ios/chrome/browser/default_browser/promo/post_default_abandonment/public/features.h"
 #import "ios/chrome/browser/feature_engagement/model/tracker_factory.h"
 #import "ios/chrome/browser/promos_manager/model/constants.h"
 #import "ios/chrome/browser/segmentation_platform/model/segmentation_platform_service_factory.h"
--- ios/chrome/browser/main/ui_bundled/default_browser_promo_scene_agent_unittest.mm
+++ ios/chrome/browser/main/ui_bundled/default_browser_promo_scene_agent_unittest.mm
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/default_browser/model/features.h"
 #import "ios/chrome/browser/default_browser/model/utils.h"
 #import "ios/chrome/browser/default_browser/model/utils_test_support.h"
-#import "ios/chrome/browser/default_browser/promo/post_default_abandonment/features.h"
+#import "ios/chrome/browser/default_browser/promo/post_default_abandonment/public/features.h"
 #import "ios/chrome/browser/dom_distiller/model/distiller_service_factory.h"
 #import "ios/chrome/browser/feature_engagement/model/tracker_factory.h"
 #import "ios/chrome/browser/promos_manager/model/constants.h"
