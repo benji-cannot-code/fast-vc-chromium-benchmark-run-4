0e274beb0517e4ada928274d0cbaf807933971fe
Roman Arora <romanarora@chromium.org>
2025-12-22 15:11:06 -0800

    [ntp][realbox][next] Contextual entrypoint IPH
    Screenshot(s):
    Tall experience: screenshot/559NG25Gg8o9JVZ
    Compact experience: screenshot/3rrU2PoNjGEufxq
    
    Fixed: b:447196911
    Change-Id: Ibf85905bb6ffa321b74bf23d4611c2e8b4d53322
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7224530
    Reviewed-by: Marlon Facey <mfacey@chromium.org>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1562002}

--- chrome/browser/resources/new_tab_page/app.ts
+++ chrome/browser/resources/new_tab_page/app.ts
@@ -127,6 +127,8 @@ const MSAL_IFRAME_ORIGIN = 'chrome-untrusted://ntp-microsoft-auth';
 
 export const CUSTOMIZE_CHROME_BUTTON_ELEMENT_ID =
     'CustomizeButtonsHandler::kCustomizeChromeButtonElementId';
+export const CONTEXTUAL_ENTRYPOINT_ELEMENT_ID =
+    'NewTabPageUI::kRealboxContextualEntrypointElementId';
 
 // 900px ~= 561px (max value for --ntp-search-box-width) * 1.5 + some margin.
 const realboxCanShowSecondarySideMediaQueryList =
@@ -623,6 +625,12 @@ export class AppElement extends AppElementBase {
 
     if (!this.modulesEnabled_) {
       this.recordBrowserPromoMetrics_();
+    }
+
+    if (this.ntpRealboxNextEnabled_) {
+      this.registerHelpBubble(
+          CONTEXTUAL_ENTRYPOINT_ELEMENT_ID,
+          ['#searchbox', '#context', '#contextEntrypoint'], {fixed: true});
     }
   }
 
