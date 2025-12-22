f5599af0ac7af47323a592e7c6d3bdf45f7d73c9
Kenichi Ishibashi <bashi@chromium.org>
2025-12-21 16:47:24 -0800

    Revert "[DomStorage] Add feature flag and DomStorageDatabase for SQLite"
    This reverts commit 72cd7345eab95ad1443326b67dfbcbf8e579ac9c.
    
    Reason for revert: Tests are failing (e.g. https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Tests%20(dbg)(1)/129687/overview)
    
    Original change's description:
    > [DomStorage] Add feature flag and DomStorageDatabase for SQLite
    >
    > Introduces the `DomStorageSqlite` class, which inherits the
    > `DomStorageDatabase` interface. The current implementation returns
    > `DbStatus::NotSupported()` for each override. Future changes will add
    > implementations one by one, just like the migration to
    > the `DomStorageDatabase` interface for LevelDB.
    >
    > Also, adds the `kDomStorageSqlite` feature flag to force enable SQLite
    > by updating `DomStorageDatabaseFactory::Open()` to return
    > `DomStorageSqlite`.
    >
    > Bug: 377242771
    > Change-Id: I1cdd946e7e3e67010fc5c6f999b5b03f3ee69cd0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7275044
    > Commit-Queue: Steve Becker <stevebe@microsoft.com>
    > Reviewed-by: Evan Stade <evanstade@microsoft.com>
    > Cr-Commit-Position: refs/heads/main@{#1561426}
    
    Bug: 377242771
    Change-Id: I6507979e6038703ab6ac6206d974a0df84220fa9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7296249
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Owners-Override: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1561668}

--- components/services/storage/dom_storage/async_dom_storage_database.cc
+++ components/services/storage/dom_storage/async_dom_storage_database.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/services/storage/dom_storage/async_dom_storage_database.h"
 
 #include "base/debug/alias.h"
+#include "base/feature_list.h"
 #include "base/functional/callback_helpers.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
--- components/services/storage/dom_storage/local_storage_impl.cc
+++ components/services/storage/dom_storage/local_storage_impl.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/byte_size.h"
 #include "base/containers/contains.h"
 #include "base/debug/dump_without_crashing.h"
+#include "base/feature_list.h"
 #include "base/files/file_enumerator.h"
 #include "base/files/file_path.h"
 #include "base/functional/bind.h"
