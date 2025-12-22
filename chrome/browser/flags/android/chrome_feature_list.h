e44143976a957ec4ce4492a8236c93de5511d826
Wenyu Fu <wenyufu@chromium.org>
2025-12-22 11:31:40 -0800

    Keep CVH as a browser controls observer
    BrowserControlsHeight can change while activity is stopped (e.g. from
    BookmarkBar settings). Keep it attached so it can receive height change
    signal to update the web content size.
    
    Add a kill switch: BrowserControlsPersistsOnCvh
    
    Bug: 466973298
    Change-Id: I05c48e48e51a91bab9c0692e3939d29959e2ec00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7284406
    Auto-Submit: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1561909}

--- chrome/browser/flags/android/chrome_feature_list.h
+++ chrome/browser/flags/android/chrome_feature_list.h
@@ -84,6 +84,7 @@ BASE_DECLARE_FEATURE(kBlockIntentsWhileLocked);
 BASE_DECLARE_FEATURE(kBookmarkPaneAndroid);
 BASE_DECLARE_FEATURE(kBrowserControlsDebugging);
 BASE_DECLARE_FEATURE(kBrowserControlsEarlyResize);
+BASE_DECLARE_FEATURE(kBrowserControlsPersistsOnCvh);
 BASE_DECLARE_FEATURE(kBrowserControlsRenderDrivenShowConstraint);
 BASE_DECLARE_FEATURE(kCCTAdaptiveButton);
 BASE_DECLARE_FEATURE(kCCTAdaptiveButtonTestSwitch);
