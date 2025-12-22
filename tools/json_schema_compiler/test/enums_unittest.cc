9c25a9f544879b8296edfe5e61d65da8d694c5d3
Victor Hugo Vianna Silva <victorvianna@google.com>
2025-12-22 09:53:51 -0800

    Migrate usages of base::to_underlying in //tools
    ```
    files_using_header="$(git grep -l base::to_underlying | tr '\n' ' ')"
    sed -i 's/base::to_underlying/std::to_underlying/g' \
      ${files_using_header}
    tools/add_header.py --header '<utility>' ${files_using_header}
    files_including_header="$(git grep -l base/types/cxx23_to_underlying.h | tr '\n' ' ')"
    tools/add_header.py --header '"base/types/cxx23_to_underlying.h"' \
      --remove ${files_including_header}
    git cl format
    ```
    
    Then some manual fixes.
    
    Bug: 470039537
    Change-Id: I0a128b8c46dbb1c05f6f91213dea11f40140275a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7277730
    Reviewed-by: Jesse McKenna <jessemckenna@google.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1561843}

--- tools/json_schema_compiler/test/enums_unittest.cc
+++ tools/json_schema_compiler/test/enums_unittest.cc
@@ -5,9 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "tools/json_schema_compiler/test/enums.h"
 
+#include <utility>
 #include <vector>
 
-#include "base/types/cxx23_to_underlying.h"
 #include "base/values.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "tools/json_schema_compiler/test/test_util.h"
@@ -32,7 +32,7 @@ TEST(JsonSchemaCompilerEnumsTest, EnumTypePopulate) {
 }
 
 TEST(JsonSchemaCompilerEnumsTest, EnumsAsTypes) {
-  static_assert(0 == base::to_underlying(enums::Enumeration::kNone));
+  static_assert(0 == std::to_underlying(enums::Enumeration::kNone));
 
   {
     base::Value::List args;
