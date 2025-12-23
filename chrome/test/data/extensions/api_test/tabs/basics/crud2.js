49d3c52e1e2ec32b585c6a68834ea73a39372fdf
Devlin Cronin <rdevlin.cronin@chromium.org>
2025-12-23 10:38:29 -0800

    [Desktop Android] Enable more (sub)tests in ExtensionApiTabTest.Tabs2
    Bug: 371432155
    Change-Id: I7c289752cd8402b38cc7dfd67d3396ad3c260530
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7301211
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1562245}

--- chrome/test/data/extensions/api_test/tabs/basics/crud2.js
+++ chrome/test/data/extensions/api_test/tabs/basics/crud2.js
@@ -247,8 +247,6 @@ const skipForAndroid = [
     'testOpenWindowInEmptyPopup',
     'testOpenEmptyPopup',
     'testCreatePopupAndMoveTab',
-    'detectLanguage',
-    'getCurrentWindow',
 ];
 
 (async function() {
