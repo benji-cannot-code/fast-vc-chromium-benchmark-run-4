9bbf0a17f3347f9681cdb2b8dc8e36a137fe7b63
Qikai <qikaizhong@microsoft.com>
2025-12-23 00:32:47 -0800

    [bedrock] Remove unused BrowserList::empty() method
    The empty() method is no longer referenced anywhere in the codebase and
    can be safely removed.
    
    Bug: 431671320
    Change-Id: Ib69b46390b6495efa86f8b6524f7270b4ce34d99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7280243
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Qikai Zhong <qikaizhong@microsoft.com>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1562116}

--- chrome/browser/ui/views/tabs/vertical/vertical_tab_drag_interactive_uitest.cc
+++ chrome/browser/ui/views/tabs/vertical/vertical_tab_drag_interactive_uitest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
+#include "chrome/browser/ui/browser_window/public/global_browser_collection.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/tabs/vertical/vertical_tab_view.h"
@@ -106,7 +107,7 @@ class VerticalTabDragHandlerTest
   }
 
   BrowserWindowInterface& GetLatestBrowser() {
-    CHECK(!BrowserList::GetInstance()->empty());
+    CHECK(!GlobalBrowserCollection::GetInstance()->IsEmpty());
     BrowserWindowInterface* browser = *(--BrowserList::GetInstance()->end());
     CHECK(browser);
     return *browser;
--- chrome/browser/ui/webui/ash/settings/pages/printing/integration_tests/printer_settings_integration_test.cc
+++ chrome/browser/ui/webui/ash/settings/pages/printing/integration_tests/printer_settings_integration_test.cc
@@ -16,8 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/test/session_manager_state_waiter.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser_commands.h"
-#include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface_iterator.h"
+#include "chrome/browser/ui/browser_window/public/global_browser_collection.h"
 #include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/browser/ui/settings_window_manager_chromeos.h"
 #include "chrome/test/base/chromeos/crosier/ash_integration_test.h"
@@ -243,7 +243,7 @@ class PrinterSettingsIntegrationTest : public AshIntegrationTest {
   auto ReloadOsPrinterSettings() {
     return Steps(
         Do([]() {
-          ASSERT_FALSE(BrowserList::GetInstance()->empty());
+          ASSERT_FALSE(GlobalBrowserCollection::GetInstance()->IsEmpty());
           chrome::Reload(GetLastActiveBrowserWindowInterfaceWithAnyProfile(),
                          WindowOpenDisposition::CURRENT_TAB);
         }),
