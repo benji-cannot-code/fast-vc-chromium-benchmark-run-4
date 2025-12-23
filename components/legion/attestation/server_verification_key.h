d499c572382a007f1effa33475606ebed8398bce
Nikhil Jakhar <nikhiljakhar@google.com>
2025-12-23 04:31:12 -0800

    legion: Add hardcoded server verification key
    This change introduces a constant byte array containing the server's
    public verification key, which will be used for attestation.
    
    Bug: b:469921004
    Change-Id: I7916b8b629d10acf4c9187a62059f43fdc85bfa0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7275584
    Reviewed-by: Oleh Lamzin <lamzin@google.com>
    Commit-Queue: Nikhil Jakhar <nikhiljakhar@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Auto-Submit: Nikhil Jakhar <nikhiljakhar@google.com>
    Cr-Commit-Position: refs/heads/main@{#1562143}

---
+++ components/legion/attestation/server_verification_key.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_LEGION_CRYPTO_SERVER_VERIFICATION_KEY_H_
+#define COMPONENTS_LEGION_CRYPTO_SERVER_VERIFICATION_KEY_H_
+
+#include <stdint.h>
+
+#include "base/containers/span.h"
+
+namespace legion {
+
+// Returns the server verification key. The steps to update this key are in
+// b/469921004.
+base::span<const uint8_t> GetDevServerVerificationKey();
+
+}  // namespace legion
+
+#endif  // COMPONENTS_LEGION_CRYPTO_SERVER_VERIFICATION_KEY_H_
