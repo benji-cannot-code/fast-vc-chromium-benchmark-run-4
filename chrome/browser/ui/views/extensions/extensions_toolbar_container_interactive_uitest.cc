bb0aa05347af10fe78b05bb76f881f4d7766df45
Masa Fujita <massan@google.com>
2025-12-22 19:12:15 -0800

    actions revamp: Store EAVMs in a std::map
    This CL changes the `std::vector` of `ExtensionActionViewModel`s in
    `ExtensionsToolbarViewModel` to an `std::map` with the `ActionId` as the
    key. Currently the vector's order holds no significance, so semantically
    it makes more sense to hold it in a `std::map`. The performance
    difference should be negligible if not marginaly better (reduced time
    complexity).
    
    We also modify so that we don't expose this implementation detail
    directly, and ask the caller to specify the action ID to get the
    specific EAVM for an action. In cases where we need the list of all
    actions, we refer to `ToolbarActionsModel::action_ids()`.
    
    Bug: 461981075
    Change-Id: Ibc3798483bffebdd0ae0db7ff13958e61ecfbda1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7276943
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Masa Fujita <massan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1562057}

--- chrome/browser/ui/views/extensions/extensions_toolbar_container_interactive_uitest.cc
+++ chrome/browser/ui/views/extensions/extensions_toolbar_container_interactive_uitest.cc
@@ -1052,8 +1052,9 @@ IN_PROC_BROWSER_TEST_P(
   auto* permissions_manager =
       extensions::PermissionsManager::Get(browser()->profile());
   EXPECT_TRUE(request_access_button()->GetVisible());
-  EXPECT_THAT(request_access_button()->GetExtensionIdsForTesting(),
-              testing::ElementsAre(extensionA->id(), extensionB->id()));
+  EXPECT_THAT(
+      request_access_button()->GetExtensionIdsForTesting(),
+      testing::UnorderedElementsAre(extensionA->id(), extensionB->id()));
 
   EXPECT_EQ(permissions_helper.GetSiteInteraction(*extensionA, web_contents),
             SiteInteraction::kWithheld);
@@ -1174,8 +1175,9 @@ IN_PROC_BROWSER_TEST_F(
   // Verify request access button is visible because extensions A and B have
   // site access requests.
   EXPECT_TRUE(request_access_button()->GetVisible());
-  EXPECT_THAT(request_access_button()->GetExtensionIdsForTesting(),
-              testing::ElementsAre(extensionA->id(), extensionB->id()));
+  EXPECT_THAT(
+      request_access_button()->GetExtensionIdsForTesting(),
+      testing::UnorderedElementsAre(extensionA->id(), extensionB->id()));
   extensions::SitePermissionsHelper permissions_helper(browser()->profile());
   auto* permissions_manager =
       extensions::PermissionsManager::Get(browser()->profile());
