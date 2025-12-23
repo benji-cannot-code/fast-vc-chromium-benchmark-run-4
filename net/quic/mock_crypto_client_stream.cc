2c509b0d66d5ffb6a564b98c20b4d85b9e84759b
Matt Menke <mmenke@chromium.org>
2025-12-22 22:21:33 -0800

    Add CHECK that triggers if a QUIC session is being used incorrectly.
    In particular, if we attempt to create an HTTP stream atop a QUIC
    session whose cert is not valid for the HTTP stream's host, we will
    now CHECK.
    
    This is motivated by the merging of HTTP stream requests at the
    HttpStreamPool layer, which ran into issues which this would have
    caught.
    
    Bug: 455891789
    Change-Id: I1cae3622224cd122021dac4f7ef44fff0fe32133
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7275288
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1562087}

--- net/quic/mock_crypto_client_stream.cc
+++ net/quic/mock_crypto_client_stream.cc
@@ -82,6 +82,10 @@ MockCryptoClientStream::MockCryptoClientStream(
   crypto_framer_.set_visitor(this);
   // Simulate a negotiated cipher_suite with a fake value.
   crypto_negotiated_params_->cipher_suite = 1;
+  if (!proof_verify_details_) {
+    static_cast<QuicChromiumClientSession*>(session)
+        ->set_allow_any_url_for_testing();
+  }
 }
 
 MockCryptoClientStream::~MockCryptoClientStream() = default;
@@ -312,6 +316,13 @@ void MockCryptoClientStream::NotifySessionOneRttKeyAvailable() {
   encryption_established_ = true;
   handshake_confirmed_ = true;
   DCHECK(session()->version().UsesTls());
+
+  FillCryptoParams();
+  if (proof_verify_details_) {
+    reinterpret_cast<QuicSpdyClientSessionBase*>(session())
+        ->OnProofVerifyDetailsAvailable(*proof_verify_details_);
+  }
+
   if (use_mock_crypter_) {
     if (session()->connection()->version().KnowsWhichDecrypterToUse()) {
       session()->connection()->InstallDecrypter(
--- net/quic/quic_chromium_client_session.h
+++ net/quic/quic_chromium_client_session.h
@@ -280,6 +280,10 @@ class NET_EXPORT_PRIVATE QuicChromiumClientSession
     // code.
     int GetSelfAddress(IPEndPoint* address) const;
 
+    // CHECKs that the cert is valid for `url`. Used as a safety check against
+    // aliasing/request merging logic.
+    void AssertIsValidFor(const GURL& url) const;
+
     // Returns the session's server ID.
     quic::QuicServerId server_id() const { return server_id_; }
 
@@ -979,6 +983,9 @@ class NET_EXPORT_PRIVATE QuicChromiumClientSession
     migration_info_ = migration_info;
   }
 
+  // Makes AssertIsValidFor() do nothing, once set.
+  void set_allow_any_url_for_testing() { allow_any_url_for_testing_ = true; }
+
  protected:
   // quic::QuicSession methods:
   bool ShouldCreateIncomingStream(quic::QuicStreamId id) override;
@@ -1100,6 +1107,10 @@ class NET_EXPORT_PRIVATE QuicChromiumClientSession
   void OnCryptoHandshakeComplete();
 
   void LogZeroRttStats();
+
+  // CHECKs that the cert is valid for `url`. Used as a safety check against
+  // aliasing/request merging logic. Applies subset of logic in CanPool().
+  void AssertIsValidFor(const GURL& url) const;
 
 #if BUILDFLAG(ENABLE_WEBSOCKETS)
   std::unique_ptr<WebSocketQuicStreamAdapter>
@@ -1260,6 +1271,9 @@ class NET_EXPORT_PRIVATE QuicChromiumClientSession
   bool enable_periodic_ping_ = false;
 
   bool crypto_handshake_complete_ = false;
+
+  // Makes AssertIsValidFor() do nothing.
+  bool allow_any_url_for_testing_ = false;
 
   base::WeakPtrFactory<QuicChromiumClientSession> weak_factory_{this};
 };
