bc001452df879229353d403defc27ea265c36422
Greg Thompson <grt@chromium.org>
2025-12-23 08:08:17 -0800

    [Windows] Fix ProgID registration for .pdf files
    ChromePDF should be used rather than ChromeHTML in .pdf\OpenWithProgids.
    
    Also clean up some UNSAFE_TODOs.
    
    Bug: 40384442
    Change-Id: I1b72264abd794a63e2a21f73df67300d782c1ab2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7299645
    Reviewed-by: David Bienvenu <davidbienvenu@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1562195}

--- chrome/installer/util/shell_util.h
+++ chrome/installer/util/shell_util.h
@@ -13,10 +13,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
+#include <array>
 #include <memory>
 #include <optional>
 #include <set>
 #include <string>
+#include <string_view>
 #include <utility>
 #include <vector>
 
@@ -315,8 +317,10 @@ class ShellUtil {
   static const wchar_t* kDefaultFileAssociations[];
 
   // File extensions that Chrome registers itself as being capable of
-  // handling.
-  static const wchar_t* kPotentialFileAssociations[];
+  // handling as a web browser.
+  static constexpr std::array<std::wstring_view, 8> kPotentialFileAssociations =
+      {L".htm", L".html", L".mhtml", L".shtml",
+       L".svg", L".xht",  L".xhtml", L".webp"};
 
   // Protocols that Chrome registers itself as the default handler for
   // when the user makes Chrome the default browser.
@@ -517,8 +521,9 @@ class ShellUtil {
   static bool ShowMakeChromeDefaultSystemUI(const base::FilePath& chrome_exe);
 
   // Opens the Windows settings dialog allowing the user to choose the default
-  // app for the given `file_extension`. It must be one of the extensions in
-  // `kPotentialFileAssociations`. The dialog will be parented to `parent_hwnd`.
+  // app for the given `file_extension`. It must be one of the extensions for
+  // which the browser is registered to handle. The dialog will be parented to
+  // `parent_hwnd`.
   // It reads:
   //   * Windows 10: "How do you want to open `file_extension` files from now
   //     on?"
