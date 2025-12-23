7e1378a246a2e939f543821d31342c17ee2dc9fa
Devlin Cronin <rdevlin.cronin@chromium.org>
2025-12-23 15:20:01 -0800

    [Desktop Android] Add support for tabId in windows.create()
    Add support for handling the tabId property in windows.create() to
    allow moving an existing tab to a newly-created window.
    
    Enable additional test coverage for this case in the form of (some of)
    ExtensionApiNewTabTest.Tabs.
    
    Bug: 431004500
    Change-Id: I1f1f3443949ff4f78479b62a38e0eb7b51798cdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7311952
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1562362}

--- chrome/test/data/extensions/api_test/tabs/basics/crud/crud.js
+++ chrome/test/data/extensions/api_test/tabs/basics/crud/crud.js
@@ -4,10 +4,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 var firstWindowId;
-var lastWindowId;
 
 const scriptUrl = '_test_resources/api_test/tabs/basics/tabs_util.js';
 let loadScript = chrome.test.loadScript(scriptUrl);
+
+let isAndroid;
+
+// Tests that the given `url` and `title` are appropriate values for the NTP.
+// This has subtly different behavior on different platforms.
+function smellsLikeNewTabPage(url, title) {
+  const ntpUrls = [
+      'chrome://newtab/',
+      'chrome-native://newtab/',
+  ];
+  chrome.test.assertTrue(ntpUrls.includes(url),
+                         `Unexpected URL: ${url}`);
+  if (!isAndroid) {
+    // For most create calls the title will be an empty string until the
+    // navigation commits, but the new tab page is an exception to this.
+    // However, this isn't true on Android.
+    chrome.test.assertEq('New Tab', title);
+  }
+}
 
 function getSelectedAdapter(winId, callback) {
   const manifest = chrome.runtime.getManifest();
@@ -26,8 +44,7 @@ function getSelectedAdapter(winId, callback) {
   });
 }
 
-loadScript.then(async function() {
-chrome.test.runTests([
+const tests = [
   function getSelected() {
     getSelectedAdapter(null, pass(function(tab) {
       assertEq('about:blank', tab.url);
@@ -42,16 +59,12 @@ chrome.test.runTests([
       assertEq(1, tab.index);
       assertEq(firstWindowId, tab.windowId);
       assertEq(false, tab.selected);
-      assertEq("chrome://newtab/", tab.pendingUrl);
+      smellsLikeNewTabPage(tab.pendingUrl, tab.title);
       assertEq("", tab.url);
-      // For most create calls the title will be an empty string until the
-      // navigation commits, but the new tab page is an exception to this.
-      assertEq("New Tab", tab.title);
       assertEq(false, tab.pinned);
       waitForAllTabs(pass(function() {
         chrome.tabs.get(tab.id, pass(function(tab) {
-          assertEq("chrome://newtab/", tab.url);
-          assertEq("New Tab", tab.title);
+          smellsLikeNewTabPage(tab.url, tab.title);
           assertEq(undefined, tab.pendingUrl);
         }));
       }));
@@ -104,7 +117,7 @@ chrome.test.runTests([
         assertEq(1, win.tabs.length);
         // In case the URL has or has not committed yet, check both.
         const url = win.tabs[0].pendingUrl || win.tabs[0].url;
-        assertEq("chrome://newtab/", url);
+        smellsLikeNewTabPage(url, win.tabs[0].title);
       });
     };
 
@@ -129,7 +142,6 @@ chrome.test.runTests([
         // In case the URL has or has not committed yet, check both.
         const url = win.tabs[0].pendingUrl || win.tabs[0].url;
         assertEq(pageUrl('a'), url);
-        lastWindowId = win.id;
       }));
     }));
   },
@@ -145,5 +157,29 @@ chrome.test.runTests([
       assertEq(null, window);
     }));
   },
+];
 
-])});
+// The following tests don't work on desktop android (yet).
+// TODO(https://crbug.com/371432155): Enable these on desktop android.
+const skipForAndroid = [
+    'createAtIndex',
+    'createWindowWithDefaultTab',
+    'createWindowWithExistingTab',
+    'windowCreate',
+];
+
+(async function() {
+  const os = await new Promise((resolve) => {
+    chrome.runtime.getPlatformInfo(info => resolve(info.os));
+  });
+  isAndroid = os === 'android';
+  let testsToRun = tests;
+  if (isAndroid) {
+    testsToRun =
+        tests.filter((t) => { return !skipForAndroid.includes(t.name); });
+  }
+
+  await loadScript;
+
+  chrome.test.runTests(testsToRun);
+})();
