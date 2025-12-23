b0055c723ebf1af180ab4ee50a095390b9dc04dd
Daniel White <danieltwhite@google.com>
2025-12-23 09:58:29 -0800

    [iOS] Fixed testPasswordLeakCheckToggle EGTest flakiness
    The testPasswordLeakCheckToggle_PresentWhenFeatureFlagDisabled EGTest is
    flaky because it fails to scroll the entire width of the destination's
    carousel in the overflow menu. In some cases, the desired destination
    (i.e Settings) is not fully rendered on the screen before the EGTest
    runner attempts to tap the destination. In the event the destination is
    only partially rendered, the attempted tap fails and the Settings menu
    does not open. This event occurs when the Settings destination is the
    last destination in the carousel. This explains why the test is flaky.
    The destination carousel stochastically determines the ordering of the
    destination items.
    
    Since the EGTest is failing during the build-up to testing the Safe
    Browsing Settings UI, which what we actually want to test, this makes
    refactoring this test using the new Coordinator EGTest pattern a perfect
    candidate.
    
    In this CL, a new function 'startPrivacySafeBrowsingCoordinator` is
    added to the ChromeCoordinatorAppInterface. This allows the Safe
    Browsing Settings to be displayed as standalone UI, avoiding the
    navigation through the overflow menu.
    
    Fixed: 468292645
    Change-Id: I391c902c8644ead4a0264ed77b7bf7c1471bdeaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7302059
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Owners-Override: Daniel White <danieltwhite@google.com>
    Reviewed-by: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1562230}

--- ios/chrome/browser/settings/ui_bundled/privacy/privacy_safe_browsing_egtest.mm
+++ ios/chrome/browser/settings/ui_bundled/privacy/privacy_safe_browsing_egtest.mm
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/grit/ios_branded_strings.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ios/chrome/test/earl_grey/chrome_actions.h"
+#import "ios/chrome/test/earl_grey/chrome_coordinator_app_interface.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
@@ -401,9 +402,7 @@ void PressInfoButtonForCell(NSString* cellId) {
 
 // Tests that the Password Leak detection toggle doesn't under Standard
 // Protection if the the feature is enabled.
-//
-// TODO(crbug.com/468292645): This test is flaky.
-- (void)FLAKY_testPasswordLeakCheckToggle_PresentWhenFeatureFlagDisabled {
+- (void)testPasswordLeakCheckToggle_PresentWhenFeatureFlagDisabled {
   // Ensure that Safe Browsing and password leak detection opt-outs start in
   // their default (opted-in) state.
   [ChromeEarlGrey setBoolValue:YES forUserPref:prefs::kSafeBrowsingEnabled];
@@ -416,7 +415,7 @@ void PressInfoButtonForCell(NSString* cellId) {
   [SigninEarlGrey signinWithFakeIdentity:fakeIdentity];
 
   // Open Privacy Safe Browsing settings.
-  OpenPrivacySafeBrowsingSettings();
+  [ChromeCoordinatorAppInterface startPrivacySafeBrowsingCoordinator];
 
   // Open Standard Protection menu.
   PressInfoButtonForCell(kSettingsSafeBrowsingStandardProtectionCellId);
--- ios/chrome/test/earl_grey/chrome_coordinator_app_interface.h
+++ ios/chrome/test/earl_grey/chrome_coordinator_app_interface.h
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 + (void)startHistoryCoordinator;
 + (void)startNewTabPageCoordinator;
 + (void)startPopupMenuCoordinator;
++ (void)startPrivacySafeBrowsingCoordinator;
 + (void)startOmniboxCoordinator;
 + (void)startQRScannerLegacyCoordinator;
 + (void)startSearchWhatYouSeePromoCoordinator;
