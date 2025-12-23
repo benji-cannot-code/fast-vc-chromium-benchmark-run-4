59cd469f237cd8ac887b7379b547308669ce43ea
chrstne <chrstne@google.com>
2025-12-23 11:13:08 -0800

    Remove unused StopTask code in AUSM, TaskIcon
    Bug: 469817191
    Change-Id: Ib1e694d7f4bfa69c03761fdc1f56c5ee3394edcd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7309637
    Commit-Queue: Christine Ying <chrstne@google.com>
    Reviewed-by: Kene Okoye <kenok@google.com>
    Cr-Commit-Position: refs/heads/main@{#1562266}

--- chrome/browser/ui/tabs/glic_actor_task_icon_manager.h
+++ chrome/browser/ui/tabs/glic_actor_task_icon_manager.h
@@ -41,9 +41,7 @@ class GlicActorTaskIconManager : public KeyedService {
   void OnActorTaskStateUpdate(actor::TaskId task_id);
 
   // Called whenever an actor task is completed.
-  void OnActorTaskStopped(actor::TaskId task_id,
-                          actor::ActorTask::State final_state,
-                          std::string task_title);
+  void OnActorTaskStopped(actor::TaskId task_id);
 
   // Called whenever an actor task is removed.
   void OnActorTaskRemoved(actor::TaskId task_id);
@@ -70,8 +68,6 @@ class GlicActorTaskIconManager : public KeyedService {
   actor::ui::ActorTaskNudgeState GetCurrentActorTaskNudgeState() const;
 
   raw_ptr<tabs::TabInterface> GetLastUpdatedTabForTaskId(actor::TaskId task_id);
-
-  void ClearStoppedTasks();
 
   std::map<actor::TaskId, ActorTaskListBubbleRowState>
   GetActorTaskListBubbleRows() const {
@@ -106,13 +102,6 @@ class GlicActorTaskIconManager : public KeyedService {
 
   // TODO(mjenn): Update implementation for multi-tab actuation.
   actor::TaskId current_task_id_;
-
-  // TODO(b/440770955): Replace complete task lists (complete + fail) with a
-  // snapshot (task title, state and tab handle) of the completed or failed
-  // tasks for the pop-over.
-  bool has_unprocessed_completed_tasks_ = false;
-  // Whether there is an unprocessed failed task.
-  bool has_unprocessed_failed_tasks_ = false;
 
   // Map of tasks needing notifications.
   std::map<actor::TaskId, ActorTaskListBubbleRowState>
