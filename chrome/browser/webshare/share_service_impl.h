4f5df4f912a99573f4f7626dc012fee6e5766811
jj <jj@imput.net>
2025-12-23 03:00:02 -0800

    [webshare] Fix building with safe_browsing_mode=0
    Fixes the following:
    ../../chrome/browser/webshare/share_service_impl.cc(228,46): error: no member named 'safe_browsing_service' in 'BrowserProcess'
      228 |   if (should_check_url && g_browser_process->safe_browsing_service()) {
          |                           ~~~~~~~~~~~~~~~~~  ^
    ../../chrome/browser/webshare/share_service_impl.cc(230,28): error: no member named 'safe_browsing_service' in 'BrowserProcess'
      230 |         g_browser_process->safe_browsing_service()->database_manager(),
          |         ~~~~~~~~~~~~~~~~~  ^
    
    Bug: 390205854
    Change-Id: I4949a1a081efc6305efe6262e4367bec1c83f08d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7277410
    Commit-Queue: jj <jj@chromium.org>
    Auto-Submit: jj <jj@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1562135}

--- chrome/browser/webshare/share_service_impl.h
+++ chrome/browser/webshare/share_service_impl.h
@@ -14,12 +14,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/memory/weak_ptr.h"
 #include "build/build_config.h"
-#include "chrome/browser/webshare/safe_browsing_request.h"
+#include "components/safe_browsing/buildflags.h"
 #include "content/public/browser/document_service.h"
 #include "third_party/blink/public/mojom/webshare/webshare.mojom.h"
 
 #if BUILDFLAG(IS_CHROMEOS)
 #include "chrome/browser/webshare/chromeos/sharesheet_client.h"
+#endif
+
+#if BUILDFLAG(SAFE_BROWSING_AVAILABLE)
+#include "chrome/browser/webshare/safe_browsing_request.h"
 #endif
 
 class GURL;
@@ -57,19 +61,20 @@ class ShareServiceImpl
              ShareCallback callback) override;
 
  private:
-  void OnSafeBrowsingResultReceived(
-      const std::string& title,
-      const std::string& text,
-      const GURL& share_url,
-      std::vector<blink::mojom::SharedFilePtr> files,
-      ShareCallback callback,
-      bool is_safe);
+  void RunShareOperation(const std::string& title,
+                         const std::string& text,
+                         const GURL& share_url,
+                         std::vector<blink::mojom::SharedFilePtr> files,
+                         ShareCallback callback,
+                         bool is_safe);
 
   ShareServiceImpl(content::RenderFrameHost& render_frame_host,
                    mojo::PendingReceiver<blink::mojom::ShareService> receiver);
   ~ShareServiceImpl() override;
 
+#if BUILDFLAG(SAFE_BROWSING_AVAILABLE)
   std::optional<SafeBrowsingRequest> safe_browsing_request_;
+#endif
 
 #if BUILDFLAG(IS_CHROMEOS)
   webshare::SharesheetClient sharesheet_client_;
