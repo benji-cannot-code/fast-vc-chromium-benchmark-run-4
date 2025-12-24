55a25fd1f7a65660ed1528b8276de5674fad4025
S. Ganesh <ganesh@chromium.org>
2025-12-23 17:19:57 -0800

    Delete the ChromeRecovery directory when the elevation service runs
    Fixed: 471269919
    Change-Id: I4b47057ba82b70aa99f6492688687ce669454e3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7312757
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1562391}

--- chrome/elevation_service/elevated_recovery_impl.cc
+++ chrome/elevation_service/elevated_recovery_impl.cc
@@ -254,18 +254,6 @@ HRESULT ValidateCRXArgs(const std::wstring& browser_appid,
   return ::IIDFromString(session_id.c_str(), &session_guid);
 }
 
-// Deletes all the files and subdirectories within |directory_path|. Errors are
-// ignored.
-void DeleteDirectoryFiles(const base::FilePath& directory_path) {
-  base::FileEnumerator file_enum(
-      directory_path, false,
-      base::FileEnumerator::FILES | base::FileEnumerator::DIRECTORIES);
-  for (base::FilePath current = file_enum.Next(); !current.empty();
-       current = file_enum.Next()) {
-    base::DeletePathRecursively(current);
-  }
-}
-
 // Schedules deletion after reboot of |dir_name| as well as all the files and
 // subdirectories within |dir_name|. Errors are ignored.
 void ScheduleDirectoryForDeletion(const base::FilePath& dir_name) {
@@ -310,7 +298,7 @@ HRESULT CleanupChromeRecoveryDirectory() {
   if (FAILED(hr))
     return hr;
 
-  DeleteDirectoryFiles(recovery_dir);
+  base::DeletePathRecursively(recovery_dir);
 
   return S_OK;
 }
