c873a0ed84225813f1999dd1f986e0a20eee9226
Tomek Jurkiewicz <tju@google.com>
2025-12-24 06:27:00 -0800

    Close incognito tabs when the feature disables it.
    After this change, incognito tabs are changed when the related pref is
    updated, rather than as a side-effect of dependent changes. However, to
    avoid closing incognito tabs for other users, the supervision status is
    checked directly.
    
    Bug: 447414264, 471369233
    Change-Id: Ic0aa7ab516c45d0dbf99fcdf20672a117d3a7aa5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7320460
    Reviewed-by: Duong Dac <ddac@chromium.org>
    Commit-Queue: Tomek Jurkiewicz <tju@google.com>
    Cr-Commit-Position: refs/heads/main@{#1562492}

--- components/supervised_user/core/browser/supervised_user_service.cc
+++ components/supervised_user/core/browser/supervised_user_service.cc
@@ -245,6 +245,11 @@ SupervisedUserService::SupervisedUserService(
       prefs::kSupervisedUserId,
       base::BindRepeating(&SupervisedUserService::OnSupervisedUserIdChanged,
                           base::Unretained(this)));
+  main_pref_change_registrar_.Add(
+      policy::policy_prefs::kIncognitoModeAvailability,
+      base::BindRepeating(
+          &SupervisedUserService::OnIncognitoModeAvailabilityChanged,
+          base::Unretained(this)));
 
   OnSupervisedUserIdChanged();
 }
@@ -310,10 +315,6 @@ void SupervisedUserService::OnFamilyLinkParentalControlsEnabled() {
 
   // Also disables incognito mode.
   SetSettingsServiceActive(true);
-  // TODO(crbug.com/447414264): Check if tabs should be closed in the first
-  // place.
-  platform_delegate_->CloseIncognitoTabs();
-
   remote_web_approvals_manager_.AddApprovalRequestCreator(
       std::make_unique<PermissionRequestCreatorImpl>(identity_manager_,
                                                      url_loader_factory_));
@@ -372,6 +373,14 @@ void SupervisedUserService::RemoveURLFilterPrefChangeHandlers() {
 
 void SupervisedUserService::RemoveCustodianPrefChangeHandlers() {
   custodian_pref_change_registrar_.RemoveAll();
+}
+
+void SupervisedUserService::OnIncognitoModeAvailabilityChanged() {
+  bool is_supervised =
+      IsSupervisedLocally() || IsSubjectToParentalControls(user_prefs_.get());
+  if (is_supervised && platform_delegate_->ShouldCloseIncognitoTabs()) {
+    platform_delegate_->CloseIncognitoTabs();
+  }
 }
 
 void SupervisedUserService::OnCustodianInfoChanged() {
@@ -443,9 +452,6 @@ void SupervisedUserService::OnSearchContentFiltersEnabled() {
 
   settings_service_->SetSuspended(true);
   content_filters_service_->SetSearchFiltersEnabled(true);
-  if (platform_delegate_->ShouldCloseIncognitoTabs()) {
-    platform_delegate_->CloseIncognitoTabs();
-  }
 
   // Required to emit WebFilterType metrics.
   UpdateURLFilter();
@@ -475,9 +481,6 @@ void SupervisedUserService::OnBrowserContentFiltersEnabled() {
   RemoveURLFilterPrefChangeHandlers();
   settings_service_->SetSuspended(true);
   content_filters_service_->SetBrowserFiltersEnabled(true);
-  if (platform_delegate_->ShouldCloseIncognitoTabs()) {
-    platform_delegate_->CloseIncognitoTabs();
-  }
 
   // Add handlers that will prevent unsupported url filter changes.
   AddURLFilterPrefChangeSentinels();
