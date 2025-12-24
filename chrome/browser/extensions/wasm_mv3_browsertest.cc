24c8fc0aac3fc419089c3c2b2e4372ce650c64e8
James Cook <jamescook@chromium.org>
2025-12-23 16:22:28 -0800

    extensions: Add WasmMV3BrowserTest to desktop Android
    This test was overlooked when the initial port of the core extensions
    system was done. No new features are being tested.
    
    Bug: 469417243
    Change-Id: I5de4593f61a9a471ec8afac4563b475d604da32b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7309639
    Auto-Submit: James Cook <jamescook@chromium.org>
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Commit-Queue: Michael Wojcicka <mwoj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1562375}

--- chrome/browser/extensions/wasm_mv3_browsertest.cc
+++ chrome/browser/extensions/wasm_mv3_browsertest.cc
@@ -5,10 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "content/public/test/browser_test.h"
+#include "extensions/buildflags/buildflags.h"
 #include "extensions/common/extension_features.h"
 #include "extensions/test/extension_test_message_listener.h"
 #include "extensions/test/result_catcher.h"
 #include "testing/gtest/include/gtest/gtest.h"
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace extensions {
 
