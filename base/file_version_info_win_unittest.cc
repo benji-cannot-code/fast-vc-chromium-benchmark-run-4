744f8e3d87f655063b76f901277eb2f14e927a3b
David Bienvenu <davidbienvenu@chromium.org>
2025-12-24 10:48:45 -0800

    fix UNSAFE_TODO in file_version_info_win_unittest.cc
    use base::span of std::wstring_view[] to fix UNSAFE_TODO's.
    
    Bug: 42271176
    Change-Id: I9d3d2d737f9c0c6afc881e01a918eef245d4e864
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7308980
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1562513}

--- base/file_version_info_win_unittest.cc
+++ base/file_version_info_win_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/compiler_specific.h"
+#include "base/containers/span.h"
 #include "base/file_version_info.h"
 #include "base/files/file_path.h"
 #include "base/path_service.h"
@@ -84,7 +85,7 @@ TYPED_TEST(FileVersionInfoTest, HardCodedProperties) {
   const base::FilePath::CharType kDLLName[] =
       FILE_PATH_LITERAL("FileVersionInfoTest1.dll");
 
-  const wchar_t* const kExpectedValues[15] = {
+  static constexpr std::wstring_view kExpectedValues[15] = {
       // FileVersionInfoTest.dll
       L"Goooooogle",                      // company_name
       L"Google",                          // company_short_name
@@ -98,6 +99,8 @@ TYPED_TEST(FileVersionInfoTest, HardCodedProperties) {
       L"1.2.3.4",                         // file_version
   };
 
+  auto expected_span = base::span(kExpectedValues);
+
   FilePath dll_path = GetTestDataPath();
   dll_path = dll_path.Append(kDLLName);
 
@@ -106,25 +109,25 @@ TYPED_TEST(FileVersionInfoTest, HardCodedProperties) {
   ASSERT_TRUE(version_info);
 
   int j = 0;
-  EXPECT_EQ(UNSAFE_TODO(kExpectedValues[j++]),
+  EXPECT_EQ(expected_span[j++],
             base::AsWStringView(version_info->company_name()));
-  EXPECT_EQ(UNSAFE_TODO(kExpectedValues[j++]),
+  EXPECT_EQ(expected_span[j++],
             base::AsWStringView(version_info->company_short_name()));
-  EXPECT_EQ(UNSAFE_TODO(kExpectedValues[j++]),
+  EXPECT_EQ(expected_span[j++],
             base::AsWStringView(version_info->product_name()));
-  EXPECT_EQ(UNSAFE_TODO(kExpectedValues[j++]),
+  EXPECT_EQ(expected_span[j++],
             base::AsWStringView(version_info->product_short_name()));
-  EXPECT_EQ(UNSAFE_TODO(kExpectedValues[j++]),
+  EXPECT_EQ(expected_span[j++],
             base::AsWStringView(version_info->internal_name()));
-  EXPECT_EQ(UNSAFE_TODO(kExpectedValues[j++]),
+  EXPECT_EQ(expected_span[j++],
             base::AsWStringView(version_info->product_version()));
-  EXPECT_EQ(UNSAFE_TODO(kExpectedValues[j++]),
+  EXPECT_EQ(expected_span[j++],
             base::AsWStringView(version_info->special_build()));
-  EXPECT_EQ(UNSAFE_TODO(kExpectedValues[j++]),
+  EXPECT_EQ(expected_span[j++],
             base::AsWStringView(version_info->original_filename()));
-  EXPECT_EQ(UNSAFE_TODO(kExpectedValues[j++]),
+  EXPECT_EQ(expected_span[j++],
             base::AsWStringView(version_info->file_description()));
-  EXPECT_EQ(UNSAFE_TODO(kExpectedValues[j++]),
+  EXPECT_EQ(expected_span[j++],
             base::AsWStringView(version_info->file_version()));
 }
 
