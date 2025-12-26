487a16071f70ead6a1e380942e0926c2ec8f6d8a
Jason Hu <hujasonx@google.com>
2025-12-26 07:14:39 -0800

    [context-input] Set lens_overlay surface from composebox query controller
    This change makes it so that ALL contextual queries from Chrome ues the same surface.
    
    Bug: b:471020355
    Change-Id: I17103619dbb9c54d430ca8c201b4e28ca9d32158
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7305950
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1562728}

--- components/contextual_search/internal/composebox_query_controller_unittest.cc
+++ components/contextual_search/internal/composebox_query_controller_unittest.cc
@@ -1891,7 +1891,7 @@ TEST_F(ComposeboxQueryControllerTest, CreateClientContextHasCorrectValues) {
   lens::LensOverlayClientContext client_context = controller().client_context();
 
   // Assert: Validate the client context values.
-  EXPECT_EQ(client_context.surface(), lens::SURFACE_CHROME_NTP);
+  EXPECT_EQ(client_context.surface(), lens::SURFACE_LENS_OVERLAY);
   EXPECT_EQ(client_context.platform(), lens::PLATFORM_LENS_OVERLAY);
   EXPECT_EQ(client_context.locale_context().language(), kLocale);
   EXPECT_EQ(client_context.locale_context().region(), kRegion);
@@ -2921,7 +2921,7 @@ TEST_F(ComposeboxQueryControllerTest, QuerySubmittedWithLnsSurfaceAndNoImage) {
   std::string lns_surface_value;
   EXPECT_TRUE(net::GetValueForKeyInQuery(aim_url, kLnsSurfaceParameterKey,
                                          &lns_surface_value));
-  EXPECT_EQ(lns_surface_value, "47");
+  EXPECT_EQ(lns_surface_value, "42");
 }
 
 TEST_F(ComposeboxQueryControllerTest,
