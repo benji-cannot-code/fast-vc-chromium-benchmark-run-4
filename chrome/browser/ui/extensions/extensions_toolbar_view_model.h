c18d89ff6d08de94636cab4dc121d573ae3182ec
Masa Fujita <massan@google.com>
2025-12-26 06:01:54 -0800

    actions revamp: Use base::flat_map for ETVM
    This CL swaps `std::map` to `base::flat_map` for better performance.
    This CL should have no functional change.
    
    Bug: 461981075
    Change-Id: I5d1a10e08b4cf9ec428373bd45cd3eb5b148dbf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7307809
    Commit-Queue: Masa Fujita <massan@google.com>
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1562719}

--- chrome/browser/ui/extensions/extensions_toolbar_view_model.h
+++ chrome/browser/ui/extensions/extensions_toolbar_view_model.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_EXTENSIONS_EXTENSIONS_TOOLBAR_VIEW_MODEL_H_
 #define CHROME_BROWSER_UI_EXTENSIONS_EXTENSIONS_TOOLBAR_VIEW_MODEL_H_
 
+#include "base/containers/flat_map.h"
 #include "chrome/browser/ui/extensions/extension_action_delegate.h"
 #include "chrome/browser/ui/extensions/extension_action_view_model.h"
 #include "chrome/browser/ui/toolbar/toolbar_action_view_model.h"
@@ -109,8 +110,8 @@ class ExtensionsToolbarViewModel : public ToolbarActionsModel::Observer {
   base::ObserverList<Observer> observers_;
 
   // Actions for all extensions.
-  std::map<ToolbarActionsModel::ActionId,
-           std::unique_ptr<ToolbarActionViewModel>>
+  base::flat_map<ToolbarActionsModel::ActionId,
+                 std::unique_ptr<ToolbarActionViewModel>>
       actions_;
 };
 
