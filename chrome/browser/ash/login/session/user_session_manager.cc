787f33cc4dd579459f989e8470a985fb323b345b
Achuith Bhandarkar <achuith@chromium.org>
2025-12-27 10:06:49 -0800

    Use CheckedObserver in UserManager::Observer
    Also in UserSessionStateObserver (both UserManager and ash)
    
    Bug: 470679673
    Change-Id: I28befd6a080bc936ecc3fc0e55451bd9043f8200
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7320509
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1562901}

--- chrome/browser/ash/login/session/user_session_manager.cc
+++ chrome/browser/ash/login/session/user_session_manager.cc
@@ -612,12 +612,6 @@ void MaybeSaveSessionStartedTimeBeforeRestart(Profile* profile) {
 
 }  // namespace
 
-UserSessionManagerDelegate::~UserSessionManagerDelegate() = default;
-
-void UserSessionStateObserver::PendingUserSessionsRestoreFinished() {}
-
-UserSessionStateObserver::~UserSessionStateObserver() = default;
-
 // static
 UserSessionManager* UserSessionManager::GetInstance() {
   return base::Singleton<UserSessionManager, base::DefaultSingletonTraits<
