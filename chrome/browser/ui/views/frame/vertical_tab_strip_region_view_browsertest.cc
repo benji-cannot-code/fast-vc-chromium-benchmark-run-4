fb547918a5db5de1763ed0e045f84251ca80a595
Eshwar Stalin <estalin@chromium.org>
2025-12-29 09:05:58 -0800

    Fixing tab separator visibility logic in collapsed state
    A tab separator is added for collapsed state in VT. However, this should
    only appear if there are tabs in the pinned container, otherwise we see
    an extra separator in collapsed state. This fixes that and adds
    additional tests to cover this scenario.
    
    Change-Id: I7629138ed16cf8e930eddb9ed8256082e3dc24a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7339086
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1563150}

--- chrome/browser/ui/views/frame/vertical_tab_strip_region_view_browsertest.cc
+++ chrome/browser/ui/views/frame/vertical_tab_strip_region_view_browsertest.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/tabs/vertical/vertical_unpinned_tab_container_view.h"
 #include "chrome/browser/ui/views/test/vertical_tabs_browser_test_mixin.h"
 #include "chrome/test/base/in_process_browser_test.h"
+#include "chrome/test/base/ui_test_utils.h"
 #include "components/tabs/public/tab_group.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test.h"
@@ -87,13 +88,18 @@ class VerticalTabStripRegionViewTest
 
 IN_PROC_BROWSER_TEST_F(VerticalTabStripRegionViewTest,
                        SeparatorVisibilityChangesWithCollapsedState) {
+  auto* tabs_separator = region_view()->tabs_separator_for_testing();
+
   state_controller()->SetCollapsed(true);
   EXPECT_TRUE(state_controller()->IsCollapsed());
-  EXPECT_TRUE(region_view()->tabs_separator_for_testing()->GetVisible());
+  ui_test_utils::ViewVisibilityWaiter(tabs_separator, false).Wait();
+
+  AppendPinnedTab();
+  ui_test_utils::ViewVisibilityWaiter(tabs_separator, true).Wait();
 
   state_controller()->SetCollapsed(false);
   EXPECT_FALSE(state_controller()->IsCollapsed());
-  EXPECT_FALSE(region_view()->tabs_separator_for_testing()->GetVisible());
+  ui_test_utils::ViewVisibilityWaiter(tabs_separator, false).Wait();
 }
 
 IN_PROC_BROWSER_TEST_F(VerticalTabStripRegionViewTest, ResizeAreaBounds) {
