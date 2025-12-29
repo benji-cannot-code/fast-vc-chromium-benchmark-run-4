2d853c9ea1ab78a9d7634196c443d380a3208363
Joe Peplowski <joepeplowski@google.com>
2025-12-29 07:08:48 -0800

    Add GeminiSettingsContext forward declaration
    CL chain:
        crrev.com/c/7264487
        crrev.com/i/8856997
    ==> crrev.com/c/7269246
    
    Bug: 462382850
    Change-Id: I5ee7d3cdad7d7890c4f676b933e6f5e05721a2fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7269246
    Reviewed-by: Ibrahim Kanouche <kanouche@google.com>
    Commit-Queue: Joe Peplowski <joepeplowski@google.com>
    Reviewed-by: Joemer Ramos <joemerramos@google.com>
    Cr-Commit-Position: refs/heads/main@{#1563117}

--- ios/chrome/browser/settings/ui_bundled/bwg/model/gemini_settings_metadata.mm
+++ ios/chrome/browser/settings/ui_bundled/bwg/model/gemini_settings_metadata.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/settings/ui_bundled/bwg/model/gemini_settings_metadata.h"
 
+#import "ios/chrome/browser/settings/ui_bundled/bwg/model/gemini_settings_context.h"
+
 @implementation GeminiSettingsMetadata
 
 - (instancetype)initWithTitle:(NSString*)title
