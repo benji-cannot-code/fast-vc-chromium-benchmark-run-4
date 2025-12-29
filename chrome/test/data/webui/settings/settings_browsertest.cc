dc9f5449ad901a8ebcf1edf8d902e39cc1261a2b
Michelle Abreo <michelleabreo@google.com>
2025-12-29 15:40:47 -0800

    [WAConsent] Gate web actuation settings on separate feature flag
    Introduces `kGlicWebActuationSettingsToggle` to control the visibility
    of the web actuation settings toggle when tiers are configured.
    
    Previously, populating `kGlicWebActuationAllowedTiers` would
    automatically expose the settings toggle to users. This change
    adds a specific feature gate, allowing the tier configuration to be
    populated for backend purposes without immediately showing the UI.
    
    Bug: b:461844626
    Change-Id: Ie2ef632be71eea5f5674a1ea7911cecafc4ab455
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7311687
    Commit-Queue: Michelle Abreo <michelleabreo@google.com>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1563257}

--- chrome/test/data/webui/settings/settings_browsertest.cc
+++ chrome/test/data/webui/settings/settings_browsertest.cc
@@ -613,6 +613,7 @@ class SettingsGlicSubPageWebActuationAllowedTierToggleTest
     // Set the allowed tiers to "100" and "200"
     scoped_feature_list_.InitWithFeaturesAndParameters(
         {{features::kGlicWebActuationSetting, {}},
+         {features::kGlicWebActuationSettingsToggle, {}},
          {features::kGlicActor,
           {{features::kGlicActorEligibleTiers.name, "100,200"}}}},
         {});
