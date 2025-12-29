59e000b421a4f825bf64dc7abcfa8b26e4e4023f
Federica Germinario <fedegermi@google.com>
2025-12-29 09:47:47 -0800

    Reland "Add GeminiSettingsContext forward declaration"
    This reverts commit 30b5ae7573d41c4febf4777879db0acaa16e31fd.
    
    Reason for revert: internal code was updated, this change can be merged now
    
    Original change's description:
    > Revert "Add GeminiSettingsContext forward declaration"
    >
    > This reverts commit bc445847e7159fdb173d9ec60374fae18079db0a.
    >
    > Reason for revert: Build is broken
    >
    > Original change's description:
    > > Add GeminiSettingsContext forward declaration
    > >
    > > CL chain:
    > >     crrev.com/c/7264487
    > >     crrev.com/i/8856997
    > > ==> crrev.com/c/7269246
    > >
    > > Bug: 462382850
    > > Change-Id: I5ee7d3cdad7d7890c4f676b933e6f5e05721a2fb
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7269246
    > > Reviewed-by: Ibrahim Kanouche <kanouche@google.com>
    > > Commit-Queue: Joe Peplowski <joepeplowski@google.com>
    > > Reviewed-by: Joemer Ramos <joemerramos@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1563117}
    >
    > Bug: 462382850
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I2dff4e66e8549f56ddff0580db5e639f2393f522
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7319363
    > Auto-Submit: Federica Germinario <fedegermi@google.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Federica Germinario <fedegermi@google.com>
    > Owners-Override: Federica Germinario <fedegermi@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1563128}
    
    Bug: 462382850
    Change-Id: Iebb6f5a9f2f10fe7fad5456a37fe77e09dfa8dfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7352725
    Reviewed-by: Joemer Ramos <joemerramos@google.com>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Ibrahim Kanouche <kanouche@google.com>
    Cr-Commit-Position: refs/heads/main@{#1563160}

--- ios/chrome/browser/settings/ui_bundled/bwg/model/gemini_settings_metadata.h
+++ ios/chrome/browser/settings/ui_bundled/bwg/model/gemini_settings_metadata.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
-#import "ios/chrome/browser/settings/ui_bundled/bwg/model/gemini_settings_context.h"
+typedef NS_ENUM(NSInteger, GeminiSettingsContext);
 
 // Metadata about a Gemini setting item.
 @interface GeminiSettingsMetadata : NSObject
--- ios/chrome/browser/settings/ui_bundled/bwg/ui/bwg_settings_view_controller.mm
+++ ios/chrome/browser/settings/ui_bundled/bwg/ui/bwg_settings_view_controller.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/settings/ui_bundled/bwg/model/gemini_dynamic_settings_item.h"
 #import "ios/chrome/browser/settings/ui_bundled/bwg/model/gemini_settings_action.h"
 #import "ios/chrome/browser/settings/ui_bundled/bwg/model/gemini_settings_action_type.h"
+#import "ios/chrome/browser/settings/ui_bundled/bwg/model/gemini_settings_context.h"
 #import "ios/chrome/browser/settings/ui_bundled/bwg/model/gemini_settings_metadata.h"
 #import "ios/chrome/browser/settings/ui_bundled/bwg/ui/bwg_location_view_controller.h"
 #import "ios/chrome/browser/shared/model/url/chrome_url_constants.h"
