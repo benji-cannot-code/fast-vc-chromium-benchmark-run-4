8e22de70af924f16339e98298d62d5e2593eecd2
Sarah Krakowiak Criel <skrakowi@chromium.org>
2025-12-29 13:43:22 -0800

    Remove CreateAndSendHitReport,MaybeReportSafeBrowsingHit methods
    This is part of hit report deprecation.
    
    Bug: 468374319
    Change-Id: I24a8185e77b0ad3a00dd887a72109853d0ed53c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7352511
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Auto-Submit: Sarah Krakowiak <skrakowi@chromium.org>
    Commit-Queue: Sarah Krakowiak <skrakowi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1563224}

--- chrome/browser/safe_browsing/download_protection/download_url_sb_client.cc
+++ chrome/browser/safe_browsing/download_protection/download_url_sb_client.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/string_number_conversions.h"
-#include "chrome/browser/metrics/chrome_metrics_service_accessor.h"
 #include "chrome/browser/safe_browsing/download_protection/download_protection_service.h"
 #include "components/safe_browsing/content/browser/client_report_util.h"
 #include "components/safe_browsing/content/browser/safe_browsing_navigation_observer_manager.h"
@@ -20,8 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/download_item_utils.h"
 
 namespace safe_browsing {
-
-using enum ExtendedReportingLevel;
 
 DownloadUrlSBClient::DownloadUrlSBClient(
     download::DownloadItem* item,
@@ -45,13 +42,6 @@ DownloadUrlSBClient::DownloadUrlSBClient(
   DCHECK(item_);
   DCHECK(service_);
   download_item_observation_.Observe(item_.get());
-  Profile* profile = Profile::FromBrowserContext(
-      content::DownloadItemUtils::GetBrowserContext(item_));
-  extended_reporting_level_ =
-      profile ? GetExtendedReportingLevel(*profile->GetPrefs())
-              : SBER_LEVEL_OFF;
-  is_enhanced_protection_ =
-      profile ? IsEnhancedProtectionEnabled(*profile->GetPrefs()) : false;
 }
 
 // Implements DownloadItem::Observer.
@@ -100,8 +90,7 @@ void DownloadUrlSBClient::CheckDone(SBThreatType threat_type) {
   if (threat_type != SBThreatType::SB_THREAT_TYPE_SAFE) {
     UpdateDownloadCheckStats(dangerous_type_);
     content::GetUIThreadTaskRunner({})->PostTask(
-        FROM_HERE,
-        base::BindOnce(&DownloadUrlSBClient::ReportMalware, this, threat_type));
+        FROM_HERE, base::BindOnce(&DownloadUrlSBClient::ReportMalware, this));
   } else {
     // Identify download referrer chain, which will be used in
     // ClientDownloadRequest.
@@ -113,7 +102,7 @@ void DownloadUrlSBClient::CheckDone(SBThreatType threat_type) {
       FROM_HERE, base::BindOnce(std::move(callback_), result));
 }
 
-void DownloadUrlSBClient::ReportMalware(SBThreatType threat_type) {
+void DownloadUrlSBClient::ReportMalware() {
   std::string post_data;
   if (!sha256_hash_.empty()) {
     post_data += base::HexEncode(sha256_hash_) + "\n";
@@ -121,22 +110,6 @@ void DownloadUrlSBClient::ReportMalware(SBThreatType threat_type) {
   for (size_t i = 0; i < url_chain_.size(); ++i) {
     post_data += url_chain_[i].spec() + "\n";
   }
-
-  std::unique_ptr<HitReport> hit_report = std::make_unique<HitReport>();
-  hit_report->malicious_url = url_chain_.back();
-  hit_report->page_url = url_chain_.front();
-  hit_report->referrer_url = referrer_url_;
-  hit_report->is_subresource = true;
-  hit_report->threat_type = threat_type;
-  hit_report->threat_source = database_manager_->GetNonBrowseUrlThreatSource();
-  hit_report->post_data = post_data;
-  hit_report->extended_reporting_level = extended_reporting_level_;
-  hit_report->is_enhanced_protection = is_enhanced_protection_;
-  hit_report->is_metrics_reporting_active =
-      ChromeMetricsServiceAccessor::IsMetricsAndCrashReportingEnabled();
-
-  ui_manager_->MaybeReportSafeBrowsingHit(
-      std::move(hit_report), content::DownloadItemUtils::GetWebContents(item_));
 
   if (base::FeatureList::IsEnabled(
           safe_browsing::kCreateWarningShownClientSafeBrowsingReports)) {
--- components/safe_browsing/content/browser/base_ui_manager.cc
+++ components/safe_browsing/content/browser/base_ui_manager.cc
@@ -392,7 +392,6 @@ void BaseUIManager::DisplayBlockingPage(const UnsafeResource& resource) {
       !already_reported) {
     // TODO(vakh): crbug/883462: The reports for SB_THREAT_TYPE_BILLING should
     // be disabled for M70 but enabled for a later release (M71?).
-    CreateAndSendHitReport(resource);
     if (base::FeatureList::IsEnabled(
             safe_browsing::kCreateWarningShownClientSafeBrowsingReports)) {
       CreateAndSendClientSafeBrowsingWarningShownReport(resource);
@@ -482,7 +481,6 @@ void BaseUIManager::EnsureAllowlistCreated(WebContents* web_contents) {
   AllowlistUrlSet::CreateForWebContents(web_contents);
 }
 
-void BaseUIManager::CreateAndSendHitReport(const UnsafeResource& resource) {}
 void BaseUIManager::CreateAndSendClientSafeBrowsingWarningShownReport(
     const UnsafeResource& resource) {}
 
@@ -498,16 +496,6 @@ BaseUIManager::CreateBlockingPage(
   // non-committed implementations, and this code won't be called if committed
   // interstitials are disabled.
   NOTREACHED();
-}
-
-// A SafeBrowsing hit is sent after a blocking page for malware/phishing
-// or after the warning dialog for download urls, only for extended_reporting
-// users who are not in incognito mode.
-void BaseUIManager::MaybeReportSafeBrowsingHit(
-    std::unique_ptr<HitReport> hit_report,
-    content::WebContents* web_contents) {
-  DCHECK_CURRENTLY_ON(BrowserThread::UI);
-  return;
 }
 
 // A client safe browsing report is sent after a blocking page for
