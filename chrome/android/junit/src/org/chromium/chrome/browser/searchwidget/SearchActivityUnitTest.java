b61c0bea3b0d06c29b099e860bbbaf6f2180b41f
Sinan Sahin <sinansahin@google.com>
2025-12-29 14:28:51 -0800

    [clank-q4-fixit] Rename omnibox_suggestion_bg to search_suggestion_bg_color
    Bug: 465898786
    Change-Id: Id9388eb79c70bb35bfe73ea978e1c98f004ddedd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7353650
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Owners-Override: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1563239}

--- chrome/android/junit/src/org/chromium/chrome/browser/searchwidget/SearchActivityUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/searchwidget/SearchActivityUnitTest.java
@@ -215,7 +215,7 @@ public class SearchActivityUnitTest {
         mAnchorView = new View(mActivity);
         GradientDrawable anchorViewBackground = new GradientDrawable();
         anchorViewBackground.setTint(
-                ContextCompat.getColor(mActivity, R.color.omnibox_suggestion_bg));
+                ContextCompat.getColor(mActivity, R.color.search_suggestion_bg_color));
         mAnchorView.setBackground(anchorViewBackground);
         mActivity.setAnchorViewForTesting(mAnchorView);
 
@@ -1188,7 +1188,7 @@ public class SearchActivityUnitTest {
                 ((GradientDrawable) mAnchorView.getBackground()).getColor());
         verify(mSearchBoxBackground)
                 .setBackgroundColor(
-                        ContextCompat.getColor(mActivity, R.color.omnibox_suggestion_bg));
+                        ContextCompat.getColor(mActivity, R.color.search_suggestion_bg_color));
 
         // Toggle the incognito state and check that the search box has the correct color scheme.
         mDataProvider.setIsIncognitoForTesting(true);
@@ -1211,6 +1211,6 @@ public class SearchActivityUnitTest {
                 ((GradientDrawable) mAnchorView.getBackground()).getColor());
         verify(mSearchBoxBackground, times(2))
                 .setBackgroundColor(
-                        ContextCompat.getColor(mActivity, R.color.omnibox_suggestion_bg));
+                        ContextCompat.getColor(mActivity, R.color.search_suggestion_bg_color));
     }
 }
