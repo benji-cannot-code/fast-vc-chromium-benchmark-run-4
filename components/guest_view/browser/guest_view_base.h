de31a2760a35402fc0722ed3a9d558146a68fa65
Zoraiz Naeem <zoraiznaeem@chromium.org>
2025-12-29 14:49:32 -0800

    [glic] Move draggable region forwarding logic to GuestViewBase
    Before this change, in `glic::OnGuestAdded()`, we were adding a custom
    WebContentsDelegate to guest web-contents that forwarded the draggable
    regions from guest web-contents to the embedder web-contents.
    
    However, it breaks different events that GuestViewBase forwarded to the
    embedder web-contents. GuestViewBase adds itself as WebContentsDelegate
    to the web-contents. However, we were overriding the delegate with the
    custom delegate breaking event forwarding done by GuestViewBase.
    
    In this CL, GuestViewBase handles forwarding draggable regions and
    removes the custom delegate added by `glic::OnGuestAdded()`
    
    Bug: b:471289924
    Change-Id: I4e52aa7edaabb6da2f2bd2d70d019b1d91b987d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7315743
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1563246}

--- components/guest_view/browser/guest_view_base.h
+++ components/guest_view/browser/guest_view_base.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_delegate.h"
 #include "content/public/browser/web_contents_observer.h"
+#include "third_party/blink/public/mojom/page/draggable_region.mojom-forward.h"
 
 namespace content {
 class NavigationHandle;
@@ -497,6 +498,9 @@ class GuestViewBase : public content::BrowserPluginGuestDelegate,
   void UpdatePreferredSize(content::WebContents* web_contents,
                            const gfx::Size& pref_size) final;
   void UpdateTargetURL(content::WebContents* source, const GURL& url) final;
+  void DraggableRegionsChanged(
+      const std::vector<blink::mojom::DraggableRegionPtr>& regions,
+      content::WebContents* contents) final;
 
   // WebContentsObserver implementation.
   void DidStopLoading() final;
