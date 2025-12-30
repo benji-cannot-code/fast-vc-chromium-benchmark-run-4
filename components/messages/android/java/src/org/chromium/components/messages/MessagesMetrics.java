6c18a081c5db2919c21d421c35d6281daf3b5739
Tanmoy Mollik <triploblastic@google.com>
2025-12-30 02:19:40 -0800

    [Android] Add a new message identifier for signin survey
    Bug: 429150215
    Change-Id: I498812f859d030f025b5ef87f25c8b114e563450
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7215953
    Reviewed-by: Lijin Shen <lazzzis@google.com>
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1563346}

--- components/messages/android/java/src/org/chromium/components/messages/MessagesMetrics.java
+++ components/messages/android/java/src/org/chromium/components/messages/MessagesMetrics.java
@@ -365,6 +365,8 @@ public class MessagesMetrics {
                 return "PermissionPromptLoud";
             case MessageIdentifier.SAVE_UPDATE_ENTITY:
                 return "SaveUpdateEntity";
+            case MessageIdentifier.SIGNIN_SURVEY:
+                return "SigninSurvey";
             default:
                 return "Unknown";
         }
