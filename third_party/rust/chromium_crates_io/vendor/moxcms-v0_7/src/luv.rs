efdd929f66c30e54e8b6aaa832490a64b426dae1
Lukasz Anforowicz <lukasza@chromium.org>
2025-12-29 17:00:57 -0800

    Roll moxcms: 0.7.10 => 0.7.11 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * moxcms: 0.7.10 => 0.7.11; https://docs.rs/crate/moxcms/0.7.11
    
    Bug: None
    Change-Id: Icc9148895970fabdd93c58195f236d2e2ddc33e7
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7354707
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1563287}

--- third_party/rust/chromium_crates_io/vendor/moxcms-v0_7/src/luv.rs
+++ third_party/rust/chromium_crates_io/vendor/moxcms-v0_7/src/luv.rs
@@ -221,7 +221,7 @@ impl PartialEq<Luv> for Luv {
 }
 
 impl PartialEq<LCh> for LCh {
-    /// Compares two colours ignoring chromaticity if L\* is zero and hue if C\*
+    /// Compares two colors ignoring chromaticity if L\* is zero and hue if C\*
     /// is zero.  Hues which are τ apart are compared equal.
     #[inline]
     fn eq(&self, other: &Self) -> bool {
