70b0b0c2d130d3e8c308f5dcee081c02d52a271b
Eva Su <evasu@google.com>
2025-12-29 16:59:46 -0800

    [Desktop Android] Add platform-agnostic logic in c/b/ui/extensions/.
    This is the first part of splitting crrev.com/c/7271425 into smaller
    CLs. This CL adds the platform-agnostic dialog building and triggering
    logic to c/b/ui/extensions/.
    
    This CL is a no-op as the new files and methods introduced are not used
    yet.
    
    Bug: 450296898
    Change-Id: I33c5d41ecff798f73b58a1db1cf12dbfacf2f530
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7334633
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: Eva Su <evasu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1563286}

---
+++ chrome/browser/ui/extensions/extension_post_install_dialog.h
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_EXTENSIONS_EXTENSION_POST_INSTALL_DIALOG_H_
+#define CHROME_BROWSER_UI_EXTENSIONS_EXTENSION_POST_INSTALL_DIALOG_H_
+
+#include <memory>
+
+#include "chrome/browser/ui/extensions/extension_install_ui.h"
+#include "extensions/buildflags/buildflags.h"
+#include "ui/base/models/dialog_model.h"
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
+
+class Profile;
+class ExtensionPostInstallDialogModel;
+
+namespace content {
+class WebContents;
+}
+
+namespace extensions {
+
+// Shows the extension post-install dialog. This function is platform-agnostic.
+void ShowExtensionPostInstallDialog(
+    Profile* profile,
+    content::WebContents* web_contents,
+    std::unique_ptr<ExtensionPostInstallDialogModel> model);
+
+}  // namespace extensions
+
+#endif  // CHROME_BROWSER_UI_EXTENSIONS_EXTENSION_POST_INSTALL_DIALOG_H_
--- chrome/browser/ui/extensions/extension_post_install_dialog_model.h
+++ chrome/browser/ui/extensions/extension_post_install_dialog_model.h
@@ -8,9 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "extensions/buildflags/buildflags.h"
 #include "extensions/common/extension_id.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/gfx/image/image_skia.h"
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 class Profile;
 
