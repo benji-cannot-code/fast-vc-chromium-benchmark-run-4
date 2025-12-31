4aeee1ff91692816e748f3724029e51ba9fdc520
Zhentao Lu <zhentao.lu@microsoft.com>
2025-12-30 21:29:14 -0800

    [bedrock] Migrate kiosk_browser_window_handler away from BrowserListObserver
    This migration is part of project bedrock to reduce the dependencies on
    Browser and BrowserList. See https://crbug.com/431671320 for more info.
    
    Bug: 431671320
    Change-Id: I42edfb77c9f79cfa754068876f75ec7656db19ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7266547
    Auto-Submit: Zhentao Lu <zhentao.lu@microsoft.com>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Qikai Zhong <qikaizhong@microsoft.com>
    Commit-Queue: Qikai Zhong <qikaizhong@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1563586}

--- chrome/browser/chromeos/app_mode/kiosk_browser_window_handler.h
+++ chrome/browser/chromeos/app_mode/kiosk_browser_window_handler.h
@@ -12,11 +12,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_forward.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/scoped_observation.h"
 #include "base/timer/timer.h"
 #include "chrome/browser/chromeos/app_mode/kiosk_policies.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/browser_list_observer.h"
+#include "chrome/browser/ui/browser_window/public/browser_collection_observer.h"
+#include "chrome/browser/ui/browser_window/public/global_browser_collection.h"
 
 class BrowserWindowInterface;
 
@@ -52,7 +54,7 @@ enum class KioskBrowserWindowType {
 // If the last browser window gets closed, the session gets ended.
 //
 // It also manages showing required settings pages in a consistent browser.
-class KioskBrowserWindowHandler : public BrowserListObserver {
+class KioskBrowserWindowHandler : public BrowserCollectionObserver {
  public:
   KioskBrowserWindowHandler(
       Profile* profile,
@@ -95,9 +97,11 @@ class KioskBrowserWindowHandler : public BrowserListObserver {
   void OnCloseBrowserTimeout();
   void CloseAllUnexpectedBrowserWindows();
 
-  // BrowserListObserver
-  void OnBrowserAdded(Browser* browser) override;
-  void OnBrowserRemoved(Browser* browser) override;
+  // BrowserCollectionObserver
+  void OnBrowserCreated(
+      BrowserWindowInterface* browser_window_interface) override;
+  void OnBrowserClosed(
+      BrowserWindowInterface* browser_window_interface) override;
 
   // Returns true if open by web application and allowed by policy.
   bool IsNewBrowserWindowAllowed(Browser* browser) const;
@@ -144,6 +148,9 @@ class KioskBrowserWindowHandler : public BrowserListObserver {
   std::map<BrowserWindowInterface*, base::OneShotTimer> closing_browsers_;
 
   std::map<Browser*, std::unique_ptr<NavigationWaiter>> url_waiters_;
+
+  base::ScopedObservation<GlobalBrowserCollection, BrowserCollectionObserver>
+      browser_collection_observation_{this};
 
   base::WeakPtrFactory<KioskBrowserWindowHandler> weak_ptr_factory_{this};
 };
