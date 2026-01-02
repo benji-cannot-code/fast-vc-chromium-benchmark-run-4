b3373917274970ed20bda3af7c213ab5d8b08a7b
Nan Lin <linnan@chromium.org>
2026-01-02 11:12:55 -0800

    [Code Health] Remove DanglingUntriaged Pointer in ios/chrome/browser/overlays/ui_bundled/overlay_request_ui_state.h
    Fixed: 446106400
    Change-Id: I0e3d9ca2e2291cedb8183e40d78c92544035dd60
    Cq-Include-Trybots: luci.chromium.try:ios-simulator
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7275670
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1563935}

--- ios/chrome/browser/overlays/ui_bundled/overlay_request_ui_state.mm
+++ ios/chrome/browser/overlays/ui_bundled/overlay_request_ui_state.mm
@@ -7,10 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/overlays/ui_bundled/overlay_request_coordinator.h"
 
-OverlayRequestUIState::OverlayRequestUIState(OverlayRequest* request)
-    : request_(request) {
-  DCHECK(request_);
-}
+OverlayRequestUIState::OverlayRequestUIState() = default;
 
 OverlayRequestUIState::~OverlayRequestUIState() {
   if (has_callback()) {
