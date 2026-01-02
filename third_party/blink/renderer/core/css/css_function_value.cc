83ac36a76bbdcd413d5eb1548587b90a1f52ca2e
moonira <moonira@google.com>
2026-01-02 03:39:50 -0800

    Support property dependent random() function inside CSSFunctionValue
    If no random identifier is specified, we need to use the property name
    and value index as the name in the cache key. Add support for this
    in CSSFunctionValue, so that we will use the correct name for random()
    inside a function.
    
    https://drafts.csswg.org/css-values-5/#random-caching-key
    
    Bug: 413385732
    Change-Id: I2d350752ce8c96fb4cbb3a70f1841979b5e3f33b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7280946
    Commit-Queue: Munira Tursunova <moonira@google.com>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1563823}

--- third_party/blink/renderer/core/css/css_function_value.cc
+++ third_party/blink/renderer/core/css/css_function_value.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/css/css_function_value.h"
 
+#include "third_party/blink/renderer/core/css/css_property_name.h"
+#include "third_party/blink/renderer/core/css/css_value_list.h"
+#include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
 
 namespace blink {
@@ -16,6 +19,21 @@ String CSSFunctionValue::CustomCSSText() const {
   result.Append(CSSValueList::CustomCSSText());
   result.Append(')');
   return result.ReleaseString();
+}
+
+const CSSValue*
+CSSFunctionValue::CopyRandomValueWithPropertyNameAndValueIndexIfNeeded(
+    const CSSPropertyName& property_name,
+    wtf_size_t property_value_index) const {
+  CSSFunctionValue* new_function_value = MakeGarbageCollected<CSSFunctionValue>(
+      value_id_, static_cast<ValueListSeparator>(value_list_separator_));
+  for (wtf_size_t i = 0; i < CSSValueList::length(); i++) {
+    new_function_value->Append(
+        *CSSValueList::Item(i)
+             .CopyRandomValueWithPropertyNameAndValueIndexIfNeeded(
+                 property_name, property_value_index + i));
+  }
+  return new_function_value;
 }
 
 }  // namespace blink
