bbab5b5fa78614d95aae3d9ca3e9054df9aee115
Jilin Yang <jilinyang@google.com>
2026-01-02 14:41:57 -0800

    [NTP-Real-Box]: Add multiline-input Flags
    https://screencast.googleplex.com/cast/NTczNDQ5MzI1NTEwNjU2MHw5ODM3MzQ5Yy04ZQ
    
    Fixed: 472543064
    Change-Id: Ic66bff12496e37c38e751cc143202d1107a3e86b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7377882
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Commit-Queue: Jilin Yang <jilinyang@google.com>
    Reviewed-by: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Cr-Commit-Position: refs/heads/main@{#1563988}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1732,6 +1732,11 @@ const FeatureEntry::FeatureParam
         {"RealboxLayoutMode", ntp_realbox::kRealboxLayoutModeTallBottomContext},
         {"CyclingPlaceholders", "true"},
 };
+const FeatureEntry::FeatureParam kShowNextRealboxTallBottomContextMultiLine[] =
+    {
+        {"RealboxLayoutMode", ntp_realbox::kRealboxLayoutModeTallBottomContext},
+        {"MultiLine", "true"},
+};
 const FeatureEntry::FeatureParam kShowNextRealboxTallTopContext[] = {
     {"RealboxLayoutMode", ntp_realbox::kRealboxLayoutModeTallTopContext},
 };
@@ -1740,6 +1745,10 @@ const FeatureEntry::FeatureParam
         {"RealboxLayoutMode", ntp_realbox::kRealboxLayoutModeTallTopContext},
         {"CyclingPlaceholders", "true"},
 };
+const FeatureEntry::FeatureParam kShowNextRealboxTallTopContextMultiLine[] = {
+    {"RealboxLayoutMode", ntp_realbox::kRealboxLayoutModeTallTopContext},
+    {"MultiLine", "true"},
+};
 const FeatureEntry::FeatureParam kShowNextRealboxCompact[] = {
     {"RealboxLayoutMode", ntp_realbox::kRealboxLayoutModeCompact},
 };
@@ -1747,6 +1756,10 @@ const FeatureEntry::FeatureParam kShowNextRealboxCompactCyclingPlaceholders[] =
     {
         {"RealboxLayoutMode", ntp_realbox::kRealboxLayoutModeCompact},
         {"CyclingPlaceholders", "true"},
+};
+const FeatureEntry::FeatureParam kShowNextRealboxCompactMultiLine[] = {
+    {"RealboxLayoutMode", ntp_realbox::kRealboxLayoutModeCompact},
+    {"MultiLine", "true"},
 };
 
 const FeatureEntry::FeatureVariation kNtpRealboxNextVariations[] = {
@@ -1767,6 +1780,15 @@ const FeatureEntry::FeatureVariation kNtpRealboxNextVariations[] = {
     {"- Show Next Realbox: Compact, Cycling placeholders",
      kShowNextRealboxCompactCyclingPlaceholders,
      std::size(kShowNextRealboxCompactCyclingPlaceholders), nullptr},
+    {"- Show Next Realbox: Bottom Context, Multiline",
+     kShowNextRealboxTallBottomContextMultiLine,
+     std::size(kShowNextRealboxTallBottomContextMultiLine), nullptr},
+    {"- Show Next Realbox: Top Context, Multiline",
+     kShowNextRealboxTallTopContextMultiLine,
+     std::size(kShowNextRealboxTallTopContextMultiLine), nullptr},
+    {"- Show Next Realbox: Compact, Multiline",
+     kShowNextRealboxCompactMultiLine,
+     std::size(kShowNextRealboxCompactMultiLine), nullptr},
 };
 
 const FeatureEntry::FeatureParam kNtpNextShowStaticText[] = {
