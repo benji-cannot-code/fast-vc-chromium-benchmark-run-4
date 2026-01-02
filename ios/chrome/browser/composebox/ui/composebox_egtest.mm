504d51874d2baed5902590a2cfa25e5decb28279
Ameur Hosni <ameurhosni@google.com>
2026-01-02 06:11:11 -0800

    [IOS][AIM] Add composebox app interface to configure aim eligibilty
    Bug: 472995788
    Change-Id: I22375b2948528f10a6d449ce8e163f8a3b83f2fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7363775
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1563851}

--- ios/chrome/browser/composebox/ui/composebox_egtest.mm
+++ ios/chrome/browser/composebox/ui/composebox_egtest.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <XCTest/XCTest.h>
 
 #import "components/omnibox/browser/aim_eligibility_service_features.h"
+#import "ios/chrome/browser/composebox/ui/composebox_app_interface.h"
 #import "ios/chrome/browser/composebox/ui/composebox_ui_constants.h"
 #import "ios/chrome/browser/omnibox/public/omnibox_constants.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
@@ -86,6 +87,25 @@ NSString* kLongText =
   [[EarlGrey
       selectElementWithMatcher:
           grey_accessibilityID(kComposeboxSendButtonAccessibilityIdentifier)]
+      assertWithMatcher:grey_notVisible()];
+}
+
+// Tests that the Composebox is hidden when not eligible.
+- (void)testComposeboxHiddenWhenNotEligible {
+  // Composebox is not available on iPad.
+  if ([ChromeEarlGrey isIPadIdiom]) {
+    EARL_GREY_TEST_SKIPPED(@"Skipped for iPad as composebox is not available.");
+  }
+
+  [ComposeboxAppInterface setAimEligible:NO];
+
+  [ChromeEarlGrey loadURL:self.testServer->GetURL("/")];
+  [ChromeEarlGreyUI focusOmnibox];
+
+  // Check that Composebox elements are NOT visible.
+  [[EarlGrey
+      selectElementWithMatcher:
+          grey_accessibilityID(kComposeboxPlusButtonAccessibilityIdentifier)]
       assertWithMatcher:grey_notVisible()];
 }
 
