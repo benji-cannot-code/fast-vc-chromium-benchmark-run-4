82332725522404bf0ecfb7e70b6086e7fb4db168
Zack Han <zackhan@chromium.org>
2026-01-02 07:52:22 -0800

    Enable SBER deprecation on iOS by default
    Remove the ExtendedReportingRemovePrefDependencyIos specific flag
    - Remove the iOS specific flag
    - Use general feature flag
    - Remove fieldtrial config
    
    Fixed: 468317745
    
    Change-Id: I24e9af3d9ba2a6ab313b9f825cdb1e7ade884277
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7256716
    Reviewed-by: thefrog <thefrog@chromium.org>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Javier Castro <jacastro@chromium.org>
    Commit-Queue: Zack Han <zackhan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1563873}

--- components/safe_browsing/core/common/safe_browsing_prefs.cc
+++ components/safe_browsing/core/common/safe_browsing_prefs.cc
@@ -610,9 +610,7 @@ bool MatchesPasswordProtectionChangePasswordURL(const GURL& url,
 }
 
 bool IsExtendedReportingDeprecated() {
-  return base::FeatureList::IsEnabled(kExtendedReportingRemovePrefDependency) ||
-         base::FeatureList::IsEnabled(
-             kExtendedReportingRemovePrefDependencyIos);
+  return base::FeatureList::IsEnabled(kExtendedReportingRemovePrefDependency);
 }
 
 }  // namespace safe_browsing
