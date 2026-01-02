4e5b55683578ab2e3b647f9dd55dc74ce3157e47
Piotr Kotynia <piotrkotynia@google.com>
2026-01-02 05:47:52 -0800

    [Android] Add KA dynamic positioning flag to chrome://flags page
    This change adds KA dynamic positioning flag to chrome://flags page and
    extends expiration date of 2 other flags.
    
    Bug: 458645230
    Change-Id: I4bc0d2933fb7cc831a0fc2cc19dda49587f68d37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7368601
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Commit-Queue: Piotr Kotynia <piotrkotynia@google.com>
    Auto-Submit: Piotr Kotynia <piotrkotynia@google.com>
    Cr-Commit-Position: refs/heads/main@{#1563845}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -12809,6 +12809,18 @@ const FeatureEntry kFeatureEntries[] = {
      FEATURE_VALUE_TYPE(
          autofill::features::kAutofillAndroidDesktopKeyboardAccessoryRevamp)},
 #endif  // BUILDFLAG(IS_ANDROID)
+
+#if BUILDFLAG(IS_ANDROID)
+    {"autofill-android-keyboard-accessory-dynamic-positioning",
+     flag_descriptions::kAutofillAndroidKeyboardAccessoryDynamicPositioningName,
+     flag_descriptions::
+         kAutofillAndroidKeyboardAccessoryDynamicPositioningDescription,
+     kOsAndroid,
+     FEATURE_VALUE_TYPE(
+         autofill::features::
+             kAutofillAndroidKeyboardAccessoryDynamicPositioning)},
+#endif  // BUILDFLAG(IS_ANDROID)
+
 #if !BUILDFLAG(IS_ANDROID)
     {"multicast-in-direct-sockets",
      flag_descriptions::kMulticastInDirectSocketsName,
