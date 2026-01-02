61f81c5a3bd7dcb9e2660156e6c752bde1caae51
David Bienvenu <davidbienvenu@chromium.org>
2026-01-02 15:00:48 -0800

    Fix UNSAFE_TODO in device_monitor_win.cc
    Use std::lexicographical_compare to compare GUIDS instead of memcmp.
    
    Bug: 42271176
    Change-Id: I9c15fec27ab2b0639688ce9d2f4578db001aff6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7380115
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1563995}

--- device/base/device_monitor_win.cc
+++ device/base/device_monitor_win.cc
@@ -10,11 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <dbt.h>
 
+#include <algorithm>
 #include <map>
 #include <memory>
 
 #include "base/at_exit.h"
 #include "base/compiler_specific.h"
+#include "base/containers/span.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/logging.h"
@@ -35,7 +37,9 @@ DeviceMonitorMessageWindow* g_message_window;
 // STL map.
 struct CompareGUID {
   bool operator()(const GUID& a, const GUID& b) const {
-    return UNSAFE_TODO(memcmp(&a, &b, sizeof a)) < 0;
+    return std::lexicographical_compare(
+        base::byte_span_from_ref(a).begin(), base::byte_span_from_ref(a).end(),
+        base::byte_span_from_ref(b).begin(), base::byte_span_from_ref(b).end());
   }
 };
 }  // namespace
