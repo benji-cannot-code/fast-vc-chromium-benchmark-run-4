70650ccf5e396dbaf85c89e97e101e0ab587f6c0
Achuith Bhandarkar <achuith@chromium.org>
2026-01-03 14:16:24 -0800

    Use ScopedObservation in SessionObserver.
    Bug: 470679673
    Change-Id: I83fd589815c7eb61e6cce064100c5c9bfc899198
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7340022
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1564128}

--- ash/public/cpp/session/session_observer.cc
+++ ash/public/cpp/session/session_observer.cc
@@ -10,14 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 ScopedSessionObserver::ScopedSessionObserver(SessionObserver* observer)
-    : observer_(observer) {
+    : observation_(observer) {
   DCHECK(SessionController::Get());
-  SessionController::Get()->AddObserver(observer_);
+  observation_.Observe(SessionController::Get());
 }
 
 ScopedSessionObserver::~ScopedSessionObserver() {
   DCHECK(SessionController::Get());
-  SessionController::Get()->RemoveObserver(observer_);
 }
 
 }  // namespace ash
--- ash/public/cpp/session/session_observer.h
+++ ash/public/cpp/session/session_observer.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/ash_public_export.h"
 #include "base/memory/raw_ptr.h"
 #include "base/observer_list_types.h"
+#include "base/scoped_observation.h"
 #include "components/session_manager/session_manager_types.h"
 
 class AccountId;
@@ -17,6 +18,7 @@ class PrefService;
 namespace ash {
 
 enum class LoginStatus;
+class SessionController;
 
 class ASH_PUBLIC_EXPORT SessionObserver : public base::CheckedObserver {
  public:
@@ -96,7 +98,7 @@ class ASH_PUBLIC_EXPORT ScopedSessionObserver {
   virtual ~ScopedSessionObserver();
 
  private:
-  const raw_ptr<SessionObserver> observer_;
+  base::ScopedObservation<SessionController, SessionObserver> observation_;
 };
 
 }  // namespace ash
