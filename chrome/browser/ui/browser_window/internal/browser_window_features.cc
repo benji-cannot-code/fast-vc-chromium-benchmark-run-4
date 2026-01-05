d87b494410e2cc529d999bd85f245bb8f22897c8
David Yeung <dayeung@chromium.org>
2026-01-05 12:58:28 -0800

    Fix side panel UI accessor for DevTools windows
    Certain browser windows like DevTools would not have a WebUI side panel
    even if the flag `IsWebUIBrowserEnabled` is enabled. Add a check to make
    sure the side panel exists before returning it.
    
    Change-Id: Ib749849b272d82a641f3390cd7394c4ae2da49f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7397930
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1564578}

--- chrome/browser/ui/browser_window/internal/browser_window_features.cc
+++ chrome/browser/ui/browser_window/internal/browser_window_features.cc
@@ -978,7 +978,7 @@ void BrowserWindowFeatures::TearDownPreBrowserWindowDestruction() {
 }
 
 SidePanelUI* BrowserWindowFeatures::side_panel_ui() {
-  if (webui_browser::IsWebUIBrowserEnabled()) {
+  if (webui_browser::IsWebUIBrowserEnabled() && webui_browser_side_panel_ui_) {
     return webui_browser_side_panel_ui_.get();
   }
 
