90b94af2fb271822b7f6f9774bc814548b6ec73f
Aliaksei Bahdzevich <thelex@google.com>
2026-01-05 05:54:52 -0800

    Add Connectors Internals page to iOS
    This change adds the Connectors Internals page to iOS, excluding the
    Signals Reporting and Device Trust tabs which are not supported on the
    platform. The certificates page is implemented as dummy page (google
    only https://screenshot.googleplex.com/8NSGWSckZW5Sc8E)
    
    Bug: 465392224
    Change-Id: Ibc09f6dd4ab4bc233d1a0e699da7b3775dba6356
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7362748
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Aliaksei Bahdzevich <thelex@google.com>
    Cr-Commit-Position: refs/heads/main@{#1564329}

---
+++ ios/chrome/browser/webui/ui_bundled/connectors_internals/connectors_internals_ui.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_WEBUI_UI_BUNDLED_CONNECTORS_INTERNALS_CONNECTORS_INTERNALS_UI_H_
+#define IOS_CHROME_BROWSER_WEBUI_UI_BUNDLED_CONNECTORS_INTERNALS_CONNECTORS_INTERNALS_UI_H_
+
+#import "components/enterprise/connectors/connectors_internals.mojom.h"
+#import "ios/web/public/webui/web_ui_ios_controller.h"
+#import "mojo/public/cpp/bindings/pending_receiver.h"
+
+class ConnectorsInternalsPageHandler;
+
+class ConnectorsInternalsUI : public web::WebUIIOSController {
+ public:
+  ConnectorsInternalsUI(web::WebUIIOS* web_ui, const std::string& host);
+  ~ConnectorsInternalsUI() override;
+
+  void BindInterface(
+      mojo::PendingReceiver<connectors_internals::mojom::PageHandler> receiver);
+
+ private:
+  std::unique_ptr<ConnectorsInternalsPageHandler> page_handler_;
+};
+
+#endif  // IOS_CHROME_BROWSER_WEBUI_UI_BUNDLED_CONNECTORS_INTERNALS_CONNECTORS_INTERNALS_UI_H_
