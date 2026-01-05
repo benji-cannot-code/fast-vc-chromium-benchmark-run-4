8044f4cf82d474a89e76e45bfb5f7d77c1b85c42
Joemer Ramos <joemerramos@google.com>
2026-01-05 13:15:34 -0800

    [iOS] Implement Floaty Offset/Opacity Transition Logic
    The floaty offset API didn't account for the new opacity changes.
    Additionally, there was no logic to handle the transition state if
    a user stopped scrolling at a partial fullscreen mode. This CL
    changes the opacity based on the fullscreen progress and also adds
    an animation block so that the Gemini floaty can listen to fullscreen
    changes happening during a transition animation.
    
    UI Video: crbug.com/469404338#comment12
    
    Fixed: 469404338
    Change-Id: I3d9830d299d556259ae3a6dece3cb840f88138b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7396988
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Joemer Ramos <joemerramos@google.com>
    Cr-Commit-Position: refs/heads/main@{#1564589}

--- ios/chrome/browser/intelligence/bwg/model/bwg_browser_agent.mm
+++ ios/chrome/browser/intelligence/bwg/model/bwg_browser_agent.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/favicon/ios/web_favicon_driver.h"
 #import "components/optimization_guide/proto/features/common_quality_data.pb.h"
 #import "components/prefs/pref_service.h"
+#import "ios/chrome/browser/fullscreen/ui_bundled/fullscreen_animator.h"
 #import "ios/chrome/browser/fullscreen/ui_bundled/fullscreen_controller.h"
 #import "ios/chrome/browser/intelligence/bwg/model/bwg_link_opening_delegate.h"
 #import "ios/chrome/browser/intelligence/bwg/model/bwg_link_opening_handler.h"
@@ -59,6 +60,12 @@ BWGPageContextComputationStateFromPageContextWrapperError(
 // will bring the overlay towards the bottom of the viewport while a positive
 // value will do the reverse.
 CGFloat kOverlayFullscreenOffset = -100;
+
+// Used for forcing fullscreen progress value.
+CGFloat kFullscreenEnabled = 0.0;
+
+// Used for forcing non-fullscreen progress value.
+CGFloat kFullscreenDisabled = 1.0;
 
 }  // namespace
 
@@ -165,7 +172,19 @@ void BwgBrowserAgent::FullscreenProgressUpdated(
   // positive value equal to the `fullyExpandedBottomToolbarHeight`. When
   // fullscreen mode is enabled (progress == 0), the offset will be a negative
   // value, `kOverlayFullscreenOffset`.
-  ios::provider::UpdateOverlayOffsetWithOpacity(offset, 0);
+  ios::provider::UpdateOverlayOffsetWithOpacity(offset, progress);
+}
+
+void BwgBrowserAgent::FullscreenWillAnimate(FullscreenController* controller,
+                                            FullscreenAnimator* animator) {
+  base::WeakPtr<BwgBrowserAgent> weak_ptr = weak_factory_.GetWeakPtr();
+  [animator addAnimations:^{
+    if (weak_ptr) {
+      weak_ptr->FullscreenProgressUpdated(
+          controller, controller->GetProgress() < 0.5 ? kFullscreenEnabled
+                                                      : kFullscreenDisabled);
+    }
+  }];
 }
 
 #pragma mark - Private
