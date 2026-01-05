099b61365cadfd672ce7593a8151e28551a726a0
Dan Harrington <harringtond@chromium.org>
2026-01-05 11:17:17 -0800

    add glic_enabling to android build
    Bug: b:470059315
    Change-Id: Ica1a8e99f33b25474491e2b16eecc5ac6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7282499
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Commit-Queue: Dan Harrington <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1564484}

--- chrome/browser/task_manager/providers/render_process_host_task_provider.cc
+++ chrome/browser/task_manager/providers/render_process_host_task_provider.cc
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if BUILDFLAG(ENABLE_GLIC)
-#include "chrome/browser/glic/public/glic_enabling.h"
+#include "chrome/browser/glic/public/glic_enabling.h"  // nogncheck
 #include "chrome/browser/glic/public/glic_keyed_service.h"
 #include "chrome/browser/glic/public/glic_keyed_service_factory.h"
 #include "chrome/browser/glic/widget/glic_window_controller.h"
@@ -118,8 +118,9 @@ void RenderProcessHostTaskProvider::DeleteTask(
   auto itr = tasks_by_rph_id_.find(render_process_host_id);
   // If the render process host id isn't being tracked in `tasks_by_rph_id` do
   // nothing.
-  if (itr == tasks_by_rph_id_.end())
+  if (itr == tasks_by_rph_id_.end()) {
     return;
+  }
 
   NotifyObserverTaskRemoved(itr->second.get());
 
