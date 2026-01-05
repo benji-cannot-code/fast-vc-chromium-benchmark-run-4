b26efa79c2feb7a3b1424174d24baf671628b5de
Kacper Bajkiewicz <kbajkiewicz@google.com>
2026-01-05 08:19:29 -0800

    [Signin] Update buttons to primary + tonal pairing in profile picker.
    Part 2.1 - changing buttons in profile picker screens. After this CL it
    looks like:
    - Profile switch screen:
    https://screenshot.googleplex.com/ADPyoZ5SGtL7ncm
    - Profile type choice screen:
    https://screenshot.googleplex.com/3s4RHu9GmwswYwk
    - Managed account notice:
    https://screenshot.googleplex.com/4Y9sDVLk6PfmsKF
    
    Bug: 359170360
    Change-Id: Ie60270c03faa80ae41eab996236793c3328e010c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7339339
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Commit-Queue: Kacper Bajkiewicz <kbajkiewicz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1564389}

--- chrome/browser/resources/signin/managed_user_profile_notice/managed_user_profile_notice_app.html.ts
+++ chrome/browser/resources/signin/managed_user_profile_notice/managed_user_profile_notice_app.html.ts
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import {html} from '//resources/lit/v3_0/lit.rollup.js';
 import type {ManagedUserProfileNoticeAppElement} from './managed_user_profile_notice_app.js';
 
@@ -77,8 +78,11 @@ ${html`
         ?hidden="${this.showProcessing_}">
       ${this.proceedLabel_}
     </cr-button>
-    <cr-button id="cancel-button" @click="${this.onCancel_}"
-        ?hidden="${!this.allowCancel_()}">
+    <cr-button id="cancel-button"
+        class="${loadTimeData.getBoolean('usePrimaryAndTonalButtonsForPromos')
+            ? 'tonal-button'
+            : ''}"
+        @click="${this.onCancel_}" ?hidden="${!this.allowCancel_()}">
       ${this.cancelLabel_}
     </cr-button>
   </div>
--- chrome/browser/resources/signin/profile_picker/profile_switch.html.ts
+++ chrome/browser/resources/signin/profile_picker/profile_switch.html.ts
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {html} from '//resources/lit/v3_0/lit.rollup.js';
+import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 
 import type {ProfileSwitchElement} from './profile_switch.js';
 
@@ -30,7 +31,11 @@ export function getHtml(this: ProfileSwitchElement) {
     <div class="subtitle">$i18n{profileSwitchSubtitle}</div>
   </div>
   <div id="actionContainer">
-    <cr-button id="cancelButton" @click="${this.onCancelClick_}">
+    <cr-button id="cancelButton" class="${
+      loadTimeData.getBoolean('usePrimaryAndTonalButtonsForPromos') ?
+          'tonal-button' :
+          ''}"
+        @click="${this.onCancelClick_}">
       $i18n{cancel}
     </cr-button>
     <cr-button id="switchButton" class="action-button"
