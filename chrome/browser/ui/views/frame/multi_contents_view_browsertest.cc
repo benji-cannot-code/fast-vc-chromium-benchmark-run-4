8e43b7f7051c2f3cd92d83026a41e620b77cd1a0
Kaan Alsan <alsan@chromium.org>
2026-01-05 08:54:56 -0800

    Make the attached context accessible from TabDragTarget
    This allows TabDragTarget to check the attached context through the
    interface rather than relying on the core dragging logic's global state,
    which will make unit testing TabDragTarget implementations easier.
    
    Bug: 439963720
    Change-Id: If4e429f5d2fae3097f3ba3d216a74245b8acbdc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7382268
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1564410}

--- chrome/browser/ui/views/frame/multi_contents_view_browsertest.cc
+++ chrome/browser/ui/views/frame/multi_contents_view_browsertest.cc
@@ -63,6 +63,7 @@ class MockDragController : public TabDragTarget::DragController {
               (int),
               (override));
   MOCK_METHOD(const DragSessionData&, GetSessionData, (), (const, override));
+  MOCK_METHOD(const TabDragContext*, GetAttachedContext, (), (const, override));
 };
 
 }  // namespace
