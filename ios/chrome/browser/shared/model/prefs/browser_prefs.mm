6bcfc869a09ed26b24a1a6880d5c89e942f55a1b
Anatoli Hancharou <antoli@google.com>
2026-01-05 06:01:28 -0800

    Enable `enterprise_client_certificates` build flag for iOS.
    The change also enables 3 fundamental policies for client certificates
    provisioning and utilization.
    
    Binary size increase: Unavoidable, this is the smallest set of changes
    that would get CQ to pass. Part of this increase is pre-existing since
    it attaches to work already submitted (refer to the bug attached).
    
    Bug: 432304211
    Change-Id: Ic94aeed4ea4129fd6049328090fc50bb90464534
    DD: http://goto.google.com/cbe-cep-client-certs-bling-dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7269271
    Reviewed-by: Elias Homsi <eliashomsi@google.com>
    Commit-Queue: Anatoli Hancharou <antoli@google.com>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1564331}

--- ios/chrome/browser/shared/model/prefs/browser_prefs.mm
+++ ios/chrome/browser/shared/model/prefs/browser_prefs.mm
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/dom_distiller/core/distilled_page_prefs.h"
 #import "components/enterprise/browser/identifiers/identifiers_prefs.h"
 #import "components/enterprise/browser/reporting/common_pref_names.h"
+#import "components/enterprise/client_certificates/core/prefs.h"
 #import "components/enterprise/connectors/core/connectors_prefs.h"
 #import "components/enterprise/data_controls/core/browser/prefs.h"
 #import "components/enterprise/idle/idle_pref_names.h"
@@ -438,6 +439,7 @@ void RegisterLocalStatePrefs(PrefRegistrySimple* registry) {
   breadcrumbs::RegisterPrefs(registry);
   ProfileAttributesStorageIOS::RegisterPrefs(registry);
   chrome_urls::RegisterPrefs(registry);
+  client_certificates::RegisterLocalStatePrefs(registry);
   flags_ui::PrefServiceFlagsStorage::RegisterPrefs(registry);
   signin::IdentityManager::RegisterLocalStatePrefs(registry);
   IOSChromeMetricsServiceClient::RegisterPrefs(registry);
