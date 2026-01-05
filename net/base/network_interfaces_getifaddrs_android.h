d8380a00cb6470ed14f9413325c4b8659fe008ef
Adam Rice <ricea@chromium.org>
2026-01-05 00:34:49 -0800

    Add a test for net::internal::Getifaddrs()
    net::internal::Getifaddrs() is used instead of the built-in getifaddrs()
    function on a small number of Android devices. It has no tests.
    
    Add a simple test that it returns coherent values. This is not a true
    unit test as it uses the real values returned by the kernel, and so it
    doesn't know what to expect. But it is better than nothing.
    
    BUG=466651186
    
    Change-Id: I7b807108084cf49199888ae047ddc1f8d22e83ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7260420
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Reviewed-by: mmenke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1564215}

--- net/base/network_interfaces_getifaddrs_android.h
+++ net/base/network_interfaces_getifaddrs_android.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <ifaddrs.h>
 
+#include "net/base/net_export.h"
+
 namespace net::internal {
 
 // Implementation of getifaddrs for Android.
@@ -17,8 +19,8 @@ namespace net::internal {
 // Due to some buggy getifaddrs() implementation in Android 11, Chromium
 // provides its own version. See https://crbug.com/1240237 for more context.
 // ifa_ifu(ifa_broadaddr, ifa_dstaddr) is not populated in this function.
-int Getifaddrs(struct ifaddrs** result);
-void Freeifaddrs(struct ifaddrs* addrs);
+NET_EXPORT_PRIVATE int Getifaddrs(struct ifaddrs** result);
+NET_EXPORT_PRIVATE void Freeifaddrs(struct ifaddrs* addrs);
 
 }  // namespace net::internal
 
