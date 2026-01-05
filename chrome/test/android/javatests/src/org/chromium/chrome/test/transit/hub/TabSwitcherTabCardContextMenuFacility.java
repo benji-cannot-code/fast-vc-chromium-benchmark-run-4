c6ee64d039774dbc9dc89de195de76b095626ee2
Henrique Nakashima <hnakashima@chromium.org>
2026-01-05 12:18:31 -0800

    [Android] Wait for tab card context menu to settle
    This menu appears on a different position while the tab is pressed, and
    after the touch up event, it moves a bit (significantly in Desktop).
    This causes tapping on a context menu option to be flaky since the
    coordinates may have changed.
    
    Wait for a second for the View to be on the same position to fix.
    
    Bug: 467341609
    Change-Id: I47a8e443c942c41057325602782a3d9db712b423
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7314755
    Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1564547}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/transit/hub/TabSwitcherTabCardContextMenuFacility.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/transit/hub/TabSwitcherTabCardContextMenuFacility.java
@@ -50,7 +50,7 @@ public class TabSwitcherTabCardContextMenuFacility<HostStationT extends TabSwitc
             declareContainerView(
                     TouchTrackingListView.class,
                     withId(R.id.tab_group_action_menu_list),
-                    ViewElement.defaultOptions());
+                    ViewElement.initialSettleTimeOption(1000));
     private final @TabId int mTabId;
 
     public Item share;
