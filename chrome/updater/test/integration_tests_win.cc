0fd58c9ff74304d5dc119e9148b4745ae70f176d
Micah Hutchens <mhutchens@dropbox.com>
2026-01-05 11:40:30 -0800

    [updater] Process installsource switch for offline installs
    Currently there is no distinction in install pings between regular
    offline installs or enterprise installs. Legacy MSI installers pass
    the `installsource=enterprisemsi` command line switch but this switch
    is not currently being passed through to the update service.
    
    Adds logic to pass the `installsource` switch as an installer setting
    similar to how app version is handled for MSI offline installs, and
    adds a few integration test cases for ensuring the flag is passed
    through to the install ping. Also updates the function spec doc.
    
    Fixed: 463966218
    Change-Id: Iae80cf1882aeaa1b7859161ef853b7397908c3d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7270800
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1564517}

--- chrome/updater/test/integration_tests_win.cc
+++ chrome/updater/test/integration_tests_win.cc
@@ -399,7 +399,8 @@ base::Process LaunchOfflineInstallProcess(bool is_legacy_install,
                                           const std::wstring& app_id,
                                           const std::wstring& offline_dir_guid,
                                           bool is_silent_install,
-                                          const std::string& language) {
+                                          const std::string& language,
+                                          const std::string& install_source) {
   auto launch_legacy_offline_install = [&] {
     auto build_legacy_switch =
         [](const std::string& switch_name) -> std::wstring {
@@ -423,6 +424,11 @@ base::Process LaunchOfflineInstallProcess(bool is_legacy_install,
         base::CommandLine::QuoteForCommandLineToArgvW(offline_dir_guid),
 
         is_silent_install ? build_legacy_switch(updater::kSilentSwitch) : L"",
+
+        install_source.empty()
+            ? L""
+            : build_legacy_switch(updater::kInstallSourceSwitch),
+        base::UTF8ToWide(install_source),
     };
 
     return base::LaunchProcess(base::JoinString(install_cmd_args, L" "), {});
@@ -443,6 +449,11 @@ base::Process LaunchOfflineInstallProcess(bool is_legacy_install,
                                    offline_dir_guid);
     if (is_silent_install) {
       install_cmd.AppendSwitch(updater::kSilentSwitch);
+    }
+
+    if (!install_source.empty()) {
+      install_cmd.AppendSwitchUTF8(updater::kInstallSourceSwitch,
+                                   install_source);
     }
 
     return base::LaunchProcess(install_cmd, {});
@@ -538,6 +549,7 @@ void RunOfflineInstallWithManifest(UpdaterScope scope,
                                    bool is_silent_install,
                                    int installer_result,
                                    int installer_error,
+                                   const std::string& install_source,
                                    base::cstring_view platform,
                                    int string_resource_id_to_find,
                                    const std::string& language,
@@ -664,7 +676,7 @@ void RunOfflineInstallWithManifest(UpdaterScope scope,
   // Trigger offline install.
   ASSERT_TRUE(LaunchOfflineInstallProcess(
                   is_legacy_install, updater_exe.value(), scope, kTestAppID,
-                  offline_dir_guid, is_silent_install, language)
+                  offline_dir_guid, is_silent_install, language, install_source)
                   .IsValid());
 
   // * Silent installs do not show any UI.
@@ -2142,21 +2154,22 @@ void RunOfflineInstall(UpdaterScope scope,
                        bool is_legacy_install,
                        bool is_silent_install,
                        int installer_result,
-                       int installer_error) {
-  RunOfflineInstallWithManifest(scope, is_legacy_install, is_silent_install,
-                                installer_result, installer_error, "win",
-                                IDS_BUNDLE_INSTALLED_SUCCESSFULLY_BASE, "en",
-                                !installer_result);
+                       int installer_error,
+                       const std::string& install_source) {
+  RunOfflineInstallWithManifest(
+      scope, is_legacy_install, is_silent_install, installer_result,
+      installer_error, install_source, "win",
+      IDS_BUNDLE_INSTALLED_SUCCESSFULLY_BASE, "en", !installer_result);
 }
 
 void RunOfflineInstallOsNotSupported(UpdaterScope scope,
                                      bool is_legacy_install,
                                      bool is_silent_install,
                                      const std::string& language) {
-  RunOfflineInstallWithManifest(scope, is_legacy_install, is_silent_install,
-                                /*installer_result=*/0, /*installer_error=*/0,
-                                "minix", IDS_UPDATER_OS_NOT_SUPPORTED_BASE,
-                                language, false);
+  RunOfflineInstallWithManifest(
+      scope, is_legacy_install, is_silent_install,
+      /*installer_result=*/0, /*installer_error=*/0, /*install_source=*/"",
+      "minix", IDS_UPDATER_OS_NOT_SUPPORTED_BASE, language, false);
 }
 
 void RunMockOfflineMetaInstall(UpdaterScope scope,
