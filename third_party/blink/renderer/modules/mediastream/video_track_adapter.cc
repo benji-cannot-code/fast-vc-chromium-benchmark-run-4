b378fa79e7f668cfaba492b8fb2d10352413aad6
Colin Blundell <blundell@chromium.org>
2026-01-05 05:40:16 -0800

    [//media] Update check in VideoTrackAdapter
    This is effectively the same check [1], but it is more easily readable
    as to the intent.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:media/base/video_frame.cc;l=1170-1172?q=VideoFrame::HasM&ss=chromium
    
    Bug: 404905709
    Change-Id: I885365fb8f3ea4d8295f8b1c347532db7425c071
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7269047
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1564323}

--- third_party/blink/renderer/modules/mediastream/video_track_adapter.cc
+++ third_party/blink/renderer/modules/mediastream/video_track_adapter.cc
@@ -428,9 +428,7 @@ void VideoTrackAdapter::VideoFrameResolutionAdapter::DeliverFrame(
   //
   // TODO(crbug.com/362521): Allow cropping/scaling of non-GPU memory backed
   // textures.
-  if (video_frame->HasSharedImage() &&
-      video_frame->storage_type() !=
-          media::VideoFrame::STORAGE_MAPPABLE_SHARED_IMAGE) {
+  if (video_frame->HasSharedImage() && !video_frame->HasMappableSharedImage()) {
     DoDeliverFrame(std::move(video_frame), estimated_capture_time);
     return;
   }
