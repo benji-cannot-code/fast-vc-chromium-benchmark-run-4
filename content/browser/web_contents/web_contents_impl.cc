2510079bf10b8fbaad3cb3b6c46a8526a2cfccd2
Joshua Thomas <masnoble@chromium.org>
2026-01-05 13:48:44 -0800

    Cleanup simulated user interaction functions
    This change renames and adds a method that is more specific to what
    is actually happening, a simulated user interaction notification.
    
    Bug: b:470325962
    Change-Id: I91a5d8d45a1783b50f48c0521fa1ea4c1caadc10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7398327
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Joshua Thomas <masnoble@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1564614}

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -10236,6 +10236,20 @@ void WebContentsImpl::DidReceiveInputEvent(
                          WindowOpenDisposition::CURRENT_TAB);
   }
 
+  HandleUserInteractionForInputEvent(render_widget_host, event);
+}
+
+void WebContentsImpl::SimulateUserInteraction(
+    RenderWidgetHostImpl* render_widget_host,
+    const blink::WebInputEvent& event) {
+  CHECK(IsUserInteractionInputType(event.GetType()));
+
+  HandleUserInteractionForInputEvent(render_widget_host, event);
+}
+
+void WebContentsImpl::HandleUserInteractionForInputEvent(
+    RenderWidgetHostImpl* render_widget_host,
+    const blink::WebInputEvent& event) {
   if (!IsUserInteractionInputType(event.GetType())) {
     return;
   }
