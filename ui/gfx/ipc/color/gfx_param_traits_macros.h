889ce938cc04506ec98831164e478d4f75ad69d0
Colin Blundell <blundell@chromium.org>
2026-01-05 02:27:24 -0800

    [//ui] Remove unneeded gfx param traits
    The remaining one is currently still used; I will investigate its usage
    in followup.
    
    Change-Id: I28932815fe99b1897b1b78c89a8fe97434d4e3d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7269384
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1564262}

--- ui/gfx/ipc/color/gfx_param_traits_macros.h
+++ ui/gfx/ipc/color/gfx_param_traits_macros.h
@@ -15,12 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #undef IPC_MESSAGE_EXPORT
 #define IPC_MESSAGE_EXPORT GFX_IPC_COLOR_EXPORT
 
-IPC_ENUM_TRAITS_MAX_VALUE(gfx::ColorSpace::PrimaryID,
-                          gfx::ColorSpace::PrimaryID::kMaxValue)
-IPC_ENUM_TRAITS_MAX_VALUE(gfx::ColorSpace::TransferID,
-                          gfx::ColorSpace::TransferID::kMaxValue)
-IPC_ENUM_TRAITS_MAX_VALUE(gfx::ColorSpace::MatrixID,
-                          gfx::ColorSpace::MatrixID::kMaxValue)
 IPC_ENUM_TRAITS_MAX_VALUE(gfx::ColorSpace::RangeID,
                           gfx::ColorSpace::RangeID::kMaxValue)
 
