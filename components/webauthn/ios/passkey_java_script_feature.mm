402b46428b3f0cbfc078cf91dec36e0d94c4dd5b
Alexis Hetu <sugoi@chromium.org>
2026-01-05 08:00:40 -0800

    [iOS] Add a utility function to defer to renderer and use it on errors
    This CL adds a public method to PasskeyTabHelper which defers to
    the renderer using a RequestInfo object and uses it when encountering
    errors in PasskeyJavaScriptFeature::ScriptMessageReceived().
    
    This required changing the signature of a few private functions to
    avoid duplicating code.
    
    Bug: 460485333
    Change-Id: I6988243522d1c3d1d0dff4b8d11516081c765925
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7280003
    Auto-Submit: Alexis Hétu <sugoi@chromium.org>
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Reviewed-by: Noémie St-Onge <noemies@google.com>
    Reviewed-by: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1564379}

--- components/webauthn/ios/passkey_java_script_feature.mm
+++ components/webauthn/ios/passkey_java_script_feature.mm
@@ -261,7 +261,8 @@ void PasskeyJavaScriptFeature::ScriptMessageReceived(
     auto assertion_request_params =
         BuildAssertionRequestParams(std::move(*request_info), dict);
     if (!assertion_request_params.has_value()) {
-      // TODO(460485333): Call DeferToRenderer.
+      // TODO(460485333): Log the error.
+      passkey_tab_helper->DeferToRenderer(std::move(*request_info));
       return;
     }
 
@@ -274,7 +275,8 @@ void PasskeyJavaScriptFeature::ScriptMessageReceived(
     auto registration_request_params =
         BuildRegistrationRequestParams(std::move(*request_info), dict);
     if (!registration_request_params.has_value()) {
-      // TODO(460485333): Call DeferToRenderer.
+      // TODO(460485333): Log the error.
+      passkey_tab_helper->DeferToRenderer(std::move(*request_info));
       return;
     }
 
