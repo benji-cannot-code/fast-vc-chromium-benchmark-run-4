c3e8387fa0fdec233034beea3faea4c0c1dd2cac
Eitan Goldberger <eitang@google.com>
2026-01-05 09:57:49 -0800

    Reenable PinnedSidePanelInteractiveTest tests
    These were failing due to running with ImmersiveReadingMode flag
    enabled. This CL explicitly turns off that flag and fixes the failures,
    and restores the tests to how they were before crrev.com/c/7363069
    disabled them due to those failures.
    
    Bug: 472526459
    Change-Id: Id327bfbfa5e728606cf463bc3ad47de12643a684
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7382047
    Commit-Queue: Eitan Goldberger <eitang@google.com>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1564442}

--- chrome/browser/ui/views/side_panel/side_panel_interactive_uitest.cc
+++ chrome/browser/ui/views/side_panel/side_panel_interactive_uitest.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/reading_list/core/reading_list_entry.h"
 #include "content/public/test/browser_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/accessibility/accessibility_features.h"
 #include "ui/base/interaction/element_identifier.h"
 #include "ui/base/interaction/element_tracker.h"
 #include "ui/base/interaction/expect_call_in_scope.h"
@@ -137,7 +138,10 @@ class PinnedSidePanelInteractiveTest : public InteractiveFeaturePromoTest {
  public:
   PinnedSidePanelInteractiveTest()
       : InteractiveFeaturePromoTest(UseDefaultTrackerAllowingPromos(
-            {feature_engagement::kIPHSidePanelGenericPinnableFeature})) {}
+            {feature_engagement::kIPHSidePanelGenericPinnableFeature})) {
+    scoped_feature_list_.InitAndDisableFeature(
+        features::kImmersiveReadAnything);
+  }
   ~PinnedSidePanelInteractiveTest() override = default;
 
   void SetUp() override {
@@ -232,13 +236,15 @@ class PinnedSidePanelInteractiveTest : public InteractiveFeaturePromoTest {
       browser()->browser_window_features()->side_panel_ui()->Show(key);
     }));
   }
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 // Verify that we can open the ReadingMode side panel from the 3dot -> More
 // tools context menu.
-// TODO(crbug.com/417601707, crbug.com/472526459): Re-enable this test
 IN_PROC_BROWSER_TEST_F(PinnedSidePanelInteractiveTest,
-                       DISABLED_OpenReadingModeSidePanel) {
+                       OpenReadingModeSidePanel) {
   // Replace the contents of the ReadingMode side panel with an empty view so it
   // loads faster.
   auto* const registry = SidePanelRegistry::From(browser());
@@ -343,10 +349,17 @@ IN_PROC_BROWSER_TEST_F(PinnedSidePanelInteractiveTest,
                     EnsureNotPresent(kSidePanelPinButtonElementId)));
 }
 
-// TODO(crbug.com/417601707, crbug.com/472526459): Re-enable this test
+// TODO(crbug.com/417601707): Re-enable this test
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_PinnedToolbarButtonsHighlightWhileSidePanelVisible \
+  DISABLED_PinnedToolbarButtonsHighlightWhileSidePanelVisible
+#else
+#define MAYBE_PinnedToolbarButtonsHighlightWhileSidePanelVisible \
+  PinnedToolbarButtonsHighlightWhileSidePanelVisible
+#endif
 IN_PROC_BROWSER_TEST_F(
     PinnedSidePanelInteractiveTest,
-    DISABLED_PinnedToolbarButtonsHighlightWhileSidePanelVisible) {
+    MAYBE_PinnedToolbarButtonsHighlightWhileSidePanelVisible) {
   // Replace the contents of the ReadingMode side panel with an empty view so it
   // loads faster.
   auto* const registry = SidePanelRegistry::From(browser());
