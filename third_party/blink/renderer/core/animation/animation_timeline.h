f8dbba8be3e030352d5f4f24f004b8177c4722f7
David Awogbemila <awogbemila@chromium.org>
2026-01-05 09:07:02 -0800

    [animation-trigger] Delete LimitTriggerAttachmentUpdates
    Data from the Finch experiment didn't show the AnimationTrigger to be
    crashier but we should default to limiting the attachment updates to
    animations that actually have trigger declarations.
    
    This patch makes LimitTriggerAttachmentUpdates the default path and
    deletes the flag.
    
    Bug: 390314945, 447174988
    Change-Id: If7b2ca0d99c2d62aaaad9c8e95ab8ead70ab34a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7266338
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Reviewed-by: David Awogbemila <awogbemila@chromium.org>
    Commit-Queue: David Awogbemila <awogbemila@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1564416}

--- third_party/blink/renderer/core/animation/animation_timeline.h
+++ third_party/blink/renderer/core/animation/animation_timeline.h
@@ -157,8 +157,6 @@ class CORE_EXPORT AnimationTimeline : public ScriptWrappable {
   virtual void RemoveTrigger(TimelineTrigger* trigger);
   void ServiceTriggers();
 
-  void UpdateAnimationTriggerAttachments();
-
  protected:
   virtual PhaseAndTime CurrentPhaseAndTime() = 0;
 
--- third_party/blink/renderer/core/animation/css/css_animations.cc
+++ third_party/blink/renderer/core/animation/css/css_animations.cc
@@ -2319,14 +2319,12 @@ void CSSAnimations::MaybeApplyPendingUpdate(Element* element) {
 
     css_animation.RemoveStaleNamedTriggerAttachments(entry.trigger_attachments);
     css_animation.SetTriggerAttachments(entry.trigger_attachments);
-    if (RuntimeEnabledFeatures::LimitTriggerAttachmentUpdatesEnabled()) {
-      if (entry.trigger_attachments) {
-        element->GetDocument().GetDocumentAnimations().AddTriggeredAnimation(
-            &css_animation);
-      } else {
-        element->GetDocument().GetDocumentAnimations().RemoveTriggeredAnimation(
-            &css_animation);
-      }
+    if (entry.trigger_attachments) {
+      element->GetDocument().GetDocumentAnimations().AddTriggeredAnimation(
+          &css_animation);
+    } else {
+      element->GetDocument().GetDocumentAnimations().RemoveTriggeredAnimation(
+          &css_animation);
     }
     css_animation.SetTriggerActionPlayState(
         entry.play_state_list[entry.index % entry.play_state_list.size()]);
@@ -2368,10 +2366,8 @@ void CSSAnimations::MaybeApplyPendingUpdate(Element* element) {
       // trigger. This allows the animation to show up in getAnimations.
       animation->pause();
       animation->SetPausedForTrigger(true);
-      if (RuntimeEnabledFeatures::LimitTriggerAttachmentUpdatesEnabled()) {
-        element->GetDocument().GetDocumentAnimations().AddTriggeredAnimation(
-            animation);
-      }
+      element->GetDocument().GetDocumentAnimations().AddTriggeredAnimation(
+          animation);
     } else {
       animation->play();
     }
