249345c45fdd5e37b288f4b95dbca4214ee21b8b
Friedrich Horschig <friedrichh@chromium.org>
2026-01-05 00:19:15 -0800

    [Crash][HybridInAutofill] Add CrashKey to identify offending item
    Strangely, the check triggers for an item on CrOS without any
    obvious CrOS-only addition. This crash key should help to identify
    the item type.
    
    Feature: AutofillReintroduceHybridPasskeyDropdownItem
    Bug: 468793696
    Change-Id: Ia7f9289437c4941e818f18f28dd3497b816108b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7367717
    Commit-Queue: Friedrich Hauser <friedrichh@chromium.org>
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1564214}

--- components/autofill/core/browser/foundations/browser_autofill_manager.cc
+++ components/autofill/core/browser/foundations/browser_autofill_manager.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_map.h"
 #include "base/containers/span.h"
 #include "base/containers/to_vector.h"
+#include "base/debug/crash_logging.h"
 #include "base/debug/dump_without_crashing.h"
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
@@ -2272,6 +2273,16 @@ void BrowserAutofillManager::DidShowSuggestions(
   if (std::ranges::any_of(shown_suggestion_types, [](SuggestionType type) {
         return type == SuggestionType::kAddressEntryOnTyping;
       })) {
+    // TODO: crbug.com/468793696 - Remove crashkey after investigation.
+    SCOPED_CRASH_KEY_STRING64(
+        "autofill", "suggestions-shown-on-typing",
+        base::JoinString(base::ToVector(shown_suggestion_types,
+                                        [](SuggestionType type) {
+                                          return base::NumberToString(
+                                              std::to_underlying(type));
+                                        }),
+                         ","));
+
     // Assert that only the expected suggestion types exist. Note that despite
     // `SuggestionType::kDatalistEntry` is optionally added by
     // `AutofillExternalDelegate`, therefore checking for it is also required.
