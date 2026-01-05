d6244721c53b8257e9deb052c6cf7b873478f206
Georg Neis <neis@chromium.org>
2026-01-04 18:15:36 -0800

    ash: Simplify some VpnService code
    Inline and simplify GetActiveConfigurationObjectPath on the extensions
    side. On the crosapi side, GetActiveConfigurationObjectPath will be
    deleted in the next CL.
    
    Bug: b:365741912, b:365902693
    Change-Id: I189ff32267af527600fb800b4474c730d647e83d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7279209
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1564165}

--- chrome/browser/chromeos/extensions/vpn_provider/vpn_service.cc
+++ chrome/browser/chromeos/extensions/vpn_provider/vpn_service.cc
@@ -250,15 +250,8 @@ void VpnService::SendToExtension(const std::string& extension_id,
 
 bool VpnService::OwnsActiveConfiguration(
     const std::string& extension_id) const {
-  return GetActiveConfigurationObjectPath(extension_id).has_value();
-}
-
-std::optional<std::string> VpnService::GetActiveConfigurationObjectPath(
-    const std::string& extension_id) const {
-  // Peek into VpnServiceAsh directly (this call does not go through mojo).
-  return GetVpnService()
-      ->GetVpnServiceForExtension(extension_id)
-      ->GetActiveConfigurationObjectPath();
+  return active_configuration_ &&
+         active_configuration_->extension_id() == extension_id;
 }
 
 void VpnService::SendOnPlatformMessageToExtension(
@@ -473,8 +466,7 @@ void VpnService::SetParameters(const std::string& extension_id,
   }
 
   ash::ShillThirdPartyVpnDriverClient::Get()->SetParameters(
-      GetActiveConfigurationObjectPath(extension_id).value(),
-      std::move(parameters),
+      active_configuration_->object_path(), std::move(parameters),
       base::IgnoreArgs<const std::string&>(std::move(success)),
       std::move(failure));
 }
@@ -494,8 +486,8 @@ void VpnService::SendPacket(const std::string& extension_id,
   }
 
   ash::ShillThirdPartyVpnDriverClient::Get()->SendPacket(
-      GetActiveConfigurationObjectPath(extension_id).value(), data,
-      std::move(success), std::move(failure));
+      active_configuration_->object_path(), data, std::move(success),
+      std::move(failure));
 }
 
 void VpnService::NotifyConnectionStateChanged(const std::string& extension_id,
@@ -508,7 +500,7 @@ void VpnService::NotifyConnectionStateChanged(const std::string& extension_id,
   }
 
   ash::ShillThirdPartyVpnDriverClient::Get()->UpdateConnectionState(
-      GetActiveConfigurationObjectPath(extension_id).value(),
+      active_configuration_->object_path(),
       connection_success
           ? std::to_underlying(
                 extensions::api::vpn_provider::VpnConnectionState::kConnected)
