092754911941c8e494a04d69a4939c477abfd27f
Scott Haseley <shaseley@chromium.org>
2026-01-05 13:05:43 -0800

    Reland "[soft navs] Use a prefinalizer instead of UntracedMember for SoftNavigationContext"
    This is a reland of commit 44f6a765b0170c296e5a61d9ed2e221b9e404138
    
    The reland removes the always-failing Contains check called from the
    prefinalizer [1], since it isn't needed with how we're currently
    shutting down contexts. It also improves one of the GC tests, adding a
    histogram expectation.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/7256268/comment/4b3c1f8b_e8f4eab8/
    
    Original change's description:
    > [soft navs] Use a prefinalizer instead of UntracedMember for SoftNavigationContext
    >
    > SoftNavigationHeuristics does custom tracing of the set of contexts
    > using UntracedMember. This used to be how we detect the set becoming
    > empty, but we don't track this any longer. We do still, however, record
    > some metrics and end a trace event, so I think we still need detect
    > when soft nav contexts are disposed.
    >
    > This CL changes how we do this to use a prefinalizer instead, which is
    > generally preferred to UntracedMember. This is being done to make set
    > iteration safer, since HashSet entry erasure invalidates iterators,
    > which would be a problem if a set interaction causes a GC, e.g. due to
    > an allocation. This is a prerequisite for crrev.com/c/7120640, where I
    > don't trust that an iteration I need to add is safe.
    >
    > Bug: 454082771, 454082773
    > Change-Id: Id7932cf4f12f838c588aaf559053421bb494235a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7256268
    > Commit-Queue: Scott Haseley <shaseley@chromium.org>
    > Reviewed-by: Michal Mocny <mmocny@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1559010}
    
    Bug: 454082771, 454082773
    Change-Id: I7829a6d37749ece49ce82a3ae71ee55e839cf5c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7275266
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Reviewed-by: Michal Mocny <mmocny@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1564582}

--- third_party/blink/renderer/core/timing/soft_navigation_heuristics_test.cc
+++ third_party/blink/renderer/core/timing/soft_navigation_heuristics_test.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/notreached.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/time/time.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/scheduler/task_attribution_id.h"
@@ -82,6 +83,8 @@ TEST_F(SoftNavigationHeuristicsTest,
 }
 
 TEST_F(SoftNavigationHeuristicsTest, ResetHeuristicOnSetBecameEmpty) {
+  base::HistogramTester histogram_tester;
+
   auto* heuristics = CreateSoftNavigationHeuristicsForTest();
   ASSERT_TRUE(heuristics);
 
@@ -97,6 +100,12 @@ TEST_F(SoftNavigationHeuristicsTest, ResetHeuristicOnSetBecameEmpty) {
     std::optional<SoftNavigationHeuristics::EventScope> event_scope(
         heuristics->MaybeCreateEventScopeForInputEvent(*event));
     root_task_state = tracker->CurrentTaskState();
+
+    // Set the URL so a histogram entry gets logged. Do this directly through
+    // the context to avoid other objects holding a reference to it, which would
+    // prevent garbage collection.
+    root_task_state->GetSoftNavigationContext()->AddUrl(
+        "foo", base::UnguessableToken::Create());
   }
   EXPECT_TRUE(root_task_state);
   EXPECT_TRUE(heuristics->IsTrackingSoftNavigationsForTest());
@@ -119,12 +128,16 @@ TEST_F(SoftNavigationHeuristicsTest, ResetHeuristicOnSetBecameEmpty) {
   // The heuristics still should not have been reset since there is a live
   // root task, which is being held onto by its descendant task.
   EXPECT_TRUE(heuristics->IsTrackingSoftNavigationsForTest());
+  histogram_tester.ExpectTotalCount("PageLoad.Internal.SoftNavigationOutcome",
+                                    0);
 
   // Finally, this should allow the click task to be GCed, which should cause
   // the heuristics to be reset.
   descendant_task_state = nullptr;
   ThreadState::Current()->CollectAllGarbageForTesting();
   EXPECT_FALSE(heuristics->IsTrackingSoftNavigationsForTest());
+  histogram_tester.ExpectTotalCount("PageLoad.Internal.SoftNavigationOutcome",
+                                    1);
 }
 
 TEST_F(SoftNavigationHeuristicsTest, NestedEventScopesAreMerged) {
