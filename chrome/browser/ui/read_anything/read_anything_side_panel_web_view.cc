f167645eb917d729e008d0c8536f6c44d1b6fc07
Eitan Goldberger <eitang@google.com>
2026-01-06 12:25:59 -0800

    [IRM] Attempt to fix the Side Panel to IRM failure on Mac Canary
    In Mac Canary, when toggling from Side Panel Reading Mode to Immersive
    Reading Mode, the screen flickers and then doesn't show Reading Mode.
    However, this works correctly on the linux dev build, so it's hard to
    test. There's probably some timing issue or race condition regarding
    view ownership during the transition.
    
    Potentially, the problem may be that the Side Panel view retains
    ownership of the WebContents slightly too long during the close
    animation, causing a conflict when the new Immersive view tries to
    attach the same WebContents. To try to fix this, this CL disables the
    animation, and explicitly sets the WebContents of the SidePanel WebView
    to nullptr before it transfers ownership of the Reading Mode WebUI back
    to the ReadAnythingController.
    
    Bug: 473787872
    Change-Id: I7c02e903cbc519c3e02420f0a0752da22bebee19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7401582
    Commit-Queue: Eitan Goldberger <eitang@google.com>
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1565194}

--- chrome/browser/ui/read_anything/read_anything_side_panel_web_view.cc
+++ chrome/browser/ui/read_anything/read_anything_side_panel_web_view.cc
@@ -84,6 +84,7 @@ ReadAnythingSidePanelWebView::GetWeakPtr() {
 
 std::unique_ptr<WebUIContentsWrapperT<ReadAnythingUntrustedUI>>
 ReadAnythingSidePanelWebView::TakeContentsWrapper() {
+  SetWebContents(nullptr);
   return std::move(contents_wrapper_);
 }
 
