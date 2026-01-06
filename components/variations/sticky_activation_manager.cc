3d8781678c4e72ad926f3b10c3ea3b34b24cce64
Alexei Svitkine <asvitkine@chromium.org>
2026-01-06 07:07:37 -0800

    Add histogram to measure stickiness of sticky study activation.
    This metric will allow us to verify the number of times "stickiness" is lost for studies using this activation type.
    
    Adds tests for the new metric and some other missing tests suggested by an AI agent.
    
    Bug: 467929965
    Change-Id: Icf766090de3e8fc7fbfe6579d125d04e2a851439
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7385040
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Auto-Submit: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1564944}

--- components/variations/sticky_activation_manager.cc
+++ components/variations/sticky_activation_manager.cc
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/field_trial.h"
 #include "base/metrics/field_trial_list_including_low_anonymity.h"
 #include "base/metrics/field_trial_params.h"
+#include "base/metrics/histogram_functions.h"
+#include "base/metrics/metrics_hashes.h"
 #include "base/strings/strcat.h"
 #include "base/strings/string_split.h"
 #include "components/prefs/pref_registry_simple.h"
@@ -167,6 +169,14 @@ void StickyActivationManager::OnFieldTrialGroupFinalized(
     DCHECK_EQ(it->second, kInactiveStickyTrialSentinel);
 
     it->second = group_name;
+
+    // Record a metric for when the study is activated for the first time.
+    // Note: This is not recorded when the study is activated on a subsequent
+    // sessions due to being sticky, because StartMonitoring() is only called
+    // following startup activations of persisted sticky studies.
+    base::UmaHistogramSparse(
+        "Variations.StickyAfterQuery.Activation",
+        static_cast<int>(base::HashFieldTrialName(trial.trial_name())));
     UpdatePref();
   }
 }
