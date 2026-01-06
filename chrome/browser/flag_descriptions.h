b75d5737177727da7e765ca6bf6c1f25238135b9
Stephen McGruer <smcgruer@chromium.org>
2026-01-06 14:16:48 -0800

    Remove "enable-secure-payment-confirmation-availability-api" flag
    The securePaymentConfirmationAvailability API is fully launched on
    SPC-supporting platforms now. The underlying RuntimeEnabledFeatures
    flag, however, has to be kept as the API is disabled on WebView (see
    android_webview/browser/aw_field_trials.cc)
    
    Bug: 465404488
    Change-Id: I43d24f87801d297d7e48799230f7b5224ab36b35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7216451
    Reviewed-by: David Bokan <bokan@chromium.org>
    Auto-Submit: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1565274}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3873,13 +3873,6 @@ inline constexpr char kDynamicSearchUpdateAnimationDescription[] =
     "animations are preempted. Shortened animation durations configurable "
     "(unit: milliseconds).";
 
-inline constexpr char kSecurePaymentConfirmationAvailabilityAPIName[] =
-    "securePaymentConfirmationAvailability API";
-inline constexpr char kSecurePaymentConfirmationAvailabilityAPIDescription[] =
-    "Enables the PaymentRequest.securePaymentConfirmationAvailability web API, "
-    "which allows for more ergonomic feature detection of Secure Payment "
-    "Confirmation";
-
 inline constexpr char kSecurePaymentConfirmationBrowserBoundKeysName[] =
     "Secure Payment Confirmation Browser Bound Key";
 inline constexpr char kSecurePaymentConfirmationBrowserBoundKeysDescription[] =
