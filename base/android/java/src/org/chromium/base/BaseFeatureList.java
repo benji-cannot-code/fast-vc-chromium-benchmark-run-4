67d24c2e394a410c3ed73f3573363776efa70b9b
Shin Kawamura <kawasin@google.com>
2026-01-05 17:08:06 -0800

    Launch EffectiveBindingState feature
    The EffectiveBindingState feature shows the expected reduction on the
    number of Binder IPC and the number of service bindings.
    
    Bug: 445572578
    Change-Id: Ia87fa7e7ba44493d7b9126f04eb7af8435b164d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7384540
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Shin Kawamura <kawasin@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1564711}

--- base/android/java/src/org/chromium/base/BaseFeatureList.java
+++ base/android/java/src/org/chromium/base/BaseFeatureList.java
@@ -20,7 +20,7 @@ public class BaseFeatureList {
 
     public static final MutableFlagWithSafeDefault sEffectiveBindingState =
             new MutableFlagWithSafeDefault(
-                    BaseFeatureMap.getInstance(), BaseFeatures.EFFECTIVE_BINDING_STATE, false);
+                    BaseFeatureMap.getInstance(), BaseFeatures.EFFECTIVE_BINDING_STATE, true);
 
     public static final MutableFlagWithSafeDefault sRebindingChildServiceConnectionController =
             new MutableFlagWithSafeDefault(
