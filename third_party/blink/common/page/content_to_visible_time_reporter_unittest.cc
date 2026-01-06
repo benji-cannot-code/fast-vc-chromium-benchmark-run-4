5a25286a58d2e3e251642fff7fec90e5fbcaea36
Victor Hugo Vianna Silva <victorvianna@google.com>
2026-01-06 14:32:07 -0800

    Fix include order in content_to_visible_time_reporter_unittest.cc
    Change-Id: I576568f94658b239cb63708f0e0af20567e2e419
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7363779
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1565281}

--- third_party/blink/common/page/content_to_visible_time_reporter_unittest.cc
+++ third_party/blink/common/page/content_to_visible_time_reporter_unittest.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "third_party/blink/public/common/page/content_to_visible_time_reporter.h"
+
 #include <string>
 #include <utility>
 #include <vector>
@@ -16,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/task_environment.h"
 #include "components/viz/common/frame_timing_details.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "third_party/blink/public/common/page/content_to_visible_time_reporter.h"
 
 namespace blink {
 
