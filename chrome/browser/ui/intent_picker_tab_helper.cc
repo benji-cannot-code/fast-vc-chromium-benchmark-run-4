149167d9d7512b9080fc668476ad4e3680cb4c58
Alison Gale <agale@chromium.org>
2026-01-06 12:38:31 -0800

    Migrate LayoutConstant enum callers to support enum class
    chrome/browser/ui
    
    This CL was uploaded by an experimental version of git cl split
    (https://crbug.com/389069356).
    
    Bug: 40281257
    Change-Id: I64d0f2db60183acc7e948adcd9c987eb86dc16dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7404159
    Auto-Submit: Alison Gale <agale@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1565202}

--- chrome/browser/ui/intent_picker_tab_helper.cc
+++ chrome/browser/ui/intent_picker_tab_helper.cc
@@ -228,7 +228,7 @@ void IntentPickerTabHelper::MaybeShowIconForApps(
 
       intent_picker_delegate_->LoadSingleAppIcon(
           apps[0].type, current_app_id_,
-          GetLayoutConstant(LOCATION_BAR_ICON_SIZE),
+          GetLayoutConstant(LayoutConstant::kLocationBarIconSize),
           base::BindOnce(&IntentPickerTabHelper::OnAppIconLoadedForChip,
                          per_navigation_weak_factory_.GetWeakPtr(),
                          current_app_id_));
@@ -259,8 +259,9 @@ IntentPickerTabHelper::IntentPickerTabHelper(content::WebContents* web_contents)
       std::make_unique<apps::ChromeOsAppsIntentPickerDelegate>(profile);
 #else
   intent_picker_delegate_ = std::make_unique<apps::WebAppsIntentPickerDelegate>(
-      profile, std::vector<int>{GetLayoutConstant(LOCATION_BAR_ICON_SIZE),
-                                GetIntentPickerBubbleIconSize()});
+      profile,
+      std::vector<int>{GetLayoutConstant(LayoutConstant::kLocationBarIconSize),
+                       GetIntentPickerBubbleIconSize()});
 #endif  // BUILDFLAG(IS_CHROMEOS)
 }
 
