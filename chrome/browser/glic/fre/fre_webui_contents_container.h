f12a7ff852fdc0d658f4ed74fa0f55e2da2d081f
Nick Birnie <birnie@google.com>
2026-01-06 15:53:18 -0800

    [Glic] Fix FRE Escape key handling and metrics
    Re-enables the `CloseWithEsc` interactive UI tests. The Esc key stopped
    working to close the legacy FRE widget. The exact cause is hard to
    pinpoint since the test was disabled due to flakiness. I have fixed this
    for now by moving the ESC key handling out of the app controller and
    into the C++ layer. This fixes the handling and logging for the legacy
    and unified FRE.
    
    Bug: b:427261741
    Change-Id: Ie978c99b6a6e4be164859bebd3926252f56d001b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7361265
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Commit-Queue: Nick Birnie <birnie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1565326}

--- chrome/browser/glic/fre/fre_webui_contents_container.h
+++ chrome/browser/glic/fre/fre_webui_contents_container.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_delegate.h"
 #include "content/public/browser/web_contents_observer.h"
+#include "ui/views/controls/webview/unhandled_keyboard_event_handler.h"
 #include "ui/views/controls/webview/webview.h"
 
 namespace glic {
@@ -31,6 +32,8 @@ class FreWebUIContentsContainer : public content::WebContentsDelegate,
   // content::WebContentsDelegate implementation:
   void SetContentsBounds(content::WebContents* source,
                          const gfx::Rect& bounds) override;
+  bool HandleKeyboardEvent(content::WebContents* source,
+                           const input::NativeWebKeyboardEvent& event) override;
 
   // content::WebContentsObserver:
   void PrimaryMainFrameRenderProcessGone(
@@ -42,6 +45,7 @@ class FreWebUIContentsContainer : public content::WebContentsDelegate,
   const std::unique_ptr<content::WebContents> web_contents_;
   raw_ptr<views::WebView> fre_web_view_;
   raw_ptr<GlicFreController> fre_controller_;
+  views::UnhandledKeyboardEventHandler unhandled_keyboard_event_handler_;
 };
 
 }  // namespace glic
