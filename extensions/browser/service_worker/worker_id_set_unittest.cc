2e276fc7014f29e98a71af08b43174f48fa680d2
Victor Hugo Vianna Silva <victorvianna@google.com>
2026-01-06 11:32:59 -0800

    Fix include order in 2 //extensions files
    Change-Id: Icacb576d265269c6a511ca6917698f5a088feea0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7380099
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1565154}

--- extensions/browser/service_worker/worker_id_set_unittest.cc
+++ extensions/browser/service_worker/worker_id_set_unittest.cc
@@ -3,12 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "extensions/browser/service_worker/worker_id_set.h"
+
 #include <memory>
 #include <string>
 
 #include "base/containers/contains.h"
 #include "extensions/browser/extensions_test.h"
-#include "extensions/browser/service_worker/worker_id_set.h"
 #include "extensions/common/extension_id.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
