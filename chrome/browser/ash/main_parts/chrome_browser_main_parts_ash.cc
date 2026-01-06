de926aed96c0ff9a6de143c70abd817110b900a9
Jun Ishiguro <junis@google.com>
2026-01-05 17:51:54 -0800

    device_name: Remove DeviceNameStore::Initialize/Shutdown calls
    These methods can be safely removed because all callers were guarded by
    the deprecated feature flag.
    
    Bug: b:466903206
    Change-Id: I35c0423be55ac6fbddc2baafe8ab13ab17c69ef7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7274497
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1564740}

--- chrome/browser/ash/main_parts/chrome_browser_main_parts_ash.cc
+++ chrome/browser/ash/main_parts/chrome_browser_main_parts_ash.cc
@@ -96,7 +96,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/dbus/vm/vm_permission_service_provider.h"
 #include "chrome/browser/ash/dbus/vm/vm_sk_forwarding_service_provider.h"
 #include "chrome/browser/ash/dbus/vm/vm_wl_service_provider.h"
-#include "chrome/browser/ash/device_name/device_name_store.h"
 #include "chrome/browser/ash/diagnostics/diagnostics_browser_delegate_impl.h"
 #include "chrome/browser/ash/events/event_rewriter_delegate_impl.h"
 #include "chrome/browser/ash/events/shortcut_mapping_pref_service.h"
@@ -1030,13 +1029,6 @@ void ChromeBrowserMainPartsAsh::PreProfileInit() {
 
   ambient_client_ = std::make_unique<AmbientClientImpl>();
 
-  if (base::FeatureList::IsEnabled(features::kEnableHostnameSetting)) {
-    DeviceNameStore::Initialize(g_browser_process->local_state(),
-                                g_browser_process->platform_part()
-                                    ->browser_policy_connector_ash()
-                                    ->GetDeviceNamePolicyHandler());
-  }
-
   // Set |local_state| for LocalSearchServiceProxyFactory.
   local_search_service::LocalSearchServiceProxyFactory::GetInstance()
       ->SetLocalState(g_browser_process->local_state());
@@ -1615,10 +1607,6 @@ void ChromeBrowserMainPartsAsh::PostMainMessageLoopRun() {
 
   BootTimesRecorder::Get()->AddLogoutTimeMarker("UIMessageLoopEnded",
                                                 /*send_to_uma=*/false);
-
-  if (base::FeatureList::IsEnabled(features::kEnableHostnameSetting)) {
-    DeviceNameStore::Shutdown();
-  }
 
   // This must be shut down before |arc_service_launcher_|.
   if (pre_profile_init_called_) {
