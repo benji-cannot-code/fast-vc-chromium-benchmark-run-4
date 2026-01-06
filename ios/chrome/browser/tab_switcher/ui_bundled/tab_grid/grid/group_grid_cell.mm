f041806a44276284b2266a2340b80fa3e5e5cd86
Anis Chara <achara@google.com>
2026-01-06 02:48:12 -0800

    [iOS] Add tab group color palette and util
    This CL adds a TabGroupColorPalette class for the tab group color
    variants, which will be applied to the tab grid and the tab group view.
    It also introduces a method to generate these variants using a group
    color as a seed.
    
    Bug: 445887468
    Change-Id: Ia25396537adc31ab7882db4874a7cb38557e2644
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7277431
    Commit-Queue: Anis Chara <achara@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1564871}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/grid/group_grid_cell.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/grid/group_grid_cell.mm
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/group_tab_view.h"
 #import "ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_group_snapshots_view.h"
 #import "ios/chrome/browser/tab_switcher/ui_bundled/tab_snapshot_and_favicon.h"
+#import "ios/chrome/browser/tab_switcher/util/tab_group_color_palette.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
 #import "ios/chrome/grit/ios_strings.h"
@@ -345,8 +346,21 @@ const CGFloat kTopBarLargeInset = 20;
 }
 
 - (void)setGroupColor:(UIColor*)groupColor {
-  _dotContainer.color = groupColor;
   _groupColor = groupColor;
+  _dotContainer.color = groupColor;
+
+  if (!IsTabGroupColorOnSurfaceEnabled()) {
+    return;
+  }
+  if (!groupColor) {
+    return;
+  }
+
+  TabGroupColorPalette* tabGroupColorPalette =
+      [[TabGroupColorPalette alloc] initWithSeedColor:groupColor];
+
+  // Apply the right tone to surfaces.
+  _dotContainer.color = tabGroupColorPalette.commonColor;
 }
 
 - (void)setTitle:(NSString*)title {
