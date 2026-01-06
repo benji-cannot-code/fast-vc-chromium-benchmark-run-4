10be94d3880a62a7cfe3c6be58372c801308f882
Dominic Austria <dominicaustria@google.com>
2026-01-06 11:50:29 -0800

    Remove ampersands from showing in open all strings in bookmark folders
    We assume sibling menus in the bookmark bar (resulting from clicking a
    folder and then hovering over another folder) have mnemonics when
    TabGroupMenuImprovements is enabled. This is because we added some new
    items to 'open all' items in a bookmark folder. Please see CL 7122112
    for more context (and a screencast).
    
    Additionally we prevent the new strings from showing in non permanent
    bookmark folders, which coincidentally fixes bug 460480077, re-enabling
    a test that I disabled myself when I was having trouble rewriting it
    with my new changes.
    
    Bug: 471847836
    Change-Id: I831c6ce3fd03d30042f158b1ffea787d5e6501ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7398292
    Commit-Queue: Dominic Austria <dominicaustria@google.com>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1565170}

--- chrome/browser/ui/views/bookmarks/bookmark_menu_controller_views.cc
+++ chrome/browser/ui/views/bookmarks/bookmark_menu_controller_views.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/bookmarks/bookmark_merged_surface_service.h"
 #include "chrome/browser/bookmarks/bookmark_merged_surface_service_observer.h"
 #include "chrome/browser/ui/bookmarks/bookmark_stats.h"
+#include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/views/bookmarks/bookmark_bar_view.h"
 #include "chrome/browser/ui/views/bookmarks/bookmark_menu_controller_observer.h"
 #include "chrome/browser/ui/views/bookmarks/bookmark_menu_delegate.h"
@@ -52,7 +53,9 @@ BookmarkMenuController::BookmarkMenuController(
     run_type |= views::MenuRunner::FOR_DROP;
   }
 
-  run_type |= views::MenuRunner::HAS_MNEMONICS;
+  if (base::FeatureList::IsEnabled(features::kTabGroupMenuImprovements)) {
+    run_type |= views::MenuRunner::HAS_MNEMONICS;
+  }
   menu_runner_ = std::make_unique<views::MenuRunner>(
       base::WrapUnique<MenuItemView>(menu_delegate_->menu()), run_type);
 }
@@ -194,7 +197,8 @@ views::MenuItemView* BookmarkMenuController::GetSiblingMenu(
   menu_delegate_->SetActiveMenu(*folder, start_index);
   *button = bookmark_bar_->GetMenuButtonForFolder(*folder);
   bookmark_bar_->GetAnchorPositionForButton(*button, anchor);
-  *has_mnemonics = false;
+  *has_mnemonics =
+      base::FeatureList::IsEnabled(features::kTabGroupMenuImprovements);
   return this->menu();
 }
 
