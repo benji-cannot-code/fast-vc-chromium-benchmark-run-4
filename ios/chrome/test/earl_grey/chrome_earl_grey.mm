491ac393f7c8838a86d276ce9b0b7784846e37ba
Elmehdi Rahmaoui <erahmaoui@google.com>
2026-01-06 03:10:52 -0800

    [iOS] Callback URL opener EG tests - Incognito text search
    This CL adds new test for text search in incognito using callback URLs,
    with and without profile switching.
    
    Change-Id: I5c22b4a999eb84064b6ab605c9e55513b9f0bfec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7269277
    Commit-Queue: Elmehdi Rahmaoui <erahmaoui@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1564879}

--- ios/chrome/test/earl_grey/chrome_earl_grey.mm
+++ ios/chrome/test/earl_grey/chrome_earl_grey.mm
@@ -1516,6 +1516,11 @@ id<GREYAction> grey_longPressWithDuration(base::TimeDelta duration) {
   return [ChromeEarlGreyAppInterface setAppGroupCommandToSearchText:text];
 }
 
+- (void)setAppGroupCommandToIncognitoSearchText:(NSString*)text {
+  return
+      [ChromeEarlGreyAppInterface setAppGroupCommandToIncognitoSearchText:text];
+}
+
 #pragma mark - Pref Utilities (EG2)
 
 - (void)commitPendingUserPrefsWrite {
