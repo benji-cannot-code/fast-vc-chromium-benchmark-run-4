061f9242e954fcb78c5660c85f2ce1ee278eb8c8
Jingyun Liu <jingyun@google.com>
2026-01-06 10:00:45 -0800

    [LiteRT-LM][ConversationAPI] Add minijinja to third_party/rust.
    minijinja is used as the rendering engine to format the conversation
    context during on-device inference.
    
    ATL discussion thread:
    http://g/chrome-atls-discuss/o6MBsEjjky8/3mBpuoWgCAAJ
    
    Bug: 467777835
    Change-Id: I31338474760e838a9e11b32f081b033ede230cad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7394758
    Commit-Queue: Jingyun Liu <jingyun@google.com>
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Julia Hansbrough <flowerhack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1565080}

---
+++ third_party/rust/chromium_crates_io/vendor/minijinja-v2/src/compiler/mod.rs
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#![allow(missing_docs)]
+/// This module contains the internals of the compiler.
+pub mod ast;
+pub mod codegen;
+pub mod instructions;
+pub mod lexer;
+pub mod meta;
+pub mod parser;
+pub mod tokens;
