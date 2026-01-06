0275120f7befeca2d8bf313d6aaa8c5f4f88a352
Radu Nitescu <radunitescu@google.com>
2026-01-06 09:02:05 -0800

    [iOS][AIM] Fix input plate memory leak retain cycle.
    The input plate owned a `UICollectionViewDiffableDataSource` that was
    initialized with a cell provider function that was strongly capturing
    self.
    
    Bug: 473466935, 470352744
    Change-Id: I041fbca01fac7de9ecf777e514dbe3f46d817ec0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7379110
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1565037}

--- ios/chrome/browser/composebox/ui/composebox_input_plate_view_controller.mm
+++ ios/chrome/browser/composebox/ui/composebox_input_plate_view_controller.mm
@@ -224,16 +224,6 @@ UIImage* SendButtonImage(BOOL highlighted, ComposeboxTheme* theme) {
   if (self) {
     _omniboxContainer = [[UIView alloc] init];
     _theme = theme;
-    [[NSNotificationCenter defaultCenter]
-        addObserver:self
-           selector:@selector(keyboardWillShow:)
-               name:UIKeyboardWillShowNotification
-             object:nil];
-    [[NSNotificationCenter defaultCenter]
-        addObserver:self
-           selector:@selector(keyboardWillHide:)
-               name:UIKeyboardWillHideNotification
-             object:nil];
   }
   return self;
 }
@@ -300,6 +290,25 @@ UIImage* SendButtonImage(BOOL highlighted, ComposeboxTheme* theme) {
         _inputPlateContainerView.layer.cornerRadius;
   }
   [self updateCarouselFade];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+  [super viewWillAppear:animated];
+  [[NSNotificationCenter defaultCenter]
+      addObserver:self
+         selector:@selector(keyboardWillShow:)
+             name:UIKeyboardWillShowNotification
+           object:nil];
+  [[NSNotificationCenter defaultCenter]
+      addObserver:self
+         selector:@selector(keyboardWillHide:)
+             name:UIKeyboardWillHideNotification
+           object:nil];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+  [super viewWillDisappear:animated];
+  [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 #pragma mark - Public
@@ -704,6 +713,7 @@ UIImage* SendButtonImage(BOOL highlighted, ComposeboxTheme* theme) {
 - (UICollectionViewDiffableDataSource<NSString*, ComposeboxInputItem*>*)
     createDataSource {
   __weak ComposeboxTheme* theme = _theme;
+  __weak __typeof(self) weakSelf = self;
   return [[UICollectionViewDiffableDataSource alloc]
       initWithCollectionView:_carouselView
                 cellProvider:^UICollectionViewCell*(
@@ -715,7 +725,7 @@ UIImage* SendButtonImage(BOOL highlighted, ComposeboxTheme* theme) {
                               kItemCellReuseIdentifier
                                                     forIndexPath:indexPath];
                   [cell configureWithItem:item theme:theme];
-                  cell.delegate = self;
+                  cell.delegate = weakSelf;
                   return cell;
                 }];
 }
