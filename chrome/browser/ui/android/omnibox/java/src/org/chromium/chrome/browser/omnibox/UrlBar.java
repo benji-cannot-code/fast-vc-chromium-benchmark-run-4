71be97ffae5143c2f8fb702b8a7d6283cc8237df
Tomasz Wiszkowski <ender@google.com>
2026-01-06 12:11:47 -0800

    Fix Elegant Text Height in Fusebox/Omnibox
    The change drops post()ing calculation in response to pending layouts
    as the algorithm no longer uses runtime view measurements to devise
    font height.
    
    Change-Id: I5c659172b28d31c6f7a7ed24e2fa38db761ca2be
    Bug: 470444763
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7282079
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1565183}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/UrlBar.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/UrlBar.java
@@ -107,7 +107,6 @@ public class UrlBar extends AutocompleteEditText {
     private @Nullable Callback<Boolean> mUrlTextWrappingChangeListener;
 
     private final Rect mClipBounds = new Rect();
-    @VisibleForTesting final Runnable mEnforceMaxTextHeight = this::enforceMaxTextHeight;
 
     private boolean mFocused;
     private boolean mFocusEventEmitted;
@@ -391,6 +390,7 @@ public class UrlBar extends AutocompleteEditText {
     @Override
     public void onFinishInflate() {
         super.onFinishInflate();
+        enforceMaxTextHeight();
         setPrivateImeOptions(IME_OPTION_RESTRICT_STYLUS_WRITING_AREA);
     }
 
@@ -1130,12 +1130,6 @@ public class UrlBar extends AutocompleteEditText {
     @Override
     public void layout(int left, int top, int right, int bottom) {
         super.layout(left, top, right, bottom);
-        // Do not scale the Omnibox font size if our height is set to WRAP_CONTENT.
-        // This ensures we don't trigger the recurring layout/adjust/layout/adjust cycle.
-        if (getLayoutParams().height != LayoutParams.WRAP_CONTENT) {
-            post(mEnforceMaxTextHeight);
-        }
-
         // Note: this must happen after the *entire* layout cycle completes.
         // Running this during onLayout guarantees that isLayoutRequested will remain true,
         // and the text layout will remain unresolved, suppressing resolution of display text
@@ -1290,15 +1284,8 @@ public class UrlBar extends AutocompleteEditText {
     @VisibleForTesting
     void enforceMaxTextHeight() {
         if (mUseSmallTextHeight) return;
-        // Our viewHeight calculation may not be correct if layout is requested, e.g. if our padding
-        // and height change simultaneously. The padding change will be reflected immediately, but
-        // the height change requires a layout cycle to be reflected.
-        if (isLayoutRequested()) {
-            post(mEnforceMaxTextHeight);
-            return;
-        }
 
-        int viewHeight = getHeight() - getPaddingTop() - getPaddingBottom();
+        int viewHeight = getResources().getDimensionPixelSize(R.dimen.location_bar_height);
         // Don't touch the text size if the view has not measured and shown yet, or if it's a
         // subject to custom layout constraints (e.g. CCT) that might result with font size being
         // too small.
