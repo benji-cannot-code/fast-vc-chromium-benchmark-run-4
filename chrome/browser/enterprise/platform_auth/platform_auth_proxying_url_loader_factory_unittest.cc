aab3e5232e0c39a15e49e53851c2c77f3e644f09
Stanisław Solarewicz <ssolarewicz@chromium.org>
2026-01-06 10:18:47 -0800

    [Okta SSO] Check Blocked IdPs policy before proxying
    ExtensibleEnterpriseSSOBlocklist allows admins to block chosen IdPs from
    working with Chrome's SSO functionality.
    
    This cl adds a check that makes sure Okta has not been blocked before
    injecting the ProxyingURLLoaderFactory, which effectively dictates
    whether the feature is enabled or not.
    
    Bug: b:433226247
    Change-Id: I2598a44256921e324c1eea2c04609f2cdaf794b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7277353
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    Commit-Queue: Stanisław Solarewicz <ssolarewicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1565094}

--- chrome/browser/enterprise/platform_auth/platform_auth_proxying_url_loader_factory_unittest.cc
+++ chrome/browser/enterprise/platform_auth/platform_auth_proxying_url_loader_factory_unittest.cc
@@ -17,8 +17,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/bind.h"
 #include "base/test/task_environment.h"
 #include "base/test/test_future.h"
+#include "base/values.h"
 #include "chrome/browser/enterprise/platform_auth/platform_auth_provider_manager.h"
+#include "chrome/common/pref_names.h"
+#include "chrome/test/base/testing_browser_process.h"
 #include "components/download/public/common/download_url_parameters.h"
+#include "components/policy/policy_constants.h"
+#include "components/prefs/pref_registry_simple.h"
+#include "components/prefs/testing_pref_service.h"
 #include "content/public/test/browser_task_environment.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
@@ -248,6 +254,32 @@ TEST_F(PlatformAuthProxyingURLLoaderFactoryTest,
 TEST_F(PlatformAuthProxyingURLLoaderFactoryTest,
        MaybeProxyRequest_PlatformAuthManagerDisabled) {
   PlatformAuthProviderManager::GetInstance().SetEnabled(false, {});
+
+  network::TestURLLoaderFactory terminal_factory;
+  scoped_refptr<network::SharedURLLoaderFactory> terminal_shared_factory =
+      terminal_factory.GetSafeWeakWrapper();
+
+  network::URLLoaderFactoryBuilder factory_builder;
+
+  ProxyingURLLoaderFactory::MaybeProxyRequest(
+      url::Origin::Create(GURL("https://foobar.example.com/")),
+      ChromeContentBrowserClient::URLLoaderFactoryType::kDocumentSubResource,
+      factory_builder);
+
+  scoped_refptr<network::SharedURLLoaderFactory> resulting_factory =
+      std::move(factory_builder).Finish(terminal_shared_factory);
+
+  EXPECT_FALSE(
+      FactoryHasInterceptors(resulting_factory, terminal_shared_factory));
+}
+
+TEST_F(PlatformAuthProxyingURLLoaderFactoryTest,
+       MaybeProxyRequest_OktaIdpBlocked) {
+  PlatformAuthProviderManager::GetInstance().SetEnabled(true, {});
+
+  base::Value::List enabled_idps;
+  TestingBrowserProcess::GetGlobal()->GetTestingLocalState()->SetList(
+      prefs::kExtensibleEnterpriseSSOEnabledIdps, std::move(enabled_idps));
 
   network::TestURLLoaderFactory terminal_factory;
   scoped_refptr<network::SharedURLLoaderFactory> terminal_shared_factory =
