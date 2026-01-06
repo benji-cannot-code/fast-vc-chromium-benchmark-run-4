50ece6476370deee8ba318c430535c0e9ac10848
Kelsen Liu <kelsenliu@chromium.org>
2026-01-06 07:56:15 -0800

    gpu: Spanify FencedAllocatorWrapper to avoid unsafe buffer usage
    This CL changes FencedAllocatorWrapper::Alloc() to return
    base::span<uint8_t> instead of a raw ptr.
    
    Bug: 40284755
    Change-Id: I6fdcbe93a1d7bc358826456738435cf4ca2a9810
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7319949
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: kelsen liu <kelsenliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1564976}

--- gpu/command_buffer/client/mapped_memory.cc
+++ gpu/command_buffer/client/mapped_memory.cc
@@ -38,7 +38,7 @@ MemoryChunk::MemoryChunk(int32_t shm_id,
                          CommandBufferHelper* helper)
     : shm_id_(shm_id),
       shm_(shm),
-      allocator_(shm->size(), helper, shm->memory()) {}
+      allocator_(shm->size(), helper, shm->as_byte_span()) {}
 
 MemoryChunk::~MemoryChunk() = default;
 
@@ -73,11 +73,11 @@ void* MappedMemoryManager::Alloc(unsigned int size,
       chunk->FreeUnused();
       total_bytes_in_use += chunk->bytes_in_use();
       if (chunk->GetLargestFreeSizeWithoutWaiting() >= size) {
-        void* mem = chunk->Alloc(size);
-        DCHECK(mem);
+        auto span = chunk->Alloc(size);
+        DCHECK(!span.empty());
         *shm_id = chunk->shm_id();
-        *shm_offset = chunk->GetOffset(mem);
-        return mem;
+        *shm_offset = chunk->GetOffset(span.data());
+        return span.data();
       }
     }
 
@@ -89,11 +89,11 @@ void* MappedMemoryManager::Alloc(unsigned int size,
       TRACE_EVENT0("gpu", "MappedMemoryManager::Alloc::wait");
       for (auto& chunk : chunks_) {
         if (chunk->GetLargestFreeSizeWithWaiting() >= size) {
-          void* mem = chunk->Alloc(size);
-          DCHECK(mem);
+          auto span = chunk->Alloc(size);
+          DCHECK(!span.empty());
           *shm_id = chunk->shm_id();
-          *shm_offset = chunk->GetOffset(mem);
-          return mem;
+          *shm_offset = chunk->GetOffset(span.data());
+          return span.data();
         }
       }
     }
@@ -121,11 +121,11 @@ void* MappedMemoryManager::Alloc(unsigned int size,
   MemoryChunk* mc = new MemoryChunk(id, shm, helper_);
   allocated_memory_ += mc->GetSize();
   chunks_.push_back(base::WrapUnique(mc));
-  void* mem = mc->Alloc(size);
-  DCHECK(mem);
+  auto span = mc->Alloc(size);
+  DCHECK(!span.empty());
   *shm_id = mc->shm_id();
-  *shm_offset = mc->GetOffset(mem);
-  return mem;
+  *shm_offset = mc->GetOffset(span.data());
+  return span.data();
 }
 
 void MappedMemoryManager::Free(void* pointer) {
