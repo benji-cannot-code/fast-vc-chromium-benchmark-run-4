cb886356f5a3c86ef35854e0682fd140475c0d7c
Mike Taylor <miketaylr@chromium.org>
2026-01-06 10:51:31 -0800

    Remove kUserAgentFrozenBuildVersion feature param
    We had the ability to experiment with different build version constants,
    for web compatibility purposes. But we have shipped with a frozen "0"
    build number in the User-Agent string for a few years, so we can clean
    this up.
    
    Bug: 470033627
    Change-Id: I25a6eb508123477f648104e6b6df9a68c043cb5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7399059
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Victor Tan <victortan@chromium.org>
    Commit-Queue: Mike Taylor <miketaylr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1565116}

--- components/version_info/version_info_with_user_agent.cc
+++ components/version_info/version_info_with_user_agent.cc
@@ -10,10 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace version_info {
 
-std::string GetProductNameAndVersionForReducedUserAgent(
-    const std::string& build_version) {
-  return base::StrCat(
-      {"Chrome/", GetMajorVersionNumber(), ".0.", build_version, ".0"});
+std::string GetProductNameAndVersionForReducedUserAgent() {
+  return base::StrCat({"Chrome/", GetMajorVersionNumber(), ".0.0.0"});
 }
 
 }  // namespace version_info
