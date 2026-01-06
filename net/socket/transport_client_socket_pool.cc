636d58303f8cb0847b1bec2408386c080d94c058
Victor Hugo Vianna Silva <victorvianna@google.com>
2026-01-06 05:55:15 -0800

    Migrate various base::Contains() to contains() in net
    None of these files removes the base/containers/contains.h include
    because they still use it.
    
    Bug: 470391351
    Change-Id: Iace0c96e8424df55629cf6607747c79402a23ccd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7363780
    Commit-Queue: David Schinazi <dschinazi@chromium.org>
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1564920}

--- net/socket/transport_client_socket_pool.cc
+++ net/socket/transport_client_socket_pool.cc
@@ -240,14 +240,14 @@ bool TransportClientSocketPool::IsStalled() const {
 void TransportClientSocketPool::AddHigherLayeredPool(
     HigherLayeredPool* higher_pool) {
   CHECK(higher_pool);
-  CHECK(!base::Contains(higher_pools_, higher_pool));
+  CHECK(!higher_pools_.contains(higher_pool));
   higher_pools_.insert(higher_pool);
 }
 
 void TransportClientSocketPool::RemoveHigherLayeredPool(
     HigherLayeredPool* higher_pool) {
   CHECK(higher_pool);
-  CHECK(base::Contains(higher_pools_, higher_pool));
+  CHECK(higher_pools_.contains(higher_pool));
   higher_pools_.erase(higher_pool);
 }
 
@@ -362,11 +362,11 @@ int TransportClientSocketPool::RequestSockets(
     }
     if (rv < 0 && rv != ERR_IO_PENDING) {
       // We're encountering a synchronous error.  Give up.
-      if (!base::Contains(group_map_, group_id))
+      if (!group_map_.contains(group_id))
         deleted_group = true;
       break;
     }
-    if (!base::Contains(group_map_, group_id)) {
+    if (!group_map_.contains(group_id)) {
       // Unexpected.  The group should only be getting deleted on synchronous
       // error.
       NOTREACHED();
@@ -584,7 +584,7 @@ void TransportClientSocketPool::SetPriority(const GroupId& group_id,
                                             RequestPriority priority) {
   auto group_it = group_map_.find(group_id);
   if (group_it == group_map_.end()) {
-    DCHECK(base::Contains(pending_callback_map_, handle));
+    DCHECK(pending_callback_map_.contains(handle));
     // The Request has already completed and been destroyed; nothing to
     // reprioritize.
     return;
@@ -620,7 +620,7 @@ void TransportClientSocketPool::CancelRequest(const GroupId& group_id,
     return;
   }
 
-  CHECK(base::Contains(group_map_, group_id));
+  CHECK(group_map_.contains(group_id));
   // Don't try to disable `fail_if_alias_require_proxy_override_` during a
   // request cancellation.
   Group* group = GetOrCreateGroup(
@@ -691,7 +691,7 @@ size_t TransportClientSocketPool::IdleSocketCountInGroup(
 LoadState TransportClientSocketPool::GetLoadState(
     const GroupId& group_id,
     const ClientSocketHandle* handle) const {
-  if (base::Contains(pending_callback_map_, handle))
+  if (pending_callback_map_.contains(handle))
     return LOAD_STATE_CONNECTING;
 
   auto group_it = group_map_.find(group_id);
@@ -911,7 +911,7 @@ void TransportClientSocketPool::OnSSLConfigForServersChanged(
 }
 
 bool TransportClientSocketPool::HasGroup(const GroupId& group_id) const {
-  return base::Contains(group_map_, group_id);
+  return group_map_.contains(group_id);
 }
 
 void TransportClientSocketPool::CleanupIdleSockets(
@@ -1192,7 +1192,7 @@ void TransportClientSocketPool::RemoveConnectJob(ConnectJob* job,
 
 void TransportClientSocketPool::OnAvailableSocketSlot(const GroupId& group_id,
                                                       Group* group) {
-  DCHECK(base::Contains(group_map_, group_id));
+  DCHECK(group_map_.contains(group_id));
   if (group->IsEmpty()) {
     RemoveGroup(group_id);
   } else if (group->has_unbound_requests()) {
@@ -1463,7 +1463,7 @@ void TransportClientSocketPool::InvokeUserCallbackLater(
     CompletionOnceCallback callback,
     int rv,
     const SocketTag& socket_tag) {
-  CHECK(!base::Contains(pending_callback_map_, handle));
+  CHECK(!pending_callback_map_.contains(handle));
   pending_callback_map_[handle] = CallbackResultPair(std::move(callback), rv);
   if (rv == OK) {
     handle->socket()->ApplySocketTag(socket_tag);
--- net/spdy/spdy_session_pool.cc
+++ net/spdy/spdy_session_pool.cc
@@ -635,7 +635,7 @@ void SpdySessionPool::RemoveRequestForSpdySession(SpdySessionRequest* request) {
                        weak_ptr_factory_.GetWeakPtr(), request->key()));
   }
 
-  DCHECK(base::Contains(iter->second.request_set, request));
+  DCHECK(iter->second.request_set.contains(request));
   RemoveRequestInternal(iter, iter->second.request_set.find(request));
 }
 
@@ -655,7 +655,7 @@ void SpdySessionPool::MapKeyToAvailableSession(
     const SpdySessionKey& key,
     const base::WeakPtr<SpdySession>& session,
     std::set<std::string> dns_aliases) {
-  DCHECK(base::Contains(sessions_, session.get()));
+  DCHECK(sessions_.contains(session.get()));
   std::pair<AvailableSessionMap::iterator, bool> result =
       available_sessions_.emplace(key, session);
   CHECK(result.second);
@@ -947,7 +947,7 @@ void SpdySessionPool::AddConnectionManagementConfig(
   // We only want to use the connection management config for the session if the
   // feature is enabled.
   if (connection_management_config.connection_change_observer) {
-    if (!base::Contains(connection_change_notifier_map_, key)) {
+    if (!connection_change_notifier_map_.contains(key)) {
       connection_change_notifier_map_[key] =
           std::make_unique<ConnectionChangeNotifier>();
     }
