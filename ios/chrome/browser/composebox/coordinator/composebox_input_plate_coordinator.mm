9219ae9bc01ee20f75708bc0ef39be646fa3d258
Ameur Hosni <ameurhosni@google.com>
2026-01-06 10:06:31 -0800

    [IOS][AIM] Refocus the composebox when dismissing the camera picker
    Bug: 463660529
    Change-Id: If5e43a921098b061bb2ddd27d6d10dec989da87e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7368603
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1565088}

--- ios/chrome/browser/composebox/coordinator/composebox_input_plate_coordinator.mm
+++ ios/chrome/browser/composebox/coordinator/composebox_input_plate_coordinator.mm
@@ -415,7 +415,13 @@ const CGFloat kSnackbarBottomMargin = 10;
 
 - (void)imagePickerController:(UIImagePickerController*)picker
     didFinishPickingMediaWithInfo:(NSDictionary<NSString*, id>*)info {
-  [picker dismissViewControllerAnimated:YES completion:nil];
+  __weak __typeof(self) weakSelf = self;
+
+  [picker dismissViewControllerAnimated:YES
+                             completion:^{
+                               [weakSelf focusComposebox];
+                             }];
+
   UIImage* image = info[UIImagePickerControllerOriginalImage];
   if (!image) {
     return;
@@ -425,7 +431,12 @@ const CGFloat kSnackbarBottomMargin = 10;
 }
 
 - (void)imagePickerControllerDidCancel:(UIImagePickerController*)picker {
-  [picker dismissViewControllerAnimated:YES completion:nil];
+  __weak __typeof(self) weakSelf = self;
+
+  [picker dismissViewControllerAnimated:YES
+                             completion:^{
+                               [weakSelf focusComposebox];
+                             }];
 }
 
 #pragma mark - ComposeboxInputPlateMediatorDelegate
@@ -517,6 +528,10 @@ const CGFloat kSnackbarBottomMargin = 10;
 }
 
 #pragma mark - Private helpers
+
+- (void)focusComposebox {
+  [_omniboxCoordinator focusOmnibox];
+}
 
 /// Dismisses the composebox via a command to the browser coordinator.
 - (void)dismissComposebox {
