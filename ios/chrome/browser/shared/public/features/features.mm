7f648bf5123ebc13289b7364c754ba6c7cfcc01a
Quentin Pubert <qpubert@google.com>
2026-01-06 08:57:54 -0800

    Reland "[iOS] Remove Choose from Drive flag and prepare feature cleanup"
    This is a reland of commit 23e0bdeb7abd03aff123e6079be1b0bcf3bf67b1
    
    Original change's description:
    > [iOS] Remove Choose from Drive flag and prepare feature cleanup
    >
    > The kIOSChooseFromDrive feature is now enabled by default so it can be
    > cleaned up soon. This CL removes the flag metadata and adds guards
    > around the declaration of kIOSChooseFromDrive so it can be cleaned up
    > once __IPHONE_OS_VERSION_MIN_REQUIRED >= __IPHONE_18_4 i.e. once the new
    > file upload menu implementation completely replaces the old one.
    >
    > Change-Id: I1bdeb5b1fd6dee0c86a36c7112f1c727cff6693c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7367719
    > Auto-Submit: Quentin Pubert <qpubert@google.com>
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1564221}
    
    Change-Id: I21c541fb9532ba2311533fc4755aff80efb202b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7367480
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Auto-Submit: Quentin Pubert <qpubert@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1565034}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -371,6 +371,7 @@ TipsSafeBrowsingExperimentType TipsSafeBrowsingExperimentTypeEnabled() {
       (int)TipsSafeBrowsingExperimentType::kShowEnhancedSafeBrowsingPromo));
 }
 
+// TODO(crbug.com/473788390): Clean-up feature once file upload menu is ready.
 BASE_FEATURE(kIOSChooseFromDrive, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kIOSDownloadNoUIUpdateInBackground,
