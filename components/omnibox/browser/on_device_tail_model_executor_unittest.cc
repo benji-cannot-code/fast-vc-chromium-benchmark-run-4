bb773223bfdad88d9e1f2ad8bbaf2b3f8a576f34
Victor Hugo Vianna Silva <victorvianna@google.com>
2026-01-06 04:33:15 -0800

    Fix include order in on_device_tail_model_executor_unittest.cc
    Change-Id: Ia91cb2134644a94cf5b2afc545ab0da0f9cf47c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7362197
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1564892}

--- components/omnibox/browser/on_device_tail_model_executor_unittest.cc
+++ components/omnibox/browser/on_device_tail_model_executor_unittest.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "components/omnibox/browser/on_device_tail_model_executor.h"
+
 #include <memory>
 #include <string>
 #include <vector>
@@ -12,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
-#include "components/omnibox/browser/on_device_tail_model_executor.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
