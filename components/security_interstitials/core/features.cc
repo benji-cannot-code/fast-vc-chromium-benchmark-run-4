4524ce08dc52f52660ee7fe6eb768b751f465a38
Chris Thompson <cthomp@chromium.org>
2026-01-06 10:59:59 -0800

    [HFM] Disable dialog UI flag on Android
    The `HttpsFirstDialogUi` flag was default enabled in
    crrev.com/c/7173972, but this had unintended side-effects on Android.
    For expediency, this CL just disables the feature flag on Android.
    
    Bug: 469092867
    Change-Id: I9d39b7ec1aa918c681a290ea1fdbf24d73dfac32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7399299
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Auto-Submit: Chris Thompson <cthomp@chromium.org>
    Commit-Queue: Chris Thompson <cthomp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1565119}

--- components/security_interstitials/core/features.cc
+++ components/security_interstitials/core/features.cc
@@ -6,10 +6,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/security_interstitials/core/features.h"
 
 #include "base/feature_list.h"
+#include "build/build_config.h"
 
 namespace security_interstitials::features {
 
 // Enables a dialog-based UI for HTTPS-First Mode.
+// The flag is currently disabled on Android as the new UI is not implemented
+// there yet. (See crbug.com/469092867 for some previous unintended side
+// effects.)
+#if BUILDFLAG(IS_ANDROID)
+BASE_FEATURE(kHttpsFirstDialogUi, base::FEATURE_DISABLED_BY_DEFAULT);
+#else
 BASE_FEATURE(kHttpsFirstDialogUi, base::FEATURE_ENABLED_BY_DEFAULT);
+#endif
 
 }  // namespace security_interstitials::features
