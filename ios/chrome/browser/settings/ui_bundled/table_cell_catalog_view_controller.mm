2d915bf8a0a6d16c1ddf8579629ba8aad019ca8a
Gauthier Ambard <gambard@chromium.org>
2026-01-06 10:28:33 -0800

    [iOS] Prepare TableViewAccountItem to move to ContentConfig
    This CL removes some dead code and also merges "managed" and "error" as
    it is not possible for them to be present at the same time.
    
    I tried to repro crbug.com/375377471 on iOS 18.1, but it seems to work:
    https://screencast.googleplex.com/cast/NjA3MTU0MDQxMTAwNjk3NnxjODcwN2UyZC1mMQ
    
    Bug: 443034511
    Change-Id: I58e205a4fec956b88ca9e4ee6303a01267ff7658
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7349381
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1565100}

--- ios/chrome/browser/settings/ui_bundled/table_cell_catalog_view_controller.mm
+++ ios/chrome/browser/settings/ui_bundled/table_cell_catalog_view_controller.mm
@@ -661,7 +661,7 @@ typedef NS_ENUM(NSInteger, ItemType) {
       @"Syncing to AccountUserNameAccount@example.com";
   accountItemDetailWithError.accessoryType =
       UITableViewCellAccessoryDisclosureIndicator;
-  accountItemDetailWithError.shouldDisplayError = YES;
+  accountItemDetailWithError.detailImage = TableViewAccountDetailImage::kError;
   [model addItem:accountItemDetailWithError
       toSectionWithIdentifier:SectionIdentifierAccount];
 
