4753c2c003d3e527b023dc15f2fe8ce0cc2efa96
Devlin Cronin <rdevlin.cronin@chromium.org>
2026-01-05 17:38:04 -0800

    [Desktop Android] Create TabsEventRouterPlatformDelegate
    We need to consolidate logic for a single TabsEventRouter between
    existing desktop platforms and the experimental desktop android
    platform. Right now, we have fully separate implementations:
    TabsEventRouter and TabsEventRouterAndroid.
    
    As a step towards introducing a single class to handle this logic, this
    CL introduces a shared TabsEventRouter class. To make reviewing easy,
    this CL *just* introduces this class and moves the existing platform-
    specific implementations to separate TabsEventRouterPlatformDelegate
    implementations.
    
    That is:
    * TabsEventRouter -> TabsEventRouterPlatformDelegate (NonAndroid)
    * TabsEventRouterAndroid -> TabsEventRouterPlatformDelegate (Android)
    
    And a TabsEventRouter that owns the (platform-appropriate)
    TabsEventRouterPlatformDelegate.
    
    Subsequent CLs will pull shared logic into this parent TabsEventRouter,
    working towards eventually minimizing or fully removing the
    PlatformDelegates.
    
    This CL has no behavior change.
    
    Bug: 473593117
    Change-Id: I72f8cb8a67522dca5e598da0f5218ef7c8b2e57f
    Bypass-Check-License: Moving files. See CL description.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7396692
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1564731}

--- chrome/browser/extensions/api/tabs/tabs_event_router_platform_delegate_android.h
+++ chrome/browser/extensions/api/tabs/tabs_event_router_platform_delegate_android.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_EXTENSIONS_API_TABS_TABS_EVENT_ROUTER_ANDROID_H_
-#define CHROME_BROWSER_EXTENSIONS_API_TABS_TABS_EVENT_ROUTER_ANDROID_H_
+#ifndef CHROME_BROWSER_EXTENSIONS_API_TABS_TABS_EVENT_ROUTER_PLATFORM_DELEGATE_ANDROID_H_
+#define CHROME_BROWSER_EXTENSIONS_API_TABS_TABS_EVENT_ROUTER_PLATFORM_DELEGATE_ANDROID_H_
 
 #include <map>
 #include <memory>
@@ -29,19 +29,18 @@ class WebContents;
 
 namespace extensions {
 
-// TabsEventRouterAndroid listens to tab events on Android and routes them to
-// listeners inside extension process renderers.
-//
-// TODO(crbug.com/427503497): This class is a temporary solution to unblock the
+// TODO(crbug.com/473593117): This class is a temporary solution to unblock the
 // Tabs API on desktop Android. It can be deleted once TabsEventRouter works on
 // desktop Android.
-class TabsEventRouterAndroid : public TabModelListObserver,
-                               public TabModelObserver {
+class TabsEventRouterPlatformDelegate : public TabModelListObserver,
+                                        public TabModelObserver {
  public:
-  explicit TabsEventRouterAndroid(Profile* profile);
-  TabsEventRouterAndroid(const TabsEventRouterAndroid&) = delete;
-  TabsEventRouterAndroid& operator=(const TabsEventRouterAndroid&) = delete;
-  ~TabsEventRouterAndroid() override;
+  explicit TabsEventRouterPlatformDelegate(Profile* profile);
+  TabsEventRouterPlatformDelegate(const TabsEventRouterPlatformDelegate&) =
+      delete;
+  TabsEventRouterPlatformDelegate& operator=(
+      const TabsEventRouterPlatformDelegate&) = delete;
+  ~TabsEventRouterPlatformDelegate() override;
 
   // TabModelListObserver:
   void OnTabModelAdded(TabModel* tab_model) override;
@@ -54,7 +53,8 @@ class TabsEventRouterAndroid : public TabModelListObserver,
  private:
   class TabEntry : public content::WebContentsObserver {
    public:
-    TabEntry(TabsEventRouterAndroid* router, content::WebContents* contents);
+    TabEntry(TabsEventRouterPlatformDelegate* owner,
+             content::WebContents* contents);
     ~TabEntry() override;
 
     // content::WebContentsObserver:
@@ -62,7 +62,7 @@ class TabsEventRouterAndroid : public TabModelListObserver,
     void TitleWasSet(content::NavigationEntry* entry) override;
     void WebContentsDestroyed() override;
 
-    const raw_ptr<TabsEventRouterAndroid> router_;
+    const raw_ptr<TabsEventRouterPlatformDelegate> owner_;
     GURL url_;
   };
 
@@ -97,4 +97,4 @@ class TabsEventRouterAndroid : public TabModelListObserver,
 
 }  // namespace extensions
 
-#endif  // CHROME_BROWSER_EXTENSIONS_API_TABS_TABS_EVENT_ROUTER_ANDROID_H_
+#endif  // CHROME_BROWSER_EXTENSIONS_API_TABS_TABS_EVENT_ROUTER_PLATFORM_DELEGATE_ANDROID_H_
--- chrome/browser/extensions/api/tabs/tabs_windows_api.cc
+++ chrome/browser/extensions/api/tabs/tabs_windows_api.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/lazy_instance.h"
+#include "chrome/browser/extensions/api/tabs/tabs_event_router.h"
 #include "chrome/browser/extensions/api/tabs/windows_event_router.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/extensions/api/tabs.h"
@@ -15,12 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/event_router.h"
 #include "extensions/browser/extension_system.h"
 #include "extensions/buildflags/buildflags.h"
-
-#if BUILDFLAG(IS_ANDROID)
-#include "chrome/browser/extensions/api/tabs/tabs_event_router_android.h"
-#else
-#include "chrome/browser/extensions/api/tabs/tabs_event_router.h"
-#endif
 
 static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
@@ -66,26 +61,13 @@ TabsWindowsAPI* TabsWindowsAPI::Get(content::BrowserContext* context) {
 }
 
 void TabsWindowsAPI::InitTabsEventRouter() {
-#if BUILDFLAG(IS_ANDROID)
-  tabs_event_router_android_ = std::make_unique<TabsEventRouterAndroid>(
-      Profile::FromBrowserContext(browser_context_));
-#else
   tabs_event_router_ = std::make_unique<TabsEventRouter>(
       Profile::FromBrowserContext(browser_context_));
-#endif
 }
 
-#if BUILDFLAG(IS_ANDROID)
-// TODO(crbug.com/371432155): Delete this method once TabsEventRouter works on
-// desktop Android.
-TabsEventRouterAndroid* TabsWindowsAPI::tabs_event_router_android() {
-  return tabs_event_router_android_.get();
-}
-#else
 TabsEventRouter* TabsWindowsAPI::tabs_event_router() {
   return tabs_event_router_.get();
 }
-#endif
 
 WindowsEventRouter* TabsWindowsAPI::windows_event_router() {
   return windows_event_router_.get();
