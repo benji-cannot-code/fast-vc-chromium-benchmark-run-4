cff04cef8e7b4a3f68cfad41f2a1c163d4c99950
Dan Harrington <harringtond@chromium.org>
2026-01-07 09:31:56 -0800

    Make GlicRegionCaptureController desktop only
    Bug: b:470059315
    Change-Id: Idd71f63ab6a84dbb38bfae964a1928c24f8b760b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7405497
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Commit-Queue: Dan Harrington <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1565730}

--- chrome/browser/glic/public/glic_keyed_service.cc
+++ chrome/browser/glic/public/glic_keyed_service.cc
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/glic/host/context/glic_tab_data.h"
 #include "chrome/browser/glic/host/context/glic_tab_data_observer.h"
 #include "chrome/browser/glic/host/glic.mojom.h"
-#include "chrome/browser/glic/host/glic_region_capture_controller.h"
 #include "chrome/browser/glic/host/glic_web_client_access.h"
 #include "chrome/browser/glic/host/glic_web_contents_warming_pool.h"
 #include "chrome/browser/glic/host/host.h"
@@ -78,6 +77,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if !BUILDFLAG(IS_ANDROID)
 #include "chrome/browser/glic/glic_metrics.h"
 #include "chrome/browser/glic/glic_occlusion_notifier.h"
+#include "chrome/browser/glic/host/glic_region_capture_controller.h"
 #include "chrome/browser/glic/widget/glic_widget.h"
 #include "chrome/browser/glic/widget/glic_window_controller_impl.h"
 #endif
@@ -172,8 +172,10 @@ GlicKeyedService::GlicKeyedService(
                                             &window_controller(),
                                             metrics_.get(),
                                             enabling_.get())),
+#if !BUILDFLAG(IS_ANDROID)
       region_capture_controller_(
           std::make_unique<GlicRegionCaptureController>()),
+#endif
       auth_controller_(std::make_unique<AuthController>(profile,
                                                         identity_manager,
                                                         /*use_for_fre=*/false)),
@@ -238,9 +240,11 @@ GlicKeyedService::~GlicKeyedService() {
   metrics_->SetControllers(nullptr, nullptr);
 }
 
+#if !BUILDFLAG(IS_ANDROID)
 GlicRegionCaptureController& GlicKeyedService::region_capture_controller() {
   return *region_capture_controller_;
 }
+#endif
 
 // static
 GlicKeyedService* GlicKeyedService::Get(content::BrowserContext* context) {
@@ -586,11 +590,13 @@ base::CallbackListSubscription GlicKeyedService::AddUserInputSubmittedCallback(
   return user_input_submitted_callback_list_.Add(std::move(callback));
 }
 
+#if !BUILDFLAG(IS_ANDROID)
 void GlicKeyedService::CaptureRegion(
     content::WebContents* web_contents,
     mojo::PendingRemote<mojom::CaptureRegionObserver> observer) {
   region_capture_controller_->CaptureRegion(web_contents, std::move(observer));
 }
+#endif
 
 void GlicKeyedService::ShareContextImage(tabs::TabInterface* tab,
                                          content::RenderFrameHost* frame,
