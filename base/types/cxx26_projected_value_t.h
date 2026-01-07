9c8e2970503eb5f99946377d97c3ce1797547adf
Jan Wilken Dörrie <jdoerrie@chromium.org>
2026-01-06 22:22:49 -0800

    [base] Allow specifying result type in base::ToVector
    Currently, base::ToVector deduces the output vector's value type based
    on the range's value type or the projection's return type. This change
    adds support for explicitly specifying the destination value type via a
    template argument.
    
    This enables conversions during vector construction, such as creating a
    std::vector<int64_t> from a range of ints, or a
    std::vector<std::string_view> from an array of string literals.
    
    To facilitate this, base::projected_value_t is introduced as a backport
    of the C++26 utility, ensuring correct type deduction when no explicit
    type is provided.
    
    Change-Id: Ieb74e8371d641fae79fa867aa816d5b95f541b27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7393726
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1565438}

---
+++ base/types/cxx26_projected_value_t.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_TYPES_CXX26_PROJECTED_VALUE_T_H_
+#define BASE_TYPES_CXX26_PROJECTED_VALUE_T_H_
+
+#include <concepts>
+#include <iterator>
+#include <type_traits>
+
+namespace base {
+
+// C++26's std::projected_value_t.
+//
+// See https://en.cppreference.com/w/cpp/iterator/projected_value_t.html
+template <typename I, typename Proj>
+  requires(std::indirectly_readable<I> &&
+           std::indirectly_regular_unary_invocable<Proj, I>)
+using projected_value_t =
+    std::remove_cvref_t<std::invoke_result_t<Proj&, std::iter_value_t<I>&>>;
+
+}  // namespace base
+
+#endif  // BASE_TYPES_CXX26_PROJECTED_VALUE_T_H_
