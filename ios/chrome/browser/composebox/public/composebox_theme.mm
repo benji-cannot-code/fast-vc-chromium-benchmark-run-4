a561ef9eff692fd9e35460370216bdb09f93a868
Radu Nitescu <radunitescu@google.com>
2026-01-07 08:01:44 -0800

    [iOS][AIM] Adjust button composebox AIM button dark mode background
    Fixed: 473014542
    Change-Id: I58fa0dba545b842f16c54a13de30cd9404f2d699
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7367121
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1565663}

--- ios/chrome/browser/composebox/public/composebox_theme.mm
+++ ios/chrome/browser/composebox/public/composebox_theme.mm
@@ -76,7 +76,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     if (self.isTopInputPlate) {
       return [UIColor colorNamed:kAimComposeboxButtonBackgroundColor];
     } else {
-      return [UIColor colorNamed:kBlueHaloColor];
+      return [[UIColor colorNamed:kDestinationHighlightColor]
+          colorWithAlphaComponent:0.7];
     }
   } else {
     return [UIColor clearColor];
