f6f37c0f265c2ab331975a67b1d7a026e9dc3389
Piotr Kotynia <piotrkotynia@google.com>
2026-01-07 09:35:00 -0800

    Reland "[Android] Add notch to undocked Keyboard Accessory"
    This is a reland of commit c68a10c961abad85e694333227a7aa5dfbf7b550
    
    This reland deletes failing
    testUndockedStyleWithDynamicPositioning_HiddenNotch test casae that was
    expecting code to throw an assert. Due to the framework limitations it
    can't be reliably expected.
    
    Original change's description:
    > [Android] Add notch to undocked Keyboard Accessory
    >
    > This change introduces a visual notch to the undocked keyboard accessory
    > on large-screen Android devices. This notch visually connects the
    > floating accessory pane to the focused input field.
    >
    > The positioning logic in ManualFillingMediator now calculates the
    > NotchPosition. It determines if the notch should be on the top or bottom
    > of the accessory based on whether the bar is positioned below or above
    > the input field, respectively.
    >
    > The change hidden behind
    > AUTOFILL_ANDROID_KEYBOARD_ACCESSORY_DYNAMIC_POSITIONING feature flag is
    > enabled.
    >
    > Demo: http://screencast/cast/NTM5NzQxOTMzOTU0NjYyNHw1MmE3MTg4OC1lZA
    > Bug: 458610649
    > Change-Id: I3e6d7756f3cf7a9732703cf2964ab2c2422c70ef
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7300045
    > Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    > Commit-Queue: Piotr Kotynia <piotrkotynia@google.com>
    > Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1565525}
    
    Bug: 458610649
    Change-Id: I678ad0be7b0791a4d7be2374c448b411c5af8721
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7408774
    Reviewed-by: Julia Sobiech <jsobiech@google.com>
    Commit-Queue: Piotr Kotynia <piotrkotynia@google.com>
    Cr-Commit-Position: refs/heads/main@{#1565734}

--- chrome/android/features/keyboard_accessory/junit/src/org/chromium/chrome/browser/keyboard_accessory/ManualFillingControllerTest.java
+++ chrome/android/features/keyboard_accessory/junit/src/org/chromium/chrome/browser/keyboard_accessory/ManualFillingControllerTest.java
@@ -1548,8 +1548,7 @@ public class ManualFillingControllerTest {
 
         when(mMockResources.getDimensionPixelSize(R.dimen.keyboard_accessory_height_redesign))
                 .thenReturn(barHeight);
-        when(mMockResources.getDimensionPixelSize(
-                        R.dimen.keyboard_accessory_dynamic_positioning_padding))
+        when(mMockResources.getDimensionPixelSize(R.dimen.keyboard_accessory_notch_height))
                 .thenReturn(paddingForNotch);
 
         mController.show(
@@ -1561,8 +1560,9 @@ public class ManualFillingControllerTest {
         KeyboardAccessoryStyle style = mStyleCaptor.getValue();
         assertFalse(style.isDocked());
         assertTrue(style.getMaxWidth() > 0);
+        assertEquals(KeyboardAccessoryStyle.NotchPosition.TOP, style.getNotchPosition());
 
-        assertEquals(bottomBound * density + paddingForNotch, style.getVerticalOffset());
+        assertEquals(bottomBound * density, style.getVerticalOffset());
         assertEquals(leftBound * density, style.getHorizontalOffset());
     }
 
@@ -1587,8 +1587,7 @@ public class ManualFillingControllerTest {
 
         when(mMockResources.getDimensionPixelSize(R.dimen.keyboard_accessory_height_redesign))
                 .thenReturn(barHeight);
-        when(mMockResources.getDimensionPixelSize(
-                        R.dimen.keyboard_accessory_dynamic_positioning_padding))
+        when(mMockResources.getDimensionPixelSize(R.dimen.keyboard_accessory_notch_height))
                 .thenReturn(paddingForNotch);
 
         mController.show(
@@ -1600,6 +1599,7 @@ public class ManualFillingControllerTest {
         KeyboardAccessoryStyle style = mStyleCaptor.getValue();
         assertFalse(style.isDocked());
         assertTrue(style.getMaxWidth() > 0);
+        assertEquals(KeyboardAccessoryStyle.NotchPosition.BOTTOM, style.getNotchPosition());
 
         assertEquals(topBound * density - paddingForNotch - barHeight, style.getVerticalOffset());
         assertEquals(leftBound * density, style.getHorizontalOffset());
