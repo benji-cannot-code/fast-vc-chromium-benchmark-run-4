81e2caa073697fbfb125f135fb06ee136b6a9849
Sophie Chang <sophiechang@chromium.org>
2026-01-06 21:18:57 -0800

    Hide Glic nudge when user interacts with omnibox add context menu
    Bug: 470419164
    Change-Id: Ieddf24e4f7f925d2e418940153b1aded62f40813
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7382294
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1565428}

--- chrome/browser/ui/tabs/glic_nudge_controller.cc
+++ chrome/browser/ui/tabs/glic_nudge_controller.cc
@@ -56,8 +56,10 @@ void GlicNudgeController::UpdateNudgeLabel(
   }
 
   if (activity &&
-      activity == tabs::GlicNudgeActivity::
-                      kNudgeIgnoredOpenedContextualTasksSidePanel &&
+      (activity == tabs::GlicNudgeActivity::
+                       kNudgeIgnoredOpenedContextualTasksSidePanel ||
+       activity == tabs::GlicNudgeActivity::
+                       kNudgeIgnoredOmniboxContextMenuInteraction) &&
       delegate_ && delegate_->GetIsShowingGlicNudge()) {
     delegate_->OnHideGlicNudgeUI();
     OnNudgeActivity(*activity);
@@ -110,6 +112,7 @@ void GlicNudgeController::OnNudgeActivity(GlicNudgeActivity activity) {
     case GlicNudgeActivity::kNudgeIgnoredActiveTabChanged:
     case GlicNudgeActivity::kNudgeIgnoredNavigation:
     case GlicNudgeActivity::kNudgeIgnoredOpenedContextualTasksSidePanel:
+    case GlicNudgeActivity::kNudgeIgnoredOmniboxContextMenuInteraction:
       nudge_activity_callback_.Run(activity);
       nudge_activity_callback_.Reset();
       scoped_window_call_to_action_ptr.reset();
