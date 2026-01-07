bc459f39bd79707084f938bb1c353c497b5a84a7
Michael Wu <mklw@google.com>
2026-01-07 11:50:45 -0800

    Add "No inactive windows" item to instance switcher
    https://screenshot.googleplex.com/5VB9TmV9DL4FeiC
    
    Bug: 428065336
    Change-Id: Iddb64b5f8aed3042d5dfa639bb9addd0ffd97d45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7263887
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Commit-Queue: Michael Wu <mklw@google.com>
    Cr-Commit-Position: refs/heads/main@{#1565829}

--- chrome/browser/ui/android/multiwindow/java/src/org/chromium/chrome/browser/multiwindow/InstanceSwitcherCoordinator.java
+++ chrome/browser/ui/android/multiwindow/java/src/org/chromium/chrome/browser/multiwindow/InstanceSwitcherCoordinator.java
@@ -107,6 +107,7 @@ public class InstanceSwitcherCoordinator {
     private boolean mNewWindowEnabled;
     private boolean mIsInactiveListShowing;
     private @MonotonicNonNull FrameLayout mInstanceListContainer;
+    private @MonotonicNonNull FrameLayout mInactiveListEmptyStateView;
     private @MonotonicNonNull RecyclerView mActiveInstancesList;
     private @MonotonicNonNull RecyclerView mInactiveInstancesList;
     private @MonotonicNonNull DialogListItemDecoration mActiveListItemDecoration;
@@ -168,6 +169,8 @@ public class InstanceSwitcherCoordinator {
                             .inflate(R.layout.instance_switcher_dialog_v2, null);
             mInstanceListContainer = mDialogView.findViewById(R.id.instance_list_container);
             mMaxInfoView = mDialogView.findViewById(R.id.max_instance_info);
+            mInactiveListEmptyStateView =
+                    mDialogView.findViewById(R.id.inactive_list_empty_state_view);
             mNewWindowLayout = mDialogView.findViewById(R.id.new_window);
             TextView newWindowTextView = mNewWindowLayout.findViewById(R.id.new_window_text);
             if (mIsIncognitoWindow) {
@@ -221,6 +224,7 @@ public class InstanceSwitcherCoordinator {
                             unselectItems(/* hideVisibleList= */ false);
                             updateMoreMenu();
                             updatePositiveButtonText();
+                            updateInactiveListEmptyStateVisibility();
                         }
 
                         @Override
@@ -736,6 +740,7 @@ public class InstanceSwitcherCoordinator {
             updateCommandUiState(getTotalInstanceCount() < mMaxInstanceCount);
             updateTabTitle(mActiveModelList.size(), mInactiveModelList.size());
             updateMoreMenu();
+            updateInactiveListEmptyStateVisibility();
         } else {
             removeItemFromModelList(instanceId, mModelList);
             // Update new window item based on instance count after instance removal.
@@ -897,6 +902,12 @@ public class InstanceSwitcherCoordinator {
                 BrowserUiListMenuUtils.getBasicListMenu(mContext, menuItems, delegate);
 
         mDialog.set(ModalDialogProperties.TITLE_MORE_BUTTON_DELEGATE, () -> listMenu);
+    }
+
+    private void updateInactiveListEmptyStateVisibility() {
+        if (mInactiveListEmptyStateView == null) return;
+        boolean showEmptyState = mIsInactiveListShowing && mInactiveModelList.size() == 0;
+        mInactiveListEmptyStateView.setVisibility(showEmptyState ? View.VISIBLE : View.GONE);
     }
 
     private void buildWindowManagerMoreMenu(PropertyModel.Builder builder) {
