f81c0715b902ba63586b984a72e0c44a16f7bc95
Riley Tatum <rtatum@google.com>
2026-01-07 11:47:59 -0800

    [ntp-next] Show action chip background when GM2 or CWS theme
    Screenshots:
    CWS theme light mode: http://screenshot/9jRjNiPdGbeqJ3V
    CWS theme dark mode: http://screenshot/BRzb4Cz53C9YbtR
    GM3 light mode: http://screenshot/9uHbCWwU65n9fpf
    GM3 dark mode: http://screenshot/7CVxQkBrJP9mHQ3
    background image light mode: http://screenshot/5WQoc9ZgHi3efrx
    background image dark mode: http://screenshot/sjFTA3wqfvrmQiQ
    
    Bug: 473811486
    Change-Id: Icdbd272cdf4c8da2d13e40cd649990bd2e7187fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7401852
    Reviewed-by: Omar Flores <omarflores@google.com>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1565826}

--- chrome/test/data/webui/new_tab_page/test_support.ts
+++ chrome/test/data/webui/new_tab_page/test_support.ts
@@ -70,6 +70,7 @@ export function createBackgroundImage(url: string): BackgroundImage {
 export function createTheme({
   isDark = false,
   isBaseline = true,
+  isGm3 = true,
   backgroundColor = {
     value: 0xffff0000,
   },
@@ -90,6 +91,7 @@ export function createTheme({
     logoColor: null,
     isBaseline: isBaseline,
     isDark,
+    isGm3,
     mostVisited: mostVisited,
     textColor: {value: 0xff0000ff},
     isCustomBackground: true,
