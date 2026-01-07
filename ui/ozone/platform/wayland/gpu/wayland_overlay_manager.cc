47ce42058e7b377f57b28c16683d7820127e01d4
Colin Blundell <blundell@chromium.org>
2026-01-07 00:13:09 -0800

    [//ui] Have WaylandBufferManagerGPU::SupportsFormat() take in SIFormat
    Step toward porting this class away from talking BufferFormat at all.
    
    Bug: 356649879
    Change-Id: Icf23279773b3f135d253112cca64f54bfebab497
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7380102
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1565460}

--- ui/ozone/platform/wayland/gpu/wayland_overlay_manager.cc
+++ ui/ozone/platform/wayland/gpu/wayland_overlay_manager.cc
@@ -63,8 +63,7 @@ void WaylandOverlayManager::CheckOverlaySupport(
 bool WaylandOverlayManager::CanHandleCandidate(
     const OverlaySurfaceCandidate& candidate,
     gfx::AcceleratedWidget widget) const {
-  if (!manager_gpu_->SupportsFormat(
-          viz::SharedImageFormatToBufferFormat(candidate.format))) {
+  if (!manager_gpu_->SupportsFormat(candidate.format)) {
     return false;
   }
 
