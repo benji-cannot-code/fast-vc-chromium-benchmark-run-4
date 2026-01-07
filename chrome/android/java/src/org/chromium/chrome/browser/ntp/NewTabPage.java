1f81e24921b20db78bed77d1e1ae0c316ced2950
Xi Han <hanxi@google.com>
2026-01-07 06:33:13 -0800

    [NtpCustomization] Clean up New tab cards on menu.
    This is the first patch of cleaning feature flag
    NewTabPageCustomization. It removes code of New Tab Page Cards menu
    settings.
    
    Bug: 376238770
    Change-Id: I8a6a8ef676eded65c3da27382836cba8c833d0e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7398611
    Reviewed-by: Xinyi Ji <xinyiji@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1565602}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPage.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPage.java
@@ -64,7 +64,6 @@ import org.chromium.chrome.browser.feed.FeedSurfaceProvider.RestoringState;
 import org.chromium.chrome.browser.feed.FeedSwipeRefreshLayout;
 import org.chromium.chrome.browser.feed.NtpFeedSurfaceLifecycleManager;
 import org.chromium.chrome.browser.feed.componentinterfaces.SurfaceCoordinator;
-import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.lifecycle.ActivityLifecycleDispatcher;
 import org.chromium.chrome.browser.lifecycle.LifecycleObserver;
 import org.chromium.chrome.browser.lifecycle.PauseResumeWithNativeObserver;
@@ -1587,17 +1586,13 @@ public class NewTabPage
 
     @Override
     public void customizeSettings() {
-        if (ChromeFeatureList.isEnabled(ChromeFeatureList.NEW_TAB_PAGE_CUSTOMIZATION)) {
-            NtpCustomizationCoordinatorFactory.getInstance()
-                    .create(
-                            mContext,
-                            mBottomSheetController,
-                            mTab::getProfile,
-                            NtpCustomizationCoordinator.BottomSheetType.NTP_CARDS)
-                    .showBottomSheet();
-        } else {
-            HomeModulesConfigManager.getInstance().onMenuClick(mContext);
-        }
+        NtpCustomizationCoordinatorFactory.getInstance()
+                .create(
+                        mContext,
+                        mBottomSheetController,
+                        mTab::getProfile,
+                        NtpCustomizationCoordinator.BottomSheetType.NTP_CARDS)
+                .showBottomSheet();
     }
 
     @Override
