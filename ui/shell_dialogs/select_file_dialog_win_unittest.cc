13b4cd389215769a95b57fc96409cd86aa2e4943
David Bienvenu <davidbienvenu@chromium.org>
2026-01-07 15:37:39 -0800

    Fix UNSAFE_TODOs in select_file_dialog_win tests
    Use modern iterators to fix UNSAFE_TODOs.
    
    Bug: 42271176
    Change-Id: I456eeea9a1d3a84154e10316538eb29e165d7eca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7395645
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1565973}

--- ui/shell_dialogs/select_file_dialog_win_unittest.cc
+++ ui/shell_dialogs/select_file_dialog_win_unittest.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "base/compiler_specific.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
@@ -222,10 +221,8 @@ TEST_F(SelectFileDialogWinTest, CancelAllDialogs) {
           ui::SelectFileDialog::SELECT_OPEN_MULTI_FILE, kSelectFileDefaultTitle,
       }};
 
-  for (size_t i = 0; i < std::size(kTestCases); ++i) {
-    SCOPED_TRACE(base::StringPrintf("i=%zu", i));
-
-    const auto& test_case = UNSAFE_TODO(kTestCases[i]);
+  for (size_t i = 0; const auto& test_case : kTestCases) {
+    SCOPED_TRACE(base::StringPrintf("i=%zu", i++));
 
     scoped_refptr<ui::SelectFileDialog> dialog =
         ui::SelectFileDialog::Create(this, nullptr);
