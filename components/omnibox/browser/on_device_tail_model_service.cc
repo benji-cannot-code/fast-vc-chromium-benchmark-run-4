af3816fbeb57262a82afe6d02a437a7f33b03838
Patrick Monette <pmonette@chromium.org>
2026-01-07 15:44:52 -0800

    Remove usage of MemoryPressureMonitor in OnDeviceTailModelService
    OnDeviceTailModelService already implements MemoryPressureListener,
    which gives access to the current memory pressure level.
    
    Bug: 465068849
    Change-Id: Ide008fe27e0f11b5f909687533901cc3b78fa557
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7410663
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1565981}

--- components/omnibox/browser/on_device_tail_model_service.cc
+++ components/omnibox/browser/on_device_tail_model_service.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/logging.h"
-#include "base/memory/memory_pressure_monitor.h"
 #include "base/metrics/field_trial_params.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
@@ -197,12 +196,8 @@ void OnDeviceTailModelService::GetPredictionsForInput(
     const OnDeviceTailModelExecutor::ModelInput& input,
     ResultCallback result_callback) {
   if (model_task_runner_) {
-    base::MemoryPressureMonitor* monitor = base::MemoryPressureMonitor::Get();
     // Do not call the model if memory pressure level is too high.
-    if (!monitor ||
-        monitor->GetCurrentPressureLevel(
-            base::MemoryPressureMonitorTag::kOnDeviceTailModelService) !=
-            base::MEMORY_PRESSURE_LEVEL_CRITICAL) {
+    if (memory_pressure_level() != base::MEMORY_PRESSURE_LEVEL_CRITICAL) {
       model_task_runner_->PostTaskAndReplyWithResult(
           FROM_HERE,
           base::BindOnce(&RunTailModelExecutor, tail_model_executor_.get(),
