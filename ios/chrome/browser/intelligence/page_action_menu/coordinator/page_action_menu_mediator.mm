f8b935fa7c540a9b4c41f93c118b7b960608fd82
Quentin Pubert <qpubert@google.com>
2026-01-07 08:20:42 -0800

    [iOS] Support Translate in page action menu when Reader mode active
    This CL ensures that the page action menu can find the correct Translate
    client when Reader mode is active i.e. the client tied to the Reader
    mode WebState.
    
    Bug: 473999513
    Change-Id: Ia801a27520460ecd6f49b06ce6d15662460f35d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7408770
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Auto-Submit: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1565678}

--- ios/chrome/browser/intelligence/page_action_menu/coordinator/page_action_menu_mediator.mm
+++ ios/chrome/browser/intelligence/page_action_menu/coordinator/page_action_menu_mediator.mm
@@ -156,8 +156,7 @@ const CGFloat kFeatureRowIconSize = 20;
 
   switch (featureType) {
     case PageActionMenuTranslate: {
-      ChromeIOSTranslateClient* translateClient =
-          ChromeIOSTranslateClient::FromWebState(_webState);
+      ChromeIOSTranslateClient* translateClient = [self findTranslateClient];
       return IsTranslateActive(translateClient);
     }
     case PageActionMenuCameraPermission: {
@@ -217,8 +216,7 @@ const CGFloat kFeatureRowIconSize = 20;
     return nil;
   }
 
-  ChromeIOSTranslateClient* translateClient =
-      ChromeIOSTranslateClient::FromWebState(_webState);
+  ChromeIOSTranslateClient* translateClient = [self findTranslateClient];
 
   if (!IsTranslateActive(translateClient) ||
       !translateClient->GetTranslateManager()) {
@@ -448,8 +446,7 @@ const CGFloat kFeatureRowIconSize = 20;
     return;
   }
 
-  ChromeIOSTranslateClient* translateClient =
-      ChromeIOSTranslateClient::FromWebState(_webState);
+  ChromeIOSTranslateClient* translateClient = [self findTranslateClient];
   if (!translateClient || !translateClient->GetTranslateManager()) {
     return;
   }
@@ -536,8 +533,7 @@ std::string GetTargetLanguageCode(ChromeIOSTranslateClient* translate_client) {
     return;
   }
 
-  ChromeIOSTranslateClient* translateClient =
-      ChromeIOSTranslateClient::FromWebState(webState);
+  ChromeIOSTranslateClient* translateClient = [self findTranslateClient];
   if (!translateClient || !translateClient->GetTranslateManager()) {
     return;
   }
@@ -598,6 +594,21 @@ std::string GetTargetLanguageCode(ChromeIOSTranslateClient* translate_client) {
       base::BindOnce(^(NSArray<NSString*>* suggestions){
           // No-op.
       }));
+}
+
+// Finds the translate client depending on whether Reader mode is active.
+- (ChromeIOSTranslateClient*)findTranslateClient {
+  web::WebState* targetWebState = _webState;
+  ReaderModeTabHelper* readerModeTabHelper =
+      ReaderModeTabHelper::FromWebState(_webState);
+  if (readerModeTabHelper) {
+    web::WebState* readerModeWebState =
+        readerModeTabHelper->GetReaderModeWebState();
+    if (readerModeWebState) {
+      targetWebState = readerModeWebState;
+    }
+  }
+  return ChromeIOSTranslateClient::FromWebState(targetWebState);
 }
 
 // Finds the translate infobar.
