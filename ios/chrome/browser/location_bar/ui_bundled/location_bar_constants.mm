05242fdd9b43a1c7b434aacd0bfa9a2a0cd410fc
Louis Romero <lpromero@google.com>
2026-01-07 07:25:35 -0800

    Revert "[iOS] Add hidden label to match OmniboxText"
    This reverts commit abbb68f9112c278f103f63e661b73be58b71ea1d.
    The hidden label is no longer necessary, as failing tests have been
    updated to no longer rely on it.
    
    Fixed: 465030009
    Change-Id: Ieb70db99603515d3242b4218547ea7916bdba22b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7369145
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1565638}

--- ios/chrome/browser/location_bar/ui_bundled/location_bar_constants.mm
+++ ios/chrome/browser/location_bar/ui_bundled/location_bar_constants.mm
@@ -16,6 +16,3 @@ NSString* const kOmniboxShareButtonIdentifier =
 
 NSString* const kOmniboxVoiceSearchButtonIdentifier =
     @"kOmniboxVoiceSearchButtonIdentifier";
-
-NSString* const kOmniboxTextHiddenLabelIdentifier =
-    @"kOmniboxTextHiddenLabelidentifier";
