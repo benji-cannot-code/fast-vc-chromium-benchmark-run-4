f85566866bc077cd4ae9a2f8f456631840b33612
Igor Kraskevich <kraskevich@google.com>
2026-01-07 07:17:06 -0800

    Remove Compositing.Display.AdpfHintUs histogram
    OBSOLETE_HISTOGRAMS=No longer useful - we ship ADPF only on Pixels + the refresh rate/target duration is unknown
    
    Bug: 433258766
    Change-Id: I4bb97c41556ab39435c25840e0da146e12dcb36e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7367127
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Igor Kraskevich <kraskevich@google.com>
    Cr-Commit-Position: refs/heads/main@{#1565635}

--- components/viz/service/display/display_scheduler.cc
+++ components/viz/service/display/display_scheduler.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/auto_reset.h"
 #include "base/feature_list.h"
 #include "base/memory/raw_ptr.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/notimplemented.h"
 #include "base/task/delay_policy.h"
 #include "base/task/single_thread_task_runner.h"
@@ -251,9 +250,6 @@ void DisplayScheduler::ReportFrameTime(
     HintSession::BoostType boost_type) {
   MaybeCreateHintSessions(std::move(animation_thread_ids),
                           std::move(renderer_main_thread_ids));
-  UMA_HISTOGRAM_CUSTOM_MICROSECONDS_TIMES("Compositing.Display.AdpfHintUs",
-                                          frame_time, base::Microseconds(1),
-                                          base::Milliseconds(50), 50);
   for (const auto& state : session_states_) {
     if (state.hint_session) {
       state.hint_session->ReportCpuCompletionTime(frame_time, draw_start,
