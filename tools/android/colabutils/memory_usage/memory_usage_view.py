7d5ed131638a34d2e27c4905715ff8f07f760cd3
Egor Pasko <pasko@chromium.org>
2026-01-07 07:25:49 -0800

    colabutils: Create memory_usage subdirectory
    Create t/a/colabutils/memory_usage and move the files related to memory
    usage visualization there.
    
    Also make the memory_usage/__init__.py to export the main class for
    less typing in Colab notebooks.
    
    Bug: 473768497
    Change-Id: I145351a19ffbf6243ff9e9e9999a037c752fbaa6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7380993
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Reviewed-by: Anand Ravi <anandrv@google.com>
    Cr-Commit-Position: refs/heads/main@{#1565639}

--- tools/android/colabutils/memory_usage/memory_usage_view.py
+++ tools/android/colabutils/memory_usage/memory_usage_view.py
@@ -4,9 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 """Utilities for viewing and diffing Sum Trees of memory usage."""
 
-# TODO(crbug.com/73768497): Move this file and other files related to memory
-# usage into a separate subdirectory.
-
 import copy
 import jinja2
 import json
@@ -30,7 +27,7 @@ except ImportError:
 from . import demangler
 
 _MEMORY_USAGE_DIR = pathlib.Path(__file__).resolve().parent
-_SRC_PATH = _MEMORY_USAGE_DIR.parents[2]
+_SRC_PATH = _MEMORY_USAGE_DIR.parents[3]
 sys.path.append(str(_SRC_PATH / 'third_party/perfetto/python'))
 from perfetto.trace_processor import TraceProcessor
 
