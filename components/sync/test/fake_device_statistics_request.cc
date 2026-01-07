314781db49deead48e72c7b81ad2e6614ea5e02c
Marc Treib <treib@chromium.org>
2026-01-07 07:51:43 -0800

    Multi-account device metrics: DeviceStatisticsTracker, part 1
    This adds a new class DeviceStatisticsTracker which fetches information
    about the syncing devices of all available accounts. In the future, it
    will record metrics about this; for this CL, it only records basic
    success/failure metrics about the individual requests.
    
    Bug: 465716865
    Change-Id: I174103ff5d6764e3d2d588fe203e30fa6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7241387
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1565653}

---
+++ components/sync/test/fake_device_statistics_request.cc
@@ -0,0 +1,50 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/sync/test/fake_device_statistics_request.h"
+
+#include "base/check.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace syncer {
+
+FakeDeviceStatisticsRequest::FakeDeviceStatisticsRequest() = default;
+FakeDeviceStatisticsRequest::~FakeDeviceStatisticsRequest() = default;
+
+void FakeDeviceStatisticsRequest::Start(base::OnceClosure callback) {
+  CHECK_EQ(state_, State::kNotStarted);
+  state_ = State::kInProgress;
+  callback_ = std::move(callback);
+}
+
+DeviceStatisticsRequest::State FakeDeviceStatisticsRequest::GetState() const {
+  return state_;
+}
+
+const std::vector<sync_pb::DeviceInfoSpecifics>&
+FakeDeviceStatisticsRequest::GetResults() const {
+  CHECK_EQ(state_, State::kComplete);
+  return results_;
+}
+
+void FakeDeviceStatisticsRequest::SimulateSuccess(
+    std::vector<sync_pb::DeviceInfoSpecifics> results) {
+  ASSERT_EQ(state_, State::kInProgress);
+  state_ = State::kComplete;
+  results_ = std::move(results);
+  if (callback_) {
+    std::move(callback_).Run();
+  }
+}
+
+void FakeDeviceStatisticsRequest::SimulateFailure() {
+  ASSERT_EQ(state_, State::kInProgress);
+  state_ = State::kFailed;
+  if (callback_) {
+    std::move(callback_).Run();
+  }
+}
+
+}  // namespace syncer
