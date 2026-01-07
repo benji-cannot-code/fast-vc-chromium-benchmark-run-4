30e6590f5d90e4975b00a1a4502a93fc4b7c915e
Sam Maier <smaier@google.com>
2026-01-07 12:59:56 -0800

    Fix memory leak in feed
    The stream is destroyed if mSectionHeaderModel is nonnull, and it should
    likewise be destroyed in the mStreamHolder case.
    
    Change-Id: I08efc7bad42733ba582ce426279e7c866a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7411384
    Auto-Submit: Sam Maier <smaier@chromium.org>
    Commit-Queue: Jian Li <jianli@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Reviewed-by: Jian Li <jianli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1565880}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceMediator.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceMediator.java
@@ -1064,6 +1064,7 @@ public class FeedSurfaceMediator
             if (mStreamHolder != null) {
                 assumeNonNull(mStreamContentChangedListener);
                 mStreamHolder.removeOnContentChangedListener(mStreamContentChangedListener);
+                mStreamHolder.destroy();
                 mStreamHolder = null;
             }
         }
