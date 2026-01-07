a5aea63fab17eaa95836ce559ae4e0f0b7c19d5f
Noah Rose Ledesma <noahrose@google.com>
2026-01-07 10:56:36 -0800

    Updater UI: Show timezone when formatting dates
    Bug: 473621694
    Change-Id: I62462b86bea5f6b70e419630027f60736a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7402945
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: Noah Rose Ledesma <noahrose@google.com>
    Cr-Commit-Position: refs/heads/main@{#1565786}

--- chrome/browser/resources/updater/event_list/event_list_item.ts
+++ chrome/browser/resources/updater/event_list/event_list_item.ts
@@ -150,6 +150,7 @@ export class EventListItemElement extends CrLitElement {
   private computeFormattedDate(): string|undefined {
     return this.eventDate ? new Intl
                                 .DateTimeFormat(undefined, {
+                                  timeZoneName: 'short',
                                   month: 'numeric',
                                   day: 'numeric',
                                   hour: 'numeric',
