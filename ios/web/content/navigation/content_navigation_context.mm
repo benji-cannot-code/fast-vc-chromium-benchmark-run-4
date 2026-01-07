bc14623681464fbf72bb3eba35a2cd0110d5a4f5
xiaobincui <xiaobincui@microsoft.com>
2026-01-07 07:04:41 -0800

    [ios blink] Address the issue related to net error conversion
    Include the missing conversion for ERR_CERT_INVALID.
    Add a default handler for any unknown errors, since it's not possible to
    cover every potential error.
    
    Bug: 40254930
    Change-Id: I8db2cce4fe76e044bd30bebf2c2186fe5343624b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7300028
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1565628}

--- ios/web/content/navigation/content_navigation_context.mm
+++ ios/web/content/navigation/content_navigation_context.mm
@@ -5,15 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/web/content/navigation/content_navigation_context.h"
 
-#import <optional>
-
 #import "base/notreached.h"
 #import "content/public/browser/navigation_handle.h"
 #import "net/base/net_errors.h"
 
 namespace web {
 
-std::optional<NSInteger> GetIOSErrorForNetError(int net_error_code) {
+NSInteger GetIOSErrorForNetError(int net_error_code) {
   switch (net_error_code) {
     case net::ERR_ACCESS_DENIED:
       // TODO(crbug.com/40257932): no analog for
@@ -79,6 +77,7 @@ std::optional<NSInteger> GetIOSErrorForNetError(int net_error_code) {
       // kCFURLErrorServerCertificateNotYetValid.
       return kCFURLErrorServerCertificateHasBadDate;
     case net::ERR_CERT_AUTHORITY_INVALID:
+    case net::ERR_CERT_INVALID:
       // TODO(crbug.com/40257932): no analog for
       // kCFURLErrorServerCertificateHasUnknownRoot.
       return kCFURLErrorServerCertificateUntrusted;
@@ -87,9 +86,12 @@ std::optional<NSInteger> GetIOSErrorForNetError(int net_error_code) {
     case net::ERR_SSL_CLIENT_AUTH_CERT_NEEDED:
       return kCFURLErrorClientCertificateRequired;
     default:
-      break;
+      // There are 200+ net error codes but only ~40 iOS error codes.
+      // Following Android WebView's pattern (ErrorCodeConversionHelper.java),
+      // we return a generic error for unmapped codes rather than maintaining
+      // an exhaustive mapping.
+      return kCFURLErrorUnknown;
   }
-  return std::nullopt;
 }
 
 NavigationContext* ContentNavigationContext::GetOrCreate(
@@ -147,10 +149,9 @@ NSError* ContentNavigationContext::GetError() const {
     error_ = nil;
   } else {
     auto ios_error_code = GetIOSErrorForNetError(net_error_code);
-    CHECK(ios_error_code);
     error_ = [[NSError alloc]
         initWithDomain:NSURLErrorDomain
-                  code:*ios_error_code
+                  code:ios_error_code
               userInfo:@{
                 @"url" : [NSString stringWithUTF8String:GetUrl().spec().c_str()]
               }];
