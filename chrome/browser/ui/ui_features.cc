499ff2085d7eeb6509324bf5c6cd5d48295f8d10
David Bienvenu <davidbienvenu@chromium.org>
2026-01-07 13:08:51 -0800

    Enable kOfferPinToTaskbarInFirstRunExperience by default
    In preparation for launching this to 50% of stable, enable on ToT.
    
    Bug: 457525852
    Change-Id: Iaafdf6554de36aca39371d55cb83e6e046f3a6f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7409526
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1565887}

--- chrome/browser/ui/ui_features.cc
+++ chrome/browser/ui/ui_features.cc
@@ -50,7 +50,7 @@ BASE_FEATURE(kOfferPinToTaskbarWhenSettingToDefault,
              "OfferPinToTaskbarWhenSettingDefault",
              base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kOfferPinToTaskbarInFirstRunExperience,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kOfferPinToTaskbarInSettings, base::FEATURE_DISABLED_BY_DEFAULT);
 #endif
 
