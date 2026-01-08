7bd7824aa7d4688d56999d9c4c369af75a5bf8ab
Elly <ellyjones@chromium.org>
2026-01-08 12:20:24 -0800

    interest-group: use base::RandomChoice()
    Instead of foo[base::RandInt(0, foo.size() - 1)] - the behavior is
    identical but it's a little more say-what-you-mean.
    
    Bug: 40283703
    Change-Id: I03652be2049df9024ec0de91529de8af9e80cb76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7416084
    Auto-Submit: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: mmenke <mmenke@chromium.org>
    Reviewed-by: mmenke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566485}

--- content/browser/interest_group/bidding_and_auction_server_key_fetcher.cc
+++ content/browser/interest_group/bidding_and_auction_server_key_fetcher.cc
@@ -190,9 +190,9 @@ std::optional<BiddingAndAuctionServerKey> BiddingAndAuctionKeySet::GetRandomKey(
     if (it == origin_scoped_keys_.end() || it->second.empty()) {
       return std::nullopt;
     }
-    return it->second[base::RandInt(0, it->second.size() - 1)];
+    return base::RandomChoice(it->second);
   } else if (!keys_.empty()) {
-    return keys_[base::RandInt(0, keys_.size() - 1)];
+    return base::RandomChoice(keys_);
   }
   return std::nullopt;
 }
