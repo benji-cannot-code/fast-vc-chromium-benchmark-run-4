60c61af75d61ad10171bc55908fba31c266fa342
Colin Blundell <blundell@chromium.org>
2026-01-08 00:21:52 -0800

    [Viz] Update comments in frame_sink_video_capturer.cc
    Bug: 404905709
    Change-Id: Ic81bb178345d738f07137c80c561595e88b2de30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7380028
    Reviewed-by: Mingjing Zhang <mjzhang@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566134}

--- components/viz/service/frame_sinks/video_capture/frame_sink_video_capturer_impl.cc
+++ components/viz/service/frame_sinks/video_capture/frame_sink_video_capturer_impl.cc
@@ -1409,7 +1409,7 @@ void FrameSinkVideoCapturerImpl::DidCopyFrame(
       const VideoCaptureOverlay::CapturedFrameProperties frame_properties{
           frame_capture.region_properties, content_rect, frame->format()};
 
-      // For GMB-backed video frames, overlays were already applied by
+      // For MappableSI-backed video frames, overlays were already applied by
       // CopyOutputRequest API. For in-memory frames, apply overlays here:
       auto overlay_renderer = VideoCaptureOverlay::MakeCombinedRenderer(
           GetOverlaysInOrder(), frame_properties);
@@ -1427,8 +1427,8 @@ void FrameSinkVideoCapturerImpl::DidCopyFrame(
     if (frame->visible_rect() != result_rect &&
         !frame->HasMappableSharedImage()) {
       // If there are parts of the frame that are visible but we have not wrote
-      // into them, letterbox them. This is not needed for GMB-backed frames as
-      // the letterboxing happens on GPU.
+      // into them, letterbox them. This is not needed for MappableSI-backed
+      // frames as the letterboxing happens on GPU.
       media::LetterboxVideoFrame(frame.get(), result_rect);
     }
 
