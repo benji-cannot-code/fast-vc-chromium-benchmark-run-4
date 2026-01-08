ab859edea1dbb829dfd767449273219676e12e0c
Tomasz Wiszkowski <ender@google.com>
2026-01-08 15:07:15 -0800

    Fix layering in FuseboxAttachmentModelList.
    This change makes the FAML manage the ModelList (UI) and the
    RecyclerViewAdapter, rather than inherit from the ModelList.
    
    Change-Id: I3389dc9f5f4fea04e0e62589be71340c60681464
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7411417
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1566580}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxCoordinator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxCoordinator.java
@@ -14,7 +14,6 @@ import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 
 import androidx.annotation.IntDef;
-import androidx.annotation.NonNull;
 import androidx.annotation.VisibleForTesting;
 import androidx.appcompat.content.res.AppCompatResources;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -147,7 +146,7 @@ public class FuseboxCoordinator implements UrlFocusChangeListener, TemplateUrlSe
         var popup = new FuseboxPopup(mContext, popupWindowBuilder.build(), popupView);
         mViewHolder = new FuseboxViewHolder(parent, popup);
 
-        var adapter = new FuseboxAttachmentRecyclerViewAdapter(mModelList);
+        var adapter = mModelList.getAdapter();
         mViewHolder.attachmentsView.setAdapter(adapter);
 
         mModel =
@@ -404,7 +403,7 @@ public class FuseboxCoordinator implements UrlFocusChangeListener, TemplateUrlSe
         }
 
         @Override
-        public void onConfigurationChanged(@NonNull Configuration configuration) {
+        public void onConfigurationChanged(Configuration configuration) {
             updateRect();
         }
 
