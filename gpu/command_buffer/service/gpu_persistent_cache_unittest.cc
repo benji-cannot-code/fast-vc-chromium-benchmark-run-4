9a97ca9cb74f903a005efbba15b500796a629d78
Greg Thompson <grt@chromium.org>
2026-01-08 10:34:21 -0800

    IWYU fixes in command buffer service tests
    Bug: none
    Change-Id: I016fa2dc7f5f34cdda8d7a461e8c004160a64e00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7413502
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Commit-Queue: Geoff Lang <geofflang@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566414}

--- gpu/command_buffer/service/gpu_persistent_cache_unittest.cc
+++ gpu/command_buffer/service/gpu_persistent_cache_unittest.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/persistent_cache/backend_storage.h"
 #include "components/persistent_cache/backend_type.h"
 #include "components/persistent_cache/pending_backend.h"
-#include "components/persistent_cache/sqlite/vfs/sandboxed_file.h"
 #include "gpu/command_buffer/service/memory_cache.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
