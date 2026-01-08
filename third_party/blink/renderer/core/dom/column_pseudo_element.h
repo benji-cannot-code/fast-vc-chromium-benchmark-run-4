3fc0556b18b20f9dd698e071ae73a4fc59b2ca21
Morten Stenshorne <mstensho@chromium.org>
2026-01-08 01:34:25 -0800

    [carousel] Avoid nullptr ::column computed style.
    Make sure that ::column pseudo-elements always store computed style.
    Checking display type here was wrong, since ::column pseudo-elements
    never create layout boxes. If a `::column` became `display:none`, its
    computed style would be set to null, which in turn would crash
    somewhere. In the crash test included it would crash in
    Element::GetOrCreateColumnPseudoElementIfNeeded() because of this.
    
    This is a speculative fix for http://crbug.com/470512590
    
    Big thanks to futhark for help with analyzing and advice!
    
    Bug: 470512590
    Change-Id: I7b62ab9c77dd6a7529415a7f2e721696d84ab25a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7362160
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Daniil Sakhapov <sakhapov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566152}

--- third_party/blink/renderer/core/dom/column_pseudo_element.h
+++ third_party/blink/renderer/core/dom/column_pseudo_element.h
@@ -34,6 +34,7 @@ class CORE_EXPORT ColumnPseudoElement : public IndexedPseudoElement {
 
   void AttachLayoutTree(AttachContext&) final;
   void DetachLayoutTree(bool performing_reattach) final;
+  bool LayoutObjectIsNeeded(const DisplayStyle&) const final;
 
  private:
   PhysicalRect column_rect_;
