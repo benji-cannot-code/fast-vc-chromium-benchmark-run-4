a9f2bff8e4b4035e87a3a4ff1d27540aa1008491
Kent Tamura <tkent@chromium.org>
2026-01-07 20:57:20 -0800

    Use IgnoringAsciiCaseHashTraits in hyphenation_minikin.cc
    It should be faster than CaseFoldingHashTraits.
    
    Bug: 40476285
    Change-Id: I905967144090f6bb5d75c9c4ec23a69a08c88e18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7396649
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566083}

--- third_party/blink/renderer/platform/text/hyphenation/hyphenation_minikin.cc
+++ third_party/blink/renderer/platform/text/hyphenation/hyphenation_minikin.cc
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/text/character.h"
 #include "third_party/blink/renderer/platform/text/hyphenation/hyphenator_aosp.h"
 #include "third_party/blink/renderer/platform/text/layout_locale.h"
-#include "third_party/blink/renderer/platform/wtf/text/case_folding_hash.h"
+#include "third_party/blink/renderer/platform/wtf/text/ignoring_ascii_case_hash.h"
 #include "third_party/blink/renderer/platform/wtf/text/utf16.h"
 
 namespace blink {
@@ -195,7 +195,7 @@ struct HyphenatorLocaleData {
 
 using LocaleMap = HashMap<AtomicString,
                           const HyphenatorLocaleData*,
-                          CaseFoldingHashTraits<AtomicString>>;
+                          IgnoringAsciiCaseHashTraits<AtomicString>>;
 
 static LocaleMap CreateLocaleFallbackMap() {
   // This data is from CLDR, compiled by AOSP.
