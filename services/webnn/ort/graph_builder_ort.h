ab51e5ea08fa39e42434f363ed1e6e4df88c2186
mingming <mingming1.xu@intel.com>
2026-01-08 15:21:04 -0800

    WebNN: Reject batched MatMul with large K dimension size on OV NPU
    The OpenVINO NPU EP will take a very long time to compile models
    with batched MatMul operations whose K dimension size is too large.
    
    This CL rejects such MatMul operations to prevent WebNN from
    becoming unresponsive.
    
    Bug: 467442135
    Cq-Include-Trybots: luci.chromium.try:win11-blink-rel
    Change-Id: I35797f771829a2ff2908a61242a6b287e264893a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7245170
    Reviewed-by: Hu, Ningxin <ningxin.hu@intel.com>
    Commit-Queue: Hu, Ningxin <ningxin.hu@intel.com>
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566584}

--- services/webnn/ort/graph_builder_ort.h
+++ services/webnn/ort/graph_builder_ort.h
@@ -59,11 +59,14 @@ class GraphBuilderOrt {
   // Factory method that creates a `GraphBuilderOrt`, builds the graph and
   // returns `ModelEditor::ModelInfo` which contains the model itself and the
   // external data (weights).
-  static std::unique_ptr<ModelEditor::ModelInfo> CreateAndBuild(
+  static base::expected<std::unique_ptr<ModelEditor::ModelInfo>,
+                        mojom::ErrorPtr>
+  CreateAndBuild(
       const mojom::GraphInfo& graph_info,
       ContextProperties context_properties,
       base::flat_map<OperandId, std::unique_ptr<WebNNConstantOperand>>
-          constant_operands);
+          constant_operands,
+      std::optional<uint32_t> batched_matmul_k_dimension_limit);
 
   GraphBuilderOrt(const GraphBuilderOrt&) = delete;
   GraphBuilderOrt& operator=(const GraphBuilderOrt&) = delete;
@@ -75,7 +78,8 @@ class GraphBuilderOrt {
       const mojom::GraphInfo& graph_info,
       ContextProperties context_properties,
       base::flat_map<OperandId, std::unique_ptr<WebNNConstantOperand>>
-          constant_operands);
+          constant_operands,
+      std::optional<uint32_t> batched_matmul_k_dimension_limit);
 
   const mojom::Operand& GetOperand(OperandId operand_id) const;
 
@@ -271,7 +275,8 @@ class GraphBuilderOrt {
     requires(std::is_same_v<LstmType, mojom::Lstm> ||
              std::is_same_v<LstmType, mojom::LstmCell>)
   void AddLstmOperation(const LstmType& lstm);
-  void AddMatMulOperation(const mojom::Matmul& matmul);
+  base::expected<void, mojom::ErrorPtr> AddMatMulOperation(
+      const mojom::Matmul& matmul);
   void AddPadOperation(const mojom::Pad& pad);
   void AddPool2dOperation(const mojom::Pool2d& pool2d);
   void AddPreluOperation(const mojom::Prelu& prelu);
@@ -290,7 +295,8 @@ class GraphBuilderOrt {
   void AddTriangularOperation(const mojom::Triangular& triangular);
   void AddWhereOperation(const mojom::Where& where);
 
-  std::unique_ptr<ModelEditor::ModelInfo> BuildModel();
+  base::expected<std::unique_ptr<ModelEditor::ModelInfo>, mojom::ErrorPtr>
+  BuildModel();
 
   // An increasing id starting from 0, used for generating unique names for each
   // operand.
@@ -309,6 +315,8 @@ class GraphBuilderOrt {
       constant_operands_;
 
   const ContextProperties context_properties_;
+
+  std::optional<uint32_t> batched_matmul_k_dimension_limit_;
 
   ModelEditor model_editor_;
 };
