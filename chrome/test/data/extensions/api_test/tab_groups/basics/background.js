c5e659ec3657b310e92648383dfb90c476232e36
James Cook <jamescook@chromium.org>
2026-01-08 15:01:56 -0800

    extensions: Port tab group API unit tests to browser tests, part 7
    The tab groups API unit test suite provides good coverage, but uses
    concepts like Browser and BrowserWindow that are not supposed to be
    used anymore in unit tests. (See chrome browser design principles in
    //docs or the comment in this browser test file.) Converting to browser
    test both fixes this design issue and sets up the tests for future
    conversion to run on desktop Android.
    
    This CL cut/pastes over all the remaining tests and deletes the unit
    test file.
    
    Bug: 405219902
    Change-Id: Ibb82b7607c1dc69b20aae80676a59a2cb6c37905
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7415049
    Reviewed-by: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566576}

--- chrome/test/data/extensions/api_test/tab_groups/basics/background.js
+++ chrome/test/data/extensions/api_test/tab_groups/basics/background.js
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Basic browser tests for the tabGroups API. Most API behavior is tested in
-// tab_groups_api_unittest.cc, this just provides end-to-end coverage.
+// Basic API tests for the tabGroups API. Most API behavior is tested in
+// tab_groups_api_browsertest.cc, this just provides end-to-end coverage.
 chrome.test.runTests([
   function testQuerySucceeds() {
     chrome.tabs.create({}, (tab) => {
