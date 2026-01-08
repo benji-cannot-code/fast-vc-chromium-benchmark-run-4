3fa6ba23f6b655770d64839eff79b5f4d767365f
Ramin Halavati <rhalavati@chromium.org>
2026-01-08 08:29:22 -0800

    [a11y] Support ACTION_SET_EXTENDED_SELECTION.
    Supports ACTION_SET_EXTENDED_SELECTION action to select multiple nodes.
    
    Internal implementation of the feature is in:
    http://go/sel-api-internal-cl-2
    
    This change is behind the `AccessibilityExtendedSelection` flag.
    
    AX-Relnotes: Adds multi-node selection to Chrome on Android.
    Bug: 443078007
    Change-Id: I561fab045ee20e76d69786dccd3e616c3e7507ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7157675
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566324}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -4731,7 +4731,7 @@ inline constexpr char kAccessibilityExtendedSelectionName[] =
     "Accessibility Extended Selection";
 inline constexpr char kAccessibilityExtendedSelectionDescription[] =
     "When enabled, extended selections are sent to Android through "
-    "setSelection API.";
+    "setSelection API add received using ACTION_SET_EXTENDED_SELECTION.";
 
 inline constexpr char kAccessibilityImproveLiveRegionAnnounceName[] =
     "Accessibility Improve Live Region Announcement";
