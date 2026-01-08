6975cf46fa3c5bde0c3aa3355f17a121b690bdae
Miyoung shin <myid.shin@igalia.com>
2026-01-07 22:02:48 -0800

    Move convert_user_script.{cc|h} to //extensions.
    This CL relocates convert_user_script.{cc|h} to //extensions since
    it no longer depends on //chrome.
    
    Bug: 358567092
    Change-Id: Idccbf693df6ce05f7880fa93ceef05505d7945cd
    Bypass-Check-License: Moving files
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7366554
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566094}

--- extensions/browser/convert_user_script.cc
+++ extensions/browser/convert_user_script.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/extensions/convert_user_script.h"
+#include "extensions/browser/convert_user_script.h"
 
 #include <stddef.h>
 
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
-#include "chrome/common/chrome_paths.h"
 #include "crypto/hash.h"
 #include "extensions/browser/extension_user_script_loader.h"
 #include "extensions/buildflags/buildflags.h"
@@ -82,10 +81,11 @@ scoped_refptr<Extension> ConvertUserScriptToExtension(
   // Create the manifest
   base::Value::Dict root;
   std::string script_name;
-  if (!script.name().empty() && !script.name_space().empty())
+  if (!script.name().empty() && !script.name_space().empty()) {
     script_name = script.name_space() + "/" + script.name();
-  else
+  } else {
     script_name = original_url.spec();
+  }
 
   // Create the public key.
   // User scripts are not signed, but the public key for an extension doubles as
@@ -115,10 +115,11 @@ scoped_refptr<Extension> ConvertUserScriptToExtension(
 
   // Not all scripts have a version, but we need one. Default to 1.0 if it is
   // missing.
-  if (!script.version().empty())
+  if (!script.version().empty()) {
     root.Set(manifest_keys::kVersion, script.version());
-  else
+  } else {
     root.Set(manifest_keys::kVersion, "1.0");
+  }
 
   root.Set(manifest_keys::kDescription, script.description());
   root.Set(manifest_keys::kPublicKey, key);
@@ -129,8 +130,9 @@ scoped_refptr<Extension> ConvertUserScriptToExtension(
   std::vector<std::string> matches;
   if (!script.url_patterns().is_empty()) {
     matches.reserve(script.url_patterns().size());
-    for (const URLPattern& pattern : script.url_patterns())
+    for (const URLPattern& pattern : script.url_patterns()) {
       matches.push_back(pattern.GetAsString());
+    }
   } else {
     // TODO(aa): Derive tighter matches where possible.
     matches.push_back("http://*/*");
@@ -140,8 +142,9 @@ scoped_refptr<Extension> ConvertUserScriptToExtension(
   // Read the exclude matches, if any are present.
   std::vector<std::string> exclude_matches;
   exclude_matches.reserve(script.exclude_url_patterns().size());
-  for (const URLPattern& pattern : script.exclude_url_patterns())
+  for (const URLPattern& pattern : script.exclude_url_patterns()) {
     exclude_matches.push_back(pattern.GetAsString());
+  }
 
   ContentScript content_script;
   content_script.matches = std::move(matches);
--- extensions/browser/convert_user_script.h
+++ extensions/browser/convert_user_script.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_EXTENSIONS_CONVERT_USER_SCRIPT_H_
-#define CHROME_BROWSER_EXTENSIONS_CONVERT_USER_SCRIPT_H_
+#ifndef EXTENSIONS_BROWSER_CONVERT_USER_SCRIPT_H_
+#define EXTENSIONS_BROWSER_CONVERT_USER_SCRIPT_H_
 
 #include <string>
 
@@ -37,4 +37,4 @@ scoped_refptr<extensions::Extension> ConvertUserScriptToExtension(
 
 }  // namespace extensions
 
-#endif  // CHROME_BROWSER_EXTENSIONS_CONVERT_USER_SCRIPT_H_
+#endif  // EXTENSIONS_BROWSER_CONVERT_USER_SCRIPT_H_
