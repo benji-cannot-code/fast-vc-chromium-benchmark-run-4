6c7b0be0cb2a1da8b73578f23390b4b42eec864d
Anders Hartvoll Ruud <andruud@chromium.org>
2026-01-08 05:52:31 -0800

    [scroll-animations] Use normal hash map lookup for timelines
    In FindTimelineForElement, we now (after CL:7390213) basically
    look up values in the hash maps linearly. Since we no longer need
    to support tree-scoped names, there's no point to this approach;
    look up using a normal find() instead.
    
    This CL does roughly the opposite of what CL:4079252 did.
    
    We still store lists of ScopedCSSName for the timeline properties
    on ComputedStyle. A subsequent CL will clean that up.
    
    Bug: 40877521
    Change-Id: Ibb3e70f719fb0c3ad33147a697f33238706b53a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7404138
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566235}

--- third_party/blink/renderer/core/animation/css/css_timeline_map.h
+++ third_party/blink/renderer/core/animation/css/css_timeline_map.h
@@ -7,9 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_ANIMATION_CSS_CSS_TIMELINE_MAP_H_
 
 #include "third_party/blink/renderer/core/core_export.h"
-
-#include "third_party/blink/renderer/core/style/scoped_css_name.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_hash_map.h"
+#include "third_party/blink/renderer/platform/heap/member.h"
 
 namespace blink {
 
@@ -19,9 +18,7 @@ class ScrollTimeline;
 class ViewTimeline;
 
 template <typename TimelineType>
-using CSSTimelineMap =
-    HeapHashMap<Member<const ScopedCSSName>, Member<TimelineType>>;
-
+using CSSTimelineMap = HeapHashMap<AtomicString, Member<TimelineType>>;
 using CSSViewTimelineMap = CSSTimelineMap<ViewTimeline>;
 using CSSScrollTimelineMap = CSSTimelineMap<ScrollTimeline>;
 using CSSDeferredTimelineMap = CSSTimelineMap<DeferredTimeline>;
