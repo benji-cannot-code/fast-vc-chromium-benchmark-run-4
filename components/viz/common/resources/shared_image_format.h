4239360dbf76366e49997d8967d56b12f286c478
Eugene Zemtsov <eugene@chromium.org>
2026-01-08 15:38:16 -0800

    media: Enable multiplanar AHardwareBuffer shared image support
    Adds support for creating shared images from multiplanar AHardwareBuffers
    (e.g., NV12) on Android by enabling external sampling.
    
    Additionally, adjusts VideoCaptureDeviceAndroid to report
    PIXEL_FORMAT_XBGR for YUV formats to align with external sampling behavior.
    
    Bug: 455911385
    Change-Id: I538867810bac8b898eda05d522ce5935732943f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7263848
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Fred Shih <ffred@chromium.org>
    Reviewed-by: Vikas Soni <vikassoni@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566599}

--- components/viz/common/resources/shared_image_format.h
+++ components/viz/common/resources/shared_image_format.h
@@ -98,7 +98,7 @@ class COMPONENT_EXPORT(VIZ_SHARED_IMAGE_FORMAT) SharedImageFormat final {
   // Returns whether this format needs to be externally sampled. Note that
   // external sampling is supported only on Ozone.
   bool PrefersExternalSampler() const {
-#if BUILDFLAG(IS_OZONE)
+#if BUILDFLAG(IS_OZONE) || BUILDFLAG(IS_ANDROID)
     return is_multi_plane()
                ? format_.multiplanar_format.prefers_external_sampler
                : false;
@@ -107,7 +107,7 @@ class COMPONENT_EXPORT(VIZ_SHARED_IMAGE_FORMAT) SharedImageFormat final {
 #endif
   }
 
-#if BUILDFLAG(IS_OZONE)
+#if BUILDFLAG(IS_OZONE) || BUILDFLAG(IS_ANDROID)
   // Sets this format (which must be multiplanar) as needing external sampling.
   void SetPrefersExternalSampler() {
     CHECK(is_multi_plane());
@@ -125,7 +125,7 @@ class COMPONENT_EXPORT(VIZ_SHARED_IMAGE_FORMAT) SharedImageFormat final {
   // using this method in case it is determined that the it's backed by shared
   // memory. https://issues.chromium.org/339546249.
   void ClearPrefersExternalSampler() {
-#if BUILDFLAG(IS_OZONE)
+#if BUILDFLAG(IS_OZONE) || BUILDFLAG(IS_ANDROID)
     CHECK(is_multi_plane() &&
           format_.multiplanar_format.prefers_external_sampler);
     format_.multiplanar_format.prefers_external_sampler = false;
@@ -209,7 +209,7 @@ class COMPONENT_EXPORT(VIZ_SHARED_IMAGE_FORMAT) SharedImageFormat final {
       PlaneConfig plane_config;
       Subsampling subsampling;
       ChannelFormat channel_format;
-#if BUILDFLAG(IS_OZONE)
+#if BUILDFLAG(IS_OZONE) || BUILDFLAG(IS_ANDROID)
       // NOTE: This field is intentionally not used as part of defining equality
       // between two MultiplanarFormat instances as clients should not generally
       // need to care. Clients who need to distinguish for a particular
