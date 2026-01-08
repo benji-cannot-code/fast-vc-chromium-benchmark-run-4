eb531c874d741afc5c3db75d7c562ad1280ee3a7
anthie@google.com <anthie@google.com>
2026-01-08 08:39:46 -0800

    Separate profile picker error dialog from the picker's main view
    This is a planned refactoring. In upcoming changes the
    dialog will be used to display more errors in the profile
    picker (not just force sign-in ones), hence the renaming.
    
    Bug: 307233905, 40280498
    Change-Id: I0ff81b2feec0e326c2119f9c9c7af19604ada5c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7380018
    Reviewed-by: Monica Salama <msalama@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Anthi Orfanou <anthie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1566333}

---
+++ chrome/browser/resources/signin/profile_picker/signin_error_dialog.html.ts
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {SigninErrorDialogElement} from './signin_error_dialog.js';
+
+export function getHtml(this: SigninErrorDialogElement) {
+  return html`
+    <cr-dialog id="dialog">
+      <div slot="title" id="dialog-title" class="key-text">
+        ${this.signinErrorDialogTitle_}
+      </div>
+      <div slot="body" id="dialog-body">
+        <div id="normal-error-message" class="warning-message">
+          ${this.signinErrorDialogBody_}
+        </div>
+      </div>
+      <div slot="button-container" class="button-container">
+        <cr-button id="ok-button" @click="${this.onOkButtonClicked_}">
+          ${this.i18n('ok')}
+        </cr-button>
+        <cr-button id="button-sign-in" class="action-button"
+            @click="${this.onReauthClicked_}"
+            ?hidden="${!this.shouldShowSigninButton_}">
+          ${this.i18n('needsSigninPrompt')}
+        </cr-button>
+      </div>
+    </cr-dialog>`;
+}
