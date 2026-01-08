2288c7182801e32df1947a94fc23cf2e5fcf03e2
Saifuddin Hitawala <hitawala@chromium.org>
2026-01-08 08:05:19 -0800

    [blink] Remove last MailboxHolder usage from StaticBitmapImage
    MailboxHolder from AcceleratedStaticBitmapImage is passed to
    OpenXrRenderLoop::SubmitFrame which passes the sync token from
    holder but that sync token is never used. This removes the param
    and thus last remaining usage of MailboxHolder.
    
    Bug: 383309406
    Change-Id: I82adeeb1875f9b2f02bb5520ff6f9d42a9202c5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7377873
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566311}

--- device/vr/android/arcore/arcore_gl.cc
+++ device/vr/android/arcore/arcore_gl.cc
@@ -1167,7 +1167,6 @@ void ArCoreGl::DidNotProduceVizFrame(int16_t frame_index) {
 }
 
 void ArCoreGl::SubmitFrame(int16_t frame_index,
-                           const gpu::MailboxHolder& mailbox,
                            base::TimeDelta time_waited) {
   NOTREACHED();
 }
