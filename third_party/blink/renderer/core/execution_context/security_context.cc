04a10aff31318e1d6b72b848258d1a524f504e10
Josh Karlin <jkarlin@chromium.org>
2026-01-08 07:25:06 -0800

    [AdTracker] Change BottomAndTop stack scan to TopOnly
    What: The AdTracker should generally focus on the top stack frame in
    determining if a stack is ad-related or not. In cases of mixed stacks
    (ads on bottom, and non-ads on top) then we should defer to non-ad
    because frequently this is publisher code injecting content on an event
    triggered from ad script. Therefore, this CL changes kBottomAndTop stack
    scanning to kTopOnly.
    
    How: Besides the enum rename, the only real change is in the AdTracker
    itself. In particular, AdTracker::IsAdScriptInStackHelper now has
    separate code for top and bottom approaches.
    
    Note that some tests had to be disabled, and will be addressed in the
    bugs associated with the disabled tests.
    
    Bug: 467970831
    Change-Id: I6a87c1da82aa38da56821cb34bf6902fa298ccad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7397670
    Commit-Queue: Josh Karlin <jkarlin@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566282}

--- third_party/blink/renderer/core/execution_context/security_context.cc
+++ third_party/blink/renderer/core/execution_context/security_context.cc
@@ -215,7 +215,7 @@ SecurityContext::FeatureStatus SecurityContext::IsFeatureEnabled(
 
         if (ad_tracker &&
             ad_tracker->IsAdScriptInStack(
-                AdTracker::StackType::kBottomAndTop,
+                AdTracker::StackType::kTopOnly,
                 /*ignore_monkey_patch=*/AdTracker::MonkeyPatchableApi::kNone,
                 &ad_ancestry)) {
           window->CountPermissionsPolicyUsage(
