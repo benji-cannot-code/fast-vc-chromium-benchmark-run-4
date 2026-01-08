e21c95ecd9a7cde5130d9835eeec150ae452f956
Jenna Himawan <jhimawan@google.com>
2026-01-07 19:08:01 -0800

    Make setDrillDownOverrideValueForTesting a static method
    This makes it easier to set in integration tests
    
    Bug: 474139112
    Change-Id: I45cd605967ac05bc98f8a2350a0ae6ea093614ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7410269
    Reviewed-by: Masa Fujita <massan@google.com>
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1566059}

--- ui/android/javatests/src/org/chromium/ui/hierarchicalmenu/HierarchicalMenuTest.java
+++ ui/android/javatests/src/org/chromium/ui/hierarchicalmenu/HierarchicalMenuTest.java
@@ -201,7 +201,7 @@ public class HierarchicalMenuTest {
     public void testHoverCreatesAnchoredFlyout() {
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
-                    mController.setDrillDownOverrideValueForTesting(false);
+                    HierarchicalMenuController.setDrillDownOverrideValueForTesting(false);
                 });
 
         // Hover on Item 1.
@@ -239,7 +239,7 @@ public class HierarchicalMenuTest {
     public void testDrillDownInteraction() {
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
-                    mController.setDrillDownOverrideValueForTesting(true);
+                    HierarchicalMenuController.setDrillDownOverrideValueForTesting(true);
                 });
 
         // Initially, Item 1 and Item 2 should be visible, but Item 1-1 should not.
@@ -285,7 +285,7 @@ public class HierarchicalMenuTest {
     public void testFlyoutTerminalItemClick() {
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
-                    mController.setDrillDownOverrideValueForTesting(false);
+                    HierarchicalMenuController.setDrillDownOverrideValueForTesting(false);
                 });
 
         // Hover on Item 1 to open first flyout.
@@ -315,7 +315,7 @@ public class HierarchicalMenuTest {
     public void testDrillDownTerminalItemClick() {
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
-                    mController.setDrillDownOverrideValueForTesting(true);
+                    HierarchicalMenuController.setDrillDownOverrideValueForTesting(true);
                 });
 
         // Click on "Item 1" to drill down.
