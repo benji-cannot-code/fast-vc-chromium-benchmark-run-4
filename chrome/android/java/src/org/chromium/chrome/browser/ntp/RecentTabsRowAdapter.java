ab576df7b75ec1d2b0d6610dfbf3c4b9a4c7094c
Aishwarya Rajesh <aishwaryarj@google.com>
2026-01-08 08:04:07 -0800

    [RWM][RecentlyClosed] Update domain text of Recently Closed window entry
    Use the active tab title in Recently Closed window entry's domain text
    when the url domain cannot be resolved, for eg., in case of chrome URLs
    like "chrome://versions".
    
    Before: https://screenshot.googleplex.com/9WuZAzRYMaQRayQ
    After: https://screenshot.googleplex.com/9q3iWo2npj4nwBu
    
    Bug: 468100556
    Change-Id: Ib9349b7ee6e9b51a2f58ad17da71b590ff3b132a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7404480
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1566309}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/RecentTabsRowAdapter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/RecentTabsRowAdapter.java
@@ -682,6 +682,10 @@ public class RecentTabsRowAdapter extends BaseExpandableListAdapter {
                 viewHolder.textView.setText(recentlyClosedWindow.getTitle());
                 String activeTabDomain =
                         UrlUtilities.getDomainAndRegistry(recentlyClosedWindow.getUrl(), false);
+                String activeTabInfo =
+                        TextUtils.isEmpty(activeTabDomain)
+                                ? recentlyClosedWindow.getActiveTabTitle()
+                                : activeTabDomain;
                 int inactiveTabCount = recentlyClosedWindow.getTabCount() - 1;
                 final String description;
                 if (inactiveTabCount > 0) {
@@ -689,10 +693,10 @@ public class RecentTabsRowAdapter extends BaseExpandableListAdapter {
                             res.getQuantityString(
                                     R.plurals.recent_tabs_window_closure_domain_text,
                                     inactiveTabCount,
-                                    activeTabDomain,
+                                    activeTabInfo,
                                     inactiveTabCount);
                 } else {
-                    description = activeTabDomain;
+                    description = activeTabInfo;
                 }
                 viewHolder.domainView.setText(description);
                 viewHolder.domainView.setVisibility(View.VISIBLE);
