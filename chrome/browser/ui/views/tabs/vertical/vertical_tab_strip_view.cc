05a030eef9611796db6aa19db2fd19e94f19e237
Alison Gale <agale@chromium.org>
2026-01-08 14:55:30 -0800

    [Vertical Tabs] Adjust top/bottom margins for collapsed tab strip
    When collapsed, this will be 8dp and when expanded it will be 12dp. Also
    adjust the uncollapsed min width to match the current Figma.
    
    https://screenshot.googleplex.com/8FyLJ8LcCoUXUza
    
    Bug: 474089011
    Change-Id: Ic0318c45f253ca591928a3fb42ad8e55a6f07ab1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7411395
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566571}

--- chrome/browser/ui/views/tabs/vertical/vertical_tab_strip_view.cc
+++ chrome/browser/ui/views/tabs/vertical/vertical_tab_strip_view.cc
@@ -68,9 +68,8 @@ views::ProposedLayout VerticalTabStripView::CalculateProposedLayout(
   }
 
   const int region_horizontal_padding = GetLayoutConstant(
-      is_collapsed_
-          ? LayoutConstant::kVerticalTabStripCollapsedHorizontalPadding
-          : LayoutConstant::kVerticalTabStripUncollapsedHorizontalPadding);
+      is_collapsed_ ? LayoutConstant::kVerticalTabStripCollapsedPadding
+                    : LayoutConstant::kVerticalTabStripUncollapsedPadding);
 
   int y = 0;
 
