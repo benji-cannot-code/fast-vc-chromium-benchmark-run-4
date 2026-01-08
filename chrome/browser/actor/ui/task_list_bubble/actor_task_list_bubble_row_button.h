8947e45f19ef5f1cfe8bf1e0c5a27ded76885a68
Claudio DeSouza <cdesouza@chromium.org>
2026-01-08 12:11:43 -0800

    [ActorTaskBubble] IWYU for `ActorTask`
    This change just adds the header for `actor::ActorTask::State`, which is
    missing in this particular case.
    
    Bug: 470101574
    Change-Id: I28e363536cf80370c874b4e7060ba6e12c054423
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7411050
    Reviewed-by: Abe Boujane <boujane@google.com>
    Commit-Queue: Claudio DeSouza <cdesouza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566479}

--- chrome/browser/actor/ui/task_list_bubble/actor_task_list_bubble_row_button.h
+++ chrome/browser/actor/ui/task_list_bubble/actor_task_list_bubble_row_button.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ACTOR_UI_TASK_LIST_BUBBLE_ACTOR_TASK_LIST_BUBBLE_ROW_BUTTON_H_
 #define CHROME_BROWSER_ACTOR_UI_TASK_LIST_BUBBLE_ACTOR_TASK_LIST_BUBBLE_ROW_BUTTON_H_
 
+#include "chrome/browser/actor/actor_task.h"
 #include "chrome/browser/actor/ui/task_list_bubble/actor_task_list_bubble_controller.h"
 #include "chrome/browser/ui/views/controls/rich_hover_button.h"
 
