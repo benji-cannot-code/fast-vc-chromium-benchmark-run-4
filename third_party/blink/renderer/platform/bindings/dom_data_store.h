c86758f4eea1929eb7100348037e62f39f2e3c71
Victor Hugo Vianna Silva <victorvianna@google.com>
2026-01-07 23:09:13 -0800

    Migrate one base::Contains() to Contains() method in blink
    Bug: 470391351
    Change-Id: I271c63b8dd328f022c6994183c22f8234ecff0bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7414578
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566118}

--- third_party/blink/renderer/platform/bindings/dom_data_store.h
+++ third_party/blink/renderer/platform/bindings/dom_data_store.h
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_BINDINGS_DOM_DATA_STORE_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_BINDINGS_DOM_DATA_STORE_H_
 
-#include "base/containers/contains.h"
 #include "third_party/blink/renderer/platform/bindings/dom_wrapper_world.h"
 #include "third_party/blink/renderer/platform/bindings/script_state.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
@@ -429,7 +428,7 @@ bool DOMDataStore::Contains(const ScriptWrappable* object) const {
   if (can_use_inline_storage_) {
     return !GetUncheckedInlineStorage(object).IsEmpty();
   }
-  return base::Contains(wrapper_map_, object);
+  return wrapper_map_.Contains(object);
 }
 
 // static
