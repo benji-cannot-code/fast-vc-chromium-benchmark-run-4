e97f5a90771b1f57b7d98f647a08a847c8ec7fd6
Mark Cogan <marq@chromium.org>
2026-01-08 09:34:54 -0800

    Change DCHECK to CHECK + NotFatalUntil in BVCCVC
    Changes a DCHECK to a CHECK so we can gather more data on why this is
    asserting.
    
    Bug: 472278494
    Change-Id: I3f676dba9a88ce361030eeda350879e6fee0abd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7414807
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Auto-Submit: Mark Cogan <marq@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566366}

--- ios/chrome/browser/main/ui_bundled/bvc_container_view_controller.mm
+++ ios/chrome/browser/main/ui_bundled/bvc_container_view_controller.mm
@@ -54,7 +54,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self.view addSubview:bvc.view];
   [bvc didMoveToParentViewController:self];
 
-  DCHECK(self.currentBVC == bvc);
+  // This will fail if there's another child view controller added before `bvc`.
+  // If this happens during startup, it may be the BVC adding the launch screen
+  // as a child VC of this VC (BVC's parent).
+  CHECK(self.currentBVC == bvc, base::NotFatalUntil::M150);
 }
 
 #pragma mark - UIViewController methods
