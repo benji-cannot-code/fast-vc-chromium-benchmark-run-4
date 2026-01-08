b813e0548c1fde8fb5232a13f190d27d7cd2957c
S. Ganesh <ganesh@chromium.org>
2026-01-08 06:10:06 -0800

    updater: refactor and helper methods to get the updater and app policies
    Bug: 456497861
    Change-Id: I19787b0aad25b08a653b66e59f2cb43e278e269c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7412577
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566247}

--- chrome/updater/policy/manager.cc
+++ chrome/updater/policy/manager.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/strings/stringprintf.h"
 #include "base/time/time.h"
 #include "chrome/updater/constants.h"
 
@@ -34,6 +35,11 @@ bool UpdatesSuppressedTimes::contains(int hour, int minute) const {
     return true;
   }
   return false;
+}
+
+std::string UpdatesSuppressedTimes::ToString() const {
+  return base::StringPrintf("%d, %d, %d", start_hour_, start_minute_,
+                            duration_minute_);
 }
 
 // DefaultValuesPolicyManager returns the default values for policies when no
