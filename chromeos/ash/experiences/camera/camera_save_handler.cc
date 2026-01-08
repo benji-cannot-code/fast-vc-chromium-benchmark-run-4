8e9dfe2656d816a4348398f3776b51096f0748a8
Orko Garai <orko@igalia.com>
2026-01-08 14:25:33 -0800

    CCA: Cancel Upload Dialog
    Implement cancel confirmation dialog presented when user clicks cancel
    button in progress upload notification for camera upload to OneDrive and
    Google Drive.
    
    Design Doc:
    https://docs.google.com/document/d/1wod7fE1aU4D0boQxx9dxgNsAvJXvBPfvc8XFAmjtFuQ/edit?usp=sharing
    
    Bug: 454152412
    Change-Id: Iea4a37c83d89b14eb1666a22fb376e9282cdcd20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7253869
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Aida Zolić <aidazolic@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Orko Garai <orko@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1566555}

--- chromeos/ash/experiences/camera/camera_save_handler.cc
+++ chromeos/ash/experiences/camera/camera_save_handler.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/ash/experiences/camera/camera_save_handler.h"
 
+#include "ash/system/camera/camera_app_prefs.h"
 #include "base/check.h"
 #include "base/check_op.h"
 #include "base/files/file_path.h"
@@ -15,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notimplemented.h"
 #include "base/task/thread_pool.h"
 #include "chromeos/ash/experiences/camera/camera_upload_notification.h"
+#include "chromeos/ash/experiences/camera/cancel_camera_upload_dialog.h"
 #include "ui/gfx/image/image.h"
 
 namespace {
@@ -182,6 +184,8 @@ void CameraSaveHandler::UpdateProgressNotification() {
   if (uploads_.empty()) {
     CHECK_EQ(total_bytes_uploaded_, 0);
     CHECK_EQ(total_size_of_uploads_, 0);
+    // Close the cancel upload confirmation dialog because all uploads are done.
+    cancel_dialog_.reset();
     progress_notification_.reset();
     return;
   }
@@ -192,11 +196,28 @@ void CameraSaveHandler::UpdateProgressNotification() {
   if (!progress_notification_) {
     progress_notification_ = std::make_unique<CameraUploadNotification>(
         delegate_->GetDestination(),
-        // TODO(crbug.com/454152412) Implement cancel upload dialog
-        base::BindOnce(&CameraSaveHandler::CancelUploads,
+        base::BindOnce(ash::camera_app_prefs::ShouldSkipCancelUploadDialog()
+                           ? &CameraSaveHandler::CancelUploads
+                           : &CameraSaveHandler::ShowCancelDialog,
                        weak_ptr_factory_.GetWeakPtr()));
   }
   progress_notification_->UpdateProgress(progress, uploads_.size());
+}
+
+void CameraSaveHandler::ShowCancelDialog() {
+  cancel_dialog_ = std::make_unique<CancelCameraUploadDialog>(
+      base::BindRepeating(&CameraSaveHandler::OnCancelDialogClicked,
+                          weak_ptr_factory_.GetWeakPtr()));
+}
+
+void CameraSaveHandler::OnCancelDialogClicked(bool cancel,
+                                              bool skip_dialog_next_time) {
+  if (cancel) {
+    CancelUploads();
+  }
+  if (skip_dialog_next_time) {
+    ash::camera_app_prefs::SetSkipCancelUploadDialog();
+  }
 }
 
 void CameraSaveHandler::CancelUploads() {
