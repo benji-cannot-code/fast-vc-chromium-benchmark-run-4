92b339c0fddd76ded84b3e3b49cd751d7f578bf0
Michał Kaczmarczyk <mickaczmarczyk@google.com>
2026-01-08 06:15:46 -0800

    Fix alignment in DevTools policy dialog for macOS and windows.
    This change corrects the vertical alignment of the icon on macOS and windows.
    before: https://screenshot.googleplex.com/9RnoUECtyEXsh4E
    after: https://screenshot.googleplex.com/3TrRRHyYuMXvw6S
    Bug: 442892562
    
    Change-Id: Ib4d45ae114b9ef932e83ff4b8a6db7d6f0c5572d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7412244
    Reviewed-by: Aliaksei Bahdzevich <thelex@google.com>
    Commit-Queue: Michał Kaczmarczyk <mickaczmarczyk@google.com>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566250}

--- chrome/browser/devtools/devtools_policy_dialog.cc
+++ chrome/browser/devtools/devtools_policy_dialog.cc
@@ -33,8 +33,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+#if BUILDFLAG(IS_LINUX)
 constexpr int kIconPadding = 4;
-
+#else
+constexpr int kIconPadding = 0;
+#endif
 }
 
 DevToolsPolicyDialog::DevToolsPolicyDialog(content::WebContents* web_contents) {
