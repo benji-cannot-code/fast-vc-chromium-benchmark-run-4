e1b047239d1f0d19ceaf4b12bfffc3032a02a5ce
Carlos Marcelo Barrera Nolasco <carlosbarrera@google.com>
2026-01-08 11:13:47 -0800

    [a11y] FIX - Update InputConnection text getters to return spannables.
    In response to the reverted CL
    https://chromium-review.googlesource.com/c/chromium/src/+/7243174
    
    [NEW] Patchset1 is the original (merged -> reverted) CL, Patchset 2
    includes just the changes to fix the reason for revert.
    
    [NEW] getSurroundingText includes a condition to return a Spannable if
    there are IME text spans existing in the text even when
    ACCESSIBILITY_IME_GET_FORMATTED_TEXT is disabled.
    
    The methods getSelectedText, getTextAfterSelection,
    getTextBeforeSelection, and getSurroundingText in TextInputState now
    take into account the flags parameter.
    
    When flags value is InputConnection.GET_TEXT_WITH_STYLES, these methods
    return a SpannableStringBuilder to preserve text styles. Otherwise, they
    return a String as before.
    
    ThreadedInputConnection is updated to pass the flags parameter from the
    InputConnection API calls to TextInputState.
    
    Tests in TextInputStateTest are expanded to cover the new behavior with
    and without the GET_TEXT_WITH_STYLES flag, and with both plain String
    and SpannableStringBuilder inputs.
    
    Bug: 420929707
    Change-Id: I54bffa82fab8f00e9628ac4c72189b712d2045e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7389981
    Reviewed-by: Chuong Ho-Dac <hdchuong@chromium.org>
    Commit-Queue: Carlos Marcelo Barrera Nolasco <carlosbarrera@google.com>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566446}

--- content/browser/android/content_feature_map.cc
+++ content/browser/android/content_feature_map.cc
@@ -37,6 +37,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &features::kAccessibilityDeprecateJavaNodeCache,
     &features::kAccessibilityDeprecateTypeAnnounce,
     &features::kAccessibilityExtendedSelection,
+    &features::kAccessibilityImeGetFormattedText,
     &features::kAccessibilityImproveLiveRegionAnnounce,
     &features::kAccessibilityMagnificationFollowsFocus,
     &features::kAccessibilityRequestLayoutBasedActions,
--- content/public/android/java/src/org/chromium/content/browser/input/ThreadedInputConnection.java
+++ content/public/android/java/src/org/chromium/content/browser/input/ThreadedInputConnection.java
@@ -736,7 +736,7 @@ class ThreadedInputConnection extends BaseInputConnection implements ChromiumBas
         }
         TextInputState textInputState = requestAndWaitForTextInputState();
         if (textInputState == null) return null;
-        return textInputState.getSurroundingText(beforeLength, afterLength);
+        return textInputState.getSurroundingText(beforeLength, afterLength, flags);
     }
 
     /**
@@ -747,7 +747,7 @@ class ThreadedInputConnection extends BaseInputConnection implements ChromiumBas
         if (DEBUG_LOGS) Log.i(TAG, "getTextBeforeCursor [%d %x]", maxChars, flags);
         TextInputState textInputState = requestAndWaitForTextInputState();
         if (textInputState == null) return null;
-        return textInputState.getTextBeforeSelection(maxChars);
+        return textInputState.getTextBeforeSelection(maxChars, flags);
     }
 
     /**
@@ -758,7 +758,7 @@ class ThreadedInputConnection extends BaseInputConnection implements ChromiumBas
         if (DEBUG_LOGS) Log.i(TAG, "getTextAfterCursor [%d %x]", maxChars, flags);
         TextInputState textInputState = requestAndWaitForTextInputState();
         if (textInputState == null) return null;
-        return textInputState.getTextAfterSelection(maxChars);
+        return textInputState.getTextAfterSelection(maxChars, flags);
     }
 
     /**
@@ -769,7 +769,7 @@ class ThreadedInputConnection extends BaseInputConnection implements ChromiumBas
         if (DEBUG_LOGS) Log.i(TAG, "getSelectedText [%x]", flags);
         TextInputState textInputState = requestAndWaitForTextInputState();
         if (textInputState == null) return null;
-        return textInputState.getSelectedText();
+        return textInputState.getSelectedText(flags);
     }
 
     /**
