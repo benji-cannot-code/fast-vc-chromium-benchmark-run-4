7940fb486d1c26c1f18be024aa5a629a113b1f8e
Fiaz Muhammad <mfiaz@google.com>
2026-01-08 12:46:18 -0800

    [Persist] Add clean unused nodes functionality to Android Service
    - New crawler to build a list of all storage IDs in a
    	TabStripCollection.
    - Add method to clear unused nodes for a window. Use TabStripCollection
    	to build set of whitelisted (used) nodes and delete all nodes
    	not in this list.
    
    Bug: 472525620
    Change-Id: Idd88496d8fbfc2d3e006c9252b02643ffc362edc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7363761
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566497}

--- chrome/browser/android/tab_state_storage_service_android.cc
+++ chrome/browser/android/tab_state_storage_service_android.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/tab_state_storage_service_android.h"
 
 #include <memory>
+#include <vector>
 
 #include "base/android/callback_android.h"
 #include "base/android/jni_android.h"
@@ -22,10 +23,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/tab_android.h"
 #include "chrome/browser/android/tab_group_collection_data_android.h"
 #include "chrome/browser/tab/protocol/tab_state.pb.h"
+#include "chrome/browser/tab/storage_id.h"
+#include "chrome/browser/tab/storage_id_mapping.h"
 #include "chrome/browser/tab/storage_loaded_data.h"
 #include "chrome/browser/tab/tab_group_collection_data.h"
 #include "chrome/browser/tab/tab_state_storage_backend.h"
 #include "chrome/browser/tab/tab_state_storage_service.h"
+#include "components/tabs/public/android/jni_conversion.h"
+#include "components/tabs/public/direct_child_walker.h"
+#include "components/tabs/public/tab_strip_collection.h"
 
 // Must come after all headers that specialize FromJniType() / ToJniType().
 #include "chrome/browser/tab/jni_headers/TabStateStorageService_jni.h"
@@ -46,6 +52,31 @@ void RunJavaCallbackLoadAll(JNIEnv* env,
   base::android::RunObjectCallbackAndroid(j_loaded_data_callback,
                                           data_android->GetJavaObject());
 }
+
+// Recursively crawls the entire tree and retrieves storage ids for all nodes.
+class CollectionStorageIdCrawler : public DirectChildWalker::Processor {
+ public:
+  explicit CollectionStorageIdCrawler(StorageIdMapping& mapping,
+                                      std::vector<StorageId>& ids)
+      : mapping_(mapping), ids_(ids) {}
+
+  void ProcessTab(const TabInterface* tab) override {
+    StorageId id = mapping_.get().GetStorageId(tab);
+    ids_->push_back(id);
+  }
+
+  void ProcessCollection(const TabCollection* collection) override {
+    StorageId id = mapping_.get().GetStorageId(collection);
+    ids_->push_back(id);
+
+    DirectChildWalker walker(collection, this);
+    walker.Walk();
+  }
+
+ private:
+  raw_ref<StorageIdMapping> mapping_;
+  raw_ref<std::vector<StorageId>> ids_;
+};
 
 }  // namespace
 
@@ -99,6 +130,20 @@ void TabStateStorageServiceAndroid::ClearState(JNIEnv* env) {
 void TabStateStorageServiceAndroid::ClearWindow(JNIEnv* env,
                                                 const std::string& window_tag) {
   tab_state_storage_service_->ClearWindow(window_tag);
+}
+
+void TabStateStorageServiceAndroid::ClearUnusedNodesForWindow(
+    JNIEnv* env,
+    const std::string& window_tag,
+    const TabStripCollection* collection) {
+  std::vector<StorageId> ids;
+  ids.push_back(tab_state_storage_service_->GetStorageId(collection));
+  CollectionStorageIdCrawler crawler(*tab_state_storage_service_, ids);
+
+  DirectChildWalker walker(collection, &crawler);
+  walker.Walk();
+
+  tab_state_storage_service_->ClearNodesForWindowExcept(window_tag, ids);
 }
 
 void TabStateStorageServiceAndroid::PrintAll(JNIEnv* env) {
