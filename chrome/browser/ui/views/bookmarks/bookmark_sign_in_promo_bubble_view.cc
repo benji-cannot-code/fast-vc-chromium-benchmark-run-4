ffebfb4c4e5419308e60a2c5ca68b1758fb51543
Amelie Schneider <amelies@google.com>
2026-01-08 03:15:26 -0800

    [UNO] Update margins of sign in promo dialog
    This sets the bottom margin of the sign in promo to 20 instead of 16.
    
    Before: https://screenshot.googleplex.com/6jba7yudo4VPUq5
    After: https://screenshot.googleplex.com/6zFpL6YzCSKgMMW
    
    Bug: 470341944
    Change-Id: I7bd5725a00a8663aa8ab6f3bcf79da2805d2c377
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7276960
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Amelie Schneider <amelies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1566188}

--- chrome/browser/ui/views/bookmarks/bookmark_sign_in_promo_bubble_view.cc
+++ chrome/browser/ui/views/bookmarks/bookmark_sign_in_promo_bubble_view.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/bookmarks/bookmark_sign_in_promo_bubble_view.h"
 
+#include "chrome/browser/signin/signin_promo_util.h"
 #include "chrome/browser/ui/signin/promos/bubble_signin_promo_view.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/bookmarks/browser/bookmark_node.h"
@@ -31,6 +32,7 @@ BookmarkSigninPromoBubbleView::BookmarkSigninPromoBubbleView(
           views::DISTANCE_UNRELATED_CONTROL_VERTICAL)));
   set_fixed_width(views::LayoutProvider::Get()->GetDistanceMetric(
       views::DISTANCE_MODAL_DIALOG_PREFERRED_WIDTH));
+  set_margins(BubbleSignInPromoView::GetBubbleSigninPromoMargins());
 
   // Show the sign in promo.
   auto* sign_in_promo = AddChildView(std::make_unique<BubbleSignInPromoView>(
