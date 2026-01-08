c0d860c45a8b3c90253bd30ae6e6a84b0b67eee2
Bryant Chandler <bryantchandler@google.com>
2026-01-08 15:31:25 -0800

    [glic] Refactor GlicPinnedTabManager to use GlicTabObserver
    Refactors GlicPinnedTabManager to use the new GlicTabObserver for
    monitoring tab activity. This simplifies the manager by removing direct
    TabStripModel observation. Updates GlicTabObserver to support a generic
    TabMutationEvent for non-creation changes.
    
    Bug: b:470059315
    Change-Id: Ia11ef2f135ee7581d8fdd57db77626066a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7416081
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566593}

--- chrome/browser/glic/host/context/glic_pinned_tab_manager.h
+++ chrome/browser/glic/host/context/glic_pinned_tab_manager.h
@@ -10,15 +10,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_list.h"
 #include "base/memory/raw_ptr.h"
+#include "chrome/browser/glic/common/glic_tab_observer.h"
 #include "chrome/browser/glic/host/context/glic_tab_data.h"
 #include "chrome/browser/glic/host/glic.mojom.h"
 #include "chrome/browser/glic/widget/glic_window_controller.h"
-#include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
 #include "components/tabs/public/tab_interface.h"
 #include "mojo/public/cpp/bindings/remote.h"
 
 class Profile;
-class BrowserTabStripTracker;
 
 namespace glic {
 class GlicMetrics;
@@ -34,12 +33,12 @@ struct GlicPinnedTabContextEvent {
 };
 
 // Manages a collection of tabs that have been selected to be shared.
-class GlicPinnedTabManager : public TabStripModelObserver {
+class GlicPinnedTabManager {
  public:
   explicit GlicPinnedTabManager(Profile* profile,
                                 GlicInstance::UIDelegate* ui_delegate,
                                 GlicMetrics* metrics);
-  ~GlicPinnedTabManager() override;
+  virtual ~GlicPinnedTabManager();
 
   // Registers a callback to be invoked when the collection of pinned tabs
   // changes.
@@ -131,15 +130,7 @@ class GlicPinnedTabManager : public TabStripModelObserver {
  private:
   class UpdateThrottler;
 
-  // TabStripModelObserver implementation:
-  void OnTabStripModelChanged(
-      TabStripModel* tab_strip_model,
-      const TabStripModelChange& change,
-      const TabStripSelectionChange& selection) override;
-  void OnTabChangedAt(tabs::TabInterface* tab,
-                      int index,
-                      TabChangeType change_type) override;
-  void OnTabWillBeRemoved(tabs::TabInterface* tab, int index) override;
+  void OnTabEvent(const GlicTabEvent& event);
 
   void OnPinCandidatesObserverDisconnected();
 
@@ -227,8 +218,8 @@ class GlicPinnedTabManager : public TabStripModelObserver {
   // A timer to debounce pin candidate updates.
   std::unique_ptr<UpdateThrottler> pin_candidate_updater_;
 
-  // Tracks all the browsers for the current profile.
-  std::unique_ptr<BrowserTabStripTracker> tab_strip_tracker_;
+  // Tracks tab events for the current profile.
+  std::unique_ptr<GlicTabObserver> tab_observer_;
 
   base::WeakPtrFactory<GlicPinnedTabManager> weak_ptr_factory_{this};
 };
