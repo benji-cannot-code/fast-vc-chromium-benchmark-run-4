924b0868e6ffeebc5bf5ae02f6af396788f8e5aa
Tom Sepez <tsepez@google.com>
2026-01-07 16:43:55 -0800

    Remove -ui/gl/ from unsafe_buffers_paths.txt
    Doing so will enforce unsafe buffers warnings on the files in this
    directory. Then make the files compliant:
    
    -- Update generate_bindings.py to emit a per-file suppression in all
       of its generated files.
    -- Run generate_bindings.py to regenerate these files.
    -- Suppress two additional non-generated files.
    
    Bug: 435317390
    Change-Id: I7f90b44844f411214f3759e1890b6668be8ad060
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7398328
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566012}

--- ui/gl/generate_bindings.py
+++ ui/gl/generate_bindings.py
@@ -2715,6 +2715,11 @@ LICENSE_AND_HEADER = """\
 //    clang-format -i -style=chromium filename
 // DO NOT EDIT!
 
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
+#pragma allow_unsafe_buffers
+#endif
+
 """
 
 GLVersion = namedtuple('GLVersion', 'is_es major_version minor_version')
