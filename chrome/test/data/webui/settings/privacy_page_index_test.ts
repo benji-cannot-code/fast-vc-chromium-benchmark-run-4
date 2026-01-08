225cffcd86a7f2201b0a2a4ca9bf60fef94c57c6
Hubert Chao <hchao@chromium.org>
2026-01-08 05:55:42 -0800

    [LNA] add privacy index tests for new permission settings
    Followup from https://crrev.com/c/7396987 to add permission index tests
    to privacy_page_index_test.ts
    
    Bug: 465491626
    Change-Id: If23506b0717536359781f07fc1d3e1ef94ce50cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7403817
    Auto-Submit: Hubert Chao <hchao@chromium.org>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Hubert Chao <hchao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566239}

--- chrome/test/data/webui/settings/privacy_page_index_test.ts
+++ chrome/test/data/webui/settings/privacy_page_index_test.ts
@@ -35,6 +35,7 @@ suite('PrivacyPageIndex', function() {
           enableHandTrackingContentSetting: false,
           enableKeyboardLockPrompt: false,
           enableLocalNetworkAccessSetting: false,
+          enableLocalNetworkAccessSplitPermissions: false,
           enablePaymentHandlerContentSetting: false,
           enablePersistentPermissions: false,
           enableSafeBrowsingSubresourceFilter: false,
@@ -530,12 +531,39 @@ suite('PrivacyPageIndex', function() {
 
     test('RoutingLocalNetworkAccess', async function() {
       assertFalse(loadTimeData.getBoolean('enableLocalNetworkAccessSetting'));
+      assertFalse(
+          loadTimeData.getBoolean('enableLocalNetworkAccessSplitPermissions'));
       await createPrivacyPageIndex({enableLocalNetworkAccessSetting: true});
 
       return testViewsForRoute(
           routes.SITE_SETTINGS_LOCAL_NETWORK_ACCESS,
           ['siteSettingsLocalNetworkAccess'], 'privacy');
     });
+
+    test('RoutingLocalNetwork', async function() {
+      assertFalse(loadTimeData.getBoolean('enableLocalNetworkAccessSetting'));
+      assertFalse(
+          loadTimeData.getBoolean('enableLocalNetworkAccessSplitPermissions'));
+      await createPrivacyPageIndex(
+          {enableLocalNetworkAccessSplitPermissions: true});
+
+      return testViewsForRoute(
+          routes.SITE_SETTINGS_LOCAL_NETWORK, ['siteSettingsLocalNetwork'],
+          'privacy');
+    });
+
+    test('RoutingLoopbackNetwork', async function() {
+      assertFalse(loadTimeData.getBoolean('enableLocalNetworkAccessSetting'));
+      assertFalse(
+          loadTimeData.getBoolean('enableLocalNetworkAccessSplitPermissions'));
+      await createPrivacyPageIndex(
+          {enableLocalNetworkAccessSplitPermissions: true});
+
+      return testViewsForRoute(
+          routes.SITE_SETTINGS_LOOPBACK_NETWORK,
+          ['siteSettingsLoopbackNetwork'], 'privacy');
+    });
+
 
     test('RoutingPaymentHandler', async function() {
       assertFalse(
