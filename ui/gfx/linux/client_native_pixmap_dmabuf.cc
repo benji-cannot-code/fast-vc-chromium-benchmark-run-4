ae5ee5b0490aff53ad6dc3b5a55af4156f480445
Colin Blundell <blundell@chromium.org>
2026-01-08 03:57:13 -0800

    Revert "[//ui] Remove BufferFormat from ClientNativePixmapDmabuf interface"
    This reverts commit f661dadf88a076806d15fa2f2a28fa7a2b9c100a.
    
    Reason for revert: CL chain broke compile on bot not covered by CQ
    
    Original change's description:
    > [//ui] Remove BufferFormat from ClientNativePixmapDmabuf interface
    >
    > Followup will remove it from the implementation.
    >
    > Bug: 356649879
    > Change-Id: Icb18e2efc724e9b3dfce8bf9e2c98682fc561257
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7404141
    > Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    > Commit-Queue: Colin Blundell <blundell@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1566190}
    
    Bug: 356649879
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I02a8f5e5377b39afb058974ad42e74a6fb01df49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7414803
    Reviewed-by: Gabrielle Lau <gabriellelau@google.com>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566206}

--- ui/gfx/linux/client_native_pixmap_dmabuf.cc
+++ ui/gfx/linux/client_native_pixmap_dmabuf.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
 #include "build/chromecast_buildflags.h"
-#include "components/viz/common/resources/shared_image_format_utils.h"
 #include "ui/gfx/linux/dmabuf_uapi.h"
 #include "ui/gfx/switches.h"
 
@@ -101,14 +100,6 @@ ClientNativePixmapDmaBuf::MapPlane(const NativePixmapPlane& plane) {
   return UNSAFE_BUFFERS(
       base::HeapArray<uint8_t, PlaneDeleter>::FromOwningPointer(
           static_cast<uint8_t*>(data), size_to_map, PlaneDeleter{size_to_map}));
-}
-
-// static
-bool ClientNativePixmapDmaBuf::IsConfigurationSupported(
-    viz::SharedImageFormat format,
-    gfx::BufferUsage usage) {
-  return IsConfigurationSupported(viz::SharedImageFormatToBufferFormat(format),
-                                  usage);
 }
 
 // static
