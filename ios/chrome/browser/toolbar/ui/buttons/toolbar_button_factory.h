6f40f07a28d84a6f2b7a532ca086143bee02d450
Gauthier Ambard <gambard@chromium.org>
2026-01-08 09:27:35 -0800

    [iOS] Create a new Toolbar
    This CL creates a new toolbar, mostly reusing the logic from the legacy
    one.
    
    Bug: 472279443
    Change-Id: I89e3877b86bd3819384589efc37c7f619b8ed3d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7402207
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566365}

---
+++ ios/chrome/browser/toolbar/ui/buttons/toolbar_button_factory.h
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_TOOLBAR_UI_BUTTONS_TOOLBAR_BUTTON_FACTORY_H_
+#define IOS_CHROME_BROWSER_TOOLBAR_UI_BUTTONS_TOOLBAR_BUTTON_FACTORY_H_
+
+#import <UIKit/UIKit.h>
+
+@class ToolbarButton;
+
+// Factory for creating toolbar buttons.
+@interface ToolbarButtonFactory : NSObject
+
+// Creates a back button.
+- (ToolbarButton*)makeBackButton;
+
+// Creates a forward button.
+- (ToolbarButton*)makeForwardButton;
+
+// Creates a reload button.
+- (ToolbarButton*)makeReloadButton;
+
+// Creates a stop button.
+- (ToolbarButton*)makeStopButton;
+
+// Creates a share button.
+- (ToolbarButton*)makeShareButton;
+
+// Creates a tab grid button.
+- (ToolbarButton*)makeTabGridButton;
+
+// Creates an assistant button.
+- (ToolbarButton*)makeAssistantButton;
+
+// Creates a tools menu button.
+- (ToolbarButton*)makeToolsMenuButton;
+
+// Creates an omnibox button (location bar).
+- (UIButton*)makeOmniboxButton;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_TOOLBAR_UI_BUTTONS_TOOLBAR_BUTTON_FACTORY_H_
---
+++ ios/chrome/browser/toolbar/ui/toolbar_constants.mm
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/toolbar/ui/toolbar_constants.h"
+
+NSString* const kToolbarViewIdentifier = @"kToolbarViewIdentifier";
+NSString* const kToolbarBackButtonIdentifier = @"kToolbarBackButtonIdentifier";
+NSString* const kToolbarForwardButtonIdentifier =
+    @"kToolbarForwardButtonIdentifier";
+NSString* const kToolbarReloadButtonIdentifier =
+    @"kToolbarReloadButtonIdentifier";
+NSString* const kToolbarStopButtonIdentifier = @"kToolbarStopButtonIdentifier";
+NSString* const kToolbarShareButtonIdentifier =
+    @"kToolbarShareButtonIdentifier";
+NSString* const kToolbarTabGridButtonIdentifier =
+    @"kToolbarTabGridButtonIdentifier";
+NSString* const kToolbarToolsMenuButtonIdentifier =
+    @"kToolbarToolsMenuButtonIdentifier";
+NSString* const kToolbarOmniboxButtonIdentifier =
+    @"kToolbarOmniboxButtonIdentifier";
+NSString* const kToolbarAssistantButtonIdentifier =
+    @"kToolbarAssistantButtonIdentifier";
