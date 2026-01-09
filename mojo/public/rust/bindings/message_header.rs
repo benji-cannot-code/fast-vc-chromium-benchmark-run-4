ccf568516827d011d09b8604eb893de98651dd8b
Devon Loehr <dloehr@google.com>
2026-01-09 08:29:44 -0800

    [rustmojo] Add cxx imports for creating message headers
    This CL exposes some C++ functionality we need when creating mojo
    headers; specifically, the ability to generate a trace nonce, and to
    get the current timestamp using base::TimeTicks.
    
    For simplicity, we use a shim for the latter instead of working out how
    to expose the entire TimeTicks class and functionality.
    
    Since this involves creating several GN targets for the bindings, we
    also move them into their own file to reduce clutter.
    
    Bug: 444509367
    Change-Id: Iec971da4f948500532c29df0cf26d95c0504a450
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7413007
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Devon Loehr <dloehr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1566959}

--- mojo/public/rust/bindings/message_header.rs
+++ mojo/public/rust/bindings/message_header.rs
@@ -13,7 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 chromium::import! {
     "//mojo/public/rust/mojom_parser";
+    "//mojo/public/rust/bindings:helper_functions_cxx";
 }
+
+use helper_functions_cxx::ffi as cxx_helpers;
 
 /// The header format used by the most recent version of Mojom.
 ///
@@ -60,7 +63,7 @@ pub struct MessageHeaderV3 {
     /// Used for associated interfaces. Rust doesn't support these yet.
     interface_ids_ptr: u64,
     /// Tracks when the header was created.
-    creation_timeticks_us: u64,
+    creation_timeticks_us: i64,
 }
 
 impl MessageHeaderV3 {
@@ -70,19 +73,20 @@ impl MessageHeaderV3 {
             interface_id,
             name,
             flags,
-            trace_nonce: todo!("Call C++ to get this"),
+            // The C++ equivalent uses static_cast<uint32_t> on the result here
+            trace_nonce: cxx_helpers::GetNextGlobalTraceId() as u32,
             request_id,
             // The payload always immediately follows the header, so it's 24
             // bytes from the start of `payload_ptr`
             payload_ptr: 24,
             // This pointer is unused in Rust for now
             interface_ids_ptr: 0,
-            creation_timeticks_us: todo!("Call C++ to get this"),
+            creation_timeticks_us: cxx_helpers::CurrentTimeTicksInMicroseconds(),
         }
     }
 
     /// Return the timestamp when this header value was created.
-    pub fn creation_timeticks_us(&self) -> u64 {
+    pub fn creation_timeticks_us(&self) -> i64 {
         self.creation_timeticks_us
     }
 
