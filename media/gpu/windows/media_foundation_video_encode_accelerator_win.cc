1c1e9c757b06d1f9a8e37b9036ecc4e13c800e2d
Colin Blundell <blundell@chromium.org>
2026-01-09 00:30:51 -0800

    [//media] Rename VideoFrame::HasNativeGpuMemoryBuffer()
    This should now be HasNativeMappableSharedImage().
    
    Bug: 404905709
    Change-Id: I8d6ed08d25ebcfcb031c52e091769387cd739cec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7415340
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Mingjing Zhang <mjzhang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566781}

--- media/gpu/windows/media_foundation_video_encode_accelerator_win.cc
+++ media/gpu/windows/media_foundation_video_encode_accelerator_win.cc
@@ -749,7 +749,7 @@ void MediaFoundationVideoEncodeAccelerator::QueueInput(
   result.discard_output = discard_output;
 
   result.generate_sample_on_wait_sync_token =
-      command_buffer_helper_ && !frame->HasNativeGpuMemoryBuffer() &&
+      command_buffer_helper_ && !frame->HasNativeMappableSharedImage() &&
       !frame->HasMappableSharedImage() && frame->HasSharedImage();
   if (result.generate_sample_on_wait_sync_token) {
     TRACE_EVENT0("media",
@@ -1947,7 +1947,7 @@ HRESULT MediaFoundationVideoEncodeAccelerator::PopulateInputSampleBuffer(
 
   if (frame->HasMappableSharedImage() ||
       input.generate_sample_on_wait_sync_token) {
-    if ((frame->HasNativeGpuMemoryBuffer() ||
+    if ((frame->HasNativeMappableSharedImage() ||
          input.generate_sample_on_wait_sync_token) &&
         dxgi_device_manager_ != nullptr) {
       if (!dxgi_resource_mapping_required_) {
@@ -2089,7 +2089,7 @@ HRESULT MediaFoundationVideoEncodeAccelerator::CopyInputSampleBufferFromGpu(
     scoped_refptr<VideoFrame> frame,
     const PendingInput& input) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  DCHECK(frame->HasNativeGpuMemoryBuffer() ||
+  DCHECK(frame->HasNativeMappableSharedImage() ||
          input.generate_sample_on_wait_sync_token);
   DCHECK(dxgi_device_manager_);
   auto& input_sample = input.input_sample;
@@ -2102,7 +2102,7 @@ HRESULT MediaFoundationVideoEncodeAccelerator::CopyInputSampleBufferFromGpu(
 
   HRESULT hr;
   ComD3D11Texture2D input_texture;
-  if (frame->HasNativeGpuMemoryBuffer()) {
+  if (frame->HasNativeMappableSharedImage()) {
     gfx::GpuMemoryBufferHandle buffer_handle =
         frame->GetGpuMemoryBufferHandle();
 
@@ -2231,7 +2231,7 @@ HRESULT MediaFoundationVideoEncodeAccelerator::PopulateInputSampleBufferGpu(
   HRESULT hr;
   ComD3D11Texture2D input_texture;
   ComD3D11Texture2D sample_texture;
-  if (frame->HasNativeGpuMemoryBuffer()) {
+  if (frame->HasNativeMappableSharedImage()) {
     gfx::GpuMemoryBufferHandle buffer_handle =
         frame->GetGpuMemoryBufferHandle();
 
