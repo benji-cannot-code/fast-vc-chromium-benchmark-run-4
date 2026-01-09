cfca53ee8205fe27e43c209fe47bfd9ddd97d420
Jacob Stanley <jacobstanley@google.com>
2026-01-08 19:52:25 -0800

    [Actor Overlay] Make magic cursor glow theme adaptive
    Screencasts
    Enabled: https://screencast.googleplex.com/cast/NTgzMTU3MzQ3NDYzOTg3MnwzODg5ODEyMi04Yw
    Enabled w/ Chrome Web Store theme: https://screencast.googleplex.com/cast/NTM2OTI3MDMwODQzODAxNnxhYjUyNTQzNS02Mg
    Disabled:
    https://screencast.googleplex.com/cast/NjA3NTQ2MjY1NTY3MjMyMHwyMTkxYWFjOS0xZg
    
    Bug: 473620443
    Change-Id: I02289ee89a4b17064bfb17b81d417fae080501f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7413011
    Reviewed-by: Christine Ying <chrstne@google.com>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Jacob Stanley <jacobstanley@google.com>
    Cr-Commit-Position: refs/heads/main@{#1566709}

--- chrome/browser/actor/ui/actor_overlay_handler.cc
+++ chrome/browser/actor/ui/actor_overlay_handler.cc
@@ -93,6 +93,8 @@ void ActorOverlayHandler::OnThemeChanged() {
         web_contents_->GetColorProvider().GetColor(kColorActorUiOverlayBorder);
     theme->border_glow_color = web_contents_->GetColorProvider().GetColor(
         kColorActorUiOverlayBorderGlow);
+    theme->magic_cursor_color =
+        web_contents_->GetColorProvider().GetColor(kColorActorUiMagicCursor);
 
     page_->SetTheme(std::move(theme));
   }
