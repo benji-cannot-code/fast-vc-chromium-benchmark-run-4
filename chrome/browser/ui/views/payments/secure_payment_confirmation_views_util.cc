d63759c4296b94146a4056c04e3b41a1e2fc2348
Darwin Yang <darwinyang@google.com>
2026-01-09 10:35:59 -0800

    [spc] Update payment strings for SPC UX Refresh
    Bug: 441560182
    Change-Id: I2d94583fa2c6f7112013d7b7882d67f900e27395
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7298526
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Commit-Queue: Darwin Yang <darwinyang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1567041}

--- chrome/browser/ui/views/payments/secure_payment_confirmation_views_util.cc
+++ chrome/browser/ui/views/payments/secure_payment_confirmation_views_util.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/theme_resources.h"
 #include "components/payments/core/features.h"
 #include "components/payments/core/sizes.h"
+#include "third_party/blink/public/common/features.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/compositor/layer.h"
@@ -156,7 +157,14 @@ std::unique_ptr<views::StyledLabel> CreateSecurePaymentConfirmationOptOutView(
   std::vector<size_t> offsets;
   std::u16string opt_out_text =
       base::ReplaceStringPlaceholders(opt_out_label, subst, &offsets);
-  DCHECK_EQ(2U, offsets.size());
+
+  // TODO: crbug.com/441560182 - This is a temporary workaround to ensure that
+  // tests are not broken. This should be removed when the new UX Refresh view
+  // is implemented.
+  if (!base::FeatureList::IsEnabled(
+          blink::features::kSecurePaymentConfirmationUxRefresh)) {
+    DCHECK_EQ(2U, offsets.size());
+  }
 
   views::StyledLabel::RangeStyleInfo link_style =
       views::StyledLabel::RangeStyleInfo::CreateForLink(on_click);
--- components/payments/content/secure_payment_confirmation_controller.h
+++ components/payments/content/secure_payment_confirmation_controller.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace payments {
 
 class PaymentRequest;
-class SecurePaymentConfirmationApp;
 
 // LINT.IfChange(SecurePaymentRequestOutcome)
 enum class SecurePaymentRequestOutcome {
@@ -75,8 +74,6 @@ class SecurePaymentConfirmationController
 
  private:
   void SetupModelAndShowDialogIfApplicable();
-  void CreateTransactionView(SecurePaymentConfirmationApp* app);
-  void CreateErrorView(SecurePaymentConfirmationApp* app);
 
   // Can be null when the webpage closes or the iframe refreshes or navigates.
   base::WeakPtr<PaymentRequest> request_;
