9834c8d3ec3b27960ea2115494393f4ea0dd784b
Ken Buchanan <kenrb@chromium.org>
2026-01-09 09:10:51 -0800

    [WebAuthn] Record the reason why passkey metadata cache is unused
    We have metrics indicating that most requests do not use the cache.
    
    This is probably fine, but knowing the reason why the cache is being
    skipped would help identify any problems if there are too many cases
    where we would rather be using it but currently are not.
    
    Also this removes a @VisibleForTesting annotation that a linter was
    complaining about.
    
    Change-Id: Ic58ae4b8346e2fc60c4a93d85a23767fa50bf184
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7424784
    Reviewed-by: Adem Derinel <derinel@google.com>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566987}

--- components/webauthn/android/java/src/org/chromium/components/webauthn/GmsCoreGetCredentialsHelper.java
+++ components/webauthn/android/java/src/org/chromium/components/webauthn/GmsCoreGetCredentialsHelper.java
@@ -9,8 +9,6 @@ import static org.chromium.components.webauthn.WebauthnLogger.log;
 
 import android.os.SystemClock;
 
-import androidx.annotation.VisibleForTesting;
-
 import com.google.android.gms.tasks.OnFailureListener;
 
 import org.chromium.base.ResettersForTesting;
@@ -49,6 +47,18 @@ public class GmsCoreGetCredentialsHelper {
 
     // LINT.ThenChange(//tools/metrics/histograms/webauthn/enums.xml)
 
+    // LINT.IfChange
+    private @interface GmsCoreSkippedCacheReason {
+        int GOOGLE_RP = 0;
+        int PAYMENT_REQUEST = 1;
+        int MODAL_ALLOW_LIST_REQUEST = 2;
+        int FEATURE_DISABLED = 3;
+        int OLD_GMSCORE_VERSION = 4;
+        int NUM_ENTRIES = 5;
+    }
+
+    // LINT.ThenChange(//tools/metrics/histograms/webauthn/enums.xml)
+
     /** The reason for a get credentials request. */
     public enum Reason {
         // A regular get assertion request for a non-Google relying party.
@@ -79,7 +89,6 @@ public class GmsCoreGetCredentialsHelper {
         return sInstance;
     }
 
-    @VisibleForTesting
     public static void overrideInstanceForTesting(GmsCoreGetCredentialsHelper helper) {
         sInstance = helper;
         ResettersForTesting.register(() -> sInstance = null);
@@ -135,6 +144,7 @@ public class GmsCoreGetCredentialsHelper {
                                         startTimeMs);
                             });
         } else {
+            recordNoCacheReason(reason);
             getCredentialsFromFido2Api(
                     authenticationContextProvider,
                     relyingPartyId,
@@ -214,5 +224,29 @@ public class GmsCoreGetCredentialsHelper {
                 return "";
         }
         return CREDENTIAL_FETCH_DURATION_HISTOGRAM + suffix;
+    }
+
+    private void recordNoCacheReason(Reason requestReason) {
+        @GmsCoreSkippedCacheReason int reason;
+        if (requestReason == Reason.GET_ASSERTION_GOOGLE_RP) {
+            reason = GmsCoreSkippedCacheReason.GOOGLE_RP;
+        } else if (requestReason == Reason.PAYMENT
+                || requestReason == Reason.GET_MATCHING_CREDENTIAL_IDS) {
+            reason = GmsCoreSkippedCacheReason.PAYMENT_REQUEST;
+        } else if (requestReason == Reason.CHECK_FOR_MATCHING_CREDENTIALS) {
+            reason = GmsCoreSkippedCacheReason.MODAL_ALLOW_LIST_REQUEST;
+        } else if (!GmsCoreUtils.isPasskeyCacheSupported()) {
+            reason = GmsCoreSkippedCacheReason.OLD_GMSCORE_VERSION;
+        } else if (!WebauthnFeatureMap.getInstance()
+                .isEnabled(WebauthnFeatures.WEBAUTHN_ANDROID_PASSKEY_CACHE_MIGRATION)) {
+            reason = GmsCoreSkippedCacheReason.FEATURE_DISABLED;
+        } else {
+            assert false : "Update metric to cover unknown reason to not use the cache";
+            return;
+        }
+        RecordHistogram.recordEnumeratedHistogram(
+                "WebAuthentication.Android.GmsCoreSkippedCacheReason",
+                reason,
+                GmsCoreSkippedCacheReason.NUM_ENTRIES);
     }
 }
