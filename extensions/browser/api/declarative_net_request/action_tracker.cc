94a9e0b08f90dc62d25c880f8109002be2ff6859
Kelvin Jiang <kelvinjiang@chromium.org>
2026-01-09 14:38:51 -0800

    [DNR] Remove ActionTracker destructor DCHECK
    Remove a DCHECK in ActionTracker's destructor. This DCHECK assumes that
    by the time ActionTracker (owned by a keyed service factory) is
    destroyed (usually when the browser is shutting down then all extensions
    have already been unloaded and OnExtensionUnloaded has been dispatched.
    
    ^This is actually not the case.
    
    The DCHECK itself doesn't have any value being here because it's normal
    if there are unfinished navigation requests in the browser at the time
    of shutdown and those requests can match against extensions' DNR rules.
    
    Fixed: 474043327
    Change-Id: If5865220ef30714ff1a3734d8a8cdf40444b73ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7417270
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Auto-Submit: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1567224}

--- extensions/browser/api/declarative_net_request/action_tracker.cc
+++ extensions/browser/api/declarative_net_request/action_tracker.cc
@@ -103,9 +103,7 @@ ActionTracker::ActionTracker(content::BrowserContext* browser_context)
   StartTrimRulesTask();
 }
 
-ActionTracker::~ActionTracker() {
-  DCHECK(pending_navigation_actions_.empty());
-}
+ActionTracker::~ActionTracker() = default;
 
 void ActionTracker::SetClockForTests(const base::Clock* clock) {
   g_test_clock = clock;
