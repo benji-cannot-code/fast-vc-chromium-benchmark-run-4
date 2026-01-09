e2722a46043dee33319f5bc9eba565257846eb94
Jan Keitel <jkeitel@google.com>
2026-01-09 08:13:00 -0800

    Use span-based file API in zlib's zip_writer.cc
    No functionality changes.
    
    Bug: 435317390
    Change-Id: Ifa7d1a92eeb953e8d74fa25403a2e1695b2f64ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7424147
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566955}

--- third_party/zlib/google/zip_writer.cc
+++ third_party/zlib/google/zip_writer.cc
@@ -8,8 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <tuple>
 
+#include "base/containers/span.h"
 #include "base/files/file.h"
 #include "base/logging.h"
+#include "base/numerics/safe_conversions.h"
 #include "base/strings/strcat.h"
 #include "base/strings/string_util.h"
 #include "third_party/zlib/google/redact.h"
@@ -35,27 +37,29 @@ bool ZipWriter::ShouldContinue() {
 }
 
 bool ZipWriter::AddFileContent(const base::FilePath& path, base::File file) {
-  char buf[zip::internal::kZipBufSize];
+  uint8_t buf[zip::internal::kZipBufSize];
 
   while (ShouldContinue()) {
-    const int num_bytes =
-        file.ReadAtCurrentPos(buf, zip::internal::kZipBufSize);
+    const std::optional<size_t> num_bytes = file.ReadAtCurrentPos(buf);
 
-    if (num_bytes < 0) {
+    if (!num_bytes) {
       PLOG(ERROR) << "Cannot read file " << Redact(path);
       return false;
     }
 
-    if (num_bytes == 0)
+    if (*num_bytes == 0) {
       return true;
+    }
 
-    if (zipWriteInFileInZip(zip_file_, buf, num_bytes) != ZIP_OK) {
+    if (zipWriteInFileInZip(zip_file_, buf,
+                            base::checked_cast<unsigned int>(*num_bytes)) !=
+        ZIP_OK) {
       PLOG(ERROR) << "Cannot write data from file " << Redact(path)
                   << " to ZIP";
       return false;
     }
 
-    progress_.bytes += num_bytes;
+    progress_.bytes += *num_bytes;
   }
 
   return false;
