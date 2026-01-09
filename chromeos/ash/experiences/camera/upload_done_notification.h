fbcbc47f459c7de079bbd819cbf2947433ab3d82
Orko Garai <orko@igalia.com>
2026-01-09 07:45:29 -0800

    CCA: Upload Done notification
    Implement the thumbnail system notification that is displayed once a
    camera file is successfully uploaded to OneDrive or Google Drive.
    
    Design Doc:
    https://docs.google.com/document/d/1wod7fE1aU4D0boQxx9dxgNsAvJXvBPfvc8XFAmjtFuQ/edit?usp=sharing
    
    Bug: 454152412
    
    Change-Id: I5e0c73c762b717ab40ee8af0320046b650c2ea00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7256063
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Reviewed-by: Aida Zolić <aidazolic@chromium.org>
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    Commit-Queue: Orko Garai <orko@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1566941}

---
+++ chromeos/ash/experiences/camera/upload_done_notification.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_ASH_EXPERIENCES_CAMERA_UPLOAD_DONE_NOTIFICATION_H_
+#define CHROMEOS_ASH_EXPERIENCES_CAMERA_UPLOAD_DONE_NOTIFICATION_H_
+
+#include "base/functional/callback_forward.h"
+
+namespace base {
+class FilePath;
+}
+
+namespace gfx {
+class Image;
+}
+
+void CreateUploadDoneNotification(bool onedrive,
+                                  const gfx::Image& thumbnail,
+                                  const base::FilePath& file_path,
+                                  base::RepeatingClosure edit_callback,
+                                  base::RepeatingClosure delete_callback);
+
+#endif  // CHROMEOS_ASH_EXPERIENCES_CAMERA_UPLOAD_DONE_NOTIFICATION_H_
