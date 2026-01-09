29c2f287cee500a0b73100cf93fdb0dce46a15ac
Jan Keitel <jkeitel@google.com>
2026-01-09 07:35:36 -0800

    Use span-based file API in seven_zip_reader.cc
    No functionality changes.
    
    Bug: 435317390
    Change-Id: Id7762224a00f34e4373c8a5ed22e80410019d09f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7420730
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566937}

--- third_party/lzma_sdk/google/seven_zip_reader.cc
+++ third_party/lzma_sdk/google/seven_zip_reader.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/containers/buffer_iterator.h"
 #include "base/containers/heap_array.h"
+#include "base/containers/span.h"
 #include "base/containers/span_writer.h"
 #include "base/files/memory_mapped_file.h"
 #include "base/functional/bind.h"
@@ -154,10 +155,12 @@ SRes FileSeekInStream::DoRead(const ISeekInStream* p, void* buf, size_t* size) {
   // so we cast away the const to do this.
   auto* stream =
       const_cast<FileSeekInStream*>(static_cast<const FileSeekInStream*>(p));
-  int res = stream->file_.ReadAtCurrentPos(static_cast<char*>(buf), *size);
-  if (res < 0)
+  std::optional<size_t> res = stream->file_.ReadAtCurrentPos(
+      base::span(static_cast<uint8_t*>(buf), *size));
+  if (!res.has_value()) {
     return SZ_ERROR_READ;
-  *size = res;
+  }
+  *size = *res;
   return SZ_OK;
 }
 
