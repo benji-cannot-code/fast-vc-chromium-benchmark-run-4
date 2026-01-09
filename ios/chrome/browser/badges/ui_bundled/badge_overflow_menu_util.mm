568cb2a384c3d49fef18284a55b0ff3924759149
Chris Lu <thegreenfrog@google.com>
2026-01-08 16:40:21 -0800

    [ios] extend metrics and remove expired ones
    OBSOLETE_HISTOGRAM=unused histograms
    
    Fixed: 470979745, 463370761
    Change-Id: I6a0e0cf1e3a54779ade702da7406151f285e0c7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7304864
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Reviewed-by: Sebastien S-G <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566630}

--- ios/chrome/browser/badges/ui_bundled/badge_overflow_menu_util.mm
+++ ios/chrome/browser/badges/ui_bundled/badge_overflow_menu_util.mm
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/notreached.h"
 #import "components/strings/grit/components_strings.h"
 #import "ios/chrome/browser/badges/ui_bundled/badge_constants.h"
-#import "ios/chrome/browser/badges/ui_bundled/badges_histograms.h"
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ui/base/l10n/l10n_util.h"
@@ -32,7 +31,6 @@ UIAction* GetOverflowMenuElementForBadgeType(
   NSString* title;
   UIActionIdentifier action_identifier;
   UIImage* image;
-  MobileMessagesInfobarType histogram_type = MobileMessagesInfobarType::Confirm;
 
   switch (badge_type) {
     case kBadgeTypePasswordSave:
@@ -40,14 +38,12 @@ UIAction* GetOverflowMenuElementForBadgeType(
       title =
           l10n_util::GetNSString(IDS_IOS_PASSWORD_MANAGER_SAVE_PASSWORD_TITLE);
       image = GetPasswordImage();
-      histogram_type = MobileMessagesInfobarType::SavePassword;
       break;
     case kBadgeTypePasswordUpdate:
       action_identifier = kBadgeButtonUpdatePasswordActionIdentifier;
       title = l10n_util::GetNSString(
           IDS_IOS_PASSWORD_MANAGER_UPDATE_PASSWORD_TITLE);
       image = GetPasswordImage();
-      histogram_type = MobileMessagesInfobarType::UpdatePassword;
       break;
     case kBadgeTypeSaveAddressProfile:
       action_identifier = kBadgeButtonSaveAddressProfileActionIdentifier;
@@ -55,14 +51,12 @@ UIAction* GetOverflowMenuElementForBadgeType(
           l10n_util::GetNSString(IDS_IOS_AUTOFILL_SAVE_ADDRESS_PROMPT_TITLE);
       image =
           CustomSymbolWithPointSize(kLocationSymbol, kInfobarSymbolPointSize);
-      histogram_type = MobileMessagesInfobarType::AutofillSaveAddressProfile;
       break;
     case kBadgeTypeSaveCard:
       action_identifier = kBadgeButtonSaveCardActionIdentifier;
       title = l10n_util::GetNSString(IDS_IOS_AUTOFILL_SAVE_CARD);
       image = DefaultSymbolWithPointSize(kCreditCardSymbol,
                                          kInfobarSymbolPointSize);
-      histogram_type = MobileMessagesInfobarType::SaveCard;
       break;
     case kBadgeTypeTranslate:
       action_identifier = kBadgeButtonTranslateActionIdentifier;
@@ -75,7 +69,6 @@ UIAction* GetOverflowMenuElementForBadgeType(
       title = l10n_util::GetNSString(
           IDS_IOS_PERMISSIONS_INFOBAR_OVERFLOW_POPUP_TITLE);
       image = CustomSymbolWithPointSize(kCameraSymbol, kInfobarSymbolPointSize);
-      histogram_type = MobileMessagesInfobarType::Permissions;
       break;
     case kBadgeTypePermissionsMicrophone:
       action_identifier = kBadgeButtonPermissionsActionIdentifier;
@@ -83,7 +76,6 @@ UIAction* GetOverflowMenuElementForBadgeType(
           IDS_IOS_PERMISSIONS_INFOBAR_OVERFLOW_POPUP_TITLE);
       image = DefaultSymbolTemplateWithPointSize(kMicrophoneSymbol,
                                                  kInfobarSymbolPointSize);
-      histogram_type = MobileMessagesInfobarType::Permissions;
       break;
     case kBadgeTypeReaderMode:
       NOTREACHED() << "Reader Mode badge should not be in overflow menu";
@@ -96,8 +88,6 @@ UIAction* GetOverflowMenuElementForBadgeType(
   }
 
   UIActionHandler handler = ^(UIAction* action) {
-    base::UmaHistogramEnumeration(kInfobarOverflowMenuTappedHistogram,
-                                  histogram_type);
     show_modal_function(badge_type);
   };
 
--- ios/chrome/browser/translate/model/translate_infobar_metrics_recorder.mm
+++ ios/chrome/browser/translate/model/translate_infobar_metrics_recorder.mm
@@ -27,9 +27,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                             presentEvent);
 }
 
-+ (void)recordUnusedInfobar {
-  // TODO(crbug.com/40107868): Use function version of macros.
-  UMA_HISTOGRAM_COUNTS_10M("Mobile.Translate.Unused.Count", 1);
-}
-
 @end
