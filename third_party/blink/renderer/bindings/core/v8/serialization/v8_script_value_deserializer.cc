d781aa855e45051e9e4af10771474db28244c392
Reilly Grant <reillyg@chromium.org>
2026-01-08 20:48:01 -0800

    Support serializing QuotaExceededError
    When QuotaExceededError was created as a subclass of DOMException the
    associated serialization and deserialization code was not added so while
    DOMException remained serializable the new QuotaExceededError subclass
    was not.
    
    Add the missing code so that the structured clone tests for
    QuotaExceededError (added by the Servo project based on the
    specification) pass.
    
    Change-Id: Id20c8bf2207a2672aef2081f29aafcfa01a3cbf7
    Fixed: 439280321
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7397562
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566726}

--- third_party/blink/renderer/bindings/core/v8/serialization/v8_script_value_deserializer.cc
+++ third_party/blink/renderer/bindings/core/v8/serialization/v8_script_value_deserializer.cc
@@ -39,11 +39,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/v8_message_port.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_mojo_handle.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_offscreen_canvas.h"
+#include "third_party/blink/renderer/bindings/core/v8/v8_quota_exceeded_error.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_readable_stream.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_throw_dom_exception.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_transform_stream.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_writable_stream.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
+#include "third_party/blink/renderer/core/dom/quota_exceeded_error.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/fileapi/blob.h"
 #include "third_party/blink/renderer/core/fileapi/file.h"
@@ -803,6 +805,20 @@ ScriptWrappable* V8ScriptValueDeserializer::ReadDOMObject(
       // DOMException::Create takes its arguments in the opposite order.
       return DOMException::Create(message, name);
     }
+    case kQuotaExceededErrorTag: {
+      // See the serialization side for |stack_unused|.
+      String message, stack_unused;
+      uint32_t has_quota, has_requested;
+      double quota, requested;
+      if (!ReadUTF8String(&message) || !ReadUTF8String(&stack_unused) ||
+          !ReadUint32(&has_quota) || !ReadDouble(&quota) ||
+          !ReadUint32(&has_requested) || !ReadDouble(&requested)) {
+        return nullptr;
+      }
+      return QuotaExceededError::Create(
+          message, has_quota ? std::make_optional(quota) : std::nullopt,
+          has_requested ? std::make_optional(requested) : std::nullopt);
+    }
     case kFencedFrameConfigTag: {
       String url_string, shared_storage_context, urn_uuid_string;
       uint32_t has_shared_storage_context, has_container_size, container_width,
@@ -1077,6 +1093,8 @@ bool V8ScriptValueDeserializer::ExecutionContextExposesInterface(
     }
     case kDOMExceptionTag:
       return V8DOMException::IsExposed(execution_context);
+    case kQuotaExceededErrorTag:
+      return V8QuotaExceededError::IsExposed(execution_context);
     case kFencedFrameConfigTag:
       return V8FencedFrameConfig::IsExposed(execution_context);
     default:
