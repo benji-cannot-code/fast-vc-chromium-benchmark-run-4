b0d33797ebf18bd412b0729f83870f2ea6431273
Maggie Jennings <mjenn@google.com>
2026-01-08 16:21:36 -0800

    Notify on actor task stopped when TaskIndicator feature disabled
    Before: https://screencast.googleplex.com/cast/NTMyNTM1Mzg2NTE4MzIzMnxkZjVjM2I3Yy03Ng
    After: https://screencast.googleplex.com/cast/NjY1NjIyMTI4ODMzMzMxMnwyNzE0YjM2ZC0xYQ
    
    Bug: 474097558
    Change-Id: I839e71e6269d5fa3dbd9be7848eebe0be1562336
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7408801
    Commit-Queue: Maggie Jennings <mjenn@google.com>
    Auto-Submit: Maggie Jennings <mjenn@google.com>
    Reviewed-by: Christine Ying <chrstne@google.com>
    Commit-Queue: Christine Ying <chrstne@google.com>
    Cr-Commit-Position: refs/heads/main@{#1566617}

--- chrome/browser/actor/ui/actor_ui_state_manager.cc
+++ chrome/browser/actor/ui/actor_ui_state_manager.cc
@@ -163,6 +163,8 @@ void ActorUiStateManager::OnActorTaskStateChange(
       if (base::FeatureList::IsEnabled(
               features::kGlicActorUiGlobalTaskIndicator)) {
         LOG(FATAL) << "Stopped states should be processed via StopTask event.";
+      } else {
+        NotifyActorTaskStopped(task_id);
       }
       break;
   }
