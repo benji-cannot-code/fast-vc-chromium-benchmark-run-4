73bffd7e9ebbc28610a28e62ce650f1168103f6d
Chris Fredrickson <cfredric@chromium.org>
2026-01-09 11:57:57 -0800

    [glic] Rework ShouldBlockNavigationUrlForOriginGating function
    This renames the function to better describe its semantics, since the
    bool that it returns does not actually indicate whether the navigation
    should be blocked. This CL also adds a header comment that describes
    what the return value means, removes the now-redundant comment from
    ExecutionEngine, and reorganizes the implementation to bail out more
    quickly when the corresponding feature is disabled (and avoid an
    unnecessary function declaration).
    
    Change-Id: I804b65b67b275aa14155b971fe0e6267731a77ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7416802
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1567114}

--- chrome/browser/actor/site_policy_unittest.cc
+++ chrome/browser/actor/site_policy_unittest.cc
@@ -199,7 +199,7 @@ TEST_F(ActorSitePolicyTest, BlockIfInBlocklist) {
 
 TEST_F(ActorSitePolicyTest, AllowIfNotBlockedForOriginGating) {
   base::test::TestFuture<bool> got_may_act;
-  EXPECT_TRUE(ShouldBlockNavigationUrlForOriginGating(
+  EXPECT_TRUE(MaybeCheckOptimizationGuideForSensitiveUrl(
       GURL("https://c.test/"), profile(), got_may_act.GetCallback()));
   EXPECT_TRUE(got_may_act.Get());
 }
@@ -209,7 +209,7 @@ TEST_F(ActorSitePolicyTest, BlockIfInBlocklistForOriginGating) {
   SetExpectedOptimizationGuideCall(
       url, optimization_guide::OptimizationGuideDecision::kFalse);
   base::test::TestFuture<bool> got_may_act;
-  EXPECT_TRUE(ShouldBlockNavigationUrlForOriginGating(
+  EXPECT_TRUE(MaybeCheckOptimizationGuideForSensitiveUrl(
       url, profile(), got_may_act.GetCallback()));
   EXPECT_FALSE(got_may_act.Get());
 }
