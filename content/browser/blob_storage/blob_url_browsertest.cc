de9ed0e509f425da3374a9ceca4325e21febe691
Chris Fredrickson <cfredric@chromium.org>
2026-01-09 11:23:11 -0800

    Add support for parsing JSON to base::test::IsSupersetOfValue
    This makes IsSupersetOfValue support the same type signatures as IsJson,
    which makes those matchers more consistent with each other and allows
    tests to use JSON literals directly in a more readable way.
    
    Change-Id: I5df7c458f02b5ca8604f06fd5b56c5b5dd5f0fb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7412850
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1567087}

--- content/browser/blob_storage/blob_url_browsertest.cc
+++ content/browser/blob_storage/blob_url_browsertest.cc
@@ -480,9 +480,8 @@ class BlobUrlDevToolsIssueTest : public ContentBrowserTest {
           return issue_code && *issue_code == "PartitioningBlobURLIssue";
         }));
 
-    EXPECT_THAT(params,
-                base::test::IsSupersetOfValue(base::test::ParseJson(JsReplace(
-                    R"({
+    EXPECT_THAT(params, base::test::IsSupersetOfValue(JsReplace(
+                            R"({
                   "issue": {
                     "code": "PartitioningBlobURLIssue",
                     "details": {
@@ -493,7 +492,7 @@ class BlobUrlDevToolsIssueTest : public ContentBrowserTest {
                     }
                   }
                 })",
-                    url, expected_info_enum))));
+                            url, expected_info_enum)));
 
     // Clear existing notifications so subsequent calls don't fail by checking
     // `url` against old notifications.
