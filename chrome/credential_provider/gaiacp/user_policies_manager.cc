00113958095baafacb5621dfc14559ae5560336e
Jan Keitel <jkeitel@google.com>
2026-01-09 12:23:43 -0800

    Use span-based file API in user_policies_manager.cc
    No functionality changes.
    
    Bug: 435317390
    Change-Id: I599b32d83b708e72f2543fd89bd6ffb0f20b6d69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7423623
    Reviewed-by: Yusuf Sengul <yusufsn@google.com>
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Yusuf Sengul <yusufsn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1567133}

--- chrome/credential_provider/gaiacp/user_policies_manager.cc
+++ chrome/credential_provider/gaiacp/user_policies_manager.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 
 #include "base/compiler_specific.h"
+#include "base/containers/span.h"
 #include "base/files/file.h"
 #include "base/files/file_enumerator.h"
 #include "base/files/file_path.h"
@@ -252,17 +253,12 @@ HRESULT UserPoliciesManager::FetchAndStorePolicies(
     return (fetch_status_ = E_FAIL);
   }
 
-  int num_bytes_written = UNSAFE_TODO(
-      policy_file->Write(0, policy_data.c_str(), policy_data.size()));
-
-  policy_file.reset();
-
-  if (size_t(num_bytes_written) != policy_data.size()) {
-    LOGFN(ERROR) << "Failed writing policy data to file! Only "
-                 << num_bytes_written << " bytes written out of "
-                 << policy_data.size();
+  if (!policy_file->WriteAndCheck(0, base::as_byte_span(policy_data))) {
+    LOGFN(ERROR) << "Failed writing policy data to file!";
     return (fetch_status_ = E_FAIL);
   }
+
+  policy_file.reset();
 
   base::Time fetch_time = base::Time::Now();
   std::wstring fetch_time_millis = base::NumberToWString(
@@ -286,7 +282,10 @@ bool UserPoliciesManager::GetUserPolicies(const std::wstring& sid,
   }
 
   std::vector<uint8_t> buffer(policy_file->GetLength());
-  policy_file->Read(0, buffer);
+  if (!policy_file->ReadAndCheck(0, buffer)) {
+    LOGFN(ERROR) << "Failed to read policy data from file!";
+    return false;
+  }
   policy_file.reset();
 
   std::optional<base::Value::Dict> policy_data = base::JSONReader::ReadDict(
