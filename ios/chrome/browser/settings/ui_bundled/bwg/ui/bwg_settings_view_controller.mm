52c4479f1a505d9b3e0017241f4434622a59f79f
Joe Peplowski <joepeplowski@google.com>
2026-01-09 08:01:50 -0800

    Add metrics for Gemini dynamic settings
    This change introduces metrics to track user interaction with the Gemini
    dynamic settings UI to help assess the visibility and usage of
    individual settings.
    
    `gemini_settings_metrics.{h/mm}` was added to log views and interactions
    with Gemini Dynamic settings. Note that:
    * Row views are logged to Histograms
    * Row taps are logged to both Histograms and User Actions
    
    Additionally, to consolidate settings-related functionality, relevant
    metrics are moved from `intelligence/bwg/metrics/gemini_metrics.{h/mm}`
    to `settings/ui_bundled/bwg/metrics/gemini_settings_metrics.{h/mm}`
    
    Bug: 473512366
    Change-Id: I161104f9bdbd0148bc7253e3fa0bbf34b5116191
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7361565
    Reviewed-by: Sebastien S-G <sebsg@chromium.org>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Joe Peplowski <joepeplowski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1566950}

--- ios/chrome/browser/settings/ui_bundled/bwg/ui/bwg_settings_view_controller.mm
+++ ios/chrome/browser/settings/ui_bundled/bwg/ui/bwg_settings_view_controller.mm
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/settings/ui_bundled/bwg/ui/bwg_settings_view_controller.h"
 
 #import "base/apple/foundation_util.h"
-#import "ios/chrome/browser/intelligence/bwg/metrics/gemini_metrics.h"
 #import "ios/chrome/browser/intelligence/features/features.h"
 #import "ios/chrome/browser/net/model/crurl.h"
 #import "ios/chrome/browser/settings/ui_bundled/bwg/coordinator/bwg_settings_mutator.h"
@@ -16,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/settings/ui_bundled/bwg/model/gemini_settings_context.h"
 #import "ios/chrome/browser/settings/ui_bundled/bwg/model/gemini_settings_metadata.h"
 #import "ios/chrome/browser/settings/ui_bundled/bwg/ui/bwg_location_view_controller.h"
+#import "ios/chrome/browser/settings/ui_bundled/bwg/utils/gemini_settings_metrics.h"
 #import "ios/chrome/browser/shared/model/url/chrome_url_constants.h"
 #import "ios/chrome/browser/shared/ui/table_view/cells/table_view_detail_text_item.h"
 #import "ios/chrome/browser/shared/ui/table_view/cells/table_view_link_header_footer_item.h"
@@ -178,10 +178,12 @@ NSString* const kPageContentSharingAction = @"PageContentSharingAction";
         toSectionWithIdentifier:SectionIdentifierActivity];
     [model setFooter:BWGAppActivityFooterItem
         forSectionWithIdentifier:SectionIdentifierActivity];
+    RecordGeminiSettingsItemShown(IOSGeminiSettingsItem::kGeminiAppsActivity);
 
     [model addSectionWithIdentifier:SectionIdentifierExtensions];
     [model addItem:[self BWGExtensionsItem]
         toSectionWithIdentifier:SectionIdentifierExtensions];
+    RecordGeminiSettingsItemShown(IOSGeminiSettingsItem::kExtensions);
   }
 }
 
@@ -196,6 +198,43 @@ NSString* const kPageContentSharingAction = @"PageContentSharingAction";
 }
 
 #pragma mark - Private
+
+- (void)recordItemShownForContext:(GeminiSettingsContext)context {
+  switch (context) {
+    case GeminiSettingsContextGeminiAppsActivity:
+      RecordGeminiSettingsItemShown(IOSGeminiSettingsItem::kGeminiAppsActivity);
+      break;
+    case GeminiSettingsContextPersonalization:
+      RecordGeminiSettingsItemShown(IOSGeminiSettingsItem::kPersonalization);
+      break;
+    case GeminiSettingsContextExtensions:
+      RecordGeminiSettingsItemShown(IOSGeminiSettingsItem::kExtensions);
+      break;
+    default:
+      RecordGeminiSettingsItemShown(IOSGeminiSettingsItem::kUnknown);
+      break;
+  }
+}
+
+- (void)recordItemUsedForContext:(GeminiSettingsContext)context {
+  switch (context) {
+    case GeminiSettingsContextGeminiAppsActivity:
+      RecordGeminiSettingsItemUsed(IOSGeminiSettingsItem::kGeminiAppsActivity);
+      RecordGeminiSettingsAppsActivity();
+      break;
+    case GeminiSettingsContextPersonalization:
+      RecordGeminiSettingsItemUsed(IOSGeminiSettingsItem::kPersonalization);
+      RecordGeminiSettingsPersonalization();
+      break;
+    case GeminiSettingsContextExtensions:
+      RecordGeminiSettingsItemUsed(IOSGeminiSettingsItem::kExtensions);
+      RecordGeminiSettingsExtensions();
+      break;
+    default:
+      RecordGeminiSettingsItemUsed(IOSGeminiSettingsItem::kUnknown);
+      break;
+  }
+}
 
 // Creates a multi detail item with multiple options.
 - (TableViewMultiDetailTextItem*)detailItemWithType:(NSInteger)type
@@ -310,12 +349,14 @@ NSString* const kPageContentSharingAction = @"PageContentSharingAction";
 
   if ([self.tableViewModel itemTypeForIndexPath:indexPath] ==
       ItemTypeAppActivity) {
-    RecordGeminiSettingsAppActivity();
+    RecordGeminiSettingsItemUsed(IOSGeminiSettingsItem::kGeminiAppsActivity);
+    RecordGeminiSettingsAppsActivity();
     [self.mutator openNewTabWithURL:GURL(kBWGAppActivityURL)];
   }
 
   if ([self.tableViewModel itemTypeForIndexPath:indexPath] ==
       ItemTypeExtensions) {
+    RecordGeminiSettingsItemUsed(IOSGeminiSettingsItem::kExtensions);
     RecordGeminiSettingsExtensions();
     [self.mutator openNewTabWithURL:GURL(kBWGExtensionsURL)];
   }
@@ -346,6 +387,8 @@ NSString* const kPageContentSharingAction = @"PageContentSharingAction";
         case GeminiSettingsActionTypeUnknown:
           break;
       }
+
+      [self recordItemUsedForContext:dynamicSettingsItem.metadata.context];
     }
   }
 
@@ -406,6 +449,8 @@ NSString* const kPageContentSharingAction = @"PageContentSharingAction";
 
   // Add a new section, item and optional footer for each dynamic setting.
   for (GeminiDynamicSettingsItem* newItem in newItems) {
+    [self recordItemShownForContext:newItem.metadata.context];
+
     NSInteger settingIdentifier = newItem.type;
 
     [self.tableViewModel addSectionWithIdentifier:settingIdentifier];
---
+++ ios/chrome/browser/settings/ui_bundled/bwg/utils/gemini_settings_metrics.mm
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/settings/ui_bundled/bwg/utils/gemini_settings_metrics.h"
+
+#import "base/metrics/histogram_functions.h"
+#import "base/metrics/user_metrics.h"
+#import "base/metrics/user_metrics_action.h"
+
+void RecordGeminiSettingsOpened() {
+  base::RecordAction(base::UserMetricsAction("MobileGeminiSettingsOpened"));
+}
+
+void RecordGeminiSettingsClose() {
+  base::RecordAction(base::UserMetricsAction("MobileGeminiSettingsClose"));
+}
+
+void RecordGeminiSettingsBack() {
+  base::RecordAction(base::UserMetricsAction("MobileGeminiSettingsBack"));
+}
+
+void RecordGeminiSettingsAppsActivity() {
+  base::RecordAction(
+      base::UserMetricsAction("Settings.GeminiSettings.GeminiAppsActivity"));
+}
+
+void RecordGeminiSettingsExtensions() {
+  base::RecordAction(
+      base::UserMetricsAction("Settings.GeminiSettings.GeminiExtensions"));
+}
+
+void RecordGeminiSettingsPersonalization() {
+  base::RecordAction(
+      base::UserMetricsAction("Settings.GeminiSettings.GeminiPersonalization"));
+}
+
+void RecordGeminiSettingsItemShown(IOSGeminiSettingsItem item) {
+  base::UmaHistogramEnumeration("IOS.Gemini.DynamicSettings.ItemShown", item);
+}
+
+void RecordGeminiSettingsItemUsed(IOSGeminiSettingsItem item) {
+  base::UmaHistogramEnumeration("IOS.Gemini.DynamicSettings.ItemUsed", item);
+}
