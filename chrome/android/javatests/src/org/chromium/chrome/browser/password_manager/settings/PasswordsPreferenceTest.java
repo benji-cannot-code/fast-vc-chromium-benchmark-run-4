f943adccc3c3f6de87bf19a040616facf0acf8f2
Henrique Nakashima <hnakashima@chromium.org>
2026-01-09 14:41:18 -0800

    [Android] Disable SettingsMultiColumn again in PasswordsPreferenceTest
    Test is getting in a weird state with the flag enabled.
    
    Bug: 474421208
    Change-Id: I283d4257d930cc95c8c0cbc2174de0cc0a800217
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7425589
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Peilin Wang <peilinwang@google.com>
    Reviewed-by: Peilin Wang <peilinwang@google.com>
    Auto-Submit: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1567228}

--- chrome/android/javatests/src/org/chromium/chrome/browser/password_manager/settings/PasswordsPreferenceTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/password_manager/settings/PasswordsPreferenceTest.java
@@ -23,7 +23,9 @@ import org.chromium.base.test.transit.TransitAsserts;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.Feature;
+import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Restriction;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.password_manager.LoginDbDeprecationUtilBridge;
 import org.chromium.chrome.browser.password_manager.LoginDbDeprecationUtilBridgeJni;
@@ -50,6 +52,7 @@ import java.io.IOException;
         reason =
                 "The tests can't be batched because the functionality under test is set up during"
                         + " Chrome start up.")
+@DisableFeatures(ChromeFeatureList.SETTINGS_MULTI_COLUMN)
 public class PasswordsPreferenceTest {
     private static final int RENDER_TEST_REVISION = 2;
 
