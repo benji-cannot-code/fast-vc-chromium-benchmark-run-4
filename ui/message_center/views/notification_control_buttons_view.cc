851acddcd14fd44e9aa68006c2e03081a02af236
Hidehiko Abe <hidehiko@chromium.org>
2026-01-08 18:45:29 -0800

    Stabilize AshNotificationViewPixelTest.CloseButtonFocused
    The test takes diff of pixels, but the focused UI has tooltip
    which makes the test flaky.
    This CL introduces a switch to disable tooltip for testing
    stability.
    
    BUG=b:442861184
    TEST=Tryjob
    
    Change-Id: I9bfda6a85bca7a05730ba19d441a3fc9e443d584
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7364662
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566688}

--- ui/message_center/views/notification_control_buttons_view.cc
+++ ui/message_center/views/notification_control_buttons_view.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/message_center/views/notification_control_buttons_view.h"
 
 #include <memory>
+#include <optional>
 
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -28,6 +29,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace message_center {
 
+namespace {
+std::optional<bool> g_tooltip_enabled_for_testing;
+}  // namespace
+
 NotificationControlButtonsView::NotificationControlButtonsView(
     MessageView* message_view)
     : message_view_(message_view) {
@@ -43,6 +48,15 @@ NotificationControlButtonsView::NotificationControlButtonsView(
 }
 
 NotificationControlButtonsView::~NotificationControlButtonsView() = default;
+
+// static
+base::AutoReset<std::optional<bool>>
+NotificationControlButtonsView::SetTooltipEnabledForTesting(bool value) {
+  CHECK(!g_tooltip_enabled_for_testing.has_value());
+  base::AutoReset<std::optional<bool>> result(&g_tooltip_enabled_for_testing,
+                                              value);
+  return result;
+}
 
 void NotificationControlButtonsView::OnThemeChanged() {
   views::View::OnThemeChanged();
@@ -63,8 +77,10 @@ void NotificationControlButtonsView::ShowCloseButton(bool show) {
     }
     close_button_->GetViewAccessibility().SetName(l10n_util::GetStringUTF16(
         IDS_MESSAGE_CENTER_CLOSE_NOTIFICATION_BUTTON_ACCESSIBLE_NAME));
-    close_button_->SetTooltipText(l10n_util::GetStringUTF16(
-        IDS_MESSAGE_CENTER_CLOSE_NOTIFICATION_BUTTON_TOOLTIP));
+    if (g_tooltip_enabled_for_testing.value_or(true)) {
+      close_button_->SetTooltipText(l10n_util::GetStringUTF16(
+          IDS_MESSAGE_CENTER_CLOSE_NOTIFICATION_BUTTON_TOOLTIP));
+    }
     close_button_->SetBackground(
         views::CreateSolidBackground(SK_ColorTRANSPARENT));
     DeprecatedLayoutImmediately();
@@ -92,8 +108,10 @@ void NotificationControlButtonsView::ShowSettingsButton(bool show) {
     }
     settings_button_->GetViewAccessibility().SetName(l10n_util::GetStringUTF16(
         IDS_MESSAGE_NOTIFICATION_SETTINGS_BUTTON_ACCESSIBLE_NAME));
-    settings_button_->SetTooltipText(l10n_util::GetStringUTF16(
-        IDS_MESSAGE_NOTIFICATION_SETTINGS_BUTTON_ACCESSIBLE_NAME));
+    if (g_tooltip_enabled_for_testing.value_or(true)) {
+      settings_button_->SetTooltipText(l10n_util::GetStringUTF16(
+          IDS_MESSAGE_NOTIFICATION_SETTINGS_BUTTON_ACCESSIBLE_NAME));
+    }
     settings_button_->SetBackground(
         views::CreateSolidBackground(SK_ColorTRANSPARENT));
     DeprecatedLayoutImmediately();
@@ -120,8 +138,10 @@ void NotificationControlButtonsView::ShowSnoozeButton(bool show) {
     }
     snooze_button_->GetViewAccessibility().SetName(l10n_util::GetStringUTF16(
         IDS_MESSAGE_CENTER_NOTIFICATION_SNOOZE_BUTTON_TOOLTIP));
-    snooze_button_->SetTooltipText(l10n_util::GetStringUTF16(
-        IDS_MESSAGE_CENTER_NOTIFICATION_SNOOZE_BUTTON_TOOLTIP));
+    if (g_tooltip_enabled_for_testing.value_or(true)) {
+      snooze_button_->SetTooltipText(l10n_util::GetStringUTF16(
+          IDS_MESSAGE_CENTER_NOTIFICATION_SNOOZE_BUTTON_TOOLTIP));
+    }
     snooze_button_->SetBackground(
         views::CreateSolidBackground(SK_ColorTRANSPARENT));
     DeprecatedLayoutImmediately();
