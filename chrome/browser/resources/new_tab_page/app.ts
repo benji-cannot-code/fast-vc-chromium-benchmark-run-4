36fadf758d07db74ec987ca9a711d5036434fd4b
Bofeng Chen <bofengchen@google.com>
2026-01-09 11:38:23 -0800

    [ntp-threads] Add a feature parameter for AIM ntp threads
    The ntp threads feature will be gated by the new parameter
    `EnableThreadsRail` introduced in this change. The WebUI typescript land
    will retrieve the param value from the browser side. The parameter is
    default to false, and is configurable via Finch experiments.
    
    Bug: 474437805
    Change-Id: I0437fde064ead6e49c2a0beddcccdd9e191b9a67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7417944
    Reviewed-by: Gianni Chen <giannich@chromium.org>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Commit-Queue: Bofeng Chen <bofengchen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1567102}

--- chrome/browser/resources/new_tab_page/app.ts
+++ chrome/browser/resources/new_tab_page/app.ts
@@ -327,6 +327,11 @@ export class AppElement extends AppElementBase {
       contextMenuGlifAnimationState_: {type: String},
       undoAutoRemovalCallback_: {type: Object},
       undoAutoRemovalMessage_: {type: Object},
+
+      /**
+       * Whether to show the AIM threads rail when composebox is open.
+       */
+      enableThreadsRail_: {type: Boolean},
     };
   }
 
@@ -421,6 +426,8 @@ export class AppElement extends AppElementBase {
       loadTimeData.getBoolean('enableEphemeralContextMenuDescription') ?? false;
   protected showContextMenuDescription_: boolean =
       loadTimeData.getBoolean('composeboxShowContextMenuDescription');
+  protected accessor enableThreadsRail_: boolean =
+      loadTimeData.getBoolean('enableThreadsRail');
 
   private callbackRouter_: PageCallbackRouter;
   private pageHandler_: PageHandlerRemote;
