6be30024dcd1934bb06e62e689150365e746f312
Rushan Suleymanov <rushans@google.com>
2026-01-09 01:30:13 -0800

    Move SkillsServiceFactory to chrome/browser/skills (new folder)
    This is needed to add dependencies on chrome/ folder in follow-up CLs.
    
    Bug: b:471795213
    Change-Id: Id359285fec14ed71dc57a68b7bcb8379b69f648e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7409286
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566809}

--- chrome/browser/skills/skills_service_factory.cc
+++ chrome/browser/skills/skills_service_factory.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/skills/content/skills_service_factory.h"
+#include "chrome/browser/skills/skills_service_factory.h"
 
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "components/keyed_service/core/keyed_service.h"
--- chrome/browser/skills/skills_service_factory.h
+++ chrome/browser/skills/skills_service_factory.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef COMPONENTS_SKILLS_CONTENT_SKILLS_SERVICE_FACTORY_H_
-#define COMPONENTS_SKILLS_CONTENT_SKILLS_SERVICE_FACTORY_H_
+#ifndef CHROME_BROWSER_SKILLS_SKILLS_SERVICE_FACTORY_H_
+#define CHROME_BROWSER_SKILLS_SKILLS_SERVICE_FACTORY_H_
 
 #include "base/no_destructor.h"
 #include "components/keyed_service/content/browser_context_keyed_service_factory.h"
@@ -36,4 +36,4 @@ class SkillsServiceFactory : public BrowserContextKeyedServiceFactory {
 
 }  // namespace skills
 
-#endif  // COMPONENTS_SKILLS_CONTENT_SKILLS_SERVICE_FACTORY_H_
+#endif  // CHROME_BROWSER_SKILLS_SKILLS_SERVICE_FACTORY_H_
