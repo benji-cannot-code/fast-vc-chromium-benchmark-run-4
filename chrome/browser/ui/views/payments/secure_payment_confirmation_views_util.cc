53bacda218a9c8dfc49b952d7eaa0270f0b93863
Darwin Yang <darwinyang@google.com>
2026-01-09 13:11:04 -0800

    [spc] Create a new UX view for SPC
    Mocks:
    http://docs/presentation/d/18MkO0EVJ99L1UL5szEoepQU2Kj_qjJXE8kDHLktSabY?slide=id.g32d0fe23bac_0_0&pli=1&resourcekey=0-xCOEtMGqIuwcokcPW7d-TQ#slide=id.g32d0fe23bac_0_0
    New SPC UX refresh dialog: http://screen/BPkttZLREQaUJSR
    No matching credentials: http://screen/3oP5JdNVktjJNyA
    Opt out: http://screen/AsdgBjNTzY7T2mY
    Opt out with no matching credentials: http://screen/3YKDEZKy8A8ncai
    No logos: http://screen/5C3ZepmJA6r6gkT
    Dark mode: http://screen/8pLq38PaS6Umvrq
    No logos in dark mode: http://screen/9QLF6fvFTLZ6ePD
    
    Bug: 441560182
    Change-Id: I69b58462779704af19b2ad05a92885bdec9afc00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7377886
    Commit-Queue: Darwin Yang <darwinyang@chromium.org>
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1567169}

--- chrome/browser/ui/views/payments/secure_payment_confirmation_views_util.cc
+++ chrome/browser/ui/views/payments/secure_payment_confirmation_views_util.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/features.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
+#include "ui/base/models/image_model.h"
 #include "ui/compositor/layer.h"
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/native_theme/native_theme.h"
@@ -112,11 +113,17 @@ std::unique_ptr<views::Label> CreateSecurePaymentConfirmationTitleLabel(
 
 std::unique_ptr<views::ImageView> CreateSecurePaymentConfirmationIconView(
     const gfx::ImageSkia& image) {
+  return CreateSecurePaymentConfirmationIconView(
+      ui::ImageModel::FromImageSkia(image));
+}
+
+std::unique_ptr<views::ImageView> CreateSecurePaymentConfirmationIconView(
+    const ui::ImageModel& image) {
   std::unique_ptr<views::ImageView> icon_view =
       std::make_unique<views::ImageView>();
-  icon_view->SetImage(ui::ImageModel::FromImageSkia(image));
+  icon_view->SetImage(image);
 
-  gfx::Size image_size = image.size();
+  gfx::Size image_size = image.Size();
   // Resize to a constant height, with a variable width in the acceptable range
   // based on the aspect ratio.
   float aspect_ratio =
