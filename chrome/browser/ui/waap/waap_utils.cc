4d0d7440954933a08c0a879a871d2f74c0ef650a
Mingyu Lei <leimy@chromium.org>
2026-01-08 21:01:38 -0800

    Webium Product: defer browser test until the reload button is painted
    This CL adds a run loop in the InProcessBrowserTest so that it waits
    until the reload button web view's content is painted
    (`DidFirstVisuallyNonEmptyPaint`). This will resolve several test
    failures caused by the reload button web view.
    
    Bug: 444358999
    Change-Id: I3caac66e176c958a981eb9cb327e380990b285f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7287120
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Mingyu Lei <leimy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566731}

--- chrome/browser/ui/waap/waap_utils.cc
+++ chrome/browser/ui/waap/waap_utils.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/waap/waap_utils.h"
 
+#include "base/containers/variant_map.h"
 #include "base/feature_list.h"
 #include "build/build_config.h"
 #include "chrome/browser/profiles/profile.h"
