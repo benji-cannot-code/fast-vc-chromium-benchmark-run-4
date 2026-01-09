dc7fb46bfb207d546ba34166aea357d618aeb850
Anthony Cui <cuianthony@google.com>
2026-01-08 16:53:29 -0800

    [GlicUnderlines] Add adaptive color support for custom themes
    https://screencast.googleplex.com/cast/NTM4MTg4NTU2NjkxMDQ2NHw0OWNkMDY3Yy05Mw
    This CL adds color mixer code to use appropriate colors for custom themes where `ColorRefPrimary` is not adaptive. To better support sufficient contrast in custom themes this also adds different colors for underlines in active vs inactive tabs.
    
    Bug: b:467105556
    Change-Id: I92453cfab1756a1b56c277b2963e2fa4051a3718
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7411359
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Anthony Cui <cuianthony@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566634}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -197,6 +197,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorGlicBackground) \
   E_CPONLY(kColorGlicModalBackground) \
   E_CPONLY(kColorGlicModalForeground) \
+  E_CPONLY(kColorGlicActiveTabUnderlineGradient1) \
+  E_CPONLY(kColorGlicActiveTabUnderlineGradient2) \
+  E_CPONLY(kColorGlicActiveTabUnderlineGradient3) \
+  E_CPONLY(kColorGlicInactiveTabUnderlineGradient1) \
+  E_CPONLY(kColorGlicInactiveTabUnderlineGradient2) \
+  E_CPONLY(kColorGlicInactiveTabUnderlineGradient3) \
   /* Hover Button colors */ \
   E_CPONLY(kColorHoverButtonBackgroundHovered) \
   /* Lens overlay colors. */ \
