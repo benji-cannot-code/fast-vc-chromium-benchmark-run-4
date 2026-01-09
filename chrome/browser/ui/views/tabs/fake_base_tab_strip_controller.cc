94ea6dc6096a841ad673301aff3de39a87ab397a
Kaan Alsan <alsan@chromium.org>
2026-01-09 06:51:15 -0800

    Move some TabStripController dragging logic into TabDragController
    This is a refactoring with no intended changes in behavior.
    
    TabStripController had some logic for updating BrowserView state when
    tab dragging starts/ends. This logic is not specific to the horizontal
    tab strip (or any tab strip). It is moved to TabDragController instead.
    
    Bug: 439963720
    Change-Id: Ieb9e1694811ded2404dae048cdf458bc98249df3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7405840
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566909}

--- chrome/browser/ui/views/tabs/fake_base_tab_strip_controller.cc
+++ chrome/browser/ui/views/tabs/fake_base_tab_strip_controller.cc
@@ -278,7 +278,7 @@ void FakeBaseTabStripController::CreateNewTab(NewTabTypes context) {
   AddTab(num_tabs_, TabActive::kActive);
 }
 
-void FakeBaseTabStripController::OnStartedDragging(bool dragging_window) {}
+void FakeBaseTabStripController::OnStartedDragging() {}
 
 void FakeBaseTabStripController::OnStoppedDragging() {}
 
