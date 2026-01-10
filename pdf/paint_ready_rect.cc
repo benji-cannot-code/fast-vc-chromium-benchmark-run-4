ca2903274a2ab086248dc7a12bdc89e44c8c69c2
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2026-01-09 20:29:19 -0800

    Revert "pdf: Use a set of rotating buffers for the PDF PaintManager"
    This reverts commit 40d8e796a4812f76d2b15dcf22bb8d97bd9dbe6f.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5458838351249408
    
    Sample build with failed test: https://ci.chromium.org/b/8693103569411817585
    Affected test(s):
    [://chrome/test\:browser_tests!gtest::PDFExtensionTestWithPartialLoading#PartialRedirectsFailInPlugin/All.0](https://ci.chromium.org/ui/test/chromium/:%2F%2Fchrome%2Ftest%5C:browser_tests%21gtest::PDFExtensionTestWithPartialLoading%23PartialRedirectsFailInPlugin%2FAll.0?q=VHash%3A15e8c90d55ca90bb)
    [://chrome/test\:browser_tests!gtest::PDFExtensionTestWithPartialLoading#PartialRedirectsFailInPlugin/All.1](https://ci.chromium.org/ui/test/chromium/:%2F%2Fchrome%2Ftest%5C:browser_tests%21gtest::PDFExtensionTestWithPartialLoading%23PartialRedirectsFailInPlugin%2FAll.1?q=VHash%3A15e8c90d55ca90bb)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5458838351249408&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F7097683&type=BUG
    
    Original change's description:
    > pdf: Use a set of rotating buffers for the PDF PaintManager
    >
    > In the PdfBufferedPaintManager experiment, we change how the
    > PaintManager handles memory. Currently, Skia owns the memory, but
    > this is untenable because there are unavoidable and unnecessary
    > transitory copies. Rather than this, we opt to manage our own memory,
    > using a new buffer for each frame, and passing the frame off to skia
    > once it is completely rastered. Once skia finishes with it, we hold on
    > to the memory, and reuse it if possible. None of the current logic has
    > been changed outside of this experiment.
    >
    > This CL introduces a use of UNSAFE_BUFFERS() to do a raw `memcpy` from
    > the engine buffer into the surface Pixmap in DoPaint(). The `memcpy` is
    > required because PDFium directly modifies the pixmap rather than going
    > through Skia. We need to copy into the PaintManager's flushing pixmap,
    > but Skia doesn't support a way to do this without creating an
    > intermediate SkImage, which would mean a ~32MB memory move every single
    > frame into and out of a newly allocated buffer, which is simply too
    > costly in terms of performance (this is the status quo, which we're
    > trying to fix).
    >
    > Change-Id: Ibfc431312950fb386d69c7d18c41103e1936dcf9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7097683
    > Commit-Queue: Sean Maher <spvm@chromium.org>
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1567279}
    >
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I9c722299e95abc97c9c0f6a61f43bbdc573f65c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7427978
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1567352}

--- pdf/paint_ready_rect.cc
+++ pdf/paint_ready_rect.cc
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/check.h"
-#include "base/feature_list.h"
-#include "pdf/pdf_features.h"
 #include "third_party/skia/include/core/SkImage.h"
 #include "third_party/skia/include/core/SkRefCnt.h"
 #include "ui/gfx/geometry/rect.h"
@@ -23,8 +21,7 @@ PaintReadyRect::PaintReadyRect(const gfx::Rect& rect,
                                sk_sp<SkImage> image,
                                bool flush_now)
     : rect_(rect), image_(std::move(image)), flush_now_(flush_now) {
-  CHECK(image_ ||
-        base::FeatureList::IsEnabled(features::kPdfBufferedPaintManager));
+  CHECK(image_);
 }
 
 PaintReadyRect::PaintReadyRect(PaintReadyRect&&) noexcept = default;
