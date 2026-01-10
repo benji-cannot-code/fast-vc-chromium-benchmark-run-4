0c442e8f37a6a681879d78eb125aad28acd10554
Gianni Chen <giannich@chromium.org>
2026-01-10 09:48:00 -0800

    [ntp-threads] Add threads rail webui component
    This change adds the boilerplate of the threads rail cr component, and
    its conditional rendering on the side of the NTP whenever the composebox
    is shown.
    
    Screencast: https://screencast.googleplex.com/cast/NTM0ODkzNDY0NDkyNDQxNnxkMTc2MzI2Zi02Mw
    
    Bug: 474430623
    Change-Id: I4caa5c836157b3c1f69e33ceaa38cc23bc793d43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7427726
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1567421}

---
+++ chrome/test/data/webui/new_tab_page/composebox/threads_rail_test.ts
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
+
+// TODO(crbug.com/474657447): Add unit tests for threads rail.
+suite('NewTabPageThreadsRailTest', () => {
+  setup(() => {
+    loadTimeData.overrideValues({
+      'enableThreadsRail_': true,
+    });
+  });
+
+  test('show threads rail on composebox open', async () => {});
+
+  test('do not show threads rail on disabled flag', async () => {});
+
+  test('open AI Mode history on show history button click', async () => {});
+});
---
+++ ui/webui/resources/cr_components/composebox/threads_rail.html.ts
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import {type ThreadsRailElement} from './threads_rail.js';
+
+export function getHtml(this: ThreadsRailElement) {
+  // clang-format off
+  return html`<!--_html_template_start_-->
+  <div id="rail">
+    <cr-icon-button id="showHistoryButton"
+        iron-icon="cr:history"
+        @click="${this.onShowHistoryClick_}"
+        title="${this.i18n('aimThreadsHistoryLabel')}">
+    </cr-icon-button>
+  </div>
+  <!--_html_template_end_-->`;
+  // clang-format on;
+}
