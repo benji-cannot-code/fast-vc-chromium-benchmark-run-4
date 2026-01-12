f909b0866f30764f388051f069ee5510651479ae
Jonathan Lee <jonathanjlee@google.com>
2026-01-12 10:33:19 -0800

    Import wpt@e7f1b842c259292b844f80035d7b47611e186716
    https://github.com/web-platform-tests/wpt/compare/73beb0dab...e7f1b842c
    
    Using wpt-import in Chromium 223a09aa30f0b66b09f58806a1fe8f7be4232ebe.
    With Chromium commits locally applied on WPT:
      a64bc652fc "[scrollbar-gutter] Fix "scrollbar-gutter: stable [...]
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    bjonesbe@adobe.com:
      external/wpt/css/css-shapes
    daniec@microsoft.com, siliu@microsoft.com, snianu@microsoft.com:
      external/wpt/input-events
    dgrogan@chromium.org:
      external/wpt/css/css-tables
    drott@chromium.org, kojii@chromium.org:
      external/wpt/css/css-fonts
    dtapuska@chromium.org:
      external/wpt/uievents
    fergal@chromium.org:
      external/wpt/service-workers/cache-storage
      external/wpt/storage
    foolip@chromium.org:
      external/wpt/infrastructure
    futhark@chromium.org, andruud@chromium.org:
      external/wpt/css/css-cascade
    hbos@chromium.org, hta@chromium.org, guidou@chromium.org, phancke@microsoft.com:
      external/wpt/webrtc
    japhet@chromium.org:
      external/wpt/fetch
    kojii@chromium.org:
      external/wpt/css/CSS2/linebox
      external/wpt/css/CSS2/text
      external/wpt/css/css-scoping
      external/wpt/css/css-writing-modes
    kojii@chromium.org, mstensho@chromium.org:
      external/wpt/css/CSS2
    mstensho@chromium.org:
      external/wpt/css/css-break
      external/wpt/css/css-multicol
      external/wpt/css/css-page
    mustaq@chromium.org:
      external/wpt/html/interaction/focus
    nzolghadr@chromium.org, mustaq@chromium.org:
      external/wpt/pointerevents
    smcgruer@chromium.org:
      external/wpt/css/css-position
    
    NOAUTOREVERT=true
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac-skia-alt-arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac12.0-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac12.0.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac13-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac13.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac14.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac15.arm64-blink-rel
    Change-Id: I7b6cd2e8d0a562e5f6568bdc39f6c3a96084ddcd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7422821
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1567894}

--- third_party/blink/web_tests/external/wpt/fetch/fetch-later/quota/max-payload.https.window.js
+++ third_party/blink/web_tests/external/wpt/fetch/fetch-later/quota/max-payload.https.window.js
@@ -26,6 +26,7 @@ promise_test(async _ => {
         activateAfter: 0,
         method: 'POST',
         bodySize: getRemainingQuota(QUOTA_PER_ORIGIN, requestUrl, headers),
+        referrer: '',
       },
       {
         targetUrl: requestUrl,
@@ -40,11 +41,12 @@ test(_ => {
 
   assert_throws_quotaexceedederror(() => {
     fetchLater(requestUrl, {
-          activateAfter: 0,
-          method: 'POST',
-          body: generatePayload(
-              getRemainingQuota(QUOTA_PER_ORIGIN, requestUrl, headers) + 1,
-              dataType),
-        });
-  }, null, null);
+      activateAfter: 0,
+      method: 'POST',
+      body: generatePayload(
+          getRemainingQuota(QUOTA_PER_ORIGIN, requestUrl, headers) + 1,
+          dataType),
+      referrer: '',
+    });
+  }, QUOTA_PER_ORIGIN + 1, QUOTA_PER_ORIGIN);
 }, `fetchLater() rejects max+1 payload in a POST request body of ${dataType}.`);
