183bc8aaf50a8306df1be6a5e1b9ca1522526f1f
Joe Peplowski <joepeplowski@google.com>
2026-01-12 11:53:38 -0800

    Add GeminiCameraViewController
    This CL adds a view controller to hold the Gemini Camera permission
    toggle.
    
    Changing the toggle currently has no effect since the business logic for
    this change will be implemented in a future CL.
    
    Screenshot: screen/A2By6kSV286cDkV
    Bug: 463434218
    Change-Id: Ic28a422c7a494e45529cbb4921666b49dbaee9ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7409869
    Reviewed-by: Sebastien S-G <sebsg@chromium.org>
    Commit-Queue: Joe Peplowski <joepeplowski@google.com>
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Reviewed-by: Joemer Ramos <joemerramos@google.com>
    Cr-Commit-Position: refs/heads/main@{#1567955}

--- ios/chrome/browser/settings/ui_bundled/bwg/ui/bwg_settings_view_controller.mm
+++ ios/chrome/browser/settings/ui_bundled/bwg/ui/bwg_settings_view_controller.mm
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/settings/ui_bundled/bwg/model/gemini_settings_context.h"
 #import "ios/chrome/browser/settings/ui_bundled/bwg/model/gemini_settings_metadata.h"
 #import "ios/chrome/browser/settings/ui_bundled/bwg/ui/bwg_location_view_controller.h"
+#import "ios/chrome/browser/settings/ui_bundled/bwg/ui/gemini_camera_view_controller.h"
 #import "ios/chrome/browser/settings/ui_bundled/bwg/utils/gemini_settings_metrics.h"
 #import "ios/chrome/browser/shared/model/url/chrome_url_constants.h"
 #import "ios/chrome/browser/shared/ui/table_view/cells/table_view_detail_text_item.h"
@@ -80,6 +81,8 @@ NSString* const kPageContentSharingAction = @"PageContentSharingAction";
   TableViewSwitchItem* _pageContentSharingItem;
   // Location view controller shown when precise location row is tapped.
   BWGLocationViewController* _locationViewController;
+  // Camera view controller shown when camera row is tapped.
+  GeminiCameraViewController* _cameraViewController;
   // Precise location preference value.
   BOOL _preciseLocationEnabled;
   // Camera preference value.
@@ -344,6 +347,13 @@ NSString* const kPageContentSharingAction = @"PageContentSharingAction";
     _locationViewController.preciseLocationEnabled = _preciseLocationEnabled;
     _locationViewController.mutator = self.mutator;
     [self.navigationController pushViewController:_locationViewController
+                                         animated:YES];
+  }
+
+  if ([self.tableViewModel itemTypeForIndexPath:indexPath] == ItemTypeCamera) {
+    _cameraViewController = [[GeminiCameraViewController alloc]
+        initWithStyle:ChromeTableViewStyle()];
+    [self.navigationController pushViewController:_cameraViewController
                                          animated:YES];
   }
 
