0a9cd2a74cd62eed78f8ed97f9cc28cc4190a374
ginnnnnnny <ginnyhuang@chromium.org>
2026-01-12 03:05:12 -0800

    Reland "[iOS][mvt-customization] Enable feature in fieldtrial testing"
    This is a reland of commit ecd7b95b46e830ef7fdf441bb22faaa99b95e670.
    
    Fixing testMinimumHeight's failure on iOS 17 that the width of each MVT
    cell being calculated wrongly by forcing a layout right after the MVT is
    added to the view hierarchy.
    
    Original change's description:
    > [iOS][mvt-customization] Enable feature in fieldtrial testing
    >
    > Also fixes the bug where the NTP returns to a different offset from the
    > saved offset after navigating back.
    >
    > Bug: N/A
    > Change-Id: I7a3805ba8f7547eac29f4c42bb56deff04e0aeac
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7408734
    > Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1566965}
    
    Bug: N/A
    Change-Id: I8d20e8763358cb2b5a1e9e7abee25b3313ded4ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7418997
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1567668}

--- ios/chrome/browser/content_suggestions/ui/content_suggestions_view_controller.mm
+++ ios/chrome/browser/content_suggestions/ui/content_suggestions_view_controller.mm
@@ -130,6 +130,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [self.mostVisitedModuleContainer.bottomAnchor
         constraintEqualToAnchor:self.view.bottomAnchor]
   ]];
+  // Force layout to make sure the subviews correctly calculates its frame size.
+  [self.view setNeedsLayout];
   [self.view layoutIfNeeded];
 }
 
