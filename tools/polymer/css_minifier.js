faaf533632142144c805eb688287cc3ba37a32cf
dpapad <dpapad@chromium.org>
2026-01-12 12:31:35 -0800

    WebUI: Switch default tsconfig moduleResolution to 'NodeNext'.
    The default TS configuration at tools/typescript/tsconfig_base.json did
    not previously explicitly specify 'moduleResolution'. In TS v5 it is
    inferred as 'classic'. In the upcoming TS v6 'classic' is deprecated [1]
    and in v7 it is completely removed.
    
    Switching off of 'classic' is a necessary preparatory step to unblock
    further migrating to TS v6 and v7. 'NodeNext' is the most appropriate
    mode from the options being offered (essentially between 'NodeNext' and
    'Bundler', also see [2]).
    
    Specifically:
     - Add top-level package.json file (required for 'NodeNext' to work).
     - Explicitly specify moduleResolution where the new default 'NodeNext'
       doesn't work. Preferring 'bundler' where possible, but still using
       'classic' in cases where non-trivial work is required to migrate off
       of 'classic'.
     - Update tools/polymer/css_minifier.js to use ESM imports since it is
       now evaluated as an ES module itself.
     - Update defaults in tools/typescript/tsconfig_base.json and
       tools/typescript/tsconfig_definitions_base.json.
    
    [1] https://github.com/microsoft/TypeScript/issues/62206
    [2] https://github.com/microsoft/TypeScript/issues/62905
    
    Bug: 423789047
    Change-Id: I34e874c096b1c8c80b952c4cd659746c3a0fc5a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7256911
    Reviewed-by: James Hawkins <jhawkins@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1567984}

--- tools/polymer/css_minifier.js
+++ tools/polymer/css_minifier.js
@@ -10,14 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * HTML or CSS input files, by minifying their CSS contents (removing any blank
  * space and comments, no other transformations made).
  */
-const postcss =
-    require('../../third_party/node/node_modules/postcss/lib/postcss.js');
-const postcssMinify =
-    require('../../third_party/node/node_modules/postcss-minify/index.js');
+import assert from 'node:assert/strict';
+import * as fs from 'node:fs/promises';
+import path from 'node:path';
 
-const assert = require('assert');
-const fs = require('fs/promises');
-const path = require('path');
+import postcssMinify from '../../third_party/node/node_modules/postcss-minify/index.js';
+import postcss from '../../third_party/node/node_modules/postcss/lib/postcss.js';
 
 // Regular expression to extract CSS content from within <style>...</style> or
 // <style include="...">...</style> tags. The 'd' flag is needed to obtain the
