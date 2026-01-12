7bb49164d6ac5b7e391fa9d2647e462696d94eed
Arkadiusz Tomczak <artomczak@google.com>
2026-01-12 00:42:47 -0800

    Add retry logic for creating Android and ChromeOS driver.
    In http://crrev.com/c/7261982 we added retry logic for Desktop.
    We observe similar issues on Android Dev and ChromeOS Beta,
    therefore we add similar mechanism for those platforms.
    Please notice that we do not reuse the timeout logic, since
    those platforms have longer start up time due to being run
    in emulator or vm.
    
    Bug: 466009188
    Change-Id: I1e06aba38b7d92bf1515c97c4f88b64c7a91830a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7408634
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Commit-Queue: Arkadiusz Tomczak <artomczak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1567639}

--- chrome/test/variations/drivers/android.py
+++ chrome/test/variations/drivers/android.py
@@ -114,8 +114,7 @@ class AndroidDriverFactory(DriverFactory):
 
     driver = None
     try:
-      yield (driver := webdriver.Chrome(service=self.get_driver_service(),
-                                        options=options))
+      yield (driver := self.get_driver(options))
     finally:
       if driver:
         driver.quit()
