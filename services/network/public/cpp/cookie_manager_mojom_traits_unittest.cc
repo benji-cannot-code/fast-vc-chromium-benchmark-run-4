64042ca0af09ad4750bdf9fca130337b4d5e3057
Ryan Lothian <rjlothian@google.com>
2026-01-12 08:59:21 -0800

    Fix typo in cookie_manager_mojom_traits_unittest.cc
    Tested by running services_unittests.
    
    Change-Id: Iab518e5db35e7e4a08cffa66fa565b75cdf01c9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7424588
    Commit-Queue: Ryan Lothian <rjlothian@google.com>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Mike Taylor <miketaylr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1567826}

--- services/network/public/cpp/cookie_manager_mojom_traits_unittest.cc
+++ services/network/public/cpp/cookie_manager_mojom_traits_unittest.cc
@@ -120,7 +120,7 @@ TEST(CookieManagerTraitsTest, Roundtrips_CookieAccessResult) {
             copied.is_allowed_to_access_secure_cookies);
 }
 
-TEST(CookieManagerTraitsTest, Rountrips_CookieWithAccessResult) {
+TEST(CookieManagerTraitsTest, Roundtrips_CookieWithAccessResult) {
   auto original_cookie = net::CanonicalCookie::CreateUnsafeCookieForTesting(
       "A", "B", "x.y", "/path", base::Time(), base::Time(), base::Time(),
       base::Time(),
