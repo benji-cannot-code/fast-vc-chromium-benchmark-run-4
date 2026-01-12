d28b184f99105875283e8859196cb9bd0e273223
Xi Han <hanxi@google.com>
2026-01-12 10:03:23 -0800

    [Theme] Fix trailing icon of chrome color is invisible.
    The bug was due to CHROME_COLOR and COLOR_FROM_HEX share the same list
    item view, and setting COLOR_FROM_HEX will revert the visibility from
    visible to invisible when user chooses CHROME_COLOR. Add a special check
    to prevent this.
    Video: http://shortn/_FWOuiBYUOe.
    
    Bug: 423579377
    Change-Id: I36aa2e9a13b8da1886a98c66d304741baf657142
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7454520
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1567869}

--- chrome/browser/ntp_customization/java/src/org/chromium/chrome/browser/ntp_customization/theme/NtpThemeMediator.java
+++ chrome/browser/ntp_customization/java/src/org/chromium/chrome/browser/ntp_customization/theme/NtpThemeMediator.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.ntp_customization.theme;
 
 import static org.chromium.chrome.browser.ntp_customization.NtpCustomizationUtils.NtpBackgroundImageType.CHROME_COLOR;
+import static org.chromium.chrome.browser.ntp_customization.NtpCustomizationUtils.NtpBackgroundImageType.COLOR_FROM_HEX;
 import static org.chromium.chrome.browser.ntp_customization.NtpCustomizationUtils.NtpBackgroundImageType.DEFAULT;
 import static org.chromium.chrome.browser.ntp_customization.NtpCustomizationUtils.NtpBackgroundImageType.IMAGE_FROM_DISK;
 import static org.chromium.chrome.browser.ntp_customization.NtpCustomizationUtils.NtpBackgroundImageType.THEME_COLLECTION;
@@ -149,6 +150,13 @@ public class NtpThemeMediator {
     void updateTrailingIconVisibilityForSectionType(@NtpBackgroundImageType int sectionType) {
         for (int i = 0; i < NtpBackgroundImageType.NUM_ENTRIES; i++) {
             if (i == THEME_COLLECTION) {
+                continue;
+            }
+
+            if (i == COLOR_FROM_HEX && sectionType == CHROME_COLOR) {
+                // Prevents overriding the visibility from visible to invisible if the user chooses
+                // a customized color theme. This is because both types share the same bottom sheet
+                // list item.
                 continue;
             }
 
