2ef3b00fc2b8c0b974ce51ea0651baeb9299c071
Joemer Ramos <joemerramos@google.com>
2026-01-12 12:32:30 -0800

    [iOS] Add public method to get the current Gemini view state
    Get current Gemini view state from GCRGemini.
    
    Should land after both of these CLs:
    crrev.com/i/8910252 (second CL)
    cl/854212153 (first CL)
    
    Fixed: 474594348
    Change-Id: I85268cb192c9ead57dab41fbe624fb02f26752d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7412260
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Joemer Ramos <joemerramos@google.com>
    Cr-Commit-Position: refs/heads/main@{#1567985}

--- ios/chrome/browser/providers/bwg/chromium_bwg.mm
+++ ios/chrome/browser/providers/bwg/chromium_bwg.mm
@@ -47,4 +47,8 @@ void UpdateOverlayOffsetWithOpacity(CGFloat offset, CGFloat opacity) {}
 
 void UpdateGeminiViewState(GeminiViewState view_state) {}
 
+GeminiViewState GetCurrentGeminiViewState() {
+  return GeminiViewState::kUnknown;
+}
+
 }  // namespace ios::provider
