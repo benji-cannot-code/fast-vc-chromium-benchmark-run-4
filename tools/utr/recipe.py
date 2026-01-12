97a8627e865998d44d17fcff0a1f5062815ed1e4
Ben Pastene <bpastene@chromium.org>
2026-01-12 15:19:20 -0800

    utr: Support --omit-default-test-args as an arg
    This will mostly wipe out all cmd-line args that would automatically
    be set for the given test(s). Useful if you want to override a specific
    arg, and setting it twice doesn't work.
    
    Recipe-side version of this CL: https://crrev.com/c/7427206
    
    Without this CL, passing in a repeated arg (--jobs) gets set twice:
    https://chromium-swarm.appspot.com/task?id=75bda0d087f2a110
    
    With it, it's mostly the only arg that gets passed:
    https://chromium-swarm.appspot.com/task?id=75bd9fde4a302110
    
    It fails, but that's expected since other needed args weren't
    passed by the user.
    
    Bug: 386167803
    Change-Id: I8879edde3b74da7f443f958feaedeb6fcd25c2bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7421918
    Commit-Queue: Ben Pastene <bpastene@chromium.org>
    Reviewed-by: Struan Shrimpton <sshrimp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1568101}

--- tools/utr/recipe.py
+++ tools/utr/recipe.py
@@ -110,7 +110,8 @@ class LegacyRunner:
                skip_coverage=False,
                no_rbe=False,
                no_siso=False,
-               use_autoninja=False):
+               use_autoninja=False,
+               **kwargs):
     """Constructor for LegacyRunner
 
     Args:
@@ -132,6 +133,7 @@ class LegacyRunner:
       no_rbe: If True, disables RBE during compile.
       no_siso: If True, disabled Siso during compile and isolate.
       use_autoninja: If True, uses autoninja during compile.
+      **kwargs: Additional args to passthrough to the recipe's input props.
     """
     self._recipes_py = recipes_py
     self._skip_coverage = skip_coverage
@@ -228,6 +230,8 @@ class LegacyRunner:
       input_props['no_rbe'] = True
     if no_siso:
       input_props['no_siso'] = True
+
+    input_props.update(kwargs)
     self._input_props = input_props
 
   def _merge_rerun_props(self, rerun_props_from_recipe):
