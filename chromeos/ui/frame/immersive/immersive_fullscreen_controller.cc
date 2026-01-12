988958c95b55d1139921e3cfb0532f8575eeda35
Mitsuru Oshima <oshima@chromium.org>
2026-01-12 10:18:44 -0800

    Remove chromeos::ViewID and migrate to ElementID
    Bug: None
    
    Change-Id: I214fadd288b63f7a6c990f5749f4b2fcc81dfd5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7411393
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1567880}

--- chromeos/ui/frame/immersive/immersive_fullscreen_controller.cc
+++ chromeos/ui/frame/immersive/immersive_fullscreen_controller.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "chromeos/ui/base/chromeos_ui_constants.h"
 #include "chromeos/ui/base/window_properties.h"
+#include "chromeos/ui/frame/caption_buttons/frame_caption_button_container_view.h"
 #include "chromeos/ui/frame/immersive/immersive_context.h"
 #include "chromeos/ui/frame/immersive/immersive_focus_watcher.h"
 #include "chromeos/ui/frame/immersive/immersive_fullscreen_controller_delegate.h"
@@ -27,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/scoped_animation_duration_scale_mode.h"
 #include "ui/views/animation/animation_delegate_views.h"
 #include "ui/views/bubble/bubble_dialog_delegate_view.h"
+#include "ui/views/interaction/element_tracker_views.h"
 #include "ui/views/view.h"
 #include "ui/views/widget/widget.h"
 #include "ui/views/window/frame_view.h"
@@ -292,8 +294,9 @@ bool ImmersiveFullscreenController::ShouldRevealTopChrome(views::View* view) {
   auto* non_client_view = widget_->non_client_view();
   auto* frame_view = non_client_view ? non_client_view->frame_view() : nullptr;
   auto* caption_button_container =
-      frame_view ? frame_view->GetViewByID(
-                       chromeos::ViewID::VIEW_ID_CAPTION_BUTTON_CONTAINER)
+      frame_view ? views::ElementTrackerViews::GetInstance()->GetUniqueView(
+                       chromeos::FrameCaptionButtonContainerView::kElementId,
+                       views::ElementTrackerViews::GetContextForWidget(widget_))
                  : nullptr;
   return caption_button_container && caption_button_container->Contains(view);
 }
