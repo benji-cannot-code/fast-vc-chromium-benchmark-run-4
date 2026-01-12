27a7cdce54319e76e515969e80177e08f7803298
Menghan Yang <myuu@google.com>
2026-01-12 03:31:19 -0800

    [Android][Signin] Update getActivityResultTracker()'s visibility
    This change modifies the visibility of getActivityResultTracker() in
    ChromeBaseAppCompatActivity from protected to @VisibleForTesting public.
    
    This allows TabUmaTest to directly call getActivityResultTracker(),
    which simplifies the test by removing the need to create a new
    ActivityResultTrackerImpl instance.
    
    This is a follow-up for https://crrev.com/c/7368056
    
    Bug: 437039516
    Change-Id: I434e1ce9f1b2a9bdb86bdd987c29a1e486ff5d04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7450858
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Auto-Submit: Menghan Yang <myuu@google.com>
    Commit-Queue: Menghan Yang <myuu@google.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1567679}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tab/TabUmaTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tab/TabUmaTest.java
@@ -34,7 +34,6 @@ import org.chromium.chrome.test.transit.ChromeTransitTestRules;
 import org.chromium.chrome.test.transit.page.WebPageStation;
 import org.chromium.components.browser_ui.util.BrowserControlsVisibilityDelegate;
 import org.chromium.content_public.browser.LoadUrlParams;
-import org.chromium.ui.base.ActivityResultTrackerImpl;
 
 import java.io.DataOutputStream;
 import java.io.File;
@@ -85,9 +84,7 @@ public class TabUmaTest {
                 cta.getCompositorViewHolderSupplier(),
                 cta.getModalDialogManagerSupplier(),
                 cta::getSnackbarManager,
-                new ActivityResultTrackerImpl(
-                        new ActivityResultTrackerImpl.RegistryImpl(
-                                cta.getActivityResultRegistry())),
+                cta.getActivityResultTracker(),
                 cta.getBrowserControlsManager(),
                 cta.getActivityTabProvider(),
                 cta.getLifecycleDispatcher(),
