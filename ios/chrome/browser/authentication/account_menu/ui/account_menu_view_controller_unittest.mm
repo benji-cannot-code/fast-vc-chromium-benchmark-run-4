4aa2c32c8380f9c28773feb3d7c1cc5120294c5c
Gauthier Ambard <gambard@chromium.org>
2026-01-12 08:47:25 -0800

    [iOS] Remove use of TableViewAccountCell outside of its item
    This CL removes the use of TableViewAccountCell in
    AccountMenuViewController, to help with the removal of the cell later.
    
    Before:
    https://screenshot.googleplex.com/3F7uF94w2tzGF32
    
    After:
    https://screenshot.googleplex.com/7ovvi7hMf7D2Awf
    
    Bug: 443034511
    Change-Id: Ia0f9aeeb6835a3123e318fd367b5dfaf563b3b1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7321025
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1567815}

--- ios/chrome/browser/authentication/account_menu/ui/account_menu_view_controller_unittest.mm
+++ ios/chrome/browser/authentication/account_menu/ui/account_menu_view_controller_unittest.mm
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/authentication/account_menu/ui/account_menu_data_source.h"
 #import "ios/chrome/browser/authentication/account_menu/ui/account_menu_mutator.h"
 #import "ios/chrome/browser/authentication/ui_bundled/cells/central_account_view.h"
-#import "ios/chrome/browser/authentication/ui_bundled/cells/table_view_account_item.h"
 #import "ios/chrome/browser/policy/model/management_state.h"
 #import "ios/chrome/browser/settings/model/sync/utils/account_error_ui_info.h"
 #import "ios/chrome/browser/settings/ui_bundled/settings_table_view_controller_constants.h"
@@ -262,10 +261,6 @@ TEST_P(AccountMenuViewControllerTest, TestDefaultSetting) {
   EXPECT_EQ(2, [TableView() numberOfRowsInSection:0]);
   // Sign Out
   EXPECT_EQ(1, [TableView() numberOfRowsInSection:1]);
-  UITableViewCell* secondary_account_cell =
-      GetCell(path_for_secondary_account_);
-  EXPECT_TRUE(
-      [secondary_account_cell isKindOfClass:[TableViewAccountCell class]]);
   ExpectTextAtPath(
       l10n_util::GetNSString(IDS_IOS_OPTIONS_ACCOUNTS_ADD_ACCOUNT_BUTTON),
       path_for_add_account_);
