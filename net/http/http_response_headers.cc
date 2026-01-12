62493dd92ac2e5cf5012e4f86fa648b79f0be64e
Jeffrey Yu <yuje@chromium.org>
2026-01-12 10:10:51 -0800

    Replace `std::unordered_set` with `absl::flat_hash_set`.
    The Chromium containers guide
    (https://chromium.googlesource.com/chromium/src/+/HEAD/base/containers/README.md)
    recommends using `absl::flat_hash_set` as a default set choice instead
    of `std::unordered_set` when pointer stability isn't needed, and
    `absl::node_hash_set` when it is. Both will still generally perform
    better and require less overhead than `std::unordered_set`.
    
    Bug: 473916362
    Change-Id: Iaaf35564ce389d2ce103de781ec5b9c943017599
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7411374
    Auto-Submit: Jeffrey Yu <yuje@chromium.org>
    Reviewed-by: mmenke <mmenke@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Jeffrey Yu <yuje@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1567874}

--- net/http/http_response_headers.cc
+++ net/http/http_response_headers.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/structured_headers.h"
 #include "net/log/net_log_capture_mode.h"
 #include "net/log/net_log_values.h"
+#include "third_party/abseil-cpp/absl/container/flat_hash_set.h"
 #include "third_party/abseil-cpp/absl/strings/ascii.h"
 
 using base::Time;
@@ -226,6 +227,9 @@ scoped_refptr<HttpResponseHeaders> HttpResponseHeaders::Builder::Build() {
   return base::MakeRefCounted<HttpResponseHeaders>(BuilderPassKey(), version_,
                                                    status_, headers_);
 }
+
+class HttpResponseHeaders::HeaderSet : public absl::flat_hash_set<std::string> {
+};
 
 HttpResponseHeaders::HttpResponseHeaders(std::string_view raw_input)
     : response_code_(-1) {
