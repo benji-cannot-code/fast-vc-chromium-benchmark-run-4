cc7c4c1d6596f0fdaf64371646183f4831da3274
Zaina Al-Mashni <zalmashni@google.com>
2026-01-12 04:01:42 -0800

    [glic][onboarding] Fix iph trigger conditions
    - Trigger the iph(s) iff the user went through the new onboarding. This
    should eliminate users who went through the old FRE.
    - Verify that the hotkey is registered before triggering the iph.
    
    Fixed: 474308200
    Change-Id: I0cb906ee4194d42c8844ba2b0a8e2f3f5b53db2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7414700
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1567685}

--- chrome/browser/glic/service/glic_instance_impl.cc
+++ chrome/browser/glic/service/glic_instance_impl.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/notimplemented.h"
 #include "base/task/sequenced_task_runner.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/contextual_cueing/contextual_cueing_service.h"
 #include "chrome/browser/contextual_cueing/contextual_cueing_service_factory.h"
 #include "chrome/browser/glic/fre/glic_fre_controller.h"
@@ -828,6 +829,12 @@ void GlicInstanceImpl::ClearActiveEmbedderAndNotifyStateChange() {
 }
 
 void GlicInstanceImpl::MaybeShowShortcutToastPromo() {
+  if (!g_browser_process->local_state()->GetBoolean(
+          prefs::kGlicLauncherEnabled)) {
+    // Hotkey might not be registered, skip the promo.
+    return;
+  }
+
   Browser* browser = chrome::FindTabbedBrowser(profile_, false);
   if (!browser) {
     // If there is no browser window open for the profile, skip the promo.
@@ -848,6 +855,12 @@ void GlicInstanceImpl::MaybeShowShortcutToastPromo() {
 }
 
 void GlicInstanceImpl::MaybeShowShortcutSnoozePromo() {
+  if (!g_browser_process->local_state()->GetBoolean(
+          prefs::kGlicLauncherEnabled)) {
+    // Hotkey might not be registered, skip the promo.
+    return;
+  }
+
   Browser* browser = chrome::FindTabbedBrowser(profile_, false);
   if (!browser) {
     // If there is no browser window open for the profile, skip the promo.
--- components/feature_engagement/public/event_constants.cc
+++ components/feature_engagement/public/event_constants.cc
@@ -49,6 +49,8 @@ const char kExtensionsRequestAccessButtonClicked[] =
 
 const char kCookieControlsBubbleShown[] = "cookie_controls_bubble_shown";
 
+const char kGlicOnboardingCompleted[] = "glic_onboarding_completed";
+
 #endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_APPLE) || BUILDFLAG(IS_LINUX) ||
         // BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_FUCHSIA)
 
