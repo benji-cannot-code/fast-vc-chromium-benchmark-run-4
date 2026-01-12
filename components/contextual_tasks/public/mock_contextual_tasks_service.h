e6d9b490116ff6ce251e8030355c03490183655c
Khanh Nguyen <khanhtmn@google.com>
2026-01-11 19:37:49 -0800

    [contextual_tasks] Remove ClearAllTabAssociationsForTask
    This CL removes the ClearAllTabAssociationsForTask function from the ContextualTasksService and its implementation. This function is no longer called, and its corresponding test has been updated to use the existing DisassociateAllTabsFromTask function.
    
    Bug: b:470346611
    Change-Id: Ie7f41c0ef21155f4de2143c00d408584ccccfe44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7413046
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1567595}

--- components/contextual_tasks/public/mock_contextual_tasks_service.h
+++ components/contextual_tasks/public/mock_contextual_tasks_service.h
@@ -111,10 +111,6 @@ class MockContextualTasksService : public ContextualTasksService {
               GetTabsAssociatedWithTask,
               (const base::Uuid&),
               (const, override));
-  MOCK_METHOD(void,
-              ClearAllTabAssociationsForTask,
-              (const base::Uuid&),
-              (override));
   MOCK_METHOD(base::WeakPtr<syncer::DataTypeControllerDelegate>,
               GetAiThreadControllerDelegate,
               (),
