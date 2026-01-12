6876bacd8146c6c9a756566f6043ac10fd819f8e
Elias Homsi <eliashomsi@google.com>
2026-01-12 08:35:05 -0800

    [Force Save to OneDrive] Add a new DownloadCheckResult for force save to OneDrive.
    Bug: 458033434
    Change-Id: I79c6cb4a6a3c38abeb26091db496f2b20fd4aa9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7412190
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Elias Klim <elklm@chromium.org>
    Commit-Queue: Elias Klim <elklm@chromium.org>
    Reviewed-by: Nasser Al-shawwa <alshawwa@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Auto-Submit: Elias Homsi <eliashomsi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1567802}

--- chrome/browser/file_system_access/chrome_file_system_access_permission_context.cc
+++ chrome/browser/file_system_access/chrome_file_system_access_permission_context.cc
@@ -634,6 +634,7 @@ InterpretSafeBrowsingResult(safe_browsing::DownloadCheckResult result) {
     case Result::BLOCKED_SCAN_FAILED:
     case Result::SENSITIVE_CONTENT_BLOCK:
     case Result::FORCE_SAVE_TO_GDRIVE:
+    case Result::FORCE_SAVE_TO_ONEDRIVE:
       return ChromeFileSystemAccessPermissionContext::AfterWriteCheckResult::
           kBlock;
 
