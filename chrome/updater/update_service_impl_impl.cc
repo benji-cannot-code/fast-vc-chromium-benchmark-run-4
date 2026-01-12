c4e21ffad08c1382e28494e0b051ccf78957a1d1
S. Ganesh <ganesh@chromium.org>
2026-01-12 08:27:48 -0800

    updater: implement UpdateService methods to get the state and policies
    Bug: 456497861
    Change-Id: Ib2db52108624343ddc9225b501cb2d0a9faf6480
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7415013
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1567794}

--- chrome/updater/update_service_impl_impl.cc
+++ chrome/updater/update_service_impl_impl.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/version.h"
 #include "build/build_config.h"
 #include "chrome/enterprise_companion/global_constants.h"
+#include "chrome/updater/app/app_uninstall.h"
 #include "chrome/updater/app/app_utils.h"
 #include "chrome/updater/auto_run_on_os_upgrade_task.h"
 #include "chrome/updater/branded_constants.h"
@@ -70,6 +71,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/update_client/protocol_definition.h"
 #include "components/update_client/update_client.h"
 #include "components/update_client/update_client_errors.h"
+#include "components/update_client/utils.h"
 
 #if BUILDFLAG(IS_MAC)
 #include <sys/mount.h>
@@ -1572,9 +1574,29 @@ void UpdateServiceImplImpl::GetUpdaterState(
   VLOG(1) << __func__;
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
-  // TODO(crbug.com/456497861): populate `UpdaterState`.
-  main_task_runner_->PostTask(
-      FROM_HERE, base::BindOnce(std::move(callback), UpdaterState()));
+  base::ThreadPool::CreateSequencedTaskRunner(
+      {base::MayBlock(), base::WithBaseSyncPrimitives(),
+       base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN})
+      ->PostTaskAndReplyWithResult(
+          FROM_HERE, base::BindOnce([]() {
+            std::vector<std::string> inactive_versions;
+            for (const base::FilePath& version_executable_path :
+                 GetVersionExecutablePaths(GetUpdaterScope())) {
+              inactive_versions.push_back(update_client::StringTypeToUTF8(
+                  version_executable_path.DirName().BaseName().value()));
+            }
+            return inactive_versions;
+          }),
+          base::BindOnce(
+              [](scoped_refptr<PersistedData> persisted_data,
+                 base::OnceCallback<void(const UpdaterState&)> callback,
+                 const std::vector<std::string>& inactive_versions) {
+                std::move(callback).Run(
+                    UpdaterState(kUpdaterVersion, inactive_versions,
+                                 persisted_data->GetLastChecked(),
+                                 persisted_data->GetLastStarted()));
+              },
+              config_->GetUpdaterPersistedData(), std::move(callback)));
 }
 
 void UpdateServiceImplImpl::GetUpdaterPolicies(
@@ -1583,10 +1605,10 @@ void UpdateServiceImplImpl::GetUpdaterPolicies(
   VLOG(1) << __func__;
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
-  // TODO(crbug.com/456497861): populate the vector of `PolicyValues`.
   main_task_runner_->PostTask(
-      FROM_HERE, base::BindOnce(std::move(callback),
-                                base::flat_map<std::string, PolicyValue>()));
+      FROM_HERE,
+      base::BindOnce(std::move(callback),
+                     config_->GetPolicyService()->GetUpdaterPolicies()));
 }
 
 void UpdateServiceImplImpl::GetAppPolicies(
@@ -1597,13 +1619,9 @@ void UpdateServiceImplImpl::GetAppPolicies(
   VLOG(1) << __func__;
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
-  // TODO(crbug.com/456497861): populate the vector of `PolicyValues`.
   main_task_runner_->PostTask(
-      FROM_HERE,
-      base::BindOnce(
-          std::move(callback),
-          base::flat_map<std::string,
-                         base::flat_map<std::string, PolicyValue>>()));
+      FROM_HERE, base::BindOnce(std::move(callback),
+                                config_->GetPolicyService()->GetAppPolicies()));
 }
 
 bool UpdateServiceImplImpl::IsUpdateDisabledByPolicy(const std::string& app_id,
