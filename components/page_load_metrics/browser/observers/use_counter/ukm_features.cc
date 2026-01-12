b2cd8ceea2e465294d6367f96fe73def7cf2b3ef
Dominik Röttsches <drott@chromium.org>
2026-01-12 04:39:51 -0800

    Deprecate XML external entity loads
    I2D approved in
    https://groups.google.com/a/chromium.org/g/blink-dev/c/4j2oGg2lyBk/m/FkVq-s-EBQAJ
    
    For test cases external-entities.xml, xhtml-alternate-entities.xml and
    dtd-in-source-document.xml there is a behavioral change in that the
    undeclared entity is no longer reported as an error, but now as a
    warning, which changes the final rendering output. This is due to the
    parser logic changing when XML_PARSE_NO_XXE (no external entity loading
    allowed) is specified, see [1]. The parser cannot verify if the entity
    was possibly declared in an external reference. This is working as
    intended and an acceptable change. For files that do not refer to
    external entities in the DTD or otherwise, undeclared entities are still
    reported as an error (locally tested).
    
    In the process revert the deprecation warning: Revert "Mark externally
    loaded entities in XML deprecated" This reverts commit
    c5f60a036a3d005f1b1dcb79c44ed672a36bb95a.
    
    [1]
    https://source.chromium.org/chromium/chromium/src/+/main:third_party/libxml/src/parser.c;l=7371
    
    Bug: 455813733
    Change-Id: I5c07bd78d0a24633aa021680896af0c8a569185d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7415378
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1567698}

--- components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
+++ components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
@@ -457,7 +457,6 @@ UseCounterMetricsRecorder::GetAllowedUkmFeatures() {
           WebFeature::kXSLProcessingInstruction,
           WebFeature::kXSLTProcessor,
           WebFeature::kXMLExternalResourceLoad,
-          WebFeature::kXMLExternalResourceLoadEntitiesOnly,
           WebFeature::kProofreader_Create,
           WebFeature::kProofreader_Proofread,
           WebFeature::kDeviceBoundSessionRegistered,
