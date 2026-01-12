b9b56bfa14fc97033ab5deefe04fbb75e4c56b6e
Longsheng Yin <longsheng@google.com>
2026-01-12 11:11:55 -0800

    Fix the crash in PixAccountLinkingManager destructor
    Call DismissPrompt() from ChromeFacilitatedPaymentsClient's destructor instead
    
    Bug: 461175062
    Change-Id: I511cec93fd2716d0d7be60ee3565c042f5c55160
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7402143
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Reviewed-by: Siddharth Shah <siashah@google.com>
    Commit-Queue: Longsheng Yin <longsheng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1567925}

--- chrome/browser/facilitated_payments/ui/chrome_facilitated_payments_client_unittest.cc
+++ chrome/browser/facilitated_payments/ui/chrome_facilitated_payments_client_unittest.cc
@@ -62,6 +62,7 @@ class MockPixAccountLinkingManager
               MaybeShowPixAccountLinkingPrompt,
               (const url::Origin& pix_payment_page_origin),
               (override));
+  MOCK_METHOD(void, DismissPrompt, (), (override));
 };
 
 class ChromeFacilitatedPaymentsClientTest
@@ -190,6 +191,17 @@ TEST_F(ChromeFacilitatedPaymentsClientTest,
 
   base_client().ShowPixPaymentPrompt({}, base::DoNothing());
   base_client().ShowProgressScreen();
+}
+
+// Test that DismissPrompt is called when the client is destroyed.
+TEST_F(ChromeFacilitatedPaymentsClientTest,
+       Destructor_DismissesPixAccountLinkingPrompt) {
+  base_client().InitPixAccountLinkingFlow(
+      url::Origin::Create(GURL("https://example.com")));
+
+  EXPECT_CALL(pix_account_linking_manager(), DismissPrompt());
+
+  client_.reset();
 }
 
 // Test the client forwards call for closing the bottom sheet to the
