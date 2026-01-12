2c9b9089a5c989d1f441fce2535b82bfbf4bd9d4
Aashna Sheth <aashnas@google.com>
2026-01-12 09:20:13 -0800

    [Toast] Fix bug with toast showing on actuating tabs
    Before:
    https://screencast.googleplex.com/cast/NjIxODk5MTMzNTg5OTEzNnwxNWRlYjUwMC02YQ
    
    After: http://screencast/cast/NjQ1NDQwMzI3NDg5OTQ1Nnw2Y2Q1ZWUzMy1iNQ
    Note~ there is a case when the toast shows on an actuating tab (when the
    side panel is closed on the tab that is actuating). The cases displayed
    in this screencast are expected.
    
    Bug: 474093341
    Change-Id: I3c011d8fa0b17d9b9627917f4176d71ccba0667b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7413269
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Reviewed-by: Rodney Ding <rodneyding@google.com>
    Reviewed-by: Steven Luong <stluong@chromium.org>
    Commit-Queue: Aashna Sheth <aashnas@google.com>
    Cr-Commit-Position: refs/heads/main@{#1567847}

--- chrome/browser/ui/toasts/toast_service.cc
+++ chrome/browser/ui/toasts/toast_service.cc
@@ -289,6 +289,7 @@ void ToastService::RegisterToasts(
         ToastId::kGeminiWorkingOnTask,
         ToastSpecification::Builder(GetTaskInProgressIcon(),
                                     IDS_TASK_IN_PROGRESS_TOAST_BODY)
+            .AddGlobalScoped()
             .AddCloseButton()
             .Build());
   }
