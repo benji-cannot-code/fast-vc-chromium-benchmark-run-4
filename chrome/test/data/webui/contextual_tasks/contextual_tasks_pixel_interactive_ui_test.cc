bbe4859e36f3e23ded1823b3a41f43a848197535
Juan Mojica <juanmojica@google.com>
2026-01-12 12:20:35 -0800

    [contextual-tasks] Add pixel test for smart compose.
    Bug: 474128338
    Change-Id: Ia14baac334a51538ba56a7dd7e6dd7dbdf0dbfe1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7422371
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1567975}

--- chrome/test/data/webui/contextual_tasks/contextual_tasks_pixel_interactive_ui_test.cc
+++ chrome/test/data/webui/contextual_tasks/contextual_tasks_pixel_interactive_ui_test.cc
@@ -79,6 +79,7 @@ struct ContextualTasksComposeBoxPixelTestParams {
   bool rtl = false;
   bool with_text = false;
   bool is_ai_page = false;
+  bool smart_compose = false;
 
   std::string ToString() const {
     std::string name;
@@ -94,6 +95,9 @@ struct ContextualTasksComposeBoxPixelTestParams {
     }
     if (is_ai_page) {
       name += "_AiPage";
+    }
+    if (smart_compose) {
+      name += "_SmartCompose";
     }
     return name;
   }
@@ -131,6 +135,12 @@ INSTANTIATE_TEST_SUITE_P(
         {.focused = true, .rtl = true},
         {.focused = true, .rtl = true, .with_text = true},
         {.rtl = true, .with_text = true},
+        // Testing smart compose.
+        {.focused = true, .with_text = true, .smart_compose = true},
+        {.focused = true,
+         .dark_mode = true,
+         .with_text = true,
+         .smart_compose = true},
     }),
     [](const testing::TestParamInfo<ContextualTasksComposeBoxPixelTestParams>&
            info) { return info.param.ToString(); });
@@ -184,6 +194,14 @@ IN_PROC_BROWSER_TEST_P(ContextualTasksComposeBoxPixelTest, Screenshots) {
                            true, composed: true}));
                          })"))),
 
+      // Set the smart compose hint if specified.
+      If([]() { return GetParam().smart_compose; },
+         Then(ExecuteJsAt(kActiveTab, kComposebox,
+                          R"((el) => {
+                           el.smartComposeInlineHint_ = ' hint';
+                           el.requestUpdate();
+                         })"))),
+
       // This step is needed to prevent test from failing on platforms that
       // don't support screenshots.
       SetOnIncompatibleAction(OnIncompatibleAction::kIgnoreAndContinue,
@@ -192,7 +210,7 @@ IN_PROC_BROWSER_TEST_P(ContextualTasksComposeBoxPixelTest, Screenshots) {
       // Take a screenshot of the composebox.
       ScreenshotWebUi(kActiveTab, kComposebox,
                       /*screenshot_name=*/"ContextualTasksComposebox",
-                      /*baseline_cl=*/"7398710"));
+                      /*baseline_cl=*/"7422371"));
 }
 
 struct AppPixelTestParams {
@@ -279,7 +297,7 @@ IN_PROC_BROWSER_TEST_P(ContextualTasksAppPixelTest, Screenshots) {
       SetOnIncompatibleAction(OnIncompatibleAction::kIgnoreAndContinue,
                               "Screenshots not captured on this platform."),
       ScreenshotWebUi(kActiveTab, kApp, "ContextualTasksApp",
-                      /*baseline_cl=*/"7398710"));
+                      /*baseline_cl=*/"7422371"));
 }
 
 enum class TitleType { kNone, kShort, kLong };
@@ -394,11 +412,11 @@ IN_PROC_BROWSER_TEST_P(ContextualTasksToolbarPixelTest, Screenshots) {
                   OnIncompatibleAction::kIgnoreAndContinue,
                   "Screenshots not captured on this platform."),
               ScreenshotWebUi(kActiveTab, menu, "ContextualTasksToolbarMenu",
-                              /*baseline_cl=*/"7398710")),
+                              /*baseline_cl=*/"7422371")),
          Else(WaitForWebContentsPainted(kActiveTab),
               SetOnIncompatibleAction(
                   OnIncompatibleAction::kIgnoreAndContinue,
                   "Screenshots not captured on this platform."),
               ScreenshotWebUi(kActiveTab, toolbar, "ContextualTasksToolbar",
-                              /*baseline_cl=*/"7398710"))));
+                              /*baseline_cl=*/"7422371"))));
 }
