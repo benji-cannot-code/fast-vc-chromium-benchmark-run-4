1198e7f7bd56d7e33fffd36ba854c686fa34cf04
Jan Wilken Dörrie <jdoerrie@chromium.org>
2026-01-13 02:54:34 -0800

    Make Profile::ToDebugString() const
    Mark Profile::ToDebugString() as const since it does not modify the
    internal state of the Profile object. This improves const-correctness
    and allows the method to be called on const instances.
    
    Change-Id: I519b911eded0904c09b1321d6e89cabda0882b99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7454246
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Auto-Submit: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1568322}

--- chrome/browser/profiles/profile.cc
+++ chrome/browser/profiles/profile.cc
@@ -610,7 +610,7 @@ base::WeakPtr<Profile> Profile::GetWeakPtr() {
   return weak_factory_.GetWeakPtr();
 }
 
-std::string Profile::ToDebugString() {
+std::string Profile::ToDebugString() const {
   std::ostringstream out;
   out << "(" << this << "):" << (IsRegularProfile() ? " regular" : "")
       << (IsIncognitoProfile() ? " incognito" : "")
