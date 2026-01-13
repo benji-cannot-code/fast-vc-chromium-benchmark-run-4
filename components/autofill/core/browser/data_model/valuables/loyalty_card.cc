e9a36215ad828f8079d4cc63a0f1d5145f51a048
Simge Şengün <simges@google.com>
2026-01-13 09:19:06 -0800

    Persist valuable metadata in ValuablesTable
    Update ValuablesTable to persist valuable metadata (use count and
    use date) by creating the valuables_metadata table.
    
    Fixed: 473523777
    Change-Id: I83af8418df1a558ea40ef95740de486b9daaf6f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7299182
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Simge Şengün <simges@google.com>
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1568466}

--- components/autofill/core/browser/data_model/valuables/loyalty_card.cc
+++ components/autofill/core/browser/data_model/valuables/loyalty_card.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 
+// TODO(crbug.com/473524209): Include metadata as part of the constructor.
 LoyaltyCard::LoyaltyCard(ValuableId id,
                          std::string merchant_name,
                          std::string program_name,
@@ -23,7 +24,8 @@ LoyaltyCard::LoyaltyCard(ValuableId id,
       program_name_(std::move(program_name)),
       program_logo_(std::move(program_logo)),
       loyalty_card_number_(std::move(loyalty_card_number)),
-      merchant_domains_(std::move(merchant_domains)) {}
+      merchant_domains_(std::move(merchant_domains)),
+      valuable_metadata_(id_, base::Time(), 0) {}
 
 LoyaltyCard::LoyaltyCard(const LoyaltyCard&) = default;
 LoyaltyCard::LoyaltyCard(LoyaltyCard&&) = default;
