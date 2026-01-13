1f3daf5a083647b5aa4de4c370916acb4e93b3c1
Tsuyoshi Horo <horo@chromium.org>
2026-01-12 17:29:32 -0800

    Revert "[contextual-tasks] Add pixel test for smart compose."
    This reverts commit dc0355b903ab9fb71cf5f4fb93aafe35d89bace2.
    
    Reason for revert: Caused ContextualTasksComposeBoxPixelTest failures
    
    Bug: 475315112
    
    Original change's description:
    > [contextual-tasks] Add pixel test for smart compose.
    >
    > Bug: 474128338
    > Change-Id: Ia14baac334a51538ba56a7dd7e6dd7dbdf0dbfe1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7422371
    > Reviewed-by: Jason Hu <hujasonx@google.com>
    > Commit-Queue: Juan Mojica <juanmojica@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1567975}
    
    Bug: 474128338
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I8991c814caf6f97858ca42414bab6095eee81220
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7454543
    Owners-Override: Tsuyoshi Horo <horo@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1568180}

--- chrome/test/data/webui/contextual_tasks/contextual_tasks_pixel_interactive_ui_test.cc
+++ chrome/test/data/webui/contextual_tasks/contextual_tasks_pixel_interactive_ui_test.cc
@@ -79,7 +79,6 @@ struct ContextualTasksComposeBoxPixelTestParams {
   bool rtl = false;
   bool with_text = false;
   bool is_ai_page = false;
-  bool smart_compose = false;
 
   std::string ToString() const {
     std::string name;
@@ -95,9 +94,6 @@ struct ContextualTasksComposeBoxPixelTestParams {
     }
     if (is_ai_page) {
       name += "_AiPage";
-    }
-    if (smart_compose) {
-      name += "_SmartCompose";
     }
     return name;
   }
@@ -135,12 +131,6 @@ INSTANTIATE_TEST_SUITE_P(
         {.focused = true, .rtl = true},
         {.focused = true, .rtl = true, .with_text = true},
         {.rtl = true, .with_text = true},
-        // Testing smart compose.
-        {.focused = true, .with_text = true, .smart_compose = true},
-        {.focused = true,
-         .dark_mode = true,
-         .with_text = true,
-         .smart_compose = true},
     }),
     [](const testing::TestParamInfo<ContextualTasksComposeBoxPixelTestParams>&
            info) { return info.param.ToString(); });
@@ -194,14 +184,6 @@ IN_PROC_BROWSER_TEST_P(ContextualTasksComposeBoxPixelTest, Screenshots) {
                            true, composed: true}));
                          })"))),
 
-      // Set the smart compose hint if specified.
-      If([]() { return GetParam().smart_compose; },
-         Then(ExecuteJsAt(kActiveTab, kComposebox,
-                          R"((el) => {
-                           el.smartComposeInlineHint_ = ' hint';
-                           el.requestUpdate();
-                         })"))),
-
       // This step is needed to prevent test from failing on platforms that
       // don't support screenshots.
       SetOnIncompatibleAction(OnIncompatibleAction::kIgnoreAndContinue,
@@ -210,7 +192,7 @@ IN_PROC_BROWSER_TEST_P(ContextualTasksComposeBoxPixelTest, Screenshots) {
       // Take a screenshot of the composebox.
       ScreenshotWebUi(kActiveTab, kComposebox,
                       /*screenshot_name=*/"ContextualTasksComposebox",
-                      /*baseline_cl=*/"7422371"));
+                      /*baseline_cl=*/"7398710"));
 }
 
 struct AppPixelTestParams {
@@ -297,7 +279,7 @@ IN_PROC_BROWSER_TEST_P(ContextualTasksAppPixelTest, Screenshots) {
       SetOnIncompatibleAction(OnIncompatibleAction::kIgnoreAndContinue,
                               "Screenshots not captured on this platform."),
       ScreenshotWebUi(kActiveTab, kApp, "ContextualTasksApp",
-                      /*baseline_cl=*/"7422371"));
+                      /*baseline_cl=*/"7398710"));
 }
 
 enum class TitleType { kNone, kShort, kLong };
@@ -412,11 +394,11 @@ IN_PROC_BROWSER_TEST_P(ContextualTasksToolbarPixelTest, Screenshots) {
                   OnIncompatibleAction::kIgnoreAndContinue,
                   "Screenshots not captured on this platform."),
               ScreenshotWebUi(kActiveTab, menu, "ContextualTasksToolbarMenu",
-                              /*baseline_cl=*/"7422371")),
+                              /*baseline_cl=*/"7398710")),
          Else(WaitForWebContentsPainted(kActiveTab),
               SetOnIncompatibleAction(
                   OnIncompatibleAction::kIgnoreAndContinue,
                   "Screenshots not captured on this platform."),
               ScreenshotWebUi(kActiveTab, toolbar, "ContextualTasksToolbar",
-                              /*baseline_cl=*/"7422371"))));
+                              /*baseline_cl=*/"7398710"))));
 }
