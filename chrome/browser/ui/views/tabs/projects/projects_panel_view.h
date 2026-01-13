be20e46cf8b52bd892dcf5d81ac477f23310f403
David M <davidjm@google.com>
2026-01-13 08:22:11 -0800

    Move ProjectsPanel controls to its own view
    The ProjectsPanel close button has some complex layout logic which will
    only become more complicated as Tab Groups and AI mode are added
    to the ProjectsPanelView. This CL moves the close button to a separate
    controls view, isolating the complex layout logic thus paving the
    way to easily add in Tab Groups and AI mode to the view hierarchy.
    The complex logic could be re-written to use FlexLayout, however, it is
    preferred to leave as is using DelegatingLayout for now until
    the UX mocks are finalized. Our main focus for now is to ensure
    Tab Groups and AI mode can be added to ProjectsPanelView in a
    maintainable and scalable way.
    
    Bug: 475302296
    Change-Id: I3c6760068b4712de55e6d0b9a7aa6bb2a7e725e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7452037
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1568432}

--- chrome/browser/ui/views/tabs/projects/projects_panel_view.h
+++ chrome/browser/ui/views/tabs/projects/projects_panel_view.h
@@ -6,7 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_TABS_PROJECTS_PROJECTS_PANEL_VIEW_H_
 #define CHROME_BROWSER_UI_VIEWS_TABS_PROJECTS_PROJECTS_PANEL_VIEW_H_
 
-#include "ui/views/layout/delegating_layout_manager.h"
+#include "chrome/browser/ui/views/tabs/projects/projects_panel_controls_view.h"
+#include "ui/views/controls/separator.h"
 #include "ui/views/view.h"
 
 namespace gfx {
@@ -15,14 +16,13 @@ class Point;
 
 namespace views {
 class ActionViewController;
-class LabelButton;
 }  // namespace views
 
 class ProjectsPanelStateController;
 
-// The container view for the Projects Panel. This view overlays the Vertical
-// Tab Strip region and is responsible for laying out the panel's exit button.
-class ProjectsPanelView : public views::View, public views::LayoutDelegate {
+// Parent view of the Projects Panel - holds together the views
+// hierarchy including Tab Groups and AI threads.
+class ProjectsPanelView : public views::View {
   METADATA_HEADER(ProjectsPanelView, views::View)
 
  public:
@@ -30,13 +30,6 @@ class ProjectsPanelView : public views::View, public views::LayoutDelegate {
   ProjectsPanelView(const ProjectsPanelView&) = delete;
   ProjectsPanelView& operator=(const ProjectsPanelView&) = delete;
   ~ProjectsPanelView() override;
-
-  // LayoutDelegate:
-  views::ProposedLayout CalculateProposedLayout(
-      const views::SizeBounds& size_bounds) const override;
-
-  // Creates a TopContainerButton based on an ActionId.
-  views::LabelButton* AddChildButtonFor(actions::ActionId action_id);
 
   bool IsPositionInWindowCaption(const gfx::Point& point);
 
@@ -47,7 +40,7 @@ class ProjectsPanelView : public views::View, public views::LayoutDelegate {
 
  private:
   raw_ptr<actions::ActionItem> root_action_item_ = nullptr;
-  raw_ptr<views::LabelButton> projects_button_ = nullptr;
+  raw_ptr<ProjectsPanelControlsView> controls_view_ = nullptr;
 
   std::unique_ptr<views::ActionViewController> action_view_controller_;
 };
