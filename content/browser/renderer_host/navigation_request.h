e3aef639895efaca50fb62c81f28c211ad13071c
Shivani Sharma <shivanisha@chromium.org>
2026-01-13 08:16:38 -0800

    [Connection-Allowlist] Enforce network restrictions on navigations
    Explainer: https://github.com/WICG/connection-allowlists
    
    This CL enforces connection-allowlist header to be checked for
    navigations. The earlier CL https://chromium-review.googlesource.com/c/chromium/src/+/7085134 enforced the check
    for subresources in the network service while navigation will be checked
    in the browser process at the time the navigation is started.
    TODO: Note that this CL does not check for redirects and the committed
    URL since that behavior has not been decided yet
    (https://github.com/WICG/connection-allowlists?tab=readme-ov-file#youre-going-to-have-to-rethink-a-ban-on-redirects)
    
    It tests that only renderer-initiated navigations should be blocked and
    not the ones initiated from the browser, like via the omnibox. Also
    tests NavigationStateKeepAlive code path.
    
    Bug: 447954811
    
    Change-Id: I467305807034d96eab59ac08be642a75233a7de3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7276327
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Shivani Sharma <shivanisha@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1568424}

--- content/browser/renderer_host/navigation_request.h
+++ content/browser/renderer_host/navigation_request.h
@@ -1918,6 +1918,10 @@ class CONTENT_EXPORT NavigationRequest
   // call.
   void CommitPageActivation();
 
+  // Checks whether this navigation is allowed based on the connection
+  // allowlist header, if present.
+  bool IsAllowedByConnectionAllowlist();
+
   // Checks if the specified CSP context's relevant CSP directive
   // allows the navigation. This is called to perform the frame-src check.
   bool IsAllowedByCSPDirective(
