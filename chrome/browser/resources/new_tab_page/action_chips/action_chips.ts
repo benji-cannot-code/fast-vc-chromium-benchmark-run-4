b3fac20c3891e187d54aebc06216be1cdf343b95
Omar Flores <omarflores@google.com>
2026-01-13 10:57:31 -0800

    [ntp-next] Matching hover text for normal chip and rows
    Screenshot: https://screenshot.googleplex.com/4CwQLVmnBEdYcq3
    
    Bug: 475486536
    Change-Id: I518a99d22144457de9d68120891e5feeb5e03bc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7456107
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Auto-Submit: Omar Flores <omarflores@google.com>
    Cr-Commit-Position: refs/heads/main@{#1568562}

--- chrome/browser/resources/new_tab_page/action_chips/action_chips.ts
+++ chrome/browser/resources/new_tab_page/action_chips/action_chips.ts
@@ -242,8 +242,7 @@ export class ActionChipsElement extends CrLitElement {
     }
     const url = new URL(chip.tab.url.url);
     const domain = url.hostname.replace(/^www\./, '');
-    return `${this.showSimplifiedUI_ ? chip.suggestion : chip.title} - ${
-        domain}`;
+    return `${chip.suggestion} - ${domain}`;
   }
 
   protected isDeepDiveChip_(chip: ActionChip) {
