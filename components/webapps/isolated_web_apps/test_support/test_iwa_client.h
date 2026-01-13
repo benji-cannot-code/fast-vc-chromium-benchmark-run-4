96e8079c062d8349810c9e9cc45383a5681a058c
Andrew Rayskiy <greengrape@google.com>
2026-01-13 09:11:11 -0800

    Run IWA trust checks while getting the IwaSource from //chrome
    The IWA URL loading logic runs a trust check once for every request,
    normally after obtaining the bundle/proxy source to fetch from. This CL
    merges the two functions into one so that the URLLoaderFactory no longer
    has to think or know about dev_mode.
    
    Bug: 474076928
    Change-Id: Ifb4354b44cc4e43a21d327039fa01d457dea1700
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7409667
    Reviewed-by: Simon Hangl <simonha@google.com>
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Cr-Commit-Position: refs/heads/main@{#1568463}

--- components/webapps/isolated_web_apps/test_support/test_iwa_client.h
+++ components/webapps/isolated_web_apps/test_support/test_iwa_client.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notreached.h"
 #include "components/web_package/signed_web_bundles/signed_web_bundle_id.h"
 #include "components/webapps/isolated_web_apps/client.h"
-#include "testing/gmock/include/gmock/gmock.h"
 
 namespace web_app::test {
 
@@ -24,23 +23,10 @@ class TestIwaClient : public IwaClient {
       const web_package::SignedWebBundleId& web_bundle_id,
       const network::ResourceRequest& request,
       const std::optional<content::FrameTreeNodeId>& frame_tree_node,
-      base::OnceCallback<void(
-          base::expected<IwaSourceWithModeOrGeneratedResponse, std::string>)>
-          callback) override;
+      base::OnceCallback<
+          void(base::expected<IwaSourceWithModeOrGeneratedResponse,
+                              SourceRequestError>)> callback) override;
   IwaRuntimeDataProvider* GetRuntimeDataProvider() override;
-};
-
-class MockIwaClient : public TestIwaClient {
- public:
-  MockIwaClient();
-  ~MockIwaClient() override;
-
-  MOCK_METHOD((base::expected<void, std::string>),
-              ValidateTrust,
-              (content::BrowserContext*,
-               const web_package::SignedWebBundleId&,
-               bool),
-              (override));
 };
 
 }  // namespace web_app::test
