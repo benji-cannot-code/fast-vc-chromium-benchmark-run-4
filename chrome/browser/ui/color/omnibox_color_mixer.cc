f0570ee72046116bb46978981bfbaf6cafb1504a
Marlon Facey <mfacey@chromium.org>
2026-01-13 09:03:43 -0800

    [Omnibox Next] Add hcm color for lens chip
    Before: http://screenshot/6a6FbbtbxinC62D
    
    After:
    Dark: http://screenshot/4RxjbkLnuq4CBtJ
    Light: http://screenshot/zDYtqo3apdpbeEu
    
    Bug: b:474138552
    Change-Id: I23364b9ce3b37cfbf1053ea36268d4c8c31593cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7417954
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1568456}

--- chrome/browser/ui/color/omnibox_color_mixer.cc
+++ chrome/browser/ui/color/omnibox_color_mixer.cc
@@ -455,9 +455,10 @@ void AddOmniboxColorMixer(ui::ColorProvider* provider,
   mixer[kColorOmniboxComposeboxFaviconBackground] = {
       kColorOmniboxComposeboxChipBackground};
   mixer[kColorOmniboxComposeboxForegroundDisabled] = {kColorOmniboxTextDimmed};
-  mixer[kColorOmniboxComposeboxSubmitButtonBackground] =
-      ui::SelectBasedOnDarkInput(kColorOmniboxResultsBackground,
-                                 gfx::kGoogleBlue200, gfx::kGoogleBlue600);
+  mixer[kColorOmniboxComposeboxPrimaryAction] = ui::SelectBasedOnDarkInput(
+      kColorOmniboxResultsBackground, gfx::kGoogleBlue200, gfx::kGoogleBlue600);
+  mixer[kColorOmniboxComposeboxSubmitButtonBackground] = {
+      kColorOmniboxComposeboxPrimaryAction};
   mixer[kColorOmniboxComposeboxSubmitButtonIcon] = ui::SelectBasedOnDarkInput(
       kColorOmniboxResultsBackground, gfx::kGoogleGrey900, gfx::kGoogleBlue200);
 
