a2749ea292cb51f8b9ea4cd460762baa09e61d04
Brandon Wylie <wylieb@google.com>
2026-01-12 17:03:47 -0800

    [sheriff] Disable failing NewTabPageTest
    Bug: 475323609
    Change-Id: Ife347a2a3b42fdc7ad3e2ec2e3fc433357a3f806
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7454809
    Owners-Override: Brandon Wylie <wylieb@google.com>
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Auto-Submit: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Brandon Fong <bjfong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1568166}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ntp/NewTabPageTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ntp/NewTabPageTest.java
@@ -588,6 +588,7 @@ public class NewTabPageTest {
     @Test
     @SmallTest
     @Feature({"NewTabPage", "FeedNewTabPage"})
+    @DisabledTest(message = "crbug.com/475323609")
     public void testFeedReliabilityLoggingFocusOmnibox() throws IOException {
         mNtp.getCoordinatorForTesting().setReliabilityLoggerForTesting(mFeedReliabilityLogger);
 
