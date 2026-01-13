4e801472e7d810d6a7aa5eefad16bcd35a7f8b32
Will Harris <wfh@chromium.org>
2026-01-13 09:20:18 -0800

    Broker link-local connections from sandboxed network service
    On some systems, Windows Firewall blocks connections to link-local
    addresses.
    
    It is believed this is caused by the network profile of the interface
    being not marked as 'private', and so the "UWP Default Outbound Block
    Rule" policy blocks it.
    
    This does not seem to happen on fresh Windows 11 installs, but can
    happen on older Windows 10 systems as well as those previously updated
    from Windows 10 to Windows 11.
    
    This CL changes the broker rules so brokered connections include those
    destined for loopback and local IP addresses (which are always blocked,
    and documented to be blocked) as well as connections over any interface
    to any link-local address.
    
    Tested manually on a Windows 10 machine which was blocking access
    previously with ERR_NETWORK_ACCESS_DENIED, now that access is permitted.
    
    BUG=466139402
    
    Change-Id: I8633bf19ee63b8a559845f1866fc5c58607bca66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7456207
    Commit-Queue: Will Harris <wfh@chromium.org>
    Reviewed-by: mmenke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1568468}

--- services/network/broker_helper_win.cc
+++ services/network/broker_helper_win.cc
@@ -5,7 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/network/broker_helper_win.h"
 
+#include <algorithm>
+
 #include "base/no_destructor.h"
+#include "base/numerics/safe_conversions.h"
 #include "base/threading/thread_local.h"
 #include "net/base/ip_address.h"
 #include "net/base/network_change_notifier.h"
@@ -41,13 +44,33 @@ bool BrokerHelperWin::ShouldBroker(const net::IPAddress& address) const {
   if (delegate_) {
     return delegate_->ShouldBroker();
   }
-  if (address.IsLoopback())
+
+  // Windows firewall blocks connections from App-Container to loopback
+  // addresses due to rule `AppContainerLoopback`.
+  if (address.IsLoopback()) {
     return true;
+  }
 
   for (const auto& network_interface : interfaces_) {
-    if (network_interface.address == address)
+    // `IPAddressMatchesPrefix` CHECKs for invalid prefix lengths but
+    // `prefix_length` comes from Windows where "a value of 255 is commonly used
+    // to represent an illegal value".
+    const uint32_t prefix_length =
+        std::min(network_interface.address.size() * 8,
+                 base::strict_cast<size_t>(network_interface.prefix_length));
+
+    // Windows firewall blocks connections to local network interfaces from
+    // App-Container due to rule `AppContainerLoopback`.
+    //
+    // In addition, on some hosts, link-local traffic through network interfaces
+    // that are not assigned a network connection profile are blocked due to
+    // rule 'UWP Default Outbound Block Rule`, so broker these connections.
+    if (net::IPAddressMatchesPrefix(address, network_interface.address,
+                                    prefix_length)) {
       return true;
+    }
   }
+
   return false;
 }
 
