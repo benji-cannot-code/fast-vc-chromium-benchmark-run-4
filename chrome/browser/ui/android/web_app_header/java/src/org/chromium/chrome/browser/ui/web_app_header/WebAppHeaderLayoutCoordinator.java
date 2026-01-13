79ce2446d367788cf7fc2f191ca8e580a66c645b
Adriana Ixba <aixba@chromium.org>
2026-01-13 14:36:54 -0800

    [TWA] Add query API to AconfigFlaggedApiDelegate
    This adds the query API to determine if an app is installed via
    the TWA installer. This change also implements it in WebappsUtil
    and makes use of it in WebAppHeaderLayoutCoordinator to help
    determine showing the origin.
    
    Next will be to implement the method in AconfigFlaggedDelegateImpl
    
    Bug: 473539016
    Change-Id: I860c1abc0fdfe72a6b4380b3257b0ebdcc6cf8a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7415369
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Adriana Ixba <aixba@chromium.org>
    Reviewed-by: Yi Xie <yixie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1568730}

--- chrome/browser/ui/android/web_app_header/java/src/org/chromium/chrome/browser/ui/web_app_header/WebAppHeaderLayoutCoordinator.java
+++ chrome/browser/ui/android/web_app_header/java/src/org/chromium/chrome/browser/ui/web_app_header/WebAppHeaderLayoutCoordinator.java
@@ -55,6 +55,7 @@ import org.chromium.components.browser_ui.widget.animation.CancelAwareAnimatorLi
 import org.chromium.components.browser_ui.widget.scrim.ScrimManager;
 import org.chromium.components.embedder_support.util.Origin;
 import org.chromium.components.url_formatter.UrlFormatter;
+import org.chromium.components.webapps.WebappsUtils;
 import org.chromium.content_public.browser.NavigationHandle;
 import org.chromium.ui.base.WindowAndroid;
 import org.chromium.ui.display.DisplayAndroid;
@@ -254,8 +255,18 @@ public class WebAppHeaderLayoutCoordinator extends EmptyTabObserver
         onAndroidControlsVisibilityChanged(
                 mBrowserControlsStateProvider.getAndroidControlsVisibility());
 
-        if (mIsTWA && ChromeFeatureList.sAndroidTwaOriginDisplay.isEnabled()) {
-            mAppOriginView = (TextView) mView.findViewById(R.id.origin);
+        if (mIsTWA
+                && ChromeFeatureList.sAndroidTwaOriginDisplay.isEnabled()
+                && mClientPackageName != null) {
+            // Show origin only for TWA Installer installed apps.
+            WebappsUtils.isTwaInstallerPackage(
+                    mClientPackageName,
+                    (isTwaInstallerPackage) -> {
+                        if (isTwaInstallerPackage) {
+                            assert mView != null;
+                            mAppOriginView = (TextView) mView.findViewById(R.id.origin);
+                        }
+                    });
         }
 
         mMediator.getUnoccludedWidthSupplier().addObserver(mOnUnoccludedWidthCallback);
