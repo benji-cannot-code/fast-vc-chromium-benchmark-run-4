74c888ce323e808ec843f9d31eea5f08dd989260
Nasser Al-shawwa <alshawwa@chromium.org>
2026-01-13 13:04:51 -0800

    [Force Save to Cloud] Update download bubble message
    Bug: 473022803
    Change-Id: I60fc54770f7dd05ad0f444b0d347e868c13faf4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7463208
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Nasser Al-shawwa <alshawwa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1568661}

--- chrome/browser/download/download_ui_model.cc
+++ chrome/browser/download/download_ui_model.cc
@@ -84,10 +84,11 @@ std::u16string FailStateDescription(FailState fail_state) {
     case FailState::FILE_TRANSIENT_ERROR:
       string_id = IDS_DOWNLOAD_INTERRUPTED_DESCRIPTION_TEMPORARY_PROBLEM;
       break;
-    // TODO(alshawwa): Handle LOCAL_DOWNLOAD_BLOCKED separately
-    case FailState::LOCAL_DOWNLOAD_BLOCKED:
     case FailState::FILE_BLOCKED:
       string_id = IDS_DOWNLOAD_INTERRUPTED_DESCRIPTION_BLOCKED;
+      break;
+    case FailState::LOCAL_DOWNLOAD_BLOCKED:
+      string_id = IDS_DOWNLOAD_INTERRUPTED_STATUS_LOCAL_DOWNLOAD_BLOCKED;
       break;
     case FailState::FILE_SECURITY_CHECK_FAILED:
       string_id = IDS_DOWNLOAD_INTERRUPTED_DESCRIPTION_SECURITY_CHECK_FAILED;
@@ -1222,10 +1223,11 @@ DownloadUIModel::BubbleStatusTextBuilder::GetInterruptedStatusText(
     case FailState::FILE_VIRUS_INFECTED:
       string_id = IDS_DOWNLOAD_INTERRUPTED_STATUS_VIRUS;
       break;
-    // TODO(alshawwa): Handle LOCAL_DOWNLOAD_BLOCKED separately.
-    case FailState::LOCAL_DOWNLOAD_BLOCKED:
     case FailState::FILE_BLOCKED:
       string_id = IDS_POLICY_ACTION_BLOCKED_BY_ORGANIZATION;
+      break;
+    case FailState::LOCAL_DOWNLOAD_BLOCKED:
+      string_id = IDS_DOWNLOAD_INTERRUPTED_STATUS_LOCAL_DOWNLOAD_BLOCKED;
       break;
     case FailState::FILE_SECURITY_CHECK_FAILED:
       string_id = IDS_DOWNLOAD_INTERRUPTED_STATUS_SECURITY_CHECK_FAILED;
