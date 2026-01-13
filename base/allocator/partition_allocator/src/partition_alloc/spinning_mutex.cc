468b6feee5a21aac1ef03c909ba1c7a0e1b760f7
Victor Hugo Vianna Silva <victorvianna@google.com>
2026-01-12 21:56:00 -0800

    Promote #warnings to #error
    #warning was only standardized in C++23, so it shouldn't be used before
    C++23 is marked as allowed. Also, from thakis@, the directive "doesn't
    honor -Werror, and we want things to either be errors, or silent". So
    flip to #error.
    
    Change-Id: I1fbc4dec990b669889b676c3118cb2b0b5db170b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7368066
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1568254}

--- base/allocator/partition_allocator/src/partition_alloc/spinning_mutex.cc
+++ base/allocator/partition_allocator/src/partition_alloc/spinning_mutex.cc
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sched.h>
 #define PA_YIELD_THREAD sched_yield()
 #else  // Other OS
-#warning "Thread yield not supported on this OS."
+#error "Thread yield not supported on this OS."
 #define PA_YIELD_THREAD ((void)0)
 #endif
 
