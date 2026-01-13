d7f393c13dba52c88be9591da50d1867ba9995ab
Vladimir Levin <vmpstr@chromium.org>
2026-01-13 09:04:18 -0800

    Overscroll: Ensure to check for overscroll areas before V2 actions.
    R=flackr@chromium.org
    
    Change-Id: Ia98094ef00644fdafaac613c24dbb67de35a9893
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7457533
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1568457}

--- third_party/blink/renderer/core/html/html_element.cc
+++ third_party/blink/renderer/core/html/html_element.cc
@@ -2772,6 +2772,12 @@ CommandEventType HTMLElement::GetCommandEventType(
     }
   }
 
+  // Overscroll gestures.
+  if (RuntimeEnabledFeatures::OverscrollGesturesEnabled() &&
+      EqualIgnoringASCIICase(action, keywords::kToggleOverscroll)) {
+    return CommandEventType::kToggleOverscroll;
+  }
+
   // V2 commands go below this point
 
   if (!RuntimeEnabledFeatures::HTMLCommandActionsV2Enabled()) {
@@ -2851,11 +2857,6 @@ CommandEventType HTMLElement::GetCommandEventType(
     if (EqualIgnoringASCIICase(action, keywords::kPageInlineEnd)) {
       return CommandEventType::kPageInlineEnd;
     }
-  }
-
-  if (RuntimeEnabledFeatures::OverscrollGesturesEnabled() &&
-      EqualIgnoringASCIICase(action, keywords::kToggleOverscroll)) {
-    return CommandEventType::kToggleOverscroll;
   }
 
   return CommandEventType::kNone;
