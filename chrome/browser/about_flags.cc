315bf1cfc3f8cd61f5b1e08b5abac977f096e661
Elias Homsi <eliashomsi@google.com>
2026-01-13 08:19:14 -0800

    [Force Save to OneDrive]  Add chrome://flags for enabling force save to one drive
    Bug: 458033434
    Change-Id: I3c3da31a7d9aa8aba29d3bc1bf06922be4733225
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7456044
    Commit-Queue: Nancy Xiao <nancylanxiao@google.com>
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Auto-Submit: Elias Homsi <eliashomsi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1568428}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -340,6 +340,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC) || \
     BUILDFLAG(IS_WIN)
 #include "chrome/browser/contextual_cueing/contextual_cueing_features.h"  // nogncheck
+#include "chrome/browser/enterprise/data_protection/data_protection_features.h"
 #include "chrome/browser/enterprise/profile_management/profile_management_features.h"
 #include "chrome/browser/enterprise/webstore/features.h"
 #include "components/infobars/core/features.h"
@@ -7061,6 +7062,12 @@ const FeatureEntry kFeatureEntries[] = {
              kContextualSuggestionsAblateOthersWhenPresent,
          kContextualSuggestionsAblateOthersWhenPresentVariations,
          "ContextualSuggestionsAblateOthersWhenPresent")},
+
+    {"enable-force-download-to-onedrive",
+     flag_descriptions::kEnableForceDownloadToOneDriveName,
+     flag_descriptions::kEnableForceDownloadToOneDriveDescription, kOsDesktop,
+     FEATURE_VALUE_TYPE(
+         enterprise_data_protection::kEnableForceDownloadToOneDrive)},
 
     {"omnibox-contextual-search-on-focus-suggestions",
      flag_descriptions::kOmniboxContextualSearchOnFocusSuggestionsName,
