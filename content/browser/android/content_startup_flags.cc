c6de6c93c362e9f54299a5c281021734992c5bbe
Bo Liu <boliu@chromium.org>
2026-01-13 14:37:04 -0800

    android: Properly enable kXrDevice
    Just setting enable-features command line switch overwrites everything
    else that was there already, meaning some debug mechanisms to enable
    features no longer works, just for XR devices. Do it properly instead.
    
    Change-Id: Id71e61032fa670b74c8fd8d22e92b49436a3c646
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7456981
    Auto-Submit: Bo Liu <boliu@chromium.org>
    Commit-Queue: Bo Liu <boliu@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1568731}

--- content/browser/android/content_startup_flags.cc
+++ content/browser/android/content_startup_flags.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/android_info.h"
 #include "base/base_switches.h"
 #include "base/command_line.h"
+#include "base/scoped_add_feature_flags.h"
 #include "base/system/sys_info.h"
 #include "cc/base/switches.h"
 #include "components/input/switches.h"
@@ -19,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/vr/public/cpp/features.h"
 #endif
 #include "gpu/command_buffer/service/gpu_switches.h"
+#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/common/switches.h"
 #include "ui/base/ui_base_switches.h"
 
@@ -64,8 +66,8 @@ void SetContentCommandLineFlags(bool single_process) {
 
 #if BUILDFLAG(IS_ANDROID) && BUILDFLAG(ENABLE_VR)
   if (device::features::IsXrDevice()) {
-    parsed_command_line->AppendSwitchASCII(switches::kEnableFeatures,
-                                           "XrDevice");
+    base::ScopedAddFeatureFlags scoped_add_features(parsed_command_line);
+    scoped_add_features.EnableIfNotSet(blink::features::kXrDevice);
   }
 #endif
 }
