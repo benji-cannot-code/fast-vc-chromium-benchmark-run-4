4d818bf2e0ea8e5c9edcd56dcd5538bf3eb68c21
Sebastien Lalancette <seblalancette@chromium.org>
2026-01-13 10:58:49 -0800

    Cleanup of the ManagedBrowserClientCertificateEnabled flag
    It has been enabled since M136.
    
    Fixed: b:369665023
    Change-Id: I7a1c73133cd204bc8eace50acc60ed9ab1da1a9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6700259
    Commit-Queue: Sébastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1568564}

--- components/enterprise/client_certificates/core/features.cc
+++ components/enterprise/client_certificates/core/features.cc
@@ -7,13 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace client_certificates::features {
 
-BASE_FEATURE(kManagedBrowserClientCertificateEnabled,
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
-bool IsManagedBrowserClientCertificateEnabled() {
-  return base::FeatureList::IsEnabled(kManagedBrowserClientCertificateEnabled);
-}
-
 BASE_FEATURE(kEnableClientCertificateProvisioningOnAndroid,
              base::FEATURE_ENABLED_BY_DEFAULT);
 
