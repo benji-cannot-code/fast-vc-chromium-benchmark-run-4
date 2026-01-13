1ec6370bc0093787070626e6c9e9d9f2fa59ef4b
Fredrik Söderquist <fs@opera.com>
2026-01-13 02:04:09 -0800

    Remove unused StringImpl::GetCharacters()
    Bug: 451652367
    Change-Id: Ib7385e82b38f7abba51299d0b2b9dde2eb24bdbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7454136
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1568303}

--- third_party/blink/renderer/platform/wtf/text/string_impl.h
+++ third_party/blink/renderer/platform/wtf/text/string_impl.h
@@ -218,10 +218,6 @@ class WTF_EXPORT StringImpl {
   // The character content is always copied.
   std::u16string ToU16String() const;
 
-  // Use Span instead.
-  template <typename CharType>
-  UNSAFE_BUFFER_USAGE ALWAYS_INLINE const CharType* GetCharacters() const;
-
   template <typename CharType>
   ALWAYS_INLINE base::span<CharType> Span() const;
 
@@ -653,16 +649,6 @@ class WTF_EXPORT StringImpl {
   const unsigned length_;
   mutable std::atomic<uint32_t> hash_and_flags_;
 };
-
-template <>
-ALWAYS_INLINE const LChar* StringImpl::GetCharacters<LChar>() const {
-  return Characters8();
-}
-
-template <>
-ALWAYS_INLINE const UChar* StringImpl::GetCharacters<UChar>() const {
-  return Characters16();
-}
 
 template <>
 ALWAYS_INLINE base::span<LChar> StringImpl::Span<LChar>() const {
