af62c2e560da01248201fcd01690b12337fabe10
Jenna Himawan <jhimawan@google.com>
2026-01-13 13:21:35 -0800

    Allow showing and hiding the magic stack
    Demo: https://screencast.googleplex.com/cast/NDkwMzY4NDk4MTkxNTY0OHw3NTA3Y2MxYS05YQ
    
    Demo with split screen mode: https://screencast.googleplex.com/cast/NjY0NTMxMTQwMDMxMjgzMnw3MzVhZTFjMC01Zg
    
    Bug: 460513504
    Change-Id: If30be76ff9224fc682c7ae5b73b48f2a140e034e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7453416
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1568675}

--- chrome/browser/magic_stack/android/java/src/org/chromium/chrome/browser/magic_stack/HomeModulesCoordinator.java
+++ chrome/browser/magic_stack/android/java/src/org/chromium/chrome/browser/magic_stack/HomeModulesCoordinator.java
@@ -9,6 +9,7 @@ import static org.chromium.build.NullUtil.assumeNonNull;
 
 import android.app.Activity;
 import android.os.SystemClock;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.VisibleForTesting;
@@ -23,6 +24,7 @@ import org.chromium.base.ResettersForTesting;
 import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.magic_stack.ModuleRegistry.OnViewCreatedCallback;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.tab.Tab;
@@ -95,8 +97,10 @@ public class HomeModulesCoordinator implements ModuleDelegate, OnViewCreatedCall
 
                     @Override
                     public void allCardsConfigChanged(boolean isEnabled) {
-                        // TODO(crbug.com/7142982): If all cards are turned off, reflect that on the
-                        // NTP.
+                        if (ChromeFeatureList.isEnabled(
+                                ChromeFeatureList.HOME_MODULE_PREF_REFACTOR)) {
+                            mRecyclerView.setVisibility(isEnabled ? View.VISIBLE : View.GONE);
+                        }
                     }
                 };
         mHomeModulesConfigManager.addListener(mHomeModulesStateListener);
