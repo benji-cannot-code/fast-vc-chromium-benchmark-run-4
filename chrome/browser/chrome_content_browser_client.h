d1cb24757c60e054b90c197c7a8a297f170118b5
Ari Chivukula <arichiv@chromium.org>
2026-01-13 09:36:11 -0800

    3PSP Enterprise Policy - Remove
    Once we've passed the M145 branch cut, we can remove this from the code
    (except for the base policy definition which is needed for documentation
    on the website of what this preference was).
    
    Fixed: 425248669
    Change-Id: Ic8ec78e406146adbeecaabdc2a64c56f9eb64798
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6761414
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1568478}

--- chrome/browser/chrome_content_browser_client.h
+++ chrome/browser/chrome_content_browser_client.h
@@ -1046,10 +1046,6 @@ class ChromeContentBrowserClient : public content::ContentBrowserClient {
   bool AreIsolatedWebAppsEnabled(
       content::BrowserContext* browser_context) override;
 
-  bool IsThirdPartyStoragePartitioningAllowed(
-      content::BrowserContext* browser_context,
-      const url::Origin& top_level_origin) override;
-
   bool AreDeprecatedAutomaticBeaconCredentialsAllowed(
       content::BrowserContext* browser_context,
       const GURL& destination_url,
