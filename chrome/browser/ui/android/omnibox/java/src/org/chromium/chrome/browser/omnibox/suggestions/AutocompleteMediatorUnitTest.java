6d14599e7900ccf192f1561136e2d0fa8ff38b42
Jiacheng Guo <gjc@google.com>
2026-01-13 00:56:45 -0800

    Implement Prewarm user interaction trigger on Android
    The CL implements the logic to trigger the prewarm page loading when the
    user interacts with the omnibox on Android. The more conservative
    trigger will help to reduce the server capacity increase and the
    renderer OOM events.
    
    Bug: 467861539
    Change-Id: If015954999044b5b95ffdd7b87d5bf0b2380e5d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7414443
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Keita Suzuki <suzukikeita@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Jiacheng Guo <gjc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1568286}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteMediatorUnitTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteMediatorUnitTest.java
@@ -67,6 +67,7 @@ import org.chromium.chrome.browser.omnibox.fusebox.FuseboxCoordinator;
 import org.chromium.chrome.browser.omnibox.styles.OmniboxResourceProvider;
 import org.chromium.chrome.browser.omnibox.suggestions.header.HeaderProcessor;
 import org.chromium.chrome.browser.omnibox.test.R;
+import org.chromium.chrome.browser.preloading.PreloadingFeatureMap;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.Tab.LoadUrlResult;
@@ -86,6 +87,7 @@ import org.chromium.components.omnibox.action.OmniboxActionDelegate;
 import org.chromium.components.omnibox.action.OmniboxActionFactoryJni;
 import org.chromium.components.omnibox.suggestions.OmniboxSuggestionUiType;
 import org.chromium.content_public.browser.NavigationHandle;
+import org.chromium.content_public.browser.WebContents;
 import org.chromium.ui.base.WindowAndroid;
 import org.chromium.ui.insets.InsetObserver;
 import org.chromium.ui.modaldialog.ModalDialogManager;
@@ -133,6 +135,7 @@ public class AutocompleteMediatorUnitTest {
             mVisualStateObserver;
     private @Mock DeferredIMEWindowInsetApplicationCallback mDeferredImeCallback;
     private @Mock FuseboxCoordinator mFuseboxCoordinator;
+    private @Mock PreloadingFeatureMap mPreloadingFeatureMap;
     private @Captor ArgumentCaptor<OmniboxLoadUrlParams> mOmniboxLoadUrlParamsCaptor;
     private @Mock CachedZeroSuggestionsManager.OverridesForTesting
             mMockCachedZeroSuggestionsManager;
@@ -153,6 +156,7 @@ public class AutocompleteMediatorUnitTest {
                         ContextUtils.getApplicationContext(), R.style.Theme_BrowserUI_DayNight);
 
         CachedZeroSuggestionsManager.setOverridesForTesting(mMockCachedZeroSuggestionsManager);
+        PreloadingFeatureMap.setInstanceForTesting(mPreloadingFeatureMap);
         LargeIconBridgeJni.setInstanceForTesting(mLargeIconBridgeJniMock);
         OmniboxActionFactoryJni.setInstanceForTesting(mActionFactoryJni);
         AutocompleteControllerJni.setInstanceForTesting(mControllerJniMock);
@@ -671,6 +675,23 @@ public class AutocompleteMediatorUnitTest {
         ShadowLooper.runUiThreadTasks();
         verify(mAutocompleteController, never()).start(any(), anyInt(), anyBoolean());
         verifyAutocompleteStartZeroSuggest("", url, pageClassification, title);
+    }
+
+    @Test
+    @SmallTest
+    public void onSuggestionsReceived_triggersPrewarm() {
+        mMediator.setAutocompleteProfile(mProfile);
+        mMediator.onNativeInitialized();
+        mMediator.onOmniboxSessionStateChange(true);
+
+        when(mPreloadingFeatureMap.shouldPrewarmOnAutocomplete()).thenReturn(true);
+        Tab tab = mock(Tab.class);
+        WebContents webContents = mock(WebContents.class);
+        when(mLocationBarDataProvider.getTab()).thenReturn(tab);
+        when(tab.getWebContents()).thenReturn(webContents);
+
+        mMediator.onSuggestionsReceived(mAutocompleteResult, /* isFinal= */ false);
+        verify(mAutocompleteController).startPrewarm(eq(webContents));
     }
 
     @Test
