c2b58f7020eb413e42cd80ba38dba60c1de3eb32
Bryant Chandler <bryantchandler@google.com>
2026-01-13 12:52:22 -0800

    [glic] Add glic_pinned_tab_manager_impl to android build
    Bug: b:470059315
    Change-Id: Ib4517195ab292a035cd8eaaa5fe5a8716a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7455514
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1568650}

--- chrome/browser/glic/host/context/glic_pinned_tab_manager_impl.cc
+++ chrome/browser/glic/host/context/glic_pinned_tab_manager_impl.cc
@@ -25,17 +25,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/glic/host/glic.mojom.h"
 #include "chrome/browser/glic/service/glic_instance_helper.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/browser_tab_strip_tracker.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface_iterator.h"
-#include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "components/prefs/pref_service.h"
+#include "content/public/browser/navigation_controller.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/page.h"
 #include "content/public/browser/web_contents.h"
 #include "glic_pinned_tab_manager.h"
 #include "url/origin.h"
+
+#if !BUILDFLAG(IS_ANDROID)
+#include "chrome/browser/ui/tabs/tab_strip_model.h"
+#endif
 
 namespace glic {
 
@@ -513,6 +515,9 @@ void GlicPinnedTabManagerImpl::SendPinCandidatesUpdate() {
 std::vector<content::WebContents*>
 GlicPinnedTabManagerImpl::GetUnsortedPinCandidates() {
   std::vector<content::WebContents*> candidates;
+#if !BUILDFLAG(IS_ANDROID)  // NEEDS_ANDROID_IMPL: This can be done one
+                            // BrowserWindowInterface::GetAllTabs is available
+                            // on Android.
   ForEachCurrentBrowserWindowInterfaceOrderedByActivation(
       [this, &candidates](BrowserWindowInterface* browser_window_interface) {
         if (browser_window_interface->GetProfile() != profile_ ||
@@ -541,6 +546,7 @@ GlicPinnedTabManagerImpl::GetUnsortedPinCandidates() {
         }
         return true;
       });
+#endif
   return candidates;
 }
 
