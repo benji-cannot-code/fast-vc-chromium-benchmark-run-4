89cd22118a0ed194aa4ed38ca42e4d23fcd64a8d
Victor Hugo Vianna Silva <victorvianna@google.com>
2026-01-13 11:40:05 -0800

    Migrate base::Contains() to std::ranges::contains() in tools
    base::Contains() is an alias to std::ranges::contains() since
    crrev.com/c/7415207, so this CL has no behavior change.
    
    Bug: 470391351
    Change-Id: Id51a13a94fde031540d65e2f83dcded103e78d4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7460965
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Alex Turner <alexmt@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1568593}

--- tools/aggregation_service/aggregation_service_tool_main.cc
+++ tools/aggregation_service/aggregation_service_tool_main.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 
 #include "base/command_line.h"
-#include "base/containers/contains.h"
 #include "base/files/file_path.h"
 #include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
@@ -153,7 +152,7 @@ int main(int argc, char* argv[]) {
       kSwitchApiVersion,
       kSwitchApi};
   for (const auto& provided_switch : command_line.GetSwitches()) {
-    if (!base::Contains(kAllowedSwitches, provided_switch.first)) {
+    if (!std::ranges::contains(kAllowedSwitches, provided_switch.first)) {
       LOG(ERROR) << "aggregation_service_tool did not expect "
                  << provided_switch.first << " to be specified.";
       PrintHelp();
