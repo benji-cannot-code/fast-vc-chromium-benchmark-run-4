f383af46215f3f6247080b5c9708a144cebbc1d0
Viktor Semeniuk <vsemeniuk@google.com>
2026-01-13 01:18:00 -0800

    Allow invoking password change feature without auth
    This CL introduces a feature flag to bypass auth requirement when
    invoking password change feature. This feature can be used with counterpart server experiment to perform password change while being
    signed-out.
    
    Bug: 407693092
    Change-Id: I31dfe14e9595fde2d8be274752bd22c7744bad04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7415481
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1568290}

--- components/optimization_guide/core/model_execution/model_execution_fetcher_impl.cc
+++ components/optimization_guide/core/model_execution/model_execution_fetcher_impl.cc
@@ -383,7 +383,6 @@ bool IsAccessTokenRequiredForFeature(ModelBasedCapabilityKey feature) {
     case ModelBasedCapabilityKey::kTest:
     case ModelBasedCapabilityKey::kHistorySearch:
     case ModelBasedCapabilityKey::kBlingPrototyping:
-    case ModelBasedCapabilityKey::kPasswordChangeSubmission:
     case ModelBasedCapabilityKey::kEnhancedCalendar:
     case ModelBasedCapabilityKey::kZeroStateSuggestions:
     case ModelBasedCapabilityKey::kWalletablePassExtraction:
@@ -395,6 +394,9 @@ bool IsAccessTokenRequiredForFeature(ModelBasedCapabilityKey feature) {
           features::kOptimizationGuideBypassFormsClassificationAuth);
     case ModelBasedCapabilityKey::kScamDetection:
       return false;
+    case ModelBasedCapabilityKey::kPasswordChangeSubmission:
+      return !base::FeatureList::IsEnabled(
+          features::kOptimizationGuideBypassPasswordChangeAuth);
   }
 }
 
