942919cec1d74ddd4eb6aeeb202fd76a476147a1
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2026-01-13 10:27:41 -0800

    gardener: Disable TtsMacTest.CachedVoiceData on Mac
    Test has been flaky for a long time:
    https://luci-milo.appspot.com/ui/test/chromium/%3A%2F%2Fcontent%2Ftest%5C%3Acontent_unittests!gtest%3A%3ATtsMacTest%23CachedVoiceData
    
    Bug: 438118294
    Change-Id: I5040810e5ae9860e9470485f9981dd8d89cce604
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7459384
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Owners-Override: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Auto-Submit: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1568530}

--- content/browser/speech/tts_mac_unittest.mm
+++ content/browser/speech/tts_mac_unittest.mm
@@ -12,7 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-TEST(TtsMacTest, CachedVoiceData) {
+// TODO(crbug.com/438118294): Re-enable this test.
+TEST(TtsMacTest, DISABLED_CachedVoiceData) {
   std::vector<VoiceData> voices;
   TtsPlatformImplMac::GetInstance()->GetVoices(&voices);
 
