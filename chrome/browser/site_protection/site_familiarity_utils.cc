1125613775cf7298d1ac2471b15142657e0ca491
Javier Castro <jacastro@chromium.org>
2026-01-12 16:37:39 -0800

    Use the URL from the Site Isolation model for creating exceptions
    By using the SiteURL from the SiteInstance, we can ensure that the
    exception that is added by calling
    site_familiarity_utils::EnableV8Optimizations will match the URL that is
    evaluated during navigation.
    
    Prior to this fix, exceptions that were added would work under
    OriginKeyedProcessesByDefault but not under SitePerProcess. Now the code
    works under both configurations. Note: Content settings exceptions that
    are set using this method, prior to features::kStrictOriginIsolation
    being enabled will likely not work after
    features::kStrictOriginIsolation is enabled -- so some content settings
    exceptions may need to be re-added after
    features::kStrictOriginIsolation is enabled.
    
    Change-Id: I09e1b9524b162dc1f4a1ac31fcf0c48be0e42467
    Fixed: 473885670
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7426181
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Commit-Queue: Javier Castro <jacastro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1568149}

--- chrome/browser/site_protection/site_familiarity_utils.cc
+++ chrome/browser/site_protection/site_familiarity_utils.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_browsing/core/common/safe_browsing_prefs.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
+#include "content/public/browser/site_instance.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/content_features.h"
 
@@ -140,7 +141,7 @@ void EnableV8Optimizations(content::WebContents* web_contents) {
     return;
   }
 
-  const GURL& site_url = web_contents->GetURL();
+  const GURL& site_url = web_contents->GetSiteInstance()->GetSiteURL();
   if (site_url.is_empty()) {
     return;
   }
