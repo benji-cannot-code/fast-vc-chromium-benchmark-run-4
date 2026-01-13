aa7659bcb95f45bf6cba2b6cf4eefe08c44848ea
Colin Blundell <blundell@chromium.org>
2026-01-13 12:46:20 -0800

    [//gpu] Remove comment referencing raster-over-GLES
    This no longer exists.
    
    Change-Id: Ib9cb26b881c3f1f250c920b8bd03ffe9473142c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7458402
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Mingjing Zhang <mjzhang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1568641}

--- gpu/command_buffer/service/shared_image/wrapped_sk_image_backing_factory.cc
+++ gpu/command_buffer/service/shared_image/wrapped_sk_image_backing_factory.cc
@@ -28,9 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gpu {
 namespace {
-// NOTE: WrappedSkImage cannot be used with raster-over-GLES2 as it doesn't
-// support GLES2 usage, and hence it doesn't support RASTER_OVER_GLES2_ONLY
-// usage.
 constexpr SharedImageUsageSet kSupportedUsage =
     SHARED_IMAGE_USAGE_DISPLAY_READ | SHARED_IMAGE_USAGE_DISPLAY_WRITE |
     SHARED_IMAGE_USAGE_RASTER_READ | SHARED_IMAGE_USAGE_RASTER_WRITE |
