8f7eb4d83d951c230f916b56c6853e7fa4ea84d7
Judith Hemp <hempjudith@google.com>
2026-01-13 01:39:57 -0800

    [ClapperQuiet] Adds Clapper Quiet Icon
    Adds the ui logic to show the clapper quiet icon on demand.
    
    Bug: 463333225
    Change-Id: Id0405384f654f63225b71d0914a796c0e8aa73df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7362147
    Commit-Queue: Judith Hemp <hempjudith@google.com>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1568293}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/status/StatusCoordinator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/status/StatusCoordinator.java
@@ -372,6 +372,11 @@ public class StatusCoordinator implements View.OnClickListener, LocationBarDataP
     }
 
     @SuppressWarnings("NullAway")
+    public StatusMediator getMediatorForTesting() {
+        return mMediator;
+    }
+
+    @SuppressWarnings("NullAway")
     public void destroy() {
         mMediator.destroy();
         mLocationBarDataProvider.removeObserver(this);
