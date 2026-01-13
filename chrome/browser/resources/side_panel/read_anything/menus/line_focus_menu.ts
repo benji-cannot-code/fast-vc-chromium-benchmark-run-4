f3a3743c87e5b7d6fdc58e400c29c32a9a5e4af8
Kristi Saney <kristislee@google.com>
2026-01-13 14:28:57 -0800

    [Reading mode] Remove separator above the first line focus header
    Bug: 447427066
    Change-Id: Iee047400f8ccaea6bd495ca837354943c2bd4a9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7463688
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Auto-Submit: Kristi Saney <kristislee@google.com>
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1568718}

--- chrome/browser/resources/side_panel/read_anything/menus/line_focus_menu.ts
+++ chrome/browser/resources/side_panel/read_anything/menus/line_focus_menu.ts
@@ -48,7 +48,10 @@ export class LineFocusMenuElement extends LineFocusMenuElementBase implements
 
   private styleOptions_: Array<MenuStateItem<LineFocusStyle>> = [
     {
-      header: loadTimeData.getString('lineFocusStyleHeading'),
+      header: {
+        title: loadTimeData.getString('lineFocusStyleHeading'),
+        separator: false,
+      },
       title: loadTimeData.getString('lineFocusOffTitle'),
       data: LineFocusStyle.OFF,
       eventName: ToolbarEvent.LINE_FOCUS_STYLE,
@@ -77,7 +80,10 @@ export class LineFocusMenuElement extends LineFocusMenuElementBase implements
 
   private movementOptions_: Array<MenuStateItem<LineFocusMovement>> = [
     {
-      header: loadTimeData.getString('lineFocusMovementHeading'),
+      header: {
+        title: loadTimeData.getString('lineFocusMovementHeading'),
+        separator: true,
+      },
       title: loadTimeData.getString('lineFocusStaticTitle'),
       data: LineFocusMovement.STATIC,
       eventName: ToolbarEvent.LINE_FOCUS_MOVEMENT,
