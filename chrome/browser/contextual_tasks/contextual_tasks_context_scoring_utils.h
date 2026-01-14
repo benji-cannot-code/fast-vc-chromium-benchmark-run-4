c1129c4d87d6fa9b7696dab8ce60cbcf481008b7
Abhay Prakash <abhayprakash@google.com>
2026-01-13 17:33:48 -0800

    Refactor code to add scoring util.
    - Will update the scoring function in a follow up CL.
    
    Bug: 474102677
    Change-Id: I22d872a10a4bda757fe0db3a25af3d47f581e9be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7416396
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Abhay Prakash <abhayprakash@google.com>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1568844}

---
+++ chrome/browser/contextual_tasks/contextual_tasks_context_scoring_utils.h
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CONTEXTUAL_TASKS_CONTEXTUAL_TASKS_CONTEXT_SCORING_UTILS_H_
+#define CHROME_BROWSER_CONTEXTUAL_TASKS_CONTEXTUAL_TASKS_CONTEXT_SCORING_UTILS_H_
+
+#include <optional>
+
+#include "base/memory/raw_ptr.h"
+#include "base/time/time.h"
+
+namespace content {
+class WebContents;
+}  // namespace content
+
+namespace contextual_tasks {
+
+struct TabSignals {
+  raw_ptr<content::WebContents> web_contents = nullptr;
+  // Static signals.
+  std::optional<float> embedding_score;
+  std::optional<int> num_query_title_matching_words;
+  // Dynamic (behavioral) signals.
+  std::optional<base::TimeDelta> duration_since_last_active;
+  std::optional<base::TimeDelta> duration_of_last_visit;
+};
+
+// Gets the score for a tab based on the provided signals.
+double GetTabScore(const TabSignals& signals);
+
+}  // namespace contextual_tasks
+
+#endif  // CHROME_BROWSER_CONTEXTUAL_TASKS_CONTEXTUAL_TASKS_CONTEXT_SCORING_UTILS_H_
--- chrome/browser/contextual_tasks/contextual_tasks_context_signal_utils.cc
+++ chrome/browser/contextual_tasks/contextual_tasks_context_signal_utils.cc
@@ -1,9 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2025 The Chromium Authors
+// Copyright 2026 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/contextual_tasks/contextual_tasks_signal_utils.h"
+#include "chrome/browser/contextual_tasks/contextual_tasks_context_signal_utils.h"
 
 #include <algorithm>
 
