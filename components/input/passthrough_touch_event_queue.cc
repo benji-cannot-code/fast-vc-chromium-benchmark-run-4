7a091512c5a3422d872b8b0bea20386d62716e0a
Oksana Zhuravlova <oksamyt@chromium.org>
2026-01-14 15:10:48 -0800

    Avoid generating empty GSUs for consumed touch moves
    This change addresses the issue of empty GSUs being generated when the
    page prevents scrolls by:
    
    * not filtering out zero delta touch moves
    if SendEmptyGestureScrollUpdate is enabled
    so that the page can consume them;
    * only sending an empty gesture scroll update
    if it passes the GestureHandlingState filter, making this code
    consistent with the logic for all regular events.
    
    Bug: 402144902, 467650983
    Change-Id: Ib87e173436e5751b22344bba194e77d394c303b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7254180
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1569367}

--- components/input/passthrough_touch_event_queue.cc
+++ components/input/passthrough_touch_event_queue.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/input/touch_timeout_handler.h"
 #include "components/input/web_touch_event_traits.h"
 #include "third_party/blink/public/common/features.h"
+#include "ui/base/ui_base_features.h"
 #include "ui/events/base_event_utils.h"
 #include "ui/gfx/geometry/point_f.h"
 
@@ -490,7 +491,13 @@ PassthroughTouchEventQueue::FilterBeforeForwardingImpl(
     }
   }
 
-  return PreFilterResult::kFilteredNoNonstationaryPointers;
+  // If SendEmptyGestureScrollUpdate is enabled, GestureScrollUpdates will be
+  // generated from zero delta touch moves. We need to send these events to the
+  // renderer so that we don't generate unnecessary scrolls if the touch moves
+  // were consumed (e.g. preventDefault() was called).
+  return base::FeatureList::IsEnabled(features::kSendEmptyGestureScrollUpdate)
+             ? PreFilterResult::kUnfiltered
+             : PreFilterResult::kFilteredNoNonstationaryPointers;
 }
 
 void PassthroughTouchEventQueue::UpdateTouchConsumerStates(
