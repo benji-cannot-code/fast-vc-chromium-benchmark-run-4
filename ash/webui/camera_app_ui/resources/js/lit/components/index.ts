0c8fe52e4f0c58a909a57aec71f23ec80f51ace0
Orko Garai <orko@igalia.com>
2026-01-13 23:13:49 -0800

    CCA: Cloud save warning dialog
    Implement the initial in-app warning dialog shown to the user if the
    camera saves files on the cloud.
    
    Also, change cloud_destination load time variable from boolean to string
    to let the app know whether the destination is OneDrive or Google Drive.
    This is needed to show the correct string in the warning dialog title.
    
    Design Doc:
    https://docs.google.com/document/d/1wod7fE1aU4D0boQxx9dxgNsAvJXvBPfvc8XFAmjtFuQ/edit?usp=sharing
    
    Bug: 454152412
    Change-Id: Ic770601d41f0578b7cdb84493855d04ed1c5b643
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7281208
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    Commit-Queue: Orko Garai <orko@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1568944}

--- ash/webui/camera_app_ui/resources/js/lit/components/index.ts
+++ ash/webui/camera_app_ui/resources/js/lit/components/index.ts
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 // This file is imported by main.ts to have all Lit components registered.
+import './cloud-save-warning-dialog.js';
 import './gallery-button.js';
 import './mode-selector.js';
 import './record-time-chip.js';
