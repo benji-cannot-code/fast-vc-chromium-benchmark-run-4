582466663b0e70792ad66f1ae634d206203076c9
Arthur Sonzogni <arthursonzogni@chromium.org>
2026-01-14 02:09:10 -0800

    Cleanup now redundant UNSAFE_TODO() device
    As part of the spanification effort, the codebase contains now many
    UNSAFE_TODO() macros used to suppress -Wunsafe-buffer-usage errors. As
    code evolves and is modernized, these macros sometimes become redundant
    but are difficult to identify exhaustively.
    
    This is an automated patch generated using:
    tools/clang/unsafe_todo_remover
    from:
    https://chromium-review.googlesource.com/c/chromium/src/+/7462506
    
    This CL was uploaded by an experimental version of git cl split
    (https://crbug.com/389069356).
    
    R=mattreynolds@chromium.org
    
    Doc: https://docs.google.com/document/d/1h8HYBuIef3AHwXiDLAQphE74clvVmVG2hj1Ty7BvNzs
    Cleanup: This is a #cleanup
    Bug: 475236716
    Change-Id: If4f2a910691a1aa9cf22baf9cd772b772fdc749b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7461746
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1568990}

--- device/gamepad/nintendo_controller.cc
+++ device/gamepad/nintendo_controller.cc
@@ -621,8 +621,7 @@ void UpdateButtonForLeftSide(const Gamepad& src_pad,
         break;
     }
   }
-  UNSAFE_TODO(dst_pad.buttons[remapped_index]) =
-      UNSAFE_TODO(src_pad.buttons[button_index]);
+  dst_pad.buttons[remapped_index] = src_pad.buttons[button_index];
 }
 
 // Update the state for a single button. The button state is taken from
@@ -677,8 +676,7 @@ void UpdateButtonForRightSide(const Gamepad& src_pad,
         NOTREACHED();
     }
   }
-  UNSAFE_TODO(dst_pad.buttons[remapped_index]) =
-      UNSAFE_TODO(src_pad.buttons[button_index]);
+  dst_pad.buttons[remapped_index] = src_pad.buttons[button_index];
 }
 
 // Update the state for a single axis. The axis state is taken from the axis at
@@ -691,7 +689,7 @@ void UpdateAxisForLeftSide(const Gamepad& src_pad,
                            size_t axis_index,
                            bool horizontal) {
   size_t remapped_index = axis_index;
-  double axis_value = UNSAFE_TODO(src_pad.axes[axis_index]);
+  double axis_value = src_pad.axes[axis_index];
   // The internal axis values assume a docked orientation for Joy-Cons. If a
   // Joy-Con is used by itself, remap the axis indices and adjust the sign on
   // the axis value for a horizontal orientation.
@@ -710,7 +708,7 @@ void UpdateAxisForLeftSide(const Gamepad& src_pad,
         NOTREACHED();
     }
   }
-  UNSAFE_TODO(dst_pad.axes[remapped_index]) = axis_value;
+  dst_pad.axes[remapped_index] = axis_value;
 }
 
 // Update the state for a single axis. The axis state is taken from the axis at
@@ -723,7 +721,7 @@ void UpdateAxisForRightSide(const Gamepad& src_pad,
                             size_t axis_index,
                             bool horizontal) {
   size_t remapped_index = axis_index;
-  double axis_value = UNSAFE_TODO(src_pad.axes[axis_index]);
+  double axis_value = src_pad.axes[axis_index];
   // The internal axis values assume a docked orientation for Joy-Cons. If a
   // Joy-Con is used by itself, remap the axis indices and adjust the sign on
   // the axis value for a horizontal orientation.
@@ -742,7 +740,7 @@ void UpdateAxisForRightSide(const Gamepad& src_pad,
         NOTREACHED();
     }
   }
-  UNSAFE_TODO(dst_pad.axes[remapped_index]) = axis_value;
+  dst_pad.axes[remapped_index] = axis_value;
 }
 
 // Convert the vibration parameters |frequency| and |amplitude| into a set of
