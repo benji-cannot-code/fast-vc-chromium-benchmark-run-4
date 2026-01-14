2454af9c3397a81efb2738413928f393412d93fe
Dale Curtis <dalecurtis@chromium.org>
2026-01-13 17:15:51 -0800

    [ImageCapture] Restore accelerated provider creation
    The original code didn't always create unaccelerated bitmaps, only
    sometimes. The new code will now always create accelerated frames
    (unless the gpu workaround to not do so is enabled).
    
    Fixed: 471617179, 468035607
    Change-Id: Ieb95529f5b33ffadf131369a1f16414e5da7be24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7457913
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1568835}

--- third_party/blink/renderer/modules/imagecapture/image_capture_frame_grabber.cc
+++ third_party/blink/renderer/modules/imagecapture/image_capture_frame_grabber.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/thread_annotations.h"
 #include "base/time/time.h"
 #include "cc/paint/skia_paint_canvas.h"
+#include "components/viz/common/gpu/raster_context_provider.h"
 #include "media/base/video_frame.h"
 #include "media/base/video_types.h"
 #include "media/base/video_util.h"
@@ -177,11 +178,7 @@ void ImageCaptureFrameGrabber::OnVideoFrame(
   if (!snapshot_provider_ ||
       !required_provider_info.Matches(*snapshot_provider_)) {
     snapshot_provider_ = CreateSnapshotProviderForVideo(
-        required_provider_info,
-        // TODO(crbug.com/468035607): The RasterContextProvider is nullptr since
-        // this API has historically provided software backed images, but maybe
-        // shouldn't be.
-        /*raster_context_provider=*/nullptr);
+        required_provider_info, GetRasterContextProvider().get());
   }
 
   scoped_refptr<StaticBitmapImage> image = CreateImageFromVideoFrame(
