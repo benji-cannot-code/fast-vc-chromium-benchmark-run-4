5036af556c6c35e63f5a039ae275840a36f1b895
Dana Fried <dfried@chromium.org>
2026-01-13 16:19:47 -0800

    [Vertical Tabs] Make very top of toolbar part of the window grab handle
    In VTS mode, the area in the toolbar above the location bar is now part
    of the drag handle.
    
    This feels "natural" as otherwise it is dead area.
    
    Bug: 474082241
    Change-Id: If7dde44dab8039bcd47680e0e39ed69cb9389b86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7464170
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1568805}

--- chrome/browser/ui/views/frame/top_container_view.cc
+++ chrome/browser/ui/views/frame/top_container_view.cc
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/browser_widget.h"
 #include "chrome/browser/ui/views/frame/custom_corners_background.h"
 #include "chrome/browser/ui/views/frame/immersive_mode_controller.h"
+#include "chrome/browser/ui/views/frame/top_container_background.h"
+#include "chrome/browser/ui/views/toolbar/toolbar_view.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/color/color_id.h"
 #include "ui/color/color_variant.h"
@@ -49,8 +51,16 @@ void TopContainerView::OnImmersiveRevealUpdated() {
 
 bool TopContainerView::IsPositionInWindowCaption(
     const gfx::Point& test_point) const {
+  const ToolbarView* const toolbar = browser_view_->toolbar();
   for (auto& child : children()) {
     if (child->bounds().Contains(test_point)) {
+      if (child == toolbar) {
+        const auto in_toolbar =
+            views::View::ConvertPointToTarget(this, toolbar, test_point);
+        if (in_toolbar.y() < toolbar->location_bar()->y()) {
+          return true;
+        }
+      }
       return false;
     }
   }
