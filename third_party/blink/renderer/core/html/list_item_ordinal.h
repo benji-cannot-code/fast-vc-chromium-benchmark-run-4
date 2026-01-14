8854d867554c3dc47754f558e9c6bd41d00acd98
Minseong Kim <jja08111@gmail.com>
2026-01-14 14:05:11 -0800

    [css-lists] Implement presentational hint for <li>'s value attribute
    This CL treats the value attribute on <li> elements as a presentational
    hint, translating it into a counter-set style for the list-item counter.
    
    This also replaces the legacy ListItemOrdinal logic. As a result, the
    usage of ListItemOrdinal's ExplicitValue-related functions is removed
    from the counter calculation path when the feature is enabled.
    
    Bug: 40760770, 40682542
    Change-Id: I0b6a9de3193dd40d0dafbca1f33075f417a59d23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7459719
    Reviewed-by: Daniil Sakhapov <sakhapov@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Minseong Kim <jja08111@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1569323}

--- third_party/blink/renderer/core/html/list_item_ordinal.h
+++ third_party/blink/renderer/core/html/list_item_ordinal.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/platform/heap/persistent.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
 namespace blink {
 
@@ -39,7 +40,10 @@ class CORE_EXPORT ListItemOrdinal {
   // element.
   std::optional<int> ExplicitValue() const;
   void SetExplicitValue(int, const Element&);
-  bool UseExplicitValue() const { return type_ == kExplicit; }
+  bool UseExplicitValue() const {
+    DCHECK(!RuntimeEnabledFeatures::CSSListCounterAccountingEnabled());
+    return type_ == kExplicit;
+  }
   void ClearExplicitValue(const Node&);
   void MarkDirty() { SetType(kNeedsUpdate); }
 
@@ -105,6 +109,9 @@ class CORE_EXPORT ListItemOrdinal {
   mutable int value_ = 0;
   // `explicit_value_` represents the value of li elements. When the `type` is
   // set to `kExplicit`, the value of `value_` is the same as `explicit_value_`.
+  //
+  // TODO(crbug.com/40760770): Remove `explicit_value_` when
+  // CSSListCounterAccounting is enabled by default and removed.
   mutable std::optional<int> explicit_value_;
   mutable unsigned type_ : 2;  // ValueType
 };
