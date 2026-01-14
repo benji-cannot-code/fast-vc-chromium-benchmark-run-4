311ca62b4694e8f47bc1ab987948a0c8a8f7944d
Henrique Nakashima <hnakashima@chromium.org>
2026-01-14 13:36:26 -0800

    Reland "[Android] Create waitForNoView(), replace waitForViewCheckingState()"
    This reverts commit a9f17efd553f18d4d9bb9df5184c7ed1e1b2f8a6.
    
    Reason for revert: Fixed version - decreased displayed % threshold
    
    Original change's description:
    > Revert "[Android] Create waitForNoView(), replace waitForViewCheckingState()"
    >
    > This reverts commit 2c35b691d2756aaa58b1d3dbb3a040d45b0500fa.
    >
    > Reason for revert: b/475619743
    >
    > Original change's description:
    > > [Android] Create waitForNoView(), replace waitForViewCheckingState()
    > >
    > > Bug: 471255537
    > > Change-Id: I00a4d97bc5b8f887b5a17bc2440c4c7d11ac5e52
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7356538
    > > Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    > > Owners-Override: Henrique Nakashima <hnakashima@chromium.org>
    > > Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1568626}
    >
    > Bug: 471255537
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: Ie2e618d6ccbf4863ff1507b0a1bcf710ed20f4db
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7467600
    > Auto-Submit: Mark Schillaci <mschillaci@google.com>
    > Owners-Override: Mark Schillaci <mschillaci@google.com>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1568760}
    
    Bug: 471255537
    Change-Id: If8c0b8abad0d36a33a247ad2696b36b3ff9d74ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7474110
    Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    Owners-Override: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1569290}

--- chrome/android/features/keyboard_accessory/javatests/src/org/chromium/chrome/browser/keyboard_accessory/ManualFillingTestHelper.java
+++ chrome/android/features/keyboard_accessory/javatests/src/org/chromium/chrome/browser/keyboard_accessory/ManualFillingTestHelper.java
@@ -12,6 +12,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static org.hamcrest.core.AllOf.allOf;
 
 import static org.chromium.autofill.mojom.FocusedFieldType.FILLABLE_NON_SEARCH_FIELD;
+import static org.chromium.base.test.transit.ViewFinder.waitForNoView;
 import static org.chromium.base.test.util.CriteriaHelper.pollInstrumentationThread;
 import static org.chromium.base.test.util.CriteriaHelper.pollUiThread;
 import static org.chromium.chrome.browser.keyboard_accessory.bar_component.KeyboardAccessoryTestHelper.accessoryStartedHiding;
@@ -19,9 +20,6 @@ import static org.chromium.chrome.browser.keyboard_accessory.bar_component.Keybo
 import static org.chromium.chrome.browser.keyboard_accessory.bar_component.KeyboardAccessoryTestHelper.accessoryViewFullyHidden;
 import static org.chromium.chrome.browser.keyboard_accessory.bar_component.KeyboardAccessoryTestHelper.checkThatAccessoryViewFullyShown;
 import static org.chromium.ui.base.LocalizationUtils.setRtlForTesting;
-import static org.chromium.ui.test.util.ViewUtils.VIEW_GONE;
-import static org.chromium.ui.test.util.ViewUtils.VIEW_INVISIBLE;
-import static org.chromium.ui.test.util.ViewUtils.VIEW_NULL;
 import static org.chromium.ui.test.util.ViewUtils.onViewWaiting;
 import static org.chromium.ui.test.util.ViewUtils.waitForView;
 
@@ -75,7 +73,6 @@ import org.chromium.content_public.browser.test.util.TestInputMethodManagerWrapp
 import org.chromium.net.test.EmbeddedTestServer;
 import org.chromium.net.test.ServerCertificate;
 import org.chromium.ui.DropdownPopupWindowInterface;
-import org.chromium.ui.test.util.ViewUtils;
 
 import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicReference;
@@ -616,7 +613,7 @@ public class ManualFillingTestHelper {
     }
 
     public static void waitToBeHidden(Matcher<View> matcher) {
-        ViewUtils.waitForViewCheckingState(matcher, VIEW_INVISIBLE | VIEW_NULL | VIEW_GONE);
+        waitForNoView(matcher);
     }
 
     public String getAttribute(String node, String attribute)
--- chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/adaptive/OptionalNewTabButtonControllerTabletTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/adaptive/OptionalNewTabButtonControllerTabletTest.java
@@ -7,6 +7,8 @@ package org.chromium.chrome.browser.toolbar.adaptive;
 
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
+import static org.chromium.base.test.transit.ViewFinder.waitForNoView;
+
 import android.content.res.Configuration;
 
 import androidx.test.filters.MediumTest;
@@ -30,7 +32,6 @@ import org.chromium.chrome.test.transit.ChromeTransitTestRules;
 import org.chromium.chrome.test.transit.page.WebPageStation;
 import org.chromium.chrome.test.util.ActivityTestUtils;
 import org.chromium.ui.base.DeviceFormFactor;
-import org.chromium.ui.test.util.ViewUtils;
 
 /**
  * Tests {@link OptionalNewTabButtonController} on tablet. Phone functionality is tested by {@link
@@ -79,8 +80,7 @@ public class OptionalNewTabButtonControllerTabletTest {
                 mActivityTestRule.getActivity(), Configuration.ORIENTATION_LANDSCAPE);
         mActivityTestRule.loadUrl(mTestPageUrl, /* secondsToWait= */ 10);
 
-        ViewUtils.waitForViewCheckingState(
-                withId(R.id.optional_toolbar_button), ViewUtils.VIEW_GONE | ViewUtils.VIEW_NULL);
+        waitForNoView(withId(R.id.optional_toolbar_button));
     }
 
     @Test
@@ -90,7 +90,6 @@ public class OptionalNewTabButtonControllerTabletTest {
                 mActivityTestRule.getActivity(), Configuration.ORIENTATION_PORTRAIT);
         mActivityTestRule.loadUrl(mTestPageUrl, /* secondsToWait= */ 10);
 
-        ViewUtils.waitForViewCheckingState(
-                withId(R.id.optional_toolbar_button), ViewUtils.VIEW_GONE | ViewUtils.VIEW_NULL);
+        waitForNoView(withId(R.id.optional_toolbar_button));
     }
 }
