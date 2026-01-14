0a1266264944bb446937afa22fef94a8f43b5695
Jan Keitel <jkeitel@google.com>
2026-01-14 10:26:24 -0800

    Use span-based file API in seccomp_broker_process_unittest.cc
    No functionality changes.
    
    Bug: 435317390
    Change-Id: Ie8e6acac44845712231cc0e4b6e97c961dd4ec54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7473280
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1569185}

--- sandbox/linux/integration_tests/seccomp_broker_process_unittest.cc
+++ sandbox/linux/integration_tests/seccomp_broker_process_unittest.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notreached.h"
 #include "base/posix/eintr_wrapper.h"
 #include "base/run_loop.h"
+#include "base/strings/cstring_view.h"
 #include "base/test/bind.h"
 #include "base/test/task_environment.h"
 #include "build/build_config.h"
@@ -2862,9 +2863,9 @@ class BaseFilePathWatcherDelegate final : public InotifyAddWatchDelegate {
     base::File temp_file_again(temp_file_path_, base::File::FLAG_OPEN |
                                                     base::File::FLAG_READ |
                                                     base::File::FLAG_WRITE);
-    char buf2[] = "a";
-    UNSAFE_TODO(BPF_ASSERT_EQ(temp_file_again.Write(0, buf2, sizeof(buf2)),
-                              sizeof(buf2)));
+    BPF_ASSERT_EQ(
+        temp_file_again.Write(0, base::byte_span_with_nul_from_cstring("a")),
+        2);
     temp_file_again.Flush();
     temp_file_again.Close();
     // Wait until we receive a notification about the file modification.
