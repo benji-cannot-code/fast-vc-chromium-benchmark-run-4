6979946b5ce1f4d181e3d2f3499b40482a12bc7e
Robert Ferens <rferens@google.com>
2026-01-14 12:02:25 -0800

    iwa: Additional unit tests for KeyDistributionInfoProvider
    This CL introduces additional tests for KeyDistributionInfoProvider. New
    tests verify that data accessing APIs and AsDebugValue() works correct
    for this class. It eliminates unit test code coverage debt.
    
    Bug: 475504356
    Change-Id: I30c23f94648428e40b903a3ca117a145b3998c8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7460996
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Commit-Queue: Robert Ferens <rferens@google.com>
    Auto-Submit: Robert Ferens <rferens@google.com>
    Cr-Commit-Position: refs/heads/main@{#1569211}

--- chrome/browser/web_applications/isolated_web_apps/test/key_distribution/test_utils.h
+++ chrome/browser/web_applications/isolated_web_apps/test/key_distribution/test_utils.h
@@ -50,7 +50,8 @@ class KeyDistributionComponentBuilder {
   // Component update requires the higher component version than the current
   // one.
   explicit KeyDistributionComponentBuilder(
-      const base::Version& component_version);
+      const base::Version& component_version,
+      bool is_preloaded = false);
   ~KeyDistributionComponentBuilder();
 
   KeyDistributionComponentBuilder(const KeyDistributionComponentBuilder&) =
