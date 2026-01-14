4e0c33baea917ae28ac50df682038f1b3c2f954c
Kuo-Hsin Yang <vovoy@google.com>
2026-01-13 17:09:13 -0800

    Remove feature OneGroupPerRenderer
    Feature OneGroupPerRenderer was only enabled on Lacros. As Lacros is
    sunsetted, OneGroupPerRenderer should also be removed.
    
    The CL adding OneGroupPerRenderer is http://crrev.com/c/3569842
    
    Bug: 172213843, 326349117
    Change-Id: I1b8db6743af35f01bc0c109fea784c5461749620
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7452339
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Vovo Yang <vovoy@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1568832}

--- base/process/process_posix.cc
+++ base/process/process_posix.cc
@@ -201,9 +201,6 @@ namespace base {
 Process::Process(ProcessHandle handle) : process_(handle) {}
 
 Process::Process(Process&& other) : process_(other.process_) {
-#if BUILDFLAG(IS_CHROMEOS)
-  unique_token_ = std::move(other.unique_token_);
-#endif
 #if BUILDFLAG(IS_IOS) && BUILDFLAG(USE_BLINK) && TARGET_OS_SIMULATOR
   content_process_ = other.content_process_;
 #endif
@@ -213,9 +210,6 @@ Process::Process(Process&& other) : process_(other.process_) {
 
 Process& Process::operator=(Process&& other) {
   process_ = other.process_;
-#if BUILDFLAG(IS_CHROMEOS)
-  unique_token_ = std::move(other.unique_token_);
-#endif
 #if BUILDFLAG(IS_IOS) && BUILDFLAG(USE_BLINK) && TARGET_OS_SIMULATOR
   content_process_ = other.content_process_;
 #endif
@@ -268,9 +262,7 @@ Process Process::Duplicate() const {
   }
 
   Process duplicate = Process(process_);
-#if BUILDFLAG(IS_CHROMEOS)
-  duplicate.unique_token_ = unique_token_;
-#elif BUILDFLAG(IS_IOS) && BUILDFLAG(USE_BLINK) && TARGET_OS_SIMULATOR
+#if BUILDFLAG(IS_IOS) && BUILDFLAG(USE_BLINK) && TARGET_OS_SIMULATOR
   duplicate.content_process_ = content_process_;
 #endif
   return duplicate;
@@ -313,11 +305,6 @@ bool Process::TerminateInternal(int exit_code, bool wait) const {
   if (maybe_compromised) {
     // Forcibly terminate the process immediately.
     const bool was_killed = kill(process_, SIGKILL) == 0;
-#if BUILDFLAG(IS_CHROMEOS)
-    if (was_killed) {
-      CleanUpProcessAsync();
-    }
-#endif
     DPLOG_IF(ERROR, !was_killed) << "Unable to terminate process " << process_;
     return was_killed;
   }
@@ -327,10 +314,6 @@ bool Process::TerminateInternal(int exit_code, bool wait) const {
     DPLOG(ERROR) << "Unable to terminate process " << process_;
     return false;
   }
-
-#if BUILDFLAG(IS_CHROMEOS)
-  CleanUpProcessAsync();
-#endif
 
   if (!wait || WaitForExitWithTimeout(Seconds(60), nullptr)) {
     return true;
@@ -417,11 +400,7 @@ bool Process::WaitForExitWithTimeoutImpl(base::ProcessHandle handle,
 }
 #endif
 
-void Process::Exited(int exit_code) const {
-#if BUILDFLAG(IS_CHROMEOS)
-  CleanUpProcessAsync();
-#endif
-}
+void Process::Exited(int exit_code) const {}
 
 int Process::GetOSPriority() const {
   DCHECK(IsValid());
--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -293,7 +293,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_switches.h"
 #include "ash/public/cpp/app_list/app_list_features.h"
 #include "ash/public/cpp/keyboard/keyboard_switches.h"
-#include "base/process/process.h"
 #include "base/system/sys_info.h"
 #include "chrome/browser/apps/app_service/chrome_app_deprecation/chrome_app_deprecation.h"
 #include "chrome/browser/ash/app_list/search/search_features.h"
@@ -5647,9 +5646,6 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kDisconnectWiFiOnEthernetConnectedName,
      flag_descriptions::kDisconnectWiFiOnEthernetConnectedDescription, kOsCrOS,
      FEATURE_VALUE_TYPE(ash::features::kDisconnectWiFiOnEthernetConnected)},
-    {"one-group-per-renderer", flag_descriptions::kOneGroupPerRendererName,
-     flag_descriptions::kOneGroupPerRendererDescription, kOsCrOS,
-     FEATURE_VALUE_TYPE(base::kOneGroupPerRenderer)},
     {"use-dhcpcd10", flag_descriptions::kUseDHCPCD10Name,
      flag_descriptions::kUseDHCPCD10Description, kOsCrOS,
      FEATURE_VALUE_TYPE(ash::features::kUseDHCPCD10)},
