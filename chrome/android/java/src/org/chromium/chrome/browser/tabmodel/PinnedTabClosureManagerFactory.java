fe40f7f3b1cd10ecfca0ff6bbed44eec4e306cd4
Zhe Li <zheliooo@google.com>
2026-01-14 15:37:53 -0800

    [PinnedTabs] Ctrl+W to close pinned tab requires two tries
    -Show toast "Press ^W again to close the pinned tab" on first try
    
    Demo: https://drive.google.com/file/d/1yunQH89X8nFPsn0F1WJKUphfEXQRk1th/view?usp=sharing
    Bug: 458095447
    Change-Id: Iabd5f8eb23c48d410f2f4bb43d34998d933c2a7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7454853
    Commit-Queue: Zhe Li <zheliooo@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1569388}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/PinnedTabClosureManagerFactory.java
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.tabmodel;
+
+import org.chromium.base.ResettersForTesting;
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
+
+/** Factory for creating {@link PinnedTabClosureManager}. */
+@NullMarked
+public class PinnedTabClosureManagerFactory {
+    private static @Nullable PinnedTabClosureManager sInstanceForTesting;
+
+    private PinnedTabClosureManagerFactory() {}
+
+    /** Obtains the singleton instance of {@link PinnedTabClosureManager}. */
+    public static PinnedTabClosureManager getInstance() {
+        if (sInstanceForTesting != null) {
+            return sInstanceForTesting;
+        }
+        return PinnedTabClosureManager.getInstance();
+    }
+
+    public static void setInstanceForTesting(PinnedTabClosureManager instanceForTesting) {
+        sInstanceForTesting = instanceForTesting;
+        ResettersForTesting.register(() -> sInstanceForTesting = null); // IN-TEST
+    }
+}
