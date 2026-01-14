3e00ec0784fc04e078355931cca3e0d5c84255f6
Hitarth Kothari <hitarthkothari@google.com>
2026-01-14 11:46:47 -0800

    [TabBottomSheet] Make toolbar swapable
    For now the toolbar doesnt have any buttons so simply inflating is fine.
    In the future they will need their own controllers and binders.
    
    Bug: 475223263
    Change-Id: I483fb94e86b298808c8adb6c79c3064df8be1eae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7455421
    Commit-Queue: Hitarth Kothari <hitarthkothari@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Hitarth Kothari <hitarthkothari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1569205}

---
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/tab_bottom_sheet/TabBottomSheetToolbar.java
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.tasks.tab_management.tab_bottom_sheet;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import org.chromium.build.annotations.NullMarked;
+
+/** Abstract class for Tab Bottom Sheet toolbars. */
+@NullMarked
+public abstract class TabBottomSheetToolbar extends FrameLayout {
+    TabBottomSheetToolbar(Context context) {
+        super(context);
+    }
+
+    View getToolbarView() {
+        return this;
+    }
+}
---
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/tab_bottom_sheet/TabBottomSheetUtils.java
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.tasks.tab_management.tab_bottom_sheet;
+
+import androidx.annotation.IntDef;
+
+import org.chromium.build.annotations.NullMarked;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/** Utility methods used by the Tab Bottom Sheet components. */
+@NullMarked
+public final class TabBottomSheetUtils {
+    private TabBottomSheetUtils() {}
+
+    /** Modes for the Tab Bottom Sheet. */
+    @IntDef({TabBottomSheetModes.SIMPLE, TabBottomSheetModes.COMPLEX})
+    @Retention(RetentionPolicy.SOURCE)
+    @interface TabBottomSheetModes {
+        int SIMPLE = 0;
+        int COMPLEX = 1;
+    }
+}
