e1eaa8e5524eeaa8333fd8e6f062694f1494f6c0
prudzinski@google.com <prudzinski@google.com>
2026-01-14 03:22:14 -0800

    Reland "Refactor ShouldHonorPolicies to use standard sensitive policy mechanism"
    This reverts commit d78ee7207df6a41df5dc17d71452a7a39f6b4165.
    
    Reland "Refactor ShouldHonorPolicies to use standard sensitive policy mechanism"
    
    Original change's description:
    > Revert "Refactor ShouldHonorPolicies to use standard sensitive policy mechanism"
    >
    > This reverts commit adfc6d73de03093fa95f70dee9ec09524eae1896.
    >
    > Reason for revert: This CL changed the behavior of URLBlocklist and
    > URLAllowlist but it shouldn't.
    >
    > Original change's description:
    > > Refactor ShouldHonorPolicies to use standard sensitive policy mechanism
    > >
    > > Refactors the URLAllowlist/URLBlocklist policy protection to use
    > > the standard kSensitivePolicies[] mechanism instead of a one-off
    > > ShouldHonorPolicies() check.
    > >
    > > This ensures that when URLAllowlist/URLBlocklist policies are blocked
    > > due to an untrusted source, they are visible on chrome://policy with
    > > an appropriate error message, rather than being silently ignored.
    > >
    > > Bug: 40765067, 1322121
    > > Change-Id: I1263d8124915e486166283b0897da878d0156619
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7232257
    > > Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    > > Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    > > Commit-Queue: Helmut Januschka <helmut@januschka.com>
    > > Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1565105}
    >
    > Bug: 40765067, 1322121
    > Change-Id: I20617fb56723b29e6cc2344f2fa8ec176c237aca
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7418543
    > Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    > Reviewed-by: Owen Min <zmin@chromium.org>
    > Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    > Auto-Submit: Owen Min <zmin@chromium.org>
    > Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    > Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1567009}
    
    Bug: 40765067, 1322121
    Change-Id: Iff7b9e1626863b584e9057985b32858e66b7eb0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7451359
    Commit-Queue: Przemek Rudziński <prudzinski@google.com>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1569021}

--- chrome/browser/policy/chrome_browser_policy_connector.cc
+++ chrome/browser/policy/chrome_browser_policy_connector.cc
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/core/common/policy_loader_win.h"
 #elif BUILDFLAG(IS_MAC)
 #include <CoreFoundation/CoreFoundation.h>
+
 #include "base/apple/foundation_util.h"
 #include "base/strings/sys_string_conversions.h"
 #include "components/policy/core/common/policy_loader_mac.h"
@@ -338,6 +339,7 @@ ChromeBrowserPolicyConnector::CreatePlatformProvider() {
   auto loader = std::make_unique<PolicyLoaderMac>(
       base::ThreadPool::CreateSequencedTaskRunner(
           {base::MayBlock(), base::TaskPriority::BEST_EFFORT}),
+      ManagementServiceFactory::GetForPlatform(),
       PolicyLoaderMac::GetManagedPolicyPath(bundle_id),
       std::make_unique<MacPreferences>(), bundle_id);
   return std::make_unique<AsyncPolicyProvider>(GetSchemaRegistry(),
--- components/policy/core/common/policy_loader_mac.h
+++ components/policy/core/common/policy_loader_mac.h
@@ -26,6 +26,7 @@ class SequencedTaskRunner;
 
 namespace policy {
 
+class ManagementService;
 class PolicyBundle;
 class PolicyMap;
 class Schema;
@@ -35,12 +36,14 @@ class Schema;
 class POLICY_EXPORT PolicyLoaderMac : public AsyncPolicyLoader {
  public:
   PolicyLoaderMac(scoped_refptr<base::SequencedTaskRunner> task_runner,
+                  ManagementService* management_service,
                   const base::FilePath& managed_policy_path,
                   std::unique_ptr<MacPreferences> preferences);
 
   // |application_id| will be passed into Mac's Preference Utilities API
   // instead of the default value of kCFPreferencesCurrentApplication.
   PolicyLoaderMac(scoped_refptr<base::SequencedTaskRunner> task_runner,
+                  ManagementService* management_service,
                   const base::FilePath& managed_policy_path,
                   std::unique_ptr<MacPreferences> preferences,
                   CFStringRef application_id);
