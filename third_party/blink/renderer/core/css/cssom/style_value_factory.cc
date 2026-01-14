6346ea08f8566c0247eb71c8df4a4936d23a5191
Koji Ishii <kojii@chromium.org>
2026-01-14 00:04:12 -0800

    [text-indent] Use `CSSPrimitiveValue` when possible
    This patch changes the CSS parser to use the `CSSPrimitiveValue`
    instead of the `CSSValueList` with one entry when possible,
    to reduce memory and some CPU cycles.
    
    This patch should have no behavior changes.
    
    Bug: 434361099
    Change-Id: I1395d397a324d365f5a3e469639ef0d6fc7dd945
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7414438
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1568953}

--- third_party/blink/renderer/core/css/cssom/style_value_factory.cc
+++ third_party/blink/renderer/core/css/cssom/style_value_factory.cc
@@ -220,7 +220,7 @@ CSSStyleValue* CreateStyleValueWithPropertyInternal(CSSPropertyID property_id,
       return nullptr;
     }
     case CSSPropertyID::kTextIndent: {
-      if (value.IsIdentifierValue()) {
+      if (value.IsIdentifierValue() || value.IsPrimitiveValue()) {
         return CreateStyleValue(value);
       }
 
