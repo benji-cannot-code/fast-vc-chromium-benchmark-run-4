626bcb83d5f1cdb5bcc01ebc10df4f0d5aecd293
Ian Kilpatrick <ikilpatrick@chromium.org>
2026-01-14 08:20:20 -0800

    [cleanup] Remove {LayoutBlock,LayoutBlockFlow}::HitTestChildren
    LayoutBlock overrides NodeAtPoint - without callings its super-class
    method LayoutBox::NodeAtPoint.
    
    HitTestChildren is only used within LayoutBox::NodeAtPoint, and
    therefore the LayoutBlock::HitTestChildren, and
    LayoutBlockFlow::HitTestChildren logic was unreachable.
    
    Remove these override, and additionally shift LayoutBox::NodeAtPoint
    down to LayoutReplaced::NodeAtPoint as this is where it is actually
    used.
    
    There should be no behaviour change.
    
    Change-Id: I01d075bea96f651489b9c3809a10973a75a1825b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7426176
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1569140}

--- third_party/blink/renderer/core/layout/layout_replaced.h
+++ third_party/blink/renderer/core/layout/layout_replaced.h
@@ -90,6 +90,11 @@ class CORE_EXPORT LayoutReplaced : public LayoutBox {
            ComputedStyleInitialValues::InitialObjectFit();
   }
 
+  bool NodeAtPoint(HitTestResult&,
+                   const HitTestLocation&,
+                   const PhysicalOffset& accumulated_offset,
+                   HitTestPhase) override;
+
   void Paint(const PaintInfo&) const override;
 
   // Compute the natural dimensions of the replaced content. Should not apply
@@ -124,6 +129,11 @@ class CORE_EXPORT LayoutReplaced : public LayoutBox {
     NOT_DESTROYED();
     return true;
   }
+
+  virtual bool HitTestChildren(HitTestResult&,
+                               const HitTestLocation&,
+                               const PhysicalOffset& accumulated_offset,
+                               HitTestPhase) const;
 
   bool IsInSelfHitTestingPhase(HitTestPhase phase) const override {
     NOT_DESTROYED();
