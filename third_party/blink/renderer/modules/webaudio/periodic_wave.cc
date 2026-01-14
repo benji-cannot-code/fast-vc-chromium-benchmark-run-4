18adb97100b9aa3c1031541becc0544e566ec60f
Tom Van Goethem <tov@chromium.org>
2026-01-13 16:13:29 -0800

    [Code health] Refactor OscillatorHandler and PeriodicWave to use base::span.
    This change replaces raw float pointers with base::span and std::array
    in OscillatorHandler and PeriodicWave methods. This improves type safety
    by providing bounds checking in debug builds and removes the need for
    UNSAFE_TODO annotations. The refactoring includes updating function
    signatures and call sites to use base::span and std::array for wave data
    and phase increments.
    
    Bug: 435317390
    Bug: 436197812
    Fixed: 436197812
    Change-Id: I83cc0cfdb950ce266f28b8a454205b1515a648a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7238950
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Reviewed-by: Michael Wilson <mjwilson@chromium.org>
    Commit-Queue: Tom Van Goethem <tov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1568801}

--- third_party/blink/renderer/modules/webaudio/periodic_wave.cc
+++ third_party/blink/renderer/modules/webaudio/periodic_wave.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/compiler_specific.h"
+#include "base/containers/span.h"
 #include "build/build_config.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_periodic_wave_options.h"
 #include "third_party/blink/renderer/modules/webaudio/base_audio_context.h"
@@ -206,8 +207,8 @@ unsigned PeriodicWaveImpl::MaxNumberOfPartials() const {
 
 void PeriodicWaveImpl::WaveDataForFundamentalFrequency(
     float fundamental_frequency,
-    float*& lower_wave_data,
-    float*& higher_wave_data,
+    base::span<const float>& lower_wave_data,
+    base::span<const float>& higher_wave_data,
     float& table_interpolation_factor) {
   // Negative frequencies are allowed, in which case we alias to the positive
   // frequency.
@@ -234,8 +235,8 @@ void PeriodicWaveImpl::WaveDataForFundamentalFrequency(
   unsigned range_index2 =
       range_index1 < NumberOfRanges() - 1 ? range_index1 + 1 : range_index1;
 
-  lower_wave_data = band_limited_tables_[range_index2]->Data();
-  higher_wave_data = band_limited_tables_[range_index1]->Data();
+  lower_wave_data = band_limited_tables_[range_index2]->as_span();
+  higher_wave_data = band_limited_tables_[range_index1]->as_span();
 
   // Ranges from 0 -> 1 to interpolate between lower -> higher.
   table_interpolation_factor = pitch_range - range_index1;
@@ -243,15 +244,15 @@ void PeriodicWaveImpl::WaveDataForFundamentalFrequency(
 
 #if defined(ARCH_CPU_X86_FAMILY)
 void PeriodicWaveImpl::WaveDataForFundamentalFrequency(
-    const float fundamental_frequency[4],
-    float* lower_wave_data[4],
-    float* higher_wave_data[4],
-    float table_interpolation_factor[4]) {
+    const std::array<float, 4> fundamental_frequency,
+    std::array<base::span<const float>, 4>& lower_wave_data,
+    std::array<base::span<const float>, 4>& higher_wave_data,
+    std::array<float, 4>& table_interpolation_factor) {
   // Negative frequencies are allowed, in which case we alias to the positive
   // frequency.  SSE2 doesn't have an fabs instruction, so just remove the sign
   // bit of the float numbers, effecitvely taking the absolute value.
   const __m128 frequency =
-      _mm_and_ps(_mm_loadu_ps(fundamental_frequency),
+      _mm_and_ps(_mm_loadu_ps(fundamental_frequency.data()),
                  reinterpret_cast<__m128>(_mm_set1_epi32(0x7fffffff)));
 
   // pos = 0xffffffff if freq > 0; otherwise 0
@@ -304,27 +305,27 @@ void PeriodicWaveImpl::WaveDataForFundamentalFrequency(
 
   const __m128 table_factor =
       _mm_sub_ps(v_pitch_range, _mm_cvtepi32_ps(v_index1));
-  _mm_storeu_ps(table_interpolation_factor, table_factor);
+  _mm_storeu_ps(table_interpolation_factor.data(), table_factor);
 
   const unsigned* range_index1 = reinterpret_cast<const unsigned*>(&v_index1);
   const unsigned* range_index2 = reinterpret_cast<const unsigned*>(&v_index2);
 
-  for (int k = 0; k < 4; ++k) {
-    UNSAFE_TODO(lower_wave_data[k]) =
-        band_limited_tables_[UNSAFE_TODO(range_index2[k])]->Data();
-    UNSAFE_TODO(higher_wave_data[k]) =
-        band_limited_tables_[UNSAFE_TODO(range_index1[k])]->Data();
+  for (unsigned k = 0; k < 4; ++k) {
+    lower_wave_data[k] =
+        band_limited_tables_[UNSAFE_TODO(range_index2[k])]->as_span();
+    higher_wave_data[k] =
+        band_limited_tables_[UNSAFE_TODO(range_index1[k])]->as_span();
   }
 }
 #elif defined(CPU_ARM_NEON)
 void PeriodicWaveImpl::WaveDataForFundamentalFrequency(
-    const float fundamental_frequency[4],
-    float* lower_wave_data[4],
-    float* higher_wave_data[4],
-    float table_interpolation_factor[4]) {
+    const std::array<float, 4> fundamental_frequency,
+    std::array<base::span<const float>, 4>& lower_wave_data,
+    std::array<base::span<const float>, 4>& higher_wave_data,
+    std::array<float, 4>& table_interpolation_factor) {
   // Negative frequencies are allowed, in which case we alias to the positive
   // frequency.
-  float32x4_t frequency = vabsq_f32(vld1q_f32(fundamental_frequency));
+  float32x4_t frequency = vabsq_f32(vld1q_f32(fundamental_frequency.data()));
 
   // pos = 0xffffffff if frequency > 0; otherwise 0.
   uint32x4_t pos = vcgtq_f32(frequency, vdupq_n_f32(0));
@@ -368,21 +369,21 @@ void PeriodicWaveImpl::WaveDataForFundamentalFrequency(
 
   const float32x4_t table_factor =
       vsubq_f32(v_pitch_range, vcvtq_f32_u32(v_index1));
-  vst1q_f32(table_interpolation_factor, table_factor);
+  vst1q_f32(table_interpolation_factor.data(), table_factor);
 
   for (int k = 0; k < 4; ++k) {
-    UNSAFE_TODO(lower_wave_data[k]) =
-        band_limited_tables_[UNSAFE_TODO(range_index2[k])]->Data();
-    UNSAFE_TODO(higher_wave_data[k]) =
-        band_limited_tables_[UNSAFE_TODO(range_index1[k])]->Data();
+    lower_wave_data[k] =
+        band_limited_tables_[UNSAFE_TODO(range_index2[k])]->as_span();
+    higher_wave_data[k] =
+        band_limited_tables_[UNSAFE_TODO(range_index1[k])]->as_span();
   }
 }
 #else
 void PeriodicWaveImpl::WaveDataForFundamentalFrequency(
-    const float fundamental_frequency[4],
-    float* lower_wave_data[4],
-    float* higher_wave_data[4],
-    float table_interpolation_factor[4]) {
+    const std::array<float, 4> fundamental_frequency,
+    std::array<base::span<const float>, 4>& lower_wave_data,
+    std::array<base::span<const float>, 4>& higher_wave_data,
+    std::array<float, 4>& table_interpolation_factor) {
   for (int k = 0; k < 4; ++k) {
     WaveDataForFundamentalFrequency(fundamental_frequency[k],
                                     lower_wave_data[k], higher_wave_data[k],
