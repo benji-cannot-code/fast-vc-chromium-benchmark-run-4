ec6bea5c7eb8d7ca8846334e31e264ef93b637a3
Mustaq Ahmed <mustaq@google.com>
2026-01-14 13:47:16 -0800

    [CodeHealth] Make ScrollableArea SetScrollOffset non-virtual
    We are doing this for two reasons:
    - to remove the default parameters from the current virtual function to
    match the style guide [1], and
    - to add (later on) a scroll promise resolver parameter to the virtual
    method, without exposing the parameter to external callers.
    
    [1] https://google.github.io/styleguide/cppguide.html#Default_Arguments
    
    Bug: 41406914
    Change-Id: I45e6e574ae972168b242380ae2948f887a49afef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7457269
    Commit-Queue: Mustaq Ahmed <mustaq@chromium.org>
    Reviewed-by: David Awogbemila <awogbemila@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1569303}

--- third_party/blink/renderer/core/scroll/scrollable_area.cc
+++ third_party/blink/renderer/core/scroll/scrollable_area.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <limits>
 
+#include "base/compiler_specific.h"
 #include "base/functional/callback_helpers.h"
 #include "base/task/single_thread_task_runner.h"
 #include "build/build_config.h"
@@ -293,6 +294,16 @@ bool ScrollableArea::SetScrollOffset(const ScrollOffset& offset,
                                      cc::ScrollSourceType source_type,
                                      mojom::blink::ScrollBehavior behavior,
                                      bool targeted_scroll) {
+  return SetScrollOffsetInternal(offset, scroll_type, source_type, behavior,
+                                 targeted_scroll);
+}
+
+bool ScrollableArea::SetScrollOffsetInternal(
+    const ScrollOffset& offset,
+    mojom::blink::ScrollType scroll_type,
+    cc::ScrollSourceType source_type,
+    mojom::blink::ScrollBehavior behavior,
+    bool targeted_scroll) {
   ScrollableArea::ScrollCallback run_scroll_complete_callbacks(BindOnce(
       [](WeakPersistent<ScrollableArea> area, ScrollCompletionMode mode) {
         if (area) {
