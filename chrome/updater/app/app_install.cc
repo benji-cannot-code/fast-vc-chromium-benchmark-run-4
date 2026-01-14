c38d2da7a9c19c6e3913ef558b60ad30dd89351e
Sorin Jianu <sorin@chromium.org>
2026-01-14 06:54:53 -0800

    updater: testing if the C++23 compiler is real
    Change-Id: I5a17c0f840e1bd2f5f2c20e489c8e7c99800c77e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7467572
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Noah Rose Ledesma <noahrose@google.com>
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Cr-Commit-Position: refs/heads/main@{#1569093}

--- chrome/updater/app/app_install.cc
+++ chrome/updater/app/app_install.cc
@@ -105,7 +105,7 @@ class AppInstallControllerImpl : public AppInstallController {
 
 scoped_refptr<App> MakeAppInstall(bool /*is_silent_install*/) {
   return base::MakeRefCounted<AppInstall>(
-      base::BindRepeating([]() -> scoped_refptr<AppInstallController> {
+      base::BindRepeating([] -> scoped_refptr<AppInstallController> {
         return base::MakeRefCounted<AppInstallControllerImpl>();
       }));
 }
