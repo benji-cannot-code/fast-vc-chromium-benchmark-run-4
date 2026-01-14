760e62623c26d122a5a9e8bb457f581055aad9f4
David M <davidjm@google.com>
2026-01-14 14:39:58 -0800

    Add Tab Groups view to Projects Panel
    This CL adds the Tab Groups view to the projects panel and includes
    the Tab Groups heading.
    
    Screenshot: https://screenshot.googleplex.com/AnyezJWC5HV3XCo
    
    Bug: 475305709
    Change-Id: Id46d91638ca9e178f30afde89674fdc90e678f1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7467504
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Auto-Submit: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1569348}

--- chrome/browser/ui/views/tabs/projects/projects_panel_view.cc
+++ chrome/browser/ui/views/tabs/projects/projects_panel_view.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/layout_constants.h"
 #include "chrome/browser/ui/tabs/projects/projects_panel_state_controller.h"
 #include "chrome/browser/ui/views/tabs/projects/projects_panel_controls_view.h"
+#include "chrome/browser/ui/views/tabs/projects/projects_panel_tab_groups_view.h"
 #include "chrome/browser/ui/views/tabs/vertical/top_container_button.h"
 #include "ui/actions/actions.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
@@ -46,6 +47,9 @@ ProjectsPanelView::ProjectsPanelView(actions::ActionItem* root_action_item)
   SetPaintToLayer();
 
   controls_view_ = AddChildView(std::make_unique<ProjectsPanelControlsView>(
+      root_action_item_.get(), action_view_controller_.get()));
+
+  tab_groups_view_ = AddChildView(std::make_unique<ProjectsPanelTabGroupsView>(
       root_action_item_.get(), action_view_controller_.get()));
 
   SetVisible(false);
