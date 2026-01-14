10408b83d8e185609b71e02b4c5e539d1ceb5179
Kunal Daftari <kunaldaftari@google.com>
2026-01-14 12:39:07 -0800

    [Vertical Tabs] Repeatedly clicking close button fix
    Fix was just checking that the collection_node_ exists before trying to
    close it.
    
    Before Fix:
    https://screencast.googleplex.com/cast/NTY4Mzc5ODIyOTM4NTIxNnxlYzZlNzJjZC1mNw
    After Fix:
    https://screencast.googleplex.com/cast/NDYzMTYwNTY0ODgxODE3NnxlY2M3Yzg2Ni1jMQ
    
    Bug: 475549840
    Change-Id: I20d64a9bad89107423dc92474f4aa66a3cc9001d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7472098
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Kunal Daftari <kunaldaftari@google.com>
    Reviewed-by: Charles Meng <charlesmeng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1569236}

--- chrome/browser/ui/views/tabs/vertical/vertical_tab_view.cc
+++ chrome/browser/ui/views/tabs/vertical/vertical_tab_view.cc
@@ -577,7 +577,9 @@ void VerticalTabView::UpdateContrastRatioValues() {
 
 void VerticalTabView::CloseButtonPressed(const ui::Event& event) {
   // TODO(crbug.com/467735166): Log tab closing UMAs.
-  collection_node_->GetController()->CloseTab(GetTabInterface());
+  if (collection_node_) {
+    collection_node_->GetController()->CloseTab(GetTabInterface());
+  }
 }
 
 bool VerticalTabView::IsHoverAnimationActive() const {
