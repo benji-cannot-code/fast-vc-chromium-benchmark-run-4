0d69f7aa2d20d54054296594e8ac03faec4eb51d
Anders Hartvoll Ruud <andruud@chromium.org>
2026-01-14 02:58:54 -0800

    [scroll-animations] Create DeferredTimelines lazily
    The timeline-scope property currently accepts a list of names,
    and each such name results in a DeferredTimeline being eagerly created
    on the relevant element. This approach does not work for
    timeline-scope:all; eagerly creating all possible timelines
    on an element isn't viable.
    
    Instead, each element now holds a CSSDeferredTimelineMap, which
    logically contains all possible DeferredTimelines by default
    (but doesn't really), and timeline-scope merely *filters* the subset
    of DeferredTimelines that are reachable.
    
    When looking for a DeferredTimeline to attach to in the ancestor chain
    (CalculateTimelineAttachmentUpdate()), the act of looking for it will
    now *create* it (if needed) behind the scenes---provided that
    the lookup matches the specified filter. This makes it obvious how
    to implement 'all': just use a CSSDeferredTimelineMap with no filter.
    
    Note that CSSDeferredTimelineMap holds DeferredTimelines internally
    using weak references. This means that "unused" DeferredTimelines will
    disappear from the CSSDeferredTimelineMap during GC. This is assumed
    to be fine; a non-retained DeferredTimeline implies that no scroll/view
    timeline is attached to it, *and* that no animations are playing on it.
    Note also that DeferredTimelines cannot be held (directly) by JS,
    and they are generally not exposed through APIs; see DeferredTimeline::
    ExposedTimeline().
    
    Bug: 41488030
    Change-Id: I166127fef1a68cb7939a8bcab859a8a4b0a29f27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7415341
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: David Awogbemila <awogbemila@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1569013}

--- third_party/blink/renderer/core/animation/css/css_animation_update.h
+++ third_party/blink/renderer/core/animation/css/css_animation_update.h
@@ -218,8 +218,9 @@ class CORE_EXPORT CSSAnimationUpdate final {
     changed_view_timelines_ = std::move(timelines);
   }
 
-  void SetChangedDeferredTimelines(CSSDeferredTimelineMap timelines) {
-    changed_deferred_timelines_ = std::move(timelines);
+  void SetUpdatedDeferredTimelineMap(CSSDeferredTimelineMap map) {
+    has_updated_deferred_timeline_map_ = true;
+    updated_deferred_timeline_map_ = std::move(map);
   }
 
   void SetChangedTimelineAttachments(TimelineAttachmentMap attachments) {
@@ -296,8 +297,16 @@ class CORE_EXPORT CSSAnimationUpdate final {
   const CSSViewTimelineMap& ChangedViewTimelines() const {
     return changed_view_timelines_;
   }
-  const CSSDeferredTimelineMap& ChangedDeferredTimelines() const {
-    return changed_deferred_timelines_;
+  const CSSDeferredTimelineMap* UpdatedDeferredTimelineMap() const {
+    return has_updated_deferred_timeline_map_ ? &updated_deferred_timeline_map_
+                                              : nullptr;
+  }
+  std::optional<CSSDeferredTimelineMap> TakeUpdatedDeferredTimelineMap() {
+    if (has_updated_deferred_timeline_map_) {
+      return std::optional<CSSDeferredTimelineMap>(
+          std::move(updated_deferred_timeline_map_));
+    }
+    return std::nullopt;
   }
   const TimelineAttachmentMap& ChangedTimelineAttachments() const {
     return changed_timeline_attachments_;
@@ -338,9 +347,8 @@ class CORE_EXPORT CSSAnimationUpdate final {
            !updated_compositor_keyframes_.empty() ||
            !changed_scroll_timelines_.empty() ||
            !changed_view_timelines_.empty() ||
-           !changed_deferred_timelines_.empty() ||
            !changed_timeline_attachments_.empty() ||
-           needs_named_trigger_update_;
+           has_updated_deferred_timeline_map_ || needs_named_trigger_update_;
   }
 
   void Trace(Visitor* visitor) const {
@@ -353,7 +361,7 @@ class CORE_EXPORT CSSAnimationUpdate final {
     visitor->Trace(active_interpolations_for_transitions_);
     visitor->Trace(changed_scroll_timelines_);
     visitor->Trace(changed_view_timelines_);
-    visitor->Trace(changed_deferred_timelines_);
+    visitor->Trace(updated_deferred_timeline_map_);
     visitor->Trace(changed_timeline_attachments_);
   }
 
@@ -380,7 +388,8 @@ class CORE_EXPORT CSSAnimationUpdate final {
 
   CSSScrollTimelineMap changed_scroll_timelines_;
   CSSViewTimelineMap changed_view_timelines_;
-  CSSDeferredTimelineMap changed_deferred_timelines_;
+  bool has_updated_deferred_timeline_map_ = false;
+  CSSDeferredTimelineMap updated_deferred_timeline_map_;
   TimelineAttachmentMap changed_timeline_attachments_;
 
   ActiveInterpolationsMap active_interpolations_for_animations_;
