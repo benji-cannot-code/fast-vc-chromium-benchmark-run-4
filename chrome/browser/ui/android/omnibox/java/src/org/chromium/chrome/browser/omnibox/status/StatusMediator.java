1f1b01cd6e47bc13442aedcb57ffa53ad060e2fb
Judith Hemp <hempjudith@google.com>
2026-01-14 12:54:42 -0800

    [ClapperQuiet] Adds Clapper Quiet Icon
    Undoing
    https://chromium-review.googlesource.com/c/chromium/src/+/7460948. This
    was reviewed on
    https://chromium-review.googlesource.com/c/chromium/src/+/7362147
    
    Note: its the same change except for me using less device specific testing calls. See diff between initial upload and patchset 4.
    
    Bug: 463333225
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7362147
    Commit-Queue: Judith Hemp <hempjudith@google.com>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1568293}
    Change-Id: I9aa35dc7587bb954902c5245c7399a3a45d25d99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7456193
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1569253}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/status/StatusMediator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/status/StatusMediator.java
@@ -498,13 +498,14 @@ public class StatusMediator
      */
     @Override
     public void updateLocationBarIcon(@IconTransitionType int transitionType) {
-        // Reset the permission icon.
-        mPermissionStatusHandler.reset();
         // Reset the store icon status.
         mIsStoreIconShowing = false;
 
         // No need to proceed further if we've already updated it for the search engine icon.
-        if (maybeUpdateStatusIconForSearchEngineIcon()) return;
+        if (maybeUpdateStatusIconForSearchEngineIcon()) {
+            mPermissionStatusHandler.reset(/* shouldDismissNativePrompt= */ true);
+            return;
+        }
 
         int icon = 0;
         int tint = 0;
@@ -515,6 +516,7 @@ public class StatusMediator
 
         if (mLocationBarDataProvider.getPageClassification(/* prefetch= */ false)
                 == PageClassification.ANDROID_HUB_VALUE) {
+            mPermissionStatusHandler.reset(/* shouldDismissNativePrompt= */ false);
             // Show the status icon primarily for incognito since it is defaulted off there.
             setStatusIconShown(/* show= */ true);
             icon = R.drawable.ic_arrow_back_24dp;
@@ -524,6 +526,7 @@ public class StatusMediator
                     mModel.get(StatusProperties.SHOW_STATUS_ICON),
                     mModel.get(StatusProperties.VERBOSE_STATUS_TEXT_VISIBLE));
         } else if (mUrlHasFocus) {
+            mPermissionStatusHandler.reset(/* shouldDismissNativePrompt= */ true);
             if (mShowStatusIconWhenUrlFocused) {
                 icon =
                         mUrlBarTextIsSearch
@@ -531,6 +534,8 @@ public class StatusMediator
                                 : R.drawable.ic_globe_24dp;
                 tint = mNavigationIconTintRes;
             }
+        } else if (mPermissionStatusHandler.isClapperQuietIconShowing()) {
+            return;
         } else if (mSecurityIconRes != 0) {
             mIsSecurityViewShown = true;
             icon = mSecurityIconRes;
@@ -747,16 +752,21 @@ public class StatusMediator
     @VisibleForTesting
     @Override
     public void resetCustomIconsStatus() {
-        mPermissionStatusHandler.reset();
-        mStoreIconHandler.removeCallbacksAndMessages(null);
-        mIconTaskHandler.removeCallbacksAndMessages(null);
-        mIsStoreIconShowing = false;
+        mPermissionStatusHandler.reset(/* shouldDismissNativePrompt= */ true);
+        resetEmbeddedIconHandlers();
     }
 
     /** Notifies that the page info was opened. */
     void onPageInfoOpened() {
-        resetCustomIconsStatus();
+        mPermissionStatusHandler.onPageInfoOpened();
+        resetEmbeddedIconHandlers();
         updateLocationBarIcon(IconTransitionType.CROSSFADE);
+    }
+
+    private void resetEmbeddedIconHandlers() {
+        mStoreIconHandler.removeCallbacksAndMessages(null);
+        mIconTaskHandler.removeCallbacksAndMessages(null);
+        mIsStoreIconShowing = false;
     }
 
     boolean isStoreIconShowing() {
