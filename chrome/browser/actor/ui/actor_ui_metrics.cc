cb13bf326795c7275ad69be4475f617e2d50e0a4
Kene Okoye <kenok@google.com>
2026-01-14 12:57:07 -0800

    Enable SplitButton and TaskIndicator changes by default
    - And move ActorTaskNudgeState::Text DCHECK in actor_ui_metrics.cc from
          RecordGlobalTaskIndicatorNudgeShown to RecordTaskNudgeShown as it
          was erroneously added in crrev.com/c/7415524
    
    Bug: 474119436
    Change-Id: Iaa557004dc37f395e3012447462a8bb3e765e861
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7414963
    Commit-Queue: Abe Boujane <boujane@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Auto-Submit: Maggie Jennings <mjenn@google.com>
    Commit-Queue: Kene Okoye <kenok@google.com>
    Cr-Commit-Position: refs/heads/main@{#1569257}

--- chrome/browser/actor/ui/actor_ui_metrics.cc
+++ chrome/browser/actor/ui/actor_ui_metrics.cc
@@ -76,6 +76,8 @@ void RecordTaskIconError(ActorUiTaskIconError error) {
 }
 
 void RecordTaskNudgeShown(ActorTaskNudgeState nudge_state) {
+  DCHECK_NE(nudge_state.text, ActorTaskNudgeState::Text::kDefault)
+      << "Nudge is hidden in default state so it cannot be shown.";
   DCHECK_NE(nudge_state.text,
             ActorTaskNudgeState::Text::kMultipleTasksNeedAttention)
       << "MultipleTasksNeedAttention state is deprecated.";
@@ -84,8 +86,6 @@ void RecordTaskNudgeShown(ActorTaskNudgeState nudge_state) {
 }
 
 void RecordGlobalTaskIndicatorNudgeShown(ActorTaskNudgeState nudge_state) {
-  DCHECK_NE(nudge_state.text, ActorTaskNudgeState::Text::kDefault)
-      << "Nudge is hidden in default state so it cannot be shown.";
   DCHECK_NE(nudge_state.text,
             ActorTaskNudgeState::Text::kMultipleTasksNeedAttention)
       << "MultipleTasksNeedAttention state is deprecated.";
--- chrome/browser/actor/ui/task_list_bubble/actor_task_list_bubble_interactive_uitest.cc
+++ chrome/browser/actor/ui/task_list_bubble/actor_task_list_bubble_interactive_uitest.cc
@@ -47,6 +47,7 @@ class ActorTaskListBubbleInteractiveUiTest
             {features::kGlicActor, {}},
             {features::kGlicActorUi,
              {{features::kGlicActorUiTaskIconName, "true"}}},
+            {features::kGlicActorUiGlobalTaskIndicator, {}},
 #endif  // BUILDFLAG(ENABLE_GLIC)
         },
         {});
@@ -67,19 +68,21 @@ IN_PROC_BROWSER_TEST_F(ActorTaskListBubbleInteractiveUiTest,
 
   const char kFirstTaskItem[] = "FirstTaskItem";
   StartActingOnTab();
-  RunTestSequence(
-      Do([&]() { PauseTask(); }),
-      InAnyContext(WaitForShow(kActorTaskListBubbleView)),
-      CheckView(
-          kActorTaskListBubbleView,
-          [](views::View* view) { return view->children().size() == 1u; }),
-      InSameContext(NameDescendantViewByType<RichHoverButton>(
-          kActorTaskListBubbleView, kFirstTaskItem, 0)),
-      CheckViewProperty(kFirstTaskItem, &RichHoverButton::GetSubtitleText,
-                        l10n_util::GetStringUTF16(
-                            IDR_ACTOR_TASK_LIST_BUBBLE_ROW_CHECK_TASK_SUBTITLE)),
-      PressButton(kFirstTaskItem),
-      InAnyContext(WaitForHide(kActorTaskListBubbleView)));
+  RunTestSequence(Do([&]() { PauseTask(); }),
+                  InAnyContext(WaitForShow(kGlicActorTaskIconElementId)),
+                  InAnyContext(WaitForShow(kActorTaskListBubbleView)),
+                  CheckView(kActorTaskListBubbleView,
+                            [](views::View* view) {
+                              return view->children().size() == 1u;
+                            }),
+                  InSameContext(NameDescendantViewByType<RichHoverButton>(
+                      kActorTaskListBubbleView, kFirstTaskItem, 0)),
+                  CheckViewProperty(
+                      kFirstTaskItem, &RichHoverButton::GetSubtitleText,
+                      l10n_util::GetStringUTF16(
+                          IDR_ACTOR_TASK_LIST_BUBBLE_ROW_CHECK_TASK_SUBTITLE)),
+                  PressButton(kFirstTaskItem),
+                  InAnyContext(WaitForHide(kActorTaskListBubbleView)));
 }
 
 IN_PROC_BROWSER_TEST_F(ActorTaskListBubbleInteractiveUiTest,
@@ -101,19 +104,21 @@ IN_PROC_BROWSER_TEST_F(ActorTaskListBubbleInteractiveUiTest,
 
   const char kFirstTaskItem[] = "FirstTaskItem";
   base::UserActionTester user_action_tester;
-  RunTestSequence(
-      Do([&]() { PauseTask(); }),
-      InAnyContext(WaitForShow(kActorTaskListBubbleView)),
-      CheckView(
-          kActorTaskListBubbleView,
-          [](views::View* view) { return view->children().size() == 1u; }),
-      InSameContext(NameDescendantViewByType<RichHoverButton>(
-          kActorTaskListBubbleView, kFirstTaskItem, 0)),
-      CheckViewProperty(kFirstTaskItem, &RichHoverButton::GetSubtitleText,
-                        l10n_util::GetStringUTF16(
-                            IDR_ACTOR_TASK_LIST_BUBBLE_ROW_CHECK_TASK_SUBTITLE)),
-      PressButton(kFirstTaskItem),
-      InAnyContext(WaitForHide(kActorTaskListBubbleView)));
+  RunTestSequence(Do([&]() { PauseTask(); }),
+                  InAnyContext(WaitForShow(kGlicActorTaskIconElementId)),
+                  InAnyContext(WaitForShow(kActorTaskListBubbleView)),
+                  CheckView(kActorTaskListBubbleView,
+                            [](views::View* view) {
+                              return view->children().size() == 1u;
+                            }),
+                  InSameContext(NameDescendantViewByType<RichHoverButton>(
+                      kActorTaskListBubbleView, kFirstTaskItem, 0)),
+                  CheckViewProperty(
+                      kFirstTaskItem, &RichHoverButton::GetSubtitleText,
+                      l10n_util::GetStringUTF16(
+                          IDR_ACTOR_TASK_LIST_BUBBLE_ROW_CHECK_TASK_SUBTITLE)),
+                  PressButton(kFirstTaskItem),
+                  InAnyContext(WaitForHide(kActorTaskListBubbleView)));
 
   EXPECT_TRUE(tab_one->IsActivated());
   EXPECT_FALSE(tab_two->IsActivated());
