2240e611205085b868704fa986ca937ea306a662
Alexis Hetu <sugoi@chromium.org>
2026-01-14 04:31:57 -0800

    [iOS] Add feature flag for passkey conditional create with JS shim
    This CL adds the flag and uses it to properly set the associated
    placeholder replacement and is also used alongside the equivalent
    modal flag to check if using the passkey JS shim is allowed.
    
    Bug: 460486709
    Change-Id: I8beaf913e4324978d8cc90fd063f7e3dfd947e0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7423342
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1569040}

--- components/webauthn/ios/features.mm
+++ components/webauthn/ios/features.mm
@@ -10,3 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 BASE_FEATURE(kIOSPasskeyShim, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kIOSPasskeyModalLoginWithShim, base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kIOSPasskeyConditionalLoginWithShim,
+             base::FEATURE_DISABLED_BY_DEFAULT);
