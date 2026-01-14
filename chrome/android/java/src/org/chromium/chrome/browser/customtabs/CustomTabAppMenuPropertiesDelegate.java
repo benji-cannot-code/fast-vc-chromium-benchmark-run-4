7554e97eafa5d8053bb5f1281d2e96feb7ed1b85
Eleanor Lee <eleanorlee@google.com>
2026-01-14 12:19:23 -0800

    [Community Contributions] Clean up readAloudInOverflowMenuInCCT flag
    Bug: 475015162
    Change-Id: Id298e160224121f8cb8311db525dc878fc0dfc45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7446007
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Eleanor Lee <eleanorlee@google.com>
    Reviewed-by: Basia Zimirska <basiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1569223}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabAppMenuPropertiesDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabAppMenuPropertiesDelegate.java
@@ -33,7 +33,6 @@ import org.chromium.chrome.browser.firstrun.FirstRunStatus;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.multiwindow.MultiWindowModeStateDispatcher;
 import org.chromium.chrome.browser.readaloud.ReadAloudController;
-import org.chromium.chrome.browser.readaloud.ReadAloudFeatures;
 import org.chromium.chrome.browser.segmentation_platform.ContextualPageActionController;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
@@ -140,7 +139,7 @@ public class CustomTabAppMenuPropertiesDelegate extends AppMenuPropertiesDelegat
         boolean downloadItemVisible = mShowDownload;
         boolean addToHomeScreenVisible = true;
         boolean requestDesktopSiteVisible = true;
-        boolean tryAddingReadAloud = ReadAloudFeatures.isEnabledForOverflowMenuInCct();
+        boolean tryAddingReadAloud = true;
         boolean readerModePrefsVisible = false;
         boolean translateVisible = true;
         // When the icon row is visible, site info is a button in that row.
