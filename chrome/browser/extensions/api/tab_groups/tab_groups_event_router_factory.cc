8253bcceae9c1d80a2fb56944b30aaf86d47f252
Eriko Kurimoto <elkurin@chromium.org>
2026-01-13 22:26:02 -0800

    Revert "extensions: Add TabGroupsEventRouter to the desktop Android build"
    This reverts commit 87f25767ca38347a702358bdbbf4f636fff5b3df.
    
    Reason for revert: Culprit of Linux ChromeOS MSan bot breakage
    
    First failure log: https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/57460/overview
    
    Original change's description:
    > extensions: Add TabGroupsEventRouter to the desktop Android build
    >
    > It doesn't work yet, but do the refactoring needed to support both
    > Win/Mac/Linux tabs and Android tabs. This involves introducing a
    > "TabHelper", similar to the Web Navigation API. For now, only the
    > Win/Mac/Linux implementation is provided.
    >
    > Do the usual cleanup: Fix the build file, fix unnecessary includes
    > and public_deps, register the event router factory on startup, etc.
    >
    > The Android implementation will require JNI/Java code to listen for
    > tab group updates and notify the native side, perhaps through new
    > methods on TabListInterfaceObserver.
    >
    > Bug: 405219902
    > Change-Id: Ib80d50bac6a9763528e779c22c748022c37d1e0a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7457952
    > Reviewed-by: Zoraiz Naeem <zoraiznaeem@chromium.org>
    > Commit-Queue: James Cook <jamescook@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1568833}
    
    Bug: 405219902
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I848357b6bdf294c254f8b9d24940e2fcfbd25c49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7462038
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Eriko Kurimoto <elkurin@chromium.org>
    Owners-Override: Eriko Kurimoto <elkurin@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1568928}

--- chrome/browser/extensions/api/tab_groups/tab_groups_event_router_factory.cc
+++ chrome/browser/extensions/api/tab_groups/tab_groups_event_router_factory.cc
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_context.h"
 #include "extensions/browser/event_router_factory.h"
 
-static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
-
 namespace extensions {
 
 // static
