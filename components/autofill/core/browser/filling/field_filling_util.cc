606168e1d0331e40d8746f41f5728ed771ceefd7
Bruno Braga <brunobraga@google.com>
2026-01-14 21:22:30 -0800

    [Autofill AI - reauth] Keep last 4 digits visible
    This is design requirement.
    
    This CL also adds obfuscation only to platforms where authentication
    is available.
    
    Demo: https://screenshot.googleplex.com/BdiQwvH8cBJrMMY
    
    Bug: 468236777
    Change-Id: I82b3fabce4ad9517515c9494dc046fbc466807d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7380093
    Auto-Submit: Bruno Braga <brunobraga@google.com>
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1569517}

--- components/autofill/core/browser/filling/field_filling_util.cc
+++ components/autofill/core/browser/filling/field_filling_util.cc
@@ -5,9 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/autofill/core/browser/filling/field_filling_util.h"
 
+#include <algorithm>
 #include <string>
 
 #include "base/i18n/case_conversion.h"
+#include "base/i18n/rtl.h"
 #include "base/i18n/string_search.h"
 #include "base/no_destructor.h"
 #include "base/strings/string_number_conversions.h"
@@ -206,7 +208,8 @@ std::optional<std::u16string> GetNumericSelectControlValue(
   return std::nullopt;
 }
 
-std::u16string GetObfuscatedValue(const std::u16string& value) {
+std::u16string GetObfuscatedValue(const std::u16string& value,
+                                  size_t visible_suffix_length) {
   // Same obfuscation symbol as used for credit cards - see also credit_card.h.
   //  - \u2022 - Bullet.
   //  - \u2006 - SIX-PER-EM SPACE (small space between bullets).
@@ -215,12 +218,26 @@ std::u16string GetObfuscatedValue(const std::u16string& value) {
   // This is only an approximation of the number of the actual unicode
   // characters - if we want to match the length exactly, we would need to use
   // `base::CountUnicodeCharacters`.
-  const size_t obfuscation_length = value.size();
+  visible_suffix_length = std::min(visible_suffix_length, value.size());
+  size_t obfuscation_length = value.size() - visible_suffix_length;
+
   std::u16string result;
-  result.reserve(sizeof(kDot) * obfuscation_length);
+  result.reserve(sizeof(kDot) * obfuscation_length + visible_suffix_length);
+
   for (size_t i = 0; i < obfuscation_length; ++i) {
     result.append(kDot);
   }
+  if (visible_suffix_length > 0) {
+    result.append(value.substr(value.size() - visible_suffix_length));
+  }
+
+  // `WrapStringWithLTRFormatting` guarantees that the following passport number
+  // 123456789 won't be rendered as 789** in RTL languaguages. It forces the
+  // browser to use LTR convention.
+  if (base::i18n::IsRTL()) {
+    base::i18n::WrapStringWithLTRFormatting(&result);
+  }
+
   return result;
 }
 
