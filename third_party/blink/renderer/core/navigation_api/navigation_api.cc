613795f97e48cd0ba38a05bebbd6fe13e604b4ff
Morten Stenshorne <mstensho@chromium.org>
2026-01-15 04:12:08 -0800

    [RouteMatching] Match "from" and "to" earlier.
    Match as soon as the navigate event is dispatched.
    Do not wait until the transition has been committed.
    
    Note that for "to" navigations in particular, we should also react to
    any redirects that might occur. Will deal with that in a separate CL.
    
    Bug: 436805487
    Change-Id: Ib0f0c0a9579de1bb43f7bd03152af143869ad9f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7455810
    Reviewed-by: Noam Rosenthal <nrosenthal@google.com>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1569648}

--- third_party/blink/renderer/core/navigation_api/navigation_api.cc
+++ third_party/blink/renderer/core/navigation_api/navigation_api.cc
@@ -308,7 +308,7 @@ void NavigationApi::UpdateForNavigation(HistoryItem& item,
   }
 
   if (auto* routemap = RouteMap::Get(window_->document())) {
-    routemap->OnNavigationStart(old_current->url(), currentEntry()->url());
+    routemap->OnNavigationCommitted();
   }
 }
 
@@ -863,6 +863,11 @@ NavigationApi::DispatchResult NavigationApi::DispatchNavigateEvent(
 
   CHECK(!ongoing_navigate_event_);
   ongoing_navigate_event_ = navigate_event;
+
+  if (auto* routemap = RouteMap::Get(window_->document())) {
+    routemap->OnNavigationStart(window_->Url(), params->url);
+  }
+
   has_dropped_navigation_ = false;
   DispatchEvent(*navigate_event);
 
