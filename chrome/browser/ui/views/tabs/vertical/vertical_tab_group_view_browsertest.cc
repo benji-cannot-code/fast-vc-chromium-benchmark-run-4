739b88cd261bbe628951524af817fc51ef561c22
Thomas Lukaszewicz <tluk@chromium.org>
2026-01-15 09:22:48 -0800

    [Vertical Tabs] Ensure tab groups have bounds before test interaction
    Currently when VerticalTabGroupViewTests create tab groups via
    CreateActiveTabGroup() or CreateInactiveTabGroup() it creates a
    TabCollectionNode and associated view. However the group's view bounds
    start empty and layout is not performed before attempting test
    interactions (such as opening and closing the group).
    
    This can result in non representative animation issues which rely on
    transitions between changing view bounds.
    
    This CL ensures that scheduled layouts are performed and the group view
    is assigned bounds before attempting to interact with the created tab
    group view.
    
    Bug: 459824840
    Change-Id: Ic808ee7d8527799652a9e7b254504555351f6d87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7482470
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1569811}

--- chrome/browser/ui/views/tabs/vertical/vertical_tab_group_view_browsertest.cc
+++ chrome/browser/ui/views/tabs/vertical/vertical_tab_group_view_browsertest.cc
@@ -40,6 +40,7 @@ class VerticalTabGroupViewTest
                TabStripUserGestureDetails::GestureType::kOther));
 
     browser()->tab_strip_model()->AddToNewGroup({1});
+    RunScheduledLayouts();
   }
 
   void CreateInactiveTabGroup() {
@@ -51,6 +52,7 @@ class VerticalTabGroupViewTest
     browser()->tab_strip_model()->ActivateTabAt(
         2, TabStripUserGestureDetails(
                TabStripUserGestureDetails::GestureType::kOther));
+    RunScheduledLayouts();
   }
 
   void ClickTabGroupHeaderToToggleCollapse() {
