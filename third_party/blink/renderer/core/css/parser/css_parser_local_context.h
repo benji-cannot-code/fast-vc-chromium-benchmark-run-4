286cab54cdaea6b4b2618ca08dd2d63f1573e51e
moonira <moonira@google.com>
2026-01-15 11:47:05 -0800

    Use property info for random() from CSSParserLocalContext
    Replace CopyRandomWithPropertyNameAndValueIndexIfNeeded approach for
    getting property name and random() index in CSSMathExpression, with
    getting that information from CSSParserLocalContext.
    
    Note: Custom properties are not yet supported, for them we need to pass
    CSSParserLocalContext to SyntaxDefinition, to be done in the next CLs.
    Also because of the workaround for random() unresolvable percentages
    issue [0], resolvable percentages are not always properly parsed, there
    is a failing palette-mix test reflecting that.
    
    [0] crbug.com/463635948
    
    Bug: 413385732
    Change-Id: I58d752624fadcf0efae1f37aa2f1567fced66606
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7380112
    Commit-Queue: Munira Tursunova <moonira@google.com>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1569918}

--- third_party/blink/renderer/core/css/parser/css_parser_local_context.h
+++ third_party/blink/renderer/core/css/parser/css_parser_local_context.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/css_property_name.h"
 #include "third_party/blink/renderer/core/css/css_property_names.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
+#include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
 
 namespace blink {
 
@@ -130,6 +131,24 @@ class CORE_EXPORT CSSParserLocalContext {
   }
 
   CSSPropertyID CurrentShorthand() const { return current_shorthand_; }
+
+  const AtomicString PropertyNameAndRandomCount() const {
+    StringBuilder str;
+    if (property_name_.has_value()) {
+      // Use string of form "PROPERTY {property_name} {property_value_index}"
+      // as name, this is later used for caching random values [0]. The prefix
+      // "PROPERTY" is needed since we need to make distinguish between custom
+      // property name and random value identifier, i.e. <dashed-ident> value in
+      // <random-value-sharing> [1]
+      // [0] https://drafts.csswg.org/css-values-5/#random-caching-key
+      // [1] https://drafts.csswg.org/css-values-5/#typedef-random-value-sharing
+      str.Append("PROPERTY ");
+      str.Append(property_name_->ToAtomicString());
+      str.Append(" ");
+      str.AppendNumber(random_value_count_);
+    }
+    return str.ToAtomicString();
+  }
 
   wtf_size_t RandomValueCount() const { return random_value_count_; }
 
