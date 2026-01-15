b5842c64db36a90c722e8abc11169c8e0e57c6ea
Andrew Vuong <akvuong@google.com>
2026-01-15 07:47:56 -0800

    [AccessibilityAnnotator] Add service for accessibility annotator
    Adds the basic structure for the Accessibility Annotator feature,
    including a ProfileKeyedService, factory, feature flag, and initial unit
    tests.
    
    Bug: 475305011
    Change-Id: Idc0775b393e8a07eab79bca29bc5ecd62246b773
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7456046
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Andrew Vuong <akvuong@google.com>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1569755}

---
+++ chrome/browser/accessibility_annotator/accessibility_annotator_features.cc
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/accessibility_annotator/accessibility_annotator_features.h"
+
+namespace accessibility_annotator {
+
+BASE_FEATURE(kAccessibilityAnnotator, base::FEATURE_DISABLED_BY_DEFAULT);
+
+}  // namespace accessibility_annotator
---
+++ chrome/browser/accessibility_annotator/accessibility_annotator_service.cc
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/accessibility_annotator/accessibility_annotator_service.h"
+
+#include "chrome/browser/profiles/profile.h"
+
+namespace accessibility_annotator {
+
+AccessibilityAnnotatorService::AccessibilityAnnotatorService(Profile* profile)
+    : profile_(profile) {}
+
+AccessibilityAnnotatorService::~AccessibilityAnnotatorService() = default;
+
+}  // namespace accessibility_annotator
