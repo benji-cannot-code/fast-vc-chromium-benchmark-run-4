ae1ae81ccfa58db11ec565b3ff1a1dd66a96a217
shomizoe <shomizoe@google.com>
2026-01-15 10:49:08 -0800

    Add a new method to RemoteSuggestionsServiceSimple
    This CL adds a new method GetActionChipSuggestionsForTab to RemoteSuggestionsServiceSimple.
    This method calls the suggestions endpoint with a new set of query parameters, so we get a
    set of suggestions for the current state.
    
    A new RequestSource has been added to use a new client chrome-ntp-action
    
    Bug: b:473586071
    Change-Id: Ibe428f4c35ca5f2880907f3893888f1a6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7410998
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Reviewed-by: Jennifer Serrano <jennserrano@google.com>
    Commit-Queue: Sho Mizoe <shomizoe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1569892}

--- chrome/browser/ui/webui/new_tab_page/action_chips/action_chips_generator_unittest.cc
+++ chrome/browser/ui/webui/new_tab_page/action_chips/action_chips_generator_unittest.cc
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_errors.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/omnibox_proto/aim_tools.pb.h"
 #include "third_party/omnibox_proto/groups.pb.h"
 #include "url/gurl.h"
 namespace {
@@ -106,6 +107,17 @@ class MockRemoteSuggestionsServiceSimple
                const GURL& url,
                base::OnceCallback<void(ActionChipSuggestionsResult&&)>),
               (override));
+  MOCK_METHOD(
+      std::unique_ptr<network::SimpleURLLoader>,
+      GetActionChipSuggestions,
+      (base::optional_ref<const std::u16string> title,
+       base::optional_ref<const GURL> url,
+       base::span<const omnibox::ToolMode> allowed_tools,
+       base::optional_ref<const omnibox::PageVertical> page_vertical,
+       base::OnceCallback<
+           void(RemoteSuggestionsServiceSimple::ActionChipSuggestionsResult&&)>
+           callback),
+      (override));
 };
 
 int32_t GetTabHandleId(const tabs::TabInterface* tab) {
--- chrome/browser/ui/webui/new_tab_page/action_chips/remote_suggestions_service_simple.h
+++ chrome/browser/ui/webui/new_tab_page/action_chips/remote_suggestions_service_simple.h
@@ -13,8 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_forward.h"
 #include "base/memory/raw_ptr.h"
 #include "base/types/expected.h"
+#include "base/types/optional_ref.h"
 #include "components/omnibox/browser/autocomplete_provider_client.h"
 #include "components/omnibox/browser/search_suggestion_parser.h"
+#include "third_party/omnibox_proto/aim_tools.pb.h"
+#include "third_party/omnibox_proto/page_vertical.pb.h"
 
 namespace action_chips {
 
@@ -30,11 +33,35 @@ class RemoteSuggestionsServiceSimple {
   using ActionChipSuggestionsResult =
       base::expected<SearchSuggestionParser::SuggestResults, Error>;
 
+  // Makes a call to a suggestions endpoint to retrieve suggestions used for
+  // deep dive chips.
+  // Args:
+  // - title: the title of a tab
+  // - url: the url of a tab
+  // - callback: the callback run when the remote call is complete.
   virtual std::unique_ptr<network::SimpleURLLoader>
   GetDeepdiveChipSuggestionsForTab(
       const std::u16string_view title,
       const GURL& url,
-      base::OnceCallback<void(ActionChipSuggestionsResult&&)>) = 0;
+      base::OnceCallback<void(ActionChipSuggestionsResult&&)> callback) = 0;
+
+  // Makes a call to a suggestions endpoint to retrieve suggestions used by the
+  // steady/deep-dive state.
+  // Args:
+  // - title: the title of a recent tab, if any.
+  // - url: the url of a recent tab, if any.
+  // - allowed_tools: a list of tool/model pairs that are allowed to be used.
+  // - page_vertical: the vertical information of the page to be passed to the
+  //   remote endpoint.
+  // - callback: the callback run when the remote call is complete.
+  virtual std::unique_ptr<network::SimpleURLLoader> GetActionChipSuggestions(
+      base::optional_ref<const std::u16string> title,
+      base::optional_ref<const GURL> url,
+      base::span<const omnibox::ToolMode> allowed_tools,
+      base::optional_ref<const omnibox::PageVertical> page_vertical,
+      base::OnceCallback<
+          void(RemoteSuggestionsServiceSimple::ActionChipSuggestionsResult&&)>
+          callback) = 0;
 
   struct NetworkError {
     int net_error = 0;
@@ -73,6 +100,15 @@ class RemoteSuggestionsServiceSimpleImpl
           void(RemoteSuggestionsServiceSimple::ActionChipSuggestionsResult&&)>
           callback) override;
 
+  std::unique_ptr<network::SimpleURLLoader> GetActionChipSuggestions(
+      base::optional_ref<const std::u16string> title,
+      base::optional_ref<const GURL> url,
+      base::span<const omnibox::ToolMode> allowed_tools,
+      base::optional_ref<const omnibox::PageVertical> page_vertical,
+      base::OnceCallback<
+          void(RemoteSuggestionsServiceSimple::ActionChipSuggestionsResult&&)>
+          callback) override;
+
  private:
   // Processes the response from the remote endpoint and run the callback with
   // the processing result.
@@ -82,6 +118,7 @@ class RemoteSuggestionsServiceSimpleImpl
       base::OnceCallback<
           void(RemoteSuggestionsServiceSimple::ActionChipSuggestionsResult&&)>
           callback,
+      const bool allow_empty_suggestion,
       const network::SimpleURLLoader* source,
       int response_code,
       std::optional<std::string> response_body);
