caee04cef4db5d1471876a1fe366b558b366645a
Wenyu Fu <wenyufu@chromium.org>
2026-01-14 16:09:16 -0800

    [Gardener] Disable testVoiceButtonDisabledOnIncognito
    Bug: 475928040
    Change-Id: I0b25db4442497496db6cdee63a1cfed2ed58d061
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7475555
    Reviewed-by: Joel Tan-Aristy <jtanaristy@google.com>
    Reviewed-by: Moe Adel <adelm@google.com>
    Reviewed-by: Gazal Agarwal <agazal@google.com>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Owners-Override: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Zhe Li <zheliooo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1569414}

--- chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/VoiceToolbarButtonControllerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/VoiceToolbarButtonControllerTest.java
@@ -38,6 +38,7 @@ import org.mockito.junit.MockitoRule;
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.Batch;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
@@ -149,6 +150,7 @@ public final class VoiceToolbarButtonControllerTest {
     @Test
     @MediumTest
     @Restriction(RESTRICTION_TYPE_NON_LOW_END_DEVICE)
+    @DisabledTest(message = "https://crbug.com/475928040")
     public void testVoiceButtonDisabledOnIncognito() {
         // Ensure the button starts visible.
         ViewUtils.waitForVisibleView(
