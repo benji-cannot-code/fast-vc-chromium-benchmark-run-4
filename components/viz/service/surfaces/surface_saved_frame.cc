fbc28b3a82cf321632e3bdae3ac61ac58d06e244
Colin Blundell <blundell@chromium.org>
2026-01-15 07:43:13 -0800

    [Viz] Rename BlitRequest::populates_gpu_memory_buffer
    This should now refer to the mappable SharedImage.
    
    Bug: 404905709
    Change-Id: I6e17aab1048a6073334603fc0e6b37f310dd6acd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7455766
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1569753}

--- components/viz/service/surfaces/surface_saved_frame.cc
+++ components/viz/service/surfaces/surface_saved_frame.cc
@@ -270,7 +270,7 @@ std::unique_ptr<CopyOutputRequest> SurfaceSavedFrame::CreateCopyRequestIfNeeded(
   request->set_blit_request(
       BlitRequest(gfx::Point(), LetterboxingBehavior::kDoNotLetterbox,
                   shared_image, shared_image->creation_sync_token(),
-                  /*populates_gpu_memory_buffer=*/false));
+                  /*populates_mappable_shared_image=*/false));
 
   return request;
 }
