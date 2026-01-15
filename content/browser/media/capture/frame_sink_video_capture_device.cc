536edef03325eac1af4a51d1b03f8ddc4f2aeb87
Colin Blundell <blundell@chromium.org>
2026-01-15 08:45:22 -0800

    [Viz] Rename kPreferGpuMemoryBuffer
    Bug: 404905709
    Change-Id: I2d6297ae75197e01e3676b9f16983cdd492931da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7461067
    Reviewed-by: Mingjing Zhang <mjzhang@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1569786}

--- content/browser/media/capture/frame_sink_video_capture_device.cc
+++ content/browser/media/capture/frame_sink_video_capture_device.cc
@@ -611,8 +611,8 @@ void FrameSinkVideoCaptureDevice::MaybeStartConsuming() {
     return;
   }
 
-  capturer_->Start(this,
-                   viz::mojom::BufferFormatPreference::kPreferGpuMemoryBuffer);
+  capturer_->Start(
+      this, viz::mojom::BufferFormatPreference::kPreferMappableSharedImage);
 }
 
 void FrameSinkVideoCaptureDevice::MaybeStopConsuming() {
