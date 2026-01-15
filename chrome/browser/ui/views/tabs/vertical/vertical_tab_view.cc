6e4e89cabd74772ef1013ea4e80c548defca89b1
Shibalik Mohapatra <shibalik@chromium.org>
2026-01-14 16:25:04 -0800

    [Vertical Tabstrip] Fix hover during unsplit operation.
    When tabs are unsplit the hovered_ needs to be reset to the correct
    value based on the mouse position.
    
    Additionally moved the logic of computing split and pinned to AddedToWidget since this needs to be recomputed on reparenting.
    
    Change-Id: I313db0195de881ec0fea5c55d4f07806e3bfe0c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7451100
    Commit-Queue: Shibalik Mohapatra <shibalik@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1569424}

--- chrome/browser/ui/views/tabs/vertical/vertical_tab_view.cc
+++ chrome/browser/ui/views/tabs/vertical/vertical_tab_view.cc
@@ -176,8 +176,6 @@ VerticalTabView::VerticalTabView(TabCollectionNode* collection_node)
       collection_node_->RegisterDataChangedCallback(base::BindRepeating(
           &VerticalTabView::OnDataChanged, base::Unretained(this)));
 
-  OnDataChanged();
-
   set_context_menu_controller(this);
 }
 
@@ -204,6 +202,7 @@ void VerticalTabView::UpdateHovered(bool hovered) {
       hover_controller_->Hide(TabStyle::HideHoverStyle::kGradual);
     }
   }
+
   UpdateColors();
   UpdateCloseButtonVisibility();
 }
@@ -337,6 +336,7 @@ void VerticalTabView::AddedToWidget() {
       GetWidget()->RegisterPaintAsActiveChangedCallback(base::BindRepeating(
           &VerticalTabView::UpdateColors, base::Unretained(this)));
 
+  OnDataChanged();
   // Recompute the hovered state as mouse events are not processed if a view
   // removed from the widget and added.
   if (!split_) {
@@ -346,6 +346,7 @@ void VerticalTabView::AddedToWidget() {
 
 void VerticalTabView::RemovedFromWidget() {
   paint_as_active_subscription_ = {};
+  UpdateHovered(false);
 }
 
 void VerticalTabView::OnBoundsChanged(const gfx::Rect& previous_bounds) {
@@ -492,7 +493,6 @@ void VerticalTabView::OnDataChanged() {
   selected_ = tab->IsSelected();
   split_ = tab->IsSplit();
   pinned_ = tab->IsPinned();
-
   tab_data_ = TabRendererData::FromTabInModel(tab_strip_model, index);
 
   icon_->SetData(tab_data_);
