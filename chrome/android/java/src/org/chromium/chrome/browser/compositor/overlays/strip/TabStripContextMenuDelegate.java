e1065bddad444de9fd6095ab6c73db8a97bb0f9e
Michael Wu <mklw@google.com>
2026-01-14 16:54:27 -0800

    Add "New tab" menu option to tab strip context menu
    Demo:
    https://screencast.googleplex.com/cast/NTE1MzQyNDg0NzkyOTM0NHxiYTYwZTQ0Mi0xNw
    This menu option is flag guarded behind the
    tab-strip-empty-space-context-menu flag, and still appears as "New tab"
    in Incognito mode.
    
    Bug: 436261525
    Change-Id: I0646859b0755d8632c1b0ab99df4bedaaa3d8e37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7453582
    Reviewed-by: Zhe Li <zheliooo@google.com>
    Commit-Queue: Michael Wu <mklw@google.com>
    Cr-Commit-Position: refs/heads/main@{#1569442}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/TabStripContextMenuDelegate.java
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.compositor.overlays.strip;
+
+import org.chromium.build.annotations.NullMarked;
+
+/** Delegate to handle actions triggered from the tab strip context menu. */
+@NullMarked
+public interface TabStripContextMenuDelegate {
+    /** Called when the "New tab" menu item is selected. */
+    void onNewTab();
+
+    /** Called when the "Name window" menu item is selected. */
+    void onNameWindow();
+}
