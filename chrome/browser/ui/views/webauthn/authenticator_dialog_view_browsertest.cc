260deeecdab7b33c569c4cd6e4779b7db28b05f3
Adem Derinel <derinel@google.com>
2026-01-14 23:15:44 -0800

    Remove dangling pointer in AuthenticatorRequestDialogView
    Change-Id: Icbff1fb90d8591b081da2fb3118b123dc99ebfad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7472015
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1569545}

--- chrome/browser/ui/views/webauthn/authenticator_dialog_view_browsertest.cc
+++ chrome/browser/ui/views/webauthn/authenticator_dialog_view_browsertest.cc
@@ -138,6 +138,14 @@ class AuthenticatorDialogViewTest : public DialogBrowserTest {
           view_controller_.get(),
           std::make_unique<TestSheetView>(std::make_unique<TestSheetModel>()));
       EXPECT_EQ(step_transition_observer.step_transition_count(), 0);
+    } else if (name == "ReplaceSheet") {
+      test::AuthenticatorRequestDialogViewTestApi::SetSheetTo(
+          view_controller_.get(),
+          std::make_unique<TestSheetView>(std::make_unique<TestSheetModel>()));
+      // Replace it immediately to trigger the dangling pointer scenario
+      test::AuthenticatorRequestDialogViewTestApi::SetSheetTo(
+          view_controller_.get(),
+          std::make_unique<TestSheetView>(std::make_unique<TestSheetModel>()));
     }
 
     dialog_model_->RemoveObserver(&step_transition_observer);
@@ -152,5 +160,9 @@ class AuthenticatorDialogViewTest : public DialogBrowserTest {
 
 // Test the dialog with a custom delegate.
 IN_PROC_BROWSER_TEST_F(AuthenticatorDialogViewTest, InvokeUi_default) {
+  ShowAndVerifyUi();
+}
+
+IN_PROC_BROWSER_TEST_F(AuthenticatorDialogViewTest, InvokeUi_ReplaceSheet) {
   ShowAndVerifyUi();
 }
