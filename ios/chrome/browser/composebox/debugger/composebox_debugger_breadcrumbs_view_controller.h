47bf795ec748b2a905d680a64df06a8e47fe7146
Radu Nitescu <radunitescu@google.com>
2026-01-15 04:00:23 -0800

    [iOS][AIM] Add UI for Composebox specific breadcrumbs debugging.
    This is only used for eng debugging and will not be user visible.
    
    https://screenshot.googleplex.com/ABdJaczbcojezSf.png
    
    Change-Id: I778d7f4ef250460f05df2f68d6e1415ce9f8e29e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7461036
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1569642}

---
+++ ios/chrome/browser/composebox/debugger/composebox_debugger_breadcrumbs_view_controller.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_COMPOSEBOX_DEBUGGER_COMPOSEBOX_DEBUGGER_BREADCRUMBS_VIEW_CONTROLLER_H_
+#define IOS_CHROME_BROWSER_COMPOSEBOX_DEBUGGER_COMPOSEBOX_DEBUGGER_BREADCRUMBS_VIEW_CONTROLLER_H_
+
+#import <UIKit/UIKit.h>
+
+#import "ios/chrome/browser/composebox/debugger/composebox_debugger_event.h"
+
+// The UI of the breadcrumbs debugging view.
+@interface ComposeboxDebuggerBreadcrumbsViewController : UITableViewController
+
+// Instantiates a new breadcrumbs interface with the given events to be
+// displayed.
+- (instancetype)initWithEvents:(NSArray<ComposeboxDebuggerEvent*>*)breadcrumbs;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_COMPOSEBOX_DEBUGGER_COMPOSEBOX_DEBUGGER_BREADCRUMBS_VIEW_CONTROLLER_H_
