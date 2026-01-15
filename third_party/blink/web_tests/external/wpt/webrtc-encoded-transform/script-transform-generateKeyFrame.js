15d23438d612938dea76af2eff226b4439c9ddca
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2026-01-15 10:41:45 -0800

    Import wpt@0eeead5cecacb0f4d0823d45822689f01319b437
    https://github.com/web-platform-tests/wpt/compare/605bc5400...0eeead5ce
    
    Using wpt-import in Chromium d26f4e4ce4f8acbf60f45974061ce7c72761a9a9.
    With Chromium commits locally applied on WPT:
      19265eda79 "PointerEvent WPT: Assert .fromElement/.toElement [...]
      346cc69770 "[blink][media] Clean up stale MailboxHolder refs"
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac-skia-alt-arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac12.0-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac12.0.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac13-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac13.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac14-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac14.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac15-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac15.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:win10.20h2-blink-rel
    Cq-Include-Trybots: luci.chromium.try:win11-arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:win11-blink-rel
    Change-Id: I3cd90c21dc0fdbefadc735b966129f3e70058eb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7486787
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1569880}

--- third_party/blink/web_tests/external/wpt/webrtc-encoded-transform/script-transform-generateKeyFrame.js
+++ third_party/blink/web_tests/external/wpt/webrtc-encoded-transform/script-transform-generateKeyFrame.js
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 onrtctransform = event => {
   const transformer = event.transformer;
   let keyFrameCount = 0;
-  let gotFrame;
+  let gotFrameCallback;
 
   transformer.options.port.onmessage = event => {
     const {method, rid} = event.data;
--- third_party/blink/web_tests/external/wpt/webrtc-encoded-transform/script-transform-sendKeyFrameRequest.js
+++ third_party/blink/web_tests/external/wpt/webrtc-encoded-transform/script-transform-sendKeyFrameRequest.js
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 onrtctransform = event => {
   const transformer = event.transformer;
-  let gotFrame;
+  let gotFrameCallback;
 
   transformer.options.port.onmessage = event => {
     const {method} = event.data;
