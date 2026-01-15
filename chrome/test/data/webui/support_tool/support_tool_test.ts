01e0942f4a10898ff3d339608152755cb599516b
rbpotter <rbpotter@chromium.org>
2026-01-15 14:32:31 -0800

    Support Tool: Migrate url-generator to Lit
    Bug: 40943652
    Change-Id: I7fada58e7c04cfd965709830294009c2a5a1da09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7455362
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Irem Uguz <iremuguz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1570024}

--- chrome/test/data/webui/support_tool/support_tool_test.ts
+++ chrome/test/data/webui/support_tool/support_tool_test.ts
@@ -11,9 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://support-tool/support_tool.js';
 import 'chrome://support-tool/url_generator.js';
 
-import type {CrButtonElement} from 'chrome://resources/cr_elements/cr_button/cr_button.js';
 import type {CrCheckboxElement} from 'chrome://resources/cr_elements/cr_checkbox/cr_checkbox.js';
-import type {CrInputElement} from 'chrome://resources/cr_elements/cr_input/cr_input.js';
 import type {CrToastElement} from 'chrome://resources/cr_elements/cr_toast/cr_toast.js';
 import {webUIListenerCallback} from 'chrome://resources/js/cr.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
@@ -113,12 +111,16 @@ class TestSupportToolBrowserProxy extends TestBrowserProxy implements
 
   getDataCollectors() {
     this.methodCalled('getDataCollectors');
-    return Promise.resolve(DATA_COLLECTORS);
+    // Using structuredClone since the UI modifies this data in place, and the
+    // data should reset between tests.
+    return Promise.resolve(structuredClone(DATA_COLLECTORS));
   }
 
   getAllDataCollectors() {
     this.methodCalled('getAllDataCollectors');
-    return Promise.resolve(ALL_DATA_COLLECTORS);
+    // Using structuredClone since the UI modifies this data in place, and the
+    // data should reset between tests.
+    return Promise.resolve(structuredClone(ALL_DATA_COLLECTORS));
   }
 
   startDataCollection(
@@ -346,20 +348,17 @@ suite('UrlGeneratorTest', function() {
 
   test('url generation success', async () => {
     // Ensure the button is disabled when we open the page.
-    const copyLinkButton = urlGenerator.shadowRoot!.getElementById(
-                               'copyURLButton')! as CrButtonElement;
-    assertTrue(copyLinkButton.disabled);
-    const caseIdInput = urlGenerator.shadowRoot!.getElementById(
-                            'caseIdInput')! as CrInputElement;
-    caseIdInput.value = 'test123';
+    const copyLinkButton = urlGenerator.$.copyURLButton;
+    assertTrue(copyLinkButton.disabled, 'link button is disabled');
+    urlGenerator.$.caseIdInput.value = 'test123';
     const dataCollectors =
-        urlGenerator.shadowRoot!.querySelectorAll('cr-checkbox');
+        urlGenerator.shadowRoot.querySelectorAll('cr-checkbox');
     // Select one of data collectors to enable the button.
     const firstDataCollector = dataCollectors[0]!;
     firstDataCollector.click();
-    await firstDataCollector.updateComplete;
+    await microtasksFinished();
     // Ensure the button is enabled after we select at least one data collector.
-    assertFalse(copyLinkButton.disabled);
+    assertFalse(copyLinkButton.disabled, 'link button is now not disabled');
     const expectedToken = 'chrome://support-tool/?case_id=test123&module=jekhh';
     // Set the expected result of URL generation to successful.
     const expectedResult: SupportTokenGenerationResult = {
@@ -386,8 +385,7 @@ suite('UrlGeneratorTest', function() {
       errorMessage: 'Test error message',
     };
     browserProxy.setSupportTokenGenerationResult(expectedResult);
-    const copyLinkButton = urlGenerator.shadowRoot!.getElementById(
-                               'copyURLButton')! as CrButtonElement;
+    const copyLinkButton = urlGenerator.$.copyURLButton;
     // Enable the button for testing. The input fields are not important as
     // we're testing for the error message.
     copyLinkButton.disabled = false;
@@ -400,15 +398,14 @@ suite('UrlGeneratorTest', function() {
 
   test('token generation success', async () => {
     // Ensure the button is disabled when we open the page.
-    const copyTokenButton = urlGenerator.shadowRoot!.getElementById(
-                                'copyTokenButton')! as CrButtonElement;
+    const copyTokenButton = urlGenerator.$.copyTokenButton;
     assertTrue(copyTokenButton.disabled);
     const dataCollectors =
-        urlGenerator.shadowRoot!.querySelectorAll('cr-checkbox');
+        urlGenerator.shadowRoot.querySelectorAll('cr-checkbox');
     // Select one of data collectors to enable the button.
     const firstDataCollector = dataCollectors[0]!;
     firstDataCollector.click();
-    await firstDataCollector.updateComplete;
+    await microtasksFinished();
     // Ensure the button is enabled after we select at least one data collector.
     assertFalse(copyTokenButton.disabled);
     const expectedToken = 'jekhh';
