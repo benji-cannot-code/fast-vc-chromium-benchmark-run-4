e020ebdbcc77e962c64dd118ac9097d3f0507680
Andy Paicu <andypaicu@chromium.org>
2026-01-15 03:47:51 -0800

    [PEPC] Create a clusterfuzz test to help catch style bypasses of
    PEPC validation rules.
    
    The test fuzzes the styles of a geolocation element and its parent and
    then checks if the element's text remains within bounds. This approach
    is based on an analysis of previously reported styling abuse bugs out of
    which about 40% result in the text being displaced.
    
    The test is added in a new folder to allow for DIR_METADATA to help
    Clustefuzz report any bugs to the correct component.
    
    Bug: 470074511
    Change-Id: Ia74b4534447f72333faf0820bb6050407d3ae6c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7277444
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Reviewed-by: Patrick Meenan <pmeenan@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1569640}

--- third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
+++ third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
@@ -2913,6 +2913,13 @@ _CONFIG = [
             'base::raw_span',
         ],
     },
+    {
+        'paths': [
+            'third_party/blink/renderer/core/html/html_permission_element_test_helper.cc'
+        ],
+        'allowed':
+        ['base::test::RunUntil', 'base::test::ScopedRunLoopTimeout'],
+    }
 ]
 
 
