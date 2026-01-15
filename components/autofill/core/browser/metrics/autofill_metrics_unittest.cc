be54a47b05827257547b0ce2c1e3998e5158d031
Jihad Hanna <jihadghanna@google.com>
2026-01-15 05:04:53 -0800

    Move metric logging from ProcessServerPredictions to AutofillManager
    The overall goal is to make ProcessServerPredictionsQueryResponse only
    depend on FormData and not FormStructure. Therefore, metrics that
    depended on field types had to move out.
    
    Bug: 475586865
    Change-Id: I7e226b379e1441c22d887163c71538d54c08767c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7461530
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1569664}

--- components/autofill/core/browser/metrics/autofill_metrics_unittest.cc
+++ components/autofill/core/browser/metrics/autofill_metrics_unittest.cc
@@ -2276,9 +2276,10 @@ TEST_F(AutofillMetricsTest, FormFillDurationFromInteraction_MultipleForms) {
 }
 
 // Test class that shares setup code for testing ParseQueryResponse.
-class AutofillMetricsParseQueryResponseTest : public testing::Test {
+class AutofillMetricsParseQueryResponseTest : public AutofillMetricsTest {
  public:
   void SetUp() override {
+    AutofillMetricsTest::SetUp();
     FormData form;
     form.set_host_frame(test::MakeLocalFrameToken());
     form.set_renderer_id(test::MakeFormRendererId());
@@ -2304,9 +2305,11 @@ class AutofillMetricsParseQueryResponseTest : public testing::Test {
         FormFieldData::CheckStatus::kCheckableButUnchecked);
     test_api(form).Append(checkable_field);
 
+    SeeForm(form);
     owned_forms_.push_back(std::make_unique<FormStructure>(form));
     forms_.emplace_back(*owned_forms_.back());
 
+    form.set_renderer_id(test::MakeFormRendererId());
     field.set_label(u"email");
     field.set_name(u"email");
     test_api(form).Append(field);
@@ -2316,12 +2319,12 @@ class AutofillMetricsParseQueryResponseTest : public testing::Test {
     field.set_form_control_type(FormControlType::kInputPassword);
     test_api(form).Append(field);
 
+    SeeForm(form);
     owned_forms_.push_back(std::make_unique<FormStructure>(form));
     forms_.emplace_back(*owned_forms_.back());
   }
 
  protected:
-  test::AutofillUnitTestEnvironment autofill_test_environment_;
   std::vector<std::unique_ptr<FormStructure>> owned_forms_;
   std::vector<raw_ref<FormStructure>> forms_;
 };
@@ -2340,9 +2343,9 @@ TEST_F(AutofillMetricsParseQueryResponseTest, ServerHasData) {
 
   std::string response_string = SerializeAndEncode(response);
   base::HistogramTester histogram_tester;
-  ParseServerPredictionsQueryResponse(response_string, forms_,
-                                      test::GetEncodedSignatures(forms_),
-                                      nullptr, /*ignore_small_forms=*/true);
+  test_api(autofill_manager())
+      .OnLoadedServerPredictions(response_string,
+                                 test::GetEncodedSignatures(forms_));
   EXPECT_THAT(
       histogram_tester.GetAllSamples("Autofill.ServerResponseHasDataForForm"),
       ElementsAre(Bucket(true, 2)));
@@ -2364,9 +2367,9 @@ TEST_F(AutofillMetricsParseQueryResponseTest, OneFormNoServerData) {
                            form_suggestion);
   std::string response_string = SerializeAndEncode(response);
   base::HistogramTester histogram_tester;
-  ParseServerPredictionsQueryResponse(response_string, forms_,
-                                      test::GetEncodedSignatures(forms_),
-                                      nullptr, /*ignore_small_forms=*/true);
+  test_api(autofill_manager())
+      .OnLoadedServerPredictions(response_string,
+                                 test::GetEncodedSignatures(forms_));
   EXPECT_THAT(
       histogram_tester.GetAllSamples("Autofill.ServerResponseHasDataForForm"),
       ElementsAre(Bucket(false, 1), Bucket(true, 1)));
@@ -2386,9 +2389,9 @@ TEST_F(AutofillMetricsParseQueryResponseTest, AllFormsNoServerData) {
 
   std::string response_string = SerializeAndEncode(response);
   base::HistogramTester histogram_tester;
-  ParseServerPredictionsQueryResponse(response_string, forms_,
-                                      test::GetEncodedSignatures(forms_),
-                                      nullptr, /*ignore_small_forms=*/true);
+  test_api(autofill_manager())
+      .OnLoadedServerPredictions(response_string,
+                                 test::GetEncodedSignatures(forms_));
   EXPECT_THAT(
       histogram_tester.GetAllSamples("Autofill.ServerResponseHasDataForForm"),
       ElementsAre(Bucket(false, 2)));
@@ -2411,9 +2414,9 @@ TEST_F(AutofillMetricsParseQueryResponseTest, PartialNoServerData) {
 
   std::string response_string = SerializeAndEncode(response);
   base::HistogramTester histogram_tester;
-  ParseServerPredictionsQueryResponse(response_string, forms_,
-                                      test::GetEncodedSignatures(forms_),
-                                      nullptr, /*ignore_small_forms=*/true);
+  test_api(autofill_manager())
+      .OnLoadedServerPredictions(response_string,
+                                 test::GetEncodedSignatures(forms_));
   EXPECT_THAT(
       histogram_tester.GetAllSamples("Autofill.ServerResponseHasDataForForm"),
       ElementsAre(Bucket(true, 2)));
