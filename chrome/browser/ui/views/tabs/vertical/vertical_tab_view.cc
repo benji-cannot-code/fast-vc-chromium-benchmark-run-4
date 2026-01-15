5ed6a970828f47fce0870a0ece2215c0675b5b80
Alison Gale <agale@chromium.org>
2026-01-14 20:41:22 -0800

    [Vertical Tabs] Fix tab view corner rendering
    The border is rendered half inside and half outside the bounds so use a
    rounded rect when painting the tabs with the correct corner radius
    
    Bug: 475597197
    Change-Id: Ie02991db40408af0409bb259994d8b48d22ac161
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7467499
    Commit-Queue: Alison Gale <agale@chromium.org>
    Reviewed-by: Charles Meng <charlesmeng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1569495}

--- chrome/browser/ui/views/tabs/vertical/vertical_tab_view.cc
+++ chrome/browser/ui/views/tabs/vertical/vertical_tab_view.cc
@@ -325,7 +325,10 @@ void VerticalTabView::OnPaint(gfx::Canvas* canvas) {
     flags.setColor(tab_style_->GetCurrentTabBackgroundColor(
         GetSelectionState(), IsHoverAnimationActive(), GetHoverAnimationValue(),
         IsFrameActive(), GetColorProvider()));
-    canvas->DrawRect(GetContentsBounds(), flags);
+    const float corner_radius =
+        GetLayoutConstant(LayoutConstant::kVerticalTabCornerRadius) -
+        (split_ ? GetInsets().top() / 2.0 : 0.0);
+    canvas->DrawRoundRect(GetContentsBounds(), corner_radius, flags);
   }
 
   views::View::OnPaint(canvas);
@@ -347,10 +350,6 @@ void VerticalTabView::AddedToWidget() {
 void VerticalTabView::RemovedFromWidget() {
   paint_as_active_subscription_ = {};
   UpdateHovered(false);
-}
-
-void VerticalTabView::OnBoundsChanged(const gfx::Rect& previous_bounds) {
-  SetClipPath(GetPath());
 }
 
 void VerticalTabView::OnThemeChanged() {
@@ -621,8 +620,7 @@ float VerticalTabView::GetHoverOpacity() const {
 
 SkPath VerticalTabView::GetPath() const {
   const SkScalar corner_radius = SkIntToScalar(
-      GetLayoutConstant(LayoutConstant::kVerticalTabCornerRadius) +
-      (split_ ? GetInsets().height() : 0));
+      GetLayoutConstant(LayoutConstant::kVerticalTabCornerRadius));
   return SkPath::RRect(SkRRect::MakeRectXY(gfx::RectToSkRect(GetLocalBounds()),
                                            corner_radius, corner_radius));
 }
