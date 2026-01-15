4fd5435db967e1db2c4096ca3c76173ce2da8b87
Solomon Kinard <solomonkinard@chromium.org>
2026-01-15 11:22:29 -0800

    CodeHealth: Grammar fixes in comments
    This CL was uploaded by git cl split.
    
    R=rdevlin.cronin@chromium.org
    
    Bug: 346952289
    Change-Id: I880064b5f6940f333eef1ab6ce003fe2d073b66c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7479515
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Auto-Submit: Solomon Kinard <solomonkinard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1569908}

--- chrome/browser/extensions/external_provider_impl_unittest.cc
+++ chrome/browser/extensions/external_provider_impl_unittest.cc
@@ -45,7 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "net/test/embedded_test_server/http_request.h"
 #include "net/test/embedded_test_server/http_response.h"
-#include "testing/gmock/include/gmock/gmock.h"
 
 #if BUILDFLAG(IS_CHROMEOS)
 #include "chrome/browser/ash/customization/customization_document.h"
