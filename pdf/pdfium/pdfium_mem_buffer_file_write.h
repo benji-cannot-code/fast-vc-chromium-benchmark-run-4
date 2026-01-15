492491da4c9ae53be608043b34acb0dc17b77ac2
Sergio Solano <sergiosolano@google.com>
2026-01-14 20:24:42 -0800

    spanification: automatically spanify pdf/pdfium/pdfium_mem_buffer_file_write.cc etc.
    This is the result of running the automatic spanification on linux and
    updating code to use and pass spans where size is known.
    
    The original patch was fully automated using script:
    //tools/clang/spanify/rewrite-multiple-platforms.sh -platforms=linux
    Then refined with gemini-cli
    
    gemini-run/batch-run-1761798078/group_79
    
    BUG=439964610
    
    Change-Id: I8ece2f96387c316efa795061771d13707bebf7c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7466675
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Sergio Solano <sergiosolano@google.com>
    Cr-Commit-Position: refs/heads/main@{#1569490}

--- pdf/pdfium/pdfium_mem_buffer_file_write.h
+++ pdf/pdfium/pdfium_mem_buffer_file_write.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "base/containers/span.h"
 #include "third_party/pdfium/public/fpdf_save.h"
 
 namespace chrome_pdf {
@@ -31,7 +32,7 @@ class PDFiumMemBufferFileWrite : public FPDF_FILEWRITE {
                             const void* data,
                             unsigned long size);
 
-  int DoWriteBlock(const uint8_t* data, unsigned long size);
+  int DoWriteBlock(base::span<const uint8_t> data);
 
   std::vector<uint8_t> buffer_;
 };
