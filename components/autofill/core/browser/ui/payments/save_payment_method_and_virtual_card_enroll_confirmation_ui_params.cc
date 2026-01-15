31fa1806a16075670ca1e4f9bdd24fb8d4f812bf
Fernando Ramirez <ferny@google.com>
2026-01-14 16:21:39 -0800

    [AF][Clank] Replace GPay with Wallet branding for card upstream failure
    Before: https://screenshot.googleplex.com/39baLPCobaggFV7.png
    After: https://screenshot.googleplex.com/qDL5TSgmsoEE7ZG.png
    Mock: https://screenshot.googleplex.com/6nvRpHx3Ez9Jg5C.png
    
    Bug: 468139053
    Change-Id: I38f6ac144e12f93722893df30373fbfb07fda960
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7465244
    Reviewed-by: Olivia Saul <osaul@google.com>
    Commit-Queue: Fernando Ramirez <ferny@google.com>
    Cr-Commit-Position: refs/heads/main@{#1569422}

--- components/autofill/core/browser/ui/payments/save_payment_method_and_virtual_card_enroll_confirmation_ui_params.cc
+++ components/autofill/core/browser/ui/payments/save_payment_method_and_virtual_card_enroll_confirmation_ui_params.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/autofill/core/browser/ui/payments/save_payment_method_and_virtual_card_enroll_confirmation_ui_params.h"
 
+#include "base/feature_list.h"
+#include "components/autofill/core/common/autofill_payments_features.h"
 #include "components/strings/grit/components_strings.h"
 #include "ui/base/l10n/l10n_util.h"
 
@@ -86,7 +88,9 @@ SavePaymentMethodAndVirtualCardEnrollConfirmationUiParams::
           IDS_AUTOFILL_SAVE_CARD_CONFIRMATION_FAILURE_TITLE_TEXT),
       /*description_text=*/
       l10n_util::GetStringUTF16(
-          IDS_AUTOFILL_SAVE_CARD_CONFIRMATION_FAILURE_DESCRIPTION_TEXT),
+          base::FeatureList::IsEnabled(features::kAutofillEnableWalletBranding)
+              ? IDS_AUTOFILL_SAVE_CARD_TO_WALLET_CONFIRMATION_FAILURE_DESCRIPTION_TEXT
+              : IDS_AUTOFILL_SAVE_CARD_CONFIRMATION_FAILURE_DESCRIPTION_TEXT),
       /*failure_ok_button_accessible_name=*/
       l10n_util::GetStringUTF16(
           IDS_AUTOFILL_SAVE_CARD_CONFIRMATION_FAILURE_OK_BUTTON_ACCESSIBLE_NAME));
