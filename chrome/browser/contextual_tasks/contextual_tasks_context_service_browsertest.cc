3dbea73bf79447c39caa4c7a0e54d3b78e87381a
Abhay Prakash <abhayprakash@google.com>
2026-01-14 20:50:44 -0800

    Add a Tab Selection mode to score based on static signals only.
    - However, logging will happen for all signals.
    
    Change-Id: If610c5a0bff2a80d1d39667a50d92cc755bee90d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7456507
    Commit-Queue: Abhay Prakash <abhayprakash@google.com>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1569499}

--- chrome/browser/contextual_tasks/contextual_tasks_context_service_browsertest.cc
+++ chrome/browser/contextual_tasks/contextual_tasks_context_service_browsertest.cc
@@ -422,8 +422,6 @@ IN_PROC_BROWSER_TEST_F(ContextualTasksContextServiceTest, Success) {
       "ContextualTasks.Context.TabOverlapPercentage", 100, 1);
   histogram_tester.ExpectUniqueSample("ContextualTasks.Context.TabExcessCount",
                                       0, 1);
-
-  EXPECT_TRUE(logs_uploader()->uploaded_logs().empty());
 }
 
 IN_PROC_BROWSER_TEST_F(ContextualTasksContextServiceTest,
@@ -527,8 +525,18 @@ IN_PROC_BROWSER_TEST_F(ContextualTasksContextServiceTest,
   EXPECT_EQ(1u, future.Get().size());
 }
 
-IN_PROC_BROWSER_TEST_F(ContextualTasksContextServiceTest,
-                       MultiSignalScoringMetrics) {
+struct ContextualTasksTestParams {
+  mojom::TabSelectionMode mode;
+  int expected_tab_score_bucket;
+};
+
+class ContextualTasksContextServiceParameterizedTest
+    : public ContextualTasksContextServiceTest,
+      public testing::WithParamInterface<ContextualTasksTestParams> {};
+
+IN_PROC_BROWSER_TEST_P(ContextualTasksContextServiceParameterizedTest,
+                       LogSignalsAndMetricsInAllTabSelectionModes) {
+  const ContextualTasksTestParams& params = GetParam();
   base::HistogramTester histogram_tester;
 
   test_clock_.SetNowTicks(base::TimeTicks::Now());
@@ -557,8 +565,7 @@ IN_PROC_BROWSER_TEST_F(ContextualTasksContextServiceTest,
 
   base::test::TestFuture<std::vector<content::WebContents*>> future;
   service()->GetRelevantTabsForQuery(
-      {.tab_selection_mode = mojom::TabSelectionMode::kMultiSignalScoring},
-      "summarize the test page",
+      {.tab_selection_mode = params.mode}, "summarize the test page",
       /*explicit_urls=*/{GURL("https://notinrelevantset.com")},
       future.GetCallback());
   EXPECT_EQ(1u, future.Get().size());
@@ -574,8 +581,10 @@ IN_PROC_BROWSER_TEST_F(ContextualTasksContextServiceTest,
       "ContextualTasks.Context.EmbeddingSimilarityScore", 99, 1);
   histogram_tester.ExpectTotalCount(
       "ContextualTasks.Context.DurationSinceLastActive", 1);
-  histogram_tester.ExpectUniqueSample("ContextualTasks.Context.TabScore", 100,
-                                      1);
+  histogram_tester.ExpectUniqueSample(
+      "ContextualTasks.Context.MatchingWordsCount", 2, 1);
+  histogram_tester.ExpectUniqueSample("ContextualTasks.Context.TabScore",
+                                      params.expected_tab_score_bucket, 1);
   histogram_tester.ExpectUniqueSample(
       "ContextualTasks.Context.ExplicitTabsCount", 1, 1);
   histogram_tester.ExpectUniqueSample("ContextualTasks.Context.TabOverlapCount",
@@ -602,13 +611,24 @@ IN_PROC_BROWSER_TEST_F(ContextualTasksContextServiceTest,
   EXPECT_EQ(uploaded_quality_log.eligible_tabs()[0].number_of_common_words(),
             2);
   EXPECT_NEAR(uploaded_quality_log.eligible_tabs()[0].aggregate_tab_score(),
-                  1.0f, 0.001f);
+              1.0f, 0.001f);
   EXPECT_EQ(uploaded_quality_log.eligible_tabs()[0].was_explicitly_chosen(),
             false);
 }
 
+INSTANTIATE_TEST_SUITE_P(
+    All,
+    ContextualTasksContextServiceParameterizedTest,
+    testing::Values(
+        ContextualTasksTestParams{mojom::TabSelectionMode::kEmbeddingsMatch,
+                                  99},
+        ContextualTasksTestParams{mojom::TabSelectionMode::kStaticSignalsOnly,
+                                  99},
+        ContextualTasksTestParams{mojom::TabSelectionMode::kMultiSignalScoring,
+                                  100}));
+
 IN_PROC_BROWSER_TEST_F(ContextualTasksContextServiceTest,
-                       HighEmbeddingsScoreQualifiesTab) {
+                       MultiSignalScoring_HighEmbeddingsScoreQualifiesTab) {
   base::HistogramTester histogram_tester;
 
   test_clock_.SetNowTicks(base::TimeTicks::Now());
@@ -654,7 +674,7 @@ IN_PROC_BROWSER_TEST_F(ContextualTasksContextServiceTest,
 }
 
 IN_PROC_BROWSER_TEST_F(ContextualTasksContextServiceTest,
-                       HighRecencyLongDurationQualifiesTab) {
+                       MultiSignalScoring_HighRecencyLongDurationQualifiesTab) {
   base::HistogramTester histogram_tester;
 
   test_clock_.SetNowTicks(base::TimeTicks::Now());
@@ -688,8 +708,9 @@ IN_PROC_BROWSER_TEST_F(ContextualTasksContextServiceTest,
       "ContextualTasks.Context.RelevantTabsCount", 1, 1);
 }
 
-IN_PROC_BROWSER_TEST_F(ContextualTasksContextServiceTest,
-                       HighRecencyShortDurationDoesNotQualifyTab) {
+IN_PROC_BROWSER_TEST_F(
+    ContextualTasksContextServiceTest,
+    MultiSignalScoring_HighRecencyShortDurationDoesNotQualifyTab) {
   base::HistogramTester histogram_tester;
 
   test_clock_.SetNowTicks(base::TimeTicks::Now());
@@ -724,7 +745,7 @@ IN_PROC_BROWSER_TEST_F(ContextualTasksContextServiceTest,
 }
 
 IN_PROC_BROWSER_TEST_F(ContextualTasksContextServiceTest,
-                       HighLexicalMatchScoreQualifiesTab) {
+                       MultiSignalScoring_HighLexicalMatchScoreQualifiesTab) {
   base::HistogramTester histogram_tester;
 
   test_clock_.SetNowTicks(base::TimeTicks::Now());
@@ -752,7 +773,8 @@ IN_PROC_BROWSER_TEST_F(ContextualTasksContextServiceTest,
       "ContextualTasks.Context.RelevantTabsCount", 1, 1);
 }
 
-IN_PROC_BROWSER_TEST_F(ContextualTasksContextServiceTest, NotRelevantTab) {
+IN_PROC_BROWSER_TEST_F(ContextualTasksContextServiceTest,
+                       MultiSignalScoring_NotRelevantTab) {
   base::HistogramTester histogram_tester;
 
   test_clock_.SetNowTicks(base::TimeTicks::Now());
