30df824c512440b0a348b96671c36bc640090565
Masa Fujita <massan@google.com>
2026-01-14 17:43:45 -0800

    actions revamp: Resolve RootUiCoordinator's lifetime dependency
    `ExtensionsToolbarBridge` will depend on `TabModels` because the native
    side implementation of the bridge observes the tab models. This means
    that `RootUiCoordinator` now depends on `TabModels` and therefore must
    be destroyed beforehand.
    
    It used to be that `RootUiCoordinator` was destroyed in
    `ChromeActivity#onDestroy()`'s `super.onDestroy()` via
    `LifetimeAssert`'s cleanup, but with this CL we now manually destroy it
    before `destroyTabModels().
    
    Bug: 473925118
    Change-Id: I13428fcc11cf5489a88785839a2b8d9e35c4f3f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7460263
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Masa Fujita <massan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1569466}

--- chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
@@ -1845,6 +1845,11 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
             mStartupMetricsTracker = null;
         }
 
+        if (mRootUiCoordinator != null) {
+            mRootUiCoordinator.onDestroy();
+            mRootUiCoordinator = null;
+        }
+
         destroyTabModels();
 
         mBookmarkModelSupplier.set(null);
