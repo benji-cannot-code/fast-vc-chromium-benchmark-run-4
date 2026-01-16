460a84096150f908b8fac3e2177556c1c015f9bb
Joanne de Abreu <jdeabreu@google.com>
2026-01-16 08:26:17 -0800

    Webview web performance apis: add null check
    This cl checks that the Java navigationclient is alive before trying to
    report web performance metrics.
    
    Bug: b:432696062, b:466054580
    Change-Id: I4c49d4e62f7986b7a537dd13c570e53f2651e130
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7486770
    Commit-Queue: Joanne de Abreu <jdeabreu@google.com>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1570404}

--- android_webview/browser/aw_navigation_client.cc
+++ android_webview/browser/aw_navigation_client.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using base::android::AttachCurrentThread;
 using base::android::ConvertUTF8ToJavaString;
+using base::android::ScopedJavaLocalRef;
 
 namespace android_webview {
 
@@ -26,24 +27,39 @@ void AwNavigationClient::OnFirstContentfulPaint(
     content::Page& page,
     const base::TimeDelta& duration) {
   JNIEnv* env = AttachCurrentThread();
-  Java_AwNavigationClient_onFirstContentfulPaint(
-      env, java_ref_.get(env), page.GetJavaPage(), duration.InMilliseconds());
+  ScopedJavaLocalRef<jobject> obj = java_ref_.get(env);
+  if (!obj) {
+    return;
+  }
+
+  Java_AwNavigationClient_onFirstContentfulPaint(env, obj, page.GetJavaPage(),
+                                                 duration.InMilliseconds());
 }
 
 void AwNavigationClient::OnLargestContentfulPaint(
     content::Page& page,
     const base::TimeDelta& duration) {
   JNIEnv* env = AttachCurrentThread();
-  Java_AwNavigationClient_onLargestContentfulPaint(
-      env, java_ref_.get(env), page.GetJavaPage(), duration.InMilliseconds());
+  ScopedJavaLocalRef<jobject> obj = java_ref_.get(env);
+  if (!obj) {
+    return;
+  }
+
+  Java_AwNavigationClient_onLargestContentfulPaint(env, obj, page.GetJavaPage(),
+                                                   duration.InMilliseconds());
 }
 
 void AwNavigationClient::OnPerformanceMark(content::Page& page,
                                            std::string mark_name,
                                            const base::TimeDelta& mark_time) {
   JNIEnv* env = AttachCurrentThread();
+  ScopedJavaLocalRef<jobject> obj = java_ref_.get(env);
+  if (!obj) {
+    return;
+  }
+
   Java_AwNavigationClient_onPerformanceMark(
-      env, java_ref_.get(env), page.GetJavaPage(),
-      ConvertUTF8ToJavaString(env, mark_name), mark_time.InMilliseconds());
+      env, obj, page.GetJavaPage(), ConvertUTF8ToJavaString(env, mark_name),
+      mark_time.InMilliseconds());
 }
 }  // namespace android_webview
