271c62a3616e4ec1a48949a17e8f37930b4a27d7
Fergal Daly <fergal@google.com>
2026-01-15 23:15:23 -0800

    Add CHECK in `StartLoadingInternal` to confirm nullptr usage.
    Bug: 471268403
    Change-Id: I95d1e6a690ad8b64277daff4f9519535698575d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7400925
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1570204}

--- third_party/blink/renderer/core/loader/document_loader.cc
+++ third_party/blink/renderer/core/loader/document_loader.cc
@@ -2054,6 +2054,8 @@ void DocumentLoader::StartLoadingInternal() {
 
   InitializePrefetchedSignedExchangeManager();
 
+  // https://crbug.com/471268403 implies that this is sometimes null.
+  CHECK(body_loader_);
   body_loader_->SetDefersLoading(freeze_mode_);
 }
 
