676f6117395e5fe97384f22622be91f30ce1232c
Chris Lu <thegreenfrog@google.com>
2026-01-16 13:52:32 -0800

    [ios] Add DumpWithoutCrashing to suspect Drag and Drop bad states.
    Main suspicion is that the dragItem is invalid. Offer three possible
    points of failure to measure. If confirmed, replace this with
    a no-op and remove other non-hitting cases.
    
    Bug: 474623344
    Change-Id: I7c49949b5c48bff9058da00fe785ffce7b4f226f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7487282
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1570627}

--- ios/chrome/browser/shared/model/web_state_list/browser_util.mm
+++ ios/chrome/browser/shared/model/web_state_list/browser_util.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/check.h"
 #import "base/check_op.h"
+#import "base/debug/dump_without_crashing.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
 #import "ios/chrome/browser/shared/model/browser/browser_list.h"
 #import "ios/chrome/browser/shared/model/browser/browser_list_factory.h"
@@ -92,6 +93,7 @@ void MoveTabToBrowser(web::WebStateID tab_id,
   BrowserAndIndex tab_info = FindBrowserAndIndex(tab_id, browsers);
 
   if (!tab_info.browser) {
+    base::debug::DumpWithoutCrashing();
     return;
   }
   MoveTabFromBrowserToBrowser(tab_info.browser, tab_info.tab_index,
