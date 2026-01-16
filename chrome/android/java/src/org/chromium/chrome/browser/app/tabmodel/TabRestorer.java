aea96af5ff955e776b9e16bcd3d669cd66b048e1
Fiaz Muhammad <mfiaz@google.com>
2026-01-16 10:50:55 -0800

    [Persist] Destroy WebContentsState when a tab has not been restored
    - There was never any clean-up for WebContentsState for filtered tabs
            in TabRestorer.
    - The increased filtering has led to an increase in crashiness
    	likely caused by the WebContentsStates associated with
    	filtered tabs not being destroyed prior to being GC-ed.
    
    Bug: 476410296
    Change-Id: I6c1dfa812e7306c5e2b19863d9932874594b84a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7489831
    Auto-Submit: Fiaz Muhammad <mfiaz@google.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1570509}

--- chrome/android/java/src/org/chromium/chrome/browser/app/tabmodel/TabRestorer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/tabmodel/TabRestorer.java
@@ -21,6 +21,7 @@ import org.chromium.chrome.browser.tab.StorageLoadedData.LoadedTabState;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabId;
 import org.chromium.chrome.browser.tab.TabState;
+import org.chromium.chrome.browser.tab.WebContentsState;
 import org.chromium.chrome.browser.tabmodel.TabCreator;
 import org.chromium.chrome.browser.tabmodel.TabGroupVisualDataStore;
 
@@ -348,7 +349,11 @@ class TabRestorer {
         assert mState == State.RESTORING;
         @TabId int tabId = loadedTabState.tabId;
         Tab tab = resolveTab(loadedTabState.tabState, tabId, index);
-        if (tab == null) return;
+        if (tab == null) {
+            WebContentsState state = loadedTabState.tabState.contentsState;
+            if (state != null) state.destroy();
+            return;
+        }
 
         boolean isIncognito = mIncognito;
         mDelegate.onDetailsRead(
