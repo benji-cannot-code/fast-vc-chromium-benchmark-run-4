b5862f9cb6f22b5683a11317a5dd721d6b8d0cb2
Dave Tapuska <dtapuska@chromium.org>
2026-01-16 12:33:23 -0800

    Invoke StringImpl dtor
    http://crrev.com/e405c8406281cdd63a5121afc796665951948bbf used a
    delete operator for StringImpl and this failed to actually execute
    the StringImpl dtor. Fortunately in non-debug builds the dtor is a no-op.
    
    Remove const_casts which shouldn't have been there.
    
    Change-Id: I88f1a2c7cd14194f89545748fef6ebac8391b768
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7486948
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1570587}

--- third_party/blink/renderer/platform/wtf/text/string_impl.h
+++ third_party/blink/renderer/platform/wtf/text/string_impl.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <array>
 #include <atomic>
 #include <functional>
+#include <new>
 
 #include "base/check_op.h"
 #include "base/compiler_specific.h"
@@ -94,8 +95,7 @@ class WTF_EXPORT StringImpl {
   // StringImpls are allocated out of the WTF buffer partition.
   void* operator new(size_t);
   void* operator new(size_t, void* ptr) { return ptr; }
-  void operator delete(void*);
-  void operator delete(void*, size_t);
+  void operator delete(StringImpl* impl, std::destroying_delete_t);
 
   // Used to construct static strings, which have a special ref_count_ that can
   // never hit zero. This means that the static string will never be destroyed.
@@ -312,7 +312,7 @@ class WTF_EXPORT StringImpl {
   //    Table Add and Removal operations (including the fetch_sub to 0) are
   //    done under a lock.
 
-  ALWAYS_INLINE void Release() const {
+  ALWAYS_INLINE void Release() {
     if (!IsStatic()) {
       // This can be a relaxed load as long as the subtraction is performed
       // with acq_rel order. Any modification to `ref_count_` reordered after
@@ -620,7 +620,7 @@ class WTF_EXPORT StringImpl {
       StripBehavior);
   NOINLINE wtf_size_t HashSlowCase() const;
 
-  void DestroyIfNeeded() const;
+  void DestroyIfNeeded();
 
   // Calculates the kContainsOnlyAscii and kIsLowerAscii flags. Returns
   // a bitfield with those 2 values.
