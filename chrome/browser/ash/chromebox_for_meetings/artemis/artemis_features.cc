b2f169b799323ac5d1e8361275f01c56d9a24d28
Niko Tsirakis <ntsirakis@google.com>
2026-01-16 10:26:44 -0800

    artemis: Add ArtemisDynamicCloudLogging feature flag
    This will allow us to configure the following via experiment,
    in an attempt to better debug fleet devices on-demand (i.e.
    without an OS update):
    
    - SupplementaryLogFiles: track additional log files that we
      don't typically include in our default tracking.
    - TelemetryVerbosity: increase our telemetry to catch system
      level resource issues that aren't adequately captured by logs.
      The default is kWatchdog, which is the bare minimum list of
      commands that are needed to power watchdogs.
    
    Bug: 364253835
    Change-Id: I6aca8e373cd5dcdff6dd67bb33068c59f45760a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7363089
    Commit-Queue: Niko Tsirakis <ntsirakis@google.com>
    Reviewed-by: Kyle Williams <kdgwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1570490}

---
+++ chrome/browser/ash/chromebox_for_meetings/artemis/artemis_features.cc
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ash/chromebox_for_meetings/artemis/artemis_features.h"
+
+#include "base/feature_list.h"
+#include "base/metrics/field_trial_params.h"
+
+namespace ash::cfm::features {
+
+BASE_FEATURE(kArtemisDynamicCloudLogging, base::FEATURE_DISABLED_BY_DEFAULT);
+
+const base::FeatureParam<std::string> kSupplementaryLogs{
+    &kArtemisDynamicCloudLogging, "SupplementaryLogs", ""};
+
+constexpr base::FeatureParam<TelemetryVerbosity>::Option
+    kTelemetryVerbosityOptions[] = {
+        {TelemetryVerbosity::kWatchdog, "WATCHDOG"},
+        {TelemetryVerbosity::kInfo, "INFO"},
+        {TelemetryVerbosity::kVerbose, "VERBOSE"},
+};
+
+const base::FeatureParam<TelemetryVerbosity> kTelemetryVerbosity{
+    &kArtemisDynamicCloudLogging, "TelemetryVerbosity",
+    TelemetryVerbosity::kWatchdog, &kTelemetryVerbosityOptions};
+
+}  // namespace ash::cfm::features
