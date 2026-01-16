795b3292e944d979ea6a4213c7d74bf3e5617376
Friedrich Horschig <friedrichh@chromium.org>
2026-01-16 08:14:21 -0800

    [Android][3PPWM] Restore 3P setting for temporary failures
    Fixes the bug that users "drop out" of 3P Autofill when it becomes
    temporarily unavailable. Resetting the setting prevents bad Autofill
    experiences (like no Autofill or Autofill with Google filling Chrome).
    
    A new pref remembers the user's choice and allows to restore the setting
    when autofill becomes available again. It is a string that stores the
    package name of the current Autofill provider. While the user uses
    platform autofill, it is continuously updated, so switching between 3P
    Autofill providers does not require adjusting the setting again. This
    ensures that 3P autofill is not unconditionally enabled but only exactly
    for the Autofill service that the user previously used in Chrome.
    
    Video: go/scrcast/NjUxODc1ODE3NTY3MDI3Mnw2MzFiNTQ4NC1hYw
    Feature: AutofillAndPasswordsInSameSurface (kill-switch)
    ProductionSupportedFlagList: not needed. WV always uses platform AF.
    Bug: 419497261
    Change-Id: I8a2a40c29f3f25f3ee8a97d9a2dc3ac5f58f83f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7461539
    Commit-Queue: Friedrich Hauser <friedrichh@chromium.org>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1570400}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -132,6 +132,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &autofill::features::kAutofillRetryImageFetchOnFailure,
     &autofill::features::kAutofillSyncEwalletAccounts,
     &autofill::features::kAutofillThirdPartyModeContentProvider,
+    &autofill::features::kAutofillThirdPartyModeRestoredOnStart,
     &base::features::kBackgroundThreadPoolFieldTrial,
     &base::features::kLowEndMemoryExperiment,
     &base::features::kPostGetMyMemoryStateToBackground,
--- chrome/browser/ui/autofill/autofill_client_provider_unittest.cc
+++ chrome/browser/ui/autofill/autofill_client_provider_unittest.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 
 #if BUILDFLAG(IS_ANDROID)
+#include "base/android/jni_string.h"
 #include "base/android/shared_preferences/shared_preferences_manager.h"
 #include "chrome/browser/autofill/android/jni_test_headers/AutofillClientProviderTestUtils_jni.h"
 #include "chrome/browser/preferences/android/chrome_shared_preferences.h"
@@ -52,6 +53,17 @@ void ResetAutofillAvailability() {
       base::android::AttachCurrentThread());
 }
 
+// This allows to set the Autofill Service Package Name.
+void SetAutofillServicePackageNameForTesting(std::string package_name) {
+  test::Java_AutofillClientProviderTestUtils_setAutofillServicePackageName(
+      base::android::AttachCurrentThread(), std::move(package_name));
+}
+
+void ResetAutofillServicePackageName() {
+  test::Java_AutofillClientProviderTestUtils_resetAutofillServicePackageName(
+      base::android::AttachCurrentThread());
+}
+
 #endif  // BUILDFLAG(IS_ANDROID)
 
 class AutofillClientProviderBaseTest : public testing::Test {
@@ -62,6 +74,7 @@ class AutofillClientProviderBaseTest : public testing::Test {
     profile_.reset();  // Important since it also resets the prefs.
 #if BUILDFLAG(IS_ANDROID)
     ResetAutofillAvailability();
+    ResetAutofillServicePackageName();
 #endif  // BUILDFLAG(IS_ANDROID)
   }
 
@@ -107,7 +120,8 @@ class AutofillClientProviderTest : public AutofillClientProviderBaseTest {
   AutofillClientProviderTest() {
     scoped_feature_list_.InitWithFeatures(
         {features::kAutofillThirdPartyModeContentProvider,
-         features::kAutofillDeepLinkAutofillOptions},
+         features::kAutofillDeepLinkAutofillOptions,
+         features::kAutofillThirdPartyModeRestoredOnStart},
         {});
   }
 
@@ -183,6 +197,38 @@ TEST_F(AutofillClientProviderTest,
       android::shared_preferences::GetChromeSharedPreferences();
   ASSERT_TRUE(prefs.ContainsKey(kAutofillThirdPartyModeState));
   EXPECT_FALSE(prefs.ReadBoolean(kAutofillThirdPartyModeState, false));
+}
+
+TEST_F(AutofillClientProviderTest,
+       UpdatesPackageNamePrefWhenUsingPlatformAutofill) {
+  SetAutofillAvailabilityForTesting(
+      AndroidAutofillAvailabilityStatus::kAvailable);
+  prefs().SetString(prefs::kAutofillThirdPartyPackageUsedForPlatformAutofill,
+                    "com.old.service");
+  SetAutofillServicePackageNameForTesting("com.new.service");
+
+  EXPECT_TRUE(provider().uses_platform_autofill());
+  EXPECT_EQ(prefs().GetString(
+                prefs::kAutofillThirdPartyPackageUsedForPlatformAutofill),
+            "com.new.service");
+}
+
+TEST_F(AutofillClientProviderTest,
+       KeepsPackageNamePrefWhenPlatformAutofillIsTemporarilyTurnedOff) {
+  SetAutofillAvailabilityForTesting(
+      AndroidAutofillAvailabilityStatus::kSettingTurnedOff);
+
+  // Assume some service was used before: it shouldn't be changed!
+  prefs().SetString(prefs::kAutofillThirdPartyPackageUsedForPlatformAutofill,
+                    "com.old.service");
+
+  // There could be a new service but it's currently not used.
+  SetAutofillServicePackageNameForTesting("com.new.service");
+
+  EXPECT_FALSE(provider().uses_platform_autofill());
+  EXPECT_EQ(prefs().GetString(
+                prefs::kAutofillThirdPartyPackageUsedForPlatformAutofill),
+            "com.old.service");
 }
 
 #endif  // BUILDFLAG(IS_ANDROID)
