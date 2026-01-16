247776148f5cfb1ee52c3412e41e8dfe493bc240
Vasiliy Telezhnikov <vasilyt@chromium.org>
2026-01-16 11:40:11 -0800

    Remove GLHelper
    It's not used anymore.
    
    Bug: 391648152
    Change-Id: Ic1a9ef73a7c7a8174fc41b3c587180c161ea8d15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7487174
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1570548}

--- content/renderer/webgraphicscontext3d_provider_impl.cc
+++ content/renderer/webgraphicscontext3d_provider_impl.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/tiles/gpu_image_decode_cache.h"
 #include "content/public/common/content_switches.h"
 #include "gpu/command_buffer/client/context_support.h"
-#include "gpu/command_buffer/client/gl_helper.h"
 #include "gpu/config/gpu_feature_info.h"
 #include "services/viz/public/cpp/gpu/context_provider_command_buffer.h"
 #include "third_party/skia/include/gpu/ganesh/GrDirectContext.h"
@@ -142,14 +141,6 @@ WebGraphicsContext3DProviderImpl::GetWebglPreferences() const {
   }
 
   return prefs;
-}
-
-gpu::GLHelper* WebGraphicsContext3DProviderImpl::GetGLHelper() {
-  if (!gl_helper_) {
-    gl_helper_ = std::make_unique<gpu::GLHelper>(provider_->ContextGL(),
-                                                 provider_->ContextSupport());
-  }
-  return gl_helper_.get();
 }
 
 void WebGraphicsContext3DProviderImpl::SetLostContextCallback(
