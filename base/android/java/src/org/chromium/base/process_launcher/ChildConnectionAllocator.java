33283edd59bdf932db6d4a54b935e8c6f6f39554
Sam Maier <smaier@google.com>
2026-01-16 10:04:20 -0800

    Add javaless renderer fallback to normal renderer in failure case
    No-Try as cast bots are broken.
    
    No-Try: true
    Bug: 475852547
    Change-Id: I72a39face34cd936bd010a770824522c6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7472103
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Auto-Submit: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1570475}

--- base/android/java/src/org/chromium/base/process_launcher/ChildConnectionAllocator.java
+++ base/android/java/src/org/chromium/base/process_launcher/ChildConnectionAllocator.java
@@ -186,6 +186,7 @@ public abstract class ChildConnectionAllocator {
             Runnable freeSlotCallback,
             String packageName,
             String serviceClassName,
+            @Nullable String fallbackServiceClassName,
             boolean bindToCaller,
             boolean bindAsExternalService,
             boolean isSandboxedForHistograms) {
@@ -219,8 +220,11 @@ public abstract class ChildConnectionAllocator {
         // experimental results.
         disableZygote = SysUtils.isLowEndDevice() || disableZygote;
         String suffix = disableZygote ? NON_ZYGOTE_SUFFIX : ZYGOTE_SUFFIX;
-        String fallbackServiceClassName =
-                disableZygote ? null : serviceClassName + NON_ZYGOTE_SUFFIX;
+        if (fallbackServiceClassName != null) {
+            fallbackServiceClassName += suffix;
+        } else {
+            fallbackServiceClassName = disableZygote ? null : serviceClassName + NON_ZYGOTE_SUFFIX;
+        }
         return new VariableSizeAllocatorImpl(
                 launcherHandler,
                 freeSlotCallback,
--- content/public/android/java/src/org/chromium/content/browser/ChildProcessLauncherHelperImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/ChildProcessLauncherHelperImpl.java
@@ -696,6 +696,7 @@ public final class ChildProcessLauncherHelperImpl {
                                 freeSlotRunnable,
                                 packageName,
                                 ChildProcessCreationParamsImpl.getSandboxedServicesName(),
+                                ChildProcessCreationParamsImpl.getBackupSandboxedServicesName(),
                                 bindToCaller,
                                 bindAsExternalService,
                                 sandboxed);
