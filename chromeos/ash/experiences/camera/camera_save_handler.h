ba7c8b8755f219e6024ff9d89dcb0dbf90f2a444
Orko Garai <orko@igalia.com>
2026-01-16 00:34:15 -0800

    CCA: Use path from OdfsSkyvaultUploader for OneDrive uploaded path
    Using the fusebox path immediately after an image is saved on OneDrive
    seems to be error-prone as that may return zero bytes initially. So when
    opening the image in the image editor from the done notification
    immediately may not work. Instead, use the storage::FileSystemURL
    returned by OdfsSkyvaultUploader for the uploaded path which is more
    reliable.
    
    Design Doc:
    https://docs.google.com/document/d/1wod7fE1aU4D0boQxx9dxgNsAvJXvBPfvc8XFAmjtFuQ/edit?usp=sharing
    
    Bug: 454152412
    Change-Id: I7861f3b4615135236074cd6d3664872864a7ee37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7473466
    Reviewed-by: Aida Zolić <aidazolic@chromium.org>
    Commit-Queue: Orko Garai <orko@igalia.com>
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1570223}

--- chromeos/ash/experiences/camera/camera_save_handler.h
+++ chromeos/ash/experiences/camera/camera_save_handler.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROMEOS_ASH_EXPERIENCES_CAMERA_CAMERA_SAVE_HANDLER_H_
 
 #include <memory>
+#include <optional>
 #include <string>
 
 #include "base/functional/callback.h"
@@ -73,13 +74,15 @@ class CameraSaveHandler : public base::SupportsUserData::Data {
     // `progress_callback` is called periodically during upload with the number
     // of bytes uploaded.
     // `done_callback` is called when the upload is finished with a boolean
-    // indicating success or failure.
+    // indicating success or failure and optionally the uploaded file path.
     virtual void PerformUpload(
         const base::FilePath& upload_from_path,
         int64_t file_size,
         const gfx::Image& thumbnail,
         base::RepeatingCallback<void(int64_t)> progress_callback,
-        base::OnceCallback<void(bool)> done_callback) = 0;
+        base::OnceCallback<void(bool,
+                                std::optional<base::FilePath> uploaded_path)>
+            done_callback) = 0;
 
     // Cancels all ongoing uploads.
     virtual void CancelUploads() = 0;
@@ -171,7 +174,9 @@ class CameraSaveHandler : public base::SupportsUserData::Data {
   // Gets called for each file when it is no longer being uploaded.
   void OnUploadDone(const base::FilePath& upload_from_path,
                     const gfx::Image& thumbnail,
-                    bool success) VALID_CONTEXT_REQUIRED(sequence_checker_);
+                    bool success,
+                    std::optional<base::FilePath> uploaded_path)
+      VALID_CONTEXT_REQUIRED(sequence_checker_);
   void OpenFileInImageEditor(const base::FilePath& file_path);
   void DeleteFileAfterUpload(const base::FilePath& file_path);
   void OnUploadErrorRetake();
