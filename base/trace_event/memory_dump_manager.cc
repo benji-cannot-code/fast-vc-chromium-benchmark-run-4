5b027d4738310c030e7a68a45c0256dc67bc9694
Joe Mason <joenotcharles@google.com>
2026-01-16 08:28:32 -0800

    Rebucket Memory.DumpProvider.FollowingProviders again and log counts
    The 99th percentile of FollowingProviders2 is still overflowing by a
    lot. Even the 95th percentile of
    Memory.DumpProvider.FollowingProviders2.TileManager is overflowing.
    Increase the max from 1K to 100K, and bump the histogram version to
    FollowingProviders3.
    
    Also adds Memory.DumpProvider.Count.{Name} to easily see which
    providers are creating many instances.
    
    OBSOLETE_HISTOGRAMS=Patterned histogram Memory.DumpProvider.FollowingProviders2.{Name} is replaced by Memory.DumpProvider.FollowingProviders3.{Name}
    
    Bug: 450929521
    Change-Id: I26856eac0e7260f0ef41988d339bb590bc09bf12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7475011
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1570406}

--- base/trace_event/memory_dump_manager.cc
+++ base/trace_event/memory_dump_manager.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "partition_alloc/buildflags.h"
 #include "third_party/abseil-cpp/absl/base/dynamic_annotations.h"
+#include "third_party/abseil-cpp/absl/container/flat_hash_map.h"
 
 #if BUILDFLAG(IS_ANDROID)
 #include "base/android/jni_android.h"
@@ -582,14 +583,27 @@ MemoryDumpManager::ProcessMemoryDumpAsyncState::ProcessMemoryDumpAsyncState(
   // which will be executed after the provider being added.
   pending_dump_providers.reserve(dump_providers.size());
   size_t num_following_providers = 0;
+  absl::flat_hash_map<std::string, size_t> provider_counts;
   for (scoped_refptr<MemoryDumpProviderInfo> provider :
        base::Reversed(dump_providers)) {
+    ++provider_counts[provider->name.histogram_name()];
     pending_dump_providers.emplace_back(std::move(provider),
                                         num_following_providers++);
   }
   MemoryDumpArgs args = {req_args.level_of_detail, req_args.determinism,
                          req_args.dump_guid};
   process_memory_dump = std::make_unique<ProcessMemoryDump>(args);
+
+  // Log the count of objects for each provider type, and the total count
+  // without a suffix.
+  for (const auto& [provider_name, count] : provider_counts) {
+    base::UmaHistogramCounts100000(
+        base::StrCat({"Memory.DumpProvider.Count.", provider_name}),
+        static_cast<int>(count));
+  }
+  base::UmaHistogramCounts100000(
+      "Memory.DumpProvider.Count",
+      static_cast<int>(pending_dump_providers.size()));
 }
 
 MemoryDumpManager::ProcessMemoryDumpAsyncState::~ProcessMemoryDumpAsyncState() =
