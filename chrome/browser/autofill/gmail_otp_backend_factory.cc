98b0a7a57709a042373d3d0889b979d6f8b9b10d
trang mai <trangm@google.com>
2026-01-16 08:11:57 -0800

    [OTP] Add GmailOtpBackend with fake impl
    Bug: 463922782
    Change-Id: Iefb788f2d2ccd0f491bb133861aad8c4a6a2a4ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7234157
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Commit-Queue: Trang Mai <trangm@google.com>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1570396}

---
+++ chrome/browser/autofill/gmail_otp_backend_factory.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/autofill/gmail_otp_backend_factory.h"
+
+#include "components/keyed_service/content/browser_context_dependency_manager.h"
+#include "components/one_time_tokens/core/browser/gmail_otp_backend.h"
+#include "content/public/browser/browser_context.h"
+
+// static
+one_time_tokens::GmailOtpBackend* GmailOtpBackendFactory::GetForBrowserContext(
+    content::BrowserContext* context) {
+  return static_cast<one_time_tokens::GmailOtpBackend*>(
+      GetInstance()->GetServiceForBrowserContext(context, /*create=*/true));
+}
+
+// static
+GmailOtpBackendFactory* GmailOtpBackendFactory::GetInstance() {
+  static base::NoDestructor<GmailOtpBackendFactory> instance;
+  return instance.get();
+}
+
+GmailOtpBackendFactory::GmailOtpBackendFactory()
+    : BrowserContextKeyedServiceFactory(
+          "GmailOtpBackend",
+          BrowserContextDependencyManager::GetInstance()) {}
+
+GmailOtpBackendFactory::~GmailOtpBackendFactory() = default;
+
+std::unique_ptr<KeyedService>
+GmailOtpBackendFactory::BuildServiceInstanceForBrowserContext(
+    content::BrowserContext* context) const {
+  return one_time_tokens::GmailOtpBackend::Create();
+}
