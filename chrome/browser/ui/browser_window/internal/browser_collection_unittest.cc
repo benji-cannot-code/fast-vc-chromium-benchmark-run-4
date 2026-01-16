b8ea420d4982791b8fda48f6006a12dedd36a5c6
Glenn Hartmann <hartmanng@chromium.org>
2026-01-16 06:25:35 -0800

    Merge BrowserCollection Android and non-Android implementations.
    * Delete basically-empty browser_collection_android.cc
    * Rename browser_collection_non_android.cc and browser_collection_non_android_unittest.cc 
      back to their original names
    * Make mock_browser_window_interface.h and browser_collection_unittest.cc work on Android
    
    Refer to
    https://docs.google.com/document/d/17yoOyDfHPaRo-MY4QDqvBK_adPnO2jymZbNh4JvNST8/edit?tab=t.0
    for reasoning.
    
    Bug: 474120522
    Bypass-Check-License: moved files, not new
    Change-Id: Ib74269f1b0034590acb36722bef4f98d6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7485129
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Glenn Hartmann <hartmanng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1570344}

--- chrome/browser/ui/browser_window/internal/browser_collection_unittest.cc
+++ chrome/browser/ui/browser_window/internal/browser_collection_unittest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/ui/browser_window/public/browser_collection.h"
+
 #include "chrome/browser/ui/browser_window/public/browser_collection_observer.h"
 #include "chrome/browser/ui/browser_window/test/mock_browser_window_interface.h"
 #include "testing/gtest/include/gtest/gtest.h"
