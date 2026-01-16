a51966197e23aabf51fdc125c265787ef4c3c5e3
Joemer Ramos <joemerramos@google.com>
2026-01-16 14:00:56 -0800

    [iOS] Modify UpdateGeminiViewState Function To Take Animation Flag
    Adds a replacement function and the old function will be removed
    after the CL lands.
    
    Bug: 475205334
    Change-Id: I9da171f50ad0a2a3d2d9b7c4acfbc169708d432c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7486839
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Commit-Queue: Joemer Ramos <joemerramos@google.com>
    Cr-Commit-Position: refs/heads/main@{#1570634}

--- ios/chrome/test/providers/bwg/test_bwg.mm
+++ ios/chrome/test/providers/bwg/test_bwg.mm
@@ -54,6 +54,8 @@ void UpdateOverlayOffsetWithOpacity(CGFloat offset, CGFloat opacity) {}
 
 void UpdateGeminiViewState(GeminiViewState view_state) {}
 
+void UpdateGeminiViewState(GeminiViewState view_state, bool animated) {}
+
 GeminiViewState GetCurrentGeminiViewState() {
   return GeminiViewState::kUnknown;
 }
