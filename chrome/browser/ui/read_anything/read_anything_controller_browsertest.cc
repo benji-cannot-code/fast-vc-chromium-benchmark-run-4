995a68bfcaeb0441d81044c75df5ccc77bfae1e5
kenoss <kenoss@chromium.org>
2026-01-15 23:56:39 -0800

    Gardener: Disable three ReadAnythingControllerBrowserTest
    This CL disables the following tests:
    
    - ReadAnythingControllerBrowserTest.ShowImmersiveUI_ClosesSidePanel
    - ReadAnythingControllerBrowserTest.ToggleImmersiveUI_ClosesSidePanel
    - ReadAnythingControllerBrowserTest.TogglePresentation_FromSidePanel_OpensImmersive
    
    These tests started to fail at 2025-01-15T16:38. As the component has 8
    commits since yesterday and it's hard to identify what the test caused
    the failure, this CL temporarily disables the tests and enables to
    handle the failure with a proper fix.
    
    Bug: 476203141
    Change-Id: I3107235272dcfa21f5b6991567650dab114c9c24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7488810
    Auto-Submit: Ken Okada <kenoss@chromium.org>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Owners-Override: Ken Okada <kenoss@chromium.org>
    Commit-Queue: Ken Okada <kenoss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1570215}

--- chrome/browser/ui/read_anything/read_anything_controller_browsertest.cc
+++ chrome/browser/ui/read_anything/read_anything_controller_browsertest.cc
@@ -976,8 +976,9 @@ IN_PROC_BROWSER_TEST_F(ReadAnythingControllerBrowserTest,
   EXPECT_NE(GetImmersiveWebContents(), starting_contents);
 }
 
+// TODO(crbug.com/476203141): Reenable the test
 IN_PROC_BROWSER_TEST_F(ReadAnythingControllerBrowserTest,
-                       ShowImmersiveUI_ClosesSidePanel) {
+                       DISABLED_ShowImmersiveUI_ClosesSidePanel) {
   tabs::TabInterface* tab = browser()->tab_strip_model()->GetActiveTab();
   ASSERT_TRUE(tab);
   auto* controller = ReadAnythingController::From(tab);
@@ -1162,8 +1163,9 @@ IN_PROC_BROWSER_TEST_F(ReadAnythingControllerBrowserTest,
             ReadAnythingController::PresentationState::kInactive);
 }
 
+// TODO(crbug.com/476203141): Reenable the test
 IN_PROC_BROWSER_TEST_F(ReadAnythingControllerBrowserTest,
-                       ToggleImmersiveUI_ClosesSidePanel) {
+                       DISABLED_ToggleImmersiveUI_ClosesSidePanel) {
   tabs::TabInterface* tab = browser()->tab_strip_model()->GetActiveTab();
   ASSERT_TRUE(tab);
   auto* controller = ReadAnythingController::From(tab);
@@ -1220,8 +1222,10 @@ IN_PROC_BROWSER_TEST_F(ReadAnythingControllerBrowserTest,
             ReadAnythingController::PresentationState::kInSidePanel);
 }
 
-IN_PROC_BROWSER_TEST_F(ReadAnythingControllerBrowserTest,
-                       TogglePresentation_FromSidePanel_OpensImmersive) {
+// TODO(crbug.com/476203141): Reenable the test
+IN_PROC_BROWSER_TEST_F(
+    ReadAnythingControllerBrowserTest,
+    DISABLED_TogglePresentation_FromSidePanel_OpensImmersive) {
   tabs::TabInterface* tab = browser()->tab_strip_model()->GetActiveTab();
   ASSERT_TRUE(tab);
   auto* controller = ReadAnythingController::From(tab);
