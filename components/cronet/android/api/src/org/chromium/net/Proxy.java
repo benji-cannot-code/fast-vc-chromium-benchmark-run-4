a4f18287397e09f1f0f840465b7c68f95c55f14f
Stefano Duo <stefanoduo@google.com>
2026-01-16 09:49:25 -0800

    Declare acceptable values for port via @IntRange
    This follows what we do for the new HttpEngine's APIs.
    
    Bug: b:469376436
    Cq-Include-Trybots: luci.chrome.try:cronet-arm64-gn2bp-debug
    Bypass-Check-License: changes in parent CL are affecting this check
    Change-Id: I2b2b5e2f25d675d57f4e00e9f6057e3a83a8ca5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7420211
    Reviewed-by: Etienne Dechamps <edechamps@google.com>
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1570461}

--- components/cronet/android/api/src/org/chromium/net/Proxy.java
+++ components/cronet/android/api/src/org/chromium/net/Proxy.java
@@ -8,6 +8,7 @@ package org.chromium.net;
 import android.util.Pair;
 
 import androidx.annotation.IntDef;
+import androidx.annotation.IntRange;
 import androidx.annotation.NonNull;
 
 import java.lang.annotation.Retention;
@@ -190,7 +191,7 @@ public final class Proxy {
     public static @NonNull Proxy createHttpProxy(
             @Scheme int scheme,
             @NonNull String host,
-            int port,
+            @IntRange(from = 0, to = 65535) int port,
             @NonNull Executor executor,
             @NonNull HttpConnectCallback callback) {
         return new Proxy(scheme, host, port, executor, callback);
