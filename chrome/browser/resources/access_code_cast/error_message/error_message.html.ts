04900ce7258a604681a1b51f8e6ca3c26d11fb56
rbpotter <rbpotter@chromium.org>
2026-01-16 08:51:29 -0800

    Access Code Cast: Migrate error-message to Lit
    Bug: 40943652
    Change-Id: I4ccc580b1af84195583865f9e0de6a8a54d8a3c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7477293
    Reviewed-by: Benjamin Zielinski <bzielinski@google.com>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1570422}

---
+++ chrome/browser/resources/access_code_cast/error_message/error_message.html.ts
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {ErrorMessageElement} from './error_message.js';
+
+export function getHtml(this: ErrorMessageElement) {
+  // clang-format off
+  return html`<!--_html_template_start_-->
+<div class="error" aria-live="assertive">${this.getErrorMessage()}</div>
+<!--_html_template_end_-->`;
+  // clang-format on
+}
