4ad84d6b86aeac8ff3458ee6d40c64ea9b86107f
Lei Zhang <thestig@chromium.org>
2026-01-16 14:52:33 -0800

    [User Education] Mark WebUI help bubbles as visible
    When HelpBubbleElement.show() gets called, setting
    `style.display = 'block'` is not enough to make the help bubble
    focusable, as it may still be hidden. So set
    `this.style.visibility = 'visible';` to fix this.
    
    Bug: 470115240
    Change-Id: I60492ab6933e7c755237daae1d14dc3fe1d88c67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7489880
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1570662}

--- ui/webui/resources/cr_components/help_bubble/help_bubble.ts
+++ ui/webui/resources/cr_components/help_bubble/help_bubble.ts
@@ -176,6 +176,7 @@ export class HelpBubbleElement extends CrLitElement {
     // contents of an opened bubble.
     this.style.display = 'block';
     this.style.position = this.fixed ? 'fixed' : 'absolute';
+    this.style.visibility = 'visible';
     this.removeAttribute('aria-hidden');
     this.updatePosition_();
 
@@ -221,6 +222,7 @@ export class HelpBubbleElement extends CrLitElement {
       this.resizeObserver_ = null;
     }
     this.style.display = 'none';
+    this.style.visibility = 'hidden';
     this.setAttribute('aria-hidden', 'true');
     this.anchorElement_ = null;
     if (this.timeoutTimerId !== null) {
