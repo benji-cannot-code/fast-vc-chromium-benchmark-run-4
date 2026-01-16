784f6693f3e8ee01640ac7e8b20d98afb22f915d
Paul Adedeji <pauladedeji@google.com>
2026-01-16 15:11:26 -0800

    [Omnibox] Adjust composebox spacing for compact layout mode.
    A previous CL [1] moved the tool chips from the bottom to the top of the
    composebox. However, vertical spacing was not updated to reflect this.
    
    This CL reallocates the existing padding/margin from the bottom to the
    top of the container.
    
    Changes:
    
    * Adjusted context's margin top to -12px. This is the sum of the
    original top margin (-48px) [2] combined with the 36px previously
    reserved at the bottom [3].
    
    * Reduced the minimum height of the context menu container to 0 when in
    compact mode. This removes the vertical gap that was at the bottom
    previously.
    
    http://screencast/cast/NTg3OTkyNTQ3ODI2MDczNnwyY2JlYzU4MC1mYQ
    
    [1]https://chromium-review.googlesource.com/c/chromium/src/+/7477295
    [2]https://source.chromium.org/chromium/chromium/src/+/main:ui/webui/resources/cr_components/composebox/composebox.css;drc=cdbbd9d3606ba7e06ea44a8fd419981626c1984a;l=574
    [3]https://source.chromium.org/chromium/chromium/src/+/main:ui/webui/resources/cr_components/composebox/contextual_entrypoint_and_carousel.css;drc=adf0dbd49ab32f907d7f91df890820143c068279;l=33
    
    Change-Id: I4d1a4283a37c4c99fe013a474998b111b838088a
    Bug: b:475569512
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7485881
    Auto-Submit: Paul Adedeji <pauladedeji@google.com>
    Reviewed-by: Teresa Mao <temao@chromium.org>
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1570669}

--- ui/webui/resources/cr_components/composebox/contextual_entrypoint_and_carousel.ts
+++ ui/webui/resources/cr_components/composebox/contextual_entrypoint_and_carousel.ts
@@ -159,6 +159,10 @@ export class ContextualEntrypointAndCarouselElement extends I18nMixinLit
       recentTabForChip_: {type: Object},
       carouselOnTop_: {type: Boolean},
       submitButtonShown: {type: Boolean},
+      isOmniboxInCompactMode_: {
+        type: Boolean,
+        reflect: true,
+      },
     };
   }
 
@@ -173,6 +177,7 @@ export class ContextualEntrypointAndCarouselElement extends I18nMixinLit
   accessor contextMenuGlifAnimationState: GlifAnimationState =
       GlifAnimationState.INELIGIBLE;
   accessor inComposebox: boolean = false;
+  accessor isOmniboxInCompactMode_: boolean = false;
 
   protected accessor attachmentFileTypes_: string =
       loadTimeData.getString('composeboxAttachmentFileTypes');
@@ -242,17 +247,18 @@ export class ContextualEntrypointAndCarouselElement extends I18nMixinLit
   }
 
   protected get shouldShowToolChipsForTallMode_(): boolean {
-    return !this.shouldShowToolChipsForCompactMode_;
+    // TODO(b/476405347): Consolidate logic here and remove the Omnibox specific
+    // code.
+    if (this.entrypointName === 'Omnibox') {
+      return !this.shouldShowToolChipsForCompactMode_;
+    }
+    return this.searchboxLayoutMode !== 'Compact' ||
+        this.shouldShowContextualChipsForCompactMode_;
   }
 
   protected get toolChipsVisible_(): boolean {
     return this.shouldShowRecentTabChip_ || this.shouldShowLensSearchChip_ ||
         this.inDeepSearchMode_ || this.inCreateImageMode_;
-  }
-
-  protected get isOmniboxInCompactMode_(): boolean {
-    return this.entrypointName === 'Omnibox' &&
-        this.searchboxLayoutMode === 'Compact';
   }
 
   protected get shouldShowToolChipsForCompactMode_(): boolean {
@@ -270,8 +276,13 @@ export class ContextualEntrypointAndCarouselElement extends I18nMixinLit
       return false;
     }
 
+    // TODO(b/476405347): Remove `entrypointName` condition.
+    // `this.shouldShowContextualChipsForCompactMode_` can possibly be removed
+    // without consequence.
     return this.showDropdown &&
-        (this.showFileCarousel_ ||
+        ((this.entrypointName !== 'Omnibox' &&
+          this.shouldShowContextualChipsForCompactMode_) ||
+         this.showFileCarousel_ ||
          this.searchboxLayoutMode === 'TallTopContext' ||
          this.submitButtonShown);
   }
@@ -323,6 +334,12 @@ export class ContextualEntrypointAndCarouselElement extends I18nMixinLit
         this.recentTabForChip_ =
             this.tabSuggestions.find(tab => tab.showInPreviousTabChip) || null;
       }
+    }
+
+    if (changedProperties.has('entrypointName') ||
+        changedProperties.has('searchboxLayoutMode')) {
+      this.isOmniboxInCompactMode_ = this.entrypointName === 'Omnibox' &&
+          this.searchboxLayoutMode === 'Compact';
     }
   }
 
