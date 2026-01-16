6526a42b234f3388f4d50e4b740db73633f162e9
Andrew Grieve <agrieve@chromium.org>
2026-01-16 09:13:12 -0800

    Reland #2 "Android: Fix crash from using TransitiveObservableSupplier after destroy()"
    This reverts commit fddf25339517b403b24a0ef67f627ab8a6f57e3e.
    
    Reason for reland: fixed safeGet() and made set() after destroy() safe
    
    Original change's description:
    > Revert "Reland "Android: Fix crash from using TransitiveObservableSupplier after destroy()""
    >
    > This reverts commit fb76dc9bac0d402d0b384bf6e9a7cfc695f86c92.
    >
    > Reason for revert:
    > The Suspect CL https://chromium-review.googlesource.com/q/I3e48fd997be6f7d18db73231f9ea5e2cebb8f605 is highly likely the cause of the crash. Please see the crash report http://crash/1e9a551bee87132d.
    > Rebase the Revert CL on top of main branch with conflicts allowed option to prevent false positive merge conflicts.
    >
    > If you agree with the revert CL, please approve it and mark as Commit-Queue +2 to submit it.
    >
    > More details can be found in the go/crash-revert-cls-creation.
    >
    > Original change's description:
    > > Reland "Android: Fix crash from using TransitiveObservableSupplier after destroy()"
    > >
    > > This reverts commit 16497eafd1246d7d5c2125017774c2803cc7e014.
    > >
    > > Reason for reland: Fixed destroy() not unregistering.
    > >
    > > Original change's description:
    > > > Revert "Android: Fix crash from using TransitiveObservableSupplier after destroy()"
    > > >
    > > > This reverts commit 9ccbbad8a1d3e15c83abdf81a4e32edd2c2c9668.
    > > >
    > > > Reason for revert: Broke another test (see bug)
    > > >
    > > > Original change's description:
    > > > > Android: Fix crash from using TransitiveObservableSupplier after destroy()
    > > > >
    > > > > Before the refactor in 86102b4bc68e75d3d392be417f8208697701184e, it was
    > > > > safe to call mConstraints.get() after mConstraints.destroy(). Make it
    > > > > safe to do so again (since it does happen).
    > > > >
    > > > > Bug: 471705353
    > > > > Change-Id: Ic3b9c7f1e1bf8e7c6c7552fed45243e3c9f074f2
    > > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7456646
    > > > > Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    > > > > Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    > > > > Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    > > > > Cr-Commit-Position: refs/heads/main@{#1569189}
    > > >
    > > > Bug: 471705353, 475890330
    > > > No-Presubmit: true
    > > > No-Tree-Checks: true
    > > > No-Try: true
    > > > Bug: 471705353
    > > > Change-Id: Ia85add21c71402952d7c707af1d9d3b0413343d8
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7475014
    > > > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > > > Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    > > > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > > > Cr-Commit-Position: refs/heads/main@{#1569315}
    > >
    > > Bug: 471705353
    > > Bug: 471705353, 475890330
    > > Change-Id: I3e48fd997be6f7d18db73231f9ea5e2cebb8f605
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7475456
    > > Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    > > Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1569474}
    > >
    > Crash Bug: 476269794
    >
    > Change-Id: I98d27bf24d983768322122ccfc910ab93f84d1cd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7487014
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1570367}
    
    Change-Id: I656c3de30981758c06c41c5a5406039d1e0633c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7486362
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1570439}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/SettableLookAheadObservableSupplierUnitTest.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/SettableLookAheadObservableSupplierUnitTest.java
@@ -10,6 +10,7 @@ import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
+import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -21,6 +22,7 @@ import org.mockito.junit.MockitoRule;
 import org.chromium.base.Callback;
 import org.chromium.base.supplier.ObservableSupplier.NotifyBehavior;
 import org.chromium.base.test.BaseRobolectricTestRunner;
+import org.chromium.build.BuildConfig;
 import org.chromium.chrome.browser.tab.LookAheadObservableSupplier;
 
 /** Unit tests for {@link LookAheadObservableSupplier}. */
@@ -112,6 +114,7 @@ public class SettableLookAheadObservableSupplierUnitTest {
 
     @Test(expected = AssertionError.class)
     public void testSetMismatchThrows() {
+        Assume.assumeTrue(BuildConfig.ENABLE_ASSERTS);
         mSupplier.willSet("value1");
         mSupplier.set("value2");
     }
@@ -122,9 +125,6 @@ public class SettableLookAheadObservableSupplierUnitTest {
         mSupplier.addObserver(mObserver);
 
         mSupplier.destroy();
-        mSupplier.set(SUPPLIER_VALUE);
-
-        verify(mLookAheadObserver, never()).onResult(SUPPLIER_VALUE);
-        verify(mObserver, never()).onResult(SUPPLIER_VALUE);
+        assertEquals(0, mSupplier.getObserverCount());
     }
 }
