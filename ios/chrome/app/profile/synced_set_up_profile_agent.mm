226c5a51f8366e03e7b182b3bd4d385d0048b04f
Benjamin Williams <bwwilliams@google.com>
2026-01-16 10:52:43 -0800

    [iOS][Code Health] Fix CrossDevicePrefTrackerObserver naming conventions
    This change updates the method names in the
    CrossDevicePrefTrackerObserver protocol to better align with Objective-C
    conventions.
    
    Specifically, it renames:
    * -onRemotePrefChanged:prefValue:remoteDeviceInfo: to
      -crossDevicePrefTracker:remotePref:didChangeToValue:fromDevice:
    * -onServiceStatusChanged: to
      -crossDevicePrefTracker:serviceStatusDidChange:
    
    This is part of a code health improvement to standardize observer
    bridge/protocol naming.
    
    Bug: 476071139
    Change-Id: I6ee9bfb415872c520b7fb859fe677a5f2dd8a1b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7485666
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1570511}

--- ios/chrome/app/profile/synced_set_up_profile_agent.mm
+++ ios/chrome/app/profile/synced_set_up_profile_agent.mm
@@ -90,10 +90,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #pragma mark - CrossDevicePrefTrackerObserver
 
-- (void)onRemotePrefChanged:(std::string_view)prefName
-                  prefValue:
-                      (const sync_preferences::TimestampedPrefValue&)prefValue
-           remoteDeviceInfo:(const syncer::DeviceInfo&)remoteDeviceInfo {
+- (void)
+    crossDevicePrefTracker:(sync_preferences::CrossDevicePrefTracker*)tracker
+       didChangeRemotePref:(std::string_view)prefName
+                   toValue:
+                       (const sync_preferences::TimestampedPrefValue&)prefValue
+                fromDevice:(const syncer::DeviceInfo&)remoteDeviceInfo {
   // This trigger should happen independently of foreground activation cycles,
   // so do not check `_activationAlreadyHandled` here.
   [self maybeTriggerSyncedSetUpWithSource:SyncedSetUpTriggerSource::
