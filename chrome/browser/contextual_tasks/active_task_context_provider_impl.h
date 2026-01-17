f52f639d583eb7271b591433850f2b228da350b7
Shakti Sahu <shaktisahu@chromium.org>
2026-01-17 08:55:44 -0800

    [contextual_tasks] Cleanup ActiveTaskContextProvider interface
    This is a pure refactor CL without functional changes.
    Public methods of ActiveTaskContextProvider (
    OnSidePanelStateUpdated and OnFullTabStateUpdated) were replaced
    by RefreshContext. All callsites were updated.
    
    Bug: b:476523954
    Change-Id: Ida2a656f8b07780020af64a95a347a8b1b7b4274
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7487542
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1570799}

--- chrome/browser/contextual_tasks/active_task_context_provider_impl.h
+++ chrome/browser/contextual_tasks/active_task_context_provider_impl.h
@@ -38,12 +38,11 @@ class ActiveTaskContextProviderImpl : public ActiveTaskContextProvider,
       const ActiveTaskContextProviderImpl&) = delete;
 
   // ActiveTaskContextProvider implementation.
-  void OnSidePanelStateUpdated() override;
+  void RefreshContext() override;
   void SetSessionHandleGetter(
       SessionHandleGetter session_handle_getter) override;
   void AddObserver(ActiveTaskContextProvider::Observer* observer) override;
   void RemoveObserver(ActiveTaskContextProvider::Observer* observer) override;
-  void OnFullTabStateUpdated() override;
 
   // ContextualTasksService::Observer implementation.
   void OnTaskAdded(const ContextualTask& task,
@@ -59,9 +58,6 @@ class ActiveTaskContextProviderImpl : public ActiveTaskContextProvider,
   void PrimaryPageChanged(content::Page& page) override;
 
  private:
-  // Determines the active task and triggers a context fetch.
-  void RefreshContext();
-
   // Callback for when GetContextForTask() completes.
   void OnGetContextForTask(int callback_id,
                            std::unique_ptr<ContextualTaskContext> context);
--- chrome/browser/contextual_tasks/contextual_tasks_side_panel_coordinator.cc
+++ chrome/browser/contextual_tasks/contextual_tasks_side_panel_coordinator.cc
@@ -914,7 +914,7 @@ ContextualTasksSidePanelCoordinator::GetSessionHandleForActiveTabOrSidePanel() {
 }
 
 void ContextualTasksSidePanelCoordinator::NotifyActiveTaskContextProvider() {
-  active_task_context_provider_->OnSidePanelStateUpdated();
+  active_task_context_provider_->RefreshContext();
 }
 
 size_t ContextualTasksSidePanelCoordinator::GetNumberOfActiveTasks() const {
