2ce54c48826ff86ff9d8509fca82c50af290c51d
Antonio Sartori <antoniosartori@chromium.org>
2026-01-19 09:13:33 -0800

    Use GeolocationContentSettingsType in components/embedder_support
    This is a small no-op refactoring that simplifies
    //components/embedder_support/permission_context_utils.cc using
    GeolocationContentSettingsType().
    
    Bug: 476929911
    Change-Id: I831074f9ef41e787e97f8f26642eba350e8e1aa3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7485576
    Reviewed-by: Mike Taylor <miketaylr@chromium.org>
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1571254}

--- components/embedder_support/permission_context_utils.cc
+++ components/embedder_support/permission_context_utils.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "build/build_config.h"
 #include "components/background_sync/background_sync_permission_context.h"
+#include "components/content_settings/core/common/content_settings_utils.h"
 #include "components/content_settings/core/common/features.h"
 #include "components/permissions/contexts/camera_pan_tilt_zoom_permission_context.h"
 #include "components/permissions/contexts/clipboard_read_write_permission_context.h"
@@ -87,10 +88,7 @@ CreateDefaultPermissionContexts(content::BrowserContext* browser_context,
           std::move(
               delegates.clipboard_sanitized_write_permission_context_delegate));
   ContentSettingsType location_context_key =
-      base::FeatureList::IsEnabled(
-          content_settings::features::kApproximateGeolocationPermission)
-          ? ContentSettingsType::GEOLOCATION_WITH_OPTIONS
-          : ContentSettingsType::GEOLOCATION;
+      content_settings::GeolocationContentSettingsType();
 #if BUILDFLAG(IS_ANDROID)
   permission_contexts[location_context_key] =
       std::make_unique<permissions::GeolocationPermissionContextAndroid>(
