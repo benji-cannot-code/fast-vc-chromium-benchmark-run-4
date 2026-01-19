946d71ef2e12010100c28afa672486b8523e7bd1
hiroki oshima <hiroki.oshima@gmail.com>
2026-01-19 15:33:46 -0800

    fix ScheduledFeatureTest dangling pointer
    Bug: 40212619
    Change-Id: I33535079549a56a076ba3e2d4970fe4503588836
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7424852
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Di Wu <diwux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1571373}

--- ash/system/scheduled_feature/scheduled_feature_unittest.cc
+++ ash/system/scheduled_feature/scheduled_feature_unittest.cc
@@ -284,7 +284,9 @@ class ScheduledFeatureTest : public NoSessionAshTestBase,
   }
 
   void TearDown() override {
+    timer_ptr_ = nullptr;
     feature_.reset();
+    geolocation_controller_ = nullptr;
     NoSessionAshTestBase::TearDown();
   }
 
@@ -428,8 +430,8 @@ class ScheduledFeatureTest : public NoSessionAshTestBase,
   // in the return value of `Now()` and defaults to 0.
   base::TimeDelta wall_clock_artificial_advancement_;
   std::unique_ptr<TestScheduledFeature> feature_;
-  raw_ptr<GeolocationController, DanglingUntriaged> geolocation_controller_;
-  raw_ptr<base::OneShotTimer, DanglingUntriaged> timer_ptr_;
+  raw_ptr<GeolocationController> geolocation_controller_;
+  raw_ptr<base::OneShotTimer> timer_ptr_;
   Geoposition position_;
 };
 
