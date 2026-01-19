b4441c78a32f4a17cedb92e1c5bb37ea6eb62bce
Xiaowei gao <xiaoweigao@microsoft.com>
2026-01-19 06:04:38 -0800

    [iOS][PageInfo]Add feature flag for certificate viewer
    This CL introduce Feature flag `kViewCertificateInformation` to control the feature rollout
    
    Bug: 476172425
    Change-Id: I3471e7ce515fb5b1dd3934a2dab7a8b1399bd8fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7478556
    Commit-Queue: Xiaowei Gao <xiaoweigao@microsoft.com>
    Reviewed-by: Filipa Senra <fsenra@google.com>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1571157}

---
+++ ios/chrome/browser/page_info/certificate/features/features.mm
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/page_info/certificate/features/features.h"
+
+#import "base/feature_list.h"
+
+namespace page_info_certificate {
+
+BASE_FEATURE(kViewCertificateInformation, base::FEATURE_DISABLED_BY_DEFAULT);
+
+bool IsViewCertificateInformationFeatureEnabled() {
+  return base::FeatureList::IsEnabled(kViewCertificateInformation);
+}
+
+}  // namespace page_info_certificate
