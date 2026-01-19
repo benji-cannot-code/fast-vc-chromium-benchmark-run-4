ec1523fc4b20404f173e2fec720b018a734bde80
Dominik Röttsches <drott@chromium.org>
2026-01-19 01:38:24 -0800

    Further improve performance of string passing from XML parser
    Avoid string clones in attribute passing build AtomicStrings from Rust
    references (without cloning them again) on the FFI side. See also [1].
    
    Improves blink_perf.parser xml-parser.html by 7.3% locally.
    
    [1] https://cxx.rs/binding/str.html
    
    Bug: 470367156
    Change-Id: Iddaf2dce32c0a059ea5fb1959541868e1f694e1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7456199
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1571030}

---
+++ third_party/blink/renderer/core/xml/parser/xml_ffi_callbacks.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/core/xml/parser/xml_ffi_callbacks.h"
+
+#include "base/strings/string_view_rust.h"
+
+namespace xml_ffi {
+namespace {
+
+blink::AtomicString RustStrToAtomicString(rust::Str str) {
+  return blink::AtomicString::FromUTF8(base::RustStrToStringView(str));
+}
+}  // namespace
+
+void xml_ffi::AttributeView::Populate(rust::Str q_name_a,
+                                      rust::Str attr_ns_a,
+                                      rust::Str value_a) {
+  this->q_name = RustStrToAtomicString(q_name_a);
+  this->attr_ns = RustStrToAtomicString(attr_ns_a);
+  this->value = RustStrToAtomicString(value_a);
+}
+
+}  // namespace xml_ffi
