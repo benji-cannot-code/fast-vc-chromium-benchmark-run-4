0895e8f0dc8038d9975b02f3ad1639e0d1783781
Michael Tatarski <mtatarski@google.com>
2026-01-19 10:36:50 -0800

    Acknowledge bookmark limit exceeded error in iOS
    When a user encounters the error for exceeding the bookmark limit, they
    are presented with a "Learn more" link. This CL acknowledges the error
    when the link is clicked, which subsequently hides the error UI.
    
    The AcknowledgeBookmarksLimitExceededError method is now called on the
    SyncService from the various places the user can access the help link.
    
    Unit tests are added for the affected coordinators to ensure the
    SyncService is called with the correct source.
    
    Screencast: http://screencast/cast/NTQ3MDkxNzAxNzczMTA3MnxlODAyYWRlNy03Nw
    
    Bug: 476026082
    Change-Id: I249d1715c52c2ee8b6b40682c0896e830df34834
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7481714
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Michael Tatarski <mtatarski@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1571287}

--- ios/chrome/browser/browser_view/ui_bundled/browser_coordinator.mm
+++ ios/chrome/browser/browser_view/ui_bundled/browser_coordinator.mm
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/supervised_user/core/browser/supervised_user_utils.h"
 #import "components/supervised_user/core/common/features.h"
 #import "components/supervised_user/core/common/supervised_user_constants.h"
+#import "components/sync/service/sync_service.h"
 #import "components/translate/core/browser/translate_manager.h"
 #import "components/trusted_vault/trusted_vault_server_constants.h"
 #import "components/webauthn/ios/ios_passkey_client.h"
@@ -309,6 +310,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/store_kit/model/store_kit_coordinator_delegate.h"
 #import "ios/chrome/browser/supervised_user/coordinator/parent_access_coordinator.h"
 #import "ios/chrome/browser/sync/model/sync_error_browser_agent.h"
+#import "ios/chrome/browser/sync/model/sync_service_factory.h"
 #import "ios/chrome/browser/synced_set_up/coordinator/synced_set_up_coordinator.h"
 #import "ios/chrome/browser/synced_set_up/coordinator/synced_set_up_coordinator_delegate.h"
 #import "ios/chrome/browser/synced_set_up/utils/utils.h"
@@ -4208,6 +4210,13 @@ const char kChromeAppStoreUrl[] =
 }
 
 - (void)showBookmarksLimitExceededHelp {
+  syncer::SyncService* syncService =
+      SyncServiceFactory::GetForProfile(self.browser->GetProfile());
+  if (syncService) {
+    syncService->AcknowledgeBookmarksLimitExceededError(
+        syncer::SyncService::BookmarksLimitExceededHelpClickedSource::
+            kSyncErrorMessage);
+  }
   GURL helpUrl(kBookmarksLimitExceededHelpCenter);
   UrlLoadParams params = UrlLoadParams::InNewTab(helpUrl);
   params.append_to = OpenPosition::kCurrentTab;
