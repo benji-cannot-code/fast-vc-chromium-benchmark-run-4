4e297b2de057f854629a768ae6de903077d3d456
hiroki oshima <hiroki.oshima@gmail.com>
2026-01-19 15:29:30 -0800

    fix PhoneHubTrayTest dangling pointer
    Bug: 40212619
    Change-Id: I9e5ff8bff76bb76633014faef2d113aa49163f63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7423871
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Di Wu <diwux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1571366}

--- ash/system/phonehub/phone_hub_tray_unittest.cc
+++ ash/system/phonehub/phone_hub_tray_unittest.cc
@@ -103,6 +103,7 @@ class PhoneHubTrayTest : public AshTestBase {
   }
 
   void TearDown() override {
+    phone_hub_tray_ = nullptr;
     AshTestBase::TearDown();
   }
 
@@ -186,7 +187,7 @@ class PhoneHubTrayTest : public AshTestBase {
   }
 
  protected:
-  raw_ptr<PhoneHubTray, DanglingUntriaged> phone_hub_tray_ = nullptr;
+  raw_ptr<PhoneHubTray> phone_hub_tray_ = nullptr;
   phonehub::FakePhoneHubManager phone_hub_manager_;
   base::test::ScopedFeatureList feature_list_;
   MockNewWindowDelegate new_window_delegate_;
