0fc111b7190a8c800831b56829114c812e3706d5
Quentin Pubert <qpubert@google.com>
2026-01-19 04:48:48 -0800

    [iOS] Ensure ChooseFileControllerImpl always calls its completion
    Previously, ChooseFileControllerImpl would only call its completion upon
    destruction if `file_picker_was_presented_` was true. This verification
    was actually only necessary for the former implementation of
    ChooseFileController, and it is now causing an issue if a navigation
    occurs before any submission. This CL ensures ChooseFileControllerImpl
    now always calls its completion upon destruction if not previously
    called already.
    
    Bug: 476951268
    Change-Id: I3fb8af5777b9c36cb0312dc6d7c3c6b1809dfd0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7487043
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Auto-Submit: Quentin Pubert <qpubert@google.com>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1571110}

--- ios/chrome/browser/web/model/choose_file/choose_file_controller_impl.mm
+++ ios/chrome/browser/web/model/choose_file/choose_file_controller_impl.mm
@@ -17,7 +17,7 @@ ChooseFileControllerImpl::ChooseFileControllerImpl(
 }
 
 ChooseFileControllerImpl::~ChooseFileControllerImpl() {
-  if (file_picker_was_presented_ && !HasSubmittedSelection()) {
+  if (!HasSubmittedSelection()) {
     SubmitSelection(nil, nil, nil);
   }
 }
@@ -27,9 +27,6 @@ bool ChooseFileControllerImpl::IsPresentingFilePicker() const {
 }
 
 void ChooseFileControllerImpl::SetIsPresentingFilePicker(bool is_presenting) {
-  if (is_presenting) {
-    file_picker_was_presented_ = true;
-  }
   is_presenting_file_picker_ = is_presenting;
 }
 
