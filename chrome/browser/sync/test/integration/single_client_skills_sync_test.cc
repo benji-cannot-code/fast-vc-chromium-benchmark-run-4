29128659c0263962e50e3e2fbe62dfd2a7774e83
Rushan Suleymanov <rushans@google.com>
2026-01-19 01:34:22 -0800

    [Skills] Implement ApplyDisableSyncChanges in the bridge
    Bug: b:471795213
    Change-Id: Ib119b93d492a61fcf126dee77c4193155a6ddd42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7483050
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1571028}

--- chrome/browser/sync/test/integration/single_client_skills_sync_test.cc
+++ chrome/browser/sync/test/integration/single_client_skills_sync_test.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 using testing::Contains;
+using testing::IsEmpty;
 using testing::Pointee;
 using testing::SizeIs;
 using testing::UnorderedElementsAre;
@@ -265,5 +266,44 @@ IN_PROC_BROWSER_TEST_P(SingleClientSkillsSyncTest, ShouldMergeRemoteData) {
                                Pointee(HasSkill("skill2", "icon2", "prompt2"))))
           .Wait());
 }
+
+// TODO(crbug.com/471795213): add a test to verify that skills can't be created
+// when sync is disabled.
+
+// ChromeOS does not support signout.
+#if !BUILDFLAG(IS_CHROMEOS)
+IN_PROC_BROWSER_TEST_P(SingleClientSkillsSyncTest,
+                       ShouldDeleteAllDataOnDisableSync) {
+  InjectSpecificsToFakeServer(
+      CreateSkillSpecifics(base::Uuid::GenerateRandomV4().AsLowercaseString(),
+                           "skill1", "icon1", "prompt1"));
+  InjectSpecificsToFakeServer(
+      CreateSkillSpecifics(base::Uuid::GenerateRandomV4().AsLowercaseString(),
+                           "skill2", "icon2", "prompt2"));
+
+  ASSERT_TRUE(SetupSync());
+
+  ASSERT_THAT(
+      GetSkillsService().GetSkills(),
+      UnorderedElementsAre(Pointee(HasSkill("skill1", "icon1", "prompt1")),
+                           Pointee(HasSkill("skill2", "icon2", "prompt2"))));
+
+  // Sign out the primary account to disable sync and verify that all data was
+  // deleted.
+  GetClient(0)->SignOutPrimaryAccount();
+
+  EXPECT_TRUE(SkillsServiceChecker(GetSkillsService(), IsEmpty()).Wait());
+
+  // Sign in again to re-enable sync and verify that the data was re-synced.
+  ASSERT_TRUE(GetClient(0)->SignInPrimaryAccount());
+
+  EXPECT_TRUE(
+      SkillsServiceChecker(
+          GetSkillsService(),
+          UnorderedElementsAre(Pointee(HasSkill("skill1", "icon1", "prompt1")),
+                               Pointee(HasSkill("skill2", "icon2", "prompt2"))))
+          .Wait());
+}
+#endif  // !BUILDFLAG(IS_CHROMEOS)
 
 }  // namespace
