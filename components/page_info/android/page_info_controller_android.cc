4227b6797f5caf7fadce3efa2d39fb57a4bd6d9d
Antonio Sartori <antoniosartori@chromium.org>
2026-01-19 06:21:58 -0800

    Use GeolocationContentSettingsType in components/page_info
    This is a no-op refactoring that simplifies
    components/page_info/android/page_info_controller_android.cc
    
    Bug: 476929911
    Change-Id: Ifa7aedd0fbe69a2560069fd96df90b9eb5b8a8bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7486780
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1571164}

--- components/page_info/android/page_info_controller_android.cc
+++ components/page_info/android/page_info_controller_android.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/browser/permission_settings_registry.h"
 #include "components/content_settings/core/common/content_settings.h"
 #include "components/content_settings/core/common/content_settings_types.h"
+#include "components/content_settings/core/common/content_settings_utils.h"
 #include "components/content_settings/core/common/features.h"
 #include "components/page_info/android/page_info_client.h"
 #include "components/page_info/core/features.h"
@@ -159,13 +160,8 @@ void PageInfoControllerAndroid::SetPermissionInfo(
   // a particular order, but only if their value is different from the
   // default. This order comes from https://crbug.com/610358.
   std::vector<ContentSettingsType> permissions_to_display;
-  if (base::FeatureList::IsEnabled(
-          content_settings::features::kApproximateGeolocationPermission)) {
-    permissions_to_display.push_back(
-        ContentSettingsType::GEOLOCATION_WITH_OPTIONS);
-  } else {
-    permissions_to_display.push_back(ContentSettingsType::GEOLOCATION);
-  }
+  permissions_to_display.push_back(
+      content_settings::GeolocationContentSettingsType());
   permissions_to_display.push_back(ContentSettingsType::MEDIASTREAM_CAMERA);
   permissions_to_display.push_back(ContentSettingsType::MEDIASTREAM_MIC);
   permissions_to_display.push_back(ContentSettingsType::NOTIFICATIONS);
