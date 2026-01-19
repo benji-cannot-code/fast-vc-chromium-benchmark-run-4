fef491eb732ca149e37ab06a2b1b20950b8acca4
Jérôme Lebel <jlebel@chromium.org>
2026-01-19 07:18:37 -0800

    [iOS] Add SystemIdentityManagerObserverBridge
    Add SystemIdentityManagerObserverBridge Obj-C class to listen for
    SystemIdentityManager events.
    
    This class is used in crrev.com/c/7493478.
    
    Bug: 476947704
    Change-Id: Ia5035a0bc85b47b114031521d704d8595315579a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7493458
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1571190}

---
+++ ios/chrome/browser/signin/model/system_identity_manager_observer_bridge.h
@@ -0,0 +1,43 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SIGNIN_MODEL_SYSTEM_IDENTITY_MANAGER_OBSERVER_BRIDGE_H_
+#define IOS_CHROME_BROWSER_SIGNIN_MODEL_SYSTEM_IDENTITY_MANAGER_OBSERVER_BRIDGE_H_
+
+#import <Foundation/Foundation.h>
+
+#import "base/scoped_observation.h"
+#import "ios/chrome/browser/signin/model/system_identity_manager.h"
+#import "ios/chrome/browser/signin/model/system_identity_manager_observer.h"
+
+// Objective-C protocol mirroring SystemIdentityManagerObserver.
+@protocol SystemIdentityManagerObserving <NSObject>
+
+@optional
+- (void)onIdentityListChanged;
+- (void)onIdentityUpdated:(id<SystemIdentity>)identity;
+
+@end
+
+// Simple observer bridge that forwards all events to its delegate observer.
+class SystemIdentityManagerObserverBridge
+    : public SystemIdentityManagerObserver {
+ public:
+  SystemIdentityManagerObserverBridge(
+      SystemIdentityManager* service,
+      id<SystemIdentityManagerObserving> observer);
+  ~SystemIdentityManagerObserverBridge() override;
+
+  // SystemIdentityManagerObserver implementation.
+  void OnIdentityListChanged() override;
+  void OnIdentityUpdated(id<SystemIdentity> identity) override;
+
+ private:
+  __weak id<SystemIdentityManagerObserving> observer_ = nil;
+  base::ScopedObservation<SystemIdentityManager, SystemIdentityManagerObserver>
+      scoped_observation_{this};
+};
+
+#endif  // IOS_CHROME_BROWSER_SIGNIN_MODEL_SYSTEM_IDENTITY_MANAGER_OBSERVER_BRIDGE_H_
