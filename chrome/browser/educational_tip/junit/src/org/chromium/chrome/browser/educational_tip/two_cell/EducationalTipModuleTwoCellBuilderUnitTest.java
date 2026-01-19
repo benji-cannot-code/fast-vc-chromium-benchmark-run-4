b0a7fc7785ff53f79e05317acb2b3da9f3b7eacd
Gazal <agazal@google.com>
2026-01-19 11:55:27 -0800

    [SetupList] Ensure correct order of manually ranked items
    This change ensures that all modules returning a rank from getHasManualOrder() are collected and then explicitly sorted according to these ranks within createInputContextForRankingAndUpdateManuallyRankedModuleList.
    
    The previous registration order of modules was not guaranteed as ModuleRegistry used a HashMap. This meant that the relative order of Setup List items, intended to be sequential, was lost.
    
    Bug: 469425754, 470405750
    Change-Id: I6109a74bff3336cb7e7e808cfd2c142bc0105019
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7477601
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Gazal Agarwal <agazal@google.com>
    Auto-Submit: Gazal Agarwal <agazal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1571316}

--- chrome/browser/educational_tip/junit/src/org/chromium/chrome/browser/educational_tip/two_cell/EducationalTipModuleTwoCellBuilderUnitTest.java
+++ chrome/browser/educational_tip/junit/src/org/chromium/chrome/browser/educational_tip/two_cell/EducationalTipModuleTwoCellBuilderUnitTest.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.educational_tip.two_cell;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
@@ -34,6 +35,7 @@ import org.chromium.chrome.browser.educational_tip.EducationTipModuleActionDeleg
 import org.chromium.chrome.browser.educational_tip.R;
 import org.chromium.chrome.browser.magic_stack.ModuleDelegate;
 import org.chromium.chrome.browser.magic_stack.ModuleProvider;
+import org.chromium.chrome.browser.setup_list.SetupListManager;
 import org.chromium.components.segmentation_platform.InputContext;
 import org.chromium.ui.shadows.ShadowAppCompatResources;
 
@@ -48,21 +50,25 @@ public class EducationalTipModuleTwoCellBuilderUnitTest {
     @Mock private ModuleDelegate mModuleDelegate;
     @Mock private Callback<ModuleProvider> mBuildCallback;
     @Mock private EducationTipModuleActionDelegate mActionDelegate;
+    @Mock private SetupListManager mSetupListManager;
 
     private Context mContext;
     private EducationalTipModuleTwoCellBuilder mModuleBuilder;
 
     @Before
     public void setUp() {
+        SetupListManager.setInstanceForTesting(mSetupListManager);
+        when(mSetupListManager.isSetupListActive()).thenReturn(true);
+
         mContext = ApplicationProvider.getApplicationContext();
         when(mActionDelegate.getContext()).thenReturn(mContext);
         mContext.setTheme(R.style.Theme_BrowserUI_DayNight);
-        mModuleBuilder = new EducationalTipModuleTwoCellBuilder(mActionDelegate);
     }
 
     @Test
     @SmallTest
     public void testBuild() {
+        mModuleBuilder = new EducationalTipModuleTwoCellBuilder(mActionDelegate);
         assertTrue(mModuleBuilder.build(mModuleDelegate, mBuildCallback));
         verify(mBuildCallback).onResult(any(EducationalTipModuleTwoCellCoordinator.class));
     }
@@ -70,6 +76,7 @@ public class EducationalTipModuleTwoCellBuilderUnitTest {
     @Test
     @SmallTest
     public void testCreateView() {
+        mModuleBuilder = new EducationalTipModuleTwoCellBuilder(mActionDelegate);
         ViewGroup parentView = new FrameLayout(mContext);
         ViewGroup moduleView = mModuleBuilder.createView(parentView);
         assertNotNull(moduleView);
@@ -78,13 +85,29 @@ public class EducationalTipModuleTwoCellBuilderUnitTest {
 
     @Test
     @SmallTest
-    public void testHasManualOrdering() {
-        assertTrue(mModuleBuilder.hasManualOrdering());
+    public void testGetManualRank_SetupListActive() {
+        when(mSetupListManager.isSetupListActive()).thenReturn(true);
+        when(mSetupListManager.shouldShowTwoCellLayout()).thenReturn(true);
+        mModuleBuilder = new EducationalTipModuleTwoCellBuilder(mActionDelegate);
+        Integer manualOrder = mModuleBuilder.getManualRank();
+        assertNotNull("Manual order should be present when setup list is active", manualOrder);
+        assertEquals(0, manualOrder.intValue());
+    }
+
+    @Test
+    @SmallTest
+    public void testGetManualRank_SetupListInActive() {
+        when(mSetupListManager.isSetupListActive()).thenReturn(false);
+        when(mSetupListManager.shouldShowTwoCellLayout()).thenReturn(true);
+        mModuleBuilder = new EducationalTipModuleTwoCellBuilder(mActionDelegate);
+        Integer manualOrder = mModuleBuilder.getManualRank();
+        assertNull("Manual order should be null when setup list is inactive", manualOrder);
     }
 
     @Test
     @SmallTest
     public void testCreateInputContext() {
+        mModuleBuilder = new EducationalTipModuleTwoCellBuilder(mActionDelegate);
         InputContext inputContext = mModuleBuilder.createInputContext();
         assertNull(inputContext);
     }
