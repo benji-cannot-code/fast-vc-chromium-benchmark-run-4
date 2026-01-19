4c9550b874a0f54cce65c05660e81c0d0ca041ca
Sylvain Defresne <sdefresne@chromium.org>
2026-01-19 04:58:55 -0800

    Reland "Skip over automatic items when navigating back/forward"
    This is a reland of commit 10cee1615fc312b75edd895a0ef7f5e41099948c
    
    The original CL was reverted as NavigationManagerDetachedModeTest
    tests were incorrectly marked as TEST_P(...) when they are not
    parameterised. This was fixed by reverting them back to TEST_F(...).
    
    Additionally, renamed the feature flag to make it clear that it is
    a kill switch (and flip the meaning). The code is unchanged as an
    helper function is added that keep the same semantic as the flag.
    
    Original change's description:
    > [ios][web] Skip over automatic items when navigating back/forward
    >
    > When navigating back/forward, skip automatic items (i.e. those that
    > are inserted in JS via pushState(...) or replaceState(...) methods).
    > This ensure that the back/forward button are in sync between the
    > //ios/web and WebKit.
    >
    > This is behind a feature flag enabled by default that can be used
    > as a way to disable the new behaviour if it causes issues.
    >
    > Bug: 464261378
    > Change-Id: I00e791c3f25c4f48e48a59f693f52daea9c47e62
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7265250
    > Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    > Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1569114}
    
    Bug: 464261378
    Change-Id: I256d0e7927615e521713dc83e60ed55a91939bf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7483470
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1571122}

--- ios/web/navigation/navigation_manager_impl.h
+++ ios/web/navigation/navigation_manager_impl.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <vector>
 
+#include "base/feature_list.h"
 #include "base/functional/callback.h"
 #include "base/gtest_prod_util.h"
 #import "base/memory/raw_ptr.h"
@@ -36,6 +37,16 @@ class NavigationStorage;
 class BrowserState;
 class NavigationItem;
 class NavigationManagerDelegate;
+
+// Feature flag controlling whether the logic to skip automatic navigation
+// history item during "back" and "forward" navigations is enabled.
+//
+// See https://crbug.com/464261378 for details.
+BASE_DECLARE_FEATURE(kSkipAutomaticNavigationInBackForwardListKillSwitch);
+
+// Helper function to check whether SkipAutomaticNavigationInBackForwardList
+// should be enabled or not.
+bool SkipAutomaticNavigationInBackForwardList();
 
 // Name of UMA histogram to log the number of items Navigation Manager was
 // requested to restore. 100 is logged when the number of navigation items is
@@ -331,6 +342,10 @@ class NavigationManagerImpl final : public NavigationManager {
 
   // Common implementation of GoBack(), GoForward() and GoToIndex(int).
   void GoTo(GoToParams params);
+
+  // Returns the index for a navigation given by `params` skipping over
+  // automatic items if any are present.
+  int IndexForParams(GoToParams params);
 
   // Appends a new session blob fetcher with given source.
   void AppendSessionDataBlobFetcher(SessionDataBlobFetcher loader,
