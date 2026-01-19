ce3a62bdd7f92f8f95dae2649565742f97c1412d
Jood <jood@google.com>
2026-01-19 06:15:17 -0800

    [iOS] Handle edge case of sign-out before migration confirmation
    Fixed: 476382080
    Change-Id: I52b3d7a65b96986090918578f205a0601a4a00bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7493538
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Jood Hajeer <jood@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1571163}

--- ios/chrome/app/profile/multi_profile_forced_migration_profile_agent.mm
+++ ios/chrome/app/profile/multi_profile_forced_migration_profile_agent.mm
@@ -115,7 +115,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   id<SystemIdentity> systemIdentity =
       authenticationService->GetPrimaryIdentity(signin::ConsentLevel::kSignin);
-  CHECK(systemIdentity, base::NotFatalUntil::M148);
+  if (!systemIdentity) {
+    // If the migrated account signs out before the confirmation dialog is shown
+    // (a rare case), skip showing the dialog.
+    localState->SetBoolean(prefs::kMultiProfileForcedMigrationDone, false);
+    return;
+  }
 
   SystemIdentityManager* systemIdentityManager =
       GetApplicationContext()->GetSystemIdentityManager();
