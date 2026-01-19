c6a093415fddf84d3fc150023207852ea9034087
Claudio DeSouza <cdesouza@chromium.org>
2026-01-19 09:21:36 -0800

    Fix unintentional glic unguarded inclusion
    The header being removed was already included under `ENABLE_GLIC` for
    these tests.
    
    Bug: 471421204
    Change-Id: I89aaa7a5e5e1c685139786e96e21a290e8d8ba4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7489143
    Commit-Queue: Claudio DeSouza <cdesouza@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1571258}

--- chrome/test/data/webui/settings/settings_browsertest.cc
+++ chrome/test/data/webui/settings/settings_browsertest.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/config/coverage/buildflags.h"
 #include "chrome/browser/actor/actor_keyed_service.h"
 #include "chrome/browser/actor/actor_policy_checker.h"
-#include "chrome/browser/glic/test_support/glic_test_util.h"
 #include "chrome/browser/preloading/preloading_features.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
