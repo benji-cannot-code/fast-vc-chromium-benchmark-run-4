0ec07cb01efdce814437852789e17962537e685c
Sophie Crowley <crowleso@google.com>
2026-01-19 04:49:43 -0800

    [Signin][Android] Add kSigninLevelUpButton feature flag
    This flag will be used for replacing the signed out avatar with a signin
    button on Android.
    
    Bug: 476038350
    Change-Id: I31f7058ec5b3c36e15074167d8af3d0c5ab74139
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7459709
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Sophie Crowley <crowleso@google.com>
    Reviewed-by: Samar Chehade <samarchehade@google.com>
    Cr-Commit-Position: refs/heads/main@{#1571111}

--- components/signin/public/base/signin_feature_map.cc
+++ components/signin/public/base/signin_feature_map.cc
@@ -38,6 +38,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &switches::kChromeAndroidIdentitySurveyNtpAvatar,
     &switches::kChromeAndroidIdentitySurveyNtpPromo,
     &switches::kChromeAndroidIdentitySurveyBookmarkPromo,
+    &switches::kSigninLevelUpButton,
 };
 
 // static
