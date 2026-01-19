79df6887cfb769d1450708073acadd60ae91be49
Clemens Backes <clemensb@chromium.org>
2026-01-19 08:38:33 -0800

    Rebaseline two devtools issue tests
    Update the source position and make sure that the `buffer` property is
    accessed (which creates and returns the `SharedArrayBuffer`).
    
    This reverts commit 5bbca7862f4ebe1444ad1f5b81a34f08f046952b and part of
    93ddfed12d036d32456ae21aac129dee19d1f7a3 (by reintroducing the `buffer`
    access).
    
    R=jkummerow@chromium.org
    
    Bug: 475195978
    Change-Id: I4c803c1a49674a0578dec664ca2ad0189ccf13c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7460968
    Commit-Queue: Clemens Backes <clemensb@chromium.org>
    Reviewed-by: Jakob Kummerow <jkummerow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1571239}

--- third_party/blink/web_tests/http/tests/inspector-protocol/issues/sab-creation-issue.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/issues/sab-creation-issue.js
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   await dp.Audits.enable();
   session.evaluate(
-      `new WebAssembly.Memory({ shared:true, initial:0, maximum:0 });`);
+      `new WebAssembly.Memory({ shared:true, initial:0, maximum:0 }).buffer;`);
   const issue = await dp.Audits.onceIssueAdded();
 
   testRunner.log(issue.params, 'Inspector issue: ');
