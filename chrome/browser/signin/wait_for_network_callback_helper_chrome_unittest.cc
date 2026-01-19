e561365b0971e4acd30a36e3dc21571de0b27b8e
Ali Hijazi <ahijazi@chromium.org>
2026-01-19 07:11:54 -0800

    signin: Collect metrics for DelayNetworkCall() network discrepancy
    This commit introduces UMA metrics to measure the size of the
    `DelayNetworkCall()` queue in `WaitForNetworkCallbackHelperChrome`.
    
    The `Signin.DelayedNetworkCallQueueSize.{EffectiveConnectionType}`
    histogram is added to track how often network requests are delayed
    despite an apparent network connection. This data will help in
    understanding the discrepancy between `NetworkConnectionTracker` status
    and actual network reachability.
    
    A `base::RepeatingTimer` is used to periodically sample the queue size
    and report it, split by effective connection type.
    
    Bug: 466303351
    Change-Id: Icd1d5a9d6cfa482fcdbbb599c99f88dd9776a39f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7415032
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1571187}

--- chrome/browser/signin/wait_for_network_callback_helper_chrome_unittest.cc
+++ chrome/browser/signin/wait_for_network_callback_helper_chrome_unittest.cc
@@ -5,9 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/signin/wait_for_network_callback_helper_chrome.h"
 
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/task_environment.h"
 #include "base/test/test_future.h"
+#include "chrome/browser/browser_process.h"
 #include "services/network/test/test_network_connection_tracker.h"
+#include "services/network/test/test_network_quality_tracker.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 class WaitForNetworkCallbackHelperChromeTest : public testing::Test {
@@ -25,9 +28,26 @@ class WaitForNetworkCallbackHelperChromeTest : public testing::Test {
         connection_type);
   }
 
-  base::test::TaskEnvironment task_environment_;
-  WaitForNetworkCallbackHelperChrome helper_;
+  base::test::TaskEnvironment task_environment_{
+      base::test::TaskEnvironment::TimeSource::MOCK_TIME};
+  WaitForNetworkCallbackHelperChrome helper_{
+      /*should_disable_metrics_for_testing=*/false};
 };
+
+TEST_F(WaitForNetworkCallbackHelperChromeTest,
+       MetricsCollection_DelayedNetworkCall) {
+  base::HistogramTester histogram_tester;
+  SetUpNetworkConnection(true, network::mojom::ConnectionType::CONNECTION_NONE);
+  g_browser_process->network_quality_tracker()
+      ->ReportEffectiveConnectionTypeForTesting(
+          net::EFFECTIVE_CONNECTION_TYPE_2G);
+
+  helper_.DelayNetworkCall(base::DoNothing());
+  task_environment_.FastForwardBy(base::Minutes(30));
+
+  histogram_tester.ExpectUniqueSample("Signin.DelayedNetworkCallQueueSize.2G",
+                                      1, 1);
+}
 
 TEST_F(WaitForNetworkCallbackHelperChromeTest,
        DelayNetworkCallRunsImmediatelyWithNetwork) {
