d14ab3acc9840c2d489f30c9454683f40a53bb36
Morten Stenshorne <mstensho@chromium.org>
2026-01-19 03:12:38 -0800

    [anchor] Main thread animation if first frame doesn't change anything.
    This is a follow-up to https://crrev.com/c/7123703 , but it was blocked
    on other problems, which got fixed by https://crrev.com/c/7239030 . We
    can now get anchor animations that don't change any styles right away
    (i.e. no visual updates until further into the animation) to trigger
    relayout of anchor-positioned elements when needed.
    
    If the very first animation frame actually has style changes that affect
    layout (e.g. transforms that affect an anchor), we still haven't moved
    the work off the main thread, and we'll therefore lay out, and by doing
    that, HasRunningAnchorTransformAnimation() in PhysicalFragment will
    return true, which in turn will make sure that the animation keeps
    running on the main thread. This was already working.
    
    On the other hand, if the first frame of an animation *doesn't* change
    style that affects layout, the animation would be moved to the
    compositor thread, and we'd never hear from it again, not even when any
    style changes actually occur later on. To fix this, detect that a
    transform-related animation wants to run on the compositor, mark for
    layout, so that the physical fragments get updated for
    HasRunningAnchorTransformAnimation(), so that we'll be able to keep the
    animation on the main thread
    
    Note: The test included would only fail with run_wpt_tests, not
    run_web_tests.
    
    Bug: 382294252
    Change-Id: I193c354f0e28b4b908d75ce1b200c2c8142be43c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7451459
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1571069}

--- third_party/blink/renderer/core/style/computed_style.h
+++ third_party/blink/renderer/core/style/computed_style.h
@@ -2055,12 +2055,15 @@ class ComputedStyle final : public ComputedStyleBase {
            HasCurrentTransformRelatedAnimation() ||
            HasCurrentFilterAnimation() || HasCurrentBackdropFilterAnimation();
   }
-  bool RequiresPropertyNodeForAnimation() const {
-    return IsRunningOpacityAnimationOnCompositor() ||
-           IsRunningTransformAnimationOnCompositor() ||
+  bool IsRunningTransformRelatedAnimationOnCompositor() const {
+    return IsRunningTransformAnimationOnCompositor() ||
            IsRunningScaleAnimationOnCompositor() ||
            IsRunningRotateAnimationOnCompositor() ||
-           IsRunningTranslateAnimationOnCompositor() ||
+           IsRunningTranslateAnimationOnCompositor();
+  }
+  bool RequiresPropertyNodeForAnimation() const {
+    return IsRunningOpacityAnimationOnCompositor() ||
+           IsRunningTransformRelatedAnimationOnCompositor() ||
            IsRunningFilterAnimationOnCompositor() ||
            IsRunningBackdropFilterAnimationOnCompositor();
   }
