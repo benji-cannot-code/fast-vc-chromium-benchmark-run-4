99d05726f9d999aa462b9a12ea3e10fc09d9b314
Kent Tamura <tkent@chromium.org>
2026-01-19 19:07:12 -0800

    FitText: Merge two properties into one
    This CL removes `text-grow` and `text-shrink` properties, and introduces
    the `text-fit` property that covers both functionalities.
    
    New Property Syntax:
    text-fit: [ none | grow | shrink]
              [ consistent | per-line | per-line-all]?
              <percentage>?
    
    This CL doesn't change the last component from <length> to <percentage>.
    It will be addressed in a following CL.
    
    * blink::ComputedStyle had two blink::FitText instances. Now it has
     a single blink::FitText, and blink::FitText has a type field of
     which value is kNone, kGrow, or kShrink.
    
    * wpt_internal/css/fit-width-text/
      text-shrink-valid.html, text-shrink-invalid.html, and text-shrink-computed.html tests are merged into text-grow-*.html.
      We're going to rename tests in this directory later.
    
    Bug: 417306102
    Change-Id: Ie72590ea479ab84bf7b004823e6fbf849648274d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7486532
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1571404}

--- third_party/blink/renderer/core/css/properties/longhands/longhands_custom.cc
+++ third_party/blink/renderer/core/css/properties/longhands/longhands_custom.cc
@@ -10231,19 +10231,19 @@ const CSSValue* TextDecorationThickness::CSSValueFromComputedStyleInternal(
       style.GetTextDecorationThickness().Thickness(), style);
 }
 
-const CSSValue* TextGrow::ParseSingleValue(
+const CSSValue* TextFit::ParseSingleValue(
     CSSParserTokenStream& stream,
     const CSSParserContext& context,
     CSSParserLocalContext& local_context) const {
   return css_parsing_utils::ConsumeFitText(stream, context, local_context);
 }
 
-const CSSValue* TextGrow::CSSValueFromComputedStyleInternal(
+const CSSValue* TextFit::CSSValueFromComputedStyleInternal(
     const ComputedStyle& style,
     const LayoutObject*,
     bool allow_visited_style,
     CSSValuePhase value_phase) const {
-  return ComputedStyleUtils::ValueForFitText(style, style.TextGrow());
+  return ComputedStyleUtils::ValueForFitText(style, style.TextFit());
 }
 const CSSValue* TextIndent::ParseSingleValue(
     CSSParserTokenStream& stream,
@@ -10437,21 +10437,6 @@ const CSSValue* TextShadow::CSSValueFromComputedStyleInternal(
 
 bool TextShadow::IsAffectedByCurrentColor(const ComputedStyle& style) const {
   return style.ShadowListHasCurrentColor(style.TextShadow());
-}
-
-const CSSValue* TextShrink::ParseSingleValue(
-    CSSParserTokenStream& stream,
-    const CSSParserContext& context,
-    CSSParserLocalContext& local_context) const {
-  return css_parsing_utils::ConsumeFitText(stream, context, local_context);
-}
-
-const CSSValue* TextShrink::CSSValueFromComputedStyleInternal(
-    const ComputedStyle& style,
-    const LayoutObject*,
-    bool allow_visited_style,
-    CSSValuePhase value_phase) const {
-  return ComputedStyleUtils::ValueForFitText(style, style.TextShrink());
 }
 
 const CSSValue* TextSizeAdjust::ParseSingleValue(
