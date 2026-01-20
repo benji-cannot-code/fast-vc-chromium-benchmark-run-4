abf9ebf1f2defce6396e822493d6975c665d2f49
Di Wu <diwux@google.com>
2026-01-20 11:11:17 -0800

    Add COLUMN_FLAGS support to TestDocumentsProvider
    This CL implements the `DocumentsContract.Document.COLUMN_FLAGS`
    projection in `TestDocumentsProvider`, addressing an existing TODO.
    
    Previously, the provider did not return these flags, which could cause
    clients (like `ContentUriUtils`) to incorrectly assess document
    capabilities.
    
    The flags are now calculated based on the provider's actual
    implementation:
    * FLAG_SUPPORTS_DELETE: Set for all documents.
    * FLAG_DIR_SUPPORTS_CREATE: Set only for directories.
    * FLAG_SUPPORTS_WRITE: Set only for files (directories cannot be opened
      in write mode).
    
    Bug: b:443222522
    Test: CQ
    Change-Id: I61aeb932917cd3d5745e3f8e90ed0ca4e76f88ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7489049
    Reviewed-by: Kay Lin <kaiyilin@google.com>
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1571769}

--- base/test/android/java/src/org/chromium/base/TestDocumentsProvider.java
+++ base/test/android/java/src/org/chromium/base/TestDocumentsProvider.java
@@ -49,7 +49,6 @@ import java.net.URLDecoder;
  * </ul>
  */
 public class TestDocumentsProvider extends DocumentsProvider {
-    // TODO(crbug.com/443222522): Implement COLUMN_FLAGS projection
     private static final String[] DEFAULT_PROJECTION_FOR_TEST =
             new String[] {
                 DocumentsContract.Document.COLUMN_DOCUMENT_ID,
@@ -57,6 +56,7 @@ public class TestDocumentsProvider extends DocumentsProvider {
                 DocumentsContract.Document.COLUMN_MIME_TYPE,
                 DocumentsContract.Document.COLUMN_SIZE,
                 DocumentsContract.Document.COLUMN_LAST_MODIFIED,
+                DocumentsContract.Document.COLUMN_FLAGS,
             };
 
     // The authority cannot be static as its initialization depends on a Context. A ContentProvider
@@ -133,6 +133,14 @@ public class TestDocumentsProvider extends DocumentsProvider {
                         row[i] = file.length();
                     } else if (DocumentsContract.Document.COLUMN_LAST_MODIFIED.equals(colName)) {
                         row[i] = file.lastModified();
+                    } else if (DocumentsContract.Document.COLUMN_FLAGS.equals(colName)) {
+                        int flags = DocumentsContract.Document.FLAG_SUPPORTS_DELETE;
+                        if (file.isDirectory()) {
+                            flags |= DocumentsContract.Document.FLAG_DIR_SUPPORTS_CREATE;
+                        } else {
+                            flags |= DocumentsContract.Document.FLAG_SUPPORTS_WRITE;
+                        }
+                        row[i] = flags;
                     }
                 }
                 cursor.addRow(row);
