fe95512e03ee40ec956e2677454f722cddbced08
Alison Gale <agale@chromium.org>
2026-01-20 10:58:22 -0800

    [Vertical Tabs] Log Tabs.TabCount by vertical/horizontal
    Bug: 474421976
    Change-Id: I4937d1889c6942cb757041164bcd7f2802fe4eed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7488325
    Commit-Queue: Alison Gale <agale@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1571757}

--- chrome/browser/metrics/tab_stats/tab_stats_tracker_unittest.cc
+++ chrome/browser/metrics/tab_stats/tab_stats_tracker_unittest.cc
@@ -19,13 +19,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/strcat.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/power_monitor_test.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
+#include "chrome/browser/ui/tabs/features.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
 #include "components/metrics/daily_event.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/testing_pref_service.h"
+#include "components/sync_preferences/testing_pref_service_syncable.h"
 #include "content/public/browser/media_player_id.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_observer.h"
@@ -44,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_enums.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "chrome/browser/ui/tabs/tab_strip_prefs.h"
 #include "chrome/test/base/test_browser_window.h"
 #endif
 
@@ -318,6 +322,7 @@ class TabStatsTrackerTest : public ChromeRenderViewHostTestHarness {
     tab_strip_modifier_ = std::make_unique<TabStripModifier>(
         tab_strip_interface_.get(), test_tab_model_.get());
 #else
+    scoped_feature_.InitWithFeatures({tabs::kVerticalTabs}, {});
     browser_ = CreateBrowserWithTestWindowForParams(
         Browser::CreateParams(profile(), true));
     tab_strip_interface_ = std::make_unique<TabStripInterface>(browser_.get());
@@ -410,6 +415,8 @@ class TabStatsTrackerTest : public ChromeRenderViewHostTestHarness {
   base::HistogramTester histogram_tester_;
 
   TestingPrefServiceSimple pref_service_;
+
+  base::test::ScopedFeatureList scoped_feature_;
 
 #if BUILDFLAG(IS_ANDROID)
   std::unique_ptr<OwningTestTabModel> test_tab_model_;
@@ -804,6 +811,12 @@ TEST_F(TabStatsTrackerTest, HeartbeatMetrics) {
 
   ExpectBucketedSample(UmaStatsReportingDelegate::kTabCountHistogramName,
                        expected_tab_count, 1);
+#if !BUILDFLAG(IS_ANDROID)
+  ExpectBucketedSample(
+      base::StrCat({UmaStatsReportingDelegate::kTabCountHistogramName,
+                    ".HorizontalTabStrip"}),
+      expected_tab_count, 1);
+#endif
   ExpectBucketedSample(UmaStatsReportingDelegate::kWindowCountHistogramName,
                        expected_window_count, 1);
 
@@ -816,6 +829,22 @@ TEST_F(TabStatsTrackerTest, HeartbeatMetrics) {
   ExpectBucketedSample(UmaStatsReportingDelegate::kWindowCountHistogramName,
                        expected_window_count, 1);
 }
+
+#if !BUILDFLAG(IS_ANDROID)
+TEST_F(TabStatsTrackerTest, HeartbeatMetricsWithVerticalTabs) {
+  profile()->GetPrefs()->SetBoolean(prefs::kVerticalTabsEnabled, true);
+
+  size_t expected_tab_count =
+      tab_stats_tracker_->AddTabs(12, this, tab_strip_modifier_.get());
+
+  tab_stats_tracker_->OnHeartbeatEvent();
+
+  ExpectBucketedSample(
+      base::StrCat({UmaStatsReportingDelegate::kTabCountHistogramName,
+                    ".VerticalTabStrip"}),
+      expected_tab_count, 1);
+}
+#endif
 
 TEST_F(TabStatsTrackerTest, VideoPlayingInTab) {
   content::WebContentsTester* const contents_tester =
