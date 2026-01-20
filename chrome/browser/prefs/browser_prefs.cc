71f479d250aed6a7b77881baa2077599cbb5ce95
Victor Hugo Vianna Silva <victorvianna@google.com>
2026-01-20 05:24:55 -0800

    Delete MigrateDefaultProfileStorePref()
    This pref migration has been in place for a year now and can be
    removed.
    
    Fixed: 453650634
    Change-Id: I5f68b1d6770df5cd71b6eb66c19b27cb8012872a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7061997
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1571575}

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -2338,12 +2338,6 @@ void MigrateObsoleteProfilePrefs(PrefService* profile_prefs,
   MigrateDefaultBrowserLastDeclinedPref(profile_prefs);
 #endif
 
-#if !BUILDFLAG(IS_ANDROID)
-  // Added 01/2025.
-  password_manager::features_util::MigrateDefaultProfileStorePref(
-      profile_prefs);
-#endif  // !BUILDFLAG(IS_ANDROID)
-
   // Added 01/2025.
   profile_prefs->ClearPref(kCompactModeEnabled);
 
--- components/password_manager/core/browser/password_manager_metrics_util.h
+++ components/password_manager/core/browser/password_manager_metrics_util.h
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/common/credential_manager_types.h"
 #include "services/metrics/public/cpp/ukm_source_id.h"
 
+class PrefService;
+
 namespace password_manager::metrics_util {
 
 // These values are persisted to logs. Entries should not be renumbered and
