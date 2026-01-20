8bfc7cc564784d5ac5685e45581db318dcfe1c35
hiroki oshima <hiroki.oshima@gmail.com>
2026-01-19 23:13:49 -0800

    fix HoldingSpaceTrayChildBubbbleTestBase dangling pointer
    Bug: 40212619
    Change-Id: Ifee9dca97257e351137a190e11cc3e716241c6c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7454862
    Reviewed-by: Di Wu <diwux@google.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1571453}

--- ash/system/holding_space/holding_space_tray_child_bubble_unittest.cc
+++ ash/system/holding_space/holding_space_tray_child_bubble_unittest.cc
@@ -60,6 +60,7 @@ class HoldingSpaceTrayChildBubbleTestBase : public HoldingSpaceAshTestBase {
   }
 
   void TearDown() override {
+    child_bubble_ = nullptr;
     widget_.reset();
     view_delegate_.reset();
 
@@ -75,8 +76,7 @@ class HoldingSpaceTrayChildBubbleTestBase : public HoldingSpaceAshTestBase {
 
   views::UniqueWidgetPtr widget_;
   std::unique_ptr<HoldingSpaceViewDelegate> view_delegate_;
-  raw_ptr<HoldingSpaceTrayChildBubble, DanglingUntriaged> child_bubble_ =
-      nullptr;
+  raw_ptr<HoldingSpaceTrayChildBubble> child_bubble_ = nullptr;
 };
 
 // Tests -----------------------------------------------------------------------
