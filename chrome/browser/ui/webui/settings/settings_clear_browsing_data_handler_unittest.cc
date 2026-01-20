8d57784962de4c7c2772774710d403558b819b07
Angela Novakovic <novakovica@google.com>
2026-01-20 12:48:44 -0800

    [iOS][PRDBD] Moves Quick Delete strings from /chrome/app to /components
    This CL moves strings (go/qd-strings) to /components such that they can
    be reused in iOS for the `kPasswordRemovalFromDeleteBrowsingData`
    feature.
    
    Design doc: go/bling-delete-browsing-data
    
    Fixed: 474333819
    Change-Id: I84fcdf6084966187cee48f78505699d8c4805208
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7416758
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Teresa Mao <temao@chromium.org>
    Commit-Queue: Angela Novakovic <novakovica@google.com>
    Reviewed-by: Noémie St-Onge <noemies@google.com>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1571829}

--- chrome/browser/ui/webui/settings/settings_clear_browsing_data_handler_unittest.cc
+++ chrome/browser/ui/webui/settings/settings_clear_browsing_data_handler_unittest.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/browsing_data/core/pref_names.h"
 #include "components/search_engines/template_url.h"
 #include "components/search_engines/template_url_service.h"
+#include "components/strings/grit/components_strings.h"
 #include "components/sync_preferences/testing_pref_service_syncable.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_task_environment.h"
