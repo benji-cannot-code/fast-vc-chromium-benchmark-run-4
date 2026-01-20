6bfb248649710284de88d4afdcf49d9fd7130bd9
Fredrik Söderquist <fs@opera.com>
2026-01-20 15:19:02 -0800

    Resolve UNSAFE_TODO in StrokeData::SetLineDash
    Use base::HeapArray<float> instead of std::unique_ptr<SkScalar[]>.
    
    Bug: 451652367
    Change-Id: I79781e367e0d45f3cbd5fac914f5277a83adc737
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7497133
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1571922}

--- third_party/blink/renderer/platform/geometry/stroke_data.cc
+++ third_party/blink/renderer/platform/geometry/stroke_data.cc
@@ -29,9 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/geometry/stroke_data.h"
 
-#include <memory>
-
-#include "base/compiler_specific.h"
+#include "base/containers/heap_array.h"
 
 namespace blink {
 
@@ -43,13 +41,13 @@ void StrokeData::SetLineDash(const DashArray& dashes, float dash_offset) {
   }
 
   wtf_size_t count = !(dash_length % 2) ? dash_length : dash_length * 2;
-  auto intervals = std::make_unique<SkScalar[]>(count);
+  auto intervals = base::HeapArray<float>::Uninit(count);
 
   for (wtf_size_t i = 0; i < count; i++) {
-    UNSAFE_TODO(intervals[i]) = dashes[i % dash_length];
+    intervals[i] = dashes[i % dash_length];
   }
 
-  dash_ = cc::PathEffect::MakeDash(intervals.get(), count, dash_offset);
+  dash_ = cc::PathEffect::MakeDash(intervals.data(), count, dash_offset);
 }
 
 void StrokeData::SetDashEffect(sk_sp<cc::PathEffect> dash_effect) {
