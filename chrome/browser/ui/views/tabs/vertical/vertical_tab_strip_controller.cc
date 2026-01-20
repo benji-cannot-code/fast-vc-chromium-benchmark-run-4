c7ad8056ddfcde10a87724768b5fa6f1c16247c2
Caroline Rising <corising@chromium.org>
2026-01-20 12:46:46 -0800

    [Vertical tabs] Update group editor bubble anchoring and triggering
    Trigger the group editor bubble to be shown after a group has been
    created. Update the editor bubble anchor based on tab strip collapsed
    state.
    
    Bug: 439961150
    Change-Id: Ic10a286762c295df00d5063b451a7b6d88796c0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7487248
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1571827}

--- chrome/browser/ui/views/tabs/vertical/vertical_tab_strip_controller.cc
+++ chrome/browser/ui/views/tabs/vertical/vertical_tab_strip_controller.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/tabs/tab_group_editor_bubble_view.h"
 #include "chrome/browser/ui/views/tabs/vertical/tab_collection_node.h"
 #include "chrome/browser/ui/views/tabs/vertical/vertical_tab_drag_handler.h"
+#include "chrome/browser/ui/views/tabs/vertical/vertical_tab_group_view.h"
 #include "components/tabs/public/tab_collection_types.h"
 #include "components/tabs/public/tab_group.h"
 #include "components/tabs/public/tab_interface.h"
@@ -197,6 +198,14 @@ void VerticalTabStripController::ToggleTabGroupCollapsedState(
           base::UserMetricsAction("TabGroups_TabGroupHeader_Collapsed"));
     }
   }
+}
+
+void VerticalTabStripController::ShowGroupEditorBubble(
+    const TabCollectionNode* group_node) {
+  auto* group_header_view =
+      static_cast<VerticalTabGroupView*>(group_node->view())->group_header();
+  group_header_view->ShowContextMenuForViewImpl(
+      group_header_view, gfx::Point(), ui::mojom::MenuSourceType::kNone);
 }
 
 views::Widget* VerticalTabStripController::ShowGroupEditorBubble(
