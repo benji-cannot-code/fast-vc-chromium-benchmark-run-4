9c5f4adba0d597e596610e41499d943602e7479b
hiroki oshima <hiroki.oshima@gmail.com>
2026-01-19 23:14:45 -0800

    fix SplitViewDragIndicatorsTest dangling pointer
    Bug: 40212619
    Change-Id: I06406bfde4a2a9273fcb2e4bafb485f6e59300f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7472102
    Reviewed-by: Di Wu <diwux@google.com>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1571454}

--- ash/wm/splitview/split_view_drag_indicators_unittest.cc
+++ ash/wm/splitview/split_view_drag_indicators_unittest.cc
@@ -54,6 +54,7 @@ class SplitViewDragIndicatorsTest : public AshTestBase {
         true);
   }
   void TearDown() override {
+    overview_session_ = nullptr;
     ui::PresentationTimeRecorder::SetReportPresentationTimeImmediatelyForTest(
         false);
     AshTestBase::TearDown();
@@ -103,7 +104,7 @@ class SplitViewDragIndicatorsTest : public AshTestBase {
   }
 
  protected:
-  raw_ptr<OverviewSession, DanglingUntriaged> overview_session_ = nullptr;
+  raw_ptr<OverviewSession> overview_session_ = nullptr;
 };
 
 TEST_F(SplitViewDragIndicatorsTest, DraggingBasic) {
