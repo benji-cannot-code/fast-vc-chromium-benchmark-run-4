1fe6d6cc35dac5ea3be9aa990c13422ff5744cc1
Jeffrey Yu <yuje@chromium.org>
2026-01-20 15:04:04 -0800

    Replace `std::unordered_set` with `absl::flat_hash_set`.
    The Chromium containers guide
    (https://chromium.googlesource.com/chromium/src/+/HEAD/base/containers/README.md)
    recommends using `absl::flat_hash_set` as a default map choice instead
    of `std::unordered_set` when pointer stability isn't needed, and
    `absl::node_hash_set` when it is. Both will still generally perform
    better and require less overhead than `std::unordered_set`.
    
    Bug: 473916362
    Change-Id: Ibfd80427233574a580f2d2dc16162518b954ba49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7498951
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Auto-Submit: Jeffrey Yu <yuje@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1571912}

--- chrome/browser/sync_file_system/drive_backend/metadata_database_index_on_disk.cc
+++ chrome/browser/sync_file_system/drive_backend/metadata_database_index_on_disk.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync_file_system/drive_backend/metadata_database_index_on_disk.h"
 
 #include <array>
-#include <unordered_set>
 
 #include "base/format_macros.h"
 #include "base/logging.h"
@@ -18,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync_file_system/drive_backend/leveldb_wrapper.h"
 #include "chrome/browser/sync_file_system/drive_backend/metadata_database.pb.h"
 #include "chrome/browser/sync_file_system/logger.h"
+#include "third_party/abseil-cpp/absl/container/flat_hash_set.h"
 #include "third_party/leveldatabase/src/include/leveldb/status.h"
 
 // LevelDB database schema
@@ -211,7 +211,7 @@ void RemoveUnreachableItemsFromDB(LevelDBWrapper* db,
 
   // Delete all unreachable trackers, and list all |file_id| referred by
   // remained trackers.
-  std::unordered_set<std::string> referred_file_ids;
+  absl::flat_hash_set<std::string> referred_file_ids;
   {
     std::unique_ptr<LevelDBWrapper::Iterator> itr = db->NewIterator();
     for (itr->Seek(kFileTrackerKeyPrefix); itr->Valid(); itr->Next()) {
