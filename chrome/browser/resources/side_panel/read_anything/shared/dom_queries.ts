ed251a9d4a4537fd4a0c23e1ea633cea1e0b68c7
Kristi Saney <kristislee@google.com>
2026-01-20 12:50:28 -0800

    [Reading mode] Pull adjusted offsets logic into utility
    This is preparation for using similar logic to keep track of the current
    read aloud position with line focus.
    
    Bug: 474105870
    Change-Id: Ica3bed344c6ec912e26414ffeb50b79f3acb1a60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7489543
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Auto-Submit: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1571832}

---
+++ chrome/browser/resources/side_panel/read_anything/shared/dom_queries.ts
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Given a root node and a start index, returns the text node and its offset
+// within the root node that contains the start index.
+export function getTextNodeOffsets(
+    rootNode: Node, start: number): {node: Node, offset: number} {
+  let offset = 0;
+  if (rootNode.nodeType === Node.TEXT_NODE) {
+    return {node: rootNode, offset};
+  }
+
+  const treeWalker = document.createTreeWalker(rootNode, NodeFilter.SHOW_TEXT);
+
+  while (treeWalker.nextNode()) {
+    const textNode = treeWalker.currentNode;
+    const length = textNode.textContent!.length;
+
+    // Check if the target start index falls within this node's range
+    // Range is [offset, offset + length)
+    if (start < offset + length) {
+      return {node: textNode, offset};
+    }
+
+    offset += length;
+  }
+
+  return {node: rootNode, offset};
+}
