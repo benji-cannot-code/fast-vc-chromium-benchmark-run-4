566c509d805676e81e27df6896c70e2b7627627a
Ming-Ying Chung <mych@chromium.org>
2026-01-20 11:39:33 -0800

    [WaaP] Split first run from startup metrics
    This CL made the following changes:
    - Fix metrics without the ".Temperature" suffix. They now
      indicate aggregation across all Temperature.
    - Add the `FirstRun` startup metric slicing for
      BrowserWindow and ReloadButton.
    - Note: when a profiler picker is shown at startup, no
      metrics will be recorded as [ProfilePickerHandler][1]
      marks it.
    - The full list of metrics are as follows:
    
    (1) Standard: No Session Restore, No First Run
    - `InitialWebUI.Startup.BrowserWindow.FirstPaint.Temperature.ColdStartup`
    - `InitialWebUI.Startup.BrowserWindow.FirstPaint.Temperature.WarmStartup`
    - `InitialWebUI.Startup.BrowserWindow.FirstPaint.Temperature.Other`
    - `InitialWebUI.Startup.ReloadButton.First{Contentful}Paint`
    - `InitialWebUI.Startup.ReloadButton.First{Contentful}Paint.Temperature.ColdStartup`
    - `InitialWebUI.Startup.ReloadButton.First{Contentful}Paint.Temperature.WarmStartup`
    - `InitialWebUI.Startup.ReloadButton.First{Contentful}Paint.Temperature.Other`
    
    (2) First Run
    - `InitialWebUI.Startup.FirstRun.BrowserWindow.FirstPaint`
    - `InitialWebUI.Startup.FirstRun.BrowserWindow.FirstPaint.Temperature.ColdStartup`
    - `InitialWebUI.Startup.FirstRun.BrowserWindow.FirstPaint.Temperature.WarmStartup`
    - `InitialWebUI.Startup.FirstRun.BrowserWindow.FirstPaint.Temperature.Other`
    - `InitialWebUI.Startup.FirstRun.ReloadButton.First{Contentful}Paint`
    - `InitialWebUI.Startup.FirstRun.ReloadButton.First{Contentful}Paint.Temperature.ColdStartup`
    - `InitialWebUI.Startup.FirstRun.ReloadButton.First{Contentful}Paint.Temperature.WarmStartup`
    - `InitialWebUI.Startup.FirstRun.ReloadButton.First{Contentful}Paint.Temperature.Other`
    
    (3) Session Restore
    - `InitialWebUI.Startup.SessionRestore.BrowserWindow.FirstPaint`
    - `InitialWebUI.Startup.SessionRestore.BrowserWindow.FirstPaint.Temperature.ColdStartup`
    - `InitialWebUI.Startup.SessionRestore.BrowserWindow.FirstPaint.Temperature.WarmStartup`
    - `InitialWebUI.Startup.SessionRestore.BrowserWindow.FirstPaint.Temperature.Other`
    - `InitialWebUI.Startup.SessionRestore.ReloadButton.First{Contentful}Paint`
    - `InitialWebUI.Startup.SessionRestore.ReloadButton.First{Contentful}Paint.Temperature.ColdStartup`
    - `InitialWebUI.Startup.SessionRestore.ReloadButton.First{Contentful}Paint.Temperature.WarmStartup`
    - `InitialWebUI.Startup.SessionRestore.ReloadButton.First{Contentful}Paint.Temperature.Other`
    
    [1]: https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ui/webui/signin/profile_picker_handler.cc;l=1012;drc=6cd8782743c351e5fb4fb002c69c3ba128b94bdc
    
    Bug: 448794588
    Change-Id: Id282a2daca9d5c99c5026245c22b8a9bed7bd183
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7496932
    Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    Reviewed-by: Sean Maher <spvm@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1571786}

--- components/startup_metric_utils/browser/startup_metric_utils.cc
+++ components/startup_metric_utils/browser/startup_metric_utils.cc
@@ -261,6 +261,7 @@ void BrowserStartupMetricRecorder::ResetSessionForTesting() {
   browser_window_first_paint_ticks_ = base::TimeTicks();
   is_privacy_sandbox_attestations_component_ready_recorded_ = false;
   is_privacy_sandbox_attestations_first_check_recorded_ = false;
+  is_first_run_ = false;
 }
 
 bool BrowserStartupMetricRecorder::WasMainWindowStartupInterrupted() const {
@@ -292,6 +293,7 @@ void BrowserStartupMetricRecorder::RecordBrowserMainMessageLoopStart(
     base::TimeTicks ticks,
     bool is_first_run) {
   DCHECK(!GetCommon().application_start_ticks_.is_null());
+  is_first_run_ = is_first_run;
 
   RecordMessageLoopStartTicks(ticks);
 
@@ -311,7 +313,6 @@ void BrowserStartupMetricRecorder::RecordBrowserMainMessageLoopStart(
         &base::UmaHistogramLongTimes100, "Startup.BrowserMessageLoopStartTime",
         GetCommon().application_start_ticks_, ticks);
   }
-
   GetCommon().AddStartupEventsForTelemetry();
 
   // Record values stored prior to startup temperature evaluation.
@@ -517,6 +518,10 @@ bool BrowserStartupMetricRecorder::ShouldLogStartupHistogram() const {
 
 StartupTemperature BrowserStartupMetricRecorder::GetStartupTemperature() const {
   return g_startup_temperature;
+}
+
+bool BrowserStartupMetricRecorder::IsFirstRun() const {
+  return is_first_run_;
 }
 
 base::TimeTicks
