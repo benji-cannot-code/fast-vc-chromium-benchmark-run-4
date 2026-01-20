3477f370ba979176216027d634f9e21cf7479ec0
Sam Maier <smaier@chromium.org>
2026-01-20 11:51:41 -0800

    Fix LeakCanary test for desktop emulator
    I'm not sure if this also fixes it for hardware CI bots, as I haven't
    been able to test locally yet. But, this does fix for my emulator.
    
    Change-Id: Ie30208a058bd9ed9b76356307d7ff2b96a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7499518
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Auto-Submit: Sam Maier <smaier@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1571790}

--- chrome/android/javatests/src/org/chromium/chrome/test/memory_leaks/PublicTransitLeakTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/test/memory_leaks/PublicTransitLeakTest.java
@@ -8,6 +8,7 @@ package org.chromium.chrome.test.memory_leaks;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.filters.LargeTest;
 
+import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -39,6 +40,11 @@ public class PublicTransitLeakTest {
             ChromeTransitTestRules.freshChromeTabbedActivityRule();
 
     @Rule public CctTransitTestRule mCustomTabActivityTestRule = new CctTransitTestRule();
+
+    @After
+    public void tearDown() {
+        mChromeTabbedActivityTestRule.closeAllWindowsAndDeleteInstanceAndTabState();
+    }
 
     @Test
     @LargeTest
