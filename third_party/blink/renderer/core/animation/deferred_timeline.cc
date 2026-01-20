fbe721a84776da4adcf414a79854bbdf1ed54ad8
Anders Hartvoll Ruud <andruud@chromium.org>
2026-01-20 08:11:12 -0800

    [scroll-animations] Last timeline name in tree order wins
    This CL implements one part of the new timeline name scoping
    behavior [1]: When multiple timeline definitions are competing
    for the same name withing a scope, last in flat tree order wins.
    
    The second part of this involves behaving as if timeline-scope:all
    existed on the root element. This will be done separately.
    
    This change is behind a flag for now.
    
    [1] https://github.com/w3c/csswg-drafts/issues/12581
    
    Bug: 476936388
    Change-Id: I65845ccec9b96dfbc4bab90da2b92246e5d32655
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7497353
    Reviewed-by: David Awogbemila <awogbemila@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1571650}

--- third_party/blink/renderer/core/animation/deferred_timeline.cc
+++ third_party/blink/renderer/core/animation/deferred_timeline.cc
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/animation/deferred_timeline.h"
 
 #include "third_party/blink/renderer/core/animation/scroll_timeline.h"
+#include "third_party/blink/renderer/core/dom/element.h"
+#include "third_party/blink/renderer/core/dom/layout_tree_builder_traversal.h"
+#include "third_party/blink/renderer/platform/wtf/wtf_size_t.h"
 
 namespace blink {
 
@@ -13,28 +16,50 @@ DeferredTimeline::DeferredTimeline(Document* document)
     : ScrollSnapshotTimeline(document) {}
 
 AnimationTimeline* DeferredTimeline::ExposedTimeline() {
-  return SingleAttachedTimeline();
+  return EffectiveScrollTimeline();
 }
 
 void DeferredTimeline::AttachTimeline(ScrollTimeline* timeline) {
-  ScrollTimeline* original_timeline = SingleAttachedTimeline();
+  ScrollTimeline* original_timeline = EffectiveScrollTimeline();
 
-  attached_timelines_.push_back(timeline);
+  wtf_size_t insertion_point = attached_timelines_.size();
 
-  if (original_timeline != SingleAttachedTimeline()) {
+  if (RuntimeEnabledFeatures::CSSTimelineScopeGlobalEnabled()) {
+    Element* reference_element = timeline->GetReferenceElement();
+    // Only named ScrollTimelines and ViewTimelines produced by CSS
+    // should be attached, and such timelines always have a reference element.
+    CHECK(reference_element);
+
+    while (insertion_point > 0) {
+      // We can insert at `insertion_point` only if `reference_element`
+      // comes after the preceding element in flat tree order.
+      Element* preceding =
+          attached_timelines_[insertion_point - 1]->GetReferenceElement();
+      if (LayoutTreeBuilderTraversal::ComparePreorderTreePosition(
+              *preceding, *reference_element) > 0) {
+        --insertion_point;
+        continue;
+      }
+      break;
+    }
+  }
+
+  attached_timelines_.insert(insertion_point, timeline);
+
+  if (original_timeline != EffectiveScrollTimeline()) {
     OnAttachedTimelineChange();
   }
 }
 
 void DeferredTimeline::DetachTimeline(ScrollTimeline* timeline) {
-  ScrollTimeline* original_timeline = SingleAttachedTimeline();
+  ScrollTimeline* original_timeline = EffectiveScrollTimeline();
 
   wtf_size_t i = attached_timelines_.Find(timeline);
   if (i != kNotFound) {
     attached_timelines_.EraseAt(i);
   }
 
-  if (original_timeline != SingleAttachedTimeline()) {
+  if (original_timeline != EffectiveScrollTimeline()) {
     OnAttachedTimelineChange();
   }
 }
@@ -45,17 +70,26 @@ void DeferredTimeline::Trace(Visitor* visitor) const {
 }
 
 DeferredTimeline::ScrollAxis DeferredTimeline::GetAxis() const {
-  if (const ScrollTimeline* attached_timeline = SingleAttachedTimeline()) {
+  if (const ScrollTimeline* attached_timeline = EffectiveScrollTimeline()) {
     return attached_timeline->GetAxis();
   }
   return ScrollAxis::kBlock;
 }
 
 DeferredTimeline::TimelineState DeferredTimeline::ComputeTimelineState() const {
-  if (const ScrollTimeline* attached_timeline = SingleAttachedTimeline()) {
+  if (const ScrollTimeline* attached_timeline = EffectiveScrollTimeline()) {
     return attached_timeline->ComputeTimelineState();
   }
   return TimelineState();
+}
+
+ScrollTimeline* DeferredTimeline::EffectiveScrollTimeline() {
+  if (!RuntimeEnabledFeatures::CSSTimelineScopeGlobalEnabled() &&
+      attached_timelines_.size() != 1u) {
+    return nullptr;
+  }
+  // The item at back() is the last attached timeline in flat tree order.
+  return attached_timelines_.empty() ? nullptr : attached_timelines_.back();
 }
 
 void DeferredTimeline::OnAttachedTimelineChange() {
