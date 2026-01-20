145902f28dd08cce7498beca41607adc7adc4343
Chris Fredrickson <cfredric@chromium.org>
2026-01-20 07:21:40 -0800

    [glic] Silence logspam from uninteresting mock calls
    Change-Id: I67344edeb4bdf4e7bccfb0bfda8907619f9bc5da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7494719
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Kevin Graney <kmg@google.com>
    Commit-Queue: Kevin Graney <kmg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1571620}

--- chrome/browser/actor/ui/test_support/mock_event_dispatcher.cc
+++ chrome/browser/actor/ui/test_support/mock_event_dispatcher.cc
@@ -11,7 +11,7 @@ MockUiEventDispatcher::MockUiEventDispatcher() = default;
 MockUiEventDispatcher::~MockUiEventDispatcher() = default;
 
 std::unique_ptr<UiEventDispatcher> NewMockUiEventDispatcher() {
-  return std::make_unique<MockUiEventDispatcher>();
+  return std::make_unique<testing::NiceMock<MockUiEventDispatcher>>();
 }
 
 }  // namespace actor::ui
