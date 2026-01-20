a8d27907e394e5683f6db3f720193a7542146a8d
Abe Boujane <boujane@google.com>
2026-01-20 10:44:53 -0800

    Remove stale feature kGlicHandoffButtonHiddenClientControl
    This feature was enabled by default. This CL removes the flag and
    simplifies the code to always assume the enabled behavior.
    
    Bug: 424495020, 458009945
    Change-Id: Ia5ada838c05a77e76955c3fff26db07a35780713
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7486365
    Reviewed-by: Michelle Abreo <michelleabreo@google.com>
    Commit-Queue: Abe Boujane <boujane@google.com>
    Auto-Submit: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1571743}

--- chrome/browser/actor/ui/actor_ui_state_manager.cc
+++ chrome/browser/actor/ui/actor_ui_state_manager.cc
@@ -65,9 +65,7 @@ const UiTabState& GetWaitingOnUserUiTabState() {
 const UiTabState& GetPausedUiTabState() {
   static const UiTabState kPausedState = {
       .actor_overlay = {.is_active = false, .border_glow_visible = false},
-      .handoff_button = {.is_active = !base::FeatureList::IsEnabled(
-                             features::kGlicHandoffButtonHiddenClientControl),
-                         .controller = kClient},
+      .handoff_button = {.is_active = false, .controller = kClient},
       .tab_indicator = TabIndicatorStatus::kNone,
       .border_glow_visible = false,
   };
--- chrome/browser/actor/ui/actor_ui_state_manager_unittest.cc
+++ chrome/browser/actor/ui/actor_ui_state_manager_unittest.cc
@@ -51,8 +51,7 @@ class ActorUiStateManagerTest : public testing::Test {
   // testing::Test:
   void SetUp() override {
     scoped_feature_list_.InitWithFeatures(
-        /*enabled_features=*/{features::kGlicActorUi,
-                              features::kGlicHandoffButtonHiddenClientControl},
+        /*enabled_features=*/{features::kGlicActorUi},
         /*disabled_features=*/{});
     profile_ = TestingProfile::Builder()
                    .AddTestingFactory(
