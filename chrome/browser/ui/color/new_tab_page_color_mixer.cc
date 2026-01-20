989ee212af57c405d9f75919f7f857de44770e5c
Gianni Chen <giannich@chromium.org>
2026-01-20 14:10:56 -0800

    [ntp-threads] Match threads rail background color to footer color
    This change simply sets the background color of the threads rail to that
    of the footer.
    
    Screencast: screencast/cast/Njc0NjEyNTIzODI3MjAwMHwwNzVlOWUwMC1mZg
    
    Bug: 476473718
    Change-Id: I21cfe35fbbadb5ffa824696eda92c5bab7298c41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7489282
    Commit-Queue: Gianni Chen <giannich@chromium.org>
    Reviewed-by: Bofeng Chen <bofengchen@google.com>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1571878}

--- chrome/browser/ui/color/new_tab_page_color_mixer.cc
+++ chrome/browser/ui/color/new_tab_page_color_mixer.cc
@@ -268,11 +268,6 @@ void AddGeneratedThemeComprehensiveColors(ui::ColorMixer& mixer) {
                           color_utils::kMinimumReadableContrastRatio),
       ui::PickGoogleColor(gfx::kGoogleGrey500, element_background_color,
                           color_utils::kMinimumReadableContrastRatio));
-
-  // Threads rail colors.
-  mixer[kColorNewTabPageThreadsRailBackground] =
-      SelectBasedOnWhiteInput({kColorNewTabPageBackground}, gfx::kGoogleGrey100,
-                              element_background_color);
 }
 
 }  // namespace
@@ -469,6 +464,9 @@ void AddNewTabPageColorMixer(ui::ColorProvider* provider,
   mixer[kColorNewTabFooterText] =
       ui::GetColorWithMaxContrast({kColorNewTabFooterBackground});
   mixer[kColorNewTabFooterLogoBackground] = {SK_ColorWHITE};
+
+  /* NewTabPageThreadsRail */
+  mixer[kColorNewTabPageThreadsRailBackground] = {kColorToolbar};
 }
 
 void AddWebThemeNewTabPageColors(ui::ColorMixer& mixer, bool dark_mode) {
@@ -620,9 +618,5 @@ void AddWebThemeNewTabPageColors(ui::ColorMixer& mixer, bool dark_mode) {
                                                          : gfx::kGoogleGrey800};
   mixer[kColorNewTabPageActionChipDeepSearchIcon] = {
       dark_mode ? SK_ColorWHITE : gfx::kGoogleGrey800};
-
-  // Threads rail colors.
-  mixer[kColorNewTabPageThreadsRailBackground] = {
-      dark_mode ? gfx::kGoogleGrey800 : gfx::kGoogleGrey100};
   // LINT.ThenChange(//chrome/browser/ui/color/material_new_tab_page_color_mixer.cc)
 }
