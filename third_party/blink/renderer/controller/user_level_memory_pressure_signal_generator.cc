11197ce30857403cfd8a5aec3e6604e209818738
Patrick Monette <pmonette@chromium.org>
2026-01-20 04:40:41 -0800

    Remove DCHECK in UserLevelMemoryPressureSignalGenerator::Instance
    The only call to `Instance()` checks the return value anyways.
    
    Bug: 465068849
    Change-Id: Id74177fa6e074f08f095d69bbade217b4261ab2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7466456
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Ziad Youssef <ziadyoussef@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1571555}

--- third_party/blink/renderer/controller/user_level_memory_pressure_signal_generator.cc
+++ third_party/blink/renderer/controller/user_level_memory_pressure_signal_generator.cc
@@ -43,7 +43,6 @@ UserLevelMemoryPressureSignalGenerator* g_instance = nullptr;
 // static
 UserLevelMemoryPressureSignalGenerator*
 UserLevelMemoryPressureSignalGenerator::Instance() {
-  DCHECK(g_instance);
   return g_instance;
 }
 
