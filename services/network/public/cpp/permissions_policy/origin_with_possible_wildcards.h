935b8965c4372941b7dba9bc14f29749ce90db52
Zgroza Klimek <zgroza@chromium.org>
2026-01-20 12:08:58 -0800

    [IWA] Add PermissionsPolicyParser::ParseIsolatedAppPermissionsPolicy
    This method allows parsing a permissions policy defined in the Isolated
    Web App manifest (which serves as a base policy) and combining it with
    the policy defined in HTTP headers.
    
    This is a preparatory step for implementing dynamic feature flags for
    Isolated Web Apps (see chained CL).
    
    Bug: 362237072
    Change-Id: I6a68be2a72599acaf44fa885647971299303d1e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7416778
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Vogelheim <vogelheim@chromium.org>
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Robert Ferens <rferens@google.com>
    Commit-Queue: Zgroza (Luke) Klimek <zgroza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1571802}

--- services/network/public/cpp/permissions_policy/origin_with_possible_wildcards.h
+++ services/network/public/cpp/permissions_policy/origin_with_possible_wildcards.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SERVICES_NETWORK_PUBLIC_CPP_PERMISSIONS_POLICY_ORIGIN_WITH_POSSIBLE_WILDCARDS_H_
 #define SERVICES_NETWORK_PUBLIC_CPP_PERMISSIONS_POLICY_ORIGIN_WITH_POSSIBLE_WILDCARDS_H_
 
+#include <compare>
+
 #include "base/component_export.h"
 #include "services/network/public/mojom/content_security_policy.mojom.h"
 #include "services/network/public/mojom/permissions_policy/permissions_policy.mojom-shared.h"
@@ -79,9 +81,11 @@ class COMPONENT_EXPORT(NETWORK_CPP_WEB_PLATFORM) OriginWithPossibleWildcards {
   COMPONENT_EXPORT(NETWORK_CPP_WEB_PLATFORM)
   friend bool operator==(const OriginWithPossibleWildcards&,
                          const OriginWithPossibleWildcards&) = default;
+
   COMPONENT_EXPORT(NETWORK_CPP_WEB_PLATFORM)
-  friend bool operator<(const OriginWithPossibleWildcards& lhs,
-                        const OriginWithPossibleWildcards& rhs);
+  friend std::strong_ordering operator<=>(
+      const OriginWithPossibleWildcards& lhs,
+      const OriginWithPossibleWildcards& rhs);
 
  private:
   friend struct mojo::StructTraits<
