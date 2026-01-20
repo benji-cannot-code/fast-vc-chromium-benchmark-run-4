8aa3819a938e7026d71b7a9621dd430da4a53ed3
Alex Gough <ajgo@chromium.org>
2026-01-20 07:48:15 -0800

    Use mojom-shared in ai page content agent unittest
    Preferred include is -shared not -data-view unless in a traits file.
    
    bug: 469801419
    Change-Id: I2189d31deaf921a6281952d7043581d6e6500ac0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7490204
    Commit-Queue: Aaron Leventhal <aleventhal@google.com>
    Reviewed-by: Aaron Leventhal <aleventhal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1571640}

--- third_party/blink/renderer/modules/content_extraction/ai_page_content_agent_unittest.cc
+++ third_party/blink/renderer/modules/content_extraction/ai_page_content_agent_unittest.cc
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/frame/frame_ad_evidence.h"
 #include "third_party/blink/public/common/input/web_mouse_event.h"
-#include "third_party/blink/public/mojom/content_extraction/ai_page_content.mojom-data-view.h"
+#include "third_party/blink/public/mojom/content_extraction/ai_page_content.mojom-shared.h"
 #include "third_party/blink/renderer/core/accessibility/ax_context.h"
 #include "third_party/blink/renderer/core/accessibility/ax_object_cache.h"
 #include "third_party/blink/renderer/core/css/style_engine.h"
