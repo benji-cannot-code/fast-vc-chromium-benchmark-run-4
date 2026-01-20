4e8f76e732d877cfa84cbe59006887079e5d1bf0
Jacques Newman <janewman@microsoft.com>
2026-01-20 15:48:08 -0800

    [focusgroup] Rename focusgroup-entry-priority to focusgroupstart
    No behavioral changes, just an attribute rename.
    This was discussed and resolved in the OpenUI call on Jan 15.
    Related issue: https://github.com/openui/open-ui/issues/1314
    
    Bug: 40210717
    Change-Id: I173893540ecbe12519ae4d37e50f0f7cae183fd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7488249
    Commit-Queue: Jacques Newman <janewman@microsoft.com>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1571938}

--- third_party/blink/renderer/core/page/focusgroup_controller_utils.cc
+++ third_party/blink/renderer/core/page/focusgroup_controller_utils.cc
@@ -448,8 +448,8 @@ FocusgroupControllerUtils::GetEntryElementForFocusgroupSegmentFromFirst(
       continue;
     }
 
-    // Check for focusgroup-entry-priority attribute.
-    if (!entry_priority_item && HasFocusgroupEntryPriority(*item_in_segment)) {
+    // Check for focusgroupstart attribute.
+    if (!entry_priority_item && IsFocusgroupStart(*item_in_segment)) {
       entry_priority_item = item_in_segment;
     }
 
@@ -729,9 +729,8 @@ const Element* FocusgroupControllerUtils::GetOptedOutSubtreeRoot(
 }
 
 // static
-bool FocusgroupControllerUtils::HasFocusgroupEntryPriority(
-    const Element& element) {
-  return element.FastHasAttribute(html_names::kFocusgroupEntryPriorityAttr);
+bool FocusgroupControllerUtils::IsFocusgroupStart(const Element& element) {
+  return element.FastHasAttribute(html_names::kFocusgroupstartAttr);
 }
 
 }  // namespace blink
