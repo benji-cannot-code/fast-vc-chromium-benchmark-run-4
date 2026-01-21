154b9018818506ac0a1b196a91c06aa9a86b857a
Akihiro Ota <akihiroota@chromium.org>
2026-01-21 07:57:21 -0800

    Face control: Observe display rotation changes
    This CL fixes the bug by making the FaceGazeBubbleController observe
    display rotation changes and updating the bubble position as needed.
    See the bug for a demo of the working solution.
    
    Fixed: 410048595
    Change-Id: I35488257141eabc2124bb8d6779cca9d4cc6bf59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7499351
    Reviewed-by: Amanda Lin Dietz <aldietz@google.com>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1572327}

--- ash/system/accessibility/facegaze_bubble_controller.h
+++ ash/system/accessibility/facegaze_bubble_controller.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/timer/timer.h"
+#include "ui/display/display_observer.h"
 #include "ui/views/view_observer.h"
 
 namespace ui {
@@ -28,7 +29,8 @@ namespace ash {
 class FaceGazeBubbleView;
 
 // Manages the FaceGaze bubble view.
-class ASH_EXPORT FaceGazeBubbleController : public views::ViewObserver {
+class ASH_EXPORT FaceGazeBubbleController : public views::ViewObserver,
+                                            public display::DisplayObserver {
  public:
   FaceGazeBubbleController(
       const base::RepeatingCallback<void()>& on_close_button_clicked);
@@ -38,6 +40,10 @@ class ASH_EXPORT FaceGazeBubbleController : public views::ViewObserver {
 
   // views::ViewObserver:
   void OnViewIsDeleting(views::View* observed_view) override;
+
+  // display::DisplayObserver:
+  void OnDisplayMetricsChanged(const display::Display& display,
+                               uint32_t changed_metrics) override;
 
   // Updates the bubble's visibility and text content.
   void UpdateBubble(const std::u16string& text, bool is_warning);
@@ -52,6 +58,9 @@ class ASH_EXPORT FaceGazeBubbleController : public views::ViewObserver {
 
   // Updates the view and widget.
   void Update(const std::u16string& text, bool is_warning);
+
+  // Updates the position of the bubble on the screen.
+  void UpdatePosition();
 
   // Called whenever the mouse enters the the main content of
   // FaceGazeBubbleView; this method doesn't get called if the mouse hovers the
