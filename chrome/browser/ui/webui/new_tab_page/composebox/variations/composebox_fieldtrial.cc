7b9df7a2ba9a1898ea5517b20b5a63dde219c054
Jilin Yang <jilinyang@google.com>
2026-01-21 12:15:28 -0800

    [NTP-Real_Box]: Remove feature param but keep the behavior as default
    Remove EnableModalComposebox feature param but keep modal behavior as default.
    
    Fixed: 476462958
    Change-Id: Iab6771ad8e1a6be4e0996846944b13293acf0fe5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7499752
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Reviewed-by: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Reviewed-by: Hannah Collins <hannahrogers@google.com>
    Cr-Commit-Position: refs/heads/main@{#1572498}

--- chrome/browser/ui/webui/new_tab_page/composebox/variations/composebox_fieldtrial.cc
+++ chrome/browser/ui/webui/new_tab_page/composebox/variations/composebox_fieldtrial.cc
@@ -319,9 +319,6 @@ const base::FeatureParam<bool> kAddTabUploadDelayOnRecentTabChipClick(
     &kNtpComposebox,
     "AddTabUploadDelayOnRecentTabChipClick",
     false);
-const base::FeatureParam<bool> kEnableModalComposebox(&kNtpComposebox,
-                                                      "EnableModalComposebox",
-                                                      true);
 const base::FeatureParam<bool> kEnableThreadsRail(&kNtpComposebox,
                                                   "EnableThreadsRail",
                                                   true);
--- chrome/browser/ui/webui/new_tab_page/new_tab_page_ui.cc
+++ chrome/browser/ui/webui/new_tab_page/new_tab_page_ui.cc
@@ -702,8 +702,6 @@ content::WebUIDataSource* CreateAndAddNewTabPageUiHtmlSource(Profile* profile) {
   source->AddBoolean(
       "addTabUploadDelayOnRecentTabChipClick",
       ntp_composebox::kAddTabUploadDelayOnRecentTabChipClick.Get());
-  source->AddBoolean("enableModalComposebox",
-                     ntp_composebox::kEnableModalComposebox.Get());
   source->AddBoolean("enableThreadsRail",
                      ntp_composebox::kEnableThreadsRail.Get());
   source->AddBoolean("showModelPicker", ntp_composebox::kShowModelPicker.Get());
