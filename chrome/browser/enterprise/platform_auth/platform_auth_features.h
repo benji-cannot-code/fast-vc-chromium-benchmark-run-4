93aed613381f2f8bcf8f614c3044dc55f067a4f6
Stanisław Solarewicz <ssolarewicz@chromium.org>
2026-01-21 02:52:09 -0800

    [Okta SSO] Parametrize Okta SSO request URL pattern
    Instead of hardcoding the pattern now it will be a feature parameter.
    
    Bug: b:433226247
    Change-Id: I41a274ff93489a695485d68659003f8c05bb991e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7486776
    Commit-Queue: Stanisław Solarewicz <ssolarewicz@chromium.org>
    Reviewed-by: Takashi Nakayama <tnak@chromium.org>
    Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1572185}

--- chrome/browser/enterprise/platform_auth/platform_auth_features.h
+++ chrome/browser/enterprise/platform_auth/platform_auth_features.h
@@ -22,6 +22,8 @@ BASE_DECLARE_FEATURE_PARAM(std::string, kOktaSsoRequestHeadersAllowlist);
 BASE_DECLARE_FEATURE_PARAM(std::string, kOktaSsoResponseHeadersAllowlist);
 
 BASE_DECLARE_FEATURE_PARAM(std::string, kOktaSsoFixedRequestHeaders);
+
+BASE_DECLARE_FEATURE_PARAM(std::string, kOktaSsoURLPattern);
 #endif
 
 }  // namespace enterprise_auth
