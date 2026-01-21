ae594df0de8704b825fe5aa676a971c04d6de35f
Javier Fernández García-Boente <jfernandez@igalia.com>
2026-01-20 16:29:51 -0800

    Reland "Intercept navigation when trying to use an unconfirmed protocol handler"
    This is a reland of commit c27a9af3427290c73c4488c1c5c6a5d2ef48b60b
    
    The root cause if the regressions detected by the Mac bots was that the
    SetAsDefault call to register the handler as default in the OS fails in
    Mac because the app_bundle got in SetAsDefaultClientForScheme() is not
    valid. As a consequence, the handler is removed asynchronously as part
    of the final steps of the registration process. When the
    NavigationThrottle tried to confirm the handler, it was not found in the
    registry. This violates a CHECK defined in the ConfirmProtocolHandler()
    method to ensure there is a valid handler for the navigation request's
    scheme being intercepted.
    
    This scenario can only happen under a testing environment, so the
    solution to use a Testing Delegate that provides a custom behavior of
    the OS registration procedure so that the handler is not removed in any
    case.
    
    We could go even further and avoid the OS registration at all, but in
    that case I think we should do it for any platform. It's a valid
    approach, given that the OS registration is not relevant for the tests
    defined in this CL. However, we have already some tests that operate in
    that scenario and having some browser tests covering the full
    functionality could be useful as well.
    
    Original change's description:
    > Intercept navigation when trying to use an unconfirmed protocol handler
    >
    > The new 'protocol_handlers' Manifest Key allows an extension to register
    > a few protocol handlers for some specific URL's schemes, similarly to
    > what the registerProtocolHandler Web API provides. However, unlike the
    > handlers registered via the Web API, these ones are registered under an
    > 'unconfirmed" state, as explained in the corresponding design doc [1].
    >
    > The design document specifies that the user must grant permission to use
    > any protocol handlers registered by an extension, at runtime, just
    > before it's used. This CL implements the change in the navigation logic
    > to add a NavigationThrottle if there is a custom handler for the URL's
    > scheme of the ongoing navigation request and such handler is in the
    > mentioned 'unconfirmed' state.
    >
    > This change affects only to the Custom Handlers logic, implementing a
    > dummy logic for the Prompt Dialog functionality. This will be
    > implemented in a follow up patch.
    >
    > [1]https://docs.google.com/document/d/1e6mSsbjLqBd1_4EAS_AX543vy53oq8SlycNwQ0mZ46g/edit?usp=sharing
    >
    > Bug: 40482153
    > Change-Id: I3cd66ca1f2e3598406418cae9ebd0dd409bd911e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7156864
    > Reviewed-by: Solomon Kinard <solomonkinard@chromium.org>
    > Reviewed-by: Balazs Engedy <engedy@chromium.org>
    > Commit-Queue: Javier Fernandez <jfernandez@igalia.com>
    > Cr-Commit-Position: refs/heads/main@{#1566155}
    
    Bug: 40482153
    Include-Ci-Only-Tests: chromium.mac:Mac13 Tests|browser_tests
    Change-Id: Ia6b18b429bce3694d8890c0156b070ee295c0aad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7416294
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Javier Fernandez <jfernandez@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1571953}

--- components/custom_handlers/protocol_handler_registry.cc
+++ components/custom_handlers/protocol_handler_registry.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 #include <utility>
 
+#include "base/check_is_test.h"
 #include "base/check_op.h"
 #include "base/command_line.h"
 #include "base/functional/bind.h"
@@ -304,12 +305,13 @@ ProtocolHandlerRegistry::GetExtensionProtocolHandlers(
 }
 
 void ProtocolHandlerRegistry::ClearUserDefinedHandlers(base::Time begin,
-                                                       base::Time end) {
+                                                       base::Time end,
+                                                       bool save) {
   for (const ProtocolHandler& handler : GetUserDefinedHandlers(begin, end))
-    RemoveHandler(handler);
+    RemoveHandler(handler, save);
 
   for (const ProtocolHandler& handler : GetUserIgnoredHandlers(begin, end))
-    RemoveIgnoredHandler(handler);
+    RemoveIgnoredHandler(handler, save);
 }
 
 ProtocolHandlerRegistry::ProtocolHandlerList
@@ -397,13 +399,16 @@ bool ProtocolHandlerRegistry::HasIgnoredEquivalent(
 }
 
 void ProtocolHandlerRegistry::RemoveIgnoredHandler(
-    const ProtocolHandler& handler) {
+    const ProtocolHandler& handler,
+    bool save) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
   bool should_notify = false;
   if (HandlerExists(handler, ignored_protocol_handlers_) &&
       HandlerExists(handler, user_ignored_protocol_handlers_)) {
     EraseHandler(handler, &user_ignored_protocol_handlers_);
-    Save();
+    if (save) {
+      Save();
+    }
     if (!HandlerExists(handler, policy_ignored_protocol_handlers_)) {
       EraseHandler(handler, &ignored_protocol_handlers_);
       should_notify = true;
@@ -418,7 +423,35 @@ bool ProtocolHandlerRegistry::IsHandledProtocol(std::string_view scheme) const {
   return enabled_ && !GetHandlerFor(scheme).IsEmpty();
 }
 
-void ProtocolHandlerRegistry::RemoveHandler(const ProtocolHandler& handler) {
+void ProtocolHandlerRegistry::ConfirmProtocolHandler(std::string_view scheme,
+                                                     bool save) {
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
+  ProtocolHandler handler = GetHandlerFor(scheme);
+  CHECK(handler.IsValid());
+  if (handler.is_confirmed()) {
+    return;
+  }
+  RemoveHandler(handler);
+  handler.Confirm();
+  RegisterProtocolHandler(handler, USER);
+  SetDefault(handler);
+  if (save) {
+    Save();
+  }
+  NotifyChanged();
+}
+
+bool ProtocolHandlerRegistry::IsProtocolHandlerConfirmed(
+    std::string_view scheme) const {
+  DCHECK(IsHandledProtocol(scheme));
+
+  ProtocolHandler handler = GetHandlerFor(scheme);
+  DCHECK(handler.is_confirmed() || handler.IsExtensionHandler());
+  return handler.is_confirmed();
+}
+
+void ProtocolHandlerRegistry::RemoveHandler(const ProtocolHandler& handler,
+                                            bool save) {
   if (IsIgnored(handler)) {
     RemoveIgnoredHandler(handler);
     return;
@@ -447,7 +480,9 @@ void ProtocolHandlerRegistry::RemoveHandler(const ProtocolHandler& handler) {
   if (erase_success && !IsHandledProtocol(handler.protocol())) {
     delegate_->DeregisterExternalHandler(handler.protocol());
   }
-  Save();
+  if (save) {
+    Save();
+  }
   if (erase_success)
     NotifyChanged();
 }
@@ -524,6 +559,12 @@ void ProtocolHandlerRegistry::AddObserver(Observer* observer) {
 
 void ProtocolHandlerRegistry::RemoveObserver(Observer* observer) {
   observers_.RemoveObserver(observer);
+}
+
+void ProtocolHandlerRegistry::SetDelegateForTesting(  // IN-TEST
+    std::unique_ptr<Delegate> delegate) {             // IN-TEST
+  CHECK_IS_TEST();                                    // IN-TEST
+  delegate_ = std::move(delegate);                    // IN-TEST
 }
 
 void ProtocolHandlerRegistry::PromoteHandler(const ProtocolHandler& handler) {
