488dc5016f45f3d7d2563df82a727d54b9c95ad7
Jun Ishiguro <junis@google.com>
2026-01-20 17:46:48 -0800

    device_name: Remove DeviceNamePolicyHandler::Observer
    This CL removes the observer class and the corresponding notifier method
    since it's no longer needed.
    
    Bug: b:466903206
    Change-Id: I6cadc59310813b026137bbfee35754aaf16783d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7452747
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1572000}

--- chrome/browser/ash/policy/handlers/device_name_policy_handler.h
+++ chrome/browser/ash/policy/handlers/device_name_policy_handler.h
@@ -7,9 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_ASH_POLICY_HANDLERS_DEVICE_NAME_POLICY_HANDLER_H_
 
 #include <optional>
-
-#include "base/observer_list.h"
-#include "base/observer_list_types.h"
+#include <string>
 
 namespace policy {
 
@@ -34,16 +32,6 @@ class DeviceNamePolicyHandler {
     kPolicyHostnameNotConfigurable,
   };
 
-  class Observer : public base::CheckedObserver {
-   public:
-    ~Observer() override = default;
-
-    // Called when the policy type and/or hostname has changed.
-    // Use GetDeviceNamePolicy() to get the new policy and
-    // GetHostnameChosenByAdministrator() to get the new hostname.
-    virtual void OnHostnamePolicyChanged() = 0;
-  };
-
   virtual ~DeviceNamePolicyHandler();
 
   // Provides hostname if requested by administrator.
@@ -51,16 +39,8 @@ class DeviceNamePolicyHandler {
   virtual std::optional<std::string> GetHostnameChosenByAdministrator()
       const = 0;
 
-  void AddObserver(Observer* observer);
-  void RemoveObserver(Observer* observer);
-
  protected:
   DeviceNamePolicyHandler();
-
-  void NotifyHostnamePolicyChanged();
-
- private:
-  base::ObserverList<Observer> observer_list_;
 };
 
 }  // namespace policy
--- chrome/browser/ash/policy/handlers/device_name_policy_handler_impl.cc
+++ chrome/browser/ash/policy/handlers/device_name_policy_handler_impl.cc
@@ -183,7 +183,6 @@ void DeviceNamePolicyHandlerImpl::SetDeviceNamePolicy(
 
   device_name_policy_ = policy;
   hostname_ = new_hostname;
-  NotifyHostnamePolicyChanged();
 }
 
 std::ostream& operator<<(
