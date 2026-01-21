b9c8ac2fbd90004ebc24c4270ac1d4201e509d07
Sophie Chang <sophiechang@chromium.org>
2026-01-21 10:13:59 -0800

    Do not reset the page data for ZSS if nothing meaningful has changed
    Now that the glic_zero_state_suggestions_manager is more aggressive about not calling the service - there is not a new opportunity to regenerate.
    
    Bug: 475521814
    Change-Id: Iaf45610435e9ae6765c6eddae128238176cab828
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7499033
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Zekun Jiang <zekunjiang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1572417}

--- chrome/browser/contextual_cueing/contextual_cueing_helper.cc
+++ chrome/browser/contextual_cueing/contextual_cueing_helper.cc
@@ -148,6 +148,10 @@ void ContextualCueingHelper::DidFinishNavigation(
                                ui::PAGE_TRANSITION_RELOAD)) {
     return;
   }
+  if (navigation_handle->GetPreviousPrimaryMainFrameURL() ==
+      navigation_handle->GetURL()) {
+    return;
+  }
 
   // Reset FCP state.
   has_first_contentful_paint_ = false;
@@ -160,7 +164,7 @@ void ContextualCueingHelper::DidFinishNavigation(
         web_contents()->GetPrimaryPage());
   }
 
-  // Ignore fragment changes.
+  // Ignore fragment changes for cueing only.
   if (navigation_handle->GetPreviousPrimaryMainFrameURL().GetWithoutRef() ==
       navigation_handle->GetURL().GetWithoutRef()) {
     return;
