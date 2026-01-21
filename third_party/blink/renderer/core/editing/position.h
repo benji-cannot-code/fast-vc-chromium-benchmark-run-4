1a6917053060819f58e9c4338b7943a6ed505bc2
Chuong Ho <hdchuong@google.com>
2026-01-21 15:19:26 -0800

    blink: fix OffsetInAnchor typo
    Bug: NONE
    Change-Id: I7ca00a134605944ad3e06ef61ad17c589b152a7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7493172
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1572610}

--- third_party/blink/renderer/core/editing/position.h
+++ third_party/blink/renderer/core/editing/position.h
@@ -146,7 +146,7 @@ class PositionTemplate {
   Node* ComputeNodeAfterPosition() const;
 
   // Returns node as |Range::firstNode()|. This position must be a
-  // |PositionAnchorType::OffsetInAhcor| to behave as |Range| boundary point.
+  // |PositionAnchorType::OffsetInAnchor| to behave as |Range| boundary point.
   Node* NodeAsRangeFirstNode() const;
 
   // Similar to |nodeAsRangeLastNode()|, but returns a node in a range.
@@ -154,7 +154,7 @@ class PositionTemplate {
 
   // Returns a node as past last as same as |Range::pastLastNode()|. This
   // function is supposed to used in HTML serialization and plain text
-  // iterator. This position must be a |PositionAnchorType::OffsetInAhcor| to
+  // iterator. This position must be a |PositionAnchorType::OffsetInAnchor| to
   // behave as |Range| boundary point.
   Node* NodeAsRangePastLastNode() const;
 
