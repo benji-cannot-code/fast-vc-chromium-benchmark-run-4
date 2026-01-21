2b204e3e6fa6cdb82edb050650e58fefed7bfe12
Sam Maier <smaier@chromium.org>
2026-01-21 12:31:28 -0800

    Deobfuscate LeakCanary reports
    Bug: 477292844
    Change-Id: I92f40eec9fe294345b9b909dc89745a36a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7504417
    Auto-Submit: Sam Maier <smaier@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1572509}

--- build/android/stacktrace/java/org/chromium/build/FlushingReTrace.java
+++ build/android/stacktrace/java/org/chromium/build/FlushingReTrace.java
@@ -22,9 +22,8 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * A wrapper around ReTrace that:
- *  1. Hardcodes a more useful line regular expression
- *  2. Disables output buffering
+ * A wrapper around ReTrace that: 1. Hardcodes a more useful line regular expression 2. Disables
+ * output buffering
  */
 @NullMarked
 public class FlushingReTrace {
@@ -82,6 +81,14 @@ public class FlushingReTrace {
                     + "(?:.* isTestClass for %c)|"
                     // E.g.: Caused by: java.lang.RuntimeException: Intentional Java Crash
                     + "(?:Caused by: %c:.*)|"
+                    // LeakCanary output looks like:
+                    // ├─ etg instance
+                    // │    Leaking: NO (fs7↓ aQ2 not leaking)
+                    // │    ↓ rQ.createView
+                    + "(?:.*├─ %c .*)|"
+                    + "(?:.*\\(%c↓ .*)|"
+                    + "(?:.*↓ (?:static )?%c\\.%f.*)|"
+
                     // Quoted values and lines that end with a class / class+method:
                     // E.g.: The class: Foo
                     // E.g.: INSTRUMENTATION_STATUS: class=Foo
