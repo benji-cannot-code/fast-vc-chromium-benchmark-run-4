720db602ac941cd55cf18a33ff4a2028e5cf9e85
Abhishek Shanthkumar <abhishek.shanthkumar@microsoft.com>
2026-01-20 23:09:29 -0800

    IDB: Delegate measurement of on-disk usage to the backing store
    Currently, IndexedDBContextImpl::GetBucketUsage() directly examines the
    disk to determine usage, delegating to the BackingStore (transitively
    through BucketContext) only when in-memory. Because of this, on the
    SQLite backing store, the bucket usage cannot reduce immediately after
    completion of a transaction that removes large amounts of data (as
    verified by the BlobsCountAgainstQuota) unless a vacuum + checkpoint is
    performed synchronously after commit. This is prohibitively expensive as
    demonstrated in this CL -
    https://chromium-review.googlesource.com/c/chromium/src/+/7362261 -
    where several WPTs time out.
    
    Hence, delegating the measurement of usage to the backing store (when
    alive) is a prerequisite for implementing a more sophisticated approach
    to vacuuming, and that is the scope of this CL.
    
    A few other functional changes dovetail nicely with this approach:
    
    1. Bucket usage no longer includes both LevelDB and SQLite files (for
    clients that turn on the experimental IdbSqliteBackingStore flag).
    
    2. IndexedDBContextImpl caches usage even when in-memory.
    
    3. In-memory (and on-disk) SQLite usage excludes freelist pages (deleted
    data).
    
    Bug: 419203257
    Change-Id: I70306087f85f66d0aae3660333910b887fdb8334
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7362688
    Reviewed-by: Evan Stade <evanstade@microsoft.com>
    Commit-Queue: Abhishek Shanthkumar <abhishek.shanthkumar@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1572096}

--- content/browser/indexed_db/indexed_db_quota_client_unittest.cc
+++ content/browser/indexed_db/indexed_db_quota_client_unittest.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread.h"
 #include "components/services/storage/public/cpp/buckets/bucket_locator.h"
 #include "components/services/storage/public/mojom/storage_usage_info.mojom.h"
+#include "content/browser/indexed_db/file_path_util.h"
 #include "content/browser/indexed_db/indexed_db_context_impl.h"
 #include "net/base/features.h"
 #include "net/base/schemeful_site.h"
@@ -56,7 +57,9 @@ class IndexedDBQuotaClientTest : public testing::Test,
         kStorageKeyFirstPartyB(
             StorageKey::CreateFromStringForTesting("http://host:8000")),
         special_storage_policy_(
-            base::MakeRefCounted<storage::MockSpecialStoragePolicy>()) {
+            base::MakeRefCounted<storage::MockSpecialStoragePolicy>()),
+        sqlite_override_(BucketContext::OverrideShouldUseSqliteForTesting(
+            IsSqliteBackingStoreEnabled())) {
     // This cannot be created above as the kThirdPartyStoragePartitioning must
     // be set.
     kStorageKeyThirdPartyA =
@@ -156,9 +159,9 @@ class IndexedDBQuotaClientTest : public testing::Test,
       LOG(ERROR) << "failed to base::CreateDirectory "
                  << file_path_storage_key.value();
     }
-    file_path_storage_key =
-        file_path_storage_key.Append(FILE_PATH_LITERAL("fake_file"));
-    SetFileSizeTo(file_path_storage_key, size);
+    const base::FilePath& fake_file_path =
+        file_path_storage_key.Append(DatabaseNameToFileName(u"fake_db"));
+    SetFileSizeTo(fake_file_path, size);
 
     {
       base::RunLoop run_loop;
@@ -201,6 +204,7 @@ class IndexedDBQuotaClientTest : public testing::Test,
 
  protected:
   scoped_refptr<storage::MockSpecialStoragePolicy> special_storage_policy_;
+  base::AutoReset<std::optional<bool>> sqlite_override_;
 
   base::test::TaskEnvironment task_environment_;
   base::ScopedTempDir temp_dir_;
--- content/browser/indexed_db/instance/bucket_context_unittest.cc
+++ content/browser/indexed_db/instance/bucket_context_unittest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_future.h"
 #include "components/services/storage/privileged/mojom/indexed_db_internals_types.mojom.h"
 #include "content/browser/indexed_db/instance/fake_transaction.h"
+#include "content/browser/indexed_db/instance/mock_blob_storage_context.h"
 #include "storage/browser/test/mock_quota_manager_proxy.h"
 #include "storage/browser/test/mock_special_storage_policy.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -50,10 +51,13 @@ class BucketContextTest : public testing::Test {
         storage::BucketInitParams::ForDefaultBucket(
             blink::StorageKey::CreateFromStringForTesting(
                 "https://example.com")));
+    mojo::PendingRemote<storage::mojom::BlobStorageContext>
+        blob_storage_context;
+    mock_blob_storage_context_.Clone(
+        blob_storage_context.InitWithNewPipeAndPassReceiver());
     bucket_context_ = std::make_unique<BucketContext>(
         bucket_info, base::FilePath(), BucketContext::Delegate(),
-        quota_manager_proxy_,
-        /*blob_storage_context=*/mojo::NullRemote(),
+        quota_manager_proxy_, std::move(blob_storage_context),
         /*file_system_access_context=*/mojo::NullRemote());
   }
 
@@ -69,6 +73,7 @@ class BucketContextTest : public testing::Test {
   scoped_refptr<storage::MockSpecialStoragePolicy> quota_policy_;
   scoped_refptr<storage::MockQuotaManager> quota_manager_;
   scoped_refptr<storage::MockQuotaManagerProxy> quota_manager_proxy_;
+  MockBlobStorageContext mock_blob_storage_context_;
   std::unique_ptr<BucketContext> bucket_context_;
 };
 
