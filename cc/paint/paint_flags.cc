a7eb65d64a527473c15c1965173b2897367ecd43
Ben Wagner <bungeman@chromium.org>
2026-01-21 08:44:08 -0800

    Use SkPathBuilder with FillPathWithPaint.
    Migrate calls from the now deprecated skpathutils::FillPathWithPaint
    which took SkPath to the new version of skpathutils::FillPathWithPaint
    which takes SkPathBuilder.
    
    Change-Id: I788160b4e5578e14502b07fab1abe705805896bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7501176
    Reviewed-by: Ben Wagner <bungeman@google.com>
    Commit-Queue: Ben Wagner <bungeman@google.com>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Auto-Submit: Ben Wagner <bungeman@google.com>
    Cr-Commit-Position: refs/heads/main@{#1572364}

--- cc/paint/paint_flags.cc
+++ cc/paint/paint_flags.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/paint/paint_op_buffer.h"
 #include "cc/paint/paint_shader.h"
 #include "third_party/skia/include/core/SkColorFilter.h"
+#include "third_party/skia/include/core/SkPathBuilder.h"
 #include "third_party/skia/include/core/SkPathEffect.h"
 #include "third_party/skia/include/core/SkPathUtils.h"
 
@@ -119,7 +120,12 @@ bool PaintFlags::getFillPath(const SkPath& src,
                              const SkRect* cull_rect,
                              SkScalar res_scale) const {
   SkPaint paint = ToSkPaint();
-  return skpathutils::FillPathWithPaint(src, paint, dst, cull_rect, res_scale);
+  SkPathBuilder dst_builder;
+  bool fill =
+      skpathutils::FillPathWithPaint(src, paint, &dst_builder, cull_rect,
+                                     SkMatrix::Scale(res_scale, res_scale));
+  *dst = dst_builder.detach();
+  return fill;
 }
 
 bool PaintFlags::SupportsFoldingAlpha() const {
