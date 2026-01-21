44757085f4acd7d81aa604c3d1a777bb65cd3fb9
Jan Zarzycki <janzarzycki@google.com>
2026-01-21 07:55:52 -0800

    Add feature flag to control SaaS usage reporting
    Bug: 466007507
    Change-Id: Id5e7a6b21c2b60d25503975318a96fc79f2cb776
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7499006
    Commit-Queue: Jan Zarzycki <janzarzycki@google.com>
    Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1572326}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -355,6 +355,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/enterprise/platform_auth/platform_auth_features.h"
 #endif
 
+#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN)
+#include "chrome/browser/enterprise/reporting/reporting_features.h"
+#endif  // BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN)
+
 #if BUILDFLAG(ENABLE_BOUND_SESSION_CREDENTIALS)
 #include "components/unexportable_keys/features.h"  // nogncheck
 #endif
@@ -13515,6 +13519,14 @@ const FeatureEntry kFeatureEntries[] = {
      kOsAndroid,
      FEATURE_VALUE_TYPE(chrome::android::kAlwaysDrawCompositedToolbarHairline)},
 #endif
+
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
+    {"saas-usage-reporting", flag_descriptions::kSaasUsageReportingName,
+     flag_descriptions::kSaasUsageReportingDescription,
+     kOsLinux | kOsMac | kOsWin,
+     FEATURE_VALUE_TYPE(enterprise_reporting::kSaasUsageReporting)},
+#endif
+
     // Add new entries above this line.
     // NOTE: Adding a new flag requires adding a corresponding entry to enum
     // "LoginCustomFlags" in tools/metrics/histograms/enums.xml. See "Flag
