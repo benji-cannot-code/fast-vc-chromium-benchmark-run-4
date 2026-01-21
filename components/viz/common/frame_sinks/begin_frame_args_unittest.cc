024f28d4abf80c13318343fd10c15eff37bc5310
Sky Debreuil <skydebreuil@google.com>
2026-01-21 06:36:12 -0800

    Add unthrottled_interval to BeginFrameArgs
    Currently, `BeginFrameArgs::interval` represents the target frame
    interval. When throttling is active (e.g. for variable refresh rate),
    this value increases, hiding the underlying hardware refresh rate.
    
    This change adds `unthrottled_interval` to `BeginFrameArgs`. This field
    stores the inverse of the maximum possible hardware refresh rate. This
    allows clients to distinguish between the effective frame rate and the
    native display capability. If no specific unthrottled value is provided,
    it defaults to `interval`.
    
    This CL only adds this new value and has no functional changes.
    
    Bug: 467315115
    Change-Id: I7bcbbd68b6c45e1e80402de3e895cd473afb6ca2
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7429037
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Auto-Submit: Sky Debreuil <skydebreuil@google.com>
    Commit-Queue: Sky Debreuil <skydebreuil@google.com>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1572281}

--- components/viz/common/frame_sinks/begin_frame_args_unittest.cc
+++ components/viz/common/frame_sinks/begin_frame_args_unittest.cc
@@ -3,9 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "components/viz/common/frame_sinks/begin_frame_args.h"
+
 #include <string>
 
-#include "components/viz/common/frame_sinks/begin_frame_args.h"
+#include "base/test/gtest_util.h"
+#include "build/build_config.h"
 #include "components/viz/test/begin_frame_args_test.h"
 #include "testing/gtest/include/gtest/gtest-spi.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -85,15 +88,19 @@ TEST(BeginFrameArgsTest, Helpers) {
   // operator<<
   std::stringstream out1;
   out1 << args1;
-  EXPECT_EQ("BeginFrameArgs(NORMAL, 0, 1, 0, 0, -1us, 0)", out1.str());
+  EXPECT_EQ("BeginFrameArgs(NORMAL, 0, 1, 0, 0, -1us, -1us (unthrottled), 0)",
+            out1.str());
   std::stringstream out2;
   out2 << args2;
-  EXPECT_EQ("BeginFrameArgs(NORMAL, 123, 10, 1, 2, 3us, 0)", out2.str());
+  EXPECT_EQ("BeginFrameArgs(NORMAL, 123, 10, 1, 2, 3us, 3us (unthrottled), 0)",
+            out2.str());
 
   // PrintTo
-  EXPECT_EQ(std::string("BeginFrameArgs(NORMAL, 0, 1, 0, 0, -1us, 0)"),
+  EXPECT_EQ(std::string("BeginFrameArgs(NORMAL, 0, 1, 0, 0, -1us, "
+                        "-1us (unthrottled), 0)"),
             ::testing::PrintToString(args1));
-  EXPECT_EQ(std::string("BeginFrameArgs(NORMAL, 123, 10, 1, 2, 3us, 0)"),
+  EXPECT_EQ(std::string("BeginFrameArgs(NORMAL, 123, 10, 1, 2, 3us, "
+                        "3us (unthrottled), 0)"),
             ::testing::PrintToString(args2));
 }
 
@@ -127,6 +134,24 @@ TEST(BeginFrameArgsTest, Location) {
   EXPECT_EQ(expected_location.ToString(), args.created_from.ToString());
 }
 #endif
+
+// TODO(crbug.com/477242770): Changing the system display refresh rate on macOS
+// causes the unthrottled_interval state to go stale, which incorrectly trips
+// the DCHECK enforced by this test. Re-enable once this is fixed.
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_InvalidUnthrottledInterval DISABLED_InvalidUnthrottledInterval
+#else
+#define MAYBE_InvalidUnthrottledInterval InvalidUnthrottledInterval
+#endif
+
+TEST(BeginFrameArgsTest, MAYBE_InvalidUnthrottledInterval) {
+#if DCHECK_IS_ON()
+  EXPECT_DCHECK_DEATH(BeginFrameArgs::Create(
+      BEGINFRAME_FROM_HERE, 0, BeginFrameArgs::kStartingFrameNumber,
+      base::TimeTicks(), base::TimeTicks(), base::Milliseconds(16),
+      BeginFrameArgs::NORMAL, base::Milliseconds(25)));
+#endif
+}
 
 }  // namespace
 }  // namespace viz
