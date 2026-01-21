5c8e371aaa0e96d542464bf83c749ec1e5eb4eb7
Mike West <mkwst@chromium.org>
2026-01-21 05:46:50 -0800

    Remove the `StrictPowerfulFeatureRestriction` flag.
    This flag is currently a no-op, as the features it once restricted have
    all been moved to `[SecureContext]` in the ~decade since we added it.
    
    Bug: 40431384
    Change-Id: Ia872cee2230a42d51252f17c1f2c541749cc6420
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7502477
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1572255}

--- third_party/blink/renderer/core/exported/web_settings_impl.cc
+++ third_party/blink/renderer/core/exported/web_settings_impl.cc
@@ -554,10 +554,6 @@ void WebSettingsImpl::SetStrictMixedContentCheckingForPlugin(bool enabled) {
   settings_->SetStrictMixedContentCheckingForPlugin(enabled);
 }
 
-void WebSettingsImpl::SetStrictPowerfulFeatureRestrictions(bool enabled) {
-  settings_->SetStrictPowerfulFeatureRestrictions(enabled);
-}
-
 void WebSettingsImpl::SetStrictlyBlockBlockableMixedContent(bool enabled) {
   settings_->SetStrictlyBlockBlockableMixedContent(enabled);
 }
