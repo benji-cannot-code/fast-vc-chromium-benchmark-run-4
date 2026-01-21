5c994a539cbbb9727eb506f5370f098928c6fccf
Jeffrey Yu <yuje@chromium.org>
2026-01-21 10:08:29 -0800

    Use string_view instead of string to avoid string copies.
    `.substr()` on string_view doesn't make a copy. It just returns a new
    string_view with moved pointers and/or size.
    
    Change-Id: I42201e056097d9dd5b944153af1b779c4993245d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7437210
    Reviewed-by: Crisrael Lucero <crisrael@google.com>
    Auto-Submit: Jeffrey Yu <yuje@chromium.org>
    Commit-Queue: Michael Hansen <hansenmichael@google.com>
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Cr-Commit-Position: refs/heads/main@{#1572413}

--- chrome/browser/nearby_sharing/logging/proto_to_dictionary_conversion.cc
+++ chrome/browser/nearby_sharing/logging/proto_to_dictionary_conversion.cc
@@ -6,9 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/nearby_sharing/logging/proto_to_dictionary_conversion.h"
 
 #include <string>
+#include <string_view>
 #include <utility>
 
 #include "base/base64url.h"
+#include "base/strings/strcat.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/time/time.h"
 
@@ -20,10 +22,11 @@ std::string Encode(const std::string& str) {
   return encoded_string;
 }
 
-std::string TruncateString(const std::string& str) {
+std::string TruncateString(std::string_view str) {
   if (str.length() <= 10)
-    return str;
-  return str.substr(0, 5) + "..." + str.substr(str.length() - 5, str.length());
+    return std::string(str);
+  return base::StrCat(
+      {str.substr(0, 5), "...", str.substr(str.length() - 5, str.length())});
 }
 }  // namespace
 
