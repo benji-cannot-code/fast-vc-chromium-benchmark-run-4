da84b55ef1b952009995583b0dd5a015145d4c8b
Alison Gale <agale@chromium.org>
2026-01-21 08:49:46 -0800

    Remove unused dependency on tabs component
    Bug: 443780638
    Change-Id: I8c435b3f65f2c26ce882e377ac32f189fb5ce90c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7503391
    Auto-Submit: Alison Gale <agale@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1572369}

--- components/page_content_annotations/content/page_context_fetcher.cc
+++ components/page_content_annotations/content/page_context_fetcher.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/paint_preview/common/mojom/paint_preview_types.mojom.h"
 #include "components/paint_preview/common/redaction_params.h"
 #include "components/pdf/common/constants.h"
-#include "components/tabs/public/tab_interface.h"
 #include "components/viz/common/frame_sinks/copy_output_result.h"
 #include "content/public/browser/render_widget_host_view.h"
 #include "content/public/browser/web_contents.h"
