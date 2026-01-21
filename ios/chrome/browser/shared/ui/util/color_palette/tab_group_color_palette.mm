429d150e032c3acb8852d0f8a861c756e5d2c16d
Anis Chara <achara@google.com>
2026-01-21 02:12:54 -0800

    [iOS] Move TabGroupColorPalette to browser/shared/ui/util/color_palette
    This cl moves the TabGroupColorPalette files to
    browser/shared/ui/util/color_palette since it will be used outside of
    the tab_switcher
    
    Bug: 445887468
    Change-Id: I7558e7b632032fb0073d804069eba1a8e564fcdc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7497817
    Commit-Queue: Anis Chara <achara@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1572164}

--- ios/chrome/browser/shared/ui/util/color_palette/tab_group_color_palette.mm
+++ ios/chrome/browser/shared/ui/util/color_palette/tab_group_color_palette.mm
@@ -3,9 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/tab_switcher/util/tab_group_color_palette.h"
+#import "ios/chrome/browser/shared/ui/util/color_palette/tab_group_color_palette.h"
 
-#import <CoreGraphics/CGColor.h>
+#import <CoreGraphics/CoreGraphics.h>
 #import <Foundation/Foundation.h>
 
 #import "base/memory/raw_ptr.h"
