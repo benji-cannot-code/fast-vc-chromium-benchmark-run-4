5f3f9e8ece2ca49697cddd55ce84bc9ada6a8168
Jeffrey Yu <yuje@chromium.org>
2026-01-21 04:31:19 -0800

    Replace `std::unordered_set` with `absl::flat_hash_set`.
    The Chromium containers guide
    (https://chromium.googlesource.com/chromium/src/+/HEAD/base/containers/README.md)
    recommends using `absl::flat_hash_set` as a default map choice instead
    of `std::unordered_set` when pointer stability isn't needed, and
    `absl::node_hash_set` when it is. Both will still generally perform
    better and require less overhead than `std::unordered_set`.
    
    Bug: 473916362
    Change-Id: I5e8522b1222d98d0ed9dfa43773a29c3aaca8428
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7499212
    Auto-Submit: Jeffrey Yu <yuje@chromium.org>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1572219}

--- components/affiliations/core/browser/affiliation_backend.cc
+++ components/affiliations/core/browser/affiliation_backend.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/affiliations/core/browser/affiliation_utils.h"
 #include "components/affiliations/core/browser/facet_manager.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
+#include "third_party/abseil-cpp/absl/container/flat_hash_set.h"
 
 namespace affiliations {
 
@@ -180,7 +181,7 @@ void AffiliationBackend::TrimUnusedCache(std::vector<FacetURI> facet_uris) {
 std::vector<GroupedFacets> AffiliationBackend::GetGroupingInfo(
     std::vector<FacetURI> facet_uris) const {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  std::unordered_set<std::string> facets_in_response;
+  absl::flat_hash_set<std::string> facets_in_response;
   std::vector<GroupedFacets> result;
 
   for (FacetURI& facet : facet_uris) {
