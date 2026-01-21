41662214a26318e4ed7d8db625e0e405f6a52f74
Kunal Daftari <kunaldaftari@google.com>
2026-01-21 15:05:54 -0800

    [Vertical Tabs] Removing fixed bug from state controller
    Forgot to remove a TODO after resolving a bug.
    
    Removing it in this CL.
    
    Change-Id: Id8b5f8d9a02d653520470fde05a28bfe2025b1ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7506931
    Commit-Queue: Kunal Daftari <kunaldaftari@google.com>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1572603}

--- chrome/browser/ui/tabs/vertical_tab_strip_state_controller.cc
+++ chrome/browser/ui/tabs/vertical_tab_strip_state_controller.cc
@@ -76,8 +76,6 @@ VerticalTabStripStateController::VerticalTabStripStateController(
       browser_list_observation_.Observe(BrowserList::GetInstance());
     }
   }
-
-  // TODO(crbug.com/455559992): Add uncollapsed text logic for collapse button.
 }
 
 VerticalTabStripStateController::~VerticalTabStripStateController() {
