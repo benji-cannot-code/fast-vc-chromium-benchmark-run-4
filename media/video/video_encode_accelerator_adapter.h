2072d4e41af42de2545e7e52a787f1d0887b0fbd
Eugene Zemtsov <eugene@chromium.org>
2026-01-20 16:53:05 -0800

    media: Default to NV12 in VideoEncodeAcceleratorAdapter
    Updates VideoEncodeAcceleratorAdapter to default to PIXEL_FORMAT_NV12 on
    most platforms (excluding Fuchsia).
    This aligns better with hardware encoder preferences.
    
    Bug: 477318782, 40241991
    Test: CI/CQ bots
    Change-Id: Iffb518ff1ea726a386dfacdf64f246eb15e43f01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7488040
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1571970}

--- media/video/video_encode_accelerator_adapter.h
+++ media/video/video_encode_accelerator_adapter.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequence_checker.h"
 #include "base/synchronization/lock.h"
 #include "base/time/time.h"
+#include "build/build_config.h"
 #include "media/base/media_export.h"
 #include "media/base/video_encoder.h"
 #include "media/base/video_frame_converter.h"
@@ -199,6 +200,12 @@ class MEDIA_EXPORT VideoEncodeAcceleratorAdapter
 
   VideoEncodeAccelerator::Config::EncoderType required_encoder_type_ =
       VideoEncodeAccelerator::Config::EncoderType::kHardware;
+#if BUILDFLAG(IS_FUCHSIA)
+  // TODO(crbug.com/40241991): Fuchsia only supports I420 for now.
+  static constexpr VideoPixelFormat kDefaultPixelFormat = PIXEL_FORMAT_I420;
+#else
+  static constexpr VideoPixelFormat kDefaultPixelFormat = PIXEL_FORMAT_NV12;
+#endif
   bool supports_frame_size_change_ = false;
   bool supports_gpu_shared_images_ = false;
 };
