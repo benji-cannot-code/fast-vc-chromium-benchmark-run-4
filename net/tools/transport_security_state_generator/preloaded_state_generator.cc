fa426d24deac101d78927e22f1de10636ac8a224
Nick Harper <nharper@chromium.org>
2026-01-21 11:31:19 -0800

    Add logs for byte sizes to hsts generator
    Change-Id: I400f26d8874be9f0ed697456330d8d30687a5b0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7489263
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Auto-Submit: Nick Harper <nharper@chromium.org>
    Commit-Queue: Nick Harper <nharper@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1572472}

--- net/tools/transport_security_state_generator/preloaded_state_generator.cc
+++ net/tools/transport_security_state_generator/preloaded_state_generator.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
@@ -175,6 +176,10 @@ std::string PreloadedStateGenerator::Generate(
 
   ReplaceTag("PINS_LIST_TIMESTAMP", base::NumberToString(timestamp.ToTimeT()),
              &output);
+
+  VLOG(1) << "PreloadedStateGenerator wrote " << new_writer.bytes().size()
+          << " bytes for the huffman table and " << huffman_tree.size()
+          << " bytes for the trie.";
 
   return output;
 }
