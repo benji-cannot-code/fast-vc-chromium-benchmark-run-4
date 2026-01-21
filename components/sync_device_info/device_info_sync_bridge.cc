f612994ae4bda843b2b1955fbca497edf69f1688
Jeffrey Yu <yuje@chromium.org>
2026-01-21 02:29:41 -0800

    Replace `std::unordered_set` with `absl::flat_hash_set`.
    The Chromium containers guide
    (https://chromium.googlesource.com/chromium/src/+/HEAD/base/containers/README.md)
    recommends using `absl::flat_hash_set` as a default map choice instead
    of `std::unordered_set` when pointer stability isn't needed, and
    `absl::node_hash_set` when it is. Both will still generally perform
    better and require less overhead than `std::unordered_set`.
    
    Bug: 473916362
    Change-Id: I30c45a399299ac7a9cee67e43b1174ca076d1344
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7499073
    Auto-Submit: Jeffrey Yu <yuje@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1572175}

--- components/sync_device_info/device_info_sync_bridge.cc
+++ components/sync_device_info/device_info_sync_bridge.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstdio>
 #include <map>
 #include <optional>
-#include <unordered_set>
 #include <utility>
 
 #include "base/compiler_specific.h"
@@ -44,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync_device_info/device_info_util.h"
 #include "components/sync_device_info/local_device_info_util.h"
 #include "third_party/abseil-cpp/absl/container/flat_hash_map.h"
+#include "third_party/abseil-cpp/absl/container/flat_hash_set.h"
 
 namespace syncer {
 
@@ -1071,7 +1071,7 @@ void DeviceInfoSyncBridge::ExpireOldEntries() {
   TRACE_EVENT0("sync", "DeviceInfoSyncBridge::ExpireOldEntries");
   const base::Time expiration_threshold =
       base::Time::Now() - kExpirationThreshold;
-  std::unordered_set<std::string> cache_guids_to_expire;
+  absl::flat_hash_set<std::string> cache_guids_to_expire;
   // Just collecting cache guids to expire to avoid modifying |all_data_| via
   // DeleteSpecifics() while iterating over it.
   for (const auto& [cache_guid, device_info_and_specifics] : all_data_) {
