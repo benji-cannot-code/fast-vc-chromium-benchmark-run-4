adc112515fadb0467d4564e4c65dccb5eeb0c85b
Kenichi Ishibashi <bashi@chromium.org>
2026-01-20 19:20:31 -0800

    HttpStreamPool: Add NetLog events in the new Attempt class
    This CL adds NetLog events to HttpStreamPool::Attempt to track the
    lifecycle of TCP attempts. These are almost the same events as
    those in the existing HttpStreamPool::TcpBasedAttempt class, which
    will be replaced by the new Attempt class once the rework completes.
    
    Bug: 457478038
    Change-Id: I5e56b6f97e3723e24e40c90f39a2eded7a848f79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7454156
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1572027}

--- net/http/http_stream_pool_attempt.h
+++ net/http/http_stream_pool_attempt.h
@@ -82,7 +82,9 @@ class HttpStreamPool::Attempt {
         scoped_refptr<SSLCertRequestInfo> cert_info) = 0;
   };
 
-  Attempt(Delegate& delegate, const StreamAttemptParams& stream_attempt_params);
+  Attempt(Delegate& delegate,
+          const StreamAttemptParams& stream_attempt_params,
+          NetLogWithSource net_log);
 
   ~Attempt();
 
@@ -141,6 +143,8 @@ class HttpStreamPool::Attempt {
   const bool using_tls_;
 
   const perfetto::Track track_;
+
+  const NetLogWithSource net_log_;
 
   std::unique_ptr<TcpAttempt> ipv4_attempt_;
   std::unique_ptr<TcpAttempt> ipv6_attempt_;
