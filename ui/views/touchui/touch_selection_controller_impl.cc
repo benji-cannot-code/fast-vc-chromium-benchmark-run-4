c0a7b4b5af0260561cf283ccb65d99331615133c
Allen Bauer <kylixrd@chromium.org>
2026-01-21 10:59:05 -0800

    [Views] Touch selection bubbles should be a floating element.
    This will, hopefully, solve the issue where the selector bubble widgets
    get obscured by the WebUI Omnibox popup.
    
    Bug: 460023940
    Change-Id: I376e5d49ebfccc717359f6b857942c87e7abb96d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7505836
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1572454}

--- ui/views/touchui/touch_selection_controller_impl.cc
+++ ui/views/touchui/touch_selection_controller_impl.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/base/models/image_model.h"
 #include "ui/base/ui_base_features.h"
+#include "ui/base/ui_base_types.h"
 #include "ui/color/color_id.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/geometry/rect.h"
@@ -189,6 +190,7 @@ std::unique_ptr<views::Widget> CreateHandleWidget(gfx::NativeView parent) {
   // overridden by fade effects.
   widget->SetVisibilityChangedAnimationsEnabled(false);
   widget->SetOpacity(kSelectionHandleOpacity);
+  widget->SetZOrderLevel(ui::ZOrderLevel::kFloatingUIElement);
 
   return widget;
 }
