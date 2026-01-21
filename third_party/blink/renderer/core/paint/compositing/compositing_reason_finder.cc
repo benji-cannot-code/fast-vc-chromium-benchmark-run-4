9db56d6fa1666af2b1c719fcda560352cc6b81e5
Stefan Zager <szager@chromium.org>
2026-01-21 14:42:44 -0800

    [HIC] Create composited layers for canvas descendants
    Composited layers will be created unconditionally for all drawn canvas
    direct children. At the grandchild level and below, composited layers
    will be created according to the same heuristics as non-canvas html
    content.
    
    This is a first step towards enabling compositor-thread scrolling and
    animations in html-in-canvas content.
    
    Bug: 475579174
    Change-Id: Id4aafd0ea0924c51e16e5a5fb4db8925ca418542
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7487907
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1572581}

--- third_party/blink/renderer/core/paint/compositing/compositing_reason_finder.cc
+++ third_party/blink/renderer/core/paint/compositing/compositing_reason_finder.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/settings.h"
 #include "third_party/blink/renderer/core/frame/visual_viewport.h"
 #include "third_party/blink/renderer/core/fullscreen/fullscreen.h"
+#include "third_party/blink/renderer/core/html/canvas/html_canvas_element.h"
 #include "third_party/blink/renderer/core/html/html_body_element.h"
 #include "third_party/blink/renderer/core/layout/layout_embedded_content.h"
 #include "third_party/blink/renderer/core/layout/layout_view.h"
@@ -334,13 +335,16 @@ CompositingReasons CompositingReasonFinder::DirectReasonsForPaintProperties(
   if (object.GetDocument().Printing())
     return CompositingReason::kNone;
 
-  // Elements under canvas can only be rendered with `drawElementImage` and do
-  // not support compositing.
-  if (RuntimeEnabledFeatures::CanvasDrawElementEnabled() &&
-      IsA<Element>(object.GetNode()) &&
-      To<Element>(object.GetNode())->IsInCanvasSubtree()) {
-    return CompositingReason::kNone;
+  auto* element = DynamicTo<Element>(object.GetNode());
+  if (element && RuntimeEnabledFeatures::CanvasDrawElementEnabled()) {
+    if (auto* canvas = DynamicTo<HTMLCanvasElement>(
+            element->ParentOrShadowHostNode())) [[unlikely]] {
+      if (canvas->layoutSubtree()) {
+        return CompositingReason::kCanvasChild;
+      }
+    }
   }
+
   auto reasons = CompositingReasonsFor3DSceneLeaf(object);
 
   if (object.CanHaveAdditionalCompositingReasons())
@@ -406,7 +410,6 @@ CompositingReasons CompositingReasonFinder::DirectReasonsForPaintProperties(
         }
       });
 
-  auto* element = DynamicTo<Element>(object.GetNode());
   if (element && element->GetRestrictionTargetId()) {
     const bool is_eligible = IsEligibleForElementCapture(object);
     element->SetIsEligibleForElementCapture(is_eligible);
