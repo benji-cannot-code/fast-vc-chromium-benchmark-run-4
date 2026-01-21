1f97bcadadec526349ad24f33d871a9e9a9caa07
Michael Wilson <mjwilson@chromium.org>
2026-01-20 23:59:23 -0800

    Prepare configurable render quantum for Origin Trial
    In order to run an Origin Trial with this feature, update the flag and
    use counters and also add a public WPT.
    
    Bug: 477311772
    Bug: 40637820
    Change-Id: I11248768ae3d6f4f0999de450804ae28612c0386
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7488230
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1572110}

--- third_party/blink/renderer/modules/webaudio/offline_audio_context.cc
+++ third_party/blink/renderer/modules/webaudio/offline_audio_context.cc
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/bindings/script_state.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
+#include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
 #include "third_party/blink/renderer/platform/wtf/cross_thread_functional.h"
 #include "third_party/blink/renderer/platform/wtf/math_extras.h"
 #include "third_party/blink/renderer/platform/wtf/text/strcat.h"
@@ -147,8 +148,10 @@ OfflineAudioContext* OfflineAudioContext::Create(
     const OfflineAudioContextOptions* options,
     ExceptionState& exception_state) {
   uint32_t render_quantum_frames = 128;
-  if (RuntimeEnabledFeatures::WebAudioConfigurableRenderQuantumEnabled() &&
+  if (RuntimeEnabledFeatures::WebAudioConfigurableRenderQuantumEnabled(
+          context) &&
       options->hasRenderSizeHint()) {
+    UseCounter::Count(context, WebFeature::kWebAudioRenderSizeHint);
     if (options->renderSizeHint()->IsUnsignedLong()) {
       render_quantum_frames = options->renderSizeHint()->GetAsUnsignedLong();
     }
