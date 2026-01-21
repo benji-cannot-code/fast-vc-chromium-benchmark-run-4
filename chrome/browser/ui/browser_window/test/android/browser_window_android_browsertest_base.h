ce499b95a1b8e4e4b6cf2934b6a12bca168b4b6f
Jack Thiesen <jthiesen@chromium.org>
2026-01-21 11:11:04 -0800

    BrowserWindowAndroidBrowserTestBase Class to Avoid Repetition
    Our browser tests for the Android Desktop Browser Windows involved some
    repeated boilerplate. This adds a base class to share this boilerplate.
    
    Bug: 441594986
    Change-Id: Icccbd096b02c7d7690ea865148541ab6388ea028
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7473365
    Commit-Queue: Jack Thiesen <jthiesen@chromium.org>
    Reviewed-by: Linyu He <linyuh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1572464}

---
+++ chrome/browser/ui/browser_window/test/android/browser_window_android_browsertest_base.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_BROWSER_WINDOW_TEST_ANDROID_BROWSER_WINDOW_ANDROID_BROWSERTEST_BASE_H_
+#define CHROME_BROWSER_UI_BROWSER_WINDOW_TEST_ANDROID_BROWSER_WINDOW_ANDROID_BROWSERTEST_BASE_H_
+
+#include "base/test/scoped_feature_list.h"
+#include "chrome/test/base/android/android_browser_test.h"
+
+class BrowserWindowAndroidBrowserTestBase : public AndroidBrowserTest {
+ public:
+  BrowserWindowAndroidBrowserTestBase();
+  ~BrowserWindowAndroidBrowserTestBase() override;
+
+  // AndroidBrowserTest:
+  void SetUpDefaultCommandLine(base::CommandLine* command_line) override;
+
+ private:
+  base::test::ScopedFeatureList feature_list_;
+};
+
+#endif  // CHROME_BROWSER_UI_BROWSER_WINDOW_TEST_ANDROID_BROWSER_WINDOW_ANDROID_BROWSERTEST_BASE_H_
