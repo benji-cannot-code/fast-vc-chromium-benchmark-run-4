2413d474043f619d557f140ab39cb8f86c0dea95
Hiroki Nakagawa <nhiroki@chromium.org>
2026-01-20 16:53:57 -0800

    Prerender: Remove redundant frame tree node ID from PrerenderInfo
    Simplify PrerendererImpl::PrerenderInfo by removing the
    prerender_frame_tree_node_id field. This field was redundant as the
    prerender_host_id is already tracked and provides sufficient
    identification for managing the prerender state.
    
    The logic is updated to check for a null prerender_host_id instead of a
    null prerender_frame_tree_node_id when determining if a prerender has
    started.
    
    Bug: 434826191
    Change-Id: I672ca13da4bbdf5728437c3283162d3672400596
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7498032
    Auto-Submit: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Huanpo Lin <robertlin@chromium.org>
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1571971}

--- content/browser/preloading/prerenderer_impl.cc
+++ content/browser/preloading/prerenderer_impl.cc
@@ -55,7 +55,6 @@ struct PrerendererImpl::PrerenderInfo {
   blink::mojom::SpeculationEagerness eagerness;
   blink::mojom::SpeculationAction action;
   bool is_target_blank;
-  FrameTreeNodeId prerender_frame_tree_node_id;
   PrerenderHostId prerender_host_id;
   GURL url;
 
@@ -87,7 +86,7 @@ bool PrerendererImpl::PrerenderInfo::PrerenderInfoComparator(
 }
 
 // `prerender_host_id` is not provided by `SpeculationCandidatePtr`, so
-// FrameTreeNodeId() is assigned instead. The value should be updated once it is
+// PrerenderHostId() is assigned instead. The value should be updated once it is
 // available.
 PrerendererImpl::PrerenderInfo::PrerenderInfo(
     const blink::mojom::SpeculationCandidatePtr& candidate)
@@ -478,9 +477,6 @@ bool PrerendererImpl::MaybePrerender(
     }
   }();
 
-  prerender_info.prerender_frame_tree_node_id =
-      PrerenderHost::GetFrameTreeNodeIdForId(prerender_info.prerender_host_id);
-
   // An existing prerender may be canceled to start a new prerender, and
   // `started_prerenders_` may be modified through this cancellation. Therefore,
   // it is needed to re-calculate the right place here on `started_prerenders_`
@@ -504,7 +500,7 @@ bool PrerendererImpl::ShouldWaitForPrerenderResult(const GURL& url) {
       started_prerenders_.begin(), started_prerenders_.end(), url,
       std::less<>(), &PrerenderInfo::url);
   for (auto it = begin; it != end; ++it) {
-    if (it->prerender_frame_tree_node_id.is_null()) {
+    if (it->prerender_host_id.is_null()) {
       return false;
     }
   }
