5f240ab9d412a13d18d7e3b70af169b96cb9156a
Wolfgang Beyer <wolfi@chromium.org>
2026-01-21 03:57:51 -0800

    [DevTools] Add unit tests for `GetHostConfig()`
    Bug: 464173045
    Change-Id: I7215940b3c4b01ca684a750047645a0cfd07ad78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7502595
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Auto-Submit: Wolfgang Beyer <wolfi@chromium.org>
    Commit-Queue: Wolfgang Beyer <wolfi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1572212}

--- chrome/browser/devtools/devtools_ui_bindings.cc
+++ chrome/browser/devtools/devtools_ui_bindings.cc
@@ -1733,10 +1733,12 @@ base::Value::Dict DevToolsUIBindings::GetSyncInformationForProfile(
   return result;
 }
 
-void DevToolsUIBindings::GetHostConfig(DispatchCallback callback) {
+// static
+base::Value::Dict DevToolsUIBindings::GetHostConfigDictionary(
+    Profile* profile) {
   base::Value::Dict response_dict;
 
-  AidaClient::Availability availability = AidaClient::CanUseAida(profile_);
+  AidaClient::Availability availability = AidaClient::CanUseAida(profile);
 
   base::Value::Dict aida_availability;
   aida_availability.Set("enabled", availability.available);
@@ -1910,7 +1912,7 @@ void DevToolsUIBindings::GetHostConfig(DispatchCallback callback) {
   ve_logging_dict.Set("testing", false);
   response_dict.Set("devToolsVeLogging", std::move(ve_logging_dict));
 
-  response_dict.Set("isOffTheRecord", profile_->IsOffTheRecord());
+  response_dict.Set("isOffTheRecord", profile->IsOffTheRecord());
 
   base::Value::Dict devtools_privacy_ui_dict;
   devtools_privacy_ui_dict.Set(
@@ -1933,7 +1935,7 @@ void DevToolsUIBindings::GetHostConfig(DispatchCallback callback) {
             content_settings::features::kTpcdHeuristicsGrants));
 
     policy::PolicyService* policy_service =
-        profile()->GetProfilePolicyConnector()->policy_service();
+        profile->GetProfilePolicyConnector()->policy_service();
     CHECK(policy_service);
     const policy::PolicyMap& policies = policy_service->GetPolicies(
         policy::PolicyNamespace(policy::POLICY_DOMAIN_CHROME, std::string()));
@@ -2039,7 +2041,7 @@ void DevToolsUIBindings::GetHostConfig(DispatchCallback callback) {
 #endif
   gdp_profiles_availability_dict.Set(
       "enterprisePolicyValue",
-      profile_->GetPrefs()->GetInteger(
+      profile->GetPrefs()->GetInteger(
           prefs::kDevToolsGoogleDeveloperProgramProfileAvailability));
   response_dict.Set("devToolsGdpProfilesAvailability",
                     std::move(gdp_profiles_availability_dict));
@@ -2089,7 +2091,11 @@ void DevToolsUIBindings::GetHostConfig(DispatchCallback callback) {
   response_dict.Set("devToolsConsoleInsightsTeasers",
                     std::move(console_insights_teasers_dict));
 
-  base::Value response = base::Value(std::move(response_dict));
+  return response_dict;
+}
+
+void DevToolsUIBindings::GetHostConfig(DispatchCallback callback) {
+  base::Value response(GetHostConfigDictionary(profile_));
   std::move(callback).Run(&response);
 }
 
