39ebbf48a153f7939efd726781a73f9c3755d724
Jeffrey Yu <yuje@chromium.org>
2026-01-21 05:33:03 -0800

    Replace `std::unordered_set` with `absl::flat_hash_set`.
    The Chromium containers guide
    (https://chromium.googlesource.com/chromium/src/+/HEAD/base/containers/README.md)
    recommends using `absl::flat_hash_set` as a default map choice instead
    of `std::unordered_set` when pointer stability isn't needed, and
    `absl::node_hash_set` when it is. Both will still generally perform
    better and require less overhead than `std::unordered_set`.
    
    Bug: 473916362
    Change-Id: I72af7ada9add19bf01e5b44c29313d4b33fe6123
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7495663
    Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Auto-Submit: Jeffrey Yu <yuje@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1572247}

--- ui/base/ime/ash/input_method_util.cc
+++ ui/base/ime/ash/input_method_util.cc
@@ -16,19 +16,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <functional>
 #include <map>
 #include <memory>
+#include <optional>  // For SetHardwareKeyboardLayoutForTesting.
 #include <string_view>
-#include <unordered_set>
 #include <utility>
 
 #include "base/logging.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
+#include "third_party/abseil-cpp/absl/container/flat_hash_set.h"
 #include "ui/base/ime/ash/component_extension_ime_manager.h"
 #include "ui/base/ime/ash/extension_ime_util.h"
-// For SetHardwareKeyboardLayoutForTesting.
-#include <optional>
-
 #include "ui/base/ime/ash/fake_input_method_delegate.h"
 #include "ui/base/ime/ash/input_method_delegate.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -795,12 +793,11 @@ bool InputMethodUtil::GetMigratedInputMethodIDs(
   if (rewritten) {
     // Removes the duplicates.
     std::vector<std::string> new_ids;
-    std::unordered_set<std::string> ids_set;
+    absl::flat_hash_set<std::string> ids_set;
     for (const auto& id : ids) {
-      if (ids_set.find(id) == ids_set.end()) {
+      if (ids_set.insert(id).second) {
         new_ids.push_back(id);
       }
-      ids_set.insert(id);
     }
     ids.swap(new_ids);
   }
