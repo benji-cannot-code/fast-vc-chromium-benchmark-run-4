5cef95d1498ff2182b1cd7dc5ee6a62ae8b3e999
Free Debreuil <freedebreuil@google.com>
2026-01-21 13:54:49 -0800

    Support single-axis scroll containers in StyleAdjuster
    Guarded by the SingleAxisScrollContainers feature flag, this CL allows
    StyleAdjuster to accept mixed overflow values (e.g. overflow-x: scroll;
    overflow-y: visible) without forcing the non-scrollable axis to 'auto'
    or 'hidden'.
    
    Bug: 477444701
    Change-Id: I58ed1adf66e83a4025ce822f9708f0bf0ee8d183
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7506709
    Commit-Queue: Free Debreuil <freedebreuil@google.com>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1572555}

--- third_party/blink/renderer/core/css/resolver/style_adjuster.cc
+++ third_party/blink/renderer/core/css/resolver/style_adjuster.cc
@@ -665,7 +665,8 @@ void StyleAdjuster::AdjustOverflow(ComputedStyleBuilder& builder,
     } else if (builder.OverflowY() == EOverflow::kVisible) {
       builder.SetOverflowX(EOverflow::kVisible);
     }
-  } else if (!IsOverflowClipOrVisible(builder.OverflowY())) {
+  } else if (!RuntimeEnabledFeatures::SingleAxisScrollContainersEnabled() &&
+             !IsOverflowClipOrVisible(builder.OverflowY())) {
     // Values of 'clip' and 'visible' can only be used with 'clip' and
     // 'visible.' If they aren't, 'clip' and 'visible' is reset.
     if (builder.OverflowX() == EOverflow::kVisible) {
@@ -673,7 +674,8 @@ void StyleAdjuster::AdjustOverflow(ComputedStyleBuilder& builder,
     } else if (builder.OverflowX() == EOverflow::kClip) {
       builder.SetOverflowX(EOverflow::kHidden);
     }
-  } else if (!IsOverflowClipOrVisible(builder.OverflowX())) {
+  } else if (!RuntimeEnabledFeatures::SingleAxisScrollContainersEnabled() &&
+             !IsOverflowClipOrVisible(builder.OverflowX())) {
     // Values of 'clip' and 'visible' can only be used with 'clip' and
     // 'visible.' If they aren't, 'clip' and 'visible' is reset.
     if (builder.OverflowY() == EOverflow::kVisible) {
