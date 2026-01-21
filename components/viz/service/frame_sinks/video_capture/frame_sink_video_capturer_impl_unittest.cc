f7371221c11faa2dedb93c947ed135f8b0396aaf
Colin Blundell <blundell@chromium.org>
2026-01-21 00:18:14 -0800

    [Viz] Rename FrameSinkVideoCapturerImpl method
    This method should now refer to MappableSI.
    
    Bug: 404905709
    Change-Id: If843023fda06ad23a56c4fc68b558061f333483e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7498152
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Mingjing Zhang <mjzhang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1572123}

--- components/viz/service/frame_sinks/video_capture/frame_sink_video_capturer_impl_unittest.cc
+++ components/viz/service/frame_sinks/video_capture/frame_sink_video_capturer_impl_unittest.cc
@@ -2270,8 +2270,9 @@ TEST_P(FrameSinkVideoCapturerTest, BufferFormatPreferencePassedToGpuFramePool) {
 
   NiceMock<MockConsumer> consumer;
   StartCapture(&consumer);
-  EXPECT_EQ(capturer_->gpu_frame_pool_buffer_format_for_testing(),
-            buffer_format_preference_);
+  EXPECT_EQ(
+      capturer_->mappable_si_frame_pool_buffer_format_preference_for_testing(),
+      buffer_format_preference_);
   StopCapture();
 }
 
