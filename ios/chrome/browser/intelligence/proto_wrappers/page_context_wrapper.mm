07385d9a078ac2055cf6928d6230e08a5d7e0f20
Vincent Boisselle <vincb@google.com>
2026-01-21 02:56:19 -0800

    [ios][apc] Use href in the right format for svg anchors
    Currently, an error is returned when extracting content on a page that
    contains a svg anchor node because the .href object isn't a string in
    that case which cannot be parsed for native messaging.
    
    On error, all the extracted content for the page is skipped which is
    bad.
    
    Fixed: 475208453
    Change-Id: Iec7a9c418e67a8d904298e0fd75fff3727b59ab8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7499392
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1572189}

--- ios/chrome/browser/intelligence/proto_wrappers/page_context_wrapper.mm
+++ ios/chrome/browser/intelligence/proto_wrappers/page_context_wrapper.mm
@@ -174,7 +174,7 @@ const linksArray = [];
 const anchorElements = node.querySelectorAll('a[href]');
 anchorElements.forEach((anchor) => {
     linksArray.push({
-        href: anchor.href,
+        href: (anchor instanceof SVGAElement) ? anchor.href.baseVal : anchor.href,
         linkText: anchor.textContent
     });
 });
--- ios/chrome/browser/intelligence/proto_wrappers/resources/page_context_extractor.ts
+++ ios/chrome/browser/intelligence/proto_wrappers/resources/page_context_extractor.ts
@@ -92,7 +92,8 @@ const constructSameOriginInnerTextTree =
             node.querySelectorAll<HTMLAnchorElement>('a[href]');
         anchorElements.forEach((anchor) => {
           linksArray.push({
-            href: anchor.href,
+            href: (anchor instanceof SVGAElement) ? anchor.href.baseVal :
+                                                    anchor.href,
             linkText: anchor.textContent,
           });
         });
