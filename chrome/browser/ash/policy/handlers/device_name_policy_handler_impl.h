9ac64694c4ab4447440a0d1ef7ba9354218783cb
Jun Ishiguro <junis@google.com>
2026-01-20 17:49:54 -0800

    device_name: Remove DeviceNamePolicyHandler
    This CL removes DeviceNamePolicyHandler and instead uses
    DeviceNamePolicyHandlerImpl.
    
    The next CL will rename DeviceNamePolicyHandlerImpl to
    DeviceNamePolicyHandler. It's intentionally a separate CL to keep git
    history clean.
    
    Bug: b:466903206
    Change-Id: I58694b25794dc9871a248cbb8523e6c186ec52e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7480428
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1572005}

--- chrome/browser/ash/policy/handlers/device_name_policy_handler_impl.h
+++ chrome/browser/ash/policy/handlers/device_name_policy_handler_impl.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ref.h"
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
-#include "chrome/browser/ash/policy/handlers/device_name_policy_handler.h"
 #include "chromeos/ash/components/network/network_state_handler_observer.h"
 #include "chromeos/ash/components/settings/cros_settings.h"
 #include "chromeos/ash/components/system/statistics_provider.h"
@@ -29,8 +28,7 @@ class BrowserPolicyConnectorAsh;
 // This class observes the device setting |DeviceHostname|, and calls
 // NetworkStateHandler::SetHostname() appropriately based on the value of that
 // setting.
-class DeviceNamePolicyHandlerImpl : public DeviceNamePolicyHandler,
-                                    public ash::NetworkStateHandlerObserver {
+class DeviceNamePolicyHandlerImpl : public ash::NetworkStateHandlerObserver {
  public:
   // Types of policies for device name functionality.
   enum class DeviceNamePolicy {
@@ -57,8 +55,7 @@ class DeviceNamePolicyHandlerImpl : public DeviceNamePolicyHandler,
 
   ~DeviceNamePolicyHandlerImpl() override;
 
-  // DeviceNamePolicyHandler:
-  std::optional<std::string> GetHostnameChosenByAdministrator() const override;
+  std::optional<std::string> GetHostnameChosenByAdministrator() const;
 
   // For unit tests:
   DeviceNamePolicy GetDeviceNamePolicyForTesting() const {
--- chrome/browser/device_api/device_attribute_api.cc
+++ chrome/browser/device_api/device_attribute_api.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 
 #include "chrome/browser/ash/policy/core/browser_policy_connector_ash.h"
-#include "chrome/browser/ash/policy/handlers/device_name_policy_handler.h"
+#include "chrome/browser/ash/policy/handlers/device_name_policy_handler_impl.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/browser_process_platform_part.h"
 #include "chromeos/ash/components/system/statistics_provider.h"
