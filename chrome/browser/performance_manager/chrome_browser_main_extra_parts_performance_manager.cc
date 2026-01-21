e455557d14de5c6100227cd8f8373068a4e9d452
Andres Ricardo Perez <andresrperez@google.com>
2026-01-21 08:03:24 -0800

    Implement PriorityBoostBrowserNetworkPolicy
    This policy disables boosting for all processes on startup, except for
    the browser and network processes.
    
    The previous iteration of this experiment showed interesting gains, but
    also a regression in BrowserResponsiveness. That led to the selection of
    these processes to remain boosted.
    
    Bug: 395914024, 465728967
    Change-Id: I7863f4465cd222226b0498cadb22cc7260e53e77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7228413
    Reviewed-by: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Andres Ricardo Perez <andresrperez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1572334}

--- chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.cc
+++ chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.cc
@@ -97,6 +97,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_WIN)
 #include "base/path_service.h"
+#include "chrome/browser/performance_manager/policies/priority_boost_browser_network_policy.h"
 #endif
 
 namespace {
@@ -203,6 +204,13 @@ void ChromeBrowserMainExtraPartsPerformanceManager::CreatePoliciesAndDecorators(
           switches::kSingleProcess)) {
     graph->PassToGraph(
         std::make_unique<performance_manager::TerminationTargetPolicy>());
+  }
+  if (base::FeatureList::IsEnabled(features::kDisableBoostPriority) &&
+      features::kDisableBoostPriorityExemption.Get() ==
+          features::DisableBoostPriorityExemption::kBrowserNetwork) {
+    graph->PassToGraph(
+        std::make_unique<performance_manager::policies::
+                             PriorityBoostBrowserNetworkPolicy>());
   }
 #endif  // BUILDFLAG(IS_WIN)
 
---
+++ chrome/browser/performance_manager/policies/priority_boost_helpers.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_PERFORMANCE_MANAGER_POLICIES_PRIORITY_BOOST_HELPERS_H_
+#define CHROME_BROWSER_PERFORMANCE_MANAGER_POLICIES_PRIORITY_BOOST_HELPERS_H_
+
+namespace performance_manager {
+
+class ProcessNode;
+
+namespace policies {
+
+// If process_node represents a valid process, it sets ProcessPriorityBoost to
+// disable_boost.
+void SetDisableBoostIfValid(const ProcessNode* process_node,
+                            bool disable_boost);
+
+// Calls ::GetProcessPriorityBoost(). The underlying process must be valid.
+bool IsProcessPriorityBoostEnabled(const ProcessNode* process_node);
+
+}  // namespace policies
+
+}  // namespace performance_manager
+
+#endif  // CHROME_BROWSER_PERFORMANCE_MANAGER_POLICIES_PRIORITY_BOOST_HELPERS_H_
