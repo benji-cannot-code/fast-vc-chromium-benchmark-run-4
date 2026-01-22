992459d5528609ccaf3e00c86f57df348bb61930
Georg Neis <neis@chromium.org>
2026-01-21 18:34:16 -0800

    ash: Remove KeystoreService crosapi interface
    Make the minimal changes needed to delete the KeystoreService mojo
    interface. The is the first step of moving its implementation out of
    chrome/browser/ash/crosapi and getting rid of keystore_service.mojom.
    
    Bug: b:365741912, b:365902693
    Change-Id: Ie7db668cb0040785249be40bac2a202c9360c429
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7500863
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1572715}

--- chrome/browser/ash/crosapi/crosapi_ash.h
+++ chrome/browser/ash/crosapi/crosapi_ash.h
@@ -77,8 +77,6 @@ class CrosapiAsh : public mojom::Crosapi {
   void BindInSessionAuth(
       mojo::PendingReceiver<chromeos::auth::mojom::InSessionAuth> receiver)
       override;
-  void BindKeystoreService(
-      mojo::PendingReceiver<mojom::KeystoreService> receiver) override;
   void BindLocalPrinter(
       mojo::PendingReceiver<mojom::LocalPrinter> receiver) override;
   void BindMachineLearningService(
