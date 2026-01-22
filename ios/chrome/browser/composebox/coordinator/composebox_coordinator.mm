9cafcb54d9e0fc8b6c5be0fe25f39b8294501a6a
Chris Lu <thegreenfrog@google.com>
2026-01-22 08:20:39 -0800

    [ios] Fix compose ipad presentation bugs
    - In incognito, the composebox was squished because there needed to be
    a call to set the preferredContentSize of the input plate after it has
    laid out. When there are omnibox suggestions, this is not a problem
    since the Composebox preferredContentSize is updated then.
    - For iOS18 ipad layouts, we still want to use the popover presentation
    for regular horizontal size classes.
    
    Video: https://screencast.googleplex.com/cast/NTc5OTA3MzcyNTU0NjQ5NnwwYzM1OWM0Ni02Zg
    
    Bug: 469368394, 477595058
    Change-Id: Ide95d21638bf25bee13bf1c5a973689df0c539cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7510926
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1573037}

--- ios/chrome/browser/composebox/coordinator/composebox_coordinator.mm
+++ ios/chrome/browser/composebox/coordinator/composebox_coordinator.mm
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
 #import "ios/chrome/browser/url_loading/model/url_loading_browser_agent.h"
 #import "ios/chrome/browser/url_loading/model/url_loading_util.h"
+#import "ios/chrome/common/ui/util/ui_util.h"
 #import "ios/web/public/web_state.h"
 
 @interface ComposeboxCoordinator () <ComposeboxViewControllerDelegate,
@@ -318,7 +319,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (BOOL)shouldUseIpadPresentationController {
   return IsComposeboxIpadEnabled() &&
          UIDevice.currentDevice.userInterfaceIdiom == UIUserInterfaceIdiomPad &&
-         base::ios::IsRunningOnIOS26OrLater();
+         (base::ios::IsRunningOnIOS26OrLater() ||
+          IsRegularXRegularSizeClass(self.baseViewController.traitCollection));
 }
 
 #pragma mark - Clipboard checks
--- ios/chrome/browser/composebox/ui/composebox_input_plate_view_controller.mm
+++ ios/chrome/browser/composebox/ui/composebox_input_plate_view_controller.mm
@@ -321,6 +321,7 @@ UIImage* SendButtonImage(BOOL highlighted, ComposeboxTheme* theme) {
         _inputPlateContainerView.layer.cornerRadius;
   }
   [self updateCarouselFade];
+  [self updatePreferredContentSize];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -1582,6 +1583,7 @@ UIImage* SendButtonImage(BOOL highlighted, ComposeboxTheme* theme) {
 - (void)updateInputPlateStackViewAnimated:(BOOL)animated {
   if (!animated) {
     [self updateInputPlateStackViewContent];
+    [self updatePreferredContentSize];
     return;
   }
 
