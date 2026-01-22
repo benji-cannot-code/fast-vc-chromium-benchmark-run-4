7fef17c62ef9a74b13c466f1758059dead5175c3
Simge Şengün <simges@google.com>
2026-01-22 03:58:02 -0800

    Add utility functions for `ValuableMetadata` sync
    Add `CreateEntityDataFromValuableMetadata` and
    `CreateSpecificsFromValuableMetadata` to support future ValuableMetadata
    server sync
    
    Bug: 477535668
    Change-Id: I8db3ba0aee10d61344489a4459ab8307d553706a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7493700
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Simge Şengün <simges@google.com>
    Cr-Commit-Position: refs/heads/main@{#1572885}

--- components/autofill/core/browser/webdata/valuables/valuables_sync_test_utils.h
+++ components/autofill/core/browser/webdata/valuables/valuables_sync_test_utils.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 
 #include "components/autofill/core/browser/data_model/valuables/loyalty_card.h"
+#include "components/autofill/core/browser/data_model/valuables/valuable_types.h"
 #include "components/sync/protocol/autofill_valuable_specifics.pb.h"
 
 namespace autofill {
@@ -21,6 +22,9 @@ sync_pb::AutofillValuableSpecifics TestLoyaltyCardSpecifics(
     std::string_view id = "1",
     std::string_view program_logo = "http://foobar.com/logo.png",
     std::string_view number = "80974934820245");
+
+// Creates a test `ValuableMetadata`.
+ValuableMetadata TestValuableMetadata(std::string_view id = "1");
 
 }  // namespace autofill
 
