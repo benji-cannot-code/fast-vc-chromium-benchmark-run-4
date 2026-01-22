81f237de2dd96378092021445f60180a9d0fa6ca
Sam Maier <smaier@chromium.org>
2026-01-22 09:50:42 -0800

    Reland "Mark Tablet and Desktop as important for PublicTransitLeakTest"
    This is a reland of commit 6385a89ebb96dcb1d2bdf1e86c1699ee1cef9c26
    
    Original change's description:
    > Mark Tablet and Desktop as important for PublicTransitLeakTest
    >
    > Mark Tablet and Desktop as important for PublicTransLeakTest
    >
    > Change-Id: I7d9e60ad85ef3f7785e3e7313baf17250b83420f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7416739
    > Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    > Commit-Queue: Sam Maier <smaier@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1567115}
    
    Change-Id: Id2cace56c72094f650377e846732d082f8361757
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7506192
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1573118}

--- chrome/android/javatests/src/org/chromium/chrome/test/memory_leaks/PublicTransitLeakTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/test/memory_leaks/PublicTransitLeakTest.java
@@ -15,6 +15,8 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisableIf;
+import org.chromium.base.test.util.ImportantFormFactors;
 import org.chromium.base.test.util.LeakCanaryChecker.EnableLeakChecks;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.customtabs.CustomTabsIntentTestUtils;
@@ -32,6 +34,7 @@ import java.util.concurrent.TimeoutException;
 /** Tests the behavior of {@link ChromeFeatureList} in instrumentation tests. */
 @RunWith(ChromeJUnit4ClassRunner.class)
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
+@ImportantFormFactors(DeviceFormFactor.TABLET_OR_DESKTOP)
 @Batch(Batch.PER_CLASS)
 @EnableLeakChecks
 public class PublicTransitLeakTest {
@@ -75,6 +78,8 @@ public class PublicTransitLeakTest {
 
     @Test
     @LargeTest
+    // For some reason, this test is flaky on desktop.
+    @DisableIf.Device(DeviceFormFactor.DESKTOP)
     public void searchActivityTest() {
         var page = mChromeTabbedActivityTestRule.startOnBlankPage();
         var activity = mChromeTabbedActivityTestRule.getActivity();
