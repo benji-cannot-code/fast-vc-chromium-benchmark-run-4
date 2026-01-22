868820781970605672bf6e36d933b5f295d6ba26
Joshua Pawlicki <waffles@chromium.org>
2026-01-22 15:12:35 -0800

    activity_reporter: Report channel and user/system.
    Bug: 454662418
    Change-Id: I68c18a6231499fa88ec0a6263f08bacbd43b9166
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7486653
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1573341}

--- ios/chrome/browser/application_context/model/application_context_impl.mm
+++ ios/chrome/browser/application_context/model/application_context_impl.mm
@@ -455,7 +455,7 @@ ApplicationContextImpl::GetActivityReporter() {
             GetSharedURLLoaderFactory(),
             // Never send cookies for activity reports.
             base::BindRepeating([](const GURL& url) { return false; })),
-        base::DoNothing());
+        base::BindRepeating(&GetChannel), base::DoNothing(), true);
   }
   return activity_reporter_.get();
 }
--- ios/chrome/test/testing_application_context.mm
+++ ios/chrome/test/testing_application_context.mm
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/promos_manager/model/mock_promos_manager.h"
 #import "ios/chrome/browser/signin/model/account_profile_mapper.h"
 #import "ios/chrome/browser/signin/model/avatar_provider.h"
+#import "ios/chrome/common/channel_info.h"
 #import "ios/components/security_interstitials/safe_browsing/fake_safe_browsing_service.h"
 #import "ios/public/provider/chrome/browser/additional_features/additional_features_api.h"
 #import "ios/public/provider/chrome/browser/push_notification/push_notification_api.h"
@@ -283,7 +284,7 @@ TestingApplicationContext::GetActivityReporter() {
             GetSharedURLLoaderFactory(),
             // Never send cookies for activity reports.
             base::BindRepeating([](const GURL& url) { return false; })),
-        base::DoNothing());
+        base::BindRepeating(&GetChannel), base::DoNothing(), true);
   }
   return activity_reporter_.get();
 }
