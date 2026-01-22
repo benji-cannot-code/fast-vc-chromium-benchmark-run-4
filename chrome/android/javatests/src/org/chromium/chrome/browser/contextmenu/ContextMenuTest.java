ac343481a61fc31b5ca00952a70481cf2ea6831f
Duong Dac <ddac@chromium.org>
2026-01-22 02:10:00 -0800

    Revert "[context menu] long press to trigger context menu with desktop UA only"
    This reverts commit cdb92a14217fb4d619e8ea0bdbf8364a29d03420.
    
    Reason for revert: builder failures (test-tablet, see bug entry).
    
    Original change's description:
    > [context menu] long press to trigger context menu with desktop UA only
    >
    > Empty space context menu may cause web compatibility issues on LFF
    > devices. Known example is youtube player speed up gesture.
    >
    > Fixed: 451888879
    > Test: empty space context menu show on long press iff on desktop UA
    > Test: ContextMenuTest.java
    > Change-Id: I72f1035005dbdba5b50809a58ebcc0ad17f24315
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7048018
    > Auto-Submit: Grace Cham <hscham@chromium.org>
    > Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    > Commit-Queue: Grace Cham <hscham@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1572050}
    
    Bug: 477596254, 451888879
    Change-Id: Ie96a7330ea71b9b53427f4fcd9795ec5cd214892
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7504716
    Owners-Override: Duong Dac <ddac@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1572821}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ContextMenuTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ContextMenuTest.java
@@ -81,7 +81,6 @@ import org.chromium.chrome.browser.share.ShareDelegateSupplier;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabContextMenuItemDelegate;
 import org.chromium.chrome.browser.tab.TabCreationState;
-import org.chromium.chrome.browser.tab.TabUtils;
 import org.chromium.chrome.browser.tabmodel.SupportedProfileType;
 import org.chromium.chrome.browser.tabmodel.TabModel;
 import org.chromium.chrome.browser.tabmodel.TabModelSelectorObserver;
@@ -739,7 +738,6 @@ public class ContextMenuTest {
     public void testSavePageLongPress() throws TimeoutException {
         DeviceInput.setSupportsPrecisionPointerForTesting(true);
         Tab tab = mActivityTestRule.getActivityTab();
-        switchToDesktopUserAgent(tab);
         int callCount = mDownloadTestRule.getChromeDownloadCallCount();
         ContextMenuUtils.selectContextMenuItem(
                 InstrumentationRegistry.getInstrumentation(),
@@ -1311,7 +1309,6 @@ public class ContextMenuTest {
     public void testSharePageLongPress() throws Exception {
         DeviceInput.setSupportsPrecisionPointerForTesting(true);
         Tab tab = mActivityTestRule.getActivityTab();
-        switchToDesktopUserAgent(tab);
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
                     // Set share delegate before triggering context menu, so the mocked share
@@ -1406,7 +1403,6 @@ public class ContextMenuTest {
     public void testPrintPageLongPress() throws Exception {
         DeviceInput.setSupportsPrecisionPointerForTesting(true);
         Tab tab = mActivityTestRule.getActivityTab();
-        switchToDesktopUserAgent(tab);
         ThreadUtils.runOnUiThreadBlocking(
                 // Set printing controller to use the mock instance.
                 () -> {
@@ -1452,13 +1448,6 @@ public class ContextMenuTest {
         ArgumentCaptor<Printable> printableCaptor = ArgumentCaptor.forClass(Printable.class);
         verify(mPrintingController).startPrint(printableCaptor.capture(), any());
         Assert.assertEquals(tab.getTitle(), printableCaptor.getValue().getTitle());
-    }
-
-    private void switchToDesktopUserAgent(Tab tab) {
-        ThreadUtils.runOnUiThreadBlocking(
-                () -> TabUtils.switchUserAgent(tab, /* switchToDesktop= */ true));
-        CriteriaHelper.pollUiThread(() -> tab.isUserInteractable() && !tab.isLoading());
-        mActivityTestRule.assertWaitForPageScaleFactorMatch(PAGE_SCALE_FACTOR);
     }
 
     // TODO(benwgold): Add more test coverage for histogram recording of other context menu types.
