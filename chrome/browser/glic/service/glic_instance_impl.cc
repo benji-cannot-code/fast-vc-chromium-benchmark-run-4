98c9a239b66908cc623edae9a8acedbeba2bd8f4
Nicola Tommasi <tommasin@chromium.org>
2026-01-22 03:36:03 -0800

    [glic][onboarding] Rely on the enablement logic, instead of the flag
    The TrustFirst Onboarding flow is only allowed if MultiInstance is
    enabled, this will help decoupling finch configs.
    
    Bug: b:476316184
    Change-Id: I4c7537dd0d565911ee3c002d36d6de1ab2c06880
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7509404
    Commit-Queue: Nicola Tommasi <tommasin@chromium.org>
    Auto-Submit: Nicola Tommasi <tommasin@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1572864}

--- chrome/browser/glic/service/glic_instance_impl.cc
+++ chrome/browser/glic/service/glic_instance_impl.cc
@@ -108,7 +108,7 @@ EmbedderKey CreateSidePanelEmbedderKey(tabs::TabInterface* tab) {
 }
 
 bool IsTrustFirstOnboardingPending(Profile* profile) {
-  return base::FeatureList::IsEnabled(features::kGlicTrustFirstOnboarding) &&
+  return GlicEnabling::IsTrustFirstOnboardingEnabled() &&
          !GlicEnabling::HasConsentedForProfile(profile);
 }
 }  // namespace
