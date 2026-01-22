47ac5cebbdad3a132c9ed70eddf82c564c89ce41
Kunal Daftari <kunaldaftari@google.com>
2026-01-22 11:54:01 -0800

    [Vertical Tabs] Fixing Sizing of Bottom Container Buttons
    This CL fixes the size of the bottom container buttons to match the
    mocks. Before, the size of the images in the buttons was 16; it is
    supposed to be 20. We edit browser_actions.cc to fix this.
    
    One thing to note, though, is that the add (new tab) icon would show a
    blurry image, so I added a custom size 20 image for it in the .icon
    file, which appeared to fix the issue.
    
    The height of the buttons is also now 32.
    
    Image: https://screenshot.googleplex.com/8vdeWf5om9wkSED.png
    Bug: 477620231
    Change-Id: Ifb8387cbd1e33f6f34da1c4add01544e0dbc07a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7505018
    Auto-Submit: Kunal Daftari <kunaldaftari@google.com>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1573218}

--- chrome/browser/ui/tabs/vertical_tab_strip_state_controller.cc
+++ chrome/browser/ui/tabs/vertical_tab_strip_state_controller.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/rtl.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/to_string.h"
+#include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sessions/session_service.h"
 #include "chrome/browser/sessions/session_service_factory.h"
@@ -60,6 +61,7 @@ VerticalTabStripStateController::VerticalTabStripStateController(
   }
 
   UpdateCollapseActionItem();
+  UpdateBottomContainerActionItems();
 
   if (session_service_) {
     session_service_->AddObserver(this);
@@ -149,6 +151,7 @@ void VerticalTabStripStateController::NotifyCollapseChanged() {
 }
 
 void VerticalTabStripStateController::NotifyModeChanged() {
+  UpdateBottomContainerActionItems();
   on_mode_changed_callback_list_.Notify(this);
 }
 
@@ -180,6 +183,29 @@ void VerticalTabStripStateController::UpdateCollapseActionItem() {
         l10n_util::GetStringUTF16(text)));
     collapse_action->SetTooltipText(BrowserActions::GetCleanTitleAndTooltipText(
         l10n_util::GetStringUTF16(text)));
+  }
+}
+
+void VerticalTabStripStateController::UpdateBottomContainerActionItems() {
+  actions::ActionItem* tab_group_action_item =
+      actions::ActionManager::Get().FindAction(kActionTabGroupsMenu,
+                                               root_action_item_);
+  actions::ActionItem* new_tab_action_item =
+      actions::ActionManager::Get().FindAction(kActionNewTab,
+                                               root_action_item_);
+
+  // If Vertical Tabs is enabled, then the size of the images would be 20;
+  // otherwise, we use the default argument of 0 to avoid any issues with the
+  // horizontal tab strip.
+  const int icon_size = ShouldDisplayVerticalTabs() ? 20 : 0;
+
+  if (tab_group_action_item) {
+    tab_group_action_item->SetImage(ui::ImageModel::FromVectorIcon(
+        kSavedTabGroupBarEverythingIcon, ui::kColorIcon, icon_size));
+  }
+  if (new_tab_action_item) {
+    new_tab_action_item->SetImage(
+        ui::ImageModel::FromVectorIcon(kAddIcon, ui::kColorIcon, icon_size));
   }
 }
 
