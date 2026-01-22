92e587ab7c997f51ed6e627125505010b983b558
Bryant Chandler <bryantchandler@google.com>
2026-01-22 14:57:35 -0800

    [glic] Refactor PinnedTabObserver visibility and clean up GlicTabData
    Switch PinnedTabObserver to use TabInterface::RegisterDidBecomeVisible
    and RegisterWillBecomeHidden. This allows us to safely use
    GetBrowserWindowInterface in GlicTabData, replacing platform-specific
    workarounds.
    
    Bug: 456445100
    Change-Id: I02660ee448804150fdce34b4186aea2f6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7489538
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1573328}

--- chrome/browser/glic/host/context/glic_tab_data.cc
+++ chrome/browser/glic/host/context/glic_tab_data.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
+#include "chrome/browser/glic/common/future_browser_features.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/common/chrome_features.h"
 #include "components/favicon/content/content_favicon_driver.h"
@@ -281,16 +282,9 @@ glic::mojom::TabDataPtr CreateTabData(tabs::TabInterface* tab) {
 
   if (base::FeatureList::IsEnabled(features::kGlicGetTabByIdApi)) {
     is_active_in_window = tab && tab->IsActivated();
-    // This code may be reached during the dragging of the tab out into a new
-    // window. In that case the BrowserWindowInterface would be null, but we
-    // cannot call GetBrowserWindowInterface to check for null. So we resort to
-    // null checking the underlying tab strip.
-    // TODO(crbug.com/456445100): Determine a better way to safely call this.
-#if !BUILDFLAG(IS_ANDROID)  // NEEDS_ANDROID_IMPL
-    is_window_active = tab &&
-                       static_cast<tabs::TabModel*>(tab)->owning_model() &&
-                       tab->GetBrowserWindowInterface()->IsActive();
-#endif
+    is_window_active = tab->GetBrowserWindowInterface()
+                           ? IsActive(tab->GetBrowserWindowInterface())
+                           : false;
   }
   return glic::mojom::TabData::New(
       GetTabId(web_contents),
