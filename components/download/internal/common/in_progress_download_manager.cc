04899c42c3b84551aa4109265b1b330ebdd39059
François Doray <fdoray@chromium.org>
2026-01-22 11:29:13 -0800

    [downloads] Avoid unnecessary copies in downloads code.
    Drive-by performance tweak.
    
    Bug: 476408124
    Change-Id: I45c1e174c02ba77da788100b473f6ebaef1d1af7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7487072
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1573199}

--- components/download/internal/common/in_progress_download_manager.cc
+++ components/download/internal/common/in_progress_download_manager.cc
@@ -514,9 +514,6 @@ void InProgressDownloadManager::StartDownload(
   DVLOG(20) << __func__
             << "() result=" << DownloadInterruptReasonToString(info->result);
 
-  GURL url = info->url();
-  std::vector<GURL> url_chain = info->url_chain;
-  std::string mime_type = info->mime_type;
 
   // If the download cannot be found locally, ask |delegate_| to provide the
   // DownloadItem.
