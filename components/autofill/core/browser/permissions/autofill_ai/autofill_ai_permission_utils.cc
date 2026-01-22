2772b1b36fb51551d7ef53d6aa1e9793214cab7a
Norge Vizcay <vizcay@google.com>
2026-01-22 04:24:51 -0800

    Small readability improvements to ai_permissions_utils.
    Bug: 434122759
    Change-Id: Id8c0a313cfe573ddfda7e4665027ebf5d2e11ad5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7510484
    Reviewed-by: Vidhan Jain <vidhanj@google.com>
    Auto-Submit: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1572897}

--- components/autofill/core/browser/permissions/autofill_ai/autofill_ai_permission_utils.cc
+++ components/autofill/core/browser/permissions/autofill_ai/autofill_ai_permission_utils.cc
@@ -94,7 +94,7 @@ void MaybeOutputReason(std::string* out, std::string_view message) {
     return country_code == GeoIpCountryCode("US");
   }
 
-  // Parses `parameter` can returns whether any of the country codes is contains
+  // Parses `parameter` and returns whether any of the country codes is contains
   // match `country_code`.
   auto contains_geo_ip = [&country_code](std::string_view parameter) {
     return std::ranges::contains(
@@ -108,9 +108,10 @@ void MaybeOutputReason(std::string* out, std::string_view message) {
       features::kAutofillAiIgnoreGeoIpAllowlist.Get();
   const std::string& blocklist =
       features::kAutofillAiIgnoreGeoIpBlocklist.Get();
-  return (blocklist.empty() && allowlist.empty()) ||
-         (blocklist.empty() && contains_geo_ip(allowlist)) ||
-         (!blocklist.empty() && !contains_geo_ip(blocklist));
+  if (!blocklist.empty()) {
+    return !contains_geo_ip(blocklist);
+  }
+  return allowlist.empty() || contains_geo_ip(allowlist);
 }
 
 // Returns the `GaiaIdHash` for the signed in account if there is one or
@@ -358,7 +359,7 @@ void MaybeOutputReason(std::string* out, std::string_view message) {
         MaybeOutputReason(debug_message, "Enterprise policy is not enabled.");
       }
       return policy_pref_enabled;
-    case autofill::AutofillAiAction::kListEntityInstancesInSettings:
+    case AutofillAiAction::kListEntityInstancesInSettings:
       return true;
   }
   NOTREACHED();
