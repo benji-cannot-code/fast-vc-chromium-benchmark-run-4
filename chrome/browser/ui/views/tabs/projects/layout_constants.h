d306cd04fc2c4203dccdc1098fa874ec5d366cb8
Chase Hartsell <chazzy@google.com>
2026-01-22 12:43:38 -0800

    [projects] Add hover state and focus ring to thread list item
    Also fixes some styling with the list items, avoiding the need for
    SetUseContentsPreferredSize(true) on the ScrollView.
    
    Screencast: http://go/scrcast/NTI5MTQwNTcyMjI1NTM2MHw3ZDk0MjdjNi1hMA
    
    Bug: 476483922
    Change-Id: Ia7229f2faa1bdd46f3db5d1132eae5e092a997f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7500421
    Reviewed-by: David Maunder <davidjm@chromium.org>
    Commit-Queue: Chase Hartsell <chazzy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1573248}

--- chrome/browser/ui/views/tabs/projects/layout_constants.h
+++ chrome/browser/ui/views/tabs/projects/layout_constants.h
@@ -7,20 +7,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_VIEWS_TABS_PROJECTS_LAYOUT_CONSTANTS_H_
 
 #include "ui/gfx/geometry/insets.h"
+#include "ui/views/controls/focus_ring.h"
 
 namespace projects_panel {
+
+// The focus ring halo inset for a list item. This contains the focus ring
+// within the bounds of the item so the halo isn't clipped by the container.
+inline constexpr float kListItemFocusRingHaloInset =
+    views::FocusRing::kDefaultHaloThickness / 2;
 
 // The size of icons within a list item, in pixels.
 inline constexpr int kListItemIconSize = 16;
 
 // The padding around a list item.
-inline constexpr gfx::Insets kListItemPadding = gfx::Insets::VH(6, 10);
+inline constexpr gfx::Insets kListItemPadding = gfx::Insets::VH(8, 10);
 
 // The preferred size of a list item.
-inline constexpr gfx::Size kListItemPreferredSize = gfx::Size(232, 24);
+inline constexpr gfx::Size kListItemPreferredSize = gfx::Size(0, 32);
 
 // The padding around the title within a list item.
-inline constexpr gfx::Insets kListItemTitlePadding = gfx::Insets::VH(0, 16);
+inline constexpr gfx::Insets kListItemTitlePadding =
+    gfx::Insets::TLBR(0, 16, 0, 0);
 
 // The padding around a list.
 inline constexpr gfx::Insets kListPadding = gfx::Insets::VH(0, 4);
--- chrome/browser/ui/views/tabs/projects/projects_panel_thread_item_view.h
+++ chrome/browser/ui/views/tabs/projects/projects_panel_thread_item_view.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_TABS_PROJECTS_PROJECTS_PANEL_THREAD_ITEM_VIEW_H_
 #define CHROME_BROWSER_UI_VIEWS_TABS_PROJECTS_PROJECTS_PANEL_THREAD_ITEM_VIEW_H_
 
+#include "ui/views/controls/button/button.h"
 #include "ui/views/view.h"
 
 namespace contextual_tasks {
@@ -13,8 +14,8 @@ struct Thread;
 }
 
 // Displays a button for a single thread (e.g., a Gemini or AIM thread).
-class ProjectsPanelThreadItemView : public views::View {
-  METADATA_HEADER(ProjectsPanelThreadItemView, views::View)
+class ProjectsPanelThreadItemView : public views::Button {
+  METADATA_HEADER(ProjectsPanelThreadItemView, views::Button)
 
  public:
   explicit ProjectsPanelThreadItemView(const contextual_tasks::Thread& task);
