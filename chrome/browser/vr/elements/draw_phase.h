ad352626b39b7a009b730c5880dcc3824ed1e539
Valeria Jankowskaja <vjankowskaja@google.com>
2026-01-22 03:55:35 -0800

    [TDR] Remove TODOs linked to issue 41361860
    This CL was generated by an AI agent and verified by the CL owner before
    sending for review. The TODOs were identified as stale by TODO Removal
    Agent (http://go/dead-todo-removal-agent).
    * Detailed reasoning can be found in the CL comments.
    * How to review this CL: http://go/tdr-review-instructions
    * To opt-out from future reviews: http://go/tdr-opt-out.
    
    The associated bug crbug.com/41361860 was closed as OBSOLETE. The bug's
    final comment explains that the draw phases, initially targeted for
    removal, were found to be useful and were intentionally kept, making the
    TODO's suggestion outdated.
    
    Change-Id: I8d4ffc43acdbd05ddec08bc81cc2ecf7062898a7
    Bug: 41361860
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7453507
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Commit-Queue: Valeria Jankowskaja <vjankowskaja@google.com>
    Cr-Commit-Position: refs/heads/main@{#1572883}

--- chrome/browser/vr/elements/draw_phase.h
+++ chrome/browser/vr/elements/draw_phase.h
@@ -11,8 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace vr {
 
 // Each draw phase is rendered independently in the order specified below.
-// TODO(crbug.com/41361860): We don't really need all these draw phases as
-// the draw order depends on an element's insert order.
 // LINT.IfChange(DrawPhaseType)
 enum DrawPhase : int {
   // kPhaseNone is to be used for elements that do not draw. Eg, layouts.
