3936adf789259bad351a46fe3d6ca085fa2c848f
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2026-01-22 04:48:44 -0800

    Import wpt@9a421a4a23d3b6224c69c138fe65af6dfb5d1644
    https://github.com/web-platform-tests/wpt/compare/e669c5435...9a421a4a2
    
    Using wpt-import in Chromium 9de974c952bef2c5898c9462f98c8536b8066c55.
    With Chromium commits locally applied on WPT:
      e9b356950d "[WebLocks] Update BFCache WPTs to verify eviction on [...]
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    hbos@chromium.org, hta@chromium.org, guidou@chromium.org, phancke@microsoft.com:
      external/wpt/webrtc
    kojii@chromium.org:
      external/wpt/css/css-font-loading
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac-skia-alt-arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac12.0-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac12.0.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac13-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac13.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac14-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac14.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac15-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac15.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:win10.20h2-blink-rel
    Cq-Include-Trybots: luci.chromium.try:win11-arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:win11-blink-rel
    Change-Id: I0debe982fd935f688a587afc05927c2f745c2147
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7509805
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1572910}

---
+++ third_party/blink/web_tests/external/wpt/mimesniff/sniffing/html-comment.tentative.window.js
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+promise_test(async t => {
+  const response = await fetch("support/html-comment?pipe=header(Content-Type,DELETE)");
+  const text = await response.text();
+  assert_true(text.startsWith("<!--"));
+
+  const popup = window.open("support/html-comment?pipe=header(Content-Type,DELETE)");
+  t.add_cleanup(() => popup.close());
+
+  await new Promise(resolve => {
+    popup.onload = resolve;
+  });
+
+  assert_equals(popup.document.contentType, "text/html");
+  assert_equals(popup.document.documentElement.localName, "html");
+  assert_equals(popup.document.documentElement.namespaceURI, "http://www.w3.org/1999/xhtml");
+}, "Content starting with <!-- without space or > is sniffed as HTML");
