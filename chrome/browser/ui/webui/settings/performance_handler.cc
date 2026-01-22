3dc64d3379f8fe6f16ff7be042dd4cf755bbbe89
Jeffrey Yu <yuje@chromium.org>
2026-01-21 16:02:11 -0800

    Replace `std::unordered_set` with `absl::flat_hash_set`.
    The Chromium containers guide
    (https://chromium.googlesource.com/chromium/src/+/HEAD/base/containers/README.md)
    recommends using `absl::flat_hash_set` as a default map choice instead
    of `std::unordered_set` when pointer stability isn't needed, and
    `absl::node_hash_set` when it is. Both will still generally perform
    better and require less overhead than `std::unordered_set`.
    
    Bug: 473916362
    Change-Id: Ib991e1f330fcb059b5e0e6cc1c56cb5bd8f344a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7495582
    Auto-Submit: Jeffrey Yu <yuje@chromium.org>
    Commit-Queue: Steven Luong <stluong@chromium.org>
    Reviewed-by: Charles Meng <charlesmeng@chromium.org>
    Reviewed-by: Steven Luong <stluong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1572624}

--- chrome/browser/ui/webui/settings/performance_handler.cc
+++ chrome/browser/ui/webui/settings/performance_handler.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/url_matcher/url_util.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
+#include "third_party/abseil-cpp/absl/container/flat_hash_set.h"
 #include "url/gurl.h"
 
 using content::WebContents;
@@ -88,10 +89,9 @@ base::Value PerformanceHandler::GetCurrentOpenSites() {
         return true;
       });
 
-  std::unordered_set<std::string> added_hosts;
+  absl::flat_hash_set<std::string> added_hosts;
   for (auto& [last_active_time, host] : last_active_time_host_pairs) {
-    if (!added_hosts.contains(host)) {
-      added_hosts.insert(host);
+    if (bool newly_inserted = added_hosts.insert(host).second; newly_inserted) {
       hosts.Append(host);
     }
   }
