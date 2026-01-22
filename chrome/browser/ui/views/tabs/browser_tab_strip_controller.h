f298feb6d3ec1631a40e8bd756a8050548898b9c
Darryl James <dljames@chromium.org>
2026-01-22 14:44:28 -0800

    Reland "[Vertical Tabs] Lazily Load TabStrips when Switching Modes"
    This reverts commit 57de341fa7b9d85001358f9fb1ae80532b2754eb.
    
    Reason for revert: Guard guard root_node before use in VerticalTabStripRegionView::InitializeTabStrip and VerticalTabStripRegionView::ResetTabStrip
    
    Original change's description:
    > Revert "[Vertical Tabs] Lazily Load TabStrips when Switching Modes"
    >
    > This reverts commit 757777b28fc5e6c16fa45a253af222363919bdae.
    >
    > Reason for revert: breaks multiple Linux and Mac bots 
    > First failure: https://ci.chromium.org/ui/p/chromium/builders/ci/linux-bfcache-rel/80596
    >
    > Original change's description:
    > > [Vertical Tabs] Lazily Load TabStrips when Switching Modes
    > >
    > > Change-Id: I2df5ca6b6d666a523834324b439cbb671bd69981
    > > Bug: 468100896
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7508566
    > > Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    > > Commit-Queue: Darryl James <dljames@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1572767}
    >
    > Bug: 468100896, 477782072
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 468100896
    > Change-Id: I6076211bd73fde7b00d40959bc47e0281b8f141d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7510563
    > Owners-Override: Viktor Semeniuk <vsemeniuk@google.com>
    > Auto-Submit: Viktor Semeniuk <vsemeniuk@google.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1572818}
    
    Bug: 468100896, 477782072
    Bug: 468100896
    Change-Id: Ifcef1ca9a70af8cbb44122aaedfae36a2e999ddf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7511474
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1573324}

--- chrome/browser/ui/views/tabs/browser_tab_strip_controller.h
+++ chrome/browser/ui/views/tabs/browser_tab_strip_controller.h
@@ -53,6 +53,7 @@ class BrowserTabStripController : public TabStripController,
   ~BrowserTabStripController() override;
 
   void InitFromModel(TabStrip* tabstrip);
+  void Reset();
 
   // TabStripController implementation:
   ui::ListSelectionModel GetSelectionModel() const override;
--- chrome/browser/ui/views/tabs/vertical/vertical_split_tab_view_browsertest.cc
+++ chrome/browser/ui/views/tabs/vertical/vertical_split_tab_view_browsertest.cc
@@ -25,6 +25,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class VerticalSplitTabViewTest
     : public VerticalTabsBrowserTestMixin<InProcessBrowserTest> {
  public:
+  RootTabCollectionNode* root_node() {
+    return browser()
+        ->GetBrowserView()
+        .vertical_tab_strip_region_view_for_testing()
+        ->root_node_for_testing();
+  }
+
   void CreateSplitTab() {
     // Add pinned split tabs.
     content::WebContents* contents1 = AppendTab();
@@ -55,14 +62,8 @@ class VerticalSplitTabViewTest
 
 IN_PROC_BROWSER_TEST_F(VerticalSplitTabViewTest, ProposedLayout_Unbounded) {
   CreateSplitTab();
-  // Create view hierarchy from an arbitrary parent view since we don't
-  // currently support updates from the API.
-  std::unique_ptr<views::View> parent_view = std::make_unique<views::View>();
-  RootTabCollectionNode root_node(
-      browser()->tab_strip_model(),
-      base::BindRepeating<TabCollectionNode::CustomAddChildView>(
-          &views::View::AddChildView, base::Unretained(parent_view.get())));
-  auto split = root_node.children()[1]->get_view_for_testing()->children()[1];
+  auto split =
+      root_node()->children()[1]->get_view_for_testing()->children()[1];
   EXPECT_TRUE(views::IsViewClass<VerticalSplitTabView>(split));
   VerticalSplitTabView* split_tab_view =
       static_cast<VerticalSplitTabView*>(split);
@@ -88,14 +89,8 @@ IN_PROC_BROWSER_TEST_F(VerticalSplitTabViewTest, ProposedLayout_Unbounded) {
 
 IN_PROC_BROWSER_TEST_F(VerticalSplitTabViewTest, ProposedLayout_LargeBounds) {
   CreateSplitTab();
-  // Create view hierarchy from an arbitrary parent view since we don't
-  // currently support updates from the API.
-  std::unique_ptr<views::View> parent_view = std::make_unique<views::View>();
-  RootTabCollectionNode root_node(
-      browser()->tab_strip_model(),
-      base::BindRepeating<TabCollectionNode::CustomAddChildView>(
-          &views::View::AddChildView, base::Unretained(parent_view.get())));
-  auto split = root_node.children()[1]->get_view_for_testing()->children()[1];
+  auto split =
+      root_node()->children()[1]->get_view_for_testing()->children()[1];
   EXPECT_TRUE(views::IsViewClass<VerticalSplitTabView>(split));
   VerticalSplitTabView* split_tab_view =
       static_cast<VerticalSplitTabView*>(split);
@@ -126,14 +121,8 @@ IN_PROC_BROWSER_TEST_F(VerticalSplitTabViewTest, ProposedLayout_LargeBounds) {
 
 IN_PROC_BROWSER_TEST_F(VerticalSplitTabViewTest, ProposedLayout_LimitedBounds) {
   CreateSplitTab();
-  // Create view hierarchy from an arbitrary parent view since we don't
-  // currently support updates from the API.
-  std::unique_ptr<views::View> parent_view = std::make_unique<views::View>();
-  RootTabCollectionNode root_node(
-      browser()->tab_strip_model(),
-      base::BindRepeating<TabCollectionNode::CustomAddChildView>(
-          &views::View::AddChildView, base::Unretained(parent_view.get())));
-  auto split = root_node.children()[1]->get_view_for_testing()->children()[1];
+  auto split =
+      root_node()->children()[1]->get_view_for_testing()->children()[1];
   EXPECT_TRUE(views::IsViewClass<VerticalSplitTabView>(split));
   VerticalSplitTabView* split_tab_view =
       static_cast<VerticalSplitTabView*>(split);
