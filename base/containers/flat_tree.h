4b3d545620fa55dd398ceddf2516d228d7bcbbfc
Tom Sepez <tsepez@google.com>
2026-01-22 11:42:43 -0800

    Add requires clause to base containers two-iterator constructors.
    Ensure that input iterators passed to such constructors/methods are
    actually iterators, otherwise an alternate form of constructor may
    be invoked for the underlying container.
    
    -- Fix one test where a vector containing 1234 copies of the value
       5678 was being created instead of one containing {1234, 5678}.
    
    Change-Id: I6dbc3b570414ac79c0570bb508b09aee78940c0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7511906
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1573211}

--- base/containers/flat_tree.h
+++ base/containers/flat_tree.h
@@ -165,6 +165,7 @@ class flat_tree {
   explicit flat_tree(const key_compare& comp);
 
   template <class InputIterator>
+    requires(std::input_iterator<InputIterator>)
   flat_tree(InputIterator first,
             InputIterator last,
             const key_compare& comp = key_compare());
@@ -178,6 +179,7 @@ class flat_tree {
             const key_compare& comp = key_compare());
 
   template <class InputIterator>
+    requires(std::input_iterator<InputIterator>)
   flat_tree(sorted_unique_t,
             InputIterator first,
             InputIterator last,
@@ -544,6 +546,7 @@ flat_tree<Key, GetKeyFromValue, KeyCompare, Container>::flat_tree(
 
 template <class Key, class GetKeyFromValue, class KeyCompare, class Container>
 template <class InputIterator>
+  requires(std::input_iterator<InputIterator>)
 flat_tree<Key, GetKeyFromValue, KeyCompare, Container>::flat_tree(
     InputIterator first,
     InputIterator last,
@@ -576,6 +579,7 @@ flat_tree<Key, GetKeyFromValue, KeyCompare, Container>::flat_tree(
 
 template <class Key, class GetKeyFromValue, class KeyCompare, class Container>
 template <class InputIterator>
+  requires(std::input_iterator<InputIterator>)
 flat_tree<Key, GetKeyFromValue, KeyCompare, Container>::flat_tree(
     sorted_unique_t,
     InputIterator first,
