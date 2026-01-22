9f60ca9f3f96fc0472325b226d4a7e8ca7994a6f
Noah Rose Ledesma <noahrose@google.com>
2026-01-22 10:26:02 -0800

    Updater UI: Add UpdaterState element
    Add an element to retrieve the current state of all Chrome Updater
    instances available on the machine. Display it in chrome://updater.
    
    Bug: 456498251
    Change-Id: If58468604c2cbbc57c01b3f3b862275b6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7499552
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Cr-Commit-Position: refs/heads/main@{#1573141}

--- chrome/browser/resources/updater/app.ts
+++ chrome/browser/resources/updater/app.ts
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import './event_list/event_list.js';
+import './updater_state/updater_state.js';
 
 import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
 
