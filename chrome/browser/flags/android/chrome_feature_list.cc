a5007a1c29278d73dbb1f13663c9d168d2ecabbc
Bhuvana Betini <bbetini@google.com>
2026-01-22 08:50:44 -0800

    [Glic] Make Glic flag accessible in Java
    Bug: 477298287
    Change-Id: I5502b0917dfdae37a3ad7759e26e8c0f93225237
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7499217
    Reviewed-by: Hailey Wang <haileywang@google.com>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Bhuvana Betini <bbetini@google.com>
    Cr-Commit-Position: refs/heads/main@{#1573059}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -178,6 +178,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &features::kEnableFullscreenToAnyScreenAndroid,
     &features::kFluidResize,
     &features::kGenericSensorExtraClasses,
+    &features::kGlic,
     &features::kHttpsFirstBalancedMode,
     &features::kLoadingPredictorLimitPreconnectSocketCount,
     &features::kNetworkServiceInProcess,
