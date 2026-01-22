db08a9750530141fac1b0ee85ddb8bdafdf5f610
Ryan Thomas <rathomas@google.com>
2026-01-22 12:19:06 -0800

    Tabs opened in "other window" are now brought to foreground.
    Bug: 441811193
    Verification: http://screencast/cast/NTk0OTA1MjIzODY5MjM1MnxkNGYyY2IyMC1hMg
    Change-Id: I4eb1ec8e640debff9f31ee161a76108de51519b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7487265
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Commit-Queue: Ryan Thomas <rathomas@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1573235}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/document/ChromeAsyncTabLauncher.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/document/ChromeAsyncTabLauncher.java
@@ -12,6 +12,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.provider.Browser;
 
+import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.IntentUtils;
 import org.chromium.base.metrics.RecordHistogram;
@@ -108,6 +109,8 @@ public class ChromeAsyncTabLauncher implements AsyncTabLauncher {
             if (otherActivity != null) {
                 assert otherActivity instanceof ChromeTabbedActivity;
                 ((ChromeTabbedActivity) otherActivity).onNewIntent(intent);
+                ApiCompatibilityUtils.moveTaskToFront(
+                        activity, ((ChromeTabbedActivity) otherActivity).getTaskId(), 0);
                 return;
             }
             if (preferNew) intent.putExtra(IntentHandler.EXTRA_PREFER_NEW, true);
