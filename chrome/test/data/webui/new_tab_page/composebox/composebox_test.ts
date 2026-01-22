1fb37c99f80cfaab2b1499d761dd3593dc2a43f9
Paul Adedeji <pauladedeji@google.com>
2026-01-22 10:02:07 -0800

    [composebox] Fix click submission for voice searches.
    Problem: `this.$.matches.selectFirst()` is called when you attempt to
    press the submit button at [1]. The current implementation assumes that
    there's an autocomplete match, however for inputs such as voice search
    this is not the case. When [2] looks for a match to submit, it fails,
    causing no search to happen.
    
    Solution: Modify the first match selection logic to only select a match
    if there's autocomplete matches.
    
    Downstream CL [3] adds interactive testing for voice and typed searches.
    
    [1]https://source.chromium.org/chromium/chromium/src/+/main:ui/webui/resources/cr_components/composebox/composebox.ts;drc=e8a4f92c5f5830b2081d8540da914b36304aff0a;l=1106
    [2]https://source.chromium.org/chromium/chromium/src/+/main:ui/webui/resources/cr_components/composebox/composebox.ts;drc=e8a4f92c5f5830b2081d8540da914b36304aff0a;l=1154
    [3]https://source.chromium.org/chromium/chromium/src/+/main:chrome/test/data/webui/new_tab_page/composebox/composebox_test.ts;drc=65f68d74e18d76bec3907fd2ef9caf19ce7847b2;l=2526
    
    Change-Id: I20b4f89016da5e8c5ff4844c1c18ab5e55bec55a
    Bug: b:477455887
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7504396
    Commit-Queue: Nihar Majmudar <niharm@google.com>
    Reviewed-by: Nihar Majmudar <niharm@google.com>
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Auto-Submit: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1573122}

--- chrome/test/data/webui/new_tab_page/composebox/composebox_test.ts
+++ chrome/test/data/webui/new_tab_page/composebox/composebox_test.ts
@@ -2524,7 +2524,9 @@ suite('NewTabPageComposeboxTest', () => {
         assertTrue((composeboxElement as any).submitEnabled_);
 
         // Simulate submit button click.
-        composeboxElement.$.submitOverlay.click();
+        composeboxElement.$.submitContainer.dispatchEvent(
+            new FocusEvent('focusin'));
+        composeboxElement.$.submitContainer.click();
         await searchboxHandler.whenCalled('submitQuery');
         await microtasksFinished();
 
