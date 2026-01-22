0f3442615faec53f2b7f7c31a86a074a4f1f9fe0
Pier-Alexandre Bouchard <pabouchard@google.com>
2026-01-22 11:58:05 -0800

    [iOS] Show background customization IPH when background customization is opened from Setup List
    Displays the Background Customization IPH when the background customization entry point is launched from the Setup List. Ensures users receive contextual guidance during initial setup and improves discoverability of background customization options.
    
    Bug: 477037668
    
    Change-Id: Idadfb2d1c7f542db052ee101ffd78f73229b60fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7509626
    Commit-Queue: Pier-Alexandre Bouchard <pabouchard@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1573222}

--- ios/chrome/browser/content_suggestions/coordinator/content_suggestions_coordinator.mm
+++ ios/chrome/browser/content_suggestions/coordinator/content_suggestions_coordinator.mm
@@ -1266,6 +1266,9 @@ using segmentation_platform::TipIdentifier;
       break;
     case SetUpListItemType::kBackgroundCustomization:
       [self.delegate openMainCustomizationMenu];
+      [HandlerForProtocol(self.browser->GetCommandDispatcher(), HelpCommands)
+          presentInProductHelpWithType:InProductHelpType::
+                                           kHomeBackgroundCustomization];
   }
 }
 
