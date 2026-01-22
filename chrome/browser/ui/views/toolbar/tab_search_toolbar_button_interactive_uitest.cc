b05ebf31852e711c370140968a7206197b25dd50
Alison Gale <agale@chromium.org>
2026-01-22 11:01:18 -0800

    Reland "Revert "Enable the tab search toolbar button on top of tree""
    This reverts commit cf1445a0b75053262d7161c2a56ee19dc59491c2.
    
    Reason for revert: Reland with fixes to explicitly enable features in
    certain tests.
    
    Passing tests:
    https://ci.chromium.org/ui/inv/u-agale-2026-01-22-15-33-00-dfa14a0fc6b4f308
    https://ci.chromium.org/ui/inv/u-agale-2026-01-22-16-35-00-d4237842e23ebd3c
    https://ci.chromium.org/ui/inv/u-agale-2026-01-22-16-39-00-5683ded652d1058c
    https://ci.chromium.org/ui/inv/u-agale-2026-01-22-14-39-00-eb242560103daec3
    
    Original change's description:
    > Revert "Revert "Enable the tab search toolbar button on top of tree""
    >
    > This reverts commit 780d1cf5c49a03e7e76e91345f79eeea506f83ba.
    >
    > Reason for revert: causing test failure https://ci.chromium.org/ui/p/chrome/builders/ci/linux64-rel-ready/61987/overview
    >
    > Original change's description:
    > > Revert "Enable the tab search toolbar button on top of tree"
    > >
    > > This reverts commit 2b2053332bf837d141e520f3ea43f9e16d744ce3.
    > >
    > > Reason for revert: Delaying the full launch
    > >
    > > Original change's description:
    > > > Enable the tab search toolbar button on top of tree
    > > >
    > > > Change-Id: I370e9128137d696ed8d4f6ecdfbd4b8225bc5b7d
    > > > Bug: 471062209, 471184045
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7301209
    > > > Reviewed-by: David Pennington <dpenning@chromium.org>
    > > > Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    > > > Commit-Queue: Alison Gale <agale@chromium.org>
    > > > Cr-Commit-Position: refs/heads/main@{#1562157}
    > >
    > > Bypass-Check-License: Restoring deleted file
    > > Bug: 471062209, 471184045, 476427521
    > > Change-Id: I29371b2f2d3b132a1b19cf1e67b3a4c18bd4c12c
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7496812
    > > Commit-Queue: Alison Gale <agale@chromium.org>
    > > Reviewed-by: David Pennington <dpenning@chromium.org>
    > > Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1572419}
    >
    > Bug: 471062209, 471184045, 476427521
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: Iad36d3797a709be2868b93c5b83c635c33c780ab
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7505970
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Auto-Submit: Xinghui Lu <xinghuilu@chromium.org>
    > Owners-Override: Xinghui Lu <xinghuilu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1572481}
    
    Bug: 471062209, 471184045, 476427521
    Change-Id: I57e7756438de20573d501dbcdd4dbe3f3df5e470
    Bypass-Check-License: Reverting files
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7504383
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1573182}

--- chrome/browser/ui/views/toolbar/tab_search_toolbar_button_interactive_uitest.cc
+++ chrome/browser/ui/views/toolbar/tab_search_toolbar_button_interactive_uitest.cc
@@ -27,7 +27,9 @@ namespace {
 
 class TabSearchToolbarButtonInteractiveUiTest : public InteractiveBrowserTest {
  public:
-  TabSearchToolbarButtonInteractiveUiTest() = default;
+  TabSearchToolbarButtonInteractiveUiTest() {
+    scoped_feature_list_.InitAndEnableFeature(features::kTabstripComboButton);
+  }
   ~TabSearchToolbarButtonInteractiveUiTest() override = default;
 
   auto SendTabSearchKeyPress(ui::ElementIdentifier target) {
@@ -39,6 +41,9 @@ class TabSearchToolbarButtonInteractiveUiTest : public InteractiveBrowserTest {
                         ui::EF_CONTROL_DOWN | ui::EF_SHIFT_DOWN);
 #endif
   }
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 // This test verifies the TabSearch functionality when pinned.
