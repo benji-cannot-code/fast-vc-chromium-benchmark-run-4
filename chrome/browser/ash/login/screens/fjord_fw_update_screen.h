345d05f34e7f61a45ce646e5d354de146e267677
courtneywong <courtneywong@google.com>
2026-01-22 10:43:28 -0800

    fjord-oobe: Implement SetFjordOobeState dbus method
    This method allows callers to transition the OOBE UI to the next state.
    For now only the READY_FOR_CALIBRATION transition is supported.
    
    Bug: b:470421245
    Change-Id: I74c628469e054c6e1a9b892545cfbf54be69fc71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7490374
    Reviewed-by: Osama Fathy <osamafathy@google.com>
    Commit-Queue: Courtney Wong <courtneywong@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1573162}

--- chrome/browser/ash/login/screens/fjord_fw_update_screen.h
+++ chrome/browser/ash/login/screens/fjord_fw_update_screen.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_LOGIN_SCREENS_FJORD_FW_UPDATE_SCREEN_H_
 #define CHROME_BROWSER_ASH_LOGIN_SCREENS_FJORD_FW_UPDATE_SCREEN_H_
 
+#include "base/functional/callback.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/ash/login/screens/base_screen.h"
 
@@ -18,10 +19,17 @@ class FjordFwUpdateScreenView;
 class FjordFwUpdateScreen : public BaseScreen {
  public:
   using TView = FjordFwUpdateScreenView;
-  explicit FjordFwUpdateScreen(base::WeakPtr<FjordFwUpdateScreenView> view);
+  FjordFwUpdateScreen(base::WeakPtr<FjordFwUpdateScreenView> view,
+                      base::RepeatingClosure exit_callback);
   FjordFwUpdateScreen(const FjordFwUpdateScreen&) = delete;
   FjordFwUpdateScreen& operator=(const FjordFwUpdateScreen&) = delete;
   ~FjordFwUpdateScreen() override;
+
+  bool ExitScreen();
+
+  void set_exit_callback_for_testing(base::RepeatingClosure exit_callback) {
+    exit_callback_ = exit_callback;
+  }
 
  private:
   // BaseScreen
@@ -29,6 +37,7 @@ class FjordFwUpdateScreen : public BaseScreen {
   void HideImpl() override {}
 
   base::WeakPtr<FjordFwUpdateScreenView> view_;
+  base::RepeatingClosure exit_callback_;
 };
 
 }  // namespace ash
--- chrome/browser/ash/login/wizard_controller_browsertest.cc
+++ chrome/browser/ash/login/wizard_controller_browsertest.cc
@@ -96,6 +96,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/ash/login/consolidated_consent_screen_handler.h"
 #include "chrome/browser/ui/webui/ash/login/display_size_screen_handler.h"
 #include "chrome/browser/ui/webui/ash/login/error_screen_handler.h"
+#include "chrome/browser/ui/webui/ash/login/fjord_fw_update_screen_handler.h"
 #include "chrome/browser/ui/webui/ash/login/fjord_station_setup_screen_handler.h"
 #include "chrome/browser/ui/webui/ash/login/fjord_touch_controller_screen_handler.h"
 #include "chrome/browser/ui/webui/ash/login/gaia_info_screen_handler.h"
@@ -1432,8 +1433,7 @@ class WizardControllerFjordOOBETest
   base::test::ScopedFeatureList scoped_feature_list_;
 };
 
-IN_PROC_BROWSER_TEST_F(WizardControllerFjordOOBETest,
-                       TouchControllerScreenShowsAndExitsToStationSetup) {
+IN_PROC_BROWSER_TEST_F(WizardControllerFjordOOBETest, FjordOobeScreenFlow) {
   WizardController* const wizard_controller =
       WizardController::default_controller();
   ScopedEnrollmentStateFetcherFactory fetcher_factory(
@@ -1468,7 +1468,7 @@ IN_PROC_BROWSER_TEST_F(WizardControllerFjordOOBETest,
   EXPECT_EQ(FjordOobeStateManager::Get()->GetFjordOobeStateInfo().oobe_state(),
             fjord_oobe_state::proto::FjordOobeStateInfo::
                 FJORD_OOBE_STATE_ENROLLMENT_DONE);
-  CheckCurrentScreen(FjordStationSetupScreenView::kScreenId);
+  CheckCurrentScreen(FjordFwUpdateScreenView::kScreenId);
   EXPECT_TRUE(wizard_controller->ExitFjordTouchControllerScreen());
 }
 
