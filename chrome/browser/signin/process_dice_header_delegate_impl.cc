ee65769d30b81fca68355e72390bd4b8c571e52c
Monica Salama <msalama@chromium.org>
2026-01-22 08:50:00 -0800

    [Uno-d clean-up] Remove histogram post deletion of SigninManager
    Bug: 475822503
    Change-Id: Ib990c6b49dc48daee085eb48a258f027ad2eb218
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7510924
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Commit-Queue: Monica Salama <msalama@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1573058}

--- chrome/browser/signin/process_dice_header_delegate_impl.cc
+++ chrome/browser/signin/process_dice_header_delegate_impl.cc
@@ -331,8 +331,6 @@ void ProcessDiceHeaderDelegateImpl::CompleteChromeSignInAfterGaiaSignin(
       IdentityManagerFactory::GetForProfile(&profile_.get());
   CHECK(identity_manager);
   if (!identity_manager->HasPrimaryAccount(signin::ConsentLevel::kSignin)) {
-    base::UmaHistogramEnumeration("Signin.SigninManager.SigninAccessPoint",
-                                  access_point_);
     if (!AttemptSettingPrimaryAccount(account_info)) {
       return;
     }
