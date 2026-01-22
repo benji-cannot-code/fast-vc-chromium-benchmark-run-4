23c1ecad53341c36ad913c185353d7da4cd401fd
Tomasz Lichoń <lichon@google.com>
2026-01-22 03:48:47 -0800

    [TDR] Remove TODOs linked to issue 40186153
    This CL was generated by an AI agent and verified by the CL owner before
    sending for review. The TODOs were identified as stale by TODO Removal
    Agent (http://go/dead-todo-removal-agent).
    * Detailed reasoning can be found in the CL comments.
    * How to review this CL: http://go/tdr-review-instructions
    * To opt-out from future reviews: http://go/tdr-opt-out.
    
    The bug crbug.com/40186153 is marked as 'FIXED'. The diff algorithm does
    not take the version into account (see
    https://crsrc.org/c/content/browser/first_party_sets/first_party_sets_handler_database_helper.cc;drc=731d7da69af6d3c1b97ff8e72cd2b85c872a18a8;l=28,
    note that the GlobalFirstPartySets::public_sets_version() is not used.).
    But this TODO can still be removed since the RWS feature is deprecated
    anyway.
    
    Change-Id: I8a72a83a8b6d3ab7a77dee9911e55f7b96968f87
    Bug: 40186153
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7485573
    Commit-Queue: Tomasz Lichoń <lichon@google.com>
    Reviewed-by: Erik Staab <estaab@chromium.org>
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1572879}

--- content/browser/first_party_sets/first_party_sets_handler_impl_instance.cc
+++ content/browser/first_party_sets/first_party_sets_handler_impl_instance.cc
@@ -216,7 +216,6 @@ void FirstPartySetsHandlerImplInstance::SetPublicFirstPartySets(
     return;
   }
 
-  // TODO(crbug.com/40186153): Use the version to compute sets diff.
   sets_loader_->SetComponentSets(version, std::move(sets_file));
 }
 
