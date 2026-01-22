b6492738c7ed8e4691bfe0e688651a9ddef610ef
Riley Tatum <rtatum@google.com>
2026-01-22 10:13:29 -0800

    Revert "[Autofill] Make ChromeAutofillClient no WebContentsObserver"
    This reverts commit b84e230c45afa4e3f80ff809724b91159e27d673.
    
    Reason for revert: Likely causing unit test failures for Linux UBSan Tests. First failure: https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20UBSan%20Tests/13239/overview
    
    Original change's description:
    > [Autofill] Make ChromeAutofillClient no WebContentsObserver
    >
    > ChromeAutofillClient doesn't observe a single event of
    > WebContentsObserver.
    >
    > To make up for the WebContentsObserver::web_contents(), the CL adds
    > ContentAutofillClient::web_contents(), which forwards to
    > ContentAutofillDriverFactory's WebContentsObserver::web_contents().
    >
    > Bug: 40100455
    > Change-Id: I0c6703d4510db648947287256df73f168b17f2ae
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7498873
    > Reviewed-by: Jan Keitel <jkeitel@google.com>
    > Commit-Queue: Christoph Schwering <schwering@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1572928}
    
    Bug: 40100455
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ib13f58f2cabca78d1c5c308d7a7e7f5f59095183
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7509747
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Riley Tatum <rtatum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1573132}

--- chrome/browser/ui/autofill/chrome_autofill_client.h
+++ chrome/browser/ui/autofill/chrome_autofill_client.h
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/ui/payments/card_unmask_prompt_options.h"
 #include "components/signin/public/identity_manager/account_info.h"
 #include "content/public/browser/visibility.h"
+#include "content/public/browser/web_contents_observer.h"
 
 #if BUILDFLAG(IS_ANDROID)
 #include "chrome/browser/ui/autofill/autofill_snackbar_controller_impl.h"
@@ -87,7 +88,8 @@ enum class SuggestionType;
 // tests derive from it. Member functions should be final unless they need to be
 // mocked or overridden in subclasses and you have verified that they are not
 // called, directly or indirectly, from the constructor.
-class ChromeAutofillClient : public ContentAutofillClient {
+class ChromeAutofillClient : public ContentAutofillClient,
+                             public content::WebContentsObserver {
  public:
   // Creates a new ChromeAutofillClient for the given `web_contents` if no
   // ContentAutofillClient is associated with the `web_contents` yet. Otherwise,
