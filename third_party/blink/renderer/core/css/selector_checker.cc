24be530cf5f22eab35538d8882338542ce11ed3c
Anders Hartvoll Ruud <andruud@chromium.org>
2026-01-23 02:14:18 -0800

    [webmcp] Expect :tool-form-active on <form> elements
    We're currently set up to handle these pseudo-class states
    on form *control* elements, but it should have been form elements.
    
    Unfortunately, it's not possible to write a test against this
    in its current state.
    
    Bug: 475992364
    Change-Id: I47d1efd4a020ad2ec4d9b6c897bbde3a4e4abfec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7510564
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1573576}

--- third_party/blink/renderer/core/css/selector_checker.cc
+++ third_party/blink/renderer/core/css/selector_checker.cc
@@ -68,6 +68,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html/forms/html_button_element.h"
 #include "third_party/blink/renderer/core/html/forms/html_field_set_element.h"
 #include "third_party/blink/renderer/core/html/forms/html_form_control_element.h"
+#include "third_party/blink/renderer/core/html/forms/html_form_element.h"
 #include "third_party/blink/renderer/core/html/forms/html_input_element.h"
 #include "third_party/blink/renderer/core/html/forms/html_option_element.h"
 #include "third_party/blink/renderer/core/html/forms/html_select_element.h"
@@ -2465,10 +2466,14 @@ bool SelectorChecker::CheckPseudoClass(const SelectorCheckingContext& context,
     case CSSSelector::kPseudoAutofillSelected:
       return CheckPseudoAutofill(selector.GetPseudoType(), element);
     case CSSSelector::kPseudoToolFormActive:
+      if (auto* form_element = DynamicTo<HTMLFormElement>(element)) {
+        return form_element->MatchesToolFormActivePseudoClass();
+      }
+      return false;
     case CSSSelector::kPseudoToolSubmitActive:
-      if (auto* form_element = DynamicTo<HTMLFormControlElement>(element)) {
-        return form_element->MatchesToolActive(
-            selector.GetPseudoType() == CSSSelector::kPseudoToolSubmitActive);
+      if (auto* form_control_element =
+              DynamicTo<HTMLFormControlElement>(element)) {
+        return form_control_element->MatchesToolSubmitActivePseudoClass();
       }
       return false;
     case CSSSelector::kPseudoAnyLink:
