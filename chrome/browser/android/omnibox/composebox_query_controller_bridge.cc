752c865881648bad95c62e9dda422681e6f48b1b
Sky Malice <skym@chromium.org>
2026-01-23 11:45:14 -0800

    [AIM] Use controller interface when possible in composebox bridge.
    This sets up to make the switch to ContextualSearchSessionHandle more
    simple when it is done in a future change.
    
    Bug: 455952553
    Change-Id: I9f9dc95393a84f79afaafdda4051498ff0e7437c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7507351
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Cr-Commit-Position: refs/heads/main@{#1573851}

--- chrome/browser/android/omnibox/composebox_query_controller_bridge.cc
+++ chrome/browser/android/omnibox/composebox_query_controller_bridge.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/channel_info.h"
 #include "components/contextual_search/contextual_search_service.h"
 #include "components/contextual_search/contextual_search_types.h"
+#include "components/contextual_search/internal/composebox_query_controller.h"
 #include "components/lens/contextual_input.h"
 #include "components/lens/lens_bitmap_processing.h"
 #include "components/lens/lens_url_utils.h"
@@ -106,12 +107,12 @@ ComposeboxQueryControllerBridge::ComposeboxQueryControllerBridge(
 ComposeboxQueryControllerBridge::~ComposeboxQueryControllerBridge() = default;
 
 void ComposeboxQueryControllerBridge::Destroy(JNIEnv* env) {
-  query_controller_->RemoveObserver(this);
+  query_controller()->RemoveObserver(this);
   delete this;
 }
 
 size_t ComposeboxQueryControllerBridge::GetAttachmentCount() const {
-  return query_controller_->GetFileInfoList().size();
+  return query_controller()->GetFileInfoList().size();
 }
 
 base::WeakPtr<ComposeboxQueryControllerBridge>
@@ -120,7 +121,7 @@ ComposeboxQueryControllerBridge::AsWeakPtr() {
 }
 
 void ComposeboxQueryControllerBridge::NotifySessionStarted(JNIEnv* env) {
-  query_controller_->InitializeIfNeeded();
+  query_controller()->InitializeIfNeeded();
 }
 
 void ComposeboxQueryControllerBridge::NotifySessionAbandoned(JNIEnv* env) {
@@ -169,8 +170,8 @@ ComposeboxQueryControllerBridge::AddFile(
                                         file_bytes_span.end());
   input_data->context_input->push_back(
       lens::ContextualInput(std::move(file_data_vector), mime_type));
-  query_controller_->StartFileUploadFlow(file_token, std::move(input_data),
-                                         std::move(image_options));
+  query_controller()->StartFileUploadFlow(file_token, std::move(input_data),
+                                          std::move(image_options));
 
   return base::android::ConvertUTF8ToJavaString(env, file_token.ToString());
 }
@@ -242,7 +243,7 @@ ComposeboxQueryControllerBridge::CreateSearchUrlRequestInfoFromUrl(GURL url) {
   // TODO(crbug.com/455952553): Rely on the contextual search session handle
   // to track uploaded context tokens.
   for (const contextual_search::FileInfo* file_info :
-       query_controller_->GetFileInfoList()) {
+       query_controller()->GetFileInfoList()) {
     search_url_request_info->file_tokens.push_back(file_info->file_token);
   }
   return search_url_request_info;
@@ -254,7 +255,7 @@ void ComposeboxQueryControllerBridge::GetAimUrl(
     const base::android::JavaRef<jobject>& j_callback) {
   auto search_url_request_info =
       CreateSearchUrlRequestInfoFromUrl(std::move(url));
-  query_controller_->CreateSearchUrl(
+  query_controller()->CreateSearchUrl(
       std::move(search_url_request_info),
       base::BindOnce(&RunJavaCallback,
                      base::android::ScopedJavaGlobalRef<jobject>(j_callback)));
@@ -267,7 +268,7 @@ void ComposeboxQueryControllerBridge::GetImageGenerationUrl(
   auto search_url_request_info =
       CreateSearchUrlRequestInfoFromUrl(std::move(url));
   search_url_request_info->additional_params["imgn"] = "1";
-  query_controller_->CreateSearchUrl(
+  query_controller()->CreateSearchUrl(
       std::move(search_url_request_info),
       base::BindOnce(&RunJavaCallback,
                      base::android::ScopedJavaGlobalRef<jobject>(j_callback)));
@@ -279,7 +280,7 @@ void ComposeboxQueryControllerBridge::RemoveAttachment(
   std::optional<base::UnguessableToken> unguessable_token =
       base::UnguessableToken::DeserializeFromString(token);
   if (unguessable_token.has_value()) {
-    query_controller_->DeleteFile(unguessable_token.value());
+    query_controller()->DeleteFile(unguessable_token.value());
   }
 }
 
@@ -303,10 +304,10 @@ ComposeboxQueryControllerBridge::CreateLensOverlaySuggestInputs() const {
   // TODO(crbug.com/455843962): Rely on the contextual search session handle
   // to track uploaded context tokens.
   for (const contextual_search::FileInfo* file_info :
-       query_controller_->GetFileInfoList()) {
+       query_controller()->GetFileInfoList()) {
     tokens.push_back(file_info->file_token);
   }
-  return query_controller_->CreateSuggestInputs(tokens);
+  return query_controller()->CreateSuggestInputs(tokens);
 }
 
 void ComposeboxQueryControllerBridge::OnFileUploadStatusChanged(
@@ -341,7 +342,7 @@ void ComposeboxQueryControllerBridge::OnGetTabPageContext(
                                  .max_width = 1600,
                                  .compression_quality = 40};
 
-  query_controller_->StartFileUploadFlow(
+  query_controller()->StartFileUploadFlow(
       context_token, std::move(page_content_data), std::move(image_options));
 }
 
