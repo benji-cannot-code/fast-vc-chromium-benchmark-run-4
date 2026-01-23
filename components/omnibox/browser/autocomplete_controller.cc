b0d306e7aabc4df1655cf1b69e713240c3789ac7
Tomasz Wiszkowski <ender@google.com>
2026-01-23 13:36:13 -0800

    Fix crashing LHZPS when Policy blocks DefaultSearchEngine.
    Change-Id: I9df53691488daba53e3ef5b2c19671147ebf0f33
    Fixed: 467055454
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7512930
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1573918}

--- components/omnibox/browser/autocomplete_controller.cc
+++ components/omnibox/browser/autocomplete_controller.cc
@@ -1892,7 +1892,7 @@ void AutocompleteController::UpdateKeywordDescriptions(
       }
 
 #if BUILDFLAG(IS_ANDROID)
-      if (i->keyword == default_engine->keyword()) {
+      if (default_engine && i->keyword == default_engine->keyword()) {
         continue;
       }
 #endif
