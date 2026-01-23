aa45477924e737a029404acae01002c95de5f88c
Shibalik Mohapatra <shibalik@chromium.org>
2026-01-22 16:43:54 -0800

    [Vertical Tab] Create highlight path generator for vertical tabs.
    This change makes the focus ring follow the path of the tab making it
    work for different types of tabs.
    
    Bug: 465159185
    Change-Id: I104d7dbe683d674c3a99968e66d2d9b91e6b6e48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7509540
    Reviewed-by: Kunal Daftari <kunaldaftari@google.com>
    Commit-Queue: Shibalik Mohapatra <shibalik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1573404}

--- chrome/browser/ui/views/tabs/vertical/vertical_tab_view.h
+++ chrome/browser/ui/views/tabs/vertical/vertical_tab_view.h
@@ -66,6 +66,7 @@ class VerticalTabView : public views::View,
 
   TabCloseButton* close_button_for_testing() { return close_button_; }
   bool collapsed_for_testing() { return collapsed_; }
+  SkPath GetPath() const;
 
  private:
   // views::View
@@ -131,8 +132,6 @@ class VerticalTabView : public views::View,
   bool IsHoverAnimationActive() const;
   double GetHoverAnimationValue() const;
   float GetHoverOpacity() const;
-
-  SkPath GetPath() const;
 
   bool IsFrameActive() const;
   TabStyle::TabSelectionState GetSelectionState() const;
