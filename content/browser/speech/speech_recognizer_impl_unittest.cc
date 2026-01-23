d1df22182e204a93af0f272e250dbd7c5feaf1d0
Thomas Guilbert <tguilbert@chromium.org>
2026-01-22 18:41:13 -0800

    [CodeHealth] Spanify FromInterleaved in speech tests
    This CL replaces uses of `AudioBus::FromInterleaved` with a safer
    spanified equivalent `FromInterleavedBytes`
    
    Bug: 373960632
    Change-Id: Ie90646561b74d4c114981c6bce5c84f53d096e67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7512928
    Reviewed-by: Evan Liu <evliu@google.com>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1573458}

--- content/browser/speech/speech_recognizer_impl_unittest.cc
+++ content/browser/speech/speech_recognizer_impl_unittest.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/audio_bus.h"
 #include "media/base/audio_glitch_info.h"
 #include "media/base/audio_sample_types.h"
+#include "media/base/audio_timestamp_helper.h"
 #include "media/base/test_helpers.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "mojo/public/cpp/system/data_pipe.h"
@@ -124,18 +125,20 @@ class SpeechRecognizerImplTest : public SpeechRecognitionEventListener,
                                            kTestingSessionId, false, false,
                                            std::move(sr_engine), std::nullopt);
 
-    int audio_packet_length_bytes =
-        (SpeechRecognizerImpl::kAudioSampleRate *
-         NetworkSpeechRecognitionEngineImpl::kAudioPacketIntervalMs *
-         ChannelLayoutToChannelCount(SpeechRecognizerImpl::kChannelLayout) *
-         SpeechRecognizerImpl::kNumBitsPerAudioSample) /
-        (8 * 1000);
-    audio_packet_.resize(audio_packet_length_bytes);
-
     const int channels =
         ChannelLayoutToChannelCount(SpeechRecognizerImpl::kChannelLayout);
-    int bytes_per_sample = SpeechRecognizerImpl::kNumBitsPerAudioSample / 8;
-    const int frames = audio_packet_length_bytes / channels / bytes_per_sample;
+
+    const int bytes_per_sample =
+        SpeechRecognizerImpl::kNumBitsPerAudioSample / 8;
+
+    const int frames = media::AudioTimestampHelper::TimeToFrames(
+        base::Milliseconds(
+            NetworkSpeechRecognitionEngineImpl::kAudioPacketIntervalMs),
+        SpeechRecognizerImpl::kAudioSampleRate);
+
+    const int audio_packet_length_bytes = frames * channels * bytes_per_sample;
+    audio_packet_.resize(audio_packet_length_bytes);
+
     audio_bus_ = media::AudioBus::Create(channels, frames);
     audio_bus_->Zero();
   }
@@ -246,9 +249,8 @@ class SpeechRecognizerImplTest : public SpeechRecognitionEventListener,
     static_assert(SpeechRecognizerImpl::kNumBitsPerAudioSample == 16,
                   "FromInterleaved expects 2 bytes.");
     // Copy the created signal into an audio bus in a deinterleaved format.
-    audio_bus_->FromInterleaved<media::SignedInt16SampleTypeTraits>(
-        UNSAFE_TODO(reinterpret_cast<int16_t*>(audio_packet_.data())),
-        audio_bus_->frames());
+    audio_bus_->FromInterleavedBytes<media::SignedInt16SampleTypeTraits>(
+        audio_packet_);
   }
 
   void FillPacketWithTestWaveform() {
