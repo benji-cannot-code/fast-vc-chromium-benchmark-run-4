e12bd8dfff829884925477727f2135f69e9f546e
Eshwar Stalin <estalin@chromium.org>
2026-01-23 09:20:08 -0800

    [Vertical Tabs] Fixing styling of bottom container buttons
    Updating the flex specification for bottom container buttons to reflect
    the figma. The tab groups button is scaled to maximum of preferred size
    and the new tab button is scaled maximum unbounded size which matches
    the spec.
    
    Also updating the implementation for icon sizing to be consistent with
    how this was done for top container, since the icon should be scaled in
    the view instead of updating the action item.
    
    Screenshot: https://screenshot.googleplex.com/njtvjAtTzwwfiFG
    Bug: 477620231
    Change-Id: I63f6390df6fe804b3d14c5b374f59599cfa0b96a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7512702
    Reviewed-by: Kunal Daftari <kunaldaftari@google.com>
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1573747}

--- chrome/browser/ui/layout_constants.cc
+++ chrome/browser/ui/layout_constants.cc
@@ -144,6 +144,8 @@ int GetLayoutConstant(LayoutConstant constant) {
       return 8;
     case LayoutConstant::kVerticalTabStripTopButtonIconSize:
       return 20;
+    case LayoutConstant::kVerticalTabStripBottomButtonIconSize:
+      return 20;
     case LayoutConstant::kVerticalTabStripTopButtonPadding:
       return 4;
     case LayoutConstant::kVerticalTabStripBottomButtonPadding:
@@ -209,6 +211,12 @@ gfx::Insets GetLayoutInsets(LayoutInset inset) {
 
     case WEBUI_TAB_STRIP_TOOLBAR_INTERIOR_MARGIN:
       return gfx::Insets::VH(4, 0);
+
+    case VERTICAL_TAB_STRIP_BOTTOM_BUTTON_UNCOLLAPSED:
+      return gfx::Insets::VH(5, 14);
+
+    case VERTICAL_TAB_STRIP_BOTTOM_BUTTON_COLLAPSED:
+      return gfx::Insets::VH(5, 6);
   }
   NOTREACHED();
 }
