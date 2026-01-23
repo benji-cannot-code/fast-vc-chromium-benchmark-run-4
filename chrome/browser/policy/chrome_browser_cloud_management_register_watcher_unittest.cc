5671ced98e81f0a15134ac46e68138c59e3c4356
David Roger <droger@chromium.org>
2026-01-23 00:05:55 -0800

    [enterprise] Fix invariant in WaitUntilCloudPolicyEnrollmentFinished
    The code breaks an invariant where it is expected that the callback for
    the dialog does not outlive the function call: "Unretained(this) is safe
    because `run_loop_` runs in the current scope and is not quit until
    after `callback` executes."
    
    This invariant was broken because an early return bypasses the runloop
    and exits the function without running the callback. The dialog would
    also be in a "showing" state, with no real content.
    
    This may cause memory issues.
    
    This CL:
    - does not show the dialog when it's not needed, by exiting the function
      early. This avoids a situation where a dialog is opened and never
      closed.
    - enforces that the dialog is closed, and the callback no longer
      referenced before the function returns, which makes `Unretained` safe
      to use.
    - adds sequence checks to avoid concurrency issues. These are likely
      unnecessary, but given the history of the changes in the file, they
      may provide confidence and protection against future changes.
    
    Bug: 468010268
    Change-Id: I302592b464b65c58c22185fd58d3b791a01b8db7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7486616
    Commit-Queue: David Roger <droger@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1573536}

--- chrome/browser/policy/chrome_browser_cloud_management_register_watcher_unittest.cc
+++ chrome/browser/policy/chrome_browser_cloud_management_register_watcher_unittest.cc
@@ -74,7 +74,7 @@ class MockEnterpriseStartupDialog : public EnterpriseStartupDialog {
   MOCK_METHOD2(DisplayErrorMessage,
                void(const std::u16string&,
                     const std::optional<std::u16string>&));
-  MOCK_METHOD0(IsShowing, bool());
+  bool IsShowing() override { return !callback_.is_null(); }
 
   void SetCallback(EnterpriseStartupDialog::DialogResultCallback callback) {
     callback_ = std::move(callback);
@@ -156,7 +156,6 @@ TEST_F(ChromeBrowserCloudManagementRegisterWatcherTest, EnrollmentSucceed) {
   base::HistogramTester histogram_tester;
 
   EXPECT_CALL(*dialog(), DisplayLaunchingInformationWithThrobber(_));
-  EXPECT_CALL(*dialog(), IsShowing()).WillOnce(Return(true));
   base::SingleThreadTaskRunner::GetCurrentDefault()->PostTask(
       FROM_HERE,
       base::BindOnce(
@@ -181,7 +180,6 @@ TEST_F(ChromeBrowserCloudManagementRegisterWatcherTest,
   base::HistogramTester histogram_tester;
 
   EXPECT_CALL(*dialog(), DisplayLaunchingInformationWithThrobber(_));
-  EXPECT_CALL(*dialog(), IsShowing()).WillOnce(Return(true));
   storage()->SetEnrollmentErrorOption(false);
   base::SingleThreadTaskRunner::GetCurrentDefault()->PostTask(
       FROM_HERE,
@@ -210,7 +208,6 @@ TEST_F(ChromeBrowserCloudManagementRegisterWatcherTest,
   EXPECT_CALL(*dialog(), DisplayErrorMessage(_, _))
       .WillOnce(
           InvokeWithoutArgs([this] { dialog()->UserClickedTheButton(false); }));
-  EXPECT_CALL(*dialog(), IsShowing()).WillOnce(Return(true));
   base::SingleThreadTaskRunner::GetCurrentDefault()->PostTask(
       FROM_HERE,
       base::BindOnce(
@@ -238,7 +235,6 @@ TEST_F(ChromeBrowserCloudManagementRegisterWatcherTest,
   EXPECT_CALL(*dialog(), DisplayErrorMessage(_, _))
       .WillOnce(
           InvokeWithoutArgs([this] { dialog()->UserClickedTheButton(true); }));
-  EXPECT_CALL(*dialog(), IsShowing()).WillOnce(Return(true));
   base::SingleThreadTaskRunner::GetCurrentDefault()->PostTask(
       FROM_HERE,
       base::BindOnce(
@@ -333,7 +329,6 @@ TEST_F(ChromeBrowserCloudManagementRegisterWatcherTest,
   base::HistogramTester histogram_tester;
 
   EXPECT_CALL(*dialog(), DisplayLaunchingInformationWithThrobber(_));
-  EXPECT_CALL(*dialog(), IsShowing()).WillOnce(Return(true));
   storage()->SetEnrollmentErrorOption(false);
   base::SingleThreadTaskRunner::GetCurrentDefault()->PostTask(
       FROM_HERE,
@@ -365,6 +360,7 @@ TEST_F(ChromeBrowserCloudManagementRegisterWatcherTest,
   histogram_tester.ExpectTotalCount(
       ChromeBrowserCloudManagementRegisterWatcher::kStartupDialogHistogramName,
       0);
+  EXPECT_FALSE(watcher()->IsDialogShowing());
 }
 
 }  // namespace policy
