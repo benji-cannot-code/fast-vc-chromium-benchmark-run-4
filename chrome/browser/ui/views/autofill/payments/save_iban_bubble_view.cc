847edd9ea8c67fcd4ab9ce8bbdf3720a06fe262b
Fernando Ramirez <ferny@google.com>
2026-01-23 14:52:30 -0800

    [IBAN] Save bubble uses Wallet logo instead of GPay logo
    Before & Mock: https://screenshot.googleplex.com/3QohvUYo6CKbWWY.png
    After: https://screenshot.googleplex.com/6ix4AvVYrDYwvou.png
    
    Bug: 468139053
    Change-Id: Ic251896e4de02c860d818c37b0e2c234547da562
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7514299
    Commit-Queue: Fernando Ramirez <ferny@google.com>
    Reviewed-by: Olivia Saul <osaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1573989}

--- chrome/browser/ui/views/autofill/payments/save_iban_bubble_view.cc
+++ chrome/browser/ui/views/autofill/payments/save_iban_bubble_view.cc
@@ -88,7 +88,11 @@ void SaveIbanBubbleView::AddedToWidget() {
   if (controller_->IsUploadSave()) {
     GetBubbleFrameView()->SetTitleView(
         std::make_unique<TitleWithIconAfterLabelView>(
-            GetWindowTitle(), TitleWithIconAfterLabelView::Icon::GOOGLE_PAY));
+            GetWindowTitle(),
+            base::FeatureList::IsEnabled(
+                features::kAutofillEnableWalletBranding)
+                ? TitleWithIconAfterLabelView::Icon::GOOGLE_WALLET
+                : TitleWithIconAfterLabelView::Icon::GOOGLE_PAY));
   }
 }
 
