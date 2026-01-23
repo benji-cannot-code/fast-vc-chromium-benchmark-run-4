a3582ed84349dd111ee06dadce8c094d08b46ab3
Ken Buchanan <kenrb@chromium.org>
2026-01-23 10:16:13 -0800

    [WebAuthn] Make ambient prototype activation more explicit
    Right now any `navigator.credentials.get` request with
    `mediation: 'conditional'` and `password: true` is treated as an
    ambient request in the prototype.
    
    This adds values for a clear invocation of the mode, corresponding to a
    plausible eventual API form: `mediation: 'conditional'` and
    `uiMode: `passive`'.
    
    Bug: 358119268
    Change-Id: I3d59cac23997700b08dffa27f94b31459902acea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7512124
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1573781}

--- third_party/blink/renderer/modules/credentialmanagement/authentication_credentials_container.cc
+++ third_party/blink/renderer/modules/credentialmanagement/authentication_credentials_container.cc
@@ -1446,26 +1446,6 @@ ScriptPromise<IDLNullable<Credential>> AuthenticationCredentialsContainer::get(
                       WebFeature::kCredentialManagerGetPasswordCredential);
   }
 
-  // TODO(crbug.com/358119268): For prototyping, any conditionally-mediated
-  // request that contains both password and publicKey credential types is
-  // assumed to be ambient, when the flag is on. This will change.
-  if (RuntimeEnabledFeatures::WebAuthenticationAmbientEnabled() &&
-      options->hasPublicKey() && options->hasPassword() &&
-      options->password() &&
-      options->mediation() ==
-          V8CredentialMediationRequirement::Enum::kConditional) {
-    // Unsupported ambient credential types:
-    if (options->hasOtp() || options->hasIdentity() ||
-        (options->publicKey()->hasExtensions() &&
-         options->publicKey()->extensions()->hasPayment()) ||
-        options->hasFederated()) {
-      resolver->Reject(MakeGarbageCollected<DOMException>(
-          DOMExceptionCode::kNotSupportedError,
-          "Unsupported combination of credential types requested."));
-      return promise;
-    }
-  }
-
   if (options->hasPublicKey()) {
     ForwardRequestToAuthenticator(script_state, resolver, options);
     return promise;
@@ -2036,8 +2016,23 @@ void AuthenticationCredentialsContainer::ForwardRequestToAuthenticator(
   }
 
   Mediation mediation = Mediation::MODAL;
-  if (options->mediation() ==
-      V8CredentialMediationRequirement::Enum::kConditional) {
+  if (RuntimeEnabledFeatures::WebAuthenticationAmbientEnabled() &&
+      options->uiMode() == V8CredentialUiModeRequirement::Enum::kPassive &&
+      options->mediation() ==
+          V8CredentialMediationRequirement::Enum::kConditional) {
+    // Unsupported ambient credential types:
+    if (options->hasOtp() || options->hasIdentity() ||
+        (options->publicKey()->hasExtensions() &&
+         options->publicKey()->extensions()->hasPayment()) ||
+        options->hasFederated()) {
+      resolver->Reject(MakeGarbageCollected<DOMException>(
+          DOMExceptionCode::kNotSupportedError,
+          "Unsupported combination of credential types requested."));
+      return;
+    }
+    mediation = Mediation::AMBIENT;
+  } else if (options->mediation() ==
+             V8CredentialMediationRequirement::Enum::kConditional) {
     if (IsImmediateGetRequest(*context, *options)) {
       resolver->Reject(MakeGarbageCollected<DOMException>(
           DOMExceptionCode::kNotSupportedError,
