2dc8bf08f8f8070d403e0c027e6aa8f7e732d4aa
Peter Kvitek <kvitekp@chromium.org>
2026-01-23 10:27:40 -0800

    [headless][mac] Implement headless mode window zoom
    This CL replaces standard window zoom behavior with a headless screen
    aware one when running in headless mode. It does this in a way that
    guarantees that normal mode zoom behavior is not affected by injecting
    the headless aware code at runtime using ObjC swizzling.
    
    This is the first in a series of CLs which are all expected to use
    swizzling to implement headless aware minimize, fullscreen and other
    window management functionality.
    
    Drive by: moved constrainFrameRect down to where other NSWindow
    overrides are.
    
    Bug: 474319540,410825510,416088625
    Change-Id: I213edc4feb07cde9747001ffb60b2afc3530b2ce
    Cq-Include-Trybots: luci.chromium.try:mac14-tests,mac13-tests
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7487666
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1573794}

---
+++ components/remote_cocoa/app_shim/native_widget_mac_nswindow_headless.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_REMOTE_COCOA_APP_SHIM_NATIVE_WIDGET_MAC_NSWINDOW_HEADLESS_H_
+#define COMPONENTS_REMOTE_COCOA_APP_SHIM_NATIVE_WIDGET_MAC_NSWINDOW_HEADLESS_H_
+
+#include <optional>
+
+#include "ui/gfx/geometry/rect.h"
+
+// Holds headless mode related info for NSWindow.
+struct NativeWidgetMacNSWindowHeadlessInfo {
+  NativeWidgetMacNSWindowHeadlessInfo();
+
+  bool is_zoomed = false;
+  std::optional<gfx::Rect> restored_bounds;
+};
+
+#endif  // COMPONENTS_REMOTE_COCOA_APP_SHIM_NATIVE_WIDGET_MAC_NSWINDOW_HEADLESS_H_
