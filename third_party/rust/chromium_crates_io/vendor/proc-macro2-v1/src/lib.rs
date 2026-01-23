439d629cd45b41d8ff6b2098569727e9c818234b
Julia Hansbrough <flowerhack@google.com>
2026-01-23 12:34:54 -0800

    Roll proc-macro2: 1.0.105 => 1.0.106 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * proc-macro2: 1.0.105 => 1.0.106;
      https://docs.rs/crate/proc-macro2/1.0.106
    
    Bug: None
    Change-Id: I5720ce4c53934c14743e5d5bfdb9c6cca7dfb951
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7507594
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Julia Hansbrough <flowerhack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1573885}

--- third_party/rust/chromium_crates_io/vendor/proc-macro2-v1/src/lib.rs
+++ third_party/rust/chromium_crates_io/vendor/proc-macro2-v1/src/lib.rs
@@ -85,7 +85,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //! a different thread.
 
 #![no_std]
-#![doc(html_root_url = "https://docs.rs/proc-macro2/1.0.105")]
+#![doc(html_root_url = "https://docs.rs/proc-macro2/1.0.106")]
 #![cfg_attr(any(proc_macro_span, super_unstable), feature(proc_macro_span))]
 #![cfg_attr(super_unstable, feature(proc_macro_def_site))]
 #![cfg_attr(docsrs, feature(doc_cfg))]
