11266f9b1bf1d6c05b53f289b51bea70cc3247bf
Sam Goto <goto@chromium.org>
2026-01-23 09:47:32 -0800

    [fedcm] Ignore interception header when on the initial empty document
    This CL addresses a bug that happens when the interception is sent
    as the continuation API window is getting loaded.
    
    In this case, the continuation API is on the initial empty document,
    so it is not a page that we can intercept navigation out of.
    
    When window.open() is called to open pop-up windows, the pop-up window
    is not loaded with an initial empty document, and so remains to be
    able to have the navigation intercepted (which is the desired
    behavior).
    
    Bug: 478032661
    Change-Id: I8255f0ae0d2c8c80c33a5c525e2a2d64a439d676
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7512763
    Commit-Queue: Sam Goto <goto@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1573763}

--- content/browser/webid/navigation_interceptor.cc
+++ content/browser/webid/navigation_interceptor.cc
@@ -9,7 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/strings/string_split.h"
 #include "base/values.h"
+#include "content/browser/renderer_host/frame_tree_node.h"
 #include "content/browser/webid/flags.h"
+#include "content/browser/webid/identity_registry.h"
 #include "content/browser/webid/request_service.h"
 #include "content/browser/webid/webid_utils.h"
 #include "content/public/browser/navigation_controller.h"
@@ -111,6 +113,16 @@ NavigationInterceptor::ProcessRequest() {
   content::RenderFrameHost* rfh = document_.AsRenderFrameHostIfValid();
 
   if (!rfh) {
+    return PROCEED;
+  }
+
+  if (IdentityRegistry::FromWebContents(
+          WebContents::FromRenderFrameHost(rfh)) &&
+      FrameTreeNode::From(rfh)->is_on_initial_empty_document()) {
+    // In pop-up windows that FedCM opens itself, such as when using the
+    // Continuation API or when the user is logged out, the render frame host
+    // needs to be loaded with a valid document before we can intercept
+    // navigations out of it.
     return PROCEED;
   }
 
