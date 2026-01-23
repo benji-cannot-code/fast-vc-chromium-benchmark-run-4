f7a3150895a307de24b4306e759e7bb799a5d01f
Brian Sheedy <bsheedy@chromium.org>
2026-01-22 16:13:16 -0800

    Add additional MediaRecorder logging
    Adds additional debug logging to the JavaScript file used by
    ExpectedColor_MediaRecorderFromVideoElement to help debug where
    flaky hangs are happening.
    
    Bug: 476172416
    Change-Id: Ic4d2dcb76c5ed99a3dbdbd7c956d271855006cb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7509738
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1573365}

--- content/test/data/gpu/media_recorder_from_video_element.js
+++ content/test/data/gpu/media_recorder_from_video_element.js
@@ -59,8 +59,11 @@ function startPlayback() {
 }
 
 function startRecording() {
+  logOutput('crbug.com/476172416: startRecording() called');
   stream = srcVideo.captureStream(30);
+  logOutput('crbug.com/476172416: Stream captured');
   recorder = new MediaRecorder(stream, { mimeType });
+  logOutput('crbug.com/476172416: MediaRecorder created');
   recorder.onstop = startPlayback;
   recorder.ondataavailable = (e) => {
     logOutput(`Recorder data available. ${e.data.size}`);
@@ -73,7 +76,9 @@ function startRecording() {
   };
 
   // Start recording and ask it to emit encoded data every 100 ms.
+  logOutput('crbug.com/476172416: Starting recorder');
   recorder.start(100);
+  logOutput('crbug.com/476172416: Playing video');
   srcVideo.play();
 
   logOutput('Recording started.');
@@ -93,6 +98,7 @@ function main() {
   dstVideo = document.getElementById('dst-video');
   dstVideo.loop = false;
   dstVideo.muted = true;  // No need to exercise audio paths.
+  logOutput('crbug.com/476172416: Setting video size');
   setVideoSize();
 
   srcVideo.onerror = _ => {
@@ -100,5 +106,6 @@ function main() {
     abort = true;
     sendResult('FAIL');
   };
+  logOutput('crbug.com/476172416: Requesting video frame callback');
   srcVideo.requestVideoFrameCallback(startRecording);
 }
