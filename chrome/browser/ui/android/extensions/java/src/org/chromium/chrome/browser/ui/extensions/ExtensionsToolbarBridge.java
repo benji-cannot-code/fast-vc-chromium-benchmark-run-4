79bc2df070480efee713c9f793742ecdd5061b3a
Masa Fujita <massan@google.com>
2026-01-22 18:10:38 -0800

    actions revamp: Refresh action properties on WebContents change
    This CL removes a TODO and implements
    `ExtensionsToolbarBridge::OnActiveWebContentsChanged()` so that it
    results in Java side's UI flushing.
    
    Bug: 476295562
    Change-Id: I7759071846fdeafaabd6119801695b76f086e1a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7500858
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Masa Fujita <massan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1573441}

--- chrome/browser/ui/android/extensions/java/src/org/chromium/chrome/browser/ui/extensions/ExtensionsToolbarBridge.java
+++ chrome/browser/ui/android/extensions/java/src/org/chromium/chrome/browser/ui/extensions/ExtensionsToolbarBridge.java
@@ -137,6 +137,13 @@ public class ExtensionsToolbarBridge implements Destroyable {
         }
     }
 
+    @CalledByNative
+    public void onActiveWebContentsChanged() {
+        for (Observer observer : mObservers) {
+            observer.onActiveWebContentsChanged();
+        }
+    }
+
     public interface Observer {
         // Called after all actions are added to the model.
         void onActionsInitialized();
@@ -152,6 +159,9 @@ public class ExtensionsToolbarBridge implements Destroyable {
 
         // Called when the pinned actions in the model are changed.
         void onPinnedActionsChanged();
+
+        // Called when the active web contents changes due to e.g. navigation or tab change.
+        void onActiveWebContentsChanged();
     }
 
     public interface Delegate {
