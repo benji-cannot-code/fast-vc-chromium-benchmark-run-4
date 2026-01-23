03c8bbf6e9e1251de85d67a5428929759bed28db
Jacobo Tapia <jatapiaro@google.com>
2026-01-23 10:24:20 -0800

    [Reading Mode]: Rely on keyDownOn for key events in Toolbar Settings Menu tests
    Previously we manually dispatched the keyboard events. A keyDownOn
    function already exists in web-ui-test-utils to achieve the same
    behavior.
    
    Fixed: 476155995
    Change-Id: Ib7aad443d1fef4f91bc22a90659665f84f9095b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7489306
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Commit-Queue: Jacobo Tapia <jatapiaro@google.com>
    Cr-Commit-Position: refs/heads/main@{#1573789}

--- chrome/test/data/webui/side_panel/read_anything/toolbar_settings_menu_test.ts
+++ chrome/test/data/webui/side_panel/read_anything/toolbar_settings_menu_test.ts
@@ -179,12 +179,7 @@ suite('Toolbar Settings Menu', () => {
     assertTrue(!!languageMenu);
     const dialog = languageMenu.$.languageMenu;
 
-    dialog.dispatchEvent(new PointerEvent('click', {
-      bubbles: true,
-      composed: true,
-      cancelable: true,
-      view: window,
-    }));
+    dialog.click();
 
     assertTrue(settingsMenu.$.lazyMenu.get().open);
   });
@@ -200,11 +195,7 @@ suite('Toolbar Settings Menu', () => {
     const elapsedTime = MENU_SHOW_DELAY_MS - 10;
     timer.tick(elapsedTime);
 
-    settingsMenu.dispatchEvent(new KeyboardEvent('keydown', {
-      key: 'ArrowDown',
-      bubbles: true,
-      composed: true,
-    }));
+    keyDownOn(settingsMenu, 0, undefined, 'ArrowDown');
     timer.tick(elapsedTime + 20);
     assertFalse(toolbar.$.fontMenu.$.menu.$.lazyMenu.get().open);
     timer.uninstall();
@@ -239,11 +230,7 @@ suite('Toolbar Settings Menu', () => {
     chrome.readingMode.onLinksEnabledToggled = () => {
       didToggleLinks = true;
     };
-    settingsMenu.dispatchEvent(new KeyboardEvent('keydown', {
-      key: 'ArrowRight',
-      bubbles: true,
-      composed: true,
-    }));
+    keyDownOn(settingsMenu, 0, undefined, 'ArrowRight');
     assertFalse(didToggleLinks);
   });
 
