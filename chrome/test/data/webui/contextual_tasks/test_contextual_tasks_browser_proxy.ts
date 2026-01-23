09def33e862c750e24a6c170a4284b39b361fcdc
Bryan Nguyen <nguyenbryan@google.com>
2026-01-23 13:47:32 -0800

    [Contextual Tasks] Add ghost loader for non-AIM navigations.
    Ghost loader is shown during OnSearchResultsNavigationInSidePanel in the
    UI service to ensure that it only shows for intercepted side panel
    navigations. It is hidden by both DOMContentLoaded and DidFailLoad -
    both outcomes were observed during testing.
    
    Behind a default-on feature param.
    
    Demo: http://shortn/_IeepTVYHF2
    
    Bug: 463731068
    Change-Id: Ida31b7305ff068abced668e93b6bbdf5a8d8e104
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7416821
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Bryan Nguyen <nguyenbryan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1573933}

--- chrome/test/data/webui/contextual_tasks/test_contextual_tasks_browser_proxy.ts
+++ chrome/test/data/webui/contextual_tasks/test_contextual_tasks_browser_proxy.ts
@@ -121,6 +121,7 @@ class TestContextualTasksPageHandler extends TestBrowserProxy implements
       'getSearchUrl',
       'getThreadUrl',
       'getUrlForTask',
+      'isAiPage',
       'isShownInTab',
       'isZeroState',
       'moveTaskUiToNewTab',
@@ -180,6 +181,11 @@ class TestContextualTasksPageHandler extends TestBrowserProxy implements
   isZeroState(url: Url) {
     this.methodCalled('isZeroState', url);
     return Promise.resolve({isZeroState: false});
+  }
+
+  isAiPage(url: Url) {
+    this.methodCalled('isAiPage', url);
+    return Promise.resolve({isAiPage: false});
   }
 
   openMyActivityUi() {
