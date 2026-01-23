cdfa237663f833328eb0b4453dbefa02ae703fd0
Dana Fried <dfried@chromium.org>
2026-01-23 07:21:26 -0800

    Reland "[Vertical Tabs] Improve minimum size calculation for grab handle"
    This reverts commit 97490b30b47f77c43b6ec60ae9c3fc325b1ac419.
    
    Reason for revert: disabled test that is no longer reliable on bots
    
    Original change's description:
    > Revert "[Vertical Tabs] Improve minimum size calculation for grab handle"
    >
    > This reverts commit a69b923fd96f829f1c4db6cd607c2589d88a6e08.
    >
    > Reason for revert:
    > LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/4544470616899584
    >
    > Sample build with failed test: https://ci.chromium.org/b/8691948624539055969
    > Affected test(s):
    > [://chrome/test\:browser_tests!gtest::SidePanelCoordinatorTest#ChangeSidePanelWidthNarrowWindow](https://ci.chromium.org/ui/test/chromium/:%2F%2Fchrome%2Ftest%5C:browser_tests%21gtest::SidePanelCoordinatorTest%23ChangeSidePanelWidthNarrowWindow?q=VHash%3A2876d8e0ab8a0546)
    >
    > If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F4544470616899584&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F7505515&type=BUG
    >
    > Original change's description:
    > > [Vertical Tabs] Improve minimum size calculation for grab handle
    > >
    > > This factors the grab handle into the minimum size of the browser window
    > > when in Vertical Tabs mode.
    > >
    > > This is a prerequisite for fixing the bug below.
    > >
    > > Bug: 477303364
    > > Change-Id: I845cd28b6d47530ef67906646c4d41d56d4e493a
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7505515
    > > Reviewed-by: Alison Gale <agale@chromium.org>
    > > Commit-Queue: Dana Fried <dfried@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1573114}
    > >
    >
    > Bug: 477303364
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I37f9d9825ea028326a47be2bd9071f01d9705e52
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7512326
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Reviewed-by: Alison Gale <agale@chromium.org>
    > Commit-Queue: Riley Tatum <rtatum@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1573258}
    
    Bug: 477303364
    Change-Id: I601728c52a7ec80e832c4fed8f8ffe1e2e3b6f64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7509334
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Auto-Submit: Dana Fried <dfried@chromium.org>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1573675}

--- chrome/browser/ui/views/frame/vertical_tab_strip_region_view.cc
+++ chrome/browser/ui/views/frame/vertical_tab_strip_region_view.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/vertical_tab_strip_state_controller.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/custom_corners_background.h"
+#include "chrome/browser/ui/views/frame/tab_strip_region_view.h"
 #include "chrome/browser/ui/views/tabs/vertical/root_tab_collection_node.h"
 #include "chrome/browser/ui/views/tabs/vertical/tab_collection_node.h"
 #include "chrome/browser/ui/views/tabs/vertical/vertical_tab_drag_handler.h"
@@ -200,6 +201,13 @@ void VerticalTabStripRegionView::ResetTabStrip() {
   RemoveChildViewT(std::exchange(drag_handler_, nullptr));
 
   root_node_.reset();
+}
+
+gfx::Size VerticalTabStripRegionView::GetMinimumSize() const {
+  auto min_size = TabStripRegionView::GetMinimumSize();
+  min_size.set_width(state_controller_->IsCollapsed() ? kCollapsedWidth
+                                                      : kUncollapsedMinWidth);
+  return min_size;
 }
 
 bool VerticalTabStripRegionView::IsTabStripEditable() const {
