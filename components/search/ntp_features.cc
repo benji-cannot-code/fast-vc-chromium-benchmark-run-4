7aa9b060cf7259ced89b072f0098d913c0dfadf2
Guillaume Jenkins <gujen@google.com>
2026-01-23 09:37:51 -0800

    [NTP] Clean up desktop-to-mobile NTP promo
    The desktop-to-mobile NTP promo has ben canceled for a while and the
    code is obsolete. The feature had already been cleaned up on the NTP UI
    itself, but some logic outside of the NTP UI code still existed. This CL
    cleans that up.
    
    Fixed: 428706084, 400504703, 400504700, 374120681, 374120703
    Change-Id: I925586f9287d3ad7c53f0352df2c36ab8b847193
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7511583
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1573758}

--- components/search/ntp_features.cc
+++ components/search/ntp_features.cc
@@ -15,14 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "build/build_config.h"
 
-namespace {
-
-const char kMobilePromoQRCodeURL[] =
-    "https://apps.apple.com/app/apple-store/"
-    "id535886823?pt=9008&ct=desktop-chr-ntp&mt=8";
-
-}  // namespace
-
 namespace ntp_features {
 
 // If enabled, shows a confirm dialog before removing search suggestions from
@@ -232,9 +224,6 @@ BASE_FEATURE(kNtpWallpaperSearchButtonHideCondition,
 BASE_FEATURE(kNtpWallpaperSearchButtonAnimationShownThreshold,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-// Feature to control the display of a mobile promo on the NTP.
-BASE_FEATURE(kNtpMobilePromo, base::FEATURE_DISABLED_BY_DEFAULT);
-
 // If enabled, the Microsoft Authentication module will be shown.
 BASE_FEATURE(kNtpMicrosoftAuthenticationModule,
              base::FEATURE_ENABLED_BY_DEFAULT);
@@ -335,7 +324,6 @@ const char kNtpWallpaperSearchButtonHideConditionParam[] =
 const char kNtpWallpaperSearchButtonAnimationShownThresholdParam[] =
     "NtpWallpaperSearchButtonAnimationShownThresholdParam";
 const char kWallpaperSearchHatsDelayParam[] = "WallpaperSearchHatsDelayParam";
-const char kNtpMobilePromoTargetUrlParam[] = "NtpMobilePromoTargetUrlParam";
 
 const base::FeatureParam<bool> kNtpNextShowStaticTextParam(
     &ntp_features::kNtpNextFeatures,
@@ -421,10 +409,6 @@ const base::FeatureParam<bool> kNtpRealboxCr23SteadyStateShadow(
     &ntp_features::kRealboxCr23Theming,
     "kNtpRealboxCr23SteadyStateShadow",
     false);
-const base::FeatureParam<int> kNtpMobilePromoImpressionLimit(
-    &ntp_features::kNtpMobilePromo,
-    "kNtpMobilePromoImpressionLimit",
-    10);
 const base::FeatureParam<bool>
     kNtpMostRelevantTabResumptionModuleFilterLocalTabsParam{
         &kNtpMostRelevantTabResumptionModule,
@@ -535,13 +519,6 @@ int GetWallpaperSearchButtonHideCondition() {
   return base::GetFieldTrialParamByFeatureAsInt(
       kNtpWallpaperSearchButtonHideCondition,
       kNtpWallpaperSearchButtonHideConditionParam, 2);
-}
-
-std::string GetMobilePromoTargetURL() {
-  std::string field_trial_url = base::GetFieldTrialParamValueByFeature(
-      ntp_features::kNtpMobilePromo,
-      ntp_features::kNtpMobilePromoTargetUrlParam);
-  return (field_trial_url.empty()) ? kMobilePromoQRCodeURL : field_trial_url;
 }
 
 int GetMaxTilesBeforeShowMore() {
