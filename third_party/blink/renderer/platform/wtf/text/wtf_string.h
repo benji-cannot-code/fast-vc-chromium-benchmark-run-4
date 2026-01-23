b4b7ec3c1e0608fa5c2556706e3a22c1fa7d9aa7
Kent Tamura <tkent@chromium.org>
2026-01-23 01:54:39 -0800

    WTF: Add `size_type` and `npos` to Blink string classes
    for better compatibility with std::string and std::string_view.
    This CL has no behavior changes.
    
    Bug: 473854537
    Change-Id: I2116ee5bf080a9810c3310a3918675fe7d026b43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7510090
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1573568}

--- third_party/blink/renderer/platform/wtf/text/wtf_string.h
+++ third_party/blink/renderer/platform/wtf/text/wtf_string.h
@@ -57,6 +57,9 @@ class WTF_EXPORT String {
   USING_FAST_MALLOC(String);
 
  public:
+  using size_type = string_size_t;
+  static constexpr size_type npos = kNotFound;
+
   // Construct a null string, distinguishable from an empty string.
   String() = default;
 
