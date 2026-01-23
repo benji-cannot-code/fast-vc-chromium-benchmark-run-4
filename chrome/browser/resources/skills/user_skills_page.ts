42e5b539f93b6c855c7b6943cbc82dca769e4e15
chrstne <chrstne@google.com>
2026-01-23 15:26:48 -0800

    Implement the zerostate page in chrome://skills
    - Implements the page that shows up when there are no skills
    associated to a user
    - Re-added explicit call to path changing behavior on CrRouter SetPath
    
    context: go/chrome-skills-1p
    
    Screenshot:https://screenshot.googleplex.com/8MwJvFbGRQC4UjV
    
    Bug: b:475590751
    Change-Id: If0806dd8c1c646fefbfcc36cce5581a9f23725d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7488318
    Commit-Queue: Christine Ying <chrstne@google.com>
    Reviewed-by: Jacob Stanley <jacobstanley@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1574005}

--- chrome/browser/resources/skills/user_skills_page.ts
+++ chrome/browser/resources/skills/user_skills_page.ts
@@ -5,11 +5,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
 
+import {getCss} from './user_skills_page.css.js';
 import {getHtml} from './user_skills_page.html.js';
+
 
 export class UserSkillsPageElement extends CrLitElement {
   static get is() {
     return 'user-skills-page';
+  }
+
+  static override get styles() {
+    return getCss();
   }
 
   override render() {
