105fdc1def5f576c8fa6b68979796c2364f16939
Qingxin Wu <qingxinwu@google.com>
2026-01-23 07:53:05 -0800

    Add feature flag kWebUISplitTabsButton.
    It's default to false. Also add a new function IsWebUIToolbarEnabled()
    which is true when at least one of WebUI reload button and split tabs
    button are enabled.
    
    Bug: 470039098
    
    Change-Id: I823bb967194ced0ec2b72f80cad2b9d65b5e6463
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7507149
    Commit-Queue: Qingxin Wu <qingxinwu@google.com>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1573696}

--- chrome/browser/ui/webui/webui_toolbar/webui_toolbar_ui.cc
+++ chrome/browser/ui/webui/webui_toolbar/webui_toolbar_ui.cc
@@ -75,7 +75,7 @@ WebUIToolbarConfig::WebUIToolbarConfig()
 
 bool WebUIToolbarConfig::IsWebUIEnabled(
     content::BrowserContext* browser_context) {
-  return features::IsWebUIReloadButtonEnabled();
+  return features::IsWebUIToolbarEnabled();
 }
 
 void WebUIToolbarUI::BindInterface(
--- chrome/common/chrome_features.h
+++ chrome/common/chrome_features.h
@@ -1347,6 +1347,8 @@ extern const base::FeatureParam<bool> kWebUIReloadButtonRestartUnresponsive;
 COMPONENT_EXPORT(CHROME_FEATURES)
 extern const base::FeatureParam<base::TimeDelta>
     kWebUIReloadButtonRestartUnresponsiveRenderersTimeout;
+COMPONENT_EXPORT(CHROME_FEATURES)
+BASE_DECLARE_FEATURE(kWebUISplitTabsButton);
 #endif  // !BUILDFLAG(IS_ANDROID)
 
 COMPONENT_EXPORT(CHROME_FEATURES)
