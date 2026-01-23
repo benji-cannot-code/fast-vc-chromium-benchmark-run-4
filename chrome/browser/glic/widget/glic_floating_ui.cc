02ad98c549068307eb661f0dda9f4e499db12ae7
Bryant Chandler <bryantchandler@google.com>
2026-01-23 10:36:09 -0800

    [glic] Set the widget web contents before Show
    This allows the site to get the intial size before
    `notifyPanelWillOpen` is called.
    
    This doesn't fix the bug by itself. The web client is
    also going to stop sending resize requests before
    `notifyPanelWillOpen` is called.
    
    Bug: b:477665175
    Change-Id: I4f4d73cb9baf1d0243cabc185bcb18186a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7513354
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1573797}

--- chrome/browser/glic/widget/glic_floating_ui.cc
+++ chrome/browser/glic/widget/glic_floating_ui.cc
@@ -124,6 +124,7 @@ void GlicFloatingUi::CreateAndSetupWidget(gfx::Rect initial_bounds) {
   auto glic_view =
       std::make_unique<GlicView>(profile_, initial_bounds.size(),
                                  glic_panel_hotkey_manager_->GetWeakPtr());
+  glic_view->SetWebContents(delegate_->host().webui_contents());
   glic_delegate_ =
       GlicWidget::CreateWidgetDelegate(std::move(glic_view), user_resizable_);
   glic_widget_ = GlicWidget::Create(glic_delegate_.get(), profile_,
@@ -291,7 +292,6 @@ void GlicFloatingUi::Show(const ShowOptions& options) {
   instance_metrics_->OnShowInFloaty(options);
   GlicProfileManager::GetInstance()->SetCurrentDetachedGlic(profile_);
   GetGlicWidget()->Show();
-  GetGlicView()->SetWebContents(delegate_->host().webui_contents());
   GetGlicView()->UpdateBackgroundColor();
   application_hotkey_manager_->InitializeAccelerators();
   glic_panel_hotkey_manager_->InitializeAccelerators();
