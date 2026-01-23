50e6362ed51ee9fb9d478795ca02e6893fc331e8
Bryan Bernhart <bryan.bernhart@intel.com>
2026-01-23 11:10:17 -0800

    WebNN: fix interop tests when fp16 is unsupported
    Unconditional requests for 'shader-f16' caused requestDevice() to throw
    a TypeError on unsupported hardware, aborting the entire test suite.
    This CL only requires the feature if the adapter supports it and ensures
    'float32' tests can run even if 'float16' cannot.
    
    Bug: 345352987
    Change-Id: I696d6375c0c17459f8c6d6afc21971865105ae72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7510471
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Commit-Queue: Bernhart, Bryan <bryan.bernhart@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1573826}

--- third_party/blink/web_tests/external/wpt/webnn/conformance_tests/tensor.https.any.js
+++ third_party/blink/web_tests/external/wpt/webnn/conformance_tests/tensor.https.any.js
@@ -1402,8 +1402,16 @@ const testExportToGPU = (testName, dataType) => {
       return;
     }
 
-    gpuDevice =
-        await gpuAdapter.requestDevice({requiredFeatures: ['shader-f16']});
+    const requiredFeatures = [];
+    if (dataType === 'float16') {
+      if (!gpuAdapter.features.has('shader-f16')) {
+        isExportToGPUSupported = false;
+        return;
+      }
+      requiredFeatures.push('shader-f16');
+    }
+
+    gpuDevice = await gpuAdapter.requestDevice({requiredFeatures});
     if (!gpuDevice) {
       isExportToGPUSupported = false;
       return;
