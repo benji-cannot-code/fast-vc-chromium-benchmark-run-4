fd0ca1f8cd6c6db1f1b986a2cfcb0671d4bfe59a
David Yeung <dayeung@chromium.org>
2026-01-23 07:46:43 -0800

    Use SetIgnoreZoomGestures in WebUILoginView
    Refactor to use `SetIgnoreZoomGestures` instead of manually intercepting
    and checking gesture events.
    
    This simplifies the code by removing the `PreHandleGestureEvent`
    implementation and removes the dependency on Blink input types. This
    also centralizes the logic so zoom capabilities are uniform across
    pages.
    
    Bug: 475836809
    Change-Id: I041d722d5cca7149b3005ab89afcb78ddab1477e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7511953
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1573692}

--- chrome/browser/ui/ash/login/webui_login_view.cc
+++ chrome/browser/ui/ash/login/webui_login_view.cc
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_ui.h"
 #include "extensions/browser/view_type_utils.h"
 #include "extensions/common/mojom/view_type.mojom.h"
-#include "third_party/blink/public/common/input/web_input_event.h"
 #include "third_party/blink/public/common/renderer_preferences/renderer_preferences.h"
 #include "ui/accessibility/ax_node_data.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -72,7 +71,6 @@ using ::web_modal::WebContentsModalDialogManager;
 
 void InitializeWebView(views::WebView* web_view) {
   WebContents* web_contents = web_view->GetWebContents();
-
   views::WebContentsSetBackgroundColor::CreateForWebContentsWithColor(
       web_contents, SK_ColorTRANSPARENT);
 
@@ -85,6 +83,9 @@ void InitializeWebView(views::WebView* web_view) {
   BrowserController::GetInstance()->CreateAutofillClientForWebContents(
       web_contents);
   ChromePasswordManagerClient::CreateForWebContents(web_contents);
+
+  // Disable pinch zooming.
+  web_contents->SetIgnoreZoomGestures(true);
 
 #if BUILDFLAG(SAFE_BROWSING_AVAILABLE)
   // Create the password reuse detection manager.
@@ -348,13 +349,6 @@ bool WebUILoginView::CheckMediaAccessPermission(
     blink::mojom::MediaStreamType type) {
   return MediaCaptureDevicesDispatcher::GetInstance()
       ->CheckMediaAccessPermission(render_frame_host, security_origin, type);
-}
-
-bool WebUILoginView::PreHandleGestureEvent(
-    content::WebContents* source,
-    const blink::WebGestureEvent& event) {
-  // Disable pinch zooming.
-  return blink::WebInputEvent::IsPinchGestureEventType(event.GetType());
 }
 
 void WebUILoginView::OnFocusLeavingSystemTray(bool reverse) {
