494a83689bdfa80717190145648037dfadd1951c
Khushal Sagar <khushalsagar@chromium.org>
2026-01-22 21:11:56 -0800

    actor: Refactor browser-side setup for script tools/webmcp
    Use a direct sub-class of TabToolRequest and Tool for browser-side
    management of script tool requests instead of using PageTool as the
    base.
    
    The behaviour between script tools and other actuation based tools is
    different such that this inheritance doesn't make sense. Script tools
    don't need to wait for page stabilization and observation shouldn't
    be delayed.
    
    Change-Id: Iee75d9d434cfd52a39fce1890343699ca02ad951
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7509063
    Commit-Queue: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1573500}

--- chrome/browser/actor/tools/page_tool.cc
+++ chrome/browser/actor/tools/page_tool.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/actor/actor_features.h"
 #include "chrome/browser/actor/actor_task.h"
 #include "chrome/browser/actor/aggregated_journal.h"
+#include "chrome/browser/actor/browser_action_util.h"
 #include "chrome/browser/actor/execution_engine.h"
 #include "chrome/browser/actor/tools/observation_delay_controller.h"
 #include "chrome/browser/actor/tools/page_target_util.h"
@@ -406,9 +407,9 @@ tabs::TabHandle PageTool::GetTargetTab() const {
 void PageTool::OnRenderFrameHostChanged() {
   // Return error if tab itself is closed or the WebContents hosted in the tab
   // is being destroyed.
-  if (!request_->GetTabHandle().Get() ||
-      request_->GetTabHandle().Get()->GetContents()->IsBeingDestroyed()) {
-    FinishInvoke(MakeResult(mojom::ActionResultCode::kTabWentAway));
+  if (auto tab_error =
+          MaybeGetErrorCodeForTab(request_->GetTabHandle().Get())) {
+    FinishInvoke(MakeResult(*tab_error));
     return;
   }
 
@@ -421,12 +422,9 @@ void PageTool::OnRenderFrameHostChanged() {
 void PageTool::OnRenderFrameGone() {
   auto* tab_interface = request_->GetTabHandle().Get();
 
-  mojom::ActionResultCode result_code = mojom::ActionResultCode::kFrameWentAway;
-  if (!tab_interface || tab_interface->GetContents()->IsBeingDestroyed()) {
-    result_code = mojom::ActionResultCode::kTabWentAway;
-  } else if (tab_interface->GetContents()->IsCrashed()) {
-    result_code = mojom::ActionResultCode::kRendererCrashed;
-  }
+  mojom::ActionResultCode result_code =
+      MaybeGetErrorCodeForTab(tab_interface)
+          .value_or(mojom::ActionResultCode::kFrameWentAway);
   FinishInvoke(MakeResult(result_code));
 }
 
