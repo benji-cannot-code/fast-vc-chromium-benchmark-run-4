6e13f15dbad6dfa7e7683d20ff5c8a0c57789774
marissa shen <marissashen@google.com>
2026-01-22 16:20:42 -0800

    Set up frontend for Skills Dialog.
    UI: https://screenshot.googleplex.com/3MwtiMykqcTR8Au
    
    Bug: b:466669081
    Change-Id: Iea64b6e658cb2821fd7cb3348a90e268e2645205
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7457861
    Reviewed-by: Christine Ying <chrstne@google.com>
    Commit-Queue: marissa shen <marissashen@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1573378}

---
+++ chrome/browser/resources/skills/skills_dialog_app.ts
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import '/strings.m.js';
+import 'chrome://resources/cr_elements/cr_button/cr_button.js';
+import 'chrome://resources/cr_elements/cr_input/cr_input.js';
+import 'chrome://resources/cr_elements/cr_textarea/cr_textarea.js';
+
+import {CrLitElement} from 'chrome://resources/lit/v3_0/lit.rollup.js';
+
+import {getCss} from './skills_dialog.css.js';
+import {getHtml} from './skills_dialog_app.html.js';
+
+export class SkillsDialogAppElement extends CrLitElement {
+  static get is() {
+    return 'skills-dialog-app';
+  }
+
+  static override get styles() {
+    return getCss();
+  }
+
+  override render() {
+    return getHtml.bind(this)();
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'skills-dialog-app': SkillsDialogAppElement;
+  }
+}
+
+customElements.define(SkillsDialogAppElement.is, SkillsDialogAppElement);
--- tools/gritsettings/resource_ids.spec
+++ tools/gritsettings/resource_ids.spec
@@ -695,7 +695,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     "includes": [4990],
   },
   "<(SHARED_INTERMEDIATE_DIR)/chrome/browser/resources/skills/resources.grd": {
-    "META": {"sizes": {"includes": [10],}},
+    "META": {"sizes": {"includes": [20],}},
     "includes": [4991],
   },
   "<(SHARED_INTERMEDIATE_DIR)/chrome/browser/resources/suggest_internals/resources.grd": {
