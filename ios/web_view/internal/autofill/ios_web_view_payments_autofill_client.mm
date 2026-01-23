bc591014f0e9a785055e3602360535058f8e1945
YiwenQian <yiwenqian@google.com>
2026-01-23 11:10:56 -0800

    [Hagrid iGA] Enable risk based authentication when Hagrid enabled
    This CL modifies
    IOSWebViewPaymentsAutofillClient::IsRiskBasedAuthEffectivelyAvailable to
    exclusively check the kRiskBasedAuthenticationEnabled preference.
    
    Previously, this method also returned true if Virtual Card Number (VCN)
    usage was enabled (kCWVAutofillVCNUsageEnabled). By removing the VCN
    check, Server Cards now correctly respect their own feature flag and
    fall back to the CVC prompt when Risk-Based Authentication is disabled.
    VCN functionality remains unaffected as FetchVirtualCard does not rely
    on this availability check.
    
    Bug: 474412457
    Change-Id: I1072fb6fb4395d8e3e3e4a2842ac6d8176918b22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7477179
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Carlos Acosta <cyacosta@google.com>
    Commit-Queue: yiwen qian <yiwenqian@chromium.org>
    Reviewed-by: Qihui Zhao <qihuizhao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1573827}

--- ios/web_view/internal/autofill/ios_web_view_payments_autofill_client.mm
+++ ios/web_view/internal/autofill/ios_web_view_payments_autofill_client.mm
@@ -309,7 +309,7 @@ IOSWebViewPaymentsAutofillClient::GetRiskBasedAuthenticator() {
 bool IOSWebViewPaymentsAutofillClient::IsRiskBasedAuthEffectivelyAvailable()
     const {
   return GetPrefService()->GetBoolean(
-      ios_web_view::kCWVAutofillVCNUsageEnabled);
+      ios_web_view::kRiskBasedAuthenticationEnabled);
 }
 
 bool IOSWebViewPaymentsAutofillClient::IsMandatoryReauthEnabled() {
