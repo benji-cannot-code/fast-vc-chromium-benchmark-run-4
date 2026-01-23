bb5667f869487e4f1afd4334d3cc6b58ce421a08
James Cook <jamescook@chromium.org>
2026-01-23 15:55:18 -0800

    extensions: Add ChromeExtensionFunctionUnitTest to desktop Android
    We now support the base class ExtensionServiceTestBase so we can add
    this test to the build. It passes without modifications.
    
    Bug: 469417243
    Change-Id: I230655e64ecf85ea5109f148cde4a2c0edf963c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7512002
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1574027}

--- chrome/browser/extensions/chrome_extension_function_unittest.cc
+++ chrome/browser/extensions/chrome_extension_function_unittest.cc
@@ -15,8 +15,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_function.h"
 #include "extensions/browser/extension_registrar.h"
 #include "extensions/browser/extension_registry.h"
+#include "extensions/buildflags/buildflags.h"
 #include "extensions/common/extension_builder.h"
 #include "testing/gtest/include/gtest/gtest.h"
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace extensions {
 
