e5b0bac254a3aac70a13f0ef76d20ac930056b58
Will Harris <wfh@chromium.org>
2026-01-23 14:22:48 -0800

    Remove V3 key feature as this has fully launched
    V3 key is now fully launched, so the code supporting this can be
    removed.
    
    This CL has no functionality change because `use_latest_encryption`
    already defaults to `true`, and all current callers were setting this to
    `true`.
    
    This is stage 2 of 3 of removing this code. The final stage will remove
    the flag entirely from `InternalFlags`.
    
    BUG=382234536
    
    Change-Id: I191e659eb238924415e07ceafb0272d3a538f93c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7513133
    Commit-Queue: Will Harris <wfh@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1573958}

--- chrome/elevation_service/elevator.h
+++ chrome/elevation_service/elevator.h
@@ -22,8 +22,7 @@ namespace elevation_service {
 // `protection_level` argument. Access these via the `EncryptAppBoundString` API
 // in chrome.
 struct EncryptFlags {
-  // Specify that the Encrypt operation should always use the latest key.
-  bool use_latest_key = false;
+  // Currently no flags are supported.
 };
 
 inline constexpr IID kTestElevatorClsid = {
@@ -42,10 +41,11 @@ inline constexpr char kFakeReencryptForTestingSwitch[] =
 
 namespace internal {
 
-inline constexpr uint32_t kFlagUseLatestKey = 1 << 23;
+// Deprecated flag. Do not reuse value.
+inline constexpr uint32_t kFlagUseLatestKeyDeprecated = 1 << 23;
 
 // Update this each time a new flag is added.
-inline constexpr uint32_t kMaxFlag = kFlagUseLatestKey;
+inline constexpr uint32_t kMaxFlag = kFlagUseLatestKeyDeprecated;
 
 // A static assert verifies the flags can always fit into 24 bits.
 static_assert((kMaxFlag & 0xFFFFFF) == kMaxFlag);
