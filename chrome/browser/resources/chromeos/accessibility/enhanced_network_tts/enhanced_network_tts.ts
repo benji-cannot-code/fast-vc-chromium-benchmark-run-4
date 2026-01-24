397a2d53e053b0adfee59ab1f394e7f8273c9d0a
Akihiro Ota <akihiroota@chromium.org>
2026-01-23 17:54:52 -0800

    Ensure enhanced network tts stays alive
    There is a bug, https://crbug.com/388867658, where Dictation will stop
    working if the service worker gets put to sleep. That bug is being
    fixed in another CL, but this CL ensures that the remaining extensions
    are kept alive so a similar bug doesn't occur elsewhere.
    
    Bug: 388867658
    Change-Id: Ie3443ba4842a5a6efae1e09197709011b892e079
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7506729
    Reviewed-by: Amanda Lin Dietz <aldietz@google.com>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1574085}

--- chrome/browser/resources/chromeos/accessibility/enhanced_network_tts/enhanced_network_tts.ts
+++ chrome/browser/resources/chromeos/accessibility/enhanced_network_tts/enhanced_network_tts.ts
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {KeepAlive} from '/common/keep_alive.js';
 import {TestImportManager} from '/common/testing/test_import_manager.js';
 
 import {OffscreenCommand, ServiceWorkerCommand} from './commands.js';
@@ -42,6 +43,7 @@ export class EnhancedNetworkTts {
   private offscreenReadyPromise_: Promise<void>|null = null;
 
   constructor() {
+    KeepAlive.init();
     this.processingQueue_ = [];
     this.api_ = null;
 
