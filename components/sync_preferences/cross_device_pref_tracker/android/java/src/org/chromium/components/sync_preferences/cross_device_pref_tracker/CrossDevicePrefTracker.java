8ac50d9f716cfee940db24f9efcd46c775db9353
Jenna Himawan <jhimawan@google.com>
2026-01-23 20:47:47 -0800

    Notify Java of state changes in CrossDevicePrefTracker
    Bug: 442902926
    Change-Id: I72464cfe04b878555e65b45fb7e8ad2f301358e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7514751
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1574120}

--- components/sync_preferences/cross_device_pref_tracker/android/java/src/org/chromium/components/sync_preferences/cross_device_pref_tracker/CrossDevicePrefTracker.java
+++ components/sync_preferences/cross_device_pref_tracker/android/java/src/org/chromium/components/sync_preferences/cross_device_pref_tracker/CrossDevicePrefTracker.java
@@ -25,7 +25,6 @@ import org.chromium.components.sync_device_info.OsType;
 public class CrossDevicePrefTracker {
 
     /** Observes cross-device preference changes. */
-    @FunctionalInterface
     public interface CrossDevicePrefTrackerObserver {
         /**
          * Called when a preference value from a remote device has changed.
@@ -41,6 +40,13 @@ public class CrossDevicePrefTracker {
                 TimestampedPrefValue timestampedPrefValue,
                 @OsType int osType,
                 @FormFactor int formFactor);
+
+        /**
+         * Called when the service status changes (e.g., the tracker becomes available to use).
+         *
+         * @param status The new service status.
+         */
+        void onServiceStatusChanged(@ServiceStatus int status);
     }
 
     private long mNativePtr;
@@ -76,6 +82,13 @@ public class CrossDevicePrefTracker {
             @FormFactor int formFactor) {
         for (CrossDevicePrefTrackerObserver observer : mObservers) {
             observer.onRemotePrefChanged(prefName, timestampedPrefValue, osType, formFactor);
+        }
+    }
+
+    @CalledByNative
+    private void onServiceStatusChanged(@ServiceStatus int status) {
+        for (CrossDevicePrefTrackerObserver observer : mObservers) {
+            observer.onServiceStatusChanged(status);
         }
     }
 
