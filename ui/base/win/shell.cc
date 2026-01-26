f38d6ec8b76a6c493478c89a682c0459d85e3eab
Robert Liao <robliao@chromium.org>
2026-01-26 10:36:28 -0800

    Manually Parse Paths to IDLists for ShellExecute
    ShellExecute may perform incorrect legacy resolution for paths
    that do not appear to have a valid extension (Example: file. ext).
    
    Extensions cannot have spaces like in "file. ext", which convinces
    ShellExecute to think that this is a legacy path like "chrome" that
    needs to resolve to "chrome.exe".
    
    ShellExecute will append one of the following extensions in an attempt
    to "find" the corrected path, even if the provided path already
    exists:
      - .pif
      - .com
      - .exe
      - .bat
      - .lnk
      - .cmd
    
    There doesn't appear to be a way to tell ShellExecute through
    SHELLEXECUTEINFO to avoid performing this legacy resolution other than
    having the caller pre-parse the path into an ID list and passing the
    ID list through SHELLEXECUTEINFO::lpIDList.
    
    Fixed: 476862276
    Bug: 478206473
    Change-Id: I6c9b1d7b66a61fc4ab84ab7d5296ab099d96aa2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7514332
    Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    Auto-Submit: Robert Liao <robliao@chromium.org>
    Reviewed-by: Jesse McKenna <jessemckenna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1574631}

--- ui/base/win/shell.cc
+++ ui/base/win/shell.cc
@@ -5,14 +5,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/base/win/shell.h"
 
-#include <dwmapi.h>
+// clang-format off
 #include <shlobj.h>  // Must be before propkey.
+// clang-format on
 
+#include <dwmapi.h>
 #include <propkey.h>
 #include <shellapi.h>
 #include <wrl/client.h>
 
 #include "base/debug/alias.h"
+#include "base/feature_list.h"
 #include "base/files/file.h"
 #include "base/files/file_path.h"
 #include "base/native_library.h"
@@ -21,12 +24,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/threading/scoped_blocking_call.h"
 #include "base/threading/scoped_thread_priority.h"
+#include "base/win/atl.h"
 #include "base/win/win_util.h"
 #include "ui/base/ui_base_switches.h"
 
 namespace ui::win {
 
 namespace {
+
+BASE_FEATURE(kManuallyParsePathForShellExecute,
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Default ShellExecuteEx flags used with "openas", "explore", and default
 // verbs.
@@ -47,7 +54,9 @@ bool InvokeShellExecute(const std::wstring& path,
   base::ScopedBlockingCall scoped_blocking_call(FROM_HERE,
                                                 base::BlockingType::WILL_BLOCK);
 
-  SHELLEXECUTEINFO sei = {sizeof(sei)};
+  SHELLEXECUTEINFO sei = {};
+  sei.cbSize = sizeof(sei);
+
   if (!class_name.empty()) {
     sei.lpClass = class_name.c_str();
     mask = (mask | SEE_MASK_CLASSNAME);
@@ -56,10 +65,27 @@ bool InvokeShellExecute(const std::wstring& path,
   sei.fMask = mask;
   sei.nShow = SW_SHOWNORMAL;
   sei.lpVerb = (verb.empty() ? nullptr : verb.c_str());
-  sei.lpFile = path.c_str();
   sei.lpDirectory =
       (working_directory.empty() ? nullptr : working_directory.c_str());
   sei.lpParameters = (args.empty() ? nullptr : args.c_str());
+
+  CComHeapPtr<ITEMIDLIST_ABSOLUTE> path_id_list;
+  if (base::FeatureList::IsEnabled(kManuallyParsePathForShellExecute)) {
+    // ShellExecute will perform legacy resolution of a path if it can't detect
+    // an extension from a given path, appending .pif, .com, .exe, .bat, .lnk,
+    // and .cmd with an assumption the file is a truncated invocable path
+    // (Example: "chrome" referring to "chrome.exe"). ShellExecute will perform
+    // this resolution even if the path refers to a valid file. Chromium
+    // expects paths to be fully qualified and does not need this resolution.
+    if (FAILED(::SHParseDisplayName(path.c_str(), nullptr, &path_id_list,
+                                    SFGAO_FILESYSTEM, nullptr))) {
+      return false;
+    }
+    sei.fMask |= SEE_MASK_IDLIST;
+    sei.lpIDList = path_id_list.m_pData;
+  } else {
+    sei.lpFile = path.c_str();
+  }
 
   // Mitigate the issues caused by loading DLLs on a background thread
   // (http://crbug/973868).
