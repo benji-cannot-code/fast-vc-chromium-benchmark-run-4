6cfd07b18cddfc4a9402fda9ade0a1054cd2cead
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2026-01-26 14:19:40 -0800

    Revert "Move PageEmbeddingsService to components"
    This reverts commit a57a097f32ceffc54a3cb006032b3b5e71430128.
    
    Reason for revert:
    LUCI Bisection has identified this change as the culprit of a build failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/compile-analysis/b/8691574947208937233
    
    Sample failed build: https://ci.chromium.org/b/8691574947208937233
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F7509881&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Fcompile-analysis%2Fb%2F8691574947208937233&type=BUG
    
    Original change's description:
    > Move PageEmbeddingsService to components
    >
    > This is a prerequisite for allowing PageEmbeddingsService to be used
    > by HistoryEmbeddingsService.
    >
    > Bug: 468035604
    > Change-Id: I1394443918a1056dd3c9c0f38cd34ea10515f7bd
    > Bypass-Check-License: moved files
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7509881
    > Auto-Submit: Mike Wittman <wittman@chromium.org>
    > Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    > Commit-Queue: Mike Wittman <wittman@chromium.org>
    > Reviewed-by: Steven Holte <holte@chromium.org>
    > Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    > Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1574771}
    >
    
    Bug: 468035604
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I11f29991ea76833d8934eac8492fe06071377737
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7517965
    Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1574777}

--- chrome/browser/contextual_tasks/contextual_tasks_context_service.h
+++ chrome/browser/contextual_tasks/contextual_tasks_context_service.h
@@ -16,8 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/tick_clock.h"
 #include "base/time/time.h"
 #include "chrome/browser/contextual_tasks/contextual_tasks_types.mojom.h"
+#include "chrome/browser/passage_embeddings/page_embeddings_service.h"
 #include "components/keyed_service/core/keyed_service.h"
-#include "components/passage_embeddings/content/page_embeddings_service.h"
 #include "components/passage_embeddings/core/passage_embeddings_types.h"
 
 class GURL;
