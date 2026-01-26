d55dd4b3537f53937224f84faea43ccf863b270f
Weidong Liu <weidongliu@chromium.org>
2026-01-26 02:51:23 -0800

    spanification: Remove DriverMemoryMapping::address* API.
    Bug: 393091624
    Change-Id: I7fbfea1f40d546e960f7852df149c120795dacff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7496851
    Commit-Queue: Weidong Liu <weidongliu@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1574419}

--- third_party/ipcz/src/ipcz/driver_memory_mapping.h
+++ third_party/ipcz/src/ipcz/driver_memory_mapping.h
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/393091624): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #ifndef IPCZ_SRC_IPCZ_DRIVER_MEMORY_MAPPING_H_
 #define IPCZ_SRC_IPCZ_DRIVER_MEMORY_MAPPING_H_
 
@@ -35,18 +30,6 @@ class DriverMemoryMapping {
   ~DriverMemoryMapping();
 
   bool is_valid() const { return mapping_ != IPCZ_INVALID_DRIVER_HANDLE; }
-
-  // Returns the base address of this mapping. Returns null if the mapping is
-  // invalid.
-  void* address() const { return address_; }
-
-  // Returns the address at `offset` bytes from this mapping's base address. It
-  // is an error to call this with an `offset` greater than or equal to the
-  // mapped region's size.
-  void* address_at(size_t offset) const {
-    ABSL_ASSERT(offset < size_);
-    return static_cast<uint8_t*>(address_) + offset;
-  }
 
   absl::Span<uint8_t> bytes() const {
     return {static_cast<uint8_t*>(address_), size_};
