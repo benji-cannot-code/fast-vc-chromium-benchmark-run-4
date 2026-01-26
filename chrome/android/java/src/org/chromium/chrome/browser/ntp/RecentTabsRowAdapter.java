a0efc5392c43614a72e5bcc42de011e89cc084ea
Calder Kitagawa <ckitagawa@chromium.org>
2026-01-26 10:21:45 -0800

    [Recent Tabs] Show full URL for about and chrome pages
    These URLs don't return a domain so appear as blank. This causes some
    visual issues. Fix this by returning the full URL in these cases.
    
    Fixed: 478240573
    Change-Id: I6afb49441e3595b1986f955600d6bcc2380d3fc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7516945
    Commit-Queue: Zhe Li <zheliooo@google.com>
    Reviewed-by: Zhe Li <zheliooo@google.com>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1574621}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/RecentTabsRowAdapter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/RecentTabsRowAdapter.java
@@ -44,9 +44,12 @@ import org.chromium.chrome.browser.ui.favicon.FaviconUtils;
 import org.chromium.components.browser_ui.styles.SemanticColorUtils;
 import org.chromium.components.browser_ui.widget.HoverHighlightViewListener;
 import org.chromium.components.browser_ui.widget.RoundedIconGenerator;
+import org.chromium.components.embedder_support.util.UrlConstants;
 import org.chromium.components.embedder_support.util.UrlUtilities;
 import org.chromium.components.tab_groups.TabGroupColorId;
 import org.chromium.components.tab_groups.TabGroupColorPickerUtils;
+import org.chromium.components.url_formatter.UrlFormatter;
+import org.chromium.content_public.common.ContentUrlConstants;
 import org.chromium.ui.UiUtils;
 import org.chromium.ui.base.DeviceFormFactor;
 import org.chromium.ui.mojom.WindowOpenDisposition;
@@ -317,7 +320,7 @@ public class RecentTabsRowAdapter extends BaseExpandableListAdapter {
             String url = sessionTab.url.getSpec();
             String text = TextUtils.isEmpty(sessionTab.title) ? url : sessionTab.title;
             viewHolder.textView.setText(text);
-            String domain = UrlUtilities.getDomainAndRegistry(url, false);
+            String domain = formatUrlForDisplay(sessionTab.url);
             if (!TextUtils.isEmpty(domain)) {
                 viewHolder.domainView.setText(domain);
                 viewHolder.domainView.setVisibility(View.VISIBLE);
@@ -628,7 +631,7 @@ public class RecentTabsRowAdapter extends BaseExpandableListAdapter {
                 List<RecentlyClosedTab> tabList) {
             List<String> domainList = new ArrayList<>();
             for (RecentlyClosedTab tab : tabList) {
-                String domain = UrlUtilities.getDomainAndRegistry(tab.getUrl().getSpec(), false);
+                String domain = formatUrlForDisplay(tab.getUrl());
                 domainList.add(domain);
             }
             String domainText =
@@ -680,8 +683,7 @@ public class RecentTabsRowAdapter extends BaseExpandableListAdapter {
             RecentlyClosedEntry entry = assumeNonNull(getChild(childPosition));
             if (entry instanceof RecentlyClosedWindow recentlyClosedWindow) {
                 viewHolder.textView.setText(recentlyClosedWindow.getTitle());
-                String activeTabDomain =
-                        UrlUtilities.getDomainAndRegistry(recentlyClosedWindow.getUrl(), false);
+                String activeTabDomain = formatUrlForDisplay(recentlyClosedWindow.getUrl());
                 String activeTabInfo =
                         TextUtils.isEmpty(activeTabDomain)
                                 ? recentlyClosedWindow.getActiveTabTitle()
@@ -738,7 +740,7 @@ public class RecentTabsRowAdapter extends BaseExpandableListAdapter {
                 String title = TitleUtil.getTitleForDisplay(tab.getTitle(), tab.getUrl());
                 viewHolder.textView.setText(title);
 
-                String domain = UrlUtilities.getDomainAndRegistry(tab.getUrl().getSpec(), false);
+                String domain = formatUrlForDisplay(tab.getUrl());
                 if (!TextUtils.isEmpty(domain)) {
                     viewHolder.domainView.setText(domain);
                     viewHolder.domainView.setVisibility(View.VISIBLE);
@@ -907,6 +909,29 @@ public class RecentTabsRowAdapter extends BaseExpandableListAdapter {
                 "HistoryPage.OtherDevicesMenu",
                 OtherSessionsActions.MENU_INITIALIZED,
                 OtherSessionsActions.NUM_ENTRIES);
+    }
+
+    /**
+     * Formats a URL for display. For most URLs, this is just the domain and registry. For about and
+     * chrome scheme URLs, the entire URL is returned.
+     *
+     * @param gurl The URL to format.
+     * @return The formatted URL or null if the domain could not be extracted.
+     */
+    private @Nullable String formatUrlForDisplay(GURL gurl) {
+        String urlSpec = gurl.getSpec();
+        String scheme = gurl.getScheme();
+        if (ContentUrlConstants.ABOUT_SCHEME.equals(scheme)
+                || UrlConstants.CHROME_SCHEME.equals(scheme)
+                || UrlConstants.CHROME_NATIVE_SCHEME.equals(scheme)) {
+            return UrlFormatter.formatUrlForDisplayOmitHTTPScheme(urlSpec);
+        }
+        // This should perhaps use UrlFormatter as well, but it has used domain for a long time.
+        return UrlUtilities.getDomainAndRegistry(urlSpec, false);
+    }
+
+    private @Nullable String formatUrlForDisplay(String urlSpec) {
+        return formatUrlForDisplay(new GURL(urlSpec));
     }
 
     /**
