aa23145af205ce7648150150a86effa70b0acca8
Henrique Nakashima <hnakashima@chromium.org>
2026-01-26 14:24:26 -0800

    [Android] Migrate Android Desktop .filters to @DisableIf (part 19/20)
    Migrated:
    - TabModelMultiWindowTest
      - testMoveTabGroupToWindow
    - TabGridDialogTest
      - *
    - TabListContainerViewBinderTest
      - testSetInitialScrollIndex_Grid
    - TabSwitcherLayoutPTTest
      - testRenderGrid_Incognito
      - testRenderGrid_TabGroupColor_Incognito_2TabsInGroup
      - testRenderGrid_TabGroupColor_Incognito_5TabsInGroup
      - testUrlUpdatedNotCrashing_ForTabNotInCurrentModel
    - TabSwitcherPanePublicTransitTest
      - *
    
    Include-Ci-Only-Tests: chromium.android.desktop:android-desktop-x64-rel-15-tests|chrome_public_test_apk
    Validate-Test-Flakiness: skip
    Bug: 459810720, 446934111, 394671175, 376527109
    Change-Id: I18a682c793253d79e1ba71a9417851eb1bea94b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7513618
    Commit-Queue: Sky Malice <skym@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Auto-Submit: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1574785}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/TabModelMultiWindowTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/TabModelMultiWindowTest.java
@@ -38,6 +38,7 @@ import org.chromium.base.test.util.ApplicationTestUtils;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.ImportantFormFactors;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
@@ -122,6 +123,7 @@ public class TabModelMultiWindowTest {
     }
 
     @Test
+    @DisableIf.Device(DeviceFormFactor.DESKTOP) // https://crbug.com/446934111
     @LargeTest
     public void testMoveTabGroupToWindow() {
         ChromeTabbedActivity activity1 = mActivityTestRule.getActivity();
