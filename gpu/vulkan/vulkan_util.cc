3373db92d329a8179820ff81d61383423dfc21c2
kylechar <kylechar@chromium.org>
2026-01-26 13:53:50 -0800

    Ensure PowerVR B-Series blocked from Vulkan
    Something changed and older Imagination PowerVR B-Series GPUs are now
    using Vulkan when they weren't in the past. Our guess is the Vulkan
    deviceName field changed to something like "PowerVR B-Series BXM-8-256"
    after a driver update which matches the existing pattern. Update the
    pattern to a regex that excludes B-Series GPU names.
    
    Bug: 477978882
    Change-Id: If5522d1db2ac246ab96087aa6b2934d5509d4232
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7517030
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1574762}

--- gpu/vulkan/vulkan_util.cc
+++ gpu/vulkan/vulkan_util.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/vulkan/vulkan_function_pointers.h"
 #include "gpu/vulkan/vulkan_info.h"
 #include "third_party/abseil-cpp/absl/cleanup/cleanup.h"
+#include "third_party/re2/src/re2/re2.h"
 #include "ui/gl/gl_switches.h"
 
 #if BUILDFLAG(IS_ANDROID)
@@ -222,11 +223,6 @@ bool IsVulkanV2Enabled(const GPUInfo& gpu_info,
 
 bool ShouldBypassMediatekBlock(const GPUInfo& gpu_info) {
   return IsVulkanV2Enabled(gpu_info, "Mediatek");
-}
-
-bool IsVulkanV2EnabledForImagination(const GPUInfo& gpu_info) {
-  // Imagination shows regression even with 2022 deQP tests.
-  return false;
 }
 
 // Everything except MediaTek.
@@ -539,14 +535,11 @@ bool CheckVulkanCompatibilities(
            IsVulkanV3EnabledForAdreno(gpu_info, device_properties);
   }
 
-  // https://crbug.com/1122650: Poor performance and untriaged crashes with
-  // Imagination GPUs.
   if (device_properties.vendor_id == kVendorImagination) {
-    // Only PowerVR D series and newer allowed in V1.
-    if (base::MatchPattern(device_properties.device_name, "PowerVR ?-Series*")) {
-      return true;
-    }
-    return IsVulkanV2EnabledForImagination(gpu_info);
+    // Only newer PowerVR GPU series allowed. Older PowerVR GPUs showed poor
+    // performance and stability problems, see https://crbug.com/1122650.
+    return RE2::FullMatch(device_properties.device_name,
+                          "PowerVR ([CDE]-Series)? [CDE]X.*");
   }
 
   // Some devices implement Vulkan using Swiftshader. We do not want those,
