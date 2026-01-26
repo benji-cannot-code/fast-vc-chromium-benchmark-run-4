d6904ff89e0bf5934554f67500fbe8b4a0fd8984
Antonio Sartori <antoniosartori@chromium.org>
2026-01-26 03:20:00 -0800

    [permissions] Use GeolocationContentSettingsType in permission_util
    This is a small refactoring that simplifies permission_util.cc using
    the newly added GeolocationContentSettingsType().
    
    Bug: 476929911
    Change-Id: I2199e4a3bef430485fd726feebf9b2d12e7da3eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7510847
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1574426}

--- components/permissions/permission_util.cc
+++ components/permissions/permission_util.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "components/content_settings/core/common/content_settings.h"
 #include "components/content_settings/core/common/content_settings_types.h"
+#include "components/content_settings/core/common/content_settings_utils.h"
 #include "components/content_settings/core/common/features.h"
 #include "components/permissions/features.h"
 #include "components/permissions/permission_request.h"
@@ -476,10 +477,7 @@ ContentSettingsType PermissionUtil::PermissionTypeToContentSettingsTypeSafe(
       return ContentSettingsType::NOTIFICATIONS;
     case PermissionType::GEOLOCATION:
     case PermissionType::GEOLOCATION_APPROXIMATE:
-      return base::FeatureList::IsEnabled(
-                 content_settings::features::kApproximateGeolocationPermission)
-                 ? ContentSettingsType::GEOLOCATION_WITH_OPTIONS
-                 : ContentSettingsType::GEOLOCATION;
+      return content_settings::GeolocationContentSettingsType();
     case PermissionType::PROTECTED_MEDIA_IDENTIFIER:
 #if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_WIN) || \
     BUILDFLAG(IS_FUCHSIA)
@@ -730,12 +728,7 @@ bool PermissionUtil::DoesPlatformSupportChip() {
 
 // static
 ContentSettingsType PermissionUtil::GetGeolocationType() {
-  if (base::FeatureList::IsEnabled(
-          content_settings::features::kApproximateGeolocationPermission)) {
-    return ContentSettingsType::GEOLOCATION_WITH_OPTIONS;
-  } else {
-    return ContentSettingsType::GEOLOCATION;
-  }
+  return content_settings::GeolocationContentSettingsType();
 }
 
 }  // namespace permissions
