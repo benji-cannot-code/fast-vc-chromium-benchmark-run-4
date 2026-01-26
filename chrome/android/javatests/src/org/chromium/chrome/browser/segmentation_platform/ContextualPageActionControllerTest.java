7e3a74c0f245842086f5d1f0f46777925f485168
Anna Tsvirchkova <atsvirchkova@google.com>
2026-01-26 00:52:30 -0800

    [Gardener] Disable flaky ContextualPageActionControllerTest
    The test seems to be not flaky for x86 and arm bots.
    
    Bug: 448846307
    Change-Id: Iffe862107e6063e8d6072d03f46885d218731bb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7514105
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Owners-Override: Anna Tsvirchkova <atsvirchkova@google.com>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Auto-Submit: Anna Tsvirchkova <atsvirchkova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1574364}

--- chrome/android/javatests/src/org/chromium/chrome/browser/segmentation_platform/ContextualPageActionControllerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/segmentation_platform/ContextualPageActionControllerTest.java
@@ -15,6 +15,7 @@ import org.junit.runner.RunWith;
 import org.chromium.base.library_loader.LibraryLoader;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.HistogramWatcher;
@@ -54,6 +55,7 @@ public class ContextualPageActionControllerTest {
     @Test
     @MediumTest
     @Restriction(DeviceFormFactor.PHONE) // Flaky on larger form factors crbug.com/422817837
+    @DisableIf.Build(supported_abis_includes = "x86_64", message = "https://crbug.com/448846307")
     public void testContextualPageModelExecution() {
         LibraryLoader.getInstance().ensureInitialized();
 
