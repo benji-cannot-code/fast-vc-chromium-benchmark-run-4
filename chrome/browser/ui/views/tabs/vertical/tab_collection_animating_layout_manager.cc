ea78a65757df6ed7da1134a1c1541b83b821f082
Alison Gale <agale@chromium.org>
2026-01-26 14:33:59 -0800

    Revert "Reland "[Vertical Tabs] Fix tab view corner rendering""
    This reverts commit 94652a18f42d336c3217e542cf61df498bf208f9.
    
    Reason for revert: breaking some cross container move animations
    Original change's description:
    > Reland "[Vertical Tabs] Fix tab view corner rendering"
    >
    > This reverts commit aeaebeebc69b0d4dfa60d0645eeac5af058de16f.
    >
    > Reason for revert: Reland with fixes (tab dragging is not painting to an
    > opaque layer and updating animations to not draw the animating layer)
    >
    > Tested on Mac and Linux
    >
    > Original change's description:
    > > Revert "[Vertical Tabs] Fix tab view corner rendering"
    > >
    > > This reverts commit 005f6c6c0e6db5cfcda204cd1aa5d71c786589dc.
    > >
    > > Reason for revert: Causing issues with drag and group rendering
    > >
    > > Original change's description:
    > > > [Vertical Tabs] Fix tab view corner rendering
    > > >
    > > > The border is rendered half inside and half outside the bounds so use a
    > > > rounded rect when painting the tabs with the correct corner radius
    > > >
    > > > Bug: 475597197
    > > > Change-Id: Ie02991db40408af0409bb259994d8b48d22ac161
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7467499
    > > > Commit-Queue: Alison Gale <agale@chromium.org>
    > > > Reviewed-by: Charles Meng <charlesmeng@chromium.org>
    > > > Cr-Commit-Position: refs/heads/main@{#1569495}
    > >
    > > Bug: 475597197
    > > No-Presubmit: true
    > > No-Tree-Checks: true
    > > No-Try: true
    > > Change-Id: I001ba3a82b9fbae0fbeb4f1dbd41bd037837cbb0
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7487653
    > > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > > Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    > > Commit-Queue: Alison Gale <agale@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1569927}
    >
    > Bug: 475597197
    > Change-Id: Ida00bf1ee768d04bce7084801533a1923ca4d575
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7487263
    > Reviewed-by: Kaan Alsan <alsan@chromium.org>
    > Commit-Queue: Alison Gale <agale@chromium.org>
    > Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1574635}
    
    Bug: 475597197
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I5c055838f5b13ff4e2748cb31fa981d7d28a5332
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7518268
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1574801}

--- chrome/browser/ui/views/tabs/vertical/tab_collection_animating_layout_manager.cc
+++ chrome/browser/ui/views/tabs/vertical/tab_collection_animating_layout_manager.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_map.h"
 #include "base/containers/flat_set.h"
 #include "ui/base/class_property.h"
-#include "ui/compositor/layer_type.h"
 #include "ui/gfx/animation/animation.h"
 #include "ui/gfx/animation/tween.h"
 #include "ui/views/view.h"
@@ -262,7 +261,7 @@ void TabCollectionAnimatingLayoutManager::AnimateAndReparentView(
     const gfx::Rect& previous_bounds_in_screen) {
   auto* child_view = host_view()->AddChildView(std::move(view_to_reparent));
   if (!delegate_ || !delegate_->IsViewDragging(*child_view)) {
-    child_view->SetPaintToLayer(ui::LAYER_NOT_DRAWN);
+    child_view->SetPaintToLayer();
     child_view->SetProperty(kPreviousCollectionBounds,
                             previous_bounds_in_screen);
   }
--- chrome/browser/ui/views/tabs/vertical/vertical_tab_view.cc
+++ chrome/browser/ui/views/tabs/vertical/vertical_tab_view.cc
@@ -358,10 +358,7 @@ void VerticalTabView::OnPaint(gfx::Canvas* canvas) {
     cc::PaintFlags flags;
     flags.setAntiAlias(true);
     flags.setColor(background_color.value());
-    const float corner_radius =
-        GetLayoutConstant(LayoutConstant::kVerticalTabCornerRadius) -
-        (split_ ? GetInsets().top() / 2.0 : 0.0);
-    canvas->DrawRoundRect(GetContentsBounds(), corner_radius, flags);
+    canvas->DrawRect(GetContentsBounds(), flags);
   }
 
   views::View::OnPaint(canvas);
@@ -387,6 +384,8 @@ void VerticalTabView::RemovedFromWidget() {
 
 void VerticalTabView::OnBoundsChanged(const gfx::Rect& previous_bounds) {
   collapsed_ = width() < VerticalTabStripRegionView::kCollapsedWidth;
+
+  SetClipPath(GetPath());
 }
 
 void VerticalTabView::OnThemeChanged() {
@@ -692,7 +691,8 @@ float VerticalTabView::GetHoverOpacity() const {
 
 SkPath VerticalTabView::GetPath() const {
   const SkScalar corner_radius = SkIntToScalar(
-      GetLayoutConstant(LayoutConstant::kVerticalTabCornerRadius));
+      GetLayoutConstant(LayoutConstant::kVerticalTabCornerRadius) +
+      (split_ ? GetInsets().height() : 0));
   return SkPath::RRect(SkRRect::MakeRectXY(gfx::RectToSkRect(GetLocalBounds()),
                                            corner_radius, corner_radius));
 }
