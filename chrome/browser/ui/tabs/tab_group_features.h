d7067cf7704fc3d6643e23ed3c56991137682cef
Caroline Rising <corising@chromium.org>
2026-01-26 08:47:18 -0800

    Reland "Reland "[Vertical tabs] Add save tab groups related icons to group header.""
    Was reverted due to conflicting cls landing at the same time.
    
    This is a reland of commit 69dad14515c7bd5163ba1fd1ca6ae2418f4e8097
    
    Original change's description:
    > Reland "[Vertical tabs] Add save tab groups related icons to group header."
    >
    > Added necessary feature flag to tests which should fix revert reason.
    >
    > This is a reland of commit a5de14ef1c7c109aa1e08106fde918e9ef3a7e04
    >
    > Original change's description:
    > > [Vertical tabs] Add save tab groups related icons to group header.
    > >
    > > Bug: 465164420
    > > Change-Id: I68ddfdeb890067f87e5f995e5304dc0b11e23b58
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7499153
    > > Reviewed-by: David Pennington <dpenning@chromium.org>
    > > Commit-Queue: Caroline Rising <corising@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1572350}
    >
    > Bug: 465164420
    > Change-Id: I1aa89da1fadd32f3c1c20b3234f7ed4616ac4436
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7512187
    > Reviewed-by: David Pennington <dpenning@chromium.org>
    > Commit-Queue: Caroline Rising <corising@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1573900}
    
    Bug: 465164420
    Change-Id: I3f0fac786f9a698a5bc10fc747de64779e62446a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7514843
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1574570}

--- chrome/browser/ui/tabs/tab_group_features.h
+++ chrome/browser/ui/tabs/tab_group_features.h
@@ -38,6 +38,9 @@ class TabGroupFeatures {
   TabGroupAttentionIndicator* attention_indicator() {
     return attention_indicator_.get();
   }
+  TabGroupAttentionIndicator* attention_indicator() const {
+    return attention_indicator_.get();
+  }
 
  protected:
   TabGroupFeatures();
