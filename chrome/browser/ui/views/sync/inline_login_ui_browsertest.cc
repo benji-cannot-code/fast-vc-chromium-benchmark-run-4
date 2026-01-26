79dfe4fb9cf95703994b0f6b094565dbf0d22c36
Ryan Sultanem <rsult@google.com>
2026-01-26 03:03:27 -0800

    [ForceSignin] Cleanup: Remove OneClickSignin logic
    OneClickSignin code last usage was in InlineLoginHandlerImpl, which
    was cleaned up in this change: http://crrev.com/c/7511330.
    
    Bug: 381231566
    Change-Id: I19242132099990f8431a105ff591dfbfbf77b5cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7510829
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1574420}

--- chrome/browser/ui/views/sync/inline_login_ui_browsertest.cc
+++ chrome/browser/ui/views/sync/inline_login_ui_browsertest.cc
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/test/test_browser_dialog.h"
-#include "chrome/browser/ui/views/sync/one_click_signin_dialog_view.h"
 #include "chrome/browser/ui/webui/signin/inline_login_handler_impl.h"
 #include "chrome/browser/ui/webui/signin/login_ui_service.h"
 #include "chrome/browser/ui/webui/signin/login_ui_service_factory.h"
