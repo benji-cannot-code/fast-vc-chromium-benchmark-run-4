c5aa8a396fba695ea69ac329577f9802db1b6ddf
Abe Boujane <boujane@google.com>
2026-01-26 11:51:09 -0800

    Remove Privacy Sandbox UI and cleanup dependencies
    This CL removes the Privacy Sandbox UI components and their associated
    logic, including:
    - Deleting `chrome/browser/ui/privacy_sandbox/` and
      `chrome/browser/ui/views/privacy_sandbox/` directories.
    - Removing `PrivacySandboxPromptHelper` and its usage in `TabHelpers`.
    - Updating build files (`chrome/browser/ui/BUILD.gn`,
      `chrome/browser/BUILD.gn`, `chrome/test/BUILD.gn`, etc.) to remove
      dependencies on the deleted directories and files.
    - Cleaning up lingering references and stale TODOs related to the
      removed components.
    
    This is part of a gradual removal of the feature.
    
    This CL also removes the associated histograms and enums:
    - Settings.PrivacySandbox.DialogDisplayHost
    - Settings.PrivacySandbox.PromptHelperEvent2
    - PrivacySandbox.Notice.NotHoldingHandle.InQueue
    - PrivacySandbox.Notice.NotHoldingHandle.NotInQueue
    
    OBSOLETE_HISTOGRAM[Settings.PrivacySandbox.DialogDisplayHost]=Removed in M145 as part of Privacy Sandbox UI cleanup.
    OBSOLETE_HISTOGRAM[Settings.PrivacySandbox.PromptHelperEvent2]=Removed in M145 as part of Privacy Sandbox UI cleanup.
    OBSOLETE_HISTOGRAM[PrivacySandbox.Notice.NotHoldingHandle.InQueue]=Removed in M145 as part of Privacy Sandbox UI cleanup.
    OBSOLETE_HISTOGRAM[PrivacySandbox.Notice.NotHoldingHandle.NotInQueue]=Removed in M145 as part of Privacy Sandbox UI cleanup.
    
    Bug: 474716334
    Change-Id: I4e3554fb7f50d2c31f28fc92c98e183df59f60e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7433478
    Commit-Queue: Abe Boujane <boujane@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1574682}

--- chrome/browser/privacy_sandbox/privacy_sandbox_queue_manager.cc
+++ chrome/browser/privacy_sandbox/privacy_sandbox_queue_manager.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "chrome/browser/privacy_sandbox/privacy_sandbox_service.h"
 #include "chrome/browser/privacy_sandbox/privacy_sandbox_service_factory.h"
-#include "chrome/browser/ui/privacy_sandbox/privacy_sandbox_prompt_helper.h"
 #include "chrome/browser/user_education/user_education_service.h"
 #include "chrome/browser/user_education/user_education_service_factory.h"
 #include "components/user_education/common/product_messaging_controller.h"
--- chrome/browser/privacy_sandbox/privacy_sandbox_queue_manager.h
+++ chrome/browser/privacy_sandbox/privacy_sandbox_queue_manager.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_PRIVACY_SANDBOX_PRIVACY_SANDBOX_QUEUE_MANAGER_H_
 
 #include "base/memory/raw_ptr.h"
-#include "chrome/browser/ui/privacy_sandbox/privacy_sandbox_prompt_helper.h"
 #include "chrome/browser/user_education/user_education_service.h"
 #include "chrome/browser/user_education/user_education_service_factory.h"
 #include "components/user_education/common/product_messaging_controller.h"
