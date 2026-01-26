3994f16c3f799b362c3ac8f09671f83bcc84f00d
Michael Tatarski <mtatarski@google.com>
2026-01-26 01:37:03 -0800

    Enable kMigrateAccountPrefs by default on Android/iOS
    This CL enables the kMigrateAccountPrefs feature by default. This
    feature migrates account-scoped preferences to a single JSON file
    implementation.
    
    The Finch experiment for this feature showed positive results, and data
    from the 1% stable rollout also looked good. As noted in
    crbug.com/460706028, the migration led to an unexpected but significant
    increase in user enablement of Enhanced Safe Browsing (ESB). It is
    hypothesized that the preference migration process triggered a mismatch
    detection and aligned the account wide ESB value with the Chrome ESB
    value.
    
    Bug: 460706028, 346508597
    Change-Id: I9d179407385d6910f9208c3236cf1b1152e08582
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7514616
    Auto-Submit: Michael Tatarski <mtatarski@google.com>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Reviewed-by: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1574380}

--- ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
+++ ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
@@ -628,9 +628,6 @@ extern const char kMeasurementsDescription[];
 extern const char kMetrickitNonCrashReportName[];
 extern const char kMetrickitNonCrashReportDescription[];
 
-extern const char kMigrateAccountPrefsOnMobileName[];
-extern const char kMigrateAccountPrefsOnMobileDescription[];
-
 extern const char kMigrateIOSKeychainAccessibilityName[];
 extern const char kMigrateIOSKeychainAccessibilityDescription[];
 
