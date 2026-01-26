9457c9281e4373ac6088994e945368160617865c
Fiaz Muhammad <mfiaz@google.com>
2026-01-26 07:53:14 -0800

    [Persist] Only raze once across all tab state store instances
    - Currently, we raze the DB for every single TabStateStore open.
    - By sharing raze state data, we avoid this.
    - This should be okay, since TabModel-related operations are performed
            on the UI thread.
    
    Bug: 475252643
    Change-Id: Ifac904b3921498b92ba0fdceb4b2c6b89fac19d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7512206
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Fiaz Muhammad <mfiaz@google.com>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1574544}

--- chrome/android/java/src/org/chromium/chrome/browser/app/tabmodel/TabPersistentStoreFactory.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/tabmodel/TabPersistentStoreFactory.java
@@ -28,6 +28,23 @@ import org.chromium.chrome.browser.tabwindow.TabWindowManager;
  */
 @NullMarked
 public class TabPersistentStoreFactory {
+    /** Data shared between stores. */
+    public static class SharedStoreData {
+        private boolean mWasStoreRazed;
+
+        /** Whether a store has razed its data during startup. */
+        public boolean wasStoreRazed() {
+            return mWasStoreRazed;
+        }
+
+        /** Called when a store has razed its data. */
+        public void onStoreRazed() {
+            mWasStoreRazed = true;
+        }
+    }
+
+    private static final SharedStoreData sSharedData = new SharedStoreData();
+
     /**
      * Builds an authoritative {@link TabPersistentStore}.
      *
@@ -167,6 +184,7 @@ public class TabPersistentStoreFactory {
 
         TabPersistentStore shadowTabPersistentStore =
                 new TabStateStore(
+                        sSharedData,
                         service,
                         selector,
                         windowTag,
