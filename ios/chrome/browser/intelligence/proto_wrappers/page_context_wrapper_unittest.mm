69f7690a44ca4e84e337fc2720e0713a731ac4f4
Sylvain Defresne <sdefresne@chromium.org>
2026-01-26 03:14:47 -0800

    [ios] Remove incorrect use of FakeBrowserState
    Code in //ios/chrome must not use FakeBrowserState, but instead must
    use TestProfileIOS, so fix incorrect usage.
    
    Bug: none
    Change-Id: Iadadc6b8479d7cd459147d245e6591c67ca3083f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7510495
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1574423}

--- ios/chrome/browser/intelligence/proto_wrappers/page_context_wrapper_unittest.mm
+++ ios/chrome/browser/intelligence/proto_wrappers/page_context_wrapper_unittest.mm
@@ -52,7 +52,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/public/js_messaging/content_world.h"
 #import "ios/web/public/js_messaging/java_script_feature.h"
 #import "ios/web/public/js_messaging/web_frames_manager.h"
-#import "ios/web/public/test/fakes/fake_browser_state.h"
 #import "ios/web/public/test/fakes/fake_web_client.h"
 #import "ios/web/public/test/fakes/fake_web_frame.h"
 #import "ios/web/public/test/fakes/fake_web_frames_manager.h"
@@ -173,10 +172,9 @@ class PageContextWrapperTest : public PlatformTest,
     });
 
     // Set the fake env used for testing errors.
-    fake_browser_state_ = std::make_unique<web::FakeBrowserState>();
+    profile2_ = TestProfileIOS::Builder().Build();
     web::test::OverrideJavaScriptFeatures(
-        fake_browser_state_.get(),
-        {web::FindInPageJavaScriptFeature::GetInstance()});
+        profile2_.get(), {web::FindInPageJavaScriptFeature::GetInstance()});
     fake_web_state_ = std::make_unique<FakeWebStateForFailureTest>();
 
     // Set fake web frames managers for scenarios to simulate a state where
@@ -263,9 +261,7 @@ class PageContextWrapperTest : public PlatformTest,
   }
 
   // Getters for fake env.
-  web::FakeBrowserState* fake_browser_state() {
-    return fake_browser_state_.get();
-  }
+  TestProfileIOS* profile2() { return profile2_.get(); }
   web::FakeWebState* fake_web_state() { return fake_web_state_.get(); }
 
   web::WebTaskEnvironment task_environment_;
@@ -281,7 +277,7 @@ class PageContextWrapperTest : public PlatformTest,
   net::EmbeddedTestServer xorigin_test_server_c_;
   id<SnapshotStorage> snapshot_storage_ = nil;
   ControllableFakeSnapshotGeneratorDelegate* snapshot_delegate_ = nil;
-  std::unique_ptr<web::FakeBrowserState> fake_browser_state_;
+  std::unique_ptr<TestProfileIOS> profile2_;
   std::unique_ptr<web::FakeWebState> fake_web_state_;
   std::unique_ptr<PageContext> page_helper_;
 };
