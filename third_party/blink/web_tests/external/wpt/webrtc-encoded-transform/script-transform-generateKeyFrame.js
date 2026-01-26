41c66e4612999728a67fa444e3b7c4207860b5a8
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2026-01-26 09:08:29 -0800

    Import wpt@ec74332812c570a84a34c2562d9cce0856a27da3
    https://github.com/web-platform-tests/wpt/compare/6a06f4cf1...ec7433281
    
    Using wpt-import in Chromium 0a642fb59600a53d8f44dee09e0843ebb40c1d5c.
    With Chromium commits locally applied on WPT:
      61ebf1c857 "PointerEvent: move .fromElement/.toElement checks to [...]
      d3ccc328ba "WebXR Layers: Avoid openxr failure due to too many layers"
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac-skia-alt-arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac12.0-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac12.0.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac13-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac13.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac14-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac14.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac15-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac15.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:win10.20h2-blink-rel
    Cq-Include-Trybots: luci.chromium.try:win11-arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:win11-blink-rel
    Change-Id: I7878ca9df258fc762c4098b1e1076cb5a67be320
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7515531
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1574580}

--- third_party/blink/web_tests/external/wpt/webrtc-encoded-transform/script-transform-generateKeyFrame.js
+++ third_party/blink/web_tests/external/wpt/webrtc-encoded-transform/script-transform-generateKeyFrame.js
@@ -34,7 +34,7 @@ onrtctransform = event => {
 
   async function generateKeyFrame(rid) {
     try {
-      const timestamp = await Promise.race([transformer.generateKeyFrame(rid), rejectInMs(8000)]);
+      const timestamp = await Promise.race([transformer.generateKeyFrame(rid), rejectInMs(16000)]);
       transformer.options.port.postMessage({result: 'success', value: timestamp, count: keyFrameCount});
     } catch (e) {
       // TODO: This does not work if we send e.name, why?
@@ -54,7 +54,7 @@ onrtctransform = event => {
 
   async function waitForFrame() {
     try {
-      await Promise.race([new Promise(r => gotFrameCallback = r), rejectInMs(8000)]);
+      await Promise.race([new Promise(r => gotFrameCallback = r), rejectInMs(16000)]);
       transformer.options.port.postMessage('got frame');
     } catch (e) {
       // TODO: This does not work if we send e.name, why?
