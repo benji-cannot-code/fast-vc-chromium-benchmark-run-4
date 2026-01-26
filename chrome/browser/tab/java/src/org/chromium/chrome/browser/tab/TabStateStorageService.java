825cf112e79e7f12876d7841fd307b4b72ae1261
Fiaz Muhammad <mfiaz@google.com>
2026-01-26 13:43:34 -0800

    [Persist] Fix scoping for shared data
    - Don't make the SharedData fully static, scope to a
    	TabStateStorageService.
    
    Bug: 475252643
    Change-Id: I3beb2f7bca1e78ea0c38cb729a6cccfe56733701
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7512146
    Auto-Submit: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1574755}

--- chrome/browser/tab/java/src/org/chromium/chrome/browser/tab/TabStateStorageService.java
+++ chrome/browser/tab/java/src/org/chromium/chrome/browser/tab/TabStateStorageService.java
@@ -36,6 +36,22 @@ public class TabStateStorageService {
         }
     }
 
+    /** Data shared between stores. */
+    public static class SharedStoreData {
+        private boolean mWasStoreRazed;
+
+        /** Whether a store has razed its data during startup. */
+        public boolean wasStoreRazed() {
+            return mWasStoreRazed;
+        }
+
+        /** Called when a store has razed its data. */
+        public void onStoreRazed() {
+            mWasStoreRazed = true;
+        }
+    }
+
+    private final SharedStoreData mSharedStoreData = new SharedStoreData();
     private final long mNativeTabStateStorageService;
 
     private TabStateStorageService(long nativeTabStateStorageService) {
@@ -159,6 +175,11 @@ public class TabStateStorageService {
     public byte[] generateKey(String windowTag) {
         return TabStateStorageServiceJni.get()
                 .generateKey(mNativeTabStateStorageService, windowTag);
+    }
+
+    /** Returns the data shared between all clients of this service. */
+    public SharedStoreData getSharedStoreData() {
+        return mSharedStoreData;
     }
 
     @NativeMethods
