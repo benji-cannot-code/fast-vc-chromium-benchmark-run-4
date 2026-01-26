f57ffade57e2e3cb70d783d4e16ce5cbc42ad725
Johannes Kron <kron@chromium.org>
2026-01-26 01:27:02 -0800

    Add basic unit test to screen_capture_kit_device_mac
    Bug: 467021231
    Change-Id: Icfa891b8ab3aa05c2abb2a9cafe899baf9d97491
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7450901
    Reviewed-by: Tove Petersson <tovep@chromium.org>
    Commit-Queue: Johannes Kron <kron@chromium.org>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1574374}

--- content/browser/media/capture/screen_capture_kit_device_mac.mm
+++ content/browser/media/capture/screen_capture_kit_device_mac.mm
@@ -752,7 +752,7 @@ std::unique_ptr<media::VideoCaptureDevice> CreateScreenCaptureKitDeviceMac(
       break;
     default:
       // ScreenCaptureKitDeviceMac supports only TYPE_SCREEN and TYPE_WINDOW.
-      // https://crbug.com/1176900
+      // See https://crbug.com/1176900
       return nullptr;
   }
 
