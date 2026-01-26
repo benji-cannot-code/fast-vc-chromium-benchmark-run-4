52839ebd2e150b888a08c87c29ac842481261c03
Judith Hemp <hempjudith@google.com>
2026-01-26 05:55:44 -0800

    [GestureGated] Add feature flag
    Bug: 412962300
    Change-Id: Iae6b579b4f0c8b244aecdc7ad1bc4e7996473e3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7458403
    Reviewed-by: Elias Klim <elklm@chromium.org>
    Commit-Queue: Judith Hemp <hempjudith@google.com>
    Cr-Commit-Position: refs/heads/main@{#1574486}

--- components/permissions/features.h
+++ components/permissions/features.h
@@ -106,6 +106,9 @@ BASE_DECLARE_FEATURE(kReturnDeniedForNotificationsWhenNoAppLevelSettings);
 COMPONENT_EXPORT(PERMISSIONS_COMMON)
 BASE_DECLARE_FEATURE(kPermissionPredictionsGeolocationAccuracy);
 
+COMPONENT_EXPORT(PERMISSIONS_COMMON)
+BASE_DECLARE_FEATURE(kPermissionsGestureGatedPrompts);
+
 }  // namespace features
 
 namespace feature_params {
@@ -180,6 +183,14 @@ extern const base::FeatureParam<std::string>
 COMPONENT_EXPORT(PERMISSIONS_COMMON)
 extern const base::FeatureParam<std::string>
     kWebKioskBrowserPermissionsAllowlist;
+
+COMPONENT_EXPORT(PERMISSIONS_COMMON)
+extern const base::FeatureParam<bool>
+    kPermissionsGestureGatedPromptsMuteNotifications;
+
+COMPONENT_EXPORT(PERMISSIONS_COMMON)
+extern const base::FeatureParam<bool>
+    kPermissionsGestureGatedPromptsMuteGeolocation;
 
 #if !BUILDFLAG(IS_ANDROID)
 COMPONENT_EXPORT(PERMISSIONS_COMMON)
