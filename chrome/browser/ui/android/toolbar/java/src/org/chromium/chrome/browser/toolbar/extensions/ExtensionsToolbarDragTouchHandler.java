ab9af3c1b7606a497e4cd354122d0bb7c96d70d4
Masa Fujita <massan@google.com>
2026-01-26 15:59:37 -0800

    actions revamp: Restrict icon's movement to RecyclerView's width
    This CL restricts icon's movement to RecyclerView's width so it doesn't go out of bounds with drag.
    
    Demo:
    [Before] http://screencast/cast/NTE4MzgyOTk5NTE1OTU1Mnw4NmI1MjljOS04ZA
    [After] http://screencast/cast/NDY4MDA1MjIwODgyODQxNnwxODM5N2JlYy1iNQ
    
    Bug: 459079173
    Change-Id: Iaf10be730953a4defd0a2bfc998a0816a55a5b25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7503994
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Masa Fujita <massan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1574854}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/extensions/ExtensionsToolbarDragTouchHandler.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/extensions/ExtensionsToolbarDragTouchHandler.java
@@ -8,6 +8,7 @@ package org.chromium.chrome.browser.toolbar.extensions;
 import android.animation.AnimatorSet;
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.graphics.Canvas;
 import android.view.View;
 
 import androidx.recyclerview.widget.ItemTouchHelper;
@@ -51,5 +52,29 @@ public class ExtensionsToolbarDragTouchHandler extends DragTouchHandler {
         animatorSet.setDuration(ANIMATION_DURATION_MS);
         animatorSet.play(elevationAnimator);
         animatorSet.start();
+    }
+
+    @Override
+    public void onChildDraw(
+            Canvas c,
+            RecyclerView recyclerView,
+            RecyclerView.ViewHolder viewHolder,
+            float dX,
+            float dY,
+            int actionState,
+            boolean isCurrentlyActive) {
+        View view = viewHolder.itemView;
+
+        float newLeft = view.getLeft() + dX;
+        float newRight = view.getRight() + dX;
+
+        // Clamp dX so that the icon doesn't go outside RecyclerView's bounds.
+        if (newLeft < 0) {
+            dX = -view.getLeft();
+        } else if (newRight > recyclerView.getWidth()) {
+            dX = recyclerView.getWidth() - view.getRight();
+        }
+
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
     }
 }
