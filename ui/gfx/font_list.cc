55199127327caa73bbfa2d1fc71ce7f6d2c0aa70
Jeffrey Yu <yuje@chromium.org>
2026-01-26 12:26:39 -0800

    Replace `std::unordered_set` with `absl::flat_hash_set`.
    The Chromium containers guide
    (https://chromium.googlesource.com/chromium/src/+/HEAD/base/containers/README.md)
    recommends using `absl::flat_hash_set` as a default map choice instead
    of `std::unordered_set` when pointer stability isn't needed, and
    `absl::node_hash_set` when it is. Both will still generally perform
    better and require less overhead than `std::unordered_set`.
    
    Bug: 473916362
    Change-Id: Id359e30904440757ec2081cd77728254443a9ee8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7509877
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Auto-Submit: Jeffrey Yu <yuje@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1574699}

--- ui/gfx/font_list.cc
+++ ui/gfx/font_list.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/font_list.h"
 
 #include <ostream>
-#include <unordered_set>
 
 #include "base/lazy_instance.h"
 #include "base/strings/string_number_conversions.h"
@@ -17,6 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkFontMgr.h"
 #include "third_party/skia/include/core/SkTypeface.h"
 #include "ui/gfx/font_list_impl.h"
+
+#if BUILDFLAG(IS_MAC)
+#include "third_party/abseil-cpp/absl/container/flat_hash_set.h"
+#endif
 
 namespace {
 
@@ -255,7 +258,8 @@ std::string FontList::FirstAvailableOrFirst(const std::string& font_name_list) {
   // names in order to avoid at worst `available_size * families.size()` string
   // comparisons.
   const int available_size = fm->countFamilies();
-  std::unordered_set<std::string> availables;
+  absl::flat_hash_set<std::string> availables;
+  availables.reserve(available_size);
   for (int i = 0; i < available_size; ++i) {
     SkString name;
     fm->getFamilyName(i, &name);
