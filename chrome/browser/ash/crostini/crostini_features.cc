0584d0dc95865f70835f089a73eec2cedda4ede8
Jun Ishiguro <junis@google.com>
2026-01-26 00:07:19 -0800

    Use InstallAttributes to check if device is managed
    This CL replaces BrowserPolicyConnectorAsh::IsDeviceEnterpriseManaged
    usage with InstallAttributes::IsEnterpriseManaged() to remove direct
    dependency on browser_process.
    
    Bug: b:404131219, b:404131876, b:404133191
    Change-Id: I4128a9b6c58048f2da13bc3d40c68852e9343192
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7512675
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Reviewed-by: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1574352}

--- chrome/browser/ash/crostini/crostini_features.cc
+++ chrome/browser/ash/crostini/crostini_features.cc
@@ -12,13 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/crostini/crostini_pref_names.h"
 #include "chrome/browser/ash/guest_os/guest_id.h"
 #include "chrome/browser/ash/guest_os/guest_os_pref_names.h"
-#include "chrome/browser/ash/policy/core/browser_policy_connector_ash.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
-#include "chrome/browser/browser_process.h"
-#include "chrome/browser/browser_process_platform_part.h"
 #include "chrome/browser/policy/profile_policy_connector.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/chrome_features.h"
+#include "chromeos/ash/components/install_attributes/install_attributes.h"
 #include "chromeos/ash/components/settings/cros_settings.h"
 #include "chromeos/ash/experiences/guest_os/virtual_machines/virtual_machines_util.h"
 #include "components/policy/proto/chrome_device_policy.pb.h"
@@ -176,9 +174,7 @@ void CrostiniFeatures::CanChangeAdbSideloading(
   }
 
   // ADB sideloading is not supported for managed devices or profiles.
-  auto* connector =
-      g_browser_process->platform_part()->browser_policy_connector_ash();
-  if (connector->IsDeviceEnterpriseManaged() ||
+  if (ash::InstallAttributes::Get()->IsEnterpriseManaged() ||
       profile->GetProfilePolicyConnector()->IsManaged()) {
     std::move(callback).Run(false);
     return;
