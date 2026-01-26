0958961334cd1425f23e4ec7e76277a492a44e70
Xinyi Ji <xinyiji@google.com>
2026-01-26 14:29:52 -0800

    [CustomizationV2] Add A11y strings for theme collection bottom sheets
    This CL did the following things:
    1. Refactors list item spacing from padding to margin to ensure the accessibility focus is precisely on the item content.
    2. Adds contentDescription for theme collection images. Setting view selected or not allows talkback to read "selected" when the item is clicked.
    
    Demo:https://drive.google.com/file/d/1fBnW01hUc2x2HYJvMkhUKPf_EMCvYCgN/view?usp=sharing&resourcekey=0-cFXDrWJ1zSMLQXZeyH1aKg
    
    Bug: 423579377
    Change-Id: I25801a608ffe18fc9c6184f8d91e7c7098dc951c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7517028
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1574794}

--- chrome/browser/ntp_customization/junit/src/org/chromium/chrome/browser/ntp_customization/theme/theme_collections/NtpThemeCollectionsAdapterUnitTest.java
+++ chrome/browser/ntp_customization/junit/src/org/chromium/chrome/browser/ntp_customization/theme/theme_collections/NtpThemeCollectionsAdapterUnitTest.java
@@ -91,12 +91,15 @@ public class NtpThemeCollectionsAdapterUnitTest {
                 new BackgroundCollection("id2", "Another Collection", JUnitTestGURLs.URL_2, 456));
 
         mImageItems = new ArrayList<>();
+        List<String> attributions1 = new ArrayList<>();
+        attributions1.add("Attribution 1");
+        attributions1.add("Attribution 2");
         mImageItems.add(
                 new CollectionImage(
                         "id1",
                         JUnitTestGURLs.URL_1,
                         PREVIEW_IMAGE_URL,
-                        new ArrayList<>(),
+                        attributions1,
                         JUnitTestGURLs.URL_1));
         mImageItems.add(
                 new CollectionImage(
@@ -153,6 +156,7 @@ public class NtpThemeCollectionsAdapterUnitTest {
         assertEquals(View.VISIBLE, viewHolder.mTitle.getVisibility());
         assertTrue(viewHolder.mView.hasOnClickListeners());
         assertFalse(viewHolder.itemView.isActivated());
+        assertFalse(viewHolder.itemView.isSelected());
 
         ArgumentCaptor<ImageFetcher.Params> paramsCaptor =
                 ArgumentCaptor.forClass(ImageFetcher.Params.class);
@@ -177,6 +181,10 @@ public class NtpThemeCollectionsAdapterUnitTest {
         assertEquals(View.GONE, viewHolder.mTitle.getVisibility());
         assertTrue(viewHolder.mView.hasOnClickListeners());
         assertFalse(viewHolder.itemView.isActivated());
+        assertFalse(viewHolder.itemView.isSelected());
+        assertEquals(
+                "Attribution 1, Attribution 2",
+                viewHolder.mView.getContentDescription().toString());
 
         ArgumentCaptor<ImageFetcher.Params> paramsCaptor =
                 ArgumentCaptor.forClass(ImageFetcher.Params.class);
@@ -322,6 +330,7 @@ public class NtpThemeCollectionsAdapterUnitTest {
         adapter.setSelection(mCollectionItems.get(0).id, null);
         adapter.onBindViewHolder(viewHolder, 0);
         assertTrue(viewHolder.itemView.isActivated());
+        assertTrue(viewHolder.itemView.isSelected());
 
         // Bind a different item, it should not be selected.
         ThemeCollectionViewHolder viewHolder2 =
@@ -330,13 +339,16 @@ public class NtpThemeCollectionsAdapterUnitTest {
         itemViewTypeField.set(viewHolder2, THEME_COLLECTIONS_ITEM);
         adapter.onBindViewHolder(viewHolder2, 1);
         assertFalse(viewHolder2.itemView.isActivated());
+        assertFalse(viewHolder2.itemView.isSelected());
 
         // Select the second item.
         adapter.setSelection(mCollectionItems.get(1).id, null);
         adapter.onBindViewHolder(viewHolder, 0);
         assertFalse(viewHolder.itemView.isActivated());
+        assertFalse(viewHolder.itemView.isSelected());
         adapter.onBindViewHolder(viewHolder2, 1);
         assertTrue(viewHolder2.itemView.isActivated());
+        assertTrue(viewHolder2.itemView.isSelected());
     }
 
     @Test
@@ -359,16 +371,19 @@ public class NtpThemeCollectionsAdapterUnitTest {
         adapter.setSelection(mImageItems.get(0).collectionId, mImageItems.get(0).imageUrl);
         adapter.onBindViewHolder(viewHolder, 0);
         assertTrue(viewHolder.itemView.isActivated());
+        assertTrue(viewHolder.itemView.isSelected());
 
         // Select with only matching collectionId, should not be activated.
         adapter.setSelection(mImageItems.get(0).collectionId, JUnitTestGURLs.URL_2);
         adapter.onBindViewHolder(viewHolder, 0);
         assertFalse(viewHolder.itemView.isActivated());
+        assertFalse(viewHolder.itemView.isSelected());
 
         // Select with only matching imageUrl, should not be activated.
         adapter.setSelection("id2", mImageItems.get(0).imageUrl);
         adapter.onBindViewHolder(viewHolder, 0);
         assertFalse(viewHolder.itemView.isActivated());
+        assertFalse(viewHolder.itemView.isSelected());
     }
 
     @Test
