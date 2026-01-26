39cff0205adc66c318c34f65b4facce9171987aa
Paris <pmeuleman@chromium.org>
2026-01-26 02:27:19 -0800

    Fix crash in promo_card_unittest.cc when DUMP_WILL_BE_NOTREACHED is on
    This is a prerequisite to have this file to run on more bots.
    
    Change-Id: I31cdb8c57ada207a994309240cebb94f84541324
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7497551
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Pâris Meuleman <pmeuleman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1574407}

--- chrome/browser/ui/webui/password_manager/promo_card_unittest.cc
+++ chrome/browser/ui/webui/password_manager/promo_card_unittest.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/password_manager/promo_cards/web_password_manager_promo.h"
 #include "chrome/browser/web_applications/test/fake_web_app_provider.h"
 #include "chrome/browser/web_applications/test/web_app_test.h"
+#include "chrome/browser/web_applications/test/web_app_test_utils.h"
 #include "chrome/browser/web_applications/web_app_helpers.h"
 #include "chrome/browser/web_applications/web_app_registrar.h"
 #include "chrome/common/webui_url_constants.h"
@@ -98,10 +99,7 @@ class FakePromoCard : public PasswordPromoCardBase {
 
 std::unique_ptr<web_app::WebApp> CreateWebApp() {
   GURL url(chrome::kChromeUIPasswordManagerURL);
-  auto web_app = std::make_unique<web_app::WebApp>(
-      /*manifest_id=*/web_app::GenerateManifestIdFromStartUrlOnly(url),
-      /*start_url=*/url,
-      /*scope=*/url.GetWithoutFilename());
+  auto web_app = web_app::test::CreateWebApp(url);
   web_app->SetUserDisplayMode(web_app::mojom::UserDisplayMode::kStandalone);
   return web_app;
 }
