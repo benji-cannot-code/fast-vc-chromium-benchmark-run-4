230657496b16bba608018dc0b43fcb9ca7ea2346
Hiroshige Hayashizaki <hiroshige@chromium.org>
2026-01-26 15:58:23 -0800

    Reduce ResourceRequest copy in tests in content/browser/webid
    This CL was uploaded by git cl split.
    
    R=yigu@chromium.org
    
    Bug: 478890167
    Change-Id: I348edbe86b95dff2418942b005ed7eff1b83d7e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7517928
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Auto-Submit: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1574852}

--- content/browser/webid/idp_network_request_manager_unittest.cc
+++ content/browser/webid/idp_network_request_manager_unittest.cc
@@ -788,7 +788,7 @@ TEST_F(IdpNetworkRequestManagerTest, CacheProfilePictures) {
   network::TestURLLoaderFactory::PendingRequest* pending_request =
       test_url_loader_factory().GetPendingRequest(0);
   ASSERT_TRUE(pending_request);
-  network::ResourceRequest resource_request = pending_request->request;
+  const network::ResourceRequest& resource_request = pending_request->request;
   EXPECT_FALSE(resource_request.load_flags & net::LOAD_ONLY_FROM_CACHE);
   net::IsolationInfo expected_isolation_info = net::IsolationInfo::Create(
       net::IsolationInfo::RequestType::kOther,
@@ -813,7 +813,7 @@ TEST_F(IdpNetworkRequestManagerTest, DownloadAndDecodeCachedImage) {
   network::TestURLLoaderFactory::PendingRequest* pending_request =
       test_url_loader_factory().GetPendingRequest(0);
   ASSERT_TRUE(pending_request);
-  network::ResourceRequest resource_request = pending_request->request;
+  const network::ResourceRequest& resource_request = pending_request->request;
   EXPECT_TRUE(resource_request.load_flags & net::LOAD_ONLY_FROM_CACHE);
   net::IsolationInfo expected_isolation_info = net::IsolationInfo::Create(
       net::IsolationInfo::RequestType::kOther,
