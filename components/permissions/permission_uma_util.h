dc2976c4398aff0db5b19740612aaaebff523efe
Judith Hemp <hempjudith@google.com>
2026-01-26 05:24:57 -0800

    [CodeHealth] Add LINT checks for PermissionHeaderPolicyForUMA
    Bug: 414527270
    Change-Id: I2aa0a71d00df07a7f1c5781e417e8cde74f1f8b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7513599
    Reviewed-by: Saeid Eid <saeideid@google.com>
    Commit-Queue: Judith Hemp <hempjudith@google.com>
    Cr-Commit-Position: refs/heads/main@{#1574463}

--- components/permissions/permission_uma_util.h
+++ components/permissions/permission_uma_util.h
@@ -186,6 +186,7 @@ enum class PermissionEmbargoStatus {
 // configured to allow sub-frame origin. Any new values should be inserted
 // immediately prior to NUM. All values here should have corresponding entries
 // PermissionsPolicyConfiguration area of enums.xml.
+// LINT.IfChange(PermissionHeaderPolicyForUMA)
 enum class PermissionHeaderPolicyForUMA {
   // No (or an invalid) Permissions-Policy header was present, results in an
   // empty features list. It indicates none security-awareness of permissions
@@ -217,6 +218,7 @@ enum class PermissionHeaderPolicyForUMA {
   // Always keep this at the end.
   NUM,
 };
+// LINT.ThenChange(//tools/metrics/histograms/metadata/permissions/enums.xml:PermissionsPolicyConfiguration)
 
 // The kind of permission prompt UX used to surface a permission request.
 // Enum used in UKMs and UMAs, do not re-order or change values. Deprecated
