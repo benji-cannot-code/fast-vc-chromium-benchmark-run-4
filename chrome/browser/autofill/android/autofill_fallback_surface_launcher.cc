1517e24ed40f4f6269ba8ca34483eb4e66504f35
Timofey Chudakov <tchudakov@google.com>
2026-01-26 05:34:41 -0800

    [AutofillAi - M4] Move AutofillFallbackSurfaceLauncher.
    Bypass-Check-License: File moved.
    Bug: 460410690
    Change-Id: Ib025a935100b9cf3923dc2794e67a0f67026b6bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7513598
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1574470}

--- chrome/browser/autofill/android/autofill_fallback_surface_launcher.cc
+++ chrome/browser/autofill/android/autofill_fallback_surface_launcher.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/android/autofill/autofill_fallback_surface_launcher.h"
+#include "chrome/browser/autofill/android/autofill_fallback_surface_launcher.h"
 
 #include "base/android/jni_android.h"
 #include "base/android/jni_string.h"
