33a9b32df1cab23da52ab6912b81686ad7ae97eb
James Lee <ljjlee@google.com>
2026-01-26 08:09:46 -0800

    Enable Taiyaki by default on iOS
    This enables the Taiyaki flag by default on iOS, with
    choice_screen_surface set to fre_only, as well as the
    CurrentDseHighlightOnChoiceScreenSupport flag.
    
    Bug: 434155606
    Change-Id: I8ebba5c3ff49c8797f0e420adf65723477a0fd3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7516704
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Auto-Submit: James Lee <ljjlee@google.com>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1574546}

--- components/search_engines/search_engine_choice/search_engine_choice_service.cc
+++ components/search_engines/search_engine_choice/search_engine_choice_service.cc
@@ -406,6 +406,8 @@ const TemplateURL* MaybeGetCurrentDefaultToHighlight(
   }
 
   if (!regional_capabilities_service.GetChoiceScreenEligibilityConfig()
+           .has_value() ||
+      !regional_capabilities_service.GetChoiceScreenEligibilityConfig()
            ->highlight_current_default) {
     return nullptr;
   }
