2ea5b3dcab7668200a829beab109f2274749d161
Alison Gale <agale@chromium.org>
2026-01-26 11:08:25 -0800

    Update import location of split tab id/visual data
    This CL was uploaded by an experimental version of git cl split
    (https://crbug.com/389069356).
    
    R=dpenning@chromium.org
    
    Bug: 443780638
    Change-Id: I94fc195f8e027414eec594dedf4ef47085784c1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7516562
    Auto-Submit: Alison Gale <agale@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1574652}

--- chrome/browser/ui/browser_commands.h
+++ chrome/browser/ui/browser_commands.h
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/tab_enums.h"
 #include "chrome/browser/ui/tabs/tab_strip_model_delegate.h"
 #include "chrome/browser/ui/tabs/tab_strip_user_gesture_details.h"
-#include "components/tabs/public/split_tab_id.h"
+#include "components/split_tabs/split_tab_id.h"
 #include "content/public/common/page_zoom.h"
 #include "printing/buildflags/buildflags.h"
 #include "ui/base/window_open_disposition.h"
