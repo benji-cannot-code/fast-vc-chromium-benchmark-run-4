fb9b14ff7aef3ee0910acdca83dab595f05cbd47
Andrey Davydov <andreydav@google.com>
2026-01-26 08:33:25 -0800

    Remove CheckPasswordsAgainstCryptohomeHelper
    Corresponding feature was deprioritized in 2024 before launching. Now
    the remaining code makes it harder to implement the local pin and
    password for the enterprise.
    
    Bug: b:40214270
    Change-Id: Ic4ed316bfb3aca36296a585649611c0e651a5b10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7510753
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Commit-Queue: Andrey Davydov <andreydav@google.com>
    Cr-Commit-Position: refs/heads/main@{#1574558}

--- chrome/browser/ash/login/screens/saml_confirm_password_screen.cc
+++ chrome/browser/ash/login/screens/saml_confirm_password_screen.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/screens/base_screen.h"
 #include "chrome/browser/ash/login/wizard_context.h"
 #include "chrome/browser/ui/ash/login/login_display_host.h"
-#include "chrome/browser/ui/webui/ash/login/check_passwords_against_cryptohome_helper.h"
 #include "chrome/browser/ui/webui/ash/login/saml_confirm_password_handler.h"
 #include "chromeos/ash/components/login/auth/public/auth_types.h"
 #include "chromeos/ash/components/login/auth/public/cryptohome_key_constants.h"
@@ -95,18 +94,7 @@ void SamlConfirmPasswordScreen::ShowImpl() {
     return;
   view_->Show(user_context_->GetAccountId().GetUserEmail(),
               scraped_saml_passwords_.empty());
-  if (!features::IsCheckPasswordsAgainstCryptohomeHelperEnabled() ||
-      scraped_saml_passwords_.empty()) {
-    ShowPasswordStep(/*retry=*/false);
-    return;
-  }
-  check_passwords_against_cryptohome_helper_ =
-      std::make_unique<CheckPasswordsAgainstCryptohomeHelper>(
-          *user_context_.get(), scraped_saml_passwords_,
-          base::BindOnce(&SamlConfirmPasswordScreen::ShowPasswordStep,
-                         base::Unretained(this),/*retry=*/false),
-          base::BindOnce(&SamlConfirmPasswordScreen::TryPassword,
-                         base::Unretained(this)));
+  ShowPasswordStep(/*retry=*/false);
 }
 
 void SamlConfirmPasswordScreen::ShowPasswordStep(bool retry) {
--- chrome/browser/ash/login/screens/saml_confirm_password_screen.h
+++ chrome/browser/ash/login/screens/saml_confirm_password_screen.h
@@ -9,10 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#include "base/functional/callback.h"
 #include "base/memory/weak_ptr.h"
 #include "base/values.h"
 #include "chrome/browser/ash/login/screens/base_screen.h"
-#include "chrome/browser/ui/webui/ash/login/check_passwords_against_cryptohome_helper.h"
 #include "chromeos/ash/components/login/auth/public/user_context.h"
 #include "components/login/base_screen_handler_utils.h"
 
@@ -61,8 +61,6 @@ class SamlConfirmPasswordScreen : public BaseScreen {
 
   ScreenExitCallback exit_callback_;
 
-  std::unique_ptr<CheckPasswordsAgainstCryptohomeHelper>
-      check_passwords_against_cryptohome_helper_;
   std::unique_ptr<UserContext> user_context_;
   ::login::StringList scraped_saml_passwords_;
   int attempt_count_ = 0;
