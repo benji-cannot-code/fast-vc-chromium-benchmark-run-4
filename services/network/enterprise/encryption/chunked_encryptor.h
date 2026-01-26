6c958434bd9752963029d7feb6f1a01b6e1f66b2
Haihan Chen <haihan@google.com>
2026-01-26 11:08:57 -0800

    [Cache Encryption] Add encrypted string support for derived keys
    Bug: 460509865
    Change-Id: I07c69d3cf5e49445389c39e589f74e195e92d5bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7512713
    Reviewed-by: Lina Ismail <ismaill@google.com>
    Commit-Queue: Haihan Chen <haihan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1574653}

--- services/network/enterprise/encryption/chunked_encryptor.h
+++ services/network/enterprise/encryption/chunked_encryptor.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/span.h"
 #include "base/types/expected.h"
 #include "crypto/aead.h"
+#include "crypto/process_bound_string.h"
 
 namespace network::enterprise_encryption {
 
@@ -50,16 +51,15 @@ inline constexpr size_t kChunkDataSize = 4096u;
 inline constexpr size_t kEncryptedChunkSize = kChunkDataSize + kAuthTagSize;
 
 struct COMPONENT_EXPORT(NETWORK_SERVICE) EncryptionContext {
-  EncryptionContext() = default;
-  ~EncryptionContext() = default;
   EncryptionContext(const EncryptionContext&) = delete;
   EncryptionContext& operator=(const EncryptionContext&) = delete;
-  EncryptionContext(EncryptionContext&&) = default;
-  EncryptionContext& operator=(EncryptionContext&&) = default;
+  EncryptionContext(EncryptionContext&&);
+  EncryptionContext& operator=(EncryptionContext&&);
+  ~EncryptionContext();
   EncryptionContext(base::span<const uint8_t, kKeySize> key,
                     base::span<const uint8_t, kNoncePrefixSize> nonce_prefix);
 
-  std::array<uint8_t, kKeySize> derived_key;
+  crypto::ProcessBoundString derived_key;
   std::array<uint8_t, kNoncePrefixSize> nonce_prefix;
 };
 
@@ -114,7 +114,6 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) ChunkedEncryptor {
                                               bool is_last_chunk) const;
 
  private:
-  std::array<uint8_t, kKeySize> derived_key_;
   std::array<uint8_t, kNoncePrefixSize> nonce_prefix_;
   crypto::Aead aead_;
 };
