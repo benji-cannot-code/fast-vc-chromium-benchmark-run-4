4cdb7e28254736d1522c5e05eb640f670a2ef552
Mike Wittman <wittman@google.com>
2026-01-27 10:37:36 -0800

    Reland "Move PageEmbeddingsService to components"
    This is a reland of commit a57a097f32ceffc54a3cb006032b3b5e71430128
    
    Fixes a cast build error.
    
    Original change's description:
    > Move PageEmbeddingsService to components
    >
    > This is a prerequisite for allowing PageEmbeddingsService to be used
    > by HistoryEmbeddingsService.
    >
    > Bug: 468035604
    > Change-Id: I1394443918a1056dd3c9c0f38cd34ea10515f7bd
    > Bypass-Check-License: moved files
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7509881
    > Auto-Submit: Mike Wittman <wittman@chromium.org>
    > Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    > Commit-Queue: Mike Wittman <wittman@chromium.org>
    > Reviewed-by: Steven Holte <holte@chromium.org>
    > Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    > Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1574771}
    
    Bug: 468035604
    Bypass-Check-License: moved files
    Change-Id: I30de11728f8a23b9550801da6928197982e4889c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7518391
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Mike Wittman <wittman@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1575363}

--- components/passage_embeddings/content/embeddings_candidate_generator.h
+++ components/passage_embeddings/content/embeddings_candidate_generator.h
@@ -3,14 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_PASSAGE_EMBEDDINGS_EMBEDDINGS_CANDIDATE_GENERATOR_H_
-#define CHROME_BROWSER_PASSAGE_EMBEDDINGS_EMBEDDINGS_CANDIDATE_GENERATOR_H_
+#ifndef COMPONENTS_PASSAGE_EMBEDDINGS_CONTENT_EMBEDDINGS_CANDIDATE_GENERATOR_H_
+#define COMPONENTS_PASSAGE_EMBEDDINGS_CONTENT_EMBEDDINGS_CANDIDATE_GENERATOR_H_
 
 #include <string>
 #include <vector>
 
-#include "chrome/browser/passage_embeddings/page_embeddings_service.h"
 #include "components/optimization_guide/proto/features/common_quality_data.pb.h"
+#include "components/passage_embeddings/content/page_embeddings_service.h"
 
 namespace passage_embeddings {
 
@@ -22,4 +22,4 @@ std::vector<std::pair<std::string, PassageType>> GenerateEmbeddingsCandidates(
 
 }  // namespace passage_embeddings
 
-#endif  // CHROME_BROWSER_PASSAGE_EMBEDDINGS_EMBEDDINGS_CANDIDATE_GENERATOR_H_
+#endif  // COMPONENTS_PASSAGE_EMBEDDINGS_CONTENT_EMBEDDINGS_CANDIDATE_GENERATOR_H_
