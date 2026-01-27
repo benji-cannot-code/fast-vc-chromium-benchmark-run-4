48332675122bf8e37ca126d32e566aab0ff088c2
Greg Thompson <grt@chromium.org>
2026-01-27 02:29:27 -0800

    Disable AsyncClipboardNoReadWriteContentCapability test on mac
    due to flaky failures.
    
    Bug: 478909983
    Change-Id: Ib152d0145fe4188f09d5ab6e9d213c6f6d234a56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7520604
    Owners-Override: Greg Thompson <grt@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Reviewed-by: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1575087}

--- chrome/browser/extensions/content_capabilities_browsertest.cc
+++ chrome/browser/extensions/content_capabilities_browsertest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
+#include "build/build_config.h"
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/chrome_paths.h"
@@ -302,8 +303,15 @@ IN_PROC_BROWSER_TEST_F(ContentCapabilitiesTest,
   CheckSiteCanRead(/*expected=*/true);
 }
 
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_AsyncClipboardNoReadWriteContentCapability \
+  DISABLED_AsyncClipboardNoReadWriteContentCapability
+#else
+#define MAYBE_AsyncClipboardNoReadWriteContentCapability \
+  AsyncClipboardNoReadWriteContentCapability
+#endif
 IN_PROC_BROWSER_TEST_F(ContentCapabilitiesTest,
-                       AsyncClipboardNoReadWriteContentCapability) {
+                       MAYBE_AsyncClipboardNoReadWriteContentCapability) {
   LoadExtensionWithCapabilitiesAndNavigateToPage("[]");
   SetPermissionOverrideForAsyncClipboardTests(
       blink::mojom::PermissionStatus::GRANTED);
