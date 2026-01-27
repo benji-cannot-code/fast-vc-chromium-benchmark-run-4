aeed018eb240404ba96c6d4039a8eb4324d419e2
Thryze Knox <thryzx@gmail.com>
2026-01-26 21:16:06 -0800

    [Findbar] Remove mac Activatable::kYes and activate browser on textfield click instead
    The original fix for crbug.com/40616214 added Activatable::kYes to the
    find bar widget. However, this caused a series of other issues on macOS.
    
    This CL removes Activatable::kYes and instead activates the browser
    window when clicking on the find bar textfield, which fixes the original
    issue while avoiding the side effects.
    
    Bug: 40205173, 40147557, 40694525, 442293378, 422444253
    Include-Ci-Only-Tests: true
    Change-Id: Ied175ff3adc06e09261649e2d31414a8da48e0bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7452301
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Auto-Submit: Thryze Knox <thryzx@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1574981}

--- chrome/browser/ui/find_bar/find_bar.h
+++ chrome/browser/ui/find_bar/find_bar.h
@@ -26,11 +26,9 @@ class Point;
 class Range;
 }  // namespace gfx
 
-#if BUILDFLAG(IS_MAC)
 namespace views {
 class Widget;
 }
-#endif
 
 class FindBar {
  public:
@@ -109,11 +107,9 @@ class FindBar {
   // Closes any overlapping bubbles, such as the translate bubble.
   virtual void CloseOverlappingBubbles() = 0;
 
-#if BUILDFLAG(IS_MAC)
   // Get the host widget. Used by immersive fullscreen to detect the find bar
   // widget and reparent as necessary.
   virtual views::Widget* GetHostWidget() = 0;
-#endif
 };
 
 class FindBarTesting {
