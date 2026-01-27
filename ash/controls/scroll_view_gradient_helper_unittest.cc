764c555598370f7e0f6c2a9f9b0a5ef2ff8a8431
hiroki oshima <hiroki.oshima@gmail.com>
2026-01-26 23:38:55 -0800

    fix ScrollViewGradientHelperTest dangling pointer
    Bug: 40212619
    Change-Id: Ia8770c49adb7ca820e10154916b95ad4b161ea0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7514726
    Reviewed-by: Di Wu <diwux@google.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Di Wu <diwux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1575021}

--- ash/controls/scroll_view_gradient_helper_unittest.cc
+++ ash/controls/scroll_view_gradient_helper_unittest.cc
@@ -55,6 +55,7 @@ class ScrollViewGradientHelperTest : public views::ViewsTestBase {
 
   void TearDown() override {
     gradient_helper_.reset();
+    scroll_view_ = nullptr;
     widget_.reset();
     ViewsTestBase::TearDown();
   }
@@ -85,7 +86,7 @@ class ScrollViewGradientHelperTest : public views::ViewsTestBase {
   }
 
   views::UniqueWidgetPtr widget_;
-  raw_ptr<views::ScrollView, DanglingUntriaged> scroll_view_ = nullptr;
+  raw_ptr<views::ScrollView> scroll_view_ = nullptr;
   std::unique_ptr<ScrollViewGradientHelper> gradient_helper_;
 };
 
