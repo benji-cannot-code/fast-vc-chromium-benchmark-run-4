b72454f41b89044a8f05dc1e315666f32de62ab7
Adam Norberg <norberg@google.com>
2026-01-27 05:57:52 -0800

    Clean up stale xz options and add forgotten .patch file.
    I started out adding the patch file I forgot but when I started that
    I realized that I had forgotten to remove a `#define` that has no effect
    on `lzma_sdk`, so I cleaned that up too.
    
    Bug: 478054545
    Change-Id: If79bb4cd9d7677e484d06360a42c97821553323b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7518486
    Auto-Submit: Adam Norberg <norberg@google.com>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1575173}

--- third_party/libdmg-hfsplus/src/dmg/compress.c
+++ third_party/libdmg-hfsplus/src/dmg/compress.c
@@ -17,14 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/lzma_sdk/C/XzEnc.h"
 #include "third_party/lzma_sdk/google/seven_zip_c_buf_stream.h"
 
-// LZMA_FAIL_FAST was introduced in 5.3.3, attempting to use it in earlier versions
-// yields an error.
-#ifdef LZMA_FAIL_FAST
-  #define LZMA_DECODE_OPTS LZMA_FAIL_FAST
-#else
-  #define LZMA_DECODE_OPTS 0
-#endif
-
 // Calls once-only initialization functions for `lzma_sdk`. Not threadsafe.
 static void reallyInitLzmaCrc()
 {
