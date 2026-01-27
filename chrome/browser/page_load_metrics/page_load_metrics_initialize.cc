37a52e210f0272d69f2ccc1ee24e8fb04aa38df5
Takashi Toyoshima <toyoshim@chromium.org>
2026-01-27 03:24:01 -0800

    LinkPreview: Remove PreviewPageLoadMetricsObserver
    Removes the PreviewPageLoadMetricsObserver as it is no longer required.
    
    This change:
    - Deletes the observer implementation and associated browser tests.
    - Removes the observer registration from page_load_metrics_initialize.cc.
    - Cleans up relevant build targets and test filters.
    - Removes definitions for the following histograms:
      - PageLoad.Clients.LinkPreview.Usage
      - PageLoad.Experimental.PageVisitType2
      - PageLoad.Experimental.PreviewFinalStatus
    
    Fixed: 478557569
    OBSOLETE_HISTOGRAMS=no longer used
    
    
    LinkPreview: Remove PreviewPLMO
    
    Change-Id: Ica7c67ae23ff95dcb2246dcc53698990382d5b3d
    Fixed: 478557569
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7519285
    Reviewed-by: Ken Okada <kenoss@chromium.org>
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1575108}

--- chrome/browser/page_load_metrics/page_load_metrics_initialize.cc
+++ chrome/browser/page_load_metrics/page_load_metrics_initialize.cc
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/page_load_metrics/observers/page_anchors_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/prefetch_page_load_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/preload_serving_metrics_page_load_metrics_observer.h"
-#include "chrome/browser/page_load_metrics/observers/preview_page_load_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/protocol_page_load_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/scheme_page_load_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/security_state_page_load_metrics_observer.h"
@@ -265,7 +264,6 @@ void PageLoadMetricsEmbedder::RegisterObservers(
         std::make_unique<LocalNetworkRequestsPageLoadMetricsObserver>());
     tracker->AddObserver(
         std::make_unique<TabStripPageLoadMetricsObserver>(web_contents()));
-    tracker->AddObserver(std::make_unique<PreviewPageLoadMetricsObserver>());
     tracker->AddObserver(std::make_unique<BookmarkBarMetricsObserver>());
     tracker->AddObserver(
         std::make_unique<NewTabPageInitiatedPageLoadMetricsObserver>());
