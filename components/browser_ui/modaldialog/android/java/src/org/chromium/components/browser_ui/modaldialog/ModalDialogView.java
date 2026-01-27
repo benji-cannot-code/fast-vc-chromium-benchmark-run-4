d756a26276cbbf0cb0ede76b5654afaabcb5e992
Timofey Chudakov <tchudakov@google.com>
2026-01-27 01:46:09 -0800

    [AutofillAi - M4] Add end title icon to the modal dialog.
    This CL adds a new property to the modal dialog MVC component
    to display the icon at the end of the title row.
    
    The desired UI: screen/AxAeDEyBELssRRf
    
    Bug: 460410690, 476754325
    Change-Id: I296b8c47c4e6e17bbcc9c3cd166b169709e5ea8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7511925
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Jack Thiesen <jthiesen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1575073}

--- components/browser_ui/modaldialog/android/java/src/org/chromium/components/browser_ui/modaldialog/ModalDialogView.java
+++ components/browser_ui/modaldialog/android/java/src/org/chromium/components/browser_ui/modaldialog/ModalDialogView.java
@@ -63,6 +63,7 @@ public class ModalDialogView extends BoundedLinearLayout implements View.OnClick
     private ViewGroup mTitleContainer;
     private TextView mTitleView;
     private ImageView mTitleIcon;
+    private ImageView mTitleEndIcon;
     private int mTitleDefaultHorizontalPadding;
     private ImageButton mTitleBackButton;
     private ListMenuButton mTitleMoreButton;
@@ -145,6 +146,7 @@ public class ModalDialogView extends BoundedLinearLayout implements View.OnClick
         mTitleContainer = findViewById(R.id.title_container);
         mTitleView = mTitleContainer.findViewById(R.id.title);
         mTitleIcon = mTitleContainer.findViewById(R.id.title_icon);
+        mTitleEndIcon = mTitleContainer.findViewById(R.id.title_end_icon);
         mTitleDefaultHorizontalPadding = mTitleContainer.getPaddingLeft();
         mTitleBackButton = mTitleContainer.findViewById(R.id.title_back);
         mTitleMoreButton = mTitleContainer.findViewById(R.id.title_more_button);
@@ -275,13 +277,24 @@ public class ModalDialogView extends BoundedLinearLayout implements View.OnClick
         updateContentVisibility();
     }
 
-    /** @param titleScrollable Whether the title is scrollable with the message. */
+    /**
+     * @param drawable The icon drawable on the title at the end.
+     */
+    public void setTitleEndIcon(Drawable drawable) {
+        mTitleEndIcon.setImageDrawable(drawable);
+        updateContentVisibility();
+    }
+
+    /**
+     * @param titleScrollable Whether the title is scrollable with the message.
+     */
     void setTitleScrollable(boolean titleScrollable) {
         if (mTitleScrollable == titleScrollable) return;
 
         mTitleScrollable = titleScrollable;
         CharSequence title = mTitleView.getText();
         Drawable icon = mTitleIcon.getDrawable();
+        Drawable endIcon = mTitleEndIcon.getDrawable();
 
         // Hide the previous title container since the scrollable and non-scrollable title container
         // should not be shown at the same time.
@@ -292,8 +305,10 @@ public class ModalDialogView extends BoundedLinearLayout implements View.OnClick
                         titleScrollable ? R.id.scrollable_title_container : R.id.title_container);
         mTitleView = mTitleContainer.findViewById(R.id.title);
         mTitleIcon = mTitleContainer.findViewById(R.id.title_icon);
+        mTitleEndIcon = mTitleContainer.findViewById(R.id.title_end_icon);
         setTitle(title);
         setTitleIcon(icon);
+        setTitleEndIcon(endIcon);
 
         LayoutParams layoutParams = (LayoutParams) mCustomViewContainer.getLayoutParams();
         if (titleScrollable) {
@@ -683,7 +698,8 @@ public class ModalDialogView extends BoundedLinearLayout implements View.OnClick
     private void updateContentVisibility() {
         boolean titleVisible = !TextUtils.isEmpty(mTitleView.getText());
         boolean titleIconVisible = mTitleIcon.getDrawable() != null;
-        boolean titleContainerVisible = titleVisible || titleIconVisible;
+        boolean titleEndIconVisible = mTitleEndIcon.getDrawable() != null;
+        boolean titleContainerVisible = titleVisible || titleIconVisible || titleEndIconVisible;
         boolean messageParagraphsVisible = mMessageParagraphsContainer.getChildCount() > 0;
         boolean menuItemsVisible = mMenuItemsContainer.getChildCount() > 0;
         boolean multipleParagraphsVisible = mMessageParagraphsContainer.getChildCount() > 1;
@@ -714,6 +730,7 @@ public class ModalDialogView extends BoundedLinearLayout implements View.OnClick
                 mTitleContainer.getPaddingBottom());
         mTitleView.setVisibility(titleVisible ? View.VISIBLE : View.GONE);
         mTitleIcon.setVisibility(titleIconVisible ? View.VISIBLE : View.GONE);
+        mTitleEndIcon.setVisibility(titleEndIconVisible ? View.VISIBLE : View.GONE);
         mTitleContainer.setVisibility(titleContainerVisible ? View.VISIBLE : View.GONE);
         mMessageParagraphsContainer.setVisibility(
                 messageParagraphsVisible ? View.VISIBLE : View.GONE);
