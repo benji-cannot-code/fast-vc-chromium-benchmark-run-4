db4ae3f45bb468cb8605b0656225d95bc7922d47
Aishwarya Rajesh <aishwaryarj@google.com>
2026-01-26 16:06:21 -0800

    [MIR] Update lastAccessedTime persistence condition
    Previously, we always updated lastAccessedTime for an instance when an
    Activity#onTopResumedActivityChanged() signal was received. When two
    Chrome windows receive this callback at the same time where one gains
    focus while the other loses focus, there is no guarantee in which the
    signal is dispatched to both windows thereby creating some ambiguity in
    subsequent ordering of instances by lastAccessedTime. For eg. the
    activity gaining focus may not always be considered the last accessed.
    
    This CL updates this behavior to persist lastAccessedTime only when an
    activity gains focus during this callback. This is based on the
    assumption that the activity losing focus will eventually persist time
    of last access when it is stopped or destroyed, and continue to persist
    time of last resumption or focus as time of last access until then,
    which is a reasonable assumption to make.
    
    Bug: 478254125
    Change-Id: I3831e071c32a7d8ae3911418b57c8252b5b98455
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7518544
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Reviewed-by: Zhe Li <zheliooo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1574864}

--- chrome/android/java/src/org/chromium/chrome/browser/multiwindow/MultiInstanceManagerApi31.java
+++ chrome/android/java/src/org/chromium/chrome/browser/multiwindow/MultiInstanceManagerApi31.java
@@ -1606,7 +1606,9 @@ class MultiInstanceManagerApi31 extends MultiInstanceManagerImpl
     @Override
     public void onTopResumedActivityChanged(boolean isTopResumedActivity) {
         super.onTopResumedActivityChanged(isTopResumedActivity);
-        MultiInstancePersistentStore.writeLastAccessedTime(mInstanceId);
+        if (isTopResumedActivity) {
+            MultiInstancePersistentStore.writeLastAccessedTime(mInstanceId);
+        }
     }
 
     @Override
