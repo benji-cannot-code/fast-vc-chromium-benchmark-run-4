508de63e2bfad125d155b1cbaa14d7bbf28f600c
Solomon Kinard <solomonkinard@chromium.org>
2026-01-27 05:23:32 -0800

    Extensions: Cleanup: Add curly braces as per the style guide
    Bug: 419321439
    Change-Id: Ic6eeda79009f9deb4b9c9c5f6c25e12a3507cce5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7516429
    Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    Reviewed-by: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1575154}

--- chrome/browser/extensions/api/file_system/chrome_file_system_delegate.cc
+++ chrome/browser/extensions/api/file_system/chrome_file_system_delegate.cc
@@ -88,15 +88,17 @@ base::FilePath ChromeFileSystemDelegate::GetDefaultDirectory() {
 base::FilePath ChromeFileSystemDelegate::GetManagedSaveAsDirectory(
     content::BrowserContext* browser_context,
     const Extension& extension) {
-  if (extension.id() != extension_misc::kPdfExtensionId)
+  if (extension.id() != extension_misc::kPdfExtensionId) {
     return base::FilePath();
+  }
 
   ChromeDownloadManagerDelegate* download_manager =
       DownloadCoreServiceFactory::GetForBrowserContext(browser_context)
           ->GetDownloadManagerDelegate();
   DownloadPrefs* download_prefs = download_manager->download_prefs();
-  if (!download_prefs->IsDownloadPathManaged())
+  if (!download_prefs->IsDownloadPathManaged()) {
     return base::FilePath();
+  }
   return download_prefs->DownloadPath();
 }
 
@@ -111,8 +113,9 @@ bool ChromeFileSystemDelegate::ShowSelectFileDialog(
   content::WebContents* web_contents =
       extension_function->GetSenderWebContents();
 
-  if (!web_contents)
+  if (!web_contents) {
     return false;
+  }
 
   // TODO(asargent/benwells) - As a short term remediation for
   // crbug.com/179010 we're adding the ability for an allowlisted extension to
@@ -155,12 +158,15 @@ void ChromeFileSystemDelegate::ConfirmSensitiveDirectoryAccess(
 
 int ChromeFileSystemDelegate::GetDescriptionIdForAcceptType(
     const std::string& accept_type) {
-  if (accept_type == "image/*")
+  if (accept_type == "image/*") {
     return IDS_IMAGE_FILES;
-  if (accept_type == "audio/*")
+  }
+  if (accept_type == "audio/*") {
     return IDS_AUDIO_FILES;
-  if (accept_type == "video/*")
+  }
+  if (accept_type == "video/*") {
     return IDS_VIDEO_FILES;
+  }
   return 0;
 }
 
