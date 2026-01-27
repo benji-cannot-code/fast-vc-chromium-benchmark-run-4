3ad2e97635fe6bfed3c6ae710d1388589228a9b3
Guillaume Jenkins <gujen@google.com>
2026-01-27 11:00:33 -0800

    [iOS] Fix content notifications menu
    It looks like the content notifications settings submenu has never
    worked because its coordinator was not creating the view controller on
    Start.
    
    This CL fixes that by simply allocating and initializing the view
    controller at the beginning of Start.
    
    Bug: 478859433
    Change-Id: I0bea651cc2f7aa9ee551fcded112a80b1ec8ceee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7520727
    Auto-Submit: Guillaume Jenkins <gujen@google.com>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1575381}

--- ios/chrome/browser/settings/ui_bundled/notifications/content_notifications/content_notifications_coordinator.mm
+++ ios/chrome/browser/settings/ui_bundled/notifications/content_notifications/content_notifications_coordinator.mm
@@ -56,6 +56,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)start {
+  ContentNotificationsViewController* viewController =
+      [[ContentNotificationsViewController alloc]
+          initWithStyle:ChromeTableViewStyle()];
+  self.viewController = viewController;
   AuthenticationService* authService =
       AuthenticationServiceFactory::GetForProfile(self.profile);
   id<SystemIdentity> identity =
