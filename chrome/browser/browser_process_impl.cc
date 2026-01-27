e6b05bf95a328e82c398bb7ca94708baf0781cae
YiwenQian <yiwenqian@chromium.org>
2026-01-27 11:30:54 -0800

    [ChromeOS] Integrate ChromeOS Device Authentication in Autofill Payments
    Bug: 472556914
    Change-Id: I0ea780687d8d4e1b72d470745c54a556adef38a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7282017
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Ioana Treib <ioanap@chromium.org>
    Commit-Queue: yiwen qian <yiwenqian@chromium.org>
    Reviewed-by: Istvan Nagy <iscsi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1575411}

--- chrome/browser/browser_process_impl.cc
+++ chrome/browser/browser_process_impl.cc
@@ -177,6 +177,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_CHROMEOS)
 #include "chrome/browser/media_galleries/media_file_system_registry.h"
+#include "components/password_manager/core/common/password_manager_pref_names.h"
 #include "components/soda/soda_installer_impl_chromeos.h"
 #else
 #include "ui/message_center/message_center.h"
@@ -1240,6 +1241,8 @@ void BrowserProcessImpl::RegisterPrefs(PrefRegistrySimple* registry) {
 #if BUILDFLAG(IS_CHROMEOS)
   registry->RegisterStringPref(prefs::kOwnerLocale, std::string());
   registry->RegisterStringPref(prefs::kHardwareKeyboardLayout, std::string());
+  registry->RegisterBooleanPref(
+      password_manager::prefs::kPinAuthenticationAvailableOnChromeOS, false);
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
   registry->RegisterBooleanPref(metrics::prefs::kMetricsReportingEnabled,
--- chrome/browser/device_reauth/chromeos/device_authenticator_chromeos.h
+++ chrome/browser/device_reauth/chromeos/device_authenticator_chromeos.h
@@ -30,6 +30,9 @@ class DeviceAuthenticatorChromeOS : public DeviceAuthenticatorCommon {
 
   void Cancel() override;
 
+  static void CacheIfPinIsAvailable(
+      AuthenticatorChromeOSInterface* authenticator);
+
  private:
   // Records authentication status and executes |callback| with |success|
   // parameter.
