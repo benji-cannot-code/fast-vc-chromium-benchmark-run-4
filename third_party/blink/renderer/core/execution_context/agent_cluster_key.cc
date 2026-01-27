5927e10213709fde30d2587d6aa0304d05a77976
Camille Lamy <clamy@chromium.org>
2026-01-27 08:02:12 -0800

    Add a file version of blink::AgentClusterKey
    This CL adds a file specific version of AgentClusterKey to be used for
    all file URLs, as all files need to be in the same AgentCluster. This is a pure
    refactoring CL with no behavior change.
    
    Bug: 476389699
    Change-Id: Ie391ea6825fb736960c802a1e4e55a49e3d9e673
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7511429
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Camille Lamy <clamy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1575249}

--- third_party/blink/renderer/core/execution_context/agent_cluster_key.cc
+++ third_party/blink/renderer/core/execution_context/agent_cluster_key.cc
@@ -25,6 +25,11 @@ AgentClusterKey AgentClusterKey::CreateOriginKeyed(
       OriginKey{.origin = std::move(origin), .isolation_key = std::nullopt});
 }
 
+// static
+AgentClusterKey AgentClusterKey::CreateUniversalFileAgent() {
+  return AgentClusterKey(File());
+}
+
 AgentClusterKey::CrossOriginIsolationKey::CrossOriginIsolationKey(
     scoped_refptr<const SecurityOrigin> common_origin,
     mojom::blink::CrossOriginIsolationMode mode)
@@ -70,6 +75,10 @@ bool AgentClusterKey::IsOriginKeyed() const {
   return std::holds_alternative<OriginKey>(key_);
 }
 
+bool AgentClusterKey::IsUniversalFileAgent() const {
+  return std::holds_alternative<File>(key_);
+}
+
 const AgentClusterKey::CrossOriginIsolationKey*
 AgentClusterKey::GetCrossOriginIsolationKey() const {
   if (!IsOriginKeyed()) {
@@ -86,6 +95,10 @@ AgentClusterKey::GetCrossOriginIsolationKey() const {
 }
 
 bool AgentClusterKey::operator==(const AgentClusterKey& b) const = default;
+
+bool AgentClusterKey::File::operator==(const AgentClusterKey::File& b) const {
+  return true;
+}
 
 bool AgentClusterKey::Empty::operator==(const AgentClusterKey::Empty& b) const {
   return true;
@@ -111,5 +124,6 @@ AgentClusterKey::AgentClusterKey(const OriginKey& origin_key)
 
 AgentClusterKey::AgentClusterKey(Empty empty) : key_(empty) {}
 AgentClusterKey::AgentClusterKey(Deleted deleted) : key_(deleted) {}
+AgentClusterKey::AgentClusterKey(File file) : key_(file) {}
 
 }  // namespace blink
