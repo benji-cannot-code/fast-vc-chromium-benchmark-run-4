ef317025d63d7ac333437b09d61fc0f2a4728fbf
Tarek Makkouk <tarekmakkouk@google.com>
2026-01-27 08:02:50 -0800

    [iOS] Update Gemini attachment UI on sharing pref change
    Observe the page content sharing preference in BwgBrowserAgent and
    trigger an immediate page context update and UI refresh when it changes.
    This ensures that the floaty attachment chip, as well as the context
    shared with the floaty, correctly reflect the user's setting without
    requiring a restart or navigation.
    
    Fix demo:
    https://drive.google.com/file/d/1hxlAfSNtuwsev3qZFLDb_VogotfOSIgr/view?usp=sharing
    
    Bug: 478280680
    Change-Id: I704e09ca9b53faa601b450e0b1975be606b52005
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7517665
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Reviewed-by: Joemer Ramos <joemerramos@google.com>
    Commit-Queue: Tarek Makkouk <tarekmakkouk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1575250}

--- ios/chrome/browser/intelligence/bwg/model/bwg_browser_agent.h
+++ ios/chrome/browser/intelligence/bwg/model/bwg_browser_agent.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/memory/raw_ptr.h"
 #import "base/time/time.h"
 #import "base/types/expected.h"
+#import "components/prefs/pref_change_registrar.h"
 #import "ios/chrome/browser/fullscreen/ui_bundled/fullscreen_controller.h"
 #import "ios/chrome/browser/fullscreen/ui_bundled/fullscreen_controller_observer.h"
 #import "ios/chrome/browser/intelligence/bwg/model/gemini_tab_helper_observer.h"
@@ -245,6 +246,12 @@ class BwgBrowserAgent : public BrowserUserData<BwgBrowserAgent>,
   // `HideFloatyIfInvoked()` call during parent/child view
   // transitions.
   base::TimeTicks floaty_hidden_timestamp_;
+
+  // Registrar for pref changes.
+  PrefChangeRegistrar pref_change_registrar_;
+
+  // Called when the page content sharing preference changes.
+  void OnPageContentPrefChanged();
 
   // Weak pointer factory.
   base::WeakPtrFactory<BwgBrowserAgent> weak_factory_{this};
