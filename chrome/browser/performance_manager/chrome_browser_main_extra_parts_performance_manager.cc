3459c9b4e04e77485aa5cc2a938ab27eb1f3ab1b
Andres Ricardo Perez <andresrperez@google.com>
2026-01-26 16:36:30 -0800

    Implement PriorityBoostLoadingBrowserNetworkPolicy
    This policy disables boosting for all processes on startup, except for
    the browser process. Additionally, when a renderer is in a loading
    state, it's boosting will be re-enabled. For the network process, its
    boosting will be enabled if there is at least one renderer loading, and
    disabled if there are none.
    
    The previous iteration of this experiment showed interesting gains, but
    also had a regression in BrowserResponsiveness. Originally the approach
    to mitigate this was to wait until the first load for renderers and then
    disable boosting, but the obtained results were similar. This led to the
    selection of this approach to scope the effect instead of leaving it set
    forever, and also treat the network and browser processes differently.
    
    Bug: 395914024, 465728967
    Change-Id: I5c54f83ba9b8a43b9d08f494b193db873f14fbd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7270508
    Reviewed-by: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Commit-Queue: Andres Ricardo Perez <andresrperez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1574880}

--- chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.cc
+++ chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.cc
@@ -99,6 +99,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "chrome/browser/performance_manager/policies/priority_boost_browser_network_policy.h"
 #include "chrome/browser/performance_manager/policies/priority_boost_gpu_browser_network_policy.h"
+#include "chrome/browser/performance_manager/policies/priority_boost_loading_browser_network_policy.h"
 #endif
 
 namespace {
@@ -217,6 +218,11 @@ void ChromeBrowserMainExtraPartsPerformanceManager::CreatePoliciesAndDecorators(
         graph->PassToGraph(
             std::make_unique<performance_manager::policies::
                                  PriorityBoostGpuBrowserNetworkPolicy>());
+        break;
+      case features::DisableBoostPriorityExemption::kLoadingBrowserNetwork:
+        graph->PassToGraph(
+            std::make_unique<performance_manager::policies::
+                                 PriorityBoostLoadingBrowserNetworkPolicy>());
         break;
     }
   }
