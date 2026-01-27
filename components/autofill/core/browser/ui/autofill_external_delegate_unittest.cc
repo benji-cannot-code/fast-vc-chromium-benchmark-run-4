aa9998cfce98aeec7a7a713d4eb291e0374f290a
Jan Keitel <jkeitel@google.com>
2026-01-27 06:09:03 -0800

    Make MockAutofillComposeDelegate a NiceMock
    Less chatter during test execution.
    
    Bug: 40100455
    Change-Id: I01463291b88b49a948bc34a799738fb24442d0e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7520485
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1575180}

--- components/autofill/core/browser/ui/autofill_external_delegate_unittest.cc
+++ components/autofill/core/browser/ui/autofill_external_delegate_unittest.cc
@@ -1880,7 +1880,7 @@ TEST_F(AutofillExternalDelegateTest,
                         Field(&AutofillClient::PopupOpenArgs::anchor_type,
                               PopupAnchorType::kCaret)),
                   _));
-  MockAutofillComposeDelegate compose_delegate;
+  NiceMock<MockAutofillComposeDelegate> compose_delegate;
   ON_CALL(autofill_client(), GetComposeDelegate)
       .WillByDefault(Return(&compose_delegate));
   ON_CALL(compose_delegate, ShouldAnchorNudgeOnCaret)
@@ -1920,7 +1920,7 @@ TEST_F(
       ShowAutofillSuggestions(
           Field(&AutofillClient::PopupOpenArgs::element_bounds, field_bounds),
           _));
-  MockAutofillComposeDelegate compose_delegate;
+  NiceMock<MockAutofillComposeDelegate> compose_delegate;
   ON_CALL(autofill_client(), GetComposeDelegate)
       .WillByDefault(Return(&compose_delegate));
   ON_CALL(compose_delegate, ShouldAnchorNudgeOnCaret)
@@ -1959,7 +1959,7 @@ TEST_F(
       ShowAutofillSuggestions(
           Field(&AutofillClient::PopupOpenArgs::element_bounds, field_bounds),
           _));
-  MockAutofillComposeDelegate compose_delegate;
+  NiceMock<MockAutofillComposeDelegate> compose_delegate;
   ON_CALL(autofill_client(), GetComposeDelegate)
       .WillByDefault(Return(&compose_delegate));
   ON_CALL(compose_delegate, ShouldAnchorNudgeOnCaret)
@@ -1988,7 +1988,7 @@ TEST_F(
                         Field(&AutofillClient::PopupOpenArgs::anchor_type,
                               default_anchor_type)),
                   _));
-  MockAutofillComposeDelegate compose_delegate;
+  NiceMock<MockAutofillComposeDelegate> compose_delegate;
   ON_CALL(autofill_client(), GetComposeDelegate)
       .WillByDefault(Return(&compose_delegate));
   ON_CALL(compose_delegate, ShouldAnchorNudgeOnCaret)
@@ -2002,7 +2002,7 @@ TEST_F(
 // Tests that accepting a Compose suggestion returns a callback that, when run,
 // fills the trigger field.
 TEST_F(AutofillExternalDelegateTest, ExternalDelegateOpensComposeAndFills) {
-  MockAutofillComposeDelegate compose_delegate;
+  NiceMock<MockAutofillComposeDelegate> compose_delegate;
   ON_CALL(autofill_client(), GetComposeDelegate)
       .WillByDefault(Return(&compose_delegate));
 
@@ -2031,7 +2031,7 @@ TEST_F(AutofillExternalDelegateTest, ExternalDelegateOpensComposeAndFills) {
 
 TEST_F(AutofillExternalDelegateTest,
        Compose_AcceptDisable_CallsComposeDelegate) {
-  MockAutofillComposeDelegate compose_delegate;
+  NiceMock<MockAutofillComposeDelegate> compose_delegate;
   ON_CALL(autofill_client(), GetComposeDelegate)
       .WillByDefault(Return(&compose_delegate));
 
@@ -2047,7 +2047,7 @@ TEST_F(AutofillExternalDelegateTest,
 
 TEST_F(AutofillExternalDelegateTest,
        Compose_AcceptGoToSettings_CallsComposeDelegate) {
-  MockAutofillComposeDelegate compose_delegate;
+  NiceMock<MockAutofillComposeDelegate> compose_delegate;
   ON_CALL(autofill_client(), GetComposeDelegate)
       .WillByDefault(Return(&compose_delegate));
 
@@ -2109,7 +2109,7 @@ TEST_F(AutofillExternalDelegateTest, SaveAndFillMetrics_SuggestionAccepted) {
 
 TEST_F(AutofillExternalDelegateTest,
        Compose_AcceptNeverShowOnThisWebsiteAgain_CallsComposeDelegate) {
-  MockAutofillComposeDelegate compose_delegate;
+  NiceMock<MockAutofillComposeDelegate> compose_delegate;
   ON_CALL(autofill_client(), GetComposeDelegate)
       .WillByDefault(Return(&compose_delegate));
 
