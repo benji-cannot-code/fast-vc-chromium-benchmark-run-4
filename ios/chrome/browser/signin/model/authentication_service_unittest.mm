adcc201c2411c5f72fa9dd59c2c8c5733e493751
Arthur Milchior <arthurmilchior@google.com>
2026-01-27 03:02:47 -0800

    [iOS]HandleForgottenIdentity switch to personal profile
    It currently directly signs-out, which should not actually occurs if the
    account is managed.
    
    That changes requires the AuthenticationService to handle forgotten
    identities asynchronously, as the account switch uses the
    AuthenticationService, which is not yet properly initialized.
    
    This CL is part of the following chain:
    + crrev.com/c/7510877
    - crrev.com/i/8961376
    - crrev.com/c/7512345
    
    Bug: 475206215
    Change-Id: Ic18f3954c3d3720f501ca99a2a48b90e6ac75d93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7510877
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1575099}

--- ios/chrome/browser/signin/model/authentication_service_unittest.mm
+++ ios/chrome/browser/signin/model/authentication_service_unittest.mm
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/test/bind.h"
 #import "base/test/gtest_util.h"
 #import "base/test/metrics/histogram_tester.h"
+#import "base/test/run_until.h"
 #import "base/test/scoped_feature_list.h"
 #import "base/test/test_future.h"
 #import "components/keyed_service/core/service_access_type.h"
@@ -138,6 +139,17 @@ class AuthenticationServiceTestBase : public PlatformTest {
 
     // Force explicit instantiation of the AuthenticationService.
     std::ignore = authentication_service();
+  }
+
+  // Wait until started tasks are executed and expects the absence of primary
+  // identity.
+  void WaitForPrimaryIdentityToBeRemoved() {
+    base::RunLoop run_loop;
+    task_environment_.GetMainThreadTaskRunner()->PostTask(
+        FROM_HERE, run_loop.QuitClosure());
+    run_loop.Run();
+    EXPECT_FALSE(authentication_service()->HasPrimaryIdentity(
+        signin::ConsentLevel::kSignin));
   }
 
   std::unique_ptr<sync_preferences::PrefServiceSyncable> CreatePrefService() {
@@ -368,6 +380,7 @@ TEST_P(AuthenticationServiceTest, TestHandleForgottenIdentityNoPromptSignIn) {
   fake_system_identity_manager()->ForgetIdentity(
       identity(0), identity_forgotten.GetCallback());
   ASSERT_TRUE(identity_forgotten.Wait());
+  WaitForPrimaryIdentityToBeRemoved();
 
   // User is signed out (no corresponding identity), but not prompted for sign
   // in (as the action was user initiated).
@@ -388,6 +401,13 @@ TEST_P(AuthenticationServiceTest, TestHandleForgottenIdentityPromptSignIn) {
   // identity.
   fake_system_identity_manager()->ForgetIdentityFromOtherApplication(
       identity(0));
+  WaitForPrimaryIdentityToBeRemoved();
+  base::RunLoop run_loop;
+  task_environment_.GetMainThreadTaskRunner()->PostTask(FROM_HERE,
+                                                        run_loop.QuitClosure());
+  run_loop.Run();
+  EXPECT_FALSE(authentication_service()->HasPrimaryIdentity(
+      signin::ConsentLevel::kSignin));
 
   // User is signed out (no corresponding identity), and reauth prompt is set.
   EXPECT_FALSE(authentication_service()->HasPrimaryIdentity(
@@ -448,6 +468,7 @@ TEST_P(AuthenticationServiceTest, HasPrimaryIdentityBackground) {
   fake_system_identity_manager()->ForgetIdentity(
       identity(0), identity_forgotten.GetCallback());
   ASSERT_TRUE(identity_forgotten.Wait());
+  WaitForPrimaryIdentityToBeRemoved();
 
   EXPECT_FALSE(authentication_service()->HasPrimaryIdentity(
       signin::ConsentLevel::kSignin));
@@ -824,6 +845,11 @@ TEST_P(AuthenticationServiceTest, TestHandleRestrictedIdentityPromptSignIn) {
 
   // Set the account restriction.
   SetPattern("foo");
+  WaitForPrimaryIdentityToBeRemoved();
+  base::RunLoop run_loop;
+  task_environment_.GetMainThreadTaskRunner()->PostTask(FROM_HERE,
+                                                        run_loop.QuitClosure());
+  run_loop.Run();
   EXPECT_FALSE(account_manager_->HasIdentities());
 
   // User is signed out (no corresponding identity), and reauth prompt is set.
