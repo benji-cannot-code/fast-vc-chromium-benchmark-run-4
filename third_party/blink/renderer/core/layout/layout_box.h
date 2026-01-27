e52f024facfdad7b3f9b41f4e3f85feed7dcca70
Ian Kilpatrick <ikilpatrick@chromium.org>
2026-01-27 06:45:32 -0800

    [layout] Make IsEligibleFor*Containment virtual
    This removes a dependency on IsAtomicInlineLevel, and arguably makes the
    function easier to reason about.
    
    Previously there were lots of boolean logic magic happening, just make
    "virtual"[1]
    
    [1] But in reality will store the result on the vtable due to virtual
    constant propagation.
    
    Bug: 41227823
    Change-Id: I237e29c013c66d06c7b9ab2fb2f499b9ee8282f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7517991
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1575197}

--- third_party/blink/renderer/core/layout/layout_box.h
+++ third_party/blink/renderer/core/layout/layout_box.h
@@ -1237,6 +1237,16 @@ class CORE_EXPORT LayoutBox : public LayoutBoxModelObject {
  protected:
   ~LayoutBox() override;
 
+  bool IsEligibleForPaintOrLayoutContainment() const override {
+    NOT_DESTROYED();
+    return true;
+  }
+
+  bool IsEligibleForSizeContainment() const override {
+    NOT_DESTROYED();
+    return true;
+  }
+
   virtual OverflowClipAxes ComputeOverflowClipAxes() const;
 
   void WillBeDestroyed() override;
