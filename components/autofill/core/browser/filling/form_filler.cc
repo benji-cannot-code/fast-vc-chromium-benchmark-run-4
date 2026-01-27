b15ed2b93b5a39d9bfcddcf072200326152903b6
Piotr Kotynia <piotrkotynia@google.com>
2026-01-27 09:10:59 -0800

    [Autofill] Remove kAutofillSupportPresentationRole kill switch
    Since there were no regressions or reports, it is safe to remove the
    kill switch and IsFocusable() effectively becomes is_focusable().
    
    Bug: 444754999
    Change-Id: If2ea161a780ebea8276be8c96d5d4313589fa687
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7517104
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Piotr Kotynia <piotrkotynia@google.com>
    Cr-Commit-Position: refs/heads/main@{#1575291}

--- components/autofill/core/browser/filling/form_filler.cc
+++ components/autofill/core/browser/filling/form_filler.cc
@@ -581,7 +581,7 @@ DenseSet<FieldFillingSkipReason> FormFiller::GetFillingSkipReasonsForField(
 
   // Don't fill unfocusable fields, with the exception of <select> fields, for
   // the sake of filling the synthetic fields.
-  add_if(!autofill_field.IsFocusable() && !autofill_field.IsSelectElement(),
+  add_if(!autofill_field.is_focusable() && !autofill_field.IsSelectElement(),
          FieldFillingSkipReason::kInvisibleField);
 
   // Do not fill fields that have been edited by the user, except if the field
@@ -918,7 +918,7 @@ void FormFiller::FillOrPreviewForm(
         FieldFillingSkipReason::kNotFocused};
     if (!kPreUkmLoggingSkips.contains_any(
             skip_reasons[autofill_field.global_id()]) &&
-        !autofill_field.IsFocusable()) {
+        !autofill_field.is_focusable()) {
       manager_->client()
           .GetFormInteractionsUkmLogger()
           .LogHiddenRepresentationalFieldSkipDecision(
@@ -1244,7 +1244,7 @@ void FormFiller::TriggerRefill(const FormData& form,
       [&](const std::unique_ptr<AutofillField>& field) {
         return std::make_tuple(
             field->origin() == refill_context->filled_origin,
-            field->IsFocusable(),
+            field->is_focusable(),
             field->global_id() == refill_context->filled_field_id,
             field->GetFieldSignature() ==
                 refill_context->filled_field_signature,
