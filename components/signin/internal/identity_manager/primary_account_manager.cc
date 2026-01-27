dca0d978c10bf677f646a9e16d94bdd7cc9c6c16
Ankush Singh <ankushkush@google.com>
2026-01-27 07:45:40 -0800

    Add metrics to track if the user has undergone sync-to-signin migration
    This adds a new metric Sync.SyncToSigninMigration.MigrationType that
    logs whether or not the user has undergone a migration and if so,
    whether they were went through the forced migration.
    
    Bug: 392063410
    Change-Id: I8fe0b89f9713baa7d30ed4111c3a57f3e1e6b3b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7506390
    Auto-Submit: Ankush Singh <ankushkush@google.com>
    Reviewed-by: Menghan Yang <myuu@google.com>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1575233}

--- components/signin/internal/identity_manager/primary_account_manager.cc
+++ components/signin/internal/identity_manager/primary_account_manager.cc
@@ -321,6 +321,8 @@ void PrimaryAccountManager::RegisterProfilePrefs(PrefRegistrySimple* registry) {
       prefs::kGoogleServicesSyncingGaiaIdMigratedToSignedIn, std::string());
   registry->RegisterStringPref(
       prefs::kGoogleServicesSyncingUsernameMigratedToSignedIn, std::string());
+  registry->RegisterIntegerPref(prefs::kGoogleServicesSyncingUserMigrationType,
+                                /*SyncToSigninMigrationType::kUnknown=*/0);
   registry->RegisterBooleanPref(prefs::kSigninAllowed, true);
   registry->RegisterBooleanPref(prefs::kSignedInWithCredentialProvider, false);
   registry->RegisterBooleanPref(kExplicitBrowserSigninWithoutFeatureEnabled,
@@ -565,6 +567,8 @@ void PrimaryAccountManager::SetPrimaryAccountInternal(
         prefs::kGoogleServicesSyncingGaiaIdMigratedToSignedIn);
     scoped_pref_commit.ClearPref(
         prefs::kGoogleServicesSyncingUsernameMigratedToSignedIn);
+    scoped_pref_commit.ClearPref(
+        prefs::kGoogleServicesSyncingUserMigrationType);
   } else {
     scoped_pref_commit.SetString(prefs::kGoogleServicesLastSignedInUsername,
                                  account_info.email);
