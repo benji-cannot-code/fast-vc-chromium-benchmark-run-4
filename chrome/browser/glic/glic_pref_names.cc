77782657a88c4a725c0fa2e06846ed03f0ea9b40
Anthony Cui <cuianthony@google.com>
2026-01-27 12:58:49 -0800

    Implement chrome://flags override of glic guest url
    Preceding change: https://crrev.com/c/7510475
    Writes values configured through command line to profile prefs, and maps stored pref values to chrome://flags FeatureParams to allow easy selection of Bluebird environment.
    
    Bug: b:474235074
    Change-Id: Ib95416042c81115702531e28e7c104b4df8c6709
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7514573
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Anthony Cui <cuianthony@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1575467}

--- chrome/browser/glic/glic_pref_names.cc
+++ chrome/browser/glic/glic_pref_names.cc
@@ -69,6 +69,10 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
   registry->RegisterListPref(prefs::kGlicActuationOnWebBlockedForURLs);
 
   registry->RegisterBooleanPref(prefs::kGlicUserEnabledActuationOnWeb, false);
+
+  registry->RegisterStringPref(prefs::kGlicGuestUrlPresetAutopush, "");
+  registry->RegisterStringPref(prefs::kGlicGuestUrlPresetPreprod, "");
+  registry->RegisterStringPref(prefs::kGlicGuestUrlPresetProd, "");
 }
 
 void RegisterLocalStatePrefs(PrefRegistrySimple* registry) {
--- chrome/browser/glic/host/glic_ui.cc
+++ chrome/browser/glic/host/glic_ui.cc
@@ -175,8 +175,10 @@ GlicUI::GlicUI(content::WebUI* web_ui)
   source->AddBoolean("loggingEnabled",
                      command_line->HasSwitch(::switches::kGlicHostLogging));
 
+  auto* profile = Profile::FromBrowserContext(browser_context);
+
   // Set up guest URL via cli flag or default to finch param value.
-  const GURL guest_url = GetGuestURL();
+  const GURL guest_url = GetGuestURL(profile);
   source->AddString("glicGuestURL", guest_url.spec());
   net_log::LogDummyNetworkRequestForTrafficAnnotation(guest_url,
                                                       net_log::GlicPage::kGlic);
@@ -197,8 +199,6 @@ GlicUI::GlicUI(content::WebUI* web_ui)
   if (allowed_origins.empty()) {
     allowed_origins = features::kGlicAllowedOriginsOverride.Get();
   }
-
-  auto* profile = Profile::FromBrowserContext(browser_context);
 
   // Allow corp origins for @google accounts.
   signin::IdentityManager* identity_manager =
