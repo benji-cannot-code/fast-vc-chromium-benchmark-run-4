196cd0bb00e1c5280150728f4bbf2e22c4c31985
Brandon Fong <bjfong@google.com>
2026-01-27 13:25:08 -0800

    [tips] Add render tests for tips notifications opt in promo
    Add render tests for the opt in promo sheet for tips notifications.
    
    Bug: 461543972
    Change-Id: I202a040da8db5fd8955af38b0d4df0987e2cf4e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7522124
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1575487}

--- chrome/android/javatests/src/org/chromium/chrome/browser/notifications/TipsNotificationsOptInPromoTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/notifications/TipsNotificationsOptInPromoTest.java
@@ -21,6 +21,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.provider.Settings;
 import android.util.Pair;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.test.core.app.ApplicationProvider;
@@ -33,24 +34,27 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.Batch;
-import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.IntentHandler;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
-import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.notifications.channels.ChromeChannelDefinitions;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.transit.ChromeTransitTestRules;
 import org.chromium.chrome.test.transit.FreshCtaTransitTestRule;
 import org.chromium.chrome.test.transit.notifications.TipsOptInBottomSheetFacility;
 import org.chromium.chrome.test.transit.ntp.RegularNewTabPageStation;
+import org.chromium.chrome.test.util.ChromeRenderTestRule;
 import org.chromium.ui.test.util.DeviceRestriction;
+import org.chromium.ui.test.util.RenderTestRule.Component;
 import org.chromium.ui.widget.ButtonCompat;
 
-/** Integration tests for the tips notifications opt in promo. */
+import java.io.IOException;
+
+// TODO(crbug.com/478907175): Remove casting when value returns the view type.
+/** Integration and render tests for the tips notifications opt in promo. */
 @RunWith(ChromeJUnit4ClassRunner.class)
-@CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
 @EnableFeatures({ChromeFeatureList.ANDROID_TIPS_NOTIFICATIONS + ":always_show_opt_in_promo/true"})
 @Batch(Batch.PER_CLASS)
 @Restriction(DeviceRestriction.RESTRICTION_TYPE_NON_AUTO)
@@ -58,6 +62,13 @@ public class TipsNotificationsOptInPromoTest {
     @Rule
     public FreshCtaTransitTestRule mCtaTestRule =
             ChromeTransitTestRules.freshChromeTabbedActivityRule();
+
+    @Rule
+    public ChromeRenderTestRule mRenderTestRule =
+            ChromeRenderTestRule.Builder.withPublicCorpus()
+                    .setRevision(1)
+                    .setBugComponent(Component.UI_NOTIFICATIONS)
+                    .build();
 
     private Context mContext;
 
@@ -68,10 +79,14 @@ public class TipsNotificationsOptInPromoTest {
 
     @Test
     @MediumTest
-    public void testOptInBottomSheetDismiss() {
+    @Feature({"RenderTest"})
+    public void testOptInBottomSheetDismiss() throws IOException {
         var tripResult = showOptInBottomSheet();
         TipsOptInBottomSheetFacility bottomSheet = tripResult.first;
         RegularNewTabPageStation openedNtp = tripResult.second;
+
+        mRenderTestRule.render(
+                ((View) bottomSheet.bottomSheetElement.value()), "opt_in_bottom_sheet");
 
         // Check that backpress dismisses the opt in bottom sheet.
         bottomSheet.dismiss();
