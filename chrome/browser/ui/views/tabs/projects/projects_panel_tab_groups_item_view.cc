fbfc7e1a70abbb651967001b5acbccdeebcd438e
David M <davidjm@google.com>
2026-01-27 14:26:03 -0800

    Add accessibility identifier to ProjectsPanelTabGroupsItemView
    To prevent built failures when DCHECKs are on
    
    Bug: 475303589
    Change-Id: I6db38ed001c7f798af261b5eff4b09c44ba3113f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7523734
    Reviewed-by: Chase Hartsell <chazzy@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1575517}

--- chrome/browser/ui/views/tabs/projects/projects_panel_tab_groups_item_view.cc
+++ chrome/browser/ui/views/tabs/projects/projects_panel_tab_groups_item_view.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/color/color_provider.h"
 #include "ui/gfx/vector_icon_types.h"
+#include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/controls/image_view.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/layout/box_layout.h"
@@ -87,6 +88,7 @@ ProjectsPanelTabGroupsItemView::ProjectsPanelTabGroupsItemView(
   SetProperty(views::kMarginsKey, kTabGroupsItemMargins);
   SetProperty(views::kElementIdentifierKey,
               kProjectsPanelTabGroupsItemViewElementId);
+  GetViewAccessibility().SetName(group.title());
 }
 
 ProjectsPanelTabGroupsItemView::~ProjectsPanelTabGroupsItemView() = default;
