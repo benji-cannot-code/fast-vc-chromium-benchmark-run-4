ccebf22155ec39ff59200bd7d2abf7a59da6514b
Evan Stade <evanstade@microsoft.com>
2026-01-27 15:00:08 -0800

    IDB: SQLite store - basic backing store migration
    Add code that can clone one BackingStore::Database into another. Since
    this uses the BackingStore API, it can be used across any two
    BackingStore types, but practically speaking it will useful for
    migrating LevelDB stores to SQLite stores.
    
    For now this is lightly tested by piggy-backing on
    `BackingStoreTests.Snapshot`. Verification is accomplished via database
    snapshotting, which itself is not very comprehensively tested yet.
    Future testing should probably be focused at the browser test level and
    creating large, real database states to verify against.
    
    TODO:
    * much more comprehensive testing
    * support for blobs
    * error handling
    * performance and success metrics
    * actually lighting up the code (behind a flag)
    
    Bug: 419264073, 419208485
    Change-Id: Ic2009d7e3b8db02402f2b5599383f77f5a4e5629
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7509489
    Commit-Queue: Evan Stade <evanstade@microsoft.com>
    Reviewed-by: Evan Stade <evanstade@microsoft.com>
    Reviewed-by: Abhishek Shanthkumar <abhishek.shanthkumar@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1575546}

--- content/browser/indexed_db/instance/backing_store_test_base.h
+++ content/browser/indexed_db/instance/backing_store_test_base.h
@@ -39,6 +39,10 @@ class BackingStoreTestBase : public testing::Test {
 
   void CreateFactoryAndBackingStore();
 
+  std::unique_ptr<BucketContext> CreateBucketContext(
+      bool use_sqlite,
+      const base::FilePath& data_path);
+
   void UpdateDatabaseVersion(indexed_db::BackingStore::Database& db,
                              int64_t version);
 
@@ -56,6 +60,9 @@ class BackingStoreTestBase : public testing::Test {
   std::vector<PartitionedLock> CreateDummyLock();
 
   void DestroyFactoryAndBackingStore();
+
+  // Clones the give database and attempts to verify the cloning worked.
+  void VerifyClone(BackingStore::Database& db);
 
   BackingStore* backing_store();
 
@@ -94,7 +101,7 @@ class BackingStoreTestBase : public testing::Test {
   raw_ptr<BackingStore> backing_store_ = nullptr;
 
  private:
-  base::AutoReset<std::optional<bool>> sqlite_override_;
+  bool use_sqlite_;
 };
 
 class BackingStoreWithExternalObjectsTestBase : public BackingStoreTestBase {
