7a48271988d05ce2603a617f0a0c478e581986c6
Calder Kitagawa <ckitagawa@chromium.org>
2026-01-28 07:59:24 -0800

    Ignore headless tab models for glic
    Headless tab models will never have live tabs, be focused, or have web
    contents. We can ignore them.
    
    Bug: 478890184
    Change-Id: Ib710769e5b88e0956e4b03c9edf5247655f8c72f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7527126
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1575948}

--- chrome/browser/glic/common/glic_tab_observer_android.cc
+++ chrome/browser/glic/common/glic_tab_observer_android.cc
@@ -109,7 +109,8 @@ GlicTabObserverAndroid::~GlicTabObserverAndroid() {
 }
 
 void GlicTabObserverAndroid::OnTabModelAdded(TabModel* model) {
-  if (model->GetProfile() != profile_) {
+  if (model->GetProfile() != profile_ ||
+      model->GetTabModelType() != TabModel::TabModelType::kStandard) {
     return;
   }
 
