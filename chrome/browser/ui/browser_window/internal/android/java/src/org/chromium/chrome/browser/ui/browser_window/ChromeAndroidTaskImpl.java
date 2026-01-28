e12ec2d20c30d63192a35310ccfc913fa59f37dc
Jack Thiesen <jthiesen@chromium.org>
2026-01-28 08:46:10 -0800

    ChromeAndroidTaskImpl#mLastActivatedTimeMillis Has Initial Value
    Browser tests that utilize BrowserWindowInterfaceIterator were crashing
    due to mLastActivatedTimeMillis being null. This change fixes this by
    setting the value of this variable during native initialization.
    
    Bug: 478905712
    Change-Id: I547b5bbf6f1767d3d1b40e3391ff716ab9d5b8d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7524646
    Reviewed-by: Linyu He <linyuh@google.com>
    Commit-Queue: Jack Thiesen <jthiesen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1575980}

--- chrome/browser/ui/browser_window/internal/android/java/src/org/chromium/chrome/browser/ui/browser_window/ChromeAndroidTaskImpl.java
+++ chrome/browser/ui/browser_window/internal/android/java/src/org/chromium/chrome/browser/ui/browser_window/ChromeAndroidTaskImpl.java
@@ -435,6 +435,7 @@ final class ChromeAndroidTaskImpl
         @Nullable Rect futureBounds = mPendingActionManager.getFutureBoundsInDp();
         @Nullable Rect futureRestoredBounds = mPendingActionManager.getFutureRestoredBoundsInDp();
         mState = State.IDLE;
+        setLastActivatedTimeMillis();
         dispatchPendingActions(topActivityScopedObjects, futureBounds, futureRestoredBounds);
 
         JniOnceCallback<Long> taskCreationCallbackForNative =
@@ -655,6 +656,10 @@ final class ChromeAndroidTaskImpl
                     return DisplayUtil.scaleToEnclosingRect(restoredBoundsInPx, 1.0f / dipScale);
                 },
                 /* defaultValue= */ new Rect());
+    }
+
+    private void setLastActivatedTimeMillis() {
+        mLastActivatedTimeMillis = TimeUtils.elapsedRealtimeMillis();
     }
 
     @Override
@@ -889,7 +894,7 @@ final class ChromeAndroidTaskImpl
         ThreadUtils.assertOnUiThread();
 
         if (isTopResumedActivity) {
-            mLastActivatedTimeMillis = TimeUtils.elapsedRealtimeMillis();
+            setLastActivatedTimeMillis();
             if (mShouldDispatchPendingDeactivate) {
                 ChromeAndroidTaskTrackerImpl.getInstance().activatePenultimatelyActivatedTask();
                 mShouldDispatchPendingDeactivate = false;
