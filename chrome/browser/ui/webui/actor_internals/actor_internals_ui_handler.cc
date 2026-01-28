2bac6034a7a1218bd222f47a6773fe29aa888eb3
Dave Tapuska <dtapuska@chromium.org>
2026-01-28 08:19:33 -0800

    [actor] Check policy for debugging page
    Do not allow the file selection dialog if the preference isn't allowed.
    
    Bug: 478774028
    Change-Id: Ife2f2e14c430cd7acf9b56b08e978cab82106bcd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7522025
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1575962}

--- chrome/browser/ui/webui/actor_internals/actor_internals_ui_handler.cc
+++ chrome/browser/ui/webui/actor_internals/actor_internals_ui_handler.cc
@@ -14,9 +14,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/actor/actor_keyed_service.h"
 #include "chrome/browser/actor/aggregated_journal.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/actor/journal_details_builder.h"
 #include "chrome/common/chrome_paths.h"
+#include "chrome/common/pref_names.h"
+#include "components/prefs/pref_service.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
 #include "third_party/abseil-cpp/absl/strings/str_format.h"
@@ -89,6 +92,11 @@ void ActorInternalsUIHandler::WillAddJournalEntry(
 void ActorInternalsUIHandler::StartLogging() {
   if (select_file_dialog_) {
     return;  // Currently running, wait for existing save to complete.
+  }
+
+  PrefService* local_state = g_browser_process->local_state();
+  if (!local_state->GetBoolean(prefs::kAllowFileSelectionDialogs)) {
+    return;
   }
 
   base::FilePath default_file;
