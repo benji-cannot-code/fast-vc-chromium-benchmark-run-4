8da92fccc3b2ca3e2fde7bab731c27cc261488d5
Avi Drissman <avi@chromium.org>
2026-01-27 16:08:08 -0800

    Switch to using optional primitive types in Mojo
    This switches over the primitive types in remoting.
    
    Bug: 40489779
    Change-Id: I28a5a25e95772b120e83382669ecf4a59767f254
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7522986
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1575586}

--- remoting/host/mojom/remoting_mojom_traits.h
+++ remoting/host/mojom/remoting_mojom_traits.h
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/host/mojom/desktop_session.mojom-shared.h"
 #include "remoting/host/mojom/remoting_host.mojom-shared.h"
 #include "remoting/host/mojom/webrtc_types.mojom-shared.h"
-#include "remoting/host/mojom/wrapped_primitives.mojom-shared.h"
 #include "remoting/proto/audio.pb.h"
 #include "remoting/proto/control.pb.h"
 #include "remoting/proto/coordinates.pb.h"
@@ -47,52 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/mojom/geometry_mojom_traits.h"
 
 namespace mojo {
-
-template <>
-class StructTraits<remoting::mojom::BoolDataView, bool> {
- public:
-  static bool value(bool value) { return value; }
-
-  static bool Read(remoting::mojom::BoolDataView data_view, bool* out_value) {
-    *out_value = data_view.value();
-    return true;
-  }
-};
-
-template <>
-class StructTraits<remoting::mojom::FloatDataView, float> {
- public:
-  static float value(float value) { return value; }
-
-  static bool Read(remoting::mojom::FloatDataView data_view, float* out_value) {
-    *out_value = data_view.value();
-    return true;
-  }
-};
-
-template <>
-class StructTraits<remoting::mojom::Int32DataView, int32_t> {
- public:
-  static int32_t value(int32_t value) { return value; }
-
-  static bool Read(remoting::mojom::Int32DataView data_view,
-                   int32_t* out_value) {
-    *out_value = data_view.value();
-    return true;
-  }
-};
-
-template <>
-class StructTraits<remoting::mojom::UInt32DataView, uint32_t> {
- public:
-  static uint32_t value(uint32_t value) { return value; }
-
-  static bool Read(remoting::mojom::UInt32DataView data_view,
-                   uint32_t* out_value) {
-    *out_value = data_view.value();
-    return true;
-  }
-};
 
 template <>
 class StructTraits<remoting::mojom::DesktopCaptureOptionsDataView,
