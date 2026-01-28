cd6ee1babb372cc1e1a4f9e5ab44044f0d6316c0
James Lee <ljjlee@google.com>
2026-01-28 05:21:40 -0800

    Reland "Enable Taiyaki by default on iOS"
    This reverts commit 68473ab44d2955c2c588225f834aa15e09e3ebfd.
    
    Reason for revert: updated failing tests.
    
    Original change's description:
    > Revert "Enable Taiyaki by default on iOS"
    >
    > This reverts commit 5d5ff51447559a5847f91a88efa2736458814296.
    >
    > Reason for revert:
    > LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5652482488270848
    >
    > Sample build with failed test: https://ci.chromium.org/b/8691596850303167457
    > Affected test(s):
    > [://ios/chrome/test/earl_grey2\:ios_chrome_ui_eg2tests_module!xctest::SearchEngineChoiceTaiyakiTestCase#testTaiyakiSearchEngineChoiceScreen](https://ci.chromium.org/ui/test/chromium/:%2F%2Fios%2Fchrome%2Ftest%2Fearl_grey2%5C:ios_chrome_ui_eg2tests_module%21xctest::SearchEngineChoiceTaiyakiTestCase%23testTaiyakiSearchEngineChoiceScreen?q=VHash%3Abb18eb69160a71af)
    > [://ios/chrome/test/earl_grey2\:ios_chrome_ui_eg2tests_module!xctest::SearchEngineChoiceTaiyakiTestCase#testTaiyakiSearchEngineChoiceScreen](https://ci.chromium.org/ui/test/chromium/:%2F%2Fios%2Fchrome%2Ftest%2Fearl_grey2%5C:ios_chrome_ui_eg2tests_module%21xctest::SearchEngineChoiceTaiyakiTestCase%23testTaiyakiSearchEngineChoiceScreen?q=VHash%3Ae65a2513aae56189)
    >
    > If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5652482488270848&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F7516704&type=BUG
    >
    > Original change's description:
    > > Enable Taiyaki by default on iOS
    > >
    > > This enables the Taiyaki flag by default on iOS, with
    > > choice_screen_surface set to fre_only, as well as the
    > > CurrentDseHighlightOnChoiceScreenSupport flag.
    > >
    > > Bug: 434155606
    > > Change-Id: I8ebba5c3ff49c8797f0e420adf65723477a0fd3a
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7516704
    > > Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    > > Auto-Submit: James Lee <ljjlee@google.com>
    > > Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1574546}
    > >
    >
    > Bug: 434155606
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: Ifb1457f4b3e2eb1319e9a354e51560ed5ac79891
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7516957
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Ginny Huang <ginnyhuang@chromium.org>
    > Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    > Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    > Reviewed-by: Andrea Gomez <andreaxg@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1574616}
    
    Bug: 434155606
    Change-Id: I1211111672edf2b8414f993e9dbd84c7c09daad3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7520446
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Auto-Submit: James Lee <ljjlee@google.com>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1575875}

--- ios/chrome/browser/search_engine_choice/coordinator/search_engine_choice_taiyaki_egtest.mm
+++ ios/chrome/browser/search_engine_choice/coordinator/search_engine_choice_taiyaki_egtest.mm
@@ -50,7 +50,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       "=jp");
   config.additional_args.push_back(
       "--" + std::string(switches::kForceSearchEngineChoiceScreen));
-  config.features_enabled.push_back(switches::kTaiyaki);
+  config.features_enabled_and_params.push_back(
+      {switches::kTaiyaki,
+       {{switches::kTaiyakiChoiceScreenSurface.name, "all"}}});
   // Relaunches the app at each test to re-display the choice screen.
   config.relaunch_policy = ForceRelaunchByKilling;
 
