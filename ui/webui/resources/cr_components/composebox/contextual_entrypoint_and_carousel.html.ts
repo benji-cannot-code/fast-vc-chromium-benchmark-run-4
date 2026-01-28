cec1f36bb75e8deed1431350bd7e498e381c5014
Dhruv Kathpalia <dhruvkathpalia@google.com>
2026-01-27 19:01:23 -0800

    [composebox] Remove inputsDisabled from ContextualEntrypointButton.
    The `inputsDisabled` property and its usage to disable the contextual
    entrypoint button have been removed as we don't disable the menu
    altogether now.
    Screenshot:
    screenshot/66tPBmpTTVLsX97
    
    Bug: b:474674386
    Change-Id: Ifc5dddf41ac304d5fe498a70b2f861b6b70fa5ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7525006
    Commit-Queue: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Auto-Submit: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1575665}

--- ui/webui/resources/cr_components/composebox/contextual_entrypoint_and_carousel.html.ts
+++ ui/webui/resources/cr_components/composebox/contextual_entrypoint_and_carousel.html.ts
@@ -106,7 +106,6 @@ export function getHtml(this: ContextualEntrypointAndCarouselElement) {
             .fileNum="${this.files_.size}"
             .searchboxLayoutMode="${this.searchboxLayoutMode}"
             .inputState="${this.inputState}"
-            ?inputs-disabled="${this.inputsDisabled_}"
             ?show-context-menu-description="${showDescription}"
             glif-animation-state="${this.contextMenuGlifAnimationState}">
         </cr-composebox-contextual-entrypoint-button>
--- ui/webui/resources/cr_components/composebox/contextual_entrypoint_button.ts
+++ ui/webui/resources/cr_components/composebox/contextual_entrypoint_button.ts
@@ -52,7 +52,6 @@ export class ContextualEntrypointButtonElement extends
       // =========================================================================
       // Public properties
       // =========================================================================
-      inputsDisabled: {type: Boolean},
       fileNum: {type: Number},
       showContextMenuDescription: {type: Boolean},
       hasImageFiles: {
@@ -75,7 +74,6 @@ export class ContextualEntrypointButtonElement extends
     };
   }
 
-  accessor inputsDisabled: boolean = false;
   accessor fileNum: number = 0;
   accessor showContextMenuDescription: boolean = false;
   accessor hasImageFiles: boolean = false;
