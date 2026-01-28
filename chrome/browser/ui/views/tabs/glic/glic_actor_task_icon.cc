8eaf8a84441327ff5328afd39e5d3fddefc3b10e
Maggie Jennings <mjenn@google.com>
2026-01-27 16:15:07 -0800

    [Task Indicator] Align popover with button's right edge
    Before: https://screenshot.googleplex.com/8wmMSkbTHoFCNGe
    After: https://screenshot.googleplex.com/5qNzNHquFGgE9t2
    
    Bug: 473600333
    Change-Id: I52fa74395e44c5e4a740eda92b18fbc155190d51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7521691
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Maggie Jennings <mjenn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1575591}

--- chrome/browser/ui/views/tabs/glic/glic_actor_task_icon.cc
+++ chrome/browser/ui/views/tabs/glic/glic_actor_task_icon.cc
@@ -226,6 +226,12 @@ void GlicActorTaskIcon::UpdateInkdropHoverColor(bool is_frame_active) {
   }
 }
 
+gfx::Rect GlicActorTaskIcon::GetAnchorBoundsInScreen() const {
+  gfx::Rect bounds = GetBoundsInScreen();
+  bounds.Inset(GetInsets());
+  return bounds;
+}
+
 GlicActorTaskIcon::~GlicActorTaskIcon() = default;
 
 BEGIN_METADATA(GlicActorTaskIcon)
