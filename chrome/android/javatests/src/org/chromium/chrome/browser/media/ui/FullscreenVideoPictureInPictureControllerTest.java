a4ca5f92912d70d770c11872d195f022692e3cba
Henrique Nakashima <hnakashima@chromium.org>
2026-01-28 15:24:32 -0800

    [Android] Migrate Android Desktop .filters to @DisableIf (part 8/20)
    Migrated:
    - PasswordGenerationIntegrationTest
      - *
    - AutoplayMutedNotificationTest
      - testMutedPlaybackDoesNotTakeAudioFocus
      - testUnmutedPlaybackTakesAudioFocus
    - FullscreenVideoPictureInPictureControllerTest
      - *
    - IncognitoNtpOmniboxAutofocusManagerTest
      - *
    - IncognitoNtpOmniboxAutofocusTrackerTest
      - *
    
    Include-Ci-Only-Tests: chromium.android.desktop:android-desktop-x64-rel-15-tests|chrome_public_test_apk
    Validate-Test-Flakiness: skip
    Bug: 459810720, 446934111
    Change-Id: Ic1fda17fcbcad2f45088f26aa830f0bf26ed8bb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7513392
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Owners-Override: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1576232}

--- chrome/android/javatests/src/org/chromium/chrome/browser/media/ui/FullscreenVideoPictureInPictureControllerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/media/ui/FullscreenVideoPictureInPictureControllerTest.java
@@ -20,6 +20,7 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
@@ -36,6 +37,7 @@ import org.chromium.content_public.browser.test.util.DOMUtils;
 import org.chromium.content_public.browser.test.util.JavaScriptUtils;
 import org.chromium.content_public.browser.test.util.WebContentsUtils;
 import org.chromium.media.MediaSwitches;
+import org.chromium.ui.base.DeviceFormFactor;
 import org.chromium.ui.test.util.DeviceRestriction;
 
 /** Tests for FullscreenVideoPictureInPictureController and related methods. */
@@ -49,6 +51,7 @@ import org.chromium.ui.test.util.DeviceRestriction;
     RESTRICTION_TYPE_NON_LOW_END_DEVICE,
     DeviceRestriction.RESTRICTION_TYPE_NON_AUTO // PiP not supported on AAOS.
 })
+@DisableIf.Device(DeviceFormFactor.DESKTOP) // https://crbug.com/446934111
 public class FullscreenVideoPictureInPictureControllerTest {
     // TODO(peconn): Add a test for exit on Tab Reparenting.
     private static final String TEST_PATH = "/chrome/test/data/media/bigbuck-player.html";
