d174fd25d0a4dbbd76a51ef9da7572514383e090
Joshua Hood <jdh@chromium.org>
2026-01-28 07:27:23 -0800

    [Bundled Security Settings] Use pre-existing Secure DNS row
    This CL adds the pre-existing Secure DNS settings row to the "Account
    and network protection" section of the page when the Secure DNS V2
    settings row is hidden (i.e.
    chrome://flags#bundled-security-settings-secure-dns-v2 is disabled).
    
    Also, the #resolverOptions element in secure-dns-settings is updated to
    be a cr-collapse instead of a standard div that is hidden or shown based
    on the toggle's state. This introduces an animation for opening and
    closing to the element when the toggle is changed, as opposed to simply
    inserting/stamping the element.
    
    Screencast: https://screencast.googleplex.com/cast/NTM1OTcxNTgxODE0Mzc0NHwwNThiMzE2Zi04NA
    
    Bug: 477667435
    Change-Id: Ia2258a9bfd8c580bfca1e0715418f90d4d8da56e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7514259
    Commit-Queue: Joshua Hood <jdh@chromium.org>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Awad Osman <awado@google.com>
    Cr-Commit-Position: refs/heads/main@{#1575931}

--- chrome/browser/resources/settings/privacy_page/security/secure_dns.ts
+++ chrome/browser/resources/settings/privacy_page/security/secure_dns.ts
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * by PrivacyPageBrowserProxy and describe the new host resolver configuration.
  */
 
+import 'chrome://resources/cr_elements/cr_collapse/cr_collapse.js';
 import 'chrome://resources/cr_elements/cr_shared_style.css.js';
 import 'chrome://resources/cr_elements/cr_shared_vars.css.js';
 import 'chrome://resources/cr_elements/md_select.css.js';
