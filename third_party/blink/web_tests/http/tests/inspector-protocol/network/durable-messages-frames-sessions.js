2cddc66c232b434d3475235bfd7c15af5c334e5a
Alex N. Jose <alexnj@chromium.org>
2026-01-28 09:39:21 -0800

    Fix flake introduced by fetching in-progress response
    This CL fixes test flakiness introduced by fetching a response
    that is in progress using Durable Messages.
    
    Bug: 478033574
    Change-Id: If3d02ffebf1ac0123c5c7eb1e51f369eb7249b6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7523440
    Commit-Queue: Alex N. Jose <alexnj@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1576020}

--- third_party/blink/web_tests/http/tests/inspector-protocol/network/durable-messages-frames-sessions.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/network/durable-messages-frames-sessions.js
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     const requestWillBeSent = (await dp.Network.onceRequestWillBeSent()).params;
     testRunner.log(`Request for ${requestWillBeSent.request.url}`);
 
-    await dp.Network.onceResponseReceived();
+    await dp.Network.onceLoadingFinished();
     const data = await dp.Network.getResponseBody(
       {requestId: requestWillBeSent.requestId});
     await logDigest(data.result.body);
