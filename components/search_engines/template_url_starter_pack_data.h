cc61b9322006e4ac9d7d85c6659e0352339a37fb
Keigo Oka <oka@google.com>
2026-01-27 21:30:24 -0800

    site search: Use enum for StarterPackId
    This allows us to export them to Java later.
    
    Bug: 399443172
    Change-Id: If3d7fbbc5be844f5f3bf1fdb876a24ea183a2c1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7512676
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Keigo Oka <oka@chromium.org>
    Auto-Submit: Keigo Oka <oka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1575696}

--- components/search_engines/template_url_starter_pack_data.h
+++ components/search_engines/template_url_starter_pack_data.h
@@ -22,7 +22,7 @@ struct TemplateURLData;
 
 namespace template_url_starter_pack_data {
 
-typedef enum {
+enum StarterPackId {
   kBookmarks = 1,
   kHistory = 2,
   kTabs = 3,
@@ -31,7 +31,7 @@ typedef enum {
   kAiMode = 6,
 
   kMaxStarterPackId
-} StarterPackId;
+};
 
 struct StarterPackEngine {
   int name_message_id;
@@ -67,7 +67,7 @@ std::vector<std::unique_ptr<TemplateURLData>> GetStarterPackEngines();
 
 // Returns the destination url for the starter pack engine associated with a
 // given starter pack id.
-std::u16string GetDestinationUrlForStarterPackId(int id);
+std::u16string GetDestinationUrlForStarterPackId(StarterPackId id);
 
 }  // namespace template_url_starter_pack_data
 
