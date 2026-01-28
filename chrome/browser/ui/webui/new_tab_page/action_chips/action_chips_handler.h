5d0bc0cc110bde47e741f4ad60ac14f0a0dc35fd
shomizoe <shomizoe@google.com>
2026-01-28 11:07:40 -0800

    [ntp-next][cleanup]: Remove client-side sensitivity check for Action Chips
    Removes the client-side sensitivity check logic gated by the feature
    parameter ntp_features::kNtpNextClientSensitivityCheckParam. This
    logic was used to query history data and content annotations to
    determine if a tab was "sensitive" before generating Action Chips on
    the New Tab Page.
    
    Cleanup includes:
    - Removal of sensitivity check in StartActionChipsRetrieval.
    - Deletion of the OnGetHistoryData callback method.
    - Removal of history_service_ and cancelable_task_tracker_ members.
    - Removal of associated unit tests and MockHistoryService.
    
    Test: out/Default/unit_tests --gtest_filter=ActionChipsHandler*
    Change-Id: Id051a45f57bf1dea880b9aebd33e3d4f6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7524429
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Commit-Queue: Sho Mizoe <shomizoe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1576081}

--- chrome/browser/ui/webui/new_tab_page/action_chips/action_chips_handler.h
+++ chrome/browser/ui/webui/new_tab_page/action_chips/action_chips_handler.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_forward.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
-#include "base/task/cancelable_task_tracker.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
 #include "chrome/browser/ui/webui/new_tab_page/action_chips/action_chips.mojom.h"
@@ -28,15 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 class TimeTicks;
-}
-
-namespace history {
-class HistoryService;
-class QueryResults;
-}  // namespace history
-
-namespace tabs {
-class TabInterface;
 }
 
 class Profile;
@@ -65,9 +55,6 @@ class ActionChipsHandler : public action_chips::mojom::ActionChipsHandler,
   void SendActionChipsToUi(
       base::TimeTicks start_time,
       std::vector<action_chips::mojom::ActionChipPtr> chips);
-  void OnGetHistoryData(const tabs::TabInterface* tab,
-                        base::TimeTicks start_time,
-                        history::QueryResults results);
 
   // Returns true if chips retrieval should not occur in the current call.
   // This method was introduced to address the issue of jittering experience
@@ -80,10 +67,6 @@ class ActionChipsHandler : public action_chips::mojom::ActionChipsHandler,
   raw_ptr<Profile> profile_;
   raw_ptr<content::WebUI> web_ui_;
   std::unique_ptr<ActionChipsGenerator> action_chips_generator_;
-  raw_ptr<history::HistoryService> history_service_;
-
-  // Task tracker for history requests.
-  base::CancelableTaskTracker cancelable_task_tracker_;
 
   std::optional<GURL> last_processed_url_;
 
