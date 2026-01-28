a690c6be0ee3fbba14da36c4792b813fe96ead75
Mitsuru Oshima <oshima@chromium.org>
2026-01-27 17:54:04 -0800

    Remove DanglingUntriaged in capture_controller_unittest.cc
    Bug: 40212619
    
    Change-Id: Icbe7a45ea5fd0c84f7b2af4b60467c84a849a71f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7518184
    Reviewed-by: Di Wu <diwux@google.com>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1575640}

--- ui/wm/core/capture_controller_unittest.cc
+++ ui/wm/core/capture_controller_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/memory/raw_ptr.h"
+#include "base/memory/raw_ptr_exclusion.h"
 #include "ui/aura/client/capture_delegate.h"
 #include "ui/aura/env.h"
 #include "ui/aura/test/aura_test_base.h"
@@ -16,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/test/test_window_delegate.h"
 #include "ui/aura/window.h"
 #include "ui/aura/window_event_dispatcher.h"
+#include "ui/aura/window_observer.h"
 #include "ui/aura/window_tracker.h"
 #include "ui/events/event.h"
 #include "ui/events/event_utils.h"
@@ -29,7 +31,8 @@ namespace {
 
 // aura::client::CaptureDelegate which allows querying whether native capture
 // has been acquired.
-class TestCaptureDelegate : public aura::client::CaptureDelegate {
+class TestCaptureDelegate : public aura::client::CaptureDelegate,
+                            public aura::WindowObserver {
  public:
   TestCaptureDelegate() = default;
 
@@ -39,15 +42,20 @@ class TestCaptureDelegate : public aura::client::CaptureDelegate {
   ~TestCaptureDelegate() override = default;
 
   bool HasNativeCapture() const { return has_capture_; }
-  aura::Window* old_capture() { return old_capture_; }
-  aura::Window* new_capture() { return new_capture_; }
+  const aura::Window* old_capture() const { return old_capture_; }
   void SetDestroyOldCapture(bool destroy) { destroy_old_capture_ = destroy; }
 
   // aura::client::CaptureDelegate:
   void UpdateCapture(aura::Window* old_capture,
                      aura::Window* new_capture) override {
+    if (new_capture_) {
+      new_capture_->RemoveObserver(this);
+    }
     old_capture_ = old_capture;
     new_capture_ = new_capture;
+    if (new_capture_) {
+      new_capture_->AddObserver(this);
+    }
     if (old_capture && destroy_old_capture_)
       delete old_capture;
   }
@@ -55,10 +63,19 @@ class TestCaptureDelegate : public aura::client::CaptureDelegate {
   void SetNativeCapture() override { has_capture_ = true; }
   void ReleaseNativeCapture() override { has_capture_ = false; }
 
+  void OnWindowDestroying(aura::Window* window) override {
+    if (window == new_capture_) {
+      window->RemoveObserver(this);
+      new_capture_ = nullptr;
+    }
+  }
+
  private:
   bool has_capture_ = false;
-  raw_ptr<aura::Window, DanglingUntriaged> old_capture_ = nullptr;
-  raw_ptr<aura::Window, DanglingUntriaged> new_capture_ = nullptr;
+  // RAW_PTR_EXCLUSION is used for old_capture_ as it is used to compare the
+  // deleted window.
+  RAW_PTR_EXCLUSION aura::Window* old_capture_ = nullptr;
+  raw_ptr<aura::Window> new_capture_ = nullptr;
   bool destroy_old_capture_ = false;
 };
 
@@ -311,7 +328,7 @@ TEST_F(CaptureControllerTest, UpdateCaptureDestroysOldCaptureWindow) {
 
   // Since delegate iteration order is not deterministic, use this to assert
   // that the two scenarios below have opposite order.
-  aura::Window* first_old_capture = nullptr;
+  const aura::Window* first_old_capture = nullptr;
 
   {
     // Create a window inside the WindowEventDispatcher.
