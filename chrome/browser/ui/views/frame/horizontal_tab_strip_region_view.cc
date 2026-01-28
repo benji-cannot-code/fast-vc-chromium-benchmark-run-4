78b571f0c4048cfccde0650de3b4a14f61349156
dljames <dljames@chromium.org>
2026-01-28 15:00:40 -0800

    [Vertical Tabs] Remove const from DisableTabStripEditingForTesting
    Removes the const qualifier from DisableTabStripEditingForTesting since
    the implementation of vertical tabs needs to flip a bool flag.
    
    Change-Id: Ide908c722fc9f9da6482c247953134b30d147450
    Bug: 467710617
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7528135
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1576226}

--- chrome/browser/ui/views/frame/horizontal_tab_strip_region_view.cc
+++ chrome/browser/ui/views/frame/horizontal_tab_strip_region_view.cc
@@ -593,7 +593,7 @@ bool HorizontalTabStripRegionView::IsTabStripEditable() const {
   return tab_strip_->IsTabStripEditable();
 }
 
-void HorizontalTabStripRegionView::DisableTabStripEditingForTesting() const {
+void HorizontalTabStripRegionView::DisableTabStripEditingForTesting() {
   tab_strip_->DisableTabStripEditingForTesting();  // IN-TEST
 }
 
