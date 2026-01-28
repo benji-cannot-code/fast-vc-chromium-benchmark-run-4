4d9ede12a51fa21d91ada10107cdf89444304e0e
christianxu <christianxu@chromium.org>
2026-01-28 08:56:07 -0800

    [iOS][Omnibox] Add OmniboxCrashFixKillSwitch feature flag
    Tentative fix for omnibox crash probably due to invalid UI state. This
    fix prevents the omnibox update it's not focused, instead updating it
    right before focus.
    
    Added with a kill switch to restore original behavior when enabled.
    
    Bug: 445820863
    Change-Id: Ia02e3f9b9348b82bf2dacc49250a30c66e05bf13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7198667
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1575987}

--- ios/chrome/browser/omnibox/coordinator/omnibox_coordinator.mm
+++ ios/chrome/browser/omnibox/coordinator/omnibox_coordinator.mm
@@ -301,6 +301,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)focusOmnibox {
+  if (!IsOmniboxCrashFixKillSwitchEnabled()) {
+    [self updateOmniboxState];
+  }
   [_omniboxTextController focusOmnibox];
 }
 
--- ios/chrome/browser/shared/public/features/features.h
+++ ios/chrome/browser/shared/public/features/features.h
@@ -849,6 +849,12 @@ BASE_DECLARE_FEATURE(kTabGroupColorOnSurface);
 // Returns true if the TabGroupColorOnSurface feature is enabled.
 bool IsTabGroupColorOnSurfaceEnabled();
 
+// Enables the OmniboxCrashFixKillSwitch feature.
+BASE_DECLARE_FEATURE(kOmniboxCrashFixKillSwitch);
+
+// Returns true if the OmniboxCrashFixKillSwitch feature is enabled.
+bool IsOmniboxCrashFixKillSwitchEnabled();
+
 // Enables the AIMEligibilityServiceStartWithProfile feature.
 BASE_DECLARE_FEATURE(kAIMEligibilityServiceStartWithProfile);
 
