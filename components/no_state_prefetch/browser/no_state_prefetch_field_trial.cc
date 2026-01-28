3924e881517baac9f80ffade8326f1f65fbc2cf6
Andreas Haas <ahaas@chromium.org>
2026-01-28 09:16:28 -0800

    Clean up stale base::Feature "GWSPrefetchHoldback"
    Remove that feature flag as part of the Chrome Code Cleanup Rotation.
    
    Bug: 356236874
    Change-Id: Ia202e3313acb0ef40c19d6855b8d2a01768bb2ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7516705
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Andreas Haas <ahaas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1576001}

--- components/no_state_prefetch/browser/no_state_prefetch_field_trial.cc
+++ components/no_state_prefetch/browser/no_state_prefetch_field_trial.cc
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace prerender {
 
-BASE_FEATURE(kGWSPrefetchHoldback, base::FEATURE_DISABLED_BY_DEFAULT);
-
 BASE_FEATURE(kNavigationPredictorPrefetchHoldback,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
--- components/no_state_prefetch/browser/no_state_prefetch_field_trial.h
+++ components/no_state_prefetch/browser/no_state_prefetch_field_trial.h
@@ -10,9 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace prerender {
 
-// Preconnects instead of prefetching from GWS.
-BASE_DECLARE_FEATURE(kGWSPrefetchHoldback);
-
 // Preconnects instead of prefetching from NavigationPredictor.
 BASE_DECLARE_FEATURE(kNavigationPredictorPrefetchHoldback);
 
