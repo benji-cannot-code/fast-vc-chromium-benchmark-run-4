87d5ca8db6d7e89d88c076ba97b8b0f19938bfb7
Rune Lillesveen <futhark@chromium.org>
2026-01-28 03:00:48 -0800

    Remove unused InvalidatesTagName method
    Change-Id: I0c5a7902b91044f34effdee6771024fa23a73670
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7523505
    Auto-Submit: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Steinar H Gunderson <sesse@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1575822}

--- third_party/blink/renderer/core/css/invalidation/invalidation_set.h
+++ third_party/blink/renderer/core/css/invalidation/invalidation_set.h
@@ -116,7 +116,6 @@ class CORE_EXPORT InvalidationSet
   }
 
   bool InvalidatesElement(Element&) const;
-  bool InvalidatesTagName(Element&) const;
 
   void AddClass(const AtomicString& class_name);
   void AddId(const AtomicString& id);
