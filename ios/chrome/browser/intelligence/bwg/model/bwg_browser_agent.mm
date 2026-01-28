fd239671ee0db659025fc86f83998b082bd882db
Tarek Makkouk <tarekmakkouk@google.com>
2026-01-27 16:09:39 -0800

    [iOS] Update Gemini floaty offset on viewport inset changes
    Implement FullscreenViewportInsetRangeChanged in BwgBrowserAgent to ensure the Gemini floaty's position is updated whenever the fullscreen viewport insets change. This fixes an issue where moving the omnibox to the bottom (or other layout changes affecting insets) would not immediately update the floaty's position until a scroll event occurred.
    
    Fix demo: https://drive.google.com/file/d/1f8LBPbjjj_d6ka9Imqh02HMHs8BGpvAV/view?usp=sharing
    
    Bug: 475085628
    Change-Id: I25e08e97bc19f6e9a88f8ae3b83f5d0fe2ca6a5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7521792
    Reviewed-by: Joemer Ramos <joemerramos@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Tarek Makkouk <tarekmakkouk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1575587}

--- ios/chrome/browser/intelligence/bwg/model/bwg_browser_agent.mm
+++ ios/chrome/browser/intelligence/bwg/model/bwg_browser_agent.mm
@@ -621,6 +621,13 @@ void BwgBrowserAgent::FullscreenControllerWillShutDown(
   fullscreen_controller_ = nullptr;
 }
 
+void BwgBrowserAgent::FullscreenViewportInsetRangeChanged(
+    FullscreenController* controller,
+    UIEdgeInsets min_viewport_insets,
+    UIEdgeInsets max_viewport_insets) {
+  FullscreenProgressUpdated(controller, controller->GetProgress());
+}
+
 #pragma mark - Private
 
 void BwgBrowserAgent::PresentFloatyWithState(
