7d6b78360b89de6045aa141e61f6747fc36a79d9
Fred Shih <ffred@chromium.org>
2026-01-27 16:32:27 -0800

    Split tab components into their own directory
    Bypass-Check-License: Moving files
    Change-Id: I6043a728b048e644984c470234c8cd8fb1afd7b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7517997
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Fred Shih <ffred@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1575598}

--- chrome/browser/resources/webui_browser/tab_strip/tab.html.ts
+++ chrome/browser/resources/webui_browser/tab_strip/tab.html.ts
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {html} from 'chrome://resources/lit/v3_0/lit.rollup.js';
 
-import type {TabElement} from './tab_element.js';
+import type {TabElement} from './tab.js';
 
 export function getHtml(this: TabElement) {
   // clang-format off
