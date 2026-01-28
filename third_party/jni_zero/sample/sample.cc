f3890a3c5d6639a7a38e2abcc54bdb6f71f6015d
Andrew Grieve <agrieve@chromium.org>
2026-01-28 13:29:10 -0800

    Make @JniType() work for vectors or C++ enums
    Protobuf has these.
    
    Change-Id: I73bf9b094387313313f6d5868f59f42891d139bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7528734
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Martin Kong <martinkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1576174}

--- third_party/jni_zero/sample/sample.cc
+++ third_party/jni_zero/sample/sample.cc
@@ -6,13 +6,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Generated file for JNI bindings from C++ to Java @CalledByNative methods.
 // Only to be included in one .cc file.
 // Name is based on the java file name: *.java -> jni/*_jni.h
+namespace jni_zero::sample {
+enum class MyEnum { A, B, C };
+}
+
 #include "third_party/jni_zero/sample/sample_jni/Sample_jni.h"
 
 using jni_zero::JavaRef;
 using jni_zero::ScopedJavaLocalRef;
 
 namespace jni_zero::sample {
-static void JNI_Sample_DoNothing(JNIEnv* env) {}
+
+static void JNI_Sample_DoSomething(JNIEnv* env) {
+  std::vector<MyEnum> values = Java_Sample_getArrayOfEnum(env);
+  Java_Sample_setArrayOfEnum(env, values);
+}
 
 static jboolean JNI_Sample_TestMultipleParams(JNIEnv* env,
                                               jint a,
