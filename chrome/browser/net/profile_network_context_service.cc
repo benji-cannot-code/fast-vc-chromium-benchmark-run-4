1807b7770a788a0d1b3efdf62cd39b5000dc4790
Daniel Rubery <drubery@chromium.org>
2026-01-28 09:36:42 -0800

    Consolidate DBSC restricted sites
    Instead of having two lists of sites, put them both in signin_util.
    
    Fixed: 475579173
    Change-Id: Ie3c242951f5c6a3e3473ab296a15b7a96a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7522895
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1576018}

--- chrome/browser/net/profile_network_context_service.cc
+++ chrome/browser/net/profile_network_context_service.cc
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/net/system_network_context_manager.h"
 #include "chrome/browser/privacy_sandbox/privacy_sandbox_settings_factory.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/signin/signin_util.h"
 #include "chrome/browser/ssl/sct_reporting_service.h"
 #include "chrome/browser/ssl/sct_reporting_service_factory.h"
 #include "chrome/browser/webid/federated_identity_permission_context.h"
@@ -1557,9 +1558,7 @@ void ProfileNetworkContextService::ConfigureNetworkContextParamsInternal(
   // an experiment to understand their session's impact on Chrome's
   // special cookie handling for these sites.
   network_context_params->device_bound_sessions_restricted_sites =
-      std::vector<net::SchemefulSite>{
-          net::SchemefulSite(GURL("https://google.com")),
-          net::SchemefulSite(GURL("https://youtube.com"))};
+      signin_util::GetDeviceBoundSessionRestrictedSites();
 
 #if BUILDFLAG(ENABLE_DEVICE_BOUND_SESSIONS)
   if (base::FeatureList::IsEnabled(net::features::kDeviceBoundSessions) &&
