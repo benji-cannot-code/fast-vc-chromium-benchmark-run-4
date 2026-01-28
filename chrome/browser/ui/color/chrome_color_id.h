97fe5cbefdf2f86be29aa03c8ade081a1a41cf42
Teresa Mao <temao@chromium.org>
2026-01-27 17:10:05 -0800

    [omnibox-next] Fix file thumbnail hover state color.
    See screenshots in the bug.
    
    Bug: b:471294852
    Change-Id: I80f356fc17700102faa19c7bfda2beb70e201050
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7511177
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Reviewed-by: Marlon Facey <mfacey@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Teresa Mao <temao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1575617}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -403,6 +403,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorOmniboxComposeboxChipBackground) \
   E_CPONLY(kColorOmniboxComposeboxDivider) \
   E_CPONLY(kColorOmniboxComposeboxFaviconBackground) \
+  E_CPONLY(kColorOmniboxComposeboxFileThumbnailOverlay) \
+  E_CPONLY(kColorOmniboxComposeboxFileThumbnailOverlayIcon) \
   E_CPONLY(kColorOmniboxComposeboxForegroundDisabled) \
   E_CPONLY(kColorOmniboxComposeboxPrimaryAction) \
   E_CPONLY(kColorOmniboxComposeboxSubmitButtonBackground) \
