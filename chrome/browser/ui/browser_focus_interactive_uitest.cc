260c9fd5bb0957554135adc081776afc41a297d1
Eriko Kurimoto <elkurin@chromium.org>
2026-01-28 00:04:24 -0800

    WaaP: Replace IsViewFocused with WaitForViewFocus in BrowserFocusInteractiveUITest.
    This CL makes the focus checks in BrowserFocusInteractiveUITest
    asynchronous by waiting for the expected view to gain focus, which helps
    reduce test flakiness.
    
    This is needed for WebUI toolbar feature which increases this flakiness.
    
    Change-Id: If2ae29e37177dd89314dbe27176007bc2f7118f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7524948
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1575748}

--- chrome/browser/ui/browser_focus_interactive_uitest.cc
+++ chrome/browser/ui/browser_focus_interactive_uitest.cc
@@ -355,7 +355,7 @@ IN_PROC_BROWSER_TEST_F(BrowserFocusTest, TabsRememberFocus) {
                  TabStripUserGestureDetails::GestureType::kOther));
 
       ViewID vid = kFocusPage[i][j] ? VIEW_ID_TAB_CONTAINER : VIEW_ID_OMNIBOX;
-      ASSERT_TRUE(IsViewFocused(vid));
+      ui_test_utils::WaitForViewFocus(browser(), vid, true);
     }
 
     browser()->tab_strip_model()->ActivateTabAt(
@@ -366,7 +366,7 @@ IN_PROC_BROWSER_TEST_F(BrowserFocusTest, TabsRememberFocus) {
     for (int j = 0; j < 15; j++) {
       ViewID vid =
           kFocusPage[i][j % 5] ? VIEW_ID_TAB_CONTAINER : VIEW_ID_OMNIBOX;
-      ASSERT_TRUE(IsViewFocused(vid));
+      ui_test_utils::WaitForViewFocus(browser(), vid, true);
 
       ASSERT_TRUE(ui_test_utils::SendKeyPressSync(browser(), ui::VKEY_TAB, true,
                                                   false, false, false));
@@ -379,7 +379,7 @@ IN_PROC_BROWSER_TEST_F(BrowserFocusTest, TabsRememberFocus) {
     for (int j = 14; j >= 0; --j) {
       ViewID vid =
           kFocusPage[i][j % 5] ? VIEW_ID_TAB_CONTAINER : VIEW_ID_OMNIBOX;
-      ASSERT_TRUE(IsViewFocused(vid));
+      ui_test_utils::WaitForViewFocus(browser(), vid, true);
 
       ASSERT_TRUE(ui_test_utils::SendKeyPressSync(browser(), ui::VKEY_TAB, true,
                                                   true, false, false));
