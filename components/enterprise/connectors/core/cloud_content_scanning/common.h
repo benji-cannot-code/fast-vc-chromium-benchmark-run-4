57284a95b9381e52555f2b9be71805830eabc847
Alex Chen <alxchn@google.com>
2026-01-28 08:31:28 -0800

    Enable FileAnalysisRequestBase to compute hash async.
    Only do so when kContentHashInFileUploadFinalCall is enabled, for large
    files. Additionally consumers of the class must opt in to the new
    behavior.
    
    Change-Id: I8cb40a5dca66c0bfe556ff41ad491a2dc7afb04f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7384524
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Commit-Queue: Alex Chen <alxchn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1575973}

--- components/enterprise/connectors/core/cloud_content_scanning/common.h
+++ components/enterprise/connectors/core/cloud_content_scanning/common.h
@@ -8,7 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/functional/callback.h"
+
 namespace enterprise_connectors {
+
+// Callback to be called when the hash of a file has been computed.
+using OnGotHashCallback = base::OnceCallback<void(std::string)>;
+
 // The result of uploading a scanning request to the WebProtect server.
 //
 // These values are persisted to logs. Entries should not be renumbered and
