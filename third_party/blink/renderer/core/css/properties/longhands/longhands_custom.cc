dcc06265569070cbf5ca6535b36f5f212bfb4409
Rune Lillesveen <futhark@chromium.org>
2026-01-27 23:42:18 -0800

    Drop 'portrait' from size descriptor when default
    All the named sizes have a size which is portrait by default.
    
    Serialization will be more minimal, and wpt test starts passing.
    
    Bug: 356917017
    Change-Id: I2188597890993be0bca1be229f0a3194bd36e5f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7524025
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1575737}

--- third_party/blink/renderer/core/css/properties/longhands/longhands_custom.cc
+++ third_party/blink/renderer/core/css/properties/longhands/longhands_custom.cc
@@ -9619,7 +9619,7 @@ const CSSValue* Size::ParseSingleValue(
   }
 
   CSSValue* page_size = ConsumePageSize(stream);
-  CSSValue* orientation =
+  CSSIdentifierValue* orientation =
       css_parsing_utils::ConsumeIdent<CSSValueID::kPortrait,
                                       CSSValueID::kLandscape>(stream);
   if (!page_size) {
@@ -9632,7 +9632,8 @@ const CSSValue* Size::ParseSingleValue(
   if (page_size) {
     result->Append(*page_size);
   }
-  if (orientation) {
+  if (orientation &&
+      (!page_size || orientation->GetValueID() != CSSValueID::kPortrait)) {
     result->Append(*orientation);
   }
   return result;
