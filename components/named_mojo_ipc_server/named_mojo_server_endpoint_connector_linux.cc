4dd57be76a8fa456f8b00af0d1281b237290bbcf
Yuwei Huang <yuweih@chromium.org>
2026-01-29 15:24:31 -0800

    [named mojo ipc][linux] Allow accepting connections from any user
    For go/crd-linux-multi-process, the daemon process runs as root and
    accepts IPC connections from any users. However, NamedMojoIpcServer
    currently creates the socket file with the 755 permission, which does
    not allow other users to connect (no write access). Also the
    AcceptSocketConnection call has check_peer_user set to true, which will
    reject any client connections if the user is different.
    
    This CL fixes this problem by introducing an allow_any_peer_user flag,
    which sets the socket file permission to 666, and calls
    AcceptSocketConnection with check_peer_user=false.
    
    Bug: 475611769
    Change-Id: Iaf9e99d1901bc972834efb47b8d9346c685481e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7530628
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1576900}

--- components/named_mojo_ipc_server/named_mojo_server_endpoint_connector_linux.cc
+++ components/named_mojo_ipc_server/named_mojo_server_endpoint_connector_linux.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/named_mojo_ipc_server/named_mojo_server_endpoint_connector.h"
 
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 
 #include <memory>
@@ -73,7 +74,8 @@ void NamedMojoServerEndpointConnectorLinux::OnSocketReady() {
   int fd = server_endpoint_.platform_handle().GetFD().get();
 
   base::ScopedFD connection_fd;
-  bool success = mojo::AcceptSocketConnection(fd, &connection_fd);
+  bool success = mojo::AcceptSocketConnection(fd, &connection_fd,
+                                              options_.require_same_peer_user);
   if (!success) {
     LOG(ERROR) << "AcceptSocketConnection failed.";
     return;
@@ -112,6 +114,14 @@ bool NamedMojoServerEndpointConnectorLinux::TryStart() {
   }
 
   server_endpoint_ = std::move(server_endpoint);
+  if (!options_.require_same_peer_user) {
+    // Allow any user to write to the UDS. fchmod doesn't work after bind(), so
+    // we need to call chmod on the socket filename, which is the server name.
+    if (chmod(options_.server_name.c_str(), 0o666) != 0) {
+      PLOG(ERROR) << "chmod failed";
+      return false;
+    }
+  }
   read_watcher_controller_ = base::FileDescriptorWatcher::WatchReadable(
       server_endpoint_.platform_handle().GetFD().get(),
       base::BindRepeating(&NamedMojoServerEndpointConnectorLinux::OnSocketReady,
