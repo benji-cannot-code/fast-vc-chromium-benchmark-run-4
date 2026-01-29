a1d36f2bc27a4dae73bbb33c0a4c674c5f8f99bd
Joshua Pawlicki <waffles@chromium.org>
2026-01-29 14:27:43 -0800

    activity_reporter: Use the internal constants for official builds.
    It turns out checkout_src_internal isn't a gn arg on the official bots.
    Also, fix the update check URL.
    
    Bug: 454662418
    Change-Id: I9056987cdabd5fa27853d6ae23e966515a61b038
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7532684
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Noah Rose Ledesma <noahrose@google.com>
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1576861}

--- components/activity_reporter/constants.h
+++ components/activity_reporter/constants.h
@@ -6,15 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_ACTIVITY_REPORTER_CONSTANTS_H_
 #define COMPONENTS_ACTIVITY_REPORTER_CONSTANTS_H_
 
-#include "components/update_client/configurator.h"
+#include <string_view>
 
 namespace activity_reporter {
 
-inline constexpr char kChromeActivityId[] =
+inline constexpr std::string_view kChromeActivityId =
     "{afb5a10b-511f-4904-9596-d3f2937fd403}";
 
 // Refer to ./buildflags.gni to enable/disable reporting.
-inline constexpr char kUrl[] = "https://update.googleapis.com/service2/json";
+inline constexpr std::string_view kUrl =
+    "https://update.googleapis.com/service/update2/json";
 
 }  // namespace activity_reporter
 
