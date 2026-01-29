fde6336138de2ff2adde2b924c2c4002b06c9947
Joemer Ramos <joemerramos@google.com>
2026-01-29 06:24:38 -0800

    [iOS] Improve Floaty Visibility Logic For Webstate Observer Calls
    Floaty logic has evolved where we no longer need to detect if we need to
    show or hide the floaty for WebStateObserver calls. The floaty just need
    to worry about if it passes the eligibility criteria in
    `ShowFloatyIfInvokedAnimated` and hide the floaty when `WasHidden` is
    called.
    
    UI Video: crbug.com/479514144#comment2
    
    Fixed: 479514144
    Change-Id: Iaf3bad73f76441adea7adb92fc0f5736decc26f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7528974
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Joemer Ramos <joemerramos@google.com>
    Cr-Commit-Position: refs/heads/main@{#1576554}

--- ios/chrome/browser/intelligence/bwg/model/bwg_tab_helper.mm
+++ ios/chrome/browser/intelligence/bwg/model/bwg_tab_helper.mm
@@ -412,7 +412,7 @@ void BwgTabHelper::WasShown(web::WebState* web_state) {
   }
 
   if (IsGeminiCopresenceEnabled()) {
-    [bwg_commands_handler_ updateFloatyVisibilityForWebState:web_state];
+    [bwg_commands_handler_ showFloatyIfInvokedAnimated:YES];
   }
 }
 
@@ -428,6 +428,12 @@ void BwgTabHelper::WasHidden(web::WebState* web_state) {
   }
 
   UpdateWebStateSnapshotInStorage();
+
+  if (!IsGeminiCopresenceEnabled()) {
+    return;
+  }
+
+  [bwg_commands_handler_ hideFloatyIfInvokedAnimated:NO];
 }
 
 void BwgTabHelper::DidStartNavigation(
@@ -485,7 +491,7 @@ void BwgTabHelper::DidFinishNavigation(
     web::WebState* web_state,
     web::NavigationContext* navigation_context) {
   if (IsGeminiCopresenceEnabled()) {
-    [bwg_commands_handler_ updateFloatyVisibilityForWebState:web_state];
+    [bwg_commands_handler_ showFloatyIfInvokedAnimated:YES];
   }
 
   const GURL& current_url = navigation_context->GetUrl().GetWithoutRef();
