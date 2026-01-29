f61e1858b4fd765ee8d5fcf40424df4e98c256e2
Shu Yang <shuyng@google.com>
2026-01-29 15:44:24 -0800

    Enable passing TabsTest on desktop bot
    Include-Ci-Only-Tests: chromium.android.desktop:android-desktop-x64-rel-15-tests|chrome_public_test_apk
    Validate-Test-Flakiness: skip
    Bug: 479863847
    Change-Id: Iaa866fbae4360f7b0a6ddcb2075094bff0369d36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7533912
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Shu Yang <shuyng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1576909}

--- chrome/android/javatests/src/org/chromium/chrome/browser/TabsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/TabsTest.java
@@ -46,7 +46,6 @@ import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.Feature;
@@ -577,7 +576,6 @@ public class TabsTest {
     @Test
     @MediumTest
     @Feature({"Android-TabSwitcher"})
-    @DisableIf.Device(DeviceFormFactor.DESKTOP) // https://crbug.com/446934111
     public void testLastClosedUndoableTabGetsHidden() {
         final TabModel model =
                 mActivityTestRule.getActivity().getTabModelSelector().getCurrentModel();
@@ -810,7 +808,6 @@ public class TabsTest {
     @Test
     @MediumTest
     @Feature({"Android-TabSwitcher"})
-    @DisableIf.Device(DeviceFormFactor.DESKTOP) // https://crbug.com/446934111
     public void testLastClosedTabTriggersNotifyChangedCall() {
         final TabModel model =
                 mActivityTestRule.getActivity().getTabModelSelector().getCurrentModel();
