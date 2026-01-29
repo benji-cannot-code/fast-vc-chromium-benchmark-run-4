8a047046b5ae3f5b34ceb7eccde472ec6c446462
María del Pilar Domínguez Molina <mardelpi@google.com>
2026-01-29 05:00:13 -0800

    Moved file operation proxy impl files
    Bypass-Check-License: Moved File
    Change-Id: I1dda770e83bf8d7ea127cfc2eabdd969a876c0b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7505375
    Reviewed-by: Nathan Memmott <memmott@chromium.org>
    Reviewed-by: Danilo Françoso Tedeschi <danft@google.com>
    Commit-Queue: Danilo Françoso Tedeschi <danft@google.com>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1576506}

--- components/on_device_translation/file_operation_proxy_impl.cc
+++ components/on_device_translation/file_operation_proxy_impl.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/on_device_translation/file_operation_proxy_impl.h"
+#include "components/on_device_translation/file_operation_proxy_impl.h"
 
 #include "base/files/file_util.h"
 
