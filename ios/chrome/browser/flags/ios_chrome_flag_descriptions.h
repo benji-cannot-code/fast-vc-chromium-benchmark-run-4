b025b2fd53e5e9c4fab345d945027137ed526d99
Benjamin Williams <bwwilliams@google.com>
2026-01-29 07:56:02 -0800

    [iOS] Launch CrossDevicePrefTracker on iOS
    This change enables kEnableCrossDevicePrefTracker by default only on
    iOS. Non-iOS platforms are unaffected, as the feature is being ramped up
    separately on Desktop and Android to monitor platform-specific impact.
    
    Bug: None
    Change-Id: I80abd80d11fd868a37aa1d0114b7ef90c2d71a05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7532875
    Commit-Queue: Dominic Battre <battre@chromium.org>
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Auto-Submit: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1576589}

--- ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
+++ ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
@@ -316,9 +316,6 @@ extern const char kEnableClipboardDataControlsIOSDescription[];
 extern const char kEnableCompromisedPasswordsMutingName[];
 extern const char kEnableCompromisedPasswordsMutingDescription[];
 
-extern const char kEnableCrossDevicePrefTrackerName[];
-extern const char kEnableCrossDevicePrefTrackerDescription[];
-
 extern const char kEnableFamilyLinkControlsName[];
 extern const char kEnableFamilyLinkControlsDescription[];
 
