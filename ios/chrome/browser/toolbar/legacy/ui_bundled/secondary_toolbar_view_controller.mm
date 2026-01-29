cfdf20e83e4c2dfb6ba676e4c3cfd30bae631752
Joemer Ramos <joemerramos@google.com>
2026-01-29 11:34:16 -0800

    Reland "[iOS] Create a Field Trial Config For Gemini Copresence"
    This reverts commit 01ebbbb1a979acb46315a44f2e352adad0b51c06.
    
    Reason for revert: Attempt to land again + fix
    
    Original change's description:
    > Revert "[iOS] Create a Field Trial Config For Gemini Copresence"
    >
    > This reverts commit bc85d37ce9f330df746b0d93c28556111d7ebdae.
    >
    > Reason for revert:
    > LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/6166631983087616
    >
    > Sample build with failed test: https://ci.chromium.org/b/8691386428186997025
    > Affected test(s):
    > [://ios/chrome/test/earl_grey2\:ios_chrome_ui_eg2tests_module!xctest::SharedTabGroupsTestCase#testCloseLastTabInIncognito](https://ci.chromium.org/ui/test/chromium/:%2F%2Fios%2Fchrome%2Ftest%2Fearl_grey2%5C:ios_chrome_ui_eg2tests_module%21xctest::SharedTabGroupsTestCase%23testCloseLastTabInIncognito?q=VHash%3Abb18eb69160a71af)
    > [://ios/chrome/test/earl_grey2\:ios_chrome_ui_eg2tests_module!xctest::SharedTabGroupsTestCase#testCloseLastTabInIncognito](https://ci.chromium.org/ui/test/chromium/:%2F%2Fios%2Fchrome%2Ftest%2Fearl_grey2%5C:ios_chrome_ui_eg2tests_module%21xctest::SharedTabGroupsTestCase%23testCloseLastTabInIncognito?q=VHash%3Ae65a2513aae56189)
    >
    > If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F6166631983087616&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F7525828&type=BUG
    >
    > Original change's description:
    > > [iOS] Create a Field Trial Config For Gemini Copresence
    > >
    > > Creates a field trial with an enable group for the Copresence feature.
    > > We also need GeminiFRERefactor to be enabled for Copresence to
    > > properly work.
    > >
    > > Fixed: 478862373
    > > Change-Id: I7f023f8b438b13afae5d76441d77178869b1645c
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7525828
    > > Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    > > Commit-Queue: Joemer Ramos <joemerramos@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1576247}
    > >
    >
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: If349d2a2b37e43dbbad1a7caaa27c1d17be0df21
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7531008
    > Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1576426}
    
    Change-Id: I0dbc15e5f4a1eb00e64a6545a18aa7d0cc353247
    Fixed: 478862373
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7531519
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1576752}

--- ios/chrome/browser/toolbar/legacy/ui_bundled/secondary_toolbar_view_controller.mm
+++ ios/chrome/browser/toolbar/legacy/ui_bundled/secondary_toolbar_view_controller.mm
@@ -61,6 +61,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)disconnect {
+  [super disconnect];
   _fullscreenController = nullptr;
   [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
