3d8e5fb07a92b2831fdf89a7b96aefd70fa49582
Muhammad Salmaan <musalmaan@chromium.org>
2026-01-29 09:59:14 -0800

    Fix tab close button highlight in touch ui
    Fix tab close button highlight in touch ui as when it was hovered upon,
    the highlight was smaller then the actual tab close button.
    
    Close button highlight after
    fix:https://screenshot.googleplex.com/47E8rwFvR3FVqAQ.png
    
    Bug: 477680428
    Change-Id: Ib84150762702331fb02cbdc1664f3bcd8c3d9c51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7530690
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Muhammad Salmaan <musalmaan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1576687}

--- chrome/browser/ui/views/tabs/tab_close_button.cc
+++ chrome/browser/ui/views/tabs/tab_close_button.cc
@@ -41,7 +41,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace {
-constexpr gfx::Size kButtonSize = {28, 28};
+gfx::Size GetButtonSize() {
+  // The close button is larger than the icon inside to provide a larger hit
+  // target.
+  constexpr int kButtonPadding = 12;
+  const int icon_size = GetLayoutConstant(LayoutConstant::kTabCloseButtonSize);
+  const int button_size = icon_size + kButtonPadding;
+  return gfx::Size(button_size, button_size);
+}
+
 }  // namespace
 
 TabCloseButton::TabCloseButton(PressedCallback pressed_callback,
@@ -167,7 +175,7 @@ void TabCloseButton::RemoveLayerFromRegions(ui::Layer* old_layer) {
 
 gfx::Size TabCloseButton::CalculatePreferredSize(
     const views::SizeBounds& available_size) const {
-  return kButtonSize;
+  return GetButtonSize();
 }
 
 views::View* TabCloseButton::TargetForRect(views::View* root,
