a14f897ec48a4087bae7ee017a07b9ac7d9b14e7
Alex N. Jose <alexnj@chromium.org>
2026-01-29 14:33:38 -0800

    Fix flake introduced by out of order responses
    This CL fixes test flakiness caused by awaiting for the right
    event on the wrong response.
    
    Bug: 478033574
    Change-Id: Icff2f3ff90c5b2efdcb1e115189991e4d12b1c79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7533653
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Alex N. Jose <alexnj@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1576866}

--- third_party/blink/web_tests/http/tests/inspector-protocol/network/durable-messages-frames-sessions.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/network/durable-messages-frames-sessions.js
@@ -19,7 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     const requestWillBeSent = (await dp.Network.onceRequestWillBeSent()).params;
     testRunner.log(`Request for ${requestWillBeSent.request.url}`);
 
-    await dp.Network.onceLoadingFinished();
+    await dp.Network.onceLoadingFinished(
+      e => e.params.requestId === requestWillBeSent.requestId);
     const data = await dp.Network.getResponseBody(
       {requestId: requestWillBeSent.requestId});
     await logDigest(data.result.body);
