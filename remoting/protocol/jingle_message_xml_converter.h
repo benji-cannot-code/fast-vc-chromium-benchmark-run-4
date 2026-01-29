c31ed127ef9167023f368b89fc84e0ba48bf9fed
Joe Downing <joedow@google.com>
2026-01-29 15:12:47 -0800

    Refactor XMPP XML code out of JingleMessage - Phase 1
    I want to introduce a set of structs to use for holding JingleMessage
    related info and eventually remove our XMPP XML dependency. The issue
    is that the XML concepts and code are intertwined with a number of
    existing classes and the messages themselves are large so the
    resulting CL would be huge if done at once.
    
    This is the first CL in a chain which aims to transition /protocol
    to use a set of common structs which are signaling method agnostic.
    This CL starts by moving IceTransportInfo parsing code into a helper
    class.
    
    Bug: 359620500
    Change-Id: I04636cd6e17b685ec150b290912f3301a1784292
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7533739
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1576893}

---
+++ remoting/protocol/jingle_message_xml_converter.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef REMOTING_PROTOCOL_JINGLE_MESSAGE_XML_CONVERTER_H_
+#define REMOTING_PROTOCOL_JINGLE_MESSAGE_XML_CONVERTER_H_
+
+#include <memory>
+
+namespace jingle_xmpp {
+class XmlElement;
+}  // namespace jingle_xmpp
+
+namespace remoting::protocol {
+
+struct IceTransportInfo;
+
+// Converts between IceTransportInfo and its XML representation.
+std::unique_ptr<jingle_xmpp::XmlElement> IceTransportInfoToXml(
+    const IceTransportInfo& transport);
+bool IceTransportInfoFromXml(const jingle_xmpp::XmlElement* element,
+                             IceTransportInfo* transport);
+
+}  // namespace remoting::protocol
+
+#endif  // REMOTING_PROTOCOL_JINGLE_MESSAGE_XML_CONVERTER_H_
