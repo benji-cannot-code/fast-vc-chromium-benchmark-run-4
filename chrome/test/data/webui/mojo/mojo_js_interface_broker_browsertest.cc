5b30de24db93f9c073a17098a2cc1940526eb02e
Eriko Kurimoto <elkurin@chromium.org>
2026-01-28 18:16:08 -0800

    WaaP: Make MojoJSInterfaceBrokerBrowserTest more robust
    This CL improves the robustness of `MojoJSInterfaceBrokerBrowserTest` by
    fixing initialization timing and narrowing the WebUI bindings. These
    improvements also resolve a crash when the `WebUIReloadButton` feature
    is enabled.
    
    This CL intents to fix MojoJSInterfaceBrokerBrowserTest even with WebUI
    toolbar feature is enabled.
    
    Change-Id: I1da1defd52e0cde42a98c91d1cefd84736b3069c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7524561
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1576320}

--- chrome/test/data/webui/mojo/mojo_js_interface_broker_browsertest.cc
+++ chrome/test/data/webui/mojo/mojo_js_interface_broker_browsertest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chrome_content_browser_client.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "chrome/common/chrome_paths.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/data/grit/webui_test_resources.h"
 #include "chrome/test/data/webui/mojo/foobar.mojom.h"
@@ -23,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_ui_browser_interface_broker_registry.h"
 #include "content/public/browser/web_ui_controller_factory.h"
 #include "content/public/browser/web_ui_data_source.h"
+#include "content/public/common/bindings_policy.h"
 #include "content/public/common/content_client.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/common/url_constants.h"
@@ -68,6 +70,9 @@ class FooUI : public content::WebUIController, public ::test::mojom::Foo {
 
     // Allow requesting chrome-untrusted://bar in iframe.
     web_ui->AddRequestableScheme(content::kChromeUIUntrustedScheme);
+
+    web_ui->SetBindings(
+        content::BindingsPolicySet{content::BindingsPolicyValue::kMojoWebUi});
   }
 
   void BindInterface(mojo::PendingReceiver<::test::mojom::Foo> receiver) {
@@ -114,6 +119,9 @@ class BarUI : public ui::UntrustedWebUIController, public ::test::mojom::Bar {
                content::WebUIDataSource::GotDataCallback callback) {
               std::move(callback).Run(nullptr);
             }));
+
+    web_ui->SetBindings(
+        content::BindingsPolicySet{content::BindingsPolicyValue::kMojoWebUi});
   }
 
   void BindInterface(mojo::PendingReceiver<::test::mojom::Bar> receiver) {
@@ -204,14 +212,17 @@ class MojoJSInterfaceBrokerBrowserTest : public InProcessBrowserTest {
     content::WebUIControllerFactory::RegisterFactory(factory_.get());
   }
 
+  void CreatedBrowserMainParts(content::BrowserMainParts* parts) override {
+    InProcessBrowserTest::CreatedBrowserMainParts(parts);
+    content::SetBrowserClientForTesting(&test_content_browser_client_);
+  }
+
   void SetUpOnMainThread() override {
     base::FilePath pak_path;
     ASSERT_TRUE(base::PathService::Get(base::DIR_ASSETS, &pak_path));
     pak_path = pak_path.AppendASCII("browser_tests.pak");
     ui::ResourceBundle::GetSharedInstance().AddDataPackFromPath(
         pak_path, ui::kScaleFactorNone);
-
-    content::SetBrowserClientForTesting(&test_content_browser_client_);
   }
 
   void SetUpCommandLine(base::CommandLine* command_line) override {
@@ -261,12 +272,23 @@ class MojoJSInterfaceBrokerBrowserTest : public InProcessBrowserTest {
 
     void RegisterTrustedWebUIInterfaceBrokers(
         content::WebUIBrowserInterfaceBrokerRegistry& registry) override {
+      ChromeContentBrowserClient::RegisterTrustedWebUIInterfaceBrokers(
+          registry);
       registry.ForWebUI<FooUI>().Add<::test::mojom::Foo>();
     }
 
     void RegisterUntrustedWebUIInterfaceBrokers(
         content::WebUIBrowserInterfaceBrokerRegistry& registry) override {
+      ChromeContentBrowserClient::RegisterUntrustedWebUIInterfaceBrokers(
+          registry);
       registry.ForWebUI<BarUI>().Add<::test::mojom::Bar>();
+    }
+
+    std::vector<base::FilePath> GetNetworkContextsParentDirectory() override {
+      base::FilePath user_data_dir;
+      base::PathService::Get(chrome::DIR_USER_DATA, &user_data_dir);
+      DCHECK(!user_data_dir.empty());
+      return {user_data_dir};
     }
   };
 
