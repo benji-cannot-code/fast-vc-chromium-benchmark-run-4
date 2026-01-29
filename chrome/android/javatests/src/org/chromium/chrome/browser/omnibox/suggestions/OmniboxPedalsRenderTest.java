7d34a771b4d070e7e63a8a01b0695f950566fb06
Luchen Peng <luchenpeng@google.com>
2026-01-29 06:54:57 -0800

    [Gardener] Disable flaky OmniboxPedalsRenderTest
    Bug: 479724464
    Change-Id: I3380e6441359fbcc1573e3c7b793e8d2ca08ac03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7532175
    Owners-Override: Luchen Peng <luchenpeng@google.com>
    Reviewed-by: Ioana Treib <ioanap@chromium.org>
    Commit-Queue: Luchen Peng <luchenpeng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1576566}

--- chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/suggestions/OmniboxPedalsRenderTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/suggestions/OmniboxPedalsRenderTest.java
@@ -20,6 +20,7 @@ import org.chromium.base.test.params.ParameterAnnotations;
 import org.chromium.base.test.params.ParameterSet;
 import org.chromium.base.test.params.ParameterizedRunner;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.night_mode.ChromeNightModeTestUtils;
@@ -39,6 +40,7 @@ import org.chromium.components.omnibox.AutocompleteResult;
 import org.chromium.components.omnibox.OmniboxFeatures;
 import org.chromium.components.omnibox.OmniboxSuggestionType;
 import org.chromium.components.omnibox.action.OmniboxPedalId;
+import org.chromium.ui.base.DeviceFormFactor;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -138,6 +140,7 @@ public class OmniboxPedalsRenderTest {
     @Test
     @MediumTest
     @Feature("RenderTest")
+    @DisableIf.Device(DeviceFormFactor.DESKTOP) // https://crbug.com/479724464
     public void testPlayChromeDinoGamePedal() throws IOException, InterruptedException {
         List<AutocompleteMatch> suggestionsList = new ArrayList<>();
         suggestionsList.add(
