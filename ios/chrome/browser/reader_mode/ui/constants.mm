ab5465d76caae23de9131fe2aadefd040b4c4236
Nohemi Fernandez <fernandex@google.com>
2026-01-29 08:14:31 -0800

    [iOS] Add feature to disable links in Reading Mode via Content Settings.
    Adds a Reading Mode section to Content Settings that includes both the
    option to enable/disable links and the existing option to enable/disable
    the Reading Mode entry point.
    
    Screenshots: crbug.com/463918572#comment4
    
    Bug: 463918572
    Change-Id: I16dbdcd76bf904083b1c0ad1889ff92f8fc4d001
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7511851
    Reviewed-by: Quentin Pubert <qpubert@google.com>
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@google.com>
    Cr-Commit-Position: refs/heads/main@{#1576607}

--- ios/chrome/browser/reader_mode/ui/constants.mm
+++ ios/chrome/browser/reader_mode/ui/constants.mm
@@ -40,6 +40,12 @@ NSString* const kReaderModeOptionsCloseButtonAccessibilityIdentifier =
 NSString* const kReaderModeOptionsTurnOffButtonAccessibilityIdentifier =
     @"ReaderModeOptionsTurnOffButtonAccessibilityIdentifier";
 
+NSString* const kReaderModeSettingsShowSuggestionAccessibilityIdentifier =
+    @"ReaderModeSettingsShowSuggestionAccessibilityIdentifier";
+
+NSString* const kReaderModeSettingsShowHyperlinksAccessibilityIdentifier =
+    @"ReaderModeSettingsShowHyperlinksAccessibilityIdentifier";
+
 UIColor* ReaderModeLightBackgroundColor() {
   return [UIColor whiteColor];
 }
