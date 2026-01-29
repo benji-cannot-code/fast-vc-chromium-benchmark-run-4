74b9ff9cf94c5d20afa768f40e94a922593a999f
Dhruv Kathpalia <dhruvkathpalia@google.com>
2026-01-29 10:18:17 -0800

    [composebox] Open Composebox on Model selection and reset on close
    When a ModelMode is selected in the searchbox, open the composebox and
    initialize it with the chosen model. Additionally, ensure that both
    ToolMode and ModelMode are reset to their unspecified states when the
    composebox is closed.
    
    Screencast: 
    http: //screencast/cast/NTIxOTAxMDYwOTE1MjAwMHw2ZjM1MGZjZC1lMw
    Change-Id: Iaf09d43bc6fa3bec6d3ea66ccf2eb1c85c454b2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7530457
    Auto-Submit: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1576699}

--- ui/webui/resources/cr_components/composebox/composebox.ts
+++ ui/webui/resources/cr_components/composebox/composebox.ts
@@ -27,8 +27,8 @@ import {hasKeyModifiers} from '//resources/js/util.js';
 import type {PropertyValues} from '//resources/lit/v3_0/lit.rollup.js';
 import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
 import type {AutocompleteMatch, AutocompleteResult, PageCallbackRouter as SearchboxPageCallbackRouter, PageHandlerRemote as SearchboxPageHandlerRemote, SearchContext, SelectedFileInfo, TabInfo} from '//resources/mojo/components/omnibox/browser/searchbox.mojom-webui.js';
-import type {InputState, ModelMode} from '//resources/mojo/components/omnibox/composebox/composebox_query.mojom-webui.js';
-import {ToolMode} from '//resources/mojo/components/omnibox/composebox/composebox_query.mojom-webui.js';
+import type {InputState} from '//resources/mojo/components/omnibox/composebox/composebox_query.mojom-webui.js';
+import {ToolMode, ModelMode} from '//resources/mojo/components/omnibox/composebox/composebox_query.mojom-webui.js';
 import type {BigBuffer} from '//resources/mojo/mojo/public/mojom/base/big_buffer.mojom-webui.js';
 import type {UnguessableToken} from '//resources/mojo/mojo/public/mojom/base/unguessable_token.mojom-webui.js';
 import type {Url} from '//resources/mojo/url/mojom/url.mojom-webui.js';
@@ -544,7 +544,8 @@ export class ComposeboxElement extends I18nMixinLit
 
   protected initializeState_(
       text: string = '', files: ContextualUpload[] = [],
-      mode: ToolMode = ToolMode.kUnspecified) {
+      mode: ToolMode = ToolMode.kUnspecified,
+      model: ModelMode = ModelMode.kUnspecified) {
     if (text) {
       this.input_ = text;
       this.lastQueriedInput_ = text;
@@ -557,6 +558,9 @@ export class ComposeboxElement extends I18nMixinLit
     }
     if (mode !== ToolMode.kUnspecified) {
       this.$.context.setInitialMode(mode);
+    }
+    if (model !== ModelMode.kUnspecified) {
+      this.searchboxHandler_.setActiveModelMode(model);
     }
   }
 
@@ -1163,6 +1167,8 @@ export class ComposeboxElement extends I18nMixinLit
 
   private closeComposebox_() {
     this.resetModes();
+    this.searchboxHandler_.setActiveToolMode(ToolMode.kUnspecified);
+    this.searchboxHandler_.setActiveModelMode(ModelMode.kUnspecified);
     this.fire('close-composebox', {composeboxText: this.input_});
 
     if (this.isCollapsible) {
