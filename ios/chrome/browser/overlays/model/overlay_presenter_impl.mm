fcc7299b2d528d829c8a173bf73f1664e2fdda1a
Sylvain Defresne <sdefresne@chromium.org>
2026-01-29 02:30:51 -0800

    [ios] Remove use of CreateTabHelperOnlyForRealizedWebStates()
    As CreateTabHelperOnlyForRealizedWebStates() is always true, remove
    usage in OverlayRequestQueueImpl.
    
    Bug: 479194758
    Change-Id: I54851baf73bd49c79cc489c510d9c93cfd4e0010
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7527152
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1576460}

--- ios/chrome/browser/overlays/model/overlay_presenter_impl.mm
+++ ios/chrome/browser/overlays/model/overlay_presenter_impl.mm
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/overlays/model/public/overlay_request.h"
 #import "ios/chrome/browser/overlays/model/public/overlay_request_support.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
-#import "ios/web/common/features.h"
 
 #pragma mark - Factory method
 
@@ -175,14 +174,10 @@ void OverlayPresenterImpl::SetActiveWebState(web::WebState* web_state) {
 
 OverlayRequestQueueImpl* OverlayPresenterImpl::GetQueueForWebState(
     web::WebState* web_state) const {
-  if (!web_state) {
+  if (!web_state || !web_state->IsRealized()) {
     return nullptr;
   }
-  if (web::features::CreateTabHelperOnlyForRealizedWebStates()) {
-    if (!web_state->IsRealized()) {
-      return nullptr;
-    }
-  }
+
   OverlayRequestQueueImpl::Container::CreateForWebState(web_state);
   return OverlayRequestQueueImpl::Container::FromWebState(web_state)
       ->QueueForModality(modality_);
