a2e1730967c35ac3686fe8c72584e4f2f60a2765
Diana OuYang <dianaou@google.com>
2026-01-29 12:47:24 -0800

    Determine Zero State from AIM-side instead of client
    This CL implements the client-side to receive Silk API notifications of
    when Chrome when the AIM zero state is rendered.
    
    - Adds NOTIFY_ZERO_STATE_RENDERED capability to ai_mode.proto and aim.proto.
    - Adds enable_notify_zero_state_rendered_capability feature flag (default false).
    - Adds browser test for the new functionality.
    
    Bug: b:475862095
    Change-Id: I8c5baafbfeef44c58d6c1d43373e5a785fd9c5d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7487989
    Auto-Submit: Diana OuYang <dianaou@google.com>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1576793}

--- chrome/browser/contextual_tasks/contextual_tasks_page_handler_unittest.cc
+++ chrome/browser/contextual_tasks/contextual_tasks_page_handler_unittest.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/test_web_ui.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/lens_server_proto/aim_communication.pb.h"
 
 namespace contextual_tasks {
 
@@ -385,6 +386,42 @@ TEST_F(ContextualTasksPageHandlerTest, OnWebviewMessage_RestoreInput) {
   message.SerializeToArray(serialized.data(), size);
 
   EXPECT_CALL(page_, RestoreInput()).Times(1);
+
+  page_handler_->OnWebviewMessage(serialized);
+}
+
+TEST_F(ContextualTasksPageHandlerTest,
+       OnWebviewMessage_NotifyZeroStateRendered) {
+  base::test::ScopedFeatureList feature_list;
+  feature_list.InitAndEnableFeature(kEnableNotifyZeroStateRenderedCapability);
+
+  lens::AimToClientMessage message;
+  message.mutable_notify_zero_state_rendered()->set_is_zero_state_rendered(
+      true);
+
+  size_t size = message.ByteSizeLong();
+  std::vector<uint8_t> serialized(size);
+  message.SerializeToArray(serialized.data(), size);
+
+  EXPECT_CALL(page_, OnZeroStateChange(true)).Times(1);
+
+  page_handler_->OnWebviewMessage(serialized);
+}
+
+TEST_F(ContextualTasksPageHandlerTest,
+       OnWebviewMessage_NotifyZeroStateRendered_False) {
+  base::test::ScopedFeatureList feature_list;
+  feature_list.InitAndEnableFeature(kEnableNotifyZeroStateRenderedCapability);
+
+  lens::AimToClientMessage message;
+  message.mutable_notify_zero_state_rendered()->set_is_zero_state_rendered(
+      false);
+
+  size_t size = message.ByteSizeLong();
+  std::vector<uint8_t> serialized(size);
+  message.SerializeToArray(serialized.data(), size);
+
+  EXPECT_CALL(page_, OnZeroStateChange(false)).Times(1);
 
   page_handler_->OnWebviewMessage(serialized);
 }
