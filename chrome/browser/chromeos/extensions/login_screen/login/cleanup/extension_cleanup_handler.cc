b7b57eddf6a827705001b63d2a24f33f9fc98452
Jeffrey Yu <yuje@chromium.org>
2026-01-29 14:54:35 -0800

    Replace `std::unordered_set` with `absl::flat_hash_set/base::flat_set`.
    The Chromium containers guide
    (https://chromium.googlesource.com/chromium/src/+/HEAD/base/containers/README.md)
    recommends using `absl::flat_hash_set` as a default map choice instead
    of `std::unordered_set` when pointer stability isn't needed, and
    `absl::node_hash_set` when it is. Both will still generally perform
    better and require less overhead than `std::unordered_set`.
    
    Bug: 473916362
    Change-Id: I83ee53e807d1ebb1021a4e1384cb109e67c57efe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7509662
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: Jeffrey Yu <yuje@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1576884}

--- chrome/browser/chromeos/extensions/login_screen/login/cleanup/extension_cleanup_handler.cc
+++ chrome/browser/chromeos/extensions/login_screen/login/cleanup/extension_cleanup_handler.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
+#include "base/containers/flat_set.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/browser_process.h"
@@ -23,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_system.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/extension_set.h"
+#include "third_party/abseil-cpp/absl/container/flat_hash_set.h"
 
 // Extensions that should not be attempted to be uninstalled and reinstalled.
 const char* const kExemptExtensions[] = {
@@ -58,7 +60,7 @@ void ExtensionCleanupHandler::Cleanup(CleanupHandlerCallback callback) {
 }
 
 void ExtensionCleanupHandler::UninstallExtensions() {
-  std::unordered_set<std::string> policy_exempt_extensions =
+  base::flat_set<std::string> policy_exempt_extensions =
       GetCleanupExemptExtensions();
 
   auto* extension_registry = extensions::ExtensionRegistry::Get(profile_);
@@ -98,7 +100,7 @@ void ExtensionCleanupHandler::UninstallExtensions() {
 
     if (!error.empty()) {
       errors_.push_back(*it + ": " + base::UTF16ToUTF8(error));
-      it = extensions_to_be_uninstalled_.erase(it);
+      extensions_to_be_uninstalled_.erase(it++);
     } else {
       ++it;
     }
@@ -140,14 +142,15 @@ void ExtensionCleanupHandler::ReinstallExtensions() {
   std::move(callback_).Run(std::nullopt);
 }
 
-std::unordered_set<std::string>
+base::flat_set<std::string>
 ExtensionCleanupHandler::GetCleanupExemptExtensions() {
-  std::unordered_set<std::string> exempt_extensions;
   const base::ListValue& exempt_list = profile_->GetPrefs()->GetList(
       prefs::kRestrictedManagedGuestSessionExtensionCleanupExemptList);
 
+  std::vector<std::string> exempt_extensions;
+  exempt_extensions.reserve(exempt_list.size());
   for (const base::Value& value : exempt_list) {
-    exempt_extensions.insert(value.GetString());
+    exempt_extensions.push_back(value.GetString());
   }
   return exempt_extensions;
 }
