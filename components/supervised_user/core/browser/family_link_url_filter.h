c333e6d2495f74cbf162fb95b975f8cf0b2f23b2
Tomek Jurkiewicz <tju@google.com>
2026-01-29 01:40:52 -0800

    Use WeakPtr in FamilyLinkUrlFilter::CheckURL callback
    Replaces base::Unretained with a WeakPtr in the FamilyLinkUrlFilter::CheckURL callback to ensure the callback is not invoked if the FamilyLinkUrlFilter instance has been destroyed.
    
    Change-Id: Id7a7ba1019d167532f327fca115d59949d5228e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7526867
    Auto-Submit: Tomek Jurkiewicz <tju@google.com>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1576432}

--- components/supervised_user/core/browser/family_link_url_filter.h
+++ components/supervised_user/core/browser/family_link_url_filter.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback.h"
 #include "base/functional/callback_forward.h"
+#include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
 #include "base/sequence_checker.h"
 #include "components/safe_search_api/url_checker.h"
@@ -245,6 +246,8 @@ class FamilyLinkUrlFilter : public UrlFilteringDelegate {
   base::CallbackListSubscription family_link_settings_subscription_;
 
   SEQUENCE_CHECKER(sequence_checker_);
+
+  base::WeakPtrFactory<FamilyLinkUrlFilter> weak_factory_{this};
 };
 
 }  // namespace supervised_user
