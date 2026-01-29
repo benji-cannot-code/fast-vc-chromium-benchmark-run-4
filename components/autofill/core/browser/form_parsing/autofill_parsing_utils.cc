9879156ae814b535e763747a7d68c8c02a9bfa4c
Jihad Hanna <jihadghanna@google.com>
2026-01-29 14:46:53 -0800

    Add a feature flag for new PHONE_COUNTRY_CODE regex
    Bug: 479503511
    Change-Id: I395fcbe8e47d84c6493239b941177a2ea67e3dbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7525532
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1576875}

--- components/autofill/core/browser/form_parsing/autofill_parsing_utils.cc
+++ components/autofill/core/browser/form_parsing/autofill_parsing_utils.cc
@@ -37,6 +37,8 @@ const base::Feature* GetFeatureOfRegexFeature(RegexFeature feature) {
       return &features::kAutofillFixCivilStateMisclassificationForESPT;
     case RegexFeature::kAutofillAddressImproveBuildingNumberRegex:
       return &features::kAutofillAddressImproveBuildingNumberRegex;
+    case RegexFeature::kAutofillNewRegexForPhoneCountryCode:
+      return &features::kAutofillNewRegexForPhoneCountryCode;
   }
   NOTREACHED();
 }
