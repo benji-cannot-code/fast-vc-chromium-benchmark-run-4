de4cbeda24586d1c9e471af8b9eedae7ad092075
Di Wu <diwux@google.com>
2026-01-29 05:51:59 -0800

    Fix typo and remove unused parameter in RootUiCoordinator
    This CL addresses two code health issues in RootUiCoordinator.java.
    
    * Fixes a typo in the Javadoc for getIncognitoReauthCoordinatorFactory
      ("Coordiantor" -> "Coordinator").
    
    * Removes the unused `isIncognito` parameter from
      onShareMenuItemSelected(). This parameter appears to be a vestige of
      the SharingHub refactoring (crrev.com/c/1850367) and is no longer used
      in the method body, as the ShareDelegate receives the profile/tab
      context directly.
    
    This CL also reformats the Javadoc for onShareMenuItemSelected to align
    with the Google Java Style Guide.
    
    Bug: None
    Test: CQ
    Change-Id: Id3569b41adf5fd8c0243a1a0126434fb67a317c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7509473
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Di Wu <diwux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1576533}

--- chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
@@ -1441,7 +1441,7 @@ public class RootUiCoordinator
      * This method is meant to be overridden for sub-classes which needs to provide an incognito
      * re-auth view.
      *
-     * @return {@link IncognitoReauthCoordiantorFactory} instance.
+     * @return {@link IncognitoReauthCoordinatorFactory} instance.
      */
     protected IncognitoReauthCoordinatorFactory getIncognitoReauthCoordinatorFactory(
             Profile profile) {
@@ -1500,14 +1500,14 @@ public class RootUiCoordinator
     }
 
     /**
-     * Triggered when the share menu item is selected.
-     * This creates and shows a share intent picker dialog or starts a share intent directly.
+     * Triggered when the share menu item is selected. This creates and shows a share intent picker
+     * dialog or starts a share intent directly.
+     *
      * @param shareDirectly Whether it should share directly with the activity that was most
-     *                      recently used to share.
-     * @param isIncognito Whether currentTab is incognito.
+     *     recently used to share.
      */
     @VisibleForTesting
-    public void onShareMenuItemSelected(final boolean shareDirectly, final boolean isIncognito) {
+    public void onShareMenuItemSelected(final boolean shareDirectly) {
         ShareDelegate shareDelegate = mShareDelegateSupplier.get();
         Tab tab = mActivityTabProvider.get();
 
@@ -1557,9 +1557,7 @@ public class RootUiCoordinator
             }
             return true;
         } else if (id == R.id.share_menu_id || id == R.id.direct_share_menu_id) {
-            onShareMenuItemSelected(
-                    id == R.id.direct_share_menu_id,
-                    mTabModelSelectorSupplier.get().isIncognitoSelected());
+            onShareMenuItemSelected(id == R.id.direct_share_menu_id);
             return true;
         } else if (id == R.id.paint_preview_show_id) {
             DemoPaintPreview.showForTab(mActivityTabProvider.get());
