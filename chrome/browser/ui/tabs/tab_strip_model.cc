693e0067edef55c8686e03abbd26eb512937447c
Dominic Austria <dominicaustria@google.com>
2026-01-28 19:45:31 -0800

    [SelectionPtr] Add even more cached LSM invalidation
    It would be safer to invalidate after every non const contents_data_ operation to make sure the selection_model_ and contents_data_ are in sync.
    
    Bug: 478921664
    Change-Id: I9a8be0d196548b569caff0888435c0f371348ea8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7528716
    Commit-Queue: Dominic Austria <dominicaustria@google.com>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1576346}

--- chrome/browser/ui/tabs/tab_strip_model.cc
+++ chrome/browser/ui/tabs/tab_strip_model.cc
@@ -4257,6 +4257,7 @@ split_tabs::SplitTabId TabStripModel::AddToSplitImpl(
                                IsTabPinned(pivot_index));
 
   contents_data_->CreateSplit(split_id, tabs, visual_data);
+  selection_model_.InvalidateListSelectionModel(base::PassKey<TabStripModel>());
 
   std::vector<std::pair<tabs::TabInterface*, int>> tabs_with_indices;
   for (tabs::TabInterface* tab : tabs) {
@@ -4300,6 +4301,7 @@ void TabStripModel::RemoveSplitImpl(
       GetTabsAndIndicesInSplit(split_id);
 
   contents_data_->Unsplit(split_id);
+  selection_model_.InvalidateListSelectionModel(base::PassKey<TabStripModel>());
 
   tabs::TabStripModelSelectionState old_selection_model = selection_model_;
 
@@ -4456,6 +4458,7 @@ void TabStripModel::AddToNewGroupImpl(
   group_model_->AddTabGroup(group_collection->GetTabGroup(),
                             base::PassKey<TabStripModel>());
   contents_data_->CreateTabGroup(std::move(group_collection));
+  selection_model_.InvalidateListSelectionModel(base::PassKey<TabStripModel>());
 
   // Find a destination for the first tab that's not pinned or inside another
   // group. We will stack the rest of the tabs up to its right.
@@ -4839,6 +4842,8 @@ void TabStripModel::TabGroupStateChanged(
                                    base::PassKey<TabStripModel>());
       CompleteModelUpdateTransaction();
       contents_data_->CloseDetachedTabGroup(initial_group.value());
+      selection_model_.InvalidateListSelectionModel(
+          base::PassKey<TabStripModel>());
     }
   }
 
