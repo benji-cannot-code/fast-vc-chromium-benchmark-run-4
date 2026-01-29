ba915afb3009b6382660106e0d33d5cb2b791c79
Stepan Khapugin <stkhapugin@chromium.org>
2026-01-29 10:58:11 -0800

    [iOS] Add Share Ablation experiment flags.
    Adds flags for go/bling-share-ablation-dd experiment.
    
    Bug: 479520755
    Change-Id: I8914424b8f996f42308414cc426fa861f0e3b48b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7526668
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1576721}

--- ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
+++ ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
@@ -289,6 +289,9 @@ extern const char kDisableKeyboardAccessoryDescription[];
 extern const char kDisableLensCameraName[];
 extern const char kDisableLensCameraDescription[];
 
+extern const char kDisableShareButtonName[];
+extern const char kDisableShareButtonDescription[];
+
 extern const char kDownloadAutoDeletionClearFilesOnEveryStartupName[];
 extern const char kDownloadAutoDeletionClearFilesOnEveryStartupDescription[];
 
@@ -856,6 +859,15 @@ extern const char
 
 extern const char kSendTabToSelfIOSPushNotificationsName[];
 extern const char kSendTabToSelfIOSPushNotificationsDescription[];
+
+extern const char kShareInOmniboxLongPressName[];
+extern const char kShareInOmniboxLongPressDescription[];
+
+extern const char kShareInOverflowMenuName[];
+extern const char kShareInOverflowMenuDescription[];
+
+extern const char kShareInVerbatimMatchName[];
+extern const char kShareInVerbatimMatchDescription[];
 
 extern const char kSharedHighlightingIOSName[];
 extern const char kSharedHighlightingIOSDescription[];
