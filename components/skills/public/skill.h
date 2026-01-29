8f413d9a133fdf45db6d02760bd993e5b1c35c0a
Kevin McNee <mcnee@chromium.org>
2026-01-29 09:50:32 -0800

    Revert "Add `source` field to `Skill` mojom to match the c++ struct."
    This reverts commit 35a4ee80a6c15eb3e420a554496adbd30b7fd7e9.
    
    Reason for revert: Broke the build https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Builder/215965/overview
    
    Original change's description:
    > Add `source` field to `Skill` mojom to match the c++ struct.
    >
    > Also add in if/then lint comments for skills related data structures.
    >
    > Change-Id: I47ef151684a678af5b6f8de008aac6d22a67e1fd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7522867
    > Reviewed-by: Fred Shih <ffred@chromium.org>
    > Reviewed-by: Rushan Suleymanov <rushans@google.com>
    > Commit-Queue: marissa shen <marissashen@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1576670}
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ic33b89a647e0f32f365f083e5c7019bc97ca44f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7533084
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1576676}

--- components/skills/public/skill.h
+++ components/skills/public/skill.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace skills {
 
-// LINT.IfChange(SkillSource)
 enum class SkillSource {
   kUnknown = 0,
   // Skill created by Google.
@@ -18,10 +17,7 @@ enum class SkillSource {
   // Skill created by an end-user.
   kUserCreated = 2,
 };
-// LINT.ThenChange(//depot/chromium/components/skills/public/skill.mojom:SkillSource,
-// //depot/chromium/chrome/browser/glic/host/glic.mojom:SkillSource)
 
-// LINT.IfChange(Skill)
 // Represents a single skill.
 struct Skill {
   // A unique identifier for the skill. It's GUID now but can be other IDs in
@@ -48,8 +44,6 @@ struct Skill {
   Skill& operator=(const Skill& other) = delete;
   ~Skill();
 };
-// LINT.ThenChange(//depot/chromium/components/skills/public/skill.mojom:Skill,
-// //depot/chromium/chrome/browser/glic/host/glic.mojom:Skill)
 
 }  // namespace skills
 
