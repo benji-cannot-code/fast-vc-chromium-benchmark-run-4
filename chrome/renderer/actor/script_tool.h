bd1177a4fa24a427fbe5bdcf451c8f979e36b1ab
Ben Greenstein <bengr@chromium.org>
2026-01-28 16:13:32 -0800

    Added support for imperative script tool cancellation.
    Bug: 475998009
    Change-Id: I97044d992ad477f9fc5b3d1240a3b22aca3e3ecb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7518247
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1576259}

--- chrome/renderer/actor/script_tool.h
+++ chrome/renderer/actor/script_tool.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_RENDERER_ACTOR_SCRIPT_TOOL_H_
 
 #include <cstdint>
+#include <optional>
 
 #include "chrome/common/actor.mojom.h"
 #include "chrome/common/actor/task_id.h"
@@ -32,10 +33,12 @@ class ScriptTool : public ToolBase {
 
   // actor::ToolBase
   void Execute(ToolFinishedCallback callback) override;
+  void Cancel() override;
   std::string DebugString() const override;
 
  private:
   mojom::ScriptToolActionPtr action_;
+  std::optional<uint32_t> execution_id_;
 };
 
 }  // namespace actor
