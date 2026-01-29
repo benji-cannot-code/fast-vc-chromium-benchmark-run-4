c11802e3e1216bf25c24edcc504c14e76a583e84
Morten Stenshorne <mstensho@chromium.org>
2026-01-29 08:52:00 -0800

    Support `page` property on monolithic content.
    If monolithic content sets a page name, we should break before it to
    establish the correct page type.
    
    Hoist setting page names from CSS one level up in the tree, so that the
    container applies it for the child, instead of having the child apply it
    on itself. Page names only honored for nodes that participate in block
    fragmentation. Monolithic content forms an exit-boundary from block
    fragmentation, and therefore page names were incorrectly ignored.
    
    Bug: 40286326
    Change-Id: Iea287cd1d49ccb87717580efacf1b8d1578ea31f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7532907
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1576641}

--- third_party/blink/renderer/core/layout/simplified_oof_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/simplified_oof_layout_algorithm.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/layout/simplified_oof_layout_algorithm.h"
 
+#include "third_party/blink/renderer/core/layout/fragmentation_utils.h"
 #include "third_party/blink/renderer/core/layout/layout_result.h"
 #include "third_party/blink/renderer/core/layout/physical_box_fragment.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
@@ -20,7 +21,8 @@ SimplifiedOofLayoutAlgorithm::SimplifiedOofLayoutAlgorithm(
   DCHECK(params.space.HasKnownFragmentainerBlockSize());
 
   container_builder_.SetBoxType(last_fragmentainer.GetBoxType());
-  container_builder_.SetPageNameIfNeeded(last_fragmentainer.PageName());
+  container_builder_.SetPageNameIfNeeded(
+      PageNameForChildFragment(container_builder_, last_fragmentainer));
   container_builder_.SetFragmentBlockSize(
       params.space.FragmentainerBlockSize());
   container_builder_.SetHasOutOfFlowFragmentChild(true);
