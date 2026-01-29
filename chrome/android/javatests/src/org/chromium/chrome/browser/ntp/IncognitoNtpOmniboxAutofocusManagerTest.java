dd585dafcafa292c5421c83fb2f6cf7a08d8cf16
Oleksii Kalinin <kalinino@google.com>
2026-01-29 02:03:05 -0800

    Hide cursor in Incognito NTP Omnibox Autofocus render test.
    Disable the cursor visibility in the UrlBar to reduce flakiness in
    render tests caused by the blinking cursor.
    
    Fixed: 477136666
    Change-Id: I303a29f4f7a6dfb446a6d67b9a379d955708a391
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7526154
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Oleksii Kalinin <kalinino@google.com>
    Reviewed-by: Kamil Jarosz <kjarosz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1576443}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ntp/IncognitoNtpOmniboxAutofocusManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ntp/IncognitoNtpOmniboxAutofocusManagerTest.java
@@ -38,6 +38,7 @@ import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.layouts.LayoutTestUtils;
 import org.chromium.chrome.browser.layouts.LayoutType;
+import org.chromium.chrome.browser.omnibox.UrlBar;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabLaunchType;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
@@ -576,13 +577,18 @@ public class IncognitoNtpOmniboxAutofocusManagerTest {
                 mActivityTestRule.loadUrlInNewTab(getOriginalNativeNtpUrl(), true);
         verifyPhoneOmniboxFocusAndKeyboardVisibility(true, incognitoNtpTab);
 
-        // Disable scrollbar to avoid screenshot diffs due to fading animation.
+        // Disable scrollbar and cursor to avoid screenshot diffs due to fading animation.
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
                     View ntpScrollView =
                             mActivityTestRule.getActivity().findViewById(R.id.ntp_scrollview);
                     if (ntpScrollView != null) {
                         ntpScrollView.setVerticalScrollBarEnabled(false);
+                    }
+
+                    UrlBar urlBar = mActivityTestRule.getActivity().findViewById(R.id.url_bar);
+                    if (urlBar != null) {
+                        urlBar.setCursorVisible(false);
                     }
                 });
 
