a03e8ff3b730dd7444abc71bedea5588fd383d4e
Eleanor Lee <eleanorlee@google.com>
2026-01-29 23:19:37 -0800

    [Entry Point] Add metrics for engagement and user eligibility
    This CL covers a) and d) in the "Metrics & Analysis Plan" section in this doc:
    
    https://docs.google.com/document/d/1mTZE32HMfyIF8GtO1pMUXnV0UsgI33ZgERirlqd-cik/edit?resourcekey=0-aX75d69N_h7XNWao8FWLow&tab=t.0#heading=h.fhk63wxyit34
    
    Conducted manual testing in Chrome://histograms.
    
    Bug: 479052060
    Change-Id: I2b0aa4ac53c65ac48b1a364a8f03e804bfab1c4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7525150
    Commit-Queue: Eleanor Lee <eleanorlee@google.com>
    Reviewed-by: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1577064}

--- chrome/browser/ui/android/default_browser_promo/java/src/org/chromium/chrome/browser/ui/default_browser_promo/DefaultBrowserPromoUtilsTest.java
+++ chrome/browser/ui/android/default_browser_promo/java/src/org/chromium/chrome/browser/ui/default_browser_promo/DefaultBrowserPromoUtilsTest.java
@@ -40,6 +40,8 @@ import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
+import org.chromium.base.test.util.HistogramWatcher;
+import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.browser.feature_engagement.TrackerFactory;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
@@ -47,6 +49,8 @@ import org.chromium.chrome.browser.preferences.ChromePreferenceKeys;
 import org.chromium.chrome.browser.preferences.ChromeSharedPreferences;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.ui.default_browser_promo.DefaultBrowserPromoUtils.DefaultBrowserPromoTriggerStateListener;
+import org.chromium.chrome.browser.util.DefaultBrowserInfo;
+import org.chromium.chrome.browser.util.DefaultBrowserInfo.DefaultBrowserState;
 import org.chromium.components.feature_engagement.Tracker;
 import org.chromium.components.messages.ManagedMessageDispatcher;
 import org.chromium.components.messages.MessageBannerProperties;
@@ -81,6 +85,26 @@ public class DefaultBrowserPromoUtilsTest {
     private ShadowRoleManager mShadowRoleManager;
 
     DefaultBrowserPromoUtils mUtils;
+
+    private static class TestingDefaultBrowserPromoUtils extends DefaultBrowserPromoUtils {
+
+        private final DefaultBrowserInfo.DefaultInfo mTestInfo;
+
+        TestingDefaultBrowserPromoUtils(
+                DefaultBrowserPromoImpressionCounter impressionCounter,
+                DefaultBrowserStateProvider stateProvider,
+                DefaultBrowserInfo.DefaultInfo testInfo) {
+            super(impressionCounter, stateProvider);
+            mTestInfo = testInfo;
+        }
+
+        @Override
+        protected void fetchDefaultBrowserInfo(
+                org.chromium.base.Callback<DefaultBrowserInfo.@Nullable DefaultInfo> callback) {
+            // This approach is used so that the lambda code is returned immediately.
+            callback.onResult(mTestInfo);
+        }
+    }
 
     @Before
     public void setUp() {
@@ -344,7 +368,32 @@ public class DefaultBrowserPromoUtilsTest {
         // role is available but not held (Chrome is not set to default).
         when(mCounter.getPromoCount()).thenReturn(0);
 
-        mUtils.onMenuItemClick(mActivity, mWindowAndroid);
+        when(mProvider.getCurrentDefaultBrowserState()).thenReturn(DefaultBrowserState.NO_DEFAULT);
+
+        HistogramWatcher watcher =
+                HistogramWatcher.newBuilder()
+                        .expectIntRecord(
+                                "Android.DefaultBrowserPromo.EntryPoint.AppMenu",
+                                DefaultBrowserState.NO_DEFAULT)
+                        .build();
+
+        DefaultBrowserInfo.DefaultInfo info =
+                new DefaultBrowserInfo.DefaultInfo(
+                        /* defaultBrowserState= */ DefaultBrowserState.NO_DEFAULT,
+                        /* isChromeSystem= */ false,
+                        /* isDefaultSystem= */ false,
+                        /* browserCount= */ 1,
+                        /* systemCount= */ 0,
+                        /* isChromePreStableInstalled= */ false);
+
+        reCreateUtilsWithTestInfo(info);
+
+        mUtils.onMenuItemClick(
+                mActivity,
+                mWindowAndroid,
+                DefaultBrowserPromoUtils.DefaultBrowserPromoEntryPoint.APP_MENU);
+
+        watcher.assertExpected();
 
         // Verify we incremented the counter.
         verify(mCounter).onPromoShown();
@@ -370,7 +419,21 @@ public class DefaultBrowserPromoUtilsTest {
         // Chrome is already the default browser (Role is Held).
         mShadowRoleManager.addHeldRole(RoleManager.ROLE_BROWSER);
 
-        mUtils.onMenuItemClick(mActivity, mWindowAndroid);
+        DefaultBrowserInfo.DefaultInfo info =
+                new DefaultBrowserInfo.DefaultInfo(
+                        /* defaultBrowserState= */ DefaultBrowserState.CHROME_DEFAULT,
+                        /* isChromeSystem= */ true,
+                        /* isDefaultSystem= */ true,
+                        /* browserCount= */ 0,
+                        /* systemCount= */ 1,
+                        /* isChromePreStableInstalled= */ false);
+
+        reCreateUtilsWithTestInfo(info);
+
+        mUtils.onMenuItemClick(
+                mActivity,
+                mWindowAndroid,
+                DefaultBrowserPromoUtils.DefaultBrowserPromoEntryPoint.APP_MENU);
 
         // Should not increment counter since we skipped Role Manager.
         verify(mCounter, never()).onPromoShown();
@@ -385,7 +448,21 @@ public class DefaultBrowserPromoUtilsTest {
         // Promo Count > 0 (Already shown once). Chrome is not set to default.
         when(mCounter.getPromoCount()).thenReturn(1);
 
-        mUtils.onMenuItemClick(mActivity, mWindowAndroid);
+        DefaultBrowserInfo.DefaultInfo info =
+                new DefaultBrowserInfo.DefaultInfo(
+                        /* defaultBrowserState= */ DefaultBrowserState.NO_DEFAULT,
+                        /* isChromeSystem= */ false,
+                        /* isDefaultSystem= */ false,
+                        /* browserCount= */ 1,
+                        /* systemCount= */ 0,
+                        /* isChromePreStableInstalled= */ false);
+
+        reCreateUtilsWithTestInfo(info);
+
+        mUtils.onMenuItemClick(
+                mActivity,
+                mWindowAndroid,
+                DefaultBrowserPromoUtils.DefaultBrowserPromoEntryPoint.APP_MENU);
 
         // Should not increment counter again.
         verify(mCounter, never()).onPromoShown();
@@ -400,9 +477,30 @@ public class DefaultBrowserPromoUtilsTest {
         // Promo never shown. Chrome is not set to default.
         when(mCounter.getPromoCount()).thenReturn(0);
 
+        when(mProvider.getCurrentDefaultBrowserState()).thenReturn(DefaultBrowserState.NO_DEFAULT);
+
+        HistogramWatcher watcher =
+                HistogramWatcher.newBuilder()
+                        .expectIntRecord(
+                                "Android.DefaultBrowserPromo.EntryPoint.Settings",
+                                DefaultBrowserState.NO_DEFAULT)
+                        .build();
+
+        DefaultBrowserInfo.DefaultInfo info =
+                new DefaultBrowserInfo.DefaultInfo(
+                        /* defaultBrowserState= */ DefaultBrowserState.NO_DEFAULT,
+                        /* isChromeSystem= */ false,
+                        /* isDefaultSystem= */ false,
+                        /* browserCount= */ 1,
+                        /* systemCount= */ 0,
+                        /* isChromePreStableInstalled= */ false);
+
+        reCreateUtilsWithTestInfo(info);
+
         // Pass null for WindowAndroid. This would happen when the menu item in Settings (not App
         // Menu) is clicked.
-        mUtils.onMenuItemClick(mActivity, null);
+        mUtils.onMenuItemClick(
+                mActivity, null, DefaultBrowserPromoUtils.DefaultBrowserPromoEntryPoint.SETTINGS);
 
         // Should not increment counter since we are not showing the RoleManaerDialog.
         verify(mCounter, never()).onPromoShown();
@@ -417,6 +515,10 @@ public class DefaultBrowserPromoUtilsTest {
                 "Should launch System Settings",
                 Settings.ACTION_MANAGE_DEFAULT_APPS_SETTINGS,
                 intent.getAction());
+    }
+
+    private void reCreateUtilsWithTestInfo(DefaultBrowserInfo.DefaultInfo info) {
+        mUtils = new TestingDefaultBrowserPromoUtils(mCounter, mProvider, info);
     }
 
     private void setDepsMockWithDefaultValues() {
