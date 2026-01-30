30096e140c65915767c966be14f661539a7fcdee
Bruno Braga <brunobraga@google.com>
2026-01-30 05:36:43 -0800

    [Autofill AI - M4] Add Jni communication to handle permissions
    These methods will be used to:
    
    1. Know if the user is eligible.
    2. Opt them in and out of Enhanced Autofill
    
    NO_IFTTT=Change was done to generate java enum
    
    Bug: 467563819
    Change-Id: Ia9667228ef1a6252ee48ab23adc4c003ed6fd480
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7532096
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1577190}

--- chrome/browser/autofill/android/entity_data_manager_android.h
+++ chrome/browser/autofill/android/entity_data_manager_android.h
@@ -8,13 +8,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/android/jni_weak_ref.h"
 #include "base/android/scoped_java_ref.h"
+#include "base/memory/raw_ptr.h"
 #include "base/memory/raw_ref.h"
 #include "chrome/browser/autofill/android/entity_type_android.h"
 #include "components/autofill/core/browser/data_manager/autofill_ai/entity_data_manager.h"
 #include "components/autofill/core/browser/data_model/autofill_ai/entity_instance.h"
+#include "components/autofill/core/browser/permissions/autofill_ai/autofill_ai_permission_utils.h"
 #include "third_party/jni_zero/jni_zero.h"
 
+class GoogleGroupsManager;
+class PrefService;
+
+namespace signin {
+class IdentityManager;
+}
+
+namespace syncer {
+class SyncService;
+}
+
 namespace autofill {
+
+class AccountSettingService;
 
 // Android wrapper of the EntityDataManager which provides access from the
 // Java layer.
@@ -22,6 +37,12 @@ class EntityDataManagerAndroid {
  public:
   EntityDataManagerAndroid(JNIEnv* env,
                            const jni_zero::JavaRef<jobject>& obj,
+                           const GoogleGroupsManager* google_groups_manager,
+                           PrefService* prefs,
+                           const signin::IdentityManager* identity_manager,
+                           const syncer::SyncService* sync_service,
+                           const AccountSettingService* account_setting_service,
+                           bool is_off_the_record,
                            EntityDataManager* entity_data_manager);
 
   EntityDataManagerAndroid(const EntityDataManagerAndroid&) = delete;
@@ -29,6 +50,16 @@ class EntityDataManagerAndroid {
 
   // Trigger the destruction of the C++ object from Java.
   void Destroy(JNIEnv* env);
+
+  // Returns whether the user is eligible for Autofill AI.
+  bool IsEligibleToAutofillAi(JNIEnv* env);
+
+  // Returns the opt-in status for Autofill AI.
+  bool GetAutofillAiOptInStatus(JNIEnv* env);
+
+  // Sets the opt-in status for Autofill AI.
+  bool SetAutofillAiOptInStatus(JNIEnv* env,
+                                AutofillAiOptInStatus opt_in_status);
 
   // Removes the entity instance represented by `guid`.
   void RemoveEntityInstance(JNIEnv* env, const std::string& guid);
@@ -59,6 +90,13 @@ class EntityDataManagerAndroid {
 
   // Pointer to the java counterpart.
   JavaObjectWeakGlobalRef weak_java_obj_;
+
+  const raw_ptr<const GoogleGroupsManager> google_groups_manager_;
+  const raw_ptr<PrefService> prefs_;
+  const raw_ptr<const signin::IdentityManager> identity_manager_;
+  const raw_ptr<const syncer::SyncService> sync_service_;
+  const raw_ptr<const AccountSettingService> account_setting_service_;
+  const bool is_off_the_record_;
 
   // Pointer to the EntityDataManager.
   raw_ref<EntityDataManager> entity_data_manager_;
