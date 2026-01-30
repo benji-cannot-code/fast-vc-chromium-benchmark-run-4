069bec4450006ee3f0ca6a53770288e199de77d6
Liviu Rau <liviurau@google.com>
2026-01-30 09:21:46 -0800

    Allow extension loading to enable it in incognito mode
    As DevTools Frontend E2E tests gets migrated to non-hosted mode [1] we are looking into on demand extension installation for our extensions suite [2]. Since non-hosted mode is running the tests in incognito we need to instruct the browser to enable the extension as it gets loaded (loadUnpacked).
    
    [1] go/chrome-devtools:real2e-design
    [2] https://crrev.com/c/6375528
    
    Bug: 40789488
    Change-Id: I13f07ffe1253d820895d1b628edf6c6a89440a8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6425021
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Liviu Rau <liviurau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1577327}

--- chrome/browser/devtools/protocol/extensions_handler.cc
+++ chrome/browser/devtools/protocol/extensions_handler.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/devtools/protocol/extensions.h"
 #include "chrome/browser/devtools/protocol/protocol.h"
 #include "chrome/browser/extensions/browser_window_util.h"
+#include "chrome/browser/extensions/extension_util.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/extensions/extension_action_view_model.h"
 #include "chrome/browser/ui/extensions/extensions_container.h"
@@ -211,6 +212,7 @@ ExtensionsHandler::~ExtensionsHandler() = default;
 
 void ExtensionsHandler::LoadUnpacked(
     const protocol::String& path,
+    const std::optional<bool> enable_in_incognito,
     std::unique_ptr<ExtensionsHandler::LoadUnpackedCallback> callback) {
   if (!allow_loading_extensions_) {
     std::move(callback)->sendFailure(
@@ -223,6 +225,9 @@ void ExtensionsHandler::LoadUnpacked(
   scoped_refptr<extensions::UnpackedInstaller> installer(
       extensions::UnpackedInstaller::Create(context));
   installer->set_be_noisy_on_failure(false);
+  if (enable_in_incognito.has_value()) {
+    installer->set_allow_incognito_access(enable_in_incognito.value());
+  }
   installer->set_completion_callback(
       base::BindOnce(&ExtensionsHandler::OnLoaded, weak_factory_.GetWeakPtr(),
                      std::move(callback)));
