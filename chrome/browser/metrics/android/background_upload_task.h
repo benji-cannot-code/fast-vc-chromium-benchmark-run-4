5141b394b46b2cf4f6cf40b0ad5872d194e70579
Luc Nguyen <lucnguyen@google.com>
2026-01-30 15:13:13 -0800

    Add browsertest for metrics JobScheduler background upload task
    Bug: 445735421
    Change-Id: I823d769b7ca2cdf8f090a531242f56f0d0002ee4
    Validate-Test-Flakiness: skip
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7518604
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Luc Nguyen <lucnguyen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1577583}

--- chrome/browser/metrics/android/background_upload_task.h
+++ chrome/browser/metrics/android/background_upload_task.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/background_task_scheduler/background_task.h"
 #include "components/background_task_scheduler/task_ids.h"
+#include "components/metrics/metrics_log_uploader.h"
+#include "components/metrics/reporting_service.h"
 
 namespace metrics {
 
@@ -18,6 +20,17 @@ class BackgroundUploadTask : public background_task::BackgroundTask {
 
   BackgroundUploadTask(const BackgroundUploadTask& other) = delete;
   BackgroundUploadTask& operator=(const BackgroundUploadTask& other) = delete;
+
+  // Because this class is not instantiated directly, there is no good way to
+  // pass in which ReportingService instance scheduled this task. StartUpload()
+  // below will simply start the upload of whichever ReportingService is
+  // registered with the browser, but in tests, it may actually have been
+  // scheduled through a custom ReportingService that we manually instantiated.
+  // This helper allows overriding which ReportingService will have its upload
+  // started. If `service` is null, the override will be deleted instead.
+  static void SetReportingServiceForTesting(
+      MetricsLogUploader::MetricServiceType service_type,
+      ReportingService* service);
 
  private:
   // background_task::BackgroundTask:
--- components/metrics/test/test_metrics_service_client.h
+++ components/metrics/test/test_metrics_service_client.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 
 #include "base/memory/raw_ptr.h"
+#include "build/build_config.h"
 #include "components/metrics/metrics_log_store.h"
 #include "components/metrics/metrics_log_uploader.h"
 #include "components/metrics/metrics_service_client.h"
@@ -55,6 +56,9 @@ class TestMetricsServiceClient : public MetricsServiceClient {
       std::string_view mime_type,
       MetricsLogUploader::MetricServiceType service_type,
       const MetricsLogUploader::UploadCallback& on_upload_complete) override;
+#if BUILDFLAG(IS_ANDROID)
+  bool IsJobSchedulerSupported() const override;
+#endif  // BUILDFLAG(IS_ANDROID)
   base::TimeDelta GetStandardUploadInterval() override;
   bool IsReportingPolicyManaged() override;
   EnableMetricsDefault GetMetricsReportingDefaultState() override;
