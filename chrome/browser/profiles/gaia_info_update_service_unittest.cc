a44dd8392bff4d17a01e40c12cb2efe32fa30bac
Alison Gale <agale@chromium.org>
2026-01-30 00:14:08 -0800

    Remove comment referencing old flag
    Pulled out of https://chromium-review.googlesource.com/c/chromium/src/+/7522306
    
    Change-Id: I5649c5fafb0bc38eeb04bd1b1eb7c7bddd2c8b91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7533995
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Auto-Submit: Alison Gale <agale@chromium.org>
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1577085}

--- chrome/browser/profiles/gaia_info_update_service_unittest.cc
+++ chrome/browser/profiles/gaia_info_update_service_unittest.cc
@@ -529,7 +529,7 @@ class GAIAInfoUpdateServiceWithGlicEnablingTest
     : public GAIAInfoUpdateServiceTest {
  public:
   GAIAInfoUpdateServiceWithGlicEnablingTest() {
-    // Enable kGlic and kTabstripComboButton by default for testing.
+    // Enable kGlic by default for testing.
     scoped_feature_list_.InitWithFeatures(
         /*enabled_features=*/
         {features::kGlic, features::kGlicRollout},
