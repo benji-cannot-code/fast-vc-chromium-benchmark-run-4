ae92671ae017a7559c1852ce6fb22406e2bce54c
Cathy Li <lychee@meta.com>
2026-01-30 13:59:06 -0800

    Allow JSReplyProxy to also execute javascript.
    Design doc:
    https://docs.google.com/document/d/1WVLFAKlPIXGt-O8Xve1r7qLpeRY60xCJ7XIym8kEJwI/edit?tab=t.0#heading=h.7nki9mck5t64
    
    In this change: introduce a method in the JSReplyProxy which would
    execute JS in the same frame and world as the reply proxy.
    
    Bug: 460178095
    Change-Id: I73fc9e3aad0236262060966621a76082b48e9d72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7486559
    Commit-Queue: Cathy Li <lychee@meta.com>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1577532}

---
+++ android_webview/java/src/org/chromium/android_webview/JavaScriptExecutionCallback.java
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.android_webview;
+
+import org.chromium.android_webview.common.Lifetime;
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.js_injection.mojom.JavaScriptExecutionError;
+
+@Lifetime.Temporary
+@NullMarked
+public interface JavaScriptExecutionCallback {
+    default void onSuccess(String result) {}
+
+    default void onError(@JavaScriptExecutionError.EnumType int errorCode) {}
+}
--- components/js_injection/browser/navigation_web_message_sender.cc
+++ components/js_injection/browser/navigation_web_message_sender.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/js_injection/browser/web_message_host.h"
 #include "components/js_injection/browser/web_message_host_factory.h"
 #include "components/js_injection/browser/web_message_reply_proxy.h"
+#include "components/js_injection/common/interfaces.mojom.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/page.h"
 #include "content/public/browser/restore_type.h"
@@ -161,6 +162,14 @@ class EmptyReplyProxy : public WebMessageReplyProxy {
   void PostWebMessage(blink::WebMessagePayload message) override {
     // Do nothing as there is no connection to the renderer.
   }
+
+  void ExecuteJavaScript(const std::u16string& java_script,
+                         bool wants_result,
+                         ExecuteJavaScriptResultCallback callback) override {
+    std::move(callback).Run(
+        base::unexpected(mojom::JavaScriptExecutionError::kNotSupported));
+  }
+
   content::Page& GetPage() override { return *page_; }
 
  private:
