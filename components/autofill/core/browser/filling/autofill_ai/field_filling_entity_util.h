a3d26702b5adfdf0c399f02167dd720bcea07f8b
Jan Keitel <jkeitel@google.com>
2026-01-30 02:24:54 -0800

    Split checking for sensitive fields and reading client pref for reauth
    This is another preparatory CL that contains no (intentional)
    functionality changes. It only splits checking whether a fill would
    involve sensitive fields and whether the user has biometric reauth
    turned on into two separate calls, since both pieces of information
    will be needed independently of each other.
    
    Bug: 477845712
    Change-Id: I4d06bb3d28fe8f415713aaf8c9852923ce9527cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7527329
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1577131}

--- components/autofill/core/browser/filling/autofill_ai/field_filling_entity_util.h
+++ components/autofill/core/browser/filling/autofill_ai/field_filling_entity_util.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/common/mojom/autofill_types.mojom-forward.h"
 #include "components/autofill/core/common/unique_ids.h"
 
-class PrefService;
 namespace autofill {
 
 class AddressNormalizer;
@@ -26,6 +25,7 @@ class AutofillField;
 struct AutofillFieldWithAttributeType;
 class EntityInstance;
 class FormStructure;
+class Section;
 
 // Returns the entities from EntityDataManager::GetEntityInstances() for which
 // filling is enabled.
@@ -48,13 +48,13 @@ std::u16string GetFillValueForEntity(
     const std::string& app_locale,
     AddressNormalizer* address_normalizer);
 
-// Returns whether the user should re-auth before filling a form with Autofill
-// AI data.
-bool ShouldReauthBeforeFilling(
-    const EntityInstance& entity,
-    base::span<const AutofillFieldWithAttributeType> fields,
-    std::string_view app_locale,
-    const PrefService& prefs);
+// Returns whether filling `form`'s `section` with `entity` would fill sensitive
+// attributes.
+bool WillFillSensitiveAttributes(const EntityInstance& entity,
+                                 const FormStructure& form,
+                                 const Section& section,
+                                 std::string_view app_locale);
+
 }  // namespace autofill
 
 #endif  // COMPONENTS_AUTOFILL_CORE_BROWSER_FILLING_AUTOFILL_AI_FIELD_FILLING_ENTITY_UTIL_H_
