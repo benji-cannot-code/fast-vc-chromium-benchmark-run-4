37bed1b364b47b03a4caec8632b790b7edc895e0
Jan Keitel <jkeitel@google.com>
2026-01-30 06:23:31 -0800

    Show throbber on accepting asynchronous Autofill AI save
    Screencast:
    https://screencast.googleplex.com/cast/NTg5NzM0NzE0MDY4MTcyOHxiYjIzZTJmNC0xMQ
    
    Bug: 477845712
    Change-Id: I63331160a3ba65621d888962921b50a6f0866084
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7534338
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1577205}

--- chrome/browser/ui/views/autofill/autofill_ai/autofill_ai_import_data_bubble_view_unittest.cc
+++ chrome/browser/ui/views/autofill/autofill_ai/autofill_ai_import_data_bubble_view_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/ui/autofill/autofill_ai/autofill_ai_import_data_controller.h"
 #include "chrome/browser/ui/autofill/autofill_ai/mock_autofill_ai_import_data_controller.h"
+#include "chrome/browser/ui/views/autofill/payments/dialog_view_ids.h"
 #include "chrome/test/base/testing_profile.h"
 #include "chrome/test/views/chrome_views_test_base.h"
 #include "components/autofill/core/browser/test_utils/autofill_test_utils.h"
@@ -154,6 +155,22 @@ TEST_F(AutofillAiImportDataBubbleViewTest, AcceptDoesNotCloseTheBubble) {
   // Clear expectations explicitly since the widget is destroyed during tear
   // down.
   Mock::VerifyAndClearExpectations(&mock_controller());
+}
+
+TEST_F(AutofillAiImportDataBubbleViewTest, AcceptShowsThrobber) {
+  CreateViewAndShow();
+  EXPECT_CALL(mock_controller(), CloseOnAccept()).WillOnce(Return(false));
+  EXPECT_CALL(mock_controller(), OnSaveButtonClicked);
+
+  views::View* throbber = view()->GetViewByID(DialogViewId::LOADING_THROBBER);
+  ASSERT_TRUE(throbber);
+  EXPECT_FALSE(throbber->parent()->GetVisible());
+
+  view()->AcceptDialog();
+
+  EXPECT_TRUE(throbber->parent()->GetVisible());
+  EXPECT_EQ(view()->buttons(),
+            static_cast<int>(ui::mojom::DialogButton::kNone));
 }
 
 TEST_F(AutofillAiImportDataBubbleViewTest, CancelInvokesTheController) {
