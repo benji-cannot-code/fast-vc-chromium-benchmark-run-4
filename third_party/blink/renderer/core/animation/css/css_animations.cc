565314160cc2e19e40df51ee076fbd547dc83d9d
David Awogbemila <awogbemila@chromium.org>
2026-01-30 06:49:15 -0800

    Cancel WAAPI-controlled CSS animation upon removal
    ignore_css_play_state_ should control whether or not we play or pause
    the animation in response to changes to animation-play-state, not
    whether or not the animation should persist after the style declaration
    is gone.
    
    Bug: 406497224
    Change-Id: Icef7a685484b88fff3658c33c299d3877b230cea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7529493
    Reviewed-by: David Awogbemila <awogbemila@chromium.org>
    Commit-Queue: David Awogbemila <awogbemila@chromium.org>
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1577219}

--- third_party/blink/renderer/core/animation/css/css_animations.cc
+++ third_party/blink/renderer/core/animation/css/css_animations.cc
@@ -2340,8 +2340,7 @@ void CSSAnimations::MaybeApplyPendingUpdate(Element* element) {
     Animation& animation =
         *running_animations_[cancelled_indices[i]]->animation;
     animation.ClearOwningElement();
-    if (animation.IsCSSAnimation() &&
-        !DynamicTo<CSSAnimation>(animation)->GetIgnoreCSSPlayState()) {
+    if (animation.IsCSSAnimation()) {
       animation.cancel();
     }
     animation.Update(kTimingUpdateOnDemand);
