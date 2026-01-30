0dda8c5454c3f5f7fc0af0a63f5134c106fbb249
Will Harris <wfh@chromium.org>
2026-01-30 09:24:19 -0800

    Add GetIsolationAttributeName API in installer utils
    This API needs to be called from chrome/app very early in process
    lifetime, as well as being called from chrome/elevation_service and from
    the installer.
    
    This CL also adds a test.
    
    BUG=433545123
    
    Change-Id: Ifd15dd5aabe06ba1b315b25525a047aa3882fd8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7524652
    Commit-Queue: Will Harris <wfh@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1577330}

---
+++ chrome/installer/util/isolation_support.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_INSTALLER_UTIL_ISOLATION_SUPPORT_H_
+#define CHROME_INSTALLER_UTIL_ISOLATION_SUPPORT_H_
+
+#include <string>
+
+namespace installer {
+
+// Returns the isolation security attribute name. E.g.
+// "GOOGLECHROME://ISOLATION".
+std::wstring GetIsolationAttributeName();
+
+}  // namespace installer
+
+#endif  // CHROME_INSTALLER_UTIL_ISOLATION_SUPPORT_H_
