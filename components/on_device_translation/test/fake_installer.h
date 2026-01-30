9ca6bae3ef556b0ad24453b271b3ec935bf858a4
Danilo Tedeschi <danft@google.com>
2026-01-30 13:44:05 -0800

    Add path methods to OnDeviceTranslationInstaller
    These methods will allow us to remove all the dependencies in
    chrome/browser from component_manager
    (chrome/browser/on_device_translation/component_manager.cc) which will
    allow us to move the rest of the files to //components.
    
    Change-Id: Ifdd7bc91ee423bfe3551d1ab88ca63da3bf09749
    Bug: 468311708
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7528635
    Reviewed-by: Nathan Memmott <memmott@chromium.org>
    Commit-Queue: Danilo Françoso Tedeschi <danft@google.com>
    Cr-Commit-Position: refs/heads/main@{#1577523}

--- components/on_device_translation/test/fake_installer.h
+++ components/on_device_translation/test/fake_installer.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_ON_DEVICE_TRANSLATION_TEST_FAKE_INSTALLER_H_
 #define COMPONENTS_ON_DEVICE_TRANSLATION_TEST_FAKE_INSTALLER_H_
 
+#include "base/files/file_path.h"
 #include "base/functional/callback_forward.h"
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
@@ -18,11 +19,15 @@ namespace on_device_translation {
 class FakeOnDeviceTranslationInstaller : public OnDeviceTranslationInstaller {
  public:
   ~FakeOnDeviceTranslationInstaller() override;
-  FakeOnDeviceTranslationInstaller();
+  explicit FakeOnDeviceTranslationInstaller(base::FilePath fake_install_dir);
 
   bool IsInit() const override;
   std::set<LanguagePackKey> RegisteredLanguagePacks() const override;
   std::set<LanguagePackKey> InstalledLanguagePacks() const override;
+  base::FilePath GetLibraryPath() const override;
+  base::FilePath GetLanguagePackPath(
+      LanguagePackKey language_pack) const override;
+
   void Init(base::RepeatingClosure on_ready_callback) override;
   // Forces initialization to happen right away.
   void InitNow(base::RepeatingClosure on_ready_callback);
@@ -33,6 +38,7 @@ class FakeOnDeviceTranslationInstaller : public OnDeviceTranslationInstaller {
   void AddOserver(Observer* observer) override;
 
  private:
+  base::FilePath fake_install_dir_;
   bool is_init_ = false;
   std::set<LanguagePackKey> installed_lang_packs_;
   std::set<LanguagePackKey> registered_lang_packs_;
