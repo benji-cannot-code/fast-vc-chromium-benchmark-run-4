4c9156bf2c976b08c85a625231d0b2c00eee0967
Jun Ishiguro <junis@google.com>
2026-01-29 22:07:49 -0800

    crostini: Pass ComponentManagerAsh to CrostiniManager
    Bug: b:404131219
    Change-Id: I2a15a3c175bbdbac9215ff3ee89b7020f5d17d33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7518791
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1577037}

--- chrome/browser/ash/crostini/crostini_manager.h
+++ chrome/browser/ash/crostini/crostini_manager.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_map.h"
 #include "base/files/file_path.h"
 #include "base/memory/raw_ptr.h"
+#include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
 #include "base/scoped_observation.h"
@@ -55,6 +56,7 @@ class SchedulerConfigurationManager;
 }  // namespace ash
 
 namespace component_updater {
+class ComponentManagerAsh;
 class ComponentUpdateService;
 }  // namespace component_updater
 
@@ -195,8 +197,11 @@ class CrostiniManager : public KeyedService,
 
   // `component_update_service` and `scheduler_configuration_manager` must
   // outlive `this`, but both of them may be null in unit tests.
+  // `component_manager_ash` may be null in unit tests.
   explicit CrostiniManager(
       const component_updater::ComponentUpdateService* component_update_service,
+      scoped_refptr<component_updater::ComponentManagerAsh>
+          component_manager_ash,
       ash::SchedulerConfigurationManager* scheduler_configuration_manager,
       Profile* profile);
 
@@ -778,6 +783,8 @@ class CrostiniManager : public KeyedService,
 
   const raw_ptr<const component_updater::ComponentUpdateService>
       component_update_service_;
+  const scoped_refptr<component_updater::ComponentManagerAsh>
+      component_manager_ash_;
   const raw_ptr<ash::SchedulerConfigurationManager>
       scheduler_configuration_manager_;
 
