930bae9d7f79fa9c16021ad255f0fef81798dfa6
Samuel Huang <huangs@chromium.org>
2026-01-29 18:29:48 -0800

    [Reader Mode] Log JavaScript debug output from DistilledPageJsTest.
    To debug DOM Distiller JavaScript tests, instead of printing messages
    using `console.log()`, we should use `domAutomationController.send()`.
    However, it turns out that more work is needed to output these messages
    to logs. This CL fills the gap by outputting messages to LOG(INFO).
    
    Bug: 479886455
    Change-Id: I3d363247b8451f90c9469512b80b4751acfd1c62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7533658
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1576989}

--- components/dom_distiller/content/browser/test/distilled_page_js_browsertest.cc
+++ components/dom_distiller/content/browser/test/distilled_page_js_browsertest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#include "base/logging.h"
 #include "base/strings/strcat.h"
 #include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
@@ -44,11 +45,18 @@ class DistilledPageJsTest : public content::ContentBrowserTest {
   void LoadAndExecuteTestScript(const std::string& file) {
     distilled_page_->AppendScriptFile(file);
     distilled_page_->Load(embedded_test_server(), shell()->web_contents());
+    // Listen for messages from domAutomationController.send() for debugging.
+    content::DOMMessageQueue message_queue(shell()->web_contents());
     // First, run the test.
     EXPECT_TRUE(content::ExecJs(shell()->web_contents(), "mocha.run()"));
     // Then, wait for the test to complete.
     EXPECT_TRUE(
         content::ExecJs(shell()->web_contents(), "window.completePromise"));
+    std::string message;
+    // Log any messages that were sent from the JavaScript.
+    while (message_queue.PopMessage(&message)) {
+      LOG(INFO) << "JS_DEBUG[" << file << "]: " << message;
+    }
   }
 
   std::unique_ptr<FakeDistilledPage> distilled_page_;
