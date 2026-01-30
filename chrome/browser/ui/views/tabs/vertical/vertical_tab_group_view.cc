58e01a5103a19101be649c5ef16ccaf59dc9e63a
Kaan Alsan <alsan@chromium.org>
2026-01-30 10:29:32 -0800

    Implement laying out of multi-selected vertical tabs during drag
    Adds support for laying out dragged tabs, either stacked vertically or
    squashed over each other.
    
    This is achieved by having VerticalDraggedTabsContainer calculate the
    bounding box of all dragged views, and tracking each dragged view's
    offset within it. Position is determined by calculating the bounding
    box's position relative to the drag point, then applying the offset of
    each dragged view.
    
    Bug: 476084253
    Change-Id: I8a77f999e7690d85b31a5d22377bf729cf7b84d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7533636
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1577379}

--- chrome/browser/ui/views/tabs/vertical/vertical_tab_group_view.cc
+++ chrome/browser/ui/views/tabs/vertical/vertical_tab_group_view.cc
@@ -67,7 +67,8 @@ bool SupportsDataSharing() {
 
 VerticalTabGroupView::VerticalTabGroupView(TabCollectionNode* collection_node)
     : VerticalDraggedTabsContainer(static_cast<views::View&>(*this),
-                                   DragAxes::kVerticalOnly),
+                                   DragAxes::kVerticalOnly,
+                                   DragLayout::kVertical),
       collection_node_(collection_node),
       tab_group_visual_data_(
           *GetTabGroupFromNode(collection_node_)->visual_data()),
@@ -155,7 +156,8 @@ views::ProposedLayout VerticalTabGroupView::CalculateProposedLayout(
   // fill available width.
   for (auto* child : children) {
     gfx::Rect bounds = gfx::Rect(child->GetPreferredSize());
-    bounds.set_y(GetYForDraggedTabBounds(*child).value_or(height));
+    bounds.set_y(
+        GetOriginForDraggedTabBounds(*child).value_or({0, height}).y());
     // If the tab strip is not collapsed then the groups tabs should be inset.
     bounds.set_x(is_tab_strip_collapsed
                      ? GetLayoutConstant(
