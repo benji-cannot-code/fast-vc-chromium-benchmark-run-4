f5cf1cc747cb30c50b29a5e94635f263298296c1
Joshua Hood <jdh@chromium.org>
2026-01-30 13:57:56 -0800

    [Bundled Settings] Fix secure DNS V2 appearance in managed environments
    This CL stops the warning indicator from being shown in the secure DNS
    V2 row when secure DNS is disabled because the browser is managed or
    OS-level parental controls are detected.
    
    Bug: b:479491202
    Change-Id: Icac8bc1488920547dcbf246c3e78079b1091d418
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7532307
    Commit-Queue: Joshua Hood <jdh@chromium.org>
    Reviewed-by: Nicola Tommasi <tommasin@chromium.org>
    Reviewed-by: Awad Osman <awado@google.com>
    Cr-Commit-Position: refs/heads/main@{#1577530}

--- chrome/browser/resources/settings/privacy_page/security/secure_dns_v2.ts
+++ chrome/browser/resources/settings/privacy_page/security/secure_dns_v2.ts
@@ -492,7 +492,7 @@ export class SettingsSecureDnsV2Element extends SettingsSecureDnsV2ElementBase {
       return;
     }
 
-    const enforced = this.getPref('dns_over_https.mode').enforcement ===
+    const enforced = this.secureDnsTogglePref_.enforcement ===
         chrome.settingsPrivate.Enforcement.ENFORCED;
 
     this.$.automaticRadioButton.disabled = enforced;
@@ -502,7 +502,6 @@ export class SettingsSecureDnsV2Element extends SettingsSecureDnsV2ElementBase {
     this.isSecureDnsWarningIconVisible_ =
         !this.secureDnsTogglePref_.value && !enforced;
   }
-
 
   /**
    * Updates the UI to match the provided configuration parameters.
