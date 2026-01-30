abba6561e20e507e9a399e9d18cfcdf3430a4479
Sinan Sahin <sinansahin@google.com>
2026-01-30 13:51:36 -0800

    Make Delete browsing data link color dynamic on All sites Settings page
    Before: https://screenshot.googleplex.com/7WPAtKRELhmQQNb
    After: https://screenshot.googleplex.com/4nTRadmcb3xBk42
    
    Change-Id: I395d10a54922e45a0979be3d21a0e299645ff11d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7534160
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1577527}

--- components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/AllSiteSettings.java
+++ components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/AllSiteSettings.java
@@ -8,6 +8,7 @@ package org.chromium.components.browser_ui.site_settings;
 import static org.chromium.build.NullUtil.assertNonNull;
 import static org.chromium.build.NullUtil.assumeNonNull;
 import static org.chromium.components.browser_ui.settings.SearchUtils.handleSearchNavigation;
+import static org.chromium.components.browser_ui.styles.SemanticColorUtils.getDefaultTextColorLink;
 
 import android.content.Context;
 import android.content.DialogInterface;
@@ -490,8 +491,7 @@ public class AllSiteSettings extends BaseSiteSettingsFragment
                     new SpannableString(
                             getResources().getString(R.string.clear_browsing_data_link));
             spannableString.setSpan(
-                    new ForegroundColorSpan(
-                            getContext().getColor(R.color.default_text_color_link_baseline)),
+                    new ForegroundColorSpan(getDefaultTextColorLink(getContext())),
                     0,
                     spannableString.length(),
                     Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
