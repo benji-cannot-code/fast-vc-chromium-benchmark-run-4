ab7348c6db81c92ae5aefb2e5bdf67fd3b5a8c95
Jun Ishiguro <junis@google.com>
2026-01-29 23:16:07 -0800

    Clean up BUILD.gn and DEPS files under chrome/browser/ash/dbus
    Fixed: b:404131870
    Change-Id: I296bdc1920fd1769bc9ed74a3df531dca8e7a41a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7534362
    Reviewed-by: Georg Neis <neis@chromium.org>
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1577061}

--- chrome/browser/ash/dbus/proxy_resolution_service_provider_browsertest.cc
+++ chrome/browser/ash/dbus/proxy_resolution_service_provider_browsertest.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/mixin_based_in_process_browser_test.h"
-#include "chrome/test/base/testing_browser_process.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/test/browser_test.h"
