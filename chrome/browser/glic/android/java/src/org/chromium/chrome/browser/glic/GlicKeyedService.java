389ada6da342dbebc1f791d4fe073f68a49c1699
Bhuvana Betini <bbetini@google.com>
2026-01-30 13:05:33 -0800

    [Glic] Add JNI Bridge for GlicKeyedService
    Adds a JNI bridge to GlicKeyedService::ToggleUI that is called in
    RootUiCoordinator, the beginning of the flow. When the entry-point in
    the 3-dot menu is clicked, it will trigger this flow.
    
    Change-Id: Ia36d0eb5f21fadc9b93e76a12c36c9a6803c1eec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7517732
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Bhuvana Betini <bbetini@google.com>
    Cr-Commit-Position: refs/heads/main@{#1577497}

---
+++ chrome/browser/glic/android/java/src/org/chromium/chrome/browser/glic/GlicKeyedService.java
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.glic;
+
+import org.jni_zero.JNINamespace;
+import org.jni_zero.JniType;
+import org.jni_zero.NativeMethods;
+
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.chrome.browser.profiles.Profile;
+
+@JNINamespace("glic")
+@NullMarked
+public class GlicKeyedService {
+
+    public GlicKeyedService() {}
+
+    public void toggleUI(long browserWindowPtr, Profile profile, int invocationSource) {
+        GlicKeyedServiceJni.get().toggleUI(browserWindowPtr, profile, invocationSource);
+    }
+
+    @NativeMethods
+    public interface Natives {
+        void toggleUI(long browserWindowPtr, @JniType("Profile*") Profile profile, int source);
+    }
+}
