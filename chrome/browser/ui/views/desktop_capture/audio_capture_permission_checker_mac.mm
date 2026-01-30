764493973d53a2d271034dc4a4f1bd7bc796ac5f
Johannes Kron <kron@chromium.org>
2026-01-30 03:41:14 -0800

    Enable kDesktopMediaPickerCheckAudioPermissions by default
    Launch approval in launch/4426742
    
    Change-Id: Icbf5ff119d6bb362c0ff0ea770db5f9dac39b789
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7423374
    Reviewed-by: Tove Petersson <tovep@chromium.org>
    Commit-Queue: Johannes Kron <kron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1577148}

--- chrome/browser/ui/views/desktop_capture/audio_capture_permission_checker_mac.mm
+++ chrome/browser/ui/views/desktop_capture/audio_capture_permission_checker_mac.mm
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 BASE_FEATURE(kDesktopMediaPickerCheckAudioPermissions,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 }  // namespace
 
 std::unique_ptr<AudioCapturePermissionCheckerMac>
