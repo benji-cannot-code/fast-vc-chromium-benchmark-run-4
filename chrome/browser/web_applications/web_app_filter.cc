60d17aa5729f78a343d112caf0e45d7c1dbc1c96
Marijn Kruisselbrink <mek@chromium.org>
2026-01-30 15:54:20 -0800

    Implement WebAppInstallFromMigrateFromFieldCommand
    This command is triggered very similarly to the existing
    ManifestSilentUpdateCommand, and checks if the manifest found is
    for a (not yet installed) migration target, causing it to be
    installed if so.
    
    This also removes the update logic from MigrationTargetInstallJob,
    to avoid any possibly issues caused by both the new command and the
    existing ManifestSilentUpdateCommand trying to apply the same updates.
    Instead this CL adds test that (at least for the
    InstallFromMigrateFromField case) ManifestSilentUpdateCommand takes
    care of (silently) updating the migration target app, even if it is
    only a suggested from migration app. The logic to possibly trigger
    a silent update job will still be needed for InstallFromMigrateTo,
    but that command can have the logic to decide which job to invoke.
    
    Bug: 468007713
    Change-Id: I3123e3646891027a0ed798895af7db865ce16a10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7529317
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1577609}

--- chrome/browser/web_applications/web_app_filter.cc
+++ chrome/browser/web_applications/web_app_filter.cc
@@ -176,4 +176,11 @@ WebAppFilter WebAppFilter::IsAppValidMigrationSource() {
   return filter;
 }
 
+// static
+WebAppFilter WebAppFilter::IsAppEligibleForManifestUpdate() {
+  WebAppFilter filter;
+  filter.is_app_eligible_for_manifest_update_ = true;
+  return filter;
+}
+
 }  // namespace web_app
