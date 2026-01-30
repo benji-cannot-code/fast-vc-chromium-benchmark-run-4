1796d6260b6caa767e32cdfd66fae33f9201c10d
Paulina Gacek <paulinagacek@google.com>
2026-01-30 11:11:13 -0800

    SmartCardEmulation: Implement request forwarding.
    This CL adds the internal plumbing for request forwarding: Receive Mojo
    Call -> Store Callback -> Notify DevTools in
    `SmartCardEmulationHandler`. The end-to-end verification is implemented
    in subsequent CLs in this stack (from crrev.com/c/7361937 to
    crrev.com/c/7520404).
    
    Design doc: go/web-smart-card-api-cdp.
    
    BYPASS_LARGE_CHANGE_WARNING=All added functionalities need to be added
    simultaneously.
    
    Bug: 474033866
    Change-Id: I952f4c6be4e5d5e5a3c50c0bda5c96f882e4d548
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7502997
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Patryk Chodur <pchodur@google.com>
    Reviewed-by: Zgroza (Luke) Klimek <zgroza@chromium.org>
    Commit-Queue: Paulina Gacek <paulinagacek@google.com>
    Cr-Commit-Position: refs/heads/main@{#1577409}

--- content/browser/devtools/protocol/smart_card_emulation_handler.h
+++ content/browser/devtools/protocol/smart_card_emulation_handler.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_BROWSER_DEVTOOLS_PROTOCOL_SMART_CARD_EMULATION_HANDLER_H_
 #define CONTENT_BROWSER_DEVTOOLS_PROTOCOL_SMART_CARD_EMULATION_HANDLER_H_
 
+#include "base/types/expected.h"
 #include "content/browser/devtools/devtools_agent_host_impl.h"
 #include "content/browser/devtools/protocol/devtools_domain_handler.h"
 #include "content/browser/devtools/protocol/smart_card_emulation.h"
@@ -37,6 +38,64 @@ class CONTENT_EXPORT SmartCardEmulationHandler
   // SmartCardEmulation::Backend implementation.
   DispatchResponse Enable() override;
   DispatchResponse Disable() override;
+
+  template <typename CallbackType, typename ResultType>
+  class PendingRequestImpl {
+   public:
+    explicit PendingRequestImpl(CallbackType cb) : callback_(std::move(cb)) {}
+
+    void ReportError(device::mojom::SmartCardError error) {
+      std::move(callback_).Run(ResultType::NewError(error));
+    }
+
+    CallbackType& callback() { return callback_; }
+
+   private:
+    CallbackType callback_;
+  };
+
+  using PendingCreateContext = PendingRequestImpl<
+      device::mojom::SmartCardContextFactory::CreateContextCallback,
+      device::mojom::SmartCardCreateContextResult>;
+
+  using PendingListReaders =
+      PendingRequestImpl<device::mojom::SmartCardContext::ListReadersCallback,
+                         device::mojom::SmartCardListReadersResult>;
+
+  using PendingGetStatusChange = PendingRequestImpl<
+      device::mojom::SmartCardContext::GetStatusChangeCallback,
+      device::mojom::SmartCardStatusChangeResult>;
+
+  using PendingConnect =
+      PendingRequestImpl<device::mojom::SmartCardContext::ConnectCallback,
+                         device::mojom::SmartCardConnectResult>;
+
+  // Handles Cancel, Disconnect, SetAttrib, EndTransaction.
+  using PendingPlainResult = PendingRequestImpl<
+      base::OnceCallback<void(device::mojom::SmartCardResultPtr)>,
+      device::mojom::SmartCardResult>;
+
+  // Handles Transmit, Control, GetAttrib.
+  using PendingDataResult = PendingRequestImpl<
+      base::OnceCallback<void(device::mojom::SmartCardDataResultPtr)>,
+      device::mojom::SmartCardDataResult>;
+
+  using PendingStatus =
+      PendingRequestImpl<device::mojom::SmartCardConnection::StatusCallback,
+                         device::mojom::SmartCardStatusResult>;
+
+  using PendingBeginTransaction = PendingRequestImpl<
+      device::mojom::SmartCardConnection::BeginTransactionCallback,
+      device::mojom::SmartCardTransactionResult>;
+
+  using PendingRequest = std::variant<PendingCreateContext,
+                                      PendingListReaders,
+                                      PendingGetStatusChange,
+                                      PendingConnect,
+                                      PendingPlainResult,
+                                      PendingDataResult,
+                                      PendingStatus,
+                                      PendingBeginTransaction>;
 
  private:
   static std::vector<SmartCardEmulationHandler*> ForAgentHost(
@@ -123,6 +182,16 @@ class CONTENT_EXPORT SmartCardEmulationHandler
       device::mojom::SmartCardDisposition disposition,
       device::mojom::SmartCardTransaction::EndTransactionCallback callback)
       override;
+
+  void AddPendingRequest(const std::string& request_id, PendingRequest request);
+
+  template <typename T>
+  base::expected<T, std::string> TakePendingRequest(
+      const std::string& request_id);
+
+  // The Handler needs to store Mojo callbacks for "paused" requests so they can
+  // be resumed later when the DevTools frontend replies.
+  std::unordered_map<std::string, PendingRequest> pending_requests_;
 
   bool IsEnabled() const { return !!factory_; }
 
