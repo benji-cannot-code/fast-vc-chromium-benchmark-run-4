bb6a31cd6b1545afd216de11ea9e4b8da4afb232
Andres Ricardo Perez <andresrperez@chromium.org>
2026-01-29 16:51:32 -0800

    Control Foreground MAY_BLOCK timeouts with feature params
    The ThreadPool uses two TimeDelta parameters,
    kForegroundMayBlockThreshold and kForegroundBlockedWorkersPoll, to
    determine when to add an extra thread if there's is a task running a
    ScopedBlockingCall.
    
    This CL adds a feature and params to control these values. Due to the
    lock that is taken on ThreadGroupImpl::Start(), it's necessary to read
    the feature params earlier. Therefore, the arguments were re-organized
    to pass definitive values to ThreadGroup::StartImplLockRequired().
    
    The argument passed to ThreadGroupImpl::Start() controlling
    may_block_threshold was renamed may_block_threshold_for_testing, as that
    was its only use and setting it to a value would override the value set
    via the feature param.
    
    Bug: 477987130
    Change-Id: I256b15a4402b852f42ea5db67e54122634b8d9c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7511833
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Andres Ricardo Perez <andresrperez@chromium.org>
    Commit-Queue: Andres Ricardo Perez <andresrperez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1576942}

--- base/task/thread_pool/thread_pool_impl.cc
+++ base/task/thread_pool/thread_pool_impl.cc
@@ -201,7 +201,7 @@ void ThreadPoolImpl::Start(const ThreadPoolInstance::InitParams& init_params,
       init_params.suggested_reclaim_time, service_thread_task_runner,
       worker_thread_observer, worker_environment,
       g_synchronous_thread_start_for_testing,
-      /*may_block_threshold=*/{});
+      /*may_block_threshold_for_testing=*/{});
 
   if (utility_thread_group_) {
     utility_thread_group_.get()->Start(
@@ -209,7 +209,7 @@ void ThreadPoolImpl::Start(const ThreadPoolInstance::InitParams& init_params,
         init_params.suggested_reclaim_time, service_thread_task_runner,
         worker_thread_observer, worker_environment,
         g_synchronous_thread_start_for_testing,
-        /*may_block_threshold=*/{});
+        /*may_block_threshold_for_testing=*/{});
   }
 
   if (background_thread_group_) {
@@ -218,7 +218,7 @@ void ThreadPoolImpl::Start(const ThreadPoolInstance::InitParams& init_params,
         init_params.suggested_reclaim_time, service_thread_task_runner,
         worker_thread_observer, worker_environment,
         g_synchronous_thread_start_for_testing,
-        /*may_block_threshold=*/{});
+        /*may_block_threshold_for_testing=*/{});
   }
 
   started_ = true;
