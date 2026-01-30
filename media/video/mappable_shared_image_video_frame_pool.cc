96901cff4cc41f36be98d1a35b55ceb6b053f634
Colin Blundell <blundell@chromium.org>
2026-01-29 23:30:13 -0800

    [//media] Rename method for clarity
    This method does *not* delete the FrameResource.
    
    Bug: 366375486
    Change-Id: If794f6aef6ad68396add01bd63d8fe53c545700f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7532436
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1577071}

--- media/video/mappable_shared_image_video_frame_pool.cc
+++ media/video/mappable_shared_image_video_frame_pool.cc
@@ -265,9 +265,10 @@ class MappableSharedImageVideoFramePool::PoolImpl
   void SharedImageReleased(FrameResource* frame_resource,
                            const gpu::SyncToken& sync_token);
 
-  // Delete resource. This has to be called on the thread where |task_runner|
-  // is current.
-  static void DeleteFrameResource(
+  // Sets the destruction token of `frame_resource`'s ClientSharedImage to be
+  // the resource's sync token. This has to be called on the thread where
+  // |task_runner| is current.
+  static void UpdateSharedImageDestructionTokenForResource(
       GpuVideoAcceleratorFactories* const gpu_factories,
       FrameResource* frame_resource);
 
@@ -1027,7 +1028,8 @@ void MappableSharedImageVideoFramePool::PoolImpl::OnCopiesDoneOnMediaThread(
       resources_pool_.erase(it);
     }
 
-    DeleteFrameResource(gpu_factories_, frame_resource);
+    UpdateSharedImageDestructionTokenForResource(gpu_factories_,
+                                                 frame_resource);
     delete frame_resource;
 
     CompleteCopyRequestAndMaybeStartNextCopy(std::move(video_frame));
@@ -1186,8 +1188,9 @@ void MappableSharedImageVideoFramePool::PoolImpl::Shutdown() {
     }
 
     media_task_runner_->PostTask(
-        FROM_HERE, base::BindOnce(&PoolImpl::DeleteFrameResource,
-                                  gpu_factories_, base::Owned(frame_resource)));
+        FROM_HERE,
+        base::BindOnce(&PoolImpl::UpdateSharedImageDestructionTokenForResource,
+                       gpu_factories_, base::Owned(frame_resource)));
   }
   resources_pool_.clear();
 }
@@ -1215,7 +1218,8 @@ MappableSharedImageVideoFramePool::PoolImpl::GetOrCreateFrameResource(
         return frame_resource;
       } else {
         resources_pool_.erase(it++);
-        DeleteFrameResource(gpu_factories_, frame_resource);
+        UpdateSharedImageDestructionTokenForResource(gpu_factories_,
+                                                     frame_resource);
         delete frame_resource;
       }
     } else {
@@ -1298,9 +1302,10 @@ void MappableSharedImageVideoFramePool::PoolImpl::
 }
 
 // static
-void MappableSharedImageVideoFramePool::PoolImpl::DeleteFrameResource(
-    GpuVideoAcceleratorFactories* const gpu_factories,
-    FrameResource* frame_resource) {
+void MappableSharedImageVideoFramePool::PoolImpl::
+    UpdateSharedImageDestructionTokenForResource(
+        GpuVideoAcceleratorFactories* const gpu_factories,
+        FrameResource* frame_resource) {
   // TODO(dcastagna): As soon as the context lost is dealt with in media,
   // make sure that we won't execute this callback (use a weak pointer to
   // the old context).
@@ -1337,7 +1342,8 @@ void MappableSharedImageVideoFramePool::PoolImpl::SharedImageReleased(
   frame_resource->sync_token = release_sync_token;
 
   if (in_shutdown_) {
-    DeleteFrameResource(gpu_factories_, frame_resource);
+    UpdateSharedImageDestructionTokenForResource(gpu_factories_,
+                                                 frame_resource);
     delete frame_resource;
     return;
   }
@@ -1352,7 +1358,7 @@ void MappableSharedImageVideoFramePool::PoolImpl::SharedImageReleased(
     if (!resource->is_used() &&
         now - resource->last_use_time() > kStaleFrameLimit) {
       resources_pool_.erase(it++);
-      DeleteFrameResource(gpu_factories_, resource);
+      UpdateSharedImageDestructionTokenForResource(gpu_factories_, resource);
       delete resource;
     } else {
       it++;
