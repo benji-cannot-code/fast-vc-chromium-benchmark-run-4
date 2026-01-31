f62ac121db2b69113724eaa1a16875a29393a861
Lukasz Anforowicz <lukasza@chromium.org>
2026-01-30 20:59:50 -0800

    Roll clang+rust llvmorg-23-init-1879-g3ddddf7a-1 : llvmorg-23-init-2224-g5bd8dadb-1 / e96bb7e44fbcc23c1e6009e8d0ee8ab208668fb4-1 : 7d8ebe3128fc87f3da1ad64240e63ccf07b8f0bd-1
    https://chromium.googlesource.com/external/github.com/llvm/llvm-project/+log/3ddddf7a..5bd8dadb
    
    https://chromium.googlesource.com/external/github.com/rust-lang/rust/+log/e96bb7e44fbc..7d8ebe3128fc
    
    Ran: ./tools/clang/scripts/upload_revision.py 5bd8dadb899d257b82db84e782a0982a2c838b70
    
    Bug: 477289191
    Disable-Rts: True
    Cq-Include-Trybots: chromium/try:android-cronet-riscv64-dbg
    Cq-Include-Trybots: chromium/try:android-cronet-riscv64-rel
    Cq-Include-Trybots: chromium/try:chromeos-amd64-generic-cfi-thin-lto-rel
    Cq-Include-Trybots: chromium/try:dawn-win10-x86-deps-rel
    Cq-Include-Trybots: chromium/try:fuchsia-arm64-cast-receiver-rel
    Cq-Include-Trybots: chromium/try:gpu-fyi-try-mac-intel-asan
    Cq-Include-Trybots: chromium/try:ios-catalyst,win-asan,android-official
    Cq-Include-Trybots: chromium/try:linux-cast-x64-rel
    Cq-Include-Trybots: chromium/try:linux-chromeos-dbg
    Cq-Include-Trybots: chromium/try:linux-swangle-try-x64,win-swangle-try-x86
    Cq-Include-Trybots: chromium/try:linux-v4l2-codec-rel
    Cq-Include-Trybots: chromium/try:linux-wayland-mutter-rel
    Cq-Include-Trybots: chromium/try:linux_chromium_cfi_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_chromeos_msan_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_msan_rel_ng
    Cq-Include-Trybots: chromium/try:mac-official,linux-official
    Cq-Include-Trybots: chromium/try:mac12-arm64-rel,mac_chromium_asan_rel_ng
    Cq-Include-Trybots: chromium/try:win-arm64-rel
    Cq-Include-Trybots: chromium/try:win-official,win32-official
    Cq-Include-Trybots: chrome/try:android-arm32-pgo,android-arm64-pgo
    Cq-Include-Trybots: chrome/try:android-x64-rel-ready
    Cq-Include-Trybots: chrome/try:chromeos-brya-chrome,chromeos-eve-chrome
    Cq-Include-Trybots: chrome/try:chromeos-volteer-chrome
    Cq-Include-Trybots: chrome/try:iphone-device,ipad-device
    Cq-Include-Trybots: chrome/try:linux-chromeos-chrome
    Cq-Include-Trybots: chrome/try:linux-pgo,mac-pgo,win32-pgo,win64-pgo
    Cq-Include-Trybots: chrome/try:win-chrome,win64-chrome,linux-chrome,mac-chrome
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:mac-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Change-Id: I68f2c6f4ff9316bfe45fb008d93f384772b3afac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7535942
    Reviewed-by: Alan Zhao <ayzhao@google.com>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1577679}

--- ui/android/texture_compressor/selectors.rs
+++ ui/android/texture_compressor/selectors.rs
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //       was inherited from etcpak.
 
 use std::simd::prelude::*;
-use std::simd::{Mask, Simd};
+use std::simd::{Mask, Select, Simd};
 
 use crate::{Reg, Reg32, UReg, SIMD_WIDTH};
 
