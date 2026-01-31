9d8d95f20772ee97e2b96e1cca079b0679bf96b8
Camille Lamy <clamy@chromium.org>
2026-01-31 04:13:37 -0800

    Implement DIP without strict SiteIsolation
    This CL enables DocumentIsolationPolicy even when the platform does not
    have strict SiteIsolation if the platform supports default
    SiteInstanceGroups.  With partial SiteIsolation, DIP contexts are marked
    as requiring a dedicated process. When no SiteIsolation is available,
    DIP contexts get a specific SiteInstance in the default
    SiteInstanceGroup, that shares the same renderer process as other
    documents in the default SiteInstanceGroup.
    
    'NO_IFTTT=The newly added parameter to AgentClusterKey is only needed on
    the browser side and does not need to be sent to the renderer process.'
    
    Validate-Test-Flakiness: skip
    Bug: 342364564
    Change-Id: Ibfd2a14fbf9b5ade732355ed313459a2bea9d8f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7246044
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Camille Lamy <clamy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1577719}

--- content/browser/agent_cluster_key_unittest.cc
+++ content/browser/agent_cluster_key_unittest.cc
@@ -45,7 +45,8 @@ TEST_F(AgentClusterKeyTest, WithCrossOriginIsolationKey) {
   url::Origin common_coi_origin = url::Origin::CreateFromNormalizedTuple(
       "https", "isolation.example.com", 443);
   AgentClusterKey::CrossOriginIsolationKey isolation_key(
-      common_coi_origin, blink::mojom::CrossOriginIsolationMode::kConcrete);
+      common_coi_origin, blink::mojom::CrossOriginIsolationMode::kConcrete,
+      true);
   AgentClusterKey key = AgentClusterKey::CreateWithCrossOriginIsolationKey(
       origin, isolation_key, AgentClusterKey::OACStatus::kSiteKeyedByDefault);
 
@@ -84,19 +85,22 @@ TEST_F(AgentClusterKeyTest, Comparisons) {
 
   // With isolation key
   AgentClusterKey::CrossOriginIsolationKey coi_a(
-      origin_a, blink::mojom::CrossOriginIsolationMode::kConcrete);
+      origin_a, blink::mojom::CrossOriginIsolationMode::kConcrete, true);
   AgentClusterKey::CrossOriginIsolationKey coi_b(
-      origin_b, blink::mojom::CrossOriginIsolationMode::kConcrete);
+      origin_b, blink::mojom::CrossOriginIsolationMode::kConcrete, true);
   AgentClusterKey::CrossOriginIsolationKey non_coi_a(
-      origin_a, blink::mojom::CrossOriginIsolationMode::kLogical);
+      origin_a, blink::mojom::CrossOriginIsolationMode::kLogical, true);
   AgentClusterKey::CrossOriginIsolationKey non_coi_b(
-      origin_b, blink::mojom::CrossOriginIsolationMode::kLogical);
+      origin_b, blink::mojom::CrossOriginIsolationMode::kLogical, true);
+  AgentClusterKey::CrossOriginIsolationKey coi_a_no_dip(
+      origin_a, blink::mojom::CrossOriginIsolationMode::kConcrete, false);
 
   EXPECT_EQ(coi_a, coi_a);
   EXPECT_EQ(non_coi_a, non_coi_a);
   EXPECT_NE(coi_a, coi_b);
   EXPECT_NE(coi_a, non_coi_a);
   EXPECT_NE(non_coi_a, non_coi_b);
+  EXPECT_EQ(coi_a, coi_a_no_dip);
 
   AgentClusterKey key_origin_a_coi_a =
       AgentClusterKey::CreateWithCrossOriginIsolationKey(
@@ -149,7 +153,7 @@ TEST_F(AgentClusterKeyTest, ComparisonsIgnoreOACStatus) {
 
   // With isolation key
   AgentClusterKey::CrossOriginIsolationKey coi(
-      origin, blink::mojom::CrossOriginIsolationMode::kConcrete);
+      origin, blink::mojom::CrossOriginIsolationMode::kConcrete, true);
 
   AgentClusterKey key_origin_a_coi_1 =
       AgentClusterKey::CreateWithCrossOriginIsolationKey(
@@ -191,7 +195,8 @@ TEST_F(AgentClusterKeyTest, StreamOutput) {
       AgentClusterKey::CreateWithCrossOriginIsolationKey(
           origin_a,
           AgentClusterKey::CrossOriginIsolationKey(
-              origin_b, blink::mojom::CrossOriginIsolationMode::kConcrete),
+              origin_b, blink::mojom::CrossOriginIsolationMode::kConcrete,
+              true),
           AgentClusterKey::OACStatus::kSiteKeyedByDefault);
   dump << key_origin_a_coi_b;
   EXPECT_EQ(dump.str(),
