dacca46810f9a35d7da6b72ccca958f3d1028b51
Lin Guo <linguo2@microsoft.com>
2026-01-30 23:29:42 -0800

    [E2E] Fix status bar icon color when omnibox focused on themed NTP
    When a custom background image is set for NTP, the status bar icons are
    forced to use light color to ensure visibility against the dark background.
    However, when the omnibox gains focus, the omnibox dropdown has a light
    background, making the light status bar icons hard to see.
    
    This CL adds a check for omnibox focus state when determining whether to
    force light status bar icon color. The light icon color is now only forced
    when all three conditions are met:
    - mForceLightIconColorForNtp is true (custom background is set)
    - Current tab is a standard NTP
    - Omnibox is NOT focused
    
    Screen record before fix:
    https://drive.google.com/file/d/1uNFlQyHQo0wzaGuSrXEZg_EB-wmggwu8/view?usp=sharing
    
    Screen record after fix:
    https://drive.google.com/file/d/1uG98XJbcEvIcNbl23JOHci8m-ZvTmAB7/view?usp=sharing
    
    Bug: 479886237
    Change-Id: I12b089c39732f541fb05fe8ebf0fcca294f6403c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7532218
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Lin Guo <linguo2@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1577694}

--- chrome/android/java/src/org/chromium/chrome/browser/ui/system/StatusBarColorController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ui/system/StatusBarColorController.java
@@ -487,7 +487,7 @@ public class StatusBarColorController
                 mEdgeToEdgeSystemBarColorHelper,
                 mActivity,
                 statusBarColor,
-                mForceLightIconColorForNtp && isStandardNtp());
+                mForceLightIconColorForNtp && isStandardNtp() && !mIsOmniboxFocused);
     }
 
     /**
