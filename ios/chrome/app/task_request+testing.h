5b32c016459c5bc03f211880f0cdf1d3d3069ab9
Federica Germinario <fedegermi@google.com>
2026-02-02 08:03:36 -0800

    [ios] Add TaskOrchestrator tests in task_orchestrator_unittest.mm
    Add tests to check that the TaskOrchestrator works as expected.
    
    Bug: 478851030
    Change-Id: I161bed7a9248655f484bfe3e20246b336f96eb04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7510822
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1578147}

---
+++ ios/chrome/app/task_request+testing.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_APP_TASK_REQUEST_TESTING_H_
+#define IOS_CHROME_APP_TASK_REQUEST_TESTING_H_
+
+#import "ios/chrome/app/task_request.h"
+
+@interface TaskRequest (Testing)
+
+- (instancetype)initWithSceneID:(std::string_view)sceneID;
+
++ (instancetype)taskForTestingWithSceneID:(std::string_view)sceneID
+                             executeBlock:(ProceduralBlock)block;
+
+@end
+
+#endif  // IOS_CHROME_APP_TASK_REQUEST_TESTING_H_
