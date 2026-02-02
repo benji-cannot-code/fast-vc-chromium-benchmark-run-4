4957fda8a390fd881830e064fc9abcb29abef775
Hiroshige Hayashizaki <hiroshige@chromium.org>
2026-02-01 21:14:34 -0800

    Make PrefetchContainer arg const in PrefetchContainer::Observer
    Preparation for
    https://chromium-review.googlesource.com/c/chromium/src/+/7536560
    as `base::ObserverList::Notify()` forbids non-const refs as args.
    
    Mostly mechanical, except for introducing/using
    `CreateConstServingHandle()` and the const version of `GetWeakPtr()`
    in `PrefetchContainer`.
    
    No behavior changes.
    
    Bug: 400761083
    Change-Id: I82ee8daa97cef66e040366f2ce4aa7a91ede1db5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7536558
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1577922}

--- content/browser/preloading/prefetch/prefetch_handle_impl.cc
+++ content/browser/preloading/prefetch/prefetch_handle_impl.cc
@@ -30,14 +30,14 @@ void PrefetchContainerObserver::SetOnPrefetchCompletedOrFailedCallback(
 }
 
 void PrefetchContainerObserver::OnWillBeDestroyed(
-    PrefetchContainer& prefetch_container) {}
+    const PrefetchContainer& prefetch_container) {}
 
 void PrefetchContainerObserver::OnGotInitialEligibility(
-    PrefetchContainer& prefetch_container,
+    const PrefetchContainer& prefetch_container,
     PreloadingEligibility eligibility) {}
 
 void PrefetchContainerObserver::OnDeterminedHead(
-    PrefetchContainer& prefetch_container) {
+    const PrefetchContainer& prefetch_container) {
   if (on_prefetch_head_received_) {
     // This condition will be used in a callback provided in the future.
     // See
@@ -53,7 +53,7 @@ void PrefetchContainerObserver::OnDeterminedHead(
 }
 
 void PrefetchContainerObserver::OnPrefetchCompletedOrFailed(
-    PrefetchContainer& prefetch_container,
+    const PrefetchContainer& prefetch_container,
     const network::URLLoaderCompletionStatus& completion_status,
     const std::optional<int>& response_code) {
   // `IsDecoy()` check is added to preserve the existing behavior.
