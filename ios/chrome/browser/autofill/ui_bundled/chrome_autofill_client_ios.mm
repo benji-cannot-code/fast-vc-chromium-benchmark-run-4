92cb95c847f3ee31bea4d5a4cc16c21a06393ad9
sugoi1 <sugoi@google.com>
2026-02-02 13:29:30 -0800

    [iOS] Add EntityDataManager factory
    This will be the entry point for enhanced autofill data on iOS.
    
    This CL implements both the factory and allows
    ChromeAutofillClientIOS::GetEntityDataManager() to return an
    EntityDataManager when the feature is enabled.
    
    Bug: 480934102
    Change-Id: I4fd29313ff4c11063043be42735a9906927b2708
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7537670
    Reviewed-by: Sebastien S-G <sebsg@chromium.org>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1578361}

--- ios/chrome/browser/autofill/ui_bundled/chrome_autofill_client_ios.mm
+++ ios/chrome/browser/autofill/ui_bundled/chrome_autofill_client_ios.mm
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/autofill/model/autocomplete_history_manager_factory.h"
 #import "ios/chrome/browser/autofill/model/autofill_log_router_factory.h"
 #import "ios/chrome/browser/autofill/model/bottom_sheet/autofill_bottom_sheet_tab_helper.h"
+#import "ios/chrome/browser/autofill/model/ios_autofill_entity_data_manager_factory.h"
 #import "ios/chrome/browser/autofill/model/personal_data_manager_factory.h"
 #import "ios/chrome/browser/autofill/model/strike_database_factory.h"
 #import "ios/chrome/browser/autofill/ui_bundled/scoped_autofill_payment_reauth_module_override.h"
@@ -66,6 +67,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/plus_addresses/model/plus_address_service_factory.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ios/chrome/browser/shared/public/commands/autofill_commands.h"
+#import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/signin/model/authentication_service.h"
 #import "ios/chrome/browser/signin/model/authentication_service_factory.h"
 #import "ios/chrome/browser/signin/model/identity_manager_factory.h"
@@ -182,6 +184,9 @@ ValuablesDataManager* ChromeAutofillClientIOS::GetValuablesDataManager() {
 }
 
 EntityDataManager* ChromeAutofillClientIOS::GetEntityDataManager() {
+  if (base::FeatureList::IsEnabled(kIOSEnhancedAutofill)) {
+    return IOSAutofillEntityDataManagerFactory::GetForProfile(profile_);
+  }
   return nullptr;
 }
 
