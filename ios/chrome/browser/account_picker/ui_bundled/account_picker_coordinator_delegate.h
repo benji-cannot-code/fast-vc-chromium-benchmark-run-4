85d55efa515cc358b9a94a83118ca34133879434
Arthur Milchior <arthurmilchior@google.com>
2026-02-02 07:06:56 -0800

    [iOS]Close the account picker if sign-in get disabled
    This is a reland of crrev.com/c/7532136 that was reverted due to a merge
    conflict in declarations.
    
    This CL does multiple related changes.
    
    Firstly, it ensures that all account picker coordinators get stopped if
    sign-in become disabled. In particular, it ensures that Chrome does not
    CHECK later when trying to add an account or signin.
    
    Secondly, this CL ensure that if the coordinator is not closed properly,
    we receive a crash report.
    
    Some delegate’s methods had to be renamed, because there can now be
    multiple reason to request to be stopped, not only the user tapping
    "cancel".
    
    Bug: 475920665
    Change-Id: I8359afd9e733ad60d44c1b138ede5fc562de74d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7539374
    Reviewed-by: Quentin Pubert <qpubert@google.com>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1578114}

--- ios/chrome/browser/account_picker/ui_bundled/account_picker_coordinator_delegate.h
+++ ios/chrome/browser/account_picker/ui_bundled/account_picker_coordinator_delegate.h
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                     askEveryTime:(BOOL)askEveryTime;
 
 // Called when the user taps "Cancel" in the confirmation screen.
-- (void)accountPickerCoordinatorCancel:
+- (void)accountPickerCoordinatorWantsToBeStopped:
     (AccountPickerCoordinator*)accountPickerCoordinator;
 
 // Called when the form has no identities left to present (identities have been
