ea6f204a1a2af9ac34aa02ba8746095cfa22bd6a
sugoi1 <sugoi@google.com>
2026-02-02 13:17:09 -0800

    [iOS] Create feature flag for Enhanced Autofill on iOS
    This CL creates a feature flag for the upcoming Enhanced Autofill
    feature.
    
    Bug: 480934102
    Change-Id: If8cab123467c0ebb58cc73f282113f180524a9b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7539276
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Reviewed-by: Sebastien S-G <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1578354}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -439,6 +439,8 @@ bool IsLiquidGlassEffectEnabled() {
   return false;
 }
 
+BASE_FEATURE(kIOSEnhancedAutofill, base::FEATURE_DISABLED_BY_DEFAULT);
+
 BASE_FEATURE(kIOSKeyboardAccessoryDefaultView,
              base::FEATURE_ENABLED_BY_DEFAULT);
 
