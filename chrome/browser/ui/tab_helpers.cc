4b05cfe88a7170e6599867feb761096073012b75
Johann Hofmann <johannhof@chromium.org>
2026-02-02 11:02:56 -0800

    Remove ThirdPartyCookies.BreakageIndicator UKM metrics
    These were needed to analyze breakage for third-party cookie deprecation, which is no longer happening.
    
    Bug: 474331666
    Change-Id: I9420ddcf4cc012eb450cf01f3c01183d3e80443e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7532544
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Johann Hofmann <johannhof@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Ryan Kalla <ryankalla@google.com>
    Cr-Commit-Position: refs/heads/main@{#1578258}

--- chrome/browser/ui/tab_helpers.cc
+++ chrome/browser/ui/tab_helpers.cc
@@ -79,7 +79,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/sessions/sync_sessions_web_contents_router_factory.h"
 #include "chrome/browser/tab_contents/navigation_metrics_recorder.h"
 #include "chrome/browser/task_manager/web_contents_tags.h"
-#include "chrome/browser/tpcd/http_error_observer/http_error_tab_helper.h"
 #include "chrome/browser/tpcd/metadata/devtools_observer.h"
 #include "chrome/browser/translate/chrome_translate_client.h"
 #include "chrome/browser/trusted_vault/trusted_vault_encryption_keys_tab_helper.h"
@@ -586,7 +585,6 @@ void TabHelpers::AttachTabHelpers(WebContents* web_contents) {
     SupervisedUserNavigationObserver::CreateForWebContents(web_contents);
   }
 #endif
-  HttpErrorTabHelper::CreateForWebContents(web_contents);
   tasks::TaskTabHelper::CreateForWebContents(web_contents);
   tpcd::metadata::TpcdMetadataDevtoolsObserver::CreateForWebContents(
       web_contents);
