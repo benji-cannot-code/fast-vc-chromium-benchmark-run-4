25f05bc9eccd12e0f61b94da8d04c209a9e939bc
Adam Norberg <norberg@google.com>
2026-02-02 13:35:10 -0800

    Introduce SizedBuf to support NULs in dmg attribution.
    Omaha tags may contain bytes with value 0.
    
    Along the way, I discovered that the existing attribution logic
    expected the sentinel to end with a NUL and used that to determine
    how much to zero out (before copying the new data over it). However,
    `hfs_tool setattr` is not guaranteed to put a NUL at the end of an
    extended attribute it creates. libdmg-hfsplus documentation alludes to
    vague problems with the B-tree implementation requiring setattr to be
    performed only after all data has been added to the volume, and in
    practice this means that the inline attr record is likely to be
    followed by zeroes anyway, letting this implementation look okay in
    practice. However, it never worked, so I have not attempted to preserve
    the behavior; reattribution requires the entire sentinel (or as much of
    it as you want to zero out, at least).
    
    This doesn't actually allow for NULs to be _entered_ in the CLI tool
    yet, I figured this patch was large enough already. I intend to add a
    `--data-format` flag for operations that take "data arguments"
    (`dmg build`'s sentinel arg, sentinel and replacement for
    `dmg attribute`, xattr data (but not key) for `hfs xattr`) to allow
    the user to specify a data representation other than "literal"; I
    intend to support `base64` encoding.
    
    This also _only_ covers `dmg attribute`, I haven't done this plumbing
    work for the other commands yet. One at a time seems easier to review.
    
    Bug: 435220364
    Change-Id: I911b18c12dbf90d58ba10ed9eef197e268bbc3ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7535446
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Adam Norberg <norberg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1578364}

--- third_party/libdmg-hfsplus/src/dmg/dmg.c
+++ third_party/libdmg-hfsplus/src/dmg/dmg.c
@@ -5,8 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string.h>
 #include <getopt.h>
 
-#include <dmg/dmg.h>
-#include <dmg/filevault.h>
+#include "dmg/dmg.h"
+#include "dmg/filevault.h"
+#include "sizedbuf.h"
 
 char endianness;
 
@@ -129,14 +130,15 @@ int main(int argc, char* argv[]) {
 	} else if(strcmp(cmd, "dmg") == 0) {
 		convertToDMG(in, out, &comp, runSectors);
 	} else if(strcmp(cmd, "attribute") == 0) {
-		char *anchor, *data;
 		if(argc < optind + 2) {
 			printf("Not enough arguments: attribute <in> <out> <sentinel> <string>");
 			return 2;
 		}
-		anchor = argv[optind++];
-		data = argv[optind++];
-		updateAttribution(in, out, anchor, data, strlen(data));
+		SizedBuf* anchorBuf = AllocBufCopyString(argv[optind++]);
+		SizedBuf* dataBuf = AllocBufCopyString(argv[optind++]);
+		updateAttributionFromBufs(in, out, anchorBuf, dataBuf);
+		free(dataBuf);
+		free(anchorBuf);
 	}
 
 	return 0;
