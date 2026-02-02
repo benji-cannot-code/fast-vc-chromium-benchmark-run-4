4c09ea2c0223506431aa3fcea6bf000c5cc3de1e
Samuel Huang <huangs@chromium.org>
2026-02-02 07:18:32 -0800

    Revert "viz::VideoCaptureOverlay: Remove gfx::ColorTransform"
    This reverts commit c04bbf7c8dddb9e908498e3575715e095b8bc4ec.
    
    Reason for revert: This likely caused ash_pixeltests failures on linux-chromeos-rel for:
    * DisplaySize/DisplayParameterizedCaptureModePixelTest.VideoCaptureNotification/2
    * DisplaySize/DisplayParameterizedCaptureModePixelTest.VideoCaptureNotification/3
    
    
    Original change's description:
    > viz::VideoCaptureOverlay: Remove gfx::ColorTransform
    >
    > Change the function to use skia::ConvertRGBAToOrFromYUVA. In the center
    > of VideoCaptureOverlay::Sprite::TransformImage, populate an array of
    > SkColor4fs instead of a separate color and alpha array (this is needed
    > because the old gfx::ColorTransform doesn't take an SkColor4f).
    >
    > This change threw up a bunch of UNSAFE_TODO warnings. Fix all of the
    > ones that this change touched by changing transformed_image_ to a
    > base::HeapArray instead of just an array, and use base::span to make the
    > pointer math safe.
    >
    > Of note is that all of this CPU-side pixel manipulation should be moved
    > into Skia calls (and ideally done on the GPU).
    >
    > Bug: 463702880
    > Change-Id: I86eb50478f310a265d8548d1732e3136afbcdd70
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7510983
    > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > Commit-Queue: ccameron chromium <ccameron@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1578052}
    
    Bug: 463702880
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I13f169832ff0e0cb5222e2d6dc0c279620b031e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7539418
    Owners-Override: Samuel Huang <huangs@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1578120}

--- components/viz/service/frame_sinks/video_capture/video_capture_overlay.h
+++ components/viz/service/frame_sinks/video_capture/video_capture_overlay.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "base/containers/heap_array.h"
 #include "base/functional/callback.h"
 #include "base/memory/raw_ref.h"
 #include "base/memory/ref_counted.h"
@@ -211,7 +210,7 @@ class VIZ_SERVICE_EXPORT VideoCaptureOverlay
     // not interlaved: Instead, there are 5 planes of data (one minus alpha, Y,
     // subsampled one minus alpha, U, V). For both formats, the color components
     // are premultiplied for more-efficient Blit()'s.
-    base::HeapArray<float> transformed_image_;
+    std::unique_ptr<float[]> transformed_image_;
   };
 
   // Computes the region of the source that, if changed, would require
