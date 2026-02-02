0bb099d07d662d4e3dad5affd80399426159a2b9
Rune Lillesveen <futhark@chromium.org>
2026-02-02 05:57:09 -0800

    [webmcp] Add support for <textarea> elements
    Bug: 480883369
    Change-Id: I5cbf3b337d640fb946eac359ca0a879655d95780
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7531660
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1578075}

--- third_party/blink/renderer/core/html/forms/html_text_area_element.cc
+++ third_party/blink/renderer/core/html/forms/html_text_area_element.cc
@@ -63,6 +63,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/instrumentation/tracing/trace_event.h"
 #include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
+#include "third_party/blink/renderer/platform/json/json_values.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/text/platform_locale.h"
 #include "third_party/blink/renderer/platform/wtf/std_lib_extras.h"
@@ -835,6 +836,18 @@ void HTMLTextAreaElement::SetFocused(bool is_focused,
     SetUserHasEditedTheFieldAndBlurred();
   }
   TextControlElement::SetFocused(is_focused, focus_type);
+}
+
+std::unique_ptr<JSONObject> HTMLTextAreaElement::GetWebMCPParameterSchema()
+    const {
+  auto schema = std::make_unique<JSONObject>();
+  schema->SetString("type", "string");
+  return schema;
+}
+
+void HTMLTextAreaElement::FillWebMCPData(JSONValue& data) {
+  String selected_value = GetMCPJSONValue(data);
+  SetValue(selected_value);
 }
 
 }  // namespace blink
