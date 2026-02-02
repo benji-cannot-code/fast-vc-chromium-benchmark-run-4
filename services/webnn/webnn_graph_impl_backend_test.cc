acbfdb0bc008859b90bf171ae71b06591af1ba8c
junwei <junwei.fu@intel.com>
2026-02-02 13:05:11 -0800

    webnn: Disable services unit tests on Windows for TFLite backend
    CL-7510831 [1] enables services unit tests of TFLite backend on Windows,
    but a large number of gardened builds are failing due to this change.
    This CL disables these tests to stabilize the build waterfall.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/7510831
    
    Bug: 480128592, 477562219
    Change-Id: I001b6f8ce827e809d7ec0425b6df992e3c4f89a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7538813
    Reviewed-by: Nathan Memmott <memmott@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1578344}

--- services/webnn/webnn_graph_impl_backend_test.cc
+++ services/webnn/webnn_graph_impl_backend_test.cc
@@ -329,7 +329,7 @@ void WebNNGraphImplBackendTest::SetUp() {
 #endif  // BUILDFLAG(IS_MAC)
 
 // TODO(crbug.com/325612086): Parameterize these tests for different backends.
-#if BUILDFLAG(WEBNN_USE_TFLITE) && !BUILDFLAG(IS_MAC)
+#if BUILDFLAG(WEBNN_USE_TFLITE) && !BUILDFLAG(IS_MAC) && !BUILDFLAG(IS_WIN)
 class WebNNGraphImplBackendTest : public testing::Test,
                                   public testing::WithParamInterface<bool> {
  public:
@@ -407,7 +407,8 @@ void WebNNGraphImplBackendTest::SetUp() {
 
   SetUpBase();
 }
-#endif  // BUILDFLAG(WEBNN_USE_TFLITE) && !BUILDFLAG(IS_MAC)
+#endif  // BUILDFLAG(WEBNN_USE_TFLITE) && !BUILDFLAG(IS_MAC) &&
+        // !BUILDFLAG(IS_WIN)
 
 void WebNNGraphImplBackendTest::SetUpBase() {
   webnn_test_environment_.BindWebNNContextProvider(
