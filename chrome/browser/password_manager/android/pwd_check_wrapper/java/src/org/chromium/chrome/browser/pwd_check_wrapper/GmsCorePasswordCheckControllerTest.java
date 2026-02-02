faf3851be1d4710f2a6355847893a6950db1f0c2
Victor Hugo Vianna Silva <victorvianna@google.com>
2026-02-02 05:59:41 -0800

    [android/passwords] Test signed-in state instead of syncing state
    The latter got replaced by the former. This is a step torwards
    deleting deprecated SyncService APIS (isSyncFeatureEnabled(),
    hasSyncConsent() and friends).
    
    Bug: None
    Change-Id: I2b68cb1382126b7543dd9296af7a37bd3c86595b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7535123
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Ioana Treib <ioanap@chromium.org>
    Commit-Queue: Ioana Treib <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1578076}

--- chrome/browser/password_manager/android/pwd_check_wrapper/java/src/org/chromium/chrome/browser/pwd_check_wrapper/GmsCorePasswordCheckControllerTest.java
+++ chrome/browser/password_manager/android/pwd_check_wrapper/java/src/org/chromium/chrome/browser/pwd_check_wrapper/GmsCorePasswordCheckControllerTest.java
@@ -87,7 +87,6 @@ public class GmsCorePasswordCheckControllerTest {
 
     private void configureMockSyncServiceToSyncPasswords() {
         SyncServiceFactory.setInstanceForTesting(mSyncService);
-        when(mSyncService.isSyncFeatureEnabled()).thenReturn(true);
         when(mSyncService.getSelectedTypes()).thenReturn(Set.of(UserSelectableType.PASSWORDS));
         when(mSyncService.getAccountInfo())
                 .thenReturn(
