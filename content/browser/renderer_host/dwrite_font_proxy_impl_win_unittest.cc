a1ad52abcafa81e6b9623e3dd4cf8f9977e98540
Lei Zhang <thestig@chromium.org>
2026-02-02 12:31:58 -0800

    Don't build font_unique_name_lookup code on Windows
    Originally, https://crrev.com/620468 only used this code on Windows for
    testing. The specific test case that used blink::FontTableMatcher no
    longer exists in modern code, so stop building this font code on
    WIndows.
    
    Change-Id: I9ca8af29ff294e7a2b0677209a985549507bb6bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7535443
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1578321}

--- content/browser/renderer_host/dwrite_font_proxy_impl_win_unittest.cc
+++ content/browser/renderer_host/dwrite_font_proxy_impl_win_unittest.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/remote.h"
 #include "services/service_manager/public/cpp/bind_source_info.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "third_party/blink/public/common/font_unique_name_lookup/font_table_matcher.h"
 #include "third_party/icu/source/common/unicode/umachine.h"
 
 namespace content {
