bea21c8dfcb7a1b5ec5a1e592359893188d73fe7
Chris Lu <thegreenfrog@google.com>
2026-02-02 01:51:51 -0800

    [ios] Do not allow drag and drop group creation when not allowed
    Drag and Drop group creation avoided the forbidden cases determined
    in -dropOperationForDropSession:toIndex:.
    
    Fixed: 479880858, 474623344
    Change-Id: I544dd85fb207ef4c982217c6f9ad553351168210
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7535555
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1577987}

--- ios/chrome/browser/tab_switcher/tab_grid/base_grid/ui/base_grid_view_controller.mm
+++ ios/chrome/browser/tab_switcher/tab_grid/base_grid/ui/base_grid_view_controller.mm
@@ -937,9 +937,15 @@ typedef NS_ENUM(NSInteger, DragEntrySide) {
         static_cast<TabGroupInfo*>(dragItem.localObject);
     isSharedGroup = [self.dragDropHandler isGroupShared:tabGroupInfo];
   }
+  // This is how the explicit forbidden icon or (+) copy icon is shown. Move
+  // has no explicit icon.
+  UIDropOperation dropOperation = [self.dragDropHandler
+      dropOperationForDropSession:session
+                          toIndex:destinationIndexPath.item];
   if (IsTabGridDragAndDropEnabled() && !isSharedGroup &&
       destinationItemIndexPath &&
-      draggedItemIndexPath != destinationItemIndexPath) {
+      draggedItemIndexPath != destinationItemIndexPath &&
+      dropOperation != UIDropOperationForbidden) {
     // If the drag goes into a different cell's frame, either highlight or allow
     // for reorder depending on location.
     DragEntrySide entryDirection = DragEntrySideNone;
@@ -972,11 +978,6 @@ typedef NS_ENUM(NSInteger, DragEntrySide) {
       [self clearCurrentlyHighlightedCell];
     }
 
-    // This is how the explicit forbidden icon or (+) copy icon is shown. Move
-    // has no explicit icon.
-    UIDropOperation dropOperation = [self.dragDropHandler
-        dropOperationForDropSession:session
-                            toIndex:destinationIndexPath.item];
     return [[UICollectionViewDropProposal alloc]
         initWithDropOperation:dropOperation
                        intent:
@@ -993,11 +994,6 @@ typedef NS_ENUM(NSInteger, DragEntrySide) {
     id<UICollectionViewDropItem> dropItem = coordinator.items.firstObject;
     NSIndexPath* sourceIndexPath = dropItem.sourceIndexPath;
     NSIndexPath* destinationIndexPath = coordinator.destinationIndexPath;
-
-    if (!dropItem.dragItem) {
-      base::debug::DumpWithoutCrashing();
-      return;
-    }
 
     self.dragEndAtNewIndex = YES;
     _dropAnimationInProgress = YES;
@@ -1025,10 +1021,6 @@ typedef NS_ENUM(NSInteger, DragEntrySide) {
                           toGroup:destinationItem.tabGroupItem.tabGroup];
     } else {
       TabInfo* tabInfo = static_cast<TabInfo*>(dropItem.dragItem.localObject);
-      if (!tabInfo) {
-        base::debug::DumpWithoutCrashing();
-        return;
-      }
       // If the index path of `sourceItem` < `destinationItem`, then the logic
       // will ensure that there is no animation for the replacement of
       // `destinationItem` into the new group. There is also logic ensure that
