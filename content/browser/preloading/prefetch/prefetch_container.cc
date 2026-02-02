199b1c389b2ea693221d75840069c8f778adb7d1
Hiroshige Hayashizaki <hiroshige@chromium.org>
2026-02-01 21:07:00 -0800

    Make PrefetchContainer::Simulate* more realistic
    This CL
    - Calls `OnEligibilityCheckComplete()` to notify
      `PrefetchContainer::Observer`.
    - Moves the `SetHoldbackStatus(PreloadingHoldbackStatus::kAllowed)` call
      to `SimulatePrefetchStartedForTest()`, because it's called when
      a prefetch is started (not upon eligibility check completion) in
      the non-test code.
    
    Test-only changes.
    
    Bug: 400761083
    Change-Id: I030642c1711b4de7673bfecefd00cc7c40a33619
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7536555
    Reviewed-by: Ken Okada <kenoss@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1577920}

--- content/browser/preloading/prefetch/prefetch_container.cc
+++ content/browser/preloading/prefetch/prefetch_container.cc
@@ -1432,15 +1432,14 @@ void PrefetchContainer::UpdateServingPageMetrics() {
 }
 
 void PrefetchContainer::SimulatePrefetchEligibleForTest() {
-  if (request().attempt()) {
-    request().attempt()->SetEligibility(PreloadingEligibility::kEligible);
-    request().attempt()->SetHoldbackStatus(PreloadingHoldbackStatus::kAllowed);
-  }
-  SetLoadState(LoadState::kEligible);
-  SetPrefetchStatus(PrefetchStatus::kPrefetchNotStarted);
+  CHECK_EQ(redirect_chain_.size(), 1u);
+  OnEligibilityCheckComplete(PreloadingEligibility::kEligible);
 }
 
 void PrefetchContainer::SimulatePrefetchStartedForTest() {
+  if (request().attempt()) {
+    request().attempt()->SetHoldbackStatus(PreloadingHoldbackStatus::kAllowed);
+  }
   SetLoadState(LoadState::kStarted);
   SetPrefetchStatus(PrefetchStatus::kPrefetchNotFinishedInTime);
 }
@@ -1452,11 +1451,8 @@ void PrefetchContainer::SimulatePrefetchCompletedForTest() {
 void PrefetchContainer::SimulatePrefetchFailedIneligibleForTest(
     PreloadingEligibility eligibility) {
   CHECK_NE(PreloadingEligibility::kEligible, eligibility);
-
-  if (request().attempt()) {
-    request().attempt()->SetEligibility(eligibility);
-  }
-  SetLoadState(LoadState::kFailedIneligible);
+  CHECK_EQ(redirect_chain_.size(), 1u);
+  OnEligibilityCheckComplete(eligibility);
 }
 
 void PrefetchContainer::OnDetectedCookiesChange(
