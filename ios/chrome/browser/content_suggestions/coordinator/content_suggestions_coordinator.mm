7f696391f86e38a76febbc601569c89ea74fef91
Cheick Cisse <cheickcisse@google.com>
2026-02-02 13:49:28 -0800

    [iOS] Enable Default Browser Promo Refresh and update Set Up List
    This CL:
    - Updates the Set Up List Default Browser item to open the
    Default Browser Settings page directly when the refresh flag is enabled.
    - Adds metrics for Set Up List source in Default Browser Settings.
    - Correctly marks the Set Up List item as complete when settings are
    interacted with.
    
    UI Changes:
    Before: https://screenshot.googleplex.com/7ZvRcqB7vdJQBWs.png
    Now: https://screenshot.googleplex.com/BQgCLHSqdoH2WZF.png
    
    Bug: 477908513
    Change-Id: I978841386d9e3e2058d285a1e32918ce1d72c4b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7509931
    Reviewed-by: Benjamin Williams <bwwilliams@google.com>
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1578377}

--- ios/chrome/browser/content_suggestions/coordinator/content_suggestions_coordinator.mm
+++ ios/chrome/browser/content_suggestions/coordinator/content_suggestions_coordinator.mm
@@ -93,6 +93,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/content_suggestions/ui/content_suggestions_view_controller_audience.h"
 #import "ios/chrome/browser/default_browser/model/promo_source.h"
 #import "ios/chrome/browser/default_browser/model/utils.h"
+#import "ios/chrome/browser/default_browser/promo/public/features.h"
 #import "ios/chrome/browser/discover_feed/model/discover_feed_service.h"
 #import "ios/chrome/browser/discover_feed/model/discover_feed_service_factory.h"
 #import "ios/chrome/browser/favicon/model/favicon_loader.h"
@@ -754,9 +755,9 @@ using segmentation_platform::TipIdentifier;
     OpenIOSDefaultBrowserSettingsPage();
   } else if (variation ==
              DefaultBrowserMagicStackIosVariationType::kTapToAppSettings) {
-    id<SettingsCommands> settings_handler = HandlerForProtocol(
+    id<SettingsCommands> settingsHandler = HandlerForProtocol(
         self.browser->GetCommandDispatcher(), SettingsCommands);
-    [settings_handler
+    [settingsHandler
         showDefaultBrowserSettingsFromViewController:nil
                                         sourceForUMA:
                                             DefaultBrowserSettingsPageSource::
@@ -1249,6 +1250,18 @@ using segmentation_platform::TipIdentifier;
 - (void)showUIForSelectedSetUpListItem:(SetUpListItemType)type {
   switch (type) {
     case SetUpListItemType::kDefaultBrowser:
+      if (IsDefaultBrowserPictureInPictureEnabled()) {
+        id<SettingsCommands> settingsHandler = HandlerForProtocol(
+            self.browser->GetCommandDispatcher(), SettingsCommands);
+        [settingsHandler
+            showDefaultBrowserSettingsFromViewController:nil
+                                            sourceForUMA:
+                                                DefaultBrowserSettingsPageSource::
+                                                    kSetUpList];
+        LogToFETDefaultBrowserPromoShown(
+            feature_engagement::TrackerFactory::GetForProfile(self.profile));
+        return;
+      }
       [self showDefaultBrowserPromo];
       break;
     case SetUpListItemType::kAutofill:
--- ios/chrome/browser/default_browser/promo/generic/coordinator/default_browser_generic_promo_mediator.mm
+++ ios/chrome/browser/default_browser/promo/generic/coordinator/default_browser_generic_promo_mediator.mm
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/default_browser/promo/generic/coordinator/default_browser_generic_promo_mediator.h"
 
 #import "ios/chrome/browser/default_browser/model/utils.h"
+#import "ios/chrome/browser/ntp/model/set_up_list_item_type.h"
+#import "ios/chrome/browser/ntp/model/set_up_list_prefs.h"
+#import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ui/base/l10n/l10n_util_mac.h"
 
 @implementation DefaultBrowserGenericPromoMediator
@@ -13,6 +16,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark - Public
 
 - (void)didTapPrimaryActionButton:(BOOL)useDefaultAppsDestination {
+  PrefService* localState = GetApplicationContext()->GetLocalState();
+  if (localState) {
+    // Mark the Set Up List Item as complete. This is a no-op if the item is
+    // already complete.
+    set_up_list_prefs::MarkItemComplete(localState,
+                                        SetUpListItemType::kDefaultBrowser);
+  }
+
   OpenIOSDefaultBrowserSettingsPage(useDefaultAppsDestination);
 }
 
