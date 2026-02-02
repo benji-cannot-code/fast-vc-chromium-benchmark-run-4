4d0b30811a80c295e8a527acedd5edf05c96dc67
Joel Tan-Aristy <jtanaristy@google.com>
2026-02-02 15:50:09 -0800

    [SetUp List] Creates bottom sheet layout structure
    Adds components for the bottom sheet layout per UX spec.
    Implementation will occur in a follow up.
    
    Bug: 479597724
    Change-Id: I4c12f66006f4c06ea9459f74b2c71179131df836
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7536341
    Commit-Queue: Joel Tan-Aristy <jtanaristy@google.com>
    Reviewed-by: Xinyi Ji <xinyiji@chromium.org>
    Reviewed-by: Gazal Agarwal <agazal@google.com>
    Commit-Queue: Gazal Agarwal <agazal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1578435}

---
+++ chrome/browser/educational_tip/java/src/org/chromium/chrome/browser/educational_tip/two_cell/EducationalTipBottomSheetListItemView.java
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.educational_tip.two_cell;
+
+import android.content.Context;
+import android.widget.RelativeLayout;
+
+import org.chromium.build.annotations.NullMarked;
+
+/**
+ * The list item view within a {@link EducationalTipBottomSheetListContainerView} that is in a
+ * bottom sheet.
+ */
+@NullMarked
+public class EducationalTipBottomSheetListItemView extends RelativeLayout {
+    public EducationalTipBottomSheetListItemView(Context context) {
+        super(context);
+    }
+
+    // TODO(crbug.com/479597724): Implement title, description, icon, and button for a list item.
+}
