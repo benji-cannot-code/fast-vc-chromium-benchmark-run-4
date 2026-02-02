7985251183103b2ec79b2e041bb591e2ceb67ced
lukedeng <lukedeng@chromium.org>
2026-02-02 15:02:54 -0800

    Add new customized ValidManifestId Class
    This CL introduces customized webapps::ValidManifestId to improving type safety. This class wraps a GURL and introduced new factory method ensure that only valid URLs are used.
    
    This change includes the ValidManifestId class, its unit tests, and the corresponding build file updates.
    
    Bug: 479587362
    Change-Id: Idf97df92796209dec14f8e928aa20ac7cbcd52fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7529415
    Commit-Queue: Luke Deng <lukedeng@google.com>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1578415}

---
+++ components/webapps/common/manifest_id.h
@@ -0,0 +1,61 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_WEBAPPS_COMMON_MANIFEST_ID_H_
+#define COMPONENTS_WEBAPPS_COMMON_MANIFEST_ID_H_
+
+#include <iosfwd>
+#include <optional>
+#include <string_view>
+#include <utility>
+
+#include "url/gurl.h"
+
+namespace webapps {
+
+// A strong type for the Manifest ID of a web app.
+class ValidManifestId {
+ public:
+  // Creates a manifest 'id' from the `url_str`, by parsing it as a url and then
+  // following the manifest `id` parsing algorithm at
+  // https://www.w3.org/TR/appmanifest/#id-member, where it strips any
+  // #fragments from the url.
+  static std::optional<ValidManifestId> Create(std::string_view url_str);
+
+  // Creates a manifest 'id' from the `url`. This follows the manifest `id`
+  // parsing
+  // algorithm at https://www.w3.org/TR/appmanifest/#id-member, and strips any
+  // #fragments from the url.
+  static std::optional<ValidManifestId> Create(const GURL& url);
+
+  // Creates a manifest 'id' from the `url`. This follows the manifest `id`
+  // parsing algorithm at https://www.w3.org/TR/appmanifest/#id-member, and
+  // strips any #fragments from the url.
+  explicit ValidManifestId(GURL url);
+
+  ValidManifestId(const ValidManifestId& other);
+  ValidManifestId& operator=(const ValidManifestId& other);
+  ~ValidManifestId();
+
+  bool is_valid() const;
+
+  const std::string& spec() const;
+
+  const GURL& value() const;
+
+  friend auto operator<=>(const ValidManifestId& lhs,
+                          const ValidManifestId& rhs);
+
+  bool operator==(const ValidManifestId& other) const;
+
+ private:
+  GURL url_;
+};
+
+std::ostream& operator<<(std::ostream& out, const ValidManifestId& manifest_id);
+
+}  // namespace webapps
+
+#endif  // COMPONENTS_WEBAPPS_COMMON_MANIFEST_ID_H_
