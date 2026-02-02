878238d612c63775173f9574f9475d3b19e03caf
Jeffrey Yu <yuje@chromium.org>
2026-02-02 11:28:23 -0800

    Replace `std::unordered_set` with `base::flat_set`.
    Bug: 473916362
    Change-Id: I8e557e040fc9d2ac41a291683cb39abc7229ecd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7531122
    Reviewed-by: Long Cheng <lgcheng@google.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Auto-Submit: Jeffrey Yu <yuje@chromium.org>
    Commit-Queue: Long Cheng <lgcheng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1578285}

--- chrome/browser/ash/app_list/arc/arc_package_syncable_service.cc
+++ chrome/browser/ash/app_list/arc/arc_package_syncable_service.cc
@@ -5,11 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/app_list/arc/arc_package_syncable_service.h"
 
-#include <unordered_set>
 #include <utility>
 #include <vector>
 
 #include "ash/constants/ash_pref_names.h"
+#include "base/containers/flat_set.h"
 #include "chrome/browser/ash/app_list/arc/arc_app_list_prefs.h"
 #include "chrome/browser/ash/app_list/arc/arc_package_install_priority_handler.h"
 #include "chrome/browser/ash/app_list/arc/arc_package_syncable_service_factory.h"
@@ -160,10 +160,8 @@ ArcPackageSyncableService::MergeDataAndStartSyncing(
   metrics_helper_.SetTimeSyncStarted();
   uint64_t num_expected_apps = 0;
 
-  const std::vector<std::string> local_packages =
-      prefs_->GetPackagesFromPrefs();
-  const std::unordered_set<std::string> local_package_set(
-      local_packages.begin(), local_packages.end());
+  const base::flat_set<std::string> local_package_set(
+      prefs_->GetPackagesFromPrefs());
 
   // Creates sync items from synced data.
   for (const syncer::SyncData& sync_data : initial_sync_data) {
@@ -194,7 +192,7 @@ ArcPackageSyncableService::MergeDataAndStartSyncing(
 
   // Creates sync items for local unsynced packages.
   syncer::SyncChangeList change_list;
-  for (const auto& local_package_name : local_packages) {
+  for (const auto& local_package_name : local_package_set) {
     if (sync_items_.contains(local_package_name)) {
       continue;
     }
