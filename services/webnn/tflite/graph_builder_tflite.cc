559a990584f4eb54478abd29b029096335c01a4d
Nidhi Jaju <nidhijaju@chromium.org>
2026-02-01 18:05:51 -0800

    Revert "webnn: Fix tensor size limit test on 32-bit platforms"
    This reverts commit cc2dbce9e00433ff65daf1e5ed1b4c4d695763bd.
    
    Reason for revert: Causing consistent failures on the WebKit Win10 builder
    i.e. https://ci.chromium.org/ui/p/chromium/builders/ci/WebKit%20Win10/170161/overview
    
    Original change's description:
    > webnn: Fix tensor size limit test on 32-bit platforms
    >
    > The number of elements in the test tensor created for this test cannot
    > be represented by a size_t on 32-bit platforms and so the test triggers
    > an unexpected Mojo error.
    >
    > A lower maximum tensor size limit needs to be chosen on 32-bit systems
    > because making a 2 GiB allocation is not possible.
    >
    > Fixed: 390358145
    > Change-Id: Ibeffb6d04236c1f16947c9faa50ca33e09291587
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7534076
    > Commit-Queue: Reilly Grant <reillyg@chromium.org>
    > Reviewed-by: Phillis Tang <phillis@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1577586}
    
    Bug: 390358145
    Change-Id: I8fb0d0ea55b2157203d890161228f463869089c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7532200
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1577888}

--- services/webnn/tflite/graph_builder_tflite.cc
+++ services/webnn/tflite/graph_builder_tflite.cc
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/types/expected.h"
 #include "base/types/expected_macros.h"
-#include "build/build_config.h"
 #include "services/webnn/public/cpp/context_properties.h"
 #include "services/webnn/public/cpp/data_type_limits.h"
 #include "services/webnn/public/cpp/graph_validation_utils.h"
@@ -609,14 +608,9 @@ ContextProperties GraphBuilderTflite::GetContextProperties() {
       OperandDataType::kInt8,    OperandDataType::kUint8,
       OperandDataType::kInt4};
 
-#if defined(ARCH_CPU_64_BITS)
   // Limit to INT_MAX for security reasons (similar to PartitionAlloc).
   static constexpr uint64_t kTensorByteLengthLimit =
       std::numeric_limits<int32_t>::max();
-#else
-  // Allocating 2GiB isn't practical on a 32-bit system. Use a 1GiB limit.
-  static constexpr uint64_t kTensorByteLengthLimit = 1024 * 1024 * 1024;
-#endif
 
   return ContextProperties(
       InputOperandLayout::kNhwc, Resample2DAxes::kChannelsLast,
