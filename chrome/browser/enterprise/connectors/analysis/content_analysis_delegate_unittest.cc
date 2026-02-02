00a6fbd01838ab3613f170cb59f74a3665f481d8
Alex Chen <alxchn@google.com>
2026-02-02 10:34:18 -0800

    Add new UMA metrics related to enterprise content analysis
    Bug: 477238232
    
    Change-Id: I60ea5aac3533c50b870e709e7549890433b2622a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7413002
    Commit-Queue: Alex Chen <alxchn@google.com>
    Reviewed-by: Sébastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1578238}

--- chrome/browser/enterprise/connectors/analysis/content_analysis_delegate_unittest.cc
+++ chrome/browser/enterprise/connectors/analysis/content_analysis_delegate_unittest.cc
@@ -668,7 +668,9 @@ TEST_F(ContentAnalysisDelegateAuditOnlyTest, Empty) {
   EXPECT_TRUE(called);
 }
 
-TEST_F(ContentAnalysisDelegateAuditOnlyTest, StringData) {
+TEST_F(ContentAnalysisDelegateAuditOnlyTest, StringDataAndReportSuccess) {
+  base::HistogramTester histogram_tester_;
+
   GURL url(kTestUrl);
   ContentAnalysisDelegate::Data data;
   ASSERT_TRUE(ContentAnalysisDelegate::IsEnabled(profile(), url, &data,
@@ -692,6 +694,11 @@ TEST_F(ContentAnalysisDelegateAuditOnlyTest, StringData) {
   RunUntilDone();
   EXPECT_EQ(1,
             test::FakeContentAnalysisDelegate::GetTotalAnalysisRequestsCount());
+
+  // FakeContentAnalysisDelegate is always constructed with UPLOAD; just
+  // verify a success histogram is recorded.
+  histogram_tester_.ExpectTotalCount(
+      "Enterprise.ContentAnalysis.Upload.Success.Duration", 1);
   EXPECT_TRUE(called);
 }
 
