a0290d09f6a5ff4719c140c4b2b3d5443fddc4a8
Wesley Wang <wesleygwang@google.com>
2026-02-02 15:07:12 -0800

    [Clank Autocorrect] Add remove autocorrect underline
    This CL adds the logic to AutocorrectManager to handle the removal
    of the underline UI for autocorrected text. This ensures that the
    visual feedback is appropriately cleared once the correction is
    accepted, modified or the user moves focus away from the
    corrected word.
    
    Bug: 475659097
    Change-Id: I8f40b29b6f889cb48384c2e9aea23bdc90236a8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7488469
    Reviewed-by: Michael Cui <mlcui@google.com>
    Commit-Queue: Wesley Wang <wesleygwang@google.com>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1578417}

--- content/public/android/java/src/org/chromium/content/browser/input/ImeAdapterImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/input/ImeAdapterImpl.java
@@ -1221,6 +1221,10 @@ public class ImeAdapterImpl
                                 lastKeyDownEvent.getScanCode(),
                                 false,
                                 lastKeyDownEvent.getUnicodeChar());
+
+                if (mAutocorrectManager != null) {
+                    mAutocorrectManager.onCommitText();
+                }
                 return true;
             }
         }
@@ -1250,6 +1254,7 @@ public class ImeAdapterImpl
             // must be committed before the span can be applied to it.
             if (mAutocorrectManager != null) {
                 mAutocorrectManager.maybeAppendAutocorrectUnderlineSpan();
+                mAutocorrectManager.onCommitText();
             }
 
         } else {
@@ -1937,6 +1942,14 @@ public class ImeAdapterImpl
                 .appendAutocorrectUnderlineSpan(mNativeImeAdapterAndroid, start, end);
     }
 
+    @Override
+    public void clearAllAutocorrectUnderlineSpans() {
+        if (!isValid()) return;
+        if (mAutocorrectManager == null) return;
+        if (DEBUG_LOGS) Log.i(TAG, "clearAllAutocorrectUnderlineSpans");
+        ImeAdapterImplJni.get().clearAllAutocorrectUnderlineSpans(mNativeImeAdapterAndroid);
+    }
+
     @NativeMethods
     interface Natives {
         long init(ImeAdapterImpl caller, WebContents webContents);
@@ -2020,5 +2033,7 @@ public class ImeAdapterImpl
         void performSpellCheck(long nativeImeAdapterAndroid);
 
         void appendAutocorrectUnderlineSpan(long nativeImeAdapterAndroid, int start, int end);
+
+        void clearAllAutocorrectUnderlineSpans(long nativeImeAdapterAndroid);
     }
 }
