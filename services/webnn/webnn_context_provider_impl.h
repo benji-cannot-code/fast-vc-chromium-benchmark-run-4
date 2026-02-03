2ff4f2945324107964578ecdc95ffbf7ce2292fb
junwei <junwei.fu@intel.com>
2026-02-03 15:57:30 -0800

    webnn: Pass incognito status to async context creation tasks
    `provider_receivers_.current_context()` must only be called within the
    stack frame of an actual interface method invocation or disconnect
    notification scheduled by a receiver. It is illegal to attempt to call
    this at any other time, such as from within an asynchronous task or
    callback posted from a message handler [1].
    
    [1]
    https://source.chromium.org/chromium/chromium/src/+/main:mojo/public/cpp/bindings/receiver_set.h;drc=5200a7ce2a93183c95b0ca9de0b34533fc90fbaf;l=426
    
    Bug: 480128592, 477562219
    Change-Id: I2a34b779ea583f673bef2ccbcc5ae4a1f6a7a046
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7540735
    Reviewed-by: Hu, Ningxin <ningxin.hu@intel.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Fu, Junwei <junwei.fu@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1579116}

--- services/webnn/webnn_context_provider_impl.h
+++ services/webnn/webnn_context_provider_impl.h
@@ -166,7 +166,8 @@ class COMPONENT_EXPORT(WEBNN_SERVICE) WebNNContextProviderImpl
       scoped_refptr<base::SingleThreadTaskRunner> task_runner,
       mojo::PendingReceiver<mojom::WebNNContext> receiver,
       mojo::PendingRemote<mojom::WebNNContext> remote,
-      CreateWebNNContextCallback callback);
+      CreateWebNNContextCallback callback,
+      bool is_incognito);
 #endif  // BUILDFLAG(WEBNN_USE_TFLITE)
 
 #if BUILDFLAG(IS_WIN)
@@ -182,6 +183,7 @@ class COMPONENT_EXPORT(WEBNN_SERVICE) WebNNContextProviderImpl
                        mojo::PendingReceiver<mojom::WebNNContext> receiver,
                        mojo::PendingRemote<mojom::WebNNContext> remote,
                        CreateWebNNContextCallback callback,
+                       bool is_incognito,
                        base::expected<scoped_refptr<ort::Environment>,
                                       std::string> env_creation_results);
 
@@ -198,6 +200,7 @@ class COMPONENT_EXPORT(WEBNN_SERVICE) WebNNContextProviderImpl
       mojo::PendingReceiver<mojom::WebNNContext> receiver,
       mojo::PendingRemote<mojom::WebNNContext> remote,
       CreateWebNNContextCallback callback,
+      bool is_incognito,
       base::flat_map<std::string, mojom::EpPackageInfoPtr> ep_package_info);
 #endif  // BUILDFLAG(IS_WIN)
 
