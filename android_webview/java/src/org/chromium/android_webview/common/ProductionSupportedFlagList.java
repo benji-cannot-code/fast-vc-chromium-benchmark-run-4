3f28c9752e174072f16dfa541f0c3acb780327e0
François Doray <fdoray@chromium.org>
2026-02-03 13:48:31 -0800

    Fix comment position at the end of ProductionSupportedFlagList.java.
    Features have been added below the comment that says to add new features
    above it. Move the comment so that features truly are above it.
    
    Change-Id: Ic98f6666f440f1ee3f70c428966b81d747d26b51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7541999
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Auto-Submit: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1579041}

--- android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
+++ android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
@@ -1228,9 +1228,6 @@ public final class ProductionSupportedFlagList {
                 BaseFeatures.REBIND_SERVICE_BATCH_API,
                 "Use a batch API to rebind service connections."),
 
-        // Add new commandline switches and features above. The final entry should have a
-        // trailing comma for cleaner diffs.
-
         // Features for PerfCombined2025_WebView study
         Flag.baseFeature("AsyncSetCookie"),
         Flag.baseFeature("ReducePPMs"),
@@ -1271,5 +1268,8 @@ public final class ProductionSupportedFlagList {
                 ContentFeatures.NO_SELECTION_MENU_CACHING,
                 "When this flag is enabled, the menu which is shown when selecting text will not be"
                         + " cached. Instead, it is recomputed each time it is shown."),
+
+        // Add new commandline switches and features above. The final entry should have a
+        // trailing comma for cleaner diffs.
     };
 }
