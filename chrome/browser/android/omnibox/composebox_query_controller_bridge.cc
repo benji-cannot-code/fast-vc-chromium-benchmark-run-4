18974e01c96466392e24e1f3b2b1283a60cd8f22
Sky Malice <skym@chromium.org>
2026-02-03 10:30:54 -0800

    [AIM] Fix adding cached tabs.
    In https://crrev.com/c/7507603 we switched from calling directly into
    the controller, to routing all attachment uploads to go through the
    session handle. This session handle tracks all outstanding tokens and
    drops any request that it doesn't have a matching token for. This
    required all the token creation to be done through the session handle as
    well, so that it would track it. However in that CL, the token creation
    for cached tabs was missed.
    
    This CL fixes it by updating that token call location to go through the
    session handle, and now when the tab is uploaded, it no longer gets
    dropped.
    
    Bug: 455952553
    Change-Id: I14dd576a9f49dad9b3b7c73be8999d8ffcae0be6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7541036
    Auto-Submit: Sky Malice <skym@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Hailey Wang <haileywang@google.com>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1578912}

--- chrome/browser/android/omnibox/composebox_query_controller_bridge.cc
+++ chrome/browser/android/omnibox/composebox_query_controller_bridge.cc
@@ -227,7 +227,7 @@ ComposeboxQueryControllerBridge::AddTabContextFromCache(JNIEnv* env,
     return {};
   }
 
-  base::UnguessableToken file_token = base::UnguessableToken::Create();
+  base::UnguessableToken file_token = session_handle_->CreateContextToken();
 
   cache->GetPageContentForTab(
       tab_id, base::BindOnce(
