518a6b63ecfee0ce44eef3dc8a578653f308f761
Jean-Philippe Gravel <jpgravel@chromium.org>
2026-02-02 16:04:46 -0800

    Use std::tuple instead of testing::tuple
    Googletest has an alias for std::tuple, but this for backward
    compatiblity only [1]. We should use the standard types.
    
    [1] http://google3/third_party/googletest/googletest/include/gtest/internal/gtest-port.h?rcl=854187933&l=923,919-920
    
    Change-Id: I17719b5d0b6b773949836c55f1aa7f18a6ae4351
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7507453
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1578442}

--- sql/database_unittest.cc
+++ sql/database_unittest.cc
@@ -2136,7 +2136,7 @@ TEST_P(SQLDatabaseTest, ReOpenWithDifferentJournalMode) {
 
 class SQLDatabaseTestExclusiveFileLockMode
     : public testing::Test,
-      public testing::WithParamInterface<::testing::tuple<bool, bool>> {
+      public testing::WithParamInterface<std::tuple<bool, bool>> {
  public:
   ~SQLDatabaseTestExclusiveFileLockMode() override = default;
 
