3b34d8bb330447b749830c374f8493351abde272
Alex Ilin <alexilin@chromium.org>
2026-02-03 00:46:02 -0800

    [DBSC] Strip query and fragment from DBSC audience field
    Ensures the "aud" claim in session binding JWT payloads is stripped of
    query parameters and fragments.
    
    This CL also adds a field trial configuration for a planned Device Bound
    Session Google experiment.
    
    Fixed: 480871259
    Change-Id: I41ffad73c552398963fb9432fd35d5698a8c5a69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7531153
    Reviewed-by: thefrog <thefrog@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1578617}

--- components/signin/public/base/session_binding_utils.cc
+++ components/signin/public/base/session_binding_utils.cc
@@ -107,6 +107,13 @@ std::optional<std::string> CreateHeaderAndPayloadWithCustomPayload(
                        Base64UrlEncode(*payload_serialized)});
 }
 
+GURL RemoveQueryAndFragment(const GURL& original) {
+  GURL::Replacements replacements;
+  replacements.ClearRef();
+  replacements.ClearQuery();
+  return original.ReplaceComponents(replacements);
+}
+
 }  // namespace
 
 std::optional<crypto::SignatureVerifier::SignatureAlgorithm>
@@ -147,7 +154,7 @@ std::optional<std::string> CreateKeyRegistrationHeaderAndPayloadForTokenBinding(
   auto payload =
       base::DictValue()
           .Set("sub", client_id)
-          .Set("aud", registration_url.spec())
+          .Set("aud", RemoveQueryAndFragment(registration_url).spec())
           .Set("jti", Base64UrlEncode(crypto::SHA256HashString(auth_code)))
           // Write out int64_t variable as a double.
           // Note: this may discard some precision, but for `base::Value`
@@ -168,7 +175,7 @@ CreateKeyRegistrationHeaderAndPayloadForSessionBinding(
     base::Time timestamp) {
   auto payload =
       base::DictValue()
-          .Set("aud", registration_url.spec())
+          .Set("aud", RemoveQueryAndFragment(registration_url).spec())
           .Set("jti", challenge)
           // Write out int64_t variable as a double.
           // Note: this may discard some precision, but for `base::Value`
@@ -190,7 +197,7 @@ std::optional<std::string> CreateKeyAssertionHeaderAndPayload(
     std::string_view ephemeral_public_key) {
   auto payload = base::DictValue()
                      .Set("sub", client_id)
-                     .Set("aud", destination_url.spec())
+                     .Set("aud", RemoveQueryAndFragment(destination_url).spec())
                      .Set("jti", challenge)
                      .Set("iss", Base64UrlEncode(crypto::SHA256Hash(pubkey)))
                      .Set("namespace", name_space);
