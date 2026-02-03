63dd5e9777a8e14032caaa4d6127232f1cb04e7a
Glenn Hartmann <hartmanng@chromium.org>
2026-02-03 09:42:37 -0800

    Make global_browser_collection_browsertest into an interactive_ui_test on non-Android.
    browsertests allow window activation between shards to affect the test
    results (in particular after
    https://chromium-review.googlesource.com/c/chromium/src/+/7515956).
    
    Bug: 477251911
    Bypass-Check-License: Just a rename
    Change-Id: Ica869dc8445045c5f0f468672ba411ca6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7537442
    Commit-Queue: Glenn Hartmann <hartmanng@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1578869}

--- chrome/browser/ui/browser_window/internal/global_browser_collection_interactive_ui_test.cc
+++ chrome/browser/ui/browser_window/internal/global_browser_collection_interactive_ui_test.cc
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/browser_window/public/global_browser_collection.h"
-
 #include "base/scoped_observation.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/profiles/profile_test_util.h"
@@ -12,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window/public/browser_collection_observer.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/browser_window/public/create_browser_window.h"
+#include "chrome/browser/ui/browser_window/public/global_browser_collection.h"
 #include "chrome/test/base/chrome_test_utils.h"
 #include "chrome/test/base/platform_browser_test.h"
 #include "chrome/test/base/testing_browser_process.h"
