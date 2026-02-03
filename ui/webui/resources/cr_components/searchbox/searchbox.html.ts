9c7ff398aaa8169f2d9258937792bcc5ab3ac049
Jilin Yang <jilinyang@google.com>
2026-02-03 11:38:36 -0800

    [NTP-Real-Box]: Use inert to prevent interaction behind error scrim
    https://screencast.googleplex.com/cast/NTY3MzgxNDQ0MzY4NzkzNnxmYzEwNTFhNy03NA
    
    Fixed: 479564912
    Change-Id: I55634d7a91dd090a96331c484aed2044eaa6d05c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7532923
    Reviewed-by: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1578951}

--- ui/webui/resources/cr_components/searchbox/searchbox.html.ts
+++ ui/webui/resources/cr_components/searchbox/searchbox.html.ts
@@ -139,7 +139,7 @@ export function getHtml(this: SearchboxElement) {
       <search-animated-glow animation-state="${this.animationState}" part="animated-glow">
       </search-animated-glow>
       ${compactLayout ? html`
-        <div id="inputInnerContainer">
+        <div id="inputInnerContainer" ?inert="${this.errorMessage_}">
           <div class="contextualEntrypointContainer contextualEntrypointContainerCompact">
             ${contextualEntrypoint}
           </div>
@@ -148,7 +148,7 @@ export function getHtml(this: SearchboxElement) {
           ${lensSearchButton}
           ${composeButton}
         </div>
-        <div class="dropdownContainer">
+        <div class="dropdownContainer" ?inert="${this.errorMessage_}">
           ${dropdown}
           ${this.recentTabForChip_ && this.dropdownIsVisible && this.isInputEmpty() ? html`
           <div id="recentTabChipContainer">
@@ -160,11 +160,11 @@ export function getHtml(this: SearchboxElement) {
           ` : nothing}
         </div>
       ` : html`
-        <div id="inputInnerContainer">
+        <div id="inputInnerContainer" ?inert="${this.errorMessage_}">
           ${inputContent}
           ${composeButton}
         </div>
-        <div id="inputInnerBottomContainer">
+        <div id="inputInnerBottomContainer" ?inert="${this.errorMessage_}">
           <div class="contextualEntrypointContainer">
             ${contextualEntrypoint}
           </div>
