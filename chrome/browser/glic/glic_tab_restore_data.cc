9f2c66b69c9f3a8fdfc8feab5583c0f3a442552a
Bryant Chandler <bryantchandler@google.com>
2026-02-03 12:22:34 -0800

    [glic] Implement glic state serialization/deserialization
    This CL adds helpers for serialization/deserialization of glic
    state in order to support tab restore.
    
    It introduces:
    - `GlicTabRestoreData`: A UserData class to hold restored state on
      WebContents.
    - `GlicTabRestoreHelper`: Utilities to serialize/deserialize Glic state
      to the session restore `extra_data` map.
    
    Bug: b:456482198
    Change-Id: Ic43a0b6a780fdded0f5d5d8d875f9e6b6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7404494
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1578986}

---
+++ chrome/browser/glic/glic_tab_restore_data.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/glic/glic_tab_restore_data.h"
+
+namespace glic {
+
+GlicRestoredState::GlicRestoredState() = default;
+GlicRestoredState::~GlicRestoredState() = default;
+GlicRestoredState::GlicRestoredState(GlicRestoredState&&) = default;
+GlicRestoredState& GlicRestoredState::operator=(GlicRestoredState&&) = default;
+
+GlicTabRestoreData::~GlicTabRestoreData() = default;
+
+GlicTabRestoreData::GlicTabRestoreData(content::WebContents* contents,
+                                       GlicRestoredState state)
+    : content::WebContentsUserData<GlicTabRestoreData>(*contents),
+      state_(std::move(state)) {}
+
+WEB_CONTENTS_USER_DATA_KEY_IMPL(GlicTabRestoreData);
+
+}  // namespace glic
