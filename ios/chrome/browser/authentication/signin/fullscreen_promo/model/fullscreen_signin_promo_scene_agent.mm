bbe2192b75d697a695e5729f629263973e441861
Cheick Cisse <cheickcisse@google.com>
2026-02-03 07:18:24 -0800

    [iOS] Update Fullscreen Sign-in Promo to use Single Display registration
    This change updates FullscreenSigninPromoSceneAgent to register the
    sign-in promo for a single display instead of a continuous display.
    
    The promo was already behaving this way in practice; however, this
    update ensures that the Promos Manager handles deregistration directly
    after the promo is displayed, rather than relying on manual lifecycle
    management.
    
    Bug: 480127103
    Change-Id: I5fc469e9e049ef34e7ea722df6e6a6a347983159
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7535477
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Auto-Submit: Cheick Cisse <cheickcisse@google.com>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1578775}

--- ios/chrome/browser/authentication/signin/fullscreen_promo/model/fullscreen_signin_promo_scene_agent.mm
+++ ios/chrome/browser/authentication/signin/fullscreen_promo/model/fullscreen_signin_promo_scene_agent.mm
@@ -123,7 +123,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           self.sceneState.browserProviderInterface.mainBrowserProvider.browser
               ->GetProfile(),
           version_info::GetVersion())) {
-    _promosManager->RegisterPromoForContinuousDisplay(
+    _promosManager->RegisterPromoForSingleDisplay(
         promos_manager::Promo::FullscreenSignin);
     return;
   }
