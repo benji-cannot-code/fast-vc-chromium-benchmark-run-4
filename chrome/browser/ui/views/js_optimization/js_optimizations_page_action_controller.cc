cc47ad4c951827bfbfa7613ca6e2141f121429cc
Tiffany Song <tiffanysong@google.com>
2026-02-03 11:07:40 -0800

    Revert "Fix dangling pointer and complete v8 optimization browser tests"
    This reverts commit 45cb559e18b3d32602b80818892653ed35319b52.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/4983590153617408
    
    Sample build with failed test: https://ci.chromium.org/b/8691321286154585873
    Affected test(s):
    [://chrome/test\:browser_tests!gtest::JavascriptOptimizerBubbleBrowserTest_EnterprisePolicy#BubbleShowsOnClick](https://ci.chromium.org/ui/test/chromium/:%2F%2Fchrome%2Ftest%5C:browser_tests%21gtest::JavascriptOptimizerBubbleBrowserTest_EnterprisePolicy%23BubbleShowsOnClick?q=VHash%3A5a9885bd2bc5b6c9)
    [://chrome/test\:browser_tests!gtest::JavascriptOptimizerBubbleBrowserTest_NotFromEnterprisePolicy#BubbleShowsOnClick](https://ci.chromium.org/ui/test/chromium/:%2F%2Fchrome%2Ftest%5C:browser_tests%21gtest::JavascriptOptimizerBubbleBrowserTest_NotFromEnterprisePolicy%23BubbleShowsOnClick?q=VHash%3A5a9885bd2bc5b6c9)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F4983590153617408&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F7521273&type=BUG
    
    Original change's description:
    > Fix dangling pointer and complete v8 optimization browser tests
    >
    > This change implements the TODO browser tests for the bubble dialog.
    >
    > Previously, the method `OnBubbleHidden` attempted to set `bubble_` to
    > nullptr. However, this method is called after the dialog has been
    > destroyed and thus `bubble_` was a dangling pointer. By moving the
    > nullptr assignment to `OnWidgetClosing`, we can avoid this error.
    >
    > Bug: 462425975
    > Change-Id: Iaacb8113dab054920641b9afd70fb75e09b2742c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7521273
    > Reviewed-by: Javier Castro <jacastro@chromium.org>
    > Commit-Queue: Tiffany Song <tiffanysong@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1576595}
    >
    
    Bug: 462425975
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ibbdf5816f1ad79c3881504527fd1fe8f05288642
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7533027
    Commit-Queue: Tiffany Song <tiffanysong@google.com>
    Reviewed-by: Javier Castro <jacastro@chromium.org>
    Reviewed-by: Tiffany Song <tiffanysong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1578936}

--- chrome/browser/ui/views/js_optimization/js_optimizations_page_action_controller.cc
+++ chrome/browser/ui/views/js_optimization/js_optimizations_page_action_controller.cc
@@ -61,13 +61,8 @@ void JsOptimizationsPageActionController::ShowBubble(
 
 void JsOptimizationsPageActionController::OnBubbleHidden(
     actions::ActionItem* action_item) {
-  action_item->SetIsShowingBubble(false);
-}
-
-void JsOptimizationsPageActionController::OnWidgetDestroying(
-    views::Widget* widget) {
-  widget_observation_.Reset();
   bubble_ = nullptr;
+  action_item->SetIsShowingBubble(false);
 }
 
 views::BubbleDialogModelHost* JsOptimizationsPageActionController::CreateBubble(
@@ -113,7 +108,6 @@ views::BubbleDialogModelHost* JsOptimizationsPageActionController::CreateBubble(
   // TODO(crbug.com/464011395): Refactor to use CLIENT_OWNS_WIDGET.
   views::Widget* const widget =
       views::BubbleDialogDelegate::CreateBubble(std::move(bubble_unique));
-  widget_observation_.Observe(widget);
   widget->Show();
   return bubble;
 }
