554702e10e4da93511b22d3fc438c7ff9f8fac69
Henrique Nakashima <hnakashima@chromium.org>
2026-02-03 12:36:07 -0800

    [Android] Cleanup redundant displayingAtLeast(51) calls
    Now that the default MIN_DISPLAYED_PERCENT is 51, explicitly passing
    51 is redundant. Removed these callsites across the codebase.
    
    Bug: 417509710
    Change-Id: I71e89469100f0083224e52699c331232944b7662
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7539477
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Owners-Override: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1578995}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/transit/hub/TabGroupDialogFacility.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/transit/hub/TabGroupDialogFacility.java
@@ -97,6 +97,8 @@ public class TabGroupDialogFacility<
                 declareView(toolbarElement.descendant(withId(R.id.toolbar_new_tab_button)));
         backButtonElement =
                 declareView(toolbarElement.descendant(withId(R.id.toolbar_back_button)));
+        listMenuButtonElement =
+                declareView(toolbarElement.descendant(withId(R.id.toolbar_menu_button)));
     }
 
     @Override
@@ -115,15 +117,6 @@ public class TabGroupDialogFacility<
                                             toolbarElement.descendant(withId(R.id.share_button)));
                         }
                     });
-
-            // Data sharing layout causes the menu button to be hidden due to the rounded corner.
-            listMenuButtonElement =
-                    declareView(
-                            toolbarElement.descendant(withId(R.id.toolbar_menu_button)),
-                            ViewElement.displayingAtLeastOption(51));
-        } else {
-            listMenuButtonElement =
-                    declareView(toolbarElement.descendant(withId(R.id.toolbar_menu_button)));
         }
     }
 
