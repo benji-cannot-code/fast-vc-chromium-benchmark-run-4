3cbe951cbe2c4c54d88d5193f2875a2b94c4cd2f
mikt <mikt@google.com>
2026-02-02 19:02:15 -0800

    [PA] Refactor and merge PA/AC shim logic
    This patch merges the PartitionAlloc with Advanced Checks shim
    delegation logic directly into the main PartitionAlloc default dispatch
    file.
    
    Changes:
    - Merged allocator_shim_default_dispatch_to_partition_alloc_with_advanced_checks.{cc,h}
      into allocator_shim_default_dispatch_to_partition_alloc.{cc,h}.
    - Renamed InstallCustomDispatchForPartitionAllocWithAdvancedChecks to
      InstallPartitionAllocWithAdvancedChecks.
    - Centralized default_dispatch to always be the delegating one when
      USE_PARTITION_ALLOC_AS_MALLOC is true.
    - Simplified namespaces and flattened buildflag checks in partition_alloc_support.cc.
    - Removes the GN arg from partition_alloc.gni.
    - Removes the ENABLE_ALLOCATOR_SHIM_PARTITION_ALLOC_DISPATCH_WITH_ADVANCED_CHECKS_SUPPORT buildflag.
    - Simplifies the build configuration in BUILD.gn.
    - Replaces buildflag usages with USE_PARTITION_ALLOC_AS_MALLOC.
    - Simplifies default dispatch definition in allocator_shim_default_dispatch_to_partition_alloc.cc.
    
    BYPASS_RECITATION_REASON=False positive due to moving code?
    
    Change-Id: I9ffcd70c1d136faead40c458d205261788b8053c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7519087
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Commit-Queue: Mikihito Matsuura <mikt@google.com>
    Cr-Commit-Position: refs/heads/main@{#1578524}

--- base/allocator/partition_alloc_support.cc
+++ base/allocator/partition_alloc_support.cc
@@ -83,11 +83,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if PA_BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
 #include "partition_alloc/memory_reclaimer.h"
-#endif
-
-#if PA_BUILDFLAG( \
-    ENABLE_ALLOCATOR_SHIM_PARTITION_ALLOC_DISPATCH_WITH_ADVANCED_CHECKS_SUPPORT)
-#include "partition_alloc/shim/allocator_shim_default_dispatch_to_partition_alloc_with_advanced_checks.h"
+#include "partition_alloc/shim/allocator_shim_default_dispatch_to_partition_alloc.h"
 #endif
 
 #if BUILDFLAG(IS_ANDROID) && PA_BUILDFLAG(HAS_MEMORY_TAGGING)
@@ -1315,15 +1311,11 @@ void PartitionAllocSupport::ReconfigureAfterFeatureListInit(
   ReconfigureSchedulerLoopQuarantineBranch(
       SchedulerLoopQuarantineBranchType::kMain);
 
-#if PA_BUILDFLAG( \
-    ENABLE_ALLOCATOR_SHIM_PARTITION_ALLOC_DISPATCH_WITH_ADVANCED_CHECKS_SUPPORT)
   bool enable_pa_with_advanced_checks =
       ShouldEnablePartitionAllocWithAdvancedChecks(process_type);
   if (enable_pa_with_advanced_checks) {
-    allocator_shim::InstallCustomDispatchForPartitionAllocWithAdvancedChecks();
+    allocator_shim::InstallPartitionAllocWithAdvancedChecks();
   }
-#endif  // PA_BUILDFLAG(
-        // ENABLE_ALLOCATOR_SHIM_PARTITION_ALLOC_DISPATCH_WITH_ADVANCED_CHECKS_SUPPORT)
 #endif  // PA_BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
 
 #if BUILDFLAG(IS_WIN)
