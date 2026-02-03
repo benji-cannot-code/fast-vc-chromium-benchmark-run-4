e86bdf405626662552028783ebf975e7f18096d0
Oleksandr Tara <otara@google.com>
2026-02-03 08:36:59 -0800

    Switch ActorLoginFederatedCredentialsFetcher to use its own feature flag.
    The federated credentials fetcher now checks `kActorLoginFederatedLoginSupport` instead of `kFedCmEmbedderInitiatedLogin` to enable/disable its functionality.
    
    Change-Id: I35cff044240ea092dff226b942a077e96f87f67a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7539442
    Reviewed-by: Ioana Treib <ioanap@chromium.org>
    Commit-Queue: Ioana Treib <ioanap@chromium.org>
    Auto-Submit: Oleksandr Tara <otara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1578824}

--- chrome/browser/password_manager/actor_login/internal/actor_login_federated_credentials_fetcher.cc
+++ chrome/browser/password_manager/actor_login/internal/actor_login_federated_credentials_fetcher.cc
@@ -8,10 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/task/sequenced_task_runner.h"
+#include "components/password_manager/core/browser/features/password_features.h"
 #include "components/url_formatter/elide_url.h"
 #include "content/public/browser/webid/identity_credential_source.h"
 #include "content/public/browser/webid/identity_request_dialog_controller.h"
-#include "content/public/common/content_features.h"
 #include "url/gurl.h"
 
 namespace actor_login {
@@ -40,7 +40,8 @@ void ActorLoginFederatedCredentialsFetcher::Fetch(
     FetchResultCallback callback) {
   callback_ = std::move(callback);
 
-  if (!base::FeatureList::IsEnabled(features::kFedCmEmbedderInitiatedLogin)) {
+  if (!base::FeatureList::IsEnabled(
+          password_manager::features::kActorLoginFederatedLoginSupport)) {
     base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
         FROM_HERE,
         base::BindOnce(std::move(callback_), std::vector<Credential>(),
