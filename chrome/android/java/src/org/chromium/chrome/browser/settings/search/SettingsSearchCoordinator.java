d5e90db09fead8fefe1b33716d6b2bb6311829a3
Jinsuk Kim <jinsukkim@chromium.org>
2026-02-03 08:11:34 -0800

    [Settings Search] Margin at wide padding case
    When determining the margin for the search UI, it should be using the
    wider one if the calculated margin is equal to the threshold.
    
    before: https://screenshot.googleplex.com/7QM9zrw9zm3hmFy (search
            bar is wider than the pref items)
    after: https://screenshot.googleplex.com/7BkXf9YuQu5cWcD
    
    Bug: 469012224
    Change-Id: Iedc0197de479bac1af334e653cba12b908216bf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7538928
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Moe Adel <adelm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1578812}

--- chrome/android/java/src/org/chromium/chrome/browser/settings/search/SettingsSearchCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/search/SettingsSearchCoordinator.java
@@ -720,7 +720,7 @@ public class SettingsSearchCoordinator implements MultiColumnSettings.Observer {
                     int margin =
                             ViewResizerUtil.computePaddingForWideDisplay(
                                     mActivity, searchBox, minWidePadding);
-                    boolean isOnWideScreen = margin > minWidePadding;
+                    boolean isOnWideScreen = margin >= minWidePadding;
                     int menuWidth = menuView.getWidth();
                     int searchBoxWidth;
                     int queryWidth;
