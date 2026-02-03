e3a8cea95861b3154c7791cda00e9b3e8e7e00c7
Viktor Semeniuk <vsemeniuk@google.com>
2026-02-03 09:36:03 -0800

    Handle user intervention in the SUBMIT_FORM step.
    kUserInterventionNeededPasswordNotSumbitted is added to
    SubmissionResult. When interruption is detected before form is submitted
    PasswordChangeDelegateImpl immediately resets submission_verifier_.
    In case user decided to take over a regular PM saving/updating will be
    offered.
    
    Bug: 474035152
    Change-Id: I1b58c9b38daace67aaff7c0fb3c508bba047f65b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7532302
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1578864}

--- chrome/browser/password_manager/password_change/password_change_submission_verifier.h
+++ chrome/browser/password_manager/password_change/password_change_submission_verifier.h
@@ -58,6 +58,7 @@ class PasswordChangeSubmissionVerifier {
     kSuccess,
     kFailure,
     kUserInterventionNeeded,
+    kUserInterventionNeededPasswordNotSumbitted,
   };
 
   using FormSubmissionResultCallback =
