3ac58efcb0a3516b1775fedc3d4762a1f78671ad
Ian Vollick <vollick@chromium.org>
2026-02-03 15:19:10 -0800

    Permit updating skills on focus changes
    This CL triggers syncing of chrome skills when glic's
    focused tab changes, web client is created or web ui
    reaches the ready state. It also wires up plumbing to
    send contextual skills when they arrive.
    
    credit: chrstne for pointing out that the logic doesn't
    need to be at the profile level.
    
    Bug: b:477286670
    Change-Id: I80dd47f23e66506663601500b15e06d8f22d4ef2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7500100
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Christine Ying <chrstne@google.com>
    Reviewed-by: Marke Hallowell <wry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1579095}

--- chrome/browser/glic/host/host.cc
+++ chrome/browser/glic/host/host.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/glic/host/context/glic_sharing_manager_provider.h"
 #include "chrome/browser/glic/host/glic.mojom.h"
 #include "chrome/browser/glic/host/glic_page_handler.h"
+#include "chrome/browser/glic/host/glic_skills_manager_impl.h"
 #include "chrome/browser/glic/host/glic_web_contents_warming_pool.h"
 #include "chrome/browser/glic/host/host.h"
 #include "chrome/browser/glic/host/host_metrics.h"
@@ -375,6 +376,13 @@ GlicSharingManager& Host::sharing_manager() {
   return sharing_manager_provider_
              ? sharing_manager_provider_->sharing_manager()
              : glic_service().sharing_manager();
+}
+
+GlicSkillsManager& Host::skills_manager() {
+  if (!skills_manager_) {
+    skills_manager_ = std::make_unique<GlicSkillsManagerImpl>(this);
+  }
+  return *skills_manager_;
 }
 
 Host::InstanceDelegate& Host::instance_delegate() {
