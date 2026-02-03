fa3e342a6e8b566d4dc26e3d5a32fb578368f664
Radu Nitescu <radunitescu@google.com>
2026-02-03 05:25:15 -0800

    [iOS][AIM] Add flag for deep search
    Change-Id: I7d33e4cef1312792955fb5297315f9f6941d7bfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7535132
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1578720}

--- ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
+++ ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
@@ -194,6 +194,9 @@ extern const char kComposeboxCloseButtonTopAlignDescription[];
 extern const char kComposeboxCompactModeName[];
 extern const char kComposeboxCompactModeDescription[];
 
+extern const char kComposeboxDeepSearchName[];
+extern const char kComposeboxDeepSearchDescription[];
+
 extern const char kComposeboxDevToolsName[];
 extern const char kComposeboxDevToolsDescription[];
 
