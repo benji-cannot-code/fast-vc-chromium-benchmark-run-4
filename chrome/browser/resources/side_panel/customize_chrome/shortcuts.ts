c0098fafcaaa288caa31db1c9712ac675aedeb97
rbpotter <rbpotter@chromium.org>
2026-02-03 15:11:55 -0800

    WebUI: Fix inline event handlers, part 3
    In this change fixing all instances in the customize chrome side panel.
    This is in preparation for adding an automated check for such event
    handlers.
    
    Bug: 481017412
    Change-Id: Ia862a4f3d24104b69610721742e8cd3225e7e0c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7538649
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1579091}

--- chrome/browser/resources/side_panel/customize_chrome/shortcuts.ts
+++ chrome/browser/resources/side_panel/customize_chrome/shortcuts.ts
@@ -48,7 +48,7 @@ export class ShortcutsElement extends CrLitElement {
 
   static override get properties() {
     return {
-      shortcutsType_: {type: Object},
+      shortcutsType_: {type: Number},
       initialized_: {type: Boolean},
       radioSelection_: {type: String},
       show_: {type: Boolean},
@@ -239,7 +239,8 @@ export class ShortcutsElement extends CrLitElement {
     this.setMostVisitedSettings_();
   }
 
-  protected onOptionClick_(type: TileType) {
+  protected onOptionClick_(e: Event) {
+    const type = Number((e.currentTarget as HTMLElement).dataset['type']);
     if (this.shortcutsType_ === type) {
       return;
     }
