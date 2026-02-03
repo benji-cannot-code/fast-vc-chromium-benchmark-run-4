3e7a296b5cf68a51feba93adde3f81427f5a41f0
Arthur Milchior <arthurmilchior@google.com>
2026-02-03 07:52:40 -0800

    [iOS]Introduce UIDisabled in Bookmarks home
    This should hopefully remove all crashes related to a tap opening a child coordinator while the previous one was still present.
    
    Bug: 474142174
    Change-Id: I4d5fd5fe82d12ecdab038167141d76074c828085
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7531568
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Tanmoy Mollik <triploblastic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1578801}

--- ios/chrome/browser/bookmarks/ui_bundled/home/bookmarks_home_view_controller.mm
+++ ios/chrome/browser/bookmarks/ui_bundled/home/bookmarks_home_view_controller.mm
@@ -263,6 +263,8 @@ BookmarkNodeIDSet GetBookmarkNodeIDSet(
   BOOL _isBeingDismissed;
   // The Signin coordinator displayed, if any.
   SigninCoordinator* _signinCoordinator;
+  // Whether the UI is disabled.
+  BOOL _UIDisabled;
 }
 
 @synthesize editingFolderCell = _editingFolderCell;
@@ -877,6 +879,7 @@ BookmarkNodeIDSet GetBookmarkNodeIDSet(
   base::RecordAction(base::UserMetricsAction(userAction));
   const BookmarkNode* editedNode = *(nodes.begin());
   const BookmarkNode* selectedFolder = editedNode->parent();
+  _UIDisabled = YES;
   _folderChooserCoordinator = [[BookmarksFolderChooserCoordinator alloc]
       initWithBaseViewController:self.navigationController
                          browser:_browser.get()
@@ -930,6 +933,7 @@ BookmarkNodeIDSet GetBookmarkNodeIDSet(
                             node:bookmarkNode
          snackbarCommandsHandler:self.snackbarCommandsHandler];
   self.bookmarkEditorCoordinator.delegate = self;
+  _UIDisabled = YES;
   [self.bookmarkEditorCoordinator start];
 }
 
@@ -952,6 +956,7 @@ BookmarkNodeIDSet GetBookmarkNodeIDSet(
                       folderNode:bookmarkNode];
   self.folderEditorCoordinator.delegate = self;
   [self.folderEditorCoordinator start];
+  _UIDisabled = YES;
 }
 
 - (void)openAllURLs:(std::vector<GURL>)urls
@@ -983,6 +988,7 @@ BookmarkNodeIDSet GetBookmarkNodeIDSet(
   if (self.actionSheetCoordinator) {
     return;
   }
+  _UIDisabled = YES;
   __weak BookmarksHomeViewController* weakSelf = self;
   [self.mediator queryLocalBookmarks:^(int local_bookmarks_count,
                                        std::string user_email) {
@@ -1047,6 +1053,7 @@ BookmarkNodeIDSet GetBookmarkNodeIDSet(
       "IOS.Bookmarks.BulkSaveBookmarksInAccountCount", localBookmarksCount);
 
   [self refreshContents];
+  _UIDisabled = NO;
 
   NSString* snackbarMessage = base::SysUTF16ToNSString(
       base::i18n::MessageFormatter::FormatWithNamedArgs(
@@ -1607,6 +1614,7 @@ BookmarkNodeIDSet GetBookmarkNodeIDSet(
 }
 
 - (void)dismissActionSheetCoordinator {
+  _UIDisabled = NO;
   [self.actionSheetCoordinator stop];
   self.actionSheetCoordinator = nil;
 }
@@ -1614,12 +1622,14 @@ BookmarkNodeIDSet GetBookmarkNodeIDSet(
 // Stops the folder chooser coordinator.
 - (void)stopFolderChooserCoordinator {
   [_folderChooserCoordinator stop];
+  _UIDisabled = NO;
   _folderChooserCoordinator.delegate = nil;
   _folderChooserCoordinator = nil;
 }
 
 // Stops the bookmark editor coordinator.
 - (void)stopBookmarksEditorCoordinator {
+  _UIDisabled = NO;
   [self.bookmarkEditorCoordinator stop];
   self.bookmarkEditorCoordinator.delegate = nil;
   self.bookmarkEditorCoordinator = nil;
@@ -1627,6 +1637,7 @@ BookmarkNodeIDSet GetBookmarkNodeIDSet(
 
 // Stops the folder editor coordinator.
 - (void)stopBookmarksFolderEditorCoordinator {
+  _UIDisabled = NO;
   [self.folderEditorCoordinator stop];
   self.folderEditorCoordinator.delegate = nil;
   self.folderEditorCoordinator = nil;
@@ -2690,7 +2701,7 @@ BookmarkNodeIDSet GetBookmarkNodeIDSet(
 }
 
 - (void)handleLongPress:(UILongPressGestureRecognizer*)gestureRecognizer {
-  if (self.mediator.currentlyInEditMode ||
+  if (_UIDisabled || self.mediator.currentlyInEditMode ||
       gestureRecognizer.state != UIGestureRecognizerStateBegan) {
     return;
   }
@@ -2976,6 +2987,11 @@ BookmarkNodeIDSet GetBookmarkNodeIDSet(
 
 - (void)tableView:(UITableView*)tableView
     didSelectRowAtIndexPath:(NSIndexPath*)indexPath {
+  if (_UIDisabled) {
+    // Deselect row.
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    return;
+  }
   BookmarksHomeSectionIdentifier sectionIdentifier =
       (BookmarksHomeSectionIdentifier)([self.tableViewModel
           sectionIdentifierForSectionIndex:indexPath.section]);
