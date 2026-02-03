50210e240f1bf6757c181dc692e8300b98afa302
Jan Keitel <jkeitel@google.com>
2026-02-03 02:11:15 -0800

    Add view for local save notification
    This CL adds a lot of boilerplate and plumbing for showing a local
    save notification for Autofill AI.
    
    Bug: 477845712
    Change-Id: I07519bab1039b5147cc508c17f1ce958fa4f9b06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7537660
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1578660}

--- chrome/browser/ui/autofill/autofill_ai/autofill_ai_import_data_controller_impl.cc
+++ chrome/browser/ui/autofill/autofill_ai/autofill_ai_import_data_controller_impl.cc
@@ -115,7 +115,13 @@ void AutofillAiImportDataControllerImpl::ShowPrompt(
 }
 
 void AutofillAiImportDataControllerImpl::ShowLocalSaveNotification() {
-  NOTIMPLEMENTED();
+  if (bubble_view() || !MaySetUpBubble()) {
+    return;
+  }
+
+  was_bubble_shown_ = false;
+  state_ = LocalSaveNotificationState();
+  QueueOrShowBubble();
 }
 
 void AutofillAiImportDataControllerImpl::OnSaveButtonClicked() {
@@ -213,6 +219,11 @@ void AutofillAiImportDataControllerImpl::DoShowBubble() {
       return *browser->window()
           ->GetAutofillBubbleHandler()
           ->ShowSaveAutofillAiDataBubble(web_contents(), this);
+    }
+    if (IsLocalSaveNotification()) {
+      return *browser->window()
+                  ->GetAutofillBubbleHandler()
+                  ->ShowAutofillAiLocalSaveNotification(web_contents(), this);
     }
     NOTREACHED();
   };
---
+++ chrome/browser/ui/views/autofill/autofill_ai/autofill_ai_local_save_notification_view.cc
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/autofill/autofill_ai/autofill_ai_local_save_notification_view.h"
+
+#include "chrome/browser/ui/autofill/autofill_ai/autofill_ai_import_data_controller.h"
+#include "ui/base/metadata/metadata_impl_macros.h"
+
+namespace autofill {
+
+AutofillAiLocalSaveNotificationView::AutofillAiLocalSaveNotificationView(
+    views::BubbleAnchor anchor_view,
+    content::WebContents* web_contents,
+    AutofillAiImportDataController* controller)
+    : AutofillLocationBarBubble(anchor_view, web_contents),
+      controller_(controller->GetWeakPtr()) {
+  // TODO(crbug.com/477845712): Add dialog content.
+}
+
+AutofillAiLocalSaveNotificationView::~AutofillAiLocalSaveNotificationView() =
+    default;
+
+void AutofillAiLocalSaveNotificationView::Hide() {
+  CloseBubble();
+}
+
+void AutofillAiLocalSaveNotificationView::WindowClosing() {
+  if (controller_) {
+    controller_->OnBubbleClosed(
+        AutofillClient::AutofillAiBubbleResult::kUnknown);
+  }
+  controller_ = nullptr;
+}
+
+BEGIN_METADATA(AutofillAiLocalSaveNotificationView)
+END_METADATA
+
+}  // namespace autofill
