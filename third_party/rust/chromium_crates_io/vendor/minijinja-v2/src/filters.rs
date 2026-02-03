b37b7a3b48a499d25260132057565c234a597b17
Liza Burakova <liza@chromium.org>
2026-02-03 09:29:19 -0800

    Roll minijinja: 2.14.0 => 2.15.1 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * minijinja: 2.14.0 => 2.15.1; https://docs.rs/crate/minijinja/2.15.1
    
    Bug: None
    Change-Id: I79beddc767b61ced76b9fabd7eeeaaa3cf5f42b3
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7532685
    Auto-Submit: Liza Burakova <liza@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Liza Burakova <liza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1578856}

--- third_party/rust/chromium_crates_io/vendor/minijinja-v2/src/filters.rs
+++ third_party/rust/chromium_crates_io/vendor/minijinja-v2/src/filters.rs
@@ -271,20 +271,29 @@ mod builtins {
         })
     }
 
-    fn cmp_helper(a: &Value, b: &Value, case_sensitive: bool) -> Ordering {
-        if !case_sensitive {
+    fn cmp_helper(a: &Value, b: &Value, case_sensitive: bool, reverse: bool) -> Ordering {
+        let ordering = if !case_sensitive {
             if let (Some(a), Some(b)) = (a.as_str(), b.as_str()) {
                 #[cfg(feature = "unicode")]
                 {
-                    return unicase::UniCase::new(a).cmp(&unicase::UniCase::new(b));
+                    unicase::UniCase::new(a).cmp(&unicase::UniCase::new(b))
                 }
                 #[cfg(not(feature = "unicode"))]
                 {
-                    return a.to_ascii_lowercase().cmp(&b.to_ascii_lowercase());
+                    a.to_ascii_lowercase().cmp(&b.to_ascii_lowercase())
                 }
+            } else {
+                a.cmp(b)
             }
+        } else {
+            a.cmp(b)
+        };
+
+        if reverse {
+            ordering.reverse()
+        } else {
+            ordering
         }
-        a.cmp(b)
     }
 
     /// Dict sorting functionality.
@@ -307,16 +316,14 @@ mod builtins {
 
         let by_value = matches!(ok!(kwargs.get("by")), Some("value"));
         let case_sensitive = ok!(kwargs.get::<Option<bool>>("case_sensitive")).unwrap_or(false);
+        let reverse = ok!(kwargs.get::<Option<bool>>("reverse")).unwrap_or(false);
         let mut rv: Vec<_> = ok!(v.try_iter())
             .map(|key| (key.clone(), v.get_item(&key).unwrap_or(Value::UNDEFINED)))
             .collect();
         safe_sort(&mut rv, |a, b| {
             let (a, b) = if by_value { (&a.1, &b.1) } else { (&a.0, &b.0) };
-            cmp_helper(a, b, case_sensitive)
+            cmp_helper(a, b, case_sensitive, reverse)
         })?;
-        if let Some(true) = ok!(kwargs.get("reverse")) {
-            rv.reverse();
-        }
         kwargs.assert_all_used()?;
         Ok(rv
             .into_iter()
@@ -763,6 +770,7 @@ mod builtins {
         .collect::<Vec<_>>();
 
         let case_sensitive = ok!(kwargs.get::<Option<bool>>("case_sensitive")).unwrap_or(false);
+        let reverse = ok!(kwargs.get::<Option<bool>>("reverse")).unwrap_or(false);
 
         if let Some(attr) = ok!(kwargs.get::<Option<&str>>("attribute")) {
             let keys: Vec<_> = attr
@@ -788,23 +796,20 @@ mod builtins {
                         keys.iter()
                             .map(|k| b.get_path_or_default(k, &Value::UNDEFINED)),
                     );
-                    cmp_helper(&key_a, &key_b, case_sensitive)
+                    cmp_helper(&key_a, &key_b, case_sensitive, reverse)
                 })?;
             } else {
                 // Fast path for a more common case of single key
                 let key = if !keys.is_empty() { keys[0] } else { attr };
                 safe_sort(&mut items, |a, b| {
                     match (a.get_path(key), b.get_path(key)) {
-                        (Ok(a), Ok(b)) => cmp_helper(&a, &b, case_sensitive),
+                        (Ok(a), Ok(b)) => cmp_helper(&a, &b, case_sensitive, reverse),
                         _ => Ordering::Equal,
                     }
                 })?;
             }
         } else {
-            safe_sort(&mut items, |a, b| cmp_helper(a, b, case_sensitive))?;
-        }
-        if let Some(true) = ok!(kwargs.get("reverse")) {
-            items.reverse();
+            safe_sort(&mut items, |a, b| cmp_helper(a, b, case_sensitive, reverse))?;
         }
         ok!(kwargs.assert_all_used());
         Ok(Value::from(items))
@@ -1397,7 +1402,7 @@ mod builtins {
         safe_sort(&mut items, |a, b| {
             let a = a.get_path_or_default(attr, &default);
             let b = b.get_path_or_default(attr, &default);
-            cmp_helper(&a, &b, case_sensitive)
+            cmp_helper(&a, &b, case_sensitive, false)
         })?;
         ok!(kwargs.assert_all_used());
 
@@ -1437,7 +1442,7 @@ mod builtins {
         for item in items {
             let group_by = item.get_path_or_default(attr, &default);
             if let Some(ref last_grouper) = grouper {
-                if cmp_helper(last_grouper, &group_by, case_sensitive) != Ordering::Equal {
+                if cmp_helper(last_grouper, &group_by, case_sensitive, false) != Ordering::Equal {
                     rv.push(Value::from_object(GroupTuple {
                         grouper: last_grouper.clone(),
                         list: std::mem::take(&mut list),
