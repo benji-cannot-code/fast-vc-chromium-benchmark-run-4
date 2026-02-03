df35f8459fd0f2c6a1dd27d618e3257581bf51e2
Dave Tapuska <dtapuska@chromium.org>
2026-02-03 15:25:21 -0800

    [lens] Create base class
    - Move common data members to base class with protected access for now
    
    BUG=b:477955819
    
    Change-Id: I69807ff9c860d7707721a65959cc376f577b9152
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7541836
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1579099}

--- chrome/browser/ui/lens/lens_overlay_controller.cc
+++ chrome/browser/ui/lens/lens_overlay_controller.cc
@@ -316,9 +316,8 @@ LensOverlayController::LensOverlayController(
     tabs::TabInterface* tab,
     LensSearchController* lens_search_controller,
     PrefService* pref_service)
-    : tab_(tab),
+    : OverlayBaseController(tab, pref_service),
       lens_search_controller_(lens_search_controller),
-      pref_service_(pref_service),
       gen204_controller_(
           std::make_unique<lens::LensOverlayGen204Controller>()) {
   InitializeTutorialIPHUrlMatcher();
@@ -330,7 +329,6 @@ LensOverlayController::LensOverlayController(
 
 LensOverlayController::~LensOverlayController() {
   tab_contents_observer_.reset();
-  state_ = State::kOff;
 }
 
 DEFINE_CLASS_ELEMENT_IDENTIFIER_VALUE(LensOverlayController, kOverlayId);
---
+++ chrome/browser/ui/lens/overlay_base_controller.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/lens/overlay_base_controller.h"
+
+#include "components/tabs/public/tab_interface.h"
+
+OverlayBaseController::OverlayBaseController(tabs::TabInterface* tab,
+                                             PrefService* pref_service)
+    : tab_(tab), pref_service_(pref_service) {}
+
+OverlayBaseController::~OverlayBaseController() {
+  state_ = State::kOff;
+}
