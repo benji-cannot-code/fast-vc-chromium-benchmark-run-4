5ad19304085f6a89a7722d8a9374474d05f6a5b5
Vladimir Levin <vmpstr@chromium.org>
2026-02-02 20:52:57 -0800

    [anchor] Fix double scroll offset hit testing.
    This patch fixes the main thread's understanding of the position of
    the chained positioned element. Note that this does *not* address lack
    of position try fallbacks selection when we intersect the containing
    block. That will be done in a follow up.
    
    R=ikilpatrick@chromium.org
    
    Bug: 478779831
    Change-Id: Ibf9408094ceecd66d0777dbdab536fcae459f17f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7539673
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1578551}

--- third_party/blink/renderer/core/layout/anchor_position_scroll_data.cc
+++ third_party/blink/renderer/core/layout/anchor_position_scroll_data.cc
@@ -183,8 +183,11 @@ AnchorPositionScrollData::ComputeAdjustmentContainersData(
               CompositorElementIdFromUniqueObjectId(
                   box->UniqueId(), CompositorElementIdNamespace::
                                        kAnchorPositionScrollTranslation));
-          result.accumulated_adjustment +=
-              data->ComputeDefaultAnchorAdjustmentData().accumulated_adjustment;
+          if (!RuntimeEnabledFeatures::CSSAnchorUpdateEnabled()) {
+            result.accumulated_adjustment +=
+                data->ComputeDefaultAnchorAdjustmentData()
+                    .accumulated_adjustment;
+          }
         }
       }
     }
