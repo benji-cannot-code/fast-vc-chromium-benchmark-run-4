f730f1564e6a223beddf1b4e5ec8eaed38abc838
Reilly Grant <reillyg@google.com>
2026-02-02 17:27:06 -0800

    Reland "webnn: Fix tensor size limit test on 32-bit platforms"
    This reverts commit 9089093721904985b6a3e1b746e087f6f4d6099f.
    
    Reason for revert: Fixed incorrect WPT.
    
    Original change's description:
    > Revert "webnn: Fix tensor size limit test on 32-bit platforms"
    >
    > This reverts commit cc2dbce9e00433ff65daf1e5ed1b4c4d695763bd.
    >
    > Reason for revert: Causing consistent failures on the WebKit Win10 builder
    > i.e. https://ci.chromium.org/ui/p/chromium/builders/ci/WebKit%20Win10/170161/overview
    >
    > Original change's description:
    > > webnn: Fix tensor size limit test on 32-bit platforms
    > >
    > > The number of elements in the test tensor created for this test cannot
    > > be represented by a size_t on 32-bit platforms and so the test triggers
    > > an unexpected Mojo error.
    > >
    > > A lower maximum tensor size limit needs to be chosen on 32-bit systems
    > > because making a 2 GiB allocation is not possible.
    > >
    > > Fixed: 390358145
    > > Change-Id: Ibeffb6d04236c1f16947c9faa50ca33e09291587
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7534076
    > > Commit-Queue: Reilly Grant <reillyg@chromium.org>
    > > Reviewed-by: Phillis Tang <phillis@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1577586}
    >
    > Bug: 390358145
    > Change-Id: I8fb0d0ea55b2157203d890161228f463869089c9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7532200
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Nidhi Jaju <nidhijaju@chromium.org>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Auto-Submit: Nidhi Jaju <nidhijaju@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1577888}
    
    Bug: 390358145
    Change-Id: Ic395f5a6353bb8e478d60e4126b688087594a718
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7539660
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1578485}

--- third_party/blink/web_tests/external/wpt/webnn/conformance_tests/tensor.https.any.js
+++ third_party/blink/web_tests/external/wpt/webnn/conformance_tests/tensor.https.any.js
@@ -253,7 +253,7 @@ const testCreateConstantTensorFails = (testName, descriptor) => {
 promise_test(async t => {
   const tensorDescriptor = {
     dataType: 'int32',
-    shape: [(context.opSupportLimits().maxTensorByteLength + 1) / 4],
+    shape: [context.opSupportLimits().maxTensorByteLength / 4 + 1],
     writable: true,
   };
   await promise_rejects_js(
