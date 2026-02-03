840cdccce8e27abf86163894b8061c3bc039e5e6
John Chen <johnchen@chromium.org>
2026-02-03 12:17:06 -0800

    [Perf] Add skeleton code for web-tests CUJs benchmark
    This CL adds the new benchmark, which doesn't run any real code yet.
    The benchmark implementation will be in a follow-up CL.
    
    Bug: b:435031130
    Change-Id: I71dba2becb20bc586ed672b732f7bb72cca0e272
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7541308
    Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    Commit-Queue: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1578984}

---
+++ tools/perf/web_tests_cuj.py
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env vpython3
+# Copyright 2026 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import sys
+
+
+def main():
+  # TODO(b/435031130): Add real implementation.
+  print('Placeholder for web-tests CUJ runner')
+  return 0
+
+
+if __name__ == '__main__':
+  sys.exit(main())
