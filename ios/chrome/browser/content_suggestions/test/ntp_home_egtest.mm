9383053264fce3958a129a8b2a0e4dae08e254cf
Chris Lu <thegreenfrog@google.com>
2026-02-03 12:57:52 -0800

    [ios] Add composeboxiPad fieldtrial config
    A few things were changed to fix tests:
    - Do not add typingShield used in old omnibox to view hierarchy.
    - Add the same a11y identifier for the composebox ipad typing shield.
    - Do not assert the NTP fakebox is hidden on ipad since the omnibox
    results suggestions list may not be that long
    - Update visibility checks for the first omnibox row since the
    composebox inputplate has a blur view surrounding it, slightly masking
    content around it.
    
    Change-Id: Ia4fdd3a8941f2f8f0dbebfadfcd0db2c979a6546
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7530380
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1579006}

--- ios/chrome/browser/content_suggestions/test/ntp_home_egtest.mm
+++ ios/chrome/browser/content_suggestions/test/ntp_home_egtest.mm
@@ -437,9 +437,12 @@ bool AreNumbersEqual(CGFloat num1, CGFloat num2) {
   [ChromeEarlGrey sceneOpenURL:GURL("chromewidgetkit://search-widget/search")];
   [ChromeEarlGrey
       waitForSufficientlyVisibleElementWithMatcher:chrome_test_util::Omnibox()];
-  // Fakebox should be mostly covered.
-  [[EarlGrey selectElementWithMatcher:chrome_test_util::FakeOmnibox()]
-      assertWithMatcher:mostlyNotVisible()];
+  // Fakebox should be mostly covered on iphone. On iPad if there are no
+  // suggestions the fakebox can be seen.
+  if (![ChromeEarlGrey isIPadIdiom]) {
+    [[EarlGrey selectElementWithMatcher:chrome_test_util::FakeOmnibox()]
+        assertWithMatcher:mostlyNotVisible()];
+  }
 }
 
 // Tests that the fake omnibox width is correctly updated after a rotation.
@@ -823,9 +826,12 @@ bool AreNumbersEqual(CGFloat num1, CGFloat num2) {
   // page.
   [self focusFakebox];
 
-  // Check the fake omnibox is mostly not visible.
-  [[EarlGrey selectElementWithMatcher:chrome_test_util::FakeOmnibox()]
-      assertWithMatcher:mostlyNotVisible()];
+  if (![ChromeEarlGrey isIPadIdiom]) {
+    // Check the fake omnibox is mostly not visible for iphone only. It can be
+    // shown if there are no results suggestions on iPad.
+    [[EarlGrey selectElementWithMatcher:chrome_test_util::FakeOmnibox()]
+        assertWithMatcher:mostlyNotVisible()];
+  }
   [[EarlGrey selectElementWithMatcher:chrome_test_util::Omnibox()]
       assertWithMatcher:grey_sufficientlyVisible()];
 
@@ -852,9 +858,12 @@ bool AreNumbersEqual(CGFloat num1, CGFloat num2) {
   // Offset after the fake omnibox has been tapped.
   CGPoint offsetAfterTap = collectionView.contentOffset;
 
-  // Make sure the fake omnibox has been hidden and the collection has moved.
-  [[EarlGrey selectElementWithMatcher:chrome_test_util::FakeOmnibox()]
-      assertWithMatcher:grey_not(grey_sufficientlyVisible())];
+  // Make sure the fake omnibox has been hidden and the collection has moved. On
+  // iPad if there are no suggestions the fakebox can be seen.
+  if (![ChromeEarlGrey isIPadIdiom]) {
+    [[EarlGrey selectElementWithMatcher:chrome_test_util::FakeOmnibox()]
+        assertWithMatcher:grey_not(grey_sufficientlyVisible())];
+  }
   GREYAssertTrue(offsetAfterTap.y >= origin.y,
                  @"The collection has not moved.");
 
@@ -1084,7 +1093,7 @@ bool AreNumbersEqual(CGFloat num1, CGFloat num2) {
   // devices, fake omnibox persists and sticks to top.
   if ([ChromeEarlGrey isIPadIdiom]) {
     [[EarlGrey selectElementWithMatcher:chrome_test_util::FakeOmnibox()]
-        assertWithMatcher:grey_notVisible()];
+        assertWithMatcher:mostlyNotVisible()];
   } else {
     [[EarlGrey selectElementWithMatcher:chrome_test_util::FakeOmnibox()]
         assertWithMatcher:grey_sufficientlyVisible()];
