10e92c6e38b0cfeb2d4d9e280df6d4ec3d261fc5
Aishwarya Rajesh <aishwaryarj@google.com>
2026-02-04 10:11:49 -0800

    [RWM][RecentlyClosed] Clear old TabRestoreService entries
    The CL updates getRecentlyClosedTabsAndWindows() to clear tab event
    entries older than six months. Note that the trigger point of the
    cleanup is dependent on a request to get the list of aggregated entries,
    and updating an already open Recent Tabs page real-time is out of scope
    for this update, because there is no objective app trigger point to
    detect retention period expiration.
    
    Bug: 480756162
    Change-Id: Id1ee2f71e0ef68ff360a5433f56245344116e522
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7537451
    Reviewed-by: Zhe Li <zheliooo@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1579575}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ntp/RecentTabsPageTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ntp/RecentTabsPageTest.java
@@ -649,8 +649,14 @@ public class RecentTabsPageTest {
         final RecentlyClosedTab tab =
                 new RecentlyClosedTab(
                         0, 0, "Tab Title", new GURL("https://www.example.com/"), null);
-        setRecentlyClosedEntries(Arrays.asList(tab, window));
-        assertEquals(2, mManager.getRecentlyClosedEntries(2).size());
+        RecentlyClosedEntriesManager recentlyClosedEntriesManager =
+                mActivity.getRecentlyClosedEntriesManagerForTesting();
+        setRecentlyClosedEntries(List.of(tab));
+        ThreadUtils.runOnUiThreadBlocking(
+                () ->
+                        recentlyClosedEntriesManager.onWindowClosed(
+                                window, /* isPermanentDeletion= */ false));
+        assertEquals(2, recentlyClosedEntriesManager.getRecentlyClosedEntries().size());
 
         final String windowDescriptionString = "google.com and " + (tabCount - 1) + " other tabs";
         View windowView = waitForView(windowTitle);
@@ -662,8 +668,6 @@ public class RecentTabsPageTest {
         // Confirm the recently closed entries are all gone after "Remove all" is clicked.
         openContextMenuAndInvokeItem(
                 windowView, RecentTabsRowAdapter.RecentlyClosedTabsGroup.ID_REMOVE_ALL);
-        RecentlyClosedEntriesManager recentlyClosedEntriesManager =
-                mActivity.getRecentlyClosedEntriesManagerForTesting();
         assertEquals(0, recentlyClosedEntriesManager.getRecentlyClosedEntries().size());
         waitForViewToDisappear(windowDescriptionString);
     }
