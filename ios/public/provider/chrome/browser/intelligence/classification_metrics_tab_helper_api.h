75d9c91a25ab8df1555fc2e6c9b6309cee36d836
Amarjot Gill <amarjotgill@chromium.org>
2026-02-04 10:02:34 -0800

    Add a no-op ClassificationMetricsTabHelper API.
    This change introduces `AttachClassificationMetricsTabHelper` in
    `ios/public/provider/chrome/browser/intelligence`. The default Chromium
    and test providers include no-op implementations. This API will be used
    to attach a TabHelper for collecting classification-related metrics,
    with a real implementation in ios_internal.
    
    Bug: 469817333
    Change-Id: I4b6740d8ef1e324a86254d39cb62bb32030942b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7546297
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Amarjot Gill <amarjotgill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1579564}

---
+++ ios/public/provider/chrome/browser/intelligence/classification_metrics_tab_helper_api.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#ifndef IOS_PUBLIC_PROVIDER_CHROME_BROWSER_INTELLIGENCE_CLASSIFICATION_METRICS_TAB_HELPER_API_H_
+#define IOS_PUBLIC_PROVIDER_CHROME_BROWSER_INTELLIGENCE_CLASSIFICATION_METRICS_TAB_HELPER_API_H_
+
+namespace web {
+class WebState;
+}  // namespace web
+
+namespace ios::provider {
+void AttachClassificationMetricsTabHelper(web::WebState* web_state);
+}  // namespace ios::provider
+
+#endif  // IOS_PUBLIC_PROVIDER_CHROME_BROWSER_INTELLIGENCE_CLASSIFICATION_METRICS_TAB_HELPER_API_H_
