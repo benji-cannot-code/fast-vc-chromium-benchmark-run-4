91e0b750fceabbac84af4b900f3d6ac6bb982015
Kene Okoye <kenok@google.com>
2026-02-04 11:47:23 -0800

    [Skills] Add Gemini in Chrome branding to the Sidebar
    Screenshot (light): https://screenshot.googleplex.com/8bfPX8kLXHtDQka
    Screenshot (dark): https://screenshot.googleplex.com/BtpGKhcw7yBsYZa
    
    Bug: b:481009893
    Change-Id: I15f8a9b89cda5a4dbdcecbbd067bf3280b5b4843
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7541131
    Reviewed-by: Christine Ying <chrstne@google.com>
    Commit-Queue: Kene Okoye <kenok@google.com>
    Cr-Commit-Position: refs/heads/main@{#1579635}

--- chrome/browser/resources/skills/sidebar.html.ts
+++ chrome/browser/resources/skills/sidebar.html.ts
@@ -9,7 +9,7 @@ import {type SkillsSidebarElement} from './sidebar.js';
 
 export function getHtml(this: SkillsSidebarElement) {
   // clang-format off
-  return html`
+  return html`<!--_html_template_start_-->
 <cr-menu-selector selectable="a" selected-attribute="selected"
     @iron-activate="${this.onMenuItemActivate_}" .selected="${this.selectedPage}"
     attr-for-selected="data-path">
@@ -20,6 +20,15 @@ export function getHtml(this: SkillsSidebarElement) {
       <div class="name">${menuItem.name}</div>
     </a>
   `)}
-</cr-menu-selector>`;
+</cr-menu-selector>
+<div class="separator"></div>
+<div class="footer">
+  $i18n{footerText}
+  <span class="branding">
+    <cr-icon icon="skills:spark"></cr-icon>
+    $i18n{footerBranding}
+  </span>
+</div>
+<!--_html_template_end_-->`;
   // clang-format on
 }
--- chrome/browser/resources/skills/sidebar.ts
+++ chrome/browser/resources/skills/sidebar.ts
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import '//resources/cr_elements/cr_icon/cr_icon.js';
 import '//resources/cr_elements/cr_menu_selector/cr_menu_selector.js';
 
+import {loadTimeData} from '//resources/js/load_time_data.js';
 import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
-import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 
 import {getCss} from './sidebar.css.js';
 import {getHtml} from './sidebar.html.js';
