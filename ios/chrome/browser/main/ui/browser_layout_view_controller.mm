d9c7f97c1bbde4e29b5dec1c86ddaca49634f034
Ewann Pelle <ewannpv@chromium.org>
2026-02-04 00:56:04 -0800

    [iOS] Cleans fallback presentation logic
    BrowserLayoutViewController no longer needs to manually forward
    presentation calls. This simplifies the controller and removes unused
    fallback logic.
    
    Change-Id: I3c2d0957ea7ae1781fea285888fd15656b076b16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7531429
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1579284}

--- ios/chrome/browser/main/ui/browser_layout_view_controller.mm
+++ ios/chrome/browser/main/ui/browser_layout_view_controller.mm
@@ -61,29 +61,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   DCHECK(self.currentBVC == bvc);
 }
 
-#pragma mark - UIViewController methods
-
-- (void)presentViewController:(UIViewController*)viewControllerToPresent
-                     animated:(BOOL)flag
-                   completion:(void (^)())completion {
-  // Force presentation to go through the current BVC, if possible, which does
-  // some associated bookkeeping.
-  UIViewController* viewController =
-      self.currentBVC ? self.currentBVC : self.fallbackPresenterViewController;
-  [viewController presentViewController:viewControllerToPresent
-                               animated:flag
-                             completion:completion];
-}
-
-- (void)dismissViewControllerAnimated:(BOOL)flag
-                           completion:(void (^)())completion {
-  // Force dismissal to go through the current BVC, if possible, which does some
-  // associated bookkeeping.
-  UIViewController* viewController =
-      self.currentBVC ? self.currentBVC : self.fallbackPresenterViewController;
-  [viewController dismissViewControllerAnimated:flag completion:completion];
-}
-
 - (UIViewController*)childViewControllerForStatusBarHidden {
   return self.currentBVC;
 }
