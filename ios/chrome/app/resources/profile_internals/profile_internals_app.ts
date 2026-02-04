19e83995d1cef244fba7ef54e2d54d288d491ec9
dpapad <dpapad@chromium.org>
2026-02-04 11:53:14 -0800

    WebUI: Fix violations of upcoming @webui-eslint/lit-element-structure, part2.
    This is in preparation of introducing a new ESLint check that will
    automatically enforce the basic structure of a CrLitElement subclass.
    
    Bug: 480028273
    Change-Id: I57fa0fbab9a25b548c075f3d6d485e155c9540f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7542954
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Teresa Mao <temao@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Teresa Mao <temao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1579645}

--- ios/chrome/app/resources/profile_internals/profile_internals_app.ts
+++ ios/chrome/app/resources/profile_internals/profile_internals_app.ts
@@ -77,5 +77,11 @@ export class ProfileInternalsAppElement extends CrLitElement {
   }
 }
 
+declare global {
+  interface HTMLElementTagNameMap {
+    'profile-internals-app': ProfileInternalsAppElement;
+  }
+}
+
 customElements.define(
     ProfileInternalsAppElement.is, ProfileInternalsAppElement);
