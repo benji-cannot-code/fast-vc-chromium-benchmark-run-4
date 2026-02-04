0888e4d8641388fca44acc25b235283a1129ef3a
Norge Vizcay <vizcay@google.com>
2026-02-04 04:04:17 -0800

    Implement UpsertPrivatePassRequest for private passes.
    This CL introduces UpsertPrivatePassRequest to handle private wallet
    passes (Passport, Driver's License, National ID Card, KTN, and Redress
    Number) using the dedicated /v1/e/privatePasses:upsert endpoint.
    
    Note that public and private passes use different API endpoints and
    different request protos.
    
    Key changes:
    * Created UpsertPrivatePassRequest class to encapsulate private pass data.
    * Refactored UpsertPassRequest to remove private pass types, which are
    now handled by the new request class.
    * Updated WalletHttpClientImpl to route requests to the appropriate
    class based on the pass category.
    * Added unit tests for the new request class.
    
    Bug: 478783796
    Change-Id: I46e42426f019d2a1fa687bb7ceb8cbc5e0e96c20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7531434
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1579347}

--- components/wallet/core/browser/network/wallet_http_client_impl.cc
+++ components/wallet/core/browser/network/wallet_http_client_impl.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/wallet/core/browser/data_models/wallet_pass.h"
 #include "components/wallet/core/browser/network/get_unmasked_pass_request.h"
 #include "components/wallet/core/browser/network/upsert_pass_request.h"
+#include "components/wallet/core/browser/network/upsert_private_pass_request.h"
 #include "components/wallet/core/browser/network/wallet_request.h"
 #include "components/wallet/core/common/wallet_features.h"
 #include "google_apis/gaia/google_service_auth_error.h"
@@ -64,8 +65,25 @@ void WalletHttpClientImpl::UpsertPass(WalletPass pass,
                                       UpsertPassCallback callback) {
   CHECK(SatisfiesFeatureRequirements(pass));
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  SendRequest(std::make_unique<UpsertPassRequest>(std::move(pass),
-                                                  std::move(callback)));
+  switch (pass.GetPassCategory()) {
+    case PassCategory::kLoyaltyCard:
+    case PassCategory::kEventPass:
+    case PassCategory::kTransitTicket:
+    case PassCategory::kBoardingPass:
+      SendRequest(std::make_unique<UpsertPassRequest>(std::move(pass),
+                                                      std::move(callback)));
+      break;
+    case PassCategory::kPassport:
+    case PassCategory::kDriverLicense:
+    case PassCategory::kNationalIdentityCard:
+    case PassCategory::kKTN:
+    case PassCategory::kRedressNumber:
+      SendRequest(std::make_unique<UpsertPrivatePassRequest>(
+          std::move(pass), std::move(callback)));
+      break;
+    case PassCategory::kUnspecified:
+      NOTREACHED();
+  }
 }
 
 void WalletHttpClientImpl::GetUnmaskedPass(std::string_view pass_id,
