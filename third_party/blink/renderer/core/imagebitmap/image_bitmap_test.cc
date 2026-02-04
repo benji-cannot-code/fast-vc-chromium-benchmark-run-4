675712a0cb375367ef4726a41a99bb3a661cb246
Colin Blundell <blundell@chromium.org>
2026-02-04 00:21:44 -0800

    [Blink] Move ImageBitmapTest away from using CanvasResourceProvider
    All this test wants to do is create a blank
    AcceleratedStaticBitmapImage; it doesn't need to go through CRPSI for
    that.
    
    Bug: 352263194
    Change-Id: Iab55e678f7054352663fd85c15991281c9889bf2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7535271
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1579275}

--- third_party/blink/renderer/core/imagebitmap/image_bitmap_test.cc
+++ third_party/blink/renderer/core/imagebitmap/image_bitmap_test.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/imagebitmap/image_bitmap.h"
 
+#include "base/test/null_task_runner.h"
 #include "build/build_config.h"
 #include "components/viz/test/test_raster_interface.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -45,7 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/loader/resource/image_resource_content.h"
 #include "third_party/blink/renderer/core/testing/dummy_page_holder.h"
 #include "third_party/blink/renderer/platform/graphics/accelerated_static_bitmap_image.h"
-#include "third_party/blink/renderer/platform/graphics/canvas_resource_provider.h"
 #include "third_party/blink/renderer/platform/graphics/gpu/shared_gpu_context.h"
 #include "third_party/blink/renderer/platform/graphics/skia/skia_utils.h"
 #include "third_party/blink/renderer/platform/graphics/test/fake_gles2_interface.h"
@@ -256,15 +256,18 @@ static void TestImageBitmapTextureBacked(
 }
 
 TEST_F(ImageBitmapTest, AvoidGPUReadback) {
-  base::WeakPtr<WebGraphicsContext3DProviderWrapper> context_provider_wrapper =
-      SharedGpuContext::ContextProviderWrapper();
-  auto resource_provider = CanvasNon2DResourceProviderSharedImage::Create(
-      gfx::Size(100, 100), GetN32FormatForCanvas(), kPremul_SkAlphaType,
-      gfx::ColorSpace::CreateSRGB(),
-      CanvasResourceProvider::ShouldInitialize::kNo, context_provider_wrapper,
-      RasterMode::kGPU, gpu::SharedImageUsageSet());
+  auto client_si = gpu::ClientSharedImage::CreateForTesting(
+      gpu::SHARED_IMAGE_USAGE_RASTER_READ);
+  gpu::SyncToken token;
+  token.Set(gpu::CommandBufferNamespace::GPU_IO,
+            gpu::CommandBufferId::FromUnsafeValue(64), 100);
 
-  scoped_refptr<StaticBitmapImage> bitmap = resource_provider->Snapshot();
+  scoped_refptr<StaticBitmapImage> bitmap =
+      AcceleratedStaticBitmapImage::CreateFromCanvasSharedImage(
+          std::move(client_si), token, kPremul_SkAlphaType,
+          SharedGpuContext::ContextProviderWrapper(),
+          base::PlatformThread::CurrentRef(),
+          base::MakeRefCounted<base::NullTaskRunner>(), base::DoNothing());
   ASSERT_TRUE(bitmap->IsTextureBacked());
 
   auto* image_bitmap = MakeGarbageCollected<ImageBitmap>(bitmap);
