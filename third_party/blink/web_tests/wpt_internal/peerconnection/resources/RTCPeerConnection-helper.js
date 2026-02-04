996d6cb8f17fc645ec2864711b5dc605be6dd2ef
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2026-02-04 12:38:15 -0800

    LNA: Move LNA WebRTC tests to wpt_internal
    This moves the Local Network Access WebRTC tests from
    fast/peerconnection to wpt_internal/peerconnection to make use of some
    WPT functionality, specifically headers files.
    
    It also updates VirtualTestSuites to include the new path in the
    'lna-webrtc' suite and updates content_shell.filter to ensure they run
    in blink_wpt_tests.
    
    Bug: 481295187
    Change-Id: Ib64cc5cd16adcc5e48435781b74b9bf7606fabce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7544781
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1579658}

---
+++ third_party/blink/web_tests/wpt_internal/peerconnection/resources/RTCPeerConnection-helper.js
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+'use strict'
+
+// Returns a promise that resolves when a |name| event is fired.
+function waitUntilEvent(obj, name) {
+  return new Promise(r => obj.addEventListener(name, r, {once: true}));
+}
+
+// Returns a promise that resolves when |pc.connectionState| is in one of the
+// wanted states.
+async function waitForConnectionStateChange(pc, wantedStates) {
+  while (!wantedStates.includes(pc.connectionState)) {
+    await waitUntilEvent(pc, 'connectionstatechange');
+  }
+}
+
+async function waitForIceGatheringState(pc, wantedStates) {
+  while (!wantedStates.includes(pc.iceGatheringState)) {
+    await waitUntilEvent(pc, 'icegatheringstatechange');
+  }
+}
