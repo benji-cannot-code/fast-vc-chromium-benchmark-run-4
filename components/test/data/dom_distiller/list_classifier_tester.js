34ac700efa10b0ae4d122eba7cfe4c100d7bdf26
Samuel Huang <huangs@chromium.org>
2026-02-04 11:50:53 -0800

    Revert "[Reader Mode] Split dom_distiller_viewer.js"
    This reverts commit 53e8075b7b3d2d8ad35ea737e7cb2271b98ab135.
    
    Reason for revert: Likely breaking build due to dependency issues.
    
    Original change's description:
    > [Reader Mode] Split dom_distiller_viewer.js
    >
    > This CL mechanically splits dom_distiller_viewer.js into 7 files:
    > * content_processing.js: Functions to process or analyze Elements.
    > * dom_distiller_viewer_main.js: Functions for DOM Distiller Viewer UI.
    > * font_size_slider.js: Desktop only: FontSizeSlider: UI for font size.
    > * image_classifier.js: ImageClassifier: Decides whether an image is
    >   inline or full-width, and applies the appropriate class.
    > * list_classifier.js: ListClassifier: Decides whether an <ul> or <ol>
    >   is intended as list, and assigns the appropriate class.
    > * pinch_handler.js: Android only: Pincher: Handles touch event to
    >   navigate the distilled page.
    > * settings_dialog.js: SettingsDialog: UI settings handler.
    >
    > This CL also updates the usage in DOM distiller in production and tests.
    >
    > For these usages, dom_distiller_viewer.js gets dynamically generated by
    > concatenating the files.
    > * We kept the dom_distiller_viewer.js name to reduce code changes at
    >   usage sites.
    > * For simplicity, we concatenate the files in alphabetical order.
    >   This causes reordering, but crrev.com/c/7533703 diffused potential
    >   problems that would have arisen.
    >
    > For DomDistillerViewerTest, the test regex for pinch zoom was fragile to
    > JS auto-formatting, so in pinch_handler.js we extracted dynamic pinching
    > to a clampScaleFun(), and added a unique comment "/* PINCH_SCALE */" for
    > regex anchoring.
    >
    > Changes related to the Readability Test Extension will be applied in a
    > follow-up CL.
    >
    > Bug: 481319224
    > Change-Id: Ic601e73ac664de46f6411a5277aedf5ced978309
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7539737
    > Reviewed-by: Brandon Wylie <wylieb@google.com>
    > Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    > Commit-Queue: Samuel Huang <huangs@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1579596}
    
    Bug: 481319224
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I3c46c53bd8f7e698e1a28d71e00aee3260e6e15e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7545028
    Owners-Override: Salma Elmahallawy <esalma@google.com>
    Commit-Queue: Salma Elmahallawy <esalma@google.com>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Auto-Submit: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Salma Elmahallawy <esalma@google.com>
    Cr-Commit-Position: refs/heads/main@{#1579641}

--- components/test/data/dom_distiller/list_classifier_tester.js
+++ components/test/data/dom_distiller/list_classifier_tester.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /**
  * @fileoverview Test suite for the ListClassifier class in
- * list_classifier.js.
+ * dom_distiller_viewer.js.
  */
 
 suite('ListClassifier', function() {
