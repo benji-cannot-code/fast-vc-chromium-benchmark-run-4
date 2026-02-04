454cf80ddd4027323a7b9928638560d1a6f35e97
Chris Lu <thegreenfrog@google.com>
2026-02-04 12:57:05 -0800

    [ios] Move setting OmniboxPopupPresenter to its own method
    This allows the logic to separate fetching the correct backgroundColor
    and assigning the presenting delegate.
    
    Fixed: 469986429
    Change-Id: I735d6d04f8da8e2bbe34c48d8af969c7ff6c831c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7497678
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1579664}

--- ios/chrome/browser/composebox/ui/composebox_view_controller.mm
+++ ios/chrome/browser/composebox/ui/composebox_view_controller.mm
@@ -644,6 +644,10 @@ UIImage* CloseButtonImage(UIColor* backgroundColor, BOOL highlighted) {
 
 #pragma mark - OmniboxPopupPresenterDelegate
 
+- (void)popupDidInitializePresenter:(OmniboxPopupPresenter*)presenter {
+  _presenter = presenter;
+}
+
 - (UIView*)popupParentViewForPresenter:(OmniboxPopupPresenter*)presenter {
   return _omniboxPopupContainer;
 }
@@ -654,7 +658,6 @@ UIImage* CloseButtonImage(UIColor* backgroundColor, BOOL highlighted) {
 }
 
 - (UIColor*)popupBackgroundColorForPresenter:(OmniboxPopupPresenter*)presenter {
-  _presenter = presenter;
   return self.view.backgroundColor;
 }
 
