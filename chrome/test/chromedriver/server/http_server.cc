8b9cf3f2e94626ceebd9997c67937aefd98b61cd
Adam Rice <ricea@chromium.org>
2026-02-04 04:05:16 -0800

    Check WebSocket Origin header in chromedriver
    The chromedriver HTTP server wasn't checking the Origin header on
    incoming WebSocket connections. Fix it.
    
    Also add a test to //chrome/test/chromedriver/test/run_py_tests.py to
    verify the fix.
    
    Bug: 478783560
    Change-Id: I1f6cc49625791cd531018e38897ce5463dd63562
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7538400
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1579348}

--- chrome/test/chromedriver/server/http_server.cc
+++ chrome/test/chromedriver/server/http_server.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_errors.h"
 #include "net/base/network_interfaces.h"
 #include "net/base/sys_addrinfo.h"
+#include "net/http/http_status_code.h"
 #include "url/gurl.h"
 
 namespace {
@@ -234,6 +235,14 @@ HttpServer::~HttpServer() = default;
 
 void HttpServer::OnWebSocketRequest(int connection_id,
                                     const net::HttpServerRequestInfo& info) {
+  if (!RequestIsSafeToServe(info, allow_remote_, whitelisted_ips_,
+                            allowed_origins_)) {
+    server_->Send(connection_id, net::HTTP_FORBIDDEN,
+                  "Host header or origin header is specified and is not "
+                  "whitelisted or localhost.",
+                  "text/html", TRAFFIC_ANNOTATION_FOR_TESTS);
+    return;
+  }
   cmd_runner_->PostTask(
       FROM_HERE, base::BindOnce(&HttpHandler::OnWebSocketRequest, handler_,
                                 this, connection_id, info));
