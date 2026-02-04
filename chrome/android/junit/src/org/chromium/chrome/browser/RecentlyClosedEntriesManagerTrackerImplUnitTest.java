8d2a947165671e1ff4bbce938107208df2d6ee6a
Salma Elmahallawy <esalma@google.com>
2026-02-04 10:33:17 -0800

    Revert "[RWM][RecentlyClosed] Only call #notifyInstanceClosed once for bulk window closures"
    This reverts commit bde613065a39214bf802713642712ad46bf39249.
    
    Reason for revert: CL caused compile failures and closed tree http://ci.chromium.org/ui/p/chromium/builders/ci/android-desktop-x64-compile-rel/66113/overview
    
    Original change's description:
    > [RWM][RecentlyClosed] Only call #notifyInstanceClosed once for bulk window closures
    >
    > Bug: 479989179
    > Change-Id: Ie0524aaa5be9a03f2b51800266399ad77e9dcd37
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7535173
    > Auto-Submit: Zhe Li <zheliooo@google.com>
    > Commit-Queue: Zhe Li <zheliooo@google.com>
    > Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    > Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    > Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1579586}
    
    Bug: 479989179
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ife31c4b0940454cb74f2f14ad5eb413ebcd6c5c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7545449
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Salma Elmahallawy <esalma@google.com>
    Commit-Queue: Salma Elmahallawy <esalma@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1579591}

--- chrome/android/junit/src/org/chromium/chrome/browser/RecentlyClosedEntriesManagerTrackerImplUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/RecentlyClosedEntriesManagerTrackerImplUnitTest.java
@@ -30,8 +30,6 @@ import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.tabmodel.TabModel;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 
-import java.util.Collections;
-
 /** Unit tests for {@link RecentlyClosedEntriesManagerTrackerImpl}. */
 @RunWith(BaseRobolectricTestRunner.class)
 @Config(manifest = Config.NONE)
@@ -92,8 +90,7 @@ public class RecentlyClosedEntriesManagerTrackerImplUnitTest {
                         /* lastAccessedTime= */ 1,
                         /* closureTime= */ 3,
                         /* markedForDeletion= */ true);
-        mTracker.onInstancesClosed(
-                Collections.singletonList(info), /* isPermanentDeletion= */ false);
+        mTracker.onInstanceClosed(info, /* isPermanentDeletion= */ false);
 
         assertEquals(1, manager1.getRecentlyClosedEntries().size());
         assertEquals(1, manager2.getRecentlyClosedEntries().size());
@@ -121,8 +118,7 @@ public class RecentlyClosedEntriesManagerTrackerImplUnitTest {
                         /* lastAccessedTime= */ 1,
                         /* closureTime= */ 3,
                         /* markedForDeletion= */ true);
-        mTracker.onInstancesClosed(
-                Collections.singletonList(info), /* isPermanentDeletion= */ false);
+        mTracker.onInstanceClosed(info, /* isPermanentDeletion= */ false);
         assertEquals(1, manager1.getRecentlyClosedEntries().size());
         assertEquals(1, manager2.getRecentlyClosedEntries().size());
 
