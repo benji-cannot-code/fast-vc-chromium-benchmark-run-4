43ab90edbf66c384a71018b0d59522a72aeb5f1e
Timofey Chudakov <tchudakov@google.com>
2026-02-04 03:13:29 -0800

    [AutofillAi - M4] Create basic EntityEditorView.
    This editor view only can set its title for now.
    
    Bypass-Check-License: Files moved.
    Bug: 476755159
    Change-Id: Id8ae727e98c1a16bd8bb647b8d96c32f19bbf629
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7531819
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1579325}

---
+++ chrome/browser/autofill/android/java/src/org/chromium/chrome/browser/autofill/editors/autofill_ai/EntityEditorProperties.java
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.autofill.editors.autofill_ai;
+
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.ui.modelutil.PropertyKey;
+import org.chromium.ui.modelutil.PropertyModel.ReadableObjectPropertyKey;
+
+/** Properties defined here reflect the visible state of the {@link EntityEditorView}. */
+@NullMarked
+public class EntityEditorProperties {
+    public static final ReadableObjectPropertyKey<String> EDITOR_TITLE =
+            new ReadableObjectPropertyKey<>("editor_title");
+
+    public static final PropertyKey[] ALL_KEYS = {
+        EDITOR_TITLE,
+    };
+
+    private EntityEditorProperties() {}
+}
