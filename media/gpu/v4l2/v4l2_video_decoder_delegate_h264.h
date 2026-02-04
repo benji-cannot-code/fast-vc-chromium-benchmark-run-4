c3f8d66a9bac6e1374134122d814b6cf8926084f
Hirokazu Honda <hiroh@chromium.org>
2026-02-04 01:07:14 -0800

    media/gpu: Update new Cencv1 H264 slice header parameters
    This change updates structures for Cencv1 H264 slice header parsing
    so it contains more necessary variables for H264 video decoding.
    
    Bug: b:449845359
    Test: video.PlayDRM.*
    Change-Id: I9463da27229bcffe5ffcb4a729a4e9e420dd0bd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7023201
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Nathan Hebert <nhebert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1579289}

--- media/gpu/v4l2/v4l2_video_decoder_delegate_h264.h
+++ media/gpu/v4l2/v4l2_video_decoder_delegate_h264.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MEDIA_GPU_V4L2_V4L2_VIDEO_DECODER_DELEGATE_H264_H_
 #define MEDIA_GPU_V4L2_V4L2_VIDEO_DECODER_DELEGATE_H264_H_
 
+#include <cassert>
 #include <memory>
 #include <vector>
 
@@ -29,9 +30,11 @@ struct V4L2VideoDecoderDelegateH264Private;
 // the required SPS/PPS fields used in slice header parsing so that we don't
 // need to re-parse the SPS/PPS in the secure world.
 struct CencV1StreamDataForSliceHeader {
+  uint8_t version;
   int32_t log2_max_frame_num_minus4;
   int32_t log2_max_pic_order_cnt_lsb_minus4;
   int32_t pic_order_cnt_type;
+  uint32_t pic_parameter_set_id;
   int32_t num_ref_idx_l0_default_active_minus1;
   int32_t num_ref_idx_l1_default_active_minus1;
   int32_t weighted_bipred_idc;
@@ -41,8 +44,9 @@ struct CencV1StreamDataForSliceHeader {
   uint8_t delta_pic_order_always_zero_flag;
   uint8_t redundant_pic_cnt_present_flag;
   uint8_t weighted_pred_flag;
-  uint8_t padding[3];
 };
+
+static_assert(sizeof(CencV1StreamDataForSliceHeader) == 44);
 
 class V4L2VideoDecoderDelegateH264 : public H264Decoder::H264Accelerator {
  public:
