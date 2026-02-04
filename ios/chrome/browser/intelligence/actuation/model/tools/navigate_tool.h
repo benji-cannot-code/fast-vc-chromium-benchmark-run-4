fae680ce5812dbda5f9c7627ec93f841da3badbc
Kirubel <kaklilu@google.com>
2026-02-04 07:32:16 -0800

    Remove tab_index member in NavigateTool.
    This was stored to avoid looking up the WebState index again.
    This isn't correct since the tab_index can easily change and cause
    this to fail.
    
    Bug: crbug.com/472291687
    Change-Id: I9ed19118cb445b446523e352121e8b556a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7500171
    Reviewed-by: Matt Reichhoff <mreichhoff@chromium.org>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Kirubel Aklilu <kaklilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1579447}

--- ios/chrome/browser/intelligence/actuation/model/tools/navigate_tool.h
+++ ios/chrome/browser/intelligence/actuation/model/tools/navigate_tool.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <string>
 
 #import "base/memory/raw_ptr.h"
+#import "base/memory/weak_ptr.h"
 #import "ios/chrome/browser/intelligence/actuation/model/tools/actuation_tool.h"
 
 class ProfileIOS;
@@ -21,10 +22,14 @@ class NavigateAction;
 }  // namespace proto
 }  // namespace optimization_guide
 
+namespace web {
+class WebState;
+}  // namespace web
+
 // Command to navigate to a URL.
 class NavigateTool : public ActuationTool {
  public:
-  ~NavigateTool() override = default;
+  ~NavigateTool() override;
 
   static base::expected<std::unique_ptr<NavigateTool>, ActuationError> Create(
       const optimization_guide::proto::NavigateAction& action,
@@ -35,12 +40,12 @@ class NavigateTool : public ActuationTool {
 
  private:
   NavigateTool(const std::string& url,
-               int tab_index,
+               base::WeakPtr<web::WebState> web_state,
                WebStateList* web_state_list,
                UrlLoadingBrowserAgent* url_loader);
 
   const std::string url_;
-  int tab_index_;
+  base::WeakPtr<web::WebState> web_state_;
   raw_ptr<WebStateList> web_state_list_;
   raw_ptr<UrlLoadingBrowserAgent> url_loader_;
 };
