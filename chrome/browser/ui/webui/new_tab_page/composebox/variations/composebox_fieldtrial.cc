d154c50c1f7226fad0521694150e4c6416cb813b
Paul Adedeji <pauladedeji@google.com>
2026-02-04 15:11:56 -0800

    [composebox] "Launch" viewport and suggest request parameters
    This change removes the feature parameters for composebox viewport
    images and suggest request context, replacing them with hardcoded
    values. This simplifies the configuration for the WebUI Omnibox and NTP
    Composebox, as they don't set these feature params anywhere.
    
    We can't avoid setting these as there are other users of the config
    params e.g. [1].
    
    [1]https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/android/omnibox/composebox_query_controller_bridge.cc;drc=a94620d92305849e7ee254290e84a04889f0ce1d;l=89
    
    Change-Id: I3347b9214b4d739843de1138d6072adebe6eb682
    Bug: b:477969358
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7540175
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1579736}

--- chrome/browser/ui/webui/new_tab_page/composebox/variations/composebox_fieldtrial.cc
+++ chrome/browser/ui/webui/new_tab_page/composebox/variations/composebox_fieldtrial.cc
@@ -193,9 +193,8 @@ CreateQueryControllerConfigParams() {
   auto config_params = std::make_unique<
       contextual_search::ContextualSearchContextController::ConfigParams>();
   config_params->send_lns_surface = true;
-  config_params->enable_viewport_images = kEnableViewportImages.Get();
-  config_params->attach_page_title_and_url_to_suggest_requests =
-      kAttachPageTitleAndUrlToSuggestRequest.Get();
+  config_params->enable_viewport_images = true;
+  config_params->attach_page_title_and_url_to_suggest_requests = false;
   return config_params;
 }
 
@@ -214,11 +213,6 @@ const base::FeatureParam<bool> kShowComposeboxImageSuggestions(
     &kNtpComposebox,
     "NtpComposeboxShowComposeboxImageSuggestions",
     true);
-
-const base::FeatureParam<bool> kAttachPageTitleAndUrlToSuggestRequest(
-    &kNtpComposebox,
-    "AttachPageTitleAndUrlToSuggestRequest",
-    false);
 
 const base::FeatureParam<bool> kShowContextMenu(&kNtpComposebox,
                                                 "NtpComposeboxShowContextMenu",
@@ -240,9 +234,6 @@ const base::FeatureParam<bool> kEnableEphemeralContextMenuDescription(
     &kNtpComposebox,
     "EnableEphemeralContextMenuDescription",
     false);
-const base::FeatureParam<bool> kEnableViewportImages(&kNtpComposebox,
-                                                     "EnableViewportImages",
-                                                     true);
 
 const base::FeatureParam<bool> kShowModelPicker(&kNtpComposebox,
                                                 "NtpComposeboxShowModelPicker",
