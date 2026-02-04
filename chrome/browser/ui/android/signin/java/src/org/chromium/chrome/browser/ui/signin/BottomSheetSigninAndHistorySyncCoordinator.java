829157b270dac6e3fb07f6857c2d4a90824b8d75
Anton Goncharenko <agonch@google.com>
2026-02-04 09:37:58 -0800

    Reland "[Signin][Android] Crash signin flow if started with an incognito profile"
    This is a reland of commit 1438dfe7b998943f831d8c3a6bc1184f31c07a04
    
    This conflicted with change-Id:
    I733958465c97fc416ca5a40fa0ec4d876707471a (test was changed at the same
    time), this::getBottomSheetController should be used instead of
    getBottomSheetController()
    
    Original change's description:
    > [Signin][Android] Crash signin flow if started with an incognito profile
    >
    > Ensures that the sign-in flow crashes if initiated with an incognito
    > profile. This change is gated behind the ENABLE_SEAMLESS_SIGNIN feature
    > flag.
    >
    > Discovered during the sign-in API migration for the NTP Avatar.
    >
    > Bug: 469772349
    > Change-Id: Ice164ce4d37a899743b553041bb9699e8d641ce7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7531746
    > Reviewed-by: Menghan Yang <myuu@google.com>
    > Commit-Queue: Anton Goncharenko <agonch@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1579337}
    
    Bug: 469772349
    Change-Id: I0ced1455cff23bb152ac835b42144713053a3505
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7544281
    Commit-Queue: Anton Goncharenko <agonch@google.com>
    Reviewed-by: Menghan Yang <myuu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1579539}

--- chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/BottomSheetSigninAndHistorySyncCoordinator.java
+++ chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/BottomSheetSigninAndHistorySyncCoordinator.java
@@ -291,7 +291,7 @@ public class BottomSheetSigninAndHistorySyncCoordinator extends SigninAndHistory
 
         mConfig = config;
         mDidShowSigninStep = false;
-        mProfileSupplier.onAvailable(this::onProfileAvailable);
+        mProfileSupplier.runSyncOrOnAvailable(this::onProfileAvailable);
     }
 
     /**
@@ -474,6 +474,7 @@ public class BottomSheetSigninAndHistorySyncCoordinator extends SigninAndHistory
 
     private void onProfileAvailable(ProfileProvider profileProvider) {
         mProfile = assumeNonNull(profileProvider.getOriginalProfile());
+        validateProfile(mProfile);
         AccountManagerFacadeProvider.getInstance()
                 .getAccounts()
                 .then(
@@ -481,6 +482,14 @@ public class BottomSheetSigninAndHistorySyncCoordinator extends SigninAndHistory
                             mFlowInitialized = true;
                             finishLoadingAndSelectSigninFlow(accounts);
                         });
+    }
+
+    private void validateProfile(Profile profile) {
+        if (profile.isOffTheRecord()
+                && SigninFeatureMap.isEnabled(SigninFeatures.ENABLE_SEAMLESS_SIGNIN)) {
+            throw new IllegalStateException(
+                    "This sign-in flow should not be initiated with an incognito profile.");
+        }
     }
 
     private void finishLoadingAndSelectSigninFlow(List<AccountInfo> accounts) {
