9f3a14d832a39c6a8d0569f552e80f31afaa0b90
Fiaz Muhammad <mfiaz@google.com>
2026-02-04 09:32:29 -0800

    [Persist] Unregister StorageLoadedData Observers flexibly
    - Add observer method to notify when a StorageLoadedData is destroyed.
    
    Bug: 476447678
    Change-Id: If3481a675538269c054c836f6cff38ff130bd168
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7545997
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Fiaz Muhammad <mfiaz@google.com>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1579530}

--- chrome/browser/tab/storage_restore_orchestrator.h
+++ chrome/browser/tab/storage_restore_orchestrator.h
@@ -52,6 +52,7 @@ class StorageRestoreOrchestrator : public TabCollectionObserver {
     explicit ObserverImpl(StorageRestoreOrchestrator* orchestrator);
     ~ObserverImpl() override;
     void OnChildRejected(StorageId parent) override;
+    void OnDestroyed() override;
 
    private:
     raw_ptr<StorageRestoreOrchestrator> orchestrator_;
@@ -61,6 +62,7 @@ class StorageRestoreOrchestrator : public TabCollectionObserver {
   void OnAddChildCollection(const TabCollection::NodeHandle& handle);
   void MaybeAddModifiedParent(const StorageId& id,
                               std::optional<TabCollectionHandle> handle);
+  void OnDataDestroyed();
 
   // Represents default observer methods.
   CollectionStorageObserver default_observer_;
@@ -78,6 +80,8 @@ class StorageRestoreOrchestrator : public TabCollectionObserver {
   // Used to keep track of parents that have had their children vector modified.
   absl::flat_hash_map<StorageId, std::optional<TabCollectionHandle>>
       modified_parents_;
+
+  bool is_data_observer_registered_;
 };
 
 }  // namespace tabs
