189c5dc9fb59752396da7595e3c8dc96d48316e5
Louis Romero <lpromero@google.com>
2026-02-04 08:11:51 -0800

    Make the placeholder and text view match
    Prior to this CL, there was a 1pt difference between the placeholder and
    the text in the text view. Now they match.
    
    Before: https://drive.google.com/file/d/1wApIV3rG-xDZz_ViPkzt6ckP3b4aHsoN
     After: https://drive.google.com/file/d/1rKuCVCN35kD1LkrhAYTEQ_-1hl79xRJ5
    
    Fixed: 474222266
    Change-Id: I13c4365b761a4ff44ae11b97083a095730d19ae2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7531351
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1579480}

--- ios/chrome/browser/omnibox/ui/omnibox_container_view.mm
+++ ios/chrome/browser/omnibox/ui/omnibox_container_view.mm
@@ -51,7 +51,7 @@ const CGFloat kLeadingImageTrailingMarginLensOverlay = 9;
 const CGFloat kLeadingImageSizeAIM = 22;
 // The leading image margins when presented in composebox.
 const CGFloat kLeadingImageLeadingMarginAIM = 7;
-const CGFloat kLeadingImageTrailingMarginAIM = 11;
+const CGFloat kLeadingImageTrailingMarginAIM = 10;
 
 /// Space between the clear button and the edge of the omnibox.
 const CGFloat kTextInputViewClearButtonTrailingOffset = 4;
