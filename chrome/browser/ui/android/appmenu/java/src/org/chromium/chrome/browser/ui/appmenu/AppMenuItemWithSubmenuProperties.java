c98845f49a41fd75386e4c44ab959d6571ce82c2
Masa Fujita <massan@google.com>
2026-02-03 20:12:56 -0800

    [Entry Point] Fix crash with #submenus-in-appmenu
    This CL fixes a crash that happened when the user tapped on the three
    dot menu icon when #submenus-in-appmenu flag set to true.
    
    Bug: 480795425
    Change-Id: Ia5903d3e54291a3b85c3cc1db8410f8dee67f2fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7538895
    Commit-Queue: Masa Fujita <massan@google.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Reviewed-by: Eric Lok <lokeric@google.com>
    Cr-Commit-Position: refs/heads/main@{#1579222}

--- chrome/browser/ui/android/appmenu/java/src/org/chromium/chrome/browser/ui/appmenu/AppMenuItemWithSubmenuProperties.java
+++ chrome/browser/ui/android/appmenu/java/src/org/chromium/chrome/browser/ui/appmenu/AppMenuItemWithSubmenuProperties.java
@@ -11,6 +11,7 @@ import static org.chromium.chrome.browser.ui.appmenu.AppMenuItemProperties.HIGHL
 import static org.chromium.chrome.browser.ui.appmenu.AppMenuItemProperties.HOVER_LISTENER;
 import static org.chromium.chrome.browser.ui.appmenu.AppMenuItemProperties.ICON;
 import static org.chromium.chrome.browser.ui.appmenu.AppMenuItemProperties.ICON_COLOR_RES;
+import static org.chromium.chrome.browser.ui.appmenu.AppMenuItemProperties.ICON_NO_TINT;
 import static org.chromium.chrome.browser.ui.appmenu.AppMenuItemProperties.ICON_SHOW_BADGE;
 import static org.chromium.chrome.browser.ui.appmenu.AppMenuItemProperties.KEY_LISTENER;
 import static org.chromium.chrome.browser.ui.appmenu.AppMenuItemProperties.MANAGED;
@@ -51,6 +52,7 @@ public class AppMenuItemWithSubmenuProperties {
                 ICON,
                 ICON_COLOR_RES,
                 ICON_SHOW_BADGE,
+                ICON_NO_TINT,
                 POSITION,
                 HOVER_LISTENER,
                 KEY_LISTENER,
