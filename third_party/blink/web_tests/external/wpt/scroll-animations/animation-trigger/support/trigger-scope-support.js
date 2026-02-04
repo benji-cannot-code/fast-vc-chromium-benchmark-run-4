2012bf0a02e589ae14209449bb94ce81ec6d1f97
David Awogbemila <awogbemila@chromium.org>
2026-02-04 13:02:13 -0800

    [animation-trigger] Prioritize ancestors in trigger name search
    This implements the resolution from [1] in which, when looking for a
    name referred to by animation-trigger, we first check whether the name
    is declared within the ancestry of the animating element before looking
    outside its ancestry, where we prioritize the tree order.
    
    [1] https://github.com/w3c/csswg-drafts/issues/13364#issuecomment-3812817974
    
    Bug: 390314945
    Change-Id: I1cd3f02425a59ba1ee16b880830e0c6f9f2994d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7539516
    Commit-Queue: David Awogbemila <awogbemila@chromium.org>
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1579666}

--- third_party/blink/web_tests/external/wpt/scroll-animations/animation-trigger/support/trigger-scope-support.js
+++ third_party/blink/web_tests/external/wpt/scroll-animations/animation-trigger/support/trigger-scope-support.js
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 async function assert_playstate_and_current_time(target_id, animation, play_state) {
   // The animation might start on a different user-agent thread and need
   // a moment to get currentTime up to date.
+  // TODO: This is incorrect use of waitForCompositorReady. We should remove it.
   await waitForCompositorReady();
 
   assert_equals(animation.playState, play_state,
@@ -18,3 +19,8 @@ async function assert_playstate_and_current_time(target_id, animation, play_stat
       `animation on ${target_id} has currentTime == 0.`);
   }
 }
+
+const assert_element_animation_state = (element, play_state) => {
+  assert_equals(element.getAnimations()[0].playState, play_state,
+      `animation on ${element.id} has playState ${play_state}.`);
+};
