dc497a96831432f32ecbc70853efcb8034f59ae7
Dibyajyoti Pal <dibyapal@google.com>
2026-02-04 10:05:14 -0800

    [PWA/App Migration] Ensure UX consistency for forced migrations
    This CL implements how forced migrations are applied during the last
    stage of an app migration. For forced migrations, the source app's name
    and icons are set for the destination app, which is implemented by
    1. Enforcing that the source app and destination app are still connected
       by the manifest id.
    2. Setting the source app's name and icon metadata in the destination
       web app.
    3. Copying over the icons from the source app to the destination app,
       newly introduced in WebAppIconManager, and only passkeyed to be
       called by the ApplyManifestMigrationCommand, as that can be  potentially destructive.
    4. Triggering OS integration on the new metadata.
    
    Also includes tests that verify that this behavior works for destination
    apps installed with or without OS integration.
    
    Bug: 465762477
    Change-Id: I8c306f48dbac86266c66380a05d49cc1ceacd4d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7540074
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1579567}

--- chrome/browser/web_applications/web_app_command_scheduler.h
+++ chrome/browser/web_applications/web_app_command_scheduler.h
@@ -92,6 +92,10 @@ struct WebAppIconDiagnosticResult;
 struct WebAppInstallInfo;
 enum class FetchManifestAndUpdateResult;
 
+namespace proto {
+enum WebAppMigrationBehavior : int;
+}  // namespace proto
+
 #if BUILDFLAG(IS_CHROMEOS)
 class CleanupBundleCacheSuccess;
 class CleanupBundleCacheError;
@@ -735,6 +739,7 @@ class WebAppCommandScheduler {
   void ApplyManifestMigration(
       const webapps::AppId& source_app_id,
       const webapps::AppId& destination_app_id,
+      const proto::WebAppMigrationBehavior migration_behavior,
       std::unique_ptr<ScopedKeepAlive> keep_alive,
       std::unique_ptr<ScopedProfileKeepAlive> profile_keep_alive,
       ApplyManifestMigrationResultCallback callback,
