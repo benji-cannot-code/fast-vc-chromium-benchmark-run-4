26d1a14356ba45600d8a7455181a372e7919c66a
Alexis Wu <alexiswu@google.com>
2026-02-03 16:17:11 -0800

    [GlicSettings] Add Glic Setting Page Behind Flag
    Screenshot1: https://screenshot.googleplex.com/6V4Tau53Y3mbEso
    Screenshot2: https://screenshot.googleplex.com/3MJ69mHscvzpdkv.png
    
    Adding Search index is in another CL. (crbug.com/479614674)
    
    Bug: 478005128
    Change-Id: I4c9883eeb425028a2c791405cd0f7c278a318d5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7518844
    Commit-Queue: Alexis Wu <alexiswu@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Reviewed-by: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1579122}

---
+++ chrome/browser/glic/android/java/src/org/chromium/chrome/browser/glic/GlicSettings.java
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.glic;
+
+import android.os.Bundle;
+
+import org.chromium.base.supplier.MonotonicObservableSupplier;
+import org.chromium.base.supplier.ObservableSuppliers;
+import org.chromium.base.supplier.SettableMonotonicObservableSupplier;
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
+import org.chromium.chrome.browser.settings.ChromeBaseSettingsFragment;
+import org.chromium.components.browser_ui.settings.SettingsFragment;
+import org.chromium.components.browser_ui.settings.SettingsUtils;
+
+/** Fragment for GLIC related configurations to Chrome. */
+@NullMarked
+public class GlicSettings extends ChromeBaseSettingsFragment {
+    private final SettableMonotonicObservableSupplier<String> mPageTitle =
+            ObservableSuppliers.createMonotonic();
+
+    @Override
+    public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
+        SettingsUtils.addPreferencesFromResource(this, R.xml.glic_settings);
+    }
+
+    @Override
+    public MonotonicObservableSupplier<String> getPageTitle() {
+        return mPageTitle;
+    }
+
+    @Override
+    public @SettingsFragment.AnimationType int getAnimationType() {
+        return SettingsFragment.AnimationType.PROPERTY;
+    }
+
+    // TODO(alexiswu): Add setting search index.
+
+}
