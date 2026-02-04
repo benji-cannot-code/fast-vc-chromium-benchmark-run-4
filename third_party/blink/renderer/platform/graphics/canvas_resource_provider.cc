7519b48c3cdb5f687e83db964c7f1193a7bbccd5
Colin Blundell <blundell@chromium.org>
2026-02-03 23:59:46 -0800

    [Blink] Turn CanvasResourceProviderSI conditional into CHECK
    This conditional handles the case where CreateSkSurface() is called when
    the CRPSI is accelerated. It is not supposed to occur, and indeed it
    does not. CreateSkSurface() is called only from GetSkSurface(), which is
    called from the following:
    
    * Else clauses for if (is_accelerated_) [1][4]
    * Under `is_software_`, which means that `is_accelerated_` is false [2][3]
    * Under a CHECK(!is_accelerated_) [5]
    * In Unaccelerated*() methods that are called only when
      `is_accelerated_` is false [6][7][8]
    * In a method that is called only in tests [9]
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc;l=545-548;drc=304ce93a12d948c378c103d4c0580f9a30a318f3
    [2] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc;l=733-744;drc=304ce93a12d948c378c103d4c0580f9a30a318f3?q=CanvasResourceProviderSharedImage::CreateSk&ss=chromium
    [3] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc;l=776;drc=304ce93a12d948c378c103d4c0580f9a30a318f3?q=CanvasResourceProviderSharedImage::CreateSk&ss=chromium
    [4] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc;l=785;drc=304ce93a12d948c378c103d4c0580f9a30a318f3?q=CanvasResourceProviderSharedImage::CreateSk&ss=chromium
    [5] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc;l=1457;drc=304ce93a12d948c378c103d4c0580f9a30a318f3?q=CanvasResourceProviderSharedImage::CreateSk&ss=chromium
    [6] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc;l=1554;drc=304ce93a12d948c378c103d4c0580f9a30a318f3?q=CanvasResourceProviderSharedImage::CreateSk&ss=chromium
    [7] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc;l=1632;drc=304ce93a12d948c378c103d4c0580f9a30a318f3?q=CanvasResourceProviderSharedImage::CreateSk&ss=chromium
    [8] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc;l=1683;drc=304ce93a12d948c378c103d4c0580f9a30a318f3?q=CanvasResourceProviderSharedImage::CreateSk&ss=chromium
    [9] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc;l=1714;drc=304ce93a12d948c378c103d4c0580f9a30a318f3;bpv=1;bpt=1
    
    Bug: 391648152
    Change-Id: I13812dd1ec3ec85751fcff9b9f18f90183ff1a48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7541140
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1579265}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
@@ -959,6 +959,9 @@ void CanvasResourceProviderSharedImage::RasterRecord(
 
 sk_sp<SkSurface> CanvasResourceProviderSharedImage::CreateSkSurface() const {
   TRACE_EVENT0("blink", "CanvasResourceProviderSharedImage::CreateSkSurface");
+
+  CHECK(!IsAccelerated());
+
   if (is_software_) {
     const auto props = GetSkSurfaceProps();
     const auto info = SkImageInfo::Make(
@@ -972,13 +975,6 @@ sk_sp<SkSurface> CanvasResourceProviderSharedImage::CreateSkSurface() const {
   }
 
   const auto props = GetSkSurfaceProps();
-  if (is_accelerated_) {
-    // No longer supported post-OOP-C.
-    // TODO(crbug.com/391648152): Replace this conditional with a
-    // CHECK.
-    return nullptr;
-  }
-
   // For software raster path, we render into cpu memory managed internally
   // by SkSurface and copy the rendered results to the GMB before dispatching
   // it to the display compositor.
