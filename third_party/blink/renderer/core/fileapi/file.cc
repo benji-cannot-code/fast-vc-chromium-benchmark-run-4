964edce95881353cb28abc67d82bedc74491ebfe
Fredrik Söderquist <fs@opera.com>
2026-02-04 03:01:19 -0800

    Use StringView in GetContentTypeFromFileName() helper
    Change MIMETypeRegistry::GetMIMETypeForExtension() to take a
    StringView to support this.
    
    Bug: 473854537
    Change-Id: Icb9f0fd60c265f784e1d0a6ee5352eade5198ff0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7539444
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1579319}

--- third_party/blink/renderer/core/fileapi/file.cc
+++ third_party/blink/renderer/core/fileapi/file.cc
@@ -50,21 +50,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-static String GetContentTypeFromFileName(const String& name,
+static String GetContentTypeFromFileName(const StringView& name,
                                          File::ContentTypeLookupPolicy policy) {
-  String type;
-  wtf_size_t index = name.ReverseFind('.');
+  wtf_size_t index = name.rfind('.');
   if (index != kNotFound) {
+    StringView extension = name.substr(index + 1);
     if (policy == File::kWellKnownContentTypes) {
-      type = MIMETypeRegistry::GetWellKnownMIMETypeForExtension(
-          name.Substring(index + 1));
+      return MIMETypeRegistry::GetWellKnownMIMETypeForExtension(extension);
     } else {
       DCHECK_EQ(policy, File::kAllContentTypes);
-      type =
-          MIMETypeRegistry::GetMIMETypeForExtension(name.Substring(index + 1));
+      return MIMETypeRegistry::GetMIMETypeForExtension(extension);
     }
   }
-  return type;
+  return String();
 }
 
 static scoped_refptr<BlobDataHandle> CreateBlobDataHandleForFileWithType(
@@ -192,8 +190,8 @@ File* File::CreateForFileSystemFile(ExecutionContext& context,
                                     const KURL& url,
                                     const FileMetadata& metadata,
                                     UserVisibility user_visibility) {
-  String content_type = GetContentTypeFromFileName(
-      url.GetPath().ToString(), File::kWellKnownContentTypes);
+  String content_type =
+      GetContentTypeFromFileName(url.GetPath(), File::kWellKnownContentTypes);
   // RegisterBlob doesn't take nullable strings.
   if (content_type.IsNull()) {
     content_type = g_empty_string;
