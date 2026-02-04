783e378392059edd3d9e7a558ec73a1bd7442d05
Emily Shack <emshack@chromium.org>
2026-02-04 11:37:00 -0800

    [ComboButton] Add combo button class
    Creates the TabStripComboButton class and refactors the vertical tab
    strip to make use of it. There should be no functionality changes as of
    this CL.
    
    Bug: 481393131
    Change-Id: Ia42973ffaf596df4e3eb2713adb656d983c410c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7542893
    Reviewed-by: Alison Gale <agale@chromium.org>
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1579618}

--- chrome/browser/ui/views/tabs/vertical/vertical_tab_strip_top_container.h
+++ chrome/browser/ui/views/tabs/vertical/vertical_tab_strip_top_container.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/layout/delegating_layout_manager.h"
 #include "ui/views/view.h"
 
+class TabStripComboButton;
 class TabStripFlatEdgeButton;
 class BrowserWindowInterface;
 
@@ -51,10 +52,11 @@ class VerticalTabStripTopContainer : public views::View,
   views::LabelButton* AddTopContainerChildButtonFor(
       actions::ActionId action_id);
   // Creates FlatEdgeButton (Tab Groups & Tab Search).
-  TabStripFlatEdgeButton* AddFlatEdgeChildButtonFor(
+  std::unique_ptr<TabStripFlatEdgeButton> CreateFlatEdgeButtonFor(
       actions::ActionId action_id);
 
-  TabStripFlatEdgeButton* GetTabSearchButton() { return tab_search_button_; }
+  TabStripComboButton* GetComboButton();
+  TabStripFlatEdgeButton* GetTabSearchButton();
   views::LabelButton* GetCollapseButton() { return collapse_button_; }
 
   bool IsPositionInWindowCaption(const gfx::Point& point);
@@ -70,18 +72,15 @@ class VerticalTabStripTopContainer : public views::View,
  private:
   void ShowEverythingMenu();
 
+  void UpdateComboButtonVisibility();
+
   void OnCollapsedStateChanged(
       tabs::VerticalTabStripStateController* controller);
-
-  // This method updates the flat edges of the tab search and tab group buttons
-  // according to the collapsed state.
-  void UpdateButtonStyles(tabs::VerticalTabStripStateController* controller);
 
   raw_ptr<tabs::VerticalTabStripStateController> state_controller_ = nullptr;
   raw_ptr<actions::ActionItem> root_action_item_ = nullptr;
   raw_ptr<BrowserWindowInterface> browser_ = nullptr;
-  raw_ptr<TabStripFlatEdgeButton> tab_search_button_ = nullptr;
-  raw_ptr<TabStripFlatEdgeButton> tab_group_button_ = nullptr;
+  raw_ptr<TabStripComboButton> combo_button_ = nullptr;
   raw_ptr<views::LabelButton> collapse_button_ = nullptr;
 
   raw_ptr<views::MenuButtonController> everything_menu_controller_ = nullptr;
