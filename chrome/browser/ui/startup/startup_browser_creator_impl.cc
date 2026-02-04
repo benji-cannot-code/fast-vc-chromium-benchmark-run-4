db5625b3fbbe78d166171f3c7d400722fb1592cc
Muhammad Salmaan <musalmaan@chromium.org>
2026-02-04 13:21:36 -0800

    [Search Integrity] Add SearchIntegrityFactory skeleton
    This CL introduces the SearchIntegrityFactory, a
    ProfileKeyedServiceFactory that will be responsible for managing search
    integrity checks.
    
    Bug: 481441610
    Change-Id: Ib4785ad67ec440b64b9f447b1b1693105397a394
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7539491
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Muhammad Salmaan <musalmaan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1579679}

--- chrome/browser/ui/startup/startup_browser_creator_impl.cc
+++ chrome/browser/ui/startup/startup_browser_creator_impl.cc
@@ -89,6 +89,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
+#include "chrome/browser/ui/views/search_integrity/search_integrity_factory.h"
 #include "chrome/browser/ui/webui/whats_new/whats_new_fetcher.h"
 #endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
 
@@ -519,8 +520,9 @@ void StartupBrowserCreatorImpl::DetermineURLsAndLaunch(
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
   // Check for DSE integrity if flag is enabled.
   if (base::FeatureList::IsEnabled(features::kDseIntegrity)) {
-    // TODO(466065123): The controller will instantiate the model, check the
-    // pref, and show the notification if needed.
+    search_integrity::SearchIntegrityFactory::GetForProfile(profile_);
+    // TODO(481450191): Add search integrity call for checking against search
+    // allowlist.
   }
 #endif
 }
