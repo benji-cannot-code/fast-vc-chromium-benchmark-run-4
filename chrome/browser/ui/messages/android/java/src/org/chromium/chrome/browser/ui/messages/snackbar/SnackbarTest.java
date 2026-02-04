5ae57eb3c96091693dcf4a030966b8d3924e01b5
Sky Malice <skym@chromium.org>
2026-02-04 14:31:39 -0800

    Disable flaky testPersistentQueueStackOrder.
    Bug: 458057621
    Change-Id: I26d93bab05a2e2f03b1be0b9fb4f0bac03431e45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7546204
    Auto-Submit: Sky Malice <skym@chromium.org>
    Reviewed-by: Jenna Himawan <jhimawan@google.com>
    Owners-Override: Sky Malice <skym@chromium.org>
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1579720}

--- chrome/browser/ui/messages/android/java/src/org/chromium/chrome/browser/ui/messages/snackbar/SnackbarTest.java
+++ chrome/browser/ui/messages/android/java/src/org/chromium/chrome/browser/ui/messages/snackbar/SnackbarTest.java
@@ -195,6 +195,7 @@ public class SnackbarTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/458057621")
     public void testPersistentQueueStackOrder() {
         final Snackbar stackbar =
                 Snackbar.make(
