5f6e2647b8d8685244d1975aa021c2b9073b002d
Chris Fredrickson <cfredric@chromium.org>
2026-02-04 11:46:13 -0800

    Convert CookiePrefix to enum class; remove distinct LAST enumerator
    COOKIE_PREFIX_LAST is safe to remove despite the "do not reorder or
    remove values" comment, because it was only ever used as the `enum_size`
    argument to `UmaHistogramEnumeration`, which was required to be strictly
    greater than every sample. I.e., COOKIE_PREFIX_LAST does not occur in
    the histogram data.
    
    Change-Id: Icd76b551070149276dca480cbbc5805a015d4b8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7545502
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1579633}

--- net/cookies/canonical_cookie.cc
+++ net/cookies/canonical_cookie.cc
@@ -426,8 +426,7 @@ std::unique_ptr<CanonicalCookie> CanonicalCookie::Create(
       cookie_util::IsCookiePrefixValid(prefix, url, parsed_cookie);
 
   if (collect_metrics) {
-    base::UmaHistogramEnumeration("Cookie.CookiePrefix.Subsampled", prefix,
-                                  COOKIE_PREFIX_LAST);
+    base::UmaHistogramEnumeration("Cookie.CookiePrefix.Subsampled", prefix);
   }
 
   if (parsed_cookie.Name() == "") {
@@ -547,7 +546,7 @@ std::unique_ptr<CanonicalCookie> CanonicalCookie::Create(
 
     // Check for "__" prefixed names, excluding the cookie prefixes.
     bool name_prefixed_with_underscores =
-        (prefix == COOKIE_PREFIX_NONE) &&
+        (prefix == CookiePrefix::kNone) &&
         parsed_cookie.Name().starts_with("__");
 
     base::UmaHistogramBoolean("Cookie.DoubleUnderscorePrefixedName.Subsampled",
@@ -1081,16 +1080,15 @@ CanonicalCookie::IsCanonicalForFromStorage() const {
                                         SecureAttribute(), IsHttpOnly(),
                                         Domain(), Path())) {
     switch (prefix) {
-      case COOKIE_PREFIX_HOST:
+      case CookiePrefix::kHost:
         return Fail(CanonicalizationFailure::kInvalidHostPrefix);
-      case COOKIE_PREFIX_SECURE:
+      case CookiePrefix::kSecure:
         return Fail(CanonicalizationFailure::kInvalidSecurePrefix);
-      case COOKIE_PREFIX_HTTP:
+      case CookiePrefix::kHttp:
         return Fail(CanonicalizationFailure::kInvalidHttpPrefix);
-      case COOKIE_PREFIX_HOSTHTTP:
+      case CookiePrefix::kHostHttp:
         return Fail(CanonicalizationFailure::kInvalidHostHttpPrefix);
-      case COOKIE_PREFIX_NONE:
-      case COOKIE_PREFIX_LAST:
+      case CookiePrefix::kNone:
         break;
     }
   }
