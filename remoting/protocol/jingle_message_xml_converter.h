96eb9bb3edecbe9b1bf5721bc227795ff9c37bbc
Joe Downing <joedow@google.com>
2026-02-04 07:48:20 -0800

    Add converters for JingleTransportInfo
    Bug: 359620500
    Change-Id: I2615db91812ddf72776be61cc4c1902513cb4f0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7539229
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1579463}

--- remoting/protocol/jingle_message_xml_converter.h
+++ remoting/protocol/jingle_message_xml_converter.h
@@ -17,6 +17,7 @@ namespace remoting::protocol {
 
 struct IceTransportInfo;
 class JingleMessage;
+struct JingleTransportInfo;
 
 // Converts between JingleMessage and its XML representation.
 std::unique_ptr<jingle_xmpp::XmlElement> JingleMessageToXml(
@@ -27,6 +28,12 @@ bool JingleMessageFromXml(const jingle_xmpp::XmlElement* stanza,
 
 // Helper to check if an XML element represents a Jingle message.
 bool IsJingleMessage(const jingle_xmpp::XmlElement* stanza);
+
+// Converts between JingleTransportInfo and its XML representation.
+std::unique_ptr<jingle_xmpp::XmlElement> JingleTransportInfoToXml(
+    const JingleTransportInfo& transport);
+bool JingleTransportInfoFromXml(const jingle_xmpp::XmlElement* element,
+                                JingleTransportInfo* transport);
 
 // Converts between IceTransportInfo and its XML representation.
 std::unique_ptr<jingle_xmpp::XmlElement> IceTransportInfoToXml(
