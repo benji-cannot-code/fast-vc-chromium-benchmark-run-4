bb796cd1bb618e611a0bf861b7c379ed7519fff3
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2026-02-04 09:59:31 -0800

    EICP: ExtensionService::CheckManagementPolicy() when policy changes
    Whenever ExtensionInstallPolicyService receives a fresh set of
    policies, notify ExtensionService via an observer. In the observer,
    ExtensionService calls CheckManagementPolicy() to disable/enable
    extensions affected by the new policy.
    
    Compile-Size: false positive
    Bug: 477545526
    Change-Id: I754da00c247da54cfeb42fcb6bf26f5e2dcbbff3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7533698
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1579559}

--- chrome/browser/extensions/extension_service.cc
+++ chrome/browser/extensions/extension_service.cc
@@ -61,6 +61,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/updater/extension_updater.h"
 #include "chrome/browser/google/google_brand.h"
 #include "chrome/browser/lifetime/termination_notification.h"
+#include "chrome/browser/policy/cloud/extension_install_policy_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/webui/extensions/extension_icon_source.h"
@@ -252,6 +253,13 @@ ExtensionService::ExtensionService(
 
   ExtensionManagementFactory::GetForBrowserContext(profile_)->AddObserver(this);
 
+  if (auto* extension_install_policy_service =
+          policy::ExtensionInstallPolicyServiceFactory::GetForBrowserContext(
+              profile_)) {
+    extension_install_policy_observation_.Observe(
+        extension_install_policy_service);
+  }
+
   if (autoupdate_enabled) {
     // Initialize and enable the ExtensionUpdater.
     updater_->InitAndEnable(
@@ -299,6 +307,7 @@ ExtensionService::~ExtensionService() {
 void ExtensionService::Shutdown() {
   delayed_install_manager_ = nullptr;
   cws_info_service_observation_.Reset();
+  extension_install_policy_observation_.Reset();
   ExtensionManagementFactory::GetForBrowserContext(profile())->RemoveObserver(
       this);
   external_install_manager_->Shutdown();
@@ -769,6 +778,10 @@ void ExtensionService::OnExtensionManagementSettingsChanged() {
       kAllowUnpublishedExtensions) {
     CWSInfoService::Get(profile_)->CheckAndMaybeFetchInfo();
   }
+}
+
+void ExtensionService::OnExtensionInstallPolicyUpdated() {
+  CheckManagementPolicy();
 }
 
 bool ExtensionService::FinishDelayedInstallationIfReady(
