46d11aaca6ca03507e4fed9de14dcc67b7693647
Sophie Chang <sophiechang@chromium.org>
2026-02-03 17:10:21 -0800

    Migrate lens tab contextualization controller to use a lighter-weight method for page context eligibility
    This avoids the copy of the APC especially since lens doesn't need it
    
    Bug: 480993676
    Change-Id: I2248214f9199eb5581e286cb2d2d98602d2ab22f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7543096
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Cr-Commit-Position: refs/heads/main@{#1579153}

--- chrome/browser/page_content_annotations/page_content_annotations_service_browsertest.cc
+++ chrome/browser/page_content_annotations/page_content_annotations_service_browsertest.cc
@@ -1723,6 +1723,8 @@ IN_PROC_BROWSER_TEST_F(
   // Should have cached data for page since there was an observer registered.
   ASSERT_TRUE(service->GetExtractedPageContentAndEligibilityForPage(
       web_contents->GetPrimaryPage()));
+  ASSERT_TRUE(service->GetServerUploadEligibilityForPage(
+      web_contents->GetPrimaryPage()));
 
   service->RemoveObserver(&observer);
 
@@ -1732,6 +1734,8 @@ IN_PROC_BROWSER_TEST_F(
 
   // Make sure cached content is cleared with a new navigation.
   ASSERT_FALSE(service->GetExtractedPageContentAndEligibilityForPage(
+      web_contents->GetPrimaryPage()));
+  ASSERT_FALSE(service->GetServerUploadEligibilityForPage(
       web_contents->GetPrimaryPage()));
 }
 
--- components/page_content_annotations/content/page_content_extraction_service.h
+++ components/page_content_annotations/content/page_content_extraction_service.h
@@ -40,6 +40,7 @@ class OSCryptAsync;
 
 namespace page_content_annotations {
 
+class AnnotatedPageContentRequest;
 struct ExtractedPageContentResult;
 class PageContentCache;
 class PageContentCacheHandler;
@@ -80,6 +81,12 @@ class PageContentExtractionService : public KeyedService,
   virtual std::optional<ExtractedPageContentResult>
   GetExtractedPageContentAndEligibilityForPage(content::Page& page);
 
+  // Returns whether the cached APC for `page` is eligible for server upload.
+  // Will return nullopt if not available.
+  // Virtual for testing.
+  virtual std::optional<bool> GetServerUploadEligibilityForPage(
+      content::Page& page);
+
   // Called when a tab is closed.
   void OnTabClosed(int64_t tab_id);
 
@@ -114,7 +121,7 @@ class PageContentExtractionService : public KeyedService,
       const std::vector<uint8_t>& screenshot_data,
       std::optional<int> tab_id);
 
-  std::optional<ExtractedPageContentResult> GetCachedContentsFromWebContents(
+  AnnotatedPageContentRequest* GetAnnotatedPageContentRequestFromWebContents(
       content::WebContents* web_contents);
 
   base::ObserverList<Observer> observers_;
