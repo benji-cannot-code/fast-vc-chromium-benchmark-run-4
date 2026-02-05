3a5a15bbdfa6b9164cb49a1c387f41cb2f8c77d2
Vince Lugli <lugli@google.com>
2026-02-05 10:36:45 -0800

    [VerticalTabs][Glic] GlicButtonInterface and basic template class.
    Initial base class for GlicButton<T>. Creating a GlicButtonInterface in
    order to eventually update all the call sites of the tab strip's
    GlicButton to GlicButtonInterface. Initial functions are what I've see
    are the required functions for non-tab strip call sites to function.
    
    b/c/ui/views/tabs/glic/glic_and_actor_buttons_container.cc uses
    GlicButton as a View.
    
    Change-Id: I3b8b92f8dde66f223ebfff33fe0999f246edffe2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7543035
    Commit-Queue: Vince Lugli <lugli@google.com>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1580259}

---
+++ chrome/browser/ui/views/glic/glic_button_interface.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_VIEWS_GLIC_GLIC_BUTTON_INTERFACE_H_
+#define CHROME_BROWSER_UI_VIEWS_GLIC_GLIC_BUTTON_INTERFACE_H_
+
+#include "ui/views/view.h"
+
+class BrowserWindowInterface;
+
+namespace views {
+class LabelButton;
+}
+
+namespace glic {
+class GlicButtonInterface {
+ public:
+  static views::LabelButton* FromBrowser(BrowserWindowInterface* browser);
+};
+}  // namespace glic
+
+#endif  // CHROME_BROWSER_UI_VIEWS_GLIC_GLIC_BUTTON_INTERFACE_H_
