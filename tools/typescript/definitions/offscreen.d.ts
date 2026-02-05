e631c9b7a1f523c2fa419cd085b7d884a688dc67
Tim Judkins <tjudkins@chromium.org>
2026-02-05 11:12:31 -0800

    [Extensions WebIDL] Convert offscreen to WebIDL
    This conversion was largely done using a detailed description of the
    conversion process passed to Gemini CLI. To double check this work, the
    file has also been copied into the converted schemas test to verify no
    functional difference in output.
    
    Also updates a pair of TypeScript files generated from this schema in
    order to give them the new schema filename.
    
    Bug: 452427574
    Change-Id: I76322faf6d4112b22ac8728c454b1b1c793a8b6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7536418
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1580288}

--- tools/typescript/definitions/offscreen.d.ts
+++ tools/typescript/definitions/offscreen.d.ts
@@ -1,13 +1,13 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2024 The Chromium Authors
+// Copyright 2026 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 /**
  * @fileoverview Definitions for chrome.offscreen API
- * Generated from: extensions/common/api/offscreen.idl
+ * Generated from: extensions/common/api/offscreen.webidl
  * run `tools/json_schema_compiler/compiler.py
- * extensions/common/api/offscreen.idl -g ts_definitions` to regenerate.
+ * extensions/common/api/offscreen.webidl -g ts_definitions` to regenerate.
  */
 
 
