585f3dd905961a00e478f567a86e1936ee633287
Ankush Singh <ankushkush@google.com>
2026-02-05 03:08:57 -0800

    Theme pref migration: Add temporary check if pref is registered
    For some reason, on Mac, in some cases, the theme prefs migration logic
    is being queried before the prefs are registered.
    
    This CL avoids the crashes by first checking if the pref is registered.
    This also adds a log dump and a histogram to help investigate this
    better.
    
    Bug: 476288050
    Change-Id: Ib79538b2834c608e2f62a60b9f3053afb78cae41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7545501
    Auto-Submit: Ankush Singh <ankushkush@google.com>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1579992}

--- chrome/browser/themes/theme_syncable_service.cc
+++ chrome/browser/themes/theme_syncable_service.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/auto_reset.h"
 #include "base/base64.h"
 #include "base/containers/fixed_flat_map.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/feature_list.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/observer_list.h"
@@ -183,6 +184,16 @@ const char ThemeSyncableService::kSyncEntityClientTag[] = "current_theme";
 const char ThemeSyncableService::kSyncEntityTitle[] = "Current Theme";
 
 void MigrateSyncingThemePrefsToNonSyncingIfNeeded(PrefService* prefs) {
+  const bool pref_registered =
+      prefs->FindPreference(prefs::kSyncingThemePrefsMigratedToNonSyncing);
+  base::UmaHistogramBoolean("Theme.ThemePrefMigration.PrefRegistered",
+                            pref_registered);
+  // TODO(crbug.com/476288050): Investigate why the pref can not be registered
+  // at this point.
+  if (!pref_registered) {
+    base::debug::DumpWithoutCrashing(FROM_HERE);
+    return;
+  }
   const bool already_migrated =
       prefs->GetBoolean(prefs::kSyncingThemePrefsMigratedToNonSyncing);
   base::UmaHistogramBoolean("Theme.ThemePrefMigration.AlreadyMigrated",
