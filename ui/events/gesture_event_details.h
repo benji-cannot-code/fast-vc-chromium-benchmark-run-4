3eb2ea5e516e360513f29912fe4337a828d3bcb6
Yisi Yu <hellofroser@gmail.com>
2026-02-05 11:30:42 -0800

    Fix UNSAFE_TODO in ui/events/gesture_event_details.cc
    Use base::span instead of memset and memcmp.
    
    Bug: 40285824
    Change-Id: Ib8ed4757e0b7f977aa55828dec6eba7f97d6cdc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7532329
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Auto-Submit: YISI YU <hellofroser@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1580304}

--- ui/events/gesture_event_details.h
+++ ui/events/gesture_event_details.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string.h>
 
 #include "base/check_op.h"
-#include "base/compiler_specific.h"
 #include "ui/events/event_constants.h"
 #include "ui/events/event_latency_metadata.h"
 #include "ui/events/events_base_export.h"
@@ -226,13 +225,7 @@ struct EVENTS_BASE_EXPORT GestureEventDetails {
   }
 
   // Supports comparison over internal structures for testing.
-  bool operator==(const GestureEventDetails& other) const {
-    return type_ == other.type_ &&
-           !UNSAFE_TODO(memcmp(&data_, &other.data_, sizeof(Details))) &&
-           device_type_ == other.device_type_ &&
-           touch_points_ == other.touch_points_ &&
-           bounding_box_ == other.bounding_box_;
-  }
+  bool operator==(const GestureEventDetails& other) const;
 
  private:
   EventType type_;
