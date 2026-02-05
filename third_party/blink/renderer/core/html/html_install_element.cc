afc789674ca95bd9fe1c7c9dde4e45dabe33e5b9
Lia Hiscock <liahiscock@microsoft.com>
2026-02-05 11:12:44 -0800

    [<install> Element] Add WebDx use counter
    * Add DRAFT_InstallElement use counter. Fires on element construction.
    
    WICG proposal - https://aka.ms/InstallElement
    
    Chromestatus - https://chromestatus.com/feature/5152834368700416
    
    Bug: 479161962,454827186
    Change-Id: I8bd76ce59d422f8e3d67d91f1e12f0f2e69ee6ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7543398
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Lia Hiscock <liahiscock@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1580289}

--- third_party/blink/renderer/core/html/html_install_element.cc
+++ third_party/blink/renderer/core/html/html_install_element.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/attribute.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/events/event.h"
+#include "third_party/blink/renderer/core/frame/web_feature.h"
 #include "third_party/blink/renderer/core/html/html_permission_element.h"
 #include "third_party/blink/renderer/core/html/html_permission_icon_element.h"
 #include "third_party/blink/renderer/core/html_names.h"
@@ -29,6 +30,7 @@ HTMLInstallElement::HTMLInstallElement(Document& document)
   CHECK(RuntimeEnabledFeatures::InstallElementEnabled(
       document.GetExecutionContext()));
   setType(AtomicString("install"));
+  UseCounter::CountWebDXFeature(document, WebDXFeature::kDRAFT_InstallElement);
 }
 
 const String& HTMLInstallElement::InstallUrl() const {
