afc83dda77efd43ec2d3e616a91d9a78f101edbf
Jonathan Lee <jonathanjlee@google.com>
2026-02-05 09:16:01 -0800

    [code-health] Clean up expired `event-based-log-upload` flag
    Expired 6+ months ago and internal Codesearch shows a Finch config where
    `EventBasedLogUpload` is fully launched.
    
    Fixed: 427495304
    Change-Id: I44ff431b942f5a93c00b14022a6eff53ecf8e14f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7547061
    Auto-Submit: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Irem Uguz <iremuguz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1580176}

--- chrome/browser/ash/policy/core/device_cloud_policy_manager_ash.cc
+++ chrome/browser/ash/policy/core/device_cloud_policy_manager_ash.cc
@@ -45,7 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/policy/uploading/status_uploader.h"
 #include "chrome/browser/ash/policy/uploading/system_log_uploader.h"
 #include "chrome/browser/browser_process.h"
-#include "chrome/common/chrome_features.h"
 #include "chrome/common/pref_names.h"
 #include "chromeos/ash/components/install_attributes/install_attributes.h"
 #include "chromeos/ash/components/system/statistics_provider.h"
@@ -269,9 +268,7 @@ void DeviceCloudPolicyManagerAsh::StartConnection(
     metric_reporting_manager_ = reporting::MetricReportingManager::Create(
         managed_session_service_.get());
     os_updates_reporter_ = reporting::OsUpdatesReporter::Create();
-    if (base::FeatureList::IsEnabled(features::kEventBasedLogUpload)) {
-      event_based_log_manager_ = std::make_unique<EventBasedLogManager>();
-    }
+    event_based_log_manager_ = std::make_unique<EventBasedLogManager>();
   }
 
   NotifyConnected();
