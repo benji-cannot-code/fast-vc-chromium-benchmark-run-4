7d983e378844d95d7662545118af999a47a498b7
James Leung <jamesleung@google.com>
2026-02-05 15:54:39 -0800

    [composebox] tab spinners
    * Currently, large tab uploads do not indicate when they are finished
      uploading, leading to user confusion
    * This adds loading spinners for tabs and refactors old code
    
    https://screencast.googleplex.com/cast/NjY0NTg3NjA1NDI5NDUyOHwzYmEzYjQ2Yy00NA
    
    Bug: b:481373366
    Change-Id: I94f400f63266fa77581c1a30241c9b180b013c6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7540715
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Commit-Queue: James Leung <jamesleung@google.com>
    Reviewed-by: Nihar Majmudar <niharm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1580478}

--- ui/webui/resources/cr_components/composebox/file_thumbnail.ts
+++ ui/webui/resources/cr_components/composebox/file_thumbnail.ts
@@ -10,6 +10,7 @@ import './composebox_tab_favicon.js';
 
 import {loadTimeData} from '//resources/js/load_time_data.js';
 import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
+import type {PropertyValues} from '//resources/lit/v3_0/lit.rollup.js';
 
 import type {ComposeboxFile} from './common.js';
 import {FileUploadStatus} from './composebox_query.mojom-webui.js';
@@ -40,6 +41,10 @@ export class ComposeboxFileThumbnailElement extends CrLitElement {
   static override get properties() {
     return {
       file: {type: Object},
+      isUploading_: {
+        type: Boolean,
+        reflect: true,
+      },
     };
   }
 
@@ -54,6 +59,19 @@ export class ComposeboxFileThumbnailElement extends CrLitElement {
     tabId: null,
     isDeletable: true,
   };
+
+  protected accessor isUploading_: boolean = false;
+
+  override willUpdate(changedProperties: PropertyValues<this>) {
+    super.willUpdate(changedProperties);
+    if (changedProperties.has('file')) {
+      this.isUploading_ = this.file.status === FileUploadStatus.kNotUploaded ||
+          this.file.status === FileUploadStatus.kProcessing ||
+          this.file.status ===
+              FileUploadStatus.kProcessingSuggestSignalsReady ||
+          this.file.status === FileUploadStatus.kUploadStarted;
+    }
+  }
 
   protected deleteFile_() {
     // TODO(crbug.com/422559977): Send call to handler to delete file from
