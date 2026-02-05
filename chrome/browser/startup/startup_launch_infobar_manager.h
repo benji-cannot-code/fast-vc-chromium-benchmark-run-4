c779405aefc3a6c694ff7f30c3d1148beded73e3
Atharv Maan <atharvmaan@chromium.org>
2026-02-05 11:10:03 -0800

    [Start Chrome with Windows] Implement Startup Launch Infobar UI
    This CL adds StartupLaunchInfobar delegate and manager. Follow up CLs
    will wire up the infobar with StartupLaunchManager which will take care
    of showing it.
    
    Change-Id: I340402b8a328a70c3a9c43a620ae71944ec7d355
    Bug: 458756653
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7542943
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Reviewed-by: Daniel Soromou <koretadaniel@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Atharv Maan <atharvmaan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1580284}

---
+++ chrome/browser/startup/startup_launch_infobar_manager.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_STARTUP_STARTUP_LAUNCH_INFOBAR_MANAGER_H_
+#define CHROME_BROWSER_STARTUP_STARTUP_LAUNCH_INFOBAR_MANAGER_H_
+
+class StartupLaunchInfoBarManager {
+ public:
+  enum class InfoBarType {
+    kForegroundOptIn,
+    kForegroundOptOut,
+  };
+
+  virtual ~StartupLaunchInfoBarManager() = default;
+
+  virtual void ShowInfoBars(InfoBarType infobar_type) = 0;
+  virtual void CloseAllInfoBars() = 0;
+};
+
+#endif  // CHROME_BROWSER_STARTUP_STARTUP_LAUNCH_INFOBAR_MANAGER_H_
