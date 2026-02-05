d08725d0ad622c18d51a5968651fbd1ae8729f98
Bryant Chandler <bryantchandler@google.com>
2026-02-05 13:34:50 -0800

    [glic] Add glic_browser_test and use it for GlicInstanceCoordinatorBrowserTest
    This CL also fixes some production code bugs exposed by
    running the browsertests.
    
    Bug: b:478860288
    Change-Id: I09e98b9dfa1a3aba2ffce7638bd286916a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7536000
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1580392}

--- chrome/browser/glic/host/auth_controller.cc
+++ chrome/browser/glic/host/auth_controller.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/base/consent_level.h"
 #include "components/signin/public/base/signin_metrics.h"
 #include "google_apis/gaia/gaia_auth_util.h"
+#include "mojo/public/cpp/bindings/callback_helpers.h"
 
 namespace glic {
 
@@ -69,6 +70,9 @@ bool AuthController::CheckAuthBeforeShowSync(base::OnceClosure after_signin) {
 
 void AuthController::CheckAuthBeforeLoad(
     base::OnceCallback<void(mojom::PrepareForClientResult)> callback) {
+  callback = mojo::WrapCallbackWithDefaultInvokeIfNotRun(
+      std::move(callback),
+      mojom::PrepareForClientResult::kErrorResyncingCookies);
   // If automation is enabled skip auth check.
   if (IsAutomationEnabled()) {
     base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
--- chrome/browser/glic/test_support/glic_test_environment.h
+++ chrome/browser/glic/test_support/glic_test_environment.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_features.h"
 
 class Profile;
+class IdentityTestEnvironmentProfileAdaptor;
 namespace glic {
 class GlicKeyedService;
 namespace internal {
@@ -105,6 +106,7 @@ class GlicTestEnvironment : public ProfileObserver {
       std::unique_ptr<base::ScopedObservation<Profile, ProfileObserver>>>
       profile_observations_;
   internal::GlicTestEnvironmentShared shared_;
+  std::unique_ptr<IdentityTestEnvironmentProfileAdaptor> adaptor_;
 };
 
 // Note: This constructs the GlicKeyedService, if it's not already created,
