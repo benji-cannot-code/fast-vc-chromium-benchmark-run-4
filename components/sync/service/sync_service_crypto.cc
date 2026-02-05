6637153fc9694c5eb49a4ee78f75aeea119bc60b
Michael Tatarski <mtatarski@google.com>
2026-02-05 10:27:44 -0800

    Remove kSyncUseOsCryptAsync feature guard
    The experiment associated with SyncUseOsCryptAsync has finished
    successfully. This CL removes the feature guard kSyncUseOsCryptAsync and
    the compat option.
    
    It also cleans up SyncServiceCryptoTest to remove dependencies on the
    legacy OSCryptMocker.
    
    Bug: 419157433
    Change-Id: I576363828e14aebb67fda564b0fe0ee692ee855e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7380057
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Michael Tatarski <mtatarski@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Auto-Submit: Michael Tatarski <mtatarski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1580244}

--- components/sync/service/sync_service_crypto.cc
+++ components/sync/service/sync_service_crypto.cc
@@ -15,8 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/to_string.h"
 #include "base/task/sequenced_task_runner.h"
 #include "components/os_crypt/async/common/encryptor.h"
-#include "components/os_crypt/sync/os_crypt.h"
-#include "components/sync/base/features.h"
 #include "components/sync/base/passphrase_enums.h"
 #include "components/sync/engine/nigori/nigori.h"
 #include "components/sync/engine/sync_string_conversions.h"
@@ -832,14 +830,9 @@ std::unique_ptr<Nigori> SyncServiceCrypto::ReadNigoriFromBootstrapToken(
   }
 
   std::string decrypted_key;
-  bool decryption_result;
-  if (base::FeatureList::IsEnabled(kSyncUseOsCryptAsync)) {
-    // If the feature is enabled, the encryptor must be available.
-    CHECK(encryptor_);
-    decryption_result = encryptor_->DecryptString(decoded_key, &decrypted_key);
-  } else {
-    decryption_result = OSCrypt::DecryptString(decoded_key, &decrypted_key);
-  }
+  CHECK(encryptor_);
+  bool decryption_result =
+      encryptor_->DecryptString(decoded_key, &decrypted_key);
   base::UmaHistogramBoolean("Sync.BootstrapTokenDecryptionResult",
                             decryption_result);
   if (!decryption_result) {
@@ -867,15 +860,9 @@ std::string SyncServiceCrypto::SerializeNigoriAsBootstrapToken(
   }
 
   std::string encrypted_key;
-  bool encryption_result;
-  if (base::FeatureList::IsEnabled(kSyncUseOsCryptAsync)) {
-    // If the feature is enabled, the encryptor must be available.
-    CHECK(encryptor_);
-    encryption_result =
-        encryptor_->EncryptString(serialized_key, &encrypted_key);
-  } else {
-    encryption_result = OSCrypt::EncryptString(serialized_key, &encrypted_key);
-  }
+  CHECK(encryptor_);
+  bool encryption_result =
+      encryptor_->EncryptString(serialized_key, &encrypted_key);
   base::UmaHistogramBoolean("Sync.BootstrapTokenEncryptionResult",
                             encryption_result);
   if (!encryption_result) {
