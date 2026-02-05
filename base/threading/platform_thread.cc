585e2cba63bcc5e5cd4758769440dfb74dcbaf34
Etienne Pierre-doray <etiennep@chromium.org>
2026-02-05 10:21:44 -0800

    [task] Use SetThreadTypeOverride for ScopedBoostPriority
    This makes ScopedBoostPriority works similarly to
    ScopedBoostablePriority.
    This avoids using `may_change_affinity` argument to
    SetCurrentThreadType, since this is achieved through
    SetThreadTypeOverride.
    
    Caveat: this isn't supported on linux in sandbox process; because of sandbox though ScopedBoostPriority doesn't work synchronously anyways, which isn't suitable for a short interval.
    
    Bug: 470337728
    Change-Id: I20f95122bc8774e33342784d6af79542821ffb75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7539489
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1580237}

--- base/threading/platform_thread.cc
+++ base/threading/platform_thread.cc
@@ -26,8 +26,7 @@ void PlatformThreadId::WriteIntoTrace(perfetto::TracedValue&& context) const {
 }
 
 // static
-void PlatformThreadBase::SetCurrentThreadType(ThreadType thread_type,
-                                              bool may_change_affinity) {
+void PlatformThreadBase::SetCurrentThreadType(ThreadType thread_type) {
   MessagePumpType message_pump_type = MessagePumpType::DEFAULT;
   if (CurrentIOThread::IsSet()) {
     message_pump_type = MessagePumpType::IO;
@@ -35,8 +34,7 @@ void PlatformThreadBase::SetCurrentThreadType(ThreadType thread_type,
     message_pump_type = MessagePumpType::UI;
   }
   CHECK_LE(thread_type, ThreadType::kMaxValue);
-  internal::SetCurrentThreadTypeImpl(thread_type, message_pump_type,
-                                     may_change_affinity);
+  internal::SetCurrentThreadTypeImpl(thread_type, message_pump_type);
   current_thread_type = thread_type;
 }
 
