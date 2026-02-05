55b3b17be30bb151fbcfe12ee89aa1179724a170
Kaan Alsan <alsan@chromium.org>
2026-02-05 08:20:00 -0800

    Disable PageActionsMigrationIntentPicker on ChromeOS.
    The kPageActionsMigrationIntentPicker feature is now disabled by default on ChromeOS.
    
    Change-Id: I1fba064fb4a02ac7cafc3d53b7f57fea8654745d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7547500
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Reviewed-by: Muhammad Salmaan <musalmaan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1580136}

--- chrome/browser/ui/ui_features.cc
+++ chrome/browser/ui/ui_features.cc
@@ -98,7 +98,6 @@ BASE_FEATURE(kShowTabGroupsMacSystemMenu, base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kSideBySide, base::FEATURE_ENABLED_BY_DEFAULT);
 
-
 BASE_FEATURE(kSideBySideLinkMenuNewBadge, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kTabDuplicateMetrics, base::FEATURE_ENABLED_BY_DEFAULT);
@@ -336,7 +335,13 @@ BASE_FEATURE_PARAM(bool,
                    kPageActionsMigrationIntentPicker,
                    &kPageActionsMigration,
                    "intent_picker",
-                   true);
+// TODOD(crbug.com/480035938): Enable on ChromeOS.
+#if BUILDFLAG(IS_CHROMEOS)
+                   false
+#else
+                   true
+#endif
+);
 
 BASE_FEATURE_PARAM(bool,
                    kPageActionsMigrationZoom,
