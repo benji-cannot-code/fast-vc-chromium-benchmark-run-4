e46e1698e3f0a88f0bbf7984da1ab6f920901f7b
WENHU CHENG <wenhuc@google.com>
2026-02-04 19:06:11 -0800

    [aim-tools-models] Ensure initial active model the first item in allowed models
    Previously selecting a model would incorrectly disable all other models.
    This CL removes that exclusive locking logic to allow model switching.
    It also fixes the initial active model to default to the first item in
    allowed models.
    
    NO_IFTTT = "we don't need `GetDefaultModel` in mojom file."
    
    Fixed: b:479265357
    Change-Id: I49a342a7263568c6510b61a7fca731dbad9cca03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7533193
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Commit-Queue: Wenhu Cheng <wenhuc@google.com>
    Reviewed-by: Ananya Seelam <ananyaseelam@google.com>
    Cr-Commit-Position: refs/heads/main@{#1579848}

--- components/omnibox/common/input_state.cc
+++ components/omnibox/common/input_state.cc
@@ -52,4 +52,11 @@ size_t InputState::EstimateMemoryUsage() const {
   return res;
 }
 
+ModelMode InputState::GetDefaultModel() const {
+  if (!allowed_models.empty()) {
+    return allowed_models[0];
+  }
+  return ModelMode::MODEL_MODE_UNSPECIFIED;
+}
+
 }  // namespace omnibox
