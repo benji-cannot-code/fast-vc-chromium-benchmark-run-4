b549a539c14f15c9beed1b3fa57c8fc7c3618f07
Minoru Chikamune <chikamune@chromium.org>
2026-02-05 04:53:47 -0800

    Add PageLoad metrics based on ActualNavigationStart
    This CL introduces a new set of UMA histograms that use
    ActualNavigationStart as their starting point, instead of the standard
    (adjusted by beforeunload) NavigationStart.
    
    While the existing NavigationStart can be delayed by beforeunload
    handlers or dialogs, ActualNavigationStart provides a more consistent
    anchor for measuring the true elapsed time from the moment the browser
    begins processing a navigation request. These new metrics help evaluate
    the impact of before-navigation overhead on the overall loading
    pipeline, including commit, parsing, and painting milestones.
    
    Added Histograms:
    - PageLoad.NavigationTiming.ActualNavigationStartToNavigationCommitSent
    - PageLoad.ParseTiming.ActualNavigationStartToParseStart
    - PageLoad.PaintTiming.ActualNavigationStartToFirstContentfulPaint
    - PageLoad.PaintTiming.ActualNavigationStartToLargestContentfulPaint
    
    These metrics will provide better insights into the "true" latency
    experienced by users, especially in cases where beforeunload handlers
    introduce significant delays that are currently excluded from standard
    performance metrics.
    
    Bug: 467158532
    Bug: 476933703
    Change-Id: Ib728bb75a973ef6c50989d64fa6a0d4c4fa1582f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7544318
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1580015}

--- components/page_load_metrics/browser/page_load_metrics_util.h
+++ components/page_load_metrics/browser/page_load_metrics_util.h
@@ -18,9 +18,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/loader/loading_behavior_flag.h"
 #include "url/gurl.h"
 
-// Up to 10 minutes, with 100 buckets.
+// 10 ms to 10 minutes, with 100 buckets.
 #define PAGE_LOAD_HISTOGRAM(name, sample)                             \
   base::UmaHistogramCustomTimes(name, sample, base::Milliseconds(10), \
+                                base::Minutes(10), 100)
+
+// 1 ms to 10 minutes, with 100 buckets.
+// Used for metrics where we want to avoid sub-10ms values being rounded
+// to zero (falling into the underflow bucket), which occurs in the
+// standard PAGE_LOAD_HISTOGRAM.
+#define PAGE_LOAD_SUB_10MS_HISTOGRAM(name, sample)                   \
+  base::UmaHistogramCustomTimes(name, sample, base::Milliseconds(1), \
                                 base::Minutes(10), 100)
 
 // 1 ms to 1 minute, with 100 buckets.
