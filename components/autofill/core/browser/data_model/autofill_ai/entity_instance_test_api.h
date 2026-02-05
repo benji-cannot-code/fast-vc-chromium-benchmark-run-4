022c79ca10eb5fb7a66c09dc428e3a49917442b9
Jan Keitel <jkeitel@google.com>
2026-02-05 00:53:55 -0800

    Add test helper for masking an entire EntityInstance
    Future tests will need to have masked and unmasked versions
    of the same entity - this makes it easier to create them.
    
    Bug: 481566741
    Change-Id: If55e656f3f31236faaead697d5499597420d4c33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7545839
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1579941}

--- components/autofill/core/browser/data_model/autofill_ai/entity_instance_test_api.h
+++ components/autofill/core/browser/data_model/autofill_ai/entity_instance_test_api.h
@@ -28,7 +28,7 @@ class AttributeInstanceTestApi {
 
 class EntityInstanceTestApi {
  public:
-  explicit EntityInstanceTestApi(EntityInstance* entity)
+  explicit EntityInstanceTestApi(const EntityInstance* entity)
       : entity_(CHECK_DEREF(entity)) {}
   ~EntityInstanceTestApi() = default;
 
@@ -37,14 +37,14 @@ class EntityInstanceTestApi {
   }
 
  private:
-  raw_ref<EntityInstance> entity_;
+  raw_ref<const EntityInstance> entity_;
 };
 
 inline AttributeInstanceTestApi test_api(AttributeInstance& attribute) {
   return AttributeInstanceTestApi(&attribute);
 }
 
-inline EntityInstanceTestApi test_api(EntityInstance& entity) {
+inline EntityInstanceTestApi test_api(const EntityInstance& entity) {
   return EntityInstanceTestApi(&entity);
 }
 
