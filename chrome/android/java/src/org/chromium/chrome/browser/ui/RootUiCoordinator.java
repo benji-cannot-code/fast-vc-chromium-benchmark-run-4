6a281b455b6ad22aec1f922c939254794a6e3e44
Wenyu Fu <wenyufu@chromium.org>
2026-02-04 19:43:02 -0800

    [E2E] Record Edge-to-Edge eligibility at different stages
    Add `Android.EdgeToEdge.Eligible2` and
    `Android.EdgeToEdge.IneligibilityReason2` to recorded on every
    ChromeActivity launch.
    
    Add `Android.EdgeToEdge.Eligible2.OnCreateController` and
    `Android.EdgeToEdge.IneligibilityReason2.OnCreateController` are
    recorded when the EdgeToEdgeController is initialized.
    
    OBSOLETE_HISTOGRAMS=Android.EdgeToEdge.IneligibilityReason, Android.EdgeToEdge.Eligible
    Bug: 463383084
    
    Record Edge-to-Edge eligibility at different stages.
    
    This change introduces new histograms to track Edge-to-Edge eligibility. Android.EdgeToEdge.Eligible2 and Android.EdgeToEdge.IneligibilityReason2 are recorded on every ChromeActivity launch. Android.EdgeToEdge.Eligible2.OnCreateController and Android.EdgeToEdge.IneligibilityReason2.OnCreateController are recorded when the EdgeToEdgeController is initialized. The eligibility check logic has been refactored to support logging to different histograms based on the call site.
    
    Change-Id: Ifb4d6d6e5e4c5898fb036711bd20fe2570a275bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7529777
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Charles Hager <clhager@google.com>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1579868}

--- chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
@@ -1130,6 +1130,8 @@ public class RootUiCoordinator
         initScrollCapture();
         mAdaptiveToolbarUiCoordinator.onFinishNativeInitialization();
 
+        EdgeToEdgeUtils.recordEligibilityForEveryStart(mActivity);
+
         if (mWindowAndroid.getInsetObserver() != null
                 && Build.VERSION.SDK_INT >= Build.VERSION_CODES.R
                 && ChromeFeatureList.sEdgeToEdgeMonitorConfigurations.isEnabled()) {
@@ -2222,7 +2224,7 @@ public class RootUiCoordinator
     /** Setup drawing using Android Edge-to-Edge. */
     @CallSuper
     protected void initializeEdgeToEdgeController() {
-        boolean eligible = EdgeToEdgeUtils.recordEligibility(mActivity);
+        boolean eligible = EdgeToEdgeUtils.recordEligibilityOnCreate(mActivity);
 
         UmaSessionStats.registerSyntheticFieldTrial(
                 "EdgeToEdgeChinEligibility", eligible ? "Eligible" : "Not Eligible");
