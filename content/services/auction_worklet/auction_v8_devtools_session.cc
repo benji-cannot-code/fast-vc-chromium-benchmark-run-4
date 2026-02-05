1e6f43623cb72ba6c744bea61f5b1ea85b427ebe
Jeffrey Yu <yuje@chromium.org>
2026-02-05 11:04:15 -0800

    Replace `std::set` with `absl::flat_hash_set`.
    The Chromium containers guide
    (https://chromium.googlesource.com/chromium/src/+/HEAD/base/containers/README.md)
    recommends using `absl::flat_hash_set` as a default set choice when
    stable iteration order and pointer stability isn't needed. It generally
    perform better and require less overhead than STL sets.
    
    Bug: 473916362
    Change-Id: I94c4efd87345b6afb4f4ac0d677b6b2ae172cb27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7539663
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Auto-Submit: Jeffrey Yu <yuje@chromium.org>
    Commit-Queue: Russ Hamilton <behamilton@google.com>
    Reviewed-by: Russ Hamilton <behamilton@google.com>
    Cr-Commit-Position: refs/heads/main@{#1580279}

--- content/services/auction_worklet/auction_v8_devtools_session.cc
+++ content/services/auction_worklet/auction_v8_devtools_session.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
-#include <set>
 #include <string>
 #include <vector>
 
@@ -25,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/services/auction_worklet/debug_command_queue.h"
 #include "content/services/auction_worklet/protocol/event_breakpoints.h"
 #include "mojo/public/cpp/bindings/self_owned_receiver.h"
+#include "third_party/abseil-cpp/absl/container/flat_hash_set.h"
 #include "third_party/inspector_protocol/crdtp/cbor.h"
 #include "third_party/inspector_protocol/crdtp/dispatch.h"
 #include "third_party/inspector_protocol/crdtp/frontend_channel.h"
@@ -53,8 +53,7 @@ class AuctionV8DevToolsSession::BreakpointHandler
 
   void MaybeTriggerInstrumentationBreakpoint(const std::string& name) {
     DCHECK_CALLED_ON_VALID_SEQUENCE(v8_sequence_checker_);
-    if (instrumentation_breakpoints_.find(name) !=
-        instrumentation_breakpoints_.end()) {
+    if (instrumentation_breakpoints_.contains(name)) {
       std::string category("EventListener");
       std::string aux_json = base::StringPrintf(
           R"({"eventName":"instrumentation:%s"})", name.c_str());
@@ -81,7 +80,7 @@ class AuctionV8DevToolsSession::BreakpointHandler
   }
 
   raw_ptr<v8_inspector::V8InspectorSession> v8_session_;
-  std::set<std::string> instrumentation_breakpoints_;
+  absl::flat_hash_set<std::string> instrumentation_breakpoints_;
   SEQUENCE_CHECKER(v8_sequence_checker_);
 };
 
