32a40d765c3c94abdc89452075a042ee757ecd54
Joemer Ramos <joemerramos@google.com>
2026-02-04 16:32:48 -0800

    [iOS] Add Gemini API To Listen To When Responses Start Streaming
    Since this function will be called from the floaty through an internal
    gateway, this API is a no-op until the internal implementation lands.
    
    This also fixes an issue where if a floaty is presented in fullscreen
    mode such as with Nano Banana, the floaty wouldn't show. Now, every time
    we trigger `PresentFloaty` we ensure the floaty is seen by exiting
    fullscreen mode.
    
    Internal CL: crrev.com/i/8984601
    
    Bug: 480919925
    Fixed: 481537898
    Change-Id: I05b53f16a856db4f8931ae1712c6ed8a5f5e7eb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7539320
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Joemer Ramos <joemerramos@google.com>
    Cr-Commit-Position: refs/heads/main@{#1579771}

--- ios/chrome/browser/intelligence/bwg/model/bwg_session_handler.mm
+++ ios/chrome/browser/intelligence/bwg/model/bwg_session_handler.mm
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/intelligence/bwg/metrics/gemini_metrics.h"
 #import "ios/chrome/browser/intelligence/bwg/model/bwg_tab_helper.h"
 #import "ios/chrome/browser/intelligence/bwg/model/gemini_session_delegate.h"
+#import "ios/chrome/browser/intelligence/bwg/utils/bwg_constants.h"
 #import "ios/chrome/browser/intelligence/features/features.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
 #import "ios/chrome/browser/shared/public/commands/bwg_commands.h"
@@ -162,7 +163,7 @@ IOSGeminiSessionCancellationReason HistogramEnumFromGeminiCancelType(
 
 - (void)UIDidDisappearWithClientID:(NSString*)clientID
                           serverID:(NSString*)serverID {
-  [_BWGHandler dismissGeminiFlowWithCompletion:nil];
+  [_geminiHandler dismissGeminiFlowWithCompletion:nil];
   [self setSessionActive:NO clientID:clientID];
 
   web::WebState* webState = [self webStateWithClientID:clientID];
@@ -205,6 +206,14 @@ IOSGeminiSessionCancellationReason HistogramEnumFromGeminiCancelType(
   // Record prompt counts for the session.
   RecordSessionPromptCount(_totalPromptsInSession);
   RecordSessionFirstPrompt(_hasSubmittedFirstPrompt);
+}
+
+- (void)startReceivingResponseWithSessionID:(NSString*)sessionID
+                             conversationID:(NSString*)conversationID {
+  [self.geminiHandler
+      updateFloatyVisibilityIfEligibleAnimated:NO
+                                    fromSource:gemini::FloatyUpdateSource::
+                                                   ForcedFromQueryResponse];
 }
 
 - (void)responseReceivedWithClientID:(NSString*)clientID
--- ios/chrome/browser/intelligence/bwg/utils/bwg_constants.h
+++ ios/chrome/browser/intelligence/bwg/utils/bwg_constants.h
@@ -49,7 +49,8 @@ enum class FloatyUpdateSource {
   ForcedFromFullscreen = 6,
   Overlay = 7,
   IneligibleSite = 8,
-  kMaxValue = IneligibleSite,
+  ForcedFromQueryResponse = 9,
+  kMaxValue = ForcedFromQueryResponse,
 };
 // LINT.ThenChange(/tools/metrics/histograms/metadata/ios/enums.xml:IOSGeminiFloatyUpdateSource)
 
