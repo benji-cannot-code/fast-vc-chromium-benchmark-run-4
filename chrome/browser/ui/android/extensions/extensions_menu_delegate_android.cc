047cb08f404ddb510a13a2f17edaf1aea858cd2d
Sky Malice <skym@chromium.org>
2026-02-04 20:00:12 -0800

    Revert "[Extensions] Populate extensions menu in Desktop Android using ViewModel"
    This reverts commit 2d6f7ea05ee1a4807f1d3c971e42b60dc73ae83f.
    
    Reason for revert: Causing org.chromium.chrome.browser.toolbar.extensions.ExtensionToolbarTest#testExtensionsMenu to fail on android-desktop-x64-rel-15-tests . Espresso cannot find "Test Action" string
    
    Original change's description:
    > [Extensions] Populate extensions menu in Desktop Android using ViewModel
    >
    > The initialization flow is updated to handle both immediate and lazy
    > population:
    > 1. If the ViewModel is already initialized during the Mediator/Bridge
    >    construction, a new JNI call (`IsReady`) allows the Mediator to
    >    populate immediately. This is necessary as observers added during
    >    construction will miss the initial notification.
    > 2. If the ViewModel initializes later, it notifies the Java layer via
    >    the JNI bridge, triggering the "on ready" callback to populate the
    >    menu.
    >
    > This CL also:
    > - Introduces the "zero-state" view for when no extensions
    >   are installed
    > - Temporarily removes icon support (to be re-added
    > separately).
    > - Removes  ExtensionActionsUpdateHelper in favor of the new ViewModel
    >   based population logic.
    >
    > Screenshots:
    > - menu with actions: crbug.com/473213114#attachment73105456
    > - menu with zero state: crbug.com/473213114#attachment73105777
    >
    > Bug: 473213114
    > Change-Id: If5dec0108dd7a702f231d63996a4e8de55f32587
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7511962
    > Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    > Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1579776}
    
    Bug: 473213114
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I11461140ba7aa4310e0979918598f6ee29c06044
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7546269
    Auto-Submit: Sky Malice <skym@chromium.org>
    Owners-Override: Sky Malice <skym@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1579872}

--- chrome/browser/ui/android/extensions/extensions_menu_delegate_android.cc
+++ chrome/browser/ui/android/extensions/extensions_menu_delegate_android.cc
@@ -5,12 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/android/extensions/extensions_menu_delegate_android.h"
 
-#include "base/android/jni_string.h"
-#include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/ui/android/extensions/extension_action_delegate_android.h"
-#include "content/public/browser/web_contents.h"
-#include "ui/gfx/android/java_bitmap.h"
-#include "ui/gfx/image/image_skia.h"
 
 // Must come after all headers that specialize FromJniType() / ToJniType().
 #include "chrome/browser/ui/android/extensions/jni_headers/ExtensionsMenuBridge_jni.h"
@@ -31,23 +26,6 @@ ExtensionsMenuDelegateAndroid::~ExtensionsMenuDelegateAndroid() = default;
 
 void ExtensionsMenuDelegateAndroid::Destroy(JNIEnv* env) {
   delete this;
-}
-
-std::vector<std::string> ExtensionsMenuDelegateAndroid::GetActions(
-    JNIEnv* env) {
-  // TODO(crbug.com/473213114): Returning a flattened list of strings is a
-  // workaround until we introduce a proper type to hold action information.
-  std::vector<std::string> actions_list;
-  for (const auto& action_model : menu_model_->action_models()) {
-    actions_list.push_back(action_model->GetId());
-    actions_list.push_back(base::UTF16ToUTF8(action_model->GetActionName()));
-  }
-
-  return actions_list;
-}
-
-bool ExtensionsMenuDelegateAndroid::IsReady(JNIEnv* env) {
-  return menu_model_->is_populated();
 }
 
 std::unique_ptr<ExtensionActionViewModel>
@@ -82,8 +60,7 @@ void ExtensionsMenuDelegateAndroid::OnActionUpdated(
 }
 
 void ExtensionsMenuDelegateAndroid::OnActionsInitialized() {
-  JNIEnv* env = base::android::AttachCurrentThread();
-  Java_ExtensionsMenuBridge_onReady(env, java_object_);
+  // TODO(crbug.com/473213114)
 }
 
 void ExtensionsMenuDelegateAndroid::OnHostAccessRequestAdded(
