18665ea2775418aa079c5c0e5a7195eb6c3dd5db
Xiaochen Zhou <xiaochenzh@chromium.org>
2026-02-05 10:01:42 -0800

    [Connection-Allowlist] Add wpt for link header preload
    The key param in the preload url is hardcoded because it is required to
    be the result of token(), but one cannot call token() in .headers file.
    
    Bug: 447954811
    Change-Id: I964bcdff8b1a23eb2ac93780de73d8c0d47a01c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7544458
    Reviewed-by: Andrew Verge <averge@chromium.org>
    Commit-Queue: Xiaochen Zhou <xiaochenzh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1580224}

---
+++ third_party/blink/web_tests/external/wpt/connection-allowlist/tentative/link_header_preload_allow.sub.window.js
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/common/utils.js
+// META: script=resources/utils.js
+//
+// The test assumes the policy `Connection-Allowlist: (response-origin)` has
+// been set in the response. The response also contains a link header that
+// triggers a preload to the same-origin KV server at
+// http://{{hosts[][]}}:{{ports[http][0]}}.
+
+promise_test(async () => {
+  const result =
+      await nextValueFromServer('6cd381d3-85b5-40ce-8c63-1ffb06154b8b');
+  assert_equals(result, 'hello');
+}, 'Link header preload to an allow-listed url succeeds.');
