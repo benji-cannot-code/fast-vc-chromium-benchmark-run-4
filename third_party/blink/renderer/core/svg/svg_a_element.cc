b42265a0855d567f00ff1ba632c468fc685d80a2
Kent Tamura <tkent@chromium.org>
2026-02-05 01:18:09 -0800

    SVG: Fix a crash in SVGAElement::DefaultEventHandler()
    This CL fixes a regression caused by https://crrev.com/c/7533096 .
    StringView::operator[] is stricter than String::operator[].
    
    Bug: 481373475
    Change-Id: I2ede74281c5ca754075ebdf40a21b1600d43ce06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7547177
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1579953}

--- third_party/blink/renderer/core/svg/svg_a_element.cc
+++ third_party/blink/renderer/core/svg/svg_a_element.cc
@@ -127,7 +127,7 @@ void SVGAElement::DefaultEventHandler(Event& event) {
     if (IsLinkClick(event)) {
       StringView url = StripLeadingAndTrailingHtmlSpaces(HrefString());
 
-      if (url[0] == '#') {
+      if (url.starts_with('#')) {
         Element* target_element =
             GetTreeScope().getElementById(AtomicString(url.substr(1)));
         if (auto* svg_smil_element =
---
+++ third_party/blink/renderer/core/svg/svg_a_element_test.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/core/svg/svg_a_element.h"
+
+#include "third_party/blink/renderer/core/testing/core_unit_test_helper.h"
+
+namespace blink {
+
+class SVGAElementTest : public RenderingTest {};
+
+// crbug.com/481373475
+TEST_F(SVGAElementTest, DefaultEventHandlerCrash) {
+  SetBodyInnerHTML(R"HTML(
+<svg width=100 height=100>
+<text x=10 y=30><a id="a" href="">link-1</a></text>
+</svg>
+)HTML");
+  auto* target = GetElementById("a");
+  target->DispatchSimulatedClick(nullptr,
+                                 SimulatedClickCreationScope::kFromScript);
+  // Pass if no crashes.
+}
+
+}  // namespace blink
