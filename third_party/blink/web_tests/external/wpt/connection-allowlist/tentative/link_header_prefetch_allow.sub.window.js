6aa13288f5f913554be69218d2b675a04b00db59
Xiaochen Zhou <xiaochenzh@chromium.org>
2026-02-05 09:53:48 -0800

    [Connection-Allowlist] Add wpt for link header prefetch
    The key param in the prefetch url is hardcoded because it is required to
    be the result of token(), but one cannot call token() in .headers file.
    
    Bug: 447954811
    Change-Id: I039dfca2a80e9c01230d2af6b036447baf5bba8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7542500
    Reviewed-by: Andrew Verge <averge@chromium.org>
    Commit-Queue: Xiaochen Zhou <xiaochenzh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1580211}

---
+++ third_party/blink/web_tests/external/wpt/connection-allowlist/tentative/link_header_prefetch_allow.sub.window.js
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/common/utils.js
+// META: script=resources/utils.js
+//
+// The test assumes the policy `Connection-Allowlist: (response-origin)` has
+// been set in the response. The response also contains a link header that
+// triggers a prefetch to the same-origin KV server at
+// http://{{hosts[][]}}:{{ports[http][0]}}.
+
+promise_test(async () => {
+  const result =
+      await nextValueFromServer('641530d4-9e7d-4760-91da-2a57191363c1');
+  assert_equals(result, 'hello');
+}, 'Link header prefetch to an allow-listed url succeeds.');
