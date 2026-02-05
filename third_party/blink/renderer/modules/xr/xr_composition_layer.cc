3df2e8479c72a89153b742185060e820bb6d8ce3
Yong Li <yyonglii@xwf.google.com>
2026-02-05 06:05:21 -0800

    WebXR Layers: Implement forceMonoPresentation
    Bug: 480928647
    Change-Id: I97737f5bb80eb1cbe9a15eb1aa9a8661e64de9b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7546597
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Yong Li (xWF) <yyonglii@google.com>
    Cr-Commit-Position: refs/heads/main@{#1580050}

--- third_party/blink/renderer/modules/xr/xr_composition_layer.cc
+++ third_party/blink/renderer/modules/xr/xr_composition_layer.cc
@@ -55,6 +55,7 @@ bool XRCompositionLayer::forceMonoPresentation() const {
 
 void XRCompositionLayer::setForceMonoPresentation(bool value) {
   force_mono_presentation_ = value;
+  SetModified(true);
 }
 
 float XRCompositionLayer::opacity() const {
@@ -138,6 +139,7 @@ XRCompositionLayer::CreateLayerData() const {
   layer_data->mutable_data = device::mojom::blink::XRLayerMutableData::New();
   layer_data->mutable_data->blend_texture_source_alpha =
       blendTextureSourceAlpha();
+  layer_data->mutable_data->force_mono_presentation = forceMonoPresentation();
   layer_data->mutable_data->opacity = opacity();
   layer_data->mutable_data->native_origin_information = NativeOrigin();
 
