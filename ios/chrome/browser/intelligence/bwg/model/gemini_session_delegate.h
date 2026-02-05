99b2053947a29903d3dc87fea1930c3cb23d477c
Joe Peplowski <joepeplowski@google.com>
2026-02-05 07:44:32 -0800

    Add metrics for Gemini image actions & input plate attachment options
    This CL adds metrics for histograms and user actions related to the
    following behaviours in Gemini:
    - Interacting with action buttons on images in Gemini responses
    - Interacting with attachment options in the input plate in Gemini queries
    
    The corresponding internal CL will need to land for this functionality
    to be complete: crrev.com/i/8984742
    
    Bug: 477271738
    Change-Id: Ib06983e22400546cc453089b376e1d12e4248a59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7540385
    Reviewed-by: Sebastien S-G <sebsg@chromium.org>
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Commit-Queue: Joe Peplowski <joepeplowski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1580107}

--- ios/chrome/browser/intelligence/bwg/model/gemini_session_delegate.h
+++ ios/chrome/browser/intelligence/bwg/model/gemini_session_delegate.h
@@ -9,8 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+#import "ios/chrome/browser/intelligence/bwg/utils/bwg_constants.h"
 #import "ios/public/provider/chrome/browser/bwg/bwg_api.h"
 
+// TODO(crbug.com/481711842): Replace this enum and its gemini_metrics.h
+// equivalent with an enum in bwg_constants.h
 // Input type for BWG queries.
 // LINT.IfChange(BWGInputType)
 typedef NS_ENUM(NSInteger, BWGInputType) {
@@ -68,6 +71,8 @@ typedef NS_ENUM(NSInteger, BWGInputType) {
 //   /tools/metrics/histograms/metadata/ios/enums.xml:IOSGeminiFirstPromptSubmissionMethod
 // )
 
+// TODO(crbug.com/481711842): Replace this enum and its gemini_metrics.h
+// equivalent with an enum in bwg_constants.h
 // The feedback type for Gemini queries.
 // LINT.IfChange(GeminiFeedbackType)
 enum class GeminiFeedbackType {
@@ -76,8 +81,13 @@ enum class GeminiFeedbackType {
   // Thumbs down feedback type.
   kThumbsDown,
 };
-// LINT.ThenChange(/ios/chrome/browser/intelligence/bwg/metrics/gemini_metrics.h:IOSGeminiFeedback)
+// LINT.ThenChange(
+//   /ios/chrome/browser/intelligence/bwg/metrics/gemini_metrics.h:IOSGeminiFeedback,
+//   /tools/metrics/histograms/metadata/ios/enums.xml:IOSGeminiFeedback
+// )
 
+// TODO(crbug.com/481711842): Replace this enum and its gemini_metrics.h
+// equivalent with an enum in bwg_constants.h
 // Cancellation types for a Gemini session.
 typedef NS_ENUM(NSInteger, GeminiCancelType) {
   // Unknown cancellation reason.
@@ -93,6 +103,9 @@ typedef NS_ENUM(NSInteger, GeminiCancelType) {
   // Close button was tapped in the loading state.
   GeminiCancelTypeLoadingStateCloseButtonTapped = 5,
 };
+
+// TODO(crbug.com/481711842): Do not add any more enums here if they are only
+// used for metrics.
 
 // Delegate for Gemini session events. Keep up to date with GCR's
 // SessionDelegate.
@@ -154,13 +167,14 @@ typedef NS_ENUM(NSInteger, GeminiCancelType) {
 
 // Called when the user taps on an attachment option e.g. camera, gallery,
 // CreateImageSelected, CreateImageDeselected  in the input plate.
-- (void)didTapInputPlateAttachmentOption:(NSString*)attachmentOption
+- (void)didTapInputPlateAttachmentOption:
+            (gemini::InputPlateAttachmentOption)attachmentOption
                                sessionID:(NSString*)sessionID
                           conversationID:(NSString*)conversationID;
 
 // Called when the user taps on an image action button e.g.  share, copy,
 // download image.
-- (void)imageActionButtonTapped:(NSString*)actionButtonType
+- (void)imageActionButtonTapped:(gemini::ImageActionButtonType)actionButtonType
                       sessionID:(NSString*)sessionID
                  conversationID:(NSString*)conversationID;
 
