dd7c30f34eca801d28fe59341addec5686c597ca
Abhijith Nair <abhijithnair@chromium.org>
2026-02-05 10:09:14 -0800

    Add metrics for WebViewChromium constructor and init methods.
    This change adds new entries to the WebViewApiCall enum and
    corresponding user actions to track the invocation of the
    WebViewChromium constructor, init, and initForReal methods. This will
    help in understanding the lifecycle and initialization flow of WebView
    instances.
    
    Change-Id: If86d361646379abb285340cee45060a463fc7a58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7545279
    Commit-Queue: Abhijith Nair <abhijithnair@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1580231}

--- android_webview/glue/java/src/com/android/webview/chromium/WebViewChromium.java
+++ android_webview/glue/java/src/com/android/webview/chromium/WebViewChromium.java
@@ -386,7 +386,10 @@ class WebViewChromium
         ApiCall.GEOLOCATION_PERMISSIONS_CLEAR,
         ApiCall.GEOLOCATION_PERMISSIONS_CLEAR_ALL,
         ApiCall.GEOLOCATION_PERMISSIONS_GET_ALLOWED,
-        ApiCall.GEOLOCATION_PERMISSIONS_GET_ORIGINS
+        ApiCall.GEOLOCATION_PERMISSIONS_GET_ORIGINS,
+        ApiCall.WEBVIEW_CHROMIUM_CONSTRUCTOR,
+        ApiCall.WEBVIEW_CHROMIUM_INIT,
+        ApiCall.WEBVIEW_CHROMIUM_INIT_FOR_REAL,
     })
     @interface ApiCall {
         int ADD_JAVASCRIPT_INTERFACE = 0;
@@ -624,7 +627,10 @@ class WebViewChromium
         int GEOLOCATION_PERMISSIONS_CLEAR_ALL = 232;
         int GEOLOCATION_PERMISSIONS_GET_ALLOWED = 233;
         int GEOLOCATION_PERMISSIONS_GET_ORIGINS = 234;
-        int COUNT = 235;
+        int WEBVIEW_CHROMIUM_CONSTRUCTOR = 235;
+        int WEBVIEW_CHROMIUM_INIT = 236;
+        int WEBVIEW_CHROMIUM_INIT_FOR_REAL = 237;
+        int COUNT = 238;
     }
 
     // LINT.ThenChange(/tools/metrics/histograms/metadata/android/enums.xml:WebViewApiCall)
@@ -879,7 +885,10 @@ class WebViewChromium
         ApiCallUserAction.WEB_STORAGE_DELETE_ORIGIN,
         ApiCallUserAction.WEB_STORAGE_GET_ORIGINS,
         ApiCallUserAction.WEB_STORAGE_GET_QUOTA_FOR_ORIGIN,
-        ApiCallUserAction.WEB_STORAGE_GET_USAGE_FOR_ORIGIN
+        ApiCallUserAction.WEB_STORAGE_GET_USAGE_FOR_ORIGIN,
+        ApiCallUserAction.WEBVIEW_INSTANCE_WEBVIEW_CHROMIUM_CONSTRUCTOR,
+        ApiCallUserAction.WEBVIEW_INSTANCE_WEBVIEW_CHROMIUM_INIT,
+        ApiCallUserAction.WEBVIEW_INSTANCE_WEBVIEW_CHROMIUM_INIT_FOR_REAL,
     })
     @interface ApiCallUserAction {
         String COOKIE_MANAGER_ACCEPT_COOKIE = "CookieManagerAcceptCookie";
@@ -1182,6 +1191,11 @@ class WebViewChromium
         String WEB_STORAGE_GET_ORIGINS = "WebStorageGetOrigins";
         String WEB_STORAGE_GET_QUOTA_FOR_ORIGIN = "WebStorageGetQuotaForOrigin";
         String WEB_STORAGE_GET_USAGE_FOR_ORIGIN = "WebStorageGetUsageForOrigin";
+        String WEBVIEW_INSTANCE_WEBVIEW_CHROMIUM_CONSTRUCTOR =
+                "WebViewInstanceWebViewChromiumConstructor";
+        String WEBVIEW_INSTANCE_WEBVIEW_CHROMIUM_INIT = "WebViewInstanceWebViewChromiumInit";
+        String WEBVIEW_INSTANCE_WEBVIEW_CHROMIUM_INIT_FOR_REAL =
+                "WebViewInstanceWebViewChromiumInitForReal";
     }
 
     // LINT.ThenChange(//tools/metrics/actions/actions.xml)
@@ -1251,6 +1265,9 @@ class WebViewChromium
             WebView webView,
             WebView.PrivateAccess webViewPrivate) {
         try (ScopedSysTraceEvent e1 = ScopedSysTraceEvent.scoped("WebViewChromium.constructor")) {
+            recordWebViewApiCall(
+                    ApiCall.WEBVIEW_CHROMIUM_CONSTRUCTOR,
+                    ApiCallUserAction.WEBVIEW_INSTANCE_WEBVIEW_CHROMIUM_CONSTRUCTOR);
             WebViewChromiumFactoryProvider.checkStorageIsNotDeviceProtected(webView.getContext());
             mWebView = webView;
             mWebViewPrivate = webViewPrivate;
@@ -1280,6 +1297,9 @@ class WebViewChromium
     // so is ignored. TODO: remove it from WebViewProvider.
     public void init(
             final Map<String, Object> javaScriptInterfaces, final boolean privateBrowsing) {
+        recordWebViewApiCall(
+                ApiCall.WEBVIEW_CHROMIUM_INIT,
+                ApiCallUserAction.WEBVIEW_INSTANCE_WEBVIEW_CHROMIUM_INIT);
         long startTime = SystemClock.uptimeMillis();
         boolean wasChromiumAlreadyInitialized = mAwInit.isChromiumInitialized();
         boolean isFirstWebViewInstance = !sFirstWebViewInstanceCreated.getAndSet(true);
@@ -1402,6 +1422,9 @@ class WebViewChromium
 
     private void initForReal() {
         try (DualTraceEvent ignored = DualTraceEvent.scoped("WebViewChromium.initForReal")) {
+            recordWebViewApiCall(
+                    ApiCall.WEBVIEW_CHROMIUM_INIT_FOR_REAL,
+                    ApiCallUserAction.WEBVIEW_INSTANCE_WEBVIEW_CHROMIUM_INIT_FOR_REAL);
             AwContentsStatics.setRecordFullDocument(
                     sRecordWholeDocumentEnabledByApi
                             || mAppTargetSdkVersion < Build.VERSION_CODES.LOLLIPOP);
