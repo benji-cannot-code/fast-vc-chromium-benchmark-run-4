d8ac780e1d1c726270b284c604a3b967c71d7fac
Dhruv Kathpalia <dhruvkathpalia@google.com>
2026-02-05 14:51:42 -0800

    [composebox] Remove onInputStateChanged listener from searchbox.ts.
    The searchbox no longer needs to listen for onInputStateChanged events
    from the callback router as there is no other state that the context
    menu can be in for the searchbox. The input state is now only fetched
    once during connectedCallback.
    Screencast:
    http://screencast/cast/NjIyOTcwMTg0MzQ4NDY3MnxhYWUzNzEzYi04OQ
    
    Bug: b:481388504
    Change-Id: I04cb1630b108d6a75331db12d1174bc068f9e9a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7543274
    Commit-Queue: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Auto-Submit: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1580440}

--- ui/webui/resources/cr_components/composebox/composebox.ts
+++ ui/webui/resources/cr_components/composebox/composebox.ts
@@ -1143,8 +1143,6 @@ export class ComposeboxElement extends I18nMixinLit
 
   private closeComposebox_() {
     this.resetModes();
-    this.searchboxHandler_.setActiveToolMode(ToolMode.kUnspecified);
-    this.searchboxHandler_.setActiveModelMode(ModelMode.kUnspecified);
     this.fire('close-composebox', {composeboxText: this.input_});
 
     if (this.isCollapsible) {
