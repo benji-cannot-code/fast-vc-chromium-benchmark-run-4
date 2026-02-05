0a403423928a60e4eb4e2f554dbb8edfafaafc68
viplavkadam <viplavkadam@google.com>
2026-02-04 22:02:19 -0800

    [BNPL][Clank] Fix for showing VCN on snackbar
    Currently, when a BNPL flow is successful and a bubble appears to see
    the virtual card, clicking the “Show card” button shows the virtual
    card to the user. However, for some websites, we are unable to show
    it. This occurs because the BrowserAutofillManager used for caching
    the virtual card is different from the one used when the virtual card
    is retrieved via the snackbar. To fix this issue, we have made
    PaymentMethodAccessoryControllerImpl an AutofillManager observer. When
    a form is filled, it triggers OnFillOrPreviewForm on the controller to
    set the correct BrowserAutofillManager, ensuring the same instance is
    used for both caching and retrieving.
    
    Video:
    Before:
    http://screencast/cast/NTUzOTg2MTMwOTE2MTQ3Mnw2MTU0MTRhOS02Zg
    After:
    http://screencast/cast/NDg2NTk5ODEyMDI4ODI1Nnw2MDg4MTc5NC1lNg
    
    Design: go/autofill-clank-bnpl
    
    Bug: 476119897
    Change-Id: I56250695d3ddcb9903d708f5a84217de3b7aa5b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7538830
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Commit-Queue: Viplav Kadam <viplavkadam@google.com>
    Cr-Commit-Position: refs/heads/main@{#1579892}

--- chrome/browser/keyboard_accessory/android/payment_method_accessory_controller_impl.h
+++ chrome/browser/keyboard_accessory/android/payment_method_accessory_controller_impl.h
@@ -17,12 +17,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/data_manager/payments/payments_data_manager.h"
 #include "components/autofill/core/browser/data_manager/valuables/valuables_data_manager.h"
 #include "components/autofill/core/browser/data_model/valuables/loyalty_card.h"
+#include "components/autofill/core/browser/foundations/scoped_autofill_managers_observation.h"
 #include "content/public/browser/web_contents_user_data.h"
 
 class ManualFillingController;
 
 namespace autofill {
 
+class AutofillManager;
 class BrowserAutofillManager;
 
 // Use either PaymentMethodAccessoryController::GetOrCreate or
@@ -32,8 +34,8 @@ class PaymentMethodAccessoryControllerImpl
     : public PaymentMethodAccessoryController,
       public PaymentsDataManager::Observer,
       public ValuablesDataManager::Observer,
-      public content::WebContentsUserData<
-          PaymentMethodAccessoryControllerImpl> {
+      public content::WebContentsUserData<PaymentMethodAccessoryControllerImpl>,
+      public AutofillManager::Observer {
  public:
   ~PaymentMethodAccessoryControllerImpl() override;
 
@@ -125,6 +127,14 @@ class PaymentMethodAccessoryControllerImpl
   // ValuablesDataManager::Observer:
   void OnValuablesDataChanged() override;
 
+  // AutofillManager::Observer:
+  void OnFillOrPreviewForm(
+      AutofillManager&,
+      FormGlobalId,
+      mojom::ActionPersistence action_persistence,
+      const base::flat_set<FieldGlobalId>& filled_field_ids,
+      const FillingPayload&) override;
+
   const PaymentsDataManager* paydm() const {
     return paydm_observation_.GetSource();
   }
@@ -136,6 +146,9 @@ class PaymentMethodAccessoryControllerImpl
   base::WeakPtr<ManualFillingController> mf_controller_;
   raw_ptr<BrowserAutofillManager> af_manager_for_testing_ = nullptr;
   raw_ptr<AutofillDriver> af_driver_for_testing_ = nullptr;
+
+  // The browser autofill manager associated with the last form fill.
+  base::WeakPtr<BrowserAutofillManager> browser_autofill_manager_;
 
   // The observer to notify if available suggestions change.
   FillingSourceObserver source_observer_;
@@ -151,6 +164,9 @@ class PaymentMethodAccessoryControllerImpl
   // Observes the `ValuablesDataManager` of the profile to react to updates.
   base::ScopedObservation<ValuablesDataManager, ValuablesDataManager::Observer>
       valuables_data_manager_observation_{this};
+
+  // The observation for the Autofill managers of the WebContents.
+  ScopedAutofillManagersObservation autofill_managers_observation_{this};
 
   WEB_CONTENTS_USER_DATA_KEY_DECL();
 
