19209c52b6567a3147fce984e3a05e1f09c98135
Neil Coronado <nemco@google.com>
2026-02-05 14:50:31 -0800

    [SidePanel] Add SideUiContainer interface
    Add SideUiContainer interface.
    -Add an enum, ANCHOR_SIDE, with values START and END.
    -Add a POD-type SideUiContainerProperties that contains a container's
     requested properties: ANCHOR_SIDE and width.
    -SideUiContainer has methods:
      -#getView so the Coordinator can attach the View to the appropriately
       anchored ViewGroup.
      -#determineContainerWidth to "accept" a new width given the available
       width and window width.
      -#getCurrentWidth
      -#setWidth
    -SideUiCoordinator has methods:
      -#(un)registerSideUiContainer
      -#requestUpdateContainer(SideUiContainerProperties)
    
    Bug: 478305347
    Change-Id: I9603c81d73023a7a14f1ee657fac041080e8fc23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7514887
    Reviewed-by: Linyu He <linyuh@google.com>
    Commit-Queue: Neil Coronado <nemco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1580438}

--- chrome/browser/ui/side_ui/internal/android/java/src/org/chromium/chrome/browser/ui/side_ui/SideUiCoordinatorImpl.java
+++ chrome/browser/ui/side_ui/internal/android/java/src/org/chromium/chrome/browser/ui/side_ui/SideUiCoordinatorImpl.java
@@ -6,11 +6,34 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.ui.side_ui;
 
 import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
 
 /** Implementation of {@link SideUiCoordinator}. */
 @NullMarked
 final class SideUiCoordinatorImpl implements SideUiCoordinator {
 
+    // TODO(crbug.com/478338737): Update to account for multiple side containers.
+    @Nullable private SideUiContainer mSideUiContainer;
+
     @Override
-    public void destroy() {}
+    public void registerSideUiContainer(SideUiContainer sideUiContainer) {
+        assert mSideUiContainer == null : "Registering a SideUiContainer when already set.";
+        mSideUiContainer = sideUiContainer;
+    }
+
+    @Override
+    public void unregisterSideUiContainer(SideUiContainer sideUiContainer) {
+        assert mSideUiContainer == sideUiContainer : "Unregistering unknown SideUiContainer.";
+        mSideUiContainer = null;
+    }
+
+    @Override
+    public void requestUpdateContainer(SideUiContainerProperties properties) {
+        // TODO(crbug.com/478306743): Implement.
+    }
+
+    @Override
+    public void destroy() {
+        if (mSideUiContainer != null) unregisterSideUiContainer(mSideUiContainer);
+    }
 }
