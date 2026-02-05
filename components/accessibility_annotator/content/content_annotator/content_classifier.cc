dffe52721363fbb55314a8f30ff63ae26a73aa89
Andrew Vuong <akvuong@google.com>
2026-02-05 12:19:04 -0800

    Add ContentClassifier file for content annotation.
    This change introduces the basic structure and types for
    ContentClassifier within the accessibility annotator component. It
    includes a ContentClassificationInput struct to gather necessary data, a
    ContentClassificationResult struct, and a placeholder function
    RunContentClassification. Follow-up CLs will add logic to create,
    store and route input for classification in content_annotator_service.
    
    Bug: 475859254
    Change-Id: I050c5c4c473a32db997eadfc3411d82e9d60d005
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7486131
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Andrew Vuong <akvuong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1580337}

---
+++ components/accessibility_annotator/content/content_annotator/content_classifier.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/accessibility_annotator/content/content_annotator/content_classifier.h"
+
+namespace accessibility_annotator {
+
+ContentClassificationInput::ContentClassificationInput(GURL url) : url(url) {}
+ContentClassificationInput::~ContentClassificationInput() = default;
+ContentClassificationInput::ContentClassificationInput(
+    const ContentClassificationInput& other) = default;
+
+void ContentClassificationInput::MergeFrom(
+    const ContentClassificationInput& data) {
+  // TODO(crbug.com/475859254): Implement logic to merge the data.
+}
+
+bool ContentClassificationInput::IsComplete() const {
+  return sensitivity_score.has_value() && navigation_timestamp.has_value() &&
+         adopted_language.has_value() && page_title.has_value();
+}
+
+ContentClassificationResult RunContentClassification(
+    const ContentClassificationInput& input) {
+  // TODO(crbug.com/479259274): Implement running classification on the input.
+  return ContentClassificationResult();
+}
+
+}  // namespace accessibility_annotator
