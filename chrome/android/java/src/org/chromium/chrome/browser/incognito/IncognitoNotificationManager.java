43aa9bb851737905f2983dd0e3cc327b86f18560
Calder Kitagawa <ckitagawa@chromium.org>
2026-02-05 08:35:09 -0800

    Migrate more incognito icons
    Bug: 373905454
    Change-Id: I368b04cbc20cd569edb02dc8cfcfff8b84e79b3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7545988
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Dan Polanco <polardz@google.com>
    Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1580150}

--- chrome/android/java/src/org/chromium/chrome/browser/incognito/IncognitoNotificationManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/incognito/IncognitoNotificationManager.java
@@ -50,7 +50,7 @@ public class IncognitoNotificationManager {
                         .setContentText(actionMessage)
                         .setOngoing(true)
                         .setVisibility(Notification.VISIBILITY_SECRET)
-                        .setSmallIcon(R.drawable.ic_incognito_fill_24dp)
+                        .setSmallIcon(R.drawable.ic_incognito)
                         .setShowWhen(false)
                         .setLocalOnly(true)
                         .setGroup(NotificationConstants.GROUP_INCOGNITO);
