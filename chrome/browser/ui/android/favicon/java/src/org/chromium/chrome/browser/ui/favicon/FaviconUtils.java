d8ee78fd8d628e9e43bbf20c579cb8d1aff7f000
Hitarth Kothari <hitarthkothari@google.com>
2026-02-05 12:38:45 -0800

    Cleanup AndroidHistoryPaneFavicons
    Bug: 470958491
    Change-Id: I87e48ea4f19a37a749cab31cea8109a38875334a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7548629
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Hitarth Kothari <hitarthkothari@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Hitarth Kothari <hitarthkothari@google.com>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1580348}

--- chrome/browser/ui/android/favicon/java/src/org/chromium/chrome/browser/ui/favicon/FaviconUtils.java
+++ chrome/browser/ui/android/favicon/java/src/org/chromium/chrome/browser/ui/favicon/FaviconUtils.java
@@ -22,7 +22,6 @@ import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.NullUnmarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.components.browser_ui.widget.RoundedIconGenerator;
-import org.chromium.ui.base.UiAndroidFeatureList;
 import org.chromium.ui.base.ViewUtils;
 import org.chromium.url.GURL;
 
@@ -38,10 +37,6 @@ public class FaviconUtils {
      */
     @Px
     public static int getDefaultFaviconSize(Context context) {
-        if (!UiAndroidFeatureList.sAndroidHistoryPaneFavicons.isEnabled()) {
-            // This returns a pixel size. This is legacy behavior.
-            return context.getResources().getDimensionPixelSize(R.dimen.default_favicon_min_size);
-        }
         return context.getResources().getDimensionPixelSize(R.dimen.default_favicon_size);
     }
 
--- ui/android/ui_android_feature_map.cc
+++ ui/android/ui_android_feature_map.cc
@@ -30,7 +30,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &ui::kRequireLeadingInTextViewWithLeading,
     &ui::kSelectFileOpenDocument,
     &ui::kAndroidTouchpadOverscrollHistoryNavigation,
-    &ui::kAndroidHistoryPaneFavicons,
 };
 
 // static
