1a8fd6894b1eb567bde87ef8b235f8735f8c6c1b
ginnnnnnny <ginnyhuang@chromium.org>
2026-02-05 13:52:47 -0800

    [iOS][mvt-customization] Earl grey tests (1/2)
    This CL adds 3 test cases for most visited tiles customization:
    
    - Tests that the user can pin/unpin site with context menu
    - Tests that the "Add Site" form works and handles error cases correctly
    - Tests that the user could edit a site with the form
    
    Still debugging, will upload later:
    
    - Tests that the user could pin a total of 8 sites
    - Drag and drop
    
    Also removed some strings that actually aren't needed.
    
    Bug: 481748477
    Change-Id: I7faca9380f2074e266ef6a6839984278d72537f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7546677
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1580408}

--- ios/chrome/browser/content_suggestions/most_visited_tiles/ui/pinned_site_form_view_controller.mm
+++ ios/chrome/browser/content_suggestions/most_visited_tiles/ui/pinned_site_form_view_controller.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/metrics/user_metrics.h"
 #import "base/metrics/user_metrics_action.h"
 #import "base/strings/sys_string_conversions.h"
+#import "components/strings/grit/components_strings.h"
 #import "ios/chrome/browser/content_suggestions/most_visited_tiles/public/metrics.h"
 #import "ios/chrome/browser/content_suggestions/most_visited_tiles/public/pinned_site_action.h"
 #import "ios/chrome/browser/content_suggestions/most_visited_tiles/ui/most_visited_item.h"
@@ -106,13 +107,11 @@ NSAttributedString* GetDisclaimerForModificationForm() {
   switch (_action) {
     case PinnedSiteAction::kCreate:
       titleId = IDS_IOS_CONTENT_SUGGESTIONS_PIN_SITE_ADD_PINNED_SITE_TITLE;
-      doneButtonTextId =
-          IDS_IOS_CONTENT_SUGGESTIONS_PIN_SITE_ADD_PINNED_SITE_APPLY_BUTTON;
+      doneButtonTextId = IDS_ADD;
       break;
     case PinnedSiteAction::kModify:
       titleId = IDS_IOS_CONTENT_SUGGESTIONS_PIN_SITE_EDIT_PINNED_SITE_TITLE;
-      doneButtonTextId =
-          IDS_IOS_CONTENT_SUGGESTIONS_PIN_SITE_EDIT_PINNED_SITE_APPLY_BUTTON;
+      doneButtonTextId = IDS_SAVE;
       break;
   }
   self.navigationItem.title = l10n_util::GetNSString(titleId);
