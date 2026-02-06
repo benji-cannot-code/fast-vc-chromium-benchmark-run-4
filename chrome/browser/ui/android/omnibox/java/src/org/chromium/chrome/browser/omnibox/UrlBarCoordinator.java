82c3e2bee40dc314dd625e6f2a28d297a3c0a351
Fred Shih <ffred@chromium.org>
2026-02-06 15:50:21 -0800

    Reland "[AIM] Stop calling updateUrl() on branded color scheme change."
    This reverts commit 67c14d563d06dd9a495d5e29836a545512de5826.
    
    Reason for revert: This breaks the build:
    
    https://ci.chromium.org/ui/p/chromium/builders/ci/android-desktop-x64-compile-rel/66384/overview
    
    Original change's description:
    > Revert "[AIM] Stop calling updateUrl() on branded color scheme change."
    >
    > This reverts commit a35240cc65c4326075363d41690d22048dcce758.
    >
    > Reason for revert: Wrong url color when toggling incog.
    >
    > Original change's description:
    > > [AIM] Stop calling updateUrl() on branded color scheme change.
    > >
    > > The text itself does not change when the color scheme changes.
    > > Reapplying the current url and UrlBarData does not have any effect.
    > > Instead all the sub components that need to change how they display UI
    > > elements are directly notified about the color scheme already.
    > >
    > > This conditional updateUrl() makes it difficult to rework how color
    > > scheme is tracked in the future without removing this performance
    > > optimization.
    > >
    > > Bug: 481357849
    > > Change-Id: Ie0c2979af4e8e52a498aef33606a3fb983940e0a
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7540979
    > > Commit-Queue: Sky Malice <skym@chromium.org>
    > > Reviewed-by: Patrick Noland <pnoland@chromium.org>
    > > Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    > > Reviewed-by: Sinan Sahin <sinansahin@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1579528}
    >
    > Bug: 481357849, 482210535
    > Bug: 481357849
    > Change-Id: Id60740d87690f27bb7d4ec036a1920f72da908e2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7551558
    > Auto-Submit: Sky Malice <skym@chromium.org>
    > Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    > Commit-Queue: Sky Malice <skym@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1581145}
    
    Bug: 481357849, 482210535
    Bug: 481357849
    Change-Id: I0e8ecca7414210191625c2bd6dca04c16a2798af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7552802
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Owners-Override: Fred Shih <ffred@chromium.org>
    Commit-Queue: Fred Shih <ffred@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1581154}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/UrlBarCoordinator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/UrlBarCoordinator.java
@@ -184,8 +184,8 @@ public class UrlBarCoordinator
     /**
      * @see UrlBarMediator#setBrandedColorScheme(int)
      */
-    public boolean setBrandedColorScheme(@BrandedColorScheme int brandedColorScheme) {
-        return mMediator.setBrandedColorScheme(brandedColorScheme);
+    public void setBrandedColorScheme(@BrandedColorScheme int brandedColorScheme) {
+        mMediator.setBrandedColorScheme(brandedColorScheme);
     }
 
     /**
