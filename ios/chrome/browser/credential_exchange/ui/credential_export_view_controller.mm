86edc377b137d008bdccf8fb196f9cd6e0ba452d
Gauthier Ambard <gambard@chromium.org>
2026-02-06 05:27:56 -0800

    [iOS] Fix favicon updates in table view
    This CL updates how the favicon are updated in the TableView by updating
    the cell instead of reconfiguring, as reconfiguring is creating a
    visible lag.
    
    It is also removing the estimatedRowHeight as it was creating an issue
    with the scroll indicator that was wrong and updated as the user was
    scrolling.
    
    Fixed: 479692041
    Change-Id: I631503acd6d7a72415a31e87a9f9504ad4263377
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7545358
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1580763}

--- ios/chrome/browser/credential_exchange/ui/credential_export_view_controller.mm
+++ ios/chrome/browser/credential_exchange/ui/credential_export_view_controller.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/credential_exchange/ui/credential_export_view_controller.h"
 
+#import "base/apple/foundation_util.h"
 #import "base/strings/string_number_conversions.h"
 #import "base/strings/sys_string_conversions.h"
 #import "components/password_manager/core/browser/ui/affiliated_group.h"
@@ -391,7 +392,10 @@ NSString* const kCredentialSectionIdentifier = @"CredentialSection";
 
   const password_manager::AffiliatedGroup& group = identifier.affiliatedGroup;
 
-  [self configureCell:cell withGroup:group identifier:identifier];
+  [self configureCell:cell
+            withGroup:group
+           identifier:identifier
+          atIndexPath:indexPath];
   [self updateAccessibilityTraitsForCell:cell indexPath:indexPath];
 
   return cell;
@@ -400,7 +404,8 @@ NSString* const kCredentialSectionIdentifier = @"CredentialSection";
 // Helper to configure the cell.
 - (void)configureCell:(UITableViewCell*)cell
             withGroup:(const password_manager::AffiliatedGroup&)group
-           identifier:(CredentialGroupIdentifier*)identifier {
+           identifier:(CredentialGroupIdentifier*)identifier
+          atIndexPath:(NSIndexPath*)indexPath {
   TableViewCellContentConfiguration* contentConfig =
       [[TableViewCellContentConfiguration alloc] init];
 
@@ -412,7 +417,9 @@ NSString* const kCredentialSectionIdentifier = @"CredentialSection";
   contentConfig.subtitleNumberOfLines = 1;
   contentConfig.subtitleLineBreakMode = NSLineBreakByTruncatingTail;
 
-  [self loadFaviconForContentConfiguration:contentConfig identifier:identifier];
+  [self loadFaviconForContentConfiguration:contentConfig
+                                identifier:identifier
+                               atIndexPath:indexPath];
 
   cell.contentConfiguration = contentConfig;
   cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
@@ -423,11 +430,40 @@ NSString* const kCredentialSectionIdentifier = @"CredentialSection";
   cell.selectedBackgroundView = selectedBackgroundView;
 }
 
+// Called when a favicon is fetched.
+- (void)didFetchFaviconAttributes:(FaviconAttributes*)attributes
+                           cached:(bool)cached
+                    configuration:
+                        (TableViewCellContentConfiguration*)contentConfig
+                       identifier:(CredentialGroupIdentifier*)identifier
+                        indexPath:(NSIndexPath*)indexPath {
+  if (cached) {
+    FaviconContentConfiguration* newFaviconConfig =
+        [[FaviconContentConfiguration alloc] init];
+    newFaviconConfig.faviconAttributes = attributes;
+    contentConfig.leadingConfiguration = newFaviconConfig;
+  } else if (attributes.faviconImage) {
+    if ([_dataSource indexPathForItemIdentifier:identifier] != indexPath) {
+      return;
+    }
+    UITableViewCell* cell = [self.tableView cellForRowAtIndexPath:indexPath];
+    TableViewCellContentConfiguration* configuration =
+        base::apple::ObjCCastStrict<TableViewCellContentConfiguration>(
+            cell.contentConfiguration);
+    FaviconContentConfiguration* newFaviconConfig =
+        [[FaviconContentConfiguration alloc] init];
+    newFaviconConfig.faviconAttributes = attributes;
+    configuration.leadingConfiguration = newFaviconConfig;
+    cell.contentConfiguration = configuration;
+  }
+}
+
 // Helper to load favicon and update configuration.
 - (void)loadFaviconForContentConfiguration:
             (TableViewCellContentConfiguration*)contentConfig
                                 identifier:
-                                    (CredentialGroupIdentifier*)identifier {
+                                    (CredentialGroupIdentifier*)identifier
+                               atIndexPath:(NSIndexPath*)indexPath {
   FaviconContentConfiguration* faviconConfiguration =
       [[FaviconContentConfiguration alloc] init];
   contentConfig.leadingConfiguration = faviconConfiguration;
@@ -442,14 +478,11 @@ NSString* const kCredentialSectionIdentifier = @"CredentialSection";
   [self.faviconProvider
       fetchFaviconForURL:URL
               completion:^(FaviconAttributes* attributes, BOOL cached) {
-                if (cached) {
-                  FaviconContentConfiguration* newFaviconConfig =
-                      [[FaviconContentConfiguration alloc] init];
-                  newFaviconConfig.faviconAttributes = attributes;
-                  contentConfig.leadingConfiguration = newFaviconConfig;
-                } else if (attributes.faviconImage) {
-                  [weakSelf reconfigureItem:identifier];
-                }
+                [weakSelf didFetchFaviconAttributes:attributes
+                                             cached:cached
+                                      configuration:contentConfig
+                                         identifier:identifier
+                                          indexPath:indexPath];
               }];
 }
 
