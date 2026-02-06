28f00c7198d51880488ff06c354f9730df8c371c
Gauthier Ambard <gambard@chromium.org>
2026-02-06 08:33:47 -0800

    [iOS] Fix toolbar calls with Next
    When Next is enabled, some call should return early.
    
    Bug: 472279443
    Change-Id: I2e8797d71f9d07870bdc4f5d0a200a84fbd6829d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7548768
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1580878}

--- ios/chrome/browser/browser_view/ui_bundled/browser_view_controller.mm
+++ ios/chrome/browser/browser_view/ui_bundled/browser_view_controller.mm
@@ -2244,8 +2244,8 @@ const CGFloat kMultilineOmniboxAnimationDuration = 0.3f;
   // Check that the computed height has a realistic value (crbug.com/1446068).
   DUMP_WILL_BE_CHECK(height >= (0.0 - FLT_EPSILON) &&
                      height <= (expandedToolbarHeight + FLT_EPSILON));
-
-  if (![self.toolbarCoordinator showingOmniboxPopup]) {
+  if (IsChromeNextIaEnabled() ||
+      ![self.toolbarCoordinator showingOmniboxPopup]) {
     self.secondaryToolbarHeightConstraint.constant = height;
   }
 }
@@ -2823,6 +2823,9 @@ const CGFloat kMultilineOmniboxAnimationDuration = 0.3f;
 }
 
 - (BOOL)canBeginToolbarSwipe {
+  if (IsChromeNextIaEnabled()) {
+    return YES;
+  }
   return ![self.toolbarCoordinator isOmniboxFirstResponder] &&
          ![self.toolbarCoordinator showingOmniboxPopup];
 }
