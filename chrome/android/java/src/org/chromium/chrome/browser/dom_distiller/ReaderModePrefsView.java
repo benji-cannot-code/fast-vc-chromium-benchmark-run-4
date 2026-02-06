1404cd4bd6e47f0a3b87402e3cd5a39afb24d288
Brandon Wylie <wylieb@google.com>
2026-02-06 10:36:35 -0800

    [reader mode] Record metric for toggle links bottom sheet action
    Bug: 479933493
    Change-Id: Ia4a57e5cb49fe4560e242afcae267e799cc2620d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7539456
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Reviewed-by: Evan Luo <evanluo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1580968}

--- chrome/android/java/src/org/chromium/chrome/browser/dom_distiller/ReaderModePrefsView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/dom_distiller/ReaderModePrefsView.java
@@ -194,7 +194,9 @@ public class ReaderModePrefsView extends LinearLayout
         if (mToggleLinksButton != null) {
             mToggleLinksButton.setOnClickListener(
                     v -> {
-                        mDistilledPagePrefs.setLinksEnabled(!mDistilledPagePrefs.getLinksEnabled());
+                        boolean enabled = !mDistilledPagePrefs.getLinksEnabled();
+                        ReaderModeMetrics.reportReaderModePrefsLinksEnabled(enabled);
+                        mDistilledPagePrefs.setLinksEnabled(enabled);
                     });
         }
     }
