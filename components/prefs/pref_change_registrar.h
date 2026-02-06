d85856d0089b03a3b13fbb76faf6d35e9bc447c6
Jeffrey Yu <yuje@chromium.org>
2026-02-06 01:14:42 -0800

    Replace `std::unordered_map` with `absl::flat_hash_map`.
    The Chromium containers guide
    (https://chromium.googlesource.com/chromium/src/+/HEAD/base/containers/README.md)
    recommends using `absl::flat_hash_map` as a default map choice instead
    of `std::unordered_map` when pointer stability isn't needed, and
    `absl::node_hash_map` when it is. Both will still generally perform
    better and require less overhead than `std::unordered_map`.
    
    Compile-Size: Size increase is unavoidable.
    Bug: 473916362
    Change-Id: I1aca3af75f3bcb4e344c8cdc54a3aeb3ae4c6246
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7546737
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Auto-Submit: Jeffrey Yu <yuje@chromium.org>
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Commit-Queue: Dominic Battre <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1580665}

--- components/prefs/pref_change_registrar.h
+++ components/prefs/pref_change_registrar.h
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "components/prefs/pref_observer.h"
 #include "components/prefs/prefs_export.h"
-#include "components/prefs/transparent_unordered_string_map.h"
+#include "third_party/abseil-cpp/absl/container/flat_hash_map.h"
 
 class PrefService;
 
@@ -82,7 +82,8 @@ class COMPONENTS_PREFS_EXPORT PrefChangeRegistrar final : public PrefObserver {
   void OnPreferenceChanged(PrefService* service,
                            std::string_view pref_name) final;
 
-  using ObserverMap = TransparentUnorderedStringMap<NamedChangeAsViewCallback>;
+  using ObserverMap =
+      absl::flat_hash_map<std::string, NamedChangeAsViewCallback>;
 
   ObserverMap observers_;
   raw_ptr<PrefService, AcrossTasksDanglingUntriaged> service_;
