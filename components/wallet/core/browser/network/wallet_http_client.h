43526a85986e4cca3bf88b1d7c0579e276610eb9
Norge Vizcay <vizcay@google.com>
2026-02-06 09:42:49 -0800

    Introduce new UpsertPrivatePass in wallet http client.
    This CL adds the UpsertPrivatePass method to WalletHttpClient and
    migrates the UpsertPrivatePassRequest implementation to use protocol
    buffers instead of JSON. It includes new proto definitions for the
    response, updates request building logic to use ClientInfo protos, and
    adds comprehensive unit tests.
    
    Bug: 478783796
    Change-Id: I4f7a00874dfcf6f53c5e3918b4d88dd9446b458d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7546123
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1580917}

--- components/wallet/core/browser/network/wallet_http_client.h
+++ components/wallet/core/browser/network/wallet_http_client.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/types/expected.h"
 #include "components/wallet/core/browser/data_models/wallet_pass.h"
+#include "components/wallet/core/browser/proto/private_pass.pb.h"
 
 namespace wallet {
 
@@ -20,16 +21,22 @@ class WalletHttpClient {
   enum class WalletRequestError {
     kGenericError = 1,
     kAccessTokenFetchFailed = 2,
+    kParseResponseFailed = 2,
     // TODO(crbug.com/468915960): Add more error codes.
   };
 
   // Callback for UpsertPass requests. On success, it returns the `WalletPass`
   // as it is stored in the Wallet backend (including its `id`).
+  // TODO(crbug.com/478783796): Update to use protos.
   using UpsertPassCallback = base::OnceCallback<void(
       const base::expected<WalletPass, WalletRequestError>&)>;
 
+  using UpsertPrivatePassCallback = base::OnceCallback<void(
+      const base::expected<PrivatePass, WalletRequestError>&)>;
+
   // Callback for GetUnmaskedPass requests. On success, it returns the
   // `WalletPass` corresponding to the requested `pass_id`.
+  // TODO(crbug.com/478783796): Update to use protos.
   using GetUnmaskedPassCallback = base::OnceCallback<void(
       const base::expected<WalletPass, WalletRequestError>&)>;
 
@@ -40,9 +47,17 @@ class WalletHttpClient {
   // Upserts a pass to the Wallet backend. If the `pass.id` is missing, it
   // will save a new pass. If the `pass.id` is present, it will attempt to
   // update the existing pass.
+  // TODO(crbug.com/478783796): Update to use protos.
   virtual void UpsertPass(WalletPass pass, UpsertPassCallback callback) = 0;
 
+  // Upserts a pass to the Wallet backend. If the `pass.id` is missing, it
+  // will save a new pass. If the `pass.id` is present, it will attempt to
+  // update the existing pass.
+  virtual void UpsertPrivatePass(PrivatePass pass,
+                                 UpsertPrivatePassCallback callback) = 0;
+
   // Retrieves the unmasked version of the pass for the given `pass_id`.
+  // TODO(crbug.com/478783796): Update to use protos.
   virtual void GetUnmaskedPass(std::string_view pass_id,
                                GetUnmaskedPassCallback callback) = 0;
 };
--- components/wallet/core/browser/network/wallet_request.cc
+++ components/wallet/core/browser/network/wallet_request.cc
@@ -10,12 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace wallet {
 
-base::DictValue WalletRequest::BuildClientInfo() {
-  base::DictValue chrome_client_info =
-      base::DictValue().Set("version", version_info::GetVersionNumber());
-
-  return base::DictValue().Set("chrome_client_info",
-                               std::move(chrome_client_info));
+ClientInfo WalletRequest::BuildClientInfo() {
+  ClientInfo client_info;
+  client_info.mutable_chrome_client_info()->set_version(
+      version_info::GetVersionNumber());
+  return client_info;
 }
 
 }  // namespace wallet
