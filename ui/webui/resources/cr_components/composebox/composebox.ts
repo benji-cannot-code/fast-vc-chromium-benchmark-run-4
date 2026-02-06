f038dd93db84ebc9b56a53ad98601ac303570d6c
Dhruv Kathpalia <dhruvkathpalia@google.com>
2026-02-06 13:30:22 -0800

    [composebox] Reset tool and model modes when closing composebox.
    When the composebox is closed, the active tool mode is now reset to
    `kUnspecified` and the active model mode is reset to the first allowed
    model or `kUnspecified`.
    Screencast:
    http://screencast/cast/NjQ0NTA4NTc5NjMzNTYxNnwzN2FkMWY2MS0wOQ
    
    Bug: b:482162637
    Change-Id: Ib7b25167217ee701b69ca2d620a15c3682ca7e13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7551375
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Auto-Submit: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Commit-Queue: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Cr-Commit-Position: refs/heads/main@{#1581093}

--- ui/webui/resources/cr_components/composebox/composebox.ts
+++ ui/webui/resources/cr_components/composebox/composebox.ts
@@ -1144,6 +1144,11 @@ export class ComposeboxElement extends I18nMixinLit
   private closeComposebox_() {
     this.resetModes();
     this.searchboxHandler_.clearFiles();
+    if (this.inputState_) {
+      this.searchboxHandler_.setActiveToolMode(ToolMode.kUnspecified);
+      this.searchboxHandler_.setActiveModelMode(
+          this.inputState_?.allowedModels[0] || ModelMode.kUnspecified);
+    }
     this.fire('close-composebox', {composeboxText: this.input_});
 
     if (this.isCollapsible) {
