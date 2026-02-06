36ae5bde9a81b8eaa27067d003f3a3a30ade3d72
Julia Sobiech <jsobiech@google.com>
2026-02-06 05:38:53 -0800

    Replace kYourSavedInfoSubPage with kAutofillSubPage.
    This change updates all entry points that previously navigated to the
    "yourSavedInfo" settings subpage to now point to the "autofill" subpage.
    The kYourSavedInfoSubPage constant is removed as it is no longer used.
    
    Bug: 478669999
    Change-Id: I36a9f7438058ef54968ba58a0ec435fe850a6d79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7545181
    Commit-Queue: Julia Sobiech <jsobiech@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1580769}

--- chrome/browser/ui/chrome_pages.h
+++ chrome/browser/ui/chrome_pages.h
@@ -148,7 +148,7 @@ void ShowPaymentMethods(BrowserWindowInterface* bwi);
 void ShowContactInfo(BrowserWindowInterface* bwi);
 void ShowIdentityDocs(BrowserWindowInterface* bwi);
 void ShowTravel(BrowserWindowInterface* bwi);
-void ShowYourSavedInfo(BrowserWindowInterface* bwi);
+void ShowAutofill(BrowserWindowInterface* bwi);
 void ShowAllSitesSettingsFilteredByRwsOwner(
     Browser* browser,
     const std::string& rws_owner_host_name);
