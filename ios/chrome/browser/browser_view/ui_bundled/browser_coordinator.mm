9bad15d9b0586ac5662f4d7122a9be85cb04f786
Thomas Thrainer <thomasth@google.com>
2026-02-06 02:56:49 -0800

    Implement completion callbacks for `showTrustedVaultReauth*` methods
    The implementation makes sure to call the `completion` callback (if not
    `nil`) after the trusted vault reauth related UIs are dismissed.
    
    Bug: 464228247
    Change-Id: Ifd7c577155abf38a84e89d7e0d1542b57c906cb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7415850
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Thomas Thrainer <thomasth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1580716}

--- ios/chrome/browser/browser_view/ui_bundled/browser_coordinator.mm
+++ ios/chrome/browser/browser_view/ui_bundled/browser_coordinator.mm
@@ -747,6 +747,8 @@ const char kChromeAppStoreUrl[] =
   AutoDeletionCoordinator* _autoDeletionCoordinator;
   TrustedVaultReauthenticationCoordinator*
       _trustedVaultReauthenticationCoordinator;
+  SyncPresenterCompletionCallback
+      _trustedVaultReauthenticationCoordinatorCompletion;
 
   // The coordinator for the Enhanced Calendar feature UI (bottom sheet).
   EnhancedCalendarCoordinator* _enhancedCalendarCoordinator;
@@ -1123,6 +1125,11 @@ const char kChromeAppStoreUrl[] =
   [_trustedVaultReauthenticationCoordinator stop];
   _trustedVaultReauthenticationCoordinator.delegate = nil;
   _trustedVaultReauthenticationCoordinator = nil;
+
+  if (_trustedVaultReauthenticationCoordinatorCompletion) {
+    _trustedVaultReauthenticationCoordinatorCompletion();
+    _trustedVaultReauthenticationCoordinatorCompletion = nil;
+  }
 }
 
 - (void)stopPasskeyWelcomeScreenCoordinator {
@@ -4314,11 +4321,10 @@ const char kChromeAppStoreUrl[] =
                                            completion:
                                                (SyncPresenterCompletionCallback)
                                                    completion {
-  // TODO(crbug.com/464228247): Implement calling `completion` once the UI is
-  // dismissed.
-  [self showTrustedVaultReauthWithTrigger:trigger
-                                   intent:
-                                       SigninTrustedVaultDialogIntentFetchKeys];
+  [self
+      showTrustedVaultReauthWithTrigger:trigger
+                                 intent:SigninTrustedVaultDialogIntentFetchKeys
+                             completion:completion];
 }
 
 - (void)
@@ -4327,11 +4333,11 @@ const char kChromeAppStoreUrl[] =
                                                     completion:
                                                         (SyncPresenterCompletionCallback)
                                                             completion {
-  // TODO(crbug.com/464228247): Implement calling `completion` once the UI is
-  // dismissed.
   SigninTrustedVaultDialogIntent intent =
       SigninTrustedVaultDialogIntentDegradedRecoverability;
-  [self showTrustedVaultReauthWithTrigger:trigger intent:intent];
+  [self showTrustedVaultReauthWithTrigger:trigger
+                                   intent:intent
+                               completion:completion];
 }
 
 - (void)showBookmarksLimitExceededHelp {
@@ -4356,8 +4362,9 @@ const char kChromeAppStoreUrl[] =
 
 - (void)showTrustedVaultReauthWithTrigger:
             (trusted_vault::TrustedVaultUserActionTriggerForUMA)trigger
-                                   intent:
-                                       (SigninTrustedVaultDialogIntent)intent {
+                                   intent:(SigninTrustedVaultDialogIntent)intent
+                               completion:
+                                   (SyncPresenterCompletionCallback)completion {
   if (_trustedVaultReauthenticationCoordinator) {
     // This can occur in case of double-tap.
     return;
@@ -4371,6 +4378,7 @@ const char kChromeAppStoreUrl[] =
                                          kChromeSync
                              trigger:trigger];
   _trustedVaultReauthenticationCoordinator.delegate = self;
+  _trustedVaultReauthenticationCoordinatorCompletion = completion;
   [_trustedVaultReauthenticationCoordinator start];
 }
 
