dc804a0cefeca56bf349fa33be97286a403c7f35
Chris Lu <thegreenfrog@google.com>
2026-02-06 14:46:35 -0800

    [ios] Fix Tab Resumption Reconfigure Issues
    This change allows for MagicStackCollectionView calls to
    [NSDiffableDataSource reconfigureItemsWithIdentifiers:] actually result
    in a reconfigure. The issue resides in MagicStackCollectionViewCell's
    -(void)configureWithConfig:(MagicStackModule*)config. There needs to be
    a second verification that the contents of the config did not change
    before ignoring the signal and returning early.
    
    Cards that trigger a snapshot reconfigure need to implement the new
    -hasDifferentContentsFromConfig: to compare between crucial properties.
    
    Bug: 458674703
    Change-Id: I51f8627e1277b16871d6403147e6d7cf6f91fb44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7552222
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1581149}

--- ios/chrome/browser/content_suggestions/magic_stack/ui/magic_stack_module.mm
+++ ios/chrome/browser/content_suggestions/magic_stack/ui/magic_stack_module.mm
@@ -27,4 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return @(int(self.type)).hash;
 }
 
+- (BOOL)hasDifferentContentsFromConfig:(MagicStackModule*)config {
+  return self.type != config.type;
+}
+
 @end
--- ios/chrome/browser/content_suggestions/tab_resumption/ui/tab_resumption_item.mm
+++ ios/chrome/browser/content_suggestions/tab_resumption/ui/tab_resumption_item.mm
@@ -47,6 +47,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   _consumerSource = item.consumerSource;
 }
 
+- (BOOL)hasDifferentContentsFromConfig:(MagicStackModule*)config {
+  if ([super hasDifferentContentsFromConfig:config]) {
+    return YES;
+  }
+  TabResumptionItem* item = static_cast<TabResumptionItem*>(config);
+  return _tabURL != item.tabURL;
+}
+
 #pragma mark - properties
 
 - (const GURL&)tabURL {
