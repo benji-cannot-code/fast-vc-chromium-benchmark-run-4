d36de916dc6bfcb605b55519ad39b7340e3f23e0
Maggie Chen <magchen@chromium.org>
2026-02-06 08:41:27 -0800

    Fix a typo
    Presention -> Presentation.
    
    No functionality change.
    
    Change-Id: I5b23a90a6a1a64e31e4b56f147088a97d1f86865
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7549427
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Maggie Chen <magchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1580882}

--- ui/display/mac/display_link_mac.mm
+++ ui/display/mac/display_link_mac.mm
@@ -70,9 +70,9 @@ void DisplayLinkMac::RecordDisplayLinkCreation(bool success) {
                         success);
 }
 
-std::unique_ptr<PresentionCallbackMac>
-DisplayLinkMac::RegisterPresentionCallback(
-    PresentionCallbackMac::Callback callback) {
+std::unique_ptr<PresentationCallbackMac>
+DisplayLinkMac::RegisterPresentationCallback(
+    PresentationCallbackMac::Callback callback) {
   NOTREACHED();
 }
 
@@ -108,15 +108,15 @@ base::WeakPtr<VSyncCallbackMac> VSyncCallbackMac::GetWeakPtr() {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-// PresentionCallbackMac
+// PresentationCallbackMac
 
-PresentionCallbackMac::PresentionCallbackMac(
+PresentationCallbackMac::PresentationCallbackMac(
     UnregisterCallback unregister_callback,
     Callback callback,
     bool post_callback_to_ctor_thread)
     : unregister_callback_(std::move(unregister_callback)) {
   if (post_callback_to_ctor_thread) {
-    auto lambda = [](base::WeakPtr<PresentionCallbackMac> weak_this,
+    auto lambda = [](base::WeakPtr<PresentationCallbackMac> weak_this,
                      Callback callback, int64_t drawable_id,
                      base::TimeTicks time) {
       if (weak_this) {
@@ -132,11 +132,11 @@ PresentionCallbackMac::PresentionCallbackMac(
   }
 }
 
-PresentionCallbackMac::~PresentionCallbackMac() {
+PresentationCallbackMac::~PresentationCallbackMac() {
   std::move(unregister_callback_).Run(this);
 }
 
-base::WeakPtr<PresentionCallbackMac> PresentionCallbackMac::GetWeakPtr() {
+base::WeakPtr<PresentationCallbackMac> PresentationCallbackMac::GetWeakPtr() {
   return weak_factory_.GetWeakPtr();
 }
 
