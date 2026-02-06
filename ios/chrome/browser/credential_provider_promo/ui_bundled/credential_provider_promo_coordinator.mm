6e014a9f1bec233a5940b8cca288b26fe7f038a1
Hira Mahmood <hiramahmood@google.com>
2026-02-06 11:30:47 -0800

    [iOS] Update CPE promo screen for Tips Notifications
    This CL updates the CPE promo screen that is triggered after the user
    taps on a Tips Notification. The change removes the "No Thanks" button
    and replaces it with a navigation done button. Versions of the promo
    triggered through other sources are unchanged.
    
    Before:
    https://screenshot.googleplex.com/BQvG3tM8xsAMwmN
    After (iOS 26):
    https://screenshot.googleplex.com/C5h9F96WwX2h5ai
    After (iOS 18):
    https://screenshot.googleplex.com/3svH9WhG6WDc2YT
    
    Fixed: 482100571
    Change-Id: I57589de89566a6a234c7c9009292955544d29142
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7548984
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1581011}

--- ios/chrome/browser/credential_provider_promo/ui_bundled/credential_provider_promo_coordinator.mm
+++ ios/chrome/browser/credential_provider_promo/ui_bundled/credential_provider_promo_coordinator.mm
@@ -94,7 +94,27 @@ using credential_provider_promo::IOSCredentialProviderPromoAction;
   self.mediator.tracker =
       feature_engagement::TrackerFactory::GetForProfile(self.profile);
   self.viewController.actionHandler = self;
-  self.viewController.presentationController.delegate = self;
+
+  UIViewController* viewControllerToPresent = self.viewController;
+
+  // Add the "Done" button to the navigation item if the promo was triggered by
+  // a Tips Notification.
+  if (trigger == CredentialProviderPromoTrigger::TipsNotification) {
+    UIBarButtonItem* dismissButton = [[UIBarButtonItem alloc]
+        initWithBarButtonSystemItem:UIBarButtonSystemItemDone
+                             target:self
+                             action:@selector
+                             (confirmationAlertSecondaryAction)];
+    self.viewController.navigationItem.rightBarButtonItem = dismissButton;
+
+    UINavigationController* navigationController =
+        [[UINavigationController alloc]
+            initWithRootViewController:self.viewController];
+    navigationController.modalPresentationStyle = UIModalPresentationFormSheet;
+    navigationController.presentationController.delegate = self;
+    viewControllerToPresent = navigationController;
+  }
+
   self.promoContext = [self promoContextFromTrigger:trigger];
   [self.mediator configureConsumerWithTrigger:trigger
                                       context:self.promoContext];
@@ -102,7 +122,7 @@ using credential_provider_promo::IOSCredentialProviderPromoAction;
   UIViewController* topViewController =
       top_view_controller::TopPresentedViewControllerFrom(
           self.baseViewController);
-  [topViewController presentViewController:self.viewController
+  [topViewController presentViewController:viewControllerToPresent
                                   animated:YES
                                 completion:nil];
   self.promoSeenInCurrentSession = YES;
--- ios/chrome/browser/credential_provider_promo/ui_bundled/credential_provider_promo_view_controller.mm
+++ ios/chrome/browser/credential_provider_promo/ui_bundled/credential_provider_promo_view_controller.mm
@@ -180,7 +180,7 @@ NSString* const kCredentialProviderPromoAccessibilityId =
         constraintEqualToAnchor:self.view.centerYAnchor];
   } else {
     self.alertScreenTopAnchorConstraint = [self.alertScreen.view.topAnchor
-        constraintEqualToAnchor:self.view.topAnchor];
+        constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor];
   }
   self.alertScreenTopAnchorConstraint.active = YES;
 }
@@ -227,7 +227,7 @@ NSString* const kCredentialProviderPromoAccessibilityId =
     [wrapper.animationView.rightAnchor
         constraintEqualToAnchor:self.view.rightAnchor],
     [wrapper.animationView.topAnchor
-        constraintEqualToAnchor:self.view.topAnchor],
+        constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor],
     [wrapper.animationView.bottomAnchor
         constraintEqualToAnchor:self.view.centerYAnchor],
   ]];
