aec16ad051022dd1f91f0ce43349b04b1a77754b
Atharv Maan <atharvmaan@chromium.org>
2026-02-05 22:07:48 -0800

    Add support for custom button styling in Confirm InfoBar
    This CL adds support for having custom button styling in Confirm InfoBar
    by overriding the ConfirmInfoBarDelegate::GetButtonStyle() method.
    
    This is needed for showing single CTA as non-primary in Start Chrome
    with Windows. UI screenshot: go/scww-infobar.
    
    The default, non-overriding implementation will keep using the existing
    styling logic.
    
    Bug: None
    Change-Id: Id9b16b682502b5569a93c1fce3ce967d666c2399
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7549075
    Commit-Queue: Atharv Maan <atharvmaan@chromium.org>
    Reviewed-by: Daniel Soromou <koretadaniel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1580613}

--- components/infobars/core/confirm_infobar_delegate.cc
+++ components/infobars/core/confirm_infobar_delegate.cc
@@ -5,9 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/infobars/core/confirm_infobar_delegate.h"
 
+#include <optional>
+
 #include "build/build_config.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/models/image_model.h"
+#include "ui/base/ui_base_types.h"
 #include "ui/strings/grit/ui_strings.h"
 
 ConfirmInfoBarDelegate::~ConfirmInfoBarDelegate() = default;
@@ -62,6 +65,11 @@ bool ConfirmInfoBarDelegate::GetButtonEnabled(InfoBarButton button) const {
 std::u16string ConfirmInfoBarDelegate::GetButtonTooltip(
     InfoBarButton button) const {
   return std::u16string();
+}
+
+std::optional<ui::ButtonStyle> ConfirmInfoBarDelegate::GetButtonStyle(
+    InfoBarButton button) const {
+  return std::nullopt;
 }
 
 bool ConfirmInfoBarDelegate::ShouldShowLinkBeforeButton() const {
