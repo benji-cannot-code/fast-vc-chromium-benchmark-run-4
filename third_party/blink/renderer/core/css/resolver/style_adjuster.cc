194f7e6e1e77685ae1b92321766b231c95a9cd6c
Kevin Ellis <kevers@google.com>
2026-02-06 12:52:27 -0800

    [scoped-vt] Apply vt-scope: auto during transition
    Bug: 480860018
    Change-Id: I60c01be553f2b68f240836565e4090a2cc0e4d27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7547092
    Commit-Queue: Kevin Ellis <kevers@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1581067}

--- third_party/blink/renderer/core/css/resolver/style_adjuster.cc
+++ third_party/blink/renderer/core/css/resolver/style_adjuster.cc
@@ -1171,6 +1171,7 @@ void StyleAdjuster::AdjustComputedStyle(StyleResolverState& state,
 
     if (is_transition_scope && !is_document_element) {
       builder.SetContain(builder.Contain() | kContainsLayout);
+      builder.SetViewTransitionScope(EViewTransitionScope::kAuto);
     } else if (builder.InternalOverscrollArea() ==
                EInternalOverscrollArea::kAuto) {
       // TODO(crbug.com/467112943): Layout containment is currently forced to
