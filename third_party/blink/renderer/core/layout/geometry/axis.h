d4d89138335f69349dcb86554accbb90a5c6d103
Free Debreuil <freedebreuil@google.com>
2026-02-06 08:30:06 -0800

    Add additional operators to PhysicalAxes and LogicalAxes
    Implements the subtraction operators (`-`, `-=`) to the geometry axes.
    
    Also implements XOR operators (`^`, `^=`) for LogicalAxes, to bring
    functionality inline with PhysicalAxes.
    
    This improves clarity and expressiveness when manipulating geometry
    axes.
    
    Bug: 481019005
    Change-Id: Ie83587ee759f52d3837dff1c971f7dd27a8f728e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7543542
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Free Debreuil <freedebreuil@google.com>
    Cr-Commit-Position: refs/heads/main@{#1580875}

--- third_party/blink/renderer/core/layout/geometry/axis.h
+++ third_party/blink/renderer/core/layout/geometry/axis.h
@@ -35,6 +35,24 @@ inline constexpr LogicalAxes operator&=(LogicalAxes& a, LogicalAxes b) {
   return a;
 }
 
+inline constexpr LogicalAxes operator^(LogicalAxes a, LogicalAxes b) {
+  return LogicalAxes(a.value() ^ b.value());
+}
+
+inline constexpr LogicalAxes operator^=(LogicalAxes& a, LogicalAxes b) {
+  a.value() ^= b.value();
+  return a;
+}
+
+inline constexpr LogicalAxes operator-(LogicalAxes a, LogicalAxes b) {
+  return LogicalAxes(a.value() & ~b.value());
+}
+
+inline constexpr LogicalAxes operator-=(LogicalAxes& a, LogicalAxes b) {
+  a.value() &= ~b.value();
+  return a;
+}
+
 inline constexpr PhysicalAxes operator|(PhysicalAxes a, PhysicalAxes b) {
   return PhysicalAxes(a.value() | b.value());
 }
@@ -59,6 +77,15 @@ inline constexpr PhysicalAxes operator^(PhysicalAxes a, PhysicalAxes b) {
 
 inline constexpr PhysicalAxes operator^=(PhysicalAxes& a, PhysicalAxes b) {
   a.value() ^= b.value();
+  return a;
+}
+
+inline constexpr PhysicalAxes operator-(PhysicalAxes a, PhysicalAxes b) {
+  return PhysicalAxes(a.value() & ~b.value());
+}
+
+inline constexpr PhysicalAxes operator-=(PhysicalAxes& a, PhysicalAxes b) {
+  a.value() &= ~b.value();
   return a;
 }
 
