523b1e1f3cc874d2ddfdb917a73edd77598d88c7
Victor Tan <victortan@chromium.org>
2026-02-06 07:51:46 -0800

    [UACH] Change Android Desktop UACH platform
    Adding support to allow experimentation with changing the platform value
    in the User-Agent Client Hints for Android Desktop and XR devices.
    
    When this feature is enabled, the Sec-CH-UA-Platform header will report
    "Android" instead of the current "Linux".
    
    Change-Id: I528e2956c6f9a40affec1da43bcc5f003466e3fe
    Bug: 479264466
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7544541
    Reviewed-by: Mike Taylor <miketaylr@chromium.org>
    Reviewed-by: Gurmeet Kalra <gurmeetk@google.com>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Victor Tan <victortan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1580849}

--- chrome/browser/android/content/content_utils.cc
+++ chrome/browser/android/content/content_utils.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/embedder_support/user_agent_utils.h"
 #include "components/version_info/version_info.h"
 #include "content/public/browser/web_contents.h"
+#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/common/user_agent/user_agent_metadata.h"
 
 // Must come after all headers that specialize FromJniType() / ToJniType().
@@ -32,7 +33,10 @@ static void JNI_ContentUtils_SetUserAgentOverride(
       embedder_support::BuildUserAgentFromOSAndProduct(
           kLinuxInfoStr, embedder_support::GetProductAndVersion());
   spoofed_ua.ua_metadata_override = metadata;
-  spoofed_ua.ua_metadata_override->platform = "Linux";
+  spoofed_ua.ua_metadata_override->platform =
+      base::FeatureList::IsEnabled(blink::features::kAndroidDesktopUAPlatform)
+          ? "Android"
+          : "Linux";
   spoofed_ua.ua_metadata_override->platform_version =
       std::string();  // match content::GetOSVersion(false) on Linux
   spoofed_ua.ua_metadata_override->model = std::string();
