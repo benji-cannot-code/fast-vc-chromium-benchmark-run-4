a32822a09c0673136502d2a93d315a0383456633
Madhav Pruthi <madhavpruthi@google.com>
2026-02-06 11:58:13 -0800

    [PinnedTabs][TechDebt] Refactor supplementary container animation to use ViewBinder in Tab Switcher
    Moves the search box and supplementary container animation logic from
    `TabSwitcherPaneMediator` to `TabListContainerViewBinder` to adhere to
    the MVC pattern.
    
    Additionally, fixing minor bugs where search box doesn't appear on rapid
    alternate scrolls.
    
    Bug: 455919135
    Fixed: 455919135
    Change-Id: I22ac7ba1387eac0ffb61de904db7164041103ea0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7546324
    Commit-Queue: Madhav Pruthi <madhavpruthi@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1581038}

--- chrome/browser/hub/android/java/src/org/chromium/chrome/browser/hub/DirectionalScrollListener.java
+++ chrome/browser/hub/android/java/src/org/chromium/chrome/browser/hub/DirectionalScrollListener.java
@@ -32,8 +32,10 @@ public class DirectionalScrollListener extends RecyclerView.OnScrollListener {
     /**
      * Creates a new instance of {@link DirectionalScrollListener}.
      *
-     * @param onScrollUp The callback to invoke when scrolling up.
-     * @param onScrollDown The callback to invoke when scrolling down.
+     * @param onScrollUp The callback invoked when the user scrolls toward the top of the content
+     *     (content moves down).
+     * @param onScrollDown The callback invoked when the user scrolls toward the bottom of the
+     *     content (content moves up).
      */
     public DirectionalScrollListener(Runnable onScrollUp, Runnable onScrollDown) {
         this(
@@ -47,8 +49,10 @@ public class DirectionalScrollListener extends RecyclerView.OnScrollListener {
     /**
      * Creates a new instance of {@link DirectionalScrollListener}.
      *
-     * @param onScrollUp The callback to invoke when scrolling up.
-     * @param onScrollDown The callback to invoke when scrolling down.
+     * @param onScrollUp The callback invoked when the user scrolls toward the top of the content
+     *     (content moves down).
+     * @param onScrollDown The callback invoked when the user scrolls toward the bottom of the
+     *     content (content moves up).
      * @param throttleMs The minimum time in milliseconds between invocations of the callbacks.
      */
     public DirectionalScrollListener(Runnable onScrollUp, Runnable onScrollDown, long throttleMs) {
@@ -63,8 +67,10 @@ public class DirectionalScrollListener extends RecyclerView.OnScrollListener {
     /**
      * Creates a new instance of {@link DirectionalScrollListener}.
      *
-     * @param onScrollUp The callback to invoke when scrolling up.
-     * @param onScrollDown The callback to invoke when scrolling down.
+     * @param onScrollUp The callback invoked when the user scrolls toward the top of the content
+     *     (content moves down).
+     * @param onScrollDown The callback invoked when the user scrolls toward the bottom of the
+     *     content (content moves up).
      * @param throttleMs The minimum time in milliseconds between invocations of the callbacks.
      * @param scrollThreshold The minimum scroll distance in pixels to trigger the callbacks.
      */
@@ -76,8 +82,10 @@ public class DirectionalScrollListener extends RecyclerView.OnScrollListener {
     /**
      * Creates a new instance of {@link DirectionalScrollListener}.
      *
-     * @param onScrollUp The callback to invoke when scrolling up.
-     * @param onScrollDown The callback to invoke when scrolling down.
+     * @param onScrollUp The callback invoked when the user scrolls toward the top of the content
+     *     (content moves down).
+     * @param onScrollDown The callback invoked when the user scrolls toward the bottom of the
+     *     content (content moves up).
      * @param throttleMs The minimum time in milliseconds between invocations of the callbacks.
      * @param scrollUpThreshold The minimum scroll up distance in pixels to trigger the callbacks.
      * @param scrollDownThreshold The minimum scroll down distance in pixels to trigger the
