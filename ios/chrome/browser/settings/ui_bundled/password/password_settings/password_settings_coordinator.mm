f62b0b28f3b3057ae7e3e2f6c252841d53c24bbe
Rafał Godlewski <rgod@google.com>
2026-02-06 09:45:59 -0800

    [iOS][CX] Handle export fetching key errors correctly
    Handles error passed from keychain provider's key fetching to
    differentiate between the case when:
    * user cancelled the GPM PIN reauth view - in this case the reauth screens should be dismissed, but the export flow should not be cancelled
    * actual error happened - in this case an alert should be displayed ending the export flow
    
    Also, reuses the newly introduced PasskeyWelcomeScreenCoordinator,
    instead of having a helper function pushing screens to a navigation
    controller, as the former is easier to reason about and a more common
    pattern.
    
    Bug: 445889307
    Change-Id: I0ead1496d3a9e96ee6cac3b5f684066b2b171d5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7545774
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1580918}

--- ios/chrome/browser/settings/ui_bundled/password/password_settings/password_settings_coordinator.mm
+++ ios/chrome/browser/settings/ui_bundled/password/password_settings/password_settings_coordinator.mm
@@ -126,14 +126,14 @@ constexpr const char* kDeleteAllSavedDataButtonClicked =
 @end
 
 @interface PasswordSettingsCoordinator () <
-    ExportActivityViewControllerDelegate,
     BulkMoveLocalPasswordsToAccountHandler,
+    CredentialExportCoordinatorDelegate,
+    ExportActivityViewControllerDelegate,
+    LocalReauthenticationCoordinatorDelegate,
     PasswordExportHandler,
     PasswordsInOtherAppsCoordinatorDelegate,
     PopoverLabelViewControllerDelegate,
-    LocalReauthenticationCoordinatorDelegate,
     SettingsNavigationControllerDelegate>
-
 @end
 
 @implementation PasswordSettingsCoordinator {
@@ -272,8 +272,7 @@ constexpr const char* kDeleteAllSavedDataButtonClicked =
   _passwordsInOtherAppsCoordinator = nil;
 
   if (@available(iOS 26, *)) {
-    [_credentialExportCoordinator stop];
-    _credentialExportCoordinator = nil;
+    [self stopCredentialExportCoordinator];
   }
 
   _passwordSettingsViewController.presentationDelegate = nil;
@@ -377,6 +376,7 @@ constexpr const char* kDeleteAllSavedDataButtonClicked =
                                    browser:self.browser
                           affiliatedGroups:_savedPasswordsPresenter
                                                ->GetAffiliatedGroups()];
+      _credentialExportCoordinator.delegate = self;
       [_credentialExportCoordinator start];
       return;
     }
@@ -647,6 +647,14 @@ constexpr const char* kDeleteAllSavedDataButtonClicked =
   [self showSetPasscodeDialogWithContent:
             l10n_util::GetNSString(
                 IDS_IOS_SETTINGS_EXPORT_PASSWORDS_SET_UP_SCREENLOCK_CONTENT)];
+}
+
+#pragma mark - CredentialExportCoordinatorDelegate
+
+- (void)credentialExportCoordinatorDidFinish:
+    (CredentialExportCoordinator*)coordinator API_AVAILABLE(ios(26.0)) {
+  CHECK_EQ(coordinator, _credentialExportCoordinator);
+  [self stopCredentialExportCoordinator];
 }
 
 #pragma mark - ExportActivityViewControllerDelegate
@@ -934,6 +942,12 @@ constexpr const char* kDeleteAllSavedDataButtonClicked =
           l10n_util::GetNSString(IDS_IOS_SETTINGS_DELETE_ALL_CREDENTIALS)
                   canReusePreviousAuth:NO
                                handler:onReauthFinished];
+}
+
+- (void)stopCredentialExportCoordinator {
+  [_credentialExportCoordinator stop];
+  _credentialExportCoordinator.delegate = nil;
+  _credentialExportCoordinator = nil;
 }
 
 @end
