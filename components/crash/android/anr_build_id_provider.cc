6fac339d39b80739105a0d5d8f127dba3f896252
Andrew Grieve <agrieve@chromium.org>
2026-02-06 14:02:34 -0800

    Android: Use only the first 40 digits of buildId for ANR process state
    There are various flags we can use for build ID, and some of them cause
    AnrCollector to hit the "The process state summary we want to save is
    over 128 bytes" exception.
    
    For our current official release builds, this is a no-op (build IDs are
    sha1 - 40 digits).
    
    Bug: 40208899
    Change-Id: I6982bb9e731db52f8c11571dbd0a444e12da98cb
    Cq-Include-Trybots: luci.chrome.try:android-internal-dbg,android-internal-binary-size
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7551570
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1581120}

--- components/crash/android/anr_build_id_provider.cc
+++ components/crash/android/anr_build_id_provider.cc
@@ -8,14 +8,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/debug/elf_reader.h"
+#include "base/logging.h"
 
 extern char __executable_start;
 
 namespace crash_reporter {
 std::string GetElfBuildId() {
   base::debug::ElfBuildIdBuffer build_id;
-  base::debug::ReadElfBuildId(&__executable_start, false, build_id);
-  return std::string(build_id);
+  size_t size =
+      base::debug::ReadElfBuildId(&__executable_start, false, build_id);
+  CHECK(size) << "Failed to read BuildId";
+
+  // Official builds use SHA1 (40 chars), but debug builds use whatever is
+  // default. For non-lld linkers, this can be sha256, which triggers an
+  // exception in AnrCollector that enforces it stays under 128 bytes.
+  return std::string(build_id).substr(0, 40);
 }
 
 }  // namespace crash_reporter
