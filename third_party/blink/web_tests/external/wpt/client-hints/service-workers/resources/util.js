d757e9f75f86448a365cd36b4fa82a61014c2e77
Rakina Zata Amni <rakina@chromium.org>
2026-02-06 01:40:39 -0800

    Don't navigate to blank.html on new windows in client-hints tests
    These tests want to check that their target URL is loaded with the
    load event, but added an extra step to navigate to blank.html before
    adding the load event listener, probably to avoid the target URL
    being navigated too quickly and missing the load event. However
    this shouldn't happen since navigation is async.
    
    Also, this isn't doing what it intended to do, since the
    blank.html page navigation ends up being cancelled by the target
    URL navigation soon after. This would cause flakiness with
    RenderDocument + SkipRendererCancellationThrottle because the
    blank.html navigation actually does not get cancelled in that case.
    
    This test makes it so that the new window navigations go to the
    target URL directly, which should be equivalent to the current
    behavior, where the blank navigation gets cancelled immediately
    after the target URL navigation starts.
    
    Bug: 40615943
    Change-Id: I67ad86c7787d850c2bd18e807fc5da188c562362
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7548187
    Auto-Submit: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Mingyu Lei <leimy@chromium.org>
    Commit-Queue: Mingyu Lei <leimy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1580675}

--- third_party/blink/web_tests/external/wpt/client-hints/service-workers/resources/util.js
+++ third_party/blink/web_tests/external/wpt/client-hints/service-workers/resources/util.js
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 async function ch_sw_test(t, worker, url, response) {
   r = await service_worker_unregister_and_register(t, worker, url);
   await wait_for_state(t, r.installing, 'activated')
-  var popup_window = window.open("/common/blank.html");
+  var popup_window = window.open(url);
   assert_not_equals(popup_window, null, "Popup windows not allowed?");
 
   t.add_cleanup(async _=>{
@@ -12,10 +12,8 @@ async function ch_sw_test(t, worker, url, response) {
 
   popup_load = new Promise((resolve, reject) => {
     popup_window.addEventListener('load', t.step_func((e) => {
-      if(popup_window.location.pathname != "/blank.html") {
-        assert_equals(popup_window.document.body.textContent, response);
-        resolve();
-      }
+      assert_equals(popup_window.document.body.textContent, response);
+      resolve();
     }))
   });
 
