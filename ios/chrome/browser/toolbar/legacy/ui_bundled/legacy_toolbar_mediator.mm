4a8f25cfc4adac7252a522f660e99ca62fb16b77
Stepan Khapugin <stkhapugin@chromium.org>
2026-02-06 11:30:58 -0800

    [iOS][Omnibox] Fix adaptive suggestions behind composebox flag
    This is effectively a reland of crrev.com/c/7545940 but behind the
    composebox flag to make it safe to merge.
    
    Bug: 481685934
    Change-Id: Ia79a6f30f0c1b6f44cb5b6983be40df7f86d4a8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7550440
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@google.com>
    Commit-Queue: Justin Cohen <justincohen@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1581012}

--- ios/chrome/browser/toolbar/legacy/ui_bundled/legacy_toolbar_mediator.mm
+++ ios/chrome/browser/toolbar/legacy/ui_bundled/legacy_toolbar_mediator.mm
@@ -251,8 +251,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return;
   }
 
-  [self.omniboxConsumer setKeyboardAttachedBottomOmniboxHeight:
-                            self.delegate.keyboardAttachedBottomOmniboxHeight];
+  if (!base::FeatureList::IsEnabled(kComposeboxIOS)) {
+    [self.omniboxConsumer
+        setKeyboardAttachedBottomOmniboxHeight:
+            self.delegate.keyboardAttachedBottomOmniboxHeight];
+  }
   [self.omniboxConsumer setPreferredOmniboxPosition:_preferredOmniboxPosition];
 
   self.omniboxPosition = [self omniboxPositionInCurrentState];
