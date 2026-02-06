d02ea790fada966525e748975d76966dbe2a2660
Joe Mason <joenotcharles@google.com>
2026-02-06 11:54:03 -0800

    Implement sync methods in TextInputClientMac with a nested RunLoop
    Bug: 475270375
    Change-Id: I8f748c77e267f79e6e465bd4f29c2c5ae0288a65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7548367
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1581033}

--- content/common/features.h
+++ content/common/features.h
@@ -218,6 +218,7 @@ CONTENT_EXPORT BASE_DECLARE_FEATURE(kStrictHighRankProcessLRU);
 CONTENT_EXPORT BASE_DECLARE_FEATURE(kTextInputClient);
 CONTENT_EXPORT BASE_DECLARE_FEATURE_PARAM(base::TimeDelta,
                                           kTextInputClientIPCTimeout);
+CONTENT_EXPORT BASE_DECLARE_FEATURE_PARAM(bool, kTextInputClientUseNestedLoop);
 #endif
 CONTENT_EXPORT BASE_DECLARE_FEATURE(kTouchpadOverscrollHistoryNavigation);
 CONTENT_EXPORT BASE_DECLARE_FEATURE(kTrustedTypesFromLiteral);
