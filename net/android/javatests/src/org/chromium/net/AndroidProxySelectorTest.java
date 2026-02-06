060bbafd86fbcc885f7625f619d622553c4a29bd
Andrew Williams <awillia@chromium.org>
2026-02-05 19:55:36 -0800

    Fix comment use of "_unittests.cc" (net/android)
    Following up on:
    https://chromium-review.googlesource.com/c/chromium/src/+/7510296
    
    Update comments in various files incorrectly referred to unit test files
    with a plural "unittests.cc" suffix.
    
     #cleanup
    
    This CL was uploaded by git cl split.
    
    R=rch@chromium.org
    
    Bug: None
    Change-Id: I3270c2e46dd63f74a74e9aa0097e2e032cc14d07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7523785
    Commit-Queue: Ryan Hamilton <rch@chromium.org>
    Auto-Submit: Andrew Williams <awillia@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1580588}

--- net/android/javatests/src/org/chromium/net/AndroidProxySelectorTest.java
+++ net/android/javatests/src/org/chromium/net/AndroidProxySelectorTest.java
@@ -4,13 +4,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 /**
- * Test suite for Android's default ProxySelector implementation. The purpose of these tests
- * is to check that the behaviour of the ProxySelector implementation matches what we have
- * implemented in net/proxy_resolution/proxy_config_service_android.cc.
+ * Test suite for Android's default ProxySelector implementation. The purpose of these tests is to
+ * check that the behaviour of the ProxySelector implementation matches what we have implemented in
+ * net/proxy_resolution/proxy_config_service_android.cc.
  *
- * IMPORTANT: These test cases are generated from net/android/tools/proxy_test_cases.py, so if any
- * of these tests fail, please be sure to edit that file and regenerate the test cases here and also
- * in net/proxy_resolution/proxy_config_service_android_unittests.cc if required.
+ * <p>IMPORTANT: These test cases are generated from net/android/tools/proxy_test_cases.py, so if
+ * any of these tests fail, please be sure to edit that file and regenerate the test cases here and
+ * also in net/proxy_resolution/proxy_config_service_android_unittest.cc if required.
  */
 package org.chromium.net;
 
