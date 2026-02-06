c71152fed649d8d76d138ee7c9d45bde46944ec7
hiroki oshima <hiroki.oshima@gmail.com>
2026-02-06 02:55:39 -0800

    fix MultiUserWindowCycleControllerTest and ModeSelectionWindowCycleControllerTest dangling pointers
    Bug: 40212619
    Change-Id: I43365464d5f3e54e9b2e02bb1ca3bd7216f36fa8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7467244
    Reviewed-by: Di Wu <diwux@google.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Di Wu <diwux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1580714}

--- ash/wm/window_cycle/window_cycle_controller_unittest.cc
+++ ash/wm/window_cycle/window_cycle_controller_unittest.cc
@@ -2022,6 +2022,12 @@ class ModeSelectionWindowCycleControllerTest
     generator_ = GetEventGenerator();
   }
 
+  // WindowCycleControllerTest:
+  void TearDown() override {
+    generator_ = nullptr;
+    WindowCycleControllerTest::TearDown();
+  }
+
   void SwitchPerDeskAltTabMode(bool per_desk_mode,
                                bool use_slow_duration = false) {
     gfx::ScopedAnimationDurationScaleMode animation_scale(
@@ -2043,7 +2049,7 @@ class ModeSelectionWindowCycleControllerTest
   }
 
  private:
-  raw_ptr<ui::test::EventGenerator, DanglingUntriaged> generator_;
+  raw_ptr<ui::test::EventGenerator> generator_;
 };
 
 // Tests that when user taps tab slider buttons, the active mode should
@@ -3203,6 +3209,8 @@ class MultiUserWindowCycleControllerTest : public NoSessionAshTestBase {
 
   void TearDown() override {
     multi_user_window_manager_.reset();
+    generator_ = nullptr;
+    shelf_view_test_.reset();
     NoSessionAshTestBase::TearDown();
   }
 
@@ -3279,7 +3287,7 @@ class MultiUserWindowCycleControllerTest : public NoSessionAshTestBase {
   }
 
  private:
-  raw_ptr<ui::test::EventGenerator, DanglingUntriaged> generator_;
+  raw_ptr<ui::test::EventGenerator> generator_;
 
   std::unique_ptr<ShelfViewTestAPI> shelf_view_test_;
 
