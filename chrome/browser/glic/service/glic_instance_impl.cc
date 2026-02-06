06d5650c0b67beeccd9fd9faedbbf37681c9e5de
Sana Akbani <sanaakbani@google.com>
2026-02-06 13:36:08 -0800

    Add GlicLiveModeOnlyGlow to Multi-Instance field trial config.
    This requires some updates to tests:
    - With Multi-Instance enabled, the glow only shows in Live mode. The ContextSharingBorderViewUiTest has been updated to reflect that.
    - The TabAlert changes from kGlicAccessing to kGlicSharing when Multi-Instance is enabled, which needed to be updated in GlicTabIndicatorHelperUiTest.
    
    Bug: b:470988948
    Change-Id: Iccc04593e7bb5681b16485c383b4319489ab35ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7523208
    Commit-Queue: Sana Akbani <sanaakbani@google.com>
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1581097}

--- chrome/browser/glic/service/glic_instance_impl.cc
+++ chrome/browser/glic/service/glic_instance_impl.cc
@@ -603,6 +603,7 @@ void GlicInstanceImpl::OnInteractionModeChange(mojom::WebClientMode new_mode) {
   interaction_mode_ = new_mode;
   sharing_manager_coordinator_.UpdateState(GetPanelState().kind,
                                            interaction_mode_);
+  ContextAccessIndicatorChanged(host().IsContextAccessIndicatorEnabled());
 }
 
 void GlicInstanceImpl::AddStateObserver(PanelStateObserver* observer) {
