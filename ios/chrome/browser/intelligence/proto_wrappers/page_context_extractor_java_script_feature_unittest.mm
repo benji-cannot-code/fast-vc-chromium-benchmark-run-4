7b7612062d2ec7855af8a051e95ec5e1bfe630ec
Vincent Boisselle <vincb@google.com>
2026-02-06 02:24:12 -0800

    [ios] Re-enable PageContextExtractorJavaScriptFeatureTest
    Now that we are using TestProfileIOS I think the flakes will be gone.
    
    Bug: 455761581
    Change-Id: I0de18ff56a9c5f26fa3f38db7e60c9ccc046feae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7547950
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1580696}

--- ios/chrome/browser/intelligence/proto_wrappers/page_context_extractor_java_script_feature_unittest.mm
+++ ios/chrome/browser/intelligence/proto_wrappers/page_context_extractor_java_script_feature_unittest.mm
@@ -129,7 +129,7 @@ TEST_F(PageContextExtractorJavaScriptFeatureTest,
 }
 
 // TODO(crbug.com/455761581): Test is flaky.
-TEST_F(PageContextExtractorJavaScriptFeatureTest, DISABLED_ExtractPageContext) {
+TEST_F(PageContextExtractorJavaScriptFeatureTest, ExtractPageContext) {
   const std::string main_html =
       base::StrCat({"<html><head><title>Main</title></head><body><p>Main frame "
                     "text</p><iframe "
@@ -179,7 +179,7 @@ TEST_F(PageContextExtractorJavaScriptFeatureTest, DISABLED_ExtractPageContext) {
 
 // TODO(crbug.com/455761581): Test is flaky.
 TEST_F(PageContextExtractorJavaScriptFeatureTest,
-       DISABLED_ExtractPageContextWithAnchors) {
+       ExtractPageContextWithAnchors) {
   const std::string main_html =
       "<html><head><title>Main</title></head><body><a "
       "href=\"http://foo.com\">foo</a></body></html>";
@@ -217,7 +217,7 @@ TEST_F(PageContextExtractorJavaScriptFeatureTest,
 
 // TODO(crbug.com/455761581): Test is flaky.
 TEST_F(PageContextExtractorJavaScriptFeatureTest,
-       DISABLED_ExtractPageContextWithForceDetach) {
+       ExtractPageContextWithForceDetach) {
   const std::string main_html = "<html><body><p>Hello</p></body></html>";
   web::test::LoadHtml(base::SysUTF8ToNSString(main_html),
                       test_server_.GetURL(kMainPagePath), web_state());
