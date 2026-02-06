156358c601e9d54bc3bcb4c276dd8095eb7e60c6
Jason Hu <hujasonx@google.com>
2026-02-06 15:57:01 -0800

    [context-input] Send filename to composebox query controller
    This cl introduces a mechanism to pass the file name from file context uploads in the various searchbox handlers, through the contextual search session handle, to the composebox query controller via lens::ContextualInput
    
    Bug: b:482156001
    Change-Id: I8abf188f1cb078b80d887e204ab12e5ffd12c297
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7549574
    Reviewed-by: Nihar Majmudar <niharm@google.com>
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1581168}

--- ios/chrome/browser/composebox/coordinator/composebox_input_plate_mediator.mm
+++ ios/chrome/browser/composebox/coordinator/composebox_input_plate_mediator.mm
@@ -1322,8 +1322,11 @@ CreateInputDataFromAnnotatedPageContent(
   // Register the file context with the UI as soon as the token is created so
   // that it can listen to all file upload events.
   [self onFileContextAdded:serverToken forIdentifier:identifier];
+  ComposeboxInputItem* item = [_items itemForIdentifier:identifier];
+  std::string fileName = item ? base::SysNSStringToUTF8(item.title) : "";
   _contextualSearchSession->StartFileContextUploadFlow(
-      serverToken, kPortableNetworkGraphicMimeType, std::move(buffer), options);
+      serverToken, fileName, kPortableNetworkGraphicMimeType, std::move(buffer),
+      options);
 }
 
 // Uploads the `image` for the item with the given `identifier`.
@@ -1400,8 +1403,10 @@ CreateInputDataFromAnnotatedPageContent(
     // Register the file context with the UI as soon as the token is created so
     // that it can listen to all file upload events.
     [self onFileContextAdded:serverToken forIdentifier:identifier];
+    std::string fileName = base::SysNSStringToUTF8(item.title);
     _contextualSearchSession->StartFileContextUploadFlow(
-        serverToken, kAdobePortableDocumentFormatMimeType, std::move(buffer),
+        serverToken, fileName, kAdobePortableDocumentFormatMimeType,
+        std::move(buffer),
         /*image_options=*/std::nullopt);
   }
 
