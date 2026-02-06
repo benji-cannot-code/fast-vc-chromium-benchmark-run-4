915f6e8a579e651da34852f9af03edacb1fc8461
Timofey Chudakov <tchudakov@google.com>
2026-02-06 06:24:43 -0800

    [AutofillAi - M4] Implement deletion for local entities.
    The EntityEditor hasn't been connected to the AutofillProfilesFragment
    yet. The AutofillProfilesFragment will pass the Delegate implementation
    to the EntityEditorCoordinator, which will delete the entity via the
    EntityDataManager.
    
    Bug: 476755159
    Change-Id: I684a68d63184bb2e6d2a9f2ff3826885bc00f557
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7544655
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1580796}

--- chrome/browser/autofill/android/java/src/org/chromium/chrome/browser/autofill/editors/autofill_ai/EntityEditorMediator.java
+++ chrome/browser/autofill/android/java/src/org/chromium/chrome/browser/autofill/editors/autofill_ai/EntityEditorMediator.java
@@ -6,22 +6,37 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.autofill.editors.autofill_ai;
 
 import static org.chromium.build.NullUtil.assumeNonNull;
+import static org.chromium.chrome.browser.autofill.editors.autofill_ai.EntityEditorProperties.ALLOW_DELETE;
 import static org.chromium.chrome.browser.autofill.editors.autofill_ai.EntityEditorProperties.CANCEL_RUNNABLE;
+import static org.chromium.chrome.browser.autofill.editors.autofill_ai.EntityEditorProperties.DELETE_CONFIRMATION_PRIMARY_BUTTON_TEXT_ID;
+import static org.chromium.chrome.browser.autofill.editors.autofill_ai.EntityEditorProperties.DELETE_CONFIRMATION_TEXT;
+import static org.chromium.chrome.browser.autofill.editors.autofill_ai.EntityEditorProperties.DELETE_CONFIRMATION_TITLE;
+import static org.chromium.chrome.browser.autofill.editors.autofill_ai.EntityEditorProperties.DELETE_RUNNABLE;
 import static org.chromium.chrome.browser.autofill.editors.autofill_ai.EntityEditorProperties.DONE_RUNNABLE;
 import static org.chromium.chrome.browser.autofill.editors.autofill_ai.EntityEditorProperties.EDITOR_TITLE;
+
+import android.content.Context;
 
 import org.chromium.build.annotations.EnsuresNonNull;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
+import org.chromium.chrome.browser.autofill.R;
+import org.chromium.chrome.browser.autofill.editors.autofill_ai.EntityEditorCoordinator.Delegate;
 import org.chromium.components.autofill.autofill_ai.EntityInstance;
+import org.chromium.components.autofill.autofill_ai.RecordType;
 import org.chromium.ui.modelutil.PropertyModel;
 
 /** Mediator for the Entity Editor. */
 @NullMarked
 class EntityEditorMediator {
+    private final Context mContext;
+    private final Delegate mDelegate;
     private @Nullable PropertyModel mEditorModel;
 
-    EntityEditorMediator() {}
+    EntityEditorMediator(Context context, Delegate delegate) {
+        mContext = context;
+        mDelegate = delegate;
+    }
 
     @EnsuresNonNull("mEditorModel")
     PropertyModel getEditorModel(EntityInstance entityInstance) {
@@ -33,6 +48,19 @@ class EntityEditorMediator {
                                     entityInstance.getEntityType().getAddEntityTypeString())
                             .with(DONE_RUNNABLE, this::onDone)
                             .with(CANCEL_RUNNABLE, this::onCancel)
+                            .with(
+                                    DELETE_CONFIRMATION_TITLE,
+                                    entityInstance.getEntityType().getDeleteEntityTypeString())
+                            .with(
+                                    DELETE_CONFIRMATION_TEXT,
+                                    mContext.getString(
+                                            R.string
+                                                    .autofill_ai_entity_editor_delete_local_entity_dialog_text))
+                            .with(
+                                    DELETE_CONFIRMATION_PRIMARY_BUTTON_TEXT_ID,
+                                    R.string.autofill_delete_suggestion_button)
+                            .with(DELETE_RUNNABLE, () -> mDelegate.onDelete(entityInstance))
+                            .with(ALLOW_DELETE, entityInstance.getRecordType() == RecordType.LOCAL)
                             .build();
         }
         return mEditorModel;
