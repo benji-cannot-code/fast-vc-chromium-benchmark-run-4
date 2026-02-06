02f4c64c025a96dfbfd3a885c705fdadbde2b73e
Zoraiz Naeem <zoraiznaeem@chromium.org>
2026-02-05 16:11:46 -0800

    Allow-list custom icons for the ChromeOS status area.
    This change introduces an allow-list to control which icons from the
    Chrome browser can be added to the Ash status area (the system tray).
    
    Currently, StatusTray in ChromeOS has limited real estate and the OWNERS
    of StatusTray in ChromeOS want a mechanism to control what can be added.
    
    Bug: b:463430271
    Change-Id: I7aac42dd1a924e7bb7e732c1ff8bb67bce0cc2b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7543387
    Auto-Submit: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1580484}

---
+++ ash/constants/chrome_custom_icon_catalog.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_CONSTANTS_CHROME_CUSTOM_ICON_CATALOG_H_
+#define ASH_CONSTANTS_CHROME_CUSTOM_ICON_CATALOG_H_
+
+namespace ash {
+
+// Used as allow-list of icons that Chrome can add to the StatusArea (a.k.a
+// StatusTray) of ChromeOS via StatusTray interface. (See `StatusTrayChromeOS`
+// for details)
+enum class ChromeCustomIconCatalogName {
+  kNotSupported = 0,
+  kGlic = 1,
+  kMaxValue = kGlic
+};
+
+}  // namespace ash
+
+#endif  // ASH_CONSTANTS_CHROME_CUSTOM_ICON_CATALOG_H_
