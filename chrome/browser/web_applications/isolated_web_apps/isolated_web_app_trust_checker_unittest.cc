65f4cc2635203b89023b4e45dc7e94b3c1c7510e
Sergii Bykov <sbykov@google.com>
2026-02-06 03:56:32 -0800

    Remove extra empty lines in IWA unit tests
    Also removes unused includes.
    
    Bug: None
    Change-Id: I55784406ad7752cecd9dbbc3492c8d0e762bb657
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7548056
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Commit-Queue: Sergii Bykov <sbykov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1580737}

--- chrome/browser/web_applications/isolated_web_apps/isolated_web_app_trust_checker_unittest.cc
+++ chrome/browser/web_applications/isolated_web_apps/isolated_web_app_trust_checker_unittest.cc
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check_deref.h"
 #include "base/containers/span.h"
-#include "base/feature_list.h"
-#include "base/strings/strcat.h"
 #include "base/test/gmock_expected_support.h"
 #include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
@@ -24,13 +22,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/pref_names.h"
 #include "components/prefs/pref_service.h"
-#include "components/web_package/mojom/web_bundle_parser.mojom.h"
 #include "components/web_package/signed_web_bundles/ed25519_public_key.h"
 #include "components/web_package/signed_web_bundles/signed_web_bundle_id.h"
-#include "components/web_package/signed_web_bundles/signed_web_bundle_integrity_block.h"
 #include "components/webapps/browser/installable/installable_metrics.h"
 #include "components/webapps/isolated_web_apps/scheme.h"
-#include "components/webapps/isolated_web_apps/types/source.h"
 #include "components/webapps/isolated_web_apps/types/storage_location.h"
 #include "content/public/common/content_features.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -194,45 +189,28 @@ TEST_F(IsolatedWebAppTrustCheckerTest,
 
 TEST_F(IsolatedWebAppTrustCheckerTest, TrustedViaDevMode) {
   EXPECT_THAT(IsolatedWebAppTrustChecker::IsOperationAllowed(
-
                   *profile(), kWebBundleId1, /*dev_mode=*/true,
-
                   IwaInstallOperation{
-
                       .source = webapps::WebappInstallSource::IWA_DEV_UI}),
-
               base::test::ErrorIs(_));
 
   base::test::ScopedFeatureList feature_list;
-
   feature_list.InitAndEnableFeature(features::kIsolatedWebAppDevMode);
-
   EXPECT_THAT(IsolatedWebAppTrustChecker::IsOperationAllowed(
-
                   *profile(), kWebBundleId1, /*dev_mode=*/true,
-
                   IwaInstallOperation{
-
                       .source = webapps::WebappInstallSource::IWA_DEV_UI}),
-
               base::test::HasValue());
 
   pref_service().SetInteger(
-
       prefs::kDevToolsAvailability,
-
       std::to_underlying(
-
           policy::DeveloperToolsPolicyHandler::Availability::kDisallowed));
 
   EXPECT_THAT(IsolatedWebAppTrustChecker::IsOperationAllowed(
-
                   *profile(), kWebBundleId1, /*dev_mode=*/true,
-
                   IwaInstallOperation{
-
                       .source = webapps::WebappInstallSource::IWA_DEV_UI}),
-
               base::test::ErrorIs(_));
 }
 
