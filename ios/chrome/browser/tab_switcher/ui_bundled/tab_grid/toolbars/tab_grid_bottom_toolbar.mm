8bea92f98d2a2c0ddf4ccf8d7c19303355bc71e0
Briana McClure <bmcclure@google.com>
2026-02-06 07:11:13 -0800

    [iOS] Fix hideAllButtons crash in TabGridBottomToolbar
    The TabGridBottomToolbar is crashing in `hideAllButtons` after a trait
    collection change. This is likely because `hideAllButtons` is being
    called too early in the view's life cycle.
    
    This change moves `registerForTraitChanges` outside of the initializer
    to ensure buttons are fully loaded before they are being accessed.
    `hideAllButtons` also returns early if _doneButton is nil to prevent
    updating the button when it hasn't been loaded.
    
    Fixed: 475827050
    Change-Id: I76571b8bbbf33137b3cfcad1ec123e96e6e8e94c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7546277
    Commit-Queue: Briana McClure <bmcclure@google.com>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1580822}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/toolbars/tab_grid_bottom_toolbar.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/toolbars/tab_grid_bottom_toolbar.mm
@@ -80,9 +80,6 @@ CGFloat CompactButtonHorizontalPadding() {
   if (self) {
     [self setupViews];
     [self updateLayout];
-    NSArray<UITrait>* traits = TraitCollectionSetForTraits(
-        @[ UITraitVerticalSizeClass.class, UITraitHorizontalSizeClass.class ]);
-    [self registerForTraitChanges:traits withAction:@selector(updateLayout)];
   }
   return self;
 }
@@ -103,6 +100,10 @@ CGFloat CompactButtonHorizontalPadding() {
           .active = YES;
     }
   }
+
+  NSArray<UITrait>* traits = TraitCollectionSetForTraits(
+      @[ UITraitVerticalSizeClass.class, UITraitHorizontalSizeClass.class ]);
+  [self registerForTraitChanges:traits withAction:@selector(updateLayout)];
   [super didMoveToSuperview];
 }
 
