c7ae2896ecd6d43f67e71c6b3743c222ed0f733a
Duncan Mercer <mercerd@google.com>
2026-02-06 17:47:11 -0800

    [Contextual Tasks] Fix plus button indentation
    The plus button was too indented so that it didn't match the hint text.
    
    Before: http://shortn/_YTw5H5jSfx
    After: http://shortn/_JF792WLwsD
    
    Bug: b:478001705
    Change-Id: I1d4fc8749b47aaf7316f19d9a572e3f5a87f819b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7552971
    Reviewed-by: James Leung <jamesleung@google.com>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1581245}

--- chrome/test/data/webui/contextual_tasks/contextual_tasks_pixel_interactive_ui_test.cc
+++ chrome/test/data/webui/contextual_tasks/contextual_tasks_pixel_interactive_ui_test.cc
@@ -206,7 +206,7 @@ IN_PROC_BROWSER_TEST_P(ContextualTasksComposeBoxPixelTest, Screenshots) {
       // Take a screenshot of the composebox.
       ScreenshotWebUi(kActiveTab, kComposebox,
                       /*screenshot_name=*/"ContextualTasksComposebox",
-                      /*baseline_cl=*/"7531588"));
+                      /*baseline_cl=*/"7552971"));
 }
 
 struct AppPixelTestParams {
@@ -317,7 +317,7 @@ IN_PROC_BROWSER_TEST_P(ContextualTasksAppPixelTest, Screenshots) {
       SetOnIncompatibleAction(OnIncompatibleAction::kIgnoreAndContinue,
                               "Screenshots not captured on this platform."),
       ScreenshotWebUi(kActiveTab, kApp, "ContextualTasksApp",
-                      /*baseline_cl=*/"7530305"));
+                      /*baseline_cl=*/"7552971"));
 }
 
 enum class TitleType { kNone, kShort, kLong };
