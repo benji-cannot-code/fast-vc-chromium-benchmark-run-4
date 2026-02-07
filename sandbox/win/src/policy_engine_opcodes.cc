d5d75fed0c4b339e59ae406e9dc8e3776870d98f
James Forshaw <forshaw@chromium.org>
2026-02-07 10:11:38 -0800

    [Windows] Remove Unsafe Buffer Usage from Sandbox Policy.
    This CL removes some easy to change uses of unsafe buffers in the
    sandbox policy code.
    
    Bug: 447171244
    Change-Id: I36ef351ebdf992aa075cb3645daed3939ba4bd4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7553070
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: James Forshaw <forshaw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1581357}

--- sandbox/win/src/policy_engine_opcodes.cc
+++ sandbox/win/src/policy_engine_opcodes.cc
@@ -50,7 +50,7 @@ size_t OpcodeFactory::memory_size() const {
 }
 
 template <int>
-EvalResult OpcodeEval(PolicyOpcode* opcode,
+EvalResult OpcodeEval(const PolicyOpcode* opcode,
                       const ParameterSet* pp,
                       MatchContext* match);
 
@@ -63,7 +63,7 @@ PolicyOpcode* OpcodeFactory::MakeOpAlwaysFalse(uint32_t options) {
 }
 
 template <>
-EvalResult OpcodeEval<OP_ALWAYS_FALSE>(PolicyOpcode* opcode,
+EvalResult OpcodeEval<OP_ALWAYS_FALSE>(const PolicyOpcode* opcode,
                                        const ParameterSet* param,
                                        MatchContext* context) {
   return EVAL_FALSE;
@@ -78,7 +78,7 @@ PolicyOpcode* OpcodeFactory::MakeOpAlwaysTrue(uint32_t options) {
 }
 
 template <>
-EvalResult OpcodeEval<OP_ALWAYS_TRUE>(PolicyOpcode* opcode,
+EvalResult OpcodeEval<OP_ALWAYS_TRUE>(const PolicyOpcode* opcode,
                                       const ParameterSet* param,
                                       MatchContext* context) {
   return EVAL_TRUE;
@@ -101,7 +101,7 @@ PolicyOpcode* OpcodeFactory::MakeOpAction(EvalResult action,
 }
 
 template <>
-EvalResult OpcodeEval<OP_ACTION>(PolicyOpcode* opcode,
+EvalResult OpcodeEval<OP_ACTION>(const PolicyOpcode* opcode,
                                  const ParameterSet* param,
                                  MatchContext* context) {
   int action = 0;
@@ -138,7 +138,7 @@ PolicyOpcode* OpcodeFactory::MakeOpVoidPtrMatch(uint8_t selected_param,
 }
 
 template <>
-EvalResult OpcodeEval<OP_NUMBER_MATCH>(PolicyOpcode* opcode,
+EvalResult OpcodeEval<OP_NUMBER_MATCH>(const PolicyOpcode* opcode,
                                        const ParameterSet* param,
                                        MatchContext* context) {
   uint32_t value_uint32 = 0;
@@ -173,7 +173,7 @@ PolicyOpcode* OpcodeFactory::MakeOpNumberAndMatch(uint8_t selected_param,
 }
 
 template <>
-EvalResult OpcodeEval<OP_NUMBER_AND_MATCH>(PolicyOpcode* opcode,
+EvalResult OpcodeEval<OP_NUMBER_AND_MATCH>(const PolicyOpcode* opcode,
                                            const ParameterSet* param,
                                            MatchContext* context) {
   uint32_t value = 0;
@@ -217,7 +217,7 @@ PolicyOpcode* OpcodeFactory::MakeOpWStringMatch(uint8_t selected_param,
 }
 
 template <>
-EvalResult OpcodeEval<OP_WSTRING_MATCH>(PolicyOpcode* opcode,
+EvalResult OpcodeEval<OP_WSTRING_MATCH>(const PolicyOpcode* opcode,
                                         const ParameterSet* param,
                                         MatchContext* context) {
   if (!context) {
@@ -358,7 +358,7 @@ ptrdiff_t OpcodeFactory::AllocRelative(void* start, std::wstring_view str) {
 
 EvalResult PolicyOpcode::Evaluate(const ParameterSet* call_params,
                                   size_t param_count,
-                                  MatchContext* match) {
+                                  MatchContext* match) const {
   if (!call_params)
     return EVAL_ERROR;
   const ParameterSet* selected_param = nullptr;
@@ -398,7 +398,7 @@ EvalResult PolicyOpcode::Evaluate(const ParameterSet* call_params,
     return OpcodeEval<op>(x, y, z)
 
 EvalResult PolicyOpcode::EvaluateHelper(const ParameterSet* parameters,
-                                        MatchContext* match) {
+                                        MatchContext* match) const {
   switch (opcode_id_) {
     OPCODE_EVAL(OP_ALWAYS_FALSE, this, parameters, match);
     OPCODE_EVAL(OP_ALWAYS_TRUE, this, parameters, match);
