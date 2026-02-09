94315737c1c7d14ed0628e5b117a90a6b6d29508
Aashna Sheth <aashnas@google.com>
2026-02-09 10:47:10 -0800

    [SkillsPage] Implement error check for saving invalid skill
    Screencast: https://screencast.googleplex.com/cast/NTI0NDczNjcyNTA1NzUzNnxlYmMyYWM5OC00Zg
    Bug: 479029101
    Change-Id: I7ef1ff4090cc2ba33e07c6e9a59d9db7001793ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7549006
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Christine Ying <chrstne@google.com>
    Commit-Queue: Aashna Sheth <aashnas@google.com>
    Cr-Commit-Position: refs/heads/main@{#1581875}

--- chrome/browser/resources/skills/card.ts
+++ chrome/browser/resources/skills/card.ts
@@ -87,7 +87,7 @@ export class SkillCardElement extends CrLitElement {
   }
 
   protected onSaveButtonClick_() {
-    this.fire('save-button-click');
+    this.fire('save-button-click', this.skill);
   }
 
   protected onMoreButtonClick_(event: MouseEvent) {
