2355985bbce9c606e8e81b7905d6b37319eeda5b
Lauren Winston <lwinston@google.com>
2026-02-09 11:30:03 -0800

    Disable omnibox during automating tests.
    Bug:b/479237585
    
    Change-Id: I9a1e053f5e2cc9c82d46c490a51f28f2526f140c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7553150
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1581918}

--- chrome/browser/ui/read_anything/read_anything_entry_point_controller.cc
+++ chrome/browser/ui/read_anything/read_anything_entry_point_controller.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <type_traits>
 
+#include "base/command_line.h"
 #include "chrome/browser/dom_distiller/tab_utils.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_features.h"
 #include "chrome/browser/ui/read_anything/read_anything_controller.h"
@@ -24,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/side_panel/side_panel_ui.h"
 #include "components/prefs/pref_filter.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/common/content_switches.h"
 #include "ui/accessibility/accessibility_features.h"
 
 namespace {
@@ -219,6 +221,14 @@ void ReadAnythingEntryPointController::CheckIfShouldSuggestReadingMode(
     BrowserWindowInterface* bwi,
     base::OnceCallback<void(bool)> result_callback) {
   if (!features::IsReadAnythingOmniboxChipEnabled() || !bwi) {
+    std::move(result_callback).Run(false);
+    return;
+  }
+  // Don't show the omnibox entrypoint if automation is enabled, such as
+  // during automated testing.
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kEnableAutomation)) {
+    std::move(result_callback).Run(false);
     return;
   }
 
