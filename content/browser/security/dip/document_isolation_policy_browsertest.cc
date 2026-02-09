c9425f5d9dfbd4edf6b1014f6eb0c196fc496f14
Camille Lamy <clamy@chromium.org>
2026-02-09 07:40:14 -0800

    Instantiate DocumentIsolationPolicy test in partial SiteIsolation mode
    This is a follow-up CL to
    https://chromium-review.googlesource.com/c/chromium/src/+/7246044. The
    previous CL added support for testing DocumentIsolationPolicy with
    SiteIsolation, partial SiteIsolation and no SiteIsolation. Initially, we
    attempted to instantiate the three modes for the test together. However,
    the Android-x86 bot is experiencing pervasive flakyness when executing
    content_browsertests and will randomly fail during some of the tests for
    reasons that have nothing to do with the tests or the code in the CL.
    Since the amount of tests added was very large, one of them would always
    hit the flakes and was impossible to submit the CL. To make progress, I
    have split the test instantiation in three, in hope that the smaller of
    tests added each time can avoid being hit by the flakes.
    
    Validate-Test-Flakiness: skip
    Bug: 342364564
    Change-Id: I2f1132296e0c93def3879231e16158f00ba46871
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7535257
    Reviewed-by: Joe DeBlasio <jdeblasio@chromium.org>
    Auto-Submit: Camille Lamy <clamy@chromium.org>
    Commit-Queue: Joe DeBlasio <jdeblasio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1581754}

--- content/browser/security/dip/document_isolation_policy_browsertest.cc
+++ content/browser/security/dip/document_isolation_policy_browsertest.cc
@@ -2670,11 +2670,12 @@ INSTANTIATE_TEST_SUITE_P(
     DocumentIsolationPolicyWithLogicalCOIBrowserTest,
     kTestParams,
     DocumentIsolationPolicyWithLogicalCOIBrowserTest::DescribeParams);
-static auto kTestParamsWithSiteIsolation =
-    testing::Combine(testing::ValuesIn(RenderDocumentFeatureLevelValues()),
-                     testing::Bool(),
-                     testing::Bool(),
-                     testing::Values(SiteIsolationStatus::kSiteIsolation));
+static auto kTestParamsWithSiteIsolation = testing::Combine(
+    testing::ValuesIn(RenderDocumentFeatureLevelValues()),
+    testing::Bool(),
+    testing::Bool(),
+    testing::Values(SiteIsolationStatus::kSiteIsolation,
+                    SiteIsolationStatus::kPartialSiteIsolation));
 INSTANTIATE_TEST_SUITE_P(All,
                          DocumentIsolationPolicyBrowserTest,
                          kTestParamsWithSiteIsolation,
