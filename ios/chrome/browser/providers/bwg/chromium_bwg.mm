f9dd46c950a1932fc175cfad3a0c4690e4b04464
Joemer Ramos <joemerramos@google.com>
2026-02-09 11:33:41 -0800

    [iOS] Create Provider Function For Passing Image Attachments To Floaty
    This API allows GeminiBrowserAgent to pass an image attachment to the
    floaty without resetting the floaty instance when Copresence is
    enabled.
    
    No-op until crrev.com/i/9005696 lands.
    
    Bug: 482334416
    Change-Id: I2f8e1bc51454df11dd9a63c81ace8ed1cee35c80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7559056
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Joemer Ramos <joemerramos@google.com>
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1581923}

--- ios/chrome/browser/providers/bwg/chromium_bwg.mm
+++ ios/chrome/browser/providers/bwg/chromium_bwg.mm
@@ -53,4 +53,6 @@ GeminiViewState GetCurrentGeminiViewState() {
 
 void RequestUIChange(GeminiUIElementType ui_element_type) {}
 
+void AttachImage(UIImage* image) {}
+
 }  // namespace ios::provider
