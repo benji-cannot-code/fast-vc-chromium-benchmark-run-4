feb826ac5aeb09981bb06fc80120dc4555a771c5
Keishi Hattori <keishi@chromium.org>
2026-02-08 23:37:10 -0800

    Revert "[bedrock] Migrate app_shim_manager_mac.h away from BrowserListObserver."
    This reverts commit 3a74aa4157df89b1c194d8725a7d709635f24c48.
    
    Reason for revert: Causes a consistent heap-use-after-free on Mac
    https://ci.chromium.org/ui/p/chromium/builders/ci/Mac%20ASan%2064%20Tests%20(1)/104474/overview
    
    Original change's description:
    > [bedrock] Migrate app_shim_manager_mac.h away from BrowserListObserver.
    >
    > This migration is part of project bedrock to reduce the dependencies on
    > Browser and BrowserList. See https://crbug.com/431671320 for more info.
    >
    > Bug: 459777668
    > Change-Id: I619f5439c879bbcf8f13cb5fe0a48b286a6a6964
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7551752
    > Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    > Commit-Queue: Glenn Hartmann <hartmanng@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1581070}
    
    Bug: 459777668
    Change-Id: I32e383dcb63649e7ff1b335a449ba09307f1b648
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7557051
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Keishi Hattori <keishi@chromium.org>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1581556}

--- chrome/browser/apps/app_shim/app_shim_manager_mac.cc
+++ chrome/browser/apps/app_shim/app_shim_manager_mac.cc
@@ -57,7 +57,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface_iterator.h"
-#include "chrome/browser/ui/browser_window/public/global_browser_collection.h"
 #include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/browser/ui/profiles/profile_picker.h"
 #include "chrome/browser/ui/ui_features.h"
@@ -478,11 +477,11 @@ AppShimManager::AppShimManager(std::unique_ptr<Delegate> delegate)
   if (profile_manager_) {
     profile_manager_->AddObserver(this);
   }
-  browser_collection_observation_.Observe(
-      GlobalBrowserCollection::GetInstance());
+  BrowserList::AddObserver(this);
 }
 
 AppShimManager::~AppShimManager() {
+  BrowserList::RemoveObserver(this);
   AppShimHostBootstrap::SetClient(nullptr);
 }
 
@@ -1746,30 +1745,29 @@ void AppShimManager::OnAppDeactivated(content::BrowserContext* context,
 void AppShimManager::OnAppStop(content::BrowserContext* context,
                                const std::string& app_id) {}
 
-void AppShimManager::OnBrowserCreated(BrowserWindowInterface* browser) {
-  Profile* profile = browser->GetProfile();
-  const std::string app_id = web_app::GetAppIdFromApplicationName(
-      browser->GetBrowserForMigrationOnly()->app_name());
+void AppShimManager::OnBrowserAdded(Browser* browser) {
+  Profile* profile = browser->profile();
+  const std::string app_id =
+      web_app::GetAppIdFromApplicationName(browser->app_name());
   if (!delegate_->AppUsesRemoteCocoa(profile, app_id)) {
     return;
   }
   if (auto* profile_state = GetOrCreateProfileState(profile, app_id)) {
-    profile_state->browsers.insert(browser->GetBrowserForMigrationOnly());
+    profile_state->browsers.insert(browser);
     if (profile_state->browsers.size() == 1) {
-      OnAppActivated(browser->GetProfile(), app_id);
+      OnAppActivated(browser->profile(), app_id);
     }
   }
 }
 
-void AppShimManager::OnBrowserClosed(BrowserWindowInterface* browser) {
+void AppShimManager::OnBrowserRemoved(Browser* browser) {
   // We can't call OnAppDeactivated() while iterating on |apps_|. It would
   // invalidate the iterator.
   std::vector<std::string> apps_to_deactivate;
 
   for (const auto& [app_id, app_state] : apps_) {
     for (const auto& [profile, profile_state] : app_state->profiles) {
-      auto found =
-          profile_state->browsers.find(browser->GetBrowserForMigrationOnly());
+      auto found = profile_state->browsers.find(browser);
       if (found != profile_state->browsers.end()) {
         // If we have no browser windows open after erasing this window, then
         // close the ProfileState (and potentially the shim as well).
@@ -1783,11 +1781,11 @@ void AppShimManager::OnBrowserClosed(BrowserWindowInterface* browser) {
   }
 
   for (const std::string& app_id : apps_to_deactivate) {
-    OnAppDeactivated(browser->GetProfile(), app_id);
+    OnAppDeactivated(browser->profile(), app_id);
   }
 }
 
-void AppShimManager::OnBrowserActivated(BrowserWindowInterface* browser) {
+void AppShimManager::OnBrowserSetLastActive(Browser* browser) {
   // Rebuild the profile menu items (to ensure that the checkmark in the menu
   // is next to the new-active item).
   if (avatar_menu_) {
@@ -1796,14 +1794,14 @@ void AppShimManager::OnBrowserActivated(BrowserWindowInterface* browser) {
   UpdateAllProfileMenus();
 
   // Update the application dock menu for the current profile.
-  const std::string app_id = web_app::GetAppIdFromApplicationName(
-      browser->GetBrowserForMigrationOnly()->app_name());
-  if (!delegate_->AppUsesRemoteCocoa(browser->GetProfile(), app_id)) {
+  const std::string app_id =
+      web_app::GetAppIdFromApplicationName(browser->app_name());
+  if (!delegate_->AppUsesRemoteCocoa(browser->profile(), app_id)) {
     return;
   }
-  auto* profile_state = GetOrCreateProfileState(browser->GetProfile(), app_id);
+  auto* profile_state = GetOrCreateProfileState(browser->profile(), app_id);
   if (profile_state) {
-    UpdateApplicationDockMenu(browser->GetProfile(), profile_state);
+    UpdateApplicationDockMenu(browser->profile(), profile_state);
   }
 }
 
