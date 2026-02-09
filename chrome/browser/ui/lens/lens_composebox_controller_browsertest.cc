1a8627bf3f59cdd0cabcda026e47e86cc43c95fa
Dave Tapuska <dtapuska@chromium.org>
2026-02-09 14:55:24 -0800

    [lens] Move reshow overlay into base class
    - The reshowing of the overlay is a unique flow of adjusting transparency
    on the webview so it doesn't show a stale image. Move the implementation
    into the base class.
    - Move triggering the fade out animation to the base class as well.
    - Adjust browser test to ensure we run the full reshow code path instead
    of ending early.
    
    Bug: b:477955819
    Change-Id: I180a8738a710739381066a621cb51420e7e033a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7559811
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1582074}

--- chrome/browser/ui/lens/lens_composebox_controller_browsertest.cc
+++ chrome/browser/ui/lens/lens_composebox_controller_browsertest.cc
@@ -662,6 +662,8 @@ IN_PROC_BROWSER_TEST_F(LensComposeboxControllerBrowserTest,
       ->HandleLensButtonClick();
   ASSERT_TRUE(
       base::test::RunUntil([&]() { return IsResultsSidePanelShowing(); }));
+  ASSERT_TRUE(base::test::RunUntil(
+      [&]() { return overlay_controller->state() == State::kOverlay; }));
 }
 
 IN_PROC_BROWSER_TEST_F(LensComposeboxControllerBrowserTest,
