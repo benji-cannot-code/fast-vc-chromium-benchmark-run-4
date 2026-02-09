1dd7198cf0f192c321acb39be25d1f08da6e5688
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2026-02-08 16:28:37 -0800

    Revert "[omnibox][webui] SubframeTaskBrowserTest & SubframeTaskPDFBrowserTest"
    This reverts commit 1b476b7b72ace268b71fa396d3d7bbeba1bdd02c.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/6680828655435776
    
    Sample build with failed test: https://ci.chromium.org/b/8690566305596659585
    Affected test(s):
    [://chrome/test\:browser_tests!gtest::SubframeTaskPDFBrowserTest#TaskManagerShowsIncognitoPDFSubframeTask/All.0](https://ci.chromium.org/ui/test/chromium/:%2F%2Fchrome%2Ftest%5C:browser_tests%21gtest::SubframeTaskPDFBrowserTest%23TaskManagerShowsIncognitoPDFSubframeTask%2FAll.0?q=VHash%3A21063c918fd9bbef)
    [://chrome/test\:browser_tests!gtest::SubframeTaskPDFBrowserTest#TaskManagerShowsIncognitoPDFSubframeTask/All.1](https://ci.chromium.org/ui/test/chromium/:%2F%2Fchrome%2Ftest%5C:browser_tests%21gtest::SubframeTaskPDFBrowserTest%23TaskManagerShowsIncognitoPDFSubframeTask%2FAll.1?q=VHash%3A21063c918fd9bbef)
    [://chrome/test\:browser_tests!gtest::SubframeTaskPDFBrowserTest#TaskManagerShowsPDFSubframeTask/All.0](https://ci.chromium.org/ui/test/chromium/:%2F%2Fchrome%2Ftest%5C:browser_tests%21gtest::SubframeTaskPDFBrowserTest%23TaskManagerShowsPDFSubframeTask%2FAll.0?q=VHash%3A21063c918fd9bbef)
    [://chrome/test\:browser_tests!gtest::SubframeTaskPDFBrowserTest#TaskManagerShowsPDFSubframeTask/All.1](https://ci.chromium.org/ui/test/chromium/:%2F%2Fchrome%2Ftest%5C:browser_tests%21gtest::SubframeTaskPDFBrowserTest%23TaskManagerShowsPDFSubframeTask%2FAll.1?q=VHash%3A21063c918fd9bbef)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F6680828655435776&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F7546190&type=BUG
    
    Original change's description:
    > [omnibox][webui] SubframeTaskBrowserTest & SubframeTaskPDFBrowserTest
    >
    > Enable `kWebUIOmniboxPopup` & `kWebUIOmniboxAimPopup` and update 6
    > failing tests to pass with the features enabled:
    >
    > SubframeTaskBrowserTest.TaskManagerHungSubframe
    > SubframeTaskBrowserTest.TaskManagerShowsSubframeTasks
    > All/SubframeTaskPDFBrowserTest.TaskManagerShowsIncognitoPDFSubframeTask/0
    > All/SubframeTaskPDFBrowserTest.TaskManagerShowsIncognitoPDFSubframeTask/1
    > All/SubframeTaskPDFBrowserTest.TaskManagerShowsPDFSubframeTask/0
    > All/SubframeTaskPDFBrowserTest.TaskManagerShowsPDFSubframeTask/1
    >
    > The tests now expect 2 additional omnibox tasks.
    >
    > Also makes the test expectations more complete:
    >     EXPECT_THAT(actual_tasks, ElementsAre("task_0", "task_1", "task_2"))
    >
    > instead of previously:
    >     EXPECT_TRUE(StartsWith(actual_tasks[2], "task")
    >
    > Bug: 452061489
    > Change-Id: I4866a6249728e99b4a3cac34433360aca99753ba
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7546190
    > Auto-Submit: manuk hovanesian <manukh@chromium.org>
    > Reviewed-by: Michael Wojcicka <mwoj@google.com>
    > Commit-Queue: manuk hovanesian <manukh@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1581215}
    >
    
    Bug: 452061489
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I51849f9f93942dd74e6977f74793ca2ae4be8212
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7554610
    Reviewed-by: Bao-Duy Tran <tranbaoduy@chromium.org>
    Owners-Override: Bao-Duy Tran <tranbaoduy@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Bao-Duy Tran <tranbaoduy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1581495}

--- chrome/browser/task_manager/mock_web_contents_task_manager.h
+++ chrome/browser/task_manager/mock_web_contents_task_manager.h
@@ -6,13 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_TASK_MANAGER_MOCK_WEB_CONTENTS_TASK_MANAGER_H_
 #define CHROME_BROWSER_TASK_MANAGER_MOCK_WEB_CONTENTS_TASK_MANAGER_H_
 
-#include <algorithm>
-#include <iterator>
-#include <string>
-#include <vector>
-
 #include "base/memory/raw_ptr.h"
-#include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/task_manager/providers/task_provider_observer.h"
 #include "chrome/browser/task_manager/providers/web_contents/web_contents_tags_manager.h"
 #include "chrome/browser/task_manager/providers/web_contents/web_contents_task_provider.h"
@@ -39,14 +33,6 @@ class MockWebContentsTaskManager : public TaskProviderObserver {
 
   const std::vector<raw_ptr<Task, VectorExperimental>>& tasks() const {
     return tasks_;
-  }
-
-  std::vector<std::string> TaskTitles() {
-    std::vector<std::string> task_titles;
-    std::ranges::transform(
-        tasks_, std::back_inserter(task_titles),
-        [&](const auto& task) { return base::UTF16ToUTF8(task->title()); });
-    return task_titles;
   }
 
  private:
