0d0d458f42d87cf160de2b4e06cb41574413da3c
Qikai <qikaizhong@microsoft.com>
2026-02-09 11:13:10 -0800

    [bedrock] Remove BrowserList::CloseAllBrowsersWithProfile() and BrowserList::CloseAllBrowsersWithIncognitoProfile()
    These methods are no longer needed as callsites have migrated to
    alternatives. Also removes the related private helper methods
    TryToCloseBrowserList() and PostTryToCloseBrowserWindow(), the
    BrowserWeakVector and CloseCallback type aliases, and cleans up
    unused includes and forward declarations in both the header and
    implementation files.
    
    Bug: 431671320
    Change-Id: If6946f39e593ef3ee23148b3aa8861701eb41866
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7557771
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1581900}

--- chrome/browser/ui/browser_list.cc
+++ chrome/browser/ui/browser_list.cc
@@ -7,9 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
-#include "base/auto_reset.h"
 #include "base/check.h"
-#include "base/functional/bind.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/user_metrics.h"
 #include "base/observer_list.h"
@@ -19,8 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/lifetime/browser_shutdown.h"
 #include "chrome/browser/lifetime/termination_notification.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/sessions/app_session_service_factory.h"
-#include "chrome/browser/sessions/session_service_factory.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_list_enumerator.h"
@@ -32,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/keep_alive_registry/keep_alive_types.h"
 
 using base::UserMetricsAction;
-using content::WebContents;
 
 // static
 base::LazyInstance<base::ObserverList<BrowserListObserver>,
