d6807f6b6211210e5d4a90f4cbd22b659361e2b2
Shu Yang <shuyng@google.com>
2026-02-09 14:54:59 -0800

    Revert "[Media Indicator] Add SHARING media stream"
    This reverts commit 0ba42008e170130575aeaa0942cde20c14b2f800.
    
    Reason for revert: b/483132652
    
    Original change's description:
    > [Media Indicator] Add SHARING media stream
    >
    > This adds a GUM_TAB_VIDEO_CAPTURE to signal MediaStreamCaptureIndicator
    > the webcontents of the capturee. This will allow us to identify which
    > tab is being mirrored in MediaStateObserver.
    >
    > CL for Java UI logic: https://crrev.com/c/7162075
    >
    > http://go/scrcast/NDg4NzAzNTY3NTQxMDQzMnwzM2Q4NGMzOC05Zg
    >
    > Fixed: 470394370
    > Change-Id: I6ea30924d0c684e291731ec96fbb7e5410be5dbd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7279381
    > Reviewed-by: Tomas Gunnarsson <tommi@chromium.org>
    > Reviewed-by: Grace Cham <hscham@chromium.org>
    > Commit-Queue: Dan Polanco <polardz@google.com>
    > Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1581881}
    
    Bug: 470394370
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ifa5669e57e6d8d7032a78f8fbae0fad3f763b95f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7560730
    Owners-Override: Shu Yang <shuyng@google.com>
    Commit-Queue: Shu Yang <shuyng@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1582073}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -4570,14 +4570,6 @@ const FeatureEntry::Choice kOmniboxAutofocusOnIncognitoNtpChoices[] = {
 };
 #endif  // !BUILDFLAG(IS_ANDROID)
 
-#if BUILDFLAG(IS_ANDROID)
-const FeatureEntry::FeatureParam kMediaIndicatorsAndroidSharingEnabled[] = {
-    {"sharing", "true"}};
-const FeatureEntry::FeatureVariation kMediaIndicatorsAndroidVariations[] = {
-    {"- With tab sharing indicator", kMediaIndicatorsAndroidSharingEnabled,
-     nullptr}};
-#endif  // BUILDFLAG(IS_ANDROID)
-
 const FeatureEntry::FeatureParam
     kDeviceBoundSessionsForRestrictedSitesExperimentIdFromFlags[] = {
         {"Value", "set_from_flags"}};
@@ -11794,9 +11786,7 @@ const FeatureEntry kFeatureEntries[] = {
 #if BUILDFLAG(IS_ANDROID)
     {"media-indicators-android", flag_descriptions::kMediaIndicatorsAndroidName,
      flag_descriptions::kMediaIndicatorsAndroidDescription, kOsAndroid,
-     FEATURE_WITH_PARAMS_VALUE_TYPE(chrome::android::kMediaIndicatorsAndroid,
-                                    kMediaIndicatorsAndroidVariations,
-                                    "MediaIndicatorsAndroid")},
+     FEATURE_VALUE_TYPE(chrome::android::kMediaIndicatorsAndroid)},
 #endif  // BUILDFLAG(IS_ANDROID)
 
 #if !BUILDFLAG(IS_ANDROID)
