c9e66f3c7d493ea194267fbb8e624c2144400084
seannli <seannli@google.com>
2026-02-08 23:50:29 -0800

    Refine notification order to ensure displayed one is latest
    Replace Map with List to save notifications to ensure the displayed one
    is always latest.
    
    Bug: 352187279, 466009472
    Test: Manual test
    Change-Id: Ifbc0f20598e47cc633573d35e6e32cebafaa2516
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7550952
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Sean Li <seannli@google.com>
    Reviewed-by: Grace Cham <hscham@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1581560}

--- chrome/android/java/src/org/chromium/chrome/browser/media/MediaCaptureNotificationServiceImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/MediaCaptureNotificationServiceImpl.java
@@ -15,6 +15,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ServiceInfo;
 import android.os.IBinder;
+import android.util.Pair;
 
 import androidx.core.app.ActivityCompat;
 
@@ -52,9 +53,9 @@ import org.chromium.ui.base.WindowAndroid;
 import org.chromium.url.GURL;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 import java.util.TreeMap;
 
@@ -77,8 +78,7 @@ public class MediaCaptureNotificationServiceImpl extends SplitCompatService.Impl
     private BaseNotificationManagerProxy mNotificationManager;
     private SharedPreferencesManager mSharedPreferences;
     private final TreeMap<Integer, Set<@MediaType Integer>> mNotificationsType = new TreeMap<>();
-    private final TreeMap<Integer, NotificationWrapper> mNotifications =
-            new TreeMap<>(Comparator.reverseOrder());
+    private final List<Pair<Integer, NotificationWrapper>> mNotifications = new ArrayList<>();
 
     private boolean mStartedForegroundService;
     private int mForgroundServiceType;
@@ -221,9 +221,11 @@ public class MediaCaptureNotificationServiceImpl extends SplitCompatService.Impl
             }
             mNotificationsType.remove(notificationId);
             if (DeviceInfo.isDesktop()) {
+                int lastIndex = mNotifications.size() - 1;
                 boolean isRemovingLatestNotification =
-                        !mNotifications.isEmpty() && notificationId == mNotifications.firstKey();
-                mNotifications.remove(notificationId);
+                        lastIndex >= 0 && mNotifications.get(lastIndex).first == notificationId;
+                mNotifications.removeIf(
+                        notificationEntry -> notificationEntry.first == notificationId);
                 if (!hasNewMediaTypesToUpate) {
                     if (mNotifications.isEmpty()) {
                         stopForegroundService();
@@ -234,10 +236,9 @@ public class MediaCaptureNotificationServiceImpl extends SplitCompatService.Impl
                         //    going to be removed.
                         // 2. Update service if the current foreground type no longer matches the
                         //    required type.
-                        int latestNotificationId = mNotifications.firstKey();
-                        NotificationWrapper latestNotification =
-                                assumeNonNull(mNotifications.get(latestNotificationId));
-                        startOrUpdateForegroundService(latestNotificationId, latestNotification);
+                        Pair<Integer, NotificationWrapper> latest =
+                                mNotifications.get(mNotifications.size() - 1);
+                        startOrUpdateForegroundService(latest.first, latest.second);
                     }
                 }
             }
@@ -319,7 +320,7 @@ public class MediaCaptureNotificationServiceImpl extends SplitCompatService.Impl
             // For large screen device, we use the latest notification to start or update
             // the foreground service.
             startOrUpdateForegroundService(notificationId, notification);
-            mNotifications.put(notificationId, notification);
+            mNotifications.add(new Pair<>(notificationId, notification));
         } else {
             mNotificationManager.notify(notification);
         }
