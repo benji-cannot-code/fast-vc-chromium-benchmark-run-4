3b4fc949c11a518f4a50b2aaf293e3219505c809
Norge Vizcay <vizcay@google.com>
2026-02-09 03:20:19 -0800

    Move accessibility annotator features out of core/public.
    Relocates accessibility_annotator_features files from the core/public
    directory to core, simplifying the directory structure. Updated all
    relevant include paths and build targets to reflect the move.
    
    Bug: 475305011
    Change-Id: Ib4f2aabf5dc4a6b110123d8c210d3ee6a29778b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7558210
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1581634}

--- chrome/browser/accessibility_annotator/content_annotator/content_annotator_service_factory.cc
+++ chrome/browser/accessibility_annotator/content_annotator/content_annotator_service_factory.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/page_content_annotations/page_content_annotations_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "components/accessibility_annotator/content/content_annotator/content_annotator_service.h"
-#include "components/accessibility_annotator/core/public/accessibility_annotator_features.h"
+#include "components/accessibility_annotator/core/accessibility_annotator_features.h"
 
 namespace accessibility_annotator {
 
