3bc2a9fe5b2fce30db7976dd42f4ff461b20960a
Stephen Nusko <nuskos@chromium.org>
2026-02-09 08:09:21 -0800

    Remove MiracleObject (PA/AC) from startup until first renderer
    Currently MiracleObject starts zapping and quarantining free'd pointers
    immediately after the FeatureList is initialized, but during the period
    of time that no contact with untrusted code the risk of a High severity
    UaF exploit is low. We attempt to proxy (with some inaccuracy) this
    point by using ReadyToCommitNavigation.
    
    Therefore we can avoid impacting startup metrics by just not
    initializing the SchedulerLoopQuarantine until the first WebContents is
    attempts to navigate. (that isn't about:blank). We do this by installing
    a WebContentsUserData during construction of the WebContentsImpl.
    
    This is reasonable because if no renderer has ever been navigated there
    are far fewer paths to an exploit that originated from web content
    which is needed for a high severity UaF exploit.
    
    Change-Id: I497addbe10debfef6a87e9be983b60cd20a9cbac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7515449
    Commit-Queue: Stephen Nusko <nuskos@chromium.org>
    Auto-Submit: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1581773}

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -92,6 +92,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/host_zoom_map_impl.h"
 #include "content/browser/media/audio_stream_monitor.h"
 #include "content/browser/media/media_web_contents_observer.h"
+#include "content/browser/memory/scheduler_loop_quarantine_web_contents_observer.h"
 #include "content/browser/permissions/permission_controller_impl.h"
 #include "content/browser/permissions/permission_util.h"
 #include "content/browser/preloading/prefetch/prefetch_request.h"
@@ -4215,6 +4216,7 @@ void WebContentsImpl::Init(const WebContents::CreateParams& params,
     AttributionHost::CreateForWebContents(this);
   }
 
+  SchedulerLoopQuarantineWebContentsObserver::MaybeCreateForWebContents(this);
   RedirectChainDetector::CreateForWebContents(this);
   BtmWebContentsObserver::MaybeCreateForWebContents(this);
   RedirectHeuristicTabHelper::CreateForWebContents(this);
