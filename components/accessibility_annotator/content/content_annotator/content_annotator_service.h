c9b1d29e07ae0eacb82af11052e4a43418ca2e51
Andrew Vuong <akvuong@google.com>
2026-02-09 10:06:20 -0800

    Implements OnPageContentAnnotated to store data
    This CL implements OnPageContentAnnotated to store sensitivity scores
    for the GURL and its corresponding timestamp. Following CLs will collect
    additional data for the GURL (language, embeddings, etc.) to be used for
    triggering annotation.
    
    Bug: 475859254
    Change-Id: Ifb2441729a092df455e2080f8276a2140930a43e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7548128
    Commit-Queue: Andrew Vuong <akvuong@google.com>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1581845}

--- components/accessibility_annotator/content/content_annotator/content_annotator_service.h
+++ components/accessibility_annotator/content/content_annotator/content_annotator_service.h
@@ -6,9 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_ACCESSIBILITY_ANNOTATOR_CONTENT_CONTENT_ANNOTATOR_CONTENT_ANNOTATOR_SERVICE_H_
 #define COMPONENTS_ACCESSIBILITY_ANNOTATOR_CONTENT_CONTENT_ANNOTATOR_CONTENT_ANNOTATOR_SERVICE_H_
 
+#include "base/containers/lru_cache.h"
 #include "base/memory/raw_ref.h"
+#include "components/accessibility_annotator/content/content_annotator/content_classifier.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/page_content_annotations/core/page_content_annotations_service.h"
+#include "url/gurl.h"
 
 namespace accessibility_annotator {
 
@@ -33,8 +36,27 @@ class ContentAnnotatorService
       override;
 
  private:
+  using CacheIterator =
+      base::LRUCache<GURL, ContentClassificationInput>::iterator;
+
+  // Returns the iterator to the entry for the given URL, creating a new entry
+  // if it does not exist.
+  CacheIterator GetOrCreateJoinEntry(const GURL& url);
+
+  // If the data is complete, runs content classification and determines
+  // annotation eligibility.
+  void MaybeAnnotate(CacheIterator it);
+
   const raw_ref<page_content_annotations::PageContentAnnotationsService>
       page_content_annotations_service_;
+
+  // Stores and joins data for URLs that are pending annotation. The cache size
+  // is `kContentAnnotatorMaxPendingUrls`. When the cache is full, the last
+  // modified entry is evicted. The cache should only go over capacity in the
+  // case expected data from an observation does not arrive or the user
+  // navigates to several URLs faster than data can be collected for the URL and
+  // annotations processed.
+  base::LRUCache<GURL, ContentClassificationInput> join_entries_;
 };
 
 }  // namespace accessibility_annotator
--- components/accessibility_annotator/core/accessibility_annotator_features.h
+++ components/accessibility_annotator/core/accessibility_annotator_features.h
@@ -7,10 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_ACCESSIBILITY_ANNOTATOR_CORE_ACCESSIBILITY_ANNOTATOR_FEATURES_H_
 
 #include "base/feature_list.h"
+#include "base/metrics/field_trial_params.h"
 
 namespace accessibility_annotator {
 
 BASE_DECLARE_FEATURE(kContentAnnotator);
+
+extern const base::FeatureParam<int> kContentAnnotatorMaxPendingUrls;
 
 }  // namespace accessibility_annotator
 
