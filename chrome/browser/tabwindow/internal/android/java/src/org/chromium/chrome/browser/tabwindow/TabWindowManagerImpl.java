befc01f52b7d4548f80bc8056a653a3038aca0f9
Fiaz Muhammad <mfiaz@google.com>
2026-02-09 11:40:48 -0800

    [Persist] Add SharedPrefs for being caught up and wire
    - The PersistentStoreMigrationManager should be aware of shadow
    	store-only events.
    - Guard diff metrics to only record upon catching up.
    - Integrate the ability for multiple migration managers to exist
            for the same window tag, which is required for when we
            reassign window IDs.
    
    Bug: 476447678
    Change-Id: I5e4bbecb9cb225b3feff98df28865283836b3e22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7511485
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1581928}

--- chrome/browser/tabwindow/internal/android/java/src/org/chromium/chrome/browser/tabwindow/TabWindowManagerImpl.java
+++ chrome/browser/tabwindow/internal/android/java/src/org/chromium/chrome/browser/tabwindow/TabWindowManagerImpl.java
@@ -104,9 +104,7 @@ public class TabWindowManagerImpl implements TabWindowManager {
 
     /** Holds dependencies associated with a specific window. */
     private static class WindowDeps {
-        @SuppressWarnings("unused")
         public final PersistentStoreMigrationManager manager;
-
         public final TabModelSelector selector;
 
         /**
@@ -439,6 +437,7 @@ public class TabWindowManagerImpl implements TabWindowManager {
                         isActivityAtRequestedWindowIdInAppTasks,
                         isActivityAtRequestedWindowIdInSameTask);
         if (!handled) return originallyAssignedWindowIdForNewActivity;
+
         @WindowId
         int releasedWindowId = clearSelectorAndWindowIdAssignments(activityAtRequestedWindowId);
         if (releasedWindowId == INVALID_WINDOW_ID) {
