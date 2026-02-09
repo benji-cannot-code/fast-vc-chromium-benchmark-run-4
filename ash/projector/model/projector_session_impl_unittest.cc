024aa62b177195168f76d3a226c4205e40835d0a
Sergii Bykov <sbykov@google.com>
2026-02-09 12:07:23 -0800

    CodeHealth: Clean up stale base::Feature ProjectorManagedUser
    Bug: 356234491
    Change-Id: I912267ca124f0bdd74c523dedc057c66c6bac60e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7557833
    Reviewed-by: Benjamin Zielinski <bzielinski@google.com>
    Commit-Queue: Sergii Bykov <sbykov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1581958}

--- ash/projector/model/projector_session_impl_unittest.cc
+++ ash/projector/model/projector_session_impl_unittest.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/projector/model/projector_session_impl.h"
 
-#include "ash/constants/ash_features.h"
 #include "ash/projector/projector_metrics.h"
 #include "ash/public/cpp/projector/projector_session.h"
 #include "ash/test/ash_test_base.h"
@@ -13,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/memory/raw_ptr.h"
 #include "base/test/metrics/histogram_tester.h"
-#include "base/test/scoped_feature_list.h"
 
 namespace ash {
 
@@ -33,15 +31,11 @@ class ProjectorSessionImplTest : public AshTestBase {
 
   // AshTestBase:
   void SetUp() override {
-    scoped_feature_list_.InitWithFeatures(
-        /*enabled_features=*/{features::kProjectorManagedUser},
-        /*disabled_features=*/{});
     AshTestBase::SetUp();
     session_ = static_cast<ProjectorSessionImpl*>(ProjectorSession::Get());
   }
 
  protected:
-  base::test::ScopedFeatureList scoped_feature_list_;
   raw_ptr<ProjectorSessionImpl, DanglingUntriaged> session_;
 };
 
