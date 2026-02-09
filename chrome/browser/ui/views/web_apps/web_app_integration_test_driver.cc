fabe8922272a7a44866fadf9a73ffd3009ab1dd2
lukedeng <lukedeng@chromium.org>
2026-02-08 20:03:38 -0800

    [PWA internal clean up] Add explicitly type for webapps::ManifestId
    This CL adds explicit type declarations for webapps::ManifestId in ['chrome/browser/sync/test/integration/web_apps', 'chrome/browser/ui/views/web_apps'].
    This is a preparatory step for migrating webapps::ManifestId to a new customized
    type, which will improve type safety and future extensibility.
    
    This CL was uploaded by git cl split.
    
    R=phillis@chromium.org
    
    Bug: 474358971
    Change-Id: I63a7370a17ad0c92e31fd136859bc5a54f33fc85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7546299
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Auto-Submit: Luke Deng <lukedeng@google.com>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Luke Deng <lukedeng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1581516}

--- chrome/browser/ui/views/web_apps/web_app_integration_test_driver.cc
+++ chrome/browser/ui/views/web_apps/web_app_integration_test_driver.cc
@@ -689,8 +689,8 @@ class UninstallCompleteWaiter final : public BrowserListObserver,
       observation_{this};
 };
 
-std::optional<ProfileState>
-GetStateForProfile(StateSnapshot* state_snapshot, Profile* profile) {
+std::optional<ProfileState> GetStateForProfile(StateSnapshot* state_snapshot,
+                                               Profile* profile) {
   CHECK(state_snapshot);
   CHECK(profile);
   auto it = state_snapshot->profiles.find(profile);
@@ -4142,8 +4142,9 @@ webapps::AppId GetAppIdForIsolatedSite(Site site) {
     // isolated-app:// origin based on the signing key of the app.
     GURL parent_app_origin = url_info.origin().GetURL();
     GURL start_url = parent_app_origin.Resolve(GetRelativeSubAppPath(site));
-    return GenerateAppId(/*manifest_id_path=*/std::nullopt, start_url,
-                         /*parent_manifest_id=*/parent_app_origin);
+    return GenerateAppId(
+        /*manifest_id_path=*/std::nullopt, start_url,
+        /*parent_manifest_id=*/webapps::ManifestId(parent_app_origin));
   }
 
   return url_info.app_id();
@@ -4164,7 +4165,8 @@ webapps::AppId WebAppIntegrationTestDriver::GetAppIdBySiteMode(Site site) {
     auto parent_site = GetSiteConfiguration(site_config.parent_site.value());
     return GenerateAppId(
         manifest_id, start_url,
-        GetTestServerForSiteMode(site).GetURL(parent_site.relative_url));
+        webapps::ManifestId(
+            GetTestServerForSiteMode(site).GetURL(parent_site.relative_url)));
   } else {
     return GenerateAppId(manifest_id, start_url);
   }
