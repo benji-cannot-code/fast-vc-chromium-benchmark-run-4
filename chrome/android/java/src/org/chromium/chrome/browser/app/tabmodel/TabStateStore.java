08a94efd2275b9597eb7070401a88d2389ee7eef
Fiaz Muhammad <mfiaz@google.com>
2026-02-09 11:40:58 -0800

    [Persist] Add TabStateStore-only ClearWindow operation
    - Our TabStateStore does not support most TabPersistencePolicy
    	functionality. Most cleaning is done by the TabStateStore,
    	which fully encapsulates all storage operations.
    - This creates a TabStateStore-only operation which clears the current
    	window.
    - Use this clearWindow operation to destroy custom tab data when
    	needed.
    
    Bug: 475252643
    Change-Id: I3bf1568283c3631adc4c817a998834f1683fcded
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7528475
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1581929}

--- chrome/android/java/src/org/chromium/chrome/browser/app/tabmodel/TabStateStore.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/tabmodel/TabStateStore.java
@@ -396,6 +396,14 @@ public class TabStateStore implements TabPersistentStore {
     }
 
     @Override
+    public void clearCurrentWindow() {
+        assertInitialized();
+
+        mTabStateStorageService.clearWindow(mWindowTag);
+        mMigrationManager.onShadowStoreRazed();
+    }
+
+    @Override
     public void addObserver(TabPersistentStoreObserver observer) {
         mObservers.addObserver(observer);
     }
