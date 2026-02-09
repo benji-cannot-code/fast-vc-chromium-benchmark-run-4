86fe8dce0a0b2cf5813ee6d5e5491fce568e1704
rbpotter <rbpotter@chromium.org>
2026-02-09 11:29:30 -0800

    Management UI: Make promotion-banner a proper custom element
    - Change banner template not to include the boolean indicating whether
      to show it.
    - Move CSS to a proper CSS file, as it should not be included in the
      HTML template.
    - Add class definition with click handling
    - Use new custom element from management_ui.html.ts, instead of
      invoking getPromotionalBannerHtml().
    
    Bug: 481519338
    Change-Id: I5acf3019ca1e40196e3a268eaa1b9103f341fb65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7548318
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1581917}

--- chrome/browser/resources/management/management_ui.html.ts
+++ chrome/browser/resources/management/management_ui.html.ts
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {html} from '//resources/lit/v3_0/lit.rollup.js';
 
 import type {ManagementUiElement} from './management_ui.js';
-import {getPromotionBannerHtml} from './promotion_banner.html.js';
 
 export function getHtml(this: ManagementUiElement) {
   // clang-format off
@@ -25,7 +24,10 @@ export function getHtml(this: ManagementUiElement) {
         </cr-icon-button>
         <h2 class="cr-title-text">${this.subtitle_}</h2>
       </section>
-      ${getPromotionBannerHtml.call(this)}
+      ${this.shouldShowPromotion_ ? html`
+        <promotion-banner @promotion-dismissed="${this.onPromotionDismissed_}">
+        </promotion-banner>
+      ` : ''}
 <if expr="is_chromeos">
       <section class="eol-section" ?hidden="${!this.eolMessage_}">
         <div class="eol-warning-icon">
