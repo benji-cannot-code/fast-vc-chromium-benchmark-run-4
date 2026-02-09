db877b67900a3b379e69e4361185a55a4636d390
Devlin Cronin <rdevlin.cronin@chromium.org>
2026-02-09 13:38:39 -0800

    [Extensions] Remove WindowController::CanClose()
    WindowController::CanClose() calls into the window to see if the
    tab strip is editable, and returns false if not. It also has only one
    caller.
    
    This can be simplified a lot: the single caller can just check the tab
    list directly, and we can get rid of the method entirely.
    
    This has the advantage of effectively porting the check to the
    experimental desktop android platform.
    
    Bug: 438960629
    Change-Id: I4399f9089e73cd45f96225b99e1c9e9af78091c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7552847
    Commit-Queue: James Cook <jamescook@chromium.org>
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1582019}

--- chrome/browser/extensions/api/tabs/app_window_controller.cc
+++ chrome/browser/extensions/api/tabs/app_window_controller.cc
@@ -47,10 +47,6 @@ void AppWindowController::SetFullscreenMode(bool is_fullscreen,
   // Full screen not supported by app windows.
 }
 
-bool AppWindowController::CanClose(Reason* reason) const {
-  return true;
-}
-
 Browser* AppWindowController::GetBrowser() const {
   return nullptr;
 }
