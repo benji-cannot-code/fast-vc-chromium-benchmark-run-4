c6cb1fa3eb84f04a1dad2fb54b5c5e849bd37d2a
Michael Tang <tangm@microsoft.com>
2026-02-10 08:36:03 -0800

    Remove EarlyFullScreenVideoOptimization flag and enable by default
    This change removes the flag EarlyFullScreenVideoOptimization since it
    is enabled by default now since m144. It also removes code that is now
    dead from the flag removal.
    
    Bug: 435514197
    Change-Id: I4d98fcba9deb9db90831a64cc8fbc8c3ef59b9cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6701791
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Reviewed-by: Michael Tang <tangm@microsoft.com>
    Commit-Queue: Michael Tang <tangm@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1582550}

--- components/viz/service/display/overlay_dc_unittest.cc
+++ components/viz/service/display/overlay_dc_unittest.cc
@@ -3016,16 +3016,7 @@ INSTANTIATE_TEST_SUITE_P(
     &OverlayProcessorWinSurfacePlaneTest::GetParamName);
 
 class OverlayProcessorWinSurfacePlaneFullScreenTest
-    : public OverlayProcessorWinSurfacePlaneTest {
- public:
-  OverlayProcessorWinSurfacePlaneFullScreenTest() {
-    feature_list_.InitAndEnableFeature(
-        features::kEarlyFullScreenVideoOptimization);
-  }
-
- private:
-  base::test::ScopedFeatureList feature_list_;
-};
+    : public OverlayProcessorWinSurfacePlaneTest {};
 
 // Check that we can correctly mark a full screen video (that has a background
 // mat) as "full screen".
@@ -3346,9 +3337,8 @@ class OverlayProcessorWinFullScreenTest
     : public OverlayProcessorWinDelegatedCompositingTest {
  public:
   OverlayProcessorWinFullScreenTest() {
-    feature_list_.InitWithFeatures(
-        {features::kEarlyFullScreenVideoOptimization},
-        {features::kDirectCompositionLetterboxVideoOptimization});
+    feature_list_.InitAndDisableFeature(
+        features::kDirectCompositionLetterboxVideoOptimization);
   }
 
   void SetUp() override {
@@ -3712,10 +3702,8 @@ class OverlayProcessorWinFullScreenWithAdjustmentTest
     : public OverlayProcessorWinDelegatedCompositingTest {
  public:
   OverlayProcessorWinFullScreenWithAdjustmentTest() {
-    feature_list_.InitWithFeatures(
-        {features::kEarlyFullScreenVideoOptimization,
-         features::kDirectCompositionLetterboxVideoOptimization},
-        {});
+    feature_list_.InitAndEnableFeature(
+        features::kDirectCompositionLetterboxVideoOptimization);
   }
 
   void SetUp() override {
