717497dbc6cf1f6e1de5421f98b742f48f61daff
Nasser Al-shawwa <alshawwa@chromium.org>
2026-02-10 05:23:02 -0800

    Cleanup EnableSinglePageAppDataProtection flag
    Bug: N/A
    Change-Id: I3e2a6c765d3fdb8fa0eddee7c6c99deb759da333
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7558753
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Commit-Queue: Nasser Al-shawwa <alshawwa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1582437}

--- chrome/browser/enterprise/data_protection/data_protection_features.cc
+++ chrome/browser/enterprise/data_protection/data_protection_features.cc
@@ -9,9 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace enterprise_data_protection {
 
-BASE_FEATURE(kEnableSinglePageAppDataProtection,
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 BASE_FEATURE(kEnableForceDownloadToCloud, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kEnableForceDownloadToOneDrive, base::FEATURE_DISABLED_BY_DEFAULT);
