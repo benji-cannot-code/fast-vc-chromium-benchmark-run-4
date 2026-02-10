602ff2b718962e10f41d06c97bed9399ab77b7bd
David Risney <david.risney@microsoft.com>
2026-02-10 15:29:15 -0800

    SurfaceEmbed: webui_examples Content Shell Client Hooks
    Allow webui_examples to use SurfaceEmbed in addition to GuestContents.
    This requires:
     * Modifying the CSP to allow object-src since SurfaceEmbed uses the
       embed HTML element.
     * Creating a 'surfaceembed' custom HTML element that wraps an embed
       HTML element.
     * Picking between the 'surfaceembed' and 'webview' HTML elements
       depending on the Surface Embed feature flag.
    
    It still uses data-content-id like GuestContents to pick the content ID
    
    Bug: 481519345
    Change-Id: I3893062965be6fa3d9169137ab46e0b3a9347ef7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7547994
    Commit-Queue: Dave Risney <david.risney@microsoft.com>
    Reviewed-by: Sam Fortiner <samfort@microsoft.com>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1582857}

--- ui/webui/examples/renderer/render_frame_observer.cc
+++ ui/webui/examples/renderer/render_frame_observer.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/logging.h"
 #include "components/guest_contents/renderer/swap_render_frame.h"
+#include "components/surface_embed/buildflags/buildflags.h"
 #include "content/public/renderer/render_frame.h"
 #include "content/public/renderer/render_frame_observer.h"
 #include "content/public/renderer/render_thread.h"
@@ -22,6 +23,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "v8/include/v8-function.h"
 #include "v8/include/v8-isolate.h"
 #include "v8/include/v8-local-handle.h"
+
+#if BUILDFLAG(ENABLE_SURFACE_EMBED)
+#include "components/surface_embed/common/features.h"
+#endif  // BUILDFLAG(ENABLE_SURFACE_EMBED)
 
 namespace webui_examples {
 
@@ -44,7 +49,24 @@ void AllowCustomElementNameRegistration(v8::Local<v8::Function> callback) {
   v8::HandleScope handle_scope(isolate);
   v8::Local<v8::Context> context = isolate->GetCurrentContext();
   blink::WebCustomElement::EmbedderNamesAllowedScope embedder_names_scope;
-  callback->Call(context, context->Global(), 0, nullptr).ToLocalChecked();
+  v8::TryCatch try_catch(isolate);
+  v8::MaybeLocal<v8::Value> result =
+      callback->Call(context, context->Global(), 0, nullptr);
+
+  if (result.IsEmpty()) {
+    v8::String::Utf8Value exception(isolate, try_catch.Exception());
+    LOG(ERROR) << "AllowCustomElementNameRegistration failed:" << *exception;
+
+    v8::Local<v8::Value> stack_trace_as_local;
+    if (try_catch.StackTrace(context).ToLocal(&stack_trace_as_local) &&
+        stack_trace_as_local->IsString() &&
+        stack_trace_as_local.As<v8::String>()->Length() > 0) {
+      v8::String::Utf8Value stack_trace_as_string(isolate,
+                                                  stack_trace_as_local);
+      LOG(ERROR) << "AllowCustomElementNameRegistration failure stack trace: "
+                 << *stack_trace_as_string;
+    }
+  }
 }
 
 content::RenderFrame* GetRenderFrame(v8::Local<v8::Value> value) {
@@ -121,11 +143,19 @@ void RenderFrameObserver::ReadyToCommitNavigation(
       render_frame()->GetWebFrame()->MainWorldScriptContext();
   v8::Context::Scope context_scope(context);
 
+#if BUILDFLAG(ENABLE_SURFACE_EMBED)
+  const bool surface_embed_enabled =
+      base::FeatureList::IsEnabled(surface_embed::features::kSurfaceEmbed);
+#else
+  const bool surface_embed_enabled = false;
+#endif  // BUILDFLAG(ENABLE_SURFACE_EMBED)
+
   v8::Local<v8::ObjectTemplate> webshell_template =
       gin::ObjectTemplateBuilder(isolate)
           .SetMethod("allowWebviewElementRegistration",
                      &AllowCustomElementNameRegistration)
           .SetMethod("attachIframeGuest", &AttachIframeGuest)
+          .SetValue("surfaceEmbedEnabled", surface_embed_enabled)
           .Build();
 
   context->Global()
