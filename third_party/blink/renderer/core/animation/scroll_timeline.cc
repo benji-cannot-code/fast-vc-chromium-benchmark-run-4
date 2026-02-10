502c6c5ee9e0f1437063ee4606251331a3b6d510
Kevin Ellis <kevers@google.com>
2026-02-10 11:37:38 -0800

    [scroll-animations] Sanitize timeline.source
    Bug: 483088585
    Change-Id: Id6212792bfa98f018dbde59110205018cf7a20f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7563369
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1582694}

--- third_party/blink/renderer/core/animation/scroll_timeline.cc
+++ third_party/blink/renderer/core/animation/scroll_timeline.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/animation/scroll_timeline_util.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/element.h"
+#include "third_party/blink/renderer/core/dom/shadow_root.h"
 #include "third_party/blink/renderer/core/layout/layout_box.h"
 #include "third_party/blink/renderer/core/paint/paint_layer.h"
 #include "third_party/blink/renderer/core/paint/paint_layer_scrollable_area.h"
@@ -151,7 +152,23 @@ void ScrollTimeline::CalculateOffsets(PaintLayerScrollableArea* scrollable_area,
 }
 
 Element* ScrollTimeline::source() const {
-  return ComputeSource();
+  Element* source = ComputeSource();
+
+  if (!source) {
+    return nullptr;
+  }
+
+  ShadowRoot* containing_shadow_root = source->ContainingShadowRoot();
+  while (containing_shadow_root && containing_shadow_root->IsUserAgent()) {
+    source = &containing_shadow_root->host();
+    containing_shadow_root = source->ContainingShadowRoot();
+  }
+
+  if (PseudoElement* pseudo = DynamicTo<PseudoElement>(source)) {
+    source = &(pseudo->UltimateOriginatingElement());
+  }
+
+  return source;
 }
 
 Element* ScrollTimeline::ComputeSource() const {
