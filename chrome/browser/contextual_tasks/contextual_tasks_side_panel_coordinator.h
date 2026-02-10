a43ae4fc86472cfe0c257f3c8e5f68f692947ed5
Sophie Chang <sophiechang@chromium.org>
2026-02-10 11:23:23 -0800

    Record histogram and user action when user presses back button to go from contextual tasks panel to tab
    Bug: 479756918
    Change-Id: I64e6f987237ff407df022ec54de2e5ed3bb15499
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7556377
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1582684}

--- chrome/browser/contextual_tasks/contextual_tasks_side_panel_coordinator.h
+++ chrome/browser/contextual_tasks/contextual_tasks_side_panel_coordinator.h
@@ -120,6 +120,8 @@ class ContextualTasksSidePanelCoordinator
   bool IsSidePanelOpen();
 
   // content::WebContentsObserver:
+  void DidStartNavigation(
+      content::NavigationHandle* navigation_handle) override;
   void DidFinishNavigation(
       content::NavigationHandle* navigation_handle) override;
   void PrimaryPageChanged(content::Page& page) override;
