f5da17fed4c01c4831ef9cfbda384a148f23de65
Vlad Krot <vkrot@google.com>
2026-02-10 05:29:27 -0800

    Refactor link_capturing_navigation_throttle_browsertest.cc
    Remove parametrized logic for conditional testing of
    NavigationCapturingV1, because the feature was launched, so this
    condition is no longer necessary.
    
    Added web_app_navigation_capturing_browsertest_base.cc as a base class
    to share some logic - piece that enabled feature flag.
    
    Bug: 383094092
    Change-Id: Ia2020c8544f7a9d9959f2efec568ee75e8bd87a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7557174
    Commit-Queue: Vlad Krot <vkrot@google.com>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1582442}

--- chrome/browser/apps/link_capturing/link_capturing_navigation_throttle_browsertest.cc
+++ chrome/browser/apps/link_capturing/link_capturing_navigation_throttle_browsertest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/os_integration/os_integration_manager.h"
 #include "chrome/browser/web_applications/test/os_integration_test_override_impl.h"
 #include "chrome/browser/web_applications/test/web_app_test_observers.h"
+#include "chrome/browser/web_applications/web_app_navigation_capturing_browsertest_base.h"
 #include "chrome/browser/web_applications/web_app_tab_helper.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
@@ -26,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/test_navigation_observer.h"
-#include "net/test/embedded_test_server/embedded_test_server.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/common/switches.h"
@@ -35,36 +35,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace apps {
 namespace {
 
-enum class NavCaptureVersion { kV1, kV2 };
-
-std::string NavCaptureVersionToString(
-    const testing::TestParamInfo<NavCaptureVersion>& version) {
-  switch (version.param) {
-    case NavCaptureVersion::kV1:
-      return "V1";
-    case NavCaptureVersion::kV2:
-      return "V2";
-  }
-}
-
 // TODO(https://issues.chromium.org/329174385): Test more end-to-end the state
 // of browser tabs rather than relying on testing callbacks, this requires
 // resolving flakiness with awaiting web app launches.
 class LinkCapturingNavigationThrottleBrowserTest
-    : public InProcessBrowserTest,
-      public testing::WithParamInterface<NavCaptureVersion> {
+    : public web_app::WebAppNavigationCapturingBrowserTestBase {
  public:
-  LinkCapturingNavigationThrottleBrowserTest() {
-    feature_list_.InitWithFeaturesAndParameters(
-        /*enabled_features=*/{base::test::FeatureRefAndParams(
-            features::kPwaNavigationCapturing,
-            {{"link_capturing_state",
-              IsV1() ? "on_by_default" : "reimpl_default_on"}})},
-        /*disabled_features=*/{});
-  }
-
-  bool IsV1() { return GetParam() == NavCaptureVersion::kV1; }
-
   void SetUpOnMainThread() override {
 #if (BUILDFLAG(IS_LINUX) && defined(MEMORY_SANITIZER)) || BUILDFLAG(IS_MAC) || \
     (BUILDFLAG(IS_WIN) && defined(ADDRESS_SANITIZER))
@@ -72,12 +48,10 @@ class LinkCapturingNavigationThrottleBrowserTest
     // Linux MSan Tests / Mac Tests / Win ASan Tests.
     GTEST_SKIP();
 #else
-    InProcessBrowserTest::SetUpOnMainThread();
-
-    ASSERT_TRUE(embedded_test_server()->Start());
+    web_app::WebAppNavigationCapturingBrowserTestBase::SetUpOnMainThread();
 
     // Set up web app as link capturing target.
-    start_url_ = embedded_test_server()->GetURL("/web_apps/basic.html");
+    start_url_ = https_server()->GetURL("/web_apps/basic.html");
     app_id_ =
         web_app::InstallWebAppFromPageAndCloseAppBrowser(browser(), start_url_);
     apps::AppReadinessWaiter(browser()->profile(), app_id_).Await();
@@ -87,7 +61,7 @@ class LinkCapturingNavigationThrottleBrowserTest
     // Set up simple page browser tab with a "fill-page" CSS class available for
     // tests to use. This class makes elements fill the page making it easy to
     // click on.
-    simple_url_ = embedded_test_server()->GetURL("/simple.html");
+    simple_url_ = https_server()->GetURL("/simple.html");
     ASSERT_TRUE(content::NavigateToURL(GetBrowserTab(), simple_url_));
     ASSERT_TRUE(content::ExecJs(GetBrowserTab(), R"(
       const style = document.createElement('style');
@@ -130,7 +104,7 @@ class LinkCapturingNavigationThrottleBrowserTest
   web_app::OsIntegrationTestOverrideBlockingRegistration faked_os_integration_;
 };
 
-IN_PROC_BROWSER_TEST_P(LinkCapturingNavigationThrottleBrowserTest,
+IN_PROC_BROWSER_TEST_F(LinkCapturingNavigationThrottleBrowserTest,
                        TriggeredByAHrefClick) {
   // Insert an <a href> link to click on.
   std::string script = base::StringPrintf(
@@ -146,13 +120,11 @@ IN_PROC_BROWSER_TEST_P(LinkCapturingNavigationThrottleBrowserTest,
   content::WebContents* loaded_contents = ClickPageAndWaitForStartUrlLoad();
   ASSERT_TRUE(loaded_contents);
 
-  bool expected_open_in_pwa_window = IsV1();
-  EXPECT_EQ(expected_open_in_pwa_window,
-            web_app::WebAppTabHelper::FromWebContents(loaded_contents)
-                ->is_in_app_window());
+  EXPECT_FALSE(web_app::WebAppTabHelper::FromWebContents(loaded_contents)
+                   ->is_in_app_window());
 }
 
-IN_PROC_BROWSER_TEST_P(LinkCapturingNavigationThrottleBrowserTest,
+IN_PROC_BROWSER_TEST_F(LinkCapturingNavigationThrottleBrowserTest,
                        TriggeredByAHrefClickTargetBlank) {
   // Insert an <a href target="_blank"> link to click on.
   std::string script = base::StringPrintf(
@@ -173,7 +145,7 @@ IN_PROC_BROWSER_TEST_P(LinkCapturingNavigationThrottleBrowserTest,
                   ->is_in_app_window());
 }
 
-IN_PROC_BROWSER_TEST_P(LinkCapturingNavigationThrottleBrowserTest,
+IN_PROC_BROWSER_TEST_F(LinkCapturingNavigationThrottleBrowserTest,
                        TriggeredByAHrefClickServerRedirect) {
   // Insert an <a href> server redirect link to click on.
   std::string script = base::StringPrintf(
@@ -183,7 +155,7 @@ IN_PROC_BROWSER_TEST_P(LinkCapturingNavigationThrottleBrowserTest,
         link.href = '%s';
         document.body.append(link);
       )",
-      embedded_test_server()
+      https_server()
           ->GetURL("/server-redirect?" + start_url_.spec())
           .spec()
           .c_str());
@@ -191,14 +163,12 @@ IN_PROC_BROWSER_TEST_P(LinkCapturingNavigationThrottleBrowserTest,
 
   content::WebContents* loaded_contents = ClickPageAndWaitForStartUrlLoad();
 
-  bool expected_open_in_pwa_window = IsV1();
   ASSERT_TRUE(loaded_contents);
-  EXPECT_EQ(expected_open_in_pwa_window,
-            web_app::WebAppTabHelper::FromWebContents(loaded_contents)
-                ->is_in_app_window());
+  EXPECT_FALSE(web_app::WebAppTabHelper::FromWebContents(loaded_contents)
+                   ->is_in_app_window());
 }
 
-IN_PROC_BROWSER_TEST_P(LinkCapturingNavigationThrottleBrowserTest,
+IN_PROC_BROWSER_TEST_F(LinkCapturingNavigationThrottleBrowserTest,
                        TriggeredByAHrefClickTargetBlankServerRedirect) {
   // Insert an <a href target="_blank"> server redirect link to click on.
   std::string script = base::StringPrintf(
@@ -209,7 +179,7 @@ IN_PROC_BROWSER_TEST_P(LinkCapturingNavigationThrottleBrowserTest,
         link.target = '_blank';
         document.body.append(link);
       )",
-      embedded_test_server()
+      https_server()
           ->GetURL("/server-redirect?" + start_url_.spec())
           .spec()
           .c_str());
@@ -222,7 +192,7 @@ IN_PROC_BROWSER_TEST_P(LinkCapturingNavigationThrottleBrowserTest,
                   ->is_in_app_window());
 }
 
-IN_PROC_BROWSER_TEST_P(LinkCapturingNavigationThrottleBrowserTest,
+IN_PROC_BROWSER_TEST_F(LinkCapturingNavigationThrottleBrowserTest,
                        TriggeredByAHrefClickTargetBlankJavaScriptRedirect) {
   // Insert an <a href target="_blank"> JavaScript redirect link to click on.
   std::string script = base::StringPrintf(
@@ -239,13 +209,11 @@ IN_PROC_BROWSER_TEST_P(LinkCapturingNavigationThrottleBrowserTest,
   content::WebContents* loaded_contents = ClickPageAndWaitForStartUrlLoad();
   ASSERT_TRUE(loaded_contents);
 
-  bool expected_open_in_pwa_window = IsV1();
-  EXPECT_EQ(expected_open_in_pwa_window,
-            web_app::WebAppTabHelper::FromWebContents(loaded_contents)
-                ->is_in_app_window());
+  EXPECT_FALSE(web_app::WebAppTabHelper::FromWebContents(loaded_contents)
+                   ->is_in_app_window());
 }
 
-IN_PROC_BROWSER_TEST_P(LinkCapturingNavigationThrottleBrowserTest,
+IN_PROC_BROWSER_TEST_F(LinkCapturingNavigationThrottleBrowserTest,
                        TriggeredByButtonClickJavaScriptWindowOpen) {
   // Insert a window.open(url) button to click on.
   std::string script = base::StringPrintf(
@@ -263,13 +231,11 @@ IN_PROC_BROWSER_TEST_P(LinkCapturingNavigationThrottleBrowserTest,
   content::WebContents* loaded_contents = ClickPageAndWaitForStartUrlLoad();
   ASSERT_TRUE(loaded_contents);
 
-  bool expected_open_in_pwa_window = IsV1();
-  EXPECT_EQ(expected_open_in_pwa_window,
-            web_app::WebAppTabHelper::FromWebContents(loaded_contents)
-                ->is_in_app_window());
+  EXPECT_FALSE(web_app::WebAppTabHelper::FromWebContents(loaded_contents)
+                   ->is_in_app_window());
 }
 
-IN_PROC_BROWSER_TEST_P(
+IN_PROC_BROWSER_TEST_F(
     LinkCapturingNavigationThrottleBrowserTest,
     TriggeredByButtonClickJavaScriptWindowOpenAboutBlankLocationHrefUrl) {
   // Insert a window.open('about:blank').location.href = url button to click on.
@@ -288,13 +254,11 @@ IN_PROC_BROWSER_TEST_P(
   content::WebContents* loaded_contents = ClickPageAndWaitForStartUrlLoad();
   ASSERT_TRUE(loaded_contents);
 
-  bool expected_open_in_pwa_window = IsV1();
-  EXPECT_EQ(expected_open_in_pwa_window,
-            web_app::WebAppTabHelper::FromWebContents(loaded_contents)
-                ->is_in_app_window());
+  EXPECT_FALSE(web_app::WebAppTabHelper::FromWebContents(loaded_contents)
+                   ->is_in_app_window());
 }
 
-IN_PROC_BROWSER_TEST_P(LinkCapturingNavigationThrottleBrowserTest,
+IN_PROC_BROWSER_TEST_F(LinkCapturingNavigationThrottleBrowserTest,
                        TriggeredByButtonClickJavaScriptLocationHrefUrl) {
   // Insert a location.href = url button to click on.
   std::string script = base::StringPrintf(
@@ -312,14 +276,12 @@ IN_PROC_BROWSER_TEST_P(LinkCapturingNavigationThrottleBrowserTest,
   content::WebContents* loaded_contents = ClickPageAndWaitForStartUrlLoad();
   ASSERT_TRUE(loaded_contents);
 
-  bool expected_open_in_pwa_window = IsV1();
-  EXPECT_EQ(expected_open_in_pwa_window,
-            web_app::WebAppTabHelper::FromWebContents(loaded_contents)
-                ->is_in_app_window());
+  EXPECT_FALSE(web_app::WebAppTabHelper::FromWebContents(loaded_contents)
+                   ->is_in_app_window());
 }
 
 // TODO(crbug.com/405508964): Test is flaky.
-IN_PROC_BROWSER_TEST_P(LinkCapturingNavigationThrottleBrowserTest,
+IN_PROC_BROWSER_TEST_F(LinkCapturingNavigationThrottleBrowserTest,
                        DISABLED_TriggeredByNoClickJavaScriptWindowOpen) {
   ui_test_utils::UrlLoadObserver load_observer(start_url_);
   ASSERT_TRUE(content::ExecJs(
@@ -328,18 +290,10 @@ IN_PROC_BROWSER_TEST_P(LinkCapturingNavigationThrottleBrowserTest,
   load_observer.Wait();
   ASSERT_TRUE(load_observer.web_contents());
 
-  bool expected_open_in_pwa_window = IsV1();
-  EXPECT_EQ(
-      expected_open_in_pwa_window,
+  EXPECT_FALSE(
       web_app::WebAppTabHelper::FromWebContents(load_observer.web_contents())
           ->is_in_app_window());
 }
-
-INSTANTIATE_TEST_SUITE_P(,
-                         LinkCapturingNavigationThrottleBrowserTest,
-                         testing::Values(NavCaptureVersion::kV1,
-                                         NavCaptureVersion::kV2),
-                         NavCaptureVersionToString);
 
 }  // namespace
 }  // namespace apps
