bbecb1df5eead3d4721b3b5ea4743036720ef64c
Samuel Huang <huangs@chromium.org>
2026-02-10 13:30:52 -0800

    [Reader Mode] Readability: Remove <label for="id-of-removed-input"> elements
    This CL change (locally modded) Readability.js to remove orphaned
    explicit labels, i.e., <label> elements with "for" attribute containing
    id to a nonexistent element (presumably <input> that got deleted). This
    reduces noise on the distilled page, since <input> elements are
    typically removed.
    
    To reduce risk of unintended side effects, we're leaving implicit
    <label>s (i.e., relies on nesting without "for" attribute) alone.
    
    Bug: 483369387
    Change-Id: Id441296387e0d191f9124199a8dbee11a11b898c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7564049
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1582781}

--- third_party/readability/modded_src/Readability.js
+++ third_party/readability/modded_src/Readability.js
@@ -835,6 +835,7 @@ Readability.prototype = {
     this._cleanConditionally(articleContent, "table");
     this._cleanConditionally(articleContent, "ul");
     this._cleanConditionally(articleContent, "div");
+    this._cleanConditionally(articleContent, "label");
 
     // replace H1 with H2 as H1 should be only title that is displayed separately
     this._replaceNodeTags(
@@ -2709,6 +2710,14 @@ Readability.prototype = {
       // Handle <img> buried inside nested <div> layers in <figure>.
       if (tag === "div" && this._hasAncestorTag(node, "figure") && this._isSingleImage(node)) {
         return false;
+      }
+
+      // Handle <label for="id-of-removed-input">.
+      if (tag === 'label') {
+        const forId = node.getAttribute('for');
+        if (forId && !e.querySelector('#' + CSS.escape(forId))) {
+          return true;
+        }
       }
 
       var weight = this._getClassWeight(node);
