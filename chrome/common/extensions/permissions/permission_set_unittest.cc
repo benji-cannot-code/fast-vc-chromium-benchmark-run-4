1255a8b71d6c41fabdf646cd700c21b108fe3b18
Zonghan Xu <xzonghan@google.com>
2026-02-09 19:35:21 -0800

    [POR Extension API] Add New Extension API to set ProxyOverrideRules
    Add a new `proxyOverrideRulesPrivate` private extension API to set `ProxyOverrideRules` preference (and related preferences including scope and affiliation).
    
    This change only implements the basic logics of the API.
    
    API review document: https://docs.google.com/document/d/1yEI8NpWzPkAgKV3hmHw5WAZzJUztZ469389Pekxky08/edit?tab=t.0#heading=h.x9snb54sjlu9
    
    Bug: 481083576, 477634637
    Change-Id: Ibe85d6f2b52fceef21750dbc5b5a6fd4fddaf331
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7509271
    Commit-Queue: Zonghan Xu <xzonghan@chromium.org>
    Auto-Submit: Zonghan Xu <xzonghan@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Sébastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1582239}

--- chrome/common/extensions/permissions/permission_set_unittest.cc
+++ chrome/common/extensions/permissions/permission_set_unittest.cc
@@ -880,6 +880,7 @@ TEST(PermissionsTest, PermissionMessages) {
   skip.insert(APIPermissionID::kMediaPerceptionPrivate);
   skip.insert(APIPermissionID::kMetricsPrivate);
   skip.insert(APIPermissionID::kPdfViewerPrivate);
+  skip.insert(APIPermissionID::kProxyOverrideRulesPrivate);
   skip.insert(APIPermissionID::kImageWriterPrivate);
   skip.insert(APIPermissionID::kResourcesPrivate);
   skip.insert(APIPermissionID::kSafeBrowsingPrivate);
--- extensions/common/features/feature_flags.cc
+++ extensions/common/features/feature_flags.cc
@@ -29,6 +29,7 @@ const base::Feature* kFeatureFlags[] = {
     &extensions_features::kApiUserScriptsExecute,
     &extensions_features::kApiUserScriptsMultipleWorlds,
     &extensions_features::kApiOdfsConfigPrivate,
+    &extensions_features::kApiProxyOverrideRulesPrivate,
     &extensions_features::kExperimentalOmniboxLabs,
     &extensions_features::kExtensionIconVariants,
     &extensions_features::kTelemetryExtensionPendingApprovalApi,
