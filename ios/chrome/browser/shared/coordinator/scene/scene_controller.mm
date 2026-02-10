77f0ddf1997e18ec36fb58efbbe5e2d97ce4dc9e
Scott Yoder <scottyoder@google.com>
2026-02-10 09:28:58 -0800

    [ios] Migrate closePresentedViews to SceneCoordinator
    This change also cleans up some logic that wasn't previously migrated.
    
    Bug: 480984591
    Change-Id: I793a1be0f39e95253130b73c9120d2e29ce67e1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7562130
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1582596}

--- ios/chrome/browser/shared/coordinator/scene/scene_controller.mm
+++ ios/chrome/browser/shared/coordinator/scene/scene_controller.mm
@@ -1686,19 +1686,7 @@ void OnListFamilyMembersResponse(
 }
 
 - (void)closePresentedViewsAndOpenURL:(OpenNewTabCommand*)command {
-  DCHECK([command fromChrome]);
-  UrlLoadParams params = UrlLoadParams::InNewTab([command URL]);
-  params.web_params.transition_type = ui::PAGE_TRANSITION_TYPED;
-  ProceduralBlock completion = ^{
-    ApplicationModeForTabOpening mode =
-        [self isIncognitoForced] ? ApplicationModeForTabOpening::INCOGNITO
-                                 : ApplicationModeForTabOpening::NORMAL;
-    [self dismissModalsAndMaybeOpenSelectedTabInMode:mode
-                                   withUrlLoadParams:params
-                                      dismissOmnibox:YES
-                                          completion:nil];
-  };
-  [self closePresentedViews:YES completion:completion];
+  [self.mainCoordinator closePresentedViewsAndOpenURL:command];
 }
 
 - (void)closePresentedViews {
@@ -1974,20 +1962,12 @@ using UserFeedbackDataCallback =
 }
 
 - (void)showAccountMenuFromWebWithURL:(const GURL&)url {
-  if (![self isTabAvailableToPresentViewController]) {
-    return;
-  }
   [self.mainCoordinator showAccountMenuFromWebWithURL:url];
 }
 
 - (void)showWebSigninPromoFromViewController:
             (UIViewController*)baseViewController
                                          URL:(const GURL&)URL {
-  // Do not display the web sign-in promo if there is any UI on the screen.
-  if (baseViewController.presentedViewController ||
-      ![self isTabAvailableToPresentViewController]) {
-    return;
-  }
   [self.mainCoordinator showWebSigninPromoFromViewController:baseViewController
                                                          URL:URL];
 }
