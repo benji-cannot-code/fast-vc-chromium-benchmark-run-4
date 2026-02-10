550f3746c759f9ae66e5a02ac8cac9da7fba9a67
Peilin Wang <peilinwang@google.com>
2026-02-10 07:54:57 -0800

    [Animated Pb] Make bug fix not flag guarded
    This bug fix applies to the case where AnimatedProgressBar is disabled
    as well. Initially flag guarded because we thought it would negatively
    effect performance, we were right.
    
    This CL makes the bug fix not flag guarded anymore, so that we get a more fair comparison of performance metrics.
    
    before fix: https://drive.google.com/file/d/1ILpSyHxlqOBz-OHX-FnSkiDA49vCWxsx/view?usp=sharing&resourcekey=0-j2w9-rfkAMWOexY2DS3S3A
    after fix: https://drive.google.com/file/d/15zFkq4Kt88h7NxheMaM6sAZR1qkAIaf6/view?usp=sharing&resourcekey=0-xwX7BnoOpRmLUr9hd3MifQ
    
    Bug: 482073464
    Change-Id: I5cef7604823c65cacd8cf15a2ee47185566d8717
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7560042
    Commit-Queue: Peilin Wang <peilinwang@google.com>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1582529}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/TopToolbarOverlayMediator.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/TopToolbarOverlayMediator.java
@@ -299,9 +299,7 @@ public class TopToolbarOverlayMediator {
 
                     @Override
                     public void onCompositedLayersVisibilityChanged() {
-                        if (ChromeFeatureList.sAndroidAnimatedProgressBarInBrowser.isEnabled()) {
-                            updateProgress();
-                        }
+                        updateProgress();
                     }
                 };
         if (progressBar != null) {
