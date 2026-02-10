239c4e8c607dd0eb5be0f04151eec835727bddcc
Brijesh Giri <brijeshvgiri@gmail.com>
2026-02-09 17:05:07 -0800

    [DebugUITabStrip] Remove feature flag `kDebugUITabStrip`
    This CL removes the feature flag `kDebugUITabStrip` that previously
    gated the debug WebUI `chrome://tab-strip-internals`.
    
    The WebUI is no longer experimental and thus the feature flag and
    associated checks are removed in this CL.
    
    Changes:
    - Remove feature flag checks from `tab_strip_internals_ui.h/cc`
    - Remove the `kDebugUITabStrip` feature definition from
      `chrome/browser/ui/tabs/features.h/cc`
    
    Project doc:
    https://docs.google.com/document/d/1EKJicM4grBpp2nA4SOPzUvsKVLa_Ndj5L0HrIsGJNUI
    
    Bug: 427204855
    Change-Id: I305c93c0b563b437784aee9db0cef4c71df3f2c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7535399
    Reviewed-by: Atharv Maan <atharvmaan@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Brijesh Giri <brijeshvgiri@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1582152}

--- chrome/browser/ui/tabs/features.cc
+++ chrome/browser/ui/tabs/features.cc
@@ -10,10 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace tabs {
 
-// Enables the debug UI used to visualize the tab strip model.
-// chrome://tab-strip-internals
-BASE_FEATURE(kDebugUITabStrip, base::FEATURE_DISABLED_BY_DEFAULT);
-
 BASE_FEATURE(kTabGroupHome, base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kVerticalTabs, base::FEATURE_DISABLED_BY_DEFAULT);
--- chrome/browser/ui/webui/tab_strip_internals/tab_strip_internals_ui.cc
+++ chrome/browser/ui/webui/tab_strip_internals/tab_strip_internals_ui.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/tabs/features.h"
 #include "chrome/browser/ui/webui/tab_strip_internals/tab_strip_internals_handler.h"
 #include "chrome/grit/tab_strip_internals_resources.h"
 #include "chrome/grit/tab_strip_internals_resources_map.h"
@@ -33,12 +32,6 @@ TabStripInternalsUI::TabStripInternalsUI(content::WebUI* web_ui)
 TabStripInternalsUI::~TabStripInternalsUI() = default;
 
 WEB_UI_CONTROLLER_TYPE_IMPL(TabStripInternalsUI)
-
-bool TabStripInternalsUIConfig::IsWebUIEnabled(
-    content::BrowserContext* browser_context) {
-  return DefaultInternalWebUIConfig::IsWebUIEnabled(browser_context) &&
-         base::FeatureList::IsEnabled(tabs::kDebugUITabStrip);
-}
 
 void TabStripInternalsUI::BindInterface(
     mojo::PendingReceiver<tab_strip_internals::mojom::PageHandlerFactory>
