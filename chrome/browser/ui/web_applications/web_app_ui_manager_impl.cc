14bceae0ff48d30d5411410c50878807b018a589
Zhentao Lu <zhentao.lu@microsoft.com>
2026-02-09 19:44:30 -0800

    [bedrock] Migrate BrowserListObserver to BrowserCollectionObserver - part 8/n
    This migration is part of project bedrock to reduce the dependencies on
    Browser and BrowserList. See https://crbug.com/431671320 for more info.
    
    Bug: 431671320
    Change-Id: I80309f4c279838a7feef76f2f60c2407022fcf89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7550350
    Commit-Queue: Tom Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Zhentao Lu <zhentao.lu@microsoft.com>
    Reviewed-by: Qikai Zhong <qikaizhong@microsoft.com>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1582244}

--- chrome/browser/ui/web_applications/web_app_ui_manager_impl.cc
+++ chrome/browser/ui/web_applications/web_app_ui_manager_impl.cc
@@ -31,13 +31,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/browser/ui/browser_finder.h"
-#include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_navigator.h"
 #include "chrome/browser/ui/browser_navigator_params.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_features.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface_iterator.h"
+#include "chrome/browser/ui/browser_window/public/global_browser_collection.h"
 #include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/browser/ui/intent_picker_tab_helper.h"
 #include "chrome/browser/ui/user_education/browser_user_education_interface.h"
@@ -215,11 +215,12 @@ void WebAppUiManagerImpl::Start() {
       FROM_HERE, base::BindOnce(&WebAppUiManagerImpl::OnExtensionSystemReady,
                                 weak_ptr_factory_.GetWeakPtr()));
 
-  BrowserList::AddObserver(this);
+  browser_collection_observation_.Observe(
+      GlobalBrowserCollection::GetInstance());
 }
 
 void WebAppUiManagerImpl::Shutdown() {
-  BrowserList::RemoveObserver(this);
+  browser_collection_observation_.Reset();
   started_ = false;
 }
 
@@ -642,7 +643,7 @@ void WebAppUiManagerImpl::MaybeShowIPHPromoForAppsLaunchedViaLinkCapturing(
 #endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
 }
 
-void WebAppUiManagerImpl::OnBrowserAdded(Browser* browser) {
+void WebAppUiManagerImpl::OnBrowserCreated(BrowserWindowInterface* browser) {
   DCHECK(started_);
   if (!IsBrowserForInstalledApp(browser)) {
     return;
@@ -651,7 +652,8 @@ void WebAppUiManagerImpl::OnBrowserAdded(Browser* browser) {
   ++num_windows_for_apps_map_[GetAppIdForBrowser(browser)];
 
 #if BUILDFLAG(IS_CHROMEOS)
-  browser->tab_strip_model()->AddObserver(this);
+  // TODO(crbug.com/452120900): TabStripModel auto-unregistered by dtor
+  browser->GetTabStripModel()->AddObserver(this);
   browser_close_cancelled_subscriptions_.push_back(
       browser->RegisterBrowserCloseCancelled(
           base::BindRepeating(&WebAppUiManagerImpl::OnBrowserCloseCancelled,
@@ -659,7 +661,7 @@ void WebAppUiManagerImpl::OnBrowserAdded(Browser* browser) {
 #endif  // BUILDFLAG(IS_CHROMEOS)
 }
 
-void WebAppUiManagerImpl::OnBrowserRemoved(Browser* browser) {
+void WebAppUiManagerImpl::OnBrowserClosed(BrowserWindowInterface* browser) {
   DCHECK(started_);
   if (!IsBrowserForInstalledApp(browser)) {
     return;
@@ -670,10 +672,6 @@ void WebAppUiManagerImpl::OnBrowserRemoved(Browser* browser) {
   size_t& num_windows_for_app = num_windows_for_apps_map_[app_id];
   DCHECK_GT(num_windows_for_app, 0u);
   --num_windows_for_app;
-
-#if BUILDFLAG(IS_CHROMEOS)
-  browser->tab_strip_model()->RemoveObserver(this);
-#endif  // BUILDFLAG(IS_CHROMEOS)
 
   if (num_windows_for_app > 0) {
     return;
