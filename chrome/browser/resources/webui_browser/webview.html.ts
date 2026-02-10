c7efa8a7b6afa0688357ebbd131e07e17ff267e7
dpapad <dpapad@chromium.org>
2026-02-10 11:24:28 -0800

    Webium: Use proper Lit patterns in cr-webview.
     - Remove usage of this.$ helper for DOM nodes that are not guaranteed
       to exist in the DOM.
     - Remove unnecessary use of different #embed and #iframe IDs, use a
       common CSS class instead.
     - Put the HTML template in and conditional rendering logic an .html.ts
       file.
     - Declare all properties that affect the render() method's result as
       Lit reactive properties.
     - Remove unnecessary async/await from connectedCallback.
    
    Bug: 393144165
    Change-Id: I6a47c0350196f36e34857f1f7d1d890b95a7de5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7561132
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1582685}

---
+++ chrome/browser/resources/webui_browser/webview.html.ts
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {WebviewElement} from './webview.js';
+
+export function getHtml(this: WebviewElement) {
+  // clang-format off
+  return html`
+${this.enableSurfaceEmbed ? html`
+  <embed class="content" type="application/x-chromium-surface-embed"
+      data-content-id="${this.guestId}">
+  </embed>
+` : html`
+  <iframe class="content"></iframe>
+`}
+`;
+  // clang-format on
+}
