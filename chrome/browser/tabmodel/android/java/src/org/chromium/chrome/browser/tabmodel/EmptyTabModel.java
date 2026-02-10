f23a2276e6f1de14ce4624100de8d144ef1ac0b2
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2026-02-10 06:02:23 -0800

    Revert "[BrowserWindow] Before destroying AndroidBrowserWindow dissociate TabModel"
    This reverts commit 7a4cb00794237acbbd97c69cf89b84071a11ae4f.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5668070568755200
    
    Sample build with failed test: https://ci.chromium.org/b/8690269927529047665
    Affected test(s):
    [://chrome/android\:chrome_junit_tests!junit:org.chromium.chrome.browser.ui.browser_window:ChromeAndroidTaskImplUnitTest#onProfileDestroyed_removesProfileScopedFeature[30]](https://ci.chromium.org/ui/test/chromium/:%2F%2Fchrome%2Fandroid%5C:chrome_junit_tests%21junit:org.chromium.chrome.browser.ui.browser_window:ChromeAndroidTaskImplUnitTest%23onProfileDestroyed_removesProfileScopedFeature%5B30%5D?q=VHash%3Ad9e463e7c630438d)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5668070568755200&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F7559377&type=BUG
    
    Original change's description:
    > [BrowserWindow] Before destroying AndroidBrowserWindow dissociate TabModel
    >
    > TabModelInterface is UnownedUserData in C++ so it needs to be removed
    > before the window is destroyed.
    >
    > Bug: 475200706
    > Change-Id: I6c73e8970c967cee8fae68c9038f4146565daa78
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7559377
    > Reviewed-by: Linyu He <linyuh@google.com>
    > Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    > Commit-Queue: Linyu He <linyuh@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1582302}
    >
    
    Bug: 475200706
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I4d3052b15abcd1ed8359d77806358cef4131cf99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7562409
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Owners-Override: Zaina Al-Mashni <zalmashni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1582463}

--- chrome/browser/tabmodel/android/java/src/org/chromium/chrome/browser/tabmodel/EmptyTabModel.java
+++ chrome/browser/tabmodel/android/java/src/org/chromium/chrome/browser/tabmodel/EmptyTabModel.java
@@ -72,9 +72,6 @@ public class EmptyTabModel implements IncognitoTabModelInternal {
     public void associateWithBrowserWindow(long nativeAndroidBrowserWindow) {}
 
     @Override
-    public void dissociateWithBrowserWindow() {}
-
-    @Override
     public boolean isIncognito() {
         return mIsIncognito;
     }
