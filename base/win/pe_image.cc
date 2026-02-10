bfb04f1561ab7fec06a12e490d5f8d700a03f0ea
Jeffrey Yu <yuje@chromium.org>
2026-02-10 13:17:47 -0800

    Replace `std::set` with `absl::flat_hash_set`.
    The Chromium containers guide
    (https://chromium.googlesource.com/chromium/src/+/HEAD/base/containers/README.md)
    recommends using `absl::flat_hash_set` as a default set choice when
    stable iteration order and pointer stability isn't needed. It generally
    perform better and require less overhead than STL sets.
    
    Bug: 473916362
    Change-Id: Ic3aadbd1fd8c139b6663bf4618b5747a53a9865c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7551552
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Auto-Submit: Jeffrey Yu <yuje@chromium.org>
    Reviewed-by: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1582765}

--- base/win/pe_image.cc
+++ base/win/pe_image.cc
@@ -11,12 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <delayimp.h>
 #include <stddef.h>
 
-#include <set>
 #include <string>
 
 #include "base/compiler_specific.h"
 #include "base/no_destructor.h"
 #include "base/win/current_module.h"
+#include "third_party/abseil-cpp/absl/container/flat_hash_set.h"
 
 namespace base {
 namespace win {
@@ -541,7 +541,8 @@ bool PEImage::EnumDelayImportChunks(EnumDelayImportChunksFunction callback,
         // Use the module_name as retrieved from the IAT because this method
         // is case sensitive.
         if (module_ == CURRENT_MODULE() && !LDR_IS_RESOURCE(module_)) {
-          static base::NoDestructor<std::set<std::string>> loaded_dlls;
+          static base::NoDestructor<absl::flat_hash_set<std::string>>
+              loaded_dlls;
           // pair.second is true if this is a new element
           if (loaded_dlls.get()->emplace(module_name).second) {
             ::__HrLoadAllImportsForDll(module_name);
