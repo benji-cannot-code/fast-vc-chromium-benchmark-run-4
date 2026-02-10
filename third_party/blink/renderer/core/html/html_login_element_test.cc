f40eb0f35b8cc68d2540eb18506e8589faee7328
Sam Goto <goto@chromium.org>
2026-02-09 17:28:54 -0800

    [<login>] Introduce the skeleton of the <login> element
    This CL introduces just a skeleton of the <login> element and
    consolidates its children (currently, only <federation>) under the
    same LoginElement feature.
    
    Explainer:
    
    https://github.com/fedidcg/declarative-login
    
    Intent To Prototype:
    
    https://groups.google.com/a/chromium.org/g/blink-dev/c/uXmdPPN2NUI/m/DpbG_eKBCQAJ
    
    Implementation Design Doc:
    
    https://docs.google.com/document/d/1Zevn7l93U8Kj3hqr_AanifIRMDuJMy4mMyExGVY96v8/edit?resourcekey=0-Bwk0c3EyEKP3p0NtDFR1fw&tab=t.0
    
    Bug: 477699742
    Change-Id: I62110a1ac4e1eba0247ee3b5db2a0c3b08eb3dc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7558995
    Commit-Queue: Sam Goto <goto@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1582167}

---
+++ third_party/blink/renderer/core/html/html_login_element_test.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/core/html/html_login_element.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/renderer/core/dom/document.h"
+#include "third_party/blink/renderer/core/html_names.h"
+#include "third_party/blink/renderer/core/testing/page_test_base.h"
+#include "third_party/blink/renderer/platform/heap/garbage_collected.h"
+
+namespace blink {
+
+class HTMLLoginElementTest : public PageTestBase {};
+
+TEST_F(HTMLLoginElementTest, TagName) {
+  auto* element = MakeGarbageCollected<HTMLLoginElement>(GetDocument());
+  EXPECT_EQ(element->tagName(), "LOGIN");
+  EXPECT_EQ(element->localName(), "login");
+}
+
+}  // namespace blink
