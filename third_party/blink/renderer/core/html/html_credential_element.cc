9f80c3990e52bb0307c181224058688afb5f3d56
Sam Goto <goto@chromium.org>
2026-02-10 09:25:02 -0800

    [<login>] Rename <federation> element to <credential>
    Explainer:
    
    https://github.com/fedidcg/declarative-login
    
    Intent To Prototype:
    
    https://groups.google.com/a/chromium.org/g/blink-dev/c/uXmdPPN2NUI/m/DpbG_eKBCQAJ
    
    Bug: 477699742
    
    Change-Id: I2e4d37674679dc65227709f0d9cc2f792e1b3250
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7558441
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Sam Goto <goto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1582591}

--- third_party/blink/renderer/core/html/html_credential_element.cc
+++ third_party/blink/renderer/core/html/html_credential_element.cc
@@ -3,13 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "third_party/blink/renderer/core/html/html_federation_element.h"
+#include "third_party/blink/renderer/core/html/html_credential_element.h"
 
 #include "third_party/blink/renderer/core/html_names.h"
 
 namespace blink {
 
-HTMLFederationElement::HTMLFederationElement(Document& document)
-    : HTMLElement(html_names::kFederationTag, document) {}
+HTMLCredentialElement::HTMLCredentialElement(Document& document)
+    : HTMLElement(html_names::kCredentialTag, document) {}
 
 }  // namespace blink
