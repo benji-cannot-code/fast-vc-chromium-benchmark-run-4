b39f2a5d887d78f3a953d4f8afc2db43fa270886
Kunal Daftari <kunaldaftari@google.com>
2026-02-10 15:51:57 -0800

    Reformatting TabDragController so Definitions match Declaration Order
    UpdateDockInfo & HideFrame do not have definitions in the .cc file, so
    they were removed.
    
    Fixing order of the functions in the .cc file to match the .h
    
    Bug: 483422102
    Change-Id: Id49ca7ea519728f3703eda4c1084d38d02da0546
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7562941
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Commit-Queue: Kunal Daftari <kunaldaftari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1582867}

--- chrome/browser/ui/views/tabs/dragging/tab_drag_controller.h
+++ chrome/browser/ui/views/tabs/dragging/tab_drag_controller.h
@@ -313,8 +313,6 @@ class TabDragController : public views::WidgetObserver,
   void OnActiveStripWebContentsReplaced(content::WebContents* previous,
                                         content::WebContents* next);
 
-  void UpdateDockInfo(const gfx::Point& point_in_screen);
-
   // Saves focus in the window that the drag initiated from. Focus will be
   // restored appropriately if the drag ends within this same window.
   [[nodiscard]] Liveness SaveFocus();
@@ -455,10 +453,6 @@ class TabDragController : public views::WidgetObserver,
 
   // Maximizes the attached window.
   void MaximizeAttachedWindow();
-
-  // Hides the frame for the window that contains the TabDragContext
-  // the current drag session was initiated from.
-  void HideFrame();
 
   void BringWindowUnderPointToFront(const gfx::Point& point_in_screen);
 
