575473d26725916515cb40e7ec91926743f72455
Colin Blundell <blundell@chromium.org>
2026-02-10 00:54:16 -0800

    [Blink] Eliminate CanvasNon2DSnapshotProviderBitmap::surface_ ivar
    Part of the move to make DoExternalDrawAndSnapshot() stateless.
    
    Note: This has a behavioral impact due to the following property of
    SkSurface (credit to vasilyt@ for the investigation):
    
    SkSurface allocates memory on creation and when we call
    makeImageSnapshot() it takes a ref to that memory. Later, if we draw to
    that SkSurface again, if the snapshot is still alive it will allocate
    new memory and copy old one (i.e copy-on-write). If this allocation
    fails, draw silently fails too.
    
    In this case it essentially means, that if the most recent snapshot is
    alive, then we're improving the situation: We'd have exactly the same
    allocation, but potentially skipping some work on copy as the draws here
    always overwrite the image completely (skia is quite smart to discard
    contents, but it depends on things being wired properly on our side).
    
    However, if the most recent snapshot is not alive, we're adding a new
    allocation.
    
    For this particular use case we believe that this behavioral change is
    not significant.
    
    Note: This has an additional minor behavioral impact that effectively
    the instance will check for and detect OOM (failure to allocate the
    SkSurface) on every invocation of DoExternalDrawAndSnapshot(). In the
    current flow, the draw might silently fail on OOM (if allocating memory
    for a new snapshot) or might succeed (if the current snapshot isn't
    live).
    
    Bug: 352263194
    Change-Id: Id99d128064e7a507dded1e771cf533b40b72ed70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7546127
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1582360}

--- third_party/blink/renderer/platform/graphics/canvas_non2d_snapshot_provider_bitmap.h
+++ third_party/blink/renderer/platform/graphics/canvas_non2d_snapshot_provider_bitmap.h
@@ -60,7 +60,6 @@ class PLATFORM_EXPORT CanvasNon2DSnapshotProviderBitmap
   };
 
   std::optional<ImageProviderImpl> image_provider_impl_;
-  sk_sp<SkSurface> surface_;
 
   const CanvasSnapshotProvider::Info info_;
 
