06accb31ef677a866ef585623b99b213b74b8be8
Fiaz Muhammad <mfiaz@google.com>
2026-02-10 13:10:08 -0800

    Reland "Revert "[Persist] Add SharedPrefs for being caught up and wire""
    This reverts commit 5e0ad783058f0b360615cd6e63f7044431966c6c.
    
    Reason for revert: This should not have been reverted (original reverts landed out of order).
    
    Original change's description:
    > Revert "Revert "[Persist] Add SharedPrefs for being caught up and wire""
    >
    > This reverts commit 42d421b3faffa67fc65af3ef4b4d2f0bd1f2400c.
    >
    > Reason for revert:
    > LUCI Bisection has identified this change as the culprit of a build failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/compile-analysis/b/8690230488261211281
    >
    > Sample failed build: https://ci.chromium.org/b/8690230488261211281
    >
    > If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F7562836&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Fcompile-analysis%2Fb%2F8690230488261211281&type=BUG
    >
    > Original change's description:
    > > Revert "[Persist] Add SharedPrefs for being caught up and wire"
    > >
    > > This reverts commit c91ba92f84f71de3b80f14d73a901e87868dd6f6.
    > >
    > > Reason for revert: NPE in TabStateStore in some cases.
    > >
    > > Original change's description:
    > > > [Persist] Add SharedPrefs for being caught up and wire
    > > >
    > > > - The PersistentStoreMigrationManager should be aware of shadow
    > > > 	store-only events.
    > > > - Guard diff metrics to only record upon catching up.
    > > > - Integrate the ability for multiple migration managers to exist
    > > >         for the same window tag, which is required for when we
    > > >         reassign window IDs.
    > > >
    > > > Bug: 476447678
    > > > Change-Id: I5e4bbecb9cb225b3feff98df28865283836b3e22
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7511485
    > > > Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    > > > Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    > > > Reviewed-by: Sky Malice <skym@chromium.org>
    > > > Cr-Commit-Position: refs/heads/main@{#1581928}
    > >
    > > Bug: 476447678
    > > No-Presubmit: true
    > > No-Tree-Checks: true
    > > No-Try: true
    > > Change-Id: If20f3f3c6aa8ff968055c582ab8e12697fee6df1
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7562836
    > > Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    > > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > > Reviewed-by: Evan Luo <evanluo@google.com>
    > > Owners-Override: Evan Luo <evanluo@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1582628}
    > >
    >
    > Bug: 476447678
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I8d52d2a2a1dc69f95ee7308982723d75d5a53976
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7563772
    > Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    > Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    > Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1582655}
    
    Bug: 476447678
    Change-Id: I283718662f7565cfb42f011e203b692921816deb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7562596
    Reviewed-by: Evan Luo <evanluo@google.com>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Owners-Override: Evan Luo <evanluo@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1582759}

--- chrome/android/java/src/org/chromium/chrome/browser/app/tabmodel/TabPersistentStoreFactory.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/tabmodel/TabPersistentStoreFactory.java
@@ -44,23 +44,6 @@ public class TabPersistentStoreFactory {
                             ? StoreType.TAB_STATE_STORE
                             : StoreType.INVALID;
                 }
-
-                @Override
-                public void onShadowStoreCreated(@StoreType int storeType) {}
-
-                @Override
-                public void onShadowStoreCaughtUp() {}
-
-                @Override
-                public boolean isShadowStoreCaughtUp() {
-                    return true;
-                }
-
-                @Override
-                public void onShadowStoreRazed() {}
-
-                @Override
-                public void onAllShadowStoresRazed() {}
             };
 
     /**
@@ -106,7 +89,6 @@ public class TabPersistentStoreFactory {
             assert TabStateStorageFlagHelper.isTabStorageEnabled();
             assert TabStateStorageFlagHelper.isStorageAuthoritative();
             return new TabStateStore(
-                    migrationManager,
                     tabModelSelector,
                     windowTag,
                     tabCreatorManager,
@@ -226,14 +208,11 @@ public class TabPersistentStoreFactory {
             AccumulatingTabCreator regularShadowTabCreator,
             String orchestratorTag) {
         if (migrationManager == null) migrationManager = sDefaultManager;
-
-        @StoreType int shadowStoreType = migrationManager.getShadowStoreType();
-        if (shadowStoreType != StoreType.TAB_STATE_STORE) return null;
+        if (migrationManager.getShadowStoreType() != StoreType.TAB_STATE_STORE) return null;
         assert TabStateStorageFlagHelper.isTabStorageEnabled();
 
         TabPersistentStore shadowTabPersistentStore =
                 new TabStateStore(
-                        migrationManager,
                         selector,
                         windowTag,
                         shadowTabCreatorManager,
@@ -242,12 +221,10 @@ public class TabPersistentStoreFactory {
 
         new ShadowTabStoreValidator(
                 authoritativeStore,
-                migrationManager,
                 shadowTabPersistentStore,
                 selector.getModel(/* incognito= */ false),
                 regularShadowTabCreator,
                 orchestratorTag);
-        migrationManager.onShadowStoreCreated(shadowStoreType);
         return shadowTabPersistentStore;
     }
 }
