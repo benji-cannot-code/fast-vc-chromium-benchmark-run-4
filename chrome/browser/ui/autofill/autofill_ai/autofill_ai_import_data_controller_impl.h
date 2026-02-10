36b0eba1cc29fd29dcd9d023adede1a39d3e9c37
Jan Keitel <jkeitel@google.com>
2026-02-10 05:03:46 -0800

    Limit the cases in which the Autofill AI controller reshows
    This CL stops the Autofill AI bubble from reshowing (e.g. after tab
    switch) in two cases:
    - If there is an ongoing upload state with a throbber. In this case
      the callback has already run and reshowing a bubble that the user
      cannot meaningfully interact with offers little information.
    - If there is a local save notification. Again, such a bubble is FYI
      only and it seems too noisy to reshow.
    
    Bug: 478775174
    Change-Id: I43cb1a6b68170edd0edc3eb89880b161c2b1eef2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7558892
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1582431}

--- chrome/browser/ui/autofill/autofill_ai/autofill_ai_import_data_controller_impl.h
+++ chrome/browser/ui/autofill/autofill_ai/autofill_ai_import_data_controller_impl.h
@@ -66,6 +66,7 @@ class AutofillAiImportDataControllerImpl
   bool CloseOnAccept() const override;
 
   // BubbleControllerBase:
+  bool CanBeReshown() const override;
   void OnBubbleDiscarded() override;
   BubbleType GetBubbleType() const override;
   base::WeakPtr<BubbleControllerBase> GetBubbleControllerBaseWeakPtr() override;
