411ddc85616348a61ecdc2b41ace8d8396d0af68
Federica Germinario <fedegermi@google.com>
2026-02-10 08:12:27 -0800

    [ios] Remove redundant checks in start_surface_util
    There is no need to check for pendingUserActivity if we're already
    checking if startupHadExternalIntent is true.
    Indeed, every time we receive an intent startupHadExternalIntent is set
    to true (this is true for all intent types, also UserActivity).
    
    Note that startupHadExternalIntent is set to YES in SceneDelegate when
    the following OS call is received:
    - (void)scene:continueUserActivity:
    
    Bug: 477167760
    Change-Id: I6b685c70879deaf1dec99b67afe9118145626959
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7557300
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1582535}

--- ios/chrome/browser/start_surface/ui_bundled/start_surface_util.mm
+++ ios/chrome/browser/start_surface/ui_bundled/start_surface_util.mm
@@ -70,7 +70,7 @@ bool ShouldShowTabGroupInGridForSceneState(SceneState* scene_state) {
     return false;
   }
   if (scene_state.presentingModalOverlay ||
-      scene_state.startupHadExternalIntent || scene_state.pendingUserActivity) {
+      scene_state.startupHadExternalIntent) {
     return false;
   }
   return true;
@@ -84,7 +84,7 @@ bool ShouldShowStartSurfaceForSceneState(SceneState* scene_state) {
     return false;
   }
   if (scene_state.presentingModalOverlay ||
-      scene_state.startupHadExternalIntent || scene_state.pendingUserActivity ||
+      scene_state.startupHadExternalIntent ||
       scene_state.incognitoState.incognitoContentVisible) {
     return false;
   }
