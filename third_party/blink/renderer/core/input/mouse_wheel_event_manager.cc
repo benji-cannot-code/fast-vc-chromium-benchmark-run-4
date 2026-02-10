fa67cc299520cc1e703bb254ee846c3b97435864
Vladimir Levin <vmpstr@chromium.org>
2026-02-10 11:42:25 -0800

    Wheel: Reset frame latch when subframe target latch is reset.
    We need to reset the element latch when we have a preventDefault
    to support cases like crbug.com/41381205
    
    When we reset the latch, if that happens in a frame, we should also
    unlatch the frame so that next hit test can select a new target in
    case the frame moved out of the way
    
    R=flackr@chromium.org
    
    Bug: 481590680
    Change-Id: I488fa62cbde8717b7860b1ef4471753c5f8f1977
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7546621
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1582699}

--- third_party/blink/renderer/core/input/mouse_wheel_event_manager.cc
+++ third_party/blink/renderer/core/input/mouse_wheel_event_manager.cc
@@ -82,9 +82,7 @@ WebInputEventResult MouseWheelEventManager::HandleWheelEvent(
   LocalFrame* subframe =
       event_handling_util::SubframeForTargetNode(wheel_target_.Get());
   if (subframe) {
-    WebInputEventResult result =
-        subframe->GetEventHandler().HandleWheelEvent(event);
-    return result;
+    return subframe->GetEventHandler().HandleWheelEvent(event);
   }
 
   if (event.phase == WebMouseWheelEvent::kPhaseMayBegin) {
@@ -109,7 +107,7 @@ WebInputEventResult MouseWheelEventManager::HandleWheelEvent(
     if (dom_event_result != DispatchEventResult::kNotCanceled) {
       // Reset the target if the dom event is cancelled to make sure that new
       // targeting happens for the next wheel event.
-      wheel_target_ = nullptr;
+      UpdateWheelTarget(nullptr);
 
       bool is_vertical = dom_event->NativeEvent().event_action ==
                          WebMouseWheelEvent::EventAction::kScrollVertical;
@@ -179,6 +177,14 @@ void MouseWheelEventManager::UpdateWheelTarget(Node* wheel_target) {
   }
 
   wheel_target_ = wheel_target;
+
+  if (!wheel_target_) {
+    if (auto* parent_frame = DynamicTo<LocalFrame>(frame_->Parent())) {
+      parent_frame->GetEventHandler()
+          .GetMouseWheelEventManager()
+          .UpdateWheelTarget(nullptr);
+    }
+  }
 }
 
 void MouseWheelEventManager::FadeInChainedScrollbarsAndDeferFadeOut() {
