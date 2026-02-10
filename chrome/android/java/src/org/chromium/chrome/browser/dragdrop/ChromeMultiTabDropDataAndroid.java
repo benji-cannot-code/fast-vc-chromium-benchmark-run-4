e3ba95361a36ef864dec0b74fd16d4d0ffae752c
Shu Yang <shuyng@google.com>
2026-02-10 15:48:34 -0800

    Clean up unused CHROME_MIMETYPE
    Bug: 450783816
    Change-Id: I96def2b5c3d2f0a8320cb0925e6e57453c9ecf04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7564361
    Commit-Queue: Shu Yang <shuyng@google.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1582865}

--- chrome/android/java/src/org/chromium/chrome/browser/dragdrop/ChromeMultiTabDropDataAndroid.java
+++ chrome/android/java/src/org/chromium/chrome/browser/dragdrop/ChromeMultiTabDropDataAndroid.java
@@ -50,8 +50,7 @@ public class ChromeMultiTabDropDataAndroid extends ChromeDropDataAndroid {
         return new String[] {
             MimeTypeUtils.CHROME_MIMETYPE_MULTI_TAB,
             ClipDescription.MIMETYPE_TEXT_PLAIN,
-            ClipDescription.MIMETYPE_TEXT_INTENT,
-            MimeTypeUtils.CHROME_MIMETYPE_LINK
+            ClipDescription.MIMETYPE_TEXT_INTENT
         };
     }
 
