e111c67b8ea3732ca72e3101c3d4bef9e13de068
Martin Verde <thesalsa@google.com>
2026-02-09 18:32:44 -0800

    Roll regex-automata: 0.4.13 => 0.4.14 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * regex-automata: 0.4.13 => 0.4.14;
      https://docs.rs/crate/regex-automata/0.4.14
    
    Bug: None
    Change-Id: I11a517c901a0b37422f96f37aa448fe5360a2b10
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7546243
    Commit-Queue: Martin Pan-Verde <thesalsa@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1582216}

---
+++ third_party/rust/chromium_crates_io/vendor/regex-automata-v0_4/tests/nfa/thompson/pikevm/regression.rs
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Tests that we can call the PikeVM with more slots
+// than is actually in the compiled regex.
+#[test]
+fn too_many_slots_normal_pattern() {
+    use regex_automata::{
+        nfa::thompson::pikevm::PikeVM, util::primitives::NonMaxUsize,
+        Anchored, Input,
+    };
+
+    let expr = PikeVM::new(r"abc").unwrap();
+    let s = "abc";
+    let input = Input::new(s).span(0..s.len()).anchored(Anchored::Yes);
+
+    let mut cache = expr.create_cache();
+    let mut slots: Vec<Option<NonMaxUsize>> = vec![None; 4];
+    let pid = expr.search_slots(&mut cache, &input, &mut slots);
+    assert_eq!(pid, Some(regex_automata::PatternID::must(0)));
+    assert_eq!(slots[0], Some(NonMaxUsize::new(0).unwrap()));
+    assert_eq!(slots[1], Some(NonMaxUsize::new(3).unwrap()));
+}
