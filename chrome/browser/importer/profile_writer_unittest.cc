a41d5b655ab62c4fd7dc62715a60c5b9c67b7497
Ahmed Nasr <anasr@google.com>
2026-02-10 09:29:57 -0800

    code health: Replace base::RunLoop for base::test::TestFuture
    This change replaces the use of base::RunLoop to test async code with
    the special wrapper base::test::TestFuture to improve clarity
    
    Bug: b:266783937
    Change-Id: Iaeecaaad9c443e3752edaa9d9d5cfa5810869d0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7562412
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1582597}

--- chrome/browser/importer/profile_writer_unittest.cc
+++ chrome/browser/importer/profile_writer_unittest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/bind.h"
+#include "base/test/test_future.h"
 #include "base/time/time.h"
 #include "chrome/browser/bookmarks/bookmark_model_factory.h"
 #include "chrome/browser/history/history_service_factory.h"
@@ -145,15 +146,11 @@ class ProfileWriterTest : public testing::Test {
             profile, ServiceAccessType::EXPLICIT_ACCESS);
     history::QueryOptions options;
     base::CancelableTaskTracker history_task_tracker;
-    base::RunLoop loop;
-    history_service->QueryHistory(
-        std::u16string(), options,
-        base::BindLambdaForTesting([&](history::QueryResults results) {
-          history_count_ = results.size();
-          loop.Quit();
-        }),
-        &history_task_tracker);
-    loop.Run();
+    base::test::TestFuture<history::QueryResults> query_results_future;
+    history_service->QueryHistory(std::u16string(), options,
+                                  query_results_future.GetCallback(),
+                                  &history_task_tracker);
+    history_count_ = query_results_future.Get().size();
   }
 
   // Creates a TemplateURL from the provided data.
