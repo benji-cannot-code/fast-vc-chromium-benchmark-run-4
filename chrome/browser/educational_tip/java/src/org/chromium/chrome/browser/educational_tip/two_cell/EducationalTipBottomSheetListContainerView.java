dd65648ef182682bc1b5a0e6918f6802d8de8744
Joel Tan-Aristy <jtanaristy@google.com>
2026-02-09 16:08:44 -0800

    [Setup List] Implements showBottomSheet for Coordinator
    Wires EducationalTipBottomSheetCoordinator to two-cell layout coordinator such that the bottom sheet is displayed when "See more"
    is pressed.
    
    Adds ViewBinder for the bottom sheet and unit tests.
    
    http://screencast/cast/NTQyODY0Njk1NjIzNjgwMHw5M2FhYzU3Ni1hZA
    
    Bug: 479597724
    Change-Id: I58f474101f3b80937a30ab690ad645e74394cd73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7549271
    Reviewed-by: Xinyi Ji <xinyiji@chromium.org>
    Reviewed-by: Gazal Agarwal <agazal@google.com>
    Commit-Queue: Joel Tan-Aristy <jtanaristy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1582116}

--- chrome/browser/educational_tip/java/src/org/chromium/chrome/browser/educational_tip/two_cell/EducationalTipBottomSheetListContainerView.java
+++ chrome/browser/educational_tip/java/src/org/chromium/chrome/browser/educational_tip/two_cell/EducationalTipBottomSheetListContainerView.java
@@ -6,9 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.educational_tip.two_cell;
 
 import android.content.Context;
+import android.util.AttributeSet;
 import android.widget.LinearLayout;
 
 import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
 
 /**
  * The container view holding multiple {@link EducationalTipBottomSheetListItemView} in a bottom
@@ -18,8 +20,9 @@ import org.chromium.build.annotations.NullMarked;
 public class EducationalTipBottomSheetListContainerView extends LinearLayout {
     // TODO(crbug.com/479597724): Implement container view.
 
-    public EducationalTipBottomSheetListContainerView(Context context) {
-        super(context);
+    public EducationalTipBottomSheetListContainerView(
+            Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
     }
 
     /** Adds list items views to this container view. */
