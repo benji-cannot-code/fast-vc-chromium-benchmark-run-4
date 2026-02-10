55d0642dbd10046a69216cbf59aa859df09d55e6
Timofey Chudakov <tchudakov@google.com>
2026-02-10 03:01:31 -0800

    [AutofillAi - M4] Move ItemType from address/ to common/.
    This is needed to use ItemType in the autofill_ai/ dir as well.
    
    Bug: 476755159
    Change-Id: Id9b0bdf33a52b33386c2d478995bed6b9821ebb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7558400
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1582400}

--- chrome/browser/autofill/android/java/src/org/chromium/chrome/browser/autofill/editors/common/EditorComponentsProperties.java
+++ chrome/browser/autofill/android/java/src/org/chromium/chrome/browser/autofill/editors/common/EditorComponentsProperties.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.autofill.editors.common;
 
+import androidx.annotation.IntDef;
+
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.ui.modelutil.MVCListAdapter.ListItem;
 import org.chromium.ui.modelutil.PropertyKey;
@@ -12,6 +14,9 @@ import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.ui.modelutil.PropertyModel.ReadableBooleanPropertyKey;
 import org.chromium.ui.modelutil.PropertyModel.ReadableIntPropertyKey;
 import org.chromium.ui.modelutil.PropertyModel.ReadableObjectPropertyKey;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 /**
  * Common properties for the editor fields or other views displayed in an editor.
@@ -32,6 +37,30 @@ public class EditorComponentsProperties {
             super(type, model);
             this.isFullLine = isFullLine;
         }
+    }
+
+    /*
+     * Types of fields this editor model supports.
+     */
+    @IntDef({ItemType.DROPDOWN, ItemType.TEXT_INPUT, ItemType.NON_EDITABLE_TEXT, ItemType.NOTICE})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface ItemType {
+        // A fixed list of values, only 1 of which can be selected.
+        int DROPDOWN = 1;
+        // User can fill in a sequence of characters subject to input type restrictions.
+        int TEXT_INPUT = 2;
+        // A non-editable constant string.
+        int NON_EDITABLE_TEXT = 3;
+        // A notice string that is not editable.
+        int NOTICE = 4;
+    }
+
+    public static boolean isDropdownField(ListItem fieldItem) {
+        return fieldItem.type == ItemType.DROPDOWN;
+    }
+
+    public static boolean isEditable(ListItem fieldItem) {
+        return fieldItem.type == ItemType.DROPDOWN || fieldItem.type == ItemType.TEXT_INPUT;
     }
 
     /** Properties specific for the non-editable text fields. */
