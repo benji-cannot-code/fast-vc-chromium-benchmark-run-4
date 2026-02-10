d9286d3658cd76c2cf6b2bc3c7d213c06aeda915
Jérôme Lebel <jlebel@chromium.org>
2026-02-10 11:46:34 -0800

    Remove default AccessPoint value in AccountInfo
    Replace signin_metrics::AccessPoint access_point by:
    std::optional<signin_metrics::AccessPoint> access_point in AccountInfo.
    
    The goal is to remove AccessPoint::kUnknown.
    
    Bug: 481695037
    Change-Id: Ib54c46c3f71253213d95a1f85a986ba6138ab69d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7544913
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1582701}

--- components/signin/internal/identity_manager/account_info_util_unittest.cc
+++ components/signin/internal/identity_manager/account_info_util_unittest.cc
@@ -519,7 +519,8 @@ TEST(AccountInfoUtilTest, DeserializeAccountInfo_FormatStability) {
       account_info->GetAccountCapabilities().can_fetch_family_member_info(),
       Tribool::kUnknown);
 #if BUILDFLAG(ENABLE_DICE_SUPPORT)
-  EXPECT_EQ(account_info->GetLastAuthenticationAccessPoint(),
+  EXPECT_TRUE(account_info->GetLastAuthenticationAccessPoint().has_value());
+  EXPECT_EQ(account_info->GetLastAuthenticationAccessPoint().value(),
             signin_metrics::AccessPoint::kWebSignin);
 #endif
 }
@@ -540,7 +541,8 @@ TEST(AccountInfoUtilTest, DeserializeAccountInfo_Minimal) {
   EXPECT_FALSE(
       account_info->GetAccountCapabilities().AreAnyCapabilitiesKnown());
 #if BUILDFLAG(ENABLE_DICE_SUPPORT)
-  EXPECT_EQ(account_info->GetLastAuthenticationAccessPoint(),
+  EXPECT_EQ(account_info->GetLastAuthenticationAccessPoint().value_or(
+                signin_metrics::AccessPoint::kUnknown),
             signin_metrics::AccessPoint::kUnknown);
 #endif  // BUILDFLAG(ENABLE_DICE_SUPPORT)
 }
@@ -639,7 +641,8 @@ TEST(AccountInfoUtilTest, DeserializeAccountInfo_InvalidAccessPoint) {
   EXPECT_EQ(account_info->account_id,
             CoreAccountId::FromString("test_account_id"));
   // Access point should be empty.
-  EXPECT_EQ(account_info->GetLastAuthenticationAccessPoint(),
+  EXPECT_EQ(account_info->GetLastAuthenticationAccessPoint().value_or(
+                signin_metrics::AccessPoint::kUnknown),
             signin_metrics::AccessPoint::kUnknown);
 }
 #endif  // BUILDFLAG(ENABLE_DICE_SUPPORT)
