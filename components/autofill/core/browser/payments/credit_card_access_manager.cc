dd17eec5cbfd17f8f9e68bd7e6c37ad63e19c7d7
Fernando Ramirez <ferny@google.com>
2026-02-10 09:59:26 -0800

    Move fetched_payments_data_context_ to PaymentsFormDataImporter
    Design: go/formdataimporter-subdirectory-refactor-design
    Bug: 481379161
    Change-Id: I026627d40815c5a1ebb269032c5776d00e212f55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7553350
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Commit-Queue: Fernando Ramirez <ferny@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1582616}

--- components/autofill/core/browser/payments/credit_card_access_manager.cc
+++ components/autofill/core/browser/payments/credit_card_access_manager.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/data_manager/payments/payments_data_manager.h"
 #include "components/autofill/core/browser/data_model/payments/credit_card.h"
 #include "components/autofill/core/browser/form_import/form_data_importer.h"
+#include "components/autofill/core/browser/form_import/payments/payments_form_data_importer.h"
 #include "components/autofill/core/browser/foundations/autofill_client.h"
 #include "components/autofill/core/browser/foundations/autofill_driver.h"
 #include "components/autofill/core/browser/foundations/browser_autofill_manager.h"
@@ -335,8 +336,9 @@ void CreditCardAccessManager::FetchCreditCard(
     OnCreditCardFetchedCallback on_credit_card_fetched) {
   auto* form_data_importer = autofill_client().GetFormDataImporter();
   CHECK(form_data_importer);
-  form_data_importer->fetched_payments_data_context() =
-      FormDataImporter::FetchedPaymentsDataContext();
+  form_data_importer->GetPaymentsFormDataImporter()
+      .fetched_payments_data_context() =
+      payments::PaymentsFormDataImporter::FetchedPaymentsDataContext();
   // Reset the variable in FormDataImporter that denotes if non-interactive
   // authentication happened. This variable will be set to a value if a
   // payments autofill non-interactive flow successfully completes.
@@ -1801,7 +1803,8 @@ void CreditCardAccessManager::OnCreditCardFetched(
     bool card_was_fetched_from_cache) {
   auto* form_data_importer = autofill_client().GetFormDataImporter();
   CHECK(form_data_importer);
-  auto& context = form_data_importer->fetched_payments_data_context();
+  auto& context = form_data_importer->GetPaymentsFormDataImporter()
+                      .fetched_payments_data_context();
   context.fetched_card_instrument_id = card.instrument_id();
   context.card_was_fetched_from_cache = card_was_fetched_from_cache;
   if (on_credit_card_fetched_callback_) {
