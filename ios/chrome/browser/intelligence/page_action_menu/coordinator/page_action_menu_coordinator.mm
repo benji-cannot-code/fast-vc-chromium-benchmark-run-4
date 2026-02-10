6f62865ab4e7dcad7cae98f11566f0085ccaea3b
Nicolas MacBeth <nicolasmacbeth@google.com>
2026-02-09 18:05:50 -0800

    Remove NotFatalUntil CHECK() for nil BWGService in page action menu
    Instances of the crash or CHECK dump could not be found.
    
    Fixed: 474126721
    Change-Id: Ifcb5549b45216dbf18a26ef22d280d30054f18a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7559496
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Auto-Submit: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1582191}

--- ios/chrome/browser/intelligence/page_action_menu/coordinator/page_action_menu_coordinator.mm
+++ ios/chrome/browser/intelligence/page_action_menu/coordinator/page_action_menu_coordinator.mm
@@ -51,11 +51,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)start {
   raw_ptr<BwgService> BWGService =
       BwgServiceFactory::GetForProfile(self.profile);
-
-  // TODO(crbug.com/474126721): Understand if/when the PageActionMenu is started
-  // while BWGService is nullptr. Remove when investigation is complete.
-  CHECK(BWGService != nullptr, base::NotFatalUntil::M150);
-
   web::WebState* activeWebState =
       self.browser->GetWebStateList()->GetActiveWebState();
 
