9f98b985916626c0d57ecc2e6c7de1569e663d0f
Timofey Chudakov <tchudakov@google.com>
2026-02-10 02:05:22 -0800

    [AutofillAi - M4] Implement optional editor source notice background.
    Desired UI: screen/9nbqYthfYdQGvP3
    Example with address editor: screen/8c92rJ3GtDeYE3h
    
    Bug: 476755159
    Change-Id: If525d1de01ad7fc4bbbbbb851cafd7ccf30f0a10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7551835
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1582385}

--- chrome/browser/autofill/android/java/src/org/chromium/chrome/browser/autofill/editors/address/AddressEditorMediator.java
+++ chrome/browser/autofill/android/java/src/org/chromium/chrome/browser/autofill/editors/address/AddressEditorMediator.java
@@ -35,6 +35,7 @@ import static org.chromium.chrome.browser.autofill.editors.common.EditorComponen
 import static org.chromium.chrome.browser.autofill.editors.common.EditorComponentsProperties.NoticeProperties.IMPORTANT_FOR_ACCESSIBILITY;
 import static org.chromium.chrome.browser.autofill.editors.common.EditorComponentsProperties.NoticeProperties.NOTICE_ALL_KEYS;
 import static org.chromium.chrome.browser.autofill.editors.common.EditorComponentsProperties.NoticeProperties.NOTICE_TEXT;
+import static org.chromium.chrome.browser.autofill.editors.common.EditorComponentsProperties.NoticeProperties.SHOW_BACKGROUND;
 import static org.chromium.chrome.browser.autofill.editors.common.dropdown_field.DropdownFieldProperties.DROPDOWN_ALL_KEYS;
 import static org.chromium.chrome.browser.autofill.editors.common.dropdown_field.DropdownFieldProperties.DROPDOWN_CALLBACK;
 import static org.chromium.chrome.browser.autofill.editors.common.dropdown_field.DropdownFieldProperties.DROPDOWN_KEY_VALUE_LIST;
@@ -358,6 +359,7 @@ class AddressEditorMediator {
                                                 NOTICE_TEXT,
                                                 mContext.getString(
                                                         R.string.payments_required_field_message))
+                                        .with(SHOW_BACKGROUND, false)
                                         // Required fields are indicated by an asterisk (*) and
                                         // announced separately by screen readers. Don't announce
                                         // the message itself.
@@ -428,6 +430,7 @@ class AddressEditorMediator {
                             NOTICE,
                             new PropertyModel.Builder(NOTICE_ALL_KEYS)
                                     .with(NOTICE_TEXT, recordTypeNoticeText)
+                                    .with(SHOW_BACKGROUND, false)
                                     .with(IMPORTANT_FOR_ACCESSIBILITY, true)
                                     .build(),
                             /* isFullLine= */ true));
