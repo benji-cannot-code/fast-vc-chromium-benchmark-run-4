5ab0de4d03ea7f3d7ebee11367b1a9b067cf6ee2
Calder Kitagawa <ckitagawa@chromium.org>
2026-02-10 09:03:53 -0800

    Update DistillablePageUtils to not use ScopedJavaGlobalRef
    One of these is created per-WebContents on Android. This leads to faster
    exhaustion of the global reference table when there are many
    WebContents.
    
    Address this by using a JavaObjectWeakGlobalRef instead since the Java
    object is a TabWebContentsUserData.
    
    Bug: 482007198
    Change-Id: Ibbd34c1fe1e9f637c78cc376c33678224d54a877
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7558794
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1582569}

--- components/dom_distiller/content/browser/distillable_page_utils_android.cc
+++ components/dom_distiller/content/browser/distillable_page_utils_android.cc
@@ -3,22 +3,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "components/dom_distiller/content/browser/distillable_page_utils.h"
+
+#include "base/android/jni_android.h"
+#include "base/android/jni_weak_ref.h"
 #include "base/functional/bind.h"
 #include "base/location.h"
 #include "base/task/single_thread_task_runner.h"
-#include "components/dom_distiller/content/browser/distillable_page_utils.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_user_data.h"
+#include "url/android/gurl_android.h"
 
 // Must come after all headers that specialize FromJniType() / ToJniType().
 #include "components/dom_distiller/content/browser/android/jni_headers/DistillablePageUtils_jni.h"
-#include "url/android/gurl_android.h"
 
 using base::android::JavaRef;
-using base::android::ScopedJavaGlobalRef;
 
-namespace dom_distiller {
-namespace android {
+namespace dom_distiller::android {
 
 namespace {
 
@@ -32,14 +33,17 @@ class JniDistillabilityObserverWrapper
       const JniDistillabilityObserverWrapper&) = delete;
 
   void SetCallback(JNIEnv* env, const JavaRef<jobject>& callback) {
-    callback_ = ScopedJavaGlobalRef<jobject>(env, callback);
+    callback_ = JavaObjectWeakGlobalRef(env, callback);
   }
 
   void OnResult(const DistillabilityResult& result) override {
     JNIEnv* env = base::android::AttachCurrentThread();
+    auto callback = callback_.get(env);
+    if (callback.is_null()) {
+      return;
+    }
     Java_DistillablePageUtils_callOnIsPageDistillableUpdate(
-        base::android::AttachCurrentThread(), callback_,
-        url::GURLAndroid::FromNativeGURL(env, result.url),
+        base::android::AttachCurrentThread(), callback, result.url,
         result.is_distillable, result.is_last, result.is_long_article,
         result.is_mobile_friendly);
   }
@@ -51,21 +55,24 @@ class JniDistillabilityObserverWrapper
   friend class content::WebContentsUserData<JniDistillabilityObserverWrapper>;
   WEB_CONTENTS_USER_DATA_KEY_DECL();
 
-  ScopedJavaGlobalRef<jobject> callback_;
+  // A weak global reference does not use an entry in the finite global ref
+  // table. Since the delegate will be owned by another object in Java this is
+  // safe.
+  JavaObjectWeakGlobalRef callback_;
 };
 
 }  // namespace
 
 static void JNI_DistillablePageUtils_SetDelegate(
     JNIEnv* env,
-    const JavaRef<jobject>& webContents,
+    content::WebContents* web_contents,
     const JavaRef<jobject>& callback) {
-  content::WebContents* web_contents(
-      content::WebContents::FromJavaWebContents(webContents));
   if (!web_contents) {
     return;
   }
 
+  // If for some reason the WebContents is reused, this will also reuse the same
+  // instance of the observer, but with a possibly different callback.
   JniDistillabilityObserverWrapper::CreateForWebContents(web_contents);
   auto* observer =
       JniDistillabilityObserverWrapper::FromWebContents(web_contents);
@@ -77,7 +84,6 @@ static void JNI_DistillablePageUtils_SetDelegate(
 
 WEB_CONTENTS_USER_DATA_KEY_IMPL(JniDistillabilityObserverWrapper);
 
-}  // namespace android
-}  // namespace dom_distiller
+}  // namespace dom_distiller::android
 
 DEFINE_JNI(DistillablePageUtils)
