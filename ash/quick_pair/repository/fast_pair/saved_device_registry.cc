d5a98958981d7153294cb16b057f07bffaa26347
Jeffrey Yu <yuje@chromium.org>
2026-02-09 16:46:58 -0800

    Replace `std::set` with `absl::flat_hash_set`.
    The Chromium containers guide
    (https://chromium.googlesource.com/chromium/src/+/HEAD/base/containers/README.md)
    recommends using `absl::flat_hash_set` as a default set choice when
    stable iteration order and pointer stability isn't needed. It generally
    perform better and require less overhead than STL sets.
    
    Bug: 473916362
    Change-Id: Ic254a79ae19ce1d48535b923d92a0c6cfa307b6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7546897
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Brando Socarras <brandosocarras@google.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Auto-Submit: Jeffrey Yu <yuje@chromium.org>
    Commit-Queue: Brando Socarras <brandosocarras@google.com>
    Cr-Commit-Position: refs/heads/main@{#1582132}

--- ash/quick_pair/repository/fast_pair/saved_device_registry.cc
+++ ash/quick_pair/repository/fast_pair/saved_device_registry.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/scoped_user_pref_update.h"
 #include "device/bluetooth//bluetooth_adapter.h"
 #include "device/bluetooth/bluetooth_device.h"
+#include "third_party/abseil-cpp/absl/container/flat_hash_set.h"
 
 namespace {
 
@@ -164,7 +165,7 @@ void SavedDeviceRegistry::RemoveDevicesIfRemovedFromDifferentUser(
 
   // Set of currently paired devices, stored by Bluetooth address, used to
   // cross reference the registry for any devices that need to be removed.
-  std::set<std::string> paired_devices;
+  absl::flat_hash_set<std::string> paired_devices;
   for (device::BluetoothDevice* device : adapter_->GetDevices()) {
     if (device->IsPaired()) {
       paired_devices.insert(device->GetAddress());
