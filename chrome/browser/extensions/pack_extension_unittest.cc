ed0d3aa600b7e7e33c77580daa30978e02b07874
James Cook <jamescook@chromium.org>
2026-02-09 19:28:02 -0800

    extensions: Add PackExtensionTest to desktop Android
    It passes without modifications.
    
    Bug: 469417243
    Change-Id: I99807cf979b9a8abda5e78fa0e90186f95432d22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7561050
    Reviewed-by: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1582235}

--- chrome/browser/extensions/pack_extension_unittest.cc
+++ chrome/browser/extensions/pack_extension_unittest.cc
@@ -13,7 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
 #include "content/public/test/browser_task_environment.h"
+#include "extensions/buildflags/buildflags.h"
 #include "testing/gtest/include/gtest/gtest.h"
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace extensions {
 
@@ -59,13 +62,18 @@ TEST_F(PackExtensionTest, ExtensionWithManagedStorage) {
                                     .AppendASCII("managed_storage")));
 }
 
+#if !BUILDFLAG(IS_ANDROID)
+// Android does not support packaged apps.
 TEST_F(PackExtensionTest, PackagedApp) {
   ASSERT_TRUE(TestPackExtension(test_data_dir_.AppendASCII("packaged_app")));
 }
+#endif  // BUILDFLAG(IS_ANDROID)
 
+#if BUILDFLAG(ENABLE_PLATFORM_APPS)
 TEST_F(PackExtensionTest, PlatformApp) {
   ASSERT_TRUE(TestPackExtension(test_data_dir_.AppendASCII("platform_apps")
                                               .AppendASCII("minimal")));
 }
+#endif  // BUILDFLAG(ENABLE_PLATFORM_APPS)
 
 }  // namespace extensions
