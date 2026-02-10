6cebf15b3731319a33bef4188318d1db07561816
EmiliaPaz <emiliapaz@chromium.org>
2026-02-10 00:29:09 -0800

    [Cleanup] Migrate NoBestEffortTasksTest test to use MV3
    Migrate extension used in
    NoBestEffortTasksTest.LoadExtensionAndSendMessages to use MV3 -- since
    it's fully rolled out and this test doesn't depend on MV2
    functionality. With this, we can remove the usage of
    ScopedTestMV2Enabler.
    
    Bug: 40804030
    Change-Id: If0a6bcd0382bc8234c99b54ded89c6e7a0629dc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7560950
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Auto-Submit: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1582348}

--- chrome/browser/no_best_effort_tasks_browsertest.cc
+++ chrome/browser/no_best_effort_tasks_browsertest.cc
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/url_constants.h"
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
-#include "chrome/browser/extensions/scoped_test_mv2_enabler.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/browser/extension_system.h"
 #include "extensions/browser/test_extension_registry_observer.h"
@@ -175,9 +174,6 @@ IN_PROC_BROWSER_TEST_F(NoBestEffortTasksTest, DISABLED_LoadAndPaintFileScheme) {
 // http://crbug.com/41436919 was resolved.
 #if BUILDFLAG(ENABLE_EXTENSIONS)
 IN_PROC_BROWSER_TEST_F(NoBestEffortTasksTest, LoadExtensionAndSendMessages) {
-  // TODO(https://crbug.com/40804030): Remove this when updated to use MV3.
-  extensions::ScopedTestMV2Enabler mv2_enabler;
-
   ASSERT_TRUE(embedded_test_server()->Start());
 
   // Load the extension, waiting until the ExtensionRegistry reports that its
