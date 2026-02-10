579d2c40298e7c1be59db26c7bda3235f700e33a
Brijesh Giri <brijeshvgiri@gmail.com>
2026-02-09 16:53:42 -0800

    [DebugUITabStrip] Add ViewModel backend to select-expand TabStrip nodes
    This CL introduces Phase 3 of the 'tab-strip-internals' page, a debug
    WebUI intended to visualize the live state of the TabStrip model.
    
    Changes:
    - Updated `tab_strip_internals_viewmodel.ts` to add backend capability
      to select and expand nodes on the TabStrip tree
    - Updated `tab_strip_internals_view.ts` to consume new method to save
      navePaneWidth state
    
    Project doc:
    https://docs.google.com/document/d/1EKJicM4grBpp2nA4SOPzUvsKVLa_Ndj5L0HrIsGJNUI
    
    Bug: 427204855
    Change-Id: Id3287b15f3e5befb52fddd35ba40dd6264746273
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7511599
    Reviewed-by: Darryl James <dljames@chromium.org>
    Reviewed-by: Atharv Maan <atharvmaan@chromium.org>
    Commit-Queue: Brijesh Giri <brijeshvgiri@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1582143}

--- chrome/browser/resources/tab_strip_internals/tab_strip_internals_view.ts
+++ chrome/browser/resources/tab_strip_internals/tab_strip_internals_view.ts
@@ -142,7 +142,7 @@ export class TabStripInternalsView implements ViewModelObserver {
       }
       window.removeEventListener('mousemove', move);
       window.removeEventListener('mouseup', up);
-      this.viewModel_.saveState();
+      this.viewModel_.saveNavPaneWidthState();
     };
 
     window.addEventListener('mousemove', move);
@@ -162,7 +162,7 @@ export class TabStripInternalsView implements ViewModelObserver {
       default:
         return;
     }
-    this.viewModel_.saveState();
+    this.viewModel_.saveNavPaneWidthState();
     e.preventDefault();
   }
 }
