6d8096aa93d73e73925f82cb1947abcf11dce652
Steve Kobes <skobes@chromium.org>
2026-02-10 08:39:17 -0800

    Add some trace events to Wayland pointer code for diagnostics.
    Bug: 40393159
    Change-Id: I58c53746a84462307b49c8fdfbd7b5bd207a29a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7560052
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1582554}

--- ui/ozone/platform/wayland/host/wayland_pointer.cc
+++ ui/ozone/platform/wayland/host/wayland_pointer.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/notimplemented.h"
+#include "base/trace_event/trace_event.h"
 #include "base/version.h"
 #include "ui/events/base_event_utils.h"
 #include "ui/events/event.h"
@@ -82,6 +83,8 @@ void WaylandPointer::OnEnter(void* data,
   self->connection_->serial_tracker().UpdateSerial(wl::SerialType::kMouseEnter,
                                                    serial);
   WaylandWindow* window = wl::RootWindowFromWlSurface(surface);
+  TRACE_EVENT_INSTANT("wayland.debug", "WaylandPointer::OnEnter", "window",
+                      window ? window->GetBoundsInDIP().ToString() : "null");
   if (!window) {
     return;
   }
@@ -101,6 +104,10 @@ void WaylandPointer::OnLeave(void* data,
   const auto timestamp = EventTimeForNow();
   auto* self = static_cast<WaylandPointer*>(data);
 
+  WaylandWindow* window = wl::RootWindowFromWlSurface(surface);
+  TRACE_EVENT_INSTANT("wayland.debug", "WaylandPointer::OnLeave", "window",
+                      window ? window->GetBoundsInDIP().ToString() : "null");
+
   self->connection_->serial_tracker().ResetSerial(wl::SerialType::kMouseEnter);
   self->delegate_->OnPointerFocusChanged(nullptr,
                                          self->delegate_->GetPointerLocation(),
@@ -114,6 +121,8 @@ void WaylandPointer::OnMotion(void* data,
                               wl_fixed_t surface_x,
                               wl_fixed_t surface_y) {
   auto* self = static_cast<WaylandPointer*>(data);
+  TRACE_EVENT_INSTANT("wayland.debug", "WaylandPointer::OnMotion", "x",
+                      surface_x, "y", surface_y);
 
   self->delegate_->OnPointerMotionEvent(
       gfx::PointF(wl_fixed_to_double(surface_x), wl_fixed_to_double(surface_y)),
@@ -128,6 +137,8 @@ void WaylandPointer::OnButton(void* data,
                               uint32_t time,
                               uint32_t button,
                               uint32_t state) {
+  TRACE_EVENT_INSTANT("wayland.debug", "WaylandPointer::OnButton", "button",
+                      button, "state", state);
   auto* self = static_cast<WaylandPointer*>(data);
   int changed_button;
   switch (button) {
