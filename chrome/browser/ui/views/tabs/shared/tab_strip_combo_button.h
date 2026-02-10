f071496aa13496c58d5376e4242a3f20eebd3d9f
Emily Shack <emshack@chromium.org>
2026-02-09 19:26:18 -0800

    [ComboButton] Refactor ComboButton to build children
    In light of recent product decisions making the vertical and horizontal
    tab strip combo buttons always identical, this refactors the combo
    button API to build its own child buttons rather than them being passed
    in by the parent. This way, we can avoid duplicate code when adding a
    combo button to the horizontal tab strip.
    
    Bug: 481393131
    Change-Id: Ic73298641476e9470375036383624da39c31ffc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7552333
    Reviewed-by: Kunal Daftari <kunaldaftari@google.com>
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1582234}

--- chrome/browser/ui/views/tabs/shared/tab_strip_combo_button.h
+++ chrome/browser/ui/views/tabs/shared/tab_strip_combo_button.h
@@ -11,6 +11,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/layout/layout_types.h"
 #include "ui/views/view.h"
 
+namespace tab_groups {
+class STGEverythingMenu;
+}  // namespace tab_groups
+
+namespace views {
+class ActionViewController;
+class MenuButtonController;
+}  // namespace views
+
+class BrowserWindowInterface;
 class TabStripFlatEdgeButton;
 
 // A container for two TabStripFlatEdgeButtons that manages their flat edges
@@ -18,8 +28,7 @@ class TabStripFlatEdgeButton;
 class TabStripComboButton : public views::View {
   METADATA_HEADER(TabStripComboButton, views::View)
  public:
-  TabStripComboButton(std::unique_ptr<TabStripFlatEdgeButton> start_button,
-                      std::unique_ptr<TabStripFlatEdgeButton> end_button);
+  explicit TabStripComboButton(BrowserWindowInterface* browser);
   TabStripComboButton(const TabStripComboButton&) = delete;
   TabStripComboButton& operator=(const TabStripComboButton&) = delete;
   ~TabStripComboButton() override;
@@ -34,11 +43,22 @@ class TabStripComboButton : public views::View {
   void ChildVisibilityChanged(views::View* child) override;
 
  private:
+  void ShowEverythingMenu();
+
+  std::unique_ptr<TabStripFlatEdgeButton> CreateFlatEdgeButtonFor(
+      actions::ActionId action_id,
+      ui::ElementIdentifier element_id);
+
   void UpdateStyles();
 
+  const raw_ptr<BrowserWindowInterface> browser_;
   raw_ptr<TabStripFlatEdgeButton> start_button_ = nullptr;
   raw_ptr<TabStripFlatEdgeButton> end_button_ = nullptr;
   views::LayoutOrientation orientation_ = views::LayoutOrientation::kHorizontal;
+
+  std::unique_ptr<views::ActionViewController> action_view_controller_;
+  std::unique_ptr<tab_groups::STGEverythingMenu> everything_menu_;
+  raw_ptr<views::MenuButtonController> everything_menu_controller_ = nullptr;
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_TABS_SHARED_TAB_STRIP_COMBO_BUTTON_H_
