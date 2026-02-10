cb0144e03c1a7f1b0f8f9732c2c2316a3ea28c10
Zaina Al-Mashni <zalmashni@google.com>
2026-02-10 07:46:32 -0800

    Revert "Passwords: Add PasswordCombinedSelectorView prototype"
    This reverts commit 2fe69a25fc3a697e2aff631fe1552b096f9137ef.
    
    Reason for revert: Introduced failing tests
    
    Sample build with failed test:
    https://ci.chromium.org/ui/b/8690244287555885713
    
    Original change's description:
    > Passwords: Add PasswordCombinedSelectorView prototype
    >
    > This CL introduces PasswordCombinedSelectorView, a view for choosing
    > passwords for a navigator.credentials.get() API call. It follows the
    > FLUX UI.
    >
    > This implementation is currently a prototype and partially clones
    > WebAuthn's CombinedSelectorView. It is intended to be merged or
    > refactored once the FLUX UI design is finalized.
    >
    > Mocks: https://docs.google.com/presentation/d/151kaZlZzkjnQsU8I5__exJOY4xwusf5Hpp87gD-1kes/edit?resourcekey=0-exbFnuXvj9ntBU8xpDhsGQ&slide=id.g3409e58cc55_0_1#slide=id.g3409e58cc55_0_1
    > Implementation: photos.app.goo.gl/U8QzRvTBDqwdkRQT7
    > Bug: 477857535
    > Change-Id: I374da5eb353cf7ea92a8477ad92c3910c37a6e8a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7515508
    > Commit-Queue: Adem Derinel <derinel@google.com>
    > Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1582406}
    
    Bug: 477857535
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I729d20acf9e57d5c85b26f50012012869297706f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7561674
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Zaina Al-Mashni <zalmashni@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1582524}

--- chrome/browser/ui/passwords/credential_manager_dialog_controller_impl.cc
+++ chrome/browser/ui/passwords/credential_manager_dialog_controller_impl.cc
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
 #include "ui/base/l10n/l10n_util.h"
-#include "url/origin.h"
 
 namespace {
 
@@ -89,10 +88,6 @@ bool CredentialManagerDialogControllerImpl::IsShowingAccountChooser() const {
 const CredentialManagerDialogController::FormsVector&
 CredentialManagerDialogControllerImpl::GetLocalForms() const {
   return local_credentials_;
-}
-
-url::Origin CredentialManagerDialogControllerImpl::GetOrigin() const {
-  return delegate_->GetOrigin();
 }
 
 std::u16string CredentialManagerDialogControllerImpl::GetAccountChooserTitle()
