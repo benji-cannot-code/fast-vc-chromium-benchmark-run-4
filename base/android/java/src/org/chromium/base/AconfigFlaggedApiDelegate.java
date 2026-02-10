97822ea15b52f7322dfa7ef508cb48cc9b272f2a
Abdelrahman Eed <abdoeed@google.com>
2026-02-10 05:27:26 -0800

    [Document PiP] Prevent launching pip window if PiP is not enabled
    This change prevents attempting to create a new Document
    Picture-in-Picture window if the Picture-in-Picture permission is not
    enabled, or if going into pinned layer is not supported.
    
    Bug: b:479138379
    Change-Id: Ib3529ca01ba9d0c454ef16a11c07ce9934ee2f83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7557632
    Commit-Queue: Abdelrahman Eed <abdoeed@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1582439}

--- base/android/java/src/org/chromium/base/AconfigFlaggedApiDelegate.java
+++ base/android/java/src/org/chromium/base/AconfigFlaggedApiDelegate.java
@@ -449,6 +449,15 @@ public interface AconfigFlaggedApiDelegate {
         return promise;
     }
 
+    /**
+     * Checks whether the {@link android.app.ActivityManager.AppTask#requestWindowingLayer(AppTask,
+     * AppTask.WINDOWING_LAYER_PINNED, Executor, OutcomeReceiver<Integer, Exception>)} method is
+     * supported.
+     */
+    default boolean isRequestPinnedWindowingLayerSupported() {
+        return false;
+    }
+
     /** Gets an Android SerialManager wrapped in an intermediary object. */
     default @Nullable SerialManager getSerialManager() {
         return null;
--- chrome/android/java/src/org/chromium/chrome/browser/app/tab_activity_glue/PopupCreator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/tab_activity_glue/PopupCreator.java
@@ -63,6 +63,7 @@ public class PopupCreator implements PopupIntentCreator {
     private static @Nullable ReparentingTask sReparentingTaskForTesting;
     private static @Nullable Insets sInsetsForecastForTesting;
     private static @Nullable Boolean sMoveTabToNewPopupResultForTesting;
+    private static @Nullable Boolean sMoveToNewDocumentPiPWindowResultForTesting;
     private static @Nullable Boolean sSetMovableTaskRequiredForPopupsForTesting;
 
     /**
@@ -132,6 +133,10 @@ public class PopupCreator implements PopupIntentCreator {
      */
     public static boolean moveWebContentsToNewDocumentPictureInPictureWindow(
             WebContents webContents, PictureInPictureWindowOptions windowOptions) {
+        if (sMoveToNewDocumentPiPWindowResultForTesting != null) {
+            return sMoveToNewDocumentPiPWindowResultForTesting;
+        }
+
         final ActivityOptions activityOptions =
                 createDocumentPipActivityOptions(
                         windowOptions.windowBounds, webContents.getTopLevelNativeWindow());
@@ -485,6 +490,13 @@ public class PopupCreator implements PopupIntentCreator {
     public static void setMoveTabToNewPopupResultForTesting(Boolean moveTabToNewPopupResult) {
         sMoveTabToNewPopupResultForTesting = moveTabToNewPopupResult;
         ResettersForTesting.register(() -> sMoveTabToNewPopupResultForTesting = null);
+    }
+
+    /** Overrides values returned by {@link moveWebContentsToNewDocumentPictureInPictureWindow}. */
+    public static void setMoveToNewDocumentPiPWindowResultForTesting(
+            Boolean moveToNewDocumentPiPWindowResult) {
+        sMoveToNewDocumentPiPWindowResultForTesting = moveToNewDocumentPiPWindowResult;
+        ResettersForTesting.register(() -> sMoveToNewDocumentPiPWindowResultForTesting = null);
     }
 
     /**
