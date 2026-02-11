092728f2b5dd04ca448e03bac53aeb79bf1fc82c
Jean-Philippe Gravel <jpgravel@chromium.org>
2026-02-11 14:51:03 -0800

    Use absl::flat_hash_set instead of std::set in database.h
    std::set if famously a inefficient data structure. TL;DR: it uses a
    red-black tree storing each values into different memory allocations.
    Every operation on an std::set requires traversal of the tree and each
    node visited likely triggers a cache miss. absl::flat_hash_set is
    recommended by https://crsrc.org/c/base/containers/README.md
    
    Bug: 479068638
    Change-Id: Id5414de96dbf4a23ee7b3b47c13708e3693544c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7564471
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1583541}

--- sql/database.h
+++ sql/database.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <optional>
-#include <set>
 #include <string>
 #include <string_view>
 #include <utility>
@@ -43,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sql/sqlite_result_code_values.h"
 #include "sql/statement_id.h"
 #include "sql/streaming_blob_handle.h"
+#include "third_party/abseil-cpp/absl/container/flat_hash_set.h"
 #include "third_party/perfetto/include/perfetto/tracing/traced_proto.h"
 
 // Forward declaration for SQLite structures. Headers in the public sql:: API
@@ -1184,7 +1184,7 @@ class COMPONENT_EXPORT(SQL) Database {
   // A list of all StatementRefs we've given out. Each ref must register with
   // us when it's created or destroyed. This allows us to potentially close
   // any open statements when we encounter an error.
-  std::set<raw_ptr<StatementRef>> open_statements_;
+  absl::flat_hash_set<raw_ptr<StatementRef>> open_statements_;
 
   // The number of blobs open for streaming, tracked for debugging purposes.
   size_t outstanding_blob_count_ = 0;
