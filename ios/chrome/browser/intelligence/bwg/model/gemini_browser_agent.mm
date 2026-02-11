e40cef6809b0a87c11ccc20716df6174c5c0b79a
Joemer Ramos <joemerramos@google.com>
2026-02-11 13:22:11 -0800

    [iOS] Pass UIWindowScene To Gemini SDK
    Passes the relevant UIWindowScene to the Gemini floaty. Needs internal
    bridging.
    
    Internal CL to land after here: crrev.com/i/9009038
    
    Bug: 482051222
    Change-Id: I3abedd2e274af19b6eca7052a7e14cd011c752b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7563409
    Reviewed-by: Tarek Makkouk <tarekmakkouk@google.com>
    Commit-Queue: Joemer Ramos <joemerramos@google.com>
    Auto-Submit: Joemer Ramos <joemerramos@google.com>
    Cr-Commit-Position: refs/heads/main@{#1583484}

--- ios/chrome/browser/intelligence/bwg/model/gemini_browser_agent.mm
+++ ios/chrome/browser/intelligence/bwg/model/gemini_browser_agent.mm
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/intelligence/features/features.h"
 #import "ios/chrome/browser/intelligence/proto_wrappers/page_context_wrapper.h"
 #import "ios/chrome/browser/shared/coordinator/layout_guide/layout_guide_util.h"
+#import "ios/chrome/browser/shared/coordinator/scene/scene_state.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
 #import "ios/chrome/browser/shared/model/browser/browser_list.h"
@@ -789,6 +790,7 @@ void GeminiBrowserAgent::PresentFloatyWithState(
     config.initialBottomOffset =
         GetFloatyOffsetFromFullscreenController(fullscreen_controller_);
   }
+  config.hostWindowScene = browser_->GetSceneState().scene;
 
   // Start the overlay and update the tab helper to reflect this.
   ios::provider::StartBwgOverlay(config);
