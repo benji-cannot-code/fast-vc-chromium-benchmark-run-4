046ee5738f8076bba41489b2c0610402f9174b6c
Phil Yan <philyan@chromium.org>
2026-02-11 11:16:33 -0800

    [Media][Clank] Allow delayed audio focus gain
    This change introduces a new feature flag
    `kAllowDelayedAudioFocusGainAndroid` on Android. When enabled, it
    allows media playback to start even if the system delays granting
    audio focus (e.g., during a phone call).
    
    Updates AudioFocusDelegate.java to use
    `setAcceptsDelayedFocusGain(true)` and treat
    `AUDIOFOCUS_REQUEST_DELAYED` as a success when the flag is enabled.
    
    Also adds a new histogram "Media.Android.AudioFocusRequestResult" to
    track the outcome of audio focus requests (Granted, Failed, or Delayed)
    and adds unit tests for AudioFocusDelegate.
    
    GVC.
    
    Bug: 483157726
    TESTED: local build w/ flag enabled. Tested with both phone calls and
    Change-Id: I11f9d7e721a345b618c9dc92772098925eec74ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7279122
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Johannes Kron <kron@chromium.org>
    Commit-Queue: Phil Yan <philyan@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1583409}

--- content/browser/android/content_feature_map.cc
+++ content/browser/android/content_feature_map.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/input/features.h"
 #include "content/common/features.h"
 #include "content/public/common/content_features.h"
+#include "media/base/media_switches.h"
 #include "third_party/blink/public/common/features.h"
 #include "ui/accessibility/accessibility_features.h"
 
@@ -29,6 +30,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &blink::features::kSecurePaymentConfirmationBrowserBoundKeys,
     &blink::features::kSecurePaymentConfirmationUxRefresh,
     &blink::features::kViewportSegments,
+    &media::kAllowDelayedAudioFocusGainAndroid,
     &input::features::kInputOnViz,
     &features::kAndroidCaptureKeyEvents,
     &features::kAndroidCaretBrowsing,
