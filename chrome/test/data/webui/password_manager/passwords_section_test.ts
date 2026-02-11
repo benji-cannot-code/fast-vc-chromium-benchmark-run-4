a801aa4a98497f9e67df830a7d73da35117778bc
Amelie Schneider <amelies@google.com>
2026-02-11 05:50:29 -0800

    [Passwords] Fix screen reader for password list items
    Due to a bug introduced in
    https://chromium-review.googlesource.com/c/chromium/src/+/7541355, the
    screen reader no longer gave details about how many passwords are
    available for an item, and whether they are local.
    
    This fixes the bug by removing `class="no-overlap"`, and also adds an
    aria label to the new `cloudUploadButton`.
    
    Bug: 479142155
    Change-Id: Ieb2f543ccdbad0e9f011e5abe233fd49b269502f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7562075
    Commit-Queue: Amelie Schneider <amelies@google.com>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1583167}

--- chrome/test/data/webui/password_manager/passwords_section_test.ts
+++ chrome/test/data/webui/password_manager/passwords_section_test.ts
@@ -627,8 +627,7 @@ suite('PasswordsSectionTest', function() {
 
     // Click the button to open the dialog.
     const cloudUploadButton =
-        listEntry.shadowRoot!.querySelector<HTMLButtonElement>(
-            '#cloudUploadButton');
+        listEntry.shadowRoot!.querySelector<HTMLElement>('#cloudUploadButton');
     assertTrue(!!cloudUploadButton);
     cloudUploadButton.click();
     await flushTasks();
@@ -641,7 +640,7 @@ suite('PasswordsSectionTest', function() {
     assertTrue(movePasswordsDialog.$.dialog.open);
   });
 
-  test('Upload icon tooltip text', async function() {
+  test('Upload icon tooltip and accessibility text', async function() {
     passwordManager.setAccountStorageEnabled(true);
     passwordManager.data.groups = [
       createCredentialGroup({
@@ -656,9 +655,14 @@ suite('PasswordsSectionTest', function() {
     const listEntry =
         section.shadowRoot!.querySelector<HTMLElement>('password-list-item');
     assertTrue(!!listEntry);
+
     assertEquals(
         listEntry.shadowRoot!.querySelector<HTMLElement>(
                                  'cr-tooltip')!.innerHTML,
+        'Save in your Google Account');
+    assertEquals(
+        listEntry.shadowRoot!.querySelector<HTMLElement>(
+                                 '#cloudUploadButton')!.ariaLabel,
         'Save in your Google Account');
   });
 });
