0fa7e5051ca70875b25f4eb56cc9b1559e0ae62d
Olivier Robin <olivierrobin@google.com>
2026-02-11 01:32:14 -0800

    Test ChooseFileTabHelper existence before using it.
    JS messages can be received after the web state is destroyed, and so
    after the tab helper is destroyed.
    Check for its existence before using it.
    
    Fixed: 483364769
    Change-Id: I8584ca4cdb6b791b3f20cec1346fbc6169f23b75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7562074
    Commit-Queue: Elmehdi Rahmaoui <erahmaoui@google.com>
    Reviewed-by: Elmehdi Rahmaoui <erahmaoui@google.com>
    Auto-Submit: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1583068}

--- ios/chrome/browser/web/model/choose_file/choose_file_java_script_feature.mm
+++ ios/chrome/browser/web/model/choose_file/choose_file_java_script_feature.mm
@@ -228,7 +228,9 @@ void ChooseFileJavaScriptFeature::ScriptMessageReceived(
     if (base::FeatureList::IsEnabled(kIOSCustomFileUploadMenu)) {
       ChooseFileTabHelper* tab_helper =
           ChooseFileTabHelper::FromWebState(web_state);
-      tab_helper->SetLastChooseFileEvent(std::move(event));
+      if (tab_helper) {
+        tab_helper->SetLastChooseFileEvent(std::move(event));
+      }
     } else {
       ChooseFileEventHolder::GetInstance()->SetLastChooseFileEvent(
           std::move(event));
