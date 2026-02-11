f07abb61eb31f7552826cb5a170276645d12e902
Ameur Hosni <ameurhosni@google.com>
2026-02-11 06:15:57 -0800

    [IOS][AIM] Return correct page classification for all model modes
    Currently not all model modes are returning the correct page
    classification or setting the right input state's active_tool.
    
    Change-Id: Ic200f55ee8c99f45d56cb61124fdb24b218ac738
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7562210
    Reviewed-by: Radu Nitescu <radunitescu@google.com>
    Auto-Submit: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1583183}

--- ios/chrome/browser/composebox/coordinator/composebox_omnibox_client_delegate.h
+++ ios/chrome/browser/composebox/coordinator/composebox_omnibox_client_delegate.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_COMPOSEBOX_COORDINATOR_COMPOSEBOX_OMNIBOX_CLIENT_DELEGATE_H_
 #define IOS_CHROME_BROWSER_COMPOSEBOX_COORDINATOR_COMPOSEBOX_OMNIBOX_CLIENT_DELEGATE_H_
 
+#import "components/contextual_search/input_state_model.h"
 #import "ios/chrome/browser/composebox/coordinator/composebox_constants.h"
 #import "third_party/omnibox_proto/tool_mode.pb.h"
 
@@ -15,7 +16,8 @@ enum class WindowOpenDisposition;
 /// Delegate for ComposeboxOmniboxClient.
 @protocol ComposeboxOmniboxClientDelegate
 
-- (omnibox::ToolMode)composeboxToolMode;
+/// Returns the current input state of the composebox.
+- (contextual_search::InputState)inputState;
 
 /// Returns the current attached suggest input in the composebox.
 - (std::optional<lens::proto::LensOverlaySuggestInputs>)suggestInputs;
