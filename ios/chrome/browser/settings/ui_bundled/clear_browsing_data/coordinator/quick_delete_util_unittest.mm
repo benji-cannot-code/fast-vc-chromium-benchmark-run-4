1c8a7ae6885d4c2d26f95f4f4756cbe667eb5a1e
Angela Novakovic <novakovica@google.com>
2026-02-11 06:36:56 -0800

    [iOS][PRDBD] Adds QuickDeleteOtherDataMediator
    This CL adds the QuickDeleteOtherDataMediator and connects it to the
    QuickDeleteOtherDataCoordinator as part of the Other Data page flow.
    
    The mediator will be responsible for observing the user's sign-in status
    and default search engine. This information will be used in a following
    CL to configure the UI.
    
    Design Doc: go/bling-delete-browsing-data
    
    Fixed: 476444343
    Bug: 464551859, 471025894
    Change-Id: Ieb34f1cd0c4f301c75321e0c0c089a26e3f9820f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7489686
    Reviewed-by: Noémie St-Onge <noemies@google.com>
    Commit-Queue: Angela Novakovic <novakovica@google.com>
    Reviewed-by: Filipa Senra <fsenra@google.com>
    Cr-Commit-Position: refs/heads/main@{#1583193}

--- ios/chrome/browser/settings/ui_bundled/clear_browsing_data/coordinator/quick_delete_util_unittest.mm
+++ ios/chrome/browser/settings/ui_bundled/clear_browsing_data/coordinator/quick_delete_util_unittest.mm
@@ -9,10 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/strings/sys_string_conversions.h"
 #import "components/browsing_data/core/browsing_data_utils.h"
 #import "components/browsing_data/core/pref_names.h"
+#import "components/search_engines/search_engines_test_environment.h"
+#import "components/search_engines/template_url.h"
 #import "components/strings/grit/components_strings.h"
 #import "components/sync_preferences/pref_service_syncable.h"
 #import "ios/chrome/browser/browsing_data/model/cache_counter.h"
 #import "ios/chrome/browser/browsing_data/model/tabs_counter.h"
+#import "ios/chrome/browser/search_engines/model/template_url_service_factory.h"
 #import "ios/chrome/browser/sessions/model/session_restoration_service_factory.h"
 #import "ios/chrome/browser/shared/model/browser/browser_list_factory.h"
 #import "ios/chrome/browser/shared/model/browser/test/test_browser.h"
@@ -26,11 +29,36 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class QuickDeleteUtilTest : public PlatformTest {
  public:
-  QuickDeleteUtilTest() { profile_ = TestProfileIOS::Builder().Build(); }
+  QuickDeleteUtilTest() {
+    TestProfileIOS::Builder builder;
+    builder.AddTestingFactory(
+        ios::TemplateURLServiceFactory::GetInstance(),
+        ios::TemplateURLServiceFactory::GetDefaultFactory());
+
+    profile_ = std::move(builder).Build();
+    template_url_service_ =
+        search_engines_test_environment_.template_url_service();
+  }
+
+  // Sets the default search engine to not be Google.
+  void SetDseToNonGoogle() {
+    TemplateURLData non_google_provider_data;
+    non_google_provider_data.SetURL(
+        "https://www.nongoogle.com/?q={searchTerms}");
+    non_google_provider_data.suggestions_url =
+        "https://www.nongoogle.com/suggest/?q={searchTerms}";
+
+    auto* non_google_provider = template_url_service_->Add(
+        std::make_unique<TemplateURL>(non_google_provider_data));
+    template_url_service_->SetUserSelectedDefaultSearchProvider(
+        non_google_provider);
+  }
 
  protected:
   web::WebTaskEnvironment task_environment_;
   std::unique_ptr<TestProfileIOS> profile_;
+  search_engines::SearchEnginesTestEnvironment search_engines_test_environment_;
+  raw_ptr<TemplateURLService> template_url_service_;
 };
 
 // Tests the construction of the counter string for cache in for the all time
@@ -174,4 +202,29 @@ TEST_F(QuickDeleteUtilTest, TestTabsCounter) {
         result, browsing_data::TimePeriod::LAST_HOUR);
     EXPECT_NSEQ(test_case.expected_output, output);
   }
+}
+
+// Tests that `GetDefaultSearchEngineState` returns the correct
+// DefaultSearchEngineState when the DSE is Google.
+TEST_F(QuickDeleteUtilTest, TestDseStateWhenDseIsGoogle) {
+  EXPECT_EQ(
+      quick_delete_util::GetDefaultSearchEngineState(template_url_service_),
+      quick_delete_util::DefaultSearchEngineState::kGoogle);
+}
+
+// Tests that `GetDefaultSearchEngineState` returns the correct
+// DefaultSearchEngineState when the DSE is not Google.
+TEST_F(QuickDeleteUtilTest, TestDseStateWhenDseIsNotGoogle) {
+  SetDseToNonGoogle();
+  EXPECT_EQ(
+      quick_delete_util::GetDefaultSearchEngineState(template_url_service_),
+      quick_delete_util::DefaultSearchEngineState::kNotGoogle);
+}
+
+// Tests that `GetDefaultSearchEngineState` returns the correct
+// DefaultSearchEngineState when the DSE is null.
+TEST_F(QuickDeleteUtilTest, TestDseStateWhenDseIsNull) {
+  EXPECT_EQ(quick_delete_util::GetDefaultSearchEngineState(
+                /*template_url_service=*/nullptr),
+            quick_delete_util::DefaultSearchEngineState::kError);
 }
