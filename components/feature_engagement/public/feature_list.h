69a23a185f2e16fd3d30088e8e819488c96c5c6d
Briana McClure <bmcclure@google.com>
2026-02-11 11:21:20 -0800

    [x-plat promos] Register Tab Groups with the FET
    This change adds the Tab Groups to the FET for the Desktop-to-iOS promo.
    Registering Tab Groups allows other IPHs to block the Tab Groups promo
    if needed.
    
    Bug: 479228538
    Change-Id: I644569657267c2a6329bf69f89121a12abee495f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7548630
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Briana McClure <bmcclure@google.com>
    Cr-Commit-Position: refs/heads/main@{#1583413}

--- components/feature_engagement/public/feature_list.h
+++ components/feature_engagement/public/feature_list.h
@@ -558,6 +558,8 @@ DEFINE_VARIATION_PARAM(kIPHiOSLensPromoDesktopFeature,
                        "IPH_iOSLensPromoDesktop");
 DEFINE_VARIATION_PARAM(kIPHiOSEnhancedBrowsingDesktopFeature,
                        "IPH_iOSEnhancedBrowsingDesktop");
+DEFINE_VARIATION_PARAM(kIPHiOSTabGroupsDesktopFeature,
+                       "IPH_iOSTabGroupsDesktop");
 #endif  // !BUILDFLAG(IS_ANDROID)
 
 #if !BUILDFLAG(IS_IOS)
@@ -851,6 +853,7 @@ inline constexpr flags_ui::FeatureEntry::FeatureVariation
         VARIATION_ENTRY(kIPHiOSPaymentPromoDesktopFeature),
         VARIATION_ENTRY(kIPHiOSLensPromoDesktopFeature),
         VARIATION_ENTRY(kIPHiOSEnhancedBrowsingDesktopFeature),
+        VARIATION_ENTRY(kIPHiOSTabGroupsDesktopFeature),
 #endif  // !BUILDFLAG(IS_ANDROID)
 };
 
