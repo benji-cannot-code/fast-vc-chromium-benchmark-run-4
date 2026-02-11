f87b6d5c2379ba30899d01df264a766247a8419b
Dominic Austria <dominicaustria@google.com>
2026-02-11 10:17:17 -0800

    [Vertical tabs] Check hover card showing for tab before hiding it.
    Bug: 465159270
    Change-Id: I99abb3bea13430fcb725866bb3c1d02da42db489
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7563666
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Dominic Austria <dominicaustria@google.com>
    Cr-Commit-Position: refs/heads/main@{#1583350}

--- chrome/browser/ui/views/tabs/vertical/vertical_tab_view.cc
+++ chrome/browser/ui/views/tabs/vertical/vertical_tab_view.cc
@@ -651,7 +651,8 @@ void VerticalTabView::ResetCollectionNode() {
   CHECK(collection_node_);
   TabHoverCardController* hover_card_controller =
       collection_node_->GetController()->GetHoverCardController();
-  if (hover_card_controller) {
+  if (hover_card_controller &&
+      hover_card_controller->IsHoverCardShowingForTab(this)) {
     hover_card_controller->UpdateHoverCard(
         nullptr, TabSlotController::HoverCardUpdateType::kTabRemoved);
   }
