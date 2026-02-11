c4b1b7c09d54bbb43ded2bae821dbdfff5429849
Christoph Schwering <schwering@google.com>
2026-02-11 06:19:59 -0800

    [RR] Add replay mechanism in RecordReplayManager
    This CL implements RecordReplayManager's StartReplay() and
    StopReplay().
    
    Bug: b:483386299
    Change-Id: I3dadb5ba32e3032ce966c01a8a38164240e93493
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7563910
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1583186}

--- chrome/browser/record_replay/record_replay_manager.h
+++ chrome/browser/record_replay/record_replay_manager.h
@@ -8,11 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <optional>
 #include <string>
+#include <vector>
 
+#include "base/functional/callback_forward.h"
 #include "base/memory/raw_ref.h"
 #include "base/memory/weak_ptr.h"
 #include "base/types/pass_key.h"
 #include "chrome/browser/record_replay/recorder.h"
+#include "chrome/browser/record_replay/replayer.h"
 
 namespace record_replay {
 
@@ -31,6 +34,8 @@ class RecordReplayManager {
   RecordReplayManager(const RecordReplayManager&) = delete;
   RecordReplayManager& operator=(const RecordReplayManager&) = delete;
   ~RecordReplayManager();
+
+  RecordReplayClient& client() { return *client_; }
 
   State state() const;
 
@@ -59,15 +64,25 @@ class RecordReplayManager {
   void StartReplay();
   void StopReplay();
 
+  // Retrieves all elements in all active frames that match `element_selector`.
+  void GetMatchingElements(const std::string& element_selector,
+                           base::OnceCallback<void(std::vector<ElementId>)> cb);
+
+  void ReportToUser(std::string_view message);
+
   base::WeakPtr<RecordReplayManager> GetWeakPtr() {
     return weak_ptr_factory_.GetWeakPtr();
   }
 
  private:
-  void ReportToUser(std::string_view message);
+  // Retrieves the recording for the last committed URL, if there is one, and
+  // passes it to `cb`.
+  void GetMatchingRecording(
+      base::OnceCallback<void(std::optional<Recording>)> cb);
 
   raw_ref<RecordReplayClient> client_;
   std::optional<Recorder> recorder_;
+  std::optional<Replayer> replayer_;
   base::WeakPtrFactory<RecordReplayManager> weak_ptr_factory_{this};
 };
 
