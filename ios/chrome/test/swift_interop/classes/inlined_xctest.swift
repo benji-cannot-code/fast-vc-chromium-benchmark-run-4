6a70f354ebaea114a47a17bb9ef66b13ac9b32e2
Justin Novosad <junov@chromium.org>
2026-02-11 12:11:26 -0800

    [iOS] swift_source_set refactor part 3: new modulemap generation
    Change:
    This change refactors modulemaps generation in a way that causes each
    swift_source_set that imports C++ symbols to have a single flat imported
    module that is always named CxxImports.
    
    Motivation:
    * Prevents linker errors caused by having multiple module paths to the
      same C++ symbols, which unlocks new use cases by allowing
      swift_source_set to have dependencies on multiple modules that import
      symbols in common.
    
    Changes to how Cxx imports work after this change:
    * swift_source_set targets now have a 'cxx_import_deps' argument
      for specifying deps that are of type swift_cxx_import.
    * All swift source files that import symbols from C++ now access
      those symbols via `import CxxImports`
    * `swift_cxx_import` targets no longer specify module names. They no
      only specify a flat list of headers to import.
    
    Note: To keep the size of this CL reasonable, support for apinotes,
    which needs to be rewritten in response to this refactor, is
    temporarily removed and will be added back in a follow-up CL
    
    Bug: 476114172
    Change-Id: Id459a14a905f58cade58ca198fa6cebe6735478e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7562615
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1583442}

--- ios/chrome/test/swift_interop/classes/inlined_xctest.swift
+++ ios/chrome/test/swift_interop/classes/inlined_xctest.swift
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import Classes
+import CxxImports
 import UIKit
 import XCTest
 
@@ -22,9 +22,9 @@ class InlinedClassTest: XCTestCase {
     XCTAssertEqual(result, 10, "Stack based class didn't work")
   }
 
-  func testRenamed() {
-    let obj = RenamedClass()
-    XCTAssertTrue(obj.Check())
-  }
-
+  // Temporarily disabled.  Will be re-enabled in follow-up CL
+  // func testRenamed() {
+  //   let obj = RenamedClass()
+  //   XCTAssertTrue(obj.Check())
+  // }
 }
--- ios/chrome/test/swift_interop/std/string_xctest.swift
+++ ios/chrome/test/swift_interop/std/string_xctest.swift
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import StdTestHelpers
+import CxxImports
 import XCTest
 
 // TODO(crbug.com/456484598): Move this extension to a public module that can
