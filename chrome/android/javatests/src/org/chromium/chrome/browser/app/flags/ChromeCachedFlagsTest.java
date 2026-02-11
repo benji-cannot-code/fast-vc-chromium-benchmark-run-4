1c6676a239c8ace9e4118e64bfb88fdb61460e19
Henrique Nakashima <hnakashima@chromium.org>
2026-02-10 17:47:53 -0800

    [Android] Re-enable ChromeCachedFlagsTest on Desktop Android
    Flags have been updated to match their C++ defaults on Desktop Android.
    
    Include-Ci-Only-Tests: chromium.android.desktop:android-desktop-x64-rel-15-tests|chrome_public_test_apk
    Bug: 481445559
    Change-Id: Ib60310f8b12308482c4a48887bca1ee3a12d7382
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7548119
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1582927}

--- chrome/android/javatests/src/org/chromium/chrome/browser/app/flags/ChromeCachedFlagsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/app/flags/ChromeCachedFlagsTest.java
@@ -16,7 +16,6 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.CommandLine;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.build.BuildConfig;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
@@ -24,7 +23,6 @@ import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.transit.ChromeTransitTestRules;
 import org.chromium.chrome.test.transit.FreshCtaTransitTestRule;
 import org.chromium.components.cached_flags.CachedFlag;
-import org.chromium.ui.base.DeviceFormFactor;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -63,7 +61,6 @@ public class ChromeCachedFlagsTest {
      */
     @Test
     @MediumTest
-    @DisableIf.Device(DeviceFormFactor.DESKTOP) // https://crbug.com/481445559
     public void testValueIsConsistentWithDefault() {
         // In Chrome-branded builds, the fieldtrial_testing_config.json isn't applied, so
         // flag values may differ.
