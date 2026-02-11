d391c640813457edd77401f5f0288a8ab766d7f4
Chris Lu <thegreenfrog@google.com>
2026-02-11 11:05:06 -0800

    [ios] A IOSMagicStackRemoteTabKillSwitch FieldTrial config
    Change-Id: Id1fed9005f6b34f7cba93061ebe12c81b16d2d2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7560976
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Auto-Submit: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1583399}

--- ios/chrome/browser/content_suggestions/tab_resumption/coordinator/tab_resumption_mediator.mm
+++ ios/chrome/browser/content_suggestions/tab_resumption/coordinator/tab_resumption_mediator.mm
@@ -109,10 +109,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// Kill switch to disable remote tab resumption.
-BASE_FEATURE(kIOSRemoteTabResumptionKillSwitch,
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
 // A command line flag to override the default sync threshold.
 const char kTabResumptionThresholdParameter[] = "tab-resumption-sync-threshold";
 
