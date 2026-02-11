80dd532b1f9fe2ce86134dff96d3b7a1c3947cc4
Julia Sobiech <jsobiech@google.com>
2026-02-11 11:50:31 -0800

    Update Suggestion constructors to use std::u16string_view
    This change updates several Suggestion constructors to accept
    std::u16string_view instead of std::string_view for main_text and
    labels. This removes the need for UTF-8 to UTF-16 conversion within the
    constructors. All call sites in browser and unit tests have been updated
    to pass UTF-16 strings.
    
    Bug: 335194240
    Change-Id: I6afb3b65d387491f3467561798e849049ae2d49f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7556152
    Commit-Queue: Julia Sobiech <jsobiech@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1583426}

--- chrome/browser/ui/views/autofill/popup/popup_row_factory_utils_browsertest.cc
+++ chrome/browser/ui/views/autofill/popup/popup_row_factory_utils_browsertest.cc
@@ -42,7 +42,7 @@ namespace {
 using ::testing::NiceMock;
 using ::testing::Return;
 
-std::vector<std::string> minor_texts = {"Minor text"};
+std::vector<std::u16string> minor_texts = {u"Minor text"};
 
 Suggestion CreatePasswordSuggestion(const std::u16string& main_text) {
   Suggestion suggestion(main_text, SuggestionType::kPasswordEntry);
@@ -105,28 +105,31 @@ Suggestion CreateAllLoyaltyCardsEntry() {
 // Suggestion main text (Suggestion::main_text) is used for the test and
 // screenshot names, avoid special symbols and keep them unique.
 const Suggestion kSuggestions[] = {
-    Suggestion("Address_entry",
+    Suggestion(u"Address_entry",
                minor_texts,
-               "label",
+               u"label",
                Suggestion::Icon::kLocation,
                SuggestionType::kAddressEntry),
     CreatePasswordSuggestion(u"Password_entry"),
     CreateTryThisRecoverySuggestion(u"Try_this_recovery_password"),
     CreateTroubleSigninInSuggestion(u"Trouble_signing_in_entry"),
     CreateBackupPasswordSuggestion(u"Backup_password_entry"),
-    Suggestion("Autofill_options",
+    Suggestion(u"Autofill_options",
                minor_texts,
-               "label",
+               u"label",
                Suggestion::Icon::kSettings,
                SuggestionType::kManageAddress),
-    Suggestion(u"Autocomplete", SuggestionType::kAutocompleteEntry),
-    Suggestion("Compose",
+    Suggestion(u"Autocomplete",
+               u"",
+               Suggestion::Icon::kNoIcon,
+               SuggestionType::kAutocompleteEntry),
+    Suggestion(u"Compose",
                minor_texts,
-               "label",
+               u"label",
                Suggestion::Icon::kMagic,
                SuggestionType::kComposeResumeNudge),
-    Suggestion("Promo_code",
-               "label",
+    Suggestion(u"Promo_code",
+               u"label",
                Suggestion::Icon::kGlobe,
                SuggestionType::kSeePromoCodeDetails)};
 
@@ -284,7 +287,7 @@ INSTANTIATE_TEST_SUITE_P(
 
 IN_PROC_BROWSER_TEST_F(CreatePopupRowViewTest, FilterMatchHighlighting) {
   CreateRowView(
-      Suggestion("Address_entry", minor_texts, "label",
+      Suggestion(u"Address_entry", minor_texts, u"label",
                  Suggestion::Icon::kLocation, SuggestionType::kAddressEntry),
       /*selected_cell=*/std::nullopt,
       AutofillPopupController::SuggestionFilterMatch{.main_text_match =
@@ -319,7 +322,7 @@ IN_PROC_BROWSER_TEST_F(CreatePopupRowViewTest, PasswordCustomIconLoader) {
                 IDR_DISABLE));
       });
 
-  Suggestion suggestion("Password_entry", minor_texts, "label",
+  Suggestion suggestion(u"Password_entry", minor_texts, u"label",
                         Suggestion::Icon::kKey, SuggestionType::kPasswordEntry);
   suggestion.custom_icon =
       Suggestion::FaviconDetails(/*domain_url=*/GURL("https://google.com"));
@@ -342,7 +345,7 @@ class CreatePopupRowViewWithNoUserEducationRateLimitTest
 
 IN_PROC_BROWSER_TEST_F(CreatePopupRowViewWithNoUserEducationRateLimitTest,
                        ComposeWithNewBadge) {
-  Suggestion suggestion("Compose with a badge", minor_texts, "label",
+  Suggestion suggestion(u"Compose with a badge", minor_texts, u"label",
                         Suggestion::Icon::kMagic,
                         SuggestionType::kComposeProactiveNudge);
   suggestion.feature_for_new_badge =
