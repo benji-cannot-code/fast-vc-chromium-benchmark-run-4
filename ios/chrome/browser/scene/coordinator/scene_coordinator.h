544389b44a20d41bca341e7d42dc017a6bc60f03
Scott Yoder <scottyoder@google.com>
2026-02-11 11:59:00 -0800

    [ios] Migrate ReportAnIssue SceneCommands to SceneCoordinator
    Fixed: 482081330
    Change-Id: I460bd8ca9a9121eb37d049fa18d81bf101ca5f9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7564229
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1583435}

--- ios/chrome/browser/scene/coordinator/scene_coordinator.h
+++ ios/chrome/browser/scene/coordinator/scene_coordinator.h
@@ -27,6 +27,7 @@ enum class SafariDataImportEntryPoint;
 @class ShowSigninCommand;
 @class SigninCoordinator;
 @protocol TabOpening;
+enum class UserFeedbackSender;
 struct UrlLoadParams;
 
 namespace password_manager {
@@ -57,7 +58,7 @@ enum class WarningType;
 @property(nonatomic, assign) Browser* incognitoBrowser;
 
 // Navigation View controller for the settings.
-// TODO(crbug.com/463347803): This property is temporarily exposed to facilitate
+// TODO(crbug.com/483418864): This property is temporarily exposed to facilitate
 // migration. It should be private once the migration is complete.
 @property(nonatomic, strong)
     SettingsNavigationController* settingsNavigationController;
@@ -171,6 +172,19 @@ enum class WarningType;
 // Shows the settings Privacy UI.
 - (void)showPrivacySettingsFromViewController:
     (UIViewController*)baseViewController;
+
+// Shows the Report an Issue UI, presenting from `baseViewController`.
+- (void)showReportAnIssueFromViewController:
+            (UIViewController*)baseViewController
+                                     sender:(UserFeedbackSender)sender;
+
+// Shows the Report an Issue UI, presenting from `baseViewController`, using
+// `specificProductData` for additional product data to be sent in the report.
+- (void)
+    showReportAnIssueFromViewController:(UIViewController*)baseViewController
+                                 sender:(UserFeedbackSender)sender
+                    specificProductData:(NSDictionary<NSString*, NSString*>*)
+                                            specificProductData;
 
 // Shows the Settings UI if nothing else is displayed.
 - (void)maybeShowSettingsFromViewController;
