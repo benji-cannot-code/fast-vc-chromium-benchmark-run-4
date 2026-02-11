99041f77d15edfd366059d4c898dea15d282707c
François Beaufort <beaufort.francois@gmail.com>
2026-02-11 09:18:30 -0800

    [WebMCP] Add parameter schema for <input type=week>
    Bug: 475972617
    Change-Id: I89fbb6c0053c1d01a563044561fb7bf3e00ef832
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7567304
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1583306}

--- third_party/blink/renderer/core/html/forms/form_mcp_schema.h
+++ third_party/blink/renderer/core/html/forms/form_mcp_schema.h
@@ -67,6 +67,9 @@ class CORE_EXPORT FormMCPSchema {
   std::unique_ptr<JSONObject> ComputeMonthParameterSchema(
       const ControlVector& controls_for_name,
       bool& required);
+  std::unique_ptr<JSONObject> ComputeWeekParameterSchema(
+      const ControlVector& controls_for_name,
+      bool& required);
   std::unique_ptr<JSONObject> ComputeTimeParameterSchema(
       const ControlVector& controls_for_name,
       bool& required);
@@ -154,6 +157,7 @@ class CORE_EXPORT FormMCPSchema {
   bool IsDate(HTMLFormControlElement&) const;
   bool IsDatetimeLocal(HTMLFormControlElement&) const;
   bool IsMonth(HTMLFormControlElement&) const;
+  bool IsWeek(HTMLFormControlElement&) const;
   bool IsTime(HTMLFormControlElement&) const;
   bool IsNumber(HTMLFormControlElement&) const;
   bool IsSelect(HTMLFormControlElement&) const;
@@ -166,6 +170,7 @@ class CORE_EXPORT FormMCPSchema {
   bool IsDate(const ControlVector& controls_for_name) const;
   bool IsDatetimeLocal(const ControlVector& controls_for_name) const;
   bool IsMonth(const ControlVector& controls_for_name) const;
+  bool IsWeek(const ControlVector& controls_for_name) const;
   bool IsTime(const ControlVector& controls_for_name) const;
   bool IsNumber(const ControlVector& controls_for_name) const;
   bool IsSelect(const ControlVector& controls_for_name) const;
