94ae8901d576a2b459284db05971873ecdf968fd
Dan Harrington <harringtond@chromium.org>
2026-02-11 11:30:51 -0800

    add GlicClearCookiesOnFirstSync
    Bug: b:478252857
    Change-Id: I8ce8238a571529cb605944b203f1d3c86a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7517934
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: Dan Harrington <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1583418}

--- chrome/browser/glic/host/glic_cookie_synchronizer.cc
+++ chrome/browser/glic/host/glic_cookie_synchronizer.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/glic/host/glic_cookie_synchronizer.h"
 
+#include <cstdint>
 #include <iterator>
 #include <memory>
 #include <string>
@@ -41,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // BUILDFLAG(ENABLE_DICE_SUPPORT)
 
 namespace glic {
+namespace {
 
 BASE_FEATURE(kGlicCookieSyncTimeout, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE_PARAM(base::TimeDelta,
@@ -48,8 +50,9 @@ BASE_FEATURE_PARAM(base::TimeDelta,
                    &kGlicCookieSyncTimeout,
                    "glic_cookie_sync_timeout_duration",
                    GlicCookieSynchronizer::kCookieSyncDefaultTimeout);
-
-namespace {
+// If enabled, clears the glic partition's cookies before the first cookie sync.
+// This ensures a fresh cookie state after restart.
+BASE_FEATURE(kGlicClearCookiesOnFirstSync, base::FEATURE_ENABLED_BY_DEFAULT);
 
 content::StoragePartitionConfig GetGlicMainStoragePartitionConfig(
     content::BrowserContext* browser_context) {
@@ -75,6 +78,27 @@ content::StoragePartitionConfig GetGlicStoragePartitionConfig(
 }
 
 }  // namespace
+
+class GlicCookieSynchronizer::ClearCookiesTask {
+ public:
+  ClearCookiesTask(content::StoragePartition* storage_partition,
+                   base::OnceClosure callback)
+      : callback_(std::move(callback)) {
+    network::mojom::CookieManager* cookie_manager =
+        storage_partition->GetCookieManagerForBrowserProcess();
+
+    cookie_manager->DeleteCookies(
+        network::mojom::CookieDeletionFilter::New(),
+        base::BindOnce(&ClearCookiesTask::DeleteDone,
+                       weak_ptr_factory_.GetWeakPtr()));
+  }
+
+ private:
+  void DeleteDone(uint32_t num_deleted) { std::move(callback_).Run(); }
+
+  base::OnceClosure callback_;
+  base::WeakPtrFactory<ClearCookiesTask> weak_ptr_factory_{this};
+};
 
 // Synchronize cookies from the main partition to the webview partition. This
 // is an alternative way to sync cookies which supports glic development, and
@@ -231,7 +255,8 @@ void GlicCookieSynchronizer::CopyCookiesToWebviewStoragePartition(
   CHECK(!callback.is_null());
   callbacks_.push_back(std::move(callback));
 
-  if (cookie_loader_ || sync_cookies_for_development_task_) {
+  if (cookie_loader_ || sync_cookies_for_development_task_ ||
+      clear_cookies_task_) {
     // A request is in progress already.
     return;
   }
@@ -241,8 +266,8 @@ void GlicCookieSynchronizer::CopyCookiesToWebviewStoragePartition(
                    base::BindOnce(&GlicCookieSynchronizer::OnTimeout,
                                   base::Unretained(this)));
   }
-
-  if (!GetStoragePartition()) {
+  auto* glic_storage_partition = GetStoragePartition();
+  if (!glic_storage_partition) {
     DLOG(ERROR) << "glic webview storage partition does not exist";
     CompleteAuth(false);
     return;
@@ -257,6 +282,12 @@ void GlicCookieSynchronizer::CopyCookiesToWebviewStoragePartition(
             base::BindOnce(
                 &GlicCookieSynchronizer::SyncCookiesForDevelopmentComplete,
                 GetWeakPtr()));
+  } else if (base::FeatureList::IsEnabled(kGlicClearCookiesOnFirstSync) &&
+             !has_cleared_cookies_) {
+    clear_cookies_task_ = std::make_unique<ClearCookiesTask>(
+        glic_storage_partition,
+        base::BindOnce(&GlicCookieSynchronizer::ClearCookiesComplete,
+                       GetWeakPtr()));
   } else {
     BeginCookieSync();
   }
@@ -266,6 +297,12 @@ void GlicCookieSynchronizer::OnIdentityManagerShutdown(
     signin::IdentityManager* identity_manager) {
   observation_.Reset();
   cookie_loader_.reset();
+}
+
+void GlicCookieSynchronizer::ClearCookiesComplete() {
+  clear_cookies_task_.reset();
+  has_cleared_cookies_ = true;
+  BeginCookieSync();
 }
 
 void GlicCookieSynchronizer::SyncCookiesForDevelopmentComplete(bool success) {
--- chrome/browser/glic/host/glic_cookie_synchronizer.h
+++ chrome/browser/glic/host/glic_cookie_synchronizer.h
@@ -71,11 +71,13 @@ class GlicCookieSynchronizer
 
  private:
   class SyncCookiesForDevelopmentTask;
+  class ClearCookiesTask;
   base::WeakPtr<GlicCookieSynchronizer> GetWeakPtr() {
     return weak_ptr_factory_.GetWeakPtr();
   }
 
   void SyncCookiesForDevelopmentComplete(bool success);
+  void ClearCookiesComplete();
   void BeginCookieSync();
 
   // signin::AccountsCookieMutator::PartitionDelegate:
@@ -93,6 +95,7 @@ class GlicCookieSynchronizer
   void CompleteAuth(bool is_success);
   void OnTimeout();
 
+  bool has_cleared_cookies_ = false;
   const raw_ptr<content::BrowserContext> context_;
   const raw_ptr<signin::IdentityManager> identity_manager_;
   base::ScopedObservation<signin::IdentityManager,
@@ -106,6 +109,7 @@ class GlicCookieSynchronizer
   base::OneShotTimer timeout_;
   std::unique_ptr<signin::AccountsCookieMutator::SetAccountsInCookieTask>
       cookie_loader_;
+  std::unique_ptr<ClearCookiesTask> clear_cookies_task_;
   std::unique_ptr<SyncCookiesForDevelopmentTask>
       sync_cookies_for_development_task_;
   base::WeakPtrFactory<GlicCookieSynchronizer> weak_ptr_factory_{this};
