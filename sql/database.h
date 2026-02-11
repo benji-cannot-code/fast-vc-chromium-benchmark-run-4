2d1ddcdf918dbae0e8431e14ce28d28215bd381a
Jean-Philippe Gravel <jpgravel@chromium.org>
2026-02-11 14:49:55 -0800

    Split the StreamingBlobHandle unit test into atomic tests
    The whole StreamingBlobHandle class was tested using a single large
    monolithic unit test. The test structure made it hard for a reader to
    understand what behavior is being tested, and hard for writers to
    modify or add test cases. Ultimately, this imposed a limit on test
    coverage because there is so much a single test can do.
    
    This CL splits the test into focused and atomic units, which will be
    useful in follow-up CLs to allow proper test coverage.
    
    Bug: 479068638
    Change-Id: I04f7e17aca8ba99c3bc304783e6ffc9787c93b6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7552690
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1583539}

--- sql/database.h
+++ sql/database.h
@@ -898,7 +898,6 @@ class COMPONENT_EXPORT(SQL) Database {
   FRIEND_TEST_ALL_PREFIXES(SQLDatabaseTest, RegisterIntentToUpload);
   FRIEND_TEST_ALL_PREFIXES(SQLiteFeaturesTest, WALNoClose);
   FRIEND_TEST_ALL_PREFIXES(SQLEmptyPathDatabaseTest, EmptyPathTest);
-  FRIEND_TEST_ALL_PREFIXES(StreamingBlobHandleTest, Basic);
 
   // A scoped utility to setup error reporting during the `Open()` operation
   class ScopedOpenErrorReporter {
