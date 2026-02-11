004c002c9188b254dfa6bd0c0348fb55027f840f
Yuwei Huang <yuweih@chromium.org>
2026-02-10 18:39:27 -0800

    [crd host][linux] Finish implementation of LinuxWorkerProcessLauncherDelegate
    Implement all the NOTIMPLEMENTED methods, mostly copying what
    UnprivilegedProcessDelegate does, and adds a ProcessExitWatcher to watch
    for process exit.
    
    Bug: 475611769
    Change-Id: Ia3920769e187cf2d40af8fa09a4c66ae29df0664
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7565158
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1582940}

--- remoting/host/linux/linux_process_launcher_delegate.h
+++ remoting/host/linux/linux_process_launcher_delegate.h
@@ -6,17 +6,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef REMOTING_HOST_LINUX_LINUX_PROCESS_LAUNCHER_DELEGATE_H_
 #define REMOTING_HOST_LINUX_LINUX_PROCESS_LAUNCHER_DELEGATE_H_
 
+#include <memory>
+
 #include "base/command_line.h"
 #include "base/environment.h"
 #include "base/files/file_path.h"
 #include "base/location.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/memory/weak_ptr.h"
+#include "base/process/process.h"
 #include "base/sequence_checker.h"
+#include "base/task/sequenced_task_runner.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/thread_annotations.h"
+#include "base/threading/sequence_bound.h"
 #include "ipc/ipc_channel_proxy.h"
 #include "ipc/ipc_listener.h"
+#include "remoting/host/mojom/desktop_session.mojom.h"
 #include "remoting/host/worker_process_launcher.h"
 
 namespace remoting {
@@ -76,12 +83,17 @@ class LinuxWorkerProcessLauncherDelegate
   void KillProcess() override;
 
  private:
+  class ProcessExitWatcher;
+
   // IPC::Listener implementation.
   void OnChannelConnected(int32_t peer_pid) override;
   void OnChannelError() override;
   void OnAssociatedInterfaceRequest(
       const std::string& interface_name,
       mojo::ScopedInterfaceEndpointHandle handle) override;
+
+  void WatchForProcessExit();
+  void OnProcessExited(int exit_code);
 
   void ReportFatalError();
 
@@ -94,10 +106,21 @@ class LinuxWorkerProcessLauncherDelegate
   std::unique_ptr<IPC::ChannelProxy> channel_
       GUARDED_BY_CONTEXT(sequence_checker_);
 
+  // The worker process, if launched.
+  base::Process worker_process_;
+
+  base::SequenceBound<ProcessExitWatcher> process_exit_watcher_
+      GUARDED_BY_CONTEXT(sequence_checker_);
+
+  mojo::AssociatedRemote<mojom::WorkerProcessControl> worker_process_control_;
+
   scoped_refptr<base::SingleThreadTaskRunner> io_task_runner_
       GUARDED_BY_CONTEXT(sequence_checker_);
   raw_ptr<WorkerProcessLauncher> event_handler_
       GUARDED_BY_CONTEXT(sequence_checker_) = nullptr;
+
+  base::WeakPtrFactory<LinuxWorkerProcessLauncherDelegate> weak_ptr_factory_{
+      this};
 };
 
 }  // namespace remoting
