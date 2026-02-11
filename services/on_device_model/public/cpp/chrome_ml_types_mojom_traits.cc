2d9c1cb568c663fb0375b5bd5a58acc4cae03673
Reilly Grant <reillyg@chromium.org>
2026-02-10 20:26:39 -0800

    Add consistency checks for deserializing ml::AudioBuffer
    The size of the data buffer must be equal to the product of the number
    of channels and frames.
    
    Fixed: 471964172
    Change-Id: Ia83a367e7b4ffc19de5369a8bf8051dd8f4872ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7564752
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Fred Shih <ffred@chromium.org>
    Commit-Queue: Fred Shih <ffred@chromium.org>
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1583001}

--- services/on_device_model/public/cpp/chrome_ml_types_mojom_traits.cc
+++ services/on_device_model/public/cpp/chrome_ml_types_mojom_traits.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
+#include "base/numerics/checked_math.h"
+
 namespace mojo {
 
 // static
@@ -190,8 +192,19 @@ bool StructTraits<on_device_model::mojom::AudioDataDataView, ml::AudioBuffer>::
   out->sample_rate_hz = in.sample_rate();
   out->num_channels = in.channel_count();
   out->num_frames = in.frame_count();
+
+  base::CheckedNumeric<size_t> expected_num_samples = out->num_channels;
+  expected_num_samples *= out->num_frames;
+  if (!expected_num_samples.IsValid()) {
+    return false;
+  }
+
   mojo::ArrayDataView<float> data_view;
   in.GetDataDataView(&data_view);
+  if (data_view.size() != expected_num_samples.ValueOrDie()) {
+    return false;
+  }
+
   out->data.reserve(data_view.size());
   std::copy_n(data_view.data(), data_view.size(),
               std::back_inserter(out->data));
