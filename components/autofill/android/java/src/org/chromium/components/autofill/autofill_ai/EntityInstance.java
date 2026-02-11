f6334002b4f0c8be2631e490b38b5ada6b719fcb
Timofey Chudakov <tchudakov@google.com>
2026-02-11 02:07:32 -0800

    [AutofillAi - M4] Show text attributes.
    This CL renders STRING, NAME and STATE attributes in the
    entity editor. The COUNTRY and DATE data types will be implemented
    separately.
    
    Screenshot: screen/7nb6P6YrJcMRD4s
    
    Bug: 476755159
    Change-Id: I9be9d71abef1cac627e8fb5a33f03a15442d2af4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7560180
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1583093}

--- components/autofill/android/java/src/org/chromium/components/autofill/autofill_ai/EntityInstance.java
+++ components/autofill/android/java/src/org/chromium/components/autofill/autofill_ai/EntityInstance.java
@@ -124,6 +124,10 @@ public class EntityInstance {
         return new ArrayList<>(mAttributes.values());
     }
 
+    public @Nullable AttributeInstance getAttribute(@AttributeTypeName int attributeTypeName) {
+        return mAttributes.get(attributeTypeName);
+    }
+
     @CalledByNative
     public EntityMetadata getMetadata() {
         return mMetadata;
