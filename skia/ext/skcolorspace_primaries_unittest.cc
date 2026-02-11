719e778d3034cd833f303800eeac755f2494b63a
Zekun Jiang <zekunjiang@google.com>
2026-02-11 11:06:44 -0800

    Fix UNSAFE_TODO in skcolorspace_primaries_unittest.cc.
    Fixed: 455759555
    Change-Id: I1f01771e7482eede8af535833a9b12e7c01d5068
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7564672
    Commit-Queue: Zekun Jiang <zekunjiang@google.com>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1583401}

--- skia/ext/skcolorspace_primaries_unittest.cc
+++ skia/ext/skcolorspace_primaries_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "skia/ext/skcolorspace_primaries.h"
 
 #include "base/compiler_specific.h"
+#include "base/containers/span.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace skia {
@@ -50,10 +51,13 @@ TEST(SkiaUtils, PrimariesD50) {
 
   // The two matrices should be the same, but the primaries will not be.
   EXPECT_FALSE(pro_photo == d65);
-  for (size_t i = 0; i < 3; ++i) {
-    for (size_t j = 0; j < 3; ++j) {
-      UNSAFE_TODO(EXPECT_NEAR(pro_photo_matrix.vals[i][j],
-                              d65_matrix.vals[i][j], kEpsilon));
+  auto pro_photo_rows = base::span(pro_photo_matrix.vals);
+  auto d65_rows = base::span(d65_matrix.vals);
+  for (size_t i = 0; i < pro_photo_rows.size(); ++i) {
+    auto pro_photo_row = base::span(pro_photo_rows[i]);
+    auto d65_row = base::span(d65_rows[i]);
+    for (size_t j = 0; j < pro_photo_rows.size(); ++j) {
+      EXPECT_NEAR(pro_photo_row[j], d65_row[j], kEpsilon);
     }
   }
 }
