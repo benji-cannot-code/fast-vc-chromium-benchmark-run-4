d0ab254c09a1c421bbfab1b848f67c8c52a5be83
Calder Kitagawa <ckitagawa@chromium.org>
2026-02-12 12:05:40 -0800

    [WebContents] Delete user data on destroy
    WebContentsImpl does not destroy its internals UserDataHost. At present,
    anything that depends on `UserData#destroy()` being called is incorrect.
    Given several WebContents UserData objects likely have C++ counterparts
    this probably leak memory anytime a WebContents is destroyed.
    
    This CL fixes this by calling destroy when the native pointer is cleared
    which should be 1:1 with destruction.
    
    Bug: 483716612
    Change-Id: If46e2d864efd41b1a226db81eccad1e86dce504a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7567605
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1584175}

--- content/public/android/java/src/org/chromium/content/browser/webcontents/WebContentsImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/webcontents/WebContentsImpl.java
@@ -305,6 +305,11 @@ public class WebContentsImpl
             mObserverProxy.webContentsDestroyed();
             mObserverProxy = null;
         }
+        UserDataHost userDataHost = getUserDataHost();
+        if (userDataHost != null) {
+            userDataHost.destroy();
+            mInternalsHolder.set(null);
+        }
         var removedValue = sWebContentsMap.remove(nativeWebContentsAndroid);
         assert removedValue != null;
     }
