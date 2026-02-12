b27abe7342be9d8c025ff7fbbc884857a0c6259a
Mike Dougherty <michaeldo@chromium.org>
2026-02-12 07:21:01 -0800

    Cleanup gCrWeb error handling
    This `hasRegisteredApi` check is remaining from when
    `callFunctionInGcrWeb` needed to support gCrWebLegacy API calls.
    
    Additionally, the try/catch block hides error details from the native
    side error reporting. In this case, it is better to allow the CrWebError
    to bubble to the native side. This should only trigger an error if
    native code is calling a non-existent gCrWeb function/property. Although
    not catching it would allow the error to be visible to webpages, this
    should be very rare that such an implementation makes it to stable
    provided JS APIs have some test coverage and kAssertOnJavaScriptErrors
    is enabled more widely.
    
    Bug: 399666983
    Change-Id: Id2ef64312fea1b6a4d001a977555261fc4e8d61c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7569208
    Reviewed-by: Alexandra Pereira <alexandrasp@google.com>
    Commit-Queue: Alexandra Pereira <alexandrasp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1583952}

--- ios/web/js_features/window_error/resources/error.ts
+++ ios/web/js_features/window_error/resources/error.ts
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview Error listener to report error details to the native app.
  */
 
+import {CrWebError} from '//ios/web/public/js_messaging/resources/gcrweb_error.js';
 import {sendWebKitMessage} from '//ios/web/public/js_messaging/resources/utils.js';
 
 /**
@@ -14,8 +15,20 @@ import {sendWebKitMessage} from '//ios/web/public/js_messaging/resources/utils.j
  * added ASAP to catch any errors in startup.
  */
 function errorEventHandler(event: ErrorEvent): void {
+  // CrWebError errors will be reported directly because they are triggered from
+  // native API calls and will provide better error details than the error
+  // handler here. Early return to prevent double reporting those errors.
+  if (event instanceof CrWebError) {
+    return;
+  }
+
   sendWebKitMessage('WindowErrorResultHandler', {
     'line_number': event.lineno,
+    // The JS error handler is limited in the error details it can access and is
+    // likely to only have the message "Script error". Thus, this is a last
+    // resort error reporting mechanism for errors which were not caught. Yet,
+    // reporting these errors are important, especially for tests where
+    // `kAssertOnJavaScriptErrors` may be enabled.
     'message': event.message.toString(),
   });
 }
