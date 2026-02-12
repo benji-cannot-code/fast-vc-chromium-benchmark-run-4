e8f4072bdc483ebee476bb25c8ba708c66775298
Patrick Noland <pnoland@google.com>
2026-02-12 14:20:42 -0800

    Make scrim transparent for tablets w/ fusebox
    Bug: 474616704
    Change-Id: I8ffd15f93f6d268a8c1b6861c56e862f8e28097a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7488345
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1584208}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/LocationBarFocusScrimHandler.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/LocationBarFocusScrimHandler.java
@@ -6,7 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.toolbar;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.View;
+
+import androidx.annotation.ColorInt;
 
 import org.chromium.base.Callback;
 import org.chromium.base.supplier.NonNullObservableSupplier;
@@ -18,6 +21,7 @@ import org.chromium.chrome.browser.omnibox.LocationBarDataProvider;
 import org.chromium.chrome.browser.omnibox.UrlFocusChangeListener;
 import org.chromium.components.browser_ui.widget.scrim.ScrimManager;
 import org.chromium.components.browser_ui.widget.scrim.ScrimProperties;
+import org.chromium.components.omnibox.OmniboxFeatures;
 import org.chromium.ui.base.DeviceFormFactor;
 import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.ui.util.ColorUtils;
@@ -93,11 +97,19 @@ public class LocationBarFocusScrimHandler implements UrlFocusChangeListener {
         }
 
         boolean isTablet = DeviceFormFactor.isNonMultiDisplayContextOnTablet(mContext);
+        boolean useTransparentScrim =
+                isTablet && OmniboxFeatures.sOmniboxMultimodalInput.isEnabled();
         boolean useLightColor =
                 !isTablet
                         && !mLocationBarDataProvider.isIncognitoBranded()
                         && !ColorUtils.inNightMode(mContext);
-        mScrimModel.set(ScrimProperties.BACKGROUND_COLOR, useLightColor ? mLightScrimColor : null);
+        @ColorInt Integer scrimColor = null;
+        if (useTransparentScrim) {
+            scrimColor = Color.TRANSPARENT;
+        } else if (useLightColor) {
+            scrimColor = mLightScrimColor;
+        }
+        mScrimModel.set(ScrimProperties.BACKGROUND_COLOR, scrimColor);
         mScrimModel.set(
                 ScrimProperties.BOTTOM_MARGIN,
                 mBottomControlsStacker.getHeightFromLayerToBottom(LayerType.BOTTOM_CHIN));
