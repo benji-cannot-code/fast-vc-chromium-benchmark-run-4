370cdd9b841c925f8c19bfe4c1e6581c2aae806d
Steinar H. Gunderson <sesse@chromium.org>
2026-02-12 14:40:39 -0800

    [css-mixins-1] Support conditionals around @result.
    These generally work just as conditionals around normal nested rules
    would be, and use the same machinery.
    
    We do not support conditionals around locals yet.
    
    Bug: 406935599
    Change-Id: Ic4e1f68d34ea0f3a2bc828ec031ae9e139c0f736
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7566873
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1584240}

--- third_party/blink/renderer/core/css/parser/css_selector_parser.cc
+++ third_party/blink/renderer/core/css/parser/css_selector_parser.cc
@@ -777,6 +777,7 @@ static std::optional<CSSSelector> MaybeCreateImplicitDescendantAnchor(
       }
       break;
     case CSSNestingType::kFunction:
+    case CSSNestingType::kMixin:
       NOTREACHED();
   }
   return std::nullopt;
