452cf99f5202f2d025a1adfe7ce9e4c9af89341b
Syed AbuTalib <lowkey@google.com>
2026-02-11 18:37:46 -0800

    Add runtime version of ChannelLayoutConfig::FromLayout()
    There are several cases where we create a ChannelLayoutConfig doing
    `{foo_layout, ChannelLayoutToChannelCount(foo_layout)}`. Adding a
    nontemplated FromLayout method will clean these cases to be a simple
    call.
    
    Bug: 483461268
    Change-Id: Ib37b8e0ec24926b8eb0bba026bfc1b0f6b7a893c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7569128
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Syed AbuTalib <lowkey@google.com>
    Cr-Commit-Position: refs/heads/main@{#1583683}

--- media/base/channel_layout.h
+++ media/base/channel_layout.h
@@ -232,6 +232,10 @@ class MEDIA_EXPORT ChannelLayoutConfig {
     return ChannelLayoutConfig(layout, ChannelLayoutToChannelCount(layout));
   }
 
+  static ChannelLayoutConfig FromLayout(ChannelLayout layout) {
+    return ChannelLayoutConfig(layout, ChannelLayoutToChannelCount(layout));
+  }
+
   static constexpr ChannelLayoutConfig Mono() {
     return ChannelLayoutConfig(Passkey(), CHANNEL_LAYOUT_MONO, 1u);
   }
