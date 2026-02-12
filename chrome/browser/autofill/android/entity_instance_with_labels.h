19050b6009f2ade5e1190135840cd51e2465b5fe
Timofey Chudakov <tchudakov@google.com>
2026-02-12 07:22:00 -0800

    [AutofillAi - M4] Create Jni converters for EntityInstanceWithLabels.
    This CL fixed the crash that is caused by the class cast exception
    in the Java EntityDataManager::getEntitiesWithLabels - the return
    type of the native method is EntityInstanceWithLabels[], but the
    C++ code returns Object[].
    
    Bug: 476754838
    Change-Id: I641caed782bc4b083b03d6b1e041c04ec64b115c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7566534
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1583953}

---
+++ chrome/browser/autofill/android/entity_instance_with_labels.h
@@ -0,0 +1,51 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_AUTOFILL_ANDROID_ENTITY_INSTANCE_WITH_LABELS_H_
+#define CHROME_BROWSER_AUTOFILL_ANDROID_ENTITY_INSTANCE_WITH_LABELS_H_
+
+#include <string>
+
+#include "base/android/jni_android.h"
+#include "base/android/scoped_java_ref.h"
+#include "third_party/jni_zero/jni_zero.h"
+
+namespace autofill {
+
+// The C++ counterpart to the Java class of the same name.
+struct EntityInstanceWithLabels {
+  EntityInstanceWithLabels(std::string guid,
+                           std::u16string entity_instance_label,
+                           std::u16string entity_instance_sublabel,
+                           bool stored_in_wallet);
+  ~EntityInstanceWithLabels();
+  EntityInstanceWithLabels(const EntityInstanceWithLabels&);
+  EntityInstanceWithLabels& operator=(const EntityInstanceWithLabels&);
+  EntityInstanceWithLabels(EntityInstanceWithLabels&&);
+  EntityInstanceWithLabels& operator=(EntityInstanceWithLabels&&);
+
+  std::string guid;
+  std::u16string entity_instance_label;
+  std::u16string entity_instance_sublabel;
+  bool stored_in_wallet;
+};
+
+}  // namespace autofill
+
+namespace jni_zero {
+
+template <>
+autofill::EntityInstanceWithLabels
+FromJniType<autofill::EntityInstanceWithLabels>(JNIEnv* env,
+                                                const JavaRef<jobject>& jobj);
+
+template <>
+ScopedJavaLocalRef<jobject> ToJniType<autofill::EntityInstanceWithLabels>(
+    JNIEnv* env,
+    const autofill::EntityInstanceWithLabels& native_instance);
+
+}  // namespace jni_zero
+
+#endif  // CHROME_BROWSER_AUTOFILL_ANDROID_ENTITY_INSTANCE_WITH_LABELS_H_
