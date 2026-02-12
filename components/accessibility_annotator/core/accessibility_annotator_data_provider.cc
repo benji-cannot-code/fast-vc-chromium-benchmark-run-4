343ed2c4edf85ce48d67d993ea15012815925a39
Zeyad El-arabaty <zelarabaty@google.com>
2026-02-12 14:18:19 -0800

    Introduce public API for Accessibility Annotator.
    This change adds the `AccessibilityAnnotatorDataProvider` interface, a
    `KeyedService` for providing data, and defines various entity types.
    
    Bug: 482088018
    Change-Id: I6028ca09bd24c4bdd7c1157cccb9c36d077495a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7547211
    Reviewed-by: Andrew Vuong <akvuong@google.com>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Zeyad El-arabaty <zelarabaty@google.com>
    Cr-Commit-Position: refs/heads/main@{#1584204}

---
+++ components/accessibility_annotator/core/accessibility_annotator_data_provider.cc
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/accessibility_annotator/core/accessibility_annotator_data_provider.h"
+
+namespace accessibility_annotator {
+
+AccessibilityAnnotatorDataProvider::AccessibilityAnnotatorDataProvider() =
+    default;
+AccessibilityAnnotatorDataProvider::~AccessibilityAnnotatorDataProvider() =
+    default;
+
+}  // namespace accessibility_annotator
