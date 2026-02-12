6ea02bdbe8472b183fedd01d102cc67115497f73
Andrew Grieve <agrieve@chromium.org>
2026-02-12 14:18:33 -0800

    Remove stale comments about @DoNotMock
    Bug: 482041379
    Change-Id: Ic859009b9b67b7244aa7c083b9d9f705b8f6846b
    Cq-Include-Trybots: luci.chrome.try:android-internal-dbg,android-internal-binary-size
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7573313
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1584205}

--- base/android/java/src/org/chromium/base/supplier/SettableNullableObservableSupplier.java
+++ base/android/java/src/org/chromium/base/supplier/SettableNullableObservableSupplier.java
@@ -15,7 +15,6 @@ import org.chromium.build.annotations.Nullable;
  * @param <T> The type of the object supplied.
  */
 @NullMarked
-// TODO(agrieve): Add @DoNotMock
 public interface SettableNullableObservableSupplier<T>
         extends NullableObservableSupplier<T>, Destroyable {
     /**
