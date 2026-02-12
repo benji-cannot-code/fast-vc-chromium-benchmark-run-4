f7f6546ef4c20a5cd0a83518f8f594a50ed93263
Bo Liu <boliu@chromium.org>
2026-02-12 07:47:00 -0800

    viz: Minor clean up to FinishPaint
    Remove unused SyncToken param.
    
    Change-Id: I5da4ac12b3141da4725cc5c7fc71fd511abc16b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7568126
    Auto-Submit: Bo Liu <boliu@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1583977}

--- components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.h
+++ components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.h
@@ -163,7 +163,6 @@ class SkiaOutputSurfaceImplOnGpu
       sk_sp<GrDeferredDisplayList> overdraw_ddl,
       std::unique_ptr<skgpu::graphite::Recording> graphite_recording,
       std::vector<raw_ptr<ImageContextImpl, VectorExperimental>> image_contexts,
-      std::vector<gpu::SyncToken> sync_tokens,
       base::OnceClosure on_finished,
       base::OnceCallback<void(gfx::GpuFenceHandle)> return_release_fence_cb);
   void SwapBuffers(OutputSurfaceFrame frame);
@@ -185,7 +184,6 @@ class SkiaOutputSurfaceImplOnGpu
       sk_sp<GrDeferredDisplayList> overdraw_ddl,
       std::unique_ptr<skgpu::graphite::Recording> graphite_recording,
       std::vector<raw_ptr<ImageContextImpl, VectorExperimental>> image_contexts,
-      std::vector<gpu::SyncToken> sync_tokens,
       base::OnceClosure on_finished,
       base::OnceCallback<void(gfx::GpuFenceHandle)> return_release_fence_cb,
       const gfx::Rect& update_rect,
