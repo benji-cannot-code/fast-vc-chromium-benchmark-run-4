174b004eaeedeb69e733004346d9e68ab6386933
Syed AbuTalib <lowkey@google.com>
2026-02-12 15:52:25 -0800

    Add include for base::DoNothing()
    Led to a compile error when building starboard_renderer_tests. This
    resolves the compile error.
    
    Test: Ran `autoninja -C out/Default starboard_renderer_tests_build_tests starboard_renderer_unittests_cast_runtime_deps`
    Change-Id: I78b438aa9ad15995e1458e1b4582163bb2acdff4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7573001
    Reviewed-by: Simeon Anfinrud <sanfin@chromium.org>
    Commit-Queue: Syed AbuTalib <lowkey@google.com>
    Cr-Commit-Position: refs/heads/main@{#1584304}

--- chromecast/starboard/media/renderer/geometry_change_handler.cc
+++ chromecast/starboard/media/renderer/geometry_change_handler.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromecast/starboard/media/renderer/geometry_change_handler.h"
 
 #include "base/check.h"
+#include "base/functional/callback_helpers.h"
 #include "base/logging.h"
 #include "chromecast/media/service/video_geometry_setter_service.h"
 #include "ui/display/screen.h"
