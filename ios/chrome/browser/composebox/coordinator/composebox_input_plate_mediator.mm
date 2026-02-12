0a58c714333d212c59c816f662d98e179245d41f
Radu Nitescu <radunitescu@google.com>
2026-02-12 01:47:33 -0800

    [iOS][AIM] Add metrics recording for canvas and deep search
    Change-Id: Ic43799fbe0c0f21ad3f9478ce5786f9aff8735b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7544384
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1583827}

--- ios/chrome/browser/composebox/coordinator/composebox_input_plate_mediator.mm
+++ ios/chrome/browser/composebox/coordinator/composebox_input_plate_mediator.mm
@@ -1159,10 +1159,19 @@ CreateInputDataFromAnnotatedPageContent(
                                                         kImageGeneration];
       break;
     case ComposeboxMode::kCanvas:
-      // TODO(crbug.com/477244841): Add metrics recording for canvas.
+      [self.metricsRecorder
+          recordComposeboxFocusResultedInNavigation:_inNavigation
+                                    withAttachments:!_items.empty
+                                        requestType:AutocompleteRequestType::
+                                                        kCanvas];
       break;
     case ComposeboxMode::kDeepSearch:
-      // TODO(crbug.com/481280186): Add metrics recording for deep search.
+      [self.metricsRecorder
+          recordComposeboxFocusResultedInNavigation:_inNavigation
+                                    withAttachments:!_items.empty
+                                        requestType:AutocompleteRequestType::
+                                                        kImageGeneration];
+      break;
   }
 }
 
@@ -1720,11 +1729,13 @@ CreateInputDataFromAnnotatedPageContent(
       [self sendText:[NSString cr_fromString16:text]];
       break;
     case ComposeboxMode::kCanvas:
-      // TODO(crbug.com/477244841): Add metrics recording for canvas.
+      [self.metricsRecorder recordAutocompleteRequestTypeAtNavigation:
+                                AutocompleteRequestType::kCanvas];
       [self sendText:[NSString cr_fromString16:text]];
       break;
     case ComposeboxMode::kDeepSearch:
-      // TODO(crbug.com/481280186): Add metrics recording for deep search.
+      [self.metricsRecorder recordAutocompleteRequestTypeAtNavigation:
+                                AutocompleteRequestType::kDeepSearch];
       [self sendText:[NSString cr_fromString16:text]];
       break;
   }
