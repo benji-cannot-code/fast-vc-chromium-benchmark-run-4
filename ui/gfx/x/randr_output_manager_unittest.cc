9e26d986819732080e296c4eec61c12e3093abb5
Jamie Walch <jamiewalch@chromium.org>
2026-02-12 11:28:26 -0800

    Handle unknown or inactive screen IDs when resizing.
    We are adding a feature to CRD to apply a display config on connect (the
    motivation being that two clients with different physical displays would
    save different host display configs and apply them on connect). In order
    to do this, we need to save the last-sent layout at the client. The
    problem with this is that any displays that are no longer present at the
    host will be ignored, but in fact we want them to be added.
    
    This CL updates the Wayland and X11 code that applies a display layout
    so that unknown display ids are treated the same as missing ones.
    
    Change-Id: Ia087af069304269f03046d3c8e9a2d985b4d7756
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7557558
    Commit-Queue: Jamie Walch <jamiewalch@chromium.org>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1584145}

--- ui/gfx/x/randr_output_manager_unittest.cc
+++ ui/gfx/x/randr_output_manager_unittest.cc
@@ -1,4 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include "ui/gfx/x/randr_output_manager.h"
 
 #include <optional>
@@ -53,11 +57,15 @@ TEST(RandrOutputManagerTest, CalculateDisplayLayoutDiff) {
        {234, gfx::Rect(3450, 1230, 2340, 2000), gfx::Vector2d(100, 96)},
        // Unchanged.
        {345, gfx::Rect(0, 1230, 3450, 3450), gfx::Vector2d(96, 96)},
+       // Unknown.
+       {456, gfx::Rect(5670, 6780, 7890, 7890), gfx::Vector2d(192, 192)},
        // New.
        {{}, gfx::Rect(3450, 3450, 4560, 4560), gfx::Vector2d(192, 192)}});
   auto diff = CalculateDisplayLayoutDiff(current_displays, new_layout);
 
   x11::RandRMonitorLayout expected_new_displays;
+  expected_new_displays.configs.push_back(RandRMonitorConfig(
+      456, gfx::Rect(5670, 6780, 7890, 7890), gfx::Vector2d(192, 192)));
   expected_new_displays.configs.push_back(RandRMonitorConfig(
       {}, gfx::Rect(3450, 3450, 4560, 4560), gfx::Vector2d(192, 192)));
   EXPECT_EQ(diff.new_displays, expected_new_displays);
