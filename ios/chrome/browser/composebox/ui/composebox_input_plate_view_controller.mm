fd884e567edd4f0ef8c87edc7c5488484d18b629
Ameur Hosni <ameurhosni@google.com>
2026-02-12 03:32:41 -0800

    [IOS][AIM] Fix deep search button visual bug
    This CL fixes a visual bug where the deep search button is compressed
    when switching from AI mode.
    
    Before: http://shortn/_9NNKdcl2dT
    After: http://shortn/_tknvJqlrjf
    
    Fixed: 483679895
    Change-Id: I49834839a52006da6f5dc80720573004c5286b20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7567268
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Reviewed-by: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Auto-Submit: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1583861}

--- ios/chrome/browser/composebox/ui/composebox_input_plate_view_controller.mm
+++ ios/chrome/browser/composebox/ui/composebox_input_plate_view_controller.mm
@@ -2043,6 +2043,10 @@ UIImage* SendButtonImage(BOOL highlighted, ComposeboxTheme* theme) {
                                                kXButtonWidthInButton]
   ]];
 
+  [button
+      setContentCompressionResistancePriority:UILayoutPriorityRequired
+                                      forAxis:UILayoutConstraintAxisHorizontal];
+
   [self setupXMarkInButton:button];
 
   return button;
