eb6a5f717783acb9c5186c30a208c1c18f623b56
Nan Lin <linnan@chromium.org>
2026-02-12 14:47:46 -0800

    Fix flakiness for PasswordRedactionMultiSourcePageContextFetcherBrowserTest.BasicRedactionInIframe
    Fixed: 483705172
    Change-Id: Iecc61ef3c5f5d80dcac70b1192a3b0a6a632022c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7571266
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1584252}

--- chrome/browser/page_content_annotations/multi_source_page_context_fetcher_browsertest.cc
+++ chrome/browser/page_content_annotations/multi_source_page_context_fetcher_browsertest.cc
@@ -585,6 +585,7 @@ IN_PROC_BROWSER_TEST_F(
       browser(),
       GetURL(kHostA, "/password_in_iframe.html?domain=/cross-site/b.test/")));
 
+  // Wait for main frame layout/render.
   {
     base::test::TestFuture<bool> future;
     web_contents()
@@ -592,6 +593,14 @@ IN_PROC_BROWSER_TEST_F(
         ->GetRenderWidgetHost()
         ->InsertVisualStateCallback(future.GetCallback());
     ASSERT_TRUE(future.Wait()) << "Timeout waiting for syncing with renderer";
+  }
+
+  // Wait for cross-site subframe layout/render.
+  {
+    base::test::TestFuture<bool> sub_future;
+    GetSubframe()->GetRenderWidgetHost()->InsertVisualStateCallback(
+        sub_future.GetCallback());
+    ASSERT_TRUE(sub_future.Wait());
   }
 
   base::test::TestFuture<FetchPageContextResultCallbackArg> future;
