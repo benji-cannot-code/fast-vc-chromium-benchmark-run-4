6a6e04f703ef6513aba3073266e6f757d950b081
Kevin McNee <mcnee@chromium.org>
2026-02-11 17:25:23 -0800

    Refactor MayActOnUrlBlockReason to ActionResultCode conversion
    We now have a dedicated function to do this conversion. We also now
    propagate a result code to DidFinishAsyncSafetyChecks.
    
    Bug: b:482599595
    Change-Id: I5a4ba785ff8904cff3200ad4cd5b3cacea6e3d2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7562556
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Auto-Submit: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1583640}

--- chrome/browser/actor/tools/validate_url_util.cc
+++ chrome/browser/actor/tools/validate_url_util.cc
@@ -19,7 +19,8 @@ namespace {
 mojom::ActionResultPtr UrlCheckToActionResult(MayActOnUrlBlockReason reason) {
   return reason == MayActOnUrlBlockReason::kAllowed
              ? MakeOkResult()
-             : MakeResult(mojom::ActionResultCode::kUrlBlocked);
+             : MakeResult(
+                   BlockReasonToResultCode(reason, /*for_navigation=*/false));
 }
 
 }  // namespace
