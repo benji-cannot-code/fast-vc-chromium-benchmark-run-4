a01c637d2f677fe90c7edb733d8087f2201a4300
Daniel White <danieltwhite@google.com>
2026-02-12 14:48:19 -0800

    [iOS] Remove TraitCollectionSetForTraits invocation
    In this CL, the TraitCollectionSetForTraits was removed from these
    files. This CL is part of a wider effort to remove the
    TraitCollectionSetForTraits function from the codebase.
    
    Bug: 483670636
    Change-Id: I0ad2ea32d04d088ba1218d521bd37a1c07c9c3c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7567764
    Auto-Submit: Daniel White <danieltwhite@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1584253}

--- ios/chrome/browser/download/ui/radial_progress_view.mm
+++ ios/chrome/browser/download/ui/radial_progress_view.mm
@@ -50,11 +50,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [self.trackLayer addSublayer:self.progressLayer];
     [self updateProgressLayer];
 
-    NSArray<UITrait>* traits = TraitCollectionSetForTraits(@[
+    NSArray<UITrait>* traits = @[
       UITraitUserInterfaceIdiom.class, UITraitUserInterfaceStyle.class,
       UITraitDisplayGamut.class, UITraitAccessibilityContrast.class,
       UITraitUserInterfaceLevel.class
-    ]);
+    ];
     __weak __typeof(self) weakSelf = self;
     UITraitChangeHandler handler = ^(id<UITraitEnvironment> traitEnvironment,
                                      UITraitCollection* previousCollection) {
