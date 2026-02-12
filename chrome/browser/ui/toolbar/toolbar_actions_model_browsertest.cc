74fac5c1854de91d0827126f0cc0a3da891d60b8
Eva Su <evasu@chromium.org>
2026-02-12 15:03:27 -0800

    [Extensions] Handle no extensions case for TAM::IsRestrictedUrl()
    Make sure site is not restricted if no extensions are installed.
    
    Bug: NA
    Change-Id: I32e87c59aac895e4a455d20752720b07334a00c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7571237
    Commit-Queue: Eva Su <evasu@chromium.org>
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1584278}

--- chrome/browser/ui/toolbar/toolbar_actions_model_browsertest.cc
+++ chrome/browser/ui/toolbar/toolbar_actions_model_browsertest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_registry.h"
 #include "extensions/common/extension_set.h"
 #include "testing/gmock/include/gmock/gmock.h"
+#include "url/gurl.h"
 
 namespace {
 
@@ -148,4 +149,15 @@ IN_PROC_BROWSER_TEST_F(ToolbarActionsModelBrowserTest, PinnedStatePersistence) {
                   extension1->id(), extension2->id(), extension3->id()));
   EXPECT_THAT(toolbar_model()->pinned_action_ids(),
               ::testing::ElementsAre(extension3->id(), extension2->id()));
+}
+
+// Test that a site is NOT restricted or policy-blocked when there are no
+// extensions installed.
+IN_PROC_BROWSER_TEST_F(ToolbarActionsModelBrowserTest,
+                       ActionsModelRestrictedUrlsWithNoExtensions) {
+  EXPECT_TRUE(toolbar_model()->action_ids().empty());
+
+  GURL example_url("http://www.example.com");
+  EXPECT_FALSE(toolbar_model()->IsRestrictedUrl(example_url));
+  EXPECT_FALSE(toolbar_model()->IsPolicyBlockedHost(example_url));
 }
