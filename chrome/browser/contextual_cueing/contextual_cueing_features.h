fbfda31599c6259b93e02749eed247e5fa869cad
Benedict Wong <benedictwong@google.com>
2026-02-12 14:28:03 -0800

    [glic]: Add auto-open capabilities
    This change adds new options to auto-open the sidepanel, in
    addition to the tabbar entrypoint. Additionally, this also adds a
    new invocation source, to support tracking of auto-opens separately.
    
    This is encoded as a eligibility bit, with fallback to tabbar entrypoint
    if the auto-open flags are disabled, or if any local circumstances
    prevent auto-open.
    
    Bypass-Glic-Api-Compatibility-Check: Adding new field
    Bug: 482402332
    Change-Id: I01fdffe7d83b0f08d32c7d0bc61a66de6dfa226f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7552113
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Benedict Wong <benedictwong@google.com>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1584220}

--- chrome/browser/contextual_cueing/contextual_cueing_features.h
+++ chrome/browser/contextual_cueing/contextual_cueing_features.h
@@ -14,6 +14,7 @@ namespace contextual_cueing {
 BASE_DECLARE_FEATURE(kContextualCueing);
 BASE_DECLARE_FEATURE(kGlicZeroStateSuggestions);
 BASE_DECLARE_FEATURE(kZeroStateSuggestionsUseLegion);
+BASE_DECLARE_FEATURE(kEnableAutoOpenGlicSidePanel);
 
 // Whether contextual cueing is enabled.
 //
@@ -96,6 +97,7 @@ extern const base::FeatureParam<int> kMaxPinnedPagesForTriggeringSuggestions;
 
 // Timeout before giving up on getting context from a page.
 extern const base::FeatureParam<base::TimeDelta> kZSSPageContextTimeout;
+
 }  // namespace contextual_cueing
 
 #endif  // CHROME_BROWSER_CONTEXTUAL_CUEING_CONTEXTUAL_CUEING_FEATURES_H_
