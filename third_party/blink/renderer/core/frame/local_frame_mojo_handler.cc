3c62d364e795727643a349ee76f1166fe111b42f
Lingqi Chi <lingqi@chromium.org>
2026-02-11 22:21:15 -0800

    [prerender] add trace event to LocalFrameMojoHandler::UpdatePrerenderURL
    Bug: 483220218
    Change-Id: Iaeb3aa00e3af360ce5f52a66553174a58a8e4706
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7556375
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1583754}

--- third_party/blink/renderer/core/frame/local_frame_mojo_handler.cc
+++ third_party/blink/renderer/core/frame/local_frame_mojo_handler.cc
@@ -1485,6 +1485,7 @@ void LocalFrameMojoHandler::UpdatePrerenderURL(
   CHECK(SecurityOrigin::Create(matched_url)
             ->IsSameOriginWith(
                 &*GetDocument()->GetExecutionContext()->GetSecurityOrigin()));
+  TRACE_EVENT("navigation", "LocalFrameMojoHandler::UpdatePrerenderURL");
   auto* params = MakeGarbageCollected<NavigateEventDispatchParams>(
       matched_url, NavigateEventType::kPrerenderNoVarySearchActivation,
       WebFrameLoadType::kReplaceCurrentItem);
