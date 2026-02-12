092b5d67d28edf0ed9750de66842398470f3b270
Andrew Grieve <agrieve@chromium.org>
2026-02-12 14:53:03 -0800

    Remove ObservableSupplier.addObserver()
    The call is an alias for "addSyncObserverAndPostIfNonNull()", and all
    existing usages have been migrated in prior commits.
    
    Bug: 483038528
    Change-Id: I4f228a303853febd2899588f5b356654e2052c40
    Cq-Include-Trybots: luci.chrome.try:android-internal-dbg,android-internal-binary-size
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7573254
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1584261}

--- base/android/java/src/org/chromium/base/supplier/NonNullObservableSupplier.java
+++ base/android/java/src/org/chromium/base/supplier/NonNullObservableSupplier.java
@@ -37,10 +37,5 @@ public interface NonNullObservableSupplier<T> extends Supplier<T>, MonotonicObse
     }
 
     @Override
-    default T addObserver(Callback<T> obs) {
-        return addSyncObserverAndPostIfNonNull(obs);
-    }
-
-    @Override
     T addObserver(Callback<T> obs, @NotifyBehavior int behavior);
 }
