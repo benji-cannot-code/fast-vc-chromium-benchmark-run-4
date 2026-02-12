06c595e07003ebeb2902645531b2ae3ce734c837
Saifuddin Hitawala <hitawala@chromium.org>
2026-02-12 08:46:12 -0800

    [ozone][gpu] Remove GetSupportedFormatsForGLNativePixmapImport
    Instead of a vector of supported formats, add bools for checking if
    Native Pixmap of NV12/P010 formats can be imported into GL for
    media/mojo client. This can be done directly through
    CanImportNativePixmap checks on GLOzone. This helps remove
    GetSupportedFormatsForGLNativePixmapImport.
    
    Eventually, we can look at moving supports_nv12_gl_native_pixmap
    and supports_p010_gl_native_pixmap to SharedImageCapabilities
    but also converging these with those for allocation and texturing.
    
    Bug: 482216427
    Change-Id: I1492ecafc91d840d03221e1115d2bc47e7ea6a32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7562839
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1584024}

--- ui/ozone/public/surface_factory_ozone.cc
+++ ui/ozone/public/surface_factory_ozone.cc
@@ -23,28 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/vulkan/vulkan_instance.h"
 #endif
 
-namespace {
-
-inline constexpr auto kMappableSharedImageFormats = {
-    viz::SinglePlaneFormat::kR_8,
-    viz::SinglePlaneFormat::kRG_88,
-    viz::SinglePlaneFormat::kR_16,
-    viz::SinglePlaneFormat::kRG_1616,
-    viz::SinglePlaneFormat::kBGR_565,
-    viz::SinglePlaneFormat::kRGBA_4444,
-    viz::SinglePlaneFormat::kRGBA_8888,
-    viz::SinglePlaneFormat::kRGBX_8888,
-    viz::SinglePlaneFormat::kBGRA_8888,
-    viz::SinglePlaneFormat::kBGRX_8888,
-    viz::SinglePlaneFormat::kRGBA_1010102,
-    viz::SinglePlaneFormat::kBGRA_1010102,
-    viz::SinglePlaneFormat::kRGBA_F16,
-    viz::MultiPlaneFormat::kYV12,
-    viz::MultiPlaneFormat::kNV12,
-    viz::MultiPlaneFormat::kP010,
-    viz::MultiPlaneFormat::kNV12A};
-}
-
 namespace ui {
 
 SurfaceFactoryOzone::SurfaceFactoryOzone() = default;
@@ -152,22 +130,6 @@ void SurfaceFactoryOzone::SetDrmModifiersFilter(
 bool SurfaceFactoryOzone::IsFormatSupportedForTexturing(
     viz::SharedImageFormat format) const {
   return false;
-}
-
-std::vector<viz::SharedImageFormat>
-SurfaceFactoryOzone::GetSupportedFormatsForGLNativePixmapImport() {
-  std::vector<viz::SharedImageFormat> supported_formats;
-  auto* gl_ozone = GetCurrentGLOzone();
-  if (!gl_ozone) {
-    return supported_formats;
-  }
-
-  for (auto format : kMappableSharedImageFormats) {
-    if (gl_ozone->CanImportNativePixmap(format)) {
-      supported_formats.push_back(format);
-    }
-  }
-  return supported_formats;
 }
 
 std::optional<viz::SharedImageFormat>
