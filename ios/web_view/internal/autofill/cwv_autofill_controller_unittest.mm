c82bbbb0bc180ae896cdb5010004a6cdbebef4eb
Vincent Boisselle <vincb@google.com>
2026-02-12 11:39:12 -0800

    Revert "[ios] Remove `userInitiated` arg"
    This reverts commit 8bc305faa0cac7e2a3912b9d2da1bbc3f3c0eee8.
    
    Reason for revert: Will break embedders
    
    Original change's description:
    > [ios] Remove `userInitiated` arg
    >
    > Removes the argument as it was not used by any -didSubmitFormWithName
    > implementation.
    >
    > Bug: 477630132
    > Change-Id: I04db9003db65fcc718161992fac819549c2aeed2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7570923
    > Commit-Queue: Julia Sobiech <jsobiech@google.com>
    > Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1584001}
    
    Bug: 477630132
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I6c8947ad31b7569bb05204a0fbd5a09610b09673
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7572710
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Auto-Submit: Vincent Boisselle <vincb@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1584154}

--- ios/web_view/internal/autofill/cwv_autofill_controller_unittest.mm
+++ ios/web_view/internal/autofill/cwv_autofill_controller_unittest.mm
@@ -466,6 +466,7 @@ TEST_F(CWVAutofillControllerTest, SubmitCallback) {
   [[delegate expect] autofillController:autofill_controller_
                   didSubmitFormWithName:kTestFormName
                                 frameID:frame_id_
+                          userInitiated:YES
                          perfectFilling:YES];
   auto frame = web::FakeWebFrame::CreateMainWebFrame(GURL());
   autofill::FormData test_form_data;
@@ -479,6 +480,7 @@ TEST_F(CWVAutofillControllerTest, SubmitCallback) {
   [[delegate expect] autofillController:autofill_controller_
                   didSubmitFormWithName:kTestFormName
                                 frameID:frame_id_
+                          userInitiated:NO
                          perfectFilling:NO];
 
   form_activity_tab_helper_->DocumentSubmitted(
@@ -499,6 +501,7 @@ TEST_F(CWVAutofillControllerTest, SubmitCallbackAcrossIframes) {
          autofillController:autofill_controller_
       didSubmitFormWithName:kTestFormName
                     frameID:base::SysUTF8ToNSString(web::kMainFakeFrameId)
+              userInitiated:YES
              perfectFilling:YES]);
 
   auto frame = web::FakeWebFrame::CreateMainWebFrame(GURL());
