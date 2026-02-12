a42bdac0ab95e6e798b8b8964409673ba9e79e5a
Justin Cohen <justincohen@google.com>
2026-02-11 16:28:11 -0800

    Re-enable BEContextMenuConfiguration.
    Refactor crw_context_menu_controller to improve stability and
    maintainability. Additionally, limit the synchronous run loop to 0.5s.
    
    Both changes are behind a feature flag.
    
    Bug: 445913861
    Change-Id: Idd83bd564963be8a0ab16333de97f4e66d5ea5d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7545862
    Commit-Queue: Justin Cohen <justincohen@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1583614}

--- ios/chrome/browser/web/model/web_state_delegate_browser_agent.h
+++ ios/chrome/browser/web/model/web_state_delegate_browser_agent.h
@@ -89,6 +89,10 @@ class WebStateDelegateBrowserAgent
       web::WebState* source,
       const web::ContextMenuParams& params,
       void (^completion_handler)(UIContextMenuConfiguration*)) override;
+  UIContextMenuConfiguration* GetCustomContextMenuConfiguration() override;
+  void ContextMenuConfigurationLoaded(
+      UIContextMenuConfiguration* configuration,
+      UIContextMenuConfiguration* update) override;
   void ContextMenuWillCommitWithAnimator(
       web::WebState* source,
       id<UIContextMenuInteractionCommitAnimating> animator) override;
