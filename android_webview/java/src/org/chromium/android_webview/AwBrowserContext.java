03f37e9030357e6c1d816ea97743eee5e7c1cdf0
Barkin Unal <barkinunal@chromium.org>
2026-02-12 07:22:55 -0800

    [WebView] Allow optional max prerender count in AwBrowserContext.
    Modify AwBrowserContext::SetAllowedPrerenderingCount to accept
    std::optional<int> for the maximum number of allowed prerenders.
    
    This change enables the Java-side `AwBrowserContext.setMaxPrerenders` to
    pass a null value, which will reset the allowed prerendering count to
    the default value of 2. Previously, only non-negative integer values
    could be passed. The native implementation in aw_browser_context.cc now
    handles std::nullopt by using a default of 2.
    
    New C++ unit tests have been added to aw_browser_context_unittest.cc to
    verify:
    
    The initial default value. Setting valid counts. The upper cap
    (MAX_ALLOWED_PRERENDERING_COUNT). The reset-to-default behavior when
    std::nullopt is provided. A new Java unit test
    (AwBrowserContextTest.java) is also included to specifically test that
    calling AwBrowserContext.setMaxPrerenders(null) does not cause a crash,
    ensuring robustness for Java callers. This work supports the broader
    effort to enhance prerendering capabilities in Android WebView.
    
    Bug: 466301565
    Test: Added new C++ unit tests in aw_browser_context_unittest.cc and a Java unit test in AwBrowserContextTest.java to cover optional/null input handling for max prerenders.
    Change-Id: If36fb7ae4837197cfa75898b43538354b5b7c4e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7550552
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Auto-Submit: Barkin Unal <barkinunal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1583954}

--- android_webview/java/src/org/chromium/android_webview/AwBrowserContext.java
+++ android_webview/java/src/org/chromium/android_webview/AwBrowserContext.java
@@ -429,7 +429,7 @@ public class AwBrowserContext implements BrowserContextHandle {
     }
 
     @UiThread
-    public void setMaxPrerenders(int maxPrerenders) {
+    public void setMaxPrerenders(@Nullable Integer maxPrerenders) {
         AwBrowserContextJni.get()
                 .setAllowedPrerenderingCount(mNativeAwBrowserContext, maxPrerenders);
     }
@@ -529,7 +529,9 @@ public class AwBrowserContext implements BrowserContextHandle {
         void setServiceWorkerIoThreadClient(
                 long nativeAwBrowserContext, AwContentsIoThreadClient ioThreadClient);
 
-        void setAllowedPrerenderingCount(long nativeAwBrowserContext, int maxPrerenders);
+        void setAllowedPrerenderingCount(
+                long nativeAwBrowserContext,
+                @JniType("std::optional<int>") @Nullable Integer maxPrerenders);
 
         @JniType("std::vector<std::string>")
         List<String> setOriginMatchedHeader(
