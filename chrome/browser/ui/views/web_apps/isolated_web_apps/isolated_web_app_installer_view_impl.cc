4309726a43e49a5a5cb8461db1b070b395891521
Simon Hangl <simonha@google.com>
2026-02-12 09:03:49 -0800

    Refactor IsolatedWebAppInstallerViewImpl::Dim
    This CL splits the function into separate functions for dimming /
    undimming.
    
    Bug: 481335542
    Change-Id: I2f1a28c5a03d8f44d16dc8445a357da54eb3d5c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7571942
    Commit-Queue: Simon Hangl <simonha@google.com>
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Cr-Commit-Position: refs/heads/main@{#1584032}

--- chrome/browser/ui/views/web_apps/isolated_web_apps/isolated_web_app_installer_view_impl.cc
+++ chrome/browser/ui/views/web_apps/isolated_web_apps/isolated_web_app_installer_view_impl.cc
@@ -447,25 +447,12 @@ class DimOverlayView : public views::View {
 BEGIN_METADATA(DimOverlayView)
 END_METADATA
 
-void IsolatedWebAppInstallerViewImpl::Dim(
-    bool dim,
+void IsolatedWebAppInstallerViewImpl::ApplyDim(
     const views::DialogDelegate* dialog_delegate) {
-  CHECK(!dim || dialog_delegate);
+  CHECK(dialog_delegate);
 
   views::View* parent_view = parent();
   CHECK(parent_view);
-
-  // Undim: remove all |DimOverlayView|
-  if (!dim) {
-    for (views::View* child : parent_view->children()) {
-      if (child->GetObjectName().compare("DimOverlayView") == 0) {
-        // |RemoveChildViewT()| returns the ownership of the child, which gets
-        // dropped, effectively deleting the child from memory.
-        parent_view->RemoveChildViewT(child);
-      }
-    }
-    return;
-  }
 
   auto dim_overlay = std::make_unique<DimOverlayView>(
       CHECK_DEREF(dialog_delegate).GetCornerRadius());
@@ -480,6 +467,17 @@ void IsolatedWebAppInstallerViewImpl::Dim(
 
   // Dim: add a |DimOverlayView| as the last child.
   parent_view->AddChildView((std::move(dim_overlay)));
+}
+
+void IsolatedWebAppInstallerViewImpl::RemoveDim() {
+  views::View* parent_view = parent();
+  for (views::View* child : parent_view->children()) {
+    if (child->GetObjectName().compare("DimOverlayView") == 0) {
+      // |RemoveChildViewT()| returns the ownership of the child, which gets
+      // dropped, effectively deleting the child from memory.
+      parent_view->RemoveChildViewT(child);
+    }
+  }
 }
 
 // static
@@ -600,7 +598,7 @@ void IsolatedWebAppInstallerViewImpl::ShowInstallSuccessScreen(
 views::Widget* IsolatedWebAppInstallerViewImpl::ShowDialog(
     const IsolatedWebAppInstallerModel::Dialog& dialog,
     const views::DialogDelegate* dialog_delegate) {
-  Dim(true, dialog_delegate);
+  ApplyDim(dialog_delegate);
   return std::visit(
       absl::Overload{
           [this](const IsolatedWebAppInstallerModel::BundleInvalidDialog&) {
@@ -767,7 +765,7 @@ void IsolatedWebAppInstallerViewImpl::ShowChildView(views::View* view) {
 
 void IsolatedWebAppInstallerViewImpl::OnChildDialogDestroying() {
   dialog_visible_ = false;
-  Dim(false, /*dialog_delegate=*/nullptr);
+  RemoveDim();
   delegate_->OnChildDialogDestroying();
 }
 
