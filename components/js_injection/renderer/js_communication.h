83f9a1178af718206440462016d9b7919fb483f4
Dave Tapuska <dtapuska@chromium.org>
2026-02-12 09:47:21 -0800

    [webview] Fix an issue around lifecycle of WebLocalFrame
    Ensure that we monitor destruction of the JsCommunication object.
    
    Bug: 481920229
    Change-Id: Ibbc6eacf0cb44fc737b0228c5d231d6fddf0caea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7567944
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1584064}

--- components/js_injection/renderer/js_communication.h
+++ components/js_injection/renderer/js_communication.h
@@ -62,6 +62,10 @@ class JsCommunication
   class JsObjectInfo;
   struct JavaScriptExecutable;
 
+  static void RunScriptsInternal(
+      base::WeakPtr<JsCommunication> js_communication,
+      mojom::DocumentInjectionTime injection_time);
+
   void BindPendingReceiver(
       mojo::PendingAssociatedReceiver<mojom::JsCommunication> pending_receiver);
 
@@ -81,6 +85,7 @@ class JsCommunication
   mojo::AssociatedRemote<mojom::JsObjectsClient> client_remote_;
 
   base::WeakPtrFactory<JsCommunication> weak_ptr_factory_for_bindings_{this};
+  base::WeakPtrFactory<JsCommunication> weak_ptr_factory_{this};
 };
 
 }  // namespace js_injection
