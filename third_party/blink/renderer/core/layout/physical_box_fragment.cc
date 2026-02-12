53c2d4000464d46f84f9f9d5d3304c575c894a84
Koji Ishii <kojii@chromium.org>
2026-02-11 21:45:51 -0800

    [perf] Follow up to `AssertFragmentTree`
    This patch changes functions used only under
    `EXPENSIVE_DCHECKS_ARE_ON()` (changed so at crrev.com/c/7558532)
    to be available only when `EXPENSIVE_DCHECKS_ARE_ON()`.
    
    Bug: 483109220
    Change-Id: I0c5e09e98c07a8230a7fe672e655c4365c4cce7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7564069
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1583746}

--- third_party/blink/renderer/core/layout/physical_box_fragment.cc
+++ third_party/blink/renderer/core/layout/physical_box_fragment.cc
@@ -1842,6 +1842,7 @@ void PhysicalBoxFragment::CheckIntegrity() const {
   }
 }
 
+#if EXPENSIVE_DCHECKS_ARE_ON()
 void PhysicalBoxFragment::AssertFragmentTreeSelf() const {
   DCHECK(!IsInlineBox());
   DCHECK(OwnerLayoutBox());
@@ -1876,7 +1877,8 @@ void PhysicalBoxFragment::AssertFragmentTreeChildren(
     }
   }
 }
-#endif
+#endif  // EXPENSIVE_DCHECKS_ARE_ON()
+#endif  // DCHECK_IS_ON()
 
 void PhysicalBoxFragment::TraceAfterDispatch(Visitor* visitor) const {
   visitor->Trace(children_);
