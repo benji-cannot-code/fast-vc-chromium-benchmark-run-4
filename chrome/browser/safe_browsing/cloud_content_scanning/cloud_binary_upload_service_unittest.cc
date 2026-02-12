5fd0769921f831d354d1876e0235af0b8bc3b2bc
Alex Chen <alxchn@google.com>
2026-02-12 07:27:49 -0800

    Use ConnectorUploadRequest factory in CloudBinaryUploadServiceTest
    Since MultipartUploadRequest and ResumableUploadRequest both inherit the
    same static RegisterFactoryForTests function, use the base class
    function to improve code readability.
    
    Change-Id: I03454a09c58482f85b3ba6156bb79cdb572366e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7569242
    Reviewed-by: Michał Kaczmarczyk <mickaczmarczyk@google.com>
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Commit-Queue: Alex Chen <alxchn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1583957}

--- chrome/browser/safe_browsing/cloud_content_scanning/cloud_binary_upload_service_unittest.cc
+++ chrome/browser/safe_browsing/cloud_content_scanning/cloud_binary_upload_service_unittest.cc
@@ -195,7 +195,7 @@ class CloudBinaryUploadServiceTest : public ::testing::Test {
  public:
   CloudBinaryUploadServiceTest()
       : task_environment_(base::test::TaskEnvironment::TimeSource::MOCK_TIME) {
-    MultipartUploadRequest::RegisterFactoryForTests(&fake_factory_);
+    ConnectorUploadRequest::RegisterFactoryForTests(&fake_factory_);
 
     // Since we have mocked the MultipartUploadRequest, we don't need a
     // URLLoaderFactory, so pass nullptr here.
@@ -205,8 +205,7 @@ class CloudBinaryUploadServiceTest : public ::testing::Test {
   }
 
   void TearDown() override {
-    ResumableUploadRequest::RegisterFactoryForTests(nullptr);
-    MultipartUploadRequest::RegisterFactoryForTests(nullptr);
+    ConnectorUploadRequest::RegisterFactoryForTests(nullptr);
   }
 
   void ExpectNetworkResponse(
@@ -514,11 +513,6 @@ TEST_F(CloudBinaryUploadServiceTest, PasteSucceeds) {
 }
 
 TEST_F(CloudBinaryUploadServiceTest, PasteImageResumableSucceeds) {
-  FakeConnectorUploadRequestFactory resumable_factory{
-      /*should_succeed=*/true, enterprise_connectors::ContentAnalysisResponse(),
-      net::HTTP_OK};
-  ResumableUploadRequest::RegisterFactoryForTests(&resumable_factory);
-
   enterprise_connectors::ScanRequestUploadResult scanning_result;
   enterprise_connectors::ContentAnalysisResponse scanning_response;
 
@@ -547,18 +541,16 @@ TEST_F(CloudBinaryUploadServiceTest, PasteImageResumableSucceeds) {
   UploadForDeepScanning(std::move(request));
   content::RunAllTasksUntilIdle();
 
-  EXPECT_TRUE(resumable_factory.called());
+  EXPECT_TRUE(fake_factory_.called());
   EXPECT_EQ(scanning_result,
             enterprise_connectors::ScanRequestUploadResult::kSuccess);
-
-  ResumableUploadRequest::RegisterFactoryForTests(nullptr);
 }
 
 TEST_F(CloudBinaryUploadServiceTest, PasteImageResumableFails) {
-  FakeConnectorUploadRequestFactory resumable_factory{
+  FakeConnectorUploadRequestFactory fake_factory{
       /*should_succeed=*/false,
       enterprise_connectors::ContentAnalysisResponse(), net::HTTP_OK};
-  ResumableUploadRequest::RegisterFactoryForTests(&resumable_factory);
+  ConnectorUploadRequest::RegisterFactoryForTests(&fake_factory);
 
   enterprise_connectors::ScanRequestUploadResult scanning_result;
   enterprise_connectors::ContentAnalysisResponse scanning_response;
@@ -588,11 +580,9 @@ TEST_F(CloudBinaryUploadServiceTest, PasteImageResumableFails) {
   UploadForDeepScanning(std::move(request));
   content::RunAllTasksUntilIdle();
 
-  EXPECT_TRUE(resumable_factory.called());
+  EXPECT_TRUE(fake_factory.called());
   EXPECT_EQ(scanning_result,
             enterprise_connectors::ScanRequestUploadResult::kUploadFailure);
-
-  ResumableUploadRequest::RegisterFactoryForTests(nullptr);
 }
 
 TEST_F(CloudBinaryUploadServiceTest, FailsWhenUploadFails) {
