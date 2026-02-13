b5efe0bc4f00dca3c55d49cdb2c5c51571fc1e5e
Colin Blundell <blundell@chromium.org>
2026-02-12 23:38:29 -0800

    [Blink] Remove unneeded EndWriteAccess() call in CNon2DRPSI
    EndWriteAccess() is a no-op if there is no current write access [1]. In
    this flow [2], there are two EndWriteAccess() calls separated by a
    WillDrawInternal() call. WillDrawInternal() does not take a write
    access (done only via calling EnsureWriteAccess() [3]).
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc;l=532-534;drc=b610a104128a46d031dc5ae3e6d486430b61efa6;bpv=1;bpt=1
    [2] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc;l=724-726;bpv=1;bpt=1?q=OverwriteImage&ss=chromium
    [3] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc;l=512;drc=b610a104128a46d031dc5ae3e6d486430b61efa6;bpv=1;bpt=1
    
    Bug: 352263194
    Change-Id: I534dae3223a90ce9ec53d5bb04c4a94f4e2e7d27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7566926
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1584463}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
@@ -728,7 +728,6 @@ bool CanvasNon2DResourceProviderSharedImage::OverwriteImage(
 
   EndWriteAccess();
   auto access = WillDrawInternal();
-  EndWriteAccess();
 
   auto dst_client_si = resource()->GetClientSharedImage();
   if (!dst_client_si) {
