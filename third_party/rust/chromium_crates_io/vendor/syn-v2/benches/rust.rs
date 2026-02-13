594959a816a64f11537eeee8c9d37222f5745f17
Jon Toohill <jtoohill@google.com>
2026-02-13 15:07:27 -0800

    Roll syn: 2.0.114 => 2.0.115 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * syn: 2.0.114 => 2.0.115; https://docs.rs/crate/syn/2.0.115
    
    Bug: None
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Change-Id: I812a5dfeb7b283cb54a792310e6c8a7d9c713d9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7573214
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Jon Toohill <jtoohill@google.com>
    Cr-Commit-Position: refs/heads/main@{#1584948}

--- third_party/rust/chromium_crates_io/vendor/syn-v2/benches/rust.rs
+++ third_party/rust/chromium_crates_io/vendor/syn-v2/benches/rust.rs
@@ -61,7 +61,6 @@ mod librustc_parse {
 
     use crate::repo;
     use rustc_errors::emitter::Emitter;
-    use rustc_errors::registry::Registry;
     use rustc_errors::translation::Translator;
     use rustc_errors::{DiagCtxt, DiagInner};
     use rustc_parse::lexer::StripTokens;
@@ -75,7 +74,7 @@ mod librustc_parse {
         struct SilentEmitter;
 
         impl Emitter for SilentEmitter {
-            fn emit_diagnostic(&mut self, _diag: DiagInner, _registry: &Registry) {}
+            fn emit_diagnostic(&mut self, _diag: DiagInner) {}
             fn source_map(&self) -> Option<&SourceMap> {
                 None
             }
--- third_party/rust/chromium_crates_io/vendor/syn-v2/src/lib.rs
+++ third_party/rust/chromium_crates_io/vendor/syn-v2/src/lib.rs
@@ -250,7 +250,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //!   dynamic library libproc_macro from rustc toolchain.
 
 #![no_std]
-#![doc(html_root_url = "https://docs.rs/syn/2.0.114")]
+#![doc(html_root_url = "https://docs.rs/syn/2.0.115")]
 #![cfg_attr(docsrs, feature(doc_cfg), doc(auto_cfg = false))]
 #![deny(unsafe_op_in_unsafe_fn)]
 #![allow(non_camel_case_types)]
