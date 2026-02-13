ced1842482074b919a408028151afc3069285749
Thomas Lukaszewicz <tluk@chromium.org>
2026-02-12 18:49:07 -0800

    [bedrock] Fix StartupBrowserCreator browsertests
    Small updates to fix breakage in downstream refactors.
    
    - Removed assertions of counts of closed but not deleted browsers.
      Future refactors will remove closed browsers from browser
      collections / counts observable by client code.
    
    - Updated code to use BrowserCreatedObserver where appropriate.
    
    Bug: 431671320
    Change-Id: I90b9082680a2045fb5f6648d511e1cc372f129d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7573023
    Reviewed-by: Fred Shih <ffred@chromium.org>
    Commit-Queue: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1584394}

--- chrome/browser/ui/startup/startup_browser_creator_triggered_reset_browsertest_win.cc
+++ chrome/browser/ui/startup/startup_browser_creator_triggered_reset_browsertest_win.cc
@@ -43,24 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// Check that there are two browsers. Find the one that is not |browser|.
-BrowserWindowInterface* FindOneOtherBrowser(Browser* browser) {
-  // There should only be one other browser.
-  EXPECT_EQ(2u, chrome::GetBrowserCount(browser->profile()));
-
-  // Find the new browser.
-  BrowserWindowInterface* result = nullptr;
-  ForEachCurrentBrowserWindowInterfaceOrderedByActivation(
-      [browser, &result](BrowserWindowInterface* browser_window_interface) {
-        if (browser_window_interface != browser) {
-          result = browser_window_interface;
-        }
-        return !result;
-      });
-
-  return result;
-}
-
 class MockTriggeredProfileResetter : public TriggeredProfileResetter {
  public:
   MockTriggeredProfileResetter() : TriggeredProfileResetter(nullptr) {}
@@ -149,15 +131,14 @@ IN_PROC_BROWSER_TEST_F(StartupBrowserCreatorTriggeredResetTest,
   MockTriggeredProfileResetter::SetHasResetTrigger(true);
 
   // Do a simple non-process-startup browser launch.
+  ui_test_utils::BrowserCreatedObserver browser_created_observer;
   base::CommandLine dummy(base::CommandLine::NO_PROGRAM);
   StartupBrowserCreatorImpl launch(base::FilePath(), dummy,
                                    chrome::startup::IsFirstRun::kNo);
   launch.Launch(profile, chrome::startup::IsProcessStartup::kNo,
                 /*restore_tabbed_browser=*/true);
 
-  // This should have created a new browser window.  |browser()| is still
-  // around at this point, even though we've closed its window.
-  BrowserWindowInterface* const new_browser = FindOneOtherBrowser(browser());
+  BrowserWindowInterface* const new_browser = browser_created_observer.Wait();
   ASSERT_TRUE(new_browser);
 
   std::vector<GURL> expected_urls(urls);
@@ -194,6 +175,7 @@ IN_PROC_BROWSER_TEST_F(StartupBrowserCreatorTriggeredResetFirstRunTest,
   MockTriggeredProfileResetter::SetHasResetTrigger(true);
 
   // Do a process-startup browser launch.
+  ui_test_utils::BrowserCreatedObserver browser_created_observer;
   base::CommandLine dummy(base::CommandLine::NO_PROGRAM);
   StartupBrowserCreatorImpl launch(base::FilePath(), dummy, &browser_creator,
                                    chrome::startup::IsFirstRun::kYes);
@@ -201,7 +183,7 @@ IN_PROC_BROWSER_TEST_F(StartupBrowserCreatorTriggeredResetFirstRunTest,
                 /*restore_tabbed_browser=*/true);
 
   // This should have created a new browser window.
-  BrowserWindowInterface* const new_browser = FindOneOtherBrowser(browser());
+  BrowserWindowInterface* const new_browser = browser_created_observer.Wait();
   ASSERT_TRUE(new_browser);
 
   // Verify that only the first-run tabs are shown.
@@ -230,6 +212,7 @@ IN_PROC_BROWSER_TEST_F(StartupBrowserCreatorTriggeredResetTest,
   MockTriggeredProfileResetter::SetHasResetTrigger(true);
 
   // Do a simple non-process-startup browser launch.
+  ui_test_utils::BrowserCreatedObserver browser_created_observer;
   base::CommandLine dummy(base::CommandLine::NO_PROGRAM);
   {
     StartupBrowserCreatorImpl launch(base::FilePath(), dummy,
@@ -240,7 +223,7 @@ IN_PROC_BROWSER_TEST_F(StartupBrowserCreatorTriggeredResetTest,
 
   // This should have created a new browser window.  |browser()| is still
   // around at this point, even though we've closed its window.
-  BrowserWindowInterface* const new_browser = FindOneOtherBrowser(browser());
+  BrowserWindowInterface* const new_browser = browser_created_observer.Wait();
   ASSERT_TRUE(new_browser);
 
   // Now create a second browser instance pointing to a different profile.
