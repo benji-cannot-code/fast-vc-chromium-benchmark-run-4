28de18cc611c69bcb2f0cbf560e8002a6e6846e2
Daniel White <danieltwhite@google.com>
2026-02-13 10:34:13 -0800

    [iOS] Remove TraitCollectionSetForTraits invocation
    In this CL, the TraitCollectionSetForTraits was removed from the file.
    This CL is part of a wider effort to remove the
    TraitCollectionSetForTraits function from the codebase.
    
    Bug: 483670636
    Change-Id: I2e74376186294a0e2042d56fa17f7d9b3a9bdab3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7568224
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1584755}

--- ios/chrome/browser/contextual_panel/ui/contextual_sheet_view_controller.mm
+++ ios/chrome/browser/contextual_panel/ui/contextual_sheet_view_controller.mm
@@ -69,9 +69,7 @@ const CGFloat kTopCornerRadius = 10;
       kCALayerMinXMinYCorner | kCALayerMaxXMinYCorner;
   self.view.clipsToBounds = YES;
 
-  NSArray<UITrait>* traits =
-      TraitCollectionSetForTraits(@[ UITraitVerticalSizeClass.class ]);
-  [self registerForTraitChanges:traits
+  [self registerForTraitChanges:@[ UITraitVerticalSizeClass.class ]
                      withAction:@selector(updateUIOnTraitChange)];
 }
 
