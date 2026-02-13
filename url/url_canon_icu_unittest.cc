cbca05eb47a8dae338dd0445d6c7945bb72cc331
Kent Tamura <tkent@chromium.org>
2026-02-12 20:53:12 -0800

    url: Renaming some functions
    This change renames several functions and classes to align with the
    Google C++ style guide. The modifications primarily involve changing
    acronyms in names from all-caps to camel case. For instance,
    `ConvertFromUTF16` has been renamed to `ConvertFromUtf16`,
    `URLComponentSource` to `UrlComponentSource`, and `ICUCharsetConverter`
    to `IcuCharsetConverter`.
    
    This CL has no behavior changes.
    
    Change-Id: Ic73e83ff38f25a364d046dfb64115decf1d6c913
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7574153
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Owners-Override: Hayato Ito <hayato@chromium.org>
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1584428}

--- url/url_canon_icu_unittest.cc
+++ url/url_canon_icu_unittest.cc
@@ -44,14 +44,14 @@ TEST(URLCanonIcuTest, ICUCharsetConverter) {
   for (size_t i = 0; i < std::size(icu_cases); i++) {
     test::UConvScoper conv(icu_cases[i].encoding);
     ASSERT_TRUE(conv.converter() != NULL);
-    ICUCharsetConverter converter(conv.converter());
+    IcuCharsetConverter converter(conv.converter());
 
     std::string str;
     StdStringCanonOutput output(&str);
 
     std::u16string input_str(
         test_utils::TruncateWStringToUtf16(icu_cases[i].input));
-    converter.ConvertFromUTF16(input_str, &output);
+    converter.ConvertFromUtf16(input_str, &output);
     output.Complete();
 
     EXPECT_STREQ(icu_cases[i].expected, str.c_str());
@@ -62,7 +62,7 @@ TEST(URLCanonIcuTest, ICUCharsetConverter) {
   const int static_size = 16;
   test::UConvScoper conv("utf-8");
   ASSERT_TRUE(conv.converter());
-  ICUCharsetConverter converter(conv.converter());
+  IcuCharsetConverter converter(conv.converter());
   for (int i = static_size - 2; i <= static_size + 2; i++) {
     // Make a string with the appropriate length.
     std::u16string input;
@@ -70,7 +70,7 @@ TEST(URLCanonIcuTest, ICUCharsetConverter) {
       input.push_back('a');
 
     RawCanonOutput<static_size> output;
-    converter.ConvertFromUTF16(input, &output);
+    converter.ConvertFromUtf16(input, &output);
     EXPECT_EQ(input.length(), output.length());
   }
 }
@@ -103,7 +103,7 @@ TEST(URLCanonIcuTest, QueryWithConverter) {
 
     test::UConvScoper conv(query_cases[i].encoding);
     ASSERT_TRUE(!query_cases[i].encoding || conv.converter());
-    ICUCharsetConverter converter(conv.converter());
+    IcuCharsetConverter converter(conv.converter());
 
     if (query_cases[i].input8) {
       std::string out_str;
