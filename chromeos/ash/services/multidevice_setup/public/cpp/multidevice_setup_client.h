686b611dffe414fffedf1fc46f2399361f82086e
Achuith Bhandarkar <achuith@chromium.org>
2026-02-13 14:42:01 -0800

    Migrate multidevice_setup observers to base::CheckedObserver.
    This change updates several observer classes within ash/services/multidevice_setup to inherit from base::CheckedObserver and use base::ObserverList instead of base::ObserverList::Unchecked. This improves observer safety by ensuring observers are properly managed.
    
    BUG=470679673
    
    Change-Id: I762d6cac33acbd3809fda447a5bf7fbf9c42131d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7577014
    Reviewed-by: Vincent Chiang <vincentchiang@chromium.org>
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1584929}

--- chromeos/ash/services/multidevice_setup/public/cpp/multidevice_setup_client.h
+++ chromeos/ash/services/multidevice_setup/public/cpp/multidevice_setup_client.h
@@ -27,7 +27,7 @@ class MultiDeviceSetupClient {
       std::pair<mojom::HostStatus, std::optional<multidevice::RemoteDeviceRef>>;
   using FeatureStatesMap = base::flat_map<mojom::Feature, mojom::FeatureState>;
 
-  class Observer {
+  class Observer : public base::CheckedObserver {
    public:
     // Called whenever the host status changes. If the host status is
     // HostStatus::kNoEligibleHosts or
@@ -41,7 +41,7 @@ class MultiDeviceSetupClient {
         const FeatureStatesMap& feature_states_map) {}
 
    protected:
-    virtual ~Observer() = default;
+    ~Observer() override = default;
   };
 
   using GetEligibleHostDevicesCallback =
@@ -92,7 +92,7 @@ class MultiDeviceSetupClient {
  private:
   friend class MultiDeviceSetupClientImplTest;
 
-  base::ObserverList<Observer>::Unchecked observer_list_;
+  base::ObserverList<Observer> observer_list_;
 };
 
 std::string FeatureStatesMapToString(
