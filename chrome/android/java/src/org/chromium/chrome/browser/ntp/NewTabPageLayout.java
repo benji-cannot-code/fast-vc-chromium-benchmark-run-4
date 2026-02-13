a21a9670e3b243b1f1d901ac54d2bf70dc287431
Xi Han <hanxi@google.com>
2026-02-13 13:35:40 -0800

    [Ntp] Fix crash in NewTabPageLayout.getToolbarTransitionPercentage.
    Bug: 477408701
    Change-Id: I444b6bd4586efc98c405e687517c5a2d657e3331
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7576695
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Xinyi Ji <xinyiji@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1584881}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageLayout.java
@@ -159,7 +159,7 @@ public class NewTabPageLayout extends LinearLayout
     private boolean mMvtContentFits;
     private float mTransitionEndOffset;
     private boolean mIsTablet;
-    private Supplier<Integer> mTabStripHeightSupplier;
+    private @Nullable Supplier<Integer> mTabStripHeightSupplier;
     // This variable is only valid when the NTP surface is in tablet mode.
     private boolean mIsInMultiWindowModeOnTablet;
     private Callback<Logo> mOnLogoAvailableCallback;
@@ -730,7 +730,7 @@ public class NewTabPageLayout extends LinearLayout
         final float transitionLength =
                 mCurrentNtpFakeSearchBoxTransitionStartOffset + mTransitionEndOffset;
         // Tab strip height is zero on phones, and may vary on tablets.
-        int tabStripHeight = mTabStripHeightSupplier.get();
+        int tabStripHeight = mTabStripHeightSupplier != null ? mTabStripHeightSupplier.get() : 0;
 
         // When scrollY equals searchBoxTop + tabStripHeight -transitionStartOffset, it marks the
         // start point of the transition. When scrollY equals searchBoxTop plus transitionEndOffset
