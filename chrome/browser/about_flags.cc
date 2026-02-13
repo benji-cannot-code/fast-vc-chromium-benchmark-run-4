082342a19bd4f1ddb3b58a55dd5b794f52b956ed
Alex Gough <ajgo@chromium.org>
2026-02-12 17:47:44 -0800

    Tidy empty #if sections in ['chrome/browser', 'chrome/common']
    Tidies empty preprocessor directives with no functional changes.
    
    This CL was uploaded by git cl split.
    
    R=tedchoc@chromium.org
    
    Bug: n/a
    Change-Id: I3a17b35d4991ba49674f8c5a15f982e61ef8dfed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7563264
    Auto-Submit: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1584367}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -3462,10 +3462,6 @@ const FeatureEntry::FeatureVariation kLinkPreviewTriggerTypeVariations[] = {
     {"Long Press", kLinkPreviewTriggerTypeLongPress, nullptr}};
 #endif  // !BUILDFLAG(IS_ANDROID)
 
-#if BUILDFLAG(IS_ANDROID)
-
-#endif  // BUILDFLAG(IS_ANDROID)
-
 const FeatureEntry::FeatureParam kGroupSuggestionEnableRecentlyOpenedOnly[] = {
     {"group_suggestion_enable_recently_opened", "true"},
     {"group_suggestion_enable_switch_between", "false"},
@@ -8352,10 +8348,6 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kDoubleBufferCompositingName,
      flag_descriptions::kDoubleBufferCompositingDescription, kOsCrOS,
      SINGLE_VALUE_TYPE(switches::kDoubleBufferCompositing)},
-
-#if BUILDFLAG(IS_CHROMEOS)
-
-#endif  // BUILDFLAG(IS_CHROMEOS)
 
 #if BUILDFLAG(IS_WIN)
     {"enable-media-foundation-video-capture",
