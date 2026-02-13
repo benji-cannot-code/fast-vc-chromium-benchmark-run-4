404fe79f9ee62ce40dcdb24c98614451f9c04d49
Henrique Nakashima <hnakashima@chromium.org>
2026-02-13 10:33:40 -0800

    Revert "[Android] Replace isEventuallyVisible() and clean up ViewUtils"
    This reverts commit 8e1db751140df51d7b1d3547fa95c5d23e47a79a.
    
    Reason for revert: crbug.com/484348549, broke KeyboardAccessoryViewTest
    
    Original change's description:
    > [Android] Replace isEventuallyVisible() and clean up ViewUtils
    >
    > Use ViewFinder which by default waits for a View to be visible and
    > accepts non-focusable Views (like IPH).
    >
    > Bug: 471253747
    > Change-Id: Ia841edec2f82f416834fac4db3259c853088ddaa
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7499819
    > Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    > Owners-Override: Henrique Nakashima <hnakashima@chromium.org>
    > Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1584656}
    
    Bug: 471253747
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I28e5f6f765a92ad8c8af27a9ecedad67acc2996d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7576653
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1584754}

--- chrome/android/features/keyboard_accessory/javatests/src/org/chromium/chrome/browser/keyboard_accessory/bar_component/KeyboardAccessoryViewTest.java
+++ chrome/android/features/keyboard_accessory/javatests/src/org/chromium/chrome/browser/keyboard_accessory/bar_component/KeyboardAccessoryViewTest.java
@@ -13,6 +13,7 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
 import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.isSelected;
 import static androidx.test.espresso.matcher.ViewMatchers.withChild;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
@@ -57,6 +58,8 @@ import android.widget.LinearLayout;
 import androidx.annotation.DimenRes;
 import androidx.annotation.Px;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.matcher.RootMatchers;
 import androidx.test.filters.MediumTest;
 
 import org.hamcrest.Matcher;
@@ -71,8 +74,6 @@ import org.mockito.junit.MockitoRule;
 
 import org.chromium.base.Callback;
 import org.chromium.base.ThreadUtils;
-import org.chromium.base.test.transit.ViewFinder;
-import org.chromium.base.test.transit.ViewPresence;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
@@ -114,6 +115,7 @@ import org.chromium.ui.AsyncViewStub;
 import org.chromium.ui.ViewProvider;
 import org.chromium.ui.modelutil.LazyConstructionPropertyMcp;
 import org.chromium.ui.modelutil.PropertyModel;
+import org.chromium.ui.test.util.ViewUtils;
 import org.chromium.ui.widget.ChromeImageView;
 import org.chromium.url.GURL;
 
@@ -747,10 +749,10 @@ public class KeyboardAccessoryViewTest {
         ThreadUtils.runOnUiThreadBlocking(() -> mModel.set(SHOW_SWIPING_IPH, true));
 
         // Wait until the bubble appears, then dismiss is by tapping it.
-        ViewPresence<View> bubble =
-                ViewFinder.waitForView(withText(R.string.iph_keyboard_accessory_swipe_for_more));
+        waitForHelpBubble(withText(R.string.iph_keyboard_accessory_swipe_for_more));
         assertThat(mKeyboardAccessoryView.take().areClicksAllowedWhenObscured(), is(true));
-        bubble.click();
+        waitForHelpBubble(withText(R.string.iph_keyboard_accessory_swipe_for_more))
+                .perform(click());
         assertThat(tracker.wasDismissed(), is(true));
     }
 
@@ -1164,8 +1166,11 @@ public class KeyboardAccessoryViewTest {
         return mActivityTestRule.getActivity().getResources().getDimensionPixelSize(res);
     }
 
-    private ViewPresence<View> waitForHelpBubble(Matcher<View> matcher) {
-        return ViewFinder.waitForView(matcher);
+    private ViewInteraction waitForHelpBubble(Matcher<View> matcher) {
+        View mainDecorView = mActivityTestRule.getActivity().getWindow().getDecorView();
+        return onView(isRoot())
+                .inRoot(RootMatchers.withDecorView(not(is(mainDecorView))))
+                .check(ViewUtils.isEventuallyVisible(matcher));
     }
 
     private void rotateActivityToLandscape() {
