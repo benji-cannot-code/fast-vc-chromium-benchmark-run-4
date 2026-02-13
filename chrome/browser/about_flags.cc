75e811183714762e6543b6531b21a71679d02d7a
Mike Taylor <miketaylr@chromium.org>
2026-02-12 17:30:54 -0800

    Add a flag to enable an Android Platform client hint on Desktop Android
    Bug: 484059749
    Change-Id: Ie4ec372aa19feed834e9336f4d47202e266249ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7571747
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Mike Taylor <miketaylr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1584359}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -11755,6 +11755,11 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kAndroidOpenIncognitoAsWindowDescription, kOsAndroid,
      FEATURE_VALUE_TYPE(feed::kAndroidOpenIncognitoAsWindow)},
 
+    {"android-desktop-ua-platform",
+     flag_descriptions::kAndroidDesktopUAPlatformName,
+     flag_descriptions::kAndroidDesktopUAPlatformDescription, kOsAndroid,
+     FEATURE_VALUE_TYPE(blink::features::kAndroidDesktopUAPlatform)},
+
     {"android-desktop-webprefs-large-display",
      flag_descriptions::kAndroidDesktopWebPrefsLargeDisplaysName,
      flag_descriptions::kAndroidDesktopWebPrefsLargeDisplaysDescription,
