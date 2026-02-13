bc2bbeb2c059fd9f98c2bccc2036aaaef91b8f7f
James Lee <ljjlee@google.com>
2026-02-13 02:51:36 -0800

    Add presubmit check for new account capabilities.
    This presubmit warns when a new ACCOUNT_CAPABILITY macro is added in account_capabilities_list.h, prompting the author to ensure the capability is fully rolled out server-side or to use the flag-guarded ACCOUNT_CAPABILITY_F definition.
    
    Change-Id: I64ffeb3124aa20fe00637a6d5b6629851c896105
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7570903
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Auto-Submit: James Lee <ljjlee@google.com>
    Commit-Queue: James Lee <ljjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1584539}

---
+++ components/signin/internal/identity_manager/PRESUBMIT.py
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2026 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""
+See https://www.chromium.org/developers/how-tos/depottools/presubmit-scripts
+for more details on the presubmit API built into depot_tools.
+"""
+
+PRESUBMIT_VERSION = '2.0.0'
+
+
+def CheckAccountCapabilityRolloutSafety(input_api, output_api):
+    """
+    Checks for modifications to account_capabilities_list.h and warns the user
+    to ensure that the capability has been rolled out server-side.
+    """
+    for f in input_api.AffectedFiles():
+        if f.LocalPath().endswith("account_capabilities_list.h"):
+            return [
+                output_api.PresubmitPromptWarning(
+                    'This CL modifies account_capabilities_list.h. If you are'
+                    ' adding a new capability or exposing an existing'
+                    ' capability on new platforms, please ensure that the'
+                    ' capability is fully rolled out server-side before'
+                    ' submitting this CL (Googlers, see'
+                    ' go/chrome-account-capability-rollout), or use the'
+                    ' flag-guarded ACCOUNT_CAPABILITY_F definition.')
+            ]
+
+    return []
