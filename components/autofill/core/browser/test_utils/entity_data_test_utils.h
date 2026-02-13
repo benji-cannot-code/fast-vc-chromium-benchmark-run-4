0ff8a0e94fb9c22199abe8694da83fce906a2438
Florian Leimgruber <fleimgruber@google.com>
2026-02-13 08:18:13 -0800

    Fix entity_data_test_utils
    Currently, entity_data_test_utils doesn't support setting a name for
    national ID cards, KTN and redress number, even though those entities
    support names. This CL adds logic for it and fixes tests that break as
    a result.
    
    It also fixes a bug in the construction of test national ID cards,
    where currently setting expiration and issue date doesn't work, because
    no format string is provided.
    
    Bug: 40100455
    Change-Id: Ib2781e7683497101516ad9f5db6529d89a11f4bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7572326
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1584677}

--- components/autofill/core/browser/test_utils/entity_data_test_utils.h
+++ components/autofill/core/browser/test_utils/entity_data_test_utils.h
@@ -93,6 +93,7 @@ EntityInstance GetVehicleEntityInstanceWithRandomGuid(
 
 template <typename = void>
 struct NationalIdCardOptionsT {
+  const char16_t* name = u"Name";
   const char16_t* number = u"987654321";
   const char16_t* country = u"United States";
   const char16_t* issue_date = u"01/12/2020";
@@ -114,6 +115,7 @@ EntityInstance GetNationalIdCardEntityInstance(
 
 template <typename = void>
 struct KnownTravelerNumberOptionsT {
+  const char16_t* name = u"Name";
   const char16_t* number = u"987654321";
   const char16_t* expiration_date = u"01/12/2030";
   std::string_view guid = "00000000-0000-4000-8000-400000000000";
@@ -133,6 +135,7 @@ EntityInstance GetKnownTravelerNumberInstance(
 
 template <typename = void>
 struct RedressNumberOptionsT {
+  const char16_t* name = u"Name";
   const char16_t* number = u"987654321";
   std::string_view guid = "00000000-0000-4000-8000-500000000000";
   std::string_view nickname = "RedressNumber";
