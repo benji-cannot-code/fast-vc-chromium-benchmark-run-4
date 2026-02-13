9beb96ecda9bebbf9362ffc7dbd44398404b71a1
Gauthier Ambard <gambard@chromium.org>
2026-02-13 02:37:09 -0800

    [iOS] Move iOS 19 guards to iOS 26
    Bug: None
    Change-Id: Iad0a4ccc2d8ce20b83fab2ff2123cbd4a5ac5812
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7572105
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1584532}

--- ios/chrome/browser/settings/ui_bundled/clear_browsing_data/test/quick_delete_egtest.mm
+++ ios/chrome/browser/settings/ui_bundled/clear_browsing_data/test/quick_delete_egtest.mm
@@ -1182,7 +1182,7 @@ NSString* CapitalizeFirstLetter(NSString* string) {
   if (![ChromeEarlGrey areMultipleWindowsSupported]) {
     EARL_GREY_TEST_DISABLED(@"Multiple windows can't be opened.");
   }
-  if (@available(iOS 19.0, *)) {
+  if (@available(iOS 26.0, *)) {
     // TODO(crbug.com/427699033): Re-enable test on iOS 26.
     // Fails final histogram check.
     EARL_GREY_TEST_DISABLED(@"Test disabled on iOS 26.");
@@ -1776,7 +1776,7 @@ NSString* CapitalizeFirstLetter(NSString* string) {
   if (![ChromeEarlGrey areMultipleWindowsSupported]) {
     EARL_GREY_TEST_DISABLED(@"Multiple windows can't be opened.");
   }
-  if (@available(iOS 19.0, *)) {
+  if (@available(iOS 26.0, *)) {
     // TODO(crbug.com/427699033): Re-enable test on iOS 26.
     // Fails interacting with both windows.
     EARL_GREY_TEST_DISABLED(@"Test disabled on iOS 26.");
