fc7bbe3c7822fd8799b1e68fc991caa4fe90bedb
lancanrong <chanran.lan@gmail.com>
2026-02-13 01:18:46 -0800

    [blink] feat: add devtools document.cookie performance issue
    We encountered a major source of jank because front-end developers often use the synchronous API document.cookie. Currently, the asynchronous API CookieStore serves as an alternative, so it would be better to add a warning message when document.cookie is used. In the future, it could be gradually marked as deprecated.
    
    Bug: 447104528
    Change-Id: I43c1cd93008b835393eaa8dc688659237afe1c9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7234194
    Commit-Queue: Peng Zhou <zhoupeng.1996@bytedance.com>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Reviewed-by: Peng Zhou <zhoupeng.1996@bytedance.com>
    Cr-Commit-Position: refs/heads/main@{#1584498}

--- third_party/blink/renderer/core/inspector/inspector_audits_issue.h
+++ third_party/blink/renderer/core/inspector/inspector_audits_issue.h
@@ -107,6 +107,9 @@ class CORE_EXPORT AuditsIssue {
                                     String frame_id,
                                     String loader_id);
 
+  static void ReportDocumentCookiePerformanceIssue(
+      ExecutionContext* execution_context);
+
   static void ReportCorsIssue(ExecutionContext* execution_context,
                               RendererCorsIssueCode code,
                               String url,
@@ -224,7 +227,6 @@ class CORE_EXPORT AuditsIssue {
       const String& disableReason = String());
 
  private:
-
   std::unique_ptr<protocol::Audits::InspectorIssue> issue_;
 };
 
---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/issues/document-cookie-performance-issue.js
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(/** @type {import('test_runner').TestRunner} */ testRunner) {
+ const { session, dp } = await testRunner.startBlank(`Tests that performance issues are reported for document.cookie access`);
+  await dp.Audits.enable();
+  const promise = dp.Audits.onceIssueAdded();
+  // Update this to test different deprecated code if WebFeature::kRangeExpand is removed.
+  session.evaluate("document.cookie");
+  const result = await promise;
+
+  testRunner.log(result.params, "Inspector issue: ");
+  testRunner.completeTest();
+});
