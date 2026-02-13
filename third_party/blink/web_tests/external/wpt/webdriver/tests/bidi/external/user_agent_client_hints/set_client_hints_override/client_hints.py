ddf1aa6d0fbcc01ca686e1c59efb144128aa7a03
Jonathan Lee <jonathanjlee@google.com>
2026-02-13 12:14:21 -0800

    Import wpt@f68e4fd27a44a8e1014c1e903c7e7b4ee3e1315f
    https://github.com/web-platform-tests/wpt/compare/392e072dd...f68e4fd27
    
    Using wpt-import in Chromium 190a3548629c822fa8226a8a9bccb11ddbf7383d.
    With Chromium commits locally applied on WPT:
      c116224bea "Reland "LCP: Use largest painted image for web- [...]
      bc971aee7d "Refactor out of order streaming to use PIs"
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    daniec@microsoft.com, siliu@microsoft.com, snianu@microsoft.com:
      external/wpt/editing
    dgrogan@chromium.org:
      external/wpt/css/css-tables
    foolip@chromium.org:
      external/wpt/infrastructure
    hongchan@chromium.org, mjwilson@chromium.org:
      external/wpt/webaudio
    kojii@chromium.org:
      external/wpt/css/css-writing-modes
    nzolghadr@chromium.org, mustaq@chromium.org:
      external/wpt/pointerevents
    pastithas@chromium.org:
      external/wpt/resources
    
    NOAUTOREVERT=true
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac-skia-alt-arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac12.0.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac13-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac13.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac14.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac15.arm64-blink-rel
    Change-Id: Ia0f121867c876688ceaddfc1e794192313ca0285
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7570872
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1584837}

---
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/external/user_agent_client_hints/set_client_hints_override/client_hints.py
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import pytest
+
+from . import SOME_CLIENT_HINTS
+
+pytestmark = pytest.mark.asyncio
+
+
+async def test_set_override_and_reset(bidi_session, top_context,
+        default_client_hints, assert_client_hints):
+    await assert_client_hints(top_context, default_client_hints)
+
+    await bidi_session.user_agent_client_hints.set_client_hints_override(
+        client_hints=SOME_CLIENT_HINTS
+    )
+    await assert_client_hints(top_context, SOME_CLIENT_HINTS)
+
+    await bidi_session.user_agent_client_hints.set_client_hints_override(
+        client_hints=None
+    )
+
+    await assert_client_hints(top_context, default_client_hints)
