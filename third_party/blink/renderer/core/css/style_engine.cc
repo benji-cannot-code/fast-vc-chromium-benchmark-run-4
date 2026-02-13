1741025c44a202f42a002f28e840d572e59f1a93
Rune Lillesveen <futhark@chromium.org>
2026-02-13 03:57:06 -0800

    Don't update the style recalc root for moveBefore() outside flat tree
    StyleTraversalRoots are confused and inconsistent if we involve elements
    outside the flat tree when updating them. There are assumptions that
    elements with non-null ComputedStyle are part of the flat tree given the
    current slot assignments, which does not hold for moveBefore() which
    does not clear ComputedStyle when removing elements from the tree.
    
    Add an extra check for elements without a GetStyleRecalcParent() which
    are not the documentElement but still have a non-null ComputedStyle to
    cover the case where moveBefore() does a move from inside to outside
    the flat tree.
    
    Also reverting the fix for issue 483298490, which is essentially the
    same issue and will be fixed by this change instead.
    
    Bug: 433696404, 483298490
    Change-Id: I0538a2dabad93774c5fae41052434aa05635c73c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7571883
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1584564}

--- third_party/blink/renderer/core/css/style_engine.cc
+++ third_party/blink/renderer/core/css/style_engine.cc
@@ -4230,7 +4230,7 @@ void StyleEngine::UpdateStyleRecalcRoot(ContainerNode* ancestor,
     return;
   }
   DCHECK(!InRebuildLayoutTree());
-  if (InDOMRemoval() || GetDocument().StatePreservingAtomicMoveInProgress()) {
+  if (InDOMRemoval()) {
     ancestor = nullptr;
     dirty_node = document_;
   }
