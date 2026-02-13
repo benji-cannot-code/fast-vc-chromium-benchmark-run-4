c016a64915ed4b2bc452c62dbc66b79aa7597388
Tomasz Wiszkowski <ender@google.com>
2026-02-13 15:29:02 -0800

    Use FreshCTA rule in OmniboxPTTest suite.
    ReusedCTA does not close activities if tests fail. When the incognito
    test fails on AL devices due to multiple top level windows being open,
    confusing espresso keyboard rules, both these windows remain open
    when the Web test starts, causing the same exact failure to resurface.
    
    FreshCTA closes all activities at the end of the test. While this
    is a bit slower, it also does not carry over bad state across test
    executions
    
    Bug: 484093894
    Change-Id: I115f59a601dd7a7b8d3522579bd76c9eb7294256
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7577656
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1584967}

--- chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/OmniboxPTTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/OmniboxPTTest.java
@@ -8,6 +8,7 @@ package org.chromium.chrome.browser.omnibox;
 import androidx.test.filters.LargeTest;
 
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
@@ -16,18 +17,20 @@ import org.junit.runner.RunWith;
 import org.chromium.base.test.transit.TransitAsserts;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.incognito.IncognitoUtils;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.transit.ChromeTransitTestRules;
-import org.chromium.chrome.test.transit.ReusedCtaTransitTestRule;
+import org.chromium.chrome.test.transit.FreshCtaTransitTestRule;
 import org.chromium.chrome.test.transit.ntp.IncognitoNewTabPageStation;
 import org.chromium.chrome.test.transit.ntp.RegularNewTabPageStation;
 import org.chromium.chrome.test.transit.omnibox.FakeOmniboxSuggestions;
 import org.chromium.chrome.test.transit.omnibox.OmniboxEnteredTextFacility;
 import org.chromium.chrome.test.transit.omnibox.OmniboxFacility;
 import org.chromium.chrome.test.transit.page.WebPageStation;
+import org.chromium.ui.base.DeviceFormFactor;
 
 /** Public Transit tests for Omnibox. */
 @RunWith(ChromeJUnit4ClassRunner.class)
@@ -35,10 +38,11 @@ import org.chromium.chrome.test.transit.page.WebPageStation;
 @Batch(Batch.PER_CLASS)
 public class OmniboxPTTest {
     @Rule
-    public ReusedCtaTransitTestRule<WebPageStation> mCtaTestRule =
-            ChromeTransitTestRules.blankPageStartReusedActivityRule();
+    public FreshCtaTransitTestRule mCtaTestRule =
+            ChromeTransitTestRules.freshChromeTabbedActivityRule();
 
     private static final FakeOmniboxSuggestions sFakeSuggestions = new FakeOmniboxSuggestions();
+    private WebPageStation mBlankPage;
 
     @BeforeClass
     public static void setUpClass() {
@@ -50,39 +54,44 @@ public class OmniboxPTTest {
         sFakeSuggestions.destroy();
     }
 
+    @Before
+    public void setUp() {
+        mBlankPage = mCtaTestRule.startOnBlankPage();
+    }
+
     @LargeTest
     @Test
     public void testOpenTypeDelete_fromWebPage() {
         ChromeFeatureList.sAndroidBottomToolbarV2ForceBottomForFocusedOmnibox.setForTesting(false);
-        WebPageStation blankPage = mCtaTestRule.start();
-        var omniboxAndKeyboard = blankPage.openOmnibox(sFakeSuggestions);
+        var omniboxAndKeyboard = mBlankPage.openOmnibox(sFakeSuggestions);
 
         doOpenTypeDelete(omniboxAndKeyboard);
 
-        TransitAsserts.assertFinalDestination(blankPage);
+        TransitAsserts.assertFinalDestination(mBlankPage);
     }
 
     @LargeTest
     @Test
     public void testOpenTypeDelete_fromNtp() {
         ChromeFeatureList.sAndroidBottomToolbarV2ForceBottomForFocusedOmnibox.setForTesting(false);
-        WebPageStation blankPage = mCtaTestRule.start();
-        RegularNewTabPageStation ntp = blankPage.openNewTabFast();
+        RegularNewTabPageStation ntp = mBlankPage.openNewTabFast();
         var omnibox = ntp.openOmnibox(sFakeSuggestions);
 
         doOpenTypeDelete(omnibox);
 
-        blankPage =
+        mBlankPage =
                 ntp.openTabSwitcherActionMenu()
                         .selectCloseTabAndDisplayAnotherTab(WebPageStation.newBuilder());
-        TransitAsserts.assertFinalDestination(blankPage);
+        TransitAsserts.assertFinalDestination(mBlankPage);
     }
 
     @LargeTest
     @Test
+    @DisableIf.Device(DeviceFormFactor.DESKTOP)
     public void testOpenTypeDelete_fromIncognitoNtp() {
-        WebPageStation blankPage = mCtaTestRule.start();
-        IncognitoNewTabPageStation incognitoNtp = blankPage.openNewIncognitoTabOrWindowFast();
+        // Desktop opens an incognito profile as a separate window, which confuses Espresso and
+        // leads to test failures.
+        IncognitoNewTabPageStation incognitoNtp = mBlankPage.openNewIncognitoTabOrWindowFast();
         var omnibox = incognitoNtp.openOmnibox(sFakeSuggestions);
 
         doOpenTypeDelete(omnibox);
@@ -90,12 +99,12 @@ public class OmniboxPTTest {
         if (IncognitoUtils.shouldOpenIncognitoAsWindow()) {
             incognitoNtp.openTabSwitcherActionMenu().selectCloseTabTo().reachLastStop();
         } else {
-            blankPage =
+            mBlankPage =
                     incognitoNtp
                             .openTabSwitcherActionMenu()
                             .selectCloseTabAndDisplayRegularTab(WebPageStation.newBuilder());
         }
-        TransitAsserts.assertFinalDestination(blankPage);
+        TransitAsserts.assertFinalDestination(mBlankPage);
     }
 
     private void doOpenTypeDelete(OmniboxFacility omnibox) {
