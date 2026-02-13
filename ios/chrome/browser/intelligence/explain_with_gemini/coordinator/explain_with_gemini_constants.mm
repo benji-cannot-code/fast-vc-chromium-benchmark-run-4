9e71194a22a51fdbc60e482fe36bd2a44153efd7
Yasaman Sedaghat <yasamans@google.com>
2026-02-13 12:54:06 -0800

    [ios] Move explain gemini directory to "intelligence"
    Phase 1 of changes: moving the explain gemini directory to
    "intelligence" with the rest of helios code Update the old code to add a
    dependency to check if helios is available for a given web tab and user
    currently clicking on explain will go to an error page since helios
    floaty is not hooked up, but code is gated behind flags the rest will be
    done in follow up CLs
    
    Just testing the new flags: screenshot of explain after edit, and after
    search, and the error page:
    https://screenshot.googleplex.com/9s2KNqqPjARLtnV
    https://screenshot.googleplex.com/4JAoZLspCVZaCiS
    https://screenshot.googleplex.com/6Rm9ALSfS9CR26u
    
    Change-Id: If6ea343b0b72480a4a8463ab6bca543359780622
    Bug: 483005855
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7568150
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Yasaman Sedaghat <yasamans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1584855}

--- ios/chrome/browser/intelligence/explain_with_gemini/coordinator/explain_with_gemini_constants.mm
+++ ios/chrome/browser/intelligence/explain_with_gemini/coordinator/explain_with_gemini_constants.mm
@@ -1,9 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2025 The Chromium Authors
+// Copyright 2026 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/explain_with_gemini/coordinator/explain_with_gemini_constants.h"
+#import "ios/chrome/browser/intelligence/explain_with_gemini/coordinator/explain_with_gemini_constants.h"
 
 const char kExplainWithGeminiURL[] =
     "https://gemini.google.com/"
--- ios/chrome/browser/intelligence/explain_with_gemini/coordinator/explain_with_gemini_mediator_egtest.mm
+++ ios/chrome/browser/intelligence/explain_with_gemini/coordinator/explain_with_gemini_mediator_egtest.mm
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2025 The Chromium Authors
+// Copyright 2026 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/authentication/test/signin_earl_grey.h"
 #import "ios/chrome/browser/browser_content/ui_bundled/edit_menu_app_interface.h"
 #import "ios/chrome/browser/browser_content/ui_bundled/edit_menu_matchers.h"
-#import "ios/chrome/browser/explain_with_gemini/coordinator/explain_with_gemini_constants.h"
+#import "ios/chrome/browser/intelligence/explain_with_gemini/coordinator/explain_with_gemini_constants.h"
 #import "ios/chrome/browser/intelligence/features/features.h"
 #import "ios/chrome/browser/reader_mode/model/features.h"
 #import "ios/chrome/browser/reader_mode/ui/constants.h"
