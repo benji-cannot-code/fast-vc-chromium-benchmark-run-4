0db8aa0ffff0757692bc4f84b0e17b0e2d1b8b97
Awad Osman <awado@google.com>
2026-02-13 12:28:39 -0800

    [Bundled Settings] Implement Javascript Guardrails row
    Integrates the Javascript Optimizer setting into the new Bundled
    Security Settings UI. This CL adds:
    
    -All strings
    -Preference hookup
    -Bundle preference controlling the setting
    -Feature state string support
    
    Screencast:crbug.com/479233209#comment3
    
    NO_IFTTT=Will address updates in a followup. Bug: b:482066535
    
    Bug: b:479233209
    Change-Id: I1fd02664c83c85bb177537bb99d79c67ec9cf563
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7542221
    Reviewed-by: Javier Castro <jacastro@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Awad Osman <awado@google.com>
    Cr-Commit-Position: refs/heads/main@{#1584843}

--- chrome/browser/content_settings/generated_javascript_optimizer_pref.h
+++ chrome/browser/content_settings/generated_javascript_optimizer_pref.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/browser/ui/javascript_optimizer_setting.h"
 #include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "components/prefs/pref_change_registrar.h"
+#include "components/safe_browsing/core/common/safe_browsing_prefs.h"
 
 namespace content_settings {
 
@@ -40,6 +41,12 @@ class GeneratedJavascriptOptimizerPref
 
   // Fired when preferences used to generate this preference are changed.
   void OnPreferencesChanged();
+
+  // Returns the default Javascript Optimizer setting for the passed-in
+  // security-bundle type.
+  static content_settings::JavascriptOptimizerSetting
+  GetDefaultJsOptimizerSetting(
+      safe_browsing::SecuritySettingsBundleSetting bundle_setting);
 
  private:
   // Profile this preference is generated for.
