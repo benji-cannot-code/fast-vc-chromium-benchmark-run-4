cfff7a78196d73658361426e15099def0f248d9e
Crisrael Lucero <crisrael@google.com>
2026-02-13 11:12:53 -0800

    [Eche] Remove unnecessary usage of EcheTray in EcheSignalerTest
    In the process auditing/fixing dangling pointers, it was revealed that
    EcheTray was not being used at all in this test anymore. This CL fixes
    that.
    
    Bug: N/a
    Test: CQ
    Change-Id: Ib0c078fcbb2ce0bcff53ef4be9dc4ebc8dd08311
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7576135
    Reviewed-by: Pu Shi <pushi@google.com>
    Commit-Queue: Crisrael Lucero <crisrael@google.com>
    Cr-Commit-Position: refs/heads/main@{#1584788}

--- ash/webui/eche_app_ui/eche_signaler_unittest.cc
+++ ash/webui/eche_app_ui/eche_signaler_unittest.cc
@@ -174,7 +174,6 @@ class EcheSignalerTest : public AshTestBase {
     AshTestSuite::LoadTestResources();
     AshTestBase::SetUp();
 
-    eche_tray_ = StatusAreaWidgetTestHelper::GetStatusAreaWidget()->eche_tray();
     eche_connection_status_handler_ =
         std::make_unique<eche_app::EcheConnectionStatusHandler>();
     apps_launch_info_provider_ = std::make_unique<AppsLaunchInfoProvider>(
@@ -245,7 +244,6 @@ class EcheSignalerTest : public AshTestBase {
   FakeEcheConnector fake_connector_{&task_runner_};
 
  private:
-  raw_ptr<EcheTray, DanglingUntriaged> eche_tray_ = nullptr;
   secure_channel::FakeConnectionManager fake_connection_manager_;
   std::unique_ptr<EcheConnectionStatusHandler> eche_connection_status_handler_;
   std::unique_ptr<AppsLaunchInfoProvider> apps_launch_info_provider_;
