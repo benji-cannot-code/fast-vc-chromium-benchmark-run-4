7c4b5794997517065911803b06d607248af643c8
Amarjot Gill <amarjotgill@chromium.org>
2026-02-13 14:03:07 -0800

    Attach tab_helper to ios::provider::AttachClassificationMetricsTabHelper
    Bug: 469817333
    Change-Id: I75c8866567a4df7c44d93802251688d64259c5fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7545858
    Reviewed-by: Matt Reichhoff <mreichhoff@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Amarjot Gill <amarjotgill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1584899}

--- ios/chrome/browser/tabs/model/tab_helper_util.mm
+++ ios/chrome/browser/tabs/model/tab_helper_util.mm
@@ -63,6 +63,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/infobars/model/overlays/translate_overlay_tab_helper.h"
 #import "ios/chrome/browser/intelligence/bwg/model/bwg_tab_helper.h"
 #import "ios/chrome/browser/intelligence/features/features.h"
+#import "ios/public/provider/chrome/browser/intelligence/classification_metrics_tab_helper_api.h"
 #import "ios/chrome/browser/itunes_urls/model/itunes_urls_handler_tab_helper.h"
 #import "ios/chrome/browser/lens/model/lens_tab_helper.h"
 #import "ios/chrome/browser/lens_overlay/coordinator/lens_overlay_availability.h"
@@ -478,4 +479,9 @@ void AttachTabHelpers(web::WebState* web_state, TabHelperFilter filter_flags) {
   attacher.CreateWhen<LastTapLocationTabHelper>(
       attacher.IsNotInTabHelperFilter() &&
       base::FeatureList::IsEnabled(kIOSCustomFileUploadMenu));
+
+  if (!attacher.IsOffTheRecord() && !attacher.IsForPrerender() &&
+      IsModelBasedPageClassificationEnabled()) {
+    ios::provider::AttachClassificationMetricsTabHelper(web_state);
+  }
 }
