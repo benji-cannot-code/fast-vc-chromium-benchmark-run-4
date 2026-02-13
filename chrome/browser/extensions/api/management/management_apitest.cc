6641080cdf2f083ebf96b7d6c287114e2e47551a
Andrew Rayskiy <greengrape@google.com>
2026-02-13 04:22:32 -0800

    Reland "Add parametrized filters for management types and install states"
    This is a reland of commit a4351d04589dbb55bc270f5227fecc4e04d900c7
    
    What changed (diff 1..3): faulty includes are now hidden behind the
    respective BUILDFLAG so that Android build no longer complains about
    missing proto headers (install_state.pb.h).
    
    Original change's description:
    > Add parametrized filters for management types and install states
    >
    > Most other filters can actually be expressed via these primitives; this
    > reduces the number of boolean predicated in the LeafFilter to around ~5.
    >
    > Bug: 463757344
    > Change-Id: Icf709fa144fb42f6d239a8cbc7103576044049fc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7567302
    > Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    > Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    > Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1584300}
    
    Bug: 463757344
    Change-Id: I1cb12414926767ef5f392a51e74743a28543f884
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7572505
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1584573}

--- chrome/browser/extensions/api/management/management_apitest.cc
+++ chrome/browser/extensions/api/management/management_apitest.cc
@@ -19,8 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/launch_util.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser_commands.h"
-#include "chrome/browser/web_applications/extension_status_utils.h"
-#include "chrome/browser/web_applications/web_app_filter.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/extensions/extension_constants.h"
 #include "content/public/test/browser_test.h"
@@ -48,11 +46,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window/public/browser_window_interface_iterator.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/web_applications/web_app_dialogs.h"
+#include "chrome/browser/web_applications/extension_status_utils.h"
 #include "chrome/browser/web_applications/os_integration/os_integration_manager.h"
 #include "chrome/browser/web_applications/proto/web_app_install_state.pb.h"
 #include "chrome/browser/web_applications/test/fake_web_app_ui_manager.h"
 #include "chrome/browser/web_applications/test/os_integration_test_override_impl.h"
 #include "chrome/browser/web_applications/test/web_app_install_test_utils.h"
+#include "chrome/browser/web_applications/web_app_filter.h"
 #include "chrome/browser/web_applications/web_app_helpers.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
 #include "chrome/browser/web_applications/web_app_registrar.h"
@@ -64,7 +64,6 @@ static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 using extensions::Extension;
 using extensions::Manifest;
 using extensions::mojom::ManifestLocation;
-using web_app::WebAppFilter;
 
 namespace {
 
@@ -314,7 +313,8 @@ class InstallReplacementWebAppApiTest : public ExtensionManagementApiTest {
     RunTest(manifest, web_app_url, kInstallReplacementWebApp,
             true /* from_webstore */);
     EXPECT_TRUE(provider->registrar_unsafe().AppMatches(
-        web_app_id, WebAppFilter::InstalledInOperatingSystemForTesting()));
+        web_app_id,
+        web_app::WebAppFilter::InstalledInOperatingSystemForTesting()));
     EXPECT_EQ(1, static_cast<int>(
                      provider->ui_manager().GetNumWindowsForApp(web_app_id)));
 
@@ -322,7 +322,8 @@ class InstallReplacementWebAppApiTest : public ExtensionManagementApiTest {
     RunTest(std::move(manifest), web_app_url, kInstallReplacementWebApp,
             true /* from_webstore */);
     EXPECT_TRUE(provider->registrar_unsafe().AppMatches(
-        web_app_id, WebAppFilter::InstalledInOperatingSystemForTesting()));
+        web_app_id,
+        web_app::WebAppFilter::InstalledInOperatingSystemForTesting()));
     EXPECT_EQ(2, static_cast<int>(
                      provider->ui_manager().GetNumWindowsForApp(web_app_id)));
   }
