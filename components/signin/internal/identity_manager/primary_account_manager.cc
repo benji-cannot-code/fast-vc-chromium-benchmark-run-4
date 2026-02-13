11e5c6639b3c6d48d7ef1ac0295ce6bf475d02de
David Roger <droger@chromium.org>
2026-02-13 04:10:39 -0800

    [signin] Wrap implicit signin logic with ENABLE_DICE_SUPPORT
    The logic for AccessPoint::kWebSignin in SetExplicitBrowserSigninPrefs
    is specific to the Dice web sign-in flow. This CL guards this block
    with BUILDFLAG(ENABLE_DICE_SUPPORT) to ensure it is only compiled and
    executed on supporting platforms.
    This also adds a TODO to remove this temporary migration code once the
    Dice migration is complete.
    
    Bug: 475822503
    Change-Id: I186cb6cd0828401a3851af2d5d8da74e086ee5e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7575077
    Reviewed-by: Ankush Singh <ankushkush@google.com>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1584568}

--- components/signin/internal/identity_manager/primary_account_manager.cc
+++ components/signin/internal/identity_manager/primary_account_manager.cc
@@ -714,10 +714,11 @@ void PrimaryAccountManager::SetExplicitBrowserSigninPrefs(
             prefs::kPrimaryAccountSetAfterSigninMigration, true);
       }
 
-      if (access_point == signin_metrics::AccessPoint::kWebSignin) {
 #if BUILDFLAG(ENABLE_DICE_SUPPORT)
+      if (access_point == signin_metrics::AccessPoint::kWebSignin) {
+        // TODO(crbug.com/475822503): Delete this code once Dice migration is
+        // complete.
         scoped_pref_commit.ClearPref(prefs::kExplicitBrowserSignin);
-#endif
         // Reset explicit sign-in prefs for the relevant data types.
         scoped_pref_commit.SetBoolean(
             prefs::kPrefsThemesSearchEnginesAccountStorageEnabled, false);
@@ -727,9 +728,10 @@ void PrimaryAccountManager::SetExplicitBrowserSigninPrefs(
             .SetBookmarksExplicitBrowserSignin(current_gaia_id, false);
         break;
       }
-#if BUILDFLAG(ENABLE_DICE_SUPPORT)
+
       scoped_pref_commit.SetBoolean(prefs::kExplicitBrowserSignin, true);
 #endif
+
       if (base::FeatureList::IsEnabled(
               switches::kEnablePreferencesAccountStorage)) {
         scoped_pref_commit.SetBoolean(
