42ab6753da477ea78dc64c2deaa3a467ccef8c8c
Daniel White <danieltwhite@google.com>
2026-02-13 10:37:32 -0800

    [iOS] Remove TraitCollectionSetForTraits invocation
    In this CL, the TraitCollectionSetForTraits was removed from these
    files. This CL is part of a wider effort to remove the
    TraitCollectionSetForTraits function from the codebase.
    
    Bug: 483670636
    Change-Id: Ice6f2a1c2ab9601626819d88dd5dffc29a3920ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7567466
    Auto-Submit: Daniel White <danieltwhite@google.com>
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1584760}

--- ios/chrome/browser/bubble/ui_bundled/gesture_iph/toolbar_swipe_gesture_in_product_help_view.mm
+++ ios/chrome/browser/bubble/ui_bundled/gesture_iph/toolbar_swipe_gesture_in_product_help_view.mm
@@ -56,8 +56,7 @@ const int kBidirectionalAnimationRepeatCount = 4;
       _bidirectional = NO;
       _animationRepeatCount = [super animationRepeatCount];
     }
-    [self registerForTraitChanges:TraitCollectionSetForTraits(
-                                      @[ UITraitVerticalSizeClass.class ])
+    [self registerForTraitChanges:@[ UITraitVerticalSizeClass.class ]
                        withAction:@selector(updateUIOnTraitChange)];
   }
   return self;
