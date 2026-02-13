c8d49f43cb46322cf40e9e807fca8f2763074e9f
Adam Norberg <norberg@google.com>
2026-02-12 17:11:05 -0800

    Use --data-format in `hfs_tool setattr`
    This allows this tool to set HFS+ extended attributes that are not valid
    C strings (because they contain NUL bytes). HFS+ extended attributes
    can be arbitrary binary data (of even length), which `setAttribute` is
    already ready for, so the only change required is to the CLI parser.
    
    Bug: 435220364
    Change-Id: I78a83a41aab8792941f92c8940a601eeeff8af64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7565397
    Commit-Queue: Adam Norberg <norberg@google.com>
    Auto-Submit: Adam Norberg <norberg@google.com>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1584349}

--- third_party/libdmg-hfsplus/src/common/sizedbuf.c
+++ third_party/libdmg-hfsplus/src/common/sizedbuf.c
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sizedbuf.h"
 
 #include <stddef.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -20,7 +21,7 @@ SizedBuf* AllocBufCopy(const SizedBuf* buf) {
     return AllocBufCopyBytes(buf->data, buf->len);
 }
 
-SizedBuf* AllocBufCopyBytes(const char* data, size_t len) {
+SizedBuf* AllocBufCopyBytes(const uint8_t* data, size_t len) {
     ASSERT(data, "AllocBufCopyBytes can't copy out of NULL!");
     SizedBuf* ret = malloc(sizeof(SizedBuf) + len);
     ASSERT(ret, "AllocBufCopyBytes OOM");
@@ -31,7 +32,22 @@ SizedBuf* AllocBufCopyBytes(const char* data, size_t len) {
 
 SizedBuf* AllocBufCopyString(const char* str) {
     ASSERT(str, "AllocBufCopyString can't copy out of NULL!");
-    SizedBuf* ret = AllocBufCopyBytes(str, strlen(str) + 1);
+    SizedBuf* ret = AllocBufCopyBytes((const uint8_t*)str, strlen(str) + 1);
     ret->len -= 1;
+    return ret;
+}
+
+SizedBuf* ReallocBuf(SizedBuf* buf, size_t new_cap) {
+    if (!buf) {
+        SizedBuf* ret = ZAllocBuf(new_cap);
+        ret->len = 0;
+        return ret;
+    }
+    SizedBuf* ret = realloc(buf, sizeof(SizedBuf) + new_cap);
+    ASSERT(ret, "ReallocBuf failed");
+    ret->cap = new_cap;
+    if (ret->len > new_cap) {
+        ret->len = new_cap;
+    }
     return ret;
 }
