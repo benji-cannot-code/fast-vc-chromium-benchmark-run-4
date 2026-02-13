d56731061477d70d2393f4915fa2c9f6b9fb6709
S. Ganesh <ganesh@chromium.org>
2026-02-13 14:41:01 -0800

    update_client: allow cancel in decompression operations
    Now the `Waiting to install` stage allows cancelling.
    
    Fixed: 483844396
    Change-Id: I29cd2d22e24b0a5233aa46685cbafd8aae5fe669
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7570230
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1584928}

--- components/update_client/op_xz_unittest.cc
+++ components/update_client/op_xz_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/bind.h"
 #include "base/test/task_environment.h"
 #include "base/types/expected.h"
+#include "components/update_client/protocol_definition.h"
 #include "components/update_client/test_utils.h"
 #include "components/update_client/unzip/in_process_unzipper.h"
 #include "components/update_client/unzipper.h"
@@ -100,6 +101,36 @@ TEST_F(XzOperationTest, BadPatch) {
   ASSERT_EQ(pings_.size(), 1u);
   EXPECT_EQ(pings_[0].FindInt("eventtype"), 60);
   EXPECT_EQ(pings_[0].FindInt("eventresult"), 0);
+}
+
+TEST_F(XzOperationTest, Cancel) {
+  base::FilePath in_file = CopyToTemp("file1.xz");
+  base::OnceClosure cancel = XzOperation(
+      base::MakeRefCounted<InProcessUnzipperFactory>(
+          InProcessUnzipperFactory::SymlinkOption::DONT_PRESERVE)
+          ->Create(),
+      MakePingCallback(), MakeStateCallback(), in_file,
+      base::BindLambdaForTesting(
+          [&](base::expected<base::FilePath, CategorizedError> result) {
+            DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+            ASSERT_FALSE(result.has_value());
+            EXPECT_EQ(result.error().code,
+                      static_cast<int>(UnpackerError::kXzFailed));
+          })
+          .Then(loop_.QuitClosure()));
+  std::move(cancel).Run();
+  loop_.Run();
+
+  EXPECT_FALSE(base::PathExists(in_file));
+  ASSERT_EQ(pings_.size(), 1u);
+  EXPECT_EQ(pings_[0].FindInt("eventtype"), protocol_request::kEventXz);
+  EXPECT_EQ(pings_[0].FindInt("eventresult"),
+            protocol_request::kEventResultError);
+  EXPECT_EQ(pings_[0].FindInt("errorcat"),
+            static_cast<int>(ErrorCategory::kUnpack));
+  EXPECT_EQ(pings_[0].FindInt("errorcode"),
+            static_cast<int>(UnpackerError::kXzFailed));
+  EXPECT_EQ(pings_[0].Find("extracode1"), nullptr);
 }
 
 }  // namespace update_client
