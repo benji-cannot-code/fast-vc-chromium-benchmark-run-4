1c25e4bbac71ac283b9af4ef808a12c97d0df299
Andrea Orru <andreaorru@chromium.org>
2026-02-13 17:59:56 -0800

    [Extensions] Remove DumpWithoutCrash in LoadPersistedLazyListeners
    Very, very occasionally this condition can be hit. See linked bug.
    
    Fixed: 477021532
    Change-Id: Iafcf0e3924a3726e4f1b35647480379a192dbc32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7572806
    Commit-Queue: Andrea Orru <andreaorru@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1585031}

--- extensions/browser/api/web_request/extension_web_request_event_router.cc
+++ extensions/browser/api/web_request/extension_web_request_event_router.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/containers/fixed_flat_map.h"
-#include "base/debug/dump_without_crashing.h"
 #include "base/feature_list.h"
 #include "base/functional/callback_helpers.h"
 #include "base/memory/raw_ptr.h"
@@ -2443,9 +2442,6 @@ void WebRequestEventRouter::LoadPersistedLazyListeners(
     for (const auto& [event_name, listeners] : *listener_map) {
       for (const auto& listener : listeners) {
         if (listener->id.extension_id == extension_id) {
-          // TODO(crbug.com/448893426): remove these for loops if we can verify
-          // this never happens.
-          base::debug::DumpWithoutCrashing();
           return;
         }
       }
