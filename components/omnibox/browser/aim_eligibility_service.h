e555a8945fe0c06b1186632269826c08f0c4486d
Josiah Kiehl <kiehl@google.com>
2026-02-13 16:10:21 -0800

    [aim-model-picker] Provide locale when requesting eligibility
    aim-server-eligibility-include-client-locale flag selects between
    existing GET request (disabled), GET with client_locale query param and
    POST with proto containing client_locale.
    
    Bug: b:481794642
    Change-Id: Ia48bc5359b9d946cbe748d1d717cf8ba383cfe41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7563749
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Auto-Submit: Josiah Kiehl <kiehl@google.com>
    Owners-Override: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Josiah Kiehl <kiehl@google.com>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1584979}

--- components/omnibox/browser/aim_eligibility_service.h
+++ components/omnibox/browser/aim_eligibility_service.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "google_apis/gaia/google_service_auth_error.h"
 #include "net/base/network_change_notifier.h"
+#include "third_party/omnibox_proto/aim_eligibility_client_request.pb.h"
 #include "third_party/omnibox_proto/aim_eligibility_response.pb.h"
 
 class PrefRegistrySimple;
@@ -83,12 +84,24 @@ class AimEligibilityService
   static std::string EligibilityResponseSourceToString(
       EligibilityResponseSource source);
 
+  // Enum describing the eligibility request mode.
+  enum class ServerEligibilityRequestMode {
+    kLegacyGet = 0,
+    kGetWithLocale = 1,
+    kPostWithProto = 2,
+  };
+
+  // Returns the current server eligibility request mode based on the feature
+  // flag configuration.
+  static ServerEligibilityRequestMode GetServerEligibilityRequestMode();
+
   AimEligibilityService(
       PrefService& pref_service,
       TemplateURLService* template_url_service,
       scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
       signin::IdentityManager* identity_manager,
-      bool is_off_the_record);
+      bool is_off_the_record,
+      const std::string& locale);
   ~AimEligibilityService() override;
 
   // Checks if the application country matches the given country.
@@ -231,10 +244,12 @@ class AimEligibilityService
   // e.g., Google is not the default search provider.
   GURL GetRequestUrl(RequestSource request_source,
                      const TemplateURLService* template_url_service,
-                     signin::IdentityManager* identity_manager);
+                     signin::IdentityManager* identity_manager,
+                     const std::string& locale);
 
   // Fetch eligibility from the server.
-  void StartServerEligibilityRequest(RequestSource request_source);
+  void StartServerEligibilityRequest(RequestSource request_source,
+                                     const std::string& locale);
   void OnServerEligibilityResponse(
       std::unique_ptr<network::SimpleURLLoader> loader,
       RequestSource request_source,
