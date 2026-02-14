347303c9a1f398dbf97b7f158fc9836b79252d50
Lukasz Anforowicz <lukasza@chromium.org>
2026-02-13 18:51:28 -0800

    [rust] Add a script for auto-applying Rust/Clippy suggestions.
    After this CL, when `rustc` or `clippy-driver` fail, then a note will be
    written to `stderr` with a `build/rust/apply_fixes.py` invocation that
    will 1) re-run `rustc` or `clippy-driver` with `--error-format=json` and
    2) parse and apply machine-applicable fixes (if any).
    
    This CL moves the list of enabled Clippy lints from `clippy_wrapper.py`
    to `build/config/compiler/BUILD.gn`.  A few notes that explain this:
    
    * `clippy::all` is equivalent to the individual, more granular
      categories that we've listed earlier.  This aspect of the change
      should be a no-op
    * We need to list the lints, because otherwise Clippy would report
      them as warnings, rather than as errors (which we force via
      `-D` = "deny").
    * The same list of lints needs to be used by `clippy_wrapper.py` and by
      `apply_fixes.py` - this is why this CL moves that list into
      `build/config/compiler:default_warnings` so that `-Dclippy::all` will
      just appear in `rustflags` that are saved into `.rustflags.json`.
    
    Bug: 41484295
    Change-Id: I2db9faacf3a42535669342587f9c400d8a37573e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7571236
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Devon Loehr <dloehr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1585049}

--- build/rust/gni_impl/clippy_wrapper.py
+++ build/rust/gni_impl/clippy_wrapper.py
@@ -11,7 +11,8 @@ import pathlib
 import sys
 import tempfile
 
-from rustc_wrapper import (ConvertPathsToAbsolute, LoadRustEnvAndFlags)
+from rustc_wrapper import (ConvertPathsToAbsolute, LoadRustEnvAndFlags,
+                           RecommendApplyFixesScript)
 
 
 def main():
@@ -23,22 +24,6 @@ def main():
 
   (rustenv, rustflags) = LoadRustEnvAndFlags(args.rustc_env_and_flags)
   ConvertPathsToAbsolute(rustenv)
-
-  rustflags += [
-      # Ask `clippy` to treat lint failures either as errors, or ignore
-      # them altogether (i.e. avoid reporting failures as warnings which
-      # are noisy but ignorable).  We cover lint categories from
-      # https://doc.rust-lang.org/stable/clippy/lints.html that are either
-      # `deny` or `warn` by default.
-      #
-      # If certain targets want to opt into additional lints, then they can
-      # do so by using `#![deny(clippy::pedantic)]` or a similar attribute.
-      "-Dclippy::correctness",
-      "-Dclippy::suspicious",
-      "-Dclippy::complexity",
-      "-Dclippy::perf",
-      "-Dclippy::style",
-  ]
 
   # `clippy-driver` should not write any files into the build directory
   # (e.g. into `out/`).
@@ -62,6 +47,7 @@ def main():
   else:
     print("NOTE: See `//docs/rust/clippy.md` for more Clippy info.",
           file=sys.stderr)
+    RecommendApplyFixesScript(args.clippy_driver, args.rustc_env_and_flags)
   return r.returncode
 
 
