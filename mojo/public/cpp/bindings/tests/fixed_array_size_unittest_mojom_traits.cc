e367f66ffbbca599d09a0b0a409c6470a0b9aee7
Daniel Cheng <dcheng@chromium.org>
2026-02-13 16:09:23 -0800

    [mojo] Add fixed-size array validation tests
    These tests verify that deserializing a fixed-size array with the wrong
    number of elements fails with the expected validation error. The tests
    cover fixed-size arrays and variably-sized arrays for thoroughness,
    even though fixed-size and variably-sized arrays have an identical wire
    representation. Also validate that custom StructTraits cannot even get
    a mojo::ArrayDataView with the incorrect number of elements.
    
    Bug: 40061960
    Change-Id: I400c91d90a2adc629063751bf3136756376b1fff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7572918
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1584978}

---
+++ mojo/public/cpp/bindings/tests/fixed_array_size_unittest_mojom_traits.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "mojo/public/cpp/bindings/tests/fixed_array_size_unittest_mojom_traits.h"
+
+namespace mojo::test::fixed_array_size_unittest {
+
+TypemappedStruct3::TypemappedStruct3() = default;
+TypemappedStruct3::~TypemappedStruct3() = default;
+TypemappedStruct3::TypemappedStruct3(const TypemappedStruct3&) = default;
+TypemappedStruct3& TypemappedStruct3::operator=(const TypemappedStruct3&) =
+    default;
+TypemappedStruct3::TypemappedStruct3(TypemappedStruct3&&) = default;
+TypemappedStruct3& TypemappedStruct3::operator=(TypemappedStruct3&&) = default;
+
+}  // namespace mojo::test::fixed_array_size_unittest
