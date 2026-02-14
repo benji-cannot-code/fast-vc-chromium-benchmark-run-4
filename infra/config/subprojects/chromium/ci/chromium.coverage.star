37dd62abb05a95cd5c6be6affb963f76886275bc
Manuel Briones <manuelbriones@google.com>
2026-02-13 16:27:36 -0800

    Add timeout for time intensive tests
    On time intensive builders like linux-code-coverage test involving I/O
    bound operations like webgpu tests may timeout due to lack of resources,
    particularly lack of time.
    
    Bug: 475855328
    Change-Id: I5c73db2cacf57c6f1916a556a655d925365240a6
    Signed-off-by: Manuel Briones <manuelbriones@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7522226
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1584991}

--- infra/config/subprojects/chromium/ci/chromium.coverage.star
+++ infra/config/subprojects/chromium/ci/chromium.coverage.star
@@ -1418,6 +1418,7 @@ coverage_builder(
             "not_site_per_process_blink_web_tests": targets.mixin(
                 args = [
                     "--additional-env-var=LLVM_PROFILE_FILE=${ISOLATED_OUTDIR}/profraw/default-%2m%c.profraw",
+                    "--timeout-multiplier=5",
                 ],
                 swarming = targets.swarming(
                     shards = 20,
--- third_party/blink/tools/blinkpy/web_tests/command_line.py
+++ third_party/blink/tools/blinkpy/web_tests/command_line.py
@@ -299,6 +299,11 @@ def add_testing_options_group(parser: argparse.ArgumentParser,
         action='store_true',
         help='Enable the leak detection of DOM objects.')
     testing_group.add_argument(
+        '--timeout-multiplier',
+        type=float,
+        default=1.0,
+        help='Multiplier to apply to default test timeouts.')
+    testing_group.add_argument(
         '--enable-sanitizer',
         action='store_true',
         help='Only alert on sanitizer-related errors and crashes')
@@ -580,10 +585,6 @@ def add_testing_options_group(parser: argparse.ArgumentParser,
                   'failures are expected in *-expected.txt.'))
     else:
         test_types = get_args(TestType)
-        testing_group.add_argument(
-            '--timeout-multiplier',
-            type=float,
-            help='Multiplier relative to standard test timeouts to use')
         testing_group.add_argument(
             '--test-types',
             nargs='*',
