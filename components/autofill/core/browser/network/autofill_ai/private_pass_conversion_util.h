a3439e782305b14b34b56d44f0bbff67b6767186
Florian Leimgruber <fleimgruber@google.com>
2026-02-16 03:57:25 -0800

    Reland "EntityInstance -> PrivatePass conversion skeleton"
    This is a reland of commit bb336b0e053449ffd7a865f2154ab98c98a9c7fd
    Due to some mistakes while rebasing CLs, adding a call to AnyWrapProto()
    at one of the call sites was missed, resulting in a compilations error.
    It was not caught by CQ, since CQ didn't rerun after rebasing. See
    diff between batchset 1 and latest.
    
    Original change's description:
    > EntityInstance -> PrivatePass conversion skeleton
    >
    > To call the Wallet API, it is necessary to convert from the
    > component/autofill representation of a private pass to the
    > representation used in components/wallet.
    >
    > This CL adds a new util file responsible for this logic. The CL doesn't
    > implement pass-specific conversion logic yet, but it does implement
    > logic shared across passes:
    > * Setting of the ID.
    > * Serialisation of metadata. This bit is shared with the sync code,
    >   since the same serialised metadata will be received again from sync.
    >
    > Bug: 478783796
    > Change-Id: I5e8e7bf319845e26e93b509e6b40076b15385a7d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7566588
    > Reviewed-by: Norge Vizcay <vizcay@google.com>
    > Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1584664}
    
    Bug: 478783796
    Change-Id: Id06ce5c30f70bac3752cb6fe0bc1009b8248d94c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7580094
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1585367}

---
+++ components/autofill/core/browser/network/autofill_ai/private_pass_conversion_util.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_AUTOFILL_CORE_BROWSER_NETWORK_AUTOFILL_AI_PRIVATE_PASS_CONVERSION_UTIL_H_
+#define COMPONENTS_AUTOFILL_CORE_BROWSER_NETWORK_AUTOFILL_AI_PRIVATE_PASS_CONVERSION_UTIL_H_
+
+namespace wallet {
+class PrivatePass;
+}
+
+namespace autofill {
+
+class EntityInstance;
+
+// Converts an `EntityInstance` of a private pass type to a
+// `wallet::PrivatePass`, used for communication with the Wallet API.
+wallet::PrivatePass EntityInstanceToPrivatePass(const EntityInstance& entity);
+
+}  // namespace autofill
+
+#endif  // COMPONENTS_AUTOFILL_CORE_BROWSER_NETWORK_AUTOFILL_AI_PRIVATE_PASS_CONVERSION_UTIL_H_
