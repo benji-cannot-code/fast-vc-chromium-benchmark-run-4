8ed921998072d5ed2003bae48dd9e79f9f96193c
Jun Ishiguro <junis@google.com>
2026-02-16 03:19:41 -0800

    login: Pass ApplicationLocaleStorage to LocaleSwitchNotificationDelegate
    Bug: b:404133029
    Change-Id: Idfe596380278c52e3de979eb59ed6f2364b77f07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7580916
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1585359}

--- chrome/browser/ash/login/screens/locale_switch_notification.h
+++ chrome/browser/ash/login/screens/locale_switch_notification.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "chrome/browser/ash/base/locale_util.h"
 
+class ApplicationLocaleStorage;
 class Profile;
 
 namespace ash {
@@ -20,7 +21,10 @@ namespace ash {
 class LocaleSwitchNotification {
  public:
   // Show locale switch notification.
-  static void Show(Profile* profile,
+  // `application_locale_storage` must be non-null and must outlive the created
+  // notification. It should be the one associated with the browser process.
+  static void Show(ApplicationLocaleStorage* application_locale_storage,
+                   Profile* profile,
                    std::string new_locale,
                    locale_util::SwitchLanguageCallback locale_switch_callback);
 };
