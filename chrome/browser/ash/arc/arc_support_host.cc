acea407d488aae3392a64ba94dffd108c0edcc77
Hidehiko Abe <hidehiko@chromium.org>
2026-02-15 19:06:18 -0800

    Fix settings link of ArcSupportHost.
    The settings link is expected to navigate users to the page
    controlling data reporting, but that was linked to chrome browser's
    privacy settings page. This CL fixes it to forward users to
    the OS privacy settings page.
    
    BUG=482277902, 481835800
    TEST=Tryjob, tested on DUT.
    
    Change-Id: Ic7312b23229e5f5ede01f6b77dc013f8c8cc4c0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7556318
    Reviewed-by: Di Wu <diwux@google.com>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1585256}

--- chrome/browser/ash/arc/arc_support_host.cc
+++ chrome/browser/ash/arc/arc_support_host.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "ash/constants/ash_features.h"
+#include "ash/webui/settings/public/constants/routes.mojom.h"
 #include "base/check_deref.h"
 #include "base/functional/bind.h"
 #include "base/i18n/timezone.h"
@@ -25,14 +26,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/ui/ash/multi_user/multi_user_util.h"
-#include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/browser/ui/extensions/app_launch_params.h"
 #include "chrome/browser/ui/extensions/application_launch.h"
 #include "chrome/browser/ui/webui/ash/diagnostics_dialog/diagnostics_dialog.h"
 #include "chrome/common/url_constants.h"
-#include "chrome/common/webui_url_constants.h"
 #include "chrome/grit/generated_resources.h"
+#include "chromeos/ash/components/browser_context_helper/browser_context_helper.h"
 #include "chromeos/ash/experiences/arc/app/arc_app_constants.h"
+#include "chromeos/ash/experiences/settings_ui/settings_app_manager.h"
 #include "components/application_locale_storage/application_locale_storage.h"
 #include "components/consent_auditor/consent_auditor.h"
 #include "components/signin/public/base/consent_level.h"
@@ -846,7 +847,11 @@ void ArcSupportHost::OnMessage(const base::DictValue& message) {
     error_delegate_->OnErrorPageShown(
         message.FindBool(kNetworkTestsShown).value_or(false));
   } else if (*event == kEventOnOpenPrivacySettingsPageClicked) {
-    chrome::ShowSettingsSubPageForProfile(profile_, chrome::kPrivacySubPage);
+    auto* user =
+        ash::BrowserContextHelper::Get()->GetUserByBrowserContext(profile_);
+    ash::SettingsAppManager::Get()->Open(
+        CHECK_DEREF(user),
+        {.sub_page = chromeos::settings::mojom::kPrivacyHubSubpagePath});
   } else if (*event == kEventRequestWindowBounds) {
     SetWindowBound(display::Screen::Get()->GetDisplayForNewWindows());
   } else {
