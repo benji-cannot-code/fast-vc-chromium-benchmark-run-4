b9bb89798fa10280d68cacdae5b135bc9152fe81
Federica Germinario <fedegermi@google.com>
2026-02-16 09:03:46 -0800

    [ios] Use kGaiaIDQueryItemName in SceneController
    Replaces the hardcoded "gaia_id" string with the defined constant
    app_group::kGaiaIDQueryItemName.
    
    Bug: none
    Change-Id: I69152efa4e586df97f283205ab4c0cd042949a6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7581677
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1585459}

--- ios/chrome/browser/shared/coordinator/scene/scene_controller.mm
+++ ios/chrome/browser/shared/coordinator/scene/scene_controller.mm
@@ -837,8 +837,8 @@ void InjectUnrealizedWebStates(Browser* browser, int count) {
   int accountChanges = 0;
   for (UIOpenURLContext* context : URLContexts) {
     std::string newGaia;
-    if (net::GetValueForKeyInQuery(net::GURLWithNSURL(context.URL), "gaia_id",
-                                   &newGaia)) {
+    if (net::GetValueForKeyInQuery(net::GURLWithNSURL(context.URL),
+                                   app_group::kGaiaIDQueryItemName, &newGaia)) {
       accountChanges++;
     }
   }
@@ -872,7 +872,8 @@ void InjectUnrealizedWebStates(Browser* browser, int count) {
     std::string newGaia;
 
     // Continue if the URL does not contain a gaia.
-    if (!net::GetValueForKeyInQuery(net::GURLWithNSURL(context.URL), "gaia_id",
+    if (!net::GetValueForKeyInQuery(net::GURLWithNSURL(context.URL),
+                                    app_group::kGaiaIDQueryItemName,
                                     &newGaia)) {
       continue;
     }
