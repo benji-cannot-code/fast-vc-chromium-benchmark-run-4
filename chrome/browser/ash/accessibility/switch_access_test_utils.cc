5023b6e2e0283fdd638b80eb387868d32e7b1db5
Katie D <katie@chromium.org>
2026-02-17 10:16:47 -0800

    [Switch Access] Remove mv3/ subdirectory
    Now that MV2 files are gone, we can remove the unnecessary
    /mv3 directory. No functional change.
    
    Bypass-Check-License: Files moved, not new
    Bug: b:402206583
    Change-Id: Id16531cc14858a26367448d18ec8174590f68a62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7577853
    Auto-Submit: Katie Dektar <katie@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Reviewed-by: Amanda Lin Dietz <aldietz@google.com>
    Commit-Queue: Amanda Lin Dietz <aldietz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1585853}

--- chrome/browser/ash/accessibility/switch_access_test_utils.cc
+++ chrome/browser/ash/accessibility/switch_access_test_utils.cc
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 constexpr char kTestSupportPathMV3[] =
-    "chrome/browser/resources/chromeos/accessibility/switch_access/mv3/"
+    "chrome/browser/resources/chromeos/accessibility/switch_access/"
     "test_support.js";
 }  // namespace
 
--- chrome/browser/resources/chromeos/accessibility/switch_access/switch_access_e2e_test_base.js
+++ chrome/browser/resources/chromeos/accessibility/switch_access/switch_access_e2e_test_base.js
@@ -4,8 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 GEN_INCLUDE([
-  '../../common/testing/assert_additions.js',
-  '../../common/testing/e2e_test_base.js',
+  '../common/testing/assert_additions.js',
+  '../common/testing/e2e_test_base.js',
 ]);
 
 /** Base class for browser tests for Switch Access. */
