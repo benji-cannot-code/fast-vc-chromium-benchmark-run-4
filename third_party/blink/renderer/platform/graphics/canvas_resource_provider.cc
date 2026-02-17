b87b8479cb251069ee4c2aad0a3352f87363cadc
Colin Blundell <blundell@chromium.org>
2026-02-17 07:50:11 -0800

    [Blink] Remove initialize param from C2DResourceProviderSI SW composite
    All clients pass kCallClear [1].
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.cc;l=1342-1343
    
    Bug: 352263194
    Change-Id: I34d48a13d75e1f70db6bcebb5ed5fb190d219c52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7581316
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1585761}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
@@ -1145,12 +1145,11 @@ Canvas2DResourceProviderSharedImage::CreateForSoftwareCompositor(
     viz::SharedImageFormat format,
     SkAlphaType alpha_type,
     const gfx::ColorSpace& color_space,
-    ShouldInitialize initialize_provider,
     WebGraphicsSharedImageInterfaceProvider* shared_image_interface_provider,
     Delegate* delegate) {
   return CreateSharedImageProviderForSoftwareCompositorBase<
       Canvas2DResourceProviderSharedImage>(
-      size, format, alpha_type, color_space, initialize_provider,
+      size, format, alpha_type, color_space, ShouldInitialize::kCallClear,
       shared_image_interface_provider, delegate);
 }
 
