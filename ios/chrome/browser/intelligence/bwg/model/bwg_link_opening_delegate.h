0afd554067bf28f939944e7255f40c3520d5f9e9
Joe Peplowski <joepeplowski@google.com>
2026-02-17 06:34:36 -0800

    Implement closePresentedViewsAndOpenURLInNewTab
    This CL adds the implementation of
    `closePresentedViewsAndOpenURLInNewTab`, which will be called in the
    future from google3 in order to allow the base Settings view to be
    dismissed when a Gemini Dynamic Settings link is tapped. This will fix a
    bug where links in Dynamic Settings open behind the Settings view.
    
    Additionally, this CL fixes a bug where tapping a link in Gemini while
    Copresence is disabled opens the link without dismissing the Gemini
    conversation.
    
    Bug: 476399222
    Change-Id: I9155943824e194a8471b2d6f0883d19808a11335
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7568582
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Commit-Queue: Joe Peplowski <joepeplowski@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1585705}

--- ios/chrome/browser/intelligence/bwg/model/bwg_link_opening_delegate.h
+++ ios/chrome/browser/intelligence/bwg/model/bwg_link_opening_delegate.h
@@ -14,6 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Opens a given URL in a new tab.
 - (void)openURLInNewTab:(NSString*)URL;
 
+// Closes any presented views and opens the given URL in a new tab.
+- (void)closePresentedViewsAndOpenURLInNewTab:(NSString*)URL;
+
 @end
 
 #endif  // IOS_CHROME_BROWSER_INTELLIGENCE_BWG_MODEL_BWG_LINK_OPENING_DELEGATE_H_
--- ios/chrome/browser/intelligence/bwg/model/bwg_link_opening_handler.h
+++ ios/chrome/browser/intelligence/bwg/model/bwg_link_opening_handler.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class UrlLoadingBrowserAgent;
 
+@class CommandDispatcher;
 @protocol GeminiViewStateDelegate;
 
 // The handler for opening links for BWG.
@@ -21,7 +22,11 @@ class UrlLoadingBrowserAgent;
 @property(nonatomic, weak) id<GeminiViewStateDelegate> geminiViewStateDelegate;
 
 // Initialize the handler with a URL loading browser agent.
+// In order to prevent a crash, we pass the 'CommandDispatcher' directly instead
+// of a command handler. This is because command handlers fail the protocol
+// conformance test at startup time, which is when this initializer get called.
 - (instancetype)initWithURLLoader:(UrlLoadingBrowserAgent*)URLLoadingAgent
+                       dispatcher:(CommandDispatcher*)dispatcher
     NS_DESIGNATED_INITIALIZER;
 - (instancetype)init NS_UNAVAILABLE;
 
