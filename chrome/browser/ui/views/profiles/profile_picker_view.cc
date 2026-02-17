fdd03fd9c33322e171c0eefa48a3e6a5f8304668
David Roger <droger@chromium.org>
2026-02-17 03:05:45 -0800

    [mac] Focus Profile Picker on reopen if visible
    When the dock icon is clicked and no browser windows are open,
    AppController currently checks for visible browser windows. If none are
    found, it proceeds to open a new window or show the profile picker, but
    it might not handle the case where the Profile Picker is already open
    correctly (or rather, it treats it as if there were "no visible
    windows").
    
    This CL adds a check for `ProfilePicker::IsOpen()` in
    `applicationShouldHandleReopen:`. If the picker is open, it calls
    `ProfilePicker::Show()` to bring it to the front and returns `NO` to
    indicate the event was handled.
    
    A regression test `ProfilePickerReopenFocus` is added to
    `AppControllerInteractiveUITest`.
    
    Fixed: 429522811
    Change-Id: I961c145d7f9972627ef9ac7fd2cc6fe63077fd35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7581553
    Reviewed-by: Amelie Schneider <amelies@google.com>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1585632}

--- chrome/browser/ui/views/profiles/profile_picker_view.cc
+++ chrome/browser/ui/views/profiles/profile_picker_view.cc
@@ -536,6 +536,7 @@ ProfilePickerView::ProfilePickerView(ProfilePicker::Params&& params)
   // Setup the WidgetDelegate.
   SetHasWindowSizeControls(true);
   SetTitle(kWindowTitleId);
+  SetProperty(views::kElementIdentifierKey, kViewId);
 
   ConfigureAccelerators();
 
@@ -938,3 +939,5 @@ void ProfilePickerView::ShowSigninErrorDialog(
 
 BEGIN_METADATA(ProfilePickerView)
 END_METADATA
+
+DEFINE_CLASS_ELEMENT_IDENTIFIER_VALUE(ProfilePickerView, kViewId);
