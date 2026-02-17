bc521b0880f0105d1ad30d27020d9b18c0bbe8b9
Abe Boujane <boujane@google.com>
2026-02-17 08:49:23 -0800

    Remove Privacy Sandbox Sentiment Survey on Desktop
    This cleanup includes:
    
    - Removing PrivacySandboxSurveyDesktopController and PrivacySandboxTabObserver.
    - Removing the PrivacySandboxSentimentSurvey feature flag.
    - Removing the PrivacySandbox.SentimentSurvey.Status histogram and PrivacySandboxSentimentSurveyStatus enum.
    - Removing references in chrome/browser/ui/hats/survey_config.*.
    - Removing references in chrome/browser/ui/tabs/tab_features.*.
    - Cleaning up BUILD.gn
    - remove histograms
    
    Bug: 474716334
    
    OBSOLETE_HISTOGRAM[PrivacySandbox.SentimentSurvey.Status]=The Privacy Sandbox Sentiment Survey is no longer being used.
    
    Change-Id: Ibc2b7b9bc263e6c55f056eb6390a9e0d4bbdea69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7575814
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Auto-Submit: Abe Boujane <boujane@google.com>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1585801}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -342,7 +342,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/password_manager/factories/password_counter_factory.h"
 #include "chrome/browser/payments/payment_request_display_manager_factory.h"
 #include "chrome/browser/prefs/persistent_renderer_prefs_manager_factory.h"
-#include "chrome/browser/privacy_sandbox/privacy_sandbox_survey_desktop_controller_factory.h"
 #include "chrome/browser/profile_resetter/reset_report_uploader_factory.h"
 #include "chrome/browser/record_replay/recording_data_manager_factory.h"
 #include "chrome/browser/screen_ai/screen_ai_service_router_factory.h"
@@ -1233,9 +1232,6 @@ void ChromeBrowserMainExtraPartsProfiles::
   PrivacySandboxNoticeServiceFactory::GetInstance();
   PrivacySandboxServiceFactory::GetInstance();
   PrivacySandboxSettingsFactory::GetInstance();
-#if !BUILDFLAG(IS_ANDROID)
-  PrivacySandboxSurveyDesktopControllerFactory::GetInstance();
-#endif
 #if BUILDFLAG(IS_ANDROID)
   PrivacySandboxActivityTypesFactory::GetInstance();
 #endif
