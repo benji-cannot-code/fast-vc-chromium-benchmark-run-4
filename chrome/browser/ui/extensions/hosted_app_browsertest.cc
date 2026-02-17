76695818b8f683fb3ca99248fd1ca2e581c5dfb6
Jagadesh P <jagadeshjai1999@gmail.com>
2026-02-17 11:40:38 -0800

    Fix dangling pointer in |HostedAppProcessModelTest|
    Bug: 40212619
    Change-Id: I6d9f4402e68f8425ff2547cad25691b8a3faff84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7545120
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: Jagadesh Jai <jagadeshjai1999@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1585909}

--- chrome/browser/ui/extensions/hosted_app_browsertest.cc
+++ chrome/browser/ui/extensions/hosted_app_browsertest.cc
@@ -1008,6 +1008,11 @@ class HostedAppProcessModelTest : public HostedOrWebAppTest {
         embedded_test_server()->GetURL("cross.domain.com", "/title1.html");
   }
 
+  void TearDownOnMainThread() override {
+    process_map_ = nullptr;
+    HostedOrWebAppTest::TearDownOnMainThread();
+  }
+
   // Opens a popup from |rfh| to |url|, verifies whether it should stay in the
   // same process as |rfh| and whether it should be in an app process, and then
   // closes the popup.
@@ -1104,7 +1109,7 @@ class HostedAppProcessModelTest : public HostedOrWebAppTest {
  protected:
   bool should_swap_for_cross_site_;
 
-  raw_ptr<extensions::ProcessMap, DanglingUntriaged> process_map_;
+  raw_ptr<extensions::ProcessMap> process_map_ = nullptr;
 
   GURL same_dir_url_;
   GURL diff_dir_url_;
