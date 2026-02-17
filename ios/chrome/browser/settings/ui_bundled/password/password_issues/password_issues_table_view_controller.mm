8d4a9de6e02476871c582feae9a2f7f896d4bbf4
Benjamin Williams <bwwilliams@google.com>
2026-02-17 13:24:57 -0800

    [iOS] Prevent index out-of-bounds crash in favicon callback
    Similar to crrev.com/c/7583219, this change prevents an index
    out-of-bounds crash caused by a race condition during async favicon
    fetching. Previously, if the user modified the underlying table model
    while a favicon was fetching, the completion block may attempt to use a
    now-invalid index path.
    
    This fix adds a -hasItemAtIndexPath: check to safely abort the callback
    if the item no longer exists.
    
    This CL was uploaded by git cl split.
    
    R=eic@google.com
    
    Bug: 485195553
    Change-Id: I77af32442bafc87199782780e564d66db11d83a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7585495
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Auto-Submit: Benjamin Williams <bwwilliams@google.com>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Reviewed-by: Ernesto Izquierdo Clua <eic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1585969}

--- ios/chrome/browser/settings/ui_bundled/password/password_issues/password_issues_table_view_controller.mm
+++ ios/chrome/browser/settings/ui_bundled/password/password_issues/password_issues_table_view_controller.mm
@@ -463,7 +463,8 @@ typedef NS_ENUM(NSInteger, ItemType) {
                         indexPath:(NSIndexPath*)indexPath {
   item.faviconAttributes = attributes;
   if (!cached && attributes.faviconImage) {
-    if ([self.tableViewModel itemAtIndexPath:indexPath] != item) {
+    if (![self.tableViewModel hasItemAtIndexPath:indexPath] ||
+        [self.tableViewModel itemAtIndexPath:indexPath] != item) {
       return;
     }
     LegacyTableViewCell* cell =
