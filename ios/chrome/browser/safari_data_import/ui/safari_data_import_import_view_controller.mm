95db4a5349b34d15374d46b4ab169b6e645e5065
Daniel White <danieltwhite@google.com>
2026-02-17 01:33:32 -0800

    [iOS] Remove TraitCollectionSetForTraits invocation
    In this CL, the TraitCollectionSetForTraits was removed from the file.
    This CL is part of a wider effort to remove the
    TraitCollectionSetForTraits function from the codebase.
    
    Bug: 483670636
    Change-Id: I59994759c73831a0ddbbce4ec3a84e62693b68c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7568643
    Auto-Submit: Daniel White <danieltwhite@google.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1585605}

--- ios/chrome/browser/safari_data_import/ui/safari_data_import_import_view_controller.mm
+++ ios/chrome/browser/safari_data_import/ui/safari_data_import_import_view_controller.mm
@@ -97,14 +97,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /// The banner image should be hidden on iPhone landscape mode. This method
 /// makes sure of that when the user rotates the device.
 - (void)makeBannerImageVisibilityAdaptive {
-  NSArray<UITrait>* traits =
-      TraitCollectionSetForTraits(@[ UITraitVerticalSizeClass.class ]);
   __weak __typeof(self) weakSelf = self;
   UITraitChangeHandler handler = ^(id<UITraitEnvironment> traitEnvironment,
                                    UITraitCollection* previousCollection) {
     weakSelf.shouldHideBanner = IsCompactHeight(traitEnvironment);
   };
-  [self registerForTraitChanges:traits withHandler:handler];
+  [self registerForTraitChanges:@[ UITraitVerticalSizeClass.class ]
+                    withHandler:handler];
 }
 
 /// Adds an instructional view to show import steps  to the view hierarchy and
