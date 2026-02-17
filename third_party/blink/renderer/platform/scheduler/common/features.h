c320890587748e11f843eccfbac97129b364fa6d
Anand Ravi <anandrv@google.com>
2026-02-17 08:21:37 -0800

    [blink] Add unit tests for BusyLoopOnRendererMain
    Bug: 390032577
    Change-Id: I81be277455adcd995ee9558a6a26d052667c6db4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7540113
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Commit-Queue: Anand Ravi <anandrv@google.com>
    Reviewed-by: Scott Haseley <shaseley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1585784}

--- third_party/blink/renderer/platform/scheduler/common/features.h
+++ third_party/blink/renderer/platform/scheduler/common/features.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_SCHEDULER_COMMON_FEATURES_H_
 
 #include "base/feature_list.h"
+#include "base/metrics/field_trial_params.h"
 #include "base/time/time.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
 
@@ -61,6 +62,9 @@ PLATFORM_EXPORT base::TimeDelta GetIntensiveWakeUpThrottlingGracePeriod(
 BASE_FEATURE(kMbiOverrideTaskRunnerHandle,
              "MbiOverrideTaskRunnerHandle",
              base::FEATURE_DISABLED_BY_DEFAULT);
+
+PLATFORM_EXPORT BASE_DECLARE_FEATURE(kBusyLoopOnRendererMain);
+PLATFORM_EXPORT BASE_DECLARE_FEATURE_PARAM(base::TimeDelta, kBusyLoopTime);
 
 }  // namespace scheduler
 }  // namespace blink
