f2e2279002db4ae3c4fa913a9ab86332367ba1ec
Eshwar Stalin <estalin@chromium.org>
2026-02-16 21:24:14 -0800

    Cleaning up unused contents rounded corner file
    The functionality provided by ContentsRoundedCorner was replaced by
    CustomCornersBackground and CustomFloatingCorner. This CL removes the
    deprecated view which is unused.
    
    Change-Id: Idd724e5cc620b3a5105c161f2090dba133cbac30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7580620
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1585562}

--- chrome/browser/ui/views/frame/multi_contents_view.cc
+++ chrome/browser/ui/views/frame/multi_contents_view.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/contents_container_view.h"
-#include "chrome/browser/ui/views/frame/contents_rounded_corner.h"
 #include "chrome/browser/ui/views/frame/contents_separator.h"
 #include "chrome/browser/ui/views/frame/contents_web_view.h"
 #include "chrome/browser/ui/views/frame/custom_floating_corner.h"
