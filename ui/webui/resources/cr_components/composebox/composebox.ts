75585a9e774f7f7f2f2c92ae2298bc6855f3ed98
Marlon Facey <mfacey@chromium.org>
2026-02-17 15:55:57 -0800

    [Composebox] Fix smart compose nudge with just space character
    Only populate smart compose hint if trimmed version is not empty. This
    should be handled server side but extra check in case we do get sent a
    string with just a space.
    
    Bug: 452701012
    Change-Id: Ie98a32bf7a4d532ad4e35b416acfb8b5c19fe80e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7585174
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1586063}

--- ui/webui/resources/cr_components/composebox/composebox.ts
+++ ui/webui/resources/cr_components/composebox/composebox.ts
@@ -1357,7 +1357,7 @@ export class ComposeboxElement extends I18nMixinLit
     }
 
     // Populate the smart compose suggestion.
-    this.smartComposeInlineHint_ = this.result_.smartComposeInlineHint ?
+    this.smartComposeInlineHint_ = this.result_.smartComposeInlineHint?.trim() ?
         this.result_.smartComposeInlineHint :
         '';
   }
