cf7b9af4a6720ea54a665aeb047016395d7cd04a
chrstne <chrstne@google.com>
2026-02-18 12:17:53 -0800

    Change chrome://skills zero state image
    + CL also adjusts the description to condense based on mocks provided
    
    Screencast: https://screencast.googleplex.com/cast/NTI3NjI1MDc3OTE1NjQ4MHxjMjVhMzgxZS1kNg
    
    Bug: b:485304727
    Change-Id: I35075183660a2cae054caeab012a13831f9362d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7586714
    Commit-Queue: Christine Ying <chrstne@google.com>
    Reviewed-by: Aashna Sheth <aashnas@google.com>
    Cr-Commit-Position: refs/heads/main@{#1586621}

---
+++ chrome/browser/resources/skills/skills_empty.ts
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {CrLitElement} from 'chrome://resources/lit/v3_0/lit.rollup.js';
+
+import {getHtml} from './skills_empty.html.js';
+
+export class SkillsEmptyElement extends CrLitElement {
+  static get is() {
+    return 'skills-empty';
+  }
+
+  override render() {
+    return getHtml.bind(this)();
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'skills-empty': SkillsEmptyElement;
+  }
+}
+
+customElements.define(SkillsEmptyElement.is, SkillsEmptyElement);
