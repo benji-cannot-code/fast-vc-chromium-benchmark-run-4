55a750336757d7bfba38d01e39e31bb41c37ac62
Colin Blundell <blundell@chromium.org>
2026-02-18 00:04:06 -0800

    [Blink] Update GMB references in CanvasRenderingContext2D test
    The point of setting this cap here is so that this check [1] will pass;
    update the comments accordingly.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/gpu/graphics_context_3d_utils.cc;l=42;drc=6d3e2ca997ca3ef995f719b22a1658a250a22d1e;bpv=1;bpt=1
    
    Bug: 404905709
    Change-Id: I4b24cc37f4b8e68b8fb6c0585ab6b7c4c8325702
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7584673
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1586255}

--- third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d_test.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d_test.cc
@@ -1719,9 +1719,9 @@ TEST_P(CanvasRenderingContext2DTest,
        SoftwareCanvasIsCompositedIfImageChromium) {
   ScopedCanvas2dImageChromiumForTest canvas_2d_image_chromium(true);
 
-  // Ensure that native support for BGRA GMBs is present, as otherwise
-  // compositing will not occur irrespective of whether
-  // `ScopedCanvas2dImageChromium` is enabled.
+  // Ensure that support for BGRA overlays is present, as otherwise compositing
+  // will not occur irrespective of whether `ScopedCanvas2dImageChromium` is
+  // enabled.
   ScopedTestingPlatformSupport<GpuCompositingTestPlatform> platform;
   const_cast<gpu::Capabilities&>(SharedGpuContext::ContextProviderWrapper()
                                      ->ContextProvider()
@@ -1744,9 +1744,9 @@ TEST_P(CanvasRenderingContext2DTest,
   CreateContext(kNonOpaque);
   EXPECT_TRUE(Context2D()->GetOrCreateResourceProvider());
 
-  // Ensure that native support for BGRA GMBs is present, as otherwise
-  // compositing will not occur irrespective of whether
-  // `ScopedCanvas2dImageChromium` is enabled.
+  // Ensure that support for BGRA overlays is present, as otherwise compositing
+  // will not occur irrespective of whether `ScopedCanvas2dImageChromium` is
+  // enabled.
   ScopedTestingPlatformSupport<GpuCompositingTestPlatform> platform;
   const_cast<gpu::Capabilities&>(SharedGpuContext::ContextProviderWrapper()
                                      ->ContextProvider()
