de1f4853e5397f47d744b74b8091e19dd649a1a1
Diana OuYang <dianaou@google.com>
2026-02-17 18:09:33 -0800

    Add scrollbar optionality to context menu
    Adds logic and UI for a scrolling context menu view.
    - Gated by enableScrolling
    - Shows two rows of chips at a time
    - Scrolls to bottom when a new chip is added
    
    Demo: https://screencast.googleplex.com/cast/NjI1OTY4MDcxNTIxMDc1Mnw0NmM4ZDBhYy0wOA
    Bug: b:483189410
    Change-Id: I7c239db6bf215fd6805008b35b9841ab8759f860
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7568944
    Commit-Queue: Diana OuYang <dianaou@google.com>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1586138}

--- ui/webui/resources/cr_components/composebox/contextual_entrypoint_and_carousel.ts
+++ ui/webui/resources/cr_components/composebox/contextual_entrypoint_and_carousel.ts
@@ -131,6 +131,7 @@ export class ContextualEntrypointAndCarouselElement extends I18nMixinLit
         type: Boolean,
         reflect: true,
       },
+      enableCarouselScrolling: {type: Boolean},
       showRecentTabChip: {type: Boolean},
       recentTabForChip: {type: Object},
 
@@ -180,6 +181,7 @@ export class ContextualEntrypointAndCarouselElement extends I18nMixinLit
       GlifAnimationState.INELIGIBLE;
   accessor inComposebox: boolean = false;
   accessor showModelPicker: boolean = false;
+  accessor enableCarouselScrolling: boolean = false;
   accessor isOmniboxInCompactMode_: boolean = false;
   accessor recentTabForChip: TabInfo|null = null;
 
