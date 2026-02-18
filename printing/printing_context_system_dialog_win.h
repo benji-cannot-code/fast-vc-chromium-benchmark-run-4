ebae14415bbfd1a0069c08bd8262bef1b33b28a3
David Bienvenu <davidbienvenu@chromium.org>
2026-02-17 18:37:05 -0800

    Fix UNSAFE_TODOS in printing code
    Use base::span (mostly) to fix UNSAFE_TODOs.
    
    Also add :: in front of Win32 API calls, since it's useful to be
    able to tell quickly if we have control over a function's behavior.
    
    Bug: 42271176
    Change-Id: I422e38355972e95518f9aa51e1644bdce05f0379
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7558994
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1586147}

--- printing/printing_context_system_dialog_win.h
+++ printing/printing_context_system_dialog_win.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/containers/span.h"
 #include "printing/mojom/print.mojom.h"
 #include "printing/printing_context_win.h"
 #include "ui/gfx/native_ui_types.h"
@@ -46,8 +47,7 @@ class COMPONENT_EXPORT(PRINTING) PrintingContextSystemDialogWin
   // its margins.
   bool InitializeSettingsWithRanges(const DEVMODE& dev_mode,
                                     const std::wstring& new_device_name,
-                                    const PRINTPAGERANGE* ranges,
-                                    int number_ranges,
+                                    base::span<const PRINTPAGERANGE> pages_span,
                                     bool selection_only);
 
   // Parses the result of a PRINTDLGEX result.
