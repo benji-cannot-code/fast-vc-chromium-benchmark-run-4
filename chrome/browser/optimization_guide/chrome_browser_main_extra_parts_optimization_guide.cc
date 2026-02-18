efe96333f8def0a5084fe0a63c36255e6560f37d
Zekun Jiang <zekunjiang@google.com>
2026-02-17 16:22:38 -0800

    Add logging for VRAM and installed model on start up, for desktop platforms only.
    Bug: 476220785
    Change-Id: Ibd5e68bc7f60471e278554688f5547a6c6f934d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7541047
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Zekun Jiang <zekunjiang@google.com>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1586077}

---
+++ chrome/browser/optimization_guide/chrome_browser_main_extra_parts_optimization_guide.cc
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/optimization_guide/chrome_browser_main_extra_parts_optimization_guide.h"
+
+#include "chrome/browser/optimization_guide/model_execution/optimization_guide_global_state.h"
+
+// Ensures global state initialization, so we can have reliable logging of
+// on-device model metrics post start-up.
+void ChromeBrowserMainExtraPartsOptimizationGuide::PostBrowserStart() {
+  optimization_guide::OptimizationGuideGlobalState::CreateOrGet();
+}
