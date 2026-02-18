4e0a8d516bd0e0fa5d7dd4b82a9ca903275e9e4a
Anthony Vallée-Dubois <anthonyvd@chromium.org>
2026-02-18 07:23:48 -0800

    Revert "Change ProcessInfo to be keyed by type + subtype"
    This reverts commit f19a9ee3a8478f2ed062bd87a215113a772315b1.
    
    Reason for revert: Breaks CPU metrics
    
    Bug: 485291343
    
    Original change's description:
    > Change ProcessInfo to be keyed by type + subtype
    >
    > This will allow suffixing CPU histograms by utility process subtypes.
    >
    > Bug: 482350910
    > Change-Id: Iefdba9666b978d93020e6aa8780f4fa13b6e2bb4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7542225
    > Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    > Reviewed-by: Olivier Li <olivierli@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1582613}
    
    Bug: 482350910
    Change-Id: Id215c9ced08310ca70c1eb15d4cb617ae6a8f95e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7585456
    Auto-Submit: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Olivier Li <olivierli@chromium.org>
    Commit-Queue: Olivier Li <olivierli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1586426}

--- chrome/browser/metrics/power/process_monitor_unittest.cc
+++ chrome/browser/metrics/power/process_monitor_unittest.cc
@@ -12,38 +12,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/network/public/mojom/network_service.mojom.h"
 TEST(ProcessMonitorTest, MonitoredProcessType) {
-  const std::pair<int, ProcessInfo::Key> kExpectations[] = {
-      {content::PROCESS_TYPE_GPU, {MonitoredProcessType::kGpu, std::nullopt}},
-      // The ChildProcessData's metrics_name isn't optional, so the subtype is
-      // ""
-      {content::PROCESS_TYPE_UTILITY, {MonitoredProcessType::kUtility, ""}},
-      {content::PROCESS_TYPE_ZYGOTE,
-       {MonitoredProcessType::kOther, std::nullopt}},
+  const std::pair<int, MonitoredProcessType> kExpectations[] = {
+      {content::PROCESS_TYPE_GPU, MonitoredProcessType::kGpu},
+      {content::PROCESS_TYPE_UTILITY, MonitoredProcessType::kUtility},
+      {content::PROCESS_TYPE_ZYGOTE, MonitoredProcessType::kOther},
   };
 
   for (const auto& [process_type, expectation] : kExpectations) {
     content::ChildProcessData data(process_type, content::ChildProcessId());
-    auto actual =
-        GetMonitoredProcessInfoKeyForNonRendererChildProcessForTesting(data);
-    EXPECT_EQ(actual.type, expectation.type);
-    EXPECT_EQ(actual.subtype, expectation.subtype);
+    EXPECT_EQ(GetMonitoredProcessTypeForNonRendererChildProcessForTesting(data),
+              expectation);
   }
-
-  content::ChildProcessData utility_data(content::PROCESS_TYPE_UTILITY,
-                                         content::ChildProcessId());
-  utility_data.metrics_name = "TestName";
-  ProcessInfo::Key expected_utility_key{MonitoredProcessType::kUtility,
-                                        "TestName"};
-  EXPECT_EQ(GetMonitoredProcessInfoKeyForNonRendererChildProcessForTesting(
-                utility_data),
-            expected_utility_key);
 
   // Special case for network process.
   content::ChildProcessData data(content::PROCESS_TYPE_UTILITY,
                                  content::ChildProcessId());
   data.metrics_name = network::mojom::NetworkService::Name_;
-  ProcessInfo::Key expected_key{MonitoredProcessType::kNetwork, std::nullopt};
-  EXPECT_EQ(
-      GetMonitoredProcessInfoKeyForNonRendererChildProcessForTesting(data),
-      expected_key);
+  EXPECT_EQ(GetMonitoredProcessTypeForNonRendererChildProcessForTesting(data),
+            MonitoredProcessType::kNetwork);
 }
