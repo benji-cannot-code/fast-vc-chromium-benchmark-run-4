cee24c31dea1cf8339baf60fe138412155e43cdd
Lauren Winston <lwinston@google.com>
2026-02-18 10:12:25 -0800

    [Read anything] Disable the omnibox in app windows.
    Bug: chromium:462249711
    Change-Id: I76434f2624e1ff669a6aadda1f82af50509187ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7499504
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1586534}

--- chrome/browser/ui/read_anything/read_anything_entry_point_controller.cc
+++ chrome/browser/ui/read_anything/read_anything_entry_point_controller.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "chrome/browser/dom_distiller/tab_utils.h"
+#include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_features.h"
 #include "chrome/browser/ui/read_anything/read_anything_controller.h"
 #include "chrome/browser/ui/read_anything/read_anything_enums.h"
@@ -204,6 +205,13 @@ void ReadAnythingEntryPointController::UpdatePageActionVisibility(
 bool ReadAnythingEntryPointController::CheckIfShouldSuggestReadingModeNaive(
     BrowserWindowInterface* bwi) {
   if (!features::IsReadAnythingOmniboxChipEnabled() || !bwi) {
+    return false;
+  }
+
+  // Disable the omnibox on app windows, as these windows don't usually have
+  // omnibox support.
+  Browser* browser = bwi->GetBrowserForMigrationOnly();
+  if (browser && (browser->is_type_app() || browser->is_type_app_popup())) {
     return false;
   }
 
