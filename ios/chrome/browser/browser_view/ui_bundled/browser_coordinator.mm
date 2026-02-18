38df3b89f2a9561d8398ba2ab7d9106b1668163a
Asami Doi <asamidoi@chromium.org>
2026-02-18 07:25:01 -0800

    [ios] Add swift_buildflags to build/config/ios/BUILD.gn
    This CL is factored out from crrev.com/c/7482810. Adds swift_buildflags
    with has_swift_6_3_support so that the code can check it by
    `BUILDFLAG(HAS_SWIFT_6_3_SUPPORT)`.
    The flag `ENABLE_SWIFT_CXX_INTEROP` moves under swift_buildflags.
    
    Change-Id: Ifb3242a7b5995ed5834a6a3e03f3754b8df680d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7567248
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1586427}

--- ios/chrome/browser/browser_view/ui_bundled/browser_coordinator.mm
+++ ios/chrome/browser/browser_view/ui_bundled/browser_coordinator.mm
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/metrics/histogram_functions.h"
 #import "base/scoped_observation.h"
 #import "base/strings/sys_string_conversions.h"
-#import "build/config/ios/buildflags.h"
+#import "build/config/ios/swift_buildflags.h"
 #import "components/autofill/core/browser/payments/autofill_error_dialog_context.h"
 #import "components/collaboration/public/collaboration_flow_type.h"
 #import "components/collaboration/public/collaboration_service.h"
