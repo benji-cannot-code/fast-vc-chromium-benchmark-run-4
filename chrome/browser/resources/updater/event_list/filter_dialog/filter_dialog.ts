313a1613164217db275d2ad0a4dc5ee26930b0c4
Noah Rose Ledesma <noahrose@google.com>
2026-02-18 12:00:55 -0800

    Updater UI: Reposition filter dialog on scroll
    The FilterDialogElement is anchored to a filter chip using positioning
    in JavaScript. However, when the view is scrolled while a dialog is
    opened, it is not repositioned over the anchor's new position. Fix this
    by repositioning the dialog when the window observes a scroll event.
    
    Ideally, this positioning could be done via CSS anchoring; however, I
    was unable to find a way for anchor-name properties to traverse the
    shadow dom.
    
    Bug: 456497614
    Change-Id: I8ab65d073a310b218cf80508f76847656a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7564251
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Auto-Submit: Noah Rose Ledesma <noahrose@google.com>
    Cr-Commit-Position: refs/heads/main@{#1586608}

--- chrome/browser/resources/updater/event_list/filter_dialog/filter_dialog.ts
+++ chrome/browser/resources/updater/event_list/filter_dialog/filter_dialog.ts
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {EventTracker} from '//resources/js/event_tracker.js';
 import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
 import type {PropertyValues} from '//resources/lit/v3_0/lit.rollup.js';
 
@@ -34,7 +35,20 @@ export class FilterDialogElement extends CrLitElement {
     };
   }
 
+  private eventTracker: EventTracker = new EventTracker();
   accessor anchorElement: HTMLElement|null = null;
+
+  override connectedCallback() {
+    super.connectedCallback();
+    this.eventTracker.add(window, 'scroll', () => {
+      this.positionDialog();
+    });
+  }
+
+  override disconnectedCallback() {
+    super.disconnectedCallback();
+    this.eventTracker.removeAll();
+  }
 
   override firstUpdated() {
     this.$.dialog.showModal();
