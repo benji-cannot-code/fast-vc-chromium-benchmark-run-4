6c82e7b2b21a2ecfcb061748c4f9eba214dfc668
Jagadesh P <jagadeshjai1999@gmail.com>
2026-02-17 21:26:47 -0800

    Resolve dangling pointer in |BookmarkBarStateTestBrowserWindow|
    Bug: 40212619
    Change-Id: I860149a134f8b1bdafda092bbba8ca55137d7bd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7545038
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Jagadesh Jai <jagadeshjai1999@gmail.com>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Auto-Submit: Jagadesh Jai <jagadeshjai1999@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1586207}

--- chrome/browser/ui/browser_unittest.cc
+++ chrome/browser/ui/browser_unittest.cc
@@ -324,7 +324,7 @@ class BrowserBookmarkBarTest : public BrowserWithTestWindowTest {
  private:
   class BookmarkBarStateTestBrowserWindow : public TestBrowserWindow {
    public:
-    BookmarkBarStateTestBrowserWindow() : browser_(nullptr) {}
+    BookmarkBarStateTestBrowserWindow() = default;
 
     BookmarkBarStateTestBrowserWindow(
         const BookmarkBarStateTestBrowserWindow&) = delete;
@@ -358,7 +358,7 @@ class BrowserBookmarkBarTest : public BrowserWithTestWindowTest {
                                             reason);
     }
 
-    raw_ptr<Browser, DanglingUntriaged> browser_;  // Weak ptr.
+    raw_ptr<Browser> browser_ = nullptr;
     BookmarkBar::State bookmark_bar_state_ = BookmarkBar::HIDDEN;
   };
 };
