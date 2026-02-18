1691b8aab674cf519b3a0a95dc3120a2ec9fe7ed
Colin Blundell <blundell@chromium.org>
2026-02-18 00:46:53 -0800

    [//cc] Make ComputeCheckerboardedNeedsRecord() virtual
    Call it from TileBasedLayerImpl::AppendQuads(). This will facilitate
    moving subclass specialization from being at the level of
    AppendQuadsSpecialization() to that of AppendQuadForTile().
    
    Bug: 450651370
    Change-Id: I50d9fe92e592a1cfd32a181daec667fab742e653
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7582037
    Reviewed-by: Vikas Soni <vikassoni@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1586271}

--- cc/layers/tile_based_layer_impl_unittest.cc
+++ cc/layers/tile_based_layer_impl_unittest.cc
@@ -81,6 +81,8 @@ class TestTileBasedLayerImpl : public TileBasedLayerImpl<FakeTiling> {
                                 float max_contents_scale) override {
     return 0;
   }
+  void ComputeCheckerboardedNeedsRecord(
+      AppendQuadsData* append_quads_data) override {}
   float GetMaximumContentsScaleForUseInAppendQuads() const override {
     return 1.f;
   }
