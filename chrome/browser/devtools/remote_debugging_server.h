519b43dbbf3c4d99f8de331b2ad785acc1e3d738
Alex Rudenko <alexrudenko@chromium.org>
2026-02-18 02:58:08 -0800

    Fix remote server not listening for pref changes
    Fixed: 485420898
    Change-Id: If2f0ccd2a766caa78b8a46c754b5886fef0085de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7588378
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1586318}

--- chrome/browser/devtools/remote_debugging_server.h
+++ chrome/browser/devtools/remote_debugging_server.h
@@ -59,7 +59,7 @@ class RemoteDebuggingServer {
 
   virtual ~RemoteDebuggingServer();
 
-  void StartHttpServerInApprovalMode(PrefService* local_state);
+  void StartHttpServerInApprovalModeIfEnabled(PrefService* local_state);
 
  protected:
   RemoteDebuggingServer();
