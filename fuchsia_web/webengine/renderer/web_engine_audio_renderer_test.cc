4f90f6e16bafafe65d30de63c2d728ef219bb7bd
Syed AbuTalib <lowkey@google.com>
2026-02-17 16:39:14 -0800

    Update AudioDecoderConfigs in fuchsia_web/ to use new ctor
    Replace AudioDecoderConfig constructors to use the ChannelLayoutConfig
    constructor over the deprecated ChannelLayout constructor.
    
    Bug: 481380798
    Change-Id: I5d5905a170b5f23a57d1ee4d14b28d94e6fab333
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7586393
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Zijie He <zijiehe@google.com>
    Commit-Queue: Syed AbuTalib <lowkey@google.com>
    Cr-Commit-Position: refs/heads/main@{#1586087}

--- fuchsia_web/webengine/renderer/web_engine_audio_renderer_test.cc
+++ fuchsia_web/webengine/renderer/web_engine_audio_renderer_test.cc
@@ -695,9 +695,10 @@ void WebEngineAudioRendererTestBase::TestPcmStream(
     size_t bytes_per_sample_input,
     fuchsia::media::AudioSampleFormat fuchsia_sample_format,
     size_t bytes_per_sample_output) {
-  media::AudioDecoderConfig config(
-      media::AudioCodec::kPCM, sample_format, media::CHANNEL_LAYOUT_STEREO,
-      kDefaultSampleRate, {}, media::EncryptionScheme::kUnencrypted);
+  media::AudioDecoderConfig config(media::AudioCodec::kPCM, sample_format,
+                                   media::ChannelLayoutConfig::Stereo(),
+                                   kDefaultSampleRate, {},
+                                   media::EncryptionScheme::kUnencrypted);
 
   demuxer_stream_ = std::make_unique<TestDemuxerStream>(config);
 
@@ -755,9 +756,10 @@ class WebEngineAudioRendererTest
     auto encryption_scheme = GetParam().simulate_fuchsia_cdm
                                  ? media::EncryptionScheme::kCenc
                                  : media::EncryptionScheme::kUnencrypted;
-    return media::AudioDecoderConfig(
-        media::AudioCodec::kPCM, media::kSampleFormatF32,
-        media::CHANNEL_LAYOUT_MONO, kDefaultSampleRate, {}, encryption_scheme);
+    return media::AudioDecoderConfig(media::AudioCodec::kPCM,
+                                     media::kSampleFormatF32,
+                                     media::ChannelLayoutConfig::Mono(),
+                                     kDefaultSampleRate, {}, encryption_scheme);
   }
 };
 
@@ -912,9 +914,10 @@ class WebEngineAudioRendererConfgChangeTest
     auto encryption_scheme = GetParam().encrypted_head
                                  ? media::EncryptionScheme::kCenc
                                  : media::EncryptionScheme::kUnencrypted;
-    return media::AudioDecoderConfig(
-        media::AudioCodec::kPCM, media::kSampleFormatF32,
-        media::CHANNEL_LAYOUT_MONO, kDefaultSampleRate, {}, encryption_scheme);
+    return media::AudioDecoderConfig(media::AudioCodec::kPCM,
+                                     media::kSampleFormatF32,
+                                     media::ChannelLayoutConfig::Mono(),
+                                     kDefaultSampleRate, {}, encryption_scheme);
   }
 };
 
@@ -947,7 +950,7 @@ TEST_P(WebEngineAudioRendererConfgChangeTest, ConfigChange) {
                                    : media::EncryptionScheme::kUnencrypted;
   media::AudioDecoderConfig updated_config(
       media::AudioCodec::kOpus, media::kSampleFormatF32,
-      media::CHANNEL_LAYOUT_STEREO, kNewSampleRate, kArbitraryExtraData,
+      media::ChannelLayoutConfig::Stereo(), kNewSampleRate, kArbitraryExtraData,
       mew_encryption_scheme);
 
   demuxer_stream_->QueueReadResult(
