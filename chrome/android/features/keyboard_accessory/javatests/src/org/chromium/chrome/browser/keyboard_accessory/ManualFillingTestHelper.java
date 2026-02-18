f305ba6d85c2f01bd1c819bfe1d9ad6633e552bd
Benoit Lize <lizeb@chromium.org>
2026-02-18 05:43:12 -0800

    Revert "[Autofill] Fix flaky tests for keyboard accessory"
    This reverts commit ae70e98da477be588f2446126266ec199bc43015.
    
    Reason for revert: crbug.com/485509238, test fails
    
    Original change's description:
    > [Autofill] Fix flaky tests for keyboard accessory
    >
    > Flakiness seems to be coming from "Manual Fallback Icons" in keyboard
    > accessory. They are populated asynchronously with relation to suggestion
    > chips hint in the same recyclerview. We need to wait not only for
    > suggestion chips but for the those fallback icons as well. Also a small
    > cleanup of AutofillKeyboardAccessoryIntegrationTest was done -> i've
    > deleted throw declarations, for exceptions which are not thrown.
    >
    > Bug: 472512225
    > Change-Id: I5a1abce58f1e3fcf545c0221561b79f565c127f7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7581915
    > Commit-Queue: Lukasz Wojtach <lwojtach@google.com>
    > Reviewed-by: Piotr Kotynia <piotrkotynia@google.com>
    > Reviewed-by: Luchen Peng <luchenpeng@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1586266}
    
    Bug: 472512225, 485509238
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I1987aaffe5156c7c3996fe22b9d5cc37e6dd5a78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7587976
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Piotr Kotynia <piotrkotynia@google.com>
    Reviewed-by: Piotr Kotynia <piotrkotynia@google.com>
    Auto-Submit: Benoit Lize <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1586363}

--- chrome/android/features/keyboard_accessory/javatests/src/org/chromium/chrome/browser/keyboard_accessory/ManualFillingTestHelper.java
+++ chrome/android/features/keyboard_accessory/javatests/src/org/chromium/chrome/browser/keyboard_accessory/ManualFillingTestHelper.java
@@ -298,12 +298,6 @@ public class ManualFillingTestHelper {
                     },
                     "Waited for suggestions that never appeared.");
         }
-        waitForManualFillingIconsToBeLoaded();
-    }
-
-    private void waitForManualFillingIconsToBeLoaded() {
-        pollUiThread(
-                () -> Criteria.checkThat(getKeyboardAccessoryBar().hasTabs(), Matchers.is(true)));
     }
 
     public DropdownPopupWindowInterface waitForAutofillPopup(String filterInput) {
