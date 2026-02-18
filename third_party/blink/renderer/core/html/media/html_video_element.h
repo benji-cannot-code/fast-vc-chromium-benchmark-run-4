49688cb8482bf95be08716bcdb7e4456d5442ec6
Colin Blundell <blundell@chromium.org>
2026-02-18 01:09:01 -0800

    [Blink] Eliminate HTMLVideoElement creation of bitmap provider
    Have it equivalently cache the CanvasSnapshotProvider::Info that will
    be used for the software draw in this case.
    
    Note: This CL does a straight conversion to be sure of not changing
    behavior. I will examine streamlining in followup.
    
    Bug: 352263194
    Change-Id: Ibd0049840020c9cb8e4a0a6378910ed23b39b4fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7581914
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1586282}

--- third_party/blink/renderer/core/html/media/html_video_element.h
+++ third_party/blink/renderer/core/html/media/html_video_element.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html/html_image_loader.h"
 #include "third_party/blink/renderer/core/html/media/html_media_element.h"
 #include "third_party/blink/renderer/core/imagebitmap/image_bitmap_source.h"
+#include "third_party/blink/renderer/platform/graphics/canvas_resource_provider.h"
 #include "third_party/blink/renderer/platform/graphics/canvas_snapshot_provider.h"
 #include "third_party/blink/renderer/platform/timer.h"
 
@@ -281,7 +282,8 @@ class CORE_EXPORT HTMLVideoElement final
 
   // Used to fulfill blink::Image requests (CreateImage(),
   // GetSourceImageForCanvas(), etc). Created on demand.
-  std::unique_ptr<CanvasSnapshotProvider> snapshot_provider_;
+  std::unique_ptr<CanvasNon2DResourceProviderSharedImage> snapshot_provider_;
+  std::optional<CanvasSnapshotProvider::Info> sw_draw_info_;
   bool allow_accelerated_images_ = true;
   HeapTaskRunnerTimer<HTMLVideoElement> cache_deleting_timer_;
 
