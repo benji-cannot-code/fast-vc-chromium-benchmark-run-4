cfd4ae31d832a676377fd8bc548286370e26789c
Tsuyoshi Horo <horo@chromium.org>
2026-02-18 15:01:02 -0800

    net/disk_cache/sql: Use EvictionTarget in EvictionCandidateAggregator
    This CL refactors EvictionCandidateAggregator and
    SqlPersistentStore::Backend to use a new EvictionTarget struct.
    
    Previously, EvictionCandidateAggregator passed a list of ResIds and a
    total bytes_usage to the backend for eviction. This change introduces
    EvictionTarget, which pairs a ResId with its entry_size_with_overhead.
    
    This refactoring allows the backend to have per-entry size information
    during the eviction process, which will be useful for future
    improvements such as pausable eviction or more accurate size accounting
    during partial evictions.
    
    Bug: 478753978
    Change-Id: I47c8de29740351f899f82c16a7a1cfe0e2240382
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7583093
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1586718}

--- net/disk_cache/sql/sql_persistent_store_backend.cc
+++ net/disk_cache/sql/sql_persistent_store_backend.cc
@@ -2316,7 +2316,9 @@ SqlPersistentStore::Backend::SelectEvictionCandidates(
       }
       candidates_total_size += bytes_usage;
       candidates_total_size += kSqlBackendStaticResourceSize;
-      candidates.emplace_back(res_id, shard_id_, bytes_usage, last_used);
+      candidates.emplace_back(res_id, shard_id_,
+                              bytes_usage + kSqlBackendStaticResourceSize,
+                              last_used);
     }
   }
   base::UmaHistogramMicrosecondsTimes(
@@ -2331,8 +2333,7 @@ SqlPersistentStore::Backend::SelectEvictionCandidates(
 void SqlPersistentStore::Backend::EvictEntries(
     ResIdListOrErrorAndStoreStatusCallback callback,
     bool is_idle_time_eviction,
-    ResIdList res_ids,
-    int64_t bytes_usage,
+    EvictionTargetList eviction_target_list,
     base::TimeTicks post_task_time) {
   const base::TimeDelta posting_delay = base::TimeTicks::Now() - post_task_time;
   // Checks that this method is called on the expected sequence when invoked via
@@ -2341,10 +2342,20 @@ void SqlPersistentStore::Backend::EvictEntries(
   TRACE_EVENT_BEGIN1("disk_cache", "SqlBackend.EvictEntries", "data",
                      [&](perfetto::TracedValue trace_context) {
                        auto dict = std::move(trace_context).WriteDictionary();
-                       dict.Add("res_ids_size", res_ids.size());
+                       dict.Add("target_size", eviction_target_list.size());
                      });
   base::ElapsedTimer timer;
   bool corruption_detected = false;
+
+  ResIdList res_ids;
+  int64_t bytes_usage = 0;
+  res_ids.reserve(eviction_target_list.size());
+  for (const auto& target : eviction_target_list) {
+    res_ids.emplace_back(target.res_id);
+    bytes_usage +=
+        target.entry_size_with_overhead - kSqlBackendStaticResourceSize;
+  }
+
   auto result = EvictEntriesInternal(
       res_ids, bytes_usage, is_idle_time_eviction, corruption_detected);
   RecordTimeAndErrorResultHistogram(
