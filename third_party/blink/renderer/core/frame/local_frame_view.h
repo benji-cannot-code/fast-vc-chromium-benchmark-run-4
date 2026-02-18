44d0081970327535885447002d6b18d4201b23b4
Philip Rogers <pdr@chromium.org>
2026-02-17 17:47:04 -0800

    [html-in-canvas] Initial implementation of canvas.onpaint
    This is an initial implementation of canvas.onpaint, which adds the
    event and makes it fire after paint. Followups will fix the timing of
    when this fires, add the arguments and `requestPaint` function, and fix
    the transform invalidation test that has been disabled in onpaint.html.
    
    Bug: 484345338
    Change-Id: Iaa728c58434718085c36b025a7be1fde6bd2a128
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7577597
    Auto-Submit: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1586123}

--- third_party/blink/renderer/core/frame/local_frame_view.h
+++ third_party/blink/renderer/core/frame/local_frame_view.h
@@ -822,6 +822,8 @@ class CORE_EXPORT LocalFrameView final
   void NotifyVideoIsDominantVisibleStatus(HTMLVideoElement* element,
                                           bool is_dominant);
 
+  void DidPaintCanvasChild(const Element&);
+
   bool HasDominantVideoElement() const;
 
   // Gets the xr overlay layer if present, or nullptr if there is none.
@@ -1305,6 +1307,11 @@ class CORE_EXPORT LocalFrameView final
   Member<TapFriendlinessChecker> tap_friendliness_checker_;
 
   HeapHashSet<WeakMember<LifecycleNotificationObserver>> lifecycle_observers_;
+
+  // Set of Element children of <canvas> which painted in the current paint
+  // lifecycle update. This is cleared at the end of the lifecycle update. Used
+  // for `CanvasDrawElement`.
+  HeapHashSet<Member<const Element>> painted_canvas_child_elements_;
 
   HeapHashSet<WeakMember<HTMLVideoElement>> fullscreen_video_elements_;
 
