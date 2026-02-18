80fa2dfab62d523172533d0439545e3a13ab8a79
Joseph Lanza <josephlanza@google.com>
2026-02-18 05:25:33 -0800

    [iOS] Add kIOSDateToCalendarSignedOut feature flag
    This CL adds a flag which will be used to enable 'dates long press to Calendar event creation' for signed-out users.
    
    Bug: 484943572
    Change-Id: I4197a5ba5282fdff979a498225d0558f320c56fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7581777
    Reviewed-by: Elmehdi Rahmaoui <erahmaoui@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Joseph Lanza <josephlanza@google.com>
    Cr-Commit-Position: refs/heads/main@{#1586360}

--- ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
+++ ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
@@ -454,6 +454,9 @@ extern const char kIOSChooseFromDriveSignedOutDescription[];
 extern const char kIOSCustomFileUploadMenuName[];
 extern const char kIOSCustomFileUploadMenuDescription[];
 
+extern const char kIOSDateToCalendarSignedOutName[];
+extern const char kIOSDateToCalendarSignedOutDescription[];
+
 extern const char kIOSDockingPromoName[];
 extern const char kIOSDockingPromoDescription[];
 
