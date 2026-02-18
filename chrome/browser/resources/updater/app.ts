fee8626b698ff2319dd0c9bc58ecf56bab6401a4
Noah Rose Ledesma <noahrose@google.com>
2026-02-18 11:36:16 -0800

    Updater UI: Lift AppStates query out of AppList
    Move the logic for querying the browser for updater application states
    out of AppListElement and into the top-level UpdaterAppElement. This
    will make it easier to use AppList to display information from either
    the user's updater installations or an external file.
    
    Additionally, add a unit test for UpdaterAppElement.
    
    Bug: 483094780
    Change-Id: I8bfa900f31438964569586066bb4377e6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7559835
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1586588}

--- chrome/browser/resources/updater/app.ts
+++ chrome/browser/resources/updater/app.ts
@@ -13,10 +13,12 @@ import type {PropertyValues} from '//resources/lit/v3_0/lit.rollup.js';
 
 import {getCss} from './app.css.js';
 import {getHtml} from './app.html.js';
+import type {AppStateDisplay} from './app_list/app_list.js';
 import {BrowserProxyImpl} from './browser_proxy.js';
 import {parsePolicySet} from './event_history.js';
 import type {PolicySet} from './event_history.js';
-import type {EnterpriseCompanionState, GetEnterpriseCompanionStateResponse, GetUpdaterStatesResponse, UpdaterState} from './updater_ui.mojom-webui.js';
+import {getKnownAppNamesById} from './known_apps.js';
+import type {EnterpriseCompanionState, GetAppStatesResponse, GetEnterpriseCompanionStateResponse, GetUpdaterStatesResponse, UpdaterState} from './updater_ui.mojom-webui.js';
 
 export class UpdaterAppElement extends CrLitElement {
   static get is() {
@@ -38,6 +40,8 @@ export class UpdaterAppElement extends CrLitElement {
       systemUpdaterState: {type: Object},
       enterpriseCompanionState: {type: Object},
       updaterStateError: {type: Boolean},
+      apps: {type: Array},
+      appStateError: {type: Boolean},
     };
   }
 
@@ -46,6 +50,8 @@ export class UpdaterAppElement extends CrLitElement {
   accessor systemUpdaterState: UpdaterState|null = null;
   accessor enterpriseCompanionState: EnterpriseCompanionState|null = null;
   accessor updaterStateError = false;
+  accessor apps: AppStateDisplay[] = [];
+  accessor appStateError = false;
 
   protected policies: PolicySet|undefined = undefined;
 
@@ -68,6 +74,9 @@ export class UpdaterAppElement extends CrLitElement {
         .catch(() => {
           this.updaterStateError = true;
         });
+    this.getAppStates()
+        .then(apps => this.apps = apps)
+        .catch(() => this.appStateError = true);
   }
 
   override willUpdate(changedProperties: PropertyValues<this>) {
@@ -106,6 +115,26 @@ export class UpdaterAppElement extends CrLitElement {
       Promise<GetEnterpriseCompanionStateResponse> {
     return await BrowserProxyImpl.getInstance()
         .handler.getEnterpriseCompanionState();
+  }
+
+  private async getAppStates(): Promise<AppStateDisplay[]> {
+    const response: GetAppStatesResponse =
+        await BrowserProxyImpl.getInstance().handler.getAppStates();
+    const knownApps = getKnownAppNamesById();
+
+    const systemApps: AppStateDisplay[] = response.systemApps.map(
+        app => ({
+          ...app,
+          scope: 'SYSTEM',
+          displayName: knownApps.get(app.appId.toLowerCase()) || app.appId,
+        }));
+    const userApps: AppStateDisplay[] = response.userApps.map(
+        app => ({
+          ...app,
+          scope: 'USER',
+          displayName: knownApps.get(app.appId.toLowerCase()) || app.appId,
+        }));
+    return [...systemApps, ...userApps];
   }
 }
 
