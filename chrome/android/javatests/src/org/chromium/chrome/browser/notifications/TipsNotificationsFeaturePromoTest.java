c86aa7c2356c115b37451ff1dc31752488182233
Xinyi Ji <xinyiji@google.com>
2026-02-18 15:19:34 -0800

    [gardening] Disable failing test TipsNotificationsFeaturePromoTest#testESBBottomSheetDetailPageAccept
    Bug: 485627769
    Change-Id: Ia110f7682cccc07a500bb7ab524c73eb463f5cc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7588422
    Reviewed-by: Brandon Fong <bjfong@google.com>
    Reviewed-by: Dan Polanco <polardz@google.com>
    Commit-Queue: Xinyi Ji <xinyiji@chromium.org>
    Commit-Queue: Dan Polanco <polardz@google.com>
    Owners-Override: Xinyi Ji <xinyiji@chromium.org>
    Reviewed-by: Evan Luo <evanluo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1586730}

--- chrome/android/javatests/src/org/chromium/chrome/browser/notifications/TipsNotificationsFeaturePromoTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/notifications/TipsNotificationsFeaturePromoTest.java
@@ -31,6 +31,7 @@ import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
 import org.chromium.base.test.util.Batch;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.Restriction;
@@ -145,6 +146,7 @@ public class TipsNotificationsFeaturePromoTest {
     @Test
     @MediumTest
     @Feature({"RenderTest"})
+    @DisabledTest(message = "https://crbug.com/485627769")
     public void testESBBottomSheetDetailPageAccept() throws IOException {
         @TipsNotificationsFeatureType
         int featureType = TipsNotificationsFeatureType.ENHANCED_SAFE_BROWSING;
