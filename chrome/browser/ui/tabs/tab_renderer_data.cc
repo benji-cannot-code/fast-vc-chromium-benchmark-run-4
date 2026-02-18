7fb364bdeeb3f242a466fd3614e4cf005627de07
Steven Luong <stluong@chromium.org>
2026-02-18 12:55:46 -0800

    [TabRendererData] Move themify and show icon to TabUIHelper
    Move favicon related properties to be calculated in the TabUIHelper
    so that the TabRendererData will be easier to deprecate.
    
    Bug: 484410926
    Change-Id: I0de4e22d4f80ddb2980a166539f85d44af66a271
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7584127
    Commit-Queue: Steven Luong <stluong@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1586641}

--- chrome/browser/ui/tabs/tab_renderer_data.cc
+++ chrome/browser/ui/tabs/tab_renderer_data.cc
@@ -33,8 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/saved_tab_groups/public/tab_group_sync_service.h"
 #include "components/security_interstitials/content/security_interstitial_tab_helper.h"
 #include "components/tabs/public/tab_interface.h"
-#include "content/public/browser/navigation_controller.h"
-#include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/url_constants.h"
 
@@ -94,7 +92,6 @@ TabRendererData TabRendererData::FromTabInterface(tabs::TabInterface* tab) {
   data.title = tab_ui_helper->GetTitle();
   data.needs_attention = tab_ui_helper->needs_attention();
   auto* const bwi = tab->GetBrowserWindowInterface();
-  Browser* browser = bwi ? bwi->GetBrowserForMigrationOnly() : nullptr;
 
   // Note that in unit tests, this may be null.
   if (bwi) {
@@ -132,16 +129,11 @@ TabRendererData TabRendererData::FromTabInterface(tabs::TabInterface* tab) {
   data.should_display_url = should_display_url;
   data.is_crashed = tab_ui_helper->IsCrashed();
   data.pinned = tab->IsPinned();
-  data.show_icon =
-      data.pinned || (browser && browser->ShouldDisplayFavicon(contents));
+  data.show_icon = tab_ui_helper->ShouldDisplayFavicon();
   data.blocked = tab->IsBlocked();
   data.should_hide_throbber = tab_ui_helper->ShouldHideThrobber();
   data.alert_state = tabs::TabAlertController::From(tab)->GetAllActiveAlerts();
-
-  content::NavigationEntry* entry =
-      contents->GetController().GetLastCommittedEntry();
-  data.should_themify_favicon =
-      entry && favicon::ShouldThemifyFaviconForEntry(entry);
+  data.should_themify_favicon = tab_ui_helper->ShouldThemifyFavicon();
 
   data.should_show_discard_status = tab_ui_helper->ShouldShowDiscardStatus();
   data.discarded_memory_savings =
