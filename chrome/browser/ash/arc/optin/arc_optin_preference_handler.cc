b67c5c355c610dc2d1049ed76e581ca4f42eb09e
Jun Ishiguro <junis@google.com>
2026-02-17 17:02:24 -0800

    arc: Remove unnecessary null check for local_state()
    g_browser_process->local_state() should be always non-null.
    
    Bug: b:404130092
    Change-Id: I227cf721a42a46734553ea01ac8e1540af628db2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7580796
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1586099}

--- chrome/browser/ash/arc/optin/arc_optin_preference_handler.cc
+++ chrome/browser/ash/arc/optin/arc_optin_preference_handler.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/arc/optin/arc_optin_preference_handler_observer.h"
 #include "chrome/browser/ash/settings/device_settings_service.h"
 #include "chrome/browser/ash/settings/stats_reporting_controller.h"
-#include "chrome/browser/browser_process.h"
 #include "chrome/browser/extensions/api/settings_private/prefs_util.h"
 #include "chrome/browser/metrics/metrics_reporting_state.h"
 #include "chrome/browser/metrics/per_user_state_manager_chromeos.h"
@@ -119,7 +118,7 @@ void ArcOptInPreferenceHandler::SendMetricsMode(
   if (IsAllowedToUpdateUserConsent(ownership_status)) {
     observer_->OnMetricsModeChanged(GetUserMetrics(),
                                     IsMetricsReportingPolicyManaged());
-  } else if (g_browser_process->local_state()) {
+  } else {
     bool enabled = ash::StatsReportingController::Get()->IsEnabled();
     observer_->OnMetricsModeChanged(enabled, IsMetricsReportingPolicyManaged());
   }
