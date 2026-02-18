9a90b7eb3fb3b15bd6726d0981766ca42eba0384
vikas soni <vikassoni@chromium.org>
2026-02-18 07:20:51 -0800

    [DCSI]: Enhance error logging in CompoundImageBacking.
    This CL updates LOG(ERROR) messages in CompoundImageBacking and
    SharedImageCopyManager to include more useful information to help with
    debugging.
    
    Bug: 475210055
    Change-Id: I104d04a57e1a92a4701451af5624e55cb40df6fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7585075
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Auto-Submit: Vikas Soni <vikassoni@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1586422}

--- gpu/command_buffer/service/shared_image/compound_image_backing.cc
+++ gpu/command_buffer/service/shared_image/compound_image_backing.cc
@@ -1009,7 +1009,8 @@ void CompoundImageBacking::NotifyBeginAccess(SharedImageBacking* backing,
                                              SharedImageAccessStream stream) {
   ElementHolder* access_element = GetElement(backing);
   if (!access_element) {
-    LOG(ERROR) << "backing not in the element list.";
+    LOG(ERROR) << "Backing (" << backing->GetName()
+               << ") not in the element list";
     return;
   }
 
@@ -1041,8 +1042,10 @@ void CompoundImageBacking::NotifyBeginAccess(SharedImageBacking* backing,
       access_element->GetBacking()->SetClearedRect(src_cleared_rect);
       SetClearedRect(src_cleared_rect);
     } else {
-      LOG(ERROR)
-          << "Failed to copy between backings. Backing can be using stale data";
+      LOG(ERROR) << "Failed to copy from "
+                 << latest_content_element->GetBacking()->GetName() << " to "
+                 << access_element->GetBacking()->GetName()
+                 << ". Backing can be using stale data";
     }
 
     UMA_HISTOGRAM_BOOLEAN("GPU.CompoundImageBacking.ContentSync.Success",
@@ -1137,7 +1140,8 @@ bool CompoundImageBacking::CopyToGpuMemoryBuffer() {
   auto* gpu_backing = GetGpuBacking();
   if (!gpu_backing ||
       !copy_manager_->CopyImage(gpu_backing, shm_element.GetBacking())) {
-    DLOG(ERROR) << "Failed to copy from GPU backing to shared memory";
+    LOG(ERROR) << "Failed to copy from GPU backing (" << gpu_backing->GetName()
+               << ") to shared memory";
     return false;
   }
 
@@ -1162,7 +1166,8 @@ void CompoundImageBacking::CopyToGpuMemoryBufferAsync(
 
   auto* gpu_backing = GetGpuBacking();
   if (!gpu_backing) {
-    DLOG(ERROR) << "Failed to copy from GPU backing to shared memory";
+    LOG(ERROR) << "Failed to copy from GPU backing (" << gpu_backing->GetName()
+               << ") to shared memory";
     std::move(callback).Run(false);
     return;
   }
