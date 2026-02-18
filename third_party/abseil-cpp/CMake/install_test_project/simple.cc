0dddfa7206f67598b075204e13b6df924e35058a
Mirko Bonadei <mbonadei@chromium.org>
2026-02-18 06:02:22 -0800

    Roll abseil_revision 351df81acd..43a941465c
    Change Log:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+log/351df81acd..43a941465c
    Full diff:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+/351df81acd..43a941465c
    
    Bug: None
    Change-Id: I1841aacd06072dc0c180dcf3d9af8a0dad237c7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7588693
    Auto-Submit: Mirko Bonadei <mbonadei@chromium.org>
    Reviewed-by: Danil Chapovalov <danilchap@chromium.org>
    Commit-Queue: Danil Chapovalov <danilchap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1586375}

--- third_party/abseil-cpp/CMake/install_test_project/simple.cc
+++ third_party/abseil-cpp/CMake/install_test_project/simple.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // limitations under the License.
 
 #include <iostream>
-
 #include "absl/base/config.h"
 #include "absl/strings/substitute.h"
 
