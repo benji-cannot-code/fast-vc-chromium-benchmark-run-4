665d38b78576f4dc66a47ba10be69ee8c76630d7
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2026-02-18 12:07:28 -0800

    Revert "[bedrock] Migrate handoff_observer.mm away from BrowserListObserver."
    This reverts commit 3489d612e1eee015e1f7755a37d8a71a1ccaec11.
    
    Reason for revert: LUCI Bisection has identified this change as the
    cause of a test failure. See the analysis:
    https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/4808632144232448
    
    Sample build with failed test:
    https://ci.chromium.org/b/8689513292035539777 Affected test(s):
    [://chrome/test\:browser_tests!gtest::BrowserTest#BrowserCloseEmitsClosedNotificationsWhenDestroyed](https://ci.chromium.org/ui/test/chromium/:%2F%2Fchrome%2Ftest%5C:browser_tests%21gtest::BrowserTest%23BrowserCloseEmitsClosedNotificationsWhenDestroyed?q=VHash%3A812306a38c719b85)
    
    If this is a false positive, please report it at
    http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F4808632144232448&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F7552710&type=BUG
    
    Original change's description:
    > [bedrock] Migrate handoff_observer.mm away from BrowserListObserver.
    >
    > This migration is part of project bedrock to reduce the dependencies on
    > Browser and BrowserList. See https://crbug.com/431671320 for more info.
    >
    > Bug: 459777668
    > Change-Id: I7120248da4d12a1b51f388080bad5c276a6a6964
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7552710
    > Reviewed-by: Elly FJ <ellyjones@chromium.org>
    > Commit-Queue: Glenn Hartmann <hartmanng@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1586404}
    >
    
    Bug: 459777668, 485606260
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I16718ae9b3b125f279455bb526372fbbbaf57db8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7590027
    Commit-Queue: Antonio Alphonse <alphonsea@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Antonio Alphonse <alphonsea@google.com>
    Reviewed-by: Antonio Alphonse <alphonsea@google.com>
    Cr-Commit-Position: refs/heads/main@{#1586616}

--- chrome/browser/ui/cocoa/handoff_observer.h
+++ chrome/browser/ui/cocoa/handoff_observer.h
@@ -9,8 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Cocoa/Cocoa.h>
 
 #include "base/memory/raw_ptr.h"
-#include "base/scoped_observation.h"
-#include "chrome/browser/ui/browser_window/public/browser_collection_observer.h"
+#include "chrome/browser/ui/browser_list_observer.h"
 #include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
 #include "content/public/browser/web_contents_observer.h"
 
@@ -19,8 +18,7 @@ class Page;
 class WebContents;
 }  // namespace content
 
-class BrowserWindowInterface;
-class GlobalBrowserCollection;
+class Browser;
 
 // A protocol that allows ObjC objects to receive delegate callbacks from
 // HandoffObserver.
@@ -31,7 +29,7 @@ class GlobalBrowserCollection;
 // This class observes changes to the "active URL". This is defined as the
 // visible URL of the WebContents of the selected tab of the most recently
 // focused browser window.
-class HandoffObserver : public BrowserCollectionObserver,
+class HandoffObserver : public BrowserListObserver,
                         public TabStripModelObserver,
                         public content::WebContentsObserver {
  public:
@@ -42,10 +40,9 @@ class HandoffObserver : public BrowserCollectionObserver,
 
   ~HandoffObserver() override;
 
- private:
-  // BrowserCollectionObserver
-  void OnBrowserActivated(BrowserWindowInterface* browser) override;
-  void OnBrowserClosed(BrowserWindowInterface* browser) override;
+ private:  // BrowserListObserver
+  void OnBrowserSetLastActive(Browser* browser) override;
+  void OnBrowserRemoved(Browser* browser) override;
 
   // TabStripModelObserver
   void OnTabStripModelChanged(
@@ -58,7 +55,7 @@ class HandoffObserver : public BrowserCollectionObserver,
   void TitleWasSet(content::NavigationEntry* entry) override;
 
   // Updates the active browser.
-  void SetActiveBrowser(BrowserWindowInterface* active_browser);
+  void SetActiveBrowser(Browser* active_browser);
 
   // Makes this object start observing the WebContents, if it is not already
   // doing so. This method is idempotent.
@@ -72,10 +69,7 @@ class HandoffObserver : public BrowserCollectionObserver,
 
   // This pointer is always up to date, and points to the most recently
   // activated browser, or nullptr if no browsers exist.
-  raw_ptr<BrowserWindowInterface> active_browser_ = nullptr;
-
-  base::ScopedObservation<GlobalBrowserCollection, BrowserCollectionObserver>
-      browser_collection_observation_{this};
+  raw_ptr<Browser> active_browser_ = nullptr;
 
   // Instances of this class should be owned by their |delegate_|.
   NSObject<HandoffObserverDelegate>* __weak delegate_;
--- chrome/browser/ui/cocoa/handoff_observer.mm
+++ chrome/browser/ui/cocoa/handoff_observer.mm
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_list.h"
-#include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
-#include "chrome/browser/ui/browser_window/public/global_browser_collection.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/web_contents.h"
@@ -18,21 +16,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 HandoffObserver::HandoffObserver(NSObject<HandoffObserverDelegate>* delegate)
     : delegate_(delegate) {
   DCHECK(delegate_);
-  browser_collection_observation_.Observe(
-      GlobalBrowserCollection::GetInstance());
+  BrowserList::AddObserver(this);
   SetActiveBrowser(chrome::FindLastActive());
 }
 
 HandoffObserver::~HandoffObserver() {
+  BrowserList::RemoveObserver(this);
   SetActiveBrowser(nullptr);
 }
 
-void HandoffObserver::OnBrowserActivated(BrowserWindowInterface* browser) {
+void HandoffObserver::OnBrowserSetLastActive(Browser* browser) {
   SetActiveBrowser(browser);
   [delegate_ handoffContentsChanged:GetActiveWebContents()];
 }
 
-void HandoffObserver::OnBrowserClosed(BrowserWindowInterface* removed_browser) {
+void HandoffObserver::OnBrowserRemoved(Browser* removed_browser) {
   if (active_browser_ != removed_browser) {
     return;
   }
@@ -61,20 +59,20 @@ void HandoffObserver::TitleWasSet(content::NavigationEntry* entry) {
   [delegate_ handoffContentsChanged:GetActiveWebContents()];
 }
 
-void HandoffObserver::SetActiveBrowser(BrowserWindowInterface* active_browser) {
+void HandoffObserver::SetActiveBrowser(Browser* active_browser) {
   if (active_browser == active_browser_) {
     return;
   }
 
   if (active_browser_) {
-    active_browser_->GetTabStripModel()->RemoveObserver(this);
+    active_browser_->tab_strip_model()->RemoveObserver(this);
     StopObservingWebContents();
   }
 
   active_browser_ = active_browser;
 
   if (active_browser_) {
-    active_browser_->GetTabStripModel()->AddObserver(this);
+    active_browser_->tab_strip_model()->AddObserver(this);
     content::WebContents* web_contents = GetActiveWebContents();
     if (web_contents) {
       StartObservingWebContents(web_contents);
@@ -97,5 +95,5 @@ content::WebContents* HandoffObserver::GetActiveWebContents() {
     return nullptr;
   }
 
-  return active_browser_->GetTabStripModel()->GetActiveWebContents();
+  return active_browser_->tab_strip_model()->GetActiveWebContents();
 }
