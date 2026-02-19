3f9a33573643b5d423232e5aaee58a6669dc8ee8
Alexis Hetu <sugoi@chromium.org>
2026-02-19 04:51:10 -0800

    [iOS] Add custom symbols for enhanced autofill
    This CL adds 2 symbols, which will be used as such:
    1) Passport symbol, used as the icon for the passport entity type
    2) Text analysis symbol, used in the enhanced autofill settings menu
    
    Bug: 484069556, 480933607
    Change-Id: I622ce0834ccc0ef176d713afe02d72572d9cf2f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7588477
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1587029}

--- ios/chrome/browser/shared/ui/symbols/symbol_names.mm
+++ ios/chrome/browser/shared/ui/symbols/symbol_names.mm
@@ -46,6 +46,7 @@ NSString* const kShieldSymbol = @"shield";
 NSString* const kReadingListSymbol = @"square_bullet_square";
 NSString* const kRecentTabsSymbol = @"laptopcomputer_and_phone";
 NSString* const kLanguageSymbol = @"language";
+NSString* const kPassportSymbol = @"passport";
 NSString* const kPasswordSymbol = @"password";
 #if !BUILDFLAG(IS_IOS_MACCATALYST)
 NSString* const kMulticolorPasswordSymbol = @"multicolor_password";
@@ -71,6 +72,7 @@ NSString* const kMagnifyingglassSparkSymbol = @"magnifyingglass_spark";
 NSString* const kPhoneSparkleSymbol = @"phone_sparkle";
 NSString* const kTextSearchSymbol = @"text_search";
 NSString* const kIncognitoRectangle = @"incognito_rectangle";
+NSString* const kTextAnalysisSymbol = @"text_analysis";
 NSString* const kTextSparkSymbol = @"text_spark";
 NSString* const kIncognitoCircleFillSymbol = @"incognito_circle_fill";
 NSString* const kPlusCircleFillSymbol = @"plus_circle_fill";
