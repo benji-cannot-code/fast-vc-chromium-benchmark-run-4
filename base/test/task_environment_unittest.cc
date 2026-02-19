d237f71c2ca82b556f746f2f78d258fa1ddf11e8
Joe Mason <joenotcharles@google.com>
2026-02-18 19:34:55 -0800

    Add null checks to MAIN_THREAD_ONLY TaskEnvironment
    RunUntilQuit and DescribeCurrentTasks both access `task_tracker_`, which
    is null in MAIN_THREAD_ONLY mode. Adds null checks to them.
    
    R=fdoray
    
    Change-Id: Id50af777686ed89931a00d73b8d8e13e21d53864
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7588182
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1586864}

--- base/test/task_environment_unittest.cc
+++ base/test/task_environment_unittest.cc
@@ -971,7 +971,8 @@ TEST_F(TaskEnvironmentTest, SetsDefaultRunTimeout) {
 
 TEST_F(TaskEnvironmentTest, DescribeCurrentTasksHasPendingMainThreadTasks) {
   TaskEnvironment task_environment;
-  SingleThreadTaskRunner::GetCurrentDefault()->PostTask(FROM_HERE, DoNothing());
+  SingleThreadTaskRunner::GetCurrentDefault()->PostTask(
+      FROM_HERE, task_environment.QuitClosure());
 
   test::MockLog mock_log;
   mock_log.StartCapturingLogs();
@@ -986,7 +987,7 @@ TEST_F(TaskEnvironmentTest, DescribeCurrentTasksHasPendingMainThreadTasks) {
       .WillOnce(Return(true));
   task_environment.DescribeCurrentTasks();
 
-  task_environment.RunUntilIdle();
+  task_environment.RunUntilQuit();
 
   // Thread pool tasks (none here) are logged.
   EXPECT_CALL(mock_log, Log(::logging::LOGGING_INFO, _, _, _,
@@ -1043,6 +1044,38 @@ TEST_F(TaskEnvironmentTest, DescribeCurrentTasksHasThreadPoolTasks) {
                             Not(HasSubstr("task_environment_unittest.cc"))))
       .WillOnce(Return(true));
   // Main thread pending tasks (none here) are logged.
+  EXPECT_CALL(mock_log, Log(::logging::LOGGING_INFO, _, _, _,
+                            HasSubstr("\"immediate_work_queue_size\":0")))
+      .WillOnce(Return(true));
+  task_environment.DescribeCurrentTasks();
+}
+
+TEST_F(TaskEnvironmentTest, DescribeCurrentTasksHasPendingMainThreadOnlyTasks) {
+  TaskEnvironment task_environment{
+      TaskEnvironment::ThreadingMode::MAIN_THREAD_ONLY};
+  SingleThreadTaskRunner::GetCurrentDefault()->PostTask(
+      FROM_HERE, task_environment.QuitClosure());
+
+  test::MockLog mock_log;
+  mock_log.StartCapturingLogs();
+
+  // Thread pool tasks aren't logged since there is no ThreadPool.
+  EXPECT_CALL(mock_log, Log(::logging::LOGGING_INFO, _, _, _,
+                            HasSubstr("ThreadPool currently running tasks")))
+      .Times(0);
+  // The pending task posted above to the main thread is logged.
+  EXPECT_CALL(mock_log, Log(::logging::LOGGING_INFO, _, _, _,
+                            HasSubstr("task_environment_unittest.cc")))
+      .WillOnce(Return(true));
+  task_environment.DescribeCurrentTasks();
+
+  task_environment.RunUntilQuit();
+
+  // Thread pool tasks aren't logged since there is no ThreadPool.
+  EXPECT_CALL(mock_log, Log(::logging::LOGGING_INFO, _, _, _,
+                            HasSubstr("ThreadPool currently running tasks")))
+      .Times(0);
+  // Pending tasks (none left) are logged.
   EXPECT_CALL(mock_log, Log(::logging::LOGGING_INFO, _, _, _,
                             HasSubstr("\"immediate_work_queue_size\":0")))
       .WillOnce(Return(true));
@@ -1662,6 +1695,22 @@ TEST_F(TaskEnvironmentTest, RunUntilQuit_RunsThreadPool) {
                          task_run = true;
                          quit.Run();
                        }));
+  task_environment.RunUntilQuit();
+
+  ASSERT_TRUE(task_run);
+}
+
+TEST_F(TaskEnvironmentTest, RunUntilQuit_RunsMainThreadOnly) {
+  TaskEnvironment task_environment{
+      TaskEnvironment::ThreadingMode::MAIN_THREAD_ONLY};
+  bool task_run = false;
+  auto quit = task_environment.QuitClosure();
+
+  SequencedTaskRunner::GetCurrentDefault()->PostTask(FROM_HERE,
+                                                     BindLambdaForTesting([&] {
+                                                       task_run = true;
+                                                       quit.Run();
+                                                     }));
   task_environment.RunUntilQuit();
 
   ASSERT_TRUE(task_run);
