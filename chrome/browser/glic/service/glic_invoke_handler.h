f977a9ba324bb2ea7b8bd8c7be2248b343840ae9
Marke Hallowell <wry@chromium.org>
2026-02-19 11:16:44 -0800

    Add a basic invoke handler.
    Creates a basic invoke handler responsible for sending invocations to
    the client. Follow-up CLs will add robust polling behavior on instance
    readiness and error handling.
    
    Bug: b:483387751
    Change-Id: I73503ad4ced9c359cf20aaa2dc2402f701f55d3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7589912
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Commit-Queue: Marke Hallowell <wry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1587300}

---
+++ chrome/browser/glic/service/glic_invoke_handler.h
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_GLIC_SERVICE_GLIC_INVOKE_HANDLER_H_
+#define CHROME_BROWSER_GLIC_SERVICE_GLIC_INVOKE_HANDLER_H_
+
+#include "base/functional/callback.h"
+#include "base/memory/raw_ref.h"
+#include "chrome/browser/glic/public/glic_instance.h"
+#include "chrome/browser/glic/public/glic_invoke_options.h"
+
+namespace glic {
+
+class GlicInstanceImpl;
+
+// Handles an invocation of Glic, parsing options and communicating with the
+// instance's host.
+class GlicInvokeHandler {
+ public:
+  using InvokeCompleteCallback =
+      base::OnceCallback<void(const InstanceId&, GlicInvokeHandler*)>;
+
+  GlicInvokeHandler(GlicInstanceImpl& instance,
+                    GlicInvokeOptions options,
+                    InvokeCompleteCallback invoke_complete_callback);
+  ~GlicInvokeHandler();
+
+  GlicInvokeHandler(const GlicInvokeHandler&) = delete;
+  GlicInvokeHandler& operator=(const GlicInvokeHandler&) = delete;
+
+ private:
+  void SendToClient();
+  mojom::InvokeOptionsPtr CreateMojoOptions();
+  void OnSendToClientComplete(base::OnceClosure callback);
+
+  const base::raw_ref<GlicInstanceImpl> instance_;
+  GlicInvokeOptions options_;
+  InvokeCompleteCallback invoke_complete_callback_;
+};
+
+}  // namespace glic
+
+#endif  // CHROME_BROWSER_GLIC_SERVICE_GLIC_INVOKE_HANDLER_H_
