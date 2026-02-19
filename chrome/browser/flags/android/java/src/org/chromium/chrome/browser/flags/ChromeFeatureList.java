8290e863a608e87753c270ca11d260a87d9a5788
Zhe Li <zheliooo@google.com>
2026-02-18 18:27:29 -0800

    [SharedPrefsMigration] Add feature flag
    Bug: 438834621
    Change-Id: I0d2b63e09f638753fe1a93cc858f238c5a3d2471
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7585387
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Auto-Submit: Zhe Li <zheliooo@google.com>
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Commit-Queue: Zhe Li <zheliooo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1586837}

--- chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
@@ -493,6 +493,8 @@ public abstract class ChromeFeatureList {
     public static final String MOST_VISITED_TILES_RESELECT = "MostVisitedTilesReselect";
     public static final String MOVE_TO_FRONT_IN_LAUNCH_INTENT_DISPATCHER =
             "MoveToFrontInLaunchIntentDispatcher";
+    public static final String MULTI_INSTANCE_SHARED_PREFS_MIGRATION =
+            "MultiInstanceSharedPrefsMigration";
     public static final String MVC_UPDATE_VIEW_WHEN_MODEL_CHANGED = "MvcUpdateViewWhenModelChanged";
     public static final String NAV_BAR_COLOR_ANIMATION = "NavBarColorAnimation";
     public static final String NEW_TAB_PAGE_CUSTOMIZATION = "NewTabPageCustomization";
@@ -974,6 +976,8 @@ public abstract class ChromeFeatureList {
                     MOVE_TO_FRONT_IN_LAUNCH_INTENT_DISPATCHER,
                     /* defaultValue= */ false,
                     /* defaultValueInTests= */ true);
+    public static final CachedFlag sMultiInstanceSharedPrefsMigration =
+            newCachedFlag(MULTI_INSTANCE_SHARED_PREFS_MIGRATION, false);
     public static final CachedFlag sMvcUpdateViewWhenModelChanged =
             newCachedFlag(
                     MVC_UPDATE_VIEW_WHEN_MODEL_CHANGED,
@@ -1220,6 +1224,7 @@ public abstract class ChromeFeatureList {
                     sMostVisitedTilesCustomization,
                     sMostVisitedTilesReselect,
                     sMoveToFrontInLaunchIntentDispatcher,
+                    sMultiInstanceSharedPrefsMigration,
                     sMvcUpdateViewWhenModelChanged,
                     sNavBarColorAnimation,
                     sNewTabPageCustomization,
