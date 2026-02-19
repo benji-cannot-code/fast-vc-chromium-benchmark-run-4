85c88299f3cc0bec3c23d2de2097dbca92c54e3a
Rebecca <qihuizhao@google.com>
2026-02-19 10:08:42 -0800

    Create PaymentsScanSaveAndFillOfferBottomSheet skeleton
    Bug: 483112815
    Change-Id: I626f71632a588f49ff7162999efba8239886ce4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7559763
    Reviewed-by: Olivia Saul <osaul@google.com>
    Commit-Queue: Qihui Zhao <qihuizhao@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1587248}

---
+++ ios/chrome/browser/autofill/scan_save_and_fill/ui/payments_scan_save_and_fill_offer_bottom_sheet_consumer.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_AUTOFILL_SCAN_SAVE_AND_FILL_UI_PAYMENTS_SCAN_SAVE_AND_FILL_OFFER_BOTTOM_SHEET_CONSUMER_H_
+#define IOS_CHROME_BROWSER_AUTOFILL_SCAN_SAVE_AND_FILL_UI_PAYMENTS_SCAN_SAVE_AND_FILL_OFFER_BOTTOM_SHEET_CONSUMER_H_
+
+#import <Foundation/Foundation.h>
+
+// Consumer for the payments scan save and fill offer bottom sheet.
+@protocol PaymentsScanSaveAndFillOfferBottomSheetConsumer
+
+// Request to dismiss the bottom sheet.
+- (void)dismiss;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_AUTOFILL_SCAN_SAVE_AND_FILL_UI_PAYMENTS_SCAN_SAVE_AND_FILL_OFFER_BOTTOM_SHEET_CONSUMER_H_
