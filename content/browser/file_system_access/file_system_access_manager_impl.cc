fc0033848abae564c53d3418c00ca5732878ae72
Andrew Paseltiner <apaseltiner@chromium.org>
2026-02-19 08:27:04 -0800

    Remove unnecessary string allocation from FileSystemAccessManagerImpl::DeserializeHandle
    ParseFromString accepts a string view.
    
    Change-Id: Ib2ea80459634dc1da1fdf038221877206fe022e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7591739
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1587169}

--- content/browser/file_system_access/file_system_access_manager_impl.cc
+++ content/browser/file_system_access/file_system_access_manager_impl.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/file_util_icu.h"
 #include "base/notreached.h"
 #include "base/strings/string_util.h"
+#include "base/strings/string_view_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/task_traits.h"
@@ -1105,9 +1106,8 @@ void FileSystemAccessManagerImpl::DeserializeHandle(
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(!bits.empty());
 
-  std::string bits_as_string(bits.begin(), bits.end());
   FileSystemAccessHandleData data;
-  if (!data.ParseFromString(bits_as_string)) {
+  if (!data.ParseFromString(base::as_string_view(bits))) {
     // Drop `token`, and directly return.
     return;
   }
