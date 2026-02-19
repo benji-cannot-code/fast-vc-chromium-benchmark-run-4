ed3142d1be5866f35c715c2aff6cbdf7d6b708b0
Kay Lin <kaiyilin@google.com>
2026-02-19 00:56:52 -0800

    Site Search: Add site search section MVC scaffolding
    This CL adds the basic MVC components for the custom site search
    section. It defines the Coordinator, Views, Properties and Adapter
    setup. The business logic and Mediator will be added in follow-up CLs.
    
    Bug: 479395655
    Change-Id: I037dd9b5b8a65a8b4be4eae1d8859c25ba48e361
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7580340
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Kay Lin <kaiyilin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1586953}

--- chrome/browser/search_engines/android/java/src/org/chromium/chrome/browser/search_engines/settings/SiteSearchSettings.java
+++ chrome/browser/search_engines/android/java/src/org/chromium/chrome/browser/search_engines/settings/SiteSearchSettings.java
@@ -15,6 +15,8 @@ import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.browser.search_engines.R;
 import org.chromium.chrome.browser.search_engines.settings.custom_search_engine.CustomSearchEngineListCoordinator;
 import org.chromium.chrome.browser.search_engines.settings.custom_search_engine.CustomSearchEngineListPreference;
+import org.chromium.chrome.browser.search_engines.settings.custom_site_search.CustomSiteSearchCoordinator;
+import org.chromium.chrome.browser.search_engines.settings.custom_site_search.CustomSiteSearchListPreference;
 import org.chromium.chrome.browser.settings.ChromeBaseSettingsFragment;
 import org.chromium.components.browser_ui.settings.SettingsFragment;
 import org.chromium.components.browser_ui.settings.SettingsUtils;
@@ -27,10 +29,12 @@ import org.chromium.components.browser_ui.settings.SettingsUtils;
 public class SiteSearchSettings extends ChromeBaseSettingsFragment {
     // TODO(crbug.com/478726836): See if this needs to be added to the search index
     private static final String CUSTOM_SEARCH_ENGINE_LIST_PREF = "custom_search_engine_item_list";
+    private static final String CUSTOM_SITE_SEARCH_LIST_PREF = "custom_site_search_item_list";
     private final SettableMonotonicObservableSupplier<String> mPageTitle =
             ObservableSuppliers.createMonotonic();
 
     private @Nullable CustomSearchEngineListCoordinator mSearchEngineCoordinator;
+    private @Nullable CustomSiteSearchCoordinator mSiteSearchCoordinator;
 
     @Override
     public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
@@ -43,6 +47,15 @@ public class SiteSearchSettings extends ChromeBaseSettingsFragment {
                 mSearchEngineCoordinator =
                         new CustomSearchEngineListCoordinator(
                                 getContext(), getProfile(), customSearchEnginePref);
+            }
+        }
+
+        SettingsUtils.addPreferencesFromResource(this, R.xml.custom_site_search_preferences);
+        CustomSiteSearchListPreference customSiteSearchPref =
+                findPreference(CUSTOM_SITE_SEARCH_LIST_PREF);
+        if (customSiteSearchPref != null) {
+            if (mSiteSearchCoordinator == null) {
+                mSiteSearchCoordinator = new CustomSiteSearchCoordinator(getContext());
             }
         }
     }
@@ -62,6 +75,10 @@ public class SiteSearchSettings extends ChromeBaseSettingsFragment {
         if (mSearchEngineCoordinator != null) {
             mSearchEngineCoordinator.destroy();
             mSearchEngineCoordinator = null;
+        }
+        if (mSiteSearchCoordinator != null) {
+            mSiteSearchCoordinator.destroy();
+            mSiteSearchCoordinator = null;
         }
         super.onDestroy();
     }
