4ab00b04c86ed98bd754b23e5e8f4c23c202f236
Dana Fried <dfried@chromium.org>
2026-02-19 11:27:29 -0800

    [Chrome Next] Fix 1px flyover issue in THSP
    This fixes the "sometimes the webcontents is 1px taller than it should
    be during animations" issue.
    
    Still to do:
     - Resolve issues with split tabs (see attached bug for current state)
    
    Bug: 485909751
    Change-Id: Ieab2d454ff807007f500993173cc22507f03c9db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7595113
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1587305}

--- chrome/browser/ui/views/frame/multi_contents_view.h
+++ chrome/browser/ui/views/frame/multi_contents_view.h
@@ -139,8 +139,6 @@ class MultiContentsView : public views::View,
     gfx::Insets clipped_area;
 
     bool operator==(const TargetContentBounds&) const = default;
-
-    void Outset(const gfx::Outsets& outsets);
   };
   void SetTargetContentBounds(
       std::optional<TargetContentBounds> target_content_bounds);
