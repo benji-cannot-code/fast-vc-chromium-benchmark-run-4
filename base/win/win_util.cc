4f39a61f124b91bfbd009431dd86c4d17da7acad
Jesse McKenna <jessemckenna@google.com>
2026-02-19 09:47:19 -0800

    Prepend :: to Windows API calls
    The norm in Chromium is the prepend Windows API calls with ::. I noticed
    a few places in win_util.cc where it's missing, so this change adds it.
    
    Change-Id: I9d3ca317302dfa9482b82e0c63a06f57b9ec59ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7588720
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: David Bienvenu <davidbienvenu@chromium.org>
    Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1587228}

--- base/win/win_util.cc
+++ base/win/win_util.cc
@@ -86,8 +86,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/windows_version.h"
 #include "base/win/wmi.h"
 
-namespace base {
-namespace win {
+namespace base::win {
 
 namespace {
 
@@ -128,7 +127,7 @@ void __cdecl ForceCrashOnSigAbort(int) {
 // Returns the current platform role. We use the PowerDeterminePlatformRoleEx
 // API for that.
 POWER_PLATFORM_ROLE GetPlatformRole() {
-  return PowerDeterminePlatformRoleEx(POWER_PLATFORM_ROLE_V2);
+  return ::PowerDeterminePlatformRoleEx(POWER_PLATFORM_ROLE_V2);
 }
 
 // Enable V2 per-monitor high-DPI support for the process. This will cause
@@ -157,7 +156,7 @@ bool EnablePerMonitorV2() {
 }
 
 bool* GetDomainEnrollmentStateStorage() {
-  static bool state = IsOS(OS_DOMAINMEMBER);
+  static bool state = ::IsOS(OS_DOMAINMEMBER);
   return &state;
 }
 
@@ -659,7 +658,7 @@ void IsDeviceSlateWithKeyboard(HWND hwnd,
   // If no touch screen detected, assume keyboard attached.
   // TODO(crbug.com/383267933) If the device is mouse only with no touch screen,
   // this will determine that the device has a keyboard.
-  if ((GetSystemMetrics(SM_DIGITIZER) & NID_INTEGRATED_TOUCH) !=
+  if ((::GetSystemMetrics(SM_DIGITIZER) & NID_INTEGRATED_TOUCH) !=
       NID_INTEGRATED_TOUCH) {
     reason << "NID_INTEGRATED_TOUCH\n";
   }
@@ -859,7 +858,7 @@ bool IsDeviceUsedAsATablet(std::string* reason) {
   // reason is NULL.
   std::optional<bool> ret;
 
-  if (GetSystemMetrics(SM_MAXIMUMTOUCHES) == 0) {
+  if (::GetSystemMetrics(SM_MAXIMUMTOUCHES) == 0) {
     if (!reason) {
       return false;
     }
@@ -869,7 +868,7 @@ bool IsDeviceUsedAsATablet(std::string* reason) {
   }
 
   // If the device is docked, the user is treating the device as a PC.
-  if (GetSystemMetrics(SM_SYSTEMDOCKED) != 0) {
+  if (::GetSystemMetrics(SM_SYSTEMDOCKED) != 0) {
     if (!reason) {
       return false;
     }
@@ -884,7 +883,7 @@ bool IsDeviceUsedAsATablet(std::string* reason) {
   // a convertible or a detachable.
   // See
   // https://msdn.microsoft.com/en-us/library/windows/desktop/dn629263(v=vs.85).aspx
-  using GetAutoRotationStateType = decltype(GetAutoRotationState)*;
+  using GetAutoRotationStateType = decltype(::GetAutoRotationState)*;
   static const auto get_auto_rotation_state_func =
       reinterpret_cast<GetAutoRotationStateType>(
           GetUser32FunctionPointer("GetAutoRotationState"));
@@ -900,7 +899,7 @@ bool IsDeviceUsedAsATablet(std::string* reason) {
   POWER_PLATFORM_ROLE role = GetPlatformRole();
   bool is_tablet = false;
   if (role == PlatformRoleMobile || role == PlatformRoleSlate) {
-    is_tablet = !GetSystemMetrics(SM_CONVERTIBLESLATEMODE);
+    is_tablet = !::GetSystemMetrics(SM_CONVERTIBLESLATEMODE);
     if (!is_tablet) {
       if (!reason) {
         return false;
@@ -1036,7 +1035,7 @@ std::wstring WStringFromGUID(const ::GUID& rguid) {
   constexpr int kGuidStringCharacters =
       1 + 8 + 1 + 4 + 1 + 4 + 1 + 4 + 1 + 12 + 1 + 1;
   wchar_t guid_string[kGuidStringCharacters];
-  CHECK(SUCCEEDED(StringCchPrintfW(
+  CHECK(SUCCEEDED(::StringCchPrintfW(
       guid_string, kGuidStringCharacters,
       L"{%08lX-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X}", rguid.Data1,
       rguid.Data2, rguid.Data3, rguid.Data4[0], rguid.Data4[1], rguid.Data4[2],
@@ -1146,7 +1145,7 @@ bool IsCurrentSessionRemote() {
 }
 
 bool IsAppVerifierLoaded() {
-  return GetModuleHandleA(kApplicationVerifierDllName);
+  return ::GetModuleHandleA(kApplicationVerifierDllName);
 }
 
 std::optional<std::wstring> ExpandEnvironmentVariables(wcstring_view str) {
@@ -1314,5 +1313,4 @@ ScopedDeviceConvertibilityStateForTesting::
 ScopedDeviceConvertibilityStateForTesting::
     ~ScopedDeviceConvertibilityStateForTesting() = default;
 
-}  // namespace win
-}  // namespace base
+}  // namespace base::win
