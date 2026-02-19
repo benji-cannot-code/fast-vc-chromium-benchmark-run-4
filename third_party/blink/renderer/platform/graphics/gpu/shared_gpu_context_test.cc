a438f29e088ef00984b5d678d0e57442b261ad60
Colin Blundell <blundell@chromium.org>
2026-02-19 00:01:24 -0800

    [Blink] Remove ShouldInitialize param from CNon2DRPSI::Create()
    All callers pass kNo.
    
    Bug: 352263194
    Change-Id: Ib30918bec94c8256118d16bf6fdcba65544037ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7588119
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1586925}

--- third_party/blink/renderer/platform/graphics/gpu/shared_gpu_context_test.cc
+++ third_party/blink/renderer/platform/graphics/gpu/shared_gpu_context_test.cc
@@ -153,7 +153,6 @@ TEST_F(BadSharedGpuContextTest, AccelerateImageBufferSurfaceCreationFails) {
       CanvasNon2DResourceProviderSharedImage::Create(
           gfx::Size(10, 10), GetN32FormatForCanvas(), kPremul_SkAlphaType,
           gfx::ColorSpace::CreateSRGB(),
-          CanvasResourceProvider::ShouldInitialize::kNo,
           SharedGpuContext::ContextProviderWrapper(),
           gpu::SharedImageUsageSet());
   EXPECT_FALSE(resource_provider);
@@ -180,7 +179,6 @@ TEST_F(SharedGpuContextTest, AccelerateImageBufferSurfaceAutoRecovery) {
       CanvasNon2DResourceProviderSharedImage::Create(
           gfx::Size(10, 10), GetN32FormatForCanvas(), kPremul_SkAlphaType,
           gfx::ColorSpace::CreateSRGB(),
-          CanvasResourceProvider::ShouldInitialize::kNo,
           SharedGpuContext::ContextProviderWrapper(),
           gpu::SharedImageUsageSet());
   EXPECT_TRUE(resource_provider && resource_provider->IsValid());
