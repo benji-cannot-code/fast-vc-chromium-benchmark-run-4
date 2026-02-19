a8f319a9f9522889ecc46f6bf321062d96c24070
Ireneusz Szulc <ireneuszszulc@google.com>
2026-02-19 07:40:15 -0800

    @memory: Define Suggestion Payload
    This change introduces Suggestion::AtMemoryPayload for the purpose
    of later use by @memory feature.
    It contains the boilerplate updates to satisfy the compiler.
    
    Currently, this doesn't change any behaviour, it only prepares
    necessary class for later use and upcoming larger changes related to
    @memory feature.
    
    Context: The "@memory" feature will enable users to recall and insert
    personal information into any text field by typing a special trigger
    like "@@".
    
    Bug: b:481980303
    Change-Id: I3059f4b8884673a5059271605c664f686a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7581273
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Ireneusz Szulc <ireneuszszulc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1587121}

--- components/autofill/core/browser/suggestions/suggestion.h
+++ components/autofill/core/browser/suggestions/suggestion.h
@@ -193,6 +193,22 @@ struct Suggestion {
     std::map<FieldType, std::u16string> fields;
   };
 
+  struct AtMemoryPayload final {
+    AtMemoryPayload();
+    explicit AtMemoryPayload(std::u16string value);
+    AtMemoryPayload(const AtMemoryPayload&);
+    AtMemoryPayload(AtMemoryPayload&&);
+    AtMemoryPayload& operator=(const AtMemoryPayload&);
+    AtMemoryPayload& operator=(AtMemoryPayload&&);
+    ~AtMemoryPayload();
+
+    friend bool operator==(const AtMemoryPayload&,
+                           const AtMemoryPayload&) = default;
+
+    // Text to fill in the trigger field upon accepting the suggestion.
+    std::u16string value;
+  };
+
   using IsLoading = base::StrongAlias<class IsLoadingTag, bool>;
   using InstrumentId = base::StrongAlias<class InstrumentIdTag, uint64_t>;
   using BnplIssuer = base::StrongAlias<class BnplIssuerTag, BnplIssuer>;
@@ -206,7 +222,8 @@ struct Suggestion {
                                PaymentsPayload,
                                IdentityCredentialPayload,
                                AutocompleteEntry,
-                               BnplIssuer>;
+                               BnplIssuer,
+                               AtMemoryPayload>;
 
   // This struct is used to provide password suggestions with custom icons,
   // using the favicon of the website associated with the credentials. While
@@ -455,6 +472,8 @@ struct Suggestion {
       case SuggestionType::kBnplEntry:
         return std::holds_alternative<PaymentsPayload>(payload) ||
                std::holds_alternative<BnplIssuer>(payload);
+      case SuggestionType::kAtMemorySearchResult:
+        return std::holds_alternative<AtMemoryPayload>(payload);
       case SuggestionType::kDevtoolsTestAddressEntry:
       default:
         return std::holds_alternative<Guid>(payload) ||
