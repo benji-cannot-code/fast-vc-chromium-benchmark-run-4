5589263cf320fc83eb4e05a367010248c54a5f4b
Tommy Steimel <steimel@chromium.org>
2026-02-19 08:41:43 -0800

    [Media Controls] Hide download button when sandbox flags disallow it
    Currently, the default Blink media controls display a (functional)
    download button, even when inside a sandboxed iframe that does not
    allow downloads. This CL hides the download button when the element
    is in a sandboxed iframe that disallows downloads.
    
    Bug: 40064543
    Change-Id: Ie25b48bb994ed66c8621429a3447cc279e67fbfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7590057
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Benjamin Keen <bkeen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1587180}

--- third_party/blink/renderer/modules/media_controls/elements/media_control_download_button_element.cc
+++ third_party/blink/renderer/modules/media_controls/elements/media_control_download_button_element.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/platform.h"
 #include "third_party/blink/public/platform/user_metrics_action.h"
 #include "third_party/blink/public/strings/grit/blink_strings.h"
+#include "third_party/blink/public/web/web_local_frame.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/events/event.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
@@ -49,6 +50,14 @@ bool MediaControlDownloadButtonElement::ShouldDisplayDownloadButton() const {
     UseCounter::Count(MediaElement().GetDocument(),
                       WebFeature::kHTMLMediaElementControlsListNoDownload);
     return false;
+  }
+
+  if (GetDocument().GetFrame()) {
+    WebLocalFrame* web_local_frame = WebLocalFrame::FromFrameToken(
+        GetDocument().GetFrame()->GetLocalFrameToken());
+    if (web_local_frame && !web_local_frame->IsAllowedToDownload()) {
+      return false;
+    }
   }
 
   return true;
