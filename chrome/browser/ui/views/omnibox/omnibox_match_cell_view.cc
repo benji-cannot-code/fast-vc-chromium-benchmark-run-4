5c7d435bad1eaaa0fb5e23cda8dcd997c59c65f1
Adam Yao <adamyao@google.com>
2026-02-19 15:08:39 -0800

    [omnibox] Use kMarginRight for OmniboxMatchCellView right inset.
    This change replaces a hardcoded right inset value with the existing
    kMarginRight constant and updates kMarginRight to 7 to match the
    previously hardcoded value.
    
    Bug: 360430665
    Change-Id: Ia28a1ad1174ca186176f2951214b2baaac567155
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7577793
    Commit-Queue: Adam Yao <adamyao@google.com>
    Reviewed-by: Jennifer Serrano <jennserrano@google.com>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1587449}

--- chrome/browser/ui/views/omnibox/omnibox_match_cell_view.cc
+++ chrome/browser/ui/views/omnibox/omnibox_match_cell_view.cc
@@ -457,7 +457,7 @@ gfx::Insets OmniboxMatchCellView::GetInsets() const {
           ? kToolbeltTextInsetRight
       : layout_style_ == LayoutStyle::IPH_SUGGESTION
           ? OmniboxMatchCellView::kMarginLeft + kIphTextIndent
-          : 7;
+          : kMarginRight;
   return gfx::Insets::TLBR(vertical_margin, OmniboxMatchCellView::kMarginLeft,
                            vertical_margin, right_margin);
 }
