280c033d532280826b259a893a30ad5bc761fd1c
Taiyo Mizuhashi <taiyo@chromium.org>
2026-02-19 12:27:22 -0800

    Prefetch: Decouple traffic annotation to prefetch_resource_request_utils
    As a series of work decoupling prefetch resource request creation from
    `PrefetchContainer` for OMT.Prefetch.
    
    This CL focuses to move the current definition as-is, to unblock the
    OMT.Prefetch work.
    * Updates to other speculation rules prefetch
    * Adds missing fields, which is currently bypassed via safe_list.txt
    will be tracked by crbug.com/452392023 and should be fixed in follow-up
    CLs.
    
    Bug: 452392023, 452406598, 452389538, 484967082
    Change-Id: I811f24c129cbb69c5630738e545bd9c7a9d26d2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7555874
    Auto-Submit: Taiyo Mizuhashi <taiyo@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: Taiyo Mizuhashi <taiyo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1587342}

---
+++ content/browser/preloading/prefetch/prefetch_resource_request_utils.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_PRELOADING_PREFETCH_PREFETCH_RESOURCE_REQUEST_UTILS_H_
+#define CONTENT_BROWSER_PRELOADING_PREFETCH_PREFETCH_RESOURCE_REQUEST_UTILS_H_
+
+#include "net/traffic_annotation/network_traffic_annotation.h"
+
+namespace content {
+
+// Avoid using `inline constexpr` here in order to place the definition to
+// `.cc` file to get `tools/traffic_annotation/scripts/auditor/auditor.py` to
+// work (See crbug.com/484967082 for more details).
+extern const net::NetworkTrafficAnnotationTag
+    kNavigationalPrefetchTrafficAnnotation;
+
+}  // namespace content
+
+#endif  // CONTENT_BROWSER_PRELOADING_PREFETCH_PREFETCH_RESOURCE_REQUEST_UTILS_H_
