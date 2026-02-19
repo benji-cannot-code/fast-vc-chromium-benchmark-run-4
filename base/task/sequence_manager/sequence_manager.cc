f48726257321216e7a9abf4a93ebf4eb7d6d179f
Etienne Pierre-doray <etiennep@chromium.org>
2026-02-19 11:49:56 -0800

    [task] DefaultTaskPriorityToThreadType allows kMaxValue
    This CL makes DefaultTaskPriorityToThreadType() more relaxed,
    since this is constraints by the executing thread's ThreadType.
    This allows e.g. a kPresentation thread to not have to provide a
    custom ThreadTypeMapping.
    
    Bug: 470337728
    Change-Id: I7dcd2bcfecf3c824907688dd2a74553bcd76b9c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7580517
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1587322}

--- base/task/sequence_manager/sequence_manager.cc
+++ base/task/sequence_manager/sequence_manager.cc
@@ -43,7 +43,7 @@ SequenceManager::PrioritySettings::CreateDefault() {
 
 ThreadType SequenceManager::PrioritySettings::DefaultTaskPriorityToThreadType(
     TaskQueue::QueuePriority priority) {
-  return ThreadType::kDefault;
+  return ThreadType::kMaxValue;
 }
 
 SequenceManager::PrioritySettings::PrioritySettings(
