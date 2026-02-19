695fe7f00735d6c0555b6e69761a756c3dcb1280
Thomas Lukaszewicz <tluk@chromium.org>
2026-02-18 23:04:10 -0800

    [bedrock] Migrate BrowserTabStripTracker to BrowserCollectionObserver
    Bug: 431671320
    Change-Id: I8993637056133acf0aa10c891e83c2e18d14b114
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7591064
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1586906}

--- chrome/browser/ui/browser_tab_strip_tracker.h
+++ chrome/browser/ui/browser_tab_strip_tracker.h
@@ -7,10 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_BROWSER_TAB_STRIP_TRACKER_H_
 
 #include "base/memory/raw_ptr.h"
-#include "chrome/browser/ui/browser_list_observer.h"
+#include "base/scoped_observation.h"
+#include "chrome/browser/ui/browser_window/public/browser_collection_observer.h"
 
 class BrowserTabStripTrackerDelegate;
 class BrowserWindowInterface;
+class GlobalBrowserCollection;
 class TabStripModelObserver;
 
 // BrowserTabStripTracker attaches a TabStripModelObserver to a subset of
@@ -25,7 +27,7 @@ class TabStripModelObserver;
 // TabFeatures. Furthermore, this class mixes state across multiple
 // TabStripModels. This is typically not desirable and instead features should
 // hold state on a per-browser-window basis, using BrowserWindowFeatures.
-class BrowserTabStripTracker : public BrowserListObserver {
+class BrowserTabStripTracker : public BrowserCollectionObserver {
  public:
   // |tab_strip_model_observer| is a non-nullptr TabStripModelObserver
   // registered on tracked Browsers. |delegate| determines which Browsers are
@@ -60,13 +62,15 @@ class BrowserTabStripTracker : public BrowserListObserver {
   // TabStripModelObserver is attached.
   void MaybeTrackBrowser(BrowserWindowInterface* browser);
 
-  // BrowserListObserver:
-  void OnBrowserAdded(Browser* browser) override;
-  void OnBrowserRemoved(Browser* browser) override;
+  // BrowserCollectionObserver:
+  void OnBrowserCreated(BrowserWindowInterface* browser) override;
+  void OnBrowserClosed(BrowserWindowInterface* browser) override;
 
   raw_ptr<TabStripModelObserver> const tab_strip_model_observer_;
   raw_ptr<BrowserTabStripTrackerDelegate> const delegate_;
   bool is_processing_initial_browsers_;
+  base::ScopedObservation<GlobalBrowserCollection, BrowserCollectionObserver>
+      browser_collection_observation_{this};
 };
 
 #endif  // CHROME_BROWSER_UI_BROWSER_TAB_STRIP_TRACKER_H_
