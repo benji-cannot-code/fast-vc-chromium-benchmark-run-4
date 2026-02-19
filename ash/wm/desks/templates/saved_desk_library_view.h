71d092c01a8c01f5942521f2552901b7da3898f3
Di Wu <diwux@google.com>
2026-02-18 22:39:19 -0800

    Ash: Unify grid view UI updates in SavedDeskLibraryView
    Currently, SavedDeskLibraryView maintains individual pointers for
    different grid views (desk_template_grid_view_,
    save_and_recall_grid_view_, and coral_grid_view_). In
    AddOrUpdateEntries, it manually groups the entries by type using a
    custom SavedDesks struct and a helper Group() function, then
    routes them to each specific grid view individually if it exists.
    
    This CL refactors this logic by replacing the individual grid view
    pointers with a base::flat_map<DeskTemplateType, SavedDeskGridView*>.
    This allows us to:
    
    1. Eliminate the SavedDesks struct and Group() helper.
    2. Group the entries into a flat_map dynamically and iterate over the
       grid_views_map_ to update each grid view uniformly in a single loop,
       simplifying the code and making it easier to scale for new desk
       template types.
    3. Update SavedDeskLibraryViewTestApi to retrieve the coral grid view
       using the new map instead of the removed pointer.
    
    Bug: None
    Test: CQ
    Change-Id: Idc962ae54cd7945b0b0b9899158b59e3ec7eeeab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7580556
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Di Wu <diwux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1586902}

--- ash/wm/desks/templates/saved_desk_library_view.h
+++ ash/wm/desks/templates/saved_desk_library_view.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <vector>
 
+#include "base/containers/flat_map.h"
 #include "base/memory/raw_ptr.h"
 #include "base/uuid.h"
 #include "ui/aura/window_observer.h"
@@ -23,6 +24,7 @@ namespace ash {
 
 class DeskMiniView;
 class DeskTemplate;
+enum class DeskTemplateType;
 class SavedDeskGridView;
 class SavedDeskItemView;
 class SavedDeskLibraryEventHandler;
@@ -112,11 +114,11 @@ class SavedDeskLibraryView : public views::View, public aura::WindowObserver {
   // aura::WindowObserver:
   void OnWindowDestroying(aura::Window* window) override;
 
-  // Pointers to the grids with saved desks of specific types. These will be set
-  // depending on which features are enabled.
-  raw_ptr<SavedDeskGridView> desk_template_grid_view_ = nullptr;
-  raw_ptr<SavedDeskGridView> save_and_recall_grid_view_ = nullptr;
-  raw_ptr<SavedDeskGridView> coral_grid_view_ = nullptr;
+  // Maps the desk template type to the corresponding grid view. These will be
+  // set depending on which features are enabled.
+  base::flat_map<DeskTemplateType,
+                 raw_ptr<SavedDeskGridView, VectorExperimental>>
+      grid_views_map_;
 
   // Used for scroll functionality of the library page. Owned by views
   // hierarchy.
