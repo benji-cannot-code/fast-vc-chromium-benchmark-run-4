7d92449d64db79f10797fed4c5086d54d161139b
Joe Mason <joenotcharles@google.com>
2026-02-19 14:57:22 -0800

    Enable heap profiler bloom filter by default
    Bug: 481079751
    Change-Id: Ie899f87b3f9275953d94a811ceeb29b16b80784f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7595319
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1587442}

--- base/sampling_heap_profiler/lock_free_address_hash_set.cc
+++ base/sampling_heap_profiler/lock_free_address_hash_set.cc
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
-BASE_FEATURE(kUseLockFreeBloomFilter, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kUseLockFreeBloomFilter, base::FEATURE_ENABLED_BY_DEFAULT);
 
 namespace {
 
