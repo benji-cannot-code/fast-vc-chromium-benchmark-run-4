7e29db345eed2097306e58d78f9ce9f35e8f7a73
Colin Blundell <blundell@chromium.org>
2026-02-18 23:59:15 -0800

    [Blink] Remove clearing from CanvasNon2DResourceProviderSI in tests
    In all of these tests, either (a) whether the resource is cleared is
    incidental to the test, or (b) clearing of the resource will occur as
    part of the test operation in any case (e.g., a call to Snapshot() or
    ProduceCanvasResource()). This CL eliminates the explicit clears as
    part of moving toward CanvasNon2DResourceProviderSI not supporting
    explicit clearing on creation.
    
    Bug: 352263194
    Change-Id: I5d2908304c5405b8612144bac63dc9b716d70b81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7587897
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1586922}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider_test.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider_test.cc
@@ -336,7 +336,7 @@ TEST_F(CanvasResourceProviderTest,
                                    /*has_alpha=*/true);
   auto provider = CanvasNon2DResourceProviderSharedImage::Create(
       gfx::Size(10, 10), color_params,
-      CanvasResourceProvider::ShouldInitialize::kCallClear,
+      CanvasResourceProvider::ShouldInitialize::kNo,
       SharedGpuContext::ContextProviderWrapper(), shared_image_usage_flags);
 
   auto resource = provider->ProduceCanvasResource(FlushReason::kOther);
@@ -375,7 +375,7 @@ TEST_F(CanvasResourceProviderTest,
                                    CanvasPixelFormat::kUint8,
                                    /*has_alpha=*/true);
   auto provider = CanvasNon2DResourceProviderSharedImage::Create(
-      kSize, color_params, CanvasResourceProvider::ShouldInitialize::kCallClear,
+      kSize, color_params, CanvasResourceProvider::ShouldInitialize::kNo,
       context_provider_wrapper_, shared_image_usage_flags);
 
   EXPECT_EQ(provider->Size(), kSize);
@@ -521,8 +521,8 @@ TEST_F(CanvasResourceProviderTest,
                                    /*has_alpha=*/true);
   auto provider = CanvasNon2DResourceProviderSharedImage::Create(
       gfx::Size(10, 10), color_params,
-      CanvasResourceProvider::ShouldInitialize::kCallClear,
-      context_provider_wrapper_, shared_image_usage_flags);
+      CanvasResourceProvider::ShouldInitialize::kNo, context_provider_wrapper_,
+      shared_image_usage_flags);
 
   ASSERT_TRUE(provider->IsValid());
 
@@ -621,7 +621,7 @@ TEST_F(CanvasResourceProviderTest,
                                    CanvasPixelFormat::kUint8,
                                    /*has_alpha=*/true);
   auto provider = CanvasNon2DResourceProviderSharedImage::Create(
-      kSize, color_params, CanvasResourceProvider::ShouldInitialize::kCallClear,
+      kSize, color_params, CanvasResourceProvider::ShouldInitialize::kNo,
       context_provider_wrapper_, shared_image_usage_flags);
 
   EXPECT_EQ(provider->Size(), kSize);
@@ -663,18 +663,18 @@ TEST_F(CanvasResourceProviderTest, DimensionsExceedMaxTextureSize_SharedImage) {
                                    /*has_alpha=*/true);
   auto provider = CanvasNon2DResourceProviderSharedImage::Create(
       gfx::Size(kMaxTextureSize - 1, kMaxTextureSize), color_params,
-      CanvasResourceProvider::ShouldInitialize::kCallClear,
-      context_provider_wrapper_, gpu::SharedImageUsageSet());
+      CanvasResourceProvider::ShouldInitialize::kNo, context_provider_wrapper_,
+      gpu::SharedImageUsageSet());
   EXPECT_TRUE(provider && provider->IsValid());
   provider = CanvasNon2DResourceProviderSharedImage::Create(
       gfx::Size(kMaxTextureSize, kMaxTextureSize), color_params,
-      CanvasResourceProvider::ShouldInitialize::kCallClear,
-      context_provider_wrapper_, gpu::SharedImageUsageSet());
+      CanvasResourceProvider::ShouldInitialize::kNo, context_provider_wrapper_,
+      gpu::SharedImageUsageSet());
   EXPECT_TRUE(provider && provider->IsValid());
   provider = CanvasNon2DResourceProviderSharedImage::Create(
       gfx::Size(kMaxTextureSize + 1, kMaxTextureSize), color_params,
-      CanvasResourceProvider::ShouldInitialize::kCallClear,
-      context_provider_wrapper_, gpu::SharedImageUsageSet());
+      CanvasResourceProvider::ShouldInitialize::kNo, context_provider_wrapper_,
+      gpu::SharedImageUsageSet());
   // The CanvasResourceProvider for SharedImage should not be created
   // if the texture size is greater than the maximum value.
   EXPECT_FALSE(provider);
@@ -686,13 +686,13 @@ TEST_F(CanvasResourceProviderTest, FlushForImage) {
                                    /*has_alpha=*/true);
   auto src_provider = CanvasNon2DResourceProviderSharedImage::Create(
       gfx::Size(10, 10), color_params,
-      CanvasResourceProvider::ShouldInitialize::kCallClear,
-      context_provider_wrapper_, gpu::SharedImageUsageSet());
+      CanvasResourceProvider::ShouldInitialize::kNo, context_provider_wrapper_,
+      gpu::SharedImageUsageSet());
 
   auto dst_provider = CanvasNon2DResourceProviderSharedImage::Create(
       gfx::Size(10, 10), color_params,
-      CanvasResourceProvider::ShouldInitialize::kCallClear,
-      context_provider_wrapper_, gpu::SharedImageUsageSet());
+      CanvasResourceProvider::ShouldInitialize::kNo, context_provider_wrapper_,
+      gpu::SharedImageUsageSet());
 
   MemoryManagedPaintCanvas& dst_canvas = dst_provider->Canvas();
 
