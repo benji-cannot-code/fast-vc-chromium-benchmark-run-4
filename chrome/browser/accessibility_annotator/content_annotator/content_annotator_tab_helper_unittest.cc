72db051b18fa219374cf5fbdaa7f64ae83095660
Amando Jimenez <amandoj@google.com>
2026-02-19 09:37:48 -0800

    Integrate content classifier into content annotator service.
    This change introduces a new ContentClassifier class to encapsulate the
    logic for running various classification methods. The classifier is
    initialized with rule-based and URL matching classifiers, which are
    configured via new flag parameters.
    
    Bug: 483108681
    Change-Id: Ic2e0af51796967561a966ddefd82ab307d2c68f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7565222
    Reviewed-by: Andrew Vuong <akvuong@google.com>
    Commit-Queue: Amando Jimenez <amandoj@google.com>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1587223}

--- chrome/browser/accessibility_annotator/content_annotator/content_annotator_tab_helper_unittest.cc
+++ chrome/browser/accessibility_annotator/content_annotator/content_annotator_tab_helper_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
 #include "chrome/test/base/testing_profile.h"
 #include "components/accessibility_annotator/content/content_annotator/content_annotator_service.h"
+#include "components/accessibility_annotator/content/content_annotator/content_classifier.h"
 #include "components/history/core/browser/history_service.h"
 #include "components/optimization_guide/core/delivery/test_optimization_guide_model_provider.h"
 #include "components/optimization_guide/core/model_execution/test/mock_remote_model_executor.h"
@@ -31,10 +32,12 @@ class MockContentAnnotatorService : public ContentAnnotatorService {
       page_content_annotations::PageContentExtractionService&
           page_content_extraction_service,
       optimization_guide::RemoteModelExecutor&
-          optimization_guide_remote_model_executor)
+          optimization_guide_remote_model_executor,
+      std::unique_ptr<ContentClassifier> content_classifier)
       : ContentAnnotatorService(page_content_annotations_service,
                                 page_content_extraction_service,
-                                optimization_guide_remote_model_executor) {}
+                                optimization_guide_remote_model_executor,
+                                std::move(content_classifier)) {}
   ~MockContentAnnotatorService() override = default;
 
   MOCK_METHOD(void,
@@ -57,10 +60,15 @@ class ContentAnnotatorTabHelperTest : public ChromeRenderViewHostTestHarness {
             PageContentExtractionServiceFactory::GetForProfile(profile());
     ASSERT_TRUE(page_content_extraction_service);
 
+    std::unique_ptr<ContentClassifier> content_classifier_ =
+        ContentClassifier::Create();
+    ASSERT_TRUE(content_classifier_);
+
     mock_service_ =
         std::make_unique<testing::StrictMock<MockContentAnnotatorService>>(
             *page_content_annotations_service_,
-            *page_content_extraction_service, mock_remote_model_executor_);
+            *page_content_extraction_service, mock_remote_model_executor_,
+            std::move(content_classifier_));
 
     tab_interface_ = std::make_unique<tabs::MockTabInterface>();
     EXPECT_CALL(*tab_interface_, GetContents())
--- components/accessibility_annotator/content/content_annotator/content_annotator_service.h
+++ components/accessibility_annotator/content/content_annotator/content_annotator_service.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_ACCESSIBILITY_ANNOTATOR_CONTENT_CONTENT_ANNOTATOR_CONTENT_ANNOTATOR_SERVICE_H_
 #define COMPONENTS_ACCESSIBILITY_ANNOTATOR_CONTENT_CONTENT_ANNOTATOR_CONTENT_ANNOTATOR_SERVICE_H_
 
+#include <memory>
 #include <string>
 
 #include "base/containers/lru_cache.h"
@@ -13,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
 #include "base/sequence_checker.h"
-#include "components/accessibility_annotator/content/content_annotator/content_classifier.h"
+#include "components/accessibility_annotator/content/content_annotator/content_classifier_types.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/page_content_annotations/content/page_content_extraction_service.h"
 #include "components/page_content_annotations/core/page_content_annotations_service.h"
@@ -31,13 +32,15 @@ struct LanguageDetectionDetails;
 
 namespace accessibility_annotator {
 
+class ContentClassifier;
+
 class ContentAnnotatorService
     : public KeyedService,
       public page_content_annotations::PageContentAnnotationsService::
           PageContentAnnotationsObserver,
       public page_content_annotations::PageContentExtractionService::Observer {
  public:
-  explicit ContentAnnotatorService(
+  static std::unique_ptr<ContentAnnotatorService> Create(
       page_content_annotations::PageContentAnnotationsService&
           page_content_annotations_service,
       page_content_annotations::PageContentExtractionService&
@@ -71,6 +74,16 @@ class ContentAnnotatorService
       scoped_refptr<
           const page_content_annotations::RefCountedAnnotatedPageContent>
           page_content) override;
+
+ protected:
+  ContentAnnotatorService(
+      page_content_annotations::PageContentAnnotationsService&
+          page_content_annotations_service,
+      page_content_annotations::PageContentExtractionService&
+          page_content_extraction_service,
+      optimization_guide::RemoteModelExecutor&
+          optimization_guide_remote_model_executor,
+      std::unique_ptr<ContentClassifier> content_classifier);
 
  private:
   using CacheIterator =
@@ -118,6 +131,8 @@ class ContentAnnotatorService
       GUARDED_BY_CONTEXT(sequence_checker_);
 
   SEQUENCE_CHECKER(sequence_checker_);
+
+  std::unique_ptr<ContentClassifier> content_classifier_;
 
   base::WeakPtrFactory<ContentAnnotatorService> weak_ptr_factory_{this};
 };
