0761de5769221d65b58c52c5449544fed60029aa
Robbie McElrath <rmcelrath@chromium.org>
2026-02-19 13:54:09 -0800

    Add tflite fp16.h shim/override
    Some internal code is running into build issues due to duplicate
    fp16 function declarations. Both third_party/fp16 and
    tensorflow/lite/types/fp16.h define the same functions, but we want
    the third_party/fp16 version to be used. This adds an fp16.h file
    at //third_party/tflite/shims/tensorflow/lite/types/fp16.h that just
    includes the third_party/fp16 header. The "shims" folder is set up to
    be before "src" in the include path, so this file will override the
    fp16.h that ships with tflite.
    
    Bug: 475279711
    Change-Id: I28e732eb8ff8a45181919a5a5d52c9cba9f49632
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7590871
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1587400}

---
+++ third_party/tflite/shims/tensorflow/lite/types/fp16.h
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Prefer the third_party fp16 library over the fp16.h that's bundled with
+// tflite. The "shims" folder should be before "src" in the include path so
+// that this file will override tflite's copy.
+
+#include "third_party/fp16/src/include/fp16.h"
