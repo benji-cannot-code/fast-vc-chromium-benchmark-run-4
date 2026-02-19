6f9554efa114720353db4682c455c471b8945c1b
Kene Okoye <kenok@google.com>
2026-02-19 08:57:14 -0800

    Extract context fetch cases/helpers
    This change:
    1. Moves context fetch retry tests from c/b/actor/ to a new file in
       c/b/glic/actor/glic_actor_context_fetch_browsertest.cc
    2. Promotes ScopedMockTabObservationResult to a shared class within
       c/b/glic/actor/glic_actor_functional_browsertest.h/cc
    
    Bug: b:465188408 b:485256220
    Change-Id: I245f64ca10543dde81dbe40dffc3f8861d2c7141
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7590073
    Commit-Queue: Kene Okoye <kenok@google.com>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1587195}

--- chrome/browser/glic/actor/glic_actor_functional_browsertest.cc
+++ chrome/browser/glic/actor/glic_actor_functional_browsertest.cc
@@ -8,9 +8,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base64.h"
 #include "base/json/json_reader.h"
 #include "base/json/json_writer.h"
+#include "base/test/bind.h"
 #include "chrome/browser/actor/actor_features.h"
+#include "chrome/browser/actor/actor_proto_conversion.h"
 
 namespace glic::actor {
+
+ScopedMockTabObservationResult::ScopedMockTabObservationResult(
+    base::RepeatingCallback<void(TabObservation*,
+                                 const FetchPageContextResult&)> callback) {
+  ::actor::SetTabObservationResultOverrideForTesting(callback);
+}
+
+ScopedMockTabObservationResult::~ScopedMockTabObservationResult() {
+  ::actor::SetTabObservationResultOverrideForTesting(
+      base::RepeatingCallback<void(TabObservation*,
+                                   const FetchPageContextResult&)>());
+}
 
 Actions MakeWaitForTaskId(std::optional<base::TimeDelta> duration,
                           std::optional<tabs::TabHandle> observe_tab_handle,
