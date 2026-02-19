74638121c171f05aa07c9d0b0b50e9c3da1ad7a3
Anna Tsvirchkova <atsvirchkova@google.com>
2026-02-19 06:54:15 -0800

    Filter out grouped affiliations for actor login
    Weak affiliations are considered to be unsafe and the actor login feature doesn't support them. This CL is intended to prepare the Weak affiliations feature to launching.
    
    Bug: 485806845
    Change-Id: I14d17b3507fe5b3be04f2d7156d39f11904570b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7593793
    Reviewed-by: Ioana Treib <ioanap@chromium.org>
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1587083}

--- components/password_manager/core/browser/actor_login/internal/actor_login_password_credentials_fetcher.cc
+++ components/password_manager/core/browser/actor_login/internal/actor_login_password_credentials_fetcher.cc
@@ -121,6 +121,12 @@ std::vector<Credential> ConstructCredentialsList(
     bool immediately_available_to_login) {
   std::vector<Credential> result;
   for (const auto& form : best_matches) {
+    // Don't consider weakly affiliated (grouped) credentials because they are
+    // low confidence matches and would require additional user confirmation.
+    if (form.match_type.value() ==
+        password_manager::PasswordForm::MatchType::kGrouped) {
+      continue;
+    }
     if (form.actor_login_approved &&
         !password_manager_util::IsCredentialWeakMatch(form)) {
       return {PasswordFormToCredential(request_origin,
@@ -132,7 +138,6 @@ std::vector<Credential> ConstructCredentialsList(
 
   return result;
 }
-
 }  // namespace
 
 std::optional<ActorLoginError>
