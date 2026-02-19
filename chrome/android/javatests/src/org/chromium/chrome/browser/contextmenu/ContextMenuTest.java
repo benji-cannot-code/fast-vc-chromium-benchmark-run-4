5e60494992e4aad2e75cc1f7134ff3fd274e6a22
Xinyi Ji <xinyiji@google.com>
2026-02-18 16:04:00 -0800

    [gardening] Disable failing test ContextMenuTest#testCopyImage
    Bug: 443865720
    Change-Id: Iaed5af68a4584eb37d02528cd8ab840b0fb52659
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7589608
    Owners-Override: Xinyi Ji <xinyiji@chromium.org>
    Commit-Queue: Xinyi Ji <xinyiji@chromium.org>
    Reviewed-by: Yanling Hu <yanlinghu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1586751}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ContextMenuTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ContextMenuTest.java
@@ -1092,6 +1092,7 @@ public class ContextMenuTest {
     @SmallTest
     @Feature({"Browser", "ContextMenu"})
     @DisableIf.Device(DeviceFormFactor.DESKTOP) // https://crbug.com/481444053
+    @DisabledTest(message = "https://crbug.com/443865720")
     public void testCopyImage() throws Throwable {
         doAnswer(sCopyIsAllowedByPolicy)
                 .when(mDataProtectionBridgeMock)
