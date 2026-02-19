8bd3779f6f6d3eab84a37bac0d2b49978169eb6f
Keita Suzuki <suzukikeita@chromium.org>
2026-02-18 22:17:22 -0800

    Record Google Search HTTP status codes for incognito profiles.
    This change adds a new UMA histogram variant to track HTTP status codes
    for Google Search page loads specifically when the user is in an
    incognito profile. The existing
    PageLoad.Clients.GoogleSearch.HttpStatusCode histograms will now have an
    ".Incognito" suffix when recorded from an incognito session.
    
    Bug: 473593133
    Change-Id: I03b80deef54ac5aff851063dfdb52fcdb4b00a87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7586665
    Commit-Queue: Keita Suzuki <suzukikeita@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1586900}

--- components/page_load_metrics/google/browser/gws_page_load_metrics_observer.cc
+++ components/page_load_metrics/google/browser/gws_page_load_metrics_observer.cc
@@ -332,7 +332,9 @@ void ReportMetricForTraverseNavigation(bool is_restore_navigation,
   PAGE_LOAD_HISTOGRAM(restore_histogram_name, latency);
 }
 
-void RecordHttpStatusCode(int http_status_code, const GURL& url) {
+void RecordHttpStatusCode(int http_status_code,
+                          const GURL& url,
+                          bool is_incognito) {
   std::string suffix;
   if (page_load_metrics::IsGoogleSearchPrewarmUrl(url)) {
     suffix = internal::kHistogramGWSHttpStatusCodePrewarm;
@@ -345,6 +347,13 @@ void RecordHttpStatusCode(int http_status_code, const GURL& url) {
   base::UmaHistogramSparse(
       base::StrCat({internal::kHistogramGWSHttpStatusCode, suffix}),
       http_status_code);
+
+  if (is_incognito) {
+    base::UmaHistogramBoolean(
+        base::StrCat({internal::kHistogramGWSHttpStatusCode, suffix,
+                      internal::kHistogramIncognitoSuffix}),
+        http_status_code);
+  }
 }
 
 }  // namespace
@@ -399,7 +408,7 @@ GWSPageLoadMetricsObserver::OnRedirect(
     content::NavigationHandle* navigation_handle) {
   if (auto* response_headers = navigation_handle->GetResponseHeaders()) {
     RecordHttpStatusCode(response_headers->response_code(),
-                         navigation_handle->GetURL());
+                         navigation_handle->GetURL(), IsIncognitoProfile());
   }
 
   return CONTINUE_OBSERVING;
@@ -427,7 +436,7 @@ GWSPageLoadMetricsObserver::OnCommit(
   }
   if (auto* response_headers = navigation_handle->GetResponseHeaders()) {
     RecordHttpStatusCode(response_headers->response_code(),
-                         navigation_handle->GetURL());
+                         navigation_handle->GetURL(), IsIncognitoProfile());
   }
   if (!is_gws_url) {
     return STOP_OBSERVING;
