87c67eebfe4b77e3215ea2ed6044f27391fd95d5
Dominik Röttsches <drott@chromium.org>
2026-02-19 12:53:45 -0800

    Fix handling of chunked append for large XML/SVG files
    In the Rust based XML parser, large XML files were not handled
    correctly. The FrameLoader provides the source in 1MB increments. This
    failed for when we were in the middle of processing an attribute, for
    example the base64 data of an embedded image.
    
    To fix this, use the option of the Rust XML parser to ignore EOF, and
    resume stream processing when a new chunk arrives.
    
    This requires tracking a syntax error until the EndDocument event and
    potentially insert it.
    
    A cleaner fix would be an asynchronous stream processing method in the
    upstream library, filed as [1].
    
    Add a WPT test to test for an embedded large image. Add an internal test
    for the case of an unclosed tag after the 1MB image, to ensure, the
    error is not ignored.
    
    [1] https://github.com/kornelski/xml-rs/issues/71
    
    Bug: 484070274
    Change-Id: Id299ca6190c0baadf8af87f80db7690b7398be08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7584654
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1587361}

--- third_party/blink/renderer/core/xml/parser/xml_document_parser_rs.cc
+++ third_party/blink/renderer/core/xml/parser/xml_document_parser_rs.cc
@@ -256,7 +256,13 @@ void XMLDocumentParserRs::ProcessEvents() {
           OrdinalNumber::FromZeroBasedInt(static_cast<int>(row)),
           OrdinalNumber::FromZeroBasedInt(static_cast<int>(column)));
     }
-    HandleError(XMLErrors::kErrorTypeFatal, error_message.c_str(), position);
+    if (xml_ffi::is_error_resumable(*read_state_) && !finish_called_) {
+      carry_unbalanced_root_error_ = {String::FromUTF8(error_message.c_str()),
+                                      position};
+      xml_ffi::reset_error(*read_state_);
+    } else {
+      HandleError(XMLErrors::kErrorTypeFatal, error_message.c_str(), position);
+    }
   }
 }
 
@@ -605,6 +611,7 @@ void XMLDocumentParserRs::DocType(rust::Str name_rs,
 
 void XMLDocumentParserRs::EndDocument() {
   UpdateLeafTextNode();
+  carry_unbalanced_root_error_ = std::nullopt;
   bool xml_viewer_mode =
       !saw_error_ && !saw_css_ && HasNoStyleInformation(GetDocument());
   if (xml_viewer_mode) {
@@ -679,6 +686,12 @@ void XMLDocumentParserRs::Finish() {
     return;
   }
 
+  if (carry_unbalanced_root_error_) {
+    HandleError(XMLErrors::kErrorTypeFatal,
+                carry_unbalanced_root_error_->error_message.Utf8().c_str(),
+                carry_unbalanced_root_error_->position);
+  }
+
   if (parser_paused_) {
     finish_called_ = true;
   } else {
@@ -728,7 +741,7 @@ void XMLDocumentParserRs::Detach() {
 }
 
 bool XMLDocumentParserRs::WellFormed() const {
-  return !xml_ffi::saw_error(*read_state_);
+  return !xml_ffi::saw_error(*read_state_) && !carry_unbalanced_root_error_;
 }
 
 void XMLDocumentParserRs::InsertErrorMessageBlock() {
