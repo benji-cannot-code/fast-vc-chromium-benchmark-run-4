62f360868268faabad09cede73e695ac7150e888
Hidehiko Abe <hidehiko@chromium.org>
2026-02-18 20:01:26 -0800

    Migrate SettingsWindowManager in c/b/ui/webui/ash/*
    Migrate them into ash::SettingsAppManager.
    
    BUG=447287122
    TEST=Tryjob
    
    Change-Id: I44a5f342deb27ced9513467ba6040c460a8bd460
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7588813
    Reviewed-by: Jun Ishiguro <junis@google.com>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1586871}

--- chrome/browser/ui/webui/ash/borealis_installer/borealis_installer_page_handler.cc
+++ chrome/browser/ui/webui/ash/borealis_installer/borealis_installer_page_handler.cc
@@ -21,9 +21,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/borealis/borealis_types.mojom.h"
 #include "chrome/browser/ash/borealis/borealis_util.h"
 #include "chrome/browser/profiles/profile_manager.h"
-#include "chrome/browser/ui/settings_window_manager_chromeos.h"
 #include "chrome/browser/ui/views/borealis/borealis_splash_screen_view.h"
 #include "chrome/grit/generated_resources.h"
+#include "chromeos/ash/components/browser_context_helper/browser_context_helper.h"
+#include "chromeos/ash/experiences/settings_ui/settings_app_manager.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/l10n/time_format.h"
 #include "ui/strings/grit/ui_strings.h"
@@ -138,8 +139,15 @@ void BorealisInstallerPageHandler::OnInstallationEnded(
 }
 
 void BorealisInstallerPageHandler::OpenStoragePage() {
-  chrome::SettingsWindowManager::GetInstance()->ShowOSSettings(
-      profile_, chromeos::settings::mojom::kStorageSubpagePath);
+  auto* user =
+      ash::BrowserContextHelper::Get()->GetUserByBrowserContext(profile_.get());
+  if (!user) {
+    // TODO(crbug.com/447287122): Revisit here to make sure this is always
+    // user profile.
+    return;
+  }
+  ash::SettingsAppManager::Get()->Open(
+      *user, {.sub_page = chromeos::settings::mojom::kStorageSubpagePath});
 }
 
 void BorealisInstallerPageHandler::RequestClosePage() {
