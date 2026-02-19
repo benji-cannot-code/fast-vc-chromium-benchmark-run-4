8ef42f0a694251d905b50b24b7d59126a071bc70
Taiyo Mizuhashi <taiyo@chromium.org>
2026-02-19 12:42:16 -0800

    Prefetch: Decouple SpeculationTags Header Addition from PrefetchContainer
    As a series of work decoupling prefetch initial resource request
    creation from `PrefetchContainer` for OMT.Prefetch.
    
    Bug: 483079815, 452406598, 452389538
    Change-Id: I857bd89c0dc07c2cb726ce5629c36d0188e6e716
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7589114
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Commit-Queue: Taiyo Mizuhashi <taiyo@chromium.org>
    Auto-Submit: Taiyo Mizuhashi <taiyo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1587355}

--- content/browser/preloading/prefetch/prefetch_container.cc
+++ content/browser/preloading/prefetch/prefetch_container.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/preloading/prefetch/prefetch_network_context.h"
 #include "content/browser/preloading/prefetch/prefetch_params.h"
 #include "content/browser/preloading/prefetch/prefetch_request.h"
+#include "content/browser/preloading/prefetch/prefetch_resource_request_utils.h"
 #include "content/browser/preloading/prefetch/prefetch_response_reader.h"
 #include "content/browser/preloading/prefetch/prefetch_servable_state.h"
 #include "content/browser/preloading/prefetch/prefetch_serving_handle.h"
@@ -773,23 +774,6 @@ void PrefetchContainer::OnEligibilityCheckComplete(
   }
 }
 
-void PrefetchContainer::AddSpeculationTagsHeader(
-    const GURL& request_url,
-    net::HttpRequestHeaders& headers) const {
-  // Sec-Speculation-Tags is set only when the prefetch is triggered
-  // by speculation rules and it is not cross-site prefetch.
-  // To see more details:
-  // https://github.com/WICG/nav-speculation/blob/main/speculation-rules-tags.md#the-cross-site-case
-  if (request().speculation_rules_tags().has_value() &&
-      !IsCrossSiteRequest(url::Origin::Create(request_url))) {
-    std::optional<std::string> serialized_list =
-        request().speculation_rules_tags()->ConvertStringToHeaderString();
-    CHECK(serialized_list.has_value());
-    headers.SetHeader(blink::kSecSpeculationTagsHeaderName,
-                      serialized_list.value());
-  }
-}
-
 std::tuple<PrefetchUpdateHeadersParams, PrefetchUpdateHeadersParams>
 PrefetchContainer::PrepareUpdateHeaders(const GURL& url) const {
   // There are sometimes other headers that are modified during navigation
@@ -814,12 +798,14 @@ PrefetchContainer::PrepareUpdateHeaders(const GURL& url) const {
   // `Sec-Speculation-Tags`:
   updates_for_resource_request.removed_headers.push_back(
       blink::kSecSpeculationTagsHeaderName);
-  AddSpeculationTagsHeader(url, updates_for_resource_request.modified_headers);
+  AddSpeculationTagsHeader(updates_for_resource_request.modified_headers, url,
+                           request());
   if (base::FeatureList::IsEnabled(
           features::kPrefetchFixHeaderUpdatesOnRedirect)) {
     updates_for_follow_redirect.removed_headers.push_back(
         blink::kSecSpeculationTagsHeaderName);
-    AddSpeculationTagsHeader(url, updates_for_follow_redirect.modified_headers);
+    AddSpeculationTagsHeader(updates_for_follow_redirect.modified_headers, url,
+                             request());
   }
 
   // ------------------------------------------------------------------------
@@ -1662,7 +1648,7 @@ void PrefetchContainer::MakeInitialResourceRequest() {
 
   // ------------------------------------------------------------------------
   // [2] `Sec-Speculation-Tags`:
-  AddSpeculationTagsHeader(url, resource_request->headers);
+  AddSpeculationTagsHeader(resource_request->headers, url, request());
 
   // ------------------------------------------------------------------------
   // [2] `X-Client-Data`:
--- content/browser/preloading/prefetch/prefetch_resource_request_utils.h
+++ content/browser/preloading/prefetch/prefetch_resource_request_utils.h
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_BROWSER_PRELOADING_PREFETCH_PREFETCH_RESOURCE_REQUEST_UTILS_H_
 #define CONTENT_BROWSER_PRELOADING_PREFETCH_PREFETCH_RESOURCE_REQUEST_UTILS_H_
 
+#include "content/browser/preloading/prefetch/prefetch_request.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
+#include "services/network/public/cpp/resource_request.h"
 
 namespace content {
 
@@ -15,6 +17,15 @@ namespace content {
 // work (See crbug.com/484967082 for more details).
 extern const net::NetworkTrafficAnnotationTag
     kNavigationalPrefetchTrafficAnnotation;
+
+// Adds Speculation Rules Tags headers for a prefetch request to `request_url`
+// to `headers`.
+// Note that `request_url` and `prefetch_request.url` / `resource_request`
+// (that `request_headers` belongs)'s `url` can be different when called from
+// `PrefetchContainer::PrepareUpdateHeaders()`.
+void AddSpeculationTagsHeader(net::HttpRequestHeaders& request_headers,
+                              const GURL& request_url,
+                              const PrefetchRequest& prefetch_request);
 
 }  // namespace content
 
