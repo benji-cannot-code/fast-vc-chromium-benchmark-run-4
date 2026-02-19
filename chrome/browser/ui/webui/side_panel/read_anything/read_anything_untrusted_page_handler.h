894c21e639224801258fd74d1239597b90135286
Kristi Saney <kristislee@google.com>
2026-02-18 17:15:31 -0800

    [Reading mode] Ensure pdfs distill when opened with RM already open
    Bug: 485312481
    Change-Id: I98547e8923f8892f45692cbd27d63be1ac87bdd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7590524
    Reviewed-by: Jacobo Tapia <jatapiaro@google.com>
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Auto-Submit: Kristi Saney <kristislee@google.com>
    Commit-Queue: Jacobo Tapia <jatapiaro@google.com>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1586799}

--- chrome/browser/ui/webui/side_panel/read_anything/read_anything_untrusted_page_handler.h
+++ chrome/browser/ui/webui/side_panel/read_anything/read_anything_untrusted_page_handler.h
@@ -318,6 +318,7 @@ class ReadAnythingUntrustedPageHandler :
   void SetLanguageCode(const std::string& code);
 
   void SetUpPdfObserver();
+  void CheckIfActiveAXTreeChangedToPdf();
 
   void OnGetPresentationState();
   ReadAnythingController* GetReadAnythingController();
