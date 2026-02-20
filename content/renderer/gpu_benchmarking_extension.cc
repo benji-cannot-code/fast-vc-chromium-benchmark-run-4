71f3f7f274c34b812b1a404d279f0917c6a28903
Gyuyoung Kim <gyuyoung@igalia.com>
2026-02-19 18:18:24 -0800

    Fix tvOS build by adding nogncheck to printing headers
    The tvOS build has been failing since crrev.com/c/7585171, which added
    //printing dependencies conditionally based on enable_printing.
    
    While the code is correctly guarded by #if defined(ENABLE_PRINTING), gn
    check still fails on platforms like tvOS where printing is disabled
    because the headers are visible but their corresponding deps are
    missing.
    
    This CL adds // nogncheck to resolve the "Include not allowed" error.
    
    Bug: 391914246
    Change-Id: I95d85bb6ab31fbfee02a50aecbff5ff2dc825b00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7593174
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1587574}

--- content/renderer/gpu_benchmarking_extension.cc
+++ content/renderer/gpu_benchmarking_extension.cc
@@ -85,8 +85,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "v8/include/v8-primitive.h"
 
 #if defined(ENABLE_PRINTING)
-#include "printing/metafile_skia.h"
-#include "printing/print_settings.h"
+#include "printing/metafile_skia.h"   // nogncheck
+#include "printing/print_settings.h"  // nogncheck
 #endif
 
 #if BUILDFLAG(IS_WIN) && !defined(NDEBUG)
