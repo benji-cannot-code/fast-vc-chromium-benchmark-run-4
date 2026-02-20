a02a22c886797d8e97b9da9675812bf86da45d60
Wolfgang Beyer <wolfi@chromium.org>
2026-02-20 02:10:13 -0800

    [DevTools] Create Chrome-flag for DevTools durable messages
    Bug: 464173051
    Change-Id: I182b857120f7d2910325314f4b2a5bbf0317ea6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7588357
    Reviewed-by: Alex N. Jose <alexnj@chromium.org>
    Commit-Queue: Wolfgang Beyer <wolfi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1587697}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -12950,6 +12950,11 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kChromeFindsInternalsDescription, kOsAll,
      FEATURE_VALUE_TYPE(features::kChromeFindsInternals)},
 
+    {"devtools-enable-durable-messages",
+     flag_descriptions::kDevToolsEnableDurableMessagesName,
+     flag_descriptions::kDevToolsEnableDurableMessagesDescription, kOsAll,
+     FEATURE_VALUE_TYPE(features::kDevToolsEnableDurableMessages)},
+
     // Add new entries above this line.
     // NOTE: Adding a new flag requires adding a corresponding entry to enum
     // "LoginCustomFlags" in tools/metrics/histograms/enums.xml. See "Flag
