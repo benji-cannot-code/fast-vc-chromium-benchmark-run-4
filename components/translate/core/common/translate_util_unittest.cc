8a6b7be3bf6d3f3e69147b144958e176f0ecd2e5
Dylan Jew <dylanj@google.com>
2026-02-20 14:43:30 -0800

    Bundle fuzz test for translate_util into unit tests
    Fixes issue where the CommandLine was not initialized by moving the fuzz
    test into the chromium test launcher, where base::CommandLine::Init() is
    called.
    
    Bug: 485817462, 485817698
    Change-Id: I88dee34c6e64907a27f23c18c56abf236a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7591460
    Reviewed-by: Basia Zimirska <basiaz@google.com>
    Commit-Queue: Dylan Jew <dylanj@google.com>
    Reviewed-by: Alex Kallam <aakallam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1588119}

--- components/translate/core/common/translate_util_unittest.cc
+++ components/translate/core/common/translate_util_unittest.cc
@@ -5,9 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/translate/core/common/translate_util.h"
 
+#include <string>
+
 #include "base/command_line.h"
+#include "base/test/scoped_command_line.h"
 #include "components/translate/core/common/translate_switches.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/fuzztest/src/fuzztest/fuzztest.h"
 #include "url/gurl.h"
 
 namespace translate {
@@ -24,6 +28,16 @@ TEST(TranslateUtilTest, SecurityOrigin) {
   GURL modified_origin = GetTranslateSecurityOrigin();
   EXPECT_EQ(running_origin, modified_origin.spec());
 }
+
+void GetTranslateSecurityOriginDoesNotCrash(std::string_view fuzzed_origin) {
+  base::test::ScopedCommandLine scoped_command_line;
+  scoped_command_line.GetProcessCommandLine()->AppendSwitchASCII(
+      switches::kTranslateSecurityOrigin, fuzzed_origin);
+  GetTranslateSecurityOrigin();
+}
+
+FUZZ_TEST(TranslateUtilFuzzTest, GetTranslateSecurityOriginDoesNotCrash)
+    .WithDomains(fuzztest::PrintableAsciiString());
 
 }  // namespace
 }  // namespace translate
