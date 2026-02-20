28b4315e9be0bfd4cc5e977aac70f5297201ae86
Marlon Facey <mfacey@chromium.org>
2026-02-19 17:14:42 -0800

    [Omnibox Next] Check if the context menu exists before closing
    We were casting the ContextMenuButton to a ContextEntrypointAndMenu
    and then calling closeMenu() on it in the omnibox.
    
    Bug: 485649350
    Change-Id: I59e9fa8f9c8b04413e44bc6500936e983c0da85f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7595979
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Reviewed-by: Hannah Collins <hannahrogers@google.com>
    Cr-Commit-Position: refs/heads/main@{#1587536}

--- ui/webui/resources/cr_components/composebox/contextual_entrypoint_and_carousel.ts
+++ ui/webui/resources/cr_components/composebox/contextual_entrypoint_and_carousel.ts
@@ -385,6 +385,10 @@ export class ContextualEntrypointAndCarouselElement extends I18nMixinLit
   }
 
   closeMenu() {
+    if (!this.showMenuOnClick) {
+      return;
+    }
+
     const entrypointAndMenu =
         this.shadowRoot.querySelector<ContextualEntrypointAndMenuElement>(
             '#contextEntrypoint');
