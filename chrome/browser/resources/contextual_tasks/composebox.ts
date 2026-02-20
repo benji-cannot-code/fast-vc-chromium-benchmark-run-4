e67e2cba8d66ae18cf194d26178a0ec241999f9c
Juan Mojica <juanmojica@google.com>
2026-02-20 15:15:56 -0800

    [contextual-tasks] Add active state styling for the Lens icon in the composebox.
    Reflect the `isLensOverlayShowing` property as an attribute to allow CSS
    styling. Add a background color and border-radius to the Lens icon when
    the Lens overlay is active, using a new color variable.
    
    Dark mode: http://screen/5DQ6MHCJQ9PUWzL
    Light mode: http://screen/5am5knaF7NvUbNc
    
    Bug: 479322975
    Change-Id: I67a740525b18bead9a0432a3e2182926c73f8868
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7596479
    Reviewed-by: Bryan Nguyen <nguyenbryan@google.com>
    Commit-Queue: Bryan Nguyen <nguyenbryan@google.com>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Auto-Submit: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1588139}

--- chrome/browser/resources/contextual_tasks/composebox.ts
+++ chrome/browser/resources/contextual_tasks/composebox.ts
@@ -81,6 +81,7 @@ export class ContextualTasksComposeboxElement extends CrLitElement {
       },
       isLensOverlayShowing: {
         type: Boolean,
+        reflect: true,
       },
       composeboxHeight_: {type: Number},
       composeboxDropdownHeight_: {type: Number},
