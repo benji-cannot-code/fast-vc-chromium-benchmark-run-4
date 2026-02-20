cab6a2b57612b22ce1fadeabc4d7627acb9bcd72
Mikel Astiz <mastiz@chromium.org>
2026-02-20 04:14:21 -0800

    [STTS] Add logic to propagate form fields, behind a flag
    Guarded behind a feature that is disabled by default, this patch
    integrates the code submitted in recent patches to extract form fields
    when sending a tab, and autofilling them in the receiving device.
    
    In this patch, it is only implemented on desktop platforms. Follow-up
    patches will tackle the mobile counterparts.
    
    Design Doc (sorry, Googlers only): go/send-tab-to-self-forms
    
    Change-Id: I6ae31e6d679752c6a040334af822f3144b7da38d
    Bug: 485145029
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7594462
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1587731}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3727,6 +3727,12 @@ inline constexpr char kQuickShareV2Description[] =
     "visibility, removes the 'Selected Contacts' visibility, removes the Quick "
     "Share On/Off toggle.";
 
+inline constexpr char kSendTabToSelfEnhancedHandoffName[] =
+    "Send Tab To Self enhanced handoff";
+inline constexpr char kSendTabToSelfEnhancedHandoffDescription[] =
+    "Enables an enhanced version of Send Tab To Self that propagates more "
+    "information, such as form fields.";
+
 inline constexpr char kSendTabToSelfIOSPushNotificationsName[] =
     "Send tab to self iOS push notifications";
 inline constexpr char kSendTabToSelfIOSPushNotificationsDescription[] =
