0ca1fed474d2c1802b954eeab829beef74e8c561
Emily Shack <emshack@chromium.org>
2026-02-20 09:37:47 -0800

    [ComboButton] Animate button show/hide
    Animates the corner radius, width and icon opacity of the buttons inside
    TabStripComboButton.
    
    Bug: 481393131
    Change-Id: If1894e5f8c102ea530e666d6e059c94a552936a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7586433
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1587908}

--- chrome/browser/ui/views/tabs/shared/tab_strip_combo_button.h
+++ chrome/browser/ui/views/tabs/shared/tab_strip_combo_button.h
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_change_registrar.h"
 #include "ui/base/interaction/element_identifier.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
+#include "ui/gfx/animation/animation_delegate.h"
+#include "ui/gfx/animation/slide_animation.h"
 #include "ui/menus/simple_menu_model.h"
 #include "ui/views/context_menu_controller.h"
 #include "ui/views/layout/layout_types.h"
@@ -38,7 +40,8 @@ class TabStripFlatEdgeButton;
 class TabStripComboButton : public views::View,
                             public views::ContextMenuController,
                             public ui::SimpleMenuModel::Delegate,
-                            public TabSearchBubbleHostObserver {
+                            public TabSearchBubbleHostObserver,
+                            public gfx::AnimationDelegate {
   METADATA_HEADER(TabStripComboButton, views::View)
  public:
   DECLARE_CLASS_ELEMENT_IDENTIFIER_VALUE(kTabSearchUnpinMenuItem);
@@ -71,6 +74,10 @@ class TabStripComboButton : public views::View,
 
   void SetTabSearchBubbleHost(TabSearchBubbleHost* host);
 
+  // gfx::AnimationDelegate:
+  void AnimationProgressed(const gfx::Animation* animation) override;
+  void AnimationEnded(const gfx::Animation* animation) override;
+
  protected:
   // views::View:
   void ChildVisibilityChanged(views::View* child) override;
@@ -90,9 +97,16 @@ class TabStripComboButton : public views::View,
 
   void MaybeHideTabSearchButton();
 
+  actions::ActionItem* GetStartButtonActionItem();
+  actions::ActionItem* GetEndButtonActionItem();
+
   const raw_ptr<BrowserWindowInterface> browser_;
   raw_ptr<TabStripFlatEdgeButton> start_button_ = nullptr;
   raw_ptr<TabStripFlatEdgeButton> end_button_ = nullptr;
+
+  gfx::SlideAnimation start_button_animation_{this};
+  gfx::SlideAnimation end_button_animation_{this};
+
   views::LayoutOrientation orientation_ = views::LayoutOrientation::kHorizontal;
 
   bool show_tab_search_ephemerally_ = false;
