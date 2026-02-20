741d7793a97b99266ebb686c063dc37c3618e106
Javier Contreras Tenorio <javiercon@microsoft.com>
2026-02-20 10:38:18 -0800

    [gap-decorations] Implement *-rule-inset-start/end shorthands
    Add six new gap decorations shorthand properties:
    column-rule-inset-start, column-rule-inset-end, row-rule-inset-start,
    row-rule-inset-end, rule-inset-start, and rule-inset-end. Each accepts a
    single <length-percentage> value that sets both the edge and interior
    inset longhands simultaneously, per the spec
    https://drafts.csswg.org/css-gaps-1/#propdef-column-rule-inset-start
    
    Also increases the inline capacity of the matching-shorthands vector
    from 4 to 6, since inset longhands now participate in up to 6
    shorthands. Adds WPT tests for parsing, computed style, valid values,
    invalid values, and shorthand expansion.
    
    Bug: 357648037, 485672644
    Change-Id: I5f7189351e200e34a6fb2ceb3f01f1d76d550509
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7591773
    Commit-Queue: Javier Contreras <javiercon@microsoft.com>
    Reviewed-by: Kevin Babbitt <kbabbitt@microsoft.com>
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1587952}

--- third_party/blink/renderer/core/css/style_property_serializer.h
+++ third_party/blink/renderer/core/css/style_property_serializer.h
@@ -71,6 +71,8 @@ class CORE_EXPORT StylePropertySerializer {
       const StylePropertyShorthand&) const;
   String GetShorthandValueForBidirectionalGapRuleEdgeInteriorInset(
       const StylePropertyShorthand&) const;
+  String GetShorthandValueForBidirectionalGapRuleInsetStartEnd(
+      const StylePropertyShorthand&) const;
   String GetShorthandValueForBidirectionalGapRules(
       const StylePropertyShorthand&) const;
   String GetShorthandValueForGapDecorationsRule(
@@ -83,6 +85,8 @@ class CORE_EXPORT StylePropertySerializer {
       const StylePropertyShorthand&,
       CSSGapDecorationPropertyDirection direction,
       bool is_edge) const;
+  String GetShorthandValueForGapDecorationsRuleInsetStartEnd(
+      const StylePropertyShorthand&) const;
   String GetShorthandValueForColumnRule(const StylePropertyShorthand&) const;
   String GetShorthandValueForColumns(const StylePropertyShorthand&) const;
   // foo || bar || ... || baz
