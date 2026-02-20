edc73574de73eba144f0349706204c93f821e02e
Duncan Mercer <mercerd@google.com>
2026-02-20 13:11:21 -0800

    [Contextual Tasks] Fix composebox missing on Lens results
    To provide a most robust experience, we need to check if the URL we are
    going to is going to be an AIM url, not the AIM we came from. This is
    only possible using the isAiPage mojom call, which is async, and
    therefore cannot be used in `onBeforeRequest` which is a sync call. To
    fix this, switch to using 'loadstart' and 'loadstop'.
    
    Note: also does some refactoring now that the ghost loader isn't the only user of the thread frame events.
    Fixed: b:485906405
    Change-Id: I8dada55ab6bc3a68bdc734a9ad39cdd1d140bf77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7597759
    Reviewed-by: Bryan Nguyen <nguyenbryan@google.com>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1588021}

--- chrome/test/data/webui/contextual_tasks/test_contextual_tasks_browser_proxy.ts
+++ chrome/test/data/webui/contextual_tasks/test_contextual_tasks_browser_proxy.ts
@@ -146,6 +146,7 @@ class TestContextualTasksPageHandler extends TestBrowserProxy implements
   private url_: Url;
   private isInTab_: boolean = true;
   private page_: MockPage;
+  private isAiPageResult_: boolean = false;
 
   constructor(url: string, page: MockPage) {
     super([
@@ -223,9 +224,13 @@ class TestContextualTasksPageHandler extends TestBrowserProxy implements
     return Promise.resolve({isZeroState: false});
   }
 
+  setIsAiPage(isAiPage: boolean) {
+    this.isAiPageResult_ = isAiPage;
+  }
+
   isAiPage(url: Url) {
     this.methodCalled('isAiPage', url);
-    return Promise.resolve({isAiPage: false});
+    return Promise.resolve({isAiPage: this.isAiPageResult_});
   }
 
   openMyActivityUi() {
