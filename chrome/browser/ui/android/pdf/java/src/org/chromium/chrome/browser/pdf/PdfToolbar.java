5a9c539604c089fe4d8d1b41488cd4424c526b31
Ryan Thomas <rathomas@google.com>
2026-02-20 11:21:35 -0800

    Adds a top toolbar for inline pdf v2.
    This CL introduce the top toolbar for pdfs guarded behind the inlinePdfV2 feature flag.
    
    Flag enabled: https://screenshot.googleplex.com/Au78kWcMSZoWD5s
    Flag disabled: https://screenshot.googleplex.com/8C6o5EJCYRyPGiW
    
    Bug: 484045966
    Change-Id: I4330865b38b0f2efd890001a220ce63acb67600a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7594975
    Commit-Queue: Ryan Thomas <rathomas@google.com>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Reviewed-by: Shu Yang <shuyng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1587982}

---
+++ chrome/browser/ui/android/pdf/java/src/org/chromium/chrome/browser/pdf/PdfToolbar.java
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.pdf;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.Toolbar;
+
+import org.chromium.build.annotations.Nullable;
+import org.chromium.build.annotations.NullMarked;
+
+/**
+ * Toolbar for the PDF viewer. To handle clicks on the navigation icon, set a listener with
+ * {@link #setNavigationOnClickListener(OnClickListener)}.
+ */
+@NullMarked
+public class PdfToolbar extends Toolbar {
+    public PdfToolbar(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
