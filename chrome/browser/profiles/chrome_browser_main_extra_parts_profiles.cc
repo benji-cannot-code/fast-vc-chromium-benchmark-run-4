7eb4ba5f0a0397b903828414fe6eff463543c4d6
Khanh Nguyen <khanhtmn@google.com>
2026-02-19 17:17:44 -0800

    [contextual tasks] Compiles `ContextualTasksUiService`, and `ContextualTasksUiServiceFactory` on Android.
    For items/components that are not available on Android, we guard them
    with `ifdef` to unblock the compilation for now. We will address these
    items/components in crbug.com/483442073.
    
    Bug: b:483119674
    Change-Id: I3d769c9461c90c0a04511d6b90bc0c3219956ecd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7572638
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Auto-Submit: Khanh Nguyen <khanhtmn@google.com>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Khanh Nguyen <khanhtmn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1587539}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -62,6 +62,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/contextual_cueing/contextual_cueing_service_factory.h"
 #include "chrome/browser/contextual_search/contextual_search_service_factory.h"
 #include "chrome/browser/contextual_tasks/contextual_tasks_service_factory.h"
+#include "chrome/browser/contextual_tasks/contextual_tasks_ui_service_factory.h"
 #include "chrome/browser/custom_handlers/protocol_handler_registry_factory.h"
 #include "chrome/browser/data_sharing/data_sharing_service_factory.h"
 #include "chrome/browser/data_sharing/personal_collaboration_data/personal_collaboration_data_service_factory.h"
@@ -440,7 +441,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if !BUILDFLAG(IS_ANDROID)
-#include "chrome/browser/contextual_tasks/contextual_tasks_ui_service_factory.h"
 #include "chrome/browser/password_manager/startup_passwords_import_service_factory.h"  // nogncheck (Desktop only)
 #include "chrome/browser/webauthn/passkey_unlock_manager_factory.h"
 #include "device/fido/public/features.h"
