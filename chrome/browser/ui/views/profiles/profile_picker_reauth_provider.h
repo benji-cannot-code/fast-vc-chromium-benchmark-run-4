f849bd1ee7df8746be61f22cb4daef73369f806d
Ryan Sultanem <rsult@google.com>
2026-02-20 07:52:53 -0800

    [ForceSignin] Fix reauth flow by supporting webauthn dialogs
    Aligns WebContentsUserData created in ProfilePickerSignInProvider to
    also be created in ProfilePickerReauthProvider.
    Some were wrongly missing when the reauth step was added, leading
    to authentication method to fail in the reauth step, but work in
    the initial sign in step.
    
    Bug: 481754598
    Change-Id: I8b27d0992f02256ec577000ba02eb15babebe7f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7594774
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1587833}

--- chrome/browser/ui/views/profiles/profile_picker_reauth_provider.h
+++ chrome/browser/ui/views/profiles/profile_picker_reauth_provider.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_observation.h"
 #include "base/timer/timer.h"
 #include "chrome/browser/profiles/keep_alive/scoped_profile_keep_alive.h"
+#include "chrome/browser/ui/chrome_web_modal_dialog_manager_delegate.h"
 #include "chrome/browser/ui/views/profiles/profile_management_types.h"
 #include "chrome/browser/ui/webui/signin/signin_ui_error.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
@@ -51,7 +52,8 @@ enum class ProfilePickerReauthResult {
 // last step (checking if the account that got reauthed matches with the
 // original one).
 class ProfilePickerReauthProvider : public signin::IdentityManager::Observer,
-                                    public content::WebContentsObserver {
+                                    public content::WebContentsObserver,
+                                    public ChromeWebModalDialogManagerDelegate {
  public:
   ProfilePickerReauthProvider(
       ProfilePickerWebContentsHost* host,
@@ -79,6 +81,10 @@ class ProfilePickerReauthProvider : public signin::IdentityManager::Observer,
   // content::WebContentsObserver:
   void DidFinishNavigation(
       content::NavigationHandle* navigation_handle) override;
+
+  // ChromeWebModalDialogManagerDelegate:
+  web_modal::WebContentsModalDialogHost* GetWebContentsModalDialogHost(
+      content::WebContents* web_contents) override;
 
  private:
   // Attempt to create the ForceSigninVerifier, refresh tokens should be loaded.
