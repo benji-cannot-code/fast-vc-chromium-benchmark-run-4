0295010f05ac2f499e23c39c3a1ad4751f775241
Bryan Enrique Gonzalez Velez <bryanenriquegv@google.com>
2026-02-20 13:37:00 -0800

    spanification: migrate base::ReadUnicodeCharacter usage to string_view
    In url::ReadUtfCharLossy, we migrated the use of
    base::ReadUnicodeCharacter to the new version that accepts a string_view
    instead of passing a raw pointer and a size parameter.
    
    BUG=439964610
    BUG=40284755
    
    Change-Id: Ic74c913e7ec5ff1946b13eaf0b108efeb287a7d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7582477
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Bryan Enrique Gonzalez <bryanenriquegv@google.com>
    Cr-Commit-Position: refs/heads/main@{#1588064}

--- url/url_canon_internal.cc
+++ url/url_canon_internal.cc
@@ -205,8 +205,7 @@ void AppendStringOfType(std::u16string_view source,
 bool ReadUtfCharLossy(std::string_view str,
                       size_t* begin,
                       base_icu::UChar32* code_point_out) {
-  if (!base::ReadUnicodeCharacter(str.data(), str.length(), begin,
-                                  code_point_out)) {
+  if (!base::ReadUnicodeCharacter(str, begin, code_point_out)) {
     *code_point_out = kUnicodeReplacementCharacter;
     return false;
   }
@@ -216,8 +215,7 @@ bool ReadUtfCharLossy(std::string_view str,
 bool ReadUtfCharLossy(std::u16string_view str,
                       size_t* begin,
                       base_icu::UChar32* code_point_out) {
-  if (!base::ReadUnicodeCharacter(str.data(), str.length(), begin,
-                                  code_point_out)) {
+  if (!base::ReadUnicodeCharacter(str, begin, code_point_out)) {
     *code_point_out = kUnicodeReplacementCharacter;
     return false;
   }
