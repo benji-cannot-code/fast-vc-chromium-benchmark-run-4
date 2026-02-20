412115220afb10fee59c76ce9760af8699d150c7
Andrew Grieve <agrieve@chromium.org>
2026-02-20 13:23:52 -0800

    Remove ShadowPostTask in favor of the default PausedExecutor (part 1)
    Bug: 341267427
    Change-Id: I3566fb0aefe9f410dbdd5026a05951268c211236
    Cq-Include-Trybots: luci.chrome.try:android-internal-dbg,android-internal-binary-size
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7595132
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1588042}

--- chrome/browser/android/customtabs/branding/java/src/org/chromium/chrome/browser/customtabs/features/branding/BrandingControllerUnitTest.java
+++ chrome/browser/android/customtabs/branding/java/src/org/chromium/chrome/browser/customtabs/features/branding/BrandingControllerUnitTest.java
@@ -21,8 +21,6 @@ import static org.chromium.chrome.browser.customtabs.features.branding.BrandingC
 import static org.chromium.chrome.browser.customtabs.features.branding.BrandingController.MAX_BLANK_TOOLBAR_TIMEOUT_MS;
 
 import android.content.Context;
-import android.os.Handler;
-import android.os.Looper;
 import android.os.SystemClock;
 
 import androidx.appcompat.view.ContextThemeWrapper;
@@ -42,7 +40,6 @@ import org.robolectric.Shadows;
 import org.robolectric.annotation.Config;
 import org.robolectric.annotation.LooperMode;
 import org.robolectric.annotation.LooperMode.Mode;
-import org.robolectric.shadows.ShadowLooper;
 import org.robolectric.shadows.ShadowSystemClock;
 import org.robolectric.shadows.ShadowToast;
 
@@ -50,9 +47,8 @@ import org.chromium.base.Callback;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.FakeTimeTestRule;
 import org.chromium.base.TimeUtils;
-import org.chromium.base.task.TaskTraits;
-import org.chromium.base.task.test.ShadowPostTask;
 import org.chromium.base.test.BaseRobolectricTestRunner;
+import org.chromium.base.test.RobolectricUtil;
 import org.chromium.chrome.browser.customtabs.features.branding.proto.AccountMismatchData.CloseType;
 import org.chromium.ui.widget.Toast;
 import org.chromium.ui.widget.ToastManager;
@@ -63,7 +59,7 @@ import java.util.concurrent.TimeUnit;
 @RunWith(BaseRobolectricTestRunner.class)
 @Config(
         manifest = Config.NONE,
-        shadows = {ShadowSystemClock.class, ShadowPostTask.class, ShadowToast.class})
+        shadows = {ShadowSystemClock.class, ShadowToast.class})
 @LooperMode(Mode.PAUSED)
 public class BrandingControllerUnitTest {
     @Rule public MockitoRule mTestRule = MockitoJUnit.rule();
@@ -73,22 +69,9 @@ public class BrandingControllerUnitTest {
     @Mock MismatchNotificationChecker mMismatchNotificationChecker;
     @Captor ArgumentCaptor<Callback<MismatchNotificationData>> mCloseCallbackCaptor;
     private BrandingController mBrandingController;
-    private ShadowPostTask.TestImpl mShadowPostTaskImpl;
 
     @Before
     public void setup() {
-        mShadowPostTaskImpl =
-                new ShadowPostTask.TestImpl() {
-                    final Handler mHandler = new Handler(Looper.getMainLooper());
-
-                    @Override
-                    public void postDelayedTask(
-                            @TaskTraits int taskTraits, Runnable task, long delay) {
-                        mHandler.postDelayed(task, delay);
-                    }
-                };
-        ShadowPostTask.setTestImpl(mShadowPostTaskImpl);
-
         SystemClock.setCurrentTimeMillis(TimeUtils.currentTimeMillis());
     }
 
@@ -281,7 +264,7 @@ public class BrandingControllerUnitTest {
         storage.put("stubPackageA", 1L);
         storage.put("stubPackageB", 1L);
         storage.put("stubPackageC", 1L);
-        ShadowLooper.idleMainLooper();
+        RobolectricUtil.runAllBackgroundAndUi();
         assertEquals("3 Stub package name should be in the storage.", 3, storage.getSize());
 
         new BrandingCheckTester()
@@ -370,7 +353,7 @@ public class BrandingControllerUnitTest {
         }
 
         public BrandingCheckTester idleMainLooper() {
-            Shadows.shadowOf(Looper.getMainLooper()).idle();
+            RobolectricUtil.runAllBackgroundAndUi();
             return this;
         }
 
