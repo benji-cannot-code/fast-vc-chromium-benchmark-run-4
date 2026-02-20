c6fbc8107a19844f7c88e40416d45842bb18860b
Keigo Oka <oka@google.com>
2026-02-20 14:42:33 -0800

    site search: Relay keyword changes from delegate to AutocompleteInput
    This updates the OmniboxActionDelegate so that it relays the keyword
    changes to the AutocompleteInput instance that is currently live.
    
    Bug: 459590224
    Change-Id: I5a9c9d3829ab0042bcd0f9912861155522d0a168
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7594237
    Auto-Submit: Keigo Oka <oka@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1588118}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteMediator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteMediator.java
@@ -540,6 +540,7 @@ class AutocompleteMediator
             mAutocompleteInput.getKeywordSupplier().removeObserver(mOnKeywordChanged);
         }
         mAutocompleteInput = input;
+        mOmniboxActionDelegate.setAutocompleteInput(input);
         if (mAutocompleteInput != null) {
             mAutocompleteInput
                     .getRequestTypeSupplier()
