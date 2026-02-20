c3816364f2fa2f80913434f89cd036b5b4c8fd76
Achuith Bhandarkar <achuith@chromium.org>
2026-02-19 17:28:44 -0800

    Migrate VideoCaptureObserver to base::CheckedObserver.
    This change makes media::VideoCaptureObserver inherit from
    base::CheckedObserver. This allows base::ObserverList instances holding
    VideoCaptureObserver to use the default base::ObserverList type,
    removing the need for Unchecked or UncheckedAndDanglingUntriaged
    specializations and improving observer safety.
    
    Bug: 470679673
    Change-Id: I1144d6822f2897c0b1d58f55b9c9e61f16fb21b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7575521
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1587548}

--- chromeos/ash/components/audio/cras_audio_handler_unittest.cc
+++ chromeos/ash/components/audio/cras_audio_handler_unittest.cc
@@ -437,7 +437,7 @@ class FakeVideoCaptureManager {
   }
 
  private:
-  base::ObserverList<media::VideoCaptureObserver>::Unchecked observers_;
+  base::ObserverList<media::VideoCaptureObserver> observers_;
 };
 
 }  // namespace
