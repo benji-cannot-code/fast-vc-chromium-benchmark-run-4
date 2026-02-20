171412d2e0da56b5e62f471cf927a59cd522c479
Thomas Thrainer <thomasth@google.com>
2026-02-20 06:09:35 -0800

    Add a feature flag for in-flow Trusted Vault key retrieval on iOS
    This change introduces the "InFlowTrustedVaultKeyRetrievalIos"
    feature flag, which controls whether the key retrieval flow is started
    after offering to save a password. The flag is added to about:flags,
    with metadata and descriptions, and is initially disabled.
    
    See https://crrev.com/c/7562995 for the corresponding CL for Android.
    
    Design: go/gpm-cross-platform-ui-errors
    
    Bug: 464228247
    Change-Id: I82ad08a8c9c19675945f00a88ac6fe52ea91cd93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7594680
    Reviewed-by: Ioana Treib <ioanap@chromium.org>
    Commit-Queue: Thomas Thrainer <thomasth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1587775}

--- ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
+++ ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
@@ -547,6 +547,9 @@ extern const char kIOSWebContextMenuNewTitleDescription[];
 extern const char kIdentityConfirmationSnackbarName[];
 extern const char kIdentityConfirmationSnackbarDescription[];
 
+extern const char kInFlowTrustedVaultKeyRetrievalIosName[];
+extern const char kInFlowTrustedVaultKeyRetrievalIosDescription[];
+
 extern const char kInProductHelpDemoModeName[];
 extern const char kInProductHelpDemoModeDescription[];
 
