d241cb16ad764702ebc45d88faf62b877eaaec45
Mitsuru Oshima <oshima@chromium.org>
2026-02-20 13:19:39 -0800

    Remove DanglingUntriaged from data_device_unittest.cc
    This CL removes DanglingUntriaged from surface_ in TestSeat and updates DataDeviceTest::TearDown to clear the focused surface pointer before the surface is destroyed.
    
    Bug: 40212619
    Change-Id: I23d42d82c9218a748955d24c019ae89565b10da5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7597675
    Auto-Submit: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Reviewed-by: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1588035}

--- components/exo/data_device_unittest.cc
+++ components/exo/data_device_unittest.cc
@@ -51,7 +51,7 @@ class TestSeat : public Seat {
   Surface* GetFocusedSurface() override { return surface_; }
 
  private:
-  raw_ptr<Surface, DanglingUntriaged> surface_ = nullptr;
+  raw_ptr<Surface> surface_ = nullptr;
 };
 
 class DataDeviceTest : public test::ExoTestBase {
@@ -65,6 +65,7 @@ class DataDeviceTest : public test::ExoTestBase {
   }
 
   void TearDown() override {
+    seat_->set_focused_surface(nullptr);
     surface_.reset();
     device_.reset();
     seat_.reset();
