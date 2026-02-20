21e5b5fc14c0f9968473766d4ba051d0a309d3f3
Matt Jones <mdjones@chromium.org>
2026-02-20 10:27:00 -0800

    [Contextual Tasks] Don't use language in common search params
    This patch updates AppendCommonSearchParametersToURL to accept the
    language param as an optional. If nullopt, the param will not be
    added to the URL. A corresponding util to check if the params exist
    has also been added.
    
    Bug: 483340750
    Change-Id: I7e267a838ca246ef75fb9ba6678fc29a8a52a6d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7594277
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1587940}

--- chrome/browser/contextual_tasks/contextual_tasks_page_handler_unittest.cc
+++ chrome/browser/contextual_tasks/contextual_tasks_page_handler_unittest.cc
@@ -630,7 +630,6 @@ TEST_F(ContextualTasksPageHandlerTest, GetCommonSearchParams) {
         /*is_dark_mode=*/false, /*is_side_panel=*/true,
         base::BindLambdaForTesting(
             [&](const base::flat_map<std::string, std::string>& params) {
-              EXPECT_EQ(params.at(lens::kLanguageCodeParameterKey), "en-US");
               EXPECT_EQ(params.at(lens::kDarkModeParameterKey),
                         lens::kDarkModeParameterLightValue);
               EXPECT_EQ(params.at(lens::kChromeSidePanelParameterKey), "2");
@@ -646,7 +645,6 @@ TEST_F(ContextualTasksPageHandlerTest, GetCommonSearchParams) {
         /*is_dark_mode=*/true, /*is_side_panel=*/false,
         base::BindLambdaForTesting(
             [&](const base::flat_map<std::string, std::string>& params) {
-              EXPECT_EQ(params.at(lens::kLanguageCodeParameterKey), "en-US");
               EXPECT_EQ(params.at(lens::kDarkModeParameterKey),
                         lens::kDarkModeParameterDarkValue);
               EXPECT_EQ(params.at(lens::kChromeSidePanelParameterKey), "");
@@ -666,7 +664,6 @@ TEST_F(ContextualTasksPageHandlerTest, GetCommonSearchParams) {
         /*is_dark_mode=*/false, /*is_side_panel=*/true,
         base::BindLambdaForTesting(
             [&](const base::flat_map<std::string, std::string>& params) {
-              EXPECT_EQ(params.at(lens::kLanguageCodeParameterKey), "US");
               EXPECT_EQ(params.at("gl"), "us");
               run_loop.Quit();
             }));
