82442c5e5129abdc11bf7d24f19c00193e655056
Matt Jones <mdjones@chromium.org>
2026-02-20 07:03:23 -0800

    [Contextual Tasks] Enable expand button by default
    Bug: 482212064
    Change-Id: Ie271b77ab05f2721fb474568945950eab39a6137
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7594433
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1587804}

--- chrome/test/data/webui/contextual_tasks/contextual_tasks_pixel_interactive_ui_test.cc
+++ chrome/test/data/webui/contextual_tasks/contextual_tasks_pixel_interactive_ui_test.cc
@@ -42,7 +42,7 @@ class ContextualTasksPixelTestBase : public WebUIComposeBoxPixelTest {
     feature_list_.InitWithFeatures(
         {contextual_tasks::kContextualTasks,
          contextual_tasks::kContextualTasksForceEntryPointEligibility},
-        {});
+        {contextual_tasks::kContextualTasksExpandButton});
     WebUIComposeBoxPixelTest::SetUp();
   }
 
