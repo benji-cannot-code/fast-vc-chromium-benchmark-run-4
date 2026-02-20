7ea574eb132a6baed218f7d110b58525429596a0
Joe Downing <joedow@google.com>
2026-02-20 11:00:14 -0800

    Roll src/remoting/internal/ 695e9f5d3..324177e62 (1 commit)
    https://chrome-internal.googlesource.com/chrome/remoting/internal.git/+log/695e9f5d398f..324177e62810
    
    Created with:
      roll-dep src/remoting/internal
    
    Includes the minimal set of changes needed allow the src-internal
    DEPS roll w/o breaking the build.
    
    Bug: 359620500
    Change-Id: I03db8f32b4ce55d90c0120cdcd5c3edcac1aeaa6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7595031
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1587967}

--- remoting/proto/messaging_service.h
+++ remoting/proto/messaging_service.h
@@ -84,6 +84,12 @@ struct ShareSessionTokenStruct {
   std::string messaging_authz_token;
 };
 
+struct OidcStruct {
+  std::string redirect_uri;
+  std::string code;
+  std::string state;
+};
+
 struct SystemTestStruct {
   SystemTestStruct();
   SystemTestStruct(const SystemTestStruct&);
@@ -92,6 +98,7 @@ struct SystemTestStruct {
 
   std::variant<BurstStruct,
                EncryptedStruct,
+               OidcStruct,
                PingPongStruct,
                SimpleStruct,
                ShareSessionTokenStruct>
