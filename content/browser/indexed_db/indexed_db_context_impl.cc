d594424cba8c7715e71c86d8cd4d47c355f28309
Evan Stade <evanstade@microsoft.com>
2026-02-19 23:56:14 -0800

    IDB: add note explaining why PerformStorageCleanup is no-op
    Bug: none
    Change-Id: I670d941d6e671b1401833d287fc3cda41242f694
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7591204
    Reviewed-by: Abhishek Shanthkumar <abhishek.shanthkumar@microsoft.com>
    Auto-Submit: Evan Stade <evanstade@microsoft.com>
    Reviewed-by: Evan Stade <evanstade@microsoft.com>
    Commit-Queue: Abhishek Shanthkumar <abhishek.shanthkumar@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1587653}

--- content/browser/indexed_db/indexed_db_context_impl.cc
+++ content/browser/indexed_db/indexed_db_context_impl.cc
@@ -1275,6 +1275,10 @@ void IndexedDBContextImpl::GetDefaultStorageKeys(
 
 void IndexedDBContextImpl::PerformStorageCleanup(
     PerformStorageCleanupCallback callback) {
+  // IndexedDB doesn't need to do anything because all traces of data are
+  // already removed when a bucket is deleted. This hook exists for databases
+  // like LocalStorage where data across many origins are stored in a single
+  // backing database.
   std::move(callback).Run();
 }
 
