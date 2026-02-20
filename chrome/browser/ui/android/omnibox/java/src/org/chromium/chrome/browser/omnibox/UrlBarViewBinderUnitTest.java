fc9dcbfefd127858a411939ff1bcf382b9fa8582
Peter Wen <wnwen@chromium.org>
2026-02-20 14:44:40 -0800

    android: Fix UrlBarViewBinderUnitTest on SDK 36
    When running on SDK 30+, Robolectric's default "Touch Mode" may be
    enabled. In touch mode, a view that is only setFocusable(true) cannot
    take focus. It must also have setFocusableInTouchMode(true). This CL
    adds setFocusableInTouchMode(true) to the UrlBar in
    testSetSelectAllOnFocus, enabling the view to successfully acquire focus
    and removing the need to restrict the test to SDK 29.
    
    Bug: 481749859
    Change-Id: Ieeb23f58200aa8db48afd50d80ddf9bd8bb4b175
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7597190
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Auto-Submit: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1588121}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/UrlBarViewBinderUnitTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/UrlBarViewBinderUnitTest.java
@@ -33,7 +33,6 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.robolectric.Robolectric;
-import org.robolectric.annotation.Config;
 
 import org.chromium.base.Callback;
 import org.chromium.base.ContextUtils;
@@ -83,9 +82,6 @@ public class UrlBarViewBinderUnitTest {
         Assert.assertEquals(newExpectColor, mUrlBar.getHintTextColors().getDefaultColor());
     }
 
-    // TODO(crbug.com/481749859): Fix failure on SDK 30+ due to focus behavior changes in
-    // Robolectric.
-    @Config(sdk = 29)
     @Test
     @SmallTest
     @DisableFeatures(OmniboxFeatureList.MULTILINE_EDIT_FIELD)
@@ -96,9 +92,6 @@ public class UrlBarViewBinderUnitTest {
                 /* expectSelection= */ true);
     }
 
-    // TODO(crbug.com/481749859): Fix failure on SDK 30+ due to focus behavior changes in
-    // Robolectric.
-    @Config(sdk = 29)
     @Test
     @SmallTest
     public void testSetSelectAllOnFocus_whileFocused() {
@@ -108,9 +101,6 @@ public class UrlBarViewBinderUnitTest {
                 /* expectSelection= */ false);
     }
 
-    // TODO(crbug.com/481749859): Fix failure on SDK 30+ due to focus behavior changes in
-    // Robolectric.
-    @Config(sdk = 29)
     @Test
     @SmallTest
     public void testUnsetSelectAllOnFocus() {
@@ -120,9 +110,6 @@ public class UrlBarViewBinderUnitTest {
                 /* expectSelection= */ false);
     }
 
-    // TODO(crbug.com/481749859): Fix failure on SDK 30+ due to focus behavior changes in
-    // Robolectric.
-    @Config(sdk = 29)
     @Test
     @SmallTest
     public void testUnsetSelectAllOnFocus_whileFocused() {
@@ -137,6 +124,7 @@ public class UrlBarViewBinderUnitTest {
         String text = "test";
         mUrlBar.setText(text);
         mUrlBar.setFocusable(true);
+        mUrlBar.setFocusableInTouchMode(true);
         Assert.assertFalse(mUrlBar.isFocused());
         Assert.assertFalse(mUrlBar.hasSelection());
 
