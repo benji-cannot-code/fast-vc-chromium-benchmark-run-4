a7171cad4cc8f2749e5c3ce4330125978ffb7bce
Mikel Astiz <mastiz@chromium.org>
2026-02-20 03:00:06 -0800

    [STTS] Add integration test for field filling
    The actual UI code is not updated yet so there is no behavioral changes
    in this patch.
    
    However, the newly-added Sync integration test now represents what the
    UI will be doing in upcoming patches (except iOS which will be slightly
    different): the newly-added utility libraries will be triggered to
    exercise the form field filling based on the PageContext information
    downloaded via Sync, as implemented in /components/send_tab_to_self.
    
    Change-Id: I94a9a250eb497e3a3abb1027bb8c8c2394f688dc
    Bug: 485145029
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7594578
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1587717}

--- chrome/browser/send_tab_to_self/send_tab_to_self_util.h
+++ chrome/browser/send_tab_to_self/send_tab_to_self_util.h
@@ -9,19 +9,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 
 #include "components/send_tab_to_self/entry_point_display_reason.h"
+#include "components/send_tab_to_self/page_context.h"
 
 namespace content {
 class WebContents;
-}
+}  // namespace content
+
+namespace url {
+class Origin;
+}  // namespace url
 
 namespace send_tab_to_self {
 
-// |web_contents| can be null.
+// `web_contents` can be null.
 std::optional<EntryPointDisplayReason> GetEntryPointDisplayReason(
     content::WebContents* web_contents);
 
 // Returns true if the entry point should be shown.
 bool ShouldDisplayEntryPoint(content::WebContents* web_contents);
+
+// Fills form fields in `web_contents` from `page_context` if the field's origin
+// matches `origin`.
+void FillWebContents(content::WebContents* web_contents,
+                     const url::Origin& origin,
+                     const PageContext& page_context);
 
 }  // namespace send_tab_to_self
 
--- chrome/browser/sync/test/integration/send_tab_to_self_helper.h
+++ chrome/browser/sync/test/integration/send_tab_to_self_helper.h
@@ -13,7 +13,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/test/integration/status_change_checker.h"
 #include "components/send_tab_to_self/send_tab_to_self_model_observer.h"
 #include "components/sync_device_info/device_info_tracker.h"
+#include "content/public/test/browser_test_utils.h"
 #include "url/gurl.h"
+
+namespace content {
+class WebContents;
+}  // namespace content
 
 namespace send_tab_to_self {
 class SendTabToSelfEntry;
@@ -22,13 +27,13 @@ class SendTabToSelfSyncService;
 
 namespace send_tab_to_self_helper {
 
-// Class that allows waiting until a particular |url| is exposed by the
-// SendTabToSelfModel in |service|.
+// Class that allows waiting until a particular `url` is exposed by the
+// SendTabToSelfModel in `service`.
 class SendTabToSelfUrlChecker
     : public StatusChangeChecker,
       public send_tab_to_self::SendTabToSelfModelObserver {
  public:
-  // The caller must ensure that |service| is not null and will outlive this
+  // The caller must ensure that `service` is not null and will outlive this
   // object.
   SendTabToSelfUrlChecker(send_tab_to_self::SendTabToSelfSyncService* service,
                           const GURL& url);
@@ -54,13 +59,13 @@ class SendTabToSelfUrlChecker
   const raw_ptr<send_tab_to_self::SendTabToSelfSyncService> service_;
 };
 
-// Class that allows waiting until a particular |url| is marked opened by the
-// SendTabToSelfModel in |service|.
+// Class that allows waiting until a particular `url` is marked opened by the
+// SendTabToSelfModel in `service`.
 class SendTabToSelfUrlOpenedChecker
     : public StatusChangeChecker,
       public send_tab_to_self::SendTabToSelfModelObserver {
  public:
-  // The caller must ensure that |service| is not null and will outlive this
+  // The caller must ensure that `service` is not null and will outlive this
   // object.
   SendTabToSelfUrlOpenedChecker(
       send_tab_to_self::SendTabToSelfSyncService* service,
@@ -91,13 +96,13 @@ class SendTabToSelfUrlOpenedChecker
   const raw_ptr<send_tab_to_self::SendTabToSelfSyncService> service_;
 };
 
-// Class that allows waiting the number of entries in until |service0|
-// matches the number of entries in |service1|.
+// Class that allows waiting the number of entries in until `service0`
+// matches the number of entries in `service1`.
 class SendTabToSelfModelEqualityChecker
     : public StatusChangeChecker,
       public send_tab_to_self::SendTabToSelfModelObserver {
  public:
-  // The caller must ensure that |service0| and |service1| are not null and
+  // The caller must ensure that `service0` and `service1` are not null and
   // will outlive this object.
   SendTabToSelfModelEqualityChecker(
       send_tab_to_self::SendTabToSelfSyncService* service0,
@@ -131,7 +136,7 @@ class SendTabToSelfActiveChecker
     : public StatusChangeChecker,
       public send_tab_to_self::SendTabToSelfModelObserver {
  public:
-  // The caller must ensure that |service| is not null and will outlive this
+  // The caller must ensure that `service` is not null and will outlive this
   // object.
   explicit SendTabToSelfActiveChecker(
       send_tab_to_self::SendTabToSelfSyncService* service);
@@ -206,7 +211,7 @@ class SendTabToSelfUrlDeletedChecker
     : public StatusChangeChecker,
       public send_tab_to_self::SendTabToSelfModelObserver {
  public:
-  // The caller must ensure that |service| is not null and will outlive this
+  // The caller must ensure that `service` is not null and will outlive this
   // object.
   SendTabToSelfUrlDeletedChecker(
       send_tab_to_self::SendTabToSelfSyncService* service,
@@ -234,6 +239,10 @@ class SendTabToSelfUrlDeletedChecker
   const GURL url_;
   const raw_ptr<send_tab_to_self::SendTabToSelfSyncService> service_;
 };
+
+// Returns the value of a form field with the given `id` in `web_contents`.
+content::EvalJsResult GetFormFieldValueById(content::WebContents* web_contents,
+                                            const std::string& id);
 
 }  // namespace send_tab_to_self_helper
 
