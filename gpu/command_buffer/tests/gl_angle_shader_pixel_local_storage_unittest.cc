e175248c09c7b93a11048186c997fae0bb9513b0
Kenneth Russell <kbr@chromium.org>
2026-02-20 17:20:54 -0800

    Completely disable ANGLEShaderPixelLocalStorageTest.
    The test can't run at all while the enums are being renumbered.
    
    Bug: angleproject:40096838
    Change-Id: I808cf2552601b60f19499220ff353b4e7da22571
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7597975
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Auto-Submit: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1588202}

--- gpu/command_buffer/tests/gl_angle_shader_pixel_local_storage_unittest.cc
+++ gpu/command_buffer/tests/gl_angle_shader_pixel_local_storage_unittest.cc
@@ -18,6 +18,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/gl_implementation.h"
 
 namespace gpu {
+// TODO(anglebug.com/40096838): put this test back in once ANGLE has
+// rolled into Chromium with non-coherent PLS changes.
+#if 0
 class ANGLEShaderPixelLocalStorageTest : public testing::Test {
  public:
   ANGLEShaderPixelLocalStorageTest() = default;
@@ -108,9 +111,6 @@ TEST_F(ANGLEShaderPixelLocalStorageTest, GetIntegerv) {
 // marshalled properly over the command buffer. Thorough testing of these
 // commands is done in angle_end2end_tests.
 
-// TODO(anglebug.com/40096838): put this test back in once ANGLE has
-// rolled into Chromium with non-coherent PLS changes.
-#if 0
 TEST_F(ANGLEShaderPixelLocalStorageTest,
        GetFramebufferPixelLocalStorageParameter) {
   if (!gl_.IsInitialized() ||
@@ -167,7 +167,6 @@ TEST_F(ANGLEShaderPixelLocalStorageTest,
 
   EXPECT_GL_ERROR(GL_NO_ERROR);
 }
-#endif
 
 // Verifies all LOAD_OP_*_ANGLE and STORE_OP_*_ANGLE tokens are accepted by the
 // command buffer.
@@ -358,4 +357,5 @@ TEST_F(ANGLEShaderPixelLocalStorageTest, BlockEmulatedDefaultFramebuffer) {
   EXPECT_GL_ERROR(GL_INVALID_OPERATION);
   EXPECT_GL_ERROR(GL_NO_ERROR);
 }
+#endif  // TODO(anglebug.com/40096838)
 }  // namespace gpu
