34ffdef9d4ebef056d4cf3fb742b7dfb571af275
Devon Loehr <dloehr@google.com>
2026-02-21 11:20:27 -0800

    Reland "[rustmojo] Add unit tests for sending messages to/from C++"
    This reverts commit ea09ade65a8c9e9b71e5920fdb2a172cfdaf57f7.
    
    Reason for revert: The problem with the original CL was that we used the SelfOwnedReceiver type in C++, which destroys itself when it gets disconnected...except that disconnection is asynchronous, and the run loop had already terminated by that point, so it never ended up happening. To fix it, we instead just give the rust code ownership of the C++ receiver so its destructor will be invoked directly when it's dropped.
    
    Original change's description:
    > Revert "[rustmojo] Add unit tests for sending messages to/from C++"
    >
    > This reverts commit 5cff65244c481d57ad3113efaf928ec87a7e73d3.
    >
    > Reason for revert: Test `RustBindingsAPI.CppReceiverTest` was consistently failing since creation, see bug: issues.chromium.org/issues/486082290
    >
    > Original change's description:
    > > [rustmojo] Add unit tests for sending messages to/from C++
    > >
    > > This CL adds some basic tests for cross-language functionality. For the
    > > Rust->C++ direction, it defines a function to create a self-owned
    > > receiver on the C++ side, then sends it messages. For the C++->Rust
    > > direction, it defines a function which sets up a remote from a given
    > > handle, and sends a few messages through.
    > >
    > > I manually verified that the tests fail if we mess with the
    > > expectations, so these tests demonstrate that at least our basic
    > > encoding of the wire format is correct, because C++ can understand it
    > > and vice-versa.
    > >
    > > Bug: 444509367
    > > Change-Id: Id6b082cf32f8e33cd4d8f9bdc37ab8ad8960357f
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7584126
    > > Reviewed-by: Julia Hansbrough <flowerhack@google.com>
    > > Commit-Queue: Devon Loehr <dloehr@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1587404}
    >
    > Bug: 444509367, 486082290
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 444509367
    > Change-Id: Iece585b4258f9d7e81de3d1d1aa0f4cfdb4d00d5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7595257
    > Owners-Override: Munira Tursunova <moonira@google.com>
    > Commit-Queue: Munira Tursunova <moonira@google.com>
    > Reviewed-by: Steinar H Gunderson <sesse@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1587711}
    
    Bug: 444509367, 486082290
    Bug: 444509367
    Change-Id: I34302c3cdc6ee4f90cc06f79fe77ae080b754560
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7597215
    Reviewed-by: Julia Hansbrough <flowerhack@google.com>
    Commit-Queue: Devon Loehr <dloehr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1588340}

---
+++ mojo/public/rust/bindings/test/cpp/cxx_shim.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MOJO_PUBLIC_RUST_BINDINGS_TEST_CPP_CXX_SHIM_H_
+#define MOJO_PUBLIC_RUST_BINDINGS_TEST_CPP_CXX_SHIM_H_
+
+#include "mojo/public/c/system/types.h"
+#include "mojo/public/rust/bindings/test/cpp/add_seven_service.h"
+
+namespace bindings_unittests::mojom {
+
+std::unique_ptr<PlusSevenMathService> CreatePlusSevenMathService(
+    MojoHandle handle);
+void TestRemoteFromCpp(MojoHandle handle);
+
+}  // namespace bindings_unittests::mojom
+
+#endif  // MOJO_PUBLIC_RUST_BINDINGS_TEST_CPP_CXX_SHIM_H_
