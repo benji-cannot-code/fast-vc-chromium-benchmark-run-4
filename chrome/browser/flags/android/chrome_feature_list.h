779c170b7d29e7676b252823a5b3ceca31ffc06c
Ben Pastene <bpastene@chromium.org>
2026-02-20 17:08:49 -0800

    Revert "Remove GridTabSwitcherUpdate feature flag."
    This reverts commit 6144d70f607352f1a519e4441d53267cd2221da7.
    
    Reason for revert: broke internal android builds:
    https://ci.chromium.org/ui/p/chrome/builders/ci/android-arm-rel-ready
    https://ci.chromium.org/ui/p/chrome/builders/ci/android-arm-rel-ready/66234/infra
    error: could not resolve field GRID_TAB_SWITCHER_UPDATE
         ChromeFeatureList.GRID_TAB_SWITCHER_UPDATE,
    
    Original change's description:
    > Remove GridTabSwitcherUpdate feature flag.
    >
    > The GridTabSwitcherUpdate feature was enabled by default. This change
    > removes the feature flag and all its usages, effectively making the code
    > paths previously guarded by this flag always active.
    >
    > Fixed: 442510269
    > Change-Id: Id4045f0fb5968e988f3116815f25130e75f78f65
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7563694
    > Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    > Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    > Commit-Queue: Ritika Gupta <ritikagup@google.com>
    > Reviewed-by: Madhav Pruthi <madhavpruthi@google.com>
    > Reviewed-by: Ritika Gupta <ritikagup@google.com>
    > Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1587918}
    
    Bug: 442510269
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I272d83670a434c1629d373e6a47f3ad5c45030fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7598253
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Owners-Override: Kelvin Jiang <kelvinjiang@chromium.org>
    Commit-Queue: Ben Pastene <bpastene@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1588194}

--- chrome/browser/flags/android/chrome_feature_list.h
+++ chrome/browser/flags/android/chrome_feature_list.h
@@ -169,6 +169,7 @@ BASE_DECLARE_FEATURE(kForceTranslucentNotificationTrampoline);
 BASE_DECLARE_FEATURE(kFullscreenInsetsApiMigration);
 BASE_DECLARE_FEATURE(kFullscreenInsetsApiMigrationOnAutomotive);
 BASE_DECLARE_FEATURE(kGridTabSwitcherSurfaceColorUpdate);
+BASE_DECLARE_FEATURE(kGridTabSwitcherUpdate);
 BASE_DECLARE_FEATURE(kGroupNewTabWithParent);
 BASE_DECLARE_FEATURE(kHistoryPaneAndroid);
 BASE_DECLARE_FEATURE(kHomeModulePrefRefactor);
