36624a0859fe2fd11a52fdebee8115a42bcb09d0
Yisi Yu <yuyisi@chromium.org>
2026-02-20 18:46:27 -0800

    Replace pointer-based HexEncode calls with span-based and string_view-based HexEncode
    Bug: 40284755
    Change-Id: Ic8eff4bc2914ae6988cec24f8d69cb08178b9ada
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7588958
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: YISI YU <yuyisi@chromium.org>
    Auto-Submit: YISI YU <yuyisi@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1588238}

--- gpu/command_buffer/service/logger.cc
+++ gpu/command_buffer/service/logger.cc
@@ -5,9 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "gpu/command_buffer/service/logger.h"
 
+#include <inttypes.h>
+
 #include "base/command_line.h"
 #include "base/logging.h"
-#include "base/strings/string_number_conversions.h"
+#include "base/strings/stringprintf.h"
 #include "gpu/command_buffer/common/debug_marker_manager.h"
 #include "gpu/command_buffer/service/gpu_switches.h"
 
@@ -22,9 +24,9 @@ Logger::Logger(const DebugMarkerManager* debug_marker_manager,
       log_message_count_(0),
       log_synthesized_gl_errors_(true),
       disable_gl_error_limit_(disable_gl_error_limit) {
-  Logger* this_temp = this;
-  this_in_hex_ = std::string("GroupMarkerNotSet(crbug.com/242999)!:") +
-      base::HexEncode(&this_temp, sizeof(this_temp));
+  this_in_hex_ =
+      base::StringPrintf("GroupMarkerNotSet(crbug.com/242999)!:%" PRIXPTR,
+                         reinterpret_cast<uintptr_t>(this));
   suppress_performance_logs_ =
       base::CommandLine::ForCurrentProcess()->HasSwitch(
           switches::kSuppressPerformanceLogs);
