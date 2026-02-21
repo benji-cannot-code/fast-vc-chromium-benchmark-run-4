a58abec72928245acbaa82b76a3ee6a72be5c7ba
Gyuyoung Kim <gyuyoung@igalia.com>
2026-02-20 16:23:55 -0800

    [iOS Blink] Fix unused variable warnings in some unit tests
    Enable kArbitraryRendezvousKey and kArbitraryDescriptorKey only for
    non-iOS platform, as they are unused on iOS OSes.
    
    Bug: 389713696
    Change-Id: I003ad1edafb33ab11449aa943db1278956b17598
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7597053
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1588167}

--- base/metrics/field_trial_unittest.cc
+++ base/metrics/field_trial_unittest.cc
@@ -1143,7 +1143,9 @@ TEST_F(FieldTrialListTest, DumpAndFetchFromSharedMemory) {
 
 #if BUILDFLAG(USE_BLINK)
 
+#if !BUILDFLAG(IS_IOS)
 constexpr shared_memory::SharedMemorySwitch::DescriptorKey kFDKey = 42;
+#endif
 
 BASE_FEATURE(kTestFeatureA, "TestFeatureA", base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kTestFeatureB, "TestFeatureB", base::FEATURE_ENABLED_BY_DEFAULT);
