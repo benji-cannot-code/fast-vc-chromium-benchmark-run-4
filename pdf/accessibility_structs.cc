fa4b29d81add71f31c58d341e67ae35b428ae666
Felipe Erias <felipeerias@igalia.com>
2026-02-21 17:48:33 -0800

    Remove unused tag_type field in PDF accessibility text runs
    Remove the tag_type field from AccessibilityTextRunInfo. This field was
    only populated behind kPdfTags, which is disabled by default.
    
    When kPdfTags is enabled with a valid structure tree, the structure
    builder will be used instead of the heuristic builder that would read
    tag_type. The field is therefore never used an can be removed.
    
    Because tag_type was unused, the methods
    StructureElementTypeToAccessibilityRole and
    StructureElementTypeToHeadingLevel were also not used and are removed
    from the heuristic tree builder as well.
    
    As part of this change, TestSelectionConversionViaFindNodeOffset is
    parameterized to run in both structured and heuristic modes.
    
    Bug: 40707542
    Change-Id: I821b0dcb7381ea9cad97c564f7d44c8a1001a9ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7586933
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Felipe Erias <felipeerias@igalia.com>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1588368}

--- pdf/accessibility_structs.cc
+++ pdf/accessibility_structs.cc
@@ -37,13 +37,11 @@ AccessibilityTextRunInfo::AccessibilityTextRunInfo() = default;
 AccessibilityTextRunInfo::AccessibilityTextRunInfo(
     uint32_t start_index,
     uint32_t len,
-    const std::string& tag_type,
     const gfx::RectF& bounds,
     AccessibilityTextDirection direction,
     const AccessibilityTextStyleInfo& style)
     : AccessibilityTextRunInfo(start_index,
                                len,
-                               tag_type,
                                bounds,
                                direction,
                                style,
@@ -52,14 +50,12 @@ AccessibilityTextRunInfo::AccessibilityTextRunInfo(
 AccessibilityTextRunInfo::AccessibilityTextRunInfo(
     uint32_t start_index,
     uint32_t len,
-    const std::string& tag_type,
     const gfx::RectF& bounds,
     AccessibilityTextDirection direction,
     const AccessibilityTextStyleInfo& style,
     bool is_searchified)
     : start_index(start_index),
       len(len),
-      tag_type(tag_type),
       bounds(bounds),
       direction(direction),
       style(style),
