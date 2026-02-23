7a80d18d86394ac0344d40400da6d05903717222
Arthur Milchior <arthurmilchior@google.com>
2026-02-23 03:36:15 -0800

    [iOS]Split Bulk Upload out of ui_bundled
    Bypass-Check-License: Moved files
    Bug: None
    Change-Id: Ic852ee834f1493aa9b1028ccec4675398e275a50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7594467
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1588611}

--- ios/chrome/browser/settings/google_services/bulk_upload/coordinator/bulk_upload_mediator.mm
+++ ios/chrome/browser/settings/google_services/bulk_upload/coordinator/bulk_upload_mediator.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/settings/ui_bundled/google_services/bulk_upload/bulk_upload_mediator.h"
+#import "ios/chrome/browser/settings/google_services/bulk_upload/coordinator/bulk_upload_mediator.h"
 
 #import "base/check_op.h"
 #import "base/i18n/message_formatter.h"
@@ -18,8 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/sync/service/local_data_description.h"
 #import "components/sync/service/sync_service.h"
 #import "ios/chrome/browser/passwords/coordinator/password_utils.h"
-#import "ios/chrome/browser/settings/ui_bundled/google_services/bulk_upload/bulk_upload_consumer.h"
-#import "ios/chrome/browser/settings/ui_bundled/google_services/bulk_upload/bulk_upload_mediator_delegate.h"
+#import "ios/chrome/browser/settings/google_services/bulk_upload/coordinator/bulk_upload_mediator_delegate.h"
+#import "ios/chrome/browser/settings/google_services/bulk_upload/ui/bulk_upload_consumer.h"
 #import "ios/chrome/browser/signin/model/system_identity.h"
 #import "ios/chrome/common/ui/reauthentication/reauthentication_module.h"
 #import "ios/chrome/grit/ios_strings.h"
--- ios/chrome/browser/settings/google_services/bulk_upload/ui/bulk_upload_mutator.h
+++ ios/chrome/browser/settings/google_services/bulk_upload/ui/bulk_upload_mutator.h
@@ -3,12 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_BROWSER_SETTINGS_UI_BUNDLED_GOOGLE_SERVICES_BULK_UPLOAD_BULK_UPLOAD_MUTATOR_H_
-#define IOS_CHROME_BROWSER_SETTINGS_UI_BUNDLED_GOOGLE_SERVICES_BULK_UPLOAD_BULK_UPLOAD_MUTATOR_H_
+#ifndef IOS_CHROME_BROWSER_SETTINGS_GOOGLE_SERVICES_BULK_UPLOAD_UI_BULK_UPLOAD_MUTATOR_H_
+#define IOS_CHROME_BROWSER_SETTINGS_GOOGLE_SERVICES_BULK_UPLOAD_UI_BULK_UPLOAD_MUTATOR_H_
 
 #import <UIKit/UIKit.h>
 
-#import "ios/chrome/browser/settings/ui_bundled/google_services/bulk_upload/bulk_upload_constants.h"
+#import "ios/chrome/browser/settings/google_services/bulk_upload/public/bulk_upload_constants.h"
 
 @protocol BulkUploadMutator <NSObject>
 
@@ -21,4 +21,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @end
 
-#endif  // IOS_CHROME_BROWSER_SETTINGS_UI_BUNDLED_GOOGLE_SERVICES_BULK_UPLOAD_BULK_UPLOAD_MUTATOR_H_
+#endif  // IOS_CHROME_BROWSER_SETTINGS_GOOGLE_SERVICES_BULK_UPLOAD_UI_BULK_UPLOAD_MUTATOR_H_
