ac785fe5e93ccbd881c63b94d6785bfcde2f3a7a
Lauren Winston <lwinston@google.com>
2026-02-23 10:02:16 -0800

    [Reading mode] Replace <mark> tags with <div>s with Readability.
    This is to prevent incorrect emphasis (with incorrect colors)
    getting added within reading mode.
    
    Fixed: 485937614
    Change-Id: I2e117ac72f27d7b55f2f1decdb20df4bc4ce23c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7597186
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1588781}

--- chrome/browser/resources/side_panel/read_anything/content/content_controller.ts
+++ chrome/browser/resources/side_panel/read_anything/content/content_controller.ts
@@ -55,6 +55,7 @@ const SCREEN2X_TAG_TO_RM_TAG: Map<string, string> = new Map([
 const READABILITY_TAG_TO_RM_TAG: Map<string, string> = new Map([
   ['button', 'div'],
   ['details', 'div'],
+  ['mark', 'div'],
 ]);
 
 export interface ContentListener {
