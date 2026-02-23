6a399fe5bdb1ed3c2c462f9d15a02c993a3b882d
Dominik Röttsches <drott@chromium.org>
2026-02-23 06:26:44 -0800

    Reland "Enable safe Rust XML parsing for SVG behind experimental flag"
    This reverts commit ff5633583abb8421f3456501d2d358674d072e46.
    
    Reason for reland: lots-of-lines.svg size reduced in https://crrev.com/c/7594465 - which hopefully allows keeping test running in the Pixel 2 and Pixel 4 harnesses.
    
    Original change's description:
    > Revert "Enable safe Rust XML parsing for SVG behind experimental flag"
    >
    > This reverts commit 4971d866b3c05c8c739b89f04b7c3e0974102860.
    >
    > Reason for revert: crbug.com/486121429 we've confirmed `Pixel_SVGHuge` times out due to the regression. This would block our OpenGL test coverage and lead to timeouts for users
    >
    > Original change's description:
    > > Enable safe Rust XML parsing for SVG behind experimental flag
    > >
    > > Add SVG parsing through Rust to the RustForNonXslt experimental flag. We
    > > have identified that the use of inline XSLT in SVG images seems to be
    > > non-existent according to Dev and Canary UMA data. Hence, add SVG to the
    > > experimentation flag under which we test the Rust based XML parser.
    > >
    > > GPU gardeners, in case you see timeouts in Pixel_SVGHuge,
    > > if possible please consider suppressing them, given discussion in [1].
    > >
    > > [1] https://chromium-review.googlesource.com/c/chromium/src/+/7562213/13#message-462902f98d8a58ccd77020b14fedf33968a09955
    > >
    > > Bug: 466303347
    > > Change-Id: I2f35c8bfc42a351cf88f58b7f999874b36c605bd
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7562213
    > > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > > Reviewed-by: Jonathan Ross <jonross@chromium.org>
    > > Commit-Queue: Dominik Röttsches <drott@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1587362}
    >
    > Bug: 466303347, 486121429
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: Ica02dfd714daf388524a981f9da79cb3fe4b27eb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7595710
    > Owners-Override: Peter McNeeley <petermcneeley@google.com>
    > Auto-Submit: Jonathan Ross <jonross@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Peter McNeeley <petermcneeley@google.com>
    > Reviewed-by: Peter McNeeley <petermcneeley@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1587834}
    
    Bug: 466303347, 486121429
    Cq-Include-Trybots: luci.chromium.try:win11-arm64-blink-rel
    Change-Id: I194c718c1bde067a10b27db9833b700cca1505b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7595639
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1588662}

--- third_party/blink/renderer/core/frame/document_loading_rendering_test.cc
+++ third_party/blink/renderer/core/frame/document_loading_rendering_test.cc
@@ -118,6 +118,7 @@ TEST_F(DocumentLoadingRenderingTest,
   // TODO(https://crbug.com/441911594): Rust XML parser always issues a
   // StartDocument before any elements, also before PIs.
   EXPECT_TRUE(RuntimeEnabledFeatures::XMLParsingRustEnabled() ||
+              RuntimeEnabledFeatures::XMLRustForNonXsltEnabled() ||
               Compositor().DeferMainFrameUpdate());
 
   // Sheet finishes loading, but no documentElement yet so don't resume.
@@ -126,6 +127,7 @@ TEST_F(DocumentLoadingRenderingTest,
   // TODO(https://crbug.com/441911594): Rust XML parser always issues a
   // StartDocument before any elements, also before PIs.
   EXPECT_TRUE(RuntimeEnabledFeatures::XMLParsingRustEnabled() ||
+              RuntimeEnabledFeatures::XMLRustForNonXsltEnabled() ||
               Compositor().DeferMainFrameUpdate());
 
   // Root inserted so resume.
