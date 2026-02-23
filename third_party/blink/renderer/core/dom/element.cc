e3026df2aebd154ce6f933d01f83daf389787594
Kurt Catti-Schmidt <kschmi@microsoft.com>
2026-02-23 15:00:32 -0800

    [CSS Modules] Split flags between style and shadowrootadoptedstylesheets
    We may end up shipping style modules before shadowrootadoptedstylesheets
    due to some pending spec issues that need to be resolved.
    
    Splitting the flags will allow for shipping them separately. For now,
    they're both implied by the main feature flag, but this can be adjusted
    in the future.
    
    Bug: 448174611
    Change-Id: I61ea032c60101feb3390229aaffe02e77ba62443
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7597684
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Commit-Queue: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1588969}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -7531,7 +7531,7 @@ bool Element::AttachDeclarativeShadowRoot(
     shadow_root.SetKeepCustomElementRegistryNull(true);
   }
   // 10.8.NEW. Process shadowrootadoptedstylesheets attribute.
-  if (RuntimeEnabledFeatures::DeclarativeCSSModulesEnabled()) {
+  if (RuntimeEnabledFeatures::ShadowRootAdoptedStyleSheetEnabled()) {
     shadow_root.ProcessAdoptedStylesheetAttribute(adopted_stylesheets);
   }
   return true;
