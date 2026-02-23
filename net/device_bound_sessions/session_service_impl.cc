d2f4da7f2eb79004d277d253a979d24c222ee08f
thefrog <thefrog@chromium.org>
2026-02-23 12:16:46 -0800

    Remove some DBSC DevTools TODOs
     - net/device_bound_sessions/cookie_craving_display.h TODO: Yes, this
       should be copyable.
     - net/device_bound_sessions/session_service_impl.cc TODO: This is a
       possible enhancement but does not need to be represented as a TODO
       in the code.
    
    Bug: 471021582
    Change-Id: I91d9444eb862aecfac74ef84c9c49ecb0eb663af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7600026
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1588856}

--- net/device_bound_sessions/session_service_impl.cc
+++ net/device_bound_sessions/session_service_impl.cc
@@ -1007,8 +1007,6 @@ void SessionServiceImpl::OnAddSessionKeyRestored(
     std::optional<std::string> session_id;
     std::optional<SessionDisplay> display_info;
     if (succeeded) {
-      // TODO(crbug.com/471021582): Some failed creation events could have a
-      // session_id. Can we thread that through?
       session_id = session_or_error.value()->id().value();
       display_info = session_or_error.value()->ToDisplay();
     }
@@ -1185,8 +1183,6 @@ SessionError::ErrorType SessionServiceImpl::OnRegistrationCompleteInternal(
               [&](SessionError error) {
                 // We failed to create a new session, so there's nothing to
                 // clean up.
-                // TODO(crbug.com/471021582): Some failed creation events could
-                // have a session_id. Can we thread that through?
                 SessionError::ErrorType error_type = error.type;
                 NotifyIfEventCallbackListeners([&] {
                   return SessionEvent::MakeCreationEvent(
