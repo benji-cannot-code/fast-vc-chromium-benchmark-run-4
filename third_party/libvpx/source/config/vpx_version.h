29fbbf05be810d699bf97ab4d67381a2edf60835
James Zern <jzern@chromium.org>
2026-02-23 14:46:01 -0800

    Roll src/third_party/libvpx/source/libvpx/ a0ac06f5d..db312f06f (7 commits)
    https://chromium.googlesource.com/webm/libvpx.git/+log/a0ac06f5d3c8..db312f06fb50
    
    $ git log a0ac06f5d..db312f06f --date=short --no-merges --format='%ad %ae %s'
    2026-02-18 jzern vp8,calc_pframe_target_size: fix integer overflow
    2026-02-18 jzern examples/*.c: fix incorrect die_codec() usage
    2026-02-17 jzern **README.libvpx: add missing fields / sync w/libaom
    2026-02-12 jzern vp8_decode_frame: improve header / partition 0 check
    2026-02-13 jzern rtcd.pl: use a fixed year for *rtcd.h Copyright
    2026-02-12 jzern vp8_decode_frame: validate partition 0 size
    2026-02-10 jzern vpx_enc_fuzzer: limit number of encoded frames
    
    Created with:
      roll-dep src/third_party/libvpx/source/libvpx
    
    Bug: 308446709
    Change-Id: Id59754d9be6c35251c54551da5ea917415be6af9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7594296
    Reviewed-by: Wan-Teh Chang <wtc@google.com>
    Auto-Submit: James Zern <jzern@google.com>
    Commit-Queue: James Zern <jzern@google.com>
    Cr-Commit-Position: refs/heads/main@{#1588954}

--- third_party/libvpx/source/config/vpx_version.h
+++ third_party/libvpx/source/config/vpx_version.h
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define VERSION_MAJOR  1
 #define VERSION_MINOR  16
 #define VERSION_PATCH  0
-#define VERSION_EXTRA  "41-ga0ac06f5d"
+#define VERSION_EXTRA  "48-gdb312f06f"
 #define VERSION_PACKED ((VERSION_MAJOR<<16)|(VERSION_MINOR<<8)|(VERSION_PATCH))
-#define VERSION_STRING_NOSP "v1.16.0-41-ga0ac06f5d"
-#define VERSION_STRING      " v1.16.0-41-ga0ac06f5d"
+#define VERSION_STRING_NOSP "v1.16.0-48-gdb312f06f"
+#define VERSION_STRING      " v1.16.0-48-gdb312f06f"
 #endif  // VPX_VERSION_H_
