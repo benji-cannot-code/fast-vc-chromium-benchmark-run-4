9019ea2a1b82aa1c765d6d92b22fa4846033fce8
Tigran Bantikyan <bantikyan@google.com>
2026-02-23 14:12:43 -0800

    [payments] Add fuzz testing for CurrencyFormatter component
    /components/payments/core/currency_formatter.cc has 0%
    coverage. Add a fuzz target that constructs the CurrencyFormatter
    with the fuzz engine's inputs, and then tries calling
    CurrencyFormatter::Format with the fuzz engine's input.
    
    Change-Id: I3b43ed597ca6a13478efa6e271b76d24fc1ae257
    Fixed: 486971455
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7595730
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Tigran Bantikyan <bantikyan@google.com>
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1588931}

--- components/payments/core/currency_formatter_unittest.cc
+++ components/payments/core/currency_formatter_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/fuzztest/src/fuzztest/fuzztest.h"
 
 namespace payments {
 namespace {
@@ -141,6 +142,16 @@ INSTANTIATE_TEST_SUITE_P(
             "fr_FR",
             "123 456 789 012 345 678 901 234 567 890,123456789 $",
             "USD")));
+
+// Fuzz tests
+void FormatDoesNotCrash(std::string currency_code,
+                        std::string locale_name,
+                        std::string amount) {
+  CurrencyFormatter formatter(currency_code, locale_name);
+  formatter.Format(amount);
+}
+
+FUZZ_TEST(CurrencyFormatterFuzzTest, FormatDoesNotCrash);
 
 }  // namespace
 }  // namespace payments
