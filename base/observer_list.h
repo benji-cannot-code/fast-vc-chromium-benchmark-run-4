779dbc66a6be1bc50b1033ee582b1c6fba4d9f16
Mitsuru Oshima <oshima@chromium.org>
2026-02-23 01:28:20 -0800

    Add CompositorList::NotifyReentrantly
    Compositor::Check is safe to be called reentrantly. This CL
    provides a way to such method reentrantly even if the observer list
    itself prohibit the reentrancy.
    
    Bug: 40562847
    Change-Id: I28fd63f8c605448847760ca087b6b30105975ca8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7579576
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1588564}

--- base/observer_list.h
+++ base/observer_list.h
@@ -157,6 +157,7 @@ class ObserverList {
       internal::UncheckedObserverAdapter<RawPtrTraits::kEmpty, true>>;
 
   // An iterator class that can be used to access the list of observers.
+  template <bool check_reentrancy = true>
   class Iter {
    public:
     using iterator_category = std::forward_iterator_tag;
@@ -177,6 +178,7 @@ class ObserverList {
       // TODO(crbug.com/40063488): Turn into CHECK once very prevalent failures
       // are weeded out.
       DUMP_WILL_BE_CHECK(
+          !check_reentrancy ||
           reentrancy != ObserverListReentrancyPolicy::kDisallowReentrancy ||
           list_.IsOnlyRemainingNode());
       // Bind to this sequence when creating the first iterator.
@@ -282,8 +284,8 @@ class ObserverList {
     size_t max_index_;
   };
 
-  using iterator = Iter;
-  using const_iterator = Iter;
+  using iterator = Iter<>;
+  using const_iterator = Iter<>;
   using value_type = ObserverType;
 
   const_iterator begin() const {
@@ -415,6 +417,20 @@ class ObserverList {
   void Notify(Method method, const Args&... args) {
     for (auto& observer : *this) {
       std::invoke(method, observer, args...);
+    }
+  }
+
+  // Same as `Notify` but doesn't check reentrancy. Use this if it's safe to
+  // call the method reentrantly even if the observer list itself should be
+  // non-reentrant. The observer should be non reentrant.
+  // TODO(40562847): Add static_assert to ensure the reentrancy is
+  // kDisallowReentrancy.
+  template <typename Method, typename... Args>
+    requires std::invocable<Method, ObserverType*, const Args&...>
+  void NotifyAllowReentrancy(Method method, const Args&... args) {
+    for (auto iter = Iter</*check_reentrancy=*/false>(this);
+         iter != Iter</*check_reentrancy=*/false>(); iter++) {
+      std::invoke(method, *iter, args...);
     }
   }
 
