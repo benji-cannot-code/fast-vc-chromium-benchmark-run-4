0a8c4a1b1f6a75737f9118b2b60aabbffa543da8
Dmytro Yeroshkin <yeroshkin@chromium.org>
2026-02-23 12:16:33 -0800

    [Clarity] Add Anchored Message support to PageActionController
    Also to ChipSelector/DefaultChipSelector
    
    Bug: 376285838
    Change-Id: I1d0f6553d16a3c98e20a24232579122cfc4be258
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7595885
    Reviewed-by: Daniel Soromou <koretadaniel@chromium.org>
    Commit-Queue: Dmytro Yeroshkin <yeroshkin@chromium.org>
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1588855}

--- chrome/browser/ui/views/page_action/chip_selector.h
+++ chrome/browser/ui/views/page_action/chip_selector.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <set>
+#include <vector>
 
 #include "base/functional/callback.h"
 #include "base/functional/callback_forward.h"
@@ -18,7 +19,7 @@ namespace page_actions {
 struct SuggestionChipConfig;
 
 // ChipSelector is an interface for the logic handling showing and hiding of
-// suggestion chips.
+// suggestion chips and anchored messages.
 class ChipSelector {
  public:
   ChipSelector() = default;
@@ -26,6 +27,8 @@ class ChipSelector {
   virtual void RequestChipShow(actions::ActionId page_action_id,
                                const SuggestionChipConfig& config) = 0;
   virtual void RequestChipHide(actions::ActionId page_action_id) = 0;
+  virtual void RequestAnchoredMessageShow(actions::ActionId page_action_id) = 0;
+  virtual void RequestAnchoredMessageHide(actions::ActionId page_action_id) = 0;
 };
 
 // CreateChipSelector returns the appropriate implementation of the
@@ -34,30 +37,47 @@ std::unique_ptr<ChipSelector> CreateChipSelector(
     base::RepeatingCallback<void(actions::ActionId,
                                  const SuggestionChipConfig&)>
         show_chip_callback,
-    base::RepeatingCallback<void(actions::ActionId)> hide_chip_callback);
+    base::RepeatingCallback<void(actions::ActionId)> hide_chip_callback,
+    base::RepeatingCallback<void(actions::ActionId)>
+        show_anchored_message_callback,
+    base::RepeatingCallback<void(actions::ActionId)>
+        hide_anchored_message_callback);
 
 namespace internal {
 
 // The default implementation of the ChipSelector, which accepts all
-// show and hide requests.
+// show and hide requests for suggestion chips and implements a FIFO queue for
+// anchored messages. Also ensures exactly one of suggestion chip and/or
+// anchored message is shown for a given page action.
 class DefaultChipSelector : public ChipSelector {
  public:
   DefaultChipSelector(
       base::RepeatingCallback<void(actions::ActionId,
                                    const SuggestionChipConfig&)>
           show_chip_callback,
-      base::RepeatingCallback<void(actions::ActionId)> hide_chip_callback);
+      base::RepeatingCallback<void(actions::ActionId)> hide_chip_callback,
+      base::RepeatingCallback<void(actions::ActionId)>
+          show_anchored_message_callback,
+      base::RepeatingCallback<void(actions::ActionId)>
+          hide_anchored_message_callback);
   ~DefaultChipSelector() override;
   void RequestChipShow(actions::ActionId page_action_id,
                        const SuggestionChipConfig& config) override;
   void RequestChipHide(actions::ActionId page_action_id) override;
+  void RequestAnchoredMessageShow(actions::ActionId page_action_id) override;
+  void RequestAnchoredMessageHide(actions::ActionId page_action_id) override;
 
  private:
   const base::RepeatingCallback<void(actions::ActionId,
                                      const SuggestionChipConfig&)>
       show_chip_callback_;
   const base::RepeatingCallback<void(actions::ActionId)> hide_chip_callback_;
+  const base::RepeatingCallback<void(actions::ActionId)>
+      show_anchored_message_callback_;
+  const base::RepeatingCallback<void(actions::ActionId)>
+      hide_anchored_message_callback_;
   std::set<actions::ActionId> active_chips_;
+  std::vector<actions::ActionId> anchored_message_queue_;
 };
 }  // namespace internal
 
