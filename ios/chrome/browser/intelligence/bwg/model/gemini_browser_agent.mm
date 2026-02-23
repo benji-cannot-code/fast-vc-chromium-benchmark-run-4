216413cf07250065e57ea43025594f24c99011db
Joemer Ramos <joemerramos@google.com>
2026-02-23 11:15:45 -0800

    [iOS] Fix Floaty Landscape Padding And Showing On Device Orientation
    Before, the floaty wouldn't look like it was scrolling off during the
    opacity animation. Additionally the floaty bottom offset wasn't
    accounting for landscape mode. This CL accounts for the
    safeAreaInset.bottom while in landscape and while scrolling in
    landscape.
    
    Scrolling Bug Failure Before: crbug.com/486172182#comment1
    Bottom Padding On Landscape Before: crbug.com/486216237#comment1
    UI Fix Video: https://drive.google.com/file/d/13uFD2cJbIuOpw6CG8fq15QJoDO1H5vsi/view?usp=drive_link&resourcekey=0-FoGHHeOq5RasH-R1Cx_t_A
    
    Bug: 486216237
    Change-Id: If696bf901be0ebc025b3d394c3140c58dd344694
    Fixed: 486172182
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7597371
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Commit-Queue: Joemer Ramos <joemerramos@google.com>
    Cr-Commit-Position: refs/heads/main@{#1588833}

--- ios/chrome/browser/intelligence/bwg/model/gemini_browser_agent.mm
+++ ios/chrome/browser/intelligence/bwg/model/gemini_browser_agent.mm
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/public/commands/settings_commands.h"
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ios/chrome/browser/shared/ui/util/layout_guide_names.h"
+#import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
 #import "ios/chrome/browser/shared/ui/util/util_swift.h"
 #import "ios/chrome/browser/signin/model/authentication_service_factory.h"
 #import "ios/chrome/browser/url_loading/model/url_loading_browser_agent.h"
@@ -295,9 +296,17 @@ CGFloat GeminiBrowserAgent::GetFloatyOffsetFromFullscreenController(
     FullscreenController* controller) {
   CGFloat fully_expanded_bottom_toolbar_height =
       controller->GetMaxViewportInsets().bottom;
+
+  SceneState* scene_state = browser_->GetSceneState();
+  if (scene_state && scene_state.window && IsLandscape(scene_state.window)) {
+    fully_expanded_bottom_toolbar_height +=
+        scene_state.window.safeAreaInsets.bottom;
+  }
+
   CGFloat offset =
       (fully_expanded_bottom_toolbar_height * controller->GetProgress()) -
       kFloatyIntrinsicPaddingCorrection;
+
   return offset;
 }
 
@@ -366,7 +375,8 @@ void GeminiBrowserAgent::UpdateForTraitCollection(
   // Update the offset for a device orientation update to landscape or portrait.
   CGFloat offset =
       GetFloatyOffsetFromFullscreenController(fullscreen_controller_);
-  ios::provider::UpdateOverlayOffsetWithOpacity(offset, kFloatyShownOpacity);
+  ios::provider::UpdateOverlayOffsetWithOpacity(
+      offset, fullscreen_controller_->GetProgress());
 }
 
 void GeminiBrowserAgent::PresentFloaty(UIViewController* base_view_controller,
