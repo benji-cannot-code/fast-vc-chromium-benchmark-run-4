4ed3705f3634b66c031d4b1ebb9f59251007e0f0
Peter Kvitek <kvitekp@chromium.org>
2026-02-23 12:20:40 -0800

    [headless] Fix default secondary screen origin
    When calculating default secondary screen origin screen info did not
    consider the previous screen scale factor causing unexpected gaps
    between adjacent screens. This CL fixes this.
    
    Bug: 476931018, 404587568
    Change-Id: I73b8e96c1839de7ecce81e8d0f8dca297a75b374
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7598008
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Auto-Submit: Peter Kvitek <kvitekp@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1588860}

--- components/headless/screen_info/headless_screen_info.cc
+++ components/headless/screen_info/headless_screen_info.cc
@@ -12,6 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "third_party/re2/src/re2/re2.h"
+#include "ui/gfx/geometry/size.h"
+#include "ui/gfx/geometry/size_conversions.h"
+#include "ui/gfx/geometry/size_f.h"
 
 using re2::RE2;
 
@@ -194,10 +197,17 @@ std::string ParseOneScreenInfo(std::string_view screen_info,
     new_screen_info.bounds.set_origin({x, y});
   } else if (!result.empty()) {
     // If no origin is given for a secondary screen shift it to the
-    // right of the previous screen so that they don't overlap.
+    // right of the previous screen accounting for its scale factor.
     const HeadlessScreenInfo& prev_screen = result.back();
+    gfx::Size prev_screen_scaled_size(prev_screen.bounds.size());
+    if (prev_screen.device_pixel_ratio != 1.0f) {
+      gfx::SizeF size(prev_screen.bounds.size());
+      size.InvScale(prev_screen.device_pixel_ratio);
+      prev_screen_scaled_size = gfx::ToCeiledSize(size);
+    }
     new_screen_info.bounds.set_origin(
-        {prev_screen.bounds.right(), prev_screen.bounds.y()});
+        {prev_screen.bounds.x() + prev_screen_scaled_size.width(),
+         prev_screen.bounds.y()});
   }
 
   // Scan in the screen size if any, matching any leading white space followed
