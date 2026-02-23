fc2073045936c338f1341fee4228770576e84fa2
Alexis Hétu <sugoi@chromium.org>
2026-02-23 15:54:50 -0800

    [iOS] Add kAutofillAiReauthBeforeViewingSensitiveData to iOS build
    This CL adds kAutofillAiReauthBeforeViewingSensitiveData to the iOS
    build and uses it to potentially skip enabling reauth on Autofill
    AI suggestions if the preference is set to false.
    
    Bug: 480934776
    Change-Id: Ie45d30e8ef1599b6e658eac2881ee94bf90c6b57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7600658
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Leo Zhao <leozhao@google.com>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1589008}

--- ios/chrome/browser/autofill/model/form_suggestion_controller.mm
+++ ios/chrome/browser/autofill/model/form_suggestion_controller.mm
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/autofill/core/browser/data_manager/autofill_ai/entity_data_manager.h"
 #import "components/autofill/core/browser/data_model/autofill_ai/entity_instance.h"
 #import "components/autofill/core/browser/ui/autofill_suggestion_delegate.h"
+#import "components/autofill/core/common/autofill_prefs.h"
 #import "components/autofill/ios/browser/form_suggestion.h"
 #import "components/autofill/ios/browser/form_suggestion_provider.h"
 #import "components/autofill/ios/common/features.h"
@@ -145,6 +146,13 @@ BOOL RequiresReauth(FormSuggestion* suggestion, web::WebState* web_state) {
   base::optional_ref<const autofill::EntityInstance> entity =
       GetAutofillAiEntity(suggestion.payload, web_state);
   if (!entity.has_value()) {
+    return NO;
+  }
+
+  ProfileIOS* profile =
+      ProfileIOS::FromBrowserState(web_state->GetBrowserState());
+  if (!autofill::prefs::IsAutofillAiReauthBeforeFillingEnabled(
+          profile->GetPrefs())) {
     return NO;
   }
 
