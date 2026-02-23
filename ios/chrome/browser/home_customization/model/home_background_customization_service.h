a0ec0af72ced86fe366f6733ca2d63928e6e9d74
Benjamin Williams <bwwilliams@google.com>
2026-02-23 13:37:52 -0800

    [iOS] Enable two-way theme syncing
    This change is the final step in enabling iOS theme syncing. It connects
    the previously implemented ThemeSyncableServiceIOS to the local
    HomeBackgroundCustomizationService, closing the loop between the local
    UI and Chrome Sync.
    
    Key changes:
    - Adopts ThemeSyncableServiceIOS::Delegate in
      HomeBackgroundCustomizationService.
    - Hooks up local theme state to remote sync updates (ApplyTheme,
      GetLocalTheme).
    - Hooks up sign-in/sign-out snapshot preservation (CacheLocalTheme,
      RestoreCachedTheme).
    
    Demo video:
    https://drive.google.com/file/d/1tO5ToJ28oby2xSaRFvZr8ohFHJqQL1JQ/view?usp=sharing&resourcekey=0-aB_gJosbYqdIf5b_s8on1w
    
    Bug: 481713548
    Change-Id: I7e1539331d2230f23e3f8b1c5e6edc8db516ec49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7592020
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1588901}

--- ios/chrome/browser/home_customization/model/home_background_customization_service.h
+++ ios/chrome/browser/home_customization/model/home_background_customization_service.h
@@ -20,13 +20,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/sync/protocol/theme_types.pb.h"
 #import "ios/chrome/browser/home_customization/model/home_background_data.h"
 #import "ios/chrome/browser/home_customization/model/home_background_image_service.h"
+#import "ios/chrome/browser/home_customization/model/theme_syncable_service_ios.h"
 #import "third_party/skia/include/core/SkColor.h"
 
 class GURL;
 class HomeBackgroundCustomizationServiceObserver;
 class PrefRegistrySimple;
 class PrefService;
-class ThemeSyncableServiceIOS;
 class UserUploadedImageManager;
 
 namespace syncer {
@@ -122,7 +122,9 @@ bool operator==(const sync_pb::ThemeIosSpecifics& lhs,
 }  // namespace sync_pb
 
 // Service for allowing customization of the Home surface background.
-class HomeBackgroundCustomizationService : public KeyedService {
+class HomeBackgroundCustomizationService
+    : public KeyedService,
+      public ThemeSyncableServiceIOS::Delegate {
  public:
   explicit HomeBackgroundCustomizationService(
       PrefService* pref_service,
@@ -138,6 +140,13 @@ class HomeBackgroundCustomizationService : public KeyedService {
 
   // KeyedService implementation:
   void Shutdown() override;
+
+  // `ThemeSyncableServiceIOS::Delegate` overrides.
+  sync_pb::ThemeIosSpecifics GetCurrentTheme() const override;
+  void ApplyTheme(const sync_pb::ThemeIosSpecifics& theme) override;
+  void CacheLocalTheme() override;
+  void RestoreCachedTheme() override;
+  bool IsCurrentThemeSyncable() const override;
 
   // Returns the current custom background data, if there is one.
   std::optional<HomeCustomBackground> GetCurrentCustomBackground();
