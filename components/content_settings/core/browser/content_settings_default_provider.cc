2e527a4be0966e1171feb9fb970c94881dd770a5
Balazs Engedy <engedy@chromium.org>
2026-02-23 14:05:36 -0800

    Add ContentSettings.RegularProfile.DefaultSensorsSetting.
    Add histogram to measure how often users change the "sensors" permission
    default setting away from ALLOW to BLOCK, and to ASK in the future.
    
    Bug: 40094424
    Change-Id: Ib00f8f6c542308abb81b9d2c797c26df32573610
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7594796
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1588926}

--- components/content_settings/core/browser/content_settings_default_provider.cc
+++ components/content_settings/core/browser/content_settings_default_provider.cc
@@ -609,6 +609,11 @@ void DefaultProvider::RecordHistogramMetrics() {
       IntToContentSetting(prefs_->GetInteger(
           GetPrefName(ContentSettingsType::JAVASCRIPT_OPTIMIZER))),
       CONTENT_SETTING_NUM_SETTINGS);
+  base::UmaHistogramEnumeration(
+      "ContentSettings.RegularProfile.DefaultSensorsSetting",
+      IntToContentSetting(
+          prefs_->GetInteger(GetPrefName(ContentSettingsType::SENSORS))),
+      CONTENT_SETTING_NUM_SETTINGS);
 #endif
 
 #if BUILDFLAG(IS_ANDROID)
