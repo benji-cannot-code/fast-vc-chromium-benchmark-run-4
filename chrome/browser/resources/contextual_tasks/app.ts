200d68a5faa305007569e6067ebdfb9518268dfd
Duncan Mercer <mercerd@google.com>
2026-02-23 14:58:19 -0800

    [Contextual Tasks] Fix isFrameLoading never false
    I must've missed changing isFrameLoading back to false when I refactored
    the code. Adding it now with tests to catch in the future.
    
    Change-Id: I59b67a179fb0077fa9b730e2c7449648e1d6c6da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7594568
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Reviewed-by: Bryan Nguyen <nguyenbryan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1588967}

--- chrome/browser/resources/contextual_tasks/app.ts
+++ chrome/browser/resources/contextual_tasks/app.ts
@@ -540,11 +540,13 @@ export class ContextualTasksAppElement extends CrLitElement {
   }
 
   private onThreadFrameContentLoad() {
+    this.isFrameLoading = false;
     this.setIsGhostLoaderVisible(false);
     this.updateBasicModeAfterNavigation();
   }
 
   private onThreadFrameLoadAbort() {
+    this.isFrameLoading = false;
     this.setIsGhostLoaderVisible(false);
     this.updateBasicModeAfterNavigation();
   }
@@ -807,6 +809,22 @@ export class ContextualTasksAppElement extends CrLitElement {
 
   onBeforeRequestForTesting(details: OnBeforeRequestDetails) {
     return this.onBeforeRequest(details);
+  }
+
+  getIsFrameLoadingForTesting() {
+    return this.isFrameLoading;
+  }
+
+  onThreadFrameLoadStartForTesting(event: chrome.webviewTag.LoadStartEvent) {
+    this.onThreadFrameLoadStart(event);
+  }
+
+  onThreadFrameContentLoadForTesting() {
+    this.onThreadFrameContentLoad();
+  }
+
+  onThreadFrameLoadAbortForTesting() {
+    this.onThreadFrameLoadAbort();
   }
 }
 
