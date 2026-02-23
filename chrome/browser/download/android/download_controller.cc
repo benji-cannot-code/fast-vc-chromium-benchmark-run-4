2ff7830811598bd03969d4a0c213ea773cfd975f
Daniel Rubery <drubery@chromium.org>
2026-02-23 11:26:17 -0800

    Fix dangling pointer in EnableVerifyAppsDone
    We already use `app_verification_prompt_download_` to track the download
    that triggered the prompt. This is either equal to `item` or `nullptr`
    by the time the callback runs.
    
    Fixed: 485885482
    Change-Id: I569945343f303f5408347f6f1c6d30266a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7594310
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1588837}

--- chrome/browser/download/android/download_controller.cc
+++ chrome/browser/download/android/download_controller.cc
@@ -422,7 +422,7 @@ void DownloadController::OnDownloadUpdated(DownloadItem* item) {
           .StartEnableVerifyApps(base::BindOnce(
               &DownloadController::EnableVerifyAppsDone,
               // base::Unretained is safe because `this` is a singleton.
-              base::Unretained(this), item));
+              base::Unretained(this)));
     } else if (app_verification_prompt_download_ != item) {
       OnDownloadComplete(item);
     }
@@ -498,9 +498,9 @@ void DownloadController::OnSensitiveDownload(download::DownloadItem* item) {
 }
 
 void DownloadController::EnableVerifyAppsDone(
-    download::DownloadItem* item,
     safe_browsing::VerifyAppsEnabledResult result) {
   if (app_verification_prompt_download_ != nullptr) {
+    DownloadItem* item = app_verification_prompt_download_;
     app_verification_prompt_download_ = nullptr;
     OnDownloadComplete(item);
   }
