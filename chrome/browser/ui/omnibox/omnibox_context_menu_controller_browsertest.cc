69dc1a5cfd5ebdde64c464bcdd292a5d5b7997e0
Khalid Peer <khalidpeer@chromium.org>
2026-02-23 12:49:36 -0800

    [aim-tools-models] Add support for dynamic rendering of model menu items
    Bug: b:483713285
    Change-Id: I5eed6d9d6c666c12c1103f36e3d7013444775e14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7577953
    Commit-Queue: Khalid Peer <khalidpeer@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Josiah Kiehl <kiehl@google.com>
    Cr-Commit-Position: refs/heads/main@{#1588867}

--- chrome/browser/ui/omnibox/omnibox_context_menu_controller_browsertest.cc
+++ chrome/browser/ui/omnibox/omnibox_context_menu_controller_browsertest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/omnibox/omnibox_edit_model.h"
 #include "chrome/browser/ui/omnibox/omnibox_next_features.h"
 #include "chrome/browser/ui/omnibox/omnibox_popup_state_manager.h"
+#include "chrome/browser/ui/omnibox/test_omnibox_popup_file_selector.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/views/location_bar/omnibox_popup_file_selector.h"
 #include "chrome/browser/ui/webui/omnibox_popup/omnibox_popup_web_contents_helper.h"
@@ -46,42 +47,6 @@ size_t GetVisibleItemCount(const ui::SimpleMenuModel* menu_model) {
 }
 
 }  // namespace
-
-// Override `OpenFileUploadDialog` to track calls.
-class TestOmniboxPopupFileSelector : public OmniboxPopupFileSelector {
- public:
-  explicit TestOmniboxPopupFileSelector(gfx::NativeWindow owning_window)
-      : OmniboxPopupFileSelector(owning_window) {}
-  ~TestOmniboxPopupFileSelector() override = default;
-
-  void OpenFileUploadDialog(
-      content::WebContents* web_contents,
-      bool is_image,
-      OmniboxEditModel* edit_model,
-      std::optional<lens::ImageEncodingOptions> image_encoding_options,
-      bool was_ai_mode_open) override {
-    open_file_upload_dialog_calls_++;
-    last_was_ai_mode_open_ = was_ai_mode_open;
-    edit_model_ = edit_model;
-  }
-
-  void FileSelectionCanceled() override {
-    if (last_was_ai_mode_open_ && edit_model_) {
-      edit_model_->OpenAiMode(false, true);
-    }
-  }
-
-  int open_file_upload_dialog_calls() const {
-    return open_file_upload_dialog_calls_;
-  }
-
-  bool last_was_ai_mode_open() const { return last_was_ai_mode_open_; }
-
- private:
-  int open_file_upload_dialog_calls_ = 0;
-  bool last_was_ai_mode_open_ = false;
-  raw_ptr<OmniboxEditModel> edit_model_ = nullptr;
-};
 
 class OmniboxContextMenuControllerBrowserTest : public InProcessBrowserTest {
  public:
