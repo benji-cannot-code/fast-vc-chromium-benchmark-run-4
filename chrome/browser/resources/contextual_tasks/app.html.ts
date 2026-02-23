7052e4e49a11a44ea460df999cd6faa09e504012
Kevin <kevinshah@google.com>
2026-02-23 12:57:31 -0800

    Build contextual_tasks resources on Android
    This CL compiles //chrome/browser/resources/contextual_tasks on Android.
    On Android, a native java Fusebox will be provided to replace the WebUI
    composebox, hence all composebox references were excluded from Android
    build.
    
    Bug: b:478282701
    Binary-Size: Size increase is unavoidable (see above).
    Change-Id: I97baabf6ffe147d1fe3285a9a6e27bd057b75fcb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7558362
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Kevin Shah <kevinshah@google.com>
    Cr-Commit-Position: refs/heads/main@{#1588873}

--- chrome/browser/resources/contextual_tasks/app.html.ts
+++ chrome/browser/resources/contextual_tasks/app.html.ts
@@ -44,6 +44,7 @@ export function getHtml(this: ContextualTasksAppElement) {
               ${this.friendlyZeroStateSubtitle}` : ''}
       </h1>
     </div>
+<if expr="not is_android">
     <contextual-tasks-composebox id="composebox"
           ?hidden="${this.isInBasicMode_ && !this.enableBasicModeZOrder_}"
           .isZeroState="${this.isZeroState_}"
@@ -54,6 +55,7 @@ export function getHtml(this: ContextualTasksAppElement) {
               "${this.enableNativeZeroStateSuggestions_}"
           .inputEnabled="${!this.isInputLocked_}">
     </contextual-tasks-composebox>
+</if>
   </div>
   <error-page id="errorPage"></error-page>
   <!--_html_template_end_-->`;
