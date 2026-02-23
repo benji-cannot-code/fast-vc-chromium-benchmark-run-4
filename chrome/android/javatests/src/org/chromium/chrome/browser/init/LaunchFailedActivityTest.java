96b7685c03b0c534bce69924d5704a56ce44d3f9
Martin Kong <martinkong@google.com>
2026-02-23 11:49:35 -0800

    Fix LaunchFailedActivityTest
    The LaunchFailedActivityTest was previously disabled because it is
    flaky on some of the bots. After investigation, this is because
    the native library is sometimes loaded after the start of test,
    after LibraryLoader.getInstance().resetForTesting() is called.
    
    What happened was that, first the test starts up, then it executes
    LibraryLoader.getInstance().resetForTesting(), which resets the
    library load state to NOT_LOADED. Then, native library is loaded,
    which sets the library load state to LOADED. Now, when test runs
    LibraryLoader.getInstance().ensureInitialized(), the library loader
    thinks that it's already loaded, so it does not throw any exception
    for failing to load the library, causing the test to fail.
    
    The fix is to remove the library load state check when determining
    whether to throw an exception. If we are in tests and we set the
    library load to fail, we always throw an exception, regardless of
    which state the library loader is in.
    
    Bug: 464010980
    Change-Id: I017468c84df686000957e4c41c61c644f0e25f30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7599627
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Martin Kong <martinkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1588844}

--- chrome/android/javatests/src/org/chromium/chrome/browser/init/LaunchFailedActivityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/init/LaunchFailedActivityTest.java
@@ -20,8 +20,6 @@ import org.chromium.base.ContextUtils;
 import org.chromium.base.library_loader.LibraryLoader;
 import org.chromium.base.library_loader.ProcessInitException;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisableIf;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.chrome.browser.base.SplitCompatApplication;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
@@ -49,7 +47,6 @@ public class LaunchFailedActivityTest {
 
     @Test
     @SmallTest
-    @DisabledTest(message = "This test is flaky. See crbug.com/465393381.")
     public void testLaunchFailedWithoutCallbackRaisesProcessInitException() {
         LibraryLoader.getInstance().resetForTesting();
 
@@ -67,8 +64,6 @@ public class LaunchFailedActivityTest {
 
     @Test
     @SmallTest
-    @DisableIf.Build(supported_abis_includes = "arm64-v8a", message = "crbug.com/464010980")
-    @DisableIf.Build(supported_abis_includes = "armeabi-v7a", message = "crbug.com/464010980")
     public void testLaunchFailedWithCallbackRaisesExceptionAndStartsActivity() {
         LibraryLoader.getInstance().resetForTesting();
 
