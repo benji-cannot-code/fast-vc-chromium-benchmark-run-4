3d2fcb65cf65671b70452900f31e0ee5fd4297da
Glenn Hartmann <hartmanng@chromium.org>
2026-02-23 05:40:33 -0800

    Rename profile_browser_collection_non_android_browsertest.cc -> profile_browser_collection_browsertest.cc.
    This CL just makes the follow-up
    (https://chromium-review.googlesource.com/c/chromium/src/+/7592559)
    easier to review.
    
    Bug: 474120522
    Bypass-Check-License: just a rename
    Change-Id: Ibdac25bdd0e83f5295979d11ba1e801d6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7593560
    Commit-Queue: Glenn Hartmann <hartmanng@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1588649}

--- chrome/browser/ui/browser_window/internal/profile_browser_collection_browsertest.cc
+++ chrome/browser/ui/browser_window/internal/profile_browser_collection_browsertest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/ui/browser_window/public/profile_browser_collection.h"
+
 #include "chrome/test/base/in_process_browser_test.h"
 #include "content/public/test/browser_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
