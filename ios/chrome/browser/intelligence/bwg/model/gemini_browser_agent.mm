38ac0b803a49e4bc6b038cecf8a010322995bca6
Joemer Ramos <joemerramos@google.com>
2026-02-23 13:57:53 -0800

    [iOS] Copresence Zero State With Chat History Feature Flag Param
    Creates a feature flag param for Gemini Copresence. When enabled,
    navigating to a new tab and starting a new query will reinvoke zero
    state. When a query is submitted a user will see the existing chat
    history instead of a new thread.
    
    Bug: 486882560
    Change-Id: Iccf7217c21c902df12c8fbd0c55193f2e30e67e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7597031
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Commit-Queue: Joemer Ramos <joemerramos@google.com>
    Cr-Commit-Position: refs/heads/main@{#1588920}

--- ios/chrome/browser/intelligence/bwg/model/gemini_browser_agent.mm
+++ ios/chrome/browser/intelligence/bwg/model/gemini_browser_agent.mm
@@ -866,6 +866,8 @@ void GeminiBrowserAgent::PresentFloatyWithState(
   config.responseReadyInterval = GetGeminiCopresenceResponseReadyInterval();
   config.responseViewDynamicSizeEnabled =
       IsGeminiResponseViewDynamicResizingEnabled();
+  config.geminiCopresenceZeroStateWithChatHistoryEnabled =
+      IsGeminiCopresenceZeroStateWithChatHistoryEnabled();
 
   // Set the location permission state.
   // TODO(crbug.com/426207968): Populate with actual value.
