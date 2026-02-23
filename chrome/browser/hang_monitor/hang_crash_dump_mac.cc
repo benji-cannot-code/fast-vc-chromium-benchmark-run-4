a56896e3f31ea34dae4b3873023da5332235feca
keybo <keybo@google.com>
2026-02-23 01:23:16 -0800

    Modularize //chrome/browser/hang_monitor
    Moved hang_monitor code from //chrome/browser to //chrome/browser/hang_monitor.
    Created a new source_set and DEPS file to enforce architectural boundaries.
    Updated //chrome/browser/BUILD.gn and //chrome/browser/ui/BUILD.gn to depend on the new target.
    
    Bug: 485936775
    Change-Id: Ibed32c9e37229caeb42d571c8bfb2002618df7b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7593915
    Auto-Submit: Keybo Qian <keybo@google.com>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1588561}

--- chrome/browser/hang_monitor/hang_crash_dump_mac.cc
+++ chrome/browser/hang_monitor/hang_crash_dump_mac.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process/process.h"
 #include "components/crash/core/app/crashpad.h"
 #include "content/public/browser/browser_child_process_host.h"
-#include "content/public/common/result_codes.h"
 
 void CrashDumpHungChildProcess(base::ProcessHandle handle) {
   base::PortProvider* provider =
