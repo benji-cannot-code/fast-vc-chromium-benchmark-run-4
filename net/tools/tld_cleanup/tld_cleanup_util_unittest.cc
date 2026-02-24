bcea31ddd1db373f8f4ffd4dad521d838eb5b840
Chris Fredrickson <cfredric@chromium.org>
2026-02-24 15:43:11 -0800

    Add unit test to ensure gperf is updated
    As long as it's nontrivial to automatically generate the gperf file at
    build time (see https://crrev.com/c/7134745), we should have a test that
    ensures the gperf file matches the current version of the dat file, to
    prevent drift.
    
    Change-Id: I566ace6055b7df9cdf1ceef1234bacc2d0109652
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7604147
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1589762}

--- net/tools/tld_cleanup/tld_cleanup_util_unittest.cc
+++ net/tools/tld_cleanup/tld_cleanup_util_unittest.cc
@@ -5,7 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/tools/tld_cleanup/tld_cleanup_util.h"
 
+#include "base/base_paths.h"
 #include "base/files/file_path.h"
+#include "base/files/file_util.h"
+#include "base/files/scoped_temp_dir.h"
+#include "base/path_service.h"
 #include "net/base/registry_controlled_domain_constants.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -193,6 +197,25 @@ TEST(TldCleanupUtilTest, RuleSerialize) {
   EXPECT_EQ(Rule(/*exception=*/true, /*wildcard=*/true, /*is_private=*/true)
                 .Serialize(),
             kDafsaExceptionRule | kDafsaPrivateRule);
+}
+
+TEST(TldCleanupUtilTest, GperfIsUpToDate) {
+  base::ScopedTempDir temp_dir;
+  ASSERT_TRUE(temp_dir.CreateUniqueTempDir());
+
+  const base::FilePath source_dir =
+      base::PathService::CheckedGet(base::DIR_SRC_TEST_DATA_ROOT)
+          .AppendASCII("net")
+          .AppendASCII("base")
+          .AppendASCII("registry_controlled_domains");
+
+  const base::FilePath temp_file = temp_dir.GetPath().AppendASCII("temp.gperf");
+  ASSERT_EQ(NormalizeFile(source_dir.AppendASCII("effective_tld_names.dat"),
+                          temp_file),
+            NormalizeResult::kSuccess);
+
+  EXPECT_TRUE(base::ContentsEqual(
+      source_dir.AppendASCII("effective_tld_names.gperf"), temp_file));
 }
 
 }  // namespace net::tld_cleanup
