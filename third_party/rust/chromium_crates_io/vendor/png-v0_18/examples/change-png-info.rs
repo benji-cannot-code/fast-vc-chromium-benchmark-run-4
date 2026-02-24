dd64dc380652a6cae644cccab1befcc50960f1e7
Lukasz Anforowicz <lukasza@chromium.org>
2026-02-23 16:25:14 -0800

    [rust png] Chromium: Update `png` to version `0.18.1`.
    Note that despite some breaking API changes this Chromium CL does *not*
    depend on any earlier Skia CLs.  In particular, this CL and
    https://review.skia.org/1169956 can land independently.
    
    This CL was created semi-automatically:
    
    * Initial manual edits:
        * edit `png` version in
          `third_party/rust/chromium_crates_io/Cargo.toml`
        * remove old, now-obsolete `png` patches from
          `third_party/rust/chromium_crates_io/patches`
    * Automated edits under `third_party/rust`:
        * `tools/crates/run_gnrt.py vendor`
        * `tools/crates/run_gnrt.py gen`
    * Follow-up manual edits:
        * `ui/android/texture_compressor/main.rs`:
          accounting for a minor API change where the return type of
          `output_buffer_size` is now wrapped in an `Option<T>`.  See:
          https://docs.rs/png/latest/png/struct.Reader.html#method.output_buffer_size
        * `third_party/blink/web_tests/...`,
          `ash/.../input_device_settings_metadata_manager_unittest.cc`, and
          `chrome/.../fast_pair_saved_devices_handler_unittest.cc`:
          accounting for changes in PNG encoder output (which is really an
          internal implementation detail of the encoder, but is still
          unfortunately depended on by some of our tests).  The changes in
          the encoder output can be bisected to the performance improvement
          from https://github.com/image-rs/image-png/pull/646
        * `third_party/blink/.../testing/plte-weirdness.png`:
          The test input has been manually edited, replacing an `fdAT` chunk
          with an `IDAT` chunk.  This edit preserves the essence of the
          `PNGTests.PlteAfterInitialImageData` test, but makes it more
          PLTE-focused and immune to APNG-related changes
        * `third_party/blink/.../png_image_decoder_test.cc`:
          Editing test expectations of `AnimatedPNGTests.fdatBeforeIdat` to
          account for https://github.com/image-rs/image-png/pull/653.
          On one hand, gracefully handling malformed inputs is nice.  OTOH,
          the fallback in the old libpng+Blink decoder would be to a static
          IDAT frame, and therefore it seems unlikely that APNG encoders
          would produce such an malformed input (since it wouldn’t result in
          an animation in the old decoder).  So I think landing this
          behavior change is ok.  Patching Chromium with a revert of
          https://github.com/image-rs/image-png/pull/653 is possible, but
          probably not needed unless such malformed images are actually used
          in the real world.
    
    Bug: chromium:486265213
    Change-Id: I4001d2df430b5054b224bc9ffd0d0f8dd820834f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7599553
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1589029}

--- third_party/rust/chromium_crates_io/vendor/png-v0_18/examples/change-png-info.rs
+++ third_party/rust/chromium_crates_io/vendor/png-v0_18/examples/change-png-info.rs
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 use std::fs::File;
 use std::io::{BufReader, BufWriter};
 use std::path::Path;
+
+use png::DecodingError::LimitsExceeded;
 pub type BoxResult<T> = Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
 fn main() -> BoxResult<()> {
@@ -16,8 +18,8 @@ fn main() -> BoxResult<()> {
     let mut reader = decoder.read_info()?;
     // Allocate the output buffer.
     let png_info = reader.info();
-    let mut buf = vec![0; reader.output_buffer_size()];
-    dbg!(png_info);
+    let mut buf = vec![0; reader.output_buffer_size().ok_or(LimitsExceeded)?];
+    println!("{png_info:?}");
 
     // # Encode
     let path_out = Path::new(r"./target/test_modified.png");
@@ -44,7 +46,6 @@ fn main() -> BoxResult<()> {
     let mut counter = 0u8;
     while let Ok(info) = reader.next_frame(&mut buf) {
         let bytes = &buf[..info.buffer_size()];
-        println!("{} {}", info.buffer_size(), reader.output_buffer_size());
         writer.write_image_data(&bytes)?;
         counter += 1;
         println!("Written frame: {}", counter);
