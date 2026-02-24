6b21f68f6fd00d251a20461a3d5369148b4ee8ea
Nancy Xiao <nancylanxiao@google.com>
2026-02-24 09:27:22 -0800

    Compile deep scanning request and relevant classes on Clank
    Binary size increase: Unavoidable, needed to make enterprise scan
    libraries accessible on Clank.
    
    Binary-Size: Size increase is unavoidable (see above).
    Bug: b:428696481
    Change-Id: I54b0bc60ea1a67f4e8411965bc35e328716bb19a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7571264
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Nancy Xiao <nancylanxiao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1589484}

--- chrome/browser/safe_browsing/download_protection/download_protection_service.cc
+++ chrome/browser/safe_browsing/download_protection/download_protection_service.cc
@@ -853,7 +853,6 @@ int DownloadProtectionService::GetDownloadAttributionUserGestureLimit(
   return kDownloadAttributionUserGestureLimit;
 }
 
-#if !BUILDFLAG(IS_ANDROID)
 void DownloadProtectionService::RequestFinished(DeepScanningRequest* request) {
   auto it = std::ranges::find_if(deep_scanning_requests_,
                                  base::MatchesUniquePtr(request));
@@ -868,7 +867,6 @@ DownloadProtectionService::GetBinaryUploadService(
   return enterprise_connectors::GetBinaryUploadServiceForConnector(profile,
                                                                    settings);
 }
-#endif
 
 void DownloadProtectionService::MaybeCheckMetadataAfterDeepScanning(
     download::DownloadItem* item,
