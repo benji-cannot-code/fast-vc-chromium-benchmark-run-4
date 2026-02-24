2a0d8dca9022bb355e3cc94fd6d9a45195128e32
Yisi Yu <yuyisi@chromium.org>
2026-02-24 10:23:17 -0800

    Replace std::ranges::fill with the default assignment operation
    Use the default assignment operation to zero out the `Exploded` struct,
    replacing std::ranges::fill.
    
    Change-Id: Ic6dfe019b159faecb86ead8ef4546d3ec4f15907
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7603392
    Auto-Submit: YISI YU <yuyisi@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1589535}

--- base/time/time_win.cc
+++ base/time/time_win.cc
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 #include <windows.foundation.h>
 
-#include <algorithm>
 #include <atomic>
 #include <ostream>
 
@@ -48,7 +47,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bit_cast.h"
 #include "base/check_op.h"
 #include "base/command_line.h"
-#include "base/containers/span.h"
 #include "base/cpu.h"
 #include "base/notreached.h"
 #include "base/rand_util.h"
@@ -380,7 +378,7 @@ bool Time::FromExploded(bool is_local, const Exploded& exploded, Time* time) {
 void Time::Explode(bool is_local, Exploded* exploded) const {
   if (!CanConvertToFileTime(us_)) {
     // We are not able to convert it to FILETIME.
-    std::ranges::fill(base::byte_span_from_ref(*exploded), 0);
+    *exploded = Exploded();
     return;
   }
 
@@ -403,7 +401,7 @@ void Time::Explode(bool is_local, Exploded* exploded) const {
   }
 
   if (!success) {
-    std::ranges::fill(base::byte_span_from_ref(*exploded), 0);
+    *exploded = Exploded();
     return;
   }
 
