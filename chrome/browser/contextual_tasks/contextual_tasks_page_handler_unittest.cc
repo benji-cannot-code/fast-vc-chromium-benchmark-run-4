33292eda67771d366a6d7dccdc27673eb2218958
Matthew Jones <mdjones@chromium.org>
2026-02-24 15:29:07 -0800

    Revert "[Contextual Tasks] Don't use language in common search params"
    This reverts commit b1bf8d6400f71bf184ae773cbea1e7798810a8d4.
    
    Reason for revert: We're taking a different approach that prefers
    search handling the "hl" param.
    
    Original change's description:
    > [Contextual Tasks] Don't use language in common search params
    >
    > This patch updates AppendCommonSearchParametersToURL to accept the
    > language param as an optional. If nullopt, the param will not be
    > added to the URL. A corresponding util to check if the params exist
    > has also been added.
    >
    > Bug: 483340750
    > Change-Id: I7e267a838ca246ef75fb9ba6678fc29a8a52a6d8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7594277
    > Commit-Queue: Matthew Jones <mdjones@chromium.org>
    > Reviewed-by: Duncan Mercer <mercerd@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1587940}
    
    Bug: 483340750
    Change-Id: I1dc5cb261ac3587318e408f605300f256d9e9983
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7603816
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1589754}

--- chrome/browser/contextual_tasks/contextual_tasks_page_handler_unittest.cc
+++ chrome/browser/contextual_tasks/contextual_tasks_page_handler_unittest.cc
@@ -630,6 +630,7 @@ TEST_F(ContextualTasksPageHandlerTest, GetCommonSearchParams) {
         /*is_dark_mode=*/false, /*is_side_panel=*/true,
         base::BindLambdaForTesting(
             [&](const base::flat_map<std::string, std::string>& params) {
+              EXPECT_EQ(params.at(lens::kLanguageCodeParameterKey), "en-US");
               EXPECT_EQ(params.at(lens::kDarkModeParameterKey),
                         lens::kDarkModeParameterLightValue);
               EXPECT_EQ(params.at(lens::kChromeSidePanelParameterKey), "2");
@@ -645,6 +646,7 @@ TEST_F(ContextualTasksPageHandlerTest, GetCommonSearchParams) {
         /*is_dark_mode=*/true, /*is_side_panel=*/false,
         base::BindLambdaForTesting(
             [&](const base::flat_map<std::string, std::string>& params) {
+              EXPECT_EQ(params.at(lens::kLanguageCodeParameterKey), "en-US");
               EXPECT_EQ(params.at(lens::kDarkModeParameterKey),
                         lens::kDarkModeParameterDarkValue);
               EXPECT_EQ(params.at(lens::kChromeSidePanelParameterKey), "");
@@ -664,6 +666,7 @@ TEST_F(ContextualTasksPageHandlerTest, GetCommonSearchParams) {
         /*is_dark_mode=*/false, /*is_side_panel=*/true,
         base::BindLambdaForTesting(
             [&](const base::flat_map<std::string, std::string>& params) {
+              EXPECT_EQ(params.at(lens::kLanguageCodeParameterKey), "US");
               EXPECT_EQ(params.at("gl"), "us");
               run_loop.Quit();
             }));
