5829a176dff771d298c6e4fc74c7268883538307
Ryo Hashimoto <hashimoto@google.com>
2026-02-23 23:04:26 -0800

    blink: Implement draggable desktop android scrollbars
    Make ScrollbarTheme::DesktopAndroidScrollbarsEnabled() public and use it
    in VisualViewport and PaintLayerScrollableArea to toggle the behavior.
    
    BUG=410285656
    
    Change-Id: I80eba75e3022024fd287753039ac4f9f5d2208bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7545041
    Commit-Queue: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1589194}

--- third_party/blink/renderer/core/frame/visual_viewport.cc
+++ third_party/blink/renderer/core/frame/visual_viewport.cc
@@ -737,7 +737,8 @@ void VisualViewport::UpdateScrollbarLayer(ScrollbarOrientation orientation) {
 }
 
 bool VisualViewport::VisualViewportSuppliesScrollbars() const {
-  return IsActiveViewport() && GetPage().GetSettings().GetViewportEnabled();
+  return IsActiveViewport() && GetPage().GetSettings().GetViewportEnabled() &&
+         !ScrollbarTheme::DesktopAndroidScrollbarsEnabled();
 }
 
 const Document* VisualViewport::GetDocument() const {
