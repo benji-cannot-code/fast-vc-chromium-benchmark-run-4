f3dd24ec7034ca6d0b46a5ffd2d1a937a6dc9a6c
Khanh Nguyen <khanhtmn@google.com>
2026-02-24 15:36:09 -0800

    [contextual tasks] Replace `TabStripModel::AddTab()` with `TabListInterface::InsertWebContentsAt()` in `ContextualTasksUiService`.
    This is a follow up after crrev.com/c/7585417. We now have replaced most
     of the call sites of TabStripModel in ContextualTasksUiService except
    for `TabStripModel::DetachWebContentsAtForInsertion` since
    `TabListInterface` doesn't have an equivalent API yet.
    
    Bug: b:483119674
    Change-Id: I50be58868469a44eb3231afede1dd664995b2416
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7601053
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Auto-Submit: Khanh Nguyen <khanhtmn@google.com>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1589758}

--- chrome/browser/contextual_tasks/contextual_tasks_ui_service.h
+++ chrome/browser/contextual_tasks/contextual_tasks_ui_service.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/timer/timer.h"
 #include "chrome/browser/contextual_tasks/contextual_tasks.mojom.h"
+#include "chrome/browser/tab_list/tab_list_interface.h"
 #include "components/contextual_search/contextual_search_session_handle.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "content/public/browser/frame_tree_node_id.h"
@@ -25,12 +26,6 @@ class AimEligibilityService;
 class BrowserWindowInterface;
 class GoogleServiceAuthError;
 class Profile;
-
-#if !BUILDFLAG(IS_ANDROID)
-// TODO(crbug.com/483442073): Remove this once we remove TabStripModel from
-// MaybeFocusExistingOpenTab.
-class TabStripModel;
-#endif
 
 namespace base {
 class Uuid;
@@ -256,7 +251,6 @@ class ContextualTasksUiService : public KeyedService {
   // Runs all pending access token callbacks with the provided token.
   void RunPendingAccessTokenCallbacks(const std::string& token);
 
-#if !BUILDFLAG(IS_ANDROID)
   // Focus an existing tab based on the provided URL if it exists. The URLs are
   // compared without text selection directives as they don't change the page
   // content and only tell the browser what text to highlight on the page. A
@@ -264,9 +258,8 @@ class ContextualTasksUiService : public KeyedService {
   // TODO(crbug.com/483442073): Remove the ifdef block once we remove
   // TabStripModel from MaybeFocusExistingOpenTab.
   tabs::TabInterface* MaybeFocusExistingOpenTab(const GURL& url,
-                                                TabStripModel* tab_strip_model,
+                                                TabListInterface* tab_list,
                                                 const base::Uuid& task_id);
-#endif
 
   // A callback for checking whether text fragments from a URL are on a page.
   void OnTextFinderLookupComplete(
