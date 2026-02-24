8f1c73e4de36f41e86fa5b76e8b6e29b32306c25
Mitsuru Oshima <oshima@chromium.org>
2026-02-23 17:27:29 -0800

    MockActivationController class to emulate widget activation in test.
    And use this class in BrowserActivator for Win/Mac/Linux
    This class is now deprecated and will be removed in a follow up CL.
    
    Note that this emulation is necessary for Win/Mac/Linux only and not
    necessary on ChromeOS and Android, because on ChromeOS, each
    test creates its own environment, and on Android, all tests run
    in sequence.
    
    Please see the comment for MockActivationController class for the
    detail.
    
    Include-Ci-Only-Tests: chromium.mac:mac15-x64-rel-tests|browser_tests
    Bug: 485886095, 485887814
    Test: covered by views_unittests and browser_tests.
    Change-Id: I9e7e27a71ebcd72caea059b90c06fb2f58ac6174
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7586664
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1589062}

--- ui/views/widget/desktop_aura/desktop_window_tree_host_platform.cc
+++ ui/views/widget/desktop_aura/desktop_window_tree_host_platform.cc
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/corewm/tooltip_controller.h"
 #include "ui/views/widget/desktop_aura/desktop_drag_drop_client_ozone.h"
 #include "ui/views/widget/desktop_aura/desktop_native_widget_aura.h"
+#include "ui/views/widget/widget_activation_delegate.h"
 #include "ui/views/widget/widget_aura_utils.h"
 #include "ui/views/widget/widget_delegate.h"
 #include "ui/views/window/native_frame_view.h"
@@ -465,6 +466,10 @@ void DesktopWindowTreeHostPlatform::Show(ui::mojom::WindowShowState show_state,
       break;
   }
 
+  if (WidgetActivationDelegate::Get()) {
+    WidgetActivationDelegate::Get()->MaybeActivate(GetWidget(), false);
+  }
+
   if (native_widget_delegate_->CanActivate()) {
     if (show_state != ui::mojom::WindowShowState::kInactive &&
         show_state != ui::mojom::WindowShowState::kMinimized) {
@@ -616,15 +621,26 @@ void DesktopWindowTreeHostPlatform::SetParent(gfx::AcceleratedWidget parent) {
 }
 
 void DesktopWindowTreeHostPlatform::Activate() {
+  if (WidgetActivationDelegate::Get()) {
+    WidgetActivationDelegate::Get()->MaybeActivate(GetWidget(),
+                                                   /*activate=*/true);
+  }
   platform_window()->Activate();
 }
 
 void DesktopWindowTreeHostPlatform::Deactivate() {
-  ReleaseCapture();
-  platform_window()->Deactivate();
+  if (WidgetActivationDelegate::Get()) {
+    WidgetActivationDelegate::Get()->Deactivate(GetWidget());
+  } else {
+    ReleaseCapture();
+    platform_window()->Deactivate();
+  }
 }
 
 bool DesktopWindowTreeHostPlatform::IsActive() const {
+  if (WidgetActivationDelegate::Get()) {
+    return WidgetActivationDelegate::Get()->IsActive(GetWidget());
+  }
   return is_active_;
 }
 
@@ -1036,6 +1052,10 @@ bool DesktopWindowTreeHostPlatform::OnRotateFocus(
 }
 
 void DesktopWindowTreeHostPlatform::OnActivationChanged(bool active) {
+  if (WidgetActivationDelegate::Get()) {
+    return;
+  }
+
   if (active) {
     auto widget = GetAcceleratedWidget();
     open_windows().remove(widget);
