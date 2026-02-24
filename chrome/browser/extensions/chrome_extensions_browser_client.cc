92005aebd9b54f468fab24663d4b5e5c52d25af2
Zonghan Xu <xzonghan@google.com>
2026-02-24 07:34:48 -0800

    [POR Extension API] Add ProxyOverrideRules preference transformer
    Add a custom preference transformer for ProxyOverrideRules, where we perform extra input validation.
    
    Fixed: 481020049
    Change-Id: I119b342ccd485289606154f809809ba38fc27009
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7542155
    Reviewed-by: Tim <tjudkins@chromium.org>
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: Zonghan Xu <xzonghan@chromium.org>
    Reviewed-by: Sébastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1589407}

--- chrome/browser/extensions/chrome_extensions_browser_client.cc
+++ chrome/browser/extensions/chrome_extensions_browser_client.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/preference/privacy_sandbox_transformer.h"
 #include "chrome/browser/extensions/api/preference/protected_content_enabled_transformer.h"
 #include "chrome/browser/extensions/api/proxy/proxy_pref_transformer.h"
+#include "chrome/browser/extensions/api/proxy_override_rules/proxy_override_rules_transformer.h"
 #include "chrome/browser/extensions/api/runtime/chrome_runtime_api_delegate.h"
 #include "chrome/browser/extensions/blocklist_factory.h"
 #include "chrome/browser/extensions/chrome_component_extension_resource_manager.h"
@@ -209,6 +210,11 @@ bool RegisterTransformers() {
       std::make_unique<CookieControlsModeTransformer>());
   pref_mapping->RegisterPrefTransformer(
       proxy_config::prefs::kProxy, std::make_unique<ProxyPrefTransformer>());
+#if BUILDFLAG(ENABLE_EXTENSIONS)
+  pref_mapping->RegisterPrefTransformer(
+      proxy_config::prefs::kProxyOverrideRules,
+      std::make_unique<ProxyOverrideRulesTransformer>());
+#endif  // ENABLE_EXTENSIONS
   pref_mapping->RegisterPrefTransformer(
       prefetch::prefs::kNetworkPredictionOptions,
       std::make_unique<NetworkPredictionTransformer>());
