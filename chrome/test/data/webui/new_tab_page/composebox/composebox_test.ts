e0b871ed4b51aa31695ef0d3e7712ee7da6ae556
Dhruv Kathpalia <dhruvkathpalia@google.com>
2026-02-24 13:28:05 -0800

    [composebox] Use specific error strings for image and PDF upload limits.
    This change introduces new resource strings for error messages when the
    maximum number of images or PDF files is reached during upload in the
    Compose feature. The searchbox handler is updated to use these new, more
    specific messages instead of the generic file limit error.
    Screenshots:
    screenshot/7MqeoVWhnma2uoW
    screenshot/7LUUgEbLHJrHLPJ
    
    Bug: b:483852166
    Change-Id: Ie245c8175b8cc77e0c95c4dcf5b9f70eee56ee2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7600715
    Auto-Submit: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Commit-Queue: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Cr-Commit-Position: refs/heads/main@{#1589674}

--- chrome/test/data/webui/new_tab_page/composebox/composebox_test.ts
+++ chrome/test/data/webui/new_tab_page/composebox/composebox_test.ts
@@ -2151,7 +2151,7 @@ suite('NewTabPageComposeboxTest', () => {
     // Check whether the right error would show up.
     const errorEvent = await errorEventPromise;
     assertEquals(
-        loadTimeData.getString('maxFilesReachedError'),
+        loadTimeData.getString('maxImagesReachedError'),
         errorEvent.detail.errorMessage);
   });
 
@@ -2313,7 +2313,7 @@ suite('NewTabPageComposeboxTest', () => {
 
     const errorEvent = await errorEventPromise;
     assertEquals(
-        loadTimeData.getString('maxFilesReachedError'),
+        loadTimeData.getString('maxImagesReachedError'),
         errorEvent.detail.errorMessage);
 
     assertEquals(
