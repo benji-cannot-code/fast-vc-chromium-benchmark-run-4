9872bb4d8c2faea4630f9950bdf20a2f9934ec7b
Rune Lillesveen <futhark@chromium.org>
2026-02-24 07:10:12 -0800

    Need to run BeginFrame() with raster=true for snapshotting
    With RunSnapshotPostLayoutStateSteps enabled, snapshotting of
    post-layout state is always done after style and layout, which means
    BeginFrame() will not do a snapshot unless we pass raster=true for the
    test.
    
    Bug: 384523570
    Change-Id: Ia67a8c9ad9ede32bf9b86a211590dcfa04cc0178
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7595266
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1589397}

--- third_party/blink/renderer/core/page/scrolling/scrolling_test.cc
+++ third_party/blink/renderer/core/page/scrolling/scrolling_test.cc
@@ -3703,7 +3703,7 @@ TEST_F(ScrollingSimTest, ScrollTimelineActiveAtBoundary) {
       static_cast<cc::AnimationHost*>(GetLayerTreeHostImpl()->mutator_host());
 
   // First frame: Initial commit creates the cc::Animation etc.
-  Compositor().BeginFrame();
+  Compositor().BeginFrame(0.016, /*raster=*/true);
 
   blink::Animation* animation =
       GetDocument().getElementById(AtomicString("align"))->getAnimations()[0];
@@ -3729,7 +3729,7 @@ TEST_F(ScrollingSimTest, ScrollTimelineActiveAtBoundary) {
   // Second frame: LTHI::Animate transitions to RunState::STARTING. Pass
   // raster=true to also reach LTHI::UpdateAnimationState, which transitions
   // STARTING -> RUNNING.
-  Compositor().BeginFrame(0.016, /* raster */ true);
+  Compositor().BeginFrame(0.016, /*raster=*/true);
   EXPECT_EQ(gfx::KeyframeModel::RUNNING, keyframe_model_impl->run_state());
 
   // Scroll to the end.
@@ -3738,7 +3738,7 @@ TEST_F(ScrollingSimTest, ScrollTimelineActiveAtBoundary) {
   // Third frame: LayerTreeHost::ApplyMutatorEvents dispatches
   // AnimationEvent::STARTED and resets
   // KeyframeModel::needs_synchronized_start_time_.
-  Compositor().BeginFrame();
+  Compositor().BeginFrame(0.016, /*raster=*/true);
   EXPECT_EQ(gfx::KeyframeModel::RUNNING, keyframe_model_impl->run_state());
 
   // Verify that KeyframeModel::CalculatePhase returns ACTIVE for the case of
@@ -3751,8 +3751,8 @@ TEST_F(ScrollingSimTest, ScrollTimelineActiveAtBoundary) {
   // local_time == before_active_boundary_time.
   animation->setPlaybackRate(-1);
   GetDocument().getElementById(AtomicString("s"))->setScrollTop(0);
-  Compositor().BeginFrame(0.016, /* raster */ true);
-  Compositor().BeginFrame();
+  Compositor().BeginFrame(0.016, /*raster=*/true);
+  Compositor().BeginFrame(0.016, /*raster=*/true);
 
   cc_animation = animation->GetCompositorAnimation()->CcAnimation();
   keyframe_model_main =
