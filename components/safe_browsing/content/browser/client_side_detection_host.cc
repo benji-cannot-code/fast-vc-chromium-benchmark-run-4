4376017c46b4be1eaf8a2a7c3d89ad4705b0061f
Sarah Krakowiak <skrakowi@chromium.org>
2026-02-24 15:06:55 -0800

    [Report-unsafe-site] Add new report type and screenshot field to proto
    This CL adds a new report type for the case where a user reports a site
    as a scam. This also adds a field for a high resolution screenshot of
    the page, in the case where the user decides to include it in the
    report.
    
    See cl/872938672 for server-side changes.
    
    Bug: 483167850
    Change-Id: Ib3d6ba400b208d0a97db012116afdb7fd6dc0c01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7589534
    Reviewed-by: thefrog <thefrog@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Seung Jae Lim <andysjlim@chromium.org>
    Commit-Queue: Sarah Krakowiak <skrakowi@chromium.org>
    Auto-Submit: Sarah Krakowiak <skrakowi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1589739}

--- components/safe_browsing/content/browser/client_side_detection_host.cc
+++ components/safe_browsing/content/browser/client_side_detection_host.cc
@@ -178,6 +178,8 @@ std::string GetRequestTypeName(
       return "CreditCardForm";
     case safe_browsing::ClientSideDetectionType::IMAGE_EMBEDDING_MATCH:
       return "ImageEmbeddingMatch";
+    case safe_browsing::ClientSideDetectionType::USER_REPORT:
+      return "UserReport";
   }
 }
 
@@ -208,6 +210,8 @@ safe_browsing::mojom::ClientSideDetectionType GetClientSideDetectionMojomType(
           kImageEmbeddingMatch;
     case safe_browsing::ClientSideDetectionType::
         CLIENT_SIDE_DETECTION_TYPE_UNSPECIFIED:
+    case safe_browsing::ClientSideDetectionType::USER_REPORT:
+      return safe_browsing::mojom::ClientSideDetectionType::kUserReport;
     default:
       NOTREACHED();
   }
