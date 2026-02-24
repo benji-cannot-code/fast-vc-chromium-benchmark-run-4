92848c437ff71dc68421c90293b46cdbeab186d4
Thomas Guilbert <tguilbert@chromium.org>
2026-02-24 15:34:11 -0800

    [CodeHealth] Spanify (de)interleaving in AudioPump
    This CL replaces uses of the deprecated ToInterleaved/FromInterleaved
    methods with their safer spanified counterparts.
    
    Bug: 373960632
    Change-Id: I2acb1b9103418f47c4bbfc2617011e0c231dbc54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7599981
    Auto-Submit: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1589757}

--- remoting/protocol/audio_pump.cc
+++ remoting/protocol/audio_pump.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/check_op.h"
+#include "base/containers/span.h"
 #include "base/functional/bind.h"
 #include "base/location.h"
 #include "base/notreached.h"
@@ -34,10 +35,8 @@ std::unique_ptr<media::AudioBus> AudioPacketToAudioBus(
   DCHECK_GT(frame_count, 0);
   std::unique_ptr<media::AudioBus> result =
       media::AudioBus::Create(packet.channels(), frame_count);
-  result->FromInterleaved<media::SignedInt16SampleTypeTraits>(
-      // TODO(crbug.com/428945428): Fix unsafe uses of std::string::data().
-      UNSAFE_TODO(reinterpret_cast<const int16_t*>(packet.data(0).data())),
-      frame_count);
+  result->FromInterleavedBytes<media::SignedInt16SampleTypeTraits>(
+      base::as_byte_span(packet.data(0)));
   return result;
 }
 
@@ -47,9 +46,8 @@ std::unique_ptr<remoting::AudioPacket> AudioBusToAudioPacket(
       std::make_unique<remoting::AudioPacket>();
   result->add_data()->resize(packet.channels() * packet.frames() *
                              sizeof(int16_t));
-  packet.ToInterleaved<media::SignedInt16SampleTypeTraits>(
-      packet.frames(),
-      reinterpret_cast<int16_t*>(&(result->mutable_data(0)->at(0))));
+  packet.ToInterleavedBytes<media::SignedInt16SampleTypeTraits>(
+      base::as_writable_byte_span(*result->mutable_data(0)));
   result->set_encoding(remoting::AudioPacket::ENCODING_RAW);
   result->set_channels(
       static_cast<remoting::AudioPacket::Channels>(packet.channels()));
