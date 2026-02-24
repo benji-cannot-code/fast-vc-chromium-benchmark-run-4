a562237a791b13b02aae50d4180182ccc7d0147d
Phil Yan <philyan@chromium.org>
2026-02-24 06:08:57 -0800

    [Auto-PiP] Reenable disabled tests in AutoPictureInPictureTabHelperTest
    Since the flakiness fix was deployed, these tests are stable:
    screen/95Ht8QYwPQEet4m
    screen/3pd4fSNcc6sMwKs
    screen/68Bc9ShoCQ4YnCq
    
    Bug: 481445841
    Change-Id: If08363d5fc889c3a7173056d11f9a804399ec8a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7602072
    Commit-Queue: Benjamin Keen <bkeen@google.com>
    Auto-Submit: Phil Yan <philyan@chromium.org>
    Reviewed-by: Benjamin Keen <bkeen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1589357}

--- chrome/browser/picture_in_picture/test/android/java/src/org/chromium/chrome/browser/picture_in_picture/AutoPictureInPictureTabHelperTest.java
+++ chrome/browser/picture_in_picture/test/android/java/src/org/chromium/chrome/browser/picture_in_picture/AutoPictureInPictureTabHelperTest.java
@@ -435,7 +435,6 @@ public class AutoPictureInPictureTabHelperTest {
 
     @Test
     @MediumTest
-    @DisableIf.Device(DeviceFormFactor.DESKTOP) // https://crbug.com/481445841
     public void testQuickDismissalIncrementsDismissCount() throws TimeoutException {
         WebContents webContents = loadUrlAndInitializeForTest(AUTO_PIP_VIDEO_PAGE);
         String url = mActivityTestRule.getTestServer().getURL(AUTO_PIP_VIDEO_PAGE);
@@ -463,7 +462,6 @@ public class AutoPictureInPictureTabHelperTest {
 
     @Test
     @MediumTest
-    @DisableIf.Device(DeviceFormFactor.DESKTOP) // https://crbug.com/481445841
     public void testSwitchingBackToTabDoesNotIncrementDismissCount() throws TimeoutException {
         WebContents webContents = loadUrlAndInitializeForTest(AUTO_PIP_VIDEO_PAGE);
         String url = mActivityTestRule.getTestServer().getURL(AUTO_PIP_VIDEO_PAGE);
@@ -492,7 +490,6 @@ public class AutoPictureInPictureTabHelperTest {
 
     @Test
     @MediumTest
-    @DisableIf.Device(DeviceFormFactor.DESKTOP) // https://crbug.com/481445841
     public void testClosingAfterTimerExpiresDoesNotIncrementDismissCount() throws TimeoutException {
         WebContents webContents = loadUrlAndInitializeForTest(AUTO_PIP_VIDEO_PAGE);
         String url = mActivityTestRule.getTestServer().getURL(AUTO_PIP_VIDEO_PAGE);
