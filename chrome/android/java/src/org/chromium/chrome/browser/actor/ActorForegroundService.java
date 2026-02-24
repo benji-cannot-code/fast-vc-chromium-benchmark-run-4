23af8e0a1d68822d08ba6a16f39c35c9049a0230
Ritika Gupta <ritikagup@google.com>
2026-02-23 18:11:57 -0800

    [Actor] Adding new foreground service for actor.
    In this CL,
    - Adding a new foreground service for actor and also methods
    for managing active notification attached to the service.
    - Added the new service entry in AndroidManifest.xml
    
    Bug: 481056567
    Change-Id: I5be77912ad84e109800c6b0d98006523a87bfd39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7598576
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Ritika Gupta <ritikagup@google.com>
    Cr-Commit-Position: refs/heads/main@{#1589084}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/actor/ActorForegroundService.java
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.actor;
+
+import org.chromium.build.annotations.IdentifierNameString;
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.chrome.browser.base.SplitCompatService;
+
+/**
+ * Foreground service for managing actor tasks. This class is a wrapper that loads {@link
+ * ActorForegroundServiceImpl}.
+ */
+@NullMarked
+public class ActorForegroundService extends SplitCompatService {
+    @SuppressWarnings("FieldCanBeFinal") // @IdentifierNameString requires non-final
+    private static @IdentifierNameString String sImplClassName =
+            "org.chromium.chrome.browser.actor.ActorForegroundServiceImpl";
+
+    public ActorForegroundService() {
+        super(sImplClassName);
+    }
+}
