8953627cb09299c532e406f48d7f03e8bf8eaec6
Zeyad El-arabaty <zelarabaty@google.com>
2026-02-24 01:23:40 -0800

    Add AccessibilityAnnotatorDataProviderFactory
    This change introduces AccessibilityAnnotatorDataProviderFactory as a
    ProfileKeyedServiceFactory in chrome/browser. It provides instances of
    AccessibilityAnnotatorDataProviderImpl, a stub implementation of the
    data provider interface located in
    components/accessibility_annotator/core. The factory is initialized
    during browser startup, and unit tests are included for both the factory
    and the stub implementation.
    
    Bug: 482087708
    Change-Id: Ic1947ee399aa615bfa1620c7e5c3c429fcd2a968
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7600092
    Commit-Queue: Zeyad El-arabaty <zelarabaty@google.com>
    Reviewed-by: Andrew Vuong <akvuong@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1589241}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/accessibility/accessibility_labels_service_factory.h"
 #include "chrome/browser/accessibility/page_colors_controller_factory.h"
+#include "chrome/browser/accessibility_annotator/accessibility_annotator_data_provider_factory.h"
 #include "chrome/browser/affiliations/affiliation_service_factory.h"
 #include "chrome/browser/ai/ai_data_keyed_service_factory.h"
 #include "chrome/browser/autocomplete/aim_eligibility_service_factory.h"
@@ -665,6 +666,7 @@ void ChromeBrowserMainExtraPartsProfiles::
   AboutSigninInternalsFactory::GetInstance();
   AboutThisSiteServiceFactory::GetInstance();
   AcceptLanguagesServiceFactory::GetInstance();
+  AccessibilityAnnotatorDataProviderFactory::GetInstance();
   AccessibilityLabelsServiceFactory::GetInstance();
 #if !BUILDFLAG(IS_ANDROID)
   accessibility_annotator::ContentAnnotatorServiceFactory::GetInstance();
