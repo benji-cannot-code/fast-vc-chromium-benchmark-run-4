da5aa6edd6dacad95326c115eafe500aff15a68d
Duncan Mercer <mercerd@google.com>
2026-02-23 17:39:09 -0800

    [Contextual Tasks] More composebox server positioning fixes
    After E2E testing, these changes were needed to fix the server side
    positioning of the composebox:
     - Move the forced styles onto `contextual-tasks-composebox` instead of `cr-composebox`
     - Reset the bounds on hard navigation
     - Update the server if there is a mismatch in the server reported height
     - Adjust the composebox positioning to be relative to the viewport instead of the <webview>
    
    Bug: b:483485787
    Change-Id: I5febaf30d3e19a094ebffec1d72082f83d4a373b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7597607
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Auto-Submit: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1589068}

--- chrome/browser/resources/contextual_tasks/composebox.html.ts
+++ chrome/browser/resources/contextual_tasks/composebox.html.ts
@@ -37,7 +37,6 @@ export function getHtml(this: ContextualTasksComposeboxElement) {
       ` : ''}
       <cr-composebox
           id="composebox"
-          style="${this.getComposeboxBoundsStyles_()}"
           ?autofocus="${false}"
           carousel-on-top_
           entrypoint-name="ContextualTasks"
