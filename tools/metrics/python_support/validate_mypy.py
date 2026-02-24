52584ab078b3fd1c3485463a9c190449dc5cbe89
Andrzej Fiedukowicz <afie@google.com>
2026-02-24 01:09:24 -0800

    Add mypy checks to presubmit
    This adds static typing checks for python using mypy as part of the
    PRESUBMIT runs. This will probably catch many of the issues we have
    recently especially in files that are already type annotated.
    
    We can improve type annotation over time to improve its efficiency.
    
    As there are already hundreds of issues detected I added them all as
    exceptions so we can slowly eliminate them over time. The
    mypy_ignore_list.txt is intentionally specific about the lines to
    encourage people to fix stuff as they go, but of course we can make it
    not care about specific lines to make it less fragile.
    
    The mypy_ignore_list.txt also gives the idea of what kind of issues are
    detected.
    
    The way it is achieved is by calling mypy in a separate process. As
    standard depot tools venv doesn't have mypy installed, we also add our
    own .vpython3 file to include it and run the mypy using the combination
    of our vpython and the root one (src/.vpython3).
    
    Bug: 482274154
    Change-Id: I0d2b29c1f7a3080dab0c91f07ff3102d16069d21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7566771
    Commit-Queue: Andrzej Fiedukowicz <afie@google.com>
    Reviewed-by: Ramon Cano Aparicio <rcanoaparicio@google.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1589231}

---
+++ tools/metrics/python_support/validate_mypy.py
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2026 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Helper script allowing to run a MyPy analysis as a standalone script."""
+
+import os
+import sys
+
+import setup_modules
+import chromium_src.tools.metrics.python_support.mypy_helpers as mypy_helpers
+
+if __name__ == '__main__':
+  mypy_errors = mypy_helpers.run_mypy_and_filter_irrelevant(
+      os.path.dirname(os.path.dirname(__file__)))
+  print("\n".join(f" * {error}" for error in mypy_errors))
+
+  sys.exit(len(mypy_errors))
