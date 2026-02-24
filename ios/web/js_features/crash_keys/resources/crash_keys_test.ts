62cc40f091299ec16ce454cc0235beaf1b24f16e
Alexandra Pereira <alexandrasp@google.com>
2026-02-24 14:44:50 -0800

    [iOS] Add TypeScript API for JavaScript crash keys
    This CL introduces a TypeScript API to manage crash keys directly from JavaScript features. These crash keys are stored in a map on the
    document and are intended to be used to report feature-specific state to the crash reporting system.
    
    Bug: 479244285
    Change-Id: If9c914262092cceeaff6fd368ff346b8a15aac63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7562474
    Commit-Queue: Alexandra Pereira <alexandrasp@google.com>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1589719}

---
+++ ios/web/js_features/crash_keys/resources/crash_keys_test.ts
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Test script to expose accessibility of crash_keys functions.
+ */
+
+import {clearAllCrashKeys, clearCrashKey, getCrashKeys, setCrashKey} from '//ios/web/js_features/crash_keys/resources/crash_keys.js';
+import {CrWebApi, gCrWeb} from '//ios/web/public/js_messaging/resources/gcrweb.js';
+
+const crash_keys_tests = new CrWebApi('crash_keys_tests');
+
+gCrWeb.registerApi(crash_keys_tests);
+
+crash_keys_tests.addFunction(
+    'getCrashKeys', () => Object.fromEntries(getCrashKeys()));
+crash_keys_tests.addFunction('setCrashKey', setCrashKey);
+crash_keys_tests.addFunction('clearCrashKey', clearCrashKey);
+crash_keys_tests.addFunction('clearAllCrashKeys', clearAllCrashKeys);
