ebb239b499fe30fff6930be64ed694972f5bd0f9
Rebecca <qihuizhao@google.com>
2026-02-24 12:34:13 -0800

    Add source_feature to payments_autofill_client
    This CL adds source_feature to payments_autofill_client so that
    confirmation button text can be set correct for scan card save
    and fill flow
    
    Bug: 485262126
    Change-Id: Ic24673fa321f9b7c07a85a79d7243a5171033140
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7603239
    Reviewed-by: Olivia Saul <osaul@google.com>
    Commit-Queue: Qihui Zhao <qihuizhao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1589637}

--- components/autofill/core/browser/payments/autofill_save_card_ui_info.cc
+++ components/autofill/core/browser/payments/autofill_save_card_ui_info.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace autofill {
 
 using CardSaveType = payments::PaymentsAutofillClient::CardSaveType;
+using SourceFeature = payments::PaymentsAutofillClient::SourceFeature;
 
 AutofillSaveCardUiInfo::AutofillSaveCardUiInfo() = default;
 AutofillSaveCardUiInfo::~AutofillSaveCardUiInfo() = default;
@@ -48,6 +49,10 @@ static std::u16string GetConfirmButtonText(
     }
   }
 #elif BUILDFLAG(IS_IOS)
+  if (options.source_feature == SourceFeature::kScanCardSaveAndFill) {
+    // TODO(bug.com/485262126): Update the confirm button text for scan card
+    // flow.
+  }
   switch (options.card_save_type) {
     case CardSaveType::kCardSaveOnly:
     case CardSaveType::kCardSaveWithCvc: {
@@ -168,7 +173,8 @@ AutofillSaveCardUiInfo AutofillSaveCardUiInfo::CreateForLocalSave(
   // On iOS, the UI (infobar vs. bottom sheet) and title are determined by
   // whether the feature is enabled and the card's strike count.
   is_for_bottom_sheet = ShouldShowSaveCardBottomSheet(
-      options.card_save_type, options.num_strikes.value_or(0),
+      options.card_save_type, options.source_feature,
+      options.num_strikes.value_or(0),
       /*should_request_name_from_user=*/false,
       /*should_request_expiration_date_from_user=*/false);
 
@@ -308,8 +314,8 @@ AutofillSaveCardUiInfo AutofillSaveCardUiInfo::CreateForUploadSave(
   }
 #elif BUILDFLAG(IS_IOS)
   is_for_bottom_sheet = ShouldShowSaveCardBottomSheet(
-      options.card_save_type, options.num_strikes.value_or(0),
-      options.should_request_name_from_user,
+      options.card_save_type, options.source_feature,
+      options.num_strikes.value_or(0), options.should_request_name_from_user,
       options.should_request_expiration_date_from_user);
   switch (options.card_save_type) {
     case CardSaveType::kCardSaveWithCvc: {
@@ -368,12 +374,14 @@ AutofillSaveCardUiInfo AutofillSaveCardUiInfo::CreateForUploadSave(
 #if BUILDFLAG(IS_IOS)
 bool ShouldShowSaveCardBottomSheet(
     CardSaveType card_save_type,
+    SourceFeature source_feature,
     int num_strikes,
     bool should_request_name_from_user,
     bool should_request_expiration_date_from_user) {
-  return card_save_type != CardSaveType::kCvcSaveOnly && num_strikes == 0 &&
-         !should_request_name_from_user &&
-         !should_request_expiration_date_from_user;
+  return source_feature == SourceFeature::kScanCardSaveAndFill ||
+         (card_save_type != CardSaveType::kCvcSaveOnly && num_strikes == 0 &&
+          !should_request_name_from_user &&
+          !should_request_expiration_date_from_user);
 }
 #endif  // BUILDFLAG(IS_IOS)
 
--- components/autofill/core/browser/payments/payments_autofill_client.h
+++ components/autofill/core/browser/payments/payments_autofill_client.h
@@ -158,6 +158,13 @@ class PaymentsAutofillClient : public RiskDataLoader {
     kCvcSaveOnly = 2,
   };
 
+  enum class SourceFeature {
+    // Default behavior for standard upload or local save.
+    kOfferSaveAfterFormSubmit,
+    // Triggered from the "Scan Card" flow.
+    kScanCardSaveAndFill,
+  };
+
   // Used for options of upload prompt.
   struct SaveCreditCardOptions {
     SaveCreditCardOptions& with_should_request_name_from_user(bool b) {
@@ -197,6 +204,11 @@ class PaymentsAutofillClient : public RiskDataLoader {
       return *this;
     }
 
+    SaveCreditCardOptions& with_source_feature(SourceFeature feature) {
+      source_feature = feature;
+      return *this;
+    }
+
     bool should_request_name_from_user = false;
     bool should_request_expiration_date_from_user = false;
     bool show_prompt = false;
@@ -205,6 +217,7 @@ class PaymentsAutofillClient : public RiskDataLoader {
         false;
     std::optional<int> num_strikes;
     CardSaveType card_save_type = CardSaveType::kCardSaveOnly;
+    SourceFeature source_feature = SourceFeature::kOfferSaveAfterFormSubmit;
   };
 
   enum class SaveCardOfferUserDecision {
