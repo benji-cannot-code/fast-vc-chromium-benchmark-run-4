feeb26a0a516335c6fc52ca870581ac534ee6b23
Benjamin Williams <bwwilliams@google.com>
2026-02-24 08:42:39 -0800

    [iOS] Re-enable -testLongPressAndHide
    This change re-enables -testLongPressAndHide, which was previously
    flaking because the Safe Browsing user preference was not explicitly
    disabled.
    
    Fixed: 475843513
    Change-Id: I2b4a77e1c16deeb8ab127ad10ceae29877f98f1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7604134
    Reviewed-by: Briana McClure <bmcclure@google.com>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1589444}

--- ios/chrome/browser/content_suggestions/safety_check/test/safety_check_view_egtest.mm
+++ ios/chrome/browser/content_suggestions/safety_check/test/safety_check_view_egtest.mm
@@ -97,10 +97,9 @@ void ScrollToSafetyCheckModule() {
   return config;
 }
 
-// TODO(crbug.com/475843513): Re-enable this test.
 // Tests that long pressing the Safety Check view displays a context menu; tests
 // the Safety Check view is properly hidden via the context menu.
-- (void)DISABLED_testLongPressAndHide {
+- (void)testLongPressAndHide {
   // Enable relevant preferences for the test.
   [ChromeEarlGrey
       setBoolValue:YES
@@ -108,6 +107,7 @@ void ScrollToSafetyCheckModule() {
 
   // Intentionally forces a Safety Check error to ensure module visibility in
   // the Magic Stack.
+  [ChromeEarlGrey setBoolValue:NO forUserPref:prefs::kSafeBrowsingEnabled];
   [ChromeEarlGrey
          setStringValue:NameForSafetyCheckState(
                             SafeBrowsingSafetyCheckState::kUnsafe)
