f44c784588d29a7a0d40dd5939cac0a5794f48a2
Zac Moreland <morez@google.com>
2026-02-24 12:39:05 -0800

    Replace cr-toolbar with a custom header in browser-switch internals.
    This change removes the dependency on cr-toolbar and replaces it with a
    custom `<header>` element. The custom header includes the page title
    and the "Export to JSON" button. The search bar provided by cr-toolbar
    was not intended to be shown on this page, and "show-search" was set to
    false, however there appears to be a bug due to the cr-toolbar
    defaulting "show search" variable to true. The absence of the
    "show-search" boolean property does not set it to false but rather
    defers to the default which is true. Since cr-toolbar could not be used
    in hide-search mode without either a breaking change or a hacky
    workaround, it makes more sense to construct the browser-switch page
    using raw divs instead.
    
    Before: https://screenshot.googleplex.com/4aq3yfWdQecG45v
    
    After: https://screenshot.googleplex.com/4hkBfDu25VBhEV6
    
    Bug: 40200942
    
    Change-Id: I10d0e4c87838c077c4edad5efe23a83dbef7c175
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7603016
    Commit-Queue: Zac Moreland <morez@google.com>
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1589641}

--- chrome/browser/resources/browser_switch/internals/app.ts
+++ chrome/browser/resources/browser_switch/internals/app.ts
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import '/strings.m.js';
-import 'chrome://resources/cr_elements/cr_toolbar/cr_toolbar.js';
 import 'chrome://resources/cr_elements/cr_button/cr_button.js';
 import 'chrome://resources/cr_elements/cr_input/cr_input.js';
 
@@ -48,7 +47,6 @@ export class AppElement extends AppElementBase {
   static override get properties() {
     return {
       isBrowserSwitcherEnabled_: {type: Boolean},
-      showSearch_: {type: Boolean},
       lastFetch_: {type: String},
       nextFetch_: {type: String},
       urlCheckerInput_: {type: String},
@@ -60,7 +58,6 @@ export class AppElement extends AppElementBase {
   }
 
   protected accessor isBrowserSwitcherEnabled_: boolean = true;
-  protected accessor showSearch_: boolean = false;
   protected accessor greyListRules_: RuleItem[] = [];
   protected accessor siteListRules_: RuleItem[] = [];
   protected accessor xmlSiteLists_: XmlSiteListItem[] = [];
