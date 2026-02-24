d49f1fc3c7eb5d475bc3f8657c10f8a6afd4f6e0
Kevin Ellis <kevers@google.com>
2026-02-24 05:26:12 -0800

    Cancel composited animation affecting important property
    An animation affecting a important property still runs, but is
    unable to run on the compositor as it doesn't know about
    composite ordering. The problem is that after a composited animation
    starts on the compositor, if the base style value is then marked as
    important, the we need to stop the animation on the compositor.
    It is to still run on main as it still generates observable events.
    
    Halting an animation on the compositor when the property becomes
    important was sufficient for transform animations, but not for
    opacity. The property tree builder has a fast path for opacity that
    can be taken even if the animation is not to be composited. An
    additional step was needed for opacity animations to ensure that
    the fast path isn't used when the opacity property is marked as
    important.
    
    Note: though the animation still needs to run on main, it would
    only need to tick every frame if a CSS transition. CSS animations
    and programmatic animation effects will not be applied during the
    style cascade if the base property is marked important. Logged as
    crbug.com/486155924. Though this may seem like an obscure edge case,
    it may be common for uses who have prefers-reduced-motion set.
    
    
    Bug: 476652058
    Change-Id: I5e2aeaabe26c845d26c3872168177826f794a3e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7532839
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1589334}

--- third_party/blink/renderer/core/css/resolver/style_resolver.cc
+++ third_party/blink/renderer/core/css/resolver/style_resolver.cc
@@ -2620,6 +2620,15 @@ bool StyleResolver::ApplyAnimatedStyle(
     DCHECK(!state.GetFontBuilder().FontDirty());
   }
 
+  if (ElementAnimations* animations =
+          animating_element->GetElementAnimations()) {
+    if (StyleBaseData* base_data = state.StyleBuilder().BaseData()) {
+      if (const CSSBitset* important_set = base_data->GetBaseImportantSet()) {
+        animations->CancelCompositedAnimationsAffectingProperties(
+            *important_set);
+      }
+    }
+  }
   CSSAnimations::CalculateCompositorAnimationUpdate(
       state.AnimationUpdate(), *animating_element, element,
       *state.StyleBuilder().GetBaseComputedStyle(), state.ParentStyle(),
--- third_party/blink/renderer/core/paint/compositing/compositing_reason_finder.cc
+++ third_party/blink/renderer/core/paint/compositing/compositing_reason_finder.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/paint/compositing/compositing_reason_finder.h"
 
 #include "third_party/blink/renderer/core/css/css_property_names.h"
+#include "third_party/blink/renderer/core/css/properties/css_bitset.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/node.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
@@ -510,8 +511,19 @@ CompositingReasons CompositingReasonFinder::CompositingReasonsForAnimation(
   if (style.HasCurrentTranslateAnimation() &&
       ObjectTypeSupportsCompositedTransformAnimation(object))
     reasons |= CompositingReason::kActiveTranslateAnimation;
-  if (style.HasCurrentOpacityAnimation())
-    reasons |= CompositingReason::kActiveOpacityAnimation;
+  // Opacity needs an additional check that the base value for opacity is not
+  // marked as important. The compositor does not know about the effect
+  // of an important property on composite ordering, and it is unsafe to use
+  // the fast path even when updating opacity from main. Other properties do not
+  // require the same added safeguard since they don't have the same fast path
+  // mechanics for deferred updates from main.
+  if (style.HasCurrentOpacityAnimation()) {
+    const CSSBitset* important_properties = style.GetBaseImportantSet();
+    if (!important_properties ||
+        !important_properties->Has(CSSPropertyID::kOpacity)) {
+      reasons |= CompositingReason::kActiveOpacityAnimation;
+    }
+  }
   if (style.HasCurrentFilterAnimation())
     reasons |= CompositingReason::kActiveFilterAnimation;
   if (style.HasCurrentBackdropFilterAnimation())
