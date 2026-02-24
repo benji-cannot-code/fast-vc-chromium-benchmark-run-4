40f1938150c44461eb34253ea8668a452a2acae5
Kent Tamura <tkent@chromium.org>
2026-02-24 01:31:18 -0800

    WTF: Use EqualIgnoringAsciiCase() in renderer/modules/
    instead of EqualIgnoringASCIICase().
    
    This CL has no behavior changes.
    
    Bug: 473854537
    Change-Id: I4504368322aa5ecf50f1ab2b5a1bb6b9abcc6b31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7593619
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1589246}

--- third_party/blink/renderer/modules/manifest/manifest_manager.cc
+++ third_party/blink/renderer/modules/manifest/manifest_manager.cc
@@ -359,7 +359,7 @@ bool ManifestManager::ManifestUseCredentials() const {
       GetSupplementable()->document()->LinkManifest();
   if (!link_element)
     return false;
-  return EqualIgnoringASCIICase(
+  return EqualIgnoringAsciiCase(
       link_element->FastGetAttribute(html_names::kCrossoriginAttr),
       "use-credentials");
 }
--- third_party/blink/renderer/modules/media_capabilities/media_capabilities.cc
+++ third_party/blink/renderer/modules/media_capabilities/media_capabilities.cc
@@ -258,7 +258,7 @@ bool IsValidMimeType(const String& content_type,
   if (parameters.ParameterCount() == 0)
     return true;
 
-  return EqualIgnoringASCIICase(parameters.begin()->name, kCodecsMimeTypeParam);
+  return EqualIgnoringAsciiCase(parameters.begin()->name, kCodecsMimeTypeParam);
 }
 
 bool IsValidMediaConfiguration(const MediaConfiguration* configuration) {
