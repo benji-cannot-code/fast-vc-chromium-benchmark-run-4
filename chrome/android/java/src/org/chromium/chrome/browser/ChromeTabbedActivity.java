ec387b2b3e097bf7bb7d77af1f3570fd1f9c0c12
Brandon Fong <bjfong@google.com>
2026-02-24 09:12:31 -0800

    [tips] Add UI and button testing for the create a tab group notif
    Add the promo UI and internals page button testing for creating a tab
    group notification.
    
    Bug: 481396156
    Change-Id: If4ef2bcb1df66ba12ed4019d0cbd9f55806ba723
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7601638
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1589467}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -2767,6 +2767,7 @@ public class ChromeTabbedActivity extends ChromeActivity implements PreAttachInt
                                     getWindowAndroid(),
                                     getCurrentTabModel().isIncognito(),
                                     getProfileProviderSupplier().get().getOriginalProfile(),
+                                    mLayoutManager,
                                     fromTipsNotifications);
                     mTipsPromoCoordinator.showBottomSheet();
                 }
--- chrome/browser/notifications/scheduler/internal/stats.cc
+++ chrome/browser/notifications/scheduler/internal/stats.cc
@@ -59,6 +59,8 @@ std::string ToHistogramTipsFeatureSuffix(
       return ".PasswordAutofill";
     case TipsNotificationsFeatureType::kSignin:
       return ".Signin";
+    case TipsNotificationsFeatureType::kCreateTabGroups:
+      return ".CreateTabGroups";
     default:
       NOTREACHED();
   }
