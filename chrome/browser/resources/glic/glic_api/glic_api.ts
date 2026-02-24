286859e736ddc57cf129fff50e58662f04b26ed8
ahmedmoussa <ahmedmoussa@google.com>
2026-02-23 20:13:04 -0800

    Add hasMediaTranscripts to FrameMetada in glic_api
    This flag was added to `ai_page_content_metadata.mojom` here
    https://chromium-review.googlesource.com/c/chromium/src/+/7573532/5/third_party/blink/public/mojom/content_extraction/ai_page_content_metadata.mojom
    
    Bug: 482435623
    Change-Id: Idac064e38af7017a2d028c605d500e137789ac24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7601373
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Commit-Queue: Ahmed Moussa <ahmedmoussa@google.com>
    Cr-Commit-Position: refs/heads/main@{#1589142}

--- chrome/browser/resources/glic/glic_api/glic_api.ts
+++ chrome/browser/resources/glic/glic_api/glic_api.ts
@@ -1573,6 +1573,8 @@ export declare interface MetaTag {
 export declare interface FrameMetadata {
   url: string;
   metaTags: MetaTag[];
+  /** Whether chrome has audio/video transcripts for this frame. */
+  hasMediaTranscripts?: boolean;
 }
 
 /** Metadata about the page.  Includes URL and meta tags for each frame. */
