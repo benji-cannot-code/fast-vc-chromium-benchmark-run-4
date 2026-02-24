9a392bb9a419b5b195a9c0c5e4f20c0a6a06afb6
Bin Li <bingoli20000@gmail.com>
2026-02-24 10:02:57 -0800

    Fixed a use-after-move issue
    Change-Id: I6f10f92e72f9916b97fccd9fde4b42a7524a9a95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7601714
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1589513}

--- ui/snapshot/snapshot.cc
+++ ui/snapshot/snapshot.cc
@@ -71,6 +71,7 @@ void GrabWindowSnapshotAndScale(gfx::NativeWindow window,
                          GrabSnapshotImageCallback callback, gfx::Image image) {
     if (image.IsEmpty()) {
       std::move(callback).Run(image);
+      return;
     }
 
     base::ThreadPool::PostTaskAndReplyWithResult(
