2c6e5f433da7e803ff6c33c9f40ca39cbf944f56
Tim Judkins <tjudkins@chromium.org>
2026-02-25 11:25:44 -0800

    Add integer value extended attribute support to the IDLParser
    Integer values are described as valid extended attribute values in the
    WebIDL spec: https://webidl.spec.whatwg.org/#idl-extended-attributes
    This CL adds support for them to the IDLParser at //tools/idl_parser/
    along with an associated test.
    
    This being added to enable the conversion of some Extensions API schema
    files (which use integer value extended attributes) from an older
    outdated IDL format to use modern WebIDL.
    
    Fixed: 487349972
    Change-Id: Ib0d0a0d76c8ca016ac082b36b0df0787bd2f350f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7604485
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1590310}

--- tools/idl_parser/idl_parser.py
+++ tools/idl_parser/idl_parser.py
@@ -1051,11 +1051,13 @@ class IDLParser(object):
   #    [ identifier ]
   #    [ identifier ( ArgumentList ) ]
   #    [ identifier = identifier ]
+  #    [ identifier = * ]
   #    [ identifier = ( IdentifierList ) ]
   #    [ identifier = identifier ( ArgumentList ) ]
+  #    [ identifier = integer ]
   #    [ identifier = StringLiteral ]
   #    [ identifier = ( StringList ) ]
-  # The first five patterns are specified in the Web IDL spec and the last two
+  # The first seven patterns are specified in the Web IDL spec and the last two
   # patterns are Blink's custom extension to support [ReflectOnly].
   def p_ExtendedAttribute(self, p):
     """ExtendedAttribute : ExtendedAttributeNoArgs
@@ -1064,6 +1066,7 @@ class IDLParser(object):
                          | ExtendedAttributeWildcard
                          | ExtendedAttributeIdentList
                          | ExtendedAttributeNamedArgList
+                         | ExtendedAttributeInteger
                          | ExtendedAttributeStringLiteral
                          | ExtendedAttributeStringLiteralList"""
     p[0] = p[1]
@@ -1112,6 +1115,11 @@ class IDLParser(object):
     """ExtendedAttributeNamedArgList : identifier '=' identifier '(' ArgumentList ')'"""
     args = self.BuildProduction('Arguments', p, 4, p[5])
     value = self.BuildNamed('Call', p, 3, args)
+    p[0] = self.BuildNamed('ExtAttribute', p, 1, value)
+
+  def p_ExtendedAttributeInteger(self, p):
+    """ExtendedAttributeInteger : identifier '=' integer"""
+    value = self.BuildAttribute('VALUE', p[3])
     p[0] = self.BuildNamed('ExtAttribute', p, 1, value)
 
   # Blink extension: Add support for string literal Extended Attribute values
