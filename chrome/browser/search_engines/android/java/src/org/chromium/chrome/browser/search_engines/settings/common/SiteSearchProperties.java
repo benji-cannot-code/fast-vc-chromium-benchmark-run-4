576f249031d3f9e07991227d9b21e20ffae8dd1b
Kay Lin <kaiyilin@google.com>
2026-02-24 19:44:07 -0800

    Site Search: Refactor to extract reusable Properties
    This CL extracts the Properties and ViewBinder to :common so we can
    reduce the boilerplate code for the properties setup.
    
    Bug: 479395655
    Change-Id: Ib10a0fc42d45e2b866fd536ef13e50d376284549
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7596741
    Commit-Queue: Kay Lin <kaiyilin@google.com>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1589890}

--- chrome/browser/search_engines/android/java/src/org/chromium/chrome/browser/search_engines/settings/common/SiteSearchProperties.java
+++ chrome/browser/search_engines/android/java/src/org/chromium/chrome/browser/search_engines/settings/common/SiteSearchProperties.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.search_engines.settings.custom_site_search;
+package org.chromium.chrome.browser.search_engines.settings.common;
 
 import android.graphics.Bitmap;
 import android.view.View.OnClickListener;
@@ -19,7 +19,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @NullMarked
-class CustomSiteSearchProperties {
+public class SiteSearchProperties {
     @IntDef({ViewType.SEARCH_ENGINE, ViewType.ADD, ViewType.MORE})
     @Retention(RetentionPolicy.SOURCE)
     public @interface ViewType {
