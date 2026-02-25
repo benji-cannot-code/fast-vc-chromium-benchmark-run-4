ce22785508775377946d03b7eb3724db06e03be3
Roger McFarlane <rogerm@chromium.org>
2026-02-25 00:32:09 -0800

    IWYU - fake_footprints_fetcher.h
    This CL adds an include for base/containers/flat_map.h, which is used
    but not directly included.
    
    Change-Id: I8163da22c500a61188d5dcd0614b6946be1fbd1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7604406
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Auto-Submit: Roger McFarlane <rogerm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1589979}

--- ash/quick_pair/repository/fast_pair/fake_footprints_fetcher.h
+++ ash/quick_pair/repository/fast_pair/fake_footprints_fetcher.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/quick_pair/proto/fastpair.pb.h"
 #include "ash/quick_pair/repository/fast_pair/footprints_fetcher.h"
+#include "base/containers/flat_map.h"
 #include "base/functional/callback.h"
 
 namespace ash {
