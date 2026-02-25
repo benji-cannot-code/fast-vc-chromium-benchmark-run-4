1017b2e6646fe48a6b5931a46c7c05c8d8a5e74e
Muhammad Salmaan <musalmaan@chromium.org>
2026-02-25 12:44:53 -0800

    [Search Integrity] IWYU
    Change-Id: I0cd6bd943e8fe5fe9e982bc3c0a539ea2ef17023
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7608569
    Commit-Queue: Muhammad Salmaan <musalmaan@chromium.org>
    Reviewed-by: Atharv Maan <atharvmaan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1590355}

--- chrome/browser/search_integrity/search_integrity_factory.cc
+++ chrome/browser/search_integrity/search_integrity_factory.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/search_integrity/search_integrity_factory.h"
 
+#include <memory>
+
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
 #include "chrome/browser/search_integrity/search_integrity.h"
