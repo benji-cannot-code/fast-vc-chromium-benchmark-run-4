909c3b5b331bdbd36ceacf5d98ecd3cd08da81b8
Roger McFarlane <rogerm@chromium.org>
2026-02-24 19:25:44 -0800

    IWYU - chromeos/ash/experiences/system_web_apps/types/system_web_app_delegate.h
    This CL explicitly includes <map> and <optional> in
    chromeos/ash/experiences/system_web_apps/types/system_web_app_delegate.h
    
    Change-Id: I26a7ad0fa859532e3e9c5219acfafb07c4d56a9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7603962
    Commit-Queue: Roger McFarlane <rogerm@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1589874}

--- chromeos/ash/experiences/system_web_apps/types/system_web_app_delegate.h
+++ chromeos/ash/experiences/system_web_apps/types/system_web_app_delegate.h
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROMEOS_ASH_EXPERIENCES_SYSTEM_WEB_APPS_TYPES_SYSTEM_WEB_APP_DELEGATE_H_
 #define CHROMEOS_ASH_EXPERIENCES_SYSTEM_WEB_APPS_TYPES_SYSTEM_WEB_APP_DELEGATE_H_
 
+#include <map>
 #include <memory>
+#include <optional>
 #include <string>
 #include <vector>
 
