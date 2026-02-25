781e8be6d59ec3f5040a5e35183caf37f7750aac
Joemer Ramos <joemerramos@google.com>
2026-02-25 12:48:20 -0800

    [iOS] Show Zero State When A New Tab Is Created For Copresence Arm
    When GeminiCopresenceZeroStateWithChatHistory is enabled, the zero state
    should be shown when a new tab is created. This CL also changes how
    metrics are logged when a user is dismissing a floaty. Before, we only
    cared about how often a user dismissed the floaty in a new minimized
    state. But now, it would be good to know if this new behavior is
    affecting the number of times the floaty is uninvoked even when in the
    expanded state.
    
    UI Video:
    https://drive.google.com/file/d/1EK-0k3prxQd0pti7snAgOWbEaxJQiSP5/view?usp=drive_link&resourcekey=0-6mdVHkHOhpOCEDeTBhokZA
    
    Fixed: 483069689
    Change-Id: Ie6f2a603462649bd9a687b4566babe539c224dcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7601199
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Joemer Ramos <joemerramos@google.com>
    Cr-Commit-Position: refs/heads/main@{#1590357}

--- ios/chrome/browser/intelligence/bwg/metrics/gemini_metrics.mm
+++ ios/chrome/browser/intelligence/bwg/metrics/gemini_metrics.mm
@@ -158,6 +158,9 @@ const char kFloatyShownFromSourceHistogram[] =
 const char kFloatyHiddenFromSourceHistogram[] =
     "IOS.Gemini.Floaty.HiddenFromSource";
 
+const char kFloatyDismissedStateHistogram[] =
+    "IOS.Gemini.Floaty.DismissedState";
+
 const char kImageRemixContextMenuEntryPointAspectRatioTappedHistogram[] =
     "IOS.Gemini.ImageRemix.ContextMenuEntryPoint.AspectRatio.Tapped";
 
@@ -375,9 +378,16 @@ void RecordFloatyCollapsedToExpanded() {
       IOSGeminiViewStateTransition::kCollapsedToExpanded);
 }
 
-void RecordFloatyDismissedWhileCollapsed() {
-  base::RecordAction(
-      base::UserMetricsAction("MobileGeminiFloatyCollapsedToDismissed"));
+void RecordFloatyDismissedState(ios::provider::GeminiViewState state) {
+  base::UmaHistogramEnumeration(kFloatyDismissedStateHistogram, state);
+
+  if (state == ios::provider::GeminiViewState::kCollapsed) {
+    base::RecordAction(
+        base::UserMetricsAction("MobileGeminiFloatyCollapsedToDismissed"));
+  } else if (state == ios::provider::GeminiViewState::kExpanded) {
+    base::RecordAction(
+        base::UserMetricsAction("MobileGeminiFloatyExpandedToDismissed"));
+  }
 }
 
 void RecordFloatyMinimizedTime(base::TimeTicks elapsed_minimized_floaty_time) {
