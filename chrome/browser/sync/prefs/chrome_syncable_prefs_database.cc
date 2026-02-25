c0b7d7e783ca498349fa111e667e6ff3f55eb6e7
Kene Okoye <kenok@google.com>
2026-02-25 06:45:25 -0800

    [EnableGlicCleanup] in chrome/browser/sync
    Fixed: b:487340372
    Change-Id: I7f2c9b7017d98cb5f8ad551c4cbaa59f76e83ca6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7602706
    Reviewed-by: Jood Hajeer <jood@google.com>
    Auto-Submit: Kene Okoye <kenok@google.com>
    Commit-Queue: Kene Okoye <kenok@google.com>
    Cr-Commit-Position: refs/heads/main@{#1590120}

--- chrome/browser/sync/prefs/chrome_syncable_prefs_database.cc
+++ chrome/browser/sync/prefs/chrome_syncable_prefs_database.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/fixed_flat_map.h"
 #include "build/build_config.h"
 #include "chrome/browser/accessibility/tree_fixing/pref_names.h"
+#include "chrome/browser/glic/glic_pref_names.h"
 #include "chrome/browser/promos/promos_pref_names.h"
 #include "chrome/browser/subscription_eligibility/subscription_eligibility_prefs.h"
 #include "chrome/browser/ui/read_anything/read_anything_prefs.h"
@@ -41,10 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 #include "components/supervised_user/core/common/pref_names.h"
 #include "extensions/browser/pref_names.h"  // nogncheck
-#endif
-
-#if BUILDFLAG(ENABLE_GLIC)
-#include "chrome/browser/glic/glic_pref_names.h"
 #endif
 
 namespace browser_sync {
@@ -1901,12 +1898,10 @@ constexpr auto kChromeSyncablePrefsAllowlist = base::MakeFixedFlatMap<
      {syncable_prefs_ids::kDesktopToiOSPriceTrackingPromoOptOut,
       syncer::PREFERENCES, sync_preferences::PrefSensitivity::kNone,
       sync_preferences::MergeBehavior::kNone}},
-#if BUILDFLAG(ENABLE_GLIC)
     {glic::prefs::kGlicRolloutEligibility,
      {syncable_prefs_ids::kGlicRolloutEligibility, syncer::PRIORITY_PREFERENCES,
       sync_preferences::PrefSensitivity::kExemptFromUserControlWhileSignedIn,
       sync_preferences::MergeBehavior::kNone}},
-#endif  // BUILDFLAG(ENABLE_GLIC)
     {subscription_eligibility::prefs::kAiSubscriptionTier,
      {syncable_prefs_ids::kSubscriptionEligibilityAiSubscriptionTier,
       syncer::PRIORITY_PREFERENCES,
