2dbd5b6b65a8e73ea84eb786833c30e95bac324b
Joe Downing <joedow@google.com>
2026-02-24 18:24:39 -0800

    Remove XmlElement from /remoting/host
    Bug: 359620500
    Change-Id: I6076ba97a3f23fc67e9cee176b0d5486c859fecb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7603443
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Auto-Submit: Joe Downing <joedow@chromium.org>
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1589842}

--- remoting/host/pam_authorization_factory_posix.cc
+++ remoting/host/pam_authorization_factory_posix.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/host/base/username.h"
 #include "remoting/host/pam_utils.h"
 #include "remoting/protocol/channel_authenticator.h"
-#include "third_party/libjingle_xmpp/xmllite/xmlelement.h"
 
 namespace remoting {
 
