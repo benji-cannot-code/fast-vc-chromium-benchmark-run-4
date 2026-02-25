d8b52b61b75e6b9e6caa9e831a4a0b85a989cfa5
Yuwei Huang <yuweih@chromium.org>
2026-02-24 18:37:45 -0800

    [crd host] Allow calling SetRequiredUsername() with the same username
    For multi-process hosts, when there is an active CRD connection and the
    user attempts to connect again from a new tab or device,
    HostProcess::OnSessionPoliciesReceived will try to call
    SetRequiredUsername and the CHECK will crash the host. The expected
    behavior is that the old connection should be terminated and the new one
    should succeed.
    
    This CL fixes this by early-returning if `username` already matches
    `required_username_`.
    
    Bug: 475611769
    Change-Id: I2e0d07a6e8c553082b44da7532c5829de77d44d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7605566
    Auto-Submit: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1589849}

--- remoting/host/ipc_desktop_environment.cc
+++ remoting/host/ipc_desktop_environment.cc
@@ -253,6 +253,11 @@ bool IpcDesktopEnvironmentFactory::BindConnectionEventsReceiver(
 void IpcDesktopEnvironmentFactory::SetRequiredUsername(
     std::string_view username) {
   DCHECK(network_task_runner_->BelongsToCurrentThread());
+
+  if (required_username_ == username) {
+    return;
+  }
+
   CHECK(active_connections_.empty())
       << "Cannot change required username when there are active connections.";
 
