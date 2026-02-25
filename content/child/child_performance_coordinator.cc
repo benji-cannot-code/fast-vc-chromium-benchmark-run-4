751d5cab45b99bb18e6a1069c0415c915c5c625b
Joe Mason <joenotcharles@google.com>
2026-02-25 08:44:43 -0800

    Remove expired ChildPerformanceCoordinator histograms
    Bug: 461499740
    Change-Id: I32014e4cd87d09bd36398e0553aec680b8bc3460
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7602705
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1590217}

--- content/child/child_performance_coordinator.cc
+++ content/child/child_performance_coordinator.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/memory/read_only_shared_memory_region.h"
 #include "base/memory/weak_ptr.h"
-#include "base/metrics/histogram_functions.h"
 #include "base/sequence_checker.h"
 #include "components/performance_manager/public/mojom/coordination_unit.mojom.h"
 #include "components/performance_manager/scenario_api/performance_scenarios.h"
@@ -30,9 +29,6 @@ mojo::PendingReceiver<performance_manager::mojom::ChildProcessCoordinationUnit>
 ChildPerformanceCoordinator::InitializeAndPassReceiver() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   auto receiver = coordination_unit_.BindNewPipeAndPassReceiver();
-  base::UmaHistogramBoolean(
-      "PerformanceManager.InitializeChildProcessCoordination.RequestCount",
-      true);
   coordination_unit_->InitializeChildProcessCoordination(
       base::BindOnce(
           &ChildPerformanceCoordinator::OnInitializeChildProcessCoordination,
@@ -44,9 +40,6 @@ void ChildPerformanceCoordinator::OnInitializeChildProcessCoordination(
     base::ReadOnlySharedMemoryRegion global_region,
     base::ReadOnlySharedMemoryRegion process_region) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  base::UmaHistogramBoolean(
-      "PerformanceManager.InitializeChildProcessCoordination.ResponseCount",
-      true);
   using performance_scenarios::ScenarioScope;
   if (global_region.IsValid()) {
     global_scenario_memory_.emplace(ScenarioScope::kGlobal,
