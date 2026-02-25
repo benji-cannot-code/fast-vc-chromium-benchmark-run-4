0e26493f1a5d3c7ab36a7bbaac5aee27dda38546
Ho Cheung <hocheung@chromium.org>
2026-02-25 15:58:08 -0800

    Refactor predictors GN targets and clean up LCPP unittest params
    1. Split predictors and lcp_critical_path_predictor into dedicated
    BUILD.gn files.
    
    2. Introduce layered targets (config/headers/impl/test_support)
    and update deps.
    
    3. Wire chrome/browser and related BUILD.gn files to the new target
    layout.
    
    4. Refactor lcp_critical_path_predictor_util_unittest parameterization
    to use bitmask params instead of static vector feature sets, fixing
    -Wexit-time-destructors without adding suppression.
    
    Bug: 486498788
    Change-Id: I18e561eec9214f3f9c49613916a10b3d51c04d0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7607719
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Aaron Teo <hocheung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1590485}

--- chrome/browser/predictors/loading_data_collector.cc
+++ chrome/browser/predictors/loading_data_collector.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 
-#include "chrome/browser/browser_features.h"
 #include "chrome/browser/predictors/loading_stats_collector.h"
 #include "chrome/browser/predictors/predictors_features.h"
 #include "chrome/browser/predictors/resource_prefetch_predictor.h"
