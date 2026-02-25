9a81bf7761e181120c7a89981577d8c44eabce9e
Jérôme Lebel <jlebel@chromium.org>
2026-02-25 07:32:00 -0800

    [iOS][Feedback] Remove unused code
    Remove declaration for unused methods:
    - CreateUserFeedbackViewController()
    - CanUseStartUserFeedbackFlow()
    
    - crrev.com/c/7604173 Remove calls for unused methods
    - crrev.com/i/9047956 Remove implementations
    => crrev.com/c/7603496 Remove declarations
    
    Fixed: 487567818
    Change-Id: I58a70a64d06bf463099171fae1b64a282d476580
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7603496
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1590161}

--- ios/chrome/browser/providers/user_feedback/chromium_user_feedback.mm
+++ ios/chrome/browser/providers/user_feedback/chromium_user_feedback.mm
@@ -15,16 +15,7 @@ bool IsUserFeedbackSupported() {
   return false;
 }
 
-UIViewController* CreateUserFeedbackViewController(
-    UserFeedbackConfiguration* configuration) {
-  NOTREACHED() << "User feedback reporting not supported.";
-}
-
 void UploadAllPendingUserFeedback() {
-  NOTREACHED() << "User feedback reporting not supported.";
-}
-
-bool CanUseStartUserFeedbackFlow() {
   NOTREACHED() << "User feedback reporting not supported.";
 }
 
