f86c293dab5633cd108c25ee481f7a1e5f0c7d36
Andreu Botella <abotella@igalia.com>
2026-02-24 19:24:41 -0800

    [line-clamp] Refactor computation of the line-clamp container's height
    Clamping by height (with `line-clamp: auto`) needs to figure out
    whether clamping at a particular clamp point will result in the height
    of the line-clamp container overflowing its `(max-)height`.
    
    Since we need to consider the end borders, paddings and margins of all
    ancestor boxes of the clamp point in order to figure out this height,
    this is currently done by passing down end margin data in
    `ConstraintSpace::LineClampEndMarginStrut`; and by decreasing a box's
    `clamp_bfc_offset` by its border-padding plus margin contribution.
    These data get passed down to descendants to have a complete picture
    of the end border/padding/margin contributions to the height.
    
    However, when we start considering additional complications, such as
    the effect of `{min,max}-height` descendant boxes, this way of
    computing the final height of the line-clamp container clearly becomes
    insufficient.
    
    Instead, this patch introduces the `LineClampAncestorChain` class. An
    instance of this class corresponds to either the line-clamp container
    or one of its descendant in-flow block boxes, and it contains all of
    the data needed to compute that box's contributions to the height of
    the container, as well as a link to the `LineClampAncestorChain` for
    its parent. That way, a linked set of `LineClampAncestorChain`s forms
    a (mostly) immutable linked list containing all of the information to
    turn a clamp point into the corresponding height the line-clamp
    container would have if clamped there.
    
    This patch is purely a refactor and therefore doesn't add or change
    any tests. A follow-up will use this new model to fix clamping when
    you have `{min,max}-height` descendants.
    
    Bug: 40336192
    Change-Id: I5070ce0776c84c60ba1f39d8813ab9fdbce9ef15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7566509
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Andreu Botella <abotella@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1589873}

--- third_party/blink/renderer/core/layout/constraint_space.h
+++ third_party/blink/renderer/core/layout/constraint_space.h
@@ -733,8 +733,8 @@ class CORE_EXPORT ConstraintSpace final {
     return rare_data_ ? rare_data_->GetLineClampData() : LineClampData();
   }
 
-  MarginStrut LineClampEndMarginStrut() const {
-    return rare_data_ ? rare_data_->LineClampEndMarginStrut() : MarginStrut();
+  const LineClampAncestorChain* GetLineClampAncestorChain() const {
+    return rare_data_ ? rare_data_->GetLineClampAncestorChain() : nullptr;
   }
 
   // Return true if `text-box-trim:trim-start` is in effect at the beginning of
@@ -934,7 +934,8 @@ class CORE_EXPORT ConstraintSpace final {
           is_adjacent_to_paper_edge_block_start(
               other.is_adjacent_to_paper_edge_block_start),
           is_adjacent_to_paper_edge_block_end(
-              other.is_adjacent_to_paper_edge_block_end) {
+              other.is_adjacent_to_paper_edge_block_end),
+          line_clamp_ancestor_chain_(other.line_clamp_ancestor_chain_) {
       switch (GetDataUnionType()) {
         case DataUnionType::kNone:
           break;
@@ -994,7 +995,9 @@ class CORE_EXPORT ConstraintSpace final {
       }
     }
 
-    void Trace(Visitor*) const {}
+    void Trace(Visitor* visitor) const {
+      visitor->Trace(line_clamp_ancestor_chain_);
+    }
 
     bool MaySkipLayout(const RareData& other) const {
       if (replaced_child_percentage_resolution_block_size !=
@@ -1044,7 +1047,9 @@ class CORE_EXPORT ConstraintSpace final {
               other.is_adjacent_to_paper_edge_block_start ||
           is_adjacent_to_paper_edge_block_end !=
               other.is_adjacent_to_paper_edge_block_end ||
-          ignore_margins_for_stretch != other.ignore_margins_for_stretch) {
+          ignore_margins_for_stretch != other.ignore_margins_for_stretch ||
+          !base::ValuesEquivalent(line_clamp_ancestor_chain_,
+                                  other.line_clamp_ancestor_chain_)) {
         return false;
       }
 
@@ -1094,7 +1099,7 @@ class CORE_EXPORT ConstraintSpace final {
           is_adjacent_to_paper_edge_inline_end ||
           is_adjacent_to_paper_edge_block_start ||
           is_adjacent_to_paper_edge_block_end ||
-          !ignore_margins_for_stretch.IsEmpty()) {
+          !ignore_margins_for_stretch.IsEmpty() || line_clamp_ancestor_chain_) {
         return false;
       }
 
@@ -1178,14 +1183,12 @@ class CORE_EXPORT ConstraintSpace final {
       EnsureBlockData()->line_clamp_data = value;
     }
 
-    MarginStrut LineClampEndMarginStrut() const {
-      return GetDataUnionType() == DataUnionType::kBlockData
-                 ? block_data_.line_clamp_end_margin_strut
-                 : MarginStrut();
+    const LineClampAncestorChain* GetLineClampAncestorChain() const {
+      return line_clamp_ancestor_chain_;
     }
 
-    void SetLineClampEndMarginStrut(MarginStrut value) {
-      EnsureBlockData()->line_clamp_end_margin_strut = value;
+    void SetLineClampAncestorChain(const LineClampAncestorChain* value) {
+      line_clamp_ancestor_chain_ = value;
     }
 
     void SetIsTableCell() { EnsureTableCellData(); }
@@ -1390,7 +1393,6 @@ class CORE_EXPORT ConstraintSpace final {
       std::optional<LayoutUnit> forced_bfc_block_offset;
       LayoutUnit clearance_offset = LayoutUnit::Min();
       LineClampData line_clamp_data;
-      MarginStrut line_clamp_end_margin_strut;
     };
 
     struct TableCellData {
@@ -1562,6 +1564,8 @@ class CORE_EXPORT ConstraintSpace final {
       StretchData stretch_data_;
       SubgridData subgrid_data_;
     };
+
+    Member<const LineClampAncestorChain> line_clamp_ancestor_chain_;
   };
 
   // This struct simply allows us easily copy, compare, and initialize all the
