b66523da7a04f0a970a6d80723485f0be9fccf86
Josiah Kiehl <kiehl@google.com>
2026-02-24 20:03:44 -0800

    [cobrowse] Allow empty query while DeepSearch is enabled and the query is a followup.
    Deep Search has a call to action for users to submit empty queries to
    begin a deep search session. This change allows that to happen for
    followup queries.
    
    Initial queries remain disabled without content (NTP and Omnibox queries
    are initial queries).
    
    Fixed: b:480984460
    Change-Id: If0bf2cefdb35fc3eacb50fc732c14c032914c649
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7600935
    Commit-Queue: Josiah Kiehl <kiehl@google.com>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Auto-Submit: Josiah Kiehl <kiehl@google.com>
    Cr-Commit-Position: refs/heads/main@{#1589897}

--- chrome/browser/resources/contextual_tasks/composebox.html.ts
+++ chrome/browser/resources/contextual_tasks/composebox.html.ts
@@ -61,6 +61,7 @@ export function getHtml(this: ContextualTasksComposeboxElement) {
           .isInCoBrowsingZeroState="${this.isZeroState}"
           .lensButtonTriggersOverlay="${true}"
           .enableCarouselScrolling="${true}"
+          .isFollowupQuery="${!this.isZeroState}"
           @result-changed="${this.onSuggestionsResultReceived_}"
           @open-image-upload="${this.handleImageUpload_}"
           @open-file-upload="${this.handleFileUpload_}"
