b33684542f69abb67b45cdcdda43d28b88b6aff5
Andrea Orru <andreaorru@chromium.org>
2026-02-25 10:20:05 -0800

    [Extensions] Remove SkipResetServiceWorkerURLLoaderFactories flag
    Make it the default behavior, since it has been enabled successfully
    for a long time.
    
    Bug: 408312299
    Change-Id: Ibcccd0f695d71d5026144f8b273a7cb8f11a7f8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7606059
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: Andrea Orru <andreaorru@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1590266}

--- extensions/browser/url_loader_factory_manager.cc
+++ extensions/browser/url_loader_factory_manager.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/constants.h"
 #include "extensions/common/cors_util.h"
 #include "extensions/common/extension.h"
-#include "extensions/common/extension_features.h"
 #include "extensions/common/extension_set.h"
 #include "extensions/common/manifest_handlers/permissions_parser.h"
 #include "extensions/common/mojom/host_id.mojom.h"
@@ -124,9 +123,7 @@ void OverrideFactoryParams(const Extension& extension,
                            bool is_for_service_worker,
                            FactoryUser factory_user,
                            network::mojom::URLLoaderFactoryParams* params) {
-  if (is_for_service_worker &&
-      base::FeatureList::IsEnabled(
-          extensions_features::kSkipResetServiceWorkerURLLoaderFactories)) {
+  if (is_for_service_worker) {
     CHECK_EQ(factory_user, FactoryUser::kExtensionProcess);
     params->ignore_factory_reset = true;
   }
