230ced83bb26316ede7b9b6a3b3448529f58beaf
Francis Beauchamp <fbeauchamp@google.com>
2026-02-25 07:36:54 -0800

    feat: Add GeminiPageContextAttachmentState to replace deprecated naming
    This change introduces GeminiPageContextAttachmentState as part of the
    renaming efforts for BWG code.
    
    For compatibility purposes, the old enum will remain as an alias until
    we make internal changes that conform to the new enum. Chaining of the
    value assignment ensures that values are synchronized during the period
    of transition (ending with the landing of the last CL of this chain).
    
    Associated CLs:
    => crrev.com/c/7523365
       crrev.com/i/8965936
       crrev.com/c/7522926
    
    Bug: 467341184
    Change-Id: I358dbab47a838330e10da459ce6385b3f6c6e7b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7523365
    Commit-Queue: Francis Beauchamp <fbeauchamp@google.com>
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1590167}

--- ios/chrome/browser/intelligence/bwg/model/gemini_page_context.h
+++ ios/chrome/browser/intelligence/bwg/model/gemini_page_context.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ios::provider {
 enum class GeminiPageContextComputationState;
-enum class BWGPageContextAttachmentState;
+enum class GeminiPageContextAttachmentState;
 }  // namespace ios::provider
 
 namespace optimization_guide::proto {
@@ -33,8 +33,14 @@ class PageContext;
     geminiPageContextComputationState;
 
 // The state of the BWG PageContext attachment.
-@property(nonatomic, assign)
-    ios::provider::BWGPageContextAttachmentState BWGPageContextAttachmentState;
+@property(nonatomic, assign) ios::provider::GeminiPageContextAttachmentState
+    geminiPageContextAttachmentState;
+
+// The state of the BWG PageContext attachment.
+// TODO(crbug.com/467341184): Remove this property once all callers have
+// migrated.
+@property(nonatomic, assign) ios::provider::GeminiPageContextAttachmentState
+    BWGPageContextAttachmentState;
 
 // The favicon of the attached page. Uses a default icon if it's unavailable.
 @property(nonatomic, strong) UIImage* favicon;
