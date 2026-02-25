7e22e696d4e35037297d9004d97c31d2a5c991a9
Jinsuk Kim <jinsukkim@chromium.org>
2026-02-24 16:17:10 -0800

    [Settings Search] Set the query UI bg right
    Screenshot: https://screenshot.googleplex.com/53w3fy4ZfFB6Gnh
    
    Bug: 487379411
    Change-Id: I9b47c842dd7889a6ca828917e50f78c7720adba4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7603219
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Moe Adel <adelm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1589777}

--- chrome/android/java/src/org/chromium/chrome/browser/settings/search/SettingsSearchCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/search/SettingsSearchCoordinator.java
@@ -252,6 +252,7 @@ public class SettingsSearchCoordinator
         }
         bg.setTint(tint);
         searchBox.setBackground(bg);
+        query.setBackground(bg);
         if (mMultiColumnSettings != null) {
             mHandler.post(this::initializeMultiColumnSearchUi);
         } else {
