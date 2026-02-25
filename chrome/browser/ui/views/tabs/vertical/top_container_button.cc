debec468f0df4f3efb66d5c276d74194de946de7
Alison Gale <agale@chromium.org>
2026-02-25 13:12:24 -0800

    [Vertical Tabs] Fix collapse button contrast with custom themes
    This token should be on-surface-subtle but we should actually reuse the
    tab title color which is that token but with enforcement of a contrast
    ratio with the tab strip background. This token is also used for the
    horizontal new tab button as well.
    
    https://screenshot.googleplex.com/8P63LxjVtZ8gd8n
    
    Bug: 487360959
    Change-Id: I1f4779550120967f8817aae28adb5693e19a9a9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7609010
    Commit-Queue: Alison Gale <agale@chromium.org>
    Reviewed-by: Steven Luong <stluong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1590375}

--- chrome/browser/ui/views/tabs/vertical/top_container_button.cc
+++ chrome/browser/ui/views/tabs/vertical/top_container_button.cc
@@ -69,8 +69,8 @@ void TopContainerButton::RemovedFromWidget() {
 
 ui::ColorId TopContainerButton::GetForegroundColor() const {
   return GetWidget() && GetWidget()->ShouldPaintAsActive()
-             ? kColorToolbarButtonIcon
-             : kColorToolbarButtonIconInactive;
+             ? kColorTabForegroundInactiveFrameActive
+             : kColorTabForegroundInactiveFrameInactive;
 }
 
 std::unique_ptr<views::ActionViewInterface>
