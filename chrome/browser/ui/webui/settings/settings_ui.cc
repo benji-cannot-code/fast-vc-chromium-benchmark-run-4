57ecdfa378e78af35e654283accdd18ac942d0ef
Amelie Schneider <amelies@google.com>
2026-02-25 05:01:23 -0800

    Create new flag `SearchSettingsUpdate`
    This flag will be used for restructuring the search settings pages.
    
    Bug: 487254893
    Change-Id: Ic7ac329924a2a91c203d2d2873085cfc79fc2ede
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7604093
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Amelie Schneider <amelies@google.com>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1590079}

--- chrome/browser/ui/webui/settings/settings_ui.cc
+++ chrome/browser/ui/webui/settings/settings_ui.cc
@@ -119,6 +119,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/regional_capabilities/regional_capabilities_service.h"
 #include "components/safe_browsing/core/common/features.h"
 #include "components/safe_browsing/core/common/hashprefix_realtime/hash_realtime_utils.h"
+#include "components/search_engines/search_engines_switches.h"
 #include "components/search_engines/template_url_service.h"
 #include "components/signin/public/base/signin_pref_names.h"
 #include "components/signin/public/base/signin_switches.h"
@@ -670,6 +671,10 @@ SettingsUI::SettingsUI(content::WebUI* web_ui)
   html_source->AddBoolean("unoPhase2FollowUp", base::FeatureList::IsEnabled(
                                                    syncer::kUnoPhase2FollowUp));
 #endif
+
+  html_source->AddBoolean(
+      "searchSettingsUpdate",
+      base::FeatureList::IsEnabled(switches::kSearchSettingsUpdate));
 
   TryShowHatsSurveyWithTimeout();
 }
