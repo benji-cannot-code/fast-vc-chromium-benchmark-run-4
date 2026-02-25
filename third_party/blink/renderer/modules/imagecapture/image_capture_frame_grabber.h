f01adbe21c34ab514b769673788771fae3b5bb36
Colin Blundell <blundell@chromium.org>
2026-02-25 00:19:44 -0800

    [Blink] Eliminate ImageCaptureFrameGrabber creating bitmap provider
    Have it directly create and cache the underlying SkSurface instead.
    
    Note: There is a small behavioral change here in that if the creation
    of the SkSurface fails, we will now no longer attempt to recreate it
    until the required provider info changes, as opposed to on the next
    invocation of this method. That failure can happen only due to OOM, in
    which case all bets are off anyway.
    
    Bug: 352263194
    Change-Id: I8f145c89b7740b4b6d690bf25934ad5f7ad158d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7603514
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1589976}

--- third_party/blink/renderer/modules/imagecapture/image_capture_frame_grabber.h
+++ third_party/blink/renderer/modules/imagecapture/image_capture_frame_grabber.h
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/web_callbacks.h"
 #include "third_party/blink/public/web/modules/mediastream/media_stream_video_sink.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
-#include "third_party/blink/renderer/platform/graphics/canvas_snapshot_provider.h"
+#include "third_party/blink/renderer/platform/graphics/canvas_resource_provider.h"
 #include "third_party/blink/renderer/platform/scheduler/public/post_cancellable_task.h"
 #include "third_party/skia/include/core/SkRefCnt.h"
 
@@ -61,8 +61,9 @@ class ImageCaptureFrameGrabber final : public MediaStreamVideoSink {
   TaskHandle timeout_task_handle_;
 
   media::PaintCanvasVideoRenderer video_renderer_;
-  std::unique_ptr<CanvasSnapshotProvider> snapshot_provider_;
+  std::unique_ptr<CanvasNon2DResourceProviderSharedImage> snapshot_provider_;
   std::optional<CanvasSnapshotProvider::Info> cached_draw_info_;
+  sk_sp<SkSurface> sw_draw_surface_;
 
   THREAD_CHECKER(thread_checker_);
   base::WeakPtrFactory<ImageCaptureFrameGrabber> weak_factory_{this};
