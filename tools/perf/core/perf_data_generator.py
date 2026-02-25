119b7f9c347e8027d4778ab03b050502d6c1601f
Wenbin Zhang <wenbinzhang@google.com>
2026-02-24 16:46:28 -0800

    [chromeperf] fixed missing merge script for linux-r350-processor-perf
    The merge script is not set for this processor.
    
    Currently the tester is uploading the data itself. We can let the processor
    take over the uploads once it is fixed.
    
    Bug: 485909175
    Change-Id: I7985c36ae50e46906c5eb0e8be1bb28330eae3cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7600408
    Commit-Queue: John Chen <johnchen@chromium.org>
    Auto-Submit: Wenbin Zhang <wenbinzhang@google.com>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1589796}

--- tools/perf/core/perf_data_generator.py
+++ tools/perf/core/perf_data_generator.py
@@ -164,7 +164,7 @@ UPLOAD_SKIA_JSON_BUILDERS = frozenset([
     'linux-perf',
     'linux-perf-fyi',
     'linux-perf-rel',
-    'linux-processor-perf',
+    'linux-r350-processor-perf',
     'linux-r350-perf',
     'mac-arm-builder-perf',
     'mac-builder-perf',
@@ -888,7 +888,8 @@ BUILDERS = {
                 ],
             },
         ],
-        'platform': 'linux',
+        'platform':
+        'linux',
         'dimension': {
             'gpu': '10de:2184',
             'os': 'Ubuntu-22.04',
@@ -961,14 +962,15 @@ BUILDERS = {
         'platform':
         'linux',
         'dimension': {
-            'os': 'Ubuntu-22.04.5',
+            'os':
+            'Ubuntu-22.04.5',
             'pool':
             'chrome.tests.perf',
             'synthetic_product_name':
             'System Product Name [System Version] (Falcon Northwest)',
         },
     },
-    'linux-processor-perf': {
+    'linux-r350-processor-perf': {
         'platform': 'linux',
         'perf_processor': True,
     },
@@ -1359,7 +1361,8 @@ BUILDERS = {
         'target_bits':
         64,
         'dimension': {
-            'os': 'Windows-11-26100.1742',
+            'os':
+            'Windows-11-26100.1742',
             'pool':
             'chrome.tests.perf',
             'synthetic_product_name':
