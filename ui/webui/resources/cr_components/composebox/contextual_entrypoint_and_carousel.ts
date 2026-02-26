ce7b1b4bc9a772fd79118c4801cf66ba71fab151
Hannah Collins <hannahrogers@google.com>
2026-02-25 17:03:23 -0800

    [simplification] Don't rely on entrypoint and carousel in searchbox
    The searchbox only cares about importing the contextual entrypoint and
    recent tab chip. Nothing else in the contextual_entrypoint_and_carousel
    component is used.
    
    Compact:
    https://screencast.googleplex.com/cast/NTM4MzMzOTE3ODQ1OTEzNnw2MjYzN2ZhMy1hNg
    Tall:
    https://screencast.googleplex.com/cast/NDUzNjM5MzgwNzE2NzQ4OHxmOTU3N2Q4NC00YQ
    
    Fixed: 479555609
    Change-Id: I0b5f4474d60202a83a1cdc6ecf5d98036f966989
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7597753
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Commit-Queue: Hannah Collins <hannahrogers@google.com>
    Cr-Commit-Position: refs/heads/main@{#1590526}

--- ui/webui/resources/cr_components/composebox/contextual_entrypoint_and_carousel.ts
+++ ui/webui/resources/cr_components/composebox/contextual_entrypoint_and_carousel.ts
@@ -342,19 +342,6 @@ export class ContextualEntrypointAndCarouselElement extends I18nMixinLit
         ComposeboxContextAddedMethod.COPY_PASTE, this.composeboxSource_);
   }
 
-  closeMenu() {
-    if (!this.showMenuOnClick) {
-      return;
-    }
-
-    const entrypointAndMenu =
-        this.shadowRoot.querySelector<ContextualEntrypointAndMenuElement>(
-            '#contextEntrypoint');
-    if (entrypointAndMenu) {
-      entrypointAndMenu.closeMenu();
-    }
-  }
-
   setContextFiles(files: ContextualUpload[]) {
     const dataTransfer = new DataTransfer();
     for (const file of files) {
@@ -435,7 +422,7 @@ export class ContextualEntrypointAndCarouselElement extends I18nMixinLit
           default:
             break;
         }
-        this.closeMenu();
+        this.closeMenu_();
       } else {
         file = {...file, status: status};
         this.files_.set(token, file);
@@ -726,7 +713,7 @@ export class ContextualEntrypointAndCarouselElement extends I18nMixinLit
     }
 
     this.recordFileValidationMetric_(metric);
-    this.closeMenu();
+    this.closeMenu_();
     this.fire('on-file-validation-error', {
       errorMessage: this.i18n(errorMessage),
     });
@@ -768,11 +755,6 @@ export class ContextualEntrypointAndCarouselElement extends I18nMixinLit
 
     if (this.activeTool_ === ComposeboxToolMode.kDeepSearch) {
       this.handleProcessFilesError_(ProcessFilesError.FILE_UPLOAD_NOT_ALLOWED);
-      return;
-    }
-
-    if (this.entrypointName === 'Realbox') {
-      this.addFileContext_(Array.from(files));
       return;
     }
 
@@ -937,21 +919,19 @@ export class ContextualEntrypointAndCarouselElement extends I18nMixinLit
   }
 
   protected handleToolClick_(tool: ComposeboxToolMode) {
-    if (this.entrypointName !== 'Realbox') {
-      if (this.contextMenuDescriptionEnabled_) {
-        if (this.activeTool_ === tool) {
-          this.showContextMenuDescription_ = true;
-        } else {
-          this.showContextMenuDescription_ =
-              tool === ComposeboxToolMode.kUnspecified;
-        }
-      }
-
+    if (this.contextMenuDescriptionEnabled_) {
       if (this.activeTool_ === tool) {
-        this.activeTool_ = ComposeboxToolMode.kUnspecified;
+        this.showContextMenuDescription_ = true;
       } else {
-        this.activeTool_ = tool;
+        this.showContextMenuDescription_ =
+            tool === ComposeboxToolMode.kUnspecified;
       }
+    }
+
+    if (this.activeTool_ === tool) {
+      this.activeTool_ = ComposeboxToolMode.kUnspecified;
+    } else {
+      this.activeTool_ = tool;
     }
 
     const isActive = this.activeTool_ === tool;
@@ -969,6 +949,19 @@ export class ContextualEntrypointAndCarouselElement extends I18nMixinLit
 
   protected onVoiceSearchClick_() {
     this.fire('open-voice-search');
+  }
+
+  private closeMenu_() {
+    if (!this.showMenuOnClick) {
+      return;
+    }
+
+    const entrypointAndMenu =
+        this.shadowRoot.querySelector<ContextualEntrypointAndMenuElement>(
+            '#contextEntrypoint');
+    if (entrypointAndMenu) {
+      entrypointAndMenu.closeMenu();
+    }
   }
 
   private recordFileValidationMetric_(
