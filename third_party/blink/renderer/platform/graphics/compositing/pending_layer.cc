f6054880d6498ecea18f6ad882cec93c4755aae2
Vasiliy Telezhnikov <vasilyt@chromium.org>
2026-02-26 08:14:28 -0800

    Don't apply 2DScaleTransformWithCompositedDescendants for videos
    While there is no visual difference for video in this case, lifting
    video from the root render pass prevents it going to overlay which is
    not good for power and sometimes might apply different color tuning.
    
    This leads up quite bad for video apps where we switch between modes
    when subtitles appear.
    
    The 2DScaleTransformWithCompositedDescendants meant mostly for the apps
    that tile their content and allow users to zoom/scroll around, it's
    unlikely it will be video content.
    
    Change-Id: I1a681374223b37423d26ba182929eb1caf7ee964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7424584
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1590841}

--- third_party/blink/renderer/platform/graphics/compositing/pending_layer.cc
+++ third_party/blink/renderer/platform/graphics/compositing/pending_layer.cc
@@ -855,4 +855,9 @@ SkColor4f PendingLayer::ComputeBackgroundColor() const {
   return background_color;
 }
 
+bool PendingLayer::HasVideo() const {
+  return Chunks().size() == 1 && FirstPaintChunk().size() == 1 &&
+         FirstDisplayItem().GetType() == DisplayItem::kForeignLayerVideo;
+}
+
 }  // namespace blink
