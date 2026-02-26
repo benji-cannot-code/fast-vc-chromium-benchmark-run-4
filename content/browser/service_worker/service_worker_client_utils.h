5d0c87f9432ad2833939c782aeb19d959a4e5815
Hubert Chao <hchao@chromium.org>
2026-02-26 14:22:40 -0800

    [LNA] Enforce LNA checks on Service Worker's WindowClient.navigate
    Change WindowClient.navigate calls
    (https://developer.mozilla.org/en-US/docs/Web/API/WindowClient/navigate)
    to properly set the initiator to be the WindowClient so that initiator
    client security state is used for LNA checks.
    
    Added test, which involved refactoring some test files.
    
    Bypass-Check-License: moved files
    Bug: 454162508
    Change-Id: I735a0b3c35b901da688bbb2398fb3f567de64510
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7535636
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Hubert Chao <hchao@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1591100}

--- content/browser/service_worker/service_worker_client_utils.h
+++ content/browser/service_worker/service_worker_client_utils.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback.h"
 #include "base/memory/weak_ptr.h"
+#include "services/network/public/mojom/client_security_state.mojom.h"
 #include "third_party/blink/public/common/service_worker/service_worker_status_code.h"
 #include "third_party/blink/public/mojom/service_worker/service_worker.mojom.h"
 #include "third_party/blink/public/mojom/service_worker/service_worker_client.mojom.h"
@@ -66,12 +67,14 @@ void OpenWindow(const GURL& url,
 
 // Navigates the client specified by `rfh_id` to `url`. `callback` is called
 // with the client information on completion.
-void NavigateClient(const GURL& url,
-                    const GURL& script_url,
-                    const blink::StorageKey& key,
-                    const GlobalRenderFrameHostId& rfh_id,
-                    const base::WeakPtr<ServiceWorkerContextCore>& context,
-                    NavigationCallback callback);
+void NavigateClient(
+    const GURL& url,
+    const GURL& script_url,
+    const blink::StorageKey& key,
+    const GlobalRenderFrameHostId& rfh_id,
+    const network::mojom::ClientSecurityStatePtr worker_client_security_state,
+    const base::WeakPtr<ServiceWorkerContextCore>& context,
+    NavigationCallback callback);
 
 // Gets the client specified by |service_worker_client|. |callback| is called
 // with the client information on completion.
