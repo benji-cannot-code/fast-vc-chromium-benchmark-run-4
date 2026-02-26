e1230e58609b36daf07a4ae70cab91de984234a5
Yoichi Osato <yoichio@chromium.org>
2026-02-26 01:34:34 -0800

    Add UseCounter for prerendering cross-origin iframes
    This change introduces a new WebFeature, kPrerender2CrossOriginIframes,
    to track the usage of cross-origin iframe navigation during
    prerendering.
    
    Bug: 440387014
    Change-Id: I23910e4efcbb9b18c230c13ead23887630f33361
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7611188
    Auto-Submit: Yoichi Osato <yoichio@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1590707}

--- content/browser/preloading/prerender/prerender_host.cc
+++ content/browser/preloading/prerender/prerender_host.cc
@@ -42,9 +42,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/render_frame_host_impl.h"
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/public/browser/browser_context.h"
+#include "content/public/browser/content_browser_client.h"
 #include "content/public/browser/navigation_controller.h"
 #include "content/public/browser/preloading_trigger_type.h"
 #include "content/public/browser/web_contents_delegate.h"
+#include "content/public/common/content_client.h"
 #include "content/public/common/referrer.h"
 #include "net/base/load_flags.h"
 #include "net/http/http_request_headers.h"
@@ -52,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/container/flat_hash_set.h"
 #include "third_party/blink/public/common/client_hints/enabled_client_hints.h"
 #include "third_party/blink/public/common/navigation/preloading_headers.h"
+#include "third_party/blink/public/mojom/use_counter/metrics/web_feature.mojom.h"
 #include "url/origin.h"
 
 #if BUILDFLAG(IS_ANDROID)
@@ -752,6 +755,9 @@ void PrerenderHost::ReadyToCommitNavigation(
             parsed_headers->supports_loading_mode,
             network::mojom::LoadingMode::kPrerenderCrossOriginFrames)) {
       allow_cross_origin_subframe_navigation_ = true;
+      GetContentClient()->browser()->LogWebFeatureForCurrentPage(
+          navigation_request->GetRenderFrameHost(),
+          blink::mojom::WebFeature::kPrerender2CrossOriginIframes);
     }
   }
   if (!has_no_vary_search_with_parse_error_header) {
