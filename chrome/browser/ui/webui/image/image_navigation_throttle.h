3a0fb0fc5dd25bd350cd7c5a0e241dc3dea83e2a
Elly <ellyjones@chromium.org>
2026-02-26 10:18:17 -0800

    webui: reject chrome://image loads
    We should never allow navigations to chrome://image; this origin is used
    exclusively to load external images as subresources in webui. Allowing
    it in the main frame opens up a phishing vector for extensions that can
    open a new tab to chrome://image to load chrome://image?<phishing URL>
    and have the page content (an image) show as "trusted chrome UI".
    
    To prevent that, this change installs a new navigation throttle
    (ImageNavigationThrottle) which forbids loads of chrome://image.
    
    This change is based off dpapad@'s earlier attempt:
    https://chromium-review.googlesource.com/c/chromium/src/+/6813891
    
    Bug: 40059921
    Change-Id: I32d839d793accbc2ed6903ab69203028c7e2f24f
    Thanks: dpapad, nasko
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7561815
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1590929}

---
+++ chrome/browser/ui/webui/image/image_navigation_throttle.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_WEBUI_IMAGE_IMAGE_NAVIGATION_THROTTLE_H_
+#define CHROME_BROWSER_UI_WEBUI_IMAGE_IMAGE_NAVIGATION_THROTTLE_H_
+
+#include "content/public/browser/navigation_throttle.h"
+
+namespace content {
+class NavigationThrottleRegistry;
+}  // namespace content
+
+// Blocks navigations to chrome://image URLs.
+class ImageNavigationThrottle : public content::NavigationThrottle {
+ public:
+  static void MaybeCreateAndAdd(content::NavigationThrottleRegistry& registry);
+
+  explicit ImageNavigationThrottle(
+      content::NavigationThrottleRegistry& registry);
+  ~ImageNavigationThrottle() override;
+  ImageNavigationThrottle(const ImageNavigationThrottle&) = delete;
+  ImageNavigationThrottle& operator=(const ImageNavigationThrottle&) = delete;
+
+  // content::NavigationThrottle:
+  ThrottleCheckResult WillStartRequest() override;
+  const char* GetNameForLogging() override;
+};
+
+#endif  // CHROME_BROWSER_UI_WEBUI_IMAGE_IMAGE_NAVIGATION_THROTTLE_H_
