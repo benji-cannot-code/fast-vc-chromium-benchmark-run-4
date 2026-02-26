af7c5823633b393607b239e7b0ebf5e1f51e9c75
Joey Arhar <jarhar@chromium.org>
2026-02-26 14:27:05 -0800

    Clone into all descendant selectedcontent elements
    This enables us to avoid doing any DOM modifications during removal of
    the selectedcontent element, and brings back older behavior which was
    removed earlier in the specification process.
    
    This patch does not change stable behavior. It is flag guarded by
    SelectedcontentSpec which is currently experimental, and
    SelectedcontentelementAttribute which is currently test.
    
    Spec issue: https://github.com/whatwg/html/issues/12096
    
    Bug: 458113204
    Change-Id: I02263cd0b02ea157f0d445dcf9267ac419a72166
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7584125
    Reviewed-by: David Baron <dbaron@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1591107}

--- third_party/blink/renderer/core/html/forms/html_select_element.h
+++ third_party/blink/renderer/core/html/forms/html_select_element.h
@@ -335,8 +335,6 @@ class CORE_EXPORT HTMLSelectElement final
       HTMLSelectedContentElement* selectedcontent);
   void SelectedContentElementInserted(
       HTMLSelectedContentElement* selectedcontent);
-  void UpdateDescendantSelectedcontentsForInsertion(
-      HTMLSelectedContentElement* selectedcontent);
   void SelectedContentElementRemoved(
       HTMLSelectedContentElement* selectedcontent);
 
