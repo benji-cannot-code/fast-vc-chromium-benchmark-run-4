c78a149da4272a86d94c4e8293a90b5547f04b9f
Jun Ishiguro <junis@google.com>
2026-02-26 00:16:25 -0800

    login: Pass local state to TargetDeviceBootstrapController
    Bug: b:404133029
    Change-Id: I9529177c689c733f854fe947ce6f2f7cd4f0a309
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7606439
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1590681}

--- chrome/browser/ash/login/oobe_quick_start/target_device_bootstrap_controller.h
+++ chrome/browser/ash/login/oobe_quick_start/target_device_bootstrap_controller.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback_forward.h"
 #include "base/memory/raw_ptr.h"
+#include "base/memory/raw_ref.h"
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
 #include "base/observer_list_types.h"
@@ -25,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/services/nearby/public/mojom/quick_start_decoder_types.mojom.h"
 #include "google_apis/gaia/gaia_id.h"
 #include "mojo/public/cpp/bindings/shared_remote.h"
+
+class PrefService;
 
 namespace ash::quick_start {
 
@@ -118,7 +121,9 @@ class TargetDeviceBootstrapController
     ~Observer() override = default;
   };
 
+  // `local_state` must be non-null and must outlive `this`.
   TargetDeviceBootstrapController(
+      PrefService* local_state,
       std::unique_ptr<SecondDeviceAuthBroker> auth_broker,
       std::unique_ptr<AccessibilityManagerWrapper>
           accessibility_manager_wrapper,
@@ -229,6 +234,8 @@ class TargetDeviceBootstrapController
       std::unique_ptr<TargetDeviceConnectionBroker> connection_broker) {
     connection_broker_ = std::move(connection_broker);
   }
+
+  const raw_ref<PrefService> local_state_;
 
   std::unique_ptr<TargetDeviceConnectionBroker> connection_broker_;
 
