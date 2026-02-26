e4716028625913e886c3edcb19036637d1688dd8
Ian Kilpatrick <ikilpatrick@chromium.org>
2026-02-26 07:52:47 -0800

    [cleanup] Add greater type-safety for LayoutView::svg_text_descendants_
    This map held pointers to LayoutBox, this was a relic from when the
    "TrackedDescendantsMap" was used for more things, however now we only
    use it for SVGText descendants.
    
    Additionally we don't need it to be a LinkedHashSet, and can just be a
    HashSet, removing some overhead.
    
    Finally we can remove the LayoutBlock::RemoveFromGlobalMaps method.
    
    As the map key is a weak-member, this will get cleared after the object
    itself is GC'd.
    
    This appears to have a small net improvement to speedometer3.
    
       Charts-chartjs                              [ -0.7%,  +0.2%]
       Charts-observable-plot                      [ -0.4%,  +0.2%]
       Editor-CodeMirror                           [ -0.7%,  +0.4%]
    👎 Editor-TipTap                               [ +0.1%,  +0.3%]
    👍 NewsSite-Next                               [ -0.5%,  -0.1%]
       NewsSite-Nuxt                               [ -0.4%,  +0.1%]
       Perf-Dashboard                              [ -0.6%,  +0.2%]
       React-Stockcharts-SVG                       [ -0.4%,  +0.2%]
    👍 Score                                       [ +0.0%,  +0.3%]
       TodoMVC-Angular-Complex-DOM                 [ -0.5%,  +0.5%]
       TodoMVC-Backbone                            [ -0.3%,  +0.3%]
       TodoMVC-JavaScript-ES5                      [ -0.3%,  +0.4%]
       TodoMVC-JavaScript-ES6-Webpack-Complex-DOM  [ -0.6%,  +0.2%]
       TodoMVC-Lit-Complex-DOM                     [ -0.4%,  +0.3%]
       TodoMVC-Preact-Complex-DOM                  [ -1.3%,  +0.5%]
       TodoMVC-React-Complex-DOM                   [ -0.3%,  +0.2%]
       TodoMVC-React-Redux                         [ -0.3%,  +0.1%]
       TodoMVC-Svelte-Complex-DOM                  [ -0.5%,  +0.3%]
    👍 TodoMVC-Vue                                 [ -0.5%,  -0.0%]
    👍 TodoMVC-WebComponents                       [ -0.8%,  -0.0%]
       TodoMVC-jQuery                              [ -0.2%,  +0.7%]
    
    There should be no user-visible behaviour change.
    
    Change-Id: I1762710d6f5e474143d6780f9e7697e91cc4b968
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7604603
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1590825}

--- third_party/blink/renderer/core/layout/layout_block.h
+++ third_party/blink/renderer/core/layout/layout_block.h
@@ -35,12 +35,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class LayoutSVGText;
 struct PaintInfo;
-
-using TrackedLayoutBoxLinkedHashSet = GCedHeapLinkedHashSet<Member<LayoutBox>>;
-using TrackedDescendantsMap =
-    GCedHeapHashMap<WeakMember<const LayoutBlock>,
-                    Member<TrackedLayoutBoxLinkedHashSet>>;
 
 // LayoutBlock is the class that is used by any LayoutObject
 // that is a containing block.
@@ -147,8 +143,8 @@ class CORE_EXPORT LayoutBlock : public LayoutBox {
 
   void RemovePositionedObjects(LayoutObject*);
 
-  void AddSvgTextDescendant(LayoutBox& svg_text);
-  void RemoveSvgTextDescendant(LayoutBox& svg_text);
+  void AddSvgTextDescendant(LayoutSVGText& svg_text);
+  void RemoveSvgTextDescendant(LayoutSVGText& svg_text);
 
   LayoutUnit TextIndentOffset() const;
 
@@ -248,10 +244,6 @@ class CORE_EXPORT LayoutBlock : public LayoutBox {
   PhysicalRect LocalCaretRect(int caret_offset,
                               CaretShape caret_shape) const final;
   bool IsInlineBoxWrapperActuallyChild() const;
-
-  // End helper functions and structs used by layoutBlockChildren.
-
-  void RemoveFromGlobalMaps();
 
  protected:
   PositionWithAffinity PositionForPointIfOutsideAtomicInlineLevel(
