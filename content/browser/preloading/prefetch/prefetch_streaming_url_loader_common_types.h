0cc3747766e96339defbc0648c4f6ed5bb0f3bb5
Hiroshige Hayashizaki <hiroshige@chromium.org>
2026-02-25 18:37:23 -0800

    Remove PrefetchErrorOnResponseReceived::kFailedInvalidHead
    It's never used, as `head` of
    `network::mojom::URLLoaderClient::OnReceiveResponse()` should be always
    non-null. (Actually `PrefetchStreamingURLLoader::OnReceiveResponse()`
    already accesses `head` without null check and isn't crashing)
    
    Bug: 432518638, 433057364
    Change-Id: I2d6d440d7e9309bf3a5081886ad608be08daa5f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7600765
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1590578}

--- content/browser/preloading/prefetch/prefetch_streaming_url_loader_common_types.h
+++ content/browser/preloading/prefetch/prefetch_streaming_url_loader_common_types.h
@@ -41,7 +41,7 @@ enum class PrefetchStreamingURLLoaderStatus {
   kHeadReceivedWaitingOnBody = 1,
 
   // The request redirected to a different target.
-  kRedirected_DEPRECATED = 2,
+  // DEPRECATED kRedirected = 2,
 
   // Both the head and body of the response were received successfully.
   kSuccessfulNotServed = 3,
@@ -51,7 +51,7 @@ enum class PrefetchStreamingURLLoaderStatus {
   // Failure reasons based on the head of the response, corresponding to
   // `PrefetchErrorOnResponseReceived`.
   kPrefetchWasDecoy = 6,
-  kFailedInvalidHead = 7,
+  // DEPRECATED kFailedInvalidHead = 7,
   kFailedInvalidHeaders = 8,
   kFailedNon2XX = 9,
   kFailedMIMENotSupported = 10,
@@ -63,7 +63,7 @@ enum class PrefetchStreamingURLLoaderStatus {
 
   // Statuses related to redirects.
   kFollowRedirect_DEPRECATED = 13,
-  kPauseRedirectForEligibilityCheck_DEPRECATED = 14,
+  // DEPRECATED kPauseRedirectForEligibilityCheck = 14,
   kFailedInvalidRedirect = 15,
   kStopSwitchInNetworkContextForRedirect = 16,
 
@@ -87,7 +87,7 @@ using PrefetchRequestHandler = base::OnceCallback<void(
 // failure reason.
 enum class PrefetchErrorOnResponseReceived {
   kPrefetchWasDecoy,
-  kFailedInvalidHead,
+  // DEPRECATED kFailedInvalidHead,
   kFailedInvalidHeaders,
   kFailedNon2XX,
   kFailedMIMENotSupported
--- content/browser/preloading/prefetch/prefetch_streaming_url_loader_unittest.cc
+++ content/browser/preloading/prefetch/prefetch_streaming_url_loader_unittest.cc
@@ -473,7 +473,7 @@ TEST_P(PrefetchStreamingURLLoaderTest, FailedInvalidHead) {
           /*on_receive_redirect=*/NotReachedTagForTests(),
           &on_head_received_loop,
           // This will cause the prefetch to be marked as not servable.
-          PrefetchErrorOnResponseReceived::kFailedInvalidHead);
+          PrefetchErrorOnResponseReceived::kFailedNon2XX);
 
   // Simulates a prefetch with a non-2XX response. This should be marked as not
   // servable.
@@ -496,7 +496,7 @@ TEST_P(PrefetchStreamingURLLoaderTest, FailedInvalidHead) {
 
   histogram_tester.ExpectUniqueSample(
       "PrefetchProxy.Prefetch.StreamingURLLoaderFinalStatus",
-      PrefetchStreamingURLLoaderStatus::kFailedInvalidHead, 1);
+      PrefetchStreamingURLLoaderStatus::kFailedNon2XX, 1);
 }
 
 TEST_P(PrefetchStreamingURLLoaderTest, FailedNetError_HeadReceived) {
@@ -1453,8 +1453,7 @@ TEST_P(PrefetchStreamingURLLoaderTest, DoesNotTakeDevToolsObserver) {
           /*on_response_received=*/NotReachedTagForTests(),
           /*on_complete=*/NotReachedTagForTests(),
           /*on_receive_redirect=*/NotReachedTagForTests(),
-          /*on_head_received=*/NotReachedTagForTests(),
-          PrefetchErrorOnResponseReceived::kFailedInvalidHead);
+          /*on_head_received=*/NotReachedTagForTests());
 
   EXPECT_TRUE(request.trusted_params->devtools_observer.is_valid());
 }
