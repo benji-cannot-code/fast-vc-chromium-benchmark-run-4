de046fbd7d38ed05e9bd8887d87ecef63df841a9
Justin Lulejian <jlulejian@chromium.org>
2026-02-26 10:10:45 -0800

    [Extensions] Add tests for SharedArrayBuffer in extension messaging
    These tests verify that SharedArrayBuffer cannot be serialized and
    sent via extension messaging (chrome.runtime.sendMessage or
    chrome.tabs.sendMessage) using structured clone serialization.
    
    The new tests verify that serialization fails, resulting in a null
    message being received, both in non-cross-origin isolated contexts and
    even when both the sender and receiver are cross-origin isolated.
    
    Bug: 40321352
    Change-Id: I50a4dd870bdbb1d6d0b97e0927759b39869772a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7601535
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Reviewed-by: Solomon Kinard <solomonkinard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1590927}

---
+++ extensions/test/data/api_test/messaging/serialization_sab/background.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function onMessageListener(message, sender) {
+  if (message.receiverReady) {
+    chrome.tabs.create({url: 'test_sender.html?receiverId=' + sender.tab.id});
+  } else if (message.testResult) {
+    chrome.runtime.onMessage.removeListener(onMessageListener);
+    if (message.testResult === 'success') {
+      chrome.test.succeed();
+    } else {
+      chrome.test.fail();
+    }
+  }
+}
+
+chrome.runtime.onMessage.addListener(onMessageListener);
+
+chrome.test.runTests(
+    [function testExtensionPageToExtensionPageSharedArrayBuffer() {
+      chrome.tabs.create({url: 'test_receiver.html'});
+    }]);
---
+++ extensions/test/data/api_test/messaging/serialization_sab/test_receiver.js
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.runtime.onMessage.addListener((message) => {
+  chrome.test.assertTrue(self.crossOriginIsolated);
+  chrome.test.assertFalse(typeof SharedArrayBuffer === 'undefined');
+
+  chrome.test.assertTrue(message === null);
+  chrome.runtime.sendMessage({testResult: 'success'});
+});
+
+// Notify background that we are ready to receive messages.
+chrome.runtime.sendMessage({receiverReady: true});
