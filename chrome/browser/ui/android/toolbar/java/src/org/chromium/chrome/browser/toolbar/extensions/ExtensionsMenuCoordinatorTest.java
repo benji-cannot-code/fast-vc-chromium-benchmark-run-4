616f2db5fc0a9cb7b7447159c919b30f1e285b8e
Eva Su <evasu@chromium.org>
2026-02-25 19:48:36 -0800

    Rename ExtensionsToolbarButton to ExtensionsMenuButton on the Java side
    Renaming all instances of `ExtensionsToolbarButton` on the C++ side
    leads to a cascading effect of changes (see crrev.com/c/7604482) that
    could be risky to land in one go.
    
    This CL aims to finish the renaming on the Java side for now.
    
    Bug: 485940926
    Change-Id: I6569cab1ee5caa5103b3296102f2067bf56e856a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7608791
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: Eva Su <evasu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1590603}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/extensions/ExtensionsMenuCoordinatorTest.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/extensions/ExtensionsMenuCoordinatorTest.java
@@ -120,7 +120,7 @@ public class ExtensionsMenuCoordinatorTest {
         // Set the current tab.
         mCurrentTabSupplier.set(mTab);
         when(mExtensionsToolbarBridge.getExtensionsMenuButtonState(any()))
-                .thenReturn(ExtensionsToolbarBridge.ExtensionsToolbarButtonState.DEFAULT);
+                .thenReturn(ExtensionsToolbarBridge.ExtensionsMenuButtonState.DEFAULT);
 
         mExtensionsMenuCoordinator =
                 new ExtensionsMenuCoordinator(
