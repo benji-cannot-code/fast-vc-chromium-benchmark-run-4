4525b9371a11d294622218d17a8d2f394f906f1c
Kay Lin <kaiyilin@google.com>
2026-02-26 11:07:28 -0800

    Site Search: Enable the addSearchEngine in Dialog
    This change connects the addSearchEngine function from
    TemplateUrlService to the positive button onClick. The input validation
    logic will be added in the future CLs after the logic is ported from
    C++.
    
    Bug: 479395655
    Change-Id: I1db523e80fce6a6b59d38341e64a825e7cfa5f79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7613507
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Kay Lin <kaiyilin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1590968}

--- chrome/browser/search_engines/android/java/src/org/chromium/chrome/browser/search_engines/settings/custom_site_search/CustomSiteSearchCoordinator.java
+++ chrome/browser/search_engines/android/java/src/org/chromium/chrome/browser/search_engines/settings/custom_site_search/CustomSiteSearchCoordinator.java
@@ -9,6 +9,7 @@ import android.content.Context;
 
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.chrome.browser.search_engines.TemplateUrlServiceFactory;
 import org.chromium.chrome.browser.search_engines.settings.common.SearchEngineListPreference;
 import org.chromium.chrome.browser.search_engines.settings.common.SiteSearchProperties;
 import org.chromium.chrome.browser.search_engines.settings.common.SiteSearchViewBinder;
@@ -38,7 +39,10 @@ public class CustomSiteSearchCoordinator {
                 new CustomSiteSearchMediator(
                         context, mModelList, profile, this::openAddSearchEngineDialog);
         mAddSearchEngineDialogCoordinator =
-                new AddSearchEngineDialogCoordinator(context, modalDialogManager);
+                new AddSearchEngineDialogCoordinator(
+                        context,
+                        modalDialogManager,
+                        TemplateUrlServiceFactory.getForProfile(profile));
 
         mPropertyModel =
                 new PropertyModel.Builder(SiteSearchProperties.ALL_KEYS)
