950c63ac57e70e547b02577c207ce6586d7c6f45
Bruno Braga <brunobraga@google.com>
2026-02-26 09:06:01 -0800

    [Autofill AI - M4] Open entity editor
    Low-Coverage-Reason: TRIVIAL_CHANGE
    
    Bug: 467563819
    Change-Id: I913e85e1da86c3ba0f5ca82244998b22f1388652
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7566652
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1590872}

--- chrome/browser/autofill/android/entity_data_manager_android.cc
+++ chrome/browser/autofill/android/entity_data_manager_android.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/autofill/android/entity_data_manager_android.h"
 
+#include "base/android/jni_android.h"
 #include "base/android/jni_array.h"
 #include "base/android/jni_string.h"
 #include "base/check_deref.h"
@@ -110,8 +111,8 @@ bool EntityDataManagerAndroid::SetAutofillAiOptInStatus(
 }
 
 jni_zero::ScopedJavaLocalRef<jobject>
-EntityDataManagerAndroid::GetEntityInstance(const std::string& guid) {
-  JNIEnv* env = jni_zero::AttachCurrentThread();
+EntityDataManagerAndroid::GetEntityInstance(JNIEnv* env,
+                                            const std::string& guid) {
   base::optional_ref<const EntityInstance> entity =
       entity_data_manager_->GetEntityInstance(EntityInstance::EntityId(guid));
   if (!entity) {
