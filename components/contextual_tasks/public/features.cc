b03743c3d7123f30a71a24b2fea315726ef50181
Yuheng Huang <yuhengh@chromium.org>
2026-02-25 22:36:53 -0800

    [Contextual tasks] Add kExpandButtonOptions FeatureParam
    The FeatureParam controls UI options to expand next panel to full tab.
    Currently 2 options are supported. More options to be added in follow
    ups.
    
    Specs:https://docs.google.com/presentation/d/19iBQHaypBq_WP6k1U3sK3lXbhouvWSNQXVKOBhWYy6k/edit?slide=id.g3cad3c86f13_1_17&resourcekey=0-yyZ9Dmonm7Tt4ga8LQpLSw#slide=id.g3cad3c86f13_1_17
    
    Bug: 487328229
    Change-Id: I8a15cf4bb294e0d2d9618992772fcdd55d180c14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7608950
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1590657}

--- components/contextual_tasks/public/features.cc
+++ components/contextual_tasks/public/features.cc
@@ -131,6 +131,16 @@ const base::FeatureParam<EntryPointOption> kShowEntryPoint(
     EntryPointOption::kToolbarPermanent,
     &kEntryPointOptions);
 
+constexpr base::FeatureParam<ExpandButtonOption>::Option kExpandButtonOption[] =
+    {{ExpandButtonOption::kSidePanelExpandButton, "side-panel-expand-button"},
+     {ExpandButtonOption::kToolbarCloseButton, "toolbar-close-button"}};
+
+const base::FeatureParam<ExpandButtonOption> kExpandButtonOptions(
+    &kContextualTasks,
+    "ContextualTasksExpandButtonOptions",
+    ExpandButtonOption::kSidePanelExpandButton,
+    &kExpandButtonOption);
+
 const base::FeatureParam<bool> kTaskScopedSidePanel(
     &kContextualTasks,
     "ContextualTasksTaskScopedSidePanel",
@@ -385,6 +395,10 @@ bool ShouldEnableCookieSync() {
 bool ShouldEnableLockAndUnlockInputCapability() {
   return base::FeatureList::IsEnabled(kContextualTasks) &&
          kContextualTasksLockAndUnlockInputCapability.Get();
+}
+
+ExpandButtonOption GetExpandButtonOption() {
+  return kExpandButtonOptions.Get();
 }
 
 namespace flag_descriptions {
