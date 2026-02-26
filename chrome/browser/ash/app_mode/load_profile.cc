9e1071662fd9d1a1eba1238e2571d5a425a6eacb
Edman Anjos <edmanp@google.com>
2026-02-26 03:21:12 -0800

    kiosk: Use std::visit in app_mode/load_profile.cc
    This replaces the current get_if calls, so we can stop using NOTREACHED.
    
    Fixed: 324365936
    Test: testing/xvfb.py tools/autotest.py -C out_/Default --run-all chrome/browser/ash/app_mode/load_profile_unittest.cc chrome/browser/ash/app_mode/test/kiosk_browsertest.cc
    Change-Id: Ic3d60bb0394910251f90433e6864b9012a0c2d2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7573135
    Commit-Queue: Edman Anjos <edman@chromium.org>
    Reviewed-by: Jeroen Dhollander <jeroendh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1590748}

--- chrome/browser/ash/app_mode/load_profile.cc
+++ chrome/browser/ash/app_mode/load_profile.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/memory/raw_ref.h"
 #include "base/memory/weak_ptr.h"
-#include "base/notimplemented.h"
 #include "base/notreached.h"
 #include "base/sequence_checker.h"
 #include "base/syslog_logging.h"
@@ -41,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/login/auth/public/user_context.h"
 #include "components/account_id/account_id.h"
 #include "components/user_manager/user_names.h"
+#include "third_party/abseil-cpp/absl/functional/overload.h"
 
 namespace ash::kiosk {
 
@@ -396,14 +396,19 @@ void ProfileLoader::LoginAsKioskAccount() {
 
 void ProfileLoader::DidLoginAsKioskAccount(PerformSigninResult result) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  if (result.has_value()) {
-    return PrepareProfile(result.value());
-  } else if (auto* error = std::get_if<PerformSigninError>(&result.error())) {
-    return ReturnError(SigninErrorToKioskLaunchError(*error));
-  } else if (auto* auth_failure = std::get_if<AuthFailure>(&result.error())) {
-    return ReturnError(LoginFailureToKioskLaunchError(*auth_failure));
+  if (!result.has_value()) {
+    ReturnError(std::visit(absl::Overload{
+                               [](PerformSigninError& error) {
+                                 return SigninErrorToKioskLaunchError(error);
+                               },
+                               [](AuthFailure& error) {
+                                 return LoginFailureToKioskLaunchError(error);
+                               },
+                           },
+                           result.error()));
+  } else {
+    PrepareProfile(result.value());
   }
-  NOTREACHED();
 }
 
 void ProfileLoader::PrepareProfile(const UserContext& user_context) {
