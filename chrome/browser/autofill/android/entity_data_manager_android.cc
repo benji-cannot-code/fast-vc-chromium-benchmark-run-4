4e2d7de432bae9b709cb2b04a301eb39410d7b33
Bruno Braga <brunobraga@google.com>
2026-02-26 13:20:20 -0800

    [Autofill AI - M4]: Add method to retrieve entities list order
    This will be used in the management page to decide how to order
    entities.
    
    Bug: 467563819
    Change-Id: I49eb9f32459d20da7b59808bd5ff828c4415ce3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7611508
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1591057}

--- chrome/browser/autofill/android/entity_data_manager_android.cc
+++ chrome/browser/autofill/android/entity_data_manager_android.cc
@@ -5,10 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/autofill/android/entity_data_manager_android.h"
 
+#include <algorithm>
+
 #include "base/android/jni_android.h"
 #include "base/android/jni_array.h"
 #include "base/android/jni_string.h"
 #include "base/check_deref.h"
+#include "base/containers/to_vector.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/types/zip.h"
 #include "chrome/browser/autofill/account_setting_service_factory.h"
@@ -180,6 +183,17 @@ std::vector<EntityTypeAndroid> EntityDataManagerAndroid::GetWritableEntityTypes(
     entity_types.emplace_back(EntityTypeAndroid(entity_type));
   }
   return entity_types;
+}
+
+std::vector<EntityTypeAndroid>
+EntityDataManagerAndroid::GetSortedEntityTypesForListDisplay(
+    JNIEnv* env) const {
+  std::vector<EntityType> all_types =
+      base::ToVector(DenseSet<EntityType>::all());
+  std::ranges::sort(all_types, EntityType::ListOrder);
+  return base::ToVector(all_types, [](const EntityType& type) {
+    return EntityTypeAndroid(type);
+  });
 }
 
 void EntityDataManagerAndroid::OnEntityInstancesChanged() {
