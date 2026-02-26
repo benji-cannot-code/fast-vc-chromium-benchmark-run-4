8500b773ae03d747c6b8a8ca3ea6f3d166c7db9b
Michael Wilson <mjwilson@chromium.org>
2026-02-26 11:40:06 -0800

    Convert some UNSAFE_TODO to safe operations in AudioBuffer
    We can use span methods to zero the buffers.  This does not remove all
    unsafe operations in this file though.
    
    This should cause no functional change.
    
    Bug: 401184803
    Change-Id: Iee7eaa671f843c6be89e50b48513b300ce05736d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7605460
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1590984}

--- third_party/blink/renderer/modules/webaudio/audio_buffer.cc
+++ third_party/blink/renderer/modules/webaudio/audio_buffer.cc
@@ -316,8 +316,7 @@ void AudioBuffer::copyToChannel(NotShared<DOMFloat32Array> source,
 void AudioBuffer::Zero() {
   for (unsigned i = 0; i < channels_.size(); ++i) {
     if (NotShared<DOMFloat32Array> array = getChannelData(i)) {
-      float* data = array->Data();
-      UNSAFE_TODO(memset(data, 0, length() * sizeof(*data)));
+      std::ranges::fill(array->AsSpan(), 0.0f);
     }
   }
 }
@@ -337,8 +336,7 @@ SharedAudioBuffer::SharedAudioBuffer(AudioBuffer* buffer)
 
 void SharedAudioBuffer::Zero() {
   for (auto& channel : channels_) {
-    float* data = static_cast<float*>(channel.Data());
-    UNSAFE_TODO(memset(data, 0, length() * sizeof(*data)));
+    std::ranges::fill(channel.ByteSpan(), 0);
   }
 }
 
