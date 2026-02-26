9c5674755684a05dc46e491d60996082a81d1293
Gauthier Ambard <gambard@chromium.org>
2026-02-26 10:06:35 -0800

    [iOS] Fix Infobar crash
    When refactoring this method in crrev.com/c/7521808, I introduced a
    crash when the browser is nil (whereas before it was failing silently).
    It is unclear why the Browser is nil here, but as it was supported
    before, this CL reverts back to the old behaviour of returning 0 if the
    browser is nil.
    
    Fixed: 487881881
    Change-Id: Ie890de702197b0c81a104e43e0367dc5553c95c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7611747
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1590921}

--- ios/chrome/browser/overlays/ui_bundled/infobar_banner/infobar_banner_overlay_coordinator.mm
+++ ios/chrome/browser/overlays/ui_bundled/infobar_banner/infobar_banner_overlay_coordinator.mm
@@ -90,6 +90,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark - InfobarBannerPositioner
 
 - (CGFloat)bannerYPosition {
+  if (!self.browser) {
+    return 0;
+  }
   LayoutGuideCenter* layoutGuideCenter =
       LayoutGuideCenterForBrowser(self.browser);
 
