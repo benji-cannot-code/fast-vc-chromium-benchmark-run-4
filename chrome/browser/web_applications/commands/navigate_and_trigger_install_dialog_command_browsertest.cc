165afe871c5fb9425d2206efae3d0a7e335c8f10
aditipage <aditipage@google.com>
2026-02-25 18:32:34 -0800

    [PWA] Refactor NavigateAndTriggerInstallDialogCommand result types
    This changelist improves the code health and layering of the web
    application system by migrating the result-related definitions of
    NavigateAndTriggerInstallDialogCommand into a dedicated, lightweight
    header file. Specifically, the
    NavigateAndTriggerInstallDialogCommandResult enum, its completion
    callback alias, and the corresponding operator<< overload have been
    moved from the main command header to a new file.
    
    This refactoring allows the WebAppCommandScheduler and other client
    components to depend only on the necessary result types without being
    forced to include the entire heavyweight command implementation and its
    transitive dependencies. This change reduces compilation overhead and
    adheres to the project goal of better separating interface from
    implementation.
    
    Fixed: 477677503
    Change-Id: I0cfb989e4ef6cce02fc217c1530c2372d424d660
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7515687
    Commit-Queue: Aditi Page <aditipage@google.com>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1590576}

--- chrome/browser/web_applications/commands/navigate_and_trigger_install_dialog_command_browsertest.cc
+++ chrome/browser/web_applications/commands/navigate_and_trigger_install_dialog_command_browsertest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/web_applications/web_app_browsertest_base.h"
+#include "chrome/browser/web_applications/scheduler/navigate_and_trigger_install_dialog_result.h"
 #include "chrome/browser/web_applications/web_app_command_scheduler.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
 #include "chrome/test/base/chrome_test_utils.h"
@@ -44,9 +45,7 @@ IN_PROC_BROWSER_TEST_F(NavigateAndTriggerInstallDialogCommandTest,
   provider().scheduler().ScheduleNavigateAndTriggerInstallDialog(
       test_url, kOriginUrl, /*is_renderer_initiated=*/true,
       base::BindLambdaForTesting(
-          [&](NavigateAndTriggerInstallDialogCommandResult result) {
-            loop.Quit();
-          }));
+          [&](NavigateAndTriggerInstallDialogResult result) { loop.Quit(); }));
   navigation_observer.Wait();
   // The browser should now have 2 tabs.
   EXPECT_EQ(2, browser()->tab_strip_model()->count());
@@ -70,9 +69,8 @@ IN_PROC_BROWSER_TEST_F(NavigateAndTriggerInstallDialogCommandTest,
   provider().scheduler().ScheduleNavigateAndTriggerInstallDialog(
       test_url, kOriginUrl, /*is_renderer_initiated=*/true,
       base::BindLambdaForTesting(
-          [&](NavigateAndTriggerInstallDialogCommandResult result) {
-            EXPECT_EQ(result,
-                      NavigateAndTriggerInstallDialogCommandResult::kFailure);
+          [&](NavigateAndTriggerInstallDialogResult result) {
+            EXPECT_EQ(result, NavigateAndTriggerInstallDialogResult::kFailure);
             loop.Quit();
           }));
   navigation_observer.Wait();
@@ -93,9 +91,8 @@ IN_PROC_BROWSER_TEST_F(NavigateAndTriggerInstallDialogCommandTest,
   provider().scheduler().ScheduleNavigateAndTriggerInstallDialog(
       test_url, kOriginUrl, /*is_renderer_initiated=*/true,
       base::BindLambdaForTesting(
-          [&](NavigateAndTriggerInstallDialogCommandResult result) {
-            EXPECT_EQ(result,
-                      NavigateAndTriggerInstallDialogCommandResult::kFailure);
+          [&](NavigateAndTriggerInstallDialogResult result) {
+            EXPECT_EQ(result, NavigateAndTriggerInstallDialogResult::kFailure);
             loop.Quit();
           }));
 
@@ -114,9 +111,9 @@ IN_PROC_BROWSER_TEST_F(NavigateAndTriggerInstallDialogCommandTest,
   provider().scheduler().ScheduleNavigateAndTriggerInstallDialog(
       test_url, kOriginUrl, /*is_renderer_initiated=*/true,
       base::BindLambdaForTesting(
-          [&](NavigateAndTriggerInstallDialogCommandResult result) {
-            EXPECT_EQ(result, NavigateAndTriggerInstallDialogCommandResult::
-                                  kAlreadyInstalled);
+          [&](NavigateAndTriggerInstallDialogResult result) {
+            EXPECT_EQ(result,
+                      NavigateAndTriggerInstallDialogResult::kAlreadyInstalled);
             loop.Quit();
           }));
 
@@ -133,10 +130,9 @@ IN_PROC_BROWSER_TEST_F(NavigateAndTriggerInstallDialogCommandTest,
   provider().scheduler().ScheduleNavigateAndTriggerInstallDialog(
       test_url, kOriginUrl, /*is_renderer_initiated=*/true,
       base::BindLambdaForTesting(
-          [&](NavigateAndTriggerInstallDialogCommandResult result) {
-            EXPECT_EQ(
-                result,
-                NavigateAndTriggerInstallDialogCommandResult::kDialogShown);
+          [&](NavigateAndTriggerInstallDialogResult result) {
+            EXPECT_EQ(result,
+                      NavigateAndTriggerInstallDialogResult::kDialogShown);
             loop.Quit();
           }));
 
---
+++ chrome/browser/web_applications/scheduler/navigate_and_trigger_install_dialog_result.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/web_applications/scheduler/navigate_and_trigger_install_dialog_result.h"
+
+#include <ostream>
+
+namespace web_app {
+
+std::ostream& operator<<(std::ostream& os,
+                         NavigateAndTriggerInstallDialogResult result) {
+  switch (result) {
+    case NavigateAndTriggerInstallDialogResult::kFailure:
+      return os << "kFailure";
+    case NavigateAndTriggerInstallDialogResult::kAlreadyInstalled:
+      return os << "kAlreadyInstalled";
+    case NavigateAndTriggerInstallDialogResult::kDialogShown:
+      return os << "kDialogShown";
+    case NavigateAndTriggerInstallDialogResult::kShutdown:
+      return os << "kShutdown";
+  }
+}
+
+}  // namespace web_app
