05101b93716b406cbcef4058a8dc3a18c57159be
Ireneusz Szulc <ireneuszszulc@google.com>
2026-02-26 09:16:06 -0800

    @memory: Implement UI for @memory popup
    This change adds keyboard navigation and layout adjustments to
    PopupViewViews to support the @memory feature.
    It also adds a `GetText` helper to the search bar to enable query
    retrieval on pressing Return key.
    
    This feature is hidden behind the experimental flag `AutofillAtMemory`.
    
    This change is part of @memory project, which is about to allow
    users type "@@" in the text fields to trigger a pop-up, where they
    can search for their local autofill data to be inserted.
    
    Bug: b:481980303
    Change-Id: Ib655a349a3afb810857a3c0af7ab35c96a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7595338
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Ireneusz Szulc <ireneuszszulc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1590882}

--- chrome/browser/ui/views/autofill/popup/popup_search_bar_view.h
+++ chrome/browser/ui/views/autofill/popup/popup_search_bar_view.h
@@ -83,6 +83,9 @@ class PopupSearchBarView : public views::View,
   // Focuses on the input field.
   void Focus();
 
+  // Returns the current text in the input field.
+  std::u16string GetText() const;
+
   void SetInputTextForTesting(const std::u16string& text);
   gfx::Point GetClearButtonScreenCenterPointForTesting() const;
 
