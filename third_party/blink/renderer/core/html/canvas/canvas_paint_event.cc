32bca59e41ea513a0a0f0f882c5a208157302de9
Philip Rogers <pdr@chromium.org>
2026-02-26 14:12:32 -0800

    [html-in-canvas] Pass changed elements to onpaint
    This patch adds a PaintEvent type which contains an array of the canvas
    children that changed in the current frame.
    
    To simplify this change, the two hash sets for canvases needing onpaint
    and painted canvas children have been combined into a single map on
    local frame view.
    
    This patch has a minor refactoring:
    LocalFrameView::needs_post_lifecycle_steps_before_impl_commit_ has
    been removed in favor of a frame tree iteration to compute the value on
    the fly.
    
    Bug: 484345338
    Change-Id: I3c71a860acc2d1551b1a51a4a54c78592861c1e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7599106
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1591090}

---
+++ third_party/blink/renderer/core/html/canvas/canvas_paint_event.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/core/html/canvas/canvas_paint_event.h"
+
+#include "third_party/blink/renderer/core/event_interface_names.h"
+
+namespace blink {
+
+CanvasPaintEvent::CanvasPaintEvent(const AtomicString& type,
+                                   const CanvasPaintEventInit* initializer)
+    : Event(type, initializer) {
+  CHECK(initializer);
+  if (initializer->hasChangedElements()) {
+    changed_elements_ = initializer->changedElements();
+  }
+}
+
+CanvasPaintEvent::~CanvasPaintEvent() = default;
+
+const HeapVector<Member<Element>>& CanvasPaintEvent::changedElements() const {
+  return changed_elements_;
+}
+
+const AtomicString& CanvasPaintEvent::InterfaceName() const {
+  return event_interface_names::kCanvasPaintEvent;
+}
+
+void CanvasPaintEvent::Trace(Visitor* visitor) const {
+  visitor->Trace(changed_elements_);
+  Event::Trace(visitor);
+}
+
+}  // namespace blink
