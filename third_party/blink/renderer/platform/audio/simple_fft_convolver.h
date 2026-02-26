a1f4b0de4ed2098571581802d4ce01e8edefc29e
Michael Wilson <mjwilson@chromium.org>
2026-02-26 11:47:50 -0800

    Convert UNSAFE_TODO to safe operations in SimpleFFTConvolver
    Since the buffers are AudioFloatArray which can be viewed as a span, we
    can use the span methods to do the copy instead.
    
    Also promote constructor DCHECK to CHECK and do some minor style
    cleanup.
    
    This should cause no functional change.
    
    Bug: 401184803
    Change-Id: I2391e1e79a1ce3404b6775207c5ce2d864a4fe89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7604848
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1590992}

--- third_party/blink/renderer/platform/audio/simple_fft_convolver.h
+++ third_party/blink/renderer/platform/audio/simple_fft_convolver.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_AUDIO_SIMPLE_FFT_CONVOLVER_H_
 
 #include <memory>
+
 #include "third_party/blink/renderer/platform/audio/audio_array.h"
 #include "third_party/blink/renderer/platform/audio/fft_frame.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
