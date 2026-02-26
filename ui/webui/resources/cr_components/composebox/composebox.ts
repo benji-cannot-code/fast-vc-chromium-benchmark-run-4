f99e33a9831bb162b4c44bd08d80c59d22142b08
Marlon Facey <mfacey@chromium.org>
2026-02-26 10:27:08 -0800

    [Omnibox Next] Clear voice search dialog on close
    The voice search dialog is not closed when the popup is closed. This CL
    closes it.
    
    Cast: http://screencast/cast/NjM5MzA2ODgwOTg3OTU1Mnw2OTlmMzc3OS00Yw
    
    Note: The flicker will be fixed with the debouncing added here:
    https://chromium-review.googlesource.com/c/chromium/src/+/7561142
    
    Bug: b:485291017
    Change-Id: I7af1efe95c685156d1edb36164baa3169bc94b0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7585934
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1590938}

--- ui/webui/resources/cr_components/composebox/composebox.ts
+++ ui/webui/resources/cr_components/composebox/composebox.ts
@@ -1557,6 +1557,9 @@ export class ComposeboxElement extends I18nMixinLit
       this.searchboxHandler_.clearFiles(shouldBlockAutoSuggestedTabs);
     }
     this.fileUploadsComplete = this.pendingUploads_.size === 0;
+    if (this.inVoiceSearchMode_) {
+      this.voiceSearchEndCleanup_();
+    }
   }
 
   clearInput() {
