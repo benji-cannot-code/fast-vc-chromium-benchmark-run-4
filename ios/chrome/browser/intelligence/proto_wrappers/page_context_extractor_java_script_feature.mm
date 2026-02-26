27eae72e47bea98d65ed5d3b3cdd32b8eb0a93c8
Vincent Boisselle <vincb@google.com>
2026-02-26 12:56:40 -0800

    [ios][apc] Switch JS feature injection timing from doc end to start
    Change-Id: I010f3bd5fcc2bc0118c3dc68635dd0f805225f58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7613179
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Auto-Submit: Vincent Boisselle <vincb@google.com>
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1591041}

--- ios/chrome/browser/intelligence/proto_wrappers/page_context_extractor_java_script_feature.mm
+++ ios/chrome/browser/intelligence/proto_wrappers/page_context_extractor_java_script_feature.mm
@@ -38,7 +38,7 @@ PageContextExtractorJavaScriptFeature::PageContextExtractorJavaScriptFeature()
           {web::JavaScriptFeature::FeatureScript::CreateWithFilename(
               kScriptName,
               web::JavaScriptFeature::FeatureScript::InjectionTime::
-                  kDocumentEnd,
+                  kDocumentStart,
               web::JavaScriptFeature::FeatureScript::TargetFrames::kAllFrames,
               web::JavaScriptFeature::FeatureScript::ReinjectionBehavior::
                   kInjectOncePerWindow,
