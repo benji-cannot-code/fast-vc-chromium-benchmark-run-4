958007a539aeebb7c3e44a121ce187f59a9d2ca4
Peter Wen <wnwen@chromium.org>
2026-02-27 07:19:13 -0800

    webapps: Use MockitoJUnit.rule() in PWA bottom sheet tests
    Replace MockitoAnnotations.initMocks(this) with MockitoJUnit.rule() in
    PWA universal install and PWA restore bottom sheet tests. This
    modernizes the tests and leverages MockitoRule's automatic
    initialization and validation.
    
    Empty setUp() methods after refactoring were removed to avoid ErrorProne
    warnings.
    
    Files refactored:
    - PwaUniversalInstallBottomSheetCoordinatorTest.java
    - PwaUniversalInstallBottomSheetContentTest.java
    - PwaRestoreBottomSheetViewRenderTest.java
    - PwaRestoreBottomSheetCoordinatorTest.java
    - PwaRestoreBottomSheetContentTest.java
    
    Bug: 487264369
    Change-Id: Ic10e392558b93b3f1629a464611ee11c10f1ae04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7604039
    Auto-Submit: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Glenn Hartmann <hartmanng@chromium.org>
    Commit-Queue: Glenn Hartmann <hartmanng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1591542}

--- components/webapps/browser/android/java/src/org/chromium/components/webapps/pwa_restore_ui/PwaRestoreBottomSheetCoordinatorTest.java
+++ components/webapps/browser/android/java/src/org/chromium/components/webapps/pwa_restore_ui/PwaRestoreBottomSheetCoordinatorTest.java
@@ -22,11 +22,13 @@ import androidx.test.filters.MediumTest;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 import org.robolectric.Robolectric;
 import org.robolectric.annotation.Config;
 
@@ -43,6 +45,7 @@ import java.util.List;
 @RunWith(BaseRobolectricTestRunner.class)
 @Config(manifest = Config.NONE)
 public class PwaRestoreBottomSheetCoordinatorTest {
+    @Rule public MockitoRule mMockitoRule = MockitoJUnit.rule();
     Activity mActivity;
 
     private final String[] mDefaultAppIds = new String[] {"appId1", "appId2", "appId3"};
@@ -59,7 +62,6 @@ public class PwaRestoreBottomSheetCoordinatorTest {
 
     @Before
     public void setUp() {
-        MockitoAnnotations.initMocks(this);
         mActivity = Robolectric.buildActivity(Activity.class).create().get();
         PwaRestoreBottomSheetMediatorJni.setInstanceForTesting(mNativeMediatorMock);
         when(mNativeMediatorMock.initialize(Mockito.any())).thenReturn(0L);
