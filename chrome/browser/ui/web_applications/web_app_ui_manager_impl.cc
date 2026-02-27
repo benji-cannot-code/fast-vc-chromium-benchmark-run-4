4145141aba7c2c1fe33b23ae8947a4de56e949c8
lukedeng <lukedeng@chromium.org>
2026-02-27 11:09:08 -0800

    Add InfoBar for blocked web app updates
    Adds WebAppBlockedUpdateInfoBarDelegate to show a warning when an
    window_app_id is changed and its migration update is blocked due to
    policy. The logic is triggered in WebAppTabHelper when app is policy
    installed and detects a pending migration under the kWebAppMigrationApi
    feature flag. Includes UI and browser tests to verify visibility and
    layout.
    
    Bug: 442651045
    Change-Id: I8c3d0b23879663fe5b0e56f408d5a4e98ec6b495
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7606823
    Reviewed-by: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Commit-Queue: Luke Deng <lukedeng@google.com>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1591690}

--- chrome/browser/ui/web_applications/web_app_ui_manager_impl.cc
+++ chrome/browser/ui/web_applications/web_app_ui_manager_impl.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/intent_picker_tab_helper.h"
 #include "chrome/browser/ui/user_education/browser_user_education_interface.h"
 #include "chrome/browser/ui/web_applications/app_browser_controller.h"
+#include "chrome/browser/ui/views/web_apps/web_app_blocked_migration_infobar_delegate.h"
 #include "chrome/browser/ui/web_applications/web_app_dialog_utils.h"
 #include "chrome/browser/ui/web_applications/web_app_dialogs.h"
 #include "chrome/browser/ui/web_applications/web_app_launch_utils.h"
@@ -608,6 +609,11 @@ void WebAppUiManagerImpl::MaybeCreateEnableSupportedLinksInfobar(
         ->AddInfoBar(CreateConfirmInfoBar(std::move(delegate)));
   }
 #endif  // !BUILDFLAG(IS_CHROMEOS)
+}
+
+void WebAppUiManagerImpl::MaybeCreateWebAppBlockedMigrationInfoBar(
+    content::WebContents* web_contents) {
+  WebAppBlockedMigrationInfoBarDelegate::Create(web_contents);
 }
 
 void WebAppUiManagerImpl::MaybeShowIPHPromoForAppsLaunchedViaLinkCapturing(
--- chrome/browser/web_applications/web_app_tab_helper.cc
+++ chrome/browser/web_applications/web_app_tab_helper.cc
@@ -39,6 +39,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/page.h"
 #include "content/public/browser/site_instance.h"
+#include "content/public/browser/web_contents.h"
+#include "third_party/blink/public/common/features.h"
 #include "services/network/public/mojom/url_response_head.mojom.h"
 #include "third_party/blink/public/common/renderer_preferences/renderer_preferences.h"
 #include "third_party/blink/public/mojom/use_counter/metrics/web_feature.mojom-shared.h"
@@ -162,6 +164,7 @@ void WebAppTabHelper::SetState(std::optional<webapps::AppId> app_id,
              *app_id, WebAppFilter::IsAppSurfaceableToUser()) ||
          provider_->registrar_unsafe().IsUninstalling(*app_id));
 
+
   if (app_id_ == app_id && window_app_id_ == window_app_id) {
     // This can be triggered for navigations that are happening in the same app
     // window, like if a navigation is captured in an open window causing a page
@@ -173,7 +176,15 @@ void WebAppTabHelper::SetState(std::optional<webapps::AppId> app_id,
 
   std::optional<webapps::AppId> previous_app_id = std::move(app_id_);
   app_id_ = std::move(app_id);
+
+  std::optional<webapps::AppId> previous_window_app_id =
+      std::move(window_app_id_);
   window_app_id_ = std::move(window_app_id);
+
+  if (base::FeatureList::IsEnabled(blink::features::kWebAppMigrationApi) &&
+      (previous_window_app_id != window_app_id_)) {
+    MaybeShowBlockedMigrationInfoBar(window_app_id_);
+  }
 
   if (previous_app_id != app_id_) {
     OnAssociatedAppChanged(previous_app_id, app_id_);
@@ -472,5 +483,21 @@ void WebAppTabHelper::OnManifestSpecifiedOnPrimaryPage(
 }
 
 WEB_CONTENTS_USER_DATA_KEY_IMPL(WebAppTabHelper);
+
+// TODO(crbug.com/442651045) Append removed logic for this info bar.
+void WebAppTabHelper::MaybeShowBlockedMigrationInfoBar(
+    const std::optional<webapps::AppId>& window_app_id) {
+  if (!window_app_id.has_value()) {
+    return;
+  }
+  const WebApp* app =
+      provider_->registrar_unsafe().GetAppById(window_app_id.value());
+  if (app && app->pending_migration_info().has_value() &&
+      provider_->registrar_unsafe().IsInstalledByPolicy(
+          window_app_id.value())) {
+    provider_->ui_manager().MaybeCreateWebAppBlockedMigrationInfoBar(
+        web_contents());
+  }
+}
 
 }  // namespace web_app
