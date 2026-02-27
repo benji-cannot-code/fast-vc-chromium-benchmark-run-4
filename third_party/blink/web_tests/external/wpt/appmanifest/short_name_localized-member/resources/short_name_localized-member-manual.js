47075b6d71eee77e5eecb611e1cd7c26475c91d7
Alexander Kyereboah <akyereboah@microsoft.com>
2026-02-26 16:25:08 -0800

    [PWA] Adding manual WPT for localized name and short_name
    Since we're unable to fully automate WPT for installed app scenarios,
    adding manual WPT for the manifest localization feature. This change
    adds testing for name and short_name members. Will follow up with
    additional WPT tests for icons and shortcuts, which are also
    localizable.
    
    Bug: 380491647
    Change-Id: Ia2408ca856880c23c86685baaa06c45fdb54857a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7604734
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Lu Huang <luhua@microsoft.com>
    Commit-Queue: Alexander Kyereboah <akyereboah@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1591183}

---
+++ third_party/blink/web_tests/external/wpt/appmanifest/short_name_localized-member/resources/short_name_localized-member-manual.js
@@ -0,0 +1,5 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register(
+    'short_name_localized-member-service-worker.js');
+}
