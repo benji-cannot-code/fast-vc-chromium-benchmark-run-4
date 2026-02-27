fbea448530940a28556bee59897c8b3c7214afe7
Joe Downing <joedow@google.com>
2026-02-27 14:20:55 -0800

    Fix JingleMessageReply type discrepancy
    This CL fixes a problem between the enum in the XMPP proto and the
    enum defined in JingleMessageReply where the 'Unspecified' value
    in the proto was mapped to 'NONE' in the struct. I've rename NONE
    and updated the struct to use std::optional to indicate whether
    an error has been set.  All of the callers and such have been
    updated to use the new mechanism.
    
    Bug: 359620500
    Change-Id: I508466f8b03cd3149a86eae754c449e21c2cab4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7616365
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1591830}

--- remoting/protocol/jingle_session.h
+++ remoting/protocol/jingle_session.h
@@ -55,8 +55,9 @@ class JingleSession : public Session {
  private:
   friend class JingleSessionManager;
 
-  using ReplyCallback = base::OnceCallback<void(const JingleMessage&,
-                                                JingleMessageReply::ErrorType)>;
+  using ReplyCallback =
+      base::OnceCallback<void(const JingleMessage&,
+                              std::optional<JingleMessageReply::ErrorType>)>;
 
   explicit JingleSession(JingleSessionManager* session_manager);
 
