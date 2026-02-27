ed3fe9d04e2360e768ce5b04a5dd32ca7adce83d
Rune Lillesveen <futhark@chromium.org>
2026-02-27 01:13:38 -0800

    Call StyleContainerChanged() for InitialData() changes
    Style container queries may query initial values of registered custom
    properties. Adding or removing @property registrations may change style()
    query evaluation without changing variable data on the ComputedStyle.
    
    Invoke StyleContainerChanged() when the InitialData() singleton has
    changed since that may be a result of initial values of registered
    properties changing.
    
    Fixes DCHECK() and incorrect use of cached evaluation.
    
    Bug: 488059555
    Change-Id: I969f59a9bff52fff83d8227d8005a99138eaf50c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7613711
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1591393}

--- third_party/blink/renderer/core/css/container_query_evaluator.cc
+++ third_party/blink/renderer/core/css/container_query_evaluator.cc
@@ -1029,6 +1029,7 @@ StyleRecalcChange ContainerQueryEvaluator::ApplyScrollStateAndStyleChanges(
   }
   if (old_style.InheritedVariables() != new_style.InheritedVariables() ||
       old_style.NonInheritedVariables() != new_style.NonInheritedVariables() ||
+      old_style.InitialData() != new_style.InitialData() ||
       DependsOnTreeCounting()) {
     switch (StyleContainerChanged()) {
       case ContainerQueryEvaluator::Change::kNone:
