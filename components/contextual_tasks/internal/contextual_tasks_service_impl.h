0320ed20ab1cfced8246dbfabcac579416c42e72
David Maunder <davidjm@chromium.org>
2026-02-27 11:16:58 -0800

    Add Gemini Threads in ContextualTasksService
    This CL adds Gemini Threads in BuildTasks() in
    ContextualTasksService and completes the data plumbing up
    to the UI. It also updates the icon for Gemini Threads in the Projects
    Panel.
    
    Video:
    https://drive.google.com/file/d/1V1zpHSkjgW1A0F4iehsIQmAa_nsDPTFu/view?usp=sharing
    
    Bug: 488320856
    Change-Id: I4f00fe7e6bd2feba1a79ec2aed9cd1a35de16a02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7615127
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Reviewed-by: Chase Hartsell <chazzy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1591696}

--- components/contextual_tasks/internal/contextual_tasks_service_impl.h
+++ components/contextual_tasks/internal/contextual_tasks_service_impl.h
@@ -127,6 +127,8 @@ class ContextualTasksServiceImpl : public ContextualTasksService,
 
   void SetAiThreadSyncBridgeForTesting(
       std::unique_ptr<AiThreadSyncBridge> bridge);
+  void SetGeminiThreadSyncBridgeForTesting(
+      std::unique_ptr<GeminiThreadSyncBridge> bridge);
 
   // AiThreadSyncBridge::Observer implementation.
   void OnThreadDataStoreLoaded() override;
