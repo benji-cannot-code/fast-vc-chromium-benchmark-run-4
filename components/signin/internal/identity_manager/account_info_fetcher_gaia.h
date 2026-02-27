376298f8d142e77a98cf3df45402a9dea25a062a
Boris Sazonov <bsazonov@chromium.org>
2026-02-27 04:28:33 -0800

    [Signin] Use a callback in AccountInfoFetcherGaia
    Changes how AccountInfoFetcherGaia signals the user info fetch
    completion: instead of passing a pointer to AccountFetcherService
    and calling OnUserInfoFetchSuccess/OnUserInfoFetchFailure on this
    pointer, a callback is passed.
    
    Bug: 449708427
    Change-Id: I3894555a833c55079d04058cead365bf8ef15de9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7612608
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1591480}

--- components/signin/internal/identity_manager/account_info_fetcher_gaia.h
+++ components/signin/internal/identity_manager/account_info_fetcher_gaia.h
@@ -7,10 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_SIGNIN_INTERNAL_IDENTITY_MANAGER_ACCOUNT_INFO_FETCHER_GAIA_H_
 
 #include <memory>
+#include <optional>
 
+#include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/scoped_refptr.h"
 #include "components/signin/internal/identity_manager/account_info_fetcher.h"
+#include "components/signin/public/identity_manager/account_info.h"
 #include "google_apis/gaia/gaia_auth_consumer.h"
 #include "google_apis/gaia/gaia_oauth_client.h"
 #include "google_apis/gaia/oauth2_access_token_manager.h"
@@ -19,7 +22,6 @@ namespace network {
 class SharedURLLoaderFactory;
 }
 
-class AccountFetcherService;
 class ProfileOAuth2TokenService;
 
 // An account information fetcher that gets an OAuth token of appropriate
@@ -33,8 +35,8 @@ class AccountInfoFetcherGaia : public AccountInfoFetcher,
   AccountInfoFetcherGaia(
       ProfileOAuth2TokenService* token_service,
       scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
-      AccountFetcherService* service,
-      const CoreAccountId& account_id);
+      const CoreAccountId& account_id,
+      base::OnceCallback<void(std::optional<AccountInfo>)> callback);
 
   AccountInfoFetcherGaia(const AccountInfoFetcherGaia&) = delete;
   AccountInfoFetcherGaia& operator=(const AccountInfoFetcherGaia&) = delete;
@@ -59,8 +61,8 @@ class AccountInfoFetcherGaia : public AccountInfoFetcher,
 
   raw_ptr<ProfileOAuth2TokenService> token_service_;
   scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory_;
-  raw_ptr<AccountFetcherService> service_;
   const CoreAccountId account_id_;
+  base::OnceCallback<void(std::optional<AccountInfo>)> callback_;
 
   std::unique_ptr<OAuth2AccessTokenManager::Request> login_token_request_;
   std::unique_ptr<gaia::GaiaOAuthClient> gaia_oauth_client_;
