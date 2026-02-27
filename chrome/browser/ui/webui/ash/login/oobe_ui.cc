aaf725666cf6b46837645aa560f98118687cc20c
Hidehiko Abe <hidehiko@chromium.org>
2026-02-26 19:02:28 -0800

    Remove chrome/common/url_constants.h from oobe_ui.cc
    By introducing a new entry in ash::chrome_urls.
    
    BUG=485470549
    TEST=Tryjob
    
    Change-Id: I2a9233379e32a40c0a12bb4ad58facc4189dcf31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7610891
    Reviewed-by: Jun Ishiguro <junis@google.com>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1591286}

--- chrome/browser/ui/webui/ash/login/oobe_ui.cc
+++ chrome/browser/ui/webui/ash/login/oobe_ui.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/constants/ash_features.h"
 #include "ash/constants/ash_switches.h"
+#include "ash/constants/chrome_url_constants.h"
 #include "ash/constants/webui_url_constants.h"
 #include "ash/public/cpp/esim_manager.h"
 #include "ash/public/cpp/network_config_service.h"
@@ -141,7 +142,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
-#include "chrome/common/url_constants.h"
 #include "chrome/grit/browser_resources.h"
 #include "chrome/grit/chrome_unscaled_resources.h"
 #include "chrome/grit/component_extension_resources.h"
@@ -596,8 +596,8 @@ void OobeUI::ConfigureOobeDisplay() {
 
   // Set up the chrome://terms/ data source, for EULA content.
   content::URLDataSource::Add(
-      profile,
-      std::make_unique<AboutUIHTMLSource>(chrome::kChromeUITermsHost, profile));
+      profile, std::make_unique<AboutUIHTMLSource>(
+                   ash::chrome_urls::kChromeUITermsHost, profile));
 
   content::WebContents* contents = web_ui()->GetWebContents();
 
