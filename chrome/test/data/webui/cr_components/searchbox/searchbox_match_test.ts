9f6efdd6b1f846dcb25135e91db4d6ed8a30796d
Aviv Kiss <avivkiss@google.com>
2026-02-27 08:44:31 -0800

    [omnibox] Fix middle clicks for webui popup.
    Prior to this change the middle clicks were resulting in full navigation.
    
    Fixed: 474382499
    Change-Id: Ia2cfc61efbbeecc152645fe5d12c2f95d7e0125a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7613444
    Auto-Submit: Aviv Kiss <avivkiss@google.com>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Commit-Queue: Aviv Kiss <avivkiss@google.com>
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1591592}

--- chrome/test/data/webui/cr_components/searchbox/searchbox_match_test.ts
+++ chrome/test/data/webui/cr_components/searchbox/searchbox_match_test.ts
@@ -93,7 +93,7 @@ suite('CrComponentsRealboxMatchTest', () => {
 
     // Middle clicks are accepted.
     const middleClickEvent =
-        new MouseEvent('click', {button: 1, cancelable: true});
+        new MouseEvent('auxclick', {button: 1, cancelable: true});
     matchEl.dispatchEvent(middleClickEvent);
     assertTrue(middleClickEvent.defaultPrevented);
     const middleClickArgs =
