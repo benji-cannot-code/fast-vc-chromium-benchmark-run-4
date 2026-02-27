a2b0b2d6138d31c7e06f77c8db28038b5831741a
Yanting Yu <yyanting@google.com>
2026-02-26 18:25:42 -0800

    Add feature flag NtpSimplification.
    Bug: 486850084
    Change-Id: I06a38c0dc17406d0c83e2a122d59be7246c417a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7610026
    Reviewed-by: Xinyi Ji <xinyiji@chromium.org>
    Commit-Queue: Yanting Yu <yyanting@google.com>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1591259}

--- chrome/browser/flags/android/chrome_feature_list.h
+++ chrome/browser/flags/android/chrome_feature_list.h
@@ -207,6 +207,7 @@ BASE_DECLARE_FEATURE(kNotificationPermissionVariant);
 BASE_DECLARE_FEATURE(kNotificationTrampoline);
 BASE_DECLARE_FEATURE(kNotificationTrampolineNoNewTask);
 BASE_DECLARE_FEATURE(kNtpMvcRefactor);
+BASE_DECLARE_FEATURE(kNtpSimplification);
 BASE_DECLARE_FEATURE(kOmahaMinSdkVersionAndroid);
 BASE_DECLARE_FEATURE(kOnDemandBackgroundTabContextCapture);
 BASE_DECLARE_FEATURE(kOptimizeGeolocationHeaderGeneration);
