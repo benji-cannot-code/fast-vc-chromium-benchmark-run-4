df28b3f5719df6f4b9a4fbf000e74cda49da7d61
Briana McClure <bmcclure@google.com>
2026-02-27 15:42:52 -0800

    [iOS] Conditionally add "Close All" to the tab switcher overflow menu
    This change makes it so the "Close All" button in the Tab Switcher
    Overflow Menu only appears if a user has open tabs or groups.
    
    Fixed: 488229680
    Change-Id: I0b76b426d3b5c24c1e782d9f3d673ad0c5fa72f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7615693
    Commit-Queue: Briana McClure <bmcclure@google.com>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1591877}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/toolbars/tab_grid_top_toolbar.h
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/toolbars/tab_grid_top_toolbar.h
@@ -51,6 +51,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)setSearchButtonEnabled:(BOOL)enabled;
 // Sets `enabled` on the select tabs action.
 - (void)setSelectTabsActionEnabled:(BOOL)enabled;
+// Sets `enabled` on the close all tabs and groups action.
+- (void)setCloseAllActionEnabled:(BOOL)enabled;
 // Sets `enabled` on the select all button.
 - (void)setSelectAllButtonEnabled:(BOOL)enabled;
 // Sets `enabled` on the close other tabs action.
--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/toolbars/tab_grid_top_toolbar.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/toolbars/tab_grid_top_toolbar.mm
@@ -94,6 +94,7 @@ CGFloat HorizontalMargin() {
 
   BOOL _undoActive;
   BOOL _selectTabsActionEnabled;
+  BOOL _closeAllActionEnabled;
   BOOL _closeOtherTabsEnabled;
 
   BOOL _scrolledToEdge;
@@ -176,6 +177,11 @@ CGFloat HorizontalMargin() {
 
 - (void)setSelectTabsActionEnabled:(BOOL)enabled {
   _selectTabsActionEnabled = enabled;
+  _overflowMenuButton.menu = [self createOverflowMenu];
+}
+
+- (void)setCloseAllActionEnabled:(BOOL)enabled {
+  _closeAllActionEnabled = enabled;
   _overflowMenuButton.menu = [self createOverflowMenu];
 }
 
@@ -620,15 +626,18 @@ CGFloat HorizontalMargin() {
                     }]];
     }
 
-    UIButton* currentOverflowMenuButton = _overflowMenuButton;
-    [menuElements addObject:[actionFactory actionToCloseAllTabsWithBlock:^{
-                    TabGridTopToolbar* strongSelf = weakSelf;
-                    if (!strongSelf) {
-                      return;
-                    }
-                    [strongSelf.buttonsDelegate
-                        closeAllButtonTapped:currentOverflowMenuButton];
-                  }]];
+    // Only display the Close All Tabs button if there are open tabs or groups.
+    if (_closeAllActionEnabled) {
+      UIButton* currentOverflowMenuButton = _overflowMenuButton;
+      [menuElements addObject:[actionFactory actionToCloseAllTabsWithBlock:^{
+                      TabGridTopToolbar* strongSelf = weakSelf;
+                      if (!strongSelf) {
+                        return;
+                      }
+                      [strongSelf.buttonsDelegate
+                          closeAllButtonTapped:currentOverflowMenuButton];
+                    }]];
+    }
 
     if (_closeOtherTabsEnabled) {
       UIAction* closeOtherTabsAction =
