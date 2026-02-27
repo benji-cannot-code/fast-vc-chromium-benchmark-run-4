5f4fda4a068d83fa19e7f70e9295142ebb666c87
Ireneusz Szulc <ireneuszszulc@google.com>
2026-02-27 02:04:54 -0800

    @memory: Provide Autofill data retrieval logic
    This change introduces `AutofillDataRetriever` and related data types
    (`AutofillDataType`, `MemorySearchResult`) for later use by @memory
    feature.
    `AutofillDataRetriever` provides a unified interface to fetch matching
    records from local Autofill sources (e.g. `PersonalDataManager` or
    `EntityDataManager`) based on a classified data type.
    
    This establishes the foundational data access layer for the @memory
    feature, which will enable users to recall and insert personal
    information into any text field by typing a special trigger like "@@".
    
    Returned data are ordered according to Autofill Ranking Algorithm,
    based on frequency and recency of use.
    
    `annotation_reducer::QueryClassifier` will use `AtMemoryDataType` in
    a follow-up CL.
    
    Bug: b:481976444
    Change-Id: I57676d840ffec0d690eea3af468d0b9a6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7575175
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Ireneusz Szulc <ireneuszszulc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1591419}

---
+++ components/autofill/core/browser/at_memory/memory_search_result.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/autofill/core/browser/at_memory/memory_search_result.h"
+
+namespace autofill {
+
+MemorySearchResult::MemorySearchResult() = default;
+MemorySearchResult::MemorySearchResult(const MemorySearchResult&) = default;
+MemorySearchResult& MemorySearchResult::operator=(const MemorySearchResult&) =
+    default;
+MemorySearchResult::MemorySearchResult(MemorySearchResult&&) = default;
+MemorySearchResult& MemorySearchResult::operator=(MemorySearchResult&&) =
+    default;
+MemorySearchResult::~MemorySearchResult() = default;
+
+}  // namespace autofill
