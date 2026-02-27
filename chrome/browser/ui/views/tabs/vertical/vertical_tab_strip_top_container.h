a6b98c4d57b64ee6b4bd7b8729202e69623467b9
Kunal Daftari <kunaldaftari@google.com>
2026-02-27 15:42:07 -0800

    [Vertical Tabs] Collapse Tabs Behind Caption Buttons Fix
    This change accounts for the state in between "wrapped" and "collapsed."
    This delays the orientation change of the combo button so that it should
    not appear behind the caption buttons.
    
    Bug: 485875700
    Change-Id: I19b86728a2cc07400b01fe34cf5875bbd1c2ef79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7615419
    Commit-Queue: Kunal Daftari <kunaldaftari@google.com>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1591876}

--- chrome/browser/ui/views/tabs/vertical/vertical_tab_strip_top_container.h
+++ chrome/browser/ui/views/tabs/vertical/vertical_tab_strip_top_container.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_VIEWS_TABS_VERTICAL_VERTICAL_TAB_STRIP_TOP_CONTAINER_H_
 
 #include "ui/views/layout/delegating_layout_manager.h"
+#include "ui/views/layout/layout_types.h"
 #include "ui/views/view.h"
 
 class TabStripComboButton;
@@ -86,6 +87,10 @@ class VerticalTabStripTopContainer : public views::View,
   // rendered inside of it becaused that area is reserved for outside UI
   // elements.
   int caption_button_width_ = 0;
+
+  // This is updated during layout calculation and then applied during layout.
+  mutable views::LayoutOrientation combo_button_orientation_ =
+      views::LayoutOrientation::kHorizontal;
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_TABS_VERTICAL_VERTICAL_TAB_STRIP_TOP_CONTAINER_H_
