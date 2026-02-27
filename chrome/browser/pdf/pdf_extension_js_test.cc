d4a83b617646de76206a14360d9383ed5cb5d489
dpapad <dpapad@chromium.org>
2026-02-26 16:29:56 -0800

    Disable failing PDFExtensionContentSettingJSTest on linux-js-code-coverage.
    Bug: 481394108
    Change-Id: I90b433ce78c58af26174ae0b7230e562f1d58bfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7614146
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1591185}

--- chrome/browser/pdf/pdf_extension_js_test.cc
+++ chrome/browser/pdf/pdf_extension_js_test.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/with_feature_override.h"
 #include "base/threading/thread_restrictions.h"
 #include "build/build_config.h"
+#include "build/config/coverage/buildflags.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/pdf/pdf_extension_test_base.h"
 #include "chrome/browser/pdf/pdf_extension_test_util.h"
@@ -402,7 +403,13 @@ IN_PROC_BROWSER_TEST_P(PDFExtensionContentSettingJSTest, Beep) {
   RunTestsInJsModule("beep_test.js", "test-beep.pdf");
 }
 
-IN_PROC_BROWSER_TEST_P(PDFExtensionContentSettingJSTest, NoBeep) {
+#if BUILDFLAG(USE_JAVASCRIPT_COVERAGE)
+// TODO(crbug.com/481394108): Test fails with JS coverage turned on.
+#define MAYBE_NoBeep DISABLED_NoBeep
+#else
+#define MAYBE_NoBeep NoBeep
+#endif
+IN_PROC_BROWSER_TEST_P(PDFExtensionContentSettingJSTest, MAYBE_NoBeep) {
   SetPdfJavaScript(/*enabled=*/false);
   RunTestsInJsModule("nobeep_test.js", "test-beep.pdf");
 }
@@ -420,7 +427,13 @@ IN_PROC_BROWSER_TEST_P(PDFExtensionContentSettingJSTest, BeepThenNoBeep) {
   EXPECT_EQ(1, CountPDFProcesses());
 }
 
-IN_PROC_BROWSER_TEST_P(PDFExtensionContentSettingJSTest, NoBeepThenBeep) {
+#if BUILDFLAG(USE_JAVASCRIPT_COVERAGE)
+// TODO(crbug.com/481394108): Test fails with JS coverage turned on.
+#define MAYBE_NoBeepThenBeep DISABLED_NoBeepThenBeep
+#else
+#define MAYBE_NoBeepThenBeep NoBeepThenBeep
+#endif
+IN_PROC_BROWSER_TEST_P(PDFExtensionContentSettingJSTest, MAYBE_NoBeepThenBeep) {
   content::RenderProcessHost::SetMaxRendererProcessCount(1);
 
   SetPdfJavaScript(/*enabled=*/false);
