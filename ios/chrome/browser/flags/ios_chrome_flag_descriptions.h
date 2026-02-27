027ba929becb2cdd3d43bc56c6ce333b48fc9f03
Tarek Makkouk <tarekmakkouk@google.com>
2026-02-27 11:39:42 -0800

    [iOS] Add flag for Gemini on all pages
    Bug: 488374222
    Change-Id: I10855445407ac1b89b569f43cc25da0b3fb76940
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7617720
    Commit-Queue: Tarek Makkouk <tarekmakkouk@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1591704}

--- ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
+++ ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
@@ -403,6 +403,9 @@ extern const char kGeminiCopresenceDescription[];
 extern const char kGeminiDynamicSettingsName[];
 extern const char kGeminiDynamicSettingsDescription[];
 
+extern const char kGeminiFloatyAllPagesName[];
+extern const char kGeminiFloatyAllPagesDescription[];
+
 extern const char kGeminiFullChatHistoryName[];
 extern const char kGeminiFullChatHistoryDescription[];
 
