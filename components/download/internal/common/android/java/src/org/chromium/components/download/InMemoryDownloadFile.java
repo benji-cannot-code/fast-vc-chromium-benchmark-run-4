1b465ffa03c9c670bdb0156bf5b5e88bdec38327
Sky Malice <skym@chromium.org>
2026-02-27 13:56:05 -0800

    Fix non-member variable naming in InMemoryDownloadFile.
    Bug: None
    Change-Id: Ie6b480cd412fc0e70510e61fbfb2b1f65a0cb4c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7617638
    Commit-Queue: Sky Malice <skym@chromium.org>
    Auto-Submit: Sky Malice <skym@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1591793}

--- components/download/internal/common/android/java/src/org/chromium/components/download/InMemoryDownloadFile.java
+++ components/download/internal/common/android/java/src/org/chromium/components/download/InMemoryDownloadFile.java
@@ -45,9 +45,9 @@ public class InMemoryDownloadFile {
 
     @RequiresApi(Build.VERSION_CODES.R)
     private InMemoryDownloadFile(String filename) throws Exception {
-        FileDescriptor mFd = Os.memfd_create(filename, 0);
-        mPfd = ParcelFileDescriptor.dup(mFd);
-        mFos = new FileOutputStream(mFd);
+        FileDescriptor fd = Os.memfd_create(filename, 0);
+        mPfd = ParcelFileDescriptor.dup(fd);
+        mFos = new FileOutputStream(fd);
     }
 
     @CalledByNative
