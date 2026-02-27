9e9f73dc06cbd7ee9fcf406a455199e74dafaa8d
Mingyu Lei <leimy@chromium.org>
2026-02-26 19:55:50 -0800

    Reland "Webium product: fix view synchronizaiton on macOS"
    This reverts commit 447442bd9d874d52820f70f5d589b885a087379d.
    
    Reason for revert: change back to the callback list implementation and
    fix the crash.
    
    Original change's description:
    > Revert "Webium product: fix view synchronizaiton on macOS"
    >
    > This reverts commit 0066c5d3d4845b9a77359aef5b598fc33a502e7e.
    >
    > Reason for revert:
    > The Suspect CL https://chromium-review.googlesource.com/q/I5675ee8722183d8ad7af5f26e25b6fa0f0a0e0fd is highly likely the cause of the crash. Please see the crash report http://crash/287891963a1d2925.
    > Rebase the Revert CL on top of main branch with conflicts allowed option to prevent false positive merge conflicts.
    >
    > If you agree with the revert CL, please approve it and mark as Commit-Queue +2 to submit it.
    >
    > More details can be found in the go/crash-revert-cls-creation.
    >
    > Original change's description:
    > > Webium product: fix view synchronizaiton on macOS
    > >
    > > On macOS, the initial window is not launched through
    > > `BrowserView::Show()`, it's activated directly from
    > > `ProfileLaunchObserver::ActivateProfile()` (see the full callstack from
    > > the bug). This CL fixes that by putting the deferring logic in the
    > > `BrowserView::Activate()` as well.
    > >
    > > Bug: 482984811
    > > Change-Id: I5675ee8722183d8ad7af5f26e25b6fa0f0a0e0fd
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7556473
    > > Commit-Queue: Mingyu Lei <leimy@chromium.org>
    > > Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1585121}
    > >
    > Crash Bug: 484707993
    >
    > Change-Id: I69e1921d294ed91ebf72e203896191aa624054b7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7578110
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    > Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1585289}
    
    Change-Id: If57cef647c090b945191bb01c07bd479e0af1c73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7595199
    Commit-Queue: Mingyu Lei <leimy@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1591306}

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -1546,9 +1546,8 @@ void BrowserView::Show() {
   }
 
   if (auto* manager = InitialWebUIManager::From(browser())) {
-    if (manager->RequestDeferShow(
-            base::BindOnce(&BrowserView::OnInitialWebUIReady,
-                           weak_ptr_factory_.GetWeakPtr()))) {
+    if (manager->RequestDeferShow(base::BindOnce(
+            &BrowserView::Show, weak_ptr_factory_.GetWeakPtr()))) {
       return;
     }
   }
@@ -1631,6 +1630,13 @@ void BrowserView::Close() {
 }
 
 void BrowserView::Activate() {
+  if (auto* manager = InitialWebUIManager::From(browser())) {
+    if (manager->RequestDeferShow(base::BindOnce(
+            &BrowserView::Activate, weak_ptr_factory_.GetWeakPtr()))) {
+      return;
+    }
+  }
+
 #if !BUILDFLAG(IS_WIN) && !BUILDFLAG(IS_CHROMEOS)
   // Update the list managed by `BrowserList` synchronously the same way
   // `BrowserView::Show()` does.
@@ -6188,10 +6194,6 @@ void BrowserView::OnFirstPresentation(
     manager->OnBrowserWindowFirstPresentation(
         frame_timing_details.presentation_feedback.timestamp);
   }
-}
-
-void BrowserView::OnInitialWebUIReady() {
-  Show();
 }
 
 #if BUILDFLAG(ENTERPRISE_SCREENSHOT_PROTECTION)
--- chrome/browser/ui/waap/initial_web_ui_manager.h
+++ chrome/browser/ui/waap/initial_web_ui_manager.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_WAAP_INITIAL_WEB_UI_MANAGER_H_
 #define CHROME_BROWSER_UI_WAAP_INITIAL_WEB_UI_MANAGER_H_
 
-#include "base/functional/callback.h"
+#include "base/callback_list.h"
 #include "base/memory/raw_ptr.h"
 #include "ui/base/unowned_user_data/scoped_unowned_user_data.h"
 
@@ -30,7 +30,10 @@ class InitialWebUIManager {
   // finishes loading. Returns true if the deferral was successful and the state
   // was updated. In this case, `callback` will be invoked when the WebUI is
   // ready. Returns false if deferral is not applicable or not needed.
-  bool RequestDeferShow(base::OnceClosure callback);
+  // Note: the callback is unsafe since it will be added to the list and maybe
+  // invoked after the caller is already destructed. The caller should ensure
+  // the safety (e.g. by binding it to a weak pointer).
+  bool RequestDeferShow(base::OnceClosure unsafe_callback);
 
   bool IsShowPending() const;
 
@@ -39,8 +42,8 @@ class InitialWebUIManager {
   void OnWebUIToolbarLoaded();
 
  private:
-  // This callback is triggered when the initial WebUI is ready.
-  base::OnceClosure web_ui_ready_callback_;
+  // These callbacks are triggered when the initial WebUI is ready.
+  base::OnceClosureList web_ui_ready_callbacks_;
 
   // Tracks the physical loading state of the Initial WebUI.
   // True when the browser launches and the WebUI begins loading in the
