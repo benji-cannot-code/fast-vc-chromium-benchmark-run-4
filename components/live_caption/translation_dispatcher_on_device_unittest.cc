dc932052b7a7a37c2ec0ccb7db9d79d45ab2e8fe
Danilo Tedeschi <danft@google.com>
2026-02-27 13:50:00 -0800

    Reland "Use the OnDeviceTranslationInstaller::Observer in ServiceController"
    This reverts commit 583ece2c83648628dfe4f9279e3107b15c1d521b.
    
    Reason for revert: reland
    
    Original change's description:
    > Revert "Use the OnDeviceTranslationInstaller::Observer in ServiceController"
    >
    > This reverts commit a97c6aabd607a235efd3318dba34ce99380064de.
    >
    > Reason for revert: The CL regress OnDeviceTranslationBrowserTest.FrameDeletedWhileCreatingATranslator
    > e.g.
    > https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/56470/overview
    > https://ci.chromium.org/ui/p/chromium/builders/ci/Win10%20Tests%20x64/107000/overview
    >
    > Original change's description:
    > > Use the OnDeviceTranslationInstaller::Observer in ServiceController
    > >
    > > Remove the logic for observing the on-device-translation installation
    > > changes from the service controller and just use
    > > OnDeviceTranslationInstaller::Observer.
    > >
    > > Change-Id: I5b3d8fa0ea590d4d54ad0782ee74a3a28f598640
    > > Bug: Bug: 478827934
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7559294
    > > Commit-Queue: Danilo Françoso Tedeschi <danft@google.com>
    > > Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1582690}
    >
    > Bug: Bug: 478827934
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I0c4935d3eb5f51fb559ccd7d10ba1527769a8fae
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7564744
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Alvin Ji <alvinji@chromium.org>
    > Reviewed-by: Alvin Ji <alvinji@chromium.org>
    > Owners-Override: Alvin Ji <alvinji@chromium.org>
    > Auto-Submit: Alvin Ji <alvinji@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1582877}
    
    Bug: Bug: 478827934
    Change-Id: I2efa80e15325703b3698eba63eb284730d6a1bb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7614213
    Reviewed-by: Evan Liu <evliu@google.com>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Danilo Françoso Tedeschi <danft@google.com>
    Cr-Commit-Position: refs/heads/main@{#1591783}

--- components/live_caption/translation_dispatcher_on_device_unittest.cc
+++ components/live_caption/translation_dispatcher_on_device_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/run_loop.h"
 #include "components/on_device_translation/service_controller.h"
+#include "components/on_device_translation/test/fake_installer.h"
 #include "components/on_device_translation/test/fake_service_controller_manager.h"
 #include "components/on_device_translation/test/fake_translator.h"
 #include "components/prefs/testing_pref_service.h"
@@ -16,9 +17,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-using testing::_;
-
 namespace captions {
+namespace {
+
+using ::on_device_translation::FakeOnDeviceTranslationInstaller;
+using ::testing::_;
 
 using CreateTranslatorCallback = base::OnceCallback<void(
     base::expected<
@@ -55,6 +58,8 @@ class MockOnDeviceTranslationServiceController
 class TranslationDispatcherOnDeviceTest : public testing::Test {
  public:
   void SetUp() override {
+    fake_installer_ =
+        std::make_unique<FakeOnDeviceTranslationInstaller>(base::FilePath());
     service_controller_manager_ =
         std::make_unique<on_device_translation::FakeServiceControllerManager>(
             &local_state_);
@@ -91,6 +96,7 @@ class TranslationDispatcherOnDeviceTest : public testing::Test {
       translation_dispatcher_on_device_;
   std::string translated_text_;
   std::unique_ptr<on_device_translation::FakeTranslator> fake_translator_;
+  std::unique_ptr<FakeOnDeviceTranslationInstaller> fake_installer_;
 
   TranslationDispatcherOnDeviceTest() = default;
 };
@@ -172,4 +178,5 @@ TEST_F(TranslationDispatcherOnDeviceTest, GetTranslationFailureOnCanTranslate) {
   EXPECT_TRUE(translated_text_.empty());
 }
 
+}  // namespace
 }  // namespace captions
--- components/on_device_translation/service_controller.h
+++ components/on_device_translation/service_controller.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/no_destructor.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/types/expected.h"
+#include "components/on_device_translation/installer.h"
 #include "components/on_device_translation/public/mojom/on_device_translation_service.mojom.h"
 #include "components/on_device_translation/public/mojom/translator.mojom.h"
 #include "components/prefs/pref_change_registrar.h"
@@ -37,7 +38,8 @@ class ServiceControllerManager;
 // created for each pair of browser context and origin.
 // TODO(crbug.com/364795294): This class does not support Android yet.
 class OnDeviceTranslationServiceController
-    : public base::RefCounted<OnDeviceTranslationServiceController> {
+    : public base::RefCounted<OnDeviceTranslationServiceController>,
+      public OnDeviceTranslationInstaller::Observer {
  public:
   OnDeviceTranslationServiceController(PrefService* local_state,
                                        ServiceControllerManager* manager,
@@ -75,7 +77,14 @@ class OnDeviceTranslationServiceController
   struct LanguagePackInfo;
 
  protected:
-  virtual ~OnDeviceTranslationServiceController();
+  ~OnDeviceTranslationServiceController() override;
+  // OnDeviceTranslationInstaller::Observer
+  void OnLanguagePackInstalled(const LanguagePackKey lang_pack) override;
+  // OnDeviceTranslationInstaller::Observer
+  void OnLanguagePackInstallationChanged(
+      const LanguagePackKey lang_pack) override;
+  // OnDeviceTranslationInstaller::Observer
+  void OnInstallationChanged() override;
 
  private:
   friend base::RefCounted<OnDeviceTranslationServiceController>;
@@ -138,9 +147,6 @@ class OnDeviceTranslationServiceController
   // this amount of time, the service will be terminated.
   base::TimeDelta service_idle_timeout_;
   mojo::Remote<mojom::OnDeviceTranslationService> service_remote_;
-  // Used to listen for changes on the pref values of TranslateKit component and
-  // language pack components.
-  PrefChangeRegistrar pref_change_registrar_;
   // The file operation proxy to access the files on disk. This is deleted on
   // a background task runner.
   std::unique_ptr<FileOperationProxyImpl, base::OnTaskRunnerDeleter>
