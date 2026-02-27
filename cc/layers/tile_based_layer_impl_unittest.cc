6e130a52518ad04dd0f7d2ed8f4661646bac1b2b
Colin Blundell <blundell@chromium.org>
2026-02-27 11:53:54 -0800

    [//cc] Dedupe {Picture, TileDisplay}LayerImpl common computations
    Bug: 450651370
    Change-Id: Ide5fe0c92a1812c273d2653be4722ff626a0f2ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7614313
    Reviewed-by: Vikas Soni <vikassoni@chromium.org>
    Commit-Queue: Vikas Soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1591716}

--- cc/layers/tile_based_layer_impl_unittest.cc
+++ cc/layers/tile_based_layer_impl_unittest.cc
@@ -88,8 +88,10 @@ class TestTileBasedLayerImpl : public TileBasedLayerImpl<FakeTiling> {
                          AppendQuadsData* append_quads_data,
                          viz::SharedQuadState* shared_quad_state,
                          const Occlusion& scaled_occlusion,
+                         const gfx::Rect& offset_geometry_rect,
+                         const gfx::Rect& offset_visible_geometry_rect,
                          const gfx::Rect& visible_geometry_rect,
-                         const gfx::Vector2d& quad_offset,
+                         bool needs_blending,
                          const std::optional<gfx::Rect>& scaled_cull_rect,
                          float max_contents_scale,
                          AppendQuadsCustomSharedData* custom_data) override {
@@ -423,8 +425,10 @@ class OcclusionTestTileBasedLayerImpl : public TestTileBasedLayerImpl {
                          AppendQuadsData* append_quads_data,
                          viz::SharedQuadState* shared_quad_state,
                          const Occlusion& scaled_occlusion,
+                         const gfx::Rect& offset_geometry_rect,
+                         const gfx::Rect& offset_visible_geometry_rect,
                          const gfx::Rect& visible_geometry_rect,
-                         const gfx::Vector2d& quad_offset,
+                         bool needs_blending,
                          const std::optional<gfx::Rect>& scaled_cull_rect,
                          float max_contents_scale,
                          AppendQuadsCustomSharedData* custom_data) override {
@@ -581,12 +585,15 @@ class QuadOffsetTestTileBasedLayerImpl : public TestTileBasedLayerImpl {
                          AppendQuadsData* append_quads_data,
                          viz::SharedQuadState* shared_quad_state,
                          const Occlusion& scaled_occlusion,
+                         const gfx::Rect& offset_geometry_rect,
+                         const gfx::Rect& offset_visible_geometry_rect,
                          const gfx::Rect& visible_geometry_rect,
-                         const gfx::Vector2d& quad_offset,
+                         bool needs_blending,
                          const std::optional<gfx::Rect>& scaled_cull_rect,
                          float max_contents_scale,
                          AppendQuadsCustomSharedData* custom_data) override {
-    quad_offset_ = quad_offset;
+    quad_offset_ =
+        offset_geometry_rect.origin() - iter.geometry_rect().origin();
     // Create a dummy quad to avoid tripping debug checks.
     auto* quad =
         render_pass->CreateAndAppendDrawQuad<viz::SolidColorDrawQuad>();
@@ -663,8 +670,10 @@ class QuadOffsetOrderTestTileBasedLayerImpl : public TestTileBasedLayerImpl {
                          AppendQuadsData* append_quads_data,
                          viz::SharedQuadState* shared_quad_state,
                          const Occlusion& scaled_occlusion,
+                         const gfx::Rect& offset_geometry_rect,
+                         const gfx::Rect& offset_visible_geometry_rect,
                          const gfx::Rect& visible_geometry_rect,
-                         const gfx::Vector2d& quad_offset,
+                         bool needs_blending,
                          const std::optional<gfx::Rect>& scaled_cull_rect,
                          float max_contents_scale,
                          AppendQuadsCustomSharedData* custom_data) override {
