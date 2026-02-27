1bda421f1fd100469b7118ac70447b9cb00765d0
Mustafa Emre Acer <meacer@chromium.org>
2026-02-26 16:49:26 -0800

    Render RTL URLs properly in the PWA page info menu
    The PWA page info menu renders the App's origin as a minor text next
    to the "App Info" menu item. Presently, this doesn't handle RTL
    hostnames correctly. This CL fixes that by forcing directionality to
    LTR.
    
    Bug: 479326680
    Change-Id: I421e2886e21dbdb9a8e8b298e3cebaa8de49955f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7608472
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1591194}

--- ui/views/controls/menu/menu_item_view.cc
+++ ui/views/controls/menu/menu_item_view.cc
@@ -552,6 +552,11 @@ void MenuItemView::SetMinorText(const std::u16string& minor_text) {
   invalidate_dimensions();  // Triggers preferred size recalculation.
 }
 
+void MenuItemView::SetMinorTextIsUrl(bool is_url) {
+  minor_text_is_url_ = is_url;
+  invalidate_dimensions();  // Triggers preferred size recalculation.
+}
+
 void MenuItemView::SetMinorIcon(const ui::ImageModel& minor_icon) {
   minor_icon_ = minor_icon;
   invalidate_dimensions();  // Triggers preferred size recalculation.
@@ -1256,6 +1261,9 @@ void MenuItemView::PaintMinorIconAndText(gfx::Canvas* canvas, SkColor color) {
     render_text->SetDisplayRect(minor_text_bounds);
     render_text->SetHorizontalAlignment(base::i18n::IsRTL() ? gfx::ALIGN_LEFT
                                                             : gfx::ALIGN_RIGHT);
+    if (GetMinorTextIsUrl()) {
+      render_text->SetDirectionalityMode(gfx::DIRECTIONALITY_AS_URL);
+    }
     render_text->Draw(canvas);
   }
 
@@ -1525,6 +1533,10 @@ std::u16string MenuItemView::GetMinorText() const {
   return MenuConfig::instance().ShouldShowAcceleratorText(this, &accel_text)
              ? accel_text
              : minor_text_;
+}
+
+bool MenuItemView::GetMinorTextIsUrl() const {
+  return minor_text_is_url_;
 }
 
 ui::ImageModel MenuItemView::GetMinorIcon() const {
