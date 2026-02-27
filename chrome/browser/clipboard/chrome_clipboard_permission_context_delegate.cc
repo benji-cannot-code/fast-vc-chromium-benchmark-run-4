2429e010852a77b627dff6dcb153dc72a6d94c63
Keybo Qian <keybo@google.com>
2026-02-27 07:38:42 -0800

    Modularize //chrome/browser/clipboard
    This change extracts the clipboard permission context delegate and its
    related files from //chrome/browser into a new, more granular component
    located at //chrome/browser/clipboard.
    
    The new component is structured with :clipboard and :impl targets to
    properly separate the public API from the implementation details. This
    change is part of the ongoing effort to break down the large
    //chrome/browser target, which will help to clarify dependencies and
    improve build efficiency.
    
    A DEPS file is also added to enforce the new module boundary.
    
    Bug: 488057942
    Change-Id: I59f63aa43648181150ff52002344b2dc8bcee848
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7614698
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Auto-Submit: Keybo Qian <keybo@google.com>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1591553}

--- chrome/browser/clipboard/chrome_clipboard_permission_context_delegate.cc
+++ chrome/browser/clipboard/chrome_clipboard_permission_context_delegate.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/map_util.h"
 #include "base/functional/bind.h"
-#include "chrome/browser/profiles/profile.h"
 #include "components/content_settings/core/common/content_settings.h"
 #include "components/permissions/content_setting_permission_context_base.h"
 #include "components/permissions/permission_decision.h"
@@ -21,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_frame_host.h"
 #include "extensions/browser/guest_view/web_view/web_view_guest.h"
 #include "extensions/browser/guest_view/web_view/web_view_permission_helper.h"
+#include "third_party/blink/public/common/permissions/permission_utils.h"
+#include "url/gurl.h"
 
 ChromeClipboardPermissionContextDelegate::
     ChromeClipboardPermissionContextDelegate(Type type)
--- chrome/browser/clipboard/chrome_clipboard_permission_context_delegate.h
+++ chrome/browser/clipboard/chrome_clipboard_permission_context_delegate.h
@@ -9,11 +9,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 #include <set>
 
+#include "base/containers/flat_map.h"
+#include "base/memory/weak_ptr.h"
 #include "components/content_settings/core/common/content_settings.h"
-#include "components/content_settings/core/common/content_settings_types.h"
 #include "components/permissions/contexts/clipboard_permission_context_delegate.h"
-#include "content/public/browser/browser_context.h"
-#include "extensions/browser/guest_view/web_view/web_view_permission_helper.h"
+#include "url/origin.h"
+
+class GURL;
+
+namespace content {
+class RenderFrameHost;
+}
+
+namespace extensions {
+class WebViewPermissionHelper;
+}
 
 // The class adds logic to check permission for a frame inside of a WebView.
 class ChromeClipboardPermissionContextDelegate
