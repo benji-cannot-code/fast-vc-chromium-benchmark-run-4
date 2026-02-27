f074418b88c183eea46f2609ff0ef4cf8de0e388
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2026-02-26 22:44:32 -0800

    Import wpt@18f431a0eff9334febc25df3671b418ef72e3071
    https://github.com/web-platform-tests/wpt/compare/d3403b1fe...18f431a0e
    
    Using wpt-import in Chromium d7f27be12c4f4ed73e0878ba368d7520ac553776.
    With Chromium commits locally applied on WPT:
      c116224bea "Reland "LCP: Use largest painted image for web- [...]
      57df6bf701 "Fix focus ring for anchor elements"
      d81bbe9922 "[anchor] account for chaining offset in hit-testing [...]
      cace4f9d59 "LCP: remove .tentative from performance-entry- [...]
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    atotic@chromium.org:
      external/wpt/css/CSS2/abspos
    emilio@chromium.org:
      external/wpt/css/css-display
    hbos@chromium.org, hta@chromium.org, guidou@chromium.org, phancke@microsoft.com:
      external/wpt/webrtc
    hongchan@chromium.org, mjwilson@chromium.org:
      external/wpt/webaudio
    ricea@chromium.org, yyanagisawa@chromium.org:
      external/wpt/encoding
    
    NOAUTOREVERT=true
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac-skia-alt-arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac12.0-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac12.0.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac13.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac14.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac15.arm64-blink-rel
    Change-Id: Ie30fd78dc5aca3f97233db62c58d2eeb0e4d6e2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7614621
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1591347}

--- third_party/blink/web_tests/external/wpt/WebCryptoAPI/encrypt_decrypt/aes.js
+++ third_party/blink/web_tests/external/wpt/WebCryptoAPI/encrypt_decrypt/aes.js
@@ -36,6 +36,38 @@ function run_test() {
         all_promises.push(promise);
     });
 
+    // Check for successful encryption even if the buffer is changed while calling encrypt.
+    passingVectors.forEach(function(vector) {
+        var plaintext = copyBuffer(vector.plaintext);
+        plaintext[0] = 255 - plaintext[0];
+        var promise = importVectorKey(vector, ["encrypt", "decrypt"])
+        .then(function(vector) {
+            promise_test(function(test) {
+                var operation = subtle.encrypt({
+                    ...vector.algorithm,
+                    get name() {
+                        plaintext[0] = vector.plaintext[0];
+                        return vector.algorithm.name;
+                    }
+                }, vector.key, plaintext)
+                .then(function(result) {
+                    assert_true(equalBuffers(result, vector.result), "Should return expected result");
+                }, function(err) {
+                    assert_unreached("encrypt error for test " + vector.name + ": " + err.message);
+                });
+                return operation;
+            }, vector.name + " with altered plaintext during call");
+        }, function(err) {
+            // We need a failed test if the importVectorKey operation fails, so
+            // we know we never tested encryption
+            promise_test(function(test) {
+                assert_unreached("importKey failed for " + vector.name);
+            }, "importKey step: " + vector.name + " with altered plaintext during call");
+        });
+
+        all_promises.push(promise);
+    });
+
     // Check for successful encryption even if the buffer is changed after calling encrypt.
     passingVectors.forEach(function(vector) {
         var plaintext = copyBuffer(vector.plaintext);
@@ -50,13 +82,13 @@ function run_test() {
                 });
                 plaintext[0] = 255 - plaintext[0];
                 return operation;
-            }, vector.name + " with altered plaintext");
+            }, vector.name + " with altered plaintext after call");
         }, function(err) {
             // We need a failed test if the importVectorKey operation fails, so
             // we know we never tested encryption
             promise_test(function(test) {
                 assert_unreached("importKey failed for " + vector.name);
-            }, "importKey step: " + vector.name + " with altered plaintext");
+            }, "importKey step: " + vector.name + " with altered plaintext after call");
         });
 
         all_promises.push(promise);
@@ -85,7 +117,39 @@ function run_test() {
         all_promises.push(promise);
     });
 
-    // Check for successful decryption even if ciphertext is altered.
+    // Check for successful decryption even if ciphertext is altered while calling encrypt.
+    passingVectors.forEach(function(vector) {
+        var ciphertext = copyBuffer(vector.result);
+        ciphertext[0] = 255 - ciphertext[0];
+        var promise = importVectorKey(vector, ["encrypt", "decrypt"])
+        .then(function(vector) {
+            promise_test(function(test) {
+                var operation = subtle.decrypt({
+                    ...vector.algorithm,
+                    get name() {
+                        ciphertext[0] = vector.result[0];
+                        return vector.algorithm.name;
+                    }
+                }, vector.key, ciphertext)
+                .then(function(result) {
+                    assert_true(equalBuffers(result, vector.plaintext), "Should return expected result");
+                }, function(err) {
+                    assert_unreached("decrypt error for test " + vector.name + ": " + err.message);
+                });
+                return operation;
+            }, vector.name + " decryption with altered ciphertext during call");
+        }, function(err) {
+            // We need a failed test if the importVectorKey operation fails, so
+            // we know we never tested encryption
+            promise_test(function(test) {
+                assert_unreached("importKey failed for " + vector.name);
+            }, "importKey step for decryption: " + vector.name + " with altered ciphertext during call");
+        });
+
+        all_promises.push(promise);
+    });
+
+    // Check for successful decryption even if ciphertext is altered after calling encrypt.
     passingVectors.forEach(function(vector) {
         var ciphertext = copyBuffer(vector.result);
         var promise = importVectorKey(vector, ["encrypt", "decrypt"])
@@ -99,13 +163,13 @@ function run_test() {
                 });
                 ciphertext[0] = 255 - ciphertext[0];
                 return operation;
-            }, vector.name + " decryption with altered ciphertext");
+            }, vector.name + " decryption with altered ciphertext after call");
         }, function(err) {
             // We need a failed test if the importVectorKey operation fails, so
             // we know we never tested encryption
             promise_test(function(test) {
                 assert_unreached("importKey failed for " + vector.name);
-            }, "importKey step for decryption: " + vector.name + " with altered ciphertext");
+            }, "importKey step for decryption: " + vector.name + " with altered ciphertext after call");
         });
 
         all_promises.push(promise);
