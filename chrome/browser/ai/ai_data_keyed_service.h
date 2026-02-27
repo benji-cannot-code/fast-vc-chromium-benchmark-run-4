89827233813d5931a966dea0f3b5b296c4ed9c3d
Kene Okoye <kenok@google.com>
2026-02-27 14:01:38 -0800

    [EnableGlicCleanup] for chrome/browser/ai
    Bug: b:487336381
    Fixed: b:487341305
    Change-Id: I7badec4d3a6166e1d899c6ddbd741419df159709
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7616361
    Commit-Queue: Kene Okoye <kenok@google.com>
    Auto-Submit: Kene Okoye <kenok@google.com>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1591802}

--- chrome/browser/ai/ai_data_keyed_service.h
+++ chrome/browser/ai/ai_data_keyed_service.h
@@ -15,15 +15,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "build/build_config.h"
+#include "chrome/browser/actor/execution_engine.h"
+#include "chrome/browser/glic/host/context/glic_page_context_fetcher.h"
 #include "chrome/common/buildflags.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/optimization_guide/proto/features/actions_data.pb.h"
 #include "components/optimization_guide/proto/features/model_prototyping.pb.h"
 
-#if BUILDFLAG(ENABLE_GLIC)
-#include "chrome/browser/actor/execution_engine.h"
-#include "chrome/browser/glic/host/context/glic_page_context_fetcher.h"
-#endif
 
 namespace content {
 class WebContents;
