dd436fec0ca91714a997935941b9f067b6549a68
Anthony Vallee-Dubois <anthonyvd@google.com>
2026-02-27 08:18:35 -0800

    flat_hash_map in ProcessMonitor
    Bug: 482350910
    Change-Id: Ib0fbb53e6254cce8910c9ad218038c857548a64e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7617210
    Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Auto-Submit: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Reviewed-by: Olivier Li <olivierli@chromium.org>
    Commit-Queue: Olivier Li <olivierli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1591574}

--- chrome/browser/metrics/power/process_monitor.cc
+++ chrome/browser/metrics/power/process_monitor.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_constants.h"
 #include "extensions/buildflags/buildflags.h"
 #include "services/network/public/mojom/network_service.mojom.h"
+#include "third_party/abseil-cpp/absl/container/flat_hash_map.h"
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
 #include "chrome/browser/extensions/chrome_content_browser_client_extensions_part.h"
@@ -249,7 +250,7 @@ void ProcessMonitor::SampleAllProcesses(Observer* observer) {
   // Aggregate all metrics into a single sum, but also per their process type.
   Metrics aggregated_metrics;
   std::array<Metrics, MonitoredProcessType::kCount> per_type_metrics;
-  std::map<ProcessInfo::Key, Metrics> per_utility_subtype_metrics;
+  absl::flat_hash_map<ProcessInfo::Key, Metrics> per_utility_subtype_metrics;
   for (auto* process_info : process_infos) {
     Metrics metrics = SampleMetrics(*process_info->process_metrics);
 
