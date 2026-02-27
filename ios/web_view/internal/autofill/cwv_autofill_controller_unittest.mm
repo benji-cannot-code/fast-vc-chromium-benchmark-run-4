0cbca1d2885b4ef7df46788c403dd2af5cabdb78
Julia Sobiech <jsobiech@google.com>
2026-02-27 03:08:35 -0800

    [ios] Deprecate `userInitiated` arg
    Deprecate the `userInitiated` parameter from
    `didSubmitFormWithName:`.
    
    Bug: 477630132
    Change-Id: Ic9877ddf2d2afc6151f2973a50120626a9d23ae1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7571275
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Julia Sobiech <jsobiech@google.com>
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1591457}

--- ios/web_view/internal/autofill/cwv_autofill_controller_unittest.mm
+++ ios/web_view/internal/autofill/cwv_autofill_controller_unittest.mm
@@ -468,6 +468,10 @@ TEST_F(CWVAutofillControllerTest, SubmitCallback) {
   [[delegate expect] autofillController:autofill_controller_
                   didSubmitFormWithName:kTestFormName
                                 frameID:frame_id_
+                         perfectFilling:YES];
+  [[delegate expect] autofillController:autofill_controller_
+                  didSubmitFormWithName:kTestFormName
+                                frameID:frame_id_
                           userInitiated:YES
                          perfectFilling:YES];
   auto frame = web::FakeWebFrame::CreateMainWebFrame(GURL());
@@ -479,6 +483,10 @@ TEST_F(CWVAutofillControllerTest, SubmitCallback) {
       /*user_initiated=*/true,
       /*perfect_filling=*/true);
 
+  [[delegate expect] autofillController:autofill_controller_
+                  didSubmitFormWithName:kTestFormName
+                                frameID:frame_id_
+                         perfectFilling:NO];
   [[delegate expect] autofillController:autofill_controller_
                   didSubmitFormWithName:kTestFormName
                                 frameID:frame_id_
@@ -499,6 +507,11 @@ TEST_F(CWVAutofillControllerTest, SubmitCallbackAcrossIframes) {
   id delegate = OCMProtocolMock(@protocol(CWVAutofillControllerDelegate));
   autofill_controller_.delegate = delegate;
 
+  OCMExpect([delegate
+         autofillController:autofill_controller_
+      didSubmitFormWithName:kTestFormName
+                    frameID:base::SysUTF8ToNSString(web::kMainFakeFrameId)
+             perfectFilling:YES]);
   OCMExpect([delegate
          autofillController:autofill_controller_
       didSubmitFormWithName:kTestFormName
