b6ccdfa6bf43f7d453d1cc8341d057ee97ad0885
Patrick Monette <pmonette@chromium.org>
2026-02-27 12:31:53 -0800

    [Memory Coordinator] Add memory limit thresholds for pressure levels
    This CL introduces kModerateMemoryPressureThreshold and
    kCriticalMemoryPressureThreshold in base/memory_coordinator/utils.h.
    These constants represent the memory limit percentages (50% and 0%,
    respectively) that correspond to the legacy moderate and critical memory
    pressure levels.
    
    Bug: 465068849
    Change-Id: I89ec64b8ee0248e737b51f624421b2d7a5321327
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7606822
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1591745}

---
+++ base/memory_coordinator/utils.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_MEMORY_COORDINATOR_UTILS_H_
+#define BASE_MEMORY_COORDINATOR_UTILS_H_
+
+#include "base/memory_coordinator/memory_consumer.h"
+
+namespace base {
+
+// These constants represent memory limit thresholds (expressed as a percentage)
+// that correspond to legacy memory pressure levels. They are intended to assist
+// with the migration of clients from MemoryPressureListener to MemoryConsumer.
+inline constexpr int kNoMemoryPressureThreshold =
+    MemoryConsumer::kDefaultMemoryLimit;
+inline constexpr int kModerateMemoryPressureThreshold =
+    kNoMemoryPressureThreshold / 2;
+inline constexpr int kCriticalMemoryPressureThreshold = 0;
+
+}  // namespace base
+
+#endif  // BASE_MEMORY_COORDINATOR_UTILS_H_
