8988baf2ca2ed1aecf05d520959c1a0a69734226
Colin Blundell <blundell@chromium.org>
2026-02-27 00:30:01 -0800

    [Blink] Add NativeMappableSharedImagesSupportedForCanvas2D()
    Abstracts the checking that CanvasRenderingContext2D does for whether
    native mappable SIs are supported for Canvas2D in preparation for
    rationalizing that logic internally.
    
    Bug: 485669935
    Change-Id: Ic25f62c69f5e476b791081e4987a9c4604b13f1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7609561
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1591379}

--- third_party/blink/renderer/platform/graphics/gpu/shared_gpu_context.cc
+++ third_party/blink/renderer/platform/graphics/gpu/shared_gpu_context.cc
@@ -275,6 +275,11 @@ bool SharedGpuContext::MaySupportImageChromium() {
 }
 #endif  // BUILDFLAG(IS_ANDROID)
 
+bool SharedGpuContext::NativeMappableSharedImagesSupportedForCanvas2D() {
+  return MaySupportImageChromium() &&
+         RuntimeEnabledFeatures::Canvas2dImageChromiumEnabled();
+}
+
 bool SharedGpuContext::OverlaysSupportedForCanvas2D() {
   return MaySupportImageChromium() &&
          RuntimeEnabledFeatures::Canvas2dImageChromiumEnabled();
