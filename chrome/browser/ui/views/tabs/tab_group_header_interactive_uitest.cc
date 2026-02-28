edafdb23434ad6807fa85600e38ae61a90176968
Marwan Tammam <quarz@chromium.org>
2026-02-27 18:11:12 -0800

    Prevent collapsed tab groups from expanding during drag.
    Fixed: 484039750
    Change-Id: I28775ceb5801fcb96c7d6e37d24ed748584774b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7617407
    Commit-Queue: Marwan Tammam <quarz@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1591940}

--- chrome/browser/ui/views/tabs/tab_group_header_interactive_uitest.cc
+++ chrome/browser/ui/views/tabs/tab_group_header_interactive_uitest.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/test/bind.h"
 #include "chrome/browser/data_sharing/data_sharing_service_factory.h"
 #include "chrome/browser/tab_group_sync/tab_group_sync_service_factory.h"
 #include "chrome/browser/ui/browser_element_identifiers.h"
@@ -30,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/codec/png_codec.h"
 #include "ui/gfx/image/image_skia_operations.h"
 #include "ui/gfx/image/image_unittest_util.h"
+#include "ui/views/interaction/element_tracker_views.h"
 #include "ui/views/interaction/polling_view_observer.h"
 
 class TabGroupHeaderInteractiveUiTest
@@ -123,4 +125,29 @@ IN_PROC_BROWSER_TEST_F(TabGroupHeaderInteractiveUiTest, AttentionIndicator) {
                         ->group_header(group_id)
                         ->ShouldShowAttentionIndicator());
       }));
+}
+
+IN_PROC_BROWSER_TEST_F(TabGroupHeaderInteractiveUiTest, DragCollapsedGroup) {
+  CreateTabGroup({CreateTab()});
+
+  RunTestSequence(
+      WaitForShow(kTabGroupHeaderElementId), FinishTabstripAnimations(),
+      PollViewProperty(kTabGroupCollapsedState, kTabGroupHeaderElementId,
+                       &TabGroupHeader::is_collapsed_for_testing),
+      // Collapse the group
+      MoveMouseTo(kTabGroupHeaderElementId), ClickMouse(ui_controls::LEFT),
+      WaitForState(kTabGroupCollapsedState, true), FinishTabstripAnimations(),
+      // Drag the group header. We drag it a bit to the right.
+      MoveMouseTo(kTabGroupHeaderElementId),
+      DragMouseTo(kTabGroupHeaderElementId,
+                  base::BindLambdaForTesting([](ui::TrackedElement* el) {
+                    return el->AsA<views::TrackedElementViews>()
+                               ->view()
+                               ->GetBoundsInScreen()
+                               .CenterPoint() +
+                           gfx::Vector2d(50, 0);
+                  })),
+      // Verify it is still collapsed
+      CheckViewProperty(kTabGroupHeaderElementId,
+                        &TabGroupHeader::is_collapsed_for_testing, true));
 }
