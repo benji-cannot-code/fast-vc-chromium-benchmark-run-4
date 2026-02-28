4573b04dd6549f677aa42f23d962f5180052d67a
Keigo Oka <oka@google.com>
2026-02-27 18:04:30 -0800

    Site search: Support escaping keyword mode via backspace
    This allows users to exit site search mode by pressing the backspace key
    when the omnibox input field is empty.
    
    To support this interaction, `KeyNavigationUtil` is updated to detect
    the backspace key (KEYCODE_DEL). `UrlBar` is updated to forward
    backspace events to `LocationBarMediator`, which clears the active
    keyword state on the `AutocompleteInput` if the user text is currently
    empty.
    
    Bug: 459590224
    Change-Id: I15ed91a5282e4a06041a3eb56c4a20f8d54c2ff4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7611194
    Auto-Submit: Keigo Oka <oka@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1591934}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/UrlBarUnitTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/UrlBarUnitTest.java
@@ -752,7 +752,8 @@ public class UrlBarUnitTest {
                         KeyEvent.KEYCODE_DPAD_UP,
                         KeyEvent.KEYCODE_DPAD_DOWN,
                         KeyEvent.KEYCODE_DPAD_LEFT,
-                        KeyEvent.KEYCODE_DPAD_RIGHT);
+                        KeyEvent.KEYCODE_DPAD_RIGHT,
+                        KeyEvent.KEYCODE_DEL);
 
         var listener = mock(View.OnKeyListener.class);
         mUrlBar.setKeyDownListener(listener);
@@ -789,7 +790,8 @@ public class UrlBarUnitTest {
                         KeyEvent.KEYCODE_ENTER,
                         KeyEvent.KEYCODE_NUMPAD_ENTER,
                         KeyEvent.KEYCODE_DPAD_UP,
-                        KeyEvent.KEYCODE_DPAD_DOWN);
+                        KeyEvent.KEYCODE_DPAD_DOWN,
+                        KeyEvent.KEYCODE_DEL);
 
         var listener = mock(View.OnKeyListener.class);
         mUrlBar.setKeyDownListener(listener);
