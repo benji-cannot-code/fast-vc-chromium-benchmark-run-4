778628ddbf4c8e059397e00f6afdecf1c1195016
Alex Moshchuk <alexmos@chromium.org>
2026-02-28 11:18:02 -0800

    Add browser-side validation for allow-popups sandboxed frame attribute
    Sandboxed frames created without an allow-popups attribute should not
    be allowed to create popups. Previously, this attribute was only
    checked in the renderer. This CL adds additional validation in
    RenderFrameHostImpl::CreateNewWindow(), the browser process's
    entrypoint for creating popups via web APIs like window.open().
    
    Bug: 487471101
    Change-Id: I0776f9d6df4200ecf20b29e8ecd18efa2edcd45a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7617636
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Auto-Submit: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1592057}

--- content/browser/renderer_host/render_frame_host_impl.h
+++ content/browser/renderer_host/render_frame_host_impl.h
@@ -3467,6 +3467,8 @@ class CONTENT_EXPORT RenderFrameHostImpl
                            AttemptDuplicateRenderWidgetHost);
   FRIEND_TEST_ALL_PREFIXES(SecurityExploitBrowserTest,
                            BindToWebUIFromWebViaMojo);
+  FRIEND_TEST_ALL_PREFIXES(SecurityExploitBrowserTest,
+                           WindowOpenDisallowedFromSandboxedFrame);
   FRIEND_TEST_ALL_PREFIXES(SitePerProcessBrowserTest,
                            RenderViewHostIsNotReusedAfterDelayedUnloadACK);
   FRIEND_TEST_ALL_PREFIXES(SitePerProcessBrowserTest,
