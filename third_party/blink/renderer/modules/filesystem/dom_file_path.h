7f5550c09ae3c6a40e9a250ee81197aef3a04c4d
Fredrik Söderquist <fs@opera.com>
2026-03-01 15:08:29 -0800

    Convert some open-coded starts/ends_with
    Bug: 473854537
    Change-Id: I617cfd47790a490e2dc8dda0e85c9af890cf9ce0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7615641
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1592200}

--- third_party/blink/renderer/modules/filesystem/dom_file_path.h
+++ third_party/blink/renderer/modules/filesystem/dom_file_path.h
@@ -68,7 +68,7 @@ class DOMFilePath {
   }
 
   static bool EndsWithSeparator(const String& path) {
-    return path[path.length() - 1] == DOMFilePath::kSeparator;
+    return path.ends_with(DOMFilePath::kSeparator);
   }
 
   // Evaluates all "../" and "./" segments. Note that "/../" expands to "/", so
