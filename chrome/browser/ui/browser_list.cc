8e997c87bf6743c3ee0967847476b570f2d25b24
Thomas Lukaszewicz <tluk@chromium.org>
2026-03-02 13:38:03 -0800

    [bedrock] Remove BrowserListEnumerator
    Bypass-Check-License: file move in upstream CL
    Bug: 441824481
    Change-Id: I64b00c40749006d0df40946134355a54eabe03c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7537385
    Commit-Queue: Tom Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Qikai Zhong <qikaizhong@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1592716}

--- chrome/browser/ui/browser_list.cc
+++ chrome/browser/ui/browser_list.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
-#include "chrome/browser/ui/browser_list_enumerator.h"
 #include "chrome/browser/ui/browser_list_observer.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "components/keep_alive_registry/keep_alive_registry.h"
