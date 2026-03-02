07c40e90981902f9d713aa5d4fc0b08e0a09ba66
Peter Wen <wnwen@chromium.org>
2026-03-02 09:10:12 -0800

    build: Improve NoAndroidLog checker and add integration tests
    The NoAndroidLog checker was missing several edge cases when
    android.util.Log is imported. Specifically:
    - Static field access (e.g., Log.VERBOSE).
    - Static imports (e.g., import static android.util.Log.ASSERT;).
    - Class references (e.g., Log.class).
    
    This CL updates the checker to use both IdentifierTreeMatcher and
    MemberSelectTreeMatcher to catch these cases. It also adds logic to
    de-duplicate warnings on nested matches (e.g., only one warning for
    android.util.Log.d()).
    
    An integration test suite (NoAndroidLogNoCompileSources.java) is added
    to the nocompile test infrastructure to verify the checker's
    effectiveness and ensure @SuppressWarnings("NoAndroidLog") continues to
    work at the method and class level.
    
    Bug: 481747262
    Test: autoninja -C out/Debug tools/android/errorprone_plugin/test:errorprone_plugin_tests
    Change-Id: I43e12093f5421a3847d7b65ba8fd346ad6aa3c25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7609321
    Reviewed-by: Ziwen Lin <ziwlin@microsoft.com>
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Auto-Submit: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1592530}

---
+++ tools/android/errorprone_plugin/test/src/org/chromium/tools/errorprone/plugin/NoAndroidLogNoCompileSources.java
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.tools.errorprone.plugin;
+
+import static android.util.Log.ASSERT;
+
+import android.util.Log;
+
+/** Test for NoAndroidLog checker. */
+public class NoAndroidLogNoCompileSources {
+    public void testAll() {
+        Log.d("TAG", "msg");
+        android.util.Log.e("TAG", "msg");
+        int i = Log.VERBOSE;
+        int j = android.util.Log.INFO;
+        int k = ASSERT;
+        Class c = Log.class;
+    }
+}
