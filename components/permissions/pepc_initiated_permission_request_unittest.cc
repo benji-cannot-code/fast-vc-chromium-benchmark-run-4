ea99f8d7e359b12728960624e895d16f6acf8214
Thomas Nguyen <tungnh@chromium.org>
2026-03-02 06:38:23 -0800

    [CoCE]: Add support for usermedia element in Capabality Element
    Update the embedded permission control (EPC) infrastructure to support
    the <usermedia> element. These changes are also part of the broader
    effort to transition from a generic <permission> element to specific
    capability elements.
    
    Fixed: 488260158
    
    Change-Id: I4daf93e6fdc899ce6cb740247173253950cd6586
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7612107
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Thomas Nguyen <tungnh@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Ravjit Uppal <ravjit@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1592430}

--- components/permissions/pepc_initiated_permission_request_unittest.cc
+++ components/permissions/pepc_initiated_permission_request_unittest.cc
@@ -43,6 +43,7 @@ using blink::mojom::GeolocationEmbeddedPermissionRequestDescriptor;
 using blink::mojom::PermissionDescriptor;
 using blink::mojom::PermissionDescriptorPtr;
 using blink::mojom::PermissionName;
+using blink::mojom::UserMediaEmbeddedPermissionRequestDescriptor;
 }  // namespace
 
 class PEPCInitiatedPermissionRequestTest
@@ -127,10 +128,12 @@ class PEPCInitiatedPermissionRequestTest
   }
 
   EmbeddedPermissionRequestDescriptorPtr
-  CreateEmbeddedPermissionRequestDescriptorPtr() {
+  CreateUserMediaEmbeddedPermissionRequestDescriptorPtr() {
     EmbeddedPermissionRequestDescriptorPtr permission_descriptor =
         EmbeddedPermissionRequestDescriptor::New();
-
+    permission_descriptor->detail =
+        EmbeddedPermissionControlDescriptorExtension::NewUserMedia(
+            UserMediaEmbeddedPermissionRequestDescriptor::New());
     return permission_descriptor;
   }
 
@@ -213,7 +216,7 @@ TEST_F(PEPCInitiatedPermissionRequestTest, PEPCRequestWhenSettingAllowed) {
   // setting.
   permission_service()->RequestPageEmbeddedPermission(
       CreatePermissionDescriptorPtrs(ContentSettingsType::MEDIASTREAM_CAMERA),
-      CreateEmbeddedPermissionRequestDescriptorPtr(),
+      CreateUserMediaEmbeddedPermissionRequestDescriptorPtr(),
       base::BindOnce(
           &PEPCInitiatedPermissionRequestTest::PermissionServiceCallbackPEPC,
           base::Unretained(this)));
@@ -256,7 +259,7 @@ TEST_F(PEPCInitiatedPermissionRequestTest, PEPCRequestWhenSettingBlocked) {
   // setting.
   permission_service()->RequestPageEmbeddedPermission(
       CreatePermissionDescriptorPtrs(ContentSettingsType::MEDIASTREAM_MIC),
-      CreateEmbeddedPermissionRequestDescriptorPtr(),
+      CreateUserMediaEmbeddedPermissionRequestDescriptorPtr(),
       base::BindOnce(
           &PEPCInitiatedPermissionRequestTest::PermissionServiceCallbackPEPC,
           base::Unretained(this)));
@@ -283,7 +286,7 @@ TEST_F(PEPCInitiatedPermissionRequestTest, PEPCRequestBlockedInFencedFrame) {
   // A PEPC request is not allowed in a fenced frame.
   permission_service()->RequestPageEmbeddedPermission(
       CreatePermissionDescriptorPtrs(ContentSettingsType::MEDIASTREAM_MIC),
-      CreateEmbeddedPermissionRequestDescriptorPtr(),
+      CreateUserMediaEmbeddedPermissionRequestDescriptorPtr(),
       base::BindOnce(
           &PEPCInitiatedPermissionRequestTest::PermissionServiceCallbackPEPC,
           base::Unretained(this)));
@@ -318,7 +321,7 @@ TEST_F(PEPCInitiatedPermissionRequestTest,
   // A PEPC request is allowed through from a frame with a valid policy.
   permission_service()->RequestPageEmbeddedPermission(
       CreatePermissionDescriptorPtrs(ContentSettingsType::MEDIASTREAM_MIC),
-      CreateEmbeddedPermissionRequestDescriptorPtr(),
+      CreateUserMediaEmbeddedPermissionRequestDescriptorPtr(),
       base::BindOnce(
           &PEPCInitiatedPermissionRequestTest::PermissionServiceCallbackPEPC,
           base::Unretained(this)));
@@ -349,7 +352,7 @@ TEST_F(PEPCInitiatedPermissionRequestTest,
   // A PEPC request is not allowed through from a frame without a valid policy.
   permission_service()->RequestPageEmbeddedPermission(
       CreatePermissionDescriptorPtrs(ContentSettingsType::MEDIASTREAM_MIC),
-      CreateEmbeddedPermissionRequestDescriptorPtr(),
+      CreateUserMediaEmbeddedPermissionRequestDescriptorPtr(),
       base::BindOnce(
           &PEPCInitiatedPermissionRequestTest::PermissionServiceCallbackPEPC,
           base::Unretained(this)));
@@ -378,7 +381,7 @@ TEST_F(PEPCInitiatedPermissionRequestTest, PEPCRequestBlockedByKillSwitch) {
   // Attempt to make a PEPC request.
   permission_service()->RequestPageEmbeddedPermission(
       CreatePermissionDescriptorPtrs(ContentSettingsType::MEDIASTREAM_CAMERA),
-      CreateEmbeddedPermissionRequestDescriptorPtr(),
+      CreateUserMediaEmbeddedPermissionRequestDescriptorPtr(),
       base::BindOnce(
           &PEPCInitiatedPermissionRequestTest::PermissionServiceCallbackPEPC,
           base::Unretained(this)));
@@ -397,7 +400,7 @@ TEST_F(PEPCInitiatedPermissionRequestTest, PEPCRequestBlockedOnInsecureOrigin) {
   // Attempt to make a PEPC request.
   permission_service()->RequestPageEmbeddedPermission(
       CreatePermissionDescriptorPtrs(ContentSettingsType::MEDIASTREAM_CAMERA),
-      CreateEmbeddedPermissionRequestDescriptorPtr(),
+      CreateUserMediaEmbeddedPermissionRequestDescriptorPtr(),
       base::BindOnce(
           &PEPCInitiatedPermissionRequestTest::PermissionServiceCallbackPEPC,
           base::Unretained(this)));
