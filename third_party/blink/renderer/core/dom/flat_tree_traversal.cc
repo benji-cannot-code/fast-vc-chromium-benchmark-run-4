b9d69feb4abde5e14446ad8aa2b8b6e28e55c486
David Baron <dbaron@chromium.org>
2026-03-02 07:09:30 -0800

    Make DOM traversal iterators satisfy std::forward_iterator.
    This allows them to be used with std::ranges algorithms.
    
    Bug: 406566432
    Change-Id: I71f00cefafe57e8197254e503b378cadaca44740
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7618662
    Reviewed-by: Steinar H Gunderson <sesse@chromium.org>
    Commit-Queue: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1592452}

--- third_party/blink/renderer/core/dom/flat_tree_traversal.cc
+++ third_party/blink/renderer/core/dom/flat_tree_traversal.cc
@@ -34,6 +34,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+static_assert(std::forward_iterator<
+              TraversalIterator<TraversalParent<FlatTreeTraversal>>>);
+static_assert(std::forward_iterator<
+              TraversalDescendantIterator<TraversalParent<FlatTreeTraversal>>>);
+static_assert(std::forward_iterator<TraversalDescendantWithFilterIterator<
+                  TraversalParent<FlatTreeTraversal>,
+                  Element::TinyBloomFilter>>);
+static_assert(std::forward_iterator<TraversalInclusiveDescendantIterator<
+                  TraversalParent<FlatTreeTraversal>>>);
+
 #if DCHECK_IS_ON()
 void FlatTreeTraversal::AssertFlatTreeNodeDataUpdated(
     const Node& root,
