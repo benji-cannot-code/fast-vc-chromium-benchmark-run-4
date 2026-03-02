c764e03a2c69c58f7bb849017371d50df5d99c36
Marlon Facey <mfacey@chromium.org>
2026-03-02 10:42:24 -0800

    [Omnibox Next] Update logic to use spark loupe when there is header
    The logic was flipped. Now we use the spark loupe when there is a header
    and the arrow when there is not. Also updates the search loupe to be the
    spark loupe.
    
    Screenshot: http://screenshot/8qdTd4iRD492eAM
    
    Bug: 488106072
    Change-Id: Ifdcbc7bf16b9dfbcbea402545644f40a03e41789
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7617294
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1592582}

--- chrome/browser/ui/webui/cr_components/searchbox/searchbox_handler.cc
+++ chrome/browser/ui/webui/cr_components/searchbox/searchbox_handler.cc
@@ -68,7 +68,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 
 namespace searchbox_internal {
-const char* kSearchIconResourceName = "//resources/images/icon_search.svg";
+
+const char* kSearchSparkIconResourceName =
+    "//resources/cr_components/searchbox/icons/search_spark.svg";
 }  // namespace searchbox_internal
 
 namespace {
@@ -150,8 +152,7 @@ const char* kNotesSparkIconResourceName =
 const char* kPageIconResourceName =
     "//resources/cr_components/searchbox/icons/page.svg";
 const char* kPedalsIconResourceName = "//theme/current-channel-logo";
-const char* kSearchSparkIconResourceName =
-    "//resources/cr_components/searchbox/icons/search_spark.svg";
+const char* kSearchIconResourceName = "//resources/images/icon_search.svg";
 const char* kSparkIconResourceName =
     "//resources/cr_components/searchbox/icons/spark.svg";
 const char* kStarActiveIconResourceName =
@@ -216,7 +217,7 @@ static void DefineChromeRefreshRealboxIcons() {
       "//resources/cr_components/searchbox/icons/page_cr23.svg";
   kPedalsIconResourceName =
       "//resources/cr_components/searchbox/icons/chrome_product_cr23.svg";
-  searchbox_internal::kSearchIconResourceName =
+  kSearchIconResourceName =
       "//resources/cr_components/searchbox/icons/search_cr23.svg";
   kTabIconResourceName =
       "//resources/cr_components/searchbox/icons/tab_cr23.svg";
@@ -417,9 +418,9 @@ void SearchboxHandler::SetupWebUIDataSource(content::WebUIDataSource* source,
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
           ? kGoogleGIconResourceName
 #else
-          ? searchbox_internal::kSearchIconResourceName
+          ? kSearchIconResourceName
 #endif
-          : searchbox_internal::kSearchIconResourceName);
+          : kSearchIconResourceName);
 
   source->AddBoolean("searchboxVoiceSearch", enable_voice_search);
   source->AddBoolean("searchboxLensSearch", enable_lens_search);
@@ -552,7 +553,7 @@ std::string SearchboxHandler::AutocompleteIconToResourceName(
   } else if (icon.name == omnibox::kProductChromeRefreshIcon.name) {
     return kPedalsIconResourceName;
   } else if (icon.name == omnibox::kSearchSparkIcon.name) {
-    return kSearchSparkIconResourceName;
+    return searchbox_internal::kSearchSparkIconResourceName;
   } else if (icon.name == omnibox::kSparkIcon.name) {
     return kSparkIconResourceName;
   } else if (icon.name == omnibox::kStarActiveChromeRefreshIcon.name) {
@@ -566,7 +567,7 @@ std::string SearchboxHandler::AutocompleteIconToResourceName(
   } else if (icon.name == vector_icons::kHistoryChromeRefreshIcon.name) {
     return kHistoryIconResourceName;
   } else if (icon.name == vector_icons::kSearchChromeRefreshIcon.name) {
-    return searchbox_internal::kSearchIconResourceName;
+    return kSearchIconResourceName;
   }
 
   // Don't add new icons here. Add them alphabetically by `if` predicate. E.g.
