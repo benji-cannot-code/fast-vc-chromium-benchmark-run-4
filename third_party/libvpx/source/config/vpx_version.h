39258d260b2fddcd2478cbc88711e4ac0786de00
Cheng Chen <chengchen@google.com>
2026-03-02 15:11:07 -0800

    Roll src/third_party/libvpx/source/libvpx/ db312f06f..4fcebeabe (8 commits)
    https://chromium.googlesource.com/webm/libvpx.git/+log/db312f06fb50..4fcebeabe58e
    
    $ git log db312f06f..4fcebeabe --date=short --no-merges --format='%ad %ae %s'
    2026-02-26 jzern yuv_video_source: fix stride/width check w/16bpp
    2026-02-24 jzern yuv_video_source: fix reading files w/odd widths
    2026-02-24 kambalaDecapitator add support for msvc v145 / VS 2026
    2026-02-23 jzern encode_api_test: disable OssFuzz471723682
    2026-02-18 jzern vpx_enc_fuzzer: condition max frames on deadline
    2026-02-19 li.zhang2 Make direct calls to Neon variance paths in Neon subpel variance
    2026-02-19 li.zhang2 Add Armv8.4 Neon DotProd subpel variance paths
    2026-02-19 li.zhang2 Remove Armv8.4 Neon DotProd 4x4 variance kernel
    
    Bug: 308446709
    
    Created with:
      roll-dep src/third_party/libvpx/source/libvpx
    R=jzern@google.com
    
    Change-Id: I88f122d046082e7bbb83af557b548718afd0f94e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7623869
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Reviewed-by: Wan-Teh Chang <wtc@google.com>
    Reviewed-by: James Zern <jzern@google.com>
    Cr-Commit-Position: refs/heads/main@{#1592784}

--- third_party/libvpx/source/config/vpx_version.h
+++ third_party/libvpx/source/config/vpx_version.h
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define VERSION_MAJOR  1
 #define VERSION_MINOR  16
 #define VERSION_PATCH  0
-#define VERSION_EXTRA  "48-gdb312f06f"
+#define VERSION_EXTRA  "56-g4fcebeabe"
 #define VERSION_PACKED ((VERSION_MAJOR<<16)|(VERSION_MINOR<<8)|(VERSION_PATCH))
-#define VERSION_STRING_NOSP "v1.16.0-48-gdb312f06f"
-#define VERSION_STRING      " v1.16.0-48-gdb312f06f"
+#define VERSION_STRING_NOSP "v1.16.0-56-g4fcebeabe"
+#define VERSION_STRING      " v1.16.0-56-g4fcebeabe"
 #endif  // VPX_VERSION_H_
