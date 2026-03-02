5fce1df918c8d9eb0b9196accdfaca113ee37411
Adam Rice <ricea@chromium.org>
2026-03-01 23:04:43 -0800

    Revert "Reland "[WebAppInstallFinalizer_Refactor] Use FinalizeUpdateJob""
    This reverts commit 72099f44404e8c2685d35b9187657ab456c18070.
    
    Reason for revert: IsolatedWebAppApplyUpdateCommandTest.FailsIfInstallFinalizerFails test failing on Win10 bots: [4760:16180:FATAL:chrome\browser\web_applications\jobs\finalize_update_job.cc:116] Check failed: pending_update_info.has_value(). Isolated Web Apps can only be updated if `IsolationData::PendingUpdateInfo` is set.
    
    See https://luci-milo.appspot.com/ui/tests/p/chromium/clusters/reason-v6/e7177a07dfcb60a6c9c14b050ae80f75?annotated=false&historyTimeRange=30d&selectedMetrics=human-cls-failed-presubmit%2Ccritical-failures-exonerated%2Cfailures%2Cbuilds-failed-due-to-flaky-tests&tab=recent-failures
    
    Original change's description:
    > Reland "[WebAppInstallFinalizer_Refactor] Use FinalizeUpdateJob"
    >
    > This is a reland of commit 850730d71604a2ee150cc51022276510a6cbd517
    >
    > This reland fixes the win failure stemming from 2 locks trying to delete
    > files at the same time. The test shows that a file could not be found
    > for deletion, but ending the test with none of the files being deleted.
    >
    > Adding a .reset to the updateJobs' callback fixes this.
    >
    > Original change's description:
    > > [WebAppInstallFinalizer_Refactor] Use FinalizeUpdateJob
    > >
    > > This CL changes call sites to use the UpdateJob instead of the
    > > Installfinalizer for updates.
    > >
    > > Bug: 259703817
    > > Change-Id: I95d110f8ff00cbc2b995342a9f528f407f5aaeee
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7600753
    > > Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    > > Commit-Queue: Adriana Ixba <aixba@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1590562}
    >
    > Bug: 259703817
    > Change-Id: Ifc9b202d1401202a9c29fc9b8b1adc07e640882c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7614668
    > Commit-Queue: Adriana Ixba <aixba@chromium.org>
    > Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1591752}
    
    Bug: 259703817
    Change-Id: I31e0f349d1fc4c8bd6b0aef378760e79df005996
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7622912
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Adam Rice <ricea@google.com>
    Cr-Commit-Position: refs/heads/main@{#1592267}

--- chrome/browser/web_applications/isolated_web_apps/commands/isolated_web_app_apply_update_command_unittest.cc
+++ chrome/browser/web_applications/isolated_web_apps/commands/isolated_web_app_apply_update_command_unittest.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/strcat.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/gmock_expected_support.h"
-#include "base/test/run_until.h"
 #include "base/test/test_future.h"
 #include "base/types/expected.h"
 #include "chrome/browser/ui/web_applications/test/isolated_web_app_test_utils.h"
@@ -101,8 +100,6 @@ blink::mojom::ManifestPtr CreateDefaultManifest(const GURL& application_url,
   return manifest;
 }
 
-}  // namespace
-
 class IsolatedWebAppApplyUpdateCommandTest : public WebAppTest {
  protected:
   void SetUp() override {
@@ -149,26 +146,13 @@ class IsolatedWebAppApplyUpdateCommandTest : public WebAppTest {
     base::WriteFile(installed_path, "");
   }
 
-  template <typename T = IsolatedWebAppApplyUpdateCommand>
-  IsolatedWebAppApplyUpdateCommandResult ApplyUpdate() {
+  IsolatedWebAppApplyUpdateCommandResult ApplyPendingUpdate() {
     base::test::TestFuture<IsolatedWebAppApplyUpdateCommandResult> future;
-    auto command_helper = std::make_unique<IsolatedWebAppInstallCommandHelper>(
-        url_info_, fake_web_contents_manager().CreateDataRetriever());
+    fake_provider().scheduler().ApplyPendingIsolatedWebAppUpdate(
+        url_info_, /*optional_keep_alive=*/nullptr,
+        /*optional_profile_keep_alive=*/nullptr, future.GetCallback());
 
-    fake_provider().command_manager().ScheduleCommand(std::make_unique<T>(
-        url_info_,
-        IsolatedWebAppInstallCommandHelper::CreateIsolatedWebAppWebContents(
-            *profile()),
-        /*optional_keep_alive=*/nullptr,
-        /*optional_profile_keep_alive=*/nullptr, future.GetCallback(),
-        std::move(command_helper)));
     return future.Take();
-  }
-
-  void CallOnFinalized(IsolatedWebAppApplyUpdateCommand& command,
-                       const webapps::AppId& app_id,
-                       webapps::InstallResultCode update_result_code) {
-    command.OnFinalizedForTesting(app_id, update_result_code);
   }
 
   FakeWebContentsManager& fake_web_contents_manager() {
@@ -255,7 +239,7 @@ TEST_F(IsolatedWebAppApplyUpdateCommandTest, Succeeds) {
       url_info_.origin().GetURL().Resolve(kIconPath));
   icon_state.bitmaps = {web_app::CreateSquareIcon(32, SK_ColorWHITE)};
 
-  EXPECT_THAT(ApplyUpdate(), HasValue());
+  EXPECT_THAT(ApplyPendingUpdate(), HasValue());
 
   const WebApp* web_app =
       fake_provider().registrar_unsafe().GetAppById(url_info_.app_id());
@@ -272,7 +256,7 @@ TEST_F(IsolatedWebAppApplyUpdateCommandTest,
   test::AwaitStartWebAppProviderAndSubsystems(profile());
   fake_provider().Shutdown();
 
-  auto result = ApplyUpdate();
+  auto result = ApplyPendingUpdate();
   EXPECT_THAT(result,
               ErrorIs(Field(&IsolatedWebAppApplyUpdateCommandError::message,
                             HasSubstr("shutting down"))));
@@ -283,7 +267,7 @@ TEST_F(IsolatedWebAppApplyUpdateCommandTest, FailsIfIwaIsNotInstalled) {
   ASSERT_NO_FATAL_FAILURE(WriteUpdateBundleToDisk());
   CreateDefaultPageState();
 
-  auto result = ApplyUpdate();
+  auto result = ApplyPendingUpdate();
   EXPECT_THAT(result, ErrorIs(_));
 
   const WebApp* web_app =
@@ -299,7 +283,7 @@ TEST_F(IsolatedWebAppApplyUpdateCommandTest, FailsIfInstalledAppIsNotIsolated) {
   ASSERT_NO_FATAL_FAILURE(WriteUpdateBundleToDisk());
   CreateDefaultPageState();
 
-  auto result = ApplyUpdate();
+  auto result = ApplyPendingUpdate();
   EXPECT_THAT(result, ErrorIs(_));
 
   const WebApp* web_app =
@@ -314,7 +298,7 @@ TEST_F(IsolatedWebAppApplyUpdateCommandTest,
   InstallIwa(/*pending_update_info=*/std::nullopt);
   CreateDefaultPageState();
 
-  auto result = ApplyUpdate();
+  auto result = ApplyPendingUpdate();
   EXPECT_THAT(result,
               ErrorIs(Field(&IsolatedWebAppApplyUpdateCommandError::message,
                             HasSubstr("does not have a pending update"))));
@@ -328,7 +312,7 @@ TEST_F(IsolatedWebAppApplyUpdateCommandTest, FailsIfAppNotTrusted) {
   CreateDefaultPageState();
   SetTrustedWebBundleIdsForTesting({});
 
-  auto result = ApplyUpdate();
+  auto result = ApplyPendingUpdate();
   EXPECT_THAT(result, ErrorIs(_));
   ExpectAppNotUpdatedAndDataCleared();
 }
@@ -341,7 +325,7 @@ TEST_F(IsolatedWebAppApplyUpdateCommandTest, FailsIfUrlLoadingFails) {
   page_state.url_load_result =
       webapps::WebAppUrlLoaderResult::kFailedErrorPageLoaded;
 
-  auto result = ApplyUpdate();
+  auto result = ApplyPendingUpdate();
   EXPECT_THAT(result,
               ErrorIs(Field(&IsolatedWebAppApplyUpdateCommandError::message,
                             HasSubstr("FailedErrorPageLoaded"))));
@@ -357,7 +341,7 @@ TEST_F(IsolatedWebAppApplyUpdateCommandTest, FailsIfInstallabilityCheckFails) {
   page_state.error_code =
       webapps::InstallableStatusCode::MANIFEST_MISSING_NAME_OR_SHORT_NAME;
 
-  auto result = ApplyUpdate();
+  auto result = ApplyPendingUpdate();
   EXPECT_THAT(
       result,
       ErrorIs(Field(
@@ -375,7 +359,7 @@ TEST_F(IsolatedWebAppApplyUpdateCommandTest, FailsIfManifestIsInvalid) {
   page_state.manifest_before_default_processing->scope =
       GURL("https://example.com/foo/");
 
-  auto result = ApplyUpdate();
+  auto result = ApplyPendingUpdate();
   EXPECT_THAT(result,
               ErrorIs(Field(&IsolatedWebAppApplyUpdateCommandError::message,
                             HasSubstr("Scope should resolve to the origin"))));
@@ -388,7 +372,7 @@ TEST_F(IsolatedWebAppApplyUpdateCommandTest, FailsIfIconDownloadFails) {
   ASSERT_NO_FATAL_FAILURE(WriteUpdateBundleToDisk());
   CreateDefaultPageState();
 
-  auto result = ApplyUpdate();
+  auto result = ApplyPendingUpdate();
   EXPECT_THAT(result,
               ErrorIs(Field(&IsolatedWebAppApplyUpdateCommandError::message,
                             HasSubstr("Error during icon downloading"))));
@@ -396,6 +380,23 @@ TEST_F(IsolatedWebAppApplyUpdateCommandTest, FailsIfIconDownloadFails) {
 }
 
 TEST_F(IsolatedWebAppApplyUpdateCommandTest, FailsIfInstallFinalizerFails) {
+  class FailingUpdateFinalizer : public WebAppInstallFinalizer {
+   public:
+    explicit FailingUpdateFinalizer(webapps::AppId app_id)
+        : WebAppInstallFinalizer(nullptr), app_id_(std::move(app_id)) {}
+
+    void FinalizeUpdate(const WebAppInstallInfo& web_app_info,
+                        InstallFinalizedCallback callback) override {
+      std::move(callback).Run(app_id_,
+                              webapps::InstallResultCode::kNotInstallable);
+    }
+
+   private:
+    webapps::AppId app_id_;
+  };
+
+  fake_provider().SetInstallFinalizer(
+      std::make_unique<FailingUpdateFinalizer>(url_info_.app_id()));
   test::AwaitStartWebAppProviderAndSubsystems(profile());
 
   InstallIwa(update_info());
@@ -406,32 +407,12 @@ TEST_F(IsolatedWebAppApplyUpdateCommandTest, FailsIfInstallFinalizerFails) {
       url_info_.origin().GetURL().Resolve(kIconPath));
   icon_state.bitmaps = {web_app::CreateSquareIcon(32, SK_ColorWHITE)};
 
-  base::test::TestFuture<IsolatedWebAppApplyUpdateCommandResult> future;
-  auto command_helper = std::make_unique<IsolatedWebAppInstallCommandHelper>(
-      url_info_, fake_web_contents_manager().CreateDataRetriever());
-
-  auto command = std::make_unique<IsolatedWebAppApplyUpdateCommand>(
-      url_info_,
-      IsolatedWebAppInstallCommandHelper::CreateIsolatedWebAppWebContents(
-          *profile()),
-      /*optional_keep_alive=*/nullptr,
-      /*optional_profile_keep_alive=*/nullptr, future.GetCallback(),
-      std::move(command_helper));
-
-  auto* command_ptr = command.get();
-  fake_provider().command_manager().ScheduleCommand(std::move(command));
-
-  ASSERT_TRUE(base::test::RunUntil([&]() { return command_ptr->IsStarted(); }));
-
-  // Manually trigger a failure on the command's OnFinalized.
-  CallOnFinalized(*command_ptr, url_info_.app_id(),
-                  webapps::InstallResultCode::kNotInstallable);
-
-  auto result = future.Take();
+  auto result = ApplyPendingUpdate();
   EXPECT_THAT(result,
               ErrorIs(Field(&IsolatedWebAppApplyUpdateCommandError::message,
                             HasSubstr("Error during finalization"))));
   ExpectAppNotUpdatedAndDataCleared();
 }
 
+}  // namespace
 }  // namespace web_app
