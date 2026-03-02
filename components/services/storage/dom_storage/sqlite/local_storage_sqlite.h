00822f584d7df342b84f2d60cc7b92beeda0adc8
Steve Becker <stevebe@microsoft.com>
2026-03-02 14:02:02 -0800

    [DomStorage] Use base::SequenceBound in AsyncDomStorageDatabase
    - Simplifies `AsyncDomStorageDatabase` by using `base::SequenceBound` directly with `AsyncCall()`.
    
    - Deletes `AsyncDomStorageDatabase::RunDatabaseTask()` and `DomStorageDatabaseFactory::CreateSequenceBoundDomStorageDatabase`.
    
    - Renames `DomStorageDatabaseFactory::Open()` to be `Create()` and updates the function to return the `base::SequenceBound<DomStorageDatabase>` immediately instead of through a callback.
    
    Bug: 377242771
    Change-Id: I082f67b71b214f5617153a16975fc61d72079bfe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7614121
    Reviewed-by: Evan Stade <evanstade@microsoft.com>
    Reviewed-by: Rahul Singh <rahsin@microsoft.com>
    Commit-Queue: Steve Becker <stevebe@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1592728}

--- components/services/storage/dom_storage/sqlite/local_storage_sqlite.h
+++ components/services/storage/dom_storage/sqlite/local_storage_sqlite.h
@@ -40,7 +40,7 @@ class LocalStorageSqlite : public DomStorageDatabase {
   using PassKey = base::PassKey<DomStorageDatabaseFactory>;
 
  public:
-  // Use `DomStorageDatabaseFactory::Open()` to construct a
+  // Use `DomStorageDatabaseFactory::Create()` to construct a
   // base::SequenceBound<DomStorageDatabase>.
   explicit LocalStorageSqlite(PassKey);
   ~LocalStorageSqlite() override;
@@ -48,12 +48,10 @@ class LocalStorageSqlite : public DomStorageDatabase {
   LocalStorageSqlite(const LocalStorageSqlite&) = delete;
   LocalStorageSqlite& operator=(const LocalStorageSqlite&) = delete;
 
-  DbStatus Open(PassKey,
-                const base::FilePath& database_path,
-                const std::optional<base::trace_event::MemoryAllocatorDumpGuid>&
-                    memory_dump_id);
-
   // The `DomStorageDatabase` interface:
+  DbStatus Open(const base::FilePath& database_path,
+                const std::optional<base::trace_event::MemoryAllocatorDumpGuid>&
+                    memory_dump_id) override;
   StatusOr<std::map<Key, Value>> ReadMapKeyValues(
       MapLocator map_locator) override;
   DbStatus UpdateMaps(std::vector<MapBatchUpdate> map_updates) override;
--- components/services/storage/dom_storage/sqlite/session_storage_sqlite_unittest.cc
+++ components/services/storage/dom_storage/sqlite/session_storage_sqlite_unittest.cc
@@ -107,9 +107,9 @@ void SessionStorageSqliteTest::OpenOnDisk(
   std::unique_ptr<SessionStorageSqlite> instance =
       std::make_unique<SessionStorageSqlite>(GetPassKey());
 
-  DbStatus status = instance->Open(GetPassKey(),
-                                   /*database_path=*/database_path,
-                                   /*memory_dump_id=*/std::nullopt);
+  DbStatus status = instance->Open(
+      /*database_path=*/database_path,
+      /*memory_dump_id=*/std::nullopt);
 
   ASSERT_TRUE(status.ok()) << status.ToString();
   *result = std::move(instance);
@@ -120,9 +120,9 @@ void SessionStorageSqliteTest::OpenInMemory(
   std::unique_ptr<SessionStorageSqlite> instance =
       std::make_unique<SessionStorageSqlite>(GetPassKey());
 
-  DbStatus status = instance->Open(GetPassKey(),
-                                   /*database_path=*/base::FilePath(),
-                                   /*memory_dump_id=*/std::nullopt);
+  DbStatus status = instance->Open(
+      /*database_path=*/base::FilePath(),
+      /*memory_dump_id=*/std::nullopt);
 
   ASSERT_TRUE(status.ok()) << status.ToString();
   *result = std::move(instance);
@@ -185,9 +185,9 @@ TEST_F(SessionStorageSqliteTest, VersionTooNew) {
 
   // Opening the database with the wrong version must fail.
   database = std::make_unique<SessionStorageSqlite>(GetPassKey());
-  DbStatus status = database->Open(GetPassKey(),
-                                   /*database_path=*/database_path,
-                                   /*memory_dump_id=*/std::nullopt);
+  DbStatus status = database->Open(
+      /*database_path=*/database_path,
+      /*memory_dump_id=*/std::nullopt);
   EXPECT_TRUE(status.IsNotFound());
 }
 
