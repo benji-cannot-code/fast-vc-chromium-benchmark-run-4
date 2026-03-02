ec41fd56955537232adb588fdf673bd6b19736bf
Colin Blundell <blundell@chromium.org>
2026-03-02 04:33:05 -0800

    [Blink] Inline Canvas2dImageChromiumEnabled in LowLatencyUsageSupported
    Inlines Canvas2dImageChromiumEnabled() [1] into its callsite at
    LowLatencyUsageSupportedForCanvas2D() and eliminates
    Canvas2dImageChromiumEnabled(). Note that MaySupportImageChromium() is
    hardcoded to true on Apple [2]. Followup work will rename that method to
    MaySupportWebGLImageChromium() once all Canvas2D-specific callsites are
    removed.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/7615008
    [2]
    https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/gpu/shared_gpu_context.h;l=60-64?q=MaySupportImageC&ss=chromium
    
    Bug: 485669935
    Change-Id: I3b317b63c344e26830323b32bfcb6612163117fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7613970
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1592370}

--- third_party/blink/renderer/platform/graphics/gpu/shared_gpu_context.cc
+++ third_party/blink/renderer/platform/graphics/gpu/shared_gpu_context.cc
@@ -46,14 +46,6 @@ bool Canvas2DSharedImagesBackedByIOSurface() {
 }
 #endif
 
-bool Canvas2dImageChromiumEnabled() {
-#if BUILDFLAG(IS_APPLE)
-  return Canvas2DSharedImagesBackedByIOSurface();
-#else
-  return false;
-#endif
-}
-
 }  // namespace
 
 SharedGpuContext* SharedGpuContext::GetInstanceForCurrentThread() {
@@ -349,9 +341,11 @@ bool SharedGpuContext::LowLatencyUsageSupportedForCanvas2D() {
     return true;
   }
 
-  if (MaySupportImageChromium() && Canvas2dImageChromiumEnabled()) {
+#if BUILDFLAG(IS_APPLE)
+  if (Canvas2DSharedImagesBackedByIOSurface()) {
     return true;
   }
+#endif
 
   if (MaySupportImageChromium() &&
       base::FeatureList::IsEnabled(
