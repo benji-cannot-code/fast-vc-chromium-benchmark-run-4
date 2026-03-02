a509329ff3f3a00373cf56ce9a58634ef9f99e46
Xi Han <hanxi@google.com>
2026-03-02 09:51:21 -0800

    [Theme] Clean up logs and polish.
    The issue of the wrong toolbar position on NTP is no longer repro,
    but logs show a different timing. Reduce the logs and polish the
    logging logic to prevent timing changes.
    
    Bug: 485266759
    Change-Id: I505ef2c34e3307791f1ce5d9e32e996a08d24f04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7622847
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1592551}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
@@ -40,7 +40,6 @@ import org.chromium.base.Callback;
 import org.chromium.base.CallbackController;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.JavaExceptionReporter;
-import org.chromium.base.Log;
 import org.chromium.base.TimeUtils;
 import org.chromium.base.TraceEvent;
 import org.chromium.base.ValueChangedCallback;
@@ -254,8 +253,6 @@ public class ToolbarManager
                 TintObserver,
                 MenuButtonDelegate,
                 TabObscuringHandler.Observer {
-    private static final String TAG = "ToolbarManager";
-
     private final IncognitoStateProvider mIncognitoStateProvider;
     private final TopUiThemeColorProvider mTopUiThemeColorProvider;
     private @Nullable final AdjustedTopUiThemeColorProvider mAdjustedTopUiThemeColorProvider;
@@ -363,7 +360,6 @@ public class ToolbarManager
     private final SettableNonNullObservableSupplier<@ControlsPosition Integer>
             mToolbarPositionSupplier = ObservableSuppliers.createNonNull(ControlsPosition.NONE);
     private final OneshotSupplier<ChromeAndroidTask> mChromeAndroidTaskSupplier;
-    private final boolean mEnableLogs;
 
     private @MonotonicNonNull HomeButtonCoordinator mHomeButtonCoordinator;
     private @MonotonicNonNull ToggleTabStackButtonCoordinator mTabSwitcherButtonCoordinator;
@@ -870,7 +866,6 @@ public class ToolbarManager
         mToolbarLayout = mActivity.findViewById(R.id.toolbar);
         NewTabPageDelegate ntpDelegate = createNewTabPageDelegate();
         mIsCustomTab = mToolbarLayout instanceof CustomTabToolbar;
-        mEnableLogs = ChromeFeatureList.sNewTabPageCustomizationV2EnableLogs.getValue();
 
         mLocationBarModel =
                 new LocationBarModel(
@@ -3201,20 +3196,7 @@ public class ToolbarManager
      * the new page.
      */
     private void checkIfNtpShowingWithNoPendingLoad() {
-        GURL url = mLocationBarModel.getCurrentGurl();
-        boolean isNtpUrl = UrlUtilities.isNtpUrl(url);
-
-        if (mEnableLogs) {
-            Log.i(
-                    TAG,
-                    "Get NewTabPage for the current tab: [null: %b] [isNtpUrl: %b] [isEmpty: %b]"
-                            + " [isValid: %b]",
-                    getNewTabPageForCurrentTab() == null,
-                    isNtpUrl,
-                    url.isEmpty(),
-                    url.isValid());
-        }
-
+        boolean isNtpUrl = UrlUtilities.isNtpUrl(mLocationBarModel.getCurrentGurl());
         if (isNtpUrl && getNewTabPageForCurrentTab() != null) {
             mIsNtpWithFakeboxShowingSupplier.set(NewTabPage.isInSingleUrlBarMode(mIsTablet));
         } else {
@@ -3233,12 +3215,6 @@ public class ToolbarManager
             mLocationBarModel.notifyNtpStartedLoading();
         }
 
-        if (mEnableLogs) {
-            Log.i(
-                    TAG,
-                    "Check if Ntp loaded with current tab %s a NTP.",
-                    ntp != null ? "is" : "isn't");
-        }
         checkIfNtpShowingWithNoPendingLoad();
 
         if (mToolbarPositionController != null) {
@@ -3288,9 +3264,6 @@ public class ToolbarManager
         if (updateUrl) {
             mLocationBarModel.notifyUrlChanged(false);
             updateButtonStatus();
-            if (mEnableLogs) {
-                Log.i(TAG, "Update tab loading state to check if NTP showing.");
-            }
             checkIfNtpShowingWithNoPendingLoad();
         }
     }
