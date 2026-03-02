3346118fb16839d7d85dc4c52bde49a252b5d54f
kenoss <kenoss@chromium.org>
2026-03-02 03:08:14 -0800

    Prerender2Fallback: Do not abort prerender if redirect
    Before this CL, prerender with redirects was aborted if
    `Prerender2FallbackPrefetchSpecRules` is enabled. This is because
    `PrefetchStatus::kPrefetchResponseUsed` is set when the last redirect is
    done, but `PrerenderURLLoaderThrottle` assumed that it is done at the
    head of redirects.
    
    This CL resolves it by adding a field
    `PreloadPipelineInfoImpl::does_prerender_matched_with_prefetch_`, which
    is set true when some prefetch matched to the prerender.
    
    This CL also enables `kPrerender2FallbackPrefetchSpecRules` in
    `PrerenderBrowserTest.SameSiteCrossOrigin*`.
    
    This CL uses `Validate-Test-Flakiness: skip` because the existing tests
    (with suffix /0) are judged as flaky. It's out of scope of this CL. Note
    that "Flaky Rate By Runs" of
    `PrerenderBrowserTest.SameSiteCrossOriginCredentialedPrerenderRedirectionSpeculationRulesWithoutOptInHeader`
    is 7.14% on android, for example.
    
    Bug: 375330756
    Fixed: 485796953
    Validate-Test-Flakiness: skip
    Change-Id: I9c551cc1e243dbe2f9e3208d6bb2bba5aec9a7ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7596854
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Commit-Queue: Ken Okada <kenoss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1592341}

--- content/browser/preloading/prefetch/prefetch_match_resolver.cc
+++ content/browser/preloading/prefetch/prefetch_match_resolver.cc
@@ -54,6 +54,7 @@ PrefetchMatchResolver::PrefetchMatchResolver(
     PrefetchServiceWorkerState expected_service_worker_state,
     bool is_nav_prerender,
     base::WeakPtr<PrerenderHost> prerender_host,
+    scoped_refptr<PreloadPipelineInfoImpl> preload_pipeline_info,
     Callback callback,
     perfetto::Flow flow)
     : navigation_request_for_metrics_(std::move(navigation_request)),
@@ -64,6 +65,7 @@ PrefetchMatchResolver::PrefetchMatchResolver(
       flow_(std::move(flow)),
       is_nav_prerender_(is_nav_prerender),
       prerender_host_for_metrics_(std::move(prerender_host)),
+      preload_pipeline_info_(std::move(preload_pipeline_info)),
       prefetch_match_metrics_(std::make_unique<PrefetchMatchMetrics>()) {
   switch (expected_service_worker_state_) {
     case PrefetchServiceWorkerState::kAllowed:
@@ -144,6 +146,9 @@ void PrefetchMatchResolver::FindPrefetch(
 
     return prerender_host->GetWeakPtr();
   })();
+  scoped_refptr<PreloadPipelineInfoImpl> preload_pipeline_info =
+      prerender_host ? prerender_host->preload_pipeline_info_scoped_refptr()
+                     : nullptr;
 
   TRACE_EVENT_END("loading");
 
@@ -151,8 +156,9 @@ void PrefetchMatchResolver::FindPrefetch(
       std::move(navigation_request), prefetch_service, std::move(navigated_key),
       expected_service_worker_state,
       frame_tree_node->frame_tree().is_prerendering(),
-      std::move(prerender_host), std::move(serving_page_metrics_container),
-      std::move(callback), std::move(flow));
+      std::move(prerender_host), std::move(preload_pipeline_info),
+      std::move(serving_page_metrics_container), std::move(callback),
+      std::move(flow));
 }
 
 // static
@@ -167,8 +173,9 @@ void PrefetchMatchResolver::FindPrefetchForTesting(
   PrefetchMatchResolver::FindPrefetchInternal1(
       /*navigation_request=*/nullptr, prefetch_service,
       std::move(navigated_key), expected_service_worker_state, is_nav_prerender,
-      /*prerender_host=*/nullptr, std::move(serving_page_metrics_container),
-      std::move(callback), perfetto::Flow::ProcessScoped(0));
+      /*prerender_host=*/nullptr, /*preload_pipeline_info=*/nullptr,
+      std::move(serving_page_metrics_container), std::move(callback),
+      perfetto::Flow::ProcessScoped(0));
 }
 
 // static
@@ -179,6 +186,7 @@ void PrefetchMatchResolver::FindPrefetchInternal1(
     PrefetchServiceWorkerState expected_service_worker_state,
     bool is_nav_prerender,
     base::WeakPtr<PrerenderHost> prerender_host,
+    scoped_refptr<PreloadPipelineInfoImpl> preload_pipeline_info,
     base::WeakPtr<PrefetchServingPageMetricsContainer>
         serving_page_metrics_container,
     Callback callback,
@@ -193,7 +201,8 @@ void PrefetchMatchResolver::FindPrefetchInternal1(
   auto prefetch_match_resolver = base::WrapUnique(new PrefetchMatchResolver(
       std::move(navigation_request), prefetch_service.GetWeakPtr(),
       std::move(navigated_key), expected_service_worker_state, is_nav_prerender,
-      std::move(prerender_host), std::move(callback), std::move(flow)));
+      std::move(prerender_host), std::move(preload_pipeline_info),
+      std::move(callback), std::move(flow)));
   PrefetchMatchResolver& ref = *prefetch_match_resolver.get();
   ref.self_ = std::move(prefetch_match_resolver);
 
@@ -712,6 +721,13 @@ void PrefetchMatchResolver::UnblockForMatch(const PrefetchKey& prefetch_key) {
   }
 
   // Postprocess for success case.
+
+  // If this is for a prerender initial navigation, mark its preload pipeline is
+  // using a prefetch. Note that the prefetch may not be the prefetch ahead of
+  // prerender.
+  if (preload_pipeline_info_) {
+    preload_pipeline_info_->MarkPrerenderMatchedWithPrefetch();
+  }
 
   PrefetchServingHandle serving_handle =
       prefetch_container.CreateServingHandle();
--- content/browser/preloading/preload_pipeline_info_impl.h
+++ content/browser/preloading/preload_pipeline_info_impl.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "base/unguessable_token.h"
 #include "content/browser/preloading/prefetch/prefetch_status.h"
+#include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/preload_pipeline_info.h"
 #include "content/public/browser/preloading.h"
 #include "third_party/perfetto/include/perfetto/tracing/track_event_args.h"
@@ -50,6 +51,11 @@ class CONTENT_EXPORT PreloadPipelineInfoImpl final
   }
   void SetPrefetchStatus(PrefetchStatus prefetch_status);
 
+  bool is_prerender_matched_with_prefetch() const {
+    return is_prerender_matched_with_prefetch_;
+  }
+  void MarkPrerenderMatchedWithPrefetch();
+
  private:
   friend class base::RefCounted<PreloadPipelineInfo>;
 
@@ -64,6 +70,10 @@ class CONTENT_EXPORT PreloadPipelineInfoImpl final
   PreloadingEligibility prefetch_eligibility_ =
       PreloadingEligibility::kUnspecified;
   std::optional<PrefetchStatus> prefetch_status_ = std::nullopt;
+
+  // Set to true when the prerender in this pipeline matches a prefetch.
+  // Note that the prefetch may not be the prefetch in this pipeline.
+  bool is_prerender_matched_with_prefetch_ = false;
 };
 
 }  // namespace content
