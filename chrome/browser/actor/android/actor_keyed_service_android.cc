97962061a2de36e1424bf2a4ca34e9aafa868cac
Hailey Wang <haileywang@google.com>
2026-03-02 15:24:49 -0800

    [Glic] TabModel integration and unit tests
    Bug: 484430578
    Change-Id: I21ec832b4bd36478bf2efccd1dc0fd85301ef81a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7598076
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1592794}

--- chrome/browser/actor/android/actor_keyed_service_android.cc
+++ chrome/browser/actor/android/actor_keyed_service_android.cc
@@ -7,14 +7,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "base/android/jni_android.h"
 #include "base/android/jni_array.h"
 #include "base/android/jni_string.h"
 #include "base/functional/bind.h"
 #include "chrome/browser/actor/actor_keyed_service_factory.h"
 #include "chrome/browser/actor/android/actor_task_android.h"
+#include "chrome/browser/profiles/profile.h"
+
+// Must come after all headers that specialize FromJniType() / ToJniType().
 #include "chrome/browser/actor/android/jni_headers/ActorKeyedServiceFactory_jni.h"
 #include "chrome/browser/actor/android/jni_headers/ActorKeyedService_jni.h"
-#include "chrome/browser/profiles/profile.h"
 
 using base::android::AttachCurrentThread;
 using base::android::JavaRef;
@@ -81,7 +84,12 @@ ActorKeyedServiceAndroid::GetActiveTasks(JNIEnv* env) {
     j_tasks.push_back(ActorTaskAndroid::GetForTask(const_cast<ActorTask*>(task))
                           ->GetJavaObject());
   }
-  return base::android::ToJavaArrayOfObjects(env, j_tasks);
+  // TODO(crbug.com/489134045): Try using JniType to convert this array.
+  return base::android::ToTypedJavaArrayOfObjects(
+      env, j_tasks,
+      base::android::GetClass(env,
+                              "org/chromium/chrome/browser/actor/ActorTask")
+          .obj());
 }
 
 int32_t ActorKeyedServiceAndroid::GetActiveTasksCount(JNIEnv* env) {
