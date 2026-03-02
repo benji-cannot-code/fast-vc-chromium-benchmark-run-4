088ce79774dee03c084ad89369c8f7063bb2ec28
Greg Thompson <grt@chromium.org>
2026-03-02 11:21:37 -0800

    [persistent_cache] Resource keys are opaque binary data
    Use base::span<const uint8_t> rather than std::string_view for keys in
    PersistentCacheCollection.
    
    Also update some comments.
    
    Bug: 374930286
    Change-Id: I4e8860554d032c2e58885e76be42096feb39efc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7619378
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1592606}

--- content/browser/code_cache/generated_code_cache_context.cc
+++ content/browser/code_cache/generated_code_cache_context.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <optional>
 #include <string>
-#include <string_view>
 
 #include "base/containers/span.h"
 #include "base/feature_list.h"
@@ -238,7 +237,7 @@ GeneratedCodeCacheContext::ShareReadOnlyConnection(
 
 void GeneratedCodeCacheContext::InsertIntoPersistentCacheCollection(
     const std::string& context_key,
-    std::string_view url,
+    base::span<const uint8_t> resource_key,
     base::span<const uint8_t> content,
     persistent_cache::EntryMetadata metadata) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
@@ -247,15 +246,8 @@ void GeneratedCodeCacheContext::InsertIntoPersistentCacheCollection(
     return;
   }
 
-  // Since `content` is coming in through mojo it's important to make sure that
-  // it's copied so it cannot be modified racily. This happens implicitly
-  // because of the way the SQLite backend (the only backend available
-  // currently) of PersistentCache stores data through the BLOB type.
-  //
-  // TODO(crbug.com/377475540): Make an explicit copy here once PersistentCache
-  // handles taking ownership of the memory passed in.
   RETURN_IF_ERROR(persistent_cache_collection_->Insert(
-                      context_key, base::as_byte_span(url), content, metadata),
+                      context_key, resource_key, content, metadata),
                   [](persistent_cache::TransactionError error) {
                     // TODO(crbug.com/374930286): Handle or at least address
                     // permanent errors.
@@ -266,7 +258,7 @@ void GeneratedCodeCacheContext::InsertIntoPersistentCacheCollection(
 std::optional<GeneratedCodeCacheContext::MetadataAndContent>
 GeneratedCodeCacheContext::FindInPersistentCacheCollection(
     const std::string& context_key,
-    std::string_view url) {
+    base::span<const uint8_t> resource_key) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   if (!persistent_cache_collection_) {
@@ -282,18 +274,17 @@ GeneratedCodeCacheContext::FindInPersistentCacheCollection(
     return base::span(content_buffer);
   };
 
-  ASSIGN_OR_RETURN(
-      std::optional<persistent_cache::EntryMetadata> metadata,
-      persistent_cache_collection_->Find(context_key, base::as_byte_span(url),
-                                         std::move(buffer_provider)),
-      // An adapter that is invoked on error. Its return value
-      // percolates up out of this function.
-      [](persistent_cache::TransactionError error)
-          -> std::optional<MetadataAndContent> {
-        // TODO(crbug.com/374930286): Handle or at least address
-        // permanent errors.
-        return std::nullopt;
-      });
+  ASSIGN_OR_RETURN(std::optional<persistent_cache::EntryMetadata> metadata,
+                   persistent_cache_collection_->Find(
+                       context_key, resource_key, std::move(buffer_provider)),
+                   // An adapter that is invoked on error. Its return value
+                   // percolates up out of this function.
+                   [](persistent_cache::TransactionError error)
+                       -> std::optional<MetadataAndContent> {
+                     // TODO(crbug.com/374930286): Handle or at least address
+                     // permanent errors.
+                     return std::nullopt;
+                   });
 
   if (!metadata.has_value()) {
     return std::nullopt;  // Cache miss.
