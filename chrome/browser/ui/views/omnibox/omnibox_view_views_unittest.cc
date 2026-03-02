edeec86d128f527e0c7d031dfe2197b465e67774
Keren Zhu <kerenzhu@chromium.org>
2026-03-02 12:36:23 -0800

    Replace WIDGET_OWNS_NATIVE_WIDGET with CLIENT_OWNS_WIDGET in chrome/browser/ui/views/omnibox
    We are refactoring Views code to use CLIENT_OWNS_WIDGET to make the
    ownership model clearer. This CL changes WIDGET_OWNS_NATIVE_WIDGET to
    CLIENT_OWNS_WIDGET.
    
    This CL was uploaded by git cl split.
    
    Bug: 339619005
    Change-Id: I0b7b0955453f14268ef0170523fdb85237c49f7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7619848
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1592639}

--- chrome/browser/ui/views/omnibox/omnibox_view_views_unittest.cc
+++ chrome/browser/ui/views/omnibox/omnibox_view_views_unittest.cc
@@ -490,7 +490,7 @@ void OmniboxViewViewsTest::SetUp() {
 
   // We need a widget so OmniboxView can be correctly focused and unfocused.
   widget_ =
-      CreateTestWidget(views::Widget::InitParams::WIDGET_OWNS_NATIVE_WIDGET);
+      CreateTestWidget(views::Widget::InitParams::CLIENT_OWNS_WIDGET);
   widget_->Show();
 
   AutocompleteClassifierFactory::GetInstance()->SetTestingFactoryAndUse(
@@ -1764,7 +1764,7 @@ TEST_F(OmniboxViewViewsSteadyStateElisionsTest,
   // Create a different Widget that will take focus away from the test widget
   // containing our test Omnibox.
   std::unique_ptr<views::Widget> other_widget =
-      CreateTestWidget(views::Widget::InitParams::WIDGET_OWNS_NATIVE_WIDGET);
+      CreateTestWidget(views::Widget::InitParams::CLIENT_OWNS_WIDGET);
   other_widget->Show();
   ExpectFullUrlDisplayed();
 
