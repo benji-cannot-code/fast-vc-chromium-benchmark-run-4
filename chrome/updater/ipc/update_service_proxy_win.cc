1161aa16b40e98ed408e170505bdeeb55ec7a91d
Sorin Jianu <sorin@chromium.org>
2026-03-02 13:58:21 -0800

    udpater: fix CHECK in UpdaterObserver::QueryResult
    Fixed: 436653135
    Change-Id: I8125cbb051b483637032a831add8a412b21d4ef7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7624043
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1592725}

--- chrome/updater/ipc/update_service_proxy_win.cc
+++ chrome/updater/ipc/update_service_proxy_win.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/updater/ipc/update_service_proxy.h"
 #include "chrome/updater/registration_data.h"
 #include "chrome/updater/service_proxy_factory.h"
+#include "chrome/updater/update_service.h"
 #include "chrome/updater/updater_scope.h"
 #include "chrome/updater/util/win_util.h"
 #include "chrome/updater/win/win_constants.h"
@@ -211,13 +212,13 @@ class UpdaterObserver : public DYNAMICIIDSIMPL(IUpdaterObserver) {
 
   static UpdateService::Result QueryResult(ICompleteStatus* complete_status) {
     CHECK(complete_status);
-
     LONG code = 0;
     base::win::ScopedBstr message;
-    CHECK(SUCCEEDED(complete_status->get_statusCode(&code)));
-
-    VLOG(2) << "ICompleteStatus::OnComplete(" << code << ")";
-    return static_cast<UpdateService::Result>(code);
+    HRESULT hr = complete_status->get_statusCode(&code);
+    VLOG(2) << "ICompleteStatus::OnComplete: code=" << code
+            << ", hr=" << std::hex << hr;
+    return SUCCEEDED(hr) ? static_cast<UpdateService::Result>(code)
+                         : UpdateService::Result::kIPCConnectionFailed;
   }
 
   // Called by IUpdaterObserver::OnStateChange when update state changes occur.
