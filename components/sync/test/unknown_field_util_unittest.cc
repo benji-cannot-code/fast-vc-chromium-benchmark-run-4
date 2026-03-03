da406be449f8a288d2fa7ec87b6dab06660cd806
Rushan Suleymanov <rushans@google.com>
2026-03-03 06:28:27 -0800

    [Skills] Preserve unknown skill sources on commit
    If there are new skill sources added in future client versions, they
    would be parsed as an unknown source. When committing back to the
    server, it would be overwritten with the same unknown value.
    
    This CL preserves the original skill source enum value by keeping it in
    trimmed specifics.
    
    NO_IFTTT=Only trimming logic has changed, no new fields were added.
    
    Bug: b:482927802
    Change-Id: If999199182c2f7f39f03eabf2f1b739112b89857
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7622039
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1593186}

---
+++ components/sync/test/unknown_field_util_unittest.cc
@@ -0,0 +1,96 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/sync/test/unknown_field_util.h"
+
+#include "components/sync/test/unknown_fields.pb.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace syncer::test {
+namespace {
+
+TEST(UnknownFieldUtilTest, AddAndGetUnknownField) {
+  sync_pb::test::UnknownFields proto;
+  const std::string kValue = "unknown_value";
+
+  AddUnknownFieldToProto(proto, kValue);
+
+  EXPECT_EQ(GetUnknownFieldValueFromProto(proto), kValue);
+}
+
+TEST(UnknownFieldUtilTest, GetMissingUnknownField) {
+  sync_pb::test::UnknownFields proto;
+
+  EXPECT_TRUE(GetUnknownFieldValueFromProto(proto).empty());
+}
+
+TEST(UnknownFieldUtilTest, AddAndGetUnknownEnumField) {
+  sync_pb::test::UnknownFields proto;
+  const int kFieldNumber = 12345;
+  const int kValue = 67890;
+
+  AddUnknownEnumFieldToProto(proto, kFieldNumber, kValue);
+
+  EXPECT_EQ(GetUnknownEnumFieldValueFromProto(proto, kFieldNumber), kValue);
+}
+
+TEST(UnknownFieldUtilTest, GetMissingUnknownEnumField) {
+  sync_pb::test::UnknownFields proto;
+  const int kFieldNumber = 12345;
+
+  EXPECT_EQ(GetUnknownEnumFieldValueFromProto(proto, kFieldNumber), -1);
+}
+
+TEST(UnknownFieldUtilTest, MultipleUnknownFields) {
+  sync_pb::test::UnknownFields proto;
+  const int kFieldNumber1 = 12345;
+  const int kValue1 = 111;
+  const int kFieldNumber2 = 12346;
+  const int kValue2 = 222;
+
+  AddUnknownEnumFieldToProto(proto, kFieldNumber1, kValue1);
+  AddUnknownEnumFieldToProto(proto, kFieldNumber2, kValue2);
+
+  EXPECT_EQ(GetUnknownEnumFieldValueFromProto(proto, kFieldNumber1), kValue1);
+  EXPECT_EQ(GetUnknownEnumFieldValueFromProto(proto, kFieldNumber2), kValue2);
+}
+
+TEST(UnknownFieldUtilTest, OverwriteUnknownEnumField) {
+  sync_pb::test::UnknownFields proto;
+  const int kFieldNumber = 12345;
+  const int kValue1 = 111;
+  const int kValue2 = 222;
+
+  AddUnknownEnumFieldToProto(proto, kFieldNumber, kValue1);
+  AddUnknownEnumFieldToProto(proto, kFieldNumber, kValue2);
+
+  // Last one wins.
+  EXPECT_EQ(GetUnknownEnumFieldValueFromProto(proto, kFieldNumber), kValue2);
+}
+
+TEST(UnknownFieldUtilTest, GetUnknownEnumFieldWithWrongType) {
+  sync_pb::test::UnknownFields proto;
+  const std::string kValue = "unknown_value";
+  // This sets field 100000 which is a string.
+  AddUnknownFieldToProto(proto, kValue);
+
+  // Try to read field 100000 as an enum (varint).
+  EXPECT_EQ(GetUnknownEnumFieldValueFromProto(
+                proto, sync_pb::test::UnknownFields::kUnknownFieldFieldNumber),
+            -1);
+}
+
+TEST(UnknownFieldUtilTest, HasUnknownFieldMatcher) {
+  sync_pb::test::UnknownFields proto;
+  const std::string kValue = "unknown_value";
+
+  AddUnknownFieldToProto(proto, kValue);
+
+  EXPECT_THAT(proto, HasUnknownField(kValue));
+  EXPECT_THAT(proto, testing::Not(HasUnknownField("other_value")));
+}
+
+}  // namespace
+}  // namespace syncer::test
