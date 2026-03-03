426fa7ff7cbbd2c229caaf6a844d14249cab4562
Andrew Vuong <akvuong@google.com>
2026-03-03 14:50:27 -0800

    Add observer methods to AccessibilityAnnotationSyncBridge.
    This change introduces new observer methods in
    AccessibilityAnnotationSyncBridge to notify observers when accessibility
    annotations are added, updated, or removed.
    
    Bug: 486856790
    Change-Id: I6f3458890f2b645796d978c6fbdbea76c6b997e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7613489
    Commit-Queue: Andrew Vuong <akvuong@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1593542}

--- components/accessibility_annotator/core/storage/accessibility_annotator_backend.h
+++ components/accessibility_annotator/core/storage/accessibility_annotator_backend.h
@@ -10,7 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/memory/weak_ptr.h"
+#include "base/scoped_observation.h"
 #include "base/threading/sequence_bound.h"
+#include "components/accessibility_annotator/core/storage/accessibility_annotation_sync_bridge.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/sync/model/data_type_store.h"
 
@@ -24,10 +26,11 @@ enum class Channel;
 
 namespace accessibility_annotator {
 
-class AccessibilityAnnotationSyncBridge;
 class AccessibilityAnnotatorDatabase;
 
-class AccessibilityAnnotatorBackend : public KeyedService {
+class AccessibilityAnnotatorBackend
+    : public KeyedService,
+      public AccessibilityAnnotationSyncBridge::Observer {
  public:
   AccessibilityAnnotatorBackend(
       version_info::Channel channel,
@@ -48,10 +51,18 @@ class AccessibilityAnnotatorBackend : public KeyedService {
   base::WeakPtr<syncer::DataTypeControllerDelegate>
   GetAccessibilityAnnotationControllerDelegate();
 
+  // AccessibilityAnnotationSyncBridge::Observer implementation.
+  void OnAccessibilityAnnotationChanged() override;
+  void OnAccessibilityAnnotationSyncBridgeLoaded() override;
+
  private:
   base::SequenceBound<AccessibilityAnnotatorDatabase> db_;
   std::unique_ptr<AccessibilityAnnotationSyncBridge>
       accessibility_annotation_sync_bridge_;
+
+  base::ScopedObservation<AccessibilityAnnotationSyncBridge,
+                          AccessibilityAnnotationSyncBridge::Observer>
+      sync_bridge_observation_{this};
 };
 
 }  // namespace accessibility_annotator
