3ec9a925b4899cb02d35690ade0238709541fecc
Kelvin Jiang <kelvinjiang@chromium.org>
2026-03-02 19:26:13 -0800

    [Signin] Add SignoutConfirmation waiter in ProfileMenuViewBrowsertest
    Refactor the waiter into its own file since it's now used by multiple
    tests. This CL also adds a method to the observer when the
    SignoutConfirmationUI* is being destroyed to avoid dangling pointers.
    
    Fixed: 482595667
    Change-Id: Iece1715a1d07e99365f438c331a40d84dad11217
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7618666
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1592945}

---
+++ chrome/browser/ui/webui/signin/signout_confirmation/test_signout_confirmation_handler_waiter.cc
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/webui/signin/signout_confirmation/test_signout_confirmation_handler_waiter.h"
+
+#include "base/check.h"
+
+TestSignoutConfirmationHandlerWaiter::TestSignoutConfirmationHandlerWaiter(
+    SignoutConfirmationUI* signout_confirmation_ui)
+    : signout_confirmation_ui_(signout_confirmation_ui) {
+  CHECK(signout_confirmation_ui_);
+  signout_confirmation_ui_observation_.Observe(signout_confirmation_ui_);
+}
+
+TestSignoutConfirmationHandlerWaiter::~TestSignoutConfirmationHandlerWaiter() =
+    default;
+
+void TestSignoutConfirmationHandlerWaiter::Wait() {
+  if (signout_confirmation_ui_->IsHandlerReadyForTesting()) {
+    return;
+  }
+  run_loop_.Run();
+}
+
+// SignoutConfirmationUI::Observer:
+void TestSignoutConfirmationHandlerWaiter::
+    OnSignoutConfirmationUIHandlerReady() {
+  run_loop_.Quit();
+}
+
+void TestSignoutConfirmationHandlerWaiter::OnSignoutConfirmationUIDestroying(
+    SignoutConfirmationUI* ui) {
+  CHECK_EQ(ui, signout_confirmation_ui_);
+
+  run_loop_.Quit();
+  signout_confirmation_ui_observation_.Reset();
+  signout_confirmation_ui_ = nullptr;
+}
