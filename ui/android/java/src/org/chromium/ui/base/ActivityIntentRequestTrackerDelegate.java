7c00608257d883c3f90dc197506b583c3a20676d
Sky Malice <skym@chromium.org>
2026-03-03 11:55:43 -0800

    Move out default of onCallbackNotFoundError.
    Bug: 486880198
    Change-Id: I416f037360f803e5018388f2512da60675ebbcd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7601112
    Reviewed-by: Menghan Yang <myuu@google.com>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1593403}

--- ui/android/java/src/org/chromium/ui/base/ActivityIntentRequestTrackerDelegate.java
+++ ui/android/java/src/org/chromium/ui/base/ActivityIntentRequestTrackerDelegate.java
@@ -67,4 +67,9 @@ public class ActivityIntentRequestTrackerDelegate implements Delegate {
     public final WeakReference<Activity> getActivity() {
         return mActivityWeakRefHolder;
     }
+
+    @Override
+    public boolean onCallbackNotFoundError(String error) {
+        return false;
+    }
 }
