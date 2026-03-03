bf31ecd89453adc15bb6c8430127c483cbad4eb2
Anis Chara <achara@google.com>
2026-03-03 06:34:06 -0800

    [iOS] Add TabGroupView's background update
    This CL implements the background color update for TabGroupView when a
    group's color is edited.
    
    Bug: 445887468
    Change-Id: Ie0cf64c0e13c9beca4f3ef83816743eb2c63c632
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7627498
    Commit-Queue: Anis Chara <achara@google.com>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1593189}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_group_view_controller.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_group_view_controller.mm
@@ -1070,6 +1070,8 @@ UIButton* TopToolbarButton(NSString* symbol_name,
   closeButtonConfig.background.backgroundColor = buttonColor;
   _closeButton.configuration = closeButtonConfig;
 
+  _gradientBackground.colors = _tabGroupColorPalette.backgroundGradientColors;
+
   [_facePileView
       setShareButtonBackgroundColor:[_tabGroupColorPalette.commonColor
                                         colorWithAlphaComponent:kButtonAlpha]];
