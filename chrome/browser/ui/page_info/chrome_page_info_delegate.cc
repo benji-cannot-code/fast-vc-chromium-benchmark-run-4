e356fc9ae3356fb4646bec81d56213ca3a698697
Sarah Murphy <murphys@microsoft.com>
2026-03-03 11:19:28 -0800

    Correct Duplicate Local/Loopback Network Permissions in PageInfo
    We're getting user reports of duplicate permissions for "LNA" and
    "Local/Loopback Network" in pageinfo displays for enterprise users.
    This change checks against the feature flag to ensure only the enabled
    permission is displayed.
    
    Bug: 485816032
    Change-Id: Ib8023c606a96deb340215ccdb1b910d7c715562f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7624054
    Reviewed-by: Filipa Senra <fsenra@google.com>
    Auto-Submit: Sarah Murphy <murphys@microsoft.com>
    Reviewed-by: Brandon Maslen <brandm@microsoft.com>
    Commit-Queue: Sarah Murphy <murphys@microsoft.com>
    Reviewed-by: Elias Klim <elklm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1593376}

--- chrome/browser/ui/page_info/chrome_page_info_delegate.cc
+++ chrome/browser/ui/page_info/chrome_page_info_delegate.cc
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/content_features.h"
 #include "media/base/media_switches.h"
+#include "services/network/public/cpp/features.h"
 #include "third_party/blink/public/common/features.h"
 #include "ui/base/window_open_disposition_utils.h"
 #include "url/origin.h"
@@ -474,6 +475,11 @@ bool ChromePageInfoDelegate::IsHttpsFirstModeEnabled() {
 
 bool ChromePageInfoDelegate::IsIncognitoProfile() {
   return GetProfile()->IsIncognitoProfile();
+}
+
+bool ChromePageInfoDelegate::IsLocalNetworkAccessSplitPermissionsEnabled() {
+  return base::FeatureList::IsEnabled(
+      network::features::kLocalNetworkAccessChecksSplitPermissions);
 }
 
 void ChromePageInfoDelegate::SetSecurityStateForTests(
