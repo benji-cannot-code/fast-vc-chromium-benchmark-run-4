15acc3d05f9be19f3a7af7e25c063e6a6c9257ee
Emily Shack <emshack@chromium.org>
2026-03-03 12:40:07 -0800

    [ComboButton] Fix combo button width when collapsed
    Bug: 489201125
    Change-Id: Ie478567576de6dd7ebfdfd2d80a5ab4416a7ae98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7629444
    Reviewed-by: Alison Gale <agale@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Auto-Submit: Emily Shack <emshack@chromium.org>
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1593439}

--- chrome/browser/ui/views/tabs/vertical/vertical_tab_strip_top_container.cc
+++ chrome/browser/ui/views/tabs/vertical/vertical_tab_strip_top_container.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/vertical_tab_strip_state_controller.h"
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/views/bookmarks/saved_tab_groups/saved_tab_group_everything_menu.h"
+#include "chrome/browser/ui/views/frame/vertical_tab_strip_region_view.h"
 #include "chrome/browser/ui/views/tabs/shared/tab_strip_combo_button.h"
 #include "chrome/browser/ui/views/tabs/shared/tab_strip_flat_edge_button.h"
 #include "chrome/browser/ui/views/tabs/vertical/top_container_button.h"
@@ -78,9 +79,10 @@ views::ProposedLayout VerticalTabStripTopContainer::CalculateProposedLayout(
           : parent()->GetAvailableSize(this).width().value_or(0);
 
   if (combo_button_
-          ->GetPreferredSizeForOrientation(
-              views::LayoutOrientation::kHorizontal)
-          .width() >= available_width) {
+              ->GetPreferredSizeForOrientation(
+                  views::LayoutOrientation::kHorizontal)
+              .width() >= available_width ||
+      available_width <= VerticalTabStripRegionView::kCollapsedWidth) {
     combo_button_orientation_ = views::LayoutOrientation::kVertical;
     int current_y = 0;
 
