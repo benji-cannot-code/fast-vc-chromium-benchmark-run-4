0128df221ee932f7a9cafbb3e483390a946af9de
keybo <keybo@google.com>
2026-03-03 09:56:15 -0800

    Modularize //chrome/browser/heavy_ad_intervention
    This CL extracts the heavy ad intervention feature from the main
    `//chrome/browser` target into its own component,
    `//chrome/browser/heavy_ad_intervention`.
    
    This is a structural refactoring with no intended change in behavior.
    This change is part of an ongoing effort to modularize
    `//chrome/browser` to improve dependency hygiene and reduce build times.
    
    The new component contains the `HeavyAdServiceFactory` and its
    corresponding browser test. Dependencies in `//chrome/browser`,
    `//chrome/browser/profiles`, and `//chrome/test` have been updated to
    point to the new build target.
    
    Bug: 485960458
    Change-Id: I27f76689f942f0512f269d83012e116be1e44119
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7604692
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Keybo Qian <keybo@google.com>
    Auto-Submit: Keybo Qian <keybo@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1593323}

--- chrome/browser/heavy_ad_intervention/heavy_ad_service_factory.cc
+++ chrome/browser/heavy_ad_intervention/heavy_ad_service_factory.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/heavy_ad_intervention/heavy_ad_service_factory.h"
 
-#include "chrome/browser/profiles/profile.h"
 #include "components/heavy_ad_intervention/heavy_ad_service.h"
 #include "content/public/browser/browser_context.h"
 
