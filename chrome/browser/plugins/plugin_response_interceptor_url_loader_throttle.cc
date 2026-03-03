f37623bee2f191773d95a31a074e6ebc1707d597
Devlin Cronin <rdevlin.cronin@chromium.org>
2026-03-02 16:24:19 -0800

    [Extensions] Move file from streams_private_api
    The streamsPrivate API is long dead. There was one remaining function
    in there that is still called from mime handler code and should be
    moved.
    
    Move it to chrome/browser/extensions/api/mime_handlers instead.
    
    Bypass-Check-License: Moved files.
    Bug: 40595895
    Change-Id: Ic326e6cae67c5c53fa49c4b51e4334a0e61915d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7618634
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Tzarial <zork@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1592832}

--- chrome/browser/plugins/plugin_response_interceptor_url_loader_throttle.cc
+++ chrome/browser/plugins/plugin_response_interceptor_url_loader_throttle.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/numerics/safe_conversions.h"
 #include "base/unguessable_token.h"
 #include "base/uuid.h"
-#include "chrome/browser/extensions/api/streams_private/streams_private_api.h"
+#include "chrome/browser/extensions/api/mime_handlers/dispatch_mime_handler_event.h"
 #include "chrome/browser/plugins/plugin_utils.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
@@ -225,7 +225,7 @@ void PluginResponseInterceptorURLLoaderThrottle::WillProcessResponse(
   content::GetUIThreadTaskRunner({})->PostTask(
       FROM_HERE,
       base::BindOnce(
-          &extensions::StreamsPrivateAPI::SendExecuteMimeTypeHandlerEvent,
+          &extensions::mime_handlers::SendExecuteMimeTypeHandlerEvent,
           extension_id, stream_id, embedded, frame_tree_node_id_,
           std::move(transferrable_loader), response_url, internal_id));
 
