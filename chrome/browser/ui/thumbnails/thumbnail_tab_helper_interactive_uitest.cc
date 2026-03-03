d51a0de426628a1cf25603819d6f9056cd565441
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2026-03-02 16:09:00 -0800

    Revert "Reland "[bedrock] Migrate BrowserListObserver to BrowserCollectionObserver - part 1/n""
    This reverts commit cc412cfafdd69b45610182aff2653d5dc19e6b25.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/6262401801388032
    
    Sample build with failed test: https://ci.chromium.org/b/8688411288938122689
    Affected test(s):
    [://chrome/test\:unit_tests!gtest::HoldingSpaceKeyedServiceWithExperimentalFeatureForGuestTest#GuestUserProfile](https://ci.chromium.org/ui/test/chromium/:%2F%2Fchrome%2Ftest%5C:unit_tests%21gtest::HoldingSpaceKeyedServiceWithExperimentalFeatureForGuestTest%23GuestUserProfile?q=VHash%3A60460b4aaa65d6fc)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F6262401801388032&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F7509603&type=BUG
    
    Original change's description:
    > Reland "[bedrock] Migrate BrowserListObserver to BrowserCollectionObserver - part 1/n"
    >
    > This is a reland of commit 25c87a7959fa2fb22bf07fabfbd1bbc6cb74a071
    >
    > Original change's description:
    > > [bedrock] Migrate BrowserListObserver to BrowserCollectionObserver - part 1/n
    > >
    > > This migration is part of project bedrock to reduce the dependencies on
    > > Browser and BrowserList. See https://crbug.com/431671320 for more info.
    > >
    > > Bug: 431671320
    > > Change-Id: Ic6f2b62c3e01b6ed5ab88f8a37c86374a51ba69b
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7452749
    > > Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    > > Reviewed-by: Qikai Zhong <qikaizhong@microsoft.com>
    > > Commit-Queue: Zhentao Lu <zhentao.lu@microsoft.com>
    > > Cr-Commit-Position: refs/heads/main@{#1570999}
    >
    > Bug: 431671320
    > Change-Id: I7d2fd6a9554445607db3d614512ce37d5b4025c4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7509603
    > Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    > Commit-Queue: Tom Lukaszewicz <tluk@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1592600}
    >
    
    Bug: 431671320
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Id8426c484476085966ddc54447a7d0afb813f708
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7623051
    Commit-Queue: Jiacheng Guo <gjc@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Jiacheng Guo <gjc@google.com>
    Owners-Override: Jiacheng Guo <gjc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1592813}

--- chrome/browser/ui/thumbnails/thumbnail_tab_helper_interactive_uitest.cc
+++ chrome/browser/ui/thumbnails/thumbnail_tab_helper_interactive_uitest.cc
@@ -5,11 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <optional>
 
-#include "base/callback_list.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
-#include "base/scoped_observation.h"
 #include "base/strings/strcat.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/test/scoped_feature_list.h"
@@ -18,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_element_identifiers.h"
+#include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_tabstrip.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/interaction/browser_elements.h"
@@ -61,21 +60,25 @@ class ThumbnailObserver : public ui::test::StateObserver<bool> {
   base::WeakPtrFactory<ThumbnailObserver> weak_ptr_factory_{this};
 };
 
-class BrowserRemovedObserver : public ui::test::StateObserver<bool> {
+class BrowserRemovedObserver : public ui::test::StateObserver<bool>,
+                               public BrowserListObserver {
  public:
-  explicit BrowserRemovedObserver(BrowserWindowInterface* browser) {
-    browser_did_close_subscription_ = browser->RegisterBrowserDidClose(
-        base::BindRepeating(&BrowserRemovedObserver::OnBrowserDidClose,
-                            base::Unretained(this)));
+  explicit BrowserRemovedObserver(Browser* browser) : browser_(browser) {
+    BrowserList::AddObserver(this);
   }
   ~BrowserRemovedObserver() override = default;
 
- private:
-  void OnBrowserDidClose(BrowserWindowInterface* browser) {
-    OnStateObserverStateChanged(true);
+ protected:
+  void OnBrowserRemoved(Browser* browser) override {
+    if (browser_ == browser) {
+      OnStateObserverStateChanged(true);
+      browser_ = nullptr;
+      BrowserList::RemoveObserver(this);
+    }
   }
 
-  base::CallbackListSubscription browser_did_close_subscription_;
+ private:
+  raw_ptr<Browser> browser_;
 };
 
 DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kFirstTab);
@@ -227,7 +230,9 @@ IN_PROC_BROWSER_TEST_F(ThumbnailTabHelperUpdatedInteractiveTest,
           AddInstrumentedTab(kThirdTab, GURL(chrome::kChromeUINewTabURL), 3),
           WaitForWebContentsReady(kThirdTab)),
       CheckTabCountInBrowser(4), CheckActiveTabInBrowser(3),
-      ObserveState(kBrowserRemovedState, [this]() { return target_browser(); }),
+      ObserveState(
+          kBrowserRemovedState,
+          [this]() { return target_browser()->GetBrowserForMigrationOnly(); }),
       // Can't close browser when WebContents is notifying observers.
       Do([this]() {
         // Override manual value set in MemorySaverInteractiveTestMixin to
