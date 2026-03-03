413a7d35a7d2892748b9d88269f092c4daace3df
Georg Neis <neis@chromium.org>
2026-03-03 00:04:20 -0800

    ash: Dismantle crosapi's cros_display_config.mojom, part 2
    Replace the crosapi mojo definition of CrosDisplayConfigObserver with a
    pure C++ interface, ash::CrosDisplayConfig::Observer.
    
    Bug: b:485123493, b:365741912, b:365902693
    Change-Id: If989e55a071bc309fec917130e8207ec4d3d6159
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7620617
    Commit-Queue: Georg Neis <neis@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1593037}

--- chrome/browser/extensions/system_display/display_info_provider_chromeos.h
+++ chrome/browser/extensions/system_display/display_info_provider_chromeos.h
@@ -22,10 +22,9 @@ class Shell;
 
 namespace extensions {
 
-class DisplayInfoProviderChromeOS
-    : public DisplayInfoProviderBase,
-      public ash::ShellObserver,
-      public crosapi::mojom::CrosDisplayConfigObserver {
+class DisplayInfoProviderChromeOS : public DisplayInfoProviderBase,
+                                    public ash::ShellObserver,
+                                    public ash::CrosDisplayConfig::Observer {
  public:
   DisplayInfoProviderChromeOS();
 
@@ -69,7 +68,7 @@ class DisplayInfoProviderChromeOS
   // ash::ShellObserver:
   void OnShellDestroying() override;
 
-  // crosapi::mojom::CrosDisplayConfigObserver
+  // ash::CrosDisplayConfig::Observer
   void OnDisplayConfigChanged() override;
 
  private:
@@ -94,7 +93,7 @@ class DisplayInfoProviderChromeOS
   base::ScopedObservation<ash::Shell, ash::ShellObserver> shell_observation_{
       this};
   base::ScopedObservation<ash::CrosDisplayConfig,
-                          crosapi::mojom::CrosDisplayConfigObserver>
+                          ash::CrosDisplayConfig::Observer>
       cros_display_config_observation_{this};
   std::string touch_calibration_target_id_;
   base::WeakPtrFactory<DisplayInfoProviderChromeOS> weak_ptr_factory_{this};
