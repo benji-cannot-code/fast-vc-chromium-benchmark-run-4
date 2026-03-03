fb95410791d1856ddabb76aa19de00a2b60a8652
Wenyu Fu <wenyufu@chromium.org>
2026-03-03 14:27:45 -0800

    Add a new notification channel and UMA logging for Actor
    Also fixed the If this then that syntax change in
    ChromeChannelDefinitions.java.
    
    Bug: 487162226
    Change-Id: I6b7317c4d56b66c9529e0e02434a1ba3e38d7eb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7621603
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1593526}

--- chrome/browser/notifications/android/java/src/org/chromium/chrome/browser/notifications/NotificationUmaTracker.java
+++ chrome/browser/notifications/android/java/src/org/chromium/chrome/browser/notifications/NotificationUmaTracker.java
@@ -88,6 +88,7 @@ public class NotificationUmaTracker {
         SystemNotificationType.TRACING,
         SystemNotificationType.SERIAL,
         SystemNotificationType.SAFETY_HUB_UNSUBSCRIBED_NOTIFICATIONS,
+        SystemNotificationType.ACTOR,
     })
     @Retention(RetentionPolicy.SOURCE)
     public @interface SystemNotificationType {
@@ -136,8 +137,9 @@ public class NotificationUmaTracker {
         int TRACING = 41;
         int SERIAL = 42;
         int SAFETY_HUB_UNSUBSCRIBED_NOTIFICATIONS = 43;
+        int ACTOR = 44;
 
-        int NUM_ENTRIES = 44;
+        int NUM_ENTRIES = 45;
     }
 
     /*
@@ -177,7 +179,10 @@ public class NotificationUmaTracker {
         ActionType.REPORT_AS_SAFE,
         ActionType.REPORT_WARNED_NOTIFICATION_AS_SPAM,
         ActionType.REPORT_UNWARNED_NOTIFICATION_AS_SPAM,
-        ActionType.DOWNLOAD_DELETE_FROM_HISTORY
+        ActionType.DOWNLOAD_DELETE_FROM_HISTORY,
+        ActionType.ACTOR_PAUSE,
+        ActionType.ACTOR_RESUME,
+        ActionType.ACTOR_CANCEL,
     })
     @Retention(RetentionPolicy.SOURCE)
     public @interface ActionType {
@@ -280,8 +285,15 @@ public class NotificationUmaTracker {
         // Delete from history button on user download notification.
         int DOWNLOAD_DELETE_FROM_HISTORY = 41;
 
+        // Pause button on actor notification.
+        int ACTOR_PAUSE = 42;
+        // Resume button on actor notification.
+        int ACTOR_RESUME = 43;
+        // Cancel button on actor notification.
+        int ACTOR_CANCEL = 44;
+
         // Number of real entries, excluding `UNKNOWN`.
-        int NUM_ENTRIES = 41;
+        int NUM_ENTRIES = 45;
     }
 
     /**
@@ -457,6 +469,10 @@ public class NotificationUmaTracker {
                         "Mobile.SystemNotification.Content.Click.Age.PriceDropUserManaged",
                         createTime);
                 break;
+            case SystemNotificationType.ACTOR:
+                recordNotificationAgeHistogram(
+                        "Mobile.SystemNotification.Content.Click.Age.Actor", createTime);
+                break;
         }
     }
 
@@ -499,6 +515,10 @@ public class NotificationUmaTracker {
             case SystemNotificationType.PRICE_DROP_ALERTS_USER_MANAGED:
                 recordNotificationAgeHistogram(
                         "Mobile.SystemNotification.Dismiss.Age.PriceDropUserManaged", createTime);
+                break;
+            case SystemNotificationType.ACTOR:
+                recordNotificationAgeHistogram(
+                        "Mobile.SystemNotification.Dismiss.Age.Actor", createTime);
                 break;
         }
     }
@@ -548,6 +568,10 @@ public class NotificationUmaTracker {
                 recordNotificationAgeHistogram(
                         "Mobile.SystemNotification.Action.Click.Age.PriceDropUserManaged",
                         createTime);
+                break;
+            case SystemNotificationType.ACTOR:
+                recordNotificationAgeHistogram(
+                        "Mobile.SystemNotification.Action.Click.Age.Actor", createTime);
                 break;
         }
     }
@@ -823,6 +847,8 @@ public class NotificationUmaTracker {
         switch (channelId) {
             case ChannelId.BROWSER:
                 return "Browser";
+            case ChannelId.ACTOR:
+                return "Actor";
             case ChannelId.COLLABORATION:
                 return "Collaboration";
             case ChannelId.DOWNLOADS:
