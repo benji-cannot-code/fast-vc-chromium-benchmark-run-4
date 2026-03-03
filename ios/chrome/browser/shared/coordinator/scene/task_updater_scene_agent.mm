40efe958607c876090a0ad0367d9f25f44d824e7
Federica Germinario <fedegermi@google.com>
2026-03-03 09:27:54 -0800

    [ios] Make sure ui tasks are executed only when app state is UIApplicationStateActive
    Current logic checks that app state is UIApplicationStateActive before
    executing a ShortcutItem, it seems that current conditions in
    TaskUpdaterSceneAgent are not sufficient to guarantee this, for this
    reason start observing UIApplicationDidBecomeActiveNotification.
    
    Note: in old logic this is done in Handle3DTouchApplicationShortcuts.
    
    Bug: 462018636
    Change-Id: I27e2c5b82eeda92cac72320e19a241bca500cf74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7617071
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1593277}

--- ios/chrome/browser/shared/coordinator/scene/task_updater_scene_agent.mm
+++ ios/chrome/browser/shared/coordinator/scene/task_updater_scene_agent.mm
@@ -27,6 +27,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self.sceneState.profileState addObserver:self];
   [self.sceneState.profileState addUIBlockerManagerObserver:self];
 
+  [NSNotificationCenter.defaultCenter
+      addObserver:self
+         selector:@selector(maybeUpdateToUIReady)
+             name:UIApplicationDidBecomeActiveNotification
+           object:nil];
+
   // Make sure that the execution stage is updated also if a scene is connected
   // after the ProfileState has reached stage ProfileInitStage::kProfileLoaded
   // or higher.
@@ -73,6 +79,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self.sceneState.profileState removeObserver:self];
   [self.sceneState removeObserver:self];
   [self.sceneState.profileState removeUIBlockerManagerObserver:self];
+  [NSNotificationCenter.defaultCenter removeObserver:self];
 }
 
 - (void)signinDidEnd:(SceneState*)sceneState {
@@ -119,6 +126,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return NO;
   }
   if ([self signinStatusInSyncWithPolicy]) {
+    return NO;
+  }
+  if ([[UIApplication sharedApplication] applicationState] !=
+      UIApplicationStateActive) {
     return NO;
   }
   return YES;
