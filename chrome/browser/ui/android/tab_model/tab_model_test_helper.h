973cc76bb19f9f79cbabe1d5b3c703a6ce0b2c99
Calder Kitagawa <ckitagawa@chromium.org>
2026-03-03 07:51:07 -0800

    [TabModel] Improve tabmodel lookup performance
    As we create more C++ code that leverages tabs and the number of tabs
    grows this becomes increasingly inefficient. This should help improve
    things a bit.
    
    Bug: 488398095
    Change-Id: I58263696d91cc8e48213107e2b676601dad78ba9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7617707
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1593236}

--- chrome/browser/ui/android/tab_model/tab_model_test_helper.h
+++ chrome/browser/ui/android/tab_model/tab_model_test_helper.h
@@ -81,6 +81,7 @@ class TestTabModel : public TabModel {
       const base::Time& end_time) const override;
   void CloseTabsNavigatedInTimeWindow(const base::Time& begin_time,
                                       const base::Time& end_time) override;
+  tabs::TabCollection* GetTabStripCollection() const override;
 
   // TODO(crbug.com/415351293): Implement these.
   // TabListInterface implementation.
@@ -196,6 +197,7 @@ class OwningTestTabModel : public TabModel {
       const base::Time& end_time) const override;
   void CloseTabsNavigatedInTimeWindow(const base::Time& begin_time,
                                       const base::Time& end_time) override;
+  tabs::TabCollection* GetTabStripCollection() const override;
 
   // Test accessors:
 
