048fd9a5c81ebfcc1156cee76d9fcba4cb87db80
Lynne Jiang <lyjiang@google.com>
2026-03-03 11:54:03 -0800

    [webnn] Add is_incognito and memory_tracker to LiteRT context creation
    Pass is_incognito and memory_tracker to ContextImplLiteRt::Create to
    match the TFLite context creation flow.
    
    Bug: 454732289
    
    Change-Id: I8b83868b60059e2e90d0062bb6c2b997deb8c484
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7624738
    Commit-Queue: Lynne Jiang <lyjiang@google.com>
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1593400}

--- services/webnn/tflite/context_impl_litert.h
+++ services/webnn/tflite/context_impl_litert.h
@@ -35,7 +35,8 @@ class ContextImplLiteRt final : public WebNNContextImpl {
       scoped_refptr<base::SingleThreadTaskRunner> owning_task_runner,
       gpu::SharedImageManager* shared_image_manager,
       scoped_refptr<base::SingleThreadTaskRunner> main_task_runner,
-      ScopedTrace scoped_trace);
+      ScopedTrace scoped_trace,
+      bool is_incognito);
 
   ContextImplLiteRt(
       mojo::PendingReceiver<mojom::WebNNContext> receiver,
@@ -47,7 +48,8 @@ class ContextImplLiteRt final : public WebNNContextImpl {
       scoped_refptr<gpu::MemoryTracker> memory_tracker,
       scoped_refptr<base::SingleThreadTaskRunner> owning_task_runner,
       gpu::SharedImageManager* shared_image_manager,
-      scoped_refptr<base::SingleThreadTaskRunner> main_task_runner);
+      scoped_refptr<base::SingleThreadTaskRunner> main_task_runner,
+      bool is_incognito);
 
   ContextImplLiteRt(const WebNNContextImpl&) = delete;
   ContextImplLiteRt& operator=(const ContextImplLiteRt&) = delete;
@@ -87,6 +89,7 @@ class ContextImplLiteRt final : public WebNNContextImpl {
       mojom::TensorInfoPtr tensor_info,
       WebNNTensorImpl::RepresentationPtr representation) override;
 
+  const bool is_incognito_;
   base::WeakPtrFactory<ContextImplLiteRt> weak_factory_{this};
 };
 
