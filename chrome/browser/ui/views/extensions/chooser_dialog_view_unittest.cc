3be513aeab927d40d86b488de2c87137f96bfd65
Keren Zhu <kerenzhu@chromium.org>
2026-03-03 13:50:39 -0800

    Replace WIDGET_OWNS_NATIVE_WIDGET with CLIENT_OWNS_WIDGET in chrome/browser/ui/views/extensions
    We are refactoring Views code to use CLIENT_OWNS_WIDGET to make the
    ownership model clearer. This CL changes WIDGET_OWNS_NATIVE_WIDGET to
    CLIENT_OWNS_WIDGET.
    
    This CL was uploaded by git cl split.
    
    Bug: 339619005
    Change-Id: Id2a9d2648300d0af6485e756b6450a24ed4dae81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7620485
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Auto-Submit: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Tom Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1593503}

--- chrome/browser/ui/views/extensions/chooser_dialog_view_unittest.cc
+++ chrome/browser/ui/views/extensions/chooser_dialog_view_unittest.cc
@@ -42,7 +42,7 @@ class ChooserDialogViewTest : public ChromeViewsTestBase {
     // We need a native view parent for the dialog to avoid a DCHECK
     // on Mac.
     parent_widget_ =
-        CreateTestWidget(views::Widget::InitParams::WIDGET_OWNS_NATIVE_WIDGET);
+        CreateTestWidget(views::Widget::InitParams::CLIENT_OWNS_WIDGET);
     parent = parent_widget_->GetNativeView();
 #endif
     widget_ = views::DialogDelegate::CreateDialogWidget(dialog_, GetContext(),
