1ac08e59e2581fba24018e5e2055b8fb05bb073f
Aliona DANGLA <alionadangla@google.com>
2026-03-03 09:37:12 -0800

    [iOS] Display a webstate in the assistant view
    This CL adds a mediator to the Assistant AIM (AI Mode) to manage a
    WebState and load a default search URL.
    
    See video:
    https://drive.google.com/file/d/11HuXoNZ9yfhS99aEQfa5v8nfBMbIkgo2/view?usp=drive_link
    
    Bug: 485524798
    Change-Id: I960f73f80b3cfd3f2a1825d7468d5fd9e665dfa2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7610711
    Reviewed-by: Radu Nitescu <radunitescu@google.com>
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1593291}

--- ios/chrome/browser/cobrowse/coordinator/assistant_aim_coordinator.mm
+++ ios/chrome/browser/cobrowse/coordinator/assistant_aim_coordinator.mm
@@ -7,20 +7,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/assistant/coordinator/assistant_container_commands.h"
 #import "ios/chrome/browser/assistant/ui/assistant_container_delegate.h"
-#import "ios/chrome/browser/assistant/ui/assistant_container_view_controller.h"
+#import "ios/chrome/browser/cobrowse/coordinator/assistant_aim_mediator.h"
 #import "ios/chrome/browser/cobrowse/ui/assistant_aim_view_controller.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
+#import "ios/chrome/browser/shared/model/profile/profile_ios.h"
 #import "ios/chrome/browser/shared/public/commands/command_dispatcher.h"
+#import "ios/web/public/web_state.h"
 
 @interface AssistantAIMCoordinator () <AssistantContainerDelegate>
 @end
 
 @implementation AssistantAIMCoordinator {
   AssistantAIMViewController* _viewController;
+  AssistantAIMMediator* _mediator;
 }
 
 - (void)start {
   _viewController = [[AssistantAIMViewController alloc] init];
+
+  web::WebState::CreateParams params(self.browser->GetProfile());
+  std::unique_ptr<web::WebState> webState = web::WebState::Create(params);
+
+  _mediator =
+      [[AssistantAIMMediator alloc] initWithWebState:std::move(webState)];
+  _mediator.consumer = _viewController;
 
   id<AssistantContainerCommands> containerHandler = HandlerForProtocol(
       self.browser->GetCommandDispatcher(), AssistantContainerCommands);
@@ -30,6 +40,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)stop {
+  [_mediator disconnect];
+  _mediator = nil;
+
   if (_viewController) {
     if (self.browser) {
       CommandDispatcher* dispatcher = self.browser->GetCommandDispatcher();
@@ -48,7 +61,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)assistantContainer:(AssistantContainerViewController*)container
       didDisappearAnimated:(BOOL)animated {
-  _viewController = nil;
   [self stop];
 }
 
