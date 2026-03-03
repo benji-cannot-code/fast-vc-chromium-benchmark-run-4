987fd3a0b27dd209efac0534c12a559ed8911a59
David Sanders <dsanders11@ucsbalum.com>
2026-03-03 12:10:34 -0800

    Remove unused include in //ui/views/metadata/view_factory.h
    Adds missing includes as needed to fix the build.
    
    Bug: 40318405
    Change-Id: I1c2a13c70c8a4ba2255d1a2568567b0882a3daa8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7624782
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1593413}

--- components/live_caption/views/translation_view_wrapper_base.cc
+++ components/live_caption/views/translation_view_wrapper_base.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/menu/menu_runner.h"
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/view.h"
+#include "ui/views/view_utils.h"
 
 namespace captions {
 namespace {
--- ui/views/metadata/view_factory.h
+++ ui/views/metadata/view_factory.h
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/border.h"
 #include "ui/views/metadata/view_factory_internal.h"
 #include "ui/views/view.h"
-#include "ui/views/view_utils.h"
 #include "ui/views/views_export.h"
 
 class SkPath;
