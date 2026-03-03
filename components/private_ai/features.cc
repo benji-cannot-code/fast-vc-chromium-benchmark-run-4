ee9cceab2f93a6854f138d4f76178d74a296ce06
Christian Dullweber <dullweber@chromium.org>
2026-03-03 02:35:00 -0800

    PrivateAi: Enable server attestation by default
    Bug: 445368508
    Change-Id: I858efef996ea73a8a34b3a68361ee1125da719a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7626259
    Auto-Submit: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Nikhil Jakhar <nikhiljakhar@google.com>
    Reviewed-by: Nikhil Jakhar <nikhiljakhar@google.com>
    Cr-Commit-Position: refs/heads/main@{#1593111}

--- components/private_ai/features.cc
+++ components/private_ai/features.cc
@@ -5,11 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/private_ai/features.h"
 
+#include "base/feature_list.h"
+
 namespace private_ai {
 
 BASE_FEATURE(kPrivateAi, base::FEATURE_DISABLED_BY_DEFAULT);
 
-BASE_FEATURE(kPrivateAiServerAttestation, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kPrivateAiServerAttestation, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kPrivateAiUseTokenAttestation, base::FEATURE_DISABLED_BY_DEFAULT);
 
