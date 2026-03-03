967fd0a2f9e4374cf595ffbd0e21d5cdc8c88684
Steinar H. Gunderson <sesse@chromium.org>
2026-03-03 06:38:58 -0800

    Fix use-after-free in PendingInvalidationMap.
    When processing NodeInvalidationSets, we hold on to a reference to it
    even as we modify the PendingInvalidationMap it is part of
    (through a recursive call to schedule other nth-child invalidation sets).
    PendingInvalidationMap is a HeapHashMap, which is not node-based,
    and thus, insertions may invalidate pointers to the buckets, causing
    a use-after-free.
    
    We could have refreshed the pointer after operations that could modify
    the map, but it seems much safer to just store pointers in the map
    instead, at the cost of slightly more allocations.
    
    The test is fairly complicated and depends on details of when HashMap
    chooses to rehash, so it was seen as not useful to keep permanently
    in the test repository and thus not included.
    
    Fixed: 487117772
    Change-Id: I0ace920e5497dd1f3c6c1ae61fc8aa551eb334c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7628401
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1593196}

--- third_party/blink/renderer/core/css/invalidation/pending_invalidations.h
+++ third_party/blink/renderer/core/css/invalidation/pending_invalidations.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_CSS_INVALIDATION_PENDING_INVALIDATIONS_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_CSS_INVALIDATION_PENDING_INVALIDATIONS_H_
 
+#include <memory>
+
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/css/invalidation/node_invalidation_sets.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_hash_map.h"
@@ -17,8 +19,12 @@ class ContainerNode;
 class Document;
 class Element;
 
+// HeapHashMap is not node-based and thus does not have stable iterators,
+// but we rely on pointers to stay stable even during modification of the map.
+// Thus, use unique_ptr to make sure the NodeInvalidationSets has a stable
+// address.
 using PendingInvalidationMap =
-    HeapHashMap<Member<ContainerNode>, NodeInvalidationSets>;
+    HeapHashMap<Member<ContainerNode>, std::unique_ptr<NodeInvalidationSets>>;
 
 // Performs deferred style invalidation for DOM subtrees.
 //
--- third_party/blink/renderer/core/css/invalidation/style_invalidator.cc
+++ third_party/blink/renderer/core/css/invalidation/style_invalidator.cc
@@ -186,7 +186,7 @@ void StyleInvalidator::PushInvalidationSetsForContainerNode(
     return;
   }
   NodeInvalidationSets& pending_invalidations =
-      pending_invalidations_iterator->value;
+      *pending_invalidations_iterator->value;
 
   DCHECK(pending_nth_sets_.empty());
 
