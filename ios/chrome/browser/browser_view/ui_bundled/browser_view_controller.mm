b19eb43d9a9c6eeb41661ae8d0bcba5124782145
Joemer Ramos <joemerramos@google.com>
2026-03-02 16:42:55 -0800

    [iOS] Fix Copresence Floaty Visibility Issues
    Issue with Omnibox Popup Dismissal:
    OmniboxPopupViewController doesn't
    trigger `dismissViewControllerAnimated:completion:` in
    BrowserViewController. To ensure that this view updates the floaty
    properly, we send a floaty update when this popup closes.
    
    Issue with Floaty Not Re-Showing:
    Certain floaty update sources hide the floaty without attempting to
    re-show the floaty. This CL adds logic so that we don't keep track
    of these sources since they're "one-off" signals
    
    ** The change below are behind the new feature flag param **
    A new feature flag param was added in the event that these changes
    regress the feature. The related bugs are quite inconsistent and
    we aren't sure if this CL fixes all the issues 100%, but would like
    to try and see if it fixes the issues behind the safety of a flag.
    
    Issue with Floaty Not Showing W/ Scrolling:
    It's intended to have
    scrolling and fullscreen updates when the floaty is **already** shown,
    but not **while** it's being shown. Entering and exiting fullscreen
    while the floaty is transitioning to being shown can cause janky
    behavior such as the floaty flickering and not being shown (if a user
    enters fullscreen mode) or can cause the keyboard to be shown without
    the floaty being displayed.
    
    To fix this, we disable fullscreen while the floaty is being shown and
    re-enable it once the floaty is fully shown. In current behavior, we do
    exit fullscreen, but this wasn't enough of a guardrail since a user was
    exiting fullscreen and then getting fullscreen updates to enter
    fullscreen within milliseconds. We also disable scrolling to prevent
    further fullscreen updates.
    
    Note: This CL will also need ample canary coverage, 2-3 days, to ensure
    that it actually worked. The repro steps of this issue are inconsistent,
    so we're not sure if this is completely fixed just yet. It is good to
    note that these additional guardrails has made it fairly
    non-reproducible (90% confidence rate).
    
    Bug: 485867273
    Fixed: 488465020
    Change-Id: I8fa5f841419ed98be73e434e4f50482fc83a2240
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7617468
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@google.com>
    Cr-Commit-Position: refs/heads/main@{#1592855}

--- ios/chrome/browser/browser_view/ui_bundled/browser_view_controller.mm
+++ ios/chrome/browser/browser_view/ui_bundled/browser_view_controller.mm
@@ -1794,6 +1794,10 @@ const CGFloat kTopDynamicIslandInset = 24;
   self.visibilityState = BrowserViewVisibilityState::kVisible;
   self.toolbarCoordinator.secondaryToolbarViewController.view
       .accessibilityElementsHidden = NO;
+  [self.geminiHandler
+      updateFloatyVisibilityIfEligibleAnimated:NO
+                                    fromSource:gemini::FloatyUpdateSource::
+                                                   ViewTransition];
 }
 
 #pragma mark - FullscreenUIElement methods
