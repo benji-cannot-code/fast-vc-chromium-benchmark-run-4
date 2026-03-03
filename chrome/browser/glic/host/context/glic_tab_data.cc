9ccdb997629115b1c49fd6b9d322049b6f8c14f9
Dan Harrington <harringtond@chromium.org>
2026-03-03 10:12:41 -0800

    Null test GetContents()
    On Android, GetContents can return null, so this CL guards against null
    dereference.
    
    Bug: b:489414406
    Change-Id: I9b2324bae5e55f63a6eb2023522fff0c6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7627889
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Dan Harrington <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1593337}

--- chrome/browser/glic/host/context/glic_tab_data.cc
+++ chrome/browser/glic/host/context/glic_tab_data.cc
@@ -252,6 +252,9 @@ glic::mojom::TabDataPtr CreateTabData(tabs::TabInterface* tab) {
     return nullptr;
   }
   content::WebContents* web_contents = tab->GetContents();
+  if (!web_contents) {
+    return nullptr;
+  }
 
   SkBitmap favicon;
   auto* favicon_driver =
