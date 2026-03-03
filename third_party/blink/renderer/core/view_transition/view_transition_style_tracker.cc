74214e8516868fbcdea8408ccf052769cccf1f10
Steve Kobes <skobes@chromium.org>
2026-03-03 06:24:09 -0800

    Rename view-transition-scope value from "auto" to "all".
    Bug: 487664855
    Change-Id: I126adccc33d9feaf33a23cae1dbcce34523d8b8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7620211
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1593184}

--- third_party/blink/renderer/core/view_transition/view_transition_style_tracker.cc
+++ third_party/blink/renderer/core/view_transition/view_transition_style_tracker.cc
@@ -911,7 +911,7 @@ bool ViewTransitionStyleTracker::HasContainmentBoundary(
     if (Element* element = DynamicTo<Element>(node)) {
       if (element != element_ &&
           element->GetComputedStyle()->ViewTransitionScope() ==
-              EViewTransitionScope::kAuto) {
+              EViewTransitionScope::kAll) {
         return true;
       }
     }
