ee883d8cf8f398979be9f8e7b261005adf2ee586
Colin Blundell <blundell@chromium.org>
2026-03-03 09:27:41 -0800

    [//cc] Add checkerboard debug color to TileDisplayLayerImpl
    I'm looking at unifying the code for generating checkerboard quads
    between PictureLayerImpl and TileDisplayLayerImpl, and I noticed that
    TileDisplayLayerImpl was missing this [1]. Add this in preparation for
    deduping the code.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:cc/layers/picture_layer_impl.cc;l=402-405?q=picture_layer_impl.cc&ss=chromium
    
    Bug: 450651370
    Change-Id: Ic08af9c2f8e4e51e66dcd91caa108db835e69431
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7627782
    Reviewed-by: Vikas Soni <vikassoni@chromium.org>
    Commit-Queue: Vikas Soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1593276}

--- cc/layers/tile_display_layer_impl_unittest.cc
+++ cc/layers/tile_display_layer_impl_unittest.cc
@@ -1229,7 +1229,7 @@ TEST_F(TileDisplayLayerImplTest, AppendQuadsAppendsDebugBordersForOomTile) {
   ++it;
   EXPECT_EQ((*it)->material, viz::DrawQuad::Material::kSolidColor);
   EXPECT_EQ(viz::SolidColorDrawQuad::MaterialCast(*it)->color,
-            raw_layer->safe_opaque_background_color());
+            DebugColors::DefaultCheckerboardColor());
 }
 
 // Verifies that AppendQuads() appends debug borders for a not-ready tile when
@@ -1285,7 +1285,7 @@ TEST_F(TileDisplayLayerImplTest,
   ++it;
   EXPECT_EQ((*it)->material, viz::DrawQuad::Material::kSolidColor);
   EXPECT_EQ(viz::SolidColorDrawQuad::MaterialCast(*it)->color,
-            raw_layer->safe_opaque_background_color());
+            DebugColors::DefaultCheckerboardColor());
 }
 
 TEST_F(TileDisplayLayerImplTest, TileResourceIsOOM) {
