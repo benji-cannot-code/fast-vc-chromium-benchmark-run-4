213ee98aa31715c9a01c81b84e0919fbec5adec9
Calder Kitagawa <ckitagawa@chromium.org>
2026-03-04 06:32:10 -0800

    SmartSelectionClient remove GlobalRef
    One of these is create per WebContents. There is a finite table of 51200
    total global refs which means this doesn't scale well to unlimited
    WebContents. Address this by just finding the Java object via the
    WebContents UserData.
    
    Bug: 484619649
    Change-Id: I879dd4ca7ad7f1622fdddc56530c9d824bbc4385
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7624640
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1593898}

--- content/browser/android/selection/smart_selection_client.h
+++ content/browser/android/selection/smart_selection_client.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "base/android/jni_weak_ref.h"
+#include "base/android/scoped_java_ref.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 
@@ -24,9 +24,7 @@ class WebContents;
 // asynchronously from the current focused frame and passed back to Java layer.
 class SmartSelectionClient {
  public:
-  SmartSelectionClient(JNIEnv* env,
-                       const base::android::JavaRef<jobject>& obj,
-                       WebContents* web_contents);
+  explicit SmartSelectionClient(WebContents* web_contents);
 
   SmartSelectionClient(const SmartSelectionClient&) = delete;
   SmartSelectionClient& operator=(const SmartSelectionClient&) = delete;
@@ -47,8 +45,7 @@ class SmartSelectionClient {
                                  uint32_t start,
                                  uint32_t end);
 
-  // A weak reference to the Java ContentSelectionClient object.
-  JavaObjectWeakGlobalRef java_ref_;
+  base::android::ScopedJavaLocalRef<jobject> GetJavaObject(JNIEnv* env);
 
   // WebContents is used to find the relevant RenderFrameHost that can send
   // the request for the text.
