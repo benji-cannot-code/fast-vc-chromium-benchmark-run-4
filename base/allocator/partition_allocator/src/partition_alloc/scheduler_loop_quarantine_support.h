7059929206a80a39df1000d1d6015faccb27d085
Stephen Nusko <nuskos@chromium.org>
2026-03-04 02:04:45 -0800

    [PA/AC] disable all work in-between tasks
    During non-task related work we aren't claiming any security properties
    of MiracleObject for our current Browser UI thread launch. However
    because of the current implementation on the quarantine and zapping
    continue to function providing hardening benefits (more crashes) but
    unclear security promises (due to zapping hardening things but with
    enough allocations/freeing a pointer could be purged from the quarantine
    and exploited).
    
    Thus to avoid performance overhead we can instead disable the quarantine
    when we aren't explicitly processing a task.
    
    Change-Id: Ie19c6547f4627f4ffa079891ac46e7c91d4c394d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7619852
    Reviewed-by: Mikihito Matsuura <mikt@google.com>
    Auto-Submit: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1593799}

--- base/allocator/partition_allocator/src/partition_alloc/scheduler_loop_quarantine_support.h
+++ base/allocator/partition_allocator/src/partition_alloc/scheduler_loop_quarantine_support.h
@@ -129,6 +129,7 @@ class PA_COMPONENT_EXPORT(PARTITION_ALLOC)
   bool IsQuarantined(void* object);
   size_t GetCapacityInBytes();
   void Purge();
+  int PausedCount();
 
  private:
   std::variant<internal::GlobalSchedulerLoopQuarantineBranch*,
