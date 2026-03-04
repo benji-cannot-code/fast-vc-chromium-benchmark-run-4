2d53f23f4a913213c589123fe0bfca93062b1633
Stanisław Solarewicz <ssolarewicz@chromium.org>
2026-03-04 08:42:40 -0800

    [Android Entra SSO] Add the feature flag
    Bug: b:484014627
    Change-Id: I0506c200bb0c401b0eb66948db8f3f56d16b6c1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7633405
    Commit-Queue: Stanisław Solarewicz <ssolarewicz@chromium.org>
    Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1593992}

--- chrome/browser/enterprise/platform_auth/platform_auth_features.cc
+++ chrome/browser/enterprise/platform_auth/platform_auth_features.cc
@@ -5,8 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/enterprise/platform_auth/platform_auth_features.h"
 
+#include "base/feature_list.h"
+
 namespace enterprise_auth {
 
 BASE_FEATURE(kEnableExtensibleEnterpriseSSO, base::FEATURE_ENABLED_BY_DEFAULT);
+
+BASE_FEATURE(kAndroidEntraSSO, base::FEATURE_DISABLED_BY_DEFAULT);
 
 }  // namespace enterprise_auth
