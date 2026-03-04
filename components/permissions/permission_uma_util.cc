8fa7b8da2fad320a7a16f97fc73393819cad1b0e
Andy Paicu <andypaicu@chromium.org>
2026-03-04 06:33:06 -0800

    Cleanup PermissionType and RequestTypeForUma
    Remove unused members. Correctly mark enums values as obsolete. Refactor
    GetAllPermissionTypes.
    
    NO_IFTTT=RequestTypeForUma entries were already removed from other
    places.
    
    Fixed: 489631773
    Change-Id: I521a09558d82bd7a7b9c2ab4788368c3764f4508
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7627753
    Reviewed-by: Elias Klim <elklm@chromium.org>
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1593899}

--- components/permissions/permission_uma_util.cc
+++ components/permissions/permission_uma_util.cc
@@ -192,8 +192,6 @@ std::string GetPermissionRequestString(RequestTypeForUma type) {
       return "LoopbackNetwork";
 
     case RequestTypeForUma::UNKNOWN:
-    case RequestTypeForUma::PERMISSION_FLASH:
-    case RequestTypeForUma::PERMISSION_FILE_HANDLING:
     case RequestTypeForUma::NUM:
       NOTREACHED();
   }
