a550ba74d32c32671716033c35bb1c28c20a51c9
Michelle Abreo <michelleabreo@google.com>
2026-03-04 12:21:32 -0800

    [Skills][Metrics] Add metric event for edit skill hover chip.
    https://screenshot.googleplex.com/C54bVfgYuWRjm4t
    
    This introduces a new event, `kClickedEditSkillHoverChip`, to the
    `SkillsWebClientEvent` enum to track when a user interacts with the
    'Edit Skill' chip that appears on hover. The new value is added to the
    Mojom interface, the TypeScript API, and the UMA histograms metadata.
    
    Bug: b:483411707
    Change-Id: I8ec47a5bb3c8a540ad0817685a47a499bf68ef2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7634278
    Commit-Queue: Michelle Abreo <michelleabreo@google.com>
    Reviewed-by: Nick Birnie <birnie@google.com>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1594163}

--- chrome/browser/glic/service/metrics/glic_instance_metrics.cc
+++ chrome/browser/glic/service/metrics/glic_instance_metrics.cc
@@ -995,6 +995,7 @@ void GlicInstanceMetrics::RecordSkillsWebClientEvent(
     // --- Dialog Metrics ---
     case kClickedAddFromMenu:
     case kClickedSaveAsSkillHoverChip:
+    case kClickedEditSkillHoverChip:
     case kClickedAddOn1pSkill:
     case kClickedEditFromMenu:
       // TODO(crbug.com/483411707): Add routing for dialog metrics.
