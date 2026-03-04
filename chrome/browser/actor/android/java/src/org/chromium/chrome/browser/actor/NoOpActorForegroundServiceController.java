a1312418b3f462bfc2216dbb2e7ffdcf127c6898
Wenyu Fu <wenyufu@chromium.org>
2026-03-04 10:12:39 -0800

    [Actor] ActorForegroundServiceController
    This CL introduces the ActorForegroundServiceController interface and
    its No-Op implementation. This is the first part of splitting up the
    larger ActorForegroundService implementation to facilitate easier
    reviews. Original CL: crrev.com/c/7602772
    
    Key changes:
    - ActorForegroundServiceController: Defines the interface for
      browser-layer interaction with the service.
    - NoOpActorForegroundServiceController: A singleton no-op
      implementation of the controller.
    - BUILD.gn and DEPS updates to support these new files.
    
    Bug: 487162226
    Change-Id: Ib162b7319caa2bb1682911097d73e05305b27059
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7624010
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594052}

---
+++ chrome/browser/actor/android/java/src/org/chromium/chrome/browser/actor/NoOpActorForegroundServiceController.java
@@ -0,0 +1,46 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.actor;
+
+import android.app.Notification;
+
+import org.chromium.build.annotations.NullMarked;
+
+/** No-op implementation of {@link ActorForegroundServiceController}. */
+@NullMarked
+public class NoOpActorForegroundServiceController implements ActorForegroundServiceController {
+    private static class Holder {
+        private static final NoOpActorForegroundServiceController INSTANCE =
+                new NoOpActorForegroundServiceController();
+    }
+
+    public static NoOpActorForegroundServiceController getInstance() {
+        return Holder.INSTANCE;
+    }
+
+    private NoOpActorForegroundServiceController() {}
+
+    @Override
+    public void startAndBindService(Runnable onConnected) {}
+
+    @Override
+    public void unbindService() {}
+
+    @Override
+    public boolean isConnected() {
+        return false;
+    }
+
+    @Override
+    public void startOrUpdateForegroundService(
+            int newNotificationId,
+            Notification newNotification,
+            int oldNotificationId,
+            boolean killOldNotification) {}
+
+    @Override
+    public void stopActorForegroundService(int flags) {}
+}
