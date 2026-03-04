b30a838bbe27a60d241b0225f0422bd675f42e36
Antonio Sartori <antoniosartori@chromium.org>
2026-03-03 23:39:58 -0800

    [permissions] Support approx geo in ResetAllContentSettingsForWebApp
    This CL updates ResetAllContentSettingsForWebApp to also reset the new
    ContentSetting GEOLOCATION_WITH_OPTIONS, which will replace
    GEOLOCATION when kApproximateGeolocationPermission is enabled.
    
    Bug: 476929911
    Change-Id: I9a00634625efb34b41e321dc903cb5034deaeb63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7627504
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1593740}

--- chrome/browser/web_applications/web_app_utils_unittest.cc
+++ chrome/browser/web_applications/web_app_utils_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "build/branding_buildflags.h"
 #include "build/build_config.h"
+#include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/web_applications/mojom/user_display_mode.mojom.h"
 #include "chrome/browser/web_applications/test/web_app_test.h"
@@ -19,6 +20,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_install_info.h"
 #include "chrome/browser/web_applications/web_app_management_type.h"
 #include "chrome/common/chrome_constants.h"
+#include "components/content_settings/core/browser/content_settings_utils.h"
+#include "components/content_settings/core/browser/host_content_settings_map.h"
+#include "components/content_settings/core/browser/permission_settings_registry.h"
+#include "components/content_settings/core/common/content_settings.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -237,5 +242,31 @@ TEST_F(WebAppUtilsTest, GeminiAppWillBeSystemWebApp) {
   }
 }
 #endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING) && BUILDFLAG(IS_CHROMEOS)
+
+TEST_F(WebAppUtilsTest, ResetAllContentSettings) {
+  HostContentSettingsMap* host_content_settings_map =
+      HostContentSettingsMapFactory::GetForProfile(profile());
+  GURL url("isolated-app://abcdef");
+  host_content_settings_map->SetPermissionSettingDefaultScope(
+      url, url, ContentSettingsType::GEOLOCATION_WITH_OPTIONS,
+      GeolocationSetting{.approximate = PermissionOption::kAllowed,
+                         .precise = PermissionOption::kDenied});
+  host_content_settings_map->SetContentSettingDefaultScope(
+      url, url, ContentSettingsType::NOTIFICATIONS,
+      ContentSetting::CONTENT_SETTING_ALLOW);
+
+  ResetAllContentSettingsForWebApp(profile(), url);
+
+  EXPECT_EQ(host_content_settings_map->GetPermissionSetting(
+                url, url, ContentSettingsType::GEOLOCATION_WITH_OPTIONS),
+            content_settings::PermissionSettingsRegistry::GetInstance()
+                ->Get(ContentSettingsType::GEOLOCATION_WITH_OPTIONS)
+                ->GetInitialDefaultSetting());
+  EXPECT_EQ(host_content_settings_map->GetPermissionSetting(
+                url, url, ContentSettingsType::NOTIFICATIONS),
+            content_settings::PermissionSettingsRegistry::GetInstance()
+                ->Get(ContentSettingsType::NOTIFICATIONS)
+                ->GetInitialDefaultSetting());
+}
 
 }  // namespace web_app
