8a8a6593ad54102d48f67fcee079d514c9e9abf4
Ryan Sultanem <rsult@google.com>
2026-03-04 01:54:48 -0800

    [AvatarSigninPromo] Trigger promo computation for Signed out profiles
    Promo on the avatar pill can now be shown to signed out users.
    Previously those promos were only triggered after the greeting
    (which is only shown to signed in users).
    Now the promo computation is triggered after a 30 seconds delay
    when the first browser of a profile is opened if the profile is
    signed out.
    
    Promo computation for Sign promo will be added in a follow up CL.
    
    Bug: 486109449
    Change-Id: Id30bbbe78ff4b3a46a8b238c84adcc4a0febb8d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7623621
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1593792}

--- chrome/browser/ui/views/profiles/avatar_toolbar_button.h
+++ chrome/browser/ui/views/profiles/avatar_toolbar_button.h
@@ -40,6 +40,8 @@ enum class AvatarDelayType {
   kSigninPendingText,
   // Delay for the promo that are shown by expanding the button.
   kPromo,
+  // Delay for the Promo trigger for signed out profiles.
+  kSignedOutPromo,
 #endif  // BUILDFLAG(ENABLE_DICE_SUPPORT)
 };
 
@@ -166,6 +168,10 @@ class AvatarToolbarButton : public ToolbarButton,
   // necessary to bypass resetting the profile - e.g. when attempting to reach
   // the limit counts.
   void ForceShowingPromoForTesting();
+
+  // Returns whether the delay timer was running or not.
+  // Stops the timer if it is running.
+  bool GetStateAndFireSignedOutTriggerDelayTimerForTesting();
 #endif  // BUILDFLAG(ENABLE_DICE_SUPPORT)
 
  private:
