00e17450ef32a539c8ff6cb6e38c91de0e060295
Aviv Kiss <avivkiss@google.com>
2026-03-04 15:49:19 -0800

    [next] Add finch variations to /async/folae requests.
    Bug: 484043972
    Change-Id: I081ae777680c2415927e040b3bb4416bf236291f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7635338
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Aviv Kiss <avivkiss@google.com>
    Auto-Submit: Aviv Kiss <avivkiss@google.com>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594289}

--- components/omnibox/browser/aim_eligibility_service.cc
+++ components/omnibox/browser/aim_eligibility_service.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/identity_manager/accounts_in_cookie_jar_info.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/signin/public/identity_manager/primary_account_access_token_fetcher.h"
+#include "components/variations/net/variations_http_headers.h"
 #include "google_apis/gaia/google_service_auth_error.h"
 #include "net/base/load_flags.h"
 #include "net/base/url_util.h"
@@ -1001,6 +1002,15 @@ void AimEligibilityService::StartServerEligibilityRequest(
   request->url = request_url;
 
   ConfigureRequestCookiesAndCredentials(request.get(), use_oauth);
+
+  // The user may be signed in or not. But we only care about experiment IDs
+  // from the variations server, which do not require the signed-in version of
+  // this method.
+  variations::AppendVariationsHeaderUnknownSignedIn(
+      request->url,
+      configuration_.is_off_the_record ? variations::InIncognito::kYes
+                                       : variations::InIncognito::kNo,
+      request.get());
   // If mode is POST with Proto, set method to POST.
   if (GetServerEligibilityRequestMode() ==
       ServerEligibilityRequestMode::kPostWithProto) {
--- components/omnibox/browser/aim_eligibility_service_unittest.cc
+++ components/omnibox/browser/aim_eligibility_service_unittest.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/search/search.h"
 #include "components/search_engines/search_engines_test_environment.h"
 #include "components/search_engines/template_url_service.h"
+#include "components/variations/scoped_variations_ids_provider.h"
 #include "net/base/url_util.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
@@ -106,6 +107,8 @@ class AimEligibilityServiceTest : public testing::Test {
 
  protected:
   base::test::TaskEnvironment task_environment_;
+  variations::test::ScopedVariationsIdsProvider scoped_variations_ids_provider_{
+      variations::VariationsIdsProvider::Mode::kUseSignedInState};
   search_engines::SearchEnginesTestEnvironment search_engines_test_environment_;
   network::TestURLLoaderFactory test_url_loader_factory_;
   std::unique_ptr<MockAimEligibilityServiceForInterception>
