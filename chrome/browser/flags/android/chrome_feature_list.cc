a49379aa6d45a82866d122644faf121806c73929
Brandon Wylie <wylieb@google.com>
2026-03-04 15:15:02 -0800

    [hub] Add flag for history clustering
    This flag will be used to control the history clustering logic for the
    hub history pane. No other history surface will use this flag.
    
    Bug: 434195538
    Change-Id: Icf68f866d00fec6c7098f81d9f566ad30dfee515
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7629716
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1594268}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -228,6 +228,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kAndroidDesktopDensity,
     &kAndroidElegantTextHeight,
     &kAndroidFirstRunLaunchBounds,
+    &kAndroidHistoryClustering,
     &kAndroidNewMediaPicker,
     &kAndroidNoVisibleHintForDifferentTLD,
     &kAndroidOmniboxFocusedNewTabPage,
@@ -558,6 +559,7 @@ BASE_FEATURE(kAndroidDataImporterService, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kAndroidDesktopDensity, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kAndroidElegantTextHeight, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kAndroidFirstRunLaunchBounds, base::FEATURE_ENABLED_BY_DEFAULT);
+BASE_FEATURE(kAndroidHistoryClustering, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kAndroidNewMediaPicker, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kAndroidNoVisibleHintForDifferentTLD, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kAndroidOmniboxFocusedNewTabPage, base::FEATURE_DISABLED_BY_DEFAULT);
