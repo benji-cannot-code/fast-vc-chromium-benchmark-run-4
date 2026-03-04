6832e22ae21e8688950cfe5908619f6f0584722a
Youssef Bourouphael <ybourouphael@google.com>
2026-03-04 15:24:17 -0800

    Add WebUI backwards and forwards buttons.
    This change adds WebUI back and forward navigation buttons, following
    the pattern used for other toolbar controls like the reload and
    split-tabs buttons.
    
    A new `back-forward-button-app` Lit component is introduced for the
    frontend, and a corresponding `WebUIBackForwardControl` class is added
    to the C++ backend to manage its logic. The `ToolbarView` now
    conditionally renders either the native views button or the WebUI
    implementation based on the `kWebUIBackForwardButton` feature flag,
    which is off by default.
    
    Bug: 470038385
    Change-Id: I3468fbbcd74875ce08c41dd1af1a984b0570a4e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7466673
    Commit-Queue: Youseff Bourouphel <ybourouphael@google.com>
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594275}

--- chrome/browser/ui/webui/webui_toolbar/adapters/browser_controls_adapter_impl.cc
+++ chrome/browser/ui/webui/webui_toolbar/adapters/browser_controls_adapter_impl.cc
@@ -8,10 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_deref.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/command_updater.h"
+#include "chrome/browser/preloading/chrome_preloading.h"
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/tabs/split_tab_metrics.h"
 #include "chrome/browser/ui/webui/webui_toolbar/utils/split_tabs_utils.h"
+#include "components/tabs/public/tab_interface.h"
+#include "content/public/browser/web_contents.h"
 
 namespace browser_controls_api {
 
@@ -32,6 +35,20 @@ void BrowserControlsAdapterImpl::Reload(bool bypass_cache,
 void BrowserControlsAdapterImpl::Stop() {
   command_updater_->ExecuteCommandWithDisposition(
       IDC_STOP, WindowOpenDisposition::CURRENT_TAB);
+}
+
+void BrowserControlsAdapterImpl::Back(WindowOpenDisposition disposition) {
+  command_updater_->ExecuteCommandWithDisposition(IDC_BACK, disposition);
+}
+
+void BrowserControlsAdapterImpl::Forward(WindowOpenDisposition disposition) {
+  command_updater_->ExecuteCommandWithDisposition(IDC_FORWARD, disposition);
+}
+
+void BrowserControlsAdapterImpl::BackButtonHovered() {
+  browser_.get().GetActiveTabInterface()->GetContents()->BackNavigationLikely(
+      chrome_preloading_predictor::kBackButtonHover,
+      WindowOpenDisposition::CURRENT_TAB);
 }
 
 void BrowserControlsAdapterImpl::CreateNewSplitTab() {
--- tools/gritsettings/resource_ids.spec
+++ tools/gritsettings/resource_ids.spec
@@ -607,7 +607,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     "includes": [4660],
   },
    "<(SHARED_INTERMEDIATE_DIR)/chrome/browser/resources/webui_toolbar/resources.grd": {
-    "META": {"sizes": {"includes": [20]}},
+    "META": {"sizes": {"includes": [30]}},
     "includes": [4670],
   },
   "<(SHARED_INTERMEDIATE_DIR)/chrome/browser/resources/sandbox_internals/resources.grd": {
