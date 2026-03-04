87bca64ad649e5a098c5ca7eaaa985375fe8568e
Sky Malice <skym@chromium.org>
2026-03-04 10:28:19 -0800

    Revert "Move out default of onCallbackNotFoundError."
    This reverts commit 97eceffad83ed35212618ff64479f1ff4a7e5781.
    
    Reason for revert: Landed on top of a CL that crashed WebView
    
    Original change's description:
    > Move out default of onCallbackNotFoundError.
    >
    > Bug: 486880198
    > Change-Id: I416f037360f803e5018388f2512da60675ebbcd3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7601112
    > Reviewed-by: Menghan Yang <myuu@google.com>
    > Commit-Queue: Sky Malice <skym@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1593403}
    
    Bug: 486880198, 488022589
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 486880198
    Change-Id: I7c458c576e873aab72068d56ae3f2608752beccc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7632584
    Auto-Submit: Sky Malice <skym@chromium.org>
    Reviewed-by: Hitarth Kothari <hitarthkothari@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1594067}

--- ui/android/java/src/org/chromium/ui/base/ActivityIntentRequestTrackerDelegate.java
+++ ui/android/java/src/org/chromium/ui/base/ActivityIntentRequestTrackerDelegate.java
@@ -67,9 +67,4 @@ public class ActivityIntentRequestTrackerDelegate implements Delegate {
     public final WeakReference<Activity> getActivity() {
         return mActivityWeakRefHolder;
     }
-
-    @Override
-    public boolean onCallbackNotFoundError(String error) {
-        return false;
-    }
 }
