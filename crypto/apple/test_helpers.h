0e98c3e17664abb10a26f110a10b92a128ed27db
Anatoli Hancharou <antoli@google.com>
2026-03-04 02:52:51 -0800

    [crypto] Extract SecKeyFromPKCS8 to test helper
    Moves the `SecKeyFromPKCS8` helper function from individual test files
    into `crypto/apple/keychain_util` to make it available as a shared
    utility. This method will be used by upcoming integration tests for
    client certificate authentication on iOS.
    
    Change-Id: I5316ae837e0cd994531e2ff1e246b471cdbae390
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7617527
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Anatoli Hancharou <antoli@google.com>
    Cr-Commit-Position: refs/heads/main@{#1593819}

---
+++ crypto/apple/test_helpers.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CRYPTO_APPLE_TEST_HELPERS_H_
+#define CRYPTO_APPLE_TEST_HELPERS_H_
+
+#include <Security/SecKey.h>
+
+#include "base/apple/scoped_cftyperef.h"
+#include "base/containers/span.h"
+
+namespace crypto::apple {
+
+// Converts a PKCS#8 encoded private key in `pkcs8` into a SecKeyRef.
+// Returns a null scoper if the key could not be parsed.
+base::apple::ScopedCFTypeRef<SecKeyRef> SecKeyFromPKCS8(
+    base::span<const uint8_t> pkcs8);
+
+}  // namespace crypto::apple
+
+#endif  // CRYPTO_APPLE_TEST_HELPERS_H_
