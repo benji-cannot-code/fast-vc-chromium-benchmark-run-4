d2760ce1af3a2538f24a94f0e958333296f891b5
Claudio DeSouza <cdesouza@chromium.org>
2026-03-04 13:39:02 -0800

    IWYU for `SecurityPrincipal`
    This CL adds the missing header for `CONTENT_EXPORT` for this class.
    
    Bug: 397481045
    Change-Id: I7f12fd3b784001c22068f1baa7bee668c25f755c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7608625
    Commit-Queue: Claudio DeSouza <cdesouza@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594216}

--- content/public/browser/security_principal.h
+++ content/public/browser/security_principal.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_PUBLIC_BROWSER_SECURITY_PRINCIPAL_H_
 #define CONTENT_PUBLIC_BROWSER_SECURITY_PRINCIPAL_H_
 
+#include "content/common/content_export.h"
+
 namespace content {
 
 class StoragePartitionConfig;
