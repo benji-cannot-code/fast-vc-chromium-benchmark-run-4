1241c03baf919020206ae3d27dd376f34f9a7382
Ananya Seelam <ananyaseelam@google.com>
2026-03-04 14:38:25 -0800

    Revert "[aim-composebox] Use `ScopedObservation` for Context Controller"
    This reverts commit 3f136aad622773193aed679a3e3894df7076f2ae.
    
    Reason for revert: May be causing crash on Composeboxhandler::ClearFiles
    
    Original change's description:
    > [aim-composebox] Use `ScopedObservation` for Context Controller
    >
    > To ensure all observers are unhooked once SessionHandle is freed, make
    > `ContextualSearchContextController` a `ScopedObservation`. This way,
    > there is no longer a need to add and remove observers and we can ensure
    > that we are not observing a freed session handle.
    >
    > Bug: b:487773783 b:489770388
    > Change-Id: I7dc35b282416bec718e98264154d83386f759f07
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7624639
    > Reviewed-by: Josiah Kiehl <kiehl@google.com>
    > Commit-Queue: Ananya Seelam <ananyaseelam@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1593549}
    
    Bug: b:487773783
    Change-Id: I96195e3c27167550858f2e2bf7ea0f7a350ca2cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7634600
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Josiah Kiehl <kiehl@google.com>
    Commit-Queue: Ananya Seelam <ananyaseelam@google.com>
    Cr-Commit-Position: refs/heads/main@{#1594249}

--- chrome/browser/ui/webui/cr_components/searchbox/contextual_searchbox_handler.h
+++ chrome/browser/ui/webui/cr_components/searchbox/contextual_searchbox_handler.h
@@ -185,13 +185,6 @@ class ContextualSearchboxHandler
     OnInputStateChanged(state);
   }
 
-  base::ScopedObservation<contextual_search::ContextualSearchContextController,
-                          contextual_search::ContextualSearchContextController::
-                              FileUploadStatusObserver>&
-  context_controller_observation_for_testing() {
-    return context_controller_observation_;
-  }
-
  protected:
   // SearchboxHandler:
   omnibox::InputState GetInputState() const override;
@@ -282,10 +275,8 @@ class ContextualSearchboxHandler
 
   // The context controller this searchbox is listening to for file upload
   // status updates.
-  base::ScopedObservation<contextual_search::ContextualSearchContextController,
-                          contextual_search::ContextualSearchContextController::
-                              FileUploadStatusObserver>
-      context_controller_observation_{this};
+  base::WeakPtr<contextual_search::ContextualSearchContextController>
+      context_controller_;
 
   std::optional<lens::ContextualInputData> context_input_data_;
 
