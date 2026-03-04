2ffa1e64f656aaa66fb1fca110a43003403b471c
Peter Kvitek <kvitekp@chromium.org>
2026-03-04 14:02:09 -0800

    [headless] Add DevTools Emulation.updateScreen() support on Linux
    http://crrev.com/c/7617849 introduced Chrome DevTools method
    Emulation.updateScreen() support in Chrome Headless Shell. This CL adds
    support for this method in Chrome Headless Mode on Linux.
    
    Bug: 397350115
    Change-Id: Ief8f108a423c21aff3f0a92ef4dcc93a1fd99080
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7633786
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594231}

--- ui/ozone/platform/headless/headless_screen.cc
+++ ui/ozone/platform/headless/headless_screen.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_deref.h"
 #include "base/command_line.h"
 #include "base/containers/flat_set.h"
-#include "base/notimplemented.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
 #include "components/headless/display_util/headless_display_util.h"
@@ -163,8 +162,7 @@ int64_t HeadlessScreen::AddDisplay(const Display& display) {
 }
 
 void HeadlessScreen::UpdateDisplay(const Display& display) {
-  // TODO(crbug.com/397350115): Implement.
-  NOTIMPLEMENTED();
+  display_list_.UpdateDisplay(display);
 }
 
 void HeadlessScreen::RemoveDisplay(int64_t display_id) {
