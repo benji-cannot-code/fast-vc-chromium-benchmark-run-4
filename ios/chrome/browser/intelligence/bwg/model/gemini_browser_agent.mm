d3514d13e97beb44e8c992f2137054317f70abca
Joemer Ramos <joemerramos@google.com>
2026-03-04 11:05:15 -0800

    [iOS] Refactor ZeroStateWithChatHistory to GeminiChatPersistence
    Refactors GeminiCopresenceZeroStateWithChatHistory to
    GeminiChatPersistence. Adds an additional config param for naming
    clarity, and will remove the old property after the rename/refactor
    is complete.
    
    Bug: 489117306
    Change-Id: I1807bf6e8571eeb38d6ef813d037431beef0403f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7629799
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Commit-Queue: Joemer Ramos <joemerramos@google.com>
    Cr-Commit-Position: refs/heads/main@{#1594096}

--- ios/chrome/browser/intelligence/bwg/model/gemini_browser_agent.mm
+++ ios/chrome/browser/intelligence/bwg/model/gemini_browser_agent.mm
@@ -766,8 +766,7 @@ void GeminiBrowserAgent::OnActiveWebStateChanged(web::WebState* old_active,
     [new_active->GetWebViewProxy().scrollViewProxy
         addObserver:scroll_observer_];
 
-    if (!IsGeminiCopresenceZeroStateWithChatHistoryEnabled() ||
-        !is_floaty_invoked_) {
+    if (!IsGeminiChatPersistenceEnabled() || !is_floaty_invoked_) {
       return;
     }
 
@@ -972,8 +971,10 @@ void GeminiBrowserAgent::PresentFloatyWithState(
   config.responseReadyInterval = GetGeminiCopresenceResponseReadyInterval();
   config.responseViewDynamicSizeEnabled =
       IsGeminiResponseViewDynamicResizingEnabled();
+  // TODO(crbug.com/489117306): Remove this property after refactor.
   config.geminiCopresenceZeroStateWithChatHistoryEnabled =
-      IsGeminiCopresenceZeroStateWithChatHistoryEnabled();
+      IsGeminiChatPersistenceEnabled();
+  config.geminiChatPersistenceEnabled = IsGeminiChatPersistenceEnabled();
 
   // Set the location permission state.
   // TODO(crbug.com/426207968): Populate with actual value.
