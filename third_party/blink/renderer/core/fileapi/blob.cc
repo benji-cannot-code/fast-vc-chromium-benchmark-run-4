bf28fae49f34f71b61b6aaef9d8899777793ed8d
Fredrik Söderquist <fs@opera.com>
2026-03-04 14:33:13 -0800

    Replace some String::DeprecatedLower() with LowerASCII()
    None of these should be a change in behavior, because the input is
    expected to be ASCII already: In Blob::NormalizeType() the previous line
    validated that the String only contains (a subset of) ASCII, and in the
    other cases it's operating on the normalized encoding name.
    
    Bug: 40476285
    Change-Id: I01e1600913dbbdedaa25d34ff88d6a24add71709
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7633472
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594248}

--- third_party/blink/renderer/core/fileapi/blob.cc
+++ third_party/blink/renderer/core/fileapi/blob.cc
@@ -336,7 +336,7 @@ String Blob::NormalizeType(const String& type) {
   if (!IsValidBlobType(type)) {
     return g_empty_string;
   }
-  return type.DeprecatedLower();
+  return type.LowerASCII();
 }
 
 }  // namespace blink
