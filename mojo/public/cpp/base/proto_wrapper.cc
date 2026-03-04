188007d11d845c9484034ca640a8d5ed3c17f1de
Dave Tapuska <dtapuska@chromium.org>
2026-03-04 12:11:24 -0800

    [mojo] Support cloning of a proto wrapper
    BigBuffer supports Clone so ProtoWrapper should as well.
    
    Bug: b:489714641
    Change-Id: Ied3aed6491d96783e1d98d27d55ffae2c0f14cab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7634678
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594154}

--- mojo/public/cpp/base/proto_wrapper.cc
+++ mojo/public/cpp/base/proto_wrapper.cc
@@ -62,4 +62,13 @@ bool ProtoWrapper::DeserializeToMessage(
   }
 }
 
+ProtoWrapper ProtoWrapper::Clone() const {
+  ProtoWrapper result;
+  result.proto_name_ = proto_name_;
+  if (is_valid()) {
+    result.bytes_ = bytes_->Clone();
+  }
+  return result;
+}
+
 }  // namespace mojo_base
