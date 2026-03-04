b347d427c39089d91f042e0b4e46bb85d6066588
Chrome Crash (Prod) <crash@system.gserviceaccount.com>
2026-03-04 10:56:52 -0800

    Revert "[PiP][Fullscreen] Add FullscreenVideoPictureInPicture feature flag for Android."
    This reverts commit 114015fc10299ed363125df7ceeabbafe4e044db.
    
    Reason for revert: 
    The Suspect CL https://chromium-review.googlesource.com/q/I41bdfb0b3ebd524aaf5b920868818c820ada6115 is highly likely the cause of the crash. Please see the crash report http://crash/583804505b0352cd.
    Rebase the Revert CL on top of main branch with conflicts allowed option to prevent false positive merge conflicts.
    
    If you agree with the revert CL, please approve it and mark as Commit-Queue +2 to submit it.
    
    More details can be found in the go/crash-revert-cls-creation.
    
    Original change's description:
    > [PiP][Fullscreen] Add FullscreenVideoPictureInPicture feature flag for Android.
    > 
    > This CL introduces a new feature flag `FullscreenVideoPictureInPicture`
    > to control the fullscreen video Picture-in-Picture behavior on Android.
    > The flag is used to guard the initialization and usage of
    > `FullscreenVideoPictureInPictureController` in `ChromeActivity`.
    > 
    > Key changes:
    > - Defined `kFullscreenVideoPictureInPicture` in media switches.
    > - Exposed the flag to Java via `ChromeFeatureList`.
    > - Guarded `FullscreenVideoPictureInPictureController` in `ChromeActivity`.
    > - Added the flag to `chrome://flags`.
    > - Added field trial testing config and metrics enums.
    > - Added tests for both enabled and disabled states.
    > 
    > Bug: 486262122
    > Change-Id: I41bdfb0b3ebd524aaf5b920868818c820ada6115
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7609066
    > Commit-Queue: Phil Yan <philyan@chromium.org>
    > Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    > Reviewed-by: Frank Liberato <liberato@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1593419}
    > 
    Crash Bug: 489715789
    
    Change-Id: Ie5cbccacd08006fc40a6921063f0e269be5d47f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7633862
    Commit-Queue: Phil Yan <philyan@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Phil Yan <philyan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594090}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -465,7 +465,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &media::kAutoDocPiPPermissionPromptAndroid,
     &media::kAutoPictureInPictureAndroid,
     &media::kContextMenuPictureInPictureAndroid,
-    &media::kFullscreenVideoPictureInPicture,
     &net::features::kVerifyQWACs,
     &network::features::kLocalNetworkAccessChecks,
     &network::features::kLocalNetworkAccessChecksSplitPermissions,
