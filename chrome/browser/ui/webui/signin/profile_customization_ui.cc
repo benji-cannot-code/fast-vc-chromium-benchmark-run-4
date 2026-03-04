e22d4db43bc26cf1aaf4004e43ac3a0dab0a7507
Kacper Bajkiewicz <kbajkiewicz@google.com>
2026-03-04 03:48:23 -0800

    [FRE Refresh] Refreshed profile customization bubble.
    Added refreshed outlook of profile customization bubble shown at the end
    of profile creation flow. Added labels above theme picker and profile
    name input.
    
    As there is an ongoing experiment at the time of landing this CL
    (#profile-creation-friction-reduction-experiment-prefill-name-requirement),
    prefilling the profile name happens only when the experiment is enabled,
    we show the labels regardless (for that refreshed UI has to be enabled
    only). Placeholder will be shown only for old UI / experiment disabled
    as we have the label above the input field that serves the same purpose.
    
    Final state of avatar placeholder is yet to be clarified with UX. All
    screenshots are from Windows.
    
    With prefill name requirement experiment enabled:
    - previous view (for comparison, light mode)
    https://screenshot.googleplex.com/8G574XU3sa7MYQS
    - empty profile input
    https://screenshot.googleplex.com/ARq52bLYU8St5PB
    - prefilled profile name
    https://screenshot.googleplex.com/BcnK9JgeZG6cXGV
    
    With the experiment disabled:
    - previous view
    - refreshed view (light mode)
    - refreshed view (dark mode)
    - custom avatar selected
    
    https: //screenshot.googleplex.com/BEash7Gc7kNywkr
    https: //screenshot.googleplex.com/3uNv67MKC9sf62H
    https: //screenshot.googleplex.com/By2F9kqQjKQrAmz
    https: //screenshot.googleplex.com/8hFYEbyeQ2iCqga
    Change-Id: I26bca7dce21e609ce509ba190bb5cbd1c94bcfab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7626178
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Commit-Queue: Kacper Bajkiewicz <kbajkiewicz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1593835}

--- chrome/browser/ui/webui/signin/profile_customization_ui.cc
+++ chrome/browser/ui/webui/signin/profile_customization_ui.cc
@@ -74,6 +74,8 @@ ProfileCustomizationUI::ProfileCustomizationUI(content::WebUI* web_ui)
       {"profileCustomizationInputErrorMessage",
        IDS_PROFILE_CUSTOMIZATION_INPUT_ERROR_MESSAGE},
       {"profileCustomizationText", IDS_PROFILE_CUSTOMIZATION_TEXT},
+      {"profileCustomizationThemePickerLabel",
+       IDS_PROFILE_CUSTOMIZATION_THEME_PICKER_LABEL},
       {"profileCustomizationTitle", IDS_PROFILE_CUSTOMIZATION_TITLE_V2},
       {"localProfileCreationTitle",
        IDS_PROFILE_CUSTOMIZATION_LOCAL_PROFILE_CREATION_TITLE},
@@ -115,6 +117,9 @@ ProfileCustomizationUI::ProfileCustomizationUI(content::WebUI* web_ui)
       base::FeatureList::IsEnabled(
           switches::
               kProfileCreationFrictionReductionExperimentPrefillNameRequirement));
+
+  source->AddBoolean("isRefreshedUI", base::FeatureList::IsEnabled(
+                                          switches::kFirstRunDesktopRefresh));
 
   if (url.GetQuery() == "debug") {
     // Not intended to be hooked to anything. The bubble will not initialize it
