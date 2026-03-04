b6368fb5af35a683b818be467e6e0adf79f22b59
Nafis Abedin <nafisabedin@google.com>
2026-03-04 08:02:21 -0800

    [Cleanup] Remove feature to use the thread pool for WebView Viz thread
    Bug: 489450060
    Change-Id: I627ba5ab783f5ab8fc84a76715fd6d66b89223ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7629755
    Commit-Queue: Nafis Abedin <nafisabedin@google.com>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1593961}

--- android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
+++ android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
@@ -827,7 +827,6 @@ public final class ProductionSupportedFlagList {
                 "Avoids cloning of gpu fences when possible"),
         Flag.baseFeature(BlinkFeatures.STREAMLINE_RENDERER_INIT),
         Flag.baseFeature("LazyBindJsInjection"),
-        Flag.baseFeature("WebViewVizUseThreadPool"),
         Flag.baseFeature("InProcessGpuUseIOThread"),
         Flag.baseFeature("EnableCustomInputStreamBufferSize"),
         Flag.baseFeature("NetworkServiceDedicatedThread"),
