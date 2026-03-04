a595d878222db5c90f10fc0086a59bf07f81c413
Jason Hu <hujasonx@google.com>
2026-03-04 09:01:53 -0800

    [context-input] Add has_chrome_tab_data to request id
    This bit is determined by whether or not a tab session id was associated
    with the contextual input data.
    
    Bug: b:489129574
    Change-Id: I01bfc274f3897b6ba5444bf9bdec8b5383bb229a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7634059
    Auto-Submit: Jason Hu <hujasonx@google.com>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Cr-Commit-Position: refs/heads/main@{#1594013}

--- components/lens/lens_overlay_request_id_generator.cc
+++ components/lens/lens_overlay_request_id_generator.cc
@@ -42,6 +42,7 @@ void LensOverlayRequestIdGenerator::ResetRequestId() {
   long_context_id_ = 0;
   analytics_id_ = base::RandBytesAsString(kAnalyticsIdBytesSize);
   context_id_ = RandInt64();
+  has_chrome_tab_data_ = false;
   routing_info_.reset();
 }
 
@@ -90,6 +91,8 @@ LensOverlayRequestIdGenerator::CreateNextRequestIdForUpdate(
         previous_request_id->routing_info());
   }
   request_id->set_context_id(previous_request_id->context_id());
+  request_id->set_has_chrome_tab_data(
+      previous_request_id->has_chrome_tab_data());
 
   bool create_analytics_id =
       update_mode != RequestIdUpdateMode::kSearchUrl &&
@@ -163,6 +166,7 @@ LensOverlayRequestIdGenerator::GetCurrentRequestId() {
     request_id->mutable_routing_info()->CopyFrom(routing_info_.value());
   }
   request_id->set_context_id(context_id_);
+  request_id->set_has_chrome_tab_data(has_chrome_tab_data_);
   return request_id;
 }
 }  // namespace lens
