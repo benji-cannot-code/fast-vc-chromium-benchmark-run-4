e9d0429220c1b985309fc6e6345f8c59ad5f527c
Anunoy Ghosh <anunoy@chromium.org>
2026-03-04 08:50:27 -0800

    Add ExtensionDOMActivityLoggingEnabled enterprise policy
    Define an enterprise policy, ExtensionDOMActivityLoggingEnabled,
    that will be used to gate the extension DOM activity telemetry
    feature for enterprise security auditing.
    
    Note: This CL only defines the policy, maps it to a profile pref
    and registers the pref. The logic to listen for real-time changes
    to the pref and toggle policy state, propagate it to the renderer
    etc. is done in a subsequent CL.
    
    Bug: 483497386
    Change-Id: I3a8453fff90215e0c5c823355992a56d4b9cd044
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7628038
    Reviewed-by: Sébastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Igor <igorcov@chromium.org>
    Commit-Queue: Anunoy Ghosh <anunoy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594004}

--- chrome/browser/policy/configuration_policy_handler_list_factory.cc
+++ chrome/browser/policy/configuration_policy_handler_list_factory.cc
@@ -2207,6 +2207,12 @@ const PolicyToPreferenceMapEntry kSimplePolicyMap[] = {
     base::Value::Type::BOOLEAN },
 #endif // !BUILDFLAG(IS_CHROMEOS) && BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 
+#if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
+  { key::kExtensionDOMActivityLoggingEnabled,
+    prefs::kExtensionDOMActivityLoggingEnabled,
+    base::Value::Type::BOOLEAN },
+#endif // BUILDFLAG(ENABLE_EXTENSIONS_CORE)
+
 #if BUILDFLAG(ENABLE_EXTENSIONS)
   { key::kExtensionExtendedBackgroundLifetimeForPortConnectionsToUrls,
     extensions::pref_names::kExtendedBackgroundLifetimeForPortConnectionsToUrls,
