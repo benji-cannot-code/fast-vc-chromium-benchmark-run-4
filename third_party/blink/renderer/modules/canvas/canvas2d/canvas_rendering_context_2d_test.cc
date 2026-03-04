f4b4015c84553b71c520cc9f783990a905ab6b74
Colin Blundell <blundell@chromium.org>
2026-03-04 01:32:56 -0800

    [Blink] Rename SupportsNativeMappableSIForCanvas2D()
    The query is not whether native mappable SIs are *supported* for Canvas
    2D but rather whether they should be *used*. Currently we choose to use
    them only in a subset of the cases where they are actually supported
    (and we might choose to use mappable SIs in this flow in the future in
    cases where the mapping would be done via shared memory, e.g. on
    Windows).
    
    Bug: 485669935
    Change-Id: Ieed6ca7fa57607a658aaf868ce5a9c49e6ebf2c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7626839
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1593775}

--- third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d_test.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d_test.cc
@@ -1533,8 +1533,7 @@ TEST_P(CanvasRenderingContext2DTest,
        UnacceleratedLowLatencyIsNotSingleBuffered) {
   // Ensure that the context will create a SharedImage provider for the test to
   // be meaningful.
-  SharedGpuContext::SetNativeMappableSharedImagesSupportedForCanvas2DForTesting(
-      true);
+  SharedGpuContext::SetUseMappableSharedImagesForCanvas2DForTesting(true);
   ScopedTestingPlatformSupport<GpuCompositingTestPlatform> platform;
   const_cast<gpu::Capabilities&>(SharedGpuContext::ContextProviderWrapper()
                                      ->ContextProvider()
@@ -1717,9 +1716,8 @@ TEST_P(CanvasRenderingContext2DTest, AutoFlushDelayedByLayer) {
 }
 
 TEST_P(CanvasRenderingContext2DTest,
-       SoftwareCanvasIsCompositedIfNativeMappableBuffersAreSupported) {
-  SharedGpuContext::SetNativeMappableSharedImagesSupportedForCanvas2DForTesting(
-      true);
+       SoftwareCanvasIsCompositedIfMappableSharedImageIsUsed) {
+  SharedGpuContext::SetUseMappableSharedImagesForCanvas2DForTesting(true);
 
   // Ensure that support for BGRA overlays is present, as otherwise compositing
   // will not occur regardless.
@@ -1739,9 +1737,8 @@ TEST_P(CanvasRenderingContext2DTest,
 }
 
 TEST_P(CanvasRenderingContext2DTest,
-       SoftwareCanvasIsNotCompositedIfNativeMappableBuffersAreNotSupported) {
-  SharedGpuContext::SetNativeMappableSharedImagesSupportedForCanvas2DForTesting(
-      false);
+       SoftwareCanvasIsNotCompositedIfMappableSharedImageIsNotUsed) {
+  SharedGpuContext::SetUseMappableSharedImagesForCanvas2DForTesting(false);
 
   CreateContext(kNonOpaque);
   EXPECT_TRUE(Context2D()->GetOrCreateResourceProvider());
