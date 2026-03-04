7f5e997fa1b23242eab19bbd26e0c9737b9e6b0e
Jun Ishiguro <junis@google.com>
2026-03-04 02:14:47 -0800

    login: Remove unnecessary include of global_features.h
    The include of global_features.h is no longer necessary for
    WizardController.
    
    Bug: b:404133029
    Change-Id: I17287c70bad851849e1038f9f84aa811ebf466cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7631279
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Georg Neis <neis@chromium.org>
    Auto-Submit: Jun Ishiguro <junis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1593809}

--- chrome/browser/ash/login/wizard_controller.cc
+++ chrome/browser/ash/login/wizard_controller.cc
@@ -153,7 +153,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process_platform_part.h"
 #include "chrome/browser/enterprise/util/affiliation.h"
 #include "chrome/browser/enterprise/util/managed_browser_utils.h"
-#include "chrome/browser/global_features.h"
 #include "chrome/browser/metrics/cros_pre_consent_metrics_manager.h"
 #include "chrome/browser/metrics/metrics_reporting_state.h"
 #include "chrome/browser/policy/profile_policy_connector.h"
