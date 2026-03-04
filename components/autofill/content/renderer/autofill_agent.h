7e474c4868b2bea9133859565b412593d70e2a88
Jihad Hanna <jihadghanna@google.com>
2026-03-04 13:23:00 -0800

    Add Autofill[Driver|Agent]::ScrollFieldIntoView()
    This will be used by the actor to bring a relevant field into view.
    
    Bug: 481379667
    Change-Id: I991768263d9480d440f7a64b4ecd0d0e87eaa554
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7606584
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594207}

--- components/autofill/content/renderer/autofill_agent.h
+++ components/autofill/content/renderer/autofill_agent.h
@@ -173,6 +173,7 @@ class AutofillAgent : public content::RenderFrameObserver,
                      base::OnceCallback<void(bool)> callback) override;
 
   // mojom::AutofillAgent:
+  void ScrollFieldIntoView(FieldRendererId field_id) override;
   void TriggerFormExtraction() override;
   void TriggerFormExtractionWithResponse(
       base::OnceCallback<void(bool)> callback) override;
