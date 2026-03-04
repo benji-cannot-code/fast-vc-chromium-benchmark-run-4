a685aa42297a1db1715913fd829a151b0b4a008b
Christopher Grant <cjgrant@chromium.org>
2026-03-04 14:28:47 -0800

    [SEH] Improve screen-reader operation on explicit-choice dialog
    See crbug.com/487728308 for more context.
    
    This change implements several screen reader fixes:
    
    - Have the "compound" radio buttons state their position in the group
      properly (eg. "1 of 2" instead of "1 of 1"). This may have been broken
      because these radio buttons aren't direct siblings like most others.
    
    - Move the RichRadioButton title onto the underlying RadioButton, and
      add the Description there as well, so the radio button screen-reads
      more like a typical RadioButton.
    
    - Have the invisible "focus trap" element make itself useful by reading
      the description of the dialog. There may be a better way to do this,
      but I can't find a way to have the description Paragraph read itself
      when the dialog is initially opened.  This is better than it was, and
      can be further improved if needed in follow-on changes.
    
    This feature is behind a flag.
    
    Bug: 461806299, 487728308
    Change-Id: Iba551c025a8335b5681d2aa8d8db85464c8d4bc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7608693
    Auto-Submit: Christopher Grant <cjgrant@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594245}

--- chrome/browser/ui/views/extensions/rich_radio_button.cc
+++ chrome/browser/ui/views/extensions/rich_radio_button.cc
@@ -28,6 +28,8 @@ RichRadioButton::RichRadioButton(const ui::ImageModel& image,
                                  const std::u16string& title,
                                  const std::u16string& description,
                                  int group_id,
+                                 int pos_in_set,
+                                 int set_size,
                                  base::RepeatingClosure on_selected_callback)
     : views::Button(base::BindRepeating(
           [](RichRadioButton* view) { view->radio_button_->SetChecked(true); },
@@ -35,7 +37,10 @@ RichRadioButton::RichRadioButton(const ui::ImageModel& image,
   SetFocusBehavior(FocusBehavior::NEVER);
   views::Builder<RichRadioButton>(this)
       .SetLayoutManager(std::make_unique<views::FlexLayout>())
-      .SetAccessibleName(title)
+      // Act as a group with no accessible name. The underlying radio button
+      // will present the necessary information.
+      .SetAccessibleRole(ax::mojom::Role::kGroup)
+      .SetAccessibleName(u"", ax::mojom::NameFrom::kAttributeExplicitlyEmpty)
       .CustomConfigure(base::BindOnce([](RichRadioButton* view) {
         static_cast<views::FlexLayout*>(view->GetLayoutManager())
             ->SetOrientation(views::LayoutOrientation::kHorizontal)
@@ -69,9 +74,12 @@ RichRadioButton::RichRadioButton(const ui::ImageModel& image,
               .SetProperty(views::kFlexBehaviorKey,
                            views::FlexSpecification().WithAlignment(
                                views::LayoutAlignment::kEnd))
-              .SetAccessibleName(
-                  title, ax::mojom::NameFrom::kAttributeExplicitlyEmpty))
+              .SetAccessibleName(title)
+              .SetAccessibleDescription(description))
       .BuildChildren();
+
+  radio_button_->GetViewAccessibility().SetPosInSet(pos_in_set);
+  radio_button_->GetViewAccessibility().SetSetSize(set_size);
 
   subscription_ = radio_button_->AddCheckedChangedCallback(base::BindRepeating(
       [](views::RadioButton* button, base::RepeatingClosure closure) {
