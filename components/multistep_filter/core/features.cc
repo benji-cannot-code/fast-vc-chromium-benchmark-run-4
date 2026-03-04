5385fcf50dd27e0e92ea98059c4368fc6401cfce
Aditya Gupta <adigupt@google.com>
2026-03-04 09:40:15 -0800

    Multistep Filter: Add initial component structure
    This CL introduces the new "Multistep Filter" component in
    //components/multistep_filter. The Multistep Filter service is
    responsible for generating filter suggestions based on the user's past
    navigation history.
    
    This component is being introduced to share this logic between: 1.
    Desktop Chrome (chrome/browser/multistep_filter) 2. Android (and
    potentially iOS) platforms
    
    More details: go/chrome-filters-code-structure
    
    The initial implementation includes:
    - Feature flag definitions (kMultistepFilter).
    - Data models (FilterSuggestion).
    - The core suggestion generation logic (FilterSuggestionGenerator).
    
    Future CLs will integrate this service into the browser process and
    provide the necessary UI bindings.
    
    Bug: 489000280, 489002584, 485485362
    Test: components_unittests --gtest_filter=FilterSuggestionGeneratorTest.*
    Change-Id: I18486b776b773b7fdc43eecd13375fc0dd336a90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7585775
    Commit-Queue: Aditya Gupta <adigupt@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Michael Lerman <mlerman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594030}

---
+++ components/multistep_filter/core/features.cc
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/multistep_filter/core/features.h"
+
+namespace multistep_filter {
+
+// Multistep Filter is a feature to generate filter suggestions to users based
+// on their previous browsing history.
+BASE_FEATURE(kMultistepFilter, base::FEATURE_DISABLED_BY_DEFAULT);
+
+}  // namespace multistep_filter
---
+++ components/multistep_filter/core/features.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_MULTISTEP_FILTER_CORE_FEATURES_H_
+#define COMPONENTS_MULTISTEP_FILTER_CORE_FEATURES_H_
+
+#include "base/feature_list.h"
+
+namespace multistep_filter {
+
+// Enables the Multistep Filter feature to generate filter suggestions.
+BASE_DECLARE_FEATURE(kMultistepFilter);
+
+}  // namespace multistep_filter
+
+#endif  // COMPONENTS_MULTISTEP_FILTER_CORE_FEATURES_H_
