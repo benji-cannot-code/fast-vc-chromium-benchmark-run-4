52dd96b78e1f97ba796f8b5675da53b2c78fd50f
Kevin McNee <mcnee@chromium.org>
2026-03-04 09:25:16 -0800

    Refactor AttemptLoginToolInteractiveUiTests
    This'll make it easier to reuse test code for an additional icon that'll
    be passed through the glic API.
    
    Bug: b:488443317
    Change-Id: I216cda90403a90712f8f0b64ead563d7ee822b3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7627246
    Auto-Submit: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Aoyuan Zuo <zuoaoyuan@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594024}

--- chrome/browser/actor/tools/attempt_login_tool_interactive_ui_test.cc
+++ chrome/browser/actor/tools/attempt_login_tool_interactive_ui_test.cc
@@ -3,12 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/base64.h"
+#include <utility>
+
 #include "base/strings/strcat.h"
 #include "base/test/run_until.h"
 #include "base/test/scoped_feature_list.h"
-#include "cc/test/pixel_comparator.h"
-#include "cc/test/pixel_test_utils.h"
 #include "chrome/browser/actor/actor_features.h"
 #include "chrome/browser/actor/actor_test_util.h"
 #include "chrome/browser/actor/execution_engine.h"
@@ -25,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/content_browser_test_utils.h"
 #include "ui/compositor/compositor_switches.h"
-#include "ui/gfx/codec/png_codec.h"
 
 #if BUILDFLAG(IS_LINUX)
 #include "ui/ozone/public/ozone_platform.h"
@@ -45,6 +43,85 @@ const SkBitmap GenerateSquareBitmap(int size, SkColor color) {
   bitmap.eraseColor(color);
   bitmap.setImmutable();
   return bitmap;
+}
+
+void SetCredentialRequestHandler(
+    content::WebContents* glic_contents,
+    int credential_index,
+    webui::mojom::UserGrantedPermissionDuration permission_duration) {
+  static constexpr char kHandleDialogRequest[] =
+      R"js(
+      (() => {
+        /** Converts a PNG (Blob) to a base64 encoded string. */
+        function blobToBase64(blob) {
+          return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+              resolve(reader.result);
+            };
+            reader.onerror = reject;
+            reader.readAsDataURL(blob);
+          });
+        }
+
+        async function iconGetterToDataUrl(iconGetter) {
+          if (!iconGetter) {
+            return undefined;
+          }
+          const blob = await iconGetter();
+          if (!blob) {
+            return undefined;
+          }
+          return await blobToBase64(blob);
+        }
+
+        window.credentialDialogRequestData = new Promise(resolve => {
+          client.browser.selectCredentialDialogRequestHandler().subscribe(
+            async (request) => {
+              // Respond to the request by selecting the specified credential.
+              request.onDialogClosed({
+                response: {
+                  taskId: request.taskId,
+                  selectedCredentialId: request.credentials[$1].id,
+                  permissionDuration: $2,
+                }
+              });
+
+              const credentialsWithIcons = await Promise.all(
+                request.credentials.map(async (cred) => {
+                  const {getIcon, ...rest} = cred;
+                  const icon = await iconGetterToDataUrl(getIcon);
+                  return {...rest, icon};
+                })
+              );
+
+              // Resolve the promise with the request data to be verified in
+              // C++.
+              resolve({
+                taskId: request.taskId,
+                showDialog: request.showDialog,
+                credentials: credentialsWithIcons,
+              });
+            }
+          );
+        });
+      })();
+      )js";
+  ASSERT_TRUE(content::ExecJs(
+      glic_contents,
+      content::JsReplace(kHandleDialogRequest, credential_index,
+                         std::to_underlying(permission_duration))));
+}
+
+base::DictValue ExtractRequestData(content::WebContents* glic_contents) {
+  static constexpr char kGetRequestData[] =
+      R"js(
+        (() => {
+          return window.credentialDialogRequestData;
+        })();
+      )js";
+  auto eval_result = content::EvalJs(glic_contents, kGetRequestData);
+  return eval_result.ExtractDict().Clone();
 }
 
 class MockExecutionEngine : public ExecutionEngine {
@@ -197,7 +274,23 @@ class AttemptLoginToolInteractiveUiTest
     return credential_id_generator_.GenerateNextId();
   }
 
-  const SkBitmap& red_bitmap() { return red_bitmap_; }
+  // Note that the URL here is the bitmap encoded `red_bitmap_` image, as
+  // bitmap encoding is what glic vends. I visually confirmed this is the same
+  // image.
+  const GURL kRedIconDataUrl{
+      "data:image/bmp;base64,Qk0aAgAAAAAAAIoAAAB8AAAACgAAAPb///"
+      "8BACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/"
+      "yBuaVcAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEA"
+      "AAAAAAAAAAAAAAAAAAAAAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//"
+      "wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//"
+      "8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//"
+      "AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//"
+      "wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//"
+      "8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//"
+      "AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//"
+      "wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//w=="};
+
+  const gfx::Image& red_image() const { return red_image_; }
 
  protected:
   MockActorLoginService mock_login_service_;
@@ -238,65 +331,12 @@ IN_PROC_BROWSER_TEST_P(AttemptLoginToolInteractiveUiTest, MAYBE_SmokeTest) {
 
   // Toggle the glic window.
   RunTestSequence(InAnyContext(WithElement(
-      glic::test::kGlicContentsElementId, [](::ui::TrackedElement* el) mutable {
-        static constexpr char kHandleDialogRequest[] =
-            R"js(
-      (() => {
-        /** Converts a PNG (Blob) to a base64 encoded string. */
-        function blobToBase64(blob) {
-          return new Promise((resolve, reject) => {
-            const reader = new FileReader();
-            reader.onloadend = () => {
-              resolve(reader.result);
-            };
-            reader.onerror = reject;
-            reader.readAsDataURL(blob);
-          });
-        }
-
-        window.credentialDialogRequestData = new Promise(resolve => {
-          client.browser.selectCredentialDialogRequestHandler().subscribe(
-            async (request) => {
-              // Respond to the request by selecting the second credential.
-              request.onDialogClosed({
-                response: {
-                  taskId: request.taskId,
-                  selectedCredentialId: request.credentials[1].id,
-                  // 1 corresponds to UserGrantedPermissionDuration.ALWAYS_ALLOW
-                  permissionDuration: 1,
-                }
-              });
-
-              const credentialsWithIcons = await Promise.all(
-                request.credentials.map(async (cred) => {
-                  const {getIcon, ...rest} = cred;
-                  if (!getIcon) {
-                    return rest;
-                  }
-                  const blob = await getIcon();
-                  if (!blob) {
-                    return rest;
-                  }
-                  const icon = await blobToBase64(blob);
-                  return {...rest, icon};
-                })
-              );
-
-              // Resolve the promise with the request data to be verified in
-              // C++.
-              resolve({
-                taskId: request.taskId,
-                showDialog: request.showDialog,
-                credentials: credentialsWithIcons,
-              });
-            }
-          );
-        });
-      })();
-              )js";
+      glic::test::kGlicContentsElementId, [](::ui::TrackedElement* el) {
         content::WebContents* glic_contents =
             AsInstrumentedWebContents(el)->web_contents();
-        ASSERT_TRUE(content::ExecJs(glic_contents, kHandleDialogRequest));
+        SetCredentialRequestHandler(
+            glic_contents, 1,
+            webui::mojom::UserGrantedPermissionDuration::kAlwaysAllow);
       })));
 
   std::unique_ptr<ToolRequest> action = MakeAttemptLoginRequest(*active_tab());
@@ -306,23 +346,6 @@ IN_PROC_BROWSER_TEST_P(AttemptLoginToolInteractiveUiTest, MAYBE_SmokeTest) {
   // shouldn't be placed inside `RunTestSequence()`.
   ExpectOkResult(result);
 
-  // Note that the URL here is the bitmap encoded `red_bitmap()` image, as
-  // bitmap encoding is what glic vends. I visually confirmed this is the same
-  // image.
-  constexpr char kExpectedIconBase64Url[] =
-      "Qk0aAgAAAAAAAIoAAAB8AAAACgAAAPb///"
-      "8BACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/"
-      "yBuaVcAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEA"
-      "AAAAAAAAAAAAAAAAAAAAAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//"
-      "wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//"
-      "8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//"
-      "AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//"
-      "wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//"
-      "8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//"
-      "AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//"
-      "wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//w==";
-  const std::string kExpectedIconDataUrl =
-      base::StrCat({"data:image/bmp;base64,", kExpectedIconBase64Url});
   const std::string expected_request_origin =
       url::Origin::Create(url).Serialize();
   const std::string expected_display_origin = "example.com:12345";
@@ -340,7 +363,7 @@ IN_PROC_BROWSER_TEST_P(AttemptLoginToolInteractiveUiTest, MAYBE_SmokeTest) {
                           .Set("sourceSiteOrApp", url.GetWithEmptyPath().spec())
                           .Set("requestOrigin", expected_request_origin)
                           .Set("displayOrigin", expected_display_origin)
-                          .Set("icon", kExpectedIconDataUrl)
+                          .Set("icon", kRedIconDataUrl.spec())
                           .Set("type", actor_login::CredentialType::kPassword))
                   .Append(
                       base::DictValue()
@@ -349,7 +372,7 @@ IN_PROC_BROWSER_TEST_P(AttemptLoginToolInteractiveUiTest, MAYBE_SmokeTest) {
                           .Set("sourceSiteOrApp", url.GetWithEmptyPath().spec())
                           .Set("requestOrigin", expected_request_origin)
                           .Set("displayOrigin", expected_display_origin)
-                          .Set("icon", kExpectedIconDataUrl)
+                          .Set("icon", kRedIconDataUrl.spec())
                           .Set("type",
                                actor_login::CredentialType::kPassword)));
 
@@ -358,14 +381,8 @@ IN_PROC_BROWSER_TEST_P(AttemptLoginToolInteractiveUiTest, MAYBE_SmokeTest) {
       glic::test::kGlicContentsElementId, [&](::ui::TrackedElement* el) {
         content::WebContents* glic_contents =
             AsInstrumentedWebContents(el)->web_contents();
-        static constexpr char kGetRequestData[] =
-            R"js(
-              (() => {
-                return window.credentialDialogRequestData;
-              })();
-            )js";
-        auto eval_result = content::EvalJs(glic_contents, kGetRequestData);
-        const auto& actual_request = eval_result.ExtractDict();
+        const base::DictValue actual_request =
+            ExtractRequestData(glic_contents);
         ASSERT_EQ(expected_request, actual_request);
       })));
 
@@ -482,37 +499,11 @@ IN_PROC_BROWSER_TEST_P(AttemptLoginToolInteractiveUiTest,
   // Toggle the glic window.
   RunTestSequence(InAnyContext(WithElement(
       glic::test::kGlicContentsElementId, [](::ui::TrackedElement* el) {
-        static constexpr char kHandleDialogRequest[] =
-            R"js(
-      (() => {
-        window.credentialDialogRequestData = new Promise(resolve => {
-          client.browser.selectCredentialDialogRequestHandler().subscribe(
-            async (request) => {
-              // Respond to the request by selecting the first credential.
-              request.onDialogClosed({
-                response: {
-                  taskId: request.taskId,
-                  selectedCredentialId: request.credentials[0].id,
-                  // 1 corresponds to UserGrantedPermissionDuration.ALWAYS_ALLOW
-                  permissionDuration: 1,
-                }
-              });
-
-              // Resolve the promise with the request data to be verified in
-              // C++.
-              resolve({
-                taskId: request.taskId,
-                showDialog: request.showDialog,
-                credentials: request.credentials,
-              });
-            }
-          );
-        });
-      })();
-              )js";
         content::WebContents* glic_contents =
             AsInstrumentedWebContents(el)->web_contents();
-        ASSERT_TRUE(content::ExecJs(glic_contents, kHandleDialogRequest));
+        SetCredentialRequestHandler(
+            glic_contents, 0,
+            webui::mojom::UserGrantedPermissionDuration::kAlwaysAllow);
       })));
 
   std::unique_ptr<ToolRequest> action = MakeAttemptLoginRequest(*active_tab());
@@ -536,7 +527,7 @@ IN_PROC_BROWSER_TEST_P(AttemptLoginToolInteractiveUiTest,
                       base::DictValue()
                           .Set("id", GenerateCredentialId().value())
                           .Set("username", "username1")
-                          .Set("sourceSiteOrApp", url.GetWithEmptyPath().spec())
+                          .Set("sourceSiteOrApp", url.host())
                           .Set("requestOrigin", expected_request_origin)
                           .Set("displayOrigin", expected_display_origin)
                           .Set("type", actor_login::CredentialType::kFederated))
@@ -544,7 +535,7 @@ IN_PROC_BROWSER_TEST_P(AttemptLoginToolInteractiveUiTest,
                       base::DictValue()
                           .Set("id", GenerateCredentialId().value())
                           .Set("username", "username2")
-                          .Set("sourceSiteOrApp", url.GetWithEmptyPath().spec())
+                          .Set("sourceSiteOrApp", url.host())
                           .Set("requestOrigin", expected_request_origin)
                           .Set("displayOrigin", expected_display_origin)
                           .Set("type", actor_login::CredentialType::kFederated))
@@ -555,6 +546,7 @@ IN_PROC_BROWSER_TEST_P(AttemptLoginToolInteractiveUiTest,
                           .Set("sourceSiteOrApp", url.GetWithEmptyPath().spec())
                           .Set("requestOrigin", expected_request_origin)
                           .Set("displayOrigin", expected_display_origin)
+                          .Set("icon", kRedIconDataUrl.spec())
                           .Set("type", actor_login::CredentialType::kPassword))
                   .Append(
                       base::DictValue()
@@ -563,6 +555,7 @@ IN_PROC_BROWSER_TEST_P(AttemptLoginToolInteractiveUiTest,
                           .Set("sourceSiteOrApp", url.GetWithEmptyPath().spec())
                           .Set("requestOrigin", expected_request_origin)
                           .Set("displayOrigin", expected_display_origin)
+                          .Set("icon", kRedIconDataUrl.spec())
                           .Set("type",
                                actor_login::CredentialType::kPassword)));
 
@@ -571,14 +564,7 @@ IN_PROC_BROWSER_TEST_P(AttemptLoginToolInteractiveUiTest,
       glic::test::kGlicContentsElementId, [&](::ui::TrackedElement* el) {
         content::WebContents* glic_contents =
             AsInstrumentedWebContents(el)->web_contents();
-        static constexpr char kGetRequestData[] =
-            R"js(
-              (() => {
-                return window.credentialDialogRequestData;
-              })();
-            )js";
-        auto eval_result = content::EvalJs(glic_contents, kGetRequestData);
-        const auto& actual_request = eval_result.ExtractDict();
+        const auto actual_request = ExtractRequestData(glic_contents);
         ASSERT_EQ(expected_request, actual_request);
       })));
 
