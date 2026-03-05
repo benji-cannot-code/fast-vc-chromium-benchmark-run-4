571ba3d54ce714beb57b878e89c1ae5cdc94d03f
Joe Downing <joedow@google.com>
2026-03-04 17:40:05 -0800

    libjingle_xmpp cleanup in /remoting
    This is the last cleanup CL as XmlElement usage is now constrained
    to a handful of signaling and conversion classes. I've also
    removed obsolete DEPS as I encountered them.
    
    Bug: 359620500
    Change-Id: I0a2c0ca633efd6756500005e9aabd429287a8f2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7634364
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594374}

--- remoting/signaling/ftl_signal_strategy.h
+++ remoting/signaling/ftl_signal_strategy.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define REMOTING_SIGNALING_FTL_SIGNAL_STRATEGY_H_
 
 #include <memory>
+#include <string>
 
 #include "base/memory/scoped_refptr.h"
 #include "remoting/signaling/signal_strategy.h"
