9977ad5abc174f1a283dc56097bcff885abff2d3
Calder Kitagawa <ckitagawa@chromium.org>
2026-03-05 04:51:19 -0800

    SelectionPopupController remove global ref
    There is a finite table of 51200 global refs (of each of weak and
    strong). This doesn't scale well to unlimited WebContents. Address this
    by fetching the java object off the WebContents each time.
    
    Bug: 484619649
    Change-Id: I3a46c8592a1624ce6c9a7bcf9011bce076dfbbe8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7633824
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594597}

--- content/public/android/java/src/org/chromium/content/browser/selection/SelectionPopupControllerImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/selection/SelectionPopupControllerImpl.java
@@ -39,6 +39,7 @@ import androidx.annotation.VisibleForTesting;
 
 import org.jni_zero.CalledByNative;
 import org.jni_zero.JNINamespace;
+import org.jni_zero.JniType;
 import org.jni_zero.NativeMethods;
 
 import org.chromium.base.ApiCompatibilityUtils;
@@ -267,7 +268,8 @@ public class SelectionPopupControllerImpl extends ActionModeCallbackHelper
      *     #create()} is not called yet.
      */
     @CalledByNative
-    public static @Nullable SelectionPopupControllerImpl fromWebContents(WebContents webContents) {
+    public static @Nullable SelectionPopupControllerImpl fromWebContents(
+            @JniType("content::WebContents*") WebContents webContents) {
         return webContents.getOrSetUserData(
                 SelectionPopupControllerImpl.class, UserDataFactoryLazyHolder.INSTANCE);
     }
@@ -279,6 +281,7 @@ public class SelectionPopupControllerImpl extends ActionModeCallbackHelper
      * @param webContents {@link WebContents} object.
      * @return {@link SelectionPopupController} object. {@code null} if not available.
      */
+    @CalledByNative
     public static @Nullable SelectionPopupControllerImpl fromWebContentsNoCreate(
             WebContents webContents) {
         return webContents.getOrSetUserData(SelectionPopupControllerImpl.class, null);
@@ -359,7 +362,7 @@ public class SelectionPopupControllerImpl extends ActionModeCallbackHelper
         }
         if (initializeNative) {
             mNativeSelectionPopupController =
-                    SelectionPopupControllerImplJni.get().init(this, mWebContents);
+                    SelectionPopupControllerImplJni.get().init(mWebContents);
             ImeAdapterImpl imeAdapter = ImeAdapterImpl.fromWebContents(mWebContents);
             if (imeAdapter != null) imeAdapter.addEventObserver(this);
         }
@@ -1977,7 +1980,7 @@ public class SelectionPopupControllerImpl extends ActionModeCallbackHelper
     interface Natives {
         boolean isMagnifierWithSurfaceControlSupported();
 
-        long init(SelectionPopupControllerImpl self, WebContents webContents);
+        long init(@JniType("content::WebContents*") WebContents webContents);
 
         void setTextHandlesTemporarilyHidden(long nativeSelectionPopupController, boolean hidden);
 
