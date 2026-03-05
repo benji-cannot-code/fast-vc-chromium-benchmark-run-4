eafa536c385830695187fb770986d53f160c7210
Alex Kallam <aakallam@chromium.org>
2026-03-05 07:36:23 -0800

    Roll pxfm: 0.1.27 => 0.1.28 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * pxfm: 0.1.27 => 0.1.28; https://docs.rs/crate/pxfm/0.1.28
    
    Bug: None
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Change-Id: I60e595c84918bbc49f44529ebed1c68b88eb8ff2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7624078
    Reviewed-by: Julia Hansbrough <flowerhack@google.com>
    Commit-Queue: Alex Kallam <aakallam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594674}

--- third_party/rust/chromium_crates_io/vendor/pxfm-v0_1/src/common.rs
+++ third_party/rust/chromium_crates_io/vendor/pxfm-v0_1/src/common.rs
@@ -27,8 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-use num_traits::MulAdd;
-use std::ops::{Add, Mul};
 
 #[inline(always)]
 pub(crate) fn is_integerf(x: f32) -> bool {
@@ -149,38 +147,6 @@ pub(crate) fn is_odd_integer(x: f64) -> bool {
         const UNIT_EXPONENT: u64 = E_BIAS + 52;
         x_e + lsb as u64 == UNIT_EXPONENT
     }
-}
-
-#[cfg(any(
-    all(
-        any(target_arch = "x86", target_arch = "x86_64"),
-        target_feature = "fma"
-    ),
-    target_arch = "aarch64"
-))]
-#[inline(always)]
-pub(crate) fn mlaf<T: Copy + Mul<T, Output = T> + Add<T, Output = T> + MulAdd<T, Output = T>>(
-    acc: T,
-    a: T,
-    b: T,
-) -> T {
-    MulAdd::mul_add(a, b, acc)
-}
-
-#[inline(always)]
-#[cfg(not(any(
-    all(
-        any(target_arch = "x86", target_arch = "x86_64"),
-        target_feature = "fma"
-    ),
-    target_arch = "aarch64"
-)))]
-pub(crate) fn mlaf<T: Copy + Mul<T, Output = T> + Add<T, Output = T> + MulAdd<T, Output = T>>(
-    acc: T,
-    a: T,
-    b: T,
-) -> T {
-    acc + a * b
 }
 
 #[inline]
@@ -329,16 +295,6 @@ pub(crate) fn dd_fmlaf(a: f32, b: f32, c: f32) -> f32 {
     {
         (a as f64 * b as f64 + c as f64) as f32
     }
-}
-
-#[allow(dead_code)]
-#[inline(always)]
-pub(crate) fn c_mlaf<T: Copy + Mul<T, Output = T> + Add<T, Output = T> + MulAdd<T, Output = T>>(
-    a: T,
-    b: T,
-    c: T,
-) -> T {
-    mlaf(c, a, b)
 }
 
 /// Copies sign from `y` to `x`
