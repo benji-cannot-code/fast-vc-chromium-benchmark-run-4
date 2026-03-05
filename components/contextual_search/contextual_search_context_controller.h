b0e4fe806409e2bd69e35bbf97521e38597fd86e
Ahmed Moussa <ahmedmoussa@google.com>
2026-03-05 14:21:34 -0800

    Revert "[contextual tasks] delay url creation"
    This reverts commit a6446fd82a18a6b3ba9660d98409832f9b133a7d.
    
    Reason for revert: Causing test failure on Linux UBSan https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20UBSan%20Tests/14237/overview
    
    Original change's description:
    > [contextual tasks] delay url creation
    >
    > * If files are uploading, delay file creation
    > * Add trackers for uploading files uploading in delete context, add
    >   context, clear all context, file status change
    > * Handled second uploadStarted as a failure since that is how
    >   multimodal filter does it (uploadStarted should only happen once)
    > * Add related tests
    >
    > lens demo - https://screenshot.googleplex.com/7SYsbPuaDVpYwiw
    >
    > https: //screencast.googleplex.com/cast/NDY1MTkxNzE1MzE0MDczNnxjYmIxZTU2Yi01Yw
    > https: //screencast.googleplex.com/cast/NjUzNzU3MzM1MzY1MjIyNHw4NjliYjk2Yi04YQ
    > Bug: b:477608944, b:484103627
    > Change-Id: Ie9ac9e1ffe2b084765473ee9b7fc1218e4c09ec9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7591854
    > Commit-Queue: James Leung <jamesleung@google.com>
    > Reviewed-by: Jason Hu <hujasonx@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1594854}
    
    Bug: b:477608944, b:484103627
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ibfe12eadb7e4180326c34937a80c2f594aa9f522
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7640318
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Ahmed Moussa <ahmedmoussa@google.com>
    Commit-Queue: Ahmed Moussa <ahmedmoussa@google.com>
    Cr-Commit-Position: refs/heads/main@{#1594964}

--- components/contextual_search/contextual_search_context_controller.h
+++ components/contextual_search/contextual_search_context_controller.h
@@ -81,6 +81,7 @@ class ContextualSearchContextController {
         lens::MimeType mime_type,
         FileUploadStatus file_upload_status,
         const std::optional<FileUploadErrorType>& error_type) = 0;
+
    protected:
     ~FileUploadStatusObserver() override = default;
   };
