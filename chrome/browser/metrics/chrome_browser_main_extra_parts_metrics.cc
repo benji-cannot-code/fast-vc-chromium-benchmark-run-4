1a55783eafda656deb8684d91443865f15162093
hikalium <hikalium@chromium.org>
2026-03-04 20:58:48 -0800

    Add CPU microcode version metric
    This change adds a new sparse histogram, Platform.MicrocodeVersion, to
    track the CPU microcode version. For now, this change primarily aims to
    collect deployed microcode versions on ChromiumOS-based systems so the
    collection logic in this CL only supports x86 Linux platforms.
    
    The microcode version is logged as an integer. If it is inaccessible,
    the value cannot be parsed, or on unsupported platforms, a value of -1
    is logged. The metric is recorded during the browser start-up for all
    platforms.
    
    BUG=b:414578452
    TEST=Opened chrome://histograms and verified the value reported.
    
    Change-Id: I82a1526c24130e6faa1a8659697c2da4fd4076be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7604002
    Commit-Queue: Masa Fujita <massan@google.com>
    Auto-Submit: Hikaru Nishida <hikalium@chromium.org>
    Commit-Queue: Hikaru Nishida <hikalium@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Masa Fujita <massan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1594465}

--- chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.cc
+++ chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.cc
@@ -87,6 +87,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <gnu/libc-version.h>
 #endif  // defined(__GLIBC__)
 
+#include "base/files/file_util.h"
 #include "base/linux_util.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
@@ -112,6 +113,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // BUILDFLAG(IS_LINUX)
 
 #if BUILDFLAG(IS_CHROMEOS)
+#include "base/files/file_util.h"
+#include "base/strings/string_util.h"
 #include "chromeos/ash/components/browser_context_helper/browser_context_helper.h"
 #include "components/user_manager/user_manager.h"
 #endif  // BUILDFLAG(IS_CHROMEOS)
@@ -378,6 +381,37 @@ void RecordMicroArchitectureStats() {
                                 base::CPU::MAX_INTEL_MICRO_ARCHITECTURE);
 #endif  // defined(ARCH_CPU_X86_FAMILY)
 }
+
+#if defined(ARCH_CPU_X86_FAMILY) && \
+    (BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS))
+// Reads the microcode version from the kernel via sysfs.
+// This function returns -1 on failure.
+int GetMicrocodeVersion() {
+  constexpr base::FilePath::CharType kMicrocodeVersionPath[] =
+      FILE_PATH_LITERAL("/sys/bus/cpu/devices/cpu0/microcode/version");
+  std::string version_string;
+  if (!base::ReadFileToString(base::FilePath(kMicrocodeVersionPath),
+                              &version_string)) {
+    return -1;
+  }
+
+  base::TrimWhitespaceASCII(version_string, base::TRIM_ALL, &version_string);
+  uint32_t version = 0;
+  if (!base::HexStringToUInt(version_string, &version)) {
+    return -1;
+  }
+  return static_cast<int>(version);
+}
+// As of 2026, microcode updates are typically only implemented on x86 CPUs.
+// However, this is not guaranteed to be the case in the future, so we abstract
+// the CPU-specific details away and record the results in a CPU-agnostic
+// histogram.
+// This function is called on a background thread, with low priority to avoid
+// slowing down the startup by accessing the filesystem.
+void RecordMicrocodeVersionStats() {
+  base::UmaHistogramSparse("Platform.MicrocodeVersion", GetMicrocodeVersion());
+}
+#endif
 
 #if BUILDFLAG(IS_LINUX)
 void RecordLinuxDistroSpecific(const std::string& version_string,
@@ -1081,6 +1115,12 @@ void ChromeBrowserMainExtraPartsMetrics::PostBrowserStart() {
         base::Seconds(45));
   }
 #endif  // BUILDFLAG(IS_WIN)
+
+#if defined(ARCH_CPU_X86_FAMILY) && \
+    (BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS))
+  base::ThreadPool::PostTask(FROM_HERE, kBestEffortTaskTraits,
+                             base::BindOnce(&RecordMicrocodeVersionStats));
+#endif
 
   auto* screen = display::Screen::Get();
   display_count_ = screen->GetNumDisplays();
