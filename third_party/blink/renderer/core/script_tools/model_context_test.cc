369604c17a79659cbb272f9fdd8279c7ffa6db30
Mason Freed <masonf@chromium.org>
2026-03-05 13:56:52 -0800

    Fix crash in declarative WebMCP file input
    Previously, a declarative WebMCP tool with a file input would crash if
    the provided path had not been granted to the renderer, typically by a
    user choosing that file in a picker. This CL fixes the crash by adding
    read-access verification in both FormMCPSchema::ValidateFileData and
    FormMCPSchema::FillFileData. If access has not been granted, the tool
    invocation is rejected as having invalid arguments. Note that this CL
    does not add code to the browser to grant access to files, it merely
    stops crashes when files are passed as inputs that have not been
    granted.
    
    Bug: 488313885
    Change-Id: I5d58d01b4b51304f8ce49cbb7c2c6967c5b27fa5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7634763
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594937}

--- third_party/blink/renderer/core/script_tools/model_context_test.cc
+++ third_party/blink/renderer/core/script_tools/model_context_test.cc
@@ -68,11 +68,10 @@ class ModelContextTest : public SimTest {
   }
 
   String EvalJsString(const char* script) {
-    return ToCoreString(Window().GetIsolate(),
-                        MainFrame()
-                            .ExecuteScriptAndReturnValue(
-                                WebScriptSource(WebString::FromUTF8(script)))
-                            .As<v8::String>());
+    return ToCoreStringWithUndefinedOrNullCheck(
+        Window().GetIsolate(),
+        MainFrame().ExecuteScriptAndReturnValue(
+            WebScriptSource(WebString::FromUTF8(script))));
   }
 
  private:
