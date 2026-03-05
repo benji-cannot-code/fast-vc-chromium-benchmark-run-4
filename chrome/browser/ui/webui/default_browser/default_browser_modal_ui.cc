3b2ca12f7fc1aa31029ba02cc14f255e36c12ade
Kaan Alsan <alsan@chromium.org>
2026-03-05 09:42:47 -0800

    Add body text variants for taskbar pinning to default browser modals
    Updates the default browser modal bodies to have variants that
    mention/don't mention pinning to the taskbar.
    
    Bug: 489793466
    Change-Id: Ic4fc4da870910502aa5ffefcf4858d2e9e97e852
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7637137
    Reviewed-by: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594766}

--- chrome/browser/ui/webui/default_browser/default_browser_modal_ui.cc
+++ chrome/browser/ui/webui/default_browser/default_browser_modal_ui.cc
@@ -25,10 +25,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 DefaultBrowserModalUI::DefaultBrowserModalUI(content::WebUI* web_ui)
     : TopChromeWebUIController(web_ui, /*enable_chrome_send=*/false) {
   bool use_settings_illustration = false;
+  bool can_pin_to_taskbar = false;
   std::string value;
   if (net::GetValueForKeyInQuery(web_ui->GetWebContents()->GetVisibleURL(),
                                  "illustration", &value)) {
     use_settings_illustration = value == "true";
+  }
+  if (net::GetValueForKeyInQuery(web_ui->GetWebContents()->GetVisibleURL(),
+                                 "can_pin_to_taskbar", &value)) {
+    can_pin_to_taskbar = value == "true";
   }
 
   content::WebUIDataSource* source = content::WebUIDataSource::CreateAndAdd(
@@ -53,7 +58,11 @@ DefaultBrowserModalUI::DefaultBrowserModalUI(content::WebUI* web_ui)
   source->AddLocalizedString(
       "bodyText",
       use_settings_illustration
-          ? IDS_DEFAULT_BROWSER_MODAL_BODY_WITH_SETTINGS_ILLUSTRATION
+          ? can_pin_to_taskbar
+                ? IDS_DEFAULT_BROWSER_MODAL_BODY_WITH_SETTINGS_ILLUSTRATION_UNPINNED
+                : IDS_DEFAULT_BROWSER_MODAL_BODY_WITH_SETTINGS_ILLUSTRATION
+      : can_pin_to_taskbar
+          ? IDS_DEFAULT_BROWSER_MODAL_BODY_WITHOUT_SETTINGS_ILLUSTRATION_UNPINNED
           : IDS_DEFAULT_BROWSER_MODAL_BODY_WITHOUT_SETTINGS_ILLUSTRATION);
 
   source->AddResourcePath("chrome_logo.svg", IDR_PRODUCT_LOGO_SVG);
