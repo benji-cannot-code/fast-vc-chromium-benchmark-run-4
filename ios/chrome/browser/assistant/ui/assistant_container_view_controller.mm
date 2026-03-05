e803dd0fe64d04b83a5aeb960edba3b52e45d0c1
Ewann Pelle <ewannpv@chromium.org>
2026-03-05 07:42:45 -0800

    [iOS][Assistant] Add continuous percentage delegate methods
    This CL refactors the AssistantContainerDelegate to expose precise,
    animatable "expand percentage" data to embedders.
    
    Key changes:
    - Replaces the granular `didUpdateHeight:` method with
      `didUpdateExpandPercentage:`.
    - Adds `animateAlongsideTransitionToPercentage:` inside the
      delegate, exposing the internal UI animation block wrapper.
    
    Demo:
    https://drive.google.com/file/d/1UHkOQ3l8uBKh11TqNFyb0WT4ZNoAPqeC/view?usp=drive_link&resourcekey=0-s5RAc_HoPxXdOo-j5mR5cw
    
    Bug: 469050167
    Change-Id: Ie61a6dcbf3c64e784a4498719e5146eaf8b4aa8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7637229
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594680}

--- ios/chrome/browser/assistant/ui/assistant_container_view_controller.mm
+++ ios/chrome/browser/assistant/ui/assistant_container_view_controller.mm
@@ -129,6 +129,7 @@ constexpr CGFloat kGestureTopAreaHeight = 44.0;
       std::clamp(matchedDetent.value, minHeight, maxHeight);
 
   _heightConstraint.constant = targetHeight;
+  CGFloat targetPercentage = [self expandPercentageForHeight:targetHeight];
 
   // The shift converts an animation curve to animation options.
   // `UIViewAnimationOptionBeginFromCurrentState` ensures that if an animation
@@ -137,20 +138,20 @@ constexpr CGFloat kGestureTopAreaHeight = 44.0;
   UIViewAnimationOptions options =
       curve << 16 | UIViewAnimationOptionBeginFromCurrentState;
 
+  self.isAnimating = YES;
+
   if (duration <= 0) {
-    self.isAnimating = YES;
-    [self.view.superview layoutIfNeeded];
+    [self executeAlongsideAnimationWithPercentage:targetPercentage];
     [self didCompleteDetentAnimationWithDetent:matchedDetent];
     return;
   }
 
-  self.isAnimating = YES;
   __weak __typeof(self) weakSelf = self;
   [UIView animateWithDuration:duration
       delay:0
       options:options
       animations:^{
-        [weakSelf.view.superview layoutIfNeeded];
+        [weakSelf executeAlongsideAnimationWithPercentage:targetPercentage];
       }
       completion:^(BOOL finished) {
         [weakSelf didCompleteDetentAnimationWithDetent:matchedDetent];
@@ -228,6 +229,18 @@ constexpr CGFloat kGestureTopAreaHeight = 44.0;
   }
 }
 
+// Executes the layout pass and notifies the delegate of the transition.
+- (void)executeAlongsideAnimationWithPercentage:(CGFloat)percentage {
+  [self.view.superview layoutIfNeeded];
+
+  if ([self.delegate
+          respondsToSelector:@selector(assistantContainer:
+                                 animateAlongsideTransitionToPercentage:)]) {
+    [self.delegate assistantContainer:self
+        animateAlongsideTransitionToPercentage:percentage];
+  }
+}
+
 // Updates the pan gesture enabled state based on animation and detents.
 - (void)updatePanGestureEnabledState {
   // Prevent the gesture recognizer from interfering with the animation.
@@ -292,6 +305,16 @@ constexpr CGFloat kGestureTopAreaHeight = 44.0;
   return MIN(self.detents.lastObject.value, absoluteMax);
 }
 
+// Converts a physical pixel height mathematically into an expansion percentage.
+- (CGFloat)expandPercentageForHeight:(CGFloat)height {
+  CGFloat minHeight = [self effectiveMinHeight];
+  CGFloat maxHeight = [self effectiveMaxHeight];
+  if (maxHeight <= minHeight) {
+    return 0.0;
+  }
+  return (height - minHeight) / (maxHeight - minHeight);
+}
+
 // Handles the state when the pan gesture changes (drags).
 - (void)handlePanGestureChanged:(UIPanGestureRecognizer*)gesture {
   CHECK(gesture == _headerPanGesture);
@@ -320,9 +343,12 @@ constexpr CGFloat kGestureTopAreaHeight = 44.0;
   }
 
   _heightConstraint.constant = newHeight;
+
+  CGFloat percentage = [self expandPercentageForHeight:newHeight];
   if ([self.delegate respondsToSelector:@selector(assistantContainer:
-                                                     didUpdateHeight:)]) {
-    [self.delegate assistantContainer:self didUpdateHeight:newHeight];
+                                            didUpdateExpandPercentage:)]) {
+    [self.delegate assistantContainer:self
+            didUpdateExpandPercentage:percentage];
   }
 }
 
@@ -527,6 +553,10 @@ constexpr CGFloat kGestureTopAreaHeight = 44.0;
 
 // Animates layout changes with standard spring parameters.
 - (void)animateLayoutIfNeededWithInitialVelocity:(CGFloat)velocity {
+  CGFloat targetHeight = _heightConstraint.constant;
+  CGFloat targetPercentage = [self expandPercentageForHeight:targetHeight];
+
+  __weak __typeof(self) weakSelf = self;
   [UIView animateWithDuration:kSpringDuration
                         delay:0
        usingSpringWithDamping:kSpringDamping
@@ -534,7 +564,8 @@ constexpr CGFloat kGestureTopAreaHeight = 44.0;
                       options:UIViewAnimationOptionCurveEaseOut |
                               UIViewAnimationOptionBeginFromCurrentState
                    animations:^{
-                     [self.view.superview layoutIfNeeded];
+                     [weakSelf executeAlongsideAnimationWithPercentage:
+                                   targetPercentage];
                    }
                    completion:nil];
 }
