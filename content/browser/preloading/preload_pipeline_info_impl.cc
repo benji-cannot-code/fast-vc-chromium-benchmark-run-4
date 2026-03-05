e274952522b2e77c138a8eb128de7d088b52ccee
kenoss <kenoss@chromium.org>
2026-03-05 15:51:26 -0800

    Prerender2Fallback: Remove CHECK for MarkPrerenderMatchedWithPrefetch()
    This CL removes a `CHECK` for preventing multiple calls of
    `PreloadPipelineInfoImpl::MarkPrerenderMatchedWithPrefetch()`. Actually,
    this can be called more than once; if the prerender is redirected, each
    redirect hops claim to use a prefetch.
    
    Fixed: 489269569
    Change-Id: Ie6e1eb1f0efe9d18f8ec099f0b012e21acb0c479
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7634658
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1595024}

--- content/browser/preloading/preload_pipeline_info_impl.cc
+++ content/browser/preloading/preload_pipeline_info_impl.cc
@@ -64,7 +64,6 @@ void PreloadPipelineInfoImpl::SetPrefetchStatus(
 }
 
 void PreloadPipelineInfoImpl::MarkPrerenderMatchedWithPrefetch() {
-  CHECK(!is_prerender_matched_with_prefetch_);
   is_prerender_matched_with_prefetch_ = true;
 }
 
