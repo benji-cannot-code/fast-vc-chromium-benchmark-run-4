95aa561c26ef04b310a1fc380046e8b5163f4720
Lusa Zhan <lusazhan@chromium.org>
2026-03-05 08:56:16 -0800

    Move webdx_feature_maps to blink
    As part of baseline audit, use counters for webdx features will
    need to be accurately reported through CPD events using this map.
    
    There are no plans to change existing use counter logic,
    i.e. if a WebFeature is counted, the corresponding WebDXFeature
    won't be counted by blink, only by page_load_metrics.
    
    Change-Id: I872f4e4db22c597eec03eee9831b263638eb4ced
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7600495
    Reviewed-by: Johnny Stenback <jstenback@google.com>
    Commit-Queue: Lusa Zhan <lusazhan@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594732}

--- components/page_load_metrics/browser/observers/use_counter_page_load_metrics_observer.cc
+++ components/page_load_metrics/browser/observers/use_counter_page_load_metrics_observer.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/render_frame_host.h"
 #include "services/metrics/public/cpp/ukm_builders.h"
+#include "third_party/blink/public/common/use_counter/webdx_feature_maps.h"
 #include "third_party/blink/public/common/user_agent/user_agent_metadata.h"
 #include "third_party/blink/public/mojom/use_counter/use_counter_feature.mojom.h"
 
@@ -179,7 +180,7 @@ void UseCounterMetricsRecorder::RecordOrDeferUseCounterFeature(
 
         // For any WebFeature use counters that are mapped to a WebDXFeature,
         // record the WebDXFeature use counter as well.
-        auto map = GetWebFeatureToWebDXFeatureMap();
+        auto map = blink::GetWebFeatureToWebDXFeatureMap();
         auto entry = map.find(web_feature);
 
         if (entry != map.end()) {
@@ -207,7 +208,7 @@ void UseCounterMetricsRecorder::RecordOrDeferUseCounterFeature(
         if (!uma_css_properties_->IsRecordedOrDeferred(css_property)) {
           uma_css_properties_->RecordOrDefer(css_property);
 
-          auto map = GetCSSProperties2WebDXFeatureMap();
+          auto map = blink::GetCSSPropertiesToWebDXFeatureMap();
           auto entry = map.find(css_property);
 
           if (entry != map.end() &&
@@ -225,7 +226,7 @@ void UseCounterMetricsRecorder::RecordOrDeferUseCounterFeature(
                 animated_css_property)) {
           uma_animated_css_properties_->RecordOrDefer(animated_css_property);
 
-          auto map = GetAnimatedCSSProperties2WebDXFeatureMap();
+          auto map = blink::GetAnimatedCSSPropertiesToWebDXFeatureMap();
           auto entry = map.find(animated_css_property);
 
           if (entry != map.end() &&
@@ -327,7 +328,7 @@ void UseCounterMetricsRecorder::RecordPrivacySensitiveFeatures(
 }
 
 // WebDXFeature use counter mappings have been moved to
-// components/page_load_metrics/browser/observers/use_counter/webdx_feature_maps.cc
+// third_party/blink/common/use_counter/webdx_feature_maps.cc
 UseCounterPageLoadMetricsObserver::UseCounterPageLoadMetricsObserver() =
     default;
 
