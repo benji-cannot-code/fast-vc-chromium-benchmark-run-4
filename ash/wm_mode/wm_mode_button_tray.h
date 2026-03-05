d77ce2a04d3a4f8843f9f8ce8e302275acd03adc
Zoraiz Naeem <zoraiznaeem@chromium.org>
2026-03-05 12:05:29 -0800

    Migrate WmModeButtonTray to use ImagedTrayIcon
    This CL refactors WmModeButtonTray to inherit from the more specialized
    ImagedTrayIcon class. This removes a significant amount of boilerplate
    code that was previously needed to manually create and configure an
    ImageView.
    
    This change is a pure refactoring and does not alter the component's
    behavior.
    
    Bug: b:463431615
    Change-Id: I85919d2d06a1df2c01684327b431e8987fb32074
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7618861
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594866}

--- ash/wm_mode/wm_mode_button_tray.h
+++ ash/wm_mode/wm_mode_button_tray.h
@@ -7,22 +7,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_WM_MODE_WM_MODE_BUTTON_TRAY_H_
 
 #include "ash/public/cpp/session/session_observer.h"
-#include "ash/system/tray/tray_background_view.h"
+#include "ash/system/tray/imaged_tray_icon.h"
 #include "base/memory/raw_ptr.h"
 #include "ui/base/metadata/metadata_header_macros.h"
-
-namespace views {
-class ImageView;
-}
 
 namespace ash {
 
 class Shelf;
-class TrayBubbleView;
 
 // Defines a shelf tray button that is used to toggle WM Mode on and off.
-class WmModeButtonTray : public TrayBackgroundView, public SessionObserver {
-  METADATA_HEADER(WmModeButtonTray, TrayBackgroundView)
+class WmModeButtonTray : public ImagedTrayIcon, public SessionObserver {
+  METADATA_HEADER(WmModeButtonTray, ImagedTrayIcon)
 
  public:
   explicit WmModeButtonTray(Shelf* shelf);
@@ -34,29 +29,20 @@ class WmModeButtonTray : public TrayBackgroundView, public SessionObserver {
   // based on the given `is_wm_mode_active`.
   void UpdateButtonVisuals(bool is_wm_mode_active);
 
-  // TrayBackgroundView:
+  // ImagedTrayIcon:
   void OnThemeChanged() override;
   void UpdateAfterLoginStatusChange() override;
-  void HandleLocaleChange() override {}
-  void HideBubbleWithView(const TrayBubbleView* bubble_view) override {}
-  void ClickedOutsideBubble(const ui::LocatedEvent& event) override {}
   // No need to override since the icon and activation state of this tray will
   // change and get updated simultaneously in `UpdateButtonVisuals()`.
   void UpdateTrayItemColor(bool is_active) override {}
-  void HideBubble(const TrayBubbleView* bubble_view) override {}
 
   // SessionObserver:
   void OnSessionStateChanged(session_manager::SessionState state) override;
-
-  views::ImageView* GetImageViewForTesting() { return image_view_; }
 
  private:
   // Updates the visibility of this tray button based on the current state of
   // the user session (i.e. whether it's blocked or not).
   void UpdateButtonVisibility();
-
-  // The view that hosts the button icon.
-  const raw_ptr<views::ImageView> image_view_;
 };
 
 }  // namespace ash
