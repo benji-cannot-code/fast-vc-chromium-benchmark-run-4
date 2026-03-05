b4dc9f634019d3a61f0a1f691538b7a1e6731639
moonira <moonira@google.com>
2026-03-05 07:47:39 -0800

    Support random() property sharing in custom property animations
    Bug: 413385732
    Change-Id: I49f35b864788a5c2435900ee9768b2e056f772f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7638231
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Munira Tursunova <moonira@google.com>
    Cr-Commit-Position: refs/heads/main@{#1594683}

--- third_party/blink/renderer/core/animation/interpolable_value_test.cc
+++ third_party/blink/renderer/core/animation/interpolable_value_test.cc
@@ -197,7 +197,7 @@ TEST_F(AnimationInterpolableValueTest, InterpolableNumberAsExpression) {
     CSSParserTokenStream stream(test_case.input);
 
     CSSParserLocalContext local_context =
-        CSSParserLocalContext::CreateWithoutPropertyForAnimations();
+        CSSParserLocalContext::CreateWithoutPropertyForTest();
     // Test expression evaluation.
     const CSSMathExpressionNode* expression =
         CSSMathExpressionNode::ParseMathFunction(
