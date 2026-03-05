9679b1eb9df7b98e8b09a583475ebbf022a9c92c
xujunwei <xujw89@gmail.com>
2026-03-04 18:41:31 -0800

    Fix bugprone-use-after-move in MakeCredentialRequestHandler constructor
    The constructor of MakeCredentialRequestHandler moves the `request`
    parameter into the `request_` member variable in the initializer list.
    However, it subsequently accesses fields of the moved-from `request`
    parameter (e.g., `attestation_preference`) in the constructor body.
    
    While `CtapMakeCredentialRequest` currently uses a default move
    constructor that likely leaves scalar fields (like enums) intact,
    accessing a moved-from object is undefined behavior and fragile
    
    Bug: 489619748
    Change-Id: Iae679fb3c25357080e204c151c0cfb1bb7480028
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7632016
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Hewro Hewei <ihewro@chromium.org>
    Commit-Queue: Hewro Hewei <ihewro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594411}

--- device/fido/make_credential_request_handler.cc
+++ device/fido/make_credential_request_handler.cc
@@ -366,7 +366,7 @@ MakeCredentialRequestHandler::MakeCredentialRequestHandler(
   transport_availability_info().resident_key_requirement =
       options_.resident_key;
   transport_availability_info().attestation_conveyance_preference =
-      request.attestation_preference;
+      request_.attestation_preference;
   transport_availability_info().user_verification_requirement =
       request_.user_verification;
   transport_availability_info().request_is_internal_only =
@@ -392,7 +392,7 @@ MakeCredentialRequestHandler::MakeCredentialRequestHandler(
   auto available_transports =
       base::STLSetIntersection<base::flat_set<FidoTransportProtocol>>(
           supported_transports, allowed_transports);
-  bool consider_enclave = request.authenticator_attachment !=
+  bool consider_enclave = request_.authenticator_attachment !=
                           AuthenticatorAttachment::kCrossPlatform;
   if (options_.is_passkey_upgrade_request) {
     consider_enclave = true;
