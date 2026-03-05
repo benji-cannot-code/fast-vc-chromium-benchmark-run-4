867823d97bcaba0d0508ad8de68acb8c7538765f
yoshiki iguchi <yoshiki@google.com>
2026-03-04 23:11:40 -0800

    Move kiosk and app launch splash screen strings to ash/ash_strings.grd
    This CL skips Translation-Screenshots-Check, for just moving the
    strings without changing the names/contents
    
    Bug: 487122295
    Test: built chromeos-chrome & browser_tests
    Test: ran `gn check`
    Skip-Translation-Screenshots-Check: True
    Change-Id: Icd22a8dc685fc9f813baf2ae3fa10bc65a1c995f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7625939
    Reviewed-by: Jun Ishiguro <junis@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Yoshiki Iguchi <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594489}

--- chrome/browser/ash/app_mode/test/kiosk_chrome_app_offline_network_prompt_browsertest.cc
+++ chrome/browser/ash/app_mode/test/kiosk_chrome_app_offline_network_prompt_browsertest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "ash/strings/grit/ash_strings.h"
 #include "base/check_deref.h"
 #include "base/check_op.h"
 #include "base/functional/bind.h"
@@ -20,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/test/test_predicate_waiter.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_window.h"
-#include "chrome/grit/generated_resources.h"
 #include "chrome/test/base/mixin_based_in_process_browser_test.h"
 #include "content/public/test/browser_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
