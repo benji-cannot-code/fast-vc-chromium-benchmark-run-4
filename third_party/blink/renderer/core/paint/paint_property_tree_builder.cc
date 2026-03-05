d70d58671cf7e667246a420232b56eb2761bb63f
Stephen Chenney <schenney@chromium.org>
2026-03-05 15:59:45 -0800

    Fix printing of elements with backdrop filter.
    A recent change to enable backdrop filter when not compositing
    broke the printing of elements with a backdrop filter. The
    underlying reason is a lack of backdrop filter support in
    printing media (not at all surprising), so enabling it caused
    printing to fail.
    
    Address the issue by not allowing backdrop filter when printing.
    
    Bug: 489633238
    Change-Id: I921295d98e16b974b13a9e9b804725530bfd35a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7639130
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Auto-Submit: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1595026}

--- third_party/blink/renderer/core/paint/paint_property_tree_builder.cc
+++ third_party/blink/renderer/core/paint/paint_property_tree_builder.cc
@@ -3891,6 +3891,10 @@ void FragmentPaintPropertyTreeBuilder::UpdateLayoutShiftRootChanged(
 void FragmentPaintPropertyTreeBuilder::PopulateBackdropFilterIfNeeded(
     EffectPaintPropertyNode::State& state,
     CompositorElementId mask_compositor_element_id) const {
+  // Backdrop filter is not supported in printing.
+  if (object_.GetDocument().Printing()) {
+    return;
+  }
   CompositorFilterOperations operations;
   SkPath bounds;
   if (object_.IsBoxModelObject()) {
