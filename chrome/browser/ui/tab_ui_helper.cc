5c059093ab5fd9cd11f069334b4debaf24e23fcf
Steven Luong <stluong@chromium.org>
2026-03-05 11:36:36 -0800

    [TabRendererData] Add missing properties to TabData
    The TabData serves as a replacement to the TabRenderer but is missing
    a few properties. This CL adds the missing properties so they are
    functionally equivalent.
    
    Bug: 447214891
    Change-Id: I2f1eb45aefda40c520826e3636f52e1a2913fbbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7635978
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Steven Luong <stluong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594845}

--- chrome/browser/ui/tab_ui_helper.cc
+++ chrome/browser/ui/tab_ui_helper.cc
@@ -269,6 +269,10 @@ GURL TabUIHelper::GetVisibleURL() {
                                   : contents->GetVisibleURL();
 }
 
+GURL TabUIHelper::GetLastCommittedURL() {
+  return tab().GetContents()->GetLastCommittedURL();
+}
+
 void TabUIHelper::TitleWasSet(content::NavigationEntry* entry) {
   tab_ui_change_callbacks_.Notify();
 }
@@ -334,6 +338,10 @@ void TabUIHelper::SetNeedsAttention(bool needs_attention) {
 
   needs_attention_ = needs_attention;
   tab_ui_change_callbacks_.Notify();
+}
+
+bool TabUIHelper::IsDiscarded() {
+  return tab().GetContents()->WasDiscarded();
 }
 
 bool TabUIHelper::ShouldShowDiscardStatus() {
