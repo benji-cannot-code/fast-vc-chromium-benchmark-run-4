1af58eda3e592fc3f420ef7d79d4c189d7d9d5da
Muhammad Salmaan <musalmaan@chromium.org>
2026-03-05 09:27:18 -0800

    Added IWYU for infobar files
    Added IWYU for all infobar files.
    
    Bug: none
    Change-Id: Iabbdb3c51680b9133fe9e0306f52925c9f536a39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7639230
    Commit-Queue: Muhammad Salmaan <musalmaan@chromium.org>
    Reviewed-by: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594759}

--- chrome/browser/ui/views/infobars/confirm_infobar.cc
+++ chrome/browser/ui/views/infobars/confirm_infobar.cc
@@ -5,9 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/infobars/confirm_infobar.h"
 
+#include <algorithm>
 #include <memory>
 #include <optional>
 #include <utility>
+#include <vector>
 
 #include "base/functional/bind.h"
 #include "build/build_config.h"
--- chrome/browser/ui/views/infobars/infobar_container_view.cc
+++ chrome/browser/ui/views/infobars/infobar_container_view.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/infobars/infobar_container_view.h"
 
+#include <algorithm>
 #include <numeric>
 
 #include "cc/paint/paint_flags.h"
