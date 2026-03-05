20b3f97ae9dcda25a3ffa0c01f2955f204a0a746
Anis Chara <achara@google.com>
2026-03-05 03:49:40 -0800

    [iOS] Apply color to nonAvatarButton when group shared
    This CL applies the same color as the close and menu buttons to the
    nonAvatarButton in the tabGroupView when the group is shared.
    
    Bug: 445887468
    Change-Id: I0242f90d68338298e0f548c4fc9300c21ce08909
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7632178
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Anis Chara <achara@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594581}

--- ios/chrome/browser/saved_tab_groups/ui/face_pile_view.mm
+++ ios/chrome/browser/saved_tab_groups/ui/face_pile_view.mm
@@ -77,8 +77,8 @@ UIBackgroundConfiguration* BackgroundConfiguration() {
   CGFloat _membersCount;
   // View to display when the face pile is empty.
   UIButton* _shareViewContainer;
-  // The background of the non-avatar.
-  UIView* _nonAvatarBackground;
+  // The button used as a background for the non-avatar element.
+  UIButton* _nonAvatarBackgroundButton;
 }
 
 - (instancetype)init {
@@ -162,6 +162,11 @@ UIBackgroundConfiguration* BackgroundConfiguration() {
       _shareViewContainer.configuration;
   shareViewContainerConfig.background.backgroundColor = backgroundColor;
   _shareViewContainer.configuration = shareViewContainerConfig;
+
+  UIButtonConfiguration* nonAvatarBackgroundConfig =
+      _nonAvatarBackgroundButton.configuration;
+  nonAvatarBackgroundConfig.background.backgroundColor = backgroundColor;
+  _nonAvatarBackgroundButton.configuration = nonAvatarBackgroundConfig;
 }
 
 #pragma mark - Private
@@ -169,7 +174,7 @@ UIBackgroundConfiguration* BackgroundConfiguration() {
 // Updates colors after UITrait collection update.
 - (void)updateColors {
   if (_facePileBackgroundColor) {
-    [_nonAvatarBackground removeFromSuperview];
+    [_nonAvatarBackgroundButton removeFromSuperview];
     _nonAvatarContainer.tintColor = [UIColor colorNamed:kSolidWhiteColor];
     _plusXLabel.textColor = [UIColor colorNamed:kSolidWhiteColor];
     _nonAvatarContainer.backgroundColor = [[UIColor colorNamed:kSolidBlackColor]
@@ -231,7 +236,7 @@ UIBackgroundConfiguration* BackgroundConfiguration() {
       setContentCompressionResistancePriority:UILayoutPriorityRequired
                                       forAxis:UILayoutConstraintAxisHorizontal];
 
-  _nonAvatarBackground = [self createNonAvatarBackground];
+  _nonAvatarBackgroundButton = [self createNonAvatarBackgroundButton];
 
   // Configure a container in order to add an inner horizontal margin around the
   // label.
@@ -239,10 +244,10 @@ UIBackgroundConfiguration* BackgroundConfiguration() {
   plusXLabelContainer.translatesAutoresizingMaskIntoConstraints = NO;
   plusXLabelContainer.layer.cornerRadius = _avatarSize / 2.0;
   plusXLabelContainer.layer.masksToBounds = YES;
-  [plusXLabelContainer addSubview:_nonAvatarBackground];
+  [plusXLabelContainer addSubview:_nonAvatarBackgroundButton];
   [plusXLabelContainer addSubview:plusXLabel];
 
-  AddSameConstraints(plusXLabelContainer, _nonAvatarBackground);
+  AddSameConstraints(plusXLabelContainer, _nonAvatarBackgroundButton);
 
   [NSLayoutConstraint activateConstraints:@[
     [plusXLabel.leadingAnchor
@@ -261,15 +266,15 @@ UIBackgroundConfiguration* BackgroundConfiguration() {
   return _nonAvatarContainer;
 }
 
-// Creates the background view for the non-avatar.
-- (UIView*)createNonAvatarBackground {
+// Creates the background button for the non-avatar.
+- (UIButton*)createNonAvatarBackgroundButton {
   // Use a button to ensure to have the same configuration as the others.
   UIButtonConfiguration* configuration =
       [UIButtonConfiguration plainButtonConfiguration];
   configuration.background = BackgroundConfiguration();
 
-  UIView* background = [UIButton buttonWithConfiguration:configuration
-                                           primaryAction:nil];
+  UIButton* background = [UIButton buttonWithConfiguration:configuration
+                                             primaryAction:nil];
   background.userInteractionEnabled = NO;
 
   background.translatesAutoresizingMaskIntoConstraints = NO;
@@ -379,7 +384,7 @@ UIBackgroundConfiguration* BackgroundConfiguration() {
       [containerView.heightAnchor constraintEqualToConstant:containerSize]
     ]];
 
-    _nonAvatarBackground = [self createNonAvatarBackground];
+    _nonAvatarBackgroundButton = [self createNonAvatarBackgroundButton];
 
     _nonAvatarContainer = [[UIView alloc] init];
     _nonAvatarContainer.translatesAutoresizingMaskIntoConstraints = NO;
@@ -390,8 +395,8 @@ UIBackgroundConfiguration* BackgroundConfiguration() {
       [_nonAvatarContainer.heightAnchor constraintEqualToConstant:_avatarSize]
     ]];
 
-    [_nonAvatarContainer addSubview:_nonAvatarBackground];
-    AddSameConstraints(_nonAvatarContainer, _nonAvatarBackground);
+    [_nonAvatarContainer addSubview:_nonAvatarBackgroundButton];
+    AddSameConstraints(_nonAvatarContainer, _nonAvatarBackgroundButton);
 
     [containerView addSubview:_nonAvatarContainer];
     AddSameCenterConstraints(_nonAvatarContainer, containerView);
