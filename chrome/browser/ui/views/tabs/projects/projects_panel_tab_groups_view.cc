31a790cfe0fd6b04ca89b92ac335661839368b4a
Chase Hartsell <chazzy@google.com>
2026-03-05 09:22:34 -0800

    [projects] Fix "Create new tab group" button icon sizing
    Screenshot: http://screen/9T3M8NMCDnygvpV
    
    Bug: 489780545
    Change-Id: I3a3f5144b6be051a89e0e2246366196810a00f82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7635963
    Reviewed-by: David Maunder <davidjm@chromium.org>
    Commit-Queue: Chase Hartsell <chazzy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594754}

--- chrome/browser/ui/views/tabs/projects/projects_panel_tab_groups_view.cc
+++ chrome/browser/ui/views/tabs/projects/projects_panel_tab_groups_view.cc
@@ -46,6 +46,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 constexpr gfx::Insets kNoTabsInteriorMargins = gfx::Insets::VH(0, 8);
+constexpr int kCreateNewTabGroupIconSize = 20;
+constexpr gfx::Insets kCreateNewTabGroupIconMargins =
+    gfx::Insets::TLBR(0, 4, 0, 0);
 
 class ProjectsPanelNewTabGroupButton : public views::Button {
   METADATA_HEADER(ProjectsPanelNewTabGroupButton, views::Button)
@@ -59,10 +62,10 @@ class ProjectsPanelNewTabGroupButton : public views::Button {
         .SetCrossAxisAlignment(views::LayoutAlignment::kCenter);
 
     auto* icon = AddChildView(std::make_unique<views::ImageView>());
-    icon->SetProperty(views::kMarginsKey, projects_panel::kTabGroupIconMargins);
-    icon->SetImage(ui::ImageModel::FromVectorIcon(
-        kCreateNewTabGroupIcon, kColorProjectsPanelButtonIcon,
-        projects_panel::kTabGroupIconSize));
+    icon->SetProperty(views::kMarginsKey, kCreateNewTabGroupIconMargins);
+    icon->SetImage(ui::ImageModel::FromVectorIcon(kCreateNewTabGroupIcon,
+                                                  kColorProjectsPanelButtonIcon,
+                                                  kCreateNewTabGroupIconSize));
 
     auto* title = AddChildView(std::make_unique<views::Label>(
         l10n_util::GetStringUTF16(IDS_CREATE_NEW_TAB_GROUP)));
