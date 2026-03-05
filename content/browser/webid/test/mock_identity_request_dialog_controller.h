b30af7533f2af81d58197238cc18dba527966964
Yi Gu <yigu@chromium.org>
2026-03-05 11:02:00 -0800

    [FedCM] Refactor the Federation-RP-Connection-Status header interception
    This CL fixes an architectural issue where we relied on a UI component
    for non-UI related logic.
    
    Bug: b:486824705
    Change-Id: I0c5a2863cf28390d57967e529389e9e4fa639930
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7613958
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594820}

--- content/browser/webid/test/mock_identity_request_dialog_controller.h
+++ content/browser/webid/test/mock_identity_request_dialog_controller.h
@@ -87,10 +87,6 @@ class MockIdentityRequestDialogController
               (override));
   MOCK_METHOD(void, CloseModalDialog, (), (override));
   MOCK_METHOD(void,
-              OnConnectionStatusHeaderReceived,
-              (const std::optional<std::string>&),
-              (override));
-  MOCK_METHOD(void,
               OnFlowCompleted,
               (content::webid::FederatedLoginResult),
               (override));
