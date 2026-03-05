68730bc863ca6b6d373e82065e4bd3eb1a6a5326
Josef Raska <josefraska@google.com>
2026-03-05 08:14:32 -0800

    [HoT][Android] Add feature toggle
    - Generated using https://source.chromium.org/chromium/chromium/src/+/main:tools/metrics/histograms/generate_flag_enums.py
    
    Bug: 482994352
    Change-Id: Ifbd1306b07b1abfd3f44038510804e2a68e7cfa2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7626640
    Commit-Queue: Josef Raska <josefraska@google.com>
    Reviewed-by: Stanislav Mikheyev <mikhe@google.com>
    Reviewed-by: Julia Sobiech <jsobiech@google.com>
    Cr-Commit-Position: refs/heads/main@{#1594706}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -460,6 +460,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kWebApkMinShellVersion,
     &kWebOtpCrossDeviceSimpleString,
     &kXplatSyncedSetup,
+    &kYourSavedInfoSettingsPageAndroid,
     &language::kCctAutoTranslate,
     &language::kDetailedLanguageSettings,
     &media::kAutoDocPiPPermissionPromptAndroid,
@@ -811,6 +812,7 @@ BASE_FEATURE(kUseLibunwindstackNativeUnwinderAndroid, base::FEATURE_ENABLED_BY_D
 // a descriptive text.
 BASE_FEATURE(kWebOtpCrossDeviceSimpleString, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kXplatSyncedSetup, base::FEATURE_ENABLED_BY_DEFAULT);
+BASE_FEATURE(kYourSavedInfoSettingsPageAndroid, base::FEATURE_DISABLED_BY_DEFAULT);
 // go/keep-sorted end
 // BASE_FEATURE_END
 
