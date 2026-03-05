ef645e9bc84c1b3cd761d6e9b5ef698a7d1138ed
Hiroshige Hayashizaki <hiroshige@chromium.org>
2026-03-05 15:45:10 -0800

    Decouple MaybeMakeSelfOwnedNetworkServiceDevToolsObserver from PrefetchContainer
    Bug: 483079815, 452406598, 452389538
    Change-Id: I6da899d694022b0a7b62c78bde50cb2ba7ff8eec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7604981
    Reviewed-by: Taiyo Mizuhashi <taiyo@chromium.org>
    Reviewed-by: Ken Okada <kenoss@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1595019}

--- content/browser/preloading/prefetch/prefetch_container.cc
+++ content/browser/preloading/prefetch/prefetch_container.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/variations/net/variations_http_headers.h"
 #include "content/browser/devtools/devtools_agent_host_impl.h"
 #include "content/browser/devtools/devtools_instrumentation.h"
-#include "content/browser/devtools/network_service_devtools_observer.h"
 #include "content/browser/devtools/render_frame_devtools_agent_host.h"
 #include "content/browser/loader/navigation_url_loader_impl.h"
 #include "content/browser/preloading/prefetch/assert_prefetch_container_observer.h"
@@ -1511,8 +1510,11 @@ void PrefetchContainer::MakeInitialResourceRequest() {
   }();
 
   mojo::PendingRemote<network::mojom::DevToolsObserver>
-      devtools_observer_remote =
-          MaybeMakeSelfOwnedNetworkServiceDevToolsObserver();
+      devtools_observer_remote;
+  if (!IsDecoy()) {
+    devtools_observer_remote =
+        MaybeMakeSelfOwnedNetworkServiceDevToolsObserverForPrefetch(request());
+  }
 
   // If we ever implement prefetching for subframes, this value should be
   // reconsidered, as this causes us to reset the site for cookies on cross-site
@@ -2077,28 +2079,6 @@ void PrefetchContainer::NotifyPrefetchRequestComplete(
 
   devtools_instrumentation::OnPrefetchRequestComplete(
       ftn, GetDevtoolsRequestId(), completion_status);
-}
-
-mojo::PendingRemote<network::mojom::DevToolsObserver>
-PrefetchContainer::MaybeMakeSelfOwnedNetworkServiceDevToolsObserver() {
-  if (IsDecoy()) {
-    return mojo::NullRemote();
-  }
-
-  auto* renderer_initiator_info = request().GetRendererInitiatorInfo();
-  if (!renderer_initiator_info) {
-    // Don't emit CDP events if the trigger is not speculation rules.
-    return mojo::NullRemote();
-  }
-
-  auto* ftn =
-      FrameTreeNode::From(renderer_initiator_info->GetRenderFrameHost());
-  if (!ftn) {
-    // Don't emit CDP events if the initiator document isn't alive.
-    return mojo::NullRemote();
-  }
-
-  return NetworkServiceDevToolsObserver::MakeSelfOwned(ftn);
 }
 
 std::string PrefetchContainer::GetMetricsSuffix() const {
