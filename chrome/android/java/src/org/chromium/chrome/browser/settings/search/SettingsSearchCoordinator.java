e67c522173a8db9e1e4130be2d0b58b2b324039e
Jinsuk Kim <jinsukkim@chromium.org>
2026-03-05 13:39:13 -0800

    [Settings Search] Let TalkBack announce the entered query
    Fixes a bug in crrev.com/c/7632902 not accouncing the entered
    query. Now talkback announces the entered query (if present)
    or the hint text, as well as the label/instruction.
    
    Bug: 452675985
    Change-Id: I8d85c75249b0a0c618398275d43b99f9fab3e5cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7637742
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Moe Adel <adelm@google.com>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594923}

--- chrome/android/java/src/org/chromium/chrome/browser/settings/search/SettingsSearchCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/search/SettingsSearchCoordinator.java
@@ -1146,9 +1146,10 @@ public class SettingsSearchCoordinator
                     public void onInitializeAccessibilityNodeInfo(
                             View host, AccessibilityNodeInfo info) {
                         super.onInitializeAccessibilityNodeInfo(host, info);
-                        String orgText = info.getText() == null ? "" : info.getText().toString();
                         info.setText(
-                                mActivity.getString(R.string.search_in_settings_hint, orgText));
+                                info.getText() == null
+                                        ? mActivity.getString(R.string.search_in_settings_hint)
+                                        : info.getText().toString());
                     }
                 });
     }
