92f16916b8c377dc0b97515d7d63138eb0ea0959
Alison Maher <almaher@microsoft.com>
2026-03-05 12:52:27 -0800

    [Masonry] Update collapsed track aggregation location
    Move where we store the collapsed track indexes for grid lanes to the
    track collection. These will be computed in the constructor if
    indicated by the new input param.
    
    This change should not have any behavioral impact. It is a refactor in
    preparation for supporting the GridSizingTree in Grid Lanes for subgrid
    handling. For the full prototype, please see CL:7624683.
    
    Bug: 343257585
    Change-Id: I3cca2faa4bc0952c05e4117992b4498db112c84b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7634374
    Commit-Queue: Alison Maher <almaher@microsoft.com>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594892}

--- third_party/blink/renderer/core/layout/grid_lanes/grid_lanes_layout_algorithm_test.cc
+++ third_party/blink/renderer/core/layout/grid_lanes/grid_lanes_layout_algorithm_test.cc
@@ -22,7 +22,6 @@ class GridLanesLayoutAlgorithmTest : public BaseLayoutAlgorithmTest {
     wtf_size_t start_offset;
     const auto& style = algorithm.Style();
     const GridLineResolver line_resolver(style, /*auto_repetitions=*/0);
-    collapsed_track_indexes_.clear();
 
     auto grid_lanes_items =
         algorithm.Node().ConstructGridLanesItems(line_resolver);
@@ -30,7 +29,7 @@ class GridLanesLayoutAlgorithmTest : public BaseLayoutAlgorithmTest {
     grid_axis_tracks_ = algorithm.ComputeGridAxisTracks(
         SizingConstraint::kLayout, /*intrinsic_repeat_track_sizes=*/nullptr,
         /*should_apply_inline_size_containment=*/false, grid_lanes_items,
-        collapsed_track_indexes_, start_offset, needs_intrinsic_track_size);
+        start_offset, needs_intrinsic_track_size);
 
     // We have a repeat() track definition with an intrinsic sized track(s). The
     // previous track sizing pass was used to find the track size to apply
@@ -40,15 +39,13 @@ class GridLanesLayoutAlgorithmTest : public BaseLayoutAlgorithmTest {
     //
     // https://www.w3.org/TR/css-grid-3/#masonry-intrinsic-repeat
     if (needs_intrinsic_track_size) {
-      CHECK(collapsed_track_indexes_.empty());
-
       HashMap<GridTrackSize, LayoutUnit> intrinsic_repeat_track_sizes =
           algorithm.GetIntrinsicRepeaterTrackSizes(!grid_lanes_items.IsEmpty(),
                                                    grid_axis_tracks_.value());
       grid_axis_tracks_ = algorithm.ComputeGridAxisTracks(
           SizingConstraint::kLayout, &intrinsic_repeat_track_sizes,
           /*should_apply_inline_size_containment=*/false, grid_lanes_items,
-          collapsed_track_indexes_, start_offset, needs_intrinsic_track_size);
+          start_offset, needs_intrinsic_track_size);
     }
 
     const auto grid_axis_direction = grid_axis_tracks_->Direction();
@@ -105,8 +102,9 @@ class GridLanesLayoutAlgorithmTest : public BaseLayoutAlgorithmTest {
   GridLanesRunningPositions InitializeGridLanesRunningPositions(
       const Vector<LayoutUnit>& running_positions,
       LayoutUnit tie_threshold) {
+    const Vector<wtf_size_t> empty_collapsed_tracks;
     return GridLanesRunningPositions(running_positions, tie_threshold,
-                                     collapsed_track_indexes_);
+                                     empty_collapsed_tracks);
   }
 
   void SetAutoPlacementCursor(wtf_size_t cursor,
@@ -130,9 +128,6 @@ class GridLanesLayoutAlgorithmTest : public BaseLayoutAlgorithmTest {
   // Virtual items represent the contributions of item groups in track sizing
   // and are not directly related to any children of the container.
   Vector<GridLanesItemCachedData> virtual_items_data_;
-
-  // List of track indexes that have been collapsed.
-  Vector<wtf_size_t> collapsed_track_indexes_;
 };
 
 TEST_F(GridLanesLayoutAlgorithmTest, ConstructGridLanesItems) {
