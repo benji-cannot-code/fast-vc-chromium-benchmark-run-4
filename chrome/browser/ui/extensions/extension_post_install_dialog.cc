1b05242eff8c37e3c85da1c161f56a447b708c8c
EmiliaPaz <emiliapaz@chromium.org>
2026-03-05 10:17:59 -0800

    [Extensions] Add ok button to post install dialog in Android
    Adds an ok button with "got it" text to the post install dialog in
    Android, per UX mocks.
    
    Note: the button should use prominent style. This is a fix that will
    be mode at the dialog model level.
    
    Screenshot: crbug.com/488500832#attachment73995895
    
    Fixed: 488500832
    Change-Id: I9ce0e3cccfab863501a2ec0ab1ac5d49ceba51fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7635739
    Reviewed-by: Richard Chui <richui@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Auto-Submit: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594785}

--- chrome/browser/ui/extensions/extension_post_install_dialog.cc
+++ chrome/browser/ui/extensions/extension_post_install_dialog.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/functional/bind.h"
+#include "base/functional/callback_helpers.h"
 #include "base/i18n/rtl.h"
 #include "base/memory/weak_ptr.h"
 #include "base/strings/strcat.h"
@@ -69,6 +70,13 @@ void ConfigurePostInstallDialogModel(
     dialog_model_builder.AddParagraph(ui::DialogModelLabel(
         l10n_util::GetStringUTF16(IDS_EXTENSION_INSTALLED_MANAGE_INFO)));
   }
+
+#if BUILDFLAG(IS_ANDROID)
+  dialog_model_builder.AddOkButton(
+      base::DoNothing(),
+      ui::DialogModel::Button::Params().SetLabel(
+          l10n_util::GetStringUTF16(IDS_EXTENSION_INSTALLED_OK_BUTTON)));
+#endif
 }
 
 void OpenExtensionsShortcutsPage(
@@ -131,8 +139,8 @@ void ShowExtensionPostInstallDialog(
       base::BindRepeating(&ExtensionPostInstallDialog::LinkClicked,
                           base::Unretained(weak_delegate));
 
-  extensions::ConfigurePostInstallDialogModel(
-      dialog_model_builder, weak_delegate->model(), manage_shortcuts_callback);
+  ConfigurePostInstallDialogModel(dialog_model_builder, weak_delegate->model(),
+                                  manage_shortcuts_callback);
 
 #if !BUILDFLAG(IS_CHROMEOS) && !BUILDFLAG(IS_ANDROID)
   // Add a sync or sign in promo in the footer if it should be shown.
