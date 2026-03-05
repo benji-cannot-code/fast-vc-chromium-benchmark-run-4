68f2f24a32f06a5c7a920a3a4c8394c17e2d5281
Adam Rice <ricea@chromium.org>
2026-03-04 19:18:59 -0800

    Keep network service process handle alive
    There is a minor issue with network::SocketBrokerImpl on Windows in that
    in corner cases it can assign a socket to the wrong process. This can
    only happen if the process ID of the old network service process is
    reassigned before SocketBrokerImpl has finished processing the mojo
    messages from the old network service, AND a new process has taken that
    process ID in the meantime.
    
    To reduce the likelihood of this happening, use
    content::NetworkServiceListener to keep the process handle of the network
    service alive for a minute after it crashes. This should be enough time
    for all mojo messages to be processed.
    
    The code only attempts to keep track of process handles for one old
    network service. If it is crash-looping we don't want to fill up memory
    with process handles.
    
    Also remove NetworkServiceListener functionality that is no longer
    needed.
    
    No impact on platforms other than Windows.
    
    Bug: 475587477
    Change-Id: Idfb140130f1168759f96d59c19296b03f6e30999
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7532563
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594426}

--- content/browser/network_service_client.cc
+++ content/browser/network_service_client.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/unguessable_token.h"
 #include "build/build_config.h"
 #include "content/browser/browsing_data/clear_site_data_handler.h"
+#include "content/browser/network/network_service_process_tracker_win.h"
 #include "content/browser/ssl/ssl_manager.h"
 #include "content/browser/webrtc/webrtc_connections_observer.h"
 #include "content/public/browser/browser_context.h"
@@ -211,6 +212,7 @@ void NetworkServiceClient::OnIPAddressChanged(
 #if BUILDFLAG(IS_WIN)
 mojo::PendingRemote<network::mojom::SocketBroker>
 NetworkServiceClient::BindSocketBroker() {
+  EnsureNetworkServiceListenerStarted();
   return socket_broker_.BindNewRemote();
 }
 #endif  // BUILDFLAG(IS_WIN)
