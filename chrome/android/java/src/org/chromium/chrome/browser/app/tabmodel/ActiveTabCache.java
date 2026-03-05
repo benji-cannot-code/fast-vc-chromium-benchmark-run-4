c4facb5c0a53d4ec222f6a6c5b91c75f90e406b7
Fiaz Muhammad <mfiaz@google.com>
2026-03-05 06:58:27 -0800

    Reland "[Persist] General changes related to Active Tab caching"
    This is a reland of commit 01f266bb67bf628064f9c7f84e205690d5dfab41
    - This fixes the flake caused by ThreadUtils occasionally not
            rethrowing an assertion error as a runtime exception.
    
    Original change's description:
    > [Persist] General changes related to Active Tab caching
    >
    > - Allow for null cipher factories when not needed. This makes
    >         changes to the TabStateFileManager.
    > - Use the flatbuffer prefix with tab state names since this triggers
    >         the use of flatbuffer as a serializer/deserializer.
    > - Update tests.
    >
    > Bug: 485907357
    > Change-Id: I3fc5f89586730f8568c46dc328925307d36fd31f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7629423
    > Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    > Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1593879}
    
    Bug: 485907357
    Change-Id: I85dfcb6e570353c6bd45300c84c582b6bd6c74f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7634902
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594656}

--- chrome/android/java/src/org/chromium/chrome/browser/app/tabmodel/ActiveTabCache.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/tabmodel/ActiveTabCache.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.app.tabmodel;
 
 import static org.chromium.base.ThreadUtils.assertOnUiThread;
+import static org.chromium.chrome.browser.tabpersistence.TabStateFileManager.FLATBUFFER_PREFIX;
 
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -69,10 +70,12 @@ public class ActiveTabCache {
      * @param tabIndex The index of the active tab.
      * @param cipherFactory The cipher factory for encrypting incognito tab state.
      */
-    public void saveActiveTab(Tab tab, int tabIndex, CipherFactory cipherFactory) {
+    public void saveActiveTab(Tab tab, int tabIndex, @Nullable CipherFactory cipherFactory) {
         assertOnUiThread();
 
         boolean isOffTheRecord = tab.isOffTheRecord();
+        assert !isOffTheRecord || cipherFactory != null;
+
         String fileName = isOffTheRecord ? mIncognitoTabFileName : mRegularTabFileName;
         File file = new File(getOrCreateCacheDirectory(), fileName);
         TabState tabState = TabStateExtractor.from(tab);
@@ -91,8 +94,9 @@ public class ActiveTabCache {
      * @param cipherFactory The cipher factory for decrypting incognito tab state.
      */
     public @Nullable CachedActiveTab restoreActiveTab(
-            boolean isOffTheRecord, CipherFactory cipherFactory) {
+            boolean isOffTheRecord, @Nullable CipherFactory cipherFactory) {
         assertOnUiThread();
+        assert !isOffTheRecord || cipherFactory != null;
 
         String fileName = isOffTheRecord ? mIncognitoTabFileName : mRegularTabFileName;
         File file = new File(getOrCreateCacheDirectory(), fileName);
@@ -181,7 +185,9 @@ public class ActiveTabCache {
 
     private static String getFileName(String windowTag, boolean incognito) {
         String suffix = incognito ? INCOGNITO_SUFFIX : REGULAR_SUFFIX;
-        return windowTag + suffix;
+        // This prefix is required to ensure FlatBuffer is used during serialization and
+        // deserialization.
+        return FLATBUFFER_PREFIX + windowTag + suffix;
     }
 
     private static void deleteFileAndPref(String fileName) {
