6fc55a6c57bef29d09ce2aa2a69d33167f890fa8
James Leung <jamesleung@google.com>
2026-03-05 14:08:48 -0800

    [contextual tasks] test mojo calls fix
    * Change private variable access to public method/
      mojo usage in contextual tasks tests
    * Removed other "as any" conversions
    * removed extraneous awaits
    * This way, we ensure the event listeners are correct
    * As suggested by @Paul A.
    
    Bug: b:481725168
    Change-Id: I70cb1c96ec9e7245fea22f4f50ea0b199ef491b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7598105
    Commit-Queue: James Leung <jamesleung@google.com>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1594955}

--- chrome/test/data/webui/contextual_tasks/composebox_submit_test.ts
+++ chrome/test/data/webui/contextual_tasks/composebox_submit_test.ts
@@ -25,11 +25,7 @@ import {TestMock} from 'chrome://webui-test/test_mock.js';
 import {isVisible, microtasksFinished} from 'chrome://webui-test/test_util.js';
 
 import {TestContextualTasksBrowserProxy} from './test_contextual_tasks_browser_proxy.js';
-import {assertStyle, installMock, mockInputState, setupAutocompleteResults, simulateUserInput, uploadFileAndVerify} from './test_utils.js';
-
-const ADD_TAB_CONTEXT_FN = 'addTabContext';
-const FAKE_TOKEN_STRING = '00000000000000001234567890ABCDEF';
-const FAKE_TOKEN_STRING_2 = '00000000000000001234567890ABCDFF';
+import {ADD_TAB_CONTEXT_FN, assertStyle, FAKE_TOKEN_STRING, FAKE_TOKEN_STRING_2, installMock, mockInputState, setupAutocompleteResults, simulateUserInput, uploadFileAndVerify} from './test_utils.js';
 
 function pressEnter(element: HTMLElement) {
   element.dispatchEvent(new KeyboardEvent('keydown', {
@@ -246,7 +242,10 @@ suite('ContextualTasksComposeboxSubmitTest', () => {
   });
 
   test('LensButtonTriggersOverlay', async () => {
-    contextualTasksApp.$.composebox.isSidePanel = true;
+    testProxy.handler.setIsShownInTab(false);
+
+    testProxy.callbackRouterRemote.onSidePanelStateChanged();
+    await testProxy.callbackRouterRemote.$.flushForTesting();
     await microtasksFinished();
 
     assertTrue(composebox.lensButtonTriggersOverlay);
@@ -260,7 +259,6 @@ suite('ContextualTasksComposeboxSubmitTest', () => {
     await mockComposeboxPageHandler.whenCalled('handleLensButtonClick');
     assertEquals(
         1, mockComposeboxPageHandler.getCallCount('handleLensButtonClick'));
-
 
     // A second click should still trigger the same handler and the button
     // should still be disabled.
@@ -278,8 +276,8 @@ suite('ContextualTasksComposeboxSubmitTest', () => {
         const threadFrame = contextualTasksApp.$.threadFrame;
         const flexCenterContainer = contextualTasksApp.$.flexCenterContainer;
 
-        (contextualTasksApp as any).isAiPage_ = false;
-        (contextualTasksApp as any).isNavigatingFromAiPage_ = false;
+        testProxy.handler.setIsAiPage(false);
+        contextualTasksApp.setIsNavigatingFromAiPageForTesting(false);
 
         testProxy.callbackRouterRemote.hideInput();
         await testProxy.callbackRouterRemote.$.flushForTesting();
@@ -332,8 +330,8 @@ suite('ContextualTasksComposeboxSubmitTest', () => {
         const contextualComposebox = contextualTasksApp.$.composebox;
         const header = contextualTasksApp.$.composeboxHeaderWrapper;
 
-        (contextualTasksApp as any).isAiPage_ = false;
-        (contextualTasksApp as any).isNavigatingFromAiPage_ = false;
+        testProxy.handler.setIsAiPage(false);
+        contextualTasksApp.setIsNavigatingFromAiPageForTesting(false);
 
         testProxy.callbackRouterRemote.hideInput();
         await testProxy.callbackRouterRemote.$.flushForTesting();
