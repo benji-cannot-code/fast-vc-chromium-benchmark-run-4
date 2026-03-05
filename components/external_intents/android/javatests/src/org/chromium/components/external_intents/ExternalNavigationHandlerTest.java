76991d319eb3d6b67f667e6e1b6da3fb6f3f261b
Sinan Sahin <sinansahin@google.com>
2026-03-04 19:33:34 -0800

    Replace hardcoded Android T SDK level in ExternalNavigationHandlerTest
    Change-Id: Ic80376992ff322cef3f7307491ab15e55714bc02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7635938
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1594437}

--- components/external_intents/android/javatests/src/org/chromium/components/external_intents/ExternalNavigationHandlerTest.java
+++ components/external_intents/android/javatests/src/org/chromium/components/external_intents/ExternalNavigationHandlerTest.java
@@ -2470,7 +2470,7 @@ public class ExternalNavigationHandlerTest {
 
     @Test
     @SmallTest
-    @MinAndroidSdkLevel(33) // TODO(twellington): Replace with version code when available.
+    @MinAndroidSdkLevel(Build.VERSION_CODES.TIRAMISU)
     public void testFileAccessHtml_AndroidT() {
         String fileUrl = "file:///sdcard/Downloads/test.html";
 
