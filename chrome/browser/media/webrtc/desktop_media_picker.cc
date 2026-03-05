806f97e5e41d440b6e9084b8aa9288c4b11be473
Grace Cham <hscham@google.com>
2026-03-05 08:42:31 -0800

    Add comment to clank media capture feature flags redirecting to finch
    To avoid confusion that they are always disabled by default.
    
    Bug: 405218400
    Change-Id: I678ca618bbd8da79b52fcf1122c05a9d52960494
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7632099
    Auto-Submit: Grace Cham <hscham@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594721}

--- chrome/browser/media/webrtc/desktop_media_picker.cc
+++ chrome/browser/media/webrtc/desktop_media_picker.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/media/webrtc/desktop_media_picker.h"
 
 #if BUILDFLAG(IS_ANDROID)
+// Enabled by Finch depending on form factor.
 BASE_FEATURE(kAndroidMediaPicker, base::FEATURE_DISABLED_BY_DEFAULT);
 #endif
 
