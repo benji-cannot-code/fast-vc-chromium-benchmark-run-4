5a6ba8413b4dab67d179646dc55eb485442ec037
Tsuyoshi Horo <horo@chromium.org>
2026-03-05 14:07:38 -0800

    net/disk_cache/sql: Flush buffers for count and size calculation
    In CL 7532735 (crrev.com/c/7532735), lazy entry creation was
    implemented, deferring database INSERT operations until necessary.
    However, it missed updating `SqlBackendImpl::GetEntryCount` and
    `SqlBackendImpl::CalculateSizeOfEntriesBetween`. These methods rely on
    asynchronous database operations and thus need to flush the write
    buffers of all active entries first to ensure that newly created
    entries or pending writes are visible to the queries.
    
    This CL introduces `SqlBackendImpl::FlushActiveEntriesBuffers()` and
    calls it from `GetEntryCount` and `CalculateSizeOfEntriesBetween`
    (as well as refactoring `CreateIterator` to use it) before they
    perform database operations.
    
    Bug: 479348720
    Fixed: 490013909
    Change-Id: Ibf31ea96b780f066bba130cf6a65a3c70abb9da3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7632098
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594953}

--- net/disk_cache/sql/sql_backend_impl.cc
+++ net/disk_cache/sql/sql_backend_impl.cc
@@ -459,6 +459,9 @@ int64_t SqlBackendImpl::MaxFileSize() const {
 
 base::expected<int32_t, net::Error> SqlBackendImpl::GetEntryCount(
     GetEntryCountCallback callback) const {
+  // Flush buffers so that the GetEntryCountAsync call can see entries not yet
+  // written to the DB.
+  FlushActiveEntriesBuffers();
   // The entry count must be retrieved asynchronously to ensure that all
   // pending database operations are reflected in the result.
   store_->GetEntryCountAsync(std::move(callback));
@@ -561,6 +564,12 @@ SqlEntryImpl* SqlBackendImpl::GetActiveEntry(const CacheEntryKey& key) {
     return &it->second.get();
   }
   return nullptr;
+}
+
+void SqlBackendImpl::FlushActiveEntriesBuffers() const {
+  for (const auto& it : active_entries_) {
+    it.second->FlushBuffer(/*force_flush_for_creation=*/true);
+  }
 }
 
 void SqlBackendImpl::DoomActiveEntry(SqlEntryImpl& entry) {
@@ -812,6 +821,10 @@ int64_t SqlBackendImpl::CalculateSizeOfEntriesBetween(
     base::Time initial_time,
     base::Time end_time,
     Int64CompletionOnceCallback callback) {
+  // Flush buffers so that the CalculateSizeOfEntriesBetween call can see
+  // entries not yet written to the DB.
+  FlushActiveEntriesBuffers();
+
   exclusive_operation_coordinator_.PostOrRunExclusiveOperation(base::BindOnce(
       &SqlBackendImpl::HandleCalculateSizeOfEntriesBetweenOperation,
       weak_factory_.GetWeakPtr(), initial_time, end_time, std::move(callback)));
@@ -842,9 +855,7 @@ void SqlBackendImpl::HandleCalculateSizeOfEntriesBetweenOperation(
 std::unique_ptr<Backend::Iterator> SqlBackendImpl::CreateIterator() {
   // Flush buffers so that the Iterator can see entries not yet written to the
   // DB.
-  for (const auto& it : active_entries_) {
-    it.second->FlushBuffer(/*force_flush_for_creation=*/true);
-  }
+  FlushActiveEntriesBuffers();
   return std::make_unique<IteratorImpl>(weak_factory_.GetWeakPtr());
 }
 
