4660a4ce5375447ff0685830d459bdea82ee1d40
Noah Rose Ledesma <noahrose@google.com>
2026-03-04 16:04:18 -0800

    Updater UI: Use cr-button for filter type dialog
    The dialog which allows the user to select the type of filter they'd
    like to edit used a plain button which did not handle
    forced-colors:active properly. Use a cr-button instead.
    
    Fixed: 489507687
    Change-Id: Id56eeba4519571643b22469f577d80ca6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7635339
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: Noah Rose Ledesma <noahrose@google.com>
    Cr-Commit-Position: refs/heads/main@{#1594306}

--- chrome/browser/resources/updater/event_list/filter_dialog/type_dialog.ts
+++ chrome/browser/resources/updater/event_list/filter_dialog/type_dialog.ts
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import './filter_dialog.js';
+import '//resources/cr_elements/cr_button/cr_button.js';
 
 import type {PropertyValues} from '//resources/lit/v3_0/lit.rollup.js';
 import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
