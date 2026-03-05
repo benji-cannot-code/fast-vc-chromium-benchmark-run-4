41373fe089e07d5294e4fea7555bf2df0cc7a866
Ewann Pelle <ewannpv@chromium.org>
2026-03-05 05:17:23 -0800

    [iOS][Assistant] Refine container resizing and detent snapping logic
    This CL updates the Assistant container to feel more native, optimizes
    the gestural math performance, and prevents the view from resting at
    arbitrary non-detent heights:
    - Removes the `_hasUserResized` tracking variable and the associated
      snapping override logic.
    - Replaces the linear rubber banding algorithm with a logarithmic one.
    - Eliminates `preferredHeight` scaling from `AssistantContainerView`.
    - Internally sorts provided `AssistantContainerDetent` payloads
      allowing `effectiveMinHeight` and `effectiveMaxHeight` limits
      to be computed in O(1).
    
    Demo:
    https://drive.google.com/file/d/1eePtqutdPgm2Q0feVk8uN7hRmD4ZzJNA/view?usp=sharing&resourcekey=0-Hk1-uxWyiGC7YXO6GHYOyA
    
    Bug: 469050167
    Change-Id: I83e8acba846072338d5a6e598f81039fe2675e89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7634178
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594611}

--- ios/chrome/browser/assistant/ui/assistant_container_view.h
+++ ios/chrome/browser/assistant/ui/assistant_container_view.h
@@ -26,8 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // The content view where subviews should be added.
 @property(nonatomic, strong, readonly) UIView* contentView;
 
-// Returns the preferred height of the container based on its content.
-- (NSInteger)preferredHeight;
 
 @end
 
