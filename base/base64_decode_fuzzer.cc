9b6b129836f294f4b4aba4f27a495e89db7c3ee1
Andrew Paseltiner <apaseltiner@chromium.org>
2026-03-06 15:00:06 -0800

    Use macro for spanified inputs in //base fuzzers where applicable
    This replaces direct use of the pointer-and-size pair with a
    base::span<const uint8_t>, which is frequently easier to work with and
    avoids the need for manual construction of such spans alongside the
    appropriate unsafe-buffer annotations.
    
    Bug: 435317390
    Change-Id: Ic957d461e05b75ba7e99faee74df5698ff4cdba6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7639472
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1595722}

--- base/base64_decode_fuzzer.cc
+++ base/base64_decode_fuzzer.cc
@@ -7,10 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 
 #include "base/base64.h"
+#include "base/containers/span.h"
+#include "base/strings/string_view_util.h"
+#include "testing/libfuzzer/libfuzzer_base_wrappers.h"
 
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+DEFINE_LLVM_FUZZER_TEST_ONE_INPUT_SPAN(base::span<const uint8_t> data) {
   std::string decode_output;
-  std::string_view data_piece(reinterpret_cast<const char*>(data), size);
-  base::Base64Decode(data_piece, &decode_output);
+  base::Base64Decode(base::as_string_view(data), &decode_output);
   return 0;
 }
--- base/strings/escape_fuzzer.cc
+++ base/strings/escape_fuzzer.cc
@@ -5,7 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/escape.h"
 
+#include <string>
 #include <string_view>
+
+#include "base/containers/span.h"
+#include "base/strings/string_view_util.h"
+#include "testing/libfuzzer/libfuzzer_base_wrappers.h"
 
 // Prevent the optimizer from optimizing away a function call by "using" the
 // result.
@@ -20,8 +25,8 @@ void UseResult(const std::string& input) {
 }
 
 // Entry point for LibFuzzer.
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  std::string_view data_string(reinterpret_cast<const char*>(data), size);
+DEFINE_LLVM_FUZZER_TEST_ONE_INPUT_SPAN(base::span<const uint8_t> data) {
+  const auto data_string = base::as_string_view(data);
 
   UseResult(base::EscapeQueryParamValue(data_string, /*use_plus=*/false));
   UseResult(base::EscapeQueryParamValue(data_string, /*use_plus=*/true));
