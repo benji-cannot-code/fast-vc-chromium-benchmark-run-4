14cb84e18e951b69e4fa19fa8856ed1202b04c25
Jack Thiesen <jthiesen@chromium.org>
2026-03-06 12:37:25 -0800

    Feature Flag for BWI Exposure to Custom Tab Activity
    Bug: 468113418, 486858979, 450667327, 485490213, 468111732
    Change-Id: Ie461d6d602225e516d1c67af38eed4565811c8ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7644210
    Commit-Queue: Jack Thiesen <jthiesen@chromium.org>
    Reviewed-by: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1595616}

--- chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
@@ -428,6 +428,8 @@ public abstract class ChromeFeatureList {
     public static final String ENABLE_ANDROID_SIDE_PANEL = "EnableAndroidSidePanel";
     public static final String ENABLE_ANDROID_SIDE_PANEL_DEV_FEATURE =
             "EnableAndroidSidePanelDevFeature";
+    public static final String ENABLE_BROWSER_WINDOW_INTERFACE_FOR_CUSTOM_TAB_ACTIVITY =
+            "EnableBrowserWindowInterfaceForCustomTabActivity";
     public static final String ENABLE_CLIPBOARD_DATA_CONTROLS_ANDROID =
             "EnableClipboardDataControlsAndroid";
     public static final String ENABLE_CONTEXT_MENU_FOR_PREVIEW_TAB =
@@ -920,6 +922,10 @@ public abstract class ChromeFeatureList {
             newCachedFlag(ENABLE_ANDROID_SIDE_PANEL, false);
     public static final CachedFlag sEnableAndroidSidePanelDevFeature =
             newCachedFlag(ENABLE_ANDROID_SIDE_PANEL_DEV_FEATURE, false);
+    public static final CachedFlag sEnableBrowserWindowInterfaceForCustomTabActivity =
+            newCachedFlag(
+                    ENABLE_BROWSER_WINDOW_INTERFACE_FOR_CUSTOM_TAB_ACTIVITY,
+                    /* defaultValue= */ true);
     public static final CachedFlag sEnableExclusiveAccessManager =
             newCachedFlag(ENABLE_EXCLUSIVE_ACCESS_MANAGER, true);
     public static final CachedFlag sEnableFullscreenToAnyScreenAndroid =
@@ -1222,6 +1228,7 @@ public abstract class ChromeFeatureList {
                     sEducationalTipDefaultBrowserPromoCard,
                     sEnableAndroidSidePanel,
                     sEnableAndroidSidePanelDevFeature,
+                    sEnableBrowserWindowInterfaceForCustomTabActivity,
                     sEnableExclusiveAccessManager,
                     sEnableFullscreenToAnyScreenAndroid,
                     sEnableXAxisActivityTransition,
