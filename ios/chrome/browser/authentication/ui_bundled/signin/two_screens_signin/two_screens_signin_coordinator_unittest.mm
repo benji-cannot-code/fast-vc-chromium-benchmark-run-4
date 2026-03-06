2947c61d19bd4c4aece2aa53581bcce5399f0943
Asami Doi <asamidoi@chromium.org>
2026-03-06 07:46:52 -0800

    [ios] Deprecate UIWindow's init in TwoScreensSigninCoordinatorTest
    UIWindow's init was deprecated on iOS 26, initWithWindowScene should be
    used instead.
    
    Bug: 489089329
    Change-Id: I3fb24cde82eb5b21cf8ef770ae929ccecef60446
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7637592
    Auto-Submit: Asami Doi <asamidoi@chromium.org>
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1595415}

--- ios/chrome/browser/authentication/ui_bundled/signin/two_screens_signin/two_screens_signin_coordinator_unittest.mm
+++ ios/chrome/browser/authentication/ui_bundled/signin/two_screens_signin/two_screens_signin_coordinator_unittest.mm
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/signin/model/fake_system_identity_manager.h"
 #import "ios/chrome/browser/sync/model/mock_sync_service_utils.h"
 #import "ios/chrome/browser/sync/model/sync_service_factory.h"
+#import "ios/chrome/test/app/uikit_test_util.h"
 #import "ios/chrome/test/ios_chrome_scoped_testing_local_state.h"
 #import "ios/web/public/test/web_task_environment.h"
 #import "testing/gtest_mac.h"
@@ -72,7 +73,8 @@ class TwoScreensSigninCoordinatorTest : public PlatformTest {
     [standardDefaults removeObjectForKey:kSigninPromoViewDisplayCountKey];
 
     UIView.animationsEnabled = NO;
-    window_ = [[UIWindow alloc] init];
+    window_ = [[UIWindow alloc]
+        initWithWindowScene:chrome_test_util::GetAnyWindowScene()];
     window_.rootViewController = [[UIViewController alloc] init];
     [window_ addSubview:window_.rootViewController.view];
 
