76a962ff47af001807540ce9f3190ba1ead4294a
Joe Mason <joenotcharles@google.com>
2026-03-05 17:16:49 -0800

    Return a dummy value from TextInputClientMac on reentry
    It seems impossible to completely prevent reentry into this
    TextInputClientMac while running a nested event loop, for two reasons:
    
    1. Even with ScopedRestrictNSEventMask, [NSApp nextEventMatchingMask]
    can pump events and send them to IME providers, it just doesn't surface
    them to Chrome. (See crash/212378c91dc8baa2 for example.) This is
    potentially fixable, but...
    
    2. Even if the loop only pumps Chrome tasks, those tasks can call
    NativeWidgetNSWindowBridge::RedispatchKeyEvent which can trigger IME
    events with [NSApp sendEvent]. (See crash/f8031ba840d728f7 for example.)
    
    So instead of CHECK'ing on reentry, return the same dummy value as a
    timeout, and log a histogram to see how often this happens.
    
    Also adds a FeatureParam to enable and disable the
    ScopedRestrictNSEventMask, to measure how much it cuts down on reentry.
    
    Bug: 475270375
    Change-Id: I9981eaede2e23afb756c88254a0c0475c59e273f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7629087
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1595059}

--- content/browser/renderer_host/text_input_client_mac.mm
+++ content/browser/renderer_host/text_input_client_mac.mm
@@ -8,9 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/check.h"
+#include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/memory/weak_ptr.h"
 #include "base/message_loop/message_pump_apple.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/no_destructor.h"
 #include "base/run_loop.h"
@@ -131,13 +133,19 @@ uint32_t TextInputClientMac::GetCharacterIndexAtPoint(
     return 0;
   }
 
+  base::UmaHistogramBoolean("TextInputClient.InSyncRequest.CharacterIndex",
+                            in_sync_request_);
+  if (in_sync_request_) {
+    return 0;
+  }
+
   base::TimeTicks start = base::TimeTicks::Now();
   base::TimeDelta wait_timeout = features::kTextInputClientIPCTimeout.Get();
 
   BeforeRequest();
   async_request_delegate_->GetCharacterIndexAtPoint(
       rfhi.get(), current_request_.value(), point);
-  if (features::kTextInputClientUseNestedLoop.Get()) {
+  if (base::FeatureList::IsEnabled(features::kTextInputClientUseNestedLoop)) {
     EnterNestedLoop(wait_timeout);
     // IMPORTANT: After this point the nested loop may have invalidated any
     // pointers and references passed in from the caller (notably `rfhi`).
@@ -175,13 +183,19 @@ gfx::Rect TextInputClientMac::GetFirstRectForRange(
     return gfx::Rect();
   }
 
+  base::UmaHistogramBoolean("TextInputClient.InSyncRequest.FirstRect",
+                            in_sync_request_);
+  if (in_sync_request_) {
+    return gfx::Rect();
+  }
+
   base::TimeTicks start = base::TimeTicks::Now();
   base::TimeDelta wait_timeout = features::kTextInputClientIPCTimeout.Get();
 
   BeforeRequest();
   async_request_delegate_->GetFirstRectForRange(
       rfhi.get(), current_request_.value(), range);
-  if (features::kTextInputClientUseNestedLoop.Get()) {
+  if (base::FeatureList::IsEnabled(features::kTextInputClientUseNestedLoop)) {
     EnterNestedLoop(wait_timeout);
     // IMPORTANT: After this point the nested loop may have invalidated any
     // pointers and references passed in from the caller (notably `rfhi`).
@@ -224,7 +238,7 @@ void TextInputClientMac::SetCharacterIndexAndSignal(
       return;
     }
     character_index_ = index;
-    if (features::kTextInputClientUseNestedLoop.Get()) {
+    if (base::FeatureList::IsEnabled(features::kTextInputClientUseNestedLoop)) {
       CHECK(nested_loop_);
       nested_loop_->Quit();
       return;
@@ -244,7 +258,7 @@ void TextInputClientMac::SetFirstRectAndSignal(
       return;
     }
     first_rect_ = first_rect;
-    if (features::kTextInputClientUseNestedLoop.Get()) {
+    if (base::FeatureList::IsEnabled(features::kTextInputClientUseNestedLoop)) {
       CHECK(nested_loop_);
       nested_loop_->Quit();
       return;
@@ -303,7 +317,7 @@ void TextInputClientMac::BeforeRequest() {
   first_rect_.reset();
 
   CHECK(!nested_loop_);
-  if (features::kTextInputClientUseNestedLoop.Get()) {
+  if (base::FeatureList::IsEnabled(features::kTextInputClientUseNestedLoop)) {
     nested_loop_.emplace(base::RunLoop::Type::kNestableTasksAllowed);
   }
 }
@@ -336,10 +350,12 @@ void TextInputClientMac::EnterNestedLoop(base::TimeDelta timeout) {
     nested_loop_timer.Start(FROM_HERE, timeout, this,
                             &TextInputClientMac::OnNestedLoopTimeout);
 
-    // Don't pump UI events in the nested loop, to prevent re-entering from
-    // queued AppKit NSTextInputContext events.
-    base::ScopedRestrictNSEventMask event_mask(
-        base::PassKey<TextInputClientMac>{});
+    std::optional<base::ScopedRestrictNSEventMask> event_mask;
+    if (features::kTextInputClientNestedLoopEventMask.Get()) {
+      // Don't pump UI events in the nested loop, to prevent re-entering from
+      // queued AppKit NSTextInputContext events.
+      event_mask.emplace(base::PassKey<TextInputClientMac>{});
+    }
 
     // The loop will exit either when a response is received, or the timer
     // fires.
