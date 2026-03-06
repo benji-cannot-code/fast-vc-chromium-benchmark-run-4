3ade92a769f4357aa6ff308cdd0abe6139522db2
Asami Doi <asamidoi@chromium.org>
2026-03-06 06:32:34 -0800

    [ios] Deprecate initWithFrame in LensOverlayFakeWebState
    initWithFrame was deprecated on iOS 26, initWithWindowScene should be
    used instead.
    
    Bug: 489089329
    Change-Id: I652a8de765b18c96b2de4b6aef1d8bf762869e9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7633242
    Auto-Submit: Asami Doi <asamidoi@chromium.org>
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1595366}

--- ios/chrome/browser/lens_overlay/coordinator/lens_overlay_coordinator_unittest.mm
+++ ios/chrome/browser/lens_overlay/coordinator/lens_overlay_coordinator_unittest.mm
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/snapshots/model/snapshot_tab_helper.h"
 #import "ios/chrome/browser/toolbar/legacy/ui_bundled/fullscreen/toolbars_size_browser_agent.h"
 #import "ios/chrome/browser/web/model/web_view_proxy/web_view_proxy_tab_helper.h"
+#import "ios/chrome/test/app/uikit_test_util.h"
 #import "ios/chrome/test/ios_chrome_scoped_testing_local_state.h"
 #import "ios/chrome/test/scoped_key_window.h"
 #import "ios/web/public/test/fakes/fake_web_state.h"
@@ -99,8 +100,9 @@ class LensOverlayCoordinatorTest : public PlatformTest {
         AuthenticationServiceFactory::GetForProfile(profile_);
 
     SceneState* mock_scene_state = OCMClassMock([SceneState class]);
-    UIWindow* window =
-        [[UIWindow alloc] initWithFrame:CGRectMake(0, 0, 320, 520)];
+    UIWindow* window = [[UIWindow alloc]
+        initWithWindowScene:chrome_test_util::GetAnyWindowScene()];
+    window.frame = CGRectMake(0, 0, 320, 520);
     OCMStub([mock_scene_state window]).andReturn(window);
     profile_state_ = [[ProfileState alloc] initWithAppState:nil];
     profile_state_.profile = profile_.get();
