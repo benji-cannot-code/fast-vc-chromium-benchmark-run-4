3b47552863b75d12c368a4ba15e386af64cb1dcb
Joe Downing <joedow@chromium.org>
2026-03-05 16:36:27 -0800

    Update JingleSession to use JingleMessage Rvalues
    Bug: 359620500
    Change-Id: I1a7e5a14dcd423b3d62d584dba5e36d7b3e978c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7639390
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1595042}

--- remoting/protocol/jingle_session.h
+++ remoting/protocol/jingle_session.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef REMOTING_PROTOCOL_JINGLE_SESSION_H_
 #define REMOTING_PROTOCOL_JINGLE_SESSION_H_
 
+#include <memory>
+#include <optional>
 #include <string>
 #include <string_view>
 #include <vector>
@@ -92,23 +94,18 @@ class JingleSession : public Session {
   // Called by JingleSessionManager on incoming |message|. Must call
   // |reply_callback| to send reply message before sending any other
   // messages.
-  void OnIncomingMessage(std::unique_ptr<JingleMessage> message,
-                         ReplyCallback reply_callback);
+  void OnIncomingMessage(JingleMessage&& message, ReplyCallback reply_callback);
 
   // Called by OnIncomingMessage() to process the incoming Jingle messages
   // in the same order that they are sent.
-  void ProcessIncomingMessage(std::unique_ptr<JingleMessage> message,
+  void ProcessIncomingMessage(JingleMessage&& message,
                               ReplyCallback reply_callback);
 
   // Message handlers for incoming messages.
-  void OnAccept(std::unique_ptr<JingleMessage> message,
-                ReplyCallback reply_callback);
-  void OnSessionInfo(std::unique_ptr<JingleMessage> message,
-                     ReplyCallback reply_callback);
-  void OnTransportInfo(std::unique_ptr<JingleMessage> message,
-                       ReplyCallback reply_callback);
-  void OnTerminate(std::unique_ptr<JingleMessage> message,
-                   ReplyCallback reply_callback);
+  void OnAccept(JingleMessage&& message, ReplyCallback reply_callback);
+  void OnSessionInfo(JingleMessage&& message, ReplyCallback reply_callback);
+  void OnTransportInfo(JingleMessage&& message, ReplyCallback reply_callback);
+  void OnTerminate(JingleMessage&& message, ReplyCallback reply_callback);
   void OnAuthenticatorStateChangeAfterAccepted();
 
   // Called from OnAccept() to initialize session config. If initialization
@@ -169,11 +166,10 @@ class JingleSession : public Session {
   struct PendingMessage {
     PendingMessage();
     PendingMessage(PendingMessage&& moved);
-    PendingMessage(std::unique_ptr<JingleMessage> message,
-                   ReplyCallback reply_callback);
+    PendingMessage(JingleMessage message, ReplyCallback reply_callback);
     ~PendingMessage();
     PendingMessage& operator=(PendingMessage&& moved);
-    std::unique_ptr<JingleMessage> message;
+    JingleMessage message;
     ReplyCallback reply_callback;
   };
 
