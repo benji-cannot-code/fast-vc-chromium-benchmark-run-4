fcd5545ba375c52ae0ebd7ede3cfcd31d22a73e9
Mahesh Bharadwaj Kannan <kmaheshb@google.com>
2026-03-05 17:01:18 -0800

    Preserve source location in worklet exceptions
    When a worklet module script fails to parse, the
    resulting exception is serialized to cross a thread
    boundary. SerializedScriptValue only serializes the
    exception object  properties, causing the source location
    contained within the v8::Message to be lost. This change
    extracts the script resource name and line number from
    the v8::Message and appends them to the exception's
    "message" property prior to serialization, ensuring
    the location data survives the thread hop.
    
    Bug: 40284931
    Change-Id: Ic59de2fc42fa6a08d6dceb1b014eb804a2694045
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7574214
    Commit-Queue: Mahesh Kannan <kmaheshb@google.com>
    Reviewed-by: Michael Wilson <mjwilson@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1595053}

---
+++ third_party/blink/web_tests/webaudio/AudioWorkletProcessor/syntax-error-processor.js
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+class SyntaxErrorProcessor extends AudioWorkletProcessor {
+  constructor() {
+    super();
+    this.syntaxError = new .
+  }
+
+  process(inputs, outputs, parameters) {
+    return true;
+  }
+}
+
+registerProcessor('syntax-error-processor', SyntaxErrorProcessor);
