d4af2507f5e504b339d73881000c95a7f3c9e5ee
Kaan Alsan <alsan@chromium.org>
2026-03-06 12:53:52 -0800

    Report correct taskbar pinning channel for new default browser prompts
    We will start experimenting with new default browser prompts, which may
    trigger taskbar pinning too.
    
    Bug: 489793466
    Change-Id: Ie3858f92808f627f3d5daf6be640f5db506c8ca1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7643749
    Reviewed-by: Jesse McKenna <jessemckenna@google.com>
    Reviewed-by: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Reviewed-by: Atharv Maan <atharvmaan@chromium.org>
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1595635}

--- chrome/browser/win/taskbar_manager.cc
+++ chrome/browser/win/taskbar_manager.cc
@@ -58,6 +58,8 @@ constexpr std::array kChannels = {
     "SettingsPage",
     "PinWebApp",
     "DefaultBrowserBubbleDialog",
+    "DefaultBrowserModalDialogWithSettingsImage",
+    "DefaultBrowserModalDialogWithoutSettingsImage",
 };
 // LINT.ThenChange(//tools/metrics/histograms/metadata/windows/histograms.xml:PinAppToTaskbarChannel)
 
