5ca55185c8e7e03d06fc458b57f301402d288f74
Eshwar Stalin <estalin@chromium.org>
2026-03-05 20:41:05 -0800

    [Vertical Tabs] Fixing bugs in scroll to active tab feature
    There was another subtle issue where the scroll layout wasn't seen after
    tab activation. That was because we used a separate observation to
    observe tab activation change and that could sometimes not see the
    layout that happened when we observed the tab strip UI from the root
    node. So had to unify that.
    
    Additionally, there was a small visual issue where the scroll was done
    and the active tab was partially covered with a gradient mask. This was
    due to the fact that visible rect doesn't exclude this. So introduced a
    new function that returns opaque visible rect so we can use that to
    compute the scroll offset.
    
    Fixed: 489834411
    Change-Id: I08e780fb8a929c6f9650c76301173bffbafd9d03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7640711
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1595159}

--- chrome/browser/ui/views/tabs/vertical/vertical_tab_strip_view.h
+++ chrome/browser/ui/views/tabs/vertical/vertical_tab_strip_view.h
@@ -7,13 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_VIEWS_TABS_VERTICAL_VERTICAL_TAB_STRIP_VIEW_H_
 
 #include "base/memory/raw_ptr.h"
-#include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
+#include "components/tabs/public/tab_interface.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/layout/delegating_layout_manager.h"
 #include "ui/views/view.h"
 
 class TabCollectionNode;
-class TabStripModel;
 class VerticalPinnedTabContainerView;
 class VerticalUnpinnedTabContainerView;
 
@@ -28,8 +27,7 @@ class ViewTracker;
 // regions and associates them to their scroll views. It also is responsible for
 // scrolling to the active tab view when the active tab changes.
 class VerticalTabStripView final : public views::View,
-                                   public views::LayoutDelegate,
-                                   public TabStripModelObserver {
+                                   public views::LayoutDelegate {
   METADATA_HEADER(VerticalTabStripView, views::View)
 
  public:
@@ -50,8 +48,6 @@ class VerticalTabStripView final : public views::View,
 
   bool IsPositionInWindowCaption(const gfx::Point& point);
 
-  void InitializeTabStrip(TabStripModel& tab_strip_model);
-
   // LayoutDelegate:
   views::ProposedLayout CalculateProposedLayout(
       const views::SizeBounds& size_bounds) const override;
@@ -61,12 +57,7 @@ class VerticalTabStripView final : public views::View,
   void OnMouseEntered(const ui::MouseEvent& event) override;
   void OnMouseExited(const ui::MouseEvent& event) override;
 
-  // TabStripModelObserver:
-  void OnTabStripModelChanged(
-      TabStripModel* tab_strip_model,
-      const TabStripModelChange& change,
-      const TabStripSelectionChange& selection) override;
-
+  void OnActiveTabChanged(const tabs::TabInterface* active_tab);
   void RecordMousePressedInTab();
 
  private:
