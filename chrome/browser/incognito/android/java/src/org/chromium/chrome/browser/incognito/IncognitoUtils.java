e0fa9a267be6b47eb07af56757a53c8a0c62f2ea
Shu Yang <shuyng@google.com>
2026-03-06 14:03:16 -0800

    Disable incognito window feature on small tablets
    This also exclude legacy foldables where DeviceInfo.isFoldable() is not
    reliable.
    
    Bug: 484151862
    Change-Id: If28e611320ff823094b8950bff4f474f6b344528
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7630176
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Shu Yang <shuyng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1595688}

--- chrome/browser/incognito/android/java/src/org/chromium/chrome/browser/incognito/IncognitoUtils.java
+++ chrome/browser/incognito/android/java/src/org/chromium/chrome/browser/incognito/IncognitoUtils.java
@@ -29,6 +29,7 @@ import org.chromium.ui.display.DisplayUtil;
 /** Utilities for working with incognito tabs spread across multiple activities. */
 @NullMarked
 public class IncognitoUtils {
+    private static final double LARGE_DIAGONAL_DISPLAY_THRESHOLD_INCHES = 8.0;
     private static @Nullable Boolean sIsEnabledForTesting;
     private static @Nullable Boolean sShouldOpenIncognitoAsWindowForTesting;
 
@@ -94,9 +95,14 @@ public class IncognitoUtils {
             return false;
         }
         if (BuildConfig.IS_FOR_TEST) {
+            // The feature should be ON for Android Desktop.
+            // The screen size check is not reliable on Android Desktop emulator.
             sShouldOpenIncognitoAsWindowForTesting =
-                    ThreadUtils.runOnUiThreadBlocking(
-                            DisplayUtil::isGlobalDefaultDisplayTabletSized);
+                    DeviceInfo.isDesktop()
+                            || ThreadUtils.runOnUiThreadBlocking(
+                                    () ->
+                                            DisplayUtil.isGlobalDefaultDisplayWithMinDiagonal(
+                                                    LARGE_DIAGONAL_DISPLAY_THRESHOLD_INCHES));
             return sShouldOpenIncognitoAsWindowForTesting;
         }
 
@@ -105,7 +111,8 @@ public class IncognitoUtils {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S_V2) {
             return false;
         }
-        return DisplayUtil.isGlobalDefaultDisplayTabletSized();
+        return DisplayUtil.isGlobalDefaultDisplayWithMinDiagonal(
+                LARGE_DIAGONAL_DISPLAY_THRESHOLD_INCHES);
     }
 
     /**
