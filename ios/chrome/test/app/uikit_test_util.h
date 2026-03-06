8d7788f8fe295052d5f5557e81df5317ca3ae0e4
Asami Doi <asamidoi@chromium.org>
2026-03-06 04:18:39 -0800

    [ios] Add util function to return an UIWindowScene
    This util function will be used in follow-up CLs to replace
    initWithFrame: with initWithWindowScene:.
    
    Bug: 489089329
    Change-Id: I52bca86e89c5540f70f07851dc425a59559895ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7628404
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1595313}

---
+++ ios/chrome/test/app/uikit_test_util.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_TEST_APP_UIKIT_TEST_UTIL_H_
+#define IOS_CHROME_TEST_APP_UIKIT_TEST_UTIL_H_
+
+#import <UIKit/UIKit.h>
+
+namespace chrome_test_util {
+
+// Returns the UIWindowScene found from the list of connected scenes. Used only
+// for testing.
+UIWindowScene* GetAnyWindowScene();
+
+}  // namespace chrome_test_util
+
+#endif  // IOS_CHROME_TEST_APP_UIKIT_TEST_UTIL_H_
---
+++ ios/chrome/test/app/uikit_test_util.mm
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/test/app/uikit_test_util.h"
+
+#import "base/apple/foundation_util.h"
+
+namespace chrome_test_util {
+
+UIWindowScene* GetAnyWindowScene() {
+  UIWindowScene* scene = nil;
+  for (UIScene* connectedScene in UIApplication.sharedApplication
+           .connectedScenes) {
+    scene = base::apple::ObjCCast<UIWindowScene>(connectedScene);
+    if (scene) {
+      return scene;
+    }
+  }
+
+  return nil;
+}
+
+}  // namespace chrome_test_util
