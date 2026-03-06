8854a3348dfe1ab08d431126538db721430fc58d
Helmut Januschka <helmut@januschka.com>
2026-03-06 13:53:46 -0800

    Add a kill switch for dark-mode explicit outline-color focus rings
    Guard the fix behind an enabled-by-default runtime feature flag so it can be remotely disabled if regressions appear.
    
    Bug: 480978106,489304736
    Change-Id: Ib52eed3ee53f2416604348b2cecfcbe7e0bb34ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7644568
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Helmut Januschka <helmut@januschka.com>
    Cr-Commit-Position: refs/heads/main@{#1595680}

--- third_party/blink/renderer/core/paint/outline_painter.cc
+++ third_party/blink/renderer/core/paint/outline_painter.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/graphics/paint/drawing_recorder.h"
 #include "third_party/blink/renderer/platform/graphics/skia/skia_utils.h"
 #include "third_party/blink/renderer/platform/graphics/styled_stroke_data.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/skia/include/core/SkPath.h"
 #include "third_party/skia/include/core/SkPathBuilder.h"
 #include "ui/gfx/geometry/point_conversions.h"
@@ -857,6 +858,13 @@ void PaintFocusRing(GraphicsContext& context,
                     const FloatRoundedRect::Radii& corner_radii,
                     const LayoutObject::OutlineInfo& info) {
   Color inner_color = style.VisitedDependentColor(GetCSSPropertyOutlineColor());
+#if !BUILDFLAG(IS_MAC)
+  if (style.DarkColorScheme() &&
+      !RuntimeEnabledFeatures::
+          FocusRingRespectExplicitOutlineColorInDarkModeEnabled()) {
+    inner_color = Color::kWhite;
+  }
+#endif
 
   const float outer_ring_width = FocusRingOuterStrokeWidth(style);
   const float inner_ring_width = FocusRingInnerStrokeWidth(style);
