bd80dc64a91702b567112c0188d82bd3930a41de
Marc Treib <treib@chromium.org>
2026-03-06 08:12:02 -0800

    Sync: Populate PreconditionContext with account managed status
    This CL adds the "account managed status" (whether the syncing account
    is a consumer or enterprise account) to PreconditionContext, which gets
    passed to DataTypeController::GetPreconditionState().
    
    So far, none of the controllers use this state yet, but that will be
    added soon in follow-up CLs.
    
    Bug: 40897778
    Change-Id: If87a0bd37fb47ef2e3d30ae175207c0f6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7642051
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Liza Bipin <mlbipin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1595432}

--- components/supervised_user/core/browser/family_link_settings_data_type_controller_unittest.cc
+++ components/supervised_user/core/browser/family_link_settings_data_type_controller_unittest.cc
@@ -38,9 +38,11 @@ TEST_F(FamilyLinkSettingsDataTypeControllerTest,
       /*dump_stack=*/base::DoNothing(),
       /*store_factory=*/base::DoNothing(),
       /*syncable_service=*/nullptr, &pref_service_);
-  EXPECT_EQ(DataTypeController::PreconditionState::kPreconditionsMet,
-            controller.GetPreconditionState(
-                syncer::DataTypeController::PreconditionContext()));
+  EXPECT_EQ(
+      DataTypeController::PreconditionState::kPreconditionsMet,
+      controller.GetPreconditionState(
+          syncer::DataTypeController::PreconditionContext(
+              signin::AccountManagedStatusFinderOutcome::kConsumerGmail)));
 }
 
 TEST_F(FamilyLinkSettingsDataTypeControllerTest,
@@ -49,9 +51,11 @@ TEST_F(FamilyLinkSettingsDataTypeControllerTest,
       /*dump_stack=*/base::DoNothing(),
       /*store_factory=*/base::DoNothing(),
       /*syncable_service=*/nullptr, &pref_service_);
-  EXPECT_EQ(DataTypeController::PreconditionState::kMustStopAndClearData,
-            controller.GetPreconditionState(
-                syncer::DataTypeController::PreconditionContext()));
+  EXPECT_EQ(
+      DataTypeController::PreconditionState::kMustStopAndClearData,
+      controller.GetPreconditionState(
+          syncer::DataTypeController::PreconditionContext(
+              signin::AccountManagedStatusFinderOutcome::kConsumerGmail)));
 }
 
 TEST_F(FamilyLinkSettingsDataTypeControllerTest, HasTransportModeDelegate) {
