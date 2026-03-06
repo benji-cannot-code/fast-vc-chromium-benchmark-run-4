e7e1b7ab096bf343dcd51c786a733319128b9795
Tarek Makkouk <tarekmakkouk@google.com>
2026-03-06 15:16:43 -0800

    [iOS] Add metric for Gemini page eligibility
    Bug: 488011082
    Change-Id: I36d60c2f51832864aeb1107511962e43652e76b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7617293
    Commit-Queue: Tarek Makkouk <tarekmakkouk@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Reviewed-by: Sebastien S-G <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1595743}

--- ios/chrome/browser/intelligence/bwg/model/bwg_tab_helper.mm
+++ ios/chrome/browser/intelligence/bwg/model/bwg_tab_helper.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/functional/callback_helpers.h"
 #import "base/ios/block_types.h"
 #import "base/memory/weak_ptr.h"
+#import "base/metrics/histogram_functions.h"
 #import "base/strings/string_number_conversions.h"
 #import "base/strings/sys_string_conversions.h"
 #import "base/strings/utf_string_conversions.h"
@@ -450,6 +451,9 @@ void BwgTabHelper::DidStartNavigation(
   const bool gemini_available = IsGeminiAvailableForWebState() &&
                                 gemini_service &&
                                 gemini_service->IsProfileEligibleForGemini();
+
+  base::UmaHistogramBoolean("IOS.Gemini.PageEligible", gemini_available);
+
   if (gemini_available &&
       profile->GetPrefs()->GetBoolean(prefs::kIOSBWGPageContentSetting)) {
     bool can_request_metadata =
