a84d256498a8a5369ef93705b2f524b1c9b754b3
Eriko Kurimoto <elkurin@chromium.org>
2026-03-05 20:38:12 -0800

    WaaP: Skip extension initialization for per-navigation
    This CL lets extension initialization skip for our webui toolbar navigation.
    
    Bug: 483888399, 486078938
    Change-Id: I25098837ab2055cd7cb7f39ae2f2e7958843cc3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7588414
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Reviewed-by: Andrea Orru <andreaorru@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1595156}

--- extensions/browser/extension_web_contents_observer.cc
+++ extensions/browser/extension_web_contents_observer.cc
@@ -6,12 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_web_contents_observer.h"
 
 #include "base/check.h"
+#include "base/feature_list.h"
 #include "components/sessions/content/session_tab_helper.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/child_process_security_policy.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
+#include "content/public/browser/site_instance.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/url_constants.h"
 #include "extensions/browser/extension_api_frame_id_map.h"
@@ -31,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/extension_id.h"
 #include "extensions/common/mojom/view_type.mojom.h"
 #include "third_party/blink/public/common/associated_interfaces/associated_interface_provider.h"
+#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/mojom/autoplay/autoplay.mojom.h"
 #include "url/origin.h"
 
@@ -206,6 +209,22 @@ void ExtensionWebContentsObserver::RenderFrameDeleted(
 
 void ExtensionWebContentsObserver::ReadyToCommitNavigation(
     content::NavigationHandle* navigation_handle) {
+#if !BUILDFLAG(IS_ANDROID)
+  // If the navigation is for the initial WebUI, we can skip extension
+  // initialization for optimization. Otherwise, we ensure the renderer process
+  // is initialized including the non-initial WebUI navigation.
+  content::RenderProcessHost* process =
+      navigation_handle->GetRenderFrameHost()->GetProcess();
+  if (process->IsForInitialWebUI() &&
+      base::FeatureList::IsEnabled(
+          blink::features::kInitialWebUIWithoutExtensions)) {
+    if (!navigation_handle->IsInitialWebUINavigation()) {
+      RendererStartupHelperFactory::GetForBrowserContext(browser_context_)
+          ->InitializeProcess(process);
+    }
+  }
+#endif  // !BUILDFLAG(IS_ANDROID)
+
   SetUpRenderFrameHost(navigation_handle->GetRenderFrameHost());
 
   ScriptInjectionTracker::ReadyToCommitNavigation(PassKey(), navigation_handle);
