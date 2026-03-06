f543ec80d4368e38140b6796adfe58e49fb1dfcf
Yuke Liao <liaoyuke@google.com>
2026-03-05 17:32:29 -0800

    Improve crash reports processing script output
    This CL improves the output formatting to make the output look nicer.
    
    Bug: 479224195
    Change-Id: I2232fab7456c206d1aebae3f14c2c0571579354f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7620618
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Yuke Liao <liaoyuke@google.com>
    Cr-Commit-Position: refs/heads/main@{#1595064}

--- tools/clang/scripts/process_crashreports.py
+++ tools/clang/scripts/process_crashreports.py
@@ -45,7 +45,7 @@ def FetchRbeCrashReports():
           logs[0])
 
   log = logs[0]
-  print('processing %s... ' % log, end='', flush=True)
+  print('processing %s...' % log)
   commands = []
   # Siso logs auxiliary outputs in the format: path <tab> digest <tab> command
   pattern = re.compile(r'out/clang-crashreports/.*?\t[0-9a-fA-F]+/\d+\t(.*)')
@@ -67,7 +67,6 @@ def FetchRbeCrashReports():
     os.makedirs(CRASHREPORTS_DIR)
 
   for cmd in commands:
-    print('running %s... ' % cmd, end='', flush=True)
     try:
       # The command string is like: siso fetch -reapi_instance=... ...
       # We want to run it with the correctly resolved SISO_BINARY.
@@ -79,6 +78,7 @@ def FetchRbeCrashReports():
         sys.stderr.write('Expected siso command, got %s\n' % args[0])
         continue
 
+      print('running %s' % ' '.join(args))
       subprocess.check_call(args)
       print('done')
     except subprocess.CalledProcessError:
