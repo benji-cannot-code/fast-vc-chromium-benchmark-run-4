aa1931121b0a701b5128f389568136a2fece63d3
mingming <mingming1.xu@intel.com>
2026-03-05 17:50:41 -0800

    WebNN: Record the first selected EP and device histograms for ORT
    This CL adds two histograms to record the first selected execution
    provider (EP) and its corresponding hardware device type in the WebNN
    ORT backend. This allows us to understand the distribution of EPs and
    devices used by WebNN ORT backend.
    
    This CL also renames `kCpuExecutionProvider` in
    `execution_providers_info.h` to `kCPUExecutionProvider` to align with
    the underlying EP string "CPUExecutionProvider" and
    `WebNNOrtEPUma::kCPU`.
    
    Bug: 367755493, 454273264
    Change-Id: Ia0b10d1b4763e845e963f1438f01305f1081d542
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7607405
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Commit-Queue: Xu, Mingming1 <mingming1.xu@intel.com>
    Reviewed-by: Hu, Ningxin <ningxin.hu@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1595075}

--- services/webnn/public/cpp/execution_providers_info.h
+++ services/webnn/public/cpp/execution_providers_info.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace webnn {
 
-inline constexpr base::cstring_view kCpuExecutionProvider =
+inline constexpr base::cstring_view kCPUExecutionProvider =
     "CPUExecutionProvider";
 inline constexpr base::cstring_view kDmlExecutionProvider =
     "DmlExecutionProvider";
