5b2b1945207f2f4c648d28d8c1c4d3704d3eb0ff
Danil Somsikov <dsv@chromium.org>
2026-03-06 10:13:20 -0800

    Remove Audits.checkContrast DevTools protocol command
    This change removes the `Audits.checkContrast` command from the DevTools protocol. This command was used to run contrast checks and report low text contrast issues. The implementation in `InspectorAuditsAgent` and the related web tests are also removed.
    
    Bug: 476073409
    Change-Id: Ib7f1848cafd19ed4eec31b8357048b684bc0731a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7639989
    Auto-Submit: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1595476}

--- third_party/blink/renderer/core/inspector/inspector_audits_agent.h
+++ third_party/blink/renderer/core/inspector/inspector_audits_agent.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/inspector/inspected_frames.h"
 #include "third_party/blink/renderer/core/inspector/inspector_base_agent.h"
-#include "third_party/blink/renderer/core/inspector/inspector_contrast.h"
 #include "third_party/blink/renderer/core/inspector/protocol/audits.h"
 
 namespace blink {
@@ -40,7 +39,6 @@ class CORE_EXPORT InspectorAuditsAgent final
   // Protocol methods.
   protocol::Response enable() override;
   protocol::Response disable() override;
-  protocol::Response checkContrast(std::optional<bool> report_aaa) override;
   protocol::Response checkFormsIssues(
       std::unique_ptr<protocol::Array<protocol::Audits::GenericIssueDetails>>*
           out_formIssues) override;
@@ -58,7 +56,6 @@ class CORE_EXPORT InspectorAuditsAgent final
 
  private:
   void InnerEnable();
-  void CheckContrastForDocument(Document* document, bool report_aaa);
 
   InspectorIssueStorage* const inspector_issue_storage_;
   InspectorAgentState::Boolean enabled_;
