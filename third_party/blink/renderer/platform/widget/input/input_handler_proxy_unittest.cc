075329b40cf59e1372723b68129e36149fb08bbc
Aman Verma <amanvr@google.com>
2026-03-06 07:41:58 -0800

    [ScrollPredictor] Fix scheduler stall for deferred scroll events
    When the `UpdateScrollPredictorInputMapping` feature is enabled, input
    events arriving after the resampling sample time (VSync - 5ms) are
    deferred to the next frame. Previously, the scheduler could transition
    to an IDLE state (stopping BeginFrame observation) if it believed all
    "ready" events were processed, even if "future" events remained in the
    queue. This caused a deadlock where deferred events would sit in the
    `CompositorThreadEventQueue` because no new BeginFrame was generated to
    process them.
    
    This change fixes the deadlock by ensuring that if the compositor event
    queue is not empty after dispatching a frame and the feature
    UpdateScrollPredictorInputMapping is enabled, we explicitly call
    `SetNeedsAnimateInput()` to request another frame from the scheduler to
    process the deferred events.
    
    Bug: 478824725
    Change-Id: Ifa0890aec6ffd9f83d6d57393f98c0799558ac16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7642724
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Aman Verma <amanvr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1595413}

--- third_party/blink/renderer/platform/widget/input/input_handler_proxy_unittest.cc
+++ third_party/blink/renderer/platform/widget/input/input_handler_proxy_unittest.cc
@@ -1432,7 +1432,8 @@ TEST_P(InputHandlerProxyEventQueueTest, EmptyGestureScrollUpdateHistogram) {
         .WillOnce(testing::Return(scroll_result_did_scroll_));
   }
 
-  EXPECT_CALL(mock_input_handler_, SetNeedsAnimateInput()).Times(2);
+  EXPECT_CALL(mock_input_handler_, SetNeedsAnimateInput())
+      .Times(testing::AnyNumber());
 
   HandleGestureEvent(WebInputEvent::Type::kGestureScrollBegin);
   DeliverInputForBeginFrame();
@@ -3875,7 +3876,8 @@ TEST_P(InputHandlerProxyEventQueueTest, FutureEventDispatch) {
       WebInputEvent::Type::kGestureScrollUpdate, WebGestureDevice::kTouchscreen,
       future_event_time, -10);
 
-  EXPECT_CALL(mock_input_handler_, SetNeedsAnimateInput()).Times(1);
+  EXPECT_CALL(mock_input_handler_, SetNeedsAnimateInput())
+      .Times(testing::AnyNumber());
   InjectInputEvent(std::move(future_event));
   EXPECT_EQ(1ul, event_queue().size());
   Mock::VerifyAndClearExpectations(&mock_input_handler_);
@@ -3893,6 +3895,8 @@ TEST_P(InputHandlerProxyEventQueueTest, FutureEventDispatch) {
   } else {
     // With kUpdateScrollPredictorInputMapping, the future event is deferred.
     EXPECT_CALL(mock_input_handler_, ScrollUpdate(_, _)).Times(0);
+    EXPECT_CALL(mock_input_handler_, SetNeedsAnimateInput())
+        .Times(testing::AnyNumber());
     DeliverInputForBeginFrame(tick_clock.NowTicks());
     EXPECT_EQ(1ul, event_queue().size());
   }
