6f4e84e5d0794189acb28cc01fdc595381fb808c
Jinsuk Kim <jinsukkim@chromium.org>
2026-03-06 10:12:26 -0800

    [MultiColumn] Fix the disabled backpress in Language
    A bug snuck in at crrev.com/c/7638951 that stops handling backpress
    after processing it once for search view. For the subsequent backpress
    to work, this CL removes the added BackPressCallback.
    
    Bug: 483009870
    Change-Id: Id91fdcf140d8156679e102da690161b28a7cb8be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7642713
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1595475}

--- chrome/browser/language/android/java/src/org/chromium/chrome/browser/language/settings/SelectLanguageFragment.java
+++ chrome/browser/language/android/java/src/org/chromium/chrome/browser/language/settings/SelectLanguageFragment.java
@@ -123,6 +123,8 @@ public class SelectLanguageFragment extends Fragment
     private final SettableMonotonicObservableSupplier<String> mPageTitle =
             ObservableSuppliers.createMonotonic();
 
+    private @Nullable OnBackPressedCallback mBackPressCallback;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -232,23 +234,26 @@ public class SelectLanguageFragment extends Fragment
                     }
                 });
 
-        var callback =
+        mBackPressCallback =
                 new OnBackPressedCallback(true) {
                     @Override
                     public void handleOnBackPressed() {
                         if (!assumeNonNull(mSearchView).isIconified()) {
                             KeyboardUtils.hideAndroidSoftKeyboard(mSearchView);
+                            mSearchView.clearFocus();
                         } else {
                             // If search is already closed, disable this callback and
                             // let the Activity handle the back press (e.g., exit the fragment).
                             setEnabled(false);
                             requireActivity().onBackPressed();
                         }
+                        assumeNonNull(mBackPressCallback).remove();
+                        mBackPressCallback = null;
                     }
                 };
         requireActivity()
                 .getOnBackPressedDispatcher()
-                .addCallback(getViewLifecycleOwner(), callback);
+                .addCallback(getViewLifecycleOwner(), mBackPressCallback);
     }
 
     @Override
@@ -260,6 +265,7 @@ public class SelectLanguageFragment extends Fragment
     public void onDestroy() {
         super.onDestroy();
         if (mSearchViewObserver != null) mSearchViewObserver.onUpdated(false);
+        if (mBackPressCallback != null) mBackPressCallback.remove();
     }
 
     @Override
