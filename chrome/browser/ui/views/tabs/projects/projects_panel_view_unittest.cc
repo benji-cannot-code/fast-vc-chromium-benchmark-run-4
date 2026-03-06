2993204fc43324cebfd2b794548ba0bbfb9b41f1
Chase Hartsell <chazzy@google.com>
2026-03-06 13:05:40 -0800

    [projects] Auto-scroll tab groups as a dragged item is held near an edge
    Adds a TabGroupsDragScrollHandler which scrolls the tab groups'
    ScrollView when a dragged group is held near the top or bottom of the
    list.
    
    Also moves the "Create new tab group" button to live within the higher
    ProjectsPanelView so it isn't within the ScrollView.
    
    Demo: http://go/scrcast/NTk2MDMwMzg3ODYwMjc1Mnw4MzJhOGM1MS1mYw
    
    Bug: 481732680
    Change-Id: I9a9cd22ebd702f65efc5a505b433c7c782826b1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7640257
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Chase Hartsell <chazzy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1595645}

--- chrome/browser/ui/views/tabs/projects/projects_panel_view_unittest.cc
+++ chrome/browser/ui/views/tabs/projects/projects_panel_view_unittest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/tab_group_sync/tab_group_sync_service_factory.h"
 #include "chrome/browser/ui/actions/chrome_action_id.h"
+#include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/browser/ui/browser_window/test/mock_browser_window_interface.h"
 #include "chrome/browser/ui/tabs/projects/projects_panel_state_controller.h"
 #include "chrome/test/base/testing_profile.h"
@@ -32,7 +33,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/actions/actions.h"
 #include "ui/base/unowned_user_data/unowned_user_data_host.h"
 #include "ui/compositor/layer.h"
+#include "ui/views/controls/button/button.h"
+#include "ui/views/test/button_test_api.h"
 #include "ui/views/test/views_test_base.h"
+#include "ui/views/view_utils.h"
 
 class ProjectsPanelViewTest : public ChromeViewsTestBase {
  public:
