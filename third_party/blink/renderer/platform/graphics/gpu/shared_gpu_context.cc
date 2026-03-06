7e01f59364b01ddff31e565e8907e13f54b4b845
Colin Blundell <blundell@chromium.org>
2026-03-06 07:26:12 -0800

    [Blink] Add wrapper around RuntimeEnabledFeatures::WebGLImageChromium
    Will remove the runtime enabled feature in followup.
    
    Bug: 488937356
    Change-Id: I48c1de40bcfa3f0e2c2b427c933013a545d4d6e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7638651
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1595406}

--- third_party/blink/renderer/platform/graphics/gpu/shared_gpu_context.cc
+++ third_party/blink/renderer/platform/graphics/gpu/shared_gpu_context.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/web_graphics_context_3d_provider.h"
 #include "third_party/blink/public/platform/web_url.h"
 #include "third_party/blink/renderer/platform/graphics/gpu/webgraphics_shared_image_interface_provider_impl.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/scheduler/public/main_thread.h"
 #include "third_party/blink/renderer/platform/scheduler/public/post_cross_thread_task.h"
 #include "third_party/blink/renderer/platform/wtf/bind_post_task.h"
@@ -435,6 +436,10 @@ bool SharedGpuContext::LowLatencyUsageSupportedForCanvas2D(
   // IsDelegatedCompositingEnabled() holds.
   return base::FeatureList::IsEnabled(
       features::kLowLatencyCanvas2dImageChromium);
+}
+
+bool SharedGpuContext::WebGLImageChromiumEnabled() {
+  return RuntimeEnabledFeatures::WebGLImageChromiumEnabled();
 }
 
 }  // namespace blink
