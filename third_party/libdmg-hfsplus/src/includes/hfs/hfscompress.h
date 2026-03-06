1b85437d03c875f848f134cb1563a182c39b38bb
Adam Norberg <norberg@google.com>
2026-03-05 16:48:07 -0800

    Set an auto-open folder in libdmg-hfsplus
    A separate patch will use this in the Chrome packaging script.
    
    Bug: 435246349
    Change-Id: I41e0c1d7b0ba02e6f2c0bfe875d23221272377e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7635459
    Reviewed-by: Noah Rose Ledesma <noahrose@google.com>
    Commit-Queue: Adam Norberg <norberg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1595049}

--- third_party/libdmg-hfsplus/src/includes/hfs/hfscompress.h
+++ third_party/libdmg-hfsplus/src/includes/hfs/hfscompress.h
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define HFSCOMPRESS_H
 
 #include <stdint.h>
+#include "hfsplus.h"
 #include "common.h"
 
 #define CMPFS_MAGIC 0x636D7066
