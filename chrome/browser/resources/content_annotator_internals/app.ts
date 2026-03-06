6e5405b4eb837f343879fc7442414ccad05ee2ee
Qiao Li <qiaoli@google.com>
2026-03-06 11:27:53 -0800

    Add a new chrome://content-annotator-internals WebUI page.
    It includes the necessary BUILD files, resource files and the C++ WebUI
    controller. The page currently displays a simple message passed from the
    C++ backend.
    
    sample page: https://screenshot.googleplex.com/7Rtedep3mimiFR3
    
    Bug: b:488160244
    Change-Id: I8adca49b6e88727108f21af40cc4dfafa81f704d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7615509
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Reviewed-by: Andrew Vuong <akvuong@google.com>
    Commit-Queue: Qiao Li <qiaoli@google.com>
    Cr-Commit-Position: refs/heads/main@{#1595566}

---
+++ chrome/browser/resources/content_annotator_internals/app.ts
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import '/strings.m.js';
+
+import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
+import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
+
+import {getCss} from './app.css.js';
+import {getHtml} from './app.html.js';
+
+export class ContentAnnotatorInternalsAppElement extends CrLitElement {
+  static get is() {
+    return 'content-annotator-internals-app';
+  }
+
+  static override get styles() {
+    return getCss();
+  }
+
+  override render() {
+    return getHtml.bind(this)();
+  }
+
+  static override get properties() {
+    return {
+      message_: {type: String},
+    };
+  }
+
+  protected accessor message_: string = loadTimeData.getString('message');
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'content-annotator-internals-app': ContentAnnotatorInternalsAppElement;
+  }
+}
+
+customElements.define(
+    ContentAnnotatorInternalsAppElement.is,
+    ContentAnnotatorInternalsAppElement);
--- tools/gritsettings/resource_ids.spec
+++ tools/gritsettings/resource_ids.spec
@@ -390,6 +390,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     "META": {"sizes": {"includes": [15]}},
     "includes": [3760],
   },
+  "<(SHARED_INTERMEDIATE_DIR)/chrome/browser/resources/content_annotator_internals/resources.grd": {
+    "META": {"sizes": {"includes": [10]}},
+    "includes": [3765],
+  },
   "<(SHARED_INTERMEDIATE_DIR)/chrome/browser/resources/contextual_tasks/resources.grd": {
     "META": {"sizes": {"includes": [50]}},
     "includes": [3770],
