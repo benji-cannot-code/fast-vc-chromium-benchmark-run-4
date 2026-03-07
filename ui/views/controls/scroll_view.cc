79628a4870b918a2cf957c1b12bc5fb97fab3618
Eshwar Stalin <estalin@chromium.org>
2026-03-06 17:19:30 -0800

    [Vertical Tabs] Unpinned tabs shift when activating tab
    There are cases where the values for gradient visibility is incorrect
    when the scroll view isn't visible and this results in us computing the
    visible opaque rect incorrectly causing us to scroll when we shouldn't
    be. This was a regression caused from the previous CL to improve the
    scroll on activating tab feature.
    
    Fixed: 490479500
    Change-Id: I7ef534b126f84d24a3ce84876664c1cff1e1f0f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7640667
    Reviewed-by: Vince Lugli <lugli@google.com>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1595821}

--- ui/views/controls/scroll_view.cc
+++ ui/views/controls/scroll_view.cc
@@ -1516,6 +1516,8 @@ void ScrollView::UpdateOverflowIndicatorVisibility(const gfx::PointF& offset) {
     UpdateGradientMask();
   } else if (layer() && layer()->HasGradientMask()) {
     layer()->SetGradientMask(gfx::LinearGradient::GetEmpty());
+    is_leading_gradient_visible_ = false;
+    is_trailing_gradient_visible_ = false;
   }
 }
 
