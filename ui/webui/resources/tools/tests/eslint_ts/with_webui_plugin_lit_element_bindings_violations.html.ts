fc520eb2ce9862d1668227ecf8252f19b48baa91
rbpotter <rbpotter@chromium.org>
2026-03-06 18:44:13 -0800

    WebUI: Add a check for incorrect bindings
    - Check that attribute data bindings are not used to bind to Array or
      Object properties.
    - Check that boolean data bindings are only used to bind to Boolean
      properties.
    
    Note: does not cover the case where a data binding is to a property
    that appears to be missing from the properties() getter, because such
    properties may be defined in a mixin, which may not be part of the
    same group of source files.
    
    Bug: 490022552
    Change-Id: I3a84907a49d54096de06ee6d81e4c22852ee674b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7635460
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1595853}

---
+++ ui/webui/resources/tools/tests/eslint_ts/with_webui_plugin_lit_element_bindings_violations.html.ts
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Test file for @webui-eslint/lit-element-expressions
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {DummyElement} from './with_webui_plugin_lit_element_bindings_violations.js';
+
+export function getHtml(this: DummyElement) {
+  return html`
+<cr-input type=number value="${this.value}" aria-label="${this.label}"
+    aria-description="${this.description}"
+    ?disabled="${this.disabled}" error-message="${this.errorMessage}"
+    min="${this.limits.min}" max="${this.limits.max}"
+    ?invalid="${this.errorMessage}">
+</cr-input>
+`;
+}
