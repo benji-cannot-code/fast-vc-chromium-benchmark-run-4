61466f9332bc3056b561ba5c54567cd4dbc7601b
James Cook <jamescook@chromium.org>
2026-03-06 17:57:58 -0800

    Fix extensions ifdefs in ChromeWebUIControllerFactory
    For extensions code that is supported on desktop Android, we change
    the if guards from ENABLE_EXTENSIONS to ENABLE_EXTENSIONS_CORE.
    Do this in ChromeWebUIControllerFactory for some favicon code.
    
    Also clean up unused includes.
    
    Bug: 469417243
    Change-Id: I5963337282dbeafea737ffc66f6878cb4b4b4928
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7640254
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1595838}

--- chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
+++ chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
@@ -129,19 +129,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/whats_new/whats_new_ui.h"
 #endif
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
-#include "chrome/browser/extensions/extension_url_overrides.h"
-#include "extensions/browser/extension_registry.h"  // nogncheck
-#include "extensions/browser/extension_system.h"    // nogncheck
-#include "extensions/common/constants.h"
-#include "extensions/common/extension.h"
-#include "extensions/common/feature_switch.h"
-#include "extensions/common/manifest.h"
-#endif
-
 #if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
+#include "chrome/browser/extensions/extension_url_overrides.h"
 #include "chrome/browser/ui/webui/extensions/extensions_ui.h"
-#endif
+#include "extensions/common/constants.h"
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 
 using content::WebUI;
 using content::WebUIController;
@@ -286,7 +278,7 @@ void ChromeWebUIControllerFactory::GetFaviconForURL(
   // overrides. This changes urls in |kChromeUIScheme| to extension urls, and
   // allows to use ExtensionUrlOverrides::GetFaviconForURL.
   GURL url(page_url);
-#if BUILDFLAG(ENABLE_EXTENSIONS)
+#if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
   ExtensionUrlOverrides::HandleChromeURLOverride(&url, profile);
 
   // All extensions get their favicon from the icons part of the manifest.
@@ -294,7 +286,7 @@ void ChromeWebUIControllerFactory::GetFaviconForURL(
     ExtensionUrlOverrides::GetFaviconForURL(profile, url, std::move(callback));
     return;
   }
-#endif
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 
   std::vector<favicon_base::FaviconRawBitmapResult> favicon_bitmap_results;
 
