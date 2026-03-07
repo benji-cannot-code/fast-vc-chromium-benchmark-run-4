bcd68b5995a4f16222cdc207c207e7a5928db705
Scott Haseley <shaseley@chromium.org>
2026-03-07 14:08:22 -0800

    TaskAttribution: Instrument NavigateEvent.intercept() handlers
    Propagate task state from NavigationEvent.intercept() to the `handler`
    and `precommitHandler`. This is needed for crrev.com/c/7638455 which
    changes how the SoftNavigationContext task state variable is set,
    wrapping individual events (including navigate) instead of creating a
    TaskScope associated with the same-document navigation IPC task.
    
    This is a bit different from other propagation in that we have to
    ignore the is-JavaScript-running "top level" because of how deeply
    nested the handler dispatch is under a ScriptState::Scope, which breaks
    out top-level check. To work around this, this CL introduced a top-level
    override scope -- pass-keyed to NavigateEvent -- to ignore the top-
    level check when propagating state. crbug.com/490536691 tracks improving
    this. We only use this with UserNavigationInvolvement::kNone, since
    the current task state should continue to be used if JS is executing.
    
    Bug: 418007230, 490536691
    Change-Id: I337afcc951b2588faa489c8139dad5ce24518ca3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7645691
    Reviewed-by: Michal Mocny <mmocny@chromium.org>
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1595990}

--- third_party/blink/renderer/core/navigation_api/navigate_event.cc
+++ third_party/blink/renderer/core/navigation_api/navigate_event.cc
@@ -44,6 +44,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/navigation_api/navigation_destination.h"
 #include "third_party/blink/renderer/core/navigation_api/navigation_precommit_controller.h"
 #include "third_party/blink/renderer/core/navigation_api/navigation_type_util.h"
+#include "third_party/blink/renderer/core/scheduler/task_attribution_top_level_override_scope.h"
+#include "third_party/blink/renderer/core/scheduler/task_attribution_util.h"
 #include "third_party/blink/renderer/core/timing/dom_window_performance.h"
 #include "third_party/blink/renderer/core/timing/responsiveness_metrics.h"
 #include "third_party/blink/renderer/platform/bindings/script_state.h"
@@ -55,6 +57,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "v8-function.h"
 
 namespace blink {
+
+namespace {
+TaskAttributionTopLevelOverrideScope::Type GetTaskAttributionScopeType(
+    NavigateEventDispatchParams* params) {
+  // The current task state should be used (i.e. not overridden) if the
+  // navigation occurs during JS execution. Otherwise ensure the intercept()
+  // state is propagated to the handlers.
+  return params->involvement == UserNavigationInvolvement::kNone
+             ? TaskAttributionTopLevelOverrideScope::Type::kDoNotOverride
+             : TaskAttributionTopLevelOverrideScope::Type::kOverride;
+}
+}  // namespace
 
 enum class HandlerPhase { kPrecommit, kPostcommit };
 
@@ -182,6 +196,8 @@ void NavigateEvent::intercept(NavigationInterceptOptions* options,
           "navigate event is cancelable.");
       return;
     }
+    options->precommitHandler()->SetTaskState(
+        CaptureCurrentTaskState(DomWindow()));
     navigation_action_precommit_handlers_list_.push_back(
         options->precommitHandler());
   }
@@ -224,6 +240,7 @@ void NavigateEvent::intercept(NavigationInterceptOptions* options,
         intercept_state_ == InterceptState::kIntercepted);
   intercept_state_ = InterceptState::kIntercepted;
   if (options->hasHandler()) {
+    options->handler()->SetTaskState(CaptureCurrentTaskState(DomWindow()));
     navigation_action_handlers_list_.push_back(options->handler());
   }
 }
@@ -449,6 +466,9 @@ void NavigateEvent::MaybeCommitImmediately(ScriptState* script_state) {
 
   HeapVector<MemberScriptPromise<IDLUndefined>> precommit_promises_list;
   for (auto& function : handlers_list) {
+    TaskAttributionTopLevelOverrideScope override_scope(
+        DomWindow(), GetTaskAttributionScopeType(dispatch_params_),
+        TaskAttributionTopLevelOverrideScope::PassKeyType{});
     ScriptPromise<IDLUndefined> result;
     if (function->Invoke(this, controller).To(&result)) {
       precommit_promises_list.push_back(result);
@@ -632,6 +652,9 @@ void NavigateEvent::FinalizeNavigationActionPromisesList() {
 
   for (auto& function : handlers_list) {
     ScriptPromise<IDLUndefined> result;
+    TaskAttributionTopLevelOverrideScope override_scope(
+        DomWindow(), GetTaskAttributionScopeType(dispatch_params_),
+        TaskAttributionTopLevelOverrideScope::PassKeyType{});
     if (function->Invoke(this).To(&result))
       navigation_action_promises_list_.push_back(result);
   }
