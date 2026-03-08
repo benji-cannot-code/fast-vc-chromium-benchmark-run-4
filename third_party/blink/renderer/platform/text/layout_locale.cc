891a5710d9442a69c54780691334c6b73d43d17d
Fredrik Söderquist <fs@opera.com>
2026-03-08 16:52:11 -0700

    Use StringView in ComputeIsMacrolanguageChinese()
    Do the splitting of the language tag using a StringView, and then do the
    lookup in the set using IgnoringAsciiCaseHashTranslator.
    
    Let MacrolanguageChineseLanguageTags() return a const reference to the
    static set.
    
    Fix the typo in ToHarfbuzLanguage() while there.
    
    Bug: 473854537
    Change-Id: I2f7548297135bcd7f2916760f58cd3597ee67444
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7638184
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1596110}

--- third_party/blink/renderer/platform/text/layout_locale.cc
+++ third_party/blink/renderer/platform/text/layout_locale.cc
@@ -40,19 +40,17 @@ IgnoringCaseHashSet CreateMacrolanguageChineseLanguageTags() {
                              "mnp", "nan", "sjc", "wuu", "yue", "zh"};
 }
 
-IgnoringCaseHashSet MacrolanguageChineseLanguageTags() {
+const IgnoringCaseHashSet& MacrolanguageChineseLanguageTags() {
   DEFINE_THREAD_SAFE_STATIC_LOCAL(IgnoringCaseHashSet, tags,
                                   (CreateMacrolanguageChineseLanguageTags()));
   return tags;
 }
 
 bool ComputeIsMacrolanguageChinese(const String& value) {
-  const wtf_size_t separater = value.find('-');
-  if (separater == kNotFound) {
-    return MacrolanguageChineseLanguageTags().Contains(value);
-  }
-  const StringView language{value, 0, separater};
-  return MacrolanguageChineseLanguageTags().Contains(language.ToString());
+  StringView language_tag{value};
+  language_tag = language_tag.substr(0, value.find('-'));
+  return MacrolanguageChineseLanguageTags()
+      .Contains<IgnoringAsciiCaseHashTranslator>(language_tag);
 }
 
 struct PerThreadData {
@@ -154,7 +152,7 @@ inline const char* LbValueFromStrictness(LineBreakStrictness strictness) {
 
 }  // namespace
 
-static hb_language_t ToHarfbuzLanguage(const AtomicString& locale) {
+static hb_language_t ToHarfbuzzLanguage(const AtomicString& locale) {
   std::string locale_as_latin1 = locale.Latin1();
   return hb_language_from_string(locale_as_latin1.data(),
                                  static_cast<int>(locale_as_latin1.length()));
@@ -279,7 +277,7 @@ void LayoutLocale::ComputeCaseMapLocale() const {
 
 LayoutLocale::LayoutLocale(const AtomicString& locale)
     : string_(locale),
-      harfbuzz_language_(ToHarfbuzLanguage(locale)),
+      harfbuzz_language_(ToHarfbuzzLanguage(locale)),
       script_(LocaleToScriptCodeForFontSelection(locale)) {}
 
 // static
