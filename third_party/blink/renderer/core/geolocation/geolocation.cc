117c8b16462f547a694f533b6d8d631cca434710
Josh Karlin <jkarlin@chromium.org>
2026-03-09 12:47:50 -0700

    Remove unneeded use counters
    This reverts commit 7333e1cea0a91717af706e174502e6ccf1dd4e45 and
    5853fbbdbb276760c2dae3baf2a0d46eeefa2b22.
    
    A different implementation is now being used, so these monitoring
    hooks are no longer needed.
    
    NO_IFTTT=Both enums.xml and web_feature.mojom were updated in this CL.
    
    Bug: 384511645
    Change-Id: I9045e04bb3d729cd2e8e155f0424e9f42181452b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7638449
    Reviewed-by: Dan Clark <daniec@microsoft.com>
    Commit-Queue: Josh Karlin <jkarlin@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1596528}

--- third_party/blink/renderer/core/geolocation/geolocation.cc
+++ third_party/blink/renderer/core/geolocation/geolocation.cc
@@ -265,12 +265,6 @@ void Geolocation::getCurrentPositionForBindings(
   auto* notifier = MakeGarbageCollected<GeoNotifierV8>(
       this, options, success_callback, error_callback);
 
-  if (GetFrame()->IsAdScriptInStack()) {
-    notifier->SetCalledWithAdScriptInStack();
-    UseCounter::Count(GetExecutionContext(),
-                      WebFeature::kAdScriptInStackOnGeoLocation);
-  }
-
   one_shots_->insert(notifier);
 
   StartRequest(notifier);
@@ -314,11 +308,6 @@ int Geolocation::watchPositionForBindings(
   auto* notifier = MakeGarbageCollected<GeoNotifierV8>(
       this, options, success_callback, error_callback);
 
-  if (GetFrame()->IsAdScriptInStack()) {
-    notifier->SetCalledWithAdScriptInStack();
-    UseCounter::Count(GetExecutionContext(),
-                      WebFeature::kAdScriptInStackOnWatchGeoLocation);
-  }
   return WatchPositionInternal(notifier);
 }
 
--- third_party/blink/renderer/modules/bluetooth/bluetooth.cc
+++ third_party/blink/renderer/modules/bluetooth/bluetooth.cc
@@ -386,12 +386,6 @@ ScriptPromise<IDLSequence<BluetoothDevice>> Bluetooth::getDevices(
     return ScriptPromise<IDLSequence<BluetoothDevice>>();
   }
 
-  LocalFrame* frame = window->GetFrame();
-  if (frame && frame->IsAdScriptInStack()) {
-    UseCounter::Count(GetExecutionContext(),
-                      WebFeature::kAdScriptInStackOnBluetooth);
-  }
-
   AddUnsupportedPlatformConsoleMessage(window);
   CHECK(window->IsSecureContext());
 
@@ -433,11 +427,6 @@ ScriptPromise<BluetoothDevice> Bluetooth::requestDevice(
   if (!LocalFrame::HasTransientUserActivation(frame)) {
     exception_state.ThrowSecurityError(kHandleGestureForPermissionRequest);
     return EmptyPromise();
-  }
-
-  if (frame->IsAdScriptInStack()) {
-    UseCounter::Count(GetExecutionContext(),
-                      WebFeature::kAdScriptInStackOnBluetooth);
   }
 
   EnsureServiceConnection(window);
