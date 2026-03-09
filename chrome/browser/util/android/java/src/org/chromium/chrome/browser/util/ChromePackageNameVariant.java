f5f08b12ebba93904bcf72ddd959b7dd20135ef8
Guting Chen <gutingchen@microsoft.com>
2026-03-08 20:46:58 -0700

    Reuse chrome multiple package name
    This CL introduces a new ChromePackageNameVariant class that
    consolidates chrome package name constants for different Chrome variants
    on Android.
    
    Use Set instead of Array for storing Chrome package name constants, improving lookup performancefrom O(n) to O(1).
    
    Bug: 40697015
    Change-Id: Id0d62b3687ad5bb7c82c87b454b250e55060afcb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7623918
    Commit-Queue: Ziwen Lin <ziwlin@microsoft.com>
    Reviewed-by: Eleanor Lee <eleanorlee@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Ziwen Lin <ziwlin@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1596144}

---
+++ chrome/browser/util/android/java/src/org/chromium/chrome/browser/util/ChromePackageNameVariant.java
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.util;
+
+import org.chromium.build.annotations.NullMarked;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Holds package name constants for different Chrome variants on Android. This includes stable,
+ * beta, dev, and canary releases of Chrome.
+ */
+@NullMarked
+public final class ChromePackageNameVariant {
+    public static final String CHROME_STABLE_PACKAGE_NAME = "com.android.chrome";
+    public static final Set<String> CHROME_PRE_STABLE_PACKAGE_NAMES =
+            Set.of("org.chromium.chrome", "com.chrome.canary", "com.chrome.beta", "com.chrome.dev");
+    public static final Set<String> CHROME_PACKAGE_NAMES;
+
+    static {
+        Set<String> all = new HashSet<>(CHROME_PRE_STABLE_PACKAGE_NAMES);
+        all.add(CHROME_STABLE_PACKAGE_NAME);
+        CHROME_PACKAGE_NAMES = Collections.unmodifiableSet(all);
+    }
+
+    private ChromePackageNameVariant() {}
+}
