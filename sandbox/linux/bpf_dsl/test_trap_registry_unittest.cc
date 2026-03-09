7a5c4daf2f8ec43c7b17094143ef4ba8aef6ece8
Francisco Ochoa <frankchavez@google.com>
2026-03-09 11:49:54 -0700

    spanification: automatically spanify sandbox/linux/bpf_dsl/test_trap_registry_unittest.cc etc.
    This is the result of running the automatic spanification on linux and
    updating code to use and pass spans where size is known.
    
    The original patch was fully automated using script:
    //tools/clang/spanify/rewrite-multiple-platforms.sh -platforms=linux
    Then refined with gemini-cli
    
    gemini-run/batch-run-1761710114/group_43
    
    Bug: 439964610
    Change-Id: I79717cc87502c4f6f4b7790f860ac8b8b70342b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7639759
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Auto-Submit: Francisco Ochoa <frankchavez@google.com>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1596482}

--- sandbox/linux/bpf_dsl/test_trap_registry_unittest.cc
+++ sandbox/linux/bpf_dsl/test_trap_registry_unittest.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
+#include <array>
+
 #include "base/compiler_specific.h"
 #include "base/memory/raw_ptr.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -25,15 +27,17 @@ intptr_t TestTrapFuncTwo(const arch_seccomp_data& data, void* aux) {
 
 // Test that TestTrapRegistry correctly assigns trap IDs to trap handlers.
 TEST(TestTrapRegistry, TrapIDs) {
-  struct {
+  struct Funcs {
     TrapRegistry::TrapFnc fnc;
     raw_ptr<const void> aux;
-  } funcs[] = {
+  };
+  int dummy = 0;
+  auto funcs = std::to_array<Funcs>({
       {TestTrapFuncOne, nullptr},
       {TestTrapFuncTwo, nullptr},
-      {TestTrapFuncOne, funcs},
-      {TestTrapFuncTwo, funcs},
-  };
+      {TestTrapFuncOne, &dummy},
+      {TestTrapFuncTwo, &dummy},
+  });
 
   TestTrapRegistry traps;
 
@@ -41,8 +45,7 @@ TEST(TestTrapRegistry, TrapIDs) {
   for (int i = 0; i < 2; ++i) {
     for (size_t j = 0; j < std::size(funcs); ++j) {
       // Trap IDs start at 1.
-      UNSAFE_TODO(
-          EXPECT_EQ(j + 1, traps.Add({funcs[j].fnc, funcs[j].aux, true})));
+      EXPECT_EQ(j + 1, traps.Add({funcs[j].fnc, funcs[j].aux, true}));
     }
   }
 }
