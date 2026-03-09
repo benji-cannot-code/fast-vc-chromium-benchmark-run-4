14df5696c51003b1c3f1abf50f22455df4b90984
Antonio Sartori <antoniosartori@chromium.org>
2026-03-09 07:09:16 -0700

    [Permissions] clear embargo when resetting permissions in page info
    This CL fixes a regression introduced by https://crrev.com/c/6818302,
    which caused the "Reset permission" button in Page Info not to work
    for permissions under embargo.
    
    This change ensures that when a permission is reset to the default
    state, any associated embargoes are also cleared, allowing the site to
    request permissions again.
    
    Bug: 490015146
    Fixed: 490015146
    Change-Id: I99b5077717c120e93591e7711b1a5b8900bde049
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7638631
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1596304}

--- chrome/browser/ui/page_info/page_info_unittest.cc
+++ chrome/browser/ui/page_info/page_info_unittest.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/content_settings/page_specific_content_settings_delegate.h"
 #include "chrome/browser/file_system_access/file_system_access_features.h"
 #include "chrome/browser/history/history_service_factory.h"
+#include "chrome/browser/permissions/permission_decision_auto_blocker_factory.h"
 #include "chrome/browser/picture_in_picture/auto_picture_in_picture_tab_helper.h"
 #include "chrome/browser/ssl/stateful_ssl_host_state_delegate_factory.h"
 #include "chrome/browser/subresource_filter/subresource_filter_profile_context_factory.h"
@@ -44,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/page_info/core/features.h"
 #include "components/page_info/page_info_ui.h"
 #include "components/permissions/features.h"
+#include "components/permissions/permission_decision_auto_blocker.h"
 #include "components/permissions/permission_recovery_success_rate_tracker.h"
 #include "components/privacy_sandbox/privacy_sandbox_prefs.h"
 #include "components/safe_browsing/buildflags.h"
@@ -2856,4 +2858,31 @@ TEST_F(PageInfoTest, SiteExceptionScopeTypeMetrics) {
   tester.ExpectBucketCount(kScopeTypeHistogram,
                            ContentSettingsPattern::Scope::kCustomScope,
                            1 /* expected_count */);
+}
+
+TEST_F(PageInfoTest, ResetPermissionClearsEmbargo) {
+  auto* autoblocker =
+      PermissionDecisionAutoBlockerFactory::GetForProfile(profile());
+  GURL target_url = url();
+
+  // Record 3 dismissals to trigger embargo.
+  autoblocker->RecordDismissAndEmbargo(target_url,
+                                       ContentSettingsType::GEOLOCATION,
+                                       /*dismissed_prompt_was_quiet=*/false);
+  autoblocker->RecordDismissAndEmbargo(target_url,
+                                       ContentSettingsType::GEOLOCATION,
+                                       /*dismissed_prompt_was_quiet=*/false);
+  autoblocker->RecordDismissAndEmbargo(target_url,
+                                       ContentSettingsType::GEOLOCATION,
+                                       /*dismissed_prompt_was_quiet=*/false);
+
+  EXPECT_TRUE(
+      autoblocker->IsEmbargoed(target_url, ContentSettingsType::GEOLOCATION));
+
+  // Reset the permission
+  page_info()->OnSitePermissionChanged(ContentSettingsType::GEOLOCATION,
+                                       std::nullopt, std::nullopt, false);
+
+  EXPECT_FALSE(
+      autoblocker->IsEmbargoed(target_url, ContentSettingsType::GEOLOCATION));
 }
