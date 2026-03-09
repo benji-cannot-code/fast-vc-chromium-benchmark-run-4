46953a33e994dc53606159162897e8b84e6cbb13
Asami Doi <asamidoi@chromium.org>
2026-03-09 07:11:39 -0700

    [ios] Deprecate UIWindow's init in SafetyCheckViewTest
    UIWindow's init was deprecated on iOS 26, initWithWindowScene should be
    used instead.
    
    Bug: 489089329
    Change-Id: Ib65ab560df1847546154b6d9e711948e1468510f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7638151
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1596305}

--- ios/chrome/browser/content_suggestions/safety_check/ui/safety_check_view_unittest.mm
+++ ios/chrome/browser/content_suggestions/safety_check/ui/safety_check_view_unittest.mm
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/content_suggestions/safety_check/ui/safety_check_item_type.h"
 #import "ios/chrome/browser/content_suggestions/ui/cells/icon_detail_view.h"
 #import "ios/chrome/browser/safety_check/model/ios_chrome_safety_check_manager_constants.h"
+#import "ios/chrome/test/app/uikit_test_util.h"
 #import "testing/platform_test.h"
 
 // Tests the SafetyCheckView and subviews.
@@ -22,7 +23,8 @@ class SafetyCheckViewTest : public PlatformTest {
   SafetyCheckViewTest() {
     _superview = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 200, 200)];
 
-    _window = [[UIWindow alloc] init];
+    _window = [[UIWindow alloc]
+        initWithWindowScene:chrome_test_util::GetAnyWindowScene()];
 
     [_window addSubview:_superview];
 
