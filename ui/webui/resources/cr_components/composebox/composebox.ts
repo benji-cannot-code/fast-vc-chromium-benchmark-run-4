41572dae74331dfa2287ddba762a2abf10c2f7d3
Omar Flores <omarflores@google.com>
2026-03-09 13:41:35 -0700

    [composebox] Fixing color caret after adding context
    Making sure caret is repositioned upon gaining focus to have alignment after adding context.
    
    - Previously: upon attaching context, losing and regaining focus, the caret would be reset so its original position as its being re-rendered and the focus is gained programmatically, and the caret repositioning would only occur upon typing.
    - New: the caret always repositions itself to to match the mirror div upon gaining focus, not just upon typing
    
    Screencast: https://screencast.googleplex.com/cast/NTgyMTczNzg2NTMxNDMwNHw1MTk5YmIwMC00ZQ
    
    Bug: 485298910, 490443569
    Change-Id: Ia4666849ec2f217fbda4ec7010eb7a600ceb4bc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7639066
    Reviewed-by: Marlon Facey <mfacey@chromium.org>
    Commit-Queue: Omar Flores <omarflores@google.com>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1596571}

--- ui/webui/resources/cr_components/composebox/composebox.ts
+++ ui/webui/resources/cr_components/composebox/composebox.ts
@@ -468,6 +468,7 @@ export class ComposeboxElement extends I18nMixinLit
 
       this.eventTracker_.add(this.$.input, 'focus', () => {
         this.$.caret.classList.add('caret-visible');
+        this.updateCaret_();
       });
       this.eventTracker_.add(this.$.input, 'blur', () => {
         this.$.caret.classList.remove('caret-visible');
@@ -1332,7 +1333,7 @@ export class ComposeboxElement extends I18nMixinLit
       this.queryAutocomplete_(/* clearMatches= */ true);
 
       if (!this.disableCaretColorAnimation) {
-        this.resetCaret();
+        this.resetCaret_();
       }
     } else {
       this.closeComposebox_();
@@ -1607,7 +1608,7 @@ export class ComposeboxElement extends I18nMixinLit
 
         caret.style.transform = `translate(${caretX}px, ${caretY}px)`;
       } else {
-        this.resetCaret();
+        this.resetCaret_();
       }
       return;
     }
@@ -1632,7 +1633,7 @@ export class ComposeboxElement extends I18nMixinLit
     }
   }
 
-  resetCaret() {
+  protected resetCaret_() {
     // 12 origin must be set as this fixes spacing to match the box padding
     // around the caret
     const isRtl = document.documentElement.dir === 'rtl';
