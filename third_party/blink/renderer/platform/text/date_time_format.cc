9aede0841d6a9853ff9fe1deceb52a145d9b8738
Fredrik Söderquist <fs@opera.com>
2026-03-09 15:25:15 -0700

    Use StringView in DateTimeFormat::QuoteAndappend() (and fix name)
    Update to take a StringView. Fix the name to QuoteAndAppend(). Update
    the caller Locale::WeekFormatInLDML() and the usage in locale_win.cc.
    
    Bug: 473854537
    Change-Id: I14fd97f1c288e05a848a63f4cf63d56ffe7974fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7649093
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1596645}

--- third_party/blink/renderer/platform/text/date_time_format.cc
+++ third_party/blink/renderer/platform/text/date_time_format.cc
@@ -244,7 +244,7 @@ static bool IsASCIIAlphabetOrQuote(UChar ch) {
   return IsASCIIAlpha(ch) || ch == '\'';
 }
 
-void DateTimeFormat::QuoteAndappend(const String& literal,
+void DateTimeFormat::QuoteAndAppend(const StringView& literal,
                                     StringBuilder& buffer) {
   if (literal.length() <= 0)
     return;
@@ -265,8 +265,7 @@ void DateTimeFormat::QuoteAndappend(const String& literal,
     if (literal[i] == '\'') {
       buffer.Append("''");
     } else {
-      String escaped = literal.Substring(i);
-      escaped.Replace("'", "''");
+      String escaped = literal.substr(i).ToString().Replace("'", "''");
       buffer.Append('\'');
       buffer.Append(escaped);
       buffer.Append('\'');
