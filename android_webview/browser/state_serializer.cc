debeb9d59c3173cdb93aa73a92f1160472fd6c0b
Joanne de Abreu <jdeabreu@google.com>
2026-03-09 08:05:51 -0700

    WebView save state unit tests
    This cl updates the unit tests for WebView save state to:
    
    *Test the new version of save state which saves headers -> see
    crrev.com/c/7612629
    *Check the version header, an individual navigation
    entry and navigation history are saved correctly for all currently
    supported save state versions
    *Cleans up the state serializer class to remove unneeded helper methods
    and instead pass through version to all methods making the code easier
    to understand, use and debug
    
    Bug: 408128748
    Test: AndroidWebViewStateSerializerTest
    Change-Id: I77c18d0f600cdccfe309508a29bacaf8aa86b9cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7633004
    Commit-Queue: Joanne de Abreu <jdeabreu@google.com>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1596327}

--- android_webview/browser/state_serializer.cc
+++ android_webview/browser/state_serializer.cc
@@ -43,8 +43,6 @@ namespace android_webview {
 
 namespace {
 
-const uint32_t AW_STATE_VERSION = internal::AW_STATE_VERSION_INCLUDE_HEADERS;
-
 // The production implementation of NavigationHistory and NavigationHistorySink,
 // backed by a NavigationController.
 // This class could be split into two independent parts, one for each interface.
@@ -109,7 +107,7 @@ bool RestoreFromPickleLegacy_VersionDataUrl(
   for (int i = 0; i < entry_count; ++i) {
     entries.push_back(content::NavigationEntry::Create());
     if (!internal::RestoreNavigationEntryFromPickle(
-            state_version, iterator, entries[i].get(), context.get())) {
+            iterator, entries[i].get(), context.get(), state_version)) {
       return false;
     }
   }
@@ -127,7 +125,8 @@ std::optional<base::Pickle> WriteToPickle(content::WebContents& web_contents,
                                           size_t max_size,
                                           bool include_forward_state) {
   NavigationControllerWrapper wrapper(&web_contents.GetController());
-  return internal::WriteToPickle(wrapper, max_size, include_forward_state);
+  return internal::WriteToPickle(wrapper, internal::AW_STATE_VERSION, max_size,
+                                 include_forward_state);
 }
 
 bool RestoreFromPickle(base::PickleIterator* iterator,
@@ -140,11 +139,12 @@ bool RestoreFromPickle(base::PickleIterator* iterator,
 namespace internal {
 
 std::optional<base::Pickle> WriteToPickle(NavigationHistory& history,
+                                          uint32_t state_version,
                                           size_t max_size,
                                           bool save_forward_history) {
   base::Pickle pickle;
 
-  internal::WriteHeaderToPickle(AW_STATE_VERSION, &pickle);
+  internal::WriteHeaderToPickle(&pickle, state_version);
 
   const int entry_count = history.GetEntryCount();
   const int selected_entry = history.GetCurrentEntry();
@@ -171,8 +171,8 @@ std::optional<base::Pickle> WriteToPickle(NavigationHistory& history,
     size_t payload_size_before_adding_entry = pickle.payload_size();
 
     pickle.WriteBool(i == selected_entry);
-    internal::WriteNavigationEntryToPickle(*history.GetEntryAtIndex(i),
-                                           &pickle);
+    internal::WriteNavigationEntryToPickle(*history.GetEntryAtIndex(i), &pickle,
+                                           state_version);
 
     if (pickle.size() > max_size) {
       if (i == start_entry) {
@@ -185,7 +185,7 @@ std::optional<base::Pickle> WriteToPickle(NavigationHistory& history,
       // with save_forward_history = false, ensuring that the current entry is
       // the first one written.
       if (!selected_entry_was_saved) {
-        return WriteToPickle(history, max_size,
+        return WriteToPickle(history, state_version, max_size,
                              /* save_forward_history= */ false);
       }
 
@@ -208,11 +208,7 @@ std::optional<base::Pickle> WriteToPickle(NavigationHistory& history,
   return pickle;
 }
 
-void WriteHeaderToPickle(base::Pickle* pickle) {
-  WriteHeaderToPickle(AW_STATE_VERSION, pickle);
-}
-
-void WriteHeaderToPickle(uint32_t state_version, base::Pickle* pickle) {
+void WriteHeaderToPickle(base::Pickle* pickle, uint32_t state_version) {
   pickle->WriteUInt32(state_version);
 }
 
@@ -230,20 +226,12 @@ uint32_t RestoreHeaderFromPickle(base::PickleIterator* iterator) {
 }
 
 bool IsSupportedVersion(uint32_t state_version) {
-  return state_version == internal::AW_STATE_VERSION_INITIAL ||
-         state_version == internal::AW_STATE_VERSION_DATA_URL ||
-         state_version == internal::AW_STATE_VERSION_MOST_RECENT_FIRST ||
-         state_version == internal::AW_STATE_VERSION_INCLUDE_HEADERS;
+  return AW_STATE_SUPPORTED_VERSIONS.contains(state_version);
 }
 
 void WriteNavigationEntryToPickle(content::NavigationEntry& entry,
-                                  base::Pickle* pickle) {
-  WriteNavigationEntryToPickle(AW_STATE_VERSION, entry, pickle);
-}
-
-void WriteNavigationEntryToPickle(uint32_t state_version,
-                                  content::NavigationEntry& entry,
-                                  base::Pickle* pickle) {
+                                  base::Pickle* pickle,
+                                  uint32_t state_version) {
   DCHECK(IsSupportedVersion(state_version));
   pickle->WriteString(entry.GetURL().spec());
   pickle->WriteString(entry.GetVirtualURL().spec());
@@ -284,10 +272,9 @@ void WriteNavigationEntryToPickle(uint32_t state_version,
             : "");
   }
 
-  // Please update AW_STATE_VERSION and IsSupportedVersion() if serialization
-  // format is changed.
-  // Make sure the serialization format is updated in a backwards compatible
-  // way.
+  // Please update AW_STATE_VERSION and AW_STATE_SUPPORTED_VERSIONS if
+  // serialization format is changed. Make sure the serialization format is
+  // updated in a backwards compatible way.
 }
 
 bool RestoreFromPickle(base::PickleIterator* iterator,
@@ -319,7 +306,7 @@ bool RestoreFromPickle(base::PickleIterator* iterator,
     entries.push_back(content::NavigationEntry::Create());
 
     if (!internal::RestoreNavigationEntryFromPickle(
-            state_version, iterator, entries.back().get(), context.get())) {
+            iterator, entries.back().get(), context.get(), state_version)) {
       return false;
     }
 
@@ -344,16 +331,8 @@ bool RestoreFromPickle(base::PickleIterator* iterator,
 bool RestoreNavigationEntryFromPickle(
     base::PickleIterator* iterator,
     content::NavigationEntry* entry,
-    content::NavigationEntryRestoreContext* context) {
-  return RestoreNavigationEntryFromPickle(AW_STATE_VERSION, iterator, entry,
-                                          context);
-}
-
-bool RestoreNavigationEntryFromPickle(
-    uint32_t state_version,
-    base::PickleIterator* iterator,
-    content::NavigationEntry* entry,
-    content::NavigationEntryRestoreContext* context) {
+    content::NavigationEntryRestoreContext* context,
+    uint32_t state_version) {
   DCHECK(IsSupportedVersion(state_version));
   DCHECK(iterator);
   DCHECK(entry);
