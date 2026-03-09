844f3beb1801eca039d08d7c315537d62d830090
Fiaz Muhammad <mfiaz@google.com>
2026-03-09 14:12:17 -0700

    [Persist] Avoid saving before associating during restore
    - This CL changes PreRestoreChangesCrawler to use a post-order
            traversal, walking the children of a collection before saving
            the collection itself.
    - Saving a collection causes the system to register new StorageIds
            for all of its children if they aren't already in the memory
            map. By associating and saving all child tabs first, we avoid
            mistakenly generating new StorageIds for tabs that already
            have an existing on-disk StorageId.
    - Always attempt to associate for manually saved children. This will
            no-op if already associated.
    
    Bug: 485907357
    Change-Id: I7d6d7e371b845212f0a6db9331bc81a4339b34b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7639962
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1596597}

--- chrome/browser/tab/storage_restore_orchestrator.cc
+++ chrome/browser/tab/storage_restore_orchestrator.cc
@@ -63,6 +63,8 @@ class PreRestoreChangesCrawler : public DirectChildWalker::Processor {
   }
 
   void ProcessCollection(const TabCollection* collection) override {
+    DirectChildWalker walker(collection, this);
+    walker.Walk();
     if (!tracker_->AssociateCollection(collection) &&
         ShouldSaveOnRestoreStart(collection)) {
       service_->Save(collection);
@@ -70,8 +72,6 @@ class PreRestoreChangesCrawler : public DirectChildWalker::Processor {
       service_->SaveDivergentChildren(collection->GetParentCollection(),
                                       passkey_);
     }
-    DirectChildWalker walker(collection, this);
-    walker.Walk();
   }
 
   bool ShouldSaveOnRestoreStart(const TabCollection* collection) {
@@ -152,7 +152,8 @@ void StorageRestoreOrchestrator::OnSaveChildTab(
   }
 
   RestoreEntityTracker* tracker = loaded_data_->GetTracker();
-  if (was_inserted && tracker->AssociateTab(tab)) {
+  bool was_associated = tracker->AssociateTab(tab);
+  if (was_inserted && was_associated) {
     TabCollectionHandle parent_handle = parent->GetHandle();
     DCHECK(tracker->HasCollectionBeenAssociated(parent_handle));
   } else {
@@ -180,7 +181,8 @@ void StorageRestoreOrchestrator::OnSaveChildCollection(
   }
 
   RestoreEntityTracker* tracker = loaded_data_->GetTracker();
-  if (was_inserted && tracker->AssociateCollection(collection)) {
+  bool was_associated = tracker->AssociateCollection(collection);
+  if (was_inserted && was_associated) {
     TabCollectionHandle parent_handle = parent->GetHandle();
     DCHECK(tracker->HasCollectionBeenAssociated(parent_handle));
   } else {
