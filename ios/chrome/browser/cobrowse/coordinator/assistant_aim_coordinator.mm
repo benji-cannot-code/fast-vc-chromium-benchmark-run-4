580662618fb3da1f523818022eab191be0b7e638
Aliona DANGLA <alionadangla@google.com>
2026-03-09 06:06:57 -0700

    [iOS] Do not show the assistant when in tab grid
    The Assistant should not be displayed when in tab grid so this CL is:
    * Dismissing cobrowse when going into tab grid
    * Do not show the assistant if tab grid is visible
    
    The later solve a crash as some tab are loaded while in tab grid so it
    prevent this: See video:
    https://drive.google.com/file/d/1sKObGXqPNB2P_qo44lXBuikIYOLjGGxy/view?usp=sharing&resourcekey=0-0z76rq1TqjGVK7c8OWssRA
    
    Fixed: 490358131
    Change-Id: Ia98d772d82ad5b3467ad1ddf6f4fd4fdf39def62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7642665
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1596283}

--- ios/chrome/browser/cobrowse/coordinator/assistant_aim_coordinator.mm
+++ ios/chrome/browser/cobrowse/coordinator/assistant_aim_coordinator.mm
@@ -9,13 +9,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/assistant/ui/assistant_container_delegate.h"
 #import "ios/chrome/browser/cobrowse/coordinator/assistant_aim_mediator.h"
 #import "ios/chrome/browser/cobrowse/ui/assistant_aim_view_controller.h"
+#import "ios/chrome/browser/shared/coordinator/scene/scene_state.h"
+#import "ios/chrome/browser/shared/coordinator/scene/state/tab_grid_state.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
 #import "ios/chrome/browser/shared/public/commands/command_dispatcher.h"
 #import "ios/web/public/web_state.h"
 
 @interface AssistantAIMCoordinator () <AssistantAIMViewControllerDelegate,
-                                       AssistantContainerDelegate>
+                                       AssistantContainerDelegate,
+                                       TabGridStateObserver>
 @end
 
 @implementation AssistantAIMCoordinator {
@@ -24,6 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)start {
+  [self.browser->GetSceneState().tabGridState addObserver:self];
+
   _viewController = [[AssistantAIMViewController alloc] init];
   _viewController.delegate = self;
 
@@ -43,30 +48,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)stop {
+  [self.browser->GetSceneState().tabGridState removeObserver:self];
+
   [_mediator disconnect];
   _mediator = nil;
 
   if (_viewController) {
     _viewController = nil;
-    if (self.browser) {
-      CommandDispatcher* dispatcher = self.browser->GetCommandDispatcher();
-      if ([dispatcher
-              dispatchingForProtocol:@protocol(AssistantContainerCommands)]) {
-        id<AssistantContainerCommands> containerHandler =
-            HandlerForProtocol(dispatcher, AssistantContainerCommands);
-        [containerHandler dismissAssistantContainerAnimated:NO completion:nil];
-      }
-    }
+    [self dismissAssistantContainerAnimated:NO];
   }
+}
+
+#pragma mark - TabGridStateObserver
+
+- (void)willEnterTabGrid {
+  [self dismissAssistantContainerAnimated:YES];
 }
 
 #pragma mark - AssistantAIMViewControllerDelegate
 
 - (void)assistantAIMViewControllerDidTapClose:
     (AssistantAIMViewController*)viewController {
-  id<AssistantContainerCommands> containerHandler = HandlerForProtocol(
-      self.browser->GetCommandDispatcher(), AssistantContainerCommands);
-  [containerHandler dismissAssistantContainerAnimated:YES completion:nil];
+  [self dismissAssistantContainerAnimated:YES];
 }
 
 #pragma mark - AssistantContainerDelegate
@@ -74,6 +77,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)assistantContainer:(AssistantContainerViewController*)container
       didDisappearAnimated:(BOOL)animated {
   [self stop];
+}
+
+#pragma mark - Private
+
+// Dismisses the assistant container safely.
+- (void)dismissAssistantContainerAnimated:(BOOL)animated {
+  if (self.browser) {
+    CommandDispatcher* dispatcher = self.browser->GetCommandDispatcher();
+    if ([dispatcher
+            dispatchingForProtocol:@protocol(AssistantContainerCommands)]) {
+      id<AssistantContainerCommands> containerHandler =
+          HandlerForProtocol(dispatcher, AssistantContainerCommands);
+      [containerHandler dismissAssistantContainerAnimated:animated
+                                               completion:nil];
+    }
+  }
 }
 
 @end
