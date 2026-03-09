4e416dc7b6989ee95ebf9fb02da1e8c44d567505
Jason Hu <hujasonx@google.com>
2026-03-09 14:04:52 -0700

    [context-input] Set process_image_for_aim when lens usage intent is set
    The server will be migrating to a new param that behaves similarly to the existing has_lens_intent field. This change populates the new field.
    
    Bug: b:490069362
    Change-Id: Id6e159ff809b69abf59c432d7f9965a25d0bdca2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7637884
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Cr-Commit-Position: refs/heads/main@{#1596594}

--- components/contextual_search/internal/composebox_query_controller_unittest.cc
+++ components/contextual_search/internal/composebox_query_controller_unittest.cc
@@ -1777,6 +1777,8 @@ TEST_F(ComposeboxQueryControllerTest, UploadPdfFileRequestSuccess) {
                 .server_address(),
             kTestServerAddress);
   EXPECT_TRUE(controller().last_sent_file_upload_request()->has_lens_intent());
+  EXPECT_TRUE(
+      controller().last_sent_file_upload_request()->process_image_for_aim());
 }
 
 TEST_F(ComposeboxQueryControllerTest,
@@ -1805,6 +1807,8 @@ TEST_F(ComposeboxQueryControllerTest,
 
   // Validate the file upload request payload.
   EXPECT_FALSE(controller().last_sent_file_upload_request()->has_lens_intent());
+  EXPECT_FALSE(
+      controller().last_sent_file_upload_request()->process_image_for_aim());
 }
 
 TEST_F(ComposeboxQueryControllerTest, UploadPageContextPdfFileRequestSuccess) {
@@ -2167,6 +2171,7 @@ TEST_F(ComposeboxQueryControllerTest,
   EXPECT_EQ(
       file_upload_request->objects_request().payload().content().webpage_url(),
       page_url.spec());
+  EXPECT_TRUE(file_upload_request->process_image_for_aim());
   // Validate the viewport upload request payload.
   EXPECT_EQ(viewport_upload_request->objects_request()
                 .image_data()
@@ -2242,6 +2247,7 @@ TEST_F(ComposeboxQueryControllerTest,
                 .request_id()
                 .media_type(),
             lens::LensOverlayRequestId::MEDIA_TYPE_PDF_AND_IMAGE);
+  EXPECT_TRUE(viewport_upload_request->process_image_for_aim());
 }
 
 TEST_F(ComposeboxQueryControllerTest,
