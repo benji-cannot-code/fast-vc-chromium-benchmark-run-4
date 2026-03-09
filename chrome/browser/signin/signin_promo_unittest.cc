f9ec7f0df15a782df8a7e206c64ded5ee94500f4
Ryan Sultanem <rsult@google.com>
2026-03-09 08:34:54 -0700

    [AvatarSigninPromo] Add feature param to control delays duration
    Also added flag entries, with an entry to facilitate testing,
    using short delays.
    
    Bug: 486109449
    Change-Id: I7dadf748a436139da950914e340b9151381d906c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7643711
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1596353}

--- chrome/browser/signin/signin_promo_unittest.cc
+++ chrome/browser/signin/signin_promo_unittest.cc
@@ -1065,8 +1065,9 @@ class AvatarButtonPromoManagerTest : public testing::Test {
       case ProfileMenuAvatarButtonPromoInfo::Type::kSyncPromo:
         break;
       case ProfileMenuAvatarButtonPromoInfo::Type::kSigninPromo:
-        // TODO(crbug.com/486109449): Use a feature param instead.
-        task_environment_.FastForwardBy(base::Days(7) + base::Days(1));
+        task_environment_.FastForwardBy(
+            switches::kSigninPromoOnAvatarPillDelayForNextPromoAllowed.Get() +
+            base::Days(1));
         break;
     }
   }
@@ -1256,8 +1257,9 @@ TEST_F(AvatarButtonPromoManagerTest,
 
   // Fast forward by less than expected.
   base::TimeDelta time_remaining_for_promo_to_show = base::Days(1);
-  // TODO(crbug.com/486109449): Use a feature param instead.
-  FastForwardBy(base::Days(7) - time_remaining_for_promo_to_show);
+  FastForwardBy(
+      switches::kSigninPromoOnAvatarPillDelayForNextPromoAllowed.Get() -
+      time_remaining_for_promo_to_show);
   // Promo shown time check should still not allow the promo to show yet.
   ASSERT_FALSE(manager.ShouldShowPromo(signin_promo_type));
 
@@ -1291,8 +1293,9 @@ TEST_F(AvatarButtonPromoManagerTest,
 
   // Fast forward by less than expected.
   base::TimeDelta time_remaining_for_promo_to_show = base::Days(1);
-  // TODO(crbug.com/486109449): Use a feature param instead.
-  FastForwardBy(base::Days(7) - time_remaining_for_promo_to_show);
+  FastForwardBy(
+      switches::kSigninPromoOnAvatarPillDelayForNextPromoAllowed.Get() -
+      time_remaining_for_promo_to_show);
   // Promo shown time check should still not allow the promo to show yet.
   ASSERT_FALSE(manager.ShouldShowPromo(signin_promo_type));
 
--- chrome/browser/ui/webui/signin/profile_picker_handler.cc
+++ chrome/browser/ui/webui/signin/profile_picker_handler.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_window.h"
 #include "chrome/browser/profiles/profiles_state.h"
 #include "chrome/browser/signin/signin_util.h"
+#include "chrome/browser/sync/sync_service_factory.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
