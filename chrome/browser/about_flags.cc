f37405dad69d3dfdeffb147a2a7dba76f37dafaf
Robert Ogden <robertogden@google.com>
2026-03-09 12:19:37 -0700

    Add about flag for disabling Glic safety checks
    Bug: 489826083
    Change-Id: I892e1996f8d1db4b1b82e1bc22ceeb0b20856adb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7649549
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Auto-Submit: Robert Ogden <robertogden@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1596511}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "cc/base/features.h"
 #include "cc/base/switches.h"
+#include "chrome/browser/actor/actor_switches.h"
 #include "chrome/browser/apps/app_discovery_service/app_discovery_service.h"
 #include "chrome/browser/browser_features.h"
 #include "chrome/browser/browser_process.h"
@@ -10953,6 +10954,11 @@ const FeatureEntry kFeatureEntries[] = {
     {"glic-selection-prompt", flag_descriptions::kGlicSelectionPromptName,
      flag_descriptions::kGlicSelectionPromptDescription, kOsDesktop,
      FEATURE_VALUE_TYPE(features::kGlicSelectionPrompt)},
+
+    {"glic-disable-actor-safety-checks",
+     flag_descriptions::kGlicDisableActorSafetyChecksName,
+     flag_descriptions::kGlicDisableActorSafetyChecksDescription, kOsDesktop,
+     SINGLE_VALUE_TYPE(actor::switches::kDisableActorSafetyChecks)},
 
 #if BUILDFLAG(IS_CHROMEOS)
     {"glic-use-non-client", flag_descriptions::kGlicUseNonClientName,
