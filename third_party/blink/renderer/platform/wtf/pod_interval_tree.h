d8e7d37ef12668e7c50970e542358bb7a4131bfa
Ian Kilpatrick <ikilpatrick@chromium.org>
2026-03-09 15:31:38 -0700

    [wtf] Remove PodRedBlackTree::SetNeedsFullOrderingComparisons
    All the usages of PodRedBlackTree set this to true, however the
    unittests for PodRedBlackTree didn't - leading to a test coverage
    divergence.
    
    Remove for code complexity reasons.
    
    There should be no behaviour change.
    
    Change-Id: Ifafc6d7fee3ad1b9926f92cbdb5971dc309d9065
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7649136
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1596650}

--- third_party/blink/renderer/platform/wtf/pod_interval_tree.h
+++ third_party/blink/renderer/platform/wtf/pod_interval_tree.h
@@ -46,12 +46,10 @@ class PodIntervalTree final : public PodRedBlackTree<PodInterval<T, UserData>> {
   // this tree.
   using IntervalType = PodInterval<T, UserData>;
 
-  PodIntervalTree() : PodRedBlackTree<IntervalType>() { Init(); }
+  PodIntervalTree() : PodRedBlackTree<IntervalType>() {}
 
   explicit PodIntervalTree(scoped_refptr<PodArena> arena)
-      : PodRedBlackTree<IntervalType>(arena) {
-    Init();
-  }
+      : PodRedBlackTree<IntervalType>(arena) {}
 
   PodIntervalTree(const PodIntervalTree&) = delete;
   PodIntervalTree& operator=(const PodIntervalTree&) = delete;
@@ -89,13 +87,6 @@ class PodIntervalTree final : public PodRedBlackTree<PodInterval<T, UserData>> {
 
  private:
   using IntervalNode = typename PodRedBlackTree<IntervalType>::Node;
-
-  // Initializes the tree.
-  void Init() {
-    // Explicit dereference of "this" required because of
-    // inheritance rules in template classes.
-    this->SetNeedsFullOrderingComparisons(true);
-  }
 
   // Starting from the given node, adds all overlaps with the given
   // interval to the result vector. The intervals are sorted by
