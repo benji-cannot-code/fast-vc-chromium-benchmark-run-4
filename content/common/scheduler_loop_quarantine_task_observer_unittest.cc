5a773cccb670be20424064d25105f2032a0d571d
Stephen Nusko <nuskos@chromium.org>
2026-03-09 10:29:01 -0700

    Update PAAC field trial testing configs.
    Currently enabled_512K is approved for launch for Mac/Linux/Windows, but
    we're likely to go for amsc_macro_only as well for all platforms, but
    for now since you can only have one we list it for the other platforms.
    
    Change-Id: I48aa0de0a9c5e874688beb26a5956d54f5849a2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7647988
    Reviewed-by: Mikihito Matsuura <mikt@google.com>
    Auto-Submit: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1596436}

--- content/common/scheduler_loop_quarantine_task_observer_unittest.cc
+++ content/common/scheduler_loop_quarantine_task_observer_unittest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/current_thread.h"
 #include "base/test/bind.h"
 #include "base/test/scoped_feature_list.h"
+#include "content/common/features.h"
 #include "content/public/test/browser_task_environment.h"
 #include "partition_alloc/buildflags.h"
 #include "partition_alloc/extended_api.h"
@@ -103,6 +104,16 @@ TEST_F(SchedulerLoopQuarantineTaskObserverTest, QuarantinePausesBetweenTasks) {
   auto slq_task_observer =
       std::make_unique<SchedulerLoopQuarantineTaskObserver>();
   base::CurrentThread::Get().AddTaskObserver(slq_task_observer.get());
+  // We always install the observer so that we can test the feature regardless
+  // of if the observer feature is installed or not, but if it is installed the
+  // TaskEnvironment will have one additional paused count (functionally the
+  // same behaviour though).
+  const int kExpectedPausedCount =
+      base::FeatureList::IsEnabled(
+          features::
+              kPartitionAllocSchedulerLoopQuarantineTaskObserverForBrowserUIThread)
+          ? 2
+          : 1;
 
   // Initially the branch isn't paused and no tasks are pending, this is just
   // ensuring consistent starting state.
@@ -124,10 +135,10 @@ TEST_F(SchedulerLoopQuarantineTaskObserverTest, QuarantinePausesBetweenTasks) {
     // During this test the PausedCount should decrease and the expectation
     // above will be tested.
     loop.Run();
-    // After the task the PausedCount should be 1 again, since we want to
-    // exclude in-between tasks.
+    // After the task the PausedCount should be incremented again, since we want
+    // to exclude in-between tasks.
     EXPECT_EQ(0u, task_environment_.GetPendingMainThreadTaskCount());
-    EXPECT_EQ(1, branch.PausedCount());
+    EXPECT_EQ(kExpectedPausedCount, branch.PausedCount());
   }
 
   // Now we check again since the first time it was zero before the task.
@@ -139,12 +150,12 @@ TEST_F(SchedulerLoopQuarantineTaskObserverTest, QuarantinePausesBetweenTasks) {
           loop.Quit();
         }));
 
-    EXPECT_EQ(1, branch.PausedCount());
+    EXPECT_EQ(kExpectedPausedCount, branch.PausedCount());
     EXPECT_EQ(1u, task_environment_.GetPendingMainThreadTaskCount());
     // Again during this test the PausedCount should decrease.
     loop.Run();
-    // After the task the PausedCount should be 1 again.
-    EXPECT_EQ(1, branch.PausedCount());
+    // After the task the PausedCount should be incremented again.
+    EXPECT_EQ(kExpectedPausedCount, branch.PausedCount());
     EXPECT_EQ(0u, task_environment_.GetPendingMainThreadTaskCount());
   }
   // Clean up the observer to prevent dangling references.
