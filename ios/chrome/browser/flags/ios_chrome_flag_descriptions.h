7d4723daa7ba5ccd28392f56c1aec9bea662fc1e
Quentin Pubert <qpubert@google.com>
2026-03-09 04:48:58 -0700

    [iOS] Add Cobalt feature flag
    This CL adds a feature flag for the Cobalt feature. It will be used in a
    follow-up CL to set up Cobalt in WKWebViewConfiguration.
    
    Bug: 483627205
    Change-Id: I7c82481fd9d4635d9766a99e523e5a35203f8fb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7643548
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Auto-Submit: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1596264}

--- ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
+++ ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
@@ -481,6 +481,9 @@ extern const char kIOSChooseFromDriveDescription[];
 extern const char kIOSChooseFromDriveSignedOutName[];
 extern const char kIOSChooseFromDriveSignedOutDescription[];
 
+extern const char kIOSCobaltName[];
+extern const char kIOSCobaltDescription[];
+
 extern const char kIOSCustomFileUploadMenuName[];
 extern const char kIOSCustomFileUploadMenuDescription[];
 
