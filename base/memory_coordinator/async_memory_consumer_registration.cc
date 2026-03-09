24bd74c727549e0ed64bbb3c8af69cf62381cb25
Andres Ricardo Perez <andresrperez@chromium.org>
2026-03-09 09:09:28 -0700

    [Memory Coordinator] Rename consumer_id param in AsyncMemoryConsumerRegistration constructor
    This rename was missing from https://crrev.com/c/7615564, which made the
    now internal consumer_id a hash of the passed consumer_name.
    
    This CL just needs to rename the parameter, as the hashing happens once
    it gets to the registry.
    
    Bug: 488438351
    Change-Id: I8b27d8f607e9060ed75b98f6374922d92fd92e36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7644948
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1596378}

--- base/memory_coordinator/async_memory_consumer_registration.cc
+++ base/memory_coordinator/async_memory_consumer_registration.cc
@@ -24,7 +24,7 @@ class AsyncMemoryConsumerRegistration::MainThread : public MemoryConsumer {
  public:
   MainThread() { DETACH_FROM_THREAD(thread_checker_); }
 
-  void Init(std::string consumer_id,
+  void Init(std::string consumer_name,
             std::optional<MemoryConsumerTraits> traits,
             CheckUnregister check_unregister,
             CheckRegistryExists check_registry_exists,
@@ -33,7 +33,7 @@ class AsyncMemoryConsumerRegistration::MainThread : public MemoryConsumer {
     DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
     consumer_task_runner_ = std::move(consumer_task_runner);
     parent_ = std::move(parent);
-    registration_.emplace(consumer_id, traits, this, check_unregister,
+    registration_.emplace(consumer_name, traits, this, check_unregister,
                           check_registry_exists);
   }
 
@@ -72,7 +72,7 @@ class AsyncMemoryConsumerRegistration::MainThread : public MemoryConsumer {
 // AsyncMemoryConsumerRegistration ---------------------------------------------
 
 AsyncMemoryConsumerRegistration::AsyncMemoryConsumerRegistration(
-    std::string_view consumer_id,
+    std::string_view consumer_name,
     std::optional<MemoryConsumerTraits> traits,
     MemoryConsumer* consumer,
     CheckUnregister check_unregister,
@@ -82,7 +82,7 @@ AsyncMemoryConsumerRegistration::AsyncMemoryConsumerRegistration(
   main_thread_ = std::make_unique<MainThread>();
   main_thread_task_runner_->PostTask(
       FROM_HERE, BindOnce(&MainThread::Init, Unretained(main_thread_.get()),
-                          std::string(consumer_id), traits, check_unregister,
+                          std::string(consumer_name), traits, check_unregister,
                           check_registry_exists, weak_ptr_factory_.GetWeakPtr(),
                           SequencedTaskRunner::GetCurrentDefault()));
 }
