d0e5823f9e82ee5c6264d581a38bc2ee87b11602
Aishwarya Rajesh <aishwaryarj@google.com>
2026-03-09 11:57:36 -0700

    [RWM][RecentlyClosed] Default-enable kRecentlyClosedTabsAndWindows
    kRobustWindowManagement (that kRecentlyClosedTabsAndWindows has a
    dependency on) was default-enabled in http://crrev.com/c/7638501, so
    we can launch this feature now.
    
    Bug: 444680983
    Change-Id: Ib2a1dab05ede86bc1d5c32b0a9d0b66c22156243
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7641421
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Zhe Li <zheliooo@google.com>
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1596492}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ntp/RecentTabsPageTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ntp/RecentTabsPageTest.java
@@ -51,6 +51,7 @@ import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
 import org.chromium.base.ThreadUtils;
+import org.chromium.base.TimeUtils;
 import org.chromium.base.Token;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
@@ -97,6 +98,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 /** Instrumentation tests for {@link RecentTabsPage}. */
 @RunWith(ChromeJUnit4ClassRunner.class)
@@ -354,7 +356,7 @@ public class RecentTabsPageTest {
     @Policies.Add(@Policies.Item(key = "BrowserSignin", string = "0"))
     public void testRecentlyClosedGroup_WithoutTitle() throws Exception {
         mPage = loadRecentTabsPage();
-        long time = 904881600000L;
+        long time = getValidTimestampForEntry();
         // Set a recently closed group and confirm a view is rendered for it.
         final RecentlyClosedGroup group = new RecentlyClosedGroup(2, time, null, COLOR_ID);
         Token tabGroupId = new Token(798L, 4389L);
@@ -445,7 +447,7 @@ public class RecentTabsPageTest {
     @Policies.Add(@Policies.Item(key = "BrowserSignin", string = "0"))
     public void testRecentlyClosedBulkEvent() throws Exception {
         mPage = loadRecentTabsPage();
-        long time = 904881600000L;
+        long time = getValidTimestampForEntry();
         // Set a recently closed bulk event and confirm a view is rendered for it.
         final RecentlyClosedBulkEvent event = new RecentlyClosedBulkEvent(3, time);
         Token tabGroupId = new Token(1L, 2L);
@@ -528,7 +530,7 @@ public class RecentTabsPageTest {
     @Policies.Add(@Policies.Item(key = "BrowserSignin", string = "0"))
     public void testRecentlyClosedWindows() throws Exception {
         mPage = loadRecentTabsPage();
-        long time = 904881600000L;
+        long time = getValidTimestampForEntry();
         String title1 = "Window 1";
         String activeTabTitle1 = "Google";
         String activeTabUrl1 = "https://www.google.com";
@@ -594,7 +596,7 @@ public class RecentTabsPageTest {
         MultiWindowUtils.setMaxInstancesForTesting(3);
 
         mPage = loadRecentTabsPage();
-        long time = 904881600000L;
+        long time = getValidTimestampForEntry();
         String title1 = "Window 1";
         String activeTabTitle1 = "Google";
         String activeTabUrl1 = "https://www.google.com";
@@ -903,5 +905,9 @@ public class RecentTabsPageTest {
                 view.dispatchTouchEvent(downEvent);
             }
         };
+    }
+
+    private static long getValidTimestampForEntry() {
+        return TimeUtils.currentTimeMillis() - TimeUnit.DAYS.toMillis(/* duration= */ 2);
     }
 }
