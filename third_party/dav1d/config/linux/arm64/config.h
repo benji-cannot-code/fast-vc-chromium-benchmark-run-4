3ea8048e097359e9f1f68cfd25c31cfce3e70257
Wan-Teh Chang <wtc@google.com>
2026-03-09 15:55:59 -0700

    Roll src/third_party/dav1d/libdav1d/ b546257f7..241a6b236 (21 commits)
    https://chromium.googlesource.com/external/github.com/videolan/dav1d.git/+log/b546257f7707..241a6b236a9c
    
    $ git log b546257f7..241a6b236 --date=short --no-merges --format='%ad %ae %s'
    2026-02-08 gramner x86: Fix warp8x8 gamma/delta naming mixup
    2026-03-04 martin arm: Switch to a more correct Windows flag for detecting I8MM
    2026-02-28 code CI: Update images
    2026-01-03 code CI: Switch to loongarch64 Debian toolchain
    2026-02-05 martin arm: mc: Add missing # for some immediate constants, for consistency
    2026-01-29 martin subprojects: Update checkasm to v1.1.0
    2026-01-30 ccawley2011 Fix compiler warning on platforms without cfi-icall
    2026-01-30 ccawley2011 Allow falling back to standard C signal on non-POSIX systems
    2026-01-26 gramner x86: Update x86inc.asm
    2023-07-03 komh Export DAV1D APIs correctly on OS/2
    (...)
    2026-01-21 ccawley2011 Replace use of sprintf with snprintf
    2026-01-21 robux4 CI: switch debian-llvm-mingw to UCRT
    2026-01-21 ccawley2011 Fix reversed parameters when TRACK_HEAP_ALLOCATIONS is enabled
    2026-01-20 martin Silence a new MSVC warning
    2026-01-14 martin checkasm: Reinstate check for TRIM_DSP_FUNCTIONS
    2026-01-09 martin arm: Fix a few misindented lines
    2026-01-04 martin checkasm: Pass HAVE_C11_GENERIC to checkasm as -DCHECKASM_HAVE_GENERIC=1/0
    2026-01-03 martin checkasm: Fix building with MSVC
    2025-10-07 git tests/checkasm: switch to external checkasm
    2025-10-07 git tests/checkasm/loopfilter: avoid printf format warning
    
    Created with:
      roll-dep src/third_party/dav1d/libdav1d
    R=dalecurtis@chromium.org,eugene@chromium.org
    
    Change-Id: I2d174f314155abeef7e8660af625538b2b2e1bb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7648256
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1596668}

--- third_party/dav1d/config/linux/arm64/config.h
+++ third_party/dav1d/config/linux/arm64/config.h
@@ -38,8 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // #define CONFIG_LOG 1 -- Logging is controlled by Chromium
 
-#define CONFIG_MACOS_KPERF 0
-
 #define ENDIANNESS_BIG 0
 
 #define HAVE_ALIGNED_ALLOC 1
@@ -87,6 +85,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define HAVE_PTHREAD_SETNAME_NP 1
 
 #define HAVE_PTHREAD_SET_NAME_NP 0
+
+#define HAVE_SIGACTION 1
 
 #define HAVE_SVE 1
 
--- third_party/dav1d/config/win/arm64/config.h
+++ third_party/dav1d/config/win/arm64/config.h
@@ -36,8 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // #define CONFIG_LOG 1 -- Logging is controlled by Chromium
 
-#define CONFIG_MACOS_KPERF 0
-
 #define ENDIANNESS_BIG 0
 
 #define HAVE_ALIGNED_ALLOC 0
@@ -85,6 +83,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define HAVE_PTHREAD_SETNAME_NP 0
 
 #define HAVE_PTHREAD_SET_NAME_NP 0
+
+#define HAVE_SIGACTION 0
 
 #define HAVE_SVE 0
 
