8cf7210bd3ddd42ea4e3a30c9a75e6a9361aa6fd
Anatoli Hancharou <antoli@google.com>
2026-03-09 05:57:34 -0700

    Add a feature flag for client certificate provisioning on iOS to chrome://flags.
    This flag enables the ability for enterprise users on iOS to provision client certificates from the cloud.
    
    Change-Id: I457f7e5c43fb07ef8ed378d981e4b28ec54eec70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7638444
    Reviewed-by: Aliaksei Bahdzevich <thelex@google.com>
    Commit-Queue: Anatoli Hancharou <antoli@google.com>
    Cr-Commit-Position: refs/heads/main@{#1596280}

--- ios/chrome/browser/flags/about_flags.mm
+++ ios/chrome/browser/flags/about_flags.mm
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/download/public/background_service/features.h"
 #import "components/enterprise/browser/enterprise_switches.h"
 #import "components/enterprise/buildflags/buildflags.h"
+#import "components/enterprise/client_certificates/core/features.h"
 #import "components/enterprise/connectors/core/features.h"
 #import "components/feature_engagement/public/feature_constants.h"
 #import "components/feature_engagement/public/feature_list.h"
@@ -2739,6 +2740,12 @@ constexpr auto kFeatureEntries = std::to_array<flags_ui::FeatureEntry>({
     {"ios-cobalt", flag_descriptions::kIOSCobaltName,
      flag_descriptions::kIOSCobaltDescription, flags_ui::kOsIos,
      FEATURE_VALUE_TYPE(web::features::kIOSCobalt)},
+    {"enable-client-certificate-provisioning-on-ios",
+     flag_descriptions::kEnableClientCertificateProvisioningOnIOSName,
+     flag_descriptions::kEnableClientCertificateProvisioningOnIOSDescription,
+     flags_ui::kOsIos,
+     FEATURE_VALUE_TYPE(client_certificates::features::
+                            kEnableClientCertificateProvisioningOnIOS)},
 });
 
 bool SkipConditionalFeatureEntry(const flags_ui::FeatureEntry& entry) {
