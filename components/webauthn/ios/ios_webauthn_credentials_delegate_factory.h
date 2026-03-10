91e8a14dc057ccf0a68e45e368a042cacd24a6df
Gauthier Ambard <gambard@chromium.org>
2026-03-10 03:52:10 -0700

    [iOS] Update ios/web BUILD.gn deps
    Cleanup deps.
    
    Bug: 483998779
    Change-Id: I35a613e67b8db7d5a0c09993afeb78a579bbfa2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7638529
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1596959}

--- components/webauthn/ios/ios_webauthn_credentials_delegate_factory.h
+++ components/webauthn/ios/ios_webauthn_credentials_delegate_factory.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_WEBAUTHN_IOS_IOS_WEBAUTHN_CREDENTIALS_DELEGATE_FACTORY_H_
 #define COMPONENTS_WEBAUTHN_IOS_IOS_WEBAUTHN_CREDENTIALS_DELEGATE_FACTORY_H_
 
+#import "base/containers/flat_map.h"
 #import "base/memory/raw_ptr.h"
 #import "components/webauthn/ios/ios_webauthn_credentials_delegate.h"
 #import "ios/web/public/js_messaging/web_frames_manager.h"
