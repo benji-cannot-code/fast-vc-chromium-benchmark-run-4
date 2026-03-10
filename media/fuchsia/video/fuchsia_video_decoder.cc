0832abdddc7c9306c7ad2641d28749b74880121d
Arthur Sonzogni <arthursonzogni@chromium.org>
2026-03-10 03:52:00 -0700

    media/fuchsia: Replace sscanf with safer string conversion
    This addresses the code review suggestion asking for the safe way
    to parse the minimum buffer size. We replace `sscanf` with
    `base::SplitStringPiece` and `base::StringToSizeT`.
    
    Bug: 409340989
    Change-Id: Ie099d087cc23b87f634082c80e350adb2f383b70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7643428
    Reviewed-by: Zijie He <zijiehe@google.com>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1596958}

--- media/fuchsia/video/fuchsia_video_decoder.cc
+++ media/fuchsia/video/fuchsia_video_decoder.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/fuchsia/video/fuchsia_video_decoder.h"
 
 #include <fuchsia/sysmem/cpp/fidl.h>
-#include <inttypes.h>
 #include <lib/zx/eventpair.h>
 #include <vulkan/vulkan.h>
 
@@ -19,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/strings/string_number_conversions.h"
+#include "base/strings/string_split.h"
 #include "base/task/bind_post_task.h"
 #include "base/task/sequenced_task_runner.h"
 #include "components/viz/common/gpu/raster_context_provider.h"
@@ -89,10 +90,13 @@ std::optional<gfx::Size> ParseMinBufferSize() {
           switches::kMinVideoDecoderOutputBufferSize);
   if (min_buffer_size_arg.empty())
     return std::nullopt;
+
+  auto parts = base::SplitStringPiece(
+      min_buffer_size_arg, "x", base::TRIM_WHITESPACE, base::SPLIT_WANT_ALL);
   size_t width;
   size_t height;
-  if (UNSAFE_TODO(sscanf(min_buffer_size_arg.c_str(), "%zux%zu" SCNu32, &width,
-                         &height)) != 2) {
+  if (parts.size() != 2 || !base::StringToSizeT(parts[0], &width) ||
+      !base::StringToSizeT(parts[1], &height)) {
     LOG(WARNING) << "Invalid value for --"
                  << switches::kMinVideoDecoderOutputBufferSize << ": '"
                  << min_buffer_size_arg << "'";
