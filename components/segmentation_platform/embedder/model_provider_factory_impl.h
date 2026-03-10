f63d4dc82d841a6689b5ecf2309515d2bd364537
Sylvain Defresne <sdefresne@chromium.org>
2026-03-10 11:31:18 -0700

    Clean global state after tests overriding the segmentation_platform model
    Overriding the segmentation_platform model is done by setting state in a
    singleton which stays between tests. This causes following tests to fail
    when run in the same process as the singleton is still present and thus
    prevents installation of a new override.
    
    Change the way the override are installed by using installing/removing a
    global pointer to a TestDefaultModelOverride instance which is scoped to
    the test fixture lifetime.
    
    This ensure that the state is cleared after the execution of the fixture
    and that the state don't leak to the following tests.
    
    Bug: none
    Change-Id: Ibbf682473bc58a5564f93b8084f47f914047abd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7649055
    Reviewed-by: Salvador Guerrero Ramos <salg@google.com>
    Commit-Queue: Salvador Guerrero Ramos <salg@google.com>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1597200}

--- components/segmentation_platform/embedder/model_provider_factory_impl.h
+++ components/segmentation_platform/embedder/model_provider_factory_impl.h
@@ -52,9 +52,11 @@ class ModelProviderFactoryImpl : public ModelProviderFactory {
 // Used only in tests to override the default model.
 class TestDefaultModelOverride {
  public:
-  static TestDefaultModelOverride& GetInstance();
+  static TestDefaultModelOverride* GetInstance();
 
+  TestDefaultModelOverride();
   ~TestDefaultModelOverride();
+
   TestDefaultModelOverride(const TestDefaultModelOverride& client) = delete;
   TestDefaultModelOverride& operator=(const TestDefaultModelOverride& client) =
       delete;
@@ -66,10 +68,6 @@ class TestDefaultModelOverride {
                           std::unique_ptr<DefaultModelProvider>);
 
  private:
-  friend class base::NoDestructor<TestDefaultModelOverride>;
-
-  TestDefaultModelOverride();
-
   std::map<proto::SegmentId, std::unique_ptr<DefaultModelProvider>>
       default_providers_;
 };
