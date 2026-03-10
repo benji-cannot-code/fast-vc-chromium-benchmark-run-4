ee47132b6f708d2fafc46b25943ecf9bd456463c
Lukasz Anforowicz <lukasza@chromium.org>
2026-03-10 12:50:32 -0700

    [rust] Fix Clippy warnings: //ui/android/texture_compressor
    Bug: 472355480
    Change-Id: I3716df76990a616980fdea1e3a8eead06d417fff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7649914
    Reviewed-by: Junichi Uekawa <uekawa@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Tatsuyuki Ishi <ishitatsuyuki@google.com>
    Cr-Commit-Position: refs/heads/main@{#1597256}

--- ui/android/texture_compressor/decoder_tests.rs
+++ ui/android/texture_compressor/decoder_tests.rs
@@ -3,6 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// Bits of inputs are grouped in a way that follows their inner structure.
+#![allow(clippy::unusual_byte_groupings)]
+
 chromium::import! {
     "//ui/android:texture_compressor";
 }
