272a8c3aa5de268212609c8d30fd5a351705743f
Luke Warlow <lwarlow@igalia.com>
2026-03-09 18:24:46 -0700

    Add initial base appearance UA-styles for remaining textual inputs
    Future patches will adjust the pseudo-elements that apply.
    
    These changes are gated behind the AppearanceBase flag.
    
    See https://drafts.csswg.org/css-forms-1
    
    Bug: 450139531
    Change-Id: I76f4d37304ba35e1bc91e4306e89677a6109a5f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7650314
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Luke <lwarlow@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1596761}

--- third_party/blink/renderer/core/html/forms/text_input_type.h
+++ third_party/blink/renderer/core/html/forms/text_input_type.h
@@ -41,8 +41,6 @@ class TextInputType final : public BaseTextInputType {
   TextInputType(HTMLInputElement& element)
       : BaseTextInputType(Type::kText, element) {}
 
-  bool SupportsBaseAppearance(Element::BaseAppearanceValue) const override;
-
  private:
   void CountUsage() override;
   bool SupportsInputModeAttribute() const override;
