9bc40575189e85b2a0dec1d9fe99ddd69bbb613b
Ernest Nguyen Hung <ernn@google.com>
2026-03-10 08:29:44 -0700

    [FRE] Implement Arm 1 FRE Sign-in experiment
    It implements first arm of the sign-in experiments on the FRE sign-in
    promo page, i.e. it moves 'Don't sign in' button to the top right corner
    and adds a copy informing a user that they can create an account in the
    next step(s).
    
    Note: comparing to mocks it doesn't contain the logo below the create
    account description. The decision regarding the logo is pending
    alignment.
    
    Screenshots (internal only):
    
    Light mode - https://screenshot.googleplex.com/58LtdJKMcDumyV3.
    
    Dark mode - https://screenshot.googleplex.com/AdvwG86TGYeNXh5.
    
    Bug: 489667011
    Change-Id: I01cabc1005710d04f1797aa89aa1964737f62c99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7645593
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Commit-Queue: Ernest Nguyen Hung <ernn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1597083}

--- chrome/browser/resources/intro/sign_in_promo_refresh.ts
+++ chrome/browser/resources/intro/sign_in_promo_refresh.ts
@@ -19,6 +19,14 @@ import {IntroBrowserProxyImpl} from './browser_proxy.js';
 import {getCss} from './sign_in_promo_refresh.css.js';
 import {getHtml} from './sign_in_promo_refresh.html.js';
 
+// LINT.IfChange(Variation)
+export enum Variation {
+  DEFAULT = 0,
+  DONT_SIGN_IN_IN_TOP_RIGHT_CORNER = 1,
+  DONT_SIGN_IN_ON_GAIA = 2,
+}
+// LINT.ThenChange(//components/signin/public/base/signin_switches.h:FirstRunDesktopSignInPromoVariation)
+
 export interface SignInPromoRefreshElement {
   $: {
     acceptSignInButton: CrButtonElement,
@@ -65,8 +73,6 @@ export class SignInPromoRefreshElement extends SignInPromoRefreshElementBase {
     };
   }
 
-  private browserProxy_: IntroBrowserProxy =
-      IntroBrowserProxyImpl.getInstance();
   protected accessor benefitCards_: BenefitCard[];
   protected accessor managedDeviceDisclaimer_: string = '';
   protected accessor isDeviceManaged_: boolean =
@@ -74,6 +80,10 @@ export class SignInPromoRefreshElement extends SignInPromoRefreshElementBase {
   protected accessor usePrimaryAndTonalButtonsForPromos_: boolean =
       loadTimeData.getBoolean('usePrimaryAndTonalButtonsForPromos');
   private accessor anyButtonClicked_: boolean = false;
+  private browserProxy_: IntroBrowserProxy =
+      IntroBrowserProxyImpl.getInstance();
+  private variation_: Variation =
+      loadTimeData.getInteger('signInPromoVariation') as Variation;
 
   constructor() {
     super();
@@ -145,6 +155,10 @@ export class SignInPromoRefreshElement extends SignInPromoRefreshElementBase {
   protected getDisclaimerVisibilityClass_(): string {
     return this.managedDeviceDisclaimer_.length === 0 ? 'temporarily-hidden' :
                                                         'fast-fade-in';
+  }
+
+  protected get isTopRightCornerVariation_(): boolean {
+    return this.variation_ === Variation.DONT_SIGN_IN_IN_TOP_RIGHT_CORNER;
   }
 }
 
