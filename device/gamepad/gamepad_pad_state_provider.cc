9a9cc15711666649f6680181574183fb146f4ee6
Zekun Jiang <zekunjiang@google.com>
2026-03-10 14:00:00 -0700

    Reland "Fix unsafe buffer usage in gamepad_pad_state_provider.cc."
    This is a reland of commit dc5a9013acaea36b8408b84c332728dc15428e51
    
    Original change's description:
    > Fix unsafe buffer usage in gamepad_pad_state_provider.cc.
    >
    > Replaced memset with assignment to `PadState()` to reset state.
    >
    > Fixed: 457303428
    > Change-Id: I4d3d9f9ad6e7a002643736bdd40c76073f671694
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7617566
    > Reviewed-by: Reilly Grant <reillyg@chromium.org>
    > Commit-Queue: Zekun Jiang <zekunjiang@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1593569}
    
    Change-Id: I06583052366b1b2c394a3bd4f2398f4aece7f265
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7651743
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Zekun Jiang <zekunjiang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1597310}

--- device/gamepad/gamepad_pad_state_provider.cc
+++ device/gamepad/gamepad_pad_state_provider.cc
@@ -86,7 +86,7 @@ PadState* GamepadPadStateProvider::GetConnectedPadState(uint32_t pad_index) {
 }
 
 void GamepadPadStateProvider::ClearPadState(PadState& state) {
-  UNSAFE_TODO(memset(&state, 0, sizeof(PadState)));
+  state = PadState();
 }
 
 void GamepadPadStateProvider::InitializeDataFetcher(
@@ -101,7 +101,7 @@ void GamepadPadStateProvider::MapAndSanitizeGamepadData(PadState* pad_state,
   DCHECK(pad);
 
   if (!pad_state->data.connected) {
-    UNSAFE_TODO(memset(pad, 0, sizeof(Gamepad)));
+    *pad = Gamepad();
     return;
   }
 
--- device/gamepad/public/cpp/gamepad.h
+++ device/gamepad/public/cpp/gamepad.h
@@ -69,7 +69,7 @@ class GamepadHapticActuator {
   static constexpr double kMaxEffectDurationMillis = 5000.0;
 
   bool not_null = false;
-  GamepadHapticActuatorType type;
+  GamepadHapticActuatorType type = GamepadHapticActuatorType::kVibration;
 };
 
 class GamepadEffectParameters {
@@ -180,7 +180,7 @@ class GamepadImpl {
 
   GamepadPose pose;
 
-  GamepadHand hand;
+  GamepadHand hand = GamepadHand::kNone;
 
   unsigned display_id = 0;
 
