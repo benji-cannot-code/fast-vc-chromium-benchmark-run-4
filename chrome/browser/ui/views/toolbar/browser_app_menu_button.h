14f444930ae81463b03da4cf80af8443aebe1ef7
Emily Shack <emshack@chromium.org>
2026-03-10 15:59:39 -0700

    [GlowUp] Animate the app menu icon on hover
    Adds a Lottie animation so that the app menu transforms into the Chrome
    logo when hovered. This is implemented behind the kTabStripDeclutter
    flag.
    
    Also updates the app menu icon (not behind a flag), which didn't match
    the latest UX mocks
    
    Bug: 486253264
    Change-Id: I040f74e60bfae26ff87ac8245a7b0d1d00f23fc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7649384
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1597385}

--- chrome/browser/ui/views/toolbar/browser_app_menu_button.h
+++ chrome/browser/ui/views/toolbar/browser_app_menu_button.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/user_education/common/feature_promo/feature_promo_handle.h"
 #include "ui/base/interaction/element_identifier.h"
 #include "ui/base/metadata/metadata_header_macros.h"
+#include "ui/lottie/animation.h"
 #include "ui/views/view.h"
 
 class ToolbarView;
@@ -55,6 +56,9 @@ class BrowserAppMenuButton : public AppMenuButton {
   // Updates the presentation according to |severity_| and the theme provider.
   void UpdateIcon() override;
 
+  // views::AnimationDelegate:
+  void AnimationProgressed(const gfx::Animation* animation) override;
+
   // Need to override to implement the Expand and Collapse actions.
   bool HandleAccessibleAction(const ui::AXActionData& action_data) override;
 
@@ -93,6 +97,8 @@ class BrowserAppMenuButton : public AppMenuButton {
       ui::TouchUiController::Get()->RegisterCallback(
           base::BindRepeating(&BrowserAppMenuButton::OnTouchUiChanged,
                               base::Unretained(this)));
+
+  std::unique_ptr<lottie::Animation> lottie_animation_;
 
   // Used to spawn weak pointers for delayed tasks to open the overflow menu.
   base::WeakPtrFactory<BrowserAppMenuButton> weak_factory_{this};
