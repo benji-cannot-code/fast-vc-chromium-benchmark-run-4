85dfe1073894ae68c68222490150603effdae967
Danilo Tedeschi <danft@google.com>
2026-03-10 16:24:36 -0700

    Add metrics increments to on-device-translation service controller
    I am renaming the metrics that are specific to TranslationManagerImpl
    that is only used by the TranslationApi product and moving the
    OnDeviceTranslation metrics to the ServiceController since every
    on-device-translation call goes through there.
    
    Bug: 489667379
    Change-Id: I47ee0860dc43204cfb74aa9d8942814de7d0b429
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7642951
    Commit-Queue: Danilo Françoso Tedeschi <danft@google.com>
    Reviewed-by: Evan Liu <evliu@google.com>
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1597394}

--- components/on_device_translation/translator.cc
+++ components/on_device_translation/translator.cc
@@ -58,8 +58,8 @@ bool Translator::VerifyPrerequisites(
     return false;
   }
 
-  RecordTranslationAPICallForLanguagePair("Translate", source_lang_,
-                                          target_lang_);
+  RecordTranslatorApiCallForLanguagePair("Translate", source_lang_,
+                                         target_lang_);
   RecordTranslationCharacterCount(source_lang_, target_lang_, input.size());
 
   // https://github.com/webmachinelearning/translation-api/pull/38: "If |input|
