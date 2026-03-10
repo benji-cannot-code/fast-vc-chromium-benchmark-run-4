496ddadf45beecf1c557116ef1b60bdd465e4569
Maks Orlovich <morlovich@chromium.org>
2026-03-10 10:21:29 -0700

    Implement WebUILocationBar::GetAnchorOrNull
    This will likely be needed for various chips soonish.
    
    Bug: 470042732
    Change-Id: I523f24ceee4c13510263b81a2d9bcb247eaff3e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7649308
    Reviewed-by: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1597159}

--- chrome/browser/ui/views/location_bar/webui_location_bar.cc
+++ chrome/browser/ui/views/location_bar/webui_location_bar.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notimplemented.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_command_controller.h"
+#include "chrome/browser/ui/browser_element_identifiers.h"
+#include "chrome/browser/ui/interaction/browser_elements.h"
 #include "chrome/browser/ui/omnibox/chrome_omnibox_client.h"
 #include "chrome/browser/ui/omnibox/omnibox_controller.h"
 #include "chrome/browser/ui/views/bubble_anchor_util_views.h"
@@ -101,8 +103,7 @@ WebUILocationBar::GetChipAnchor() {
 }
 
 ui::TrackedElement* WebUILocationBar::GetAnchorOrNull() {
-  NOTIMPLEMENTED();
-  return nullptr;
+  return BrowserElements::From(browser_)->GetElement(kLocationBarElementId);
 }
 
 Browser* WebUILocationBar::GetBrowser() {
