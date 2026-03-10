a8cc063e35ea0b7e114d6b56a326de6535768486
Devlin Cronin <rdevlin.cronin@chromium.org>
2026-03-10 15:37:02 -0700

    [Extensions] Update content_settings_apitest.cc to MV3
    MV2 has been unsupported for several months. We can remove testing
    support for it. Existing tests should be updated to MV3.
    
    Update tests in content_settings_apitest.cc to use manifest version 3
    instead of manifest version 2 and 3.
    
    With the transition to MV3, many of these tests may also be able to be
    updated to use new patterns, such as async / await, service worker
    modules, and more. However, those changes are out of scope for these
    CLs, and can be addressed in followups.
    
    Bug: 491516661
    Change-Id: Ie19fe30c8e8a68bdfef9ad1ab9923a143b5ed1bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7649826
    Reviewed-by: Tim <tjudkins@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1597371}

--- chrome/browser/extensions/api/content_settings/content_settings_apitest.cc
+++ chrome/browser/extensions/api/content_settings/content_settings_apitest.cc
@@ -311,31 +311,6 @@ INSTANTIATE_TEST_SUITE_P(All,
                          ExtensionContentSettingsApiTestWithClipboard,
                          ::testing::Bool());
 
-class ExtensionContentSettingsApiTestWithContextType
-    : public ExtensionContentSettingsApiTest,
-      public testing::WithParamInterface<ContextType> {
- public:
-  ExtensionContentSettingsApiTestWithContextType()
-      : ExtensionContentSettingsApiTest(GetParam()) {}
-  ~ExtensionContentSettingsApiTestWithContextType() override = default;
-  ExtensionContentSettingsApiTestWithContextType(
-      const ExtensionContentSettingsApiTestWithContextType&) = delete;
-  ExtensionContentSettingsApiTestWithContextType& operator=(
-      const ExtensionContentSettingsApiTestWithContextType&) = delete;
-};
-
-// Android only supports MV3 and later, therefore don't need to test for
-// persistent background context.
-#if !BUILDFLAG(IS_ANDROID)
-INSTANTIATE_TEST_SUITE_P(PersistentBackground,
-                         ExtensionContentSettingsApiTestWithContextType,
-                         ::testing::Values(ContextType::kPersistentBackground));
-#endif
-
-INSTANTIATE_TEST_SUITE_P(ServiceWorker,
-                         ExtensionContentSettingsApiTestWithContextType,
-                         ::testing::Values(ContextType::kServiceWorker));
-
 IN_PROC_BROWSER_TEST_P(ExtensionContentSettingsApiTestWithClipboard, Standard) {
   CheckContentSettingsDefault();
 
@@ -361,7 +336,7 @@ IN_PROC_BROWSER_TEST_P(ExtensionContentSettingsApiTestWithClipboard, Standard) {
   CheckContentSettingsDefault();
 }
 
-IN_PROC_BROWSER_TEST_P(ExtensionContentSettingsApiTestWithContextType,
+IN_PROC_BROWSER_TEST_F(ExtensionContentSettingsApiTest,
                        UnsupportedDefaultSettings) {
   const char kExtensionPath[] = "content_settings/unsupporteddefaultsettings";
   EXPECT_TRUE(RunExtensionTest(kExtensionPath)) << message_;
@@ -369,8 +344,7 @@ IN_PROC_BROWSER_TEST_P(ExtensionContentSettingsApiTestWithContextType,
 
 // Tests if an extension clearing content settings for one content type leaves
 // the others unchanged.
-IN_PROC_BROWSER_TEST_P(ExtensionContentSettingsApiTestWithContextType,
-                       ClearProperlyGranular) {
+IN_PROC_BROWSER_TEST_F(ExtensionContentSettingsApiTest, ClearProperlyGranular) {
   const char kExtensionPath[] = "content_settings/clearproperlygranular";
   EXPECT_TRUE(RunExtensionTest(kExtensionPath)) << message_;
 }
@@ -417,7 +391,7 @@ IN_PROC_BROWSER_TEST_F(ExtensionContentSettingsApiTest,
       << message_;
 }
 
-IN_PROC_BROWSER_TEST_P(ExtensionContentSettingsApiTestWithContextType,
+IN_PROC_BROWSER_TEST_F(ExtensionContentSettingsApiTest,
                        EmbeddedSettingsMetric) {
   base::HistogramTester histogram_tester;
   const char kExtensionPath[] = "content_settings/embeddedsettingsmetric";
