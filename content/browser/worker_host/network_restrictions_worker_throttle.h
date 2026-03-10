24d61fd085c25f687261864a5084c4809a9374d4
Xiaochen Zhou <xiaochenzh@chromium.org>
2026-03-10 05:39:21 -0700

    [Connection Allowlist] Fix dangling pointer to StoragePartition in NetworkRestrictionsWorkerThrottle
    Use weak ptr instead.
    
    Verified by running test DedicatedWorkerServiceObserver locally with
    connection allowlist feature enabled. See linked bug for details.
    
    Bug: 490396395
    Change-Id: I303dc12957d089b248a35e3e4a3d671b3a5f27f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7639700
    Reviewed-by: Andrew Verge <averge@chromium.org>
    Commit-Queue: Xiaochen Zhou <xiaochenzh@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1597001}

--- content/browser/worker_host/network_restrictions_worker_throttle.h
+++ content/browser/worker_host/network_restrictions_worker_throttle.h
@@ -6,14 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_BROWSER_WORKER_HOST_NETWORK_RESTRICTIONS_WORKER_THROTTLE_H_
 #define CONTENT_BROWSER_WORKER_HOST_NETWORK_RESTRICTIONS_WORKER_THROTTLE_H_
 
-#include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "base/unguessable_token.h"
 #include "content/browser/renderer_host/policy_container_host.h"
 #include "third_party/blink/public/common/loader/url_loader_throttle.h"
 
 namespace content {
 
-class StoragePartition;
+class StoragePartitionImpl;
 
 // A URLLoaderThrottle that applies network restrictions for a worker's
 // subresources based on the worker script's response headers (for network
@@ -21,12 +21,12 @@ class StoragePartition;
 class NetworkRestrictionsWorkerThrottle : public blink::URLLoaderThrottle {
  public:
   static std::unique_ptr<NetworkRestrictionsWorkerThrottle> Create(
-      StoragePartition* storage_partition,
+      base::WeakPtr<StoragePartitionImpl> storage_partition,
       const base::UnguessableToken& network_restrictions_id,
       PolicyContainerPolicies creator_policies);
 
   NetworkRestrictionsWorkerThrottle(
-      StoragePartition* storage_partition,
+      base::WeakPtr<StoragePartitionImpl> storage_partition,
       const base::UnguessableToken& network_restrictions_id,
       PolicyContainerPolicies creator_policies);
   ~NetworkRestrictionsWorkerThrottle() override;
@@ -40,7 +40,7 @@ class NetworkRestrictionsWorkerThrottle : public blink::URLLoaderThrottle {
  private:
   void OnRevokeComplete();
 
-  raw_ptr<StoragePartition> storage_partition_;
+  base::WeakPtr<StoragePartitionImpl> storage_partition_;
   const base::UnguessableToken network_restrictions_id_;
   const PolicyContainerPolicies creator_policies_;
 
