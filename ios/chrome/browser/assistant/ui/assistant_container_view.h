7f8b5dbb5226738e85cb85ce01f40ba3f2321155
Ewann Pelle <ewannpv@chromium.org>
2026-03-10 05:03:00 -0700

    [iOS][Assistant] Remove scroll view from AssistantContainerView
    This CL removes the scroll view from the AssistantContainerView to
    facilitate embedder interactions.
    
    Bug: 469050167
    Change-Id: Ibae9d60acc459554da601ae484bf4dc69c272102
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7651494
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1596986}

--- ios/chrome/browser/assistant/ui/assistant_container_view.h
+++ ios/chrome/browser/assistant/ui/assistant_container_view.h
@@ -15,11 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // +----------------------------------+
 // |             Grabber              |
 // +----------------------------------+
-// |           scrollView             |
-// |  +----------------------------+  |
-// |  |        contentView         |  |
-// |  |  (Child VC View goes here) |  |
-// |  +----------------------------+  |
+// |           contentView            |
+// |    (Child VC View goes here)     |
 // +----------------------------------+
 @interface AssistantContainerView : UIView
 
