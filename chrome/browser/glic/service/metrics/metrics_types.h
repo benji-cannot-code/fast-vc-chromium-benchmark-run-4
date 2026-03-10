ef1284ff26b811422ad1a24700db1ab3705c4bad
Basia Zimirska <basiaz@google.com>
2026-03-10 13:54:05 -0700

    [Glic Metrics] Duration for first side panel open.
    This CL adds a Glic.Instance.{Entrypoint}.SidePanelFirstOpenDuration metric that records how long side panel was open for the first time when instance is created. Switching tabs counts as side panel closure.
    
    Bug: 487306585
    Change-Id: Ief1a23add1f2aafa4cadb4b6df4fd0a227751c69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7638564
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Commit-Queue: Basia Zimirska <basiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1597300}

--- chrome/browser/glic/service/metrics/metrics_types.h
+++ chrome/browser/glic/service/metrics/metrics_types.h
@@ -50,7 +50,7 @@ enum class GlicEntrypoint {
 
 GlicEntrypoint GetEntrypointFromInvocationSource(
     mojom::InvocationSource source);
-
+std::string GetEntrypointString(GlicEntrypoint entrypoint);
 }  // namespace glic
 
 #endif  // CHROME_BROWSER_GLIC_SERVICE_METRICS_METRICS_TYPES_H_
