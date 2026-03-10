a08fd19642f0716e91d29141b55d28404a0ed5e8
Mark Schillaci <mschillaci@google.com>
2026-03-09 17:53:07 -0700

    [Side Panel Infra] Stub wrapper for Android specific Side Panel view
    This CL puts a simple wrapper class around the SidePanelNativeView for
    Android that is currently only a jobject. We do this so that we can
    add additional methods so that even with an object alias we can match
    the same approach as Desktop without the need for many #ifdefs. There
    are some methods we can not perform on the jobject ref directly, so by
    using a wrapper class we can add the specialized methods that already
    exist for the views::View implementation, without the need for
    many #ifdefs to bypass them.
    
    Bypass-Check-License: Warning is from upstream CL file move.
    Bug: 489780669
    Change-Id: Ib159f34c93ea49a6118cf3b538bab84ec520d35f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7644186
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1596747}

--- chrome/browser/ui/side_panel/side_panel_native_view.h
+++ chrome/browser/ui/side_panel/side_panel_native_view.h
@@ -6,19 +6,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_SIDE_PANEL_SIDE_PANEL_NATIVE_VIEW_H_
 #define CHROME_BROWSER_UI_SIDE_PANEL_SIDE_PANEL_NATIVE_VIEW_H_
 
+#include <memory>
+
 #include "build/build_config.h"
+#include "ui/base/class_property.h"
 
 #if BUILDFLAG(IS_ANDROID)
 #include "base/android/scoped_java_ref.h"
 #include "third_party/jni_zero/jni_zero.h"
 #else
-#include <memory>
-
 #include "ui/views/view.h"
 #endif
 
 #if BUILDFLAG(IS_ANDROID)
-using SidePanelNativeView = base::android::ScopedJavaGlobalRef<jobject>;
+class SidePanelNativeViewAndroid : public ui::PropertyHandler {
+ public:
+  explicit SidePanelNativeViewAndroid(
+      base::android::ScopedJavaGlobalRef<jobject> view)
+      : view_(std::move(view)) {}
+  ~SidePanelNativeViewAndroid() override = default;
+
+  base::android::ScopedJavaGlobalRef<jobject> view() const { return view_; }
+
+ private:
+  base::android::ScopedJavaGlobalRef<jobject> view_;
+};
+
+using SidePanelNativeView = std::unique_ptr<SidePanelNativeViewAndroid>;
 #else
 using SidePanelNativeView = std::unique_ptr<views::View>;
 #endif
