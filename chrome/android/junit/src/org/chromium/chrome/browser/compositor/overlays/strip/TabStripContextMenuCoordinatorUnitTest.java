a5e61e0eeae010fd97bb1cb63f345c7658416009
Michael Wu <mklw@google.com>
2026-03-10 13:25:20 -0700

    Remove duplicate Glic strings on Android
    Reuse existing Glic strings in cross platform glic_strings.grdp
    
    Change-Id: I96858dbf95f3cbcccd8eade498899ad0849c9472
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7649582
    Reviewed-by: Neil Coronado <nemco@google.com>
    Reviewed-by: Hailey Wang <haileywang@google.com>
    Commit-Queue: Michael Wu <mklw@google.com>
    Cr-Commit-Position: refs/heads/main@{#1597274}

--- chrome/android/junit/src/org/chromium/chrome/browser/compositor/overlays/strip/TabStripContextMenuCoordinatorUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/compositor/overlays/strip/TabStripContextMenuCoordinatorUnitTest.java
@@ -225,8 +225,7 @@ public class TabStripContextMenuCoordinatorUnitTest {
         mCoordinator.showMenu(mRectProvider, false, mActivity);
         verifyMenuState(/* expectedNumItems= */ 6);
         assertEquals(
-                R.string.menu_pin_glic,
-                getItemModelAtPosition(5).get(ListMenuItemProperties.TITLE_ID));
+                R.string.glic_pin, getItemModelAtPosition(5).get(ListMenuItemProperties.TITLE_ID));
 
         // Act: Select "Pin Gemini" option.
         mCoordinator
@@ -248,7 +247,7 @@ public class TabStripContextMenuCoordinatorUnitTest {
         mCoordinator.showMenu(mRectProvider, false, mActivity);
         verifyMenuState(/* expectedNumItems= */ 6);
         assertEquals(
-                R.string.menu_unpin_glic,
+                R.string.glic_unpin,
                 getItemModelAtPosition(5).get(ListMenuItemProperties.TITLE_ID));
 
         // Act: Select "Unpin Gemini" option.
