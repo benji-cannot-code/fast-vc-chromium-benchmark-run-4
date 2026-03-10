7f31bd9812c55964cb4852ebed18d49b573001fb
Philip Rogers <pdr@chromium.org>
2026-03-10 07:32:01 -0700

    [html-in-canvas] Fix IsCanvasOrInCanvasSubtree in frames
    This patch makes Element::IsCanvasOrInCanvasSubtree work inside frames.
    
    Bug: 448174609
    Change-Id: I1538f15cb5bdb3d7190232319d24cd5a1959d128
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7646352
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Stephen Chenney <schenney@chromium.org>
    Auto-Submit: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1597048}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -4200,6 +4200,11 @@ Node::InsertionNotificationRequest Element::InsertedInto(
   auto* parent = ParentOrShadowHostElement();
   if (parent && parent->IsCanvasOrInCanvasSubtree()) {
     SetIsCanvasOrInCanvasSubtree(true);
+  } else if (!parent && insertion_point.IsDocumentNode()) {
+    auto* owner = GetDocument().LocalOwner();
+    if (owner && owner->IsCanvasOrInCanvasSubtree()) {
+      SetIsCanvasOrInCanvasSubtree(true);
+    }
   }
 
   if (!insertion_point.IsInTreeScope()) {
@@ -10074,7 +10079,14 @@ bool Element::ShouldStoreComputedStyle(const ComputedStyle& style) const {
 
 bool Element::IsInCanvasSubtree() const {
   auto* parent = ParentOrShadowHostElement();
-  return parent && parent->IsCanvasOrInCanvasSubtree();
+  if (parent) {
+    return parent->IsCanvasOrInCanvasSubtree();
+  }
+  if (!isConnected()) {
+    return false;
+  }
+  auto* owner = GetDocument().LocalOwner();
+  return owner && owner->IsCanvasOrInCanvasSubtree();
 }
 
 AtomicString Element::ComputeInheritedLanguage() const {
--- third_party/blink/renderer/core/html/html_frame_owner_element.cc
+++ third_party/blink/renderer/core/html/html_frame_owner_element.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/style_change_reason.h"
 #include "third_party/blink/renderer/core/css/style_engine.h"
 #include "third_party/blink/renderer/core/dom/events/event.h"
+#include "third_party/blink/renderer/core/dom/shadow_root.h"
 #include "third_party/blink/renderer/core/events/current_input_event.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/exported/web_plugin_container_impl.h"
@@ -52,6 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/remote_frame.h"
 #include "third_party/blink/renderer/core/frame/remote_frame_view.h"
 #include "third_party/blink/renderer/core/frame/settings.h"
+#include "third_party/blink/renderer/core/html/canvas/html_canvas_element.h"
 #include "third_party/blink/renderer/core/html/fenced_frame/html_fenced_frame_element.h"
 #include "third_party/blink/renderer/core/html/lazy_load_frame_observer.h"
 #include "third_party/blink/renderer/core/html/loading_attribute.h"
@@ -232,6 +234,36 @@ Node::InsertionNotificationRequest HTMLFrameOwnerElement::InsertedInto(
   }
 
   return result;
+}
+
+static void SetIsCanvasOrInCanvasSubtreeRecursively(Element& element,
+                                                    bool is_in_canvas) {
+  if (IsA<HTMLCanvasElement>(element)) {
+    is_in_canvas = true;
+  }
+  if (element.IsCanvasOrInCanvasSubtree() == is_in_canvas) {
+    return;
+  }
+  element.SetIsCanvasOrInCanvasSubtree(is_in_canvas);
+
+  if (ShadowRoot* shadow_root = element.GetShadowRoot()) {
+    for (Element& child : ElementTraversal::ChildrenOf(*shadow_root)) {
+      SetIsCanvasOrInCanvasSubtreeRecursively(child, is_in_canvas);
+    }
+  }
+  for (Element& child : ElementTraversal::ChildrenOf(element)) {
+    SetIsCanvasOrInCanvasSubtreeRecursively(child, is_in_canvas);
+  }
+}
+
+void HTMLFrameOwnerElement::DidChangeIsCanvasOrInCanvasSubtree() {
+  HTMLElement::DidChangeIsCanvasOrInCanvasSubtree();
+  if (Document* inner_document = contentDocument()) {
+    if (Element* root = inner_document->documentElement()) {
+      SetIsCanvasOrInCanvasSubtreeRecursively(*root,
+                                              IsCanvasOrInCanvasSubtree());
+    }
+  }
 }
 
 void HTMLFrameOwnerElement::RemovedFrom(ContainerNode& insertion_point) {
