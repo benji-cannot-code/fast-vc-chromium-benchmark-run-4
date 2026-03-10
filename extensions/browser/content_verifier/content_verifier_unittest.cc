7ec439a8c69f7a42fe747e2bcad5aae0f3aa083f
Nikita Leshenko <nikita@island.io>
2026-03-10 03:21:00 -0700

    [Extensions] Define threading model for ContentVerifier::CreateIOData
    Commit 412555a579acf ([Extensions] Fix ContentVerifierJob race condition
    with extension roots, 2026-01-13) added calls to ResetIODataForTesting()
    from IO thread, which reaches CreateIOData().  Previously it was only
    called on the UI thread.
    
    Though there wasn't a defined thread affinity for CreateIOData(),
    CreateIOData() uses ChromeContentVerifierDelegate.  That delegate has a
    BrowserContext* and is Shutdown() on the UI thread, so in this CL we
    declare CreateIOData() to be a UI thread function to make mistakes
    around that area less likely.  (Also, historically CreateIOData() was
    created to do UI thread work that's passed to the IO thread.
    crrev.com/c/996489)
    
    Given this new definition, update ResetIODataForTesting() to create the
    IOData on the UI thread, post it to the IO thread to update io_data_,
    and invoke a callback when done.
    
    Add a ResetIOData() helper on ContentVerifierTest that wraps this with a
    RunLoop, and use it across all tests.
    
    Bug: 416484593
    Change-Id: I3ae0a1e13dd33e5c6c87c0784f42b8c324cefa63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7609642
    Reviewed-by: Solomon Kinard <solomonkinard@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Michael Maltsev <michaelm@island.io>
    Cr-Commit-Position: refs/heads/main@{#1596946}

--- extensions/browser/content_verifier/content_verifier_unittest.cc
+++ extensions/browser/content_verifier/content_verifier_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/memory/raw_ptr.h"
 #include "base/path_service.h"
+#include "base/run_loop.h"
 #include "base/strings/string_util.h"
 #include "base/synchronization/lock.h"
 #include "base/test/bind.h"
@@ -186,8 +187,12 @@ class ContentVerifierTest : public ExtensionsTest {
     OnContentVerifierReady();
   }
 
-  virtual void OnContentVerifierReady() {
-    content_verifier_->ResetIODataForTesting(extension_.get());
+  void OnContentVerifierReady() { ResetIOData(extension_.get()); }
+
+  void ResetIOData(const Extension* extension) {
+    base::RunLoop run_loop;
+    content_verifier_->ResetIODataForTesting(extension, run_loop.QuitClosure());
+    run_loop.Run();
   }
 
   void TearDown() override {
@@ -527,12 +532,6 @@ class ContentVerifierExtensionRootHashTest
         // Necessary to use BrowserTaskEnvironment::RunIOThreadUntilIdle().
         ContentVerifierTest(content::BrowserTaskEnvironment::REAL_IO_THREAD) {}
 
-  void OnContentVerifierReady() override {
-    RunTaskOnIOThreadAndWait(
-        base::BindOnce(&ContentVerifier::ResetIODataForTesting,
-                       content_verifier(), base::RetainedRef(extension())));
-  }
-
   void RunTaskOnIOThreadAndWait(base::OnceClosure task) {
     base::RunLoop run_loop;
     content::GetIOThreadTaskRunner({})->PostTask(
@@ -573,9 +572,7 @@ TEST_P(ContentVerifierExtensionRootHashTest,
     SCOPED_TRACE(
         "Waiting for the verifier to update with Extension B's data (same ID, "
         "different root)");
-    RunTaskOnIOThreadAndWait(
-        base::BindOnce(&ContentVerifier::ResetIODataForTesting,
-                       content_verifier(), base::RetainedRef(extension_b)));
+    ResetIOData(extension_b.get());
   }
   {
     TestContentHashWaiter waiter;
@@ -658,9 +655,7 @@ TEST_P(ContentVerifierExtensionRootHashTest, StaleJobOnUpdatedExtension) {
     SCOPED_TRACE(
         "Waiting for the verifier to update to Root B (simulating a "
         "corruption repair)");
-    RunTaskOnIOThreadAndWait(
-        base::BindOnce(&ContentVerifier::ResetIODataForTesting,
-                       content_verifier(), base::RetainedRef(extension_b)));
+    ResetIOData(extension_b.get());
   }
   {
     SCOPED_TRACE("waiting for extension with Root B to create it's hash");
