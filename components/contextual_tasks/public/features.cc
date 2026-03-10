117c1ce637299ad47baccfbd6f2587c47dc96554
Diana OuYang <dianaou@google.com>
2026-03-10 10:34:15 -0700

    Create finch flag to match SRP dark mode
    Prepare and begin to match colors of cobrowse UI to SRP. Guarded behind
    kContextualTasksUseStratusDarkModeColors flag.
    
    Demo: https://screencast.googleplex.com/cast/NTY3MDgxMjcxNDUzMjg2NHw4NGQxODE1OS01YQ
    Bug: b:490462172
    Change-Id: I0375a746921faa2fbe8a99f8f5ee4d5e1026a217
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7641754
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Commit-Queue: Diana OuYang <dianaou@google.com>
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1597165}

--- components/contextual_tasks/public/features.cc
+++ components/contextual_tasks/public/features.cc
@@ -64,6 +64,9 @@ BASE_FEATURE(kContextualTasksSendFullVersionListEnabled,
 BASE_FEATURE(kContextualTasksUrlRedirectToAimUrl,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kContextualTasksUseStratusDarkModeColors,
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 // If enabled, animates the caret.
 BASE_FEATURE(kContextualTasksAnimatedCaret, base::FEATURE_ENABLED_BY_DEFAULT);
 
@@ -461,6 +464,10 @@ bool ShouldEnableCookieSync() {
 bool ShouldEnableLockAndUnlockInputCapability() {
   return base::FeatureList::IsEnabled(kContextualTasks) &&
          kContextualTasksLockAndUnlockInputCapability.Get();
+}
+
+bool ShouldUseStratusDarkModeColors() {
+  return base::FeatureList::IsEnabled(kContextualTasksUseStratusDarkModeColors);
 }
 
 bool GetEnableFileHint() {
