ac096c2761e6430028fb41dd9d34b4ae2f7072b2
Sylvain Defresne <sdefresne@chromium.org>
2026-03-10 01:55:20 -0700

    Respect two-step shutdown of KeyedService in send tab to self service
    The KeyedService API recommends to break reference to other KeyedService
    in the Shutdown() method. In particular, HistoryService wants to enforce
    that all observers are removed after its Shutdown() method is called and
    that no new observers are registered after that point.
    
    Ensure that send tab to self KeyedServices destroy or unregister the
    HistoryService observers in their Shutdown() method in preparation for
    adding the new assertion in HistoryService.
    
    Bug: 481635191
    Change-Id: Ieceb6878b5044c1dbf0f334758baaf19ab8cb235
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7649351
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1596910}

--- chrome/browser/send_tab_to_self/send_tab_to_self_client_service.cc
+++ chrome/browser/send_tab_to_self/send_tab_to_self_client_service.cc
@@ -27,6 +27,7 @@ SendTabToSelfClientService::SendTabToSelfClientService(
 SendTabToSelfClientService::~SendTabToSelfClientService() = default;
 
 void SendTabToSelfClientService::Shutdown() {
+  model_observation_.Reset();
   receiving_ui_handler_.reset();
 }
 
--- components/send_tab_to_self/send_tab_to_self_sync_service.cc
+++ components/send_tab_to_self/send_tab_to_self_sync_service.cc
@@ -43,6 +43,10 @@ SendTabToSelfSyncService::SendTabToSelfSyncService(
 
 SendTabToSelfSyncService::~SendTabToSelfSyncService() = default;
 
+void SendTabToSelfSyncService::Shutdown() {
+  bridge_.reset();
+}
+
 void SendTabToSelfSyncService::OnSyncServiceInitialized(
     syncer::SyncService* sync_service) {
   sync_service_ = sync_service;
