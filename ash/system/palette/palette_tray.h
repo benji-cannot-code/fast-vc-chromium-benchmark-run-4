bb73c5d065df29ff5d94c6d5e981f4ac530831de
Zoraiz Naeem <zoraiznaeem@google.com>
2026-03-10 14:05:56 -0700

    Migrate PaletteTray to use ImagedTrayIcon
    This CL refactors PaletteTray to inherit from the more
    specialized ImagedTrayIcon class. This removes a significant amount of
    boilerplate code that was previously needed to manually create and
    configure an ImageView.
    
    This change is a pure refactoring and does not alter the component's
    behavior.
    
    Bug: b:463431615
    Change-Id: I8ce6472d097187352cfc4741246c2e10c428741d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7645832
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1597315}

--- ash/system/palette/palette_tray.h
+++ ash/system/palette/palette_tray.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell_observer.h"
 #include "ash/system/palette/palette_tool_manager.h"
 #include "ash/system/palette/stylus_battery_delegate.h"
+#include "ash/system/tray/imaged_tray_icon.h"
 #include "ash/system/tray/tray_background_view.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
@@ -39,7 +40,6 @@ class TouchEvent;
 }  // namespace ui
 
 namespace views {
-class ImageView;
 class Widget;
 }  // namespace views
 
@@ -63,9 +63,9 @@ class ASH_EXPORT PaletteTray : public AnnotatorController::AnnotatorObserver,
                                public SessionObserver,
                                public ShelfObserver,
                                public ShellObserver,
-                               public TrayBackgroundView,
+                               public ImagedTrayIcon,
                                public ui::InputDeviceEventObserver {
-  METADATA_HEADER(PaletteTray, TrayBackgroundView)
+  METADATA_HEADER(PaletteTray, ImagedTrayIcon)
 
  public:
   explicit PaletteTray(Shelf* shelf);
@@ -102,7 +102,7 @@ class ASH_EXPORT PaletteTray : public AnnotatorController::AnnotatorObserver,
   // display::DisplayManagerObserver:
   void OnDidApplyDisplayChanges() override;
 
-  // TrayBackgroundView:
+  // ImagedTrayIcon:
   void ClickedOutsideBubble(const ui::LocatedEvent& event) override;
   void UpdateTrayItemColor(bool is_active) override;
   void OnThemeChanged() override;
@@ -194,9 +194,6 @@ class ASH_EXPORT PaletteTray : public AnnotatorController::AnnotatorObserver,
   raw_ptr<PrefService> active_user_pref_service_ = nullptr;  // Not owned.
   std::unique_ptr<PrefChangeRegistrar> pref_change_registrar_local_;
   std::unique_ptr<PrefChangeRegistrar> pref_change_registrar_user_;
-
-  // Weak pointer, will be parented by TrayContainer for its lifetime.
-  raw_ptr<views::ImageView> icon_ = nullptr;
 
   // Cached palette pref value.
   bool is_palette_enabled_ = true;
