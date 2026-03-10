d068b1e36cbf32b15f09324bd1e1696d3daef485
Michal Mocny <mmocny@chromium.org>
2026-03-10 12:38:03 -0700

    [soft-navs] Fix Trace events to adjust to new timestamps.
    We recently adjusted the startTime of soft-navs backwards (from
    ~processingEnd-or-url-time to event timeStamp).
    
    That change automatically adjusted the timestamp value of the
    SoftNavigationStart trace event (by design) but we didn't adjust the
    SoftNavigation "umbrella" event, which was based on the Now() time of
    when the new SoftNavigationContext was created. This led to some
    unintended nesting issues, so fixing that now.
    
    (An alternative could be to create the Start trace event not-nested, but
    as a Flow event. However, there seems no advantage for this.)
    
    This patch also adds two new trace events:
    - SoftNavigationContextCreated
    - SoftNavigationEmitted
    
    SoftNavigationContextCreated is a replacement for the old SoftNavigation
    umbrella start time, which is the Now() time when the context is
    created, and which is still useful to know. It should be equivalent to
    the processingStart time of the first interaction event timing for this
    context. This event is not added to the devtools.frontend trace
    category.
    
    SoftNavigationEmitted is a new event that marks the time when the full
    set of soft-nav heuristics are met, and a new navigationId is
    incremented and assigned to future PerformanceEntry. I.e. this is the
    timestamp where the performance timeline gets "sliced" into subparts in
    a soft-nav centric view of the performance timeline. This event is added
    to the devtools.frontend trace category.
    
    Bug: 491157749
    Change-Id: Ia32b9c42afb61674142d1ae2c38877166a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7652997
    Commit-Queue: Michal Mocny <mmocny@chromium.org>
    Reviewed-by: Scott Haseley <shaseley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1597251}

--- third_party/blink/web_tests/http/tests/inspector-protocol/tracing/soft-navigations.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/tracing/soft-navigations.js
@@ -93,6 +93,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // presentation time value to mark the timestamp, explicitly.
   const supportedTraceEventNames = [
     'SoftNavigationStart',
+    'SoftNavigationEmitted',
     'largestContentfulPaint::Candidate',
     'largestContentfulPaint::CandidateForSoftNavigation',
   ];
@@ -147,25 +148,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   const timestamps = new TimestampMapper();
   const ids = new IdMapper();
-  const softNavs = [];
+  const softNavStarts = [];
+  const softNavEmitted = [];
   const lcpCandidates = [];
   const lcpCandidatesForSoftNav = []
   for (const event of filteredEvents) {
     if (event.name === 'SoftNavigationStart') {
-      testRunner.log('-> SoftNavigation event');
-      testRunner.log(
-        '   timeOrigin: ' +
-        timestamps.map(event.args.context.timeOrigin));
+      testRunner.log('-> SoftNavigationStart event');
       testRunner.log('   ts: ' + timestamps.map(event.ts));
-      testRunner.log(
-          '   firstContentfulPaint: ' +
-          timestamps.map(event.args.context.firstContentfulPaint));
       testRunner.log('   frame: ' + ids.map(event.args.frame));
       testRunner.log(
           '   performanceTimelineNavigationId: ' +
           ids.map(event.args.context.performanceTimelineNavigationId));
       testRunner.log('   URL: ' + event.args.context.URL)
-      softNavs.push(event);
+      softNavStarts.push(event);
+    } else if (event.name === 'SoftNavigationEmitted') {
+      testRunner.log('-> SoftNavigationEmitted event');
+      testRunner.log('   ts: ' + timestamps.map(event.ts));
+      testRunner.log(
+          '   performanceTimelineNavigationId: ' +
+          ids.map(event.args.context.performanceTimelineNavigationId));
+      softNavEmitted.push(event);
     } else if (event.name === 'largestContentfulPaint::CandidateForSoftNavigation') {
       testRunner.log('-> LCP candidate for soft navigation event');
       testRunner.log('   ts: ' + timestamps.map(event.ts));
@@ -185,8 +188,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     }
   }
 
-  testRunner.log('\nSoftNavigation event shape:');
-  tracingHelper.logEventShape(softNavs[0]);
+  testRunner.log('\nSoftNavigationStart event shape:');
+  tracingHelper.logEventShape(softNavStarts[0]);
+
+  testRunner.log('\nSoftNavigationEmitted event shape:');
+  tracingHelper.logEventShape(softNavEmitted[0]);
 
   testRunner.log('\nLCP candidate event shape:');
   tracingHelper.logEventShape(lcpCandidates[0]);
