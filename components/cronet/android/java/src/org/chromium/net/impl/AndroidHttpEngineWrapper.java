14323b771ae109cb7453821b0e22e121d628f404
Mohannad Farrag <aymanm@google.com>
2026-03-10 06:54:31 -0700

    Fix typo in setting TrafficStats UID.
    The code was incorrectly calling setTrafficStatsTag with the UID value instead of setTrafficStatsUid.
    
    Change-Id: I9cad4eb3042478fcc1f8815a80b7da74b00e2bf1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7638429
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Auto-Submit: Mohannad Farrag <aymanm@google.com>
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1597031}

--- components/cronet/android/java/src/org/chromium/net/impl/AndroidHttpEngineWrapper.java
+++ components/cronet/android/java/src/org/chromium/net/impl/AndroidHttpEngineWrapper.java
@@ -250,7 +250,7 @@ class AndroidHttpEngineWrapper extends CronetEngineBase {
             requestBuilder.setTrafficStatsTag(trafficStatsTag);
         }
         if (trafficStatsUidSet) {
-            requestBuilder.setTrafficStatsTag(trafficStatsUid);
+            requestBuilder.setTrafficStatsUid(trafficStatsUid);
         }
         requestBuilder.bindToNetwork(getNetwork(networkHandle));
         requestBuilder.setHttpMethod(method);
