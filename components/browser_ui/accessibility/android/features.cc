947b15440f978034eb993e6f28664e3fdbf75d2d
Hitarth Kothari <hitarthkothari@google.com>
2026-03-10 07:49:47 -0700

    [Zoom Indicator] Enable on ToT
    Bug: 491424317
    Change-Id: Ie5257a9cd122cac73b5a18f2fc5f35fbc0a5d45c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7644549
    Commit-Queue: Hitarth Kothari <hitarthkothari@google.com>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Hitarth Kothari <hitarthkothari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1597057}

--- components/browser_ui/accessibility/android/features.cc
+++ components/browser_ui/accessibility/android/features.cc
@@ -9,6 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace browser_ui {
 
-BASE_FEATURE(kAndroidZoomIndicator, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kAndroidZoomIndicator, base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace browser_ui
