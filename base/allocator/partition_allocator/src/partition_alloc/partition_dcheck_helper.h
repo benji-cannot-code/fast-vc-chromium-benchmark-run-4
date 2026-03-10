e40b00180578f005e154ce09cb53c41346c4a57d
Kalvin Lee <kdlee@chromium.org>
2026-03-09 17:32:59 -0700

    PA: Reduce `partition_lock.h` exposure
    This CL moves the inclusion of `partition_lock.h` into the
    implementation of `partition_dcheck_helper` where it belongs (gated
    behind "is DCHECK on" conditions).
    
    Note --- this contributed ~5.3 GiB of bloat when this `#include` was
    exposed to `span.h` (via `slot_start.h`).
    
    Note --- this isn't strictly necessary for Checked Span, since primarily
    we hope to save by further slimming down `bounds_check.h`.
    
    Bug: 488206062
    Change-Id: I946fb46f4afce0f85f3c3d5959e4382168142b7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7642911
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1596733}

--- base/allocator/partition_allocator/src/partition_alloc/partition_dcheck_helper.h
+++ base/allocator/partition_allocator/src/partition_alloc/partition_dcheck_helper.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "partition_alloc/partition_alloc_base/compiler_specific.h"
 #include "partition_alloc/partition_alloc_base/component_export.h"
 #include "partition_alloc/partition_alloc_forward.h"
-#include "partition_alloc/partition_lock.h"
 
 namespace partition_alloc::internal {
 
