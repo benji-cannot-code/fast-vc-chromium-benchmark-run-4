34fd0776ded891e3f31e326d5213987f2fa53c5f
Fahad Mansoor <fahadmansoor@google.com>
2026-03-11 08:08:25 -0700

    LPP: Introduce LocalAuthFactorsPolicyController class
    Add logic to handle local auth factor policy change. Mark the next login
    as online reauth, when the policy does not allow any local auth factors
    and the user has a local auth factor set up.
    
    Bug: 445626656
    
    Test: LocalAuthFactorsPolicyControllerBrowserTest
    Change-Id: I98363f47497c0dba0fed59e4931e8015466e6d49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7595246
    Commit-Queue: Fahad Mansoor <fahadmansoor@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Ivan Šandrk <isandrk@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1597754}

---
+++ chrome/browser/ash/login/auth_factors_policy/README.md
@@ -0,0 +1,3 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+This is the location where local auth factors policy related code resides.
+[Design Doc](http://go/gb-password-pin-policy) (Note: This is an internal Google document.)
--- chrome/browser/ash/login/test/login_manager_mixin.cc
+++ chrome/browser/ash/login/test/login_manager_mixin.cc
@@ -78,6 +78,10 @@ AccountId LoginManagerMixin::CreateConsumerAccountId(int unique_number) {
   return CreateAccountId(unique_number, kGmailDomain);
 }
 
+AccountId LoginManagerMixin::CreateEnterpriseAccountId(int unique_number) {
+  return CreateAccountId(unique_number, kManagedDomain);
+}
+
 // static
 UserContext LoginManagerMixin::CreateDefaultUserContext(
     const TestUserInfo& user_info) {
