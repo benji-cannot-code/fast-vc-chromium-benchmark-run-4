bd0453a35c5549ec862d9fcbcb909953d5a8f86e
Anna Sato <annasato@chromium.org>
2026-03-10 21:41:28 -0700

    Replace WebSocket with WebRTC in BackForwardCache feature tests
    The tests for capturing JavaScript source locations of blocklisted
    features in BackForwardCache were using WebSocket. However, there is an
    ongoing work to allow pages with active WebSockets to enter the BFCache
    by disconnecting them upon entry so that WebSockets can no longer be
    used as a BFCache blocker. This change replaces the WebSocket usage with
    WebRTC, which is also a blocklisted feature, and re-enables the tests on
    Android.
    
    Bug: 467838624
    Change-Id: Ib8d279658698ec1bda9e565fd8cf80f1027820d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7650606
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Commit-Queue: Anna Sato <annasato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1597518}

---
+++ content/test/data/back_forward_cache/webrtc_external_script.js
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+let pcA;
+let pcB;
+
+function openWebRTCConnectionA() {
+  return new Promise(resolve => {
+    pcA = new RTCPeerConnection();
+    pcA.addIceCandidate({ candidate: "test", sdpMLineIndex: 0 }).finally(()=>{
+      resolve();
+    });
+  });
+}
+
+function openWebRTCConnectionB() {
+  return new Promise(resolve => {
+    pcB = new RTCPeerConnection();
+    pcB.addIceCandidate({ candidate: "test", sdpMLineIndex: 0 }).finally(()=>{
+      resolve();
+    });
+  });
+}
+
+function closeConnectionA() {
+    pcA.close();
+}
