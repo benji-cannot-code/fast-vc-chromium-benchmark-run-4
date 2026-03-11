b7f3b47b625564f6f2f67e8a5265be0462027876
Quentin Pubert <qpubert@google.com>
2026-03-11 07:23:34 -0700

    [iOS] Add new ios/chrome Cobalt provider APIs
    This CL adds new provider APIs for Cobalt tab helpers, browser agents
    and profile keyed service factories to be properly created.
    
    OWNERS files are also created in the new provider directories.
    
    Bug: 475806588, 475807907, 475808206
    Change-Id: Ie8b3e46d241eeb2334bb2281ad99f578f90aabb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7656226
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Quentin Pubert <qpubert@google.com>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1597725}

---
+++ ios/public/provider/chrome/browser/cobalt/cobalt_api.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_PUBLIC_PROVIDER_CHROME_BROWSER_COBALT_COBALT_API_H_
+#define IOS_PUBLIC_PROVIDER_CHROME_BROWSER_COBALT_COBALT_API_H_
+
+class Browser;
+class TabHelperAttacher;
+
+namespace ios::provider {
+
+// Attaches the Cobalt tab helpers using the given `attacher`.
+void AttachCobaltTabHelpers(TabHelperAttacher& attacher);
+
+// Attaches the Cobalt browser agents to the given `browser`.
+void AttachCobaltBrowserAgentsForActiveBrowser(Browser* browser);
+
+// Ensures the Cobalt profile keyed service factories are built.
+void EnsureCobaltProfileKeyedServiceFactoriesBuilt();
+
+}  // namespace ios::provider
+
+#endif  // IOS_PUBLIC_PROVIDER_CHROME_BROWSER_COBALT_COBALT_API_H_
