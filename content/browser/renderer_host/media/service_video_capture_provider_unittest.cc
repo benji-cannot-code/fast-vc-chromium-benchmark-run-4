234fdc1954efb02573d3a9ee8a29513e68157a87
Avi Drissman <avi@chromium.org>
2026-03-11 08:23:57 -0700

    Make the video capture service not use the plugin helper
    The plugin helper has been disabled and is being removed, so remove its
    usage from the video capture service. Also remove "safe mode" as that
    was introduced to avoid crashes from poorly-written DAL plugins, but DAL
    plugins are no longer supported given the hardened runtime.
    
    Bug: 461717105
    Bug: 40244340
    Change-Id: I5d66786987d15f77405edd837844d1686a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7648193
    Reviewed-by: Johannes Kron <kron@chromium.org>
    Commit-Queue: Johannes Kron <kron@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1597764}

--- content/browser/renderer_host/media/service_video_capture_provider_unittest.cc
+++ content/browser/renderer_host/media/service_video_capture_provider_unittest.cc
@@ -76,10 +76,6 @@ class ServiceVideoCaptureProviderTest : public testing::Test {
 
  protected:
   void SetUp() override {
-    // Those tests are incompatible with the automatic retry with safe mode on
-    // macOS.
-    scoped_feature_list_.InitAndDisableFeature(
-        features::kRetryGetVideoCaptureDeviceInfos);
 #if BUILDFLAG(IS_CHROMEOS)
     provider_ = std::make_unique<ServiceVideoCaptureProvider>(
         base::BindRepeating([]() {
