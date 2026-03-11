f3017effe0abd52dc3f6a3e9bead57fb6301d6af
Quentin Pubert <qpubert@google.com>
2026-03-11 01:26:16 -0700

    [iOS] Invoke Cobalt provider API in WKWebViewConfigurationProvider
    This CL invokes InitializeCobaltInWKWebViewConfiguration inside
    WKWebViewConfigurationProvider so Cobalt is initialized in the provided
    configuration.
    
    Bug: 475806371
    Change-Id: I38bef5c618b8cf3c0dc826476e7905d5b0d7a920
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7646151
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1597579}

--- ios/web/web_state/ui/wk_web_view_configuration_provider.mm
+++ ios/web/web_state/ui/wk_web_view_configuration_provider.mm
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/task/sequenced_task_runner.h"
 #import "base/uuid.h"
 #import "components/safe_browsing/core/common/features.h"
+#import "ios/public/provider/web/cobalt_api.h"
 #import "ios/web/common/features.h"
 #import "ios/web/js_messaging/java_script_feature_manager.h"
 #import "ios/web/js_messaging/java_script_feature_util_impl.h"
@@ -202,7 +203,8 @@ void WKWebViewConfigurationProvider::ResetWithWebViewConfiguration(
   UpdateScripts();
 
   if (web::features::IsCobaltEnabled()) {
-    // TODO(crbug.com/475806371): Update configuration for Cobalt.
+    web::provider::InitializeCobaltInWKWebViewConfiguration(configuration_,
+                                                            browser_state_);
   }
 
   if (!scheme_handler_) {
