12e4e8e9dc491f5674a0f5f27bac4515dd0040a0
Abhishek Shanthkumar <abhishek.shanthkumar@microsoft.com>
2026-03-11 05:48:55 -0700

    IDB: Treat backing stores with data loss as new stores
    In the kUseLevelDbAsControl and kUseSqliteForNewStores rollout stages,
    existing LevelDB backing stores that report data loss (due to
    corruption) will be erased and recreated as new stores.
    
    Bug: 484257587
    Change-Id: I428fe95d1f51c937eacae984616d36e5679b748a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7652454
    Reviewed-by: Evan Stade <evanstade@microsoft.com>
    Commit-Queue: Abhishek Shanthkumar <abhishek.shanthkumar@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1597680}

--- content/browser/indexed_db/indexed_db_unittest.cc
+++ content/browser/indexed_db/indexed_db_unittest.cc
@@ -3185,15 +3185,15 @@ TEST_P(IndexedDBTestForSqliteMigration, UseLevelDbAsControl) {
        {StoreType::kEmptyLevelDbDirectory,
         {StoreInitResult::kCreated, kIsLevelDb, kExperimentalSuffix}},
        {StoreType::kLevelDbWithCorruptionInfo,
-        {StoreInitResult::kDataLoss, kIsLevelDb}},
+        {StoreInitResult::kDataLoss, kIsLevelDb, kExperimentalSuffix}},
        {StoreType::kLevelDbCurrentMissing,
         {StoreInitResult::kOpened, kIsLevelDb}},
        {StoreType::kLevelDbFilesMissing,
         {StoreInitResult::kFailed, kIsLevelDb}},
        {StoreType::kLevelDbInternalCorruption,
-        {StoreInitResult::kDataLoss, kIsLevelDb}},
+        {StoreInitResult::kDataLoss, kIsLevelDb, kExperimentalSuffix}},
        {StoreType::kLevelDbBackingStoreCorruption,
-        {StoreInitResult::kDataLoss, kIsLevelDb}}});
+        {StoreInitResult::kDataLoss, kIsLevelDb, kExperimentalSuffix}}});
   // The experimental suffix should persist across opens.
   CloseAllBackingStores();
   ValidateExpectationsForStage(
@@ -3206,15 +3206,15 @@ TEST_P(IndexedDBTestForSqliteMigration, UseLevelDbAsControl) {
        {StoreType::kEmptyLevelDbDirectory,
         {StoreInitResult::kOpened, kIsLevelDb, kExperimentalSuffix}},
        {StoreType::kLevelDbWithCorruptionInfo,
-        {StoreInitResult::kOpened, kIsLevelDb}},
+        {StoreInitResult::kOpened, kIsLevelDb, kExperimentalSuffix}},
        {StoreType::kLevelDbCurrentMissing,
         {StoreInitResult::kOpened, kIsLevelDb}},
        {StoreType::kLevelDbFilesMissing,
         {StoreInitResult::kFailed, kIsLevelDb}},
        {StoreType::kLevelDbInternalCorruption,
-        {StoreInitResult::kOpened, kIsLevelDb}},
+        {StoreInitResult::kOpened, kIsLevelDb, kExperimentalSuffix}},
        {StoreType::kLevelDbBackingStoreCorruption,
-        {StoreInitResult::kOpened, kIsLevelDb}}});
+        {StoreInitResult::kOpened, kIsLevelDb, kExperimentalSuffix}}});
 }
 
 TEST_P(IndexedDBTestForSqliteMigration, UseSqliteForNewStores) {
@@ -3228,15 +3228,15 @@ TEST_P(IndexedDBTestForSqliteMigration, UseSqliteForNewStores) {
        {StoreType::kEmptyLevelDbDirectory,
         {StoreInitResult::kCreated, kIsSqlite, kExperimentalSuffix}},
        {StoreType::kLevelDbWithCorruptionInfo,
-        {StoreInitResult::kDataLoss, kIsLevelDb}},
+        {StoreInitResult::kDataLoss, kIsSqlite, kExperimentalSuffix}},
        {StoreType::kLevelDbCurrentMissing,
         {StoreInitResult::kOpened, kIsLevelDb}},
        {StoreType::kLevelDbFilesMissing,
         {StoreInitResult::kFailed, kIsLevelDb}},
        {StoreType::kLevelDbInternalCorruption,
-        {StoreInitResult::kDataLoss, kIsLevelDb}},
+        {StoreInitResult::kDataLoss, kIsSqlite, kExperimentalSuffix}},
        {StoreType::kLevelDbBackingStoreCorruption,
-        {StoreInitResult::kDataLoss, kIsLevelDb}}});
+        {StoreInitResult::kDataLoss, kIsSqlite, kExperimentalSuffix}}});
   // SQLite should persist across opens.
   CloseAllBackingStores();
   ValidateExpectationsForStage(
@@ -3249,15 +3249,15 @@ TEST_P(IndexedDBTestForSqliteMigration, UseSqliteForNewStores) {
        {StoreType::kEmptyLevelDbDirectory,
         {StoreInitResult::kOpened, kIsSqlite, kExperimentalSuffix}},
        {StoreType::kLevelDbWithCorruptionInfo,
-        {StoreInitResult::kOpened, kIsLevelDb}},
+        {StoreInitResult::kOpened, kIsSqlite, kExperimentalSuffix}},
        {StoreType::kLevelDbCurrentMissing,
         {StoreInitResult::kOpened, kIsLevelDb}},
        {StoreType::kLevelDbFilesMissing,
         {StoreInitResult::kFailed, kIsLevelDb}},
        {StoreType::kLevelDbInternalCorruption,
-        {StoreInitResult::kOpened, kIsLevelDb}},
+        {StoreInitResult::kOpened, kIsSqlite, kExperimentalSuffix}},
        {StoreType::kLevelDbBackingStoreCorruption,
-        {StoreInitResult::kOpened, kIsLevelDb}}});
+        {StoreInitResult::kOpened, kIsSqlite, kExperimentalSuffix}}});
 }
 
 TEST_P(IndexedDBTestForSqliteMigration, UseSqliteOnly) {
