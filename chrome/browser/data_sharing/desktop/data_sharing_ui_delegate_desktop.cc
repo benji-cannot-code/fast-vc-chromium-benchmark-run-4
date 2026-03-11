e7f6dbc6f157584279076245ce930584da5d7379
Antonio Gomes <tonikitoo@igalia.com>
2026-03-11 15:40:19 -0700

    Modularize chrome/browser/data_sharing/
    This CL moves the compilation of source code in c/b/data_sharing/ to
    their own GN target, as per [1].
    
    [1] docs/chrome_browser_design_principles.md
    
    R=dljames@chromium.org
    
    Bug: 353332589
    Change-Id: Icf67e432a2b19e0cea27f9145e88ee101d8140c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7653817
    Commit-Queue: Antonio Gomes Netto <tonikitoo@igalia.com>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1598041}

--- chrome/browser/data_sharing/desktop/data_sharing_ui_delegate_desktop.cc
+++ chrome/browser/data_sharing/desktop/data_sharing_ui_delegate_desktop.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_features.h"
 #include "chrome/browser/ui/views/data_sharing/collaboration_controller_delegate_desktop.h"
-#include "chrome/browser/ui/views/data_sharing/data_sharing_bubble_controller.h"
 #include "components/collaboration/public/collaboration_service.h"
 #include "components/data_sharing/public/data_sharing_service.h"
 #include "url/gurl.h"
