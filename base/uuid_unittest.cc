8e8208078e8c34cd3cdffd91b31fd8dfd11d8b1c
Florian Leimgruber <fleimgruber@google.com>
2026-03-11 03:27:54 -0700

    [base] Add Uuid::AsInteger()
    Adds a function that converts the string-based Uuid representation to a
    128-bit integer.
    See http://shortn/_GHW2iCC9Lg for the motivation behind this change.
    
    Bug: 489354073
    Change-Id: I43b03a9d4bdba9077d024dde97533c9ca0bf22de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7633224
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1597623}

--- base/uuid_unittest.cc
+++ base/uuid_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/abseil-cpp/absl/numeric/int128.h"
 
 namespace base {
 
@@ -188,6 +189,20 @@ TEST(UuidTest, Compare) {
   EXPECT_TRUE(guid_invalid <= guid);
   EXPECT_FALSE(guid_invalid > guid);
   EXPECT_FALSE(guid_invalid >= guid);
+}
+
+TEST(UuidTest, AsIntegerValid) {
+  const Uuid uuid =
+      Uuid::ParseCaseInsensitive("01234567-89ab-cdef-fedc-ba9876543210");
+  ASSERT_TRUE(uuid.is_valid());
+  EXPECT_EQ(uuid.AsInteger(),
+            absl::MakeUint128(0x01234567'89ab'cdef, 0xfedc'ba9876543210));
+}
+
+TEST(UuidTest, AsIntegerInvalid) {
+  const Uuid uuid = Uuid::ParseCaseInsensitive("invalid");
+  ASSERT_FALSE(uuid.is_valid());
+  EXPECT_EQ(uuid.AsInteger(), 0);
 }
 
 }  // namespace base
