0f83fd26b5ecb9115970929c3c960dd3aa95f47c
Antonio Sartori <antoniosartori@chromium.org>
2026-03-11 02:34:23 -0700

    [geo] Simplify logic to populate X-Geo header on Android
    This CL refactors some bits of the GeolocationHeader code in order to
    avoid duplicated permission checks (both at the OS level and at the
    DSE level) when requesting geolocation.
    
    Bug: 432628270
    Change-Id: I6165d8d6977cf0e5dae5d8b4ef19c0aa2fe5ded6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7642811
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Alvin Ji <alvinji@chromium.org>
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1597605}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/geo/GeolocationHeaderUnitTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/geo/GeolocationHeaderUnitTest.java
@@ -153,7 +153,9 @@ public class GeolocationHeaderUnitTest {
     public void testGetGeoHeaderOld() {
         checkOldLocation("X-Geo: w " + ENCODED_PROTO_LOCATION_PRECISE);
         GeolocationHeader.setAppPermissionsForTesting(/* hasCoarse= */ true, /* hasFine= */ false);
-        checkOldLocation("X-Geo: w " + ENCODED_PROTO_LOCATION_COARSE);
+        // Even if the app-level permission is coarse, the site has precise location so the X-Geo
+        // header will report a fine location permission.
+        checkOldLocation("X-Geo: w " + ENCODED_PROTO_LOCATION_PRECISE);
     }
 
     @Test
@@ -205,11 +207,13 @@ public class GeolocationHeaderUnitTest {
         assertEquals(
                 OmniboxFeatures.sGeolocationRequestPriority.getValue(),
                 actualRequest.getPriority());
-        assertEquals(Granularity.GRANULARITY_FINE, actualRequest.getGranularity());
+        assertEquals(Granularity.GRANULARITY_PERMISSION_LEVEL, actualRequest.getGranularity());
 
         Location mockLocation = generateMockLocation("network", LOCATION_TIME);
         mLocationListenerCaptor.getValue().onLocationChanged(mockLocation);
-        assertEquals(mockLocation, GeolocationHeader.getLastKnownLocation(/* useFine= */ true));
+        assertEquals(
+                mockLocation,
+                GeolocationHeader.getLastKnownLocation(/* hasFineSitePermission= */ true));
         assertEquals(0, mRefreshLastKnownLocationCount);
 
         GeolocationHeader.stopListeningForLocationUpdates();
@@ -237,11 +241,13 @@ public class GeolocationHeaderUnitTest {
                         eq(null));
 
         LocationRequest actualRequest = mLocationRequestCaptor.getValue();
-        assertEquals(Granularity.GRANULARITY_FINE, actualRequest.getGranularity());
+        assertEquals(Granularity.GRANULARITY_PERMISSION_LEVEL, actualRequest.getGranularity());
 
         Location mockLocation = generateMockLocation("network", LOCATION_TIME);
         mLocationListenerCaptor.getValue().onLocationChanged(mockLocation);
-        assertEquals(mockLocation, GeolocationHeader.getLastKnownLocation(/* useFine= */ true));
+        assertEquals(
+                mockLocation,
+                GeolocationHeader.getLastKnownLocation(/* hasFineSitePermission= */ true));
         assertEquals(0, mRefreshLastKnownLocationCount);
     }
 
@@ -265,7 +271,9 @@ public class GeolocationHeaderUnitTest {
 
         Location mockLocation = generateMockLocation("network", LOCATION_TIME);
         mLocationListenerCaptor.getValue().onLocationChanged(mockLocation);
-        assertEquals(mockLocation, GeolocationHeader.getLastKnownLocation(/* useFine= */ false));
+        assertEquals(
+                mockLocation,
+                GeolocationHeader.getLastKnownLocation(/* hasFineSitePermission= */ false));
         assertEquals(0, mRefreshLastKnownLocationCount);
     }
 
@@ -303,12 +311,14 @@ public class GeolocationHeaderUnitTest {
 
         // The request should be coarse because the app permission is the most restrictive.
         LocationRequest actualRequest = mLocationRequestCaptor.getValue();
-        assertEquals(Granularity.GRANULARITY_COARSE, actualRequest.getGranularity());
+        assertEquals(Granularity.GRANULARITY_PERMISSION_LEVEL, actualRequest.getGranularity());
 
         // Verify that the location is treated as not fine.
         Location mockLocation = generateMockLocation("network", LOCATION_TIME);
         mLocationListenerCaptor.getValue().onLocationChanged(mockLocation);
-        assertEquals(mockLocation, GeolocationHeader.getLastKnownLocation(/* useFine= */ false));
+        assertEquals(
+                mockLocation,
+                GeolocationHeader.getLastKnownLocation(/* hasFineSitePermission= */ false));
         assertEquals(0, mRefreshLastKnownLocationCount);
     }
 
