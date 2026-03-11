766df0814ef9c3591eaf9ee869c0b64bfa1b0e92
Keita Suzuki <suzukikeita@chromium.org>
2026-03-10 19:39:38 -0700

    Introduce EnableErrorCodePropagationForPreconnect feature
    This CL introduces a new feature which will expose a preconnect attempt
    made in ConnectJob to HttpStreamFactory::Job layer.
    
    Bug: 453308537
    Change-Id: I763c8bc888b646569ecf8eced8a8b5eb2e00fb24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7487751
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Keita Suzuki <suzukikeita@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1597486}

--- net/http/http_stream_factory_job.cc
+++ net/http/http_stream_factory_job.cc
@@ -973,8 +973,12 @@ int HttpStreamFactory::Job::DoInitConnectionComplete(int result) {
     if (using_quic_) {
       return result;
     }
-    DCHECK_EQ(OK, result);
-    return OK;
+    // When the feature is enabled, the result of preconnect may not be OK.
+    if (!base::FeatureList::IsEnabled(
+            net::features::kEnableErrorCodePropagationForPreconnect)) {
+      DCHECK_EQ(OK, result);
+    }
+    return result;
   }
 
   resolve_error_info_ = connection_->resolve_error_info();
