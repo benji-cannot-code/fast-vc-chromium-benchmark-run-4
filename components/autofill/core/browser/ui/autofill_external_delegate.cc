c7af9c97395d7f014cff8414c94193036a67c60c
Bruno Braga <brunobraga@google.com>
2026-03-11 08:37:49 -0700

    Add Autofill.Ai.ReauthToFill.ResultPerFieldType metric
    This metric is added to validate whether users fail re-auth on
    specific field types.
    
    Bug: 468236777
    Change-Id: Ie05f6a7a439c5e272a047c9cbb68cadbb6760987
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7656803
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1597775}

--- components/autofill/core/browser/ui/autofill_external_delegate.cc
+++ components/autofill/core/browser/ui/autofill_external_delegate.cc
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/foundations/autofill_driver.h"
 #include "components/autofill/core/browser/foundations/browser_autofill_manager.h"
 #include "components/autofill/core/browser/integrators/autofill_ai/autofill_ai_manager.h"
+#include "components/autofill/core/browser/integrators/autofill_ai/metrics/autofill_ai_metrics.h"
 #include "components/autofill/core/browser/integrators/compose/autofill_compose_delegate.h"
 #include "components/autofill/core/browser/integrators/identity_credential/identity_credential_delegate.h"
 #include "components/autofill/core/browser/integrators/one_time_tokens/otp_suggestion.h"
@@ -1466,11 +1467,13 @@ void AutofillExternalDelegate::FillAutofillAiFormAndHidePopup(
         // BUILDFLAG(IS_IOS)
   base::OnceCallback<std::optional<EntityInstance>(bool)>
       convert_auth_response = base::BindOnce(
-          [](EntityInstance masked_entity, bool auth_succeeded) {
+          [](const FieldTypeSet& ai_field_types, EntityInstance masked_entity,
+             bool auth_succeeded) {
+            LogReauthToFillResultPerFieldType(ai_field_types, auth_succeeded);
             return auth_succeeded ? std::move(masked_entity)
                                   : std::optional<EntityInstance>();
           },
-          *entity);
+          autofill_field->Type().GetAutofillAiTypes(), *entity);
   MaybeAuthenticateBeforeFilling(
       message, "Autofill.Ai.ReauthToFill",
       std::move(convert_auth_response).Then(std::move(fill_and_hide)));
