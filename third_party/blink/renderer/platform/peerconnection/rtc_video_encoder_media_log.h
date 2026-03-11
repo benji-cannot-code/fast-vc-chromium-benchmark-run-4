1e1ef3238400f0a443b482ccee482d722256e837
Eugene Zemtsov <eugene@chromium.org>
2026-03-10 18:18:16 -0700

    Add media log for webrtc uses of video encoder
    Bug: 479892740
    Change-Id: Ie20f3455fadd2b1cd633c4ba0caee6629e9e3478
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7640834
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Reviewed-by: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1597452}

---
+++ third_party/blink/renderer/platform/peerconnection/rtc_video_encoder_media_log.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_PEERCONNECTION_RTC_VIDEO_ENCODER_MEDIA_LOG_H_
+#define THIRD_PARTY_BLINK_RENDERER_PLATFORM_PEERCONNECTION_RTC_VIDEO_ENCODER_MEDIA_LOG_H_
+
+#include "media/base/media_log.h"
+#include "third_party/blink/renderer/platform/platform_export.h"
+
+namespace blink {
+
+class PLATFORM_EXPORT RTCVideoEncoderMediaLog : public media::MediaLog {
+ public:
+  RTCVideoEncoderMediaLog();
+  RTCVideoEncoderMediaLog(const RTCVideoEncoderMediaLog&) = delete;
+  RTCVideoEncoderMediaLog& operator=(const RTCVideoEncoderMediaLog&) = delete;
+  ~RTCVideoEncoderMediaLog() override;
+
+  void AddLogRecordLocked(
+      std::unique_ptr<media::MediaLogRecord> event) override;
+};
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_PLATFORM_PEERCONNECTION_RTC_VIDEO_ENCODER_MEDIA_LOG_H_
