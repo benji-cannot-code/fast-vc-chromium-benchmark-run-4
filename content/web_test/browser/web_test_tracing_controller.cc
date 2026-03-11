2a0a88a253c2e1cef3ab4eae8119c5c84e60342e
Andrew Paseltiner <apaseltiner@chromium.org>
2026-03-11 05:24:26 -0700

    Make non-spanified base::File::WriteAtCurrentPos overload private
    Bug: 435317390
    Change-Id: Iab79430a615d36c1a528a3cd4f96bca1cd38bec3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7653098
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Petr Cermak <petrcermak@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1597668}

--- content/web_test/browser/web_test_tracing_controller.cc
+++ content/web_test/browser/web_test_tracing_controller.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/web_test/browser/web_test_tracing_controller.h"
 
 #include "base/compiler_specific.h"
+#include "base/containers/span.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/threading/thread_restrictions.h"
 #include "base/trace_event/trace_config.h"
@@ -111,7 +112,9 @@ void WebTestTracingController::OnTracingStopped() {
       [this](perfetto::TracingSession::ReadTraceCallbackArgs args) {
         CHECK(tracing_file_.IsValid());
         if (args.size > 0) {
-          UNSAFE_TODO(tracing_file_.WriteAtCurrentPos(args.data, args.size));
+          // SAFETY: Data and size are valid per perfetto's preconditions.
+          UNSAFE_BUFFERS(base::span<const char> data(args.data, args.size));
+          tracing_file_.WriteAtCurrentPos(base::as_bytes(data));
         }
         if (!args.has_more) {
           TracingFinished();
