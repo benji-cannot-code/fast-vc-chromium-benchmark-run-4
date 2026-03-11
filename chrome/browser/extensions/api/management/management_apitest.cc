9046d62a0e33e28318f917fb64b006dc34783f3c
Devlin Cronin <rdevlin.cronin@chromium.org>
2026-03-11 16:23:48 -0700

    [Extensions] Update management API tests to MV3
    MV2 has been unsupported for several months. We can remove testing
    support for it. Existing tests should be updated to MV3.
    
    Update API tests for the management API to use manifest version 3
    instead of manifest version 2 and 3.
    
    With the transition to MV3, many of these tests may also be able to be
    updated to use new patterns, such as async / await, service worker
    modules, and more. However, those changes are out of scope for these
    CLs, and can be addressed in followups.
    
    Bug: 491516661
    Change-Id: I0529252670a8a15bb3ec8340b97bafe9d4e58b4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7657542
    Reviewed-by: Eva Su <evasu@chromium.org>
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1598061}

--- chrome/browser/extensions/api/management/management_apitest.cc
+++ chrome/browser/extensions/api/management/management_apitest.cc
@@ -73,10 +73,9 @@ constexpr char kManifest[] =
     R"({
           "name": "Management API Test",
           "version": "0.1",
-          "manifest_version": 2,
+          "manifest_version": 3,
           "background": {
-            "scripts": ["background.js"],
-            "persistent": true
+            "service_worker": "background.js"
           },
           "replacement_web_app": "%s"
         })";
@@ -92,13 +91,9 @@ bool ExpectChromeAppsDefaultEnabled() {
 
 }  // namespace
 
-using ContextType = extensions::browser_test_util::ContextType;
-
-class ExtensionManagementApiTest
-    : public extensions::ExtensionApiTest,
-      public testing::WithParamInterface<ContextType> {
+class ExtensionManagementApiTest : public extensions::ExtensionApiTest {
  public:
-  ExtensionManagementApiTest() : ExtensionApiTest(GetParam()) {
+  ExtensionManagementApiTest() {
 #if !BUILDFLAG(IS_ANDROID)
     enable_chrome_apps_ = std::make_unique<base::AutoReset<bool>>(
         &extensions::testing::g_enable_chrome_apps_for_testing, true);
@@ -177,18 +172,7 @@ class ExtensionManagementApiTest
 #endif
 };
 
-#if !BUILDFLAG(IS_ANDROID)
-// Android does not support persistent background pages.
-INSTANTIATE_TEST_SUITE_P(PersistentBackground,
-                         ExtensionManagementApiTest,
-                         ::testing::Values(ContextType::kPersistentBackground));
-#endif  // !BUILDFLAG(IS_ANDROID)
-
-INSTANTIATE_TEST_SUITE_P(ServiceWorker,
-                         ExtensionManagementApiTest,
-                         ::testing::Values(ContextType::kServiceWorker));
-
-IN_PROC_BROWSER_TEST_P(ExtensionManagementApiTest, Basics) {
+IN_PROC_BROWSER_TEST_F(ExtensionManagementApiTest, Basics) {
 #if !BUILDFLAG(IS_ANDROID)
   // Android does not provide the XmlUnitTestResultPrinter this method needs.
   base::AddFeatureIdTagToTestResult(
@@ -215,12 +199,12 @@ IN_PROC_BROWSER_TEST_P(ExtensionManagementApiTest, Basics) {
 #else
 #define MAYBE_NoPermission NoPermission
 #endif
-IN_PROC_BROWSER_TEST_P(ExtensionManagementApiTest, MAYBE_NoPermission) {
+IN_PROC_BROWSER_TEST_F(ExtensionManagementApiTest, MAYBE_NoPermission) {
   LoadExtensions();
   ASSERT_TRUE(RunExtensionTest("management/no_permission"));
 }
 
-IN_PROC_BROWSER_TEST_P(ExtensionManagementApiTest, Uninstall) {
+IN_PROC_BROWSER_TEST_F(ExtensionManagementApiTest, Uninstall) {
   LoadExtensions();
   // Confirmation dialog will be shown for uninstallations except for self.
   extensions::ScopedTestDialogAutoConfirm auto_confirm(
@@ -230,7 +214,7 @@ IN_PROC_BROWSER_TEST_P(ExtensionManagementApiTest, Uninstall) {
 
 #if !BUILDFLAG(IS_ANDROID)
 // Skipped on Android because it does not support Chrome apps.
-IN_PROC_BROWSER_TEST_P(ExtensionManagementApiTest, CreateAppShortcut) {
+IN_PROC_BROWSER_TEST_F(ExtensionManagementApiTest, CreateAppShortcut) {
   LoadExtensions();
   base::FilePath basedir = test_data_dir_.AppendASCII("management");
   LoadNamedExtension(basedir, "packaged_app");
@@ -240,7 +224,7 @@ IN_PROC_BROWSER_TEST_P(ExtensionManagementApiTest, CreateAppShortcut) {
 }
 
 // Skipped on Android because it does not support Chrome apps.
-IN_PROC_BROWSER_TEST_P(ExtensionManagementApiTest, GenerateAppForLink) {
+IN_PROC_BROWSER_TEST_F(ExtensionManagementApiTest, GenerateAppForLink) {
   web_app::test::WaitUntilReady(web_app::WebAppProvider::GetForTest(profile()));
   ASSERT_TRUE(RunExtensionTest("management/generate_app_for_link"));
 }
@@ -332,14 +316,7 @@ class InstallReplacementWebAppApiTest : public ExtensionManagementApiTest {
   net::EmbeddedTestServer https_test_server_;
 };
 
-INSTANTIATE_TEST_SUITE_P(PersistentBackground,
-                         InstallReplacementWebAppApiTest,
-                         ::testing::Values(ContextType::kPersistentBackground));
-INSTANTIATE_TEST_SUITE_P(ServiceWorker,
-                         InstallReplacementWebAppApiTest,
-                         ::testing::Values(ContextType::kServiceWorker));
-
-IN_PROC_BROWSER_TEST_P(InstallReplacementWebAppApiTest, NotWebstore) {
+IN_PROC_BROWSER_TEST_F(InstallReplacementWebAppApiTest, NotWebstore) {
   static constexpr char kBackground[] = R"(
   chrome.management.installReplacementWebApp(function() {
     chrome.test.assertLastError(
@@ -353,7 +330,7 @@ IN_PROC_BROWSER_TEST_P(InstallReplacementWebAppApiTest, NotWebstore) {
       kBackground, false /* from_webstore */);
 }
 
-IN_PROC_BROWSER_TEST_P(InstallReplacementWebAppApiTest, NoGesture) {
+IN_PROC_BROWSER_TEST_F(InstallReplacementWebAppApiTest, NoGesture) {
   static constexpr char kBackground[] = R"(
   chrome.management.installReplacementWebApp(function() {
     chrome.test.assertLastError(
@@ -367,7 +344,7 @@ IN_PROC_BROWSER_TEST_P(InstallReplacementWebAppApiTest, NoGesture) {
       kBackground, true /* from_webstore */);
 }
 
-IN_PROC_BROWSER_TEST_P(InstallReplacementWebAppApiTest, NotInstallableWebApp) {
+IN_PROC_BROWSER_TEST_F(InstallReplacementWebAppApiTest, NotInstallableWebApp) {
   static constexpr char kBackground[] =
       R"(chrome.test.runWithUserGesture(function() {
            chrome.management.installReplacementWebApp(function() {
@@ -382,7 +359,7 @@ IN_PROC_BROWSER_TEST_P(InstallReplacementWebAppApiTest, NotInstallableWebApp) {
           kBackground, true /* from_webstore */);
 }
 
-IN_PROC_BROWSER_TEST_P(InstallReplacementWebAppApiTest, InstallableWebApp) {
+IN_PROC_BROWSER_TEST_F(InstallReplacementWebAppApiTest, InstallableWebApp) {
   static constexpr char kGoodWebAppURL[] =
       "/management/install_replacement_web_app/acceptable_web_app/index.html";
 
@@ -391,7 +368,7 @@ IN_PROC_BROWSER_TEST_P(InstallReplacementWebAppApiTest, InstallableWebApp) {
 
 // Check that web app still installs and launches correctly when start_url does
 // not match replacement_web_app_url.
-IN_PROC_BROWSER_TEST_P(InstallReplacementWebAppApiTest,
+IN_PROC_BROWSER_TEST_F(InstallReplacementWebAppApiTest,
                        InstallableWebAppWithStartUrl) {
   static constexpr char kGoodWebAppUrl[] =
       "/management/install_replacement_web_app/"
@@ -405,7 +382,7 @@ IN_PROC_BROWSER_TEST_P(InstallReplacementWebAppApiTest,
   RunInstallableWebAppTest(kManifest, kGoodWebAppUrl, kGoodWebAppStartUrl);
 }
 
-IN_PROC_BROWSER_TEST_P(InstallReplacementWebAppApiTest,
+IN_PROC_BROWSER_TEST_F(InstallReplacementWebAppApiTest,
                        InstallableWebAppInPlatformApp) {
   static constexpr char kAppManifest[] =
       R"({
@@ -425,7 +402,7 @@ IN_PROC_BROWSER_TEST_P(InstallReplacementWebAppApiTest,
 #endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
 // Tests actions on extensions when no management policy is in place.
-IN_PROC_BROWSER_TEST_P(ExtensionManagementApiTest, ManagementPolicyAllowed) {
+IN_PROC_BROWSER_TEST_F(ExtensionManagementApiTest, ManagementPolicyAllowed) {
   LoadExtensions();
   extensions::ScopedTestDialogAutoConfirm auto_confirm(
       extensions::ScopedTestDialogAutoConfirm::ACCEPT);
@@ -448,7 +425,7 @@ IN_PROC_BROWSER_TEST_P(ExtensionManagementApiTest, ManagementPolicyAllowed) {
 }
 
 // Tests actions on extensions when management policy prohibits those actions.
-IN_PROC_BROWSER_TEST_P(ExtensionManagementApiTest, ManagementPolicyProhibited) {
+IN_PROC_BROWSER_TEST_F(ExtensionManagementApiTest, ManagementPolicyProhibited) {
   LoadExtensions();
   extensions::ExtensionRegistry* registry =
       extensions::ExtensionRegistry::Get(profile());
@@ -470,7 +447,7 @@ IN_PROC_BROWSER_TEST_P(ExtensionManagementApiTest, ManagementPolicyProhibited) {
 
 #if !BUILDFLAG(IS_ANDROID)
 // Skipped on Android because it does not support Chrome apps.
-IN_PROC_BROWSER_TEST_P(ExtensionManagementApiTest, LaunchPanelApp) {
+IN_PROC_BROWSER_TEST_F(ExtensionManagementApiTest, LaunchPanelApp) {
   // Load an extension that calls launchApp() on any app that gets
   // installed.
   ExtensionTestMessageListener launcher_loaded("launcher loaded");
@@ -526,7 +503,7 @@ IN_PROC_BROWSER_TEST_P(ExtensionManagementApiTest, LaunchPanelApp) {
 }
 
 // Skipped on Android because it does not support Chrome apps.
-IN_PROC_BROWSER_TEST_P(ExtensionManagementApiTest, LaunchTabApp) {
+IN_PROC_BROWSER_TEST_F(ExtensionManagementApiTest, LaunchTabApp) {
   // Load an extension that calls launchApp() on any app that gets
   // installed.
   ExtensionTestMessageListener launcher_loaded("launcher loaded");
@@ -580,7 +557,7 @@ IN_PROC_BROWSER_TEST_P(ExtensionManagementApiTest, LaunchTabApp) {
 }
 
 // Skipped on Android because it does not support Chrome apps.
-IN_PROC_BROWSER_TEST_P(ExtensionManagementApiTest,
+IN_PROC_BROWSER_TEST_F(ExtensionManagementApiTest,
                        NoLaunchPanelAppsDeprecated) {
   extensions::testing::g_enable_chrome_apps_for_testing = false;
   // Load an extension that calls launchApp() on any app that gets
@@ -610,7 +587,7 @@ IN_PROC_BROWSER_TEST_P(ExtensionManagementApiTest,
 }
 
 // Skipped on Android because it does not support Chrome apps.
-IN_PROC_BROWSER_TEST_P(ExtensionManagementApiTest, NoLaunchTabAppDeprecated) {
+IN_PROC_BROWSER_TEST_F(ExtensionManagementApiTest, NoLaunchTabAppDeprecated) {
   extensions::testing::g_enable_chrome_apps_for_testing = false;
   // Load an extension that calls launchApp() on any app that gets
   // installed.
@@ -650,7 +627,7 @@ IN_PROC_BROWSER_TEST_P(ExtensionManagementApiTest, NoLaunchTabAppDeprecated) {
 #else
 #define MAYBE_LaunchType LaunchType
 #endif
-IN_PROC_BROWSER_TEST_P(ExtensionManagementApiTest, MAYBE_LaunchType) {
+IN_PROC_BROWSER_TEST_F(ExtensionManagementApiTest, MAYBE_LaunchType) {
   LoadExtensions();
   base::FilePath basedir = test_data_dir_.AppendASCII("management");
   LoadNamedExtension(basedir, "packaged_app");
