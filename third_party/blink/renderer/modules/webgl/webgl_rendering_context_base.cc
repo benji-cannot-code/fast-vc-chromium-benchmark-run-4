371641bd558be5f1ac388f94be97c519ea16fcae
Colin Blundell <blundell@chromium.org>
2026-03-11 00:53:55 -0700

    [Blink] Move check of MaySupportWebGLImage() into DrawingBuffer
    Move this to its actual usage site in order to (a) clarify the code and
    (b) pave the way for decomposing the notion of "WebGLImageChromium" into
    "use scanout for WebGL" and "low-latency supported for WebGL". A
    followup CL will port ChromiumImageUsage to a simple
    "is_offscreen_canvas" bool that we will then eliminate usage of under a
    killswitch.
    
    Note: The site changed here is the only usage site [1].
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/gpu/drawing_buffer.h;l=706;bpv=1;bpt=1?q=chromium_image_usage_&ss=chromium
    
    Bug: 488937356
    Change-Id: I6a19a3b3b0b5bbfcdea9df9e7522520c463e6085
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7652014
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1597561}

--- third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
+++ third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
@@ -1315,7 +1315,6 @@ scoped_refptr<DrawingBuffer> WebGLRenderingContextBase::CreateDrawingBuffer(
   // non-WebGLImageChromium for OffscreenCanvas.
   // See detailed discussion in crbug.com/649668.
   DrawingBuffer::ChromiumImageUsage chromium_image_usage =
-      SharedGpuContext::MaySupportWebGLImageChromium() &&
               !Host()->IsOffscreenCanvas()
           ? DrawingBuffer::kAllowChromiumImage
           : DrawingBuffer::kDisallowChromiumImage;
