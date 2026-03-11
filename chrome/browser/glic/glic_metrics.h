ff13c0ee0284da376092091317584c7389881cc6
Benedict Wong <benedictwong@google.com>
2026-03-11 16:52:12 -0700

    Add kAnchoredContextualCue InvocationSource
    Add InvocationSource::kAnchoredContextualCue (22) for tracking
    when users click the anchored contextual cue chip.
    
    Add corresponding ResponseSegmentation entries (89-92) for
    attached/detached text/audio combinations.
    
    Change-Id: I672f3c6433d5f1b67193d1b11e48cf87daccceb1
    Bypass-Glic-Api-Compatibility-Check: Adding new field
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7655210
    Commit-Queue: Benedict Wong <benedictwong@google.com>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1598081}

--- chrome/browser/glic/glic_metrics.h
+++ chrome/browser/glic/glic_metrics.h
@@ -222,7 +222,11 @@ enum class ResponseSegmentation {
   kIphAttachedAudio = 86,
   kIphDetachedText = 87,
   kIphDetachedAudio = 88,
-  kMaxValue = kIphDetachedAudio,
+  kAnchoredContextualCueAttachedText = 89,
+  kAnchoredContextualCueAttachedAudio = 90,
+  kAnchoredContextualCueDetachedText = 91,
+  kAnchoredContextualCueDetachedAudio = 92,
+  kMaxValue = kAnchoredContextualCueDetachedAudio,
 };
 // LINT.ThenChange(//tools/metrics/histograms/metadata/glic/enums.xml:GlicResponseSegmentation)
 
