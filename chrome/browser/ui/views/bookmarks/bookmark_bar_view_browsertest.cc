e45d0f44ed6668fec91b9558e094f5d7b418406e
Yuki Shiino <yukishiino@chromium.org>
2026-03-10 22:40:05 -0700

    gardening: Disable BookmarkBarTest.*Highlight tests on Win
    Bug: 491651711
    Change-Id: Ia360be8d0517a6b19fa0ca4d098ae218c0dc7c2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7651358
    Auto-Submit: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Owners-Override: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1597529}

--- chrome/browser/ui/views/bookmarks/bookmark_bar_view_browsertest.cc
+++ chrome/browser/ui/views/bookmarks/bookmark_bar_view_browsertest.cc
@@ -1014,7 +1014,13 @@ class BookmarkBarTest : public BookmarkBarTestBase {
 
 }  // namespace
 
-IN_PROC_BROWSER_TEST_F(BookmarkBarTest, AllBookmarksButtonHighlight) {
+#if BUILDFLAG(IS_WIN)
+//  TODO(crbug.com/491651711): This test is flaky.
+#define MAYBE_AllBookmarksButtonHighlight DISABLED_AllBookmarksButtonHighlight
+#else
+#define MAYBE_AllBookmarksButtonHighlight AllBookmarksButtonHighlight
+#endif
+IN_PROC_BROWSER_TEST_F(BookmarkBarTest, MAYBE_AllBookmarksButtonHighlight) {
   TestContextMenuHighlight(bookmark_bar()->all_bookmarks_button());
 }
 
@@ -1030,7 +1036,13 @@ IN_PROC_BROWSER_TEST_F(BookmarkBarTest, BookmarkFolderButtonHighlight) {
   TestContextMenuHighlight(GetBookmarkButton(0));
 }
 
-IN_PROC_BROWSER_TEST_F(BookmarkBarTest, AppsPageShortcutHighlight) {
+#if BUILDFLAG(IS_WIN)
+//  TODO(crbug.com/491651711): This test is flaky.
+#define MAYBE_AppsPageShortcutHighlight DISABLED_AppsPageShortcutHighlight
+#else
+#define MAYBE_AppsPageShortcutHighlight AppsPageShortcutHighlight
+#endif
+IN_PROC_BROWSER_TEST_F(BookmarkBarTest, MAYBE_AppsPageShortcutHighlight) {
   TestContextMenuHighlight(GetAppsPageShortCut());
 }
 
