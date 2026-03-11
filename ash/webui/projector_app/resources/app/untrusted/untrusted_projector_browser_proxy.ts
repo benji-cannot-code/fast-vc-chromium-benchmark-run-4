2c392b1b93ac00f8c1e6efe005dbec49b8336253
Ahmed Nasr <anasr@google.com>
2026-03-11 14:31:06 -0700

    Add api definition file for projector app and delegate interfaces.
    This will be used for migrating the comm factory and the mock files
    to ts.
    
    Bug: b:487609693
    Change-Id: Ic5461478778a9903758ec7566addedd42fee95df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7654906
    Reviewed-by: Benjamin Zielinski <bzielinski@google.com>
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1597996}

--- ash/webui/projector_app/resources/app/untrusted/untrusted_projector_browser_proxy.ts
+++ ash/webui/projector_app/resources/app/untrusted/untrusted_projector_browser_proxy.ts
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import type {XhrResponseResult} from './app_api.js';
 import {UntrustedProjectorPageCallbackRouter, UntrustedProjectorPageHandlerFactory, UntrustedProjectorPageHandlerRemote} from './ash/webui/projector_app/mojom/untrusted_projector.mojom-webui.js';
 import {Account, JsNetErrorCode, NewScreencastPrecondition, PendingScreencast, PrefsThatProjectorCanAskFor, RequestType, VideoInfo, XhrResponseCode} from './ash/webui/projector_app/public/mojom/projector_types.mojom-webui.js';
 
@@ -69,13 +70,6 @@ const errorCodeMap = new Map<XhrResponseCode, string>([
     'INVALID_ACCOUNT_EMAIL',
   ],
 ]);
-
-export interface XhrResponseResult {
-  success: boolean;
-  response?: string;
-  error?: string;
-  errorCode: JsNetErrorCode;
-}
 
 export class UntrustedProjectorBrowserProxyImpl {
   private pageHandlerRemote: UntrustedProjectorPageHandlerRemote;
