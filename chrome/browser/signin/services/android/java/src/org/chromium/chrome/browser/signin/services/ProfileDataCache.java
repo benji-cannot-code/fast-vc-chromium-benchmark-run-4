55fa05f0d311dfbd2b3c854bcc9ce0e3ca8098d2
Łukasz Barczyński <lbarczynski@google.com>
2026-03-11 11:46:15 -0700

    [Signin][Android] Move AccountManagerFacade observer to dedicated class
    This change moves AccountsChangeObserver to a dedicated inner class. It
    is done to make future IdentityManager migration work easier. In a next
    CLs, a similar observer will be introduced for IdentityManager accounts
    with refresh token changes. Then, based on the feature flag, one of
    these logics will be selected.
    
    Bug: 485773785
    Change-Id: Ifdd8e49f1af3152aa2e9461c227d2c22171a6833
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7633682
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Łukasz Barczyński <lbarczynski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1597900}

--- chrome/browser/signin/services/android/java/src/org/chromium/chrome/browser/signin/services/ProfileDataCache.java
+++ chrome/browser/signin/services/android/java/src/org/chromium/chrome/browser/signin/services/ProfileDataCache.java
@@ -54,7 +54,7 @@ import java.util.function.Function;
  */
 @MainThread
 @NullMarked
-public class ProfileDataCache implements IdentityManager.Observer, AccountsChangeObserver {
+public class ProfileDataCache implements IdentityManager.Observer {
     /** Observer to get notifications about changes in profile data. */
     public interface Observer {
 
@@ -76,6 +76,7 @@ public class ProfileDataCache implements IdentityManager.Observer, AccountsChang
 
     private final Context mContext;
     private final AccountManagerFacade mAccountManagerFacade;
+    private final AccountManagerAccountsChangeObserver mAccountManagerAccountsChangeObserver;
     private final IdentityManager mIdentityManager;
     private final int mImageSize;
     // The badge for a given account is selected as follows:
@@ -98,6 +99,7 @@ public class ProfileDataCache implements IdentityManager.Observer, AccountsChang
         assert identityManager != null;
         mContext = context;
         mAccountManagerFacade = accountManagerFacade;
+        mAccountManagerAccountsChangeObserver = new AccountManagerAccountsChangeObserver();
         mIdentityManager = identityManager;
         mImageSize = imageSize;
         mDefaultBadgeConfig = badgeConfig;
@@ -200,9 +202,9 @@ public class ProfileDataCache implements IdentityManager.Observer, AccountsChang
 
         // TODO(https://crbug.com/483627535): Remove that fallback to AccountManagerFacade after
         // full migration to IdentityManager.
-        var accounts = mAccountManagerFacade.getAccounts();
-        if (accounts.isFulfilled()) {
-            for (var account : accounts.getResult()) {
+        var accounts = getCoreAccountsIfLoaded();
+        if (accounts != null) {
+            for (var account : accounts) {
                 if (account.getId().equals(accountId)) {
                     return createDefaultProfileData(account.getEmail());
                 }
@@ -268,7 +270,7 @@ public class ProfileDataCache implements IdentityManager.Observer, AccountsChang
     public void addObserver(Observer observer) {
         ThreadUtils.assertOnUiThread();
         if (mObservers.isEmpty()) {
-            mAccountManagerFacade.addObserver(this);
+            mAccountManagerFacade.addObserver(mAccountManagerAccountsChangeObserver);
             mIdentityManager.addObserver(this);
         }
         mObservers.addObserver(observer);
@@ -281,14 +283,9 @@ public class ProfileDataCache implements IdentityManager.Observer, AccountsChang
         ThreadUtils.assertOnUiThread();
         mObservers.removeObserver(observer);
         if (mObservers.isEmpty()) {
-            mAccountManagerFacade.removeObserver(this);
             mIdentityManager.removeObserver(this);
+            mAccountManagerFacade.removeObserver(mAccountManagerAccountsChangeObserver);
         }
-    }
-
-    @Override
-    public void onCoreAccountInfosChanged() {
-        updateCache();
     }
 
     /** Implements {@link IdentityManager.Observer}. */
@@ -313,15 +310,18 @@ public class ProfileDataCache implements IdentityManager.Observer, AccountsChang
     }
 
     private void updateCache() {
-        var accounts = mAccountManagerFacade.getAccounts();
-        if (!accounts.isFulfilled()) {
-            // When the list of accounts is ready - onCoreAccountInfosChanged will call
-            // updateCache again.
+        var accounts = getCoreAccountsIfLoaded();
+        if (accounts == null) {
+            // Accounts are not loaded yet, cache will be updated by the observer once they are
+            // available.
             return;
         }
+        updateCache(accounts);
+    }
 
+    private void updateCache(List<AccountInfo> accounts) {
         List<AccountsCache.AccountEntry> displayableAccounts = new ArrayList<>();
-        for (CoreAccountInfo account : accounts.getResult()) {
+        for (CoreAccountInfo account : accounts) {
             var accountInfo = mIdentityManager.findExtendedAccountInfoByAccountId(account.getId());
             if (accountInfo != null
                     && (accountInfo.hasDisplayableInfo()
@@ -379,6 +379,14 @@ public class ProfileDataCache implements IdentityManager.Observer, AccountsChang
         }
     }
 
+    private @Nullable List<AccountInfo> getCoreAccountsIfLoaded() {
+        var accounts = mAccountManagerFacade.getAccounts();
+        if (accounts.isFulfilled()) {
+            return accounts.getResult();
+        }
+        return null;
+    }
+
     // TODO(crbug.com/40944114): Consider using UiUtils.drawIconWithBadge instead.
     private Drawable overlayBadgeOnUserPicture(BadgeConfig badgeConfig, Drawable userPicture) {
         int badgeSize = badgeConfig.getBadgeSize();
@@ -431,6 +439,15 @@ public class ProfileDataCache implements IdentityManager.Observer, AccountsChang
         return mPerAccountBadgeConfig.get(accountId) != null
                 ? mPerAccountBadgeConfig.get(accountId)
                 : mDefaultBadgeConfig;
+    }
+
+    private class AccountManagerAccountsChangeObserver implements AccountsChangeObserver {
+
+        /** Implements {@link AccountsChangeObserver}. */
+        @Override
+        public void onCoreAccountInfosChanged() {
+            updateCache(mAccountManagerFacade.getAccounts().getResult());
+        }
     }
 
     private static final class AccountsCache {
