b62ce46abbfde909e106b040d0c8be7c5d7ceac9
christianxu <christianxu@chromium.org>
2026-03-11 07:21:39 -0700

    [iOS][AIM] Add flag for aim eligibility fetching on navigation
    Bug: None
    Change-Id: I9c5eb5284cddb6e2bddc7c5b31c33738e3ca878d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7653859
    Reviewed-by: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1597724}

--- ios/chrome/browser/flags/ios_chrome_flag_descriptions.cc
+++ ios/chrome/browser/flags/ios_chrome_flag_descriptions.cc
@@ -37,6 +37,10 @@ const char kAIMNTPEntrypointTabletDescription[] =
 const char kAimCobrowseName[] = "AimCobrowse";
 const char kAimCobrowseDescription[] = "Enables the AimCobrowse feature.";
 
+const char kAimUrlNavigationFetchEnabledName[] = "AimUrlNavigationFetchEnabled";
+const char kAimUrlNavigationFetchEnabledDescription[] =
+    "Enables the AimUrlNavigationFetchEnabled feature.";
+
 const char kAnimatedDefaultBrowserPromoInFREName[] =
     "Enable the animated Default Browser Promo in the FRE";
 const char kAnimatedDefaultBrowserPromoInFREDescription[] =
