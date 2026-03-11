f02a5e672d312f7db38817c49a6081478d7134ed
Chuong Ho <hdchuong@google.com>
2026-03-10 17:12:34 -0700

    spellcheck: also remove suggestion markers of Misspelling / Grammar types
    Because Misspelling markers exist in two forms: DocumentMarker::Marker
    type::kSpelling and SuggestionMarker::SuggestionType::kMisspelling, when
    we send a spell check request, we need to clear the suggestion type as
    well upon receiving the result.
    
    See go/clank-gboard-spellcheck-flakiness for context.
    
    Bug: 474442687
    Change-Id: I57201ad4142083fcda9f6d3a043dfbb2fff7d4c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7533302
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Chuong Ho-Dac <hdchuong@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1597422}

--- third_party/blink/renderer/core/editing/spellcheck/spell_check_requester_helper.cc
+++ third_party/blink/renderer/core/editing/spellcheck/spell_check_requester_helper.cc
@@ -22,13 +22,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-bool ShouldSendSpellingMarkersInfo() {
+bool ShouldUseAndroidSpellcheckFullApiBlink() {
 #if BUILDFLAG(IS_ANDROID)
   return base::FeatureList::IsEnabled(
       blink::features::kAndroidSpellcheckFullApiBlink);
 #else
   return false;
 #endif  // BUILDFLAG(IS_ANDROID)
+}
+
+bool ShouldSendSpellingMarkersInfo() {
+  return ShouldUseAndroidSpellcheckFullApiBlink();
+}
+
+bool ShouldRemoveSuggestionMarkerOfMisspellingAndGrammarType() {
+  return ShouldUseAndroidSpellcheckFullApiBlink();
 }
 
 DocumentMarkerVector GetSpellingMarkersFromRange(const Document& document,
--- third_party/blink/renderer/core/editing/spellcheck/spell_checker.h
+++ third_party/blink/renderer/core/editing/spellcheck/spell_checker.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/editing/forward.h"
 #include "third_party/blink/renderer/core/editing/markers/document_marker.h"
+#include "third_party/blink/renderer/core/editing/markers/suggestion_marker.h"
 #include "third_party/blink/renderer/core/editing/spellcheck/text_checking.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 
@@ -103,6 +104,8 @@ class CORE_EXPORT SpellChecker final : public GarbageCollected<SpellChecker> {
   std::pair<String, int> FindFirstMisspelling(const Position&, const Position&);
 
   void RemoveMarkers(const EphemeralRange&, DocumentMarker::MarkerTypes);
+  void RemoveSuggestionMarkersByType(const EphemeralRange&,
+                                     SuggestionMarker::SuggestionType);
 
   Member<LocalDOMWindow> window_;
 
