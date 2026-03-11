5ba7dc040505be1e4c3221abd23443f016a4597e
Lei Zhang <thestig@chromium.org>
2026-03-11 13:34:01 -0700

    Replace content::AttributionStorageSql::Error with std::monostate
    Remove an empty struct and make it more obvious that there is only one
    possible unexpected condition.
    
    Change-Id: I9859bc84fd9cc1d0a98b86311fcee605335d01a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7658225
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1597962}

--- content/browser/attribution_reporting/attribution_storage_sql.h
+++ content/browser/attribution_reporting/attribution_storage_sql.h
@@ -111,8 +111,6 @@ class CONTENT_EXPORT AttributionStorageSql {
     sql::Transaction transaction_;
   };
 
-  struct Error {};
-
   // If `user_data_directory` is empty, the DB is created in memory and no data
   // is persisted to disk.
   AttributionStorageSql(const base::FilePath& user_data_directory,
@@ -406,7 +404,7 @@ class CONTENT_EXPORT AttributionStorageSql {
     int64_t priority;
   };
 
-  base::expected<std::optional<ReportIdAndPriority>, Error>
+  base::expected<std::optional<ReportIdAndPriority>, std::monostate>
   GetReportWithMinPriority(StoredSource::Id, base::Time report_time);
 
   [[nodiscard]] bool DeactivateSourceAtEventLevel(StoredSource::Id);
