7da02fd682e0a42b71e483094d85b7c1f0f2f8ff
Javier Fernández García-Boente <jfernandez@igalia.com>
2026-03-11 16:03:06 -0700

    The <command> element should be treated as invalid in HTML parsing.
    The WHATWG HTML spec removed <command> as a known element long ago. It
    should be parsed as an ordinary unknown element (non-void, can have
    children). We are still treating it as a void head element and a
    "special" element.
    
    Bug: 40775952, 491158089
    Change-Id: Ia35e19daa2fd02319729397ecc201bfc697b8f99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7633305
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Javier Fernandez <jfernandez@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1598051}

--- third_party/blink/renderer/core/html/parser/html_stack_item.h
+++ third_party/blink/renderer/core/html/parser/html_stack_item.h
@@ -230,7 +230,6 @@ class HTMLStackItem final : public GarbageCollected<HTMLStackItem> {
         case html_names::HTMLTag::kCenter:
         case html_names::HTMLTag::kCol:
         case html_names::HTMLTag::kColgroup:
-        case html_names::HTMLTag::kCommand:
         case html_names::HTMLTag::kDd:
         case html_names::HTMLTag::kDetails:
         case html_names::HTMLTag::kDir:
@@ -295,6 +294,8 @@ class HTMLStackItem final : public GarbageCollected<HTMLStackItem> {
         case html_names::HTMLTag::kWbr:
         case html_names::HTMLTag::kXmp:
           return true;
+        case html_names::HTMLTag::kCommand:
+          return !RuntimeEnabledFeatures::HTMLCommandElementRemovalEnabled();
         default:
           return false;
       }
