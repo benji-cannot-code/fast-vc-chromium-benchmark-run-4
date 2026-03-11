c2438022944b35e09d854d2bd618cff22868bc4e
Marc Treib <treib@chromium.org>
2026-03-11 11:00:37 -0700

    ForeignSessionHandler: Remove initial_session_list_
    ...and InitializeForeignSessions(). There seems to be no reason for
    these to exist; the foreign sessions can just be queried on demand.
    
    Bug: 491707172
    Change-Id: I5b5bf4f52d09b60c5e50f824cae199d86a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7657843
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1597873}

--- chrome/browser/ui/webui/history/foreign_session_handler.h
+++ chrome/browser/ui/webui/history/foreign_session_handler.h
@@ -61,8 +61,6 @@ class ForeignSessionHandler : public content::WebUIMessageHandler {
 
   ~ForeignSessionHandler() override;
 
-  void InitializeForeignSessions();
-
   static void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry);
 
   static void OpenForeignSessionTab(content::WebUI* web_ui,
@@ -112,8 +110,6 @@ class ForeignSessionHandler : public content::WebUIMessageHandler {
   void HandleDeleteForeignSession(const base::ListValue& args);
 
   void HandleSetForeignSessionCollapsed(const base::ListValue& args);
-
-  std::optional<base::ListValue> initial_session_list_;
 
   base::CallbackListSubscription foreign_session_updated_subscription_;
 };
