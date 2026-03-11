0e88bc7c41a889c5befa34a01441da8a9b106f81
Ivana Žužić <izuzic@google.com>
2026-03-11 09:21:54 -0700

    Check for TrustedVaultKeyNeeded error in ShouldAllowSavingPasswordsWithInFlowRecovery
    The function now verifies that the password store is in the
    kTrustedVaultKeyNeeded error state before allowing in-flow recovery.
    
    The unit test is updated to use a backend that specifically returns
    kTrustedVaultKeyNeeded. FakePasswordStoreBackend::GetError is updated to
    return password_store_backend_error_ that was set through
    ReturnErrorOnRequest().
    
    DD: go/gpm-cross-platform-ui-errors
    Bug: 483652520
    Change-Id: I81fb0538b7786b779d4ec580f62c1853f0961aa5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7592075
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Reviewed-by: Ioana Treib <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1597800}

--- components/password_manager/core/browser/password_store/fake_password_store_backend.cc
+++ components/password_manager/core/browser/password_store/fake_password_store_backend.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/affiliation/affiliated_match_helper.h"
 #include "components/password_manager/core/browser/password_form.h"
 #include "components/password_manager/core/browser/password_store/get_logins_with_affiliations_request_handler.h"
+#include "components/password_manager/core/browser/password_store/password_store_util.h"
 #include "components/password_manager/core/browser/password_store/psl_matching_helper.h"
 #include "components/sync/model/proxy_data_type_controller_delegate.h"
 
@@ -112,6 +113,10 @@ void FakePasswordStoreBackend::Shutdown(base::OnceClosure shutdown_completed) {
 }
 
 ActionableError FakePasswordStoreBackend::GetError() {
+  if (password_store_backend_error_.has_value()) {
+    return BackendErrorToActionableError(
+        password_store_backend_error_.value().type);
+  }
   return ActionableError::kNoError;
 }
 
