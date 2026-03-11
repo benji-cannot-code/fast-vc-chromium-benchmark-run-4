463f381a0e25767ffef54352fd6d04952263b8f1
Andrew Paseltiner <apaseltiner@chromium.org>
2026-03-11 14:19:43 -0700

    Use macro for spanified inputs in components/apdu fuzzers
    This replaces direct use of the pointer-and-size pair with a
    base::span<const uint8_t>, which is frequently easier to work with and
    avoids the need for manual construction of such spans alongside the
    appropriate unsafe-buffer annotations.
    
    This CL was uploaded by git cl split.
    
    R=martinkr@google.com
    
    Bug: 491067796
    Change-Id: I57b6c7d2df5d998a5ae5300a3b9250ebcc500422
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7653240
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Auto-Submit: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1597990}

--- components/apdu/apdu_fuzzer.cc
+++ components/apdu/apdu_fuzzer.cc
@@ -3,15 +3,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/compiler_specific.h"
+#include "base/containers/span.h"
 #include "components/apdu/apdu_command.h"
 #include "components/apdu/apdu_response.h"
+#include "testing/libfuzzer/libfuzzer_base_wrappers.h"
 
 namespace apdu {
 
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  ApduCommand::CreateFromMessage(UNSAFE_TODO(base::span(data, size)));
-  ApduResponse::CreateFromMessage(UNSAFE_TODO(base::span(data, size)));
+DEFINE_LLVM_FUZZER_TEST_ONE_INPUT_SPAN(const base::span<const uint8_t> data) {
+  ApduCommand::CreateFromMessage(data);
+  ApduResponse::CreateFromMessage(data);
   return 0;
 }
 
