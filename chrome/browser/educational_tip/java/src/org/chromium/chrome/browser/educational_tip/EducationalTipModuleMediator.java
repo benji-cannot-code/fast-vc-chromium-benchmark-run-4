cef18ce943c9034029c4ccd9c2b41a778547d920
Gazal <agazal@google.com>
2026-03-11 08:39:33 -0700

    [Setup List] Prevent premature removal of default browser promo
    When the startup default browser promo (Role Manager) is triggered, it notifies listeners to hide other promo surfaces. This inadvertently caused the Setup List default browser promo to remove itself before being shown, creating an invisible incomplete task that blocked the celebratory promo.
    
    Bug: 469425754, 491506953, 491494883
    Change-Id: I8a954ce07e94205edbbc26262770c4e80bc78098
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7653663
    Auto-Submit: Gazal Agarwal <agazal@google.com>
    Reviewed-by: Xinyi Ji <xinyiji@chromium.org>
    Commit-Queue: Gazal Agarwal <agazal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1597776}

--- chrome/browser/educational_tip/java/src/org/chromium/chrome/browser/educational_tip/EducationalTipModuleMediator.java
+++ chrome/browser/educational_tip/java/src/org/chromium/chrome/browser/educational_tip/EducationalTipModuleMediator.java
@@ -207,7 +207,9 @@ public class EducationalTipModuleMediator {
      * the educational tip module from the magic stack.
      */
     private void removeModule() {
-        mModuleDelegate.removeModule(mModuleType);
+        if (!SetupListModuleUtils.isSetupListModule(mModuleType)) {
+            mModuleDelegate.removeModule(mModuleType);
+        }
         removeDefaultBrowserPromoTriggerStateListener();
     }
 
