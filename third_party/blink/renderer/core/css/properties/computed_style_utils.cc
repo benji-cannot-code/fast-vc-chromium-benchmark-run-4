d870a60b0ccba645f03714f79203747249ef18a0
Ian Kilpatrick <ikilpatrick@chromium.org>
2026-03-12 11:07:41 -0700

    [style] Fix parsing of CSS will-change.
    For forwards-compatibility CSS will-change accepts a list of
    <custom-ident> as valid values. We did this previously however within
    computed style we dropped any unknown values which we couldn't resolve
    to a CSS property.
    
    This was incorrect.
    
    This patch instead stores a Vector<AtomicString> of values in
    StyleWillChangeData. This allows us to preserve the values for
    getComputedStyle().
    
    We can do this now we have a CSSBitset for querying if a particular
    longhand is present.
    
    Change-Id: I76ce32fcab04e8352d370ab0380627513109de5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7658908
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1598553}

--- third_party/blink/renderer/core/css/properties/computed_style_utils.cc
+++ third_party/blink/renderer/core/css/properties/computed_style_utils.cc
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/cssom/css_unparsed_value.h"
 #include "third_party/blink/renderer/core/css/cssom/css_unsupported_color.h"
 #include "third_party/blink/renderer/core/css/cssom_utils.h"
+#include "third_party/blink/renderer/core/css/parser/css_property_parser.h"
 #include "third_party/blink/renderer/core/css/properties/css_parsing_utils.h"
 #include "third_party/blink/renderer/core/css/properties/longhands.h"
 #include "third_party/blink/renderer/core/css/properties/shorthands.h"
@@ -2442,15 +2443,14 @@ CSSValue* ComputedStyleUtils::ValueForWillChange(
     const StyleWillChangeData* will_change) {
   CSSValueList* list = CSSValueList::CreateCommaSeparated();
   if (will_change) {
-    for (const auto& value : will_change->values) {
-      if (value.is_contents) {
+    for (const AtomicString& value : will_change->values) {
+      const CSSValueID id = CssValueKeywordID(value);
+      if (id == CSSValueID::kContents) {
         list->Append(*CSSIdentifierValue::Create(CSSValueID::kContents));
-      } else if (value.is_scroll_position) {
+      } else if (id == CSSValueID::kScrollPosition) {
         list->Append(*CSSIdentifierValue::Create(CSSValueID::kScrollPosition));
       } else {
-        const CSSPropertyID id = static_cast<CSSPropertyID>(value.property_id);
-        DCHECK_NE(id, CSSPropertyID::kInvalid);
-        list->Append(*MakeGarbageCollected<CSSCustomIdentValue>(id));
+        list->Append(*MakeGarbageCollected<CSSCustomIdentValue>(value));
       }
     }
   }
