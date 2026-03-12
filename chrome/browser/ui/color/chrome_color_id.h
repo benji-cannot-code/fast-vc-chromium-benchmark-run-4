c39619c56b83e9d0ae03c8a79bf4080088d872a2
Emily Shack <emshack@chromium.org>
2026-03-11 19:15:23 -0700

    [GlowUp] Change dark mode active tab color
    Bug: 491931310
    Change-Id: Ie57333737f4e268bc880d9d30afa77dea582f171
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7659773
    Auto-Submit: Emily Shack <emshack@chromium.org>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1598151}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -875,6 +875,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorHoverCardTabAlertAudioPlayingIcon) \
   /* Tab colors. */ \
   E_CPONLY(kColorTabBackgroundActiveFrameActive) \
+  E_CPONLY(kColorDetachedTabBackgroundActiveFrameActive) \
   E_CPONLY(kColorTabBackgroundActiveFrameInactive) \
   E_CPONLY(kColorTabBackgroundInactiveFrameActive) \
   E_CPONLY(kColorTabBackgroundInactiveFrameInactive) \
