166f7a256a2c20835bfd949925a9467932394f98
Devlin Cronin <rdevlin.cronin@chromium.org>
2026-03-12 14:05:04 -0700

    [Extensions] Update system.display API tests to MV3
    MV2 has been unsupported for several months. We can remove testing
    support for it. Existing tests should be updated to MV3.
    
    Update API tests for the system.display API to use manifest version 3
    instead of manifest version 2 and 3.
    
    With the transition to MV3, many of these tests may also be able to be
    updated to use new patterns, such as async / await, service worker
    modules, and more. However, those changes are out of scope for these
    CLs, and can be addressed in followups.
    
    Bug: 491516661
    Change-Id: I264b268b7e39ef2550114fb0b27c03e65ef9557e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7659218
    Reviewed-by: Tim <tjudkins@chromium.org>
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1598673}

--- chrome/browser/extensions/api/system_display/system_display_extension_apitest.cc
+++ chrome/browser/extensions/api/system_display/system_display_extension_apitest.cc
@@ -24,13 +24,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace extensions {
 
-using ContextType = extensions::browser_test_util::ContextType;
-
-class SystemDisplayExtensionApiTest
-    : public ExtensionApiTest,
-      public testing::WithParamInterface<ContextType> {
+class SystemDisplayExtensionApiTest : public ExtensionApiTest {
  public:
-  SystemDisplayExtensionApiTest() : ExtensionApiTest(GetParam()) {}
+  SystemDisplayExtensionApiTest() = default;
   ~SystemDisplayExtensionApiTest() override = default;
   SystemDisplayExtensionApiTest(const SystemDisplayExtensionApiTest&) = delete;
   SystemDisplayExtensionApiTest& operator=(
@@ -41,10 +37,6 @@ class SystemDisplayExtensionApiTest
     DisplayInfoProvider::InitializeForTesting(provider_.get());
   }
 
-  void TearDownOnMainThread() override {
-    ExtensionApiTest::TearDownOnMainThread();
-  }
-
  protected:
   std::unique_ptr<MockDisplayInfoProvider> provider_ =
       std::make_unique<MockDisplayInfoProvider>();
@@ -53,34 +45,11 @@ class SystemDisplayExtensionApiTest
 // TODO(crbug.com/40779611): Revisit this after screen creation refactoring.
 #if !BUILDFLAG(IS_WIN)
 
-INSTANTIATE_TEST_SUITE_P(PersistentBackground,
-                         SystemDisplayExtensionApiTest,
-                         ::testing::Values(ContextType::kPersistentBackground));
-INSTANTIATE_TEST_SUITE_P(ServiceWorker,
-                         SystemDisplayExtensionApiTest,
-                         ::testing::Values(ContextType::kServiceWorker));
-
-IN_PROC_BROWSER_TEST_P(SystemDisplayExtensionApiTest, GetDisplayInfo) {
+IN_PROC_BROWSER_TEST_F(SystemDisplayExtensionApiTest, GetDisplayInfo) {
   ASSERT_TRUE(RunExtensionTest("system_display/info")) << message_;
 }
 
-class SystemDisplayExtensionApiEventTest
-    : public SystemDisplayExtensionApiTest {
- public:
-  SystemDisplayExtensionApiEventTest() = default;
-  ~SystemDisplayExtensionApiEventTest() override = default;
-  SystemDisplayExtensionApiEventTest(
-      const SystemDisplayExtensionApiEventTest&) = delete;
-  SystemDisplayExtensionApiEventTest& operator=(
-      const SystemDisplayExtensionApiEventTest&) = delete;
-};
-
-INSTANTIATE_TEST_SUITE_P(ServiceWorker,
-                         SystemDisplayExtensionApiEventTest,
-                         ::testing::Values(ContextType::kServiceWorker));
-
-IN_PROC_BROWSER_TEST_P(SystemDisplayExtensionApiEventTest,
-                       OnDisplayChangedEvent) {
+IN_PROC_BROWSER_TEST_F(SystemDisplayExtensionApiTest, OnDisplayChangedEvent) {
   ExtensionTestMessageListener listener_for_extension_ready("ready");
   const Extension* extension = LoadExtension(
       test_data_dir_.AppendASCII("system_display/on_display_changed"));
@@ -102,13 +71,7 @@ IN_PROC_BROWSER_TEST_P(SystemDisplayExtensionApiEventTest,
 
 #if !BUILDFLAG(IS_CHROMEOS)
 
-using SystemDisplayExtensionApiFunctionTest = SystemDisplayExtensionApiTest;
-
-INSTANTIATE_TEST_SUITE_P(PersistentBackground,
-                         SystemDisplayExtensionApiFunctionTest,
-                         ::testing::Values(ContextType::kPersistentBackground));
-
-IN_PROC_BROWSER_TEST_P(SystemDisplayExtensionApiFunctionTest, SetDisplay) {
+IN_PROC_BROWSER_TEST_F(SystemDisplayExtensionApiTest, SetDisplay) {
   scoped_refptr<SystemDisplaySetDisplayPropertiesFunction> set_info_function(
       new SystemDisplaySetDisplayPropertiesFunction());
 
