6127d4da41f5803adb0b25aabbe15505a035ca57
Devlin Cronin <rdevlin.cronin@chromium.org>
2026-03-12 16:10:12 -0700

    [Extensions Cleanup] Simple JS updates in api_test/all_urls
    This CL updates extension JS files in api_test/all_urls to
    * use `let` and `const` instead of `var`
    * update variable naming patterns (avoiding underscores, properly
      naming consts)
    * prefer single quotes instead of double quotes and use template
      literals when appropriate
    
    Bug: 492151886
    Change-Id: If340e5fb73ed968e23e74ebbd3b23a1fe919553b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7664006
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Eva Su <evasu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1598755}

--- chrome/test/data/extensions/api_test/all_urls/content_script/background.js
+++ chrome/test/data/extensions/api_test/all_urls/content_script/background.js
@@ -6,6 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     // Let the extension know where the script ran.
-    var url = sender.tab ? sender.tab.url : 'about:blank';
-    chrome.test.sendMessage('content script: ' + url);
+    const url = sender.tab ? sender.tab.url : 'about:blank';
+    chrome.test.sendMessage(`content script: ${url}`);
   });
--- chrome/test/data/extensions/api_test/all_urls/content_script/content_script.js
+++ chrome/test/data/extensions/api_test/all_urls/content_script/content_script.js
@@ -4,4 +4,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 // Let the background page know this content script executed.
-chrome.runtime.sendMessage({greeting: "hello"});
+chrome.runtime.sendMessage({greeting: 'hello'});
