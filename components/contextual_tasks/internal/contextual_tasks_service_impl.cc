97953f0db138ff819e00046e1b883d906274901d
Matt Jones <mdjones@chromium.org>
2026-03-12 10:02:00 -0700

    [Contextual Tasks] Remove c_ prefix from gemini server IDs if present
    Gemini server IDs are shipped from the backend with a c_ prefix. This
    needs to be removed prior to building the URL in order for it to work
    correctly. This patch removes the prefix if it exists.
    
    Change-Id: I782b78a6900240962817b04f7a8148aefd225e39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7661510
    Reviewed-by: Chase Hartsell <chazzy@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1598506}

--- components/contextual_tasks/internal/contextual_tasks_service_impl.cc
+++ components/contextual_tasks/internal/contextual_tasks_service_impl.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/notreached.h"
+#include "base/strings/string_util.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/uuid.h"
 #include "components/contextual_search/contextual_search_service.h"
@@ -578,7 +579,11 @@ void ContextualTasksServiceImpl::GetThreadUrlFromTaskId(
               if (path.back() != '/') {
                 path += '/';
               }
-              path += thread->server_id;
+              std::string server_id = thread->server_id;
+              if (base::StartsWith(server_id, "c_")) {
+                server_id.erase(0, 2);
+              }
+              path += server_id;
               replacements.SetPathStr(path);
 
               url = url.ReplaceComponents(replacements);
