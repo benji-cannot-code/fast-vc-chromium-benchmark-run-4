1dae35e974825b9a3f220f355ee90195bc183968
Alison Gale <agale@chromium.org>
2026-03-12 10:36:45 -0700

    [Bookmarks Tree View] Ensure drag/add/remove changes are propagated
    Since a copy of the children was made, we were missing change detection
    events. Added a regression test and confirmed it fails at head.
    
    Bug: 491834039
    Change-Id: I4a2a344c1352dc4064715aff92d238e4edf16f02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7658836
    Commit-Queue: Alison Gale <agale@chromium.org>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1598523}

--- chrome/browser/resources/side_panel/bookmarks/power_bookmark_row.ts
+++ chrome/browser/resources/side_panel/bookmarks/power_bookmark_row.ts
@@ -106,7 +106,6 @@ export class PowerBookmarkRowElement extends CrLitElement {
   accessor hasActiveDrag: boolean = false;
   accessor activeFolderPath: BookmarksTreeNode[] = [];
   accessor hasFolders: boolean = false;
-  accessor sortedChildren: BookmarksTreeNode[] = [];
   accessor activeSortIndex: number = 0;
 
   accessor listItemSize: CrUrlListItemSize = CrUrlListItemSize.COMPACT;
@@ -157,10 +156,10 @@ export class PowerBookmarkRowElement extends CrLitElement {
     if (changedProperties.has('bookmark') &&
         this.bookmark.id !== changedProperties.get('bookmark')?.id) {
       this.toggleExpand = false;
-      this.sortedChildren =
-          this.bookmark.children ? [...this.bookmark.children] : [];
-      this.bookmarksService_.sortBookmarks(
-          this.sortedChildren, this.activeSortIndex);
+      if (this.isFolder_() && this.bookmark.children) {
+        this.bookmarksService_.sortBookmarks(
+            this.bookmark.children, this.activeSortIndex);
+      }
     }
 
     if (changedProperties.has('activeFolderPath')) {
@@ -181,9 +180,10 @@ export class PowerBookmarkRowElement extends CrLitElement {
       }
     }
 
-    if (changedProperties.has('activeSortIndex')) {
+    if (changedProperties.has('activeSortIndex') && this.isFolder_() &&
+        this.bookmark.children) {
       this.bookmarksService_.sortBookmarks(
-          this.sortedChildren, this.activeSortIndex);
+          this.bookmark.children, this.activeSortIndex);
     }
   }
 
