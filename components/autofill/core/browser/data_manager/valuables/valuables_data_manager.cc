0920e58ce61d2389e7b6fa509a195559aacc35bb
Simge Şengün <simges@google.com>
2026-03-12 06:09:52 -0700

    Implement server-to-client sync for `ValuableMetadata`
    Implements the sync logic for `ValuableMetadata` in
    `ValuableMetadataSyncBridge` to support merging remote changes and
    uploading initial local data. Updates `ValuableSyncBridge` to preserve
    local metadata when applying remote `LoyaltyCard` updates and to notify
    metadata observers upon deletion. Finally, updates
    `ValuablesDataManager` to reload data when `ValuableMetadata` changes
    occur.
    
    Fixed: 477841597
    Change-Id: Idc6aff8dc34a5af19c5b28336b5b13608acd65b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7509499
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Simge Şengün <simges@google.com>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1598375}

--- components/autofill/core/browser/data_manager/valuables/valuables_data_manager.cc
+++ components/autofill/core/browser/data_manager/valuables/valuables_data_manager.cc
@@ -169,7 +169,8 @@ bool ValuablesDataManager::HasPendingQueries() const {
 }
 
 void ValuablesDataManager::OnAutofillChangedBySync(syncer::DataType data_type) {
-  if (data_type == syncer::DataType::AUTOFILL_VALUABLE) {
+  if (data_type == syncer::DataType::AUTOFILL_VALUABLE ||
+      data_type == syncer::DataType::AUTOFILL_VALUABLE_METADATA) {
     LoadLoyaltyCards();
   }
 }
