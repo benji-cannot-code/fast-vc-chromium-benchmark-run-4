93b03629f350bbd6c4d89abd0bd441f559748971
Chrome Crash (Prod) <crash@system.gserviceaccount.com>
2026-03-12 04:45:54 -0700

    Revert "Simplify the is_isolated branch when resolving effective display mode"
    This reverts commit 74374c0eb16f0594bbf7c39a23bb41f6756a5da8.
    
    Reason for revert: 
    The Suspect CL https://chromium-review.googlesource.com/q/I582666c11d1e0fb3c0f6202179e14a654a915935 is highly likely the cause of the crash. Please see the crash report http://crash/571d79c264e0935e.
    Rebase the Revert CL on top of main branch with conflicts allowed option to prevent false positive merge conflicts.
    
    If you agree with the revert CL, please approve it and mark as Commit-Queue +2 to submit it.
    
    More details can be found in the go/crash-revert-cls-creation.
    
    Original change's description:
    > Simplify the is_isolated branch when resolving effective display mode
    > 
    > This CL articulates the IWA-specific branch and highlights that the
    > potential resolved modes can be standalone, unframed and window
    > controls overlay.
    > 
    > Bug: None
    > Change-Id: I582666c11d1e0fb3c0f6202179e14a654a915935
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7614307
    > Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    > Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1597817}
    > 
    Crash Bug: 492057027
    
    Change-Id: I235ffb2271e01de067df735f76f58db062920a86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7657095
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1598345}

--- chrome/browser/web_applications/web_app.h
+++ chrome/browser/web_applications/web_app.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/check.h"
-#include "base/check_is_test.h"
 #include "base/containers/flat_map.h"
 #include "base/containers/flat_set.h"
 #include "base/time/time.h"
@@ -128,12 +127,7 @@ class WebApp {
     return dark_mode_background_color_;
   }
 
-  DisplayMode display_mode() const {
-    if (display_mode_ == DisplayMode::kUndefined) {
-      CHECK_IS_TEST();
-    }
-    return display_mode_;
-  }
+  DisplayMode display_mode() const { return display_mode_; }
 
   mojom::UserDisplayMode user_display_mode() const {
     return ToMojomUserDisplayMode(
