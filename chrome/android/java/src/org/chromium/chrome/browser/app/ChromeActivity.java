6da9009577c71cf82e3ed6b76cdaddf9cd72c199
Michael Thiessen <mthiesse@chromium.org>
2026-03-12 08:08:38 -0700

    Remove flushing arm from UmaSessionCorrectnessFixed experiment
    Flushing doesn't work as I thought it did, and doesn't solve the
    data loss issues. We're definitely not going to ship this arm, so let's
    delete the code.
    
    Bug: 400975101
    Change-Id: I8a03b00ef08de482b66a7a99c468df3770399be7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7656604
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1598437}

--- chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
@@ -1507,10 +1507,6 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
 
     @CallSuper
     public void flushPersistentState() {
-        boolean flushSession =
-                ChromeFeatureList.getFieldTrialParamByFeatureAsBoolean(
-                        ChromeFeatureList.UMA_SESSION_CORRECTNESS_FIXES, "flush_session", true);
-        if (flushSession) mUmaActivityObserver.flushUmaSession();
         ProfileManagerUtils.flushPersistentDataForAllProfiles();
     }
 
--- chrome/android/java/src/org/chromium/chrome/browser/metrics/UmaSessionStats.java
+++ chrome/android/java/src/org/chromium/chrome/browser/metrics/UmaSessionStats.java
@@ -187,10 +187,6 @@ public class UmaSessionStats {
         UmaSessionStatsJni.get().umaEndSession(sNativeUmaSessionStats);
     }
 
-    public void flushSession() {
-        UmaSessionStatsJni.get().flushSession(sNativeUmaSessionStats);
-    }
-
     /**
      * Updates the metrics services based on a change of consent. This can happen during first-run
      * flow, and when the user changes their preferences.
@@ -311,8 +307,6 @@ public class UmaSessionStats {
         void umaResumeSession(long nativeUmaSessionStats);
 
         void umaEndSession(long nativeUmaSessionStats);
-
-        void flushSession(long nativeUmaSessionStats);
 
         void registerExternalExperiment(int[] experimentIds, boolean overrideExistingIds);
 
