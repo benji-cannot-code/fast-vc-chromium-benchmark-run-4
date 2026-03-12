500c0bf45321aba30297b386c8c5ba31f82787da
Sam Goto <goto@chromium.org>
2026-03-12 14:38:21 -0700

    [page actions] Allow PageActionObservers to observe anchored messages
    This CL adds the ability for PageActionObservers to observe changes to
    the visibility status of anchored messages.
    
    Bug: 448399684
    Change-Id: Ieb64843bf27964574057b5459bf4ce6e0819ccca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7662199
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Sam Goto <goto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1598690}

--- chrome/browser/ui/views/page_action/page_action_observer.h
+++ chrome/browser/ui/views/page_action/page_action_observer.h
@@ -34,6 +34,7 @@ struct PageActionState {
   actions::ActionId action_id;
   bool showing = false;
   bool chip_showing = false;
+  bool anchored_message_showing = false;
 
   // Not set if the page action is not showing.
   std::optional<std::u16string> tooltip = std::nullopt;
@@ -62,6 +63,13 @@ class PageActionObserver {
   // This is invoked in addition to the "icon shown" notification.
   virtual void OnPageActionChipShown(const PageActionState& page_action) {}
   virtual void OnPageActionChipHidden(const PageActionState& page_action) {}
+
+  // Invoked when the specified page action anchored message becomes
+  // visible/hidden.
+  virtual void OnPageActionAnchoredMessageShown(
+      const page_actions::PageActionState& page_action) {}
+  virtual void OnPageActionAnchoredMessageHidden(
+      const page_actions::PageActionState& page_action) {}
 
   const PageActionState& GetCurrentPageActionState() const;
 
