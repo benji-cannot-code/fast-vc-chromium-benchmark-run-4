d10f9e84fbc359d9cf0800dce8cbe70c75c56626
Gyuyoung Kim <gyuyoung@igalia.com>
2026-03-11 23:06:14 -0700

    [iOS Blink] Add dummy Set|GetInternalScrollToTextFragment() methods
    https://crrev.com/c/7628840 added Set|GetInternalScrollToTextFragment()
    methods, so ContentNavigationItem also needs to implement them. However,
    the iOS Blink port does not support the text_fragment field yet. This CL
    adds placeholder implementations for now.
    
    Bug: 482925620
    Change-Id: I5f1aff48f69e3ff93fbdd213345550007628c69b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7652337
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1598228}

--- ios/web/content/navigation/content_navigation_item.h
+++ ios/web/content/navigation/content_navigation_item.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
+#import <optional>
+
 #import "base/memory/raw_ptr.h"
 #import "ios/web/common/user_agent.h"
 #import "ios/web/public/favicon/favicon_status.h"
@@ -50,6 +52,12 @@ class ContentNavigationItem : public NavigationItem {
   const std::u16string& GetTitle() const override;
 
   const std::u16string& GetTitleForDisplay() const override;
+
+  void SetInternalScrollToTextFragment(
+      const std::optional<std::string>& internal_scroll_to_text_fragment)
+      override;
+  const std::optional<std::string>& GetInternalScrollToTextFragment()
+      const override;
 
   void SetTransitionType(ui::PageTransition transition_type) override;
   ui::PageTransition GetTransitionType() const override;
