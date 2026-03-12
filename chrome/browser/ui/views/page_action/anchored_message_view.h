7a53fd4276b389f0e92c4109db8b12ee3b4410a6
Dmytro Yeroshkin <yeroshkin@chromium.org>
2026-03-12 13:24:05 -0700

    [Clarity] Allow anchored messages to be updated when model is updated
    Refactored anchored_message_view.cc to always create the possible child views, and using visibility to control whether the child view is visible.
    
    Added unit tests to verify that the visibility of child views is set correctly both in the initial creation of anchored messages and when updating.
    
    Also fixed some IWYU issues
    
    Bug: 376285838
    Change-Id: I334a6ad3054bda69cd7284f91c230ddd39e8909a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7657425
    Commit-Queue: Dmytro Yeroshkin <yeroshkin@chromium.org>
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Reviewed-by: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1598635}

--- chrome/browser/ui/views/page_action/anchored_message_view.h
+++ chrome/browser/ui/views/page_action/anchored_message_view.h
@@ -12,17 +12,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
 #include "chrome/browser/ui/views/page_action/page_action_model.h"
+#include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/base/models/image_model.h"
 #include "ui/views/bubble/bubble_dialog_delegate_view.h"
-#include "ui/views/controls/button/image_button.h"
-#include "ui/views/controls/image_view.h"
 #include "ui/views/view.h"
 
 namespace views {
+class ImageButton;
+class ImageView;
+class Label;
 class Widget;
 }
 
 namespace page_actions {
+
+class ChipContainerView;
 
 // AnchoredMessageBubbleView is the view displaying the anchored message for a
 // given page action. It is created and destroyed dynamically.
@@ -45,6 +49,8 @@ class AnchoredMessageBubbleView : public views::BubbleDialogDelegate,
   views::Widget* GetWidget() override;
   const views::Widget* GetWidget() const override;
 
+  void UpdateContent(const PageActionModelInterface& model);
+
  protected:
   void OnThemeChanged() override;
 
@@ -52,12 +58,12 @@ class AnchoredMessageBubbleView : public views::BubbleDialogDelegate,
   void ChipCallback();
 
   raw_ptr<views::Label> label_ = nullptr;
-  raw_ptr<views::View> chip_container_ = nullptr;
+  raw_ptr<ChipContainerView> chip_container_ = nullptr;
   raw_ptr<views::ImageButton> close_button_ = nullptr;
   raw_ptr<views::ImageView> icon_view_ = nullptr;
   std::optional<ui::ImageModel> icon_ = std::nullopt;
-  const std::u16string label_text_;
-  const bool show_close_button_;
+  std::u16string label_text_;
+  bool show_close_button_;
   base::RepeatingClosure chip_callback_;
   base::RepeatingClosure close_callback_;
 };
