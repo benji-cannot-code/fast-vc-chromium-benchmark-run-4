47cfe518b148b2ad738328dbfb7f91d3d8e34c6f
Jeremy Roman <jbroman@chromium.org>
2026-03-12 09:08:44 -0700

    Reland "Allow the Indigo onboarding dialog to resize automatically."
    This is a reland of commit e1f0f1324853df0ea9c86c368489e9f6eb01bd26
    
    IndigoOnboardingDialog::view_observation_ needed to be reset before the
    observed view is destroyed.
    
    Original change's description:
    > Allow the Indigo onboarding dialog to resize automatically.
    >
    > There's just one trick here, which is that the tab dialog manager
    > manages the bounds of the dialogs and so it needs to be notified when
    > the contents desires to change size.
    >
    > The web page by default will grow but not shrink unless it takes care
    > to make sure that its <html> element shrinks to match its contents.
    >
    > Bug: b:491559009
    > Change-Id: I632bf05780c26789e41b4ddbf0d2a070e5140ce2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7655293
    > Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    > Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1598163}
    
    Bug: b:491559009
    Cq-Include-Trybots: luci.chromium.try:linux_chromium_asan_rel_ng
    Change-Id: Iff6614facc3a251b00106e9a177221ca5e4dcd0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7661504
    Auto-Submit: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1598468}

--- chrome/browser/indigo/onboarding/indigo_onboarding_dialog.h
+++ chrome/browser/indigo/onboarding/indigo_onboarding_dialog.h
@@ -9,7 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/functional/callback_forward.h"
+#include "base/memory/raw_ptr.h"
+#include "base/scoped_observation.h"
 #include "ui/base/interaction/element_identifier.h"
+#include "ui/views/view_observer.h"
 #include "ui/views/widget/widget.h"
 
 class GURL;
@@ -20,12 +23,13 @@ class TabInterface;
 
 namespace views {
 class DialogDelegate;
-}
+class View;
+}  // namespace views
 
 namespace indigo {
 
 // Owns and manages an onboarding dialog which is mainly powered by a WebView.
-class IndigoOnboardingDialog {
+class IndigoOnboardingDialog : public views::ViewObserver {
  public:
   DECLARE_CLASS_ELEMENT_IDENTIFIER_VALUE(kWebViewId);
 
@@ -41,7 +45,7 @@ class IndigoOnboardingDialog {
   IndigoOnboardingDialog(const IndigoOnboardingDialog&) = delete;
   IndigoOnboardingDialog& operator=(const IndigoOnboardingDialog&) = delete;
 
-  ~IndigoOnboardingDialog();
+  ~IndigoOnboardingDialog() override;
 
   // Closes the dialog immediately. This will call the `close_callback` passed
   // to `Show`, likely resulting in the destruction of this object.
@@ -54,12 +58,23 @@ class IndigoOnboardingDialog {
 
   void OnWidgetClosed(views::Widget::ClosedReason reason);
 
+  // views::ViewObserver:
+  void OnViewPreferredSizeChanged(views::View* observed_view) override;
+
+  // It is safe to hold a raw pointer to `tab_` because the dialog is tab-modal
+  // and will be closed (and this object destroyed by its owner) if the tab is
+  // destroyed.
+  const raw_ptr<tabs::TabInterface> tab_;
+
   base::OnceClosure close_callback_;
 
   // `widget_` must be destroyed before `delegate_` because the widget holds a
   // raw pointer to the delegate.
   std::unique_ptr<views::DialogDelegate> delegate_;
   std::unique_ptr<views::Widget> widget_;
+
+  base::ScopedObservation<views::View, views::ViewObserver> view_observation_{
+      this};
 };
 
 }  // namespace indigo
