0cc9eff9002b1ae09f36fcb62d0184eac4c3fb01
Hana Gustyn <hanagustyn@microsoft.com>
2026-03-12 10:39:03 -0700

    Add markdown MIME type mapping
    Adds a `text/markdown` MIME type mapping for `.md` files in the
    secondary mappings table. This enables correct MIME type identification
    for markdown files by extension.
    
    Added unit test entries for the new mapping in `mime_util_unittest.cc`.
    
    Change-Id: Id668031aa2622c0576f7751b1b9272aac75cf030
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7637407
    Reviewed-by: mmenke <mmenke@chromium.org>
    Reviewed-by: Brandon Maslen <brandm@microsoft.com>
    Commit-Queue: Hana Gustyn <hanagustyn@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1598527}

--- net/base/mime_util.cc
+++ net/base/mime_util.cc
@@ -254,6 +254,7 @@ static const MimeInfo kSecondaryMappings[] = {
     {"message/rfc822", "eml"},
     {"text/calendar", "ics"},
     {"text/html", "ehtml"},
+    {"text/markdown", "md"},
     {"text/plain", "txt,text"},
     {"text/vtt", "vtt"},
     {"text/x-sh", "sh"},
