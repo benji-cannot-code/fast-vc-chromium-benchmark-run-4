7ce93e49f36aa91278b023e3258e876ba16e42ca
Devlin Cronin <rdevlin.cronin@chromium.org>
2026-03-11 18:24:59 -0700

    [Extensions] Update nativeMessaging API tests to MV3
    MV2 has been unsupported for several months. We can remove testing
    support for it. Existing tests should be updated to MV3.
    
    Update API tests for the nativeMessaging API to use manifest version 3
    instead of manifest version 2 and 3.
    
    With the transition to MV3, many of these tests may also be able to be
    updated to use new patterns, such as async / await, service worker
    modules, and more. However, those changes are out of scope for these
    CLs, and can be addressed in followups.
    
    Additional Notes:
    * These tests used to have a separate "/lazy" directory for the lazy
      vs non-lazy version of a test extension. These have been collapsed
      into a single extension.
    
    Bypass-Check-License: Moved source file.
    Bug: 491516661
    Change-Id: I73ed44000f0fb0b37faa9d6db4ab847ab72df37d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7658646
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Andrea Orru <andreaorru@chromium.org>
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1598126}

--- chrome/browser/extensions/api/messaging/native_messaging_apitest.cc
+++ chrome/browser/extensions/api/messaging/native_messaging_apitest.cc
@@ -44,17 +44,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace extensions {
 namespace {
 
-using ContextType = extensions::browser_test_util::ContextType;
-
-class NativeMessagingApiTestBase : public ExtensionApiTest {
+class NativeMessagingApiTest : public ExtensionApiTest {
  public:
-  explicit NativeMessagingApiTestBase(
-      ContextType context_type = ContextType::kNone)
-      : ExtensionApiTest(context_type) {}
-  ~NativeMessagingApiTestBase() override = default;
-  NativeMessagingApiTestBase(const NativeMessagingApiTestBase&) = delete;
-  NativeMessagingApiTestBase& operator=(const NativeMessagingApiTestBase&) =
-      delete;
+  NativeMessagingApiTest() = default;
+  ~NativeMessagingApiTest() override = default;
+  NativeMessagingApiTest(const NativeMessagingApiTest&) = delete;
+  NativeMessagingApiTest& operator=(const NativeMessagingApiTest&) = delete;
 
  protected:
   size_t GetTotalTabCount() const {
@@ -70,13 +65,13 @@ class NativeMessagingApiTestBase : public ExtensionApiTest {
 
 // Tests basic functionality of chrome.runtime.sendNativeMessage in an MV3
 // extension.
-IN_PROC_BROWSER_TEST_F(NativeMessagingApiTestBase, SendNativeMessage) {
+IN_PROC_BROWSER_TEST_F(NativeMessagingApiTest, SendNativeMessage) {
   constexpr bool kUserLevel = false;
   ASSERT_NO_FATAL_FAILURE(test_host_.RegisterTestHost(kUserLevel));
   ASSERT_TRUE(RunExtensionTest("native_messaging_send_native_message"));
 }
 
-IN_PROC_BROWSER_TEST_F(NativeMessagingApiTestBase, UserLevelSendNativeMessage) {
+IN_PROC_BROWSER_TEST_F(NativeMessagingApiTest, UserLevelSendNativeMessage) {
   constexpr bool kUserLevel = true;
   ASSERT_NO_FATAL_FAILURE(test_host_.RegisterTestHost(kUserLevel));
   ASSERT_TRUE(RunExtensionTest("native_messaging_send_native_message"));
@@ -86,7 +81,7 @@ IN_PROC_BROWSER_TEST_F(NativeMessagingApiTestBase, UserLevelSendNativeMessage) {
 // On Windows, a new codepath is used to directly launch .EXE-based Native
 // Hosts. This codepath allows launching of Native Hosts even when cmd.exe is
 // disabled or misconfigured.
-class NativeMessagingLaunchExeTest : public NativeMessagingApiTestBase,
+class NativeMessagingLaunchExeTest : public NativeMessagingApiTest,
                                      public testing::WithParamInterface<bool> {
  public:
   NativeMessagingLaunchExeTest() {
@@ -135,55 +130,27 @@ IN_PROC_BROWSER_TEST_P(NativeMessagingLaunchExeTest,
 }
 #endif
 
-class NativeMessagingApiTest : public NativeMessagingApiTestBase,
-                               public testing::WithParamInterface<ContextType> {
- public:
-  NativeMessagingApiTest() : NativeMessagingApiTestBase(GetParam()) {}
-  ~NativeMessagingApiTest() override = default;
-  NativeMessagingApiTest(const NativeMessagingApiTest&) = delete;
-  NativeMessagingApiTest& operator=(const NativeMessagingApiTest&) = delete;
-
- protected:
-  bool RunTest(const char* extension_name) {
-    if (GetParam() == ContextType::kPersistentBackground) {
-      return RunExtensionTest(extension_name);
-    }
-    std::string lazy_extension_name = base::StrCat({extension_name, "/lazy"});
-    return RunExtensionTest(lazy_extension_name.c_str());
-  }
-};
-
-INSTANTIATE_TEST_SUITE_P(PersistentBackground,
-                         NativeMessagingApiTest,
-                         ::testing::Values(ContextType::kPersistentBackground));
-INSTANTIATE_TEST_SUITE_P(EventPage,
-                         NativeMessagingApiTest,
-                         ::testing::Values(ContextType::kEventPage));
-INSTANTIATE_TEST_SUITE_P(ServiceWorker,
-                         NativeMessagingApiTest,
-                         ::testing::Values(ContextType::kServiceWorker));
-
 // Tests chrome.runtime.sendNativeMessage to a native messaging host.
-IN_PROC_BROWSER_TEST_P(NativeMessagingApiTest, NativeMessagingBasic) {
+IN_PROC_BROWSER_TEST_F(NativeMessagingApiTest, NativeMessagingBasic) {
   ASSERT_NO_FATAL_FAILURE(test_host_.RegisterTestHost(false));
-  ASSERT_TRUE(RunTest("native_messaging")) << message_;
+  ASSERT_TRUE(RunExtensionTest("native_messaging")) << message_;
 }
 
-IN_PROC_BROWSER_TEST_P(NativeMessagingApiTest, UserLevelNativeMessaging) {
+IN_PROC_BROWSER_TEST_F(NativeMessagingApiTest, UserLevelNativeMessaging) {
   ASSERT_NO_FATAL_FAILURE(test_host_.RegisterTestHost(true));
-  ASSERT_TRUE(RunTest("native_messaging")) << message_;
+  ASSERT_TRUE(RunExtensionTest("native_messaging")) << message_;
 }
 
 // Tests chrome.runtime.connectNative to a native messaging host.
-IN_PROC_BROWSER_TEST_P(NativeMessagingApiTest, ConnectNative) {
+IN_PROC_BROWSER_TEST_F(NativeMessagingApiTest, ConnectNative) {
   ASSERT_NO_FATAL_FAILURE(test_host_.RegisterTestHost(false));
-  ASSERT_TRUE(RunTest("native_messaging_connect")) << message_;
+  ASSERT_TRUE(RunExtensionTest("native_messaging_connect")) << message_;
 }
 
-IN_PROC_BROWSER_TEST_P(NativeMessagingApiTest,
+IN_PROC_BROWSER_TEST_F(NativeMessagingApiTest,
                        UserLevelNativeMessagingConnectNative) {
   ASSERT_NO_FATAL_FAILURE(test_host_.RegisterTestHost(true));
-  ASSERT_TRUE(RunTest("native_messaging_connect")) << message_;
+  ASSERT_TRUE(RunExtensionTest("native_messaging_connect")) << message_;
 }
 
 #if !BUILDFLAG(IS_CHROMEOS)
@@ -205,7 +172,7 @@ base::CommandLine CreateNativeMessagingConnectCommandLine(
   return command_line;
 }
 
-class NativeMessagingLaunchApiTest : public NativeMessagingApiTestBase {
+class NativeMessagingLaunchApiTest : public NativeMessagingApiTest {
  public:
   NativeMessagingLaunchApiTest() {
     feature_list_.InitAndEnableFeature(features::kOnConnectNative);
@@ -495,7 +462,7 @@ IN_PROC_BROWSER_TEST_F(NativeMessagingLaunchBackgroundModeApiTest,
 
 #if BUILDFLAG(IS_WIN)
 class NativeHostExecutablesLaunchDirectlyPolicyTest
-    : public extensions::NativeMessagingApiTestBase,
+    : public extensions::NativeMessagingApiTest,
       public testing::WithParamInterface<bool> {
  public:
   NativeHostExecutablesLaunchDirectlyPolicyTest() {
