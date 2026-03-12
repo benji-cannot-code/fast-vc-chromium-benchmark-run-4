59d7645f33d5f8eb4b5595e3d40058115104f6e9
Devlin Cronin <rdevlin.cronin@chromium.org>
2026-03-11 20:21:31 -0700

    [Extensions] Update permissions API tests to MV3
    MV2 has been unsupported for several months. We can remove testing
    support for it. Existing tests should be updated to MV3.
    
    Update API tests for the permissions API to use manifest version 3
    instead of manifest version 2 and 3.
    
    With the transition to MV3, many of these tests may also be able to be
    updated to use new patterns, such as async / await, service worker
    modules, and more. However, those changes are out of scope for these
    CLs, and can be addressed in followups.
    
    Additional Notes:
    * PermissionsApiTestWithContextType is now no longer needed. Remove it.
    * A few tests still use MV2 extensions because they rely on certain
      functionality (typically, access to a DOM or sharing a thread with
      other contexts). In this case, notes have been added above the
      associated test.
    * XHR calls (which are unavailable in SWs) in tests have been updated
      to use fetch().
    
    Bug: 491516661
    Change-Id: If9daa1097f0c457acb95b8a8adbabb0b21cea7ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7659164
    Reviewed-by: Tim <tjudkins@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1598181}

--- chrome/browser/extensions/api/permissions/permissions_apitest.cc
+++ chrome/browser/extensions/api/permissions/permissions_apitest.cc
@@ -43,8 +43,6 @@ static void AddPattern(URLPatternSet* extent, const std::string& pattern) {
 
 }  // namespace
 
-using ContextType = extensions::browser_test_util::ContextType;
-
 class ExperimentalApiTest : public ExtensionApiTest {
  public:
   ExperimentalApiTest() = default;
@@ -61,8 +59,7 @@ class ExperimentalApiTest : public ExtensionApiTest {
 class PermissionsApiTest : public ExtensionApiTest {
  public:
  public:
-  explicit PermissionsApiTest(ContextType context_type = ContextType::kNone)
-      : ExtensionApiTest(context_type) {}
+  PermissionsApiTest() = default;
   ~PermissionsApiTest() override = default;
   PermissionsApiTest(const PermissionsApiTest&) = delete;
   PermissionsApiTest& operator=(const PermissionsApiTest&) = delete;
@@ -73,19 +70,7 @@ class PermissionsApiTest : public ExtensionApiTest {
   }
 };
 
-class PermissionsApiTestWithContextType
-    : public PermissionsApiTest,
-      public testing::WithParamInterface<ContextType> {
- public:
-  PermissionsApiTestWithContextType() : PermissionsApiTest(GetParam()) {}
-  ~PermissionsApiTestWithContextType() override = default;
-  PermissionsApiTestWithContextType(const PermissionsApiTestWithContextType&) =
-      delete;
-  PermissionsApiTestWithContextType& operator=(
-      const PermissionsApiTestWithContextType&) = delete;
-};
-
-IN_PROC_BROWSER_TEST_P(PermissionsApiTestWithContextType, PermissionsFail) {
+IN_PROC_BROWSER_TEST_F(PermissionsApiTest, PermissionsFail) {
   ASSERT_TRUE(RunExtensionTest("permissions/disabled")) << message_;
 
   // Since the experimental APIs require a flag, this will fail even though
@@ -101,8 +86,7 @@ IN_PROC_BROWSER_TEST_F(ExperimentalApiTest, PermissionsSucceed) {
   ASSERT_TRUE(RunExtensionTest("permissions/enabled")) << message_;
 }
 
-IN_PROC_BROWSER_TEST_P(PermissionsApiTestWithContextType,
-                       ExperimentalPermissionsFail) {
+IN_PROC_BROWSER_TEST_F(PermissionsApiTest, ExperimentalPermissionsFail) {
   // At the time this test is being created, there is no experimental
   // function that will not be graduating soon, and does not require a
   // tab id as an argument.  So, we need the tab permission to get
@@ -133,8 +117,7 @@ IN_PROC_BROWSER_TEST_F(PermissionsApiTest, AlwaysAllowed) {
 #endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
 // Tests that the optional permissions API works correctly.
-IN_PROC_BROWSER_TEST_P(PermissionsApiTestWithContextType,
-                       OptionalPermissionsGranted) {
+IN_PROC_BROWSER_TEST_F(PermissionsApiTest, OptionalPermissionsGranted) {
   // Mark all the tested APIs as granted to bypass the confirmation UI.
   APIPermissionSet apis;
   apis.insert(extensions::mojom::APIPermissionID::kBookmark);
@@ -153,8 +136,7 @@ IN_PROC_BROWSER_TEST_P(PermissionsApiTestWithContextType,
 }
 
 // Tests that the optional permissions API works correctly.
-IN_PROC_BROWSER_TEST_P(PermissionsApiTestWithContextType,
-                       OptionalPermissionsAutoConfirm) {
+IN_PROC_BROWSER_TEST_F(PermissionsApiTest, OptionalPermissionsAutoConfirm) {
   // Rather than setting the granted permissions, set the UI autoconfirm flag
   // and run the same tests.
   auto dialog_action_reset =
@@ -188,8 +170,7 @@ IN_PROC_BROWSER_TEST_F(PermissionsApiTest, OptionalPermissionsDeny) {
 
 // Tests that the permissions.request function must be called from within a
 // user gesture.
-IN_PROC_BROWSER_TEST_P(PermissionsApiTestWithContextType,
-                       OptionalPermissionsGesture) {
+IN_PROC_BROWSER_TEST_F(PermissionsApiTest, OptionalPermissionsGesture) {
   PermissionsRequestFunction::SetIgnoreUserGestureForTests(false);
   ASSERT_TRUE(StartEmbeddedTestServer());
   EXPECT_TRUE(RunExtensionTest("permissions/optional_gesture")) << message_;
@@ -197,6 +178,10 @@ IN_PROC_BROWSER_TEST_P(PermissionsApiTestWithContextType,
 
 // Tests that the user gesture is retained in the permissions.request function
 // callback.
+// TODO(https://crbug.com/491516661): This explicitly uses an MV2 extension
+// because it "consumes" a user gesture from the background page via a
+// window.open() call; this doesn't have an analogous version in service
+// workers.
 IN_PROC_BROWSER_TEST_F(PermissionsApiTest, OptionalPermissionsRetainGesture) {
   auto dialog_action_reset =
       PermissionsRequestFunction::SetDialogActionForTests(
@@ -279,7 +264,7 @@ IN_PROC_BROWSER_TEST_F(PermissionsApiTest, FileLoad) {
 
 // Test requesting, querying, and removing host permissions for host
 // permissions that are a subset of the optional permissions.
-IN_PROC_BROWSER_TEST_P(PermissionsApiTestWithContextType, HostSubsets) {
+IN_PROC_BROWSER_TEST_F(PermissionsApiTest, HostSubsets) {
   auto dialog_action_reset =
       PermissionsRequestFunction::SetDialogActionForTests(
           PermissionsRequestFunction::DialogAction::kAutoConfirm);
@@ -294,21 +279,14 @@ IN_PROC_BROWSER_TEST_P(PermissionsApiTestWithContextType, HostSubsets) {
 // crbug.com/40394805, see details there for trickiness.
 // TODO(crbug.com/371432155): Port to desktop Android when chrome.tabs API
 // is available.
+// TODO(https://crbug.com/491516661): This uses an MV2 extension because it
+// involves a different page reaching into the background page to call a
+// function, which isn't directly supported in SWs.
 IN_PROC_BROWSER_TEST_F(PermissionsApiTest, OptionalPermissionsUpdatesBindings) {
   ASSERT_TRUE(RunExtensionTest("permissions/optional_updates_bindings"))
       << message_;
 }
 #endif  // BUILDFLAG(ENABLE_EXTENSIONS)
-
-// Desktop Android only supports manifest V3 / service worker.
-#if !BUILDFLAG(IS_ANDROID)
-INSTANTIATE_TEST_SUITE_P(PersistentBackground,
-                         PermissionsApiTestWithContextType,
-                         testing::Values(ContextType::kPersistentBackground));
-#endif
-INSTANTIATE_TEST_SUITE_P(ServiceWorker,
-                         PermissionsApiTestWithContextType,
-                         testing::Values(ContextType::kServiceWorker));
 
 class PermissionsApiHostAccessRequestsTest : public PermissionsApiTest {
  public:
