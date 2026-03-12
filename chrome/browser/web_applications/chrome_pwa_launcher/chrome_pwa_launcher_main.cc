4b30378256b4ba3afed754b8249478d33ffc7b49
David Bienvenu <davidbienvenu@chromium.org>
2026-03-11 19:11:26 -0700

    Remove pwa launch success metric
    Error rate is very low -
    https://uma.googleplex.com/p/chrome/timeline_v2?sid=33cbec8d95052cbf3043984d49f599f0.
    Regressions in PWA launching should be caught by normal release
    channels.
    
    Bug: 483056765
    Change-Id: I72b113beceb499f3fe62da462f73d01e2da0afbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7651679
    Reviewed-by: Jesse McKenna <jessemckenna@google.com>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1598149}

--- chrome/browser/web_applications/chrome_pwa_launcher/chrome_pwa_launcher_main.cc
+++ chrome/browser/web_applications/chrome_pwa_launcher/chrome_pwa_launcher_main.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <windows.h>
+
 #include "base/command_line.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
@@ -12,10 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process/launch.h"
 #include "base/win/windows_types.h"
 #include "chrome/browser/web_applications/chrome_pwa_launcher/last_browser_file_util.h"
-#include "chrome/browser/web_applications/chrome_pwa_launcher/launcher_log.h"
 #include "chrome/common/chrome_switches.h"
-#include "chrome/install_static/install_details.h"
-#include "chrome/install_static/product_install_details.h"
 #include "components/version_info/version_info_values.h"
 
 namespace {
@@ -44,8 +43,7 @@ base::FilePath GetChromePathFromLastBrowserFile() {
 
 // Launches |chrome_path| with the current command-line arguments, and returns
 // the launch result.
-web_app::WebAppLauncherLaunchResult LaunchPwa(
-    const base::FilePath& chrome_path) {
+int LaunchPwa(const base::FilePath& chrome_path) {
   // Launch chrome.exe, passing it all command-line arguments.
   base::CommandLine command_line(chrome_path);
   command_line.AppendArguments(*base::CommandLine::ForCurrentProcess(),
@@ -70,9 +68,7 @@ web_app::WebAppLauncherLaunchResult LaunchPwa(
   base::LaunchOptions launch_options;
   launch_options.current_directory = chrome_path.DirName();
   launch_options.grant_foreground_privilege = true;
-  return base::LaunchProcess(command_line, launch_options).IsValid()
-             ? web_app::WebAppLauncherLaunchResult::kSuccess
-             : web_app::WebAppLauncherLaunchResult::kError;
+  return base::LaunchProcess(command_line, launch_options).IsValid() ? 0 : 2;
 }
 
 }  // namespace
@@ -92,18 +88,6 @@ int WINAPI wWinMain(HINSTANCE instance,
   logging_settings.logging_dest = logging::LOG_TO_SYSTEM_DEBUG_LOG;
   logging::InitLogging(logging_settings);
 
-  // Use |chrome_path| to set InstallDetails for the process, which tells the
-  // LauncherLog which registry key to log the launch result to. No logging can
-  // take place until |chrome_path| is known, so errors before this point are
-  // not recorded.
   const base::FilePath chrome_path = GetChromePathFromLastBrowserFile();
-  install_static::InstallDetails::SetForProcess(
-      install_static::MakeProductDetails(chrome_path.value()));
-
-  web_app::LauncherLog launcher_log;
-  launcher_log.Log(web_app::WebAppLauncherLaunchResult::kStarted);
-
-  auto launch_result = LaunchPwa(chrome_path);
-  launcher_log.Log(launch_result);
-  return static_cast<int>(launch_result);
+  return LaunchPwa(chrome_path);
 }
