f4a3ce1929838bd4a97410e34c4815cd1f5752e7
Yasaman Sedaghat <yasamans@google.com>
2026-03-12 11:18:44 -0700

    [ios] Add a provider method to update floaty prompt
    This API allows chrome to update the prepopulated prompt via the edit
    menu entry point when floaty is in the collapsed state
    
    This is no op until the SDK and internal sides are updated, after which
    the todo in browser agent will be updated
    
    ios_internal cl:
    https://chrome-internal-review.git.corp.google.com/c/chrome/ios_internal/+/9095938
    
    Bug: 483004001
    Change-Id: Ibd5e6157bc46beeb274a68d2343d58f1b58de5ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7657429
    Reviewed-by: Ibrahim Kanouche <kanouche@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Yasaman Sedaghat <yasamans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1598563}

--- ios/chrome/test/providers/bwg/test_bwg.mm
+++ ios/chrome/test/providers/bwg/test_bwg.mm
@@ -59,6 +59,9 @@ void UpdateGeminiViewState(GeminiViewState view_state) {}
 
 void UpdateGeminiViewState(GeminiViewState view_state, bool animated) {}
 
+void UpdatePromptAction(gemini::EntryPoint entry_point,
+                        NSString* prepopulated_prompt) {}
+
 GeminiViewState GetCurrentGeminiViewState() {
   return GeminiViewState::kUnknown;
 }
