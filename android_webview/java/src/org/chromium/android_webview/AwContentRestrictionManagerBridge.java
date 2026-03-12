74e3d984a09307a35d5abc63cbbbd52d2c63640e
Vignesh Shenvi <vshenvi@google.com>
2026-03-11 18:39:44 -0700

    Add: JNI APIs for requesting content restriction classification
    This change adds new JNI APIs that will allow for requesting content
    restriction classification for WebViews. Corresponding C++ client
    consumer implementation will follow in a separate CL.
    
    Bug: b:481113570
    Change-Id: I646fdf94eb4d6241b5227940572cb7bcb2163751
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7650285
    Commit-Queue: Vignesh Shenvi <vshenvi@google.com>
    Reviewed-by: Luke Cao <caott@google.com>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1598138}

--- android_webview/java/src/org/chromium/android_webview/AwContentRestrictionManagerBridge.java
+++ android_webview/java/src/org/chromium/android_webview/AwContentRestrictionManagerBridge.java
@@ -4,15 +4,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 package org.chromium.android_webview;
 
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
+
 import org.jni_zero.CalledByNative;
 import org.jni_zero.JNINamespace;
+import org.jni_zero.JniType;
 
 import org.chromium.android_webview.common.AwFeatureMap;
 import org.chromium.android_webview.common.AwFeatures;
 import org.chromium.base.AconfigFlaggedApiDelegate;
+import org.chromium.base.ContextUtils;
+import org.chromium.base.JniOnceCallback;
 import org.chromium.base.Log;
+import org.chromium.base.Promise;
 import org.chromium.base.ServiceLoaderUtil;
 import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
 
 /**
  * JNI bridge for tapping into the ContentRestrictionManager system service in order to enforce
@@ -22,6 +30,13 @@ import org.chromium.build.annotations.NullMarked;
 @NullMarked
 public class AwContentRestrictionManagerBridge {
     private static final String TAG = "AwCRMBridge";
+
+    private static @Nullable Uri parseUrl(@Nullable String url) {
+        if (url == null) {
+            return null;
+        }
+        return Uri.parse(url);
+    }
 
     @CalledByNative
     public static boolean isContentRestrictionEnabled() {
@@ -38,5 +53,43 @@ public class AwContentRestrictionManagerBridge {
             return false;
         }
         return delegate.isContentRestrictionEnabled();
+    }
+
+    @CalledByNative
+    public static void requestContentClassification(
+            @JniType("std::string") String url,
+            @JniType("std::string") String mimeType,
+            JniOnceCallback<Boolean> callback) {
+        Uri uri = parseUrl(url);
+        if (uri == null) {
+            callback.onResult(false);
+            return;
+        }
+        AconfigFlaggedApiDelegate delegate =
+                ServiceLoaderUtil.maybeCreate(AconfigFlaggedApiDelegate.class);
+        if (delegate == null) {
+            Log.w(TAG, "Unable to retrieve the AconfigFlaggedApiDelegate instance.");
+            callback.onResult(false);
+            return;
+        }
+
+        // TODO(crbug.com/481113476): Also process the request body.
+        ParcelFileDescriptor requestBody = null;
+        Promise<Boolean> promise =
+                delegate.requestContentRestrictionClassification(
+                        uri,
+                        requestBody,
+                        mimeType,
+                        ContextUtils.getApplicationContext().getMainExecutor());
+        promise.then(
+                (Boolean isAllowed) -> {
+                    callback.onResult(isAllowed);
+                },
+                (@Nullable Exception error) -> {
+                    if (error != null) {
+                        Log.w(TAG, "Failed to classify content", error);
+                    }
+                    callback.onResult(false);
+                });
     }
 }
