5bb93464a09647d76dc92e92fe9b0bdc036d04a2
Kaylee Lubick <kjlubick@google.com>
2026-03-13 07:35:18 -0700

    Update uses of legacy Mock Ganesh API
    https://review.skia.org/1169976 adds this API to phase out
    the old one.
    
    Bug: b:293490566
    Change-Id: Ie219a185c8111f13c997508051cff61bbd5ecdad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7664720
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Auto-Submit: Kaylee Lubick <kjlubick@chromium.org>
    Commit-Queue: Kaylee Lubick <kjlubick@chromium.org>
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1599038}

--- components/viz/test/fake_skia_output_surface.cc
+++ components/viz/test/fake_skia_output_surface.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/gpu/ganesh/GrBackendSurface.h"
 #include "third_party/skia/include/gpu/ganesh/SkImageGanesh.h"
 #include "third_party/skia/include/gpu/ganesh/SkSurfaceGanesh.h"
+#include "third_party/skia/include/gpu/ganesh/mock/GrMockBackendSurface.h"
 #include "ui/gfx/gpu_fence_handle.h"
 #include "ui/gfx/presentation_feedback.h"
 #include "ui/gfx/swap_result.h"
@@ -341,9 +342,9 @@ bool FakeSkiaOutputSurface::GetGrBackendTexture(
   GrMockTextureInfo mock_texture_info = GrMockTextureInfo(
       SkColorTypeToGrColorType(ToClosestSkColorType(image_context.format())),
       SkTextureCompressionType::kNone, ++next_id);
-  *backend_texture = GrBackendTexture(image_context.size().width(),
-                                      image_context.size().height(),
-                                      skgpu::Mipmapped::kNo, mock_texture_info);
+  *backend_texture = GrBackendTextures::MakeMock(
+      image_context.size().width(), image_context.size().height(),
+      skgpu::Mipmapped::kNo, mock_texture_info);
   return true;
 }
 
