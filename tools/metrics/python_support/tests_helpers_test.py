9d160e6aafc2be345585800c5093a5f8b52e4d75
Greg Thompson <grt@chromium.org>
2026-03-13 08:32:19 -0700

    Fix invocation of python scripts from within python scripts
    vpython documentation says "When invoking a Python script from another
    Python script, use sys.executable at the beginning of invocation.", so
    update the test helpers to do so. This fixes a problem on Windows (for
    me, at least) whereby presubmits fail with file not found because
    "vpython3" is not an executable and subprocess.Popen is being used
    without shell=True.
    
    Bug: 492198566
    Change-Id: I56549fb9dbd14e5522e6ef9d9e1f6662d8bfa8b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7665883
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1599083}

--- tools/metrics/python_support/tests_helpers_test.py
+++ tools/metrics/python_support/tests_helpers_test.py
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 import os
+import sys
 import unittest
 from pathlib import Path
 from unittest.mock import patch, mock_open
@@ -35,7 +36,7 @@ class TestableScriptUtilTest(unittest.TestCase):
     self.assertEqual(script.file_path, script_path)
     self.assertEqual(
         script.cmd,
-        ["vpython3", str(script_path), "--flag1", "value1"])
+        [sys.executable, str(script_path), "--flag1", "value1"])
 
 
 class TestScanningTest(unittest.TestCase):
