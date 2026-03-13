3f1110bd3664dba19cef81f82d555a018e5db4cd
Erik Ovelius <ovelius@google.com>
2026-03-13 09:11:44 -0700

    Extract ConnectivityManagerDelegate to it's own file.
    This enforces the "one class one purpose" and allows this to be a first class abstraction around the ConnectivityManager.
    
    Bug: b:474048542
    Change-Id: I7635408da45923bae97930aabf0865f2f42322fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7647916
    Reviewed-by: Chidera Olibie <colibie@google.com>
    Commit-Queue: Erik Ovelius <ovelius@google.com>
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1599107}

--- net/android/java/src/org/chromium/net/NetworkChangeNotifier.java
+++ net/android/java/src/org/chromium/net/NetworkChangeNotifier.java
@@ -268,7 +268,7 @@ public class NetworkChangeNotifier {
                     // the experiment as its definitely a redundant call.
                     if (forceUpdateNetworkState) mAutoDetector.updateCurrentNetworkState();
 
-                    final NetworkChangeNotifierAutoDetect.NetworkState networkState =
+                    final ConnectivityManagerWrapper.NetworkState networkState =
                             mAutoDetector.getCurrentNetworkState();
 
                     updateCurrentConnectionType(networkState.getConnectionType());
