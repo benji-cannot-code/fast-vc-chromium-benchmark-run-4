e8221c53230de436c9f22960d43d532311bba5bf
Hubert Chao <hchao@chromium.org>
2026-03-13 12:05:35 -0700

    Revert^2 "[WebCrypto] add encapsulate/decapsulate operations"
    This reverts commit 912fc7f5c3324acce0d4f63058864cec2f74d196.
    
    Reland "[WebCrypto] add encapsulate/decapsulate operations"
    
    This reverts commit 912fc7f5c3324acce0d4f63058864cec2f74d196.
    
    Reason for revert: CL has nothing to do with the build failure for rust bindings of boringssl
    
    Original change's description:
    > Revert "[WebCrypto] add encapsulate/decapsulate operations"
    >
    > This reverts commit 8f133b7abf7ce464fff807380e1af96f25676b97.
    >
    > Reason for revert:
    > LUCI Bisection has identified this change as the culprit of a build failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/compile-analysis/b/8687431725366662625
    >
    > Sample failed build: https://ci.chromium.org/b/8687431725366662625
    >
    > If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F7609661&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Fcompile-analysis%2Fb%2F8687431725366662625&type=BUG
    >
    > Original change's description:
    > > [WebCrypto] add encapsulate/decapsulate operations
    > >
    > > Add the following functions to SubtleCrypto:
    > >
    > > * encapsulateKey
    > > * encapsulateBits
    > > * decapsulateKey
    > > * decapsulateBits
    > >
    > > Following the spec at
    > > https://wicg.github.io/webcrypto-modern-algos/#partial-subtlecrypto-interface.
    > >
    > > Functions are not implemented yet; this is just adding the
    > > boilerplate scaffolding needed to recognize these functions.
    > >
    > > These will be used in ML-KEM. Updated expectations here are because the error
    > > messages have changed.
    > >
    > > Bug: 450627019
    > > Change-Id: I4a61bc20d9bcdfa5d1d996b2df55ac264733e72c
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7609661
    > > Commit-Queue: Hubert Chao <hchao@chromium.org>
    > > Reviewed-by: Lily Chen <chlily@chromium.org>
    > > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1599077}
    > >
    >
    > Bug: 450627019
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: Idf591bbecad75d45292fc1e7a8acc98e0f8ca37d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7665332
    > Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    > Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    > Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1599112}
    
    Bug: 450627019
    Change-Id: I3f80266f2b37d459f5170333136220376ee42cdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7664839
    Owners-Override: David Baron <dbaron@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Hubert Chao <hchao@chromium.org>
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1599233}

--- third_party/blink/public/platform/web_crypto_algorithm.h
+++ third_party/blink/public/platform/web_crypto_algorithm.h
@@ -55,7 +55,11 @@ enum WebCryptoOperation {
   kWebCryptoOperationDeriveBits,
   kWebCryptoOperationWrapKey,
   kWebCryptoOperationUnwrapKey,
-  kWebCryptoOperationLast = kWebCryptoOperationUnwrapKey,
+  kWebCryptoOperationEncapsulateKey,
+  kWebCryptoOperationEncapsulateBits,
+  kWebCryptoOperationDecapsulateKey,
+  kWebCryptoOperationDecapsulateBits,
+  kWebCryptoOperationLast = kWebCryptoOperationDecapsulateBits,
 };
 
 enum WebCryptoAlgorithmId {
--- third_party/blink/renderer/modules/crypto/subtle_crypto.cc
+++ third_party/blink/renderer/modules/crypto/subtle_crypto.cc
@@ -41,6 +41,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/web/web_crypto_histograms.h"
 #include "third_party/blink/renderer/bindings/core/v8/dictionary.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_encapsulated_bits.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_encapsulated_key.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_json_web_key.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_union_arraybuffer_arraybufferview_jsonwebkey.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
@@ -812,6 +814,57 @@ ScriptPromise<IDLAny> SubtleCrypto::deriveKey(
       key_length_algorithm, extractable, key_usages, result->Result(),
       std::move(task_runner));
   return promise;
+}
+
+// TODO(crbug.com/450627019): implement encapsulate/decapsulate functions
+ScriptPromise<EncapsulatedKey> SubtleCrypto::encapsulateKey(
+    ScriptState* script_state,
+    const V8AlgorithmIdentifier* encapsulation_algorithm,
+    CryptoKey* encapsulation_key,
+    const V8AlgorithmIdentifier* shared_key_algorithm,
+    bool extractable,
+    const Vector<String>& raw_key_usages,
+    ExceptionState& exception_state) {
+  WebCryptoKeyUsageMask key_usages;
+  if (!CryptoKey::ParseUsageMask(raw_key_usages, key_usages, exception_state)) {
+    return EmptyPromise();
+  }
+
+  return EmptyPromise();
+}
+
+ScriptPromise<EncapsulatedBits> SubtleCrypto::encapsulateBits(
+    ScriptState* script_state,
+    const V8AlgorithmIdentifier* encapsulation_algorithm,
+    CryptoKey* encapsulation_key,
+    ExceptionState& exception_state) {
+  return EmptyPromise();
+}
+
+ScriptPromise<CryptoKey> SubtleCrypto::decapsulateKey(
+    ScriptState* script_state,
+    const V8AlgorithmIdentifier* decapsulation_algorithm,
+    CryptoKey* decapsulation_key,
+    const V8BufferSource* ciphertext,
+    const V8AlgorithmIdentifier* shared_key_algorithm,
+    bool extractable,
+    const Vector<String>& raw_key_usages,
+    ExceptionState& exception_state) {
+  WebCryptoKeyUsageMask key_usages;
+  if (!CryptoKey::ParseUsageMask(raw_key_usages, key_usages, exception_state)) {
+    return EmptyPromise();
+  }
+
+  return EmptyPromise();
+}
+
+ScriptPromise<DOMArrayBuffer> SubtleCrypto::decapsulateBits(
+    ScriptState* script_state,
+    const V8AlgorithmIdentifier* decapsulation_algorithm,
+    CryptoKey* decapsulation_key,
+    const V8BufferSource* ciphertext,
+    ExceptionState& exception_state) {
+  return EmptyPromise();
 }
 
 }  // namespace blink
