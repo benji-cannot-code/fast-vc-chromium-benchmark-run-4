329e6bb78ab4340ccb73c63952ad920c67ac521d
Ramin Halavati <rhalavati@chromium.org>
2026-03-13 06:43:30 -0700

    android-a11y: Update AccessibilityNodeInfo#textSelectable property.
    Per API definition, `textSelectable` property should specify which
    non-editable text node can be used for text selection.
    To comply with that and supported node types in extended selection API,
    update the code to set `textSelectable`.
    
    AX-Relnotes: n/a
    Bug: 443078007
    Change-Id: Iae74bbe6c5dbc8fadda8ea6211b10eaa502e6d03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7648350
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Auto-Submit: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1599022}

--- content/public/android/java/src/org/chromium/content/browser/accessibility/AccessibilityNodeInfoBuilder.java
+++ content/public/android/java/src/org/chromium/content/browser/accessibility/AccessibilityNodeInfoBuilder.java
@@ -31,6 +31,7 @@ import static androidx.core.view.accessibility.AccessibilityNodeInfoCompat.Acces
 import static androidx.core.view.accessibility.AccessibilityNodeInfoCompat.AccessibilityActionCompat.ACTION_SCROLL_LEFT;
 import static androidx.core.view.accessibility.AccessibilityNodeInfoCompat.AccessibilityActionCompat.ACTION_SCROLL_RIGHT;
 import static androidx.core.view.accessibility.AccessibilityNodeInfoCompat.AccessibilityActionCompat.ACTION_SCROLL_UP;
+import static androidx.core.view.accessibility.AccessibilityNodeInfoCompat.AccessibilityActionCompat.ACTION_SET_EXTENDED_SELECTION;
 import static androidx.core.view.accessibility.AccessibilityNodeInfoCompat.AccessibilityActionCompat.ACTION_SET_PROGRESS;
 import static androidx.core.view.accessibility.AccessibilityNodeInfoCompat.AccessibilityActionCompat.ACTION_SET_SELECTION;
 import static androidx.core.view.accessibility.AccessibilityNodeInfoCompat.AccessibilityActionCompat.ACTION_SET_TEXT;
@@ -207,6 +208,7 @@ public class AccessibilityNodeInfoBuilder {
             boolean password,
             boolean scrollable,
             boolean selected,
+            boolean textSelectable,
             boolean visibleToUser,
             boolean hasCharacterLocations,
             boolean isRequired,
@@ -225,6 +227,11 @@ public class AccessibilityNodeInfoBuilder {
         node.setFieldRequired(isRequired);
         node.setContentInvalid(contentInvalid);
         node.setHeading(isHeading);
+
+        if (ContentFeatureMap.isEnabled(
+                ContentFeatureList.ACCESSIBILITY_SET_SELECTABLE_ON_ALL_NODES_WITH_TEXT)) {
+            node.setTextSelectable(textSelectable);
+        }
 
         List<String> availableExtraData = new ArrayList<>();
         if (hasImage) {
@@ -258,6 +265,7 @@ public class AccessibilityNodeInfoBuilder {
     private void addAccessibilityNodeInfoActions(
             AccessibilityNodeInfoCompat node,
             int virtualViewId,
+            boolean canSetExtendedSelection,
             boolean canScrollForward,
             boolean canScrollBackward,
             boolean canScrollUp,
@@ -299,6 +307,9 @@ public class AccessibilityNodeInfoBuilder {
                 }
                 node.addAction(ACTION_COPY);
             }
+        }
+        if (canSetExtendedSelection) {
+            node.addAction(ACTION_SET_EXTENDED_SELECTION);
         }
 
         if (canScrollForward) {
@@ -504,18 +515,6 @@ public class AccessibilityNodeInfoBuilder {
             node.setContentDescription(computedText);
         } else {
             node.setText(computedText);
-
-            // Though actions are generally set elsewhere, we make an exception here in order to
-            // stay consistent with when we supply `text` on a node. In these cases, we can
-            // confidently state there is text selection available via
-            // WebContentsAccessibilityAndroid::SetSelection.
-            if (computedText.length() > 0
-                    && ContentFeatureMap.isEnabled(
-                            ContentFeatureList
-                                    .ACCESSIBILITY_SET_SELECTABLE_ON_ALL_NODES_WITH_TEXT)) {
-                node.addAction(ACTION_SET_SELECTION);
-                node.setTextSelectable(true);
-            }
         }
 
         recordTimeToCreateSpannables(now);
@@ -585,18 +584,6 @@ public class AccessibilityNodeInfoBuilder {
             node.setContentDescription(computedText);
         } else {
             node.setText(computedText);
-
-            // Though actions are generally set elsewhere, we make an exception here in order to
-            // stay consistent with when we supply `text` on a node. In these cases, we can
-            // confidently state there is text selection available via
-            // WebContentsAccessibilityAndroid::SetSelection.
-            if (computedText.length() > 0
-                    && ContentFeatureMap.isEnabled(
-                            ContentFeatureList
-                                    .ACCESSIBILITY_SET_SELECTABLE_ON_ALL_NODES_WITH_TEXT)) {
-                node.addAction(ACTION_SET_SELECTION);
-                node.setTextSelectable(true);
-            }
         }
 
         recordTimeToCreateSpannables(now);
