629b5e57e2628ecee27131e38c9bf96c9ede9e51
David Baron <dbaron@chromium.org>
2026-03-13 13:32:19 -0700

    Revert "[X11] Make IsSelectionOwner asynchronous in PlatformClipboard"
    This reverts commit 8dc861921d9d2891e2e2c58f9f9404a59416d2aa.
    
    Reason for revert: likely cause of consistent failure of
    OmniboxViewTest.Paste on linux64-rel-ready starting from
    https://ci.chromium.org/ui/b/8687423217582013825
    
    The first (of multiple) failures in that test was:
    
    ../../chrome/browser/ui/omnibox/omnibox_view_browsertest.cc:1374: Failure
    Expected equality of these values:
      kSearchText
        Which is: u"abc"
      omnibox_view->GetText()
        Which is: u"about:blank"
    
    Original change's description:
    > [X11] Make IsSelectionOwner asynchronous in PlatformClipboard
    >
    > This change refactors the IsSelectionOwner method in the
    > PlatformClipboard interface and its implementations to be asynchronous.
    > This allows the X11 backend to eliminate a synchronous X11 Sync() call
    > in GetSelectionOwner, furthering the goal of an entirely asynchronous
    > clipboard architecture.
    >
    > R=thestig
    >
    > Change-Id: Ic616ebb106d3fcb36a83a47c70da6742887a70d3
    > Bug: 40398800
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7659748
    > Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    > Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    > Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1599158}
    
    Bug: 40398800, 492462319
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 40398800
    Change-Id: I75d81ac546c7ae6798fb57eb3c2226709f68f6b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7663950
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: David Baron <dbaron@chromium.org>
    Auto-Submit: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1599287}

--- ui/base/x/x11_clipboard_helper.cc
+++ ui/base/x/x11_clipboard_helper.cc
@@ -99,17 +99,9 @@ void SelectionChangeObserver::OnEvent(const x11::Event& xev) {
   }
 }
 
-void GetSelectionOwner(x11::Atom selection,
-                       base::OnceCallback<void(x11::Window)> callback) {
-  x11::Connection::Get()
-      ->GetSelectionOwner({selection})
-      .OnResponse(base::BindOnce(
-          [](base::OnceCallback<void(x11::Window)> callback,
-             x11::GetSelectionOwnerResponse response) {
-            std::move(callback).Run(response ? response->owner
-                                             : x11::Window::None);
-          },
-          std::move(callback)));
+x11::Window GetSelectionOwner(x11::Atom selection) {
+  auto response = x11::Connection::Get()->GetSelectionOwner({selection}).Sync();
+  return response ? response->owner : x11::Window::None;
 }
 
 }  // namespace
@@ -193,18 +185,7 @@ void XClipboardHelper::ReadAsync(
     const std::vector<x11::Atom>& types,
     base::OnceCallback<void(SelectionData)> callback) {
   x11::Atom selection_name = LookupSelectionForClipboardBuffer(buffer);
-  GetSelectionOwner(selection_name,
-                    base::BindOnce(&XClipboardHelper::OnReadAsync, GetWeakPtr(),
-                                   buffer, types, std::move(callback)));
-}
-
-void XClipboardHelper::OnReadAsync(
-    ClipboardBuffer buffer,
-    const std::vector<x11::Atom>& types,
-    base::OnceCallback<void(SelectionData)> callback,
-    x11::Window owner) {
-  x11::Atom selection_name = LookupSelectionForClipboardBuffer(buffer);
-  if (owner == x_window_) {
+  if (GetSelectionOwner(selection_name) == x_window_) {
     // We can local fastpath instead of playing the nested run loop game
     // with the X server.
     const SelectionFormatMap& format_map = LookupStorageForAtom(selection_name);
@@ -329,16 +310,9 @@ void XClipboardHelper::IsFormatAvailableAsync(
           format, std::move(callback)));
 }
 
-void XClipboardHelper::IsSelectionOwnerAsync(
-    ClipboardBuffer buffer,
-    base::OnceCallback<void(bool)> callback) const {
+bool XClipboardHelper::IsSelectionOwner(ClipboardBuffer buffer) const {
   x11::Atom selection = LookupSelectionForClipboardBuffer(buffer);
-  GetSelectionOwner(selection, base::BindOnce(
-                                   [](base::OnceCallback<void(bool)> callback,
-                                      x11::Window x_window, x11::Window owner) {
-                                     std::move(callback).Run(owner == x_window);
-                                   },
-                                   std::move(callback), x_window_));
+  return GetSelectionOwner(selection) == x_window_;
 }
 
 std::vector<x11::Atom> XClipboardHelper::GetTextAtoms() const {
@@ -362,17 +336,7 @@ void XClipboardHelper::GetTargetListAsync(
     ClipboardBuffer buffer,
     base::OnceCallback<void(TargetList)> callback) {
   x11::Atom selection_name = LookupSelectionForClipboardBuffer(buffer);
-  GetSelectionOwner(selection_name,
-                    base::BindOnce(&XClipboardHelper::OnGetTargetList,
-                                   GetWeakPtr(), buffer, std::move(callback)));
-}
-
-void XClipboardHelper::OnGetTargetList(
-    ClipboardBuffer buffer,
-    base::OnceCallback<void(TargetList)> callback,
-    x11::Window owner) {
-  x11::Atom selection_name = LookupSelectionForClipboardBuffer(buffer);
-  if (owner == x_window_) {
+  if (GetSelectionOwner(selection_name) == x_window_) {
     // We can local fastpath and return the list of local targets.
     const SelectionFormatMap& format_map = LookupStorageForAtom(selection_name);
     std::vector<x11::Atom> out;
