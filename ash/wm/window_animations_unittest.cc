72c79df20bfe4264adb17d19b1e2d75e579f34db
hiroki oshima <hiroki.oshima@gmail.com>
2026-03-13 01:08:16 -0700

    fix MinimizeAnimationObserver dangling pointer
    Bug: 40212619
    Change-Id: Ie12e42b8fad50095fcca4928875b7d5555f93653
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7645908
    Reviewed-by: Di Wu <diwux@google.com>
    Commit-Queue: Di Wu <diwux@google.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1598918}

--- ash/wm/window_animations_unittest.cc
+++ ash/wm/window_animations_unittest.cc
@@ -76,6 +76,8 @@ class MinimizeAnimationObserver : public ui::LayerAnimationObserver {
   MinimizeAnimationObserver& operator=(const MinimizeAnimationObserver&) =
       delete;
 
+  ~MinimizeAnimationObserver() override { animator_ = nullptr; }
+
   base::TimeDelta duration() { return duration_; }
 
  protected:
@@ -84,12 +86,13 @@ class MinimizeAnimationObserver : public ui::LayerAnimationObserver {
       ui::LayerAnimationSequence* sequence) override {
     duration_ = animator_->GetTransitionDuration();
     animator_->RemoveObserver(this);
+    animator_ = nullptr;
   }
   void OnLayerAnimationEnded(ui::LayerAnimationSequence* sequence) override {}
   void OnLayerAnimationAborted(ui::LayerAnimationSequence* sequence) override {}
 
  private:
-  raw_ptr<ui::LayerAnimator, DanglingUntriaged> animator_;
+  raw_ptr<ui::LayerAnimator> animator_;
   base::TimeDelta duration_;
 };
 
