93bfd4f4b5be8c37cb5cb860d05fd736f2acf933
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2026-03-13 11:30:46 -0700

    webstore_private: use blocked_message from EIPS
    ExtensionInstallPolicyService::CanInstall() returns a localized message
    for displaying to the user. Use that message for
    ShowBlockedByPolicyDialog().
    
    Bug: 477545527
    Change-Id: I44ede6b814a245939e9078bdd741cbf36a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7624660
    Reviewed-by: Tim <tjudkins@chromium.org>
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1599201}

--- chrome/browser/extensions/api/webstore_private/extension_install_status.h
+++ chrome/browser/extensions/api/webstore_private/extension_install_status.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_EXTENSIONS_API_WEBSTORE_PRIVATE_EXTENSION_INSTALL_STATUS_H_
 #define CHROME_BROWSER_EXTENSIONS_API_WEBSTORE_PRIVATE_EXTENSION_INSTALL_STATUS_H_
 
+#include <string>
+
 #include "base/version.h"
 #include "extensions/buildflags/buildflags.h"
 #include "extensions/common/extension_id.h"
@@ -75,7 +77,8 @@ void GetWebstoreExtensionInstallStatus(
     const Manifest::Type manifest_type,
     const PermissionSet& required_permission_set,
     int manifest_version,
-    base::OnceCallback<void(ExtensionInstallStatus)> callback);
+    base::OnceCallback<void(ExtensionInstallStatus,
+                            std::u16string blocked_message)> callback);
 
 }  // namespace extensions
 
