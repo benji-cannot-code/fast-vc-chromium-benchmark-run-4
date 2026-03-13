7c5b257fa8c84b5055ce3ef7ebfdfed7fb84a4ff
Devlin Cronin <rdevlin.cronin@chromium.org>
2026-03-12 18:21:46 -0700

    [Extensions Cleanup] Simple JS updates in active_tab_file_urls
    This CL updates extension JS files in api_test/active_tab_file_urls to
    * use `let` and `const` instead of `var`
    * update variable naming patterns (avoiding underscores, properly
      naming consts)
    * prefer single quotes instead of double quotes and use template
      literals when appropriate
    
    Bug: 492151886
    Change-Id: I47511b6475c11c919bb9bbca7ee9a52c7713167c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7664320
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1598810}

--- chrome/test/data/extensions/api_test/active_tab_file_urls/window_onload.js
+++ chrome/test/data/extensions/api_test/active_tab_file_urls/window_onload.js
@@ -1,12 +1,16 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 window.onload = function() {
-  var iframe = document.getElementById('file_iframe');
+  const iframe = document.getElementById('file_iframe');
   try {
-    var url = iframe.contentWindow.location.href
+    const url = iframe.contentWindow.location.href
     if (url === 'about:blank')
       chrome.test.sendMessage('denied');
   } catch (e) {
-    var expectedError =
+    const expectedError =
         `Failed to read a named property 'href' from 'Location': Blocked a ` +
         `frame with origin "${window.location.origin}" from ` +
         `accessing a cross-origin frame.`;
