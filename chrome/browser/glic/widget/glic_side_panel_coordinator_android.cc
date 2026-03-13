ae0f303c3278118386b5e69f150d9b79cba6b96e
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2026-03-12 22:02:46 -0700

    Revert "[glic] Clear WebContents in Android CoBrowseViews"
    This reverts commit 30e510ea3d9deb693ff2d728d61f0cea016fd119.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/4898485544419328
    
    Sample build with failed test: https://ci.chromium.org/b/8687508235989924065
    Affected test(s):
    [://chrome/test\:android_browsertests!gtest::GlicInstanceCoordinatorBrowserTest#CloseHidesInstance](https://ci.chromium.org/ui/test/chromium/:%2F%2Fchrome%2Ftest%5C:android_browsertests%21gtest::GlicInstanceCoordinatorBrowserTest%23CloseHidesInstance?q=VHash%3Ae59f0f7e0ddff848)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F4898485544419328&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F7658909&type=BUG
    
    Original change's description:
    > [glic] Clear WebContents in Android CoBrowseViews
    >
    > When the GlicSidePanelCoordinatorAndroid is closed or destroyed, the
    > associated WebContents may be destroyed before the Java UI is fully
    > dismantled. This adds a JNI method to CoBrowseViews to explicitly clear
    > the WebContents reference, preventing dangling pointers.
    >
    > Bug: b:491864717
    > Change-Id: Iaf1716fdfec54d01e6a392fc506876f66a6a6964
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7658909
    > Reviewed-by: Hitarth Kothari <hitarthkothari@google.com>
    > Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1598568}
    >
    
    Bug: b:491864717
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I863915b59f0ac67b7a8b9953afcbe16140c59019
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7663038
    Commit-Queue: Anna Sato <annasato@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Anna Sato <annasato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1598878}

--- chrome/browser/glic/widget/glic_side_panel_coordinator_android.cc
+++ chrome/browser/glic/widget/glic_side_panel_coordinator_android.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/rand_util.h"
 #include "chrome/browser/android/tab_android.h"
 #include "chrome/browser/context_sharing/tab_bottom_sheet/android/jni_headers/CoBrowseViewFactory_jni.h"
-#include "chrome/browser/context_sharing/tab_bottom_sheet/android/jni_headers/CoBrowseViews_jni.h"
 #include "chrome/browser/context_sharing/tab_bottom_sheet/android/jni_headers/TabBottomSheetNativeInterface_jni.h"
 #include "components/tabs/public/tab_interface.h"
 #include "content/public/browser/web_contents.h"
@@ -39,10 +38,6 @@ GlicSidePanelCoordinatorAndroid::GlicSidePanelCoordinatorAndroid(
 }
 
 GlicSidePanelCoordinatorAndroid::~GlicSidePanelCoordinatorAndroid() {
-  if (co_browse_views_) {
-    Java_CoBrowseViews_setWebContents(AttachCurrentThread(), co_browse_views_,
-                                      nullptr);
-  }
   Java_TabBottomSheetNativeInterface_destroy(AttachCurrentThread(),
                                              java_interface_);
 }
@@ -96,13 +91,9 @@ void GlicSidePanelCoordinatorAndroid::Close(const CloseOptions& options) {
     return;
   }
 
-  if (co_browse_views_) {
-    Java_CoBrowseViews_setWebContents(AttachCurrentThread(), co_browse_views_,
-                                      nullptr);
-  }
-
   Java_TabBottomSheetNativeInterface_close(AttachCurrentThread(),
                                            java_interface_);
+  SetState(State::kClosed);
 }
 
 bool GlicSidePanelCoordinatorAndroid::IsShowing() const {
