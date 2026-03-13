1c5e40a1ea906dc3ffa728c5f592f4fcc896c4d6
Tanmoy Mollik <triploblastic@google.com>
2026-03-13 07:47:38 -0700

    [Android] Fulfill native initialization promise after flow is known
    Native initialization promise can be fulfilled before flow is known in
    FirstRunActivity. That's because these are asynchronous parallel flows.
    This cl adds a check in onInternalStateChanged() to check that both
    flows have finished before fulfilling the promise.
    
    Bug: 489986312
    Change-Id: I5c5002fdbb61af0f95d2cf21d33d71042f51e281
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7651934
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1599044}

--- chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunActivity.java
@@ -213,6 +213,7 @@ public class FirstRunActivity extends FirstRunActivityBase implements FirstRunPa
     private boolean mLaunchedFromChromeIcon;
 
     private boolean mLaunchedFromCct;
+    private boolean mTemplateUrlServiceLoaded;
 
     /**
      * {@link SystemClock} timestamp from when the FRE intent was initially created. This marks when
@@ -448,14 +449,15 @@ public class FirstRunActivity extends FirstRunActivityBase implements FirstRunPa
     public void finishNativeInitialization() {
         super.finishNativeInitialization();
 
-        Runnable onNativeFinished =
+        Runnable onTemplateUrlServiceLoaded =
                 () -> {
                     if (isActivityFinishingOrDestroyed()) return;
 
+                    mTemplateUrlServiceLoaded = true;
                     onNativeDependenciesFullyInitialized();
                 };
         Profile profile = assumeNonNull(getProfileProviderSupplier().get()).getOriginalProfile();
-        TemplateUrlServiceFactory.getForProfile(profile).runWhenLoaded(onNativeFinished);
+        TemplateUrlServiceFactory.getForProfile(profile).runWhenLoaded(onTemplateUrlServiceLoaded);
         // Notify feature engagement that FRE occurred.
         TrackerFactory.getTrackerForProfile(profile)
                 .notifyEvent(EventConstants.RESTORE_TABS_ON_FIRST_RUN_SHOW_PROMO);
@@ -476,9 +478,7 @@ public class FirstRunActivity extends FirstRunActivityBase implements FirstRunPa
     }
 
     private void onNativeDependenciesFullyInitialized() {
-        mNativeInitializationPromise.fulfill(null);
         mPager.setOffscreenPageLimit(ViewPager2.OFFSCREEN_PAGE_LIMIT_DEFAULT);
-
         onInternalStateChanged();
     }
 
@@ -495,6 +495,11 @@ public class FirstRunActivity extends FirstRunActivityBase implements FirstRunPa
     private void onInternalStateChanged() {
         if (!isFlowKnown()) {
             return;
+        }
+
+        if (mTemplateUrlServiceLoaded && mNativeInitializationPromise.isPending()) {
+            // `mNativeInitializationPromise` should only be set after flow is known.
+            mNativeInitializationPromise.fulfill(null);
         }
 
         if (mPagerAdapter == null) {
