d543f3c07a381efe4f804efe9f02602942601acf
Jacobo Tapia <jatapiaro@google.com>
2026-03-13 11:10:25 -0700

    [Immersive Reading Mode]: Fix visible but inactive links
    This CL fixes a race condition that occurs when AXTree updates are
    received before Readability finishes its distillation.
    
    Previously, we only processed AXTree updates if the
    should_extract_anchors_from_tree_for_readability property in
    ReadAnythingModel was set to true. This property was only enabled upon
    receiving the Readability content. If AXTree updates arrived first, they
    were essentially discarded. Consequently, by the time we attempted to
    process the anchors, the necessary data from the AXTree was missing,
    leaving the links visible but inactive.
    
    Fixed: 491205174
    Change-Id: I3192700547b0b7dd7244f678c11fea4f6b705ae9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7651741
    Commit-Queue: Jacobo Tapia <jatapiaro@google.com>
    Reviewed-by: Eitan Goldberger <eitang@google.com>
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1599182}

--- chrome/renderer/accessibility/read_anything/read_anything_app_model.h
+++ chrome/renderer/accessibility/read_anything/read_anything_app_model.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list_types.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
+#include "chrome/common/read_anything/read_anything.mojom-shared.h"
 #include "chrome/common/read_anything/read_anything.mojom.h"
 #include "chrome/common/read_anything/read_anything_util.h"
 #include "services/metrics/public/cpp/ukm_source_id.h"
@@ -267,6 +268,32 @@ class ReadAnythingAppModel {
   }
   bool is_readability_next_distillation_method() const {
     return next_distillation_method() == DistillationMethod::kReadability;
+  }
+  bool is_readability_current_distillation_method() const {
+    return current_content_distillation_method_ ==
+           DistillationMethod::kReadability;
+  }
+  bool should_apply_accessibility_updates_for_readability() const {
+    if (!features::IsReadAnythingWithReadabilityEnabled() &&
+        !features::IsReadAnythingWithReadabilityAllowLinksEnabled()) {
+      return false;
+    }
+
+    if (is_readability_next_distillation_method() &&
+        distillation_state_ ==
+            read_anything::mojom::ReadAnythingDistillationState::
+                kDistillationInProgress) {
+      return true;
+    }
+
+    if (is_readability_current_distillation_method() &&
+        distillation_state_ ==
+            read_anything::mojom::ReadAnythingDistillationState::
+                kDistillationWithContent) {
+      return true;
+    }
+
+    return false;
   }
 
   read_anything::mojom::LetterSpacing letter_spacing() const {
