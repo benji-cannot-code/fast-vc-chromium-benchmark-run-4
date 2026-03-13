927038631bf6fb88f163f2b82d99a69f95acdc4e
Colin Blundell <blundell@chromium.org>
2026-03-13 02:27:13 -0700

    [//gpu] Move CopySharedImageToGLTextureViaTextureCopy to GLES2Interface
    Also duplicates this helper [1].
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:media/renderers/paint_canvas_video_renderer.cc;l=258-269?q=paint_canvas_video&ss=chromium
    
    Bug: 343011436
    Change-Id: I3dc6d3353428c7c789e5286c5bff0244392cf3e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7657508
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1598946}

--- gpu/command_buffer/client/gles2_interface.h
+++ gpu/command_buffer/client/gles2_interface.h
@@ -12,6 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "gpu/command_buffer/client/interface_base.h"
+#include "gpu/command_buffer/common/sync_token.h"
+#include "third_party/skia/include/core/SkAlphaType.h"
+#include "third_party/skia/include/gpu/ganesh/GrTypes.h"
 
 namespace cc {
 class ClientTransferCacheEntry;
@@ -21,6 +24,7 @@ class ImageProvider;
 
 namespace gfx {
 class Rect;
+class Size;
 class Vector2d;
 class Vector2dF;
 }  // namespace gfx
@@ -48,6 +52,22 @@ class GLES2Interface : public InterfaceBase {
   // via CopyTexture().
   virtual bool CanCopySharedImageToGLTextureViaTextureCopy(
       ClientSharedImage* shared_image);
+
+  // Copies the contents of |source_shared_image| to |texture| of the current
+  // context.
+  virtual gpu::SyncToken CopySharedImageToGLTextureViaTextureCopy(
+      const gfx::Size& coded_size,
+      const gfx::Rect& visible_rect,
+      ClientSharedImage* source_shared_image,
+      const gpu::SyncToken& source_sync_token,
+      uint32_t target,
+      uint32_t texture,
+      uint32_t internal_format,
+      uint32_t format,
+      uint32_t type,
+      int32_t level,
+      SkAlphaType dst_alpha_type,
+      GrSurfaceOrigin dst_origin);
 
   virtual void FreeSharedMemory(void*) {}
 
