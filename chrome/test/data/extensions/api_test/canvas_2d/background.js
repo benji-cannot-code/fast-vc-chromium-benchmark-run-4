420be2676a7f06f00415ae0207aa815f43b60684
Devlin Cronin <rdevlin.cronin@chromium.org>
2026-03-13 13:51:22 -0700

    [Extensions Cleanup] Simple JS updates in canvas_2d
    This CL updates extension JS files in api_test/canvas_2d to
    * use `let` and `const` instead of `var`
    * update variable naming patterns (avoiding underscores, properly
      naming consts)
    * prefer single quotes instead of double quotes and use template
      literals when appropriate
    
    Bug: 492151886
    Change-Id: I9ba5ace07cc9d7e180a4ea986503c6d244c1e0af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7664997
    Reviewed-by: Tim <tjudkins@chromium.org>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1599298}

--- chrome/test/data/extensions/api_test/canvas_2d/background.js
+++ chrome/test/data/extensions/api_test/canvas_2d/background.js
@@ -3,20 +3,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-canvas = document.getElementById("my-canvas");
+const canvas = document.getElementById('my-canvas');
 if (canvas) {
   if (canvas.getContext) {
-    context = canvas.getContext("2d");
+    const context = canvas.getContext('2d');
     if (context) {
       context.fillStyle = 'red';
       context.fillRect(20, 20, 40, 40);
       chrome.test.notifyPass();
     } else {
-      chrome.test.notifyFail("unable to getContext('2d')");
+      chrome.test.notifyFail(`unable to getContext('2d')`);
     }
   } else {
-    chrome.test.notifyFail("canvas.getContext null");
+    chrome.test.notifyFail('canvas.getContext null');
   }
 } else {
-  chrome.test.notifyFail("couldn't find element my-canvas");
+  chrome.test.notifyFail(`couldn't find element my-canvas`);
 }
