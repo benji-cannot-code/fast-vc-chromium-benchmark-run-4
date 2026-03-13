f4263c380e09b3adc70c10a6508bae76c945e4dd
Fiona Macintosh <fmacintosh@google.com>
2026-03-13 14:47:46 -0700

    Clean up remaining obsolete TrackingProtection metrics
    Bug: 482352097
    Change-Id: I73d6079bb1d3d6f3ea20be9a586f8454c4c98aaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7663658
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Fiona Macintosh <fmacintosh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1599329}

--- chrome/browser/feedback/public/feedback_source.h
+++ chrome/browser/feedback/public/feedback_source.h
@@ -63,7 +63,7 @@ enum FeedbackSource {
   kFeedbackSourceBorealis = 43,
   kFeedbackSourceSunfish = 44,
   kFeedbackSourceBocaApp = 45,
-  kFeedbackSourceTrackingProtections = 46,
+  // kFeedbackSourceTrackingProtections = 46, Obsolete
   kFeedbackSourceSplitView = 47,
   kFeedbackSourceVerticalTabs = 48,
 
