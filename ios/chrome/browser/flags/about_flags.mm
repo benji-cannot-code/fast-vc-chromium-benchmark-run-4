7c4f40daa46ac8de2bd416e50e166c8ea58c7012
Pier-Alexandre Bouchard <pabouchard@google.com>
2026-03-13 07:00:11 -0700

    [iOS] Remove CPE from Setup List items when IOSExpandedSetupListVariationParamAllExceptCPE is enabled
    CPE should not be in the setup list when
    IOSExpandedSetupListVariationParamAllExceptCPE is enabled. This CL
    removes CPE from the setup list.
    
    Change-Id: I8a0df06ac88a66280d25bdec48a382a0d62e10da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7663520
    Commit-Queue: Pier-Alexandre Bouchard <pabouchard@google.com>
    Reviewed-by: Tarek Makkouk <tarekmakkouk@google.com>
    Reviewed-by: Xu Li <mxlg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1599026}

--- ios/chrome/browser/flags/about_flags.mm
+++ ios/chrome/browser/flags/about_flags.mm
@@ -1296,6 +1296,11 @@ const FeatureEntry::FeatureParam
         {kIOSExpandedSetupListVariationParam,
          kIOSExpandedSetupListVariationParamBackgroundCustomization}};
 
+const FeatureEntry::FeatureParam
+    IOSExpandedSetupListVariationParamAllExceptCPE[] = {
+        {kIOSExpandedSetupListVariationParam,
+         kIOSExpandedSetupListVariationParamAllExceptCPE}};
+
 const FeatureEntry::FeatureParam kIOSExpandedSetupListAll[] = {
     {kIOSExpandedSetupListVariationParam,
      kIOSExpandedSetupListVariationParamAll}};
@@ -1305,7 +1310,7 @@ const FeatureEntry::FeatureVariation kIOSExpandedSetupListVariations[] = {
     {"Home Background Customization",
      kIOSExpandedSetupListBackgroundCustomization, nullptr},
     {"Safari Data Import & Home Background Customization (without CPE)",
-     kIOSExpandedSetupListAll, nullptr},
+     IOSExpandedSetupListVariationParamAllExceptCPE, nullptr},
     {"Safari Data Import, Home Background Customization, CPE",
      kIOSExpandedSetupListAll, nullptr}};
 
