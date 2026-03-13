7770ac596db4087be89e27edc393aeb1c9d3d884
Patrick Monette <pmonette@chromium.org>
2026-03-13 06:39:34 -0700

    [Memory Coordinator] Introduce kStatefulMemoryPressure feature flag
    As part of the migration to the base::MemoryConsumer API,
    implementations of MemoryPressureListener are being updated to be
    stateful. This means that instead of one-time cache evictions, the
    maximum size of the cache is adjusted for the duration of memory
    pressure to ensure usage stays low.
    
    To avoid potential performance regressions during this transition, this
    CL reverts the migration for several components and places the new
    stateful behavior behind a feature flag.
    
    Bug: 465068849
    Change-Id: I5ba923b498ebf393e91a40513e31185c71e4d208
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7628101
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1599021}

---
+++ base/memory_coordinator/memory_coordinator_features.cc
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/memory_coordinator/memory_coordinator_features.h"
+
+namespace base {
+
+BASE_FEATURE(kStatefulMemoryPressure, base::FEATURE_DISABLED_BY_DEFAULT);
+
+}  // namespace base
---
+++ base/memory_coordinator/memory_coordinator_features.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_MEMORY_COORDINATOR_MEMORY_COORDINATOR_FEATURES_H_
+#define BASE_MEMORY_COORDINATOR_MEMORY_COORDINATOR_FEATURES_H_
+
+#include "base/base_export.h"
+#include "base/feature_list.h"
+
+namespace base {
+
+// When enabled, MemoryPressureListeners/MemoryConsumers that support this
+// feature will change their cache's max size for the duration of memory
+// pressure, instead of simply evicting entries.
+BASE_EXPORT BASE_DECLARE_FEATURE(kStatefulMemoryPressure);
+
+}  // namespace base
+
+#endif  // BASE_MEMORY_COORDINATOR_MEMORY_COORDINATOR_FEATURES_H_
