cd1116972a79411d4a670c2668c5d94bbf2d05f1
Ernest Nguyen Hung <ernn@google.com>
2026-03-13 08:06:46 -0700

    [FRE] Add 'Don't sign in' button to the native toolbar
    It implements second arm of the sign-in experiments, i.e. it adds 'Don't
    sign in' button to the profile picker native toolbar shown on top of
    Gaia screens. The button is only shown for the FRE flow. It's hidden
    once the user proceeds to the next Gaia screen. Updates to the WebUI
    part for this experiment arm will be done in a separate CL.
    
    Screenshots (internal only):
    
    Light mode - https://screenshot.googleplex.com/ndzeodxDAppeYpq
    
    Dark mode - https://screenshot.googleplex.com/6JFxwJUtJjvwGXp
    
    Recording of the manual verification (internal only):
    
    https://screencast.googleplex.com/cast/NDcwMjU1MjYwMjk2ODA2NHxiMTY4ZTRlZS0yMA
    
    Bug: 489667011
    Change-Id: I65ba178f2afe0c4d0b174dcd5e82bf578b78348e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7648075
    Commit-Queue: Ernest Nguyen Hung <ernn@google.com>
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1599062}

--- chrome/browser/ui/views/profiles/profile_picker_sign_in_toolbar.h
+++ chrome/browser/ui/views/profiles/profile_picker_sign_in_toolbar.h
@@ -7,8 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_VIEWS_PROFILES_PROFILE_PICKER_SIGN_IN_TOOLBAR_H_
 
 #include "base/functional/callback_forward.h"
+#include "ui/base/interaction/element_identifier.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/view.h"
+
+DECLARE_ELEMENT_IDENTIFIER_VALUE(
+    kProfilePickerSignInToolbarDontSignInButtonElementId);
 
 // Class responsible for the top toolbar shown during the GAIA sign-in within
 // profile creation flow.
@@ -23,7 +27,19 @@ class ProfilePickerSignInToolbar : public views::View {
       delete;
 
   // Builds the actual toolbar, before calling this function, it remains empty.
-  void BuildToolbar(base::RepeatingClosure on_back_callback);
+  // `on_back_callback` is called when the user clicks on the back button
+  // (MUST be set to a non-null callback).
+  // `on_dont_sign_in_callback` is called when the user clicks on
+  // the "Don't sign in" button, if null, the button is not created.
+  void BuildToolbar(base::RepeatingClosure on_back_callback,
+                    base::RepeatingClosure on_dont_sign_in_callback);
+
+  // Changes the visibility of the "Don't sign in" button. It's no-op if the
+  // button is not created (see `BuildToolbar` method).
+  void SetDontSignInButtonVisible(bool visible);
+
+ private:
+  raw_ptr<views::View> dont_sign_in_button_ = nullptr;
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_PROFILES_PROFILE_PICKER_SIGN_IN_TOOLBAR_H_
