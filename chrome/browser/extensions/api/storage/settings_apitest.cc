a88fd58ec5093f9cf4df209621902ca6544be49c
Devlin Cronin <rdevlin.cronin@chromium.org>
2026-03-12 17:21:34 -0700

    [Extensions] Update storage API tests to MV3
    MV2 has been unsupported for several months. We can remove testing
    support for it. Existing tests should be updated to MV3.
    
    Update API tests for the storage API to use manifest version 3 instead
    of manifest version 2 and 3.
    
    With the transition to MV3, many of these tests may also be able to be
    updated to use new patterns, such as async / await, service worker
    modules, and more. However, those changes are out of scope for these
    CLs, and can be addressed in followups.
    
    Bug: 491516661
    Change-Id: I1a401fe5cb1fc7478244e3e648a2e3e2c29e02bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7659406
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Eva Su <evasu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1598789}

--- chrome/browser/extensions/api/storage/settings_apitest.cc
+++ chrome/browser/extensions/api/storage/settings_apitest.cc
@@ -92,9 +92,7 @@ class TestSchemaRegistryObserver : public policy::SchemaRegistry::Observer {
 
 class ExtensionSettingsApiTest : public ExtensionApiTest {
  public:
-  explicit ExtensionSettingsApiTest(
-      ContextType context_type = ContextType::kNone)
-      : ExtensionApiTest(context_type) {}
+  ExtensionSettingsApiTest() = default;
   ~ExtensionSettingsApiTest() override = default;
   ExtensionSettingsApiTest(const ExtensionSettingsApiTest&) = delete;
   ExtensionSettingsApiTest& operator=(const ExtensionSettingsApiTest&) = delete;
@@ -584,14 +582,9 @@ IN_PROC_BROWSER_TEST_F(ExtensionSettingsApiTest, IsStorageEnabled) {
   EXPECT_TRUE(frontend->IsStorageEnabled(settings_namespace::MANAGED));
 }
 
-using ContextType = extensions::browser_test_util::ContextType;
-
-class ExtensionSettingsManagedStorageApiTest
-    : public ExtensionSettingsApiTest,
-      public testing::WithParamInterface<ContextType> {
+class ExtensionSettingsManagedStorageApiTest : public ExtensionSettingsApiTest {
  public:
-  ExtensionSettingsManagedStorageApiTest()
-      : ExtensionSettingsApiTest(GetParam()) {}
+  ExtensionSettingsManagedStorageApiTest() = default;
   ~ExtensionSettingsManagedStorageApiTest() override = default;
   ExtensionSettingsManagedStorageApiTest(
       const ExtensionSettingsManagedStorageApiTest& other) = delete;
@@ -608,18 +601,7 @@ class ExtensionSettingsManagedStorageApiTest
   ResultCatcher events_result_catcher_;
 };
 
-// Desktop Android supports only service worker, not persistent background.
-#if BUILDFLAG(ENABLE_EXTENSIONS)
-INSTANTIATE_TEST_SUITE_P(PersistentBackground,
-                         ExtensionSettingsManagedStorageApiTest,
-                         ::testing::Values(ContextType::kPersistentBackground));
-#endif
-
-INSTANTIATE_TEST_SUITE_P(ServiceWorker,
-                         ExtensionSettingsManagedStorageApiTest,
-                         ::testing::Values(ContextType::kServiceWorker));
-
-IN_PROC_BROWSER_TEST_P(ExtensionSettingsManagedStorageApiTest,
+IN_PROC_BROWSER_TEST_F(ExtensionSettingsManagedStorageApiTest,
                        ExtensionsSchemas) {
   // Verifies that the Schemas for the extensions domain are created on startup.
   ExtensionSystem* extension_system = ExtensionSystem::Get(profile());
@@ -705,7 +687,7 @@ IN_PROC_BROWSER_TEST_P(ExtensionSettingsManagedStorageApiTest,
 
 // TODO(crbug.com/40789870): This test should be rewritten. See the bug for more
 // details.
-IN_PROC_BROWSER_TEST_P(ExtensionSettingsManagedStorageApiTest, ManagedStorage) {
+IN_PROC_BROWSER_TEST_F(ExtensionSettingsManagedStorageApiTest, ManagedStorage) {
   // Set policies for the test extension.
   base::DictValue policy =
       base::DictValue()
@@ -735,7 +717,7 @@ IN_PROC_BROWSER_TEST_P(ExtensionSettingsManagedStorageApiTest, ManagedStorage) {
 #if BUILDFLAG(ENABLE_EXTENSIONS)
 // TODO(crbug.com/40789870): This test should be rewritten. See the bug for more
 // details.
-IN_PROC_BROWSER_TEST_P(ExtensionSettingsManagedStorageApiTest,
+IN_PROC_BROWSER_TEST_F(ExtensionSettingsManagedStorageApiTest,
                        PRE_ManagedStorageEvents) {
   // This test starts without any test extensions installed.
   EXPECT_FALSE(GetSingleLoadedExtension());
@@ -770,7 +752,7 @@ IN_PROC_BROWSER_TEST_P(ExtensionSettingsManagedStorageApiTest,
       << events_result_catcher_.message();
 }
 
-IN_PROC_BROWSER_TEST_P(ExtensionSettingsManagedStorageApiTest,
+IN_PROC_BROWSER_TEST_F(ExtensionSettingsManagedStorageApiTest,
                        ManagedStorageEvents) {
   // This test runs after PRE_ManagedStorageEvents without having deleted the
   // profile, so the extension is still around. While the browser restarted the
@@ -791,7 +773,7 @@ IN_PROC_BROWSER_TEST_P(ExtensionSettingsManagedStorageApiTest,
 }
 #endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
-IN_PROC_BROWSER_TEST_P(ExtensionSettingsManagedStorageApiTest,
+IN_PROC_BROWSER_TEST_F(ExtensionSettingsManagedStorageApiTest,
                        ManagedStorageDisabled) {
   // Disable the 'managed' namespace.
   StorageFrontend* frontend = StorageFrontend::Get(profile());
