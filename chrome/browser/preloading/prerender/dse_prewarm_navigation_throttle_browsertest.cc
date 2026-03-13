1bc2d365a9a6630e4e8be3ade5875dcd8fab68e8
Jiacheng Guo <gjc@google.com>
2026-03-12 23:59:43 -0700

    Throttle redirects in DSEPrewarmNavigationThrottle
    The CL throttles non-prewarm navigations during redirects if there is an
    on-going prewarm to avoid race condition and inconsistent cookies.
    The CL moves the `GetPrerenderHostId()` method from NavigationRequest
    to the public NavigationHandle interface. This makes it possible for
    NavigationThrottles (like DSEPrewarmNavigationThrottle) to query the
    PrerenderHostId associated with an ongoing navigation to determine if
    it corresponds to an ongoing prewarm.
    
    Bug: 485414743
    Change-Id: I259e4324d37f8d7904cd0312c1e9b20328dd9593
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7651256
    Commit-Queue: Jiacheng Guo <gjc@google.com>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1598903}

--- chrome/browser/preloading/prerender/dse_prewarm_navigation_throttle_browsertest.cc
+++ chrome/browser/preloading/prerender/dse_prewarm_navigation_throttle_browsertest.cc
@@ -126,6 +126,8 @@ IN_PROC_BROWSER_TEST_F(DSEPrewarmNavigationThrottleBrowserTest,
       std::make_unique<DSEPrewarmNavigationThrottleForTesting>(registry);
   EXPECT_EQ(content::NavigationThrottle::PROCEED,
             throttle->WillStartRequest().action());
+  EXPECT_EQ(content::NavigationThrottle::PROCEED,
+            throttle->WillRedirectRequest().action());
 
   // Simulate a prewarm starting.
   content::PrerenderHostId dummy_host_id(1);
@@ -137,6 +139,8 @@ IN_PROC_BROWSER_TEST_F(DSEPrewarmNavigationThrottleBrowserTest,
       std::make_unique<DSEPrewarmNavigationThrottleForTesting>(registry);
   EXPECT_EQ(content::NavigationThrottle::DEFER,
             deferred_throttle->WillStartRequest().action());
+  EXPECT_EQ(content::NavigationThrottle::DEFER,
+            deferred_throttle->WillRedirectRequest().action());
 
   // Simulate prewarm finishing. This should trigger the callback to Resume()
   // the throttle.
