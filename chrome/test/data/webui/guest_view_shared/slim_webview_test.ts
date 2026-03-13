b41b8b3e44836129444bd4368e7136022f4cd4e6
Aldo Culquicondor <acondor@chromium.org>
2026-03-13 12:36:49 -0700

    SlimWebview: test creation and navigation
    This is the first browser test for SlimWebview. It simply creates a
    webview, sets a URL and verifies the firing of load events.
    
    More tests will follow in subsequent CLs.
    
    Bug: 484389846
    Change-Id: Ia8a23adcfe84de7d146cdbd2047167e8ec1253f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7661508
    Commit-Queue: Aldo Culquicondor <acondor@chromium.org>
    Auto-Submit: Aldo Culquicondor <acondor@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1599251}

---
+++ chrome/test/data/webui/guest_view_shared/slim_webview_test.ts
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import '//glic/shared/guest_view/slim_webview.js';
+import '//glic/strings.m.js';
+
+import {loadTimeData} from '//resources/js/load_time_data.js';
+import {assertEquals, assertTrue} from 'chrome://webui-test/chai_assert.js';
+import {eventToPromise} from 'chrome://webui-test/test_util.js';
+
+suite('SlimWebViewTest', function() {
+  test('LoadEvents', async function() {
+    const webviewUrl = loadTimeData.getString('glicGuestURL');
+    assertTrue(webviewUrl.length > 0);
+
+    const webview = document.createElement('webview');
+    document.body.appendChild(webview);
+
+    const loadStartPromise = eventToPromise('loadstart', webview);
+    const loadCommitPromise = eventToPromise('loadcommit', webview);
+    const contentLoadPromise = eventToPromise('contentload', webview);
+
+    webview.src = webviewUrl;
+
+    const [loadStartEvent, loadCommitEvent] = await Promise.all([
+      loadStartPromise,
+      loadCommitPromise,
+      contentLoadPromise,
+    ]);
+
+    assertEquals(webviewUrl, loadStartEvent.url);
+    assertEquals(webviewUrl, loadCommitEvent.url);
+  });
+});
