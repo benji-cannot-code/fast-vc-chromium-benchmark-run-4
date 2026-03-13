ea4dcb0a88217c18374d89895a16e50e80b1d763
Andrew Paseltiner <apaseltiner@chromium.org>
2026-03-13 12:53:18 -0700

    Use macro for spanified inputs in media fuzzers
    This replaces direct use of the pointer-and-size pair with a
    base::span<const uint8_t>, which is frequently easier to work with and
    avoids the need for manual construction of such spans alongside the
    appropriate unsafe-buffer annotations.
    
    This CL was uploaded by git cl split.
    
    R=liberato@chromium.org, muyaoxu@google.com, toyoshim@chromium.org
    
    Bug: 491067796, 40285824
    Change-Id: I2a9c2bd3af89e963c84f4e0edc06296eaefe6008
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7666385
    Auto-Submit: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1599263}

--- media/parsers/webp_parser_fuzzertest.cc
+++ media/parsers/webp_parser_fuzzertest.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include <stddef.h>
 #include <stdint.h>
 
@@ -15,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "media/parsers/vp8_parser.h"
 #include "media/parsers/webp_parser.h"
+#include "testing/libfuzzer/libfuzzer_base_wrappers.h"
 
 struct Environment {
   Environment() { logging::SetMinLogLevel(logging::LOGGING_FATAL); }
@@ -23,8 +19,7 @@ struct Environment {
 Environment* env = new Environment();
 
 // Entry point for LibFuzzer.
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  base::span<const uint8_t> encoded_data(data, size);
-  media::ParseWebPImage(encoded_data);
+DEFINE_LLVM_FUZZER_TEST_ONE_INPUT_SPAN(const base::span<const uint8_t> data) {
+  media::ParseWebPImage(data);
   return 0;
 }
--- media/test/pipeline_integration_fuzzertest.cc
+++ media/test/pipeline_integration_fuzzertest.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include <stddef.h>
 #include <stdint.h>
 
@@ -15,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/at_exit.h"
 #include "base/command_line.h"
+#include "base/containers/span.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/location.h"
@@ -30,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/media_buildflags.h"
 #include "media/test/pipeline_integration_test_base.h"
 #include "media/test/test_media_source.h"
+#include "testing/libfuzzer/libfuzzer_base_wrappers.h"
 #include "third_party/googletest/src/googletest/src/gtest-internal-inl.h"
 
 namespace {
@@ -251,12 +248,11 @@ struct Environment {
 Environment* env = new Environment();
 
 // Entry point for LibFuzzer.
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+DEFINE_LLVM_FUZZER_TEST_ONE_INPUT_SPAN(const base::span<const uint8_t> bytes) {
   // Media pipeline starts new threads, which needs AtExitManager.
   base::AtExitManager at_exit;
 
   FuzzerVariant variant = PIPELINE_FUZZER_VARIANT;
-  base::span<const uint8_t> bytes(data, size);
 
   // These tests use GoogleTest assertions without using the GoogleTest
   // framework. While this is the case, tell GoogleTest's stack trace getter
