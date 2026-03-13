ccf12285936f976253e8d6ffd538eb78fecd5cf4
Ziran Sun <zsun@igalia.com>
2026-03-13 02:42:50 -0700

    SpellCheckCustomDictionary: introduce the dictionary class.
    Introduce SpellCheckCustomDictionary module, including WebIDL and
    class implementation.
    
    The SpellCheckCustomDictionary will be used by browser's spell checker
    to add/remove domain specific words in order to suppress spelling
    violation.
    
    https://github.com/Igalia/explainers/tree/main/spell-check-dictionary
    
    Bug: 428005649
    Change-Id: I4537c5d7b8a8320a39b76694c30642ec4a20a243
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7652434
    Commit-Queue: Ziran Sun <zsun@igalia.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1598950}

---
+++ third_party/blink/renderer/modules/spell_check_custom_dictionary/window_spell_check_custom_dictionary.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_SPELL_CHECK_CUSTOM_DICTIONARY_WINDOW_SPELL_CHECK_CUSTOM_DICTIONARY_H_
+#define THIRD_PARTY_BLINK_RENDERER_MODULES_SPELL_CHECK_CUSTOM_DICTIONARY_WINDOW_SPELL_CHECK_CUSTOM_DICTIONARY_H_
+
+#include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
+
+namespace blink {
+
+class SpellCheckCustomDictionary;
+class LocalDOMWindow;
+
+// The "spellCheckCustomDictionary" attribute on the Window global scope.
+class WindowSpellCheckCustomDictionary {
+  STATIC_ONLY(WindowSpellCheckCustomDictionary);
+
+ public:
+  static SpellCheckCustomDictionary* spellCheckCustomDictionary(
+      LocalDOMWindow&);
+};
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_MODULES_SPELL_CHECK_CUSTOM_DICTIONARY_WINDOW_SPELL_CHECK_CUSTOM_DICTIONARY_H_
--- third_party/blink/renderer/platform/bindings/wrapper_type_info.h
+++ third_party/blink/renderer/platform/bindings/wrapper_type_info.h
@@ -89,6 +89,7 @@ enum class ScriptWrappableArrayTag : std::underlying_type_t<
   kObservableArrayExoticObjectTag,
   kV8ObservableArrayCSSStyleSheetTag,
   kV8ObservableArraySpeechRecognitionPhraseTag,
+  kV8ObservableArrayStringTag,
   // End of ObservableArrayExoticObject subclasses
   kLastTag,
 };
