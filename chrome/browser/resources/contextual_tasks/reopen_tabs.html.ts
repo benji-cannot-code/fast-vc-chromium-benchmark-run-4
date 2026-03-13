cfb4116e09f32fb11f2cabcf606112bb005338af
Kevin <kevinshah@google.com>
2026-03-13 16:48:06 -0700

    [contextual_tasks] Implement Reopen Tabs UI
    This CL adds the "Reopen Tabs" section to the toolbar which is to be
    displayed when revisiting an older thread.
    https://screenshot.googleplex.com/3uvWGhoAF4EM3ng
    
    Bug: b:489832161
    Change-Id: I350d9ee63dc23c81943f46e458da40e301760c9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7635264
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Commit-Queue: Kevin Shah <kevinshah@google.com>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1599377}

---
+++ chrome/browser/resources/contextual_tasks/reopen_tabs.html.ts
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {ReopenTabsElement} from './reopen_tabs.js';
+
+export function getHtml(this: ReopenTabsElement) {
+  return html`<!--_html_template_start_-->
+    <div id="reopenContainer">
+      <div id="reopenText">$i18n{continueThread}</div>
+      <div class="reopen-buttons">
+        <cr-button class="action" aria-label="$i18n{reopenTab}"
+            @click="${this.onReopenClick_}">
+          $i18n{reopenTab}
+        </cr-button>
+        <cr-icon-button id="reopenDismiss" iron-icon="cr:close"
+            @click="${this.onDismissClick_}">
+        </cr-icon-button>
+      </div>
+    </div>
+  <!--_html_template_end_-->`;
+}
---
+++ chrome/browser/resources/contextual_tasks/reopen_tabs.ts
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import '//resources/cr_elements/cr_button/cr_button.js';
+import '//resources/cr_elements/cr_icon_button/cr_icon_button.js';
+
+import {CrLitElement} from 'chrome://resources/lit/v3_0/lit.rollup.js';
+
+import {getCss} from './reopen_tabs.css.js';
+import {getHtml} from './reopen_tabs.html.js';
+
+export class ReopenTabsElement extends CrLitElement {
+  static get is() {
+    return 'reopen-tabs';
+  }
+
+  static override get styles() {
+    return getCss();
+  }
+
+  override render() {
+    return getHtml.bind(this)();
+  }
+
+  protected onReopenClick_() {
+    this.fire('reopen-click');
+  }
+
+  protected onDismissClick_() {
+    this.fire('dismiss-click');
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'reopen-tabs': ReopenTabsElement;
+  }
+}
+
+customElements.define(ReopenTabsElement.is, ReopenTabsElement);
