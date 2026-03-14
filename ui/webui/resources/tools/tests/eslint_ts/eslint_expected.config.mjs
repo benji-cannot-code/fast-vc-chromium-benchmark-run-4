1a463d2665edee6bbe4b96c684d4b12d2030dda9
dpapad <dpapad@chromium.org>
2026-03-13 18:20:00 -0700

    WebUI: Add ESLint check to disallow use of chrome.send in Desktop.
    This is added as part of build_webui() ESLint checks and is enforcing
    the updated guidance at [1]. Exceptions are added for all existing users
    of chrome.send().
    
    [1] https://chromium.googlesource.com/chromium/src/+/main/docs/webui/webui_explainer.md#Pre_Mojo-alternative_chrome_send_WebUIMessageHandler
    
    Bug: 40245923
    Change-Id: I175ce9be0f7c4ad3f280c93ee41ff54f399a18e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7662733
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1599429}

--- ui/webui/resources/tools/tests/eslint_ts/eslint_expected.config.mjs
+++ ui/webui/resources/tools/tests/eslint_ts/eslint_expected.config.mjs
@@ -1,10 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import path from 'path';
 
-import {defaultConfig} from '../../../eslint_ts.config_base.mjs';
+import {defaultConfig, } from '../../../eslint_ts.config_base.mjs';
 
 export default [
   ...defaultConfig,
+  
   {
     languageOptions: {
       parserOptions: {
