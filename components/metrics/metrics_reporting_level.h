b36b0278dedce85737f5b37bf376530475f51796
Chirag Arora <heychirag@google.com>
2026-03-14 05:33:21 -0700

    Create enum for new Metrics Reporting Levels
    This commit adds an enum that will be used to determine the metrics
    reporting level in the new metrics settings design.
    
    Bug: 483043192
    Change-Id: Id112d67df1ebb919a672a308828c7081792a9b1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7665301
    Commit-Queue: Chirag Arora <heychirag@google.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Matt Dembski <dembski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1599496}

---
+++ components/metrics/metrics_reporting_level.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_METRICS_METRICS_REPORTING_LEVEL_H_
+#define COMPONENTS_METRICS_METRICS_REPORTING_LEVEL_H_
+
+namespace metrics {
+
+// The three levels of the metrics reporting setting.
+// TODO(b/492510818): This is part of a new feature being developed to
+// restructure metrics privacy settings into a cleaner, three-state model.
+enum class MetricsReportingLevel {
+  kNone = 0,
+  kBasic = 1,
+  kAdvanced = 2,
+};
+
+}  // namespace metrics
+
+#endif  // COMPONENTS_METRICS_METRICS_REPORTING_LEVEL_H_
