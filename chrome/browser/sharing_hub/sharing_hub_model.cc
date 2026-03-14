5c9ad03edbe6bc8c3d626ebdcc690e9ea61ec571
Antonio Gomes <tonikitoo@igalia.com>
2026-03-14 01:06:12 -0700

    Operate over BrowserWindowInterface in chrome::CanSavePage()
    Additionally, the CL also advances on not using c/b/ui/browser_finder.h
    APIs from sharing_hub_model.cc.
    
    This is a step towards not depending on //c/b/ui:ui from
    //c/b/sharing_hub:sharing_hub GN target, and finally merge both
    :sharing_hub and :impl.
    
    R=dljames@chromium.org, tluk@chromium.org
    
    Bug: 353332589
    Change-Id: Idd3d5c9a936f8590d0e546bd3e8b1236bfab05b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7665713
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Antonio Gomes Netto <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1599482}

--- chrome/browser/sharing_hub/sharing_hub_model.cc
+++ chrome/browser/sharing_hub/sharing_hub_model.cc
@@ -21,11 +21,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sharing_hub/sharing_hub_features.h"
 #include "chrome/browser/shell_integration.h"
 #include "chrome/browser/ui/browser_commands.h"
-#include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_navigator.h"
 #include "chrome/browser/ui/browser_navigator_params.h"
 #include "chrome/browser/ui/qrcode_generator/qrcode_generator_bubble_controller.h"
 #include "chrome/grit/generated_resources.h"
+#include "components/tabs/public/tab_interface.h"
 #include "components/vector_icons/vector_icons.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_thread.h"
@@ -80,8 +80,10 @@ std::vector<SharingHubAction> SharingHubModel::GetFirstPartyActionList(
         results.push_back(action);
       }
     } else if (action.command_id == IDC_SAVE_PAGE) {
-      if (chrome::CanSavePage(chrome::FindBrowserWithTab(web_contents))) {
-        results.push_back(action);
+      if (auto* tab = tabs::TabInterface::MaybeGetFromContents(web_contents)) {
+        if (chrome::CanSavePage(tab->GetBrowserWindowInterface())) {
+          results.push_back(action);
+        }
       }
     } else {
       results.push_back(action);
--- chrome/browser/ui/browser_commands.h
+++ chrome/browser/ui/browser_commands.h
@@ -244,7 +244,7 @@ void GenerateQRCode(BrowserWindowInterface* bwi);
 void SharingHub(Browser* browser);
 void ScreenshotCapture(Browser* browser);
 void SavePage(Browser* browser);
-bool CanSavePage(const Browser* browser);
+bool CanSavePage(const BrowserWindowInterface* bwi);
 void Print(BrowserWindowInterface* bwi);
 bool CanPrint(BrowserWindowInterface* bwi);
 #if BUILDFLAG(ENABLE_PRINTING)
