2094447a7d4f202f40bfa7dafa9adca656350e1a
Devlin Cronin <rdevlin.cronin@chromium.org>
2026-03-13 17:50:45 -0700

    [Extensions Cleanup] Simple JS updates in alarms api
    This CL updates extension JS files in API tests for the alarms api to
    * use `let` and `const` instead of `var`
    * update variable naming patterns (avoiding underscores, properly
      naming consts)
    * prefer single quotes instead of double quotes and use template
      literals when appropriate
    
    Bug: 492151886
    Change-Id: I64dbcd97550eb783260f074c7d3f9bb658ceed55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7664001
    Reviewed-by: Tim <tjudkins@chromium.org>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1599411}

--- chrome/test/data/extensions/api_test/alarms/spanning/background.js
+++ chrome/test/data/extensions/api_test/alarms/spanning/background.js
@@ -3,12 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-let inIncognito = chrome.extension.inIncognitoContext;
-let alarmName = inIncognito ? 'incognito' : 'normal';
-let createParams = {delayInMinutes: 0.001, periodInMinutes: 60};
+const inIncognito = chrome.extension.inIncognitoContext;
+const alarmName = inIncognito ? 'incognito' : 'normal';
+const createParams = {delayInMinutes: 0.001, periodInMinutes: 60};
 
-var alarmFired = false;
-var succeedOnAlarm = false;
+let alarmFired = false;
+let succeedOnAlarm = false;
 
 chrome.alarms.onAlarm.addListener(function(alarm) {
   chrome.test.assertFalse(alarmFired);
@@ -22,7 +22,7 @@ chrome.alarms.onAlarm.addListener(function(alarm) {
 chrome.test.runTests([
   // Creates an alarm with the name of the context it was created in.
   function createAlarm() {
-    isWaitingForAlarm = true;
+    let isWaitingForAlarm = true;
     chrome.alarms.create(alarmName, createParams, () => {
       chrome.test.assertNoLastError();
       // The alarm (which was set for an obscenely short amount of time) could
@@ -55,8 +55,8 @@ chrome.test.runTests([
     });
   },
   function clearAlarms() {
-    chrome.alarms.create(alarmName + '-1', createParams);
-    chrome.alarms.create(alarmName + '-2', createParams);
+    chrome.alarms.create(`${alarmName}-1`, createParams);
+    chrome.alarms.create(`${alarmName}-2`, createParams);
     chrome.alarms.clearAll(function(wasCleared) {
       chrome.test.assertTrue(wasCleared);
       chrome.test.succeed();
--- chrome/test/data/extensions/api_test/alarms/split/background.js
+++ chrome/test/data/extensions/api_test/alarms/split/background.js
@@ -3,11 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-let inIncognito = chrome.extension.inIncognitoContext;
-let alarmName = inIncognito ? 'incognito' : 'normal';
+const inIncognito = chrome.extension.inIncognitoContext;
+const alarmName = inIncognito ? 'incognito' : 'normal';
 let alarmTriggered = false;
 let testEventFired = false;
-let createParams = {delayInMinutes: 0.001, periodInMinutes: 60};
+const createParams = {delayInMinutes: 0.001, periodInMinutes: 60};
 
 function checkAndCompleteTest() {
   if (alarmTriggered && testEventFired)
@@ -52,8 +52,8 @@ chrome.test.runTests([
     });
   },
   function clearAlarms() {
-    chrome.alarms.create(alarmName + '-1', createParams);
-    chrome.alarms.create(alarmName + '-2', createParams);
+    chrome.alarms.create(`${alarmName}-1`, createParams);
+    chrome.alarms.create(`${alarmName}-2`, createParams);
     chrome.alarms.clearAll(function(wasCleared) {
       chrome.test.assertTrue(wasCleared);
       chrome.test.succeed();
@@ -63,4 +63,4 @@ chrome.test.runTests([
 
 // Send a message to C++ to let it know that JS has registered event
 // listeners for onAlarm and onMessage.
-chrome.test.sendMessage('ready: ' + inIncognito);
+chrome.test.sendMessage(`ready: ${inIncognito}`);
