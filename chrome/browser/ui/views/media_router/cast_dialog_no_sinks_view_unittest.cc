ad83e8e536a57bbe66c91edd8e44969fa53c8344
Brad Peters <brpeters@microsoft.com>
2026-03-13 17:38:10 -0700

    Make Cast 'info' icon have link role for screen readers
    The CastDialogNoSinksView creates a HoverButtonHandCursor for the 'info'
    icon when no cast devices were found. This inherits the Button role from
    the HoverButton base class. But the PressedCallback assigned by the
    CastDialogNoSinksView triggers a navigation to a new tab show
    kCastNoDestinationFoundURL. UI that navigates should be seen by screen
    readers as a Link, not a Button.
    
    Bug: 491869935
    Change-Id: Ib60eb2692a4968d07c5162551aa77029ca8a352d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7659562
    Reviewed-by: Muyao Xu <muyaoxu@google.com>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Commit-Queue: Brad Peters <brpeters@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1599406}

--- chrome/browser/ui/views/media_router/cast_dialog_no_sinks_view_unittest.cc
+++ chrome/browser/ui/views/media_router/cast_dialog_no_sinks_view_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/views/accessibility/view_accessibility.h"
 
 namespace media_router {
 
@@ -71,6 +72,8 @@ TEST_F(CastDialogNoSinksViewTest, SwitchViews) {
   EXPECT_NE(initial_title, get_label_text());
   EXPECT_EQ(l10n_util::GetStringUTF16(IDS_MEDIA_ROUTER_NO_DEVICES_FOUND_BUTTON),
             get_icon()->GetAccessibleName());
+  EXPECT_EQ(ax::mojom::Role::kLink,
+            get_icon()->GetViewAccessibility().GetCachedRole());
 }
 
 class CastDialogNoSinksViewWithPermissionIssueTest
@@ -98,6 +101,8 @@ TEST_F(CastDialogNoSinksViewWithPermissionIssueTest, CreateView) {
   EXPECT_EQ(l10n_util::GetStringUTF16(
                 IDS_MEDIA_ROUTER_LOCAL_DISCOVERY_PERMISSION_REJECTED_BUTTON),
             get_icon()->GetAccessibleName());
+  EXPECT_EQ(ax::mojom::Role::kLink,
+            get_icon()->GetViewAccessibility().GetCachedRole());
 }
 
 }  // namespace media_router
