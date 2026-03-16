f110c9cbcc43b1139e4b87f466671c1d390f5c7d
David Baron <ldavidbaron@google.com>
2026-03-16 11:05:48 -0700

    Change meaning of Element::customElementRegistry to simplify callers.
    This changes the meaning of Element::customElementRegistry when the
    ScopedCustomElementRegistry flag is *disabled* so that it returns the
    appropriate registry (for the flag being disabled) rather than returning
    null.
    
    This allows simplifying a number of callers so that they no longer need
    to test the ScopedCustomElementRegistry feature flag.
    
    This also makes a few behavior changes when the
    ScopedCustomElementRegistry flag is *enabled* in cases where the
    element-specific registry was not being used but probably should have
    been, in particular, in:
    * Element::ErrorMessageForAttachShadow
    * ElementInternals::IsTargetFormAssociated
    and also a more subtle behavior change in HTMLSink::start.  These should
    be the only behavior changes in this CL.
    
    There should be no behavior changes when the ScopedCustomElementRegistry
    is disabled.
    
    (It's possible that we should consider further changes to
    Element::customElementRegistry when the element is an
    HTMLTemplateElement and the flag is enabled.)
    
    Bug: 40826514
    Change-Id: I10a39c9ee8c263ffbcc16835c4a6b7a77b726431
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7663582
    Reviewed-by: Jayson Chen <jaysonchen@microsoft.com>
    Commit-Queue: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1600018}

--- third_party/blink/renderer/core/html/custom/custom_element.cc
+++ third_party/blink/renderer/core/html/custom/custom_element.cc
@@ -324,10 +324,7 @@ void CustomElement::TryToUpgrade(Element& element) {
 
   DCHECK_EQ(element.GetCustomElementState(), CustomElementState::kUndefined);
 
-  CustomElementRegistry* registry =
-      RuntimeEnabledFeatures::ScopedCustomElementRegistryEnabled()
-          ? element.customElementRegistry()
-          : element.GetTreeScope().customElementRegistry();
+  CustomElementRegistry* registry = element.customElementRegistry();
 
   if (!registry)
     return;
