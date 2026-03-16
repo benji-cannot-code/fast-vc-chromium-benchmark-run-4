4f88669450c6410a9e5d5a953006ed05b076fcd2
EmiliaPaz <emiliapaz@chromium.org>
2026-03-16 02:25:44 -0700

    Move download_danger_prompt to chrome/browser/download/BUILD.gn
    This moves download_danger_prompt from //chrome/browser target to the
    more specific //chrome/browser/download target.
    
    Additionally, unused Safe Browsing headers were removed from
    download_danger_prompt.cc.
    
    Bugs: 450662444
    Change-Id: I2825954351684bed9173ea483c54890a65e3a820
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7657916
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Auto-Submit: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1599744}

--- chrome/browser/download/download_danger_prompt.cc
+++ chrome/browser/download/download_danger_prompt.cc
@@ -7,13 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/download/download_item_warning_data.h"
 #include "components/download/public/common/download_item.h"
-#include "components/safe_browsing/buildflags.h"
 #include "content/public/browser/download_item_utils.h"
-
-#if BUILDFLAG(SAFE_BROWSING_AVAILABLE)
-#include "chrome/browser/safe_browsing/download_protection/download_protection_service.h"
-#include "chrome/browser/safe_browsing/safe_browsing_service.h"
-#endif
 
 // static
 void DownloadDangerPrompt::RecordDownloadWarningEvent(
