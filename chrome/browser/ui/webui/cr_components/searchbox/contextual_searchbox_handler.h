3611bbd973d0ed5dd54f779e3fea458baa83472c
Orin Jaworski <orinj@chromium.org>
2026-03-16 16:29:08 -0700

    [omnibox][next] Clean kExhaustiveGetRecentTabs; remove unoptimized code
    Fixed: 493255183
    Change-Id: Icb4eeb28f9eb8f1753fe9d7c758c50818f0b079e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7671417
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1600186}

--- chrome/browser/ui/webui/cr_components/searchbox/contextual_searchbox_handler.h
+++ chrome/browser/ui/webui/cr_components/searchbox/contextual_searchbox_handler.h
@@ -90,8 +90,6 @@ class ContextualSearchboxHandler
       public SearchboxHandler,
       public TabStripModelObserver {
  public:
-  DECLARE_FEATURE(kExhaustiveGetRecentTabs);
-
   explicit ContextualSearchboxHandler(
       mojo::PendingReceiver<searchbox::mojom::PageHandler>
           pending_searchbox_handler,
