557124d26aedda6ba5d139ba51768508051ee735
Sinan Sahin <sinansahin@google.com>
2026-03-16 15:04:29 -0700

    [Link Capturing] Don't initialize for CCTs not opened by Chrome
    This also ensures an Open in app option doesn't show up in the TWA header overflow menu.
    
    Bug: 450253146, 492239868
    Change-Id: Ib9a43439a8454ef4dd51c3214b13c75b4e2e5454
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7667270
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1600151}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/BaseCustomTabRootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/BaseCustomTabRootUiCoordinator.java
@@ -339,7 +339,7 @@ public class BaseCustomTabRootUiCoordinator extends RootUiCoordinator {
                     .addDelegate(browserControlsManager.getBrowserVisibilityDelegate());
         }
 
-        if (OpenInAppUtils.isOpenInAppAvailable()) {
+        if (OpenInAppUtils.isOpenInAppAvailable() && intentDataProvider.get().isOpenedByChrome()) {
             mOpenInAppEntryPoint =
                     new CustomTabOpenInAppEntryPoint(
                             mActivityTabProvider.asObservable(),
