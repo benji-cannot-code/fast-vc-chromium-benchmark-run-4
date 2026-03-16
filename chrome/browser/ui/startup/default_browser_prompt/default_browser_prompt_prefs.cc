25ddd2e24b40bd8a2368a24d86c082ca413182e4
Atharv Maan <atharvmaan@chromium.org>
2026-03-16 16:47:05 -0700

    [Default Browser] Store amount of declines and last decline time in pref
    crrev.com/c/7597112 added the prefs to track prompt declines but did not
    add the logic to update them. This CL adds the same.
    
    Bug: None
    Change-Id: I780967c85054d4b030973650144609f8379c3768
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7669408
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Commit-Queue: Atharv Maan <atharvmaan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1600193}

--- chrome/browser/ui/startup/default_browser_prompt/default_browser_prompt_prefs.cc
+++ chrome/browser/ui/startup/default_browser_prompt/default_browser_prompt_prefs.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/startup/default_browser_prompt/default_browser_prompt_prefs.h"
 
 #include "chrome/browser/browser_process.h"
-#include "chrome/browser/ui/ui_features.h"
+#include "chrome/browser/default_browser/default_browser_features.h"
 #include "chrome/common/pref_names.h"
 #include "components/prefs/pref_service.h"
 
@@ -16,6 +16,9 @@ void chrome::startup::default_prompt::ResetPromptPrefs(Profile* profile) {
   PrefService* local_state = g_browser_process->local_state();
   local_state->ClearPref(prefs::kDefaultBrowserInfobarLastDeclinedTime);
   local_state->ClearPref(prefs::kDefaultBrowserInfobarDeclinedCount);
+
+  local_state->ClearPref(prefs::kDefaultBrowserDeclinedCount);
+  local_state->ClearPref(prefs::kDefaultBrowserLastDeclinedTime);
 }
 
 void chrome::startup::default_prompt::UpdatePrefsForDismissedPrompt(
@@ -28,5 +31,10 @@ void chrome::startup::default_prompt::UpdatePrefsForDismissedPrompt(
   local_state->SetTime(prefs::kDefaultBrowserInfobarLastDeclinedTime, now);
   local_state->SetInteger(
       prefs::kDefaultBrowserInfobarDeclinedCount,
+      local_state->GetInteger(prefs::kDefaultBrowserInfobarDeclinedCount) + 1);
+
+  local_state->SetTime(prefs::kDefaultBrowserLastDeclinedTime, now);
+  local_state->SetInteger(
+      prefs::kDefaultBrowserDeclinedCount,
       local_state->GetInteger(prefs::kDefaultBrowserInfobarDeclinedCount) + 1);
 }
