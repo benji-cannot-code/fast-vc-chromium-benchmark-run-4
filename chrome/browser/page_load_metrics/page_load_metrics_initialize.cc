81c2b9564071034c726cab018991c7ca22cdab19
Eitan Goldberger <eitang@google.com>
2026-03-16 09:31:28 -0700

    Close IRM on SPA soft navigations
    Immersive Reading Mode (IRM) should close when a user navigates to a
    different page. However, typical fragment navigations shouldn't close
    IRM, because it's the same page.
    
    SPAs use fragment navigations or `history.pushState()` to do true
    navigations. PrimaryPageChanged doesn't fire even though it's navigated
    to a new page, so IRM doesn't close, even though it should because it's
    functionally a real navigation.
    
    To distinguish between true SPA page loads and minor fragment changes,
    this CL introduces `ReadAnythingSoftNavigationObserver`. This observer
    taps into the `PageLoadMetricsObserver` pipeline to listen for
    `OnSoftNavigation` events. When a soft navigation indicates a meaningful
    SPA page change has occurred, the ReadAnythingController closes IRM.
    
    Also, currently, Readability doesn't redistill on SPA soft navigations,
    so after reopening IRM, it would still show the previous page's
    distillation. As a workaround, this CL makes the WebUI be recreated when
    it's reopened after a soft navigation, to force a new distillation.
    
    Bug: 479873813
    Change-Id: I129059f3cf93c3be308ec536e81e81011a664bef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7654593
    Reviewed-by: Jacobo Tapia <jatapiaro@google.com>
    Auto-Submit: Eitan Goldberger <eitang@google.com>
    Commit-Queue: Michal Mocny <mmocny@chromium.org>
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Reviewed-by: Michal Mocny <mmocny@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Eitan Goldberger <eitang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1599951}

--- chrome/browser/page_load_metrics/page_load_metrics_initialize.cc
+++ chrome/browser/page_load_metrics/page_load_metrics_initialize.cc
@@ -66,6 +66,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/url_utils.h"
 #include "extensions/buildflags/buildflags.h"
 #include "third_party/blink/public/common/loader/lcp_critical_path_predictor_util.h"
+#include "ui/accessibility/accessibility_features.h"
 #include "url/gurl.h"
 
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || \
@@ -82,6 +83,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/page_load_metrics/observers/initial_webui_page_load_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/non_tab_webui_page_load_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/top_chrome_webui_metrics_observer.h"
+#include "chrome/browser/ui/read_anything/read_anything_soft_navigation_observer.h"
 #include "chrome/browser/ui/waap/waap_utils.h"
 #include "chrome/browser/ui/webui/top_chrome/top_chrome_webui_config.h"
 #include "chrome/browser/ui/webui/omnibox_popup/omnibox_popup_ui.h"
@@ -302,6 +304,13 @@ void PageLoadMetricsEmbedder::RegisterObservers(
     tracker->AddObserver(std::move(translate_observer));
   }
   tracker->AddObserver(std::make_unique<ZstdPageLoadMetricsObserver>());
+
+#if !BUILDFLAG(IS_ANDROID)
+  if (features::IsImmersiveReadAnythingEnabled()) {
+    tracker->AddObserver(
+        std::make_unique<ReadAnythingSoftNavigationObserver>());
+  }
+#endif
 
 #if BUILDFLAG(IS_CHROMEOS)
   if (AshSessionRestorePageLoadMetricsObserver::ShouldBeInstantiated(
