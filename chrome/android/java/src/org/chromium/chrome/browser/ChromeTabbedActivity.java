fcbbe1ac7f67954a9cfc94ee3b27d4b8779de873
Calder Kitagawa <ckitagawa@chromium.org>
2026-03-16 07:51:46 -0700

    [Hub] Remove select_tabs from the overflow menu on non-tabs panes
    This also sets us up to support other "pane specific" customizations in
    the future.
    
    Fixed: 493182478
    Change-Id: Iab2f3b072ae5fffa3c55c63d8c50afe35f7285a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7667560
    Reviewed-by: Madhav Pruthi <madhavpruthi@google.com>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Madhav Pruthi <madhavpruthi@google.com>
    Reviewed-by: Dan Polanco <polardz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1599889}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -3675,6 +3675,7 @@ public class ChromeTabbedActivity extends ChromeActivity implements PreAttachInt
                 mRootUiCoordinator.getIncognitoReauthControllerSupplier(),
                 mRootUiCoordinator.getReadAloudControllerSupplier(),
                 mRootUiCoordinator.getPageZoomManager(),
+                mHubManagerSupplier,
                 mRootUiCoordinator.getOpenInAppMenuItemProvider());
     }
 
