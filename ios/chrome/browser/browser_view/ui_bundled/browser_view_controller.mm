d9d3616ea7bdfa3f62a15d0d8d5323bf2a688bf7
Scott Yoder <scottyoder@google.com>
2026-03-16 06:56:45 -0700

    [ios] Remove redundant BVC fullscreen update
    The updateForFullscreenMinViewportInsets:maxViewportInsets: method in
    BrowserViewController is redundant because the BVC already receives
    progress updates through the updateForFullscreenProgress: method.
    
    During orientation changes, this redundant update path could trigger a
    race condition. Specifically, it calls updateForFullscreenProgress:,
    which forces a layout pass via layoutIfNeeded. If this occurs while the
    view hierarchy is in a transient state during rotation, it can result
    in the bottom toolbar being incorrectly positioned or disappearing.
    
    Removing this method ensures that BVC relies on its standard layout
    paths during rotation, which are better synchronized with the view
    transition.
    
    Fixed: 491996752
    Change-Id: If6596fa1a394706cf5f8dcd24a844312ed335b9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7663751
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Ran Sun <collinssun@microsoft.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1599858}

--- ios/chrome/browser/browser_view/ui_bundled/browser_view_controller.mm
+++ ios/chrome/browser/browser_view/ui_bundled/browser_view_controller.mm
@@ -1851,11 +1851,6 @@ const CGFloat kTopDynamicIslandInset = 24;
   }];
 }
 
-- (void)updateForFullscreenMinViewportInsets:(UIEdgeInsets)minViewportInsets
-                           maxViewportInsets:(UIEdgeInsets)maxViewportInsets {
-  [self updateForFullscreenProgress:self.fullscreenController->GetProgress()];
-}
-
 #pragma mark - FullscreenUIElement helpers
 
 // The minimum amount by which the top toolbar overlaps the browser content
@@ -1972,11 +1967,13 @@ const CGFloat kTopDynamicIslandInset = 24;
                      height <= (expandedToolbarHeight + FLT_EPSILON));
   if (IsChromeNextIaEnabled() ||
       ![self.toolbarCoordinator showingOmniboxPopup]) {
-    self.secondaryToolbarHeightConstraint.constant = height;
-    // Force a layout to cause the frame to be recalculated.
-    UIView* view = self.view;
-    [view setNeedsLayout];
-    [view layoutIfNeeded];
+    if (self.secondaryToolbarHeightConstraint.constant != height) {
+      self.secondaryToolbarHeightConstraint.constant = height;
+      // Force a layout to cause the frame to be recalculated.
+      UIView* view = self.view;
+      [view setNeedsLayout];
+      [view layoutIfNeeded];
+    }
   }
 }
 
