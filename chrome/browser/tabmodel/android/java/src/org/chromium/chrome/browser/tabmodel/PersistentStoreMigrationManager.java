ab2b1ced5ae5bf6fb71d5a1832d8bc53858a063d
Fiaz Muhammad <mfiaz@google.com>
2026-03-16 09:29:30 -0700

    [Persist] Add histograms to record instantiated store types
    Bug: 492517604
    Change-Id: If94f979e1079cdf59c479143f236d30396f9393b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7663640
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1599948}

--- chrome/browser/tabmodel/android/java/src/org/chromium/chrome/browser/tabmodel/PersistentStoreMigrationManager.java
+++ chrome/browser/tabmodel/android/java/src/org/chromium/chrome/browser/tabmodel/PersistentStoreMigrationManager.java
@@ -20,6 +20,7 @@ import java.lang.annotation.Target;
  */
 @NullMarked
 public interface PersistentStoreMigrationManager {
+    // LINT.IfChange(StoreType)
     @IntDef({
         StoreType.INVALID,
         StoreType.LEGACY,
@@ -38,6 +39,8 @@ public interface PersistentStoreMigrationManager {
         // Represents a window which needs to be cleaned or is of an unknown type.
         int UNKNOWN = 3;
     }
+
+    // LINT.ThenChange(/tools/metrics/histograms/metadata/tab/enums.xml:PersistentStoreType)
 
     /** Returns the {@link StoreType} that is considered the authoritative source of truth. */
     @StoreType
