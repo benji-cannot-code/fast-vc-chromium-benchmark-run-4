ccf333d28e1a1786a9b53e72f10165c57a21dea6
Anand Ravi <anandrv@google.com>
2026-03-16 14:45:30 -0700

    Add UMA histogram for gesture timeouts.
    Records the type of gesture event that was part a touch sequence that
    timed out.
    
    Bug: 489509871
    Change-Id: Ib96cc4070f9d206e1d02eb347cfc5cd20958f9ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7660562
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Anand Ravi <anandrv@google.com>
    Cr-Commit-Position: refs/heads/main@{#1600140}

--- ui/events/gesture_detection/touch_disposition_gesture_filter.cc
+++ ui/events/gesture_detection/touch_disposition_gesture_filter.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/trace_event/typed_macros.h"
 #include "ui/base/ui_base_features.h"
+#include "ui/events/gesture_detection/gesture_touch_uma_histogram.h"
 #include "ui/events/gesture_event_details.h"
 
 namespace ui {
@@ -376,6 +377,7 @@ void TouchDispositionGestureFilter::FilterAndSendPacket(
       continue;
     }
     if (packet.gesture_source() == GestureEventDataPacket::TOUCH_TIMEOUT) {
+      GestureTouchUMAHistogram::RecordGestureTimeout(gesture);
       // Sending a timed gesture could delete |this|, so we need to return
       // directly after the |SendGesture| call.
       SendGesture(gesture, packet);
