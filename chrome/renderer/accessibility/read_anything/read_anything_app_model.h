240c8edeb2209f9d68511e160d849e74c7a2617d
Lauren Winston <lwinston@google.com>
2026-03-16 12:58:21 -0700

    [Readability] Fix Readability crash.
    crrev.com/c/7651741 introduced a logic error which
    causes pages to crash when Readability is enabled
    but the links flag is disabled.
    
    Fixed: 492605106
    Change-Id: I0240551a6fd09d0cf1714c9474a2db9dda6e43ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7667295
    Auto-Submit: Lauren Winston <lwinston@google.com>
    Reviewed-by: Eitan Goldberger <eitang@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1600069}

--- chrome/renderer/accessibility/read_anything/read_anything_app_model.h
+++ chrome/renderer/accessibility/read_anything/read_anything_app_model.h
@@ -274,7 +274,7 @@ class ReadAnythingAppModel {
            DistillationMethod::kReadability;
   }
   bool should_apply_accessibility_updates_for_readability() const {
-    if (!features::IsReadAnythingWithReadabilityEnabled() &&
+    if (!features::IsReadAnythingWithReadabilityEnabled() ||
         !features::IsReadAnythingWithReadabilityAllowLinksEnabled()) {
       return false;
     }
