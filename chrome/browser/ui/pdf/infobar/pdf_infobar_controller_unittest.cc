4cfab0d3600cfdb81ad77a294db85edb3486fd4f
Jeff Yoon <jeffyoon@google.com>
2026-03-16 13:38:28 -0700

    Refactor BrowserWindowFeatures: Migrate PdfInfoBarController to UnownedUserData
    This CL continues the refactoring of BrowserWindowFeatures by migrating
    the `PdfInfoBarController` to use the UnownedUserData pattern, removing
    its getter from `BrowserWindowFeatures`.
    
    By providing `PdfInfoBarController::From(browser)` or `Get(*host)`, we
    can retrieve the controller without relying on the monolithic
    `BrowserWindowFeatures` registry, helping to break circular dependencies
    and simplify testing.
    
    Bug: 491874875
    Change-Id: I2a26bdf31b79e245681beffc31adb5bf4d430a1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7658869
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Jeff Yoon <jeffyoon@google.com>
    Cr-Commit-Position: refs/heads/main@{#1600091}

--- chrome/browser/ui/pdf/infobar/pdf_infobar_controller_unittest.cc
+++ chrome/browser/ui/pdf/infobar/pdf_infobar_controller_unittest.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/test_renderer_host.h"
 #include "content/public/test/web_contents_tester.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/base/unowned_user_data/unowned_user_data_host.h"
 
 namespace pdf::infobar {
 
@@ -42,6 +43,8 @@ class PdfInfoBarControllerTest : public testing::Test {
         .WillByDefault(::testing::Return(tab_strip_model()));
     ON_CALL(*browser_window_interface_, GetProfile())
         .WillByDefault(::testing::Return(profile()));
+    ON_CALL(*browser_window_interface_, GetUnownedUserDataHost())
+        .WillByDefault(::testing::ReturnRef(unowned_user_data_host_));
     delegate_->SetBrowserWindowInterface(browser_window_interface());
   }
 
@@ -105,6 +108,8 @@ class PdfInfoBarControllerTest : public testing::Test {
  private:
   // Must be the first member.
   content::BrowserTaskEnvironment task_environment_;
+
+  ui::UnownedUserDataHost unowned_user_data_host_;
 
   base::test::ScopedFeatureList feature_list_;
 
