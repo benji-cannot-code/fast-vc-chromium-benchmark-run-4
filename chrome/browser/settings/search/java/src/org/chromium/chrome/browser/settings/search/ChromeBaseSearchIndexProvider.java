faaf5f757ea34fa4e6f79840f6922b63c24fbfc0
Mohamed Adel <adelm@google.com>
2026-03-16 14:42:14 -0700

    [MultiColumn][Settings Search] Add search visibility flag to base providers and migrate some opt-outs as an example
    Updates BaseSearchIndexProvider and ChromeBaseSearchIndexProvider to
    accept an isSearchable boolean flag, allowing fragments to control their
    visibility in search results while still being indexed for structure.
    
    Bug: 467921632
    Change-Id: I1f27fee49c0b9521a56d1d69f253053cc7cdffd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7664398
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Auto-Submit: Moe Adel <adelm@google.com>
    Commit-Queue: Moe Adel <adelm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1600136}

--- chrome/browser/settings/search/java/src/org/chromium/chrome/browser/settings/search/ChromeBaseSearchIndexProvider.java
+++ chrome/browser/settings/search/java/src/org/chromium/chrome/browser/settings/search/ChromeBaseSearchIndexProvider.java
@@ -30,6 +30,10 @@ public class ChromeBaseSearchIndexProvider extends BaseSearchIndexProvider
         super(fragmentName, xmlRes);
     }
 
+    public ChromeBaseSearchIndexProvider(String fragmentName, int xmlRes, boolean isSearchable) {
+        super(fragmentName, xmlRes, isSearchable);
+    }
+
     /**
      * Override of {@link SearchIndexProvider#initPreferenceXml()} that also accepts {@link Profile}
      * to determine the preference xml resource.
@@ -49,7 +53,13 @@ public class ChromeBaseSearchIndexProvider extends BaseSearchIndexProvider
         if (xmlRes == 0) return;
 
         PreferenceParser.parseAndPopulate(
-                context, xmlRes, indexData, getPrefFragmentName(), getExtras(), providerMap);
+                context,
+                xmlRes,
+                indexData,
+                getPrefFragmentName(),
+                getExtras(context),
+                providerMap,
+                isSearchable());
     }
 
     /**
