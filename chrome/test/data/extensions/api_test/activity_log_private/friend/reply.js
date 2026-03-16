129c6ddb72ca8c4327aaf7538b5adb65629798c4
Devlin Cronin <rdevlin.cronin@chromium.org>
2026-03-16 13:58:52 -0700

    [Extensions Cleanup] Simple JS updates in activity_log_private
    This CL updates extension JS files in api_test/activity_log_private to
    * use `let` and `const` instead of `var`
    * update variable naming patterns (avoiding underscores, properly
      naming consts)
    * prefer single quotes instead of double quotes and use template
      literals when appropriate
    
    Bug: 492151886
    Change-Id: If591a93acd19abc382462b8d1acf06055e895553
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7662823
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1600101}

--- chrome/test/data/extensions/api_test/activity_log_private/friend/reply.js
+++ chrome/test/data/extensions/api_test/activity_log_private/friend/reply.js
@@ -3,19 +3,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var defaultUrl = 'http://www.google.com';
+const DEFAULT_URL = 'http://www.google.com';
 
 // Utility function to open a URL in a new tab.  If the useIncognito global is
 // true, the URL is opened in a new incognito window, otherwise it is opened in
 // a new tab in the current window.  Alternatively, whether to use incognito
 // can be specified as a second argument which overrides the global setting.
-var useIncognito = false;
+let useIncognito = false;
 
 // For the automated tests a port needs to be specified for the URLs in order to
 // contact the embedded test server. The manual tests do not use an embedded
 // test server so the port will remain undefined for manual testing and no
 // modifications will be made to the URLs.
-var testServerPort = undefined;
+let testServerPort = undefined;
 
 chrome.test.getConfig(function(config) {
   // config is undefined in manual mode, this check required to stop crashes in
@@ -31,9 +31,9 @@ function getURLWithPort(url) {
 }
 
 function openTab(url, incognito) {
-  var testUrl = getURLWithPort(url);
+  const testUrl = getURLWithPort(url);
   if (incognito == undefined ? useIncognito : incognito) {
-    chrome.windows.create({'url': testUrl, 'incognito': true});
+    chrome.windows.create({url: testUrl, incognito: true});
   } else {
     window.open(testUrl);
   }
@@ -46,9 +46,9 @@ function openTab(url, incognito) {
 function makeApiCall() {
   resetStatus();
   chrome.cookies.set({
-    'url': 'https://www.cnn.com',
-    'name': 'activity_log_test_cookie',
-    'value': 'abcdefg'
+    url: 'https://www.cnn.com',
+    name: 'activity_log_test_cookie',
+    value: 'abcdefg'
   });
   appendCompleted('makeApiCall');
 }
@@ -56,8 +56,8 @@ function makeApiCall() {
 // Makes an API call that has a custom binding.
 function makeSpecialApiCalls() {
   resetStatus();
-  var url = chrome.runtime.getURL('image/cat.jpg');
-  var noparam = chrome.extension.getViews();
+  const url = chrome.runtime.getURL('image/cat.jpg');
+  const noparam = chrome.extension.getViews();
   appendCompleted('makeSpecialApiCalls');
 }
 
@@ -72,16 +72,16 @@ function checkNoDoubleLogging() {
 // Check whether we log calls to chrome.app.*;
 function checkAppCalls() {
   resetStatus();
-  var callback = function() {};
+  const callback = function() {};
   chrome.app.getDetails();
-  var b = chrome.app.isInstalled;
-  var c = chrome.app.installState(callback);
+  const b = chrome.app.isInstalled;
+  const c = chrome.app.installState(callback);
   appendCompleted('checkAppCalls');
 }
 
 function callObjectMethod() {
   resetStatus();
-  var storageArea = chrome.storage.sync;
+  const storageArea = chrome.storage.sync;
   storageArea.clear();
   appendCompleted('callObjectMethod');
 }
@@ -93,42 +93,42 @@ function doWebRequestModifications() {
   // Install a webRequest handler that will add an HTTP header to the outgoing
   // request for the main page.
   function doModifyRequestHeaders(details) {
-    var response = {};
+    const response = {};
 
-    var headers = details.requestHeaders;
+    let headers = details.requestHeaders;
     if (headers === undefined) {
       headers = [];
     }
-    headers.push({'name': 'X-Test-Activity-Log-Send',
-                  'value': 'Present'});
+    headers.push({name: 'X-Test-Activity-Log-Send',
+                  value: 'Present'});
     response['requestHeaders'] = headers;
 
     return response;
   }
   function doModifyResponseHeaders(details) {
-    var response = {};
+    const response = {};
 
-    headers = details.responseHeaders;
+    let headers = details.responseHeaders;
     if (headers === undefined) {
       headers = [];
     }
     headers = headers.filter(
         function(x) {return x['name'] != 'Cache-Control';});
-    headers.push({'name': 'X-Test-Response-Header',
-                  'value': 'Inserted'});
-    headers.push({'name': 'Set-Cookie',
-                  'value': 'ActivityLog=InsertedCookie'});
+    headers.push({name: 'X-Test-Response-Header',
+                  value: 'Inserted'});
+    headers.push({name: 'Set-Cookie',
+                  value: 'ActivityLog=InsertedCookie'});
     response['responseHeaders'] = headers;
 
     return response;
   }
   chrome.webRequest.onBeforeSendHeaders.addListener(
       doModifyRequestHeaders,
-      {'urls': ['http://*/*'], 'types': ['main_frame']},
+      {urls: ['http://*/*'], types: ['main_frame']},
       ['blocking', 'requestHeaders']);
   chrome.webRequest.onHeadersReceived.addListener(
       doModifyResponseHeaders,
-      {'urls': ['http://*/*'], 'types': ['main_frame']},
+      {urls: ['http://*/*'], types: ['main_frame']},
       ['blocking', 'responseHeaders']);
 
   // Open a tab, then close it when it has finished loading--this should give
@@ -147,7 +147,7 @@ function doWebRequestModifications() {
       }
     }
   );
-  openTab(defaultUrl);
+  openTab(DEFAULT_URL);
 }
 
 function sendMessageToSelf() {
@@ -185,7 +185,7 @@ function connectToOther() {
 
 function tabIdTranslation() {
   resetStatus();
-  var tabIds = [-1, -1];
+  const tabIds = [-1, -1];
 
   // Test the case of a single int
   chrome.tabs.onUpdated.addListener(
@@ -194,7 +194,7 @@ function tabIdTranslation() {
           tab.url.match(/google\.com/g)) {
         chrome.tabs.executeScript(
             tabId,
-            {'file': 'google_cs.js'},
+            {file: 'google_cs.js'},
             function() {
               chrome.tabs.onUpdated.removeListener(testSingleInt);
               tabIds[0] = tabId;
@@ -208,7 +208,7 @@ function tabIdTranslation() {
   chrome.tabs.onUpdated.addListener(
     function testArray(tabId, changeInfo, tab) {
       if (changeInfo['status'] === 'complete' && tab.url.match(/google\.be/g)) {
-        chrome.tabs.move(tabId, {'index': -1});
+        chrome.tabs.move(tabId, {index: -1});
         tabIds[1] = tabId;
         chrome.tabs.onUpdated.removeListener(testArray);
         chrome.tabs.remove(tabIds);
@@ -217,7 +217,7 @@ function tabIdTranslation() {
     }
   );
 
-  openTab(defaultUrl);
+  openTab(DEFAULT_URL);
 }
 
 function executeApiCallsOnTabUpdated() {
@@ -235,7 +235,7 @@ function executeApiCallsOnTabUpdated() {
         // Inject a content script
         chrome.tabs.executeScript(
             tab.id,
-            {'file': 'google_cs.js'},
+            {file: 'google_cs.js'},
             function() {
               appendCompleted('injectContentScript');
             });
@@ -244,7 +244,7 @@ function executeApiCallsOnTabUpdated() {
         // finished.
         chrome.tabs.executeScript(
             tab.id,
-            {'code': 'document.write("g o o g l e");'},
+            {code: 'document.write("g o o g l e");'},
             function() {
               appendCompleted('injectScriptBlob');
               chrome.tabs.remove(tabId);
@@ -252,7 +252,7 @@ function executeApiCallsOnTabUpdated() {
       }
     }
   );
-  openTab(defaultUrl);
+  openTab(DEFAULT_URL);
 }
 
 
@@ -262,8 +262,8 @@ function executeApiCallsOnTabUpdated() {
 // Does an XHR from this [privileged] context.
 function doBackgroundXHR() {
   resetStatus();
-  var request = new XMLHttpRequest();
-  request.open('POST', defaultUrl, false);
+  const request = new XMLHttpRequest();
+  request.open('POST', DEFAULT_URL, false);
   request.setRequestHeader('Content-type', 'text/plain;charset=UTF-8');
   try {
     request.send();
@@ -275,18 +275,18 @@ function doBackgroundXHR() {
 
 function executeDOMChangesOnTabUpdated() {
   resetStatus();
-  code = '';
+  let code = '';
 
   // Accesses the Location object from inside a content script.
   code = 'window.location = "http://www.google.com/#foo"; ' +
           'document.location = "http://www.google.com/#bar"; ' +
-          'var loc = window.location; ' +
+          'let loc = window.location; ' +
           'loc.assign("http://www.google.com/#fo"); ' +
           'loc.replace("http://www.google.com/#bar");';
 
   // Mutates the DOM tree from inside a content script.
-  code += 'var d1 = document.createElement("div"); ' +
-          'var d2 = document.createElement("div"); ' +
+  code += 'let d1 = document.createElement("div"); ' +
+          'let d2 = document.createElement("div"); ' +
           'document.body.appendChild(d1); ' +
           'document.body.insertBefore(d2, d1); ' +
           'document.body.replaceChild(d1, d2);';
@@ -296,33 +296,33 @@ function executeDOMChangesOnTabUpdated() {
           'document.body.innerHTML = "Hello using innerHTML";';
 
   // Accesses the HTML5 Navigator API from inside a content script.
-  code += 'var geo = navigator.geolocation; ' +
-          'var successCallback = function(x) { }; ' +
-          'var errorCallback = function(x) { }; ' +
+  code += 'let geo = navigator.geolocation; ' +
+          'let successCallback = function(x) { }; ' +
+          'let errorCallback = function(x) { }; ' +
           'geo.getCurrentPosition(successCallback, errorCallback); ' +
-          'var id = geo.watchPosition(successCallback, errorCallback);';
+          'let id = geo.watchPosition(successCallback, errorCallback);';
 
   // Accesses the HTML5 WebStorage API from inside a content script.
-  code += 'var store = window.sessionStorage; ' +
+  code += 'let store = window.sessionStorage; ' +
           'store.setItem("foo", 42); ' +
-          'var val = store.getItem("foo"); ' +
+          'let val = store.getItem("foo"); ' +
           'store.removeItem("foo"); ' +
           'store.clear();';
 
   // Same but for localStorage.
-  code += 'var store = window.localStorage; ' +
+  code += 'store = window.localStorage; ' +
           'store.setItem("foo", 42); ' +
-          'var val = store.getItem("foo"); ' +
+          'val = store.getItem("foo"); ' +
           'store.removeItem("foo"); ' +
           'store.clear();';
 
   // Accesses the HTML5 Canvas API from inside a content script.
-  code += 'var testCanvas = document.createElement("canvas"); ' +
-          'var testContext = testCanvas.getContext("2d");';
+  code += 'let testCanvas = document.createElement("canvas"); ' +
+          'let testContext = testCanvas.getContext("2d");';
 
   // Does an XHR from inside a content script.
-  var xhrUrl = getURLWithPort(defaultUrl);
-  code += 'var request = new XMLHttpRequest(); ' +
+  const xhrUrl = getURLWithPort(DEFAULT_URL);
+  code += 'let request = new XMLHttpRequest(); ' +
           'request.open("POST", "' + xhrUrl + '", false); ' +
           'request.setRequestHeader("Content-type", ' +
           '                         "text/plain;charset=UTF-8"); ' +
@@ -332,14 +332,15 @@ function executeDOMChangesOnTabUpdated() {
   // This function is used as a handler for hooking mouse and keyboard events.
   code += 'function handlerHook(event) { };';
 
-  hookNames = ['onclick', 'ondblclick', 'ondrag', 'ondragend', 'ondragenter',
-               'ondragleave', 'ondragover', 'ondragstart', 'ondrop', 'oninput',
-               'onkeydown', 'onkeypress', 'onkeyup', 'onmousedown',
-               'onmouseenter', 'onmouseleave', 'onmousemove', 'onmouseout',
-               'onmouseover', 'onmouseup', 'onmousewheel'];
+  const hookNames =
+      ['onclick', 'ondblclick', 'ondrag', 'ondragend', 'ondragenter',
+       'ondragleave', 'ondragover', 'ondragstart', 'ondrop', 'oninput',
+       'onkeydown', 'onkeypress', 'onkeyup', 'onmousedown',
+       'onmouseenter', 'onmouseleave', 'onmousemove', 'onmouseout',
+       'onmouseover', 'onmouseup', 'onmousewheel'];
 
   // Access to each hook can be monitored for Element, Document, and Window.
-  for (var i = 0; i < hookNames.length; i++) {
+  for (let i = 0; i < hookNames.length; i++) {
     // handler on Element
     code += 'document.body.' + hookNames[i] + ' = handlerHook;';
 
@@ -356,7 +357,7 @@ function executeDOMChangesOnTabUpdated() {
           tab.url.match(/google\.com/g)) {
         chrome.tabs.onUpdated.removeListener(callback);
         chrome.tabs.executeScript(
-             tabId, {'code': code},
+             tabId, {code: code},
              function() {
                chrome.tabs.remove(tabId);
                appendCompleted('executeDOMChangesOnTabUpdated');
@@ -364,7 +365,7 @@ function executeDOMChangesOnTabUpdated() {
       }
     }
   );
-  openTab(defaultUrl);
+  openTab(DEFAULT_URL);
 }
 
 function executeDOMFullscreen() {
@@ -382,7 +383,7 @@ function launchDOMFullscreenTest() {
 }
 
 // ADD TESTS CASES TO THE MAP HERE.
-var fnMap = {};
+const fnMap = {};
 fnMap['api_call'] = makeApiCall;
 fnMap['special_call'] = makeSpecialApiCalls;
 fnMap['double'] = checkNoDoubleLogging;
@@ -431,7 +432,7 @@ function $(o) {
   return document.querySelector('#' + o);
 }
 
-var completed = 0;
+let completed = 0;
 function resetStatus(str) {
   completed = 0;
   if ($('status') != null) {
@@ -461,7 +462,7 @@ function appendError(str) {
 // (chrome-extension://pkn.../options.html#dom_fullscreen), launch that test
 // automatically.
 function setupEvents() {
-  for (var key in fnMap) {
+  for (const key in fnMap) {
     if (fnMap.hasOwnProperty(key) && key != '' && $(key) != null) {
       $(key).addEventListener('click', fnMap[key]);
     }
@@ -476,7 +477,7 @@ function setupEvents() {
 
   // Automatically launch a requested test if specified in the URL.
   if (window.location.hash) {
-    var requestedTest = window.location.hash.substr(1);
+    const requestedTest = window.location.hash.substr(1);
     if (fnMap.hasOwnProperty(requestedTest)) {
       fnMap[requestedTest]();
     }
