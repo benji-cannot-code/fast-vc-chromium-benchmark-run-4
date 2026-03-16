f7b722310411c4b46ef104d9210c1b646be27a03
Samar Chehade <samarchehade@google.com>
2026-03-16 08:48:36 -0700

    Revert "Address the final cleanup comments for 322342499 in AppBannerManager"
    This reverts commit a3bed6fb18d65f1648efbdc3a637b1f3faf593b1.
    
    Reason for revert: crbug.com/493179669
    
    Original change's description:
    > Address the final cleanup comments for 322342499 in AppBannerManager
    >
    > Gemini-written.
    >
    > Fixed: 322342499
    > Change-Id: I3bf1d370e715470ee158eb228ea0d948c759509a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7654022
    > Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    > Commit-Queue: Nate Chapin <japhet@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1597792}
    
    Bug: 493179669,322342499
    Change-Id: If65d85d1315cd45e8b1023f86e5f59bcaa99bcaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7669122
    Commit-Queue: Samar Chehade <samarchehade@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Samar Chehade <samarchehade@google.com>
    Cr-Commit-Position: refs/heads/main@{#1599923}

--- components/webapps/browser/android/app_banner_manager_android.cc
+++ components/webapps/browser/android/app_banner_manager_android.cc
@@ -129,7 +129,7 @@ bool AppBannerManagerAndroid::IsRunningForTesting(JNIEnv* env) {
 }
 
 int AppBannerManagerAndroid::GetPipelineStatusForTesting(JNIEnv* env) {
-  return static_cast<int>(app_banner_manager_->state_for_testing());
+  return static_cast<int>(app_banner_manager_->state());
 }
 
 int AppBannerManagerAndroid::GetBadgeStatusForTesting(JNIEnv* env) {
@@ -148,7 +148,8 @@ void AppBannerManagerAndroid::OnAppDetailsRetrieved(
     const JavaRef<jstring>& jicon_url) {
   // If the state isn't fetching native data, that means the page must have
   // navigated or reset in some way.
-  if (!app_banner_manager_->IsFetchingNativeData()) {
+  if (app_banner_manager_->state() !=
+      AppBannerManager::State::FETCHING_NATIVE_DATA) {
     return;
   }
   if (request_id != current_native_request_id_) {
@@ -194,7 +195,7 @@ void AppBannerManagerAndroid::ShowBannerFromBadge(
   // Close our bindings to ensure that any existing beforeinstallprompt events
   // cannot trigger add to home screen (which would cause a crash). If the
   // banner is dismissed, the event will be resent.
-  app_banner_manager_->PrepareForBannerPromptReplacement();
+  app_banner_manager_->ResetBindings();
 }
 
 // static
@@ -341,8 +342,7 @@ void AppBannerManagerAndroid::MaybeShowAmbientBadge(
                              current_config->web_app_data.manifest_id) {
     // TODO(https://crbug.com/324322110): remove once crash understood.
     DUMP_WILL_BE_CHECK(false)
-        << "Pipeline state:"
-        << static_cast<int>(app_banner_manager_->state_for_testing());
+        << "Pipeline state:" << static_cast<int>(app_banner_manager_->state());
     return;
   }
 
@@ -380,9 +380,8 @@ void AppBannerManagerAndroid::MaybeShowAmbientBadge(
                                   install_config.validated_url))));
 }
 
-AppBannerManager::ShowBannerUiResult AppBannerManagerAndroid::ShowBannerUi(
-    WebappInstallSource install_source,
-    const InstallBannerConfig& config) {
+void AppBannerManagerAndroid::ShowBannerUi(WebappInstallSource install_source,
+                                           const InstallBannerConfig& config) {
   content::WebContents* contents = app_banner_manager_->web_contents();
   DCHECK(contents);
 
@@ -413,12 +412,15 @@ AppBannerManager::ShowBannerUiResult AppBannerManagerAndroid::ShowBannerUi(
 
   if (was_shown) {
     if (native_java_app_data.is_null()) {
-      return AppBannerManager::ShowBannerUiResult::kShownWebApp;
+      app_banner_manager_->ReportStatus(
+          InstallableStatusCode::SHOWING_WEB_APP_BANNER);
     } else {
-      return AppBannerManager::ShowBannerUiResult::kShownNativeApp;
+      app_banner_manager_->ReportStatus(
+          InstallableStatusCode::SHOWING_NATIVE_APP_BANNER);
     }
   } else {
-    return AppBannerManager::ShowBannerUiResult::kFailed;
+    app_banner_manager_->ReportStatus(
+        InstallableStatusCode::FAILED_TO_CREATE_BANNER);
   }
 }
 
