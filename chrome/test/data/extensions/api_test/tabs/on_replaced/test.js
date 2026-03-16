fec89381e504298071b35025c7309babb7f8fe23
Devlin Cronin <rdevlin.cronin@chromium.org>
2026-03-16 16:53:21 -0700

    [Extensions] Update tabs API tests to MV3
    MV2 has been unsupported for several months. We can remove testing
    support for it. Existing tests should be updated to MV3.
    
    Update API tests for the tabs API to use manifest version 3 instead of
    manifest version 2 and 3.
    
    With the transition to MV3, many of these tests may also be able to be
    updated to use new patterns, such as async / await, service worker
    modules, and more. However, those changes are out of scope for these
    CLs, and can be addressed in followups.
    
    Additional Notes:
    * Some of these tests used a background page (e.g. background.html)
      instead of a background script (background.js). Luckily, these pages
      mostly didn't do anything, and were trivial to convert to scripts (and
      then to service workers).
    * This CL uncovered that the Reload and LastAccessed browsertests are
      flaky on desktop android. It moves these back to the
      ENABLE_EXTENSIONS blocks.
    
    Bypass-Check-License: Moved source file.
    Bug: 491516661
    Change-Id: I754284f0af87d513ee244eddd9effa47180ed20b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7658965
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1600198}

---
+++ chrome/test/data/extensions/api_test/tabs/on_replaced/test.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2012 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.test.getConfig(function(config) {
+  chrome.tabs.create({"url": "about:blank"}, function(tab) {
+    chrome.test.runTests([
+        function onReplacedEvent() {
+          var tabId = tab.id;
+
+          var onReplaceListener = function(new_tab_id, old_tab_id) {
+            chrome.test.assertNe(new_tab_id, tabId);
+            chrome.test.assertEq(tabId, old_tab_id);
+            chrome.tabs.onReplaced.removeListener(onReplaceListener);
+            chrome.test.succeed();
+          };
+          chrome.tabs.onReplaced.addListener(onReplaceListener);
+
+          chrome.test.notifyPass();
+        }
+    ]);
+  });
+});
