22aae4e3c19256450414a0a808267032b18fe2a5
Colin Blundell <blundell@chromium.org>
2026-03-16 03:56:00 -0700

    [//Blink] Remove kLowLatencyUsedSupportedForCanvas2D usage on CrOS
    This feature is always enabled (it came in here [1]). Have
    SharedGPUContext::LowLatencySupportedForCanvas2D() just return true on
    ChromeOS in the relevant place instead of using the base::Feature for
    full transparency and in preparation for making the base::Feature
    Android-only.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/1870052
    
    Bug: 485669935
    Change-Id: I7ed83718e68c545b59f786880ae51eea2d822473
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7666117
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1599781}

--- third_party/blink/renderer/platform/graphics/gpu/shared_gpu_context.cc
+++ third_party/blink/renderer/platform/graphics/gpu/shared_gpu_context.cc
@@ -433,6 +433,10 @@ bool SharedGpuContext::LowLatencyUsageSupportedForCanvas2D(
   }
 #endif
 
+#if BUILDFLAG(IS_CHROMEOS)
+  // Low-latency usage is always supported for Canvas2D on ChromeOS.
+  return true;
+#else
   // NOTE: crbug.com/41435781 would need to be resolved in order to support
   // low-latency usage on Mac (currently setting the desynchronized attribute
   // on a canvas is a no-op on Mac). If/once that bug is resolved, determine
@@ -440,6 +444,7 @@ bool SharedGpuContext::LowLatencyUsageSupportedForCanvas2D(
   // IsDelegatedCompositingEnabled() holds.
   return base::FeatureList::IsEnabled(
       features::kLowLatencyUsageSupportedForCanvas2D);
+#endif
 }
 
 bool SharedGpuContext::LowLatencyUsageSupportedForWebGL() {
