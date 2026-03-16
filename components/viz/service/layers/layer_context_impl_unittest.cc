6355d5ba1f0c16d60cabd5e2d84f8397cc64aae2
Colin Blundell <blundell@chromium.org>
2026-03-16 09:10:21 -0700

    [//cc] Dedupe {Picture, TileDisplay}LayerImpl::AppendQuadForTile()
    A couple notes:
    
    * TileDisplayLayerImpl now effectively sets
      produced_tile_last_append_quads_. This is a no-op as it never queries
      that value.
    * The TileBasedLayerImpl unittests that were previously creating
      solid-color dummy quads now create checkerboard quads instead (by the
      operation of AppendQuadForTile() in TileBasedLayerImpl). This change
      does not materially impact those tests.
    
    Bug: 450651370
    Change-Id: I10f16dfef40b5e0ecf52e42dd33015e4904a3d3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7653979
    Commit-Queue: Vikas Soni <vikassoni@chromium.org>
    Reviewed-by: Vikas Soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1599936}

--- components/viz/service/layers/layer_context_impl_unittest.cc
+++ components/viz/service/layers/layer_context_impl_unittest.cc
@@ -1955,7 +1955,7 @@ TEST_F(LayerContextImplUpdateDisplayTreeTileDisplayLayerPropertiesTest,
   auto* tile_display_layer_impl =
       static_cast<cc::TileDisplayLayerImpl*>(layer_impl_base);
 
-  EXPECT_FALSE(tile_display_layer_impl->nearest_neighbor());
+  EXPECT_FALSE(tile_display_layer_impl->GetNearestNeighbor());
 
   // Second update: Set nearest_neighbor to true.
   auto update2 = CreateDefaultUpdate();
@@ -1970,7 +1970,7 @@ TEST_F(LayerContextImplUpdateDisplayTreeTileDisplayLayerPropertiesTest,
   EXPECT_TRUE(
       layer_context_impl_->DoUpdateDisplayTree(std::move(update2)).has_value());
 
-  EXPECT_TRUE(tile_display_layer_impl->nearest_neighbor());
+  EXPECT_TRUE(tile_display_layer_impl->GetNearestNeighbor());
 
   // Third update: Set nearest_neighbor to false.
   auto update3 = CreateDefaultUpdate();
@@ -1985,7 +1985,7 @@ TEST_F(LayerContextImplUpdateDisplayTreeTileDisplayLayerPropertiesTest,
   EXPECT_TRUE(
       layer_context_impl_->DoUpdateDisplayTree(std::move(update3)).has_value());
 
-  EXPECT_FALSE(tile_display_layer_impl->nearest_neighbor());
+  EXPECT_FALSE(tile_display_layer_impl->GetNearestNeighbor());
 }
 
 TEST_F(LayerContextImplUpdateDisplayTreeTileDisplayLayerPropertiesTest,
