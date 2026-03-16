ed2c96d89ed2472c7e4275272aae3933f8a24a2d
Phil Yan <philyan@chromium.org>
2026-03-16 10:18:00 -0700

    [PiP] Add duration metric for fullscreen video PiP
    Add `Media.FullscreenVideoPictureInPicture.Android.Duration` histogram
    to track the duration of fullscreen video PiP sessions. Recorded when
    the PiP session is exited (e.g., when Chrome switches back to non-PiP
    mode or when the PiP window is directly closed).
    
    Bug: 486262122
    Change-Id: I893de5854c443c748e76c30d20aaabc90c61c5df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7669330
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Phil Yan <philyan@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1599978}

--- chrome/android/javatests/src/org/chromium/chrome/browser/media/ui/FullscreenVideoPictureInPictureControllerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/media/ui/FullscreenVideoPictureInPictureControllerTest.java
@@ -412,6 +412,8 @@ public class FullscreenVideoPictureInPictureControllerTest {
                         .expectIntRecord(
                                 FullscreenVideoPictureInPictureController.EXIT_REASON_HISTOGRAM,
                                 MetricsEndReason.LEFT_FULLSCREEN)
+                        .expectAnyRecord(
+                                FullscreenVideoPictureInPictureController.DURATION_HISTOGRAM)
                         .build();
 
         exitPipAndFullscreenAndWait();
