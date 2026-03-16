e123b2c858200ef7804c7d1c10d78d3fc7e98bdb
David Yeung <dayeung@chromium.org>
2026-03-16 14:18:48 -0700

    Add SplitTabChanged event to the TabStripAPI
    Cl touches a lot of files but changes are small. Its adding additional
    information using existing patterns.
    - Add OnSplitTabChanged event. Even though we have TabCollection observers, the data changed events was missing.
    - Updates some of the toy tabstrip testing utilities.
    
    Bug: 477686464
    Change-Id: I9198d204ed18fbdee85d6392478da0c67639cba8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7667240
    Reviewed-by: Fred Shih <ffred@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1600119}

--- components/tabs/impl/split_tab_data.cc
+++ components/tabs/impl/split_tab_data.cc
@@ -26,6 +26,10 @@ std::vector<tabs::TabInterface*> SplitTabData::ListTabs() const {
   return collection_->GetTabsRecursive();
 }
 
+tabs::TabCollection::Handle SplitTabData::GetCollectionHandle() const {
+  return collection_->GetHandle();
+}
+
 gfx::Range SplitTabData::GetIndexRange() const {
   std::vector<tabs::TabInterface*> split_tabs = ListTabs();
 
