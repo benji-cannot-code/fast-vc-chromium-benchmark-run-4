7c99679dffe5b36a77f708bb393518aa07c4b113
Duong Dac <ddac@chromium.org>
2026-03-16 07:12:31 -0700

    [iOS] SigninAccountCapabilitiesSceneAgent to observe capabilities
    SigninAccountCapabilitiesSceneAgent now checks the CanSignInToChrome
    account capability once available, and signs out the primary account
    if the capability is false.
    
    Bug: 484261211
    Change-Id: I98c71ea81a9a0b7d37638c0d45b3e3a76aedde41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7666329
    Reviewed-by: Anthi Orfanou <anthie@google.com>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1599872}

--- ios/chrome/browser/signin/coordinator/signin_account_capabilities_scene_agent.mm
+++ ios/chrome/browser/signin/coordinator/signin_account_capabilities_scene_agent.mm
@@ -10,7 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/barrier_closure.h"
 #import "base/functional/callback.h"
 #import "base/functional/callback_helpers.h"
+#import "base/strings/sys_string_conversions.h"
 #import "components/signin/public/base/consent_level.h"
+#import "components/signin/public/base/signin_metrics.h"
+#import "components/signin/public/identity_manager/account_capabilities.h"
+#import "components/signin/public/identity_manager/identity_manager.h"
+#import "components/signin/public/identity_manager/objc/identity_manager_observer_bridge.h"
 #import "google_apis/gaia/gaia_id.h"
 #import "ios/chrome/app/profile/profile_state.h"
 #import "ios/chrome/app/profile/profile_state_observer.h"
@@ -20,6 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/coordinator/scene/scene_ui_provider.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
+#import "ios/chrome/browser/signin/model/authentication_service.h"
+#import "ios/chrome/browser/signin/model/authentication_service_factory.h"
 #import "ios/chrome/browser/signin/model/identity_manager_factory.h"
 #import "ios/chrome/browser/signin/model/signin_util.h"
 #import "ios/chrome/browser/signin/model/system_identity.h"
@@ -27,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/signin/model/system_identity_manager_observer_bridge.h"
 
 @interface SigninAccountCapabilitiesSceneAgent () <
+    IdentityManagerObserverBridgeDelegate,
     ProfileStateObserver,
     SystemIdentityManagerObserving,
     UIBlockerManagerObserver>
@@ -42,6 +50,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   std::unique_ptr<SystemIdentityManagerObserverBridge>
       _systemIdentityManagerObserver;
+
+  std::unique_ptr<signin::IdentityManagerObserverBridge>
+      _identityManagerObserver;
 }
 
 - (instancetype)initWithSceneUIProvider:(id<SceneUIProvider>)sceneUIProvider {
@@ -61,6 +72,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [super setSceneState:sceneState];
   [self.sceneState.profileState addObserver:self];
   [self.sceneState.profileState addUIBlockerManagerObserver:self];
+
+  signin::IdentityManager* identityManager =
+      IdentityManagerFactory::GetForProfile(
+          self.sceneState.profileState.profile);
+  _identityManagerObserver =
+      std::make_unique<signin::IdentityManagerObserverBridge>(identityManager,
+                                                              self);
 }
 
 #pragma mark - SceneStateObserver
@@ -75,6 +93,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self.sceneState.profileState removeObserver:self];
   [self.sceneState removeObserver:self];
   _systemIdentityManagerObserver.reset();
+  _identityManagerObserver.reset();
 }
 
 - (void)sceneStateDidHideModalOverlay:(SceneState*)sceneState {
@@ -93,6 +112,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)onIdentityListChanged {
   [self fetchCapabilitiesForUnhandledIdentities];
+}
+
+#pragma mark - IdentityManagerObserverBridgeDelegate
+
+- (void)onExtendedAccountInfoUpdated:(const AccountInfo&)info {
+  signin::IdentityManager* identityManager =
+      IdentityManagerFactory::GetForProfile(
+          self.sceneState.profileState.profile);
+  if (info.account_id !=
+      identityManager->GetPrimaryAccountId(signin::ConsentLevel::kSignin)) {
+    return;
+  }
+
+  AuthenticationService* authenticationService =
+      AuthenticationServiceFactory::GetForProfile(
+          self.sceneState.profileState.profile);
+  if (!authenticationService) {
+    return;
+  }
+
+  if (info.capabilities.can_sign_in_to_chrome() == signin::Tribool::kFalse) {
+    authenticationService->SignOut(
+        signin_metrics::ProfileSignout::kSignoutFromCanSignInToChromeCapability,
+        nil);
+  }
 }
 
 #pragma mark - UIBlockerManagerObserver
