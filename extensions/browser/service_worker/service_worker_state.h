203ad5a5055c667890b795758a1819a28010f4a2
Chris Staite <christopher.staite@menlosecurity.com>
2026-03-16 16:06:20 -0700

    Migrate ServiceWorkerContext to ChildProcessId
    Update `ServiceWorkerContext` to use the strongly typed
    `content::ChildProcessId` instead of raw integers for process IDs. This
    change is part of a broader effort to replace unsafe integer process IDs
    with strongly typed identifiers to improve type safety and reduce the
    risk of ID mix-ups.
    
    Bug: 379869738
    Change-Id: Icbc335dff9f042c52cb4562d4c1007c53743f225
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7666125
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Andrea Orru <andreaorru@chromium.org>
    Commit-Queue: Christopher Staite <christopher.staite@menlosecurity.com>
    Cr-Commit-Position: refs/heads/main@{#1600168}

--- extensions/browser/service_worker/service_worker_state.h
+++ extensions/browser/service_worker/service_worker_state.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_observation.h"
 #include "base/time/time.h"
 #include "content/public/browser/service_worker_context.h"
+#include "content/public/common/child_process_id.h"
 #include "extensions/browser/service_worker/sequenced_context_id.h"
 #include "extensions/browser/service_worker/worker_id.h"
 #include "third_party/blink/public/common/service_worker/service_worker_status_code.h"
@@ -121,7 +122,7 @@ class ServiceWorkerState
   void DidStartWorkerForScope(const SequencedContextId& context_id,
                               base::Time start_time,
                               int64_t version_id,
-                              int process_id,
+                              content::ChildProcessId process_id,
                               int thread_id,
                               const blink::ServiceWorkerToken& token);
   // Called when the worker was requested to start, but failed.
