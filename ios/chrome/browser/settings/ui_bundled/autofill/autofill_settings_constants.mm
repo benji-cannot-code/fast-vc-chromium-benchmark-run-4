e07f88d75de6961cb78124f9d48cb11f78727e60
Alexis Hétu <sugoi@chromium.org>
2026-03-16 13:06:52 -0700

    [iOS] Add EG test for Enhanced Autofill user verification toggle
    This CL adds an EG test to verify that the Enhanced Autofill user
    verification toggle in settings behaves correctly when reauth
    succeeds or fails.
    
    Bug: 484069556
    Change-Id: I4017e16ea7a3bc41188fea74725e8ee7cb5548fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7663797
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Leo Zhao <leozhao@google.com>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1600071}

--- ios/chrome/browser/settings/ui_bundled/autofill/autofill_settings_constants.mm
+++ ios/chrome/browser/settings/ui_bundled/autofill/autofill_settings_constants.mm
@@ -12,7 +12,8 @@ NSString* const kAutofillAddressSwitchViewId = @"kAutofillAddressSwitchViewId";
 NSString* const kAutofillAddressManagedViewId =
     @"kAutofillAddressManagedViewId";
 NSString* const kEnhancedAutofillTableViewId = @"kEnhancedAutofillTableViewId";
-
+NSString* const kAutofillVerificationSwitchTableViewId =
+    @"kAutofillVerificationSwitchTableViewId";
 NSString* const kAutofillCreditCardTableViewId =
     @"kAutofillCreditCardTableViewId";
 NSString* const kAutofillCreditCardSwitchViewId =
