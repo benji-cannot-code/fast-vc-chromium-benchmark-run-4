e2160b3be4edd3fa6b6d44d61de6f75fb82c1f3d
Bhuvana Betini <bbetini@google.com>
2026-03-16 17:18:57 -0700

    [Glic] Add Pause/Play button to PiP
    Bug: 484430973
    Change-Id: I11689be5b38c40135a1a17cc5d468cc2aa34173c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7647605
    Reviewed-by: Ritika Gupta <ritikagup@google.com>
    Commit-Queue: Bhuvana Betini <bbetini@google.com>
    Reviewed-by: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1600217}

---
+++ chrome/browser/actor/android/java/src/org/chromium/chrome/browser/actor/ActorIntentConstants.java
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.actor;
+
+import org.chromium.build.annotations.NullMarked;
+
+/** Constants used for Intents and Broadcasts related to Actor tasks. */
+@NullMarked
+public final class ActorIntentConstants {
+    /** Extra for the unique task ID associated with the intent. */
+    public static final String EXTRA_TASK_ID = "org.chromium.chrome.browser.actor.EXTRA_TASK_ID";
+
+    /** Extra to pause task. */
+    public static final String ACTION_PAUSE = "org.chromium.chrome.browser.actor.ACTION_PAUSE";
+
+    /** Extra to resume task. */
+    public static final String ACTION_RESUME = "org.chromium.chrome.browser.actor.ACTION_RESUME";
+
+    /** Request Code to receive broadcast. */
+    public static final int REQUEST_CODE_PAUSE_RESUME = 101;
+
+    // Prevent instantiation
+    private ActorIntentConstants() {}
+}
