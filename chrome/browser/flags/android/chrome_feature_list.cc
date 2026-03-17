23bf8bff005381c346b54d7ee710e32a565943af
Mark Teffeteller <mteffeteller@google.com>
2026-03-17 12:04:47 -0700

    Add WebGPU disablement string to "Advanced Protection" Privacy Setting.
    The feature flag is exposed to java, and optionally displayed
    in settings based on its set value.
    
    Screenshot (before): https://screenshot.googleplex.com/9YdzWWjocZWd47X
    Screenshot (after ): https://screenshot.googleplex.com/6vFHoygxm5Wwt5J
    
    Change-Id: I64a67b029f1aa4f43937fb80640bb79cd3a2c9da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7596378
    Commit-Queue: Mark Teffeteller <mteffeteller@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1600710}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -84,6 +84,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_features.h"
 #include "extensions/buildflags/buildflags.h"
 #include "extensions/common/extension_features.h"
+#include "gpu/config/gpu_finch_features.h"
 #include "media/base/media_switches.h"
 #include "net/base/features.h"
 #include "services/device/public/cpp/device_features.h"
@@ -172,6 +173,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &download::features::kShowDownloadScanningState,
     &download::features::kSmartSuggestionForLargeDownloads,
     &feature_engagement::kIPHTabSwitcherButtonFeature,
+    &features::kAAPMBlocksWebGPU,
     &features::kAbortNavigationsFromTabClosures,
     &features::kAndroidAnimatedProgressBarInBrowser,
     &features::kBackForwardCache,
