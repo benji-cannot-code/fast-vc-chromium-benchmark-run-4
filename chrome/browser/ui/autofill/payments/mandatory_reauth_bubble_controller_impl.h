3802e85b80d5671b3dbcca28e40da56b47bf3a9d
Xiang Ji <jxiang@google.com>
2026-03-17 06:20:23 -0700

    Modularize chrome/browser/mandatory_reauth
    Bug: 490742286
    Change-Id: I866bd2e576c0ff775fb924d408cd59f0a5f4d23b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7646868
    Commit-Queue: Xiang Ji <jxiang@google.com>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1600486}

--- chrome/browser/ui/autofill/payments/mandatory_reauth_bubble_controller_impl.h
+++ chrome/browser/ui/autofill/payments/mandatory_reauth_bubble_controller_impl.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_ANDROID)
 #include "base/android/scoped_java_ref.h"
-#include "chrome/browser/mandatory_reauth/android/mandatory_reauth_opt_in_view_android.h"
+#include "chrome/browser/mandatory_reauth/android/mandatory_reauth_opt_in_view_android.h"  // nogncheck
 #endif
 
 namespace autofill {
