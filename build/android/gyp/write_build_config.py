b32692c684566d835d54b30aee7704ae78d00b1f
Sam Maier <smaier@google.com>
2026-03-17 10:48:30 -0700

    Format a few android build python files
    Change-Id: If960c0a3ce7780c765d3796302019e676a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7666992
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Sam Maier <smaier@google.com>
    Commit-Queue: Sam Maier <smaier@google.com>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1600653}

--- build/android/gyp/write_build_config.py
+++ build/android/gyp/write_build_config.py
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright 2014 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-
 """Writes a .build_config.json file.
 
 This script collects information about a target and all of its transitive
@@ -13,7 +12,6 @@ steps. It also performs a few validations.
 
 See //build/android/docs/build_config.md for more information.
 """
-
 
 import argparse
 import itertools
@@ -55,6 +53,7 @@ class OrderedSet(dict):
 
 class AndroidManifest:
   """Helper class to inspect properties of an AndroidManifest.xml file."""
+
   def __init__(self, path):
     self.path = path
     dom = xml.dom.minidom.parse(path)
@@ -76,9 +75,8 @@ class AndroidManifest:
       instrumented_package = instr.getAttributeNS(
           'http://schemas.android.com/apk/res/android', 'targetPackage')
       if instrumented_package != expected_package:
-        raise Exception(
-            'Wrong instrumented package. Expected %s, got %s'
-            % (expected_package, instrumented_package))
+        raise Exception('Wrong instrumented package. Expected %s, got %s' %
+                        (expected_package, instrumented_package))
 
   def GetPackageName(self):
     return self.manifest.getAttribute('package')
