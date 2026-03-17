b611908307b672665fb562dd332d1906c45ed575
Dmytro Dmytriiev <dmytriievd@google.com>
2026-03-17 14:19:00 -0700

    Introduce Screenshot Protection API
    This API allows developers to mark specific UIViews to be obfuscated
    during system screen capture events (both static screenshots and screen
    recordings). This is intended to help prevent sensitive information from
    leaving the app via screen capture.
    
    The public Chromium provider implementation is a no-op stub. The
    functional implementation will be provided internally.
    
    Please note: This CL will not be merged until the design document is
    fully approved. Early review is requested to gather feedback on the API
    structure and approach.
    
    Discussion Document (Context & Design):
    https://docs.google.com/document/d/1NRJZ708bZwLxrxQtgaSJAeKzvFD_m5IdL3s6kcithXI/edit?usp=sharing&resourcekey=0-cUAJGsWt4YM3Dc8y5oJ8CA
    
    Bug: 477571342
    Change-Id: Ib0c2c8026686d563aea6b1fdcc0581e764c816ac
    
    This CL is part of a multi-CL change:
     => https://crrev.com/c/7507649
      - https://crrev.com/i/8970216
    
    Change-Id: Ib0c2c8026686d563aea6b1fdcc0581e764c816ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7507649
    Reviewed-by: Ernesto Izquierdo Clua <eic@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1600808}

---
+++ ios/chrome/browser/providers/screenshot_protection/chromium_screenshot_protection.mm
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/public/provider/chrome/browser/screenshot_protection/screenshot_protection_api.h"
+
+namespace ios {
+namespace provider {
+
+void SetScreenshotProtection(UIView* view,
+                             ScreenshotProtectionOptions options) {
+  // Screenshot protection is not supported in the public Chromium
+  // implementation. This is a no-op stub.
+}
+
+}  // namespace provider
+}  // namespace ios
---
+++ ios/chrome/test/providers/screenshot_protection/test_screenshot_protection.mm
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/public/provider/chrome/browser/screenshot_protection/screenshot_protection_api.h"
+
+namespace ios {
+namespace provider {
+
+void SetScreenshotProtection(UIView* view,
+                             ScreenshotProtectionOptions options) {
+  // The test provider does nothing by default to avoid side effects on the test
+  // runner.
+}
+
+}  // namespace provider
+}  // namespace ios
