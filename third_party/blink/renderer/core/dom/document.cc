2dc305149f73016c101a423473c9b09f8831dd02
Vladimir Levin <vmpstr@chromium.org>
2026-03-17 13:27:41 -0700

    overscroll: Fire overscroll events on a similar timing to snap change.
    This patch adds overscroll events enqueuing to a similar timing as
    scroll snap.
    
    R=flackr@chromium.org
    
    Bug: 493186857
    Change-Id: Id18d68f1013ace152fb40e80c6e06b14d826e0b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7666859
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1600775}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -95,6 +95,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/v8_focus_options.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_import_node_options.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_observable_array_css_style_sheet.h"
+#include "third_party/blink/renderer/bindings/core/v8/v8_overscroll_event_init.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_throw_dom_exception.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_union_elementcreationoptions_string.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_union_htmlscriptelement_svgscriptelement.h"
@@ -311,6 +312,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/mathml_names.h"
 #include "third_party/blink/renderer/core/mobile_metrics/mobile_friendliness_checker.h"
 #include "third_party/blink/renderer/core/origin_trials/origin_trial_context.h"
+#include "third_party/blink/renderer/core/overscroll/overscroll_event.h"
 #include "third_party/blink/renderer/core/page/chrome_client.h"
 #include "third_party/blink/renderer/core/page/event_with_hit_test_results.h"
 #include "third_party/blink/renderer/core/page/focus_controller.h"
@@ -6576,6 +6578,22 @@ void Document::EnqueueScrollSnapChangingEvent(Node* target,
   scrollsnapchanging_event->SetTarget(target);
   scripted_animation_controller_->EnqueuePerFrameEvent(
       scrollsnapchanging_event);
+}
+
+void Document::EnqueueOverscrollEvent(const AtomicString& type,
+                                      Node* target,
+                                      Element* overscroll_target,
+                                      bool overscrolling) {
+  OverscrollEventInit* init = OverscrollEventInit::Create();
+  init->setOverscrollTarget(overscroll_target);
+  // We bubble if we're on the document.
+  init->setBubbles(target->IsDocumentNode());
+  if (type == event_type_names::kOverscrollchanging) {
+    init->setOverscrolling(overscrolling);
+  }
+  Event* overscroll_event = OverscrollEvent::Create(type, init);
+  overscroll_event->SetTarget(target);
+  scripted_animation_controller_->EnqueuePerFrameEvent(overscroll_event);
 }
 
 void Document::EnqueueMoveEvent() {
--- third_party/blink/renderer/core/overscroll/overscroll_event.h
+++ third_party/blink/renderer/core/overscroll/overscroll_event.h
@@ -29,10 +29,13 @@ class OverscrollEvent final : public Event {
 
   void Trace(Visitor*) const override;
 
-  Element* overscrollElement() const;
+  Element* overscrollTarget() const;
+
+  std::optional<bool> overscrolling() const;
 
  private:
-  Member<Element> overscroll_element_;
+  Member<Element> overscroll_target_;
+  std::optional<bool> overscrolling_;
 };
 
 }  // namespace blink
