333345a39fc3d5e42c41644b8e1e0365c373b148
Bruno Braga <brunobraga@google.com>
2026-03-17 03:24:58 -0700

    [Autofill AI - M4] Introduce method to display wallet disabled banner
    Bug: 467563385
    Change-Id: I5cb640ab317b3f5b48f48399596ec0d3cf166cc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7664419
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Auto-Submit: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1600424}

--- chrome/browser/autofill/android/entity_data_manager_android.h
+++ chrome/browser/autofill/android/entity_data_manager_android.h
@@ -99,6 +99,8 @@ class EntityDataManagerAndroid : public autofill::EntityDataManager::Observer {
   // See `AutofillAiAction::kEnableOrDisable` for details.
   bool CanEnableOrDisableAutofillAi(JNIEnv* env);
 
+  bool IsWalletPublicPassStorageEnabled(JNIEnv* env);
+
  private:
   ~EntityDataManagerAndroid() override;
 
@@ -110,6 +112,8 @@ class EntityDataManagerAndroid : public autofill::EntityDataManager::Observer {
   EntityDataManager& entity_data_manager() {
     return entity_data_manager_.get();
   }
+
+  bool IsWalletPublicPassStorageEnabledHelper();
 
   base::ScopedObservation<autofill::EntityDataManager,
                           autofill::EntityDataManager::Observer>
