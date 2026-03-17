afa203974f16178e26742f8fc89758d2a86e4ecf
Devlin Cronin <rdevlin.cronin@chromium.org>
2026-03-17 10:57:38 -0700

    [Extensions Cleanup] Simple JS updates in i18n API
    This CL updates extension JS files in API tests for the i18n API to
    * use `let` and `const` instead of `var`
    * update variable naming patterns (avoiding underscores, properly
      naming consts)
    * prefer single quotes instead of double quotes and use template
      literals when appropriate
    
    Bug: 492151886
    Change-Id: Ie07d2081a731755fa7c2395de3867f462d6e5493
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7667263
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1600667}

--- chrome/test/data/extensions/api_test/i18n/a.js
+++ chrome/test/data/extensions/api_test/i18n/a.js
@@ -4,4 +4,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 chrome.runtime.sendMessage(
-    chrome.i18n.getMessage("message_with_one_placeholder", "19"));
+    chrome.i18n.getMessage('message_with_one_placeholder', '19'));
--- chrome/test/data/extensions/api_test/i18nUpdate/contentscript.js
+++ chrome/test/data/extensions/api_test/i18nUpdate/contentscript.js
@@ -3,5 +3,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-document.title = chrome.i18n.getMessage("testMessage");
+document.title = chrome.i18n.getMessage('testMessage');
 chrome.test.notifyPass();
