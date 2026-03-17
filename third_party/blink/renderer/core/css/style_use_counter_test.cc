82ed7949cbdc009dbaa6be38b11c647419e54563
moonira <moonira@google.com>
2026-03-17 05:36:16 -0700

    Count discarded attr() functions with valid argument grammar
    <attr-args> = attr( <declaration-value>, <declaration-value>? )
    
    Bug: 491166375
    Change-Id: Iefcc43a08f4ea7fd78e3fe7617e89101c61d1557
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7665807
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Munira Tursunova <moonira@google.com>
    Cr-Commit-Position: refs/heads/main@{#1600473}

--- third_party/blink/renderer/core/css/style_use_counter_test.cc
+++ third_party/blink/renderer/core/css/style_use_counter_test.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/frame/local_frame_view.h"
 #include "third_party/blink/renderer/core/testing/dummy_page_holder.h"
+#include "third_party/blink/renderer/platform/testing/runtime_enabled_features_test_helpers.h"
 #include "third_party/blink/renderer/platform/testing/task_environment.h"
 
 namespace blink {
@@ -77,6 +78,25 @@ TEST_F(StyleUseCounterTest, CSSDiscardedVarWithValidArgumentGrammar) {
   EXPECT_TRUE(IsCountedOnParsing(feature, "html { --p: var(--foo bar); }"));
   EXPECT_TRUE(IsCountedOnParsing(feature, "html { --p: var(--foo bar,); }"));
   EXPECT_TRUE(IsCountedOnParsing(feature, "html { --p: var(foo, bar); }"));
+}
+
+TEST_F(StyleUseCounterTest, CSSDiscardedAttrWithValidArgumentGrammar) {
+  ScopedCSSArgumentGrammarForTest scoped(false);
+  WebFeature feature = WebFeature::kCSSDiscardedAttrWithValidArgumentGrammar;
+  EXPECT_FALSE(IsCountedOnParsing(
+      feature, "html { --p: attr(data-foo type(<number>)); }"));
+  EXPECT_FALSE(IsCountedOnParsing(
+      feature, "html { --p: attr(data-foo type(<number>), fallback); }"));
+  EXPECT_FALSE(IsCountedOnParsing(feature, "html { --p: attr(!); }"));
+  EXPECT_FALSE(IsCountedOnParsing(feature, "html { --p: attr(); }"));
+  EXPECT_FALSE(IsCountedOnParsing(feature, "html { --p: attr(data-foo;);}"));
+  EXPECT_TRUE(IsCountedOnParsing(feature, "html { --p: attr(foo type()); }"));
+  EXPECT_TRUE(IsCountedOnParsing(
+      feature, "html { --p: attr(foo type(<number>) fallback); }"));
+  EXPECT_TRUE(
+      IsCountedOnParsing(feature, "html { --p: attr(var(--foo), bar); }"));
+  EXPECT_TRUE(IsCountedOnParsing(
+      feature, "html { --p: attr(attr(data-foo), var(--bar)); }"));
 }
 
 }  // namespace blink
