1a6d7de4b0aca6a02369240b88c46be4a9063f8c
Maks Orlovich <morlovich@chromium.org>
2026-03-16 20:46:44 -0700

    Permit running WebUILocationBar tests on CrOS
    (There is only one for now, but in-progress CLs add more).
    
    Bug: 470042732
    Change-Id: I542651c77c9128c7ac8f1b13784f37b4b66571cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7662616
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1600307}

--- chrome/browser/ui/views/location_bar/webui_location_bar_browsertest.cc
+++ chrome/browser/ui/views/location_bar/webui_location_bar_browsertest.cc
@@ -19,11 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test.h"
 #include "ui/webui/tracked_element/tracked_element_web_ui.h"
 
-// kWebUILocationBar currently doesn't work on CrOS because
-// TopControlsSlideControllerChromeOS relies on LocationBarView; but CrOS
-// is also not a targeted platform.
-#if !BUILDFLAG(IS_CHROMEOS)
-
 namespace {
 
 class WebUILocationBarBrowserTest : public InProcessBrowserTest {
@@ -73,5 +68,3 @@ IN_PROC_BROWSER_TEST_F(WebUILocationBarBrowserTest, GetAnchor) {
 }
 
 }  // namespace
-
-#endif
