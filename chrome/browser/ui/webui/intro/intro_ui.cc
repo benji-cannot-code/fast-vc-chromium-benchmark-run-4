a45a79312bbc710a2a4624a34f814bc64bde1ec6
Ernest Nguyen Hung <ernn@google.com>
2026-03-17 04:01:31 -0700

    [FRE] Adjust FRE intro page to Arm 2 Sign-in experiment
    It adjusts the FRE intro page to the 2nd arm of the sign-in experiments,
    i.e. moving 'Don't sign in' button to the native toolbar shown on the
    top of Gaia screen(s). It removes the 'Don't sign in' button on the FRE
    intro page and updates the 'Sign in' button copy. This is a follow up to
    crrev.com/c/7648075.
    
    Screenshots (internal only):
    
    Light mode: https://screenshot.googleplex.com/6FKDfQxmXyeGupH
    
    Dark mode: https://screenshot.googleplex.com/4Tb33XmjKs7u6yB
    
    Note, changes to the content max width (that currently makes the title
    wrap up to the next line) will be done (if needed) in a separate CL
    after alignment with UX.
    
    Bug: 489667011
    Change-Id: I751feae823ef6e522793e44cfe6ebecfc6874d16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7668940
    Commit-Queue: Ernest Nguyen Hung <ernn@google.com>
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1600439}

--- chrome/browser/ui/webui/intro/intro_ui.cc
+++ chrome/browser/ui/webui/intro/intro_ui.cc
@@ -48,7 +48,14 @@ IntroUI::IntroUI(content::WebUI* web_ui) : content::WebUIController(web_ui) {
                                   ? IDR_INTRO_INTRO_REFRESH_HTML
                                   : IDR_INTRO_INTRO_HTML);
 
-  int title_id = IDS_FRE_SIGN_IN_TITLE_0;
+  const bool is_dont_sign_in_on_gaia_page_variation =
+      is_first_run_desktop_refresh_enabled &&
+      switches::kFirstRunDesktopSignInPromoVariation.Get() ==
+          switches::FirstRunDesktopSignInPromoVariation::kDontSignInOnGaiaPage;
+
+  const int title_id = is_dont_sign_in_on_gaia_page_variation
+                           ? IDS_FRE_GET_YOUR_BROWSER_READY_TITLE
+                           : IDS_FRE_SIGN_IN_TITLE_0;
 
   // Setting the title here instead of relying on the one provided from the
   // page itself makes it available much earlier, and avoids having to fallback
@@ -96,6 +103,11 @@ IntroUI::IntroUI(content::WebUI* web_ui) : content::WebUIController(web_ui) {
           switches::kProfileCreationDeclineSigninCTAExperiment)
           ? IDS_FRE_STAY_SIGNED_OUT_BUTTON_TITLE
           : IDS_FRE_DECLINE_SIGN_IN_BUTTON_TITLE);
+
+  source->AddLocalizedString("acceptSignInButtonTitle",
+                             is_dont_sign_in_on_gaia_page_variation
+                                 ? IDS_FRE_NEXT_BUTTON_TITLE
+                                 : IDS_FRE_ACCEPT_SIGN_IN_BUTTON_TITLE);
 
   const bool is_device_managed =
       policy::ManagementServiceFactory::GetForPlatform()->IsManaged();
@@ -146,9 +158,8 @@ IntroUI::IntroUI(content::WebUI* web_ui) : content::WebUIController(web_ui) {
   }
 
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-  source->AddResourcePath(
-      "images/refresh_showcase_illustration.png",
-      IDR_DEFAULT_BROWSER_SHOWCASE_CHROME);
+  source->AddResourcePath("images/refresh_showcase_illustration.png",
+                          IDR_DEFAULT_BROWSER_SHOWCASE_CHROME);
 #else
   source->AddResourcePath(
       "images/refresh_showcase_illustration.png",
