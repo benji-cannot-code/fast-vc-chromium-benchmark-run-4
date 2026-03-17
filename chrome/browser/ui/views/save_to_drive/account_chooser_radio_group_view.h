1055af758f2bcdf49a13b0304ffb0cb7bcf5eee3
Vincent Chiang <vincentchiang@chromium.org>
2026-03-17 10:03:54 -0700

    [Save to Drive] Change initially focused element
    This changes the initial focus element to the `Use a different account`
    button for single-account case and the first account radio button
    for multi-account case.
    
    Bug: 492172802
    Change-Id: I61a83513a5261760e4822ef6a13e8d0eeb591192
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7665717
    Commit-Queue: Vincent Chiang <vincentchiang@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1600624}

--- chrome/browser/ui/views/save_to_drive/account_chooser_radio_group_view.h
+++ chrome/browser/ui/views/save_to_drive/account_chooser_radio_group_view.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/save_to_drive/account_chooser_view_delegate.h"
 #include "components/signin/public/identity_manager/account_info.h"
+#include "ui/base/interaction/element_identifier.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/views/controls/button/radio_button.h"
 #include "ui/views/layout/box_layout_view.h"
@@ -61,6 +62,8 @@ class AccountChooserRadioGroupView : public views::BoxLayoutView,
                                      public AccountChooserRadioButtonDelegate {
   METADATA_HEADER(AccountChooserRadioGroupView, views::BoxLayoutView)
  public:
+  DECLARE_CLASS_ELEMENT_IDENTIFIER_VALUE(kFirstAccountRadioButtonId);
+
   // Precondition: accounts.size() > 1
   AccountChooserRadioGroupView(AccountChooserViewDelegate& parent_dialog,
                                const std::vector<AccountInfo>& accounts,
--- chrome/browser/ui/views/save_to_drive/account_chooser_view.cc
+++ chrome/browser/ui/views/save_to_drive/account_chooser_view.cc
@@ -39,6 +39,7 @@ AccountChooserView::AccountChooserView(
     : parent_dialog_(parent_dialog) {
   SetProperty(views::kElementIdentifierKey, kTopViewId);
   SetOrientation(views::LayoutOrientation::kVertical);
+  is_single_account_ = IsSingleAccount(accounts);
   header_view_ = AddChildView(CreateHeaderView(accounts));
   body_view_ = AddChildView(CreateBodyView(accounts, primary_account_id));
   footer_view_ = AddChildView(CreateFooterView());
@@ -48,12 +49,18 @@ AccountChooserView::~AccountChooserView() = default;
 void AccountChooserView::UpdateView(
     const std::vector<AccountInfo>& accounts,
     std::optional<CoreAccountId> primary_account_id) {
+  is_single_account_ = IsSingleAccount(accounts);
   UpdateHeaderView(accounts);
   UpdateBodyView(accounts, primary_account_id);
 }
 
 views::View* AccountChooserView::GetInitiallyFocusedView() {
-  return footer_view_->GetViewByElementId(kSaveButtonId);
+  if (is_single_account_) {
+    return footer_view_->GetViewByElementId(kAddAccountButtonId);
+  } else {
+    return body_view_->GetViewByElementId(
+        AccountChooserRadioGroupView::kFirstAccountRadioButtonId);
+  }
 }
 
 std::unique_ptr<views::View> AccountChooserView::CreateBodyMultiAccount(
@@ -109,7 +116,7 @@ std::unique_ptr<views::View> AccountChooserView::CreateBodyView(
   CHECK(IsSingleAccount(accounts) || IsMultiAccount(accounts))
       << "Account chooser view should "
          "only be used if there are one or more accounts.";
-  if (IsSingleAccount(accounts)) {
+  if (is_single_account_) {
     parent_dialog_->OnAccountSelected(accounts.front());
     return CreateBodySingleAccount(accounts.front());
   } else {
