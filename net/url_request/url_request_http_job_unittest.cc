e9be392510256dc0bbfea265fb9177e3a2a17c08
Alex Ilin <alexilin@chromium.org>
2026-03-17 04:24:35 -0700

    [DBSC] Disable DBSC for PAC file fetches to prevent deadlocks
    This CL broadens the scope of the
    `allows_device_bound_session_registration` flag to
    `allows_device_bound_sessions`. This flag now controls all DBSC
    activities in a context of a specific request, including session
    registration, processing challenges, and request deferral.
    
    Disabling DBSC for PAC file fetches is necessary to avoid a circular
    dependency that can lead to a deadlock. A PAC fetch could otherwise
    trigger a DBSC session refresh, which might then require another PAC
    fetch to resolve the proxy for the refresh request.
    
    Fixed: 483088603
    Change-Id: Ifc2ab77b5c0df8e0e4876540602b1711e55da43a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7661684
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1600448}

--- net/url_request/url_request_http_job_unittest.cc
+++ net/url_request/url_request_http_job_unittest.cc
@@ -1367,7 +1367,7 @@ class URLRequestHttpJobWithMockSocketsDeviceBoundSessionServiceTest
     request_ = context_->CreateRequest(GURL("https://www.example.com"),
                                        DEFAULT_PRIORITY, &delegate_,
                                        TRAFFIC_ANNOTATION_FOR_TESTS);
-    request_->set_allows_device_bound_session_registration(true);
+    request_->set_allows_device_bound_sessions(true);
   }
 
   device_bound_sessions::SessionServiceMock& GetMockService() {
@@ -1767,7 +1767,7 @@ TEST_F(URLRequestHttpJobWithMockSocketsDeviceBoundSessionServiceTest,
   std::unique_ptr<URLRequest> request = context_->CreateRequest(
       GURL("wss://www.example.com"), DEFAULT_PRIORITY, &delegate_,
       TRAFFIC_ANNOTATION_FOR_TESTS, /*is_for_websockets=*/true);
-  request->set_allows_device_bound_session_registration(true);
+  request->set_allows_device_bound_sessions(true);
   HttpRequestHeaders headers = WebSocketCommonTestHeaders();
   request->SetExtraRequestHeaders(headers);
 
