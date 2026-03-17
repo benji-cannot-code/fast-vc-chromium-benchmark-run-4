73b08b1e66ef2e553705ca7fb1f17953deeda450
christianxu <christianxu@chromium.org>
2026-03-17 05:34:02 -0700

    [iOS][Composebox] Record menu shown and visible options
    Record plus button shown and visible elements.
    
    Fixed: 493222242
    Change-Id: Iaff0e8d514a4da82e7c795487fbfd81e3f8d431a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7670899
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1600472}

--- ios/chrome/browser/composebox/ui/composebox_input_plate_view_controller.h
+++ ios/chrome/browser/composebox/ui/composebox_input_plate_view_controller.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @protocol ComposeboxInputPlateMutator;
 @protocol ComposeboxInputPlateViewControllerDelegate;
+@class ComposeboxMetricsRecorder;
 @class ComposeboxTheme;
 @protocol TextFieldViewContaining;
 
@@ -22,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(nonatomic, weak) id<ComposeboxInputPlateViewControllerDelegate>
     delegate;
 @property(nonatomic, weak) id<ComposeboxInputPlateMutator> mutator;
+@property(nonatomic, weak) ComposeboxMetricsRecorder* metricsRecorder;
 
 /// Height of the input view.
 @property(nonatomic, readonly) CGFloat inputHeight;
--- ios/chrome/browser/composebox/ui/composebox_metrics_recorder.h
+++ ios/chrome/browser/composebox/ui/composebox_metrics_recorder.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
+#import <vector>
+
 // LINT.IfChange(AiModeActivationSource)
 enum class AiModeActivationSource {
   kToolMenu = 0,
@@ -105,6 +107,10 @@ enum class ComposeboxDragAndDropType {
 - (void)recordVoiceSearchButtonUsed;
 - (void)recordLensSearchButtonUsed;
 - (void)recordQRScannerButtonUsed;
+
+// Records that the attachments menu was opened with the given visible buttons.
+- (void)recordAttachmentsMenuOpenedWithVisibleButtons:
+    (std::vector<FuseboxAttachmentButtonType>)visibleButtons;
 
 @end
 
