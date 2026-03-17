beb6b51e154352962bf760c4274e879864f0973f
Lauren Winston <lwinston@google.com>
2026-03-16 17:13:21 -0700

    [Images] Clear image data on updateContent.
    This was causing some issues with crrev.com/c/7664127, which
    updates how images are handled when there's no text because it
    resulted in selection accessing stale image ids.
    
    Bug: 488041420
    Change-Id: I3a1c7fd2213f941435074658812226df27aa14e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7671422
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1600214}

--- chrome/browser/resources/side_panel/read_anything/content/content_controller.ts
+++ chrome/browser/resources/side_panel/read_anything/content/content_controller.ts
@@ -228,7 +228,7 @@ export class ContentController {
 
     this.speechController_.saveReadAloudState();
     this.speechController_.resetForNewContent();
-    this.nodeStore_.clearDomNodes();
+    this.nodeStore_.clear();
 
     if (isDistilledByReadability()) {
       return this.updateContentForReadability();
