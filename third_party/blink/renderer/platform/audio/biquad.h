f9d5c2476757fcfa64173977f9483241ae980969
Kelsen Liu <kelsenliu@chromium.org>
2026-03-16 21:23:40 -0700

    Spanify Biquad::Process and related methods to use base::span
    Replace raw pointer parameters with base::span in Biquad::Process,
    ProcessFast and ProcessSliceFast, and remove UNSAFE_TODO usages. Update
    BiquadFilterHandler to pass spans to the new interface.
    
    This should cause no functional change.
    
    Bug: 40285824
    Change-Id: I0337ac39467f786c4c3b72e137ce9f7b3602410c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7666537
    Reviewed-by: Michael Wilson <mjwilson@chromium.org>
    Commit-Queue: kelsen liu <kelsenliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1600321}

--- third_party/blink/renderer/platform/audio/biquad.h
+++ third_party/blink/renderer/platform/audio/biquad.h
@@ -52,9 +52,7 @@ class PLATFORM_EXPORT Biquad final {
   explicit Biquad(unsigned render_quantum_frames);
   ~Biquad();
 
-  void Process(const float* source_p,
-               float* dest_p,
-               uint32_t frames_to_process);
+  void Process(base::span<const float> source, base::span<float> dest);
 
   bool HasSampleAccurateValues() const { return has_sample_accurate_values_; }
   void SetHasSampleAccurateValues(bool is_sample_accurate) {
@@ -112,12 +110,10 @@ class PLATFORM_EXPORT Biquad final {
   AudioDoubleArray a2_;
 
 #if BUILDFLAG(IS_MAC)
-  void ProcessFast(const float* source_p,
-                   float* dest_p,
-                   uint32_t frames_to_process);
-  void ProcessSliceFast(double* source_p,
-                        double* dest_p,
-                        double* coefficients_p,
+  void ProcessFast(base::span<const float> source, base::span<float> dest);
+  void ProcessSliceFast(base::span<double> source,
+                        base::span<double> dest,
+                        base::span<const double, 5> coefficients,
                         uint32_t frames_to_process);
 
   AudioDoubleArray input_buffer_;
