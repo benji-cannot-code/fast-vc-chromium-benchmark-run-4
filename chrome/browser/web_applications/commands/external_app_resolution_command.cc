41659a3448276545994dd3269655a01fa229cfa6
dmurph@chromium.org <dmurph@chromium.org>
2026-03-17 16:39:06 -0700

    [PWA] Fix invalid scopes from external installs
    This change fixes invalid scopes coming from externally managed
    installations until the WebAppInstallInfo scope is impossible to set
    invalidly, or we delete WebAppInstallInfo in favor of a valid Manifest.
    
    R=japhet@chromium.org
    
    Fixed: 493562276
    Change-Id: Ia0da9e6a9712ca43254e1c837d1ff819379eeb11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7673272
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1600869}

--- chrome/browser/web_applications/commands/external_app_resolution_command.cc
+++ chrome/browser/web_applications/commands/external_app_resolution_command.cc
@@ -52,6 +52,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/webapps/browser/web_contents/web_app_url_loader.h"
 #include "components/webapps/common/web_app_id.h"
 #include "content/public/browser/web_contents.h"
+#include "url/origin.h"
 
 namespace web_app {
 
@@ -722,6 +723,18 @@ void ExternalAppResolutionCommand::InstallFromInfo() {
   // trusted ones.
   web_app_info_->trusted_icons = web_app_info_->manifest_icons;
   web_app_info_->trusted_icon_bitmaps = web_app_info_->icon_bitmaps;
+
+  if (!web_app_info_->scope.is_valid() ||
+      !url::IsSameOriginWith(web_app_info_->scope,
+                             web_app_info_->start_url()) ||
+      !base::StartsWith(web_app_info_->start_url().spec(),
+                        web_app_info_->scope.spec(),
+                        base::CompareCase::SENSITIVE)) {
+    DLOG(ERROR) << "Invalid scope "
+                << web_app_info_->scope.possibly_invalid_spec()
+                << " for start_url " << web_app_info_->start_url();
+    web_app_info_->scope = web_app_info_->start_url().GetWithoutFilename();
+  }
 
   if (!apps_lock_) {
     apps_lock_ = std::make_unique<SharedWebContentsWithAppLock>();
