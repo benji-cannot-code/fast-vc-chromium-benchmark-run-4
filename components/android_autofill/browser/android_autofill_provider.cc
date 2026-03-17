1b49541ee4a030d622597094f1d40dbd2717f6f9
Gianmarco Picarella <picarella@google.com>
2026-03-17 02:26:12 -0700

    [finch] Use FormFieldId for equality checks in AndroidAutofillProvider
    This CL introduces the kAutofillAndroidFormDataCompareFieldGlobalId
    feature flag and implements the corresponding logic to use an ID-based
    equality check for retrieving form field indices in
    AndroidAutofillProvider.
    
    Prior to the introduction of FormFieldId for unique identification,
    checking form field equality required heavy logic that compared
    attribute and label values (FormFieldDataAndroid::SimilarFieldAs or
    IdenticalAndEquivalentDomElements).
    
    Transitioning the equality check to use a simple FormFieldId simplifies
    the underlying logic and brings performance improvements. This new
    ID-based logic is guarded by the flag so it can be evaluated via a Finch
    experiment. The experiment will compare the ID-based approach against
    the legacy attribute-based checks to monitor for and prevent any
    potential regressions.
    
    Bug: 456526604
    Change-Id: I2c854c9491a4eb8b2ce527e431f40dfc2fdfaf3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7666184
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Reviewed-by: Julia Sobiech <jsobiech@google.com>
    Commit-Queue: Gianmarco Picarella <picarella@google.com>
    Cr-Commit-Position: refs/heads/main@{#1600400}

--- components/android_autofill/browser/android_autofill_provider.cc
+++ components/android_autofill/browser/android_autofill_provider.cc
@@ -455,7 +455,11 @@ void AndroidAutofillProvider::OnTextFieldDidScroll(
   CHECK(session_state_ && session_state_->form);
 
   // IsLinkedForm ensures session_state_ and session_state_->form exist.
-  if (!session_state_->form->GetSimilarFieldIndex(field, &field_info.index)) {
+  if (base::FeatureList::IsEnabled(
+          features::kAutofillAndroidFormDataCompareFieldGlobalId)
+          ? !session_state_->form->GetFieldByGlobalId(field, &field_info.index)
+          : !session_state_->form->GetSimilarFieldIndex(field,
+                                                        &field_info.index)) {
     return;
   }
 
@@ -552,8 +556,12 @@ std::optional<FieldInfo> AndroidAutofillProvider::StartFocusChange(
   }
   CHECK(session_state_ && session_state_->form);
   FieldInfo field_to_focus;
-  if (!session_state_->form->GetSimilarFieldIndex(field,
-                                                  &field_to_focus.index)) {
+  if (base::FeatureList::IsEnabled(
+          features::kAutofillAndroidFormDataCompareFieldGlobalId)
+          ? !session_state_->form->GetFieldByGlobalId(field,
+                                                      &field_to_focus.index)
+          : !session_state_->form->GetSimilarFieldIndex(
+                field, &field_to_focus.index)) {
     return std::nullopt;
   }
   field_to_focus.bounds = ToClientAreaBound(field.bounds());
@@ -575,7 +583,11 @@ void AndroidAutofillProvider::MaybeFireFormFieldDidChange(
     return;
   }
   CHECK(session_state_ && session_state_->form);
-  if (!session_state_->form->GetSimilarFieldIndex(field, &field_info.index)) {
+  if (base::FeatureList::IsEnabled(
+          features::kAutofillAndroidFormDataCompareFieldGlobalId)
+          ? !session_state_->form->GetFieldByGlobalId(field, &field_info.index)
+          : !session_state_->form->GetSimilarFieldIndex(field,
+                                                        &field_info.index)) {
     return;
   }
   // Propagate the changed values to Java.
