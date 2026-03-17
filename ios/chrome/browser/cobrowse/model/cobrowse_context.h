5e62ad329ed47a59b1bb906b024c661eca6d3c69
Ameur Hosni <ameurhosni@google.com>
2026-03-17 07:18:35 -0700

    [IOS][AIM] Include the composebox attached items in the cobrowse context
    Change-Id: Ia2ea45de1043afef441447610e6b87c8fa0facc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7673482
    Auto-Submit: Ameur Hosni <ameurhosni@google.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1600521}

--- ios/chrome/browser/cobrowse/model/cobrowse_context.h
+++ ios/chrome/browser/cobrowse/model/cobrowse_context.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
+@class ComposeboxInputItem;
 class GURL;
 
 // Context for the Cobrowse flow.
@@ -27,6 +28,9 @@ class GURL;
 // The server-side ID of the conversation.
 // Corresponds to the "mtid" query parameter.
 @property(nonatomic, copy, readonly) NSString* serverID;
+
+// The items attached from the composebox.
+@property(nonatomic, copy) NSArray<ComposeboxInputItem*>* attachedItems;
 
 // Returns a default context with the base search URL, used mainly for debugging
 // purposes.
