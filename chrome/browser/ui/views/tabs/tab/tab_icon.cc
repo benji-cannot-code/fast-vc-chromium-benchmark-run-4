4c8f3053e0edb0dbcb49d73d91ca3f18c19a7a49
Jiayu Chen <jiayuchen@microsoft.com>
2026-03-18 11:52:44 -0700

    [bedrock] Migrate FindBrowserWithUiElementContext usages to BWI*
    Update call sites of chrome::FindBrowserWithUiElementContext() to store
    the result as BrowserWindowInterface* instead of Browser*.
    
    Will update the return type of chrome::FindBrowserWithUiElementContext()
    to `BrowserWindowInterface` in future CL.
    
    Bug: 431671320
    Change-Id: I08c3a80f0aeaa3362b3469f0b286190bf6a709f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7677079
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Tom Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Qikai Zhong <qikaizhong@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1601442}

--- chrome/browser/ui/views/tabs/tab/tab_icon.cc
+++ chrome/browser/ui/views/tabs/tab/tab_icon.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/browser/ui/browser_finder.h"
-#include "chrome/browser/ui/browser_window.h"
+#include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/color/chrome_color_id.h"
 #include "chrome/browser/ui/layout_constants.h"
 #include "chrome/browser/ui/tabs/tab_data.h"
@@ -481,8 +481,10 @@ void TabIcon::SetDiscarded(bool discarded) {
       favicon_size_animation_.Hide();
 
       // Potentially show an IPH if a tab was discarded.
-      Browser* browser = chrome::FindBrowserWithUiElementContext(
-          views::ElementTrackerViews::GetInstance()->GetContextForView(this));
+      BrowserWindowInterface* const browser =
+          chrome::FindBrowserWithUiElementContext(
+              views::ElementTrackerViews::GetInstance()->GetContextForView(
+                  this));
       BrowserUserEducationInterface::From(browser)->MaybeShowFeaturePromo(
           feature_engagement::kIPHDiscardRingFeature);
     } else {
