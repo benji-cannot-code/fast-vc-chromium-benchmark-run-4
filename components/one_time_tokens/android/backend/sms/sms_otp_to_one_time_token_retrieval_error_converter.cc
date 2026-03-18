cc257ece0e77c94c13258da384fc893b1ecc1d8a
Jack Thiesen <jthiesen@chromium.org>
2026-03-17 23:53:43 -0700

    Fix Crashes by giving ConvertSmsOptRetrievalApiErrorCode a Default Case
    NO_IFTTT=Default Cases Are Safer
    
    Bug: 493648142
    Change-Id: I69052f0e44c5333e932390995a03e06bad20818f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7677068
    Commit-Queue: Jack Thiesen <jthiesen@chromium.org>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1601062}

--- components/one_time_tokens/android/backend/sms/sms_otp_to_one_time_token_retrieval_error_converter.cc
+++ components/one_time_tokens/android/backend/sms/sms_otp_to_one_time_token_retrieval_error_converter.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/one_time_tokens/android/backend/sms/sms_otp_to_one_time_token_retrieval_error_converter.h"
 
+#include "base/notreached.h"
 #include "components/one_time_tokens/android/backend/sms/sms_otp_retrieval_api_error_codes.h"
 #include "components/one_time_tokens/core/browser/one_time_token_retrieval_error.h"
 
@@ -26,6 +27,9 @@ OneTimeTokenRetrievalError ConvertSmsOtpRetrievalApiErrorCode(
       return OneTimeTokenRetrievalError::kSmsOtpBackendApiNotAvailable;
     case SmsOtpRetrievalApiErrorCode::kUserPermissionRequired:
       return OneTimeTokenRetrievalError::kSmsOtpBackendUserPermissionRequired;
+    default:
+      // TODO(crbug.com/493648142) REMOVE THIS ONCE FIXED.
+      return OneTimeTokenRetrievalError::kUnknown;
   }
   // LINT.ThenChange(//components/one_time_tokens/core/browser/one_time_token_retrieval_error.h)
 }
