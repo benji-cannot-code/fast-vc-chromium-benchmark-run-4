c2c5da1e6525978397f6cd7f2e4a0611d4b74433
Quentin Pubert <qpubert@google.com>
2026-03-18 05:41:17 -0700

    [iOS] Always show origin in download manager
    This CL changes the download mediator UI so that the download origin
    will no longer be hidden if it happens to match the content of the
    address bar. Instead there will either be a origin host displayed if any
    is available, or instead a string indicating that the origin of the
    download is unknown.
    
    Bug: 474338157
    Change-Id: Iae9389ac3aed54b5afefc72079d277b79daeeb1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7674638
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Auto-Submit: Quentin Pubert <qpubert@google.com>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1601200}

--- ios/chrome/test/fakes/fake_download_manager_consumer.h
+++ ios/chrome/test/fakes/fake_download_manager_consumer.h
@@ -35,9 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // The host that triggered the download.
 @property(nonatomic) NSString* originatingHost;
 
-// Whether the originating host is displayed.
-@property(nonatomic) BOOL originatingHostDisplayed;
-
 @end
 
 #endif  // IOS_CHROME_TEST_FAKES_FAKE_DOWNLOAD_MANAGER_CONSUMER_H_
--- ios/chrome/test/fakes/fake_download_manager_consumer.mm
+++ ios/chrome/test/fakes/fake_download_manager_consumer.mm
@@ -29,9 +29,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)setCanOpenFile:(BOOL)canOpenFile {
 }
 
-- (void)setOriginatingHost:(NSString*)originatingHost display:(BOOL)display {
-  _originatingHost = originatingHost;
-  _originatingHostDisplayed = display;
-}
-
 @end
