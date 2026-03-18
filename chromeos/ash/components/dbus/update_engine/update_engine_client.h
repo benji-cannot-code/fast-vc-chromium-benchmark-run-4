35d868d00c4131bfa571f8565a4350bc071c9378
Achuith Bhandarkar <achuith@chromium.org>
2026-03-18 08:31:56 -0700

    Migrate UpdateEngineClient::Observer to base::CheckedObserver
    Update UpdateEngineClient::Observer to inherit from base::CheckedObserver. This allows using base::ObserverList without the Unchecked or UncheckedAndDanglingUntriaged variants, improving observer safety.
    
    Bug: 470679673
    Change-Id: I6d2629c79fbd936f1f453d4ee1cf18a01455d4ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7630995
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1601308}

--- chromeos/ash/components/dbus/update_engine/update_engine_client.h
+++ chromeos/ash/components/dbus/update_engine/update_engine_client.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/component_export.h"
 #include "base/functional/callback.h"
 #include "base/notimplemented.h"
+#include "base/observer_list_types.h"
 #include "base/time/time.h"
 #include "chromeos/ash/components/dbus/update_engine/update_engine.pb.h"
 #include "chromeos/dbus/common/dbus_client.h"
@@ -56,10 +57,8 @@ class COMPONENT_EXPORT(ASH_DBUS_UPDATE_ENGINE) UpdateEngineClient
   };
 
   // Interface for observing changes from the update engine.
-  class Observer {
+  class Observer : public base::CheckedObserver {
    public:
-    virtual ~Observer() {}
-
     // Called when the status is updated.
     virtual void UpdateStatusChanged(
         const update_engine::StatusResult& status) {}
