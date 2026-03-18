343e5241129e8eccbe4e71f88c94327c48fe8efa
Emily Fu <fumengyao@google.com>
2026-03-18 10:42:26 -0700

    Integrate with chrome history service
    Make AccessibilityAnnotatorBackend as a direct observer of
    HistoryService to listen to history events.
    
    Bug: b:489871834
    Change-Id: I969038a414447cbbb2cee7d14d2a2f12eb807bcd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7669706
    Commit-Queue: Emily Fu <fumengyao@google.com>
    Reviewed-by: Andrew Vuong <akvuong@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1601393}

--- chrome/browser/accessibility_annotator/accessibility_annotator_backend_factory.cc
+++ chrome/browser/accessibility_annotator/accessibility_annotator_backend_factory.cc
@@ -6,11 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/accessibility_annotator/accessibility_annotator_backend_factory.h"
 
 #include "base/files/file_path.h"
+#include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sync/data_type_store_service_factory.h"
 #include "chrome/common/channel_info.h"
 #include "components/accessibility_annotator/core/accessibility_annotator_features.h"
 #include "components/accessibility_annotator/core/storage/accessibility_annotator_backend.h"
+#include "components/history/core/browser/history_service.h"
 #include "components/sync/model/data_type_store_service.h"
 
 constexpr base::FilePath::CharType kAccessibilityAnnotatorDatabaseFileName[] =
@@ -35,6 +37,7 @@ AccessibilityAnnotatorBackendFactory::AccessibilityAnnotatorBackendFactory()
           "AccessibilityAnnotatorBackend",
           ProfileSelections::BuildRedirectedInIncognito()) {
   DependsOn(DataTypeStoreServiceFactory::GetInstance());
+  DependsOn(HistoryServiceFactory::GetInstance());
 }
 
 AccessibilityAnnotatorBackendFactory::~AccessibilityAnnotatorBackendFactory() =
@@ -56,6 +59,8 @@ AccessibilityAnnotatorBackendFactory::BuildServiceInstanceForBrowserContext(
   auto backend =
       std::make_unique<accessibility_annotator::AccessibilityAnnotatorBackend>(
           chrome::GetChannel(),
+          HistoryServiceFactory::GetForProfile(
+              profile, ServiceAccessType::EXPLICIT_ACCESS),
           DataTypeStoreServiceFactory::GetForProfile(profile)
               ->GetStoreFactory(),
           profile->GetPath().Append(kAccessibilityAnnotatorDatabaseFileName));
