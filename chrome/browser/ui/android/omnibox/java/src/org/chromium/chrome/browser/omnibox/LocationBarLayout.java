9fd92238b0a4b04cf0db214a2723a4ab272eb933
Madhav Pruthi <madhavpruthi@google.com>
2026-03-18 07:15:47 -0700

    [IA] Adjust location bar status icon visibility logic
    Demo: http://screencast/cast/NTk2NjM4NTI1MDMwNDAwMHxmZjg0YTY3ZS1mZQ
    Change-Id: I8ff8c7452765f53fddda80c505efe145b33002bc
    Bug: 493264902
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7674096
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Madhav Pruthi <madhavpruthi@google.com>
    Reviewed-by: Dan Polanco <polardz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1601269}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
@@ -26,6 +26,7 @@ import org.chromium.base.MathUtils;
 import org.chromium.build.annotations.Initializer;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.lens.LensEntryPoint;
 import org.chromium.chrome.browser.omnibox.fusebox.FuseboxCoordinator.FuseboxState;
 import org.chromium.chrome.browser.omnibox.status.StatusCoordinator;
@@ -58,6 +59,7 @@ public class LocationBarLayout extends ConstraintLayout {
 
     protected boolean mNativeInitialized;
     private final View mMarginSpacer;
+    private final int mLocationBarIconStartingPadding;
 
     protected @Nullable CompositeTouchDelegate mCompositeTouchDelegate;
     protected @Nullable SearchEngineUtils mSearchEngineUtils;
@@ -101,6 +103,8 @@ public class LocationBarLayout extends ConstraintLayout {
                         - OmniboxResourceProvider.getToolbarSidePadding(context);
         mUrlActionContainerEndMargin =
                 getResources().getDimensionPixelOffset(R.dimen.location_bar_url_action_offset);
+        mLocationBarIconStartingPadding =
+                getResources().getDimensionPixelSize(R.dimen.location_bar_icon_starting_padding);
     }
 
     /** Called when activity is being destroyed. */
@@ -122,6 +126,12 @@ public class LocationBarLayout extends ConstraintLayout {
 
         StatusView statusView = findViewById(R.id.location_bar_status);
         statusView.setCompositeTouchDelegate(mCompositeTouchDelegate);
+        statusView
+                .getIsVisibleSupplier()
+                .addSyncObserverAndCallIfNonNull(
+                        (visible) ->
+                                setLocationBarStartPadding(
+                                        visible ? 0 : mLocationBarIconStartingPadding));
     }
 
     @Override
@@ -556,4 +566,9 @@ public class LocationBarLayout extends ConstraintLayout {
      * @param hasSuggestions Number of suggestions being presented
      */
     void onSuggestionsChanged(boolean hasSuggestions) {}
+
+    private void setLocationBarStartPadding(int padding) {
+        if (!ChromeFeatureList.sAndroidPageInfoAsAppMenuItem.isEnabled()) return;
+        setPaddingRelative(padding, getPaddingTop(), getPaddingEnd(), getPaddingBottom());
+    }
 }
