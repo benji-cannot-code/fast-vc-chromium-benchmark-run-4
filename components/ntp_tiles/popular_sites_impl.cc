1e797195bc2811d8a493247f59d79fa3e02ba4c0
Samuel Huang <huangs@chromium.org>
2026-03-18 10:00:36 -0700

    Reland "[Popular Sites] Add #popular-sites-us-refresh flag with 3 arms"
    This reverts commit 6402f82349b96486e8186dbc945bda59061c6ff1.
    
    Reason for revert: Fixed static initializer issue by specifying
    FeatureEntry::FeatureParam::param_name with literal "arm".
    
    Original change's description:
    > Revert "[Popular Sites] Add #popular-sites-us-refresh flag with 3 arms"
    >
    > This reverts commit 0264fdf9a343eaf93ba4f19f132bfcadc281e93d.
    >
    > Reason for revert:
    > LUCI Bisection has identified this change as the culprit of a build failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/compile-analysis/b/8687498396917161777
    >
    > Sample failed build: https://ci.chromium.org/b/8687498396917161777
    >
    > If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F7629975&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Fcompile-analysis%2Fb%2F8687498396917161777&type=BUG
    >
    > Original change's description:
    > > [Popular Sites] Add #popular-sites-us-refresh flag with 3 arms
    > >
    > > We wish to experiment with changing Popular Sites suggestions for the
    > > US / DEFAULT region, and have 3 arms. This CL sets up the flag for
    > > experimentation. The 6 new data files
    > >
    > > https://www.gstatic.com/chrome/ntp/suggested_sites_{US,DEFAULT}_7_{1,2,3}.json
    > >
    > > have been landed already.
    > >
    > > Note: For local testing, chrome://ntp-tiles-internals provides a way to
    > > refresh Popular Sites data via "Update" button -- although one might
    > > need to change "Country" field to non-US and then back to US.
    > >
    > > Bug: 481023261
    > > Change-Id: I030c4672a6ba3100114ecc519aabc62762216e0e
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7629975
    > > Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    > > Commit-Queue: Samuel Huang <huangs@chromium.org>
    > > Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1598696}
    > >
    >
    > Bug: 481023261
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I2bd531fa408aa4260897308e654f48a64bfa4190
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7664321
    > Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    > Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    > Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1598726}
    
    Bug: 481023261
    Change-Id: Ic6fa1f395eec599c4e4f64ee78025405040d9982
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7665282
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1601369}

--- components/ntp_tiles/popular_sites_impl.cc
+++ components/ntp_tiles/popular_sites_impl.cc
@@ -61,6 +61,8 @@ namespace {
 
 const char kPopularSitesURLFormat[] =
     "https://www.gstatic.com/%ssuggested_sites_%s_%s.json";
+const char kPopularSitesURLFormatWithArm[] =
+    "https://www.gstatic.com/%ssuggested_sites_%s_%s_%d.json";
 const char kPopularSitesDefaultDirectory[] = "chrome/ntp/";
 const char kPopularSitesDefaultCountryCode[] = "DEFAULT";
 const char kPopularSitesDefaultVersion[] = "5";
@@ -85,6 +87,15 @@ GURL GetPopularSitesURL(const std::string& directory,
 
   return GURL(base::StringPrintf(kPopularSitesURLFormat, directory.c_str(),
                                  country.c_str(), version.c_str()));
+}
+
+GURL GetPopularSitesURLWithArm(const std::string& directory,
+                               const std::string& country,
+                               const std::string& version,
+                               int arm) {
+  return GURL(base::StringPrintf(kPopularSitesURLFormatWithArm,
+                                 directory.c_str(), country.c_str(),
+                                 version.c_str(), arm));
 }
 
 // Extract the country from the default search engine if the default search
@@ -396,9 +407,19 @@ GURL PopularSitesImpl::GetURLToFetch() {
 
   const GURL override_url =
       GURL(prefs_->GetString(prefs::kPopularSitesOverrideURL));
-  return override_url.is_valid()
-             ? override_url
-             : GetPopularSitesURL(directory, country, version);
+  if (override_url.is_valid()) {
+    return override_url;
+  }
+
+  if (base::FeatureList::IsEnabled(kPopularSitesRefreshUs)) {
+    int arm = kPopularSitesRefreshUsArm.Get();
+    if (arm >= 1 && arm <= 3 &&
+        (country == "US" || country == kPopularSitesDefaultCountryCode)) {
+      return GetPopularSitesURLWithArm(directory, country, version, arm);
+    }
+  }
+
+  return GetPopularSitesURL(directory, country, version);
 }
 
 std::string PopularSitesImpl::GetDirectoryToFetch() {
