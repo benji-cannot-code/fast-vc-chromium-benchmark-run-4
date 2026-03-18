5dc1c6d2f46e4d4a2539f8fba95e152372abace9
Georg Neis <neis@chromium.org>
2026-03-18 00:35:01 -0700

    ash: Dismantle crosapi's cros_display_config.mojom, part 10
    Migrate the DisplayConfigProperties type from mojo to C++.
    
    Bug: b:485123493, b:365741912, b:365902693
    Change-Id: Ia50bf5610d6dfb31c9ebfdf4f73375ad1a4c38ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7668099
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Di Wu <diwux@google.com>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1601078}

--- chrome/browser/apps/app_discovery_service/recommended_arc_apps/recommend_apps_fetcher_impl_unittest.cc
+++ chrome/browser/apps/app_discovery_service/recommended_arc_apps/recommend_apps_fetcher_impl_unittest.cc
@@ -109,7 +109,7 @@ class TestCrosDisplayConfig final : public ash::CrosDisplayConfig {
   }
   crosapi::mojom::DisplayConfigResult SetDisplayProperties(
       const std::string& id,
-      crosapi::mojom::DisplayConfigPropertiesPtr properties,
+      const ash::DisplayConfigProperties& properties,
       crosapi::mojom::DisplayConfigSource source) override {
     NOTREACHED();
   }
