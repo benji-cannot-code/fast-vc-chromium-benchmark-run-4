e9e456efababf16fceabc8a3ef8e09bb0dd59d65
Peter Conn <peconn@chromium.org>
2026-03-18 08:26:45 -0700

    Enable OriginMatcherNewCopyAssignment by default.
    Approval to launch to 100% can be found in the linked bug.
    
    Bug: 444641006
    Change-Id: I99b8cc0faffa414623b4f9ab7677d2e05a387999
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7671523
    Reviewed-by: Joanne de Abreu <jdeabreu@google.com>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1601305}

--- components/origin_matcher/features.cc
+++ components/origin_matcher/features.cc
@@ -10,6 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace origin_matcher {
 
 BASE_FEATURE(kOriginMatcherNewCopyAssignment,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace origin_matcher
