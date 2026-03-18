c3b8a2be93055896ae4eaf11a653b5ac54ae5053
Arthur Sonzogni <arthursonzogni@chromium.org>
2026-03-18 02:03:36 -0700

    Convert to UNSAFE_TODO in //net
    We are migrating from coarse-grained file-level suppression (#pragma
    allow_unsafe_buffers) to granular, expression-level markers
    (UNSAFE_TODO()). The pragma disables safety checks for an entire file,
    whereas UNSAFE_TODO() isolates specific potentially unsafe operations,
    allowing the rest of the file to be enforced as safe.
    
    This was initially automated by the [Script] below, and then completed
    by Gemini (+ human reviewed) for the problematic lines.
    
    Script: https://docs.google.com/document/d/1ORQGBNn2R-CEvNbDTjRd-GrOBOFlCxIHdcvSUA_EhR4/edit?usp=sharing
    AX-Relnotes: N/A
    Cleanup: This is an automated #cleanup.
    Bug: 409340989
    Change-Id: I61702876b3d6dcfda9d012eaab29c3158808327d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7552930
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1601110}

--- net/ssl/ssl_platform_key_win.cc
+++ net/ssl/ssl_platform_key_win.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40284755): Remove this and spanify to fix the errors.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "net/ssl/ssl_platform_key_win.h"
 
 #include <algorithm>
@@ -16,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/compiler_specific.h"
 #include "base/logging.h"
 #include "base/strings/utf_string_conversions.h"
 #include "crypto/openssl_util.h"
@@ -359,8 +355,10 @@ class SSLPlatformKeyCNG : public ThreadedSSLPrivateKey::Delegate {
 
       // Convert the RAW ECDSA signature to a DER-encoded ECDSA-Sig-Value.
       bssl::UniquePtr<ECDSA_SIG> sig(ECDSA_SIG_new());
-      if (!sig || !BN_bin2bn(signature->data(), order_len, sig->r) ||
-          !BN_bin2bn(signature->data() + order_len, order_len, sig->s)) {
+      if (!sig ||
+          !UNSAFE_TODO(BN_bin2bn(signature->data(), order_len, sig->r)) ||
+          !UNSAFE_TODO(
+              BN_bin2bn(signature->data() + order_len, order_len, sig->s))) {
         return ERR_SSL_CLIENT_AUTH_SIGNATURE_FAILED;
       }
 
