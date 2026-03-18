a3c1027bacbf983251d7a29a04005eb84468f1b2
Hitarth Kothari <hitarthkothari@google.com>
2026-03-18 07:10:02 -0700

    [TabBottomSheet] Native is responsible for CoBrowseViews lifecycle
    Bug: 493646836
    Change-Id: Id97c62d9911d5b6198f4acb4639baa2a1aef40e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7674999
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Commit-Queue: Hitarth Kothari <hitarthkothari@google.com>
    Reviewed-by: Hitarth Kothari <hitarthkothari@google.com>
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Auto-Submit: Hitarth Kothari <hitarthkothari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1601261}

--- chrome/browser/glic/widget/glic_side_panel_coordinator_android.cc
+++ chrome/browser/glic/widget/glic_side_panel_coordinator_android.cc
@@ -159,6 +159,10 @@ void GlicSidePanelCoordinatorAndroid::OnTabWillDeactivate(
 }
 
 void GlicSidePanelCoordinatorAndroid::OnClose(JNIEnv* env) {
+  if (co_browse_views_) {
+    Java_CoBrowseViews_destroy(AttachCurrentThread(), co_browse_views_);
+    co_browse_views_.Reset();
+  }
   SetState(State::kClosed);
 }
 
