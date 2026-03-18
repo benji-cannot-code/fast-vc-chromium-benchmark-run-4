579f8215695266d9ab522013c05cb6f3f4d66558
Devlin Cronin <rdevlin.cronin@chromium.org>
2026-03-17 18:49:27 -0700

    [Extensions Cleanup] Simple JS updates in document_scan API
    This CL updates extension JS files in API tests for the document_scan
    API to
    * use `let` and `const` instead of `var`
    * update variable naming patterns (avoiding underscores, properly
      naming consts)
    * prefer single quotes instead of double quotes and use template
      literals when appropriate
    
    Bug: 492151886
    Change-Id: I16224a2b6fe3df21f84a25d91d3db35aee8a14ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7666341
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1600951}

--- chrome/test/data/extensions/api_test/document_scan/scan_utils.js
+++ chrome/test/data/extensions/api_test/document_scan/scan_utils.js
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-OperationResult = chrome.documentScan.OperationResult;
-OptionType = chrome.documentScan.OptionType;
+const OperationResult = chrome.documentScan.OperationResult;
+const OptionType = chrome.documentScan.OptionType;
 
 async function getScannerList(filter) {
   return new Promise(resolve => {
@@ -61,7 +61,7 @@ async function startScan(scannerHandle, maxReadSize) {
       format: 'format',
     };
     if (maxReadSize != undefined) {
-      options['maxReadSize'] = maxReadSize;
+      options.maxReadSize = maxReadSize;
     }
     chrome.documentScan.startScan(scannerHandle, options, resolve);
   });
