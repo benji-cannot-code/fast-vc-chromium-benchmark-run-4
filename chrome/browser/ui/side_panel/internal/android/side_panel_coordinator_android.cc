1e74eb8e51a980bbf696b902459da25705686313
Linyu He <linyuh@google.com>
2026-03-18 12:56:23 -0700

    Side Panel: Modularize SidePanelCoordinatorAndroid-related code
    This CL follows go/how-to-modularize-clank to modularize Android code
    in c/b/ui/side_panel, which will make its integration with
    ChromeActivity/ChromeAndroidTask easier and cleaner.
    
    Bug: 491597112
    Change-Id: I5b014da969afc0519a63791a594377b4434c6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7677567
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Linyu He <linyuh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1601473}

--- chrome/browser/ui/side_panel/internal/android/side_panel_coordinator_android.cc
+++ chrome/browser/ui/side_panel/internal/android/side_panel_coordinator_android.cc
@@ -3,20 +3,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/side_panel/android/side_panel_coordinator_android.h"
+#include "chrome/browser/ui/side_panel/internal/android/side_panel_coordinator_android.h"
 
 #include <jni.h>
 
 #include "base/android/jni_android.h"
 #include "base/android/scoped_java_ref.h"
 #include "base/check.h"
-#include "chrome/browser/ui/side_panel/android/jni_headers/SidePanelCoordinatorAndroidBridge_jni.h"
+#include "chrome/browser/ui/side_panel/internal/android/jni_headers/SidePanelCoordinatorAndroidBridgeImpl_jni.h"
 
 using base::android::JavaRef;
 using base::android::ScopedJavaLocalRef;
 
-// Implements Java `SidePanelCoordinatorAndroidBridge.Natives#create`.
-static int64_t JNI_SidePanelCoordinatorAndroidBridge_Create(
+// Implements Java `SidePanelCoordinatorAndroidBridgeImpl.Natives#create`.
+static int64_t JNI_SidePanelCoordinatorAndroidBridgeImpl_Create(
     JNIEnv* env,
     const JavaRef<jobject>& caller) {
   return reinterpret_cast<intptr_t>(
@@ -29,7 +29,7 @@ SidePanelCoordinatorAndroid::SidePanelCoordinatorAndroid(
     : java_bridge_(env, java_bridge) {}
 
 SidePanelCoordinatorAndroid::~SidePanelCoordinatorAndroid() {
-  Java_SidePanelCoordinatorAndroidBridge_clearNativePtr(
+  Java_SidePanelCoordinatorAndroidBridgeImpl_clearNativePtr(
       base::android::AttachCurrentThread(), java_bridge());
 }
 
@@ -48,4 +48,4 @@ ScopedJavaLocalRef<jobject> SidePanelCoordinatorAndroid::java_bridge() const {
   return local_ref;
 }
 
-DEFINE_JNI(SidePanelCoordinatorAndroidBridge)
+DEFINE_JNI(SidePanelCoordinatorAndroidBridgeImpl)
--- chrome/browser/ui/side_panel/test/android/java/src/org/chromium/chrome/browser/ui/side_panel/SidePanelCoordinatorAndroidNativeUnitTestSupport.java
+++ chrome/browser/ui/side_panel/test/android/java/src/org/chromium/chrome/browser/ui/side_panel/SidePanelCoordinatorAndroidNativeUnitTestSupport.java
@@ -22,11 +22,11 @@ import org.chromium.build.annotations.NullMarked;
  */
 @NullMarked
 final class SidePanelCoordinatorAndroidNativeUnitTestSupport {
-    private final SidePanelCoordinatorAndroidBridge mBridge;
+    private final SidePanelCoordinatorAndroidBridgeImpl mBridge;
 
     @CalledByNativeForTesting
     private SidePanelCoordinatorAndroidNativeUnitTestSupport() {
-        mBridge = new SidePanelCoordinatorAndroidBridge();
+        mBridge = new SidePanelCoordinatorAndroidBridgeImpl();
     }
 
     @CalledByNativeForTesting
