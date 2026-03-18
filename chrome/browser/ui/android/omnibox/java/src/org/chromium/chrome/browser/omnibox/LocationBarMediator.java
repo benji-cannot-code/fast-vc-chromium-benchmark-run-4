a49a4ad77142090eabbb484b168bf88a7d54a783
Sky Malice <skym@chromium.org>
2026-03-18 14:20:02 -0700

    [AIM] Update for request type on begin input.
    We were only resetting the hint if request type changed inside of an
    input session. Now on beginInput, we now reset values.
    
    Bug: 493902557
    Change-Id: I64dc8d0d436934aac011ba9938f0c96b898cec40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7681672
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1601536}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediator.java
@@ -1038,7 +1038,9 @@ class LocationBarMediator
                 });
 
         mCurrentInput = session.getAutocompleteInput();
-        mCurrentInput.getRequestTypeSupplier().addSyncObserver(mAutocompleteRequestTypeObserver);
+        mCurrentInput
+                .getRequestTypeSupplier()
+                .addSyncObserverAndCallIfNonNull(mAutocompleteRequestTypeObserver);
 
         UrlBarData data = UrlBarData.forNonUrlText(mCurrentInput.getUserText());
         mUrlCoordinator.setUrlBarData(
