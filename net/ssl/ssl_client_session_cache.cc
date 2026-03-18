16a9e0f91604c5f5d77deeb2faeaef59c4c93d84
Patrick Monette <pmonette@chromium.org>
2026-03-17 22:13:08 -0700

    [SSLClientSessionCache] Stateful memory pressure behavior behind feature
    In https://crrev.com/c/7224452, SSLClientSessionCache was changed to
    modify the cache max size depending on the memory pressure level.
    
    This change is a partial revert that puts that stateful behavior behind
    the kStatefulMemoryPressure feature flag, to ensure that we can test
    out the behavioral change with an experiment.
    
    Bug: 465068849
    Change-Id: Ifdf7130c46eeab65d3a3efd793251ce1e6783688
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7671598
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1601037}

--- net/ssl/ssl_client_session_cache.cc
+++ net/ssl/ssl_client_session_cache.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/containers/flat_set.h"
+#include "base/memory_coordinator/memory_coordinator_features.h"
 #include "base/time/clock.h"
 #include "base/time/default_clock.h"
 #include "third_party/boringssl/src/include/openssl/ssl.h"
@@ -216,15 +217,29 @@ void SSLClientSessionCache::FlushExpiredSessions() {
 
 void SSLClientSessionCache::OnMemoryPressure(
     base::MemoryPressureLevel memory_pressure_level) {
+  if (base::FeatureList::IsEnabled(base::kStatefulMemoryPressure)) {
+    switch (memory_pressure_level) {
+      case base::MEMORY_PRESSURE_LEVEL_NONE:
+        cache_.UpdateMaxSize(config_.max_entries);
+        break;
+      case base::MEMORY_PRESSURE_LEVEL_MODERATE:
+        cache_.UpdateMaxSize(config_.max_entries / 2);
+        break;
+      case base::MEMORY_PRESSURE_LEVEL_CRITICAL:
+        cache_.UpdateMaxSize(0);
+        break;
+    }
+    return;
+  }
+
   switch (memory_pressure_level) {
     case base::MEMORY_PRESSURE_LEVEL_NONE:
-      cache_.UpdateMaxSize(config_.max_entries);
       break;
     case base::MEMORY_PRESSURE_LEVEL_MODERATE:
-      cache_.UpdateMaxSize(config_.max_entries / 2);
+      FlushExpiredSessions();
       break;
     case base::MEMORY_PRESSURE_LEVEL_CRITICAL:
-      cache_.UpdateMaxSize(0);
+      Flush();
       break;
   }
 }
