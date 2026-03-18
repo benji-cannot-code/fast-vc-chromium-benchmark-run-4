55fe25c6e24d5886909ec2fc5a7d908e2d173f88
Ivan Šandrk <isandrk@chromium.org>
2026-03-18 06:34:27 -0700

    LPP: Remove extraneous comment in setup-pin-keyboard
    No behavioral changes, purely mechanical change.
    
    Bug: 445625494
    Change-Id: If25b447d194e23a6fd2663bf26675b69b5aeb5a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7673944
    Reviewed-by: Fahad Mansoor <fahadmansoor@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Ivan Šandrk <isandrk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1601229}

--- ash/webui/common/resources/quick_unlock/setup_pin_keyboard.ts
+++ ash/webui/common/resources/quick_unlock/setup_pin_keyboard.ts
@@ -414,9 +414,6 @@ export class SetupPinKeyboardElement extends SetupPinKeyboardElementBase {
     return !!problemMessageId && problemClass === ProblemType.ERROR;
   }
 
-  /**
-   * Format problem message
-   */
   private formatProblemMessage_(
       locale: string, messageId: string|undefined,
       messageParameters: string): string {
