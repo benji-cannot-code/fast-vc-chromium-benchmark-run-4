e4fdfac4d7840c9c0b71c0316a04fdbcb4697b80
Erik Chen <erikchen@chromium.org>
2026-03-18 10:33:33 -0700

    delete TextSelectionChanged user action
    no longer used
    
    Change-Id: If8e10ef8d9754fd5d41b0a633db03865beeaa697
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7673838
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1601388}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -9327,7 +9327,6 @@ void RenderFrameHostImpl::FocusedElementChanged(
 void RenderFrameHostImpl::TextSelectionChanged(const std::u16string& text,
                                                uint32_t offset,
                                                const gfx::Range& range) {
-  RecordAction(base::UserMetricsAction("TextSelectionChanged"));
   has_selection_ = !text.empty();
   GetRenderWidgetHost()->SelectionChanged(text, offset, range);
 
