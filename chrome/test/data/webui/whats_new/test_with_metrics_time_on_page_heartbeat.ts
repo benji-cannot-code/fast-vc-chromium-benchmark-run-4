a291d13ee3ed032fd8843b485c21143a901d1d31
Mickey Burks <mickeyburks@chromium.org>
2026-03-18 06:13:49 -0700

    [What's New] Add TimeOnPageHeartbeat metric browser test
    Fixed: 477692869
    Change-Id: I6179ffde862133ca5605aff02094be0ce5dca03e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7675310
    Reviewed-by: Vince Lugli <lugli@google.com>
    Commit-Queue: Mickey Burks <mickeyburks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1601216}

---
+++ chrome/test/data/webui/whats_new/test_with_metrics_time_on_page_heartbeat.ts
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {assertTrue} from 'chrome://webui-test/chai_assert.js';
+
+window.onload = function() {
+  assertTrue(!!window.top);
+
+  // Total time spent on page as a heartbeat.
+  window.top.postMessage(
+      {
+        data: {
+          event: 'time_on_page_heartbeat_ms',
+          time: 3000,  // milliseconds
+        },
+      },
+      'chrome://whats-new/');
+
+  // Send another event to signal that the heartbeat has been processed.
+  window.top.postMessage(
+      {
+        data: {
+          event: 'cta_click',
+        },
+      },
+      'chrome://whats-new/');
+};
