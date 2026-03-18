375fd53ff505cec3aef8b7483d304e5d4daab66c
Mitsuru Oshima <oshima@chromium.org>
2026-03-18 00:50:18 -0700

    Revert "Migrate ChromeOS-exclusive switches from chrome/browser to ash/constants"
    This reverts commit 4502e56b64fc1770a3e02cbe7f6a27d5410b4ce0.
    
    Reason for revert: These switches should be moved when the
     directories are moved.
    
    Original change's description:
    > Migrate ChromeOS-exclusive switches from chrome/browser to ash/constants
    >
    > This stage migrates the ChromeOS-exclusive
    > switches scattered under chrome/browser:
    >
    > kCroshCommand[];
    > kTestName[];
    > kNearbyShareCertificateValidityPeriodHours[];
    > kNearbyShareDeviceID[];
    > kNearbyShareHTTPHost[];
    > kNearbyShareNumPrivateCertificates[];
    >
    > Bug: 434082728
    > Change-Id: I65ae5b950b93c499125ffa6a03a169d6455c2b7b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7660440
    > Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    > Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1598952}
    
    Bypass-Check-License: revert
    Bug: 491803262
    Change-Id: I8d122d7e0accab01eae2e3f17f3c9d7a8d2d05c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7672297
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1601089}

--- chrome/browser/nearby_sharing/local_device_data/nearby_share_local_device_data_manager_impl.cc
+++ chrome/browser/nearby_sharing/local_device_data/nearby_share_local_device_data_manager_impl.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 
-#include "ash/constants/ash_switches.h"
 #include "base/command_line.h"
 #include "base/functional/bind.h"
 #include "base/memory/ptr_util.h"
@@ -17,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/nearby_sharing/common/nearby_share_prefs.h"
+#include "chrome/browser/nearby_sharing/common/nearby_share_switches.h"
 #include "chrome/browser/nearby_sharing/local_device_data/nearby_share_device_data_updater.h"
 #include "chrome/browser/nearby_sharing/local_device_data/nearby_share_device_data_updater_impl.h"
 #include "chrome/grit/generated_resources.h"
@@ -116,8 +116,8 @@ std::string NearbyShareLocalDeviceDataManagerImpl::GetId() {
     return id;
 
   base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
-  if (command_line->HasSwitch(ash::switches::kNearbyShareDeviceID)) {
-    id = command_line->GetSwitchValueASCII(ash::switches::kNearbyShareDeviceID);
+  if (command_line->HasSwitch(switches::kNearbyShareDeviceID)) {
+    id = command_line->GetSwitchValueASCII(switches::kNearbyShareDeviceID);
   } else {
     for (size_t i = 0; i < kDeviceIdLength; ++i)
       id += kAlphaNumericChars[base::RandGenerator(kAlphaNumericChars.size())];
