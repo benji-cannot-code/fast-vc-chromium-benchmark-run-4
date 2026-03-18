706eefa12321c3ba50480cf94ee9d68f89ca9a76
Andrey Kosyakov <caseq@chromium.org>
2026-03-18 11:39:38 -0700

    [bindings] Perform detach check when passing span instead of retaining underlying array buffer
    This CL renames and repurposes kSupportReentry that used to retain the underlying storage for an array buffer so that we now check if an AB has been detached and return an empty span in such case, as the previous behavior turned out to be not spec compliant.
    
    Also use this this for fall-back generated bindings of [NoAllocDirectCall] operations.
    
    Fixed: 490810422, 489325431
    Change-Id: Iff55e62f06655796d1e73d01430af73377e704c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7677065
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1601433}

--- third_party/blink/renderer/bindings/core/v8/pass_as_span.h
+++ third_party/blink/renderer/bindings/core/v8/pass_as_span.h
@@ -16,7 +16,7 @@ namespace blink {
 
 namespace bindings::internal {
 
-template <bool kSupportReentry>
+template <bool kPerformDetachCheck>
 class CORE_EXPORT ByteSpanWithInlineStorage {
   STACK_ALLOCATED();
 
@@ -33,18 +33,16 @@ class CORE_EXPORT ByteSpanWithInlineStorage {
       span_ = span.first(r.span_.size());
     } else {
       span_ = r.span_;
-      backing_store_ = r.backing_store_;
+      orig_buffer_for_detach_check_ = r.orig_buffer_for_detach_check_;
     }
     return *this;
   }
 
   void Assign(base::span<const uint8_t> span) { span_ = span; }
   void Assign(v8::MemorySpan<const uint8_t> span) { span_ = span; }
-  void MaybeSetBackingStore(v8::Local<v8::ArrayBuffer> array_buffer) {
-    if constexpr (kSupportReentry) {
-      if (array_buffer->IsDetachable()) {
-        backing_store_ = array_buffer->GetBackingStore();
-      }
+  void MaybeSetArrayBuffer(v8::Local<v8::ArrayBuffer> array_buffer) {
+    if constexpr (kPerformDetachCheck) {
+      orig_buffer_for_detach_check_ = array_buffer;
     }
   }
 
@@ -52,9 +50,17 @@ class CORE_EXPORT ByteSpanWithInlineStorage {
   // class tightly coupled to the bindings generator that knows how to use it.
   // Note rvalue conversion is explicitly disabled.
   // NOLINTNEXTLINE(google-explicit-constructor)
-  operator base::span<const uint8_t>() const& { return span_; }
+  operator base::span<const uint8_t>() const& { return as_span(); }
   operator base::span<const uint8_t>() const&& = delete;
-  const base::span<const uint8_t> as_span() const { return span_; }
+  const base::span<const uint8_t> as_span() const {
+    if constexpr (kPerformDetachCheck) {
+      if (!orig_buffer_for_detach_check_.IsEmpty() &&
+          orig_buffer_for_detach_check_->WasDetached()) {
+        return {};
+      }
+    }
+    return span_;
+  }
 
   v8::MemorySpan<uint8_t> GetInlineStorage() { return inline_storage_; }
 
@@ -62,8 +68,8 @@ class CORE_EXPORT ByteSpanWithInlineStorage {
   base::span<const uint8_t> span_;
   uint8_t inline_storage_[kInlineStorageSize];
   struct Void {};
-  std::conditional_t<kSupportReentry, std::shared_ptr<v8::BackingStore>, Void>
-      backing_store_;
+  std::conditional_t<kPerformDetachCheck, v8::Local<v8::ArrayBuffer>, Void>
+      orig_buffer_for_detach_check_;
 };
 
 template <typename T>
@@ -73,7 +79,7 @@ v8::MemorySpan<const uint8_t> GetArrayData(v8::Local<T> array) {
       static_cast<const uint8_t*>(array->Data()), array->ByteLength());
 }
 
-template <typename T, bool kSupportReentry>
+template <typename T, bool kPerformDetachCheck>
 class SpanWithInlineStorage {
   STACK_ALLOCATED();
 
@@ -91,8 +97,8 @@ class SpanWithInlineStorage {
                                      bytes.size() / sizeof(T)));
   }
 
-  void MaybeSetBackingStore(v8::Local<v8::ArrayBuffer> array_buffer) {
-    bytes_.MaybeSetBackingStore(array_buffer);
+  void MaybeSetArrayBuffer(v8::Local<v8::ArrayBuffer> array_buffer) {
+    bytes_.MaybeSetArrayBuffer(array_buffer);
   }
   void Assign(base::span<const uint8_t> span) { bytes_.Assign(span); }
   v8::MemorySpan<uint8_t> GetInlineStorage() {
@@ -100,10 +106,10 @@ class SpanWithInlineStorage {
   }
 
  private:
-  ByteSpanWithInlineStorage<kSupportReentry> bytes_;
+  ByteSpanWithInlineStorage<kPerformDetachCheck> bytes_;
 };
 
-template <typename T, bool kSupportReentry>
+template <typename T, bool kPerformDetachCheck>
 class SpanOrVector {
   STACK_ALLOCATED();
 
@@ -115,8 +121,8 @@ class SpanOrVector {
   operator base::span<const T>() const&& = delete;
   const base::span<const T> as_span() const { return span_.as_span(); }
 
-  void MaybeSetBackingStore(v8::Local<v8::ArrayBuffer> array_buffer) {
-    span_.MaybeSetBackingStore(array_buffer);
+  void MaybeSetArrayBuffer(v8::Local<v8::ArrayBuffer> array_buffer) {
+    span_.MaybeSetArrayBuffer(array_buffer);
   }
   void Assign(base::span<const uint8_t> span) { span_.Assign(span); }
   void Assign(Vector<T> vec) {
@@ -136,7 +142,7 @@ class SpanOrVector {
   }
 
  private:
-  SpanWithInlineStorage<T, kSupportReentry> span_;
+  SpanWithInlineStorage<T, kPerformDetachCheck> span_;
   Vector<T> vector_;
 };
 
@@ -196,7 +202,7 @@ struct PassAsSpanMarkerBase {
     kNone,
     kAllowShared = 1 << 0,
     kAllowSequence = 1 << 1,
-    kSupportReentry = 1 << 2,
+    kPerformDetachCheck = 1 << 2,
   };
 };
 
@@ -212,7 +218,8 @@ template <PassAsSpanMarkerBase::Flags flags =
 struct PassAsSpan : public PassAsSpanMarkerBase {
   static constexpr bool allow_shared = flags & Flags::kAllowShared;
   static constexpr bool allow_sequence = flags & Flags::kAllowSequence;
-  static constexpr bool support_reentry = flags & Flags::kSupportReentry;
+  static constexpr bool perform_detach_check =
+      flags & Flags::kPerformDetachCheck;
 
   static constexpr bool is_typed = !std::is_same_v<T, void>;
 
@@ -221,11 +228,11 @@ struct PassAsSpan : public PassAsSpanMarkerBase {
   using ElementType = T;
   using ReturnType = std::conditional_t<
       allow_sequence,
-      bindings::internal::SpanOrVector<T, support_reentry>,
+      bindings::internal::SpanOrVector<T, perform_detach_check>,
       std::conditional_t<
           is_typed,
-          bindings::internal::SpanWithInlineStorage<T, support_reentry>,
-          bindings::internal::ByteSpanWithInlineStorage<support_reentry>>>;
+          bindings::internal::SpanWithInlineStorage<T, perform_detach_check>,
+          bindings::internal::ByteSpanWithInlineStorage<perform_detach_check>>>;
 };
 
 }  // namespace blink
