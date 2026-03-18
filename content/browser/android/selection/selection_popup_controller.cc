d2471400499be507f91cd0bc1b4e7a33aa2b7b43
Calder Kitagawa <ckitagawa@chromium.org>
2026-03-18 06:50:42 -0700

    Revert "SelectionPopupController remove global ref"
    This reverts commit 80f9d7968533ee61e337de7ee03184b4a3ef935e.
    
    Reason for revert: speculative revert for crbug.com/493087298
    
    Original change's description:
    > SelectionPopupController remove global ref
    >
    > There is a finite table of 51200 global refs (of each of weak and
    > strong). This doesn't scale well to unlimited WebContents. Address this
    > by fetching the java object off the WebContents each time.
    >
    > Bug: 484619649
    > Change-Id: I3a46c8592a1624ce6c9a7bcf9011bce076dfbbe8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7633824
    > Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    > Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    > Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1594597}
    
    Bug: 484619649, 493087298
    Bug: 484619649
    Change-Id: I82df1f7ea2517ec6b8626b8db3b39ec0d4cf94c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7676159
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1601245}

--- content/browser/android/selection/selection_popup_controller.cc
+++ content/browser/android/selection/selection_popup_controller.cc
@@ -76,24 +76,27 @@ JNI_SelectionPopupControllerImpl_IsMagnifierWithSurfaceControlSupported(
 
 static int64_t JNI_SelectionPopupControllerImpl_Init(
     JNIEnv* env,
-    WebContents* web_contents) {
+    const JavaRef<jobject>& obj,
+    const JavaRef<jobject>& jweb_contents) {
+  WebContents* web_contents = WebContents::FromJavaWebContents(jweb_contents);
   DCHECK(web_contents);
 
   // Owns itself and gets destroyed when |WebContentsDestroyed| is called.
-  auto* controller = new SelectionPopupController(web_contents);
+  auto* controller = new SelectionPopupController(env, obj, web_contents);
   controller->Initialize();
   return reinterpret_cast<intptr_t>(controller);
 }
 
 SelectionPopupController* SelectionPopupController::FromWebContents(
     WebContents& web_contents) {
+  ScopedJavaLocalRef<jobject> jweb_contents = web_contents.GetJavaWebContents();
   JNIEnv* env = AttachCurrentThread();
   // Call the Java-side fromWebContents method. This gets the Java
   // SelectionPopupController if it already exists. Otherwise, this will
   // instantiate the Java SelectionPopupController and store it in the web
   // contents.
   ScopedJavaLocalRef<jobject> jselection_popup_controller =
-      Java_SelectionPopupControllerImpl_fromWebContents(env, &web_contents);
+      Java_SelectionPopupControllerImpl_fromWebContents(env, jweb_contents);
   // Then get the native pointer from the newly-created
   // SelectionPopupController. The Java SelectionPopupController owns the C++
   // SelectionPopupController.
@@ -104,34 +107,31 @@ SelectionPopupController* SelectionPopupController::FromWebContents(
       selection_popup_controller);
 }
 
-SelectionPopupController::SelectionPopupController(WebContents* web_contents)
-    : RenderWidgetHostConnector(web_contents) {}
+SelectionPopupController::SelectionPopupController(JNIEnv* env,
+                                                   const JavaRef<jobject>& obj,
+                                                   WebContents* web_contents)
+    : RenderWidgetHostConnector(web_contents) {
+  java_obj_ = JavaObjectWeakGlobalRef(env, obj);
+}
 
 SelectionPopupController::~SelectionPopupController() {
   JNIEnv* env = AttachCurrentThread();
-  ScopedJavaLocalRef<jobject> obj = GetJavaObject(env);
+  ScopedJavaLocalRef<jobject> obj = java_obj_.get(env);
   if (!obj.is_null()) {
     Java_SelectionPopupControllerImpl_nativeSelectionPopupControllerDestroyed(
         env, obj);
   }
 }
 
-ScopedJavaLocalRef<jobject> SelectionPopupController::GetContext(
-    JNIEnv* env) const {
-  ScopedJavaLocalRef<jobject> obj = GetJavaObject(env);
+ScopedJavaLocalRef<jobject> SelectionPopupController::GetContext() const {
+  JNIEnv* env = AttachCurrentThread();
+
+  ScopedJavaLocalRef<jobject> obj = java_obj_.get(env);
   if (obj.is_null()) {
     return nullptr;
   }
 
   return Java_SelectionPopupControllerImpl_getContext(env, obj);
-}
-
-ScopedJavaLocalRef<jobject> SelectionPopupController::GetJavaObject(
-    JNIEnv* env) const {
-  WebContents* contents = web_contents();
-  CHECK(contents);
-  return Java_SelectionPopupControllerImpl_fromWebContentsNoCreate(env,
-                                                                   contents);
 }
 
 void SelectionPopupController::SetTextHandlesHiddenForDropdownMenu(
@@ -176,8 +176,7 @@ std::unique_ptr<ui::TouchHandleDrawable>
 SelectionPopupController::CreateTouchHandleDrawable(
     gfx::NativeView parent_native_view,
     cc::slim::Layer* parent_layer) {
-  JNIEnv* env = AttachCurrentThread();
-  ScopedJavaLocalRef<jobject> activityContext = GetContext(env);
+  ScopedJavaLocalRef<jobject> activityContext = GetContext();
   // If activityContext is null then Application context is used instead on
   // the java side in CompositedTouchHandleDrawable.
   return std::make_unique<CompositedTouchHandleDrawable>(
@@ -221,7 +220,7 @@ void SelectionPopupController::UpdateRenderProcessConnection(
     return;
   }
   JNIEnv* env = AttachCurrentThread();
-  ScopedJavaLocalRef<jobject> obj = GetJavaObject(env);
+  ScopedJavaLocalRef<jobject> obj = java_obj_.get(env);
   if (obj.is_null()) {
     return;
   }
@@ -233,7 +232,7 @@ void SelectionPopupController::OnSelectionEvent(
     ui::SelectionEventType event,
     const gfx::RectF& selection_rect) {
   JNIEnv* env = AttachCurrentThread();
-  ScopedJavaLocalRef<jobject> obj = GetJavaObject(env);
+  ScopedJavaLocalRef<jobject> obj = java_obj_.get(env);
   if (obj.is_null()) {
     return;
   }
@@ -247,7 +246,7 @@ void SelectionPopupController::OnDragUpdate(
     const ui::TouchSelectionDraggable::Type type,
     const gfx::PointF& position) {
   JNIEnv* env = AttachCurrentThread();
-  ScopedJavaLocalRef<jobject> obj = GetJavaObject(env);
+  ScopedJavaLocalRef<jobject> obj = java_obj_.get(env);
   if (obj.is_null()) {
     return;
   }
@@ -258,7 +257,7 @@ void SelectionPopupController::OnDragUpdate(
 
 void SelectionPopupController::OnSelectionChanged(const std::string& text) {
   JNIEnv* env = AttachCurrentThread();
-  ScopedJavaLocalRef<jobject> obj = GetJavaObject(env);
+  ScopedJavaLocalRef<jobject> obj = java_obj_.get(env);
   if (obj.is_null()) {
     return;
   }
@@ -271,7 +270,7 @@ bool SelectionPopupController::ShowSelectionMenu(
     const ContextMenuParams& params,
     int handle_height) {
   JNIEnv* env = AttachCurrentThread();
-  ScopedJavaLocalRef<jobject> obj = GetJavaObject(env);
+  ScopedJavaLocalRef<jobject> obj = java_obj_.get(env);
   if (obj.is_null()) {
     return false;
   }
@@ -339,7 +338,7 @@ void SelectionPopupController::OnSelectAroundCaretAck(
     int surroundingTextLength,
     blink::mojom::SelectAroundCaretResultPtr result) {
   JNIEnv* env = AttachCurrentThread();
-  ScopedJavaLocalRef<jobject> obj = GetJavaObject(env);
+  ScopedJavaLocalRef<jobject> obj = java_obj_.get(env);
   if (obj.is_null()) {
     return;
   }
@@ -356,7 +355,7 @@ void SelectionPopupController::OnSelectAroundCaretAck(
 
 void SelectionPopupController::HidePopupsAndPreserveSelection() {
   JNIEnv* env = AttachCurrentThread();
-  ScopedJavaLocalRef<jobject> obj = GetJavaObject(env);
+  ScopedJavaLocalRef<jobject> obj = java_obj_.get(env);
   if (obj.is_null()) {
     return;
   }
@@ -368,7 +367,7 @@ void SelectionPopupController::HidePopupsAndPreserveSelection() {
 
 void SelectionPopupController::RestoreSelectionPopupsIfNecessary() {
   JNIEnv* env = AttachCurrentThread();
-  ScopedJavaLocalRef<jobject> obj = GetJavaObject(env);
+  ScopedJavaLocalRef<jobject> obj = java_obj_.get(env);
   if (obj.is_null()) {
     return;
   }
@@ -377,7 +376,7 @@ void SelectionPopupController::RestoreSelectionPopupsIfNecessary() {
 
 void SelectionPopupController::ChildLocalSurfaceIdChanged() {
   JNIEnv* env = AttachCurrentThread();
-  ScopedJavaLocalRef<jobject> obj = GetJavaObject(env);
+  ScopedJavaLocalRef<jobject> obj = java_obj_.get(env);
   if (obj.is_null()) {
     return;
   }
--- content/public/android/java/src/org/chromium/content/browser/selection/SelectionPopupControllerImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/selection/SelectionPopupControllerImpl.java
@@ -39,7 +39,6 @@ import androidx.annotation.VisibleForTesting;
 
 import org.jni_zero.CalledByNative;
 import org.jni_zero.JNINamespace;
-import org.jni_zero.JniType;
 import org.jni_zero.NativeMethods;
 
 import org.chromium.base.ApiCompatibilityUtils;
@@ -268,8 +267,7 @@ public class SelectionPopupControllerImpl extends ActionModeCallbackHelper
      *     #create()} is not called yet.
      */
     @CalledByNative
-    public static @Nullable SelectionPopupControllerImpl fromWebContents(
-            @JniType("content::WebContents*") WebContents webContents) {
+    public static @Nullable SelectionPopupControllerImpl fromWebContents(WebContents webContents) {
         return webContents.getOrSetUserData(
                 SelectionPopupControllerImpl.class, UserDataFactoryLazyHolder.INSTANCE);
     }
@@ -281,9 +279,8 @@ public class SelectionPopupControllerImpl extends ActionModeCallbackHelper
      * @param webContents {@link WebContents} object.
      * @return {@link SelectionPopupController} object. {@code null} if not available.
      */
-    @CalledByNative
     public static @Nullable SelectionPopupControllerImpl fromWebContentsNoCreate(
-            @JniType("content::WebContents*") WebContents webContents) {
+            WebContents webContents) {
         return webContents.getOrSetUserData(SelectionPopupControllerImpl.class, null);
     }
 
@@ -362,7 +359,7 @@ public class SelectionPopupControllerImpl extends ActionModeCallbackHelper
         }
         if (initializeNative) {
             mNativeSelectionPopupController =
-                    SelectionPopupControllerImplJni.get().init(mWebContents);
+                    SelectionPopupControllerImplJni.get().init(this, mWebContents);
             ImeAdapterImpl imeAdapter = ImeAdapterImpl.fromWebContents(mWebContents);
             if (imeAdapter != null) imeAdapter.addEventObserver(this);
         }
@@ -1980,7 +1977,7 @@ public class SelectionPopupControllerImpl extends ActionModeCallbackHelper
     interface Natives {
         boolean isMagnifierWithSurfaceControlSupported();
 
-        long init(@JniType("content::WebContents*") WebContents webContents);
+        long init(SelectionPopupControllerImpl self, WebContents webContents);
 
         void setTextHandlesTemporarilyHidden(long nativeSelectionPopupController, boolean hidden);
 
