d06012e2b72b3127d5217ffd09fcdce5db5f64d0
Finnur Thorarinsson <finnur@chromium.org>
2026-03-18 11:21:18 -0700

    [Sheriff] Avoid error log spew frequently seen in tests.
    ... and not just failing/flaky tests but passing ones also.
    
    This looks like an unexpected but recoverable situation, so
    it should not be logged as an error. It just creates a red
    herring for people trying to investigate test failures.
    
    Bug: None
    Change-Id: I194f10aa8898814fe47c0f3bb9d4c948bc305b08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7679872
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Auto-Submit: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1601424}

--- third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.cc
@@ -1085,7 +1085,7 @@ AXObject* AXObjectCacheImpl::FocusedObject() const {
     // gets trimmed.
     // In these cases, treat the focus as on the root object itself, so that
     // AT users have some starting point.
-    DLOG(ERROR) << "The focus was not part of the a11y tree: " << FocusedNode();
+    DLOG(INFO) << "The focus was not part of the a11y tree: " << FocusedNode();
     return Get(document_);
   }
 
