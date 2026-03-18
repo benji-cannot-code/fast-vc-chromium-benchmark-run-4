06c0cef117271a4a38947c724d72b291295dbba6
Ritika Gupta <ritikagup@google.com>
2026-03-18 16:09:23 -0700

    [Actor] Cleanup ActorIntentConstants.
    In this CL, using NotificationConstants instead of duplicating them in
    ActorIntentConstants.
    
    Bug: 493671305
    Change-Id: Ic9c6f059a3ea81fb3163209741cdeef438d5da78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7676904
    Auto-Submit: Ritika Gupta <ritikagup@google.com>
    Reviewed-by: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1601610}

--- chrome/browser/actor/android/java/src/org/chromium/chrome/browser/actor/ActorPictureInPictureControllerTest.java
+++ chrome/browser/actor/android/java/src/org/chromium/chrome/browser/actor/ActorPictureInPictureControllerTest.java
@@ -36,6 +36,7 @@ import org.robolectric.annotation.Config;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.chrome.browser.actor.ui.ActorPictureInPictureOverlayCoordinator;
 import org.chromium.chrome.browser.actor.ui.R;
+import org.chromium.chrome.browser.notifications.NotificationConstants;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.profiles.ProfileResolver;
 import org.chromium.chrome.browser.profiles.ProfileResolverJni;
@@ -202,7 +203,7 @@ public class ActorPictureInPictureControllerTest {
 
         assertEquals(action, intent.getAction());
         assertEquals(mActivity.getPackageName(), intent.getPackage());
-        assertEquals(taskId, intent.getIntExtra(ActorIntentConstants.EXTRA_TASK_ID, -1));
+        assertEquals(taskId, intent.getIntExtra(NotificationConstants.EXTRA_ACTOR_TASK_ID, -1));
     }
 
     @Test
