364b04cabb6a8a81eceafa2d26ab76a255bc2d7b
Arthur Milchior <arthurmilchior@google.com>
2026-03-18 06:28:32 -0700

    [iOS]Remove openAccountsListFromSettings
    This is part of the sequence:
    * crrev.com/c/7670900
    * crrev.com/i/9108698
    + crrev.com/c/7671080
    
    Bug: None
    Change-Id: Ia9bef2fe4e9cb4d761a5adcc0f66529b1774e4a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7671080
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Menghan Yang <myuu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1601222}

--- ios/chrome/browser/authentication/test/signin_earl_grey_ui_test_util.h
+++ ios/chrome/browser/authentication/test/signin_earl_grey_ui_test_util.h
@@ -93,7 +93,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 + (void)openSyncSettings;
 
 // Opens settings > identity > Manage accounts on this device.
-+ (void)openAccountsListFromSettings;
 + (void)openManageAccountsFromSettings;
 
 @end
--- ios/chrome/browser/settings/ui_bundled/google_services/manage_sync_settings_egtest.mm
+++ ios/chrome/browser/settings/ui_bundled/google_services/manage_sync_settings_egtest.mm
@@ -1728,7 +1728,7 @@ void ExpectBatchUploadConfirmationSnackbar(int count, NSString* email) {
       [FakeSystemIdentity fakeManagedIdentity];
   [SigninEarlGrey
       signinWithFakeManagedIdentityInPersonalProfile:managedIdentity];
-  [SigninEarlGreyUI openAccountsListFromSettings];
+  [SigninEarlGreyUI openManageAccountsFromSettings];
 
   [[EarlGrey selectElementWithMatcher:grey_accessibilityID(
                                           kSettingsEditAccountListTableViewId)]
