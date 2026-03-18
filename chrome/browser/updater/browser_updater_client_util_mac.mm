e2f2ed0a426261f8ae3738bac1f00eb47ebb0184
Joshua Pawlicki <waffles@chromium.org>
2026-03-18 08:20:24 -0700

    uma: Record calls of SetUpSystemUpdater
    Bug: 487960705
    Change-Id: Idb3129e3db34e57e449441a1abc22932c1398411
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7675557
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1601297}

--- chrome/browser/updater/browser_updater_client_util_mac.mm
+++ chrome/browser/updater/browser_updater_client_util_mac.mm
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/authorization_util.h"
 #include "base/mac/scoped_authorizationref.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/process/launch.h"
 #include "base/process/process.h"
 #include "base/strings/strcat.h"
@@ -310,6 +311,8 @@ void EnsureUpdater(base::TaskPriority priority,
 }
 
 void SetUpSystemUpdater() {
+  base::UmaHistogramBoolean("GoogleUpdate.MacOS.SetUpSystemUpdaterCalled",
+                            true);
   NSString* prompt = l10n_util::GetNSStringFWithFixup(
       IDS_PROMOTE_AUTHENTICATION_PROMPT,
       l10n_util::GetStringUTF16(IDS_PRODUCT_NAME));
