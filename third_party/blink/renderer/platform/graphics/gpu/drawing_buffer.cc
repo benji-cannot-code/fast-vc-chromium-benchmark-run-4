659df08b9586b5d5b7f6a19024e4bd8a5cccca76
Colin Blundell <blundell@chromium.org>
2026-03-19 01:10:19 -0700

    [Blink] Remove DrawingBuffer::low_latency_enabled()
    This method is not necessary.
    
    Bug: 488937356
    Change-Id: I727cc82fe280da88989152d076b6312a343fd45f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7678691
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1601806}

--- third_party/blink/renderer/platform/graphics/gpu/drawing_buffer.cc
+++ third_party/blink/renderer/platform/graphics/gpu/drawing_buffer.cc
@@ -931,7 +931,7 @@ bool DrawingBuffer::Initialize(const gfx::Size& size, bool use_multisampling) {
   // D3D11 texture backing the back buffer is single-sampled.
   supports_implicit_resolve =
       supports_implicit_resolve &&
-      !(low_latency_enabled() &&
+      !(low_latency_enabled_ &&
         SharedGpuContext::LowLatencyUsageSupportedForWebGL(
             ContextProvider()->SharedImageInterface()));
 #endif
@@ -2003,7 +2003,7 @@ scoped_refptr<DrawingBuffer::ColorBuffer> DrawingBuffer::CreateColorBuffer(
   // First see if creating a SharedImage that can be used as an overlay is
   // feasible.
 #if BUILDFLAG(IS_WIN)
-  if (low_latency_enabled() &&
+  if (low_latency_enabled_ &&
       SharedGpuContext::LowLatencyUsageSupportedForWebGL(sii)) {
     usage = usage | gpu::SHARED_IMAGE_USAGE_SCANOUT;
     usage = usage | gpu::SHARED_IMAGE_USAGE_CONCURRENT_READ_WRITE;
@@ -2027,7 +2027,7 @@ scoped_refptr<DrawingBuffer::ColorBuffer> DrawingBuffer::CreateColorBuffer(
        base::FeatureList::IsEnabled(kAllowOverlaysForOffscreenCanvas))) {
     use_as_overlay = SharedGpuContext::UseOverlaysForWebGL();
     low_latency_usage_supported =
-        low_latency_enabled() &&
+        low_latency_enabled_ &&
         SharedGpuContext::LowLatencyUsageSupportedForWebGL(sii);
   }
   if (use_as_overlay || low_latency_usage_supported) {
