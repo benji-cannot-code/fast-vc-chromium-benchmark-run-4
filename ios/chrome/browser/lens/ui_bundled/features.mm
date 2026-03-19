6eb4f30b39dacce585fff0a4b04b9953ca7904e2
Jonathan Lee <jonathanjlee@google.com>
2026-03-19 02:56:19 -0700

    [code-health] Clean up `lens-web-page-load-optimization-enabled` flag
    * This flag expired in M140.
    * The `base::Feature` isn't listed in
      go/stale-base-features-exceptions.
    * No Finch configs appear to control the `base::Feature`.
    
    Fixed: 427495858
    Change-Id: I26efaca6733e589085048f9556335037b533ed7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7654864
    Reviewed-by: Radu Nitescu <radunitescu@google.com>
    Auto-Submit: Jonathan Lee <jonathanjlee@google.com>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1601842}

--- ios/chrome/browser/lens/ui_bundled/features.mm
+++ ios/chrome/browser/lens/ui_bundled/features.mm
@@ -35,9 +35,6 @@ int LensTranslateToggleMode() {
   return kLensTranslateToggleModeFeature.Get();
 }
 
-BASE_FEATURE(kLensWebPageLoadOptimizationEnabled,
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
 BASE_FEATURE(kLensUnaryApisWithHttpTransportEnabled,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
