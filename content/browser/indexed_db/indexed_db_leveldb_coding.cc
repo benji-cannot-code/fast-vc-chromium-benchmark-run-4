ea22845c95e4153d7c70ae1caaf5947a66f0adee
Evan Stade <evanstade@microsoft.com>
2026-03-19 14:27:23 -0700

    IDB: handle unexpected bytes on disk more gracefully
    Bug: 492805140
    Change-Id: I37a4d63ce3505498a2fc9f16a7f9cd9b74b9296f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7676019
    Reviewed-by: Evan Stade <evanstade@microsoft.com>
    Commit-Queue: Evan Stade <evanstade@microsoft.com>
    Reviewed-by: Steve Becker <stevebe@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1602238}

--- content/browser/indexed_db/indexed_db_leveldb_coding.cc
+++ content/browser/indexed_db/indexed_db_leveldb_coding.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <iterator>
 #include <limits>
 #include <list>
+#include <optional>
 #include <sstream>
 #include <string>
 #include <string_view>
@@ -1063,7 +1064,8 @@ bool ExtractEncodedIDBKey(std::string_view* slice, std::string* result) {
   return true;
 }
 
-static blink::mojom::IDBKeyType KeyTypeByteToKeyType(unsigned char type) {
+static std::optional<blink::mojom::IDBKeyType> KeyTypeByteToKeyType(
+    unsigned char type) {
   switch (type) {
     case kIndexedDBKeyNullTypeByte:
       return blink::mojom::IDBKeyType::Invalid;
@@ -1079,10 +1081,9 @@ static blink::mojom::IDBKeyType KeyTypeByteToKeyType(unsigned char type) {
       return blink::mojom::IDBKeyType::Number;
     case kIndexedDBKeyMinKeyTypeByte:
       return blink::mojom::IDBKeyType::Min;
+    default:
+      return std::nullopt;
   }
-
-  DUMP_WILL_BE_NOTREACHED() << "Got invalid type " << type;
-  return blink::mojom::IDBKeyType::Invalid;
 }
 
 int CompareEncodedStringsWithLength(std::string_view* slice1,
@@ -1184,9 +1185,17 @@ int CompareEncodedIDBKeys(std::string_view* slice_a,
   slice_a->remove_prefix(1);
   slice_b->remove_prefix(1);
 
-  if (int x = CompareTypes(KeyTypeByteToKeyType(type_a),
-                           KeyTypeByteToKeyType(type_b)))
+  std::optional<blink::mojom::IDBKeyType> key_type_a =
+      KeyTypeByteToKeyType(type_a);
+  std::optional<blink::mojom::IDBKeyType> key_type_b =
+      KeyTypeByteToKeyType(type_b);
+  if (!key_type_a || !key_type_b) {
+    *ok = false;
+    return 0;
+  }
+  if (int x = CompareTypes(*key_type_a, *key_type_b)) {
     return x;
+  }
 
   switch (type_a) {
     case kIndexedDBKeyNullTypeByte:
