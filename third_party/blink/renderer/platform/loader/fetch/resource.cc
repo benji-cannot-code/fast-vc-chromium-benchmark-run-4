365e6b29d85034898ba53e535564d74800f0409c
Mike West <mkwst@chromium.org>
2026-03-19 03:34:41 -0700

    [Unencoded-Digest] Drop the runtime flag.
    We shipped this along with signature-based integrity checks in 2025Q3.
    I think it's going to stick. This CL removes the flag.
    
    Bug: 375343417
    Change-Id: I1a2db1cc0947711fb5a2842535b75ac692280675
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7660919
    Commit-Queue: Mike West <mkwst@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1601866}

--- third_party/blink/renderer/platform/loader/fetch/resource.cc
+++ third_party/blink/renderer/platform/loader/fetch/resource.cc
@@ -202,10 +202,7 @@ void Resource::CheckResourceIntegrity() {
 
   // Check `Unencoded-Digest` headers. If the digest doesn't match, fail.
   // Otherwise, fall through to validating SRI.
-  const FeatureContext* feature_context =
-      loader_ ? loader_->GetFeatureContext() : nullptr;
-  if (RuntimeEnabledFeatures::UnencodedDigestEnabled(feature_context) &&
-      !SubresourceIntegrity::CheckUnencodedDigests(
+  if (!SubresourceIntegrity::CheckUnencodedDigests(
           GetResponse().GetUnencodedDigests(), Data())) {
     integrity_disposition_ =
         ResourceIntegrityDisposition::kFailedUnencodedDigest;
@@ -215,6 +212,9 @@ void Resource::CheckResourceIntegrity() {
          "not match the resource's body."}));
     return;
   }
+
+  const FeatureContext* feature_context =
+      loader_ ? loader_->GetFeatureContext() : nullptr;
 
   HashMap<HashAlgorithm, String> integrity_hashes;
   bool is_cors_same_origin = response_.IsCorsSameOrigin();
