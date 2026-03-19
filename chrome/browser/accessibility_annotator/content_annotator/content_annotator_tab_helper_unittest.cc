eacc0aca239667141be9f36bfc5a46a8975f6a6a
Sylvain Defresne <sdefresne@chromium.org>
2026-03-19 04:13:09 -0700

    Avoid registering observer before calling HistoryService::Init(...)
    In production it is not possible to get access to an HistoryService
    that has not been initialized so change DomainDiversityReporterTest
    to test this case.
    
    Change ContentAnnotatorTabHelperTest to delegate the creation and
    destruction of HistoryService to TestPageContentAnnotationsService
    which takes care of correctly initializing it.
    
    Bug: 481635191
    Change-Id: I46d741e5c95949b98dc64f39f9b3cfb07ab5f4f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7679356
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1601875}

--- chrome/browser/accessibility_annotator/content_annotator/content_annotator_tab_helper_unittest.cc
+++ chrome/browser/accessibility_annotator/content_annotator/content_annotator_tab_helper_unittest.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/accessibility_annotator/content/content_annotator/content_classifier.h"
 #include "components/accessibility_annotator/core/accessibility_annotator_features.h"
 #include "components/accessibility_annotator/core/storage/accessibility_annotator_backend.h"
-#include "components/history/core/browser/history_service.h"
 #include "components/optimization_guide/core/delivery/test_optimization_guide_model_provider.h"
 #include "components/optimization_guide/core/model_execution/test/mock_remote_model_executor.h"
 #include "components/page_content_annotations/content/page_content_extraction_service.h"
@@ -81,7 +80,7 @@ class ContentAnnotatorTabHelperTest : public ChromeRenderViewHostTestHarness {
 
     page_content_annotations_service_ =
         page_content_annotations::TestPageContentAnnotationsService::Create(
-            &optimization_guide_model_provider_, &history_service_);
+            &optimization_guide_model_provider_, /*history_service=*/nullptr);
 
     page_content_annotations::PageContentExtractionService*
         page_content_extraction_service = page_content_annotations::
@@ -133,7 +132,6 @@ class ContentAnnotatorTabHelperTest : public ChromeRenderViewHostTestHarness {
   MockContentAnnotatorService* mock_service() { return mock_service_.get(); }
 
   base::test::ScopedFeatureList feature_list_;
-  history::HistoryService history_service_;
   optimization_guide::TestOptimizationGuideModelProvider
       optimization_guide_model_provider_;
   optimization_guide::MockRemoteModelExecutor mock_remote_model_executor_;
