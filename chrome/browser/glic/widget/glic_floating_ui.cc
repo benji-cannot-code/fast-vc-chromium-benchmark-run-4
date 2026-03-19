f1066ba2ace4e6e68534302e8c307c4cb4a3a80a
Zoraiz Naeem <zoraiznaeem@google.com>
2026-03-19 12:16:26 -0700

    Nit: Pass Widget ptr to OnWidgetUserResizeStarted/Ended
    This is a nit to make OnWidgetUserResizeStarted and
    OnWidgetUserResizeEnded consistent with other methods in the
    views::WidgetObserver interface.
    
    Change-Id: I02bb970822eeeafdf335a69601a32bd9e0fae09d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7682139
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1602151}

--- chrome/browser/glic/widget/glic_floating_ui.cc
+++ chrome/browser/glic/widget/glic_floating_ui.cc
@@ -391,7 +391,7 @@ void GlicFloatingUi::OnWidgetBoundsChanged(views::Widget* widget,
       &web_modal::ModalDialogHostObserver::OnPositionRequiresUpdate);
 }
 
-void GlicFloatingUi::OnWidgetUserResizeStarted() {
+void GlicFloatingUi::OnWidgetUserResizeStarted(views::Widget* widget) {
   user_resizing_ = true;
   instance_metrics_->OnUserResizeStarted(GetPanelSize());
   if (GlicWebClientAccess* client = delegate_->host().GetPrimaryWebClient()) {
@@ -399,7 +399,7 @@ void GlicFloatingUi::OnWidgetUserResizeStarted() {
   }
 }
 
-void GlicFloatingUi::OnWidgetUserResizeEnded() {
+void GlicFloatingUi::OnWidgetUserResizeEnded(views::Widget* widget) {
   instance_metrics_->OnUserResizeEnded(GetPanelSize());
   if (GlicWebClientAccess* client = delegate_->host().GetPrimaryWebClient()) {
     client->ManualResizeChanged(false);
--- ui/views/widget/widget.cc
+++ ui/views/widget/widget.cc
@@ -2046,11 +2046,11 @@ void Widget::OnNativeWidgetSizeChanged(const gfx::Size& new_size) {
 }
 
 void Widget::OnNativeWidgetUserResizeStarted() {
-  observers_.Notify(&WidgetObserver::OnWidgetUserResizeStarted);
+  observers_.Notify(&WidgetObserver::OnWidgetUserResizeStarted, this);
 }
 
 void Widget::OnNativeWidgetUserResizeEnded() {
-  observers_.Notify(&WidgetObserver::OnWidgetUserResizeEnded);
+  observers_.Notify(&WidgetObserver::OnWidgetUserResizeEnded, this);
 }
 
 void Widget::OnNativeWidgetWorkspaceChanged() {}
