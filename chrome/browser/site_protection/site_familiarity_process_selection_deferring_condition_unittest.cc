723ab1124a831baac5b225ff31a532d5450c7758
Sylvain Defresne <sdefresne@chromium.org>
2026-03-19 02:42:11 -0700

    Avoid registering observer before calling HistoryService::Init(...)
    In production it is not possible to get access to an HistoryService
    that has not been initialized so change DomainDiversityReporterTest
    to test this case.
    
    Ensure SiteFamiliarityProcessSelectionDeferringConditionMockHistoryTest
    initialize HistoryService before the object is used.
    
    Bug: 481635191
    Change-Id: I032c35481df3bc7e9db9a24ad52539fe7542c94a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7673960
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1601838}

--- chrome/browser/site_protection/site_familiarity_process_selection_deferring_condition_unittest.cc
+++ chrome/browser/site_protection/site_familiarity_process_selection_deferring_condition_unittest.cc
@@ -20,8 +20,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/safety_hub/mock_safe_browsing_database_manager.h"
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
 #include "chrome/test/base/testing_browser_process.h"
+#include "components/history/core/browser/history_database_params.h"
 #include "components/history/core/browser/history_service.h"
 #include "components/history/core/browser/history_types.h"
+#include "components/history/core/test/test_history_database.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/safe_browsing/core/browser/db/test_database_manager.h"
 #include "components/site_engagement/content/site_engagement_service.h"
@@ -362,7 +364,10 @@ class ManualCallbackEmptyHistoryService : public history::HistoryService {
 
 std::unique_ptr<KeyedService> BuildManualCallbackEmptyHistoryService(
     content::BrowserContext* browser_context) {
-  return std::make_unique<ManualCallbackEmptyHistoryService>();
+  TestingProfile* profile = static_cast<TestingProfile*>(browser_context);
+  auto service = std::make_unique<ManualCallbackEmptyHistoryService>();
+  service->Init(history::TestHistoryDatabaseParamsForPath(profile->GetPath()));
+  return service;
 }
 
 // SiteFamiliarityProcessSelectionDeferringConditionTest subclass which uses a
