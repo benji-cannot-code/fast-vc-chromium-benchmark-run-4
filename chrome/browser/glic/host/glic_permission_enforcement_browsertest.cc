6f0bebee538f5eb519a6c8e25828eaa465e59695
Dan Harrington <harringtond@chromium.org>
2026-03-19 10:02:54 -0700

    migrate test to browsertest: glic_permission_enforcement
    Browser tests can be run in parallel, so they're preferred to
    interactive UI tests.
    
    Bypass-Check-License: moved file
    Change-Id: I3ea083beb79fd64a278a583cd6a68cb26a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7681971
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Commit-Queue: Dan Harrington <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1602064}

--- chrome/browser/glic/host/glic_permission_enforcement_browsertest.cc
+++ chrome/browser/glic/host/glic_permission_enforcement_browsertest.cc
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/glic/glic_pref_names.h"
-#include "chrome/browser/glic/test_support/interactive_glic_test.h"
+#include "chrome/browser/glic/test_support/non_interactive_glic_test.h"
 #include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/test/interaction/interactive_browser_test.h"
@@ -19,7 +19,7 @@ namespace glic {
 
 DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kActiveTabId);
 
-class GlicPermissionEnforcementUiTest : public test::InteractiveGlicTest {
+class GlicPermissionEnforcementUiTest : public NonInteractiveGlicTest {
  public:
   GlicPermissionEnforcementUiTest()
       : geolocation_overrider_(
