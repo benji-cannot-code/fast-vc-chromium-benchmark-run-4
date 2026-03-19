45adf87d917e35418e76bf34a648d9bae8e3c7a1
Maks Orlovich <morlovich@chromium.org>
2026-03-19 15:31:13 -0700

    webui_browser: Fix bookmark bar favicons.
    Looks like we were not getting the chrome://favicon2/ source
    registered any more. I am not sure what's been doing it for us,
    but just do it ourselves.
    
    Change-Id: I950388dc332b2ee72af6031c6ee53f4f7f261954
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7685313
    Reviewed-by: David Yeung <dayeung@chromium.org>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1602273}

--- chrome/browser/ui/webui_browser/webui_browser_ui.cc
+++ chrome/browser/ui/webui_browser/webui_browser_ui.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/side_panel/side_panel_entry_id.h"
 #include "chrome/browser/ui/tabs/tab_strip_api/tab_strip_service_feature.h"
 #include "chrome/browser/ui/webui/cr_components/searchbox/searchbox_handler.h"
+#include "chrome/browser/ui/webui/favicon_source.h"
 #include "chrome/browser/ui/webui/searchbox/realbox_handler.h"
 #include "chrome/browser/ui/webui_browser/bookmark_bar_page_handler.h"
 #include "chrome/browser/ui/webui_browser/webui_browser.h"
@@ -30,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/webui_browser_resources_map.h"
 #include "components/contextual_search/contextual_search_service.h"
 #include "components/contextual_search/contextual_search_session_handle.h"
+#include "components/favicon_base/favicon_url_parser.h"
 #include "components/guest_contents/browser/guest_contents_host_impl.h"
 #include "components/surface_embed/buildflags/buildflags.h"
 #include "content/public/browser/browser_context.h"
@@ -96,6 +98,7 @@ WebUIBrowserUI::WebUIBrowserUI(content::WebUI* web_ui)
                               /*enable_chrome_histograms=*/true) {
   WebUIBrowserWindow* webui_browser_window =
       WebUIBrowserWindow::FromWebShellWebContents(web_ui->GetWebContents());
+  Profile* profile = Profile::FromWebUI(web_ui);
   browser_ = webui_browser_window->browser();
 
   // Set up the chrome://webui-browser source.
@@ -118,7 +121,7 @@ WebUIBrowserUI::WebUIBrowserUI(content::WebUI* web_ui)
   };
   source->AddLocalizedStrings(kStrings);
 
-  SearchboxHandler::SetupWebUIDataSource(source, Profile::FromWebUI(web_ui));
+  SearchboxHandler::SetupWebUIDataSource(source, profile);
 
 #if BUILDFLAG(ENABLE_SURFACE_EMBED)
   source->AddBoolean(
@@ -141,6 +144,10 @@ WebUIBrowserUI::WebUIBrowserUI(content::WebUI* web_ui)
   // source->AddString(
   //     "resultChangedToPaintMetricName",
   //     "Omnibox.Popup.WebUI.ResultChangedToRepaintLatency.ToPaint");
+
+  content::URLDataSource::Add(
+      profile, std::make_unique<FaviconSource>(
+                   profile, chrome::FaviconUrlFormat::kFavicon2));
 }
 
 WebUIBrowserUI::~WebUIBrowserUI() = default;
