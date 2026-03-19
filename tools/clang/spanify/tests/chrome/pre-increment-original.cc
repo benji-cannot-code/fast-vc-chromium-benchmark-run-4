b7d7064b50b8b0b68cad820304b8366cb0314d49
Daniel Angulo <angdaniel@google.com>
2026-03-19 09:05:12 -0700

    [Spanify] fix pre increment/decrement in autospanifier
    This commit fixes problems detected in autospanifier related to pre
    increment and pre decrement operators.
    
    This change is the eighth step towards enabling spanification for
    PartitionAlloc and other third-party libraries. These projects require
    specific configurations, such as different header paths for `base::span`
    or custom exclusion logic, which will be handled based on the selected
    project.
    
    Design doc: go/partition-alloc-spanification
    
    Bug: 481768474
    Change-Id: Ie268a88408f71f4bb6edfdf9863577ba5b036c3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7552507
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Daniel Angulo <angdaniel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1602018}

---
+++ tools/clang/spanify/tests/chrome/pre-increment-original.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// A macro obscures the source location of the operator
+// No expected rewrite, macros are not supported yet because
+// spanifier will generate closing parenthesis in unexpected locations
+#define PRE_INC(x) ++x
+
+int* test_macro_increment(int buf[3]) {
+  int* ptr = buf;
+  // Regression test for "++i" crashing when written inside macros.
+  PRE_INC(ptr);
+  return ptr;
+}
+
+int main() {
+  int buf[3] = {1, 2, 3};
+  int* ptr = test_macro_increment(buf);
+  return ptr[0];
+}
