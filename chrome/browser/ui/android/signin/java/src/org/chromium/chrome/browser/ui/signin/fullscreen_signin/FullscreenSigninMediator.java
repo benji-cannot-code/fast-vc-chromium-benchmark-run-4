f9680e5f705a43488d29ac17cd105ce1956dca28
Samar <samarchehade@google.com>
2026-03-19 02:42:28 -0700

    [Signin][Android] Support selecting an account when signin is forced
    Bug: 467707243
    Change-Id: I45018e8d8450ea9a8af7cf57fac494b8001ad4d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7653603
    Commit-Queue: Samar Chehade <samarchehade@google.com>
    Reviewed-by: Anton Goncharenko <agonch@google.com>
    Reviewed-by: Liza Bipin <mlbipin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1601839}

--- chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/fullscreen_signin/FullscreenSigninMediator.java
+++ chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/fullscreen_signin/FullscreenSigninMediator.java
@@ -726,6 +726,7 @@ public class FullscreenSigninMediator
             mModel.set(
                     FullscreenSigninProperties.SELECTED_ACCOUNT_DATA,
                     mProfileDataCache.getProfileDataOrDefault(mSelectedAccount.getEmail()));
+            mModel.set(FullscreenSigninProperties.ENABLE_ACCOUNT_SELECTION, !mIsChild);
         }
     }
 
@@ -745,6 +746,7 @@ public class FullscreenSigninMediator
             mDefaultAccount = null;
             mSelectedAccount = null;
             mModel.set(FullscreenSigninProperties.SELECTED_ACCOUNT_DATA, null);
+            mModel.set(FullscreenSigninProperties.ENABLE_ACCOUNT_SELECTION, false);
             if (mDialogCoordinator != null) {
                 mDialogCoordinator.dismissDialog();
             }
@@ -774,6 +776,9 @@ public class FullscreenSigninMediator
             // Subtitle is hidden for child accounts.
             mModel.set(FullscreenSigninProperties.SUBTITLE_STRING, null);
         }
+        mModel.set(
+                FullscreenSigninProperties.ENABLE_ACCOUNT_SELECTION,
+                !mIsChild && mSelectedAccount != null);
         updateShouldHideDismissButton();
         // Selected account data will be updated in {@link #onProfileDataUpdated}
         mProfileDataCache.setBadge(
