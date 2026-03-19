639f04baa943c415016fd53cc79fbb6b0bfbc27c
Colin Blundell <blundell@chromium.org>
2026-03-19 02:19:59 -0700

    [Blink] Separate CRP::WritePixels() by 2D and non-2D variants
    Will enable tightening the non-2D variant for invariants of its one
    callsite in followup.
    
    Note: As with CanvasRC2D, OffcreenCanvasRC2D::resource_provider_ is
    always Canvas2D [1][2].
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/modules/canvas/offscreencanvas2d/offscreen_canvas_rendering_context_2d.cc;l=234-260;drc=4d1c1512e2d831a1dc525ac32f3c22c1e5583067
    [2] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc;l=1517-1519;drc=4d1c1512e2d831a1dc525ac32f3c22c1e5583067
    
    Bug: 352263194
    Change-Id: Ie5995a54bf451788150ea2df813a920c3871390b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7679352
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1601827}

--- third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d_test.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d_test.cc
@@ -616,7 +616,7 @@ class FakeCanvasResourceProvider : public Canvas2DResourceProviderSharedImage {
               (ImageOrientation orientation));
 
   MOCK_METHOD(bool,
-              WritePixels,
+              WritePixelsForCanvas2D,
               (const SkImageInfo& orig_info,
                const void* pixels,
                size_t row_bytes,
@@ -1365,7 +1365,7 @@ TEST_P(CanvasRenderingContext2DTestAccelerated, PutImageData_FullCoverage) {
   // `WritePixels` is called.
   InSequence s;
   EXPECT_CALL(*provider, RasterRecord).Times(0);
-  EXPECT_CALL(*provider, WritePixels).Times(1);
+  EXPECT_CALL(*provider, WritePixelsForCanvas2D).Times(1);
 
   Context2D()->SetCanvas2DResourceProviderForTesting(std::move(provider), size);
 
@@ -1396,7 +1396,7 @@ TEST_P(CanvasRenderingContext2DTestAccelerated, PutImageData_PartialCoverage) {
   InSequence s;
   EXPECT_CALL(*provider, RasterRecord(RecordedOpsAre(PaintOpIs<DrawRectOp>())))
       .Times(1);
-  EXPECT_CALL(*provider, WritePixels).Times(1);
+  EXPECT_CALL(*provider, WritePixelsForCanvas2D).Times(1);
 
   Context2D()->SetCanvas2DResourceProviderForTesting(std::move(provider), size);
 
