4aeca9a8560e3554b3f22a770d88d00b3fb4f8a8
Jeff Yoon <jeffyoon@google.com>
2026-03-19 15:48:15 -0700

    Make MemorySaverOptInIPHController unowned user data
    This CL uplifts MemorySaverOptInIPHController to use the
    UnownedUserData pattern. This ensures that the controller
    registers itself correctly to the browser window and avoids
    providing a direct getter on BrowserWindowFeatures, which
    helps to reduce monolith circular dependencies.
    
    Bug: none
    Change-Id: I12b7391df8a9133178798f6a39e24fbcc6789c31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7685272
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Jeff Yoon <jeffyoon@google.com>
    Cr-Commit-Position: refs/heads/main@{#1602288}

--- chrome/browser/ui/performance_controls/memory_saver_opt_in_iph_controller.cc
+++ chrome/browser/ui/performance_controls/memory_saver_opt_in_iph_controller.cc
@@ -20,9 +20,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // saver opt-in IPH.
 constexpr base::ByteSize kMemoryCap16GB = base::GiBU(16);
 
+DEFINE_USER_DATA(MemorySaverOptInIPHController);
+
+// static
+MemorySaverOptInIPHController* MemorySaverOptInIPHController::From(
+    BrowserWindowInterface* interface) {
+  return Get(interface->GetUnownedUserDataHost());
+}
+
 MemorySaverOptInIPHController::MemorySaverOptInIPHController(
     BrowserWindowInterface* interface)
-    : browser_window_interface_(interface) {
+    : browser_window_interface_(interface),
+      scoped_unowned_user_data_(interface->GetUnownedUserDataHost(), *this) {
   auto* manager = performance_manager::user_tuning::
       UserPerformanceTuningManager::GetInstance();
   memory_saver_observer_.Observe(manager);
