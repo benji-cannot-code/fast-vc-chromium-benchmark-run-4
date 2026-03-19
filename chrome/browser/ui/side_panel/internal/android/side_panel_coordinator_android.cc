9ee47239e04730722783e9127f3471ff4a9479ca
Linyu He <linyuh@google.com>
2026-03-18 17:40:11 -0700

    Side Panel: Drop "Bridge" suffix from SidePanelCoordinatorAndroidBridge
    This is a quick follow-up to this code review comment:
    https://chromium-review.git.corp.google.com/c/chromium/src/+/7677567/comment/27a6ae6b_44dec321/
    
    Bug: 491597112
    Change-Id: I8dc7823f7375893691a5d4732329ea25d2553ace
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7682756
    Auto-Submit: Linyu He <linyuh@google.com>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Linyu He <linyuh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1601655}

--- chrome/browser/ui/side_panel/internal/android/side_panel_coordinator_android.cc
+++ chrome/browser/ui/side_panel/internal/android/side_panel_coordinator_android.cc
@@ -10,13 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_android.h"
 #include "base/android/scoped_java_ref.h"
 #include "base/check.h"
-#include "chrome/browser/ui/side_panel/internal/android/jni_headers/SidePanelCoordinatorAndroidBridgeImpl_jni.h"
+#include "chrome/browser/ui/side_panel/internal/android/jni_headers/SidePanelCoordinatorAndroidImpl_jni.h"
 
 using base::android::JavaRef;
 using base::android::ScopedJavaLocalRef;
 
-// Implements Java `SidePanelCoordinatorAndroidBridgeImpl.Natives#create`.
-static int64_t JNI_SidePanelCoordinatorAndroidBridgeImpl_Create(
+// Implements Java `SidePanelCoordinatorAndroidImpl.Natives#create`.
+static int64_t JNI_SidePanelCoordinatorAndroidImpl_Create(
     JNIEnv* env,
     const JavaRef<jobject>& caller) {
   return reinterpret_cast<intptr_t>(
@@ -25,27 +25,27 @@ static int64_t JNI_SidePanelCoordinatorAndroidBridgeImpl_Create(
 
 SidePanelCoordinatorAndroid::SidePanelCoordinatorAndroid(
     JNIEnv* env,
-    const JavaRef<jobject>& java_bridge)
-    : java_bridge_(env, java_bridge) {}
+    const JavaRef<jobject>& java_coordinator)
+    : java_coordinator_(env, java_coordinator) {}
 
 SidePanelCoordinatorAndroid::~SidePanelCoordinatorAndroid() {
-  Java_SidePanelCoordinatorAndroidBridgeImpl_clearNativePtr(
-      base::android::AttachCurrentThread(), java_bridge());
+  Java_SidePanelCoordinatorAndroidImpl_clearNativePtr(
+      base::android::AttachCurrentThread(), java_coordinator());
 }
 
 void SidePanelCoordinatorAndroid::Destroy(JNIEnv* env) {
   delete this;
 }
 
-ScopedJavaLocalRef<jobject> SidePanelCoordinatorAndroid::java_bridge() const {
+ScopedJavaLocalRef<jobject> SidePanelCoordinatorAndroid::java_coordinator()
+    const {
   JNIEnv* env = base::android::AttachCurrentThread();
-  ScopedJavaLocalRef<jobject> local_ref = java_bridge_.get(env);
+  ScopedJavaLocalRef<jobject> local_ref = java_coordinator_.get(env);
 
-  CHECK(local_ref)
-      << "Java SidePanelCoordinatorAndroidBridge is the sole owner of "
-         "SidePanelCoordinatorAndroid, so it shouldn't be destroyed before "
-         "SidePanelCoordinatorAndroidBridge";
+  CHECK(local_ref) << "Java SidePanelCoordinatorAndroid is the sole owner of "
+                      "C++ SidePanelCoordinatorAndroid, so the Java object "
+                      "shouldn't be destroyed before the C++ object";
   return local_ref;
 }
 
-DEFINE_JNI(SidePanelCoordinatorAndroidBridgeImpl)
+DEFINE_JNI(SidePanelCoordinatorAndroidImpl)
