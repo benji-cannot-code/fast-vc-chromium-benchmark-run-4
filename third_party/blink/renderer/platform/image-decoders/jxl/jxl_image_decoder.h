8cfcbe8ffe2d92f1e6151a88bbccd981ded3f768
Helmut Januschka <helmut@januschka.com>
2026-03-19 08:29:03 -0700

    Roll jxl: 0.3.0 => 0.4.0, jxl_macros: 0.3.0 => 0.4.0, jxl_simd: 0.3.0 => 0.4.0, jxl_transforms: 0.3.0 => 0.4.0
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    * jxl: 0.3.0 => 0.4.0; https://docs.rs/crate/jxl/0.4.0
    * jxl_macros: 0.3.0 => 0.4.0; https://docs.rs/crate/jxl_macros/0.4.0
    * jxl_simd: 0.3.0 => 0.4.0; https://docs.rs/crate/jxl_simd/0.4.0
    * jxl_transforms: 0.3.0 => 0.4.0; https://docs.rs/crate/jxl_transforms/0.4.0
    
    Bug: None
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Binary-Size: Size increase is unavoidable
    Change-Id: I93a34612731334aedcca564c27b746f41f2fce2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7671659
    Reviewed-by: Luca Versari <veluca@google.com>
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Wan-Teh Chang <wtc@google.com>
    Commit-Queue: Helmut Januschka <helmut@januschka.com>
    Cr-Commit-Position: refs/heads/main@{#1601991}

--- third_party/blink/renderer/platform/image-decoders/jxl/jxl_image_decoder.h
+++ third_party/blink/renderer/platform/image-decoders/jxl/jxl_image_decoder.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/functional.h"
 #include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
-#include "third_party/rust/jxl/v0_3/wrapper/lib.rs.h"
+#include "third_party/rust/jxl/v0_4/wrapper/lib.rs.h"
 #include "third_party/skia/include/core/SkImageInfo.h"
 
 namespace blink {
