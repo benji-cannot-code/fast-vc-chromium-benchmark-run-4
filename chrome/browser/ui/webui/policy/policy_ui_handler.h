15bf99f546fac9b934a19dc96981edbfc53185e3
Jonathan Lee <jonathanjlee@google.com>
2026-03-18 23:47:14 -0700

    [code-health] Clean up `PolicyPagePromotionEligibilityCheckedBanner`
    The `base::Feature` doesn't appear Finch-controlled or wired to anything
    in the browser (even in internal repos).
    
    NO_IFTTT=N/A, flag isn't used in `//ios/chrome/`
    
    Bug: None
    Change-Id: I1c9d07e86972d5c926a51650bbf6a3d63c89a5a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7662628
    Commit-Queue: Igor <igorcov@chromium.org>
    Auto-Submit: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Igor <igorcov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1601786}

--- chrome/browser/ui/webui/policy/policy_ui_handler.h
+++ chrome/browser/ui/webui/policy/policy_ui_handler.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 
-#include "base/feature_list.h"
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
 #include "base/values.h"
@@ -33,12 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // !BUILDFLAG(IS_ANDROID)
 
 class PrefChangeRegistrar;
-
-namespace features {
-// If enabled, the banner on the chrome://policy page to be shown only to
-// eligible users passing through the promotion eligibility checker.
-BASE_DECLARE_FEATURE(kPolicyPagePromotionEligibilityCheckedBanner);
-}  // namespace features
 
 namespace enterprise_management {
 class GetUserEligiblePromotionsResponse;
