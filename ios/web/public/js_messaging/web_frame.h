40f90269fab3242475d887bf89753f246044a843
Mike Dougherty <michaeldo@chromium.org>
2026-03-19 11:44:08 -0700

    Remove reference to previously removed function
    Bug: 40256922
    Change-Id: I5e9f76b8b81ee0cabdf89de54e2fec7eb9f5b8d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7682785
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Reviewed-by: Daniel White <danieltwhite@google.com>
    Auto-Submit: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1602135}

--- ios/web/public/js_messaging/web_frame.h
+++ ios/web/public/js_messaging/web_frame.h
@@ -56,8 +56,7 @@ class WebFrame : public base::SupportsUserData {
   // webpage DOM could change in a way which prevents the function from
   // executing.
   // Returns true if function call was requested, false otherwise. Function call
-  // may still fail even if this function returns true. Always returns false if
-  // `CanCallJavaScriptFunction` is false.
+  // may still fail even if this function returns true.
   virtual bool CallJavaScriptFunction(const std::string& name,
                                       const base::ListValue& parameters) = 0;
 
@@ -67,8 +66,7 @@ class WebFrame : public base::SupportsUserData {
   // If `timeout` is reached, callback is called with the nullptr parameter
   // and no result received later will be sent.
   // Returns true if function call was requested, false otherwise. Function call
-  // may still fail even if this function returns true. Always returns false if
-  // `CanCallJavaScriptFunction` is false.
+  // may still fail even if this function returns true.
   virtual bool CallJavaScriptFunction(
       const std::string& name,
       const base::ListValue& parameters,
