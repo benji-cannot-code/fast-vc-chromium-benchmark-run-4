b8bc3b8ed36cc982ad2e923ab7be3d2f9ce72154
Andrew Verge <averge@chromium.org>
2026-03-19 13:41:55 -0700

    [Connection-Allowlist] Implement "redirects=" param for navigations.
    Previously, we blocked all redirects for navigations when a Connection
    Allowlist header was enforced. We decided to use a global header param
    called `redirects=` instead. When the value of the param is "allow",
    navigations for redirects will be allowed regardless of destination.
    When the value is "block", redirects will be blocked regardless of the
    destination.
    
    A follow-up CL will implement this same logic for subresource fetch
    redirects.
    
    Bug: 492439215
    Change-Id: I2f9de6c9a8f9746b2f7f5206e9476709145b77d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7667118
    Commit-Queue: Andrew Verge <averge@chromium.org>
    Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1602209}

---
+++ third_party/blink/web_tests/external/wpt/connection-allowlist/tentative/navigation-redirect-block.sub.window.js
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/common/get-host-info.sub.js
+// META: script=resources/navigation_redirect_test.js
+
+// We're loading this page from `http://{{hosts[][]}}`.
+// The connection allowlist header is `Connection-Allowlist:
+// (response-origin);redirects=block`. Thus, all redirects should fail.
+
+// Redirect from an allowlisted origin (same-origin):
+// origin: http://{{hosts[][]}} (allowed by allowlist)
+// target: http://{{hosts[][]}} (also allowed)
+// This should FAIL, because of the redirects=block portion of the header.
+navigation_redirect_test(
+    'http://{{hosts[][]}}' + port, 'http://{{hosts[][]}}' + port, FAILURE);
+
+// Redirect from an allowlisted origin to a different origin:
+// origin: http://{{hosts[][]}} (allowed by allowlist)
+// target: http://{{hosts[alt][]}} (not allowed)
+// This should FAIL, because of the redirects=block portion of the header.
+navigation_redirect_test(
+    'http://{{hosts[][]}}' + port, 'http://{{hosts[alt][]}}' + port, FAILURE);
+
+// Initial navigation to a non-allowlisted origin:
+// origin: http://{{hosts[alt][]}} (not allowed)
+// This is blocked before the redirect even happens.
+navigation_redirect_test(
+    'http://{{hosts[alt][]}}' + port, 'http://{{hosts[][]}}' + port, FAILURE);
