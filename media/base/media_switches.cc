e2d2c2add6777a801e429d30c0f3958d26ab13a4
Christopher Cameron <ccameron@chromium.org>
2026-03-18 18:20:42 -0700

    MP4StreamParser: Parse IT35 timed metadata tracks
    Add FourCCs and Box types for parsing IT35 timed metadata track, setting
    the HandlerReference::type to TrackType::kMetadata. Store the number of
    metadata tracks successfully parsed in
    StreamParser::InitParameters::detected_metadata_track_count, and write a
    test to verify this.
    
    The test file intentionally only is intentionally truncated (holding off
    adding larger files until the format is finalized).
    
    Bug: 490319976
    Change-Id: I5761436e71bb5d3c83c25198dc26d6e34d8a34bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7673438
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1601669}

--- media/base/media_switches.cc
+++ media/base/media_switches.cc
@@ -1640,6 +1640,8 @@ bool IsChromeWideEchoCancellationEnabled() {
 #endif
 }
 
+BASE_FEATURE(kMP4TimedMetadataTrack, base::FEATURE_DISABLED_BY_DEFAULT);
+
 BASE_FEATURE(kWebRtcAudioNeuralResidualEchoEstimation,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
