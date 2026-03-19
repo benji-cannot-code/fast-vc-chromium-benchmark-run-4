e2a0089182b840fa7c01f52e001c1745930572d1
Olivia Saul <osaul@google.com>
2026-03-19 16:58:19 -0700

    [AF] Clean up AutofillSharedStorageServerCardData flag usage
    Bug: 41492904, 360924313
    Change-Id: Ie7e2169a18951336b2504517e55ffdd9b4cc5bf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7635979
    Commit-Queue: Olivia Saul <osaul@google.com>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1602334}

--- components/autofill/content/browser/content_autofill_shared_storage_handler.cc
+++ components/autofill/content/browser/content_autofill_shared_storage_handler.cc
@@ -6,10 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/content/browser/content_autofill_shared_storage_handler.h"
 
 #include "base/base64.h"
-#include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/metrics/histogram_macros.h"
-#include "base/strings/utf_string_conversions.h"
 #include "components/autofill/content/browser/autofill_shared_storage.pb.h"
 #include "components/autofill/core/browser/data_model/payments/credit_card.h"
 #include "components/autofill/core/browser/payments/payments_service_url.h"
@@ -20,22 +18,6 @@ namespace {
 
 constexpr char16_t kAutofillServerCardDataSharedStorageKey[] =
     u"browser_autofill_card_data";
-
-std::u16string EncodeServerCardDataForSharedStorage(
-    const std::vector<std::unique_ptr<CreditCard>>& server_card_data) {
-  AutofillCreditCardList card_list_proto;
-  for (auto& card : server_card_data) {
-    AutofillCreditCardData* card_data_proto =
-        card_list_proto.add_server_cards();
-    card_data_proto->set_last_four(base::UTF16ToUTF8(card->LastFourDigits()));
-    card_data_proto->set_network(card->network());
-    card_data_proto->set_expiration_month(card->expiration_month());
-    card_data_proto->set_expiration_year(card->expiration_year());
-  }
-
-  return base::ASCIIToUTF16(
-      base::Base64Encode(card_list_proto.SerializeAsString()));
-}
 
 }  // namespace
 
@@ -48,25 +30,8 @@ ContentAutofillSharedStorageHandler::~ContentAutofillSharedStorageHandler() =
 
 void ContentAutofillSharedStorageHandler::OnServerCardDataRefreshed(
     const std::vector<std::unique_ptr<CreditCard>>& server_card_data) {
-  if (!base::FeatureList::IsEnabled(
-          features::kAutofillSharedStorageServerCardData)) {
-    // Ensure the data is cleared, in case the feature was previously enabled.
-    ClearAutofillSharedStorageData();
-    return;
-  }
-
-  if (server_card_data.empty()) {
-    ClearAutofillSharedStorageData();
-  } else {
-    shared_storage_manager_->Set(
-        payments::GetGooglePayScriptOrigin(),
-        kAutofillServerCardDataSharedStorageKey,
-        EncodeServerCardDataForSharedStorage(server_card_data),
-        base::BindOnce(&ContentAutofillSharedStorageHandler::
-                           OnSharedStorageSetAutofillDataComplete,
-                       weak_factory_.GetWeakPtr()),
-        storage::SharedStorageDatabase::SetBehavior::kDefault);
-  }
+  // Ensure the data is cleared, in case the feature was previously enabled.
+  ClearAutofillSharedStorageData();
 }
 
 void ContentAutofillSharedStorageHandler::ClearAutofillSharedStorageData() {
