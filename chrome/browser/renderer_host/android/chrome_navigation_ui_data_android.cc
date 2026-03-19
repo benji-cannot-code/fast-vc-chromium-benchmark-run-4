40076d13f5027190cfd415d9633785a4877cdb0d
Antonio Gomes <tonikitoo@igalia.com>
2026-03-18 18:42:03 -0700

    Move chrome_navigation_ui_data_android.cc to chrome/browser/renderer_host/android/
    ... from chrome/browser/android/renderer_host.
    
    This confines the implementation under the target directory, and makes
    completing the modularization of it simpler.
    
    R=dljames@chromium.org
    
    Bypass-Check-License: Moving files
    Bug: 353332589
    Change-Id: Ie6baa928ddd8220eee608b4752e7633cae040a55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7674407
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Antonio Gomes Netto <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1601689}

--- chrome/browser/renderer_host/android/chrome_navigation_ui_data_android.cc
+++ chrome/browser/renderer_host/android/chrome_navigation_ui_data_android.cc
@@ -3,8 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/android/jni_android.h"
 #include "chrome/browser/renderer_host/chrome_navigation_ui_data.h"
+
+#include "base/android/jni_android.h"
 #include "content/public/browser/navigation_ui_data.h"
 
 // Must come after all headers that specialize FromJniType() / ToJniType().
