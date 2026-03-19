f07d93a3792db938f59b8595106c1b22808dff66
Jingyun Liu <jingyun@google.com>
2026-03-19 11:19:31 -0700

    [builtin-ai] Update usage tracker observer to key on use case instead of feature.
    - Functions to read/write features are not removed, to avoid updating existing clients all at once. The plan is to retire them after the rollout is complete.
    - WasUseCaseRecentlyUsed read both use_case_name and feature keys in pref, to ensure feature usage won't be lost during migration.
    
    Next CL:
    - ManifestAssetManager observes usage tracker and download requested assets.
    
    Bug: 489511499
    Change-Id: Ibc90cc6684339f8248bebb1276800179b3f48b12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7680411
    Commit-Queue: Jingyun Liu <jingyun@google.com>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1602113}

--- components/optimization_guide/core/model_execution/on_device_model_component.cc
+++ components/optimization_guide/core/model_execution/on_device_model_component.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/core/delivery/model_util.h"
 #include "components/optimization_guide/core/model_execution/model_execution_prefs.h"
 #include "components/optimization_guide/core/model_execution/model_execution_util.h"
+#include "components/optimization_guide/core/model_execution/on_device_features.h"
 #include "components/optimization_guide/core/model_execution/performance_class.h"
 #include "components/optimization_guide/core/model_execution/usage_tracker.h"
 #include "components/optimization_guide/core/optimization_guide_constants.h"
@@ -427,11 +428,17 @@ void OnDeviceModelComponentStateManager::
   BeginUpdateRegistration();
 }
 
-void OnDeviceModelComponentStateManager::OnDeviceEligibleFeatureUsed(
-    mojom::OnDeviceFeature feature) {
+void OnDeviceModelComponentStateManager::OnDeviceEligibleUseCaseUsed(
+    const std::string& use_case_name,
+    bool is_first_usage) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
-  if (GetOnDeviceModelType(feature) != model_type_) {
+  auto feature = GetFeatureForUseCase(use_case_name);
+  if (!feature) {
+    return;
+  }
+
+  if (GetOnDeviceModelType(*feature) != model_type_) {
     return;
   }
 
