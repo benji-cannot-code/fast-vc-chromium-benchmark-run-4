2a5b662b4531eefb58f1ccad5624cba022b7e62a
Bofeng Chen <bofengchen@google.com>
2026-03-20 09:27:08 -0700

    [composebox] Refactor composebox input into a new component [part 2]
    Design doc: go/chrome-search-componentization
    
    A complete implementation of this task will be like: crrev.com/c/7676660
    
    This change introduces a getInputElement method to the
    ComposeboxEmbedderMixin and its interface, allowing components using the
    mixin to access the underlying input element. The mixin's default
    implementation asserts not reached, expecting concrete implementations
    to override it.
    
    Bug: b:486706313
    Depends-On: I43660d38e80a80ee7b57f3846a7a58300bf86eb3
    Change-Id: I31e45d6eac36c4ca5c6b799b7a26fc4ebec9a0c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7685412
    Commit-Queue: Bofeng Chen <bofengchen@google.com>
    Reviewed-by: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1602663}

--- ui/webui/resources/cr_components/composebox/composebox_mixin.ts
+++ ui/webui/resources/cr_components/composebox/composebox_mixin.ts
@@ -3,11 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {assertNotReached} from '//resources/js/assert.js';
 import {loadTimeData} from '//resources/js/load_time_data.js';
 import type {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
 import type {UnguessableToken} from '//resources/mojo/mojo/public/mojom/base/unguessable_token.mojom-webui.js';
 
 import type {ComposeboxFile} from './common.js';
+import type {ComposeboxInputElement} from './composebox_input.js';
 import {ToolMode as ComposeboxToolMode} from './composebox_query.mojom-webui.js';
 import type {InputState} from './composebox_query.mojom-webui.js';
 
@@ -39,6 +41,10 @@ export const ComposeboxEmbedderMixin =
         accessor inputPlaceholder: string =
             loadTimeData.getString('searchboxComposePlaceholder');
         accessor inputState: InputState|null = null;
+
+        getInputElement(): ComposeboxInputElement {
+          assertNotReached();
+        }
       }
 
       return ComposeboxEmbedderMixin;
@@ -52,4 +58,6 @@ export interface ComposeboxEmbedderMixinInterface {
   input: string;
   inputPlaceholder: string;
   inputState: InputState|null;
+
+  getInputElement(): ComposeboxInputElement;
 }
