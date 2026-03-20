6c3d430d9407ee242be8ca947125c5a62654ab4d
Matt Jones <mdjones@chromium.org>
2026-03-20 10:02:02 -0700

    Allow ContextualTasksService creation for sync features
    This patch allows the creation of the ContextualTasksService for other
    features that need threads - namely the project panel. The service
    will now be created if either kSyncGeminiThread or kSyncAIThread are
    enabled since the service is needed to utilize either thread type.
    
    Change-Id: I5c2c0b3cce7d81878eef435eb1122fd780630e57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7674409
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1602690}

--- chrome/browser/contextual_tasks/contextual_tasks_service_factory.cc
+++ chrome/browser/contextual_tasks/contextual_tasks_service_factory.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/contextual_tasks/public/features.h"
 #include "components/favicon/core/favicon_service.h"
 #include "components/history/core/browser/history_service.h"
+#include "components/sync/base/features.h"
 #include "components/sync/model/data_type_store_service.h"
 #include "content/public/browser/browser_context.h"
 
@@ -117,7 +118,9 @@ ContextualTasksServiceFactory::~ContextualTasksServiceFactory() = default;
 std::unique_ptr<KeyedService>
 ContextualTasksServiceFactory::BuildServiceInstanceForBrowserContext(
     content::BrowserContext* context) const {
-  if (!base::FeatureList::IsEnabled(kContextualTasks)) {
+  if (!base::FeatureList::IsEnabled(kContextualTasks) &&
+      !base::FeatureList::IsEnabled(syncer::kSyncAIThread) &&
+      !base::FeatureList::IsEnabled(syncer::kSyncGeminiThread)) {
     return nullptr;
   }
 
