41f7381aae2718ac224b32b75fd57e383c5a4704
rbpotter <rbpotter@chromium.org>
2026-03-19 18:49:38 -0700

    Side Panel Commerce: Migrate insights-comment-row to Lit
    Bug: 493823435
    Change-Id: I94139e4d0cb26981a5c6887bd2ab35e91678cbf9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7682350
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1602387}

---
+++ chrome/browser/resources/side_panel/commerce/insights_comment_row.html.ts
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {InsightsCommentRowElement} from './insights_comment_row.js';
+
+export function getHtml(this: InsightsCommentRowElement) {
+  // clang-format off
+  return html`<!--_html_template_start_-->
+<div id="commentRow">
+  <span id="comment">$i18n{historyDescription}</span>
+  <a href="#" ?hidden="${!this.shouldShowFeedback_}"
+      @click="${this.onFeedbackClick_}" class="link">$i18n{feedback}</a>
+</div>
+<!--_html_template_end_-->`;
+  // clang-format on
+}
