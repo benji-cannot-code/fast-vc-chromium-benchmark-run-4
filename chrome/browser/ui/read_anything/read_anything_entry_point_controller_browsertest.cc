8999262b5722eade4a874953d2aae3e7c94c0303
Kristi Saney <kristislee@google.com>
2026-03-20 15:16:16 -0700

    [Reading mode] Fix omnibox flaky tests
    Bug: 491392993, 492548628
    Change-Id: I903cf55e0d320fddc0dba109552b411b1706a750
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7689393
    Auto-Submit: Kristi Saney <kristislee@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1602870}

--- chrome/browser/ui/read_anything/read_anything_entry_point_controller_browsertest.cc
+++ chrome/browser/ui/read_anything/read_anything_entry_point_controller_browsertest.cc
@@ -322,19 +322,13 @@ IN_PROC_BROWSER_TEST_P(ReadAnythingEntryPointControllerOmniboxBrowserTest,
 
 IN_PROC_BROWSER_TEST_P(ReadAnythingEntryPointControllerOmniboxBrowserTest,
                        UpdatePageActionVisibility_ShowsPageAction) {
-  EXPECT_TRUE(
-      ui_test_utils::NavigateToURL(browser(), GURL("https://www.google.com")));
   RegisterPageActionObserver();
-
   ReadAnythingEntryPointController::UpdatePageActionVisibility(true, browser());
-
   VerifyPageActionIsShowing(true);
 }
 
 IN_PROC_BROWSER_TEST_P(ReadAnythingEntryPointControllerOmniboxBrowserTest,
                        UpdatePageActionVisibility_ShowsChip) {
-  EXPECT_TRUE(
-      ui_test_utils::NavigateToURL(browser(), GURL("https://www.google.com")));
   RegisterPageActionObserver();
 
   ReadAnythingEntryPointController::UpdatePageActionVisibility(true, browser());
@@ -346,8 +340,6 @@ IN_PROC_BROWSER_TEST_P(ReadAnythingEntryPointControllerOmniboxBrowserTest,
 IN_PROC_BROWSER_TEST_P(
     ReadAnythingEntryPointControllerOmniboxBrowserTest,
     UpdatePageActionVisibility_DoesNotShowChipIfIgnoredManyTimes) {
-  EXPECT_TRUE(
-      ui_test_utils::NavigateToURL(browser(), GURL("https://www.google.com")));
   RegisterPageActionObserver();
   browser()->GetProfile()->GetPrefs()->SetInteger(
       prefs::kAccessibilityReadAnythingOmniboxChipIgnoredCount, 10);
@@ -360,8 +352,6 @@ IN_PROC_BROWSER_TEST_P(
 
 IN_PROC_BROWSER_TEST_P(ReadAnythingEntryPointControllerOmniboxBrowserTest,
                        UpdatePageActionVisibility_ShowsPromo) {
-  EXPECT_TRUE(
-      ui_test_utils::NavigateToURL(browser(), GURL("https://www.google.com")));
   base::test::TestFuture<user_education::FeaturePromoResult> future;
 
   ReadAnythingEntryPointController::UpdatePageActionVisibility(
@@ -373,8 +363,6 @@ IN_PROC_BROWSER_TEST_P(ReadAnythingEntryPointControllerOmniboxBrowserTest,
 
 IN_PROC_BROWSER_TEST_P(ReadAnythingEntryPointControllerOmniboxBrowserTest,
                        UpdatePageActionVisibility_HidesPageAction) {
-  EXPECT_TRUE(
-      ui_test_utils::NavigateToURL(browser(), GURL("https://www.google.com")));
   RegisterPageActionObserver();
   browser()
       ->GetActiveTabInterface()
@@ -393,8 +381,6 @@ IN_PROC_BROWSER_TEST_P(ReadAnythingEntryPointControllerOmniboxBrowserTest,
 IN_PROC_BROWSER_TEST_P(ReadAnythingEntryPointControllerOmniboxBrowserTest,
                        UpdatePageActionVisibility_AbortsPromo) {
   base::HistogramTester histogram_tester;
-  EXPECT_TRUE(
-      ui_test_utils::NavigateToURL(browser(), GURL("https://www.google.com")));
   auto* const user_ed = BrowserUserEducationInterface::From(browser());
   base::test::TestFuture<user_education::FeaturePromoResult> future;
   ReadAnythingEntryPointController::UpdatePageActionVisibility(
