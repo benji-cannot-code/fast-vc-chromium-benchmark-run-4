ad6310248580e1d6a75ad010188a6720a9bb1746
Mohamed Amir Yosef <mamir@chromium.org>
2026-03-20 03:55:40 -0700

    [DC] Return NotAllowedError when cancelling the QR code dialog
    This CL ensures that explicitly cancelling the Digital Credentials flow
    from the desktop QR code or Bluetooth configuration dialogs returns a
    `NotAllowedError` instead of a generic `NetworkError`.
    
    This is achieved by updating
    `DigitalIdentityProviderDesktop::OnCanceled` to return
    `RequestStatusForMetrics::kErrorUserDeclined`. This error code is
    already plumbed to map to `NotAllowedError` in the renderer's
    `digital_identity_credential.cc`.
    
    Changes:
    - Update `DigitalIdentityProviderDesktop::OnCanceled` to return
      `kErrorUserDeclined`.
    - Move `Transact`, `ShowQrCodeDialog`, and associated members to
      `protected` in `DigitalIdentityProviderDesktop` to facilitate
      targeted integration testing via subclassing.
    - Add `QrCodeDialogCancelledReturnsNotAllowedError` integration test.
      The test uses a DCQL request that bypasses the safety interstitial to
      trigger the QR code flow, simulates a user cancellation, and verifies
      the resulting JavaScript error name.
    
    Bug: 494191081
    Change-Id: Ice69dfe05e52b20fbe14f7881b221e721199026e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7687490
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Rafał Godlewski <rgod@google.com>
    Auto-Submit: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1602513}

--- chrome/browser/digital_credentials/digital_identity_provider_desktop.cc
+++ chrome/browser/digital_credentials/digital_identity_provider_desktop.cc
@@ -391,7 +391,7 @@ void DigitalIdentityProviderDesktop::OnCableConnectingTimerComplete() {
 }
 
 void DigitalIdentityProviderDesktop::OnCanceled() {
-  EndRequestWithError(RequestStatusForMetrics::kErrorOther);
+  EndRequestWithError(RequestStatusForMetrics::kErrorUserDeclined);
 }
 
 void DigitalIdentityProviderDesktop::EndRequestWithError(
