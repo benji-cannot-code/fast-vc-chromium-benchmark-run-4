5354d9a3883e081baf1cf08c7f4104d34c409511
Maggie Chen <magchen@chromium.org>
2026-03-20 12:31:19 -0700

    Update DisplayLink TRACE EVENT on Mac
    Add a trace event for vsync callback in vsync_provider_mac.
    Update the trace event category for CADisplayLink and CVDisplayLink.
    
    Bug: 345275139
    Change-Id: I094cb5ebc8149862b4c3a967da3d05f96fcf32c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7685963
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1602774}

--- ui/display/mac/vsync_provider_mac.cc
+++ ui/display/mac/vsync_provider_mac.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/task/bind_post_task.h"
+#include "base/trace_event/trace_event.h"
 
 namespace ui {
 
@@ -110,6 +111,7 @@ void VSyncProviderMac::UnregisterCallback(VSyncCallbackMac::Callback callback,
 void VSyncProviderMac::OnVSync(const VSyncParamsMac& params,
                                int64_t display_id) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(vsync_sequence_checker_);
+  TRACE_EVENT0("gpu", "VSyncProviderMac::OnVSync");
 
   // DisplayLink entry might no longer exist.
   auto found = callback_lists_.find(display_id);
