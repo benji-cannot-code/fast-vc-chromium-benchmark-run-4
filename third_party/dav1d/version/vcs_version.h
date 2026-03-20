7f39dd8d5774aab6148d9c48105d0d604cbb4651
Wan-Teh Chang <wtc@google.com>
2026-03-20 16:45:08 -0700

    Roll src/third_party/dav1d/libdav1d/ 241a6b236..d69235dd8 (7 commits)
    https://chromium.googlesource.com/external/github.com/videolan/dav1d.git/+log/241a6b236a9c..d69235dd804b
    
    $ git log 241a6b236..d69235dd8 --date=short --no-merges --format='%ad %ae %s'
    2026-03-17 code CI: Use shortform QEMU_CPU for loongarch64
    2026-03-17 code CI: loongarch64: Move QEMU_LD_PREFIX to crossfile
    2026-03-17 code CI: riscv64: Move QEMU_LD_PREFIX to crossfile
    2026-03-17 code CI: aarch64: Move QEMU_LD_PREFIX to crossfile
    2026-03-17 code CI: Remove outdated version suffix from job name
    2026-03-11 martin arm: Add Armv9.3-A GCS (Guarded Control Stack) support
    2026-03-17 gramner Improve the memory pool API
    
    Created with:
      roll-dep src/third_party/dav1d/libdav1d
    R=dalecurtis@chromium.org,eugene@chromium.org
    
    Change-Id: Ibf9dc5fa28c377f734c3f084eaeb47564524dff6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7689636
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Auto-Submit: Wan-Teh Chang <wtc@google.com>
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1602911}

--- third_party/dav1d/version/vcs_version.h
+++ third_party/dav1d/version/vcs_version.h
@@ -1,3 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* auto-generated, do not edit */
-#define DAV1D_VERSION "1.5.3-21-g241a6b2"
+#define DAV1D_VERSION "1.5.3-28-gd69235dd"
