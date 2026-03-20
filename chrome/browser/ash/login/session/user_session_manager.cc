861bed8a4d975551533d0b523e29d6407d100ce4
Allen Ballway <ballway@google.com>
2026-03-20 16:05:40 -0700

    Display frozen update notifications
    Adds logic to user session manager to check if frozen update
    notifications should be displayed and show them if so.
    
    Bug: 444487784
    Change-Id: Id676fd725826b349209bc178a49d3045d9c88554
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7613124
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Allen Ballway <ballway@google.com>
    Cr-Commit-Position: refs/heads/main@{#1602897}

--- chrome/browser/ash/login/session/user_session_manager.cc
+++ chrome/browser/ash/login/session/user_session_manager.cc
@@ -150,6 +150,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/settings/cros_settings_names.h"
 #include "chromeos/ash/components/tpm/prepare_tpm.h"
 #include "chromeos/ash/experiences/arc/arc_prefs.h"
+#include "chromeos/ash/experiences/frozen_update/frozen_update_notification.h"
 #include "chromeos/dbus/tpm_manager/tpm_manager.pb.h"
 #include "chromeos/dbus/tpm_manager/tpm_manager_client.h"
 #include "chromeos/ui/base/app_types.h"
@@ -2388,6 +2389,27 @@ void UserSessionManager::CheckEolInfo(Profile* profile) {
   iter->second->CheckEolInfo();
 }
 
+void UserSessionManager::CheckFrozenUpdateInfo(user_manager::User* user) {
+  PrefService* prefs = user->GetProfilePrefs();
+  if (!prefs ||
+      !FrozenUpdateNotification::ShouldShowFrozenUpdateNotification(*prefs)) {
+    return;
+  }
+
+  auto iter = frozen_update_notification_handler_.find(user->GetAccountId());
+  if (iter == frozen_update_notification_handler_.end()) {
+    auto frozen_update_notification =
+        std::make_unique<FrozenUpdateNotification>(*prefs);
+
+    iter = frozen_update_notification_handler_
+               .insert(std::make_pair(user->GetAccountId(),
+                                      std::move(frozen_update_notification)))
+               .first;
+  }
+
+  iter->second->MaybeShowNotification();
+}
+
 void UserSessionManager::DoBrowserLaunchInternal(Profile* profile,
                                                  bool locale_pref_checked) {
   TRACE_EVENT0("login", "UserSessionManager::DoBrowserLaunchInternal");
@@ -2503,9 +2525,14 @@ void UserSessionManager::DoBrowserLaunchInternal(Profile* profile,
   BootTimesRecorder::Get()->LoginDone(
       user_manager::UserManager::Get()->IsCurrentUserNew());
 
-  // Check to see if this profile should show EndOfLife Notification and show
+  // Check to see if this user should show EndOfLife Notification and show
   // the message accordingly.
   CheckEolInfo(profile);
+
+  // Check to see if this profile should show the FrozenUpdateNotification and
+  // show the message accordingly.
+  user_manager::User* user = ProfileHelper::Get()->GetUserByProfile(profile);
+  CheckFrozenUpdateInfo(user);
 
   ShowNotificationsIfNeeded(profile);
 }
@@ -2618,6 +2645,7 @@ void UserSessionManager::Shutdown() {
   password_was_saved_ = false;
   xdr_manager_.reset();
   token_handle_store_ = nullptr;
+  frozen_update_notification_handler_.clear();
 }
 
 void UserSessionManager::SetSwitchesForUser(
