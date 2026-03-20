fac4b8db05e77a2422eaddeafbd206127ec2f645
Edman Anjos <edmanp@google.com>
2026-03-20 03:42:58 -0700

    iwa: Implement chromeos.isolatedWebApp.setShape
    This implements the setShape blink extension for IWAs in CrOS. Currently
    the API is disabled by default, future CLs will introduce an allowlist
    of IWAs for which the API is enabled in CrOS.
    
    Enable the API locally with this line in /etc/chrome_dev.conf:
    --enable-blink-features=BlinkExtensionChromeOS,BlinkExtensionChromeOSIsolatedWebAppSetShape
    
    Fixed: 480146201
    Test: fd test chromeos/ash/experiences/isolated_web_app | xargs testing/xvfb.py tools/autotest.py -C out_/Default
    Test: testing/xvfb.py tools/autotest.py -C out_/Default chrome/browser/ash/isolated_web_app/blink_extensions_browsertest.cc
    Change-Id: Ie290a6f3b2e5585ab3149da798925033a7c39ffd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7566876
    Commit-Queue: Edman Anjos <edman@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Simon Hangl <simonha@google.com>
    Cr-Commit-Position: refs/heads/main@{#1602512}

---
+++ chromeos/ash/experiences/isolated_web_app/shaped_window_targeter.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/ash/experiences/isolated_web_app/shaped_window_targeter.h"
+
+#include <memory>
+#include <vector>
+
+#include "ui/aura/window.h"
+#include "ui/aura/window_targeter.h"
+#include "ui/gfx/geometry/rect.h"
+
+namespace ash {
+
+ShapedWindowTargeter::ShapedWindowTargeter(std::vector<gfx::Rect> shape_rects)
+    : shape_rects_(std::move(shape_rects)) {}
+
+ShapedWindowTargeter::~ShapedWindowTargeter() = default;
+
+std::unique_ptr<aura::WindowTargeter::HitTestRects>
+ShapedWindowTargeter::GetExtraHitTestShapeRects(aura::Window* target) const {
+  return std::make_unique<aura::WindowTargeter::HitTestRects>(shape_rects_);
+}
+
+}  // namespace ash
