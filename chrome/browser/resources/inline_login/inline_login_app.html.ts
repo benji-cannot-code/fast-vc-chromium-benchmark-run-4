6593b408d32dbea20e3559595ee5018388a6c2f1
rbpotter <rbpotter@chromium.org>
2026-03-20 13:57:17 -0700

    WebUI: Migrate inline-login-app from Polymer to Lit
    Bug: 493823435
    Change-Id: I3a9763114350f789b98cc9ae39a74f2907b423ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7676781
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1602832}

---
+++ chrome/browser/resources/inline_login/inline_login_app.html.ts
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {InlineLoginAppElement} from './inline_login_app.js';
+
+export function getHtml(this: InlineLoginAppElement) {
+  // clang-format off
+  return html`<!--_html_template_start_-->
+<div id="spinner" class="spinner" ?hidden="${!this.isSpinnerActive_()}">
+</div>
+<webview id="signinFrame" name="signin-frame" class="signin-frame"
+    ?hidden="${this.isSpinnerActive_()}" allowscaling>
+</webview>
+<!--_html_template_end_-->`;
+  // clang-format on
+}
