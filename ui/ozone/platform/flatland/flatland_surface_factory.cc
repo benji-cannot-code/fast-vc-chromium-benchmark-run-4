46c299bf0073896c86bd8895c77884fdc3191151
Andrew Paseltiner <apaseltiner@chromium.org>
2026-03-20 08:13:23 -0700

    [viz] Optimize SharedImageFormat set usage in Flatland and GL utils
    - In gpu/command_buffer/service/gl_utils.cc:
      Use the base::flat_set initializer list constructor directly for
      mappable_formats instead of through an intermediate vector.
    - In FlatlandSurfaceFactory and FlatlandSysmemBufferCollection:
      Update the initialization of kSupportedFormats to use the flat_set
      initializer list constructor directly.
    
    These sites previously used base::MakeFlatSet with a temporary
    std::vector. Since base::MakeFlatSet takes its input container by const
    ref, this resulted in an unnecessary vector being created and copied.
    
    Note: Using base::MakeFixedFlatSet for these format lists would be
    preferable for performance, but it's currently not possible because
    viz::SharedImageFormat lacks constexpr comparison operators.
    
    Bug: 494183505
    Change-Id: I798c9a9dd6259afc6964fafe9a56c967b3fcc369
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7684276
    Reviewed-by: David Song <wintermelons@google.com>
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1602621}

--- ui/ozone/platform/flatland/flatland_surface_factory.cc
+++ ui/ozone/platform/flatland/flatland_surface_factory.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/containers/flat_set.h"
 #include "base/fuchsia/fuchsia_logging.h"
 #include "base/fuchsia/process_context.h"
 #include "base/functional/bind.h"
@@ -206,13 +207,11 @@ FlatlandSurfaceFactory::CreateVulkanImplementation(
 
 bool FlatlandSurfaceFactory::IsFormatSupportedForTexturing(
     viz::SharedImageFormat format) const {
-  base::flat_set<viz::SharedImageFormat> kSupportedFormats =
-      base::MakeFlatSet<viz::SharedImageFormat>(std::vector(
-          {viz::SinglePlaneFormat::kR_8, viz::SinglePlaneFormat::kRG_88,
-           viz::SinglePlaneFormat::kRGBA_8888,
-           viz::SinglePlaneFormat::kBGRA_8888,
-           viz::SinglePlaneFormat::kRGBX_8888,
-           viz::SinglePlaneFormat::kBGRX_8888, viz::MultiPlaneFormat::kNV12}));
+  auto kSupportedFormats = base::flat_set<viz::SharedImageFormat>(
+      {viz::SinglePlaneFormat::kR_8, viz::SinglePlaneFormat::kRG_88,
+       viz::SinglePlaneFormat::kRGBA_8888, viz::SinglePlaneFormat::kBGRA_8888,
+       viz::SinglePlaneFormat::kRGBX_8888, viz::SinglePlaneFormat::kBGRX_8888,
+       viz::MultiPlaneFormat::kNV12});
   return kSupportedFormats.contains(format);
 }
 
