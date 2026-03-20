d50b01b73109a362244b0a4b8c6b8ad0c529811a
Louis Romero <lpromero@google.com>
2026-03-20 09:38:08 -0700

    Add experimental setting for Cobrowse GWS URL
    This CL adds an experimental setting to set a different GWS URL than
    google.com for Cobrowse.
    
    Bug: 484938524
    Change-Id: Ib49170bbe1bf31cc602fdd24ab1f923a49cf4182
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7675098
    Reviewed-by: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1602670}

--- ios/chrome/browser/cobrowse/model/cobrowse_context.mm
+++ ios/chrome/browser/cobrowse/model/cobrowse_context.mm
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/cobrowse/model/cobrowse_context.h"
 
+#import "base/logging.h"
 #import "base/strings/sys_string_conversions.h"
+#import "ios/chrome/browser/shared/public/features/system_flags.h"
 #import "net/base/url_util.h"
 #import "url/gurl.h"
 
@@ -24,7 +26,21 @@ const char kBaseSearchURL[] = "https://www.google.com/search?udm=50";
 - (instancetype)initWithURL:(const GURL&)url {
   self = [super init];
   if (self) {
-    _url = net::AppendOrReplaceQueryParameter(url, "gsc", "2");
+    NSString* overrideURL = experimental_flags::GetCobrowseGwsURL();
+    if (overrideURL) {
+      DVLOG(1)
+          << "\n"
+          << "***********************************************************\n"
+          << "*                                                         *\n"
+          << "*   COBROWSE GWS URL OVERRIDDEN VIA EXPERIMENTAL SETTINGS *\n"
+          << "*   URL: " << base::SysNSStringToUTF8(overrideURL) << "\n"
+          << "*                                                         *\n"
+          << "***********************************************************\n";
+      _url = GURL(base::SysNSStringToUTF8(overrideURL));
+    } else {
+      _url = url;
+    }
+    _url = net::AppendOrReplaceQueryParameter(_url, "gsc", "2");
     _url = net::AppendOrReplaceQueryParameter(_url, "sourceid", "chrome-mobile");
     _url = net::AppendOrReplaceQueryParameter(_url, "gsas", "4");
 
