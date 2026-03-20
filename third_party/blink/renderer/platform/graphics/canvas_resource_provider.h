750c09dcf7cf453fe7b3b197260f04c0e81819a4
Colin Blundell <blundell@chromium.org>
2026-03-20 01:06:33 -0700

    [Blink] Make CanvasResourceProvider::LastRecording() Canvas2D-specific
    The only callsite is from CanvasRenderingContext2D.
    
    Bug: 352263194
    Change-Id: Iefa9cb26ed49b5a6fd2411863dbd1b03b16b7e0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7680498
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1602472}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
@@ -190,7 +190,8 @@ class PLATFORM_EXPORT CanvasResourceProvider
     always_enable_raster_timers_for_testing_ = value;
   }
 
-  const std::optional<cc::PaintRecord>& LastRecording() {
+  const std::optional<cc::PaintRecord>& LastRecordingForCanvas2D() {
+    CHECK(IsCanvas2D());
     return last_recording_;
   }
 
