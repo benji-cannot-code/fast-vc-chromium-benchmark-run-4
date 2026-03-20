47e214ecfe3bcbb2f2fcbe6b5b59d876312b9e88
Jeremy Roman <jbroman@chromium.org>
2026-03-20 12:09:35 -0700

    Close IndigoOnboardingDialog when web content calls window.close().
    This allows the onboarding page to close itself, either on explicit
    action or when the page determines that it's complete.
    
    Bug: b:491559009
    Change-Id: Ib5fb2f9033b32855194e0f5593c25b080b861680
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7686080
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1602762}

--- chrome/browser/indigo/onboarding/indigo_onboarding_dialog.cc
+++ chrome/browser/indigo/onboarding/indigo_onboarding_dialog.cc
@@ -40,6 +40,10 @@ class OnboardingWebView : public views::WebView {
  public:
   using WebView::WebView;
 
+  void SetOnCloseCallback(base::OnceClosure close_callback) {
+    close_callback_ = std::move(close_callback);
+  }
+
   void SetBrowserWindowCallback(
       base::RepeatingCallback<BrowserWindowInterface*()>
           browser_window_callback) {
@@ -47,6 +51,12 @@ class OnboardingWebView : public views::WebView {
   }
 
   // WebContentsDelegate:
+
+  void CloseContents(content::WebContents* web_contents) override {
+    if (close_callback_) {
+      std::move(close_callback_).Run();
+    }
+  }
 
   void RunFileChooser(content::RenderFrameHost* render_frame_host,
                       scoped_refptr<content::FileSelectListener> listener,
@@ -94,6 +104,7 @@ class OnboardingWebView : public views::WebView {
   }
 
   base::RepeatingCallback<BrowserWindowInterface*()> browser_window_callback_;
+  base::OnceClosure close_callback_;
   views::UnhandledKeyboardEventHandler unhandled_keyboard_event_handler_;
 };
 
@@ -120,6 +131,8 @@ IndigoOnboardingDialog::IndigoOnboardingDialog(tabs::TabInterface& tab,
   Profile* profile =
       Profile::FromBrowserContext(tab.GetContents()->GetBrowserContext());
   auto web_view = std::make_unique<OnboardingWebView>(profile);
+  web_view->SetOnCloseCallback(
+      base::BindOnce(&IndigoOnboardingDialog::Close, base::Unretained(this)));
   web_view->SetBrowserWindowCallback(base::BindRepeating(
       [](const base::WeakPtr<tabs::TabInterface>& tab) {
         return tab ? tab->GetBrowserWindowInterface() : nullptr;
