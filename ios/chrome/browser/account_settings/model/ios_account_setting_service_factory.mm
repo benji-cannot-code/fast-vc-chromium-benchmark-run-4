1a9b1f4022fab8220daf5a2a01bd8ab81f214c25
Alexis Hetu <sugoi@chromium.org>
2026-03-20 09:43:19 -0700

    Reland "[iOS] Move IOSAccountSettingServiceFactory to account_settings"
    This is a reland of commit 5dd14c6f562f949cfbcd8ed5dbe0ab9c17ab2a1d
    
    Original change's description:
    > [iOS] Move IOSAccountSettingServiceFactory to account_settings
    >
    > This CL moves IOSAccountSettingServiceFactory to a new directory:
    > ios/chrome/browser/account_settings/model/
    >
    > This will allow using IOSAccountSettingServiceFactory in
    > ios/chrome/browser/sync/model while avoiding the circular dependency:
    >   //ios/chrome/browser/sync/model:model ->
    >   //ios/chrome/browser/autofill/model:model ->
    >   //ios/chrome/browser/sync/model:model
    >
    > Bug: 493865594
    > Change-Id: I46517bce12e63f72841a4782af4a7d43ddfbc055
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7685376
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Reviewed-by: Vidhan Jain <vidhanj@google.com>
    > Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1602518}
    
    Bug: 493865594
    Change-Id: Id68a78bfdb40c33c1616ae414f5108ef813037e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7685894
    Reviewed-by: Vidhan Jain <vidhanj@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1602679}

--- ios/chrome/browser/account_settings/model/ios_account_setting_service_factory.mm
+++ ios/chrome/browser/account_settings/model/ios_account_setting_service_factory.mm
@@ -3,10 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/autofill/model/ios_account_setting_service_factory.h"
+#import "ios/chrome/browser/account_settings/model/ios_account_setting_service_factory.h"
 
 #import "base/functional/callback_helpers.h"
 #import "components/autofill/core/browser/webdata/account_settings/account_setting_service.h"
+#import "components/autofill/core/browser/webdata/account_settings/account_setting_sync_bridge.h"
 #import "components/sync/base/features.h"
 #import "components/sync/model/client_tag_based_data_type_processor.h"
 #import "components/sync/model/data_type_store_service.h"
