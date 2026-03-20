2f4ed4d2c8fc0449cbbfac38f7f696f0045eb157
Alexei Svitkine <asvitkine@chromium.org>
2026-03-20 15:24:50 -0700

    Move Crashpad and crash-related histograms to stability sub-dir.
    This change relocates enums and histograms related to crashes and
    stability from tools/metrics/histograms/enums.xml and
    tools/metrics/histograms/metadata/others/histograms.xml to
    tools/metrics/histograms/metadata/stability/enums.xml and
    tools/metrics/histograms/metadata/stability/histograms.xml,
    respectively. Comments in chrome_result_codes.h and result_codes.h are
    updated to reflect the new file paths.
    
    Change-Id: If4e899941d4ec163e2b1ed3bd1f63eaa3a124f81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7689190
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Auto-Submit: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1602874}

--- chrome/common/chrome_result_codes.h
+++ chrome/common/chrome_result_codes.h
@@ -10,8 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "partition_alloc/page_allocator.h"
 
 // IMPORTANT: This needs to stay in sync with <enum name="CrashExitCodes"> and
-// <enum name="WindowsExitCode"> in tools/metrics/histograms/enums.xml. So do
-// not remove any entries, and always append entries to the bottom just above
+// <enum name="WindowsExitCode"> in
+// tools/metrics/histograms/metadata/stability/enums.xml. So do not remove any
+// entries, and always append entries to the bottom just above
 // RESULT_CODE_CHROME_LAST_CODE.
 
 enum ResultCode {
