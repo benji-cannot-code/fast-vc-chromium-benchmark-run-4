49f4e89996e15bd3720c9531f610fa445676c55d
Dominic Farolino <dom@chromium.org>
2026-03-19 19:37:01 -0700

    WebMCP: Fix declarative form detachment crash
    Mmmm, events detaching DOM elements, my favorite. This CL fixes a crash
    in declarative WebMCP forms, whereby a form is detached from the DOM
    and unregistered as a tool synchronously when the `submit` event is
    fired during declarative tool execution. When this happens, we should
    NOT react to the Promise passed to `SubmitEvent::respondWith()`,
    registering `HTMLFormElement::HandleWebMcpToolResponse()` as a
    reaction handler. Instead, do nothing.
    
    This allows us to assert that `tool` is non-null in the aforementioned
    method.
    
    R=masonf
    
    Bug: 489045948,493850055
    Change-Id: I987dacf14d5425bad01f3464bd62b3538a1f3663
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7684654
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1602400}

--- third_party/blink/renderer/core/html/forms/html_form_element.cc
+++ third_party/blink/renderer/core/html/forms/html_form_element.cc
@@ -280,6 +280,7 @@ void HTMLFormElement::HandleWebMcpToolResponse(HTMLFormMcpTool* tool,
                                                bool resolved,
                                                ScriptState* script_state,
                                                ScriptValue value) {
+  CHECK(tool);
   if (!tool->CurrentlyRunning()) {
     return;
   }
@@ -587,7 +588,8 @@ void HTMLFormElement::PrepareForSubmission(
 
     UseCounter::Count(GetDocument(), WebFeature::kFormSubmissionStarted);
     // Interactive validation must be done before dispatching the submit event.
-    const bool declarative_webmcp_call =
+    // We also re-perform this validation *after* dispatching the submit event.
+    bool declarative_webmcp_call =
         IsValidWebMCPForm() && active_webmcp_tool_->CurrentlyRunning();
     if (!skip_validation && !ValidateInteractively()) {
       should_submit = false;
@@ -613,6 +615,19 @@ void HTMLFormElement::PrepareForSubmission(
           event_type_names::kSubmit, submit_event_init);
       should_submit =
           DispatchEvent(*submit_event) == DispatchEventResult::kNotCanceled;
+      // `DispatchEvent()` above could have disconnected `this` from the DOM. In
+      // that case, the form would have been unregistered as a tool,
+      // `active_webmcp_tool_` will be null, and `IsValidWebMCPForm()` will be
+      // false; there's no need to react to the Promise held by
+      // `SubmitEvent::respondWith()`.
+      //
+      // If `active_webmcp_tool_` is non-null here, but the form gets
+      // unregistered as a tool asynchronously before `promise` fulfills, then
+      // `HandleWebMcpToolResponse()` will handle this appropriately.
+      //
+      // To handle all of this, update the boolean.
+      declarative_webmcp_call =
+          IsValidWebMCPForm() && active_webmcp_tool_->CurrentlyRunning();
       if (declarative_webmcp_call) {
         if (auto promise = submit_event->TakeRespondWithPromise()) {
           // Since we have a promise, respondWith() was called. That should only
@@ -621,6 +636,7 @@ void HTMLFormElement::PrepareForSubmission(
           CHECK(!should_submit);
           // Wait for the provided promise to resolve or reject, and then call
           // the active_webmcp_tool_'s callback with the result.
+          CHECK(active_webmcp_tool_.Get());
           std::move(*promise).Then(
               BindOnce(&HTMLFormElement::HandleWebMcpToolResponse,
                        WrapWeakPersistent(this),
