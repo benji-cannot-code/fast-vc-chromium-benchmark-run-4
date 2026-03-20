eb26f77c2c159b55f29dce11d9f2f8560620bac6
Darwin Yang <darwinyang@google.com>
2026-03-20 07:48:23 -0700

    [spc][cleanup] Clean up SPC Fallback feature flag
    Note that the SPC fallback flag is no longer used and is replaced by
    the SPC UX Refresh feature flag.
    
    Bug: 430541440
    Change-Id: I995251613d76f805fbc80dccade8fa95596c83f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7680876
    Commit-Queue: Darwin Yang <darwinyang@chromium.org>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1602602}

--- components/payments/content/android/java/src/org/chromium/components/payments/PaymentRequestService.java
+++ components/payments/content/android/java/src/org/chromium/components/payments/PaymentRequestService.java
@@ -688,10 +688,8 @@ public class PaymentRequestService
             // credentials to do that.
             if (obscureRealError
                     && reason == PaymentErrorReason.USER_CANCEL
-                    && (PaymentFeatureList.isEnabledOrExperimentalFeaturesEnabled(
-                                    PaymentFeatureList.SECURE_PAYMENT_CONFIRMATION_FALLBACK)
-                            || ContentFeatureMap.isEnabled(
-                                    BlinkFeatures.SECURE_PAYMENT_CONFIRMATION_UX_REFRESH))) {
+                    && ContentFeatureMap.isEnabled(
+                            BlinkFeatures.SECURE_PAYMENT_CONFIRMATION_UX_REFRESH)) {
                 obscureRealError = false;
             }
 
@@ -982,10 +980,7 @@ public class PaymentRequestService
     }
 
     private boolean shouldShowSecurePaymentConfirmationFallback() {
-        if (!(PaymentFeatureList.isEnabledOrExperimentalFeaturesEnabled(
-                        PaymentFeatureList.SECURE_PAYMENT_CONFIRMATION_FALLBACK)
-                || ContentFeatureMap.isEnabled(
-                        BlinkFeatures.SECURE_PAYMENT_CONFIRMATION_UX_REFRESH))) {
+        if (!ContentFeatureMap.isEnabled(BlinkFeatures.SECURE_PAYMENT_CONFIRMATION_UX_REFRESH)) {
             return false;
         }
         assert mSpec.isSecurePaymentConfirmationRequested();
--- components/payments/core/features.cc
+++ components/payments/core/features.cc
@@ -45,8 +45,6 @@ BASE_FEATURE(kSecurePaymentConfirmationUseCredentialStoreAPIs,
 #endif
 );
 
-BASE_FEATURE(kSecurePaymentConfirmationFallback,
-             base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kPaymentRequestUseRendererUrlLoader,
              base::FEATURE_DISABLED_BY_DEFAULT);
