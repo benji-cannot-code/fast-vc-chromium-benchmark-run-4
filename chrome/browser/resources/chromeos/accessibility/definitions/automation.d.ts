aa8b170a686654ebc1fd3b8990feea2d149735a6
Tim Judkins <tjudkins@chromium.org>
2026-03-20 14:31:42 -0700

    [Extensions] Convert automation to WebIDL
    This conversion was largely done using a detailed description of the
    conversion process passed to Gemini CLI, although it struggled with a
    schema of this size and complexity so I had to step in and fix a lot of
    the small things which caused problems. To double check this work, the
    file has also been copied into the converted schemas test to verify no
    functional difference in output.
    
    The automation API has a lot of weird edge cases and defines a lot of
    dictionaries which should really be full on interfaces, but since we don't currently support nested interfaces like that yet this will do for now.
    
    It also extensively uses the `[nocompile]` extended attribute, which due
    the broken way in which the old IDL processor handles this property end
    up quite ugly in this conversion to ensure zero-difference in the output. A followup will improve this situation by moving `[nocompile]`
    tags back to the top level of dictionaries and adding it to the top
    level of the schema, like was originally intended in this schema.
    
    AX-Relnotes: n/a
    Bug: 452423217
    Change-Id: I87aebaabeb14357def712c8f121aeaa95dcf830a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7670315
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Erik Staab <estaab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1602848}

--- chrome/browser/resources/chromeos/accessibility/definitions/automation.d.ts
+++ chrome/browser/resources/chromeos/accessibility/definitions/automation.d.ts
@@ -5,9 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /**
  * @fileoverview Definitions for chrome.automation API
- * Generated from: extensions/common/api/automation.idl
+ * Generated from: extensions/common/api/automation.webidl
  * run `tools/json_schema_compiler/compiler.py
- * extensions/common/api/automation.idl -g ts_definitions` to regenerate.
+ * extensions/common/api/automation.webidl -g ts_definitions` to regenerate.
  */
 
 
