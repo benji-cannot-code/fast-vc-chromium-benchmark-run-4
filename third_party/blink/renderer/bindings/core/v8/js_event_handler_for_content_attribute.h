cfd52d82e727254f05b11b832b0114feb13b8278
Josh Karlin <jkarlin@chromium.org>
2026-03-20 10:17:59 -0700

    [AdTracker] Identify attribute script created by ad script
    One form of ad script that the AdTracker currently misses is attribute
    script created by ad script attached to some element. This CL calls
    IsAdScriptInStack at parsing time, and later tells the AdTracker
    about the ad's id and ancestry at compile time.
    
    The overall runtime cost is minimal. IsAdScriptInStack calls in which
    there is no running JS (e.g., document parsing) will avoid stack traces
    so we don't have to worry about slowing down non-ad document parsing.
    
    Testing on some ad heavy pages shows that this only represents 7% of
    the total calls to IsAdScriptInStack on large news sites.
    
    Bug: 492558468
    Change-Id: I8024fbed2afade67fb60e4b728a56e1f6a5b12d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7662275
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Josh Karlin <jkarlin@chromium.org>
    Reviewed-by: Yao Xiao <yaoxia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1602701}

--- third_party/blink/renderer/bindings/core/v8/js_event_handler_for_content_attribute.h
+++ third_party/blink/renderer/bindings/core/v8/js_event_handler_for_content_attribute.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_BINDINGS_CORE_V8_JS_EVENT_HANDLER_FOR_CONTENT_ATTRIBUTE_H_
 
 #include "third_party/blink/renderer/bindings/core/v8/js_event_handler.h"
+#include "third_party/blink/renderer/core/ad_tracker/ad_script_identifier.h"
 #include "third_party/blink/renderer/platform/bindings/dom_wrapper_world.h"
 #include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
 #include "third_party/blink/renderer/platform/wtf/text/text_position.h"
@@ -38,6 +39,11 @@ class JSEventHandlerForContentAttribute final : public JSEventHandler {
   SourceLocation* GetSourceLocation(EventTarget&) override;
 
   const String& ScriptBody() const override { return script_body_; }
+
+  void SetAdRelated(const std::optional<AdScriptIdentifier>& ad_script) {
+    is_ad_related_ = true;
+    parent_ad_script_ = ad_script;
+  }
 
  protected:
   // blink::JSBasedEventListener override:
@@ -74,6 +80,8 @@ class JSEventHandlerForContentAttribute final : public JSEventHandler {
   String source_url_;
   TextPosition position_;
   v8::Isolate* isolate_;
+  bool is_ad_related_ = false;
+  std::optional<AdScriptIdentifier> parent_ad_script_;
 };
 
 }  // namespace blink
