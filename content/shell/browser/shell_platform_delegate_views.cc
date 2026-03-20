67d2512c6f8a0c2e3cd82240f9741da5a51b8757
Keren Zhu <kerenzhu@chromium.org>
2026-03-19 23:29:39 -0700

    [content_shell] Fix white flash during initial rendering with --force-dark-mode
    When launching content_shell with --force-dark-mode, there is an white
    flash before the first paint of the web content. This flash is caused by
    the RenderWidgetHostView defaulting to SK_ColorWHITE (or an
    uninitialized background color) which overlays the NativeView.
    
    This CL initializes the WebContents' background color to match the
    Widget's background color via views::WebContentsSetBackgroundColor
    before showing the window.
    
    Pre-fix screencast:
    https://drive.google.com/file/d/1Rl52Tr6o1qbVhL_yxIFVuRrrVm16GCFI/view?usp=sharing&resourcekey=0-REau0BvbyGgssOh4iUK4fg
    Post-fix screencast:
    https://drive.google.com/file/d/1AYOlMF2Z5l9Mbel4T3s3uMMweQbBoiiQ/view?usp=sharing&resourcekey=0-IviG7ajtBZJVz3rmpphYFw
    
    To test manually, run:
    
    ./out/Default/content_shell --force-dark-mode "data:text/html,<body
    bgcolor=black></body>"
    
    Credit to andrewsavage@google.com who reported the bug. The issue was
    initially observed in Cobalt, a Chromium-based YouTube TV app framework.
    
    Bug: none
    Change-Id: Icb3f474c65b9c4c003d09994d5fc8d830a131f98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7676223
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1602439}

--- content/shell/browser/shell_platform_delegate_views.cc
+++ content/shell/browser/shell_platform_delegate_views.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/button/md_text_button.h"
 #include "ui/views/controls/textfield/textfield.h"
 #include "ui/views/controls/textfield/textfield_controller.h"
+#include "ui/views/controls/webview/web_contents_set_background_color.h"
 #include "ui/views/controls/webview/webview.h"
 #include "ui/views/layout/box_layout_view.h"
 #include "ui/views/layout/flex_layout_types.h"
@@ -403,6 +404,12 @@ void ShellPlatformDelegate::SetContents(Shell* shell) {
 
   ShellViewForWidget(shell_data.window_widget)
       ->SetWebContents(shell->web_contents(), shell_data.content_size);
+
+  SkColor bg_color = shell_data.window_widget->GetColorProvider()->GetColor(
+      ui::kColorWindowBackground);
+  views::WebContentsSetBackgroundColor::CreateForWebContentsWithColor(
+      shell->web_contents(), bg_color);
+
   shell_data.window_widget->GetNativeWindow()->GetHost()->Show();
   shell_data.window_widget->Show();
 }
