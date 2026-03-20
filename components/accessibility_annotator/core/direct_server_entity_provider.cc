34fd884f5adcc209328ad42074a22d97a1c3f559
Norge Vizcay <vizcay@google.com>
2026-03-20 11:20:56 -0700

    accessibility_annotator: Refactor DirectServerEntityProvider and tests
    Refactor DirectServerEntityProvider to improve location and code style.
    
    Update DirectServerEntityProviderTest to use base::test::TestFuture for
    asynchronous testing, replacing base::RunLoop and
    base::MockOnceCallback. Also clean up unused headers and improve Gmock
    matchers.
    
    Bug: None
    Test: components_unittests --gtest_filter=DirectServerEntityProviderTest.*
    Change-Id: Iba9dadf1c9281179b1ef4c4407bd953b6fe0d14e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7687435
    Reviewed-by: Andrew Vuong <akvuong@google.com>
    Auto-Submit: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1602737}

--- components/accessibility_annotator/core/direct_server_entity_provider.cc
+++ components/accessibility_annotator/core/direct_server_entity_provider.cc
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
-#include "base/functional/bind.h"
-#include "base/task/sequenced_task_runner.h"
 #include "components/accessibility_annotator/core/data_models/entity_converter.h"
 #include "components/accessibility_annotator/core/storage/accessibility_annotator_backend.h"
 
@@ -45,10 +43,12 @@ void DirectServerEntityProvider::GetEntities(
     return;
   }
 
+  std::vector<sync_pb::AccessibilityAnnotationSpecifics> annotations =
+      bridge->GetAnnotationsByTypes(types);
   std::vector<Entity> entities;
-  for (const auto& specifics : bridge->GetAnnotationsByTypes(types)) {
-    std::optional<Entity> entity = CreateEntityFromSpecifics(specifics);
-    if (entity.has_value()) {
+  entities.reserve(annotations.size());
+  for (const auto& specifics : annotations) {
+    if (std::optional<Entity> entity = CreateEntityFromSpecifics(specifics)) {
       entities.push_back(std::move(*entity));
     }
   }
