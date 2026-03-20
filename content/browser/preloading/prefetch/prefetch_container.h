04e831f34592d3f0f9d97f031d4337be5b954c8c
Taiyo Mizuhashi <taiyo@chromium.org>
2026-03-20 01:07:52 -0700

    OMTPrefetch: Create a prefetch dedup function from external records
    `AwPrefetchManager` needs to perform the Prefetch/PrePrefetch
    deduplication taking both prefetch/preprefetch into account on non-main
    thread. However, the current function using for dedup
    `BrowserContext::IsPrefetchDuplicate` ->
    `PrefetchService::IsPrefetchDuplicate` ties on the UI thread
    `PrefetchService::owned_prefetches_`.
    
    To 1) keep the deduplication logic in a one place 2) somewhere in the
    prefetch component, this CL decouples the deduplication logic by making
    this accept `std::vector<PrefetchDeduplicationEntry>` (which will be
    either PrefetchService::owned_prefetches_, or `AwPrefetchManager`'s
    Prefetch/PrePrefetch map in a next CL (*)) externally instead.
    
    More discussion log:
    https://docs.google.com/document/d/1sg1iJ6ueuMCvgesMcXJfN9dX4jq2tngXoanLf9Ogct4/edit?resourcekey=0-X2fTM2j-DoiTij-R-u9APQ&tab=t.0#bookmark=id.vmoaoabei2v7
    
    Note that this is a short-term logic for OMT.Prefetch. As a longer-term
    we want all the deduplication logic into UPC. Relevant discussions:
    https://docs.google.com/document/d/1TvPFWq3hPXvHSyKG376YpbPm9mJiQcYU7nT19g5MyE0/edit?tab=t.0#heading=h.f4rogtuhh7yr
    
    (*) Content owners: Please see the next CL crrev.com/c/7656849 for the
    embedder usage.
    
    Bug: 490251714, 452406598, 452389538
    Change-Id: I65677b078b41b540aa78aeb25aa0c28a2b16c538
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7622797
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Ken Okada <kenoss@chromium.org>
    Commit-Queue: Taiyo Mizuhashi <taiyo@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1602473}

--- content/browser/preloading/prefetch/prefetch_container.h
+++ content/browser/preloading/prefetch/prefetch_container.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/preloading/preload_pipeline_info_impl.h"
 #include "content/browser/preloading/preload_serving_metrics.h"
 #include "content/common/content_export.h"
+#include "content/public/browser/prefetch_deduplication_utils.h"
 #include "content/public/browser/preloading.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "net/http/http_no_vary_search_data.h"
@@ -172,7 +173,11 @@ enum class PrefetchContainerLoadState {
 // - `PrefetchService::OnGotEligibilityForRedirect()` [redirect].
 // A new `PrefetchStreamingURLLoader` is also created if needed in
 // `PrefetchService::MakePrefetchRequest()`.
-class CONTENT_EXPORT PrefetchContainer {
+//
+// Do not pass `PrefetchContainer` to outside //content using
+// `PrefetchDeduplicationEntry`.
+class CONTENT_EXPORT PrefetchContainer
+    : public content::PrefetchDeduplicationEntry {
  public:
   // In non-test, `PrefetchContainer` should be only created and owned by
   // `PrefetchService`.
@@ -186,7 +191,7 @@ class CONTENT_EXPORT PrefetchContainer {
   PrefetchContainer(base::PassKey<PrefetchContainer>,
                     std::unique_ptr<const PrefetchRequest> request);
 
-  ~PrefetchContainer();
+  ~PrefetchContainer() override;
 
   PrefetchContainer(const PrefetchContainer&) = delete;
   PrefetchContainer& operator=(const PrefetchContainer&) = delete;
@@ -268,10 +273,26 @@ class CONTENT_EXPORT PrefetchContainer {
 
   void OnWillBeDestroyed();
 
-  const PrefetchKey& key() const;
-
+  // `content::PrefetchDeduplicationEntry` overrides.
   // The initial URL that was requested to be prefetched.
-  const GURL& GetURL() const;
+  // Equivalent to `request().key().url()`.
+  const GURL& GetURL() const override;
+
+  // Equivalent to `request().no_vary_search_hint()`.
+  // Exposed for `PrefetchMatchResolver`.
+  const std::optional<net::HttpNoVarySearchData>& GetNoVarySearchHint()
+      const override;
+
+  // Returns `true` if the `prefetch_container` is stale. I.e.
+  // the prefetch either is not or never will be servable to a
+  // navigation.
+  //
+  // Note: This is currently used for WebView initiated prefetches so
+  // consideration should be taken if updating the underlying implementation (or
+  // its dependencies).
+  bool IsPrefetchStale() const override;
+
+  const PrefetchKey& key() const;
 
   // The current URL being fetched.
   GURL GetCurrentURL() const;
@@ -305,10 +326,6 @@ class CONTENT_EXPORT PrefetchContainer {
   // `SetPrefetchStatusWithoutUpdatingTriggeringOutcome()`, where resource
   // request might not yet created.
   const std::string& GetDevtoolsRequestId() const;
-
-  // Equivalent to `request().no_vary_search_hint()`.
-  // Exposed for `PrefetchMatchResolver`.
-  const std::optional<net::HttpNoVarySearchData>& GetNoVarySearchHint() const;
 
   base::WeakPtr<PrefetchContainer> GetWeakPtr() {
     return weak_method_factory_.GetWeakPtr();
