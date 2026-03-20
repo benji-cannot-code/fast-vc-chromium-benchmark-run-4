e50607a75caecbde49eec5361b377f871307c99c
Pier-Alexandre Bouchard <pabouchard@google.com>
2026-03-20 12:36:53 -0700

    [iOS] Fix flakiness in Home Background Customization EG test
    [ios] Fix flakiness in testCustomizeColor
    
    This CL resolves a race condition in home_background_customization_egtest.mm where the test verified the palette seed color before the app finished updating it.
    
    Because the app perceived no color selection, the Done button only dismissed the color picker instead of the entire customization stack, leaving the main menu visible and failing the final assertion.
    
    The fix introduces a polling wait to synchronize the color state before tapping Done.
    
    Fixed: 474141910
    
    
    Change-Id: I3bd1d1ebe6c0d405b616c87ffbfb5646de4bdb41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7689230
    Reviewed-by: Tarek Makkouk <tarekmakkouk@google.com>
    Commit-Queue: Pier-Alexandre Bouchard <pabouchard@google.com>
    Cr-Commit-Position: refs/heads/main@{#1602777}

--- ios/chrome/browser/home_customization/test/home_background_customization_egtest.mm
+++ ios/chrome/browser/home_customization/test/home_background_customization_egtest.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <XCTest/XCTest.h>
 
 #import "base/strings/sys_string_conversions.h"
+#import "base/test/ios/wait_util.h"
 #import "components/themes/ntp_background.pb.h"
 #import "ios/chrome/browser/content_suggestions/test/new_tab_page_app_interface.h"
 #import "ios/chrome/browser/home_customization/model/home_customization_seed_colors.h"
@@ -129,13 +130,6 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
 
 // Tests that a custom color can be set.
 - (void)testCustomizeColor {
-#if !TARGET_IPHONE_SIMULATOR
-  // TODO(crbug.com/474141910): Re-enable when fixed.
-  if (![ChromeEarlGrey isIPadIdiom]) {
-    EARL_GREY_TEST_DISABLED(@"Test is flaky on iPhone device.");
-  }
-#endif
-
   [[EarlGrey
       selectElementWithMatcher:grey_accessibilityID(
                                    kNTPCustomizationMenuButtonIdentifier)]
@@ -158,10 +152,12 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
                                    chosenColor.accessibilityNameId))]
       performAction:grey_tap()];
 
-  NewTabPageColorPalette* palette =
-      [NewTabPageAppInterface currentBackgroundColor];
-  SkColor paletteSeedColor = skia::UIColorToSkColor(palette.seedColor);
-  EXPECT_EQ(chosenColor.color, paletteSeedColor);
+  EXPECT_TRUE(base::test::ios::WaitUntilConditionOrTimeout(
+      base::test::ios::kWaitForUIElementTimeout, ^bool() {
+        NewTabPageColorPalette* palette =
+            [NewTabPageAppInterface currentBackgroundColor];
+        return skia::UIColorToSkColor(palette.seedColor) == chosenColor.color;
+      }));
 
   // Tapping Done should dismiss the entire menu and keep the background color.
   [[EarlGrey
@@ -174,9 +170,12 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
                      kHomeCustomizationMainViewAccessibilityIdentifier)]
       assertWithMatcher:grey_nil()];
 
-  palette = [NewTabPageAppInterface currentBackgroundColor];
-  paletteSeedColor = skia::UIColorToSkColor(palette.seedColor);
-  EXPECT_EQ(chosenColor.color, paletteSeedColor);
+  EXPECT_TRUE(base::test::ios::WaitUntilConditionOrTimeout(
+      base::test::ios::kWaitForUIElementTimeout, ^bool() {
+        NewTabPageColorPalette* palette =
+            [NewTabPageAppInterface currentBackgroundColor];
+        return skia::UIColorToSkColor(palette.seedColor) == chosenColor.color;
+      }));
 }
 
 // Tests that a custom gallery background can be set.
