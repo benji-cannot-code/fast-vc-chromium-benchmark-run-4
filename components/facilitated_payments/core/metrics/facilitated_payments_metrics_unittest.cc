681cfbcf52a31f44cb2305dfb30bac0f92f30077
Longsheng Yin <longsheng@google.com>
2026-03-20 21:11:23 -0700

    Log Pix flow exited reason when payflow has already started
    Bug: b:464010651
    Change-Id: I8aeb012be2b689be1f29b6c9ce24f68a860faaaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7684160
    Commit-Queue: Longsheng Yin <longsheng@google.com>
    Reviewed-by: Siddharth Shah <siashah@google.com>
    Reviewed-by: Olivia Saul <osaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1602977}

--- components/facilitated_payments/core/metrics/facilitated_payments_metrics_unittest.cc
+++ components/facilitated_payments/core/metrics/facilitated_payments_metrics_unittest.cc
@@ -537,7 +537,8 @@ INSTANTIATE_TEST_SUITE_P(
                     PixFlowExitedReason::kFrameNotActive,
                     PixFlowExitedReason::kCctWithGboardAsDefaultIme,
                     PixFlowExitedReason::kStaticCode,
-                    PixFlowExitedReason::kIframeUrlNotAllowlisted));
+                    PixFlowExitedReason::kIframeUrlNotAllowlisted,
+                    PixFlowExitedReason::kFlowAlreadyStarted));
 
 class FacilitatedPaymentsMetricsUkmTest : public testing::Test {
  public:
