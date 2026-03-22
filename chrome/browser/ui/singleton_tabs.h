0bb513ffda9f796f7493b46583698955e968854c
Yu He <yhe@microsoft.com>
2026-03-22 11:57:30 -0700

    [bedrock] Migrate chrome_pages and singleton_tabs from Browser* to BrowserWindowInterface*
    Migrate function signatures in chrome_pages.h/cc and singleton_tabs.h/cc
    to accept BrowserWindowInterface* instead of Browser*.
    
    This allows callers to pass a BrowserWindowInterface* directly,
    eliminating intermediate GetBrowserForMigrationOnly() calls at:
    
    extension_install_ui_desktop.cc startup_launch_infobar_manager_impl.cc
    singleton_tabs.cc (internal usage) Also:
    
    Guard desktop-only includes in notification_common.cc with #if
    !BUILDFLAG(IS_ANDROID) to fix Android build. Add missing browser.h
    include to inspect_ui.cc. Remove forward declaration of Browser from
    chrome_pages.h and singleton_tabs.h.
    
    Bug: 494010890
    Change-Id: Ica78133224b67d439dc264fa187d261b4ee6f87c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7667947
    Reviewed-by: Qikai Zhong <qikaizhong@microsoft.com>
    Auto-Submit: Yu He <yhe@microsoft.com>
    Commit-Queue: Tom Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1603192}

--- chrome/browser/ui/singleton_tabs.h
+++ chrome/browser/ui/singleton_tabs.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/browser_navigator_params.h"
 
-class Browser;
 class BrowserWindowInterface;
 class GURL;
 
@@ -19,7 +18,7 @@ class GURL;
 
 // Shows a given a URL. If a tab with the same URL (ignoring the ref) is already
 // visible in this browser, it becomes selected. Otherwise a new tab is created.
-void ShowSingletonTab(Browser* browser, const GURL& url);
+void ShowSingletonTab(BrowserWindowInterface* browser, const GURL& url);
 
 // Like above, but uses the last active tabbed browser or creates a new one if
 // possible.
@@ -32,13 +31,14 @@ void ShowSingletonTabOverwritingNTP(
     const GURL& url,
     NavigateParams::PathBehavior path_behavior = NavigateParams::RESPECT);
 void ShowSingletonTabOverwritingNTP(
-    Browser* browser,
+    BrowserWindowInterface* browser,
     const GURL& url,
     NavigateParams::PathBehavior path_behavior = NavigateParams::RESPECT);
 void ShowSingletonTabOverwritingNTP(NavigateParams* params);
 
 // Creates a NavigateParams struct for a singleton tab navigation.
-NavigateParams GetSingletonTabNavigateParams(Browser* browser, const GURL& url);
+NavigateParams GetSingletonTabNavigateParams(BrowserWindowInterface* browser,
+                                             const GURL& url);
 
 // If the given navigational URL is already open in |browser|, return
 // the tab and tab index for it. Otherwise, returns -1.
--- chrome/browser/ui/webui/inspect/inspect_ui.cc
+++ chrome/browser/ui/webui/inspect/inspect_ui.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/devtools/devtools_window.h"
 #include "chrome/browser/devtools/features.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_navigator_params.h"
 #include "chrome/browser/ui/singleton_tabs.h"
 #include "chrome/browser/ui/webui/theme_source.h"
