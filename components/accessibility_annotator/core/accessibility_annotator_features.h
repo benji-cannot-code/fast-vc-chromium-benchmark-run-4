4097b175dbbf6888fad55711e59a76228f3aa821
Nicola Tommasi <tommasin@chromium.org>
2026-03-23 07:12:48 -0700

    [a11y][ffr] Add kAccessibilityAnnotatorFirstRun feature
    This feature will be used to control behavior specific to the first run
    of the accessibility annotator
    
    Bug: b:489372995
    Change-Id: I214dfde222be247cbe56d1ccf91e0a2c55d6ddad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7692135
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Nicola Tommasi <tommasin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1603448}

--- components/accessibility_annotator/core/accessibility_annotator_features.h
+++ components/accessibility_annotator/core/accessibility_annotator_features.h
@@ -14,6 +14,7 @@ namespace accessibility_annotator {
 
 BASE_DECLARE_FEATURE(kContentAnnotator);
 BASE_DECLARE_FEATURE(kAccessibilityAnnotator);
+BASE_DECLARE_FEATURE(kAccessibilityAnnotatorFirstRun);
 BASE_DECLARE_FEATURE(kAccessibilityAnnotatorGetEntities);
 
 BASE_DECLARE_FEATURE_PARAM(int, kContentAnnotatorMaxPendingUrls);
