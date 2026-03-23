71a9f4716f2b7c524a2d64c4ad304ef884d41ba8
Linyu He <linyuh@google.com>
2026-03-23 12:27:56 -0700

    Side Panel: Add SidePanelUIProvider to provide SidePanelUI for all OSes
    SidePanelUI is a cross-platform interface. Currently, individual
    SidePanelUI implementations define their own UnownedUserData, so we
    decided to use #ifdef to retrieve a concrete SidePanelUI from an
    UnownedUserDataHost as a way to support cross-platform code that
    needs to call SidePanelUI.
    
    Instead of adding SidePanelUI::From(), the logic has to be in a
    separate SidePanelUIProvider class to avoid circular dependencies.
    Please see SidePanelUIProvider documentation for details.
    
    Bug: 493930108
    Change-Id: Iba91a3e327dafbb9df3e044ae1d7c1dc505022eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7690391
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Linyu He <linyuh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1603648}

---
+++ chrome/browser/ui/side_panel/side_panel_ui_provider.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/side_panel/side_panel_ui_provider.h"
+
+#include "build/build_config.h"
+#include "chrome/browser/ui/side_panel/side_panel_ui.h"
+
+#if BUILDFLAG(IS_ANDROID)
+#include "chrome/browser/ui/side_panel/internal/android/side_panel_coordinator_android.h"
+#else
+#include "chrome/browser/ui/views/side_panel/side_panel_coordinator.h"
+#endif
+
+// static
+SidePanelUI* SidePanelUIProvider::From(BrowserWindowInterface* browser) {
+#if BUILDFLAG(IS_ANDROID)
+  return SidePanelCoordinatorAndroid::From(browser);
+#else
+  return SidePanelCoordinator::From(browser);
+#endif
+}
--- chrome/browser/ui/views/side_panel/side_panel_coordinator_browsertest.cc
+++ chrome/browser/ui/views/side_panel/side_panel_coordinator_browsertest.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/side_panel/side_panel_entry_observer.h"
 #include "chrome/browser/ui/side_panel/side_panel_native_view.h"
 #include "chrome/browser/ui/side_panel/side_panel_registry.h"
+#include "chrome/browser/ui/side_panel/side_panel_ui_provider.h"
 #include "chrome/browser/ui/side_panel/side_panel_util.h"
 #include "chrome/browser/ui/tabs/public/tab_features.h"
 #include "chrome/browser/ui/tabs/split_tab_metrics.h"
@@ -288,6 +289,11 @@ class SidePanelCoordinatorTest : public InProcessBrowserTest {
   std::vector<raw_ptr<SidePanelRegistry, DanglingUntriaged>>
       contextual_registries_;
 };
+
+IN_PROC_BROWSER_TEST_F(SidePanelCoordinatorTest, TestSidePanelUIProvider) {
+  Init();
+  EXPECT_EQ(SidePanelUIProvider::From(browser()), coordinator());
+}
 
 IN_PROC_BROWSER_TEST_F(SidePanelCoordinatorTest, ToggleSidePanel) {
   Init();
