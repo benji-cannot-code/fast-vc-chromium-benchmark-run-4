a1710343f34fa3ce1d8b9a2c3b50df28da44817c
Rusty Sammon <sammon@google.com>
2026-03-23 06:34:55 -0700

    Store encrypted session files in a different folder.
    Using different file paths for cleartext and encrypted files allows them
    to coexist during intermediate stages of rollout (see followup
    crrev.com/c/7551291 that defines stages of rollout).
    
    BUG=479420496
    
    Change-Id: Iba5cf9763b637b488bd22812a2789279270b2374
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7674380
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Rusty Sammon <sammon@google.com>
    Reviewed-by: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1603421}

--- components/sessions/core/session_constants.cc
+++ components/sessions/core/session_constants.cc
@@ -10,6 +10,9 @@ namespace sessions {
 const base::FilePath::StringViewType kSessionsDirectory =
     FILE_PATH_LITERAL("Sessions");
 
+const base::FilePath::StringViewType kEncryptedSessionsDirectory =
+    FILE_PATH_LITERAL("Sessions_Encrypted");
+
 const base::FilePath::CharType kTabSessionFileNamePrefix[] =
     FILE_PATH_LITERAL("Tabs");
 
