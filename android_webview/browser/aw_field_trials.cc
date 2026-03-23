a2d58445aa3f929dc845f235493b10f8b5902867
Colin Blundell <blundell@chromium.org>
2026-03-23 02:58:33 -0700

    [Blink] Merge features controlling low-latency for Canvas2D & WebGL
    There's no reason for these to be separate base::Features at this point;
    the only purpose is to serve as a toggle disabling this functionality on
    WebView.
    
    Bug: 488937356
    Change-Id: I37341525c37569e49f332b428d3629e8e9852248
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7687533
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1603339}

--- android_webview/browser/aw_field_trials.cc
+++ android_webview/browser/aw_field_trials.cc
@@ -102,9 +102,7 @@ void AwFieldTrials::RegisterFeatureOverrides(base::FeatureList* feature_list) {
   // enable these optimizations in WebView though they are not fundamentally
   // impossible.
   aw_feature_overrides.DisableFeature(
-      blink::features::kLowLatencyUsageSupportedForCanvas2D);
-  aw_feature_overrides.DisableFeature(
-      blink::features::kLowLatencyUsageSupportedForWebGL);
+      blink::features::kLowLatencyUsageSupportedForCanvas);
 
   // Disable Shared Storage on WebView.
   aw_feature_overrides.DisableFeature(network::features::kSharedStorageAPI);
