8f3aba20a73256c960e5e58fd209cef98e72cce0
Georg Neis <neis@chromium.org>
2026-03-23 00:35:22 -0700

    ash: Remove obsolete class crosapi::FilesAppLauncher
    Bug: b:365902693
    Change-Id: Ibc196ada215d2e296ea71696e5a309316bcb8883
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7692222
    Auto-Submit: Georg Neis <neis@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Reviewed-by: Di Wu <diwux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1603293}

--- chrome/browser/ash/crosapi/browser_manager.cc
+++ chrome/browser/ash/crosapi/browser_manager.cc
@@ -56,7 +56,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
 #include "chrome/browser/ash/crosapi/crosapi_manager.h"
-#include "chrome/browser/ash/crosapi/files_app_launcher.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
 #include "chrome/browser/notifications/system_notification_helper.h"
 #include "chrome/browser/profiles/profile_manager.h"
