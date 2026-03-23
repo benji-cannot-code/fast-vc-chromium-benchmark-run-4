522fecbbd1bd0f74b762498240e06c16352f6aa7
Dan Polanco <polardz@google.com>
2026-03-23 16:57:44 -0700

    AppBannerManagerAndroid remove ScopedJavaGlobalWeakRef
    The C++ AppBannerManagerAndroid no longer holds a weak reference to the
    Java object. Instead, it retrieves the Java object from a static map.
    
    Bug: 484619649
    Change-Id: Iedec43f074d47440c51e548f07a5835c1a0b2164
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7639530
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Dan Polanco <polardz@google.com>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Dan Polanco <polardz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1603797}

--- chrome/browser/banners/android/ambient_badge_manager_browsertest.cc
+++ chrome/browser/banners/android/ambient_badge_manager_browsertest.cc
@@ -31,6 +31,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "url/gurl.h"
 
+using ::base::android::AttachCurrentThread;
+using ::base::android::ScopedJavaGlobalRef;
+using ::base::android::ScopedJavaLocalRef;
 using ::base::test::RunOnceCallback;
 using testing::_;
 
@@ -119,11 +122,17 @@ class TestAppBannerManager : public AppBannerManagerAndroid {
     ambient_badge_test_->WaitForState(target_badge_state_,
                                       std::move(on_badge_done_));
 
+    JNIEnv* env = AttachCurrentThread();
+    ScopedJavaLocalRef<jobject> native_java_app_data =
+        GetNativeJavaAppData(env);
+    auto global_native_java_app_data =
+        ScopedJavaGlobalRef<jobject>(env, native_java_app_data);
+
+    WebappInstallSource install_source = InstallableMetrics::GetInstallSource(
+        &GetWebContents(), InstallTrigger::AMBIENT_BADGE);
     std::unique_ptr<AddToHomescreenParams> a2hs_params =
         AppBannerManagerAndroid::CreateAddToHomescreenParams(
-            install_config, native_java_app_data_for_testing(),
-            InstallableMetrics::GetInstallSource(
-                &GetWebContents(), InstallTrigger::AMBIENT_BADGE));
+            install_config, global_native_java_app_data, install_source);
 
     ambient_badge_test_->MaybeShow(
         install_config.validated_url, install_config.GetWebOrNativeAppName(),
@@ -133,7 +142,7 @@ class TestAppBannerManager : public AppBannerManagerAndroid {
                        GetAndroidWeakPtr(), install_config),
         // Create the params, then pass them to MaybeShow.
         base::BindOnce(&AppBannerManagerAndroid::CreateAddToHomescreenParams,
-                       install_config, native_java_app_data_for_testing())
+                       install_config, global_native_java_app_data)
             .Then(base::BindOnce(
                 &PwaBottomSheetController::MaybeShow,
                 app_banner_manager()->web_contents(),
