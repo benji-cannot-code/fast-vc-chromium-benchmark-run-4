39aaae7b80dbe17cba64b591a2e1efd5fdd92f95
Pierre-Laurent Coirier <plcoirier@google.com>
2026-03-23 07:09:32 -0700

    [Encrypt Bookmarks] Move encrypted file for sync to sign in migration
    When data are moved as part of the sync to sign in migration, also move
    the encrypted local bookmark file to the encrypted account bookmark
    file.
    
    Bug: 435317726
    Change-Id: I5cf6f413058b98b6df40ed6df2fcff18a3aac5e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7684411
    Commit-Queue: Pierre-Laurent Coirier <plcoirier@google.com>
    Reviewed-by: Ankush Singh <ankushkush@google.com>
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1603443}

--- components/browser_sync/sync_to_signin_migration.cc
+++ components/browser_sync/sync_to_signin_migration.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "build/build_config.h"
 #include "components/bookmarks/common/bookmark_constants.h"
+#include "components/bookmarks/common/bookmark_features.h"
 #include "components/browser_sync/browser_sync_switches.h"
 #include "components/password_manager/core/browser/password_manager_constants.h"
 #include "components/prefs/pref_service.h"
@@ -580,6 +581,15 @@ void MaybeMigrateSyncingUserToSignedInInternal(
     blocking_operations.push_back(
         base::BindOnce(&RenameFileAndReportSuccess, from_path, to_path,
                        "Sync.SyncToSigninMigrationOutcome.BookmarksFileMove"));
+    if (base::FeatureList::IsEnabled(bookmarks::kEncryptBookmarks)) {
+      base::FilePath encrypted_from_path = profile_path.Append(
+          bookmarks::kEncryptedLocalOrSyncableBookmarksFileName);
+      base::FilePath encrypted_to_path =
+          profile_path.Append(bookmarks::kEncryptedAccountBookmarksFileName);
+      blocking_operations.push_back(base::BindOnce(
+          &RenameFileAndReportSuccess, encrypted_from_path, encrypted_to_path,
+          "Sync.SyncToSigninMigrationOutcome.EncryptedBookmarksFileMove"));
+    }
   }
 
   // Reading list: Set migration pref. The DataTypeStoreServiceImpl will read
