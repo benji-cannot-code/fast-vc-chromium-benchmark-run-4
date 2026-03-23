a5feb21d33576a37d7839ce6eeebb92f7f9a8365
Kevin Puthusseri <pskevin@google.com>
2026-03-23 14:22:33 -0700

    Resolve accessibility annotations of query
    Using the accessibility annotations fetched from the OneP service, this
    change calls the RemoteModelExecutor and processes the model's JSON
    response to produce a list of MemorySearchResult objects.
    
    Bug: b:487416734
    Change-Id: I434505b90c728952636c117ab5aed87f3d134a76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7659928
    Auto-Submit: Kevin Puthusseri <pskevin@google.com>
    Commit-Queue: Kevin Puthusseri <pskevin@google.com>
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1603691}

--- components/optimization_guide/core/model_execution/model_execution_fetcher_impl.cc
+++ components/optimization_guide/core/model_execution/model_execution_fetcher_impl.cc
@@ -440,6 +440,9 @@ net::NetworkTrafficAnnotationTag GetNetworkTrafficAnnotation(
     case ModelBasedCapabilityKey::kFinds:
       // TODO(crbug.com/490501055): Add network traffic annotation.
       return MISSING_TRAFFIC_ANNOTATION;
+    case ModelBasedCapabilityKey::kAnnotationReducerOnePResolver:
+      // TODO(crbug.com/487416734): Add network traffic annotation.
+      return MISSING_TRAFFIC_ANNOTATION;
     case ModelBasedCapabilityKey::kAnnotationReducerQueryClassifier:
       // TODO(crbug.com/492168146): Add network traffic annotation.
       return MISSING_TRAFFIC_ANNOTATION;
@@ -472,6 +475,7 @@ bool IsAccessTokenRequiredForFeature(ModelBasedCapabilityKey feature) {
     case ModelBasedCapabilityKey::kSkills:
     case ModelBasedCapabilityKey::kContentAnnotation:
     case ModelBasedCapabilityKey::kFinds:
+    case ModelBasedCapabilityKey::kAnnotationReducerOnePResolver:
     case ModelBasedCapabilityKey::kAnnotationReducerQueryClassifier:
       return true;
     case ModelBasedCapabilityKey::kFormsClassifications:
--- components/optimization_guide/core/optimization_guide_util.cc
+++ components/optimization_guide/core/optimization_guide_util.cc
@@ -136,6 +136,9 @@ std::string_view GetStringNameForModelExecutionFeature(
     case proto::ModelExecutionFeature::MODEL_EXECUTION_FEATURE_FINDS:
       return "Finds";
     case proto::ModelExecutionFeature::
+        MODEL_EXECUTION_FEATURE_ANNOTATION_REDUCER_ONE_P_RESOLVER:
+      return "AnnotationReducerOnePResolver";
+    case proto::ModelExecutionFeature::
         MODEL_EXECUTION_FEATURE_ANNOTATION_REDUCER_QUERY_CLASSIFIER:
       return "AnnotationReducerQueryClassifier";
     case proto::ModelExecutionFeature::MODEL_EXECUTION_FEATURE_UNSPECIFIED:
