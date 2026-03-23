f9519f41d6329eaac3d3ababde0791579349748f
Stephen McGruer <smcgruer@chromium.org>
2026-03-23 06:55:25 -0700

    [Payment Handler] Respect dev-tools auto-open setting
    In dev-tools Settings there is an option for "Auto-open DevTools for
    pop-ups". Currently, this setting is not respected for Payment Handler
    windows, because they are not normal web popups. This CL hooks up
    Payment Handler modal windows to the setting, and automatically opens a
    devtools window if the setting is on.
    
    Before: https://screencast.googleplex.com/cast/NDg5NTEyNTQ0NzYzOTA0MHxhNTk5OTliMy01Yw
    After: https://screencast.googleplex.com/cast/NTc0MjE2NzM1NTAzMTU1MnxlMzA1ZjA1YS1kOA
    
    Bug: 494293287
    Change-Id: I733fded7c347eccffc847499f16744cc2bfc41a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7685414
    Reviewed-by: Darwin Yang <darwinyang@chromium.org>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1603432}

--- chrome/browser/ui/views/payments/payment_handler_web_flow_view_controller.cc
+++ chrome/browser/ui/views/payments/payment_handler_web_flow_view_controller.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/app/vector_icons/vector_icons.h"
+#include "chrome/browser/devtools/devtools_window.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/task_manager/web_contents_tags.h"
 #include "chrome/browser/themes/theme_service.h"
@@ -320,9 +321,11 @@ void PaymentHandlerWebFlowViewController::FillContentView(
   PaymentHandlerNavigationThrottle::MarkPaymentHandlerWebContents(
       web_contents());
   web_contents()->SetDelegate(this);
-  DCHECK_NE(log_.web_contents(), web_contents());
+  content::WebContents* parent_tab_web_contents = log_.web_contents();
+
+  DCHECK_NE(parent_tab_web_contents, web_contents());
   content::PaymentAppProvider::GetOrCreateForWebContents(
-      /*payment_request_web_contents=*/log_.web_contents())
+      /*payment_request_web_contents=*/parent_tab_web_contents)
       ->SetOpenedWindow(
           /*payment_handler_web_contents=*/web_contents());
 
@@ -337,6 +340,17 @@ void PaymentHandlerWebFlowViewController::FillContentView(
       web_contents());
   web_modal::WebContentsModalDialogManager::FromWebContents(web_contents())
       ->SetDelegate(&dialog_manager_delegate_);
+
+  // If the web-contents for the parent tab has devtools open and the "Auto-open
+  // DevTools for pop-ups" setting is enabled, trigger devtools for the Payment
+  // Handler modal. This does not happen by default as Payment Handler is not a
+  // regular pop-up window.
+  DevToolsWindow* window = DevToolsWindow::GetInstanceForInspectedWebContents(
+      parent_tab_web_contents);
+  if (window && window->OpenNewWindowForPopups()) {
+    DevToolsWindow::OpenDevToolsWindow(
+        web_contents(), DevToolsOpenedByAction::kAutomaticForNewTarget);
+  }
 
   // The webview must get an explicitly set height otherwise the layout doesn't
   // make it fill its container. This is likely because it has no content at the
