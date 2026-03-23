5c470abd4baf8288c2c34fe8ea564d63c8b1bb44
Radu Nitescu <radunitescu@google.com>
2026-03-23 07:07:51 -0700

    [iOS][Cobrowse] Don't skip responder delegate call in cobrowse
    The omniboxDidBecomeFirstResponder callback was failing to trigger
    because the controller incorrectly gated it behind a suggestion-based
    check. Since Cobrowse doesn't utilize standard suggestions, this check
    consistently failed.
    
    Also remove the unnecessary endEditing now that delegate calls are
    balanced.
    
    Change-Id: Ib59038c22ec0a4378b87b1b181d4d6c8f87572b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7688521
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1603442}

--- ios/chrome/browser/cobrowse/coordinator/assistant_aim_coordinator.mm
+++ ios/chrome/browser/cobrowse/coordinator/assistant_aim_coordinator.mm
@@ -141,8 +141,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       setAssistantContainerDetents:{AssistantContainerDetent::kMinimized,
                                     AssistantContainerDetent::kMedium,
                                     AssistantContainerDetent::kLarge}];
-
-  [_inputPlateCoordinator endEditing];
 }
 
 - (void)assistantAIMViewControllerDidRequestEndEditing:
