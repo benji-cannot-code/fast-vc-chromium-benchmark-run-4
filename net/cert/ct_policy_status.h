edc436321d113f7278ce140df7af8ea9c79c095b
Matt Mueller <mattm@chromium.org>
2026-03-23 12:13:19 -0700

    Add CTRequirementsStatus value for CT requirement overridden by delegate
    This CL doesn't change behavior (aside from the status value recorded in
    netlogs).
    
    Bug: 41392053
    Change-Id: Iaaf5645711fa04218355fd7eff2e56e4c6f5c628
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7644721
    Reviewed-by: Carlos IL <carlosil@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1603639}

--- net/cert/ct_policy_status.h
+++ net/cert/ct_policy_status.h
@@ -37,6 +37,10 @@ enum class CTPolicyCompliance {
 NET_EXPORT const char* CTPolicyComplianceToString(CTPolicyCompliance status);
 
 // Indicates whether a path met CT requirements.
+// This value is histogrammed, so do not re-order or change values, and add new
+// values at the end.
+// TODO(crbug.com/41392053): remove the comment about this being histogrammed
+// once we've finished the refactoring and removed the histogram.
 enum class CTRequirementsStatus {
   // CT was not required for the path.
   CT_NOT_REQUIRED,
@@ -45,7 +49,14 @@ enum class CTRequirementsStatus {
   CT_REQUIREMENTS_MET,
   // CT was required for the path but valid CT info was not provided.
   CT_REQUIREMENTS_NOT_MET,
-  kMaxValue = CT_REQUIREMENTS_NOT_MET
+  // CT requirements were not met, but the delegate allowed it anyway (eg, due
+  // to enterprise policy overriding the CT requirement).
+  CT_REQUIREMENT_OVERRIDDEN,
+  // CT requirements were not met, but the delegate allowed it anyway (eg, due
+  // to enterprise policy overriding the CT requirement), and the override
+  // applies to all SANs in the leaf certificate.
+  CT_REQUIREMENT_OVERRIDDEN_APPLIES_ACROSS_NAMES,
+  kMaxValue = CT_REQUIREMENT_OVERRIDDEN_APPLIES_ACROSS_NAMES
 };
 
 NET_EXPORT const char* CTRequirementStatusToString(CTRequirementsStatus status);
