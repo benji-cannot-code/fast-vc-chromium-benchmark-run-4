18ed6a6378496c549713b53f0b37dfc485417d2d
Sylvain Defresne <sdefresne@chromium.org>
2026-03-23 02:56:34 -0700

    Avoid registering observer before calling HistoryService::Init(...)
    In production it is not possible to get access to an HistoryService
    that has not been initialized so change PrintingManagerFactoryTest
    to test this case.
    
    Ensure PrintingManagerFactoryTest initialize the HistoryService
    before the object is used.
    
    Bug: 481635191
    Change-Id: I0c5ecf78c5db76c9739dc7d5bd572563e110b1dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7679165
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1603338}

--- chrome/browser/ash/printing/print_management/printing_manager_factory_unittest.cc
+++ chrome/browser/ash/printing/print_management/printing_manager_factory_unittest.cc
@@ -9,7 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/test/base/testing_profile.h"
 #include "chromeos/ash/components/browser_context_helper/browser_context_types.h"
+#include "components/history/core/browser/history_database_params.h"
 #include "components/history/core/browser/history_service.h"
+#include "components/history/core/test/history_service_test_util.h"
+#include "components/history/core/test/test_history_database.h"
 #include "content/public/test/browser_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -19,7 +22,10 @@ namespace print_management {
 
 std::unique_ptr<KeyedService> BuildTestHistoryService(
     content::BrowserContext* context) {
-  return std::make_unique<history::HistoryService>();
+  TestingProfile* profile = static_cast<TestingProfile*>(context);
+  auto service = std::make_unique<history::HistoryService>();
+  service->Init(history::TestHistoryDatabaseParamsForPath(profile->GetPath()));
+  return service;
 }
 
 std::unique_ptr<KeyedService> BuildPrintingManager(
