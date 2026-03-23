4276cf492fd9240554bd61ad9dd26f6466224c39
Aldo Culquicondor <acondor@chromium.org>
2026-03-23 15:16:44 -0700

    Reland "SlimWebview: Add more browser tests"
    This is a reland of commit 271da935523db9612c387ad0fefd83e55d2a5b17
    
    In some bots, geolocation might be disabled at the system level, so the
    permission would be denied before the guest has a chance to get the
    request. Skipping the test in this case.
    
    Also took the chance to pass the test server URL via the window object,
    instead of reusing a Glic switch.
    
    Original change's description:
    > SlimWebview: Add more browser tests
    >
    > New tests for:
    > - Loading failures
    > - New window events
    > - Permission requests
    >
    > Bug: 484389846
    > Change-Id: Ic14ec10028967dda555a584ee7ad4f8a9d1a4916
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7674411
    > Commit-Queue: Kevin McNee <mcnee@chromium.org>
    > Auto-Submit: Aldo Culquicondor <acondor@chromium.org>
    > Reviewed-by: Kevin McNee <mcnee@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1602294}
    
    Bug: 484389846
    Change-Id: Ia44550ead1d16faef9d259a1cd6d1727d3f027c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7687789
    Commit-Queue: Aldo Culquicondor <acondor@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Thomas Nguyen <tungnh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1603737}

--- chrome/browser/resources/guest_view_shared/slim_webview.ts
+++ chrome/browser/resources/guest_view_shared/slim_webview.ts
@@ -298,7 +298,11 @@ export class SlimWebviewElement extends CrLitElement {
   accessor maxheight: number = 0;
   accessor partition: string = '';
 
-  contentWindow: WindowProxy|null = null;
+  private contentWindow_: WindowProxy|null = null;
+
+  get contentWindow() {
+    return this.contentWindow_;
+  }
 
   private viewInstanceId: number = chrome.slimWebViewPrivate.getNextId();
   private containerId: number|null = null;
@@ -355,7 +359,7 @@ export class SlimWebviewElement extends CrLitElement {
         this.createGuest();
         return;
       }
-      if (this.contentWindow === null) {
+      if (this.contentWindow_ === null) {
         return;
       }
       this.navigate();
@@ -423,7 +427,7 @@ export class SlimWebviewElement extends CrLitElement {
         params,
         iframeElement.contentWindow,
         () => {
-          this.contentWindow = iframeElement.contentWindow;
+          this.contentWindow_ = iframeElement.contentWindow;
           this.navigate();
         },
     );
