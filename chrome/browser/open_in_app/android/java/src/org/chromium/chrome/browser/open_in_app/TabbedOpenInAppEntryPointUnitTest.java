562485eca7c995b6d536478c53f3f8754fc4c91c
Sinan Sahin <sinansahin@google.com>
2026-03-23 11:04:06 -0700

    [Link Capturing] Associate fetched resolve infos with specific delegate
    This CL associates AsyncTask results with a specific OpenInAppDelegate
    to ensure we can handle the case where we receive the result after
    switching to a different tab. This fixes a crash in
    NavigateAndroidBrowserTest.Disposition_OffTheRecord_FromIncognitoProfile
    but also makes the code more robust in general.
    
    Bug: 450253146
    Change-Id: I8c50a8b0eda1393a23bb64e2296814d3dbc1ea62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7688477
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1603592}

--- chrome/browser/open_in_app/android/java/src/org/chromium/chrome/browser/open_in_app/TabbedOpenInAppEntryPointUnitTest.java
+++ chrome/browser/open_in_app/android/java/src/org/chromium/chrome/browser/open_in_app/TabbedOpenInAppEntryPointUnitTest.java
@@ -146,7 +146,7 @@ public class TabbedOpenInAppEntryPointUnitTest {
 
         // Simulate receiving resolve infos.
         var infos = new OpenInAppEntryPoint.ResolveResult.Info(mResolveInfo);
-        mEntryPoint.onResolveInfosFetched(infos, mIntent, mUrl, /* navigationId= */ 123L);
+        mEntryPoint.onResolveInfosFetched(delegate, infos, mIntent, mUrl, /* navigationId= */ 123L);
 
         // Resolve infos received; the app info should be non-null.
         assertNonNull(delegate.getCurrentOpenInAppInfo());
@@ -188,7 +188,11 @@ public class TabbedOpenInAppEntryPointUnitTest {
 
         // Empty resolve infos; the app info should be null.
         mEntryPoint.onResolveInfosFetched(
-                new OpenInAppEntryPoint.ResolveResult.None(), mIntent, mUrl, /* navigationId= */ 0);
+                delegate,
+                new OpenInAppEntryPoint.ResolveResult.None(),
+                mIntent,
+                mUrl,
+                /* navigationId= */ 0);
         assertNull(delegate.getCurrentOpenInAppInfo());
         verify(mOmniboxChipManager, times(2)).dismissChip();
     }
@@ -206,7 +210,7 @@ public class TabbedOpenInAppEntryPointUnitTest {
         ShadowLooper.idleMainLooper();
 
         var infos = new OpenInAppEntryPoint.ResolveResult.Info(mResolveInfo);
-        mEntryPoint.onResolveInfosFetched(infos, mIntent, mUrl, /* navigationId= */ 123L);
+        mEntryPoint.onResolveInfosFetched(delegate, infos, mIntent, mUrl, /* navigationId= */ 123L);
 
         ArgumentCaptor<Runnable> actionCaptor = ArgumentCaptor.forClass(Runnable.class);
         verify(mOmniboxChipManager).placeChip(any(), any(), any(), actionCaptor.capture(), any());
