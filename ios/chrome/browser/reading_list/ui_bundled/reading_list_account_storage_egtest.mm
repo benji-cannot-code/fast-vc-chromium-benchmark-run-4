daa642e9c655d760894e242648fc32e612e0fc20
Alexandra Pereira <alexandrasp@google.com>
2026-03-23 09:12:50 -0700

    [iOS] Clean up Reading List EG tests and DEPS
    This CL removes the remaining dependencies on the deprecated
    web::HttpServer in favor of net::EmbeddedTestServer within the Reading
    List UI integration tests.
    
    Specifically:
    - Updates reading_list_account_storage_egtest.mm to inherit from
      ChromeTestCase instead of WebHttpServerChromeTestCase.
    - Removes the specific_include_rules exception in DEPS for
      reading_list_egtest.mm as it no longer uses the deprecated
      test server.
    
    Bug: 493548553
    Change-Id: I43ce3cf9e64fa5ab44b023700215ace92789eee8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7685918
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Alexandra Pereira <alexandrasp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1603535}

--- ios/chrome/browser/reading_list/ui_bundled/reading_list_account_storage_egtest.mm
+++ ios/chrome/browser/reading_list/ui_bundled/reading_list_account_storage_egtest.mm
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
-#import "ios/chrome/test/earl_grey/web_http_server_chrome_test_case.h"
+#import "ios/chrome/test/earl_grey/chrome_test_case.h"
 #import "ios/testing/earl_grey/earl_grey_test.h"
 #import "net/test/embedded_test_server/embedded_test_server.h"
 #import "ui/base/l10n/l10n_util.h"
