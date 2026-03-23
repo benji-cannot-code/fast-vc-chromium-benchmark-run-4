bab763046845c6a99994b30d657d6d6ba893838d
Georg Neis <neis@chromium.org>
2026-03-23 16:42:27 -0700

    Remove obsolete build dependencies on crosapi
    ... from chrome/browser.
    
    Bug: b:365902693
    AX-Relnotes: n/a.
    Change-Id: I927ad2eee4036d83a69edf0d6881acda404e2491
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7692333
    Auto-Submit: Georg Neis <neis@chromium.org>
    Reviewed-by: Di Wu <diwux@google.com>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1603791}

--- chrome/browser/media/webrtc/capture_policy_utils_unittest.cc
+++ chrome/browser/media/webrtc/capture_policy_utils_unittest.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 
 #if BUILDFLAG(IS_CHROMEOS)
-#include "chrome/browser/ash/crosapi/crosapi_manager.h"
 #include "chrome/browser/ash/login/users/fake_chrome_user_manager.h"
 #include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile_manager.h"
