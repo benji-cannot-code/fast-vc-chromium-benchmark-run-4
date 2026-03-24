41eaf050738b950e0e99aa3f8dcf3c63f0c38fc9
Victor Shu <victorshu@google.com>
2026-03-24 16:05:23 -0700

    Add SyncBridgeDataProvider
    - SyncBridgeDataProvider provides data from the
    AccessibilityAnnotatorBackend to AtMemory
    - Add QueryIntentTypeUtil to hold the conversion methods from
    QueryIntentType to EntityType and extracting the fields and metadata
    from the entities
    - QueryIntentTypeUtil methods are no-op for now
    
    Bug: b:493849593
    Change-Id: Ie8ee4737522735b62b20fd31ce32555fe171908b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7681910
    Reviewed-by: Ireneusz Szulc <ireneuszszulc@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Auto-Submit: Victor Shu <victorshu@google.com>
    Commit-Queue: Ireneusz Szulc <ireneuszszulc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1604442}

---
+++ components/accessibility_annotator/core/annotation_reducer/query_intent_type_util.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/accessibility_annotator/core/annotation_reducer/query_intent_type_util.h"
+
+#include "base/notimplemented.h"
+
+namespace accessibility_annotator {
+
+MemorySearchResult CreateResultFromEntity(QueryIntentType intent_type,
+                                          const Entity& entity) {
+  // TODO(crbug.com/493849593): Implement.
+  NOTIMPLEMENTED();
+  return MemorySearchResult(QueryIntentType::kUnknown, std::u16string(),
+                            std::u16string());
+}
+
+EntityTypeEnumSet GetEntityTypesForQueryIntentType(
+    QueryIntentType intent_type) {
+  // TODO(crbug.com/493849593): Implement.
+  NOTIMPLEMENTED();
+  return EntityTypeEnumSet();
+}
+
+}  // namespace accessibility_annotator
