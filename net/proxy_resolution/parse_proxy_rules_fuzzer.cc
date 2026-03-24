c5f2688305de01a1d272157a60623d084c88b144
Andrew Paseltiner <apaseltiner@chromium.org>
2026-03-24 14:14:38 -0700

    Spanify inputs in net proxy resolution fuzzers
    This replaces direct use of the pointer-and-size pair with a
    base::span<const uint8_t> via the DEFINE_LLVM_FUZZER_TEST_ONE_INPUT_SPAN
    macro. This avoids the need for manual construction of spans and
    appropriate unsafe-buffer annotations.
    
    Bug: 491067796
    Change-Id: If88b7b282ee7c6213de57cfc4461680415008fca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7697295
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1604381}

--- net/proxy_resolution/parse_proxy_rules_fuzzer.cc
+++ net/proxy_resolution/parse_proxy_rules_fuzzer.cc
@@ -3,16 +3,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <stddef.h>
 #include <stdint.h>
 
-#include "base/compiler_specific.h"
+#include "base/containers/span.h"
+#include "base/strings/string_view_util.h"
 #include "net/proxy_resolution/proxy_config.h"
+#include "testing/libfuzzer/libfuzzer_base_wrappers.h"
 
 // Entry point for LibFuzzer.
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+DEFINE_LLVM_FUZZER_TEST_ONE_INPUT_SPAN(base::span<const uint8_t> data) {
   net::ProxyConfig::ProxyRules rules;
-  std::string input(data, UNSAFE_TODO(data + size));
-  rules.ParseFromString(input);
+  rules.ParseFromString(base::as_string_view(data));
   return 0;
 }
