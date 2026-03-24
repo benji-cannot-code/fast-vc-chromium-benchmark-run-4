1072c0f28576358fa150dab50a24e1eb6f993d59
Andrew Verge <averge@chromium.org>
2026-03-24 16:44:47 -0700

    [Connection-Allowlist] Use "redirects" param for subresource fetches.
    This change will affect dedicated worker fetches as well. Updating
    corresponding worker tests will come in a follow-up CL.
    
    There is also a TODO for reporting support, which can be updated after
    the E2E reporting implementation is complete.
    
    Bug: 492439215
    Change-Id: I2bd67283419bc475a1979b7801f200b4d378a88e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7693712
    Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    Commit-Queue: Andrew Verge <averge@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1604469}

---
+++ third_party/blink/web_tests/external/wpt/connection-allowlist/tentative/fetch-redirect-allow.sub.window.js
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/common/get-host-info.sub.js
+// META: script=resources/fetch_redirect_test.js
+//
+// The following tests assume the policy `Connection-Allowlist:
+// (response-origin);redirects=allow` has been set. Redirects for fetches
+// allowed through connection allowlists should be allowed.
+
+// We're loading this page from `http://{{hosts[][]}}`.
+// The connection allowlist header is `Connection-Allowlist:
+// (response-origin);redirects=allow`. Thus, only `http://{{hosts[][]}}` is
+// allowlisted for fetches.
+
+// Same-origin redirect:
+// origin: http://{{hosts[][]}} (allowed by allowlist)
+// target: http://{{hosts[][]}} (also allowed)
+// This should SUCCEED, because of the "allow" header param.
+fetch_redirect_test(
+    'http://{{hosts[][]}}' + port, 'http://{{hosts[][]}}' + port, SUCCESS);
+
+// Redirect from an allowlisted origin to a different origin:
+// origin: http://{{hosts[][]}} (allowed by allowlist)
+// target: http://{{hosts[alt][]}} (not allowed)
+// This should SUCCEED, because of the "allow" header param.
+fetch_redirect_test(
+    'http://{{hosts[][]}}' + port, 'http://{{hosts[alt][]}}' + port, SUCCESS);
+
+// Fetch to a non-allowlisted origin:
+// origin: http://{{hosts[alt][]}} (not allowed)
+// This is blocked before the redirect even happens.
+fetch_redirect_test(
+    'http://{{hosts[alt][]}}' + port, 'http://{{hosts[][]}}' + port, FAILURE);
