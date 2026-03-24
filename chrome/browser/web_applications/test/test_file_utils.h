c7b709b8c3181d265e9583bca25306b93c2d323e
dmurph@chromium.org <dmurph@chromium.org>
2026-03-24 11:51:52 -0700

    [PWA] Moving from strict mock to fake for uninstall command test
    This change moves the WebAppUninstallCommandTest from using a strict
    mock on the file system utils to the 'fake' version, which allows us
    to intercept / inspect calls but still has the normal filesystem
    backing. This is a more robust way to test the system without
    hard-coding filesystem call patterns in the tests.
    
    There are a few comment update changes in this patch too.
    
    Change-Id: I0f10facf36db50d355f4de55c6e26690adc95347
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7694995
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Auto-Submit: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1604284}

--- chrome/browser/web_applications/test/test_file_utils.h
+++ chrome/browser/web_applications/test/test_file_utils.h
@@ -6,12 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_WEB_APPLICATIONS_TEST_TEST_FILE_UTILS_H_
 #define CHROME_BROWSER_WEB_APPLICATIONS_TEST_TEST_FILE_UTILS_H_
 
-#include <map>
 #include <memory>
 #include <optional>
 
 #include "base/containers/span.h"
 #include "chrome/browser/web_applications/file_utils_wrapper.h"
+#include "third_party/abseil-cpp/absl/container/flat_hash_map.h"
 
 namespace web_app {
 
@@ -21,10 +21,10 @@ class TestFileUtils : public FileUtilsWrapper {
   // Initializer list type deduction does not work through std::make_unique so
   // provide this helper function.
   static scoped_refptr<TestFileUtils> Create(
-      std::map<base::FilePath, base::FilePath> read_file_rerouting);
+      absl::flat_hash_map<base::FilePath, base::FilePath> read_file_rerouting);
 
-  explicit TestFileUtils(
-      std::map<base::FilePath, base::FilePath> read_file_rerouting = {});
+  explicit TestFileUtils(absl::flat_hash_map<base::FilePath, base::FilePath>
+                             read_file_rerouting = {});
   TestFileUtils(const TestFileUtils&) = delete;
   TestFileUtils& operator=(const TestFileUtils&) = delete;
 
@@ -41,14 +41,16 @@ class TestFileUtils : public FileUtilsWrapper {
   void SetRemainingDiskSpaceSize(int remaining_disk_space);
 
   void SetNextDeleteFileRecursivelyResult(std::optional<bool> delete_result);
+  void SetDeleteFileRecursivelyResult(const base::FilePath& path, bool result);
 
   TestFileUtils* AsTestFileUtils() override;
 
  private:
   ~TestFileUtils() override;
 
-  std::map<base::FilePath, base::FilePath> read_file_rerouting_;
+  absl::flat_hash_map<base::FilePath, base::FilePath> read_file_rerouting_;
   std::optional<bool> delete_file_recursively_result_;
+  absl::flat_hash_map<base::FilePath, bool> delete_file_recursively_results_;
   int remaining_disk_space_ = kNoLimit;
 };
 
