da8e746926e67886517818472600adc6ffae9727
dpapad <dpapad@chromium.org>
2026-03-24 16:56:36 -0700

    WebUI: Add type parameter to sendWithPromise(), part 2.
    The parameter indicates the payload type of the returned Promise as
    follows: export function sendWithPromise<T>(...): Promise<T> {...}
    
    In this part making the type parameter required by removing the "= any"
    fallback.
    
    Bug: 494464740
    Change-Id: I153bd07ffcc2b16d4c5caa3c9c0730cd2177d87b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7690239
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1604484}

--- chrome/browser/resources/sandbox_internals/sandbox_internals_win.ts
+++ chrome/browser/resources/sandbox_internals/sandbox_internals_win.ts
@@ -548,5 +548,6 @@ function onGetSandboxDiagnostics(results: SandboxDiagnostics) {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  sendWithPromise('requestSandboxDiagnostics').then(onGetSandboxDiagnostics);
+  sendWithPromise<SandboxDiagnostics>('requestSandboxDiagnostics')
+      .then(onGetSandboxDiagnostics);
 });
--- chrome/browser/resources/settings/languages_page/languages_browser_proxy.ts
+++ chrome/browser/resources/settings/languages_page/languages_browser_proxy.ts
@@ -34,7 +34,7 @@ export class LanguagesBrowserProxyImpl implements LanguagesBrowserProxy {
   }
 
   getProspectiveUiLanguage() {
-    return sendWithPromise('getProspectiveUILanguage');
+    return sendWithPromise<string>('getProspectiveUILanguage');
   }
 
   // </if>
