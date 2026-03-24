e5460cac6f6e197d6bdecf7a2d8c9fc48f8a9f92
James Cook <jamescook@chromium.org>
2026-03-24 10:46:39 -0700

    sync: Update ENABLE_EXTENSIONS build flag in ExtensionsActivityMonitor
    Now that we have extensions on desktop Android, there's a new build flag
    for extensions ENABLE_EXTENSIONS_CORE required for "traditional
    extensions platforms + desktop Android". Switch to that. This enables
    bookmark API activity monitoring on desktop Android, as we do on Win,
    Mac, Linux and Chrome OS.
    
    The unit tests are also enabled on desktop Android.
    
    See bug for details on the history of the two build flags.
    
    Bug: 494643383
    Change-Id: I5d6113b628735b8b5975542d30a1fb5400aa9b7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7693917
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1604240}

--- chrome/browser/sync/glue/extensions_activity_monitor.cc
+++ chrome/browser/sync/glue/extensions_activity_monitor.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/base/extensions_activity.h"
 #include "content/public/browser/browser_thread.h"
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
+#if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 #include "chrome/browser/extensions/api/bookmarks/bookmarks_api_watcher.h"  // nogncheck
 #include "extensions/browser/extension_function.h"  // nogncheck
 #include "extensions/browser/extension_function_histogram_value.h"
@@ -27,7 +27,7 @@ ExtensionsActivityMonitor::ExtensionsActivityMonitor(
   // the fly so there is no reliable object to point to (same problem if we
   // wanted to use the string name).  Thus, we use all sources and filter in
   // Observe.
-#if BUILDFLAG(ENABLE_EXTENSIONS)
+#if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
   bookmarks_api_observation_.Observe(
       extensions::BookmarksApiWatcher::GetForBrowserContext(context));
 #endif
@@ -37,7 +37,7 @@ ExtensionsActivityMonitor::~ExtensionsActivityMonitor() {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 }
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
+#if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 void ExtensionsActivityMonitor::OnBookmarksApiInvoked(
     const ExtensionFunction* func) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
