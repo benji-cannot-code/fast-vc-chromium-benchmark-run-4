217f82a5edfcf425f506e0fcbb1a0b18ba5b25a8
Arthur Sonzogni <arthursonzogni@chromium.org>
2026-03-24 07:19:16 -0700

    Convert #pragma allow_unsafe_* to PA_UNSAFE_TODO [3/4] (base util)
    This removes the #ifdef UNSAFE_BUFFERS_BUILD blocks from
    base/allocator/partition_allocator/src and wraps the newly exposed
    unsafe buffer accesses with PA_UNSAFE_TODO() macros to allow the
    build to succeed.
    
    Bug: 40284755
    Change-Id: I3c9b33d2defd632dd60bd1baa35083f6eee65323
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7691657
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1604102}

--- base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/strings/stringprintf.cc
+++ base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/strings/stringprintf.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40284755): Remove this and spanify to fix the errors.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "partition_alloc/partition_alloc_base/strings/stringprintf.h"
 
 #include <cstdarg>
@@ -25,10 +20,11 @@ std::string TruncatingStringPrintf(const char* format, ...) {
   va_list arguments;
   va_start(arguments, format);
 #if PA_BUILDFLAG(IS_WIN)
-  int result = vsnprintf_s(stack_buf, std::size(stack_buf), _TRUNCATE, format,
-                           arguments);
+  int result = PA_UNSAFE_TODO(vsnprintf_s(stack_buf, std::size(stack_buf),
+                                          _TRUNCATE, format, arguments));
 #else
-  int result = vsnprintf(stack_buf, std::size(stack_buf), format, arguments);
+  int result = PA_UNSAFE_TODO(
+      vsnprintf(stack_buf, std::size(stack_buf), format, arguments));
 #endif
   va_end(arguments);
 #if PA_BUILDFLAG(IS_WIN)
