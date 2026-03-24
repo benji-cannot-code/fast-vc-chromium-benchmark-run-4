36f465721914a3ac30ba99cd45081ec21ed956b1
Saifuddin Hitawala <hitawala@chromium.org>
2026-03-23 18:33:25 -0700

    [gpu] Add AutoLock for is_texture_sampling_queried_
    I missed AutoLock when using is_texture_sampling_queried_ in SIManager
    in [1]. This change adds that when using the bool.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/7667553
    
    Bug: 490087053
    Change-Id: If29877e8014166bc43712c8e7630a141878bf12d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7691718
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1603836}

--- gpu/command_buffer/service/shared_image/shared_image_manager.cc
+++ gpu/command_buffer/service/shared_image/shared_image_manager.cc
@@ -668,6 +668,7 @@ void SharedImageManager::QueryMultiplanarTextureSamplingSupport() {
 
 bool SharedImageManager::SupportsNV12TextureSampling() {
 #if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_FUCHSIA)
+  AutoLock autolock(this);
   if (!is_texture_sampling_queried_) {
     QueryMultiplanarTextureSamplingSupport();
   }
@@ -681,6 +682,7 @@ bool SharedImageManager::SupportsNV12TextureSampling() {
 
 bool SharedImageManager::SupportsP010TextureSampling() {
 #if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_FUCHSIA)
+  AutoLock autolock(this);
   if (!is_texture_sampling_queried_) {
     QueryMultiplanarTextureSamplingSupport();
   }
