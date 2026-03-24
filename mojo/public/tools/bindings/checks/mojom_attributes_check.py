c764cc8578d32cc38a13189db00855cf59763438
Daniel Cheng <dcheng@chromium.org>
2026-03-24 09:35:13 -0700

    No longer treat DirectReceiver as a valid mojom attribute
    Bug: 495474994
    Change-Id: I714ac955eb45044ba23e18e73790c1f322ce1f2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7694018
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1604192}

--- mojo/public/tools/bindings/checks/mojom_attributes_check.py
+++ mojo/public/tools/bindings/checks/mojom_attributes_check.py
@@ -37,7 +37,6 @@ _ENUMVAL_ATTRIBUTES = _COMMON_ATTRIBUTES | {
 
 _INTERFACE_ATTRIBUTES = _COMMON_ATTRIBUTES | {
     'DispatchDebugAlias',
-    'DirectReceiver',
     'RenamedFrom',
     'RequireContext',
     'RuntimeFeature',
