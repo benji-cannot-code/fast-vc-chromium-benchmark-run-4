e847235b783ef234af41fce7ae2b377bab63ff3d
Dominik Röttsches <drott@chromium.org>
2026-03-24 01:56:29 -0700

    Instantiation and src: format() support for Avar2
    Allow the avar2 production in @font-face src: format.
    
    Create a feature flag that gates supports for this technology keyword as
    well as whether to enable bridging to Fontations backend support for
    avar2 fonts.
    
    Preparation for enabling avar2.
    
    Bug: 40246300
    Change-Id: Ic69353acd6b17c8a8e299d8130b0e637e0cef8b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6874548
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1603961}

--- third_party/blink/renderer/platform/fonts/opentype/font_format_check.cc
+++ third_party/blink/renderer/platform/fonts/opentype/font_format_check.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/span_rust.h"
 #include "base/numerics/byte_conversions.h"
 #include "third_party/blink/renderer/platform/fonts/opentype/format_check.rs.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 #include "third_party/skia/include/core/SkTypeface.h"
 
@@ -49,6 +50,11 @@ bool FontFormatCheck::IsVariableColrV0Font() const {
 bool FontFormatCheck::IsColorFont() const {
   return IsSbixColorFont() || IsCbdtCblcColorFont() ||
          IsColrCpalColorFontV0() || IsColrCpalColorFontV1();
+}
+
+bool FontFormatCheck::IsAvar2Font() const {
+  return RuntimeEnabledFeatures::FontFormatAvar2Enabled() &&
+         font_format_check::is_avar2(*format_info_);
 }
 
 FontFormatCheck::VariableFontSubType FontFormatCheck::ProbeVariableFont(
