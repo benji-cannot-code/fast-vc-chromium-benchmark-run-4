0563ce08be631a706d41b5c4d813c8afd3662d31
Trevor Perrier <perrier@chromium.org>
2026-03-24 16:46:16 -0700

    [glic] Record a user action when a Glic instance is opened.
    This change adds the "Glic.Instance.Open" user action to complement
    existing "Glic.Instance.Toggle/Close" actions and a new Open
    InstanceEvent to match.
    
    The browser test is updated to verify this new action, as well as
    existing "Glic.Instance.Close" and "Glic.Instance.Toggle" actions.
    
    Bug: b:495535969
    Change-Id: I020f63c4b42efab5720a08f683cbfb4d13fef645
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7694339
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Trevor Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1604472}

--- chrome/browser/glic/service/metrics/glic_instance_metrics.h
+++ chrome/browser/glic/service/metrics/glic_instance_metrics.h
@@ -110,7 +110,8 @@ enum class GlicInstanceEvent {
   kTurnCompleted = 45,
   kReaction = 46,
   kShown = 47,
-  kMaxValue = kShown,
+  kOpen = 48,
+  kMaxValue = kOpen,
 };
 // LINT.ThenChange(//tools/metrics/histograms/metadata/glic/enums.xml:GlicInstanceEvent)
 
