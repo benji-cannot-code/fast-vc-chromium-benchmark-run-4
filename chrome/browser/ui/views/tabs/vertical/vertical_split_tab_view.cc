da733a66aef5b7521e5c6eb082569459cc91dd65
Dominic Austria <dominicaustria@google.com>
2026-03-24 13:48:16 -0700

    [Vertical Tabs] Clean up hover card on vertical split tab view reset
    Bug: 495269751
    Change-Id: I262a316a4d4dc04011c2f3277b4787e7950baa08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7696728
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Dominic Austria <dominicaustria@google.com>
    Cr-Commit-Position: refs/heads/main@{#1604364}

--- chrome/browser/ui/views/tabs/vertical/vertical_split_tab_view.cc
+++ chrome/browser/ui/views/tabs/vertical/vertical_split_tab_view.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/layout_constants.h"
 #include "chrome/browser/ui/tabs/tab_style.h"
 #include "chrome/browser/ui/views/tabs/tab/glow_hover_controller.h"
+#include "chrome/browser/ui/views/tabs/tab_hover_card_controller.h"
 #include "chrome/browser/ui/views/tabs/vertical/tab_collection_animating_layout_manager.h"
 #include "chrome/browser/ui/views/tabs/vertical/tab_collection_node.h"
 #include "chrome/browser/ui/views/tabs/vertical/vertical_tab_drag_handler.h"
@@ -204,6 +205,14 @@ double VerticalSplitTabView::GetHoverAnimationValue() const {
 }
 
 void VerticalSplitTabView::ResetCollectionNode() {
+  CHECK(collection_node_);
+  TabHoverCardController* hover_card_controller =
+      collection_node_->GetController()->GetHoverCardController();
+  if (hover_card_controller) {
+    hover_card_controller->UpdateHoverCard(
+        nullptr, TabSlotController::HoverCardUpdateType::kTabRemoved);
+  }
+
   collection_node_ = nullptr;
 }
 
