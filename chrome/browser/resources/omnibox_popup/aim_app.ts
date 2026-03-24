2a59cc7b33b8f0664a5a523aa4ca844f374d76dd
Ananya <ananyaseelam@google.com>
2026-03-24 13:13:24 -0700

    [aim-eligibility] Remove `SearchboxConfig` Fallback Config
    Instead of creating a fallback config, we will not show the context menu
    entrypoint if the `SearchboxConfig` doesn't exist in the aim eligibility
    response. This CL also updates the formatting of the omnibox aim popup
    such that if no context menu is available, the omnibox is in the Compact
    layout with the voice icon next to the clear button.
    
    Before:http://screencast/cast/NjExMDIyOTE0MDg2NTAyNHw3OTUxMGY2OC1iZQ
    After:https://screencast.googleplex.com/cast/NDgyODExMjk1MTk2Nzc0NHwxOGZjZjFiZS0wYw
    
    Bug: b:490044016
    Change-Id: Ic73bd0aa1801f148337b5814bdd8b4b8357f5b85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7693213
    Commit-Queue: Ananya Seelam <ananyaseelam@google.com>
    Reviewed-by: Josiah Kiehl <kiehl@google.com>
    Cr-Commit-Position: refs/heads/main@{#1604343}

--- chrome/browser/resources/omnibox_popup/aim_app.ts
+++ chrome/browser/resources/omnibox_popup/aim_app.ts
@@ -13,6 +13,7 @@ import {EventTracker} from '//resources/js/event_tracker.js';
 import {loadTimeData} from '//resources/js/load_time_data.js';
 import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
 import type {SearchContext} from '//resources/mojo/components/omnibox/browser/searchbox.mojom-webui.js';
+import type {InputState} from '//resources/mojo/components/omnibox/composebox/composebox_query.mojom-webui.js';
 
 import {getCss} from './aim_app.css.js';
 import {getHtml} from './aim_app.html.js';
@@ -38,8 +39,16 @@ export class OmniboxAimAppElement extends CrLitElement {
     return getHtml.bind(this)();
   }
 
-  protected searchboxLayoutMode_: string =
+  static override get properties() {
+    return {
+      searchboxLayoutMode_: {type: String},
+      hasAllowedInputs_: {type: Boolean},
+    };
+  }
+
+  protected accessor searchboxLayoutMode_: string =
       loadTimeData.getString('searchboxLayoutMode');
+  protected accessor hasAllowedInputs_: boolean = false;
   protected disableCaretColorAnimation_: boolean =
       loadTimeData.getBoolean('caretColorAnimationDisabled');
   protected disableComposeboxAnimation_: boolean =
@@ -73,6 +82,15 @@ export class OmniboxAimAppElement extends CrLitElement {
           this.setPreserveContextOnClose_.bind(this)),
     ];
 
+    this.eventTracker_.add(
+        this.$.composebox, 'input-state-changed',
+        (e: CustomEvent<{inputState: InputState}>) => {
+          const inputState = e.detail.inputState;
+          this.hasAllowedInputs_ = inputState.allowedModels.length > 0 ||
+              inputState.allowedTools.length > 0 ||
+              inputState.allowedInputTypes.length > 0;
+        });
+
     this.focusInput_();
 
     this.setupLocalizedLinkListener();
@@ -86,6 +104,14 @@ export class OmniboxAimAppElement extends CrLitElement {
       this.callbackRouter_.removeListener(listenerId);
     }
     this.listenerIds_ = [];
+  }
+
+  protected getSearchboxLayoutMode_(): string {
+    if (this.searchboxLayoutMode_.startsWith('Tall') &&
+        !this.hasAllowedInputs_) {
+      return 'Compact';
+    }
+    return this.searchboxLayoutMode_;
   }
 
   // As links do not navigate in the omnibox as they do in normal
@@ -164,6 +190,22 @@ export class OmniboxAimAppElement extends CrLitElement {
     e.preventDefault();
     const href = (e.currentTarget as HTMLAnchorElement).href;
     this.pageHandler_.navigateCurrentTab(href);
+  }
+
+  setSearchboxLayoutModeForTesting(mode: string) {
+    this.searchboxLayoutMode_ = mode;
+  }
+
+  setHasAllowedInputsForTesting(hasAllowedInputs: boolean) {
+    this.hasAllowedInputs_ = hasAllowedInputs;
+  }
+
+  getSearchboxLayoutModeForTesting(): string {
+    return this.getSearchboxLayoutMode_();
+  }
+
+  getHasAllowedInputsForTesting(): boolean {
+    return this.hasAllowedInputs_;
   }
 }
 
