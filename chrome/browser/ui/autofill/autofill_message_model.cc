71f51626dfc81029cfe2af1f7d18974e14ba6053
Fernando Ramirez <ferny@google.com>
2026-03-24 16:32:42 -0700

    [AF][Clank] Remove generic card icon from local card fallback message
    Mock: https://screenshot.googleplex.com/36VqAqvYttspRtD.png
    Before: https://screenshot.googleplex.com/BpA89kYfWm3ziH6.png
    After: https://screenshot.googleplex.com/5oESwTUaPoPs4xw.png
    
    Bug: 468139053
    Change-Id: Idc40ba38dcbe2df1e5c979097953a8a376d9db8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7698713
    Reviewed-by: Olivia Saul <osaul@google.com>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Fernando Ramirez <ferny@google.com>
    Cr-Commit-Position: refs/heads/main@{#1604460}

--- chrome/browser/ui/autofill/autofill_message_model.cc
+++ chrome/browser/ui/autofill/autofill_message_model.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "chrome/browser/android/resource_mapper.h"
 #include "components/autofill/core/browser/ui/payments/save_payment_method_and_virtual_card_enroll_confirmation_ui_params.h"
+#include "components/autofill/core/common/autofill_payments_features.h"
 #include "components/grit/components_scaled_resources.h"
 #include "components/messages/android/message_enums.h"
 #include "components/messages/android/message_wrapper.h"
@@ -52,8 +53,11 @@ AutofillMessageModel::CreateForSaveCardFailure() {
   message->SetTitle(ui_params.title_text);
   message->SetDescription(ui_params.description_text);
   message->SetPrimaryButtonText(ui_params.failure_ok_button_text);
-  message->SetIconResourceId(
-      ResourceMapper::MapToJavaDrawableId(IDR_AUTOFILL_CC_GENERIC_OLD));
+  if (!base::FeatureList::IsEnabled(
+          features::kAutofillEnableWalletBrandingV2)) {
+    message->SetIconResourceId(
+        ResourceMapper::MapToJavaDrawableId(IDR_AUTOFILL_CC_GENERIC_OLD));
+  }
 
   return base::WrapUnique(
       new AutofillMessageModel(std::move(message), Type::kSaveCardFailure));
