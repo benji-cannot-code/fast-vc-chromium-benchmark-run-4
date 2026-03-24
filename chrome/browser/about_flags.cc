d581932231c3dd3bebfbd0b9fc8db144a1b5e538
Jordan Bayles <jophba@chromium.org>
2026-03-24 11:41:52 -0700

    [Symphonia] Cleanup feature flags, about:flag controls
    This patch cleans up use of the feature flags, reducing
    duplicate code in the FFmpegAudioDecoder, adding some missed
    flags to the audio file reader tests, and cleaning up the
    about:flag to do what I would expect it to do.
    
    Bug: 385172063
    Change-Id: I5f8a52f900d2dbe08ab05bf7ffc7e28d96dc609f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7693460
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1604278}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -4778,6 +4778,18 @@ const FeatureEntry::Choice kConnectionAllowlistsChoices[] = {
      "ConnectionAllowlists,OverrideConnectionAllowlistOriginTrial"},
 };
 
+#if BUILDFLAG(ENABLE_SYMPHONIA)
+// The choices for the Symphonia audio decoding feature.
+const FeatureEntry::Choice kSymphoniaAudioDecodingChoices[] = {
+    {flags_ui::kGenericExperimentChoiceDefault, "", ""},
+    {"Enabled (All Codecs)", switches::kEnableFeatures,
+     "SymphoniaAudioDecoding,SymphoniaMp3Decoding,SymphoniaPcmDecoding,"
+     "SymphoniaVorbisDecoding"},
+    {"Disabled (Overrides Finch)", switches::kDisableFeatures,
+     "SymphoniaAudioDecoding,SymphoniaMp3Decoding,SymphoniaPcmDecoding,"
+     "SymphoniaVorbisDecoding"}};
+#endif
+
 // RECORDING USER METRICS FOR FLAGS:
 // -----------------------------------------------------------------------------
 // The first line of the entry is the internal name.
@@ -9415,7 +9427,7 @@ const FeatureEntry kFeatureEntries[] = {
 #if BUILDFLAG(ENABLE_SYMPHONIA)
     {"symphonia-audio-decoding", flag_descriptions::kSymphoniaAudioDecodingName,
      flag_descriptions::kSymphoniaAudioDecodingDescription, kOsAll,
-     FEATURE_VALUE_TYPE(media::kSymphoniaAudioDecoding)},
+     MULTI_VALUE_TYPE(kSymphoniaAudioDecodingChoices)},
 #endif
 
     {"safety-check-unused-site-permissions",
