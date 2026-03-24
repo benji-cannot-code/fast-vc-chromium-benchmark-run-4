56976d7d99ee33ecc8fb957040f3711adfb4f73f
Jun Ishiguro <junis@google.com>
2026-03-23 17:29:59 -0700

    login: Pass local state PrefService for lock_screen_utils.cc
    This CL passes local state PrefService to utility functions defined
    in lock_screen_utils.cc.
    
    Bug: b:404133029
    Change-Id: Iccfc4c05699dfbd399ebfc5e04f681a6582986e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7691565
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1603810}

--- chrome/browser/ash/login/lock_screen_utils.h
+++ chrome/browser/ash/login/lock_screen_utils.h
@@ -11,17 +11,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ime/ash/input_method_manager.h"
 
 class AccountId;
+class PrefService;
 
 namespace ash::lock_screen_utils {
 
 // Update current input method in the given IME state to last input method used
 // by this user.
-void SetUserInputMethod(const AccountId& account_id,
+void SetUserInputMethod(PrefService& local_state,
+                        const AccountId& account_id,
                         input_method::InputMethodManager::State* ime_state,
                         bool honor_device_policy);
 
 // Get user's last input method ID.
-std::string GetUserLastInputMethodId(const AccountId& account_id);
+std::string GetUserLastInputMethodId(PrefService& local_state,
+                                     const AccountId& account_id);
 
 // Sets the currently allowed input method ID, including those that are enforced
 // by device policy.
@@ -31,7 +34,7 @@ void EnforceDevicePolicyInputMethods(std::string user_input_method_id);
 void StopEnforcingPolicyInputMethods();
 
 // Update the keyboard settings for `account_id`.
-void SetKeyboardSettings(const AccountId& account_id);
+void SetKeyboardSettings(PrefService& local_state, const AccountId& account_id);
 
 // Covert a base::ListValue of locale info to a list of ash struct LocaleItem.
 std::vector<LocaleItem> FromListValueToLocaleItem(base::ListValue locales);
