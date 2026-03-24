932a7f9dc4c420bd0ef421d70b2ca8bd52d58c7e
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2026-03-23 21:54:16 -0700

    Revert "[Socket Pool] (8) Experiment with raising kDefaultMaxSocketsPerProxyChain"
    This reverts commit 97a68baac042baec1cd1c60397f62ceb2491213e.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5761554055168000
    
    Sample build with failed test: https://ci.chromium.org/b/8686494306193761009
    Affected test(s):
    [://chrome/test\:browser_tests!gtest::NetworkContextConfigurationProxySettingsBrowserTest#MaxConnectionsPerProxy/ProfileMainNetworkContext.0](https://ci.chromium.org/ui/test/chromium/:%2F%2Fchrome%2Ftest%5C:browser_tests%21gtest::NetworkContextConfigurationProxySettingsBrowserTest%23MaxConnectionsPerProxy%2FProfileMainNetworkContext.0?q=VHash%3Aefe34715356dfeed)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5761554055168000&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F7690292&type=BUG
    
    Original change's description:
    > [Socket Pool] (8) Experiment with raising kDefaultMaxSocketsPerProxyChain
    >
    > We allow significantly fewer connections (32 vs 256+) when proxied. This
    > causes significantly different performance, and has been true for around
    > 12 years. Let's try some different limits and see what we can do.
    >
    > This CL completes the work by adding a finch controlled trial for the
    > per-proxy socket limit. Let's see what we can do here!
    >
    > This CL is part of a series:
    > (1) Switch to a strict enum for SocketPoolType
    > (2) Remove iOS-specific proxy pool settings
    > (3) Remove kDefaultMaxSocketsPerProxyChain
    > (4) Expand the enterprise policy controlling the proxy limit
    > (5) Expand OS availability of MaxConnectionsPerProxy
    > (6) Improve policy tests
    > (7) Add an enterprise policy to control the websocket proxy limit
    > (8) Experiment with raising kDefaultMaxSocketsPerProxyChain
    >
    > Bug: 467278609
    > Change-Id: Id114eb4201088c57686637d3037849d7f1b5fe72
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7690292
    > Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    > Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    > Reviewed-by: mmenke <mmenke@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1603745}
    >
    
    Bug: 467278609
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I28381cfadbad9ef7d9ecd86a46d27acc6a4fa508
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7693944
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Owners-Override: Kalvin Lee <kdlee@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1603893}

--- chrome/browser/net/network_context_configuration_browsertest.cc
+++ chrome/browser/net/network_context_configuration_browsertest.cc
@@ -1884,7 +1884,7 @@ class WaitingHandshakeClient : public network::mojom::WebSocketHandshakeClient {
 class NetworkContextConfigurationProxySettingsBrowserTest
     : public NetworkContextConfigurationHttpPacBrowserTest {
  public:
-  const size_t kDefaultMaxConnectionsPerProxy = 64;
+  const size_t kDefaultMaxConnectionsPerProxy = 32;
 
   NetworkContextConfigurationProxySettingsBrowserTest() {
     // This is disabled as backup jobs cause extra connections without opening
@@ -2075,7 +2075,7 @@ IN_PROC_BROWSER_TEST_P(NetworkContextConfigurationProxySettingsBrowserTest,
 class NetworkContextConfigurationManagedProxySettingsBrowserTest
     : public NetworkContextConfigurationProxySettingsBrowserTest {
  public:
-  const size_t kTestMaxConnectionsPerProxy = 16;
+  const size_t kTestMaxConnectionsPerProxy = 37;
 
   NetworkContextConfigurationManagedProxySettingsBrowserTest() = default;
 
