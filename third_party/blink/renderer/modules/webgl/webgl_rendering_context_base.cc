c79c8836ad44cf15f26e6f287719c3533d9eadc6
Vasiliy Telezhnikov <vasilyt@chromium.org>
2026-03-24 09:52:44 -0700

    Add dirty_rect_for_commit_ to webgl context
    Logic here mimics the logic in OffscreenCanvasRenderingContext2D, with
    the exception that damage is always full.
    
    This CL is no-op, because the only way to get to the PushFrame is to
    have `needs_push_frame` to be true here [1]. That in turn means DidDraw
    must have been called [2].
    
    For WebGL+OffscreenCanvas it would be:
    * OffscreenCanvas::SetSize [3], which makes rect full inside Reshape()
      call above.
    * MarkContextChanged [4], which makes rect to full.
    
    As we reset rect only after we called PushFrame, rect is guaranteed
    to be set to full size in PushFrame call.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.cc;drc=bef3f0079c139376da94621012cb728b73fa4b5a;l=496
    [2] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.cc;drc=bef3f0079c139376da94621012cb728b73fa4b5a;l=482
    [3] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.cc;drc=5cfe525024bc42f5433fff03ee62998590268c2b;l=177
    [4] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc;drc=5cfe525024bc42f5433fff03ee62998590268c2b;l=1581
    
    Change-Id: I48494af113f523752b43c4ab57a76abde81fb4a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7694254
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1604206}

--- third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
+++ third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
@@ -1290,6 +1290,8 @@ WebGLRenderingContextBase::WebGLRenderingContextBase(
   ADD_VALUES_TO_SET(supported_tex_image_source_formats_, kSupportedFormatsES2);
   ADD_VALUES_TO_SET(supported_types_, kSupportedTypesES2);
   ADD_VALUES_TO_SET(supported_tex_image_source_types_, kSupportedTypesES2);
+
+  dirty_rect_for_commit_.setEmpty();
 }
 
 scoped_refptr<DrawingBuffer> WebGLRenderingContextBase::CreateDrawingBuffer(
@@ -1579,6 +1581,9 @@ void WebGLRenderingContextBase::MarkContextChanged(
 
   if (Host()->IsOffscreenCanvas()) {
     marked_canvas_dirty_ = true;
+    dirty_rect_for_commit_.join(
+        SkIRect::MakeWH(Host()->Size().width(), Host()->Size().height()));
+
     DidDraw(draw_type);
     return;
   }
@@ -1620,8 +1625,8 @@ bool WebGLRenderingContextBase::PushFrameNoCopy() {
   if (!canvas_resource)
     return false;
   const bool submitted_frame =
-      Host()->PushFrame(std::move(canvas_resource), std::nullopt);
-
+      Host()->PushFrame(std::move(canvas_resource), dirty_rect_for_commit_);
+  dirty_rect_for_commit_.setEmpty();
   MarkLayerComposited();
   return submitted_frame;
 }
@@ -1642,8 +1647,9 @@ bool WebGLRenderingContextBase::PushFrameWithCopy() {
   if (resource_provider && resource_provider_has_content_for_frame_push_) {
     submitted_frame = Host()->PushFrame(
         resource_provider->ProduceCanvasResource(FlushReason::kOther),
-        std::nullopt);
+        dirty_rect_for_commit_);
     resource_provider_has_content_for_frame_push_ = false;
+    dirty_rect_for_commit_.setEmpty();
   }
   MarkLayerComposited();
   return submitted_frame;
@@ -2195,6 +2201,8 @@ WebGLRenderingContextBase::GetRGBAUnacceleratedStaticBitmapImage(
 }
 
 void WebGLRenderingContextBase::Reshape(int width, int height) {
+  dirty_rect_for_commit_ = SkIRect::MakeWH(width, height);
+
   if (isContextLost())
     return;
 
