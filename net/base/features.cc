7c924b42651a2f1fc48b6ae8c3aed9ee82c4305f
Kenichi Ishibashi <bashi@chromium.org>
2026-03-24 01:13:55 -0700

    [Android] Add feature flag for Local Network Protection check
    So that we can enable/disable the check at runtime.
    
    Bug: 495481382
    Change-Id: If55407301c2be77f3c7cd391d78dfae7ff651ea3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7695633
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Auto-Submit: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1603939}

--- net/base/features.cc
+++ net/base/features.cc
@@ -790,4 +790,6 @@ BASE_FEATURE(kEnableErrorCodePropagationForPreconnect,
 BASE_FEATURE(kPermitTcpSocketPoolConnectBackupJobs,
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+BASE_FEATURE(kLocalNetworkPermissionCheck, base::FEATURE_ENABLED_BY_DEFAULT);
+
 }  // namespace net::features
