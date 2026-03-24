8075e674ad0417f438ce2a20d713e47d8065abbd
Khalid Peer <khalidpeer@chromium.org>
2026-03-24 10:07:19 -0700

    [cobrowse] Make p13n settings link clickable in zero-state side panel.
    The changes in this CL enable users to actually click the
    "personalization settings" link that shows up in the CoBrowsing side
    panel UI when it's in the zero-state. More specifically, clicking on
    this link will now trigger a navigation to the appropriate page in
    a new foreground tab.
    
    Bug: b:487751677
    Change-Id: Ib552abe3fa35e2beb21a37ed2c9b755e7b104425
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7695272
    Commit-Queue: Khalid Peer <khalidpeer@chromium.org>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1604217}

--- chrome/browser/resources/contextual_tasks/composebox.html.ts
+++ chrome/browser/resources/contextual_tasks/composebox.html.ts
@@ -36,7 +36,8 @@ export function getHtml(this: ContextualTasksComposeboxElement) {
       ${this.showSuggestionsActivityLink_ && this.showSuggestions_ ? html`
         <div id="suggestionActivity">
           <localized-link
-            localized-string="${this.i18nAdvanced('suggestionActivityLink')}">
+            localized-string="${this.i18nAdvanced('suggestionActivityLink')}"
+            @link-clicked="${this.onSuggestionActivityLinkClicked_}">
           </localized-link>
         </div>
       `: ''}
@@ -85,7 +86,8 @@ export function getHtml(this: ContextualTasksComposeboxElement) {
       ${this.showSuggestionsActivityLink_ && this.showSuggestions_ ? html`
         <div id="suggestionActivity">
           <localized-link
-            localized-string="${this.i18nAdvanced('suggestionActivityLink')}">
+            localized-string="${this.i18nAdvanced('suggestionActivityLink')}"
+            @link-clicked="${this.onSuggestionActivityLinkClicked_}">
           </localized-link>
         </div>
       `: ''}
--- chrome/test/data/webui/contextual_tasks/test_contextual_tasks_browser_proxy.ts
+++ chrome/test/data/webui/contextual_tasks/test_contextual_tasks_browser_proxy.ts
@@ -281,8 +281,8 @@ class TestContextualTasksPageHandler extends TestBrowserProxy implements
     this.methodCalled('openOnboardingHelpUi');
   }
 
-  openUrl() {
-    this.methodCalled('openUrl');
+  openUrl(url: Url|string, disposition: number) {
+    this.methodCalled('openUrl', url, disposition);
   }
 
   onboardingTooltipDismissed() {
