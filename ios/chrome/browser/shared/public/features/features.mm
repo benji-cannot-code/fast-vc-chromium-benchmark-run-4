6d692833524f9ae3a320f9b5d427e4f4131ee1fc
Olivier Robin <olivierrobin@google.com>
2026-03-25 08:37:52 -0700

    Enable some MiniMap detection restrictions by default
    Enable Mini Map restrictions
    - Minimum characters for an address: 14 characters
    - Minimum proportion of alphanum characters: 80%.
    
    Note that we may tweak the value further in the future, but this state
    will be our new base state.
    
    Bug: 400942152
    Change-Id: Ic30dee05a973e2083a407f654fb3d2eb9d2f7a23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7698862
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1604847}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -578,7 +578,7 @@ bool IsIPHGestureRecognitionImprovementEnabled() {
       kIPHGestureRecognitionAblation, kIPHGestureRecognitionImprovement, false);
 }
 
-BASE_FEATURE(kIOSOneTapMiniMapRestrictions, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kIOSOneTapMiniMapRestrictions, base::FEATURE_ENABLED_BY_DEFAULT);
 
 const char kIOSOneTapMiniMapRestrictionCrossValidateParamName[] =
     "ios-one-tap-minimap-cross-validate";
@@ -600,7 +600,7 @@ BASE_FEATURE_PARAM(int,
                    kIOSOneTapMiniMapRestrictionMinCharsParam,
                    &kIOSOneTapMiniMapRestrictions,
                    kIOSOneTapMiniMapRestrictionMinCharsParamName,
-                   0);
+                   14);
 const char kIOSOneTapMiniMapRestrictionMaxSectionsParamName[] =
     "ios-one-tap-minimap-max-section";
 BASE_FEATURE_PARAM(int,
@@ -621,7 +621,7 @@ constexpr base::FeatureParam<double>
     kIOSOneTapMiniMapRestrictionMinAlphanumProportionParam{
         &kIOSOneTapMiniMapRestrictions,
         /*name=*/kIOSOneTapMiniMapRestrictionMinAlphanumProportionParamName,
-        /*default_value=*/0};
+        /*default_value=*/0.8};
 
 bool IsNotificationCollisionManagementEnabled() {
   return base::FeatureList::IsEnabled(kNotificationCollisionManagement);
