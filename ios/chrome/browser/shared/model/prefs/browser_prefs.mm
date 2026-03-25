72c5d4d498e251e88c17404ea0416a9f9ee94328
Chirag Arora <heychirag@google.com>
2026-03-25 01:06:07 -0700

    Use new consent prefs in `IsMetricsReportingEnabledForOfficialBuild()`.
    This commit adds support for `kMetricsReportingLevel` to be used to
    determine if UMA metrics should be enabled or not. The changes are
    gated behind the flag `kRestructureMetricsConsentSettings` and an
    additional check is added on `kMetricsReportingMigrationDone` to make
    sure the new consent state is only used once the migration has happened.
    
    Bug: b:483043192
    Change-Id: I1d4199b8129c6dbf722ad3a9b245835f589689fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7669111
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Commit-Queue: Chirag Arora <heychirag@google.com>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1604640}

--- ios/chrome/browser/shared/model/prefs/browser_prefs.mm
+++ ios/chrome/browser/shared/model/prefs/browser_prefs.mm
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/lens/lens_overlay_permission_utils.h"
 #import "components/metrics/demographics/user_demographics.h"
 #import "components/metrics/metrics_pref_names.h"
+#import "components/metrics/metrics_reporting_level.h"
 #import "components/network_time/network_time_tracker.h"
 #import "components/ntp_tiles/custom_links_manager_impl.h"
 #import "components/ntp_tiles/most_visited_sites.h"
@@ -386,6 +387,11 @@ void RegisterLocalStatePrefs(PrefRegistrySimple* registry) {
                                std::string());
   registry->RegisterBooleanPref(prefs::kEulaAccepted, false);
   registry->RegisterBooleanPref(metrics::prefs::kMetricsReportingEnabled,
+                                false);
+  registry->RegisterIntegerPref(
+      metrics::prefs::kMetricsReportingLevel,
+      static_cast<int>(metrics::MetricsReportingLevel::kNone));
+  registry->RegisterBooleanPref(metrics::prefs::kMetricsReportingMigrationDone,
                                 false);
 
   // Deprecated 07/2025 (migrated to profile prefs).
