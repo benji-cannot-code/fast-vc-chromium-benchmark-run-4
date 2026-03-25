eaad597eff21e5b61e702541e2af69e71568f61c
Norge Vizcay <vizcay@google.com>
2026-03-25 06:46:56 -0700

    accessibility_annotator: Restructure features and params
    Restructure the content of accessibility_annotator_features.h and
    accessibility_annotator_features.cc so that feature params are next to
    the corresponding feature to improve readability and maintainability.
    
    Bug: 428686605
    Change-Id: Ia0f54b4b45c71c4770e22876d5df75ef865e211a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7697758
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1604758}

--- components/accessibility_annotator/core/accessibility_annotator_features.cc
+++ components/accessibility_annotator/core/accessibility_annotator_features.cc
@@ -8,13 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace accessibility_annotator {
 
 BASE_FEATURE(kContentAnnotator, base::FEATURE_DISABLED_BY_DEFAULT);
-BASE_FEATURE(kAccessibilityAnnotator, base::FEATURE_DISABLED_BY_DEFAULT);
-BASE_FEATURE(kAccessibilityAnnotatorFirstRun,
-             base::FEATURE_DISABLED_BY_DEFAULT);
-BASE_FEATURE(kAccessibilityAnnotatorGetEntities,
-             base::FEATURE_DISABLED_BY_DEFAULT);
-BASE_FEATURE(kAccessibilityAnnotatorLiveTabContext, base::FEATURE_DISABLED_BY_DEFAULT);
-
 BASE_FEATURE_PARAM(int,
                    kContentAnnotatorMaxPendingUrls,
                    &kContentAnnotator,
@@ -81,19 +74,28 @@ BASE_FEATURE_PARAM(std::string,
                    "content_annotator_extracted_data_validation_schema",
                    "");
 
-BASE_FEATURE(kAccessibilityAnnotationReducerOnePResolver,
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
-BASE_FEATURE_PARAM(std::string,
-                   kAccessibilityAnnotatorOnePServiceUrl,
-                   &kAccessibilityAnnotationReducerOnePResolver,
-                   "one_p_service_url",
-                   "");
-
+BASE_FEATURE(kAccessibilityAnnotator, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE_PARAM(base::TimeDelta,
                    kAccessibilityAnnotationTTL,
                    &kAccessibilityAnnotator,
                    "accessibility_annotation_ttl",
                    base::Days(7));
+
+BASE_FEATURE(kAccessibilityAnnotatorFirstRun,
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kAccessibilityAnnotatorGetEntities,
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kAccessibilityAnnotatorLiveTabContext,
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kAccessibilityAnnotationReducerOnePResolver,
+             base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE_PARAM(std::string,
+                   kAccessibilityAnnotatorOnePServiceUrl,
+                   &kAccessibilityAnnotationReducerOnePResolver,
+                   "one_p_service_url",
+                   "");
 
 }  // namespace accessibility_annotator
--- components/accessibility_annotator/core/accessibility_annotator_features.h
+++ components/accessibility_annotator/core/accessibility_annotator_features.h
@@ -13,11 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace accessibility_annotator {
 
 BASE_DECLARE_FEATURE(kContentAnnotator);
-BASE_DECLARE_FEATURE(kAccessibilityAnnotator);
-BASE_DECLARE_FEATURE(kAccessibilityAnnotatorFirstRun);
-BASE_DECLARE_FEATURE(kAccessibilityAnnotatorGetEntities);
-BASE_DECLARE_FEATURE(kAccessibilityAnnotatorLiveTabContext);
-
 BASE_DECLARE_FEATURE_PARAM(int, kContentAnnotatorMaxPendingUrls);
 BASE_DECLARE_FEATURE_PARAM(std::string,
                            kContentAnnotatorClassifierTitleKeywordRules);
@@ -37,11 +32,17 @@ BASE_DECLARE_FEATURE_PARAM(double, kContentAnnotatorSemanticMatchThreshold);
 BASE_DECLARE_FEATURE_PARAM(std::string,
                            kContentAnnotatorExtractedDataValidationSchema);
 
-BASE_DECLARE_FEATURE(kAccessibilityAnnotationReducerOnePResolver);
-
-BASE_DECLARE_FEATURE_PARAM(std::string, kAccessibilityAnnotatorOnePServiceUrl);
-
+BASE_DECLARE_FEATURE(kAccessibilityAnnotator);
 BASE_DECLARE_FEATURE_PARAM(base::TimeDelta, kAccessibilityAnnotationTTL);
+
+BASE_DECLARE_FEATURE(kAccessibilityAnnotatorFirstRun);
+
+BASE_DECLARE_FEATURE(kAccessibilityAnnotatorGetEntities);
+
+BASE_DECLARE_FEATURE(kAccessibilityAnnotatorLiveTabContext);
+
+BASE_DECLARE_FEATURE(kAccessibilityAnnotationReducerOnePResolver);
+BASE_DECLARE_FEATURE_PARAM(std::string, kAccessibilityAnnotatorOnePServiceUrl);
 
 }  // namespace accessibility_annotator
 
