c7810ea01774a1daecf66ab4aa5d270e334f70aa
Rainhard Findling <rainhard@chromium.org>
2026-03-25 02:02:47 -0700

    [a11y][ffr] Add settings linkout skeleton
    Add the skeleton of the A11yAnnotator cr-link-row in Settings.
    Actual visibility, click-target, strings, and metrics will be added
    later.
    
    Bug: b:493906459
    Change-Id: I9f45e5a50f55a0bb6c532d2eb9ecc2e12b8c9692
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7687214
    Reviewed-by: Ireneusz Szulc <ireneuszszulc@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Nicola Tommasi <tommasin@chromium.org>
    Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1604664}

--- chrome/browser/resources/settings/your_saved_info_page/collapsible_autofill_settings_card.ts
+++ chrome/browser/resources/settings/your_saved_info_page/collapsible_autofill_settings_card.ts
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://resources/cr_elements/cr_collapse/cr_collapse.js';
 import 'chrome://resources/cr_elements/cr_expand_button/cr_expand_button.js';
 import 'chrome://resources/cr_elements/cr_icon/cr_icon.js';
+import 'chrome://resources/cr_elements/cr_link_row/cr_link_row.js';
 import 'chrome://resources/cr_elements/cr_shared_style.css.js';
 import 'chrome://resources/cr_elements/icons.html.js';
 import '/shared/settings/controls/extension_controlled_indicator.js';
@@ -145,6 +146,14 @@ export class CollapsibleCardElement extends SettingsViewMixin
               'enableYourSavedInfoPolicyAndExtentionToggleIndicators');
         },
       },
+
+      showAccessibilityAnnotatorSettingsLink_: {
+        type: Boolean,
+        value() {
+          return loadTimeData.getBoolean(
+              'showAccessibilityAnnotatorSettingsLink');
+        },
+      },
     };
   }
 
@@ -167,6 +176,7 @@ export class CollapsibleCardElement extends SettingsViewMixin
   declare private autofillAiAvailableByDefault_: boolean;
   declare private enableYourSavedInfoPolicyAndExtentionToggleIndicators_:
       boolean;
+  declare private showAccessibilityAnnotatorSettingsLink_: boolean;
 
   private entityInstancesChangedListener_: EntityInstancesChangedListener|null =
       null;
@@ -225,6 +235,11 @@ export class CollapsibleCardElement extends SettingsViewMixin
       return;
     }
     this.entityDataManager_.toggleAutofillAiReauthRequirement();
+  }
+
+  private onAccessibilityAnnotatorSettingsLinkClick_() {
+    // TODO(b/493907185): Implement on-click action.
+    // TODO(b/494136945): Implement on-click metrics.
   }
 
   /**
