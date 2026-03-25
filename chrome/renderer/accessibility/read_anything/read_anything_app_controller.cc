190b1dcb433704e6f8c51cb50c9cdea694ebca82
Kristi Saney <kristislee@google.com>
2026-03-25 16:34:02 -0700

    [Reading mode] Don't listen for url changes with Readability enabled
    Bug: 496270401
    Change-Id: Ieb1465133f4e9537e87a4498f92200aedaee21fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7701877
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Auto-Submit: Kristi Saney <kristislee@google.com>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1605172}

--- chrome/renderer/accessibility/read_anything/read_anything_app_controller.cc
+++ chrome/renderer/accessibility/read_anything/read_anything_app_controller.cc
@@ -537,6 +537,7 @@ void ReadAnythingAppController::OnStringAttributeChanged(
     const std::string& new_value) {
   // Return early when the images flag is disabled to avoid potential crashes.
   if (!features::IsReadAnythingImagesViaAlgorithmEnabled() ||
+      features::IsReadAnythingWithReadabilityEnabled() ||
       attr != ax::mojom::StringAttribute::kUrl) {
     return;
   }
