d6dfff88af05e57bcd50b96677865edad9f5a446
Evan Luo <evanluo@google.com>
2026-03-25 13:24:45 -0700

    [finds] Add notification triggering test button on internals page
    Trigger a mock notification to test finds notification client logic.
    Adds a new button onto the chrome-finds-internal page.
    
    Add a new feature param that allows for bypassing theme and notification
    scheduling cooldowns.
    
    Screen recording:
    http://screencast/cast/NDcyOTYxMzY0ODM5NjI4OHw5MzMyZTgyOC04YQ
    
    Bug: 493645592
    Change-Id: Ib6f7a5dfbbdc64d2c3a8ee9047edc9f1f2f8e0cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7681451
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Commit-Queue: Evan Luo <evanluo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1605068}

--- chrome/android/java/src/org/chromium/chrome/browser/notifications/scheduler/DisplayAgent.java
+++ chrome/android/java/src/org/chromium/chrome/browser/notifications/scheduler/DisplayAgent.java
@@ -249,6 +249,8 @@ public class DisplayAgent {
         switch (type) {
             case SchedulerClientType.TIPS:
                 return ChannelId.TIPS;
+            case SchedulerClientType.CHROME_FINDS:
+                return ChannelId.CHROME_FINDS;
             default:
                 return ChannelId.BROWSER;
         }
