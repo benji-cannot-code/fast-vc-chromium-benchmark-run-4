23ea7c55137d041164af7ba5dd1d7044aea230d6
Michael Tang <tangm@microsoft.com>
2026-03-25 13:00:39 -0700

    Fix DC handle leak in screen_win.cc
    In crrev.com/c/7219217, we add a call to GetDC without a corresponding
    call to ReleaseDC. This CL uses ScopedGetDC to ensure we clean up all
    outstanding DCs.
    
    Bug: 476663753
    Change-Id: Id86c0d9b2256629d2a1e5f3a9649012a6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7701137
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1605046}

--- ui/display/win/screen_win.cc
+++ ui/display/win/screen_win.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/no_destructor.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/trace_event/trace_event.h"
+#include "base/win/scoped_hdc.h"
 #include "base/win/win_util.h"
 #include "base/win/windows_version.h"
 #include "components/device_event_log/device_event_log.h"
@@ -483,8 +484,9 @@ class MonitorDeviceCapsProvider {
 
  private:
   void CollectMonitorDeviceCaps() {
+    base::win::ScopedGetDC hdc_desktop(HWND_DESKTOP);
     ::EnumDisplayMonitors(
-        ::GetDC(HWND_DESKTOP), /*lprcClip=*/nullptr,
+        hdc_desktop, /*lprcClip=*/nullptr,
         [](HMONITOR hMonitor, HDC hdc, LPRECT lpRect, LPARAM lParam) -> BOOL {
           auto* monitors =
               reinterpret_cast<base::flat_map<HMONITOR, DeviceCaps>*>(lParam);
