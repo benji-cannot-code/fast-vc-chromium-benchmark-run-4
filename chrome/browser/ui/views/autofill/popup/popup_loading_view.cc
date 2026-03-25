a3ddf8ed84619ae876334fa7a7188aed54930d92
Wilson Low <wilsonlow@google.com>
2026-03-25 11:40:46 -0700

    [BNPL][PNPL] Make progress spinner larger to match mocks
    Before: https://screenshot.googleplex.com/3gcUQfFZZ2M56ko
    After: https://screenshot.googleplex.com/BhpQnc6DUdTvj9L
    Mock: http://shortn/_ILirtK4iqn
    
    Bug: 477689220
    Change-Id: I328e0d544f276cf46feccab71667461635d4487d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7698824
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Wilson Low <wilsonlow@google.com>
    Cr-Commit-Position: refs/heads/main@{#1604986}

--- chrome/browser/ui/views/autofill/popup/popup_loading_view.cc
+++ chrome/browser/ui/views/autofill/popup/popup_loading_view.cc
@@ -21,23 +21,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 
+namespace {
+
+constexpr int kThrobberSize = 24;
+
+}  // namespace
+
 PopupLoadingView::PopupLoadingView(int expected_number_of_suggestions) {
   SetUseDefaultFillLayout(true);
 
   const gfx::Size expected_size =
       CalculateSizeOfSuggestions(expected_number_of_suggestions);
 
-  AddChildView(
+  views::BoxLayoutView* container_view = AddChildView(
       views::Builder<views::BoxLayoutView>()
           .SetOrientation(views::BoxLayout::Orientation::kVertical)
           .SetMainAxisAlignment(views::BoxLayout::MainAxisAlignment::kCenter)
           .SetCrossAxisAlignment(views::BoxLayout::CrossAxisAlignment::kCenter)
           .SetPreferredSize(expected_size)
-          .AddChild(views::Builder<views::Throbber>().CustomConfigure(
-              base::BindOnce([](views::Throbber* throbber) {
-                throbber->Start();
-              })))
           .Build());
+
+  views::Throbber* throbber = container_view->AddChildView(
+      std::make_unique<views::Throbber>(kThrobberSize));
+  throbber->Start();
 
   GetViewAccessibility().SetRole(ax::mojom::Role::kProgressIndicator);
 }
