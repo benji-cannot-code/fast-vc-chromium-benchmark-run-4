14a8669db8a3b1d2021118767d4a1aee82c8fc10
Ewann Pelle <ewannpv@chromium.org>
2026-03-25 08:42:38 -0700

    [iOS] Fix tab strip positioning regression on iPad
    `setTopToolbarInset:` updates `_topToolbarInset` but doesn't update the
    `primaryToolbarOffsetConstraint`. This caused the tab strip to be
    incorrectly positioned when dragging the window close to the status bar
    in windowed mode.
    
    Demos: https://drive.google.com/drive/folders/1AfPf_DbKyTvU7QleWhWUJ4ta7YqzNTL3?resourcekey=0-KaBWactT5MAEGAokNH7cHg&usp=sharing
    
    Bug: 495688787
    Change-Id: I90b1e5296cbd275d6c03498d8c1be116ce53f0a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7698218
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1604854}

--- ios/chrome/browser/browser_view/ui_bundled/browser_view_controller.mm
+++ ios/chrome/browser/browser_view/ui_bundled/browser_view_controller.mm
@@ -593,6 +593,7 @@ const CGFloat kTopDynamicIslandInset = 24;
     return;
   }
   _topToolbarInset = inset;
+  self.primaryToolbarOffsetConstraint.constant = inset;
   [self updateToolbarState];
   [self.view setNeedsLayout];
 }
