96f072e04784f08c202e3bae97cd8ec1b064f8da
Patrick Monette <pmonette@chromium.org>
2026-03-25 08:38:02 -0700

    Rename RendererMemoryCoordinatorPolicy to LastResortGCPolicy
    In addition to the renaming, this CL:
    - Conditionally instantiates LastResortGCPolicy in RendererMain only
      when the kMemoryCoordinatorLastResortGC feature is enabled and the
      process is not in single-process mode.
    - Updates LastResortGCPolicy::Get() to return a pointer instead of a
      reference, allowing it to return nullptr when the policy is not
      active.
    - Simplifies the related unit tests and removes the now-redundant
      Disabled test case.
    
    Bypass-Check-License: File rename.
    Bug: 447352454
    Change-Id: I069f609f99596a3c5e7eef462bf483c8d8254918
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7696455
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1604848}

--- content/renderer/renderer_blink_platform_impl.cc
+++ content/renderer/renderer_blink_platform_impl.cc
@@ -55,7 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/media/inspector_media_event_handler.h"
 #include "content/renderer/media/render_media_event_handler.h"
 #include "content/renderer/media/renderer_webaudiodevice_impl.h"
-#include "content/renderer/memory_coordinator/renderer_memory_coordinator_policy.h"
+#include "content/renderer/memory_coordinator/last_resort_gc_policy.h"
 #include "content/renderer/render_frame_impl.h"
 #include "content/renderer/render_thread_impl.h"
 #include "content/renderer/renderer_navigation_metrics_manager.h"
@@ -1180,12 +1180,9 @@ bool RendererBlinkPlatformImpl::IsUserLevelMemoryPressureSignalEnabled() {
 #endif  // BUILDFLAG(IS_ANDROID)
 
 void RendererBlinkPlatformImpl::OnV8HeapLastResortGC() {
-  // In --single-process mode, the RendererMemoryCoordinatorPolicy does not run.
-  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kSingleProcess)) {
-    return;
+  if (auto* policy = LastResortGCPolicy::Get()) {
+    policy->OnV8HeapLastResortGC();
   }
-  RendererMemoryCoordinatorPolicy::Get().OnV8HeapLastResortGC();
 }
 
 }  // namespace content
--- content/renderer/renderer_main.cc
+++ content/renderer/renderer_main.cc
@@ -43,7 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/main_function_params.h"
 #include "content/public/renderer/content_renderer_client.h"
 #include "content/public/renderer/render_thread.h"
-#include "content/renderer/memory_coordinator/renderer_memory_coordinator_policy.h"
+#include "content/renderer/memory_coordinator/last_resort_gc_policy.h"
 #include "content/renderer/render_process_impl.h"
 #include "content/renderer/render_thread_impl.h"
 #include "content/renderer/renderer_main_platform_delegate.h"
@@ -224,8 +224,10 @@ int RendererMain(MainFunctionParams parameters) {
   // is OK.
   InitializeWebRtcModuleBeforeSandbox();
 
-  RendererMemoryCoordinatorPolicy render_memory_coordinator_policy(
-      ChildMemoryCoordinator::Get());
+  std::optional<LastResortGCPolicy> last_resort_gc_policy;
+  if (base::FeatureList::IsEnabled(kMemoryCoordinatorLastResortGC)) {
+    last_resort_gc_policy.emplace(ChildMemoryCoordinator::Get());
+  }
 
   {
     content::ContentRendererClient* client = GetContentClient()->renderer();
