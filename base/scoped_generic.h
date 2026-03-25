819ac2ece352de187c5eb58679291a0deaad078e
gate kibr <gkvjwa@gmail.com>
2026-03-25 10:00:08 -0700

    Fix ScopedGeneric swap by dropping const and adding member function
    The template used constant references, and the ScopedGeneric class was
    missing a swap member function. this commit fixes both issues, allowing
    swapping
    
    Change-Id: I26aa7821e549f66f914e4d8e9a1a2510e1335dd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7686088
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1604909}

--- base/scoped_generic.h
+++ base/scoped_generic.h
@@ -135,6 +135,13 @@ class ScopedGeneric {
     return *this;
   }
 
+  void swap(ScopedGeneric& other) {
+    CHECK(!receiving_);
+    CHECK(!other.receiving_);
+    using std::swap;
+    swap(data_.generic, other.data_.generic);
+  }
+
   // Frees the currently owned object, if any. Then takes ownership of a new
   // object, if given. Self-resets are not allowed as on unique_ptr. See
   // http://crbug.com/162971
@@ -297,8 +304,8 @@ class ScopedGeneric {
 };
 
 template <class T, class Traits>
-void swap(const ScopedGeneric<T, Traits>& a,
-          const ScopedGeneric<T, Traits>& b) {
+void swap(ScopedGeneric<T, Traits>& a,
+          ScopedGeneric<T, Traits>& b) {
   a.swap(b);
 }
 
