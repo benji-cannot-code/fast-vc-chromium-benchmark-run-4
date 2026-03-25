589037563f3e53e9ba5fdd5483b22a7210fd9d29
Ian Wells <iwells@chromium.org>
2026-03-25 12:32:27 -0700

    GlicEntrypointVariations cleanup 1/4: nudge highlight
    Remove the "highlight" view that was drawn behind the icon and label in
    the nudge state.
    
    Margins that were applied to the highlight parent view now need to be
    set on the icon (left) and the label (right).
    
    Screenshots to show the button's appearance hasn't changed:
    before https://screenshot.googleplex.com/8SK8qFb4zqbt9Af
    after https://screenshot.googleplex.com/4jHW7S9oibV36CG
    
    Bug: 469850069
    Change-Id: I4e1f4e7af75a1c7524bde2e670deb04d3c34de99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7695598
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1605029}

--- chrome/browser/ui/views/tabs/glic/tab_strip_glic_button.h
+++ chrome/browser/ui/views/tabs/glic/tab_strip_glic_button.h
@@ -151,8 +151,6 @@ class TabStripGlicButton : public TabStripNudgeButton,
 
   void UpdateTextAndBackgroundColors();
   void UpdateIcon();
-  bool IsHighlightVisible() const;
-  void CreateIconAndLabelContainer();
   void SetCloseButtonVisible(bool visible);
 
   void ShowNudge();
@@ -160,7 +158,6 @@ class TabStripGlicButton : public TabStripNudgeButton,
   void ApplyTextAndFadeIn(std::optional<std::u16string> text,
                           base::TimeDelta delay,
                           base::TimeDelta duration);
-  void MaybeFadeHighlightOnHover(float final_opacity);
   int CalculateExpandedWidth();
 
   bool IsAnimatingTextVisibility() const;
@@ -173,7 +170,6 @@ class TabStripGlicButton : public TabStripNudgeButton,
 
   void SetLabelMargins();
 
-  views::View* highlight_view() { return highlight_view_; }
   WidthState width_state() { return width_state_; }
 
   void OnLabelVisibilityChanged();
@@ -205,12 +201,6 @@ class TabStripGlicButton : public TabStripNudgeButton,
   // Start and end values for width animations.
   int start_width_ = 0;
   int end_width_ = 0;
-
-  // View to be drawn behind the icon and label with a background color.
-  raw_ptr<View> highlight_view_ = nullptr;
-
-  // Container view for the icon and label, and the highlight drawn behind them.
-  raw_ptr<View> icon_label_highlight_view_ = nullptr;
 
   // Holds the incoming nudge text until the point in the animation when it can
   // be applied.
