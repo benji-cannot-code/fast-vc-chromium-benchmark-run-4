5f9f5e095ca2c598612dac6540a5cbe4dcfdae61
Tim Judkins <tjudkins@chromium.org>
2026-03-25 13:26:23 -0700

    [Extensions] Convert accessibilityServicePrivate to WebIDL
    This conversion was largely done using a detailed description of the
    conversion process passed to Gemini CLI. To double check this work, the
    file has also been copied into the converted schemas test to verify no
    functional difference in output.
    
    Fixed: 493989968
    Change-Id: I6196eeb223bc5660322ed7e77d0e16667a04d495
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7683130
    Reviewed-by: Andrea Orru <andreaorru@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1605071}

--- tools/typescript/definitions/accessibility_service_private.d.ts
+++ tools/typescript/definitions/accessibility_service_private.d.ts
@@ -1,14 +1,14 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2023 The Chromium Authors
+// Copyright 2026 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 /**
  * @fileoverview Definitions for chrome.accessibilityServicePrivate API
  * Generated from:
- * chrome/common/extensions/api/accessibility_service_private.idl run
+ * chrome/common/extensions/api/accessibility_service_private.webidl run
  * `tools/json_schema_compiler/compiler.py
- * chrome/common/extensions/api/accessibility_service_private.idl -g
+ * chrome/common/extensions/api/accessibility_service_private.webidl -g
  * ts_definitions` to regenerate.
  */
 
