2a68d5de45c969d9a8ee024abf184d05563aabe5
shomizoe <shomizoe@google.com>
2026-03-25 16:23:52 -0700

    [codehealth] Clean up dead NtpSafeBrowsingModule feature flag
    The `kNtpSafeBrowsingModule` feature flag and all related WebUI module
    code in `chrome/browser/new_tab_page/modules/safe_browsing` are dead
    and unreferenced. This CL removes the module directory, its
    descriptions, preference registrations, and flag configuration to
    declutter the codebase.
    
    Bug: b:356236925
    Test: components_unittests, unit_tests
    Change-Id: I98533f91aa5a499110eb2cf9948c93866a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7688752
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Sho Mizoe <shomizoe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1605161}

--- components/search/ntp_features.cc
+++ components/search/ntp_features.cc
@@ -160,9 +160,6 @@ BASE_FEATURE(kNtpPhotosModuleSplitSvgOptInArtWork,
 // If enabled, Following Feed module will be shown.
 BASE_FEATURE(kNtpFeedModule, base::FEATURE_DISABLED_BY_DEFAULT);
 
-// If enabled, SafeBrowsing module will be shown to a target user.
-BASE_FEATURE(kNtpSafeBrowsingModule, base::FEATURE_DISABLED_BY_DEFAULT);
-
 // If enabled, sharepoint module will be shown.
 BASE_FEATURE(kNtpSharepointModule, base::FEATURE_ENABLED_BY_DEFAULT);
 
@@ -292,10 +289,6 @@ const char kNtpPhotosModuleDataParam[] = "NtpPhotosModuleDataParam";
 const char kNtpPhotosModuleOptInTitleParam[] = "NtpPhotosModuleOptInTitleParam";
 const char kNtpPhotosModuleOptInArtWorkParam[] =
     "NtpPhotosModuleOptInArtWorkParam";
-const char kNtpSafeBrowsingModuleCooldownPeriodDaysParam[] =
-    "NtpSafeBrowsingModuleCooldownPeriodDaysParam";
-const char kNtpSafeBrowsingModuleCountMaxParam[] =
-    "NtpSafeBrowsingModuleCountMaxParam";
 const char kNtpMostRelevantTabResumptionModuleDataParam[] =
     "NtpMostRelevantTabResumptionModuleDataParam";
 const char kNtpMostRelevantTabResumptionModuleMaxVisitsParam[] =
