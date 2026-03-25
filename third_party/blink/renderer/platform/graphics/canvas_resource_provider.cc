08e53138c7a196d07fb9098a37dc2e3ee1e27895
Colin Blundell <blundell@chromium.org>
2026-03-25 00:57:06 -0700

    [Blink] Eliminate FlushAndSubmit() calls in canvas_resource_provider.cc
    This method is a no-op if the SkSurface doesn't have a GrContext [1].
    The SkSurfaces passed here are created via SkSurfaces::Raster [2][3].
    This makes an SkSurface_Raster [4], which returns null for
    recordingContext() via not overriding the base class's impl here [5].
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/skia/src/gpu/ganesh/surface/SkSurface_Ganesh.cpp;l=778?q=skgpu::ganesh::FlushAn&ss=chromium
    [2] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc;l=167;drc=71a49dfb2ffd044b3c538f66ae21234599e3e76e
    [3] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc;l=1178-1196;drc=71a49dfb2ffd044b3c538f66ae21234599e3e76e
    [4] https://source.chromium.org/chromium/chromium/src/+/main:third_party/skia/src/image/SkSurface_Raster.cpp;l=227;drc=71a49dfb2ffd044b3c538f66ae21234599e3e76e
    [5] https://source.chromium.org/chromium/chromium/src/+/main:third_party/skia/src/image/SkSurface_Base.cpp;l=47;drc=71a49dfb2ffd044b3c538f66ae21234599e3e76e;bpv=1;bpt=1
    
    Bug: 352263194
    Change-Id: I4826413e94bf1a157444620d97e8f767218abfc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7694138
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1604638}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
@@ -1139,7 +1139,6 @@ void CanvasNon2DResourceProviderSharedImage::RasterRecord(
   if (!is_accelerated_) {
     EnsureSkiaCanvas();
     skia_canvas_->drawPicture(std::move(last_recording));
-    skgpu::ganesh::FlushAndSubmit(GetSkSurface());
     return;
   }
 
@@ -1924,7 +1923,6 @@ void CanvasResourceProvider::UnacceleratedRasterRecordForCanvas2D(
 
   EnsureSkiaCanvas();
   skia_canvas_->drawPicture(std::move(last_recording));
-  skgpu::ganesh::FlushAndSubmit(GetSkSurface());
 }
 
 bool CanvasResourceProviderSharedImage::IsGpuContextLost() const {
