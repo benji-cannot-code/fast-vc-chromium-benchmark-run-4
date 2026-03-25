0c23b82043a55ea7046f0e825029cbaa27e69458
Kent Tamura <tkent@chromium.org>
2026-03-25 03:01:01 -0700

    Remove String::FromUTF8() usages in renderer/platform/
    Standardize on String::FromUtf8() for consistency.
    
    This CL also modifies the output of the blink::ResourceError
    stream printer. The TrustTokenOperationError field will now be printed
    as an integer instead of a string:
      Original: ..., TrustTokenOperationError = "0"
      After this CL: ..., TrustTokenOperationError = 0
    This difference is purely cosmetic and will not affect production
    behavior.
    
    Bug: 492927412
    Change-Id: I51b285bd76be86e245b01b87e2447718c0f9d129
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7698659
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1604695}

--- third_party/blink/renderer/platform/network/http_parsers.cc
+++ third_party/blink/renderer/platform/network/http_parsers.cc
@@ -117,11 +117,11 @@ blink::LoadingMode ConvertToBlink(LoadingMode in) {
 
 // ===== Converters for other basic Blink types =====
 ::blink::String ConvertToBlink(const std::string& in) {
-  return ::blink::String::FromUTF8(in);
+  return ::blink::String::FromUtf8(in);
 }
 
 ::blink::String ConvertToBlink(const std::optional<std::string>& in) {
-  return in ? ::blink::String::FromUTF8(*in) : ::blink::String();
+  return in ? ::blink::String::FromUtf8(*in) : ::blink::String();
 }
 
 ::blink::KURL ConvertToBlink(const GURL& in) {
@@ -963,8 +963,8 @@ bool ParseMultipartHeadersFromBody(base::span<const uint8_t> bytes,
 
   std::string mime_type, charset;
   response_headers->GetMimeTypeAndCharset(&mime_type, &charset);
-  response->SetMimeType(AtomicString(String::FromUTF8(mime_type)));
-  response->SetTextEncodingName(AtomicString(String::FromUTF8(charset)));
+  response->SetMimeType(AtomicString(String::FromUtf8(mime_type)));
+  response->SetTextEncodingName(AtomicString(String::FromUtf8(charset)));
 
   // Copy headers listed in replaceHeaders to the response.
   for (const AtomicString& header : ReplaceHeaders()) {
@@ -1015,7 +1015,7 @@ bool ParseMultipartFormHeadersFromBody(base::span<const uint8_t> bytes,
     std::string value;
     while (response_headers->EnumerateHeader(
         &iterator, header_name_string_piece, &value)) {
-      header_fields->Add(*header_name, AtomicString(String::FromUTF8(value)));
+      header_fields->Add(*header_name, AtomicString(String::FromUtf8(value)));
     }
   }
 
