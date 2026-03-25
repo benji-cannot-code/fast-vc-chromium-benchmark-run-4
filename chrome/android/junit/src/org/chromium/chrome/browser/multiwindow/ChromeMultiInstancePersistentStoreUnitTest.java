abf7766b547479b45ffa19752075226426975c26
Zhe Li <zheliooo@google.com>
2026-03-24 20:56:30 -0700

    [SharedPrefsMigration] Implement migrate and downgrade path
    BYPASS_LARGE_CHANGE_WARNING: Large CL due to migration logic
    
    Bug: 438834621
    Change-Id: I53387de988698163d82d8d561bd8a22137131c73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7683011
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Zhe Li <zheliooo@google.com>
    Commit-Queue: Zhe Li <zheliooo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1604588}

--- chrome/android/junit/src/org/chromium/chrome/browser/multiwindow/ChromeMultiInstancePersistentStoreUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/multiwindow/ChromeMultiInstancePersistentStoreUnitTest.java
@@ -12,6 +12,7 @@ import static org.junit.Assert.assertTrue;
 
 import static org.chromium.chrome.browser.multiwindow.MultiInstanceManager.INVALID_WINDOW_ID;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,7 +23,6 @@ import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.incognito.IncognitoUtils;
 import org.chromium.chrome.browser.tabmodel.SupportedProfileType;
-import org.junit.After;
 
 import java.util.Set;
 
