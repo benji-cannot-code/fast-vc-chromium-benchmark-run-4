323fe6148c247efc5035d444e3652966d1cfb987
Linyu He <linyuh@google.com>
2026-03-25 16:11:21 -0700

    Side Panel: Support tab/window-scoped SidePanelRegistry
    Context:
    On Windows/Mac/Linux, a SidePanelRegistry can be associated with a tab
    or a window, then it can be obtained via:
      SidePanelRegistry::From(TabInterface), or
      SidePanelRegistry::From(BrowserWindowInterface).
    
    To implement this on Android, we need:
    (1) A Java tab-scoped object to manage the lifecycle of a tab-scoped,
        native SidePanelRegistry;
    (2) A Java BrowserWindowInterface-scoped object to manage the lifecycle
        of a window-scoped, native SidePanelRegistry.
    
    For (1), this CL creates:
    * Java TabScopedSidePanelRegistryBridge, which is a TabObserver.
    * C++ TabScopedSidePanelRegistryBridge, which owns a native
      SidePanelRegistry.
    
    For (2), this CL creates:
    * Java WindowScopedSidePanelRegistryBridge, which is a
      ChromeAndroidTaskFeature.
    * C++ WindowScopedSidePanelRegistryBridge, which owns a native
      SidePanelRegistry.
    
    (1) is initialized when a new tab is added to the TabModel.
    (2) is initialized when the side panel UI is initialized for the browser
    window.
    
    I choose not to merge (1) and (2) into a single <Java, C++> pair to make
    what happens on Android clear.
    
    BYPASS_LARGE_CHANGE_WARNING: CL has a lot of boilerplate code
    
    Bug: 495941361
    Change-Id: Ic6db6112e7afc0d6084ab8cbffaddf8c06ff19c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7699534
    Commit-Queue: Linyu He <linyuh@google.com>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1605156}

---
+++ chrome/browser/ui/side_panel/internal/android/window_scoped_side_panel_registry_bridge.h
@@ -0,0 +1,46 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_SIDE_PANEL_INTERNAL_ANDROID_WINDOW_SCOPED_SIDE_PANEL_REGISTRY_BRIDGE_H_
+#define CHROME_BROWSER_UI_SIDE_PANEL_INTERNAL_ANDROID_WINDOW_SCOPED_SIDE_PANEL_REGISTRY_BRIDGE_H_
+
+#include <jni.h>
+
+#include <memory>
+
+#include "base/android/jni_weak_ref.h"
+#include "base/android/scoped_java_ref.h"
+#include "chrome/browser/ui/side_panel/side_panel_registry.h"
+
+class BrowserWindowInterface;
+
+// JNI bridge that owns a window-scoped `SidePanelRegistry` and is owned by the
+// Java `WindowScopedSidePanelRegistryBridge`.
+class WindowScopedSidePanelRegistryBridge final {
+ public:
+  WindowScopedSidePanelRegistryBridge(
+      JNIEnv* env,
+      const base::android::JavaRef<jobject>& java_bridge,
+      BrowserWindowInterface* browser);
+  WindowScopedSidePanelRegistryBridge(
+      const WindowScopedSidePanelRegistryBridge&) = delete;
+  WindowScopedSidePanelRegistryBridge& operator=(
+      const WindowScopedSidePanelRegistryBridge&) = delete;
+
+  ~WindowScopedSidePanelRegistryBridge();
+
+  // Implements Java `WindowScopedSidePanelRegistryBridgeImpl.Natives#destroy`.
+  void Destroy(JNIEnv* env);
+
+  SidePanelRegistry* GetSidePanelRegistryForTesting() const;
+
+ private:
+  base::android::ScopedJavaLocalRef<jobject> java_bridge() const;
+
+  JavaObjectWeakGlobalRef java_bridge_;
+  std::unique_ptr<SidePanelRegistry> side_panel_registry_;
+};
+
+#endif  // CHROME_BROWSER_UI_SIDE_PANEL_INTERNAL_ANDROID_WINDOW_SCOPED_SIDE_PANEL_REGISTRY_BRIDGE_H_
