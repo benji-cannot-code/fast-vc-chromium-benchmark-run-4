a20b1d9d90d0852d23ff6f8d98781c164656eb3e
Ibrahim Kanouche <kanouche@google.com>
2026-03-25 09:52:51 -0700

    [PAM] Always show features if Auth Flow flag is enabled.
    Bug: 492462071
    Change-Id: I4a375542f6649c62d128d30abcf560fd8db30fa3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7701112
    Auto-Submit: Ibrahim Kanouche <kanouche@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1604902}

--- ios/chrome/browser/intelligence/page_action_menu/coordinator/page_action_menu_mediator.mm
+++ ios/chrome/browser/intelligence/page_action_menu/coordinator/page_action_menu_mediator.mm
@@ -134,6 +134,9 @@ const CGFloat kFeatureRowIconSize = 20;
 #pragma mark - PageActionMenuMutator
 
 - (BOOL)shouldShowFeatureEntryPoints {
+  if (IsPageActionMenuAuthFlowEnabled()) {
+    return YES;
+  }
   if (!_authenticationService) {
     return NO;
   }
