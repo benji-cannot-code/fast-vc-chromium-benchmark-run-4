8d60a42bf87bf3dd15933f3c4ae3ca1d96db3c83
mikt <mikt@google.com>
2026-03-24 21:08:00 -0700

    Add AMSC macro to FilePathWatcherFSEvents
    As a band aid fix for potential memory issue.
    
    Bug: 495782021
    Change-Id: I2e53ef61ba9a0cbcff71b22148f800b3c59e0bb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7699084
    Reviewed-by: Mingyu Lei <leimy@chromium.org>
    Commit-Queue: Mikihito Matsuura <mikt@google.com>
    Cr-Commit-Position: refs/heads/main@{#1604590}

--- content/browser/file_system_access/file_path_watcher/file_path_watcher_fsevents.h
+++ content/browser/file_system_access/file_path_watcher/file_path_watcher_fsevents.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/apple/scoped_dispatch_object.h"
 #include "base/files/file_path.h"
+#include "base/memory/safety_checks.h"
 #include "base/memory/weak_ptr.h"
 #include "content/browser/file_system_access/file_path_watcher/file_path_watcher.h"
 #include "content/browser/file_system_access/file_path_watcher/file_path_watcher_fsevents_change_tracker.h"
@@ -29,6 +30,9 @@ namespace content {
 // directory. See file_path_watcher_mac.cc for the code that decides when to
 // use which one.
 class FilePathWatcherFSEvents : public FilePathWatcher::PlatformDelegate {
+  // TODO(https://crbug.com/495782021): Remove this macro.
+  ADVANCED_MEMORY_SAFETY_CHECKS();
+
  public:
   using ChangeEvent = FilePathWatcherFSEventsChangeTracker::ChangeEvent;
 
