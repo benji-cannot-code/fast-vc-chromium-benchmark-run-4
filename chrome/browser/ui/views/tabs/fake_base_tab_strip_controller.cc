116f780970d9097b51ed37c337bdea10e85df770
Michael Stapelberg <stapelberg@google.com>
2026-03-25 13:51:23 -0700

    Restore middle click new tab button functionality (Linux)
    Many users (myself included) dearly miss this functionality,
    see http://crbug.com/457495649 for a continuous stream of
    complaints over many weeks.
    
    Revert "Remove unused CreateNewTabWithLocation"
    
    This reverts commit 0d6273a69a47ab59c990c667bfc0ac8f7485f9b4.
    
    Bug: 457495649
    
    
    Revert "Remove middle click new tab button functionality"
    
    This reverts commit 3b48235a4d3d853824288d3cc1310916ddc334a6.
    
    Bug: 457495649
    Change-Id: I587ab67e961936382e4cbe7e8304ef94a565fd0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7368605
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Michael Stapelberg <stapelberg@google.com>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1605090}

--- chrome/browser/ui/views/tabs/fake_base_tab_strip_controller.cc
+++ chrome/browser/ui/views/tabs/fake_base_tab_strip_controller.cc
@@ -203,6 +203,9 @@ void FakeBaseTabStripController::CreateNewTab(NewTabTypes context) {
   AddTab(num_tabs_, TabActive::kActive);
 }
 
+void FakeBaseTabStripController::CreateNewTabWithLocation(
+    const std::u16string& location) {}
+
 void FakeBaseTabStripController::OnStartedDragging() {}
 
 void FakeBaseTabStripController::OnStoppedDragging() {}
--- chrome/browser/ui/views/tabs/tab_strip.cc
+++ chrome/browser/ui/views/tabs/tab_strip.cc
@@ -87,6 +87,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/tab_groups/tab_group_visual_data.h"
 #include "components/tabs/public/tab_alert.h"
 #include "content/public/browser/web_contents.h"
+#include "ui/base/clipboard/clipboard.h"
+#include "ui/base/clipboard/clipboard_constants.h"
 #include "ui/base/dragdrop/drag_drop_types.h"
 #include "ui/base/dragdrop/mojom/drag_drop_types.mojom.h"
 #include "ui/base/interaction/element_identifier.h"
@@ -1186,6 +1188,22 @@ void TabStrip::NewTabButtonPressed(const ui::Event& event) {
     // normal fade-in.
     if (hover_card_controller_) {
       hover_card_controller_->PreventImmediateReshow();
+    }
+
+    const ui::MouseEvent& mouse = static_cast<const ui::MouseEvent&>(event);
+    if (mouse.IsOnlyMiddleMouseButton()) {
+      if (ui::Clipboard::IsSupportedClipboardBuffer(
+              ui::ClipboardBuffer::kSelection)) {
+        ui::Clipboard* clipboard = ui::Clipboard::GetForCurrentThread();
+        CHECK(clipboard)
+            << "Clipboard instance is not available, cannot proceed with "
+               "middle mouse button action.";
+        clipboard->ReadText(ui::ClipboardBuffer::kSelection,
+                            /* data_dst = */ std::nullopt,
+                            base::BindOnce(&TabStrip::OnMiddleClickReadText,
+                                           weak_ptr_factory_.GetWeakPtr()));
+      }
+      return;
     }
   }
   controller_->CreateNewTab(NewTabTypes::kNewTabButton);
@@ -2456,6 +2474,14 @@ void TabStrip::OnTouchUiChanged() {
 
   tab_container_->CompleteAnimationAndLayout();
   PreferredSizeChanged();
+}
+
+void TabStrip::OnMiddleClickReadText(std::u16string text) {
+  if (!text.empty()) {
+    base::RecordAction(
+        base::UserMetricsAction("NewTabButton_PasteAndNavigate"));
+    controller_->CreateNewTabWithLocation(text);
+  }
 }
 
 void TabStrip::AnnounceTabAddedToGroup(tab_groups::TabGroupId group_id) {
