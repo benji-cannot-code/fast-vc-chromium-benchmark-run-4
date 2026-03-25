c75bd09e8ae32229011aa29b40f61247ef98cf95
Mitsuru Oshima <oshima@chromium.org>
2026-03-25 09:20:59 -0700

    Explicitly allow reentrancy for observer list of PrintJob::Observer
    (identified by gemini-cli)
    
    R=thestig@chromium.org
    
    Bug: 40562847, 484371187
    Change-Id: I4a2d4a877bfea41b0f1ee7ad7318975431840ad9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7692394
    Auto-Submit: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1604881}

--- chrome/browser/printing/print_job.h
+++ chrome/browser/printing/print_job.h
@@ -167,7 +167,11 @@ class PrintJob : public base::RefCountedThreadSafe<PrintJob> {
   // Returns the ID of the source.
   const std::string& source_id() const;
 
-  const base::ObserverList<Observer>& GetObserversForTesting() {
+  const base::ObserverList<
+      Observer,
+      /*check_empty=*/false,
+      base::ObserverListReentrancyPolicy::kAllowReentrancyUntriaged>&
+  GetObserversForTesting() {
     return observers_;
   }
 #endif  // BUILDFLAG(IS_CHROMEOS)
@@ -253,7 +257,12 @@ class PrintJob : public base::RefCountedThreadSafe<PrintJob> {
       uint32_t total_page_count);
 #endif  // BUILDFLAG(IS_WIN)
 
-  base::ObserverList<Observer> observers_;
+  // TODO(crbug.com/484371187): Investigate if reentrancy can be removed.
+  base::ObserverList<
+      Observer,
+      /*check_empty=*/false,
+      base::ObserverListReentrancyPolicy::kAllowReentrancyUntriaged>
+      observers_;
 
   // All the UI is done in a worker thread because many Win32 print functions
   // are blocking and enters a message loop without your consent. There is one
