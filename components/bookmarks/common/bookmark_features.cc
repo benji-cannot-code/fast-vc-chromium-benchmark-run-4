649ad72b544404b3f63e331ca60c4bd997a87b7f
Pierre-Laurent Coirier <plcoirier@google.com>
2026-03-25 10:37:24 -0700

    [Encrypt Bookmarks] Add browser test to verify encryption stages.
    Add local and syncable bookmarks and account bookmarks. Move between
    stages and verify those bookmarks still exist and the expected clear
    text / encrypted files are present. Verify when user goes through all
    stages but also when some stages are skipped.
    
    Bug: 435317726
    Change-Id: I6d85cf5f227834f58bb00c53fb1da4de3ade7d69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7689570
    Reviewed-by: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Pierre-Laurent Coirier <plcoirier@google.com>
    Cr-Commit-Position: refs/heads/main@{#1604939}

--- components/bookmarks/common/bookmark_features.cc
+++ components/bookmarks/common/bookmark_features.cc
@@ -124,4 +124,8 @@ std::string GetBookmarkEncryptionStageNameForTesting(  // IN-TEST
   NOTREACHED();
 }
 
+BookmarkEncryptionStage GetCurrentBookmarkEncryptionStageForTesting() {
+  return GetBookmarkEncryptionStage();
+}
+
 }  // namespace bookmarks
