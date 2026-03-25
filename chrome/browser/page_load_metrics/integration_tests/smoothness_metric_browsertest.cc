50baffe3d7212f31dcc0367326d2bd1d6328ac5d
Jonathan Ross <jonross@chromium.org>
2026-03-25 11:25:00 -0700

    Remove unused Graphics UKMs
    Found some old Graphics UKMs that aren't emitted anymore. Removing the
    definitions.
    
    Bug: 473685584
    Change-Id: Ieb98f5f4fab539cc423293c417b174003a60e482
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7676557
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1604975}

--- chrome/browser/page_load_metrics/integration_tests/smoothness_metric_browsertest.cc
+++ chrome/browser/page_load_metrics/integration_tests/smoothness_metric_browsertest.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/metrics/public/cpp/ukm_builders.h"
 
 using ukm::builders::Graphics_Smoothness_FrameSequence;
-using ukm::builders::Graphics_Smoothness_NormalizedPercentDroppedFrames;
 
 namespace {
 
