cdc354918a0bc319a188c09b0d90a4586c71d992
Alexis Hétu <sugoi@chromium.org>
2026-03-25 11:33:13 -0700

    [iOS] Adjust AutofillProfileTableViewControllerTest for new features
    This CL preemptively adjusts AutofillProfileTableViewControllerTest
    tests for new enhanced autofill features which will soon be enabled.
    
    Bug: 480934103
    Change-Id: I98415cc2ec79cb05f6ba3bd314381d89815f198c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7697135
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1604982}

--- ios/chrome/browser/settings/ui_bundled/autofill/autofill_profile_table_view_controller_unittest.mm
+++ ios/chrome/browser/settings/ui_bundled/autofill/autofill_profile_table_view_controller_unittest.mm
@@ -71,6 +71,15 @@ class AutofillProfileTableViewControllerTest
         initWithBrowser:browser_.get()];
   }
 
+  void SetUp() override {
+    LegacyChromeTableViewControllerTest::SetUp();
+    feature_list_.InitWithFeatures(
+        {autofill::features::kAutofillAiCreateEntityDataManager,
+         autofill::features::kAutofillAiWithDataSchema,
+         autofill::features::kAutofillAiReauthRequired},
+        /*disabled_features=*/{});
+  }
+
   void TearDown() override {
     [base::apple::ObjCCastStrict<AutofillProfileTableViewController>(
         controller()) settingsWillBeDismissed];
@@ -115,6 +124,7 @@ class AutofillProfileTableViewControllerTest
     return GetApplicationContext()->GetLocalState();
   }
 
+  base::test::ScopedFeatureList feature_list_;
   web::WebTaskEnvironment task_environment_;
   IOSChromeScopedTestingLocalState scoped_testing_local_state_;
   std::unique_ptr<TestProfileIOS> profile_;
@@ -128,12 +138,20 @@ TEST_F(AutofillProfileTableViewControllerTest, TestInitialization) {
   CheckController();
 
   // Expect only the header section.
-  EXPECT_EQ(1, NumberOfSections());
+  EXPECT_EQ(3, NumberOfSections());
   // Expect header section to contain one row (the address Autofill toggle).
   EXPECT_EQ(1, NumberOfItemsInSection(0));
   // Expect subtitle section to contain one row (the address Autofill toggle
   // subtitle).
   EXPECT_NE(nil, [controller.tableViewModel footerForSectionIndex:0]);
+  // Expect header section to contain one row (the enhanced Autofill toggle).
+  EXPECT_EQ(1, NumberOfItemsInSection(1));
+  EXPECT_EQ(nil, [controller.tableViewModel footerForSectionIndex:1]);
+  // Expect header section to contain one row (the user verification toggle).
+  EXPECT_EQ(1, NumberOfItemsInSection(2));
+  // Expect subtitle section to contain one row (the use verification toggle
+  // subtitle).
+  EXPECT_NE(nil, [controller.tableViewModel footerForSectionIndex:2]);
 
   // Check the footer of the first section.
   CheckSectionFooterWithId(IDS_AUTOFILL_ENABLE_PROFILES_TOGGLE_SUBLABEL, 0);
@@ -146,10 +164,11 @@ TEST_F(AutofillProfileTableViewControllerTest, TestOneProfile) {
   CreateController();
   CheckController();
 
-  // Expect two sections (header, and addresses section).
-  EXPECT_EQ(2, NumberOfSections());
+  // Expect four sections (address toggle, enhanced autofill, user verification,
+  // addresses).
+  EXPECT_EQ(4, NumberOfSections());
   // Expect address section to contain one row (the address itself).
-  EXPECT_EQ(1, NumberOfItemsInSection(1));
+  EXPECT_EQ(1, NumberOfItemsInSection(3));
 }
 
 // Checks if there is a plus address section when
@@ -165,9 +184,10 @@ TEST_F(AutofillProfileTableViewControllerTest, TestPlusAddressSection) {
       LegacyChromeTableViewControllerTest::controller();
   CheckController();
 
-  // Expect only the header section.
-  EXPECT_EQ(2, NumberOfSections());
-  // Expect header section to contain one row.
+  // Expect 4 sections (address toggle, plus address, enhanced autofill, user
+  // verification).
+  EXPECT_EQ(4, NumberOfSections());
+  // Expect plus address section to contain one row.
   EXPECT_EQ(1, NumberOfItemsInSection(1));
   // Expect subtitle section to contain one row.
   EXPECT_NE(nil, [controller.tableViewModel footerForSectionIndex:1]);
@@ -189,7 +209,7 @@ TEST_F(AutofillProfileTableViewControllerTest,
   CheckController();
 
   AutofillProfileItem* item = base::apple::ObjCCastStrict<AutofillProfileItem>(
-      GetTableViewItem(/*section=*/1, /*item=*/0));
+      GetTableViewItem(/*section=*/3, /*item=*/0));
 
   EXPECT_NSEQ(@"Montreal", item.detailText);
 }
@@ -208,7 +228,7 @@ TEST_F(AutofillProfileTableViewControllerTest,
   CheckController();
 
   AutofillProfileItem* item = base::apple::ObjCCastStrict<AutofillProfileItem>(
-      GetTableViewItem(/*section=*/1, /*item=*/0));
+      GetTableViewItem(/*section=*/3, /*item=*/0));
 
   EXPECT_NSEQ(@"Montreal", item.detailText);
 }
@@ -225,7 +245,7 @@ TEST_F(AutofillProfileTableViewControllerTest,
   CheckController();
 
   AutofillProfileItem* item = base::apple::ObjCCastStrict<AutofillProfileItem>(
-      GetTableViewItem(/*section=*/1, /*item=*/0));
+      GetTableViewItem(/*section=*/3, /*item=*/0));
 
   EXPECT_NSEQ(@"Canada", item.detailText);
 }
@@ -234,10 +254,6 @@ TEST_F(AutofillProfileTableViewControllerTest,
 // enabled.
 TEST_F(AutofillProfileTableViewControllerTest,
        TestEnhancedAutofillMenuPresent) {
-  base::test::ScopedFeatureList scoped_feature_list;
-  scoped_feature_list.InitAndEnableFeature(
-      autofill::features::kAutofillAiWithDataSchema);
-
   CreateController();
   CheckController();
 
