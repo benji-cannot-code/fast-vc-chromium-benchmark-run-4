5760775578476e937930727a0ae766d4c9cdcdae
Lei Zhang <thestig@chromium.org>
2026-03-24 18:55:50 -0700

    Move chrome/browser/browser_features.cc to browser_features GN target
    Remove a superfluous include from browser_features.cc that should have
    been deleted in https://crrev.com/946246, and move browser_features.cc
    into the same target as browser_features.h.
    
    Change-Id: I03875f938defdd04563d06b2fa73da7c6bf053ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7694828
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1604545}

--- chrome/browser/browser_features.cc
+++ chrome/browser/browser_features.cc
@@ -9,10 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/branding_buildflags.h"
 #include "build/build_config.h"
 
-#if BUILDFLAG(IS_WIN)
-#include "chrome/browser/net/system_network_context_manager.h"
-#endif
-
 namespace features {
 
 #if BUILDFLAG(IS_ANDROID)
