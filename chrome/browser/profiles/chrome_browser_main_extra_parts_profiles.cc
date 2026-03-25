442c9ba7cf92474eac16c0e61d95968e2dfb4c92
Michael Cui <mlcui@google.com>
2026-03-24 20:10:59 -0700

    Add auxiliary donation to EnsureBrowserContextKeyedServiceFactoriesBuilt
    Also update `ServiceIsCreatedWithBrowserContext` to ensure that the
    service's dependents aren't redundantly created if the feature is
    disabled.
    
    Bug: b:432359106
    Change-Id: I6a6a2100b6cac9fe0e5cab6bb7ec8972dd31cc68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7594126
    Reviewed-by: Hailey Wang <haileywang@google.com>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1604575}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -311,6 +311,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/webapk/webapk_install_service_factory.h"
 #include "chrome/browser/android/webapk/webapk_sync_service_factory.h"
 #include "chrome/browser/autofill/android/android_sms_otp_backend_factory.h"
+#include "chrome/browser/auxiliary_search/auxiliary_search_donation_service_factory.h"
 #include "chrome/browser/auxiliary_search/auxiliary_search_provider.h"
 #include "chrome/browser/commerce/merchant_viewer/merchant_viewer_data_manager_factory.h"
 #include "chrome/browser/flags/android/chrome_feature_list.h"
@@ -753,8 +754,9 @@ void ChromeBrowserMainExtraPartsProfiles::
   autofill::ValuablesDataManagerFactory::GetInstance();
   autofill::WalletPassAccessManagerFactory::GetInstance();
 #if BUILDFLAG(IS_ANDROID)
-  AuxiliarySearchProvider::EnsureFactoryBuilt();
   AutocompleteControllerAndroid::EnsureFactoryBuilt();
+  AuxiliarySearchDonationServiceFactory::GetInstance();
+  AuxiliarySearchProvider::EnsureFactoryBuilt();
 #endif
 #if BUILDFLAG(ENABLE_BACKGROUND_CONTENTS)
   BackgroundContentsServiceFactory::GetInstance();
