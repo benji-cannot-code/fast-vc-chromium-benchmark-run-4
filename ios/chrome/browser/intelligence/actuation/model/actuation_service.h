52cf23a504535c29e9e99ce057cd8e1fd6b430ef
Kirubel <kaklilu@chromium.org>
2026-03-25 09:50:55 -0700

    Add EG tests for Click Tool
    This tool was recently updated to support targeting elements in
    iframes and to support targeting elements by identifier rather
    than coordinate. This CL updates the ActuationService EGTest
    to cover these cases.
    
    We also fix a lifetime issue in actuation_service.mm that was
    making the test flaky.
    
    Bug: b:472289820
    Change-Id: I64cbf2290cf771ba35372f241bc15e066a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7684642
    Reviewed-by: Fikre Mengistu <fikremengistu@chromium.org>
    Commit-Queue: Kirubel Aklilu <kaklilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1604901}

--- ios/chrome/browser/intelligence/actuation/model/actuation_service.h
+++ ios/chrome/browser/intelligence/actuation/model/actuation_service.h
@@ -9,8 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <memory>
 #import <string>
 
-#import "base/functional/callback_forward.h"
 #import "base/memory/raw_ptr.h"
+#import "base/memory/weak_ptr.h"
 #import "components/keyed_service/core/keyed_service.h"
 #import "ios/chrome/browser/intelligence/actuation/model/tools/actuation_tool.h"
 
@@ -46,6 +46,8 @@ class ActuationService : public KeyedService {
   raw_ptr<ProfileIOS> profile_;
   std::unique_ptr<ActuationToolFactory> tool_factory_;
   std::unique_ptr<AggregatedJournal> journal_;
+
+  base::WeakPtrFactory<ActuationService> weak_ptr_factory_{this};
 };
 
 #endif  // IOS_CHROME_BROWSER_INTELLIGENCE_ACTUATION_MODEL_ACTUATION_SERVICE_H_
