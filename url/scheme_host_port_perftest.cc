3344ce706c464d6fb07500031dee2b2eeef431f9
Adam Rice <ricea@chromium.org>
2026-03-25 22:48:59 -0700

    Add a benchmark for SchemeHostPort::Serialize()
    It would be good to have a benchmark for
    url::SchemeHostPort::Serialize() so we can detect regressions. Add one.
    
    Because this is the first benchmark in net_perftests using Google
    Benchmark, update the BUILD.gn file to use the perftest main() function
    provided by //base instead of its own.
    
    BUG=388956603
    
    Change-Id: Iceaf1d243abcb802bfa8161f5de5f19fedb3deba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7171787
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1605280}

---
+++ url/scheme_host_port_perftest.cc
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "url/scheme_host_port.h"
+
+#include "third_party/google_benchmark/src/include/benchmark/benchmark.h"
+
+// A benchmark for SchemeHostPort::Serialize().
+namespace url {
+
+namespace {
+
+void BM_SchemeHostPortSerialize(benchmark::State& state) {
+  SchemeHostPort scheme_host_port("https", "example.com", 443);
+  benchmark::DoNotOptimize(scheme_host_port);
+  for (auto _ : state) {
+    std::string serialization = scheme_host_port.Serialize();
+    benchmark::DoNotOptimize(serialization);
+    benchmark::ClobberMemory();
+  }
+}
+
+BENCHMARK(BM_SchemeHostPortSerialize);
+
+}  // namespace
+
+}  // namespace url
