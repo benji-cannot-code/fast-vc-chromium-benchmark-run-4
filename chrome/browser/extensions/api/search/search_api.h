6316f9ea2d9bcf16ffea304f2ba550d4a4b5fb54
James Cook <jamescook@chromium.org>
2026-03-26 12:47:44 -0700

    extensions: Fix search API navigation on desktop Android
    An earlier CL (http://crrev.com/c/7698693) enabled some search API
    tests and attempted to fix navigation triggered by the API. It
    incorrectly assumed that calls to ExtensionTabUtil::NavigateToURL()
    took a WebContents in order to use it for content (e.g. via
    NavigateParams::contents_to_insert). That isn't supported yet on
    desktop Android.
    
    However, the NavigateToURL() WebContents is *actually* the "source
    contents" for the navigation, and desktop Android does support
    that, as long as the asynchronous version of Navigate() is used.
    
    Switch to async Navigate() on all platforms for consistency and
    update the relevant tests.
    
    Bug: 442873371, 496596048
    Change-Id: Ie76247943fd16fade3955c855579b46f4ceeddbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7702234
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1605719}

--- chrome/browser/extensions/api/search/search_api.h
+++ chrome/browser/extensions/api/search/search_api.h
@@ -21,6 +21,9 @@ class SearchQueryFunction : public ExtensionFunction {
  private:
   ~SearchQueryFunction() override = default;
   ResponseAction Run() override;
+
+  // Called after navigate is initiated.
+  void OnNavigate();
 };
 
 }  // namespace extensions
