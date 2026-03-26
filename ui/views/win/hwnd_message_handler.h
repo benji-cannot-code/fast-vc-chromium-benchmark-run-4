1bf9792446b6b3cf7234e7422fe5604c545fcb37
mikt <mikt@google.com>
2026-03-25 22:10:47 -0700

    Add AMSC macro to HWNDMessageHandler
    Bug: 495981317
    Change-Id: I7516d2d73f139c0b51e325c4afa3073d9477c9ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7699604
    Reviewed-by: David Bienvenu <davidbienvenu@chromium.org>
    Commit-Queue: Mikihito Matsuura <mikt@google.com>
    Cr-Commit-Position: refs/heads/main@{#1605271}

--- ui/views/win/hwnd_message_handler.h
+++ ui/views/win/hwnd_message_handler.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/lazy_instance.h"
+#include "base/memory/advanced_memory_safety_checks.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
@@ -91,6 +92,9 @@ class VIEWS_EXPORT HWNDMessageHandler : public gfx::WindowImpl,
                                         public ui::InputMethodObserver,
                                         public ui::WindowEventTarget,
                                         public ui::AXFragmentRootDelegateWin {
+  // TODO(https://crbug.com/495981317): Remove this macro.
+  ADVANCED_MEMORY_SAFETY_CHECKS();
+
  public:
   // See WindowImpl for details on |debugging_id|.
   static std::unique_ptr<HWNDMessageHandler> Create(
