304645cab2da20a7da5ee8194e304319c3bf88d0
Federica Germinario <fedegermi@google.com>
2026-03-26 02:55:49 -0700

    [ios] Add LINT to synchronize UserActivityType and compatibility logic
    This CL adds LINT.IfChange tags to ensure that if a new AnctivityType
    is added, CompatibleModeForActivityType is updated too.
    This will prevent future regressions where new activities are
    accidentally unhandled when there is an enterprise policy.
    
    Bug: 492115056
    Change-Id: I79f7c0a5393fb46a45a0a276cb551054042262ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7702412
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1605354}

--- ios/chrome/app/task_request_user_activity.mm
+++ ios/chrome/app/task_request_user_activity.mm
@@ -33,6 +33,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // Possible user activity types, including invalid ones.
+// If a new activity type is added, `CompatibleModeForActivityType` in
+// user_activity_compatibility_util.mm needs to be updated too.
+// LINT.IfChange
 enum class UserActivityType {
   kInvalid,
   kHandoff,
@@ -61,6 +64,7 @@ enum class UserActivityType {
   kClearBrowsingData,
   kCredentialExchange,
 };
+// LINT.ThenChange(ios/chrome/browser/intents/model/user_activity_compatibility_util.mm)
 
 // Maps user activity type string to user activity items.
 struct UserActivityMapping {
