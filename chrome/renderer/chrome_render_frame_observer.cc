6055a5760eebba1c6bf1915a83cf45af62ebb501
Alexei Svitkine <asvitkine@chromium.org>
2026-03-26 10:00:21 -0700

    Cleanup: Remove histogram macros header includes in chrome/renderer
    Bug: None
    Change-Id: I944bfbb7f2c8b79d52c7f249d146278e9f326212
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7701716
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Auto-Submit: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1605616}

--- chrome/renderer/chrome_render_frame_observer.cc
+++ chrome/renderer/chrome_render_frame_observer.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/functional/bind.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/no_destructor.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
--- chrome/renderer/searchbox/searchbox_extension.cc
+++ chrome/renderer/searchbox/searchbox_extension.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/rtl.h"
 #include "base/json/json_writer.h"
 #include "base/json/string_escape.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/metrics/user_metrics.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
