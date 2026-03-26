291cfbdaeeec9da88ca888005408e7b40519f6b7
Yeonghan Kim <soosungp33@gmail.com>
2026-03-26 03:47:18 -0700

    Add microtask checkpoint before prepare script in XML parser
    According to [1], when an XML script element's end tag is parsed, the
    user agent must perform a microtask checkpoint before preparing the
    script element. The current implementation has the spec requirement
    documented as a comment, but it appears the actual implementation is
    missing. This causes an issue where microtask callbacks are not
    executed.
    So, this patch fixes the bug by calling PerformMicrotaskCheckpoint().
    
    [1] https://html.spec.whatwg.org/#parsing-xhtml-documents
    
    Bug: 439639184
    Change-Id: Ieafeb7e8a201ad44ee1eda2bb8e8ff3d840892b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7587954
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Philip Jägenstedt <foolip@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1605376}

--- third_party/blink/renderer/core/script/xml_parser_script_runner.cc
+++ third_party/blink/renderer/core/script/xml_parser_script_runner.cc
@@ -6,11 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/script/xml_parser_script_runner.h"
 
 #include "third_party/blink/renderer/core/dom/document.h"
+#include "third_party/blink/renderer/core/execution_context/agent.h"
 #include "third_party/blink/renderer/core/inspector/console_message.h"
 #include "third_party/blink/renderer/core/script/classic_pending_script.h"
 #include "third_party/blink/renderer/core/script/script_loader.h"
 #include "third_party/blink/renderer/core/script/xml_parser_script_runner_host.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
 namespace blink {
 
@@ -73,6 +75,9 @@ void XMLParserScriptRunner::ProcessScriptElement(
   // [Parsing] When the element's end tag is subsequently parsed, the user agent
   // must perform a microtask checkpoint, and then prepare the script element.
   // [spec text]
+  if (RuntimeEnabledFeatures::RunMicrotaskBeforeXmlScriptEnabled()) {
+    document.GetAgent().event_loop()->PerformMicrotaskCheckpoint();
+  }
   PendingScript* pending_script =
       ScriptLoaderFromElement(element)->PrepareScript(
           ScriptLoader::ParserBlockingInlineOption::kAllow,
