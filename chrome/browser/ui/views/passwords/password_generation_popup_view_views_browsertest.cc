7cf79a13b19f8608c475ebc0eea78842303405ce
Anders Hartvoll Ruud <andruud@chromium.org>
2026-03-26 08:06:01 -0700

    Revert "[a11y][Passwords] Fix generation popup a11y behavior"
    This reverts commit 6e67fa61459158c63bc77067938dd9244322e3eb.
    
    Reason for revert: Causing failure
    on linux-blink-web-tests-force-accessibility-rel.
    
    Original change's description:
    > [a11y][Passwords] Fix generation popup a11y behavior
    >
    > Currently, password generation popup's is initialized with
    > views::Widget::InitParams::Activatable::kDefault [1], which means
    > that the widget will not be activated, since it's TYPE_POPUP [2].
    >
    > This is an intended behavior for non-screen reader users, as the
    > intention is to keep the focus on the password form field, so they can
    > still type and reject the generation this way.
    >
    > For screen reader users though, not activating the widget means that
    > they are not really notified about a dialog appearing and cannot
    > navigate properly through the elements of the popup.
    >
    > This CL proposes a following solution to improve that:
    > * for non-screen readers users the behavior will be kept as is (i.e.
    > focus will remain on the form and the custom keyboard handling will be
    > kept, so it's possible to accept/reject the popup with keyboard only)
    > * for screen reader users the popup will be initialized with
    > Activatable::kYes, only in generation state (in editing state there
    > is nothing to be done in the popup)
    > * activating widget will trigger `GenerationElementLostFocus` event,
    > hence a special case is added there to prevent that
    >
    > Additionally, the CL updates a couple of elements of the popup to have
    > the proper a11y role, as suggested in the bug.
    >
    > [1] https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ui/views/autofill/popup/popup_base_view.h;l=70;drc=00d0ab6b175a24959579e681586c009894af8317
    > [2] https://crsrc.org/c/ui/views/widget/widget.cc;l=207;drc=71a49dfb2ffd044b3c538f66ae21234599e3e76e
    >
    > Fixed: 484415822
    > Change-Id: If2bdf7b0e5cfe936c74d235bcb8db89787234993
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7697712
    > Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    > Commit-Queue: Rafał Godlewski <rgod@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1605403}
    
    Bug: 484415822
    Change-Id: Ief290908729f47224885d0e50b60b33fe008cc4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7702418
    Owners-Override: Anders Hartvoll Ruud <andruud@google.com>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1605511}

--- chrome/browser/ui/views/passwords/password_generation_popup_view_views_browsertest.cc
+++ chrome/browser/ui/views/passwords/password_generation_popup_view_views_browsertest.cc
@@ -117,10 +117,8 @@ class PasswordGenerationPopupViewBrowsertest
   PasswordGenerationPopupViewViews* CreateView(
       MockPasswordGenerationPopupController& controller) override {
     return new PasswordGenerationPopupViewViews(
-        views::Widget::InitParams::Activatable::kDefault,
-        controller.GetWeakPtr(),
-        views::Widget::GetWidgetForNativeWindow(
-            browser()->window()->GetNativeWindow()));
+        controller.GetWeakPtr(), views::Widget::GetWidgetForNativeWindow(
+                                     browser()->window()->GetNativeWindow()));
   }
 
  private:
