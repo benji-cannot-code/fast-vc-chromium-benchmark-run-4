115bdcdc1f5eed674e890dee2b3783f3e1adf141
Alexei Svitkine <asvitkine@chromium.org>
2026-03-26 09:18:00 -0700

    Cleanup: Remove histogram macros header includes in sandbox
    Bug: None
    Change-Id: Id634c78dca926f360095e759eca749c18c285fec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7702455
    Auto-Submit: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1605580}

--- sandbox/policy/linux/sandbox_linux.cc
+++ sandbox/policy/linux/sandbox_linux.cc
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/logging.h"
 #include "base/memory/singleton.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/posix/eintr_wrapper.h"
 #include "base/process/set_process_title.h"
 #include "base/strings/string_number_conversions.h"
