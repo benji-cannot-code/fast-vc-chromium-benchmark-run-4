3c02ee429ddec14398a697e3449864a643858fe5
Sylvain Defresne <sdefresne@chromium.org>
2026-03-26 14:15:22 -0700

    [ios] Simplify the implementation of WKWebView -> WebState mapping
    Introduce WebStateHandleForWebView that stores a weak pointer to a
    WebState and associate it to the WKWebView via the ObjC runtime.
    
    This is simpler than the previous logic which stored the mapping in
    a linked list and used linear search for update and retrieval. The
    new implementation is managed by the runtime (and likely stored in
    a dictionary-like structure that has better than linear complexity).
    
    This changes the semantic of SetAssociatedWebViewForWebState(...)
    as it is now possible to have two WKWebView associated to the same
    WebState object.
    
    In practice, this is only relevant in tests as in production, the
    CRWWebController calls ClearAssociatedWebViewForWebState(...) with
    the old mapping before associating a new WKWebView with a WebState.
    
    Fixed: 495778943
    Change-Id: I425581aa9a7945a96a16790a249c2eb797879e46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7703921
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1605779}

--- ios/web/js_messaging/java_script_content_world.mm
+++ ios/web/js_messaging/java_script_content_world.mm
@@ -314,10 +314,7 @@ void JavaScriptContentWorld::ScriptMessageReceived(
     return;
   }
 
-  web::WebViewWebStateMap* map =
-      web::WebViewWebStateMap::FromBrowserState(browser_state_);
-  web::WebState* web_state = map->GetWebStateForWebView(script_message.webView);
-
+  web::WebState* web_state = web::GetWebStateForWebView(script_message.webView);
   if (!web_state) {
     return;
   }
@@ -345,10 +342,7 @@ void JavaScriptContentWorld::ScriptMessageReceivedWithReply(
     return;
   }
 
-  web::WebViewWebStateMap* map =
-      web::WebViewWebStateMap::FromBrowserState(browser_state_);
-  web::WebState* web_state = map->GetWebStateForWebView(script_message.webView);
-
+  web::WebState* web_state = web::GetWebStateForWebView(script_message.webView);
   if (!web_state) {
     reply_handler(nullptr, kInternalError);
     return;
