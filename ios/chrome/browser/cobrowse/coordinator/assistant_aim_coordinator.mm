9a465b112cd5fc04efe5df96aa1cc7759b6d6b9e
Aliona DANGLA <alionadangla@google.com>
2026-03-26 09:00:06 -0700

    [iOS] Add flag check in assistant AIM
    Fixed: 496545051
    Change-Id: Iead90605fba2b5d849355e9c86d44a06f59cedf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7703922
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1605559}

--- ios/chrome/browser/cobrowse/coordinator/assistant_aim_coordinator.mm
+++ ios/chrome/browser/cobrowse/coordinator/assistant_aim_coordinator.mm
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/model/browser/browser.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
 #import "ios/chrome/browser/shared/public/commands/command_dispatcher.h"
+#import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/web/public/web_state.h"
 
 @interface AssistantAIMCoordinator () <AssistantAIMViewControllerDelegate,
@@ -41,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 
 - (void)start {
+  CHECK(IsAimCobrowseEnabled());
   if (self.browser->GetProfile()->IsOffTheRecord()) {
     return;
   }
