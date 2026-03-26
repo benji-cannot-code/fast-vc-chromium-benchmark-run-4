20bc1f97c5e7ee53d7ad80a32d92f6a0c38ed92b
Alex Turner <alexmt@chromium.org>
2026-03-26 15:15:11 -0700

    Update GetMetaTagNamesAffectingEligibility to avoid ABI issues
    The GetMetaTagNamesAffectingEligibility function in the
    PageContextEligibilityAPI is updated to return a span of string_views,
    instead of returning via an out param. This change avoids memory
    allocations across the ABI boundary and improves readability.
    
    We can't use a std::span as that causes its own ABI issues.
    
    This is an update to the interface added in https://crrev.com/c/7684652.
    It is safe to update as it is not yet defined. (This will occur after
    landing https://crrev.com/i/9078642.)
    
    Bug: b:490161242
    Change-Id: Ia87e1112b3d219d9ea838b89734e51f3a0d2d247
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7700141
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1605814}

--- components/optimization_guide/content/browser/page_context_eligibility_api.h
+++ components/optimization_guide/content/browser/page_context_eligibility_api.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_OPTIMIZATION_GUIDE_CONTENT_BROWSER_PAGE_CONTEXT_ELIGIBILITY_API_H_
 #define COMPONENTS_OPTIMIZATION_GUIDE_CONTENT_BROWSER_PAGE_CONTEXT_ELIGIBILITY_API_H_
 
+#include <stddef.h>
+
 #include <string>
 #include <string_view>
 #include <vector>
@@ -41,6 +43,13 @@ struct FrameMetadata {
   std::vector<MetaTag> meta_tags;
 };
 
+// Represents an span of string views. Used instead of a std::span to avoid ABI
+// issues.
+struct StringViewSpan {
+  const std::string_view* data;
+  size_t size;
+};
+
 // Table of C API functions defined within the library.
 struct PageContextEligibilityAPI {
   // Whether the page is context eligible.
@@ -59,15 +68,13 @@ struct PageContextEligibilityAPI {
       const std::string& host,
       const std::string& path,
       const std::vector<std::string>& updated_meta_tags);
-  // Gets a vector of meta tag names that, if changed on a frame, could affect
-  // the page context eligibility. This could be empty if meta tag changes would
-  // not affect eligibility. Returns the vector via `out_meta_tag_names`, which
-  // is always overwritten.
-  void (*GetMetaTagNamesAffectingEligibility)(
+  // Returns the meta tag names that, if changed on a frame, could affect the
+  // page context eligibility. This could be empty if meta tag changes would
+  // not affect eligibility.
+  StringViewSpan (*GetMetaTagNamesAffectingEligibility)(
       std::string_view host,
       std::string_view path,
-      const std::vector<FrameMetadata>& frame_metadata,
-      std::vector<std::string>& out_meta_tag_names);
+      const std::vector<FrameMetadata>& frame_metadata);
 };
 
 // Signature of the GetPageContextEligibilityAPI() function which the shared
