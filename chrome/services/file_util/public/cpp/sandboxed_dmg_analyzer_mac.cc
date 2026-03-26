edc36ba2e528e3c6c26fbe0a93d8d8d75465b4da
Alexei Svitkine <asvitkine@chromium.org>
2026-03-26 08:50:44 -0700

    Cleanup: Remove histogram macros header includes in chrome/services
    Bug: None
    Change-Id: I8c52e357a19346545ebbe12e38938e576fc375fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7703133
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Auto-Submit: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1605550}

--- chrome/services/file_util/public/cpp/sandboxed_dmg_analyzer_mac.cc
+++ chrome/services/file_util/public/cpp/sandboxed_dmg_analyzer_mac.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/bind.h"
 #include "base/metrics/histogram_functions.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
 #include "chrome/common/safe_browsing/archive_analyzer_results.h"
