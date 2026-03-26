6971e6eec0619593628d3eb29221e76f9797c56d
Alexei Svitkine <asvitkine@chromium.org>
2026-03-26 16:33:26 -0700

    Cleanup: Remove histogram macros header includes in device
    Bug: None
    Change-Id: Iba5af9264639fd9cef94707f0658e0e37ca79b71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7701737
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Reviewed-by: Michael Sun <michaelfsun@google.com>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Auto-Submit: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1605852}

--- device/vr/android/arcore/arcore_impl.cc
+++ device/vr/android/arcore/arcore_impl.cc
@@ -6,16 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/vr/android/arcore/arcore_impl.h"
 
 #include <algorithm>
+#include <array>
 #include <optional>
 
 #include "base/android/jni_android.h"
 #include "base/compiler_specific.h"
 #include "base/containers/span.h"
 #include "base/functional/bind.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/numerics/checked_math.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/task/single_thread_task_runner.h"
+#include "base/time/time.h"
 #include "base/trace_event/trace_event.h"
 #include "base/types/pass_key.h"
 #include "device/vr/android/arcore/arcore_math_utils.h"
