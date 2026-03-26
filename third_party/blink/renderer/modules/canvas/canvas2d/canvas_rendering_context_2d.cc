77815e4a7b287b282154427d605a445e1adb4f18
Colin Blundell <blundell@chromium.org>
2026-03-26 02:23:46 -0700

    [Blink] Have SharedGpuContext method take in ContextProviderWrapper
    Will enable moving this method out of SharedGpuContext into canvas_utils
    in followup. Note that this is a behavioral no-op [1].
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/gpu/shared_gpu_context.cc;l=94-99?q=SharedGpuContext::Contex&ss=chromium
    
    Bug: 488937356
    Change-Id: I5a6e654601fd33eec65dbe7509d941ce0dd8ef8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7697661
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1605329}

--- third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.cc
@@ -318,7 +318,8 @@ void CanvasRenderingContext2D::WillDrawImage(CanvasImageSource* source,
   // If the source is GPU-accelerated, and the canvas is not, but could be...
   if (source_is_accelerated && canvas()->ShouldAccelerate2dContext() &&
       canvas()->GetRasterModeForCanvas2D() == RasterMode::kCPU &&
-      SharedGpuContext::AllowSoftwareToAcceleratedCanvasUpgrade()) {
+      SharedGpuContext::AllowSoftwareToAcceleratedCanvasUpgrade(
+          SharedGpuContext::ContextProviderWrapper().get())) {
     // Recreate the CRP in GPU raster mode and signal that it needs a
     // compositing update.
     canvas()->SetPreferred2DRasterMode(RasterModeHint::kPreferGPU);
@@ -867,7 +868,8 @@ DOMMatrix* CanvasRenderingContext2D::drawElementImage(
 
 void CanvasRenderingContext2D::EnableAccelerationIfPossible() {
   if (canvas()->GetRasterModeForCanvas2D() == RasterMode::kCPU &&
-      SharedGpuContext::AllowSoftwareToAcceleratedCanvasUpgrade()) {
+      SharedGpuContext::AllowSoftwareToAcceleratedCanvasUpgrade(
+          SharedGpuContext::ContextProviderWrapper().get())) {
     canvas()->SetPreferred2DRasterMode(RasterModeHint::kPreferGPU);
     DropAndRecreateExistingResourceProvider();
   }
