12f7ac0701295c0f5eea143f998d7cdcd465840f
Ben Chin <lungc@chromium.org>
2026-03-25 21:46:25 -0700

    Decouple Tab Strip sync from Web Content during fluid resize
    This CL passes a 0-frame deadline to the Viz compositor during fluid
    resize. It allows the tab strip and Toolbar stay synced with the
    resizing window edge, while the Web Content is allowed to temporarily
    stretch or clip until it catches up, eliminating the UI thread hang.
    
    Bug: 493381827
    Change-Id: Ie4214a5642ae1111b19dd660d01d14dae4871780
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7699620
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: ben chin <lungc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1605265}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/CompositorView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/CompositorView.java
@@ -670,7 +670,12 @@ public class CompositorView extends FrameLayout
 
     void onPhysicalBackingSizeChanged(WebContents webContents, int width, int height) {
         CompositorViewJni.get()
-                .onPhysicalBackingSizeChanged(mNativeCompositorView, webContents, width, height);
+                .onPhysicalBackingSizeChanged(
+                        mNativeCompositorView,
+                        webContents,
+                        width,
+                        height,
+                        isFluidResizeEnabledAndLff());
     }
 
     void onControlsResizeViewChanged(WebContents webContents, boolean controlsResizeView) {
@@ -933,7 +938,11 @@ public class CompositorView extends FrameLayout
                 @Nullable InputTransferToken browserInputToken);
 
         void onPhysicalBackingSizeChanged(
-                long nativeCompositorView, WebContents webContents, int width, int height);
+                long nativeCompositorView,
+                WebContents webContents,
+                int width,
+                int height,
+                boolean isFluidResize);
 
         void onControlsResizeViewChanged(
                 long nativeCompositorView, WebContents webContents, boolean controlsResizeView);
