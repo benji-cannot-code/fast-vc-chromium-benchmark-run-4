e38bb6249c77ce320f11736fb8aecd0508db0acf
Colin Blundell <blundell@chromium.org>
2026-03-26 00:51:17 -0700

    [Blink] Have Canvas2DRPSI::ProduceCanvasResource() call FlushCanvas2D()
    Bug: 352263194
    Change-Id: I101248a00aaadf8100dba743cb31e25e7b5161a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7700472
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1605302}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
@@ -906,7 +906,7 @@ Canvas2DResourceProviderSharedImage::ProduceCanvasResource(FlushReason reason) {
       return nullptr;
     }
 
-    FlushCanvas(reason);
+    FlushCanvas2D(reason);
 
     // Note that the resource *must* be a CanvasResourceSharedImage as this
     // class creates CanvasResourceSharedImage instances exclusively.
@@ -924,7 +924,7 @@ Canvas2DResourceProviderSharedImage::ProduceCanvasResource(FlushReason reason) {
   // backing SharedImage). Hence, we must make sure that the SI is updated to
   // reflect the ops made in the current write access (if any) and give up any
   // such write access.
-  FlushCanvas(reason);
+  FlushCanvas2D(reason);
   EndWriteAccess();
 
   return resource_;
