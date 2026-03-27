da7e73f6c8d2321613cf03ac3f7c8330d9cfe7d4
Sky Malice <skym@chromium.org>
2026-03-27 16:37:28 -0700

    Remove unused shouldJumpStartOmnibox().
    Bug: None
    Change-Id: I37d1803f5a88afa855efabf228bb66dc8ff984d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7705393
    Auto-Submit: Sky Malice <skym@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1606556}

--- components/omnibox/common/android/java/src/org/chromium/components/omnibox/OmniboxFeatures.java
+++ components/omnibox/common/android/java/src/org/chromium/components/omnibox/OmniboxFeatures.java
@@ -473,8 +473,4 @@ public class OmniboxFeatures {
     public static boolean allowMultilineEditField() {
         return sMultilineEditField.isEnabled();
     }
-
-    public static boolean shouldJumpStartOmnibox() {
-        return isJumpStartOmniboxEnabled();
-    }
 }
