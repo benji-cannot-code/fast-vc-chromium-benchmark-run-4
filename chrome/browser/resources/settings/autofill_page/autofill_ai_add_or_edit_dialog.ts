19ca045f0aa83483b76ab0d42d11bfe1ec4f8988
dpapad <dpapad@chromium.org>
2026-03-26 17:14:20 -0700

    Settings: Fix for AttributeTypeDataType error reports.
    This fix works around a known issue with the Extensions system on WebUI
    pages, crbug.com/40162029, where private extension APIs are not
    immediately available when the page loads if it was opened with
    window.open() call, which causing many error reports with the following
    error:
    
    Uncaught TypeError: Cannot read properties of undefined (reading
    'AttributeTypeDataType')
    
    Bug: 495810744,40162029
    Change-Id: I040fce7226909e45b5b5275c18537cc46a5f4d98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7705096
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1605883}

--- chrome/browser/resources/settings/autofill_page/autofill_ai_add_or_edit_dialog.ts
+++ chrome/browser/resources/settings/autofill_page/autofill_ai_add_or_edit_dialog.ts
@@ -35,7 +35,6 @@ import {EntityDataManagerProxyImpl} from './entity_data_manager_proxy.js';
 
 type AttributeInstance = chrome.autofillPrivate.AttributeInstance;
 type AttributeType = chrome.autofillPrivate.AttributeType;
-const AttributeTypeDataType = chrome.autofillPrivate.AttributeTypeDataType;
 type AttributeTypeDataType = chrome.autofillPrivate.AttributeTypeDataType;
 type CountryEntry = chrome.autofillPrivate.CountryEntry;
 type DateValue = chrome.autofillPrivate.DateValue;
@@ -77,7 +76,7 @@ export class SettingsAutofillAiAddOrEditDialogElement extends
 
       attributeTypeDataTypeEnum_: {
         type: Object,
-        value: AttributeTypeDataType,
+        value: () => chrome.autofillPrivate.AttributeTypeDataType,
       },
 
       /**
@@ -300,12 +299,14 @@ export class SettingsAutofillAiAddOrEditDialogElement extends
       return {
         type: attributeType,
         value: existingAttributeInstance?.value ||
-            (attributeType.dataType === AttributeTypeDataType.DATE ? {
-                 month: '',
-                 day: '',
-                 year: '',
-               } :
-                                                                     ''),
+            (attributeType.dataType ===
+                     chrome.autofillPrivate.AttributeTypeDataType.DATE ?
+                 {
+                   month: '',
+                   day: '',
+                   year: '',
+                 } :
+                 ''),
       };
     });
   }
@@ -323,7 +324,8 @@ export class SettingsAutofillAiAddOrEditDialogElement extends
     // This logic exists because of a trade-off in the C++ autofill private API,
     // that has to call `EntityInstance::GetCompleteInfo()`, instead of
     // `EntityInstance::GetRawInfo()`.
-    if (attributeInstance.type.dataType === AttributeTypeDataType.COUNTRY) {
+    if (attributeInstance.type.dataType ===
+        chrome.autofillPrivate.AttributeTypeDataType.COUNTRY) {
       // TODO(crbug.com/403312087): Remove comment and exclamation marks once
       // the <hr> TODO below is solved.
       // The find operation will always find a match. Currently, the only entry
@@ -495,7 +497,8 @@ export class SettingsAutofillAiAddOrEditDialogElement extends
    * first time. Subsequent validations occur any time a field is changed.
    */
   private isDateInvalid_(attributeInstance: AttributeInstance): boolean {
-    if (attributeInstance.type.dataType !== AttributeTypeDataType.DATE ||
+    if (attributeInstance.type.dataType !==
+            chrome.autofillPrivate.AttributeTypeDataType.DATE ||
         !this.userClickedSaveButton_) {
       return false;
     }
@@ -572,7 +575,8 @@ export class SettingsAutofillAiAddOrEditDialogElement extends
    */
   private isAttributeInstanceNotEmpty(attributeInstance: AttributeInstance):
       boolean {
-    if (attributeInstance.type.dataType === AttributeTypeDataType.DATE) {
+    if (attributeInstance.type.dataType ===
+        chrome.autofillPrivate.AttributeTypeDataType.DATE) {
       const value: DateValue = attributeInstance.value as DateValue;
       return value.month.trim().length > 0 || value.day.trim().length > 0 ||
           value.year.trim().length > 0;
