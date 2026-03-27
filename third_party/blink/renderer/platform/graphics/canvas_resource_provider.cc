ad7e7d091d7255fae0bf19f50a03bc666dab7a26
Colin Blundell <blundell@chromium.org>
2026-03-27 04:08:14 -0700

    [Blink] Have FlushCanvas() take is_overwrite param
    This gets threaded into WillDrawInternal(). All callsites pass false to
    preserve current behavior; I'll incrementally reason about flipping them
    to pass true.
    
    Bug: 352263194
    Change-Id: I9b98188924be8630bda9d856bdff99224d314ea1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7702745
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1606096}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
@@ -941,7 +941,7 @@ CanvasNon2DResourceProviderSharedImage::ProduceCanvasResource() {
       return nullptr;
     }
 
-    FlushCanvas();
+    FlushCanvas(/*is_overwrite=*/false);
 
     // Note that the resource *must* be a CanvasResourceSharedImage as this
     // class creates CanvasResourceSharedImage instances exclusively.
@@ -959,7 +959,7 @@ CanvasNon2DResourceProviderSharedImage::ProduceCanvasResource() {
   // backing SharedImage). Hence, we must make sure that the SI is updated to
   // reflect the ops made in the current write access (if any) and give up any
   // such write access.
-  FlushCanvas();
+  FlushCanvas(/*is_overwrite=*/false);
   EndWriteAccess();
 
   return resource_;
@@ -1090,7 +1090,7 @@ CanvasNon2DResourceProviderSharedImage::Snapshot(ImageOrientation orientation) {
       return nullptr;
     }
 
-    FlushCanvas();
+    FlushCanvas(/*is_overwrite=*/false);
 
     cc::PaintImage paint_image;
 
@@ -1120,7 +1120,7 @@ CanvasNon2DResourceProviderSharedImage::Snapshot(ImageOrientation orientation) {
   }
 
   if (!cached_snapshot_) {
-    FlushCanvas();
+    FlushCanvas(/*is_overwrite=*/false);
     EndWriteAccess();
     cached_snapshot_ = resource_->Bitmap();
 
@@ -1255,7 +1255,7 @@ void Canvas2DResourceProviderSharedImage::OnFlushForImage(
 void CanvasNon2DResourceProviderSharedImage::OnFlushForImage(
     cc::PaintImage::ContentId content_id) {
   if (Canvas().IsCachingImage(content_id)) {
-    FlushCanvas();
+    FlushCanvas(/*is_overwrite=*/false);
   }
   if (cached_snapshot_ &&
       cached_snapshot_->PaintImageForCurrentFrame().GetContentIdForFrame(0) ==
@@ -1911,7 +1911,7 @@ ScopedRasterTimer CanvasResourceProvider::CreateScopedRasterTimer() {
                            always_enable_raster_timers_for_testing_);
 }
 
-void CanvasNon2DResourceProviderSharedImage::FlushCanvas() {
+void CanvasNon2DResourceProviderSharedImage::FlushCanvas(bool is_overwrite) {
   if (!recorder_->HasReleasableDrawOps()) {
     return;
   }
@@ -1921,7 +1921,7 @@ void CanvasNon2DResourceProviderSharedImage::FlushCanvas() {
     EnsureSkiaCanvas();
     skia_canvas_->drawPicture(std::move(last_recording));
   } else if (!IsGpuContextLost()) {
-    auto access = WillDrawInternal(/*is_overwrite=*/false);
+    auto access = WillDrawInternal(is_overwrite);
     EnsureWriteAccess();
 
     const bool needs_clear = !is_cleared_;
