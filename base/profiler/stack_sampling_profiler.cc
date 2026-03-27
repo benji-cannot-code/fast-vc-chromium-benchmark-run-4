65611c421a9eea5406252b22a503bbf46a5693b0
Etienne Pierre-doray <etiennep@chromium.org>
2026-03-27 12:54:48 -0700

    [task] Simplify thread state
    There's a lot of complexity in 'tracking' whether a thread is running
    or not. It turns out this isn't needed for any of the handful StopSoon()
    use cases, while the remaining can be simply synchronized on the owning
    thread.
    This CL removes the multi-threaded logic and controls the thread state
    with an enum. The resulting behavior of IsRunning() is similar to
    std::thread::joinable()
    
    Bug: 40881604
    Change-Id: I102078d7124fc2840e9be8c336ad4af97626526d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3485359
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1606416}

--- base/profiler/stack_sampling_profiler.cc
+++ base/profiler/stack_sampling_profiler.cc
@@ -115,6 +115,9 @@ class StackSamplingProfiler::SamplingThread : public Thread {
     // posted.
     static void ShutdownAssumingIdle(bool simulate_intervening_add);
 
+    // Returns whether the sampling thread is currently running or not.
+    static bool IsSamplingThreadRunning();
+
    private:
     // Calls the sampling threads ShutdownTask and then signals an event.
     static void ShutdownTaskAndSignalEvent(SamplingThread* sampler,
@@ -375,6 +378,17 @@ void StackSamplingProfiler::SamplingThread::TestPeer::
                                WaitableEvent* event) {
   sampler->ShutdownTask(add_events);
   event->Signal();
+}
+
+// static
+bool StackSamplingProfiler::SamplingThread::TestPeer::
+    IsSamplingThreadRunning() {
+  SamplingThread* sampler = SamplingThread::GetInstance();
+  if (!sampler->IsRunning()) {
+    return false;
+  }
+  AutoLock lock(sampler->thread_execution_state_lock_);
+  return sampler->thread_execution_state_ == RUNNING;
 }
 
 AtomicSequenceNumber StackSamplingProfiler::SamplingThread::CollectionContext::
@@ -813,7 +827,7 @@ void StackSamplingProfiler::TestPeer::Reset() {
 
 // static
 bool StackSamplingProfiler::TestPeer::IsSamplingThreadRunning() {
-  return SamplingThread::GetInstance()->IsRunning();
+  return SamplingThread::TestPeer::IsSamplingThreadRunning();
 }
 
 // static
