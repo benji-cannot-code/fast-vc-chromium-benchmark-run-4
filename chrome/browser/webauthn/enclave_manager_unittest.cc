979ef84f6c9f4af781774c82c552268d2410a331
Noah Rose Ledesma <noahrose@google.com>
2026-03-27 13:59:03 -0700

    Revert "Reland "[webauthn] Fix UV key consideration""
    This reverts commit d3b453c76f1b8d9fdf9baa0530571b9fa9560f79.
    
    Reason for revert: Consistent ASan failures on a gardened bot (ex: https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ASan%20LSan%20Tests%20(1)/139796/test-results?q=ID%3A%3A%2F%2Fchrome%2Ftest%5C%3Abrowser_tests%21gtest%3A%3AEnclaveAuthenticatorBrowserTest%23GetAssertionUvUnsatisfiable)
    
    Original change's description:
    > Reland "[webauthn] Fix UV key consideration"
    >
    > This is a reland of commit e3401b0eb364fc6555b8472486387158af7c5a6d
    >
    > The original patch contained a bug in the form of assuming that
    > `AuthenticatorRequestDialogModel::platform_has_biometrics` would be set
    > when `OnUVCapabilityKnown` was called. However, at this point the UI may
    > not have been shown, in which case the value is not set. Another bug in
    > the exact same code path checked for presence of the optional instead of
    > the value of the contained boolean.
    >
    > This version of the patch moves checking for `platform_has_biometrics`
    > later on the request as needed. I also updated `can_make_uv_keys_` to no
    > longer be an optional and instead default to false. The UI state machine
    > is complex enough that I can't be sure dereferencing the optional is
    > safe.
    >
    > See the diff against PS#1 for new changes.
    >
    > Original change's description:
    > > [webauthn] Fix UV key consideration
    > >
    > > Availability of the user verifying key provider is not sufficient to
    > > determine if UV keys are available or not. This assumption was baked in
    > > https://crrev.com/c/7557586 and made it so that fix wouldn't work on
    > > Windows.
    > >
    > > This patch moves determining whether user verifying keys are available
    > > earlier in the flow. Then, we use that information to determine the
    > > state of user verification for GPM and drive the UI accordingly.
    > >
    > > Bug: 469125044
    > > Change-Id: I695f54a682db0aa87ac5284974eb3a794b4b4e2d
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7666398
    > > Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    > > Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    > > Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1602743}
    >
    > Bug: 469125044
    > Cq-Include-Trybots: luci.chromium.try:mac15-arm64-rel
    > Change-Id: I1ea1c0e6fe32149bc89c288632bae371285b2dd9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7703936
    > Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    > Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    > Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1606227}
    
    Bug: 469125044
    Cq-Include-Trybots: luci.chromium.try:mac15-arm64-rel
    Change-Id: I4b1d5bfe9bfbb5c75af29a6c7522884b008ac431
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7707391
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Owners-Override: Noah Rose Ledesma <noahrose@google.com>
    Cr-Commit-Position: refs/heads/main@{#1606462}

--- chrome/browser/webauthn/enclave_manager_unittest.cc
+++ chrome/browser/webauthn/enclave_manager_unittest.cc
@@ -2076,8 +2076,7 @@ TEST_F(EnclaveManagerTest, MAYBE_HardwareKeyLost) {
 #if BUILDFLAG(IS_WIN)
   // Windows does deferred UV key creation. This test has to trigger the actual
   // create before testing that it is later deleted.
-  EXPECT_EQ(manager_.uv_key_state(
-                EnclaveManager::PlatformUvSupport::kUvKeyButNoBiometrics),
+  EXPECT_EQ(manager_.uv_key_state(/*platform_has_biometrics=*/false),
             EnclaveManager::UvKeyState::kUsesSystemUIDeferredCreation);
   std::unique_ptr<EnclaveManager::UvKeyCreationLock> uv_creation_lock;
   device::enclave::UVKeyCreationCallback key_creation_callback;
@@ -2659,12 +2658,10 @@ TEST_F(EnclaveUVTest, UserVerifyingKeyAvailable) {
   EXPECT_TRUE(add_future.Wait());
 
 #if BUILDFLAG(IS_WIN)
-  EXPECT_EQ(manager_.uv_key_state(
-                EnclaveManager::PlatformUvSupport::kUvKeyButNoBiometrics),
+  EXPECT_EQ(manager_.uv_key_state(/*platform_has_biometrics=*/false),
             EnclaveManager::UvKeyState::kUsesSystemUIDeferredCreation);
 #else
-  EXPECT_EQ(manager_.uv_key_state(
-                EnclaveManager::PlatformUvSupport::kUvKeyButNoBiometrics),
+  EXPECT_EQ(manager_.uv_key_state(/*platform_has_biometrics=*/false),
             EnclaveManager::UvKeyState::kUsesSystemUI);
 #endif
 }
@@ -2694,7 +2691,7 @@ TEST_F(EnclaveUVTest, UserVerifyingKeyUnavailable) {
   ASSERT_FALSE(manager_.is_idle());
   EXPECT_TRUE(add_future.Wait());
   ASSERT_TRUE(manager_.IsRegistered());
-  EXPECT_EQ(manager_.uv_key_state(EnclaveManager::PlatformUvSupport::kNoUvKey),
+  EXPECT_EQ(manager_.uv_key_state(/*platform_has_biometrics=*/false),
             EnclaveManager::UvKeyState::kNone);
 }
 
@@ -2726,8 +2723,7 @@ TEST_F(EnclaveUVTest, UserVerifyingKeyLost) {
 #if BUILDFLAG(IS_WIN)
   // Windows does deferred UV key creation. This test has to trigger the actual
   // create before testing that it is later deleted.
-  EXPECT_EQ(manager_.uv_key_state(
-                EnclaveManager::PlatformUvSupport::kUvKeyButNoBiometrics),
+  EXPECT_EQ(manager_.uv_key_state(/*platform_has_biometrics=*/false),
             EnclaveManager::UvKeyState::kUsesSystemUIDeferredCreation);
   std::unique_ptr<EnclaveManager::UvKeyCreationLock> uv_creation_lock;
   device::enclave::UVKeyCreationCallback key_creation_callback;
@@ -2742,8 +2738,7 @@ TEST_F(EnclaveUVTest, UserVerifyingKeyLost) {
           }));
   task_env_.RunUntilQuit();
 #else
-  ASSERT_EQ(manager_.uv_key_state(
-                EnclaveManager::PlatformUvSupport::kUvKeyButNoBiometrics),
+  ASSERT_EQ(manager_.uv_key_state(/*platform_has_biometrics=*/false),
             EnclaveManager::UvKeyState::kUsesSystemUI);
 #endif
   manager_.ClearCachedKeysForTesting();
@@ -2808,8 +2803,7 @@ TEST_F(EnclaveUVTest, UserVerifyingKeyUseExisting) {
   ASSERT_FALSE(manager_.is_idle());
   EXPECT_TRUE(add_future.Wait());
 
-  ASSERT_EQ(manager_.uv_key_state(
-                EnclaveManager::PlatformUvSupport::kUvKeyButNoBiometrics),
+  ASSERT_EQ(manager_.uv_key_state(/*platform_has_biometrics=*/false),
             EnclaveManager::UvKeyState::kUsesSystemUI);
 }
 
@@ -3102,14 +3096,12 @@ TEST_F(EnclaveUVTest, ChromeHandlesBiometrics) {
 
   scoped_fake_keychain_.SetUVMethod(
       crypto::apple::ScopedFakeKeychainV2::UVMethod::kBiometrics);
-  EXPECT_EQ(manager_.uv_key_state(
-                EnclaveManager::PlatformUvSupport::kUvKeyWithBiometrics),
+  EXPECT_EQ(manager_.uv_key_state(/*platform_has_biometrics=*/true),
             EnclaveManager::UvKeyState::kUsesChromeUI);
 
   scoped_fake_keychain_.SetUVMethod(
       crypto::apple::ScopedFakeKeychainV2::UVMethod::kPasswordOnly);
-  EXPECT_EQ(manager_.uv_key_state(
-                EnclaveManager::PlatformUvSupport::kUvKeyButNoBiometrics),
+  EXPECT_EQ(manager_.uv_key_state(/*platform_has_biometrics=*/false),
             EnclaveManager::UvKeyState::kUsesSystemUI);
 }
 #endif  // BUILDFLAG(IS_MAC)
@@ -3139,8 +3131,7 @@ TEST_F(EnclaveUVTest, DeferredUVKeyCreation) {
   ASSERT_FALSE(manager_.is_idle());
   EXPECT_TRUE(add_future.Wait());
 
-  EXPECT_EQ(manager_.uv_key_state(
-                EnclaveManager::PlatformUvSupport::kUvKeyButNoBiometrics),
+  EXPECT_EQ(manager_.uv_key_state(/*platform_has_biometrics=*/false),
             EnclaveManager::UvKeyState::kUsesSystemUIDeferredCreation);
   const auto& user_state = manager_.local_state_for_testing()
                                .users()
@@ -3191,8 +3182,7 @@ TEST_F(EnclaveUVTest, UnregisterOnFailedDeferredUVKeyCreation) {
   ASSERT_FALSE(manager_.is_idle());
   EXPECT_TRUE(add_future.Wait());
 
-  EXPECT_EQ(manager_.uv_key_state(
-                EnclaveManager::PlatformUvSupport::kUvKeyButNoBiometrics),
+  EXPECT_EQ(manager_.uv_key_state(/*platform_has_biometrics=*/false),
             EnclaveManager::UvKeyState::kUsesSystemUIDeferredCreation);
   const auto& user_state = manager_.local_state_for_testing()
                                .users()
@@ -3262,8 +3252,7 @@ TEST_F(EnclaveUVTest, UnregisterOnMissingUserVerifyingKey) {
 
   base::RepeatingClosure quit_closure;
 
-  EXPECT_EQ(manager_.uv_key_state(
-                EnclaveManager::PlatformUvSupport::kUvKeyButNoBiometrics),
+  EXPECT_EQ(manager_.uv_key_state(/*platform_has_biometrics=*/false),
             EnclaveManager::UvKeyState::kUsesSystemUIDeferredCreation);
 
   // Generate a UV key and reset the deferred UV key flag, without sending a
--- chrome/browser/webauthn/gpm_enclave_controller.cc
+++ chrome/browser/webauthn/gpm_enclave_controller.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/task/sequenced_task_runner.h"
-#include "base/task/single_thread_task_runner.h"
 #include "base/time/default_tick_clock.h"
 #include "base/time/tick_clock.h"
 #include "base/time/time.h"
@@ -447,22 +446,6 @@ GPMEnclaveController::GPMEnclaveController(
     return;
   }
   SetActive(EnclaveEnabledStatus::kEnabled);
-  FIDO_LOG(EVENT) << "Checking for UV key capability";
-  base::SingleThreadTaskRunner::GetCurrentDefault()->PostTask(
-      FROM_HERE,
-      base::BindOnce(&EnclaveManager::AreUserVerifyingKeysSupported,
-                     base::BindOnce(&GPMEnclaveController::OnUVCapabilityKnown,
-                                    weak_ptr_factory_.GetWeakPtr())));
-}
-
-GPMEnclaveController::~GPMEnclaveController() {
-  // Ensure that any secret is dropped from memory after a transaction.
-  enclave_manager_->TakeSecret();
-}
-
-void GPMEnclaveController::OnUVCapabilityKnown(bool can_make_uv_keys) {
-  FIDO_LOG(EVENT) << "UV key capability: " << can_make_uv_keys;
-  can_make_uv_keys_ = can_make_uv_keys;
   if (enclave_manager_->IsLoaded()) {
     OnEnclaveLoaded();
   } else {
@@ -473,26 +456,22 @@ void GPMEnclaveController::OnUVCapabilityKnown(bool can_make_uv_keys) {
   }
 }
 
-EnclaveManager::PlatformUvSupport GPMEnclaveController::GetPlatformUvSupport() {
-  if (!can_make_uv_keys_) {
-    return EnclaveManager::PlatformUvSupport::kNoUvKey;
-  }
-  return model_->platform_has_biometrics.value_or(false)
-             ? EnclaveManager::PlatformUvSupport::kUvKeyWithBiometrics
-             : EnclaveManager::PlatformUvSupport::kUvKeyButNoBiometrics;
+GPMEnclaveController::~GPMEnclaveController() {
+  // Ensure that any secret is dropped from memory after a transaction.
+  enclave_manager_->TakeSecret();
 }
 
 std::optional<EnclaveUserVerificationMethod>
 GPMEnclaveController::GetEnclaveUserVerificationMethod() {
+  // TODO(crbug.com/393055190): Figure out why `ready_for_ui` is not enough for
+  // `IsReady`.
   if (!enclave_manager_->IsReady()) {
-    // We allow the UI to show before the controller had time to load the
-    // enclave and check for UV availability. In that case, we return nullopt to
-    // signal that we don't know the enclave user verification method.
     return std::nullopt;
   }
+
   bool has_pin = enclave_manager_->has_wrapped_pin();
-  EnclaveManager::UvKeyState uv_key_state =
-      enclave_manager_->uv_key_state(GetPlatformUvSupport());
+  EnclaveManager::UvKeyState uv_key_state = enclave_manager_->uv_key_state(
+      model_->platform_has_biometrics.value_or(false));
 
   return PickEnclaveUserVerificationMethod(
       user_verification_requirement_, /*have_entered_pin_for_recovery=*/false,
@@ -562,7 +541,7 @@ EnclaveUserVerificationMethod GPMEnclaveController::GetUvMethod() {
       user_verification_requirement_,
       have_added_device_ && !recovered_with_icloud_keychain_,
       enclave_manager_->has_wrapped_pin(),
-      enclave_manager_->uv_key_state(GetPlatformUvSupport()),
+      enclave_manager_->uv_key_state(*model_->platform_has_biometrics),
       *model_->platform_has_biometrics, BrowserIsApp());
   return *uv_method_;
 }
@@ -630,7 +609,7 @@ void GPMEnclaveController::OnEnclaveLoaded() {
           user_verification_requirement_,
           /*have_entered_pin_for_recovery=*/false,
           enclave_manager_->has_wrapped_pin(),
-          enclave_manager_->uv_key_state(GetPlatformUvSupport()),
+          enclave_manager_->uv_key_state(/*platform_has_biometrics=*/false),
           /*platform_has_biometrics=*/false, BrowserIsApp())) {
         case EnclaveUserVerificationMethod::kPIN:
         case EnclaveUserVerificationMethod::kUnsatisfiable:
@@ -647,6 +626,20 @@ void GPMEnclaveController::OnEnclaveLoaded() {
       }
     }
   }
+
+  FIDO_LOG(EVENT) << "Checking for UV key capability";
+  EnclaveManager::AreUserVerifyingKeysSupported(
+      base::BindOnce(&GPMEnclaveController::OnUVCapabilityKnown,
+                     weak_ptr_factory_.GetWeakPtr()));
+}
+
+void GPMEnclaveController::OnUVCapabilityKnown(bool can_make_uv_keys) {
+  FIDO_LOG(EVENT) << "UV key capability: " << can_make_uv_keys;
+  can_make_uv_keys_ = can_make_uv_keys;
+  DownloadAccountState();
+}
+
+void GPMEnclaveController::DownloadAccountState() {
   FIDO_LOG(EVENT) << "Fetching account state";
 
   auto* rfh = content::RenderFrameHost::FromID(render_frame_host_id_);
@@ -796,7 +789,7 @@ void GPMEnclaveController::OnKeysStored() {
   store_keys_lock_.reset();
 
   if ((pin_metadata_.has_value() && pin_metadata_->usable_pin_metadata) ||
-      GetPlatformUvSupport() != EnclaveManager::PlatformUvSupport::kNoUvKey) {
+      *can_make_uv_keys_) {
     // No need to create a GPM PIN if the user already has a usable GPM PIN or
     // can make UV keys.
     if (!enclave_manager_->AddDeviceToAccount(
@@ -950,7 +943,7 @@ void GPMEnclaveController::OnEnclaveAccountSetUpComplete() {
       user_verification_requirement_,
       have_added_device_ && !recovered_with_icloud_keychain_,
       enclave_manager_->has_wrapped_pin(),
-      enclave_manager_->uv_key_state(GetPlatformUvSupport()),
+      enclave_manager_->uv_key_state(*model_->platform_has_biometrics),
       *model_->platform_has_biometrics, BrowserIsApp());
   switch (*uv_method_) {
     case EnclaveUserVerificationMethod::kUVKeyWithSystemUI:
@@ -1071,7 +1064,7 @@ void GPMEnclaveController::OnGPMCreationSelected() {
           user_verification_requirement_,
           have_added_device_ && !recovered_with_icloud_keychain_,
           enclave_manager_->has_wrapped_pin(),
-          enclave_manager_->uv_key_state(GetPlatformUvSupport()),
+          enclave_manager_->uv_key_state(*model_->platform_has_biometrics),
           *model_->platform_has_biometrics, BrowserIsApp());
 
       switch (*uv_method_) {
@@ -1146,7 +1139,7 @@ void GPMEnclaveController::OnGPMPasskeySelected(
           user_verification_requirement_,
           have_added_device_ && !recovered_with_icloud_keychain_,
           enclave_manager_->has_wrapped_pin(),
-          enclave_manager_->uv_key_state(GetPlatformUvSupport()),
+          enclave_manager_->uv_key_state(*model_->platform_has_biometrics),
           *model_->platform_has_biometrics, BrowserIsApp());
 
       switch (*uv_method_) {
