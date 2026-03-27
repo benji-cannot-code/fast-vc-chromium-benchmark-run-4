82ae63d1e2447eb420a8682afcb349c807d184ce
S. Ganesh <ganesh@chromium.org>
2026-03-27 13:10:03 -0700

    updater: experimental WebView2 UI
    This experimental UI is a work in progress.
    
    Bug: 409590312
    Change-Id: I88185b454674d291a78283f77eb09797fba8b453
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7690312
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1606425}

--- chrome/updater/app/app_install_win.cc
+++ chrome/updater/app/app_install_win.cc
@@ -71,7 +71,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/updater/win/ui/progress_wnd.h"
 #include "chrome/updater/win/ui/resources/resources.grh"
 #include "chrome/updater/win/ui/resources/updater_installer_strings.h"
-#include "chrome/updater/win/ui/webview_progress_wnd.h"
+#include "chrome/updater/win/ui/webview2_progress_wnd.h"
 #include "chrome/updater/win/win_constants.h"
 #include "components/update_client/update_client_errors.h"
 #include "url/gurl.h"
@@ -899,7 +899,7 @@ void AppInstallControllerImpl::InitializeUI() {
   observer_.reset(progress_wnd.release());
 
     if (base::CommandLine::ForCurrentProcess()->HasSwitch(kWebViewUISwitch)) {
-      auto wnd = std::make_unique<ui::WebviewProgressWnd>();
+      auto wnd = std::make_unique<ui::WebView2ProgressWnd>();
       VISIT_PROGRESS_WND(wnd);
       return;
     }
