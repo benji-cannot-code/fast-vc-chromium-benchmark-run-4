a32c8e3fc9260a6968b868969fa72da73ad3df8a
Hira Mahmood <hiramahmood@google.com>
2026-03-27 13:58:29 -0700

    [iOS] Fix UAF in OverlayDispatchCallback
    Previously, if a dispatched callback destroyed the OverlayRequest (and
    manager), the loop in DispatchResponse() would continue iterating over
    the freed std::vector, triggering a crash. This patch adds a
    base::WeakPtr tripwire to DispatchResponse(). After executing a
    callback, we immediately check if the manager was destroyed, and exit
    the iteration if needed.
    
    Bug: 496416288
    Change-Id: I6ea1e01a5286edc621b6547176026510b3c16d74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7708375
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1606461}

--- ios/chrome/browser/overlays/model/overlay_callback_manager_impl.h
+++ ios/chrome/browser/overlays/model/overlay_callback_manager_impl.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "base/memory/weak_ptr.h"
 #include "ios/chrome/browser/overlays/model/public/overlay_callback_manager.h"
 #include "ios/chrome/browser/overlays/model/public/overlay_response.h"
 
@@ -33,6 +34,7 @@ class OverlayCallbackManagerImpl : public OverlayCallbackManager {
   std::unique_ptr<OverlayResponse> completion_response_;
   std::vector<OverlayCompletionCallback> completion_callbacks_;
   std::vector<OverlayDispatchCallback> dispatch_callbacks_;
+  base::WeakPtrFactory<OverlayCallbackManagerImpl> weak_ptr_factory_{this};
 };
 
 #endif  // IOS_CHROME_BROWSER_OVERLAYS_MODEL_OVERLAY_CALLBACK_MANAGER_IMPL_H_
