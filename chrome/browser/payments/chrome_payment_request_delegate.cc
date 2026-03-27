6c9cda20571cddbc9822027d6a9e16e5aa3b3c7d
keybo <keybo@google.com>
2026-03-26 23:32:22 -0700

    Modularize //chrome/browser/payments into separate build targets
    This CL extracts the source files for //chrome/browser/payments from the
    monolithic //chrome/browser target into independent GN source sets
    (`payments` and `impl`). It also extracts the Android-specific code into
    `//chrome/browser/payments/android:service_worker_payment_app_bridge`,
    resolving an existing TODO.
    
    Additionally, this change replaces some #includes in
    `chrome_payment_request_delegate.h` with forward declarations to reduce
    build dependencies. Missing includes that were previously provided
    transitively have been explicitly added to the respective .cc and test
    files.
    
    Bug: 496537636
    Change-Id: Icd1dafb2ec2ec4ebc0eb32631bc753309c3e9ee2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7705453
    Auto-Submit: Keybo Qian <keybo@google.com>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1605989}

--- chrome/browser/payments/chrome_payment_request_delegate.cc
+++ chrome/browser/payments/chrome_payment_request_delegate.cc
@@ -34,6 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/keyed_service/core/service_access_type.h"
 #include "components/payments/content/payment_request.h"
 #include "components/payments/content/payment_request_dialog.h"
+#include "components/payments/content/secure_payment_confirmation_controller.h"
+#include "components/payments/content/secure_payment_confirmation_no_creds.h"
 #include "components/payments/content/ssl_validity_checker.h"
 #include "components/payments/content/web_payments_web_data_service.h"
 #include "components/payments/core/payment_prefs.h"
