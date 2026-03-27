58a43729fdeca31b7b9fd4b4d361ed9c9533ec0b
Dale Curtis <dalecurtis@chromium.org>
2026-03-26 18:33:13 -0700

    Remove a bunch of old DecoderStreamAdapter remnants
    We stopped work on DecoderStream for WebRTC some time ago, so none
    of these methods are reachable anymore.
    
    R=jophba
    
    Bug: 40167137
    
    Change-Id: I8eb249d0a4a97d933350a5644f537343b7272e39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7706618
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Jordan Bayles <jophba@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1605912}

--- media/base/supported_video_decoder_config.h
+++ media/base/supported_video_decoder_config.h
@@ -16,12 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
-// The min and max resolution used by SW decoders (dav1d, libgav1, libvpx and
-// ffmpeg for example) when queried about decoding capabilities. For now match
-// the supported resolutions of HW decoders.
-constexpr gfx::Size kDefaultSwDecodeSizeMin(8, 8);
-constexpr gfx::Size kDefaultSwDecodeSizeMax(8192, 8192);
-
 // Specification of a range of configurations that are supported by a video
 // decoder.  Also provides the ability to check if a VideoDecoderConfig matches
 // the supported range.
--- media/mojo/clients/mojo_gpu_video_accelerator_factories_unittest.cc
+++ media/mojo/clients/mojo_gpu_video_accelerator_factories_unittest.cc
@@ -72,6 +72,9 @@ constexpr gfx::Size kCodedSize(320, 240);
 constexpr gfx::Rect kVisibleRect(320, 240);
 constexpr gfx::Size kNaturalSize(320, 240);
 
+constexpr gfx::Size kMinDecoderTestSize(8, 8);
+constexpr gfx::Size kMaxDecoderTestSize(8192, 8192);
+
 constexpr auto kGpuStreamPriorityDefault = gpu::SchedulingPriority::kNormal;
 constexpr int kGpuStreamIdDefault = 0;
 
@@ -79,8 +82,8 @@ const media::SupportedVideoDecoderConfig kH264MaxSupportedVideoDecoderConfig =
     media::SupportedVideoDecoderConfig(
         media::VideoCodecProfile::H264PROFILE_MIN,
         media::VideoCodecProfile::H264PROFILE_MAX,
-        media::kDefaultSwDecodeSizeMin,
-        media::kDefaultSwDecodeSizeMax,
+        kMinDecoderTestSize,
+        kMaxDecoderTestSize,
         true,
         false);
 
@@ -113,8 +116,8 @@ const media::SupportedVideoDecoderConfig kH265MaxSupportedVideoDecoderConfig =
     media::SupportedVideoDecoderConfig(
         media::VideoCodecProfile::HEVCPROFILE_MIN,
         media::VideoCodecProfile::HEVCPROFILE_MAX,
-        media::kDefaultSwDecodeSizeMin,
-        media::kDefaultSwDecodeSizeMax,
+        kMinDecoderTestSize,
+        kMaxDecoderTestSize,
         true,
         false);
 #endif
