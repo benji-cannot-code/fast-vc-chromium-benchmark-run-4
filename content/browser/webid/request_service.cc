a1957e56f8f7f48ed139658c9f94944467d7fe2d
Christian Biesinger <cbiesinger@chromium.org>
2026-03-27 10:55:02 -0700

    [FedCM] Make sure iterator is valid when autofill is accepted
    If a FedCM request is aborted before the user accepts the
    suggestion, the IDP will not be found in the map and, so
    this CL checks for that.
    
    Fixed: 494764370
    Change-Id: I4ca99eb58f3bc3e5879d9b0065d28bea628fe4d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7705015
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1606349}

--- content/browser/webid/request_service.cc
+++ content/browser/webid/request_service.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base64url.h"
 #include "base/command_line.h"
 #include "base/containers/span.h"
+#include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/functional/callback_helpers.h"
 #include "base/json/json_reader.h"
@@ -1373,6 +1374,13 @@ void RequestService::NotifyAutofillSuggestionAccepted(
   // approved_clients array). We should figure out how to reconcile these two
   // modes.
   auto get_info_it = token_request_get_infos_.find(idp);
+  if (get_info_it == token_request_get_infos_.end()) {
+    base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
+        FROM_HERE,
+        base::BindOnce(std::move(token_received_callback_for_autofill_),
+                       false));
+    return;
+  }
 
   // TODO(crbug.com/412640661): Currently, in order to skip the account chooser
   // and go straight to the disclosure UI, we have to call ShowLoadingDialog()
