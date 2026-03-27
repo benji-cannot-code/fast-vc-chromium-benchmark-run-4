7094fed9a5fcc51f73c15e9f653e2a5a9cfa2d22
Arthur Sonzogni <arthursonzogni@chromium.org>
2026-03-27 03:31:15 -0700

    Move theme and background color IPCs to LocalMainFrameHost
    The DidChangeThemeColor and DidChangeBackgroundColor IPCs in
    LocalFrameHost were only intended for main frames but were accessible to
    subframes. This could allow a compromised subframe to trigger a CHECK in
    the browser process.
    
    This CL moves these IPCs to the LocalMainFrameHost interface, which is
    exclusively bound to main frames. By leveraging Mojo interface
    boundaries, this architectural change makes it structurally impossible
    for a compromised subframe to bind to the interface and spoof these
    messages.
    
    Fixed: 495329387
    Change-Id: Ie6607cb49da2f1377d61f2275d8a9efdea2518cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7698392
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1606072}

--- content/browser/renderer_host/render_frame_host_impl.h
+++ content/browser/renderer_host/render_frame_host_impl.h
@@ -2482,9 +2482,6 @@ class CONTENT_EXPORT RenderFrameHostImpl
   void SetNeedsOcclusionTracking(bool needs_tracking) override;
   void SetVirtualKeyboardMode(ui::mojom::VirtualKeyboardMode mode) override;
   void VisibilityChanged(blink::mojom::FrameVisibility) override;
-  void DidChangeThemeColor(std::optional<SkColor> theme_color) override;
-  void DidChangeBackgroundColor(const SkColor4f& background_color,
-                                bool color_adjust) override;
   void DidFailLoadWithError(const GURL& url, int32_t error_code) override;
   void DidFocusFrame() override;
   void DidCallFocus() override;
@@ -2683,6 +2680,9 @@ class CONTENT_EXPORT RenderFrameHostImpl
                      SetWindowRectCallback callback) override;
   void DidFirstVisuallyNonEmptyPaint() override;
   void DidAccessInitialMainDocument() override;
+  void DidChangeThemeColor(std::optional<SkColor> theme_color) override;
+  void DidChangeBackgroundColor(const SkColor4f& background_color,
+                                bool color_adjust) override;
 #if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
   void Minimize() override;
   void Maximize() override;
