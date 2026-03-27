e0849d0d36a5a8e975aa6b66bb45ae0d1dfabe96
Peter Kotwicz <pkotwicz@chromium.org>
2026-03-27 08:58:42 -0700

    Move security-related settings to SecuritySettingsProvider.
    This moves the recording of the following histograms to SecuritySettingsProvider:
    - enableBlockV8OptimizerOnUnfamiliarSites
    - enableHashPrefixRealTimeLookups
    - extendedReportingRemovePrefDependency
    - hashPrefixRealTimeLookupsSamplePing
    
    Bug: 458521865
    Change-Id: Ia8af0ca0ede96ef753d8cf1c06c8363f625773b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7697613
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1606259}

--- chrome/browser/ui/webui/settings/settings_ui.cc
+++ chrome/browser/ui/webui/settings/settings_ui.cc
@@ -123,7 +123,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/privacy_sandbox/privacy_sandbox_features.h"
 #include "components/regional_capabilities/regional_capabilities_service.h"
 #include "components/safe_browsing/core/common/features.h"
-#include "components/safe_browsing/core/common/hashprefix_realtime/hash_realtime_utils.h"
 #include "components/search_engines/search_engines_switches.h"
 #include "components/search_engines/template_url_service.h"
 #include "components/signin/public/base/signin_pref_names.h"
@@ -365,10 +364,6 @@ SettingsUI::SettingsUI(content::WebUI* web_ui)
                           base::FeatureList::IsEnabled(
                               features::kSensorsAllowAskBlockPermissionModel));
 
-  html_source->AddBoolean("enableHashPrefixRealTimeLookups",
-                          safe_browsing::hash_realtime_utils::
-                              IsHashRealTimeLookupEligibleInSession());
-
   html_source->AddBoolean(
       "enableKeyboardLockPrompt",
       base::FeatureList::IsEnabled(permissions::features::kKeyboardLockPrompt));
@@ -412,26 +407,11 @@ SettingsUI::SettingsUI(content::WebUI* web_ui)
                           download_bubble_controlled_by_pref);
 
   html_source->AddBoolean(
-      "extendedReportingRemovePrefDependency",
-      base::FeatureList::IsEnabled(
-          safe_browsing::kExtendedReportingRemovePrefDependency));
-
-  html_source->AddBoolean(
-      "hashPrefixRealTimeLookupsSamplePing",
-      base::FeatureList::IsEnabled(
-          safe_browsing::kHashPrefixRealTimeLookupsSamplePing));
-
-  html_source->AddBoolean(
       "shouldShowPayOverTimeSettings",
       autofill::ContentAutofillClient::FromWebContents(web_ui->GetWebContents())
           ->GetPaymentsAutofillClient()
           ->GetPaymentsDataManager()
           .ShouldShowBnplSettings());
-
-  html_source->AddBoolean("enableBlockV8OptimizerOnUnfamiliarSites",
-                          base::FeatureList::IsEnabled(
-                              content_settings::features::
-                                  kBlockV8OptimizerOnUnfamiliarSitesSetting));
 
   html_source->AddBoolean("enableYourSavedInfoSettingsPage",
                           base::FeatureList::IsEnabled(
