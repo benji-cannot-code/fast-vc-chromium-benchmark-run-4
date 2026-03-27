2ee669aa99e4ef199739f356540e46a1b1a307e2
Pier-Alexandre Bouchard <pabouchard@google.com>
2026-03-27 09:41:25 -0700

    [iOS] Fix accessibility focus when dismissing secondary Home Customization sheets
    When a secondary sheet presented over the main "Customize homepage"
    sheet is dismissed, the main "Customize homepage" sheet remained
    inaccessible to VoiceOver.
    
    This CL fixes the issue by resetting `accessibilityElementsHidden = NO`
    when returning to the base sheet.
    
    Fixed: 479590152
    Change-Id: I46b8a58449c0020aa6d6c30890e6a6d8ba008a99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7704140
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Pier-Alexandre Bouchard <pabouchard@google.com>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1606300}

--- ios/chrome/browser/home_customization/coordinator/home_customization_coordinator.mm
+++ ios/chrome/browser/home_customization/coordinator/home_customization_coordinator.mm
@@ -449,6 +449,7 @@ CGFloat const kSheetCornerRadius = 30;
 
     // The presenting page should become interactable for voiceover.
     self.currentPageViewController.view.accessibilityViewIsModal = YES;
+    self.currentPageViewController.view.accessibilityElementsHidden = NO;
   }
 }
 
