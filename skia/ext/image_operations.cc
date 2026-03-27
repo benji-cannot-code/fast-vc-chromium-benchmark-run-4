ccdac302fa1ff7670b9a6febe7496116512246a4
Alexei Svitkine <asvitkine@chromium.org>
2026-03-27 04:42:49 -0700

    Cleanup: Remove histogram macros header includes in skia
    Bug: None
    Change-Id: Ia3e454e47356fec339e7d5b747d9ae0462803843
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7701719
    Auto-Submit: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Eric Boren <borenet@google.com>
    Commit-Queue: Eric Boren <borenet@google.com>
    Cr-Commit-Position: refs/heads/main@{#1606109}

--- skia/ext/image_operations.cc
+++ skia/ext/image_operations.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "skia/ext/image_operations.h"
 
 #include "base/check.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/notreached.h"
 #include "base/time/time.h"
 #include "base/trace_event/trace_event.h"
