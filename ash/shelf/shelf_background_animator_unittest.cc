1c2b44cb427977731f9b0d6e210b51e75626ca2e
hiroki oshima <hiroki.oshima@gmail.com>
2026-03-27 06:21:18 -0700

    fix ShelfBackgroundAnimatorTestApi and ShelfBackgroundAnimatorTest dangling pointers
    Bug: 40212619
    Change-Id: I72308431ea904ce45d4fcae512be448fb34a1e44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7695363
    Commit-Queue: Di Wu <diwux@google.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Di Wu <diwux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1606161}

--- ash/shelf/shelf_background_animator_unittest.cc
+++ ash/shelf/shelf_background_animator_unittest.cc
@@ -93,7 +93,7 @@ class ShelfBackgroundAnimatorTestApi {
   ShelfBackgroundAnimatorTestApi& operator=(
       const ShelfBackgroundAnimatorTestApi&) = delete;
 
-  ~ShelfBackgroundAnimatorTestApi() = default;
+  ~ShelfBackgroundAnimatorTestApi() { animator_ = nullptr; }
 
   ShelfBackgroundType previous_background_type() const {
     return animator_->previous_background_type_;
@@ -107,7 +107,7 @@ class ShelfBackgroundAnimatorTestApi {
 
  private:
   // The instance to provide internal access to.
-  raw_ptr<ShelfBackgroundAnimator, DanglingUntriaged> animator_;
+  raw_ptr<ShelfBackgroundAnimator> animator_;
 };
 
 class ShelfBackgroundAnimatorTest : public AshTestBase {
@@ -122,6 +122,12 @@ class ShelfBackgroundAnimatorTest : public AshTestBase {
 
   // testing::Test:
   void SetUp() override;
+
+  void TearDown() override {
+    animator_ = nullptr;
+    test_api_.reset();
+    AshTestBase::TearDown();
+  }
 
  protected:
   // Convenience wrapper for |animator_|'s PaintBackground().
@@ -138,7 +144,7 @@ class ShelfBackgroundAnimatorTest : public AshTestBase {
   TestShelfBackgroundObserver observer_;
 
   // Test target.
-  raw_ptr<ShelfBackgroundAnimator, DanglingUntriaged> animator_;
+  raw_ptr<ShelfBackgroundAnimator> animator_;
 
   // Provides internal access to |animator_|.
   std::unique_ptr<ShelfBackgroundAnimatorTestApi> test_api_;
