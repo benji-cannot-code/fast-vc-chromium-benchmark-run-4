21804a9aec9efe214bd29839b6d7fb624ac90762
Lukasz Anforowicz <lukasza@chromium.org>
2026-03-27 03:44:26 -0700

    [rust] Move logging-related Rust targets into `//base/logging`.
    This CL moves two logging-related Rust targets from `//base/BUILD.gn`
    into a new `//base/logging/BUILD.gn`:
    
    * `//base:rust_logger` becomes `//base/logging:rust_logger`
    * `//base:logging_log_severity_bindgen` becomes
      `//base/logging:log_severity_bindgen`.
    
    Bug: 495537792
    Change-Id: Id9c6cbafeced8a6ab48faffe992e137c70d109ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7694977
    Commit-Queue: Etienne Dechamps <edechamps@google.com>
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Etienne Dechamps <edechamps@google.com>
    Cr-Commit-Position: refs/heads/main@{#1606079}

--- base/logging/logger.rs
+++ base/logging/logger.rs
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 chromium::import! {
-    "//base:logging_log_severity_bindgen" as log_severity;
+    "//base/logging:log_severity_bindgen" as log_severity;
 }
 
 use log::Level::{Debug, Error, Info, Trace, Warn};
