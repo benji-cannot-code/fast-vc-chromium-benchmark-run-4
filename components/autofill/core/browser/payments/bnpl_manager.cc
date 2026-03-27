8aaf02285d036d078ba2c8adffe1209cc94ff75c
Yishui Liu <yishuil@google.com>
2026-03-27 09:25:51 -0700

    [BNPL][PNPL] Show Progress suggestion on issuer acception
    Show a progress suggestion in the pay later tab for first time and
    second time users after an issuer is accepted while waiting for amount
    extraction or server calls.
    
    Bug: 477689220
    Change-Id: Id965ab5ae18250ffbbcf98365540f4c993af2333
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7705260
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Commit-Queue: Yishui Liu <yishuil@google.com>
    Cr-Commit-Position: refs/heads/main@{#1606290}

--- components/autofill/core/browser/payments/bnpl_manager.cc
+++ components/autofill/core/browser/payments/bnpl_manager.cc
@@ -213,6 +213,11 @@ void BnplManager::OnUserDecisionToUseBnpl(
 void BnplManager::OnIssuerAccepted(BnplIssuer issuer) {
   ongoing_flow_state_->issuer = std::move(issuer);
 
+  if (base::FeatureList::IsEnabled(
+          features::kAutofillEnablePayNowPayLaterTabs)) {
+    ShowProgressUiForPayLaterTab();
+  }
+
   // When an issuer is accepted but no checkout amount is present, call
   // server-side AI to extract the amount.
   if (base::FeatureList::IsEnabled(
@@ -220,10 +225,6 @@ void BnplManager::OnIssuerAccepted(BnplIssuer issuer) {
       !ongoing_flow_state_->final_checkout_amount) {
     browser_autofill_manager_->GetAmountExtractionManager()
         .TriggerCheckoutAmountExtractionWithAi();
-    if (base::FeatureList::IsEnabled(
-            features::kAutofillEnablePayNowPayLaterTabs)) {
-      ShowProgressUiForPayLaterTab();
-    }
     return;
   }
 
