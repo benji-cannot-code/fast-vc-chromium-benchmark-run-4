986f9b375b8d6364b1b7e1f4b450e358f3f8a6f0
Olivia Saul <osaul@google.com>
2026-03-27 14:44:34 -0700

    [AF] Update Desktop post-save iOS promo to v2 Wallet branding
    Before: https://screenshot.googleplex.com/BBQShmsWxXnk6TT.png
    After: https://screenshot.googleplex.com/8DSe4wo82KJJfVh.png
    
    This bubble is not owned by the Autofill team, so there's no Mock for
    this particular change.
    
    Bug: 468139053
    Change-Id: I81c0b47895c1a0b7b9f5d842b93fd09dff6cbfd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7705404
    Commit-Queue: Olivia Saul <osaul@google.com>
    Reviewed-by: Fernando Ramirez <ferny@google.com>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1606492}

--- chrome/browser/ui/views/promos/ios_promo_bubble.cc
+++ chrome/browser/ui/views/promos/ios_promo_bubble.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/branded_strings.h"
 #include "chrome/grit/generated_resources.h"
 #include "chrome/grit/theme_resources.h"
+#include "components/autofill/core/common/autofill_payments_features.h"
 #include "components/desktop_to_mobile_promos/features.h"
 #include "components/desktop_to_mobile_promos/promos_types.h"
 #include "components/feature_engagement/public/feature_constants.h"
@@ -153,7 +154,10 @@ IOSPromoConstants::IOSPromoTypeConfigs SetUpPaymentBubble(
   config.bubble_title_id =
       IDS_AUTOFILL_SAVE_CARD_CONFIRMATION_SUCCESS_TITLE_TEXT;
   config.bubble_subtitle_id =
-      IDS_AUTOFILL_SAVE_CARD_CONFIRMATION_SUCCESS_DESCRIPTION_TEXT;
+      base::FeatureList::IsEnabled(
+          autofill::features::kAutofillEnableWalletBrandingV2)
+          ? IDS_AUTOFILL_SAVE_CARD_TO_WALLET_CONFIRMATION_SUCCESS_DESCRIPTION_TEXT_V2
+          : IDS_AUTOFILL_SAVE_CARD_CONFIRMATION_SUCCESS_DESCRIPTION_TEXT;
   config.promo_title_id = IDS_IOS_DESKTOP_PAYMENT_PROMO_BUBBLE_FOOTER_TITLE;
   config.promo_description_id =
       IDS_IOS_DESKTOP_PAYMENT_PROMO_BUBBLE_FOOTER_DESCRIPTION_QR;
