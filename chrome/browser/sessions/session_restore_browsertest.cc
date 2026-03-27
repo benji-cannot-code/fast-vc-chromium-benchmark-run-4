7a3e3787bef507c485997dd83bcd3ea4bacbe3f0
Eriko Kurimoto <elkurin@chromium.org>
2026-03-26 17:54:58 -0700

    Reland "WaaP: Fix SessionRestoreTest with initial webui"
    This is a reland of commit 97c08672481a1a1d3b96442316918a2743243c05
    
    This CL excluded `WaitUntilBrowserBecomeActive` which caused the
    flakiness in Mac. It turns out that this method was not needed if we
    simply get the new browser.
    
    The diff from the original change can be checked by comparing with the
    patchset "base".
    
    Original change's description:
    > WaaP: Fix SessionRestoreTest with initial webui
    >
    > When InitialWebUI is enabled, new window visibility is deferred until
    > the WebUI loads. In tests, this causes SessionRestore to see a hidden
    > window and incorrectly spawn a second window for restored tabs instead
    > of reusing the existing one.
    >
    > This CL updates the `QuitBrowserAndRestore` test helper to manually call
    > `InitialWebUIManager::OnWebUIToolbarLoaded()`. This forces the window to
    > show immediately, fixing failures in tests like
    > `RestoreWithNavigateSelectedTab`.
    >
    > Bug: 477426026
    > Change-Id: I9d1509f3140b19e1f98cf119a811334e4525c592
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7671986
    > Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    > Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1601709}
    
    Bug: 477426026
    Change-Id: Ic6cc2a19cf845ce63bf1d47b882d2eaae52eebf1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7702936
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1605904}

--- chrome/browser/sessions/session_restore_browsertest.cc
+++ chrome/browser/sessions/session_restore_browsertest.cc
@@ -79,6 +79,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/tab_group_model.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/ui_features.h"
+#include "chrome/browser/ui/waap/initial_web_ui_manager.h"
 #include "chrome/browser/ui/web_applications/app_browser_controller.h"
 #include "chrome/browser/ui/web_applications/test/web_app_browsertest_util.h"
 #include "chrome/browser/web_applications/model/display_override.h"
@@ -373,6 +374,12 @@ class SessionRestoreTest : public InProcessBrowserTest {
     } else {
       NavigateParams params(profile, url, ui::PAGE_TRANSITION_LINK);
       Navigate(&params);
+      if (params.browser) {
+        if (auto* manager = InitialWebUIManager::From(
+                params.browser->GetBrowserForMigrationOnly())) {
+          manager->OnWebUIToolbarLoaded();
+        }
+      }
     }
 
     Browser* new_browser = chrome::FindBrowserWithTab(tab_waiter.Wait());
