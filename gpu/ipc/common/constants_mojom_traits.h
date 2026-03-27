3f186cdff70ec8d0aafc0e431511ce93a826169e
Jan Wilken Dörrie <jdoerrie@chromium.org>
2026-03-27 09:49:00 -0700

    [mojo] Modernize EnumTraits in gpu/
    Bug: 483092898
    Change-Id: I491a5d7d19f6615675c0bc8fd5283b1a6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7698954
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1606305}

--- gpu/ipc/common/constants_mojom_traits.h
+++ gpu/ipc/common/constants_mojom_traits.h
@@ -14,7 +14,7 @@ namespace mojo {
 template <>
 struct EnumTraits<gpu::mojom::Error, gpu::error::Error> {
   static gpu::mojom::Error ToMojom(gpu::error::Error error);
-  static bool FromMojom(gpu::mojom::Error input, gpu::error::Error* out);
+  static gpu::error::Error FromMojom(gpu::mojom::Error input);
 };
 
 template <>
@@ -22,8 +22,8 @@ struct EnumTraits<gpu::mojom::ContextLostReason,
                   gpu::error::ContextLostReason> {
   static gpu::mojom::ContextLostReason ToMojom(
       gpu::error::ContextLostReason reason);
-  static bool FromMojom(gpu::mojom::ContextLostReason input,
-                        gpu::error::ContextLostReason* out);
+  static gpu::error::ContextLostReason FromMojom(
+      gpu::mojom::ContextLostReason input);
 };
 
 template <>
@@ -31,8 +31,8 @@ struct EnumTraits<gpu::mojom::CommandBufferNamespace,
                   gpu::CommandBufferNamespace> {
   static gpu::mojom::CommandBufferNamespace ToMojom(
       gpu::CommandBufferNamespace namespace_id);
-  static bool FromMojom(gpu::mojom::CommandBufferNamespace input,
-                        gpu::CommandBufferNamespace* out);
+  static gpu::CommandBufferNamespace FromMojom(
+      gpu::mojom::CommandBufferNamespace input);
 };
 
 }  // namespace mojo
