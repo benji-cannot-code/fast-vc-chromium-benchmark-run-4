0b4b2c16a7364ac996ac06c395f86bcbf70dd89b
Ramin Halavati <rhalavati@chromium.org>
2026-03-27 09:50:14 -0700

    android-a11y: Remove SET_EXTENDED_SELECTION action from tree dump tests.
    `ACTION_SET_EXTENDED_SELECTION` will be supported by all nodes, see
    http://crrev.com/c/7698855, which makess adding this action to tree dump
    tests useless.
    
    AX-Relnotes: n/a
    Bug: 443078007
    Change-Id: I33e8bafbc12ad15b02d843b7aa91b14481ad683a
    Cq-Include-Trybots: luci.chromium.try:android-16-x64-dbg,android-16-x64-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7706636
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1606306}

--- content/public/android/java/src/org/chromium/content/browser/accessibility/AccessibilityNodeInfoBuilder.java
+++ content/public/android/java/src/org/chromium/content/browser/accessibility/AccessibilityNodeInfoBuilder.java
@@ -31,7 +31,6 @@ import static androidx.core.view.accessibility.AccessibilityNodeInfoCompat.Acces
 import static androidx.core.view.accessibility.AccessibilityNodeInfoCompat.AccessibilityActionCompat.ACTION_SCROLL_LEFT;
 import static androidx.core.view.accessibility.AccessibilityNodeInfoCompat.AccessibilityActionCompat.ACTION_SCROLL_RIGHT;
 import static androidx.core.view.accessibility.AccessibilityNodeInfoCompat.AccessibilityActionCompat.ACTION_SCROLL_UP;
-import static androidx.core.view.accessibility.AccessibilityNodeInfoCompat.AccessibilityActionCompat.ACTION_SET_EXTENDED_SELECTION;
 import static androidx.core.view.accessibility.AccessibilityNodeInfoCompat.AccessibilityActionCompat.ACTION_SET_PROGRESS;
 import static androidx.core.view.accessibility.AccessibilityNodeInfoCompat.AccessibilityActionCompat.ACTION_SET_SELECTION;
 import static androidx.core.view.accessibility.AccessibilityNodeInfoCompat.AccessibilityActionCompat.ACTION_SET_TEXT;
@@ -266,7 +265,6 @@ public class AccessibilityNodeInfoBuilder {
     private void addAccessibilityNodeInfoActions(
             AccessibilityNodeInfoCompat node,
             int virtualViewId,
-            boolean canSetExtendedSelection,
             boolean canScrollForward,
             boolean canScrollBackward,
             boolean canScrollUp,
@@ -308,9 +306,6 @@ public class AccessibilityNodeInfoBuilder {
                 }
                 node.addAction(ACTION_COPY);
             }
-        }
-        if (canSetExtendedSelection) {
-            node.addAction(ACTION_SET_EXTENDED_SELECTION);
         }
 
         if (canScrollForward) {
