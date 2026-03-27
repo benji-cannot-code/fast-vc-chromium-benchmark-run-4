1116661c32f28a1f3c4d09106b6d2bd18a1c5c06
Jan Wilken Dörrie <jdoerrie@chromium.org>
2026-03-27 09:47:47 -0700

    [mojo] Allow optional returns for all enums
    Update the Mojo C++ bindings generator to support optional return types
    for EnumTraits::FromMojom, even when the conversion is infallible.
    Previously, the generator enforced a direct return of the converted type
    for such enums.
    
    This change allows traits to return std::nullopt for invalid inputs,
    allowing Mojo to handle the failure as a validation error instead of a
    crash.
    
    The Skia AlphaType and ColorType traits are migrated to this new
    signature to resolve a fuzzer-detected CHECK failure. Unknown or
    unsupported values now result in a null return rather than reaching
    unreachable code paths.
    
    Bug: 483092898, 496356549
    Change-Id: I6ba430851ca3816aa7166d00fd91f4f36a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7703775
    Auto-Submit: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1606303}

--- skia/public/mojom/image_info_mojom_traits.cc
+++ skia/public/mojom/image_info_mojom_traits.cc
@@ -76,7 +76,8 @@ skia::mojom::AlphaType EnumTraits<skia::mojom::AlphaType, SkAlphaType>::ToMojom(
 }
 
 // static
-SkAlphaType EnumTraits<skia::mojom::AlphaType, SkAlphaType>::FromMojom(
+std::optional<SkAlphaType>
+EnumTraits<skia::mojom::AlphaType, SkAlphaType>::FromMojom(
     skia::mojom::AlphaType in) {
   switch (in) {
     case skia::mojom::AlphaType::ALPHA_TYPE_OPAQUE:
@@ -87,9 +88,9 @@ SkAlphaType EnumTraits<skia::mojom::AlphaType, SkAlphaType>::FromMojom(
       return kUnpremul_SkAlphaType;
     case skia::mojom::AlphaType::UNKNOWN:
       // Unknown types should not be sent over mojo.
-      NOTREACHED();
+      break;
   }
-  NOTREACHED();
+  return std::nullopt;
 }
 
 // static
@@ -118,7 +119,8 @@ skia::mojom::ColorType EnumTraits<skia::mojom::ColorType, SkColorType>::ToMojom(
 }
 
 // static
-SkColorType EnumTraits<skia::mojom::ColorType, SkColorType>::FromMojom(
+std::optional<SkColorType>
+EnumTraits<skia::mojom::ColorType, SkColorType>::FromMojom(
     skia::mojom::ColorType in) {
   switch (in) {
     case skia::mojom::ColorType::ALPHA_8:
@@ -138,7 +140,7 @@ SkColorType EnumTraits<skia::mojom::ColorType, SkColorType>::FromMojom(
       // UNKNOWN or unsupported values should not be sent over mojo.
       break;
   }
-  NOTREACHED();
+  return std::nullopt;
 }
 
 // static
