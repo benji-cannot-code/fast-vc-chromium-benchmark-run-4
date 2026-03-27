87d5b6f578e16fd7b82d2c61b4cd1706cfd740fb
Lauren Winston <lwinston@google.com>
2026-03-27 10:26:29 -0700

    Fix some issues with node_store and TSTextSegmentation.
    Bug: 486979604
    Change-Id: Ibc17f18a33a183bff594e99625af7c169078d573
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7706664
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1606335}

--- chrome/test/data/webui/side_panel/read_anything/node_store_test.ts
+++ chrome/test/data/webui/side_panel/read_anything/node_store_test.ts
@@ -24,7 +24,10 @@ suite('NodeStore', () => {
 
   function getReadAloudNode(axNodeId: number): ReadAloudNode {
     const domNode = nodeStore.getDomNode(axNodeId);
-    return ReadAloudNode.create(domNode!, nodeStore)!;
+    if (!domNode) {
+      return undefined as unknown as ReadAloudNode;
+    }
+    return ReadAloudNode.create(domNode, nodeStore)!;
   }
 
   function setDomNodes(axNodeIds: number[]) {
@@ -39,6 +42,7 @@ suite('NodeStore', () => {
     document.body.innerHTML = window.trustedTypes!.emptyHTML;
     BrowserProxy.setInstance(new TestColorUpdaterBrowserProxy());
     readingMode = new FakeReadingMode();
+    readingMode.isTsTextSegmentationEnabled = true;
     chrome.readingMode = readingMode as unknown as typeof chrome.readingMode;
     now = 0;
     Date.now = () => now;
@@ -124,17 +128,8 @@ suite('NodeStore', () => {
     assertEquals(replacer, children.item(0));
   });
 
-  test('hideImageNode', () => {
-    const id = 216;
-    nodeStore.hideImageNode(id);
-    assertFalse(areNodesAllHidden([id]));
-
-    setDomNodes([id]);
-    nodeStore.hideImageNode(id);
-    assertTrue(areNodesAllHidden([id]));
-  });
-
   test('areNodesAllHidden', () => {
+    readingMode.activeDistillationMethod = readingMode.distillationTypeScreen2x;
     const id1 = 216;
     const id2 = 218;
     const id3 = 219;
@@ -145,6 +140,27 @@ suite('NodeStore', () => {
     assertFalse(areNodesAllHidden([id1, id2, id3]));
     assertTrue(areNodesAllHidden([id1, id2]));
     assertFalse(areNodesAllHidden([id1, id3]));
+  });
+
+  test('areNodesAllHidden with Readability', () => {
+    readingMode.activeDistillationMethod =
+        readingMode.distillationTypeReadability;
+
+    const element = document.createElement('p');
+    element.innerText = 'Some text';
+    document.body.appendChild(element);
+    const readAloudNode = ReadAloudNode.create(element, nodeStore)!;
+
+    // Initially not hidden.
+    assertFalse(nodeStore.areNodesAllHidden([readAloudNode]));
+
+    // Hide the element.
+    element.style.display = 'none';
+    assertTrue(nodeStore.areNodesAllHidden([readAloudNode]));
+
+    // Show it again.
+    element.style.display = 'block';
+    assertFalse(nodeStore.areNodesAllHidden([readAloudNode]));
   });
 
   test('hasAnyNode', () => {
