11b181bf46ab4461c280fbda236169f7d04a5c76
Dominik Röttsches <drott@chromium.org>
2026-03-27 01:06:46 -0700

    Roll Fontations crates
    Contains fixes for Avar2 rendering, see [1].
    
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * font-types: 0.11.0 => 0.11.1; https://docs.rs/crate/font-types/0.11.1
    * read-fonts: 0.37.0 => 0.38.0; https://docs.rs/crate/read-fonts/0.38.0
    * skrifa: 0.40.0 => 0.41.0; https://docs.rs/crate/skrifa/0.41.0
    
    [1] https://github.com/googlefonts/fontations/issues/1767
    
    Binary-Size: Required library roll, can't disable or isolate features further.
    Bug: 40246300
    Change-Id: I8ba55dd3b22f3f0e9cfd131b585529a474504e5f
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7700473
    Auto-Submit: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1606011}

---
+++ third_party/rust/chromium_crates_io/vendor/read-fonts-v0_38/src/table_ref.rs
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+use std::ops::Range;
+
+/// Return the minimum range of the table bytes
+///
+/// This trait is implemented in generated code, and we use this to get the
+/// minimum length/bytes of a table.
+pub trait MinByteRange<'a> {
+    fn min_byte_range(&self) -> Range<usize>;
+    fn min_table_bytes(&self) -> &'a [u8];
+}
