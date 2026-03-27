770d178ac6d37da4ad9ee9a20f984764dc33df00
Gianmarco Picarella <picarella@google.com>
2026-03-27 04:14:56 -0700

    Revert "Make last name core and prefix types syncable"
    Reverting AutofillSupportLastNamePrefix as it was decided during the
    Autofill feature sync that its impact is too low for it to be launched.
    
    Bug: 386916943
    Change-Id: Icc1b1935931b9aa74f472095f5bbcafae5c6f9e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7687630
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Gianmarco Picarella <picarella@google.com>
    Cr-Commit-Position: refs/heads/main@{#1606098}

--- components/autofill/core/browser/webdata/addresses/autofill_profile_sync_util_unittest.cc
+++ components/autofill/core/browser/webdata/addresses/autofill_profile_sync_util_unittest.cc
@@ -50,18 +50,14 @@ AutofillProfile ConstructBaseProfile(
   profile.set_profile_label("profile_label");
 
   // Set testing values and statuses for the name.
-  profile.SetRawInfoWithVerificationStatus(NAME_FULL, u"John K. von Doe",
+  profile.SetRawInfoWithVerificationStatus(NAME_FULL, u"John K. Doe",
                                            VerificationStatus::kUserVerified);
   profile.SetRawInfoWithVerificationStatus(NAME_FIRST, u"John",
                                            VerificationStatus::kObserved);
   profile.SetRawInfoWithVerificationStatus(NAME_MIDDLE, u"K.",
                                            VerificationStatus::kObserved);
-  profile.SetRawInfoWithVerificationStatus(NAME_LAST, u"von Doe",
+  profile.SetRawInfoWithVerificationStatus(NAME_LAST, u"Doe",
                                            VerificationStatus::kFormatted);
-  profile.SetRawInfoWithVerificationStatus(NAME_LAST_PREFIX, u"von",
-                                           VerificationStatus::kParsed);
-  profile.SetRawInfoWithVerificationStatus(NAME_LAST_CORE, u"Doe",
-                                           VerificationStatus::kParsed);
   profile.SetRawInfoWithVerificationStatus(NAME_LAST_FIRST, u"D",
                                            VerificationStatus::kParsed);
   profile.SetRawInfoWithVerificationStatus(NAME_LAST_SECOND, u"e",
@@ -363,20 +359,10 @@ AutofillProfileSpecifics ConstructBaseSpecifics() {
       AutofillProfileSpecifics::VerificationStatus::
           AutofillProfileSpecifics_VerificationStatus_OBSERVED);
 
-  specifics.add_name_last("von Doe");
+  specifics.add_name_last("Doe");
   specifics.add_name_last_status(
       AutofillProfileSpecifics::VerificationStatus::
           AutofillProfileSpecifics_VerificationStatus_FORMATTED);
-
-  specifics.add_name_last_prefix("von");
-  specifics.add_name_last_prefix_status(
-      AutofillProfileSpecifics::VerificationStatus::
-          AutofillProfileSpecifics_VerificationStatus_PARSED);
-
-  specifics.add_name_last_core("Doe");
-  specifics.add_name_last_core_status(
-      AutofillProfileSpecifics::VerificationStatus::
-          AutofillProfileSpecifics_VerificationStatus_PARSED);
 
   specifics.add_name_last_first("D");
   specifics.add_name_last_first_status(
@@ -393,7 +379,7 @@ AutofillProfileSpecifics ConstructBaseSpecifics() {
       AutofillProfileSpecifics::VerificationStatus::
           AutofillProfileSpecifics_VerificationStatus_PARSED);
 
-  specifics.add_name_full("John K. von Doe");
+  specifics.add_name_full("John K. Doe");
   specifics.add_name_full_status(
       AutofillProfileSpecifics::VerificationStatus::
           AutofillProfileSpecifics_VerificationStatus_USER_VERIFIED);
@@ -1002,7 +988,6 @@ class AutofillProfileSyncUtilTest
         {
             features::kAutofillUseINAddressModel,
             features::kAutofillSupportPhoneticNameForJP,
-            features::kAutofillSupportLastNamePrefix,
             features::kAutofillSupportSplitZipCode,
         },
         {});
