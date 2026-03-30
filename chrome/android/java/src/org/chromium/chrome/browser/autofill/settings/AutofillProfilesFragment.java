73d6d146537aadac3b29ceec9b68b2c7ba7b6365
Bruno Braga <brunobraga@google.com>
2026-03-30 08:50:46 -0700

    [Autofill AI - M4] Add specific flag to render wallet disabled banner
    Bug: 467563385
    Change-Id: I5bcbd892277a6d73afc2d41e664102d36423a786
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7711813
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1607132}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill/settings/AutofillProfilesFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/settings/AutofillProfilesFragment.java
@@ -263,7 +263,8 @@ public class AutofillProfilesFragment extends ChromeBaseSettingsFragment
         if (!disabledSettingsInThirdPartyMode(getProfile())
                 && entityDataManager != null
                 && !entityDataManager.isWalletPublicPassStorageEnabled()
-                && ChromeFeatureList.isEnabled(ChromeFeatureList.AUTOFILL_AI_WITH_DATA_SCHEMA)) {
+                && ChromeFeatureList.isEnabled(
+                        ChromeFeatureList.AUTOFILL_AI_SHOW_WALLET_DISABLED_BANNER)) {
             addDisabledWalletDataSharingDataCard(screen);
         }
 
@@ -730,7 +731,7 @@ public class AutofillProfilesFragment extends ChromeBaseSettingsFragment
                             && entityDataManager != null
                             && !entityDataManager.isWalletPublicPassStorageEnabled()
                             && ChromeFeatureList.isEnabled(
-                                    ChromeFeatureList.AUTOFILL_AI_WITH_DATA_SCHEMA)) {
+                                    ChromeFeatureList.AUTOFILL_AI_SHOW_WALLET_DISABLED_BANNER)) {
                         addDisabledWalletDataSharingDataCard(indexData, getPrefFragmentName());
                     }
                     addAutofillSwitch(indexData);
@@ -786,7 +787,8 @@ public class AutofillProfilesFragment extends ChromeBaseSettingsFragment
         if (!disabledSettingsInThirdPartyMode
                 && entityDataManager != null
                 && !entityDataManager.isWalletPublicPassStorageEnabled()
-                && ChromeFeatureList.isEnabled(ChromeFeatureList.AUTOFILL_AI_WITH_DATA_SCHEMA)) {
+                && ChromeFeatureList.isEnabled(
+                        ChromeFeatureList.AUTOFILL_AI_SHOW_WALLET_DISABLED_BANNER)) {
             if (indexData.getEntryForKey(prefFragmentName, DISABLED_WALLET_DATA_SHARING) == null) {
                 addDisabledWalletDataSharingDataCard(indexData, prefFragmentName);
             }
