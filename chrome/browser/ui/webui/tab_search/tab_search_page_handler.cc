0c4be931473727e3dd0769c710b935de5b711b4c
David Yeung <dayeung@chromium.org>
2026-03-29 17:27:12 -0700

    [TabSearch] Update fetching active tab.
    Use BrowserWindowInterface::GetActiveTabInterface. This removes a
    dependency on the TabStripModel.
    
    Bug: 477686464
    Change-Id: I96fa721d24617e65cbe1c2a0f35436fc38e0eba2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7709163
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1606847}

--- chrome/browser/ui/webui/tab_search/tab_search_page_handler.cc
+++ chrome/browser/ui/webui/tab_search/tab_search_page_handler.cc
@@ -318,7 +318,7 @@ void TabSearchPageHandler::OpenRecentlyClosedEntry(int32_t session_id) {
 
   tab_restore_service->RestoreEntryById(
       BrowserLiveTabContext::FindContextForWebContents(
-          browser_->tab_strip_model()->GetActiveWebContents()),
+          browser_->GetActiveTabInterface()->GetContents()),
       SessionID::FromSerializedValue(session_id),
       WindowOpenDisposition::NEW_FOREGROUND_TAB);
 }
