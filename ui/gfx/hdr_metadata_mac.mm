0a1da4c728ee949d46c0d07fdfcae3e7c12fe400
Avi Drissman <avi@chromium.org>
2026-03-30 00:14:14 -0700

    Be sure CFDataRefs are of the correct size
    Use sizeof() when creating CFDataRefs rather than constants to ensure
    that the created CFDataRefs are of the correct size.
    
    Fixed: 497527002
    Change-Id: I1721e7c936ab44b8a06a172b3d19c76e6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7709895
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1606911}

--- ui/gfx/hdr_metadata_mac.mm
+++ ui/gfx/hdr_metadata_mac.mm
@@ -3,11 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-
 #include "ui/gfx/hdr_metadata_mac.h"
-#include "ui/gfx/hdr_metadata.h"
 
 #include <simd/simd.h>
+
+#include "base/apple/scoped_cftyperef.h"
+#include "ui/gfx/hdr_metadata.h"
 
 namespace gfx {
 
@@ -33,7 +34,7 @@ base::apple::ScopedCFTypeRef<CFDataRef> GenerateContentLightLevelInfo(
       __builtin_bswap16(hdr_metadata.GetCLLI().getUint16MaxFALL());
 
   return base::apple::ScopedCFTypeRef<CFDataRef>(
-      CFDataCreate(nullptr, reinterpret_cast<const UInt8*>(&sei), 4));
+      CFDataCreate(nullptr, reinterpret_cast<const UInt8*>(&sei), sizeof(sei)));
 }
 
 base::apple::ScopedCFTypeRef<CFDataRef> GenerateMasteringDisplayColorVolume(
@@ -82,7 +83,7 @@ base::apple::ScopedCFTypeRef<CFDataRef> GenerateMasteringDisplayColorVolume(
       __builtin_bswap32(md.fMinimumDisplayMasteringLuminance + 0.5f);
 
   return base::apple::ScopedCFTypeRef<CFDataRef>(
-      CFDataCreate(nullptr, reinterpret_cast<const UInt8*>(&sei), 24));
+      CFDataCreate(nullptr, reinterpret_cast<const UInt8*>(&sei), sizeof(sei)));
 }
 
 base::apple::ScopedCFTypeRef<CFDataRef> GenerateAmbientViewingEnvironment() {
@@ -105,7 +106,7 @@ base::apple::ScopedCFTypeRef<CFDataRef> GenerateAmbientViewingEnvironment() {
   sei.ambient_light_y = __builtin_bswap16(0x4042);        // 0.329
 
   return base::apple::ScopedCFTypeRef<CFDataRef>(
-      CFDataCreate(nullptr, reinterpret_cast<const UInt8*>(&sei), 24));
+      CFDataCreate(nullptr, reinterpret_cast<const UInt8*>(&sei), sizeof(sei)));
 }
 
 }  // namespace gfx
