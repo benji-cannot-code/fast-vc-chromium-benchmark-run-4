120de69b7d54eadc9a14158d1437704e6677d1f1
Etienne Dechamps <edechamps@google.com>
2026-03-30 08:35:01 -0700

    Add a sentinel class for enabling NativeCronetProvider
    For now the class is shipped in the same jar as NativeCronetProvider,
    making this change a no-op. This may change in subsequent CLs, depending
    on how we where we want to do the split for final packaging.
    
    Bug: b:496938249
    Change-Id: I3e0db10607e56a904f3f0f32525469c90a6c9e79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7684370
    Commit-Queue: Etienne Dechamps <edechamps@google.com>
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1607121}

---
+++ components/cronet/android/test/javatests/src/org/chromium/net/impl/NativeCronetProviderTest.java
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.net.impl;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.SmallTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.chromium.base.test.util.Batch;
+
+@Batch(Batch.UNIT_TESTS)
+@RunWith(AndroidJUnit4.class)
+public class NativeCronetProviderTest {
+    @Test
+    @SmallTest
+    public void testIsEnabledReturnsTrue() throws Exception {
+        assertThat(
+                        new NativeCronetProvider(ApplicationProvider.getApplicationContext())
+                                .isEnabled())
+                .isTrue();
+    }
+}
