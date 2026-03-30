97bc6d754f74a77375afda39eabb989b60641b0d
Alexei Svitkine <asvitkine@chromium.org>
2026-03-30 09:16:41 -0700

    Cleanup: Remove histogram macros header includes in gin
    Bug: None
    Change-Id: I132c5b76b67ceaccdf41b7cf19ea9d8eede4823d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7700626
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Auto-Submit: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1607142}

--- gin/v8_initializer.cc
+++ gin/v8_initializer.cc
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/lazy_instance.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/notreached.h"
 #include "base/path_service.h"
 #include "base/rand_util.h"
