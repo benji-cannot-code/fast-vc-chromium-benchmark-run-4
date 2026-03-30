a29e86ace5be45b63600fe8c2cc92fe3662b9d0d
dljames <dljames@chromium.org>
2026-03-30 11:32:41 -0700

    [CUJService] Add 2 minute timeout to prevent infinite length journeys
    Adds a 2 minute timeout to prevent journeys from running for
    indeterminate amounts of time without any progression. The 2 minute
    timeout is an arbitrary number and is subject to change.
    
    Tests are added.
    
    Change-Id: Ifeb4deb3ad4be2a2a2690bedfed381b87cff28ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7705142
    Reviewed-by: Steven Luong <stluong@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1607257}

--- chrome/browser/metrics/critical_user_journeys/critical_user_journey_session.h
+++ chrome/browser/metrics/critical_user_journeys/critical_user_journey_session.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/timer/timer.h"
 #include "ui/base/interaction/interaction_sequence.h"
 
 namespace ui {
@@ -33,9 +34,10 @@ class CriticalUserJourneySession {
   enum class JourneyResult {
     kCompleted = 0,
     kAborted = 1,
-    kMaxValue = kAborted,
+    kTimeout = 2,
+    kMaxValue = kTimeout,
   };
-  // LINT.ThenChange(tools/metrics/histograms/metadata/critical_user_journeys/enums.xml:CriticalUserJourneyResult)
+  // LINT.ThenChange(//tools/metrics/histograms/metadata/critical_user_journeys/enums.xml:CriticalUserJourneyResult)
 
   explicit CriticalUserJourneySession(const CriticalUserJourney* journey);
   ~CriticalUserJourneySession();
@@ -65,7 +67,8 @@ class CriticalUserJourneySession {
       ui::TrackedElement* initial_element);
 
   // Callbacks for InteractionSequence events.
-  void OnStepStarted(int metric_id);
+  void OnStepStarted(int metric_id, base::TimeDelta timeout);
+  void OnTimeout();
   void OnAborted(const ui::InteractionSequence::AbortedData& data);
   void OnCompleted();
 
@@ -74,6 +77,8 @@ class CriticalUserJourneySession {
   std::unique_ptr<ui::InteractionSequence> sequence_;
 
   int last_reached_metric_id_ = -1;
+  base::OneShotTimer timeout_timer_;
+  bool was_timeout_ = false;
 
   base::WeakPtrFactory<CriticalUserJourneySession> weak_factory_{this};
 };
--- chrome/browser/metrics/critical_user_journeys/critical_user_journey_session_unittest.cc
+++ chrome/browser/metrics/critical_user_journeys/critical_user_journey_session_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/bind.h"
 #include "base/test/bind.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/run_until.h"
 #include "base/test/task_environment.h"
 #include "chrome/browser/metrics/critical_user_journeys/critical_user_journey.h"
@@ -34,8 +35,9 @@ class CriticalUserJourneySessionTest : public testing::Test {
   CriticalUserJourneySessionTest() = default;
   ~CriticalUserJourneySessionTest() override = default;
 
- private:
-  base::test::SingleThreadTaskEnvironment task_environment_;
+ protected:
+  base::test::SingleThreadTaskEnvironment task_environment_{
+      base::test::TaskEnvironment::TimeSource::MOCK_TIME};
 };
 
 TEST_F(CriticalUserJourneySessionTest, SimpleJourneyCompletion) {
@@ -141,6 +143,34 @@ TEST_F(CriticalUserJourneySessionTest, BranchingJourneyCompletion) {
   el2.Show();
 
   EXPECT_TRUE(base::test::RunUntil([&]() { return done; }));
+}
+
+TEST_F(CriticalUserJourneySessionTest, JourneyTimeout) {
+  base::HistogramTester histogram_tester;
+  auto journey = CriticalUserJourney::Builder("Test Journey")
+                     .AddStep(kTestElementId1,
+                              ui::InteractionSequence::StepType::kShown, 1)
+                     .AddStep(kTestElementId2,
+                              ui::InteractionSequence::StepType::kShown, 2)
+                     .Build();
+
+  bool done = false;
+  auto session = std::make_unique<CriticalUserJourneySession>(journey.get());
+  session->set_on_done_callback(
+      base::BindLambdaForTesting([&]() { done = true; }));
+
+  ui::test::TestElement el1(kTestElementId1, kTestContext);
+  el1.Show();
+
+  session->Start(/*first_step_metric_id=*/std::nullopt, &el1);
+
+  // Fast forward by 2 minutes to trigger the timeout.
+  task_environment_.FastForwardBy(base::Minutes(2));
+
+  EXPECT_TRUE(done);
+  histogram_tester.ExpectUniqueSample(
+      "CriticalUserJourney.Test Journey.Result",
+      CriticalUserJourneySession::JourneyResult::kTimeout, 1);
 }
 
 }  // namespace metrics
