0e83ba112bb28fba62df532831a6c4644ffbdbdb
Arthur Sonzogni <arthursonzogni@chromium.org>
2026-03-30 02:06:16 -0700

    Convert to UNSAFE_TODO in media [7/7] Renderers and Other
    This was generated using [Script] below, followed by Gemini to
    Address the unsupported/difficult cases.
    
    We are migrating from coarse-grained file-level suppression (#pragma
    allow_unsafe_buffers) to granular, expression-level markers
    (UNSAFE_TODO()). The pragma disables safety checks for an entire file,
    whereas UNSAFE_TODO() isolates specific potentially unsafe operations,
    allowing the rest of the file to be enforced as safe.
    
    Script: https://docs.google.com/document/d/1ORQGBNn2R-CEvNbDTjRd-GrOBOFlCxIHdcvSUA_EhR4/edit?usp=sharing
    AX-Relnotes: N/A
    Cleanup: This is an automated #cleanup.
    Bug: 409340989
    Change-Id: I9bd7b5e33bcec9dde0d7bc0486c0b16ccf91e1d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7687211
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1606969}

--- media/midi/midi_webmidi_data_validator_fuzzer.cc
+++ media/midi/midi_webmidi_data_validator_fuzzer.cc
@@ -3,18 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include <stddef.h>
 #include <stdint.h>
 
+#include "base/compiler_specific.h"
 #include "media/midi/message_util.h"
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  std::vector<uint8_t> buffer(data, data + size);
+  std::vector<uint8_t> buffer(data, UNSAFE_TODO(data + size));
 
   if (midi::IsValidWebMIDIData(buffer))
     return 0;
--- media/renderers/paint_canvas_video_renderer.cc
+++ media/renderers/paint_canvas_video_renderer.cc
@@ -3,10 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
 
 #include "media/renderers/paint_canvas_video_renderer.h"
 
@@ -401,9 +397,10 @@ void ConvertVideoFrameToRGBPixelsTask(const VideoFrame* video_frame,
           // pixel on row N and column M will wrap to column M on row N + 1.
           .stride = video_frame->stride(plane),
 
-          .data = video_frame->visible_data(plane) +
-                  video_frame->stride(plane) * (chunk_start * rows_per_chunk) /
-                      VideoFrame::SampleSize(format, plane).height()};
+          .data = UNSAFE_TODO(
+              video_frame->visible_data(plane) +
+              video_frame->stride(plane) * (chunk_start * rows_per_chunk) /
+                  VideoFrame::SampleSize(format, plane).height())};
     } else {
       plane_meta[plane] = {.stride = 0, .data = nullptr};
     }
