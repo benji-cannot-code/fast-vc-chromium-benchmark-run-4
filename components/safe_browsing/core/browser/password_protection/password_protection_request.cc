e7f61bb2c14409d51ca00db84f6290bf466f4f54
Seung Jae Lim <andysjlim@chromium.org>
2026-03-30 09:03:38 -0700

    Stop fetching visual features from CSD in PG pings
    With the deprecation of fetching visual features from CSD in PG pings,
    we also do not collect other features from CSD pipeline such as the
    renderer classification.
    
    OBSOLETE_HISTOGRAM[PasswordProtection.CSDCacheContainsImages]=No longer used with this deprecation.
    OBSOLETE_HISTOGRAM[PasswordProtection.CSDCacheSizeAtHit2]=No longer used with this deprecation.
    OBSOLETE_HISTOGRAM[PasswordProtection.SuccessfulPhishingDetectionWithinTimeout]=No longer used with this deprecation
    
    Bug: 493269332
    Change-Id: I93c3b9ff0d1c0e0f4d3956740654640e516a13b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7696888
    Reviewed-by: thefrog <thefrog@chromium.org>
    Commit-Queue: Seung Jae Lim <andysjlim@chromium.org>
    Reviewed-by: Nwokedi Idika <nwokedi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1607134}

--- components/safe_browsing/core/browser/password_protection/password_protection_request.cc
+++ components/safe_browsing/core/browser/password_protection/password_protection_request.cc
@@ -292,7 +292,7 @@ void PasswordProtectionRequest::FillRequestProto(bool is_sampled_ping) {
 
 #if BUILDFLAG(SAFE_BROWSING_AVAILABLE)
   if (IsClientSideDetectionEnabled()) {
-    GetDomFeatures();
+    AddDebuggingMetadata();
   } else if (IsVisualFeaturesEnabled()) {
     MaybeCollectVisualFeatures();
   } else {
