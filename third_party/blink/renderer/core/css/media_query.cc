ce575783815316c63798a3f31f19c5f1f31c9483
Steinar H. Gunderson <sesse@chromium.org>
2026-03-30 03:10:05 -0700

    Property serialize identifiers in media queries.
    Fixed: 496627699
    Change-Id: I11f1c16e3182cee0d809e47aa8b3b96777c409cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7708299
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1606998}

--- third_party/blink/renderer/core/css/media_query.cc
+++ third_party/blink/renderer/core/css/media_query.cc
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 #include <memory>
+
+#include "third_party/blink/renderer/core/css/css_markup.h"
 #include "third_party/blink/renderer/core/css/media_query_exp.h"
 #include "third_party/blink/renderer/core/html/parser/html_parser_idioms.h"
 #include "third_party/blink/renderer/core/media_type_names.h"
@@ -55,7 +57,7 @@ String MediaQuery::Serialize() const {
   const ConditionalExpNode* exp_node = ExpNode();
 
   if (!exp_node) {
-    result.Append(MediaType());
+    SerializeIdentifier(MediaType(), result);
     return result.ReleaseString();
   }
 
