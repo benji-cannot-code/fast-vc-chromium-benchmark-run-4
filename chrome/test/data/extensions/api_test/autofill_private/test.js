23e054249321893d2429575ec898a6d145be70eb
Vidhan Jain <vidhanj@google.com>
2026-03-30 06:13:37 -0700

    Enable kAutofillAiWalletPrivatePasses in AutofillPrivateApiBrowserTest
    Bug: 496527599
    Change-Id: I3436f6d69055b6d3ae507f70af013133c66f3e04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7710697
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1607060}

--- chrome/test/data/extensions/api_test/autofill_private/test.js
+++ chrome/test/data/extensions/api_test/autofill_private/test.js
@@ -1224,10 +1224,10 @@ var availableTests = [
     // Add 2 Passports, their labels should be:
     //
     // Passport
-    // •⁠ ⁠•⁠ ⁠•⁠ ⁠•⁠ ⁠•⁠ ⁠•⁠ ⁠1234
+    // ⁠•⁠ ⁠•⁠ ⁠•⁠ ⁠•⁠ ⁠1234
     //
     // Passport
-    // •⁠ ⁠•⁠ ⁠•⁠ ⁠•⁠ ⁠•⁠ ⁠•⁠ ⁠5678
+    // ⁠•⁠ ⁠•⁠ ⁠•⁠ ⁠•⁠ ⁠5678
     var entityInstancesWithExpectedLabels = [
       {
         entity: {
@@ -1254,8 +1254,7 @@ var availableTests = [
         },
         // Obfuscated Number
         // "...1234"
-        expectedLabel: OBFUSCATION_DOT + OBFUSCATION_DOT +
-            OBFUSCATION_DOT + OBFUSCATION_DOT + OBFUSCATION_DOT +
+        expectedLabel: OBFUSCATION_DOT + OBFUSCATION_DOT + OBFUSCATION_DOT +
             OBFUSCATION_DOT + '1234'
       },
       {
@@ -1282,8 +1281,7 @@ var availableTests = [
           nickname: 'Passport 2',
         },
         // "...5678"
-        expectedLabel: OBFUSCATION_DOT + OBFUSCATION_DOT +
-            OBFUSCATION_DOT + OBFUSCATION_DOT + OBFUSCATION_DOT +
+        expectedLabel: OBFUSCATION_DOT + OBFUSCATION_DOT + OBFUSCATION_DOT +
             OBFUSCATION_DOT + '5678'
       },
     ];
@@ -1446,7 +1444,7 @@ var availableTests = [
       },
       {
         typeName: 7,
-        typeNameAsString: 'Number',
+        typeNameAsString: 'License Number',
         dataType: AttributeTypeDataType.STRING,
       },
       {
@@ -1464,14 +1462,14 @@ var availableTests = [
     chrome.test.succeed();
   },
 
-  async function getAllAttributeTypesForEntityTypeName() {
+  async function getRequiredAttributeTypesForEntityTypeName() {
     const attributeTypesList =
         await chrome.autofillPrivate.getRequiredAttributeTypesForEntityTypeName(
             /*entityTypeName=*/ 1);
     const expectedAttributeTypesList = [
       {
         typeName: 7,
-        typeNameAsString: 'Number',
+        typeNameAsString: 'License Number',
         dataType: AttributeTypeDataType.STRING,
       },
     ];
