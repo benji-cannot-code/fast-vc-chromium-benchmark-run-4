ec8c8681efc9e636855dee2aa7ac662c7098e45b
Jun Ishiguro <junis@google.com>
2026-03-30 01:11:19 -0700

    login: Pass local state PrefService to SaveScreenAfterConsumerUpdate
    Bug: b:404133029
    Change-Id: I2ca9428275315bd07436d70f70a70766b754f5c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7707154
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1606936}

--- chrome/browser/ash/login/screens/consumer_update_screen_browsertest.cc
+++ chrome/browser/ash/login/screens/consumer_update_screen_browsertest.cc
@@ -139,6 +139,7 @@ class ConsumerUpdateScreenTest : public OobeBaseTest {
     // this local state is set in OnUserCreationScreenExit and in the test we
     // advance directly to the consumerUpdate Screen.
     StartupUtils::SaveScreenAfterConsumerUpdate(
+        CHECK_DEREF(g_browser_process->local_state()),
         GaiaInfoScreenView::kScreenId.name);
 
     if (ash::features::IsOobeAutoEnrollmentCheckForcedEnabled()) {
@@ -153,7 +154,8 @@ class ConsumerUpdateScreenTest : public OobeBaseTest {
   }
 
   void SaveScreenAfterConsumerUpdate(const std::string& screen_name) {
-    StartupUtils::SaveScreenAfterConsumerUpdate(screen_name);
+    StartupUtils::SaveScreenAfterConsumerUpdate(
+        CHECK_DEREF(g_browser_process->local_state()), screen_name);
   }
 
   void TearDownOnMainThread() override {
