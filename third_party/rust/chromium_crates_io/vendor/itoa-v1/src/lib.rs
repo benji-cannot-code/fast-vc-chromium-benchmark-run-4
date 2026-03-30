dada9d7cc35b709c93a32f7791f19b17ea85f3c8
Liza Burakova <liza@chromium.org>
2026-03-30 08:01:00 -0700

    Roll itoa: 1.0.17 => 1.0.18 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * itoa: 1.0.17 => 1.0.18; https://docs.rs/crate/itoa/1.0.18
    
    Bug: None
    Change-Id: I5c67b2fbf088d261c39af911bfb7568dd9454fc9
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7697057
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Liza Burakova <liza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1607103}

--- third_party/rust/chromium_crates_io/vendor/itoa-v1/src/lib.rs
+++ third_party/rust/chromium_crates_io/vendor/itoa-v1/src/lib.rs
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //!
 //! ![performance](https://raw.githubusercontent.com/dtolnay/itoa/master/itoa-benchmark.png)
 
-#![doc(html_root_url = "https://docs.rs/itoa/1.0.17")]
+#![doc(html_root_url = "https://docs.rs/itoa/1.0.18")]
 #![no_std]
 #![allow(
     clippy::cast_lossless,
@@ -56,7 +56,6 @@ mod u128_ext;
 
 use core::hint;
 use core::mem::{self, MaybeUninit};
-use core::ptr;
 use core::str;
 #[cfg(feature = "no-panic")]
 use no_panic::no_panic;
@@ -106,9 +105,8 @@ impl Buffer {
     /// representation within the buffer.
     #[cfg_attr(feature = "no-panic", no_panic)]
     pub fn format<I: Integer>(&mut self, i: I) -> &str {
-        let string = i.write(unsafe {
-            &mut *ptr::addr_of_mut!(self.bytes).cast::<<I as private::Sealed>::Buffer>()
-        });
+        let buf_ptr = self.bytes.as_mut_ptr().cast::<I::Buffer>();
+        let string = i.write(unsafe { &mut *buf_ptr });
         if string.len() > I::MAX_STR_LEN {
             unsafe { hint::unreachable_unchecked() };
         }
@@ -413,7 +411,7 @@ fn enc_16lsd<const OFFSET: usize>(buf: &mut [MaybeUninit<u8>], n: u64) {
     let mut remain = n;
 
     // Format per four digits from the lookup table.
-    for quad_index in (0..4).rev() {
+    for quad_index in (1..4).rev() {
         // pull two pairs
         let quad = remain % 1_00_00;
         remain /= 1_00_00;
@@ -428,6 +426,15 @@ fn enc_16lsd<const OFFSET: usize>(buf: &mut [MaybeUninit<u8>], n: u64) {
             buf[quad_index * 4 + OFFSET + 3]
                 .write(*DECIMAL_PAIRS.0.get_unchecked(pair2 as usize * 2 + 1));
         }
+    }
+
+    // final two pairs
+    let (pair1, pair2) = divmod100(remain as u32);
+    unsafe {
+        buf[OFFSET + 0].write(*DECIMAL_PAIRS.0.get_unchecked(pair1 as usize * 2 + 0));
+        buf[OFFSET + 1].write(*DECIMAL_PAIRS.0.get_unchecked(pair1 as usize * 2 + 1));
+        buf[OFFSET + 2].write(*DECIMAL_PAIRS.0.get_unchecked(pair2 as usize * 2 + 0));
+        buf[OFFSET + 3].write(*DECIMAL_PAIRS.0.get_unchecked(pair2 as usize * 2 + 1));
     }
 }
 
