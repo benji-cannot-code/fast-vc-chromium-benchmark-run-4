44edfabe3429eae6f3317a5a5275350d6cb0b3ec
Colin Blundell <blundell@chromium.org>
2026-03-30 01:15:55 -0700

    [Blink] Make CRP::SetRecorder() Canvas2D-specific
    This method is called only on Canvas2D-specific CRP instances.
    
    Bug: 352263194
    Change-Id: I080430c398f7ed8f61c158c696f90dc117eadc48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7706334
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1606941}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
@@ -1732,8 +1732,9 @@ CanvasResourceProvider::ReleaseRecorder() {
   return recorder;
 }
 
-void CanvasResourceProvider::SetRecorder(
+void CanvasResourceProvider::SetRecorderForCanvas2D(
     std::unique_ptr<MemoryManagedPaintRecorder> recorder) {
+  CHECK(IsCanvas2D());
   recorder->SetClient(this);
   recorder_ = std::move(recorder);
   DisableLineDrawingAsPathsIfNecessary();
