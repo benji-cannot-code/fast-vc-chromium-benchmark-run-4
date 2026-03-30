cb5a7f8eea742188719075a28f37cd26d37b87bd
Marc Treib <treib@chromium.org>
2026-03-30 02:10:04 -0700

    ForeignSessionHandler: Improve unit test coverage
    Before adding more functionality here, let's improve the test coverage
    for the existing stuff a bit.
    
    Bug: 488252161
    Change-Id: I533c77d2ef838fe45fe62b57045d0e056a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7705841
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1606972}

--- chrome/browser/ui/webui/history/foreign_session_handler_unittest.cc
+++ chrome/browser/ui/webui/history/foreign_session_handler_unittest.cc
@@ -10,10 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/callback_list.h"
-#include "base/containers/flat_map.h"
-#include "base/memory/raw_ptr.h"
 #include "chrome/browser/sync/session_sync_service_factory.h"
+#include "chrome/common/pref_names.h"
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
+#include "components/prefs/pref_service.h"
 #include "components/sessions/core/session_id.h"
 #include "components/sync_sessions/mock_open_tabs_ui_delegate.h"
 #include "components/sync_sessions/session_sync_service.h"
@@ -135,6 +135,34 @@ TEST_F(ForeignSessionHandlerTest, OpenForeignSessionTab) {
 
   handler()->OpenForeignSessionTab("my_session_tag", 456,
                                    ui::mojom::ClickModifiers::New());
+}
+
+TEST_F(ForeignSessionHandlerTest, DeleteForeignSession) {
+  EXPECT_CALL(*session_sync_service()->GetOpenTabsUIDelegate(),
+              DeleteForeignSession("my_session_tag"))
+      .Times(testing::AtLeast(1));
+
+  handler()->DeleteForeignSession("my_session_tag");
+}
+
+TEST_F(ForeignSessionHandlerTest, SetForeignSessionCollapsed) {
+  EXPECT_FALSE(profile()
+                   ->GetPrefs()
+                   ->GetDict(prefs::kNtpCollapsedForeignSessions)
+                   .FindBool("my_session_tag")
+                   .value_or(false));
+  handler()->SetForeignSessionCollapsed("my_session_tag", true);
+  EXPECT_TRUE(profile()
+                  ->GetPrefs()
+                  ->GetDict(prefs::kNtpCollapsedForeignSessions)
+                  .FindBool("my_session_tag")
+                  .value_or(false));
+  handler()->SetForeignSessionCollapsed("my_session_tag", false);
+  EXPECT_FALSE(profile()
+                   ->GetPrefs()
+                   ->GetDict(prefs::kNtpCollapsedForeignSessions)
+                   .FindBool("my_session_tag")
+                   .value_or(false));
 }
 
 }  // namespace browser_sync
