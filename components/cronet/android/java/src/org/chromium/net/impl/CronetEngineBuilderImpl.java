e9c86d798d1ad898e09df2e6bf91c90140eea75a
Stefano Duo <stefanoduo@google.com>
2026-03-30 04:52:14 -0700

    De-experimentalize the new Cronet proxy APIs
    These are now public and set in stone.
    
    ICronetEngineBuilder#setProxyOptions has been suffixed with V2 to
    surface ABI mismatches at CronetEngine.Builder#setProxyOptions time,
    when developer can still hopefully recover from setProxyOptions not
    being supported by the Cronet implementation being used.
    
    Note this breaks API-impl compatibility for any existing users of
    setProxyOptions, as the setProxyOptions (not V2) method will be missing
    from the impl. But this is fine, because this API was never advertised
    to Play Services Cronet users, which is the only setup where the API and
    impl versions can diverge.
    
    Bug: b:469376436
    Cq-Include-Trybots: luci.chrome.try:cronet-arm64-gn2bp-debug
    Change-Id: I2c9aae955751549cec8d68476e6e4f65d1367582
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7422118
    Reviewed-by: Etienne Dechamps <edechamps@google.com>
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1607040}

--- components/cronet/android/java/src/org/chromium/net/impl/CronetEngineBuilderImpl.java
+++ components/cronet/android/java/src/org/chromium/net/impl/CronetEngineBuilderImpl.java
@@ -10,6 +10,7 @@ import android.os.SystemClock;
 import android.util.Base64;
 
 import androidx.annotation.IntDef;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 
@@ -506,10 +507,8 @@ public abstract class CronetEngineBuilderImpl extends ICronetEngineBuilder {
     }
 
     @Override
-    public CronetEngineBuilderImpl setProxyOptions(@Nullable ProxyOptions proxyOptions) {
-        if (proxyOptions != null) {
-            mProxyOptions = new VersionSafeProxyOptions(proxyOptions);
-        }
+    public CronetEngineBuilderImpl setProxyOptionsV2(@NonNull ProxyOptions proxyOptions) {
+        mProxyOptions = new VersionSafeProxyOptions(proxyOptions);
         return this;
     }
 
