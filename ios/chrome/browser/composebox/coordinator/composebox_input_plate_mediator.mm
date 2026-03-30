937b2a6556dd2393f235f74311e3403abc9d9600
Radu Nitescu <radunitescu@google.com>
2026-03-30 09:49:11 -0700

    [iOS] Add a DumpWithoutCrashing for uninterpreted model modes
    `omnibox::ModelMode` is an open enum and should not be exhaustively
    checked. To mitigate uninterpreted models, add a dump without crash.
    
    Change-Id: Ic84f9d2b069ab1a219486c6c8b7d13e8039d28e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7711815
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1607169}

--- ios/chrome/browser/composebox/coordinator/composebox_input_plate_mediator.mm
+++ ios/chrome/browser/composebox/coordinator/composebox_input_plate_mediator.mm
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <utility>
 
 #import "base/apple/foundation_util.h"
+#import "base/debug/dump_without_crashing.h"
 #import "base/files/file_path.h"
 #import "base/functional/bind.h"
 #import "base/ios/block_types.h"
@@ -112,7 +113,14 @@ ComposeboxModelOption ModelOptionForModelMode(omnibox::ModelMode model_mode) {
     case omnibox::ModelMode::MODEL_MODE_GEMINI_PRO_NO_GEN_UI:
       return ComposeboxModelOption::kThinkingNoGenUI;
     case omnibox::ModelMode::MODEL_MODE_GEMINI_REGULAR:
+    case omnibox::ModelMode::MODEL_MODE_UNSPECIFIED:
+      return ComposeboxModelOption::kRegular;
     default:
+      // `ModelMode` is an open enum and prohibits exhaustive switch statements.
+      // See https://protobuf.dev/programming-guides/enum/
+      //
+      // Dump without crashing if the received model is not interpreted iOS.
+      base::debug::DumpWithoutCrashing();
       return ComposeboxModelOption::kRegular;
   }
 }
