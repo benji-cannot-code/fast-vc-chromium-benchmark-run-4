fd349d963483fa00d16a48069c451772c6a18739
Xinyi Ji <xinyiji@google.com>
2026-03-30 10:05:35 -0700

    [gardening] Disable failing test ReengagementNotificationControllerIntegrationTest#testReengagementActivity
    Bug: 497812812
    Change-Id: Iaadb3792f31970bebdce90cbfee8e88b856ce8db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7712193
    Owners-Override: Xinyi Ji <xinyiji@chromium.org>
    Commit-Queue: Xinyi Ji <xinyiji@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Dan Polanco <polardz@google.com>
    Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1607185}

--- chrome/android/javatests/src/org/chromium/chrome/browser/reengagement/ReengagementNotificationControllerIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/reengagement/ReengagementNotificationControllerIntegrationTest.java
@@ -274,6 +274,7 @@ public class ReengagementNotificationControllerIntegrationTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "https://crbug.com/497812812")
     public void testReengagementActivity() throws Exception {
         WebPageStation blankPage = mTabbedActivityTestRule.startOnBlankPage();
         int initialTabCount =
