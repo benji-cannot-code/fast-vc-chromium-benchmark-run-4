ebfd2c79a90ececd0b6221c9667134029aa989ea
Federica Germinario <fedegermi@google.com>
2026-03-30 03:23:55 -0700

    [ios] Implement execute for kInvalid NSUserActivity
    This is a no-op. In this case simply return and don't execute the
    intent.
    
    Bug: 492115056
    Change-Id: I36cc275da635ab2e75907e20555f33162ced820d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7707356
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1607005}

--- ios/chrome/app/task_request_user_activity.mm
+++ ios/chrome/app/task_request_user_activity.mm
@@ -398,8 +398,7 @@ void AddBookmarkToChromeWithIntent(INIntent* intent,
       // TODO(crbug.com/492115056): Add implementation.
       break;
     case UserActivityType::kInvalid:
-      // TODO(crbug.com/492115056): Add implementation.
-      break;
+      return;
   }
 
   if (!webpageGURL.is_valid()) {
