e3d177b64e66e0b5aeba9c04a90ba598661ae7c8
Joe Mason <joenotcharles@google.com>
2026-03-30 13:05:58 -0700

    Fix IWYU error with WrapUnique in FakeModalDialogManagerBridge
    R=jthiesen
    
    Change-Id: Id59c5f5dc2449746ff4b8c2f5ee00841e5d0c88a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7712115
    Reviewed-by: Jack Thiesen <jthiesen@chromium.org>
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Jack Thiesen <jthiesen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1607329}

--- ui/android/fake_modal_dialog_manager_bridge.cc
+++ ui/android/fake_modal_dialog_manager_bridge.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/android/jni_android.h"
+#include "base/memory/ptr_util.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/android/window_android.h"
 #include "ui/gfx/android/java_bitmap.h"
