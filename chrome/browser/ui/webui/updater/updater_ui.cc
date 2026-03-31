c8851a5e5e676cffa0e86f6cbbcc8178f6cc5e4c
Noah Rose Ledesma <noahrose@google.com>
2026-03-31 15:05:48 -0700

    Updater UI: Configure defaults per channel
    The event history section of chrome://updater should default to showing
    you the update history of the browser channel accessing the page.
    
    Bug: 498286804
    Change-Id: Ia673fcb1653dad28bdf37366c33385626a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7719260
    Auto-Submit: Noah Rose Ledesma <noahrose@google.com>
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1608127}

--- chrome/browser/ui/webui/updater/updater_ui.cc
+++ chrome/browser/ui/webui/updater/updater_ui.cc
@@ -35,6 +35,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/webui/mojo_web_ui_controller.h"
 #include "ui/webui/webui_util.h"
 
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+#include "base/version_info/channel.h"
+#include "chrome/common/channel_info.h"
+#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
+
 namespace {
 
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
@@ -208,7 +213,20 @@ UpdaterUI::UpdaterUI(content::WebUI* web_ui)
               {"{44FC7FE2-65CE-487C-93F4-EDEE46EEAAAB}"});
   AddKnownApp(*source, num_known_apps++, "Chrome Enterprise Companion App",
               {"{85EEDF37-756C-4972-9399-5A12A4BEE148}"});
-  source->AddLocalizedString("defaultAppFilters", IDS_PRODUCT_NAME);
+
+  source->AddLocalizedString("defaultAppFilters", [] {
+    switch (chrome::GetChannel()) {
+      case version_info::Channel::BETA:
+        return IDS_SHORTCUT_NAME_BETA;
+      case version_info::Channel::DEV:
+        return IDS_SHORTCUT_NAME_DEV;
+      case version_info::Channel::CANARY:
+        return IDS_SXS_SHORTCUT_NAME;
+      case version_info::Channel::STABLE:
+      case version_info::Channel::UNKNOWN:
+        return IDS_PRODUCT_NAME;
+    }
+  }());
 #else
   source->AddString("defaultAppFilters", "");
 #endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
