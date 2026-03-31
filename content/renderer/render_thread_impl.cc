5554e13d5d6125223ab86e10a98c14ec175168f4
Sergio Gonzalez Martin <sergiog@microsoft.com>
2026-03-31 10:22:03 -0700

    [rust jpeg] Configure rust exif
    Configure rusty EXIF so EXIF parsing will be done by a rust
    implementation instead of C++ implementation. There is added kill-
    switch feature `kForceSkExifCppParsing` to allow forcing the use of C++
    implementation in case of issues found in the field.
    
    This change will not enable it yet, as this will be done in a follow-up
    CL by adding SK_EXIF_PARSE_WITH_RUST to skia/BUILD.gn
    
    This change requires tha this skia CL is completed:
    https://skia-review.googlesource.com/c/skia/+/1196997
    
    Bug: 463653726
    Bug: 493315750
    Change-Id: I38337bb9d9c5b11409fbef6d88619d270c4be7f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7708949
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Reviewed-by: Kaylee Lubick <kjlubick@chromium.org>
    Commit-Queue: Sergio Gonzalez Martin <sergiog@microsoft.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1607951}

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -167,6 +167,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkFontMgr.h"
 #include "third_party/skia/include/core/SkGraphics.h"
 #include "third_party/skia/include/private/chromium/SkCodecsICCProfileChromium.h"
+#include "third_party/skia/include/private/chromium/SkExifChromium.h"
 #include "ui/base/ui_base_switches.h"
 #include "ui/base/ui_base_switches_util.h"
 #include "ui/display/display_switches.h"
@@ -807,6 +808,14 @@ void RenderThreadImpl::InitializeWebKit(mojo::BinderMap* binders) {
   // Stable.
   SkCodecs::ICCProfileChromium::ForceSkcms(
       base::FeatureList::IsEnabled(blink::features::kForceSkcmsICCParsing));
+
+  // Configure the EXIF parser kill-switch early, before any image decoding
+  // occurs. When the feature is enabled, this forces the C++ SkExif parser to
+  // be used instead of the Rust-based EXIF parser.
+  // TODO(crbug.com/463653726): Remove this once the feature is validated in
+  // Stable.
+  SkExif::ForceSkExif(
+      base::FeatureList::IsEnabled(blink::features::kForceSkExifCppParsing));
 
   // Hook up blink's codecs so skia can call them. Since only the renderer
   // processes should be doing image decoding, this is not done in the common
