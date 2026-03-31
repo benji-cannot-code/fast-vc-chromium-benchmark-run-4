0daa2ecefc10adf7a515fa773b13a1e58c2d322c
Colin Blundell <blundell@chromium.org>
2026-03-30 23:59:16 -0700

    [Blink] Have ImageBitmapRC use ExternalCanvasDrawHelper()
    Part of the elimination of direct external exposure of
    CanvasNon2DResourceProviderSI's canvas. Changes this code to execute
    [1]. This is not a behavioral change, as the cached snapshot will
    already be dropped here in the relevant flows:
    
    * The snapshot is populated only if accelerated [2]
    * The call to ProduceCanvasResource() below will call FlushCanvas() in
      this case [3]
    * As the recording has draw ops, FlushCanvas() will call
      WillDrawInternal() when accelerated [4]
    * WillDrawInternal() will drop the cached snapshot [5]
    
    The only case where the above won't happen is if IsGpuContextLost() is
    true. In that edge case, it is fine to drop the cached snapshot and
    recreate it the next time it is needed and the GPU context is available
    (as caching the snapshot is just a performance optimization and we don't
    expect GPU context losses to be frequent events).
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc;l=1026-1027;drc=4376ecb9520cb832e52d06b36db3806440ab19b2;bpv=1;bpt=1
    [2] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc;l=1125;drc=4376ecb9520cb832e52d06b36db3806440ab19b2;bpv=1;bpt=1
    [3] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc;l=962;drc=4376ecb9520cb832e52d06b36db3806440ab19b2;bpv=1;bpt=1
    [4] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc;l=1924;drc=4376ecb9520cb832e52d06b36db3806440ab19b2
    [5] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc;l=680;drc=4376ecb9520cb832e52d06b36db3806440ab19b2
    
    Bug: 352263194
    Change-Id: I55fd789acdf470ff78baa872613832d3dc8485a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7710834
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1607668}

--- third_party/blink/renderer/modules/canvas/imagebitmap/image_bitmap_rendering_context.cc
+++ third_party/blink/renderer/modules/canvas/imagebitmap/image_bitmap_rendering_context.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/metrics/histogram_functions.h"
+#include "cc/paint/paint_canvas.h"
+#include "cc/paint/paint_flags.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_union_htmlcanvaselement_offscreencanvas.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_union_offscreen_rendering_context.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_union_rendering_context.h"
@@ -223,9 +225,11 @@ bool ImageBitmapRenderingContext::PushFrame() {
 
   cc::PaintFlags paint_flags;
   paint_flags.setBlendMode(SkBlendMode::kSrc);
-  resource_provider_for_offscreen_canvas_->GetCanvasDeprecated().drawImage(
-      image->PaintImageForCurrentFrame(), 0, 0, SkSamplingOptions(),
-      &paint_flags);
+  resource_provider_for_offscreen_canvas_->ExternalCanvasDrawHelper(
+      [&](cc::PaintCanvas& canvas) {
+        canvas.drawImage(image->PaintImageForCurrentFrame(), 0, 0,
+                         SkSamplingOptions(), &paint_flags);
+      });
   scoped_refptr<CanvasResource> resource =
       resource_provider_for_offscreen_canvas_->ProduceCanvasResource();
   Host()->PushFrame(std::move(resource));
