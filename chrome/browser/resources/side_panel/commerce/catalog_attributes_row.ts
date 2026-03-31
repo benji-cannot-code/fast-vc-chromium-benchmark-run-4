93f7bbfbd0069db1acc71c37ffbb57fd4555f4d9
rbpotter <rbpotter@chromium.org>
2026-03-31 11:50:12 -0700

    Commerce side panel: Migrate catalog-attributes-row to Lit
    Bug: 493823435
    Change-Id: I8e3cbd6a78d7c83511101c66596eebdd8ea187fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7708976
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1607996}

--- chrome/browser/resources/side_panel/commerce/catalog_attributes_row.ts
+++ chrome/browser/resources/side_panel/commerce/catalog_attributes_row.ts
@@ -3,10 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'chrome://resources/cr_elements/cr_hidden_style.css.js';
 import 'chrome://resources/cr_elements/cr_icon/cr_icon.js';
-import 'chrome://resources/cr_elements/cr_icons.css.js';
-import 'chrome://resources/cr_elements/cr_shared_vars.css.js';
 import 'chrome://resources/cr_elements/icons.html.js';
 import '/strings.m.js';
 
@@ -14,30 +11,35 @@ import type {PriceInsightsInfo} from '//resources/cr_components/commerce/shoppin
 import {PriceInsightsInfo_PriceBucket} from '//resources/cr_components/commerce/shopping_service.mojom-webui.js';
 import type {ShoppingServiceBrowserProxy} from '//resources/cr_components/commerce/shopping_service_browser_proxy.js';
 import {ShoppingServiceBrowserProxyImpl} from '//resources/cr_components/commerce/shopping_service_browser_proxy.js';
-import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
 
-import {getTemplate} from './catalog_attributes_row.html.js';
+import {getCss} from './catalog_attributes_row.css.js';
+import {getHtml} from './catalog_attributes_row.html.js';
 
-export class CatalogAttributesRow extends PolymerElement {
+export class CatalogAttributesRowElement extends CrLitElement {
   static get is() {
     return 'catalog-attributes-row';
   }
 
-  static get template() {
-    return getTemplate();
+  static override get styles() {
+    return getCss();
   }
 
-  static get properties() {
+  override render() {
+    return getHtml.bind(this)();
+  }
+
+  static override get properties() {
     return {
-      priceInsightsInfo: Object,
+      priceInsightsInfo: {type: Object},
     };
   }
 
-  declare priceInsightsInfo: PriceInsightsInfo;
+  accessor priceInsightsInfo: PriceInsightsInfo;
   private shoppingApi_: ShoppingServiceBrowserProxy =
       ShoppingServiceBrowserProxyImpl.getInstance();
 
-  private openJackpot_() {
+  protected onJackpotClick_() {
     this.shoppingApi_.openUrlInNewTab(this.priceInsightsInfo.jackpot);
     chrome.metricsPrivate.recordEnumerationValue(
         'Commerce.PriceInsights.BuyingOptionsClicked',
@@ -48,8 +50,9 @@ export class CatalogAttributesRow extends PolymerElement {
 
 declare global {
   interface HTMLElementTagNameMap {
-    'catalog-attributes-row': CatalogAttributesRow;
+    'catalog-attributes-row': CatalogAttributesRowElement;
   }
 }
 
-customElements.define(CatalogAttributesRow.is, CatalogAttributesRow);
+customElements.define(
+    CatalogAttributesRowElement.is, CatalogAttributesRowElement);
