08db8a4cf0f075c17a87c0ae40653cec97cfb9de
Takashi Nakayama <tnak@chromium.org>
2026-03-30 18:00:16 -0700

    Revert "[views-ax] Generate Event::kCheckedStateChanged for Views"
    This reverts commit b92de6af3075ac1d919010f8c14fb9eefe965eb8.
    
    Reason for revert: This CL is the culprit for https://crbug.com/497978521
    
    Original change's description:
    > [views-ax] Generate Event::kCheckedStateChanged for Views
    >
    > This CL makes it so the accessibility event kCheckedStateChanged is
    > auto-generated by the AXTree's AXEventGenerator. The code is already in
    > place for web content -- all we have to do is prevent this event from
    > being serialized and the AXEventGenerator will generate the event upon
    > deserialization based on the kCheckedState property.
    >
    > Tested this change using the Checkbox view, which sets the checked
    > state.
    >
    > Bug: 40672441
    > Change-Id: I3c1f939960c80ef3ca7b2ff2c14ad6aec45a2215
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7533204
    > Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    > Reviewed-by: Elly FJ <ellyjones@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1607331}
    
    Bug: 40672441
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I6822973453c783b0d808f72123940e7a7bb78133
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7714018
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Takashi Nakayama <tnak@chromium.org>
    Owners-Override: Takashi Nakayama <tnak@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1607524}

--- ui/views/accessibility/tree/widget_ax_manager.cc
+++ ui/views/accessibility/tree/widget_ax_manager.cc
@@ -54,7 +54,6 @@ bool ShouldSerializeEvent(Event event_type) {
   switch (event_type) {
     // TODO(crbug.com/40672441): Add events here as needed.
     case Event::kActiveDescendantChanged:
-    case Event::kCheckedStateChanged:
     case Event::kChildrenChanged:
       return false;
     default:
@@ -82,6 +81,7 @@ bool ShouldSerializeEvent(Event event_type) {
   // being addressed incrementally, one event at a time.
   switch (event_type) {
     case Event::kAlert:
+    case Event::kCheckedStateChanged:
     case Event::kExpandedChanged:
     case Event::kFocusAfterMenuClose:
     case Event::kFocusContext:
