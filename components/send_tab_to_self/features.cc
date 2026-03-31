c83c9d5b9c4b265a80fb483fc726fc48c5735395
Michael Tatarski <mtatarski@google.com>
2026-03-31 06:28:26 -0700

    [STTS] Improve "last active" labels for the device picker.
    The existing "Active today" label is too broad and can be confusing for
    devices that were active very recently (e.g., a few seconds or minutes
    ago).
    
    This CL replaces the coarse "Active today" label with more precise and
    granular relative timestamps for devices that provide high-precision
    timestamp information (i.e., those syncing Sessions or History data).
    
    The new logic is as follows:
    
    - If a device was active less than one minute ago, "Active now" is shown.
    - Otherwise, it displays relative timestamps like "Active 5 minutes ago" or "Active 2 hours ago" up to 24 hours.
    - For devices without high-precision timestamps, or for durations of a day or more, it continues to use the existing day-based labels ("Active 1 day ago", etc.).
    
    Example mapping:
    
    | Time Since Active | Label Before      | Label After            |
    | :---------------- | :---------------- | :--------------------- |
    | 10 seconds        | Active today      | Active now             |
    | 5 minutes         | Active today      | Active 5 minutes ago   |
    | 2 hours           | Active today      | Active 2 hours ago     |
    | 23 hours          | Active today      | Active 23 hours ago    |
    | 1 day             | Active 1 day ago  | Active 1 day ago       |
    | 3 days            | Active 3 days ago | Active 3 days ago      |
    
    Example of new UI (behind feature flag):
    - https://screenshot.googleplex.com/6hMjf6xBDewiJfX.png
    
    Bug: 496176767
    Change-Id: I2d4832759522ae7e810d823bed1734d70abad645
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7709038
    Commit-Queue: Michael Tatarski <mtatarski@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1607821}

--- components/send_tab_to_self/features.cc
+++ components/send_tab_to_self/features.cc
@@ -31,6 +31,9 @@ BASE_FEATURE(kSendTabToSelfPropagateFormFields,
 BASE_FEATURE(kSendTabToSelfPropagateScrollPosition,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kSendTabToSelfImprovedLastActiveLabels,
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 BASE_FEATURE(kSendTabToSelfPropagateNavigationHistory,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
