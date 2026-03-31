facc5f0373f1cfe57a2f3bfafaf0e58acc64a640
Saifuddin Hitawala <hitawala@chromium.org>
2026-03-31 06:28:16 -0700

    [media] Add nullptr checks in DownShiftHighbitVideoFrame
    Add a nullptr check in DownShiftHighbitVideoFrame as the VideoFrame
    created there can be potentially null as per crash reports.
    
    Bug: 491815851
    Change-Id: I63eee913da9be57b23b74b7af456e88f9e527898
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7710682
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1607820}

--- media/renderers/paint_canvas_video_renderer.cc
+++ media/renderers/paint_canvas_video_renderer.cc
@@ -1123,6 +1123,9 @@ scoped_refptr<VideoFrame> DownShiftHighbitVideoFrame(
   scoped_refptr<VideoFrame> ret = VideoFrame::CreateFrame(
       format, video_frame->coded_size(), video_frame->visible_rect(),
       video_frame->natural_size(), video_frame->timestamp());
+  if (!ret) {
+    return nullptr;
+  }
 
   ret->set_color_space(video_frame->ColorSpace());
   // Copy all metadata.
@@ -1338,6 +1341,11 @@ void PaintCanvasVideoRenderer::ConvertVideoFrameToRGBPixels(
       return;
     default:
       break;
+  }
+
+  // DownShiftHighbitVideoFrame may fail creation of the `temporary_frame`.
+  if (!video_frame) {
+    return;
   }
 
   const size_t n_tasks =
