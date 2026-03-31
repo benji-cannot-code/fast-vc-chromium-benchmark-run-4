f3abf7589c87817d4db6d6dccbf30c3d1c9a2f8c
Frank Liberato <liberato@chromium.org>
2026-03-31 16:30:44 -0700

    Don't start the exclusive access bubble timer until commit
    Bug: 343352552
    Change-Id: If0f8704a23c51e1431f9919ea88b989f74de4a4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7615417
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Muyao Xu <muyaoxu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1608174}

--- chrome/browser/ui/exclusive_access/exclusive_access_test.cc
+++ chrome/browser/ui/exclusive_access/exclusive_access_test.cc
@@ -75,6 +75,8 @@ ExclusiveAccessTest::ExclusiveAccessTest() {
 ExclusiveAccessTest::~ExclusiveAccessTest() = default;
 
 void ExclusiveAccessTest::SetUpOnMainThread() {
+  ExclusiveAccessBubbleViews::set_skip_presentation_delay_for_testing(true);
+
   permission_controller_ =
       std::make_unique<content::MockPermissionController>();
   ON_CALL(*permission_controller_, RequestPermissionsFromCurrentDocument)
@@ -111,6 +113,8 @@ void ExclusiveAccessTest::SetUpOnMainThread() {
 }
 
 void ExclusiveAccessTest::TearDownOnMainThread() {
+  ExclusiveAccessBubbleViews::set_skip_presentation_delay_for_testing(false);
+
   GetExclusiveAccessManager()
       ->pointer_lock_controller()
       ->bubble_hide_callback_for_test_ =
--- chrome/browser/ui/views/exclusive_access_bubble_views.cc
+++ chrome/browser/ui/views/exclusive_access_bubble_views.cc
@@ -25,10 +25,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/generated_resources.h"
 #include "components/fullscreen_control/fullscreen_features.h"
 #include "components/fullscreen_control/subtle_notification_view.h"
+#include "components/viz/common/frame_timing_details.h"
 #include "content/public/browser/web_contents.h"
 #include "ui/accessibility/ax_enums.mojom.h"
 #include "ui/accessibility/ax_node_data.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/compositor/compositor.h"
 #include "ui/events/keycodes/keyboard_codes.h"
 #include "ui/gfx/animation/slide_animation.h"
 #include "ui/strings/grit/ui_strings.h"
@@ -40,6 +42,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IS_WIN)
 #include "ui/base/l10n/l10n_util_win.h"
 #endif
+
+bool ExclusiveAccessBubbleViews::skip_presentation_delay_for_testing_ = false;
 
 namespace {
 
@@ -311,6 +315,16 @@ void ExclusiveAccessBubbleViews::AnimationProgressed(
   if (opacity == 0) {
     popup_->Hide();
   } else {
+    if (presentation_cb_) {
+      ui::Compositor* compositor = popup_->GetCompositor();
+      if (!compositor || skip_presentation_delay_for_testing_) {
+        // Start the hide timer immediately, since we won't get any feedback.
+        std::move(presentation_cb_).Run({});
+      } else {
+        compositor->RequestSuccessfulPresentationTimeForNextFrame(
+            std::move(presentation_cb_));
+      }
+    }
     popup_->Show();
     popup_->SetOpacity(opacity);
   }
@@ -361,6 +375,7 @@ void ExclusiveAccessBubbleViews::Hide() {
   // `ExclusiveAccessBubble::kShowTime`.
   DCHECK(!hide_timeout_.IsRunning());
   RunHideCallbackIfNeeded(ExclusiveAccessBubbleHideReason::kTimeout);
+  presentation_cb_.Reset();
 
   animation_->SetSlideDuration(base::Milliseconds(700));
   animation_->Hide();
@@ -372,6 +387,14 @@ void ExclusiveAccessBubbleViews::Show() {
   }
   animation_->SetSlideDuration(base::Milliseconds(350));
   animation_->Show();
+}
+
+void ExclusiveAccessBubbleViews::ShowAndStartTimers() {
+  presentation_cb_ =
+      base::BindOnce(&ExclusiveAccessBubbleViews::OnFirstPresentation,
+                     weak_ptr_factory_.GetWeakPtr());
+  Show();
+  Snooze();
 }
 
 void ExclusiveAccessBubbleViews::OnWidgetDestroyed(views::Widget* widget) {
@@ -389,6 +412,11 @@ void ExclusiveAccessBubbleViews::OnWidgetDestroyed(views::Widget* widget) {
   // Note: |this| is destroyed on the line above. Check that the destructor was
   // invoked. This is safe to do since |popup_| is deleted via a posted task.
   DCHECK(!popup_on_stack->HasObserver(this));
+}
+
+void ExclusiveAccessBubbleViews::OnFirstPresentation(
+    const viz::FrameTimingDetails& details) {
+  StartHideTimer();
 }
 
 void ExclusiveAccessBubbleViews::RunHideCallbackIfNeeded(
