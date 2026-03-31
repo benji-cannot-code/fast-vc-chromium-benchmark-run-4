6f9a670d7d26d14b1ff377ba508620859d0e4b14
James Cook <jamescook@chromium.org>
2026-03-30 17:01:32 -0700

    net: Remove an unused include from system_network_context_manager.cc
    As part of the effort to bring extensions to desktop Android we're
    auditing all usage of ENABLE_EXTENSIONS in //chrome/browser. This
    one just wraps an unused include, so remove it.
    
    Bug: 494643383
    Change-Id: I69c8e7cfba74c2e96afd1ce712f636b75376e32c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7712246
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Auto-Submit: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1607478}

--- chrome/browser/net/system_network_context_manager.cc
+++ chrome/browser/net/system_network_context_manager.cc
@@ -101,10 +101,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process_platform_part.h"
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
-#include "extensions/common/constants.h"
-#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
-
 #if BUILDFLAG(IS_WIN)
 #include "chrome/browser/net/chrome_mojo_proxy_resolver_win.h"
 #endif  // BUILDFLAG(IS_WIN)
