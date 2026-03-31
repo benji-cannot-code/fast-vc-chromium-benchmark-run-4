71cc9559ca4e0de2ae5a305fa225c4d64d023037
Will Harris <wfh@chromium.org>
2026-03-31 13:26:20 -0700

    Add --enable-logging to allowed switches for isolated processes
    It's useful to be able to log from isolated processes, so add
    kEnableLogging.
    
    BUG=484520095
    
    Change-Id: I6767a70b557ed6ec0cea92c4bfd1545e9f5ecffb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7712717
    Commit-Queue: Will Harris <wfh@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1608068}

--- chrome/elevation_service/elevator.cc
+++ chrome/elevation_service/elevator.cc
@@ -364,6 +364,7 @@ HRESULT Elevator::RunIsolatedChrome(DWORD flags,
       ::switches::kUninstall,
       ::switches::kUninstallAppId,
       // Logging.
+      ::switches::kEnableLogging,
       ::switches::kLoggingLevel,
       ::switches::kLogFile,
   };
