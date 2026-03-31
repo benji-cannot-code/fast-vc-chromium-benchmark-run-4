60851721c59e3059ce86c06d381998862fd9c18b
Xu Li <mxlg@google.com>
2026-03-31 15:18:23 -0700

    Create skeleton for FileRequestHandlerBase.
    The FileRequestHandlerBase will be shared across all platforms to handle
    file deep scanning request. This is part of the overall effort of
    refactoring the current FileRequestHandler in //chrome.
    
    Related design doc: go/cbe-cep-enterprise-refactor-for-bling-dd
    
    Bug: 493229596
    Change-Id: I5b720728fc1b59cb3d242348b7fd6327b6eb5fd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7718958
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Xu Li <mxlg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1608134}

---
+++ components/enterprise/connectors/core/cloud_content_scanning/files_request_handler_base.cc
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/enterprise/connectors/core/cloud_content_scanning/files_request_handler_base.h"
+
+#include "components/enterprise/connectors/core/common.h"
+
+namespace enterprise_connectors {
+
+FilesRequestHandlerBase::FilesRequestHandlerBase(
+    ContentAnalysisInfoBase* content_analysis_info,
+    BinaryUploadService* upload_service,
+    GURL url,
+    DeepScanAccessPoint access_point,
+    std::unique_ptr<FilesRequestHandlerBase::Delegate> delegate)
+    : RequestHandlerBase(content_analysis_info,
+                         upload_service,
+                         url,
+                         access_point),
+      delegate_(std::move(delegate)) {}
+
+FilesRequestHandlerBase::~FilesRequestHandlerBase() = default;
+
+void FilesRequestHandlerBase::ReportWarningBypass(
+    std::optional<std::u16string> user_justification) {
+  delegate_->ReportWarningBypass(user_justification);
+}
+
+bool FilesRequestHandlerBase::UploadDataImpl() {
+  return delegate_->UploadDataImpl();
+}
+
+}  // namespace enterprise_connectors
