220903394441cdc99f20ca5b8014c56c9417940e
Joseph Lanza <josephlanza@google.com>
2026-03-31 02:35:03 -0700

    [iOS] Add metrics for 'Save to Drive'
    Add metrics for the 'Save to Drive' sign in flow.
    
    Bug: 481988302
    Change-Id: Ic736e8d0d7929ccbbda6b2a354913894046db805
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7706255
    Reviewed-by: Quentin Pubert <qpubert@google.com>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1607742}

--- ios/chrome/browser/save_to_drive/ui_bundled/save_to_drive_coordinator.mm
+++ ios/chrome/browser/save_to_drive/ui_bundled/save_to_drive_coordinator.mm
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/account_picker/ui_bundled/account_picker_coordinator_delegate.h"
 #import "ios/chrome/browser/account_picker/ui_bundled/account_picker_logger.h"
 #import "ios/chrome/browser/authentication/ui_bundled/continuation.h"
+#import "ios/chrome/browser/authentication/ui_bundled/signin/signin_constants.h"
 #import "ios/chrome/browser/authentication/ui_bundled/signin/signin_coordinator.h"
 #import "ios/chrome/browser/authentication/ui_bundled/signin/signin_utils.h"
 #import "ios/chrome/browser/download/model/download_manager_tab_helper.h"
@@ -153,8 +154,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     if ([_mediator selectedFileDestinationRequiresSignin]) {
       _shouldShowSignIn = YES;
       [_accountPickerCoordinator stopAnimated:YES];
+      base::UmaHistogramEnumeration(
+          kSaveToDriveSignInStatus,
+          [_mediator hasIdentitiesOnDevice]
+              ? SaveToDriveSignInStatus::kSignedOutWithAccountOnDevice
+              : SaveToDriveSignInStatus::kSignedOutWithoutAccountOnDevice);
       return;
     }
+  }
+  if ([_mediator isSignedIn]) {
+    base::UmaHistogramEnumeration(kSaveToDriveSignInStatus,
+                                  SaveToDriveSignInStatus::kSignedIn);
   }
   [_mediator saveWithSelectedIdentity:identity];
 }
@@ -316,8 +326,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                             identity:(id<SystemIdentity>)identity {
   [_signinCoordinator stop];
   _signinCoordinator = nil;
-  if (result == SigninCoordinatorResultSuccess) {
-    [_mediator saveWithSelectedIdentity:identity];
+  switch (result) {
+    case SigninCoordinatorResultSuccess:
+      base::UmaHistogramEnumeration(kSaveToDriveSignInResult,
+                                    SaveToDriveSignInResult::kSignInSuccess);
+      [_mediator saveWithSelectedIdentity:identity];
+      break;
+    case SigninCoordinatorResultCanceledByUser:
+      base::UmaHistogramEnumeration(kSaveToDriveSignInResult,
+                                    SaveToDriveSignInResult::kSignInCanceled);
+      break;
+    case SigninCoordinatorProfileSwitch:
+      base::UmaHistogramEnumeration(
+          kSaveToDriveSignInResult,
+          SaveToDriveSignInResult::kSignInSuccessWithProfileSwitch);
+      break;
+    default:
+      base::UmaHistogramEnumeration(kSaveToDriveSignInResult,
+                                    SaveToDriveSignInResult::kSignInFailed);
+      break;
   }
 }
 
--- ios/chrome/browser/save_to_drive/ui_bundled/save_to_drive_mediator.mm
+++ ios/chrome/browser/save_to_drive/ui_bundled/save_to_drive_mediator.mm
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/signin/public/identity_manager/objc/identity_manager_observer_bridge.h"
 #import "google_apis/gaia/gaia_id.h"
 #import "ios/chrome/browser/account_picker/ui_bundled/account_picker_coordinator.h"
+#import "ios/chrome/browser/authentication/ui_bundled/signin/signin_utils.h"
 #import "ios/chrome/browser/download/model/download_manager_tab_helper.h"
 #import "ios/chrome/browser/download/model/download_mimetype_util.h"
 #import "ios/chrome/browser/drive/model/drive_file_uploader.h"
@@ -222,6 +223,11 @@ void StorageQuotaCompletionHelper(__weak SaveToDriveMediator* mediator,
 
 - (BOOL)selectedFileDestinationRequiresSignin {
   return _fileDestination == FileDestination::kDrive && ![self isSignedIn];
+}
+
+- (BOOL)hasIdentitiesOnDevice {
+  return [signin::GetIdentitiesOnDevice(_identityManager,
+                                        _accountManagerService) count] > 0;
 }
 
 #pragma mark - Properties getters/setters
