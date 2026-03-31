c780057be8d8d030e7e773f6d4041d3e62fd8a95
Darwin Yang <darwinyang@google.com>
2026-03-31 09:05:16 -0700

    [autofill][metric] Clean up expired VCN histogram
    Remove the `Autofill.VirtualCardEnrollBubble.ConfirmationShown`
    histogram
    
    Updated go/autofill-payments-uma.
    
    OBSOLETE_HISTOGRAMS=Cleaning up as the feature has launched and the
    metrics are no longer useful.
    
    Fixed: 497834535
    Change-Id: If14abec181905f4da5dbbe086cb8d437af1d430f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7712384
    Auto-Submit: Darwin Yang <darwinyang@chromium.org>
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Commit-Queue: Slobodan Pejic <slobodan@chromium.org>
    Reviewed-by: Olivia Saul <osaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1607901}

--- ios/chrome/browser/autofill/ui_bundled/bottom_sheet/virtual_card_enrollment_bottom_sheet_mediator.mm
+++ ios/chrome/browser/autofill/ui_bundled/bottom_sheet/virtual_card_enrollment_bottom_sheet_mediator.mm
@@ -148,9 +148,6 @@ class UiModelObserverBridge
   switch (enrollmentProgress) {
     case autofill::VirtualCardEnrollUiModel::EnrollmentProgress::kEnrolled: {
       [self.consumer showConfirmationState];
-      autofill::LogVirtualCardEnrollmentConfirmationViewShown(
-          /*is_shown=*/true,
-          /*is_card_enrolled=*/true);
       __weak __typeof__(self) weakSelf = self;
       base::SequencedTaskRunner::GetCurrentDefault()->PostDelayedTask(
           FROM_HERE, base::BindOnce(^{
@@ -163,9 +160,6 @@ class UiModelObserverBridge
       // Dismiss the virtual card enrollment bottom sheet. Failure messages are
       // expected to be initiated by the IOSChromePaymentsAutofillClient.
       [_browserCoordinatorHandler dismissVirtualCardEnrollmentBottomSheet];
-      autofill::LogVirtualCardEnrollmentConfirmationViewShown(
-          /*is_shown=*/true,
-          /*is_card_enrolled=*/false);
       break;
     case autofill::VirtualCardEnrollUiModel::EnrollmentProgress::kOffered:
       // The enrollment progress is set by IOSChromePaymentsAutofillClient to
