77a5e4ac93315872dc65baad407446e68b9d72b8
Eshwar Stalin <estalin@chromium.org>
2026-03-31 14:08:27 -0700

    [Vertical Tabs] Optimize querying for expand on hover setting
    Currently we check a pref each time when query expand on hover setting.
    Because this is done during each mouse move event, that should be
    optimized. So updating to cache the value in a variable which is updated
    only when the preference changes.
    
    Change-Id: Ifed2bf8fbf1f277324473ec1aaaa327a708d89e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7718882
    Reviewed-by: Kunal Daftari <kunaldaftari@google.com>
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1608087}

--- chrome/browser/ui/tabs/vertical_tab_strip_state_controller.h
+++ chrome/browser/ui/tabs/vertical_tab_strip_state_controller.h
@@ -100,6 +100,7 @@ class VerticalTabStripStateController : public SessionServiceBaseObserver,
   void NotifyModeChanged();
 
   void OnModeChanged();
+  void OnExpandOnHoverEnabledChanged();
 
   // Updates the SessionService with the current state (collapsed status and
   // uncollapsed width) for the associated session ID.
@@ -144,6 +145,8 @@ class VerticalTabStripStateController : public SessionServiceBaseObserver,
 
   bool is_vertical_tabs_enabled_ = false;
   int enable_state_lock_count_ = 0;
+
+  bool is_expand_on_hover_enabled_ = false;
 
   base::WeakPtrFactory<VerticalTabStripStateController> weak_ptr_factory_{this};
 };
