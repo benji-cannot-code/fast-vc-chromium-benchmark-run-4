5cde565e99d14a8143543bd5f782b05e3da5df6f
Saifuddin Hitawala <hitawala@chromium.org>
2026-03-31 06:49:28 -0700

    [viz] Add DisplayCompositorGpuThread to sampling profiler
    This change adds kDisplayCompositorGpu to
    sampling_profiler::ProfilerThreadType which is enabled for tracing
    through task posted over DisplayCompositorGpuThread task runner.
    The task runner is plumbed over to chrome/gpu from viz main.
    
    With this, we can now track the stack sampling metrics for the
    Display Compositor GPU thread which is missing.
    
    Bug: 489138114
    Change-Id: Ib565c02282e7f38f069b9d37dfc66e50acfb0a01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7663145
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Thiabaud Engelbrecht <thiabaud@google.com>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1607832}

--- components/viz/service/main/viz_main_impl.cc
+++ components/viz/service/main/viz_main_impl.cc
@@ -228,6 +228,13 @@ void VizMainImpl::CreateGpuService(
       dependencies_.shutdown_event);
 #endif
 
+  CompositorGpuThread* compositor_gpu_thread =
+      gpu_service_->compositor_gpu_thread();
+  if (delegate_ && compositor_gpu_thread) {
+    delegate_->PostDisplayCompositorGpuThreadCreated(
+        compositor_gpu_thread->task_runner().get());
+  }
+
   gpu_service_->Bind(std::move(pending_receiver));
 
   {
@@ -242,9 +249,6 @@ void VizMainImpl::CreateGpuService(
 #if BUILDFLAG(IS_ANDROID)
     viz_compositor_thread_runner_->SetGpuMainThreadId(main_thread_id);
 #endif
-
-    CompositorGpuThread* compositor_gpu_thread =
-        gpu_service_->compositor_gpu_thread();
 
     if (compositor_gpu_thread) {
       gpu_process_thread_ids.insert(compositor_gpu_thread->GetThreadId());
