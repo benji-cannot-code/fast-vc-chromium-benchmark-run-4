d4e632501c730731fb7d877b16ac348f4d1ec42f
James Cook <jamescook@chromium.org>
2026-03-30 17:00:21 -0700

    Update ENABLE_EXTENSIONS in ChromeContentBrowserClient
    Now that we have extensions on desktop Android, there's a new build flag
    for extensions ENABLE_EXTENSIONS_CORE required for "traditional
    extensions platforms + desktop Android". Switch to that where we want
    code to run on desktop Android (downloading content scripts). Clean up
    one usage that wraps instant, which should technically be !IS_ANDROID.
    
    The remaining usage is for things like isolated web apps, which are not
    supported on desktop Android and should remain ENABLE_EXTENSIONS.
    
    See bug for details on the history of the two build flags.
    
    Bug: 494643383
    Change-Id: I1ee7585d101c5cd796c82e5e426982cb95867b38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7709193
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1607476}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -638,6 +638,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/mojom/context_type.mojom.h"
 #include "extensions/common/permissions/permissions_data.h"
 #include "extensions/common/switches.h"
+#include "extensions/common/user_script.h"
 
 #if BUILDFLAG(ENABLE_GUEST_VIEW)
 #include "extensions/browser/guest_view/web_view/web_view_guest.h"
@@ -662,7 +663,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/web_applications/app_browser_controller.h"
 #include "chrome/browser/web_applications/web_app_utils.h"
 #include "extensions/browser/api/web_request/web_request_proxying_webtransport.h"
-#include "extensions/common/user_script.h"
 #endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
 #if BUILDFLAG(ENABLE_PLUGINS)
@@ -6347,7 +6347,8 @@ void AddChromeSchemeFactories(
     content::WebContents* web_contents,
     const extensions::Extension* extension,
     ChromeContentBrowserClient::NonNetworkURLLoaderFactoryMap* factories) {
-#if BUILDFLAG(ENABLE_EXTENSIONS)
+  // Android does not support instant.
+#if !BUILDFLAG(IS_ANDROID)
   Profile* profile =
       Profile::FromBrowserContext(web_contents->GetBrowserContext());
   InstantService* instant_service =
@@ -6365,7 +6366,7 @@ void AddChromeSchemeFactories(
                            frame_host, chrome::kChromeSearchScheme,
                            /*allowed_hosts=*/base::flat_set<std::string>()));
   }
-#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
+#endif  // !BUILDFLAG(IS_ANDROID)
 
   extensions::ExtensionWebContentsObserver* web_observer =
       extensions::ExtensionWebContentsObserver::GetForWebContents(web_contents);
@@ -6817,7 +6818,7 @@ bool ChromeContentBrowserClient::ShouldForceDownloadResource(
     content::BrowserContext* browser_context,
     const GURL& url,
     const std::string& mime_type) {
-#if BUILDFLAG(ENABLE_EXTENSIONS)
+#if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
   // Special-case user scripts to get downloaded instead of viewed.
   if (extensions::UserScript::IsURLUserScript(url, mime_type)) {
     return true;
@@ -6841,7 +6842,7 @@ bool ChromeContentBrowserClient::ShouldForceDownloadResource(
     }
   }
 #endif  // BUILDFLAG(IS_CHROMEOS)
-#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS_CORE)
   return false;
 }
 
