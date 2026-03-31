17c359cb1d5931c5aefc4e7d83590b9e6a08df14
David Yeung <dayeung@chromium.org>
2026-03-31 15:20:16 -0700

    [TabSearch] Remove unused index property
    TabSearch no longer needs to use the index as it pertains to the
    TabStripModel. Remove it for cleanup.
    
    Bug: 477686464
    Change-Id: I469b21395f593ef1e787709bf3d4c63b285c4b5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7718184
    Reviewed-by: Fred Shih <ffred@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1608136}

--- chrome/test/data/webui/tab_search/tab_search_test_data.ts
+++ chrome/test/data/webui/tab_search/tab_search_test_data.ts
@@ -17,7 +17,6 @@ export function createTab(overrides: Partial<Tab>): Tab {
         faviconUrl: null,
         groupId: null,
         alertStates: [],
-        index: 0,
         isDefaultFavicon: false,
         lastActiveElapsedText: '',
         lastActiveTimeTicks: {internalValue: BigInt(0)},
@@ -39,7 +38,6 @@ export const SAMPLE_WINDOW_DATA_WITH_MEDIA_TAB: Window[] = [{
     createTab({
       active: false,
       alertStates: [TabAlertState.kMediaRecording],
-      index: 0,
       tabId: 1,
       title: 'Meet',
       url: 'https://meet.google.com/',
@@ -47,7 +45,6 @@ export const SAMPLE_WINDOW_DATA_WITH_MEDIA_TAB: Window[] = [{
     }),
     createTab({
       active: false,
-      index: 1,
       tabId: 2,
       title: 'Google',
       url: 'https://www.google.com',
@@ -55,7 +52,6 @@ export const SAMPLE_WINDOW_DATA_WITH_MEDIA_TAB: Window[] = [{
     }),
     createTab({
       active: false,
-      index: 2,
       tabId: 3,
       title: 'Example',
       url: 'https://www.example.com',
@@ -77,14 +73,12 @@ export const SAMPLE_WINDOW_DATA: Window[] = [
         lastActiveTimeTicks: {internalValue: BigInt(5)},
       }),
       createTab({
-        index: 1,
         tabId: 5,
         title: 'Amazon',
         url: 'https://www.amazon.com',
         lastActiveTimeTicks: {internalValue: BigInt(4)},
       }),
       createTab({
-        index: 2,
         tabId: 6,
         title: 'Apple',
         url: 'https://www.apple.com',
@@ -98,21 +92,18 @@ export const SAMPLE_WINDOW_DATA: Window[] = [
     height: SAMPLE_WINDOW_HEIGHT,
     tabs: [
       createTab({
-        index: 0,
         tabId: 2,
         title: 'Bing',
         url: 'https://www.bing.com/',
         lastActiveTimeTicks: {internalValue: BigInt(2)},
       }),
       createTab({
-        index: 1,
         tabId: 3,
         title: 'Yahoo',
         url: 'https://www.yahoo.com',
         lastActiveTimeTicks: {internalValue: BigInt(1)},
       }),
       createTab({
-        index: 2,
         tabId: 4,
         title: 'Apple',
         url: 'https://www.apple.com/',
@@ -160,10 +151,8 @@ export function sampleSiteNames(count: number): string[] {
 
 /**
  * Generates sample tabs based on some given site names.
- * @param hasIndex Whether the items have an index property.
  */
-export function generateSampleTabsFromSiteNames(
-    siteNames: string[], hasIndex: boolean = true): Tab[] {
+export function generateSampleTabsFromSiteNames(siteNames: string[]): Tab[] {
   return siteNames.map((siteName, i) => {
     return createTab({
       tabId: i + 1,
@@ -171,7 +160,6 @@ export function generateSampleTabsFromSiteNames(
       title: siteName,
       url: 'https://www.' + siteName.toLowerCase() + '.com',
       lastActiveTimeTicks: {internalValue: BigInt(siteNames.length - i)},
-      index: hasIndex ? i : 0,
     });
   });
 }
