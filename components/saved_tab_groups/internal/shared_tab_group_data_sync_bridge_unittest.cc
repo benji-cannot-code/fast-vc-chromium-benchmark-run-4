393410ba0edf9508afa5a902038288aa29eee685
Rushan Suleymanov <rushans@google.com>
2026-03-31 06:24:07 -0700

    Validate collaboration ID before applying changes
    Bug: 495985532
    Change-Id: Ifb04139cbb68c6a205edce3065289da0e226b268
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7711295
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1607816}

--- components/saved_tab_groups/internal/shared_tab_group_data_sync_bridge_unittest.cc
+++ components/saved_tab_groups/internal/shared_tab_group_data_sync_bridge_unittest.cc
@@ -2004,6 +2004,8 @@ TEST_F(SharedTabGroupDataSyncBridgeTest,
       "http://google.com/1", u"tab", group.saved_guid(), /*position=*/0));
   model()->AddedLocally(group);
 
+  // Update the group with an unexpected collaboration ID, the group should not
+  // be updated.
   sync_pb::SharedTabGroupDataSpecifics group_update_specifics =
       MakeTabGroupSpecifics("title", sync_pb::SharedTabGroup::BLUE);
   group_update_specifics.set_guid(group.saved_guid().AsLowercaseString());
@@ -2011,9 +2013,13 @@ TEST_F(SharedTabGroupDataSyncBridgeTest,
                 group_update_specifics,
                 CollaborationId("unexpected_collaboration_id"))),
             std::nullopt);
+  EXPECT_EQ(model()->Get(group.saved_guid())->color(),
+            tab_groups::TabGroupColorId::kGrey);
 
+  // Update the tab with an unexpected collaboration ID, the tab should not be
+  // updated.
   sync_pb::SharedTabGroupDataSpecifics tab_update_specifics = MakeTabSpecifics(
-      "tab", GURL("http://google.com/1"),
+      "new tab title", GURL("http://google.com/new"),
       /*group_id=*/group.saved_guid(), GenerateRandomUniquePosition());
   tab_update_specifics.set_guid(
       group.saved_tabs()[0].saved_tab_guid().AsLowercaseString());
@@ -2021,6 +2027,7 @@ TEST_F(SharedTabGroupDataSyncBridgeTest,
                 tab_update_specifics,
                 CollaborationId("unexpected_collaboration_id"))),
             std::nullopt);
+  EXPECT_EQ(model()->Get(group.saved_guid())->saved_tabs()[0].title(), u"tab");
 }
 
 TEST_F(SharedTabGroupDataSyncBridgeTest, ShouldStoreLocalIdOnRemoteUpdate) {
