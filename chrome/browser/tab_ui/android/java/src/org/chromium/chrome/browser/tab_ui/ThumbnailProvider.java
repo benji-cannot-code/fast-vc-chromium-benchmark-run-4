b232ff93830bbd08af66a798863b25254d96e0eb
Bhuvana Betini <bbetini@google.com>
2026-03-31 12:47:09 -0700

    [Glic] Implements Actor UI Indication for Tab Groups in GTS
    This CL removes the Actor UI state for the overall tab group and instead places it on the specific quadrant of the tab group. If the thumbnail of the acting tab is visible in the tab group, a glow is placed on top of the thumbnail and the favicon is replaced with the spark icon. If the thumbnail is not visible in the tab group, then the spark icon is placed on the 4th quadrant where the "+X" panel is shown.
    
    Screenshots: https://screenshot.googleplex.com/7c2q9XsG96MXJnx.png, https://screenshot.googleplex.com/BRFYtKNZUeB7J3D.png
    
    Bug: 479912526
    Change-Id: Ib40647e6d52a293265168bf810d057dfc1809c2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7707247
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Bhuvana Betini <bbetini@google.com>
    Cr-Commit-Position: refs/heads/main@{#1608029}

--- chrome/browser/tab_ui/android/java/src/org/chromium/chrome/browser/tab_ui/ThumbnailProvider.java
+++ chrome/browser/tab_ui/android/java/src/org/chromium/chrome/browser/tab_ui/ThumbnailProvider.java
@@ -34,18 +34,21 @@ public interface ThumbnailProvider {
         public final boolean isInTabGroup;
         public final boolean isIncognito;
         public final @Nullable @TabGroupColorId Integer tabGroupColor;
+        public final List<Integer> actingTabIds;
 
         private MultiThumbnailMetadata(
                 int tabId,
                 List<GURL> urlList,
                 boolean isInTabGroup,
                 boolean isIncognito,
-                @Nullable @TabGroupColorId Integer tabGroupColor) {
+                @Nullable @TabGroupColorId Integer tabGroupColor,
+                List<Integer> actingTabIds) {
             this.tabId = tabId;
             this.urlList = urlList;
             this.isInTabGroup = isInTabGroup;
             this.isIncognito = isIncognito;
             this.tabGroupColor = tabGroupColor;
+            this.actingTabIds = actingTabIds;
         }
 
         /** Create a {@link MultiThumbnailMetadata} object with a urlList. */
@@ -56,7 +59,12 @@ public interface ThumbnailProvider {
                 boolean isIncognito,
                 @Nullable @TabGroupColorId Integer tabGroupColor) {
             return new MultiThumbnailMetadata(
-                    tabId, urlList, isInTabGroup, isIncognito, tabGroupColor);
+                    tabId,
+                    urlList,
+                    isInTabGroup,
+                    isIncognito,
+                    tabGroupColor,
+                    Collections.emptyList());
         }
 
         /** Create a {@link MultiThumbnailMetadata} object without requiring a urlList. */
@@ -66,7 +74,29 @@ public interface ThumbnailProvider {
                 boolean isIncognito,
                 @Nullable @TabGroupColorId Integer tabGroupColor) {
             return new MultiThumbnailMetadata(
-                    tabId, Collections.emptyList(), isInTabGroup, isIncognito, tabGroupColor);
+                    tabId,
+                    Collections.emptyList(),
+                    isInTabGroup,
+                    isIncognito,
+                    tabGroupColor,
+                    Collections.emptyList());
+        }
+
+        /** Create a {@link MultiThumbnailMetadata} object with actingTabIds. */
+        public static MultiThumbnailMetadata createMetadataWithActingTabs(
+                int tabId,
+                boolean isInTabGroup,
+                boolean isIncognito,
+                @Nullable @TabGroupColorId Integer tabGroupColor,
+                List<Integer> actingTabIds) {
+            Collections.sort(actingTabIds);
+            return new MultiThumbnailMetadata(
+                    tabId,
+                    Collections.emptyList(),
+                    isInTabGroup,
+                    isIncognito,
+                    tabGroupColor,
+                    actingTabIds);
         }
 
         @Override
@@ -76,7 +106,8 @@ public interface ThumbnailProvider {
                     this.urlList,
                     this.isInTabGroup,
                     this.isIncognito,
-                    this.tabGroupColor);
+                    this.tabGroupColor,
+                    this.actingTabIds);
         }
 
         @Override
@@ -86,7 +117,8 @@ public interface ThumbnailProvider {
                     && Objects.equals(this.urlList, other.urlList)
                     && this.isInTabGroup == other.isInTabGroup
                     && this.isIncognito == other.isIncognito
-                    && Objects.equals(this.tabGroupColor, other.tabGroupColor);
+                    && Objects.equals(this.tabGroupColor, other.tabGroupColor)
+                    && Objects.equals(this.actingTabIds, other.actingTabIds);
         }
     }
 
