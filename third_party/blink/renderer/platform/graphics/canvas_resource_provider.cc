799e220cb9b804b147102f5f1c3bc3255bc447ff
Colin Blundell <blundell@chromium.org>
2026-03-30 23:59:40 -0700

    [Blink] Introduce CNon2DRPSI::DoExternalDrawAndProduceResource()
    Have ImageBitmapRenderingContext use it. This is part of the workstream
    toward being able to ensure that all draws to this class' canvas are
    immediately followed by flushing the canvas internally.
    
    Bug: 352263194
    Change-Id: I381f3ec396987f1d94a82d28bfd1661ce8995ad5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7711574
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1607669}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
@@ -1021,6 +1021,13 @@ void CanvasNon2DResourceProviderSharedImage::ExternalCanvasDrawHelper(
   draw_callback(GetCanvasDeprecated());
 }
 
+scoped_refptr<CanvasResource>
+CanvasNon2DResourceProviderSharedImage::DoExternalDrawAndProduceResource(
+    base::FunctionRef<void(cc::PaintCanvas&)> draw_callback) {
+  ExternalCanvasDrawHelper(draw_callback);
+  return ProduceCanvasResource();
+}
+
 scoped_refptr<StaticBitmapImage>
 CanvasNon2DResourceProviderSharedImage::DoExternalDrawAndSnapshot(
     base::FunctionRef<void(cc::PaintCanvas&)> draw_callback,
