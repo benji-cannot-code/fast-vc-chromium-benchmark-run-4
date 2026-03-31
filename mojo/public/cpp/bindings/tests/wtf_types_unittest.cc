efd61b9ff10f7bc11c47bc4762ec922b2e1a21a6
Kent Tamura <tkent@chromium.org>
2026-03-30 21:49:18 -0700

    WTF: Remove deprecated String::FromUTF8()
    This CL removes the deprecated String::FromUTF8() methods and updates
    all call sites to use String::FromUtf8() instead. The FromUTF8 methods
    were deprecated as they simply forwarded to FromUtf8 and are no longer
    needed.
    
    Bug: 492927412
    Change-Id: I0055b96becdb1a48b73e982ea959dcab67219384
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7707740
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1607616}

--- mojo/public/cpp/bindings/tests/wtf_types_unittest.cc
+++ mojo/public/cpp/bindings/tests/wtf_types_unittest.cc
@@ -70,7 +70,7 @@ class WTFTypesTest : public testing::Test {
   // strs[1] is empty.
   strs[1] = "";
   strs[2] = kHelloWorld;
-  strs[3] = ::blink::String::FromUTF8(kUTF8HelloWorld);
+  strs[3] = ::blink::String::FromUtf8(kUTF8HelloWorld);
 
   return strs;
 }
@@ -80,7 +80,7 @@ class WTFTypesTest : public testing::Test {
   // A null string as value.
   str_map.insert("0", ::blink::String());
   str_map.insert("1", kHelloWorld);
-  str_map.insert("2", ::blink::String::FromUTF8(kUTF8HelloWorld));
+  str_map.insert("2", ::blink::String::FromUtf8(kUTF8HelloWorld));
 
   return str_map;
 }
@@ -141,7 +141,7 @@ TEST_F(WTFTypesTest, Serialization_WTFVectorInlineCapacity) {
   // strs[1] is empty.
   strs[1] = "";
   strs[2] = kHelloWorld;
-  strs[3] = ::blink::String::FromUTF8(kUTF8HelloWorld);
+  strs[3] = ::blink::String::FromUtf8(kUTF8HelloWorld);
   auto cloned_strs = strs;
 
   mojo::Message message(0, 0, 0, 0, nullptr);
