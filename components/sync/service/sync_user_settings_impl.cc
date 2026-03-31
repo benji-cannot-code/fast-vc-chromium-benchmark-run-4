48a49cdd8237a7cfaed566df4ac718db8e153094
Tanmoy Mollik <triploblastic@google.com>
2026-03-31 09:21:49 -0700

    Remove unused histogram Signin.SyncFirstSetupCompleteSource
    OBSOLETE_HISTOGRAMS: Histogram already expired
    
    Bug: 347710361
    Change-Id: I3931618525a60004e5ebbec11ff90772bfdb050d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7714921
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1607913}

--- components/sync/service/sync_user_settings_impl.cc
+++ components/sync/service/sync_user_settings_impl.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/feature_list.h"
 #include "base/logging.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/version.h"
 #include "components/signin/public/base/gaia_id_hash.h"
 #include "components/signin/public/base/signin_switches.h"
@@ -83,7 +82,6 @@ void SyncUserSettingsImpl::SetInitialSyncFeatureSetupComplete(
   if (IsInitialSyncFeatureSetupComplete()) {
     return;
   }
-  UMA_HISTOGRAM_ENUMERATION("Signin.SyncFirstSetupCompleteSource", source);
   prefs_->SetInitialSyncFeatureSetupComplete();
   delegate_->OnInitialSyncFeatureSetupCompleted();
 }
