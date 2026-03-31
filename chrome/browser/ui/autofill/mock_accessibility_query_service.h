637d378d0b5578f8d19f844ff1421fe8ace93211
Ireneusz Szulc <ireneuszszulc@google.com>
2026-03-31 08:03:03 -0700

    Autofill: Differentiate hybrid search modes
    This change introduces a way to distinguish between incremental search
    (as-you-type) and full search (on Enter key) for the Accessibility Query
    Service.
    
    It introduces `FilterSource` enum in `AutofillPopupController` to
    indicate why a filter was applied (due to input change, search
    submission, or tab selection).  It updates
    `AccessibilityQueryService::Query` to accept a `full_search` boolean
    argument.  `PopupViewViews` is updated to provide the correct
    `FilterSource`.
    
    Bug: b:485778418
    Change-Id: I3718bafb3257ace15f06bfd6b77aefd36a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7706295
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Ireneusz Szulc <ireneuszszulc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1607868}

--- chrome/browser/ui/autofill/mock_accessibility_query_service.h
+++ chrome/browser/ui/autofill/mock_accessibility_query_service.h
@@ -24,6 +24,7 @@ class MockAccessibilityQueryService
       void,
       Query,
       (std::u16string_view query,
+       bool full_search,
        base::RepeatingCallback<
            void(accessibility_annotator::MemorySearchResults)> update_callback),
       (override));
