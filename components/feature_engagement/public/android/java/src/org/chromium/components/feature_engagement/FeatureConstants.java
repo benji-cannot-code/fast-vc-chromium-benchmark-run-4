0bcec6599dfbe918bb485ddcd42c99a10802cf97
Mark Schillaci <mschillaci@google.com>
2026-03-30 23:11:37 -0700

    [User Engagement] Add anchor comments to constants files
    This CL adds comments to the FeatureConstants and EventConstants files
    which act as "anchors" to denote sections of the files that scripts
    can easily find for generating boilerplate code.
    
    Bug: 495515287
    Change-Id: I5e564e7de2a3d43436f35ea903bad1b831386c8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7712249
    Auto-Submit: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1607654}

--- components/feature_engagement/public/android/java/src/org/chromium/components/feature_engagement/FeatureConstants.java
+++ components/feature_engagement/public/android/java/src/org/chromium/components/feature_engagement/FeatureConstants.java
@@ -18,6 +18,7 @@ import java.lang.annotation.RetentionPolicy;
  */
 @StringDef({
     // keep-sorted start
+    // FEATURE_CONSTANTS_JAVA_STRING_DEF_START
     FeatureConstants.ACCOUNT_SETTINGS_HISTORY_SYNC,
     FeatureConstants.ADAPTIVE_BUTTON_IN_TOP_TOOLBAR_CUSTOMIZATION_ADD_TO_BOOKMARKS_FEATURE,
     FeatureConstants.ADAPTIVE_BUTTON_IN_TOP_TOOLBAR_CUSTOMIZATION_GLIC_FEATURE,
@@ -143,11 +144,13 @@ import java.lang.annotation.RetentionPolicy;
     FeatureConstants.VIDEO_TUTORIAL_NTP_VOICE_SEARCH_FEATURE,
     FeatureConstants.VIDEO_TUTORIAL_TRY_NOW_FEATURE,
     FeatureConstants.WEB_FEED_AWARENESS_FEATURE,
+    // FEATURE_CONSTANTS_JAVA_STRING_DEF_END
     // keep-sorted end
 })
 @Retention(RetentionPolicy.SOURCE)
 @NullMarked
 public @interface FeatureConstants {
+    // FEATURE_CONSTANTS_JAVA_INTERFACE_START
     String ACCOUNT_SETTINGS_HISTORY_SYNC = "IPH_AccountSettingsHistorySync";
 
     String ADAPTIVE_BUTTON_IN_TOP_TOOLBAR_CUSTOMIZATION_ADD_TO_BOOKMARKS_FEATURE =
@@ -529,4 +532,5 @@ public @interface FeatureConstants {
 
     /** An IPH used for web feed awareness to be shown on the NTP for the Web Feed tab. */
     String WEB_FEED_AWARENESS_FEATURE = "IPH_WebFeedAwareness";
+    // FEATURE_CONSTANTS_JAVA_INTERFACE_END
 }
