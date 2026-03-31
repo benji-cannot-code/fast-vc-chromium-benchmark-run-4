ca2c6d2125d5d9e0e651e3512ff41293ded9fa8c
Olivia Saul <osaul@google.com>
2026-03-30 17:01:12 -0700

    [AF] OmniboxAutofillDelegate requires complete credit card form
    Fixed: 490215195
    Change-Id: I5c9f9d2b6ac3de500c4378d3633aff19d5367b0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7706666
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Commit-Queue: Olivia Saul <osaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1607477}

--- components/autofill/core/browser/ui/payments/omnibox_autofill_delegate.cc
+++ components/autofill/core/browser/ui/payments/omnibox_autofill_delegate.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check_deref.h"
 #include "components/autofill/core/browser/data_manager/payments/payments_data_manager.h"
+#include "components/autofill/core/browser/form_structure.h"
 #include "components/autofill/core/browser/foundations/autofill_client.h"
 #include "components/autofill/core/browser/foundations/autofill_manager.h"
 #include "components/autofill/core/browser/foundations/scoped_autofill_managers_observation.h"
@@ -63,6 +64,21 @@ void OmniboxAutofillDelegate::OnFieldTypesDetermined(
           .empty()) {
     LogOmniboxAutofillShowChipDecisionPart1(
         OmniboxAutofillShowChipDecisionPart1::kNoCreditCardsSaved);
+    return;
+  }
+
+  // The parsed form must have credit card number and expiration date fields.
+  const FormStructure* form_structure = manager.FindCachedFormById(form_id);
+  if (!form_structure) {
+    LogOmniboxAutofillShowChipDecisionPart1(
+        OmniboxAutofillShowChipDecisionPart1::kCouldNotFindCachedForm);
+    return;
+  }
+  if (!form_structure->IsCompleteCreditCardForm(
+          autofill::FormStructure::CreditCardFormCompleteness::
+              kCompleteCreditCardForm)) {
+    LogOmniboxAutofillShowChipDecisionPart1(
+        OmniboxAutofillShowChipDecisionPart1::kNotCompleteCreditCardForm);
     return;
   }
 
