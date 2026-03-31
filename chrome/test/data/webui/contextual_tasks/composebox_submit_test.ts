2735ff17b03ee5fa228c3388453a6eb547818fc3
Bofeng Chen <bofengchen@google.com>
2026-03-30 19:28:01 -0700

    [composebox] Remove redundant getter "getMatchesElement"
    This change updates calls to the getter for the
    ComposeboxDropdownElement within the Composebox component to use
    `getDropdownElement`, and removes the old `getMatchesElement` method.
    
    Bug: b:496651073
    Change-Id: Idfd9fde913e1a11996c5904c5a6ffdc091920fd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7706736
    Auto-Submit: Bofeng Chen <bofengchen@google.com>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Bofeng Chen <bofengchen@google.com>
    Reviewed-by: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Cr-Commit-Position: refs/heads/main@{#1607565}

--- chrome/test/data/webui/contextual_tasks/composebox_submit_test.ts
+++ chrome/test/data/webui/contextual_tasks/composebox_submit_test.ts
@@ -198,7 +198,7 @@ suite('ContextualTasksComposeboxSubmitTest', () => {
         searchboxCallbackRouterRemote, TEST_QUERY, mockTimer);
 
     // Wait for the matches to be populated.
-    while (!composebox.getMatchesElement().result) {
+    while (!composebox.getDropdownElement().result) {
       mockTimer.tick(10);
       await Promise.resolve();
     }
@@ -223,7 +223,7 @@ suite('ContextualTasksComposeboxSubmitTest', () => {
         '', inputElement.value,
         'Input should be cleared, but input = ' + inputElement.value);
     assertEquals(
-        null, composebox.getMatchesElement().result,
+        null, composebox.getDropdownElement().result,
         'Matches should be cleared');
   });
 
