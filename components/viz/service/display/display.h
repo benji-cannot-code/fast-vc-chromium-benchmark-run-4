1f769c7984e02218591df28fd73f33b1745524a0
Igor Kraskevich <kraskevich@google.com>
2026-03-31 10:38:12 -0700

    Revert "Reland "ADPF: report different actual duration for frames with throttled swaps""
    This reverts commit 7a92eb441f4b5ce500355ea90fa437dfd4c65231.
    
    The experiment didn't help based on field data, so cleaning up the
    code.
    
    Bug: 469586679
    Change-Id: Ic9c29435cfc3edee397e89a846bc987cc95aa653
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7715059
    Reviewed-by: Kartar Singh <kartarsingh@google.com>
    Commit-Queue: Igor Kraskevich <kraskevich@google.com>
    Cr-Commit-Position: refs/heads/main@{#1607963}

--- components/viz/service/display/display.h
+++ components/viz/service/display/display.h
@@ -256,8 +256,7 @@ class VIZ_SERVICE_EXPORT Display : public DisplaySchedulerClient,
                 int64_t choreographer_vsync_id,
                 int64_t swap_trace_id,
                 std::optional<PossibleDeadline> deadline,
-                std::optional<PossibleDeadline> preferred,
-                base::TimeTicks throttled_adjusted_frame_time);
+                std::optional<PossibleDeadline> preferred);
     void OnSwap(gfx::SwapTimings timings, DisplaySchedulerBase* scheduler);
     bool HasSwapped() const { return !swap_timings_.is_null(); }
     void OnPresent(const gfx::PresentationFeedback& feedback);
@@ -291,7 +290,6 @@ class VIZ_SERVICE_EXPORT Display : public DisplaySchedulerClient,
     int64_t swap_trace_id_ = 0;
     std::optional<PossibleDeadline> deadline_;
     std::optional<PossibleDeadline> preferred_;
-    base::TimeTicks throttled_adjusted_frame_time_;
   };
 
   void InitializeRenderer();
