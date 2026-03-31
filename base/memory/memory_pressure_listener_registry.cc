75644a1d4a68f68e37fd15d9e48650843c349195
Patrick Monette <pmonette@chromium.org>
2026-03-31 07:01:19 -0700

    Enable SuppressMemoryListeners on ChromeOS
    Bug: 443264587
    Change-Id: I85ff6f3e2879fd43078cb6a9ee7bdf73657edd4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7710807
    Commit-Queue: Alex Attar <aattar@google.com>
    Auto-Submit: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Alex Attar <aattar@google.com>
    Cr-Commit-Position: refs/heads/main@{#1607838}

--- base/memory/memory_pressure_listener_registry.cc
+++ base/memory/memory_pressure_listener_registry.cc
@@ -22,7 +22,7 @@ namespace {
 MemoryPressureListenerRegistry* g_memory_pressure_listener_registry = nullptr;
 
 BASE_FEATURE(kSuppressMemoryListeners,
-#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN)
+#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN) || BUILDFLAG(IS_CHROMEOS)
              FEATURE_ENABLED_BY_DEFAULT
 #else
              FEATURE_DISABLED_BY_DEFAULT
@@ -35,6 +35,8 @@ BASE_FEATURE_PARAM(std::string,
                    "suppress_memory_listeners_mask",
 #if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN)
                    "0200200202220200020020020002020020000002000000020"
+#elif BUILDFLAG(IS_CHROMEOS)
+                   "0000000200000200000000000000000000000000000000000"
 #else
                    ""
 #endif
