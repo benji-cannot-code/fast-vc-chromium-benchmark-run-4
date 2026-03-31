f2f478ae827518aa7a53fe387da4cd2aa5035a20
Stefano Duo <stefanoduo@google.com>
2026-03-31 03:59:14 -0700

    Generalize HostResolverDnsTask's secure parameter to an enum
    HostResolverDnsTask no longer supports only secure vs insecure DNS. This
    makes the secure boolean no longer sufficient. Extend
    HostResolverDnsTask by plumbing DnsTransactionFactory::AttemptMode up.
    
    This also lets us create HostResolverDnsTask backed by
    DnsTransactionFactory::AttemptMode::kPlatform, which is useful to test
    built-in resolver parsing logic when kPlatform is used.
    
    Bug: 448975408
    Change-Id: Ib5e28c2d84ba922988154710da481d5a95f2c888
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7710800
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1607768}

--- net/dns/host_resolver_dns_task.h
+++ net/dns/host_resolver_dns_task.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/ip_endpoint.h"
 #include "net/base/net_export.h"
 #include "net/base/request_priority.h"
+#include "net/dns/dns_transaction.h"
 #include "net/dns/host_resolver.h"
 #include "net/dns/httpssvc_metrics.h"
 #include "net/dns/public/secure_dns_mode.h"
@@ -33,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace net {
 
 class DnsClient;
-class DnsTransaction;
 class DnsResponse;
 class HostResolverInternalResult;
 class HostResolverInternalErrorResult;
@@ -94,7 +94,7 @@ class NET_EXPORT_PRIVATE HostResolverDnsTask final {
                       NetworkAnonymizationKey anonymization_key,
                       DnsQueryTypeSet query_types,
                       ResolveContext* resolve_context,
-                      bool secure,
+                      DnsTransactionFactory::AttemptMode attempt_mode,
                       SecureDnsMode secure_dns_mode,
                       Delegate* delegate,
                       const NetLogWithSource& job_net_log,
@@ -114,7 +114,15 @@ class NET_EXPORT_PRIVATE HostResolverDnsTask final {
     return base::checked_cast<int>(transactions_in_progress_.size());
   }
 
-  bool secure() const { return secure_; }
+  bool secure() const {
+    switch (attempt_mode_) {
+      case DnsTransactionFactory::AttemptMode::kHttp:
+        return true;
+      case DnsTransactionFactory::AttemptMode::kClassic:
+      case DnsTransactionFactory::AttemptMode::kPlatform:
+        return false;
+    }
+  }
 
   bool https_disabled() const { return https_disabled_; }
 
@@ -230,8 +238,7 @@ class NET_EXPORT_PRIVATE HostResolverDnsTask final {
 
   base::SafeRef<ResolveContext> resolve_context_;
 
-  // Whether lookups in this DnsTask should occur using DoH or plaintext.
-  const bool secure_;
+  const DnsTransactionFactory::AttemptMode attempt_mode_;
   const SecureDnsMode secure_dns_mode_;
 
   // The listener to the results of this DnsTask.
