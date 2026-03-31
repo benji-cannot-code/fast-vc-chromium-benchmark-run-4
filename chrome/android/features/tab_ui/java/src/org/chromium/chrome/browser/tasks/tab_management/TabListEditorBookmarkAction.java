24b1cdf08c945d85f575e01a513e96620593f4aa
Michael Wu <mklw@google.com>
2026-03-31 16:34:48 -0700

    Support keyboard shortcut Ctrl+Shift+D to bookmark all tabs
    Saves all open tabs as bookmarks in a new folder
    
    OBSOLETE_HISTOGRAM[Android.TabStrip.BookmarkAllTabs.Result]=Replaced by Android.BookmarkAllTabs.Result
    OBSOLETE_HISTOGRAM[Accessibility.Android.KeyboardShortcut.ScreenReaderRunning5]=Replaced by Accessibility.Android.KeyboardShortcut.ScreenReaderRunning6
    OBSOLETE_HISTOGRAM[Accessibility.Android.KeyboardShortcut.NoScreenReader5]=Replaced by Accessibility.Android.KeyboardShortcut.NoScreenReader6
    
    Bug: 440241598
    Change-Id: I1e15f5fc2616c92ab6f5c524924c2662bbbde598
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7624818
    Reviewed-by: Brandon Fong <bjfong@google.com>
    Reviewed-by: Evan Liu <evliu@google.com>
    Commit-Queue: Michael Wu <mklw@google.com>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1608179}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListEditorBookmarkAction.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListEditorBookmarkAction.java
@@ -11,6 +11,7 @@ import android.graphics.drawable.Drawable;
 import androidx.appcompat.content.res.AppCompatResources;
 
 import org.chromium.base.ResettersForTesting;
+import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.browser.bookmarks.BookmarkManagerOpenerImpl;
@@ -93,12 +94,17 @@ public class TabListEditorBookmarkAction extends TabListEditorAction {
             BookmarkModel bookmarkModel = BookmarkModel.getForProfile(profile);
             bookmarkModel.finishLoadingBookmarkModel(
                     () -> {
-                        BookmarkUtils.addBookmarksOnMultiSelect(
-                                activity,
-                                bookmarkModel,
-                                tabs,
-                                snackbarManager,
-                                new BookmarkManagerOpenerImpl());
+                        int count =
+                                BookmarkUtils.addTabsToBookmarks(
+                                        activity,
+                                        bookmarkModel,
+                                        tabs,
+                                        snackbarManager,
+                                        new BookmarkManagerOpenerImpl());
+                        if (count > 1) {
+                            RecordHistogram.recordCount100Histogram(
+                                    "Android.TabMultiSelectV2.BookmarkTabsCount", count);
+                        }
                     });
         }
     }
--- chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
@@ -60,6 +60,7 @@ import org.chromium.chrome.browser.app.tabmodel.ArchivedTabModelOrchestrator;
 import org.chromium.chrome.browser.app.tabwindow.TabWindowManagerSingleton;
 import org.chromium.chrome.browser.automotivetoolbar.AutomotiveBackButtonToolbarCoordinator;
 import org.chromium.chrome.browser.back_press.BackPressManager;
+import org.chromium.chrome.browser.bookmarks.BookmarkAllTabsHandler;
 import org.chromium.chrome.browser.bookmarks.BookmarkModel;
 import org.chromium.chrome.browser.bookmarks.TabBookmarker;
 import org.chromium.chrome.browser.browser_controls.BottomControlsStacker;
@@ -403,6 +404,7 @@ public class RootUiCoordinator
     private @Nullable ReaderModeBottomSheetManager mReaderModeBottomSheetManager;
     private @Nullable AppMenuObserver mAppMenuObserver;
     private @Nullable LinkHoverStatusBarCoordinator mLinkHoverStatusBarCoordinator;
+    private @Nullable BookmarkAllTabsHandler mBookmarkAllTabsHandler;
 
     private final OneshotSupplierImpl<ToolbarManager> mToolbarManagerOneshotSupplier =
             new OneshotSupplierImpl<>();
@@ -601,6 +603,12 @@ public class RootUiCoordinator
                 savedIncognitoReauthPending || persistedIncognitoReauthPending;
 
         mMenuOrKeyboardActionController.registerMenuOrKeyboardActionHandler(this);
+
+        mBookmarkAllTabsHandler =
+                new BookmarkAllTabsHandler(
+                        mTabModelSelectorSupplier, mSnackbarManagerSupplier, mWindowAndroid);
+        mMenuOrKeyboardActionController.registerMenuOrKeyboardActionHandler(
+                mBookmarkAllTabsHandler);
 
         // This little bit of arithmetic is necessary because of Java doesn't like accepting
         // Supplier<BaseImpl> where Supplier<Base> is expected. We should remove the need for
@@ -812,6 +820,11 @@ public class RootUiCoordinator
         //  other than the mActivity. If the nulling calls are not necessary, we can remove them.
         mCallbackController.destroy();
         mMenuOrKeyboardActionController.unregisterMenuOrKeyboardActionHandler(this);
+        if (mBookmarkAllTabsHandler != null) {
+            mMenuOrKeyboardActionController.unregisterMenuOrKeyboardActionHandler(
+                    mBookmarkAllTabsHandler);
+            mBookmarkAllTabsHandler = null;
+        }
 
         SnackbarManager bottomSheetSnackbarManager = mBottomSheetSnackbarManagerSupplier.get();
         if (bottomSheetSnackbarManager != null) {
