f8e9022615e0942027a9e23bfa35d57aa95f1282
Steinar H. Gunderson <sesse@chromium.org>
2026-03-31 01:51:16 -0700

    Call AdjustStyleForFirstLetter() from only one place.
    There's no need for this to happen in both branches of the if,
    which is a bit confusing.
    
    Also update the comment a bit for the current situation.
    
    Change-Id: I4908debfe420fca08fa6ba100dd0025a0ed7eaad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7702865
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1607723}

--- third_party/blink/renderer/core/css/resolver/style_adjuster.cc
+++ third_party/blink/renderer/core/css/resolver/style_adjuster.cc
@@ -1178,9 +1178,6 @@ void StyleAdjuster::AdjustComputedStyle(StyleResolverState& state,
                              : EDisplay::kInline);
     }
 
-    // We don't adjust the first letter style earlier because we may change the
-    // display setting in AdjustStyleForHTMLElement() above.
-    AdjustStyleForFirstLetter(builder, parent_style);
     AdjustStyleForMarker(builder, parent_style, &state.GetElement());
 
     if (builder.StyleType() != kPseudoIdScrollMarker) {
@@ -1230,9 +1227,12 @@ void StyleAdjuster::AdjustComputedStyle(StyleResolverState& state,
       // layout children of the container element.
       builder.SetContain(builder.Contain() | kContainsLayout);
     }
-  } else {
-    AdjustStyleForFirstLetter(builder, parent_style);
   }
+
+  // We don't adjust the first letter style earlier because we may change the
+  // display setting above (including in AdjustStyleForHTMLElement()),
+  // and this needs to override those changes.
+  AdjustStyleForFirstLetter(builder, parent_style);
 
   builder.SetForcesStackingContext(false);
 
