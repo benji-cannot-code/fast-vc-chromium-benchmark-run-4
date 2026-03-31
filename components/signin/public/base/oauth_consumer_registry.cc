32668c96b690aa688f28b004bb244912a7288289
Oleksandr Tara <otara@google.com>
2026-03-30 17:38:34 -0700

    [Actor Login M2] Use both chrome sync and the agentic scope
    This is needed as we are transitioning to the new scope.
    
    Bug: b:491035927
    Change-Id: Ia2b1a905cf16f748e8e7c26d04b520e1e21e99d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7710801
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Reviewed-by: Monica Salama <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1607508}

--- components/signin/public/base/oauth_consumer_registry.cc
+++ components/signin/public/base/oauth_consumer_registry.cc
@@ -568,7 +568,8 @@ OAuthConsumer OAuthConsumerRegistry::GetOAuthConsumerFromId(
       if (base::FeatureList::IsEnabled(kActorLoginUseChromeSyncScope)) {
         return OAuthConsumer(
             /*name=*/kActorLoginPermissionServiceName,
-            /*scopes=*/{GaiaConstants::kChromeSyncOAuth2Scope});
+            /*scopes=*/{GaiaConstants::kChromeSyncOAuth2Scope,
+                        GaiaConstants::kAgenticPermissionOAuth2Scope});
       } else {
         return OAuthConsumer(
             /*name=*/kActorLoginPermissionServiceName,
