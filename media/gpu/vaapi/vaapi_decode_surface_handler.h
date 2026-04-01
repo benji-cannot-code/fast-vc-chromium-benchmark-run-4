79ca62a876adb859337e57e10c237b5e6a310e3d
Christopher Cameron <ccameron@chromium.org>
2026-04-01 02:14:16 -0700

    media::VaapiVideoDecoder: Plumb HDR metadata
    Rename VaapiVideoDecoder::hdr_metadata_ to static_hdr_metadata_ to make
    a clear distinction between static and dynamic metadata.
    
    Add a dynamic HDR metadata parameter to VaapiVideoDecoder::SurfaceReady.
    Change all callsites to set that parameter to the value that came from
    the CodecPicture.
    
    Merge the static and dynamic metadata together and attach them to the
    VideoFrame.
    
    Bug: 395659818
    Change-Id: Ia78f295f454c1e32c7285d8f1d36b01eaa8f647e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7718239
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1608376}

--- media/gpu/vaapi/vaapi_decode_surface_handler.h
+++ media/gpu/vaapi/vaapi_decode_surface_handler.h
@@ -9,6 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/gpu/vaapi/vaapi_utils.h"
 #include "ui/gfx/geometry/rect.h"
 
+namespace gfx {
+struct HDRMetadata;
+}  // namespace gfx
+
 namespace media {
 
 class VASurface;
@@ -42,7 +46,8 @@ class VaapiDecodeSurfaceHandler {
   virtual void SurfaceReady(VASurfaceID va_surface_id,
                             int32_t bitstream_id,
                             const gfx::Rect& visible_rect,
-                            const VideoColorSpace& color_space) = 0;
+                            const VideoColorSpace& color_space,
+                            const gfx::HDRMetadata& dynamic_hdr_metadata) = 0;
 };
 
 }  // namespace media
