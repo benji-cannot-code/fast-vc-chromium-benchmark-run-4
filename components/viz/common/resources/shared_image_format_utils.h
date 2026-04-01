71c3bc09d3d591090269e2bdc1fbc33b962d8dc1
Takashi Sakamoto <tasak@google.com>
2026-03-31 21:48:11 -0700

    Add CHECK() to ensure the given SharedImageFormat matches the io surface pixel format.
    Bug: 497531791
    Change-Id: Icd6861530814067886c65c4dd7baa7d919b164d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7710673
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Owners-Override: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1608285}

--- components/viz/common/resources/shared_image_format_utils.h
+++ components/viz/common/resources/shared_image_format_utils.h
@@ -7,7 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_VIZ_COMMON_RESOURCES_SHARED_IMAGE_FORMAT_UTILS_H_
 
 #include "base/component_export.h"
+#include "build/build_config.h"
 #include "components/viz/common/resources/shared_image_format.h"
+
+#if BUILDFLAG(IS_MAC)
+// Includes `MacTypes.h` for an io surface pixel format, i.e. `OSType`.
+#include <MacTypes.h>
+#endif  // BUILDFLAG(IS_MAC)
 
 enum SkColorType : int;
 
@@ -130,6 +136,17 @@ bool IsOddSizeMultiPlanarBuffersAllowed();
 // Returns a span containing all mappable SharedImageFormats.
 COMPONENT_EXPORT(VIZ_SHARED_IMAGE_FORMAT)
 base::span<const SharedImageFormat> GetMappableSharedImageFormatForTesting();
+
+#if BUILDFLAG(IS_MAC)
+// Returns whether the given shared image format matches io surface pixel
+// format or not.
+COMPONENT_EXPORT(VIZ_SHARED_IMAGE_FORMAT)
+bool MatchesSharedImageFormatWithIOSurfacePixelFormat(
+    const SharedImageFormat& format,
+    OSType io_surface_format,
+    bool override_rgba_to_brga);
+#endif  // BUILDFLAG(IS_MAC)
+
 }  // namespace viz
 
 #endif  // COMPONENTS_VIZ_COMMON_RESOURCES_SHARED_IMAGE_FORMAT_UTILS_H_
