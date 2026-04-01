0d4eef5e8cef837659b88800d3e30d49805e656a
Olivier Robin <olivierrobin@google.com>
2026-04-01 05:29:43 -0700

    Use the Cobalt JavaScript feature
    This is part of a 5 part CL to create the cobalt JS feature.
     - crrev.com/c/7662244 declare the origin filter provider
     - crrev.com/i/9099636 implement the origin filter provider
     - crrev.com/c/7653176 declare the JS feature provider
     - crrev.com/i/9102596 implement the JS feature provider
    => crrev.com/c/7670857 use the JS feature
    
    
    Change-Id: I53f527b25ca400a76bb3533dfa4cce841f8fb213
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7670857
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1608455}

--- ios/chrome/browser/web/model/chrome_web_client.mm
+++ ios/chrome/browser/web/model/chrome_web_client.mm
@@ -115,6 +115,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/components/ui_util/dynamic_type_util.h"
 #import "ios/components/webui/web_ui_url_constants.h"
 #import "ios/net/protocol_handler_util.h"
+#import "ios/public/provider/chrome/browser/cobalt/cobalt_api.h"
 #import "ios/public/provider/chrome/browser/fullscreen/fullscreen_api.h"
 #import "ios/public/provider/chrome/browser/url_rewriters/url_rewriters_api.h"
 #import "ios/web/common/features.h"
@@ -472,6 +473,13 @@ std::vector<web::JavaScriptFeature*> ChromeWebClient::GetJavaScriptFeatures(
     }
     if (MediaAPIUsageJavaScriptFeature::ShouldOverrideAPI()) {
       features.push_back(MediaAPIUsageJavaScriptFeature::GetInstance());
+    }
+  }
+  if (web::features::IsCobaltEnabled()) {
+    web::JavaScriptFeature* cobalt_feature =
+        ios::provider::GetCobaltJavascriptFeatureForProfile(profile);
+    if (cobalt_feature) {
+      features.push_back(cobalt_feature);
     }
   }
 
