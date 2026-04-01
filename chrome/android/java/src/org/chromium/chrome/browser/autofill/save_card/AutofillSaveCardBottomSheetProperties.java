37d3a37b5c943c0d0a017ba581845e88d85ef854
Fernando Ramirez <ferny@google.com>
2026-04-01 15:48:41 -0700

    [AF][Clank] Add GPay pill icon on card upstream bottom sheet
    This CL adds the GPay pill icon in the card suggestion during card
    upstream. The icon is added in crrev.com/i/9151937 and a placeholder for
    non-branded builds is added in this CL. This icon will only appear for
    server card saves on branded builds.
    
    Mock: https://screenshot.googleplex.com/8wVWmjdJiuXWJgE.png
    After: https://screenshot.googleplex.com/AfPFdAc5PS5rDyp.png
    
    Bug: 468139053
    Change-Id: I8934199ec9310ed31aa1511edcfe3572fe9859c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7713272
    Reviewed-by: Olivia Saul <osaul@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Fernando Ramirez <ferny@google.com>
    Reviewed-by: Darwin Yang <darwinyang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1608830}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill/save_card/AutofillSaveCardBottomSheetProperties.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/save_card/AutofillSaveCardBottomSheetProperties.java
@@ -85,6 +85,9 @@ import java.util.function.Consumer;
     static final ReadableObjectPropertyKey<String> LOADING_DESCRIPTION =
             new ReadableObjectPropertyKey<>();
 
+    /** The Google Pay pill logo. */
+    static final ReadableIntPropertyKey GOOGLE_PAY_PILL_LOGO = new ReadableIntPropertyKey();
+
     static final PropertyKey[] ALL_KEYS = {
         TITLE,
         DESCRIPTION,
@@ -98,7 +101,8 @@ import java.util.function.Consumer;
         ACCEPT_BUTTON_LABEL,
         CANCEL_BUTTON_LABEL,
         SHOW_LOADING_STATE,
-        LOADING_DESCRIPTION
+        LOADING_DESCRIPTION,
+        GOOGLE_PAY_PILL_LOGO,
     };
 
     /** Do not instantiate. */
