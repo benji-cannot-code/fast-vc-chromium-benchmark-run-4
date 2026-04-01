6bd18441fad11d38998a96ad6a0c0450cc52f640
James Cook <jamescook@chromium.org>
2026-03-31 17:32:42 -0700

    Update ENABLE_EXTENSIONS flag in //chrome/browser/chooser_controller
    Now that we have extensions on desktop Android, there's a new build flag
    ENABLE_EXTENSIONS_CORE required for "traditional extensions platforms +
    desktop Android". Switch to that, so we get the same test coverage on
    desktop Android as we do on Win, Mac, Linux and Chrome OS.
    
    See bug for details on the history of the two build flags.
    
    Bug: 494643383
    Change-Id: Ic6512f554db69ac58cf5afd915c15c0a91039df3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7707245
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1608208}

--- chrome/browser/chooser_controller/title_util_unittest.cc
+++ chrome/browser/chooser_controller/title_util_unittest.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 #include "url/origin.h"
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
+#if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 #include "base/values.h"
 #include "chrome/browser/extensions/extension_service.h"  // nogncheck
 #include "chrome/browser/extensions/test_extension_system.h"
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_registry.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/extension_builder.h"
-#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 
 #if !BUILDFLAG(IS_ANDROID)
 #include "base/test/gmock_expected_support.h"
@@ -59,7 +59,7 @@ TEST_F(CreateChooserTitleTest, UrlFrameTree) {
             CreateChooserTitle(subframe, kTitleResourceId));
 }
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
+#if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 TEST_F(CreateChooserTitleTest, ExtensionsFrameTree) {
   auto manifest = base::DictValue()
                       .Set("name", "Chooser Title Subframe Test")
@@ -92,7 +92,7 @@ TEST_F(CreateChooserTitleTest, ExtensionsFrameTree) {
   EXPECT_EQ(u"Chooser Title Subframe Test wants to connect",
             CreateChooserTitle(subframe, kTitleResourceId));
 }
-#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 
 #if !BUILDFLAG(IS_ANDROID)
 TEST_F(CreateChooserTitleTest, IsolatedWebAppFrameTree) {
