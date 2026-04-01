c51d566cc9a076299d687d5f44cbd82b8aa82efe
Federica Germinario <fedegermi@google.com>
2026-04-01 08:29:30 -0700

    [ios] Implement execute for kOpenReadingList NSUserActivity
    Add implementation to handle opening reading list from Siri Shortcuts in
    TaskRequestForUserActivity.
    
    Tests will be added in a follow-up CL.
    
    Note that this solution does not work with cold start. The reason is
    that for reading list intents, waiting for ui to be ready is not
    sufficient to execute the intent (reading list model should also be
    loaded before executing the action). Implementation to support this will
    be added in a different CL.
    
    Bug: 492115056
    Change-Id: If2a3a9b423bd93bfec8c10b3f9d9f8777e52c19c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7719538
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1608549}

--- ios/chrome/app/task_request_user_activity.mm
+++ ios/chrome/app/task_request_user_activity.mm
@@ -248,6 +248,15 @@ UserActivityType UserActivityTypeOf(NSUserActivity* user_activity) {
   return UserActivityType::kInvalid;
 }
 
+// Returns a completion block that opens the reading list.
+void OpenReadingListWithBrowser(base::WeakPtr<Browser> weak_browser) {
+  if (Browser* browser = weak_browser.get()) {
+    id<BrowserCoordinatorCommands> handler = HandlerForProtocol(
+        browser->GetCommandDispatcher(), BrowserCoordinatorCommands);
+    [handler showReadingList];
+  }
+}
+
 // Navigates to the bookmark manager UI.
 void OpenBookmarksWithBrowser(base::WeakPtr<Browser> weak_browser) {
   if (Browser* browser = weak_browser.get()) {
@@ -383,7 +392,9 @@ std::vector<GURL> GetURLsFromOpenInChromeIntent(INIntent* intent) {
       // TODO(crbug.com/492115056): Add implementation.
       break;
     case UserActivityType::kOpenReadingList:
-      // TODO(crbug.com/492115056): Add implementation.
+      completion = base::CallbackToBlock(base::BindRepeating(
+          &OpenReadingListWithBrowser, browser->AsWeakPtr()));
+      webpageGURLs.push_back(GURL(kChromeUINewTabURL));
       break;
     case UserActivityType::kOpenBookmarks:
       completion = base::CallbackToBlock(
