8b9dfc4f33026956459696237576df9baf846850
adamta <adamta@google.com>
2026-04-01 08:37:34 -0700

    [iOS] Gemini client migration flag
    Bug: 498572524
    Change-Id: If0f8fa1f730155d82288df8771c046563613f266
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7719046
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Auto-Submit: Adam Arcaro <adamta@google.com>
    Reviewed-by: Yasaman Sedaghat <yasamans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1608557}

--- ios/chrome/browser/flags/about_flags.mm
+++ ios/chrome/browser/flags/about_flags.mm
@@ -2736,6 +2736,9 @@ constexpr auto kFeatureEntries = std::to_array<flags_ui::FeatureEntry>({
     {"ios-actor-tools", flag_descriptions::kIOSActorToolsName,
      flag_descriptions::kIOSActorToolsDescription, flags_ui::kOsIos,
      FEATURE_VALUE_TYPE(kActorTools)},
+    {"gemini-client-migration", flag_descriptions::kGeminiClientMigrationName,
+     flag_descriptions::kGeminiClientMigrationDescription, flags_ui::kOsIos,
+     FEATURE_VALUE_TYPE(kGeminiClientMigration)},
 });
 
 bool SkipConditionalFeatureEntry(const flags_ui::FeatureEntry& entry) {
