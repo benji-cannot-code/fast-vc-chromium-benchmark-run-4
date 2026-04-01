534bfaff4ab749497fa4a6972810cef2a4194900
christianxu <christianxu@chromium.org>
2026-04-01 07:33:05 -0700

    [iOS][Composebox] Enable conditional plus button and fix regression
    Restore the behavior of conditional plus button and enable it by
    default.
    
    Bug: None
    Change-Id: Ia681b9d752fc3a18fbb6029885f955dac815e8df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7720682
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1608517}

--- ios/chrome/browser/composebox/public/features.mm
+++ ios/chrome/browser/composebox/public/features.mm
@@ -103,7 +103,7 @@ bool IsComposeboxFetchContextualSuggestionsForMultiAttachmentsEnabled() {
 }
 
 BASE_FEATURE(kComposeboxConditionalPlusButton,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 const char kComposeboxConditionalPlusButtonParam[] =
     "ComposeboxConditionalPlusButtonParam";
