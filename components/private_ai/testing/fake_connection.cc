b8f867c001a95de2a48ae1575854bda425d9834f
Oleh Lamzin <lamzin@google.com>
2026-04-01 02:48:24 -0700

    private_ai: Remove split callback
    Remove base::SplitOnceCallback as it is error-prone and might lead to
    crashes at runtime.
    
    Bug: b:496093625
    Change-Id: I241e4b02539a66f6672110d52da4568462cff4d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7643950
    Reviewed-by: Mohamad Arab <arabm@google.com>
    Commit-Queue: Oleh Lamzin <lamzin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1608394}

--- components/private_ai/testing/fake_connection.cc
+++ components/private_ai/testing/fake_connection.cc
@@ -25,7 +25,7 @@ FakeConnection::PendingRequest& FakeConnection::PendingRequest::operator=(
 FakeConnection::PendingRequest::~PendingRequest() = default;
 
 FakeConnection::FakeConnection(
-    base::OnceCallback<void(ErrorCode)> on_disconnect,
+    base::RepeatingCallback<void(ErrorCode)> on_disconnect,
     base::OnceClosure on_destruction)
     : on_disconnect_(std::move(on_disconnect)),
       on_destruction_(std::move(on_destruction)) {}
@@ -49,7 +49,6 @@ void FakeConnection::Send(proto::PrivateAiRequest request,
 }
 
 void FakeConnection::OnDestroy(ErrorCode error) {
-  on_disconnect_.Reset();
   auto callbacks = std::move(pending_requests_);
   for (auto& pending_request : callbacks) {
     std::move(pending_request.callback).Run(base::unexpected(error));
@@ -57,9 +56,7 @@ void FakeConnection::OnDestroy(ErrorCode error) {
 }
 
 void FakeConnection::SimulateDisconnect() {
-  if (on_disconnect_) {
-    std::move(on_disconnect_).Run(ErrorCode::kNetworkError);
-  }
+  on_disconnect_.Run(ErrorCode::kNetworkError);
 }
 
 }  // namespace private_ai
