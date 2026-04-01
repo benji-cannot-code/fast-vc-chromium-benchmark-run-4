6cdd726755fe659a19022f4a35b23c6f383a9ef5
Ioana Pandele <ioanap@chromium.org>
2026-04-01 08:41:08 -0700

    Initialize the ActorLoginPermissionCleaningFactory
    This was forgotten in the original CL.
    
    The factory is now extracted into the public actor_login:factories
    target.
    
    Bug: b:486089293
    Change-Id: I3cb5a9f86b354d47d1c9494fcef07bb48a4b0689
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7719741
    Commit-Queue: Ioana Treib <ioanap@chromium.org>
    Reviewed-by: Oleksandr Tara <otara@google.com>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1608563}

--- chrome/browser/password_manager/actor_login/actor_login_permission_cleaning_service_factory.cc
+++ chrome/browser/password_manager/actor_login/actor_login_permission_cleaning_service_factory.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/password_manager/actor_login/internal/actor_login_permission_cleaning_service_factory.h"
+#include "chrome/browser/password_manager/actor_login/actor_login_permission_cleaning_service_factory.h"
 
 #include "chrome/browser/password_manager/actor_login/actor_login_permission_service_factory.h"
 #include "chrome/browser/password_manager/actor_login/internal/actor_login_permission_cleaning_service.h"
