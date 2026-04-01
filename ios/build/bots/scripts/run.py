faa97afe3ee9ed1a302370e3b82d2646326c570f
Cameron Higgins <cameronhiggins@google.com>
2026-04-01 14:00:27 -0700

    Create feature flag for ios simulator caches.
    Creates feature flag for iOS test runner to enable use of simulator caches and passes down into the relevant functions.
    
    Bug: 497833905
    Change-Id: I093d21e3ef52550b023dcad935943b246a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7649798
    Reviewed-by: Yue She <yueshe@google.com>
    Reviewed-by: Will Yeager <wyeager@google.com>
    Commit-Queue: Cameron Higgins <cameronhiggins@google.com>
    Cr-Commit-Position: refs/heads/main@{#1608763}

--- ios/build/bots/scripts/run.py
+++ ios/build/bots/scripts/run.py
@@ -157,6 +157,7 @@ class Runner():
             env_vars=env_vars,
             record_video_option=self.args.record_video,
             output_disabled_tests=self.args.output_disabled_tests,
+            use_simulator_cache=self.args.use_simulator_cache,
         )
       elif self.args.variations_seed_path != 'NO_PATH':
         tr = variations_runner.VariationsSimulatorParallelTestRunner(
@@ -171,7 +172,9 @@ class Runner():
             release=self.args.release,
             test_cases=self.args.test_cases,
             test_args=self.test_args,
-            env_vars=env_vars)
+            env_vars=env_vars,
+            use_simulator_cache=self.args.use_simulator_cache,
+        )
       elif self.args.iossim and self.args.platform and self.args.version:
         tr = test_runner.SimulatorTestRunner(
             self.args.app,
@@ -189,6 +192,7 @@ class Runner():
             use_clang_coverage=self.args.use_clang_coverage,
             xctest=self.args.xctest,
             output_disabled_tests=self.args.output_disabled_tests,
+            use_simulator_cache=self.args.use_simulator_cache,
         )
       elif self.args.xcodebuild_device_runner and self.args.xctest:
         tr = xcodebuild_runner.DeviceXcodeTestRunner(
@@ -506,6 +510,11 @@ class Runner():
         '--output-disabled-tests',
         action='store_true',
         help='Whether or not disabled test should be included in test output.',
+    )
+    parser.add_argument(
+        '--use-simulator-cache',
+        action='store_true',
+        help='Whether to use a cache of prelaunched simulators for this run.',
     )
 
     def load_from_json(args):
