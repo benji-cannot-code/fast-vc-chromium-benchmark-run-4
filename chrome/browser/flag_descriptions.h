a6f1d6022fd3618ad8f5ff641da4327531f4f876
Takashi Nakayama <tnak@chromium.org>
2026-04-01 02:33:00 -0700

    [persistent_cache] Add chrome://flags#use-persistent-cache-for-code-cache
    This CL adds chrome://flags UI to enable PersistentCache code cache.
    
    Bug: 374930286
    Change-Id: Iadf5cf7073e75eeb9e993622d4015f08b5b06d15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7720028
    Auto-Submit: Takashi Nakayama <tnak@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Takashi Nakayama <tnak@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1608387}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -4796,6 +4796,12 @@ inline constexpr char kUsePassthroughCommandDecoderDescription[] =
     "Use chrome passthrough command decoder instead of validating command "
     "decoder.";
 
+inline constexpr char kUsePersistentCacheForCodeCacheName[] =
+    "Use PersistentCache for Code Cache";
+inline constexpr char kUsePersistentCacheForCodeCacheDescription[] =
+    "Use PersistentCache backend for code cache for JavaScript and "
+    "WebAssembly.";
+
 inline constexpr char kUsePrimaryAndTonalButtonsForPromosName[] =
     "Use primary and tonal buttons for promos";
 inline constexpr char kUsePrimaryAndTonalButtonsForPromosDescription[] =
