398cdcdb05b7cd71caca83e61383b23d18478329
Kevin <kevinshah@google.com>
2026-03-31 18:01:20 -0700

    Add Contextual Tasks Native page for Android
    This CL implements Contextual Tasks Native Page as a wrapper for
    CoBrowse Views to host WebUI on Android as a Native page. Updated C++
    interception logic to route AI task to native page.
    
    Bug: b:493305503
    Change-Id: I52f0c8db8fe9d31a2d32144bc351f3ac224701f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7694022
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Kevin Shah <kevinshah@google.com>
    Cr-Commit-Position: refs/heads/main@{#1608228}

--- chrome/browser/ui/android/native_page/java/src/org/chromium/chrome/browser/ui/native_page/NativePage.java
+++ chrome/browser/ui/android/native_page/java/src/org/chromium/chrome/browser/ui/native_page/NativePage.java
@@ -179,7 +179,8 @@ public interface NativePage {
         NativePageType.HISTORY,
         NativePageType.EXPLORE,
         NativePageType.MANAGEMENT,
-        NativePageType.PDF
+        NativePageType.PDF,
+        NativePageType.CONTEXTUAL_TASKS
     })
     @Retention(RetentionPolicy.SOURCE)
     @interface NativePageType {
@@ -193,6 +194,7 @@ public interface NativePage {
         int EXPLORE = 7;
         int MANAGEMENT = 8;
         int PDF = 9;
+        int CONTEXTUAL_TASKS = 10;
     }
 
     /**
@@ -286,6 +288,8 @@ public interface NativePage {
                 return NativePageType.NONE;
             }
             return NativePageType.MANAGEMENT;
+        } else if (UrlConstants.CONTEXTUAL_TASKS_HOST.equals(host)) {
+            return NativePageType.CONTEXTUAL_TASKS;
         } else {
             return NativePageType.NONE;
         }
