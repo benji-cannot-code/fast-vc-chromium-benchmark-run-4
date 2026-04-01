8c5cd72325cb9d77ecb9cef68d86f2b38e8bc194
Oleh Lamzin <lamzin@google.com>
2026-04-01 02:57:10 -0700

    private_ai: Create ConnectionUnusedTimeout
    Create ConnectionUnusedTimeout implementation of Connection interface
    which schedules a timer to destroy itself if unused after certain period
    of time after creation.
    
    Bug: b:496093625
    Change-Id: I277e7d206451915e8e75f87ec0fa99c96e3b1f46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7698279
    Reviewed-by: Mohamad Arab <arabm@google.com>
    Commit-Queue: Oleh Lamzin <lamzin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1608405}

--- components/private_ai/connection_factory_impl.cc
+++ components/private_ai/connection_factory_impl.cc
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/private_ai/connection_proxy.h"
 #include "components/private_ai/connection_timeout.h"
 #include "components/private_ai/connection_token_attestation.h"
+#include "components/private_ai/connection_unused_timeout.h"
+#include "components/private_ai/features.h"
 #include "components/private_ai/phosphor/token_manager.h"
 #include "components/private_ai/secure_channel.h"
 #include "components/private_ai/secure_channel_impl.h"
@@ -52,6 +54,10 @@ std::unique_ptr<Connection> CreateConnectionStack(
   }
 
   connection = std::make_unique<ConnectionMetrics>(std::move(connection));
+
+  connection = std::make_unique<ConnectionUnusedTimeout>(
+      std::move(connection), on_disconnect,
+      kPrivateAiUnusedConnectionTimeout.Get());
 
   return connection;
 }
