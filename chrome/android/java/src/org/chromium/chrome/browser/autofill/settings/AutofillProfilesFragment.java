fa5bdcaf7216a67084da9b82f78ab91c8a14a91e
Timofey Chudakov <tchudakov@google.com>
2026-04-01 06:13:11 -0700

    [AutofillAi - M4] Notify EntityEditor of configuration changes.
    Bug: 497725286, 497724811
    Change-Id: I1fe621b29b0f542f8f6b4d3d8a4da04fc58e5e2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7718802
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1608473}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill/settings/AutofillProfilesFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/settings/AutofillProfilesFragment.java
@@ -218,6 +218,9 @@ public class AutofillProfilesFragment extends ChromeBaseSettingsFragment
         if (mAddressEditor != null) {
             mAddressEditor.onConfigurationChanged();
         }
+        if (mEntityEditor != null) {
+            mEntityEditor.onConfigurationChanged();
+        }
     }
 
     @Override
