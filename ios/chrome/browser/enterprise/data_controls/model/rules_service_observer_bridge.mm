30349b26449ef2feb78fd92004253fa7d78066bb
Ernesto Izquierdo Clua <eic@google.com>
2026-04-01 14:52:21 -0700

    Create RulesServiceObserverBridge
    This observation protocol is needed for the implementation of Enterprise
    Screenshot Protection on Chrome iOS.
    
    Bug: 485585995
    Change-Id: I4d7831f0031cfd9b22d8c98f069f66a26a282cc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7722778
    Reviewed-by: Yixi Wu <yixiwu@google.com>
    Commit-Queue: Yixi Wu <yixiwu@google.com>
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Auto-Submit: Ernesto Izquierdo Clua <eic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1608790}

---
+++ ios/chrome/browser/enterprise/data_controls/model/rules_service_observer_bridge.mm
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/enterprise/data_controls/model/rules_service_observer_bridge.h"
+
+namespace data_controls {
+
+RulesServiceObserverBridge::RulesServiceObserverBridge(
+    id<RulesServiceObserving> observer)
+    : observer_(observer) {}
+
+RulesServiceObserverBridge::~RulesServiceObserverBridge() = default;
+
+void RulesServiceObserverBridge::OnRulesUpdated() {
+  [observer_ onRulesUpdated];
+}
+
+}  // namespace data_controls
