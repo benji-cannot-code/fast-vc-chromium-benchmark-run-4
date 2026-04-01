be5319a9f1315350e17ef123112559e03df083ca
Xiang Ji <jxiang@google.com>
2026-04-01 08:28:33 -0700

    Modularize chrome/browser/plugins into its own BUILD.gn
    Extract all sources from //chrome/browser:browser into a dedicated
    chrome/browser/plugins/BUILD.gn, following the incremental modularization
    pattern. Introduces a :plugins target (headers, clean deps) and an :impl
    target (cc files, circular dep via browser_public_dependencies), plus
    :test_support, :unit_tests, and :browser_tests targets. Moves the
    corresponding test sources out of chrome/test/BUILD.gn.
    
    Bug: 497966835
    Change-Id: I12dbe63ab26e76b99a2ceef253debc95af02c0e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7712830
    Commit-Queue: Xiang Ji <jxiang@google.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1608548}

--- chrome/browser/plugins/plugin_info_host_impl.cc
+++ chrome/browser/plugins/plugin_info_host_impl.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/plugins/plugin_metadata.h"
 #include "chrome/browser/plugins/plugin_utils.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/browser_otr_state.h"
 #include "chrome/common/buildflags.h"
 #include "chrome/common/chrome_content_client.h"
 #include "chrome/common/plugin.mojom.h"
