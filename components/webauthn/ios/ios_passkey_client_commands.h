49f2cae925c9115510ef593d37ae3b0d4a6087c0
Sourav <sinhasourav@google.com>
2026-04-01 12:43:48 -0700

    [iOSPasskeysM3] Implement UI dismissal for canceled passkey requests
    This CL implements the actual UI dismissal logic for canceled passkey
    requests via AbortSignal.
    
    Bug: 491303127
    Change-Id: I7781fb3ac59042efddae5e12a42d90595ec33d0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7718120
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Sourav Uttam Sinha <sinhasourav@google.com>
    Cr-Commit-Position: refs/heads/main@{#1608722}

--- components/webauthn/ios/ios_passkey_client_commands.h
+++ components/webauthn/ios/ios_passkey_client_commands.h
@@ -34,6 +34,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     (webauthn::IOSPasskeyClient::InterstitialCallback)callback;
 - (void)dismissPasskeyIncognitoInterstitial;
 
+// Cancels the passkey request matching the given `requestInfo`, dismissing any
+// active passkey UI.
+- (void)cancelPasskeyRequest:
+    (webauthn::IOSPasskeyClient::RequestInfo)requestInfo;
+
 @end
 
 #endif  // COMPONENTS_WEBAUTHN_IOS_IOS_PASSKEY_CLIENT_COMMANDS_H_
