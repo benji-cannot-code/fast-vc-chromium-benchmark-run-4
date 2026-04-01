6bdd0b80016aa6fd84a54df34a75fe144b7659b3
Joey Arhar <jarhar@chromium.org>
2026-04-01 14:46:11 -0700

    Remove CustomizableSelectListbox flag
    This has been enabled for 3 milestones (since M145), so it is safe to
    remove now.
    
    Bug: 357649033
    Change-Id: I962bae49da021b11f18568c7ec7552cf70baad4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7684220
    Reviewed-by: David Baron <dbaron@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1608784}

--- third_party/blink/renderer/core/html/forms/html_option_element.cc
+++ third_party/blink/renderer/core/html/forms/html_option_element.cc
@@ -149,7 +149,6 @@ FocusableState HTMLOptionElement::SupportsFocus(
     bool base_with_picker =
         select->UsesMenuList() && popover && popover->popoverOpen();
     bool base_in_page =
-        RuntimeEnabledFeatures::CustomizableSelectListboxEnabled() &&
         !select->UsesMenuList() && select->IsAppearanceBase();
     if (base_with_picker || base_in_page) {
       // If this option is being rendered as regular web content inside a
@@ -171,8 +170,7 @@ bool HTMLOptionElement::IsKeyboardFocusableSlow(
   if (!HTMLElement::IsKeyboardFocusableSlow(update_behavior)) {
     return false;
   }
-  if (!RuntimeEnabledFeatures::CustomizableSelectListboxEnabled() ||
-      !OwnerSelectElement() || OwnerSelectElement()->UsesMenuList()) {
+  if (!OwnerSelectElement() || OwnerSelectElement()->UsesMenuList()) {
     return true;
   }
 
@@ -666,7 +664,6 @@ void HTMLOptionElement::DefaultEventHandlerInternal(Event& event) {
   }
 
   const bool appearance_base_in_page =
-      RuntimeEnabledFeatures::CustomizableSelectListboxEnabled() &&
       !select->UsesMenuList() && select->IsAppearanceBase();
 
   if (!appearance_base_in_page && !select->PickerIsPopover()) {
--- third_party/blink/web_tests/virtual/customizable-select-listbox-disabled/README.md
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-This is a virtual test suite which enables the CustomizableSelect flag but
-disables the follow-up work for customizable select in the
-CustomizableSelectInPage flag. This represents behavior that we are currently
-shipping in stable.
-
-Flags: --disable-features=CustomizableSelectInPage --enable-features=CustomizableSelect
-Bug: crbug.com/1511354
