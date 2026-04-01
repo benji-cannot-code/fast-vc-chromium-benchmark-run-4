56403e591a4a46e92eaf0bbb0587c1387144fbbb
Leo Zhao <leozhao@google.com>
2026-04-01 15:52:03 -0700

    [iOS][Forms AI] Use the right icon and show Google Wallet text for entities in Settings
    This CL adds right icons for entity cells in Settings. It also adds Google Wallet text when the entity is a server instance.
    
    Screenshots of larger text and smaller text (When the entity has a long name, then Google Wallet text is compressed):
    https://screenshot.googleplex.com/Aj8y8joSjHZfRZ7
    https://screenshot.googleplex.com/5EaPaxnMgLCb2zR
    
    Bug: 480934103
    Change-Id: I7dafc488f509f7db738cd1bde7f4f293d894726f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7722079
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Leo Zhao <leozhao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1608833}

--- ios/chrome/browser/settings/ui_bundled/autofill/autofill_profile_table_view_controller.mm
+++ ios/chrome/browser/settings/ui_bundled/autofill/autofill_profile_table_view_controller.mm
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/prefs/pref_service.h"
 #import "components/strings/grit/components_strings.h"
 #import "components/sync/service/sync_user_settings.h"
+#import "ios/chrome/browser/autofill/autofill_ai/public/autofill_ai_ui_util.h"
 #import "ios/chrome/browser/autofill/model/autofill_ai_util.h"
 #import "ios/chrome/browser/autofill/model/ios_autofill_entity_data_manager_factory.h"
 #import "ios/chrome/browser/autofill/model/ios_autofill_entity_data_manager_observer_bridge.h"
@@ -427,14 +428,13 @@ ItemType ItemTypeForEntitySectionHeader(SectionIdentifier section_identifier) {
       base::SysUTF16ToNSString(instance.type().GetNameForI18n());
   item.guid = instance.guid();
 
-  if (instance.record_type() ==
-      autofill::EntityInstance::RecordType::kServerWallet) {
+  if (instance.IsServerInstance()) {
     item.isServerWalletItem = YES;
     item.trailingText = l10n_util::GetNSString(IDS_IOS_AUTOFILL_WALLET_TEXT);
   }
 
-  item.icon = DefaultSymbolTemplateWithPointSize(kPersonCropCircleSymbol,
-                                                 kEntityIconPointSize);
+  item.icon = autofill::DefaultIconForAutofillAiEntityType(
+      instance.type().name(), kEntityIconPointSize);
   return item;
 }
 
