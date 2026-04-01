4ac4c2b614fef7c2823087337d34bb88979f25e7
Aditya Batra <adibatra@google.com>
2026-03-31 21:10:25 -0700

    [A11Y] Map Android slider gestures to kIncrement/kDecrement
    When using TalkBack to swipe up or down on a custom ARIA slider, the
    gestures fail to update the slider's value. This occurs because custom
    ARIA sliders rely on synthesized keyboard events to update their state,
    per the W3C APG specifications.
    
    Previously, WebContentsAccessibilityAndroid::AdjustSlider attempted to
    calculate a custom delta and inject it via SetValue(). This approach
    failed for custom sliders, and contained a bug where a missing
    aria-valuestep attribute resulted in a delta of 0.0f, causing early
    termination.
    
    This CL fixes the issue by mapping TalkBack's ACTION_SCROLL_FORWARD and
    ACTION_SCROLL_BACKWARD gestures to ax::mojom::Action::kIncrement and
    ax::mojom::Action::kDecrement for sliders.
    
    For more context - go/fixingslidergestures
    
    Fixed: 495450622
    Change-Id: I7430208c356293742d646a6758c2847a5505590a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7711735
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Aditya Batra <adibatra@google.com>
    Reviewed-by: Kyra Seevers <kyraseevers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1608277}

--- content/public/android/javatests/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityTest.java
@@ -1902,7 +1902,7 @@ public class WebContentsAccessibilityTest {
             // Confirm slider values.
             Assert.assertEquals(
                     INPUT_RANGE_VALUE_MISMATCH,
-                    20 + (2 * i),
+                    20 + i,
                     mNodeInfo.getRangeInfo().getCurrent(),
                     0.001);
 
@@ -1923,7 +1923,7 @@ public class WebContentsAccessibilityTest {
             // Confirm slider values.
             Assert.assertEquals(
                     INPUT_RANGE_VALUE_MISMATCH,
-                    40 - (2 * i),
+                    30 - i,
                     mNodeInfo.getRangeInfo().getCurrent(),
                     0.001);
 
@@ -1993,7 +1993,7 @@ public class WebContentsAccessibilityTest {
     @SmallTest
     public void testNodeInfo_inputTypeRange_withRequiredMin() throws Throwable {
         // Create a basic input range, and find the associated |AccessibilityNodeInfo| object.
-        setupTestWithHTML("<input type='range' min='0' max='1000' step='1'>");
+        setupTestWithHTML("<input type='range' min='0' max='1000' step='50'>");
 
         // Find the input range and assert we have the correct node.
         int inputNodeVirtualViewId = waitForNodeMatching(sRangeInfoMatcher, "");
