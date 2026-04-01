2d9ee87c42c881b0847513f48a06004460725227
christianxu <christianxu@chromium.org>
2026-04-01 07:13:16 -0700

    [iOS][Composebox] Guard snackbard handler
    Similar to this CL:
    https://chromium-review.git.corp.google.com/c/chromium/src/+/7596620
    
    Guard the snackbar handler as these might be called during shutdown
    where the snackbar command is no longer dispatched.
    
    Bug: None
    Change-Id: I6240b835e77d2ba33f4180d42573f1d8f440d411
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7717122
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Radu Nitescu <radunitescu@google.com>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1608504}

--- ios/chrome/browser/composebox/ui/composebox_snackbar_presenter.mm
+++ ios/chrome/browser/composebox/ui/composebox_snackbar_presenter.mm
@@ -34,10 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       IDS_IOS_COMPOSEBOX_MAXIMUM_TABS_REACHED, attachmentLimit);
   SnackbarMessage* message = [[SnackbarMessage alloc] initWithTitle:title];
 
-  CommandDispatcher* dispatcher = _browser->GetCommandDispatcher();
-  id<SnackbarCommands> snackbarHandler =
-      HandlerForProtocol(dispatcher, SnackbarCommands);
-  [snackbarHandler showSnackbarMessage:message bottomOffset:0];
+  [self.snackbarHandler showSnackbarMessage:message bottomOffset:0];
 }
 
 - (void)showSnackbarForAttachmentLimit:(NSUInteger)attachmentLimit {
@@ -50,10 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       IDS_IOS_COMPOSEBOX_MAXIMUM_ATTACHMENTS_REACHED, attachmentLimit);
   SnackbarMessage* message = [[SnackbarMessage alloc] initWithTitle:title];
 
-  CommandDispatcher* dispatcher = _browser->GetCommandDispatcher();
-  id<SnackbarCommands> snackbarHandler =
-      HandlerForProtocol(dispatcher, SnackbarCommands);
-  [snackbarHandler showSnackbarMessage:message bottomOffset:bottomOffset];
+  [self.snackbarHandler showSnackbarMessage:message bottomOffset:bottomOffset];
 }
 
 - (void)showUnableToAddAttachmentSnackbarWithBottomOffset:
@@ -62,10 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       l10n_util::GetNSString(IDS_IOS_COMPOSEBOX_UNABLE_TO_ADD_ATTACHMENT);
   SnackbarMessage* message = [[SnackbarMessage alloc] initWithTitle:title];
 
-  CommandDispatcher* dispatcher = _browser->GetCommandDispatcher();
-  id<SnackbarCommands> snackbarHandler =
-      HandlerForProtocol(dispatcher, SnackbarCommands);
-  [snackbarHandler showSnackbarMessage:message bottomOffset:bottomOffset];
+  [self.snackbarHandler showSnackbarMessage:message bottomOffset:bottomOffset];
 }
 
 - (void)showAttachmentLimitForImageGenerationSnackbarWithBottomOffset:
@@ -75,10 +66,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       kAttachmentLimitForImageGeneration);
   SnackbarMessage* message = [[SnackbarMessage alloc] initWithTitle:title];
 
-  CommandDispatcher* dispatcher = _browser->GetCommandDispatcher();
-  id<SnackbarCommands> snackbarHandler =
-      HandlerForProtocol(dispatcher, SnackbarCommands);
-  [snackbarHandler showSnackbarMessage:message bottomOffset:bottomOffset];
+  [self.snackbarHandler showSnackbarMessage:message bottomOffset:bottomOffset];
 }
 
 - (void)showCannotReloadTabError {
@@ -86,19 +74,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       l10n_util::GetNSString(IDS_IOS_COMPOSEBOX_CANNOT_RELOAD_TAB_ERROR);
   SnackbarMessage* message = [[SnackbarMessage alloc] initWithTitle:title];
 
-  CommandDispatcher* dispatcher = _browser->GetCommandDispatcher();
-  id<SnackbarCommands> snackbarHandler =
-      HandlerForProtocol(dispatcher, SnackbarCommands);
-  [snackbarHandler showSnackbarMessage:message bottomOffset:0];
+  [self.snackbarHandler showSnackbarMessage:message bottomOffset:0];
 }
 
 - (void)dismissAllSnackbars {
+  [self.snackbarHandler dismissAllSnackbars];
+}
+
+#pragma mark - Private
+
+// Returns the snackbar handler if it exist.
+- (id<SnackbarCommands>)snackbarHandler {
+  if (!_browser) {
+    return nil;
+  }
   CommandDispatcher* dispatcher = _browser->GetCommandDispatcher();
   if ([dispatcher dispatchingForProtocol:@protocol(SnackbarCommands)]) {
     id<SnackbarCommands> snackbarHandler =
         HandlerForProtocol(dispatcher, SnackbarCommands);
-    [snackbarHandler dismissAllSnackbars];
+    return snackbarHandler;
   }
+  return nil;
 }
 
 @end
