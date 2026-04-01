6c3dd67c11eaffd1f010d07251510a85c110f4a7
Ritika Gupta <ritikagup@google.com>
2026-04-01 14:51:30 -0700

    [Actor] Updating interrupted notification a as per UX.
    Bug: 495516337
    Change-Id: If2e6ca362fec0605186fc11d08ad5cd5068bf9a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7712234
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: Hailey Wang <haileywang@google.com>
    Commit-Queue: Ritika Gupta <ritikagup@google.com>
    Cr-Commit-Position: refs/heads/main@{#1608789}

--- chrome/browser/actor/android/java/src/org/chromium/chrome/browser/actor/ActorNotificationFactoryTest.java
+++ chrome/browser/actor/android/java/src/org/chromium/chrome/browser/actor/ActorNotificationFactoryTest.java
@@ -243,7 +243,7 @@ public class ActorNotificationFactoryTest {
 
         assertEquals(
                 "Content title should match paused status for fallback",
-                mContext.getString(R.string.actor_notification_title_task_paused),
+                mContext.getString(R.string.actor_notification_title_task_interrupted),
                 shadowNotification.getContentTitle());
         assertEquals(
                 "Content text should match interrupted template",
@@ -256,8 +256,6 @@ public class ActorNotificationFactoryTest {
         assertTrue(
                 "Notification should be ongoing",
                 (notification.flags & Notification.FLAG_ONGOING_EVENT) != 0);
-
-        assertEquals("Should have 1 action", 1, notification.actions.length);
     }
 
     @Test
