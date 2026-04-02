a38c5f16fefa00355473544802abfd6ba89a55d9
devadharuns <devadharuns@google.com>
2026-04-02 09:03:57 -0700

    Added usb device_id data to print metrics
    Bug=b:498484348
    Test=./out/Default/unit_tests --gtest_filter=
    PrinterEventTrackerTest.*
    
    Change-Id: Ief5d743d160eb99d1b1b823aa8ed2a5c6b7023e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7720263
    Commit-Queue: Piotr Pawliczek <pawliczek@chromium.org>
    Reviewed-by: Benjamin Gordon <bmgordon@chromium.org>
    Reviewed-by: Piotr Pawliczek <pawliczek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1609264}

--- chrome/browser/ash/printing/printer_event_tracker_unittest.cc
+++ chrome/browser/ash/printing/printer_event_tracker_unittest.cc
@@ -22,6 +22,7 @@ constexpr int kVendorId = 0x3241;
 constexpr int kProductId = 0x1337;
 constexpr char kUsbManufacturer[] = "Usb MakesPrinters";
 constexpr char kUsbModel[] = "Printer ModelName";
+constexpr char kDeviceId[] = "USB DeviceId";
 
 constexpr char kMakeAndModel[] = "Chromium RazLazer X4321er";
 constexpr char kEffectiveMakeAndModel[] = "Generic PostScript";
@@ -288,6 +289,7 @@ TEST_F(PrinterEventTrackerTest, InstalledUsbPrinter) {
   ppd_search_data.usb_product_id = kProductId,
   ppd_search_data.usb_manufacturer = kUsbManufacturer,
   ppd_search_data.usb_model = kUsbModel,
+  ppd_search_data.printer_id.set_raw_id(kDeviceId),
 
   tracker_.RecordUsbPrinterInstalled(ppd_reference, ppd_search_data,
                                      PrinterEventTracker::SetupMode::kUser);
@@ -301,6 +303,7 @@ TEST_F(PrinterEventTrackerTest, InstalledUsbPrinter) {
   EXPECT_EQ(kProductId, record.usb_model_id());
   EXPECT_EQ(kUsbManufacturer, record.usb_printer_manufacturer());
   EXPECT_EQ(kUsbModel, record.usb_printer_model());
+  EXPECT_EQ(kDeviceId, record.device_id());
 
   EXPECT_EQ(kEffectiveMakeAndModel, record.ppd_identifier());
   EXPECT_FALSE(record.user_ppd());
@@ -343,6 +346,7 @@ TEST_F(PrinterEventTrackerTest, AbandonedUsbPrinter) {
   ppd_search_data.usb_product_id = kProductId;
   ppd_search_data.usb_manufacturer = kUsbManufacturer;
   ppd_search_data.usb_model = kUsbModel;
+  ppd_search_data.printer_id.set_raw_id(kDeviceId);
 
   tracker_.RecordUsbSetupAbandoned(ppd_search_data);
 
@@ -355,6 +359,7 @@ TEST_F(PrinterEventTrackerTest, AbandonedUsbPrinter) {
   EXPECT_EQ(kProductId, record.usb_model_id());
   EXPECT_EQ(kUsbManufacturer, record.usb_printer_manufacturer());
   EXPECT_EQ(kUsbModel, record.usb_printer_model());
+  EXPECT_EQ(kDeviceId, record.device_id());
 
   EXPECT_FALSE(record.has_user_ppd());
   EXPECT_FALSE(record.has_ppd_identifier());
