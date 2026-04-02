478ddf53aa16a079c72e55a2e1bc8b2e23bbb3c8
Jamie Walch <jamiewalch@chromium.org>
2026-04-02 16:03:40 -0700

    Include pixel type in layout messages triggered by `OnDesktopDisplayChanged`.
    This is triggered after capabilities are negotiated and results in a layout message being sent to the client with no pixel type set, which breaks the cursor scaling (and probably some other things) if resize-to-fit is disabled (if it's enabled, then the desktop is resized almost immediately, resulting in an updated layout message). This CL refactors the proto->`DesktopDisplayInfo` conversion and updates it to include the pixel type. It also resets the pixel type in `DesktopDisplayInfo::Reset`, which isn't required for the bug-fix, but seems like the right thing to do.
    
    BUG=481041921
    
    Change-Id: I173f18f7053129314db167f6eeddeb8a8dd23c32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7722623
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1609544}

--- remoting/host/desktop_display_info.cc
+++ remoting/host/desktop_display_info.cc
@@ -101,6 +101,7 @@ webrtc::DesktopSize DesktopDisplayInfo::CalcSizeDips(webrtc::DesktopSize size,
 
 void DesktopDisplayInfo::Reset() {
   displays_.clear();
+  pixel_type_.reset();
 }
 
 int DesktopDisplayInfo::NumDisplays() const {
@@ -262,6 +263,26 @@ std::unique_ptr<protocol::VideoLayout> DesktopDisplayInfo::GetVideoLayoutProto()
     }
   }
   return layout;
+}
+
+void DesktopDisplayInfo::CopyFromVideoLayoutProto(
+    const protocol::VideoLayout& layout) {
+  Reset();
+  if (layout.has_pixel_type()) {
+    switch (layout.pixel_type()) {
+      case protocol::VideoLayout::LOGICAL:
+        pixel_type_ = PixelType::LOGICAL;
+        break;
+      case protocol::VideoLayout::PHYSICAL:
+        pixel_type_ = PixelType::PHYSICAL;
+        break;
+      default:
+        break;
+    }
+  }
+  for (const auto& track : layout.video_track()) {
+    AddDisplayFrom(track);
+  }
 }
 
 std::ostream& operator<<(std::ostream& out, const DisplayGeometry& geo) {
