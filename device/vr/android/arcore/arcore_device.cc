b182cb98550c89b892e1354c6fa14bfa2c519134
Brandon Jones <bajones@chromium.org>
2026-04-01 17:12:01 -0700

    Gate front-facing WebXR on WebXRIncubations in browser proc
    Updates the ARCore WebXR backend to only allow support for the
    FRONT_FACING WebXR experimental feature if the WebXRIncubations
    flag has been enabled. Previously the backend always indicated it
    was allowed and the flag was handled in the renderer process.
    
    Bug: 497975477
    Change-Id: Ie4348988e330eb27c37bf8bb08c1756ea97c5a25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7722806
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1608879}

--- device/vr/android/arcore/arcore_device.cc
+++ device/vr/android/arcore/arcore_device.cc
@@ -44,8 +44,7 @@ const std::vector<mojom::XRSessionFeature>& GetSupportedFeatures() {
                           mojom::XRSessionFeature::ANCHORS,
                           mojom::XRSessionFeature::DEPTH,
                           mojom::XRSessionFeature::IMAGE_TRACKING,
-                          mojom::XRSessionFeature::HIT_TEST,
-                          mojom::XRSessionFeature::FRONT_FACING}};
+                          mojom::XRSessionFeature::HIT_TEST}};
 
   return *kSupportedFeatures;
 }
@@ -93,6 +92,11 @@ ArCoreDevice::ArCoreDevice(
   // and shared buffers will be used.
   if (base::FeatureList::IsEnabled(features::kWebXRWebGPUBinding)) {
     device_features.emplace_back(mojom::XRSessionFeature::WEBGPU);
+  }
+
+  // Only support Front Facing mode if the WebXR incubations flag is enabled.
+  if (base::FeatureList::IsEnabled(features::kWebXRIncubations)) {
+    device_features.emplace_back(mojom::XRSessionFeature::FRONT_FACING);
   }
 
   SetSupportedFeatures(device_features);
