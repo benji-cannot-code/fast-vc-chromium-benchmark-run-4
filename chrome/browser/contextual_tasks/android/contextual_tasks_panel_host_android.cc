f75b1008858b8add85038531a81920fdc36196e5
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2026-04-02 12:48:02 -0700

    Revert "[TabBottomSheet] Introduce new signals to send to native"
    This reverts commit 9f2c79b1fb44027e74caacb596614df95d5b3519.
    
    Reason for revert:
    LUCI Bisection has identified this change as the culprit of a build failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/compile-analysis/b/8685605640806577553
    
    Sample failed build: https://ci.chromium.org/b/8685605640806577553
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F7711533&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Fcompile-analysis%2Fb%2F8685605640806577553&type=BUG
    
    Original change's description:
    > [TabBottomSheet] Introduce new signals to send to native
    >
    > Bug: 497915826, 497789643
    > Change-Id: Ib01060a7efc79959fece4d537d6011d10c2bcdc0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7711533
    > Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    > Auto-Submit: Hitarth Kothari <hitarthkothari@google.com>
    > Commit-Queue: Hitarth Kothari <hitarthkothari@google.com>
    > Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1609426}
    >
    
    Bug: 497915826, 497789643
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I56827ca9a6d358a1e1a19198eaebfd55e87de22f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7728101
    Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1609433}

--- chrome/browser/contextual_tasks/android/contextual_tasks_panel_host_android.cc
+++ chrome/browser/contextual_tasks/android/contextual_tasks_panel_host_android.cc
@@ -93,15 +93,11 @@ void ContextualTasksPanelHostAndroid::SetWebContents(
   }
 }
 
-void ContextualTasksPanelHostAndroid::OnClosed() {
+void ContextualTasksPanelHostAndroid::OnClose() {
   is_open_ = false;
   observers_.Notify(&ContextualTasksPanelHost::Observer::OnSurfaceStateChanged,
                     SurfaceState::kClosed, StateChangeReason::kUserAction);
 }
-
-void ContextualTasksPanelHostAndroid::OnSuppressed() {}
-
-void ContextualTasksPanelHostAndroid::OnOpened(bool is_expanded) {}
 
 context_sharing::TabBottomSheetBridge*
 ContextualTasksPanelHostAndroid::GetOrCreateBridge() {
