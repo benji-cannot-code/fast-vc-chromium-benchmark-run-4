b80cff2a962750d17a49d72c90257217d724c48b
Christopher Grant <cjgrant@chromium.org>
2026-04-02 07:43:45 -0700

    NTP Promos: Remove the sign-in promo
    This promo is being dropped from the roster, per results from the
    initial study.
    
    Bug: 493254333
    
    Change-Id: Ia9a95ceec57abc62c0a3c3e55ccd12d9d7a3991f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7722460
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1609205}

--- chrome/browser/user_education/ntp_promo_identifiers.h
+++ chrome/browser/user_education/ntp_promo_identifiers.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // LINT.IfChange(NtpPromoIdentifiers)
 inline constexpr char kNtpCustomizationPromoId[] = "Customization";
 inline constexpr char kNtpExtensionsPromoId[] = "Extensions";
-inline constexpr char kNtpSignInPromoId[] = "SignIn";
 // LINT.ThenChange(//tools/metrics/histograms/metadata/user_education/histograms.xml:NtpPromoIdentifiers)
 
 #endif  // CHROME_BROWSER_USER_EDUCATION_NTP_PROMO_IDENTIFIERS_H_
