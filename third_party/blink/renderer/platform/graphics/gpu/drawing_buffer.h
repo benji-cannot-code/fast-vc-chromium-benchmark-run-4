fb2031c6b09951d26582274ee8c666a0b438cfb8
Sarath Singapati <sarath.singapati@huawei.com>
2026-04-01 20:17:20 -0700

    [WebGL] Ensure transferToImageBitmap() clears DrawingBuffer
    Previously, SetBufferClearNeeded() was skipped when
    preserveDrawingBuffer=true, causing ClearIfComposited() to
    skip the GL clear and readPixels() to return stale data.
    
    Fix by adding RequireExplicitBufferClear() to DrawingBuffer,
    which unconditionally sets buffer_clear_needed_, and calling
    it from TransferToImageBitmapBase() after the transfer.
    
    Bug: 406187744
    Change-Id: Ie9573b1808984a532e0328b4a623000f82d257dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7705337
    Commit-Queue: Sarath Singapati <sarath.singapati@huawei.com>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1608951}

--- third_party/blink/renderer/platform/graphics/gpu/drawing_buffer.h
+++ third_party/blink/renderer/platform/graphics/gpu/drawing_buffer.h
@@ -214,6 +214,7 @@ class PLATFORM_EXPORT DrawingBuffer : public cc::TextureLayerClient,
   bool MarkContentsChanged();
 
   void SetBufferClearNeeded(bool);
+  void RequireExplicitBufferClear();
   bool BufferClearNeeded() const;
 
   void SetIsInHiddenPage(bool);
