b0bbbfa1c623a3f786bf4d680490b82cad8a1b43
Ian Wells <iwells@chromium.org>
2026-04-02 16:53:54 -0700

    Create skeletal v2 ContextualCueingService
    The service has a registry of features that show cues. This CL creates
    that registry and adds methods for handling click and dismissal. Future
    CLs will deal with showing the cue and populating CueActionData from a
    model response.
    
    Bug: 496000131
    Change-Id: I8f351ef10e379668f8a621aba231400947525b44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7709155
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1609567}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -66,6 +66,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/content_index/content_index_provider_factory.h"
 #include "chrome/browser/content_settings/cookie_settings_factory.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
+#include "chrome/browser/contextual_cueing/contextual_cueing_service_factory.h"
 #include "chrome/browser/contextual_search/contextual_search_service_factory.h"
 #include "chrome/browser/contextual_tasks/contextual_tasks_context_service_factory.h"
 #include "chrome/browser/contextual_tasks/contextual_tasks_service_factory.h"
@@ -864,6 +865,8 @@ void ChromeBrowserMainExtraPartsProfiles::
 #if !BUILDFLAG(IS_ANDROID)
   ContentAnnotatorServiceFactory::GetInstance();
 #endif
+
+  contextual_cueing::ContextualCueingServiceFactory::GetInstance();
 
   contextual_tasks::ContextualTasksContextServiceFactory::GetInstance();
   contextual_tasks::ContextualTasksServiceFactory::GetInstance();
