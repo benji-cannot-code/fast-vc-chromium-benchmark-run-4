d78bcd2073f4ac2d6e09c3c1347d5be5419e0de6
Alexis Hétu <sugoi@chromium.org>
2026-04-02 09:18:48 -0700

    [iOS][Enhanced Autofill] Add EG test for passport form filling
    This new EG test creates a passport entity, opens a passport form,
    taps on a passport related field to trigger the keyboard accessory
    to open. The keyboard accessory then shows a passport suggestion
    and the test verifies that tapping that suggestion fills all the
    fields on the form.
    
    Bug: 480933607
    Change-Id: Id33bf83d640cbeb8b215e68254f7c51603a75377
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7703899
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Leo Zhao <leozhao@google.com>
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1609272}

--- ios/chrome/browser/autofill/ui_bundled/autofill_app_interface.h
+++ ios/chrome/browser/autofill/ui_bundled/autofill_app_interface.h
@@ -167,6 +167,9 @@ enum CreditCardSaveManagerObserverEvent : int {
 // Deletes all entities modified since the given time.
 + (void)removeEntityModifiedSince:(NSDate*)time;
 
+// Saves a Passport entity with test data.
++ (BOOL)savePassportEntity;
+
 @end
 
 #endif  // IOS_CHROME_BROWSER_AUTOFILL_UI_BUNDLED_AUTOFILL_APP_INTERFACE_H_
