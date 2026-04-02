1709004f5d763f851d935e473b90677cbe15a847
Ryan Akiyama <ryanakiyama@chromium.org>
2026-04-02 09:55:23 -0700

    [composebox] Create `cr-composebox-submit` component
    `composebox_submit.html.ts` was branched from
    `composebox/composebox_submit_button.html.ts` (not shown in Gerrit), and
    styles and logic were reused from `composebox/composebox.css` and
    `composebox/composebox.ts`, respectively. The component will get
    integrated, and redundant files, methods, and styles will get cleaned up
    in follow-up CLs.
    
    Bug: b:486705724
    Change-Id: I05e51b164b58069fe8bb751dce02772144b5ee20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7705947
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Reviewed-by: Sho Mizoe <shomizoe@google.com>
    Commit-Queue: Ryan Akiyama <ryanakiyama@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1609293}

---
+++ ui/webui/resources/cr_components/composebox/composebox_submit.html.ts
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import './icons.html.js';
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {ComposeboxSubmitElement} from './composebox_submit.js';
+
+export function getHtml(this: ComposeboxSubmitElement) {
+  // clang-format off
+  return html`
+<div id="submitContainer" class="icon-fade" part="submit"
+    tabindex="-1" @click="${this.onSubmitClick_}"
+    @focusin="${this.onSubmitFocusin_}">
+  <div id="submitOverlay" part="submit-overlay"
+      title="${this.submitButtonTitle || this.i18n('composeboxSubmitButtonTitle')}">
+  </div>
+  <cr-icon-button id="submitIcon"
+      class="action-icon ${this.submitButtonIconClass_()}"
+      part="action-icon submit-icon" tabindex="0"
+      title="${this.submitButtonTitle || this.i18n('composeboxSubmitButtonTitle')}"
+      ?disabled="${this.disabled}">
+  </cr-icon-button>
+</div>`;
+  // clang-format on
+}
