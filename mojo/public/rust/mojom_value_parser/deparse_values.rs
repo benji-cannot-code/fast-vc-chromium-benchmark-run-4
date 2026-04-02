b2951844de01bd98ca6ceb8adbdb231da9f86058
Devon Loehr <dloehr@google.com>
2026-04-02 07:39:52 -0700

    [rustmojo] Document mojom_value_parser and address FOR_RELEASEs
    This CL finishes addressing the last few FOR_RELEASE comments in
    the value parser, and also fills out the README to give an overview
    of the parser and its architecture, with the goal of helping to
    orient any future engineers who want to work on it.
    
    Bug: 444509367, 493223675, 498239042
    Change-Id: I32807ea01518eb261b13c4d27ad730399683b834
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7718398
    Reviewed-by: Julia Hansbrough <flowerhack@google.com>
    Commit-Queue: Devon Loehr <dloehr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1609200}

--- mojo/public/rust/mojom_value_parser/deparse_values.rs
+++ mojo/public/rust/mojom_value_parser/deparse_values.rs
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //! This module defines the code for deparsing (serializing) `MojomValues`. That
 //! is, it defines how to convert a `MojomValue` into a `[u8]`.
 //!
+//! See `//docs/mojo/wire_format_spec.md` for details on the wire encoding.
 //! Like the parser, the functions in this file mirror the AST structure. The
 //! "core" functionality is in `deparse_structured_body`, which is called by
 //! various more specialized functions (`deparse_struct`, `deparse_array`, etc).
@@ -328,30 +329,7 @@ fn deparse_map(
 ) -> Result<()> {
     let (keys, values) = values_map.into_iter().unzip();
     let field_values = vec![MojomValue::Array(keys), MojomValue::Array(values)];
-    let packed_fields = [
-        StructuredBodyElement::SingleValue(
-            0,
-            MojomWireType::Pointer {
-                nested_data_type: PackedStructuredType::Array {
-                    // This clone is cheap because it's in an Arc
-                    element_type: key_type.clone(),
-                    array_type: PackedArrayType::UnsizedArray,
-                },
-                is_nullable: false,
-            },
-        ),
-        StructuredBodyElement::SingleValue(
-            1,
-            MojomWireType::Pointer {
-                nested_data_type: PackedStructuredType::Array {
-                    // This clone is cheap because it's in an Arc
-                    element_type: value_type.clone(),
-                    array_type: PackedArrayType::UnsizedArray,
-                },
-                is_nullable: false,
-            },
-        ),
-    ];
+    let packed_fields = convert_map_ty_to_struct_fields(key_type, value_type);
     deparse_struct(data, field_values, &packed_fields)
 }
 
