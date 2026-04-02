b79787a2330c2a7676f232f0485b8fc00f8f3e99
Arthur Sonzogni <arthursonzogni@chromium.org>
2026-04-02 10:16:04 -0700

    Revert DCHECK->CHECK changes #1
    This reverts parts of commit e9ccc55c247e1d9c5001c0909c8aa984c33eb1c4.
    
    We found 4 crashes affecting this function. One of the DCHECK was
    previously identified as failing, but it turns out all of them are
    failing.
    
    Fixed: 498956242
    Bug: 497761255
    DesignDoc: http://go/check-content-navigation
    Change-Id: Ib9e8604383e99df6d403aa4df8973be01613b661
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7725988
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1609315}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -9304,7 +9304,9 @@ void RenderFrameHostImpl::SuddenTerminationDisablerChanged(
     blink::mojom::SuddenTerminationDisablerType disabler_type) {
   switch (disabler_type) {
     case blink::mojom::SuddenTerminationDisablerType::kBeforeUnloadHandler:
-      CHECK_NE(has_before_unload_handler_, present);
+      // TODO(https://crbug.com/497761255): CHECK-exclusion: Convert to CHECK
+      // once we are sure this isn't hit.
+      DCHECK_NE(has_before_unload_handler_, present);
       if (IsNestedWithinFencedFrame()) {
         bad_message::ReceivedBadMessage(
             GetProcess(),
@@ -9314,7 +9316,9 @@ void RenderFrameHostImpl::SuddenTerminationDisablerChanged(
       has_before_unload_handler_ = present;
       break;
     case blink::mojom::SuddenTerminationDisablerType::kPageHideHandler:
-      CHECK_NE(has_pagehide_handler_, present);
+      // TODO(https://crbug.com/497761255): CHECK-exclusion: Convert to CHECK
+      // once we are sure this isn't hit.
+      DCHECK_NE(has_pagehide_handler_, present);
       has_pagehide_handler_ = present;
       break;
     case blink::mojom::SuddenTerminationDisablerType::kUnloadHandler:
