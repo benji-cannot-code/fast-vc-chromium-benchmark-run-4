34af103b3e7d22fbeb2d31131b4285970eea8b0d
Yixi Wu <yixiwu@google.com>
2026-04-02 10:56:13 -0700

    Move Enterprise Connectors Utils Unit Tests from Chrome to Components
    Refactor and move some unit tests under the c/b/e/c/common_unittest.cc
    to the newly created unit test file
    c/e/c/c/c/deep_scanning_utils_unittest.cc
    
    Created helper test method in analysis_test_utils file to setup testing
    pref service for different analysis connectors and Created a test class
    "TestConnectorsService" to help with unit testing.
    
    Bug: 493229596
    Change-Id: I768d8c0accb28bc9b022aa93af2a29c27d56b56d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7711256
    Reviewed-by: Xu Li <mxlg@google.com>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Yixi Wu <yixiwu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1609348}

--- components/enterprise/connectors/core/common.cc
+++ components/enterprise/connectors/core/common.cc
@@ -12,14 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/escape.h"
 #include "base/strings/strcat.h"
 #include "base/strings/utf_string_conversions.h"
+#include "components/download/public/common/download_item.h"
 #include "components/enterprise/connectors/core/connectors_prefs.h"
 #include "components/signin/public/base/consent_level.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "ui/gfx/range/range.h"
-
-#if BUILDFLAG(USE_BLINK)
-#include "components/download/public/common/download_item.h"  // nogncheck
-#endif  // BUILDFLAG(USE_BLINK)
 
 namespace enterprise_connectors {
 
@@ -321,7 +318,6 @@ CreateSampleCustomRuleMessage(const std::u16string& msg,
   return custom_message;
 }
 
-#if BUILDFLAG(USE_BLINK)
 std::optional<ContentAnalysisResponse::Result::TriggeredRule::CustomRuleMessage>
 GetDownloadsCustomRuleMessage(const download::DownloadItem* download_item,
                               download::DownloadDangerType danger_type) {
@@ -358,7 +354,6 @@ GetDownloadsCustomRuleMessage(const download::DownloadItem* download_item,
   }
   return std::nullopt;
 }
-#endif  // BUILDFLAG(USE_BLINK)
 
 bool ContainsMalwareVerdict(const ContentAnalysisResponse& response) {
   return std::ranges::any_of(response.results(), [](const auto& result) {
