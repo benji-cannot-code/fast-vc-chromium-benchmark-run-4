a839ba9d34e52754c81c8d54caba1a232605424d
Vasilii Sukhanov <vasilii@chromium.org>
2026-04-02 06:58:34 -0700

    Refactor: Move password_manager_util_win to smaller build target
    Move password_manager_util_win.cc/h out of //chrome/browser and into
    //chrome/browser/password_manager:password_manager. Update
    AuthenticatorWin to pass PrefService* to util functions, removing the
    dependency on g_browser_process from the util file itself.
    
    Bug: 353332589
    Change-Id: Ied8fdfe2c843e5e6e181abbeda0dd10d6d899243
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7722180
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1609175}

--- chrome/browser/device_reauth/win/authenticator_win.cc
+++ chrome/browser/device_reauth/win/authenticator_win.cc
@@ -35,11 +35,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/scoped_winrt_initializer.h"
 #include "base/win/windows_types.h"
 #include "base/win/windows_version.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/password_manager/password_manager_util_win.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
+#include "components/prefs/pref_service.h"
 #include "ui/aura/window.h"
 #include "ui/views/win/hwnd_util.h"
 
@@ -191,7 +193,7 @@ void AuthenticateWithLegacyApi(const std::u16string& message,
   base::SequencedTaskRunner::GetCurrentDefault()->PostTaskAndReplyWithResult(
       FROM_HERE,
       base::BindOnce(&password_manager_util_win::AuthenticateUser, window,
-                     message),
+                     message, g_browser_process->local_state()),
       std::move(result_callback));
 }
 
@@ -394,5 +396,6 @@ void AuthenticatorWin::CheckIfBiometricsAvailable(
 }
 
 bool AuthenticatorWin::CanAuthenticateWithScreenLock() {
-  return password_manager_util_win::CanAuthenticateWithScreenLock();
+  return password_manager_util_win::CanAuthenticateWithScreenLock(
+      g_browser_process->local_state());
 }
