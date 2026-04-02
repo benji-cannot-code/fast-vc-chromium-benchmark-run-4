b21182d3ebab320171af17f58f1d23413986fa53
Dominic Battre <battre@chromium.org>
2026-04-02 03:03:04 -0700

    [Extensions] Ensure iterator stability for EventListenerMap
    By moving from flat_map to std::map, we can guarantee that iterators
    remain stable in the presence of modifying the map.
    
    Bug: b:497977591
    Change-Id: I81bd322f69c8628d3d0b3cedcbaaf7e26a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7718823
    Commit-Queue: Dominic Battre <battre@chromium.org>
    Reviewed-by: Andrea Orru <andreaorru@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1609075}

--- extensions/browser/event_listener_map.cc
+++ extensions/browser/event_listener_map.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/memory/ptr_util.h"
+#include "base/no_destructor.h"
 #include "content/public/browser/render_process_host.h"
 #include "extensions/browser/event_router.h"
 #include "extensions/browser/service_worker/worker_id.h"
@@ -156,6 +157,16 @@ EventListener::EventListener(const std::string& event_name,
 EventListenerMap::EventListenerMap(Delegate* delegate) : delegate_(delegate) {}
 
 EventListenerMap::~EventListenerMap() = default;
+
+const EventListenerMap::ListenerList& EventListenerMap::GetEventListenersByName(
+    const std::string& event_name) const {
+  auto it = listeners_.find(event_name);
+  if (it != listeners_.end()) {
+    return it->second;
+  }
+  static const base::NoDestructor<ListenerList> empty_list;
+  return *empty_list;
+}
 
 bool EventListenerMap::AddListener(std::unique_ptr<EventListener> listener) {
   if (HasListener(listener.get())) {
@@ -377,7 +388,7 @@ std::set<const EventListener*> EventListenerMap::GetEventListeners(
       }
     }
   } else {
-    for (const auto& listener : listeners_[event.event_name]) {
+    for (const auto& listener : GetEventListenersByName(event.event_name)) {
       interested_listeners.insert(listener.get());
     }
   }
