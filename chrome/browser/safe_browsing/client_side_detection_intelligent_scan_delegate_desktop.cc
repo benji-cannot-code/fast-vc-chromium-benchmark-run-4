96f75c1be64bea91760cae37656f4f45b17b1d30
Xinghui Lu <xinghuilu@chromium.org>
2026-04-02 16:33:19 -0700

    Add two new IntelligentScanVerdict.
    Reserve two additional values for future use. We don't have concrete use
    cases for the new values yet, but we will keep go/clams-verdict-mapping
    updated once we settle down the mapping between the new verdict type and
    campaign type.
    
    Internal CL: http://cl/869427070
    
    Bug: 482051149
    Change-Id: I3c364731c6404fca5a9cbcd4d421e52137943b8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7574053
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Seung Jae Lim <andysjlim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1609560}

--- chrome/browser/safe_browsing/client_side_detection_intelligent_scan_delegate_desktop.cc
+++ chrome/browser/safe_browsing/client_side_detection_intelligent_scan_delegate_desktop.cc
@@ -243,6 +243,8 @@ bool ClientSideDetectionIntelligentScanDelegateDesktop::ShouldShowScamWarning(
          (base::FeatureList::IsEnabled(
               kClientSideDetectionShowLlamaScamVerdictWarning) &&
           *verdict == IntelligentScanVerdict::SCAM_EXPERIMENT_VERDICT_2) ||
+         *verdict == IntelligentScanVerdict::SCAM_EXPERIMENT_VERDICT_3 ||
+         *verdict == IntelligentScanVerdict::SCAM_EXPERIMENT_VERDICT_4 ||
          *verdict ==
              IntelligentScanVerdict::SCAM_EXPERIMENT_CATCH_ALL_ENFORCEMENT;
 }
