aaea72614f392b66a098b5ad7153a9e76d05a8d7
Mitsuru Oshima <oshima@chromium.org>
2026-04-01 20:04:17 -0700

    Remove unnecessary vector_icons.h includes in //components
    (identified by gemini-cli)
    
    Bug: b:492325954
    
    Change-Id: Ic170f571d23fc3afa05b5f790b734c4c3e852e5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7713123
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Mickey Burks <mickeyburks@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1608930}

--- components/user_education/views/help_bubble_view.cc
+++ components/user_education/views/help_bubble_view.cc
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/user_education/views/help_bubble_factory_views.h"
 #include "components/user_education/views/help_bubble_views.h"
 #include "components/variations/variations_associated_data.h"
-#include "components/vector_icons/vector_icons.h"
 #include "ui/base/interaction/element_identifier.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_header_macros.h"
