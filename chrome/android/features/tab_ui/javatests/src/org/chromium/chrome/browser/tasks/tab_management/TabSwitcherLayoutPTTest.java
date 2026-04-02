efa174e885cfa1d4c69da57d3833c41a8fe0c024
Xinyi Ji <xinyiji@google.com>
2026-04-02 08:54:18 -0700

    [Code Health] Restrict failing test TabSwitcherLayoutPTTest#testTabToGridAndBack_SoftCleanup
    Changes `@Restriction(DESKTOP)` to `@DisableIf.Device(DESKTOP)` for
    `testTabToGridAndBack_SoftCleanup`. The test is known to be flaky on
    desktop, but the previous
    CL(https://chromium-review.git.corp.google.com/c/chromium/src/+/7590402)
    incorrectly restricted the test to run exclusively on desktop rather
    than disabling it there.
    
    Bug: 485611939
    Change-Id: I961039eb1ced3b6f34cada562c9cce246a9aff57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7722306
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Xinyi Ji <xinyiji@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1609255}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherLayoutPTTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherLayoutPTTest.java
@@ -38,6 +38,7 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features.DisableFeatures;
@@ -397,7 +398,7 @@ public class TabSwitcherLayoutPTTest {
     @Test
     @MediumTest
     @EnableAnimations
-    @Restriction(DeviceFormFactor.DESKTOP) // Flaky on desktop crbug.com/485611939
+    @DisableIf.Device(DeviceFormFactor.DESKTOP) // Flaky on desktop crbug.com/485611939
     public void testTabToGridAndBack_SoftCleanup() {
         WebPageStation firstPage = mCtaTestRule.startOnBlankPage();
         ChromeTabbedActivity cta = mCtaTestRule.getActivity();
