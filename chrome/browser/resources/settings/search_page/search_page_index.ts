279230a8a5fb1328f955d7592fd0411e05001a7d
Amelie Schneider <amelies@google.com>
2026-04-02 05:44:33 -0700

    [Search] Add site shortcuts section to search settings page
    This section holds the regional search engines and custom site
    shortcuts. The custom shortcuts can be edited. There is also the option
    to add a new custom shortcut. If there are no shortcuts in the section,
    a message is shown.
    
    Screenshots are linked in the bug. This change is behind the flag
    `SearchSettingsUpdate`.
    
    Bug: 490315684
    Change-Id: I2a5c1d08f97cc3115687505903e3bb08ed716b10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7706304
    Reviewed-by: Tomek Jurkiewicz <tju@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Amelie Schneider <amelies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1609138}

--- chrome/browser/resources/settings/search_page/search_page_index.ts
+++ chrome/browser/resources/settings/search_page/search_page_index.ts
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/cr_elements/cr_view_manager/cr_view_manager.js';
 import '/shared/settings/prefs/prefs.js';
+import './site_shortcuts_page.js';
 import './feature_shortcuts_page.js';
 import './keyboard_shortcut_page.js';
 import './search_page.js';
@@ -67,7 +68,8 @@ export class SettingsSearchPageIndexElement extends
     const defaultViews: string[] = ['parent'];
 
     if (this.searchSettingsUpdateEnabled_) {
-      defaultViews.push('featureShortcuts', 'keyboardShortcut');
+      defaultViews.push(
+          'siteShortcuts', 'featureShortcuts', 'keyboardShortcut');
     }
 
     this.$.viewManager.switchViews(
