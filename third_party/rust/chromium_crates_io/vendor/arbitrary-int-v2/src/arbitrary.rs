c8de993fe268ba2e140190383a686192bc9dddb0
alphonsea@google.com <alphonsea@google.com>
2026-04-02 11:04:06 -0700

    Add arbitrary-int dependency for libafl crate usage
    Due to the size of crrev.com/c/7564051, this CL works to split all of
    the unsafe dependencies into separate CLs for individual review. The end
    goal is to have all unsafe transitive dependencies for libafl imported
    and reviewed prior to importing the remaining dependencies (including
    libafl). We have approval for importing libafl by Chrome TSCs/ATLs (see
    https://groups.google.com/a/google.com/g/chrome-tsc-discuss/c/jvyNkbHOLhM).
    
    The arbitrary-int crate has also been marked as `group = test` and
    `allow_first_party_usage = false`.
    
    Bug: 495772162
    Change-Id: I1aeef84cfc626445928b9625140ee7c41bdf4086
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7697216
    Commit-Queue: Antonio Alphonse <alphonsea@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1609361}

---
+++ third_party/rust/chromium_crates_io/vendor/arbitrary-int-v2/src/arbitrary.rs
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+use crate::prelude::*;
+use arbitrary::{unstructured, Arbitrary, Result, Unstructured};
+
+impl<'a, T: UnsignedInteger + BuiltinInteger, const BITS: usize> Arbitrary<'a> for UInt<T, BITS>
+where
+    T: unstructured::Int,
+    Self: UnsignedInteger<UnderlyingType = T>,
+{
+    fn arbitrary(u: &mut Unstructured<'a>) -> Result<Self> {
+        u.int_in_range(Self::MIN.value()..=Self::MAX.value())
+            .map(Self::new)
+    }
+}
+
+impl<'a, T: SignedInteger + BuiltinInteger, const BITS: usize> Arbitrary<'a> for Int<T, BITS>
+where
+    T: unstructured::Int,
+    Self: SignedInteger<UnderlyingType = T>,
+{
+    fn arbitrary(u: &mut Unstructured<'a>) -> Result<Self> {
+        u.int_in_range(Self::MIN.value()..=Self::MAX.value())
+            .map(Self::new)
+    }
+}
---
+++ third_party/rust/chromium_crates_io/vendor/arbitrary-int-v2/tests/v1_compat.rs
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#![allow(deprecated)]
+
+// These are some typical patterns in users of arbitrary_int v1 we want to make sure don't break
+
+#[test]
+fn use_wildcard() {
+    use arbitrary_int::*;
+
+    // Use a constant from `Number` (which conflicts with `Integer`)
+    const _: u5 = u5::MAX;
+}
+
+#[test]
+fn use_number_directly() {
+    use arbitrary_int::{u5, Number};
+
+    // Use a constant from `Number` (which conflicts with `Integer`)
+    const _: u5 = u5::MAX;
+}
