01e1374c184c69eb04b0519cec9f01ad32065468
Adam Ren <adamren@google.com>
2026-04-02 21:14:43 -0700

    Implement Android OpenXR mesh manager with incremental streaming
    - Add OpenXrMeshManagerAndroid implementation
    - Support incremental mesh data streaming from OpenXR runtime on Android
    
    Bug: 473611121
    Change-Id: If8d5399d279a9404c2f13722e88c3eca00f37c71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7579917
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Adam Ren (xWF) <adamren@google.com>
    Cr-Commit-Position: refs/heads/main@{#1609652}

--- device/vr/openxr/openxr_util.h
+++ device/vr/openxr/openxr_util.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/vr/public/mojom/vr_service.mojom-forward.h"
 #include "device/vr/public/mojom/xr_session.mojom-forward.h"
 #include "third_party/openxr/src/include/openxr/openxr.h"
+#include "third_party/openxr/dev/xr_android.h"
 #include "ui/gfx/geometry/transform.h"
 #include "ui/gfx/geometry/transform_util.h"
 
@@ -70,6 +71,11 @@ bool IsPoseValid(XrSpaceLocationFlags locationFlags);
 
 bool IsFeatureSupportedForMode(device::mojom::XRSessionFeature feature,
                                device::mojom::XRSessionMode mode);
+
+mojom::XRSemanticLabel ToMojomSemanticLabel(
+    XrSpatialPlaneSemanticLabelEXT label);
+mojom::XRSemanticLabel ToMojomSemanticLabel(
+    XrSceneMeshSemanticLabelANDROID label);
 
 // Define a concept for a struct to help validate that it can be safely cast to
 // an XrBaseOutStructure.
