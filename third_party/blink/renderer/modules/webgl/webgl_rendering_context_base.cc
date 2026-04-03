f7939c1949f101c0f4b53ba94a5323bef3208d0c
Colin Blundell <blundell@chromium.org>
2026-04-02 23:34:08 -0700

    [//media] Have CopyVideoFrameTexturesToGLTexture take in RGB SI cache
    Updates PCVR::CopyVideoFrameTexturesToGLTextureViaIntermediateSI() to
    take the cache used to hold the intermediate RGB SI as an argument
    instead of creating it on demand internally. This is a step toward
    having clients hold instances of the cache rather than PCVR instances.
    
    Bug: 343011436
    Change-Id: Iff2009f2ddf20906ac1c5ce76bbb1da29518294e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7726122
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1609686}

--- third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
+++ third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
@@ -6457,7 +6457,8 @@ void WebGLRenderingContextBase::TexImageHelperMediaVideoFrame(
       }
 
       if (video_renderer->CopyVideoFrameTexturesToGLTextureViaIntermediateSI(
-              raster_context_provider, gl, media_video_frame, params.target,
+              raster_context_provider, gl, media_video_frame,
+              video_renderer->GetRGBSharedImageCache(), params.target,
               texture->Object(), adjusted_internalformat, params.format,
               params.type, params.level, dst_alpha_type,
               params.GetDestinationOrigin())) {
