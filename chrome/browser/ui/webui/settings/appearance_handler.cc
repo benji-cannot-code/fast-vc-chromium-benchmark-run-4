52f955c6be4e7a40d12f5b4acffe70d10e936df7
Alison Gale <agale@chromium.org>
2026-04-03 14:56:05 -0700

    [Vertical Tabs] Move vertical tabs metrics out of views directory
    Change-Id: I3cee58751d365ef41bc996c583b120f877491bad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7729570
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1609973}

--- chrome/browser/ui/webui/settings/appearance_handler.cc
+++ chrome/browser/ui/webui/settings/appearance_handler.cc
@@ -16,8 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
+#include "chrome/browser/ui/tabs/vertical_tab_strip_metrics.h"
 #include "chrome/browser/ui/toolbar/pinned_toolbar/pinned_toolbar_actions_model.h"
-#include "chrome/browser/ui/views/tabs/vertical/vertical_tab_strip_metrics.h"
 #include "chrome/common/pref_names.h"
 #include "content/public/browser/web_ui.h"
 
