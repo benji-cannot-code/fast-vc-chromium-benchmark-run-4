8499d5ee6c515bd963f7d2a542804f4dae3d1531
Stepan Khapugin <stkhapugin@chromium.org>
2026-04-03 07:57:25 -0700

    [iOS][omnibox] Fix DSE icon in incognito omnibox.
    Fix the icon and add an EGTest to prevent regressions in the future.
    
    Fixed: 495779690
    Change-Id: I5aeff4fd182beaeb54fa83f76a4341620aad33d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7725786
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1609784}

--- ios/chrome/browser/location_bar/ui_bundled/location_bar_steady_view_mediator.mm
+++ ios/chrome/browser/location_bar/ui_bundled/location_bar_steady_view_mediator.mm
@@ -170,7 +170,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // Records the leading icon type when the document changes to avoid too many
   // recording. Don't record on NTP as the leading icon is not visible.
   if (navigation && !navigation->IsSameDocument() &&
-      !IsURLNewTabPage(navigation->GetUrl())) {
+      !IsUrlNtp(navigation->GetUrl())) {
     [self.consumer recordLensOverlayAvailability];
   }
 }
@@ -236,9 +236,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)notifyConsumerOfChangedLocation {
   [self.consumer updateLocationText:[self currentLocationString]
                            clipTail:[self locationShouldClipTail]];
-  GURL URL =
-      self.currentWebState ? self.currentWebState->GetVisibleURL() : GURL();
-  BOOL isNTP = IsURLNewTabPage(URL);
+  BOOL isNTP = IsVisibleURLNewTabPage(self.currentWebState);
   if (isNTP) {
     [self.consumer updateAfterNavigatingToNTP];
   }
