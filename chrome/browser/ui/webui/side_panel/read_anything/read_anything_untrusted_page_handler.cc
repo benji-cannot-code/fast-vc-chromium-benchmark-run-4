9277d81400af24a2a384da7902870f796c819f4a
Lauren Winston <lwinston@google.com>
2026-04-03 10:18:07 -0700

    Use Screen2x when the Phrase Highlighting flag is enabled.
    Phrase highlighting currently doesn't work with the TS text
    segmentation model, so it therefore doesn't work with Readability.
    
    This will be fixed with crbug.com/444029483, but this helps
    allow phrase highlighting to be testable during the interim
    and make it easier to remove the TSTextSegmentation flag.
    
    Bug: 486979604
    Change-Id: Ida65b97002bc91703c2add11bbf7de2d7b898b6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7712974
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1609839}

--- chrome/browser/ui/webui/side_panel/read_anything/read_anything_untrusted_page_handler.cc
+++ chrome/browser/ui/webui/side_panel/read_anything/read_anything_untrusted_page_handler.cc
@@ -439,7 +439,8 @@ ReadAnythingUntrustedPageHandler::ReadAnythingUntrustedPageHandler(
                 weak_factory_.GetWeakPtr()));
   }
 
-  if (features::IsReadAnythingWithReadabilityEnabled()) {
+  if (features::IsReadAnythingWithReadabilityEnabled() &&
+      !features::IsReadAnythingReadAloudPhraseHighlightingEnabled()) {
     // Set the JavaScript world ID.
     if (!dom_distiller::DistillerJavaScriptWorldIdIsSet()) {
       dom_distiller::SetDistillerJavaScriptWorldId(
@@ -1377,8 +1378,13 @@ void ReadAnythingUntrustedPageHandler::OnActiveAXTreeIDChanged() {
   content::RenderFrameHost* rfh = contents->GetPrimaryMainFrame();
   VLOG(1) << "Sending non-pdf tree with id " << rfh->GetAXTreeID();
 
+  // TODO: crbug.com/444029483- Phrase highlighting currently doesn't work
+  // with the TS text segmentation method. Therefore, it doesn't work with
+  // Readability. Until phrase highlighting works with TSTextSegmentation,
+  // default to using Screen2x when the phrase highlighting flag is enabled.
   const bool use_readability =
-      features::IsReadAnythingWithReadabilityEnabled() && !is_pdf_with_frame_;
+      features::IsReadAnythingWithReadabilityEnabled() && !is_pdf_with_frame_ &&
+      !features::IsReadAnythingReadAloudPhraseHighlightingEnabled();
 
   if (use_readability) {
     // We must emit `kDistillationInProgress` before sending the new tree ID
@@ -1406,7 +1412,9 @@ void ReadAnythingUntrustedPageHandler::OnActiveAXTreeIDChanged() {
 }
 void ReadAnythingUntrustedPageHandler::RequestDomDistillerDistillation(
     content::WebContents* content) {
-  if (!features::IsReadAnythingWithReadabilityEnabled() || is_pdf_with_frame_) {
+  if (!features::IsReadAnythingWithReadabilityEnabled() ||
+      features::IsReadAnythingReadAloudPhraseHighlightingEnabled() ||
+      is_pdf_with_frame_) {
     return;
   }
 
@@ -1474,7 +1482,8 @@ void ReadAnythingUntrustedPageHandler::RecordDistillationSchemeHistogram(
 void ReadAnythingUntrustedPageHandler::ProcessDistilledArticle(
     const dom_distiller::DistilledArticleProto* article_proto) {
   CHECK(features::IsReadAnythingWithReadabilityEnabled() &&
-        !is_pdf_with_frame_);
+        !is_pdf_with_frame_ &&
+        !features::IsReadAnythingReadAloudPhraseHighlightingEnabled());
   if (article_proto && article_proto->pages_size() > 0) {
     dom_distiller_title_ = article_proto->title();
 
