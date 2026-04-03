fcd790b1a6af70a96150d645da4b31ca418c6f43
Hira Mahmood <hiramahmood@google.com>
2026-04-03 15:03:28 -0700

    [iOS] Exit fullscreen on non-same-document navigation
    This aligns with the legacy fullscreen behavior where fullscreen is
    exited for most page navigations.
    
    Same Document:
    https://screencast.googleplex.com/cast/NjQzMzA0NDU4NzA4NTgyNHw4YmY4N2QyNy1iMw
    Non-same Document:
    https://screencast.googleplex.com/cast/NjYyOTQwMTM2NTA1MzQ0MHw0OGU2YmY5Yy03Mw
    
    Fixed: 499332199
    Change-Id: Id451e0699dfa21addea62ee55650d94ce19bc440
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7727411
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1609986}

--- ios/chrome/browser/fullscreen/coordinator/fullscreen_mediator.mm
+++ ios/chrome/browser/fullscreen/coordinator/fullscreen_mediator.mm
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list_observer_bridge.h"
 #import "ios/chrome/browser/web/model/web_view_proxy/web_view_proxy_tab_helper.h"
 #import "ios/chrome/browser/web/model/web_view_proxy/web_view_proxy_tab_helper_observer_bridge.h"
+#import "ios/web/public/navigation/navigation_context.h"
 #import "ios/web/public/ui/crw_web_view_proxy.h"
 #import "ios/web/public/ui/crw_web_view_scroll_view_proxy.h"
 #import "ios/web/public/web_state.h"
@@ -188,6 +189,13 @@ const CGFloat kFullscreenSnapThreshold = 10.0;
   // TODO(crbug.com/496229929): Call InvalidateInsetRange() from the correct
   // event(s).
   _browserAgent->InvalidateInsetRange(PassKey());
+}
+
+- (void)webState:(web::WebState*)webState
+    didFinishNavigation:(web::NavigationContext*)navigationContext {
+  if (!navigationContext->IsSameDocument()) {
+    _browserAgent->ExitFullscreen(PassKey(), /*animated=*/true);
+  }
 }
 
 - (void)webStateDestroyed:(web::WebState*)webState {
