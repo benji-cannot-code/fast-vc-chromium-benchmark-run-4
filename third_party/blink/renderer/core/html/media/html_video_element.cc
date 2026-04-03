51b3198694888db7a514605c3bb84eb27770fe5f
Vikram Pasupathy <vpasupathy@chromium.org>
2026-04-02 17:26:00 -0700

    media: Selectively attach video visibility tracker based on feature flag
    We don't want to attach the video visibility tracker for clear video
    when the encrypted media occlusion tracking switch is set to true.
    
    By gating the CreateVisibilityTracker call in OnPlay, we restrict the
    call sites for the visibility tracker to:
    
    OnPlay -> visibility tracker creation is for the AutoPiPHeuristics
    feature.
    
    OnEncryptedMediaInitData -> visibility tracker is for encrypted
    occlusion.
    
    This way, even if clear playback occurs when the
    kEncryptedMediaOcclusionTracking flag is set, the visibility tracker is
    not created.
    
    Bug: b:498652735
    Change-Id: Icb05c7d6174f60a821a7aa8460f244685a3ab67c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7722048
    Commit-Queue: Vikram Pasupathy <vpasupathy@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Sangbaek Park <sangbaekpark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1609583}

--- third_party/blink/renderer/core/html/media/html_video_element.cc
+++ third_party/blink/renderer/core/html/media/html_video_element.cc
@@ -433,6 +433,7 @@ void HTMLVideoElement::CreateVisibilityTrackerIfNeeded() {
   }
 
   if (visibility_tracker_) {
+    UpdateVideoVisibilityTracker();
     return;
   }
 
@@ -446,6 +447,7 @@ void HTMLVideoElement::CreateVisibilityTrackerIfNeeded() {
 
   visibility_tracker_ = MakeGarbageCollected<MediaVideoVisibilityTracker>(
       *this, kVisibilityThreshold, std::move(report_visibility_cb));
+  UpdateVideoVisibilityTracker();
 }
 
 void HTMLVideoElement::ReportVisibility(bool meets_visibility_threshold) {
@@ -461,6 +463,8 @@ void HTMLVideoElement::OnEncryptedMediaInitData() {
     return;
   }
 
+  // Need to create and attach the tracker for Encrypted Media Occlusion
+  // tracking.
   CreateVisibilityTrackerIfNeeded();
   if (visibility_tracker_) {
     visibility_tracker_->RequestVisibilityRatio(BindOnce(
@@ -490,8 +494,17 @@ void HTMLVideoElement::OnPlay() {
     UpdatePictureInPictureAvailability();
   }
 
-  CreateVisibilityTrackerIfNeeded();
-  UpdateVideoVisibilityTracker();
+  // The Video Visibility Tracker is shared by both Auto-PiP and Encrypted
+  // Media Occlusion Tracking. In OnPlay(), we only want to initialize it
+  // for AutoPictureInPictureVideoHeuristicsEnabled feature.
+  //
+  // For encrypted media, the tracker should only be created via
+  // OnEncryptedMediaInitData(). Initializing it here when only the
+  // kEncryptedMediaOcclusionTracking feature is enabled would incorrectly
+  // attach the tracker to clear videos.
+  if (RuntimeEnabledFeatures::AutoPictureInPictureVideoHeuristicsEnabled()) {
+    CreateVisibilityTrackerIfNeeded();
+  }
 
   if (!RuntimeEnabledFeatures::VideoAutoFullscreenEnabled() ||
       FastHasAttribute(html_names::kPlaysinlineAttr)) {
