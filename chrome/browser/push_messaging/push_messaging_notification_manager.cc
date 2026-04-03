220334b926da074cc594c5b7bf06840853cc029b
James Cook <jamescook@chromium.org>
2026-04-02 17:45:44 -0700

    push: Update ENABLE_EXTENSIONS flag in PushMessagingNotificationManager
    Now that we have extensions on desktop Android, there's a new build flag
    ENABLE_EXTENSIONS_CORE required for "traditional extensions platforms +
    desktop Android". Switch to that, so the push messaging code does the
    same user visibility checks for extensions on desktop Android that it
    does on Win, Mac, Linux and Chrome OS.
    
    See bug for details on the history of the two build flags.
    
    Bug: 494643383
    Change-Id: I5273edda199bb18b81ace9355e0f4c037763c1f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7718869
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1609590}

--- chrome/browser/push_messaging/push_messaging_notification_manager.cc
+++ chrome/browser/push_messaging/push_messaging_notification_manager.cc
@@ -44,12 +44,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "url/gurl.h"
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
+#if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 #include "extensions/browser/extension_registry.h"
 #include "extensions/common/constants.h"
 #include "extensions/common/extension_set.h"
 #include "extensions/common/manifest_handlers/background_info.h"
-#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 
 #if BUILDFLAG(IS_ANDROID)
 #include "chrome/browser/ui/android/tab_model/tab_model.h"
@@ -286,12 +286,12 @@ void PushMessagingNotificationManager::DidWriteNotificationData(
 bool PushMessagingNotificationManager::ShouldBypassUserVisibleOnlyRequirement(
     const GURL& origin,
     bool requested_user_visible_only) {
-#if BUILDFLAG(ENABLE_EXTENSIONS)
+#if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
   if (origin.SchemeIs(extensions::kExtensionScheme)) {
     return ShouldExtensionsBypassUserVisibleOnlyRequirement(
         origin, requested_user_visible_only);
   }
-#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 
   // Returning true is an exception, so default to deny for anything we don't
   // explicitly identify.
@@ -303,7 +303,7 @@ void PushMessagingNotificationManager::LogSilentPushEvent(
   UMA_HISTOGRAM_ENUMERATION("PushMessaging.SilentNotification", event);
 }
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
+#if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 bool PushMessagingNotificationManager::
     ShouldExtensionsBypassUserVisibleOnlyRequirement(
         const GURL& origin,
@@ -322,4 +322,4 @@ bool PushMessagingNotificationManager::
   }
   return extensions::BackgroundInfo::IsServiceWorkerBased(extension);
 }
-#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS_CORE)
