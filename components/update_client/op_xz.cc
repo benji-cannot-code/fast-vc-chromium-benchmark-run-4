492a5512262521b393ce09149c6a0d813ad55fa9
S. Ganesh <ganesh@chromium.org>
2026-04-03 14:36:13 -0700

    update_client: use `BEST_EFFORT` tasks for background decompression
    Bug: 492496530
    Change-Id: Icdb4c2984035faf46a0875decf1b528226ff1b80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7723650
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1609961}

--- components/update_client/op_xz.cc
+++ components/update_client/op_xz.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/bind_post_task.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/thread_pool.h"
-#include "base/threading/scoped_thread_priority.h"
 #include "base/types/expected.h"
 #include "base/values.h"
 #include "components/update_client/op_zucchini.h"
@@ -76,16 +75,14 @@ base::OnceClosure XzOperation(
     std::unique_ptr<Unzipper> unzipper,
     base::RepeatingCallback<void(base::DictValue)> event_adder,
     base::RepeatingCallback<void(ComponentState)> state_tracker,
-    bool is_foreground,
+    bool /*is_foreground*/,
     const base::FilePath& in_file,
     base::OnceCallback<void(base::expected<base::FilePath, CategorizedError>)>
         callback) {
-  // Apply background priority if this is a background task.
-  std::optional<base::ScopedBoostPriority> priority;
-  if (!is_foreground) {
-    priority.emplace(base::ThreadType::kBackground);
-  }
-
+  // `is_foreground` is unused right now since XZ is primarily used in
+  // foreground scenarios. If the unzipper component adds support for background
+  // scenarios in the future, `is_foreground` can be passed through to the
+  // unzipper component.
   state_tracker.Run(ComponentState::kDecompressing);
   base::FilePath dest_file = in_file.DirName().AppendUTF8("decoded_xz");
   Unzipper* unzipper_raw = unzipper.get();
