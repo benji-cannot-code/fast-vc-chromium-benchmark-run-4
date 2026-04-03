4e42a45f6083b9f1beb13d10848d5a2dffc01b59
Jiacheng Guo <gjc@google.com>
2026-04-02 23:08:33 -0700

    Use ScopedPrewarmFeatureList in SearchPreloadBrowserTest
    Bug: 423465927
    Change-Id: I4c6aacaea8799fb753f52a6dc381581cc3b2d30d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7724908
    Commit-Queue: Jiacheng Guo <gjc@google.com>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1609680}

--- chrome/browser/preloading/search_preload/search_preload_browsertest.cc
+++ chrome/browser/preloading/search_preload/search_preload_browsertest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/preloading/prerender/prerender_manager.h"
 #include "chrome/browser/preloading/prerender/search_prewarm_progress_service.h"
 #include "chrome/browser/preloading/prerender/search_prewarm_progress_service_factory.h"
+#include "chrome/browser/preloading/scoped_prewarm_feature_list.h"
 #include "chrome/browser/preloading/search_preload/search_preload_features.h"
 #include "chrome/browser/preloading/search_preload/search_preload_service.h"
 #include "chrome/browser/profiles/profile.h"
@@ -1805,13 +1806,6 @@ class SearchPreloadBrowserTest_Throttle : public SearchPreloadBrowserTestBase {
       base::test::ScopedFeatureList& scoped_feature_list) override {
     scoped_feature_list.InitWithFeaturesAndParameters(
         {
-            // TODO(crbug.com/423465927): Allow ScopedPrewarmFeatureList
-            // to be used in the SearchPreloadBrowserTest Currently we are
-            // running into dependency issue when using the test helper.
-            {
-                features::kPrewarm,
-                {{"throttle_prefetch", "true"}},
-            },
             {
                 features::kDsePreload2,
                 {
@@ -1829,12 +1823,18 @@ class SearchPreloadBrowserTest_Throttle : public SearchPreloadBrowserTestBase {
             },
         },
         /*disabled_features=*/{});
+    scoped_prewarm_feature_list_ = std::make_unique<
+        test::ScopedPrewarmFeatureList>(
+        test::ScopedPrewarmFeatureList::PrewarmState::kEnabledWithNoTrigger);
   }
 
   void SetUpOnMainThread() override {
     PrerenderManager::CreateForWebContents(&GetWebContents());
     SearchPreloadBrowserTestBase::SetUpOnMainThread();
   }
+
+ private:
+  std::unique_ptr<test::ScopedPrewarmFeatureList> scoped_prewarm_feature_list_;
 };
 
 // Tests that search prefetch/prerender are throttled during an ongoing search
