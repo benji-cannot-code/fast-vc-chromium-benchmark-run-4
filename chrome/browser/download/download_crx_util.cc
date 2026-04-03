48979dc059bc2c1e034c71dd5ee6202042127be2
James Cook <jamescook@chromium.org>
2026-04-03 14:58:40 -0700

    downloads: Update ENABLE_EXTENSIONS in download_crx_util.cc
    As part of bringing extensions to desktop Android we're switching to
    a new buildflag, ENABLE_EXTENSIONS_CORE, leaving ENABLE_EXTENSIONS for
    things we still need to port.
    
    In this file, ENABLE_EXTENSIONS is being used to mean !IS_ANDROID, so
    switch to that for consistency with the rest of the code.
    
    Bug: 494643383
    Change-Id: I491e099a1271459248c766cd4b6f9020d087ca3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7728609
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1609978}

--- chrome/browser/download/download_crx_util.cc
+++ chrome/browser/download/download_crx_util.cc
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/extension_urls.h"
 #include "extensions/common/user_script.h"
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
+#if !BUILDFLAG(IS_ANDROID)
 #include "chrome/browser/ui/browser.h"
 #endif
 
