d2d2aeb10fc9d5b0c3badd381519e61f67dd719a
Joemer Ramos <joemerramos@google.com>
2026-04-03 10:36:30 -0700

    [iOS] Fix Privacy And Security Switch Buttons With a11y Full Keyboard
    Toggle switch UI wouldn't update when using a11y full keyboard. This
    CL ensures that the switch button is updated even when toggled with a
    keyboard instead of a tap.
    
    UI Video:https://drive.google.com/file/d/17-v-uaLGoZJQd9sQo327zMvOODNwQTjI/view?usp=drive_link&resourcekey=0-zRqby9eKovsgPpAxkvKXBg
    
    Bug link seems broken:crbug.com/496740161
    
    Fix: 496740161
    Change-Id: Ie2d2b44cb9078754a4e3af6d118299d5dc3cea4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7726970
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Auto-Submit: Joemer Ramos <joemerramos@google.com>
    Reviewed-by: Daniel White <danieltwhite@google.com>
    Cr-Commit-Position: refs/heads/main@{#1609848}

--- ios/chrome/browser/settings/ui_bundled/privacy/handoff_table_view_controller.mm
+++ ios/chrome/browser/settings/ui_bundled/privacy/handoff_table_view_controller.mm
@@ -26,6 +26,11 @@ typedef NS_ENUM(NSInteger, SectionIdentifier) {
   SectionIdentifierSwitch = kSectionIdentifierEnumZero,
 };
 
+NSString* const kHandoffTableViewAccessibilityIdentifier =
+    @"HandoffTableViewAccessibilityIdentifier";
+NSString* const kHandoffSwitchAccessibilityIdentifier =
+    @"HandoffSwitchAccessibilityIdentifier";
+
 typedef NS_ENUM(NSInteger, ItemType) {
   ItemTypeSwitch = kItemTypeEnumZero,
   ItemTypeFooter,
@@ -70,6 +75,8 @@ typedef NS_ENUM(NSInteger, ItemType) {
 - (void)viewDidLoad {
   [super viewDidLoad];
   self.tableView.estimatedSectionFooterHeight = 70;
+  self.tableView.accessibilityIdentifier =
+      kHandoffTableViewAccessibilityIdentifier;
 
   [self loadModel];
 }
@@ -83,6 +90,8 @@ typedef NS_ENUM(NSInteger, ItemType) {
   [model addSectionWithIdentifier:SectionIdentifierSwitch];
   _handoffSwitchItem =
       [[TableViewSwitchItem alloc] initWithType:ItemTypeSwitch];
+  _handoffSwitchItem.accessibilityIdentifier =
+      kHandoffSwitchAccessibilityIdentifier;
   _handoffSwitchItem.text =
       l10n_util::GetNSString(IDS_IOS_OPTIONS_ENABLE_HANDOFF_TO_OTHER_DEVICES);
   _handoffSwitchItem.on = _handoffEnabled.value;
@@ -139,6 +148,8 @@ typedef NS_ENUM(NSInteger, ItemType) {
 
 - (void)switchChanged:(UISwitch*)switchView {
   _handoffEnabled.value = switchView.isOn;
+  _handoffSwitchItem.on = switchView.isOn;
+  [self reconfigureCellsForItems:@[ _handoffSwitchItem ]];
 }
 
 @end
--- ios/chrome/browser/settings/ui_bundled/privacy/privacy_table_view_controller.mm
+++ ios/chrome/browser/settings/ui_bundled/privacy/privacy_table_view_controller.mm
@@ -847,6 +847,8 @@ const char kSyncSettingsURL[] = "settings://open_sync";
 - (void)HTTPSOnlyModeSwitchToggled:(UISwitch*)switchView {
   BOOL isOn = switchView.isOn;
   [_HTTPSOnlyModePref setValue:isOn];
+  self.HTTPSOnlyModeItem.on = isOn;
+  [self reconfigureCellsForItems:@[ self.HTTPSOnlyModeItem ]];
   [self enhancedSafeBrowsingInlinePromoTriggerCriteriaMet];
 }
 
@@ -857,6 +859,8 @@ const char kSyncSettingsURL[] = "settings://open_sync";
 - (void)PasswordLeakCheckSwitchToggled:(UISwitch*)switchView {
   BOOL isOn = switchView.isOn;
   [_passwordLeakCheckPref setValue:isOn];
+  self.passwordLeakCheckItem.on = isOn;
+  [self reconfigureCellsForItems:@[ self.passwordLeakCheckItem ]];
 }
 
 // Called from the Incognito interstitial setting's UIControlEventValueChanged.
@@ -865,6 +869,8 @@ const char kSyncSettingsURL[] = "settings://open_sync";
 // switchView.on is YES.
 - (void)incognitoInterstitialSwitchToggled:(UISwitch*)switchView {
   self.incognitoInterstitialPref.value = switchView.on;
+  self.incognitoInterstitialItem.on = switchView.on;
+  [self reconfigureCellsForItems:@[ self.incognitoInterstitialItem ]];
   UMA_HISTOGRAM_ENUMERATION(
       kIncognitoInterstitialSettingsActionsHistogram,
       switchView.on ? IncognitoInterstitialSettingsActions::kEnabled
@@ -900,6 +906,10 @@ const char kSyncSettingsURL[] = "settings://open_sync";
                                  }
                                  [switchView setOn:enabled animated:YES];
                                  weakSelf.incognitoReauthPref.value = enabled;
+                                 weakSelf.incognitoReauthItem.on = enabled;
+                                 [weakSelf reconfigureCellsForItems:@[
+                                   weakSelf.incognitoReauthItem
+                                 ]];
                                  [weakSelf
                                      enhancedSafeBrowsingInlinePromoTriggerCriteriaMet];
                                }];
