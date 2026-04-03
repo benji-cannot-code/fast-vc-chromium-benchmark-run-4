ccca866d1f5ccc12e2410c1bf993152747f9eb4e
Robert Liao <robliao@chromium.org>
2026-04-03 15:14:36 -0700

    Remove Unused is_secondary_widget API from Widget
    Fixed: 499329250
    Change-Id: I2b77617538038dcb11af24bf0f6f63501d422728
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7729683
    Commit-Queue: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Auto-Submit: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1609998}

--- ui/views/widget/widget_unittest.cc
+++ ui/views/widget/widget_unittest.cc
@@ -1614,10 +1614,6 @@ TEST_P(WidgetWithDestroyedNativeViewOrNativeWidgetTest, Init) {
   EXPECT_DCHECK_DEATH(widget()->Init(std::move(params)));
 }
 
-TEST_P(WidgetWithDestroyedNativeViewOrNativeWidgetTest, is_secondary_widget) {
-  widget()->is_secondary_widget();
-}
-
 TEST_P(WidgetWithDestroyedNativeViewOrNativeWidgetTest, IsActive) {
   widget()->IsActive();
 }
