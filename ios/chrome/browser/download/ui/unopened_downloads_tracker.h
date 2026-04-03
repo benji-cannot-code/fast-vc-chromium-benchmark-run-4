d7192fd4cd074daf767b78dd105dd856847c3e59
Quentin Pubert <qpubert@google.com>
2026-04-03 03:06:53 -0700

    [iOS] Move header guards to top of the file in UnopenedDownloadsTracker
    Header guards should be located at the top of the file.
    
    Change-Id: Ic51263d6bf5bca50a51aed9d0f67673ba11bd603
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7725862
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Auto-Submit: Quentin Pubert <qpubert@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1609734}

--- ios/chrome/browser/download/ui/unopened_downloads_tracker.h
+++ ios/chrome/browser/download/ui/unopened_downloads_tracker.h
@@ -3,6 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifndef IOS_CHROME_BROWSER_DOWNLOAD_UI_UNOPENED_DOWNLOADS_TRACKER_H_
+#define IOS_CHROME_BROWSER_DOWNLOAD_UI_UNOPENED_DOWNLOADS_TRACKER_H_
+
 #import <set>
 
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list_observer.h"
@@ -14,9 +17,6 @@ class WebStateListChangeDetach;
 namespace web {
 class DownloadTask;
 }  // namespace web
-
-#ifndef IOS_CHROME_BROWSER_DOWNLOAD_UI_UNOPENED_DOWNLOADS_TRACKER_H_
-#define IOS_CHROME_BROWSER_DOWNLOAD_UI_UNOPENED_DOWNLOADS_TRACKER_H_
 
 // Tracks download tasks which were not opened by the user yet. Reports various
 // metrics in DownloadTaskObserver callbacks.
