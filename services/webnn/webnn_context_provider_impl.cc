da91e6b08d1b467904a55b460ec749c967647ed7
Alex Gough <ajgo@chromium.org>
2026-04-02 17:04:16 -0700

    Mark WebNN as unsafe in ASAN
    Additional info will now look like this:
    
    ```
    ==49720==ADDITIONAL INFO
    
    ==49720==Note: Please include this section with the ASan report.
    Task trace:
        #0 0x7ff987ea83f9 in gpu::Sche...
    
    Command line: `"chrome.exe" --type=gpu-process ...
    
    Unsafe feature: WebMachineLearningNeuralNetwork
    
    ==49720==END OF ADDITIONAL INFO
    ```
    
    Bug: b:499056589
    Change-Id: I13f3f614b6617793c73938fea8900b32bed205f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7728303
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1609573}

--- services/webnn/webnn_context_provider_impl.cc
+++ services/webnn/webnn_context_provider_impl.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/command_line.h"
+#include "base/compiler_specific.h"
 #include "base/feature_list.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/task/sequenced_task_runner.h"
@@ -55,6 +56,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/webnn/tflite/context_impl_litert.h"
 #endif
 
+#if defined(ADDRESS_SANITIZER)
+#include <sanitizer/asan_interface.h>
+
+#include "base/debug/asan_service.h"
+#endif
+
 namespace webnn {
 
 namespace {
@@ -97,6 +104,16 @@ void RecordDeviceType(const mojom::Device device) {
   base::UmaHistogramEnumeration("WebNN.DeviceType", uma_value);
 }
 
+#if defined(ADDRESS_SANITIZER)
+NO_SANITIZE("address")
+void AsanUnsafeFeatureWarning(const char* reason,
+                              bool* should_exit_cleanly,
+                              bool* should_abort) {
+  auto* asan_service = base::debug::AsanService::GetInstance();
+  asan_service->Log("\nUnsafe feature: WebMachineLearningNeuralNetwork");
+}
+#endif
+
 }  // namespace
 
 WebNNContextProviderImpl::WebNNContextProviderImpl(
@@ -124,6 +141,12 @@ WebNNContextProviderImpl::WebNNContextProviderImpl(
   // `gpu_host_` is used to ensure that the execution providers used by the ORT
   // backend are ready. It should be connected to the browser process.
   CHECK(gpu_host_.is_bound());
+
+#if defined(ADDRESS_SANITIZER)
+  LOG(ERROR) << "WebMachineLearningNeuralNetwork is an unsafe feature.";
+  base::debug::AsanService::GetInstance()->AddErrorCallback(
+      AsanUnsafeFeatureWarning);
+#endif
 }
 
 WebNNContextProviderImpl::~WebNNContextProviderImpl() {
