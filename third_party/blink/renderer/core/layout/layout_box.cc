82212296b9dc277e57f1e955e72061929dc3dad5
Ian Kilpatrick <ikilpatrick@chromium.org>
2026-04-03 15:57:19 -0700

    [cleanup] Remove IsHTMLLegendElement bitfield.
    Previously we added this bitfield as IsHTMLLegendElement was on the
    hot-path for "FirstChild()" etc, when iterating of layout-object
    children.
    
    Since then we added the LayoutFieldset class which handles this special
    legend behaviour in the layout-tree directly.
    
    Remove.
    
    There should be no behaviour change.
    
    Change-Id: I7ee6dd8ddc479bf15dcfa2ec63856bb4c0a54565
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7685391
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1610011}

--- third_party/blink/renderer/core/layout/layout_box.cc
+++ third_party/blink/renderer/core/layout/layout_box.cc
@@ -492,8 +492,6 @@ void LayoutBoxRareData::Trace(Visitor* visitor) const {
 }
 
 LayoutBox::LayoutBox(ContainerNode* node) : LayoutBoxModelObject(node) {
-  if (blink::IsA<HTMLLegendElement>(node))
-    SetIsHTMLLegendElement();
 }
 
 void LayoutBox::Trace(Visitor* visitor) const {
--- third_party/blink/renderer/core/layout/layout_object.h
+++ third_party/blink/renderer/core/layout/layout_object.h
@@ -1647,11 +1647,6 @@ class CORE_EXPORT LayoutObject : public GarbageCollected<LayoutObject>,
     return bitfields_.IsGlobalRootScroller();
   }
 
-  bool IsHTMLLegendElement() const {
-    NOT_DESTROYED();
-    return bitfields_.IsHTMLLegendElement();
-  }
-
   // Returns true if this can be used as a rendered legend.
   bool IsRenderedLegendCandidate() const {
     NOT_DESTROYED();
@@ -1659,8 +1654,9 @@ class CORE_EXPORT LayoutObject : public GarbageCollected<LayoutObject>,
     // case where the legend is a flex/grid item, LayoutObject::IsFloating()
     // could get set to false, even if the legend's computed style indicates
     // that it is floating.
-    return IsHTMLLegendElement() && !IsOutOfFlowPositioned() &&
-           !StyleRef().IsFloating();
+    const Node* node = GetNode();
+    return !IsOutOfFlowPositioned() && !StyleRef().IsFloating() && node &&
+           node->HasTagName(html_names::kLegendTag);
   }
 
   // Return true if this is the "rendered legend" of a fieldset. They get
@@ -2091,10 +2087,6 @@ class CORE_EXPORT LayoutObject : public GarbageCollected<LayoutObject>,
   void SetIsGlobalRootScroller(bool is_global_root_scroller) {
     NOT_DESTROYED();
     bitfields_.SetIsGlobalRootScroller(is_global_root_scroller);
-  }
-  void SetIsHTMLLegendElement() {
-    NOT_DESTROYED();
-    bitfields_.SetIsHTMLLegendElement(true);
   }
   void SetWhitespaceChildrenMayChange(bool b) {
     NOT_DESTROYED();
@@ -3832,7 +3824,6 @@ class CORE_EXPORT LayoutObject : public GarbageCollected<LayoutObject>,
           is_effective_root_scroller_(false),
           is_global_root_scroller_(false),
           registered_as_first_line_image_observer_(false),
-          is_html_legend_element_(false),
           being_destroyed_(false),
           is_table_column_constraints_dirty_(false),
           is_grid_placement_dirty_(true),
@@ -4101,10 +4092,6 @@ class CORE_EXPORT LayoutObject : public GarbageCollected<LayoutObject>,
     // observer.
     ADD_BOOLEAN_BITFIELD(registered_as_first_line_image_observer_,
                          RegisteredAsFirstLineImageObserver);
-
-    // Whether this object's |Node| is a HTMLLegendElement. Used to increase
-    // performance of |IsRenderedLegend| which is performance sensitive.
-    ADD_BOOLEAN_BITFIELD(is_html_legend_element_, IsHTMLLegendElement);
 
     // True at start of |Destroy()| before calling |WillBeDestroyed()|.
     ADD_BOOLEAN_BITFIELD(being_destroyed_, BeingDestroyed);
