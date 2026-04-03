66a239c619488f9cb1bba9dfc22dadf6f0c0a9df
Michael Wilson <mjwilson@chromium.org>
2026-04-03 10:13:18 -0700

    [WebAudio] Remove unused vector include from audio_destination.h
    This should cause no functional change.
    
    Change-Id: Ibf70966bd3f88764b4299b7a8c9b6249d393cdb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7727798
    Auto-Submit: Michael Wilson <mjwilson@chromium.org>
    Reviewed-by: Mahesh Kannan <kmaheshb@google.com>
    Commit-Queue: Mahesh Kannan <kmaheshb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1609837}

--- third_party/blink/renderer/platform/audio/audio_destination.h
+++ third_party/blink/renderer/platform/audio/audio_destination.h
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <optional>
-#include <vector>
 
 #include "base/memory/raw_ref.h"
 #include "base/memory/scoped_refptr.h"
