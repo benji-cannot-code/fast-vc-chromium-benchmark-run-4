58dbe6614ec34a7b9c6739199797474e12f24565
Colin Blundell <blundell@chromium.org>
2026-04-02 23:37:37 -0700

    [Blink] Remove dead code in CNon2DResourceProviderSI::Snapshot()
    This check is made just above.
    
    Bug: 352263194
    Change-Id: If257adad8977495c00c48f0e593ce56f29f7faf4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7726142
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1609690}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
@@ -1081,10 +1081,6 @@ CanvasNon2DResourceProviderSharedImage::Snapshot(ImageOrientation orientation) {
   // while in this case we are simply returning the rendered CPU-side results to
   // the client.
   if (!is_accelerated_) {
-    if (!IsValid()) {
-      return nullptr;
-    }
-
     FlushCanvas(/*is_overwrite=*/false);
 
     cc::PaintImage paint_image;
