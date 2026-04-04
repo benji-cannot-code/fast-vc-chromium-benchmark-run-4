b4bd548b7279b6d735ca43ded5e7b4192888025b
Matt Jones <mdjones@chromium.org>
2026-04-03 17:00:09 -0700

    Do not dismiss snackbars on bottomsheet close
    This patch prevents clearing all snackbars when the bottom sheet
    closes. Before crrev.com/c/7712352, snackbars that were created
    prior to the bottom sheet closing would stay open for some time to
    allow the user to interact with it. The change above changes that
    expectation to close them immediately. Long-term, snackbars that
    show as part of an action from a bottom sheet should either wait
    until the sheet is closed or be able to select the scope they want
    to be displayed in.
    
    Demo:
    https://screencast.googleplex.com/cast/NjM2ODM5ODE0ODYzMjU3NnxjN2I4ZDNhNS1lZA
    
    Bug: 499164555
    Change-Id: I81e8f8b3469cad925ac07289ed10cf1fc3e86d32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7728452
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1610032}

--- chrome/android/junit/src/org/chromium/chrome/browser/ui/ChromeActivitySnackbarHelperUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/ui/ChromeActivitySnackbarHelperUnitTest.java
@@ -140,24 +140,21 @@ public class ChromeActivitySnackbarHelperUnitTest {
         observer.onSheetStateChanged(BottomSheetController.SheetState.HALF, 0);
         verify(mSnackbarManager)
                 .pushParentViewOverride(eq(ParentOverrideSlot.BOTTOM_SHEET), any(), any());
-        verify(mSnackbarManager).dismissAllSnackbars();
 
         // FULL state -> should not push again
         observer.onSheetStateChanged(BottomSheetController.SheetState.FULL, 0);
         verify(mSnackbarManager, times(1))
                 .pushParentViewOverride(eq(ParentOverrideSlot.BOTTOM_SHEET), any(), any());
-        verify(mSnackbarManager, times(1)).dismissAllSnackbars();
 
         // PEEK state -> pop override
         observer.onSheetStateChanged(BottomSheetController.SheetState.PEEK, 0);
         verify(mSnackbarManager).popParentViewOverride(eq(ParentOverrideSlot.BOTTOM_SHEET));
-        verify(mSnackbarManager, times(2)).dismissAllSnackbars();
 
         // HIDDEN state -> should not pop again
         observer.onSheetStateChanged(BottomSheetController.SheetState.HIDDEN, 0);
         verify(mSnackbarManager, times(1))
                 .popParentViewOverride(eq(ParentOverrideSlot.BOTTOM_SHEET));
-        verify(mSnackbarManager, times(2)).dismissAllSnackbars();
+        verify(mSnackbarManager, times(0)).dismissAllSnackbars();
     }
 
     @Test
@@ -203,7 +200,6 @@ public class ChromeActivitySnackbarHelperUnitTest {
         observer.onSheetContentChanged(mockContent);
         verify(mSnackbarManager)
                 .pushParentViewOverride(eq(ParentOverrideSlot.BOTTOM_SHEET), any(), any());
-        verify(mSnackbarManager).dismissAllSnackbars();
 
         // Switch to not allowed content -> pop override
         BottomSheetContent mockContent2 = mock(BottomSheetContent.class);
@@ -211,7 +207,7 @@ public class ChromeActivitySnackbarHelperUnitTest {
         when(mockContent2.allowInSheetContentSnackbars()).thenReturn(false);
         observer.onSheetContentChanged(mockContent2);
         verify(mSnackbarManager).popParentViewOverride(eq(ParentOverrideSlot.BOTTOM_SHEET));
-        verify(mSnackbarManager, times(2)).dismissAllSnackbars();
+        verify(mSnackbarManager, times(0)).dismissAllSnackbars();
     }
 
     @Test
