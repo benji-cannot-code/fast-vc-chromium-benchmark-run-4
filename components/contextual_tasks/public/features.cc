0a315f2ab4323493c6a764f6246f207975109d7f
Shakti Sahu <shaktisahu@chromium.org>
2026-04-03 17:28:24 -0700

    [contextual_tasks] Remove kTaskScopedSidePanel feature
    This CL cleans up kTaskScopedSidePanel feature as it's decided to
    use the default value. Updated the tests.  Clean refactor.
    
    Bug: b:499270923
    Change-Id: I8d265bfa122ca9f23836ec3c3d61b7e7a519717d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7728967
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1610040}

--- components/contextual_tasks/public/features.cc
+++ components/contextual_tasks/public/features.cc
@@ -190,11 +190,6 @@ const base::FeatureParam<ExpandButtonOption> kExpandButtonOptions(
     ExpandButtonOption::kToolbarCloseButton,
     &kExpandButtonOption);
 
-const base::FeatureParam<bool> kTaskScopedSidePanel(
-    &kContextualTasks,
-    "ContextualTasksTaskScopedSidePanel",
-    true);
-
 const base::FeatureParam<bool> kOpenSidePanelOnLinkClicked(
     &kContextualTasks,
     "ContextualTasksOpenSidePanelOnLinkClicked",
