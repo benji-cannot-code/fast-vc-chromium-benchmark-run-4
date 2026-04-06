8a25022ec9e334fb9594e6bae02bf7a49962c95f
Scott Haseley <shaseley@chromium.org>
2026-04-06 10:27:47 -0700

    Don't expose internal state to LargestContentfulPaintCalculatorTest
    Follow-up to crrev.com/c/7675140, this CL changes LCP tests to read
    state from public APIs rather than reaching into private state. As a
    result, we can un-friend LargestContentfulPaintCalculatorTest. The
    two bits that change are:
     1. The LCP candidate size, which we can read via the delegate as
        entries are emitted.
     2. The largest image BPP. This one was a bit different in that we
        stored internal state just so we could read it in tests, but this
        really isn't necessary because we can read it from the metrics
        state. The difference is that we previously only set this if the
        image was emitted to performance timeline, but this was not a
        necessary restriction because we can monitor the candidates.
    
    Bug: 454082771
    Change-Id: I2420767ef77e772add2940f59f740f0327ec4372
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7713460
    Reviewed-by: Johannes Henkel <johannes@chromium.org>
    Reviewed-by: Michal Mocny <mmocny@chromium.org>
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1610345}

--- third_party/blink/renderer/core/paint/timing/largest_contentful_paint_calculator_test.cc
+++ third_party/blink/renderer/core/paint/timing/largest_contentful_paint_calculator_test.cc
@@ -111,7 +111,7 @@ class LargestContentfulPaintCalculatorTest : public RenderingTest {
   }
 
   uint64_t LargestReportedSize() {
-    return GetLargestContentfulPaintCalculator()->largest_reported_size_;
+    return test_delegate_->LargestReportedSize();
   }
 
   uint64_t LargestImagePaintSize() {
@@ -127,7 +127,9 @@ class LargestContentfulPaintCalculatorTest : public RenderingTest {
   }
 
   double LargestContentfulPaintCandidateImageBPP() {
-    return GetLargestContentfulPaintCalculator()->largest_image_bpp_;
+    return GetLargestContentfulPaintCalculator()
+        ->LatestLcpDetails()
+        .largest_contentful_paint_image_bpp;
   }
 
   uint64_t CandidateCount() { return test_delegate_->CandidateCount(); }
@@ -152,6 +154,7 @@ class LargestContentfulPaintCalculatorTest : public RenderingTest {
                                  Element* element) override {
       ++candidate_count_;
       current_candidate_ = element;
+      largest_reported_size_ = paint_size;
     }
 
     void OnLcpMetricsForReportingChanged() override {}
@@ -164,10 +167,12 @@ class LargestContentfulPaintCalculatorTest : public RenderingTest {
 
     Element* CurrentCandidate() { return current_candidate_; }
     wtf_size_t CandidateCount() { return candidate_count_; }
+    uint64_t LargestReportedSize() { return largest_reported_size_; }
 
    private:
     Member<Element> current_candidate_;
-    wtf_size_t candidate_count_;
+    wtf_size_t candidate_count_ = 0;
+    uint64_t largest_reported_size_ = 0;
   };
 
   LargestContentfulPaintCalculator* GetLargestContentfulPaintCalculator() {
@@ -238,7 +243,9 @@ TEST_F(LargestContentfulPaintCalculatorTest, ImageLargerText) {
   EXPECT_GT(LargestReportedSize(), 9u);
   EXPECT_EQ(CandidateCount(), 1u);
   EXPECT_EQ(CurrentLcpCandidate()->GetIdAttribute(), "text");
-  EXPECT_FLOAT_EQ(LargestContentfulPaintCandidateImageBPP(), 0.0f);
+  // The image is still reported to metrics even though it wasn't a web-exposed
+  // candidate.
+  EXPECT_FLOAT_EQ(LargestContentfulPaintCandidateImageBPP(), 800.0f / 9.0f);
   trace_analyzer::Stop();
 }
 
