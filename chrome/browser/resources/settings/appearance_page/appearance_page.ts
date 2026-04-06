a6ecb569bba2e907566d196dc3856c14266d134b
Kunal Daftari <kunaldaftari@google.com>
2026-04-06 12:53:10 -0700

    [Vertical Tabs] Changing Settings Page Ordering and Visibility
    This CL indents the tab groups button and EOH toggle button in
    chrome://settings. It also makes sure that they are both only visible
    when in VT and not HT.
    
    Occasionally, I would get an issue where when I navigated directly to
    chrome://settings/appearance it would not show the TabStripPosition and
    its secondary options because of an issue accessing the pref. I added a
    guard in the EOH setting check which should help this.
    
    There are two versions of the tab groups button that can be indented,
    and they are mutually exclusive since showProjectsPanelButton requires
    showProjectsPanelEnabled_ but showEverythingMenuButton requires
    !showProjectsPanelButton.
    
    Bug: 497852282
    Change-Id: I3abba61b0323bfc5430ce98607ee105a54d7706c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7722401
    Commit-Queue: Kunal Daftari <kunaldaftari@google.com>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1610411}

--- chrome/browser/resources/settings/appearance_page/appearance_page.ts
+++ chrome/browser/resources/settings/appearance_page/appearance_page.ts
@@ -579,6 +579,9 @@ export class SettingsAppearancePageElement extends
   }
 
   private showVerticalTabsExpandOnHoverSetting_(): boolean {
+    if (this.prefs === undefined) {
+      return false;
+    }
     return this.showVerticalTabsExpandOnHoverEnabled_ &&
         this.getPref<boolean>('vertical_tabs.enabled').value;
   }
