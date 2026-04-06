4334a71198b3a38f96f9a1983e882cb89f8fe914
Karan Shukla <karanshukla@google.com>
2026-04-06 13:13:29 -0700

    [Live Tab Retrieval] Search Functions
    Implement the two search functions for LiveTabRetriever:
    * Semantic Search
    * Keyword Match.
    
    These functions are implemented separately from the main
    LiveTabRetriever class in order to better encapsulate the search logic
    away from the service management logic (managing callbacks, requests,
    lifetimes, etc.).
    
    Bug: b:492628039
    Change-Id: I65ea87ae0b8a3899a9d99767bab23ccbb3938e80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7705557
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Commit-Queue: Karan Shukla <karanshukla@google.com>
    Cr-Commit-Position: refs/heads/main@{#1610420}

---
+++ components/accessibility_annotator/core/live_tab_context/search.h
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_ACCESSIBILITY_ANNOTATOR_CORE_LIVE_TAB_CONTEXT_SEARCH_H_
+#define COMPONENTS_ACCESSIBILITY_ANNOTATOR_CORE_LIVE_TAB_CONTEXT_SEARCH_H_
+
+#include <string>
+#include <vector>
+
+#include "components/page_content_annotations/core/page_embeddings_common.h"
+#include "components/passage_embeddings/core/passage_embeddings_types.h"
+
+namespace accessibility_annotator {
+
+struct ScoredPassage {
+  float score;
+  std::u16string passage;
+};
+
+// Returns the `page_embeddings` closest to `query_embedding`, sorted by
+// relevance.
+std::vector<ScoredPassage> RankPassagesBySemanticSimilarity(
+    const passage_embeddings::Embedding& query_embedding,
+    const std::vector<page_content_annotations::PassageEmbedding>&
+        page_embeddings);
+
+// Returns passages that contain `query`.
+std::vector<ScoredPassage> FindPassagesByKeywordMatching(
+    const std::u16string& query,
+    const std::vector<std::string>& passages);
+
+}  // namespace accessibility_annotator
+
+#endif  // COMPONENTS_ACCESSIBILITY_ANNOTATOR_CORE_LIVE_TAB_CONTEXT_SEARCH_H_
