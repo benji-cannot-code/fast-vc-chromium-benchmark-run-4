b1bfd8475b70e585b06f21c4a4de11b32d940efb
hiroki oshima <hiroki.oshima@gmail.com>
2026-04-06 05:10:56 -0700

    fix HotspotTrayViewTest dangling pointer
    Bug: 40212619
    Change-Id: Ie0f1598b786f1438138079bebe4f6f6dc97a1e1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7693940
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Di Wu <diwux@google.com>
    Commit-Queue: Di Wu <diwux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1610251}

--- ash/system/hotspot/hotspot_tray_view_unittest.cc
+++ ash/system/hotspot/hotspot_tray_view_unittest.cc
@@ -66,6 +66,7 @@ class HotspotTrayViewTest : public AshTestBase {
   }
 
   void TearDown() override {
+    hotspot_tray_view_ = nullptr;
     widget_.reset();
     AshTestBase::TearDown();
   }
@@ -92,7 +93,7 @@ class HotspotTrayViewTest : public AshTestBase {
  protected:
   base::test::ScopedFeatureList scoped_feature_list_;
   std::unique_ptr<views::Widget> widget_;
-  raw_ptr<HotspotTrayView, DanglingUntriaged> hotspot_tray_view_;
+  raw_ptr<HotspotTrayView> hotspot_tray_view_;
 };
 
 TEST_F(HotspotTrayViewTest, HotspotIconImage) {
