33d403a1acf71a4dadab473658a24a21d36ffd10
ahmedmoussa <ahmedmoussa@google.com>
2026-04-06 14:04:45 -0700

    Fix DCHECK crash in UninstallSoda by making DeleteSodaFiles static
    The crash was caused by SodaInstallerImpl::UninstallSoda posting a task to the ThreadPool using a WeakPtr bound to the UI thread. When
    DeleteSodaFiles ran on a background thread, the WeakPtr sequence checker failed because it was being accessed from a different sequence than it was created on.
    
    Since DeleteSodaFiles only performs file operations using static utility functions and doesn't require any class state, this CL resolves the issue by:
    1. Declaring DeleteSodaFiles as a static method in SodaInstallerImpl.
    2. Updating the PostTask in UninstallSoda to bind to the static method
    directly, removing the need for a WeakPtr.
    
    This ensures the background task can safely execute on the ThreadPool
    without violating sequence safety.
    
    Fixed: 497836918
    Change-Id: I21bb9faafdffa60d7aa9eb2b667618430586aed9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7723739
    Reviewed-by: Katie D <katie@chromium.org>
    Commit-Queue: Ahmed Moussa <ahmedmoussa@google.com>
    Reviewed-by: Evan Liu <evliu@google.com>
    Auto-Submit: Ahmed Moussa <ahmedmoussa@google.com>
    Cr-Commit-Position: refs/heads/main@{#1610449}

--- chrome/browser/accessibility/soda_installer_impl.cc
+++ chrome/browser/accessibility/soda_installer_impl.cc
@@ -125,11 +125,11 @@ std::vector<std::string> SodaInstallerImpl::GetAvailableLanguages() const {
 }
 
 void SodaInstallerImpl::UninstallSoda(PrefService* global_prefs) {
-  base::ThreadPool::PostTask(FROM_HERE,
-                             {base::MayBlock(), base::TaskPriority::BEST_EFFORT,
-                              base::TaskShutdownBehavior::BLOCK_SHUTDOWN},
-                             base::BindOnce(&SodaInstallerImpl::DeleteSodaFiles,
-                                            weak_factory_.GetWeakPtr()));
+  base::ThreadPool::PostTask(
+      FROM_HERE,
+      {base::MayBlock(), base::TaskPriority::BEST_EFFORT,
+       base::TaskShutdownBehavior::BLOCK_SHUTDOWN},
+      base::BindOnce(&SodaInstallerImpl::DeleteSodaFiles));
 
   SodaInstaller::UnregisterLanguages(global_prefs);
 
@@ -234,6 +234,7 @@ void SodaInstallerImpl::OnSodaLanguagePackInstalled(
       GetInstallationResultMetricForLanguagePack(language_code), true);
 }
 
+// static
 void SodaInstallerImpl::DeleteSodaFiles() {
   base::DeletePathRecursively(speech::GetSodaDirectory());
   base::DeletePathRecursively(speech::GetSodaLanguagePacksDirectory());
--- chrome/browser/accessibility/soda_installer_impl.h
+++ chrome/browser/accessibility/soda_installer_impl.h
@@ -63,7 +63,7 @@ class SodaInstallerImpl : public SodaInstaller,
   void OnSodaLanguagePackInstalled(speech::LanguageCode language_code);
 
  private:
-  void DeleteSodaFiles();
+  static void DeleteSodaFiles();
 
   void UpdateAndNotifyOnSodaProgress(speech::LanguageCode language_code);
 
