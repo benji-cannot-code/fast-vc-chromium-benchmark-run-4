c63d8b48157143b827dd1797734c4fa686a02f8b
Kenichi Ishibashi <bashi@chromium.org>
2026-04-06 02:57:49 -0700

    Revert "[Vertical tabs] Update the combobutton to show labels in expand on hover state."
    This reverts commit 69a7fe291eec5fa752bc7636541f49866dbfbdb8.
    
    Reason for revert: Speculatively reverting for many test failures on Linux CFI, https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20CFI/31717/overview
    
    Original change's description:
    > [Vertical tabs] Update the combobutton to show labels in expand on hover state.
    >
    > This updates the combobutton content alignments and sets the label
    > button text if there is space based on if the width is larger than the
    > preferred width. Also this adds a setting on the TabStripFlatEdgeButton
    > to enable this so it does not apply to the NTB.
    >
    > screencast: https://screencast.googleplex.com/cast/NDgzMTk2NTYzODI5NTU1MnwxYzMzZWZmYS0xOQ
    >
    > Bug: 493593953
    > Change-Id: I3db2c19470d53b2d30228a9a1b01b778633c3029
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7728780
    > Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    > Commit-Queue: Caroline Rising <corising@chromium.org>
    > Reviewed-by: Kunal Daftari <kunaldaftari@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1610050}
    
    No-Try: true
    Bug: 493593953
    Change-Id: I8be8762de974a0242cdbf835e01ba164cebbaedc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7725647
    Auto-Submit: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Kenichi Ishibashi <bashi@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1610244}

--- chrome/browser/ui/views/tabs/shared/tab_strip_flat_edge_button.cc
+++ chrome/browser/ui/views/tabs/shared/tab_strip_flat_edge_button.cc
@@ -27,8 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/widget/widget.h"
 
 namespace {
-constexpr int kButtonWithLabelPadding = 4;
-
 class TabStripFlatEdgeButtonActionViewInterface
     : public views::LabelButtonActionViewInterface {
  public:
@@ -41,11 +39,8 @@ class TabStripFlatEdgeButtonActionViewInterface
   // views::LabelButtonActionViewInterface:
   void ActionItemChangedImpl(actions::ActionItem* action_item) override {
     // Calling ButtonActionViewInterface instead of
-    // LabelButtonActionViewInterface to avoid the text of the button being set
-    // by default. We will set it manually if there is space.
+    // LabelButtonActionViewInterface to avoid the text of the button being set.
     ButtonActionViewInterface::ActionItemChangedImpl(action_item);
-    action_view_->SetLabelText(std::u16string(action_item->GetText()));
-
     if (action_item->GetImage().IsVectorIcon()) {
       action_view_->UpdateIcon(action_item->GetImage());
     }
@@ -53,7 +48,6 @@ class TabStripFlatEdgeButtonActionViewInterface
 
   void OnViewChangedImpl(actions::ActionItem* action_item) override {
     ButtonActionViewInterface::OnViewChangedImpl(action_item);
-
     if (action_item->GetImage().IsVectorIcon()) {
       action_view_->UpdateIcon(action_item->GetImage());
     }
@@ -78,7 +72,6 @@ TabStripFlatEdgeButton::TabStripFlatEdgeButton() {
       kColorTabStripControlButtonInkDropRipple);
   SetIconSize(
       GetLayoutConstant(LayoutConstant::kVerticalTabStripButtonIconSize));
-  SetImageLabelSpacing(kButtonWithLabelPadding);
   SetInsets(gfx::Insets());
 }
 
@@ -159,16 +152,6 @@ void TabStripFlatEdgeButton::SetFlatEdgeFactor(float factor) {
   SchedulePaint();
 }
 
-void TabStripFlatEdgeButton::SetShouldShowLabel(bool show_label) {
-  should_show_label_ = show_label;
-  if (should_show_label_) {
-    label()->SetPaintToLayer();
-    label()->SetSkipSubpixelRenderingOpacityCheck(true);
-    label()->layer()->SetFillsBoundsOpaquely(false);
-    label()->SetSubpixelRenderingEnabled(false);
-  }
-}
-
 base::CallbackListSubscription
 TabStripFlatEdgeButton::RegisterWillInvokeActionCallback(
     base::RepeatingClosure callback) {
@@ -196,9 +179,6 @@ void TabStripFlatEdgeButton::OnThemeChanged() {
       GetImageModel(views::Button::STATE_NORMAL);
   if (model && model->IsVectorIcon()) {
     UpdateIcon(*model);
-  }
-  if (should_show_label_) {
-    UpdateLabelColor();
   }
 }
 
@@ -231,18 +211,6 @@ void TabStripFlatEdgeButton::SetIconSize(int icon_size) {
   if (model && model->IsVectorIcon()) {
     UpdateIcon(*model);
   }
-}
-
-void TabStripFlatEdgeButton::SetLabelText(const std::u16string& text) {
-  if (label_text_ == text) {
-    return;
-  }
-  label_text_ = text;
-  UpdateLabel(width() > CalculatePreferredSize({}).width());
-}
-
-void TabStripFlatEdgeButton::OnBoundsChanged(const gfx::Rect& previous_bounds) {
-  UpdateLabel(width() > CalculatePreferredSize({}).width());
 }
 
 void TabStripFlatEdgeButton::AddedToWidget() {
@@ -305,32 +273,6 @@ SkRRect TabStripFlatEdgeButton::GetButtonShape() const {
   SkRRect rrect;
   rrect.setRectRadii(rect, radii);
   return rrect;
-}
-
-void TabStripFlatEdgeButton::UpdateLabel(bool should_show) {
-  if (!should_show_label_) {
-    return;
-  }
-  if (should_show == GetText().empty()) {
-    SetHorizontalAlignment(should_show ? gfx::ALIGN_LEFT : gfx::ALIGN_CENTER);
-    SetInsets(should_show ? gfx::Insets::VH(0, kButtonWithLabelPadding)
-                          : gfx::Insets());
-  }
-  auto label = should_show ? label_text_ : std::u16string();
-  if (label != GetText()) {
-    SetText(label);
-  }
-  if (should_show) {
-    UpdateLabelColor();
-  }
-}
-
-void TabStripFlatEdgeButton::UpdateLabelColor() {
-  const ui::ColorId color_id = GetForegroundColor();
-  SetTextColor(views::Button::STATE_NORMAL, color_id);
-  SetTextColor(views::Button::STATE_HOVERED, color_id);
-  SetTextColor(views::Button::STATE_PRESSED, color_id);
-  SetTextColor(views::Button::STATE_DISABLED, color_id);
 }
 
 BEGIN_METADATA(TabStripFlatEdgeButton)
