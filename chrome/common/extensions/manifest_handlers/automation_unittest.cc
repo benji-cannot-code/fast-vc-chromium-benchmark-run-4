d4110370e2b8e5490eea2dd88faaf5606a7bc7ed
Justin Lulejian <jlulejian@chromium.org>
2026-04-06 15:15:40 -0700

    [Extensions] Remove unused GetAutomationInfo method.
    The GetAutomationInfo helper method in AutomationManifestTest is no
    longer called and can be safely removed.
    
    Bug: N/A
    Change-Id: Iaec615d2f18d2133e59ef62cbe31597129b7bbdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7732181
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1610473}

--- chrome/common/extensions/manifest_handlers/automation_unittest.cc
+++ chrome/common/extensions/manifest_handlers/automation_unittest.cc
@@ -27,12 +27,6 @@ class AutomationManifestTest : public ChromeManifestTest {
  public:
   AutomationManifestTest() : channel_(version_info::Channel::UNKNOWN) {}
 
- protected:
-  AutomationInfo* GetAutomationInfo(scoped_refptr<Extension> extension) {
-    return static_cast<AutomationInfo*>(
-        extension->GetManifestData(manifest_keys::kAutomation));
-  }
-
  private:
   void SetUp() override {
     auto* command_line = base::CommandLine::ForCurrentProcess();
