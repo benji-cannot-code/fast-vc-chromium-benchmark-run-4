321de6e28340e75e61efbc99c99871df746ca07a
Cheick Cisse <cheickcisse@google.com>
2026-04-06 09:27:58 -0700

    [iOS] Use custom UIView with AVPlayerLayer
    Currently, AVPlayerLayer is added as a sublayer of a generic UIView, and
    its frame is manually synchronized viewDidLayoutSubviews. This approach
    seems to have recently started failing because viewDidLayoutSubviews is
    being fired before Auto Layout constraints fully resolve
    (_playerView.bounds = 0x0) the view's bounds are CGRectZero, causing the
    layer (_playerLayer.frame) to be sized to zero.
    
    This CL fixes the issue by creating PictureInPicturePlayerView, a custom
    UIView that overrides +layerClass to return AVPlayerLayer. By making
    AVPlayerLayer the backing layer of the view, we get autolayout for free,
    and don't need to manually update the layer's frame in
    viewDidLayoutSubviews anymore.
    
    Bug: 498903534
    Change-Id: I9d8a469d90528de56e2aa4223b2659120e5dd19b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7731523
    Reviewed-by: Ibrahim Kanouche <kanouche@google.com>
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Cr-Commit-Position: refs/heads/main@{#1610322}

---
+++ ios/chrome/browser/picture_in_picture/ui/picture_in_picture_player_view.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_PICTURE_IN_PICTURE_UI_PICTURE_IN_PICTURE_PLAYER_VIEW_H_
+#define IOS_CHROME_BROWSER_PICTURE_IN_PICTURE_UI_PICTURE_IN_PICTURE_PLAYER_VIEW_H_
+
+#import <UIKit/UIKit.h>
+
+@class AVPlayerLayer;
+
+// A UIView subclass that provides an AVPlayerLayer as its backing layer.
+@interface PictureInPicturePlayerView : UIView
+
+// The player layer that backs this view.
+@property(nonatomic, readonly) AVPlayerLayer* playerLayer;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_PICTURE_IN_PICTURE_UI_PICTURE_IN_PICTURE_PLAYER_VIEW_H_
---
+++ ios/chrome/browser/picture_in_picture/ui/picture_in_picture_player_view.mm
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/picture_in_picture/ui/picture_in_picture_player_view.h"
+
+#import <AVFoundation/AVFoundation.h>
+
+#import "base/apple/foundation_util.h"
+
+@implementation PictureInPicturePlayerView
+
++ (Class)layerClass {
+  return [AVPlayerLayer class];
+}
+
+- (AVPlayerLayer*)playerLayer {
+  return base::apple::ObjCCastStrict<AVPlayerLayer>(self.layer);
+}
+
+@end
