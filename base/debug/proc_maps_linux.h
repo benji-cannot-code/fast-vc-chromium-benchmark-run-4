d873e9638e3be74cbac85959d1a27cf6b9e0cc31
gate kibr <gkvjwa@gmail.com>
2026-04-06 13:29:52 -0700

    Add const to MappedMemoryRegion::operator=
    operator=(MappedMemoryRegion&) should be operator=(const
    MappedMemoryRegion&), matching the copy constructor's signature
    
    Change-Id: I8ceb0dcc4d6d6499f4eee85b8484552a335de18e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7711892
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1610426}

--- base/debug/proc_maps_linux.h
+++ base/debug/proc_maps_linux.h
@@ -30,7 +30,7 @@ struct BASE_EXPORT MappedMemoryRegion {
   MappedMemoryRegion();
   MappedMemoryRegion(const MappedMemoryRegion&);
   MappedMemoryRegion(MappedMemoryRegion&&) noexcept;
-  MappedMemoryRegion& operator=(MappedMemoryRegion&);
+  MappedMemoryRegion& operator=(const MappedMemoryRegion&);
   MappedMemoryRegion& operator=(MappedMemoryRegion&&) noexcept;
 
   // The address range [start,end) of mapped memory.
