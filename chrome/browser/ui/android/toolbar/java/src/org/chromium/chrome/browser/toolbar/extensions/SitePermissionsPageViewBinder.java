9bad5aee40c84df33f3ba5f56bdbcd4851ec3068
EmiliaPaz <emiliapaz@chromium.org>
2026-04-05 22:38:29 -0700

    [Extensions] Add header to site permissions page in extensions menu
    This change adds a header to the site permissions page in the extensions
    menu for Desktop Android. The header includes the extension's icon and name, a back button to return to the main menu, and a close button.
    
    A follow up CL will add dimensions for the hardcoded values.
    
    Screencast: crbug.com/431902556#attachment75150846
    
    Bug: 431902556
    Change-Id: I3b84866c812eb5eb0937f411a13b351ee8d364a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7727008
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1610228}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/extensions/SitePermissionsPageViewBinder.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/extensions/SitePermissionsPageViewBinder.java
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.toolbar.extensions;
 
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.chrome.browser.ui.extensions.R;
@@ -20,6 +22,16 @@ public class SitePermissionsPageViewBinder {
             view.findViewById(R.id.extensions_menu_back_button)
                     .setOnClickListener(
                             model.get(SitePermissionsPageProperties.BACK_CLICK_LISTENER));
+        } else if (key == SitePermissionsPageProperties.CLOSE_CLICK_LISTENER) {
+            view.findViewById(R.id.extensions_menu_site_permissions_close_button)
+                    .setOnClickListener(
+                            model.get(SitePermissionsPageProperties.CLOSE_CLICK_LISTENER));
+        } else if (key == SitePermissionsPageProperties.EXTENSION_NAME) {
+            TextView nameView = view.findViewById(R.id.extensions_menu_extension_name);
+            nameView.setText(model.get(SitePermissionsPageProperties.EXTENSION_NAME));
+        } else if (key == SitePermissionsPageProperties.EXTENSION_ICON) {
+            ImageView iconView = view.findViewById(R.id.extensions_menu_extension_icon);
+            iconView.setImageBitmap(model.get(SitePermissionsPageProperties.EXTENSION_ICON));
         } else if (key == SitePermissionsPageProperties.EXTENSION_ID) {
             // TODO(cburg.com/432392216): Implement data pull for site permissions page.
         }
