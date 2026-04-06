db0a8b3e754a7837622cc309a7394c8b042d5e53
hiroki oshima <hiroki.oshima@gmail.com>
2026-04-06 07:07:10 -0700

    fix VpnDetailedViewPixelTest dangling pointer
    Bug: 40212619
    Change-Id: Ibff6421828a1303a1849c94406fba5f234ab370a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7709435
    Commit-Queue: Di Wu <diwux@google.com>
    Reviewed-by: Di Wu <diwux@google.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1610264}

--- ash/system/network/vpn_detailed_view_pixeltest.cc
+++ ash/system/network/vpn_detailed_view_pixeltest.cc
@@ -66,6 +66,11 @@ class VpnDetailedViewPixelTest : public AshTestBase {
     vpn_detailed_view_ = static_cast<VpnDetailedView*>(detailed_view);
   }
 
+  void TearDown() override {
+    vpn_detailed_view_ = nullptr;
+    AshTestBase::TearDown();
+  }
+
   std::optional<pixel_test::InitParams> CreatePixelTestInitParams()
       const override {
     return pixel_test::InitParams();
@@ -113,7 +118,7 @@ class VpnDetailedViewPixelTest : public AshTestBase {
     vpn_detailed_view_->OnGetNetworkStateList(std::move(networks));
   }
 
-  raw_ptr<VpnDetailedView, DanglingUntriaged> vpn_detailed_view_ = nullptr;
+  raw_ptr<VpnDetailedView> vpn_detailed_view_ = nullptr;
 };
 
 TEST_F(VpnDetailedViewPixelTest, OnlyBuiltInVpn) {
