320b6be4a59aa06d77a6bc67c474b08c8d5e8497
Shu Yang <shuyng@google.com>
2026-04-06 11:45:03 -0700

    [Android] Add EnforceIncognitoIsolation flag
    This CL adds the EnforceIncognitoIsolation feature flag to Chrome on
    Android. This flag will be used in subsequent CLs to stub out the
    regular or incognito TabModel depending on the window profile type,
    enforcing isolation.
    The flag is currently disabled by default and has no effect.
    
    Bug: 440167607
    Change-Id: I80d9508cff3bcbd64d7773419362e54d6d4717b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7731604
    Reviewed-by: Michael Wu <mklw@google.com>
    Commit-Queue: Shu Yang <shuyng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1610380}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -362,6 +362,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kEnableToolbarPositioningInResizeMode,
     &kEnableToolbarSwipeOnNonDesktopLff,
     &kEnableXAxisActivityTransition,
+    &kEnforceIncognitoIsolation,
     &kEscCancelDrag,
     &kExperimentsForAgsa,
     &kForceTranslucentNotificationTrampoline,
@@ -711,6 +712,7 @@ BASE_FEATURE(kEnableSwipeToSwitchPane, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kEnableToolbarPositioningInResizeMode, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kEnableToolbarSwipeOnNonDesktopLff, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kEnableXAxisActivityTransition, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kEnforceIncognitoIsolation, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kEscCancelDrag, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kExperimentsForAgsa, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kForceTranslucentNotificationTrampoline, base::FEATURE_DISABLED_BY_DEFAULT);
