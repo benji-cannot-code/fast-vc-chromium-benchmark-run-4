167c7835db7d920cba5d579fd9a708d61a64491a
hiroki oshima <hiroki.oshima@gmail.com>
2026-04-06 05:10:38 -0700

    fix HoldingSpaceTrayChildBubblePlaceholderTest dangling pointers
    Bug: 40212619
    Change-Id: I98f7456eb2aea76bd6469d84cee1978fa1780150
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7702436
    Reviewed-by: Di Wu <diwux@google.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Di Wu <diwux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1610250}

--- ash/system/holding_space/holding_space_tray_child_bubble_unittest.cc
+++ ash/system/holding_space/holding_space_tray_child_bubble_unittest.cc
@@ -37,7 +37,6 @@ class HoldingSpaceTrayChildBubbleTestBase : public HoldingSpaceAshTestBase {
     return child_bubble_;
   }
 
- private:
   // HoldingSpaceAshTestBase:
   void SetUp() override {
     HoldingSpaceAshTestBase::SetUp();
@@ -67,6 +66,7 @@ class HoldingSpaceTrayChildBubbleTestBase : public HoldingSpaceAshTestBase {
     AshTestBase::TearDown();
   }
 
+ private:
   // Invoked from `SetUp()` to create the `child_bubble()`.
   virtual std::unique_ptr<HoldingSpaceTrayChildBubble> CreateChildBubble(
       HoldingSpaceViewDelegate* view_delegate) {
@@ -111,6 +111,12 @@ class HoldingSpaceTrayChildBubblePlaceholderTest
     : public HoldingSpaceTrayChildBubbleTestBase,
       public testing::WithParamInterface</*has_placeholder=*/bool> {
  protected:
+  void TearDown() override {
+    placeholder_ = nullptr;
+    section_ = nullptr;
+    HoldingSpaceTrayChildBubbleTestBase::TearDown();
+  }
+
   void ExpectPlaceholderOrGone() {
     if (has_placeholder()) {
       EXPECT_TRUE(child_bubble()->GetVisible());
@@ -171,8 +177,8 @@ class HoldingSpaceTrayChildBubblePlaceholderTest
   }
 
   // Owned by view hierarchy.
-  raw_ptr<views::View, DanglingUntriaged> placeholder_ = nullptr;
-  raw_ptr<views::View, DanglingUntriaged> section_ = nullptr;
+  raw_ptr<views::View> placeholder_ = nullptr;
+  raw_ptr<views::View> section_ = nullptr;
 };
 
 INSTANTIATE_TEST_SUITE_P(All,
