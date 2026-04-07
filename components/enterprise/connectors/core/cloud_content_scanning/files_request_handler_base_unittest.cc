49a9493bd2090ab0483d827af5c6022f365b6d7a
Xu Li <mxlg@google.com>
2026-04-07 09:32:40 -0700

    Move FileInfo to FilesRequestHandlerBase.
    The `FileInfo` is a concept only used by the FilesRequestHandler. Move
    it to the FilesRequestHandlerBase.
    
    Bug: 498615391
    Change-Id: I590239c24b603dc61be3cadf5994b5173d2b647f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7727394
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Commit-Queue: Xu Li <mxlg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1610817}

--- components/enterprise/connectors/core/cloud_content_scanning/files_request_handler_base_unittest.cc
+++ components/enterprise/connectors/core/cloud_content_scanning/files_request_handler_base_unittest.cc
@@ -49,7 +49,10 @@ class MockFilesRequestHandlerBaseDelegate
               GetPath,
               (size_t index),
               (const, override));
-  MOCK_METHOD(const FileInfo&, GetFileInfo, (size_t index), (override));
+  MOCK_METHOD(const FilesRequestHandlerBase::FileInfo&,
+              GetFileInfo,
+              (size_t index),
+              (override));
   MOCK_METHOD(ReportingEventRouter*, GetReportingEventRouter, (), (override));
   MOCK_METHOD(void, MaybeCompleteScanRequest, (), (override));
   MOCK_METHOD(std::string, GetSource, (), (override));
@@ -269,7 +272,7 @@ TEST_F(FilesRequestHandlerBaseTest, FileRequestCallback) {
   auto delegate_ptr = std::make_unique<MockFilesRequestHandlerBaseDelegate>();
   auto* delegate = delegate_ptr.get();
 
-  FileInfo file_info;
+  FilesRequestHandlerBase::FileInfo file_info;
   EXPECT_CALL(content_analysis_info_, settings())
       .WillRepeatedly(testing::ReturnRef(settings_));
   EXPECT_CALL(*delegate, UpdateRequestHandlerResult(0, testing::_, testing::_))
