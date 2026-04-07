a605bfaedca3e29d6979a72e873ef1b3395d10ed
Alex Moshchuk <alexmos@chromium.org>
2026-04-07 09:33:15 -0700

    Guard against unwanted path traversal with dot-dot-space in CPSP.
    Previously, ChildProcessSecurityPolicyImpl::HasPermissionsForFile()
    handled parent references in the FilePath by comparing directly to
    base::FilePath::kParentDirectory. In some obscure corner cases on
    Windows, this is insufficient, as strings like ".. " (dot-dot-space)
    could fail to be recognized as parent dir references, allowing a
    renderer process to potentially access files outside of the path it
    was previously granted access for. Fix this by using
    FilePath::ReferencesParent() instead, which already properly deals
    with these corner cases.
    
    Note that it appears that the ".. " trick only works when it's at the
    end of a path, and not in the middle. However, this CL still adds
    tests for both cases (both written with help from Gemini).
    
    Change-Id: I2897a271cb82cac7e19c1afd802d6be52fe11f02
    Bug: 498887785
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7728123
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1610818}

--- content/browser/renderer_host/file_utilities_host_impl.h
+++ content/browser/renderer_host/file_utilities_host_impl.h
@@ -7,13 +7,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_BROWSER_RENDERER_HOST_FILE_UTILITIES_HOST_IMPL_H_
 
 #include "build/build_config.h"
+#include "content/common/content_export.h"
 #include "content/public/common/child_process_id.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "third_party/blink/public/mojom/file/file_utilities.mojom.h"
 
 namespace content {
 
-class FileUtilitiesHostImpl : public blink::mojom::FileUtilitiesHost {
+class CONTENT_EXPORT FileUtilitiesHostImpl
+    : public blink::mojom::FileUtilitiesHost {
  public:
   explicit FileUtilitiesHostImpl(ChildProcessId process_id);
   ~FileUtilitiesHostImpl() override;
