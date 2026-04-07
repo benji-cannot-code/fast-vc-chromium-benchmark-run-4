14c6c5b9bda34216fb7b58bf990b83e563b25adb
Kunal Daftari <kunaldaftari@google.com>
2026-04-07 11:13:17 -0700

    [Vertical Tabs] [Test Coverage] Test for toggling EOH with collapse button
    VerticalTabStripTopContainerInteractiveUiTest.VerifyToggleExpandOnHoverWithCollapseButton
    
    Bug: 498175346
    Change-Id: I4999c874bb1b128a4be342ac2365dca740d27193
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7731632
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Kunal Daftari <kunaldaftari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1610888}

--- chrome/browser/ui/views/tabs/vertical/vertical_tab_strip_top_container.cc
+++ chrome/browser/ui/views/tabs/vertical/vertical_tab_strip_top_container.cc
@@ -36,6 +36,10 @@ constexpr int kRegionVerticalPadding = 5;
 constexpr int kComboButtonCollapsedPadding = 2;
 }
 
+DEFINE_CLASS_ELEMENT_IDENTIFIER_VALUE(
+    VerticalTabStripTopContainer,
+    kToggleVerticalTabsExpandOnHoverElementId);
+
 VerticalTabStripTopContainer::VerticalTabStripTopContainer(
     tabs::VerticalTabStripStateController* state_controller,
     actions::ActionItem* root_action_item,
@@ -320,6 +324,9 @@ void VerticalTabStripTopContainer::ShowContextMenuForViewImpl(
         IDC_TOGGLE_VERTICAL_TABS_EXPAND_ON_HOVER,
         l10n_util::GetStringUTF16(
             IDS_VERTICAL_TABS_COLLAPSE_BUTTON_TOGGLE_EXPAND_ON_HOVER));
+    context_menu_model_->SetElementIdentifierAt(
+        context_menu_model_->GetItemCount() - 1,
+        kToggleVerticalTabsExpandOnHoverElementId);
 
     int32_t menu_runner_flags =
         views::MenuRunner::HAS_MNEMONICS | views::MenuRunner::CONTEXT_MENU;
