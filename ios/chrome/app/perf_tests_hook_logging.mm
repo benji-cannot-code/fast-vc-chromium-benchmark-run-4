ac1876a3c4532e88a289d53c0b54cf386d13f90a
Arthur Sonzogni <arthursonzogni@chromium.org>
2026-04-07 01:40:14 -0700

    [ios] Convert to UNSAFE_TODO
    We are migrating from coarse-grained file-level suppression (#pragma
    allow_unsafe_buffers) to granular, expression-level markers
    (UNSAFE_TODO()). The pragma disables safety checks for an entire file,
    whereas UNSAFE_TODO() isolates specific potentially unsafe operations,
    allowing the rest of the file to be enforced as safe.
    
    Doc: https://docs.google.com/document/d/1ORQGBNn2R-CEvNbDTjRd-GrOBOFlCxIHdcvSUA_EhR4/
    AX-Relnotes: N/A
    Cleanup: This is an mechanical #cleanup.
    Bug: 409340989
    Change-Id: I1349de2a20755d32c130e95ec65575070d29daf9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7726083
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1610613}

--- ios/chrome/app/perf_tests_hook_logging.mm
+++ ios/chrome/app/perf_tests_hook_logging.mm
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/474474271): Address use of unsafe formatting functions.
-#pragma allow_unsafe_buffers
-#endif
-
 // clang-format off
 #import "ios/chrome/app/tests_hook.h"
 // clang-format on
