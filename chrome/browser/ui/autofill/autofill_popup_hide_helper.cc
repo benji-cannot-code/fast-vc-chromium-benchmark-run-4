34af8b271e2937a1149d8949eb5da508b3b0aa11
Mikita Kuchyn <kuchyn@google.com>
2026-04-07 06:55:18 -0700

    [Autofill] Fix popup dismissal on same-page navigation.
    ZoomController broadcasts zoom change events during frame navigations
    even if the zoom level remains identical. AutofillPopupHideHelper was
    reacting to these events by unconditionally hiding the popup. This CL
    adds a check to ignore zoom events where the levels are equal according
    to blink::ZoomValuesEqual().
    
    More details in this doc: https://docs.google.com/document/d/1Yt2yZeXvrHjhTKV3ePV2NYMxBO0ASCXRXZ9ywYxtXNo/edit?resourcekey=0-DQZ2iVTJKbpBXz6lx1YoJg&tab=t.0
    
    Bug: 498489010
    Change-Id: Ibbf8b6946c606851512971d3b3d2070b85e61392
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7726504
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Mikita Kuchyn <kuchyn@chromium.org>
    Reviewed-by: Ireneusz Szulc <ireneuszszulc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1610710}

--- chrome/browser/ui/autofill/autofill_popup_hide_helper.cc
+++ chrome/browser/ui/autofill/autofill_popup_hide_helper.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_widget_host.h"
 #include "content/public/browser/visibility.h"
 #include "content/public/browser/web_contents.h"
+#include "third_party/blink/public/common/page/page_zoom.h"
 
 namespace autofill {
 
@@ -97,6 +98,12 @@ void AutofillPopupHideHelper::OnZoomControllerDestroyed(
 
 void AutofillPopupHideHelper::OnZoomChanged(
     const zoom::ZoomController::ZoomChangedEventData& data) {
+  // The ZoomController broadcasts OnZoomChanged on every navigation (including
+  // same-document URL changes). To prevent closing the popup when the content
+  // hasn't actually moved, ignore events where the zoom level is unchanged.
+  if (blink::ZoomValuesEqual(data.old_zoom_level, data.new_zoom_level)) {
+    return;
+  }
   hiding_callback_.Run(SuggestionHidingReason::kContentAreaMoved);
 }
 #endif  // !BUILDFLAG(IS_ANDROID)
