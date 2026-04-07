7b37e0d150ccf95bb5ce4bd231593d7e4459accf
Ming-Ying Chung <mych@chromium.org>
2026-04-07 09:01:34 -0700

    [Webium] Add more UKM recordings for TopChrome WebUI
    [UKM Review][1]
    
    This CL:
    - Updates `NavigationRequest::ShouldRecordNavigationTimelineUkm`
      to allow `chrome://` URLs to log `NavigationTimeline` UKM.
      http://screen/4YpxRTisY6EbFiD
    - Adds renderer process creation and launch timestamps to the
      `NavigationTimeline` UKM event to correlate navigation timings
      with process creation.
    
    [1]: https://docs.google.com/document/d/1TRVzhB1mOzwSoxgxyAOSged6xcwKft-V8jYBKhJ9teQ/edit?resourcekey=0-N9RQ9sQMDM9W6PCKZ4bx_A&tab=t.0
    
    
    Bug: 490810407
    Change-Id: I68d2b9ffd10c367a10ecd57e361b1e6ea2295802
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7701615
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1610795}

--- content/browser/renderer_host/navigation_request.h
+++ content/browser/renderer_host/navigation_request.h
@@ -1685,6 +1685,11 @@ class CONTENT_EXPORT NavigationRequest
     // DidFinishNavigation() to observers, and destroy the NavigationRequest.
     // `MarkFinish()` is used to record this timestamp at the end of navigation.
     base::TimeTicks finish;
+
+    // The time when the renderer process was created.
+    base::TimeTicks renderer_process_created;
+    // The time when the renderer process was launched.
+    base::TimeTicks renderer_process_launched;
   };
 
   // Fill in the timestamps needed to generate a trace of the navigation
@@ -1795,6 +1800,8 @@ class CONTENT_EXPORT NavigationRequest
  private:
   friend class NavigationRequestTest;
   FRIEND_TEST_ALL_PREFIXES(NavigationRequestTest, SanitizeRedirectsForCommit);
+  FRIEND_TEST_ALL_PREFIXES(NavigationRequestTest,
+                           ShouldRecordNavigationTimelineUkmForChromeUI);
 
   struct ConsoleMessage {
     blink::mojom::ConsoleMessageLevel level;
