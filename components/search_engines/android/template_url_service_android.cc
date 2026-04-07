b8bafa0a1b7a84d44e2e82f4835675646c8357f3
Kay Lin <kaiyilin@google.com>
2026-04-07 02:38:03 -0700

    Site Search: User metrics for adding search provider
    This CL adds the logic for recording user metric
    "KeywordEditor_AddKeyword" on Android to align with other platforms that
    are from WebUI.
    
    Bug: 497723443
    Change-Id: I6d2588704453fff2bed68095ee9a2fbe1579ac97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7707200
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Kay Lin <kaiyilin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1610632}

--- components/search_engines/android/template_url_service_android.cc
+++ components/search_engines/android/template_url_service_android.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/memory/raw_ptr.h"
 #include "base/metrics/field_trial_params.h"
+#include "base/metrics/user_metrics.h"
 #include "base/notreached.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_ostream_operators.h"
@@ -44,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Must come after all headers that specialize FromJniType() / ToJniType().
 #include "components/search_engines/android/jni_headers/TemplateUrlService_jni.h"
 
+using base::UserMetricsAction;
 using base::android::JavaRef;
 using base::android::ScopedJavaLocalRef;
 
@@ -518,6 +520,10 @@ bool TemplateUrlServiceAndroid::AddSearchEngine(
   if (search_url.empty()) {
     return false;
   }
+
+  // For WML, this is recorded at
+  // chrome/browser/ui/search_engines/keyword_editor_controller.cc
+  base::RecordAction(UserMetricsAction("KeywordEditor_AddKeyword"));
 
   TemplateURLData data;
   data.SetShortName(short_name);
