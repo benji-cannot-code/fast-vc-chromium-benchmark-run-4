9de3debca8d26de287d05316d115f0eec3d9c35b
Szymon (Simon) Kurek <szymonkurek@google.com>
2026-04-07 09:43:46 -0700

    Refactor: Create a base class for OS signals collector tests
    This change introduces `GenericOsSignalsCollectorTestBase` to share the common test logic between `AndroidOsSignalsCollectorTest` and `DesktopOsSignalsCollectorTest`.
    
    Bug: 446918304
    Change-Id: I1adcc4a725d43c0722d3ae8a88a35f35030b774d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7711776
    Reviewed-by: Zonghan Xu <xzonghan@chromium.org>
    Reviewed-by: Michał Kaczmarczyk <mickaczmarczyk@google.com>
    Commit-Queue: Szymon (Simon) Kurek <szymonkurek@google.com>
    Cr-Commit-Position: refs/heads/main@{#1610828}

--- components/device_signals/core/browser/android/android_os_signals_collector_unittest.cc
+++ components/device_signals/core/browser/android/android_os_signals_collector_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/metrics/histogram_tester.h"
 #include "base/values.h"
 #include "components/device_signals/core/browser/browser_utils.h"
+#include "components/device_signals/core/browser/os_signals_collector_test_base.h"
 #include "components/device_signals/core/browser/signals_types.h"
 #include "components/device_signals/core/browser/user_permission_service.h"
 #include "components/policy/core/common/cloud/cloud_policy_constants.h"
@@ -40,7 +41,6 @@ namespace {
 using safe_browsing::HasHarmfulAppsResultStatus;
 using safe_browsing::VerifyAppsEnabledResult;
 
-constexpr char kFakeBrowserEnrollmentDomain[] = "fake.domain.google.com";
 constexpr char kHarmfulAppsResultHistogramName[] =
     "Enterprise.DeviceSignals.HarmfulApps.Result";
 constexpr char kHarmfulAppsCountHistogramName[] =
@@ -52,19 +52,11 @@ constexpr int kSampleHarmfulAppsErrorCode = 123;
 
 namespace device_signals {
 
-// TODO(446918304): This class should be sharing a base class with
-// `DesktopOsSignalsCollectorTest` to reduce code duplication.
-class AndroidOsSignalsCollectorTest : public testing::Test {
+class AndroidOsSignalsCollectorTest : public GenericOsSignalsCollectorTestBase {
  protected:
   void SetUp() override {
-    auto mock_browser_cloud_policy_store =
-        std::make_unique<policy::MockCloudPolicyStore>(
-            policy::dm_protocol::kChromeMachineLevelUserCloudPolicyType);
-    mock_browser_cloud_policy_store_ = mock_browser_cloud_policy_store.get();
-    mock_browser_cloud_policy_manager_ =
-        std::make_unique<policy::MockCloudPolicyManager>(
-            std::move(mock_browser_cloud_policy_store),
-            task_environment_.GetMainThreadTaskRunner());
+    task_environment_ = std::make_unique<content::BrowserTaskEnvironment>();
+    GenericOsSignalsCollectorTestBase::SetUp();
     signal_collector_ = std::make_unique<AndroidOsSignalsCollector>(
         mock_browser_cloud_policy_manager_.get());
 
@@ -72,32 +64,17 @@ class AndroidOsSignalsCollectorTest : public testing::Test {
     SetHarmfulAppsResult(HasHarmfulAppsResultStatus::SUCCESS, 2);
   }
 
-  void TearDown() override { mock_browser_cloud_policy_store_ = nullptr; }
-
-  void SetFakeBrowserPolicyData() {
-    auto policy_data = std::make_unique<enterprise_management::PolicyData>();
-    policy_data->set_managed_by(kFakeBrowserEnrollmentDomain);
-    mock_browser_cloud_policy_store_->set_policy_data_for_testing(
-        std::move(policy_data));
-  }
-
   // Helper function to check a subset of signals that should or should not be
   // collected based on permission.
-  void CheckSignalsCollected(OsSignalsResponse& response,
-                             bool can_collect_pii) {
-    if (can_collect_pii) {
-      EXPECT_EQ(response.display_name,
-                base::android::device_info::device_name());
-    } else {
-      EXPECT_EQ(response.display_name, std::nullopt);
-    }
-    EXPECT_EQ(response.operating_system, policy::GetOSPlatform());
+  void CheckAndroidSignalsCollected(OsSignalsResponse& response,
+                                    bool can_collect_pii) {
+    CheckSignalsCollected(response, can_collect_pii,
+                          base::android::device_info::device_name());
+
     EXPECT_EQ(response.os_version, base::SysInfo::OperatingSystemVersion());
-    EXPECT_EQ(response.browser_version, version_info::GetVersionNumber());
     EXPECT_EQ(response.device_model, base::android::android_info::model());
     EXPECT_EQ(response.device_manufacturer,
               base::android::android_info::manufacturer());
-    EXPECT_EQ(response.device_enrollment_domain, kFakeBrowserEnrollmentDomain);
     EXPECT_EQ(response.security_patch_ms,
               device_signals::GetSecurityPatchLevelEpoch());
     EXPECT_EQ(response.verified_apps_enabled, expected_verify_app_result_);
@@ -145,10 +122,6 @@ class AndroidOsSignalsCollectorTest : public testing::Test {
             expected_harmful_app_count_ != 0);
   }
 
-  content::BrowserTaskEnvironment task_environment_;
-  std::unique_ptr<policy::MockCloudPolicyManager>
-      mock_browser_cloud_policy_manager_;
-  raw_ptr<policy::MockCloudPolicyStore> mock_browser_cloud_policy_store_;
   std::unique_ptr<AndroidOsSignalsCollector> signal_collector_;
   bool expected_verify_app_result_;
   HasHarmfulAppsResultStatus expected_harmful_app_result_;
@@ -188,8 +161,8 @@ TEST_F(AndroidOsSignalsCollectorTest, GetSignal_Success) {
 
   ASSERT_FALSE(response.top_level_error.has_value());
   ASSERT_TRUE(response.os_signals_response);
-  CheckSignalsCollected(response.os_signals_response.value(),
-                        /*can_collect_pii=*/true);
+  CheckAndroidSignalsCollected(response.os_signals_response.value(),
+                               /*can_collect_pii=*/true);
 }
 
 // Tests that an unsupported signal is marked as unsupported.
@@ -223,8 +196,8 @@ TEST_F(AndroidOsSignalsCollectorTest, GetSignal_MissingConsent) {
 
   ASSERT_FALSE(response.top_level_error.has_value());
   ASSERT_TRUE(response.os_signals_response);
-  CheckSignalsCollected(response.os_signals_response.value(),
-                        /*can_collect_pii=*/false);
+  CheckAndroidSignalsCollected(response.os_signals_response.value(),
+                               /*can_collect_pii=*/false);
 }
 
 // Tests that signal collection is halted if permission is not sufficient.
