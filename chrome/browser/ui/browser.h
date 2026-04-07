a4ddfe44eba3422ce4b97fa222084e60e91dc71f
Jeff Yoon <jeffyoon@google.com>
2026-04-07 06:57:38 -0700

    Migrate TabSearchToolbarButtonController to UnownedUserData
    This change completes the migration of TabSearchToolbarButtonController
    to the UnownedUserData pattern, decoupling it from
    BrowserWindowFeatures. Access is now done via the static From() method.
    
    Detailed changes:
    - TabSearchToolbarButtonController: Inherits from UnownedUserData,
      uses ScopedUnownedUserData to bound its lifetime to
      BrowserWindowInterface.
    - BrowserWindowFeatures: Removed TabSearchToolbarButtonController
      accessor and ownership.
    - BrowserView: Updated call sites to use
      TabSearchToolbarButtonController::From().
    - BrowserActivationWaiter: Updated to use ScopedObservation to avoid
      potential UAF or missing removal on destruction.
    
    Cq-Include-Trybots: luci.chromium.try:android-cast-arm-rel
    Bug: 491863835
    Change-Id: I32c2a8adc26719845b92b7f02a309b8c35f0f717
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7731427
    Commit-Queue: Jeff Yoon <jeffyoon@google.com>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1610714}

--- chrome/browser/ui/browser.h
+++ chrome/browser/ui/browser.h
@@ -1374,6 +1374,8 @@ class Browser : public TabStripModelObserver,
       base::RepeatingCallbackList<void(BrowserWindowInterface*)>;
   DidBecomeInactiveCallbackList did_become_inactive_callback_list_;
 
+  ui::UnownedUserDataHost unowned_user_data_host_;
+
   std::unique_ptr<BrowserWindowFeatures> features_;
 
 #if BUILDFLAG(IS_OZONE)
@@ -1386,8 +1388,6 @@ class Browser : public TabStripModelObserver,
 
   // Tracks whether the browser object is fully initialized.
   bool is_initialized_ = false;
-
-  ui::UnownedUserDataHost unowned_user_data_host_;
 
   // The following factory is used for chrome update coalescing.
   base::WeakPtrFactory<Browser> chrome_updater_factory_{this};
--- chrome/browser/ui/toolbar/pinned_toolbar/tab_search_toolbar_button_controller.cc
+++ chrome/browser/ui/toolbar/pinned_toolbar/tab_search_toolbar_button_controller.cc
@@ -9,14 +9,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/ui/actions/chrome_action_id.h"
 #include "chrome/browser/ui/browser_actions.h"
+#include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/tab_search_bubble_host.h"
 #include "chrome/browser/ui/views/toolbar/pinned_toolbar_actions.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_view.h"
 
+DEFINE_USER_DATA(TabSearchToolbarButtonController);
+
+// static
+TabSearchToolbarButtonController* TabSearchToolbarButtonController::From(
+    BrowserWindowInterface* bwi) {
+  return Get(bwi->GetUnownedUserDataHost());
+}
+
 TabSearchToolbarButtonController::TabSearchToolbarButtonController(
+    BrowserWindowInterface* bwi,
     BrowserView* browser_view)
-    : browser_view_(browser_view) {}
+    : browser_view_(browser_view),
+      scoped_unowned_user_data_(bwi->GetUnownedUserDataHost(), *this) {}
 
 TabSearchToolbarButtonController::~TabSearchToolbarButtonController() = default;
 
