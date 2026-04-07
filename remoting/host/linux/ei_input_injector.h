5b04f3479097821ed40ca23c0b500690107dd72e
Jamie Walch <jamiewalch@chromium.org>
2026-04-06 21:42:35 -0700

    Recreate `EiSenderSession` if it gets disconnected.
    BUG=500107489
    
    Change-Id: Ia5ae2f6e6c867bc16fbf9fbe017970e886b38b67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7732421
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Commit-Queue: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1610543}

--- remoting/host/linux/ei_input_injector.h
+++ remoting/host/linux/ei_input_injector.h
@@ -34,6 +34,7 @@ class EiInputInjector : public InputInjector {
   base::WeakPtr<EiInputInjector> GetWeakPtr();
 
   void SetKeymap(base::WeakPtr<EiKeymap> keymap);
+  void SetEiSession(base::WeakPtr<EiSenderSession> session);
 
   // InputInjector implementation
   void Start(
--- remoting/host/linux/ei_sender_session.h
+++ remoting/host/linux/ei_sender_session.h
@@ -46,6 +46,9 @@ class EiSenderSession {
   void SetKeyboardLayoutMonitor(base::WeakPtr<EiKeyboardLayoutMonitor> monitor);
   void SetInputInjector(base::WeakPtr<EiInputInjector> input_injector);
 
+  // Transfer post-initialization state to a replacement session.
+  void TransferStateTo(EiSenderSession& replacement);
+
   // Injects an event for the provided |usb_keycode|. |is_press| should be true
   // for key-down and repeat events, and false for release events.
   void InjectKeyEvent(std::uint32_t usb_keycode, bool is_press);
@@ -80,7 +83,11 @@ class EiSenderSession {
   // Asynchronously attempts to establish a session with an EIS implementation
   // over |fd| and invokes |callback| with the result. Takes ownership of |fd|,
   // closing it if the session cannot be established.
-  static void CreateWithFd(base::ScopedFD fd, CreateCallback callback);
+  // |disconnect_callback| is invoked if the session is unexpectedly
+  // disconnected after being established.
+  static void CreateWithFd(base::ScopedFD fd,
+                           CreateCallback callback,
+                           base::OnceClosure disconnect_callback);
 
  private:
   using InitCallback = base::OnceCallback<void(base::expected<void, Loggable>)>;
@@ -110,7 +117,9 @@ class EiSenderSession {
   EiSenderSession();
 
   // Attempt to initialize this instance, invoking |callback| with the result.
-  void InitWithFd(base::ScopedFD fd, InitCallback callback);
+  void InitWithFd(base::ScopedFD fd,
+                  InitCallback callback,
+                  base::OnceClosure disconnect_callback);
 
   // Invoked whenever the libei-provided event fd becomes readable, signaling
   // that there is work for the library to perform.
@@ -151,6 +160,7 @@ class EiSenderSession {
   void FreeDeviceState(const EiDevicePtr& device);
 
   InitCallback init_callback_;
+  base::OnceClosure disconnect_callback_;
 
   EiPtr ei_;
   // We currently assume that the first-received seat will be the default, and
