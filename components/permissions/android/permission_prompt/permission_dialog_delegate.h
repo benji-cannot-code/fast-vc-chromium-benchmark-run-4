0cc85b9aba9d2e5f8b4c985aa20b8d48bf8389cf
Antonio Sartori <antoniosartori@chromium.org>
2026-04-07 01:46:27 -0700

    [permissions] Destroy PermissionDialogJavaDelegate async
    This CL makes permissionDialogDelegate::Dismissed destroy the
    PermissionDialogJavaDelegate asynchronously. This prevents UaF issues in
    case Dismissed() is called while a PermissionDialogJavaDelegate method
    is running.
    
    Bug: 499247910
    Change-Id: Iac8a46b4f4fc3fc83dc989c54d0fea41d3a3e6ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7734121
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1610616}

--- components/permissions/android/permission_prompt/permission_dialog_delegate.h
+++ components/permissions/android/permission_prompt/permission_dialog_delegate.h
@@ -121,8 +121,6 @@ class PermissionDialogDelegate : public content::WebContentsObserver {
   // On navigation or page destruction, hide the dialog.
   void DismissDialog();
 
-  void DestroyJavaDelegate() { java_delegate_.reset(); }
-
   // WebContentsObserver:
   void PrimaryPageChanged(content::Page&) override;
   void WebContentsDestroyed() override;
