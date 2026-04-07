77d29d32dbd560587e25a56c51a284c801b2c3b4
Paul Jensen <pauljensen@chromium.org>
2026-04-07 08:04:06 -0700

    Re-enable four WebUIPinnedToolbarActions tests
    Tests needed to a couple feature flags to facilitate the tests.
    
    Bug: 474061420, 499825436
    Change-Id: I4c762e7b6e26a83d522ae84b9436bb75e2089f02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7731570
    Reviewed-by: Youseff Bourouphel <ybourouphael@google.com>
    Auto-Submit: Paul Jensen <pauljensen@chromium.org>
    Commit-Queue: Paul Jensen <pauljensen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1610762}

--- chrome/browser/ui/views/toolbar/webui_toolbar_web_view_browsertest.cc
+++ chrome/browser/ui/views/toolbar/webui_toolbar_web_view_browsertest.cc
@@ -49,6 +49,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/browser_apis/ui_controllers/toolbar/toolbar_ui_api_data_model.mojom.h"
 #include "components/collaboration/public/features.h"
+#include "components/contextual_tasks/public/features.h"
+#include "components/data_sharing/public/features.h"
 #include "components/metrics/content/subprocess_metrics_provider.h"
 #include "components/prefs/pref_service.h"
 #include "components/strings/grit/components_strings.h"
@@ -2304,7 +2306,11 @@ class WebUIPinnedToolbarActionsBrowserTest
              // Need non-zero initial toolbar size, otherwise hidden on Mac.
              features::kWebUIReloadButton,
              // Facilitate testing kActionSidePanelShowComments
-             collaboration::features::kCollaborationComments},
+             collaboration::features::kCollaborationComments,
+             // Facilitate testing kActionsSidePanelShowContextualTasks
+             contextual_tasks::kContextualTasks,
+             // Facilitate testing kActionSendSharedTabGroupFeedback
+             data_sharing::features::kDataSharingFeature},
             {}) {}
 
   void SetUpOnMainThread() override {
@@ -2434,15 +2440,8 @@ class WebUIPinnedToolbarActionsBrowserTest
       };
 };
 
-// TODO(crbug.com/499825436): Fix and enable these tests on Windows and
-// ChromeOS.
-#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_WIN)
-#define MAYBE_PinUnpinIndividually DISABLED_PinUnpinIndividually
-#else
-#define MAYBE_PinUnpinIndividually PinUnpinIndividually
-#endif
 IN_PROC_BROWSER_TEST_F(WebUIPinnedToolbarActionsBrowserTest,
-                       MAYBE_PinUnpinIndividually) {
+                       PinUnpinIndividually) {
   WebUIToolbarWebView* webui_toolbar_view = GetWebUIToolbarWebView(browser());
   views::WebView* web_view = webui_toolbar_view->GetWebViewForTesting();
   content::WebContents* web_contents = web_view->GetWebContents();
@@ -2464,13 +2463,7 @@ IN_PROC_BROWSER_TEST_F(WebUIPinnedToolbarActionsBrowserTest,
   }
 }
 
-#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_WIN)
-#define MAYBE_PinAllTogether DISABLED_PinAllTogether
-#else
-#define MAYBE_PinAllTogether PinAllTogether
-#endif
-IN_PROC_BROWSER_TEST_F(WebUIPinnedToolbarActionsBrowserTest,
-                       MAYBE_PinAllTogether) {
+IN_PROC_BROWSER_TEST_F(WebUIPinnedToolbarActionsBrowserTest, PinAllTogether) {
   WebUIToolbarWebView* webui_toolbar_view = GetWebUIToolbarWebView(browser());
   views::WebView* web_view = webui_toolbar_view->GetWebViewForTesting();
   content::WebContents* web_contents = web_view->GetWebContents();
@@ -2485,13 +2478,7 @@ IN_PROC_BROWSER_TEST_F(WebUIPinnedToolbarActionsBrowserTest,
   }
 }
 
-#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_WIN)
-#define MAYBE_InvokeActions DISABLED_InvokeActions
-#else
-#define MAYBE_InvokeActions InvokeActions
-#endif
-IN_PROC_BROWSER_TEST_F(WebUIPinnedToolbarActionsBrowserTest,
-                       MAYBE_InvokeActions) {
+IN_PROC_BROWSER_TEST_F(WebUIPinnedToolbarActionsBrowserTest, InvokeActions) {
   WebUIToolbarWebView* webui_toolbar_view = GetWebUIToolbarWebView(browser());
   views::WebView* web_view = webui_toolbar_view->GetWebViewForTesting();
   content::WebContents* web_contents = web_view->GetWebContents();
@@ -2876,13 +2863,8 @@ IN_PROC_BROWSER_TEST_F(WebUIPinnedToolbarActionsBrowserTest, ToolbarDivider) {
   ASSERT_TRUE(base::test::RunUntil([&]() { return !is_divider_visible(); }));
 }
 
-#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_WIN)
-#define MAYBE_SetActionElementIdentifier DISABLED_SetActionElementIdentifier
-#else
-#define MAYBE_SetActionElementIdentifier SetActionElementIdentifier
-#endif
 IN_PROC_BROWSER_TEST_F(WebUIPinnedToolbarActionsBrowserTest,
-                       MAYBE_SetActionElementIdentifier) {
+                       SetActionElementIdentifier) {
   WebUIToolbarWebView* webui_toolbar_view = GetWebUIToolbarWebView(browser());
   views::WebView* web_view = webui_toolbar_view->GetWebViewForTesting();
   content::WebContents* web_contents = web_view->GetWebContents();
