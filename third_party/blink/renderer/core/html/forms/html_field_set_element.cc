b63fa247bece3931441b63e63a6fec4f91d5cfd6
Dominic Farolino <dom@chromium.org>
2026-04-07 12:38:27 -0700

    DOM: Fix frame desynchronization bug
    Script should not run during a state-preserving atomic move, i.e.,
    during `Node::moveBefore()`. However, we did not catch the fact that
    `HTMLFieldSetElement::ChildrenChanged()` calls `blur()` on the focused
    element, and this CL prevents this from happening during an atomic
    move. This is because neither focus should change, nor script should
    run during this.
    
    R=nrosenthal@google.com
    
    Bug: 497964917
    Change-Id: Ic7c1ec53df63a21e502b01130ee72395b6cb9fa0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7736582
    Reviewed-by: Noam Rosenthal <nrosenthal@google.com>
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1610937}

--- third_party/blink/renderer/core/html/forms/html_field_set_element.cc
+++ third_party/blink/renderer/core/html/forms/html_field_set_element.cc
@@ -190,8 +190,9 @@ void HTMLFieldSetElement::ChildrenChanged(const ChildrenChange& change) {
         focused_element = element;
     }
   }
-  if (focused_element)
+  if (!GetDocument().StatePreservingAtomicMoveInProgress() && focused_element) {
     focused_element->blur();
+  }
 }
 
 FocusableState HTMLFieldSetElement::SupportsFocus(
