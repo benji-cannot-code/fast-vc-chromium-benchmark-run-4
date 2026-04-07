8f302dc4ec8f7634797170cf8ede8f893c77615f
Cheick Cisse <cheickcisse@google.com>
2026-04-07 11:12:02 -0700

    [iOS] Fix default browser promo on ipad RTL lottie animation
    Screenshots:
    light: https://screenshot.googleplex.com/BybLaHaBMzqh7An.png
    dark: https://screenshot.googleplex.com/BUouEeGhWfKVNuW.png
    
    Bug: 497843844
    Change-Id: I66af41004e38542ec5dcbe4c2d60f31c5576db50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7730866
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Cr-Commit-Position: refs/heads/main@{#1610886}

--- ios/chrome/browser/default_browser/promo/ui/default_browser_instructions_view_controller.mm
+++ ios/chrome/browser/default_browser/promo/ui/default_browser_instructions_view_controller.mm
@@ -210,7 +210,9 @@ NSString* const kDefaultBrowserInstructionsViewDarkAnimationViewId =
 - (void)configureAnimationColors {
   // Custom dynamic colors for kDefaultBrowserAnimationIpad.
   if ([[self animationAssetName]
-          isEqualToString:kDefaultBrowserAnimationIpad]) {
+          isEqualToString:kDefaultBrowserAnimationIpad] ||
+      [[self animationAssetName]
+          isEqualToString:kDefaultBrowserAnimationRtlIpad]) {
     // Configure shape layers.
     ConfigureAnimationSemanticColor(self.animationViewWrapper, kSeparatorColor,
                                     kSeparatorColor);
