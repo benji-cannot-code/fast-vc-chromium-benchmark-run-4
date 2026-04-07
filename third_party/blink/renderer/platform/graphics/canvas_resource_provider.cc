3d2d390d303b4a695c45df7510cac10d5d2258c0
Colin Blundell <blundell@chromium.org>
2026-04-07 08:54:39 -0700

    [Blink] Inline CanvasResourceProvider::EnsureSkiaCanvas()
    Further step toward decoupling CanvasNon2DResourceProviderSharedImage
    from this class hierarchy.
    
    Bug: 352263194
    Change-Id: I9e97fdf10da6b0d80b9b66b36ce599f03bb4b8f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7725726
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1610790}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
@@ -1810,16 +1810,6 @@ void CanvasResourceProvider::NotifyWillTransfer(
   GetFlushForImageListener()->NotifyFlushForImage(content_id);
 }
 
-void CanvasResourceProvider::EnsureSkiaCanvas() {
-  CHECK(!IsAccelerated());
-
-  if (skia_canvas_)
-    return;
-
-  skia_canvas_ = std::make_unique<cc::SkiaPaintCanvas>(
-      GetSkSurface()->getCanvas(), GetOrCreateSWCanvasImageProvider());
-}
-
 CanvasResourceProvider::CanvasImageProvider*
 CanvasResourceProvider::GetOrCreateSWCanvasImageProvider() {
   if (canvas_image_provider_) {
@@ -1931,7 +1921,10 @@ void CanvasNon2DResourceProviderSharedImage::FlushCanvas(bool is_overwrite) {
 
   cc::PaintRecord last_recording = recorder_->ReleaseMainRecording();
   if (!is_accelerated_) {
-    EnsureSkiaCanvas();
+    if (!skia_canvas_) {
+      skia_canvas_ = std::make_unique<cc::SkiaPaintCanvas>(
+          GetSkSurface()->getCanvas(), GetOrCreateSWCanvasImageProvider());
+    }
     skia_canvas_->drawPicture(std::move(last_recording));
   } else if (!IsGpuContextLost()) {
     auto access = WillDrawInternal(is_overwrite);
@@ -2037,7 +2030,10 @@ void CanvasResourceProvider::UnacceleratedRasterRecordForCanvas2D(
   CHECK(!IsAccelerated());
   CHECK(IsCanvas2D());
 
-  EnsureSkiaCanvas();
+  if (!skia_canvas_) {
+    skia_canvas_ = std::make_unique<cc::SkiaPaintCanvas>(
+        GetSkSurface()->getCanvas(), GetOrCreateSWCanvasImageProvider());
+  }
   skia_canvas_->drawPicture(std::move(last_recording));
 }
 
@@ -2163,7 +2159,10 @@ bool CanvasResourceProvider::UnacceleratedWritePixelsForCanvas2D(
   DCHECK(IsValid());
   DCHECK(!recorder_for_canvas_2d_->HasRecordedDrawOps());
 
-  EnsureSkiaCanvas();
+  if (!skia_canvas_) {
+    skia_canvas_ = std::make_unique<cc::SkiaPaintCanvas>(
+        GetSkSurface()->getCanvas(), GetOrCreateSWCanvasImageProvider());
+  }
 
   bool wrote_pixels = GetSkSurface()->getCanvas()->writePixels(
       orig_info, pixels, row_bytes, x, y);
--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
@@ -238,8 +238,6 @@ class PLATFORM_EXPORT CanvasResourceProvider
 
   void ReleaseLockedImages();
 
-  void EnsureSkiaCanvas();
-
   template <class T>
   static std::unique_ptr<T> CreateSharedImageProviderBase(
       gfx::Size size,
