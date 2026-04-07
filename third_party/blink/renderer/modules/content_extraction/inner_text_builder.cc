29928d943997c0fecd158ba27deec29368fabbb2
Kent Tamura <tkent@chromium.org>
2026-04-07 02:56:36 -0700

    Migrate String::Substring() to substr()/subview() in modules/
    This change updates several usages of the legacy String::Substring()
    method to String::substr() or String::subview() to align with modern
    C++ string API usage and improve consistency across the codebase.
    
    The transitions in the affected files are safe:
    
    * modules/ai/proofreader.cc
    ** Tokenize()
       `start` is a result of WordBreakIterator, and it must be winhin the
       bounds of `text`.
    ** Proofreader::GetCorrectionTypes()
       `correction` is an index calculated by accumulating the lengths of
       tokenized strings. It is guaranteed not to exceed the length of the
       original string.
    
    * modules/ai/proofreader_test.cc
      Due to the logic of the `ApplyCorrections()` function, `index` is
      within the bounds of `input`.
    
    * modules/content_extraction/inner_text_builder.cc
      The argument `text_offset` is calculated in conjunction with the
      caller's InnerTextBuilder::Build() to be within the bounds of the
      source string.
    
    Bug: 473854537
    Change-Id: I2d3010ade894a9e75faf8299e33ca8853df22e8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7727522
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1610638}

--- third_party/blink/renderer/modules/content_extraction/inner_text_builder.cc
+++ third_party/blink/renderer/modules/content_extraction/inner_text_builder.cc
@@ -78,7 +78,7 @@ void InnerTextBuilder::AddNextNonFrameSegments(
       *matching_node_location_ <= next_child_offset) {
     if (text_offset != *matching_node_location_) {
       frame.segments.push_back(mojom::blink::InnerTextSegment::NewText(
-          text.Substring(text_offset, *matching_node_location_ - text_offset)));
+          text.substr(text_offset, *matching_node_location_ - text_offset)));
       text_offset = *matching_node_location_;
     }
     frame.segments.push_back(mojom::blink::InnerTextSegment::NewNodeLocation(
@@ -87,7 +87,7 @@ void InnerTextBuilder::AddNextNonFrameSegments(
   }
   if (next_child_offset > text_offset) {
     frame.segments.push_back(mojom::blink::InnerTextSegment::NewText(
-        text.Substring(text_offset, next_child_offset - text_offset)));
+        text.substr(text_offset, next_child_offset - text_offset)));
     text_offset = next_child_offset;
   }
 }
