18f4d5d17e81f715bbb0ccbd5a0652286c081e0c
Francis Beauchamp <fbeauchamp@google.com>
2026-04-08 15:09:04 -0700

    [iOS] Log gemini retry last request
    This change introduces a new histogram and enum to record a log whenever
    the user regenerates an answer in the Gemini floaty.
    
    Associated internal CL:
    => http://crrev.com/i/9161239
    
    Bug: 492473105
    Change-Id: If9db0e1b2ff8980630624e8480c51b852fd8c649
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7722858
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Sebastien S-G <sebsg@chromium.org>
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Commit-Queue: Francis Beauchamp <fbeauchamp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1611810}

--- ios/chrome/browser/intelligence/bwg/metrics/gemini_metrics.mm
+++ ios/chrome/browser/intelligence/bwg/metrics/gemini_metrics.mm
@@ -140,6 +140,9 @@ const char kPromptContextAttachmentHistogram[] =
 const char kResponseGeneratedImageIncluded[] =
     "IOS.Gemini.Response.GeneratedImage.Included";
 
+const char kRegenerateButtonTappedHistogram[] =
+    "IOS.Gemini.RegenerateButton.Tapped";
+
 const char kResponseLatencyWithContextHistogram[] =
     "IOS.Gemini.Response.Latency.WithContext";
 
@@ -477,6 +480,12 @@ void RecordGeminiEntryPointClick(gemini::EntryPoint entry_point,
 
 void RecordGeminiNewChatButtonTapped() {
   base::RecordAction(base::UserMetricsAction("MobileGeminiNewChatTapped"));
+}
+
+void RecordGeminiRegenerateButtonTapped(gemini::RegenerateOptionType option) {
+  base::RecordAction(
+      base::UserMetricsAction("MobileGeminiRegenerateButtonTapped"));
+  base::UmaHistogramEnumeration(kRegenerateButtonTappedHistogram, option);
 }
 
 void RecordAIHubNewBadgeTapped() {
