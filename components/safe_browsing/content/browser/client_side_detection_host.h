305f1d334d534aa85954303b9f7fc1110468bca4
Brian Begnoche <bcb@chromium.org>
2026-04-08 08:53:46 -0700

    Update CSD Credit Card Form history lookup to ignore recent visits.
    This change modifies the ClientSideDetectionHost to use
    HistoryService::GetDailyVisitsToOrigin instead of
    GetVisibleVisitCountToHost for determining if a site is a "new site" for
    credit card form detection.
    
    The new API call is configured to only count visits that occurred more
    than 10 minutes ago*, effectively ignoring very recent visits. This
    helps prevent false positives on sites where a user might be navigating
    back and forth within a short period.
    
    * The 10 minute window is configurable via a new "UserVisitLookback" Finch param.
    
    The associated histogram for tracking the duration of the history
    service call has also been updated.
    
    OBSOLETE_HISTOGRAM[SBClientPhishing.HistoryServiceDuration.GetVisibleVisitCountToHost]=Replaced
    by SBClientPhishing.HistoryServiceDuration.GetDailyVisitsToOrigin
    
    Bug: 492126455
    Change-Id: Id877faee3569d7ce4acfbdf573cc5926d66231da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7675697
    Reviewed-by: Seung Jae Lim <andysjlim@chromium.org>
    Commit-Queue: Brian Begnoche <bcb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1611540}

--- components/safe_browsing/content/browser/client_side_detection_host.h
+++ components/safe_browsing/content/browser/client_side_detection_host.h
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_status_code.h"
 #include "services/service_manager/public/cpp/binder_registry.h"
 #include "url/gurl.h"
+#include "url/origin.h"
 
 #if BUILDFLAG(IS_ANDROID)
 #include "components/safe_browsing/core/browser/referring_app_info.h"  // nogncheck
@@ -328,6 +329,8 @@ class ClientSideDetectionHost
   FRIEND_TEST_ALL_PREFIXES(ClientSideDetectionHostCreditCardFormTest,
                            DoesNotStartPreclassificationOnRepeatSiteVisit);
   FRIEND_TEST_ALL_PREFIXES(ClientSideDetectionHostCreditCardFormTest,
+                           IgnoresVisitsInPastTenMinutes);
+  FRIEND_TEST_ALL_PREFIXES(ClientSideDetectionHostCreditCardFormTest,
                            DoesNotStartPreclassificationOnServerHeuristic);
   FRIEND_TEST_ALL_PREFIXES(
       ClientSideDetectionHostCreditCardFormReferringAppTest,
@@ -554,7 +557,7 @@ class ClientSideDetectionHost
   void OnCreditCardFormVisitCount(
       std::optional<base::TimeTicks> start_time,
       credit_card_form::FieldDetectionHeuristic field_heuristic,
-      history::VisibleVisitCountToHostResult history_result);
+      history::DailyVisitsResult history_result);
 
   // Fills in the screenshot data for the given `request`. Only fill if the
   // report type is USER_REPORT.
@@ -616,7 +619,7 @@ class ClientSideDetectionHost
   // Cached result of calling HistoryService.GetVisibleVisitCountToHost
   // for some URL.
   std::optional<GURL> last_history_url_;
-  std::optional<history::VisibleVisitCountToHostResult> last_history_result_;
+  std::optional<history::DailyVisitsResult> last_history_result_;
 
   // The token fetcher used for getting access token.
   std::unique_ptr<SafeBrowsingTokenFetcher> token_fetcher_;
