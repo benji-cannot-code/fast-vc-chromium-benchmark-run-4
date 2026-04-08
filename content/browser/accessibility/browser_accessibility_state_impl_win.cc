a6c18048e9c4ef0090e68bb16561f5324bd09725
Jacques Newman <janewman@microsoft.com>
2026-04-07 17:29:59 -0700

    [a11y] Remove kDisableUiaProviderWhenJawsIsRunning killswitch
    This enabled-by-default killswitch was due for removal at Chrome 139
    according the to TODO. The UIA provider disable for incompatible JAWS
    versions is now unconditional.
    
    Fixed: 407891291
    Change-Id: Iec440f885864ffb4906bce5fb730b56d6731b05c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7736505
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Auto-Submit: Jacques Newman <janewman@microsoft.com>
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Commit-Queue: Jacques Newman <janewman@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1611075}

--- content/browser/accessibility/browser_accessibility_state_impl_win.cc
+++ content/browser/accessibility/browser_accessibility_state_impl_win.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_deref.h"
 #include "base/containers/heap_array.h"
 #include "base/debug/crash_logging.h"
-#include "base/feature_list.h"
 #include "base/files/file_path.h"
 #include "base/functional/bind.h"
 #include "base/metrics/histogram_macros.h"
@@ -35,12 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 namespace {
-
-// Killswitch to turn off this feature remotely in case it affects ATs in a way
-// we didn't expect. This is temporary.
-// TODO(crbug.com/407891291): Remove this feature flag in Chrome 139.
-BASE_FEATURE(kDisableUiaProviderWhenJawsIsRunning,
-             base::FEATURE_ENABLED_BY_DEFAULT);
 
 const wchar_t kNarratorRegistryKey[] = L"Software\\Microsoft\\Narrator\\NoRoam";
 const wchar_t kWinMagnifierRegistryKey[] =
@@ -405,8 +398,7 @@ void BrowserAccessibilityStateImplWin::OnDiscoveredAssistiveTech(
   //   * 2023.2402.1+
   //   * 2024.2312.99+
   //   * 2025+
-  if (base::FeatureList::IsEnabled(kDisableUiaProviderWhenJawsIsRunning) &&
-      ui::AXPlatform::GetInstance().IsUiaProviderEnabled()) {
+  if (ui::AXPlatform::GetInstance().IsUiaProviderEnabled()) {
     for (const auto& info : at_infos) {
       if (info.tech == AccessibilityTarget::kJaws && info.version.has_value()) {
         if (!IsJawsCompatibleWithUIA(*info.version)) {
