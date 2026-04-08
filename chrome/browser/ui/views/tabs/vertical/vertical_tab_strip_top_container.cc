28fa05280396cf923319f80f95f32735a5ac804b
Kunal Daftari <kunaldaftari@google.com>
2026-04-08 14:00:19 -0700

    [Vertical Tabs] Ensuring Vertical Padding for Collapsed State is Consistent
    This change modifies the TopContainer class so that the three
    measurements will always have the same padding:
    - Distance between the Collapse Button and top of window
    - Distance between Collapse Button and Combo Button
    - Distance between Combo Button and Separator
    
    Images of Linux + Mac on associated bug.
    
    Bug: 500450582
    Change-Id: I24c3b5baaf0a4adc5a52397e71d529fadda10154
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7738057
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Kunal Daftari <kunaldaftari@google.com>
    Reviewed-by: Charles Meng <charlesmeng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1611768}

--- chrome/browser/ui/views/tabs/vertical/vertical_tab_strip_top_container.cc
+++ chrome/browser/ui/views/tabs/vertical/vertical_tab_strip_top_container.cc
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/view_class_properties.h"
 
 namespace {
-constexpr int kRegionVerticalPadding = 5;
 constexpr int kComboButtonCollapsedPadding = 2;
 }
 
@@ -176,9 +175,13 @@ views::ProposedLayout VerticalTabStripTopContainer::CalculateProposedLayout(
                                           combo_button_->GetVisible(), bounds);
         host_size.SetToMax(gfx::Size(bounds.right(), 0));
 
-        current_y += pref_size.height() + kRegionVerticalPadding;
+        current_y +=
+            pref_size.height() +
+            GetLayoutConstant(
+                LayoutConstant::kVerticalTabStripCollapsedVerticalPadding);
       } else if (caption_button_width_ != 0) {
-        current_y += kRegionVerticalPadding;
+        current_y += GetLayoutConstant(
+            LayoutConstant::kVerticalTabStripCollapsedVerticalPadding);
       }
     }
 
