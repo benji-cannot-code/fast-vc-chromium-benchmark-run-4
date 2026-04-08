01129f4cf943da0b3c95cc66e3c59b5bb823ff96
Jack Thiesen <jthiesen@chromium.org>
2026-04-07 21:56:13 -0700

    ExtensionTabUtil::GetTabById Checks if BrowserWindowInterface is Alive
    This is a speculative fix for the crashes in the associated bug.
    
    Include-Ci-Only-Tests: true
    Bug: 482126243
    Change-Id: Ib1846ee19672cc00eaf8cb3aee7f4b57f58784bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7735230
    Commit-Queue: Jack Thiesen <jthiesen@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1611228}

--- chrome/browser/extensions/activity_log/activity_log_unittest.cc
+++ chrome/browser/extensions/activity_log/activity_log_unittest.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/window_controller.h"
 #include "chrome/browser/extensions/window_controller_list.h"
 #include "chrome/browser/preloading/prefetch/no_state_prefetch/no_state_prefetch_manager_factory.h"
+#include "chrome/browser/ui/browser_window/test/mock_browser_window_interface.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
@@ -462,9 +463,13 @@ class MockWindowController : public WindowController {
                        bool open_in_tab) override {
     return false;
   }
+  BrowserWindowInterface* GetBrowserWindowInterface() override {
+    return &browser_window_interface_;
+  }
 
  private:
   raw_ptr<content::WebContents> contents_;
+  testing::NiceMock<MockBrowserWindowInterface> browser_window_interface_;
 };
 
 TEST_F(ActivityLogTest, ExtractUrls_ScriptingExecuteScript) {
