009db868a61423564f2c14132b4d97640116a98d
Ewann Pellé <ewannpv@chromium.org>
2026-04-08 10:08:36 -0700

    [iOS][Assistant] Remove duplicate pragma section
    Bug: 469050167
    Change-Id: I80d3f07d17f3f13e3997f19b3a3decc0eb3af4bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7735409
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1611618}

--- ios/chrome/browser/assistant/ui/assistant_container_view_controller.mm
+++ ios/chrome/browser/assistant/ui/assistant_container_view_controller.mm
@@ -401,6 +401,25 @@ NSInteger GetMediumDetentHeight(NSInteger absoluteMax) {
   return NO;
 }
 
+#pragma mark - FullscreenUIElement
+
+- (void)updateForFullscreenProgress:(CGFloat)progress {
+  if (_presentationContext == AssistantPresentationContext::kPanel) {
+    self.view.alpha = 1.0;
+    return;
+  }
+
+  self.view.alpha = MIN(1.0, progress / kFullscreenMinimizationThreshold);
+
+  AssistantContainerDetent smallestDetent = _detents.front();
+  if (progress <= kFullscreenMinimizationThreshold &&
+      _activeDetent != smallestDetent) {
+    [self animateToDetent:smallestDetent
+                 duration:kAssistantSheetSpringDuration
+                    curve:UIViewAnimationCurveEaseInOut];
+  }
+}
+
 #pragma mark - Private
 
 // Resumes all the previously paused scroll views.
@@ -422,27 +441,6 @@ NSInteger GetMediumDetentHeight(NSInteger absoluteMax) {
     scrollView.scrollEnabled = NO;
   }
 }
-
-#pragma mark - FullscreenUIElement
-
-- (void)updateForFullscreenProgress:(CGFloat)progress {
-  if (_presentationContext == AssistantPresentationContext::kPanel) {
-    self.view.alpha = 1.0;
-    return;
-  }
-
-  self.view.alpha = MIN(1.0, progress / kFullscreenMinimizationThreshold);
-
-  AssistantContainerDetent smallestDetent = _detents.front();
-  if (progress <= kFullscreenMinimizationThreshold &&
-      _activeDetent != smallestDetent) {
-    [self animateToDetent:smallestDetent
-                 duration:kAssistantSheetSpringDuration
-                    curve:UIViewAnimationCurveEaseInOut];
-  }
-}
-
-#pragma mark - Private
 
 // Configures and adds the background dimming view.
 - (void)setupDimmingView {
