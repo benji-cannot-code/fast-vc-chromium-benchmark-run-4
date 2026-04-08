551827375d1e72bdae91ef2a89aea5be1c057e29
Boris Sazonov <bsazonov@chromium.org>
2026-04-08 11:26:49 -0700

    Update tests to remove usage of raw scopes
    ash::babelorca::OAuthTokenFetcher is using
    OAuthConsumerId::kChromeOsBabelOrca to request access scopes.
    This CL updates tests to use the corresponding test API instead of
    using raw scopes.
    
    Bug: None
    Change-Id: Ib6661910f5ab0f1a33269eebf56c2d00e8f367a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7735737
    Commit-Queue: April Zhou <aprilzhou@google.com>
    Auto-Submit: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: April Zhou <aprilzhou@google.com>
    Cr-Commit-Position: refs/heads/main@{#1611680}

--- chromeos/ash/components/boca/babelorca/oauth_token_fetcher_unittest.cc
+++ chromeos/ash/components/boca/babelorca/oauth_token_fetcher_unittest.cc
@@ -14,9 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/boca/babelorca/token_data_wrapper.h"
 #include "chromeos/ash/components/boca/babelorca/token_fetcher.h"
 #include "components/signin/public/base/consent_level.h"
+#include "components/signin/public/base/oauth_consumer_id.h"
 #include "components/signin/public/identity_manager/account_info.h"
 #include "components/signin/public/identity_manager/identity_test_environment.h"
-#include "google_apis/gaia/gaia_constants.h"
 #include "google_apis/gaia/google_service_auth_error.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -27,7 +27,6 @@ namespace {
 constexpr char kOAuthToken[] = "test-token";
 constexpr base::Time kExpirationTime =
     base::Time::FromDeltaSinceWindowsEpoch(base::Days(50000));
-constexpr char kIdToken[] = "id-test-token";
 constexpr base::TimeDelta kRetryInitialBackoff = base::Milliseconds(500);
 constexpr char kErrorUmaPathTemplate[] =
     "Ash.Boca.Babelorca.Tachyon.OAuthFetchError";
@@ -54,9 +53,9 @@ TEST_F(OAuthTokenFetcherTest, SuccessfulOAuthTokenFetch) {
 
   oauth_token_fetcher.FetchToken(fetch_future.GetCallback());
   identity_test_env_
-      .WaitForAccessTokenRequestIfNecessaryAndRespondWithTokenForScopes(
-          kOAuthToken, kExpirationTime, kIdToken,
-          {GaiaConstants::kTachyonOAuthScope});
+      .WaitForAccessTokenRequestIfNecessaryAndRespondWithTokenForConsumerId(
+          kOAuthToken, kExpirationTime,
+          signin::OAuthConsumerId::kChromeOsBabelOrca);
 
   auto token_data = fetch_future.Get();
   ASSERT_TRUE(token_data.has_value());
@@ -91,9 +90,9 @@ TEST_F(OAuthTokenFetcherTest, OverlappingTokenFetchShouldFail) {
   oauth_token_fetcher.FetchToken(first_fetch_future.GetCallback());
   oauth_token_fetcher.FetchToken(overlap_fetch_future.GetCallback());
   identity_test_env_
-      .WaitForAccessTokenRequestIfNecessaryAndRespondWithTokenForScopes(
-          kOAuthToken, kExpirationTime, kIdToken,
-          {GaiaConstants::kTachyonOAuthScope});
+      .WaitForAccessTokenRequestIfNecessaryAndRespondWithTokenForConsumerId(
+          kOAuthToken, kExpirationTime,
+          signin::OAuthConsumerId::kChromeOsBabelOrca);
 
   EXPECT_TRUE(first_fetch_future.Get().has_value());
   EXPECT_FALSE(overlap_fetch_future.Get().has_value());
@@ -106,14 +105,14 @@ TEST_F(OAuthTokenFetcherTest, SuccessiveTokenFetchShouldProceed) {
 
   oauth_token_fetcher.FetchToken(first_fetch_future.GetCallback());
   identity_test_env_
-      .WaitForAccessTokenRequestIfNecessaryAndRespondWithTokenForScopes(
-          kOAuthToken, kExpirationTime, kIdToken,
-          {GaiaConstants::kTachyonOAuthScope});
+      .WaitForAccessTokenRequestIfNecessaryAndRespondWithTokenForConsumerId(
+          kOAuthToken, kExpirationTime,
+          signin::OAuthConsumerId::kChromeOsBabelOrca);
   oauth_token_fetcher.FetchToken(second_fetch_future.GetCallback());
   identity_test_env_
-      .WaitForAccessTokenRequestIfNecessaryAndRespondWithTokenForScopes(
-          kOAuthToken, kExpirationTime, kIdToken,
-          {GaiaConstants::kTachyonOAuthScope});
+      .WaitForAccessTokenRequestIfNecessaryAndRespondWithTokenForConsumerId(
+          kOAuthToken, kExpirationTime,
+          signin::OAuthConsumerId::kChromeOsBabelOrca);
 
   EXPECT_TRUE(first_fetch_future.Get().has_value());
   EXPECT_TRUE(second_fetch_future.Get().has_value());
@@ -131,9 +130,9 @@ TEST_F(OAuthTokenFetcherTest, RetryOnRetriableError) {
   // First retry success.
   task_environment_.FastForwardBy(kRetryInitialBackoff);
   identity_test_env_
-      .WaitForAccessTokenRequestIfNecessaryAndRespondWithTokenForScopes(
-          kOAuthToken, kExpirationTime, kIdToken,
-          {GaiaConstants::kTachyonOAuthScope});
+      .WaitForAccessTokenRequestIfNecessaryAndRespondWithTokenForConsumerId(
+          kOAuthToken, kExpirationTime,
+          signin::OAuthConsumerId::kChromeOsBabelOrca);
 
   EXPECT_TRUE(fetch_future.Get().has_value());
 }
@@ -156,9 +155,9 @@ TEST_F(OAuthTokenFetcherTest, RespondOnLastSuccessfulRetry) {
   // Second retry success.
   task_environment_.FastForwardBy(kRetryInitialBackoff * 2);
   identity_test_env_
-      .WaitForAccessTokenRequestIfNecessaryAndRespondWithTokenForScopes(
-          kOAuthToken, kExpirationTime, kIdToken,
-          {GaiaConstants::kTachyonOAuthScope});
+      .WaitForAccessTokenRequestIfNecessaryAndRespondWithTokenForConsumerId(
+          kOAuthToken, kExpirationTime,
+          signin::OAuthConsumerId::kChromeOsBabelOrca);
 
   EXPECT_TRUE(fetch_future.Get().has_value());
 }
