5517038b78806f3feed98cff5ed10d6dd4436263
Adam Rice <ricea@chromium.org>
2026-04-08 03:36:12 -0700

    SocketDataPump: Don't destroy the data pipe while a write is pending
    network::SocketDataPump had an issue where if it shut down while a
    network write was pending the contents of the mojo data pipe would be
    read from after it was freed.
    
    To prevent this, keep the handle on the mojo data pipe alive while the
    IOBuffer exists. Also add a test.
    
    Fixed: 497989379
    Change-Id: I43fcb26fd8f9f02f587a7402ead99ca1d03abbb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7735462
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1611366}

--- services/network/socket_data_pump.cc
+++ services/network/socket_data_pump.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/network/socket_data_pump.h"
 
+#include <algorithm>
 #include <optional>
 #include <utility>
 
@@ -21,6 +22,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/tls_client_socket.h"
 
 namespace network {
+
+namespace {
+
+// This is similar to MojoToNetIOBuffer, but only keeps the pending_buffer
+// alive for the duration of the write, without assuming that the entire
+// buffer will be written.
+class MojoToNetPendingBufferOwningIOBuffer : public net::WrappedIOBuffer {
+ public:
+  explicit MojoToNetPendingBufferOwningIOBuffer(
+      scoped_refptr<MojoToNetPendingBuffer> pending_buffer)
+      : net::WrappedIOBuffer(*pending_buffer),
+        pending_buffer_(std::move(pending_buffer)) {}
+
+ private:
+  ~MojoToNetPendingBufferOwningIOBuffer() override = default;
+
+  scoped_refptr<MojoToNetPendingBuffer> pending_buffer_;
+};
+
+}  // namespace
 
 SocketDataPump::SocketDataPump(
     net::StreamSocket* socket,
@@ -199,7 +220,9 @@ void SocketDataPump::SendMore() {
     ShutdownSend();
     return;
   }
-  auto buf = base::MakeRefCounted<net::WrappedIOBuffer>(*pending_send_buffer_);
+
+  auto buf = base::MakeRefCounted<MojoToNetPendingBufferOwningIOBuffer>(
+      pending_send_buffer_);
 
   // Use WeakPtr here because |this| doesn't outlive |socket_|.
   int write_result =
