95ebe6e2e7fc91003d9dd2f4a2d65f1719488901
Sylvain Defresne <sdefresne@chromium.org>
2026-04-08 06:18:29 -0700

    [ios] Use raw_ptr<...> to store the inactive Browser pointer
    By default the compiler generates an ivar of type Browser* for the
    property, so change it to use raw_ptr<Browser> to detect use after
    free.
    
    Bug: none
    Change-Id: Ide255ea65ef1798bbd580651d8d0f67bf44c1c3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7735430
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1611432}

--- ios/chrome/browser/scene/coordinator/scene_coordinator.mm
+++ ios/chrome/browser/scene/coordinator/scene_coordinator.mm
@@ -174,6 +174,7 @@ void OnListFamilyMembersResponse(
   id<TabOpening> _tabOpener;
   base::WeakPtr<Browser> _inactiveBrowser;
   base::WeakPtr<Browser> _regularBrowser;
+  raw_ptr<Browser> _incognitoBrowser;
   // Coordinator for the Tab Grid
   TabGridCoordinator* _tabGridCoordinator;
   // Coordinator for the AppBar.
@@ -248,7 +249,7 @@ void OnListFamilyMembersResponse(
       initWithSceneCommandsEndpoint:self
                      regularBrowser:_regularBrowser.get()
                     inactiveBrowser:_inactiveBrowser.get()
-                   incognitoBrowser:_incognitoBrowser];
+                   incognitoBrowser:_incognitoBrowser.get()];
   _tabGridCoordinator.delegate = self.tabGridDelegate;
   [_tabGridCoordinator start];
   if (IsUseSceneViewControllerEnabled()) {
@@ -273,14 +274,14 @@ void OnListFamilyMembersResponse(
         initWithRegularFullscreenController:FullscreenController::FromBrowser(
                                                 _regularBrowser.get())
               incognitoFullscreenController:FullscreenController::FromBrowser(
-                                                _incognitoBrowser)];
+                                                _incognitoBrowser.get())];
     _sceneMediator.consumer = _viewController;
   }
 
   if (IsChromeNextIaEnabled()) {
-    _appBarCoordinator =
-        [[AppBarCoordinator alloc] initWithRegularBrowser:_regularBrowser.get()
-                                         incognitoBrowser:_incognitoBrowser];
+    _appBarCoordinator = [[AppBarCoordinator alloc]
+        initWithRegularBrowser:_regularBrowser.get()
+              incognitoBrowser:_incognitoBrowser.get()];
     [_appBarCoordinator start];
     [_viewController setAppBar:_appBarCoordinator.viewController];
   }
@@ -1375,6 +1376,10 @@ void OnListFamilyMembersResponse(
 - (void)setTabGridDelegate:(id<TabGridCoordinatorDelegate>)delegate {
   _tabGridDelegate = delegate;
   _tabGridCoordinator.delegate = delegate;
+}
+
+- (Browser*)incognitoBrowser {
+  return _incognitoBrowser.get();
 }
 
 - (void)setIncognitoBrowser:(Browser*)incognitoBrowser {
