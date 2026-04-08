a0be87987f67b807f27dee0be164c63701fe07c8
Thomas Lukaszewicz <tluk@chromium.org>
2026-04-08 11:52:28 -0700

    Create GlobalFeatures during BrowserProcess constructor
    Low-Coverage-Reason: TRIVIAL_CHANGE
    Bug: 431671320
    Change-Id: I995166e8d4f6b3b335daba2e1dc099d0f59b1989
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7735339
    Reviewed-by: Qikai Zhong <qikaizhong@microsoft.com>
    Commit-Queue: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1611688}

--- chrome/browser/browser_process_impl.cc
+++ chrome/browser/browser_process_impl.cc
@@ -324,7 +324,8 @@ BrowserProcessImpl::BrowserProcessImpl(StartupData* startup_data)
 #endif
       platform_part_(std::make_unique<BrowserProcessPlatformPart>()),
       network_time_tracker_(startup_data->chrome_feature_list_creator()
-                                ->TakeNetworkTimeTracker()) {
+                                ->TakeNetworkTimeTracker()),
+      features_(GlobalFeatures::CreateGlobalFeatures()) {
   CHECK(!g_browser_process);
   g_browser_process = this;
 
@@ -349,7 +350,6 @@ const ui::UnownedUserDataHost& BrowserProcessImpl::GetUnownedUserDataHost()
 }
 
 void BrowserProcessImpl::Init() {
-  features_ = GlobalFeatures::CreateGlobalFeatures();
   features_->PreBrowserProcessInit();
 
 #if BUILDFLAG(IS_ANDROID)
