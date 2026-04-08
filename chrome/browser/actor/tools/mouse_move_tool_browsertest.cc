4817a0bde99f4cf1985b819f5a7062e43ec44565
Thomas Nguyen <tungnh@chromium.org>
2026-04-08 04:41:37 -0700

    [Gardener]: Disable test MouseMoveTool_MoveToCoordinateOffScreen
    Bug: 500641605
    Change-Id: I2889b89391786729cf69b30b982d77f6ee746c18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7735436
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Thomas Nguyen <tungnh@chromium.org>
    Owners-Override: Thomas Nguyen <tungnh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1611390}

--- chrome/browser/actor/tools/mouse_move_tool_browsertest.cc
+++ chrome/browser/actor/tools/mouse_move_tool_browsertest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/test/test_future.h"
+#include "build/build_config.h"
 #include "chrome/browser/actor/actor_test_util.h"
 #include "chrome/browser/actor/tools/tool_request.h"
 #include "chrome/browser/actor/tools/tools_test_util.h"
@@ -144,8 +145,15 @@ IN_PROC_BROWSER_TEST_F(ActorMouseMoveToolBrowserTest,
 
 // Moving mouse to a coordinate not in the viewport should fail without
 // dispatching events.
+#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_WIN)
+#define MAYBE_MouseMoveTool_MoveToCoordinateOffScreen \
+  DISABLED_MouseMoveTool_MoveToCoordinateOffScreen
+#else
+#define MAYBE_MouseMoveTool_MoveToCoordinateOffScreen \
+  MouseMoveTool_MoveToCoordinateOffScreen
+#endif
 IN_PROC_BROWSER_TEST_F(ActorMouseMoveToolBrowserTest,
-                       MouseMoveTool_MoveToCoordinateOffScreen) {
+                       MAYBE_MouseMoveTool_MoveToCoordinateOffScreen) {
   const GURL url = embedded_test_server()->GetURL("/actor/mouse_log.html");
   ASSERT_TRUE(content::NavigateToURL(web_contents(), url));
 
