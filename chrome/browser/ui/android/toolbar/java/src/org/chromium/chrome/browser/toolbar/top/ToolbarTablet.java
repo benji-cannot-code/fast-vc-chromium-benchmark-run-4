8a715c4adc7e5cc568383e10755ac650df6dd81b
Sophie Crowley <crowleso@google.com>
2026-04-08 11:50:17 -0700

    [Signin][Android] Remove VisibleForTesting annotation in ToolbarTablet
    Renamed setReloadButtonCoordinator and setBackButtonCoordinator as they
    are only used for testing and removed unneeded annotation for
    allocateAvailableToolbarWidth.
    
    Follow up from
    https://crrev.com/c/7686514/21..24/chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarTablet.java#b863
    
    Bug: 478828569
    Change-Id: I0955b5cda61bca93c19a79f3dc1b16b23994fe61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7734542
    Reviewed-by: Tanmoy Mollik <triploblastic@google.com>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Sophie Crowley <crowleso@google.com>
    Cr-Commit-Position: refs/heads/main@{#1611685}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarTablet.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarTablet.java
@@ -26,7 +26,6 @@ import androidx.annotation.ColorInt;
 import androidx.annotation.ColorRes;
 import androidx.annotation.DimenRes;
 import androidx.annotation.DrawableRes;
-import androidx.annotation.VisibleForTesting;
 import androidx.appcompat.content.res.AppCompatResources;
 import androidx.core.view.ViewCompat;
 import androidx.core.widget.ImageViewCompat;
@@ -548,7 +547,6 @@ public class ToolbarTablet extends ToolbarLayout {
      * @param widthMeasureSpec The width measure spec to be used for measurement.
      * @param heightMeasureSpec The height measure spec to be used for measurement.
      */
-    @VisibleForTesting
     static void allocateAvailableToolbarWidth(
             @Nullable ToolbarWidthConsumer[] toolbarWidthConsumer,
             int availableWidthDp,
@@ -854,14 +852,12 @@ public class ToolbarTablet extends ToolbarLayout {
         mToolbarButtonsVisible = value;
     }
 
-    @VisibleForTesting
-    void setReloadButtonCoordinator(ReloadButtonCoordinator coordinator) {
+    void setReloadButtonCoordinatorForTesting(ReloadButtonCoordinator coordinator) {
         mReloadButtonCoordinator = coordinator;
         mToolbarWidthConsumers[ToolbarComponentId.RELOAD] = mReloadButtonCoordinator;
     }
 
-    @VisibleForTesting
-    void setBackButtonCoordinator(BackButtonCoordinator coordinator) {
+    void setBackButtonCoordinatorForTesting(BackButtonCoordinator coordinator) {
         mBackButtonCoordinator = coordinator;
         mToolbarWidthConsumers[ToolbarComponentId.BACK] = mBackButtonCoordinator;
     }
