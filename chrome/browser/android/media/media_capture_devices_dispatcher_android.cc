4a873f78bde8f9e84ed6d52134e4572184ec1a75
Grace Cham <hscham@google.com>
2026-04-07 20:42:31 -0700

    [Media Capture] add observer for tab sharing event state change
    This will be used by clank (crrev.com/i/9151916) to notify android
    system that the app content media projection is ended on the app side.
    
    Bug: 478029555
    Test: manual
    Change-Id: I55d20f85aba1e21b7255d8c7e0e3b713b88be30c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7728468
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Grace Cham <hscham@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1611199}

--- chrome/browser/android/media/media_capture_devices_dispatcher_android.cc
+++ chrome/browser/android/media/media_capture_devices_dispatcher_android.cc
@@ -9,19 +9,59 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/android/jni_android.h"
 #include "base/android/jni_string.h"
-#include "content/public/browser/web_contents.h"
+#include "base/no_destructor.h"
 #include "chrome/browser/media/webrtc/media_stream_capture_indicator.h"
+#include "content/public/browser/web_contents.h"
 
 // Must come after all headers that specialize FromJniType() / ToJniType().
 #include "chrome/android/chrome_jni_headers/MediaCaptureDevicesDispatcherAndroid_jni.h"
 
 using base::android::JavaRef;
+using base::android::ScopedJavaLocalRef;
 
 namespace {
+
+class MediaCaptureDevicesDispatcherObserverAndroid
+    : public MediaStreamCaptureIndicator::Observer {
+ public:
+  static MediaCaptureDevicesDispatcherObserverAndroid* GetInstance() {
+    static base::NoDestructor<MediaCaptureDevicesDispatcherObserverAndroid>
+        instance;
+    return instance.get();
+  }
+
+  MediaCaptureDevicesDispatcherObserverAndroid() {
+    MediaCaptureDevicesDispatcher::GetInstance()
+        ->GetMediaStreamCaptureIndicator()
+        ->AddObserver(this);
+  }
+
+  MediaCaptureDevicesDispatcherObserverAndroid(
+      const MediaCaptureDevicesDispatcherObserverAndroid&) = delete;
+  MediaCaptureDevicesDispatcherObserverAndroid& operator=(
+      const MediaCaptureDevicesDispatcherObserverAndroid&) = delete;
+
+  ~MediaCaptureDevicesDispatcherObserverAndroid() override = default;
+
+  // MediaStreamCaptureIndicator::Observer:
+  void OnIsCapturingTabChanged(content::WebContents* web_contents,
+                               bool is_capturing_tab) override {
+    JNIEnv* env = base::android::AttachCurrentThread();
+    ScopedJavaLocalRef<jobject> java_web_contents =
+        web_contents->GetJavaWebContents();
+    Java_MediaCaptureDevicesDispatcherAndroid_onIsCapturingTabChanged(
+        env, java_web_contents, is_capturing_tab);
+  }
+};
+
+void EnsureObserverCreated() {
+  MediaCaptureDevicesDispatcherObserverAndroid::GetInstance();
+}
 
 bool CallIndicator(const JavaRef<jobject>& java_web_contents,
                    bool (MediaStreamCaptureIndicator::*predicate)(
                        content::WebContents*) const) {
+  EnsureObserverCreated();
   content::WebContents* web_contents =
       content::WebContents::FromJavaWebContents(java_web_contents);
   const auto& indicator = MediaCaptureDevicesDispatcher::GetInstance()
@@ -69,6 +109,7 @@ static bool JNI_MediaCaptureDevicesDispatcherAndroid_IsCapturingScreen(
 static void JNI_MediaCaptureDevicesDispatcherAndroid_NotifyStopped(
     JNIEnv* env,
     const JavaRef<jobject>& java_web_contents) {
+  EnsureObserverCreated();
   content::WebContents* web_contents =
       content::WebContents::FromJavaWebContents(java_web_contents);
   const auto& indicator = MediaCaptureDevicesDispatcher::GetInstance()
@@ -81,6 +122,7 @@ static void JNI_MediaCaptureDevicesDispatcherAndroid_NotifyStopped(
 static void JNI_MediaCaptureDevicesDispatcherAndroid_NotifyDisplayMediaStopped(
     JNIEnv* env,
     const JavaRef<jobject>& java_web_contents) {
+  EnsureObserverCreated();
   content::WebContents* web_contents =
       content::WebContents::FromJavaWebContents(java_web_contents);
   const auto& indicator = MediaCaptureDevicesDispatcher::GetInstance()
