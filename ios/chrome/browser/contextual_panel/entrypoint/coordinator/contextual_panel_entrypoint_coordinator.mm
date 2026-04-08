a19d6959a0b045b4faea705aecade0930b16a556
Ewann Pelle <ewannpv@chromium.org>
2026-04-08 09:31:33 -0700

    [iOS] Remove protocol defined in header
    Bug: 500645192
    Change-Id: I741db248e59a8f9e118a7aa14f384608b64b6a17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7735589
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1611582}

--- ios/chrome/browser/contextual_panel/entrypoint/coordinator/contextual_panel_entrypoint_coordinator.mm
+++ ios/chrome/browser/contextual_panel/entrypoint/coordinator/contextual_panel_entrypoint_coordinator.mm
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/ui/util/omnibox_util.h"
 
 @interface ContextualPanelEntrypointCoordinator () <
-    ContextualPanelEntrypointCommands,
     ContextualPanelEntrypointMediatorDelegate> {
   // Observer that updates ContextualPanelEntrypointViewController for
   // fullscreen events.
