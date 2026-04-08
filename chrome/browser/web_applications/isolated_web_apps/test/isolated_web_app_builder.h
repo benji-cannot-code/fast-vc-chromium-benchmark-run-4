28881f28b03cc1953add99186dae036e8635be55
Patrick Monette <pmonette@chromium.org>
2026-04-08 10:41:28 -0700

    [traits_bag.h] Use base::ParameterPack for trait validation
    This refactor changes how valid traits are specified in trait bags.
    Instead of using a struct with constructors for each valid trait,
    ValidTraits is now a using alias for base::ParameterPack. The motivation
    for this change is to allow for template metaprogramming to iterate over
    valid traits, which is useful for tasks like generating test cases.
    
    Inheriting valid traits is now done using the new ConcatParameterPacks
    utility.
    
    Existing usages are all migrated.
    
    This change exposed some existing unused fields warning in
    TaskEnvironment on non-Windows platforms. They were silenced using
    [[maybe_unused]].
    
    Bug: 441951621
    Change-Id: I6a8a48c5a5015167118c33a63e2e021ebd9c83db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7708114
    Owners-Override: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1611636}

--- chrome/browser/web_applications/isolated_web_apps/test/isolated_web_app_builder.h
+++ chrome/browser/web_applications/isolated_web_apps/test/isolated_web_app_builder.h
@@ -350,10 +350,7 @@ class BundledIsolatedWebApp {
         BundledIsolatedWebApp::ValidTraits, TraitNames...>
   struct DoNotFakeInstallPage {};
   struct DoNotTrustKey {};
-  struct ValidTraits {
-    explicit ValidTraits(DoNotFakeInstallPage);
-    explicit ValidTraits(DoNotTrustKey);
-  };
+  using ValidTraits = base::ParameterPack<DoNotFakeInstallPage, DoNotTrustKey>;
 
   BundledIsolatedWebApp(const web_package::SignedWebBundleId& web_bundle_id,
                         const std::vector<uint8_t> serialized_bundle,
