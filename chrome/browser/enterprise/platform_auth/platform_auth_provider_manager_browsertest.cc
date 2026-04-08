c9851168742c338b96f2b64b8b026f3ae8ddca63
Stanisław Solarewicz <ssolarewicz@chromium.org>
2026-04-08 02:41:18 -0700

    [Android Entra SSO] Enable platform auth browser tests on Android
    Update the platform authentication browser tests to support Android,
    providing test coverage for the Android Entra SSO feature.
    
    To achieve cross-platform compatibility, the following modifications are
    implemented:
    * Transition test base classes from InProcessBrowserTest to
      PlatformBrowserTest.
    * Replace desktop-specific UI test utilities with
      chrome_test_utils::GetActiveWebContents for navigation.
    * Abstract preference and policy keys to conditionally use Android
      Entra SSO keys or Windows Cloud AP keys based on the platform.
    * Include the updated test files in the Android build configuration.
    
    Bug: b:484014627
    Change-Id: Iad7c3d337aa4b0245173fd12a71e096a43db909d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7725985
    Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    Commit-Queue: Stanisław Solarewicz <ssolarewicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1611344}

--- chrome/browser/enterprise/platform_auth/platform_auth_provider_manager_browsertest.cc
+++ chrome/browser/enterprise/platform_auth/platform_auth_provider_manager_browsertest.cc
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/enterprise/platform_auth/mock_platform_auth_provider.h"
 #include "chrome/browser/enterprise/platform_auth/scoped_set_provider_for_testing.h"
-#include "chrome/test/base/in_process_browser_test.h"
-#include "chrome/test/base/ui_test_utils.h"
+#include "chrome/test/base/chrome_test_utils.h"
+#include "chrome/test/base/platform_browser_test.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
 #include "net/http/http_request_headers.h"
@@ -18,7 +18,7 @@ using ::testing::_;
 
 namespace enterprise_auth {
 
-class PlatformAuthManagerBrowserTest : public InProcessBrowserTest {
+class PlatformAuthManagerBrowserTest : public PlatformBrowserTest {
  public:
   PlatformAuthManagerBrowserTest() = default;
   ~PlatformAuthManagerBrowserTest() override = default;
@@ -32,7 +32,6 @@ class PlatformAuthManagerBrowserTest : public InProcessBrowserTest {
 IN_PROC_BROWSER_TEST_F(PlatformAuthManagerBrowserTest,
                        DataWithoutOriginFiltering) {
   ASSERT_TRUE(embedded_test_server()->Start());
-
   // Install a mock provider.
   auto mock_provider =
       std::make_unique<::testing::StrictMock<MockPlatformAuthProvider>>();
@@ -56,8 +55,9 @@ IN_PROC_BROWSER_TEST_F(PlatformAuthManagerBrowserTest,
   PlatformAuthProviderManager::GetInstance().SetEnabled(true,
                                                         base::OnceClosure());
 
-  EXPECT_TRUE(ui_test_utils::NavigateToURL(
-      browser(), embedded_test_server()->GetURL("/empty.html")));
+  EXPECT_TRUE(
+      content::NavigateToURL(chrome_test_utils::GetActiveWebContents(this),
+                             embedded_test_server()->GetURL("/empty.html")));
   ::testing::Mock::VerifyAndClearExpectations(unsafe_mock_provider);
 
   // The provider instance will be destroyed when `set_provider` is destroyed.
@@ -90,8 +90,9 @@ IN_PROC_BROWSER_TEST_F(PlatformAuthManagerBrowserTest,
   }
 
   // A request now should not invoke the provider for auth data.
-  EXPECT_TRUE(ui_test_utils::NavigateToURL(
-      browser(), embedded_test_server()->GetURL("/empty.html")));
+  EXPECT_TRUE(
+      content::NavigateToURL(chrome_test_utils::GetActiveWebContents(this),
+                             embedded_test_server()->GetURL("/empty.html")));
   ::testing::Mock::VerifyAndClearExpectations(unsafe_mock_provider);
 
   // Configure the manager with the embedded test server as the IdP origin.
@@ -118,8 +119,9 @@ IN_PROC_BROWSER_TEST_F(PlatformAuthManagerBrowserTest,
                                "new-cookie=new-cookie-data");
         std::move(callback).Run(std::move(auth_headers));
       });
-  EXPECT_TRUE(ui_test_utils::NavigateToURL(
-      browser(), embedded_test_server()->GetURL("/empty.html")));
+  EXPECT_TRUE(
+      content::NavigateToURL(chrome_test_utils::GetActiveWebContents(this),
+                             embedded_test_server()->GetURL("/empty.html")));
   ::testing::Mock::VerifyAndClearExpectations(unsafe_mock_provider);
 
   // The provider instance will be destroyed when `set_provider` is destroyed.
