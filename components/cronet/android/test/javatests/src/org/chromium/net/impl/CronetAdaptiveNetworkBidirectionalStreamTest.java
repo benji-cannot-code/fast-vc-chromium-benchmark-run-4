503b2be1fc34aa3596492e5aecd8bfb9c24b74d2
Erik Ovelius <ovelius@google.com>
2026-04-08 10:07:26 -0700

    Refactor CronetAdaptiveRequestContext to do everything in one method that just returns the network handles (primary and fallback) that should be use for an adaptive stream.
    This also uses a previously memorized fallback network as the primary network for new requests.
    
    This simplifies the incision we need to make in CronetUrlRequestContext to support adaptive requests to basically just one method + adaptive stream construction.
    
    Bug: b:474048542
    Change-Id: I0c75e32de350ae87b50ee78443b4112abe7a2325
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7710283
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Commit-Queue: Erik Ovelius <ovelius@google.com>
    Cr-Commit-Position: refs/heads/main@{#1611616}

--- components/cronet/android/test/javatests/src/org/chromium/net/impl/CronetAdaptiveNetworkBidirectionalStreamTest.java
+++ components/cronet/android/test/javatests/src/org/chromium/net/impl/CronetAdaptiveNetworkBidirectionalStreamTest.java
@@ -511,7 +511,10 @@ public class CronetAdaptiveNetworkBidirectionalStreamTest {
                 new CronetAdaptiveRequestContext(ApplicationProvider.getApplicationContext());
         adaptiveRequestContext.setConnectivityManagerWrapperForTest(mockConnectivityManagerWrapper);
 
-        assertEquals(null, adaptiveRequestContext.computeAlternativeNetworkHandle());
+        assertEquals(
+                null,
+                adaptiveRequestContext.computeAlternativeNetworkHandle(
+                        adaptiveRequestContext.getAllNetworks()));
     }
 
     @Test
@@ -530,7 +533,10 @@ public class CronetAdaptiveNetworkBidirectionalStreamTest {
                 new CronetAdaptiveRequestContext(ApplicationProvider.getApplicationContext());
         adaptiveRequestContext.setConnectivityManagerWrapperForTest(mockConnectivityManagerWrapper);
 
-        assertEquals(null, adaptiveRequestContext.computeAlternativeNetworkHandle());
+        assertEquals(
+                null,
+                adaptiveRequestContext.computeAlternativeNetworkHandle(
+                        adaptiveRequestContext.getAllNetworks()));
     }
 
     @Test
@@ -554,7 +560,10 @@ public class CronetAdaptiveNetworkBidirectionalStreamTest {
         adaptiveRequestContext.setConnectivityManagerWrapperForTest(mockConnectivityManagerWrapper);
 
         assertEquals(
-                alternativeHandle, (long) adaptiveRequestContext.computeAlternativeNetworkHandle());
+                alternativeHandle,
+                (long)
+                        adaptiveRequestContext.computeAlternativeNetworkHandle(
+                                adaptiveRequestContext.getAllNetworks()));
     }
 
     @Test
