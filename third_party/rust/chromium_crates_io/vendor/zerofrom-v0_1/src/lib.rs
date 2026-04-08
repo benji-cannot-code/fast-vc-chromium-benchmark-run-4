929c3f5e5069f2edabe8231385ef28a41c5e204c
Martin Verde <thesalsa@google.com>
2026-04-07 20:13:27 -0700

    Roll zerofrom: 0.1.6 => 0.1.7 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * zerofrom: 0.1.6 => 0.1.7; https://docs.rs/crate/zerofrom/0.1.7
    
    Bug: None
    Change-Id: I5d3657c693e8d22399b0a71a80a3cde1351d116f
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7728663
    Reviewed-by: Julia Hansbrough <flowerhack@google.com>
    Commit-Queue: Martin Pan-Verde <thesalsa@google.com>
    Cr-Commit-Position: refs/heads/main@{#1611181}

--- third_party/rust/chromium_crates_io/vendor/zerofrom-v0_1/src/lib.rs
+++ third_party/rust/chromium_crates_io/vendor/zerofrom-v0_1/src/lib.rs
@@ -3,10 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-//! This crate provides [`ZeroFrom`], a trait for converting types in a zero-copy way.
-//!
-//! See the documentation of [`ZeroFrom`] for more details.
-
 // https://github.com/unicode-org/icu4x/blob/main/documents/process/boilerplate.md#library-annotations
 #![cfg_attr(not(any(test, doc)), no_std)]
 #![cfg_attr(
@@ -16,12 +12,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         clippy::unwrap_used,
         clippy::expect_used,
         clippy::panic,
-        clippy::exhaustive_structs,
-        clippy::exhaustive_enums,
-        clippy::trivially_copy_pass_by_ref,
-        missing_debug_implementations,
     )
 )]
+#![warn(missing_docs)]
+
+//! This crate provides [`ZeroFrom`], a trait for converting types in a zero-copy way.
+//!
+//! See the documentation of [`ZeroFrom`] for more details.
+
 // The lifetimes here are important for safety and explicitly writing
 // them out is good even when redundant
 #![allow(clippy::needless_lifetimes)]
