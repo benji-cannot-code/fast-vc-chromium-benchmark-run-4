bf34c3256fbc0d0aab4d48a6081db0db51d0b643
Thomas Nguyen <tungnh@chromium.org>
2026-04-08 07:27:14 -0700

    [Gardener]: Disable test PartitionedCookiePresentV3_CountOnce
    Bug: 500674319
    Change-Id: I9535ff53e4445f918d1d4295581737427b6a034b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7736434
    Owners-Override: Thomas Nguyen <tungnh@chromium.org>
    Reviewed-by: Thomas Nguyen <tungnh@chromium.org>
    Commit-Queue: Thomas Nguyen <tungnh@chromium.org>
    Reviewed-by: Tom Van Goethem <tov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1611475}

--- chrome/browser/net/cookie_use_counter_browsertest.cc
+++ chrome/browser/net/cookie_use_counter_browsertest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/path_service.h"
 #include "base/test/metrics/histogram_tester.h"
+#include "build/build_config.h"
 #include "chrome/browser/net/storage_test_utils.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
@@ -171,8 +172,15 @@ IN_PROC_BROWSER_TEST_F(
       /*expected_count=*/0);
 }
 
+#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_WIN)
+#define MAYBE_PartitionedCookiePresentV3_CountOnce \
+  DISABLED_PartitionedCookiePresentV3_CountOnce
+#else
+#define MAYBE_PartitionedCookiePresentV3_CountOnce \
+  PartitionedCookiePresentV3_CountOnce
+#endif
 IN_PROC_BROWSER_TEST_F(CookieUseCounterBrowserTest,
-                       PartitionedCookiePresentV3_CountOnce) {
+                       MAYBE_PartitionedCookiePresentV3_CountOnce) {
   ukm::TestAutoSetUkmRecorder ukm_recorder;
 
   NavigateTo(kHost, "/set-cookie?p1=a;Secure;Partitioned");
