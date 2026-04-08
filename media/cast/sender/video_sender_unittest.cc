a9fc4db2db3492c05f1b6c966153bfac717afa6b
Thomas Nguyen <tungnh@chromium.org>
2026-04-08 02:32:52 -0700

    [Gardener] Disable MockEncoderGoldenCase tests
    The test is failing on builder Linux MSan bots
    
    Bug: 500613219
    Change-Id: I00f36189709273ce3d23eb2a5422c6ef78400012
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7735744
    Commit-Queue: Thomas Nguyen <tungnh@chromium.org>
    Reviewed-by: Tom Van Goethem <tov@chromium.org>
    Owners-Override: Thomas Nguyen <tungnh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1611340}

--- media/cast/sender/video_sender_unittest.cc
+++ media/cast/sender/video_sender_unittest.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/simple_test_tick_clock.h"
 #include "base/test/task_environment.h"
 #include "base/time/time.h"
+#include "build/build_config.h"
 #include "gpu/command_buffer/client/test_shared_image_interface.h"
 #include "media/base/fake_single_thread_task_runner.h"
 #include "media/base/media_switches.h"
@@ -273,7 +274,13 @@ TEST_P(VideoSenderTest, BuiltInEncoder) {
   RunUntilQuit();
 }
 
-TEST_P(VideoSenderTest, MockEncoderGoldenCase) {
+// TODO(crbug.com/500613219): Enable the test.
+#if defined(MEMORY_SANITIZER) && BUILDFLAG(IS_LINUX)
+#define MAYBE_MockEncoderGoldenCase DISABLED_MockEncoderGoldenCase
+#else
+#define MAYBE_MockEncoderGoldenCase MockEncoderGoldenCase
+#endif
+TEST_P(VideoSenderTest, MAYBE_MockEncoderGoldenCase) {
   CreateSender(EncoderType::kMock);
 
   VideoEncoder::FrameEncodedCallback callback;
