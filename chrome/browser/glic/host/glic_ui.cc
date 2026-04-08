ce0f6986d691e295aa5c78a832def6eb425b3162
Dan Harrington <harringtond@chromium.org>
2026-04-08 14:13:31 -0700

    Disable glic-in-a-tab
    Adds kGlicInATab, and disables it by default. This disables connecting
    to a glic page handler when loading chrome://glic in a tab. It doesn't
    actually prevent loading chrome://glic in a tab, but does prevent
    connection of the glic api.
    
    Bug: b:500728478
    Link: https://chromium-review.googlesource.com/id/Ie75528386ef50d16582a401b31b8db206a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7738056
    Commit-Queue: Dan Harrington <harringtond@chromium.org>
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1611775}

--- chrome/browser/glic/host/glic_ui.cc
+++ chrome/browser/glic/host/glic_ui.cc
@@ -60,6 +60,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace glic {
 
+BASE_FEATURE(kGlicInATab, base::FEATURE_DISABLED_BY_DEFAULT);
+
 // Sets the maximum number of in-flight requests to the guest.
 BASE_FEATURE(kGlicMaxInFlightRequests, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE_PARAM(int,
@@ -396,7 +398,7 @@ void GlicUI::CreatePageHandler(
   if (!IsProfileEligible()) {
     return;
   }
-  if (!host_) {
+  if (!host_ && base::FeatureList::IsEnabled(kGlicInATab)) {
     // Create a Host for tabs navigated to chrome://glic
     GlicKeyedService* service = GlicKeyedServiceFactory::GetGlicKeyedService(
         web_ui()->GetWebContents()->GetBrowserContext());
