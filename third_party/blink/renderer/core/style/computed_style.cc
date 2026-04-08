98fa4b30f8cb314ccdf5c1ce0f940f612962275e
Vladimir Levin <vmpstr@chromium.org>
2026-04-08 08:59:14 -0700

    overscroll: Introduce overscroll-behavior: chain.
    This introduces overscroll-behavior: chain. It also updates the code
    to not use x/y and enum values directly in order to ensure good usage.
    
    Bug: 499018879
    R=flackr@chromium.org
    
    Change-Id: Ibf0abb7d73cc0709674924307a4ff638d5f7ea3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7727248
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1611549}

--- third_party/blink/renderer/core/style/computed_style.cc
+++ third_party/blink/renderer/core/style/computed_style.cc
@@ -3257,6 +3257,8 @@ STATIC_ASSERT_ENUM(cc::OverscrollBehavior::Type::kAuto,
                    EOverscrollBehavior::kAuto);
 STATIC_ASSERT_ENUM(cc::OverscrollBehavior::Type::kContain,
                    EOverscrollBehavior::kContain);
+STATIC_ASSERT_ENUM(cc::OverscrollBehavior::Type::kChain,
+                   EOverscrollBehavior::kChain);
 STATIC_ASSERT_ENUM(cc::OverscrollBehavior::Type::kNone,
                    EOverscrollBehavior::kNone);
 
