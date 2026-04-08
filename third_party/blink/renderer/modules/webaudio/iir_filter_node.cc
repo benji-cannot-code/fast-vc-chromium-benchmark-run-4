849f7537e20b154bf800a18eae44737a11e99a36
Michael Wilson <mjwilson@chromium.org>
2026-04-08 14:09:00 -0700

    [WebAudio] Use .empty() instead of comparing size to 0
    This should cause no functional change.
    
    Change-Id: I606d57513d5789b877ca1f443f546ef72b1f19a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7738044
    Reviewed-by: Mahesh Kannan <kmaheshb@google.com>
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1611773}

--- third_party/blink/renderer/modules/webaudio/iir_filter_node.cc
+++ third_party/blink/renderer/modules/webaudio/iir_filter_node.cc
@@ -97,7 +97,7 @@ IIRFilterNode* IIRFilterNode::Create(BaseAudioContext& context,
     return nullptr;
   }
 
-  if (feedback_coef.size() == 0 ||
+  if (feedback_coef.empty() ||
       (feedback_coef.size() > IIRFilter::kMaxOrder + 1)) {
     exception_state.ThrowDOMException(
         DOMExceptionCode::kNotSupportedError,
@@ -108,7 +108,7 @@ IIRFilterNode* IIRFilterNode::Create(BaseAudioContext& context,
     return nullptr;
   }
 
-  if (feedforward_coef.size() == 0 ||
+  if (feedforward_coef.empty() ||
       (feedforward_coef.size() > IIRFilter::kMaxOrder + 1)) {
     exception_state.ThrowDOMException(
         DOMExceptionCode::kNotSupportedError,
