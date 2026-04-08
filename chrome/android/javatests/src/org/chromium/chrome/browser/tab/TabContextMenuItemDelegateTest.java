8bbc1a6e2c0edc7687766fb6645a462905cdc643
Aishwarya Rajesh <aishwaryarj@google.com>
2026-04-08 16:13:26 -0700

    Re-enable TabContextMenuItemDelegateTest test on desktop
    Speculatively re-enabling, assuming that un-batching the tests in this
    class in crrev.com/c/7723298 should have fixed the issue.
    
    Bug: 497724495
    Change-Id: Ib994b4645ee2d29f3dd2ca7ba12007c6d83006be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7736518
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1611844}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tab/TabContextMenuItemDelegateTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tab/TabContextMenuItemDelegateTest.java
@@ -27,7 +27,6 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.ApplicationTestUtils;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.base.test.util.Restriction;
@@ -103,7 +102,6 @@ public class TabContextMenuItemDelegateTest {
 
     @Test
     @SmallTest
-    @DisableIf.Device(DeviceFormFactor.DESKTOP) // crbug.com/497724495
     public void testOpenInNewTabInGroup_ExistingGroup_ParityEnabled() {
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
