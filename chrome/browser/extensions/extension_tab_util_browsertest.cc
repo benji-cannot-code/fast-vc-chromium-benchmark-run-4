97792a9c6aa7a96327c567abf4ea93e5ee204775
Michael Wojcicka <mwoj@google.com>
2026-04-07 17:35:12 -0700

    extensions: Port ChromeRuntimeOpenOptionsPage* to Desktop Android
    - Refactored ChromeRuntimeAPIDelegate::OpenOptionsPage to allow for
      opening a Browser Window asynchronously (required on Desktop
      Android).
    
    Bug: crbug.com/383366125
    Change-Id: I93c5886118ba5cadfdca5b63eb086455b71aa188
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7729856
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Michael Wojcicka <mwoj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1611084}

--- chrome/browser/extensions/extension_tab_util_browsertest.cc
+++ chrome/browser/extensions/extension_tab_util_browsertest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/bind.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
+#include "base/test/test_future.h"
 #include "build/build_config.h"
 #include "chrome/browser/extensions/chrome_extensions_browser_client.h"
 #include "chrome/browser/extensions/extension_browsertest.h"
@@ -66,7 +67,9 @@ const GURL& GetActiveUrl(BrowserWindowInterface* browser) {
 bool OpenOptionsPageFromAPI(const Extension* extension,
                             content::BrowserContext* browser_context) {
   RuntimeAPI* api = RuntimeAPI::GetFactoryInstance()->Get(browser_context);
-  return api->OpenOptionsPage(extension, browser_context);
+  base::test::TestFuture<bool> future;
+  api->OpenOptionsPage(extension, browser_context, future.GetCallback());
+  return future.Get();
 }
 
 #endif  // BUILDFLAG(ENABLE_EXTENSIONS)
@@ -269,7 +272,7 @@ IN_PROC_BROWSER_TEST_F(ExtensionTabUtilBrowserTest,
   // have only one tab, and it should be open to the options page.
   EXPECT_EQ(1, browser()->tab_strip_model()->count());
   EXPECT_TRUE(content::WaitForLoadStop(
-                  browser()->tab_strip_model()->GetActiveWebContents()));
+      browser()->tab_strip_model()->GetActiveWebContents()));
   EXPECT_EQ(options_url, GetActiveUrl(browser()));
 
   // Calling OpenOptionsPage again shouldn't result in any new tabs, since we
@@ -277,7 +280,7 @@ IN_PROC_BROWSER_TEST_F(ExtensionTabUtilBrowserTest,
   EXPECT_TRUE(ExtensionTabUtil::OpenOptionsPage(options_in_tab, browser()));
   EXPECT_EQ(1, browser()->tab_strip_model()->count());
   EXPECT_TRUE(content::WaitForLoadStop(
-                  browser()->tab_strip_model()->GetActiveWebContents()));
+      browser()->tab_strip_model()->GetActiveWebContents()));
   EXPECT_EQ(options_url, GetActiveUrl(browser()));
 
   // Navigate to google.com (something non-newtab, non-options). Calling
@@ -289,7 +292,7 @@ IN_PROC_BROWSER_TEST_F(ExtensionTabUtilBrowserTest,
   EXPECT_TRUE(ExtensionTabUtil::OpenOptionsPage(options_in_tab, browser()));
   EXPECT_EQ(2, browser()->tab_strip_model()->count());
   EXPECT_TRUE(content::WaitForLoadStop(
-                  browser()->tab_strip_model()->GetActiveWebContents()));
+      browser()->tab_strip_model()->GetActiveWebContents()));
   EXPECT_EQ(options_url, GetActiveUrl(browser()));
 
   // Navigate the tab to a different extension URL, and call OpenOptionsPage().
@@ -301,7 +304,7 @@ IN_PROC_BROWSER_TEST_F(ExtensionTabUtilBrowserTest,
   EXPECT_TRUE(ExtensionTabUtil::OpenOptionsPage(options_in_tab, browser()));
   EXPECT_EQ(3, browser()->tab_strip_model()->count());
   EXPECT_TRUE(content::WaitForLoadStop(
-                  browser()->tab_strip_model()->GetActiveWebContents()));
+      browser()->tab_strip_model()->GetActiveWebContents()));
   EXPECT_EQ(options_url, GetActiveUrl(browser()));
 
   // If the user navigates to the options page e.g. by typing in the url, it
