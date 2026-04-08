e1c9a2f0395fb0992a6875663b4fb6de29c4fb3b
Awad Osman <awado@google.com>
2026-04-08 05:16:56 -0700

    [Bundled Settings] Add Modal when user turns off SafeBrowsing
    Adds the previous dialog that was used in the V1 version of the security
    settings page when the user tries to turn off safebrowsing. This CL also
    adds some logic to restore the previous safebrowsing state if the user
    cancels out of the dialog.
    
    Screenshot:crbug.com/488374886#comment2
    
    Bug: 488374886
    Change-Id: I8b8e982bafb7ed61a3e6f0ee326f01b9fa35c438
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7703694
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Awad Osman <awado@google.com>
    Cr-Commit-Position: refs/heads/main@{#1611399}

--- chrome/test/data/webui/settings/security_page_v2_test.ts
+++ chrome/test/data/webui/settings/security_page_v2_test.ts
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // clang-format off
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
-import type {CrExpandButtonElement, SettingsSecureDnsV2Element, SettingsSecurityPageV2Element} from 'chrome://settings/lazy_load.js';
+import type {CrExpandButtonElement, SettingsSecureDnsV2Element, SettingsSecurityPageV2Element, SettingsSimpleConfirmationDialogElement} from 'chrome://settings/lazy_load.js';
 import {HttpsFirstModeSetting, JavascriptOptimizerSetting, SafeBrowsingSetting, SecuritySettingsBundleSetting} from 'chrome://settings/lazy_load.js';
 import type {SettingsPrefsElement} from 'chrome://settings/settings.js';
 import type {ControlledRadioButtonElement, SettingsToggleButtonElement} from 'chrome://settings/settings.js';
@@ -22,6 +22,30 @@ import {TestSecurityPageBrowserProxy} from './test_security_page_browser_proxy.j
 import {TestOpenWindowProxy} from 'chrome://webui-test/test_open_window_proxy.js';
 
 // clang-format on
+
+async function clickCancelOnDisableSafebrowsingDialog(
+    page: SettingsSecurityPageV2Element) {
+  const confirmationDialog =
+      page.shadowRoot!.querySelector<SettingsSimpleConfirmationDialogElement>(
+          'settings-simple-confirmation-dialog');
+  assertTrue(!!confirmationDialog);
+  const closePromise = eventToPromise('close', confirmationDialog);
+  confirmationDialog.$.cancel.click();
+  await closePromise;
+  await flushTasks();
+}
+
+async function clickConfirmOnDisableSafebrowsingDialog(
+    page: SettingsSecurityPageV2Element) {
+  const confirmationDialog =
+      page.shadowRoot!.querySelector<SettingsSimpleConfirmationDialogElement>(
+          'settings-simple-confirmation-dialog');
+  assertTrue(!!confirmationDialog);
+  const closePromise = eventToPromise('close', confirmationDialog);
+  confirmationDialog.$.confirm.click();
+  await closePromise;
+  await flushTasks();
+}
 
 suite('Main', function() {
   let testMetricsBrowserProxy: TestMetricsBrowserProxy;
@@ -559,6 +583,85 @@ suite('Main', function() {
             routes.SITE_SETTINGS_JAVASCRIPT_OPTIMIZER,
             Router.getInstance().getCurrentRoute());
       });
+
+  test('DisableSafebrowsingDialog_Confirm', async function() {
+    // Set SB to disabled.
+    page.setPrefValue('generated.safe_browsing', SafeBrowsingSetting.DISABLED);
+    await flushTasks();
+    page.$.safeBrowsingRow.dispatchEvent(new CustomEvent(
+        'toggle-button-change', {bubbles: true, composed: true}));
+    flush();
+
+    // Confirm that SB is Off after dialog confirmation.
+    await clickConfirmOnDisableSafebrowsingDialog(page);
+    assertFalse(page.$.standardProtectionButton.checked);
+    assertFalse(page.$.enhancedProtectionButton.checked);
+
+    assertEquals(
+        SafeBrowsingSetting.DISABLED, page.prefs.generated.safe_browsing.value);
+  });
+
+  // TODO(crbug.com/500641616): Investigate why user click action is flaky.
+  test('DisableSafebrowsingDialog_CancelFromStandard', async function() {
+    // Expand the row.
+    page.$.safeBrowsingRow.$.expandButton.click();
+    await flushTasks();
+    assertEquals(
+        SafeBrowsingSetting.STANDARD, page.prefs.generated.safe_browsing.value);
+
+    // Simulate clicks on safe-browsing-radio-group.
+    page.$.enhancedProtectionButton.click();
+    await flushTasks();
+    page.$.standardProtectionButton.click();
+    await flushTasks();
+
+    assertEquals(
+        SafeBrowsingSetting.STANDARD, page.prefs.generated.safe_browsing.value);
+
+    // Simulate turning off Safe Browsing.
+    page.setPrefValue('generated.safe_browsing', SafeBrowsingSetting.DISABLED);
+    await flushTasks();
+    page.$.safeBrowsingRow.dispatchEvent(new CustomEvent(
+        'toggle-button-change', {bubbles: true, composed: true}));
+    flush();
+
+    await clickCancelOnDisableSafebrowsingDialog(page);
+
+    assertTrue(page.$.standardProtectionButton.checked);
+    assertEquals(
+        SafeBrowsingSetting.STANDARD, page.prefs.generated.safe_browsing.value);
+  });
+
+  test('DisableSafebrowsingDialog_CancelFromEnhanced', async function() {
+    page.$.safeBrowsingRow.$.expandButton.click();
+    await flushTasks();
+    assertEquals(
+        SafeBrowsingSetting.STANDARD, page.prefs.generated.safe_browsing.value);
+
+    // Set SB to Enhanced.
+    page.setPrefValue('generated.safe_browsing', SafeBrowsingSetting.ENHANCED);
+    await flushTasks();
+
+    page.$.safeBrowsingRadioGroup.dispatchEvent(
+        new CustomEvent('change', {bubbles: true, composed: true}));
+    await flushTasks();
+    assertEquals(
+        SafeBrowsingSetting.ENHANCED, page.prefs.generated.safe_browsing.value);
+
+    // Simulate turning off Safe Browsing.
+    page.setPrefValue('generated.safe_browsing', SafeBrowsingSetting.DISABLED);
+    await flushTasks();
+    page.$.safeBrowsingRow.dispatchEvent(new CustomEvent(
+        'toggle-button-change', {bubbles: true, composed: true}));
+    flush();
+
+    // Make sure enhanced is still selected after cancelling out of the dialog.
+    await clickCancelOnDisableSafebrowsingDialog(page);
+
+    assertTrue(page.$.enhancedProtectionButton.checked);
+    assertEquals(
+        SafeBrowsingSetting.ENHANCED, page.prefs.generated.safe_browsing.value);
+  });
 });
 
 suite('SecurityKeysSubpageDisabled', function() {
