9773b026ca12b61eea55f0c4ee82d066abf15aa7
Kent Tamura <tkent@chromium.org>
2026-04-08 01:52:47 -0700

    Migrate String::Substring() to substr()/subview() in core/
    This change updates several usages of the legacy String::Substring()
    method to String::substr() or String::subview() to align with modern
    C++ string API usage and improve consistency across the codebase.
    
    The transitions in the affected files are safe:
    
    * core/dom/first_letter_pseudo_element.cc
    `length` won't extend `old_text.length()` because of the
    FirstLetterLength() logic.
    
    * core/origin_trials/origin_trial_context.cc
    `header_value[pos]` must be safe in this function, so `start_pos`
    is within bounds as well.
    
    * core/script/import_map.cc
    ResolveImportsMatchInternal() is called after checking `key` is
    longer than `matched->key`.
    
    * core/xml/xpath_functions.cc
    ComputeSubstringStartEnd() ensures `bounds.first` is in the range of 1
    to `source_string.length() + 1`.
    
    * core/xml/xpath_parser.cc
    Within this class, `next_pos_` is always less than or equal to
    `data_.length()`, and since `start_pos` is the value of `next_pos_`
    before the loop starts, it is also always less than or equal to
    `data_.length()`.
    
    Bug: 473854537
    Change-Id: I71ca244cdd81662e67811fd384bc1dcd5a7a1f1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7738201
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1611321}

--- third_party/blink/renderer/core/xml/xpath_parser.cc
+++ third_party/blink/renderer/core/xml/xpath_parser.cc
@@ -208,7 +208,7 @@ Token Parser::LexString() {
 }
 
 Token Parser::LexNumber() {
-  int start_pos = next_pos_;
+  string_size_t start_pos = next_pos_;
   bool seen_dot = false;
 
   // Go until end or a non-digits character.
@@ -226,7 +226,7 @@ Token Parser::LexNumber() {
   }
 
   return Token(TokenType::kNumber,
-               data_.Substring(start_pos, next_pos_ - start_pos));
+               data_.substr(start_pos, next_pos_ - start_pos));
 }
 
 bool Parser::LexNCName(String& name) {
